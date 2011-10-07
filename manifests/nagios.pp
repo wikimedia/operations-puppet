07bdfc76ff0706cfea6f6c7f30cd112153a58735
Bhartshorne <ben@wikimedia.org>
2011-10-07 19:10:16 +0000

    removing puppet's cronjob in anticipation of the package installing its own cronjob instead.
    Change-Id: If4cacb975110f99c9aef0c303ba6218cdc5bb982

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -493,11 +493,6 @@ class nagios::ganglia::ganglios {
 	package { "ganglios":
 		ensure => latest;
 	}
-	cron { "ganglios-cron":
-		command => "/usr/sbin/ganglia_parser",
-		user => nagios,
-		ensure => present;
-	}
 	file { "/var/lib/ganglia/xmlcache":
 		ensure => directory,
 		mode => 0755,
