fa8ec8d94c51033ff9b0fe12bcbb580cb2dd802b
asher <afeldman@wikimedia.org>
2011-10-16 14:15:20 -0700

    change quote style
    Change-Id: Iabf72668e521a2e4fa1d9be3d6908d2c7b900a91

--- manifests/varnish.pp
+++ manifests/varnish.pp
@@ -193,7 +193,7 @@ class varnish3 {
 
 		monitor_service { "varnish http ${title}":
 			description => "Varnish HTTP ${title}",
-			check_command => 'check_http_generic!varnishcheck!${port}'
+			check_command => "check_http_generic!varnishcheck!${port}"
 		}
 	}
 
