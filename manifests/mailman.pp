ecb0f6189d1d59b2dadc975834d739b765d6e3ba
dzahn <dzahn@wikimedia.org>
2011-10-25 18:31:53 +0200

    adding lighttpd config for mailing list server into mailman class
    Change-Id: Ifefe1b1203ecdae38a277ef01930c6fc7b51fc65

--- manifests/mailman.pp
+++ manifests/mailman.pp
@@ -18,4 +18,27 @@ class mailman::base {
 			source => "puppet:///files/mailman/mm_cfg.py";
 
 	}
+
+	# lighttpd is used for the mailman UI
+		
+	package { "lighttpd":
+			ensure => latest;
+	}
+
+	file { "lighttpd.conf":
+			mode => 0444,
+			owner => root,
+			group => root,
+			path => "/etc/lighttpd/lighttpd.conf",
+			source => "puppet:///files/lighttpd/list-server.conf";
+	}
+
+	service { "lighttpd":
+			require => [ File["lighttpd.conf"], Package[lighttpd] ],
+			subscribe => File["lighttpd.conf"],
+			ensure => running;
+	}
+
+	# Monitoring
+	monitor_service { "http": description => "HTTP", check_command => "check_http" }
 }
