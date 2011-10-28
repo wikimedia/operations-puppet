7b98f154cdabcef45495737a3fd1302a51e3a487
Demon <chadh@wikimedia.org>
2011-10-27 23:06:25 -0400

    Missing " makes a big difference
    Change-Id: I4e86ee8147c1110fd859e88a4eb97953553dcff5

--- files/svn/hooks/deferred-updates.sh
+++ files/svn/hooks/deferred-updates.sh
@@ -7,5 +7,5 @@
 REV="$1"
 nohup sh -c "
 wget -q --timeout=30 -O /dev/null --post-data=\"\" \
-  \"http://www.mediawiki.org/w/api.php?action=codeupdate&format=xml&repo=MediaWiki&rev=$REV\"
+  \"http://www.mediawiki.org/w/api.php?action=codeupdate&format=xml&repo=MediaWiki&rev=$REV\""
 
