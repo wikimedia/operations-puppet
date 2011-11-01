531b0e21437bbac17c600d309ccc697ddea240ad
Ryan Lane <rlane@wikimedia.org>
2011-11-01 13:27:49 -0700

    Including memcached properly on virt1
    Change-Id: Ifaeac7f1424668dd3cf2fb341e21a71027f9be1d

--- manifests/openstack.pp
+++ manifests/openstack.pp
@@ -315,7 +315,9 @@ class openstack::ldap-server {
 
 class openstack::openstack-manager {
 
-	package { [ 'apache2', 'memcached', 'php5', 'php5-cli', 'php5-mysql', 'php5-ldap', 'php5-uuid', 'php5-curl', 'php5-memcache', 'php-apc' ]:
+	include memcached
+
+	package { [ 'apache2', 'php5', 'php5-cli', 'php5-mysql', 'php5-ldap', 'php5-uuid', 'php5-curl', 'php5-memcache', 'php-apc' ]:
 		ensure => latest;
 	}
 
