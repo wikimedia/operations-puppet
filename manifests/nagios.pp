531da060c393eab455822859c0a8f9ce50cc49d2
jgreen <jgreen@wikimedia.org>
2011-11-01 16:22:20 +0000

    spence nsca iptables rules are tested, enabling puppet to auto-load them
    Change-Id: I7c3da5a79c20bcf67a5bb50a7107985520880da9

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -590,7 +590,7 @@ class nagios::nsca::daemon {
 
 		# temporarily remove the exec rule so that the ruleset is simply created
 		# and we can inspect the file before allowing puppet to auto-load the rules
-		#iptables_add_exec{ "${hostname}": service => "nsca" }
+		iptables_add_exec{ "${hostname}": service => "nsca" }
 	}
 
 	require "nagios::nsca::daemon::iptables"
