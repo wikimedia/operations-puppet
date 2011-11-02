0aeff4e25be7d40eeb19d7bd6763dbebbcb06990
jgreen <jgreen@wikimedia.org>
2011-11-02 20:08:40 +0000

    added a few packages used in fundraising
    Change-Id: I095f8bf9466c768bea8e53bb340e47532c4fc7f2

--- manifests/misc-servers.pp
+++ manifests/misc-servers.pp
@@ -965,7 +965,7 @@ class misc::fundraising {
 	#what is currently on grosley
 	system_role { "misc::fundraising": description => "fundraising sites and operations" }
 
-	package { [ "libapache2-mod-php5", "php-pear", "php5-cli", "php5-common", "php5-curl", "php5-dev", "php5-gd", "php5-mysql", "php5-sqlite", "subversion", "mysql-client-5.1", "phpunit", "dovecot-imapd", "exim4-daemon-heavy", "exim4-config", "python-scipy", "python-matplotlib", "python-argparse", "python-dev", "python-setuptools", "python-mysqldb", "libapache2-mod-python" ]:
+	package { [ "libapache2-mod-php5", "php-pear", "php5-cli", "php5-common", "php5-curl", "php5-dev", "php5-gd", "php5-mysql", "php5-sqlite", "subversion", "mysql-client-5.1", "phpunit", "dovecot-imapd", "exim4-daemon-heavy", "exim4-config", "python-scipy", "python-matplotlib", "python-libxml2", "python-sqlite", "python-sqlitecachec", "python-urlgrabber", "python-argparse", "python-dev", "python-setuptools", "python-mysqldb", "libapache2-mod-python" ]:
 		ensure => latest;
 }
 
