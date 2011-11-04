5616dc201bb0ef1378ad5e8c5fee5864e625e54b
Ryan Lane <rlane@wikimedia.org>
2011-11-04 12:04:10 -0700

    Fix cron spam again
    Change-Id: I748f27dc49ef6719576e024ebac074ee4cf383e2

--- manifests/ldap.pp
+++ manifests/ldap.pp
@@ -682,7 +682,7 @@ class ldap::client::includes {
 			}
 	
 			cron { "manage-exports":
-				command => "/etc/init.d/nscd restart > /dev/null; /usr/bin/python /usr/local/sbin/manage-exports --logfile=/var/log/manage-exports.log > /dev/null",
+				command => "/etc/init.d/nscd restart &> /dev/null; /usr/bin/python /usr/local/sbin/manage-exports --logfile=/var/log/manage-exports.log > /dev/null",
 				require => [ File["/usr/local/sbin/manage-exports"], Package["nscd"], Package["libnss-ldap"], Package["ldap-utils"], File["/etc/ldap.conf"], File["/etc/ldap/ldap.conf"], File["/etc/nsswitch.conf"] ];
 			}
 		} else {
