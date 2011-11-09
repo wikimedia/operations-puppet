71c88e0cde49ba82799c791588b64f12e77fac76
Tim Starling <tstarling@wikimedia.org>
2011-11-10 10:49:06 +1100

    Another one
    Change-Id: Ib668eb4885c7f6065572c5f00feb02518d68e9e8

--- manifests/misc-servers.pp
+++ manifests/misc-servers.pp
@@ -1491,9 +1491,7 @@ class misc::scripts {
 	require misc::passwordScripts
 
 	# For sync-common, required by scap
-	package { wikimedia-task-appserver:
-		ensure => latest;
-	}
+	require mediawiki::packages
 
 	# TODO: Should this be in a package instead, maybe? It's conceptually nicer than keeping scripts in the puppet git repo,
 	# but rebuilding packages isn't as easy as updating a file through this mechanism, right?
