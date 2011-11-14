e88fa0a52da13d5427f39fc0070fa26689fca9c3
Mark Bergsma <mark@wikimedia.org>
2011-11-14 14:11:38 +0100

    Setup interface aggregate on cp3001/cp3002
    Change-Id: If2d0c12f1fb3f48f37f90c13ab7b2c559823cbf5

--- manifests/site.pp
+++ manifests/site.pp
@@ -620,7 +620,9 @@ node "carbon.wikimedia.org" {
 
 node /^cp300[12]\.esams\.wikimedia\.org$/ {
 	$ganglia_aggregator = "true"
-	
+
+	interface_aggregate { "bond0": orig_interface => "eth0", members => [ "eth0", "eth1" ] }
+
 	include cache::bits
 }
 
