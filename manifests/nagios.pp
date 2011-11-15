0bda45fa20dcd40a81444740cf4b265f067aaa94
py <py@wikimedia.org>
2011-11-15 11:50:21 -0500

    adding some junk as part of destroying testing.cfg
    Change-Id: I36ac178e5b05c6f1e533c71db445fa8b527b58f3

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -178,6 +178,7 @@ define decommission_monitor_host {
 # Class which implements the monitoring services on the monitor host
 class nagios::monitor {
 
+	include nagios::monitor::homeless
 	include passwords::nagios::mysql
 	$nagios_mysql_check_pass = $passwords::nagios::mysql::mysql_check_pass
 
@@ -441,6 +442,21 @@ class nagios::monitor {
 			group => root,
 			mode => 0755;
 	}
+}
+
+class nagios::monitor::homeless {
+
+	@monitor_host { "google": ip_address => "74.125.225.84" }
+
+	monitor_service { "GSB_mediawiki": description => "check google safe browsing for mediawiki.org", check_command => "check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=mediawiki.org/!'This site is not currently listed as suspicious'", hostname => "google" }
+	monitor_service { "GSB_wikibooks": description => "check google safe browsing for wikibooks.org", check_command => "check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikibooks.org/!'This site is not currently listed as suspicious'", host_name => "google" }
+	monitor_service { "GSB_wikimedia": description => "check google safe browsing for wikimedia.org", check_command => "check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikimedia.org/!'This site is not currently listed as suspicious'", host_name => "google" }
+	monitor_service { "GSB_wikinews": description => "check google safe browsing for wikinews.org", check_command => "check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikinews.org/!'This site is not currently listed as suspicious'", host_name => "google" }
+	monitor_service { "GSB_wikipedia": description => "check google safe browsing for wikipedia.org", check_command => "check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikipedia.org/!'This site is not currently listed as suspicious'", host_name => "google" }
+	monitor_service { "GSB_wikiquotes": description => "check google safe browsing for wikiquotes.org", check_command => "check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikiquotes.org/!'This site is not currently listed as suspicious'", host_name => "google" }
+	monitor_service { "GSB_wikisource": description => "check google safe browsing for wikisource.org", check_command => "check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikisource.org/!'This site is not currently listed as suspicious'", host_name => "google" }
+	monitor_service { "GSB_wikiversity": description => "check google safe browsing for wikiversity.org", check_command => "check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikiversity.org/!'This site is not currently listed as suspicious'", host_name => "google" }
+	monitor_service { "GSB_wiktionary": description => "check google safe browsing for wiktionary.org", check_command => "check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wiktionary.org/!'This site is not currently listed as suspicious'", host_name => "google" }
 }
 
 class nagios::monitor::pager {
