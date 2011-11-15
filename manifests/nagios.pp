b32a4ddeeede7bac77e09d5ef754538d7e748582
jgreen <jgreen@wikimedia.org>
2011-11-15 22:15:38 +0000

    passive checks nagios master config for payments servers
    Change-Id: I49c025a47b44570da2fa1ec60b0f4ea9103589f8

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -313,6 +313,13 @@ class nagios::monitor {
 		mode => 0644;
 	}
 
+	file { "/etc/nagios/nsca_payments.cfg":
+		source => "puppet:///private/nagios/nsca_payments.cfg",
+		owner => root,
+		group => root,
+		mode => 0644;
+	}
+
 	file { "/etc/nagios/htpasswd.users":
 		source => "puppet:///private/nagios/htpasswd.users",
 		owner => root,
