da44537c7bdb1297197c136afb56e62cd7f9a607
Ryan Lane <rlane@wikimedia.org>
2011-11-16 18:17:03 -0600

    I hate puppet stages. Only run these for labs.
    Change-Id: Ia214fd240aede397ef758e95e4afd2b487030ecb

--- manifests/ldap.pp
+++ manifests/ldap.pp
@@ -584,15 +584,16 @@ class ldap::client::autofs {
 class ldap::client::instance-finish {
 
 	# Hacks to ensure these services are reloaded after the puppet run finishes
+	if $realm == "labs" {
+		exec { "check_nscd":
+			command => "/etc/init.d/nscd restart",
+			unless => "/usr/bin/id novaadmin";
+		}
 
-	exec { "check_nscd":
-		command => "/etc/init.d/nscd restart",
-		unless => "/usr/bin/id novaadmin";
-	}
-
-	exec { "check_autofs":
-		command => "/etc/init.d/autofs restart",
-		creates => "/home/autofs_check";
+		exec { "check_autofs":
+			command => "/etc/init.d/autofs restart",
+			creates => "/home/autofs_check";
+		}
 	}
 
 }
