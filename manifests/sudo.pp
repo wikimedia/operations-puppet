466648b25f70875fd044afa7ac8080cc48f343c2
Ryan Lane <rlane@wikimedia.org>
2011-11-22 00:21:28 -0600

    Give project members sudo access, excluding specific projects
    Change-Id: Ibcb0430cf08c0f5cc0d4cc9598dd342135f78d28

--- manifests/sudo.pp
+++ manifests/sudo.pp
@@ -8,4 +8,26 @@ define sudo_user( $user, $privileges ) {
 		mode => 0440,
 		content => template("sudo/sudoers.erb");
 	}
+
+}
+
+define sudo_group( $group, $privileges ) {
+
+	file { "/etc/sudoers.d/$group":
+		owner => root,
+		group => root,
+		mode => 0440,
+		content => template("sudo/sudoers.erb");
+	}
+
+}
+
+class sudo::labs_project {
+
+	# For all project except ones listed here, give sudo privileges
+	# to all project members
+	if ! ($instanceproject in ['testlabs', 'admininstances']) {
+		sudo_group { $instanceproject: group => "${instanceproject}", privileges => ['ALL = NOPASSWD: ALL'] }
+	}
+
 }
