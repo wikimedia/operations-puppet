e8afc7b702d7292c2ef8459bbf1040170d1409f7
Ryan Lane <rlane@wikimedia.org>
2011-11-22 11:17:44 -0600

    Make sudo_user calls on formey multi-line
    Change-Id: I361970b36199cb73e7ed652c42ba00b5f613481a

--- manifests/site.pp
+++ manifests/site.pp
@@ -1033,9 +1033,15 @@ node "fenari.wikimedia.org" {
 node "formey.wikimedia.org" {
 	install_certificate{ "star.wikimedia.org": }
 
-	sudo_user { demon: user => "demon", privileges => ['ALL = NOPASSWD: /usr/local/sbin/add-ldap-user', 'ALL = NOPASSWD: /usr/local/sbin/delete-ldap-user', 'ALL = NOPASSWD: /usr/local/sbin/modify-ldap-user', 'ALL = NOPASSWD: /usr/local/bin/svn-group', 'ALL = NOPASSWD: /usr/local/sbin/add-labs-user'] }
-	sudo_user { robla: user => "robla", privileges => ['ALL = NOPASSWD: /usr/local/sbin/add-ldap-user', 'ALL = NOPASSWD: /usr/local/sbin/delete-ldap-user', 'ALL = NOPASSWD: /usr/local/sbin/modify-ldap-user', 'ALL = NOPASSWD: /usr/local/bin/svn-group', 'ALL = NOPASSWD: /usr/local/sbin/add-labs-user'] }
-	sudo_user { sumanah: user => "sumanah", privileges => ['ALL = NOPASSWD: /usr/local/sbin/add-ldap-user', 'ALL = NOPASSWD: /usr/local/sbin/delete-ldap-user', 'ALL = NOPASSWD: /usr/local/sbin/modify-ldap-user', 'ALL = NOPASSWD: /usr/local/bin/svn-group', 'ALL = NOPASSWD: /usr/local/sbin/add-labs-user'] }
+	$sudo_perms = [ 'ALL = NOPASSWD: /usr/local/sbin/add-ldap-user',
+			'ALL = NOPASSWD: /usr/local/sbin/delete-ldap-user',
+			'ALL = NOPASSWD: /usr/local/sbin/modify-ldap-user',
+			'ALL = NOPASSWD: /usr/local/bin/svn-group',
+			'ALL = NOPASSWD: /usr/local/sbin/add-labs-user' ];
+	sudo_user { demon: user => "demon", privileges => $sudo_perms }
+	sudo_user { robla: user => "robla", privileges => $sudo_perms }
+	sudo_user { sumanah: user => "sumanah", privileges => $sudo_perms }
+
 	$cluster = "misc"
 	$gid = 550
 	$ldapincludes = ['openldap', 'nss', 'utils']
