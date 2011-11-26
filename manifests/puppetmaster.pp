c3ccac37abd1beef312d11f4bc6c70593a4e766a
Mark Bergsma <mark@wikimedia.org>
2011-11-26 22:31:58 +0100

    Use SSLCARevocationPath to fix an SSL issue, and setup the CRL dir
    Change-Id: Iace3fee2ea084ef9e330be2405f8625069d67b99

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -34,14 +34,20 @@ class puppetmaster($server_name="puppet", $bind_address="*", $verify_client="opt
 			owner => puppet,
 			group => root,
 			mode => 0771;
-		[ "$ssldir/ca", "$ssldir/certificate_requests", "$ssldir/certs", "$ssldir/private", "$ssldir/private_keys", "$ssldir/public_keys" ]:
+		[ "$ssldir/ca", "$ssldir/certificate_requests", "$ssldir/certs", "$ssldir/private", "$ssldir/private_keys", "$ssldir/public_keys", "$ssldir/crl" ]:
 			ensure => directory;
 	}
 	
-	exec { "generate hostcert":
-		require => File["$ssldir/certs"],
-		command => "/usr/bin/puppet cert generate ${server_name}",
-		creates => "$ssldir/certs/${server_name}.pem"
+	exec {
+		"generate hostcert":
+			require => File["$ssldir/certs"],
+			command => "/usr/bin/puppet cert generate ${server_name}",
+			creates => "$ssldir/certs/${server_name}.pem";
+		"setup crl dir":
+			require => File["$ssldir/crl"],
+			path => "/usr/sbin:/usr/bin:/sbin:/bin",
+			command => "ln -s ${ssldir}/ca/ca_crl.pem ${ssldir}/crl/$(openssl crl -in ${ssldir}/ca/ca_crl.pem -hash -noout).0",
+			onlyif => "test ! -L ${ssldir}/crl/$(openssl crl -in ${ssldir}/ca/ca_crl.pem -hash -noout).0"
 	}
 
 	file { "/etc/puppet/fileserver.conf":
