be5a62e2e4795ef626aeee07f05938ecd47c2f62
Antoine Musso <hashar@free.fr>
2011-12-01 14:48:54 +0100

    contint::test prepare testswarm deployement
    testswarm is disabled for now since the package is not yet available
    on our apt repository. Pending build and testing.
    
    This commit is to prepare future testswarm deployement so I got the
    puppet file properly reviewed. That will let me enable testswarm
    fastly once the package is validated.
    
    Change-Id: I264d14f5c73fc88420785e86ef20fa78824c3903

--- manifests/site.pp
+++ manifests/site.pp
@@ -1138,6 +1138,8 @@ node "gallium.wikimedia.org" {
 		misc::contint::test,
 		misc::contint::test::packages,
 		misc::contint::test::jenkins,
+		# Commenting out testswarm since the package is not available yet
+		#misc::contint::test::testswarm,
 		admins::roots,
 		accounts::demon,
 		accounts::hashar,
