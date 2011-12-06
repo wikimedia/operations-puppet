efc714590356b4d9a296d8ac5004e2d14d9865ce
Antoine Musso <hashar@free.fr>
2011-12-06 18:43:01 +0100

    script to fetch mediawiki + puppetization
    Per discussion with mark, provide the scripts using puppet. It is only
    two files so it is easier to handle than a debian package.
    Set up user / cronjob / directory creation
    
    Patchset 2: trim trailing whitespaces
    Patchset 4:
     -create user using 'systemuser' template
     - use /var/lib instead of /home
    
    Change-Id: I119952d7e1d3f6611b5ad6b2218ee901347566ee

--- manifests/site.pp
+++ manifests/site.pp
@@ -1137,6 +1137,7 @@ node "gallium.wikimedia.org" {
 	$cluster = "misc"
 	$gid=500
 	sudo_user { [ "demon", "hashar", "reedy" ]: privileges => ['ALL = (jenkins) NOPASSWD: ALL', 'ALL = NOPASSWD: /etc/init.d/jenkins'] }
+	sudo_user { [ "demon", "hashar", "reedy" ]: privileges => ['ALL = (testswarm) NOPASSWD: ALL'] }
 	include base,
 		ganglia,
 		ntp::client,
