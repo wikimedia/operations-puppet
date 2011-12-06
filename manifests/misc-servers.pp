65ff979a8c61c5e0f5022405e26a148e7983a3c8
Antoine Musso <hashar@free.fr>
2011-12-06 18:43:01 +0100

    script to fetch mediawiki + puppetization
    Per discussion with mark, provide the scripts using puppet. It is only
    two files so it is easier to handle than a debian package.
    Set up user / cronjob / directory creation
    
    Patchset 2: trim trailing whitespaces
    Patchset 4:
     -create user using 'systemuser' template
     - use /var/lib instead of /home
    
    Change-Id: I119952d7e1d3f6611b5ad6b2218ee901347566ee
    
    Conflicts:
    
    	manifests/misc-servers.pp
    
    testswarm: let Apache access our files
    
    * publish checkouts on the www. Apache conf in a new file managed by
      puppet: files/testswarm/testswarm-checkouts.conf
    * Make sure we reload Apache when .conf is changed
    * user testswarm is now part of www-data group so any script run with
      that user would be able to grant access to Apache. Example: allowing
      access to the SQLite databases
    
    Patchset2: get checkouts in /checkouts/ to avoid overriding existing
     /testswarm alias
    Patchset3: wrap apache conf in a VirtualHost
    Patchset4: remove setgid mode which was commented anyway.
    Patchset5: wrap PHP5 stuff with <IfModule mod_php5.c>
    Patchset6: merge Apache configuration with integration.wm.org
    
    Local-Branch: tw-apachefix
    
    Change-Id: I5567693f9b5804bfb43048e0253c1021b4e0dafb
    
    Conflicts:
    
    	manifests/misc-servers.pp
    
    testswarm www conf now depends on integration.wm.org
    
    Change-Id: I1522b3ac1f05a19e08348fdc3049a72ea877bcdc
    
    Conflicts:
    
    	manifests/misc-servers.pp
    
    update testswarm fetcher and fix installation
    
    Change-Id: I1ab9d7cd7cefef7855fc82c9051d990dec958aa9
    
    testswarm: only provide a sample .ini file
    
    Change-Id: I0c93e7540f3c5809c80e3c970e002b5bc0c62871
    
    testswarm: fetcher use a .ini file now
    
    Change-Id: Ide399be47efc4f88a6c8e4636f260abd456e1802
    
    testswarm: exit when no new rev found
    
    Change-Id: I9f7431cdd2f14a4d56012662ef0360f933c3cde3
    
    testwarm: append cron output instead of replacing
    
    Change-Id: I6ac83921872d516c9d2dbbe85219d1c3fb80c4ff
    
    Conflicts:
    
    	manifests/misc-servers.pp
    
    testwarm: reenable install and settings appending
    
    They were disabled during testing phase. We now really want to do the
    checkouts installation since testswarm is in preproduction.
    
    Change-Id: I96b484c2606ca8cc23856442cd9a6bf9772eb71b
    
    testswarm: allow apache to write SQLite databases
    
    This is done by changing the group of SQLite databases files to www-data
    and allowing group right. The testswarm user will still be able to do
    whatever it wants.
    Also rename DB files.
    
    Change-Id: I63b7def12df44a45d50d429d3d1676df8818d333
    
    testswarm: allow apache to write SQLite databases
    
    This is done by changing the group of SQLite databases files to www-data
    and allowing group right. The testswarm user will still be able to do
    whatever it wants.
    Also rename DB files.
    
    Change-Id: I63b7def12df44a45d50d429d3d1676df8818d333
    
    testswarm: debug mode was always enabled!
    
    Change-Id: I34228e9ed7409c073784b96c42ec0b2db4833f40
    
    testswarm: fix DB paths
    
    Change-Id: Id01c040c8309254e5443b0b12d34620c7fb7dd25
    
    testswarm: php chmod need octal, not a string!
    
    Change-Id: I41d91ffb11053ffa60f98183db6ef2c79b242381
    
    testswarm: installer now set wgScriptPath
    
    '/wiki' just does not work :)
    
    Change-Id: I7c86597d6dbac03547683cbd8435385610e89272
    
    testswarm: wgServer is now always autodetected
    
    Change-Id: Ie2562f8a747ae3243dd22ca3da07a52c0fa71be9

--- manifests/misc-servers.pp
+++ manifests/misc-servers.pp
@@ -1378,6 +1378,242 @@ class misc::udp2log::emeryconfig {
 	}
 }
 
+
+# CI test server as per RT #1204
+class misc::contint::test {
+
+	system_role { "misc::contint::test": description => "continuous integration test server" }
+
+	class packages {
+		# split up packages into groups a bit for readability and flexibility ("ensure present" vs. "ensure latest" ?)
+
+		require generic::webserver::php5
+
+		$CI_PHP_packages = [ "php-apc", "php5-cli", "php5-curl", "php5-gd", "php5-intl", "php5-mysql", "php-pear", "php5-sqlite", "php5-tidy", "php5-pgsql" ]
+		$CI_DB_packages  = [ "mysql-server", "sqlite3", "postgresql" ]
+		$CI_DEV_packages = [ "ant", "imagemagick" ]
+
+		package { $CI_PHP_packages:
+			ensure => present;
+		}
+
+		package { $CI_DB_packages:
+			ensure => present;
+		}
+
+		package { $CI_DEV_packages:
+			ensure => present;
+		}
+
+		include svn::client
+
+		include generic::packages::git-core
+
+		# Prefer the PHP packages from Ubuntu
+		generic::apt::pin-package { $CI_PHP_packages: }
+
+	}
+
+	# Common apache configuration
+	apache_module { ssl: name => "ssl" }
+	apache_site { integration: name => "integration.mediawiki.org" }
+
+	class jenkins {
+		# first had code here to add the jenkins repo and key, but this package should be added to our own repo instead
+		# package { "jenkins":
+		#	ensure => present,
+		#	require => File["jenkins.list"],
+		#}
+
+		service { 'jenkins':
+			enable => true,
+			ensure => 'running',
+			hasrestart => true,
+			start => '/etc/init.d/jenkins start',
+			stop => '/etc/init.d/jenkins stop';
+		}
+
+		# nagios monitoring
+		monitor_service { "jenkins": description => "jenkins_service_running", check_command => "check_jenkins_service" }
+
+		file {
+			# Top level jobs folder
+			"/var/lib/jenkins/jobs/":
+				owner => "jenkins",
+				group => "wikidev",
+				mode => 0775,
+				ensure => directory;
+			# Let wikidev users maintain the homepage
+			 "/srv/org":
+					mode => 0755,
+					owner => www-data,
+					group => wikidev,
+					ensure => directory;
+			 "/srv/org/mediawiki":
+					mode => 0755,
+					owner => www-data,
+					group => wikidev,
+					ensure => directory;
+			 "/srv/org/mediawiki/integration":
+					mode => 0755,
+					owner => www-data,
+					group => wikidev,
+					ensure => directory;
+			"/srv/org/mediawiki/integration/index.html":
+				owner => www-data,
+				group => wikidev,
+				mode => 0555,
+				source => "puppet:///files/misc/jenkins/index.html";
+			# Placing the file in sites-available	
+			"/etc/apache2/sites-available/integration.mediawiki.org":
+				path => "/etc/apache2/sites-available/integration.mediawiki.org",
+				mode => 0444,
+				owner => root,
+				group => root,
+				source => "puppet:///files/apache/sites/integration.mediawiki.org";
+
+		}
+
+		# run jenkins behind Apache and have pretty URLs / proxy port 80
+		# https://wiki.jenkins-ci.org/display/JENKINS/Running+Jenkins+behind+Apache
+
+		apache_module { proxy: name => "proxy" }
+		apache_module { proxy_http: name => "proxy_http" }
+
+		file {
+			"/etc/default/jenkins":
+				owner => "root",
+				group => "root",
+				mode => 0444,
+				source => "puppet:///files/misc/jenkins/etc_default_jenkins";
+			"/etc/apache2/conf.d/jenkins_proxy":
+				owner => "root",
+				group => "root",
+				mode => 0444,
+				source => "puppet:///files/misc/jenkins/apache_proxy";
+		}		
+	}
+
+	class testswarm {
+		# Testswarm is configured using the debian package
+		package { testswarm: ensure => latest; }
+
+		# Create a user to run the cronjob with
+		systemuser { testswarm:
+			name  => "testswarm",
+			home  => "/var/lib/testswarm",
+			shell => "/bin/bash",
+			# And part of web server user group so we can let it access
+			# the SQLite databases
+			groups => [ 'www-data' ];
+		}
+
+		# install scripts
+		file {
+			"/etc/testswarm/fetcher-sample.ini":
+				require => [
+					Systemuser[testswarm],
+					Package["testswarm"]
+				],
+				source  => "puppet:///files/testswarm/fetcher-sample.ini",
+				mode    => 0660,
+				owner   => testswarm,
+				group   => testswarm;
+			"/var/lib/testswarm/script":
+				require => Systemuser[testswarm],
+				ensure  => directory,
+				owner   => testswarm,
+				group   => testswarm;
+			"/var/lib/testswarm/script/testswarm-mw-fetcher-run.php":
+				require => Systemuser[testswarm],
+				ensure  => present,
+				source  => "puppet:///files/testswarm/testswarm-mw-fetcher-run.php",
+				owner   => testswarm,
+				group   => testswarm;
+			"/var/lib/testswarm/script/testswarm-mw-fetcher.php":
+				require => Systemuser[testswarm],
+				ensure  => present,
+				source  => "puppet:///files/testswarm/testswarm-mw-fetcher.php",
+				owner   => testswarm,
+				group   => testswarm;
+			# Directory that hold the mediawiki fetches
+			"/var/lib/testswarm/mediawiki-trunk":
+				require => Systemuser[testswarm],
+				ensure  => directory,
+				owner   => testswarm,
+				group   => testswarm;
+			# SQLite databases files need specific user rights
+			"/var/lib/testswarm/mediawiki-trunk/dbs":
+				require => Systemuser[testswarm],
+				ensure  => directory,
+				mode    => 0774,
+				owner   => testswarm,
+				group   => www-data;
+			# Apache configuration to publish mediawiki fetches
+			"/etc/apache2/sites-enabled/testswarm-checkouts.conf":
+				source => "puppet:///files/testswarm/testswarm-checkouts.conf",
+				ensure => present,
+				owner  => root,
+				group  => root;
+			# Override Apache configuration coming from the testswarm package.
+			"/etc/apache2/conf.d/testswarm.conf":
+				ensure => absent;
+		}
+
+		# Reload apache whenever testswarm checkouts configuration change
+		exec {	"update-testswarm-publish-checkout":
+			command => "/usr/sbin/service apache2 reload",
+			subscribe => File['/etc/apache2/sites-available/integration.mediawiki.org'],
+			refreshonly => true,
+			onlyif => "/usr/sbin/apache2ctl configtest"
+		}
+
+		# Finally setup cronjob to fetch our files and setup a MediaWiki instance
+		cron {
+			testswarm-fetcher-mw-trunk:
+				command => "(cd /var/lib/testswarm; php script/testswarm-mw-fetcher-run.php --prod) >> mediawiki-trunk/cron.log 2>&1",
+				user => testswarm,
+				ensure => present;
+		}
+
+	}
+
+	# prevent users from accessing port 8080 directly (but still allow from localhost and own net)
+
+	class iptables-purges {
+
+		require "iptables::tables"
+
+		iptables_purge_service{  "deny_all_http-alt": service => "http-alt" }
+	}
+
+	class iptables-accepts {
+
+		require "misc::contint::test::iptables-purges"
+
+		iptables_add_service{ "lo_all": interface => "lo", service => "all", jump => "ACCEPT" }
+		iptables_add_service{ "localhost_all": source => "127.0.0.1", service => "all", jump => "ACCEPT" }
+		iptables_add_service{ "private_all": source => "10.0.0.0/8", service => "all", jump => "ACCEPT" }
+		iptables_add_service{ "public_all": source => "208.80.154.128/26", service => "all", jump => "ACCEPT" }
+	}
+
+	class iptables-drops {
+
+		require "misc::contint::test::iptables-accepts"
+
+		iptables_add_service{ "deny_all_http-alt": service => "http-alt", jump => "DROP" }
+	}
+
+	class iptables {
+
+		require "misc::contint::test::iptables-drops"
+
+		iptables_add_exec{ "${hostname}": service => "http-alt" }
+	}
+	
+	require "misc::contint::test::iptables"
+}
+
 class misc::udpprofile::collector {
 	system_role { "misc::udpprofile::collector": description => "MediaWiki UDP profile collector" }
 
