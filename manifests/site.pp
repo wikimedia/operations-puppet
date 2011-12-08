53485d6befb80c95ba1ab301a1fda245e1e0356d
Mark Bergsma <mark@wikimedia.org>
2011-12-08 12:25:59 +0100

    Fix all_drives generation
    Change-Id: I2d795b4d0a9cfcf33fb39009112beaa5ea95a609

--- manifests/site.pp
+++ manifests/site.pp
@@ -1595,7 +1595,7 @@ node "lily.knams.wikimedia.org" {
 }
 
 node /ms[1-3]\.pmtpa\.wmnet/ {
-	$all_drives = split(inline_template("<%= ('a'..'${last_drive}').each do |drive| print '/dev/sd'+drive+',' end -%>"), ",")
+	$all_drives = split(inline_template("<%= ('a'..'av').each do |drive| print '/dev/sd'+drive+',' end -%>"), ",")
 	info("all drives: ${all_drives}")
 	include standard
 
