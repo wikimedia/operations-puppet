7be1e1841fe507d1cb0453500daf91f21cf4b5b9
dzahn <dzahn@wikimedia.org>
2011-12-09 14:58:44 +0100

    add techblog apache_site configs - rebased
    Change-Id: I64a8c6a59fa09fc9ed294340df7e05a051554404

--- manifests/misc-servers.pp
+++ manifests/misc-servers.pp
@@ -2178,3 +2178,15 @@ class misc::civicrm {
 
 	systemuser { civimail: name => "civimail", home => "/home/civimail", groups => [ "civimail" ] }
 }
+
+# https://techblog.wikimedia.org
+class misc::techblog {
+
+	system_role { "misc::techblog": description => "Technology blog server" }
+
+	class {'generic::webserver::php5': ssl => 'true'; }
+
+	apache_site { contacts: name => "techblog.wikimedia.org" }
+	apache_site { contacts-ssl: name => "techblog.wikimedia.org-ssl" }
+
+}
