ded99caab48580431d4f1d39dcb1c5023d85d5aa
dzahn <dzahn@wikimedia.org>
2011-12-09 15:04:23 +0100

    add wm09schols, wm10schols, wm10reg apache_site configs & basic puppet classes, rebase, move to wikimanias.pp
    Change-Id: Iea8a3ac47f7724982f5090d928c1b73f0147d3e4

--- manifests/misc/wikimanias.pp
+++ manifests/misc/wikimanias.pp
@@ -6,5 +6,32 @@ class misc::ocs::wikimania2009 {
 
 	class {'generic::webserver::php5': ssl => 'true'; }
 
-	apache_site { contacts: name => "ocs.wikimania2009.wikimedia.org" }
+	apache_site { ocs_wikimania2009: name => "ocs.wikimania2009.wikimedia.org" }
+}
+
+# wm09schols
+class misc::schols::wm09 {
+	system_role { "misc::schols:wm09": description => "wm09schols server" }
+
+	class {'generic::webserver::php5': ssl => 'true'; }
+
+	apache_site { wm09schols: name => "wm09schols.wikimedia.org" }
+}
+
+# wm10schols
+class misc::schols::wm10 {
+	system_role { "misc::schols:wm10": description => "wm10schols server" }
+
+	class {'generic::webserver::php5': ssl => 'true'; }
+
+	apache_site { wm10schols: name => "wm10schols.wikimedia.org" }
+}
+
+# wm10reg
+class misc::reg::wm10 {
+	system_role { "misc::reg:wm10": description => "wm10reg server" }
+
+	class {'generic::webserver::php5': ssl => 'true'; }
+
+	apache_site { wm10reg: name => "wm10reg.wikimedia.org" }
 }
