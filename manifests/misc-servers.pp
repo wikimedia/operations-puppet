b617746c94127980ce7737f1cc6022ee407fb536
dzahn <dzahn@wikimedia.org>
2011-12-09 14:58:44 +0100

    add techblog apache_site configs
    Change-Id: I64a8c6a59fa09fc9ed294340df7e05a051554404

--- manifests/misc-servers.pp
+++ manifests/misc-servers.pp
@@ -2142,3 +2142,52 @@ class misc::racktables {
 	apache_module { rewrite: name => "rewrite" }
 	apache_module { ssl: name => "ssl" }
 }
+
+# http://planet.wikimedia.org/
+class misc::planet {
+	system_role { "misc::planet": description => "Planet weblog aggregator" }
+
+	systemuser { planet: name => "planet", home => "/var/lib/planet", groups => [ "planet" ] }
+
+	class {'generic::webserver::php5': ssl => 'true'; }
+
+	file {
+		"/etc/apache2/sites-available/planet.wikimedia.org":
+			path => "/etc/apache2/sites-available/planet.wikimedia.org",
+			mode => 0444,
+			owner => root,
+			group => root,
+			source => "puppet:///files/apache/sites/planet.wikimedia.org";
+	}
+
+	apache_site { planet: name => "planet.wikimedia.org" }
+
+	package { "python2.6":
+		ensure => latest;
+	}
+}
+
+# https://contacts.wikimedia.org | http://en.wikipedia.org/wiki/CiviCRM
+class misc::civicrm {
+	system_role { "misc::civicrm": description => "CiviCRM server" }
+
+	class {'generic::webserver::php5': ssl => 'true'; }
+
+	apache_site { contacts: name => "contacts.wikimedia.org" }
+	apache_site { contacts-ssl: name => "contacts.wikimedia.org-ssl" }
+
+	systemuser { civimail: name => "civimail", home => "/home/civimail", groups => [ "civimail" ] }
+}
+
+
+# https://techblog.wikimedia.org
+class misc::techblog {
+
+	system_role { "misc::techblog": description => "Technology blog server" }
+
+	class {'generic::webserver::php5': ssl => 'true'; }
+
+	apache_site { contacts: name => "techblog.wikimedia.org" }
+	apache_site { contacts-ssl: name => "techblog.wikimedia.org-ssl" }
+
+}
