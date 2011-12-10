2e5b928024463510e03690109a209776cb71af71
Petrb <benapetr@gmail.com>
2011-12-11 00:13:20 +0100

    Added a base file for bots cluster, this file should contain all defaults for servers we will use on bots project
    Change-Id: I2487c61b924e8f73c5c1b0eb12e75e12e680fb34

---
+++ manifests/bots.pp
@@ -0,0 +1,13 @@
+# bots.pp
+
+@monitor_group { "appserver": description => "wmf labs bot servers" }
+
+class bots::packages {
+	package { [ "mono-runtime", "php5", "php5-cli", "php5-common", "php5-curl", "mysql-client", "ksh", "csh", "lynx" ]:
+		ensure => latest;
+	}
+}
+
+class bots::monitoring {
+	monitor_service { "ssh": description => "SSH", check_command => "check_ssh" }
+}
