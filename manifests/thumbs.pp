7e7023521b320336c98aef07f0dbce22a14887ed
Ariel T. Glenn <ariel@wikimedia.org>
2011-12-12 12:49:46 +0200

    thumbs class to fix thumbs writing perms issues
    Change-Id: Ieb2b8e47fcd3489f08924f989ee3990de4d95597

---
+++ manifests/thumbs.pp
@@ -0,0 +1,52 @@
+class thumbs::server {
+  require "thumbs::users::apache",
+}
+
+class thumbs::users {
+
+  # this matches the values the nginx
+  # user gets out of the box, except that
+  # we also add it to the apache group.
+  class www-data {
+    user { "www-data":
+      name => "www-data",
+      uid => 33,
+      gid => 33,
+      groups => [ "apache" ],
+      home => "/var/www",
+      shell => "/bin/sh",
+      ensure => "present",
+      membership => "minimum",
+      manages_homedir => false,
+      allowdupe => false,
+      system => true,
+      require => thumbs::groups,
+    }
+  }
+
+}
+          
+class thumbs::groups {
+
+  class www-data {
+    group { "www-data":
+      name => "www-data",
+      gid => 33,
+      ensure => "present",
+      allowdupe => false,
+      system => true,
+    }
+  }
+
+  class apache {
+    group { "apache":
+      name => "apache",
+      gid => 48,
+      ensure => "present",
+      allowdupe => false,
+      system => true,
+    }
+  }
+
+}
+    
