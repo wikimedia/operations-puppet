94ff815f8fc31d69a90a20ab8efa351662c6e077
dzahn <dzahn@wikimedia.org>
2011-12-16 12:28:13 +0100

    move blog.wm to blogs.pp, together with techblog - remove duplicate apache_site in techblog after merging configs
    Change-Id: Ic7ec4780a3b0489c8bfbfa852d7ae34d3734a3d6

--- manifests/misc/blogs.pp
+++ manifests/misc/blogs.pp
@@ -1,3 +1,22 @@
+# Wikimedia Blogs
+
+# https://blog.wikimedia.org/
+class misc::blog-wikimedia {
+	system_role { "misc::blog-wikimedia": description => "blog.wikimedia.org" }
+
+	require apaches::packages,
+		generic::php5-gd
+
+	file {
+		"/etc/apache2/sites-available/blog.wikimedia.org":
+			path => "/etc/apache2/sites-available/blog.wikimedia.org",
+			mode => 0444,
+			owner => root,
+			group => root,
+			source => "puppet:///files/apache/sites/blog.wikimedia.org";
+	}
+}
+
 # https://techblog.wikimedia.org
 class misc::techblog {
 
@@ -6,6 +25,5 @@ class misc::techblog {
 	class {'generic::webserver::php5': ssl => 'true'; }
 
 	apache_site { contacts: name => "techblog.wikimedia.org" }
-	apache_site { contacts-ssl: name => "techblog.wikimedia.org-ssl" }
 
 }
