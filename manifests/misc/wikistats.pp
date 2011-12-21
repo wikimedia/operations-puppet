c25c5c0b48a44101ddef181ea0e0b194ee6f6c9f
dzahn <dzahn@wikimedia.org>
2011-12-21 15:17:06 +0100

    initial wikistats class for labs instance
    Change-Id: Ic3b9a3ffa93b5f3827daa4b1852a1a9a1b81551b

---
+++ manifests/misc/wikistats.pp
@@ -0,0 +1,43 @@
+# misc/wikistats.pp
+# mediawiki statistics site
+
+class misc::wikistats {
+system_role { "misc::wikistats": description => "wikistats host" }
+
+	class misc::wikistats::httpd {
+
+		class {'generic::webserver::php5': ssl => 'true'; }
+
+			$wikistats_host = "$instancename.${domain}"
+			$wikistats_ssl_cert = "/etc/ssl/certs/star.wmflabs.pem"
+			$wikistats_ssl_key = "/etc/ssl/private/star.wmflabs.key"
+	
+		file {
+			"/etc/apache2/sites-available/wikistats.wmflabs.org":
+			mode => 444,
+			owner => root,
+			group => root,
+			content => template('apache/sites/wikistats.wmflabs.org.erb'),
+			ensure => present;
+		}
+		
+		
+		apache_confd { namevirtualhost: install => "true", name => "namevirtualhost" }
+		apache_site { wikistats: name => "wikistats.wmflabs.org" }
+	
+		service { apache2:
+			subscribe => Package[libapache2-mod-php5],
+			ensure => running;
+		}
+
+	}
+
+	class misc::wikistats::updates {
+		
+		include generic::mysql::client
+		package { "php5-cli": ensure => 'latest'; }
+		systemuser { wikistats: name => "wikistats", home => "/home/wikistats", groups => [ "wikistats" ] }
+	}
+
+}
+
