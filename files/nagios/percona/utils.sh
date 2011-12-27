d17441181b7f382bef01d74ab8141da18b3a5a07
asher <afeldman@wikimedia.org>
2011-12-27 15:02:13 -0800

    adding percona nagios checks - code with license
    Change-Id: I69ce432042c798779c2329a0c50af7d91eb05857

---
+++ files/nagios/percona/utils.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# ########################################################################
+# This program is part of percona-nagios-checks (http://code.google.com/p/percona-nagios-checks/)
+# License: BSD License (see COPYING)
+# Authors:
+#  Baron Schwartz
+# ########################################################################
+
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+STATE_DEPENDENT=4
