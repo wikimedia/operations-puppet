c5e7940e3cb0a0c76b7aa3558e55e0904690096d
dzahn <dzahn@wikimedia.org>
2011-12-28 19:31:01 +0100

    mariadb - put repo setup into it's own class, require that for package installs
    Change-Id: I7bc42e9d40422e42f3f0c2b0e5686836561b71b5

--- manifests/misc/mariadb.pp
+++ manifests/misc/mariadb.pp
@@ -1,39 +1,48 @@
 # MariaDB - for LABS ONLY
 
 class misc::mariadb($version="5.3") {
-	system_role { "misc::mariadb": description => "MariaDB host" }
+	system_role { "misc::mariadb": description => "host uses external MariaDB repository" }
 
-	file { "/etc/apt/sources.list.d/mariadb${version}.list":
-		ensure	=> present,
-		mode	=> "0444",
-		owner	=> root,
-		group	=> root,
-		source	=> "puppet:///files/apt/mariadb${version}.list";
-	}
+	class repository {
 
-	apt::key { "MariaDB":
-		keyid	=> "1BB943DB",
-		ensure	=> present;
-	}
+		file { "/etc/apt/sources.list.d/mariadb${version}.list":
+			ensure	=> present,
+			mode	=> "0444",
+			owner	=> root,
+			group	=> root,
+			source	=> "puppet:///files/apt/mariadb${version}.list";
+		}
 
-	exec { ["mariadb_update_apt"]:
-		command => "/usr/bin/apt-get update",
-		logoutput => true,
-		onlyif => "/bin/false",
-		subscribe => File["/etc/apt/sources.list.d/mariadb${version}.list"];
+		apt::key { "MariaDB":
+			keyid	=> "1BB943DB",
+			ensure	=> present;
+		}
+
+		exec { ["mariadb_update_apt"]:
+			command => "/usr/bin/apt-get update",
+			logoutput => true,
+			onlyif => "/bin/false",
+			subscribe => File["/etc/apt/sources.list.d/mariadb${version}.list"];
+		}
 	}
 
 	class client {
+		system_role { "misc::mariadb::client": description => "MariaDB client" }
+
+		require repository
+
 		package { "mariadb-client-${version}":
-			ensure	=> latest,
-			require => File["/etc/apt/sources.list.d/mariadb${version}.list"];
+			ensure	=> latest;
 		}
 	}
 
 	class server {
+		system_role { "misc::mariadb::server": description => "MariaDB server" }
+
+		require repository
+
 		package { "mariadb-server-${version}":
-			ensure	=> latest,
-			require => File["/etc/apt/sources.list.d/mariadb${version}.list"];
+			ensure	=> latest;
 		}
 	}
 }
