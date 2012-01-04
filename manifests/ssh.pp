c50796401fbcd120be5a480ef05b3f3de78d4376
Ryan Lane <rlane@wikimedia.org>
2012-01-04 12:49:12 -0800

    Adding in a pre-login banner for labs
    Change-Id: I671274a4c87598a1467dc7a30cdf259ac01e2b24

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -72,6 +72,16 @@ class ssh::config {
 			mode  => 0444,
 			content => template("ssh/sshd_config.erb");
 		}
+		if ( $realm == "labs" ) {
+			file { "/etc/ssh/sshd_banner":
+				owner => root,
+				group => root,
+				mode  => 0444,
+				content => "
+If you are having access problems, please see: https://labsconsole.wikimedia.org/wiki/Access#Accessing_public_and_private_instances
+"
+			}
+		}
 	}
 }
 
