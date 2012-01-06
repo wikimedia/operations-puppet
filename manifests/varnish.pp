0b7817d13cbb20576e2934f2cbc2a9638ff1ed18
asher <afeldman@wikimedia.org>
2012-01-05 16:29:52 -0800

    test new varnish pkgs on mobile cp servers out of production
    Change-Id: I7c7b2ad14b5804f7b1a85bb7b1d3f8c56b3f7e46

--- manifests/varnish.pp
+++ manifests/varnish.pp
@@ -7,10 +7,15 @@
 
 class varnish {
 	class packages {
-		package { varnish3:
-			ensure => "3.0.0-1wmf6";
+		if $hostname =~ /^cp10(41|42)?1$/ {
+			package { [ 'varnish3', 'libvarnishapi1' ]:
+				ensure => "3.0.2-1wmf1";
+			}
+		} else {
+			package { varnish3:
+				ensure => "3.0.0-1wmf6";
+			}
 		}
-
 		package { libworking-daemon-perl: 
 			ensure => present;
 		}
