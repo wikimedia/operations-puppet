328f47a9e21a5e559ab8850dd8c64235b62edbea
dzahn <dzahn@wikimedia.org>
2012-01-09 16:16:56 +0100

    mariadb - add a check if realm is labs, dont setup repo otherwise
    Change-Id: I177a76fa0a71d2568794c771b8eccf4df3876ab2

--- manifests/misc/mariadb.pp
+++ manifests/misc/mariadb.pp
@@ -3,27 +3,32 @@
 class misc::mariadb($mariadb_version="5.3") {
 
 	class repository {
-		system_role { "misc::mariadb::repo": description => "host which uses external MariaDB repository" }
 
-		file { "/etc/apt/sources.list.d/mariadb${mariadb_version}.list":
-			ensure	=> present,
-			mode	=> "0444",
-			owner	=> root,
-			group	=> root,
-			source	=> "puppet:///files/apt/mariadb${mariadb_version}.list";
+		if $realm == "labs" {
+
+			system_role { "misc::mariadb::repo": description => "host which uses external MariaDB repository" }
+
+			file { "/etc/apt/sources.list.d/mariadb${mariadb_version}.list":
+				ensure	=> present,
+				mode	=> "0444",
+				owner	=> root,
+				group	=> root,
+				source	=> "puppet:///files/apt/mariadb${mariadb_version}.list";
+			}
+
+			apt::key { "MariaDB":
+				keyid	=> "1BB943DB",
+				ensure	=> present;
+			}
+
+			exec { ["mariadb_update_apt"]:
+				command => "/usr/bin/apt-get update",
+				logoutput => true,
+				onlyif => "/bin/false",
+				subscribe => File["/etc/apt/sources.list.d/mariadb${mariadb_version}.list"];
+			}
 		}
 
-		apt::key { "MariaDB":
-			keyid	=> "1BB943DB",
-			ensure	=> present;
-		}
-
-		exec { ["mariadb_update_apt"]:
-			command => "/usr/bin/apt-get update",
-			logoutput => true,
-			onlyif => "/bin/false",
-			subscribe => File["/etc/apt/sources.list.d/mariadb${mariadb_version}.list"];
-		}
 	}
 
 	class client {
