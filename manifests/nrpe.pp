540b1331ad32e1df67eca8af3c611cdb6e8cb677
Mark Bergsma <mark@wikimedia.org>
2012-01-16 13:49:53 +0100

    Fix nrpe.pp
    Change-Id: I9e46edab5d509713216cef252082343e652610f4

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -1,32 +1,30 @@
 class nrpe::packages {
-	package { [ "opsview-agent" ]:
-		ensure => absent;
+	package { [ "nagios-nrpe-server", "nagios-plugins", "nagios-plugins-basic", "nagios-plugins-extra", "nagios-plugins-standard" ]:
+		ensure => latest;
 	}
-        package { [ "nagios-nrpe-server", "nagios-plugins", "nagios-plugins-basic", "nagios-plugins-extra", "nagios-plugins-standard" ]:
-                ensure => latest;
-        }
 
-        file {
-                "/etc/nagios/nrpe_local.cfg":
-                        owner => root,
-                        group => root,
-                        mode => 0644,
-                        source => "puppet:///files/nagios/nrpe_local.cfg";
+	file {
+		"/etc/nagios/nrpe_local.cfg":
+			require => Package[nagios-nrpe-server],
+			owner => root,
+			group => root,
+			mode => 0444,
+			source => "puppet:///files/nagios/nrpe_local.cfg";
 		"/usr/lib/nagios/plugins/check_dpkg":
 			owner => root,
 			group => root,
-			mode => 0755,
+			mode => 0555,
 			source => "puppet:///files/nagios/check_dpkg";
-        }
+	}
 }
 
 class nrpe::service {
-        service { nagios-nrpe-server:
-                require => [ Package[nagios-nrpe-server], File["/etc/nagios/nrpe_local.cfg"], File["/usr/lib/nagios/plugins/check_dpkg"] ],
-                subscribe => File["/etc/nagios/nrpe_local.cfg"],
+	service { nagios-nrpe-server:
+		require => [ Package[nagios-nrpe-server], File["/etc/nagios/nrpe_local.cfg"], File["/usr/lib/nagios/plugins/check_dpkg"] ],
+		subscribe => File["/etc/nagios/nrpe_local.cfg"],
 		pattern => "/usr/sbin/nrpe",
-                ensure => running;
-        }
+		ensure => running;
+	}
 
 	if $lsbdistid == "Ubuntu" and versioncmp($lsbdistrelease, "10.04") >= 0 {
 		file { "/etc/sudoers.d/nrpe":
