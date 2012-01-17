e069958b8e824098b36257fee192d75f63a73556
Dzahn <dzahn@wikimedia.org>
2012-01-17 04:10:34 -0800

    try out planet-venus package in labs
    Change-Id: I4cfd1eeadfbdd64d3296355f4c084128d937b46a

---
+++ manifests/misc/planet.pp
@@ -0,0 +1,33 @@
+# http://planet.wikimedia.org/
+class misc::planet {
+	#The host of this role must have the star certificate installed on it
+	system_role { "misc::planet": description => "Planet weblog aggregator" }
+
+	systemuser { planet: name => "planet", home => "/var/lib/planet", groups => [ "planet" ] }
+
+	class {'generic::webserver::php5': ssl => 'true'; }
+
+	include generic::locales::international
+
+	file {
+		"/etc/apache2/sites-available/planet.wikimedia.org":
+			path => "/etc/apache2/sites-available/planet.wikimedia.org",
+			mode => 0444,
+			owner => root,
+			group => root,
+			source => "puppet:///files/apache/sites/planet.wikimedia.org";
+	}
+
+	apache_site { planet: name => "planet.wikimedia.org" }
+
+	package { "python2.6":
+		ensure => latest;
+	}
+}
+
+# http://intertwingly.net/code/venus/
+class misc::planet-venus {
+	package { "planet-venus":
+		ensure => latest;
+	}
+}
