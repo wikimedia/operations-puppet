f2911670bb0feeb9997335e3bc102b0217d28790
lcarr <lcarr@wikimedia.org>
2012-01-17 16:40:00 -0800

    tryuing to see where ganglia chokes
    Change-Id: I8dee6180904b480374a671518edaf44045889111

--- manifests/ganglia.pp
+++ manifests/ganglia.pp
@@ -15,7 +15,7 @@ class ganglia {
 		$deaf = "no"
 	} else {
 		if ! $cluster {
-			$cluster = [ "misc", "appserver", "imagescaler" ]
+			$cluster = [ "appserver", "imagescaler" ]
 		}
 		if $ganglia_aggregator {
 			$deaf = "no"
