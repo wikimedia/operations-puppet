7f134ec0cafd83f7cb2c8ee7350f64bc3769b4fb
lcarr <lcarr@wikimedia.org>
2012-01-21 15:49:00 -0800

    adding firewall testing
    Change-Id: I617bdd09d030e8307bae8889c30451fe0c1d0953

--- manifests/misc/firewall.pp
+++ manifests/misc/firewall.pp
@@ -1,2 +1,37 @@
-class misc::firewall-server
-	{ File <<|tag == 'firewall'|>> }
+class firewall::builder {
+  file { 
+    "/usr/local/fwbuilder.d":
+        owner => root,
+	group => root,
+	mode => 0755,
+	ensure => directory;
+  }
+
+  # collect all fw definitions
+  File <<| tag == 'inboundacl' |>>
+
+  # TODO: add script here that does the work.
+      
+}
+
+class firewall { 
+  # for each inbound ACL create an exported file on the main server 
+  define inboundacl ($title=$title, $ip_address=$ipaddress, port=$port) {
+    @@file { 
+       "/usr/local/fwbuilder.d/$title-$port":
+          content => "$title,$ipaddress,$port\n", 
+          tag => "inboundacl";
+    }
+  }
+}
+
+
+class testcase1 {
+	include firewall
+	firewall::inboundacl {
+	   "testbox":
+	      ip_address=>"1.2.3.4",
+  	      port => 80;
+        }
+	    
+}
