17c7ff03f340393257104f765ca8a49b752ad756
lcarr <lcarr@wikimedia.org>
2012-01-22 11:43:57 -0800

    changing file resource to a definition and making hostname automatically configured from facter info
    Change-Id: I458f98eef1d215e1aad8554e5d365ac3f40e5ef7

--- manifests/misc/firewall.pp
+++ manifests/misc/firewall.pp
@@ -1,39 +1,39 @@
 class firewall::builder {
-  file { 
-    "/usr/local/fwbuilder.d":
-        owner => root,
-	group => root,
-	mode => 0755,
-	ensure => directory;
-  }
+	file { 
+		"/usr/local/fwbuilder.d":
+		owner => root,
+		group => root,
+		mode => 0755,
+		ensure => directory;
+	}
 
-  # collect all fw definitions
-  File <<| tag == 'inboundacl' |>>
+	# collect all fw definitions
+	File <<| tag == 'inboundacl' |>>
 
-  # TODO: add script here that does the work.
-      
+	# TODO: add script here that does the work.
+
 }
 
 class firewall { 
-  # for each inbound ACL create an exported file on the main server 
-  define inboundacl ($ip_address=$ipaddress, port=$port) {
-    @@file { 
-       "/usr/local/fwbuilder.d/${title}-${port}":
-          content => "$title,$ipaddress,$port\n", 
-          tag => "inboundacl";
-    }
-  }
+	# for each inbound ACL create an exported file on the main server
+	define inboundacl ($ip_address=$ipaddress, port=$port) {
+		@@definition {
+			file {
+				"/usr/local/fwbuilder.d/${hostname}-${port}":
+					content => "$hostname,$ipaddress,$protocol,$port\n",
+					tag => "inboundacl";
+			}
+		}
+	}
 }
-
 
 class testcase1 {
 	include firewall
 	firewall::inboundacl {
 	   "testbox":
-	      ip_address=>"1.2.3.4",
-  	      port => 80;
-        }
-	    
+			ip_address=>"1.2.3.4",
+			port => 80;
+	}
 }
 
 class testcase2 {
