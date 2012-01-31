4a79bfec13bfbdc7f48e2282c21fbd5c48f29dd0
sara <ssmollett@wikimedia.org>
2012-01-31 11:41:30 -0800

    Third iteration of adding ganglia for labs.
    Change-Id: I4e53bc889ece576c3b86434484203efe074e7cb4

---
+++ files/puppet/projectgid.rb
@@ -0,0 +1,13 @@
+# projectgid.rb
+# 
+# This fact provides project_gid (gidNumber) for projects in labs.
+# This is used to set the udp_send_channel port in ganglia's gmond.conf.
+
+require 'facter'
+
+Facter.add(:project_gid) do
+  setcode do
+    project_name = Facter::Util::Resolution.exec("egrep -A1 '^cluster {' /etc/ganglia/gmond.conf | awk -F '\"' '/name =/ {print $2}'").chomp
+    Facter::Util::Resolution.exec("getent group #{project_name} | cut -d : -f 3").chomp
+  end
+end
--- manifests/base.pp
+++ manifests/base.pp
@@ -134,6 +134,11 @@ class base::puppet($server="puppet") {
 			group => root,
 			mode => 0755,
 			source => "puppet:///files/puppet/default_gateway.rb";
+		"/var/lib/puppet/lib/facter/projectgid.rb":
+			owner => root,
+			group => root,
+			mode => 0755,
+			source => "puppet:///files/puppet/projectgid.rb";
 	}
 
 	# Compile /etc/puppet/puppet.conf from individual files in /etc/puppet/puppet.conf.d
