4fbb251e2cdd278657431cac939745f17973756b
Pyoungmeister <py@wikimedia.org>
2012-02-02 17:41:39 -0500

    attempting to force absolute lookup of class per mr. feldman's suggestion
    Change-Id: Id1354351b20ce4942975bee486db99c489e6b237

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -21,7 +21,7 @@ class role::cache {
 			include lvs::configuration
 
 			include	standard
-			include	squid
+			include	::squid
 			
 			class { "lvs::realserver": realserver_ips => $lvs::configuration::lvs_service_ips[$::realm][$role][$::site] }
 
