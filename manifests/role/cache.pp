da124b9f23001b681efc2db6d7b6d3a0edcff090
Mark Bergsma <mark@wikimedia.org>
2012-02-02 14:27:41 +0100

    Refactor, remove duplication
    Change-Id: I81cfdf7cab05e1146d09bf7e8d394492ab0ef94d

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -1,52 +1,37 @@
 class role::cache {
 	class squid {
-		class text {
-			system_role { "role::cache::squid::text": description => "text Squid server" }
+		class common($role) {
+			system_role { "role::cache::squid::${role}": description => "${role} Squid server"}
 
-			$cluster = "squids_text"
+			$cluster = "squids_${role}"
 
 			include lvs::configuration
 
 			# FIXME: make coherent with $cluster
 			$nagios_group = $site ? {
-				'pmtpa' => 'squids_text',
-				'esams' => 'squids_esams_text',
-				'eqiad' => 'squids_eqiad_text'
+				'pmtpa' => 'squids_${role}',
+				'esams' => 'squids_esams_${role}',
+				'eqiad' => 'squids_eqiad_${role}'
 			}
 
 			include	standard,
 				squid
 			
-			class { "lvs::realserver": realserver_ips => $lvs::configuration::lvs_service_ips[$::realm][text][$::site] }
+			class { "lvs::realserver": realserver_ips => $lvs::configuration::lvs_service_ips[$::realm][$role][$::site] }
 
 			# HTCP packet loss monitoring on the ganglia aggregators
 			if $ganglia_aggregator == "true" and $site != "esams" {
 				include misc::monitoring::htcp-loss
 			}
+
+		}
+
+		class text {
+			class { "role::cache::squid::common": role => "text" }
 		}
 
 		class upload {
-			system_role { "role::cache::squid::upload": description => "upload Squid server" }
-
-			$cluster = "squids_upload"
-
-			include lvs::configuration
-
-			# FIXME: make coherent with $cluster
-			$nagios_group = $site ? {
-				'pmtpa' => 'squids_upload',
-				'esams' => 'squids_esams_upload'
-			}
-
-			include standard,
-				squid
-
-			class { "lvs::realserver": realserver_ips => $lvs::configuration::lvs_service_ips[$::realm][upload][$::site] }
-
-			# HTCP packet loss monitoring on the ganglia aggregators
-			if $ganglia_aggregator == "true" and $site != "esams" {
-				include misc::monitoring::htcp-loss
-			}
+			class { "role::cache::squid::common": role => "upload" }
 		}
 	}
 }
