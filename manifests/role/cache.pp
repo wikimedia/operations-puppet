1b71770cbdcea239da45322a639c92f363d4d9c0
Mark Bergsma <mark@wikimedia.org>
2012-02-02 12:16:57 +0100

    Add new role::cache::squid::upload class
    Change-Id: I30ad9b178764ae88670e82eef167bdc98cc8f765

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -1,40 +1,74 @@
-class role::cache::squid::text {
-	system_role { "role::cache::squid::text": description => "text Squid server" }
+class role::cache {
+	class squid {
+		class text {
+			system_role { "role::cache::squid::text": description => "text Squid server" }
 
-	$cluster = "squids_text"
+			$cluster = "squids_text"
 
-	if ! $lvs_realserver_ips {
-		include lvs::configuration
+			if ! $lvs_realserver_ips {
+				include lvs::configuration
 		
-		$sip = $lvs::configuration::lvs_service_ips[$::realm][text][$::site]
-		
-		$lvs_realserver_ips = [
-			$sip['wikimedialb'],
-			$sip['wikipedialb'],
-			$sip['wiktionarylb'],
-			$sip['wikiquotelb'],
-			$sip['wikibookslb'],
-			$sip['wikisourcelb'],
-			$sip['wikinewslb'],
-			$sip['wikiversitylb'],
-			$sip['mediawikilb'],
-			$sip['foundationlb']
-		]
-	}
+				$sip = $lvs::configuration::lvs_service_ips[$::realm][text][$::site]
 
-	# FIXME: make coherent with $cluster
-	$nagios_group = $site ? {
-		'pmtpa' => 'squids_text',
-		'esams' => 'squids_esams_text',
-		'eqiad' => 'squids_eqiad_text'
-	}
+				$lvs_realserver_ips = [
+					$sip['textsvc'],
+					$sip['wikimedialb'],
+					$sip['wikipedialb'],
+					$sip['wiktionarylb'],
+					$sip['wikiquotelb'],
+					$sip['wikibookslb'],
+					$sip['wikisourcelb'],
+					$sip['wikinewslb'],
+					$sip['wikiversitylb'],
+					$sip['mediawikilb'],
+					$sip['foundationlb']
+				]
+			}
 
-	include	standard,
-		squid,
-		lvs::realserver
+			# FIXME: make coherent with $cluster
+			$nagios_group = $site ? {
+				'pmtpa' => 'squids_text',
+				'esams' => 'squids_esams_text',
+				'eqiad' => 'squids_eqiad_text'
+			}
 
-	# HTCP packet loss monitoring on the ganglia aggregators
-	if $ganglia_aggregator == "true" and $site != "esams" {
-		include misc::monitoring::htcp-loss
+			include	standard,
+				squid,
+				lvs::realserver
+
+			# HTCP packet loss monitoring on the ganglia aggregators
+			if $ganglia_aggregator == "true" and $site != "esams" {
+				include misc::monitoring::htcp-loss
+			}
+		}
+
+		class upload {
+			system_role { "role::cache::squid::upload": description => "upload Squid server" }
+
+			$cluster = "squids_upload"
+
+			if ! $lvs_realserver_ips {
+				include lvs::configuration
+
+				$sip = $lvs::configuration::lvs_service_ips[$::realm][upload][$::site]
+				
+				$lvs_realserver_ips = [ $sip['uploadsvc'], $sip['uploadlb'] ]
+			}
+
+			# FIXME: make coherent with $cluster
+			$nagios_group = $site ? {
+				'pmtpa' => 'squids_upload',
+				'esams' => 'squids_esams_upload'
+			}
+
+			include standard,
+				squid,
+				lvs::realserver
+
+			# HTCP packet loss monitoring on the ganglia aggregators
+			if $ganglia_aggregator == "true" and $site != "esams" {
+				include misc::monitoring::htcp-loss
+			}
+		}
 	}
 }
