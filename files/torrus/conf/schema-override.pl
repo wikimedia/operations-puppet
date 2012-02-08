8891709deccf4e3d87537294e3d9a051bf2bc7ab
Mark Bergsma <mark@wikimedia.org>
2012-02-08 13:43:41 +0100

    Add eqiad hosts to aggregates (incomplete)
    Change-Id: Ic4865031a912ef5b401454e4a0c96557c333ff9b

--- files/torrus/conf/schema-override.pl
+++ files/torrus/conf/schema-override.pl
@@ -7,6 +7,11 @@ $Torrus::Renderer::graphStyles{'text-esams'}{'color'} = '#33cc33';
 $Torrus::Renderer::graphStyles{'upload-esams'}{'color'} = '#1d941d'; # '#1b8a1b';
 $Torrus::Renderer::graphStyles{'bits-esams'}{'color'} = '#24ba24';
 
+# FIXME
+$Torrus::Renderer::graphStyles{'text-eqiad'}{'color'} = '#5555cc';
+$Torrus::Renderer::graphStyles{'upload-eqiad'}{'color'} = '#33337a';
+$Torrus::Renderer::graphStyles{'bits-eqiad'}{'color'} = '#4545a6';
+
 $Torrus::Renderer::graphStyles{'text-pmtpa'}{'color'} = '#5555cc';
 $Torrus::Renderer::graphStyles{'upload-pmtpa'}{'color'} = '#33337a';
 $Torrus::Renderer::graphStyles{'bits-pmtpa'}{'color'} = '#4545a6';
