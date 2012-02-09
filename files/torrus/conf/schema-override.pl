fc683806475b703034e8f6d62282cb598562ab2c
Mark Bergsma <mark@wikimedia.org>
2012-02-09 15:55:31 +0100

    Change eqiad colors for clarity
    Change-Id: I480d452fb5ff616fc8b2abb11617638bceac0373

--- files/torrus/conf/schema-override.pl
+++ files/torrus/conf/schema-override.pl
@@ -7,10 +7,9 @@ $Torrus::Renderer::graphStyles{'text-esams'}{'color'} = '#33cc33';
 $Torrus::Renderer::graphStyles{'upload-esams'}{'color'} = '#1d941d'; # '#1b8a1b';
 $Torrus::Renderer::graphStyles{'bits-esams'}{'color'} = '#24ba24';
 
-# FIXME
-$Torrus::Renderer::graphStyles{'text-eqiad'}{'color'} = '#5555cc';
-$Torrus::Renderer::graphStyles{'upload-eqiad'}{'color'} = '#33337a';
-$Torrus::Renderer::graphStyles{'bits-eqiad'}{'color'} = '#4545a6';
+$Torrus::Renderer::graphStyles{'text-eqiad'}{'color'} = '#b3003c';
+$Torrus::Renderer::graphStyles{'upload-eqiad'}{'color'} = '#990033';
+$Torrus::Renderer::graphStyles{'bits-eqiad'}{'color'} = '#cc0044';
 
 $Torrus::Renderer::graphStyles{'text-pmtpa'}{'color'} = '#5555cc';
 $Torrus::Renderer::graphStyles{'upload-pmtpa'}{'color'} = '#33337a';
