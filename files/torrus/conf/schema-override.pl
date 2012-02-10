40f33aec4a66c6fc00cdf28359259ca08828a727
Mark Bergsma <mark@wikimedia.org>
2012-02-10 15:47:30 +0100

    Add mobile graphs, corrections
    Change-Id: I8c355b72f9186b7474108b3e5f5246fa14b804ca

--- files/torrus/conf/schema-override.pl
+++ files/torrus/conf/schema-override.pl
@@ -10,8 +10,12 @@ $Torrus::Renderer::graphStyles{'bits-esams'}{'color'} = '#24ba24';
 $Torrus::Renderer::graphStyles{'text-eqiad'}{'color'} = '#b3003c';
 $Torrus::Renderer::graphStyles{'upload-eqiad'}{'color'} = '#990033';
 $Torrus::Renderer::graphStyles{'bits-eqiad'}{'color'} = '#cc0044';
+$Torrus::Renderer::graphStyles{'mobile-eqiad'}{'color'} = 'e6004d';
+$Torrus::Renderer::graphStyles{'api-eqiad'}{'color'} = '##yellow';
 
 $Torrus::Renderer::graphStyles{'text-pmtpa'}{'color'} = '#5555cc';
 $Torrus::Renderer::graphStyles{'upload-pmtpa'}{'color'} = '#33337a';
 $Torrus::Renderer::graphStyles{'bits-pmtpa'}{'color'} = '#4545a6';
+$Torrus::Renderer::graphStyles{'mobile-pmtpa'}{'color'} = '#4545A6';
+$Torrus::Renderer::graphStyles{'api-eqiad'}{'color'} = '##orange';
 
