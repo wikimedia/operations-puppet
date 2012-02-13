135172a70510e86dde2c7715c3be06b482e20237
Pyoungmeister <py@wikimedia.org>
2012-02-13 13:25:47 -0500

    making new lucene classes and configs
    Change-Id: Ida8399947e962e039a31ff138c6328b3058e8d41

--- manifests/search.pp
+++ manifests/search.pp
@@ -88,3 +88,149 @@ class search::indexer {
 		require => [ File["/etc/default/rsync"], File["/etc/rsyncd.conf"] ]
 	}
 }
+
+# new lucene class for better naming convention and migration to new 
+# puppet-based lucene setup
+
+class lucene {
+
+	class server($indexer="false", $udplogging="true") {
+		Class["lucene::config"] -> Class[lucene::server]
+		Class["lucene::packages"] -> Class[lucene::server]
+
+		include lucene::packages,
+			lucene::config,
+			lucene::service
+
+		if $indexer == "true" {
+			include lucene::indexer
+		}
+	}
+
+	class packages {
+		package { ["sun-j2sdk1.6", "lucene-search-2"]:
+			ensure => latest;
+		}
+# need to figure out what the indexer in particular needs.
+#		if $indexer == "true" {
+#			package { :
+#				ensure => latest;
+#			}
+#		}
+	}
+
+	class config {
+		file {
+			"/etc/lsearch.conf":
+				owner => root,
+				group => root,
+				mode => 0444,
+				content => template("lucene/lsearch.conf.new.erb"),
+				ensure => present;
+			"/a/search/conf/lsearch-global-2.1.conf":
+				require => File["/a/search/conf"],
+				owner => root,
+				group => root,
+				mode => 0444,
+				content => template("lucene/lsearch-global-2.1.conf.erb"),
+				ensure => present;
+			[ "/a/search/indexes", "/a/search/log", "/a/search/conf" ]:
+                                ensure => directory,
+                                owner => rainman,
+                                group => search,
+				require => Package[lucene-search-2]
+		}
+	}
+
+	class service {
+		service { lucene-search-2:
+			ensure => running,
+			require => [ File["/etc/lsearch.conf", "/a/search/conf/lsearch-global-2.1.conf", "/a/search/indexes", "/a/search/log"], Package[lucene-search-2] ]
+		}
+
+		monitor_service { "lucene": description => "Lucene", check_command => "check_lucene", retries => 6 }
+	}
+
+	class sudo {
+		file { "/etc/sudoers":
+			owner => root,
+			group => root,
+			mode => 0440,
+			source => "puppet:///files/sudo/sudoers.search",
+			ensure => present;
+		}
+	}
+
+	class indexer {
+
+		include passwords::lucene
+		$lucene_oai_pass = $passwords::lucene::oai_pass
+
+		class { 'generic::rsyncd': config => "searchidx" }
+		
+		file { "/etc/php5/apache2/php.ini":
+			owner => root,
+			group => root,
+			mode => 0440,
+			source => "puppet:///files/php/php.ini.appserver";
+		}
+
+		monitor_service { "lucene_indexer": description => "Lucene indexer", check_command => "check_lucene_indexer", retries => 6 }
+
+		## TO DO: pull these out of rainman's homedir and into something not on nfs
+
+		file { "/a/search/lucene.jobs.sh":
+			owner => rainman,
+			group => search,
+			mode => 0755,
+			source => "puppet:///files/lucene/lucene.jobs.sh";
+		}
+
+		cron {
+			snapshot:
+				require => File["/a/search/lucene.jobs.sh"],
+				command => '/a/search/lucene.jobs.sh snapshot',
+				user => rainman,
+				hour => 4,
+				minute => 30,
+				ensure => present;
+			snapshot-precursors:
+				require => File["/a/search/lucene.jobs.sh"],
+				command => '/a/search/lucene.jobs.sh snapshot-precursors',
+				user => rainman,
+				weekday => 5,
+				hour => 9,
+				minute => 30,
+				ensure => present;	
+			indexer-cron:
+				require => File["/a/search/lucene.jobs.sh"],
+				command => '/a/search/lucene.jobs.sh indexer-cron',
+				user => rainman,
+				weekday => 6,
+				hour => 0,
+				minute => 0,
+				ensure => present;
+			import-private-cron:
+				require => File["/a/search/lucene.jobs.sh"],
+				command => '/a/search/lucene.jobs.sh import-private-cron',
+				user => rainman,
+				hour => 2,
+				minute => 0,
+				ensure => present;
+			import-broken-cron:
+				require => File["/a/search/lucene.jobs.sh"],
+				command => '/a/search/lucene.jobs.sh import-broken-cron',
+				user => rainman,
+				hour => 3,
+				minute => 0,
+				ensure => present;
+			build-prefix:
+				require => File["/a/search/lucene.jobs.sh"],
+				command => '/a/search/lucene.jobs.sh build-prefix',
+				user => rainman,
+				hour => 9,
+				minute => 25,
+				ensure => present;
+		}
+	}
+}
