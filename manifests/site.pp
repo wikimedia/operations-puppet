d7849600786abc18fd235fb3d59f50f9a92d9998
lcarr <lcarr@wikimedia.org>
2012-02-22 09:46:00 -0800

    removing defunct ganglia1001
    Change-Id: I982cd7faabe87ccc3abfb5f56b9fb7cf26742c9e

--- manifests/site.pp
+++ manifests/site.pp
@@ -753,11 +753,6 @@ node "gallium.wikimedia.org" {
 	install_certificate{ "star.mediawiki.org": }
 }
 
-node "ganglia1001.eqiad.wmnet" {
-	include standard,
-		ganglia::aggregator
-}
-
 node "gilman.wikimedia.org" {
 
 	$exim_signs_dkim = "false"
