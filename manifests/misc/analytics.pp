c29e00864644d31664df2e25032916b7bf4a180f
ottomata <otto@wikimedia.org>
2012-02-23 18:42:08 -0800

    Amending previos commit: installing python-mysqldb from deb.  Not requiring mysql client class, this should be installed by the .dev if needed manifests/misc/analytics.pp - some comments
    Change-Id: I0b949c40399080d2b5d12e9d3266a2eb858e29b2

--- manifests/misc/analytics.pp
+++ manifests/misc/analytics.pp
@@ -1,22 +1,20 @@
 # Class: misc::analytics::packages
 #
 # Installs needed packages for analytics tools.
+# Currently these are needed by the pipeline data 
+# transformation tool.  This is a work in progress.
 class misc::analytics::packages
 {
-  include generic::mysql::client
-  
-  package { 'MySQL-python':
-    provider => 'pip',
+  package { 'python-mysqldb':
     ensure   => 'installed',
-    requires => Class['generic::mysql::client'],
   }
-  
+
   package { 'pywurfl':
     provider => 'pip',
     ensure => 'installed',
   }
-  
-  package { 'python-geoip': 
+
+  package { 'python-geoip':
     ensure => 'installed',
   }
 }
