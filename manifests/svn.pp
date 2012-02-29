4afd6e7b2e297779ae4f974d607d803b3d91e1d0
Antoine Musso <hashar@free.fr>
2012-02-29 15:49:59 +0100

    Bug 28469 - Make SVN Documentation be indexed
    Puppetize svn.wikimedia.org robots.txt file which is:
    https://svn.wikimedia.org/robots.txt
    
    Patchset2: explicitly allow /docs/
    Allow: seems to be accepted since bing and google have them in their.
    
    Patchset3: use a recursive definition for the docroot
    
    Change-Id: I9269fbbb7b7958f965a6e24a2f6fdc8496a8bb0d

--- manifests/svn.pp
+++ manifests/svn.pp
@@ -28,6 +28,13 @@ class svn::server {
 			mode => 0444,
 			source => "puppet:///files/apache/sites/svn.wikimedia.org",
 			notify => Service[apache2];
+		"/srv/org/wikimedia/svn":
+			ensure => directory,
+			source => "puppet:///files/svn/docroot",
+			owner => root,
+			group => root,
+			mode  => 0444,
+			recurse => true;
 		"/var/mwdocs":
 			owner => mwdocs,
 			group => svn,
