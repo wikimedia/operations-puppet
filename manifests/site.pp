4c3bea1a49f5106c4f8c9fc76ced10d5b6d58c55
Ryan Lane <rlane@wikimedia.org>
2012-03-06 15:50:13 -0800

    Test
    Change-Id: Id1b62ee7995789d6a84c4f90648429c630ad2f48

--- manifests/site.pp
+++ manifests/site.pp
@@ -725,10 +725,6 @@ node "formey.wikimedia.org" {
 	$cluster = "misc"
 	$gid = 550
 	$ldapincludes = ['openldap', 'nss', 'utils']
-	$ssh_tcp_forwarding = "no"
-	$ssh_x11_forwarding = "no"
-	$gerrit_slave = "true"
-	$gerrit_no_apache = "true"
 	include standard,
 		svn::server,
 		ldap::client::wmf-cluster,
