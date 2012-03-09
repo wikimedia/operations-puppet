9dd7f00e69af30f2d1ba7358e2ca53b61c41bce9
Ryan Lane <rlane@wikimedia.org>
2012-03-09 11:45:09 -0800

    Ensure glustermanager can run mkdir -p, not just mkdir
    Change-Id: I6d321074da56633cb37c2c7806896bb4c8264dda

--- manifests/openstack.pp
+++ manifests/openstack.pp
@@ -278,7 +278,7 @@ class openstack::project-storage {
 
 	include openstack::gluster-service
 
-	$sudo_privs = [ 'ALL = NOPASSWD: /bin/mkdir /a/*',
+	$sudo_privs = [ 'ALL = NOPASSWD: /bin/mkdir -p /a/*',
 			'ALL = NOPASSWD: /usr/sbin/gluster *' ]
 	sudo_user { [ "glustermanager" ]: privileges => $sudo_privs, require => Systemuser["glustermanager"] }
 
