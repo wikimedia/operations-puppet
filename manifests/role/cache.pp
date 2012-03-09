8edcca6453c053afa0deeac75ce43031a907fd29
Mark Bergsma <mark@wikimedia.org>
2012-03-09 12:46:56 +0100

    Syntax, missing include
    Change-Id: I3b0c979f6536aa81b5291c39439ee34c82fadfe2

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -337,7 +337,7 @@ class role::cache {
 			$cluster = "cache_upload"
 			$nagios_group = "cache_upload_${::site}"
 
-			include lvs::configuration, role::cache::configuration
+			include lvs::configuration, role::cache::configuration, network::constants
 			
 			class { "lvs::realserver": realserver_ips => $lvs::configuration::lvs_service_ips[$::realm]['upload'][$::site] }
 
