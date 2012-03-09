dd062293f558522b172a8c42b38fc67601235be4
dzahn <dzahn@wikimedia.org>
2012-03-09 16:57:11 +0930

    nagios monitoring for mw profiling collector and graphite (RT-2367)
    Change-Id: Ife4c3175f373071e7e1f7fbd90722ca2302db4c6

--- manifests/misc-servers.pp
+++ manifests/misc-servers.pp
@@ -809,7 +809,11 @@ class misc::udpprofile::collector {
 		ensure => running;
 	}
 
-	# FIXME: Nagios monitoring
+	# Nagios monitoring (RT-2367)
+	monitor_service { "carbon-cache": description => "carbon-cache.py", check_command => "nrpe_check_carbon_cache" }
+	monitor_service { "profiler-to-carbon": description => "profiler-to-carbon", check_command => "nrpe_check_profiler_to_carbon" }
+	monitor_service { "profiling collector": description => "profiling collector", check_command => "nrpe_check_profiling_collector" }
+
 }
 
 class misc::graphite {
