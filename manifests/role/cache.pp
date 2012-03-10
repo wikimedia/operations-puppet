c9c224230e2e04a7d16c6408648e43d21706abaa
Mark Bergsma <mark@wikimedia.org>
2012-03-10 17:07:55 +0100

    Let's not have Varnish writing to the same file concurrently, shall we
    Change-Id: I6cf219cc6c22f66ae50c96dd57cfdfac080a3566

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -365,7 +365,7 @@ class role::cache {
 				vcl => "upload-backend",
 				port => 3128,
 				admin_port => 6083,
-				storage => "-s sda3=file,/srv/sdb3/varnish.persist,50% -s sdb3=file,/srv/sdb3/varnish.persist,50%",
+				storage => "-s sda3=file,/srv/sda3/varnish.persist,50% -s sdb3=file,/srv/sdb3/varnish.persist,50%",
 				backends => [ "10.2.1.24", "10.2.1.27" ],
 				directors => { "backend" => [ "10.2.1.24" ], "swift_thumbs" => [ "10.2.1.27" ] },
 				backend_options => {
