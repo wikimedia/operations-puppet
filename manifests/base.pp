0ed70ec0d0de9ba4508be19700b39b23f03ed631
lcarr <lcarr@wikimedia.org>
2012-03-12 09:54:31 -0700

    Fixing fixme's from previous change https://gerrit.wikimedia.org/r/#change,2936
    Change-Id: I9ec4c04f23ac77867b863632c86c99c8a3b07ede

--- manifests/base.pp
+++ manifests/base.pp
@@ -87,8 +87,7 @@ class base::puppet($server="puppet") {
 		ensure => latest;
 	}
 
-	# FIXME: remove $hostname from the title, it's already being prepended. Then, purge the existing Nagios resources.
-	monitor_service { "$hostname puppet freshness": description => "Puppet freshness", check_command => "puppet-FAIL", passive => "true", freshness => 36000, retries => 1 ; }
+	monitor_service { "puppet freshness": description => "Puppet freshness", check_command => "puppet-FAIL", passive => "true", freshness => 36000, retries => 1 ; }
 	
 	exec { "spence puppet snmp trap":
 		command => "snmptrap -v 1 -c public nagios.wikimedia.org .1.3.6.1.4.1.33298 `hostname` 6 1004 `uptime | awk '{ split(\$3,a,\":\"); print (a[1]*60+a[2])*60 }'`",
@@ -97,9 +96,9 @@ class base::puppet($server="puppet") {
 	}
 
 	exec {	"neon puppet snmp trap":
-		command => "snmptrap -v 1 -c public neon.wikimedia.org .1.3.6.1.4.1.33298 `hostname` 6 1004 `uptime | awk '{ split(\$3,a,\":\"); print (a[1]*60+a[2])*60 }'`",
-		path => "/bin:/usr/bin",
-		require => Package["snmp"]
+			command => "snmptrap -v 1 -c public neon.wikimedia.org .1.3.6.1.4.1.33298 `hostname` 6 1004 `uptime | awk '{ split(\$3,a,\":\"); print (a[1]*60+a[2])*60 }'`",
+			path => "/bin:/usr/bin",
+			require => Package["snmp"]
 	}
 
 	file {
