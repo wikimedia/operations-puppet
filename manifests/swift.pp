4f745f2abdb82e0d01004f0735d0ad6a664448fc
dzahn <dzahn@wikimedia.org>
2012-03-12 14:52:34 +0930

    make check_http_swift flexible for different ports and add an if-statement on hostname
    Change-Id: I619e90a4320dd7e9e163da8da944553dad807208

--- manifests/swift.pp
+++ manifests/swift.pp
@@ -141,7 +141,12 @@ class swift::proxy {
 }
 
 class swift::proxy::monitoring {
-	monitor_service { "swift http": description => "Swift HTTP", check_command => "check_http_swift" }
+
+	if $hostname =~ /^ms-fe[12]$/ {
+		monitor_service { "swift http": description => "Swift HTTP", check_command => "check_http_swift!80" }
+	} else {
+		monitor_service { "swift http": description => "Swift HTTP", check_command => "check_http_swift!8080" }
+	}
 }
 
 # TODO: document parameters
