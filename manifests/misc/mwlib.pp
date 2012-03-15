cb40f66ad25f37f8146aedaefc3df3906921e4d7
jgreen <jgreen@wikimedia.org>
2012-03-15 21:00:07 +0000

    added misc::mwlib::users and misc::mwlib::groups
    Change-Id: I11505c01d8c265e17db6c8d471f5ea60fa20aed3

--- manifests/misc/mwlib.pp
+++ manifests/misc/mwlib.pp
@@ -1,8 +1,8 @@
 # Class: misc::mwlib::packages
 
 # Installs needed packages for pediapress/mwlib.
-class misc::mwlib::packages
-{
+class misc::mwlib::packages {
+
   package { [ "gcc", "g++", "make", "python", "python-dev", "python-virtualenv", "libjpeg-dev", "libz-dev", "libfreetype6-dev", "liblcms-dev", "libxml2-dev", "libxslt-dev", "ocaml-nox", "git-core", "python-imaging", "python-lxml", "texlive-latex-recommended", "ploticus", "dvipng", "imagemagick", "pdftk" ]:
     ensure   => 'installed',
   }
@@ -11,6 +11,36 @@ class misc::mwlib::packages
   #  provider => 'pip',
   #  ensure => 'installed',
   #}
+
+}
+
+
+class misc::mwlib::users {
+
+  user { "pp":
+    name => "pp",
+    groups => [ "pp" ],
+    home => "/opt/pp",
+    shell => "/bin/sh",
+    ensure => "present",
+    membership => "minimum",
+    manages_homedir => false,
+    allowdupe => false,
+    system => true,
+    require => misc::mwlib::groups,
+  }
+
+}
+
+
+class misc::mwlib::groups {
+
+  group { "pp":
+    name => "pp",
+    ensure => "present",
+    allowdupe => false,
+    system => true,
+  }
 
 }
 
