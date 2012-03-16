ffdbf929b2b1cb55c6d3f5b60694ca428210af3f
lcarr <lcarr@wikimedia.org>
2012-03-16 13:47:05 -0700

    Fixing icinga init file
    Change-Id: Ic1a20d60e4dfabd87cb43ddcb40daf73fde9e962

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -932,7 +932,7 @@ class nagios::monitor::newmonitor {
 			command => "/usr/local/sbin/purge-nagios-resources.py /etc/icinga/puppet_hosts.cfg /etc/icinga/puppet_hostgroups.cfg /etc/icinga/puppet_hostextinfo.cfg /etc/icinga/puppet_servicegroups.cfg /etc/icinga/puppet_services.cfg /etc/icinga/puppet_checks.d/*",
 			timeout => 60;
 		"purge_nagios_decommissioned":
-			command => "/usr/local/sbin/purge-nagios-resources.py /etc/nagios/puppet_hosts.cfg /etc/nagios/puppet_hostgroups.cfg /etc/nagios/puppet_hostextinfo.cfg /etc/nagios/puppet_servicegroups.cfg  /etc/nagios/puppet_checks.d/*",
+			command => "/usr/local/sbin/purge-nagios-resources.py /etc/nagios/puppet_hosts.cfg /etc/nagios/puppet_hostgroups.cfg /etc/nagios/puppet_hostextinfo.cfg /etc/nagios/puppet_servicegroups.cfg /etc/nagios/puppet_checks.d/*",
 			timeout => 60;
 	}
 
