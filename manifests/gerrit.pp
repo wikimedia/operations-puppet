dce3edd731e816252b1be8c37d988ebd3479de8f
Ryan Lane <rlane@wikimedia.org>
2012-03-16 10:31:56 -0700

    Fixing labs/production split in gerrit reporting
    Change-Id: I3df0547048eadf3e3aeed1a3f14974b9293b863c

--- manifests/gerrit.pp
+++ manifests/gerrit.pp
@@ -215,7 +215,7 @@ class gerrit::ircbot {
 
 	include gerrit::gerrit_config
 
-	$ircecho_infile = "/var/lib/gerrit2/review_site/logs/operations.log:#wikimedia-operations,#wikimedia-labs;/var/lib/gerrit2/review_site/logs/mobile.log:#wikimedia-mobile;/var/lib/gerrit2/review_site/logs/mediawiki.log:#mediawiki"
+	$ircecho_infile = "/var/lib/gerrit2/review_site/logs/operations.log:#wikimedia-operations;/var/lib/gerrit2/review_site/logs/labs.log:#wikimedia-labs;/var/lib/gerrit2/review_site/logs/mobile.log:#wikimedia-mobile;/var/lib/gerrit2/review_site/logs/mediawiki.log:#mediawiki"
 	$ircecho_nick = "gerrit-wm"
 	$ircecho_chans = "#wikimedia-operations,#wikimedia-labs,#wikimedia-mobile,#mediawiki"
 	$ircecho_server = "irc.freenode.net"
