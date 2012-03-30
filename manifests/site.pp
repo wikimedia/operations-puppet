929e889a3e7e5224056a57c83459b4dda3a2a95f
lcarr <lcarr@wikimedia.org>
2012-03-30 11:39:40 -0700

    removing nagios_config_dir from neon
    Change-Id: I74fa338a14bcf4b1ae69a39ed4826cc024d3920c

--- manifests/site.pp
+++ manifests/site.pp
@@ -1444,7 +1444,6 @@ node /^ms-be[1-5]\.pmtpa\.wmnet$/ {
 }
 
 node "neon.wikimedia.org" {
-	$nagios_config_dir = "/etc/icinga"
 	$domain_search = "wikimedia.org pmtpa.wmnet eqiad.wmnet esams.wikimedia.org"
 	include standard,
 		nagios::monitor::newmonitor
