7ed0d554a6679067dc27a8f7524f438b15a817d4
Lcarr <lcarr@wikimedia.org>
2012-04-02 19:59:26 +0000

    this broke something Revert "Deploy Wikipedia Zero and Teahouse filters, upgrade package to 0.2.0 Uninstall old package Change-Id: Ie4951a567b5bb9d7b5c06c3dc17f217f1caa59ba"
    This reverts commit aa3c5f8b02b54256a43fc6c53c55dd2871d9092a

--- manifests/site.pp
+++ manifests/site.pp
@@ -467,7 +467,7 @@ node "emery.wikimedia.org" {
 		generic::sysctl::high-bandwidth-rsync,
 		misc::udp2log::monitoring,
 		misc::udp2log::emeryconfig,
-		misc::udp2log::udp-filter
+		misc::udp2log::udp-filters
 }
 
 node "erzurumi.pmtpa.wmnet" {
