434f2cc3a94ed0f50c7ceca6de95d9d705ba0886
jgreen <jgreen@wikimedia.org>
2012-04-03 16:06:34 +0000

    added search qa mode where we can test through LVS rather than direct to lucene hosts
    Change-Id: I1c76054dbfe86505201d75b6c24577ec6aef8da9

--- files/searchqa/lib/searchqa.pm
+++ files/searchqa/lib/searchqa.pm
@@ -1,7 +1,14 @@
+<pre>
 package searchqa;
 use strict;
 use Net::DNS::Resolver;
 use URI::Escape;
+
+# The $conf variables and routines determine_api_host() and
+# determine_database() combine for a coarse approximation of how lvs pools,
+# apache config, and mediawiki code combine to route search subrequests. It
+# would be nice if we could just recycle existing code to do this, but I don't
+# see how that is feasible.  So we hack and replicate as best we can.
 
 our $servers; # hostname to ip address cache for api servers
 
@@ -15,6 +22,7 @@ our $conf = {
 	'max_queries_per_type' => 25, # don't rerun *everything*, just the first 25 queries
 	'perl_threads' => 50, # 50 threads is a fairly mellow number
 	'datacenter' => 'eqiad,pmtpa', # process both datacenters by default
+	'pool_mode' => 'lvs', # (lvs|host) query lvs IP or individual host IPs?
 	'api_hosts' => [ qw(
 		search1.pmtpa.wmnet search2.pmtpa.wmnet search3.pmtpa.wmnet search4.pmtpa.wmnet search5.pmtpa.wmnet
 		search6.pmtpa.wmnet search7.pmtpa.wmnet search8.pmtpa.wmnet search9.pmtpa.wmnet search10.pmtpa.wmnet
@@ -26,45 +34,82 @@ our $conf = {
 		search1013.eqiad.wmnet search1014.eqiad.wmnet search1015.eqiad.wmnet search1016.eqiad.wmnet
 		search1017.eqiad.wmnet search1018.eqiad.wmnet search1019.eqiad.wmnet search1020.eqiad.wmnet
 	)],
+	'lvs' => {
+		'eqiad' => {
+			'search_pool1' => {
+				'lvs' => [qw(10.2.2.11)],
+				'host' => [qw(search1001.eqiad.wmnet search1002.eqiad.wmnet search1003.eqiad.wmnet]),
+			}
+			'search_pool2' => {
+				'lvs' => [qw(10.2.2.12)],
+				'host' => [qw(search1007.eqiad.wmnet search1008.eqiad.wmnet]),
+			}
+			'search_pool3' => {
+				'lvs' => [qw(10.2.2.13)],
+				'host' => [qw(search1011.eqiad.wmnet search1012.eqiad.wmnet search1013.eqiad.wmnet]),
+			}
+			'search_pool4' => {
+				'lvs' => [qw(10.2.2.14)],
+				'host' => [qw(search1015.eqiad.wmnet search1016.eqiad.wmnet]),
+			}
+			'search_prefix' => {
+				'lvs' => [qw(10.2.2.15)],
+				'host' => [qw(search1017.eqiad.wmnet search1018.eqiad.wmnet]),
+			}
+		}
+		'pmtpa' => {
+			'search_pool1' => {
+				'lvs' => [qw(10.2.1.11)],
+				'host' => [qw(search1.pmtpa.wmnet search3.pmtpa.wmnet search4.pmtpa.wmnet search9.pmtpa.wmnet)],
+			}
+			'search_pool2' => )
+				'lvs' => [qw(10.2.1.12)],
+				'host' => [qw(search6.pmtpa.wmnet search15.pmtpa.wmnet]),
+			}
+			'search_pool3' => {
+				'lvs' => [qw[10.2.1.13]),
+				'host' => [qw(search7.pmtpa.wmnet]),
+			}
+		}
+	}	
 };
 
-# The routines determine_api_host() and determine_database() combine for a
-# coarse approximation of how lvs pools, apache config, and mediawiki code
-# combine to route search subrequests. It would be nice if we could just
-# recycle existing code to do this, but I don't see how that is feasible.  So
-# we hack and replicate as best we can.
-
 sub determine_api_host {
-	# approximate the combination of lvs + lucene.php (pmtpa) + lucene.php (eqiad)
+	# approximate the combination of lucene.php (pmtpa) + (eqiad)
 	# to determine which hosts to query based on search parameters
 	my $q = shift;
 	my @hosts;
-	if (defined $q->{'suggest'}) {
-		if ($q->{'wgDBname'} eq 'enwiki') {
-			push @hosts, qw(search1002.eqiad.wmnet search1006.eqiad.wmnet) if $q->{'datacenter'} =~ /eqiad/;
-			push @hosts, qw(search8.pmtpa.wmnet) if $q->{'datacenter'} =~ /pmtpa/;
-		} else { # default mwsuggest host
-			push @hosts, qw(search1017.eqiad.wmnet search1018.eqiad.wmnet) if $q->{'datacenter'} =~ /eqiad/;
-			push @hosts, qw(search18.pmtpa.wmnet) if $q->{'datacenter'} =~ /pmtpa/;
+	my $mode = $q->{'pool_mode'};
+	for my $dc (split /,/, $q->{'datacenter'}) {
+		if ($dc eq 'eqiad') {
+			if (defined $q->{'suggest'}) {
+				push @hosts, @{$conf->{'lvs'}->{$dc}->{'search_prefix'}->{$mode}}
+			} elsif ($q->{'wgDBname'} eq 'enwiki') {
+				push @hosts, @{$conf->{'lvs'}->{$dc}->{'search_pool1'}->{$mode}};
+			} elsif ($q->{'wgDBname'} =~ /^(dewiki|frwiki|jawiki)$/) {
+				push @hosts, @{$conf->{'lvs'}->{$dc}->{'search_pool2'}->{$mode}};
+			} elsif ($q->{'wgDBname'} =~ /^(eswiki|itwiki|ptwiki|plwiki|nlwiki|ruwiki|svwiki|zhwiki)$/) {
+				push @hosts, @{$conf->{'lvs'}->{$dc}->{'search_pool3'}->{$mode}};
+			} else {
+				push @hosts, @{$conf->{'lvs'}->{$dc}->{'search_pool4'}->{$mode}};
+			}
+		}  elsif ($dc eq 'pmtpa') {
+			if (defined $q->{'suggest'}) and ($q->{'wgDBname'} eq 'enwiki')) {
+				push @hosts, 'search8.pmtpa.wmnet';
+			} elsif (defined $q->{'suggest'}) {
+				push @hosts, 'search18.pmtpa.wmnet';
+			} elsif ($q->{'wgDBname'} eq 'enwiki') {
+				push @hosts, @{$conf->{'lvs'}->{$dc}->{'search_pool1'}->{$mode}};
+			} elsif ($q->{'wgDBname'} =~ /^(dewiki|frwiki|jawiki)$/) {
+				push @hosts, @{$conf->{'lvs'}->{$dc}->{'search_pool2'}->{$mode}};
+			} elsif ($q->{'wgDBname'} =~ /^(itwiki|ptwiki|plwiki|nlwiki|ruwiki|svwiki|zhwiki)$/) {
+				push @hosts, @{$conf->{'lvs'}->{$dc}->{'search_pool3'}->{$mode}};
+			} elsif ($q->{'wgDBname'} eq 'eswiki') {
+				push @hosts, 'search14.pmtpa.wmnet';
+			} else {
+				push @hosts, 'search11.eqiad.wmnet';
+			}
 		}
-	} elsif ($q->{'wgDBname'} eq 'enwiki') {
-		#return qw(10.2.1.11); # search_pool1 pmtpa
-		push @hosts, qw(search1001.eqiad.wmnet search1002.eqiad.wmnet search1003.eqiad.wmnet) if $q->{'datacenter'} =~ /eqiad/;
-		push @hosts, qw(search1.pmtpa.wmnet search3.pmtpa.wmnet search4.pmtpa.wmnet search9.pmtpa.wmnet) if $q->{'datacenter'} =~ /pmtpa/;
-	} elsif ($q->{'wgDBname'} =~ /^(dewiki|frwiki|jawiki)$/) {
-		#return qw(10.2.1.12); # search_pool2 pmtpa
-		push @hosts, qw(search1007.eqiad.wmnet search1008.eqiad.wmnet) if $q->{'datacenter'} =~ /eqiad/;
-		push @hosts, qw(search6.pmtpa.wmnet search15.pmtpa.wmnet) if $q->{'datacenter'} =~ /pmtpa/;
-	} elsif ($q->{'wgDBname'} =~ /^(itwiki|ptwiki|plwiki|nlwiki|ruwiki|svwiki|zhwiki)$/) {
-		#return qw(10.2.1.13); # search_pool3 pmtpa
-		push @hosts, qw(search1011.eqiad.wmnet search1012.eqiad.wmnet search1013.eqiad.wmnet) if $q->{'datacenter'} =~ /eqiad/;
-		push @hosts, qw(search7.pmtpa.wmnet) if $q->{'datacenter'} =~ /pmtpa/;
-	} elsif ($q->{'wgDBname'} eq 'eswiki') {
-		push @hosts, qw(search1011.eqiad.wmnet search1012.eqiad.wmnet) if $q->{'datacenter'} =~ /eqiad/;
-		push @hosts, qw(search14.pmtpa.wmnet) if $q->{'datacenter'} =~ /pmtpa/;
-	} else {
-		push @hosts, qw(search1015.eqiad.wmnet search1016.eqiad.wmnet) if $q->{'datacenter'} =~ /eqiad/;
-		push @hosts, qw(search11.pmtpa.wmnet) if $q->{'datacenter'} =~ /pmtpa/;
 	}
 	return @hosts;
 }
