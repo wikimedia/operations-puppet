33087568144ccde4f70f198b623315a7e7e246cf
Antoine Musso <hashar@free.fr>
2012-04-04 14:15:57 +0200

    document fenari symbolic links
    We have been using short symlinks on our bastion for ages, we might as
    well have them documented in puppet.
    
    I have added /h to points to /home. Used to be on zwinger, disappeared
    with fenari.
    
    Since deployment and bastion hosts are going to be split, this change
    also create the new misc::deployment-host class which hold those
    symbolic links.
    
    Change-Id: I6082e4f5332aca610055d6a3e9ad1192f90bdb72

---
+++ manifests/misc/deployment-host.pp
@@ -0,0 +1,23 @@
+# misc/deployment-host.pp
+
+# deployment hosts
+
+class misc::deployment-host {
+	system_role { "misc::deployment-host": description => "Deployment" }
+
+	$wp = '/home/wikipedia';
+
+	file { "/h"         : ensure => link, target =>  "/home" }
+	file { "/home/w"    : ensure => link, target =>  '/home/wikipedia' }
+
+	file { $wp + "/b"   : ensure => link, target =>  $wp + "/bin" }
+	file { $wp + "/c"   : ensure => link, target =>  $wp + "/common" }
+	file { $wp + "/d"   : ensure => link, target =>  $wp + "/doc" }
+	file { $wp + "/docs": ensure => link, target =>  $wp + "/doc" }
+	file { $wp + "/h"   : ensure => link, target =>  $wp + "/htdocs" }
+	file { $wp + "/l"   : ensure => link, target =>  $wp + "/logs" }
+	file { $wp + "/log" : ensure => link, target =>  $wp + "/logs" }
+	file { $wp + "/s"   : ensure => link, target =>  $wp + "/src" }
+
+}
+
