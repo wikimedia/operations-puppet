985a7e4521cf4eead7439873f1cbfd8069ca37e6
Antoine Musso <hashar@free.fr>
2012-04-04 14:15:57 +0200

    document fenari symbolic links
    We have been using short symlinks on our bastion for ages, we might as
    well have them documented in puppet.
    
    I have added /h to points to /home. Used to be on zwinger, disappeared
    with fenari.
    
    Change-Id: I6082e4f5332aca610055d6a3e9ad1192f90bdb72

--- manifests/misc/bastion.pp
+++ manifests/misc/bastion.pp
@@ -6,7 +6,7 @@ class misc::bastionhost {
 	system_role { "misc::bastionhost": description => "Bastion" }
 
 	require mysql::client
-	
+
 	package { "irssi":
 		ensure => absent;
 		"traceroute-nanog":
@@ -21,4 +21,17 @@ class misc::bastionhost {
 		mode => 0440,
 		source => "puppet:///files/sudo/sudoers.appserver";
 	}
+
+	file { "/h"       : ensure => link, target =>  "/home" }
+	file { "/home/b"  : ensure => link, target =>  "bin" }
+	file { "/home/c"  : ensure => link, target =>  "common" }
+	file { "/home/d"  : ensure => link, target =>  "doc" }
+	file { "/home/docs": ensure => link, target =>  "doc" }
+	file { "/home/h"  : ensure => link, target =>  "htdocs" }
+	file { "/home/l"  : ensure => link, target =>  "logs" }
+	file { "/home/log": ensure => link, target =>  "logs" }
+	file { "/home/s"  : ensure => link, target =>  "src" }
+	file { "/home/w"  : ensure => link, target =>  "wikipedia" }
+
 }
+
