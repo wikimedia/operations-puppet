b70cddd600d2166ff4838bfe0ffa8cedd45586d8
jgreen <jgreen@wikimedia.org>
2012-04-05 17:21:39 +0000

    looks like systemuser may be broken in generic-definitions.pp skipping user creation step for now
    Change-Id: I93caad96e7a6078e9f682bf849772ec7d7132dd5

--- manifests/misc/mwlib.pp
+++ manifests/misc/mwlib.pp
@@ -3,12 +3,13 @@
 # Installs needed packages for pediapress/mwlib.
 class misc::mwlib::packages {
 
-  package { [ "gcc", "g++", "make", "python", "python-dev", "python-virtualenv", "libjpeg-dev", "libz-dev", "libfreetype6-dev", "liblcms-dev", "libxml2-dev", "libxslt-dev", "ocaml-nox", "git-core", "python-imaging", "python-lxml", "texlive-latex-recommended", "ploticus", "dvipng", "imagemagick", "pdftk" ]:
-    ensure   => 'installed',
-  }
+	package { [ "gcc", "g++", "make", "python", "python-dev", "python-virtualenv", "libjpeg-dev", "libz-dev", "libfreetype6-dev", "liblcms-dev", "libxml2-dev", "libxslt-dev", "ocaml-nox", "git-core", "python-imaging", "python-lxml", "texlive-latex-recommended", "ploticus", "dvipng", "imagemagick", "pdftk" ]:
+		ensure   => 'installed',
+	}
 
 }
 
 class misc::mwlib::users {
-	systemuser { "pp": name => "pp", home => "/opt/pp", shell => "/bin/bash" }
+	# not lolling. generic.pp is broken
+	#systemuser { "pp": name => "pp", home => "/opt/pp", shell => "/bin/bash" }
 }
