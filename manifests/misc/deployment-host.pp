36293dd30adec750a6fdf40ecd9345e7f7d946a3
Antoine Musso <hashar@free.fr>
2012-04-13 14:38:04 +0200

    fix variable concatenation
    Fix up variable - string concatenation that used the + operand.
    Introduced by change 4261.
    
    Change-Id: I422f7d1f431a4bbff52d5a4b5ce3c348f1895aa6

--- manifests/misc/deployment-host.pp
+++ manifests/misc/deployment-host.pp
@@ -11,14 +11,14 @@ class misc::deployment-host {
 		"/h"         : ensure => link, target =>  "/home";
 		"/home/w"    : ensure => link, target =>  '/home/wikipedia';
 
-		"${wp}/b"   : ensure => link, target =>  $wp + "/bin";
-		"${wp}/c"   : ensure => link, target =>  $wp + "/common";
-		"${wp}/d"   : ensure => link, target =>  $wp + "/doc";
-		"${wp}/docs": ensure => link, target =>  $wp + "/doc";
-		"${wp}/h"   : ensure => link, target =>  $wp + "/htdocs";
-		"${wp}/l"   : ensure => link, target =>  $wp + "/logs";
-		"${wp}/log" : ensure => link, target =>  $wp + "/logs";
-		"${wp}/s"   : ensure => link, target =>  $wp + "/src";
+		"${wp}/b"   : ensure => link, target =>  "${wp}/bin";
+		"${wp}/c"   : ensure => link, target =>  "${wp}/common";
+		"${wp}/d"   : ensure => link, target =>  "${wp}/doc";
+		"${wp}/docs": ensure => link, target =>  "${wp}/doc";
+		"${wp}/h"   : ensure => link, target =>  "${wp}/htdocs";
+		"${wp}/l"   : ensure => link, target =>  "${wp}/logs";
+		"${wp}/log" : ensure => link, target =>  "${wp}/logs";
+		"${wp}/s"   : ensure => link, target =>  "${wp}/src";
 	}
 }
 
