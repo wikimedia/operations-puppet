cd57050ec374d960efa605afd02709f64c59595f
Mark Bergsma <mark@wikimedia.org>
2012-04-19 13:05:10 +0200

    Double the backend weights to improve chash distribution
    Change-Id: I8287257aaf9ad3bdb712e1bdf613884b14603d9d

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -400,6 +400,7 @@ class role::cache {
 					'between_bytes_timeout' => "2s",
 					'max_connections' => 100000,
 					'probe' => "varnish",
+					'weight' => 10,
 				},
 				xff_sources => $network::constants::all_networks,
 				before => Class[varnish::logging]
