dfdad3c8e21640361a14f40aeb8c4321d2f04560
Dzahn <dzahn@wikimedia.org>
2012-04-20 15:06:29 +0930

    write logs for refreshLinks cron jobs to mwdeploy home, ugh, capitalization
    Change-Id: Icef4c630be92bc8447775c0cec5a65a4d7f8c041

--- manifests/mediawiki.pp
+++ manifests/mediawiki.pp
@@ -33,8 +33,15 @@ class mediawiki::refreshlinks {
 		$cluster = regsubst($name, '@.*', '\1')
 		$hour = regsubst($name, '.*@', '\1')
 
+		file { "/home/mwdeploy/refreshLinks":
+			ensure => directory,
+			owner => mwdeploy,
+			group => mwdeploy,
+			mode => 0664,
+		}
+
 		cron { "cron-refreshlinks-${name}":
-			command => "/usr/local/bin/mwscriptwikiset refreshLinks.php ${cluster}.dblist --dfn-only",
+			command => "/usr/local/bin/mwscriptwikiset refreshLinks.php ${cluster}.dblist --dfn-only > /home/mwdeploy/refreshLinks/${name}.log 2>&1",
 			user => mwdeploy,
 			hour => $hour,
 			minute => 0,
