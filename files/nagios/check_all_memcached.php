b1e9f19254bc029fd1240abd3b74c655c056395a
Dzahn <dzahn@wikimedia.org>
2012-04-23 16:19:17 +0930

    check_all_memcached: check all, then tell if multiple are down instead of exiting after first error
    Change-Id: Ie6a4358a3f5e4e76740db596cae60f581feaaace

--- files/nagios/check_all_memcached.php
+++ files/nagios/check_all_memcached.php
@@ -1,4 +1,5 @@
 <?php
+# check all memcached servers - Nagios plugin
 
 require_once( '/home/w/common/wmf-config/mc.php' );
 #$wgMemCachedServers[] = '10.0.2.101:11000';
@@ -9,6 +10,8 @@ $CRITICAL=2;
 
 $statuscode = array( $OK => 'OK', $WARNING => 'WARNING', $CRITICAL => 'CRITICAL' );
 
+$crit_hosts = array();
+$message = "Could not connect: ";
 
 function nagios_return( $result, $message ) {
 	global $statuscode;
@@ -23,21 +26,29 @@ foreach ( $wgMemCachedServers as $server ) {
 	$fp = @fsockopen( $host, $port, $errno, $errstr, 3 );
 
 	if ( !$fp ) {
-		nagios_return( $CRITICAL, "Can not connect to $host:$port ($errstr)" );
+		$crit_hosts[] = "${host}:${port} ($errstr) ";
 	} else {
-
-        	@fwrite($fp, "stats\r\n");
-        	stream_set_timeout($fp, 2);
-        	$res = @fread($fp, 2000);
-
-        	$info = @stream_get_meta_data($fp);
-        	fclose($fp);
+		@fwrite($fp, "stats\r\n");
+		stream_set_timeout($fp, 2);
+		$res = @fread($fp, 2000);
+		$info = @stream_get_meta_data($fp);
+		fclose($fp);
 
 
-        	if ($info['timed_out']) {
-                	nagios_return( $CRITICAL, "Timeout reading from $host:$port" );
-        	}
+		if ($info['timed_out']) {
+			$crit_hosts[] = "${host}:${port} (timeout) ";
+		}
 	}
 }
 
-nagios_return( $OK, "All memcacheds are online" );
+if (count($crit_hosts) == 0 ) {
+	nagios_return( $OK, "All memcacheds are online" );
+} else {
+	foreach ( $crit_hosts as $crit_host ) {
+		$hosts_string .= $crit_host;
+	}
+	nagios_return( $CRITICAL, "${message} ${hosts_string}" );
+}
+
+nagios_return( $WARNING, "check check_all_memcached.php - we should not be here" );
+
