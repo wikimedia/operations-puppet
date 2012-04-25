011fb4eb4692bb43225ed01eeae8ed5fbe8e7431
Antoine Musso <hashar@free.fr>
2012-04-25 14:15:20 +0200

    classes for deployment preparation project (beta)
    This are very basic classes for the deployment preparation project
    hosted on WMF labs and also known simply as "beta".
    
    Change-Id: I962fa60198fd2105b2a04e87490fcea4ba4c336e

---
+++ manifests/labs/beta/bastion.pp
@@ -0,0 +1,30 @@
+class labs::beta::bastion {
+
+	include misc::scripts
+
+	# Unwanted packages:
+	package { [
+		  'ack'
+		, 'irssi'
+		]: ensure => absent
+	}
+
+	# Start with some tools used by sysadmins
+	package { [
+		  'ack-grep'
+		, 'dsh'
+		, 'joe'
+		, 'tree'
+		]: ensure => installed
+	}
+
+	file {
+		'/usr/local/bin/ack':
+			  ensure => link
+			, target => '/usr/bin/ack-grep'
+			, require => Package['ack-grep']
+		;
+	}
+
+	# `sudo` policies are managed through the nova interface
+}
