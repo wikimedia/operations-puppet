5a29cab3fdfc7b8f854d2533f60bb512e1877256
Jan Gerber <j@thing.net>
2012-04-27 17:54:07 +0200

    move tmh ppa into its own class
    move adding of ppa to its own class,
    only required for lucid(10.04),
    precise(12.04) has all required packages
    
    Change-Id: I4c2342b353ba96869450fc1d6db4a0b11936a468

--- manifests/timedmediahandler.pp
+++ manifests/timedmediahandler.pp
@@ -1,25 +1,34 @@
 # Install TimedMediaHandler dependencies and configuration
 
+class timedmediahandler::ppa {
+    apt::pparepo {
+        "timedmediahandler": 
+            repo_string => "j/timedmediahandler",
+            apt_key => "01975EF3",
+            dist => "lucid",
+            ensure => "present"
+    }
+    package {
+        ["ffmpeg"]:
+            ensure => latest,
+            require => Apt::Pparepo["timedmediahandler"];
+    }
+}
+
 class timedmediahandler::web {
     include webserver::php5
 
     if ( $lsbdistcodename == "lucid" ) {
-        apt::pparepo {
-            "timedmediahandler": 
-                repo_string => "j/timedmediahandler",
-                apt_key => "01975EF3",
-                dist => "lucid",
-                ensure => "present"
-        }
-        package {
-            [ "php-pear",
-              "imagemagick",
-              "ffmpeg"
-            ]:
-            ensure => latest,
-            notify => Service["apache2"],
-            require => Apt::Pparepo["timedmediahandler"], Exec[add-mimetype-webm];
-        }
+        include timedmediahandler::ppa
+    }
+    package {
+        [ "php-pear",
+          "imagemagick",
+          "ffmpeg"
+        ]:
+        ensure => latest,
+        notify => Service["apache2"],
+        require => Exec[add-mimetype-webm];
     }
     exec {
         add-mimetype-webm:
