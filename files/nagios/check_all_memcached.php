e849fa82854a44ad82d667b68850ac08d29d49b8
Dzahn <dzahn@wikimedia.org>
2012-04-27 19:40:44 +0930

    check_all_memcached - do not rely on NFS, instead fetch mc.php from noc http (and add check for unset $wgMemCachedServers)
    we do not want Nagios checks relying on NFS (RT-1269).
    
    i made a change a while ago switching it to a local path, but that had to be
    reverted because at that point there was no method to keep the file on spence in
    sync with the right mc.php (as server on NOC).
    
    after discussing other methods and rsync with notpeter and apergos and a shell exec
    of rsync out of the check script itself was suggest, instead doing it with a HTTP
    request to NOC using PHP methods (file_get_contents()) and parsing the result
    for <IP>:11000. cutting off before string RELOCATED to match only active server
    
    returns currently 79 servers like this, quote from mc.php "79 active nodes (a nice prime number)"
    
    Change-Id: I415e9fe61cbb1b7e5dbc7cb0f3a7c4b8eefef48a

--- files/nagios/check_all_memcached.php
+++ files/nagios/check_all_memcached.php
@@ -1,7 +1,28 @@
 <?php
 # check all memcached servers - Nagios plugin
 
-require_once( '/home/w/common/wmf-config/mc.php' );
+# new method to get mc.php, avoid NFS, rather fetch it from noc http
+$buffer=file_get_contents("http://noc.wikimedia.org/conf/mc.php.txt");
+
+# cut off at string RELOCATED to get active servers only
+$buffer=explode("RELOCATED",$buffer);
+
+# match for IP:11000 strings
+preg_match_all("/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:11000/", $buffer[0], $matches);
+$wgMemCachedServers=$matches[0];
+
+#DEBUG# print_r($wgMemCachedServers);
+
+# we can't simply do the following due to security config
+# http:// wrapper is disabled in the server configuration by allow_url_include=0
+# require_once( 'http://noc.wikimedia.org/conf/mc.php.txt' );
+# new method to get mc.php, avoid NFS, rather fetch it from noc
+
+# old method to get mc.php - relies on NFS, which is a -1
+# but as of today (20120427) you could also still use it if there are any issues with the new way
+# require_once( '/home/w/common/wmf-config/mc.php' );
+
+# should stay commented. was here for debug.
 #$wgMemCachedServers[] = '10.0.2.101:11000';
 
 $OK=0;
@@ -12,6 +33,7 @@ $statuscode = array( $OK => 'OK', $WARNING => 'WARNING', $CRITICAL => 'CRITICAL'
 
 $crit_hosts = array();
 $message = "Could not connect: ";
+$hosts_string="";
 
 function nagios_return( $result, $message ) {
 	global $statuscode;
@@ -20,35 +42,42 @@ function nagios_return( $result, $message ) {
 	exit( $result );
 }
 
-foreach ( $wgMemCachedServers as $server ) {
-	list( $host, $port ) = explode( ':', $server );
+if (isset($wgMemCachedServers)) {
 
-	$fp = @fsockopen( $host, $port, $errno, $errstr, 3 );
+	foreach ( $wgMemCachedServers as $server ) {
+		list( $host, $port ) = explode( ':', $server );
 
-	if ( !$fp ) {
-		$crit_hosts[] = "${host}:${port} ($errstr) ";
-	} else {
-		@fwrite($fp, "stats\r\n");
-		stream_set_timeout($fp, 2);
-		$res = @fread($fp, 2000);
-		$info = @stream_get_meta_data($fp);
-		fclose($fp);
+		#DEBUG# echo "connecting to host: $host\n";
+		$fp = @fsockopen( $host, $port, $errno, $errstr, 3 );
+
+		if ( !$fp ) {
+			$crit_hosts[] = "${host}:${port} ($errstr) ";
+		} else {
+			@fwrite($fp, "stats\r\n");
+			stream_set_timeout($fp, 2);
+			$res = @fread($fp, 2000);
+			$info = @stream_get_meta_data($fp);
+			fclose($fp);
 
 
-		if ($info['timed_out']) {
-			$crit_hosts[] = "${host}:${port} (timeout) ";
+			if ($info['timed_out']) {
+				$crit_hosts[] = "${host}:${port} (timeout) ";
+			}
 		}
 	}
-}
 
-if (count($crit_hosts) == 0 ) {
-	nagios_return( $OK, "All memcacheds are online" );
-} else {
-	foreach ( $crit_hosts as $crit_host ) {
-		$hosts_string .= $crit_host;
+
+	if (count($crit_hosts) == 0 ) {
+		nagios_return( $OK, "All memcacheds are online" );
+	} else {
+		foreach ( $crit_hosts as $crit_host ) {
+			$hosts_string .= $crit_host;
+		}
+		nagios_return( $CRITICAL, "${message} ${hosts_string}" );
 	}
-	nagios_return( $CRITICAL, "${message} ${hosts_string}" );
+
+} else {
+
+nagios_return( $WARNING, "check check_all_memcached.php or mc.php - we should not be here" );
+
 }
-
-nagios_return( $WARNING, "check check_all_memcached.php - we should not be here" );
-
