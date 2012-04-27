935685744fb371cf3a9b482db314dfc7e2486232
ottomata <otto@wikimedia.org>
2012-04-27 12:50:23 -0400

    git::clone define improvements.
    ensure => present|latest|absent.
      - present will just clone
      - latest will pull if there are any changes
      - absent will delete the directory.
    
    directory now specifies the actual working copy directory.
    I have changed the puppetmaster::gitclone and mediawiki::clone usage of this accordingly.
    
    Also getting rid of my previously created mediawiki::clone define.
    This didn't do much other than store the origin url in a variable.
    Might as well store that in a global variable somewhere one day.
    Using git::clone directly in class misc::statistics::mediawiki instead.
    
    Change-Id: I70202e66f7cebc0b0dca5247dabff369abb648dd

--- manifests/generic-definitions.pp
+++ manifests/generic-definitions.pp
@@ -590,33 +590,87 @@ class generic::packages::git-core {
 # Creates a git clone of a specified origin into a top level directory
 #
 # Parameters:
-# - $title
-#		Should be the repository name
-# $branch
-# 	Branch you would like to check out
-#
-define git::clone($directory, $branch="", $origin) {
+#   $directory	-	path to clone the repository into.  Required.
+#	$origin		-	Origin repository URL.
+# 	$branch		-	Branch you would like to check out.
+#   $ensure     -	'absent', 'present', or 'latest'.  Defaults to 'present'.  
+#					'latest' will execute a git pull if there are any changes.
+#					'absent' will ensure the directory is deleted.
+#	$owner		-	owner of $directory, default: 'root'.  git commands will be run by this user.
+#	$group		-	group owner of $directory, default: 'root',
+#	$mode		-	permission mode of $directory, default: 0755
+#   
+# Usage:
+#	git::clone{ "my_clone_name": 
+#		directory => "/path/to/clone/container", 
+#		origin => "http://blabla.org/core.git", 
+#		branch => "the_best_branch" 
+#	}  # clones http://blabla.org/core.git branch 'the_best_branch' at /path/to/clone/container/core
+define git::clone(
+	$directory, 
+	$origin,
+	$branch="", 
+	$ensure='present',
+	$owner="root",
+	$group="root",
+	$mode=0755) {
+		
 	require generic::packages::git-core
+	
+	case $ensure {
+		"absent": {
+			# make sure $directory does not exist
+			file { $directory:
+				ensure  => 'absent',
+				recurse => true,
+				force   => true,
+			}
+		}
 
-	$suffix = regsubst($title, '^([^/]+\/)*([^/]+)$', '\2')
+		# otherwise clone the repository
+		default: {
+			# make sure $directory exists
+			file { $directory:
+				ensure  => 'directory',
+				owner   => $owner,
+				group   => $group,
+				mode    => $mode,
+			}
+			
+			# if branch was specified
+			if $branch {
+				$brancharg = "-b $branch "
+			}
+			# else don't checkout a non-default branch
+			else {
+				$brancharg = ""
+			}
 
-	if $branch {
-		$brancharg = "-b $branch "
-	}
-	else {
-		$brancharg = ""
-	}
-
-	Exec {
-		path => "/usr/bin:/bin",
-		cwd => $directory
-	}
-	exec {
-		"git clone ${title}":
-			command => "git clone ${brancharg}${origin}",
-			creates => "${directory}/${suffix}/.git/config";
+			# set PATH for following execs
+			Exec { path => "/usr/bin:/bin" }
+			# clone the repository
+			exec { "git_clone_${title}":
+				command => "git clone ${brancharg}${origin} $directory",
+				creates => "$directory/.git/config",
+				user    => $owner,
+				require => File[$directory],
+			}
+			
+			# pull if $ensure == latest and if there are changes to merge in.
+			if $ensure == "latest" {
+				exec { "git_pull_${title}":
+					cwd     => $directory,
+					command => "git pull --quiet",
+					# git diff --quiet will exit 1 (return false) if there are differences
+					unless  => "git fetch && git diff --quiet remotes/origin/HEAD",
+					user    => $owner,
+					require => Exec["git_clone_${title}"],
+				}
+			}
+		}
 	}
 }
+
 
 define git::init($directory) {
 	require generic::packages::git-core
