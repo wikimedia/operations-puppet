28bf8ba52a676ccf9e17d06cac06be7ba67a6ca3
Mark Bergsma <mark@wikimedia.org>
2012-05-02 09:29:39 +0000

    git::clone define improvements.
    ensure => present|latest|absent.
    - present will just clone
    - latest will pull if there are any changes
    - absent will delete the directory.
    
    directory now specifies the actual working copy directory.
    I have changed the puppetmaster::gitclone and mediawiki::clone usage of this accordingly.
    
    Also getting rid of my previously created mediawiki::clone define.
    This didn't do much other than store the origin url in a variable.
    Might as well store that in a global variable somewhere one day.
    Using git::clone directly in class misc::statistics::mediawiki instead.
    
    By: Ottomata
    
    Change-Id: Id26123ca04cbf68d0c77ac66722e8950fd141786

--- manifests/mediawiki.pp
+++ manifests/mediawiki.pp
@@ -53,31 +53,6 @@ class mediawiki::refreshlinks {
 	refreshlinks::cronjob { ['s1@0']: }
 }
 
-# Define: mediawiki::clone
-#
-# Parameters:
-# $branch = "",   optional
-# $origin_url = "https://gerrit.wikimedia.org/r/p/test/mediawiki/core.git", optional.
-#
-# Usage:
-# mediawiki::clone { "name_of_my_clone":
-#   path   =>  '/path/to/mediawiki"
-#   branch => 'crazy_branch',
-# }
-# # This will clone mediawiki core into /path/to/mediawiki/core
-#
-# TODO: add ensure parameter to ensure absent or present.
-#       absent would delete the $path, ensure would clone.
-#       Need to modify git::clone to do this.
-# TODO: handle owner, group, and mode.
-define mediawiki::clone (
-	$path   = "/var/www/mediawiki",
-	$branch = "",
-	$origin_url = "https://gerrit.wikimedia.org/r/p/test/mediawiki/core.git") {
-
-	git::clone { "mediawiki_${title}":
-		directory => $path,
-		branch    => $branch,
-		origin    => $origin_url,
-	}	
+class mediawiki::user {
+	systemuser { "mwdeploy": name => "mwdeploy" }
 }
