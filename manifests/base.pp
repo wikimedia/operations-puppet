c3f00a163092d50163a04303ca3b803fac6fec26
Mark Bergsma <mark@wikimedia.org>
2012-05-03 14:07:48 +0200

    Remove 'quiet' kernel option in Puppet instead of in package wikimedia-base
    Change-Id: I1bde0d31ef796561a30afe96ddb0de723df649f2

--- manifests/base.pp
+++ manifests/base.pp
@@ -87,6 +87,23 @@ deb-src http://old-releases.ubuntu.com/ubuntu ${lsbdistcodename}-updates main un
 }
 
 class base::grub {
+	# Disable the 'quiet' kernel command line option so console messages
+	# will be printed.
+	exec {
+		"grub1 remove quiet":
+			path => "/bin:/usr/bin",
+			command => "sed -i '/^# defoptions.*[= ]quiet /s/quiet //' /boot/grub/menu.lst",
+			unless => "grep -q '^# defoptions.*[= ]quiet ' /boot/grub/menu.lst",
+			onlyif => "test -f /boot/grub/menu.lst",
+			notify => Exec["update-grub"];
+		"grub2 remove quiet":
+			path => "/bin:/usr/bin",
+			command => "sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash\"/s/quiet splash//' /etc/default/grub",
+			unless => "grep -q '^GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash\"' /etc/default/grub",
+			onlyif => "test -f /etc/default/grub",
+			notify => Exec["update-grub"];
+	}
+
 	# Ubuntu Precise Pangolon no longer has a server kernel flavour.
 	# The generic flavour uses the CFQ I/O scheduler, which is rather
 	# suboptimal for some of our I/O work loads. Override with deadline.
