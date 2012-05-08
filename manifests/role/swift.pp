f95e39201487fa8f867ce84d2be8bd9869430e1e
Bhartshorne <ben@wikimedia.org>
2012-05-08 19:55:52 +0000

    introducing sharding to all wikis with more than 25k objects
    Change-Id: Id7f3d40af212906de476f5c4eed22afeeb9b2d19

--- manifests/role/swift.pp
+++ manifests/role/swift.pp
@@ -102,7 +102,7 @@ class role::swift {
 				rewrite_password => $passwords::swift::pmtpa-prod::rewrite_password,
 				rewrite_thumb_server => "ms5.pmtpa.wmnet",
 				shard_containers => "some",
-				shard_container_list => "wikipedia-commons-local-thumb,wikipedia-en-local-thumb"
+				shard_container_list => "wikipedia-commons-local-thumb,wikipedia-de-local-thumb,wikipedia-en-local-thumb,wikipedia-fi-local-thumb,wikipedia-fr-local-thumb,wikipedia-he-local-thumb,wikipedia-hu-local-thumb,wikipedia-id-local-thumb,wikipedia-it-local-thumb,wikipedia-ja-local-thumb,wikipedia-ro-local-thumb,wikipedia-ru-local-thumb,wikipedia-th-local-thumb,wikipedia-tr-local-thumb,wikipedia-uk-local-thumb,wikipedia-zh-local-thumb"
 			}
 			include ::swift::proxy
 			include ::swift::proxy::monitoring
