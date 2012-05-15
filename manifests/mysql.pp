288b35a6662fe845ad75d4d2fc52575bb5d84d6c
asher <afeldman@wikimedia.org>
2012-05-14 18:01:59 -0700

    s4 master -> db51
    Change-Id: I670ff3029633fad99107a96cdd5485af67ae8956

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -12,7 +12,7 @@ class mysql {
 	#######################################################################
 	### MASTERS - make sure to update here whenever changing replication
 	#######################################################################
-	if $hostname =~ /^db(38|52|31|35|39|43|37|48)$/ {
+	if $hostname =~ /^db(38|52|51|35|39|43|37|48)$/ {
 		$master = "true"
 		$writable = "true"
 	} else {
