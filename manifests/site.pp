9ca6e07c5fbc3e283c38b1d0ac5670116ea4ea46
ottomata <otto@wikimedia.org>
2012-05-17 15:31:52 -0400

    Creating generic::mysql::server class that installs packages and sets up my.cnf and starts mysqld.
    - Renamed generic::mysql::client to generic::mysql::packages::client
    - Renamed generic::mysql::server to generic::mysql::packages::server
    - Created generic::mysql::server.  This is a parameterized class that renders my.cnf
    - Created generic::apparmor::service in order to notify the service to reload when mysqld's apparmor file changes.
    - including generic::mysql::server on stat1.
    
    (cherry picked from commit eb91214cbbad7f20adaaf9be1ba661defb896329)
    
    Conflicts:
    
    	manifests/generic-definitions.pp
    	manifests/misc-servers.pp
    	manifests/misc/statistics.pp
    	manifests/site.pp
    
    Change-Id: Ibc7df359dbd88d9f5260dfce9e83422dd2f46f52

--- manifests/site.pp
+++ manifests/site.pp
@@ -919,7 +919,7 @@ node "hume.wikimedia.org" {
 		admins::mortals,
 		admins::restricted,
 		nrpe,
-		mysql::client
+                mysql::client
 }
 
 node "ixia.pmtpa.wmnet" {
@@ -1487,7 +1487,7 @@ node "singer.wikimedia.org" {
 		groups::wikidev,
 		accounts::austin,
 		accounts::awjrichards,
-		generic::mysql::client,
+		generic::mysql::packages::client,
 		misc::planet
 
 
