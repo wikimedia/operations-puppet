c99901d6cdd96f2bba26c83f64f6f540efb11ee9
Dzahn <dzahn@wikimedia.org>
2012-05-21 18:47:15 +0930

    add monitoring group analytics-eqiad and set in analytics role class
    Change-Id: Ie1f106744701ea1f2b6492538f17f2eb1c9d464f

--- manifests/role/analytics.pp
+++ manifests/role/analytics.pp
@@ -1,7 +1,10 @@
 # analytics servers (RT-1985)
 
+@monitor_group { "analytics-eqiad": description => "analytics servers in eqiad" }
+
 class role::analytics {
 	system_role { "role::analytics": description => "analytics server" }
+	$nagios_group = "analytics-eqiad"
 
 	include standard,
 		admins::roots,
