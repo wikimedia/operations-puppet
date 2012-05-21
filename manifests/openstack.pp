ab674afb65bc58210644fd8a7c0262ca467745a3
jeremyb <jeremy@tuxmachine.com>
2012-05-21 17:40:55 +0000

    make ircecho config sane (not just very long strings)
    easier to read and easier to see what changed in a diff
    
    Change-Id: Ibb454f8883bfa89122260e53461108ab8775b38a

--- manifests/openstack.pp
+++ manifests/openstack.pp
@@ -244,9 +244,8 @@ class openstack::compute {
 
 class openstack::project-storage-cron {
 
-	$ircecho_infile = "/var/lib/glustermanager/manage-volumes.log"
+	$ircecho_logs = { "/var/lib/glustermanager/manage-volumes.log" => "wikimedia-labs" }
 	$ircecho_nick = "labs-storage-wm"
-	$ircecho_chans = "#wikimedia-labs"
 	$ircecho_server = "irc.freenode.net"
 
 	package { "ircecho":
