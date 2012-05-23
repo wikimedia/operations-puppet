f018e6bd425a62058c910f5671fe7b1d36fb6540
ottomata <otto@wikimedia.org>
2012-05-23 15:18:02 -0400

    Rewrote geoip.pp to be more modular and to use the licensed Maxmind GeoIP data files.
    - Removed 'misc::' prefix from geoip classes.
    - Removing updategeoipdb cron job from puppetmaster in favor of using the geoip::data class.
    - geoip::data can install via puppet or via direct maxmind download depending on the provider paremeter.
    Change-Id: I770d14eb726bbbc842c36cf2ce1f1944397bd049

--- manifests/misc/geoip.pp
+++ manifests/misc/geoip.pp
@@ -1,100 +1,199 @@
-# Installs maxmind IP address Geocoding 
-# packages and database files.
+# geoip.pp
 #
-# Just include misc::geoip
+# Classes to manage installation and update of
+# Maxmind GeoIP libraries and data files.
+#
+# To install geoip packages and ensure that you have up to date .dat files, just do
+#   include geoip
+#
+# If you want to manage the installation of the .dat files yourself,
+# use the geoip::data class.  The default provider is 'puppet', which means
+# the .dat files will be synced from the puppetmaster.  A provider of 'maxmind'
+# will download the files directly from maxmind.
+#
+# NOTE:  The $data_directory parameter (and a few others as well) are
+# used multiple times in a few classes.  I have defined them each time
+# with a default value so that you CAN use the lower level classes if you
+# so choose without having to worry about specifying defaults.  This is
+# less DRY than leaving off the default values in the low level classes,
+# but meh?  If someone doesn't like this we can remove the default values
+# from the low level classes.
 
-class misc::geoip {
-	include misc::geoip::packages
-	include misc::geoip::data
-	include misc::geoip::data::symlink
+
+# == Class geoip
+# Installs Maxmind IP address Geocoding
+# packages and database files (via puppet).
+#
+# This is the only class you need to include if
+# you want to be able to use Maxmind GeoIP libs and data.
+#
+# == Parameters
+# $data_directory - Where the GeoIP data files should live.  default: /usr/share/GeoIP
+#
+class geoip($data_directory = "/usr/share/GeoIP") {
+	class { "geoip::packages":                                        }
+	class { "geoip::data":          data_directory => $data_directory }
+	class { "geoip::data::symlink": data_directory => $data_directory }
 }
 
-# Installs geoip .deb packages.
-class misc::geoip::packages {
+
+# == Class geoip::packages
+# Installs GeoIP packages.
+#
+class geoip::packages {
 	package { [ "libgeoip1", "libgeoip-dev", "geoip-bin" ]:
 		ensure => latest;
 	}
 }
 
+# == Class geoip::packages::python
 # Installs pygeoip package.
-class misc::geoip::python::package {
-	package { "python-geoip": ensure => latest }
-}
+#
+class geoip::packages::python {
+	include geoip::packages
 
-# Installs GeoIP database files.
-class misc::geoip::data {
-	require misc::geoip::packages
-
-	file {
-		"/usr/share/GeoIP/GeoIP.dat":
-			mode => 0644,
-			owner => root,
-			group => root,
-			source => "puppet:///volatile/misc/GeoIP.dat";
-		"/usr/share/GeoIP/GeoIPCity.dat":
-			mode => 0644,
-			owner => root,
-			group => root,
-			source => "puppet:///volatile/misc/GeoIPcity.dat";
+	package { "python-geoip":
+		ensure  => latest,
+		require => Class["geoip::packages"],
 	}
 }
 
-# sets up symlink from /usr/local/share/GeoIP 
-# to /usr/share/GeoIP.  Some scripts expect GeoIP 
+# == Class geoip::data::symlink
+# sets up symlink from /usr/local/share/GeoIP
+# to /usr/share/GeoIP.  Some scripts expect GeoIP
 # databases to be in this location.
-class misc::geoip::data::symlink {
-	require misc::geoip::data
-	
+#
+# == Parameters
+# $data_directory - Where the data files should live.  default: /usr/share/GeoIP
+#
+class geoip::data::symlink($data_directory = "/usr/share/GeoIP") {
 	file { "/usr/local/share/GeoIP":
-		ensure => "/usr/share/GeoIP"
+		ensure  => "$data_directory",
+		require => File["$data_directory"],
 	}
 }
 
 
 
+# == Class geoip::data
+# Conditionally includes either
+# geoip::data::sync or geoip::data::download.
+#
+# The sync class assumes that the data files are
+# available via puppet in puppet:///volatile/GeoIP/
+#
+# The download class runs geoipupdate to download the
+# files from maxmind directly.
+#
+# Currently, source => 'maxmind' is only used by puppetmaster
+# to download the files.  All other nodes get these files
+# via the default source => 'puppet'.  You shouldn't have
+# to worry about this as a user of GeoIP data anyway.  You
+# Should just be includeing geoip to get the data files
+# and geoip packages.
+#
+# == Parameters
+# $provider       - either 'puppet' or 'maxmind'.      default: puppet.
+# $data_directory - Where the data files should live.  default: /usr/share/GeoIP
+# $config_file    - the config file for the geoipupdate command.  This will be put in place from puppet:///private/geoip/GeoIP.conf.  This will not be used if the provider is 'puppet'.  default: /etc/GeoIP.conf
+# $source         - puppet file source for data_directory.  This is not used if provider is 'maxmind'. default: puppet:///volatile/GeoIP
+#
+class geoip::data(
+	$provider       = "puppet",
+	$data_directory = "/usr/share/GeoIP",
+	$config_file    = "/etc/GeoIP.conf",
+	$source         = "puppet:///volatile/GeoIP") {
+
+	# if installing data files from puppet, use
+	# geoip::data::sync class
+	if $provider == "puppet" {
+		class { "geoip::data::sync":
+			data_directory => $data_directory,
+			source         => $source,
+		}
+	}
+
+	# else install the files from the maxmind download
+	# by including geoip::data::download
+	else {
+		class { "geoip::data::download":
+			data_directory => $data_directory,
+			config_file    => $config_file
+		}
+	}
+
+}
 
 
-# This class was not included anywhere.
-# I am putting it here for historical reasons.
-# We have a full version of GeoIPCity.dat, so 
-# I assume that Erik would prefer to use that
-# one rather than the GeoIPLite.  If this is wrong,
-# we can fix it later.
-# Rather than using a custom cron job here to 
-# download and update  the GeoIP database files,
-# we will rely on puppetmaster.pp updategeoipdb cron
-# to update these files.
+
+# == Class geoip::data::sync
+# Installs GeoIP database files from puppetmaster.
+#
+# == Parameters
+# $data_directory - Where the data files should live.  default: /usr/share/GeoIP
+# $source         - A valid puppet source directory.   default: puppet:///volatile/GeoIP
+#
+class geoip::data::sync($data_directory = "/usr/share/GeoIP", $source = "puppet:///volatile/GeoIP") {
+	# recursively copy the $data_directory from $source.
+	file { "$data_directory":
+		source  => "$source",
+		recurse => true,
+	}
+}
 
 
-# # RT 2164
-# class misc::statistics::geoip {
-# 
-# 	file {
-# 		"/usr/local/share/GeoIP":
-# 			owner => root,
-# 			group => wikidev,
-# 			mode => 0770,
-# 			ensure => directory;
-# 		"/usr/local/bin/`":
-# 			owner => root,
-# 			group => wikidev,
-# 			mode => 0750,
-# 			source => "puppet:///files/misc/geoiplogtag",
-# 			ensure => present;
-# 		"/usr/local/bin/update-maxmind-geoip-lib":
-# 			owner => root,
-# 			group => wikidev,
-# 			mode => 0750,
-# 			source => "puppet:///files/misc/update-maxmind-geoip-lib",
-# 			ensure => present;
-# 	}
-# 
-# 	cron {
-# 		"update-maxmind-geoip-lib":
-# 			ensure => present,
-# 			user => ezachte,
-# 			command => "/usr/local/bin/update-maxmind-geoip-lib",
-# 			monthday => 1,
-# 			require => File['/usr/local/bin/update-maxmind-geoip-lib'];
-# 	}
-# }
+# == Class geoip::data::download
+# Installs Maxmind GeoIP database files by downloading
+# them from Maxmind with the geoipupdate command.
+# This also installs a cron job to do this weekly.
+#
+# == Parameters
+# $data_directory - Where the data files should live.  default: /usr/share/GeoIP
+# $config_file    - the config file for the geoipupdate command.  This will be put in place from puppet:///private/geoip/GeoIP.conf.  default: /etc/GeoIP.conf
+#
+class geoip::data::download($data_directory = "/usr/share/GeoIP", $config_file = "/etc/GeoIP.conf") {
+	# Need this to get /usr/bin/geoipupdate installed.
+	include geoip::packages
+
+	# Install GeoIP.conf with Maxmind license keys.
+	file { "$config_file":
+		source => "puppet:///private/geoip/GeoIP.conf"
+	}
+
+	# Make sure the volatile GeoIP directory exists.
+	# Data files will be downloaded by geoipupdate into
+	# this directory.
+	file { "$data_directory":
+		ensure => "directory",
+	}
+
+	# command to run to update the GeoIP database files
+	$geoipupdate_command = "/usr/bin/geoipupdate -f $config_file -d $data_directory"
+
+	# Go ahead and exec geoipupdate now, so that
+	# we can be sure we have these files if
+	# this is the first time puppetmaster is
+	# running this class.
+	exec { "geoipupdate":
+		command     => "$geoipupdate_command",
+		refreshonly => true,
+		subscribe   => File["$config_file"],
+		require     => [Package["geoip-bin"], File["$data_directory"]],
+	}
+
+	# Set up a cron to run geoipupdate weekly.
+	# This will download GeoIP.dat and GeoIPCity.dat
+	# into /usr/share/GeoIP.  If there are other
+	# Maxmind .dat files you want, then
+	# modify GeoIP.conf and add the Maxmind
+	# product IDs for those files.
+	cron { "geoipupdate":
+		command => "$geoipupdate_command",
+		user    => root,
+		weekday => 0,
+		hour    => 3,
+		minute  => 30,
+		ensure  => present,
+		require => [File["$config_file"], Package["geoip-bin"], File["$data_directory"]],
+	}
+}
