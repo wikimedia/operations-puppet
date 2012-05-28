e0effe7ae8fa032247f653d9dc9488f0ce344996
Andrew Bogott <abogott@wikimedia.org>
2012-05-27 21:07:54 -0500

    I long for the day when I can test crap like this without having to publish it for the whole world to see.
    Change-Id: I0929a5f41650004685ebebed729c89a3ba2f9f2c

---
+++ manifests/role/labsmw.pp
@@ -0,0 +1,28 @@
+#include "labsmysql.pp"
+#include "webserver.pp"
+#include "generic-definitions.pp"
+
+
+# A one-step class for setting up a single-node MediaWiki install,
+#  running from a Git tree.
+#
+# (Totally unstable and unreliable, for the moment.)
+class role::labs-mediawiki-install {
+
+        require role::labs-mysql-server
+
+	git::clone {
+		"mediawiki core":
+		require => webserver::php5-mysql,
+		require => webserver::php5,
+		directory => "/var/www",
+		branch => "master",
+		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/core.git";
+	}
+
+	file { '/var/www/index.php':
+                require => git::clone["mediawiki core"],
+		ensure => 'link',
+		target => '/var/www/core/index.php',
+	}
+}
