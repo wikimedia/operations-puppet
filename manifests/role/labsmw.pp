535d27bb18ebf21bf11cfd86bb4473ed0279edee
Andrew Bogott <abogott@wikimedia.org>
2012-05-27 21:39:44 -0500

    I'm pretty much just trying random stuff now.
    Change-Id: Ib1f3a4ae78e97fe8e65d16bd34ae6a79f3a69263

--- manifests/role/labsmw.pp
+++ manifests/role/labsmw.pp
@@ -9,11 +9,10 @@
 # (Totally unstable and unreliable, for the moment.)
 class role::labs-mediawiki-install {
 
-        require role::labs-mysql-server
+        require [ "role::labs-mysql-server", "webserver::php5-mysql", "webserver::php5" ]
 
 	git::clone {
 		"mediawiki core":
-		require => [ Class["webserver::php5-mysql"], Class["webserver::php5"] ],
 		directory => "/var/www",
 		branch => "master",
 		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/core.git";
