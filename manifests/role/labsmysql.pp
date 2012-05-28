6ca19e82f18c569bad19998ec6a1379c0248fb03
Andrew Bogott <abogott@wikimedia.org>
2012-05-28 07:00:44 -0500

    Move mysql datadir to /mnt/mysql, and try to actually move the data there.
    Change-Id: Idf9da685bbf49142d829c438a36d2c136f8ab455

--- manifests/role/labsmysql.pp
+++ manifests/role/labsmysql.pp
@@ -2,6 +2,7 @@
 
 class role::labs-mysql-server {
 	class { "generic::mysql::server":
-		datadir => "/mnt"
+		# Move mysql data to a place where there's actually space.
+		datadir => "/mnt/mysql"
 	}
 }
