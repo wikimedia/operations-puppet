f1c9adaae5dbcb3a08608d9b05e2bcd6fca45572
Andrew Bogott <abogott@wikimedia.org>
2012-05-28 11:26:38 -0500

    Move socket and pid_file for labs.
    Change-Id: Ia8ff0b9893736f9697ced4329800190e902895ce

--- manifests/role/labsmysql.pp
+++ manifests/role/labsmysql.pp
@@ -3,6 +3,9 @@
 class role::labs-mysql-server {
 	class { "generic::mysql::server":
 		# Move mysql data to a place where there's actually space.
-		datadir => "/mnt/mysql"
+		datadir => "/mnt/mysql",
+		# And, labs has /run instead of /var/run.
+		socket => "/run/mysqld/mysqld.sock",
+		pid_file => "/run/mysqld/mysqld.pid"
 	}
 }
