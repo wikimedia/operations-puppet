9bd670708e982fd64973c3608fb90c781ba39f1e
Faidon Liambotis <faidon@wikimedia.org>
2012-05-28 21:00:55 +0300

    puppetmaster: abstract SSL setup into a subclass
    Create a new parameterized class puppetmaster::ssl and include that
    instead of doing the SSL setup directly into the puppetmaster class.
    This is being done to improve reusability and abstraction.
    
    Change-Id: Ie8e538e0dba856eff72705aeb4f1f44b2b2015d5

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -28,32 +28,35 @@ class puppetmaster($server_name="puppet", $bind_address="*", $verify_client="opt
 		ensure => latest;
 	}
 
-	$ssldir = "/var/lib/puppet/server/ssl"
-	# Move the puppetmaster's SSL files to a separate directory from the client's
-	file {
-		[ "/var/lib/puppet/server", $ssldir ]:
-			before => Package["puppetmaster"],
-			ensure => directory,
-			owner => puppet,
-			group => root,
-			mode => 0771;
-		[ "/var/lib/puppet", "$ssldir/ca", "$ssldir/certificate_requests", "$ssldir/certs", "$ssldir/private", "$ssldir/private_keys", "$ssldir/public_keys", "$ssldir/crl" ]:
-			ensure => directory;
-	}
+	class ssl($server_name="puppet", $ca="false") {
+		$ssldir = "/var/lib/puppet/server/ssl"
 
-	if $config['ca'] != "false" {
-		exec { "generate hostcert":
-			require => File["$ssldir/certs"],
-			command => "/usr/bin/puppet cert generate ${server_name}",
-			creates => "$ssldir/certs/${server_name}.pem";
+		# Move the puppetmaster's SSL files to a separate directory from the client's
+		file {
+			[ "/var/lib/puppet/server", $ssldir ]:
+				before => Package["puppetmaster"],
+				ensure => directory,
+				owner => puppet,
+				group => root,
+				mode => 0771;
+			[ "/var/lib/puppet", "$ssldir/ca", "$ssldir/certificate_requests", "$ssldir/certs", "$ssldir/private", "$ssldir/private_keys", "$ssldir/public_keys", "$ssldir/crl" ]:
+				ensure => directory;
 		}
-	}
 
-	exec { "setup crl dir":
-		require => File["$ssldir/crl"],
-		path => "/usr/sbin:/usr/bin:/sbin:/bin",
-		command => "ln -s ${ssldir}/ca/ca_crl.pem ${ssldir}/crl/$(openssl crl -in ${ssldir}/ca/ca_crl.pem -hash -noout).0",
-		onlyif => "test ! -L ${ssldir}/crl/$(openssl crl -in ${ssldir}/ca/ca_crl.pem -hash -noout).0"
+		if $ca != "false" {
+			exec { "generate hostcert":
+				require => File["$ssldir/certs"],
+				command => "/usr/bin/puppet cert generate ${server_name}",
+				creates => "$ssldir/certs/${server_name}.pem";
+			}
+		}
+
+		exec { "setup crl dir":
+			require => File["$ssldir/crl"],
+			path => "/usr/sbin:/usr/bin:/sbin:/bin",
+			command => "ln -s ${ssldir}/ca/ca_crl.pem ${ssldir}/crl/$(openssl crl -in ${ssldir}/ca/ca_crl.pem -hash -noout).0",
+			onlyif => "test ! -L ${ssldir}/crl/$(openssl crl -in ${ssldir}/ca/ca_crl.pem -hash -noout).0"
+		}
 	}
 
 	# Class: puppetmaster::config
@@ -351,6 +354,11 @@ class puppetmaster($server_name="puppet", $bind_address="*", $verify_client="opt
 		bind_address => $bind_address,
 		verify_client => $verify_client,
 		allow_from => $allow_from
+	}
+
+	class { "puppetmaster::ssl":
+		server_name => $server_name,
+		ca => $config['ca']
 	}
 
 	include scripts, gitclone
