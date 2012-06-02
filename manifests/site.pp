f955dfdca7fc2e36ed2f7ecac566ba44fa1cbab6
Bhartshorne <ben@wikimedia.org>
2012-06-02 16:28:47 +0000

    removing sumanah from manganese's sudo list after confirming with her that she does not need access.
    Change-Id: I84f6404b717d4ff5da9f319a36cf14be5c56af3d

--- manifests/site.pp
+++ manifests/site.pp
@@ -1325,7 +1325,7 @@ node "manganese.wikimedia.org" {
 			'ALL = NOPASSWD: /usr/local/bin/svn-group',
 			'ALL = NOPASSWD: /usr/local/sbin/add-labs-user',
 			'ALL = NOPASSWD: /var/lib/gerrit2/review_site/bin/gerrit.sh' ]
-	sudo_user { [ "demon", "robla", "sumanah", "reedy" ]: privileges => $sudo_privs }
+	sudo_user { [ "demon", "robla", "reedy" ]: privileges => $sudo_privs }
 
 	$cluster = "misc"
 	$ldapincludes = ['openldap', 'nss', 'utils']
