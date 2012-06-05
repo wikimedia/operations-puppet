87de54d9ee424dacd26cad9fc52bc44f34f14824
Ryan Lane <rlane@wikimedia.org>
2012-06-05 19:35:46 +0200

    Add 6to4relay role to capella
    Change-Id: Id3cb94c84a02b9671e8b8d25a73f68e934d91cf7

--- manifests/generic-definitions.pp
+++ manifests/generic-definitions.pp
@@ -263,6 +263,42 @@ class base::bonding-tools {
 	package { ["ifenslave-2.6", "ethtool"] : ensure => latest; }
 }
 
+define interface_tun6to4($remove=undef) {
+	if $remove == 'true' {
+		$augeas_cmd = [	"rm auto[./1 = 'tun6to4']",
+				"rm iface[. = 'tun6to4']"
+			]
+	} else {
+		$augeas_cmd = [	"set auto[./1 = 'tun6to4']/1 'tun6to4'",
+				"set iface[. = 'tun6to4'] 'tun6to4'",
+				"set iface[. = 'tun6to4']/family 'inet6'",
+				"set iface[. = 'tun6to4']/method 'v4tunnel'",
+				"set iface[. = 'tun6to4']/endpoint 'any'",
+				"set iface[. = 'tun6to4']/local '192.88.99.1'",
+				"set iface[. = 'tun6to4']/ttl '64'",
+				"set iface[. = 'tun6to4']/pre-up 'ip address add 192.88.99.1/32 dev lo label lo:6to4'",
+				"set iface[. = 'tun6to4']/down 'ip address del 192.88.99.1/32 dev lo label lo:6to4'",
+				"set iface[. = 'tun6to4']/up 'ip -6 route add 2002::/16 dev \$IFACE'",
+			]
+	}
+
+	if $lsbdistid == "Ubuntu" and versioncmp($lsbdistrelease, "10.04") >= 0 {
+		if $remove == 'true' {
+			exec { "/sbin/ifdown tun6to4": before => Augeas["tun6to4"] }
+		}
+
+		# Use augeas
+		augeas { "tun6to4":
+			context => "/files/etc/network/interfaces/",
+			changes => $augeas_cmd;
+		}
+
+		if $remove != 'true' {
+			exec { "/sbin/ifup tun6to4": require => Augeas["tun6to4"] }
+		}
+	}
+}
+
 define interface_tagged($base_interface, $vlan_id, $address=undef, $netmask=undef, $family="inet", $method="static", $up=undef, $remove=undef) {
 	require base::vlan-tools
 
@@ -746,8 +782,8 @@ define sysctl($value) {
 
 	if ${sysctl.${title}} != ${value} {
 		exec { "sysctl $title":
-			command => "/sbin/sysctl -w ${quoted_param}",
-			user => root;
+		command => "/sbin/sysctl -w ${quoted_param}",
+		user => root;
 		}
 	}
 */
@@ -770,7 +806,7 @@ class generic::sysctl::high-http-performance($ensure="present") {
 }
 
 class generic::sysctl::advanced-routing($ensure="present") {
-	if $lsbdistrelease != "8.04" {
+	if $lsbdistid == "Ubuntu" and versioncmp($lsbdistrelease, "12.04") >= 0 {
 		file { advanced-routing-sysctl:
 			name => "/etc/sysctl.d/50-advanced-routing.conf",
 			owner => root,
@@ -778,6 +814,20 @@ class generic::sysctl::advanced-routing($ensure="present") {
 			mode => 0444,
 			notify => Exec["/sbin/start procps"],
 			source => "puppet:///files/misc/50-advanced-routing.conf.sysctl",
+			ensure => $ensure
+		}
+	}
+}
+
+class generic::sysctl::advanced-routing-ipv6($ensure="present") {
+	if $lsbdistrelease != "8.04" {
+		file { advanced-routing-sysctl:
+			name => "/etc/sysctl.d/50-advanced-routing-ipv6.conf",
+			owner => root,
+			group => root,
+			mode => 0444,
+			notify => Exec["/sbin/start procps"],
+			source => "puppet:///files/misc/50-advanced-routing-ipv6.conf.sysctl",
 			ensure => $ensure
 		}
 	}
