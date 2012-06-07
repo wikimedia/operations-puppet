cff6fd8c252a59d038496cbf024854bab4e53642
Ryan Lane <rlane@wikimedia.org>
2012-06-07 18:54:21 +0200

    Adding /public
    Change-Id: Ice01b46a9b464049feeb405332621bbfcd4c3bc2

--- manifests/base.pp
+++ manifests/base.pp
@@ -608,6 +608,13 @@ class base {
 			group => root,
 			mode => 0755;
 		}
+		# Add directory for public (ro) automounts
+		file { "/public":
+			ensure => directory,
+			owner => root,
+			group => root,
+			mode => 0755;
+		}
 	}
 
 }
