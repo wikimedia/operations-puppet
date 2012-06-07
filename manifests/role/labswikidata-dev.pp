014bb94176c13e509500be2adb9627593167c1ae
Jens Ohlig <jens.ohlig@wikimedia.de>
2012-06-07 16:19:56 +0200

    Added labswikidata-dev.pp
    Change-Id: Ia53039f97a3c4e2dc7aad3089af428a40f058aaf

---
+++ manifests/role/labswikidata-dev.pp
@@ -0,0 +1,42 @@
+#include "labsmysql.pp"
+#include "webserver.pp"
+#include "generic-definitions.pp"
+
+class role::labswikidata-dev {
+
+    require "apachesetup",
+		"role::labs-mysql-server",
+		"webserver::php5-mysql",
+		"webserver::php5",
+
+	git::clone { "w":
+		directory => "/var/www/wikidata-dev-repo",
+		branch => "Wikidata",
+		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/core.git";
+	}
+	git::clone { "w":
+		directory => "/var/www/wikidata-dev-client",
+		branch => "Wikidata",
+		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/core.git";
+	}
+	git::clone { "WikibaseLib":
+		directory => "/var/www/wikidata-dev-client/w/extensions",
+		branch => "master",
+		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikibaseLib.git";
+	}
+	git::clone { "wikibase":
+		directory => "/var/www/wikidata-dev-client/w/extensions",
+		branch => "master",
+		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikidataRepo.git";
+	}
+	git::clone { "WikibaseLib":
+		directory => "/var/www/wikidata-dev-repo/w/extensions",
+		branch => "master",
+		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikibaseLib.git";
+	}
+	git::clone { "wikibase":
+		directory => "/var/www/wikidata-dev-repo/w/extensions",
+		branch => "master",
+		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/WikidataRepo.git";
+	}
+}
