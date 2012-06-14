3a6d7bb3e2a0b17622724d0683fe4638cb03be60
Antoine Musso <hashar@free.fr>
2012-06-14 21:22:47 +0200

    Explicitly define fonts package for Precise
    Imagescaler requires several fonts package to render math and SVG. Lucid
    used to provides useful language-support-fonts-* packages which would
    install fonts suitable to that language. Precise no more provide the
    meta packages so we have to explicitly list all the fonts.
    
    To generate the list of fonts, I have used the apt-cache depends command
    on a Lucid box.
    
    Change-Id: If1a7dc1e97945f3020be32ca05961e2a6bd44665

--- manifests/imagescaler.pp
+++ manifests/imagescaler.pp
@@ -13,65 +13,77 @@ class imagescaler::cron {
 }
 
 class imagescaler::packages {
-		package {
-			[
-				"imagemagick",
-				"ghostscript",
-				"ffmpeg",
-				"ffmpeg2theora",
-				"librsvg2-bin",
-				"djvulibre-bin",
-				"netpbm",
-				"language-support-fonts-am",
-				"language-support-fonts-ar",
-				"language-support-fonts-as",
-				"language-support-fonts-bn",
-				"language-support-fonts-bo",
-				"language-support-fonts-dz",
-				"language-support-fonts-el",
-				"language-support-fonts-fa",
-				"language-support-fonts-gu",
-				"language-support-fonts-he",
-				"language-support-fonts-hi",
-				"language-support-fonts-ii",
-				"language-support-fonts-ja",
-				"language-support-fonts-km",
-				"language-support-fonts-kn",
-				"language-support-fonts-ko",
-				"language-support-fonts-lo",
-				"language-support-fonts-ml",
-				"language-support-fonts-mn",
-				"language-support-fonts-mnc",
-				"language-support-fonts-mr",
-				"language-support-fonts-my",
-				"language-support-fonts-ne",
-				"language-support-fonts-or",
-				"language-support-fonts-pa",
-				"language-support-fonts-ta",
-				"language-support-fonts-te",
-				"language-support-fonts-th",
-				"language-support-fonts-ur",
-				"language-support-fonts-yi",
-				"language-support-fonts-zh",
-				"gsfonts",
-				"xfonts-scalable",
-				"wikimedia-fonts",
-				"xfonts-100dpi",
-				"xfonts-75dpi",
-				"xfonts-base",
-				"xfonts-mplus",
-				"ttf-liberation",
-				"ttf-linux-libertine",
-				"ttf-ubuntu-font-family",
-				"libogg0",
-				"libvorbisenc2",
-				"libtheora0",
-				"oggvideotools",
-				"libvips15",
-				"libvips-tools"]:
-			ensure => latest;
-		}
+
+	include imagescaler::packages::fonts
+
+	package {
+		[
+			"imagemagick",
+			"ghostscript",
+			"ffmpeg",
+			"ffmpeg2theora",
+			"librsvg2-bin",
+			"djvulibre-bin",
+			"netpbm",
+			"wikimedia-fonts",
+			"libogg0",
+			"libvorbisenc2",
+			"libtheora0",
+			"oggvideotools",
+			"libvips15",
+			"libvips-tools"]:
+		ensure => latest;
+	}
+
 }
+
+class imagescaler::packages::fonts {
+	package {
+		[
+			"gsfonts",
+			"ttf-alee",
+			"ttf-arabeyes",
+			"ttf-arphic-ukai",
+			"ttf-arphic-uming",
+			"ttf-bengali-fonts",
+			"ttf-devanagari-fonts",
+			"ttf-farsiweb",
+			"ttf-gujarati-fonts",
+			"ttf-kacst",
+			"ttf-kannada-fonts",
+			"ttf-khmeros",
+			"ttf-lao",
+			"ttf-liberation",
+			"ttf-linux-libertine",
+			"ttf-malayalam-fonts",
+			"ttf-manchufont",
+			"ttf-mgopen",
+			"ttf-nafees",
+			"ttf-oriya-fonts",
+			"ttf-punjabi-fonts",
+			"ttf-sil-abyssinica",
+			"ttf-sil-ezra",
+			"ttf-sil-padauk",
+			"ttf-sil-scheherazade",
+			"ttf-sil-yi",
+			"ttf-takao-gothic",
+			"ttf-takao-mincho",
+			"ttf-tamil-fonts",
+			"ttf-telugu-fonts",
+			"ttf-thai-tlwg",
+			"ttf-tmuni",
+			"ttf-ubuntu-font-family",
+			"ttf-unfonts-extra",
+			"ttf-wqy-zenhei",
+			"xfonts-100dpi",
+			"xfonts-75dpi",
+			"xfonts-base",
+			"xfonts-mplus",
+			"xfonts-scalable"]:
+		ensure => latest;
+	}
+}
+
 
 class imagescaler::files {
 
