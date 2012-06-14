7225d269fa9fb990433e18e9259339037f45415a
Antoine Musso <hashar@free.fr>
2012-06-14 21:22:47 +0200

    Explicitly define fonts package for Precise
    Imagescaler requires several fonts package to render math and SVG. Lucid
    used to provides useful language-support-fonts-* that depended on fonts
    for that language, Precise no more provide them so we have to explicitly
    list all the fonts.
    
    To generate the list of fonts, I have used the apt-cache depends command
    on a Lucid box.
    
    Change-Id: If1a7dc1e97945f3020be32ca05961e2a6bd44665

--- manifests/imagescaler.pp
+++ manifests/imagescaler.pp
@@ -22,37 +22,6 @@ class imagescaler::packages {
 				"librsvg2-bin",
 				"djvulibre-bin",
 				"netpbm",
-				"language-support-fonts-am",
-				"language-support-fonts-ar",
-				"language-support-fonts-as",
-				"language-support-fonts-bn",
-				"language-support-fonts-bo",
-				"language-support-fonts-dz",
-				"language-support-fonts-el",
-				"language-support-fonts-fa",
-				"language-support-fonts-gu",
-				"language-support-fonts-he",
-				"language-support-fonts-hi",
-				"language-support-fonts-ii",
-				"language-support-fonts-ja",
-				"language-support-fonts-km",
-				"language-support-fonts-kn",
-				"language-support-fonts-ko",
-				"language-support-fonts-lo",
-				"language-support-fonts-ml",
-				"language-support-fonts-mn",
-				"language-support-fonts-mnc",
-				"language-support-fonts-mr",
-				"language-support-fonts-my",
-				"language-support-fonts-ne",
-				"language-support-fonts-or",
-				"language-support-fonts-pa",
-				"language-support-fonts-ta",
-				"language-support-fonts-te",
-				"language-support-fonts-th",
-				"language-support-fonts-ur",
-				"language-support-fonts-yi",
-				"language-support-fonts-zh",
 				"gsfonts",
 				"xfonts-scalable",
 				"wikimedia-fonts",
@@ -71,7 +40,96 @@ class imagescaler::packages {
 				"libvips-tools"]:
 			ensure => latest;
 		}
+
+	# Additionally include packages which are shipped differently between
+	# different Ubuntu versions.
+	if ( $lsbdistcodename == 'lucid' ) {
+		include imagescaler::packages::fonts::lucid
+	} elsif ( $lsbdistcodename == 'precise' ) {
+		include imagescaler::packages::fonts::precise
+	}
+
 }
+
+# Fonts packages as shipped by Ubuntu Lucid
+class imagescaler::packages::fonts::lucid {
+	package {
+		[
+			"language-support-fonts-am",
+			"language-support-fonts-ar",
+			"language-support-fonts-as",
+			"language-support-fonts-bn",
+			"language-support-fonts-bo",
+			"language-support-fonts-dz",
+			"language-support-fonts-el",
+			"language-support-fonts-fa",
+			"language-support-fonts-gu",
+			"language-support-fonts-he",
+			"language-support-fonts-hi",
+			"language-support-fonts-ii",
+			"language-support-fonts-ja",
+			"language-support-fonts-km",
+			"language-support-fonts-kn",
+			"language-support-fonts-ko",
+			"language-support-fonts-lo",
+			"language-support-fonts-ml",
+			"language-support-fonts-mn",
+			"language-support-fonts-mnc",
+			"language-support-fonts-mr",
+			"language-support-fonts-my",
+			"language-support-fonts-ne",
+			"language-support-fonts-or",
+			"language-support-fonts-pa",
+			"language-support-fonts-ta",
+			"language-support-fonts-te",
+			"language-support-fonts-th",
+			"language-support-fonts-ur",
+			"language-support-fonts-yi",
+			"language-support-fonts-zh"]:
+		ensure => latest;
+	}
+}
+
+# Fonts packages as shipped by Ubuntu Precise
+class imagescaler::packages::fonts::precise {
+	package {
+		[
+			"ttf-alee",
+			"ttf-arabeyes",
+			"ttf-arphic-ukai",
+			"ttf-arphic-uming",
+			"ttf-bengali-fonts",
+			"ttf-devanagari-fonts",
+			"ttf-farsiweb",
+			"ttf-gujarati-fonts",
+			"ttf-kacst",
+			"ttf-kannada-fonts",
+			"ttf-khmeros",
+			"ttf-lao",
+			"ttf-malayalam-fonts",
+			"ttf-manchufont",
+			"ttf-mgopen",
+			"ttf-nafees",
+			"ttf-oriya-fonts",
+			"ttf-punjabi-fonts",
+			"ttf-sil-abyssinica",
+			"ttf-sil-ezra",
+			"ttf-sil-padauk",
+			"ttf-sil-scheherazade",
+			"ttf-sil-yi",
+			"ttf-takao-gothic",
+			"ttf-takao-mincho",
+			"ttf-tamil-fonts",
+			"ttf-telugu-fonts",
+			"ttf-thai-tlwg",
+			"ttf-tmuni",
+			"ttf-unfonts-extra",
+			"ttf-wqy-zenhei"]:
+		ensure => latest;
+		]
+	}
+}
+
 
 class imagescaler::files {
 
