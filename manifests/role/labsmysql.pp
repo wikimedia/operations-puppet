8d8be8f8c5bc6672f6707bde87b80a87d825e9b6
Ryan Lane <rlane@wikimedia.org>
2012-06-22 16:37:13 +0200

    Allow the labs mysql role to be more configurable
    Change-Id: Iac48376f1a32e29bdb065f88f2856a3e293f090a

--- manifests/role/labsmysql.pp
+++ manifests/role/labsmysql.pp
@@ -1,8 +1,19 @@
 #include "mysql.db"
 
 class role::labs-mysql-server {
+	if !$::mysql_datadir {
+		$datadir = "/mnt/mysql"
+	} else {
+		$datadir = $mysql_datadir
+	}
+	if !$::mysql_file_per_table {
+		$file_per_table = "1"
+	} else {
+		$file_per_table = $mysql_file_per_table
+	}
 	class { "generic::mysql::server":
 		# Move mysql data to a place where there's actually space.
-		datadir => "/mnt/mysql",
+		datadir => $datadir,
+		innodb_file_per_table => $file_per_table,
 	}
 }
