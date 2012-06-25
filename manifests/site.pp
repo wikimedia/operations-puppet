9a98cd42168ba6ae4c21bb14f5d4155422f4dff7
lcarr <lcarr@wikimedia.org>
2012-06-25 14:55:12 -0700

    fixing kaulen (again)
    Change-Id: I7ae2e7cfa37c5b7ca350d01ac6cb411f35b5cb5d

--- manifests/site.pp
+++ manifests/site.pp
@@ -1149,7 +1149,6 @@ node "kaulen.wikimedia.org" {
 
 	install_certificate{ "star.wikimedia.org": }
 
-	monitor_service { "http": description => "Apache HTTP", check_command => "check_http" }
 	monitor_service { "memorysurge": description => "Memory using more than expected", check_command => "check_memory_used!500000!510000" }
 	sudo_user { [ "demon", "reedy" ]: privileges => ['ALL = (mwdeploy) NOPASSWD: ALL'] }
 }
