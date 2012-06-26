e091515b114a28bab8db54817cd64b4b3b75d414
pyoungmeister <py@wikimedia.org>
2012-06-26 14:14:36 -0400

    adding shell on fenari for erosen RT 3119
    should be the last of his access needs.
    
    Change-Id: Ifba268e8fc672d7e1bc685d9badee0479240ff06

--- manifests/site.pp
+++ manifests/site.pp
@@ -800,6 +800,7 @@ node "fenari.wikimedia.org" {
 		drac::management,
 		squid::cachemgr,
 		accounts::awjrichards,
+		admins::globaldev,
 		mediawiki::packages
 
 	install_certificate{ "star.wikimedia.org": }
