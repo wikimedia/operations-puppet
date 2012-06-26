793c2df83429e9875fbaca69f7f7e47e5e2d2204
Dzahn <dzahn@wikimedia.org>
2012-06-26 13:19:41 +0930

    add mediawiki-math package class (installed on pdf servers), add to pdf role, ..and single quotes for strings without variables.
    Change-Id: I82531dbeafe66be8d1f2c3ce1ee79aafcaad18e7

--- manifests/role/pdf.pp
+++ manifests/role/pdf.pp
@@ -1,8 +1,9 @@
 # PDF rendering servers (RT-1100,RT-1101)
 
 class role::pdf {
-	system_role { "role::pdf": description => "PDF rendering server" }
+	system_role { 'role::pdf': description => 'PDF rendering server' }
 
 	include standard,
-		misc::pdf::fonts
+		misc::pdf::fonts,
+		mediawiki::math
 }
