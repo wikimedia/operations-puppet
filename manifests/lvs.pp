c1d9bb2cbac2a7d49ec12fe691d7554d6d4e6751
Mark Bergsma <mark@wikimedia.org>
2012-06-27 14:54:02 +0200

    Attempt to fix the inline_template
    Change-Id: I39fe388e3b3cab2abd23cf5cb0df77a6baee20e6

--- manifests/lvs.pp
+++ manifests/lvs.pp
@@ -87,7 +87,7 @@ class lvs::configuration {
 			},
 		'bgp-nexthop-ipv4' => $::ipaddress_eth0,
 		# FIXME: make a Puppet function, or fix facter
-		'bgp-nexthop-ipv6' => inline_template("<%= require 'ipaddr'; (IPAddr.new(scope.lookupvar(\"::ipaddress6_eth0\")).mask(64) | IPAddr.new(scope.lookupvar(\"::ipaddress_eth0\").gsub('.', ':'))).to_s() %>")
+		'bgp-nexthop-ipv6' => inline_template("<%= require 'ipaddr'; (IPAddr.new(scope.lookupvar(\"::ipaddress6_eth0\")).mask(64) | IPAddr.new(\"::\" + scope.lookupvar(\"::ipaddress_eth0\").gsub('.', ':'))).to_s() %>")
 	}
 
 	$idleconnection_monitor_options = {
