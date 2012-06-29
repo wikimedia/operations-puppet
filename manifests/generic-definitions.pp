c8c3448ba62f177078df6784ea6b3a3bbfbf5fa2
Mark Bergsma <mark@wikimedia.org>
2012-06-29 14:12:31 +0000

    Revert "notify Service[apache2] when its conf changes"
    This reverts commit 3a13ae2b548980eac9c1b1a3663ff1e291d7f820

--- manifests/generic-definitions.pp
+++ manifests/generic-definitions.pp
@@ -54,7 +54,6 @@ define systemuser($name, $home=undef, $shell="/bin/false", $groups=undef, $defau
 define apache_site($name, $prefix="", $ensure="link") {
 	file { "/etc/apache2/sites-enabled/${prefix}${name}":
 		target => "/etc/apache2/sites-available/$name",
-		notify => Service["apache2"],
 		ensure => $ensure;
 	}
 }
@@ -79,7 +78,6 @@ define apache_confd($install="false", $enable="true", $ensure="present") {
 			file { "/etc/apache2/conf.d/${name}":
 				source => "puppet:///files/apache/conf.d/${name}",
 				mode => 0444,
-				notify => Service["apache2"],
 				ensure => $ensure;
 			}
 		}
@@ -87,7 +85,6 @@ define apache_confd($install="false", $enable="true", $ensure="present") {
 			file { "/etc/apache2/conf.d/${name}":
 				content => template("apache/conf.d/${name}.erb"),
 				mode => 0444,
-				notify => Service["apache2"],
 				ensure => $ensure;
 			}
 		}
