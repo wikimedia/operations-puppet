0700cb24f1eaa2c073ae35e20d1f37370a7e101a
Antoine Musso <hashar@free.fr>
2012-07-11 11:47:59 +0200

    Precise: get rid of php5-fpm
    The 'php5' Precise package install php5-fpm a Fast/CGI daemon handler.
    It is unneeded since we use libapache2-mod-php5.
    
    Change-Id: I475d11ea50a6baf3fff0b115c35312af3d02b6da

--- manifests/apaches.pp
+++ manifests/apaches.pp
@@ -21,6 +21,11 @@ class apaches::packages {
 		package { [ "php5", "php-wikidiff2", "php5-wmerrors", "php5-intl" ]:
 			ensure => latest;
 		}
+		# On Precise, the 'php5' packages also provides the 'php5-fpm' which
+		# install an unneeded fast CGI server.
+		package { [ "php5-fpm" ]:
+			ensure => absent;
+		}
 	}
 
 	# Explicitly require the Wikimedia version of some packages
