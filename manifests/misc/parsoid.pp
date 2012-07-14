7a31e61046b74ccc8ed8234fc2f90f8e3988915d
Catrope <roan.kattouw@gmail.com>
2012-07-14 11:17:07 -0700

    Add a service class for Parsoid
    Not used in site.pp, waiting for procurement ticket #3271
    
    Eventually this could be packaged as a Debian package, but that means
    that deploying Parsoid fixes requires a package rebuild, so unless
    that's automated, documented, and easy to do for me, I prefer installing
    Parsoid through Puppet and updating it manually.
    
    Change-Id: I71470a079e11e88eed4281c05865685dd50bc5e2

---
+++ manifests/misc/parsoid.pp
@@ -0,0 +1,61 @@
+class misc::parsoid {
+	package { [ "nodejs", "npm" ]:
+		ensure => latest
+	}
+
+	file {
+		"/var/lib/parsoid":
+			ensure => directory,
+			owner => parsoid,
+			group => parsoid,
+			mode => 0755;
+		"/etc/init.d/parsoid":
+			source => "puppet:///files/misc/parsoid.init",
+			owner => root,
+			group => root,
+			mode => 0555;
+		"/usr/bin/parsoid":
+			source => "puppet:///files/misc/parsoid",
+			owner => root,
+			group => root,
+			mode => 0555;
+	}
+
+	systemuser {
+		parsoid:
+			name => "parsoid",
+			default_group => "parsoid",
+			home => "/var/lib/parsoid";
+	}
+
+	git::clone {
+		"parsoid":
+			directory => "/var/lib/parsoid/VisualEditor",
+			owner => parsoid,
+			group => parsoid,
+			origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/VisualEditor.git",
+			#ensure => latest,   # Not enabling this yet cause I'm not sure it's a good idea
+			branch => "master",
+			require => [File["/var/lib/parsoid"], Systemuser["parsoid"]];
+	}
+
+	exec {
+		"parsoid-npm-install":
+			command => "/usr/bin/npm install",
+			cwd => "/var/lib/parsoid/VisualEditor/modules/parser",
+			user => "parsoid",
+			# Needed so we don't try to write /root/.npm and fail
+			environment => "HOME=/var/lib/parsoid",
+			creates => "/var/lib/parsoid/VisualEditor/modules/parser/node_modules",
+			require => [Package['npm'], Git::Clone['parsoid'], Systemuser["parsoid"]];
+	}
+
+	service {
+		"parsoid":
+			hasstatus => true,
+			hasrestart => true,
+			enable => true,
+			ensure => running,
+			require => [File["/etc/init.d/parsoid"], Exec["parsoid-npm-install"]];
+	}
+}
