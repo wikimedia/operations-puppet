fd4b49596211c789c31ebfed59931f516e55bc20
Jan Gerber <j@thing.net>
2012-07-24 05:04:13 -0700

    migrate wikimedia-job-runner to puppet
    import debian package wikimedia-job-runner
    into puppet and allow configuration
    
    Change-Id: I645d66524a0c58de0268b68a377ecd9e322e3743

--- manifests/jobrunner.pp
+++ manifests/jobrunner.pp
@@ -5,3 +5,60 @@ class jobrunner::packages {
 	}
 
 }
+
+class jobrunner::files {
+
+	if $jobrunner_user == undef {
+		$jobrunner_user = "apache"
+	}
+	if $jobrunner_type == undef {
+		$jobrunner_type = ""
+	}
+	if $jobrunner_nice == undef {
+		$jobrunner_nice = 20
+	}
+	if $jobrunner_script == undef {
+		$jobrunner_script = "/usr/local/bin/jobs-loop.sh"
+	}
+	if $jobrunner_pid_file == undef {
+		$jobrunner_pid_file = "/var/run/mw-jobs.pid"
+	}
+	if $jobrunner_timeout == undef {
+		$jobrunner_timeout = 300
+	}
+	if $jobrunner_extra_args == undef {
+		$jobrunner_extra_args = ""
+	}
+	file {
+		"/etc/init.d/mw-job-runner":
+			owner => root,
+			group => root,
+			mode => 0755,
+			source => "puppet:///files/jobrunner/mw-job-runner.init";
+	}
+	file {
+		"/etc/default/mw-job-runner":
+			content => template("jobrunner/mw-job-runner.default.erb");
+	}
+	file {
+		"/usr/local/bin/jobs-loop.sh":
+			owner => root,
+			group => root,
+			mode => 0755,
+			source => "puppet:///files/jobrunner/jobs-loop.sh";
+	}
+	service {
+		"mw-job-runner":
+			require => [
+				File[
+					"/etc/default/mw-job-runner",
+					"/etc/init.d/mw-job-runner",
+					"/usr/local/bin/jobs-loop.sh",
+				],
+			],
+			hasstatus => false,
+			pattern => $jobrunner_pid_file,
+			ensure => running;
+	}
+
+}
