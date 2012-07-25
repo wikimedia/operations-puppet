0cbd464fffe3392800cfeedbbecde3bfd9a4d23b
Antoine Musso <hashar@free.fr>
2012-07-25 21:31:36 +0200

    (bug 36748) deployment-dbdump is the syslog server
    On beta the deployment-dbdump machine is meant to be used as a bastion
    and log collector (it already receive the udplog feed). The rsyslog
    package installed by base::remote-syslog conflicts with syslog-ng
    installed by misc::syslog-server class.
    
    This horrible hack add an exception for the instance named
    deployment-dbdump just like we have exception for nfs1 / nfs2 in
    production.
    
    Change-Id: Ifd9f295b875d10b7c1fd8544e9922a1b6848dc5f

--- manifests/base.pp
+++ manifests/base.pp
@@ -288,7 +288,7 @@ class base::puppet($server="puppet") {
 }
 
 class base::remote-syslog {
-	if ($::lsbdistid == "Ubuntu") and ($::hostname != "nfs1") and ($::hostname != "nfs2") {
+	if ($::lsbdistid == "Ubuntu") and ($::hostname != "nfs1") and ($::hostname != "nfs2") and ($::instancename != "deployment-dbdump") {
 		package { rsyslog:
 			ensure => latest;
 		}
