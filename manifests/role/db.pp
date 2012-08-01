392cb98cb0f8eadd10016ece4be7a1d68e2bc3ea
pyoungmeister <py@wikimedia.org>
2012-08-01 18:48:15 -0400

    moving db role classes to role/db.pp.
    also renaming to role::db::(core|es) and moving the nagios group
    definitions to role/db.pp
    
    Change-Id: I104597ccc465fa7a328243674f50c0b364b191da

--- manifests/role/db.pp
+++ manifests/role/db.pp
@@ -1,3 +1,39 @@
+# role/db.pp
+# db::core and db::es
+
+# Virtual resource for the monitoring server
+@monitor_group { "es": description => "External Storage" }
+@monitor_group { "mysql_pmtpa": description => "pmtpa mysql core" }
+@monitor_group { "mysql_eqiad": description => "eqiad mysql core" }
+
+class role::db::core {
+	$cluster = "mysql"
+
+	system_role { "db::core": description => "Core Database server" }
+
+	include standard,
+		mysql
+}
+
+class role::db::es($mysql_role = "slave") {
+	$cluster = "mysql"
+
+	$nagios_group = "es"
+
+	system_role { "db::es": description => "External Storage server (${mysql_role})" }
+
+	include	standard,
+		mysql,
+		mysql::mysqluser,
+		mysql::datadirs,
+		mysql::conf,
+		mysql::mysqlpath,
+		mysql::monitor::percona::es,
+		mysql::packages,
+		nrpe
+
+}
+
 class role::db::fundraising {
 
 	$crit = $master
