9a3aaf1a6639692bb81c4eea69b05f4065f7fa95
Mark Bergsma <mark@wikimedia.org>
2012-08-03 16:56:34 +0200

    Get rid of $lvs_realserver_ips completely, only the param is now supported
    Change-Id: I76e1e05f0b835a017f1e0c006d9e6668a4592342

--- manifests/lvs.pp
+++ manifests/lvs.pp
@@ -738,15 +738,8 @@ class lvs::balancer::runcommand {
 # Parameters:
 #	- $realserver_ips
 #		Array or hash (name => ip) of service IPs to answer on
-# FIXME: dynamic lookup $lvs_realserver_ips
-class lvs::realserver($realserver_ips=$lvs_realserver_ips) {
-	if $::realm == "labs" {
-		# FIXME: Hack for arrays in LDAP - you suck puppet
-		$ips = split(get_var('lvs_realserver_ips'), ',')
-	}
-	else {
-		$ips = $realserver_ips
-	}
+class lvs::realserver($realserver_ips=[]) {
+
 	file { "/etc/default/wikimedia-lvs-realserver":
 		mode => 0444,
 		owner => root,
