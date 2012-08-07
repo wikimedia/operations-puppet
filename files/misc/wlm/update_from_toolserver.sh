25a11e01ee4b4bd1e59b00e29cf8a875bb822c07
Max Semenik <maxsem.wiki@gmail.com>
2012-08-07 21:40:52 +0400

    WLM updater script
    Change-Id: I30ac53be0c48f337b36eb6ad1bdf0e8fcfaba93b

--- files/misc/wlm/update_from_toolserver.sh
+++ files/misc/wlm/update_from_toolserver.sh
@@ -2,5 +2,19 @@
 # This file is maintained by Puppet
 # puppet:///files/wlm/update_from_toolserver.sh
 
-# dummy so far
+set -e
+
+DUMP="export.`date '+%Y%m%d'`.tar.gz"
+cd /var/wlm/data/
+rm *.txt
+wget https://toolserver.org/~erfgoed/export.tar.gz -O $DUMP
+tar -xzf $DUMP
+
+echo 'DROP TABLE IF EXISTS `monuments_all_tmp`; CREATE TABLE `monuments_all_tmp` (LIKE `monuments_all`);' | mysql wlm
+echo 'DROP TABLE IF EXISTS `admin_tree_tmp`; CREATE TABLE `admin_tree_tmp` (LIKE `admin_tree`);' | mysql wlm
+mysqlimport --local --default-character-set=UTF8 wlm /var/wlm/data/admin_tree_tmp.txt /var/wlm/data/monuments_all_tmp.txt
+echo 'RENAME TABLE `monuments_all` TO `monuments_all_old`, `monuments_all_tmp` TO `monuments_all`, `admin_tree` TO `admin_tree_old`, `admin_tree_tmp` TO `admin_tree`; DROP TABLE IF EXISTS `monuments_all_old`, `admin_tree_old`;' | mysql wlm
+echo 'DROP TABLE IF EXISTS `monuments_all`; DROP TABLE IF EXISTS `admin_tree`; ALTER TABLE `monuments_all_tmp` RENAME TO `monuments_all`; ALTER TABLE `admin_tree_tmp` RENAME TO `admin_tree`;' | mysql wlm
+
+rm -f /var/wlm/cache/countries.ser
 
