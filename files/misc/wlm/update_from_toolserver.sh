b3a70d6e1bef8b1b4a0b69ea32a86119cb8b6613
Max Semenik <maxsem.wiki@gmail.com>
2012-08-07 21:40:52 +0400

    Real WLM updater script
    Change-Id: I30ac53be0c48f337b36eb6ad1bdf0e8fcfaba93b

--- files/misc/wlm/update_from_toolserver.sh
+++ files/misc/wlm/update_from_toolserver.sh
@@ -2,5 +2,7 @@
 # This file is maintained by Puppet
 # puppet:///files/wlm/update_from_toolserver.sh
 
-# dummy so far
+wget http://toolserver.org/~erfgoed/monuments.sql.gz -O /tmp/monuments.sql.gz \
+        && gunzip -c /tmp/monuments.sql.gz | sed -e 's/`monuments_all`/`monuments_all_tmp`/' -e 's/`admin_tree`/`admin_tree_tmp`/' | mysql wlm \
+        && echo 'DROP TABLE IF EXISTS `monuments_all`; DROP TABLE IF EXISTS `admin_tree`; ALTER TABLE `monuments_all_tmp` RENAME TO `monuments_all`; ALTER TABLE `admin_tree_tmp` RENAME TO `admin_tree`;' | mysql wlm
 
