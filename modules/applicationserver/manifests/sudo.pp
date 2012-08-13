da1e013eb19aa81573fd91bb7d7e412cfdbf5656
pyoungmeister <py@wikimedia.org>
2012-08-13 16:51:58 -0400

    sudo::group : allowing for multiple priv. definitions for one group
    Change-Id: I1769bf34b0279331122103c398d7b196c0bb90a2

--- modules/applicationserver/manifests/sudo.pp
+++ modules/applicationserver/manifests/sudo.pp
@@ -3,8 +3,8 @@ class applicationserver::sudo {
 
 	require groups::wikidev
 
-	sudo_group {"wikidev": privileges => ['ALL = (apache) NOPASSWD: ALL'] }
+	sudo_group {"wikidev_apache": privileges => ['ALL = (apache) NOPASSWD: ALL'], group => "wikidev" }
 
-	sudo_group {"wikidev": privileges => ['ALL= NOPASSWD: /usr/sbin/apache2ctl, /etc/init.d/apache2, /usr/bin/renice'] }
+	sudo_group {"wikidev_root": privileges => ['ALL= NOPASSWD: /usr/sbin/apache2ctl, /etc/init.d/apache2, /usr/bin/renice'], group => "wikidev" }
 
 }
