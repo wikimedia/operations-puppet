bc6b45dd3987fcb67447e3082e2079efcf60ba67
Ryan Lane <rlane@wikimedia.org>
2012-08-14 10:09:15 -0700

    Rework openstack and ldap manifests
    Move role-like code from openstack.pp into roles/nova.pp, roles/keystone.pp and roles/glance.pp. Also move role-like code from ldap.pp into role/ldap.pp. Also move some openstack puppet role-like code from openstack.pp to role/puppet.pp.
    
    Configuration for ldap, nova, glance, and keystone are moved into the roles. The configuration is pushed into the paramaterized classes.
    
    Change-Id: Ia720ad0ad6f8910d7380250e9269919b03675135

--- manifests/base.pp
+++ manifests/base.pp
@@ -708,14 +708,18 @@ class base::syslogs($readable = 'false') {
 class base {
 	case $::operatingsystem {
 		Ubuntu,Debian: {
-			include openstack::nova_config
-			
 			include	base::apt,
-				base::apt::update
+				base::apt::update,
+				role::nova::config::pmtpa,
+				role::nova::config::eqiad
 
 			class { base::puppet:
 				server => $::realm ? {
-					'labs' => $openstack::nova_config::nova_puppet_host,
+					'labs' => $::site ? {
+						'pmtpa' => $role::nova::config::pmtpa::puppet_host,
+						'eqiad' => $role::nova::config::eqiad::puppet_host,
+						default => $role::nova::config::pmtpa::puppet_host,
+					},
 					default => "puppet"
 				}
 			}
