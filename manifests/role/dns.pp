3d78d4aef2fdcd58e6527adb8ec2a00f215973ba
Ryan Lane <rlane@wikimedia.org>
2012-08-15 15:50:38 -0700

    Switch dns ldap config to use new ldap hash
    Change-Id: I74c88843ebba13757985eba32ca0566236494350

--- manifests/role/dns.pp
+++ manifests/role/dns.pp
@@ -1,36 +1,36 @@
 # role/dns.pp
 
 class role::dns::ldap {
+	include role::ldap::config::labs
+
+	$ldapconfig = $role::ldap::config::labs::ldapconfig
+
 	if $site == "pmtpa" {
 		interface_ip { "role::dns::ldap": interface => "eth0", address => "208.80.152.33" }
 
 		# FIXME: turn these settings into a hash that can be included somewhere
-		include openstack::nova_config
-
 		class { "dns::auth-server::ldap":
 			dns_auth_ipaddress => "208.80.152.33 208.80.152.32 208.80.153.135",
 			dns_auth_query_address => "208.80.152.33",
 			dns_auth_soa_name => "labs-ns0.wikimedia.org",
-			ldap_host => $openstack::nova_config::nova_ldap_host,
-			ldap_base_dn => $openstack::nova_config::nova_ldap_base_dn,
-			ldap_user_dn => $openstack::nova_config::nova_ldap_user_dn,
-			ldap_user_pass => $openstack::nova_config::nova_ldap_user_pass
+			ldap_host => $ldapconfig["servernames"][0],
+			ldap_base_dn => $ldapconfig["basedn"],
+			ldap_user_dn => $ldapconfig["proxyagent"],
+			ldap_user_pass => $ldapconfig["proxypass"],
 		}
 	}
 	if $site == "eqiad" {
 		interface_ip { "role::dns::ldap": interface => "eth0", address => "208.80.154.19" }
 
 		# FIXME: turn these settings into a hash that can be included somewhere
-		include openstack::nova_config
-
 		class { "dns::auth-server::ldap":
 			dns_auth_ipaddress => "208.80.154.19 208.80.154.18",
 			dns_auth_query_address => "208.80.154.19",
 			dns_auth_soa_name => "labs-ns1.wikimedia.org",
-			ldap_host => "virt1000.wikimedia.org",
-			ldap_base_dn => $openstack::nova_config::nova_ldap_base_dn,
-			ldap_user_dn => $openstack::nova_config::nova_ldap_user_dn,
-			ldap_user_pass => $openstack::nova_config::nova_ldap_user_pass
+			ldap_host => $ldapconfig["servernames"][0],
+			ldap_base_dn => $ldapconfig["basedn"],
+			ldap_user_dn => $ldapconfig["proxyagent"],
+			ldap_user_pass => $ldapconfig["proxypass"],
 		}
 	}
 }
