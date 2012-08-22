43e7324faf439f687c3e4b61adecf95c2d30731f
Daniel Zahn <dzahn@wikimedia.org>
2012-08-22 11:37:08 -0700

    update system role description, add some comments
    Change-Id: I6f3ebac11f6d82674cc9725f7ad8f9f63ba2ed2c

--- manifests/role/planet.pp
+++ manifests/role/planet.pp
@@ -1,12 +1,14 @@
-# planet RSS feed aggregator (planet-venus)
+# planet RSS feed aggregator 2.0 (planet-venus)
 
 class role::planet {
 
-	system_role { "misc::planet-venus": description => "Planet (venus) weblog aggregator" }
-
+	system_role { "role::planet": description => "Planet (venus) weblog aggregator" }
+	
+	# locales are essential for planet. if a new language is added check these too
 	include standard,
 		generic::locales::international
 
+	# be flexible about labs vs. prod
 	case $::realm {
 		labs: {
 			$planet_domain_name = 'wmflabs.org'
@@ -18,15 +20,19 @@ class role::planet {
 			fail('unknown realm, should be labs or production')
 		}
 	}
-
+	
+	# webserver setup
 	install_certificate{ "star.${planet_domain_name}": }
 	class {'webserver::php5': ssl => 'true'; }
 	apache_module { rewrite: name => "rewrite" }
 
+	# dependencies
 	Install_certificate["star.${planet_domain_name}"] -> apache_module["rewrite"] -> Class['webserver::php5']
 
+	# list all planet languages here, cronjobs are auto-created from this array
 	$planet_languages = [ "ar", "ca", "cs", "de", "en", "es", "fr", "gmq", "it", "ja", "pl", "pt", "ro", "ru", "sr", "zh", ]
 
+	# the actual planet-venus class doing all the reset
 	class {'misc::planet-venus':
 		planet_domain_name => $planet_domain_name,
 		planet_languages => $planet_languages,
