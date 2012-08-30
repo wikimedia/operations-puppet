80b032cb45d1469832cf99bc2b0f48e030327aca
Chad Horohoe <chadh@wikimedia.org>
2012-08-30 11:14:49 -0400

    Also add SSH key to the slave host
    Fixup to I3d8a9fd2, this will fix replication from manganese -> formey
    
    Change-Id: I7c6056c06705c4ce110b87e062e0b0e763c2ea45

--- manifests/role/gerrit.pp
+++ manifests/role/gerrit.pp
@@ -35,7 +35,8 @@ class role::gerrit {
 			slave => true,
 			no_apache => true,
 			db_host => "db1048.eqiad.wmnet",
-			host => "formey.wikimedia.org"
+			host => "formey.wikimedia.org",
+			ssh_key => "AAAAB3NzaC1yc2EAAAABIwAAAQEAxOlshfr3UaPr8gQ8UVskxHAGG9xb55xDyfqlK7vsAs/p+OXpRB4KZOxHWqI40FpHhW+rFVA0Ugk7vBK13oKCB435TJlHYTJR62qQNb2DVxi5rtvZ7DPnRRlAvdGpRft9JsoWdgsXNqRkkStbkA5cqotvVHDYAgzBnHxWPM8REokQVqil6S/yHkIGtXO5J7F6I1OvYCnG1d1GLT5nDt+ZeyacLpZAhrBlyFD6pCwDUhg4+H4O3HGwtoh5418U4cvzRgYOQQXsU2WW5nBQHE9LXVLoL6UeMYY4yMtaNw207zN6kXcMFKyTuF5qlF5whC7cmM4elhAO2snwIw4C3EyQgw=="
 		}
 	}
 
