cc7407e190cdc551eea273833ac705b48c7654e2
Antoine Musso <hashar@free.fr>
2012-08-30 22:32:27 +0200

    (bug 39701) beta: automatic MediaWiki update
    This patch is supposed to be run on one server having access to
    /home/wikipedia/common.
    
    The shell script is an infinite loop which is managed by an upstartjob.
    The script will run git pull in an existing MediaWiki core working copy
    and then in the extension working copy, will then invoke an update of
    all submodules.
    
    TODO: use the `mwdeploy` system user.
    
    Change-Id: I84292345f5e4135e03c524a02dd7eb34ee9119cb

---
+++ manifests/misc/beta.pp
@@ -0,0 +1,34 @@
+class misc::beta::scripts {
+
+	require misc::deployment::scripts
+
+	file {
+		"/usr/local/bin/wmf-beta-autoupdate":
+			owner => root,
+			group => root,
+			mode => 0555,
+			require => File["/usr/local/bin/mw-update-l10n"],
+			source => "puppet:///files/misc/beta/wmf-beta-autoupdate";
+		"/etc/default/wmf-beta-autoupdate":
+			owner => root,
+			group => root,
+			mode => 0444,
+			source => "puppet:///files/misc/beta/wmf-beta-autoupdate.default";
+	}
+
+	upstart_job { "wmf-beta-autoupdate": install => true }
+
+	service { "wmf-beta-autoupdate":
+		require => [
+			File["/usr/local/bin/wmf-beta-autoupdate"],
+			Upstart_job["wmf-beta-autoupdate"],
+		],
+		subscribe => [
+			File["/etc/default/wmf-beta-autoupdate"],
+			File["/usr/local/bin/wmf-beta-autoupdate"],
+		],
+		provider => upstart,
+		ensure => running;
+	}
+
+}
