aefc08f7fbbe06f1a762d3daddcb88c5c7e51619
Antoine Musso <hashar@free.fr>
2012-09-24 23:24:22 +0200

    zuul configuration for Wikimedia
    This bootstrap a zuul installation for use on integration-jenkins
    instance and in production.
    
    For production, we will need to fill in the password repository the
    Jenkins API key of the zuul user.
    
    The URL pattern points to a non existing path for now, will be
    implemented later on.
    
    I have left a remember note on the manganese node definition so we do
    not forget about updating role::zuul::production whenever Gerrit is
    migrated to another box.
    
    Finally, the layout.yaml file is provided from another repository
    (integration/zuul-config) to let integration group the possibility to
    deploy changes to the configuration without requiring an op to merge the
    change.
    
    Change-Id: Ife7e6e7dc55c74468051b8cafa5bdc83d4a83dcf

---
+++ manifests/role/zuul.pp
@@ -0,0 +1,38 @@
+# manifests/role/zuul.pp
+
+class role::zuul {
+
+	system_role { "role::zuul": description => "Zuul gating system for Gerrit/Jenkins" }
+
+	class labs {
+
+		system_role { "role::zuul::labs": description => "Zuul on labs!" }
+
+		# Setup the instance for labs usage
+		zuulwikimedia::instance { "zuul-labs":
+			jenkins_server => 'http://10.4.0.227:8080/ci',
+			jenkins_user => 'zuul',
+			gerrit_server => '10.4.0.227',
+			gerrit_user => 'jenkins',
+			# Not enabled yet but we need a pattern anyway:
+			url_pattern => 'http://jenkinslogs.wmflabs.org/{change.number}/{change.patchset}/{pipeline.name}/{job.name}/{build.number}',
+		}
+
+	} # /role::zuul::labs
+
+	class production {
+
+		system_role { "role::zuul::production": description => "Zuul on production" }
+
+		zuulwikimedia::instance { "zuul-production":
+			jenkins_server => 'https://integration.mediawiki.org/ci',
+			jenkins_user => 'zuul-bot',
+			gerrit_server => 'manganese.wikimedia.org',
+			gerrit_user => 'jenkins-bot',
+			# Not enabled yet but we need a pattern anyway:
+			url_pattern => 'http://integration.mediawiki.org/zuulreport/{change.number}/{change.patchset}/{pipeline.name}/{job.name}/{build.number}',
+		}
+
+	} # /role::zuul::production
+
+} # /role::zuul
