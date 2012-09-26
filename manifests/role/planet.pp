e5fef7e77535442bd51ac8f6e7819943e29a6d99
Daniel Zahn <dzahn@wikimedia.org>
2012-09-26 14:09:47 -0700

    planet: add translations of the index.html, turn index.html.tmpl.erb into a puppet template. add a definition to create planet theme. add hash of translations to role class,...
    Change-Id: Id15337e522f97d78a15f945678ef31cecbe1ea46

--- manifests/role/planet.pp
+++ manifests/role/planet.pp
@@ -30,7 +30,90 @@ class role::planet {
 	Class['webserver::php5'] -> apache_module["rewrite"] -> Install_certificate["star.${planet_domain_name}"]
 
 	# list all planet languages here, configs,dirs,cronjobs are auto-created from this array
-	$planet_languages = [ "ar", "ca", "cs", "de", "en", "es", "fr", "gmq", "it", "ja", "pl", "pt", "ro", "ru", "sr", "zh", ]
+	# $planet_languages = [ "ar", "ca", "cs", "de", "en", "es", "fr", "gmq", "it", "ja", "pl", "pt", "ro", "ru", "sr", "zh", ]
+
+	# languages and translations for the index.html.tmlp
+	$planet_languages = {
+		ar => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		ca => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		cs => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		de => {
+			'subscribe' => 'Abonnieren',
+			'subscriptions' => 'Teilnehmer',
+			'lastupdated' => 'Zuletzt aktualisiert',
+		},
+		en => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		es => { 'subscribe' => 'Suscribir',
+			'subscriptions' => 'Suscripciones',
+			'lastupdated' => 'Last updated',
+		},
+		fr => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		gmq => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		it => {
+			'subscribe' => 'Abbonati',
+			'subscriptions' => 'Sottoscrizioni',
+			'lastupdated' => 'Last updated',
+		},
+		ja => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		pl => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		pt => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		ro => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		ru => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		sr => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		zh => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated'
+		},
+	}
 
 	# the actual planet-venus class doing all the rest
 	class {'misc::planet-venus':
@@ -38,3 +121,4 @@ class role::planet {
 		planet_languages => $planet_languages,
 	}
 }
+
