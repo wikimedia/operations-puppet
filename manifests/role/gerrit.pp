c13ab44f58b9c43be1ee85f3bfae260d27486029
Chad Horohoe <chadh@wikimedia.org>
2012-09-27 11:37:29 -0400

    Stop using gerrit2 for replication purposes
    Cleanup and refactor all of the replication destination stuff and
    invent a new role that can easily be added to boxes that want to
    receive replication from gerrit
    
    The gerrit2 account is still created by the gerrit package (as it
    should be) and will thus be on the gerrit::jetty boxes as required
    
    We're still abusing gerrit2 for the hook stuff, but that's slated
    to be removed and replaced with jenkins probably
    
    Change-Id: Ie91b0077ebe82f97078724dc250bcaf721843c9d

--- manifests/role/gerrit.pp
+++ manifests/role/gerrit.pp
@@ -18,6 +18,14 @@ class role::gerrit {
 		}
 	}
 
+	class production::replicationdest {
+		system_role { "role::gerrit::replicationdest": description => "Destination for gerrit replication" }
+
+		class { "gerrit::replicationdest":
+			sshkey => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDIb6jbDSyzSD/Pw8PfERVKtNkXgUteOTmZJjHtbOjuoC7Ty6dbvUMX+45GedcD1wAYkWEY26RhI1lW2yEwKvh7VWkKixXqPNyrQGvI+ldjYEyWsGlEHCNqsh37mJD5K3cwr7X/PMaxzxh7rjTk4uRKjtiga9bz1vTDRDaNlXcj84kifsu7xmCY1E+OL4oqqy7b3SKhOpcpZc7n5GonfRSeon5uFHVUjoZ57xQ8x2736zbuLBwMRKtaB+V63cU9ArL90XdVrWfbjI4Fzfex4tBG9fOvt8lINR62cjH5Lova2kZ6VBeUnJYdZ8V1mOSwtITjwkE0K98FNZdqaANZAH7V"
+		}
+	}
+
 
 	class production {
 		system_role { "role::gerrit::production": description => "Gerrit master" }
@@ -32,7 +40,7 @@ class role::gerrit {
 			ssl_ca => "Equifax_Secure_CA",
 			replication => {
 				"formey" => {
-				  "url" => 'gerrit2@formey.wikimedia.org:/var/lib/gerrit2/review_site/git/${name}.git',
+				  "url" => 'gerritslave@formey.wikimedia.org:/var/lib/gerrit2/review_site/git/${name}.git',
 				  "threads" => "4"
 				}
 			},
@@ -48,8 +56,19 @@ class role::gerrit {
 			no_apache => true,
 			db_host => "db1048.eqiad.wmnet",
 			host => "formey.wikimedia.org",
-			ssh_key => "AAAAB3NzaC1yc2EAAAABIwAAAQEAxOlshfr3UaPr8gQ8UVskxHAGG9xb55xDyfqlK7vsAs/p+OXpRB4KZOxHWqI40FpHhW+rFVA0Ugk7vBK13oKCB435TJlHYTJR62qQNb2DVxi5rtvZ7DPnRRlAvdGpRft9JsoWdgsXNqRkkStbkA5cqotvVHDYAgzBnHxWPM8REokQVqil6S/yHkIGtXO5J7F6I1OvYCnG1d1GLT5nDt+ZeyacLpZAhrBlyFD6pCwDUhg4+H4O3HGwtoh5418U4cvzRgYOQQXsU2WW5nBQHE9LXVLoL6UeMYY4yMtaNw207zN6kXcMFKyTuF5qlF5whC7cmM4elhAO2snwIw4C3EyQgw=="
 		}
+
+		# Slaves need to be able to recieve replication
+		include role::gerrit::production::replicationdest
 	}
 
+	# Include this role on *any* production host that wants to
+	# receive gerrit replication
+	class production::replicationdest {
+		system_role { "role::gerrit::replicationdest": description => "Destination for gerrit replication" }
+
+		class { "gerrit::replicationdest":
+			sshkey => "AAAAB3NzaC1yc2EAAAABIwAAAQEAxOlshfr3UaPr8gQ8UVskxHAGG9xb55xDyfqlK7vsAs/p+OXpRB4KZOxHWqI40FpHhW+rFVA0Ugk7vBK13oKCB435TJlHYTJR62qQNb2DVxi5rtvZ7DPnRRlAvdGpRft9JsoWdgsXNqRkkStbkA5cqotvVHDYAgzBnHxWPM8REokQVqil6S/yHkIGtXO5J7F6I1OvYCnG1d1GLT5nDt+ZeyacLpZAhrBlyFD6pCwDUhg4+H4O3HGwtoh5418U4cvzRgYOQQXsU2WW5nBQHE9LXVLoL6UeMYY4yMtaNw207zN6kXcMFKyTuF5qlF5whC7cmM4elhAO2snwIw4C3EyQgw=="
+		}
+	}
 }
