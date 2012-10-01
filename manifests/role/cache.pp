e0b18a375283350756fb086983c49f10e3132149
Mark Bergsma <mark@wikimedia.org>
2012-10-01 16:03:44 +0200

    Add the final two varnish servers to the backend pool
    Change-Id: If099e2ae7e28c201076fd8bc5a7bb70aab13f56d

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -149,8 +149,8 @@ class role::cache {
 					'cp1032.eqiad.wmnet',
 					'cp1033.eqiad.wmnet',
 					'cp1034.eqiad.wmnet',
-					# 'cp1035.eqiad.wmnet',
-					# 'cp1036.eqiad.wmnet',
+					'cp1035.eqiad.wmnet',
+					'cp1036.eqiad.wmnet',
 				],
 				"esams" => [
 					'knsq16.knams.wikimedia.org',
