93f756c63b21d3075ef1f38ad395dbd2849874c3
Demon <chadh@wikimedia.org>
2012-10-01 14:44:08 +0000

    Revert "Perform daily backups of gerrit for amanda to pick up"
    Per Mark, this isn't necessary since Amanda can just backup the whole git directory.
    
    This reverts commit 85c09175a027712bac2ae1f277a95dea805e4713

--- manifests/gerrit.pp
+++ manifests/gerrit.pp
@@ -93,8 +93,7 @@ class gerrit::jetty ($ldap_hosts,
 	system_role { "gerrit::jetty": description => "Wikimedia gerrit (git) server" }
 
 	include gerrit::crons,
-		gerrit::gitweb,
-		gerrit::backup
+		gerrit::gitweb
 
 	class { "gerrit::account":
 		ssh_key => $ssh_key
@@ -391,24 +390,5 @@ class gerrit::crons {
 		hour => 2,
 		ensure => absent
 	}
-}
 
-# Tar and gzip the git directory daily so amanda can pick it up
-class gerrit::backup {
-
-	file {
-		"/var/lib/gerrit2/review_site/backup":
-			mode  => 0644,
-			owner => "gerrit2",
-			ensure => directory,
-			require => Package["gerrit"];
-	}
-
-	cron { backup_git_data:
-		command => 'tar -czf /var/lib/gerrit2/review_site/backup/gerrit-latest.tar.gz /var/lib/gerrit2/review_site/git',
-		user => gerrit2,
-		hour => 3,
-		ensure => present,
-		require => File["/var/lib/gerrit2/review_site/backup"]
-	}
 }
