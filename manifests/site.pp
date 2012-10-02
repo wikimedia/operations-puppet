c5b98cb685d5fe67ae6c2026ec76a4b7cf43f281
pyoungmeister <py@wikimedia.org>
2012-10-02 08:51:43 -0400

    re-enabling precise jobrunners
    faidon has built php5-intl against ICU 4.2, package php5-intl 5.3.10-1ubuntu3.4+wmf1
    
    Change-Id: I74ed0e53c7f87ea6cb9d3b2f1e864eeb97af2ddf

--- manifests/site.pp
+++ manifests/site.pp
@@ -1624,8 +1624,8 @@ node /mw([1-9]|1[0-6])\.pmtpa\.wmnet/ {
 	if $hostname =~ /^mw[12]$/ {
 		$ganglia_aggregator = "true"
 	}
-	# removed due to bug 40462 -- TS
-	#include	role::applicationserver::jobrunner
+
+	include	role::applicationserver::jobrunner
 }
 
 node /mw(1[7-9]|[2-4][0-9]|5[0-4])\.pmtpa\.wmnet/ {
@@ -1654,8 +1654,7 @@ node /mw10(0[1-9]|1[0-6])\.eqiad\.wmnet/ {
 		$ganglia_aggregator = "true"
 	}
 
-	# removed due to bug 40462 -- TS
-	#include	role::applicationserver::jobrunner
+	include	role::applicationserver::jobrunner
 }
 
 # mw 1017-1113 are apaches (precise)
