a77947e68ea5ce2be9c8824d843610b6177b4337
Damian Zaremba <damian@damianzaremba.co.uk>
2012-10-03 03:24:03 +0100

    First hash at starting to puppetize bots in labs.
    This adds 4 classes and 3 roles.
    1 common class (used for symlinks/language packs that exist on all instances)
    1 mysql class (used for bots-sql1..2..3..4..etc)
    1 application class (used for bots1..2..3..4..etc)
    1 userweb class (used for bots-apache1..2..3..4..etc)
    
    This is no where near mergable yet, more a work in process.
    
    Change-Id: I0013ab9a2619cb4ff71920a7f9fcb2566c5626df

---
+++ manifests/role/labs-bots.pp
@@ -0,0 +1,11 @@
+class role::labs-bots-application {
+    class{ 'labs::bots::common': }
+}
+
+class role::labs-bots-mysql {
+    class{ 'labs::bots::mysql': }
+}
+
+class role::labs-bots-userweb {
+    class{ 'labs::bots::userweb': }
+}
