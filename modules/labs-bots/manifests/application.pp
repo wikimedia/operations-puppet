c349ff1bc64c1cc54701a1c13016a3e4b6f5dfa9
Damian Zaremba <damian@damianzaremba.co.uk>
2012-10-03 03:24:03 +0100

    First hash at starting to puppetize bots in labs.
    This adds 4 classes and 3 roles.
    * common class (used for symlinks/language packs that exist on all instances)
    * mysql class (used for bots-sql1..2..3..4..etc)
    * application class (used for bots1..2..3..4..etc)
    * userweb class (used for bots-apache1..2..3..4..etc)
    
    This is no where near mergable yet, more a work in process.
    
    Change-Id: I0013ab9a2619cb4ff71920a7f9fcb2566c5626df

---
+++ modules/labs-bots/manifests/application.pp
@@ -0,0 +1,15 @@
+class labs-bots::application {
+    # Common stuff
+    include labs-bots::common
+
+    # Standard application packages
+    include generic::packages::git-core
+    package {
+        [ 'python3-minimal', 'python-virtualenv' ]:
+            ensure => latest
+    }
+
+    # TODO - for things like mono do we want generic classes then
+    # include packages::mono or such in here?
+    # TODO - Add all standard software
+}
