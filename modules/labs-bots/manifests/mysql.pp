418c52597b36071f38503622f8a917160976d5dd
Damian Zaremba <damian@damianzaremba.co.uk>
2012-10-03 03:24:03 +0100

    First hash at starting to puppetize bots in labs.
    This adds 4 classes and 3 roles.
    1 common class (used for symlinks/language packs that exist on all instances)
    1 mysql class (used for bots-sql1..2..3..4..etc)
    1 application class (used for bots1..2..3..4..etc)
    1 userweb class (used for bots-apache1..2..3..4..etc)
    
    This is no where near mergable yet, more a work in process.
    
    Change-Id: I0013ab9a2619cb4ff71920a7f9fcb2566c5626df

---
+++ modules/labs-bots/manifests/mysql.pp
@@ -0,0 +1,63 @@
+class labs-bots::mysql {
+    # Common stuff
+    include labs-bots::common
+
+    # MySQL server
+    class {
+        'generic::mysql::server':
+            thread_stack => '192K',
+            thread_cache_size => '8',
+
+            query_cache_limit => '1M',
+            query_cache_size => '16M',
+
+            tmp_table_size => '64M',
+            innodb_buffer_pool_size => '512M',
+            key_buffer_size => '16M',
+
+            max_allowed_packet => '16M',
+            datadir => '/mnt/mysql',
+            version => $::lsbdistrelease ? {
+                '12.04' => '5.5',
+                default => false,
+            }
+    }
+
+    file {
+        # Data dir
+        '/mnt/data/':
+            owner => 'mysql',
+            group => 'mysql',
+            mode => 700,
+            ensure => directory;
+
+        # Backup script
+        '/usr/local/bin/backup_mysql.sh':
+            source => 'puppet:///modules/labs-bots/mysql/backup.sh',
+            ensure => present
+    }
+
+    # Nightly backup cronjob
+    cron {
+        'backup-mysql':
+            command => '/bin/sh /usr/local/bin/backup_mysql.sh &> /dev/null',
+            user => root,
+            minute => '0',
+            hour => '1',
+            ensure => present,
+            require => File[ '/usr/local/bin/backup_mysql.sh']
+    }
+
+    # Write out a my.cnf file
+    file {
+        '/root/.my.cnf':
+            content => "[client]\nuser=root\npass=puppet\n",
+            ensure => present
+    }
+
+    # Remove any remote root logins, we don't want this, ever
+    exec {
+        'purge remote root mysql servers':
+            command => '/usr/bin/mysql -Bse \'DELETE FROM mysql.user WHERE host != "127.0.0.1" AND host != "localhost" AND user = "root"; FLUSH PRIVILEGES;\'; exit 0'
+    }
+}
