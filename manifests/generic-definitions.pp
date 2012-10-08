d0f6f6257b0ed59308160a7f13c2d0c8d1aea36c
Antoine Musso <hashar@free.fr>
2012-10-08 14:27:13 +0200

    fix doc for git::clone
    This simply make git::clone documentation to use the rdoc format. I have
    split the required and optional parameters in two sections.
    
    Documents $timeout.
    
    Change-Id: Iac6f462b2be281ff3996094f13975482e1d3d9dc

--- manifests/generic-definitions.pp
+++ manifests/generic-definitions.pp
@@ -687,37 +687,47 @@ class generic::packages::tree {
 }
 
 # Definition: git::clone
-# Creates a git clone of a specified origin into a top level directory
 #
-# Parameters:
-#	$directory	-	path to clone the repository into.  Required.
-#	$origin		-	Origin repository URL.
-# 	$branch		-	Branch you would like to check out.
-#	$ensure		-	'absent', 'present', or 'latest'.  Defaults to 'present'.
-#					'latest' will execute a git pull if there are any changes.
-#					'absent' will ensure the directory is deleted.
-#	$owner		-	owner of $directory, default: 'root'.  git commands will be run by this user.
-#	$group		-	group owner of $directory, default: 'root',
-#	$mode		-	permission mode of $directory, default: 0755
-#	$ssh		-	SSH command/wrapper to use when checking out (optional)
-#   
-# Usage:
-#	git::clone{ "my_clone_name": 
-#		directory => "/path/to/clone/container", 
-#		origin => "http://blabla.org/core.git", 
-#		branch => "the_best_branch" 
-#	}  # clones http://blabla.org/core.git branch 'the_best_branch' at /path/to/clone/container/core
+# Creates a git clone of a specified origin into a top level directory.
+#
+# === Required parameters
+#
+# $+directory+:: path to clone the repository into.
+# $+origin+:: Origin repository URL.
+#
+# === Optional parameters
+#
+# $+branch+:: Branch you would like to check out.
+# $+ensure+:: _absent_, _present_, or _latest_.  Defaults to _present_.
+#             - _present_ (default) will just clone once.
+#             - _latest_ will execute a git pull if there are any changes.
+#             - _absent_ will ensure the directory is deleted.
+# $+owner+:: Owner of $directory, default: _root_.  git commands will be run by this user.
+# $+group+:: Group owner of $directory, default: 'root'
+# $+mode+:: Permission mode of $directory, default: 0755
+# $+ssh+:: SSH command/wrapper to use when checking out, default: ''
+# $+timeout+:: Time out in seconds for the exec command, default: 300
+#
+# === Example usage
+#
+#	git::clone{ "my_clone_name":
+#		directory => "/path/to/clone/container",
+#		origin    => "http://blabla.org/core.git",
+#		branch    => "the_best_branch"
+#	}
+#
+# Will clones +http://blabla.org/core.git+ branch +the_best_branch+ at +/path/to/clone/container/core+
 define git::clone(
-	$directory, 
+	$directory,
 	$origin,
-	$branch="", 
+	$branch="",
 	$ssh="",
 	$ensure='present',
 	$owner="root",
 	$group="root",
 	$timeout="300",
 	$mode=0755) {
-		
+
 	require generic::packages::git-core
 	
 	case $ensure {
