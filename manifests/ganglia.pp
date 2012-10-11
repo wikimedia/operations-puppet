9e105378953e55f7240f331f0f76e2ed1a062e85
ottomata <otto@wikimedia.org>
2012-10-11 11:08:39 -0400

    Setting up ganglia aggregators for Analytics cluster.
    Change-Id: Id010910f45b7d26220d19c4521ad97486246efd5

--- manifests/ganglia.pp
+++ manifests/ganglia.pp
@@ -130,6 +130,10 @@ class ganglia {
 		"fundraising"	=> {
 			"name"		=> "Fundraising",
 			"ip_oct"	=> "31" },
+		"analytics"		=> {
+			"name"		=> "Analytics cluster",
+			"ip_oct"	=> "32",
+		}
 	}
 	# NOTE: Do *not* add new clusters *per site* anymore,
 	# the site name will automatically be appended now,
@@ -284,7 +288,8 @@ class ganglia {
 						"Text squids esams" => "amssq31.esams.wikimedia.org amssq32.esams.wikimedia.org",
 						"Upload squids esams" => "knsq16.esams.wikimedia.org knsq17.esams.wikimedia.org",
 						"LVS loadbalancers esams" => "amslvs1.esams.wikimedia.org amslvs2.esams.wikimedia.org",
-						"Miscellaneous esams" => "hooft.esams.wikimedia.org"
+						"Miscellaneous esams" => "hooft.esams.wikimedia.org",
+						"Analytics cluster eqiad" => "analytics1001.wikimedia.org analytics1010.eqiad.wmnet",
 					}
 					$rra_sizes = '"RRA:AVERAGE:0.5:1:360" "RRA:AVERAGE:0.5:24:245" "RRA:AVERAGE:0.5:168:241" "RRA:AVERAGE:0.5:672:241" "RRA:AVERAGE:0.5:5760:371"'
 					$rrd_rootdir = "/mnt/ganglia_tmp/rrds.pmtpa"
