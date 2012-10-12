7547003b071c1d46f8023f075fdf0a2fafba4019
Antoine Musso <hashar@free.fr>
2012-10-12 18:27:23 +0200

    fix sudo rights for wmf-beta-autoupdater
    mwdepoy was lacking some sudo right to run the mw-l10n-update script.
    Tested them on lab instance succesfully.
    
    Change-Id: I8e98d666b9221ada707a53eda6de49925f5cb1bc

--- manifests/misc/beta.pp
+++ manifests/misc/beta.pp
@@ -22,7 +22,11 @@ class misc::beta::scripts {
 	}
 
 	# Make sure wmf-beta-autoupdate can run the l10n updater as l10nupdate
-	sudo_user { "mwdeploy" : privileges => ['ALL = (l10nupdate) NOPASSWD:/usr/local/bin/mw-update-l10n'] }
+	sudo_user { "mwdeploy" : privileges => [
+		'ALL = (l10nupdate) NOPASSWD:/usr/local/bin/mw-update-l10n',
+		'ALL = (l10nupdate) NOPASSWD:/usr/local/bin/mwscript',
+		'ALL = (mwdeploy) NOPASSWD: ALL',
+	] }
 
 	upstart_job { "wmf-beta-autoupdate": install => true }
 
