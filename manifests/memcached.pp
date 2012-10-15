0f989d045aba2a69f23881413bebc1f4a5d6bcaa
asher <afeldman@wikimedia.org>
2012-10-15 14:22:45 -0700

    memcached conf for new mc servers
    Change-Id: I4dc4c2bca68ae3667107f277d94b8dd6fc4b7b97

--- manifests/memcached.pp
+++ manifests/memcached.pp
@@ -3,10 +3,12 @@
 # Virtual resource for monitoring server
 @monitor_group { "memcached": description => "all memcached servers" }
 @monitor_group { "mc_pmtpa": description => "pmtpa memcached" }
+@monitor_group { "mc_eqiad": description => "pmtpa memcached" }
 
-class memcached ($memcached_size = '2000', $memcached_port = '11000', $memcached_ip = '0.0.0.0') {
+class memcached ($memcached_size = '2000', $memcached_port = '11000', $memcached_ip = '0.0.0.0', $memcached_options = {}) {
 
-	class { "memcached::config": memcached_size => "$memcached_size", memcached_port => "$memcached_port", memcached_ip => "$memcached_ip" }
+	class { "memcached::config": memcached_size => "$memcached_size", memcached_port => "$memcached_port", 
+		memcached_ip => "$memcached_ip", memcached_options => $memcached_options }
 
 	package { memcached:
 		ensure => latest;
@@ -55,7 +57,7 @@ class memcached ($memcached_size = '2000', $memcached_port = '11000', $memcached
 	include memcached::monitoring
 }
 
-class memcached::config ($memcached_size, $memcached_port, $memcached_ip) {
+class memcached::config ($memcached_size, $memcached_port, $memcached_ip, $memcached_options) {
 
 	file {
 		"/etc/memcached.conf":
