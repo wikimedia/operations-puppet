64dab53dbc9b970770e7bdb5ceb41133e7c267e8
Andrew Bogott <abogott@wikimedia.org>
2012-10-23 15:13:15 +0000

    Add "role::mediawiki-update::labs" for updating labs-MW-installations
    Refactor role::mediawiki::labs into a parameterized base class
    named 'mediawiki::singlenode'.
    
    Replace role::mediawiki::labs with a simple wrapper around ::singlenode.
    
    Add class "role::mediawiki-update::labs" as a version of ::singlenode that
    tracks git trunk.
    
    Change the mediawiki git checkout to use --depth=1 which reduces timeouts.
    
    Change-Id: I7f1e8d841bc4fef6aa43c00478417dd860e73a42

--- manifests/role/labsmediawiki.pp
+++ manifests/role/labsmediawiki.pp
@@ -1,107 +1,15 @@
-#include "labsmysql.pp"
-#include "webserver.pp"
-#include "generic-definitions.pp"
+#  Install Mediawiki from git and keep in sync with git trunk
+class role::mediawiki-install-latest::labs {
 
+	class { "mediawiki::singlenode":
+		keep_up_to_date => true
+	}
+}
 
-# A one-step class for setting up a single-node MediaWiki install,
-#  running from a Git tree.
-#
-# (Totally unstable and unreliable, for the moment.)
+#  Install Mediawiki from git and then leave it alone.
 class role::mediawiki-install::labs {
 
-        require "role::labs-mysql-server",
-		"webserver::php5-mysql",
-		"webserver::php5"
-
-	package { [ "imagemagick", "php-apc",  ] :
-		ensure => latest
-	}
-
-	class { "memcached":
-		memcached_ip => "127.0.0.1" }
-
-	git::clone { "mediawiki":
-		directory => "/srv/mediawiki",
-		branch => "master",
-		timeout => 1800,
-		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/core.git";
-	}
-
-	git::clone { "nuke" :
-		require => git::clone["mediawiki"],
-		directory => "/srv/mediawiki/extensions/Nuke",
-		branch => "master",
-		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Nuke.git";
-	}
-
-	git::clone { "SpamBlacklist" :
-		require => git::clone["mediawiki"],
-		directory => "/srv/mediawiki/extensions/SpamBlacklist",
-		branch => "master",
-		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/SpamBlacklist.git";
-	}
-
-	git::clone { "ConfirmEdit" :
-		require => git::clone["mediawiki"],
-		directory => "/srv/mediawiki/extensions/ConfirmEdit",
-		branch => "master",
-		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/ConfirmEdit.git";
-	}
-
-	file {
-		"/etc/apache2/sites-available/wiki":
-			mode => 644,
-			owner => root,
-			group => root,
-			content => template('apache/sites/simplewiki.wmflabs.org'),
-			ensure => present;
-	}
-
-	file { '/var/www/srv':
-		ensure => 'directory';
-	}
-
-	file { '/var/www/srv/mediawiki':
-		require => [File['/var/www/srv'], git::clone['mediawiki']],
-		ensure => 'link',
-		target => '/srv/mediawiki';
-	}
-
-	if $labs_mediawiki_hostname {
-		$mwserver = "http://$labs_mediawiki_hostname"
-	} else {
-		$mwserver = "http://$hostname.pmtpa.wmflabs"
-	}
-
-	file { '/srv/mediawiki/orig':
-		require => git::clone["mediawiki"],
-		ensure => 'directory';
-	}
-
-        exec { 'password_gen':
-		require => [git::clone["mediawiki"],  File["/srv/mediawiki/orig"]],
-		creates => "/srv/mediawiki/orig/adminpass",
-		command => "/usr/bin/openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /srv/mediawiki/orig/adminpass"
-	}
-
-	exec { 'mediawiki_setup':
-		require => [git::clone["mediawiki"],  File["/srv/mediawiki/orig"], exec['password_gen']],
-		creates => "/srv/mediawiki/orig/LocalSettings.php",
-		command => "/usr/bin/php /srv/mediawiki/maintenance/install.php testwiki admin --dbname testwiki --dbuser root --passfile '/srv/mediawiki/orig/adminpass' --server $mwserver --scriptpath '/srv/mediawiki' --confpath '/srv/mediawiki/orig/'",
-		logoutput => "on_failure",
-	}
-
-        apache_site { controller: name => "wiki" }
-        apache_site { 000_default: name => "000-default", ensure => absent }
-
-	exec { 'apache_restart':
-		require => [Apache_site['controller'], Apache_site['000_default']],
-		command => "/usr/sbin/service apache2 restart"
-	}
-
-	file { '/srv/mediawiki/LocalSettings.php':
-		require => Exec["mediawiki_setup"],
-		content => template('mediawiki/labs-localsettings'),
-		ensure => present,
+	class { "mediawiki::singlenode":
+		keep_up_to_date => false
 	}
 }
