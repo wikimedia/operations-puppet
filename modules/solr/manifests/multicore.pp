5516b8e1c03e929eb46d59cdc6631bc3722b876f
Max Semenik <maxsem.wiki@gmail.com>
2012-10-24 23:04:06 +0400

    WIP: support for multiple Solr cores
    Change-Id: Iff54c3749dbfe5a7a3bcbbbe183deee96f343cdb

---
+++ modules/solr/manifests/multicore.pp
@@ -0,0 +1,40 @@
+class solr::multicore {
+  include solr::multicore::files
+
+  # Update service only if setup script reported changes
+  exec { "echo > /dev/null":
+    unless  => "setup-solr-multicore",
+    path    => "/bin:/usr/sbin",
+    require => [ Class['solr::multicore::files'], Class['solr:install'] ],
+    notify  => Service['jetty'],
+  }
+
+class solr::multicore::files {
+  file {
+    "/var/lib/solr/cores":
+      ensure => directory,
+      owner => 'jetty',
+      group => 'jetty',
+      mode => 0700,
+      require => Class['solr::install'];
+    "/usr/sbin/create-solr-core":
+      ensure => present,
+      source => "puppet://modules/solr/create-solr-core",
+      owner => 'root',
+      group => 'root',
+      mode => 0544;
+    "/usr/sbin/delete-solr-core":
+      ensure => present,
+      source => "puppet://modules/solr/delete-solr-core",
+      owner => 'root',
+      group => 'root',
+      mode => 0544;
+    "/usr/sbin/setup-solr-multicore":
+      ensure => present,
+      source => "puppet://modules/solr/setup-solr-multicore",
+      owner => 'root',
+      group => 'root',
+      mode => 0544;
+  }
+}
+
