e3f4f345a86937bcb61885d1d6a6d75660e7ffc2
Mark Bergsma <mark@wikimedia.org>
2012-10-24 14:41:02 +0200

    Collect response headers Via and X-Varnish on delivery
    Change-Id: I7f0a9f0e49e93c52d9024162d543532621d8f123

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -343,7 +343,7 @@ class role::cache {
 			include standard,
 				nrpe
 
-			class { "varnish::packages": version => "3.0.3plus~rc1-wm4" }
+			class { "varnish::packages": version => "3.0.3plus~rc1-wm5" }
 
 			varnish::setup_filesystem{ ["sda3", "sdb3"]:
 				before => Varnish::Instance["upload-backend"]
