9e8afca8ff5af9b89d7124e5e2d095215d6efa36
Silke Meyer <silke.meyer@wikimedia.de>
2012-10-29 15:53:04 +0100

    Added puppet files for Wikidata on labs
    Change-Id: I4c9a5a8d2eec301284074a3eaefbe3d3014a3036

--- manifests/role/labsmediawiki.pp
+++ manifests/role/labsmediawiki.pp
@@ -13,3 +13,43 @@ class role::mediawiki-install::labs {
 		keep_up_to_date => false
 	}
 }
+
+#  Install Wikidata repo (incl. MediaWiki) from git and keep in sync with git trunk
+class role::wikidata-repo-latest::labs {
+
+	class { "wikidata::singlenode":
+		install_repo => true,
+		install_client => false,
+		keep_up_to_date => true
+	}
+}
+
+#  Install Wikidata repo (incl. Mediawiki) from git and then leave it alone.
+class role::wikidata-repo::labs {
+
+	class { "wikidata::singlenode":
+		install_repo => true,
+		install_client => false,
+		keep_up_to_date => false
+	}
+}
+
+#  Install Wikidata client (incl. MediaWiki) from git and keep in sync with git trunk
+class role::wikidata-client-latest::labs {
+
+	class { "wikidata::singlenode":
+		install_client => true,
+		install_repo => false,
+		keep_up_to_date => true
+	}
+}
+
+#  Install Wikidata (incl. Mediawiki) from git and then leave it alone.
+class role::wikidata-client::labs {
+
+	class { "wikidata::singlenode":
+		install_client  => true,
+		install_repo => false,
+		keep_up_to_date => false
+	}
+}
