c89e987b75a7727d76476c80365690799323e4e7
Mark Bergsma <mark@wikimedia.org>
2012-10-30 18:10:54 +0100

    Define backends for upload-backend in esams
    Change-Id: I688a078d072a887934a5116d30d5f31fb71bb8f1

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -368,8 +368,14 @@ class role::cache {
 				port => 3128,
 				admin_port => 6083,
 				storage => "-s main-sda3=file,/srv/sda3/varnish.persist,${storage_size_main}G -s main-sdb3=file,/srv/sdb3/varnish.persist,${storage_size_main}G -s bigobj-sda3=file,/srv/sda3/large-objects.persist,${storage_size_bigobj}G -s bigobj-sdb3=file,/srv/sdb3/large-objects.persist,${storage_size_bigobj}G",
-				backends => [ "10.2.1.24", "10.2.1.27" ],
-				directors => { "backend" => [ "10.2.1.24" ], "swift" => [ "10.2.1.27" ] },
+				backends => $::site ? {
+					'eqiad' => [ "10.2.1.24", "10.2.1.27" ],
+					'esams' => [ "208.80.154.235" ],
+				},
+				directors => $::site ? {
+					'eqiad' => { "backend" => [ "10.2.1.24" ], "swift" => [ "10.2.1.27" ] },
+					'esams' => { "backend" => [ "208.80.154.235" ] },
+				},
 				director_type => "random",
 				vcl_config => {
 					'retry5xx' => 1,
