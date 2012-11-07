c6fa3895b86801a59f80fb407235701f40d51b98
Mark Bergsma <mark@wikimedia.org>
2012-11-07 11:30:05 +0100

    Make every frontend only talk to its own backend, for now
    Change-Id: I54c7d67e8e6b87386658d3f61ff8fbdfd94e77c5

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -339,7 +339,7 @@ class role::cache {
 				"pmtpa" => {},
 				"eqiad" => { "backend" => $role::cache::configuration::active_nodes['upload'][$::site] },
 				# TODO: replace after removing Squid
-				"esams" => { "backend" => [ "cp3003.esams.wikimedia.org"] },
+				"esams" => { "backend" => [ $::fqdn ] },
 			}
 
 			$varnish_be_directors = {
