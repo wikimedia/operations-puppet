6d856d6fb34c162e15af695872b2ba5dbd41cdef
Ryan Lane <rlane@wikimedia.org>
2012-11-20 12:18:36 -0800

    Add master_finger fingerprint for salt masters
    Change-Id: I14b26775e097ba84f5a77b3fca7b8d57a6ad81b5

--- manifests/role/salt.pp
+++ manifests/role/salt.pp
@@ -25,16 +25,19 @@ class role::salt::minions {
 		$salt_grains = {
 			"instanceproject" => $instanceproject,
 		}
+		$salt_master_finger = "5d:07:fb:28:21:60:fb:db:46:ff:e8:1c:91:a2:1a:f9"
 	} else {
 		$salt_master = "sockpuppet.pmtpa.wmnet"
 		$salt_client_id = undef
 		$salt_grains = {}
+		$salt_master_finger = "e6:f5:71:f5:b0:5c:45:7b:b1:f2:1d:06:4e:b9:98:9f"
 	}
 
 	class { "salt::minion":
 		salt_master => $salt_master,
 		salt_client_id => $salt_client_id,
 		salt_grains => $salt_grains,
+		salt_master_finger => $salt_master_finger,
 	}
 
 }
