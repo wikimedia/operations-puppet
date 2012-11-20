f9536794681a406d799ec9f39ab0ba3456540c38
Dzahn <dzahn@wikimedia.org>
2012-11-20 11:37:33 -0800

    move misc::nfs-server::home / misc::nfs-server::home:rsync from misc-servers to nfs.pp (RT-720)
    Change-Id: Icab02a43799e8b2da07672ced19df77e8fed1d89

--- manifests/misc-servers.pp
+++ manifests/misc-servers.pp
@@ -58,58 +58,6 @@ class misc::noc-wikimedia {
 	monitor_service { "http": description => "HTTP", check_command => "check_http_url!noc.wikimedia.org!http://noc.wikimedia.org" }
 }
 
-class misc::nfs-server::home {
-	system_role { "misc::nfs-server::home": description => "/home NFS" }
-
-	class backup {
-		cron { home-rsync:
-			require => File["/root/.ssh/home-rsync"],
-			command => '[ -d /home/wikipedia ] && rsync --rsh="ssh -c blowfish-cbc -i /root/.ssh/home-rsync" -azu /home/* db20@tridge.wikimedia.org:~/home/',
-			user => root,
-			hour => 2,
-			minute => 35,
-			weekday => 6,
-			ensure => present;
-		}
-
-		file { "/root/.ssh/home-rsync":
-			owner => root,
-			group => root,
-			mode => 0400,
-			source => "puppet:///private/backup/ssh-keys/home-rsync";
-		}
-	}
-
-	package { nfs-kernel-server:
-		ensure => latest;
-	}
-
-	file { "/etc/exports":
-		require => Package[nfs-kernel-server],
-		mode => 0444,
-		owner => root,
-		group => root,
-		source => "puppet:///files/nfs/exports.home";
-	}
-
-	service { nfs-kernel-server:
-		require => [ Package[nfs-kernel-server], File["/etc/exports"] ],
-		subscribe => File["/etc/exports"];
-	}
-
-	class monitoring {
-		monitor_service { "nfs": description => "NFS", check_command => "check_tcp!2049" }
-	}
-
-	include monitoring
-}
-
-class misc::nfs-server::home::rsyncd {
-	system_role { "misc::nfs-server::home::rsyncd": description => "/home rsync daemon" }
-
-	class { 'generic::rsyncd': config => "home" }
-}
-
 class misc::images::rsyncd {
 	system_role { "misc::images::rsyncd": description => "images rsync daemon" }
 
