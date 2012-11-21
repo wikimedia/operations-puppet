0eae5cf296dbdf580b394b6cfa66f57616b3e66c
Silke Meyer <silke.meyer@wikimedia.de>
2012-11-21 11:35:56 +0100

    Added puppet files for Wikidata on labs
    Change-Id: I4c9a5a8d2eec301284074a3eaefbe3d3014a3036

--- manifests/role/labsmediawiki.pp
+++ manifests/role/labsmediawiki.pp
@@ -13,3 +13,56 @@ class role::mediawiki-install::labs {
 		keep_up_to_date => false
 	}
 }
+
+#  Install Wikidata repo (incl. MediaWiki) from git and keep in sync with git trunk
+class role::wikidata-repo-latest::labs {
+
+	class { "wikidata::singlenode":
+		install_repo => true,
+		install_client => false,
+		keep_up_to_date => true,
+		# require_once stuff for repo
+		role_requires => [ '"$IP/extensions/Diff/Diff.php"', '"$IP/extensions/DataValues/DataValues.php"', '"$IP/extensions/UniversalLanguageSelector/UniversalLanguageSelector.php"', '"$IP/extensions/Wikibase/lib/WikibaseLib.php"', '"$IP/extensions/Wikibase/repo/Wikibase.php"', '"$IP/extensions/Wikibase/repo/ExampleSettings.php"' ],
+		# additional config lines
+		role_config_lines => [ '$wgShowExceptionDetails = true' ]
+	}
+}
+
+#  Install Wikidata repo (incl. Mediawiki) from git and then leave it alone.
+class role::wikidata-repo::labs {
+
+	class { "wikidata::singlenode":
+		install_repo => true,
+		install_client => false,
+		keep_up_to_date => false,
+		role_requires => [ '"$IP/extensions/Diff/Diff.php"', '"$IP/extensions/DataValues/DataValues.php"', '"$IP/extensions/UniversalLanguageSelector/UniversalLanguageSelector.php"', '"$IP/extensions/Wikibase/lib/WikibaseLib.php"', '"$IP/extensions/Wikibase/repo/Wikibase.php"', '"$IP/extensions/Wikibase/repo/ExampleSettings.php"' ],
+		# additional config lines
+		role_config_lines => [ '$wgShowExceptionDetails = true' ]
+	}
+}
+
+#  Install Wikidata client (incl. MediaWiki) from git and keep in sync with git trunk
+class role::wikidata-client-latest::labs {
+
+	class { "wikidata::singlenode":
+		install_client => true,
+		install_repo => false,
+		keep_up_to_date => true,
+		role_requires => [ '"$IP/extensions/Diff/Diff.php"', '"$IP/extensions/DataValues/DataValues.php"', '"$IP/extensions/UniversalLanguageSelector/UniversalLanguageSelector.php"', '"$IP/extensions/Wikibase/lib/WikibaseLib.php"', '"$IP/extensions/Wikibase/client/WikibaseClient.php"' ],
+		# additional config lines
+		role_config_lines => [ '$wgShowExceptionDetails = true', '$wgWBSettings[\'namespaces\'] = array( NS_MAIN, NS_CATEGORY )', '$wgWBSettings[\'siteGroup\'] = \'wiki\'', '$wgWBSettings[\'sort\'] = \'code\'' ]
+	}
+}
+
+#  Install Wikidata (incl. Mediawiki) from git and then leave it alone.
+class role::wikidata-client::labs {
+
+	class { "wikidata::singlenode":
+		install_client => true,
+		install_repo => false,
+		keep_up_to_date => false,
+		role_requires => [ '"$IP/extensions/Diff/Diff.php"', '"$IP/extensions/DataValues/DataValues.php"', '"$IP/extensions/UniversalLanguageSelector/UniversalLanguageSelector.php"', '"$IP/extensions/Wikibase/lib/WikibaseLib.php"', '"$IP/extensions/Wikibase/client/WikibaseClient.php"' ],
+		# additional config lines
+		role_config_lines => [ '$wgShowExceptionDetails = true', '$wgWBSettings[\'namespaces\'] = array( NS_MAIN, NS_CATEGORY )', '$wgWBSettings[\'siteGroup\'] = \'wiki\'', '$wgWBSettings[\'sort\'] = \'code\'' ]
+	}
+}
---
+++ manifests/wikidata.pp
@@ -0,0 +1,137 @@
+# wikidata.pp
+# This file is a modified copy of mediawiki.pp. It will install a mediawiki with Wikibase extensions on labs.
+
+# A one-step class for setting up a single-node MediaWiki install,
+#  running from a Git tree.
+
+# The following are defaults, the exact specifications are in the role definitions
+class wikidata::singlenode( $keep_up_to_date = true,
+							$install_repo = true,
+							$install_client = true,
+							$role_requires = ['"$IP/extensions/Diff/Diff.php"', '"$IP/extensions/DataValues/DataValues.php"', '"$IP/extensions/UniversalLanguageSelector/UniversalLanguageSelector.php"', '"$IP/extensions/Wikibase/lib/WikibaseLib.php"'],
+							$role_config_lines = [ '$wgShowExceptionDetails = true' ]) {
+
+	class { mediawiki::singlenode:
+		keep_up_to_date => $keep_up_to_date,
+		role_requires => $role_requires,
+		role_config_lines => $role_config_lines
+	}
+
+# install either Wikibase repo or client to /srv/mediawiki/extensions
+
+# get the Wikibase extensions and dependencies
+	git::clone { "Diff" :
+		require => [Git::Clone["mediawiki"], Exec["mediawiki_setup"]],
+			directory => "/srv/mediawiki/extensions/Diff",
+			branch => "master",
+			ensure => $keep_up_to_date ? {
+				true => latest,
+				default => present
+			},
+			origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Diff.git",
+	}
+
+	git::clone { "DataValues" :
+		require => [Git::Clone["mediawiki"], Exec["mediawiki_setup"]],
+			directory => "/srv/mediawiki/extensions/DataValues",
+			branch => "master",
+			ensure => $keep_up_to_date ? {
+				true => latest,
+				default => present
+			},
+			origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/DataValues.git",
+	}
+
+	git::clone { "UniversalLanguageSelector" :
+		require => [Git::Clone["mediawiki"], Exec["mediawiki_setup"]],
+			directory => "/srv/mediawiki/extensions/UniversalLanguageSelector",
+			branch => "master",
+			ensure => $keep_up_to_date ? {
+				true => latest,
+				default => present
+			},
+			origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/UniversalLanguageSelector.git",
+	}
+
+	git::clone { "Wikibase" :
+		require => [Git::Clone["mediawiki"], Exec["mediawiki_setup"], Git::Clone["Diff"], Git::Clone["DataValues"]],
+		directory => "/srv/mediawiki/extensions/Wikibase",
+		branch => "master",
+		ensure => $keep_up_to_date ? {
+				true => latest,
+				default => present
+			},
+		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Wikibase.git",
+	}
+
+	exec { "populateSitesTable":
+			require => [Git::Clone["Wikibase"], File["/srv/mediawiki/LocalSettings.php"]],
+			cwd => "/srv/mediawiki/extensions/Wikibase/lib/maintenance",
+			command => "/usr/bin/php populateSitesTable.php",
+			logoutput => "on_failure",
+	}
+
+	exec { "update-script":
+			require => [Git::Clone["Wikibase"], Exec["populateSitesTable"]],
+			cwd => "/srv/mediawiki",
+			command => "/usr/bin/php maintenance/update.php --quick",
+			logoutput => "on_failure",
+	}
+
+	exec { "localisation-cache":
+			require => [Git::Clone["Wikibase"], Exec["populateSitesTable"], Exec["update-script"]],
+			cwd => "/srv/mediawiki",
+			command => "/usr/bin/php maintenance/rebuildLocalisationCache.php",
+			logoutput => "on_failure",
+	}
+
+# Wikibase repo only:
+	if $install_repo == true {
+		exec { "populate_repo":
+			require => [Git::Clone["Wikibase"], Exec["populateSitesTable"], Exec["update-script"]],
+			cwd => "/srv/mediawiki/extensions/Wikibase/repo/maintenance",
+			command => "/usr/bin/php importInterlang.php --verbose --ignore-errors simple simple-elements.csv",
+			logoutput => "on_failure",
+			}
+	}
+
+# Wikibase client only:
+	if $install_client == true {
+		user { "www-data":
+			ensure => present
+		}
+		file { "/etc/logrotate.d/wikidata-replication":
+			ensure => present,
+			source => "puppet:///files/logrotate/wikidata-replication",
+			owner => 'root',
+		}
+		file { "/var/log/wikidata-replication.log":
+			ensure => present,
+			owner => 'www-data',
+			group => 'www-data',
+			mode => '0664',
+		}
+
+		cron {"pollForChanges":
+			ensure => present,
+			command => "/usr/bin/php /srv/mediawiki/extensions/Wikibase/lib/maintenance/pollForChanges.php > /var/log/wikidata-replication.log",
+			user => 'www-data',
+			minute => '*/10',
+		}
+	}
+
+# longterm stuff
+	if $keep_up_to_date == true {
+		exec { 'wikidata_update':
+			require => [Git::Clone["mediawiki"],
+				Git::Clone["Diff"],
+				Git::Clone["DataValues"],
+				Git::Clone["UniversalLanguageSelector"],
+				Git::Clone["Wikibase"],
+				File["/srv/mediawiki/LocalSettings.php"]],
+			command => "/usr/bin/php /srv/mediawiki/maintenance/update.php --quick --conf '/srv/mediawiki/LocalSettings.php'",
+			logoutput => "on_failure",
+		}
+	}
+}
+
