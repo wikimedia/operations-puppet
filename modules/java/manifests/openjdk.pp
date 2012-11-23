ce965e179faa1cdc8c89aab67fa73434fc8ace9a
Antoine Musso <hashar@free.fr>
2012-11-23 11:06:42 +0100

    java module and class to install OpenJDK
    This java::openjdk class would let us install OpenJDK jre and jdk at a
    specific version (6, 7 or whater ubuntu uses per default).  The java
    module could then be expanded to let us install the Oracle java version
    as well.
    
    I only tested it locally using:
    
    	puppet apply --noop --verbose --modulepath=modules \
    		-e "include java::openjdk"
    
    Change-Id: I8123013c41887107490245553e8c132285abbc1d

---
+++ modules/java/manifests/openjdk.pp
@@ -0,0 +1,44 @@
+# Class used to install an OpenJDK package
+#
+# Parameters:
+#  $version  - An OpenJDK version such as 6, 1.7 (default: ubuntu)
+#              Puppet will fail whenever the version is not recognized.
+#  $ensure   - Either 'present', 'latest' or 'absent'. This is passed to the
+#              package {} directive (default: present).
+#  $jdk      - Whether to include the JDK (default: false).
+#
+class java::openjdk(
+	$version='ubuntu',
+	$ensure='present',
+	$jdk=false,
+ ) {
+
+	$pkg_prefix = $version ? {
+
+		'6'      => 'openjdk-6',
+		'1.6'    => 'openjdk-6',
+
+		'7'      => 'openjdk-7',
+		'1.7'    => 'openjdk-7',
+
+		'ubuntu' => 'default',
+
+		# Unrecognized version :(
+		default  => undef,
+	}
+
+	if( !$pkg_prefix ) {
+		fail("Unrecognized OpenJDK version '${version}'")
+	}
+
+	if( $jdk ) {
+		package { "${pkg_prefix}-jdk":
+			ensure => $ensure,
+		}
+	}
+
+	package { "${pkg_prefix}-jre":
+		ensure => $ensure,
+	}
+
+}
