928eb3065c7cb2bf9c8f9f9dedd6198a04a2e3b9
Daniel Zahn <dzahn@wikimedia.org>
2012-11-27 12:57:12 -0800

    remove mysql server from host iron, remove firewall rules for mysql
    this has been added by Ben some time ago, see git lot site.pp for
    some testing purposes. It already had a FIXME on it saying this
    should probably not be on a local mysql. Also i guess these tests
    are not being used by others anymore. And we just want to get rid
    of the extra cruft in misc-servers.pp Can this just go completely
    or other suggestions?
    
    Change-Id: Ic84e92be7627b82fc2287a0cda10bc34ec495e0b

--- manifests/site.pp
+++ manifests/site.pp
@@ -982,9 +982,6 @@ node "iron.wikimedia.org" {
 	# let's see if the swiftcleaner can run here
 	include swift::cleaner
 
-	# run a mysqld instance for testing and dev (not replicated or backed up)
-	include generic::mysql::packages::server
-
 	# include the swift cli so I can call out to swift instances
 	include swift::utilities
 }
