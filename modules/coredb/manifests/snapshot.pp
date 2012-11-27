343039aa4379e9917238865a33e24457aa35548b
pyoungmeister <py@wikimedia.org>
2012-11-26 17:57:22 -0800

    coredb: more reasonable inhritence model
    Change-Id: Id89866e79297bbaf6a3501e8196dc5eee1139262

--- modules/coredb/manifests/snapshot.pp
+++ modules/coredb/manifests/snapshot.pp
@@ -8,24 +8,13 @@ class coredb::snapshot {
 			source => "puppet:///modules/coredb/utils/snaprotate.pl"
 	}
 
-	if $::hostname in $role::coredb::config::topology[$role::coredb::common::shard]['snapshot'] {
-		# TODO: shame. this will stay for now.
-		$snaprotate_extraparams = $::hostname ? {
-			'db26' => "-c 1",
-			default => ""
-		}
-		cron { snaprotate:
-			command => "/usr/local/sbin/snaprotate.pl -a swap -V tank -s data -L 100G $snaprotate_extraparams",
+	cron { 
+		snaprotate:
+			command => "/usr/local/sbin/snaprotate.pl -a swap -V tank -s data -L 100G",
 			require => File["/usr/local/sbin/snaprotate.pl"],
 			user => root,
 			minute => 15,
 			hour => '*/8',
 			ensure => present;
-		}
-	} else {
-		cron { snaprotate:
-			ensure => absent;
-		}
 	}
-
 }
