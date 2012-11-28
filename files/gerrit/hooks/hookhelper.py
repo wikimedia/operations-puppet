8acbe23bde4de262563cbdcb648079120c8203c6
Chad Horohoe <chadh@wikimedia.org>
2012-11-28 08:48:44 -0500

    Reformat gerrit hooks to use more python-esque style
    Mostly whitespace changes, only real change is removing some
    semicolons and an unused import
    
    Change-Id: I09a24ad00e0b16c0ae1cb08f055e15df79ddcba5

--- files/gerrit/hooks/hookhelper.py
+++ files/gerrit/hooks/hookhelper.py
@@ -11,92 +11,92 @@ from optparse import OptionParser
 sys.path.append('/var/lib/gerrit2/review_site/etc')
 import hookconfig
 
+
 class HookHelper:
-	def __init__(self):
-		self.patchsets = {}
-		self.parser = OptionParser(conflict_handler="resolve")
-		self.add_default_options()
+    def __init__(self):
+        self.patchsets = {}
+        self.parser = OptionParser(conflict_handler="resolve")
+        self.add_default_options()
 
-	def add_default_options(self):
-		self.parser.add_option("--change", dest="change")
-		self.parser.add_option("--change-url", dest="changeurl")
-		self.parser.add_option("--project", dest="project")
-		self.parser.add_option("--branch", dest="branch")
+    def add_default_options(self):
+        self.parser.add_option("--change", dest="change")
+        self.parser.add_option("--change-url", dest="changeurl")
+        self.parser.add_option("--project", dest="project")
+        self.parser.add_option("--branch", dest="branch")
 
-	def ssh_exec_command(self, command):
-		ssh = paramiko.SSHClient()
-		ssh.load_host_keys(hookconfig.sshhostkeys)
-		try:
-			ssh.connect(hookconfig.sshhost, hookconfig.sshport, hookconfig.gerrituser, key_filename=hookconfig.sshkey)
-			stdin, stdout, stderr = ssh.exec_command(command)
-			out = stdout.readlines()
-			err = stderr.readlines()
-			ssh.close()
-			return (out,err)
-		except (paramiko.SSHException, socket.error):
-			sys.stderr.write("Failed to connect to %s." % hookconfig.sshhost)
-			traceback.print_exc(file=sys.stderr)
-			return (None, None)
+    def ssh_exec_command(self, command):
+        ssh = paramiko.SSHClient()
+        ssh.load_host_keys(hookconfig.sshhostkeys)
+        try:
+            ssh.connect(hookconfig.sshhost, hookconfig.sshport, hookconfig.gerrituser, key_filename=hookconfig.sshkey)
+            stdin, stdout, stderr = ssh.exec_command(command)
+            out = stdout.readlines()
+            err = stderr.readlines()
+            ssh.close()
+            return (out, err)
+        except (paramiko.SSHException, socket.error):
+            sys.stderr.write("Failed to connect to %s." % hookconfig.sshhost)
+            traceback.print_exc(file=sys.stderr)
+            return (None, None)
 
-	def get_patchsets(self, change):
-		command = 'gerrit query --format=JSON --patch-sets ' + change
-		queryresult, err = self.ssh_exec_command(command)
-		if not queryresult:
-			sys.stderr.write("Couldn't find patchset for change: " + change + "\n") 
-			return False
-		try:
-			self.patchsets[change] = json.loads(queryresult[0])
-			return True
-		except Exception:
-			sys.stderr.write("Couldn't load patchset json for change: " + change + "\n")
-			traceback.print_exc(file=sys.stderr)
-			return False
+    def get_patchsets(self, change):
+        command = 'gerrit query --format=JSON --patch-sets ' + change
+        queryresult, err = self.ssh_exec_command(command)
+        if not queryresult:
+            sys.stderr.write("Couldn't find patchset for change: " + change + "\n")
+            return False
+        try:
+            self.patchsets[change] = json.loads(queryresult[0])
+            return True
+        except Exception:
+            sys.stderr.write("Couldn't load patchset json for change: " + change + "\n")
+            traceback.print_exc(file=sys.stderr)
+            return False
 
-	def get_subject(self, change):
-		if change not in self.patchsets:
-			patchsets_fetched = self.get_patchsets(change)
-			if not patchsets_fetched:
-				return None
-		subject = str(self.patchsets[change]['subject'])
-		if not subject:
-			subject = "(no subject)"
-		return subject
+    def get_subject(self, change):
+        if change not in self.patchsets:
+            patchsets_fetched = self.get_patchsets(change)
+            if not patchsets_fetched:
+                return None
+        subject = str(self.patchsets[change]['subject'])
+        if not subject:
+            subject = "(no subject)"
+        return subject
 
-	def log_to_file(self, project, branch, message, user):
-		filename = self.get_log_filename(project, branch, message)
-		# These users are annoying, ignore them
-		if user in hookconfig.spammyusers:
-			return
-		f = open(filename, 'a')
-		f.write(message)
-		f.close()
+    def log_to_file(self, project, branch, message, user):
+        filename = self.get_log_filename(project, branch, message)
+        # These users are annoying, ignore them
+        if user in hookconfig.spammyusers:
+            return
+        f = open(filename, 'a')
+        f.write(message)
+        f.close()
 
-	def get_log_filename(self, project, branch, message):
-		filename     = None;
-		foundproject = None;
-		if hookconfig.logdir and hookconfig.logdir[-1] == '/':
-			hookconfig.logdir = hookconfig.logdir[0:-1]
-		if project in hookconfig.filenames:
-			foundproject = project
-		if foundproject is None:
-			# Attempt to use the wildcard filters
-			for filter,value in hookconfig.filenames.iteritems():
-				if not "*" in filter:
-					# It is a project name, not a filter!
-					continue
-				# Replace wildcard with a proper regex snippet
-				pattern = re.compile( filter.replace( '*', '.+') )
-				if( pattern.match( project ) ):
-					foundproject = filter
-					break
-		if foundproject is None:
-			foundproject = 'default'
-		if branch not in hookconfig.filenames[foundproject]:
-			branch = 'default'
-		if branch in hookconfig.filenames[foundproject]:
-			filename = hookconfig.filenames[foundproject][branch]
-		else:
-			# Direct assignement such as 'default': 'mediawiki.log'
-			filename = hookconfig.filenames[foundproject]
-		return hookconfig.logdir + "/" + filename
-
+    def get_log_filename(self, project, branch, message):
+        filename = None
+        foundproject = None
+        if hookconfig.logdir and hookconfig.logdir[-1] == '/':
+            hookconfig.logdir = hookconfig.logdir[0:-1]
+        if project in hookconfig.filenames:
+            foundproject = project
+        if foundproject is None:
+            # Attempt to use the wildcard filters
+            for filter, value in hookconfig.filenames.iteritems():
+                if not "*" in filter:
+                    # It is a project name, not a filter!
+                    continue
+                # Replace wildcard with a proper regex snippet
+                pattern = re.compile(filter.replace('*', '.+'))
+                if(pattern.match(project)):
+                    foundproject = filter
+                    break
+        if foundproject is None:
+            foundproject = 'default'
+        if branch not in hookconfig.filenames[foundproject]:
+            branch = 'default'
+        if branch in hookconfig.filenames[foundproject]:
+            filename = hookconfig.filenames[foundproject][branch]
+        else:
+            # Direct assignement such as 'default': 'mediawiki.log'
+            filename = hookconfig.filenames[foundproject]
+        return hookconfig.logdir + "/" + filename
--- files/gerrit/hooks/hookhelper_test.py
+++ files/gerrit/hooks/hookhelper_test.py
@@ -8,10 +8,10 @@ import unittest
 # We shell out to ruby since there is no easy way to expand an erb template
 # using python.
 import os
-ruby_exit_code = os.system( "./generatehookconfig.rb > hookconfig.py" );
+ruby_exit_code = os.system("./generatehookconfig.rb > hookconfig.py")
 if ruby_exit_code != 0:
-	import sys
-	sys.exit( "Could not execute generatehookconfig.rb" );
+    import sys
+    sys.exit("Could not execute generatehookconfig.rb")
 
 # Class we are going to test:
 from hookhelper import HookHelper
@@ -20,97 +20,97 @@ import hookconfig
 
 helper = HookHelper()
 
-class TestLogToFile( unittest.TestCase ):
 
-	def assertLogFile( self, filename, project, branch = 'master' ):
+class TestLogToFile(unittest.TestCase):
+    def assertLogFile(self, filename, project, branch='master'):
 
-		# Ask helper to provide us with the full filename
-		actual = helper.get_log_filename( project, branch, '' )
-		# and now get ride of the common long directory
-		actual = actual.replace( hookconfig.logdir + "/", '' );
+        # Ask helper to provide us with the full filename
+        actual = helper.get_log_filename(project, branch, '')
+        # and now get ride of the common long directory
+        actual = actual.replace(hookconfig.logdir + "/", '')
 
-		self.assertEqual( filename, actual )
+        self.assertEqual(filename, actual)
 
-	def test_operations_puppet( self ):
-		self.assertLogFile( 'operations.log',
-			'operations/puppet',
-			'production' )
-		self.assertLogFile( 'labs.log',
-			'operations/puppet',
-			'test' )
-		self.assertLogFile( 'labs.log',
-			'operations/puppet',
-			'IAmNotConfigured' )
+    def test_operations_puppet(self):
+        self.assertLogFile('operations.log',
+            'operations/puppet',
+            'production')
+        self.assertLogFile('labs.log',
+            'operations/puppet',
+            'test')
+        self.assertLogFile('labs.log',
+            'operations/puppet',
+            'IAmNotConfigured')
 
-	def test_labs_private_to_labs( self ):
-		self.assertLogFile( 'labs.log',
-			'labs/private' )
+    def test_labs_private_to_labs(self):
+        self.assertLogFile('labs.log',
+            'labs/private')
 
-	def test_labs_to_wikimedia_labs( self ):
-		self.assertLogFile( 'labs.log',
-			'labs/someproject' )
+    def test_labs_to_wikimedia_labs(self):
+        self.assertLogFile('labs.log',
+            'labs/someproject')
 
-	def test_operations_software_to_operations( self ):
-		self.assertLogFile( 'operations.log',
-			'operations/software' )
+    def test_operations_software_to_operations(self):
+        self.assertLogFile('operations.log',
+            'operations/software')
 
-	def test_operations_dumps_to_operations( self ):
-		self.assertLogFile( 'operations.log',
-			'operations/dumps' )
+    def test_operations_dumps_to_operations(self):
+        self.assertLogFile('operations.log',
+            'operations/dumps')
 
-	def test_operations_to_operations( self ):
-		self.assertLogFile( 'operations.log',
-			'operations/someProject' )
+    def test_operations_to_operations(self):
+        self.assertLogFile('operations.log',
+            'operations/someProject')
 
-	# Some very specific WMF projects
-	def test_parsoid( self ):
-		self.assertLogFile( 'parsoid.log',
-			'mediawiki/extensions/Parsoid' )
+    # Some very specific WMF projects
+    def test_parsoid(self):
+        self.assertLogFile('parsoid.log',
+            'mediawiki/extensions/Parsoid')
 
-	def test_mobile( self ):
-		self.assertLogFile( 'mobile.log',
-			'mediawiki/extensions/MobileFrontend' )
+    def test_mobile(self):
+        self.assertLogFile('mobile.log',
+            'mediawiki/extensions/MobileFrontend')
 
-	# Semantic MediaWiki related
-	def test_semantic_mediawiki( self ):
-		for repo in [
-			'SemanticFoobar',
-			'Validator',
-			'Maps',
-			'RDFIO',
-			'SolrStore',
-			'SMWFoobar',
-			]:
-			self.assertLogFile( 'semantic-mediawiki.log',
-				'mediawiki/extensions/%s' % repo
-			)
+    # Semantic MediaWiki related
+    def test_semantic_mediawiki(self):
+        for repo in [
+            'SemanticFoobar',
+            'Validator',
+            'Maps',
+            'RDFIO',
+            'SolrStore',
+            'SMWFoobar',
+            ]:
+            self.assertLogFile('semantic-mediawiki.log',
+                'mediawiki/extensions/%s' % repo
+            )
 
-	# Wikidata related
-	def test_wikidata_extensions( self ):
-		for repo in [
-			'Wikibase',
-			'Diff',
-			'DataValues',
-			]:
-			self.assertLogFile( 'wikidata.log',
-				'mediawiki/extensions/%s' % repo
-			)
+    # Wikidata related
+    def test_wikidata_extensions(self):
+        for repo in [
+            'Wikibase',
+            'Diff',
+            'DataValues',
+            ]:
+            self.assertLogFile('wikidata.log',
+                'mediawiki/extensions/%s' % repo
+            )
 
-	def test_core_wikidata_branch( self ):
-		# Wikidata branch is sent to a specific log
-		self.assertLogFile( 'wikidata.log',
-			'mediawiki/core',
-			'Wikidata'
-		)
-		# Make sure mediawiki/core.git@master is still sent to #mediawiki
-		self.assertLogFile( 'mediawiki.log',
-			'mediawiki/core',
-			'master'
-		)
+    def test_core_wikidata_branch(self):
+        # Wikidata branch is sent to a specific log
+        self.assertLogFile('wikidata.log',
+            'mediawiki/core',
+            'Wikidata'
+        )
+        # Make sure mediawiki/core.git@master is still sent to #mediawiki
+        self.assertLogFile('mediawiki.log',
+            'mediawiki/core',
+            'master'
+        )
 
-	def test_catchall_to_mediawiki( self ):
-		self.assertLogFile( 'mediawiki.log',
-			'department/project' )
+    def test_catchall_to_mediawiki(self):
+        self.assertLogFile('mediawiki.log',
+            'department/project')
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
