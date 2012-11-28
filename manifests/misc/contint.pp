b3bf82c91ba7859f768db85d8a6870a856fd3cb2
Antoine Musso <hashar@free.fr>
2012-11-28 19:48:07 +0100

    install python pep8 on contint server
    pep8 is a python linter which we are going to use to lint changes being
    submitted to Gerrit.
    
    Change-Id: Ie077da22facccd510ea17856872c0842cdaf02b2

--- manifests/misc/contint.pp
+++ manifests/misc/contint.pp
@@ -74,6 +74,8 @@ class misc::contint::test {
 
 		include generic::packages::maven
 
+		include python::pep8
+
 		# split up packages into groups a bit for readability and flexibility ("ensure present" vs. "ensure latest" ?)
 
 		$CI_PHP_packages = [ "libapache2-mod-php5", "php-apc", "php5-cli", "php5-curl", "php5-gd", "php5-intl", "php5-mysql", "php-pear", "php5-sqlite", "php5-tidy", "php5-pgsql" ]
