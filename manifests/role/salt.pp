d7c3bcda43b9bf590e037e2e05a2355024a8c0c8
Ryan Lane <rlane@wikimedia.org>
2012-11-28 17:19:07 -0800

    Add module, runner and add module directory
    Change-Id: I0c758868a3df8a7b2065241c1ec04e8d099968fc

--- manifests/role/salt.pp
+++ manifests/role/salt.pp
@@ -10,6 +10,7 @@ class role::salt::masters::labs {
 
 	$salt_file_roots = {"base"=>["/srv/salt"]}
 	$salt_pillar_roots = {"base"=>["/srv/pillar"]}
+	$salt_module_roots = {"base"=>["/srv/salt/_modules"]}
 
 	class { "salt::master":
 		salt_runner_dirs => "['/srv/runners']",
@@ -23,6 +24,7 @@ class role::salt::masters::labs {
 	salt::master_environment{ "base":
 		salt_file_roots => $salt_file_roots,
 		salt_pillar_roots => $salt_pillar_roots,
+		salt_module_roots => $salt_module_roots,
 	}
 
 }
