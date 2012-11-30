e35f9cbdac6f5b6f07f46b52f9294e9ff6201621
Max Semenik <maxsem.wiki@gmail.com>
2012-11-30 13:23:32 +0400

    WIP: OSM module
    Change-Id: I5930bbc5007580708886927ced2f78264fb7c45b

---
+++ modules/osm/manifests/tileserver.pp
@@ -0,0 +1,44 @@
+class osm::tileserver(
+	$osm_host,
+	$postgres_host,
+	$htcp_host = undef,
+	$admin_email,
+	$tile_expiry = 45
+	) {
+
+	class { "osm::tileserver::files":
+		osm_host => $osm_host,
+		admin_email => $admin_email,
+		htcp_host => $htcp_host,
+	}
+
+	package {
+		[ "libapache2-mod-tile", "renderd", "openstreetmap-mapnik-stylesheet-data" ]:
+			ensure => latest,
+	}
+	
+	service {
+		"apache2":
+			ensure => running,
+			enable => "true",
+			require => Package["libapache2-mod-tile"],
+			hasrestart => "true",
+	}
+
+	service {
+		"renderd":
+			ensure => running,
+			enable => "true",
+			require => Package["renderd"],
+			hasrestart => "true",
+	}
+
+	if ( $tile_expiry > 0 ) {
+		cron { "delete-old-jetty-logs":
+			command => "/usr/bin/find /var/lib/mod_tile/* -mtime +$tile_expiry -delete",
+			user => "root",
+			hour => 3,
+			ensure => present,
+		}
+	}
+}
