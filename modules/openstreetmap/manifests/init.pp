ba0a5ee2781df256a3f5d4ae356c3be0dc27ab2a
Max Semenik <maxsem.wiki@gmail.com>
2012-11-30 22:37:29 +0400

    WIP: OSM module
    Change-Id: I5930bbc5007580708886927ced2f78264fb7c45b

---
+++ modules/openstreetmap/manifests/init.pp
@@ -0,0 +1,36 @@
+class openstreetmap::tileserver {
+	system_role { "openstreetmap::tileserver": description => "OpenStreetMap tile server" }
+	
+	include generic::apache::no-default-site
+	
+	package {
+		[ "libapache2-mod-tile", "renderd", "openstreetmap-mapnik-stylesheet-data", "osmosis", "osm2pgsql" ]:
+			ensure => latest,
+	}
+	
+	systemuser{ "osm": name => "osm" }
+
+	service {
+		"apache2":
+			ensure => running,
+			enable => "true",
+			require => Package["libapache2-mod-tile"],
+			hasrestart => "true",
+	}
+
+	service {
+		"renderd":
+			ensure => running,
+			enable => "true",
+			require => Package["renderd"],
+			hasrestart => "true",
+	}
+
+	cron { "osm_db_update":
+		ensure	=> present,
+		command => "/usr/bin/openstreetmap-update-db > /dev/null 2>&1",
+		user => "osm",
+		minute => [5, 20, 35, 50],
+		require => File["/usr/bin/openstreetmap-update-db"],
+	}
+}
