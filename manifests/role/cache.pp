f5ced05b9fac701d6de0867a67123bbf0b0f9e7b
asher <afeldman@wikimedia.org>
2012-11-30 12:04:15 -0800

    analytics wants varnishncsa fields to be tab delimited
    Change-Id: I3eda0119c6c32fe177104c8101de3f32a3e42eab

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -553,7 +553,7 @@ class role::cache {
 				port => "8422",
 				instance_name => "",
 				cli_args => '-m RxURL:^/event\.gif -D',
-				log_fmt => "%U %q %{Host}i %t %h %{X-Forwarded-For}i %{Referer}i %{Accept-Language}i %{Cookie}i %{X-WAP-Profile}i %{User-agent}i %l %n",
+				log_fmt => "%U	%q	%{Host}i	%t	%h	%{X-Forwarded-For}i	%{Referer}i	%{Accept-Language}i	%{Cookie}i	%{X-WAP-Profile}i	%{User-agent}i	%l	%n",
 				monitor => false,
 			}
 		}
