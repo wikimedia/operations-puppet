d3d29edb86049124d2424046348223e9d740ef04
Max Semenik <maxsem.wiki@gmail.com>
2012-11-30 22:37:29 +0400

    WIP: OSM module
    Change-Id: I5930bbc5007580708886927ced2f78264fb7c45b

---
+++ modules/osm/manifests/init.pp
@@ -0,0 +1,27 @@
+class osm::tileserver {
+	system_role { "openstreetmap::tileserver": description => "OpenStreetMap tile server" }
+	
+	include generic::apache::no-default-site,
+		osm::tileserver::files
+	
+	package {
+		[ "libapache2-mod-tile", "renderd", "openstreetmap-mapnik-stylesheet-data" ]:
+			ensure => latest,
+	}
+	
+	service {
+		"apache2":
+			ensure => running,
+			enable => "true",
+			require => Package["libapache2-mod-tile"],
+			hasrestart => "true",
+	}
+
+	service {
+		"renderd":
+			ensure => running,
+			enable => "true",
+			require => Package["renderd"],
+			hasrestart => "true",
+	}
+}
