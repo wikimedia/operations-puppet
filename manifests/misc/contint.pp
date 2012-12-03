cc80771c8bcdc4ecedcc02d02de737ebf758f7be
jarry1250 <jarry1250@gmail.com>
2012-12-03 17:41:15 +0000

    Add librsvg to contint puppet manifest
    Having librsvg on gallium and/or any other continuious integration
    servers allows SVGs to be used in unit tests.
    
    Change-Id: I74b9a095fc5e9335f450429e8b9f0483cc5c61d4

--- manifests/misc/contint.pp
+++ manifests/misc/contint.pp
@@ -78,7 +78,7 @@ class misc::contint::test {
 
 		$CI_PHP_packages = [ "libapache2-mod-php5", "php-apc", "php5-cli", "php5-curl", "php5-gd", "php5-intl", "php5-mysql", "php-pear", "php5-sqlite", "php5-tidy", "php5-pgsql" ]
 		$CI_DB_packages  = [ "mysql-server", "sqlite3", "postgresql" ]
-		$CI_DEV_packages = [ "imagemagick" ]
+		$CI_DEV_packages = [ "imagemagick", "librsvg2-bin" ]
 
 		package { $CI_PHP_packages:
 			ensure => present;
