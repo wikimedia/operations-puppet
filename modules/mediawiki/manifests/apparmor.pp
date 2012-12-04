1ef47a7a080fd5661f7ecc82ed27b99a47ed5670
Jan Gerber <jgerber@wikimedia.org>
2012-12-04 17:09:22 +0100

    Add apparmor profiles for avconv/ffmpeg2theora
    Add apparmor profiles to production environment to
    restrict avconv and ffmpeg2theora to only access files in /tmp
    avconv also needs network access to extract thumbnails from swift
    
    Bug:T42099
    Change-Id: I6e0782b4bd72d4869a11fd911b7a18d65db749d3

---
+++ modules/mediawiki/manifests/apparmor.pp
@@ -0,0 +1,19 @@
+class mediawiki::apparmor {
+
+    file {
+        '/etc/apparmor.d/usr.bin.avconv':
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0644',
+            source => 'puppet:///modules/mediawiki/apparmor/usr.bin.avconv',
+    }
+
+    file {
+        '/etc/apparmor.d/usr.bin.ffmpeg2theora':
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0644',
+            source => 'puppet:///modules/mediawiki/apparmor/usr.bin.ffmpeg2theora',
+    }
+
+}
