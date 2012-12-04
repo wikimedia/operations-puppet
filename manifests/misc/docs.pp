df32486d2304a8fc7a30b81a131f5b616e4c703b
Andrew Bogott <abogott@wikimedia.org>
2012-12-04 22:21:14 +0000

    Add class for a doc site
    Change-Id: Iee3b2f7e692d59792e22a6a8bea5e7a1363bab92

---
+++ manifests/misc/docs.pp
@@ -0,0 +1,12 @@
+class misc::docsite {
+	file {
+		"/etc/apache2/sites-available/doc.mediawiki.org":
+		path => "/etc/apache2/sites-available/doc.mediawiki.org",
+		mode => 0444,
+		owner => root,
+		group => root,
+		source => "puppet:///files/apache/sites/doc.mediawiki.org";
+	}
+
+	apache_site { integration: name => "doc.mediawiki.org" }
+}
