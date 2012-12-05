26a556ca878c49159b7278da7f988973a311c4fd
Daniel Zahn <dzahn@wikimedia.org>
2012-12-05 11:48:14 -0800

    add simple index.html for doc.wm site
    Change-Id: I2bb0b1a82d0e0e589589ee02bfd3bc3489234239

--- manifests/misc/docs.pp
+++ manifests/misc/docs.pp
@@ -9,6 +9,11 @@ class misc::docsite {
 			source => 'puppet:///files/apache/sites/doc.wikimedia.org';
 		'/srv/org/wikimedia/doc':
 			ensure => 'directory';
+		'/srv/org/wikimediaq/doc/index.html':
+			mode => 0444,
+			ensure => 'present',
+			source => 'puppet:///files/misc/jenkins/doc_index.html',
+			require => File["/srv/org/wikimedia/doc"];
 	}
 
 	apache_site { docs: name => 'doc.wikimedia.org' }
