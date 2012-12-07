19a14a70a1980dadf76ba88fcb1e55eedb8579a9
lcarr <lcarr@wikimedia.org>
2012-12-07 10:54:50 -0800

    Chanigng class name to icinga::monitor:;configuration::variables
    Change-Id: I554c6d8e75af634bdb1d2cc022d20a389b7a1d19

--- manifests/misc/icinga.pp
+++ manifests/misc/icinga.pp
@@ -32,7 +32,7 @@ class icinga::monitor {
 
 # Nagios/icinga configuration files
 
-class icinga::configuration::variables {
+class icinga::monitor::configuration::variables {
 
 	#This variable declares the monitoring hosts
 	#It is called master hosts as monitor_host is already
@@ -45,22 +45,22 @@ class icinga::configuration::variables {
 
 	# puppet_hosts.cfg must be first
 	$puppet_files = [
-			  "${icinga::configuration::variables::icinga_config_dir}/puppet_hostgroups.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/puppet_servicegroups.cfg"]
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/puppet_hostgroups.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/puppet_servicegroups.cfg"]
 
 	$static_files = [
-			  "${icinga::configuration::variables::icinga_config_dir}/puppet_hostextinfo.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/puppet_services.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/icinga.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/cgi.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/checkcommands.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/contactgroups.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/contacts.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/migration.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/misccommands.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/resource.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/timeperiods.cfg",
-			  "${icinga::configuration::variables::icinga_config_dir}/htpasswd.users"]
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/puppet_hostextinfo.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/puppet_services.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/icinga.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/cgi.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/checkcommands.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/contactgroups.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/contacts.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/migration.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/misccommands.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/resource.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/timeperiods.cfg",
+			  "${icinga::monitor::configuration::variables::icinga_config_dir}/htpasswd.users"]
 
 }
 class icinga::monitor::apache {
