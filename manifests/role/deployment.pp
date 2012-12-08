1d0bd7a3ef7ca347a858cb652fa8ee6e2ae60baa
Ryan Lane <rlane@wikimedia.org>
2012-12-08 15:03:26 -0800

    Remove denies from config - it's an incorrect rule
    Change-Id: I66e2d62f27678d3639086fbf4ac7aa4a8fae9af3

--- manifests/role/deployment.pp
+++ manifests/role/deployment.pp
@@ -75,7 +75,6 @@ class role::deployment::deployment_servers {
     port		=> 80,
     docroot		=> "/srv/deployment",
     docroot_dir_allows  => ['10.0.0.0/16','10.64.0.0/22','10.64.16.0/24','208.80.152.0/24'],
-    docroot_dir_denies  => ['all'],
     serveradmin		=> "noc@wikimedia.org",
     configure_firewall 	=> false,
   }
