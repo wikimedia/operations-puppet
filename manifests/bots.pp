f555fd956f50ca36cb0d3a317a6071ccf32bc934
Andrew Bogott <abogott@wikimedia.org>
2012-12-11 00:04:17 +0000

    Puppetize adminbot
    Add a role for labs-morebots
    
    Change-Id: Iaf09a74be5fc4f3ccc9cb836c3d017a554b492fd

---
+++ manifests/bots.pp
@@ -0,0 +1,41 @@
+# Installs a logbot.  Channel, name, etc. are configurable.
+#  at the moment, only one bot per instance, and only one
+#  log page per bot will work.
+class bots::logbot( $ensure = 'present',
+			     $targets = '()',
+			     $nick = '("anonbot")',
+			     $nickserv = 'nickserv',
+			     # For security reasons, nick_password, wiki_user and wiki_pass
+			     #  are not puppetized.  Instead, insert them into this
+			     #  file and it will be included in the config, python-style.
+			     $password_include_file = '',
+			     $network = 'irc.freenode.net',
+			     $port = 6667,
+			     $author_map = '{}',
+			     $title_map = '{}',
+			     $wiki_connection = '("https","labsconsole.wikimedia.org")',
+			     $wiki_path = "/w/",
+			     $wiki_user = "anon",
+			     $wiki_domain = "",
+			     $wiki_page = "",
+			     $wiki_header_depth = 3,
+                             $wiki_category = "SAL") {
+
+	package { adminbot:
+		ensure => $ensure;
+	}
+
+	file {
+		"/etc/adminbot/config.py":
+			mode => 644,
+			owner => root,
+			group => root,
+			content => template('adminbot/config.py.erb'),
+			ensure => present;
+		"/var/run/adminbot":
+			mode => 644,
+			owner => adminbot,
+			group => root,
+			ensure => directory;
+	}
+}
