947b356f546c7ae8d7d1c691c358e3205e947598
asher <afeldman@wikimedia.org>
2012-12-10 22:24:25 -0800

    * New optional compatibility with XtraDB >= 1.1.8 as used in MariaDB 5.5 * Default is to expect InnoDB as in the MysSQL 5.1 + Facebook patchset * Not currently compatible with Oracle 5.1/5.5 releases
    Change-Id: Ifeeefe7cdbfe5e6998cda6cd73d3889a96c0d35f

--- files/ganglia/plugins/mysql.py
+++ files/ganglia/plugins/mysql.py
@@ -2,7 +2,9 @@
 The MIT License
 
 Copyright (c) 2008 Gilad Raphaelli <gilad@raphaelli.com>
-Adapted for 5.1+ InnoDB status 2012 Asher Feldman <asher@wikimedia.org>
+
+Adapted for 5.1-facebook, 5.5, and other additions
+2012 Asher Feldman <asher@wikimedia.org>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -60,7 +62,7 @@ REPORT_SLAVE  = True
 
 MAX_UPDATE_TIME = 15
 
-def update_stats(get_innodb=True, get_master=True, get_slave=True):
+def update_stats(get_innodb=True, get_master=True, get_slave=True, innodb_version="51fb"):
 	logging.debug('updating stats')
 	global last_update
 	global mysql_stats, mysql_stats_last
@@ -111,7 +113,7 @@ def update_stats(get_innodb=True, get_master=True, get_slave=True):
 		if get_innodb:
 			cursor = conn.cursor(MySQLdb.cursors.Cursor)
 			cursor.execute("SHOW ENGINE INNODB STATUS")
-			innodb_status = parse_innodb_status(cursor.fetchone()[2].split('\n'))
+			innodb_status = parse_innodb_status(cursor.fetchone()[2].split('\n'), innodb_version)
 			cursor.close()
 			logging.debug('innodb_status: ' + str(innodb_status))
 
@@ -340,6 +342,7 @@ def metric_init(params):
 	REPORT_INNODB = str(params.get('get_innodb', True)) == "True"
 	REPORT_MASTER = str(params.get('get_master', True)) == "True"
 	REPORT_SLAVE  = str(params.get('get_slave', True)) == "True"
+	INNODB_VERSION = str(params.get('innodb_version', "51fb"))
 
 	logging.debug("init: " + str(params))
 
@@ -971,7 +974,7 @@ def metric_init(params):
 		)
 
 	descriptors = []
-	update_stats(REPORT_INNODB, REPORT_MASTER, REPORT_SLAVE)
+	update_stats(REPORT_INNODB, REPORT_MASTER, REPORT_SLAVE, INNODB_VERSION)
 
 	time.sleep(MAX_UPDATE_TIME)
 	update_stats(REPORT_INNODB, REPORT_MASTER, REPORT_SLAVE)
