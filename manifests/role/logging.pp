6e3286809a6f333f8b0198470c7f08cd6ee51d0e
Max Semenik <maxsem.wiki@gmail.com>
2012-12-12 04:19:27 +0400

    A short script for viewing exceptions
    Based on fatalmonitor
    
    Change-Id: If26c9a164f47cb98d98108f178af4b0cb49d5af4

--- manifests/role/logging.pp
+++ manifests/role/logging.pp
@@ -105,6 +105,13 @@ class role::logging::mediawiki($monitor = true, $log_directory = '/home/wikipedi
         provider  => 'upstart',
         subscribe => File['/usr/local/bin/wfdebug-ganglia.py', '/etc/init/wfdebug-ganglia.conf'],
     }
+
+	file { "/usr/local/bin/exceptionmonitor":
+		owner => root,
+		group => root,
+		mode => 0555,
+		content => template("misc/exceptionmonitor.erb");
+	}
 }
 
 class role::beta::logging::mediawiki {
