db708463836e3b8bf9e28c5e5098f16c1fe5ad45
Ariel T. Glenn <ariel@wikimedia.org>
2012-12-12 12:25:48 +0200

    wrap flaggedrev stats script with MWScript.php
    Change-Id: I09be19de87e25836d76c0af5cb19056aab2fe50a

--- files/misc/scripts/wikimedia-periodic-update.sh
+++ files/misc/scripts/wikimedia-periodic-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for db in `</home/wikipedia/common/flaggedrevs.dblist`;do
 	echo $db
-	php -n /home/wikipedia/common/php/extensions/FlaggedRevs/maintenance/updateStats.php $db
+	php -n /home/wikipedia/common/multiversion/MWScript.php extensions/FlaggedRevs/maintenance/updateStats.php $db
 done
