78b556ed3fec6fdb783e413561f4a34f3af5213e
Ariel T. Glenn <ariel@wikimedia.org>
2012-12-13 13:57:04 +0200

    php -n wasn't loading the pecl memcache module for flaggedrevs, fixed
    Change-Id: I8c8421af11478013b2158d819896f2b2c37d02f1

--- files/misc/scripts/wikimedia-periodic-update.sh
+++ files/misc/scripts/wikimedia-periodic-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for db in `</home/wikipedia/common/flaggedrevs.dblist`;do
 	echo $db
-	php -n /home/wikipedia/common/multiversion/MWScript.php extensions/FlaggedRevs/maintenance/updateStats.php $db
+	php /home/wikipedia/common/multiversion/MWScript.php extensions/FlaggedRevs/maintenance/updateStats.php $db
 done
