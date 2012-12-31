59eb0a63ed144176fee623c3968da95f7a6519e7
asher <afeldman@wikimedia.org>
2012-12-31 11:51:12 -0800

    missing class parameter
    Change-Id: Id4362fdc594ccceec042c1e04676939caa84b8e0

--- manifests/memcached.pp
+++ manifests/memcached.pp
@@ -1,9 +1,10 @@
 # memcached.pp
 
-class memcached ($memcached_size = '2000', $memcached_port = '11000', $memcached_ip = '0.0.0.0', $memcached_options = {}) {
+class memcached ($memcached_size = '2000', $memcached_port = '11000', $memcached_ip = '0.0.0.0',
+		version => "present", $memcached_options = {}) {
 
 	class { "memcached::config": memcached_size => "$memcached_size", memcached_port => "$memcached_port",
-		memcached_ip => "$memcached_ip", version => "present", memcached_options => $memcached_options }
+		memcached_ip => "$memcached_ip", version => "$version", memcached_options => $memcached_options }
 
 	package { memcached:
 		ensure => $version;
