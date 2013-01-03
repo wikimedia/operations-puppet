eba49c4034705937b5bbff9b4ee5461e45f8cac0
Reedy <reedy@wikimedia.org>
2013-01-03 21:35:45 +0000

    Add script to update the interwiki cache for all active MediaWiki versions
    Change-Id: I84ec9fd7c701a4c2d61779708da91ad3121edb64

--- manifests/misc/deployment.pp
+++ manifests/misc/deployment.pp
@@ -196,6 +196,11 @@ class misc::deployment::scripts {
 			group => root,
 			mode => 0555,
 			source => "puppet:///files/misc/scripts/set-group-write2";
+		"${scriptPath}/updateinterwikicache":
+			owner => root,
+			group => root,
+			mode => 0555,
+			source => "puppet:///files/misc/scripts/updateinterwikicache";
 
 		# Manpages
 		# Need to be generated manually using make in files/misc/scripts
