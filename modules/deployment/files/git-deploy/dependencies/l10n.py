775a1bdc188bbf952ad76de691f92cc849cbcfd8
Ryan Lane <rlane@wikimedia.org>
2013-01-08 13:54:29 -0800

    Add support for dependent repos
    Change-Id: I5637939351df885391db36488adfc82bfdba20b4

---
+++ modules/deployment/files/git-deploy/dependencies/l10n.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+def main():
+    return 0
+
+# NOOP for now
+if __name__ == "__main__":
+    main()
---
+++ modules/deployment/files/git-deploy/hooks/depends.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import os
+import deploylib
+
+def main():
+    prefix = os.environ['DEPLOY_ROLLOUT_PREFIX']
+    tag = os.environ['DEPLOY_ROLLOUT_TAG']
+    force = os.environ['DEPLOY_FORCE']
+    #TODO: Use this message to notify IRC
+    #msg = os.environ['DEPLOY_DEPLOY_TEXT']
+
+    deploylib.update_repos(prefix, tag)
+    # In general, for dependent repos, the parent repo is handling
+    # fetch and checkout. Some dependent repos also update outside
+    # of their parent repos. If the repo forces a sync, then we should
+    # handle it.
+    if force:
+        deploylib.fetch(prefix, tag)
+        deploylib.checkout(prefix, tag, "True")
+
+if __name__ == "__main__":
+    main()
