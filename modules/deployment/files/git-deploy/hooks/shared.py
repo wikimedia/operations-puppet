415e397b1c5020d30bc72086ceccd6268aedaafc
Ryan Lane <rlane@wikimedia.org>
2013-01-08 13:54:29 -0800

    Add support for dependent repos
    Change-Id: I5637939351df885391db36488adfc82bfdba20b4

---
+++ modules/deployment/files/git-deploy/hooks/shared.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+import deploylib
+
+def main():
+    prefix = os.environ['DEPLOY_ROLLOUT_PREFIX']
+    tag = os.environ['DEPLOY_ROLLOUT_TAG']
+    force = os.environ['DEPLOY_FORCE']
+    if force:
+        force = "True"
+    else:
+        force = "False"
+    #TODO: Use this message to notify IRC
+    #msg = os.environ['DEPLOY_DEPLOY_TEXT']
+
+    deploylib.update_repos(prefix, tag)
+    deploylib.fetch(prefix, tag)
+    deploylib.checkout(prefix, tag, force)
+
+if __name__ == "__main__":
+    main()
--- modules/deployment/manifests/deployment_repo_sync_hook_link.pp
+++ modules/deployment/manifests/deployment_repo_sync_hook_link.pp
@@ -1,6 +1,6 @@
-define deployment::deployment_repo_sync_hook_link($deployment_global_hook_dir="/var/lib/git-deploy/hooks") {
-  file { "${deployment_global_hook_dir}/sync/$title.sync":
+define deployment::deployment_repo_sync_hook_link($deployment_global_hook_dir="/var/lib/git-deploy/hooks", $target) {
+  file { "${deployment_global_hook_dir}/sync/${title}.sync":
     ensure => link,
-    target => "${deployment_global_hook_dir}/sync/shared.py";
+    target => "${deployment_global_hook_dir}/sync/${target}";
   }
 }
