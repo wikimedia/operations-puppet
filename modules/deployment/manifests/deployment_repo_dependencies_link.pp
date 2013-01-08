c014bda901407ef4875f233c4f93ea7cc18de53d
Ryan Lane <rlane@wikimedia.org>
2013-01-08 15:27:44 -0800

    Follow up to I56379393
    Change-Id: I7d921df75e2c2f0de25c0ea24af780c4b664fb3c

--- modules/deployment/manifests/deployment_repo_dependencies_link.pp
+++ modules/deployment/manifests/deployment_repo_dependencies_link.pp
@@ -1,6 +1,6 @@
 define deployment::deployment_repo_dependencies_link($deployment_dependencies_dir="/var/lib/git-deploy/dependencies", $target) {
-  file { "${deployment_dependencies_dir}/sync/${title}.dep":
+  file { "${deployment_dependencies_dir}/${title}.dep":
     ensure => link,
-    target => "${deployment_dependencies_dir}/sync/${target}";
+    target => "${deployment_dependencies_dir}/${target}";
   }
 }
