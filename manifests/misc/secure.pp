76f08ad03aab99c894cac4a72f32470a27575ace
Tim Starling <tstarling@wikimedia.org>
2013-01-10 16:46:09 +1100

    Don't proxy secure.wikimedia.org to the main apache cluster
    secure.wikimedia.org is meant to be just a few rewrite rules now. So if
    a requested URL doesn't match one of the rewrite rules, don't proxy it
    to the apaches, just give a 404. This will allow us to remove
    secure.wikimedia.org from the configuration of the main apache cluster.
    
    Also remove mod_proxy from the list of required modules.
    
    Change-Id: Id9493e8c233a91fc635caf3bf0973438e9e9bc37

--- manifests/misc/secure.pp
+++ manifests/misc/secure.pp
@@ -2,10 +2,6 @@
 # https://secure.wikimedia.org | http://en.wikipedia.org/wiki/Wikipedia:Secure_server
 class misc::secure {
 	system_role { "misc::secure": description => "secure.wikimedia.org" }
-
 	apache_module { rewrite: name => "rewrite" }
-	apache_module { proxy: name => "proxy" }
-	apache_module { proxy_http: name => "proxy_http" }
-
 	apache_site { secure: name => "secure.wikimedia.org" }
 }
