d39f5b1cdb76ac6908bb0763c0ff1e524e299b90
Antoine Musso <hashar@free.fr>
2013-01-10 14:33:10 +0100

    contint: mount dir must exist /var/lib/jenkins/tmpfs
    Puppet does not create the mount destination for us so we need a file {}
    definition to get it created before mounting. Puppet error:
       mount point /var/lib/jenkins/tmpfs does not exist
    
    Change-Id: I35854dc1559c88abedb57c9d11566f8fe70f195d

--- manifests/misc/contint.pp
+++ manifests/misc/contint.pp
@@ -176,12 +176,20 @@ class misc::contint::test {
 		}
 
 		# Setup tmpfs to write SQLite files to
+		file { '/var/lib/jenkins/tmpfs':
+			ensure => directory,
+			mode => 0755,
+			owner => jenkins,
+			group => jenkins,
+			require => [ User['jenkins'], Group['jenkins'] ];
+		}
+
 		mount { '/var/lib/jenkins/tmpfs':
 			ensure => mounted,
 			device => 'tmpfs',
 			fstype => 'tmpfs',
 			options => 'noatime,defaults,size=512M,mode=755,uid=jenkins,gid=jenkins',
-			require => [ User['jenkins'], Group['jenkins'] ];
+			require => [ User['jenkins'], Group['jenkins'], File['/var/lib/jenkins/tmpfs'] ];
 		}
 
 		# nagios monitoring
