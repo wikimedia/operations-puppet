49eab8f659b1acb5c424dea94c5590c93869c471
Ryan Lane <rlane@wikimedia.org>
2013-01-09 22:12:34 -0800

    Make secure actually pull its config from puppet
    Change-Id: I1dff835fc69a87067443d11733a08276b7b16d6d

--- manifests/misc/secure.pp
+++ manifests/misc/secure.pp
@@ -3,5 +3,14 @@
 class misc::secure {
 	system_role { "misc::secure": description => "secure.wikimedia.org" }
 	apache_module { rewrite: name => "rewrite" }
+
+	file {
+		"/etc/apache2/sites-available/secure.wikimedia.org":
+			source => "puppet:///files/apache/sites/secure.wikimedia.org",
+			owner => root,
+			group => root,
+			mode => 0444;
+	}
+
 	apache_site { secure: name => "secure.wikimedia.org" }
 }
