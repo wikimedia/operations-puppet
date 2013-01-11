ef1c7898a9169773feca5f668c350e2f57fdbcc1
Ryan Lane <rlane@wikimedia.org>
2013-01-10 21:31:56 -0800

    Add back in accidentally removed functions
    Change-Id: I0a1ed61bbd569f936719664b1a98047ea8eb2940

--- modules/deployment/files/git-deploy/hooks/shared.py
+++ modules/deployment/files/git-deploy/hooks/shared.py
@@ -17,11 +17,15 @@ def main():
     log = raw_input("Log message: ")
     serv = redis.Redis(host='localhost', port=6379, db=0)
     serv.rpush("deploy:log", "!log {0} started synchronizing {1} '{2}'".format(getpass.getuser(), tag, log))
+
+    deploylib.update_repos(prefix, tag)
+    deploylib.fetch(prefix)
     if not deploylib.ask(prefix, 'fetch'):
         return 1
     deploylib.checkout(prefix, force)
     if not deploylib.ask(prefix, 'checkout', force):
         return 1
+
     serv.rpush("deploy:log", "!log {0} synchronized {1} '{2}'".format(getpass.getuser(), tag, log))
 
 if __name__ == "__main__":
