5fb6e14b4411927cca23314cff27bfdad2e6ec40
SilkeMeyer <silke.meyer@wikimedia.de>
2013-01-18 17:33:50 +0100

    Variables for the client config
    Change-Id: If03ff40448fea45ada373ff6fc168459dc3edcd8

--- manifests/role/labsmediawiki.pp
+++ manifests/role/labsmediawiki.pp
@@ -63,6 +63,8 @@ class role::wikidata-client-latest::labs {
 	class { "wikidata::singlenode":
 		install_client => true,
 		install_repo => false,
+		repo_ip => $wikidata_repo_ip,
+		repo_url => $wikidata_repo_ip,
 		database_name => "client",
 		ensure => latest,
 		install_path => "/srv/mediawiki",
@@ -72,9 +74,9 @@ class role::wikidata-client-latest::labs {
 		],
 		# additional config lines
 		role_config_lines => [
-		'$wgWBSettings[\'repoUrl\'] = "//wikidata-puppet-repoo.pmtpa.wmflabs"',
-		'// Load Balancer',
-		'$wgLBFactoryConf = array( \'class\' => \'LBFactory_Multi\', \'serverTemplate\' => array( \'dbname\' => $wgDBname, \'user\' => $wgDBuser, \'password\' => $wgDBpassword, \'type\' => \'mysql\', \'flags\' => DBO_DEFAULT | DBO_DEBUG, ), \'sectionLoads\' => array( \'DEFAULT\' => array( \'localhost\' => 1, ), \'repo\' => array( \'local1\' => 1, ), ), \'sectionsByDB\' => array( $wgDBname => \'DEFAULT\', \'repo\' => \'repo\', ), \'hostsByName\' => array( \'localhost\' => \'127.0.0.1:3306\', \'local1\' => \'10.4.0.23:3306\', ), \'masterTemplateOverrides\' => array( \'fakeMaster\' => true ), )',
+		#'$wgWBSettings[\'repoUrl\'] = "//wikidata-puppet-repoo.pmtpa.wmflabs"',
+		#'// Load Balancer',
+		#'$wgLBFactoryConf = array( \'class\' => \'LBFactory_Multi\', \'serverTemplate\' => array( \'dbname\' => $wgDBname, \'user\' => $wgDBuser, \'password\' => $wgDBpassword, \'type\' => \'mysql\', \'flags\' => DBO_DEFAULT | DBO_DEBUG, ), \'sectionLoads\' => array( \'DEFAULT\' => array( \'localhost\' => 1, ), \'repo\' => array( \'local1\' => 1, ), ), \'sectionsByDB\' => array( $wgDBname => \'DEFAULT\', \'repo\' => \'repo\', ), \'hostsByName\' => array( \'localhost\' => \'127.0.0.1:3306\', \'local1\' => \'10.4.0.23:3306\', ), \'masterTemplateOverrides\' => array( \'fakeMaster\' => true ), )',
 		'$wgSiteNotice = \'<div style="font-size: 90%; background: #FFCC33; padding: 1ex; border: #940 dotted; margin-top: 1ex; margin-bottom: 1ex; ">This is a demo system that shows the current development state of Wikidata.<br /> <strong>This is the bleeding edge.</strong><br /> Expect things to be broken. </div>\'',
 		]
 	}
@@ -86,6 +88,8 @@ class role::wikidata-client::labs {
 	class { "wikidata::singlenode":
 		install_client => true,
 		install_repo => false,
+		repo_ip => $wikidata_repo_ip,
+		repo_url => $wikidata_repo_ip,
 		database_name => "client",
 		ensure => present,
 		install_path => "/srv/mediawiki",
@@ -95,9 +99,9 @@ class role::wikidata-client::labs {
 		],
 		# additional config lines
 		role_config_lines => [
-		'$wgWBSettings[\'repoUrl\'] = "//wikidata-puppet-repoo.pmtpa.wmflabs"',
-		'// Load Balancer',
-		'$wgLBFactoryConf = array( \'class\' => \'LBFactory_Multi\', \'serverTemplate\' => array( \'dbname\' => $wgDBname, \'user\' => $wgDBuser, \'password\' => $wgDBpassword, \'type\' => \'mysql\', \'flags\' => DBO_DEFAULT | DBO_DEBUG, ), \'sectionLoads\' => array( \'DEFAULT\' => array( \'localhost\' => 1, ), \'repo\' => array( \'local1\' => 1, ), ), \'sectionsByDB\' => array( $wgDBname => \'DEFAULT\', \'repo\' => \'repo\', ), \'hostsByName\' => array( \'localhost\' => \'127.0.0.1:3306\', \'local1\' => \'10.4.0.23:3306\', ), \'masterTemplateOverrides\' => array( \'fakeMaster\' => true ), )',
+		#'$wgWBSettings[\'repoUrl\'] = "//wikidata-puppet-repoo.pmtpa.wmflabs"',
+		#'// Load Balancer',
+		#'$wgLBFactoryConf = array( \'class\' => \'LBFactory_Multi\', \'serverTemplate\' => array( \'dbname\' => $wgDBname, \'user\' => $wgDBuser, \'password\' => $wgDBpassword, \'type\' => \'mysql\', \'flags\' => DBO_DEFAULT | DBO_DEBUG, ), \'sectionLoads\' => array( \'DEFAULT\' => array( \'localhost\' => 1, ), \'repo\' => array( \'local1\' => 1, ), ), \'sectionsByDB\' => array( $wgDBname => \'DEFAULT\', \'repo\' => \'repo\', ), \'hostsByName\' => array( \'localhost\' => \'127.0.0.1:3306\', \'local1\' => \'10.4.0.23:3306\', ), \'masterTemplateOverrides\' => array( \'fakeMaster\' => true ), )',
 		'// DismissableSiteNotice',
 		'$wgSiteNotice = \'<div style="font-size: 90%; background: #FFCC33; padding: 1ex; border: #940 dotted; margin-top: 1ex; margin-bottom: 1ex; ">This is a demo system that shows the current development state of Wikidata. It is going to evolve over the next few weeks. All data here can be deleted any time.<br>If you find bugs please report them in [https://bugzilla.wikimedia.org/enter_bug.cgi?product=MediaWiki%20extensions Bugzilla] for the Wikidata Client or Repo component. [https://meta.wikimedia.org/wiki/Wikidata/Development/Howto_Bugreport Here is how to submit a bug.] If you would like to discuss something or give input please use the [https://lists.wikimedia.org/mailman/listinfo/wikidata-l mailing list]. Thank you!</div>\'',
 		]
