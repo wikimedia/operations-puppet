bb6d76e85fa7fa78b2e5c5f2164f27d9df158f1c
Andrew Otto <acotto@gmail.com>
2013-01-21 11:00:25 -0500

    Sending blog.wikimedia.org traffic logs to analytics1001 udp2log instance.
    The firehose stream has too much data for a single machine to
    consume unsampled logs from.  We will soon start a discussion about
    sharding of the main webrequest firehose, but for now this change
    will at least allow us to give Tilman and the communications team
    accurate stats about blog request that they have been asking for for
    a while now.
    
    Change-Id: I626bc0bb776d15b32fc29feecfb4aadcc4c92144

--- manifests/misc/blogs.pp
+++ manifests/misc/blogs.pp
@@ -65,5 +65,9 @@ class misc::blogs::wikimedia {
 	varnish::logging { "locke" :           listener_address => "208.80.152.138" }
 	varnish::logging { "emery" :           listener_address => "208.80.152.184" }
 	varnish::logging { "multicast_relay" : listener_address => "208.80.154.15", port => "8419" }
+
+	# Capture blog traffic logs on its own stream in analytics cluster.
+	# 208.80.154.154 == analytics1001.wikimedia.org
+	varnish::logging { "analytics-blog" :  listener_address => "208.80.154.154", port => "8411" }
 }
 
