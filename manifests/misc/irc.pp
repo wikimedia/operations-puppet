6d21e735cb0449e3da4bd31a1a9617bc032d8ded
Antoine Musso <hashar@free.fr>
2013-01-22 10:18:04 +0100

    BZ 26784 - Adding process monitoring for ircecho
    Change-Id: I826244519d459d2c2faa9a2c3914d58f0067ad94

--- manifests/misc/irc.pp
+++ manifests/misc/irc.pp
@@ -100,5 +100,8 @@ class misc::ircecho {
 			mode => 0755;
 	}
 
+	# bug 26784 - IRC bots process need nagios monitoring
+    monitor_service { "ircecho": description => "ircecho_service_running", check_command => "nrpe_check_ircecho" }
+
 }
 
