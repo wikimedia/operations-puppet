cee1c1da838ba78b1227e1c45604835e14ecb1f4
pyoungmeister <py@wikimedia.org>
2013-01-23 14:32:15 -0800

    fixing ganglia for eqiad mc boxes
    rt 4389
    
    Change-Id: I66890cc4532da4c3896fc7ec2e28a687576bf7a5

--- manifests/ganglia.pp
+++ manifests/ganglia.pp
@@ -311,6 +311,7 @@ class ganglia {
 						"Miscellaneous esams" => "hooft.esams.wikimedia.org",
 						"Analytics cluster eqiad" => "analytics1003.eqiad.wmnet analytics1011.eqiad.wmnet",
 						"Memcached pmtpa" => "mc1.pmtpa.wmnet mc2.pmtpa.wmnet",
+						"Memcached eqiad" => "mc1001.eqiad.wmnet mc1002.eqiad.wmnet",
 						"Upload caches esams" => "cp3003.esams.wikimedia.org cp3004.esams.wikimedia.org",
 						"Ceph cluster esams" => "ms-be3001.esams.wikimedia.org ms-be3002.esams.wikimedia.org",
 						"Parsoid pmtpa" => "wtp1.pmtpa.wmnet",
