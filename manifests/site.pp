b4c624a6e8e28774f002e167be63a016d9909bc4
pyoungmeister <py@wikimedia.org>
2013-01-29 12:12:22 -0800

    db box bookkeeping
    added db1013 to the unused section, redid quitespace to tabs, and
    added standard to unused boxes so that they don't have slate
    puppet alerts
    
    Change-Id: I05cb154ecc68265d8e065c9f1ea5f945bb67a870

--- manifests/site.pp
+++ manifests/site.pp
@@ -354,74 +354,74 @@ node "dataset1001.wikimedia.org" {
 
 # pmtpa dbs
 node /db(32|36|38|59|60|63)\.pmtpa\.wmnet/ {
-  if $hostname == "db59" {
-    class { role::coredb::s1 : mariadb => true }
-  } else {
-    include role::coredb::s1
-  }
+	if $hostname == "db59" {
+		class { role::coredb::s1 : mariadb => true }
+	} else {
+		include role::coredb::s1
+	}
 }
 
 node /db(52|53|54|57)\.pmtpa\.wmnet/ {
-  include role::coredb::s2
+	include role::coredb::s2
 }
 
 node /db(34|39|64|66)\.pmtpa\.wmnet/ {
-  include role::coredb::s3
+	include role::coredb::s3
 }
 
 node /db(31|33|51|65)\.pmtpa\.wmnet/ {
-  if $hostname =~ /^db51/ {
-    $ganglia_aggregator = "true"
-  }
-  include role::coredb::s4
+	if $hostname =~ /^db51/ {
+		$ganglia_aggregator = "true"
+	}
+	include role::coredb::s4
 }
 
 node /db(35|44|45|55)\.pmtpa\.wmnet/ {
-  include role::coredb::s5
+	include role::coredb::s5
 }
 
 node /db(43|46|47|50)\.pmtpa\.wmnet/ {
-  if $hostname =~ /^db50/ {
-    $ganglia_aggregator = "true"
-  }
-  include role::coredb::s6
+	if $hostname =~ /^db50/ {
+		$ganglia_aggregator = "true"
+	}
+	include role::coredb::s6
 }
 
 node /db(37|56|58|68)\.pmtpa\.wmnet/ {
-  include role::coredb::s7
+	include role::coredb::s7
 }
 
 ## m1 shard (still mostly not puppetized...)
 node /^(db(9|10)|blondel|bellin)\.pmtpa\.wmnet$/ {
-  ## do not have most of our current puppet classe
-  include role::db::core
+	## do not have most of our current puppet classe
+	include role::db::core
 
-  if $hostname =~ /^(bellin|blondel)/ {
-    include mysql::mysqluser,
-    mysql::datadirs,
-    mysql::conf,
-    mysql::packages
-  }
+	if $hostname =~ /^(bellin|blondel)/ {
+		include mysql::mysqluser,
+		mysql::datadirs,
+		mysql::conf,
+		mysql::packages
+	}
 }
 
 ## m2 shard
 node /db4[89]\.pmtpa\.wmnet/ {
-  include role::coredb::m2
+	include role::coredb::m2
 }
 
 node "db78.pmtpa.wmnet" {
-  include role::fundraising::database::dump_slave
-  class { 'misc::fundraising::backup::archive_sync': hour => [4,12,20], minute => 5 }
+	include role::fundraising::database::dump_slave
+	class { 'misc::fundraising::backup::archive_sync': hour => [4,12,20], minute => 5 }
 }
 
 ## researchdb
 node /db67\.pmtpa\.wmnet/ {
-  include role::coredb::researchdb
+	include role::coredb::researchdb
 }
 
 ## not in use for various reasons
 node /db(42|6[129]|7[0-7])\.pmtpa\.wmnet/{
-
+	include standard
 }
 
 # eqiad dbs
@@ -493,8 +493,8 @@ node /db1047\.eqiad\.wmnet/ {
 }
 
 ## not currently in production and/or hardware issues
-node /db10(1[2456]|2[39]|3[012367]|4[45])\.eqiad\.wmnet/ {
-
+node /db10(1[23456]|2[39]|3[012367]|4[45])\.eqiad\.wmnet/ {
+	include standard
 }
 
 node "dobson.wikimedia.org" {
