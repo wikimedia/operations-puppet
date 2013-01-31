bd4e3bf76bce4ad67cf8b0c03adf01c5199c6785
Andrew Bogott <abogott@wikimedia.org>
2013-01-31 21:03:03 +0000

    Rework the RT manifests so it can be installed in Labs
    - Create a labs and a production role
    - Add a zillion dependencies which were missing from the previous class
    - Automate the db creation
    
    This still doesn't work perfectly but is much better than what we had.
    
    Change-Id: I8a7db9d98d4f62fcae6d495a7f209d1990f14061

--- manifests/misc/rt-server.pp
+++ manifests/misc/rt-server.pp
@@ -1,50 +1,74 @@
 # RT - Request Tracker
-class misc::rt::server {
-	system_role { "misc::rt::server": description => "RT server" }
+class misc::rt::server ( $site = 'rt.wikmedia.org', $datadir = '/var/lib/mysql' ) {
+  system_role { 'misc::rt::server': description => 'RT server' }
 
-	package { [ "request-tracker3.8", "rt3.8-db-mysql", "rt3.8-clients", "libcgi-fast-perl", "lighttpd" ]:
-		ensure => latest;
-	}
+  package { [ 'request-tracker3.8', 'rt3.8-db-mysql', 'rt3.8-clients', 'libcgi-fast-perl', 'lighttpd',
+    'libdbd-pg-perl' ]:
+    ensure => latest;
+  }
 
-	$rtconf = "# This file is for the command-line client, /usr/bin/rt.\n\nserver http://localhost/rt\n"
+  class { 'generic::mysql::server':
+    version => $::lsbdistrelease ? {
+      '12.04' => '5.5',
+      default => false,
+    },
+    datadir => $datadir,
+    require => package ['request-tracker3.8', 'rt3.8-db-mysql', 'rt3.8-clients'],
+    notify  => Exec['rt-db-initialize'];
+  }
 
-	file {
-		"/etc/lighttpd/conf-available/10-rt.conf":
-			source => "puppet:///files/rt/10-rt.lighttpd.conf";
-		"/var/run/fastcgi":
-			ensure => directory,
-			owner => "www-data",
-			group => "www-data",
-			mode => 0750;
-		"/etc/request-tracker3.8/RT_SiteConfig.d/50-debconf":
-			source => "puppet:///files/rt/50-debconf",
-			notify => Exec["update-rt-siteconfig"];
-		"/etc/request-tracker3.8/RT_SiteConfig.d/80-wikimedia":
-			source => "puppet:///files/rt/80-wikimedia",
-			notify => Exec["update-rt-siteconfig"];
-		"/etc/request-tracker3.8/RT_SiteConfig.pm":
-			owner => "root",
-			group => "www-data",
-			mode => 0440;
-		"/etc/request-tracker3.8/rt.conf":
-			require => Package["request-tracker3.8"],
-			content => $rtconf;
-		"/etc/cron.d/mkdir-var-run-fastcgi":
-			content => "@reboot	root	mkdir /var/run/fastcgi";
-	}
+  $rtconf = '# This file is for the command-line client, /usr/bin/rt.\n\nserver http://localhost/rt\n'
 
-	exec { "update-rt-siteconfig":
-		command => "update-rt-siteconfig-3.8",
-		path => "/usr/sbin",
-		refreshonly => true;
-	}
+  file {
+    '/etc/lighttpd/conf-available/10-rt.conf':
+      ensure  => present,
+      content => template('rt/10-rt.lighttpd.conf.erb');
+    '/var/run/fastcgi':
+      ensure => directory,
+      owner  => 'www-data',
+      group  => 'www-data',
+      mode   => '0750';
+    '/etc/request-tracker3.8/RT_SiteConfig.d/50-debconf':
+      source => 'puppet:///files/rt/50-debconf',
+      notify => Exec['update-rt-siteconfig'];
+    '/etc/request-tracker3.8/RT_SiteConfig.d/80-wikimedia':
+      source => 'puppet:///files/rt/80-wikimedia',
+      notify => Exec['update-rt-siteconfig'];
+    '/etc/request-tracker3.8/RT_SiteConfig.pm':
+      owner => 'root',
+      group => 'www-data',
+      mode  => '0440';
+    '/etc/request-tracker3.8/rt.conf':
+      require => Package['request-tracker3.8'],
+      content => $rtconf;
+    '/etc/cron.d/mkdir-var-run-fastcgi':
+      content => '@reboot root  mkdir /var/run/fastcgi';
+  }
 
-	lighttpd_config { "10-rt":
-		require => [ Package["request-tracker3.8"], File["/etc/lighttpd/conf-available/10-rt.conf"] ],
-	}
+  exec { 'rt-db-initialize':
+    command => "/bin/echo '' | rt-setup-database --action init --dba root --prompt-for-dba-password",
+    require => package[ 'request-tracker3.8', 'rt3.8-db-mysql', 'rt3.8-clients', 'libcgi-fast-perl', 'lighttpd',
+      'libdbd-pg-perl' ],
+    path        => '/usr/sbin',
+    refreshonly => true;
+  }
 
-	service { lighttpd:
-		ensure => running;
-	}
+  exec { 'update-rt-siteconfig':
+    command     => 'update-rt-siteconfig-3.8',
+    path        => '/usr/sbin',
+    require => [ exec[ "rt-db-initialize" ],
+                 file[ "/etc/request-tracker3.8/RT_SiteConfig.d/50-debconf",
+                       "/etc/request-tracker3.8/RT_SiteConfig.d/80-wikimedia" ] ],
+    refreshonly => true,
+    notify      => Service[lighttpd];
+  }
+
+  lighttpd_config { '10-rt':
+    require => package[ 'request-tracker3.8', 'rt3.8-db-mysql', 'rt3.8-clients', 'libcgi-fast-perl', 'lighttpd' ];
+  }
+
+  service { 'lighttpd':
+    ensure => running;
+  }
 }
 
