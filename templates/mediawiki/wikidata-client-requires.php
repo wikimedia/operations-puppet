938cf5dceac6c8c6f9965d17cacb74babf083f1d
SilkeMeyer <silke.meyer@wikimedia.de>
2013-02-05 14:46:22 +0100

    Enable memcached in Wikidata's LocalSettings files
    Change-Id: I4043833b32f6087d726a433c99a77eb01391d295

--- templates/mediawiki/wikidata-client-requires.php
+++ templates/mediawiki/wikidata-client-requires.php
@@ -20,6 +20,10 @@ $wgLogo = "$wgStylePath/common/images/Wikidata-logo-democlient.png";
 $wgCacheDirectory = "/var/cache/mw-cache/<%=database_name%>/";
 $wgLocalisationCacheConf['manualRecache'] = true;
 
+// Shared memory settings
+$wgMainCacheType    = CACHE_MEMCACHED;
+$wgMemCachedServers = array( "127.0.0.1:11000" );
+
 // experimental features
 define( 'WB_EXPERIMENTAL_FEATURES', <%=experimental%> );
 
