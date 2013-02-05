16c62c2118ae5cc2a4b3400266216a90eaa7dd8a
Antoine Musso <hashar@free.fr>
2013-02-05 22:37:52 +0100

    cleanout testswarm from the manifests
    testswarm has been phased out a while ago. This cleanout the contint
    manifest but keep the 'testswarm' system user which is moved to the new
    'testswarm' module.
    
    The module is very simple but will be amended later on to actually
    install the new testswarm system.
    
    Change-Id: I4f4d4df62fdfa6fc5f3aa0ee9d9287e7a9281e08

---
+++ modules/testswarm/manifests/systemuser.pp
@@ -0,0 +1,13 @@
+class testswarm::systemuser {
+
+  # Create a user to run the cronjob with
+  systemuser { 'testswarm':
+    name   => 'testswarm',
+    home   => '/var/lib/testswarm',
+    shell  => '/bin/false',
+    # And part of web server user group so we can let it access
+    # the SQLite databases
+    groups => [ 'www-data' ];
+  }
+
+}
