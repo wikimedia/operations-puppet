f789e8653b60118081866a6b7eb7497065bce595
Antoine Musso <hashar@free.fr>
2013-02-05 22:25:12 +0100

    Jenkins module created out of contint manifests
    misc::contint contained a Jenkins class for the continuous integration.
    I have extracted out the part which are only related to Jenkins in a new
    Jenkins module.  Simply 'include jenkins' and you get an installation.
    
    The Jenkins instance might need to be proxying via Apache, that
    configuration is not part of the jenkins module.
    
    Change-Id: I33bce60e08fd4c1e8184d6fac5ba0d3f9798fe35

---
+++ modules/jenkins/manifests/init.pp
@@ -0,0 +1,44 @@
+class jenkins {
+  require jenkins::user
+
+  # Upgrades are usually done manually by upload the Jenkins
+  # package at apt.wikimedia.org then restarting jenkins and
+  # double checking everything went fine.
+  package { 'jenkins': ensure => present; }
+
+  # Graphiz is needed by the job dependency graph plugin
+  package { 'graphviz': ensure => present; }
+
+  service { 'jenkins':
+    ensure     => 'running',
+    enable     => true,
+    hasrestart => true,
+  }
+
+  # nagios monitoring
+  monitor_service { 'jenkins':
+    description   => 'jenkins_service_running',
+    check_command => 'nrpe_check_jenkins'
+  }
+
+  file { '/var/lib/jenkins':
+    ensure => directory,
+    mode   => '2775',  # group sticky bit
+    owner  => 'jenkins',
+    group  => 'jenkins',
+  }
+  # Top level jobs folder
+  file { '/var/lib/jenkins/jobs':
+    ensure => directory,
+    mode   => '2775',  # group sticky bit
+    owner  => 'jenkins',
+    group  => 'jenkins',
+  }
+  file { '/etc/default/jenkins':
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0444',
+    source => 'puppet:///modules/jenkins/etc_default_jenkins',
+  }
+
+}
