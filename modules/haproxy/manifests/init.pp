f34238aed27c653025d27a32c99c172495ad8d9d
Andrew Otto <acotto@gmail.com>
2013-02-07 11:46:01 -0500

    Adding very simple haproxy module.
    Change-Id: I2220db197a93dcfc720f634d0ee9113cd5d902af

---
+++ modules/haproxy/manifests/init.pp
@@ -0,0 +1,14 @@
+# == Class haproxy
+# Installs haproxy and ensures that it is running.
+# Note: This class does not currently manage haproxy.cfg.
+#
+class haproxy {
+  package { 'haproxy':
+    ensure => 'installed'
+  }
+
+  service { 'haproxy':
+    ensure    => running,
+    enable    => true,
+  }
+}
