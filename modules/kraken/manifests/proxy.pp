6a320821b782424a32bdd1ccacb123a397f01df2
Andrew Otto <acotto@gmail.com>
2013-02-07 18:14:41 -0500

    Adding kraken role and module. Including role::kraken::proxy on analytics1001.
    Change-Id: If8cfa4f9a8627f8ae500161cd39b2229e95892ab

---
+++ modules/kraken/manifests/proxy.pp
@@ -0,0 +1,30 @@
+# == Class kraken::proxy
+# 
+# Includes haproxy module and installs
+# installs custom haproxy.cfg file.
+# This allows public access to
+# privately hosted Kraken web UIs.
+#
+# NOTE:  This class will probably change soon
+# to be replaced with a better method
+# of accessing internal Kraken web UIs.
+#
+class kraken::proxy(
+	$namenode_hostname,
+	$datanode_hostname,
+	$hue_hostname,
+	$oozie_hostname,
+	$storm_hostname,
+	$storm_port,
+	$bind      = "0.0.0.0:80",
+	$whitelist = {},
+	$http_auth = {}
+)
+{
+	include haproxy
+
+	file { "/etc/haproxy/haproxy.cfg":
+		content => template("kraken/haproxy.cfg.erb"),
+		notify  => Service["haproxy"],
+	}
+}
