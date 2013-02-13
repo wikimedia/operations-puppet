7991e91b11939868b58219e464ae287a30e05674
Mark Bergsma <mark@wikimedia.org>
2013-02-13 14:25:48 +0100

    Remove statically configured test backend in favor of dynamic director
    Change-Id: I5c771c9903b06206286469840e5bfaf7ef98d79f

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -503,7 +503,7 @@ class role::cache {
 					'pmtpa','eqiad': {
 						$varnish_directors = {
 							"backend" => $::role::cache::configuration::backends[$::realm]['bits_appservers'][$::mw_primary],
-							#"test_wikipedia" => $::role::cache::configuration::backends[$::realm]['test_appservers'][$::mw_primary],
+							"test_wikipedia" => $::role::cache::configuration::backends[$::realm]['test_appservers'][$::mw_primary],
 						}
 						$probe = "bits"
 					}
@@ -650,6 +650,7 @@ class role::cache {
 			admin_port => 6082,
 			directors => {
 				"backend" => $::role::cache::configuration::active_nodes['mobile'][$::site],
+				"test_wikipedia" => $::role::cache::configuration::backends[$::realm]['test_appservers'][$::mw_primary],
 			},
 			director_options => {
 				'retries' => 40,
@@ -665,12 +666,6 @@ class role::cache {
 				'between_bytes_timeout' => "2s",
 				'max_connections' => 100000,
 				'probe' => "varnish",
-			},
-			cluster_options => {
-				'test_appserver' => $::realm ? {
-					'production' => 'srv193.pmtpa.wmnet',
-					'labs'       => '10.4.0.166',
-				},
 			},
 			xff_sources => $network::constants::all_networks,
 		}
