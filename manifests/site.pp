d71ba7a7663d4fb1c733229ad31247e28e6d5021
Faidon Liambotis <faidon@wikimedia.org>
2013-02-13 16:27:30 -0800

    Adjust knsq24 for its broken disk
    knsq24 backend squid has been down for 158d. It seems sdd is non-present
    (probably broken) but otherwise the box looks good. Remove the disk from
    the configuration to revive it.
    
    Change-Id: I8e75dff98fc83345f093e243c74d6dbd6d6439a4

--- manifests/site.pp
+++ manifests/site.pp
@@ -928,8 +928,14 @@ node /knsq(1[6-9]|2[0-2])\.esams\.wikimedia\.org/ {
 }
 
 # knsq23-30 are text squids
-node /knsq(2[3-9]|30)\.esams\.wikimedia\.org/ {
+node /knsq(23|[5-9]|30)\.esams\.wikimedia\.org/ {
 	$squid_coss_disks = [ 'sda5', 'sdb5', 'sdc', 'sdd' ]
+
+	include role::cache::text
+}
+# broken/non-present sdd
+node "knsq24.esams.wikimedia.org" {
+	$squid_coss_disks = [ 'sda5', 'sdb5', 'sdc' ]
 
 	include role::cache::text
 }
