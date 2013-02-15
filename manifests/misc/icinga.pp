420ca31fb8f468be70e0ac90f5f320db41c65562
Faidon Liambotis <faidon@wikimedia.org>
2013-02-15 15:21:00 -0800

    Kill check_solr (at least in this form)
    The check is really broken, keeping state between invocations in an
    sqlite3 in a random place /var/lib and alerting only based on the errors
    diff between invocations. Revert this for now, until a sensible version
    of that appears (quick Googling suggests there are other idempotent
    checks around)
    
    Change-Id: I460b89a4c87d4595b4a17a2641dfbd03f26c78ea

--- manifests/misc/icinga.pp
+++ manifests/misc/icinga.pp
@@ -563,16 +563,6 @@ class icinga::monitor::files::nagios-plugins {
 			owner => root,
 			group => root,
 			mode => 0755;
-		"/usr/local/nagios/libexec/check_solr":
-			source => "puppet:///files/nagios/check_solr",
-			owner => root,
-			group => root,
-			mode => 0755;
-		"/var/lib/check_solr":
-			ensure => directory,
-			owner => nagios,
-			group => wikidev,
-			mode => 0750;
 	}
 
 	# some default configuration files conflict and should be removed
