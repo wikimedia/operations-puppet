902fe6037d21e517f2d2f314e2b7a66ad3728fbf
Faidon Liambotis <faidon@wikimedia.org>
2013-02-15 16:44:06 -0800

    Restore previous version of solr monitoring
    Restore solr monitoring to pre-66109b55, since the check_solr check was
    removed.
    
    Change-Id: I51be2bbdd911faf7f8f68fa39d83b24f66b3fa8f

--- manifests/role/solr.pp
+++ manifests/role/solr.pp
@@ -6,20 +6,27 @@
 #
 # $schema::             Schema file for Solr (only one schema per instance supported)
 # $replication_master:: Replication master, if this is current hostname, this server will be a master
-class role::solr($schema = undef, $replication_master = undef ) {
+# $monitor::            How to monitor this server:
+#                       * "service" - just presence of Solr
+#                       * "results" - whether Solr has some data in its index
+#                       Any other input will disable monitoring
+class role::solr($schema = undef, $replication_master = undef, $monitor = "service" ) {
 	class { "::solr":
 		schema => $schema,
 		replication_master => $replication_master,
 	}
 
-	if ($replication_master) {
-		$check_command = "check_solr"
-	} else {
-		$check_command = "check_replicated_solr"
+	if ($monitor == "service") {
+		monitor_service { "Solr":
+			description => "Solr",
+			check_command => "check_http_url_on_port!$::hostname!8983!200!/solr/select/?q=*%3A*\&start=0\&rows=1\&indent=on"
+		}
 	}
-	monitor_service { "Solr":
-		description => "Solr",
-		check_command => "$check_command!$::hostname!400:600!5",
+	elsif ($monitor == "results") {
+		monitor_service { "Solr":
+			description => "Solr (with a result set check)",
+			check_command => "check_http_url_for_string_on_port!$::hostname!8983!'<str name=\"rows\">1</str>'!/solr/select/?q=*%3A*\&start=0\&rows=1\&indent=on"
+		}
 	}
 }
 
