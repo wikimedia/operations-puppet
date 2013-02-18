7eb05e76ebe3ce62f715dfafbbf4d810846f40fc
Andrew Otto <acotto@gmail.com>
2013-02-18 09:45:39 -0500

    Adding Analytics team members to 'stats' group.
    This will allow us to group-own sensitive files in HDFS by stats,
    so that these users can still have read access to it.
    
    Created role::analytics::users class to organize this change.
    
    Change-Id: I1596ba90978e56426fcfb05f4b366753d6d1ea09

--- manifests/role/analytics.pp
+++ manifests/role/analytics.pp
@@ -8,26 +8,19 @@ class role::analytics {
 	# ganglia cluster name.
 	$cluster = "analytics"
 
-	include standard,
-		admins::roots,
-		accounts::diederik,
-		accounts::dsc,
-		accounts::otto,
-		accounts::dartar,
-		accounts::erosen,
-		accounts::olivneh,
-		accounts::erik,
-		accounts::dandreescu,
-		accounts::spetrea # per RT4402
+	include standard
+	include admins::roots
 
-	# include stats system user to
-	# run automated jobs
+	# Include stats system user to
+	# run automated jobs and for file
+	# ownership.
 	include misc::statistics::user
 
-	sudo_user { [ "diederik", "dsc", "otto" ]: privileges => ['ALL = (ALL) NOPASSWD: ALL'] }
+	# include analytics user accounts
+	include role::analytics::users
 
 	# Install Sun/Oracle Java JDK on analytics cluster
-	java { "java-6-oracle": 
+	java { "java-6-oracle":
 		distribution => 'oracle',
 		version      => 6,
 	}
@@ -42,6 +35,37 @@ class role::analytics {
 
 	# udp-filter is a useful thing!
 	include misc::udp2log::udp_filter
+}
+
+class role::analytics::users {
+	# Analytics user accounts will be added to the
+	# 'stats' group which gets created by this class.
+	require misc::statistics::user
+
+	include accounts::diederik,
+		accounts::dsc,
+		accounts::otto,
+		accounts::dartar,
+		accounts::erosen,
+		accounts::olivneh,
+		accounts::erik,
+		accounts::dandreescu,
+		accounts::spetrea # per RT4402
+
+	# add Analytics team members to the stats group so they can
+	# access data group owned by 'stats'.
+	User<|title == dandreescu|>  { groups +> [ "stats" ] }
+	User<|title == dartar|>      { groups +> [ "stats" ] }
+	User<|title == dsc|>         { groups +> [ "stats" ] }
+	User<|title == diederik|>    { groups +> [ "stats" ] }
+	User<|title == erik|>        { groups +> [ "stats" ] }
+	User<|title == erosen|>      { groups +> [ "stats" ] }
+	User<|title == olivneh|>     { groups +> [ "stats" ] }
+	User<|title == otto|>        { groups +> [ "stats" ] }
+	User<|title == spetrea|>     { groups +> [ "stats" ] }
+
+	# Diederik, David and Otto have sudo privileges on Analytics nodes.
+	sudo_user { [ "diederik", "dsc", "otto" ]: privileges => ['ALL = (ALL) NOPASSWD: ALL'] }
 }
 
 # front end interfaces for Kraken and Hadoop
