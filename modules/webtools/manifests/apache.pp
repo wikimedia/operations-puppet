8db3370dc708ee2d6be6c6c8d0efbbc353d99aea
Platonides <platonides@gmail.com>
2013-02-20 16:18:12 +0100

    Configuration for webtools-apache VMs
    Change-Id: I5491445c0c0e68163e2c960fd71ceda721da1cbf

---
+++ modules/webtools/manifests/apache.pp
@@ -0,0 +1,12 @@
+class webtools::apache {
+	include webserver::php5
+
+	file {
+		"/etc/apache2/sites-available/default":
+			source => "puppet:///modules/webtools/files/apache-default",
+		"/etc/apache2/sites-enabled/000-default":
+			ensure => link,
+			target => "/etc/apache2/sites-available/default"
+	}
+}
+
