887ee162df8690ddd72ad50183309eb30749c831
Platonides <platonides@gmail.com>
2013-02-20 16:55:07 +0100

    Handling of packages
    Change-Id: Ia92ee45f878c7ab3e98475beb979674f64a74c56

---
+++ modules/webtools/manifests/bastion.pp
@@ -0,0 +1,12 @@
+class webtools::bastion {
+	include webtools::libraries
+	package { [
+			'autoconf',
+			'build-essential',
+			'debhelper',
+			'devscripts',
+			'libtool',
+		]: ensure => present,
+	}
+}
+
