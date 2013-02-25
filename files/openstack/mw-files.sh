cebdbbfdf206e697dbb50fc2fb4c30417202680e
Ryan Lane <rlane@wikimedia.org>
2013-02-25 11:11:27 -0800

    Make images and files labsconsole backup chdir
    Change-Id: I4533ef42276e388d116214cfb04957be823160c4

--- files/openstack/mw-files.sh
+++ files/openstack/mw-files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tar -czvf /a/backup/labswiki-$(date '+%Y%m%d')-files.tar.gz /srv/org/wikimedia/controller
-tar -czvf /a/backup/public/labswiki-$(date '+%Y%m%d')-images.tar.gz /srv/org/wikimedia/controller/wikis/images
+tar -czvf /a/backup/labswiki-$(date '+%Y%m%d')-files.tar.gz -C /srv/org/wikimedia controller
+tar -czvf /a/backup/public/labswiki-$(date '+%Y%m%d')-images.tar.gz -C /srv/org/wikimedia/controller/wikis images
