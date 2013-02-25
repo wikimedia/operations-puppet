e8b1b807e3882912ac2b8572d3a4a057e8c74f35
Ryan Lane <rlane@wikimedia.org>
2013-02-25 10:31:56 -0800

    Move xml and image backups into public location
    Change-Id: I8c4ca335fa8d9f4d90d467e43133985bb741e098

--- files/openstack/mw-files.sh
+++ files/openstack/mw-files.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 tar -czvf /a/backup/labswiki-$(date '+%Y%m%d')-files.tar.gz /srv/org/wikimedia/controller
+tar -czvf /a/backup/public/labswiki-$(date '+%Y%m%d')-images.tar.gz /srv/org/wikimedia/controller/wikis/images
--- files/openstack/mw-xml.sh
+++ files/openstack/mw-xml.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /srv/org/wikimedia/controller/wikis/w
-php maintenance/dumpBackup.php --full --uploads | nice -n 19 gzip -9 > /a/backup/labswiki-$(date '+%Y%m%d').xml.gz
+php maintenance/dumpBackup.php --full --uploads | nice -n 19 gzip -9 > /a/backup/public/labswiki-$(date '+%Y%m%d').xml.gz
 cd -
