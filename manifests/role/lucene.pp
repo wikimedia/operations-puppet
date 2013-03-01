d59b32b8ec1d7c13d7d2c2d5bab632e1a7bc4959
pyoungmeister <py@wikimedia.org>
2013-03-01 14:45:38 -0800

    one more fix
    Change-Id: I7b587d2b35c50dcffa3686c519115232f27944a5

--- manifests/role/lucene.pp
+++ manifests/role/lucene.pp
@@ -129,7 +129,7 @@ class role::lucene {
 
 		# Include packages needed for MW maintenance scripts
 		include standard,
-			mediawiki_new,
+			mediawiki,
 			applicationserver::config::php,
 			applicationserver::config::base,
 			applicationserver::packages,
