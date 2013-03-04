a5b9b00789294c410f5026255837a2804e71a3e1
Ryan Lane <rlane@wikimedia.org>
2013-03-04 13:59:17 -0800

    Order bonding before tagging for network node
    Change-Id: Ia74c6362ce9564a860a75321eb537d7f5f00a5b5

--- manifests/role/nova.pp
+++ manifests/role/nova.pp
@@ -263,11 +263,25 @@ class role::nova::api {
 	class { "openstack::api-service": openstack_version => $openstack_version, novaconfig => $novaconfig }
 }
 
+class role::nova::network::bonding {
+	interface_aggregate { "bond1": orig_interface => "eth1", members => [ "eth1", "eth2", "eth3" ] }
+}
+
 class role::nova::network {
 	include role::nova::config::pmtpa,
 		role::nova::config::eqiad
 
 	include role::nova::common
+
+	require role::nova::network::bonding
+
+	interface_tagged { $novaconfig["network_flat_interface"]:
+		base_interface => $novaconfig["network_flat_interface_name"],
+		vlan_id => $novaconfig["network_flat_interface_vlan"],
+		method => "manual",
+		up => 'ip link set $IFACE up',
+		down => 'ip link set $IFACE down',
+	}
 
 	$novaconfig = $site ? {
 		"pmtpa" => $role::nova::config::pmtpa::novaconfig,
@@ -310,6 +324,14 @@ class role::nova::compute {
 	$novaconfig = $site ? {
 		"pmtpa" => $role::nova::config::pmtpa::novaconfig,
 		"eqiad" => $role::nova::config::eqiad::novaconfig,
+	}
+
+	interface_tagged { $novaconfig["network_flat_interface"]:
+		base_interface => $novaconfig["network_flat_interface_name"],
+		vlan_id => $novaconfig["network_flat_interface_vlan"],
+		method => "manual",
+		up => 'ip link set $IFACE up',
+		down => 'ip link set $IFACE down',
 	}
 
 	class { "openstack::compute-service": openstack_version => $openstack_version, novaconfig => $novaconfig }
