4fc7adf07f60e632f924a05b93f1d6ab8ba0c581
Andre Klapper <a9016009@gmx.de>
2013-03-06 12:30:00 +0100

    [bug 45770] Add TODO about deprecated mysql_connect()
    Change-Id: I5d657cec716f2f4b97a136fe7ba814d3f431d2ac

--- templates/misc/bugzilla_report.php
+++ templates/misc/bugzilla_report.php
@@ -172,6 +172,7 @@ else
 
 print "MediaWiki Bugzilla Report for " . date('F d, Y', $begin_date) . " - " . date('F d, Y', $end_date) . "\n\n";
 
+/* TODO: mysql_connect is deprecated - switch to MySQLi or PDO */
 $ok = mysql_connect("db9.pmtpa.wmnet", "bugs", "<%= scope.lookupvar('passwords::bugzilla::bugzilla_db_pass') %>");
 if (!$ok)
         reportFailure("DB connection failure");
