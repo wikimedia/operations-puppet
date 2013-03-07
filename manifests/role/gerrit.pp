12060ea61b5caa0606c77f6612094691a01a0b97
Chad Horohoe <chadh@wikimedia.org>
2013-03-07 14:37:08 -0800

    We don't actually run any slaves, turn formey into a replicationdest
    Change-Id: I814e06a3893b2797eb2b8e53a8af1a37c44a96c8

--- manifests/role/gerrit.pp
+++ manifests/role/gerrit.pp
@@ -54,24 +54,6 @@ class role::gerrit {
 		}
 	}
 
-	class production::slave {
-		system_role { "role::gerrit::slave": description => "Gerrit slave" }
-
-		# It's a full slave, so install gerrit
-		class { "gerrit::instance":
-			slave => true,
-			no_apache => true,
-			db_host => "db1048.eqiad.wmnet",
-			host => "formey.wikimedia.org",
-		}
-
-		# Slaves need to be able to recieve replication
-		class { "gerrit::replicationdest":
-			sshkey => "AAAAB3NzaC1yc2EAAAABIwAAAQEAxOlshfr3UaPr8gQ8UVskxHAGG9xb55xDyfqlK7vsAs/p+OXpRB4KZOxHWqI40FpHhW+rFVA0Ugk7vBK13oKCB435TJlHYTJR62qQNb2DVxi5rtvZ7DPnRRlAvdGpRft9JsoWdgsXNqRkkStbkA5cqotvVHDYAgzBnHxWPM8REokQVqil6S/yHkIGtXO5J7F6I1OvYCnG1d1GLT5nDt+ZeyacLpZAhrBlyFD6pCwDUhg4+H4O3HGwtoh5418U4cvzRgYOQQXsU2WW5nBQHE9LXVLoL6UeMYY4yMtaNw207zN6kXcMFKyTuF5qlF5whC7cmM4elhAO2snwIw4C3EyQgw==",
-			extra_groups => ["gerrit2"]
-		}
-	}
-
 	# Include this role on *any* production host that wants to
 	# receive gerrit replication
 	class production::replicationdest {
