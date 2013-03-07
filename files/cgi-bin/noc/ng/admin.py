04a2fcb0d1c9068c15e43c1085d5d0f6a233bab5
Chad Horohoe <chadh@wikimedia.org>
2013-03-07 15:55:34 -0800

    Fix various noc files to be pep8 compliant
    Change-Id: I933e9d58c9b4403c66a61d2c6399c6cb5b78ff0b

--- files/cgi-bin/noc/ng/admin.py
+++ files/cgi-bin/noc/ng/admin.py
@@ -1,57 +1,58 @@
 #!/usr/bin/python
 #####################################################################
-### THIS FILE IS MANAGED BY PUPPET 
+### THIS FILE IS MANAGED BY PUPPET
 ### puppet:///files/cgi-bin/noc/ng/admin.py
 #####################################################################
 
 import shelve
 import cgi
-import cgitb; cgitb.enable()
+import cgitb
+cgitb.enable()
 
 import datetime
 import sys
 import config
 
-password=config.password
+password = config.password
 
 print "Content-type: text/html\n"
 
-form=cgi.SvFormContentDict()
+form = cgi.SvFormContentDict()
 
 if 'password' in form:
-	if form['password'] != "" and form['password'] != password: 
-		print "access denied!!!!!!!!!1111oneoneeleven"
-		sys.exit()
-	else:
-		authed=True
+    if form['password'] != "" and form['password'] != password:
+        print "access denied!!!!!!!!!1111oneoneeleven"
+        sys.exit()
+    else:
+        authed = True
 else:
-	authed=False
+    authed = False
 
 print """
-	<script>function deletesample(sample) {form=document.forms['actions'];form['sample'].value=sample;form.submit();}</script>
-	<form name='actions' method='POST' action='admin.py'>
-	<input type='submit' name='action' value='take'>
-	<input type='submit' name='action' value='clear'>
-	<input type='hidden' name='sample' value=''>
-	<input type='%s' name='password' value='%s'
-	</form>""" % ((authed and 'hidden' or 'password'),(authed and password or ''))
+    <script>function deletesample(sample) {form=document.forms['actions'];form['sample'].value=sample;form.submit();}</script>
+    <form name='actions' method='POST' action='admin.py'>
+    <input type='submit' name='action' value='take'>
+    <input type='submit' name='action' value='clear'>
+    <input type='hidden' name='sample' value=''>
+    <input type='%s' name='password' value='%s'
+    </form>""" % ((authed and 'hidden' or 'password'), (authed and password or ''))
 
 store = shelve.open('/var/run/profile-baselines')
 
 if 'action' not in form:
-	if 'sample' in form and authed:
-		del store[form['sample']]
+    if 'sample' in form and authed:
+        del store[form['sample']]
 elif form['action'] == 'clear' and authed:
-	from socket import *
-	sock = socket(AF_INET,SOCK_DGRAM)
-	sock.sendto('-truncate',(config.host,config.port))
+    from socket import *
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.sendto('-truncate', (config.host, config.port))
 elif form['action'] == 'take' and authed:
-	from extractprofile import SocketProfile
-	store[str(datetime.datetime.now()).replace(" ","-")] = SocketProfile(config.host,config.port).extract()
+    from extractprofile import SocketProfile
+    store[str(datetime.datetime.now()).replace(" ", "-")] = SocketProfile(config.host, config.port).extract()
 elif 'sample' in form and authed:
-	del store[form['sample']]
+    del store[form['sample']]
 
 for entry in store.keys():
-	print """<div><a href='report.py?sample=%s'>%s</a> <a href='javascript:deletesample("%s")'>delete</a></div>""" % (entry,entry,entry)
-	
+    print """<div><a href='report.py?sample=%s'>%s</a> <a href='javascript:deletesample("%s")'>delete</a></div>""" % (entry, entry, entry)
+
 print "<div><a href='report.py'>current</a></div>"
--- files/cgi-bin/noc/report.py
+++ files/cgi-bin/noc/report.py
@@ -10,23 +10,24 @@
 profilehost = "professor.pmtpa.wmnet"
 profileport = 3811
 
-db="all"
-sort="real"
-limit=50
-prefix=""
+db = "all"
+sort = "real"
+limit = 50
+prefix = ""
 
 
 from extractprofile import ExtractProfile
 
 import cgi
-import cgitb; cgitb.enable()
+import cgitb
+cgitb.enable()
 
 import socket
 import codecs
 import sys
 
 
-print "Content-type: text/html; charset=utf-8";
+print "Content-type: text/html; charset=utf-8"
 print "\n"
 
 utf8 = codecs.getencoder("UTF-8")
@@ -42,48 +43,49 @@ if "prefix" in form:
     prefix = form["prefix"]
 
 if "limit" in form:
-    limit=int(form["limit"])
+    limit = int(form["limit"])
 
-compare="none"
+compare = "none"
 if "compare" in form:
     compare = form["compare"]
+
 
 class SocketSource (socket.socket):
     def read(self, what):
         enc = self.recv(what, 0)
         return enc.decode('latin-1').encode('utf-8')
 
-sock=SocketSource()
+sock = SocketSource()
 sock.connect((profilehost, profileport))
 
-cache={}
-fullprofile=ExtractProfile().extract(sock)
+cache = {}
+fullprofile = ExtractProfile().extract(sock)
 try:
-    events=fullprofile[db]["-"].items()
+    events = fullprofile[db]["-"].items()
 except KeyError:
     for dbname in fullprofile.keys():
         print " [<a href='report.py?db=%s'>%s</a>] " % (dbname, dbname)
         sys.exit(0)
-    
 
-cache[db]=events
-cache["_dbs"]=fullprofile.keys()
-dbs=cache["_dbs"]
+cache[db] = events
+cache["_dbs"] = fullprofile.keys()
+dbs = cache["_dbs"]
 
-total=fullprofile[db]["-"]["-total"]
+total = fullprofile[db]["-"]["-total"]
 
 #cache.close()
-if sort=="name":
-    events.sort(lambda x,y: cmp(x[0], y[0]))
+if sort == "name":
+    events.sort(lambda x, y: cmp(x[0], y[0]))
 else:
-    events.sort(lambda y,x: cmp(x[1][sort], y[1][sort]))
+    events.sort(lambda y, x: cmp(x[1][sort], y[1][sort]))
+
 
 def surl(stype, stext=None, limit=50):
-    if (stext == None):
+    if (stext is None):
         stext = stype
 
     if stype == sort:
-        return """<td>%s</td>"""%stext
+        return """<td>%s</td>""" % stext
     return """<td><a href='report.py?db=%s&sort=%s&limit=%d'>%s</a></td>""" % (db, stype, limit, stext)
 
 print """
@@ -102,11 +104,11 @@ tr.head td { text-align: center; }
 
 for dbname in dbs:
     if db == dbname:
-        print " [%s] "% dbname
+        print " [%s] " % dbname
     else:
         print " [<a href='report.py?db=%s'>%s</a>] " % (dbname, dbname)
 
-if limit==50:
+if limit == 50:
     print " [ showing %d events, <a href='report.py?db=%s&sort=%s&limit=5000'>show more</a> ] " % (limit, db, sort)
 else:
     print " [ showing %d events, <a href='report.py?db=%s&sort=%s&limit=50'>show less</a> ] " % (limit, db, sort)
@@ -117,10 +119,10 @@ print """
 <tr class="head">"""
 print surl("name")
 print surl("count")
-print surl("cpu","cpu%")
-print surl("onecpu","cpu/c")
-print surl("real","real%")
-print surl("onereal","real/c")
+print surl("cpu", "cpu%")
+print surl("onecpu", "cpu/c")
+print surl("real", "real%")
+print surl("onereal", "real/c")
 print "</tr>"
 
 rowformat = u"""<tr class="data"><td class="name">%s</td><td>%d</td>
@@ -131,12 +133,12 @@ for event in events:
         continue
     if not event[0].startswith(prefix):
         continue
-        
+
     limit -= 1
     if limit < 0:
         break
     row = rowformat % \
-        (event[0].replace(",",", "), event[1]["count"], event[1]["cpu"] / total["cpu"] * 100, event[1]["onecpu"]  *1000,
+        (event[0].replace(",", ", "), event[1]["count"], event[1]["cpu"] / total["cpu"] * 100, event[1]["onecpu"] * 1000,
             event[1]["real"] / total["real"] * 100, event[1]["onereal"] * 1000)
     print utf8(row)[0]
 
