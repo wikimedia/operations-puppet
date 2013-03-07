a6d8fd85e6912a88ab838eed185f96e7a6db8321
DZ <dzahn@wikimedia.org>
2013-03-07 11:42:09 -0800

    merge ./misc/nagios.pp into icinga.pp
    Change-Id: Id654711831996dba08080832a2b4be21623ae2e9

--- manifests/misc/icinga.pp
+++ manifests/misc/icinga.pp
@@ -852,3 +852,8 @@ class icinga::ganglia::ganglios {
 		owner => icinga;
 	}
 }
+
+# global monitoring groups - formerly misc/nagios.pp
+
+@monitor_group { "misc_eqiad": description => "eqiad misc servers" }
+@monitor_group { "misc_pmtpa": description => "pmtpa misc servers" }
