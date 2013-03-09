9947138bd2a2cbbc711e68e59925e272ce05c6d8
Mark Bergsma <mark@wikimedia.org>
2013-03-09 17:37:18 +0100

    Rewrite varnish.py to use the varnishstat JSON interface
    varnishstat in varnish 3.0 has a JSON interface that is easier
    to parse and provides more info.
    
    The plugin can now also write its own config file,
    which is useful as it allows new metrics to be detected
    and added on the fly.
    
    Because the varnish instance (hostname if empty) is now
    always prepended to the metric name, this change is
    backwards incompatible.
    
    Change-Id: I2e44affa6d6f5726407879c08574b21b802a9d25

--- manifests/varnish.pp
+++ manifests/varnish.pp
@@ -148,16 +148,23 @@ class varnish {
 	}
 
 	class monitoring::ganglia($varnish_instances=['']) {
-		file {
-			"/usr/lib/ganglia/python_modules/varnish.py":
-				require => File["/usr/lib/ganglia/python_modules"],
-				source => "puppet:///files/ganglia/plugins/varnish.py",
-				notify => Service[gmond];
-			"/etc/ganglia/conf.d/varnish.pyconf":
-				require => File["/etc/ganglia/conf.d"],
-				content => template("ganglia/plugins/varnish.pyconf.erb"),
+		file { "/usr/lib/ganglia/python_modules/varnish.py":
+			require => File["/usr/lib/ganglia/python_modules"],
+			source => "puppet:///files/ganglia/plugins/varnish.py";
+		}
+
+		exec {
+			"generate varnish.pyconf":
+				require => File["/usr/lib/ganglia/python_modules/varnish.py", "/etc/ganglia/conf.d"],
+				command => "/usr/bin/python /usr/lib/ganglia/python/modules/varnish.py > /etc/ganglia/conf.d/varnish.pyconf.new";
+			"replace varnish.pyconf":
+				cwd => "/etc/ganglia/conf.d",
+				path => "/bin:/usr/bin",
+				unless => "diff -q varnish.pyconf.new varnish.pyconf",
+				command => "mv varnish.pyconf.new varnish.pyconf",
 				notify => Service[gmond];
 		}
+		Exec["generate varnish.pyconf"] -> Exec["replace varnish.pyconf"]
 	}
 
 	define setup_filesystem() {
