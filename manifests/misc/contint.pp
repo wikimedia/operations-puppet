0ff4e3777e4dbe73a40f36a8699036c612185c85
Antoine Musso <hashar@free.fr>
2013-03-12 21:34:27 +0100

    contint: get rid of Sun JDK
    We no more need the Sun JDK so just remove the class. Will purge the
    packages manually once this is merged.
    
    Change-Id: If4030ed7e6334f8c2df129183fe2e091d067a0ff

--- manifests/misc/contint.pp
+++ manifests/misc/contint.pp
@@ -1,44 +1,9 @@
-class misc::contint::jdk {
-# JDK for android continuous integration
-# extra stuff for license agreement acceptance
-# Based off of http://offbytwo.com/2011/07/20/scripted-installation-java-ubuntu.html
-
-	package { "debconf-utils":
-		ensure => installed
-	}
-
-	exec { "agree-to-jdk-license":
-		command => "/bin/echo -e sun-java6-jdk shared/accepted-sun-dlj-v1-1 select true | debconf-set-selections",
-		unless => "debconf-get-selections | grep 'sun-java6-jdk.*shared/accepted-sun-dlj-v1-1.*true'",
-		path => ["/bin", "/usr/bin"], require => Package["debconf-utils"],
-	}
-
-	exec { "agree-to-jre-license":
-		command => "/bin/echo -e sun-java6-jre shared/accepted-sun-dlj-v1-1 select true | debconf-set-selections",
-		unless => "debconf-get-selections | grep 'sun-java6-jre.*shared/accepted-sun-dlj-v1-1.*true'",
-		path => ["/bin", "/usr/bin"], require => Package["debconf-utils"],
-	}
-
-	package { "sun-java6-jdk":
-		ensure => latest,
-		require => [ Exec["agree-to-jdk-license"] ],
-	}
-
-	package { "sun-java6-jre":
-		ensure => latest,
-		require => [ Exec["agree-to-jre-license"] ],
-	}
-
-}
-
 class misc::contint::android::sdk {
 	# Class installing prerequisites to the Android SDK
 	# The SDK itself need to be installed manually for now.
 	#
 	# Help link: http://developer.android.com/sdk/installing.html
 
-	# We really want Sun/Oracle JDK
-	require misc::contint::jdk
 	include generic::packages::ant18
 
 	# 32bit libs needed by Android SDK
