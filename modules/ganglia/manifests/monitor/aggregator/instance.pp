ac1f1850fd729e405adc60917e654347fc5e877c
Mark Bergsma <mark@wikimedia.org>
2013-03-13 14:46:30 +0100

    Initial attempt at a new Ganglia module
    Untested. Moves to a model with central aggregators, and
    no multicast.
    
    Change-Id: I0a00cc0ea51577b98332eac422a7ed1a1626ade6

---
+++ modules/ganglia/manifests/monitor/aggregator/instance.pp
@@ -0,0 +1,21 @@
+define ganglia::monitor::aggregator::instance() {
+	Class[ganglia::monitor::aggregator] -> Ganglia::Monitor::Aggregator::Instance[$title]
+	Ganglia::Monitor::Aggregator::Instance[$title] -> Service[ganglia-monitor-aggregator]
+
+	$aggregator = true
+
+	# TODO: support multiple $site
+	$cluster = $title
+	$id = $ganglia::configuration::clusters[$cluster]['id']
+	$portnr = $ganglia::configuration::base_port + $id
+	$gmond_port = $::realm ? {
+		production => $portnr,
+		labs => $::project_gid
+	}
+
+	file { "/etc/ganglia/aggregators/${id}.conf":
+		mode => 0444,
+		content => template("ganglia/gmond.conf.erb"),
+		notify => Service[$title]
+	}
+}
---
+++ modules/ganglia/manifests/monitor/config.pp
@@ -0,0 +1,17 @@
+class ganglia::monitor::config($cluster) {
+	require ganglia::monitor::packages
+
+	$aggregator = false
+	$id = $ganglia::configuration::clusters[$cluster]['id']
+	$portnr = $ganglia::configuration::base_port + $id
+	$gmond_port = $::realm ? {
+		production => $portnr,
+		labs => $::project_gid
+	}
+
+	file { "/etc/ganglia/gmond.conf":
+		mode => 0444,
+		content => template("ganglia/gmond.conf.erb"),
+		notify => Service["ganglia-monitor"]
+	}
+}
