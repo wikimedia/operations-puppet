3fb4c1fdae27b74f0ddc15e6a9bdd98ca173bb03
Mark Bergsma <mark@wikimedia.org>
2013-03-13 16:21:28 +0100

    Move the upstart job for a normal gmond to ganglia::monitor::service
    Change-Id: I8ec29e92a4b518c91b7df0dfa69ffdb18ac5ab31

--- modules/ganglia/manifests/monitor/packages.pp
+++ modules/ganglia/manifests/monitor/packages.pp
@@ -2,11 +2,4 @@ class ganglia::monitor::packages {
 	if !defined(Package["ganglia-monitor"]) {
 		package { "ganglia-monitor": ensure => latest }
 	}
-
-	file { "/etc/init/ganglia-monitor.conf":
-		source => "puppet:///modules/ganglia/upstart/ganglia-monitor.conf",
-		mode => 0444
-	}
-
-	upstart_job { "ganglia-monitor": }
 }
