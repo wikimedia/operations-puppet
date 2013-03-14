c222594bf60060dcccb9129d6a9429605cfb1ac5
Antoine Musso <hashar@free.fr>
2013-03-14 10:38:14 +0100

    puppet now manage jenkins ssh authorized_keys
    I am going to setup some slaves on labs and need an easy way to publish
    the authorized keys there.  The Jenkins master is gallium, from=<ip>
    should ensure that only gallium can connect to the slaves.
    
    The class dependency is required since puppet does not manage the home
    directory for the user jenkins.
    
    Change-Id: If719f2d5b610815e205b46d63b36bb64d95e2083

--- modules/jenkins/manifests/user.pp
+++ modules/jenkins/manifests/user.pp
@@ -2,13 +2,30 @@ class jenkins::user {
 
   include jenkins::group
 
+  $JENKINS_HOME = '/var/lib/jenkins'
+
   systemuser { 'jenkins':
     name       => 'jenkins',
-    home       => '/var/lib/jenkins',
+    home       => $JENKINS_HOME,
     managehome => false,
     shell      => '/bin/bash',
     gid        =>  'jenkins',
-    require    => Group['jenkins'];
+    require    => Group['jenkins'],
+  }
+
+  Ssh_authorized_key {
+    require => User['jenkins']
+  }
+
+  ssh_authorized_key {
+    'jenkins@gallium':
+      ensure  => present,
+      user    => 'jenkins',
+      type    => 'ssh-rsa',
+      key     => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw==',
+      target  => "${JENKINS_HOME}/.ssh/authorized_keys",
+      # Lame restriction from gallium
+      options => [ 'from=208.80.154.191' ],
   }
 
 }
