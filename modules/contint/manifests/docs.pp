54329587c3e78e6c5c2d4a766c39b1389161e44a
Timo Tijhof <ttijhof@wikimedia.org>
2013-03-15 08:32:11 +0100

    contint: Move docs.pp into contint
    Change-Id: I1a503f65ea15e160ae4b5e1a8110c5bfba37a348

--- modules/contint/manifests/docs.pp
+++ modules/contint/manifests/docs.pp
@@ -1,19 +1,3 @@
-class misc::docsite {
-	system_role { "misc::docsite": description => "doc site server" }
-	file {
-		'/etc/apache2/sites-available/doc.wikimedia.org':
-			path => '/etc/apache2/sites-available/doc.wikimedia.org',
-			mode => 0444,
-			owner => root,
-			group => root,
-			source => 'puppet:///files/apache/sites/doc.wikimedia.org';
-		'/srv/org/wikimedia/doc':
-			ensure => 'directory';
-	}
-
-	apache_site { docs: name => 'doc.wikimedia.org' }
-}
-
 class misc::docs::puppet {
 
 	git::clone { "puppetsource":
