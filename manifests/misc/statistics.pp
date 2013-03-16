6fb59e38e59c88c4938a6ca412598f6a7b3d5741
milimetric <dandreescu@wikimedia.org>
2013-03-15 20:15:19 -0400

    security fix for stat1 cron job
    Change-Id: I1aa52fc55321e19fe400fee94d40ea88bb053ee6

--- manifests/misc/statistics.pp
+++ manifests/misc/statistics.pp
@@ -676,22 +676,19 @@ define misc::statistics::rsync_job($source, $destination) {
 # Sets up daily cron jobs to run a script which
 # groups blog pageviews by url and emails them
 class misc::statistics::cron_blog_pageviews {
+	$script = "/usr/local/bin/blog.sh"
+
 	file {
-		"/usr/local/bin/blog.sh":
+		"$script":
 			mode    => 0755,
 			content => template("misc/email-blog-pageviews.erb");
-		"/home/dandreescu/blog_cron_output":
-			ensure => "directory",
-			owner  => "dandreescu",
-			group  => "wikidev",
-			mode   => 0755;
 	}
 
 	# Create a daily cron job to run the blog script
 	# This requires that the $misc::statistics::user::username
 	# user is installed on the source host.
 	cron { "rsync_${name}_logs":
-		command => "/usr/local/bin/blog.sh",
+		command => "$script",
 		user    => "$misc::statistics::user::username",
 		hour    => 8,
 		minute  => 0,
