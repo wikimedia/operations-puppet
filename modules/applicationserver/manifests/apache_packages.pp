dab60f7999050d5d35214a41f31bef0884249e2c
Faidon Liambotis <faidon@wikimedia.org>
2013-03-18 23:14:37 +0200

    appserver: don't install a particular PHP version
    puppet tries to install an older version of PHP than the current one and
    fails. Remove the version altogether, just use "present".
    
    Change-Id: Ic4f9331d4b41efef6559a423b14882b690daa640

--- modules/applicationserver/manifests/apache_packages.pp
+++ modules/applicationserver/manifests/apache_packages.pp
@@ -1,7 +1,7 @@
 class applicationserver::apache_packages {
 	include applicationserver::packages
 
-	package { "libapache2-mod-php5":
-		ensure => "5.3.10-1ubuntu3.4+wmf1";
+	package { 'libapache2-mod-php5':
+		ensure => present,
 	}
 }
