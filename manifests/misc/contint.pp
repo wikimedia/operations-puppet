63ccc160914f72f87bef3d6d591ab6082b70bec6
DZ <dzahn@wikimedia.org>
2013-03-17 19:15:44 -0700

    puppet-lint: fix all "ERROR: tab character found", :retab with 2-space soft tabs per puppet style guide
    Change-Id: I9ae906c768319e6d81e8208d3be75996f0cda91c

--- manifests/misc/contint.pp
+++ manifests/misc/contint.pp
@@ -1,107 +1,107 @@
 class misc::contint::android::sdk {
-	# Class installing prerequisites to the Android SDK
-	# The SDK itself need to be installed manually for now.
-	#
-	# Help link: http://developer.android.com/sdk/installing.html
+  # Class installing prerequisites to the Android SDK
+  # The SDK itself need to be installed manually for now.
+  #
+  # Help link: http://developer.android.com/sdk/installing.html
 
-	include generic::packages::ant18
+  include generic::packages::ant18
 
-	# 32bit libs needed by Android SDK
-	# ..but NOT just all of ia32-libs ..
-	package { [
-		"libstdc++6:i386",
-		"libgcc1:i386",
-		"zlib1g:i386",
-		"libncurses5:i386",
-		"libsdl1.2debian:i386",
-		"libswt-gtk-3.5-java"
-		]: ensure => installed;
-	}
+  # 32bit libs needed by Android SDK
+  # ..but NOT just all of ia32-libs ..
+  package { [
+    "libstdc++6:i386",
+    "libgcc1:i386",
+    "zlib1g:i386",
+    "libncurses5:i386",
+    "libsdl1.2debian:i386",
+    "libswt-gtk-3.5-java"
+    ]: ensure => installed;
+  }
 }
 
 # CI test server as per RT #1204
 class misc::contint::test {
 
-	system_role { "misc::contint::test": description => "continuous integration test server" }
+  system_role { "misc::contint::test": description => "continuous integration test server" }
 
-	class packages {
-		include contint::packages
-	}
+  class packages {
+    include contint::packages
+  }
 
-	class jenkins {
+  class jenkins {
 
-		# Load the Jenkins module
-		include ::jenkins
+    # Load the Jenkins module
+    include ::jenkins
 
-		# We need a basic site to publish nightly builds in
-		include contint::website
+    # We need a basic site to publish nightly builds in
+    include contint::website
 
-		include contint::proxy_jenkins
+    include contint::proxy_jenkins
 
-		# Get several OpenJDK packages including the jdk.
-		# (openjdk is the default distribution for the java define.
-		# The java define is found in modules/java/manifests/init.pp )
-		java { 'java-6-openjdk': version => 6, alternative => true  }
-		java { 'java-7-openjdk': version => 7, alternative => false }
+    # Get several OpenJDK packages including the jdk.
+    # (openjdk is the default distribution for the java define.
+    # The java define is found in modules/java/manifests/init.pp )
+    java { 'java-6-openjdk': version => 6, alternative => true  }
+    java { 'java-7-openjdk': version => 7, alternative => false }
 
-		include ::contint::tmpfs
+    include ::contint::tmpfs
 
-		file {
-			"/var/lib/jenkins/.gitconfig":
-				mode => 0444,
-				owner => "jenkins",
-				group => "jenkins",
-				ensure => present,
-				source => "puppet:///files/misc/jenkins/gitconfig",
-				require => User['jenkins'];
-		}
+    file {
+      "/var/lib/jenkins/.gitconfig":
+        mode => 0444,
+        owner => "jenkins",
+        group => "jenkins",
+        ensure => present,
+        source => "puppet:///files/misc/jenkins/gitconfig",
+        require => User['jenkins'];
+    }
 
-		file {
-			"/var/lib/jenkins/.git":
-				mode   => 2775,  # group sticky bit
-				group  => "jenkins",
-				ensure => directory;
-			"/var/lib/jenkins/bin":
-				owner => "jenkins",
-				group => "wikidev",
-				mode => 0775,
-				ensure => directory;
-		}
+    file {
+      "/var/lib/jenkins/.git":
+        mode   => 2775,  # group sticky bit
+        group  => "jenkins",
+        ensure => directory;
+      "/var/lib/jenkins/bin":
+        owner => "jenkins",
+        group => "wikidev",
+        mode => 0775,
+        ensure => directory;
+    }
 
-	}
+  }
 
-	# prevent users from accessing port 8080 directly (but still allow from localhost and own net)
+  # prevent users from accessing port 8080 directly (but still allow from localhost and own net)
 
-	class iptables-purges {
+  class iptables-purges {
 
-		require "iptables::tables"
+    require "iptables::tables"
 
-		iptables_purge_service{  "deny_all_http-alt": service => "http-alt" }
-	}
+    iptables_purge_service{  "deny_all_http-alt": service => "http-alt" }
+  }
 
-	class iptables-accepts {
+  class iptables-accepts {
 
-		require "misc::contint::test::iptables-purges"
+    require "misc::contint::test::iptables-purges"
 
-		iptables_add_service{ "lo_all": interface => "lo", service => "all", jump => "ACCEPT" }
-		iptables_add_service{ "localhost_all": source => "127.0.0.1", service => "all", jump => "ACCEPT" }
-		iptables_add_service{ "private_all": source => "10.0.0.0/8", service => "all", jump => "ACCEPT" }
-		iptables_add_service{ "public_all": source => "208.80.152.0/22", service => "all", jump => "ACCEPT" }
-	}
+    iptables_add_service{ "lo_all": interface => "lo", service => "all", jump => "ACCEPT" }
+    iptables_add_service{ "localhost_all": source => "127.0.0.1", service => "all", jump => "ACCEPT" }
+    iptables_add_service{ "private_all": source => "10.0.0.0/8", service => "all", jump => "ACCEPT" }
+    iptables_add_service{ "public_all": source => "208.80.152.0/22", service => "all", jump => "ACCEPT" }
+  }
 
-	class iptables-drops {
+  class iptables-drops {
 
-		require "misc::contint::test::iptables-accepts"
+    require "misc::contint::test::iptables-accepts"
 
-		iptables_add_service{ "deny_all_http-alt": service => "http-alt", jump => "DROP" }
-	}
+    iptables_add_service{ "deny_all_http-alt": service => "http-alt", jump => "DROP" }
+  }
 
-	class iptables {
+  class iptables {
 
-		require "misc::contint::test::iptables-drops"
+    require "misc::contint::test::iptables-drops"
 
-		iptables_add_exec{ "${hostname}": service => "http-alt" }
-	}
+    iptables_add_exec{ "${hostname}": service => "http-alt" }
+  }
 
-	require "misc::contint::test::iptables"
+  require "misc::contint::test::iptables"
 }
