d2cee3de8df93f20ac240ffcda5b8ba686297b8c
Ryan Lane <rlane@wikimedia.org>
2013-03-20 21:50:11 -0700

    Use mysqldump --single-transaction where possible
    Change-Id: I14e6838a185ec8d84206bfa8f55583b6866823dd

--- files/openstack/db-bak.sh
+++ files/openstack/db-bak.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-for i in `mysql -BNe 'show databases'`
+for i in labswiki keystone nova 
 do
-        if [ "${i}" == "information_schema" ]
-        then   
-                continue
-        fi
+        nice -n 19 mysqldump --single-transaction -u root ${i} -c | nice -n 19 gzip -9 > /a/backup/${i}-$(date '+%Y%m%d').sql.gz
+done
+
+for i in glance mysql
+do
         nice -n 19 mysqldump -u root ${i} -c | nice -n 19 gzip -9 > /a/backup/${i}-$(date '+%Y%m%d').sql.gz
 done
