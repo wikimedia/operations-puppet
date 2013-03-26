87779041c7278776e0d5a2cb8f3c76c5ec77d7c4
Tim Starling <tstarling@wikimedia.org>
2013-03-26 14:25:50 +1100

    Move scap source location from fenari to tin
    Have the scap source locally on tin instead of on NFS. This should make
    it faster.
    
    Not being on NFS means cron jobs on hume can't access
    /home/wikipedia/common anymore. That's not really a problem since it's
    already a scap target, the scripts just need to be updated to use
    /usr/local/apache/common ($MW_COMMON) instead of /home. Many cron jobs
    were doing the right thing already.
    
    Made tin be the primary rsync server for the MW source, instead of nfs1.
    
    Coexisting with git-deploy on tin is a bit awkward since it conflicts
    with the migration plan we had assumed during the development of the
    git-deploy manifests. Just remove the class that creates the conflicting
    mw-deployment-vars.sh for now.
    
    Use dsh instead of ddsh, since ddsh is an unpuppetized hack.
    
    Change-Id: I9d315c4cb937389fb881535816a73827db5933cb

--- manifests/misc/maintenance.pp
+++ manifests/misc/maintenance.pp
@@ -143,7 +143,7 @@ class misc::maintenance::translationnotifications( $enabled = inline_template("<
 class misc::maintenance::tor_exit_node( $enabled = inline_template("<%= $::site == $::primary_site  %>") ) {
 	cron {
 		tor_exit_node_update:
-			command => "php /usr/local/apache/common/multiversion/MWScript.php extensions/TorBlock/loadExitNodes.php aawiki --force > /dev/null",
+			command => "/usr/local/bin/mwscript extensions/TorBlock/loadExitNodes.php --wiki=aawiki --force > /dev/null",
 			user => apache,
 			minute => '*/20',
 			ensure => $enabled ?{
