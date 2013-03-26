50b39ff06895608b3503087c57552e7483720083
pyoungmeister <py@wikimedia.org>
2013-03-26 15:05:54 -0700

    fixing scope of icinga_config_dir in mysql.pp
    Change-Id: I07e6f8cdb7f4db487f4eacc824f6caa5b70fb46f

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -163,7 +163,7 @@ class mysql_wmf {
 		$mysql_check_pass = $passwords::nagios::mysql::mysql_check_pass
 
 		file {
-			"${icinga_config_dir}/nrpe.d/nrpe_percona.cfg":
+			"${icinga::monitor::configuration::variables::icinga_config_dir}/nrpe.d/nrpe_percona.cfg":
 				owner => root,
 				group => nagios,
 				mode => 0440,
