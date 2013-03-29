fcac10e87f1d713196201a34b2b9284cf47f5a83
DZ <dzahn@wikimedia.org>
2013-03-28 18:37:19 -0700

    add a script and cron to mail out bugzilla audit log and move bugzilla scripts to files/bugzilla instead of misc
    Change-Id: I7b49e82857b32a46e6109156ebe49078841cb5e3

---
+++ files/bugzilla/bugzilla_audit_log.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# send the Bugzilla audit log to BZ admin(s)
+# per RT-4802 - dzahn 20130328
+
+declare rcpt_address='bugzillaadmin@wikimedia.org'
+declare sndr_address='4802@rt.wikimedia.org'
+
+# reads db user/pass/host from bugzilla config
+declare bugzilla_path='/srv/org/wikimedia/bugzilla'
+declare -a config_var=(host name user pass)
+declare -A my_var
+declare script_user='www-data'
+
+define(){ IFS='\n' read -r -d '' ${1} || true; }
+
+for mv in "${config_var[@]}"; do
+	my_var[$mv]=$(grep db_${mv} ${bugzilla_path}/localconfig | cut -d\' -f2 | sed 's/;/\\\;/g')
+done
+
+# fix if there is a ; in the pass itself
+mypass=$(echo ${my_var[pass]} | sed 's/\\//g')
+
+my_result=$(/usr/bin/mysql -h ${my_var[host]} -u${my_var[user]} ${my_var[name]} -p${mypass}<< END
+
+select * from audit_log order by at_time desc;
+
+END
+)
+
+# the actual email
+cat <<EOF | /usr/bin/mail -s "bugzilla audit log" ${rcpt_address} -- -f ${sndr_address}
+
+Hi Bugzilla admins,
+
+this is your automatic Bugzilla audit log mail:
+
+$my_result
+
+Yours sincerely,
+
+Bugs Zilla
+
+(via $(basename $0) on $(hostname) at $(date))
+EOF
+
