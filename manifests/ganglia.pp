49334334afd87abf76836d705bbe19646346afb8
Andrew Otto <otto@wikimedia.org>
2013-04-01 12:04:38 -0400

    Adding define ganglia::view for abstracting ganglia web views.
    Adding udp2log view on ganglia.wikimedia.org
    
    Change-Id: Ie3ab11316cf89e6ea1c6ec0edb2ea8a80947d9ab

--- manifests/ganglia.pp
+++ manifests/ganglia.pp
@@ -412,6 +412,7 @@ class ganglia::web {
 		$ganglia_servername = "ganglia.wmflabs.org"
 		$ganglia_serveralias = "aggregator1.pmtpa.wmflabs"
 		$ganglia_webdir = "/usr/share/ganglia-webfrontend"
+		$ganglia_confdir = "/var/lib/ganglia/conf"
 
 		require ganglia::aggregator
 
@@ -421,6 +422,9 @@ class ganglia::web {
 		# TODO(ssmollett): when switching to ganglia-webfrontend
 		# package, use /usr/share/ganglia-webfrontend
 		$ganglia_webdir = "/srv/org/wikimedia/ganglia-web-3.5.4+security"
+		# Why is this set to a different directory than $ganglia_webdir???
+		$ganglia_confdir = "/srv/org/ganglia_storage/3.5.1/conf"
+
 		$ganglia_ssl_cert = "/etc/ssl/certs/star.wikimedia.org.pem"
 		$ganglia_ssl_key = "/etc/ssl/private/star.wikimedia.org.key"
 	}
@@ -538,3 +542,79 @@ class ganglia::logtailer {
 	}
 }
 
+
+# == Define ganlia::view
+# Defines a Ganglia view JSON file.
+# See http://sourceforge.net/apps/trac/ganglia/wiki/ganglia-web-2#JSONdefinitionforviews
+# for documentation on Ganglia view JSON format.
+#
+# == Parameters:
+# $graphs       - Shortcut for of describing items that represent aggregate_graphs.
+# $items        - Should match exactly the JSON structure expected by Ganglia for views.
+# $view_type    - If you are using aggregate_graphs, this must be set to 'standard'.
+#                 'regex' will allow you to use non-aggregate graphs and match hostnames by regex.
+#                 Default: 'standard'.
+# $default_size - Default size for graphs.  Default: 'large'.
+# $conf_dir     - Path to directory where ganglia view JSON files should live.
+#                 Defaults to the appropriate directory based on WMF $::realm.  Default: to /var/lib/ganglia/conf
+# $template     - The ERb template to use for the JSON file.  Only change this if you need to do fancier things than this define allows.
+#
+# == Examples:
+# # A 'regex' (non aggregate graph) view:
+# # Note that no aggregate_graphs are used.
+# # This will add 4 graphs to the 'cpu' view.
+# # (i.e. cpu_user and cpu_system for each myhost and myhost1)
+# $host_regex = 'myhost[01]'
+# ganglia::view { 'cpu':
+#   view_type => 'regex',
+#   items     => [
+#     {
+#       'metric'   => 'cpu_user',
+#       'hostname' => $host_regex,
+#     }
+#     {
+#       'metric'   => 'cpu_system',
+#       'hostname' => $host_regex,
+#     }
+#   ],
+# }
+#
+#
+# # Use the $graphs parameter to describe aggregate graphs.
+# # You can describe the same graphs to add with $items.
+# # $graphs is just a shortcut.  aggregate_graphs in $items
+# # are a bit overly verbose.
+# $host_regex = 'locke|emery|oxygen|gadolinium'
+# ganglia::view { 'udp2log':
+#   graphs => [
+#     {
+#       'host_regex'   => $host_regex,
+#       'metric_regex' => 'packet_loss_average',
+#     }
+#     {
+#       'host_regex'   => $host_regex,
+#       'metric_regex' => 'drops',
+#     }
+#     {
+#       'host_regex'   => $host_regex,
+#       'metric_regex' => 'packet_loss_90th',
+#     }
+#   ],
+# }
+#
+define ganglia::view(
+	$graphs       = [],
+	$items        = [],
+	$view_type    = 'standard',
+	$default_size = 'large',
+	$conf_dir     = $ganglia::web::ganglia_confdir,
+	$template     = 'ganglia/ganglia_view.json.erb')
+{
+	require ganglia::web
+
+	# require ganglia::web
+	$view_name = $name
+	file { "${conf_dir}/view_${name}.json":
+		content => template($template),
+	}
+}
