7adc154e0a854e31e5b7428efff895c5e0347895
Faidon Liambotis <faidon@wikimedia.org>
2013-04-02 17:35:56 +0300

    upload varnish: switch everything to Swift
    Squids now pass everything to Swift, so remove this extra hop for
    non-matching Swift URLs and divert everything from Varnishes to Swift.
    
    Change-Id: Id5535bcae04e6f8c54c21bc46531aae9c2cf5e08

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -386,8 +386,7 @@ class role::cache {
 					"image_scalers" => $lvs::configuration::lvs_service_ips[$::realm]['rendering'][$::mw_primary],
 				},
 				"eqiad" => {
-					"backend" => $lvs::configuration::lvs_service_ips[$::realm]['upload']['pmtpa']['uploadsvc'],
-					"swift" => $lvs::configuration::lvs_service_ips[$::realm]['swift']['pmtpa'],
+					"backend" => $lvs::configuration::lvs_service_ips[$::realm]['swift']['pmtpa'],
 					"image_scalers" => $lvs::configuration::lvs_service_ips[$::realm]['rendering'][$::mw_primary],
 				},
 				"esams" => {
