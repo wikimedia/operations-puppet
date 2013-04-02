3af309e0c1fc5c4e6cb59d3271da4b39237f2625
jgreen <jgreen@wikimedia.org>
2013-04-02 13:43:49 -0400

    fundraising banner rotation tweaks
    Change-Id: If058e3c36677172386baa7d82531006cdf5f48a6

--- manifests/misc/fundraising.pp
+++ manifests/misc/fundraising.pp
@@ -445,25 +445,29 @@ class misc::fundraising::jenkins_maintenance {
 
 class misc::fundraising::udp2log_rotation {
 
-	include accounts::file_mover
+	#include accounts::file_mover
 
-	sudo_user { "file_mover": privileges => ['ALL = NOPASSWD: /usr/bin/killall -HUP udp2log'] }
+	sudo_user { "udp2log": privileges => ['ALL = NOPASSWD: /usr/bin/killall -HUP udp2log'] }
 
 	file {
 		'/usr/local/bin/rotate_fundraising_logs':
-			owner => file_mover,
-			group => file_mover,
+			owner => udp2log,
+			group => udp2log,
 			mode => 0554,
 			source => 'puppet:///files/misc/scripts/rotate_fundraising_logs';
+		'/a/log/fundraising/logs/':
+			owner => udp2log,
+			group => udp2log,
+			mode => 0750;
 	}
 
-	cron {
-		'rotate_fundraising_logs':
-			user => file_mover,
-			minute => '*/15',
-			command => '/usr/local/bin/rotate_fundraising_logs',
-			ensure => present;
-	}
+	#cron {
+	#	'rotate_fundraising_logs':
+	#		user => udp2log,
+	#		minute => '*/15',
+	#		command => '/usr/local/bin/rotate_fundraising_logs',
+	#		ensure => present;
+	#}
 
 	#class { "nfs::netapp::fr_archive": mountpoint => "/a/squid/fundraising/logs/fr_archive" }
 	class { "nfs::netapp::fr_archive": mountpoint => "/a/log/fundraising/logs/fr_archive" }
