346e2a8b4b7969a7d5f1f4d7b421356272f49fd8
pyoungmeister <py@wikimedia.org>
2013-04-05 14:51:13 -0700

    updating labsdb role class
    also whitespace
    
    Change-Id: I42322b9ee3be78c8a6f636e1b03bb5fca564bbf1

--- manifests/role/db.pp
+++ manifests/role/db.pp
@@ -13,54 +13,82 @@ class role::db::core {
 
 
 class role::db::sanitarium( $instances = {} ) {
-  ## $instances must be a 2-level hash of the form:
-  ## 'shard9001' => { port => NUMBER, innodb_log_file_size => "CORRECT_M", ram => "HELLA_G" },
-  ## 'shard9002' => { port => NUMBER+1, innodb_log_file_size => "CORRECT_M", ram => "HELLA_G" },
-   $cluster = "mysql"
+## $instances must be a 2-level hash of the form:
+## 'shard9001' => { port => NUMBER, innodb_log_file_size => "CORRECT_M", ram => "HELLA_G" },
+## 'shard9002' => { port => NUMBER+1, innodb_log_file_size => "CORRECT_M", ram => "HELLA_G" },
+	$cluster = "mysql"
 
-   system_role {"role::db::sanitarium": description => "pre-labsdb dbs for Data Sanitization" }
+	system_role {"role::db::sanitarium": description => "pre-labsdb dbs for Data Sanitization" }
 
-   include standard,
-    cpufrequtils,
-    mysql_multi_instance
+	include standard,
+		cpufrequtils,
+		mysql_multi_instance
 
-   class { mysql :
-    package_name => 'mariadb-client-5.5'
-   }
+	class { mysql :
+		package_name => 'mariadb-client-5.5'
+	}
 
-  ## for key in instances, make a mysql instance. need port, innodb_log_file_size, and amount of ram
-  $instances_keys = keys($instances)
-  mysql_multi_instance::instance { $instances_keys :
-    instances => $instances
-  }
+	## for key in instances, make a mysql instance. need port, innodb_log_file_size, and amount of ram
+	$instances_keys = keys($instances)
+	mysql_multi_instance::instance { $instances_keys :
+		instances => $instances
+	}
 
-  ## some per-node monitoring here
-  $instances_count = size($instances_keys)
+	## some per-node monitoring here
+	$instances_count = size($instances_keys)
 
-  file { "/usr/lib/nagios/plugins/percona":
-    ensure => directory,
-    recurse => true,
-    owner => root,
-    group => root,
-    mode => 0555,
-    source => "puppet:///files/icinga/percona";
-  }
+	file { "/usr/lib/nagios/plugins/percona":
+		ensure => directory,
+		recurse => true,
+		owner => root,
+		group => root,
+		mode => 0555,
+		source => "puppet:///files/icinga/percona";
+	}
 
-  nrpe::monitor_service { "mysqld":
-    description => "mysqld processes",
-    nrpe_command => "/usr/lib/nagios/plugins/check_procs -c ${instances_count}:${instances_count} -C mysqld"
-  }
+	nrpe::monitor_service { "mysqld":
+		description => "mysqld processes",
+		nrpe_command => "/usr/lib/nagios/plugins/check_procs -c ${instances_count}:${instances_count} -C mysqld"
+	}
 
 }
 
 class role::db::labsdb( $instances = {} ) {
-  $cluster = "mysql"
+## $instances must be a 2-level hash of the form:
+## 'shard9001' => { port => NUMBER, innodb_log_file_size => "CORRECT_M", ram => "HELLA_G" },
+## 'shard9002' => { port => NUMBER+1, innodb_log_file_size => "CORRECT_M", ram => "HELLA_G" },
+	$cluster = "mysql"
 
-  system_role {"role::db::labsdb": description => "labsdb dbs for labs use" }
+	system_role {"role::db::labsdb": description => "labsdb dbs for labs use" }
 
-  include standard
+	include standard,
+		cpufrequtils,
+		mysql_multi_instance
 
-  class { mysql :
-    package_name => 'mariadb-client-5.5'
-  }
+	class { mysql :
+		package_name => 'mariadb-client-5.5'
+	}
+
+	## for key in instances, make a mysql instance. need port, innodb_log_file_size, and amount of ram
+	$instances_keys = keys($instances)
+	mysql_multi_instance::instance { $instances_keys :
+		instances => $instances
+	}
+
+	## some per-node monitoring here
+	$instances_count = size($instances_keys)
+
+	file { "/usr/lib/nagios/plugins/percona":
+		ensure => directory,
+		recurse => true,
+		owner => root,
+		group => root,
+		mode => 0555,
+		source => "puppet:///files/icinga/percona";
+	}
+
+	nrpe::monitor_service { "mysqld":
+		description => "mysqld processes",
+		nrpe_command => "/usr/lib/nagios/plugins/check_procs -c ${instances_count}:${instances_count} -C mysqld"
+	}
 }
