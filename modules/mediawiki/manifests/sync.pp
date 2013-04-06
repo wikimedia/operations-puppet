4096d086cc8a089287d04cf7a8d0977cd3d425dd
Ori Livneh <ori@wikimedia.org>
2013-04-06 11:53:51 -0700

    Migrate scap-1, scap-2, & sync-common from wikimedia-task-appserver
    This change migrates three scripts from wikimedia-task-appserver:
    
    - scap-1 (at ff2d5a7fe3 / Gerrit change Iae053bd09)
    - scap-2 (at ea5a1a889a / Gerrit change I64d6246ed)
    - sync-common (at 66fc5c65cc /
        <http://www.mediawiki.org/wiki/Special:Code/MediaWiki/91703>)
    
    Managing these scripts in a single repository will make it easier to update
    their content and keep them in sync.
    
    Somewhat awkwardly, the resources representing these scripts must be declared
    in class mediawiki::sync, rather than misc::deployment::scap_scripts. This is
    because the scripts must be present on both the scap source server and all scap
    targets.
    
    Because scap already depends on sync-common, the cross-module dependency is
    already in place: misc::deployment::scap_scripts requires
    misc::deployment::common_scripts, which in turn requires mediawiki.
    
    The corresponding change to wikimedia-task-appserver to remove these scripts is
    I047f37b0a.
    
    I am unfortunately not able to test this thoroughly.
    
    RT: 3276
    Bug: 40025
    Change-Id: I33d93347f811906f37d503039af7035d122938b9

--- modules/mediawiki/manifests/sync.pp
+++ modules/mediawiki/manifests/sync.pp
@@ -1,18 +1,39 @@
 # mediawiki syncing class
 class mediawiki::sync {
 
-  require mediawiki::packages
-  require mediawiki::users::l10nupdate
+	require mediawiki::packages
+	require mediawiki::users::l10nupdate
+
+	$scriptpath = $misc::deployment::scap_scripts::scriptpath
+
+	file {
+		"${scriptpath}/scap-1":
+			owner  => root,
+			group  => root,
+			mode   => '0555',
+			source => 'puppet:///files/scap/scap-1';
+		"${scriptpath}/scap-2":
+			owner  => root,
+			group  => root,
+			mode   => '0555',
+			source => 'puppet:///files/scap/scap-2';
+		"${scriptpath}/sync-common":
+			owner  => root,
+			group  => root,
+			mode   => '0555',
+			source => 'puppet:///files/scap/sync-common';
+	}
 
 	exec { 'mw-sync':
-		command => '/usr/bin/sync-common',
-		cwd => '/tmp',
-		user => root,
-		group => root,
-		path => '/usr/bin:/usr/sbin',
+		command     => "${scriptpath}/sync-common",
+		require     => File["${scriptpath}/sync-common"],
+		cwd         => '/tmp',
+		user        => root,
+		group       => root,
+		path        => "${scriptpath}:/usr/bin:/usr/sbin",
 		refreshonly => true,
-		timeout => 600,
-		logoutput => on_failure;
+		timeout     => 600,
+		logoutput   => on_failure;
 	}
 }
 
