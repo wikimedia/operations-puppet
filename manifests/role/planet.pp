b99defbd77290901c39ebcd9b49eb95b4997d4b8
Mark Bergsma <mark@wikimedia.org>
2013-04-10 14:36:44 +0200

    Revert "make the planet logo a "per language" thing"
    This reverts commit f2264e7a97f26b6b2d6548e9d730e01ce42baebe.
    
    Revert "resource references should now be capitalized"
    
    This reverts commit 54d5baca9de9ea519c216ceb08030eb93cffc674.
    
    Revert "remove duplicate definition of locales package"
    
    This reverts commit ebd76e02e4d7322a57a605b33afde16e4bf93a72.
    
    Revert "turn planet into a puppet module"
    
    This reverts commit ec09aaefb8b9f7eafc82aaca5dff7e940c243f4e.
    
    Change-Id: I093cfc85d1ca8550c19a246ae9ed8fb0e91149e3

---
+++ manifests/role/planet.pp
@@ -0,0 +1,123 @@
+# planet RSS feed aggregator 2.0 (planet-venus)
+
+class role::planet {
+
+	system_role { "role::planet": description => "Planet (venus) weblog aggregator" }
+
+	# locales are essential for planet. if a new language is added check these too
+	include standard,
+	generic::locales::international
+
+	# be flexible about labs vs. prod
+	case $::realm {
+		labs: {
+			$planet_domain_name = 'wmflabs.org'
+		}
+		production: {
+			$planet_domain_name = 'wikimedia.org'
+		}
+		default: {
+			fail('unknown realm, should be labs or production')
+		}
+	}
+
+	# webserver setup
+	install_certificate{ "star.planet.${planet_domain_name}": }
+	class {'webserver::php5': ssl => 'true'; }
+	apache_module { rewrite: name => "rewrite" }
+
+	# dependencies
+	Class['webserver::php5'] -> apache_module["rewrite"] -> Install_certificate["star.planet.${planet_domain_name}"]
+
+	# List all planet languages and translations for
+	# index.html.tmpl here.	 Configurations, directories and
+	# cronjobs are auto-created from this hash.
+	$planet_languages = {
+		ar => {
+			'subscribe' => 'اشترك',
+			'subscriptions' => 'الاشتراكات',
+			'lastupdated' => 'اخر تحديث',
+		},
+		ca => {
+			'subscribe' => 'Subscriure\'s',
+			'subscriptions' => 'Subscripcions',
+			'lastupdated' => 'Última actualització',
+		},
+		cs => {
+			'subscribe' => 'Přihlásit odběr',
+			'subscriptions' => 'Odběry',
+			'lastupdated' => 'Poslední aktualizace',
+		},
+		de => {
+			'subscribe' => 'Abonnieren',
+			'subscriptions' => 'Teilnehmer',
+			'lastupdated' => 'Zuletzt aktualisiert',
+		},
+		en => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		es => { 'subscribe' => 'Suscribirse',
+			'subscriptions' => 'Suscripciones',
+			'lastupdated' => 'Última actualización',
+		},
+		fr => {
+			'subscribe' => 'S\'abonner',
+			'subscriptions' => 'Abonnements',
+			'lastupdated' => 'Dernière mise à jour',
+		},
+		gmq => {
+			'subscribe' => 'Abonnér',
+			'subscriptions' => 'Abonnementer',
+			'lastupdated' => 'Senest opdateret',
+		},
+		it => {
+			'subscribe' => 'Abbonati',
+			'subscriptions' => 'Sottoscrizioni',
+			'lastupdated' => 'Last updated',
+		},
+		ja => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		pl => {
+			'subscribe' => 'Subskrybuj',
+			'subscriptions' => 'Subskrypcje',
+			'lastupdated' => 'Ostatnia zmiana',
+		},
+		pt => {
+			'subscribe' => 'Subscrever',
+			'subscriptions' => 'Subcrições',
+			'lastupdated' => 'Última actualização',
+		},
+		ro => {
+			'subscribe' => 'Abonare',
+			'subscriptions' => 'Abonări',
+			'lastupdated' => 'Ultima actualizare',
+		},
+		ru => {
+			'subscribe' => 'Подписаться',
+			'subscriptions' => 'Подписки',
+			'lastupdated' => 'Последнее обновление',
+		},
+		sr => {
+			'subscribe' => 'Subscribe',
+			'subscriptions' => 'Subscriptions',
+			'lastupdated' => 'Last updated',
+		},
+		zh => {
+			'subscribe' => '訂閱',
+			'subscriptions' => '收錄',
+			'lastupdated' => '最近更新'
+		},
+	}
+
+	# the actual planet-venus class doing all the rest
+	class {'misc::planet-venus':
+		planet_domain_name => $planet_domain_name,
+		planet_languages => $planet_languages,
+	}
+}
+
