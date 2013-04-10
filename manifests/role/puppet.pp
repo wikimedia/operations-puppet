8da9e1b4d97bb3b84d0f2b5b8c3fefa091b9a347
Andrew Otto <otto@wikimedia.org>
2013-04-10 12:58:33 -0400

    Refactoring puppetmaster::self to allow for puppet clients.
    You should now use role::puppet::self instead.
    puppetmaster::self is retained for backwards compatiblity.
    
    Woowee, let's see if this works!
    
    Change-Id: I7d8d68684e726c79d56e9027423590b94957cbcd

--- manifests/role/puppet.pp
+++ manifests/role/puppet.pp
@@ -34,3 +34,42 @@ class role::puppet::server::labs {
 		};
 	}
 }
+
+
+# == Class role::puppet::self
+# Wrapper class for puppet::self::master
+# and puppet::self::client.
+# If $::puppetmaster is localhost or matches the $::fqdn of this node,
+# then this node will be configured as a puppetmaster.
+# NOTE:  $::puppetmaster == 'localhost' (or undef) does the exact same
+# thing as the original puppetmaster::self class used to do.
+#
+# $::puppetmaster must be set as a global variable.
+# This allows puppet classes to be configured via LDAP
+# and labsconsole instance configuration.
+#
+class role::puppet::self {
+	# If $::puppetmaster is not set, assume
+	# this is a self hosted puppetmaster, not allowed
+	# to serve any other puppet clients.
+	$server = $::puppetmaster ? {
+		undef       => 'localhost',
+		'localhost' => 'localhost',
+		''          => 'localhost',
+		default     => "${::puppetmaster}.${domain}",
+	}
+
+	# If localhost or if $server matches this node's
+	# $fqdn, then this is a puppetmaster.
+	if ($server == 'localhost' or $server == $::fqdn) {
+		class { 'puppet::self::master':
+			server => $server,
+		}
+	}
+	# Else this is a puppet client.
+	else {
+		class { 'puppet::self::client':
+			server => $server,
+		}
+	}
+}
