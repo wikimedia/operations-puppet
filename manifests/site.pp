0fa5f17aa75e8b80b5dd6ec84af75fb8f72e7a35
Tim Starling <tstarling@wikimedia.org>
2013-04-16 12:55:43 +1000

    Enable terbium cron jobs
    Per Iffd8a7cd. RobH removed refreshlinks and update_flaggedrev_stats,
    presumably because of a /home dependency, but I can sort that out later.
    
    Change-Id: I95d41bbbcf9754ec32a1021a0fa7a08a2759b9f7

--- manifests/site.pp
+++ manifests/site.pp
@@ -1087,19 +1087,22 @@ node "hume.wikimedia.org" {
 		admins::restricted,
 		nrpe
 
-	class { misc::maintenance::foundationwiki: enabled => true }
-	class { misc::maintenance::pagetriage: enabled => true }
-	class { misc::maintenance::refreshlinks: enabled => true }
-	class { misc::maintenance::translationnotifications: enabled => true }
-	class { misc::maintenance::wikidata: enabled => true }
-	class { misc::maintenance::tor_exit_node: enabled => true }
-	class { misc::maintenance::echo_mail_batch: enabled => true }
-	class { misc::maintenance::update_flaggedrev_stats: enabled => true }
-	class { misc::maintenance::update_special_pages: enabled => true }
-	class { misc::maintenance::parsercachepurging: enabled => true }
-	class { misc::maintenance::geodata: enabled => true }
-	class { misc::maintenance::cleanup_upload_stash: enabled => true }
 
+	# Moved to terbium
+	class { misc::maintenance::foundationwiki: enabled => false }
+	class { misc::maintenance::pagetriage: enabled => false }
+	class { misc::maintenance::translationnotifications: enabled => false }
+	class { misc::maintenance::wikidata: enabled => false }
+	class { misc::maintenance::tor_exit_node: enabled => false }
+	class { misc::maintenance::echo_mail_batch: enabled => false }
+	class { misc::maintenance::update_special_pages: enabled => false }
+	class { misc::maintenance::parsercachepurging: enabled => false }
+	class { misc::maintenance::geodata: enabled => false }
+	class { misc::maintenance::cleanup_upload_stash: enabled => false }
+
+	# These cron jobs were left behind for some reason (I7a786046)
+	class { misc::maintenance::refreshlinks: enabled => true }
+	class { misc::maintenance::update_flaggedrev_stats: enabled => true }
 }
 
 node "iron.wikimedia.org" {
@@ -2670,16 +2673,16 @@ node "terbium.eqiad.wmnet" {
 		admins::restricted,
 		nrpe
 
-	class { misc::maintenance::foundationwiki: enabled => false }
-	class { misc::maintenance::pagetriage: enabled => false }
-	class { misc::maintenance::translationnotifications: enabled => false }
-	class { misc::maintenance::wikidata: enabled => false }
-	class { misc::maintenance::tor_exit_node: enabled => false }
-	class { misc::maintenance::echo_mail_batch: enabled => false }
-	class { misc::maintenance::update_special_pages: enabled => false }
-	class { misc::maintenance::parsercachepurging: enabled => false }
-	class { misc::maintenance::geodata: enabled => false }
-	class { misc::maintenance::cleanup_upload_stash: enabled => false }
+	class { misc::maintenance::foundationwiki: enabled => true }
+	class { misc::maintenance::pagetriage: enabled => true }
+	class { misc::maintenance::translationnotifications: enabled => true }
+	class { misc::maintenance::wikidata: enabled => true }
+	class { misc::maintenance::tor_exit_node: enabled => true }
+	class { misc::maintenance::echo_mail_batch: enabled => true }
+	class { misc::maintenance::update_special_pages: enabled => true }
+	class { misc::maintenance::parsercachepurging: enabled => true }
+	class { misc::maintenance::geodata: enabled => true }
+	class { misc::maintenance::cleanup_upload_stash: enabled => true }
 
 }
 
