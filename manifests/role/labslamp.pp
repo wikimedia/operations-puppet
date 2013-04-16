83b9c6cb5fd1a9c5316033fff5d951be743954fe
Andrew Bogott <abogott@wikimedia.org>
2013-04-16 23:57:28 +0000

    Added role::lamp::labs
    There's chronic confusion about all the different webserver
    and mysql packages available... this should provide one
    true path for labs instances.
    
    Change-Id: I5fcb8a64687268bbea466ebe1bcc3fb7d3457fe3

---
+++ manifests/role/labslamp.pp
@@ -0,0 +1,12 @@
+#  Installs basic LAMP services on an instance:
+#
+#  - Apache
+#  - Mysql
+#  - PHP5
+# 
+#  The root mysql password is empty to start.  You should
+#  change it!
+class role::lamp::labs {
+
+    include "role::labs-mysql-server", "webserver::php5-mysql"
+}
