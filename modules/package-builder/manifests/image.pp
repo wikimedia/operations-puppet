784f62893ddab3f48c6caf30a2e1b308eb7200be
Antoine Musso <hashar@free.fr>
2013-04-17 16:14:06 +0200

    package builder now supports Debian.org unstable
    Whenever upstreaming a package to Debian.org, we want to be able to
    build it against their 'unstable' repository.  This adds to points to
    debian.org APT repositories whenever the distribution is 'unstable'.
    
    Had to add:
    * mirror parameter to let us override the default mirror with the Debian
      one.  That is set to the US Debian mirror whenever dist is `unstable`
    * debootstrapopt variable to pass the debian archive keyring
    
    Source list according to http://debgen.simplylinux.ch
    
    Change-Id: I4aa8bb5bed22dc982bc098b8357188b6f0017b4c

--- modules/package-builder/manifests/image.pp
+++ modules/package-builder/manifests/image.pp
@@ -17,6 +17,14 @@
 #  *pbuilder* is not set by default which means the distribution will be
 #  interpolated from the defined title (see *namevar*).
 #
+# [*mirror*]
+#  Main mirror to use. If not set, will use whatever mirror is configured
+#  by default.
+#
+#  For `unstable` that will default to the US debian mirror.
+#
+#  Default: undef (or US Debian mirror for `unstable` dist)
+#
 # [*components*]
 #  The repositories to use from the mirrors. Should be an array or a space
 #  delimited string. This is passed to the pbuilder package with
@@ -27,6 +35,8 @@
 #  The distribution to build for (hardy, lucid, precise..).  *dist* is not
 #  set by default which means the distribution will be interpolated from the
 #  defined title (see *namevar*).
+#  When using the dist 'unstable', the repository will be pointed to a
+#  debian.org mirror.
 #
 # === Examples
 #
@@ -43,7 +53,7 @@
 #   $images = [ 'cowbuilder-precise', 'cowbuilder-lucid' ],
 #   package-builder::image { $images: }
 #
-define package-builder::image( $pbuilder=undef, $components=['main','universe'], $dist=undef ) {
+define package-builder::image( $pbuilder=undef, $mirror=undef, $components=['main','universe'], $dist=undef ) {
   if $pbuilder {
     $realpbuilder = $pbuilder
   } else {
@@ -77,13 +87,35 @@ define package-builder::image( $pbuilder=undef, $components=['main','universe'],
     default: { fail('Only package builder types supported are pbuilder and cowbuilder') }
   }
 
+  # Hack to support Debian unstable
+  case $realdist {
+    'unstable': {
+      $debootstrapopts = '--debootstrapopts --keyring=/usr/share/keyrings/debian-archive-keyring.gpg'
+      # Make sure we override the Ubuntu default mirror
+      if $mirror == undef {
+        $mirror = 'http://ftp.us.debian.org/debian'
+      }
+      $othermirror = "--othermirror 'deb-src http://ftp.us.debian.org/debian ${realdist} main'"
+      $components = "--components 'main'"
+    }
+    default: {
+      $debootstrapopts = ''
+      $othermirror = "--othermirror 'deb http://apt.wikimedia.org/wikimedia ${realdist}-wikimedia main universe' --othermirror 'deb-src http://apt.wikimedia.org/wikimedia ${realdist}-wikimedia main universe'"
+      $components = "--components 'main universe'"
+    }
+  }
+
+  if $mirror {
+    $realmirror = "--mirror ${mirror}"
+  }
+
   $pbuilder_root = '/var/cache/pbuilder'
 
   $othermirror = "--othermirror 'deb http://apt.wikimedia.org/wikimedia ${realdist}-wikimedia ${realcomponents}' --othermirror 'deb-src http://apt.wikimedia.org/wikimedia ${realdist}-wikimedia ${realcomponents}'"
   $image_file = "${pbuilder_root}/${file_prefix}${realdist}.${file_ext}"
 
   exec { "imaging ${realdist} for ${realpbuilder}":
-    command   => "${realpbuilder} --create --distribution ${realdist} ${base_option} ${image_file} --components '${realcomponents}' ${othermirror}",
+    command   => "${realpbuilder} --create --distribution ${realdist} ${base_option} ${image_file} --components '${realcomponents}' ${realmirror} ${othermirror} ${debootstrapopts}",
     creates   => $image_file,
     path      => '/bin:/sbin:/usr/bin:/usr/sbin',
     timeout   => 600,
