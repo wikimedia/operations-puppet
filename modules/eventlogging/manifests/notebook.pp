22a905b5c958c2ec2f9140e56bc5ada99fdb6e2a
Ori Livneh <ori@wikimedia.org>
2013-04-17 13:59:44 -0700

    Parametrize and extend IPython Notebook class
    This change replaces hard-coded paths with class parameters and template
    expansion. It also configures IPython Notebook to run under supervisord.
    
    Also adds 'python-pexpect', which is a recommended package. (IPython Notebook
    ships with an inferior process management library, but it will use pexpect if
    available.)
    
    Change-Id: I0d0de6a4d00bb2bba512c2ae355b93028be7caff

--- modules/eventlogging/manifests/notebook.pp
+++ modules/eventlogging/manifests/notebook.pp
@@ -2,20 +2,41 @@
 # (like Mathematica), based on PyLab and IPython Notebook.
 # See <http://ipython.org/ipython-doc/dev/interactive/htmlnotebook.html>
 # and <http://www.scipy.org/PyLab>.
-class eventlogging::notebook {
+class eventlogging::notebook(
+	$ipython_dir = '/var/lib/ipython',
+	$ipython_user = 'ipython',
+	$notebook_dir = '/var/lib/ipython/notebooks'
+) {
 
 	package { [
 		'ipython-notebook',
 		'python-matplotlib',
 		'python-numpy',
 		'python-pandas',
+		'python-pexpect',
 		'python-scipy',
 	]:
 		ensure => latest,
 	}
 
-	systemuser { 'ipython':
-		name => 'ipython',
+	systemuser { $ipython_user:
+		name => $ipython_user,
+		home => $ipython_dir,
+	}
+
+	file { $notebook_dir:
+		ensure  => directory,
+		owner   => $ipython_user,
+		group   => $ipython_user,
+		mode    => '0555',
+		require => Systemuser[$ipython_user],
+	}
+
+	file { '/etc/supervisor/conf.d/notebook.conf':
+		content => template('/eventlogging/notebook.conf.erb'),
+		require => [ Package['ipython-notebook'], File[$notebook_dir] ],
+		notify  => Service['supervisor'],
+		mode    => '0444',
 	}
 
 }
