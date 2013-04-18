a87afa8373f2396b005131f973a755158df61870
Chad Horohoe <chadh@wikimedia.org>
2013-04-18 14:04:44 -0400

    Initial puppetization for git.wikimedia.org
    - Install apache
    - Setup replication destination for repositories
    - Create gitblit user
    
    Change-Id: Ic2f3a7fd82360c21845ac916db91f555806cbec0

---
+++ manifests/role/gitblit.pp
@@ -0,0 +1,13 @@
+# manifests/role/gitblit.pp
+
+class role::gitblit {
+	system_role { "role::gitblit": description => "Gitblit, a git viewer" }
+
+	include role::gerrit::production::replicationdest
+
+	class { "gitblit":
+		host => "git.wikimedia.org",
+		ssl_cert => "git.wikimedia.org",
+		ssl_cert_key => "git.wikimedia.org"
+	}
+}
