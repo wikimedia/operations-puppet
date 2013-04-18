d4fb847ccbab6020e905a625238180585f526632
Ori Livneh <ori@wikimedia.org>
2013-04-17 18:36:20 -0700

    Create IPython config based on $ipython_profile
    This change parametrizes the profile name and adds an exec resource that
    creates a default config for that profile if one does not already exist.
    Also documents parameters in RDoc format.
    
    Change-Id: Ib4c6e662740fc9c56f5c29bb58ce1badd535c89e

--- modules/eventlogging/manifests/notebook.pp
+++ modules/eventlogging/manifests/notebook.pp
@@ -1,9 +1,27 @@
+# == Class: eventlogging::notebook
+#
 # Configures an integrated, web-based numeric computation environment
 # (like Mathematica), based on PyLab and IPython Notebook.
 # See <http://ipython.org/ipython-doc/dev/interactive/htmlnotebook.html>
 # and <http://www.scipy.org/PyLab>.
+#
+# === Parameters
+#
+# [*ipython_dir*]
+#   IPython working directory.
+#
+# [*ipython_profile*]
+#   Name of IPython profile to create.
+#
+# [*ipython_user*]
+#   Create this user account and run IPython as this user.
+#
+# [*notebook_dir*]
+#   Specifies where IPython should archive notebooks.
+#
 class eventlogging::notebook(
 	$ipython_dir = '/var/lib/ipython',
+	$ipython_profile = 'nbserver',
 	$ipython_user = 'ipython',
 	$notebook_dir = '/var/lib/ipython/notebooks'
 ) {
@@ -28,8 +46,15 @@ class eventlogging::notebook(
 		ensure  => directory,
 		owner   => $ipython_user,
 		group   => $ipython_user,
-		mode    => '0555',
+		mode    => '0660',
 		require => Systemuser[$ipython_user],
+	}
+
+	exec { "ipython profile create ${ipython_profile}":
+		creates     => "${ipython_dir}/profile_${ipython_profile}",
+		environment => "IPYTHONDIR=${ipython_dir}",
+		user        => $ipython_user,
+		require     => [ Package['ipython-notebook'], Systemuser[$ipython_user] ],
 	}
 
 	file { '/etc/supervisor/conf.d/notebook.conf':
