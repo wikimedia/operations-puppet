8d59c90fce187cbd8e9394e4faaf760f83b17f2a
Ori Livneh <ori@wikimedia.org>
2013-04-17 18:36:20 -0700

    Parametrize IPython notebook profile configuration
    Tell Puppet to create an IPython profile and to configure it according to
    the parameters passed to the notebook class. Also add RDoc documentation for
    parameters and set permission of $notebook_dir to 0664 (was: 0555).
    
    Change-Id: Ib4c6e662740fc9c56f5c29bb58ce1badd535c89e

--- modules/eventlogging/manifests/notebook.pp
+++ modules/eventlogging/manifests/notebook.pp
@@ -1,11 +1,42 @@
+# == Class: eventlogging::notebook
+#
 # Configures an integrated, web-based numeric computation environment
 # (like Mathematica), based on PyLab and IPython Notebook.
 # See <http://ipython.org/ipython-doc/dev/interactive/htmlnotebook.html>
 # and <http://www.scipy.org/PyLab>.
+#
+# === Parameters
+#
+# [*ipython_dir*]
+#   IPython working directory.
+#
+# [*ipython_profile*]
+#   Name of IPython profile to create.
+#
+# [*ipython_user*]
+#   Create this user account and run IPython as this user.
+#
+# [*notebook_dir*]
+#   Specifies where IPython should archive notebooks.
+#
+# [*notebook_ip*]
+#   Interface on which the notebook server will listen (or '*' for all
+#   interfaces).
+#
+# [*notebook_port*]
+#   The port the notebook server will listen on.
+#
+# [*exec_files*]
+#   Array of Python files to execute at start of Notebook session.
+#
 class eventlogging::notebook(
 	$ipython_dir = '/var/lib/ipython',
+	$ipython_profile = 'nbserver',
 	$ipython_user = 'ipython',
-	$notebook_dir = '/var/lib/ipython/notebooks'
+	$notebook_dir = '/var/lib/ipython/notebooks',
+	$notebook_ip = '*',
+	$notebook_port = 8888,
+	$exec_files = []
 ) {
 
 	package { [
@@ -28,8 +59,25 @@ class eventlogging::notebook(
 		ensure  => directory,
 		owner   => $ipython_user,
 		group   => $ipython_user,
-		mode    => '0555',
+		mode    => '0664',
 		require => Systemuser[$ipython_user],
+	}
+
+	exec { "Create IPython profile ${ipython_profile}":
+		command     => "ipython profile create ${ipython_profile} --ipython-dir=\"${ipython_dir}\"",
+		creates     => "${ipython_dir}/profile_${ipython_profile}",
+		environment => "IPYTHONDIR=${ipython_dir}",
+		user        => $ipython_user,
+		require     => [ Package['ipython-notebook'], Systemuser[$ipython_user] ],
+	}
+
+	file { "Configure IPython profile ${ipython_profile}":
+		path    => "${ipython_dir}/profile_${ipython_profile}/ipython_notebook_config.py",
+		content => template('eventlogging/ipython_notebook_config.py.erb'),
+		require => Exec["Create IPython profile ${ipython_profile}"],
+		owner   => $ipython_user,
+		group   => $ipython_user,
+		mode    => '0444',
 	}
 
 	file { '/etc/supervisor/conf.d/notebook.conf':
