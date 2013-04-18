78fb7b717bf6e9c5bf141bb2836b824e5849899f
coren <marc@uberbox.org>
2013-04-18 18:46:44 -0400

    Preliminary toollabs module
    Preliminary implementation of the toollabs module that implements
    the Tool Labs instance roles.  At this time, only the structure
    and some packages are in place.
    
    Important note: those classes are interdependent and cross-
    configure each other through a shared filesystem (exec hosts push
    they configurations for the master to collect, submit hosts pick
    the queue information from the master and shadow, and so on).
    
    Change-Id: Ic9595f375ece581bf41ceff44e9d14a7bdfcd70c

---
+++ manifests/role/labstools.pp
@@ -0,0 +1,60 @@
+class role::labs::tools::bastion {
+	include role::labsnfs::client # temporary measure
+
+	$grid_master = "tools-master.pmtpa.wmflabs"
+	system_role { "role::labs::tools::bastion": description => "Tool Labs bastion" }
+	class { 'toollabs::bastion':
+		gridmaster => $grid_master,
+	}
+}
+
+class role::labs::tools::execnode {
+	include role::labsnfs::client # temporary measure
+
+	$grid_master = "tools-master.pmtpa.wmflabs"
+	system_role { "role::labs::tools::execnode": description => "Tool Labs execution host" }
+	class { 'toollabs::execnode':
+		gridmaster => $grid_master,
+	}
+}
+
+class role::labs::tools::master {
+	include role::labsnfs::client # temporary measure
+
+	$grid_master = "tools-master.pmtpa.wmflabs"
+	system_role { "role::labs::tools::master": description => "Tool Labs gridengine master" }
+	class { 'toollabs::master':
+		gridmaster => $grid_master,
+	}
+}
+
+class role::labs::tools::shadow {
+	include role::labsnfs::client # temporary measure
+
+	$grid_master = "tools-master.pmtpa.wmflabs"
+	system_role { "role::labs::tools::shadow": description => "Tool Labs gridengine shadow (backup) master" }
+	class { 'toollabs::shadow':
+		gridmaster => $grid_master,
+	}
+}
+
+class role::labs::tools::webserver {
+	include role::labsnfs::client # temporary measure
+
+	$grid_master = "tools-master.pmtpa.wmflabs"
+	system_role { "role::labs::tools::webserver": description => "Tool Labs webserver" }
+	class { 'toollabs::webserver':
+		gridmaster => $grid_master,
+	}
+}
+
+class role::labs::tools::webproxy {
+	include role::labsnfs::client # temporary measure
+
+	$grid_master = "tools-master.pmtpa.wmflabs"
+	system_role { "role::labs::tools::webproxy": description => "Tool Labs web proxy" }
+	class { 'toollabs::webproxy':
+		gridmaster => $grid_master,
+	}
+}
+
---
+++ modules/toollabs/manifests/execnode.pp
@@ -0,0 +1,24 @@
+# Class: toollabs::execnode
+#
+# This role sets up an execution node in the Tool Labs model.
+#
+# Parameters:
+#       gridmaster => FQDN of the gridengine master
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class toollabs::execnode($gridmaster) {
+	include toollabs
+	include toollabs::exec_environ
+	class { 'gridengine::exec_host':
+		gridmaster => $gridmaster,
+	}
+
+# TODO: grid node setup
+# TODO: sshd config
+}
+
