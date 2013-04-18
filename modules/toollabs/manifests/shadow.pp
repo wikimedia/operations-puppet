5154aad5f79bad38969c66baa06e7db8b5d4cc94
coren <marc@uberbox.org>
2013-04-18 18:46:44 -0400

    Preleminary toollabs module
    Preleminary implementation of the toollabs module that implements
    the Tool Labs instance roles.  At this time, only the structure
    and some packages are in place.
    
    Important note: those classes are interdependent and cross-
    configure each other through a shared filesystem (exec hosts push
    they configurations for the master to collect, sumbit hosts pick
    the queue information from the master and shadow, and so on).
    
    Change-Id: Ic9595f375ece581bf41ceff44e9d14a7bdfcd70c

---
+++ modules/toollabs/manifests/shadow.pp
@@ -0,0 +1,24 @@
+# Class: toollabs::shadow
+#
+# This role sets up a grid shadow master in the Tool Labs model.
+#
+# Parameters:
+#       gridmaster => FQDN of the gridengine master
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class toollabs::shadow($gridmaster) {
+	include toollabs
+	include toollabs::exec_environ
+	class { 'gridengine::shadow_master':
+		gridmaster => $gridmaster,
+	}
+
+# TODO: grid setup
+# TODO: NFS overrides (job queue)
+}
+
