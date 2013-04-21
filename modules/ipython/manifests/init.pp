e3452e42899ce11cd9ec80762156ef312da76ff8
Ori Livneh <ori@wikimedia.org>
2013-04-20 23:10:58 -0700

    Create self-standing IPython Notebook Puppet module
    - For the sake of portability, replaced dependency on 'systemuser' resource
      (from generic-definitions.pp) with equivalent user & group resources.
    
    - Dito generic-definitions.pp's 'upstart_job' resource (which could stand to be
      improved..) Replaced with a proper service resource, specifying upstart as
      the provider, but also creating an init.d symlink to /lib/init/upstart-job.
    
    - Added README, expanded parameter description, and documented default values.
    
    - Change default IPYTHONDIR from '/var/lib/ipython' to '/srv/ipython', which
      better conforms to the Filesystem Hierarchy Standard.
    
    - Changed ill-named 'graphs_dir' to 'mplconfigdir', in keeping with the name of
      the environment variable it shadows.
    
    - Followed Puppet's style guide for specifying default values for parametrized
      classes that are based on other variables.
    
    - In erb templates, use '@varname' rather than 'varname'. (The latter notation
      is liable to conflict with Ruby built-ins and is thus discouraged.)
    
    I tested it using Vagrant and it works well.
    
    Change-Id: I2db8206d91e929c943fddab8e09db60f458739b0

---
+++ modules/ipython/manifests/init.pp
@@ -0,0 +1,60 @@
+# == Class: ipython
+#
+# Base class for IPython setups. IPython is an interactive interpreter for
+# Python which provides facilities for scientific computing, such as
+# distributed computation and graphical development environment.
+#
+# Note that in most cases, rather than including this class directly, you will
+# want to simply accept the defaults and skip directly to including a specific
+# IPython service, such as 'ipython::notebook'.
+#
+# === Parameters
+#
+# [*ipythondir*]
+#   IPython working directory. Equivalent to the IPYTHONDIR environment
+#   variable. Defaults to '/srv/ipython'.
+#
+# [*user*]
+#   Any IPython services will run under this user's uid. The user account will
+#   be created if it does not already exist. Defaults to 'ipython'.
+#
+# [*group*]
+#   Run services under this gid. Will be created if it does not already exist.
+#   Defaults to 'ipython'.
+#
+# === Authors
+#
+# Ori Livneh <ori@wikimedia.org>
+#
+# === Copyright
+#
+# Copyright (C) 2013 Ori Livneh
+# Licensed under the GNU Public License, version 2
+#
+class ipython(
+	$ipythondir = '/srv/ipython',
+	$user       = 'ipython',
+	$group      = 'ipython'
+) {
+
+	package { 'ipython':
+		ensure => latest,
+	}
+
+	if ! defined(Group[$group]) {
+		group { $user:
+			ensure => present,
+		}
+	}
+
+	if ! defined(User[$user]) {
+		user { $user:
+			ensure     => present,
+			gid        => $group,
+			shell      => '/bin/false',
+			home       => $ipythondir,
+			managehome => true,
+			system     => true,
+		}
+	}
+}
