d812345023a1bbee839f67c8755063b90df99691
DZ <dzahn@wikimedia.org>
2013-04-22 14:16:51 -0700

    wikibugs - also need /var/lib/wikibugs/bin summarize needed directories, set File defaults, retab 2-space softtabs, align arrows to make puppet-lint like it more
    Change-Id: I72956a74ab0c965297fc6801e88df3348122a7ab

--- manifests/misc/wikibugs.pp
+++ manifests/misc/wikibugs.pp
@@ -8,51 +8,53 @@
 
 # Package dependencies for the wikibugs script
 class misc::irc::wikibugs::packages {
-	package { 'libemail-mime-perl':
-		ensure => present;
-	}
+  package { 'libemail-mime-perl':
+    ensure => present;
+  }
 }
 
 class misc::irc::wikibugs {
 
-	# We are an IRC bot!
+  # We are an IRC bot!
 
-	# Some Bugzilla product have been blessed with their own log files out of the
-	# default one. Values are hardcoded in the Wikibugs perl script
-	$ircecho_logbase = '/var/lib/wikibugs/logs'
-	$ircecho_logs = {
-		"${ircecho_logbase}/wikimedia-labs.log"           => '#wikimedia-labs',
-		"${ircecho_logbase}/wikimedia-mobile.log"         => '#wikimedia-mobile',
-		"${ircecho_logbase}/wikimedia-dev.log"            => '#wikimedia-dev',
-	}
+  # Some Bugzilla product have been blessed with their own log files out of the
+  # default one. Values are hardcoded in the Wikibugs perl script
+  $ircecho_logbase = '/var/lib/wikibugs/logs'
+  $ircecho_logs = {
+    "${ircecho_logbase}/wikimedia-labs.log"           => '#wikimedia-labs',
+    "${ircecho_logbase}/wikimedia-mobile.log"         => '#wikimedia-mobile',
+    "${ircecho_logbase}/wikimedia-dev.log"            => '#wikimedia-dev',
+  }
 
-	$ircecho_nick = 'wikibugs'
-	$ircecho_server = 'chat.freenode.net'
+  $ircecho_nick = 'wikibugs'
+  $ircecho_server = 'chat.freenode.net'
 
-	include misc::ircecho
-	include misc::irc::wikibugs::packages
+  include misc::ircecho
+  include misc::irc::wikibugs::packages
 
-	systemuser { 'wikibugs': name => 'wikibugs' }
+  systemuser { 'wikibugs': name => 'wikibugs' }
 
-	file {
-		'/var/lib/wikibugs':
-			ensure => directory,
-			owner  => wikibugs,
-			group => wikidev,
-			mode  => '0755';
-		'/var/lib/wikibugs/log':
-			ensure => directory,
-			owner  => wikibugs,
-			group => wikidev,
-			mode  => '0775',
-			require => User['wikibugs'];
-	}
+  File {
+    owner   => wikibugs,
+    group   => wikidev,
+    mode    => '0755',
+    require => User['wikibugs'];
+  }
 
-	git::clone { 'wikibugs' :
-		directory => '/var/lib/wikibugs/bin',
-		origin => 'https://gerrit.wikimedia.org/r/p/wikimedia/bugzilla/wikibugs.git',
-		owner => wikibugs,
-		group => wikidev,
-		require => User['wikibugs'];
-	}
+  file {
+    '/var/lib/wikibugs':
+      ensure => directory;
+    '/var/lib/wikibugs/bin':
+      ensure => directory;
+    '/var/lib/wikibugs/log':
+      ensure => directory;
+  }
+
+  git::clone { 'wikibugs' :
+    directory => '/var/lib/wikibugs/bin',
+    origin    => 'https://gerrit.wikimedia.org/r/p/wikimedia/bugzilla/wikibugs.git',
+    owner     => wikibugs,
+    group     => wikidev,
+    require   => User['wikibugs'];
+  }
 }
