eb75880f7844c3774913a9fa883ce07c060f3c2e
DZ <dzahn@wikimedia.org>
2013-04-22 14:16:51 -0700

    wikibugs - also need /var/lib/wikibugs/bin summarize needed directories, set File defaults, retab 2-space softtabs, align arrows to make puppet-lint like it more
    Change-Id: I72956a74ab0c965297fc6801e88df3348122a7ab

--- manifests/misc/wikibugs.pp
+++ manifests/misc/wikibugs.pp
@@ -8,13 +8,14 @@
 
 # Package dependencies for the wikibugs script
 class misc::irc::wikibugs::packages {
-	package { 'libemail-mime-perl':
-		ensure => present;
-	}
+  package { 'libemail-mime-perl':
+    ensure => present;
+  }
 }
 
 class misc::irc::wikibugs {
 
+<<<<<<< HEAD
 	# We are an IRC bot!
 
 	# Some Bugzilla product have been blessed with their own log files out of the
@@ -55,4 +56,48 @@ class misc::irc::wikibugs {
 		group => wikidev,
 		require => User['wikibugs'];
 	}
+=======
+  # We are an IRC bot!
+
+  # Some Bugzilla product have been blessed with their own log files out of the
+  # default one. Values are hardcoded in the Wikibugs perl script
+  $ircecho_logbase = '/var/lib/wikibugs/logs'
+  $ircecho_logs = {
+    "${ircecho_logbase}/wikimedia-labs.log"           => '#wikimedia-labs',
+    "${ircecho_logbase}/wikimedia-mobile.log"         => '#wikimedia-mobile',
+    "${ircecho_logbase}/wikimedia-dev.log"            => '#wikimedia-dev',
+  }
+
+  $ircecho_nick = 'wikibugs'
+  $ircecho_server = 'chat.freenode.net'
+
+  include misc::ircecho
+  include misc::irc::wikibugs::packages
+
+  systemuser { 'wikibugs': name => 'wikibugs' }
+
+  File {
+    owner   => wikibugs,
+    group   => wikidev,
+    mode    => '0755',
+    require => User['wikibugs'];
+  }
+
+  file {
+    '/var/lib/wikibugs':
+      ensure => directory;
+    '/var/lib/wikibugs/bin':
+      ensure => directory;
+    '/var/lib/wikibugs/log':
+      ensure => directory;
+  }
+
+  git::clone { 'wikibugs' :
+    directory => '/var/lib/wikibugs/bin',
+    origin    => 'https://gerrit.wikimedia.org/r/p/wikimedia/bugzilla/wikibugs.git',
+    owner     => wikibugs,
+    group     => wikidev,
+    require   => User['wikibugs'];
+  }
+>>>>>>> f6b58a4... wikibugs: also need /var/lib/wikibugs/bin dir, summarize needed dirs,
 }
