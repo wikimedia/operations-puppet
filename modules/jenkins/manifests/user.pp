622bbccca6b0a7a2b0bf82f7491bd6562b39d145
Antoine Musso <hashar@free.fr>
2013-04-23 13:50:44 +0200

    Revert "create jenkins user with systemuser"
    Come back to using the user {} definition. systemuser {} defines the
    group which would prevents us from keeping the group definition inside
    the jenkins module.
    
    This reverts commit bff1865e793de6aa812fe18cfd5b913210e8588e.
    
    Change-Id: I7482ca5dce574e6557656b8c15d640b07df20b0c

--- modules/jenkins/manifests/user.pp
+++ modules/jenkins/manifests/user.pp
@@ -2,11 +2,16 @@ class jenkins::user {
 
   include jenkins::group
 
-  systemuser { 'jenkins':
+  # We do not use systemuser{} since we would like to keep
+  # the group definition in the jenkins module.
+  user { 'jenkins':
     name       => 'jenkins',
     home       => '/var/lib/jenkins',
+    shell      => '/bin/bash',  # admins need to be able to login
+    gid        => 'jenkins',
+    system     => true,
     managehome => false,
-    shell      => '/bin/bash',
+    require    => Group['jenkins'];
   }
 
 }
