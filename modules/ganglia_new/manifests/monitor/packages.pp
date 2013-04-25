560f5213a9d51b8a954c77434c0cb740e271ad61
Mark Bergsma <mark@wikimedia.org>
2013-04-25 15:30:11 +0200

    Add file type for python_modules directory
    Change-Id: I093ebd9901dc82748c76f7781816b2f2c4835acf

--- modules/ganglia_new/manifests/monitor/packages.pp
+++ modules/ganglia_new/manifests/monitor/packages.pp
@@ -2,4 +2,9 @@ class ganglia_new::monitor::packages {
 	if !defined(Package["ganglia-monitor"]) {
 		package { "ganglia-monitor": ensure => latest }
 	}
+
+	file { "/usr/lib/ganglia/python_modules":
+		require => Package["ganglia-monitor"],
+		ensure => directory
+	}
 }
