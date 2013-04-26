f945ec5238810f5dc18f0cddc448d9ea141cc26a
Faidon Liambotis <faidon@wikimedia.org>
2013-04-26 15:56:05 +0300

    Initial Ceph module, role class & site.pp
    This is inspired from the puppet-ceph module and the Chef recipes,
    although it's very different from either of them. The puppet-ceph module
    is really weird and wouldn't work well for us at all.
    
    Change-Id: Ia3dc6838fc776628da1cf9d9cc1e9fff6899f5b1

---
+++ manifests/role/ceph.pp
@@ -0,0 +1,104 @@
+@monitor_group { 'Ceph': description => 'Ceph servers' }
+
+class role::ceph::base {
+    $cluster      = 'ceph'
+    $nagios_group = 'ceph'
+
+    include standard
+}
+
+class role::ceph::eqiad inherits role::ceph::base {
+    system_role { 'role::ceph::eqiad': description => 'Ceph eqiad cluster' }
+
+    include passwords::ceph::eqiad
+    class { 'ceph':
+        admin_key      => $passwords::ceph::eqiad::admin_key,
+        config         => {
+            fsid                => 'c9da36e1-694a-4166-b346-9d8d4d1d1ac1',
+            mon_initial_members => 'ms-fe1001 ms-fe1002 ms-be1003',
+            mon_addresses       => [
+                '10.64.0.167:6789', # ms-fe1001
+                '10.64.0.168:6789', # ms-fe1002
+                '10.64.0.175:6789', # ms-be1003
+            ],
+            'global'            => {
+                # eqiad hardware has H710s which have a BBU
+                'osd fs mount options xfs'    => 'noatime,nobarrier',
+            },
+            'mon'               => {
+                'mon osd down out interval'   => '600',
+            },
+            'osd'               => {
+                'osd journal'                 => '/var/lib/ceph/journal/$cluster-$id',
+                'osd journal size'            => '10240',
+                'osd op thread timeout'       => '7200',
+                'osd recovery thread timeout' => '7200',
+            },
+            'radosgw'           => {
+                'rgw enable ops log'          => 'false',
+                'rgw enable usage log'        => 'false',
+                'rgw extended http attrs'     => 'x_content_duration',
+                'debug rgw'                   => '1',
+            },
+        },
+    }
+
+    class mon inherits role::ceph::eqiad {
+        system_role { 'role::ceph::eqiad::mon':
+            description => 'Ceph eqiad monitor',
+        }
+
+        class { 'ceph::mon':
+            monitor_secret => $passwords::ceph::eqiad::monitor_secret,
+        }
+
+        # FIXME: need a Ceph nagios check
+    }
+
+    class osd inherits role::ceph::eqiad {
+        system_role { 'role::ceph::eqiad::osd':
+            description => 'Ceph eqiad OSD',
+        }
+
+        include ceph::osd
+
+        # I/O busy systems, tune a few knobs to avoid page alloc failures
+        sysctl { 'sys.vm.min_free_kbytes':
+            value => '512000',
+        }
+        sysctl { 'sys.vm.vfs_cache_pressure':
+            value => '120',
+        }
+    }
+
+    class radosgw inherits role::ceph::eqiad {
+        system_role { 'role::ceph::eqiad::radosgw':
+            description => 'Ceph eqiad radosgw',
+        }
+
+        class { "lvs::realserver": realserver_ips => [ "10.2.2.27" ] }
+
+        include generic::sysctl::high-http-performance
+
+        class { 'ceph::radosgw':
+            servername  => 'ms-fe.eqiad.wmnet',
+            serveradmin => 'webmaster@wikimedia.org',
+        }
+
+        monitor_service { 'http':
+            description   => 'HTTP',
+            check_command => 'check_http',
+        }
+        # FIXME: more high-level Swift/S3 check, actual file check
+
+        file { '/usr/lib/ganglia/python_modules/apache_status.py':
+            source => 'puppet:///files/ganglia/plugins/apache_status.py',
+            notify => Service['gmond'],
+        }
+
+        file { '/etc/ganglia/conf.d/apache_status.pyconf':
+            source => 'puppet:///files/ganglia/plugins/apache_status.pyconf',
+            notify => Service['gmond'],
+        }
+    }
+}
---
+++ modules/ceph/manifests/osd.pp
@@ -0,0 +1,23 @@
+class ceph::osd {
+    Class['ceph::osd'] -> Class['ceph']
+
+    package { ['parted', 'gdisk' ]:
+        ensure => present,
+    }
+
+    ceph::bootstrap_key { 'ceph-osd':
+        type   => 'osd',
+    }
+
+    file { '/usr/local/sbin/ceph-add-disk':
+        ensure => present,
+        owner  => root,
+        group  => root,
+        mode   => '0555',
+        source => 'puppet:///modules/ceph/ceph-add-disk',
+    }
+
+    file { '/var/lib/ceph/journal':
+        ensure  => directory,
+    }
+}
