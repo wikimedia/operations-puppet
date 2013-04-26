03ab61b7f97cd12d3eab8c06c00a892cd84b2cd2
Mark Bergsma <mark@wikimedia.org>
2013-04-26 13:18:54 +0200

    Support labs
    Change-Id: Iccdd3f454e17a73a56ea0ad7d63501ec82e766d6

--- modules/ganglia_new/manifests/monitor.pp
+++ modules/ganglia_new/manifests/monitor.pp
@@ -1,5 +1,27 @@
 class ganglia_new::monitor($cluster) {
 	include packages, service
 
-	class { "ganglia_new::monitor::config": cluster => $cluster }
+	if $::realm == "production" {
+		$id = $ganglia_new::configuration::clusters[$cluster]['id'] + $ganglia_new::configuration::id_prefix[$::site]
+		$desc = $ganglia_new::configuration::clusters[$cluster]['name']
+		$portnr = $ganglia_new::configuration::base_port + $id
+	}
+	$gmond_port = $::realm ? {
+		production => $portnr,
+		labs => $::project_gid
+	}
+	$cname = $::realm ? {
+		production => "${desc} ${::site}",
+		labs => $::instanceproject
+	}
+	$override_hostname = $::realm ? {
+		production => undef,
+		labs => $::instancename
+	}
+
+	class { "ganglia_new::monitor::config":
+		gmond_port => $gmond_port,
+		cname => $cname,
+		override_hostname => $override_hostname
+	}
 }
--- modules/ganglia_new/manifests/monitor/config.pp
+++ modules/ganglia_new/manifests/monitor/config.pp
@@ -1,16 +1,8 @@
-class ganglia_new::monitor::config($cluster) {
+class ganglia_new::monitor::config($gmond_port, $cname, $override_hostname=undef) {
 	require ganglia_new::monitor::packages
 	include ganglia_new::configuration
 
 	$aggregator = false
-	$id = $ganglia_new::configuration::clusters[$cluster]['id'] + $ganglia_new::configuration::id_prefix[$::site]
-	$desc = $ganglia_new::configuration::clusters[$cluster]['name']
-	$portnr = $ganglia_new::configuration::base_port + $id
-	$gmond_port = $::realm ? {
-		production => $portnr,
-		labs => $::project_gid
-	}
-	$cname = "${desc} ${::site}"
 
 	file { "/etc/ganglia/gmond.conf":
 		mode => 0444,
