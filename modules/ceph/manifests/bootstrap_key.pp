4ef49da81050657fcda8b951b3afb6a8ce2ab3e5
Faidon Liambotis <faidon@wikimedia.org>
2013-04-26 15:56:05 +0300

    Initial Ceph module, role class & site.pp
    This is inspired from the puppet-ceph module and the Chef recipes,
    although it's very different from either of them. The puppet-ceph module
    is really weird and wouldn't work well for us at all.
    
    Change-Id: Ia3dc6838fc776628da1cf9d9cc1e9fff6899f5b1

---
+++ modules/ceph/manifests/bootstrap_key.pp
@@ -0,0 +1,34 @@
+define ceph::bootstrap_key($type, $cluster='ceph') {
+    $keyring = "/var/lib/ceph/bootstrap-${type}/${cluster}.keyring"
+
+    file { "/var/lib/ceph/bootstrap-${type}":
+        ensure  => directory,
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+    }
+
+    # ping-pong trickery to securely do permissions, puppet has no umask on exec
+    file { $keyring:
+        ensure  => present,
+        mode    => '0600',
+        owner   => 'root',
+        group   => 'root',
+        require => File["/var/lib/ceph/bootstrap-${type}"],
+    }
+
+    $caps = $type ? {
+        'osd' => 'mon "allow command osd create ...; allow command osd crush set ...; allow command auth add * osd allow\ * mon allow\ rwx; allow command mon getmap"',
+        'mds' => 'mon "allow command auth get-or-create * osd allow\ * mds allow mon allow\ rwx; allow command mon getmap"',
+    }
+
+    exec { "ceph bootstrap ${keyring}":
+        command  => "/usr/bin/ceph \
+                    --cluster=${cluster} \
+                    auth get-or-create client.bootstrap-${type} \
+                    ${caps} \
+                    > ${keyring}",
+        unless   => "/usr/bin/test -s ${keyring}",
+        require  => File[$keyring],
+    }
+}
