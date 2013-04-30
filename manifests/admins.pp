9d9dbd47cafdf5109976bc25679710723c3995f4
cmjohnson <cmjohnson@wikimedia.org>
2013-04-30 10:06:55 -0400

    Adding rfaulk's new key to admins for stat1 access..rt5040
    Change-Id: I0a04e5e48dc45b0ccaa5fcfd52f4ee49aaa0a399

--- manifests/admins.pp
+++ manifests/admins.pp
@@ -1486,6 +1486,12 @@ class accounts {
 				type	=> "ssh-rsa",
 				key	=> "AAAAB3NzaC1yc2EAAAABIwAAAQEA1WerRcJULxj26JimklCkoFUO6XKBjwTbPH54/hUv5c3lvyuUol6AF86rWziHgwIAy4+jPlm9mTS3IzWoRf0sSMgSrjkmykVaD4Zc+7QnXXGtnehHfryKTyB30TI39/JB5CoS1pGQQoMg67kF0nl2RKP47r0HY07m3rl1m5MWMTKByZ9p6/oVAuJ7XNxLjfN1N+Li7HhyueQonkw8Na4CxSz/Uj6zDgxB1Odw9LgHErgOe99Nza4yOsvpa9iq30eLJjXYKJ+9s8aPI68H+nBh8/CaBaPJkuTcYfwXhE6EG9JTtUrf43y88J+SDeN0lRF8w0SMluheIzkv/3TNyDdr1w=="
 			}
+			ssh_authorized_key { "rfaulk":
+				ensure	=> present,
+				user	=> $username,
+				type	=> "ssh-rsa",
+				key	=> "AAAAB3NzaC1yc2EAAAADAQABAAABAQDFlxYVmNcVsP33eEHhNc+nHZ1HJcuAZNGNfoNhiHRTisyqPm1jFvt0WQ3r+AS1rErCD3594rkApGAzO20if86OAChUwq2C3k7xB28eOXOGB/EMNswpdoDXraSxgshptbFloy1ekLX/lxWUVKU3omc3jaA4anF9FQD4EGpNbL6lJaO0oloOHdKwyjUlvp8gvWOX2/LH10ALh5/KcA1nC3zlyrLyDfHTwnkDCsvVKe8rQB3pM4b7mVOLstj+dcCqxaVbjyZap8dV4W4QbhqUFVhe9ZL1Crop0TemhWBSw9oh2vQhGj6LWQIwIAfSe6zHIJ2exfq/53nVsHeC98gWr4QZ"
+			}
 		}
 	}
 
