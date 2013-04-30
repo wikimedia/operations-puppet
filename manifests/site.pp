9d455d881e7107517aa61226b2ea1256f34a9a9e
cmjohnson <cmjohnson@wikimedia.org>
2013-04-30 15:01:25 -0400

    Decommission db10 - certs cleaned, changes made to site.pp, decom.pp and dhcpd files
    Change-Id: I5c391cab92ccf04ec41f6e1c65c17833ee077279

--- manifests/site.pp
+++ manifests/site.pp
@@ -487,7 +487,7 @@ node /^db(36|38)\.pmtpa\.wmnet/ {
 }
 
 ## m1 shard (still mostly not puppetized...)
-node /^(db(9|10)|blondel|bellin)\.pmtpa\.wmnet$/ {
+node /^(db(9)|blondel|bellin)\.pmtpa\.wmnet$/ {
 	## do not have most of our current puppet classe
 	include role::db::core
 
