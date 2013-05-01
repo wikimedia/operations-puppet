41e6cf4af7972e6785cb3881a5b34ef3b7cceb58
root <root@i-00000273.pmtpa.wmflabs>
2013-05-01 11:49:14 +0000

    Intial version of puppet script for LaTeXML
    Only install LaTeXML.
    
    ToDo: Verify folder /home/LaTeXML
    
    Change-Id: I54628a364e762bd1d4d200a09ae39798152e3f10

---
+++ modules/latexml/manifests/init.pp
@@ -0,0 +1,37 @@
+class latexml( $srvnm='localhost') {
+
+    package { ['texlive',
+        'subversion',
+        'libclone-perl',
+        'libdata-compare-perl',
+        'libio-prompt-perl',
+        'perlmagick',
+        'libparse-recdescent-perl',
+        'libxml-libxml-perl',
+        'libxml-libxslt-perl',
+        'libarchive-zip-perl',
+        'libio-string-perl',
+        'libplack-perl' ,
+        'libjson-xs-perl',
+        'libfile-which-perl'
+        ]: ensure => present,
+    }
+
+    git::clone { 'operations/debs/latexml':
+        directory => '/home/LaTeXML',
+        origin => 'https://gerrit.wikimedia.org/r/operations/debs/latexml'
+    }
+
+    class {'apache':  }
+
+    class {'apache::mod::perl': }
+
+    apache::vhost { 'latexml':
+        vhost_name      => '*',
+        priority        => '10',
+        port            => '80',
+        docroot         => '/home/latexml/',
+        template       => '/etc/puppet/modules/latexml/templates/latexml-apache-site.erb',
+    }
+
+}
