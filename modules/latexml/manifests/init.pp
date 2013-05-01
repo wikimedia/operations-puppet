e1f291333cbb34c55f42f4f2542c3a921270a468
Moritz Shubotz <wiki@physikerwelt.de>
2013-05-01 11:49:14 +0000

    Initial version of puppet script for LaTeXML
    Installs LaTeXML web-service and configures it to listen to
    *:80
    
    Change-Id: I54628a364e762bd1d4d200a09ae39798152e3f10

---
+++ modules/latexml/manifests/init.pp
@@ -0,0 +1,41 @@
+class latexml( $server_name = 'localhost',
+    $latexml_dir = '/usr/local/src/latexml',
+    $vhost_name = '*',
+    $port = '80'
+     ) {
+
+    package { ['texlive',
+        'libclone-perl',
+        'libdata-compare-perl',
+        'libio-prompt-perl',
+        'perlmagick',
+        'libparse-recdescent-perl',
+        'libxml-libxml-perl',
+        'libxml-libxslt-perl',
+        'libarchive-zip-perl',
+        'libio-string-perl',
+        'libplack-perl' ,
+        'libjson-xs-perl',
+        'libfile-which-perl',
+        ]: ensure => present,
+    }
+
+    git::clone { 'operations/debs/latexml':
+        directory => $latexml_dir,
+        origin => 'https://gerrit.wikimedia.org/r/operations/debs/latexml',
+        branch => 'production',
+    }
+
+    class {'apache':  }
+
+    class {'apache::mod::perl': }
+
+    apache::vhost { 'latexml':
+        vhost_name      => $vhost,
+        priority        => '10',
+        port            => $port,
+        docroot         => $latexml_dir,
+        template       => '/etc/puppet/modules/latexml/templates/latexml-apache-site.erb',
+    }
+
+}
