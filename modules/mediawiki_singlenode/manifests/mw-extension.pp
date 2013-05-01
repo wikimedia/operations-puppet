998a6fa04b2a9d8c2e00d6a994b933b8d80fee15
Ori Livneh <ori@wikimedia.org>
2013-05-01 09:28:52 -0700

    Lint mediawiki_singlenode module
    I'd like to migrate some patches from MediaWiki-Vagrant to this module. But
    first -- code style!
    
    - Single quotes unless interpolating variable.
    - 'ensure' should be the first parameter, if present, per Puppet style guide.
    - When resources are specified as dependencies, every word needs to be
      capitalized ('Git::Clone').
    - I assumed 'hostname' and 'labs_mediawiki_hostname' are defined in top scope
      and thus specified them as '$::hostname' and '$::labs_mediawiki_hostname'.
    - Other misc. things suggested by puppet-lint and the puppet style guide.
    
    I haven't tested this, sorry.
    
    Change-Id: Ib39a9b5a39ea443cba15f472694fca99d01a12b9

--- modules/mediawiki_singlenode/manifests/mw-extension.pp
+++ modules/mediawiki_singlenode/manifests/mw-extension.pp
@@ -1,14 +1,14 @@
 # definition to clone mediawiki extensions
-define mw-extension(
-	# defaults
-	$branch="master",
-	$ensure=present,
-	$install_path="/srv/mediawiki") {
-	git::clone { "$name":
-		require => git::clone["mediawiki"],
+define mediawiki_singlenode::mw-extension(
+	$ensure       = present,
+	$branch       = 'master',
+	$install_path = '/srv/mediawiki',
+) {
+	git::clone { $name:
+		require   => Git::Clone['mediawiki'],
 		directory => "${install_path}/extensions/${name}",
-		origin => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/${name}.git",
-		branch => $branch,
-		ensure => $ensure,
+		origin    => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/${name}.git",
+		branch    => $branch,
+		ensure    => $ensure,
 	}
 }
