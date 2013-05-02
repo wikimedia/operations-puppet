382a59751b1f1965577e0ffc677fb382880fcb74
Andrew Otto <otto@wikimedia.org>
2013-05-02 10:42:07 -0400

    Sending bits esams EventLogging traffic to gadolinium for vandium relay.
    Also some formatting style cleanup.
    
    Change-Id: I32eb5dfefb3fbca7073147c8c36a159a5ad9955d

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -632,35 +632,39 @@ class role::cache {
 		}
 		
 		class logging {
-			if $::realm == "production" {
+			if $::realm == 'production' {
 				$event_listener = $::site ? {
 					/^(pmtpa|eqiad)$/ => '10.64.21.123', # vanadium
-					'esams' => '208.80.154.15', # oxygen
+					'esams'           => '208.80.154.73', # gadolinium
 				}
-				varnish::logging { "kraken" :
+
+				varnish::logging { 'kraken' :
 					listener_address => '208.80.154.154', # analytics1001
-					port => "8422",
-					instance_name => "",
-					cli_args => '-m RxURL:^/event\.gif\?. -D',
-					log_fmt => "%U	%q	%{Host}i	%t	%h	%{X-Forwarded-For}i	%{Referer}i	%{Accept-Language}i	%{Cookie}i	%{X-WAP-Profile}i	%{User-agent}i	%l	%n",
-					monitor => false,
+					port             => '8422',
+					instance_name    => '',
+					cli_args         => '-m RxURL:^/event\.gif\?. -D',
+					log_fmt          => '%U	%q	%{Host}i	%t	%h	%{X-Forwarded-For}i	%{Referer}i	%{Accept-Language}i	%{Cookie}i	%{X-WAP-Profile}i	%{User-agent}i	%l	%n',
+					monitor          => false,
 				}
-				varnish::logging { "locke" :
-					listener_address => "208.80.152.138",
-					port => "8420",
-					instance_name => "",
-					cli_args => "-m RxRequest:^(?!PURGE\$) -D"
+
+				varnish::logging { 'locke' :
+					listener_address => '208.80.152.138',
+					port             => '8420',
+					instance_name    => '',
+					cli_args         => '-m RxRequest:^(?!PURGE\$) -D',
 				}
-			} else {
+			}
+			else {
 				$event_listener = '10.4.1.14' # deployment-eventlogging
 			}
-			varnish::logging { "vanadium" :
+
+			varnish::logging { 'vanadium' :
 				listener_address => $event_listener,
-				port => "8422",
-				instance_name => "",
-				cli_args => '-m RxURL:^/event\.gif\?. -D',
-				log_fmt => "%q	%l	%n	%t	%h",
-				monitor => false,
+				port             => '8422',
+				instance_name    => '',
+				cli_args         => '-m RxURL:^/event\.gif\?. -D',
+				log_fmt          => '%q	%l	%n	%t	%h',
+				monitor          => false,
 			}
 		}
 		
