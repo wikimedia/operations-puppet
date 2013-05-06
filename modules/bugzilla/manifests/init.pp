cd4c7eb6a2ec378f2be32b00b7cbbe7be2eadb13
Ori Livneh <ori@wikimedia.org>
2013-05-05 17:27:15 -0700

    Puppetize Bugzilla
    This change set adds a Puppet module for configuring a Bugzilla server like the
    one running on kaulen.
    
    The dependencies that can be satisfied by Debian packages are installed via
    apt. The few that aren't are installed by Bugzilla's 'install-module.pl', a
    CPAN wrapper that installs to the lib/ subdirectory of the Bugzilla trunk.
    Bugzilla itself is retrieved by release tag from its Bazaar repository.
    Wikimedia-specific modifications are applied on top and the result is saved as
    a local Bazaar commit.
    
    This module does not configure the Apache site that will server the Bugzilla
    application. (This is currently managed in <manifests/misc/bugzilla.pp>.)
    
    Change-Id: Ia323925edd6c7218be8277c4ad9022b0f1cdb6f1

---
+++ modules/bugzilla/manifests/init.pp
@@ -0,0 +1,231 @@
+# == Class: bugzilla
+#
+# This Puppet class provisions Bugzilla, the web-based bug management
+# system that powers <https://bugzilla.wikimedia.org/>. It will retrieve
+# a Bugzilla release from Bazaar, configure it, and apply Wikimedia
+# patches and extensions.
+#
+# Wikimedia-specific patches are pulled from a Gerrit repository,
+# 'wikimedia/bugzilla/modifications'. The patches are synced to the
+# Bugzilla checkout and then committed locally.
+#
+# Note that not all Bugzilla dependencies have been packaged for Debian,
+# so some must be fetched from CPAN. Bugzilla's 'install-module.pl' script
+# facilitates downloading and installing these modules to a site-specific
+# subdirectory ('lib') of the main Bugzilla folder.
+#
+# === Parameters
+#
+# [*user*]
+#   Owner of Bugzilla files and configuration data (default:
+#   'bugzilla'). Will be created if it does not exist.
+#
+# [*group*]
+#   Group for Bugzilla files and configuration data (default:
+#   'bugzilla'). Will be created if it does not exist.
+#
+# [*dir*]
+#   Main path for Bugzilla files (default: '/srv/bugzilla'). The actual
+#   Bugzilla release will be checked out into a subfolder of this
+#   directory. Will be created if it does not exist.
+#
+# [*release*]
+#   A string of three dot-separated numbers specifying the exact
+#   Bugzilla release to retrieve and configure (default: '4.2.5').
+#
+# [*localconfig*]
+#   A hash-map of Bugzilla configuration keys and their values. The keys
+#   correspond to variables defined in Bugzilla's localconfig.
+#
+#   This map will be merged onto the default set of values, shown below.
+#   The values you should specify are 'urlbase' (the full URL that will
+#   server Bugzilla), 'db_host' (the hostname of the MySQL database that
+#   will store Bugzilla data), 'db_name' (the logical database name),
+#   and the credentials for the database user ('db_user' and 'db_pass').
+#
+# === Examples
+#
+#  class { 'bugzilla':
+#     release     => '4.2.5',
+#  	  localconfig => {
+#  	  	  db_host = 'db9.pmtpa.wmnet',
+#  		  db_name = 'bugs',
+#  		  db_user = 'bugs',
+#  		  db_pass = scope.lookupvar('passwords::bugzilla::bugzilla_db_pass'),
+#  	  }
+#  }
+#
+class bugzilla (
+	$user        = 'bugzilla',
+	$group       = 'bugzilla',
+	$dir         = '/srv/bugzilla',
+	$release     = '4.2.5',
+	$localconfig = {},
+) {
+	class { 'bugzilla::apache': }
+	class { 'bugzilla::modifications': }
+
+	$install_dir = "${dir}/bugzilla-${release}"
+
+	# The version string of a Bugzilla release comprises three
+	# dot-separated values ('4.2.5', for example). The release series
+	# spans the first two ('4.2').
+	$series = regsubst($release,'\.\d+$','')
+
+	$defaults = {
+		'ADMIN_EMAIL'     => "admin@${::fqdn}",
+		'ADMIN_PASSWORD'  => 'change_me',
+		'ADMIN_REALNAME'  => 'Bugzilla Administrator',
+		'create_htaccess' => 1,
+		'cvsbin'          => '',
+		'db_check'        => 1,
+		'db_driver'       => 'mysql',
+		'db_host'         => 'localhost',
+		'db_sock'         => '',
+		'db_port'         => 0,
+		'db_name'         => 'bugs',
+		'db_user'         => 'bugs',
+		'db_pass'         => '',
+		'diffpath'        => '/usr/bin',
+		'index_html'      => 0,
+		'interdiffbin'    => '/usr/bin/interdiff',
+		'urlbase'         => "http://${::fqdn}/bugs",
+		'use_suexec'      => 0,
+		'webservergroup'  => 'www-data',
+	}
+
+	if ! defined(Group[$group]) {
+		group { $group:
+			ensure => present,
+		}
+	}
+
+	if ! defined(User[$user]) {
+		user { $user:
+			ensure     => present,
+			gid        => $group,
+			shell      => '/bin/false',
+			home       => $dir,
+			managehome => true,
+			system     => true,
+		}
+	}
+
+	if ! defined(File[$dir]) {
+		file { $dir:
+			ensure => directory,
+			owner  => $user,
+			group  => $group,
+			mode   => '0755',
+		}
+	}
+
+	package { 'bzr': ensure => present, }
+
+	file { $install_dir:
+		ensure  => directory,
+		owner   => $user,
+		group   => $group,
+		mode    => '0755',
+		require => File[$dir],
+	}
+
+	exec { 'bzr checkout':
+		command => "bzr co -r tag:bugzilla-${release} bzr://bzr.mozilla.org/bugzilla/${series} ${install_dir}",
+		creates => "${install_dir}/.bzr/branch-format",
+		require => Package['bzr'],
+		path    => '/usr/bin',
+		user    => $user,
+	}
+
+
+	$dependencies = [
+		'libapache2-mod-perl2',
+		'libapache2-mod-perl2-dev',
+		'libauthen-sasl-perl',
+		'libauthen-simple-ldap-perl',
+		'libauthen-simple-radius-perl',
+		'libcgi-pm-perl',
+		'libchart-perl',
+		'libdatetime-perl',
+		'libdatetime-timezone-perl',
+		'libdbd-mysql-perl',
+		'libdbi-perl',
+		'libdigest-sha-perl',
+		'libemail-mime-perl',
+		'libemail-send-perl',
+		'libencode-detect-perl',
+		'libgd-gd2-perl',
+		'libgd-graph-perl',
+		'libgd-text-perl',
+		'libhtml-parser-perl',
+		'libhtml-scrubber-perl',
+		'libjson-rpc-perl',
+		'liblwp-useragent-determined-perl',
+		'libmath-random-isaac-perl',
+		'libmime-tools-perl',
+		'libsoap-lite-perl',
+		'libtemplate-perl',
+		'libtemplate-plugin-gd-perl',
+		'libtest-taint-perl',
+		'libtheschwartz-perl',
+		'libtimedate-perl',
+		'liburi-perl',
+		'libxml-twig-perl',
+		'libyaml-perl',
+	]
+
+	package { $dependencies:
+		ensure => present,
+	}
+
+	# These Perl modules have not been packaged for Debian. They'll be
+	# pulled from CPAN by Bugzilla's install-module.pl and installed to
+	# Bugzilla's 'lib' subdirectory.
+	$extra_modules = [
+		'Apache2::SizeLimit',
+		'Email::MIME::Attachment::Stripper',
+		'Email::Reply',
+		'PatchReader',
+		'Daemon::Generic',  # Bugzilla doesn't like 'libdaemon-generic-perl' in apt.
+		'Search::Sitemap',
+	]
+
+	exec { 'extra modules':
+		command => shellquote('perl', 'install-module.pl', $extra_modules),
+		onlyif  => 'perl checksetup.pl --check-modules 2>&1 | grep -q install-module',
+		cwd     => $install_dir,
+		path    => [ '/bin', '/usr/bin' ],
+		require => [ Package[$dependencies], Exec['bzr checkout'] ],
+		user    => $user,
+	}
+
+	# Generate an answer file for an unattended setup. See 'perldoc
+	# checksetup.pl' for details.
+	file { 'answer file':
+		path    => "${dir}/answerfile.pl",
+		content => template('bugzilla/answerfile.pl.erb'),
+		require => File[$dir],
+	}
+
+	# checksetup.pl needs to run twice: once to generate the
+	# configuration file, and a second time to act on it.
+	exec { 'check setup':
+		command => "perl checksetup.pl ${dir}/answerfile.pl",
+		creates => "${install_dir}/localconfig",
+		cwd     => $install_dir,
+		path    => '/usr/bin',
+		require => [ File['answer file'], Exec['extra modules'] ],
+		user    => $user,
+	}
+
+	exec { 'run setup':
+		command   => 'perl checksetup.pl',
+		creates   => "${install_dir}/data",
+		cwd       => $install_dir,
+		path      => '/usr/bin',
+		require   => Exec['check setup'],
+		subscribe => Exec['check setup'],
+		user      => $user,
+	}
+}
