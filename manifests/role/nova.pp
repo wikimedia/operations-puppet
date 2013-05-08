5f8dcec72ebdc4b2ad69709ccea593bfd44a9552
Ryan Lane <rlane@wikimedia.org>
2013-05-08 14:32:42 -0500

    Only bond interface in pmtpa for nova-network
    Change-Id: I9a9f8803967a363303ba791ba97c4545361e8351

--- manifests/role/nova.pp
+++ manifests/role/nova.pp
@@ -291,16 +291,18 @@ class role::nova::network {
 
 	include role::nova::common
 
-	require role::nova::network::bonding
+	if ($::site == "pmtpa") {
+		require role::nova::network::bonding
 
-	interface_ip { "openstack::network_service_public_dynamic_snat": interface => "lo", address => $site ? { "pmtpa" => "208.80.153.192", "eqiad" => "208.80.155.255" } }
+		interface_ip { "openstack::network_service_public_dynamic_snat": interface => "lo", address => $site ? { "pmtpa" => "208.80.153.192", "eqiad" => "208.80.155.255" } }
 
-	interface_tagged { "bond1.103":
-		base_interface => "bond1",
-		vlan_id => "103",
-		method => "manual",
-		up => 'ip link set $IFACE up',
-		down => 'ip link set $IFACE down',
+		interface_tagged { "bond1.103":
+			base_interface => "bond1",
+			vlan_id => "103",
+			method => "manual",
+			up => 'ip link set $IFACE up',
+			down => 'ip link set $IFACE down',
+		}
 	}
 
 	class { "openstack::network-service": openstack_version => $openstack_version, novaconfig => $novaconfig }
