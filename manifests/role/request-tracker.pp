f7ea8e42673df431e6a970bd3d30ddb9c976ebcb
Andrew Bogott <abogott@wikimedia.org>
2013-05-10 20:06:42 +0000

    Added manifest for rt4 running with Apache.
    Change-Id: I62e06979cf0aca4437805812565c74af8c4b369f

--- manifests/role/request-tracker.pp
+++ manifests/role/request-tracker.pp
@@ -17,3 +17,37 @@ class role::request-tracker::labs {
 	}
 }
 
+#  Labs/testing RT with Apache
+#
+class role::request-tracker-apache::labs {
+	include passwords::misc::rt
+
+	class { "misc::rt-apache::server":
+		dbuser => passwords::misc::rt::rt_mysql_user,
+		dbpass => passwords::misc::rt::rt_mysql_pass,
+		site => $fqdn,
+		datadir => "/a/mysql",
+	}
+
+	class { 'generic::mysql::server':
+		version => $::lsbdistrelease ? {
+			'12.04' => '5.5',
+			default => false,
+		},
+		datadir => $datadir;
+	}
+}
+
+#  Production RT with Apache
+#
+class role::request-tracker-apache::production {
+	include passwords::misc::rt
+
+	class { "misc::rt-apache::server":
+		site => 'rt.wikimedia.org',
+		dbhost => 'db1001',
+		dbport => '',
+		dbuser => passwords::misc::rt::rt_mysql_user,
+		dbpass => passwords::misc::rt::rt_mysql_pass,
+	}
+}
