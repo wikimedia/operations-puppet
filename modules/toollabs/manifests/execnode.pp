ca80398de661d26882c932ee897dc063023657c6
coren <marc@uberbox.org>
2013-05-11 22:08:12 -0400

    Tool Labs: finish with the access.conf
    - Also store mostly populated now.
    
    Change-Id: I12b06758d5e7e2a0e77d2d6120ab43701aeee559

--- modules/toollabs/manifests/execnode.pp
+++ modules/toollabs/manifests/execnode.pp
@@ -35,6 +35,46 @@ class toollabs::execnode($gridmaster) inherits toollabs {
     content => "$ipaddress\n",
   }
 
+  file { "/usr/local/sbin/project-make-shosts":
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+    source => "puppet:///modules/toollabs/project-make-shosts",
+  }
+
+  exec { "make-shosts":
+    command => "/usr/local/sbin/project-make-shosts >/etc/ssh/shosts.equiv~",
+    require => File['/usr/local/sbin/project-make-shosts', $store],
+  }
+
+  file { "/etc/ssh/shosts.equiv":
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0444',
+    source => "/etc/ssh/shosts.equiv~",
+    require => Exec['make-shosts'],
+  }
+
+  file { "/usr/local/sbin/project-make-access":
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+    source => "puppet:///modules/toollabs/project-make-access",
+  }
+
+  exec { "make-access":
+    command => "/usr/local/sbin/project-make-access >/etc/security/access.conf~",
+    require => File['/usr/local/sbin/project-make-access', $store],
+  }
+
+  File <| title == '/etc/security/access.conf' |> {
+    source => "/etc/security/access.conf~",
+    require => Exec['make-access'],
+  }
+
   # TODO: grid node setup
 }
 
