69a36041874b0aa2bea1fc991d79d7b9eaf634f6
Antoine Musso <hashar@free.fr>
2013-05-13 12:21:24 +0200

    beta protoproxy
    Change-Id: I0b02a46f350a99e2e0d29a2da72d6ef6932c8c22

---
+++ modules/protoproxy/manifests/proxy_configuration.pp
@@ -0,0 +1,65 @@
+# == Definition: protoproxy::proxy_configuration
+#
+# This class creates a nginx site. The parameters are merely expanded in the
+# templates which has all of the logic.
+#
+# The resulting site will always listen on the server real IP.
+#
+# === Parameters:
+#
+# [*proxy_addresses*]
+# Additional IP address to listen to. IPv6 addresses will be skipped
+# unless *IpV6_enabled* is true. The hash first level is made of sites
+# entries, the IP are passed as an array.
+# This is optional, the site will always listen on the server real IP.
+# Defaults to {}
+#
+# [*proxy_server_name*]
+#
+# [*proxy_server_cert_name*]
+#
+# [*proxy_backend*]
+#
+# [*enabled*]
+# Whether to enable the site configuration. It will always be generated under
+# /etc/nginx/sites-available , enabling this parameter will create a symbolic
+# link under /etc/nginx/sites-enabled.
+# Defaults to false
+#
+# [*proxy_listen_flags*]
+# Defaults to ''
+#
+# [*proxy_port*]
+# The TCP port to listen on.
+# Defaults to '80'
+#
+# [*ipV6_enabled*]
+# Whether to have the site listen on IPv6 addresses set via *proxy_addresses*
+# Defaults to false
+#
+# [*ssl_backend*]
+# Defaults to {}
+#
+define protoproxy::proxy_configuration(
+  $proxy_addresses={},
+  $proxy_server_name,
+  $proxy_server_cert_name,
+  $proxy_backend,
+  $enabled=false,
+  $proxy_listen_flags='',
+  $proxy_port='80',
+  $ipv6_enabled=false,
+  $ssl_backend={},
+) {
+
+  nginx_site { $name:
+    template => 'proxy',
+    install  => 'template',
+    enable   => $enabled,
+    require  => Package['nginx'],
+    # Make sure we do the configuration before the service
+    # FIXME use notify {} ?
+    before   => Class['protoproxy::service'],
+  }
+
+}
