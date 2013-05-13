3cb1f33d07e5816ad3a6748bac52876c5ee97076
Antoine Musso <hashar@free.fr>
2013-05-13 12:21:24 +0200

    protoproxy: mobile + beta support
    The global protoproxy manifest has been converted to a new module
    'protoproxy' which is fairly simple.  The configuration bits have been
    moved to some new role classes.
    
    Added roles for beta which has a slightly different setup, since we do
    not have LVS in labs, there is a nginx proxy on each of the caches that
    terminates the SSL connection and relay it to the instance IP address.
    
    Only bits is supported for beta, but it is trivial to add new ones by
    using the nice create_resources() definition.
    
    The Ganglia configuration is only applied for production. It is setup to
    create a localhost listening on port 80 which would, in labs, conflict
    with the existing cache (varnish/squid).
    
    Change-Id: I0b02a46f350a99e2e0d29a2da72d6ef6932c8c22

---
+++ modules/protoproxy/manifests/ganglia.pp
@@ -0,0 +1,22 @@
+# vim:sw=4:ts=4:et:
+
+# Ganglia monitoring
+class protoproxy::ganglia {
+
+    file { '/usr/lib/ganglia/python_modules/apache_status.py':
+        source => 'puppet:///files/ganglia/plugins/apache_status.py',
+        notify => Service[gmond];
+    }
+    file { '/etc/ganglia/conf.d/apache_status.pyconf':
+        source => 'puppet:///files/ganglia/plugins/apache_status.pyconf',
+        notify => Service[gmond];
+    }
+
+    # Dummy site to provide a status to Ganglia
+    nginx_site { 'localhost.conf':
+        install => true,
+        enable  => true,
+        require => Package['nginx'],
+    }
+
+}
