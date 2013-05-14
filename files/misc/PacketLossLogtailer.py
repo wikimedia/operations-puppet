be64eb0f0b2573f90469f261ff8f877afbf7c0f7
Andrew Otto <otto@wikimedia.org>
2013-05-14 11:42:23 -0400

    Fixing one more PacketLossLogtailer error
    Change-Id: I01d03a9c1503ebbbcac28090b2dae3496d26f509

--- files/misc/PacketLossLogtailer.py
+++ files/misc/PacketLossLogtailer.py
@@ -28,11 +28,12 @@ class PacketLossLogtailer(object):
         '''This function should initialize any data structures or variables
         needed for the internal state of the line parser.'''
         self.last_update_time = time.time()
+        self.day_in_seconds = 24 * 60 * 60
         self.reset_state()
         self.lock = threading.RLock()
+
         # a list of rolematchers which are simple object to determine the role of a particular server
         # this list is obtained from crawling noc.wikimedia.org/pybal and parse the available configurations
-        self.day_in_seconds = 24 * 60 * 60
         self.matchers = rolematcher.init()
         # this is what will match the packet loss lines
         # packet loss format :
