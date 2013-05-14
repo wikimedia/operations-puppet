da6f9b75001d33d6ff14da8cff4ffdcb1ff0c7a4
Catrope <roan.kattouw@gmail.com>
2013-05-14 12:01:44 -0700

    Add wtp1005-wtp1024 to Parsoid deployment group
    Change-Id: Id94aa51f77770bdaf1da34b45d045163a90c94c9

--- manifests/role/deployment.pp
+++ manifests/role/deployment.pp
@@ -109,7 +109,7 @@ class role::deployment::salt_masters::common($deployment_servers) {
 
 class role::deployment::salt_masters::production {
   $mediawiki_regex = "^(srv|mw|snapshot|tmh)|(searchidx2|searchidx1001).*.(eqiad|pmtpa).wmnet$|^(hume|spence|fenari).wikimedia.org$"
-  $parsoid_regex = "^(wtp1|mexia|tola|lardner|kuo|celsus|constable|wtp1001|wtp1002|wtp1003|wtp1004|cerium|titanium)\..*"
+  $parsoid_regex = "^(wtp1|mexia|tola|lardner|kuo|celsus|constable|wtp10[01][0-9]|wtp102[0-4]|cerium|titanium)\..*"
   $eventlogging_regex = "^(vanadium).eqiad.wmnet$"
   $deployment_servers = {
     "pmtpa" => "tin.eqiad.wmnet",
