0e782cb68adf143bffd63a575be11a97a62c7379
Antoine Musso <hashar@free.fr>
2013-05-14 16:58:08 +0200

    jenkins::slave and a basic role applied gallium
    This patch introduce jenkins::slave that let us setup a jenkins-slave
    user that will accept ssh commands from Jenkins.
    
    The first use case is setting up a slave on gallium. We will later on
    have a second slave setup whenever the server is ready.
    
    Change-Id: If0b518e2e79724307fa7897186fc6720269e4a01

--- manifests/role/jenkins.pp
+++ manifests/role/jenkins.pp
@@ -22,3 +22,19 @@ class role::jenkins::master::production {
 	}
 
 }
+
+class role::jenkins::slave::production {
+
+	system_role { 'role::jenkins::slave::production': description => 'Jenkins slave on production' }
+
+	class { 'jenkins::slave':
+		ssh_authorized_key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw==',
+		ssh_key_name       => 'jenkins@gallium',
+		# Lamely restrict to master which is gallium
+		ssh_key_options => [ 'from=208.80.154.191' ],
+		user            => 'jenkins-slave',
+		home            => '/srv/ssd/jenkins-slave',
+		require         => Mount['/srv/ssd'],
+	}
+
+}
