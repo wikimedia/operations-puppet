18c262fc9c6ea98e1f3aa856acc01ed9132e12b6
DZ <dzahn@wikimedia.org>
2013-05-15 14:35:20 -0700

    new RT4 on Apache config - enable SSL, redirect http to https, avoid conflict with other sites, use a ports.conf with NameVirtualHost *:443, ...
    Change-Id: Idef1da4e06dce16a8c8aca8b5bc04e31222a983b

--- manifests/misc/rt-server-apache.pp
+++ manifests/misc/rt-server-apache.pp
@@ -6,7 +6,7 @@ class misc::rt-apache::server ( $dbuser, $dbpass, $site = 'rt.wikimedia.org', $d
   system_role { 'misc::rt-apache::server': description => 'RT server with Apache' }
 
 
-  if ! defined(Class["webserver::php5"]) {
+  if ! defined(Class['webserver::php5']) {
     class {'webserver::php5': ssl => true; }
   }
 
@@ -16,7 +16,10 @@ class misc::rt-apache::server ( $dbuser, $dbpass, $site = 'rt.wikimedia.org', $d
   $rt_mysql_host = $dbhost
   $rt_mysql_port = $dbport
 
-  package { [ 'request-tracker4', 'rt4-db-mysql', 'rt4-clients', 'libdbd-pg-perl' ]:
+  package { [ 'request-tracker4',
+              'rt4-db-mysql',
+              'rt4-clients',
+              'libdbd-pg-perl' ]:
     ensure => latest;
   }
 
@@ -26,20 +29,20 @@ class misc::rt-apache::server ( $dbuser, $dbpass, $site = 'rt.wikimedia.org', $d
     '/etc/request-tracker4/RT_SiteConfig.d/50-debconf':
       require => package['request-tracker4'],
       content => template('rt/50-debconf.erb'),
-      notify => Exec['update-rt-siteconfig'];
+      notify  => Exec['update-rt-siteconfig'];
     '/etc/request-tracker4/RT_SiteConfig.d/51-dbconfig-common':
       require => package['request-tracker4'],
       content => template('rt/51-dbconfig-common.erb'),
-      notify => Exec['update-rt-siteconfig'];
+      notify  => Exec['update-rt-siteconfig'];
     '/etc/request-tracker4/RT_SiteConfig.d/80-wikimedia':
       require => package['request-tracker4'],
-      source => 'puppet:///files/rt/80-wikimedia',
-      notify => Exec['update-rt-siteconfig'];
+      source  => 'puppet:///files/rt/80-wikimedia',
+      notify  => Exec['update-rt-siteconfig'];
     '/etc/request-tracker4/RT_SiteConfig.pm':
       require => package['request-tracker4'],
-      owner => 'root',
-      group => 'www-data',
-      mode  => '0440';
+      owner   => 'root',
+      group   => 'www-data',
+      mode    => '0440';
     '/etc/request-tracker4/rt.conf':
       require => Package['request-tracker4'],
       content => $rtconf;
@@ -47,10 +50,11 @@ class misc::rt-apache::server ( $dbuser, $dbpass, $site = 'rt.wikimedia.org', $d
 
   exec { 'update-rt-siteconfig':
     command     => '/usr/sbin/update-rt-siteconfig-4',
-    subscribe => file[ "/etc/request-tracker4/RT_SiteConfig.d/50-debconf",
-                       "/etc/request-tracker4/RT_SiteConfig.d/51-dbconfig-common",
-                       "/etc/request-tracker4/RT_SiteConfig.d/80-wikimedia" ],
-    require => package[ 'request-tracker4', 'rt4-db-mysql', 'rt4-clients' ],
+    subscribe   => file[
+                        '/etc/request-tracker4/RT_SiteConfig.d/50-debconf',
+                        '/etc/request-tracker4/RT_SiteConfig.d/51-dbconfig-common',
+                        '/etc/request-tracker4/RT_SiteConfig.d/80-wikimedia' ],
+    require     => package[ 'request-tracker4', 'rt4-db-mysql', 'rt4-clients' ],
     refreshonly => true,
     notify      => Service[apache2];
   }
@@ -61,6 +65,17 @@ class misc::rt-apache::server ( $dbuser, $dbpass, $site = 'rt.wikimedia.org', $d
     group   => root,
     mode    => '0644',
     content => template('rt/rt4.apache.erb'),
+  }
+
+  # use this to have a NameVirtualHost *:443
+  # avoid [warn] _default_ VirtualHost overlap
+
+  file { '/etc/apache2/ports.conf':
+    ensure => present,
+    mode   => '0444',
+    owner  => root,
+    group  => root,
+    source => 'puppet:///files/apache/ports.conf.ssl';
   }
 
   apache_module { 'perl':
