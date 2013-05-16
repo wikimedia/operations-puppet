de4bf7bdd103c928f6fac4b927e9524a88fd05da
Antoine Musso <hashar@free.fr>
2013-05-16 15:47:08 +0200

    beta: appserver should uses /data/project/apache
    On beta, this make sure any application server (web or jobrunner) has a
    /usr/local/apache pointing to /data/project/apache.
    
    Change-Id: I89a92a8d5af7c6923e34abad75cde4e24e3a7090

--- modules/applicationserver/manifests/config/base.pp
+++ modules/applicationserver/manifests/config/base.pp
@@ -3,4 +3,13 @@
 class applicationserver::config::base {
 	# Other classes can depend on this to ensure all configuration
 	# is complete
+	if $::realm == 'labs' {
+		file { '/usr/local/apache':
+			ensure => link,
+			target => '/data/project/apache',
+      # Create it before wikimedia-task-appserver attempts
+      # to create /usr/local/apache/common
+      before => Package['wikimedia-task-appserver'],
+		}
+	}
 }
