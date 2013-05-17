be8395c25433f86db723b970526c6f3ceca6646f
Ryan Lane <rlane@wikimedia.org>
2013-05-16 17:39:56 -0700

    Simplify salt module
    Change-Id: I228b3294022efa391f0f79353ac1db8a2f8f35f4

--- modules/salt/manifests/master_environment.pp
+++ modules/salt/manifests/master_environment.pp
@@ -1,12 +1,17 @@
-define salt::master_environment($salt_state_roots, $salt_file_roots, $salt_pillar_roots, $salt_module_roots, $salt_returner_roots) {
+define salt::master_environment(
+  $salt_state_roots,
+  $salt_file_roots,
+  $salt_pillar_roots,
+  $salt_module_roots,
+  $salt_returner_roots) {
 
   if ! defined(File[$salt_state_roots[$title]]) {
     file { $salt_state_roots[$title]:
-        ensure => directory,
-        mode => 0755,
-        owner => root,
-        group => root;
-      }
+      ensure => directory,
+      mode => 0755,
+      owner => root,
+      group => root;
+    }
   }
 
   if ! defined(File[$salt_file_roots[$title]]) {
--- modules/salt/manifests/reactors.pp
+++ modules/salt/manifests/reactors.pp
@@ -1,27 +1,30 @@
-class salt_reactors($salt_reactor_root="/srv/reactors", $salt_reactor_options={"puppet_server"=>"puppet"}) {
+class salt::reactors(
+  $salt_reactor_root="/srv/reactors",
+  $salt_reactor_options={"puppet_server"=>"puppet"}) {
+
   file { "${salt_reactor_root}/auth.sls":
-    content => template("salt_reactors/auth.sls.erb"),
+    content => template("salt/reactors/auth.sls.erb"),
     owner => root,
     group => root,
     mode => 0444,
     require => File[$salt_reactor_root];
   }
   file { "${salt_reactor_root}/key.sls":
-    content => template("salt_reactors/key.sls.erb"),
+    content => template("salt/reactors/key.sls.erb"),
     owner => root,
     group => root,
     mode => 0444,
     require => File[$salt_reactor_root];
   }
   file { "${salt_reactor_root}/minion_start.sls":
-    content => template("salt_reactors/minion_start.sls.erb"),
+    content => template("salt/reactors/minion_start.sls.erb"),
     owner => root,
     group => root,
     mode => 0444,
     require => File[$salt_reactor_root];
   }
   file { "${salt_reactor_root}/puppet.sls":
-    content => template("salt_reactors/puppet.sls.erb"),
+    content => template("salt/reactors/puppet.sls.erb"),
     owner => root,
     group => root,
     mode => 0444,
