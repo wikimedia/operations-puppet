eb1e7a56e72ed328a90b4a3470e5fa58d16ae3ff
Ariel T. Glenn <ariel@wikimedia.org>
2013-05-17 11:11:58 +0300

    adapt redis template for labs use, update labs redis role settings
    Change-Id: I53a0a5b1350af44f23a365d9d3f5203a576e4d48

--- manifests/role/labsredis.pp
+++ manifests/role/labsredis.pp
@@ -1,7 +1,14 @@
 # See https://gerrit.wikimedia.org/r/#/c/54970/ and https://projects.puppetlabs.com/issues/2053
 # before renaming.
 class role::labs-redis {
-	class { "::redis":
-		dir => "/var/lib/redis/",
-	}
+        require passwords::redis
+
+        class { "::redis":
+                dir                       => "/var/lib/redis/",
+                maxmemory                 => "500mb",
+                persist                   => "aof",
+                redis_replication         => undef,
+                password                  => $::passwords::redis::main_password,
+                auto_aof_rewrite_min_size => "64mb",
+        }
 }
