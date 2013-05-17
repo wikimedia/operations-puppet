6835ea4b83581914654df097aa0d5b5263cee767
Ryan Lane <rlane@wikimedia.org>
2013-05-16 17:39:56 -0700

    Simplify salt module
    Change-Id: I228b3294022efa391f0f79353ac1db8a2f8f35f4

--- modules/salt/manifests/master.pp
+++ modules/salt/manifests/master.pp
@@ -1,36 +1,47 @@
-class salt::master($salt_interface="0.0.0.0", $salt_publish_port="4505", $salt_user="root", $salt_worker_threads="5", $salt_ret_port="4506", $salt_root_dir="/", $salt_pki_dir="/etc/salt/pki", $salt_cachedir="/var/cache/salt", $salt_keep_jobs="24", $salt_timeout="5", $salt_job_cache="True", $salt_runner_dirs=["/srv/runners"], $salt_external_nodes="None", $salt_renderer="yaml_jinja", $salt_failhard="False", $salt_file_roots={"base"=>["/srv/salt"]}, $salt_hash_type="md5", $salt_file_buffer_size="1048576", $salt_pillar_roots={"base"=>["/srv/pillar"]}, $salt_ext_pillar={}, $salt_reactor_root="/srv/reactors", $salt_reactor = {}, $salt_peer={}, $salt_peer_run={}, $salt_cluster_masters="[]", $salt_cluster_mode="paranoid", $salt_nodegroups={}) {
+class salt::master(
+  $salt_interface=undef,
+  $salt_worker_threads=undef,
+  $salt_runner_dirs=["/srv/runners"],
+  $salt_file_roots={"base"=>["/srv/salt"]},
+  $salt_pillar_roots={"base"=>["/srv/pillar"]},
+  $salt_ext_pillar={},
+  $salt_reactor_root="/srv/reactors",
+  $salt_reactor = {},
+  $salt_peer={},
+  $salt_peer_run={},
+  $salt_nodegroups={}) {
 
-	package { ["salt-master"]:
-		ensure => present;
-	}
+  package { ["salt-master"]:
+    ensure => present;
+  }
 
-	service { "salt-master":
-		ensure => running,
-		enable => true,
-		require => [Package["salt-master"]];
-	}
+  service { "salt-master":
+    ensure => running,
+    enable => true,
+    require => [Package["salt-master"]];
+  }
 
-	file { "/etc/salt/master":
-		content => template("salt/master.erb"),
-		owner => root,
-		group => root,
-		mode => 0444,
-		notify => Service["salt-master"],
-		require => [Package["salt-master"]];
-	}
+  file { "/etc/salt/master":
+    content => template("salt/master.erb"),
+    owner => root,
+    group => root,
+    mode => 0444,
+    notify => Service["salt-master"],
+    require => [Package["salt-master"]];
+  }
 
-	file { $salt_runner_dirs:
-		ensure => directory,
-		mode => 0755,
-		owner => root,
-		group => root;
-	}
+  file { $salt_runner_dirs:
+    ensure => directory,
+    mode => 0755,
+    owner => root,
+    group => root;
+  }
 
-	file { $salt_reactor_root:
-		ensure => directory,
-		mode => 0755,
-		owner => root,
-		group => root;
-	}
+  file { $salt_reactor_root:
+    ensure => directory,
+    mode => 0755,
+    owner => root,
+    group => root;
+  }
 
 }
--- modules/salt/manifests/minion.pp
+++ modules/salt/manifests/minion.pp
@@ -1,22 +1,34 @@
-class salt::minion($salt_master="salt", $salt_master_port="4506", $salt_user="root", $salt_root_dir="/", $salt_pki_dir="/etc/salt/pki", $salt_client_id="${fqdn}", $salt_cache_jobs="False", $salt_module_dirs="[]", $salt_returner_dirs="[]", $salt_returner_dirs="[]", $salt_states_dirs="[]", $salt_render_dirs="[]", $salt_grains={}, $salt_renderer="yaml_jinja", $salt_state_verbose="False", $salt_environment="None", $salt_hash_type="md5", $salt_master_finger="", $salt_dns_check="True") {
+class salt::minion(
+  $salt_master=undef,
+  $salt_client_id=undef,
+  $salt_cache_jobs=undef,
+  $salt_module_dirs="[]",
+  $salt_returner_dirs="[]",
+  $salt_returner_dirs="[]",
+  $salt_states_dirs="[]",
+  $salt_render_dirs="[]",
+  $salt_grains={},
+  $salt_environment=undef,
+  $salt_master_finger=undef,
+  $salt_dns_check=undef) {
 
-	package { ["salt-minion"]:
-		ensure => present;
-	}
+  package { ["salt-minion"]:
+    ensure => present;
+  }
 
-	service { "salt-minion":
-		ensure => running,
-		enable => true,
-		require => [Package["salt-minion"]];
-	}
+  service { "salt-minion":
+    ensure => running,
+    enable => true,
+    require => [Package["salt-minion"]];
+  }
 
-	file { "/etc/salt/minion":
-		content => template("salt/minion.erb"),
-		owner => root,
-		group => root,
-		mode => 0444,
-		notify => Service["salt-minion"],
-		require => Package["salt-minion"];
-	}
+  file { "/etc/salt/minion":
+    content => template("salt/minion.erb"),
+    owner => root,
+    group => root,
+    mode => 0444,
+    notify => Service["salt-minion"],
+    require => Package["salt-minion"];
+  }
 
 }
