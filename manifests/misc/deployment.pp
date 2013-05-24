899cad8d8890d722cc3bafbdcffdcead656b5d2b
Antoine Musso <hashar@free.fr>
2013-05-24 14:15:16 +0200

    beta: symlink /a/common
    The multiversion/defines.php does use /a/common which is not existing on
    the beta cluster.  This patch makes it a symbolic link to the shared
    directory.
    
    That also makes puppet ensure /a/common exists in production.
    
    Change-Id: Iaf19f0e8cf029cdbbbb2dabe3ae1d07cee417c39

--- manifests/misc/deployment.pp
+++ manifests/misc/deployment.pp
@@ -364,20 +364,27 @@ class misc::deployment::vars ($system = 'scap') {
 		$dblist_common_source = $dblist_common
 	} elsif $system == "scap" {
 		$mw_common = "/usr/local/apache/common-local"
-		if $::realm == 'production' {
-			$mw_common_source = "/a/common"
-		} else {
-			# For the beta cluster
-			$mw_common_source = "/data/project/apache/common-local"
-		}
+		$mw_common_source = "/a/common"
 		$dblist_common = $mw_common
 		$dblist_common_source = $mw_common_source
 	}
 
 	if $::realm == 'production' {
 		$mw_rsync_host = 'tin.eqiad.wmnet'
+
+		file { $mw_common_source:
+			ensure => directory,
+			owner  => root,
+			group  => wikidev,
+			mode   => '0775',
+		}
 	} else {
 		$mw_rsync_host = 'deployment-bastion.pmtpa.wmflabs'
+
+		file { $mw_common_source:
+			ensure => link,
+			target => '/data/project/apache/common-local',
+		}
 	}
 
 	file {
