ee41e24c10f3bee65e05f1013c0f9ede6890293b
RobH <rob@wikimedia.org>
2013-05-26 11:09:25 +0200

    updating smokeping puppetization
    Change-Id: I9cc90d20a4300263c943f54cecbe8ebe776be600

---
+++ manifests/role/smokeping.pp
@@ -0,0 +1,56 @@
+# https://smokeping.wikimedia.org
+
+class role::smokeping {
+
+    system_role { 'role::smokeping': description => 'Smokeping' }
+
+    include standard-noexim, webserver::php5-gd,
+    misc::smokeping
+
+    if ! defined(Class['webserver::php5']) {
+        class {'webserver::php5': ssl => true; }
+    }
+
+    # be flexible about labs vs. prod
+    case $::realm {
+        labs: {
+            $smokeping_host = "${instancename}.${domain}"
+            $smokeping_ssl_cert = '/etc/ssl/certs/star.wmflabs.pem'
+            $smokeping_ssl_key = '/etc/ssl/private/star.wmflabs.key'
+            install_certificate{ 'star.wmflabs.org': }
+        }
+        production: {
+            $smokeping_host = 'smokeping.wikimedia.org'
+            $smokeping_ssl_cert = '/etc/ssl/certs/star.wikimedia.org.pem'
+            $smokeping_ssl_key = '/etc/ssl/private/star.wikimedia.org.key'
+            install_certificate{ 'star.wikimedia.org': }
+        }
+        default: {
+            fail('unknown realm, should be labs or production')
+        }
+    }
+
+
+    file {
+        "/etc/apache2/sites-available/${smokeping_host}":
+        ensure  => present,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        notify  => Service['apache2'],
+        content => template('apache/sites/smokeping.wikimedia.org.erb');
+    }
+
+    file {
+        "/srv/org/wikimedia/smokeping/index.cgi":
+        ensure  => 'link',
+        target  => '/usr/lib/cgi-bin/smokeping.cgi',
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+    }
+
+    apache_site { 'smokeping': name => "${smokeping_host}" }
+    apache_confd {'namevirtualhost': install => true, name => 'namevirtualhost'}
+
+}
