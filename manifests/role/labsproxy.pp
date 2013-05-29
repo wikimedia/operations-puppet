eb0ae285529a1f877f8c0f6fdba86267c3f80e6b
andrewbogott <abogott@wikimedia.org>
2013-05-29 00:58:25 -0500

    Add a robots.txt for all labs proxy access
    Change-Id: Ia24c35e63fb96c377075f6ea4c1bf83736369778

--- manifests/role/labsproxy.pp
+++ manifests/role/labsproxy.pp
@@ -11,6 +11,21 @@ class role::pmtpa-proxy {
 		install => "template",
 		template => "labs-proxy";
 	}
+
+	file {
+		"/var/www":
+			ensure => directory,
+			owner => root,
+			group => root,
+			mode => 0555;
+		"/var/www/robots.txt":
+			ensure => present,
+			require => file["/var/www"],
+			mode => 0444,
+			owner => root,
+			group => root,
+			source => "puppet:///files/misc/robots-txt-disallow";
+	}
 }
 
 #  Install an http proxy for eqiad labs instances.
@@ -25,5 +40,20 @@ class role::eqiad-proxy {
 	nginx { "eqiad-labs-proxy":
 		install => "template",
 		template => "labs-proxy";
+	}
+
+	file {
+		"/var/www":
+			ensure => directory,
+			owner => root,
+			group => root,
+			mode => 0555;
+		"/var/www/robots.txt":
+			ensure => present,
+			require => file["/var/www"],
+			mode => 0444,
+			owner => root,
+			group => root,
+			source => "puppet:///files/misc/robots-txt-disallow";
 	}
 }
