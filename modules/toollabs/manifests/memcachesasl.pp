bf1e5ea2f390831db1c463a21499d9cb90963947
Petrb <benapetr@gmail.com>
2013-06-01 12:53:08 +0200

    created a stub for memcache
    Change-Id: Iee1d8361801d89ca4035114dc3b83764d2604505

---
+++ modules/toollabs/manifests/memcachesasl.pp
@@ -0,0 +1,18 @@
+# Class: toollabs::memcachesasl
+#
+# This role sets up a memcache with sasl
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class toollabs::memcachesasl inherits toollabs {
+  include toollabs::infrastructure
+
+  #TODO: memcache config
+}
+
