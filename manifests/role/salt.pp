e818c6d9631c17bbcff194507d37e4e1de4ed56b
Ryan Lane <rlane@wikimedia.org>
2013-06-05 19:23:39 -0400

    Set new salt master fingerprint for labs master
    I've regenerated the salt master key, and the minions must trust
    it.
    
    Change-Id: Ic489c759ab4702b499cf541d07674b522f6f91c4

--- manifests/role/salt.pp
+++ manifests/role/salt.pp
@@ -76,7 +76,7 @@ class role::salt::minions {
 			"site" => $site,
 			"cluster" => $cluster,
 		}
-		$salt_master_finger = "5d:07:fb:28:21:60:fb:db:46:ff:e8:1c:91:a2:1a:f9"
+		$salt_master_finger = "c5:b1:35:45:3e:0a:19:70:aa:5f:3a:cf:bf:a0:61:dd"
 	} else {
 		$salt_master = "sockpuppet.pmtpa.wmnet"
 		$salt_client_id = "${fqdn}"
