3c9facd14922877ae4565ed3740f0546be6e54e5
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-06-06 15:12:10 +0200

    Increase ramBufferSizeMB from 32 to 100 and make maximum heap size configurable.
    This makes Solr use more memory and increases performance. Details at
    available at https://issues.apache.org/jira/browse/SOLR-4074. I considered
    setting it to 320, which is what I did somewhere else, but apparently
    there are deminishing returns when using values over 100.
    
    Making the maximum heap configurable is required so we have spurious failures
    on machines without enough ram.  We're defaulting to using 1G in labs and ttm
    and 4G everywhere else.
    
    Bug: 48164
    Change-Id: I61d03705a80f67762091ea3bde8fc2bbcc6ce520

--- manifests/role/solr.pp
+++ manifests/role/solr.pp
@@ -8,10 +8,13 @@
 # $replication_master::   Replication master, if this is current hostname, this server will be a master
 # $average_request_time:: Average request time check threshold, the format is
 #                         "warning threshold:error threshold", or simply "error threshold"
-class role::solr($schema = undef, $replication_master = undef, $average_request_time = "400:600" ) {
+# $max_heap::             Maximum size of the JVM heap that Solr will use.  Use Xmx and Xms valid
+#                         parameters like 4G, 512M, etc.  Defaults to 4G.
+class role::solr($schema = undef, $replication_master = undef, $average_request_time = "400:600", $max_heap = "4G" ) {
 	class { "::solr":
 		schema => $schema,
 		replication_master => $replication_master,
+		max_heap => $max_heap,
 	}
 
 	$check_command = $replication_master ? {
@@ -30,10 +33,11 @@ class role::solr::ttm {
 	class { "role::solr":
 		schema => "puppet:///modules/solr/schema-ttmserver.xml",
 		average_request_time => '1000:1500', # Translate uses fairly slow queries
+		max_heap => "1G",
 	}
 }
 
-class role::solr::geodata($replication_master = 'solr1001.eqiad.wmnet') {
+class role::solr::geodata($replication_master = 'solr1001.eqiad.wmnet', $max_heap = undef) {
 	system_role { "solr-geodata": description => "Solr server for GeoData" }
 
 	include standard
@@ -41,11 +45,13 @@ class role::solr::geodata($replication_master = 'solr1001.eqiad.wmnet') {
 	class { "role::solr":
 		schema => "puppet:///modules/solr/schema-geodata.xml",
 		replication_master => $replication_master,
+		max_heap => $max_heap,
 	}
 }
 
 class role::solr::geodata::labs {
 	class { "role::solr::geodata":
 		replication_master => undef,
+		max_heap => "1G",
 	}
 }
