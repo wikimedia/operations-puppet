2e830cbe6536552e418a66037863f5d572dccf60
Petrb <benapetr@gmail.com>
2013-06-07 17:08:46 +0200

    created a manifest for syslog
    Change-Id: Ie6c9472fec32c5ec92f98d67ed3a053fc4789f99

---
+++ modules/toollabs/manifests/syslog.pp
@@ -0,0 +1,18 @@
+# Class: toollabs::syslog
+#
+# This role sets up a central logging server in the Tool Labs model.
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class toollabs::syslog inherits toollabs {
+  include toollabs::infrastructure
+
+  #TODO:  config
+}
+
