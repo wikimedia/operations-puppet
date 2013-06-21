b8ec4677dc03cf64015e0cbb038e15b0171656fd
RobH <rob@wikimedia.org>
2013-06-21 13:37:37 -0700

    RT 5017 maryana access to analytics cluster + tab spacing cleanup
    Change-Id: Ie523d2b0cd62f73725600778f781cb006babe504

--- manifests/role/analytics.pp
+++ manifests/role/analytics.pp
@@ -56,9 +56,10 @@ class role::analytics::users {
         accounts::olivneh,
         accounts::erik,
         accounts::milimetric,
-        accounts::yurik,   # RT 5158
-        accounts::spetrea, # RT 4402
-        accounts::ram      # RT 5059
+        accounts::yurik,    # RT 5158
+        accounts::spetrea,  # RT 4402
+        accounts::ram,      # RT 5059
+        accounts::maryana   # RT 5017
 
     # add Analytics team members to the stats group so they can
     # access data group owned by 'stats'.
