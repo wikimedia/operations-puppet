e0b74485b355c1a05ee778eec648525fed51c68f
YuviPanda <yuvipanda@gmail.com>
2013-06-24 22:50:00 +0530

    Add initial role for redis server for tool labs
    Change-Id: I016fc22752980224d083bc8aec5fc9d6f8f9dca5

---
+++ modules/toollabs/manifests/redis.pp
@@ -0,0 +1,36 @@
+# Class: toollabs::execnode
+#
+# This role sets up a redis node for use by tool-labs
+# Restricts usage of certain commands, to prevent
+# people from trampling on others' keys
+# Uses default amount of RAM (1G) specified by redis class
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class toollabs::redis inherits toollabs {
+	include toollabs::infrastructure
+
+	class { '::redis': 
+		$persist => "aof",
+		$dir     => "/var/lib/redis",
+		# Disable the following commands, to try to limit people from
+		# Trampling on each others' keys
+		$rename_commands => {
+			"CONFIG"     => "",
+			"FLUSHALL"   => "",
+			"FLUSHDB"    => "",
+			"KEYS"       => "",
+			"SHUTDOWN"   => "",
+			"SLAVEOF"    => "",
+			"CLIENT"     => "",
+			"RANDOMKEY"  => "",
+			"DEBUG"      => ""
+		}
+	}
+}
