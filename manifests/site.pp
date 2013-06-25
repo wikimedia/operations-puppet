bd40adb123f4f600331530c0aba555793b16e46e
Antoine Musso <hashar@free.fr>
2013-06-25 16:18:55 +0200

    contint: vary tmpfs conf on master and slaves
    I am going to need tmpfs filesystem on Jenkins slaves.  Jenkins runs
    under a different usernamee (jenkins-slave instead of jenkins). To
    achieve that this patch:
    
    Make contint::tmpfs a define which let us call it several time on the
    same server, for example whenever the master also has a slave setup.
    
    Change-Id: Ie77991f70459436b749d7b5d347e92959a648788

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,5 @@
 #site.pp
+# vim: noet
 
 import "realm.pp"	# These ones first
 import "generic-definitions.pp"
@@ -1000,6 +1001,11 @@ node "gallium.wikimedia.org" {
 		role::zuul::production,
 		admins::roots,
 		admins::jenkins
+
+	# Make sure continuous integration configuration is applied after we have
+	# setup Jenkins.  We do not want be missing a user.
+	Class['role::jenkins::master::production'] -> Class['misc::contint::test::jenkins::master']
+	Class['role::jenkins::slave::production'] -> Class['misc::contint::test::jenkins::slave']
 
 	# gallium received a SSD drive (RT #4916) mount it
 	file { '/srv/ssd':
