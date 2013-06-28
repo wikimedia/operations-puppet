cd83e3d4c95deab949849489447d7320003f33a7
Andrew Bogott <abogott@wikimedia.org>
2013-06-28 20:32:49 +0000

    Remove misc::deployment::scap_proxy as it appears unused.
    Also removed a conf file that it used.
    
    Change-Id: Ia792a461778e726ffa2473e5224a8431e7e0e72a

--- manifests/misc/deployment.pp
+++ manifests/misc/deployment.pp
@@ -386,7 +386,3 @@ class misc::deployment::vars ($system = "git-deploy") {
 class misc::deployment::scap_primary {
 	class { 'generic::rsyncd': config => "scap-primary" }
 }
-
-class misc::deployment::scap_proxy {
-	class { 'generic::rsyncd': config => "scap" }
-}
