4b61d41b0bc964451de1550edc52d7bf7af010a4
Ori Livneh <ori@wikimedia.org>
2013-07-03 00:09:54 -0700

    Refactor salt::grain & related tool
    This patch renames 'grain-merge' to 'grain-ensure' and makes it accept an
    action parameter, which may be one of 'add', 'remove', or 'contains'. The
    'contains' action is then used to define satisfaction criteria for the exec
    resource that sets or removes the grain value.
    
    Change-Id: I5c8c292ad854bd5cfa262ceedab6b87f5ae10efd

--- modules/salt/manifests/grain.pp
+++ modules/salt/manifests/grain.pp
@@ -1,19 +1,40 @@
+# == Define: salt::grain
+#
+# Set or remove a grain value.
+#
+# === Parameters
+#
+# [*grain*]
+#   The name of the grain. For example, 'deployment-target'.
+#   Defaults to the resource title.
+#
+# [*value*]
+#   Value to set or remove from grain.
+#
+# [*ensure*]
+#   If 'present', adds value to grain. If 'absent', removes it. Defaults
+#   to 'present'.
+#
+# === Examples
+#
+#  salt::grain { 'deployment_target':
+#    value => 'parsoid',
+#  }
+#
 define salt::grain(
-  $grain,
   $value,
+  $grain  = $title,
   $ensure = present,
 ) {
-
-  $opts = $ensure ? {
-    absent  => '--purge',
-    default => '',
-  }
-
-  if ! defined(Exec["grain_$grain_$value"]) {
-    exec { "grain_$grain_$value":
-      command => "/usr/local/sbin/grain-merge $opts $grain $value",
-      require => File["/usr/local/sbin/grain-merge"];
+  if $ensure == 'absent' {
+    exec { "grain-ensure remove ${grain} ${value}":
+      onlyif  => "grain-ensure contains ${grain} ${value}",
+      require => File['/usr/local/sbin/grain-ensure'],
+    }
+  } else {
+    exec { "grain-ensure set ${grain} ${value}":
+      unless  => "grain-ensure contains ${grain} ${value}",
+      require => File['/usr/local/sbin/grain-ensure'],
     }
   }
-
 }
