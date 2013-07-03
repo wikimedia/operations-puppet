6aa61805d8c99b0b884858a9010d2a014fe1e30b
Ori Livneh <ori@wikimedia.org>
2013-07-03 04:12:29 -0700

    Rewrite of EventLogging module
    This commit is the first in a series of commits that aim to comprehensively
    refactor the EventLogging Puppet module. The refactoring migrates process
    management of EventLogging daemons from Supervisord to Upstart and abstracts
    site-specific configurations out of the module itself. The best Puppet modules
    use Puppet abstractions to provide a mini-DSL for managing a service class, and
    that is what I have attempted to do here. The module provides a suite of four
    custom resource types. Each type corresponds to a particular kind of daemonized
    actor that performs some data processing step by transforming the event stream
    in some definite way. These resources can be freely composed into a data
    processing pipeline. The provisioned services are glued together using a
    publisher/subscribe messaging pattern over TCP/IP, which can scale to an
    arbitrary number of instances on an arbitrary number of hosts.
    
    This change provides the bulk of the implementation and most of the
    clean-up.
    
    Change-Id: Iba8cc5d7b121e3e0dd5b557523022c7aa9824f58

---
+++ modules/eventlogging/manifests/packages.pp
@@ -0,0 +1,16 @@
+# == Class: eventlogging::packages
+#
+# This class configures EventLogging package dependencies.
+#
+class eventlogging::packages {
+    package { [
+        'python-jsonschema',
+        'python-mysqldb',
+        'python-pygments',
+        'python-pymongo',
+        'python-sqlalchemy',
+        'python-zmq',
+    ]:
+        ensure => present,
+    }
+}
