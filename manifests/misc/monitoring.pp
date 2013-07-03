28da19cb9bb040ba938942f08c708b20c14fa2c0
Andrew Otto <acotto@gmail.com>
2013-07-03 12:36:29 -0400

    Adding an analytics-data ganglia view
    Change-Id: I2b5e48b3fc2e0c7bf149256b60698e47ca763df8

--- manifests/misc/monitoring.pp
+++ manifests/misc/monitoring.pp
@@ -89,6 +89,11 @@ class misc::monitoring::views {
 		kafka_broker_host_regex   => 'analytics102[12].eqiad.wmnet',
 		kafka_producer_host_regex => 'analytics100[689].eqiad.wmnet',
 	}
+	class { 'misc::monitoring::view::analytics':
+		hdfs_stat_host            => 'analytics1027.eqiad.wmnet',
+		kafka_broker_host_regex   => 'analytics102[12].eqiad.wmnet',
+		kafka_producer_host_regex => 'analytics100[689].eqiad.wmnet',
+	}
 }
 
 # == Define misc:monitoring::view::udp2log
@@ -161,8 +166,8 @@ define misc::monitoring::view::udp2log($host_regex) {
 # kafka specific metrics to include in kafka
 #
 # == Parameters:
-# $kafka_broker_host_regex  - regex matching kafka broker hosts
-# kafka_producer_host_regex - regex matching kafka producer hosts
+# $kafka_broker_host_regex   - regex matching kafka broker hosts
+# $kafka_producer_host_regex - regex matching kafka producer hosts
 #
 define misc::monitoring::view::kafka($kafka_broker_host_regex, $kafka_producer_host_regex) {
 	ganglia::view { $name:
@@ -201,6 +206,41 @@ define misc::monitoring::view::kafka($kafka_broker_host_regex, $kafka_producer_h
 				'host_regex'   => $kafka_producer_host_regex,
 				'metric_regex' => 'udp2log_kafka_producer_.+.AsyncProducerEvents',
 				'type'         => 'stack',
+			},
+		],
+	}
+}
+
+# == Class misc::monitoring::view::analytics::data
+# View for analytics data flow.
+# This is a class instead of a define because it is specific enough to never need
+# multiple instances.
+#
+# == Parameters
+# $hdfs_stat_host            - host on which the webrequest_loss_percentage metric is gathered.
+# $kafka_broker_host_regex   - regex matching kafka broker hosts
+# $kafka_producer_host_regex - regex matching kafka producer hosts, this is the same as upd2log hosts
+#
+class misc::monitoring::view::analytics::data($hdfs_stat_host, $kafka_broker_host_regex, $kafka_producer_host_regex) {
+	ganglia::view { 'analytics-data':
+		graphs => [
+			{
+				'host_regex'   => $kafka_producer_host_regex,
+				'metric_regex' => 'packet_loss_average',
+			},
+			{
+				'host_regex'   => $kafka_producer_host_regex,
+				'metric_regex' => 'udp2log_kafka_producer_.+.AsyncProducerEvents',
+				'type'         => 'stack',
+			},
+			{
+				'host_regex'   => $kafka_broker_host_regex,
+				'metric_regex' => 'kafka_network_SocketServerStats.ProduceRequestsPerSecond',
+				'type'         => 'stack',
+			},
+			{
+				'host_regex'   => $hdfs_stat_host,
+				'metric_regex' => 'webrequest_loss_average',
 			},
 		],
 	}
