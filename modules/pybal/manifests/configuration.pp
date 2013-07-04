3bb0947ec2f51354ab329f15aa86ff902d20eada
Mark Bergsma <mark@wikimedia.org>
2013-07-04 17:04:05 +0200

    Create simple PyBal module
    Change-Id: I80968f26b48883f8e3dd5b83cdf4d6ac359c8ce8

---
+++ modules/pybal/manifests/configuration.pp
@@ -0,0 +1,11 @@
+class pybal::configuration(
+    $global_options={},
+    $lvs_services,
+    $lvs_class_hosts) {
+
+    # Generate PyBal config file
+    file { "/etc/pybal/pybal.conf":
+        require => Package['pybal'],
+        content => template("${module_name}/pybal.conf.erb");
+    }
+}
