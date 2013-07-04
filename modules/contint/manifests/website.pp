61ee75ace47ec938fc848ddd9cf6d6a4467f7313
Antoine Musso <hashar@free.fr>
2013-07-04 11:02:58 +0200

    contint: publish Zuul git repositories
    When running jobs on slaves, we need the Zuul reference it crafts.  This
    can be done by publishing them with git-http-backend bound to the URL
    http://integration.wikimedia.org/zuul/git/
    
    This is limited to the internal network for now, localhost and gallium
    public IP address (the server has a Jenkins slave). Also it is done over
    HTTP instead of HTTPS to save up a bit of SSL processing.
    
    bug: 50695
    Change-Id: I0248003dc3564d4d2fce63f962517a0220b36300

--- modules/contint/manifests/website.pp
+++ modules/contint/manifests/website.pp
@@ -1,7 +1,9 @@
 # Class for website hosted on the continuous integration server
 # https://integration.mediawiki.org/
 # https://doc.wikimedia.org/
-class contint::website {
+class contint::website(
+  $zuul_git_dir = '/var/lib/zuul/git'
+){
 
   # Static files in these docroots are in integration/docroot.git
 
@@ -42,10 +44,10 @@ class contint::website {
 
   # Apache configuration for integration.wikimedia.org
   file { '/etc/apache2/sites-available/integration.wikimedia.org':
-    mode   => '0444',
-    owner  => 'root',
-    group  => 'root',
-    source => 'puppet:///modules/contint/apache/integration.wikimedia.org',
+    mode    => '0444',
+    owner   => 'root',
+    group   => 'root',
+    content => template('contint/apache/integration.wikimedia.org.erb'),
   }
   apache_site { 'integration.wikimedia.org':
     name => 'integration.wikimedia.org',
