f2bf3865dbd7d7af1c1ab76fe7c963899d246006
coren <marc@uberbox.org>
2013-07-05 11:24:48 -0400

    Tool Labs: Skeleton puppet class for UWSGI tyrant
    Change-Id: I768e83ec15e21feb95d37259676c5144e565e152

---
+++ modules/toollabs/manifests/tyrant.pp
@@ -0,0 +1,16 @@
+# Class: toollabs::tyrant
+#
+# This role sets up a node as an UWSGI tyrant
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class toollabs::redis inherits toollabs {
+    include toollabs::infrastructure
+
+}
