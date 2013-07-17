0dc45b808f918dfa2fc69609e8d524f51c74697a
Ryan Lane <rlane@wikimedia.org>
2013-07-17 16:23:52 -0700

    Call apt-get update after adding a new apt repo
    Change-Id: I6adbb113b9429e99608a2893b9ad8af563b86b90

--- modules/apt/manifests/repository.pp
+++ modules/apt/manifests/repository.pp
@@ -1,51 +1,60 @@
 define apt::repository(
-	$uri,
-	$dist,
-	$components,
-	$source=true,
-	$comment_old=false,
-	$keyfile='',
-	$ensure=present
+    $uri,
+    $dist,
+    $components,
+    $source=true,
+    $comment_old=false,
+    $keyfile='',
+    $ensure=present
 ) {
-	$binline = "deb ${uri} ${dist} ${components}\n"
-	$srcline = $source ? {
-		true    => "deb-src ${uri} ${dist} ${components}\n",
-		default => '',
-	}
+    $binline = "deb ${uri} ${dist} ${components}\n"
+    $srcline = $source ? {
+        true    => "deb-src ${uri} ${dist} ${components}\n",
+        default => '',
+    }
 
-	file { "/etc/apt/sources.list.d/${name}.list":
-		ensure  => $ensure,
-		owner   => root,
-		group   => root,
-		mode    => '0444',
-		content => "${binline}${srcline}",
-	}
+    file { "/etc/apt/sources.list.d/${name}.list":
+        ensure  => $ensure,
+        owner   => root,
+        group   => root,
+        mode    => '0444',
+        content => "${binline}${srcline}",
+    }
 
-	if $comment_old {
-		$escuri = regsubst(regsubst($uri, '/', '\/', 'G'), '\.', '\.', 'G')
-		$binre = "deb(-src)?\s+${escuri}\s+${dist}\s+${components}"
+    if $comment_old {
+        $escuri = regsubst(regsubst($uri, '/', '\/', 'G'), '\.', '\.', 'G')
+        $binre = "deb(-src)?\s+${escuri}\s+${dist}\s+${components}"
 
-		# comment out the old entries in /etc/apt/sources.list
-		exec { "apt-${name}-sources":
-			command => "/bin/sed -ri '/${binre}/s/^deb/#deb/' /etc/apt/sources.list",
-			creates => "/etc/apt/sources.list.d/${name}.list",
-			before  => File["/etc/apt/sources.list.d/${name}.list"],
-		}
-	}
+        # comment out the old entries in /etc/apt/sources.list
+        exec { "apt-${name}-sources":
+            command => "/bin/sed -ri '/${binre}/s/^deb/#deb/' /etc/apt/sources.list",
+            creates => "/etc/apt/sources.list.d/${name}.list",
+            before  => File["/etc/apt/sources.list.d/${name}.list"],
+        }
+    }
 
-	if $keyfile {
-		file { "/var/lib/apt/keys/${name}.gpg":
-			ensure  => present,
-			owner   => root,
-			group   => root,
-			mode    => '0400',
-			source  => $keyfile,
-			require => File['/var/lib/apt/keys']
-		}
+    if $keyfile {
+        file { "/var/lib/apt/keys/${name}.gpg":
+            ensure  => present,
+            owner   => root,
+            group   => root,
+            mode    => '0400',
+            source  => $keyfile,
+            require => File['/var/lib/apt/keys'],
+            before  => File["/etc/apt/sources.list.d/${name}.list"],
+        }
 
-		exec { "/usr/bin/apt-key add /var/lib/apt/keys/${name}.gpg":
-			subscribe   => File["/var/lib/apt/keys/${name}.gpg"],
-			refreshonly => true,
-		}
-	}
+        exec { "/usr/bin/apt-key add /var/lib/apt/keys/${name}.gpg":
+            subscribe   => File["/var/lib/apt/keys/${name}.gpg"],
+            refreshonly => true,
+        }
+    }
+
+    exec { 'apt-update-for-${name}':
+        command => '/usr/bin/apt-get update',
+        timeout => 240,
+        returns => [ 0, 100 ],
+        subscribe   => File["/etc/apt/sources.list.d/${name}.list"],
+        refreshonly => true,
+    }
 }
