392f327c36b0c46e32a1d85c0140675f7274e6aa
Mark Bergsma <mark@wikimedia.org>
2013-07-18 20:44:33 +0200

    Set upload backend default cache TTL to 30d
    Change-Id: Ia0cd1f2d8040e54c43238601b2b55fce0693a84a

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -690,8 +690,8 @@ class role::cache {
 			port => 3128,
 			admin_port => 6083,
 			runtime_parameters => $::site ? {
-				'esams' => ["prefer_ipv6=on", "default_ttl=86400"],
-				default => [],
+				'esams' => ['prefer_ipv6=on', 'default_ttl=2592000'],
+				default => ['default_ttl=2592000'],
 			},
 			storage => $::realm ? {
 				production => "-s main1=persistent,/srv/sda3/varnish.main1,${storage_size_main}G -s main2=persistent,/srv/sdb3/varnish.main2,${storage_size_main}G -s bigobj1=file,/srv/sda3/varnish.bigobj1,${storage_size_bigobj}G -s bigobj2=file,/srv/sdb3/varnish.bigobj2,${storage_size_bigobj}G",
