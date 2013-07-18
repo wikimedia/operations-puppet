838fbce67993282791d115c2498847f38b6a7bb6
Ori Livneh <ori@wikimedia.org>
2013-07-18 02:37:45 -0700

    Add eventlogging::plugin custom resource type
    This patch adds a custom resource type, 'eventlogging::plugin'. EventLogging
    plug-ins are Python scripts which register themselves as handlers of some URI
    scheme and which are placed in /usr/local/lib/eventlogging. If an EventLogging
    service is directed at a stream that has a URI with a scheme matching the
    plug-in's, EventLogging will delegate the handling of the stream to that
    plug-in.
    
    Change-Id: I3feae1458e99d30e22bdcc49ab04a5eadead1168

---
+++ modules/eventlogging/manifests/plugin.pp
@@ -0,0 +1,50 @@
+# == Define: eventlogging::plugin
+#
+# EventLogging plug-ins are Python scripts which register themselves as
+# handlers for a particular URI scheme, like 'mongodb://' or 'mysql://'
+# or 'file://'. Upon initialization, EventLogging services traverse the
+# plug-in directory and auto-load any plug-ins they encounter. The
+# services will automatically delegate a stream URI to the plug-in if
+# its scheme matches the one registered by the plug-in.
+#
+# By default, the plug-in directory is </usr/local/lib/eventlogging>,
+# but another path may be configured by setting EVENTLOGGING_PLUGIN_DIR
+# in the environment.
+#
+# === Parameters
+#
+# [*ensure*]
+#   If 'present' (the default), ensures the plug-in file is in place.
+#   If 'absent', ensures the file is removed.
+#
+# [*source*]
+#   Specifies the file that contains the plug-in code. May be a URI or a
+#   path to a local file. Either this or 'content' (but not both) must
+#   be set.
+#
+# [*content*]
+#   The desired contents of the plug-in file, as a string. Either this
+#   or 'source' (but not both) must be set.
+#
+# === Examples
+#
+#  eventlogging::plugin { 'hadoop':
+#    source => 'puppet:///modules/eventlogging/hadoop.py',
+#  }
+#
+define eventlogging::plugin(
+    $ensure  = present,
+    $source  = undef,
+    $content = undef,
+) {
+    $pathsafe = regsubst($title, '\W', '-', 'G')
+    $basename = regsubst($pathsafe, '\.py$', '', 'I')
+
+    file { "/usr/local/lib/eventlogging/${basename}.py":
+        ensure  => $ensure,
+        content => $content,
+        source  => $source,
+        require => File['/usr/local/lib/eventlogging'],
+        notify  => Service['eventlogging/init'],
+    }
+}
