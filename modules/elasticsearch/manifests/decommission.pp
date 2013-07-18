5b71268e00e9a7d442fac2878fa14b9828fb0a76
Nik Everett <neverett@wikimedia.org>
2013-07-18 17:39:35 -0400

    Add elasticsearch module and role.
    I make three changes from the default debian install, all of which are
    recommended by
    http://www.elasticsearch.org/guide/reference/setup/installation/
    1.  Make heap size configurable in the module but default it to 2G which
    is good for labs and probably beta.  I just guessed a big number for
    production and imagine we'll change it.
    2.  Turn on mlockall.
    3.  Turn on logging to see if mlockall works.
    
    Change-Id: I0d764c34c7a35f53fa0b22a4732697ab98ff70ec

---
+++ modules/elasticsearch/manifests/decommission.pp
@@ -0,0 +1,27 @@
+# = Class: elasticsearch
+#
+# This class decommissions the elasticsearch service.
+#
+class elasticsearch::decommission {
+    # Remove package
+    package { 'elasticsearch':
+        ensure  => absent
+    }
+
+    # Remove configuration
+    file { '/etc/elasticsearch/elasticsearch.yml':
+        ensure => absent,
+    }
+    file { '/etc/elasticsearch/logging.yml':
+        ensure => absent,
+    }
+    file { '/etc/default/elasticsearch':
+        ensure => absent,
+    }
+
+    # Stop the service
+    service { 'elasticsearch':
+        ensure => stopped,
+        enable => false,
+    }
+}
