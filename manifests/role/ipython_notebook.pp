a1b4d31d523ac5cfd5643196456cb9d95c483bb1
Ori Livneh <ori@wikimedia.org>
2013-07-18 02:34:51 -0700

    Clean-up: port 'analysis' class to a role class.
    eventlogging::analysis contained much that was vanadium-specific, and its
    placement in modules/eventlogging is bizarre, given that there exists an
    ipython module. I tidied it up and spun it into a role class.
    
    Change-Id: I8bb114cba65ee998d677da919eab3c36868ebfd1

---
+++ manifests/role/ipython_notebook.pp
@@ -0,0 +1,28 @@
+# == Class: role::ipython_notebook
+#
+# This Puppet role provisions IPython Notebook, a web-based data analysis
+# environment based on Python. See the IPython module for more details.
+#
+class role::ipython_notebook {
+    system_role { 'role::ipython_notebook':
+        description => 'IPython Notebook',
+    }
+
+    $ipythondir = '/srv/ipython'
+    $helperfile = "${ipython_dir}/helpers.py"
+
+    class { 'ipython::notebook':
+        ipythondir => $ipythondir,
+        exec_files => [ $helperfile ],
+    }
+
+    file { $helperfile:
+        ensure => present,
+    }
+
+    Pandas and Sympy are popular Python libraries for data analysis
+    and symbolic mathematics.
+    package { [ 'python-pandas', 'python-sympy' ]:
+        ensure => latest,
+    }
+}
