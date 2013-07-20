0c2f49d9f1fbf1d95254c00ea0bbc5c78c814115
andrewbogott <abogott@wikimedia.org>
2013-07-20 10:31:21 -0500

    Create an 'interface' module.
    This is mostly a copy-paste job from manifests::generic-definitions.pp
    
    Change-Id: Ia0bf79e227a6738f7072a8bf4bf984d445d81a4f

---
+++ modules/interface/manifests/add_ip6_mapped.pp
@@ -0,0 +1,29 @@
+define interface::add_ip6_mapped($interface=undef, $ipv4_address=undef) {
+    if ! $interface {
+        $all_interfaces = split($::interfaces, ",")
+        $intf = $all_interfaces[0]
+    }
+    else {
+        $intf = $interface
+    }
+
+    if ! member(split($::interfaces, ","), $intf) {
+        warning("Not adding IPv6 address to $intf because this interface does not exist!")
+    }
+    else {
+        if ! $ipv4_address {
+            $ip4_address = "::${::ipaddress}"
+        }
+        else {
+            $ip4_address = "::${ipv4_address}"
+        }
+
+        $ipv6_address = inline_template("<%= require 'ipaddr'; (IPAddr.new(scope.lookupvar(\"::ipaddress6_${intf}\")).mask(64) | IPAddr.new(ip4_address.gsub('.', ':'))).to_s() %>")
+
+        interface { $title:
+            interface => $intf,
+            address => $ipv6_address,
+            prefixlen => "64"
+        }
+    }
+}
