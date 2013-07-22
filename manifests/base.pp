c58f25bb42c3f7d83ec63b965dc28bc29e5f4c03
Faidon Liambotis <faidon@wikimedia.org>
2013-07-22 09:51:14 +0300

    Workaround fallout from sysctlfile
    sysctlfile breaks puppet on all 8.04 systems of ours. Apply a very
    hackish patch to workaround it for now.
    
    Change-Id: I6138325dfe9ee11b66fa07ac465da3411067b965

--- manifests/base.pp
+++ manifests/base.pp
@@ -303,6 +303,7 @@ class base::sysctl {
 			refreshonly => true;
 		}
 
+		# FIXME: *never* source a file from a module
 		sysctlfile { 'wikimedia-base':
 			source => 'puppet:///modules/sysctlfile/50-wikimedia-base.conf',
 			number_prefix => '50',
@@ -313,6 +314,13 @@ class base::sysctl {
 		# Disable IPv6 privacy extensions, we rather not see our servers hide
 		file { "/etc/sysctl.d/10-ipv6-privacy.conf":
 			ensure => absent
+		}
+	} else {
+	    # FIXME: this is a super ugly hack but the sysctlfile module is broken,
+	    # relying on a definition to be defined in base.pp to actually work
+		exec { "/sbin/start procps":
+			command => '/bin/true',
+			refreshonly => true,
 		}
 	}
 }
