e257318c36666ca07c0c01e54881461d09d0096c
Chad Horohoe <chadh@wikimedia.org>
2013-07-23 09:14:08 -0700

    Adding init script for gitblit
    Bug: 51861
    Change-Id: I7bc57b40072892f328c5e5acea53de5305140d69

--- manifests/misc/gitblit.pp
+++ manifests/misc/gitblit.pp
@@ -38,6 +38,20 @@ class gitblit::instance($host,
 			group => $user,
 			source => "puppet:///files/gitblit/header.md",
 			require => Systemuser[$user];
+		"/etc/init.d/gitblit":
+			mode => 0554,
+			owner => $user,
+			group => $user,
+			source => "puppet:///files/gitblit/gitblit-ubuntu",
+			require => Systemuser[$user];
+	}
+
+	service {
+		"gitblit":
+			subscribe => File["/var/lib/${user}/data/gitblit.properties"],
+			enable => true,
+			ensure => running,
+			require => Systemuser[$user];
 	}
 
 	apache_site { git: name => "git.wikimedia.org" }
