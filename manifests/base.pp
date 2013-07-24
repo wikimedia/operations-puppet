2baf8e83d36e3f213cf399b0350b426802c1ae64
lcarr <lcarr@wikimedia.org>
2013-07-24 14:58:43 -0700

    getting rid of nagios.wikimedia.org trap
    Change-Id: I8857779b86074dbcaaf3cbf7af81e33f5ccdbe83

--- manifests/base.pp
+++ manifests/base.pp
@@ -106,12 +106,6 @@ class base::puppet($server="puppet", $certname=undef) {
 
 	case $::realm {
 		'production': {
-			exec { "puppet snmp trap":
-				command => "snmptrap -v 1 -c public nagios.wikimedia.org .1.3.6.1.4.1.33298 `hostname` 6 1004 `uptime | awk '{ split(\$3,a,\":\"); print (a[1]*60+a[2])*60 }'`",
-				path => "/bin:/usr/bin",
-				require => Package["snmp"]
-			}
-
 			exec {	"neon puppet snmp trap":
 					command => "snmptrap -v 1 -c public neon.wikimedia.org .1.3.6.1.4.1.33298 `hostname` 6 1004 `uptime | awk '{ split(\$3,a,\":\"); print (a[1]*60+a[2])*60 }'`",
 					path => "/bin:/usr/bin",
