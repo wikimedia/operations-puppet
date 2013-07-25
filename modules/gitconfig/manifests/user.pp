f541246e00cd16b34d8b24b0f6d007a651220b68
Antoine Musso <hashar@free.fr>
2013-07-25 14:24:49 +0200

    gitconfig::user to easily craft .gitconfig user files
    The new module git contains a single define 'gitconfig::user' which let
    one craft a .gitconfig file using a hash defining its content.  Simple
    example is provided in the class documentation and a very basic rspec
    test is provided to check whether the expansion happens properly.
    
    Example usage:
    
      gitconfig::user { 'gitconf for jenkins user':
        homedir => '/var/lib/jenkins',
        settings => {
          'user' => {  # '[user]'
             'name'  => 'Antoine Musso',  # 'name = Antoine Musso'
             'email' => 'hashar@free.fr', # 'email = hashar@free.fr'
          },  # end of [user] section
        }
      }
    
    Change-Id: I49a03405fbe715269e396f6f1e0c956ba10a5f0a

---
+++ modules/gitconfig/manifests/user.pp
@@ -0,0 +1,33 @@
+# == gitconfig::user
+#
+# Generate a .gitconfig in $homedir based on a hash of gitconfig values.
+# The file will be owned by root since it is fully managed by puppet.
+#
+# Parameters:
+#  [*homedir*] - user home dir where .gitconfig will be written
+#  [*settings*] - hash of gitconfig section name, each should be in turn a hash
+#  of configuration name => value.
+#
+# Example usage:
+#
+# gitconfig::user{ 'gitconf for jenkins user':
+#   homedir => '/var/lib/jenkins',
+#   settings => {
+#     'user' => {  # '[user]'
+#        'name'  => 'Antoine Musso',  # 'name = Antoine Musso'
+#        'email' => 'hashar@free.fr', # 'email = hashar@free.fr'
+#     },  # end of [user] section
+#   }
+# }
+#
+define gitconfig::user($homedir, $settings) {
+
+  file { "${homedir}/.gitconfig":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    content => template( 'gitconfig/gitconfig.erb' )
+  }
+
+}
