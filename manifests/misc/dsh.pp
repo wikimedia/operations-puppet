7ec89c7038deee02891414ccb85ee1dbeefce4fb
Max Semenik <maxsem.wiki@gmail.com>
2013-07-26 15:59:37 +0400

    Icinga check for host's presence in needed dsh groups
    Not adding it to any role classes yet
    
    Change-Id: I0b86c6939821434ff40e5360e17d21f6e0a3ee3f

--- manifests/misc/dsh.pp
+++ manifests/misc/dsh.pp
@@ -4,18 +4,31 @@ class misc::dsh {
 	package { "dsh":
 		ensure => present
 	}
+	include files
+	
+	class files {
+		file {
+			"/etc/dsh/group":
+				owner => root,
+				group => root,
+				mode => 0444,
+				source => "puppet:///files/dsh/group",
+				recurse => true;
+			"/etc/dsh/dsh.conf":
+				owner => root,
+				group => root,
+				mode => 0444,
+				source => "puppet:///files/dsh/dsh.conf";
+		}
+	}
+}
 
-	file {
-		"/etc/dsh/group":
-			owner => root,
-			group => root,
-			mode => 0444,
-			source => "puppet:///files/dsh/group",
-			recurse => true;
-		"/etc/dsh/dsh.conf":
-			owner => root,
-			group => root,
-			mode => 0444,
-			source => "puppet:///files/dsh/dsh.conf";
+# Checks that this host belongs to dsh group(s)
+define dsh_groups( $groups = [] ) {
+	$groups_string = join( $groups, ' ' )
+
+	monitor_service { "dsh":
+		description => 'dsh groups',
+		check_command => "check_dsh_groups!$groups_string",
 	}
 }
