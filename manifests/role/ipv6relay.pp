56f66d9b88814c86e62c38e6c38cd6b83f90c956
Antoine Musso <hashar@free.fr>
2013-07-27 14:50:57 +0200

    style for ipv6relay.pp
    Pass most puppet-lint checks and reindent to four spaces.
    
    Change-Id: I1108020d7ec73918a38ed1890a3d7d5fb4078c29

--- manifests/role/ipv6relay.pp
+++ manifests/role/ipv6relay.pp
@@ -1,11 +1,16 @@
+# vim: set sw=4 ts=4 expandtab:
+
+# == Class: role::ipv6relay
+#
+# Provides Teredo connectivity which let IPv4 only hosts to use IPv6 with us.
 class role::ipv6relay {
-        system_role { "role::ipv6relay": description => "IPv6 tunnel relay (6to4/Teredo)" }
+    system_role { 'role::ipv6relay': description => 'IPv6 tunnel relay (6to4/Teredo)' }
 
-	include sysctlfile::advanced-routing-ipv6
+    include sysctlfile::advanced-routing-ipv6
 
-	# Teredo
-	include misc::miredo
+    # Teredo
+    include misc::miredo
 
-	# 6to4
-	interface_tun6to4 { "tun6to4": }
+    # 6to4
+    interface_tun6to4 { 'tun6to4': }
 }
