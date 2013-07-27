2980e5d491a5aefa4a98be424afde23c30d3c1de
Antoine Musso <hashar@free.fr>
2013-07-27 14:43:21 +0200

    style for beta.pp role
    Pass most puppet-lint checks and reindent to four spaces.
    
    Change-Id: Iaecaa783dd16cc95500e2419c290c156aba74b10

--- manifests/role/beta.pp
+++ manifests/role/beta.pp
@@ -1,13 +1,20 @@
+# vim: set sw=4 ts=4 expandtab:
+
+# == Class: role::beta::autoupdater
+#
+# For host continuously updating MediaWiki core and extensions on the beta
+# cluster. This is the lame way to automatically pull any code merged in master
+# branches.
 class role::beta::autoupdater {
 
-	include misc::beta::autoupdater
+    include misc::beta::autoupdater
 
-	system_role { 'role::beta::autoupdater':
-		description => 'Server is autoupdating MediaWiki core and extension on beta.'
-	}
+    system_role { 'role::beta::autoupdater':
+        description => 'Server is autoupdating MediaWiki core and extension on beta.'
+    }
 
 }
 
 class role::beta::maintenance {
-	class{ 'misc::maintenance::geodata': enabled => true }
+    class{ 'misc::maintenance::geodata': enabled => true }
 }
