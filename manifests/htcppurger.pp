43aa7e9266009bba8094bc6521d6ec9284129b9b
Mark Bergsma <mark@wikimedia.org>
2013-07-31 12:39:09 +0200

    Don't use absolute URIs for purging
    Change-Id: I50a76153055a262ec628fa437c0bf4922c0319d1

--- manifests/htcppurger.pp
+++ manifests/htcppurger.pp
@@ -11,8 +11,7 @@ class varnish::htcppurger($varnish_instances=["localhost:80"]) {
         mode => 0444,
         require => Package["vhtcpd"], # if we go first, we get overwritten
         # TODO: -r ^upload\\.wikimedia\\.org\$ (POSIX ERE, new param for class, quoting/escaping will be tricky...)
-        # TODO: remove -F when VCL updated to match (no hostname in PURGE URL)
-        content => inline_template('DAEMON_OPTS="-F -m 239.128.0.112<% varnish_instances.each do |inst| -%> -c <%= inst %><% end -%>"');
+        content => inline_template('DAEMON_OPTS="-m 239.128.0.112<% varnish_instances.each do |inst| -%> -c <%= inst %><% end -%>"');
     }
 
     # Wikimedia used to provide vhtcpd under the name varnishhtcpd with an
