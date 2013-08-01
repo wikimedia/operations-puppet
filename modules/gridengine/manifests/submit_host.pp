9e8383c4f6c7d70322d3371ab23b2f86a72a8ba8
Tim Landscheidt <tim@tim-landscheidt.de>
2013-08-01 21:33:54 +0000

    Tools: Manage obsolete files in /usr/local/bin
    /usr/local/bin had some obsolete versions of scripts that since then
    have been moved to /usr/bin.  To properly manage those, we add
    symbolic links pointing to /usr/bin.  Also, the requirement of the
    jobutils package is moved from toollabs::bastion and
    toollabs::webserver to gridengine::submit_host.
    
    Change-Id: I28ff4ea1806f0f58e1738cf8194f44ea49085608

--- modules/gridengine/manifests/submit_host.pp
+++ modules/gridengine/manifests/submit_host.pp
@@ -6,7 +6,7 @@ class gridengine::submit_host($gridmaster = $grid_master) {
 		gridmaster => $gridmaster,
 	}
 
-        package { "gridengine-client":
+        package { [ "gridengine-client", "jobutils" ]:
                 ensure => latest,
         }
 
@@ -15,6 +15,14 @@ class gridengine::submit_host($gridmaster = $grid_master) {
           user => root,
           minute => [1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56],
           ensure => present;
+        }
+
+        # Temporary hack to manage obsolete files in /usr/local/bin.
+        # TODO: Remove when no longer needed.
+        file { [ "/usr/local/bin/job", "/usr/local/bin/jstart",
+                 "/usr/local/bin/jstop", "/usr/local/bin/jsub"]:
+          ensure => link,
+          target => regsubst(name, "^/usr/local/bin/", "/usr/bin/")
         }
 
 # Not actually possible in the labs
