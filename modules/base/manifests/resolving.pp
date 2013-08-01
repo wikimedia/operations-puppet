77046f27db0d74f566a40e4528a6dc0732fc6a69
Andrew Bogott <abogott@wikimedia.org>
2013-08-01 19:43:45 +0000

    Move base class and subclasses into a 'base' module.
    This isn't perfect, but it should be a safe change...
    more to come.
    
    Change-Id: I81db39b163533530f0b5b19945ca9cf54c596400

---
+++ modules/base/manifests/resolving.pp
@@ -0,0 +1,15 @@
+class base::resolving {
+	if ! $::nameservers {
+		error("Variable $::nameservers is not defined!")
+	}
+	else {
+		if $::realm != "labs" {
+			file { "/etc/resolv.conf":
+				owner => root,
+				group => root,
+				mode => 0444,
+				content => template("base/resolv.conf.erb");
+			}
+		}
+	}
+}
