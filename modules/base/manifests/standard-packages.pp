c47f02d3512d646abd322d489a62e2e7cbe8bf3d
Andrew Bogott <abogott@wikimedia.org>
2013-08-01 19:43:45 +0000

    Move base class and subclasses into a 'base' module.
    This isn't perfect, but it should be a safe change...
    more to come.
    
    Change-Id: I81db39b163533530f0b5b19945ca9cf54c596400

---
+++ modules/base/manifests/standard-packages.pp
@@ -0,0 +1,44 @@
+class base::standard-packages {
+
+	$packages = [
+		'wikimedia-base',
+		'wipe',
+		'tzdata',
+		'zsh-beta',
+		'jfsutils',
+		'xfsprogs',
+		'wikimedia-raid-utils',
+		'screen',
+		'gdb',
+		'iperf',
+		'atop',
+		'htop',
+		'vim',
+		'sysstat',
+		'ngrep',
+		'httpry',
+		'acct',
+		'git-core',
+		'ack-grep',
+	]
+
+	if $::lsbdistid == "Ubuntu" {
+		package { $packages:
+			ensure => latest;
+		}
+
+		if $::network_zone == "internal" {
+			include nrpe
+		}
+
+		# Run lldpd on all >= Lucid hosts
+		if $::lsbdistid == "Ubuntu" and versioncmp($::lsbdistrelease, "10.04") >= 0 {
+			package { lldpd: ensure => latest; }
+		}
+
+		# DEINSTALL these packages
+		package { [ "mlocate" ]:
+			ensure => absent;
+		}
+	}
+}
---
+++ modules/base/manifests/vimconfig.pp
@@ -0,0 +1,16 @@
+class base::vimconfig {
+	file { "/etc/vim/vimrc.local":
+		owner => root,
+		group => root,
+		mode => 0444,
+		source => "puppet:///files/misc/vimrc.local",
+		ensure => present;
+	}
+
+	if $::lsbdistid == "Ubuntu" {
+		# Joe is for pussies
+		file { "/etc/alternatives/editor":
+			ensure => "/usr/bin/vim"
+		}
+	}
+}
