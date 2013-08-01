41a9bf2aaee66ebde994cfe30dc069e93c38b1f5
Andrew Otto <acotto@gmail.com>
2013-08-01 17:05:12 -0400

    Replicating operations/puppet/kafka to github wikimedia/puppet-kafka
    Change-Id: Ia6e7f4070f0c5321d649ed1bd63d848617087e33

--- manifests/role/gerrit.pp
+++ manifests/role/gerrit.pp
@@ -25,21 +25,22 @@ class role::gerrit {
 			ssh_key      => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAxOlshfr3UaPr8gQ8UVskxHAGG9xb55xDyfqlK7vsAs/p+OXpRB4KZOxHWqI40FpHhW+rFVA0Ugk7vBK13oKCB435TJlHYTJR62qQNb2DVxi5rtvZ7DPnRRlAvdGpRft9JsoWdgsXNqRkkStbkA5cqotvVHDYAgzBnHxWPM8REokQVqil6S/yHkIGtXO5J7F6I1OvYCnG1d1GLT5nDt+ZeyacLpZAhrBlyFD6pCwDUhg4+H4O3HGwtoh5418U4cvzRgYOQQXsU2WW5nBQHE9LXVLoL6UeMYY4yMtaNw207zN6kXcMFKyTuF5qlF5whC7cmM4elhAO2snwIw4C3EyQgw==',
 			ssl_cert     => 'gerrit.wikimedia.org',
 			ssl_cert_key => 'gerrit.wikimedia.org',
+			smtp_host    => 'smtp.pmtpa.wmnet',
 			replication  => {
 				# If adding a new entry, remember to add the fingerprint to gerrit2's known_hosts
-				'inside-wmf'             => {
+				'inside-wmf'              => {
 					'url'                  => 'gerritslave@gallium.wikimedia.org:/var/lib/git/${name}.git',
 					'threads'              => '4',
 					'mirror'               => 'true',
 				},
-				'gitblit'                => {
+				'gitblit'                 => {
 					'url'                   => 'gerritslave@antimony.wikimedia.org:/var/lib/git/${name}.git',
 					'threads'               => '4',
 					'authGroup'             => 'mediawiki-replication',
 					'push'                  => '+refs/*:refs/*',
 					'mirror'                => 'true',
 				},
-				'github'                 => {
+				'github'                  => {
 					'url'                  => 'git@github.com:wikimedia/${name}',
 					'threads'              => '4',
 					'authGroup'            => 'mediawiki-replication',
@@ -48,7 +49,7 @@ class role::gerrit {
 					'remoteNameStyle'      => 'dash',
 					'mirror'               => 'true',
 				},
-				'github-puppet-cdh4'     => {
+				'github-puppet-cdh4'      => {
 					'url'                  => 'git@github.com:wikimedia/puppet-cdh4',
 					'threads'              => '1',
 					'authGroup'            => 'mediawiki-replication',
@@ -58,7 +59,7 @@ class role::gerrit {
 					'mirror'               => 'true',
 					'projects'             => 'operations/puppet/cdh4',
 				},
-				'github-puppet-jmxtrans' => {
+				'github-puppet-jmxtrans'  => {
 					'url'                  => 'git@github.com:wikimedia/puppet-jmxtrans',
 					'threads'              => '1',
 					'authGroup'            => 'mediawiki-replication',
@@ -78,7 +79,7 @@ class role::gerrit {
 					'mirror'               => 'true',
 					'projects'             => 'operations/puppet/zookeeper',
 				},
-				'github-kraken' => {
+			    'github-kraken'           => {
 					'url'                  => 'git@github.com:wikimedia/kraken',
 					'threads'              => '1',
 					'authGroup'            => 'mediawiki-replication',
@@ -88,8 +89,18 @@ class role::gerrit {
 					'mirror'               => 'true',
 					'projects'             => 'analytics/kraken',
 				},
+
+				'github-puppet-kafka'     => {
+					'url'                  => 'git@github.com:wikimedia/puppet-kafka',
+					'threads'              => '1',
+					'authGroup'            => 'mediawiki-replication',
+					'push'                 => '+refs/heads/*:refs/heads/*
+  push = +refs/tags/*:refs/tags/*',
+					'remoteNameStyle'      => 'dash',
+					'mirror'               => 'true',
+					'projects'             => 'operations/puppet/kafka',
+				},
 			},
-			smtp_host => 'smtp.pmtpa.wmnet'
 		}
 	}
 
