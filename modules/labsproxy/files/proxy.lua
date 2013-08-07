628bd6c3a50d92b21d46820d93435df803c5008c
YuviPanda <yuvipanda@gmail.com>
2013-08-07 16:50:19 +0800

    Read routing tables from Redis
    Change-Id: I549958d0370426a3317d053d978b31f8a4fc9aef

--- modules/labsproxy/files/proxy.lua
+++ modules/labsproxy/files/proxy.lua
@@ -1,14 +1,16 @@
 -- Lua file run by nginx that does appropriate routing
 -- Gets domain name, figures out instance name from it, and routes there
+
+local redis = require 'resty.redis'
+local red = redis:new()
+red.set_timeout(1000)
+
+red:connect('127.0.0.1', 6379)
+
 local frontend = ngx.re.match(ngx.var.http_host, "^([^:]*)")[1]
-local instance_match = ngx.re.match(frontend, "(\\d+)?\\.?([^.]+)\\.proxy\\.wmflabs\\.org$")
+local domain = ngx.re.match(frontend, "([^.]+)\\.proxy\\.wmflabs\\.org$")[1]
 
-local instance_port = 80
-local instance_name = instance_match[2]
+local backend = red:srandmember('frontend:' .. domain)[1]
 
-if instance_match[1] ~= nil then
-        instance_port = instance_match[1]
-end
-
-ngx.var.backend = "http://" .. instance_name .. ":" .. instance_port
-ngx.var.vhost = instance_name
+ngx.var.backend = backend
+ngx.var.vhost = domain
