a041a28cca47e5f4696b065dd13d2c18241a7794
Tim Landscheidt <tim@tim-landscheidt.de>
2013-08-10 05:06:53 +0000

    Tools: Puppetize tips.sh
    Change-Id: Iad5685e0fe559108d36ae50d43c28fcce809eace

--- modules/toollabs/manifests/bastion.pp
+++ modules/toollabs/manifests/bastion.pp
@@ -45,6 +45,26 @@ class toollabs::bastion($gridmaster) inherits toollabs {
     source => "puppet:///modules/toollabs/sql",
   }
 
+  # Display tips.
+  package { 'grep':
+    ensure => present,
+  }
+
+  file { "/etc/profile.d/motd-tips.sh":
+    ensure => file,
+    mode => "0555",
+    owner => "root",
+    group => "root",
+    source => "puppet:///modules/toollabs/motd-tips.sh",
+    require => Package['grep'],
+  }
+
+  file { [ '/data/project/.system/tips.sh',
+           '/data/project/.system/bin/tips.sh',
+           '/data/project/.system/bin/tips2.sh' ]:
+    ensure => absent,
+  }
+
   package { [ 'jobutils', 'misctools' ]:
     ensure => latest,
   }
