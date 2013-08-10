ff1a67de6ef99e556434879c696e355d2e370e7a
Tim Landscheidt <tim@tim-landscheidt.de>
2013-08-10 05:06:53 +0000

    Tools: Puppetize tips.sh
    Change-Id: Iad5685e0fe559108d36ae50d43c28fcce809eace

--- modules/toollabs/files/profile-tool-labs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if tty -s
-then
-  [ -f /data/project/.system/tips.sh ] && /data/project/.system/tips.sh
-fi
---
+++ modules/toollabs/files/tips.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Disable when non-interactive.
+if ! tty -s; then
+    return
+fi
+
+# Check if user wants to display tips.
+if [ -f ~/.suppresstips ]; then
+    return
+fi
+
+# Check if there is a tip DB.
+if [ ! -f /data/project/.system/tips ]; then
+    return
+fi
+
+# Don't display tips for root and tool accounts.
+if [ $UID -eq 0 ] || [ "${USER:0:6}" = "local-" ]; then
+    return
+fi
+
+# Check shell level.
+if [ "$SHLVL" -gt 1 ]; then
+    return
+fi
+
+# If user is sysadmin print the sysadmin motd instead.
+if groups | fgrep -qw local-admin; then
+    if [ -f /etc/motd.sysadmin ]; then
+        echo
+        echo
+        cat /etc/motd.sysadmin
+    fi
+
+    # We don't want to show tips to sysadmins.
+    return
+fi
+
+echo -e "\n\033[0;1;4mDid you know\033[0m that `shuf -n 1 /data/project/.system/tips`\n"
