e286f98391f8ade54552493d0afa31e415b12116
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-08-12 19:34:49 +0300

    Rewrite etherpad to etherpad-lite URLs for compat
    Trying to keep URLs from old etherpad for compatibility reason.
    Rewriting any URLs not having a / in them assuming that all are names
    Also add the old hostname to ServerName
    
    Change-Id: Iffccb3a2097295d98ba4a66f4501ef9bf7f674e5

--- manifests/misc/etherpad.pp
+++ manifests/misc/etherpad.pp
@@ -63,7 +63,7 @@ class misc::etherpad_lite {
 		$etherpad_ssl_cert = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
 		$etherpad_ssl_key = '/etc/ssl/private/ssl-cert-snakeoil.key'
 	} else {
-		$etherpad_host = 'epl.wikimedia.org'
+		$etherpad_host = 'etherpad.wikimedia.org epl.wikimedia.org'
 		$etherpad_ssl_cert = '/etc/ssl/certs/star.wikimedia.org.pem'
 		$etherpad_ssl_key = '/etc/ssl/private/star.wikimedia.org.key'
 	}
