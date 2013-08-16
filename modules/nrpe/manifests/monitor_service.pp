9d4f7b59dedd9fd64d15e459c0192873438c89a0
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-08-16 12:12:04 +0300

    Refactoring nrpe module (round 2/??)
    /usr/local/lib/nagios/plugins directory for plugins shipped by module
    Merge nrpe::packages and nrpe::service classes in nrpe class
    nrpe class now accepts an allowed_hosts argument. It defaults to undef.
    Cleaned-up Service['nagios-nrpe-server']
     hasrestart defaults to true in puppet 2.7
     hasstatus defaults to true in puppet 2.7 so pattern is not needed
     restart not needed (init script works fine)
    Updated rspec examples
    Updated puppet tests
    Removed spec fixtures that caused a problem with puppet doc(cyclic deps)
    Added Rake code to create and purge above fixtures before each run
    Updated nrpe_local.cfg.erb
    nrpe::check now ships virtual resources which are realized by nrpe class
    virtual monitor_service resources tagged nrpe are no longer realized by
    nrpe class (none existed anyway)
    
    Change-Id: I96bcccae226c0b308ec528a52259d4c1bf764afb

--- modules/nrpe/manifests/monitor_service.pp
+++ modules/nrpe/manifests/monitor_service.pp
@@ -9,33 +9,31 @@
 #    $description
 #       Service check description
 #    $nrpe_command
-#       If defined, installs this NRPE command as check_${title}
+#       The path to the actual binary/script. A stanza for nrpe daemon will be
+#       added with that path and a nagios_service check will be exported to
+#       nagios server.
 #    $contact_group
-#       Defaults to admins, the nagios contact groupo for the service
+#       Defaults to admins, the nagios contact group for the service
 #    $retries
 #       Defaults to 3. The number of times a service will be retried before
 #       notifying
 #    $ensure
 #       Defaults to present
 #
-define nrpe::monitor_service(
-                            $description,
-                            $nrpe_command  = undef,
-                            $contact_group = 'admins',
-                            $retries       = 3,
-                            $ensure        = 'present') {
-    if $nrpe_command != undef {
-        nrpe::check { "check_${title}":
-            command => $nrpe_command,
-            before  => ::Monitor_service[$title],
-        }
-    }
-    else {
-        # TODO: Figure out why this is here
-        Nrpe::Check["check_${title}"] -> Nrpe::Monitor_service[$title]
+define nrpe::monitor_service( $description,
+                              $nrpe_command,
+                              $contact_group = 'admins',
+                              $retries       = 3,
+                              $ensure        = 'present') {
+
+    nrpe::check { "check_${title}":
+        command => $nrpe_command,
+        before  => ::Monitor_service[$title],
     }
 
-    ::monitor_service{ $title:
+    # TODO: Refactor this to make a call to nagios::monitor_service (or similar) after nagios
+    # has been refactored to a module. It is known to cause rspec tests to fail
+    ::monitor_service { $title:
         ensure        => $ensure,
         description   => $description,
         check_command => "nrpe_check!check_${title}",
--- modules/nrpe/spec/defines/monitor_service_spec.rb
+++ modules/nrpe/spec/defines/monitor_service_spec.rb
@@ -2,19 +2,26 @@ require 'spec_helper'
 
 describe 'nrpe::monitor_service', :type => :define do
     let(:title) { 'something' }
-    let(:params) { {
-	    :description   => 'this is a description',
-	    :contact_group => 'noone',
-    } }
-    #let(:params) { { :command => '/usr/local/bin/mycommand -i this -o that' } }
+    let(:params) do {
+        :description   => 'this is a description',
+        :contact_group => 'none',
+        :nrpe_command  => '/usr/local/bin/mycommand -i this -o that',
+        }
+    end
 
     it do
-	should contain_monitor_service('something').with(
-	    :description   => 'this is a description',
-	    :contact_group => 'noone',
-	    :retries       => 3,
-	    :ensure        => 'present',
-	    :check_command => 'nrpe!check_something'
-	)
+        should contain_nrpe__check('check_something').with(
+            :command       => '/usr/local/bin/mycommand -i this -o that',
+            :ensure        => 'present'
+        )
+    end
+    it do
+        should contain_monitor_service('something').with(
+            :description   => 'this is a description',
+            :contact_group => 'none',
+            :retries       => 3,
+            :ensure        => 'present',
+            :check_command => 'nrpe!check_something'
+        )
     end
 end
