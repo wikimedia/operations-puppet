7f74fddb3fd24da1d3c169458c1cea4e2980581a
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-08-16 12:12:04 +0300

    Refactoring nrpe module (round 2/??)
    /usr/local/lib/nagios/plugins directory for plugins shipped by module
    Merge nrpe::packages and nrpe::service classes in nrpe class
    nrpe class now accepts an allowed_hosts argument. It defaults to undef.
    Cleaned-up Service['nagios-nrpe-server']
     hasrestart defaults to true in puppet 2.7
     hasstatus defaults to true in puppet 2.7 so pattern is not needed
     restart not needed (init script works fine)
    Updated rspec examples
    Updated puppet tests
    Removed spec fixtures that caused a problem with puppet doc(cyclic deps)
    Added Rake code to create and purge above fixtures before each run
    Updated nrpe_local.cfg.erb
    
    Change-Id: I96bcccae226c0b308ec528a52259d4c1bf764afb

--- modules/nrpe/manifests/init.pp
+++ modules/nrpe/manifests/init.pp
@@ -1,6 +1,6 @@
 # Class: nrpe
 #
-# This installes nrpe packages, ensures service in running and collects all
+# This installes nrpe packages, ensures service is running and collects all
 # configuration
 #
 # Parameters:
@@ -11,16 +11,62 @@
 #   Collect all needed exported resources
 #
 # Requires:
-#   Class[nrpe::packages]
-#   Class[nrpe::service]
-#   Define[monitor_service]
+#   Definition[monitor_service]
 #
 # Sample Usage:
 #   include nrpe
 
-class nrpe {
-    include nrpe::packages
-    include nrpe::service
+class nrpe($allowed_hosts=undef) {
+    package { [ 'nagios-nrpe-server',
+                'nagios-plugins',
+                'nagios-plugins-basic',
+                'nagios-plugins-extra',
+                'nagios-plugins-standard' ]:
+        ensure => present,
+    }
+
+    if $allowed_hosts == undef {
+        $nrpe_allowed_hosts = $::realm ? {
+            'production' => '127.0.0.1,208.80.152.185,208.80.152.161,208.80.154.14',
+            'labs'       => '10.4.0.120',
+            default      => '127.0.0.1',
+        }
+    } else {
+        $nrpe_allowed_hosts = $allowed_hosts
+    }
+
+    # TODO: Figure this out
+    file { '/etc/nagios/nrpe_local.cfg':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0444',
+        content => template('nrpe/nrpe_local.cfg.erb'),
+        require => Package['nagios-nrpe-server'],
+        notify  => Service['nagios-nrpe-server'],
+    }
+
+    # TODO: Remove this after the file has been purged everywhere
+    file { '/usr/lib/nagios/plugins/check_dpkg':
+        ensure  => absent,
+    }
+
+    # Have a directory with all our plugins.
+    file { '/usr/local/lib/nagios/plugins/':
+        ensure  => directory,
+        owner   => root,
+        group   => root,
+        mode    => '0444',
+        recurse => true,
+        purge   => true,
+        force   => true,
+        source  => 'puppet:///modules/nrpe/plugins',
+    }
+
+    service { 'nagios-nrpe-server':
+        ensure  => running,
+        require => Package['nagios-nrpe-server'],
+    }
 
     #Collect virtual NRPE nagios service checks
     Monitor_service <| tag == 'nrpe' |>
--- modules/nrpe/spec/classes/service_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe 'nrpe::service', :type => :class do
-    it { should contain_service('nagios-nrpe-server') }
-end
