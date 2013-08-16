05fc2e5eee29e3d88b979ea84ab1b0fca9dd70a4
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-08-16 12:12:04 +0300

    Refactoring nrpe module (round 2/??)
    /usr/local/lib/nagios/plugins directory for plugins shipped by module
    Merge nrpe::packages and nrpe::service classes in nrpe class
    nrpe class now accepts an allowed_hosts argument. It defaults to undef.
    Cleaned-up Service['nagios-nrpe-server']
     hasrestart defaults to true in puppet 2.7
     hasstatus defaults to true in puppet 2.7 so pattern is not needed
     restart not needed (init script works fine)
    Updated rspec examples
    Updated puppet tests
    Removed spec fixtures that caused a problem with puppet doc(cyclic deps)
    Added Rake code to create and purge above fixtures before each run
    Updated nrpe_local.cfg.erb
    nrpe::check now ships virtual resources which are realized by nrpe class
    virtual monitor_service resources tagged nrpe are no longer realized by
    nrpe class (none existed anyway)
    
    Change-Id: I96bcccae226c0b308ec528a52259d4c1bf764afb

--- modules/nrpe/spec/classes/nrpe_spec.rb
+++ modules/nrpe/spec/classes/nrpe_spec.rb
@@ -1,6 +1,36 @@
 require 'spec_helper'
 
 describe 'nrpe', :type => :class do
-    it { should include_class('nrpe::packages') }
-    it { should include_class('nrpe::service') }
+    it { should contain_package('nagios-nrpe-server') }
+    it { should contain_package('nagios-plugins') }
+    it { should contain_package('nagios-plugins-basic') }
+    it { should contain_package('nagios-plugins-extra') }
+    it { should contain_package('nagios-plugins-standard') }
+    it { should contain_file('/etc/nagios/nrpe_local.cfg') }
+    it { should contain_file('/usr/local/lib/nagios/plugins/') }
+    it { should contain_service('nagios-nrpe-server') }
+end
+
+describe 'nrpe', :type => :class do
+    let(:facts) { { :realm => 'production' } }
+
+    it 'should generate valid content for nrpe_local.cfg' do 
+        should contain_file('/etc/nagios/nrpe_local.cfg').with_content(/allowed_hosts=127.0.0.1/)
+    end
+end
+
+describe 'nrpe', :type => :class do
+    let(:facts) { { :realm => 'labs' } }
+    it 'should generate valid content for nrpe_local.cfg' do 
+        should contain_file('/etc/nagios/nrpe_local.cfg').with_content(/allowed_hosts=10.4.0.120/)
+    end
+end
+
+describe 'nrpe', :type => :class do
+    let(:facts) { { :realm => 'labs' } }
+    let(:params) { { :allowed_hosts => '10.10.10.10' } }
+
+    it 'should generate valid content for nrpe_local.cfg' do 
+        should contain_file('/etc/nagios/nrpe_local.cfg').with_content(/allowed_hosts=10.10.10.10/)
+    end
 end
--- modules/nrpe/spec/defines/check_spec.rb
+++ modules/nrpe/spec/defines/check_spec.rb
@@ -4,7 +4,23 @@ describe 'nrpe::check', :type => :define do
     let(:title) { 'something' }
     let(:params) { { :command => '/usr/local/bin/mycommand -i this -o that' } }
 
-    it do
-	should contain_file('/etc/nagios/nrpe.d/something.cfg')
+    context 'with nrpe class not defined' do
+        it 'should not create /etc/nagios/nrpe.d/something.cfg' do
+            should_not contain_file('/etc/nagios/nrpe.d/something.cfg')
+        end
     end
+
+    context 'with nrpe class defined' do
+        let(:pre_condition) { "class { 'nrpe': }" }
+        it 'should create /etc/nagios/nrpe.d/something.cfg' do
+            should contain_file('/etc/nagios/nrpe.d/something.cfg')
+        end
+    end
+
+    context 'with ensure absent' do
+        it 'should not create /etc/nagios/nrpe.d/something.cfg' do
+            should_not contain_file('/etc/nagios/nrpe.d/something.cfg')
+        end
+    end
+
 end
