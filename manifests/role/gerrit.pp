d402195cc539726e8d163776124ff1598a1c0427
Chad Horohoe <chadh@wikimedia.org>
2013-08-19 18:53:39 -0700

    Remove old ircbot and gitweb cruft
    Change-Id: I134fa74027f407971ef5566ac8998ecee7ecd796

--- manifests/role/gerrit.pp
+++ manifests/role/gerrit.pp
@@ -5,7 +5,6 @@ class role::gerrit {
 		system_role { 'role::gerrit::labs': description => 'Gerrit in labs!' }
 
 		class { 'gerrit::instance':
-			ircbot       => false,
 			db_host      => 'gerrit-db',
 			host         => 'gerrit-dev.wmflabs.org',
 			ssh_key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDIb6jbDSyzSD/Pw8PfERVKtNkXgUteOTmZJjHtbOjuoC7Ty6dbvUMX+45GedcD1wAYkWEY26RhI1lW2yEwKvh7VWkKixXqPNyrQGvI+ldjYEyWsGlEHCNqsh37mJD5K3cwr7X/PMaxzxh7rjTk4uRKjtiga9bz1vTDRDaNlXcj84kifsu7xmCY1E+OL4oqqy7b3SKhOpcpZc7n5GonfRSeon5uFHVUjoZ57xQ8x2736zbuLBwMRKtaB+V63cU9ArL90XdVrWfbjI4Fzfex4tBG9fOvt8lINR62cjH5Lova2kZ6VBeUnJYdZ8V1mOSwtITjwkE0K98FNZdqaANZAH7V',
@@ -19,7 +18,6 @@ class role::gerrit {
 		system_role { 'role::gerrit::production': description => 'Gerrit master' }
 
 		class { "gerrit::instance":
-			ircbot        => true,
 			db_host      => 'db1048.eqiad.wmnet',
 			host         => 'gerrit.wikimedia.org',
 			ssh_key      => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAxOlshfr3UaPr8gQ8UVskxHAGG9xb55xDyfqlK7vsAs/p+OXpRB4KZOxHWqI40FpHhW+rFVA0Ugk7vBK13oKCB435TJlHYTJR62qQNb2DVxi5rtvZ7DPnRRlAvdGpRft9JsoWdgsXNqRkkStbkA5cqotvVHDYAgzBnHxWPM8REokQVqil6S/yHkIGtXO5J7F6I1OvYCnG1d1GLT5nDt+ZeyacLpZAhrBlyFD6pCwDUhg4+H4O3HGwtoh5418U4cvzRgYOQQXsU2WW5nBQHE9LXVLoL6UeMYY4yMtaNw207zN6kXcMFKyTuF5qlF5whC7cmM4elhAO2snwIw4C3EyQgw==',
