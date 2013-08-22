d5a882533f5662b2d55ac5dd3f00eb7a86d90278
Tim Landscheidt <tim@tim-landscheidt.de>
2013-08-22 02:05:00 +0000

    WIP: Tools: Add infrastructure for AWStats
    WORK IN PROGRESS, DO NOT COMMIT
    
    This change is part of an effort to simplify and standardize the web
    server log creation and distribution.
    
    Previously, access logs were collected in a distributed manner on all
    webservers and saved on an NFS share.  Instead this change siphons the
    data centrally from the web proxy.
    
    A significant problem for the processing of the log data by AWStats is
    the daily log rotation.  Synchronizing this process between different
    hosts, perhaps even a grid job, is nontrivial.  Instead, we run the
    AWStats update process on the web proxy, hourly per cron and on demand
    from logrotate.  The memory and CPU footprint of the AWStats update
    process is small enough that there is no concern that this could
    impede the proxy's performance so it does not warrant replacing the
    direct execution of the AWStats update process with for example a
    spool system.
    
    To redact the log, we use a sed command instead of logsplitter so that
    we do not have to worry about managing the latter's compilation and
    deployment.
    
    Another challenge is redundancy of data.  While we could save the
    redacted version of the log to disk, it is not necessary from a time
    or load viewpoint compared to dynamic redaction.  Also, as this would
    likely mean that the redacted log would be in the realm of the awstats
    tool but writable by root as the web server process, we would have to
    be very careful to not open up security holes.  Instead we rely on
    dynamic redaction and sudo for privileges.
    
    Change-Id: Ic7c542214c754e0e790308b1fae1aeba1cc2890a

--- modules/toollabs/manifests/webproxy.pp
+++ modules/toollabs/manifests/webproxy.pp
@@ -14,5 +14,45 @@ class toollabs::webproxy inherits toollabs {
   include toollabs::infrastructure
 
   #TODO: apache config
-}
 
+  # AWStats infrastructure.
+  file { "/usr/local/sbin/awstats-filter.sh":
+    ensure => file,
+    mode => "0755",
+    owner => "root",
+    group => "root",
+    source => "puppet:///modules/toollabs/awstats/awstats-filter.sh";
+  }
+
+  file { "/etc/logrotate.d/httpd-prerotate":
+    ensure => directory,
+    mode => "0755",
+    owner => "root",
+    group => "root";
+  }
+
+  file { "/etc/logrotate.d/httpd-prerotate/awstats.sh":
+    ensure => file,
+    mode => "0755",
+    owner => "root",
+    group => "root",
+    require => File["/etc/logrotate.d/httpd-prerotate"],
+    source => "puppet:///modules/toollabs/awstats/logrotate";
+  }
+
+  file { "/etc/sudoers.d/awstats":
+    ensure => file,
+    mode => "0440",
+    owner => "root",
+    group => "root",
+    source => "puppet:///modules/toollabs/awstats/sudoers";
+  }
+
+  cron { "awstats-update":
+    ensure => present,
+    minute => 14,
+    hour => "*",
+    user => local-awstats,
+    command => "/data/project/awstats/refresh-awstats.sh";
+  }
+}
