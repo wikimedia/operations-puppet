7dacb9c1627d5a0b0b567ef3f3a0c411aaeb2f54
Ori Livneh <ori@wikimedia.org>
2013-08-27 14:09:50 -0700

    Add self ('olivneh') to professor.pmtpa.wmnet
    Access to professor will help me debug relaying of NavigationTiming
    data to Graphite. The patch grants me sudo as well. If all the relevant
    configuration and data files are wikidev-readable, I may not need it, but
    having it means being able to inspect things without having to nag on IRC.
    
    Change-Id: I6e04acbf401d4d808d23ca1dfc7b6f5cd24715e5

--- manifests/site.pp
+++ manifests/site.pp
@@ -2235,7 +2235,13 @@ node "professor.pmtpa.wmnet" {
         ganglia,
         ntp::client,
         udpprofile::collector,
-        misc::graphite
+        misc::graphite,
+        groups::wikidev,
+        accounts::olivneh
+
+    sudo_user { 'olivneh':
+        privileges => ['ALL = (ALL) NOPASSWD: ALL'],
+    }
 }
 
 node "project1.wikimedia.org" {
