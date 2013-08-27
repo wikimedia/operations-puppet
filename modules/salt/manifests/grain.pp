0e3557f3c7b49eb97b46585bdf90a06765ad8109
Ryan Lane <rlane@wikimedia.org>
2013-08-27 12:57:42 -0700

    Allow grains to be set with a single value
    Some grains should be set explicitly with a single value, rather
    than a hash/array of values.
    
    Change-Id: I9e24551c26bf659059d29c2b0eee2f7636a0dd92

--- modules/salt/manifests/grain.pp
+++ modules/salt/manifests/grain.pp
@@ -15,26 +15,43 @@
 #   If 'present', adds value to grain. If 'absent', removes it.
 #   Defaults to 'present'.
 #
+# [*replace*]
+#   If true, replaces the value in the grain. If false, it adds the value.
+#   Defaults to true.
+#
 # === Examples
 #
 #  salt::grain { 'deployment_target':
 #    value => 'parsoid',
 #  }
 #
+#  salt::grain { 'cluster':
+#    value => 'eqiad_text_cache',
+#    replace => true,
+#  }
+#
 define salt::grain(
   $value,
   $grain  = $title,
   $ensure = present,
+  $replace = false,
 ) {
-  if $ensure == 'absent' {
-    exec { "/usr/local/sbin/grain-ensure remove ${grain} ${value}":
-      onlyif  => "/usr/local/sbin/grain-ensure contains ${grain} ${value}",
-      require => File['/usr/local/sbin/grain-ensure'],
+  $command = $replace ? {
+    true => 'set',
+    default => 'add',
+  }
+  case $ensure {
+    'absent': {
+        exec { "/usr/local/sbin/grain-ensure remove ${grain} ${value}":
+          onlyif  => "/usr/local/sbin/grain-ensure contains ${grain} ${value}",
+          require => File['/usr/local/sbin/grain-ensure'],
+        }
     }
-  } else {
-    exec { "/usr/local/sbin/grain-ensure add ${grain} ${value}":
-      unless  => "/usr/local/sbin/grain-ensure contains ${grain} ${value}",
-      require => File['/usr/local/sbin/grain-ensure'],
+    'present': {
+        exec { "/usr/local/sbin/grain-ensure ${command} ${grain} ${value}":
+          unless  => "/usr/local/sbin/grain-ensure contains ${grain} ${value}",
+          require => File['/usr/local/sbin/grain-ensure'],
+        }
     }
   }
 }
