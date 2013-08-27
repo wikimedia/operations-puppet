c23b2ab66a2c8ccd801c7414bb63921e75f7be7e
Ryan Lane <rlane@wikimedia.org>
2013-08-27 11:01:50 -0700

    Ensure deployment targets have dependencies
    Deployment requires python-redis and git-core currently. The only
    thing that currently adds a node as a deployment target is the
    deployment::target definition, so the definition should also be
    responsible for adding the dependencies.
    
    Change-Id: I3364e2656cac2add6f13381720fcb09167ac5bcb

--- modules/deployment/manifests/target.pp
+++ modules/deployment/manifests/target.pp
@@ -4,4 +4,14 @@ define deployment::target($ensure=present) {
         grain  => "deployment_target",
         value  => $name;
     }
+    if ! defined(Package["git-core"]){
+      package { "git-core":
+        ensure => present;
+      }
+    }
+    if ! defined(Package["python-redis"]){
+      package { "python-redis":
+        ensure => present;
+      }
+    }
 }
