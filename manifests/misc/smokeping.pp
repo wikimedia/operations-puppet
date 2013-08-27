9d3d1e5a2fc3dc8c5cf453eee503a1195fbbeead
RobH <rob@wikimedia.org>
2013-08-27 23:36:31 +0000

    Revert "include curl in smokeping class"
    This reverts commit 06403f5ee3f12568f2a1d0a6321a441950420bf8.
    
    Change-Id: I387ae6d6fe294a64e427381ada1bfcf8cfde9d33

--- manifests/misc/smokeping.pp
+++ manifests/misc/smokeping.pp
@@ -2,33 +2,28 @@
 
 class misc::smokeping {
 
-    yepsystem_role { "misc::smokeping": description => "Smokeping server" }
+	system_role { "misc::smokeping": description => "Smokeping server" }
 
-    package {
-        "smokeping":
-        ensure => latest;
-    }
+	package {
+		"smokeping":
+		ensure => latest;
+	}
 
-    package {
-        "curl":
-        ensure => latest;
-        }
+	file { "/etc/smokeping/config.d/" :
+		require => Package[smokeping],
+		ensure => directory,
+		recurse => true,
+		owner => "root",
+		group => "root",
+		mode => 0444,
+		source => "puppet:///files/smokeping";
+	}
 
-    file { "/etc/smokeping/config.d/" :
-        require => Package[smokeping],
-        ensure => directory,
-        recurse => true,
-        owner => "root",
-        group => "root",
-        mode => 0444,
-        source => "puppet:///files/smokeping";
-    }
-
-    service { smokeping:
-        require => [ Package[smokeping],
-        File["/etc/smokeping/config.d" ] ],
-        subscribe => File["/etc/smokeping/config.d" ],
-        ensure => running;
-    }
+	service { smokeping:
+		require => [ Package[smokeping],
+		File["/etc/smokeping/config.d" ] ],
+		subscribe => File["/etc/smokeping/config.d" ],
+		ensure => running;
+	}
 
 }
