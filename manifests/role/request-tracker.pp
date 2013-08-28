00fedb97f850866d83bd9281f070f9be67f1ee30
DZ <dzahn@wikimedia.org>
2013-08-27 17:51:32 -0700

    remove old RT 3.8 stuff and just keep RT 4.x Apache stuff
    remove everything that was just used by the old RT 3.8 lighttpd setup
    
    keep everything that is used by the new RT 4.x apache setup
    
    it's files/rt/50-debconf in old, templates/rt/50-debconf.erb in new
    
    role/request-trackers.pp isn't used anymore, neither is the lighttpd template
    
    Change-Id: Iff9eeafa26889d0f2b123c6ddfb2e02e441042f3

--- manifests/role/request-tracker.pp
+++ manifests/role/request-tracker.pp
@@ -1,22 +1,3 @@
-#  This is production RT
-#
-class role::request-tracker::production {
-
-	class { "misc::rt::server":
-		site => "rt.wikimedia.org";
-	}
-}
-
-#  Labs/testing RT
-#
-class role::request-tracker::labs {
-
-	class { "misc::rt::server":
-		site => $fqdn,
-		datadir => "/a/mysql";
-	}
-}
-
 #  Labs/testing RT with Apache
 #
 class role::request-tracker-apache::labs {
