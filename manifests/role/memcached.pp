c9947cf84c41c9ff633c935f86c358b0288b4d94
Antoine Musso <hashar@free.fr>
2013-08-30 11:53:24 +0200

    tweak memcached limit on beta (89GB -> 15GB)
    The memcached instances in the beta cluster have 16GB of memory but are
    still allowed to grow up to 89GB.  I am not sure what will happen once
    the RAM is filled up, I guess it would swap and eventually send the
    instance to death / OOM kill.
    
    This patch creates a new role::memcached::configuration class to let us
    finely tweak the memcached configuration per realm.  I have limited
    the beta instances to roughly 15GB which should prevents them from
    diyng.
    
    bug: 52378
    Change-Id: Ifcef51fd4592c02e2d8a3a01a16930f23541554c

--- manifests/role/memcached.pp
+++ manifests/role/memcached.pp
@@ -1,7 +1,18 @@
+# vim: noet
 
 # Virtual resource for monitoring server
 @monitor_group { "memcached_pmtpa": description => "pmtpa memcached" }
 @monitor_group { "memcached_eqiad": description => "eqiad memcached" }
+
+class role::memcached::configuration {
+
+	# memcached -m parameter, the maximum memory in MegaBytes
+	$memcached_size = {
+		'production' => '89088',
+		'labs'       => '15000',
+	}
+
+}
 
 class role::memcached {
 
@@ -10,10 +21,14 @@ class role::memcached {
 	system_role { "role::memcached": description => "memcached server" }
 
 	include standard,
+		role::memcached::configuration,
 		sysctlfile::high-http-performance
 
+	# Look up configuration coming from role::memcached::configuration
+	$memcached_size = $role::memcached::configuration::memcached_size[$::realm]
+
 	class { "::memcached":
-		memcached_size => '89088',
+		memcached_size => $memcached_size,
 		memcached_port => '11211',
 		version => '1.4.15-0wmf1',
 		memcached_options => {
