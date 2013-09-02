6ed643b3acd2827740587396b3001c06340e0cea
Faidon Liambotis <faidon@wikimedia.org>
2013-09-02 20:14:07 +0300

    authdns: introduce an authdns::lint class
    It sets up gdnsd & dependencies but without the service running. This is
    to be used by contint to lint authdns proposed changesets.
    
    RT: 5688
    Change-Id: Ic204f65c6fd5743249ba90391cf3874d39932693

---
+++ modules/authdns/manifests/lint.pp
@@ -0,0 +1,21 @@
+# == Class authdns::lint
+# A class to lint Wikimedia's authoritative DNS system
+#
+class authdns::lint {
+    include authdns::scripts
+
+    class { '::geoip':
+        data_provider => 'package',
+    }
+
+    package { 'gdnsd':
+        ensure => installed,
+    }
+
+    service { 'gdnsd':
+        ensure     => 'stopped',
+        hasrestart => true,
+        hasstatus  => true,
+        require    => Package['gdnsd'],
+    }
+}
