ac3d1240501a451763adfa29af27151171ea05af
Coren <marc@uberbox.org>
2013-09-03 08:42:52 -0400

    Tool Labs: package python-rsvg
    Bug: 53626
    Change-Id: If43a379274a0228b0345209c4c9f4fb0f9617aa8

--- modules/toollabs/manifests/exec_environ.pp
+++ modules/toollabs/manifests/exec_environ.pp
@@ -109,6 +109,7 @@ class toollabs::exec_environ {
       'python-problem-report',
       'python-redis',
       'python-requests',
+      'python-rsvg',
       'python-scipy',
       'python-sqlalchemy',
       'python-twisted',
