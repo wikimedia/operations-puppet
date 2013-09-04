b212384a0a123739e92e1f75c78ba0abc2a0627e
Antoine Musso <hashar@free.fr>
2013-09-04 17:43:37 +0200

    contint: publish Zuul git over git protocol
    This is similar as ed2fb38 https://gerrit.wikimedia.org/r/#/c/71968/
    which published the Zuul repositories over HTTP using git-http-backend
    for bug 50695.  It turns out the process hangs (bug 53683), so here we
    publish the Zuul repositories over the git protocol.
    
    * git-daemon-sysvinit package provides all the glue around git daemon
      command.
    * GIT_DAEMON_DIRECTORY and GIT_DAEMON_BASE_PATH are both point to Zuul
      git root path. Let us have URL without a /git/ prefix in the path.
    * git-daemon port is firewalled to prevent access from outside.
    
    bug: 50695
    bug: 53683
    Change-Id: I0c50e5dc2899c3f23a875309e1920ee4d51551cf

---
+++ modules/contint/manifests/zuul/git-daemon.pp
@@ -0,0 +1,29 @@
+# Class publishing the Zuul repositories with git-daemon
+class contint::zuul::git-daemon(
+    $zuul_git_dir = '/var/lib/zuul/git'
+) {
+
+  packages { 'git-daemon-sysvinit': ensure => present }
+
+  $git_daemon_directory = $zuul_git_dir
+  # We dont want to honor `git send-pack` commands so make sure the receive-pack
+  # service is always disabled.
+  $git_daemon_options = '--export-all --forbid-override=receive-pack'
+
+  file { '/etc/default/git-daemon':
+    mode    => '0444',
+    owner   => 'root',
+    group   => 'root',
+    content => template('contint/default.git-daemon.erb'),
+    require => Package['git-daemon-sysvinit'],
+  }
+
+  service { 'git-daemon':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    subscribe  => File['/etc/default/git-daemon'],
+    require    => Package['git-daemon-sysvinit'],
+  }
+
+}
