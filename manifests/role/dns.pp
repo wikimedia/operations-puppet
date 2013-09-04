bc92bef76833e6290feb1f7460081ae1466683a3
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-09-04 14:44:45 +0300

    Whitespace cleanup (mostly)
    Making puppet-lint happy.
    * tabs -> spaces
    * => symbols alignment
    * $site => $::site
    * split some oneliners to multiline format
    
    Change-Id: Ic7250cf5d01109787d75cc0000000b2ed82b5c70

--- manifests/role/dns.pp
+++ manifests/role/dns.pp
@@ -1,53 +1,62 @@
 # role/dns.pp
 
 class role::dns::ldap {
-	include ldap::role::config::labs
+    include ldap::role::config::labs
 
-	$ldapconfig = $ldap::role::config::labs::ldapconfig
+    $ldapconfig = $ldap::role::config::labs::ldapconfig
 
-	if $site == "pmtpa" {
-		interface::ip { "role::dns::ldap": interface => "eth0", address => "208.80.152.33" }
+    if $::site == 'pmtpa' {
+        interface::ip { 'role::dns::ldap':
+            interface => 'eth0',
+            address   => '208.80.152.33'
+        }
 
-		# FIXME: turn these settings into a hash that can be included somewhere
-		class { "dns::auth-server::ldap":
-			dns_auth_ipaddress => "208.80.152.33 208.80.152.32 208.80.153.135",
-			dns_auth_query_address => "208.80.152.33",
-			dns_auth_soa_name => "labs-ns0.wikimedia.org",
-			ldap_hosts => $ldapconfig["servernames"],
-			ldap_base_dn => $ldapconfig["basedn"],
-			ldap_user_dn => $ldapconfig["proxyagent"],
-			ldap_user_pass => $ldapconfig["proxypass"],
-		}
-	}
-	if $site == "eqiad" {
-		interface::ip { "role::dns::ldap": interface => "eth0", address => "208.80.154.19" }
+        # FIXME: turn these settings into a hash that can be included somewhere
+        class { 'dns::auth-server::ldap':
+            dns_auth_ipaddress      => '208.80.152.33 208.80.152.32 208.80.153.135',
+            dns_auth_query_address  => '208.80.152.33',
+            dns_auth_soa_name       => 'labs-ns0.wikimedia.org',
+            ldap_hosts              => $ldapconfig['servernames'],
+            ldap_base_dn            => $ldapconfig['basedn'],
+            ldap_user_dn            => $ldapconfig['proxyagent'],
+            ldap_user_pass          => $ldapconfig['proxypass'],
+        }
+    }
+    if $::site == 'eqiad' {
+        interface::ip { 'role::dns::ldap':
+            interface => 'eth0',
+            address   => '208.80.154.19'
+        }
 
-		# FIXME: turn these settings into a hash that can be included somewhere
-		class { "dns::auth-server::ldap":
-			dns_auth_ipaddress => "208.80.154.19 208.80.154.18",
-			dns_auth_query_address => "208.80.154.19",
-			dns_auth_soa_name => "labs-ns1.wikimedia.org",
-			ldap_hosts => $ldapconfig["servernames"],
-			ldap_base_dn => $ldapconfig["basedn"],
-			ldap_user_dn => $ldapconfig["proxyagent"],
-			ldap_user_pass => $ldapconfig["proxypass"],
-		}
-	}
+        # FIXME: turn these settings into a hash that can be included somewhere
+        class { 'dns::auth-server::ldap':
+            dns_auth_ipaddress      => '208.80.154.19 208.80.154.18',
+            dns_auth_query_address  => '208.80.154.19',
+            dns_auth_soa_name       => 'labs-ns1.wikimedia.org',
+            ldap_hosts              => $ldapconfig['servernames'],
+            ldap_base_dn            => $ldapconfig['basedn'],
+            ldap_user_dn            => $ldapconfig['proxyagent'],
+            ldap_user_pass          => $ldapconfig['proxypass'],
+        }
+    }
 }
 
 class role::dns::recursor {
-	system_role { "role::dns::recursor": description => "Recursive DNS server" }
-	
-	include lvs::configuration, network::constants
+    system_role { 'role::dns::recursor': description => 'Recursive DNS server' }
 
-	class {
-		"lvs::realserver":
-			realserver_ips => $lvs::configuration::lvs_service_ips[$::realm]['dns_rec'][$::site];
-		"::dns::recursor":
-			require => Class["lvs::realserver"],
-			listen_addresses => [ $::ipaddress, $::ipaddress6_eth0, $lvs::configuration::lvs_service_ips[$::realm]['dns_rec'][$::site] ],
-			allow_from => $network::constants::all_networks;
-	}
-	
-	::dns::recursor::monitor { [ $::ipaddress, $::ipaddress6_eth0 ]: }
+    include lvs::configuration, network::constants
+
+    class {
+        'lvs::realserver':
+            realserver_ips => $lvs::configuration::lvs_service_ips[$::realm]['dns_rec'][$::site];
+        '::dns::recursor':
+            require             => Class['lvs::realserver'],
+            listen_addresses    => [$::ipaddress,
+                                    $::ipaddress6_eth0,
+                                    $lvs::configuration::lvs_service_ips[$::realm]['dns_rec'][$::site],
+                                  ],
+            allow_from          => $network::constants::all_networks;
+    }
+
+    ::dns::recursor::monitor { [ $::ipaddress, $::ipaddress6_eth0 ]: }
 }
