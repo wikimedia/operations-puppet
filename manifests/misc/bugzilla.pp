29a8dc39cf17f0c0c5ca0746636e98e8ca8f1224
RobH <rob@wikimedia.org>
2013-09-05 11:33:23 -0700

    RT#5011 bugzilla to use own certificate
    Change-Id: I3e163570feecab48809d86b5c8c7ddaa629babbe

--- manifests/misc/bugzilla.pp
+++ manifests/misc/bugzilla.pp
@@ -2,101 +2,103 @@
 
 class misc::bugzilla::server {
 
-	system_role { "misc::bugzilla::server": description => "Bugzilla server" }
+    system_role { 'misc::bugzilla::server': description => 'Bugzilla server' }
 
-	class {'webserver::php5': ssl => true; }
+    class {'webserver::php5': ssl => true; }
+    install_certificate{ 'bugzilla.wikimedia.org': }
 
-	apache_site { bugzilla: name => "bugzilla.wikimedia.org" }
-	file {
-		"/etc/apache2/sites-available/bugzilla.wikimedia.org":
-			source => "puppet:///files/apache/sites/bugzilla.wikimedia.org",
-			mode => 0444,
-			owner => root,
-			group => www-data;
-	}
+
+    apache_site { bugzilla: name => 'bugzilla.wikimedia.org' }
+    file {
+        '/etc/apache2/sites-available/bugzilla.wikimedia.org':
+            source  => 'puppet:///files/apache/sites/bugzilla.wikimedia.org',
+            mode    => 0444,
+            owner   => root,
+            group   => www-data;
+    }
 }
 
 class misc::bugzilla::crons {
-	cron { bugzilla_whine:
-		command => "cd /srv/org/wikimedia/bugzilla/ ; ./whine.pl",
-		user => root,
-		minute => 15
-	}
+    cron { bugzilla_whine:
+        command => 'cd /srv/org/wikimedia/bugzilla/ ; ./whine.pl',
+        user    => root,
+        minute  => 15
+    }
 
-	# 2 cron jobs to generate charts data
-	# See https://bugzilla.wikimedia.org/29203
-	# 1) get statistics for the day:
-	cron { bugzilla_collectstats:
-		command => "cd /srv/org/wikimedia/bugzilla/ ; ./collectstats.pl",
-		user    => root,
-		hour    => 0,
-		minute  => 5,
-		weekday => [ 1, 2, 3, 4, 5, 6 ] # Monday - Saturday
-	}
-	# 2) on sunday, regenerates the whole statistics data
-	cron { bugzilla_collectstats_regenerate:
-		command => "cd /srv/org/wikimedia/bugzilla/ ; ./collectstats.pl --regenerate",
-		user    => root,
-		hour    => 0,
-		minute  => 5,
-		weekday => 0  # Sunday
-	}
+    # 2 cron jobs to generate charts data
+    # See https://bugzilla.wikimedia.org/29203
+    # 1) get statistics for the day:
+    cron { bugzilla_collectstats:
+        command => 'cd /srv/org/wikimedia/bugzilla/ ; ./collectstats.pl',
+        user    => root,
+        hour    => 0,
+        minute  => 5,
+        weekday => [ 1, 2, 3, 4, 5, 6 ] # Monday - Saturday
+    }
+    # 2) on sunday, regenerates the whole statistics data
+    cron { bugzilla_collectstats_regenerate:
+        command => 'cd /srv/org/wikimedia/bugzilla/ ; ./collectstats.pl --regenerate',
+        user    => root,
+        hour    => 0,
+        minute  => 5,
+        weekday => 0  # Sunday
+    }
 }
 
 # RT-3962 - mail bz user stats to community metrics
 class misc::bugzilla::communitymetrics {
 
-	file { bugzilla_communitymetrics_file:
-		path => "/srv/org/wikimedia/bugzilla/bugzilla_community_metrics.sh",
-		owner => root,
-		group => www-data,
-		mode => 0550,
-		source => "puppet:///files/bugzilla/bugzilla_community_metrics.sh",
-		ensure => present,
-	}
+    file { bugzilla_communitymetrics_file:
+        path    => '/srv/org/wikimedia/bugzilla/bugzilla_community_metrics.sh',
+        owner   => root,
+        group   => www-data,
+        mode    => 0550,
+        source  => 'puppet:///files/bugzilla/bugzilla_community_metrics.sh',
+        ensure  => present,
+    }
 
-	cron { bugzilla_communitymetrics_cron:
-		command	=> "cd /srv/org/wikimedia/bugzilla/ ; ./bugzilla_community_metrics.sh",
-		user => www-data,
-		hour => 0,
-		minute => 0,
-		monthday => 1,
-	}
+    cron { bugzilla_communitymetrics_cron:
+        command     => 'cd /srv/org/wikimedia/bugzilla/ ; ./bugzilla_community_metrics.sh',
+        user        => www-data,
+        hour        => 0,
+        minute      => 0,
+        monthday    => 1,
+    }
 }
 
 class misc::bugzilla::report {
 
-	systemuser { bzreporter: name => 'reporter', home => '/home/reporter', groups => [ 'reporter' ] }
+    systemuser { bzreporter: name => 'reporter', home => '/home/reporter', groups => [ 'reporter' ] }
 
-	require passwords::bugzilla
+    require passwords::bugzilla
 
-	file { bugzilla_report:
-		path    => '/home/reporter/bugzilla_report.php',
-		owner   => 'reporter',
-		group   => 'reporter',
-		mode    => '0550',
-		content => template('misc/bugzilla_report.php'),
-		ensure  => present,
-	}
+    file { bugzilla_report:
+        path    => '/home/reporter/bugzilla_report.php',
+        owner   => 'reporter',
+        group   => 'reporter',
+        mode    => '0550',
+        content => template('misc/bugzilla_report.php'),
+        ensure  => present,
+    }
 
 }
 
 # RT-4802 - mail bz audit_log to bugzilla admins
 class misc::bugzilla::auditlog {
 
-	file { bugzilla_auditlog_file:
-		path   => '/srv/org/wikimedia/bugzilla/bugzilla_audit_log.sh',
-		owner  => 'root',
-		group  => 'www-data',
-		mode   => '0550',
-		source => 'puppet:///files/bugzilla/bugzilla_audit_log.sh',
-		ensure => present,
-	}
+    file { bugzilla_auditlog_file:
+        path   => '/srv/org/wikimedia/bugzilla/bugzilla_audit_log.sh',
+        owner  => 'root',
+        group  => 'www-data',
+        mode   => '0550',
+        source => 'puppet:///files/bugzilla/bugzilla_audit_log.sh',
+        ensure => present,
+    }
 
-	cron { bugzilla_auditlog_cron:
-		command => 'cd /srv/org/wikimedia/bugzilla/ ; ./bugzilla_audit_log.sh',
-		user    => 'www-data',
-		hour    => 0,
-		minute  => 0,
-	}
+    cron { bugzilla_auditlog_cron:
+        command => 'cd /srv/org/wikimedia/bugzilla/ ; ./bugzilla_audit_log.sh',
+        user    => 'www-data',
+        hour    => 0,
+        minute  => 0,
+    }
 }
