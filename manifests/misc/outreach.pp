56d5bd55182b48d216c89a514a55b753682ac2ee
DZ <dzahn@wikimedia.org>
2013-09-05 16:15:30 -0700

    tabs and lint stuff in outreach.pp
    Change-Id: Ifa2315c028a2759c4ea6112f83630fb68f9e0282

--- manifests/misc/outreach.pp
+++ manifests/misc/outreach.pp
@@ -1,18 +1,18 @@
 # https://contacts.wikimedia.org | http://en.wikipedia.org/wiki/CiviCRM
 class misc::outreach::civicrm {
 
-	system_role { "misc::outreach::civicrm": description => "contacts.wikimedia.org - Drupal/CiviCRM service" }
+    system_role { 'misc::outreach::civicrm': description => 'contacts.wikimedia.org - Drupal/CiviCRM' }
 
-	file {
-		"/etc/apache2/sites-available/contacts.wikimedia.org":
-			ensure => present,
-			mode   => '0444',
-			owner  => root,
-			group  => root,
-			source => 'puppet:///files/apache/sites/contacts.wikimedia.org';
-	}
+    file {
+        '/etc/apache2/sites-available/contacts.wikimedia.org':
+            ensure => present,
+            mode   => '0444',
+            owner  => 'root',
+            group  => 'root',
+            source => 'puppet:///files/apache/sites/contacts.wikimedia.org';
+    }
 
-	apache_site { "contacts": name => "contacts.wikimedia.org" }
+    apache_site { 'contacts': name => 'contacts.wikimedia.org' }
 
 }
 
