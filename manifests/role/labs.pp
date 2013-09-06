04c5cec54cc6b6fb26c5fd1b87f6306229df0628
YuviPanda <yuvipanda@gmail.com>
2013-09-07 01:17:43 +0530

    Rename labsproxy module to dynamicproxy
    Because that is what it does. Also same module should be
    customized for both general labsproxy as well as a toollabs
    specific proxy
    
    Change-Id: If4944db7518b0d6a10aece82fb1b8811b5832069

--- manifests/role/labs.pp
+++ manifests/role/labs.pp
@@ -51,7 +51,7 @@ class role::labs::tools {
 
   class proxy inherits role::labs::tools::config {
       system_role { "role::labs::tools::proxy": description => "Tool labs generic web proxy" }
-      class { '::labsproxy': }
+      class { '::dynamicproxy': }
       class { 'toollabs::infrastructure': }
   }
 
