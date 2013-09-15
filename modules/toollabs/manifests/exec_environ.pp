286dc36aff19547456c0187972df30c924ef712e
drtrigon <dr.trigon@surfeu.ch>
2013-09-15 21:33:13 +0200

    Tool Labs: package fabric
    Bug: 54135
    Change-Id: I391cea6b4ed3fe581a491f524c69f311a85fd520

--- modules/toollabs/manifests/exec_environ.pp
+++ modules/toollabs/manifests/exec_environ.pp
@@ -168,7 +168,8 @@ class toollabs::exec_environ {
       'mdbtools',                    # Bug #48805.
       'p7zip',
       'phpunit',
-      'tree'                         # Bug #48862.
+      'tree',                        # Bug #48862.
+      'fabric'                       # Bug #54135.
       ]:
     ensure => present
   }
