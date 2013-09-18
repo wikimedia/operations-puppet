wikimedia/operations-puppet

commit 4d105737116a927344ccd501d34d903060668f96

contint: publish Zuul git over git protocol
This is similar as ed2fb38 https://gerrit.wikimedia.org/r/#/c/71968/
which published the Zuul repositories over HTTP using git-http-backend
for bug 50695.  It turns out the process hangs (bug 53683), so here we
publish the Zuul repositories over the git protocol.

* git-daemon-sysvinit package provides all the glue around git daemon
  command.
* GIT_DAEMON_DIRECTORY and GIT_DAEMON_BASE_PATH are both pointing to
  Zuul git root path. Let us have URL without a /git/ prefix in the
  path.
* git-daemon port is firewalled to prevent access from outside.
* added git_daemon to iptables port and protocols tables.

bug: 50695
bug: 53683
Change-Id: I0c50e5dc2899c3f23a875309e1920ee4d51551cf

modules/contint/manifests/zuul/git-daemon.pp
@@ -1,0 +1,33 @@
+# Class publishing the Zuul repositories with git-daemon
+class contint::zuul::git-daemon(
+    $zuul_git_dir = '/var/lib/zuul/git'
+) {
+
+  packages { 'git-daemon-sysvinit': ensure => present }
+
+  # Point both git daemon paths to the same dir, this way we do not have a
+  # /git/ prefix in the git:// URLs.
+  $git_daemon_directory = $zuul_git_dir
+  $git_daemon_base_path = $zuul_git_dir
+
+  # We dont want to honor `git send-pack` commands so make sure the receive-pack
+  # service is always disabled.
+  $git_daemon_options = '--export-all --forbid-override=receive-pack'
+
+  file { '/etc/default/git-daemon':
+    mode    => '0444',
+    owner   => 'root',
+    group   => 'root',
+    content => template('contint/default.git-daemon.erb'),
+    require => Package['git-daemon-sysvinit'],
+  }
+
+  service { 'git-daemon':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    subscribe  => File['/etc/default/git-daemon'],
+    require    => Package['git-daemon-sysvinit'],
+  }
+
+}

manifests/iptables.pp
@@ -4,6 +4,7 @@ $iptables_ports = {
 	beam2 => "5672",
 	beam3 => "56918",
 	epmd => "4369",
+	git_daemon => "9418",
 	glance_api => "9292",
 	glance_registry => "9191",
 	gmond_tcp => "8649",
@@ -54,6 +55,7 @@ $iptables_protocols = {
 	beam2 => "tcp",
 	beam3 => "tcp",
 	epmd => "tcp",
+	git_daemon => "tcp",
 	glance_api => "tcp",
 	glance_registry => "tcp",
 	gmond_tcp => "tcp",

manifests/role/zuul.pp
@@ -47,6 +47,10 @@ class role::zuul::labs {
 #
 # https://www.mediawiki.org/wiki/Continuous_integration/Zuul
 #
+# The Zuul git repositories are published over the git:// protocol by using git
+# daemon. That allows remote Jenkins slaves to fetch the references crafted by
+# Zuul when a change is submitted.
+#
 class role::zuul::production {
     system_role { 'role::zuul::production': description => 'Zuul on production' }
 
@@ -73,6 +77,10 @@ class role::zuul::production {
         git_dir          => $role::zuul::configuration::zuul_git_dir,
         push_change_refs => false,
         statsd_host      => '',
+    }
+
+    class { 'contint::zuul::git-daemon':
+      zuul_git_dir => $role::zuul::configuration::zuul_git_dir,
     }
 
 } # /role::zuul::production

modules/contint/manifests/firewall.pp
@@ -9,6 +9,7 @@ class contint::firewall {
 
     iptables_purge_service{  'deny_all_http-alt': service => 'http-alt' }
     iptables_purge_service{  'deny_all_zuul-daemon': service => 'zuul_webservice' }
+    iptables_purge_service{  'deny_all_git-daemon': service  => 'git_daemon' }
   }
 
   class iptables-accepts {
@@ -28,6 +29,8 @@ class contint::firewall {
     iptables_add_service{ 'deny_all_http-alt': service => 'http-alt', jump => 'DROP' }
     # Deny direct access to the Zuul daemon
     iptables_add_service{ 'deny_all_zuul-daemon': service => 'zuul_webservice', jump => 'DROP' }
+    # Deny git daemon listening on port 9418
+    iptables_add_service{ 'deny_all_git-daemon': service => 'git_daemon', jump => 'DROP' }
   }
 
   class iptables {

modules/contint/templates/default.git-daemon.erb
@@ -1,0 +1,17 @@
+### THIS FILE IS MANAGED BY PUPPET
+
+# Defaults for git-daemon initscript
+# sourced by /etc/init.d/git-daemon
+# installed at /etc/default/git-daemon by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+GIT_DAEMON_ENABLE=true
+GIT_DAEMON_USER=gitdaemon
+GIT_DAEMON_DIRECTORY=<%= git_daemon_directory %>
+GIT_DAEMON_BASE_PATH=<%= git_daemon_base_path %>
+
+# Additional options that are passed to the Daemon.
+GIT_DAEMON_OPTIONS="<%= git_daemon_options %>"

commit 65bf21ea54fec07505fbaaeedc65b53ab7be48eb

contint: typo packages {} -> package {}
I am often using package {} with its plural form, no idea how it could
be detected but that typo is hard to catch :( Introduced by 4d10573.

Change-Id: I2fb297d22fe9b2f8b9a184b5d6bea613ae67e0b7

modules/contint/manifests/zuul/git-daemon.pp
@@ -3,7 +3,7 @@ class contint::zuul::git-daemon(
     $zuul_git_dir = '/var/lib/zuul/git'
 ) {
 
-  packages { 'git-daemon-sysvinit': ensure => present }
+  package { 'git-daemon-sysvinit': ensure => present }
 
   # Point both git daemon paths to the same dir, this way we do not have a
   # /git/ prefix in the git:// URLs.