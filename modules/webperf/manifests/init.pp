52127f7b4b4c2e5e2202a2f80295825424208493
Ori Livneh <ori@wikimedia.org>
2013-09-23 13:50:40 -0700

    Add 'webperf' module; provision asset-check service
    This patch adds a 'webperf' module which I plan to grow into a set of scripts
    and configs for keeping tabs on front-end performance. A follow-up patch will
    migrate the Navigation Timing Puppet code to this module. Although Puppet is
    not a Deployment System™, the scripts involved are usually quite small and
    benefit from living in close proximity to the Puppet code that provisions them.
    Aggregating these scripts in a proper repository or package is something I'd
    like to do in the future, but right now I don't have a firm sense of what this
    will grow into, so I don't want to do that yet.
    
    The webperf module included in this patch comes with a single tool that I wrote
    called 'asset-check'. It connects to a site using PhantomJS (a headless WebKit
    platform) and keeps track of the number of resources that had to be fetched in
    the course of rendering the page and their byte sizes. PhantomJS is required
    because many of the asset requests on Wikimedia sites are constructed by
    JavaScript code, and so merely cURLing the page won't do -- one has to actually
    parse and execute its JavaScript payload in order to assess its total size.
    
    A wrapper script implemented in Python invokes PhantomJS with asset-check.js
    for a set of URLs representing the main pages of popular Wikimedia wikis. The
    script repeats this every five minutes, reporting the measurements to Ganglia
    by shelling out to Gmetric.
    
    The payload size of wiki main pages don't change minute-to-minute, so it's a
    bit silly to poll so often (i.e., every five minutes). At the same time,
    Ganglia tends to choke on longer intervals, and I'd really like to have this
    data in Ganglia. I don't think the hourly or daily views of this data will be
    interesting, but the weekly / monthly / yearly views will be.
    
    Because the scripts are small and simple, the configuration is hard-coded in
    the script source. This isn't especially nice, but I'll clean it up as I
    refactor this module into a proper suite of tools.
    
    Change-Id: I8844e1941a5a5ceb207ebd92ad11f1e81618584d

---
+++ modules/webperf/manifests/init.pp
@@ -0,0 +1,47 @@
+# == Class: webperf
+#
+# This Puppet module provisions a set of client-side performance
+# monitoring scripts for Wikimedia sites.
+#
+class webperf {
+    package { 'phantomjs':
+        ensure => present,
+    }
+
+    group { 'webperf':
+        ensure => present,
+    }
+
+    user { 'webperf':
+        ensure => present,
+        gid    => 'webperf',
+        shell  => '/bin/false',
+        home   => '/nonexistent',
+        system => true,
+    }
+
+    file { '/srv/webperf':
+        ensure => directory,
+    }
+
+    file { '/srv/webperf/asset-check.js':
+        source => 'puppet:///modules/webperf/asset-check.js',
+        require => Package['phantomjs'],
+    }
+
+    file { '/srv/webperf/asset-check.py':
+        source => 'puppet:///modules/webperf/asset-check.py',
+        require => [ File['/srv/webperf/asset-check.js'], Package['ganglia-monitor'] ],
+    }
+
+    file { '/etc/init/asset-check.conf':
+        source  => 'puppet:///modules/webperf/asset-check.conf',
+        require => [ File['/srv/webperf/asset-check.py'], User['webperf'] ],
+    }
+
+    service { 'asset-check':
+        ensure   => running,
+        provider => 'upstart',
+        require  => File['/etc/init/asset-check.conf'],
+    }
+}
