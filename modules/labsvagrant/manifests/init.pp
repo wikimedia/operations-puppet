598715a71b0fa2f84474c7af957a47f3eef800c9
YuviPanda <yuvipanda@gmail.com>
2013-09-24 07:18:32 +0530

    Add labsvagrant module
    This lets people setup labs instances that use the puppet
    files from Mediawiki Vagrant. This helps in provisoning
    complex / simple mediawiki setups very trivially
    
    Thanks to bd808 for the runner script
    
    Change-Id: I1664d881a730af89efbff933b1144c63695e28d4

---
+++ modules/labsvagrant/manifests/init.pp
@@ -0,0 +1,27 @@
+class labsvagrant {
+    user { 'vagrant':
+        ensure => 'present'
+    }
+
+    file { '/v/':
+        ensure => 'directory',
+        owner => 'vagrant',
+        group => 'www-data',
+        require => User['vagrant']
+    }
+
+    git::clone { '/v/vagrant':
+        directory => '/vagrant/',
+        origin => 'https://gerrit.wikimedia.org/r/mediawiki/vagrant',
+        owner => 'vagrant',
+        group => 'www-data',
+        require => File['/v/']
+    }
+
+    file { '/bin/vagrant-puppet-runner':
+        ensure => 'present',
+        source => 'puppet:///modules/labsvagrant/vagrant-puppet-runner.bash',
+        mode => 'a=rx,o=rwx',
+        owner => 'root'
+    }
+}
