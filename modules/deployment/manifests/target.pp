898c49a2b74b63852b97efde7ddd6c75290f1222
Ryan Lane <rlane@wikimedia.org>
2013-09-25 15:30:01 -0400

    Pull dependencies and deploy all when targeted
    When a deployment target grain is added to a deployment target
    have the minion also fetch its modules, returners and pillars,
    then deploy all configured repositories.
    
    Change-Id: I52afb079dd01d3b84ac237babc6b15974e0db478

--- modules/deployment/manifests/target.pp
+++ modules/deployment/manifests/target.pp
@@ -1,17 +1,38 @@
 define deployment::target($ensure=present) {
-    salt::grain { "deployment_target_$name":
+    salt::grain { "deployment_target_${name}":
         ensure => $ensure,
         grain  => "deployment_target",
-        value  => $name;
+        value  => $name,
+        notify => [
+            Exec["deployment_target_sync_all"],
+            Exec["deployment_target_refresh_pillars"],
+            Exec["deployment_target_deploy_all"],
+        ];
     }
-    if ! defined(Package["git-core"]){
-      package { "git-core":
-        ensure => present;
-      }
+    if ! defined(Exec["deployment_target_sync_all"]){
+        exec { "deployment_target_sync_all":
+            refreshonly => true,
+            path        => ["/usr/bin"],
+            command     => "salt-call saltutil.sync_all";
+        }
     }
-    if ! defined(Package["python-redis"]){
-      package { "python-redis":
-        ensure => present;
-      }
+    if ! defined(Exec["deployment_target_refresh_pillars"]){
+        exec { "deployment_target_refresh_pillars":
+            refreshonly => true,
+            path        => ["/usr/bin"],
+            command     => "salt-call saltutil.refresh_pillar";
+        }
     }
+    if ! defined(Exec["deployment_target_deploy_all"]){
+        exec { "deployment_target_deploy_all":
+            refreshonly => true,
+            path        => ["/usr/bin"],
+            command     => "salt-call deploy.sync_all",
+            require     => [
+                Exec["deployment_target_sync_all"],
+                Exec["deployment_target_refresh_pillars"],
+            ];
+        }
+    }
+    include deployment::packages
 }
