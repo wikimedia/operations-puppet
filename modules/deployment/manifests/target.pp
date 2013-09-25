68cf276273d3ac70491aee5c53e26ba7e7fe4fb5
Ryan Lane <rlane@wikimedia.org>
2013-09-25 15:30:01 -0400

    Pull modules/returners/pillars when targeted
    When a deployment target grain is added to a deployment target
    have the minion also fetch its modules, returners and pillars.
    
    Change-Id: I52afb079dd01d3b84ac237babc6b15974e0db478

--- modules/deployment/manifests/target.pp
+++ modules/deployment/manifests/target.pp
@@ -1,17 +1,26 @@
 define deployment::target($ensure=present) {
-    salt::grain { "deployment_target_$name":
+    salt::grain { "deployment_target_${name}":
         ensure => $ensure,
         grain  => "deployment_target",
-        value  => $name;
+        value  => $name,
+        notify => [
+            Exec["deployment_target_sync_all"],
+            Exec["deployment_target_refresh_pillars"],
+        ];
     }
-    if ! defined(Package["git-core"]){
-      package { "git-core":
-        ensure => present;
-      }
+    if ! defined(Exec["deployment_target_sync_all"]){
+        exec { "deployment_target_sync_all":
+            refreshonly => true,
+            path        => ["/usr/bin"],
+            command     => "salt-call saltutil.sync_all";
+        }
     }
-    if ! defined(Package["python-redis"]){
-      package { "python-redis":
-        ensure => present;
-      }
+    if ! defined(Exec["deployment_target_refresh_pillars"]){
+        exec { "deployment_target_refresh_pillars":
+            refreshonly => true,
+            path        => ["/usr/bin"],
+            command     => "salt-call saltutil.refresh_pillar";
+        }
     }
+    include deployment::packages
 }
