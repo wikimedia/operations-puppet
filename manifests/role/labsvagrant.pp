aa8f36364b7fdb7b9fb444e85e9582c201d96adc
YuviPanda <yuvipanda@gmail.com>
2013-09-27 20:27:15 +0530

    Rename labsvagrant to labs_vagrant
    Since apparently labsvagrant sounds like a Subway Station in
    northern Oslo, while labs-vagrant does not
    
    Change-Id: I488fa31ee0707aef91669c7f601a752429607c80

--- manifests/role/labsvagrant.pp
+++ manifests/role/labsvagrant.pp
@@ -1,6 +1,6 @@
 # Install Mediawiki-Vagrant puppet repo, manage manually from CLI
 
-class role::labsvagrant {
-    class { '::labsvagrant':
+class role::labs::vagrant {
+    class { '::labs_vagrant':
     }
 }
