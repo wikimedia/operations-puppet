900a95447d8908fc875ef4cc8c94be4d61aaab85
Antoine Musso <hashar@free.fr>
2013-09-30 15:21:38 +0200

    contint::localvhost easily setup an apache listener
    This generalized the QUnit vhost listening on localhost.  To run browser
    tests, I would need as well a vhost listening on localhost and pointing
    to some document root.
    
    I simply converted the existing Apache conf for QUnit to be a template,
    the class contint::localvhost let us fill the placeholders.
    
    The QUnit vhost has been migrated to use the new class.
    
    Change-Id: I3e17c2ab82fbbab5be654f48ca55cca98070ba40

---
+++ modules/contint/manifests/localvhost.pp
@@ -0,0 +1,34 @@
+# == Class contint::localvhost
+#
+# Craft an apache configuration file to listen on localhost:$port (default to
+# port 9412) and point that vhost to $docroot (default to
+# /srv/localhost${name}).
+#
+# The $name is by default used as a prefix for the Apache logs. You can
+# override it using $log_prefix.
+#
+# == Example:
+#
+# contint::localvhost { 'qunit:' }
+#
+# Creates a vhost listening on 127.0.0.1:9412 having a DocumentRoot at
+# /srv/localhost/qunit.
+#
+class contint::localvhost(
+  $docroot = "/srv/localhost/${name}",
+  $port = 9412,
+  $log_prefix = $name,
+){
+
+  file { "/etc/apache2/sites-available/${name}.localhost":
+    mode    => '0444',
+    owner   => 'root',
+    group   => 'root',
+    content => template('contint/apache/localvhost.erb'),
+  }
+
+  apache_site { "${name} localhost":
+    name => "${name}.localhost"
+  }
+
+}
