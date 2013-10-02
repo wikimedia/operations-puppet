4c103885d91d8524d0db94b70855a2dab4aef6a0
Matanya Moses <matanya.moses@gmail.com>
2013-10-02 21:52:44 +0300

    Miredo: Convert into a module.
    Thanks for all comments.
    
    Change-Id: I5c0eac593a143b00e72a47d443cfe9d64c627af0

---
+++ modules/miredo/manifests/init.pp
@@ -0,0 +1,23 @@
+class miredo {
+
+    package { 'miredo':
+        ensure    => installed,
+	}
+
+	file { '/etc/miredo.conf':
+		owner     => root,
+		group     => root,
+		mode      => '0444',
+		require   => Package['miredo'],
+		source    => 'puppet:///modules/miredo/files/miredo.conf',
+	}
+
+	service { 'miredo':
+		ensure    => running,
+		enable    => true,
+		hasstatus => false,
+		require   => Package['miredo'],
+        subscribe => File['/etc/miredo.conf'],
+	}
+
+}
