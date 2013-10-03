e85ee33dd3b787161c6165004550b1a24c392d9b
Matanya Moses <matanya.moses@gmail.com>
2013-10-03 15:43:42 +0300

    Drac: Convert into a module.
    Change-Id: Ifb1ccc6e8615ea5d6a92658a9808f468545d0d44

---
+++ modules/drac/manifests/init.pp
@@ -0,0 +1,16 @@
+class drac {
+
+    package {  'python-paramiko' :
+        ensure      => latest,
+    }
+
+    file {
+        '/usr/local/sbin/drac':
+            owner   => root,
+            group   => root,
+            mode    => '0755',
+            require => Package['python-paramiko'],
+            source  => 'puppet:///modules/drac/files/drac/drac.py',
+    }
+
+}
