68da49cf0538419735fab3a76fb2542e4f77c382
Sean Pringle <springle@wikimedia.org>
2013-10-07 12:44:55 +1000

    db1022 to mariadb
    Change-Id: Ia01b4a78460f7bd96a70a5f4224587d7d8e6ce96

--- manifests/site.pp
+++ manifests/site.pp
@@ -645,7 +645,7 @@ node /^db10(05|21|26|58)\.eqiad\.wmnet/ {
 }
 
 node /^db10(06|22|27|39|40)\.eqiad\.wmnet/ {
-    if $hostname =~ /^db10(39)/ {
+    if $hostname =~ /^db10(22|39)/ {
         class { role::coredb::s6 : innodb_file_per_table => true, mariadb => true }
     } elsif $hostname == "db1027" {
         class { role::coredb::s6 : mariadb => true }
