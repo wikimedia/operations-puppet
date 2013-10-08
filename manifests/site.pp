e29cce8ed5cb08a0a77c368d956251520b95e2fd
cmjohnson <cmjohnson@wikimedia.org>
2013-10-08 13:22:54 -0400

    setting up amssq48-62 as text varnish
    Change-Id: I7afb7a44bbba4eb045f027b44d9928924c5dbdf0

--- manifests/site.pp
+++ manifests/site.pp
@@ -115,8 +115,8 @@ node /amssq(3[1-9]|4[0-6])\.esams\.wikimedia\.org/ {
     include role::cache::text
 }
 
-# amssq47 is a text varnish
-node /^amssq47\.esams\.wikimedia\.org$/ {
+# amssq47-62 are text varnish
+node /amssq4[7-9]|5[0-9]|6[0-2]\.esams\.wikimedia\.org$/ {
     include role::cache::text, role::cache::ssl::unified
 
     interface::add_ip6_mapped { "main": }
