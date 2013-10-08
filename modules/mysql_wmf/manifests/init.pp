8ba3380462ac3891471a6b1405b250c132ab2b61
Andrew Bogott <abogott@wikimedia.org>
2013-10-08 22:35:52 +0000

    Move mysql_wmf into a module.
    The manifest changes here are pretty brainless -- I split the major
    classes into separate files and rearranged the file and template
    includes to reflect the new position of the source files.
    
    I also moved quite a few files that were filed under ganglia
    or icinga but only used by these classes -- now they live in the
    mysql_wmf module instead.
    
    Change-Id: I31b8d8562f8eda4a6fde4a6110d83a339fc38b94

---
+++ modules/mysql_wmf/manifests/init.pp
@@ -0,0 +1,162 @@
+# mysql.pp
+
+# These classes contain a bunch of stuff that's specific to
+# the wmf production DB systems.  If you want to construct
+# a general-purpose DB server or client, best look elsewhere.
+
+class mysql_wmf {
+    monitor_service { "mysql disk space": description => "MySQL disk space", check_command => "nrpe_check_disk_6_3", critical => true }
+
+    #######################################################################
+    ### MASTERS - make sure to update here whenever changing replication
+    #######################################################################
+    if $hostname =~ /^blondel/ {
+        $master = true
+        $writable = true
+    } else {
+        $master = false
+    }
+
+    #######################################################################
+    ### Cluster Definitions - update if changing / building new dbs
+    #######################################################################
+    if $hostname =~ /^blondel|bellin$/ {
+        $db_cluster = "m1"
+    }
+    else {
+        $db_cluster = undef
+    }
+
+    if ($db_cluster) {
+        file { "/etc/db.cluster":
+            content => "${db_cluster}";
+        }
+        # this is for the pt-heartbeat daemon, which needs super privs
+        # to write to read_only=1 databases.
+        if ($db_cluster !~ /fund/) {
+            include passwords::misc::scripts
+            file {
+                "/root/.my.cnf":
+                    owner => root,
+                    group => root,
+                    mode => 0400,
+                    content => template("mysql_wmf/root.my.cnf.erb");
+                "/etc/init.d/pt-heartbeat":
+                    owner => root,
+                    group => root,
+                    mode => 0555,
+                    source => "puppet:///modules/mysql_wmf/pt-heartbeat.init";
+            }
+            service { pt-heartbeat:
+                require => [ File["/etc/init.d/pt-heartbeat"], Package[percona-toolkit] ],
+                subscribe => File["/etc/init.d/pt-heartbeat"],
+                ensure => running,
+                hasstatus => false;
+            }
+            include mysql_wmf::monitor::percona
+            if ($db_cluster =~ /^m1/) {
+                include mysql_wmf::slow_digest
+            }
+        }
+    }
+
+    file { "/usr/local/bin/master_id.py":
+        owner => root,
+        group => root,
+        mode => 0555,
+        source => "puppet:///modules/mysql_wmf/master_id.py"
+    }
+
+    include mysql_wmf::ganglia,
+        mysql_wmf::monitor::percona::files
+
+}
+
+class mysql_wmf::mysqluser {
+    user {
+        "mysql": ensure => "present",
+    }
+}
+
+class mysql_wmf::datadirs {
+    file {
+        "/a/sqldata":
+            owner => mysql,
+            group => mysql,
+            mode => 0755,
+            ensure => directory,
+            require => User["mysql"];
+        "/a/tmp":
+            owner => mysql,
+            group => mysql,
+            mode => 0755,
+            ensure => directory,
+            require => User["mysql"];
+    }
+}
+
+class mysql_wmf::pc::conf inherits mysql_wmf {
+    file { "/etc/my.cnf":
+        content => template("mysql_wmf/paresercache.my.cnf.erb")
+    }
+    file { "/etc/mysql/my.cnf":
+        source => "puppet:///modules/mysql_wmf/empty-my.cnf"
+    }
+}
+
+class mysql_wmf::mysqlpath {
+    file { "/etc/profile.d/mysqlpath.sh":
+        owner => root,
+        group => root,
+        mode => 0444,
+        source => "puppet:///modules/mysql_wmf/mysqlpath.sh"
+    }
+}
+
+# TODO do we want to have a class for PHP clients (php5-mysql) as well
+# and rename this to mysql::client-cli?
+class mysql_wmf::client {
+    if versioncmp($::lsbdistrelease, "12.04") >= 0 {
+        package { "mysql-client-5.5":
+            ensure => latest;
+        }
+    } else {
+        package { "mysql-client-5.1":
+            ensure => latest;
+        }
+    }
+}
+
+class mysql_wmf::slow_digest {
+    include passwords::mysql::querydigest
+    $mysql_user = "ops"
+    $digest_host = "db9.pmtpa.wmnet"
+    $digest_db = "query_digests"
+
+    file {
+        "/usr/local/bin/send_query_digest.sh":
+            owner => root,
+            group => root,
+            mode => 0500,
+            content => template("mysql_wmf/send_query_digest.sh.erb");
+    }
+
+    if $mysql_wmf::db_cluster {
+        cron { slow_digest:
+            command => "/usr/local/bin/send_query_digest.sh >/dev/null 2>&1",
+            require => File["/usr/local/bin/send_query_digest.sh"],
+            user => root,
+            minute => '*/20',
+            hour => '*',
+            ensure => present;
+        }
+        cron { tcp_query_digest:
+            command => "/usr/local/bin/send_query_digest.sh tcpdump >/dev/null 2>&1",
+            require => File["/usr/local/bin/send_query_digest.sh"],
+            user => root,
+            minute => [5, 25, 45],
+            hour => '*',
+            ensure => present;
+        }
+    }
+}
