5c316e064482a93e619f00d46db286423a2c5a43
Andrew Bogott <abogott@wikimedia.org>
2013-10-08 22:35:52 +0000

    Move mysql_wmf into a module.
    The manifest changes here are pretty brainless -- I split the major
    classes into separate files and rearranged the file and template
    includes to reflect the new position of the source files.
    
    I also moved quite a few files that were filed under ganglia
    or icinga but only used by these classes -- now they live in the
    mysql_wmf module instead.
    
    Change-Id: I31b8d8562f8eda4a6fde4a6110d83a339fc38b94

---
+++ modules/mysql_wmf/manifests/ganglia.pp
@@ -0,0 +1,49 @@
+class mysql_wmf::ganglia {
+    include passwords::ganglia
+    $ganglia_mysql_pass = $passwords::ganglia::ganglia_mysql_pass
+
+    if $::mariadb {
+        $innodb_version = '55xdb'
+    }
+
+    # Ganglia
+    package { 'python-mysqldb':
+        ensure => present;
+    }
+
+    # FIXME: this belongs in ganglia.pp, not here.
+    if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '8.04') == 0 {
+        file {
+            '/etc/ganglia':
+                ensure => directory,
+                owner  => root,
+                group  => root,
+                mode   => '0755';
+            '/etc/ganglia/conf.d':
+                ensure => directory,
+                owner  => root,
+                group  => root,
+                mode   => '0755';
+            '/usr/lib/ganglia/python_modules':
+                ensure => directory,
+                owner  => root,
+                group  => root,
+                mode   => '0755';
+        }
+    }
+
+    file {
+        '/usr/lib/ganglia/python_modules/DBUtil.py':
+            require => File['/usr/lib/ganglia/python_modules'],
+            source  => 'puppet:///modules/mysql_wmf/ganglia/plugins/DBUtil.py',
+            notify  => Service['gmond'];
+        '/usr/lib/ganglia/python_modules/mysql.py':
+            require => File['/usr/lib/ganglia/python_modules'],
+            source  => 'puppet:///modules/mysql_wmf/ganglia/plugins/mysql.py',
+            notify  => Service['gmond'];
+        '/etc/ganglia/conf.d/mysql.pyconf':
+            require => File['/usr/lib/ganglia/python_modules'],
+            content => template('mysql_wmf/mysql.pyconf.erb'),
+            notify  => Service['gmond'];
+    }
+}
