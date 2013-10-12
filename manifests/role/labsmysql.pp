61beff19798672b4c369a02570f85f15b778e62f
Andrew Bogott <abogott@wikimedia.org>
2013-10-12 15:09:50 +0000

    Change role::labs-mysql-server to use the mysql module.
    Change-Id: I80e6da64b0c31a60ed68178682f55caac02b3ad7

--- manifests/role/labsmysql.pp
+++ manifests/role/labsmysql.pp
@@ -1,23 +1,21 @@
 #include "mysql.db"
 
 class role::labs-mysql-server {
-	class { "generic::mysql::server":
-		# Move mysql data to a place where there's actually space.
-		datadir => $::mysql_datadir ? {
-			undef => "/mnt/mysql",
-			default => $::mysql_datadir,
-		},
-		innodb_file_per_table => $::mysql_file_per_table ? {
-			undef => "1",
-			default => $::mysql_file_per_table,
-		},
-		version => $::lsbdistrelease ? {
-			"12.04" => "5.5",
-			default => false,
-		},
-		bind_address => $mysql_server_bind_address ? {
-			undef => "127.0.0.1",
-			default => $mysql_server_bind_address
-		},
-	}
+
+    $datadir = $::mysql_datadir ? {
+        undef   => '/mnt/mysql',
+        default => $::mysql_datadir,
+    }
+    $bind_address = $::mysql_server_bind_address ? {
+        undef   => '127.0.0.1',
+        default => $::mysql_server_bind_address
+    }
+
+    class { 'mysql::server':
+        config_hash => {
+            # Move mysql data to a place where there's actually space.
+            'datadir' => $role::labs-mysql-server::datadir,
+            'bind_address' => $role::labs-mysql-server::bind_address,
+        }
+    }
 }
