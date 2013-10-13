c36314329b75e43ff16edc4230302b9d4b61342c
Andrew Bogott <abogott@wikimedia.org>
2013-10-13 16:45:50 +0000

    Change the labs rt role to use the mysql module.
    (transitional test-patch)
    
    Change-Id: Ic7ac381c7fa848ef4670961b86384be60b81003f

--- manifests/role/labsmysql.pp
+++ manifests/role/labsmysql.pp
@@ -1,6 +1,6 @@
 #include "mysql.db"
 
-class role::labs-mysql-server {
+class role::labs-mysql-server-new {
 
     $datadir = $::mysql_datadir ? {
         undef   => '/mnt/mysql',
@@ -18,4 +18,26 @@ class role::labs-mysql-server {
             'bind_address' => $role::labs-mysql-server::bind_address,
         }
     }
+}
+
+class role::labs-mysql-server {
+	class { "generic::mysql::server":
+		# Move mysql data to a place where there's actually space.
+		datadir => $::mysql_datadir ? {
+			undef => "/mnt/mysql",
+			default => $::mysql_datadir,
+		},
+		innodb_file_per_table => $::mysql_file_per_table ? {
+			undef => "1",
+			default => $::mysql_file_per_table,
+		},
+		version => $::lsbdistrelease ? {
+			"12.04" => "5.5",
+			default => false,
+		},
+		bind_address => $mysql_server_bind_address ? {
+			undef => "127.0.0.1",
+			default => $mysql_server_bind_address
+		},
+	}
 }
