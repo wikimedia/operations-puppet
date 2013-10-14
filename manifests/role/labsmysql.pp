de7ebcf4c258be073cd9fb45d8176b6b08d4bba9
Andrew Bogott <abogott@wikimedia.org>
2013-10-14 23:02:41 +0000

    Dumb variable rename; attempting to mollify puppet lint.
    Change-Id: I13d9dcfc6b28cd0483d34e65c60fa89c60d19055

--- manifests/role/labsmysql.pp
+++ manifests/role/labsmysql.pp
@@ -2,11 +2,11 @@
 
 class role::labs-mysql-server-new {
 
-    $datadir = $::mysql_datadir ? {
+    $role::labs-mysql-server::datadir = $::mysql_datadir ? {
         undef   => '/mnt/mysql',
         default => $::mysql_datadir,
     }
-    $bind_address = $::mysql_server_bind_address ? {
+    $role::labs-mysql-server::bind_address = $::mysql_server_bind_address ? {
         undef   => '127.0.0.1',
         default => $::mysql_server_bind_address
     }
