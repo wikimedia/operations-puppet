eba3834154fbba5e706b13bce8a4d412d6bbe824
Ori Livneh <ori@wikimedia.org>
2013-10-18 01:23:41 -0700

    coredb_mysql: convert a few additional leftover tabs to spaces
    Follows I419f48c97. I missed a few spots.
    
    Change-Id: I7183fa78ee2f1eb41cc69e987d7a853e35583732

--- modules/coredb_mysql/manifests/slow_digest.pp
+++ modules/coredb_mysql/manifests/slow_digest.pp
@@ -1,19 +1,19 @@
 # class for sending sending slow query digest logs
 class coredb_mysql::slow_digest {
-	include passwords::mysql::querydigest
+    include passwords::mysql::querydigest
 
     $mysql_user = 'ops'
     $digest_host = 'db1001.eqiad.wmnet'
     $digest_db = 'query_digests'
 
-	file { '/usr/local/bin/send_query_digest.sh':
+    file { '/usr/local/bin/send_query_digest.sh':
         owner   => 'root',
         group   => 'root',
         mode    => '0500',
         content => template('coredb_mysql/send_query_digest.sh.erb'),
-	}
+    }
 
-	cron { 'slow_digest':
+    cron { 'slow_digest':
         ensure  => present,
         command => '/usr/local/bin/send_query_digest.sh &>/dev/null',
         require => File['/usr/local/bin/send_query_digest.sh'],
@@ -22,7 +22,7 @@ class coredb_mysql::slow_digest {
         hour    => '*',
     }
 
-	cron { 'tcp_query_digest':
+    cron { 'tcp_query_digest':
         ensure  => present,
         command => '/usr/local/bin/send_query_digest.sh tcpdump &>/dev/null',
         require => File['/usr/local/bin/send_query_digest.sh'],
