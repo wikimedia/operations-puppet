5c63d05af5a43b65aa83a7498d47674a5bb0e1c7
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-10-22 11:32:00 +0300

    Modularizing puppetmaster
    Create modules:
    	puppetmaster
    	puppet
    Moved respective classes in the 2 modules
    Added tests and made minor changes to make tests pass
    Make sure $gitdir/operations/{puppet,private} exist before git cloning
    into them
    Amend the changeset for system_role => system::role change in fa42658
    Retab and linted the files
    Removed unused dashboard class
    
    Change-Id: I1f09d6169ca4289272409b8d4a66efe6dde077c4

---
+++ modules/puppetmaster/tests/puppetmaster.pp
@@ -0,0 +1,6 @@
+# TODO: At some point this should not be needed
+import "../../../manifests/nagios.pp"
+import "../../../manifests/backups.pp"
+import "../../../manifests/role/backup.pp"
+
+include puppetmaster
