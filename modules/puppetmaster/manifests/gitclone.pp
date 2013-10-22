2b394cf5dcdd1c2cd525a9bb81b0d5372b28f011
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-10-22 11:32:00 +0300

    Modularizing puppetmaster
    Create modules:
    	puppetmaster
    	puppet
    Moved respective classes in the 2 modules
    Added tests and made minor changes to make tests pass
    Make sure $gitdir/operations/{puppet,private} exist before git cloning
    into them
    Amend the changeset for system_role => system::role change in fa42658
    Retab and linted the files
    Removed unused dashboard class
    
    Change-Id: I1f09d6169ca4289272409b8d4a66efe6dde077c4

---
+++ modules/puppetmaster/manifests/gitclone.pp
@@ -0,0 +1,125 @@
+# Class: puppetmaster::gitclone
+#
+# This class handles the repositories from which the puppetmasters pull
+class puppetmaster::gitclone {
+    file {
+        $puppetmaster::gitdir:
+            ensure  => directory,
+            owner   => 'root',
+            group   => 'root';
+        "${puppetmaster::gitdir}/operations":
+            ensure  => directory,
+            owner   => 'root',
+            group   => 'root';
+        "${puppetmaster::gitdir}/operations/puppet":
+            ensure  => directory,
+            owner   => 'gitpuppet',
+            group   => 'root',
+            require => File["${puppetmaster::gitdir}/operations"];
+        "${puppetmaster::gitdir}/operations/software":
+            ensure  => directory,
+            owner   => 'gitpuppet',
+            group   => 'root',
+            require => File["${puppetmaster::gitdir}/operations"];
+        "${puppetmaster::gitdir}/operations/puppet/.git/hooks/post-merge":
+            require => Git::Clone['operations/puppet'],
+            owner   => 'gitpuppet',
+            source  => 'puppet:///modules/puppetmaster/git/post-merge',
+            mode    => '0550';
+        "${puppetmaster::gitdir}/operations/puppet/.git/hooks/pre-commit":
+            require => Git::Clone['operations/puppet'],
+            owner   => 'gitpuppet',
+            source  => 'puppet:///modules/puppetmaster/git/pre-commit',
+            mode    => '0550';
+        "${puppetmaster::gitdir}/operations/puppet/.git/hooks/pre-merge":
+            require => Git::Clone['operations/puppet'],
+            owner   => 'gitpuppet',
+            source  => 'puppet:///modules/puppetmaster/git/pre-merge',
+            mode    => '0550';
+        "${puppetmaster::gitdir}/operations/puppet/.git/hooks/pre-rebase":
+            require => Git::Clone['operations/puppet'],
+            owner   => 'gitpuppet',
+            source  => 'puppet:///modules/puppetmaster/git/pre-rebase',
+            mode    => '0550';
+        "${puppetmaster::gitdir}/operations/software/.git/hooks/pre-commit":
+            require => Git::Clone['operations/software'],
+            source  => 'puppet:///modules/puppetmaster/git/pre-commit',
+            mode    => '0550';
+        $puppetmaster::volatiledir:
+            ensure  => directory,
+            mode    => '0750',
+            owner   => 'root',
+            group   => 'puppet';
+        "$puppetmaster::volatiledir/misc":
+            ensure  => directory,
+            mode    => '0750',
+            owner   => 'root',
+            group   => 'puppet';
+    }
+
+    if ! $::is_labs_puppet_master {
+        # Set up private repo.
+        # Note that puppet does not actually clone the repo -- puppetizing that
+        # turns out to be a big, insecure mess.  On a new puppetmaster you will
+        # will need to do a clone of
+        #       ${puppetmaster::gitdir}/operations/puppet/private
+        # by hand and with a forwarded key.
+        file {
+            "${puppetmaster::gitdir}/operations/private":
+                ensure  => directory,
+                owner   => 'gitpuppet',
+                mode    => '0750';
+            "${puppetmaster::gitdir}/operations/private/.git/hooks/post-merge":
+                source  => 'puppet:///modules/puppetmaster/git/private/post-merge',
+                owner   => 'gitpuppet',
+                mode    => '0550';
+            '/etc/puppet/private':
+                ensure => link,
+                target => "${puppetmaster::gitdir}/operations/private",
+                force  => true;
+        }
+    } else {
+        file { '/etc/puppet/private':
+            ensure => link,
+            target => "${puppetmaster::gitdir}/operations/labs/private",
+            force  => true,
+        }
+    }
+
+    git::clone {
+        'operations/puppet':
+            require     => File["${puppetmaster::gitdir}/operations/puppet"],
+            directory   => "${puppetmaster::gitdir}/operations/puppet",
+            owner       => 'gitpuppet',
+            branch      => 'production',
+            origin      => 'https://gerrit.wikimedia.org/r/p/operations/puppet';
+        'operations/software':
+            require     => File["${puppetmaster::gitdir}/operations/software"],
+            owner       => 'gitpuppet',
+            directory   => "${puppetmaster::gitdir}/operations/software",
+            origin      => 'https://gerrit.wikimedia.org/r/p/operations/software';
+    }
+
+    # These symlinks will allow us to use /etc/puppet for the puppetmaster to
+    # run out of.
+    file { '/etc/puppet/templates':
+        ensure => link,
+        target => "${puppetmaster::gitdir}/operations/puppet/templates",
+        force  => true,
+    }
+    file { '/etc/puppet/files':
+        ensure => link,
+        target => "${puppetmaster::gitdir}/operations/puppet/files",
+        force  => true,
+    }
+    file { '/etc/puppet/manifests':
+        ensure => link,
+        target => "${puppetmaster::gitdir}/operations/puppet/manifests",
+        force  => true,
+    }
+    file { '/etc/puppet/modules':
+        ensure => link,
+        target => "${puppetmaster::gitdir}/operations/puppet/modules",
+        force  => true,
+    }
+}
---
+++ modules/puppetmaster/manifests/ssl.pp
@@ -0,0 +1,50 @@
+class puppetmaster::ssl(
+            $server_name='puppet',
+            $ca='false') {
+    $ssldir = '/var/lib/puppet/server/ssl'
+
+    # TODO: Hack to make class pass tests
+    if defined(Package['puppetmaster']) {
+        $before = Package['puppetmaster']
+    } else {
+        $before = undef
+    }
+
+    # Move the puppetmaster's SSL files to a separate directory from the client
+    file {
+        [ '/var/lib/puppet/server',
+            $ssldir
+        ]:
+            ensure  => directory,
+            before  => $before,
+            owner   => 'puppet',
+            group   => 'root',
+            mode    => '0771';
+        [
+            '/var/lib/puppet',
+            "${ssldir}/ca",
+            "${ssldir}/certificate_requests",
+            "${ssldir}/certs",
+            "${ssldir}/private",
+            "${ssldir}/private_keys",
+            "${ssldir}/public_keys",
+            "${ssldir}/crl"
+        ]:
+            ensure => directory;
+    }
+
+    if $ca != 'false' {
+        exec { 'generate hostcert':
+            require => File["${ssldir}/certs"],
+            command => "/usr/bin/puppet cert generate ${server_name}",
+            creates => "${ssldir}/certs/${server_name}.pem";
+        }
+    }
+
+    exec { 'setup crl dir':
+        require => File["$ssldir/crl"],
+        path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+        command => "ln -s ${ssldir}/ca/ca_crl.pem ${ssldir}/crl/$(openssl crl -in ${ssldir}/ca/ca_crl.pem -hash -noout).0",
+        onlyif  => "test ! -L ${ssldir}/crl/$(openssl crl -in ${ssldir}/ca/ca_crl.pem -hash -noout).0"
+    }
+}
