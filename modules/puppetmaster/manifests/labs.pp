e178bc61ec96c1de20a89c0fa2877095fe83298d
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-10-22 11:32:00 +0300

    Modularizing puppetmaster
    Create modules:
    	puppetmaster
    	puppet
    Moved respective classes in the 2 modules
    
    Change-Id: I1f09d6169ca4289272409b8d4a66efe6dde077c4

---
+++ modules/puppetmaster/manifests/labs.pp
@@ -0,0 +1,20 @@
+# Class: puppetmaster::labs
+#
+# This class handles the Wikimedia Labs specific bits of a Puppetmaster
+class puppetmaster::labs {
+    package { "libldap-ruby1.8": ensure => latest; }
+    # Use a specific revision for the checkout, to ensure we are using
+    # a known and approved version of this script.
+    file {
+        "/usr/local/sbin/puppetsigner.py":
+            ensure => link,
+            target => "/usr/local/lib/instance-management/puppetsigner.py";
+    }
+
+    cron {
+        "puppet_certificate_signer":
+            command => "/usr/local/sbin/puppetsigner.py --scriptuser > /dev/null 2>&1",
+            require => File["/usr/local/sbin/puppetsigner.py"],
+            user    => root;
+    }
+}
