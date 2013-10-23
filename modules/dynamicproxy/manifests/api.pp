5fe66fc7e0f22e7fb1df7b5b0fd1817391175390
Andrew Bogott <abogott@wikimedia.org>
2013-10-23 20:45:50 +0000

    Add install and upstart for proxy api
    Change-Id: I7f142644bc07bcd56aefcc66c412cae7fa8eb040

---
+++ modules/dynamicproxy/manifests/api.pp
@@ -0,0 +1,10 @@
+class dynamicproxy::api {
+    package { ['python-invisible-unicorn', 'python-flask-sqlalchemy', 'python-flask']:
+        ensure => 'present',
+    }
+
+    upstart_job{ 'dynamic-proxy-api':
+        require => Package['python-invisible-unicorn', 'python-flask-sqlalchemy', 'redis', 'python-flask'],
+        install => true,
+    }
+}
