c061e4d0c436467ba27e26b52216bc2f78ccc2e0
andrewbogott <abogott@wikimedia.org>
2013-10-25 14:19:10 -0500

    Include $hostname in backup filename so we know what we backed up.
    Change-Id: I35a280f88336d59bb4066ba9a960346716771cc7

--- modules/dynamicproxy/files/proxydb-bak.sh
+++ modules/dynamicproxy/files/proxydb-bak.sh
@@ -2,5 +2,5 @@
 
 DATE=$(date '+%Y%m%d')
 
-nice -n 19 sqlite3 /etc/dynamicproxy-api/data.db .dump | nice -n 19 gzip -9 > /data/project/backup/proxy-${DATE}.bak.gz
+nice -n 19 sqlite3 /etc/dynamicproxy-api/data.db .dump | nice -n 19 gzip -9 > /data/project/backup/proxy-${HOSTNAME}-${DATE}.bak.gz
 
