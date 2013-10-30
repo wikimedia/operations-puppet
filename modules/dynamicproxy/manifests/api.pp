6c1b147fcf96608ba9180b2778f7e4ff4cfbebd5
Andrew Bogott <abogott@wikimedia.org>
2013-10-30 15:49:40 +0000

    Various dynamicproxy-api changes:
    - Switch to using uwsgi
    - Create initial db if needed
    
    Change-Id: I797d8f5e96e0dcc504668b1cd8832aa2e5de6e41

--- modules/dynamicproxy/manifests/api.pp
+++ modules/dynamicproxy/manifests/api.pp
@@ -4,14 +4,30 @@ class dynamicproxy::api {
         require => Class['misc::labsdebrepo'],
     }
 
-    package { ['python-invisible-unicorn', 'python-flask-sqlalchemy']:
+    package { ['python-invisible-unicorn', 'python-flask-sqlalchemy', 'uwsgi', 'uwsgi-plugin-python']:
         ensure => 'present',
         require => Package['python-flask'],
     }
 
     generic::upstart_job{ 'dynamicproxy-api':
-        require => Package['python-invisible-unicorn', 'python-flask-sqlalchemy', 'redis', 'python-flask'],
+        require => Package['python-invisible-unicorn', 'python-flask-sqlalchemy', 'redis', 'python-flask', 'uwsgi', 'uwsgi-plugin-python'],
         install => 'true',
         start   => 'true'
+    }
+
+    file { '/etc/dynamicproxy-api':
+        ensure => 'directory',
+        owner => 'www-data',
+        group => 'www-data',
+    }
+
+    # Create initial db file if it doesn't exist, but don't clobber if it does.
+    file { '/etc/dynamicproxy-api/data.db':
+        ensure => 'exists',
+        source => 'puppet:///modules/dynamicproxy/initial-data.db',
+        replace => false,
+        require => File['/etc/dynamicproxy-api'],
+        owner => 'www-data',
+        group => 'www-data',
     }
 }
