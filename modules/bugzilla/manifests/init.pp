10e9bf35f8b0dbd11e18e35cdec4040dccd367cc
DZ <dzahn@wikimedia.org>
2013-11-06 16:19:12 -0800

    bugzilla module
    make bugzilla stuff a module
    
    these things are not new, they are a conversion to module from the existing ./manifests/misc/bugzilla.pp
    
    so i'm not specifically asking for a review of the scripts so much,
    but about the module structure itself.
    
    i purposely did not make it a change that also uses the module right
    away and (re-)moves the existing files. the old classes are called
    misc::bugzilla while this would be just bugzilla::.
    
    i would like to first merge the module, test it on boogz in labs and on
    zirconium in eqiad and when confirmed it works switch from old Tampa
    server kaulen to zirconium, also resolving RT #4783.
    
    after that i'd remove the old misc::bugzilla classes and files and we
    can shutdown kaulen
    
    PS7: unify auditlog and communitymetrics classes into a single
         defined type as suggested by akosiaris
    
    Change-Id: I5381a2c57b0a3483f7ac3e79639b242054ad9e6f

---
+++ modules/bugzilla/manifests/init.pp
@@ -0,0 +1,77 @@
+# setup Bugzilla instance for WMF
+# also see README.md
+
+class bugzilla {
+
+    # system role for motd
+    system::role { 'role::bugzilla': description => 'Bugzilla server' }
+
+    file { [ '/srv/org','/srv/org/wikimedia','/srv/org/wikimedia/bugzilla']:
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755';
+    }
+
+    # basic apache site and certs
+    class {'bugzilla::apache':
+        svc_name        => 'bugzilla.wikimedia.org',
+        attach_svc_name => 'bug-attachment.wikimedia.org',
+        docroot         => '/srv/org/wikimedia/bugzilla/',
+    }
+
+    # community metrics mail
+    bugzilla::logmail {'communitymetrics':
+        script_name  => 'bugzilla_community_metrics.sh',
+        rcpt_address => 'communitymetrics@wikimedia.org',
+        sndr_address => '3962@rt.wikimedia.org',
+        monthday     => '1',
+    }
+
+    # audit log mail for admins
+    bugzilla::logmail {'auditlog':
+        script_name  => 'bugzilla_audit_log.sh',
+        rcpt_address => 'bugzilla-admin@wikimedia.org',
+        sndr_address => '4802@rt.wikimedia.org',
+        monthday     => '*',
+    }
+
+    # bugzilla reporter PHP script
+    class {'bugzilla::reporter':
+        bz_report_user => 'reporter',
+    }
+
+
+    # whining - http://www.bugzilla.org/docs/tip/en/html/whining.html
+    $bz_path = '/srv/org/wikimedia/bugzilla'
+    $whine = 'whine.pl'
+
+    cron { 'bugzilla_whine':
+        command => "${bz_path}/${whine}",
+        user    => 'root',
+        minute  => '15',
+    }
+
+    # 2 cron jobs to generate charts data
+    # See https://bugzilla.wikimedia.org/29203
+    $collectstats = 'collectstats.pl'
+
+    # 1) get statistics for the day:
+    cron { 'bugzilla_collectstats':
+        command => "${bz_path}/${collectstats}",
+        user    => 'root',
+        hour    => '0',
+        minute  => '5',
+        weekday => [ 1, 2, 3, 4, 5, 6 ] # Monday - Saturday
+    }
+
+    # 2) on sunday, regenerates the whole statistics data
+    cron { 'bugzilla_collectstats_regenerate':
+        command => "${bz_path}/${collectstats} --regenerate",
+        user    => root,
+        hour    => 0,
+        minute  => 5,
+        weekday => 0  # Sunday
+    }
+
+}
