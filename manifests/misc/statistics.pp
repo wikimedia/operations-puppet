7c04d85164ff48091b151b72baa7dfb3409ddb8a
Christian Aistleitner <christian@quelltextlich.at>
2013-11-10 17:57:37 +0100

    Fix indentation in manifests/misc/statistic.pp
    Change-Id: Ia65df0cdd7723f41e1368e6b31388fb2b281727c

--- manifests/misc/statistics.pp
+++ manifests/misc/statistics.pp
@@ -264,24 +264,24 @@ class misc::statistics::sites::stats {
         source  => "puppet:///private/apache/htpasswd.stats",
     }
 
-  install_certificate{ $site_name: }
+    install_certificate{ $site_name: }
 
-  file {
-    '/etc/apache2/sites-available/stats.wikimedia.org':
-        ensure => present,
-        mode => '0444',
-        owner => root,
-        group => root,
-        content => template('apache/sites/stats.wikimedia.org.erb');
-    '/etc/apache2/ports.conf':
-        ensure  => present,
-        mode    => '0644',
-        owner   => root,
-        group   => root,
-        source  => 'puppet:///files/apache/ports.conf.ssl';
+    file {
+        '/etc/apache2/sites-available/stats.wikimedia.org':
+            ensure => present,
+            mode => '0444',
+            owner => root,
+            group => root,
+            content => template('apache/sites/stats.wikimedia.org.erb');
+        '/etc/apache2/ports.conf':
+            ensure  => present,
+            mode    => '0644',
+            owner   => root,
+            group   => root,
+            source  => 'puppet:///files/apache/ports.conf.ssl';
     }
 
-  apache_site { 'statswikimedia': name => 'stats.wikimedia.org' }
+    apache_site { 'statswikimedia': name => 'stats.wikimedia.org' }
 
 }
 
