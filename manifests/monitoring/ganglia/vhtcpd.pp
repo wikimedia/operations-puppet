e368f1237efbf8fc9f11ce59b0ef4f672ec0d4fe
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-11-11 20:03:21 +0200

    More fixes for file permissions/ownerships
    Turning ulsfo to the new puppetmaster infrastructure revealed a few more
    permissions/ownerships inconsistencies. Fixing them and linting a bit in
    the process
    
    Change-Id: I68c2376d990ff9e8a9635e3106a1bfd15a2806e3

--- manifests/monitoring/ganglia/vhtcpd.pp
+++ manifests/monitoring/ganglia/vhtcpd.pp
@@ -1,14 +1,17 @@
-class varnish::monitoring::ganglia::vhtcpd() {
+class varnish::monitoring::ganglia::vhtcpd {
     file {
-        "/usr/lib/ganglia/python_modules/vhtcpd.py":
-            source => "puppet:///modules/${module_name}/ganglia/ganglia-vhtcpd.py",
-            notify => Service['gmond'];
+        '/usr/lib/ganglia/python_modules/vhtcpd.py':
+            source  => "puppet:///modules/${module_name}/ganglia/ganglia-vhtcpd.py",
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            notify  => Service['gmond'];
 
-        "/etc/ganglia/conf.d/vhtcpd.pyconf":
-            owner => root,
-            group => root,
-            mode => 0444,
+        '/etc/ganglia/conf.d/vhtcpd.pyconf':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
             content => template("${module_name}/ganglia/vhtcpd.pyconf.erb"),
-            notify => Service['gmond'];
+            notify  => Service['gmond'];
     }
 }
