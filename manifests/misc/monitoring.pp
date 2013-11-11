a43611065852d04e0ab3c8a3d87b41730ce7d9e2
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-11-11 20:03:21 +0200

    More fixes for file permissions/ownerships
    Turning ulsfo to the new puppetmaster infrastructure revealed a few more
    permissions/ownerships inconsistencies. Fixing them and linting a bit in
    the process
    
    Change-Id: I68c2376d990ff9e8a9635e3106a1bfd15a2806e3

--- manifests/misc/monitoring.pp
+++ manifests/misc/monitoring.pp
@@ -10,20 +10,35 @@ class misc::monitoring::htcp-loss {
 
     # Ganglia
     file {
-        "/usr/lib/ganglia/python_modules/htcpseqcheck.py":
-            source => "puppet:///files/ganglia/plugins/htcpseqcheck.py";
-        "/usr/lib/ganglia/python_modules/htcpseqcheck_ganglia.py":
-            source => "puppet:///files/ganglia/plugins/htcpseqcheck_ganglia.py";
-        "/usr/lib/ganglia/python_modules/util.py":
-            source => "puppet:///files/ganglia/plugins/util.py";
-        "/usr/lib/ganglia/python_modules/compat.py":
-            source => "puppet:///files/ganglia/plugins/compat.py";
-        "/etc/ganglia/conf.d/htcpseqcheck.pyconf":
+        '/usr/lib/ganglia/python_modules/htcpseqcheck.py':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            source  => 'puppet:///files/ganglia/plugins/htcpseqcheck.py';
+        '/usr/lib/ganglia/python_modules/htcpseqcheck_ganglia.py':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            source  => 'puppet:///files/ganglia/plugins/htcpseqcheck_ganglia.py';
+        '/usr/lib/ganglia/python_modules/util.py':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            source  => 'puppet:///files/ganglia/plugins/util.py';
+        '/usr/lib/ganglia/python_modules/compat.py':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            source  => 'puppet:///files/ganglia/plugins/compat.py';
+        '/etc/ganglia/conf.d/htcpseqcheck.pyconf':
             # Disabled due to excessive memory and CPU usage -- TS
-            notify => Service['gmond'],
-            ensure => absent;
+            #owner   => 'root',
+            #group   => 'root',
+            #mode    => '0444',
+            notify  => Service['gmond'],
+            ensure  => absent;
             #require => File["/etc/ganglia/conf.d"],
-            #source => "puppet:///files/ganglia/plugins/htcpseqcheck.pyconf";
+            #source  => "puppet:///files/ganglia/plugins/htcpseqcheck.pyconf";
     }
 }
 
