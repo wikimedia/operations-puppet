22d6dc121724c9b631bf5afade932460fc7422fb
Nik Everett <neverett@wikimedia.org>
2013-11-15 18:13:41 -0500

    Puppet configuration for new elasticsearch servers
    Refactors role::elasticsearch::(labs|beta|production) into
    role::elassticsearch::config and role::elasticsearch like Kafka.  This
    makes sure there are parameters to the role class
    
    Also sets up minimum_master_nodes, master_eligible, and holds_data.
    
    Also retires testsearch100[1-3].
    
    Change-Id: Ic30396c1f47767b05a8e0cfe51693f845e763715

--- manifests/site.pp
+++ manifests/site.pp
@@ -904,18 +904,6 @@ node "ekrem.wikimedia.org" {
         misc::mediawiki-irc-relay
 }
 
-node /^elastic10(0[1-9]|1[0-2])\.eqiad\.wmnet$/ {
-    include accounts::manybubbles,
-        accounts::demon,
-        groups::wikidev
-
-    sudo_user { [ "manybubbles" ]: privileges => ['ALL = NOPASSWD: ALL'] }
-    sudo_user { [ "demon" ]: privileges => ['ALL = NOPASSWD: ALL'] }
-
-    include standard
-    include role::elasticsearch::production
-}
-
 # base_analytics_logging_node is defined in role/logging.pp
 node "emery.wikimedia.org" inherits "base_analytics_logging_node" {
     include
@@ -2770,7 +2758,19 @@ node /^testsearch100[1-3]\.eqiad\.wmnet/ {
     sudo_user { [ "demon" ]: privileges => ['ALL = NOPASSWD: ALL'] }
 
     include standard
-    include role::elasticsearch::production
+    include elasticsearch::decommission
+}
+
+node /^elastic10(0[1-9]|1[0-2])\.eqiad\.wmnet/ {
+    include accounts::manybubbles,
+        accounts::demon,
+        groups::wikidev
+
+    sudo_user { [ "manybubbles" ]: privileges => ['ALL = NOPASSWD: ALL'] }
+    sudo_user { [ "demon" ]: privileges => ['ALL = NOPASSWD: ALL'] }
+
+    include standard
+    include role::elasticsearch
     class { "lvs::realserver": realserver_ips => [ "10.2.2.30" ] }
 }
 
