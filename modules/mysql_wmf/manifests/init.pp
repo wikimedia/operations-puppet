cb0aec10298b876cee1f159a03e2b2126fd5e750
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-11-15 17:39:16 +0200

    mysql_wmf::pc::conf perms/owner fixups
    Change-Id: Ica89b19876ed87a6c45727e299a0a64069602f15

--- modules/mysql_wmf/manifests/init.pp
+++ modules/mysql_wmf/manifests/init.pp
@@ -99,9 +99,15 @@ class mysql_wmf::datadirs {
 
 class mysql_wmf::pc::conf inherits mysql_wmf {
     file { '/etc/my.cnf':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
         content => template('mysql_wmf/paresercache.my.cnf.erb')
     }
     file { '/etc/mysql/my.cnf':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
         source => 'puppet:///modules/mysql_wmf/empty-my.cnf'
     }
 }
