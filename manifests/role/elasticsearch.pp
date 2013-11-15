76b929f7d20f33fed92941bfbc76005f592fef89
Nik Everett <neverett@wikimedia.org>
2013-11-15 18:13:41 -0500

    Puppet configuration for new elasticsearch servers
    Also sets up minimum_master_nodes, master_eligible, and holds_data.
    
    Change-Id: Ic30396c1f47767b05a8e0cfe51693f845e763715

--- manifests/role/elasticsearch.pp
+++ manifests/role/elasticsearch.pp
@@ -1,18 +1,29 @@
 # = Class: role::elasticsearch::production
 #
+# == Parameters:
+# - $minimum_master_nodes:  how many master nodes must be online for this node
+#       to believe that the Elasticsearch cluster is functioning correctly.
+#       Defaults to 1.  Should be set to number of masster eligible nodes in
+#       cluster / 2 + 1.
+# - $master_eligible:  is this node eligible to be a master node?  Defaults to
+#       true.
+#
 # This class manages an elasticsearch service in a WMF-specific way for
 # production.
 #
-class role::elasticsearch::production {
+class role::elasticsearch::production($minimum_master_nodes,
+                                      $master_eligible) {
     $multicast_group = $::site ? {
         'eqiad' => '224.2.2.5',
         'pmtpa' => '224.2.2.6',
     }
     class { '::elasticsearch':
-        cluster_name    => "production-search-${::site}",
-        heap_memory     => '7G',
-        multicast_group => $multicast_group,
-        plugins_dir     => '/srv/deployment/elasticsearch/plugins',
+        cluster_name         => "production-search-${::site}",
+        heap_memory          => '7G',
+        multicast_group      => $multicast_group,
+        plugins_dir          => '/srv/deployment/elasticsearch/plugins',
+        minimum_master_nodes => $minimum_master_nodes,
+        master_eligible      => $master_eligible,
     }
     deployment::target { 'elasticsearchplugins': }
 
@@ -26,9 +37,10 @@ class role::elasticsearch::production {
 #
 class role::elasticsearch::beta {
     class { '::elasticsearch':
-        cluster_name => 'beta-search',
-        heap_memory  => '4G',
-        plugins_dir  => '/srv/deployment/elasticsearch/plugins',
+        cluster_name         => 'beta-search',
+        heap_memory          => '4G',
+        plugins_dir          => '/srv/deployment/elasticsearch/plugins',
+        minimum_master_nodes => 3, # Has four nodes all of which can be master
     }
     deployment::target { 'elasticsearchplugins': }
 
@@ -43,7 +55,8 @@ class role::elasticsearch::beta {
 #
 class role::elasticsearch::labs {
     if $::elasticsearch_cluster_name == undef {
-        fail("$::elasticsearch_cluster_name must be set to something unique for your cluster")
+        $message = 'must be set to something unique for your cluster'
+        fail("$::elasticsearch_cluster_name $message")
     }
     class { '::elasticsearch':
         cluster_name => $::elasticsearch_cluster_name,
