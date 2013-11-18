b97018c78061b2a4cd86f5254e98b0ad3bb91023
jgreen <jgreen@wikimedia.org>
2013-11-18 17:58:09 -0500

    resurrecting ocg role
    Change-Id: I50301b7e12f92f18580128854351bfdcb498ecd0

---
+++ manifests/role/ocg.pp
@@ -0,0 +1,14 @@
+# role/ocg.pp
+# offline content generator
+
+# Virtual resources for the monitoring server
+@monitor_group { "ocg_eqiad": description => "offline content generator eqiad" }
+
+class role::ocg {
+    system::role { "ocg": description => "offline content generator" }
+
+	package {
+		[ 'nodejs' ]:
+			ensure => latest;
+	}
+}
