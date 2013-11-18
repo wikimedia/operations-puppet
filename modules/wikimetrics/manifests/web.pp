25f03ec0204588055eaf084d2a8c6c2e6e0a2a02
milimetric <dandreescu@wikimedia.org>
2013-11-18 11:34:00 -0500

    [not ready for review] Productionizing Wikimetrics
    Change-Id: Ib19894c1c38f6853e86a05419feb6cd57870c5ee
    Card: analytics 1185

---
+++ modules/wikimetrics/manifests/web.pp
@@ -0,0 +1,32 @@
+# == Class wikimetrics::web inherits wikimetrics
+#
+class wikimetrics::web(
+    $server_name,
+    $flask_secret_key,
+    # Google Auth
+    $google_client_secret,
+    $google_client_id,
+    $google_client_email,
+    $ssl_redirect = true,
+    $server_aliases = [],
+)
+{
+    Class['wikimetrics'] -> Class['wikimetrics::web']
+
+    file { "${wikimetrics::config_directory}/web_config.yaml":
+        content => template('wikimetrics/web_config.yaml')
+    }
+
+    $docroot = "${wikimetrics::path}/wikimetrics"
+    # apache stuff
+    apache::vhost { 'wikimetrics':
+        port          => 80,
+        docroot       => $docroot,
+        servername    => $server_name,
+        serveraliases => $server_aliases,
+        serveradmin   => 'noc@wikimedia.org',
+        # Use our own vhost template instead of apache module's.
+        template      => 'wikimetrics/wikimetrics.vhost.erb',
+        require       => File["${wikimetrics::config_directory}/web_config.yaml"],
+    }
+}
