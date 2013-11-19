2eedaaa755cfbfcac02a87d422c9b1586b86de6e
Antoine Musso <hashar@free.fr>
2013-11-19 17:32:30 +0100

    lint role/parsoid
    No code change, linting only.
    
    * retab to 4 spaces
    * align arrows
    * quote file modes
    * 'ensure' as first parameter
    * double quotes to single quotes
    * one file statement per file
    * explode arrays to have one element per line
    * trailing comma for next array element
    * join statement (file, define, service) and defined name on the same
      line.
    
    Change-Id: Ic1a3bcc2d6b27eef23f2cbd548311855269eeec2

--- manifests/role/parsoid.pp
+++ manifests/role/parsoid.pp
@@ -1,51 +1,60 @@
+# vim: set ts=4 et sw=4:
 
-@monitor_group { "parsoid_eqiad": description => "eqiad parsoid servers" }
-@monitor_group { "parsoid_pmtpa": description => "pmtpa parsoid servers" }
+@monitor_group { 'parsoid_eqiad': description => 'eqiad parsoid servers' }
+@monitor_group { 'parsoid_pmtpa': description => 'pmtpa parsoid servers' }
 
 class role::parsoid::production {
-	system::role { "role::parsoid::production": description => "Parsoid server" }
-	deployment::target { "parsoid": }
 
-	package { [ "nodejs", "npm", "build-essential" ]:
-		ensure => latest
-	}
+    system::role { 'role::parsoid::production': description => 'Parsoid server' }
 
-	file {
-		"/var/lib/parsoid":
-			ensure => directory,
-			owner => parsoid,
-			group => wikidev,
-			mode => 2775;
-		"/var/lib/parsoid/Parsoid":
-			ensure => link,
-			target => "/srv/deployment/parsoid/Parsoid";
-		"/etc/init.d/parsoid":
-			source => "puppet:///files/misc/parsoid.init",
-			owner => root,
-			group => root,
-			mode => 0555;
-		"/usr/bin/parsoid":
-			source => "puppet:///files/misc/parsoid",
-			owner => root,
-			group => root,
-			mode => 0555;
-	}
+    deployment::target { 'parsoid': }
 
-	generic::systemuser {
-		parsoid:
-			name => "parsoid",
-			default_group => "parsoid",
-			home => "/var/lib/parsoid";
-	}
+    package { [
+        'nodejs',
+        'npm',
+        'build-essential',
+        ]: ensure => latest
+    }
 
-	service {
-		"parsoid":
-			hasstatus => true,
-			hasrestart => true,
-			enable => true,
-			ensure => running,
-			require => [File["/etc/init.d/parsoid"]];
-	}
+    file { '/var/lib/parsoid':
+        ensure => directory,
+        owner  => parsoid,
+        group  => wikidev,
+        mode   => '2775',
+    }
 
-	monitor_service { "parsoid": description => "Parsoid", check_command => "check_http_on_port!8000" }
+    file { '/var/lib/parsoid/Parsoid':
+        ensure => link,
+        target => '/srv/deployment/parsoid/Parsoid',
+    }
+
+    file { '/etc/init.d/parsoid':
+        source => 'puppet:///files/misc/parsoid.init',
+        owner  => root,
+        group  => root,
+        mode   => '0555',
+    }
+
+    file { '/usr/bin/parsoid':
+        source => 'puppet:///files/misc/parsoid',
+        owner  => root,
+        group  => root,
+        mode   => '0555',
+    }
+
+    generic::systemuser { 'parsoid':
+        name          => 'parsoid',
+        default_group => 'parsoid',
+        home          => '/var/lib/parsoid';
+    }
+
+    service { 'parsoid':
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        enable     => true,
+        require    => [File['/etc/init.d/parsoid']];
+    }
+
+    monitor_service { 'parsoid': description => 'Parsoid', check_command => 'check_http_on_port!8000' }
 }
