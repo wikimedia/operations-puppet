46781700f1ad5c0ca689138e160bbe79b4ed75f4
DZ <dzahn@wikimedia.org>
2013-11-18 17:26:12 -0800

    retab role/gitblit to spaces
    Change-Id: I46472b93fdcc880f4dc871c461d647d460a5311e

--- manifests/role/gitblit.pp
+++ manifests/role/gitblit.pp
@@ -1,19 +1,19 @@
 # manifests/role/gitblit.pp
 
 class role::gitblit {
-	system::role { "role::gitblit": description => "Gitblit, a git viewer" }
+    system::role { "role::gitblit": description => "Gitblit, a git viewer" }
 
-	include role::gerrit::production::replicationdest
+    include role::gerrit::production::replicationdest
 
-	class { "gitblit::instance":
-		host => "git.wikimedia.org",
-		ssl_cert => "git.wikimedia.org",
-		ssl_cert_key => "git.wikimedia.org"
-	}
+    class { "gitblit::instance":
+        host => "git.wikimedia.org",
+        ssl_cert => "git.wikimedia.org",
+        ssl_cert_key => "git.wikimedia.org"
+    }
 
-	# Firewall GitBlit, it should be accessed from localhost or Varnish
-	include base::firewall
-	ferm::rule { 'gitblit_8080':
-		rule => 'proto tcp dport 8080 { saddr $INTERNAL ACCEPT; DROP; }'
-	}
+    # Firewall GitBlit, it should be accessed from localhost or Varnish
+    include base::firewall
+    ferm::rule { 'gitblit_8080':
+        rule => 'proto tcp dport 8080 { saddr $INTERNAL ACCEPT; DROP; }'
+    }
 }
