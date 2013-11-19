e88801b4983b309aebb60f9d8eba24f7a291e03a
DZ <dzahn@wikimedia.org>
2013-11-19 12:59:53 -0800

    retab, quoting, linting of ishmael.pp
    do this first, next move it to module,
    less changes in a single patch set
    
    then put the definition into its own file
    to also get rid of warning that a
    "definition (is) defined inside a class"
    
    Change-Id: I15ed9f259ec810781e34a22bced40bc90e99bc20

--- manifests/misc/ishmael.pp
+++ manifests/misc/ishmael.pp
@@ -4,33 +4,36 @@
 # cd /srv ; git clone https://github.com/asher/ishmael.git ; cd ishmael ; git clone https://github.com/asher/ishmael.git sample
 #
 class misc::ishmael {
-	system::role { "misc::ishmael": description => "ishmael server" }
+    system::role { 'misc::ishmael': description => 'ishmael server' }
 
-	include passwords::ldap::wmf_cluster
-	$proxypass = $passwords::ldap::wmf_cluster::proxypass
+    include passwords::ldap::wmf_cluster
+    $proxypass = $passwords::ldap::wmf_cluster::proxypass
 
-	file { '/etc/apache2/sites-available/ishmael.wikimedia.org':
-		ensure => present,
-		owner  => 'root',
-		group  => 'root',
-		mode   => '0440',
-		content => template('apache/sites/ishmael.wikimedia.org.erb');
-	}
+    file { '/etc/apache2/sites-available/ishmael.wikimedia.org':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0440',
+        content => template('apache/sites/ishmael.wikimedia.org.erb');
+    }
 
-	apache_site { 'ishmael': name => 'ishmael.wikimedia.org' }
+    apache_site { 'ishmael': name => 'ishmael.wikimedia.org' }
 
-	define ishmael_config( $db_central_host='db1001.eqiad.wmnet', $review_table='%query_review', $history_table='%query_review_history' ) {
-		include passwords::mysql::querydigest
+    define ishmael_config( $db_central_host='db1001.eqiad.wmnet', $review_table='%query_review', $history_table='%query_review_history' ) {
+        include passwords::mysql::querydigest
 
-		file { $title:
-			owner => "root",
-			group => "root",
-			mode => 0444,
-			content => template('ishmael/conf.php.erb');
-		}
-	}
+        file { $title:
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            content => template('ishmael/conf.php.erb');
+        }
+    }
 
-	ishmael_config { '/srv/ishmael/conf.php': }
-	ishmael_config { '/srv/ishmael/sample/conf.php':
-		review_table => '%tcpquery_review', history_table => '%tcpquery_review_history' }
+    ishmael_config { '/srv/ishmael/conf.php': }
+
+    ishmael_config { '/srv/ishmael/sample/conf.php':
+        review_table  => '%tcpquery_review',
+        history_table => '%tcpquery_review_history',
+    }
 }
