9fc5a28870827f894b77cf30f4b1caa0cb0d47f1
addshore <addshorewiki@gmail.com>
2013-11-20 20:00:52 +0100

    Start wikidata puppet module for builder
    This 'wikidata 'module just serves as a container
    
    The builder manifest within the module is to be used
    the the wikidata build script. This first step pulls
    in all dependancies and sets up the environment
    
    This first test should allow us to do some
    initial testing
    
    The second step will be to:
     -Uncomment and expand the cronjob inc the following
      -Take the build and commit it to the Wikidata repo
      -Push the commit to the remote
    
    Change-Id: Ib6bb4c0d0a4a3b33d5d9f140f3305383822afa99

---
+++ modules/wikidata/manifests/builder.pp
@@ -0,0 +1,72 @@
+# == Class: wikidata::builder
+
+class wikidata::builder {
+
+	package { [
+		'nodejs',
+		'npm',
+		'php5',
+		'git',
+		]:
+		ensure => 'present';
+	}
+	package { 'grunt-cli':
+		ensure		=> 'present',
+		provider	=> 'npm',
+		require		=> Package['nodejs'],
+	}
+
+	file { '/home/wdbuilder':
+		ensure	=> 'directory',
+		owner	=> 'wdbuilder',
+		group	=> 'wdbuilder',
+		mode	=> '0700',
+		require	=> [ User['wdbuilder'], Group['wdbuilder'] ],
+	}
+
+	group { 'wdbuilder':
+		ensure	=> present,
+	}
+
+	user { 'wdbuilder':
+		ensure		=> 'present',
+		home		=> '/home/wdbuilder',
+		name		=> 'wdbuilder',
+		shell		=> '/bin/bash',
+		managehome	=> true,
+	}
+
+	git::clone { 'clone_wikidatabuilder':
+		directory	=> '/home/wdbuilder/buildscript',
+		origin		=> 'https://github.com/wmde/WikidataBuilder.git',
+		ensure		=> 'latest',
+		owner		=> 'wdbuilder',
+		group		=> 'wdbuilder',
+		before  => Exec['npm_install']
+	}
+
+	git::clone { 'clone_wikidata':
+		directory	=> '/home/wdbuilder/wikidata',
+		# TODO use a different repo once deploying!
+		origin		=> 'https://github.com/addshore/WikidataBuild.git',
+		ensure		=> 'latest',
+		owner		=> 'wdbuilder',
+		group		=> 'wdbuilder',
+	}
+
+	exec { 'npm_install':
+		user	=> 'wdbuilder',
+		command	=> 'cd /home/wdbuilder/buildscript && npm install',
+	}
+
+# TODO uncomment when ready
+#	cron { 'builder_cron':
+#		ensure	=> present,
+#		# TODO commit the build to another repo
+#		command	=> 'cd /home/wdbuilder/buildscript && grunt build:Wikidata_master',
+#		user	=> 'wdbuilder',
+#		hour	=> '*/1',
+#		minute	=> [ 0, 30 ],
+#	}
+
+}
