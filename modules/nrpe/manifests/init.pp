79037e4edd45d045d26762c713756e065da3f237
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-11-20 19:03:42 +0200

    Remove redundant DROP rules.
    Now that DROP is the default policy in ferm, no need for extra DROP
    rules
    
    Change-Id: I7fccf98db4d5c1491a85c2f867460387da669a71

--- modules/nrpe/manifests/init.pp
+++ modules/nrpe/manifests/init.pp
@@ -73,7 +73,7 @@ class nrpe($allowed_hosts=undef) {
 
     # firewall nrpe-server, only accept nrpe/5666 from internal
     ferm::rule { 'nrpe_5666':
-        rule => 'proto tcp dport 5666 { saddr $INTERNAL ACCEPT; DROP; }'
+        rule => 'proto tcp dport 5666 { saddr $INTERNAL ACCEPT; }'
     }
 
     #Collect virtual nrpe checks
