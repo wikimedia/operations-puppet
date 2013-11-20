dc165778b7c12c6fb07694f13099f07ff90051c5
addshore <addshorewiki@gmail.com>
2013-11-20 20:00:52 +0100

    Start wikidata puppet module for builder
    This 'wikidata 'module just serves as a container
    
    The builder manifest within the module is to be used
    the the wikidata build script. This first step pulls
    in all dependancies and sets up the environment
    
    This first test should allow us to do some
    initial testing
    
    The second step will be to:
     -Uncomment and expand the cronjob inc the following
      -Take the build and commit it to the Wikidata repo
      -Push the commit to the remote
    
    Change-Id: Ib6bb4c0d0a4a3b33d5d9f140f3305383822afa99

---
+++ modules/wikidata/manifests/builder.pp
@@ -0,0 +1,96 @@
+# == Class: wikidata::builder
+
+class wikidata::builder {
+
+    package { [
+        'nodejs',
+        'npm',
+        'php5',
+        'php5-cli',
+        'git'
+    ]:
+        ensure => 'present',
+    }
+
+    exec { 'grunt-cli_install':
+        user => 'root',
+        command => '/usr/bin/npm install -g grunt-cli',
+        require => Package['nodejs', 'npm'],
+    }
+
+    group { 'wdbuilder':
+        ensure => present,
+    }
+
+    user { 'wdbuilder':
+        ensure => 'present',
+        home => '/home/wdbuilder',
+        shell => '/bin/bash',
+        managehome => true,
+    }
+
+    git::clone { 'wikidatabuilder':
+        ensure => 'latest',
+        directory => '/home/wdbuilder/buildscript',
+        origin => 'https://github.com/wmde/WikidataBuilder.git',
+        owner => 'wdbuilder',
+        group => 'wdbuilder',
+        require => File['/home/wdbuilder'],
+    }
+
+    git::clone { 'wikidata':
+        ensure => 'latest',
+        directory => '/home/wdbuilder/wikidata',
+        # TODO use a different repo once deploying!
+        # origin => 'ssh://USER@gerrit.wikimedia.org:29418/mediawiki/extensions/Wikidata.git',
+        origin => 'https://github.com/addshore/WikidataBuild.git',
+        owner => 'wdbuilder',
+        group => 'wdbuilder',
+        require => File['/home/wdbuilder'],
+    }
+
+    git::userconfig{ 'gitconf for jenkins user':
+        homedir => '/home/wdbuilder',
+        settings => {
+            'user' => {
+                'name' => 'WikidataBuilder',
+                'email' => 'wikidata@wikimedia.de',
+            },
+        },
+        require => File['/home/wdbuilder'],
+    }
+
+    exec { 'npm_install':
+        user => 'root',
+        cwd => '/home/wdbuilder/buildscript',
+        command => '/usr/bin/npm install',
+        require => [
+            Package['npm'],
+            Git::Clone['wikidatabuilder']
+        ],
+    }
+
+    file { '/home/wdbuilder/builder_cron.sh':
+        ensure => file,
+        mode => '0755',
+        owner => 'wdbuilder',
+        group => 'wdbuilder',
+        source => 'puppet:///modules/wikidata/builder_cron.sh',
+        require => [
+            Exec['npm_install'],
+            Git::Clone['wikidata']
+        ],
+    }
+
+# TODO uncomment when ready
+# cron { 'builder_cron':
+#     ensure => present,
+#     # TODO commit the build to another repo
+#     command => '/home/wdbuilder/builder_cron.sh',
+#     user => 'wdbuilder',
+#     hour => '*/1',
+#     minute => [ 0, 30 ],
+#     require => [ File['/home/wdbuilder/builder_cron.sh'] ],
+# }
+
+}
