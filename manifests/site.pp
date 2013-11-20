3e7907bfaed3478cc1c84e4fd48a792a02e092c9
DZ <dzahn@wikimedia.org>
2013-11-19 16:22:32 -0800

    role and module structure for ishmael
    - add a role and use it in site.pp
    - move to module structure
    - separate config definition from the class
    
    Change-Id: Ia0905a8e949ea78e72508a9e33a65772dfb6ae54

--- manifests/site.pp
+++ manifests/site.pp
@@ -2087,7 +2087,7 @@ node "neon.wikimedia.org" {
     $ircecho_server = "chat.freenode.net"
     include standard,
         icinga::monitor,
-        misc::ishmael,
+        role::ishmael,
         misc::ircecho,
         tcpircbot,
         passwords::logmsgbot
--- modules/ishmael/manifests/init.pp
+++ modules/ishmael/manifests/init.pp
@@ -3,8 +3,7 @@
 # NOTE: this does not install ishmael.. it could be git deployed, but it hasn't been moved to a wmf repo. for now:
 # cd /srv ; git clone https://github.com/asher/ishmael.git ; cd ishmael ; git clone https://github.com/asher/ishmael.git sample
 #
-class misc::ishmael {
-    system::role { 'misc::ishmael': description => 'ishmael server' }
+class ishmael {
 
     include passwords::ldap::wmf_cluster
     $proxypass = $passwords::ldap::wmf_cluster::proxypass
@@ -19,20 +18,9 @@ class misc::ishmael {
 
     apache_site { 'ishmael': name => 'ishmael.wikimedia.org' }
 
-    define ishmael_config( $db_central_host='db1001.eqiad.wmnet', $review_table='%query_review', $history_table='%query_review_history' ) {
-        include passwords::mysql::querydigest
+    ishmael::config { '/srv/ishmael/conf.php': }
 
-        file { $title:
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            content => template('ishmael/conf.php.erb');
-        }
-    }
-
-    ishmael_config { '/srv/ishmael/conf.php': }
-
-    ishmael_config { '/srv/ishmael/sample/conf.php':
+    ishmael::config { '/srv/ishmael/sample/conf.php':
         review_table  => '%tcpquery_review',
         history_table => '%tcpquery_review_history',
     }
