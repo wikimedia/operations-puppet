cc8acb61f179359f9f931980e88e9c07c48732bf
Matanya Moses <matanya.moses@gmail.com>
2013-11-21 15:19:26 +0200

    haproxy: get rid of misc, add a role and template
    the haproxy module was not managing the config file, moved it into a template
    and added a role for it.
    
    Change-Id: Ib55fabd3ef7c5f4e54bd8dc4bf9fe3d7048a1e10

--- modules/haproxy/manifests/init.pp
+++ modules/haproxy/manifests/init.pp
@@ -1,17 +1,23 @@
-# == Class haproxy
+# Class haproxy
 # Installs haproxy and ensures that it is running.
-# Note: This class does not currently manage haproxy.cfg.
-#
-class haproxy
-{
-	package { "haproxy":
-		ensure => present,
-	}
+class haproxy($endpoint_hostname, $endpoint_ip) {
+    package { 'haproxy':
+        ensure => present,
+    }
 
-	service { "haproxy":
-		ensure     => running,
-		enable     => true,
-		hasstatus  => true,
-		hasrestart => true,
-	}
+    file { '/etc/haproxy/haproxy.cfg':
+        ensure  => present,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => template('haproxy/haproxy.cfg.erb'),
+        notify  => Service['haproxy']
+    }
+
+    service { 'haproxy':
+        ensure     => running,
+        enable     => true,
+        hasstatus  => true,
+        hasrestart => true,
+    }
 }
