b6ebb1dbe8dcc6cd76a0160834eea3377578d12f
Matt Walker <mwalker@khaosdev.com>
2013-11-21 11:48:41 -0800

    Initial Puppet Try for OCG::Collection Role
    -- Depends on operations/ocg-config repo to be created
    -- Needs the ocg folder to be created on the deployment host
    ---- Needs the two repos to be checked out on that host?
    
    Change-Id: Ied8b5ac362319be65606530575d56a96085f7fe1

--- manifests/role/ocg.pp
+++ manifests/role/ocg.pp
@@ -6,11 +6,18 @@
 @monitor_group { "ocg_eqiad": description => "offline content generator eqiad" }
 
 class role::ocg {
-    system::role { "ocg": description => "offline content generator" }
+    system::role { "ocg": description => "offline content generator base" }
+    
+    deployment::target { 'ocg': }
 
     package {
         [ 'nodejs' ]:
             ensure => latest;
+    }
+
+    file { '/etc/ocg':
+        ensure  => link,
+        target  => '/srv/deployment/ocg/config/'
     }
 }
 
@@ -20,5 +27,56 @@ class role::ocg::test {
     package {
         [ 'redis-server' ]:
             ensure => latest;
+    }
+}
+
+class role::ocg::collection {
+    system::role { "ocg-collection": description => "offline concent generator for Collection extension" }
+    
+    deployment::target { 'ocg-collection': }
+    
+    package { [
+        'texlive-xetex',
+        'imagemagick',
+        ]: ensure => latest;
+    }
+    
+    file { '/var/lib/ocg':
+        ensure => directory,
+        owner  => ocg,
+        group  => wikidev,
+        mode   => '2775',
+    }
+    
+    file { '/var/lib/ocg/collection':
+        ensure => link,
+        target => '/srv/deployment/ocg/collection',
+    }
+
+    file { '/etc/init/ocg-collection':
+        ensure => present,
+        owner  => root,
+        group  => root,
+        mode   => '0555',
+        source => 'puppet:///files/misc/ocg-collection.conf',
+    }
+    
+    generic::systemuser { 'ocg':
+        name          => 'ocg',
+        default_group => 'ocg',
+        home          => '/var/lib/ocg',
+    }
+
+    service { 'ocg-collection':
+        ensure     => running,
+        hasstatus  => false,
+        hasrestart => false,
+        enable     => true,
+        require    => File['/etc/init/ocg-collection.conf'],
+    }
+
+    monitor_service { 'ocg-collection':
+        description   => 'Offline Content Generation - Collection',
+        check_command => 'check_http_on_port!17080',
     }
 }
