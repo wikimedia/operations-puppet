0b7e8ca811e1cbc40aa04013eb0927eb68cf8d7a
Matanya Moses <matanya.moses@gmail.com>
2013-11-27 15:19:29 +0200

    varnish : lint clean up
    Change-Id: Ib04d9b72a6398c110dcf5c1e0d6a59b46523d132

--- modules/varnish/manifests/instance.pp
+++ modules/varnish/manifests/instance.pp
@@ -1,7 +1,7 @@
 define varnish::instance(
     $name="",
     $vcl = "",
-    $extra_vcl = [],
+    $vcl_config,
     $port="80",
     $admin_port="6083",
     $storage="-s malloc,1G",
@@ -10,7 +10,7 @@ define varnish::instance(
     $directors={},
     $director_type="hash",
     $director_options={},
-    $vcl_config,
+    $extra_vcl = [],
     $backend_options,
     $cluster_options={},
     $wikimedia_networks=[],
@@ -39,69 +39,69 @@ define varnish::instance(
     # $cluster_option is referenced directly
 
     # Install VCL include files shared by all instances
-    require "varnish::common::vcl"
+    require varnish::common::vcl
 
     $extra_vcl_variable_to_make_puppet_parser_happy = suffix($extra_vcl, " ${instancesuffix}")
     extra_vcl{ $extra_vcl_variable_to_make_puppet_parser_happy:
         before => Service["varnish${instancesuffix}"]
     }
 
-    file {
-        "/etc/init.d/varnish${instancesuffix}":
+    file { "/etc/init.d/varnish${instancesuffix}":
             content => template("${module_name}/varnish.init.erb"),
-            mode => 0555;
-        "/etc/default/varnish${instancesuffix}":
+            mode    => '0555',
+    }
+    file { "/etc/default/varnish${instancesuffix}":
             content => template("${module_name}/varnish-default.erb"),
-            mode => 0444;
-        "/etc/varnish/${vcl}.inc.vcl":
+            mode    => '0444',
+    }
+    file { "/etc/varnish/${vcl}.inc.vcl":
             content => template("varnish/${vcl}.inc.vcl.erb"),
-            notify => Exec["load-new-vcl-file${instancesuffix}"],
-            mode => 0444;
-        "/etc/varnish/wikimedia_${vcl}.vcl":
+            notify  => Exec["load-new-vcl-file${instancesuffix}"],
+            mode    => '0444',
+    }
+    file { "/etc/varnish/wikimedia_${vcl}.vcl":
             require => File["/etc/varnish/${vcl}.inc.vcl"],
             content => template("${module_name}/vcl/wikimedia.vcl.erb"),
-            mode => 0444;
+            mode    => '0444',
     }
 
     service { "varnish${instancesuffix}":
-        require => [
+        ensure    => running,
+        require   => [
                 File[
                     "/etc/default/varnish${instancesuffix}",
                     "/etc/init.d/varnish${instancesuffix}",
                     "/etc/varnish/${vcl}.inc.vcl",
                     "/etc/varnish/wikimedia_${vcl}.vcl"
                 ],
-                Mount["/var/lib/varnish"]
+                Mount['/var/lib/varnish']
             ],
         hasstatus => false,
-        pattern => "/var/run/varnishd${instancesuffix}.pid",
+        pattern   => "/var/run/varnishd${instancesuffix}.pid",
         subscribe => Package[varnish],
-        before => Exec['generate varnish.pyconf'],
-        ensure => running;
+        before    => Exec['generate varnish.pyconf'],
     }
 
     exec { "load-new-vcl-file${instancesuffix}":
-        require => [ Service["varnish${instancesuffix}"], File["/etc/varnish/wikimedia_${vcl}.vcl"] ],
-        subscribe => [File["/etc/varnish/wikimedia_${vcl}.vcl"],
-            Class["varnish::common::vcl"],
-            File[suffix(prefix($extra_vcl, "/etc/varnish/"), ".inc.vcl")]
-            ],
-        command => "/usr/share/varnish/reload-vcl $extraopts",
-        path => "/bin:/usr/bin",
-        refreshonly => true;
+        require     => [ Service["varnish${instancesuffix}"], File["/etc/varnish/wikimedia_${vcl}.vcl"] ],
+        subscribe   => [ Class[varnish::common::vcl],
+                         File[suffix(prefix($extra_vcl, '/etc/varnish/'),".inc.vcl"),
+                              "/etc/varnish/wikimedia_${vcl}.vcl"],],
+        command     => "/usr/share/varnish/reload-vcl ${extraopts}",
+        path        => '/bin:/usr/bin',
+        refreshonly => true,
     }
 
     monitor_service { "varnish http ${title}":
-        description => "Varnish HTTP ${title}",
+        description   => "Varnish HTTP ${title}",
         check_command => "check_http_generic!varnishcheck!${port}"
     }
 
     # Restart gmond if this varnish instance has been (re)started later
     # than gmond was started
     exec { "restart gmond for varnish${instancesuffix}":
-        path => "/bin:/sbin:/usr/bin:/usr/sbin",
-        command => "true",
-        onlyif => "test /var/run/varnishd${instancesuffix}.pid -nt /var/run/gmond.pid",
-        notify => Service['gmond']
+        command => '/bin/true',
+        onlyif  => "test /var/run/varnishd${instancesuffix}.pid -nt /var/run/gmond.pid",
+        notify  => Service['gmond'],
     }
 }
