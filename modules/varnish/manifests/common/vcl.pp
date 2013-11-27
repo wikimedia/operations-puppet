3faf11e2d8310b3fcd68ba05f9f345c0bf38dff9
Matanya Moses <matanya.moses@gmail.com>
2013-11-27 15:19:29 +0200

    varnish: whitespace & lint cleanups
    Change-Id: Ib04d9b72a6398c110dcf5c1e0d6a59b46523d132

--- modules/varnish/manifests/common/vcl.pp
+++ modules/varnish/manifests/common/vcl.pp
@@ -1,12 +1,13 @@
 class varnish::common::vcl {
-    require "varnish::common"
+    require varnish::common
 
-    file {
-        "/etc/varnish/geoip.inc.vcl":
-            content => template("varnish/geoip.inc.vcl.erb");
-        "/etc/varnish/device-detection.inc.vcl":
-            content => template("varnish/device-detection.inc.vcl.erb");
-        "/etc/varnish/errorpage.inc.vcl":
-            content => template("varnish/errorpage.inc.vcl.erb");
+    file { '/etc/varnish/geoip.inc.vcl':
+            content => template('varnish/geoip.inc.vcl.erb'),
+    }
+    file { '/etc/varnish/device-detection.inc.vcl':
+            content => template('varnish/device-detection.inc.vcl.erb'),
+    }
+    file { '/etc/varnish/errorpage.inc.vcl':
+            content => template('varnish/errorpage.inc.vcl.erb'),
     }
 }
--- modules/varnish/manifests/extra_vcl.pp
+++ modules/varnish/manifests/extra_vcl.pp
@@ -1,10 +1,11 @@
 define varnish::extra_vcl {
     $vcl = regsubst($title, '^([^ ]+) .*$', '\1')
     $filename = "/etc/varnish/${vcl}.inc.vcl"
+
     if !defined(File[$filename]) {
         file { $filename:
             content => template("varnish/${vcl}.inc.vcl.erb"),
-            mode => 0444;
+            mode    => '0444',
         }
     }
 }
