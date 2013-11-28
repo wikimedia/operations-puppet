0f4882d4b3818e34fa4fd6e1c1b41bbbe88921d4
Matanya Moses <matanya.moses@gmail.com>
2013-11-28 09:50:43 +0200

    toollabs: lint cleanup
    Change-Id: I748652a44071a98c74637c92f6b66f81213c5b9a

--- modules/toollabs/manifests/execnode.pp
+++ modules/toollabs/manifests/execnode.pp
@@ -18,21 +18,21 @@ class toollabs::execnode($gridmaster) inherits toollabs {
     gridmaster => $gridmaster,
   }
 
-  file { "/etc/update-motd.d/40-exechost-banner":
+  file { '/etc/update-motd.d/40-exechost-banner':
     ensure => file,
-    mode => "0755",
-    owner => "root",
-    group => "root",
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
     source => "puppet:///modules/toollabs/40-${instanceproject}-exechost-banner",
   }
 
-  file { "$store/execnode-$fqdn":
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0444',
+  file { "${store/execnode-$fqdn}":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
     require => File[$store],
-    content => "$ipaddress\n",
+    content => "${ipaddress\n}",
   }
 
   # Execution hosts have funky access requirements; they need to be ssh-able
@@ -41,44 +41,44 @@ class toollabs::execnode($gridmaster) inherits toollabs {
   # We override /etc/ssh/shosts.equiv and /etc/security/access.conf
   # accordingly from information collected from the project store.
 
-  file { "/usr/local/sbin/project-make-shosts":
+  file { '/usr/local/sbin/project-make-shosts':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-    source => "puppet:///modules/toollabs/project-make-shosts",
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/toollabs/project-make-shosts',
   }
 
-  exec { "make-shosts":
-    command => "/usr/local/sbin/project-make-shosts >/etc/ssh/shosts.equiv~",
+  exec { 'make-shosts':
+    command => '/usr/local/sbin/project-make-shosts >/etc/ssh/shosts.equiv~',
     require => File['/usr/local/sbin/project-make-shosts', $store],
   }
 
-  file { "/etc/ssh/shosts.equiv":
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0444',
-    source => "/etc/ssh/shosts.equiv~",
+  file { '/etc/ssh/shosts.equiv':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    source  => '/etc/ssh/shosts.equiv~',
     require => Exec['make-shosts'],
   }
 
-  file { "/usr/local/sbin/project-make-access":
+  file { '/usr/local/sbin/project-make-access':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-    source => "puppet:///modules/toollabs/project-make-access",
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/toollabs/project-make-access',
   }
 
-  exec { "make-access":
-    command => "/usr/local/sbin/project-make-access >/etc/security/access.conf~",
+  exec { 'make-access':
+    command => '/usr/local/sbin/project-make-access >/etc/security/access.conf~',
     require => File['/usr/local/sbin/project-make-access', $store],
   }
 
   File <| title == '/etc/security/access.conf' |> {
     content => undef,
-    source => "/etc/security/access.conf~",
+    source  => '/etc/security/access.conf~',
     require => Exec['make-access'],
   }
 
