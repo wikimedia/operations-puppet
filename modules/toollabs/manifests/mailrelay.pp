18600199c9d4435e4bc506fcfe8ab202bf0a9b55
Matanya Moses <matanya.moses@gmail.com>
2013-11-28 09:50:43 +0200

    toollabs: lint cleanup
    Change-Id: I748652a44071a98c74637c92f6b66f81213c5b9a

--- modules/toollabs/manifests/mailrelay.pp
+++ modules/toollabs/manifests/mailrelay.pp
@@ -13,45 +13,45 @@
 class toollabs::mailrelay($maildomain) inherits toollabs {
   include toollabs::infrastructure
 
-  package { "procmail":
+  package { 'procmail':
     ensure => present,
   }
 
-  file { "$store/mail-relay":
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0444',
+  file { "${store}/mail-relay":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
     require => File[$store],
-    content => template("toollabs/mail-relay.erb"),
+    content => template('toollabs/mail-relay.erb'),
   }
 
   File <| title == '/etc/exim4/exim4.conf' |> {
-    source => undef,
-    content => template("toollabs/exim4.conf.erb"),
-    notify => Service['exim4'],
+    source  => undef,
+    content => template('toollabs/exim4.conf.erb'),
+    notify  => Service['exim4'],
   }
 
   File <| title == '/etc/default/exim4' |> {
     content => undef,
-    source =>  "puppet:///modules/toollabs/exim4.default.mailrelay",
-    notify => Service['exim4'],
+    source  =>  'puppet:///modules/toollabs/exim4.default.mailrelay',
+    notify  => Service['exim4'],
   }
 
   # Enable Ganglia monitoring.
   file { '/usr/local/bin/collect_exim_stats_via_gmetric':
-    owner => 'root',
-    group => 'root',
-    mode => 0755,
-    source => 'puppet:///files/ganglia/collect_exim_stats_via_gmetric';
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///files/ganglia/collect_exim_stats_via_gmetric',
   }
 
   cron {
     'collect_exim_stats_via_gmetric':
-      user => 'root',
+      ensure  => present,
+      user    => 'root',
       command => '/usr/local/bin/collect_exim_stats_via_gmetric',
       require => File['/usr/local/bin/collect_exim_stats_via_gmetric'],
-      ensure => present;
   }
 }
 
