e98e28cd18b27d6e10252097cba66ae3cf84b891
Matanya Moses <matanya.moses@gmail.com>
2013-12-01 22:40:24 +0200

    tcpircbot: tabs to spaces
    Change-Id: Id5b1e9e724e489d75804ec81338c3f747bc77daa

--- modules/tcpircbot/manifests/init.pp
+++ modules/tcpircbot/manifests/init.pp
@@ -21,8 +21,8 @@
 # === Examples
 #
 # The following snippet will configure a bot nicknamed 'announcebot' that will
-# sit on #wikimedia-operations on Freenode and forward messages that come in from
-# private and loopback IPs on port 9200:
+# sit on #wikimedia-operations on Freenode and forward messages that come in
+#from private and loopback IPs on port 9200:
 #
 #   include tcpircbot
 #
@@ -32,36 +32,36 @@
 #   }
 #
 class tcpircbot (
-	$user        = 'tcpircbot',
-	$group       = 'tcpircbot',
-	$dir         = '/srv/tcpircbot',
+    $user        = 'tcpircbot',
+    $group       = 'tcpircbot',
+    $dir         = '/srv/tcpircbot',
 ) {
-	package { [ 'python-irclib', 'python-netaddr' ]:
-		ensure => present,
-	}
+    package { [ 'python-irclib', 'python-netaddr' ]:
+        ensure => present,
+    }
 
-	if ! defined(Group[$group]) {
-		group { $group:
-			ensure => present,
-		}
-	}
+    if ! defined(Group[$group]) {
+        group { $group:
+            ensure => present,
+        }
+    }
 
-	if ! defined(User[$user]) {
-		user { $user:
-			ensure     => present,
-			gid        => $group,
-			shell      => '/bin/false',
-			home       => $dir,
-			managehome => true,
-			system     => true,
-		}
-	}
+    if ! defined(User[$user]) {
+        user { $user:
+            ensure     => present,
+            gid        => $group,
+            shell      => '/bin/false',
+            home       => $dir,
+            managehome => true,
+            system     => true,
+        }
+    }
 
-	file { "${dir}/tcpircbot.py":
-		ensure => present,
-		source => 'puppet:///modules/tcpircbot/tcpircbot.py',
-		owner  => $user,
-		group  => $group,
-		mode   => '0555',
-	}
+    file { "${dir}/tcpircbot.py":
+        ensure => present,
+        source => 'puppet:///modules/tcpircbot/tcpircbot.py',
+        owner  => $user,
+        group  => $group,
+        mode   => '0555',
+    }
 }
--- modules/tcpircbot/manifests/instance.pp
+++ modules/tcpircbot/manifests/instance.pp
@@ -53,41 +53,41 @@
 #   }
 #
 define tcpircbot::instance(
-	$channel,
-	$password,
-	$nickname    = $title,
-	$server_host = 'chat.freenode.net',
-	$server_port = 7000,
-	$cidr        = undef,
-	$ssl         = true,
-	$max_clients = 5,
-	$listen_port = 9200,
+    $channel,
+    $password,
+    $nickname    = $title,
+    $server_host = 'chat.freenode.net',
+    $server_port = 7000,
+    $cidr        = undef,
+    $ssl         = true,
+    $max_clients = 5,
+    $listen_port = 9200,
 ) {
-	include tcpircbot
+    include tcpircbot
 
-	file { "${tcpircbot::dir}/${title}.json":
-		ensure  => present,
-		content => template('tcpircbot/tcpircbot.json.erb'),
-		require => User[$tcpircbot::user],
-	}
+    file { "${tcpircbot::dir}/${title}.json":
+        ensure  => present,
+        content => template('tcpircbot/tcpircbot.json.erb'),
+        require => User[$tcpircbot::user],
+    }
 
-	file { "/etc/init/tcpircbot-${title}.conf":
-		ensure  => present,
-		content => template('tcpircbot/tcpircbot.conf.erb'),
-	}
+    file { "/etc/init/tcpircbot-${title}.conf":
+        ensure  => present,
+        content => template('tcpircbot/tcpircbot.conf.erb'),
+    }
 
-	file { "/etc/init.d/tcpircbot-${title}":
-		ensure => link,
-		target => '/lib/init/upstart-job',
-	}
+    file { "/etc/init.d/tcpircbot-${title}":
+        ensure => link,
+        target => '/lib/init/upstart-job',
+    }
 
-	service { "tcpircbot-${title}":
-		ensure    => running,
-		provider  => 'upstart',
-		subscribe => File["/etc/init/tcpircbot-${title}.conf"],
-		require   => [
-			Package['python-irclib'],
-			File["${tcpircbot::dir}/${title}.json"],
-		],
-	}
+    service { "tcpircbot-${title}":
+        ensure    => running,
+        provider  => 'upstart',
+        subscribe => File["/etc/init/tcpircbot-${title}.conf"],
+        require   => [
+            Package['python-irclib'],
+            File["${tcpircbot::dir}/${title}.json"],
+        ],
+    }
 }
