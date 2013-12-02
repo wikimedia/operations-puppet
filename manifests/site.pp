cc8215f76e71ee473efbbdd3091cfc55670851f2
Sean Pringle <springle@wikimedia.org>
2013-12-02 21:47:23 +1000

    unbreak puppet run on pc[123]
    Change-Id: Ia569017df9860527a844840692b9eecb271075a4

--- manifests/site.pp
+++ manifests/site.pp
@@ -2189,7 +2189,11 @@ node /pc([1-3]\.pmtpa|100[1-3]\.eqiad)\.wmnet/ {
         mysql_wmf::datadirs,
         mysql_wmf::pc::conf
 
-    class { mysql_wmf::packages : mariadb => true }
+    if $::hostname =~ /^pc100(1|2|3)/ {
+        class { mysql_wmf::packages : mariadb => true }
+    } else {
+        include mysql_wmf::packages
+    }
 
     system::role { "mysql::pc::conf": description => "parser cache mysql server" }
 }
