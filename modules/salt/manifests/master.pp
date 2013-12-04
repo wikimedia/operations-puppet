2e7c90b6b6cd79d399108c31c3358ffabc80c863
Matanya Moses <matanya.moses@gmail.com>
2013-12-04 11:17:43 +0200

    salt: lint cleanup
    Change-Id: I71dc0694f0d14b7064b8e880529d6c26f3c9f31c

--- modules/salt/manifests/master.pp
+++ modules/salt/manifests/master.pp
@@ -1,48 +1,47 @@
 class salt::master(
-  $salt_version="installed",
-  $salt_interface=undef,
-  $salt_worker_threads=undef,
-  $salt_runner_dirs=["/srv/runners"],
-  $salt_file_roots={"base"=>["/srv/salt"]},
-  $salt_pillar_roots={"base"=>["/srv/pillar"]},
-  $salt_ext_pillar={},
-  $salt_reactor_root="/srv/reactors",
-  $salt_reactor = {},
-  $salt_peer={},
-  $salt_peer_run={},
-  $salt_nodegroups={}) {
+    $salt_version='installed',
+    $salt_interface=undef,
+    $salt_worker_threads=undef,
+    $salt_runner_dirs=['/srv/runners'],
+    $salt_file_roots={"base"=>['/srv/salt']},
+    $salt_pillar_roots={"base"=>['/srv/pillar']},
+    $salt_ext_pillar={},
+    $salt_reactor_root='/srv/reactors',
+    $salt_reactor = {},
+    $salt_peer={},
+    $salt_peer_run={},
+    $salt_nodegroups={}) {
 
-  package { ["salt-master"]:
-    ensure => $salt_version;
-  }
+    package { ['salt-master']:
+        ensure => $salt_version,
+    }
 
-  service { "salt-master":
-    ensure => running,
-    enable => true,
-    require => [Package["salt-master"]];
-  }
+    service { 'salt-master':
+        ensure  => running,
+        enable  => true,
+        require => Package['salt-master'],
+    }
 
-  file { "/etc/salt/master":
-    content => template("salt/master.erb"),
-    owner => root,
-    group => root,
-    mode => 0444,
-    notify => Service["salt-master"],
-    require => [Package["salt-master"]];
-  }
+    file { '/etc/salt/master':
+        content => template('salt/master.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['salt-master'],
+        require => Package['salt-master'],
+    }
 
-  file { $salt_runner_dirs:
-    ensure => directory,
-    mode => 0755,
-    owner => root,
-    group => root;
-  }
+    file { $salt_runner_dirs:
+        ensure => directory,
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
 
-  file { $salt_reactor_root:
-    ensure => directory,
-    mode => 0755,
-    owner => root,
-    group => root;
-  }
-
+    file { $salt_reactor_root:
+        ensure => directory,
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
 }
