e578a2d0cfa53d05aa17af416c900ef79d8c8498
Matanya Moses <matanya.moses@gmail.com>
2013-12-04 16:14:35 +0200

    redis: lint clean
    Change-Id: Iead24177bf56f0767ca8afc0e321f56533f6ed13

--- modules/redis/manifests/ganglia.pp
+++ modules/redis/manifests/ganglia.pp
@@ -1,25 +1,25 @@
 class redis::ganglia {
-	include redis
+    include redis
 
-	$password = $redis::password
+    $password = $redis::password
 
-	file { '/etc/ganglia/conf.d/redis.pyconf':
-			owner => root,
-			group => root,
-			mode => 0444,
-			content => template('redis/redis.pyconf.erb'),
-			notify => Service['gmond'];
-	}
-	file { '/usr/lib/ganglia/python_modules/redis_monitoring.py':
-			owner => root,
-			group => root,
-			mode => 0444,
-			source => 'puppet:///modules/redis/ganglia/redis_monitoring.py',
-			notify => Service['gmond'];
-	}
+    file { '/etc/ganglia/conf.d/redis.pyconf':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('redis/redis.pyconf.erb'),
+        notify  => Service['gmond'],
+    }
+    file { '/usr/lib/ganglia/python_modules/redis_monitoring.py':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/redis/ganglia/redis_monitoring.py',
+        notify => Service['gmond'],
+    }
 
-	# redis_monitoring.py was called redis.py before, cleanup
-	file { '/usr/lib/ganglia/python_modules/redis.py':
-			ensure => absent,
-	}
+    # redis_monitoring.py was called redis.py before, cleanup
+    file { '/usr/lib/ganglia/python_modules/redis.py':
+        ensure => absent,
+    }
 }
