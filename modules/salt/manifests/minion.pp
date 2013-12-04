49af1ed69bcf113926072dbb7fd78dc3427a0d58
Matanya Moses <matanya.moses@gmail.com>
2013-12-04 11:17:43 +0200

    salt: lint cleanup
    Change-Id: I71dc0694f0d14b7064b8e880529d6c26f3c9f31c

--- modules/salt/manifests/minion.pp
+++ modules/salt/manifests/minion.pp
@@ -1,41 +1,41 @@
 class salt::minion(
-  $salt_version='installed',
-  $salt_master=undef,
-  $salt_client_id=undef,
-  $salt_cache_jobs=undef,
-  $salt_module_dirs="[]",
-  $salt_returner_dirs="[]",
-  $salt_returner_dirs="[]",
-  $salt_states_dirs="[]",
-  $salt_render_dirs="[]",
-  $salt_grains={},
-  $salt_environment=undef,
-  $salt_master_finger=undef,
-  $salt_dns_check=undef) {
+    $salt_version='installed',
+    $salt_master=undef,
+    $salt_client_id=undef,
+    $salt_cache_jobs=undef,
+    $salt_module_dirs="[]",
+    $salt_returner_dirs="[]",
+    $salt_returner_dirs="[]",
+    $salt_states_dirs="[]",
+    $salt_render_dirs="[]",
+    $salt_grains={},
+    $salt_environment=undef,
+    $salt_master_finger=undef,
+    $salt_dns_check=undef) {
 
-  package { ["salt-minion"]:
-    ensure => $salt_version;
-  }
+    package { ['salt-minion']:
+        ensure => $salt_version,
+    }
 
-  service { "salt-minion":
-    ensure => running,
-    enable => true,
-    require => [Package["salt-minion"]];
-  }
+    service { 'salt-minion':
+        ensure  => running,
+        enable  => true,
+        require => [Package['salt-minion']],
+    }
 
-  file {
-    "/etc/salt/minion":
-      content => template("salt/minion.erb"),
-      owner => root,
-      group => root,
-      mode => 0444,
-      notify => Service["salt-minion"],
-      require => Package["salt-minion"];
-    "/usr/local/sbin/grain-ensure":
-      source => "puppet:///modules/salt/grain-ensure.py",
-      owner => root,
-      group => root,
-      mode => 0544;
-  }
+    file { '/etc/salt/minion':
+        content => template('salt/minion.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['salt-minion'],
+        require => Package['salt-minion'],
+    }
 
+    file { '/usr/local/sbin/grain-ensure':
+        source => 'puppet:///modules/salt/grain-ensure.py',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0544',
+    }
 }
