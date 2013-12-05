39f617f844edf0190a57906c9b5495da382dcd0f
Matanya Moses <matanya.moses@gmail.com>
2013-12-05 14:01:27 +0200

    puppet: lint cleanup
    Change-Id: I9d2c5a2007ed6f4ef694f110011e32b3b7e4c821

--- modules/puppet/manifests/self/client.pp
+++ modules/puppet/manifests/self/client.pp
@@ -6,13 +6,13 @@
 # $server - hostname of the puppetmaster.
 #
 class puppet::self::client($server) {
-	system::role { 'puppetclient':
-		description => "Puppet client of ${server}"
-	}
+    system::role { 'puppetclient':
+        description => "Puppet client of ${server}"
+    }
 
-	# Most of the defaults in puppet::self::config
-	# are good for setting up a puppet client.
-	class { 'puppet::self::config':
-		server => $server,
-	}
+    # Most of the defaults in puppet::self::config
+    # are good for setting up a puppet client.
+    class { 'puppet::self::config':
+        server => $server,
+    }
 }
--- modules/puppet/manifests/self/config.pp
+++ modules/puppet/manifests/self/config.pp
@@ -14,79 +14,80 @@
 # $certname             - Name of the puppet CA certificate.  Default: "$dc.$domain", e.g. the labs instance name:  i-00000699.pmtpa.wmflabs.
 #
 class puppet::self::config(
-	$server,
-	$is_puppetmaster      = false,
-	$bindaddress          = undef,
-	$puppet_client_subnet = undef,
-	$certname             = "${dc}") inherits base::puppet
+    $server,
+    $is_puppetmaster      = false,
+    $bindaddress          = undef,
+    $puppet_client_subnet = undef,
+    $certname             = $dc) inherits base::puppet
 {
-	include ldap::role::config::labs
+    include ldap::role::config::labs
 
-	$ldapconfig = $ldap::role::config::labs::ldapconfig
-	$basedn = $ldapconfig['basedn']
+    $ldapconfig = $ldap::role::config::labs::ldapconfig
+    $basedn = $ldapconfig['basedn']
 
-	$config = {
-		'dbadapter'     => 'sqlite3',
-		'node_terminus' => 'ldap',
-		'ldapserver'    => $ldapconfig['servernames'][0],
-		'ldapbase'      => "ou=hosts,${basedn}",
-		'ldapstring'    => '(&(objectclass=puppetClient)(associatedDomain=%s))',
-		'ldapuser'      => $ldapconfig['proxyagent'],
-		'ldappassword'  => $ldapconfig['proxypass'],
-		'ldaptls'       => true
-	}
+    $config = {
+        'dbadapter'     => 'sqlite3',
+        'node_terminus' => 'ldap',
+        'ldapserver'    => $ldapconfig['servernames'][0],
+        'ldapbase'      => "ou=hosts,${basedn}",
+        'ldapstring'    => '(&(objectclass=puppetClient)(associatedDomain=%s))',
+        'ldapuser'      => $ldapconfig['proxyagent'],
+        'ldappassword'  => $ldapconfig['proxypass'],
+        'ldaptls'       => true
+    }
 
-	# This is set to something different than the default
-	# /var/lib/puppet/ssl to avoid conflicts with previously
-	# generated puppet certificates from the normal puppet setup.
-	if $is_puppetmaster {
-		$ssldir = '/var/lib/puppet/server/ssl'
-		# include puppetmaster::ssl for self hosted
-		# puppetmasters.  (This sets up the ssl directories).
-		class { 'puppetmaster::ssl':
-			server_name => $::fqdn,
-			ca          => true,
-		}
+    # This is set to something different than the default
+    # /var/lib/puppet/ssl to avoid conflicts with previously
+    # generated puppet certificates from the normal puppet setup.
+    if $is_puppetmaster {
+        $ssldir = '/var/lib/puppet/server/ssl'
+        # include puppetmaster::ssl for self hosted
+        # puppetmasters.  (This sets up the ssl directories).
+        class { 'puppetmaster::ssl':
+            server_name => $::fqdn,
+            ca          => true,
+        }
 
-		# Make sure the puppet.conf compile (defined in base::puppet)
-		# runs before puppetmaster::ssl tries to generate the puppet
-		# cert.
-		Exec['compile puppet.conf'] -> Class['puppetmaster::ssl']
-	}
-	else {
-		$ssldir = '/var/lib/puppet/client/ssl'
-		# ensure $ssldir's parent dir exists
-		# so that puppet can create $ssldir.
-		file { '/var/lib/puppet/client':
-			ensure  => directory,
-			owner   => puppet,
-			group   => root,
-			mode    => 0771,
-			require => Package['puppet'],
-		}
-	}
+        # Make sure the puppet.conf compile (defined in base::puppet)
+        # runs before puppetmaster::ssl tries to generate the puppet
+        # cert.
+        Exec['compile puppet.conf'] -> Class['puppetmaster::ssl']
+    }
+    else {
+        $ssldir = '/var/lib/puppet/client/ssl'
+        # ensure $ssldir's parent dir exists
+        # so that puppet can create $ssldir.
+        file { '/var/lib/puppet/client':
+            ensure  => directory,
+            owner   => 'puppet',
+            group   => 'root',
+            mode    => '0771',
+            require => Package['puppet'],
+        }
+    }
 
-	File['/etc/puppet/puppet.conf.d/10-main.conf'] {
-		ensure => absent,
-	}
+    File['/etc/puppet/puppet.conf.d/10-main.conf'] {
+        ensure => absent,
+    }
 
-	file { '/etc/puppet/puppet.conf.d/10-self.conf':
-		owner   => root,
-		group   => root,
-		mode    => 0444,
-		content => template('puppet/puppet.conf.d/10-self.conf.erb'),
-		require => [File['/etc/puppet/puppet.conf.d'], File['/etc/puppet/puppet.conf.d/10-main.conf']],
-		notify  => Exec['compile puppet.conf'];
-	}
+    file { '/etc/puppet/puppet.conf.d/10-self.conf':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('puppet/puppet.conf.d/10-self.conf.erb'),
+        require => [File['/etc/puppet/puppet.conf.d'], File['/etc/puppet/puppet.conf.d/10-main.conf']],
+        notify  => Exec['compile puppet.conf'],
+    }
+    $puppetmaster_status = $is_puppetmaster ? {
+            true    => 'file',
+            default => absent,
+        }
 
-	file { '/etc/puppet/fileserver.conf':
-		owner    => root,
-		group    => root,
-		mode     => 0444,
-		content  => template('puppet/fileserver-self.conf.erb'),
-		ensure   => $is_puppetmaster ? {
-			true    => 'file',
-			default => absent,
-		}
-	}
+    file { '/etc/puppet/fileserver.conf':
+        ensure   => $puppetmaster_status,
+        owner    => 'root',
+        group    => 'root',
+        mode     => '0444',
+        content  => template('puppet/fileserver-self.conf.erb'),
+    }
 }
