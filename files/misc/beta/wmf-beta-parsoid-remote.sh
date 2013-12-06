fb7bf8d14e3e7d57c5820e3d7bbbab5b2e929379
Antoine Musso <hashar@free.fr>
2013-12-06 16:31:33 +0100

    beta: properly connect to parsoid instance
    The wmf-beta-autoupdate.py script runs as the mwdeploy user. Thus the
    ssh commands to the parsoid instance would fail because that user does
    not have any ssh credential to connect there (jenkins-deploy has).
    
    This patch introduce a new tiny shell wrapper to let us execute the
    parsoid init script on deployment-parsoid2. It must be run as the
    mwdeploy user (like wmf-beta-autoupdate.py) and will sudo as
    jenkins-deploy to have the proper credentials.
    
    A new sudo policy is added to let mwdeploy execute the shell wrapper as
    jenkins-deploy.
    
    bug: 57233
    Change-Id: I0fa1beba0f7e91b0999d6820b20a7569eb735087

---
+++ files/misc/beta/wmf-beta-parsoid-remote.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+#######################################################################
+# WARNING: this file is managed by Puppet
+# puppet:///files/misc/beta/wmf-beta-parsoid-restart.sh
+#######################################################################
+
+if [ "$USER" != 'mwdeploy' ]
+then
+    echo "Script MUST be run as mwdeploy user"
+    echo "Additionally the user needs sudo right for ssh as jenkins-deploy"
+    exit 1
+fi
+
+PARSOID_INSTANCE="deployment-parsoid2.pmtpa.wmflabs"
+
+# The beta autoupdater runs as mwdeploy. We need jenkins-deploy ssh credentials
+# to be able to connect to the parsoid instance. On there, we restart Parsoid
+# as root.
+sudo -H -u jenkins-deploy \
+    ssh $PARSOID_INSTANCE \
+        sudo -u root /etc/init.d/parsoid $1
