879b43399c96f9dd56c59eff809a3fb1835f101b
Andrew Bogott <abogott@wikimedia.org>
2013-12-09 04:28:26 +0000

    Add proper nova auth to labsstatus.rb
    Change-Id: Ibe665d1d46507a83cc675add4247ea61d2e08342

--- modules/puppetmaster/lib/puppet/reports/labsstatus.rb
+++ modules/puppetmaster/lib/puppet/reports/labsstatus.rb
@@ -5,6 +5,7 @@
 require 'puppet'
 require 'yaml'
 require 'erb'
+require 'open3'
 
 Puppet::Reports.register_report(:labsstatus) do
     desc = 'Record puppet status of labs instances in labs instance metadata'
@@ -12,6 +13,7 @@ Puppet::Reports.register_report(:labsstatus) do
     def process
         project = ""
         hostname = ""
+        conf = YAML.load_file('/etc/labsstatus.cfg')
         self.logs.each do |logline|
             if logline.message =~ /'instanceproject: (.*)'/
                 project = $1
@@ -21,8 +23,17 @@ Puppet::Reports.register_report(:labsstatus) do
             end
         end
         if (!project.empty?) and (!hostname.empty?)
-            command = "nova --os-tenant-name #{project} meta #{hostname} set puppetstatus=#{self.status}"
-            system(command)
+            command = "/usr/bin/nova --no-cache --os-region-name #{conf['region']} --os-auth-url #{conf['auth_url']} --os-password #{conf['password']} --os-username #{conf['username']} --os-tenant-name #{project} meta #{hostname} set puppetstatus=#{self.status}"
+	    #system("echo trying \"#{command}\" >> /var/log/labsstatus.log")
+
+	    stdin, stdout, stderr, wait_thr = Open3.popen3(command)
+	    std_out = stdout.read
+	    std_err = stderr.read
+	    stdin.close
+	    stdout.close
+	    stderr.close
+	    insp = std_err.inspect
+	    #system("echo insp: #{insp} >> /var/log/labsstatus.log")
         end
     end
 end
