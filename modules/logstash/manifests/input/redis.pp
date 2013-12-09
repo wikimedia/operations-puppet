e4e0ed0ae40ee22f56dd2cb731d25dc2af3cb531
Bryan Davis <bd808@wikimedia.org>
2013-12-09 10:02:58 -0700

    Logstash puppet class
    Puppet class to install and configure logstash. This class assumes that
    a logstash package exists in apt that is built from the Debian package
    components specified in the logstash github repository.
    
    Change-Id: I18800e66b55ad9eb28acae160c88a2b9867231aa

---
+++ modules/logstash/manifests/input/redis.pp
@@ -0,0 +1,37 @@
+# vim:sw=4 ts=4 sts=4 et:
+
+# = Class: logstash::input::redis
+#
+# Configure logstash to collect input from a Redis server
+#
+# == Parameters:
+# - $host: Redis server to contact
+# - $port: Redis server port
+# - $data_type: Type of communication: 'list', 'channel', 'pattern_channel'
+# - $key: Name of a redis list or channel
+# - $priority: Configuration loading priority. Default '10'.
+# - $ensure: Whether the config should exist.
+#
+# == Sample usage:
+#
+#   class { 'logstash::input::redis':
+#       host => '127.0.0.1',
+#       key  => 'logstash',
+#   }
+#
+class logstash::input::redis(
+    $host       = '127.0.0.1',
+    $port       = 6379,
+    $data_type  = 'list',
+    $key        = 'logstash',
+    $priority   = 10,
+    $ensure     = present,
+) {
+    require logstash
+
+    @logstash::conf{ 'input-redis':
+        content  => template('logstash/input/redis.erb'),
+        priority => $priority,
+        ensure   => $ensure,
+    }
+}
---
+++ modules/logstash/manifests/output/elasticsearch.pp
@@ -0,0 +1,94 @@
+# vim:sw=4 ts=4 sts=4 et:
+
+# = Class: logstash::output::elasticsearch
+#
+# Configure logstash to output to elasticsearch
+#
+# == Parameters:
+# - $host: Elasticsearch server. Default '127.0.0.1'.
+# - $flush_size: Maximum number of events to buffer before sending.
+#       Default 100.
+# - $idle_flush_time: Maxmimum seconds to wait between sends. Default 1.
+# - $index: Index to write events to. Default 'logstash-%{+YYYY.MM.dd}'.
+# - $port: Elasticsearch server port. Default 9200.
+# - $replication: Type of elasticsearch replication to use ('async', 'sync').
+#       Default 'sync'.
+# - $require_tag: Tag to require on events. Default undef.
+# - $manage_indices: Whether cron jobs should be installed to manage
+#       elasticsearch indices. Default false.
+# - $priority: Configuration loading priority. Default 10.
+# - $ensure: Whether the config should exist. Default present.
+#
+# == Sample usage:
+#
+#   class { 'logstash::output::elasticsearch':
+#       host           => '127.0.0.1',
+#       replication    => 'async',
+#       require_tag    => 'es',
+#       manage_indices => 'true',
+#   }
+#
+class logstash::output::elasticsearch(
+    $host            = '127.0.0.1',
+    $flush_size      = 100,
+    $idle_flush_time = 1,
+    $index           = 'logstash-%{+YYYY.MM.dd}',
+    $port            = 9200,
+    $replication     = 'sync',
+    $require_tag     = undef,
+    $manage_indices  = false,
+    $priority        = 10,
+    $ensure          = present,
+) {
+    require logstash
+
+    @logstash::conf{ 'output-elasticsearch':
+        content  => template('logstash/output/elasticsearch.erb'),
+        priority => $priority,
+        ensure   => $ensure,
+    }
+
+    # TODO: add support for manage_template when we upgrade to v1.3.2+
+
+    file { "/usr/local/bin/logstash_delete_index.sh":
+        ensure  => $ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        source  => 'puppet:///files/logstash/logstash_delete_index.sh',
+    }
+
+    file { "/usr/local/bin/logstash_optimize_index.sh":
+        ensure  => $ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        source  => 'puppet:///files/logstash/logstash_optimize_index.sh',
+    }
+
+    $ensure_cron = $manage_indices ? {
+        true    => 'present',
+        default => 'absent'
+    }
+
+    cron { 'logstash_delete_index':
+        ensure  => $ensure_cron,
+        command => "/usr/local/bin/logstash_delete_index.sh http://${host}:${port}",
+        user    => 'root',
+        hour    => 0,
+        minute  => 42,
+        require => File['/usr/local/bin/logstash_delete_index.sh'],
+    }
+
+    cron { 'logstash_optimize_index':
+        ensure  => $ensure_cron,
+        command => "/usr/local/bin/logstash_optimize_index.sh http://${host}:${port}",
+        user    => 'root',
+        hour    => 1,
+        # Stagger execution on each node of cluster to avoid running in
+        # parallel.
+        minute  => 5 * fqdn_rand(12, 'logstash_optimize_index'),
+        require => File['/usr/local/bin/logstash_optimize_index.sh'],
+    }
+
+}
