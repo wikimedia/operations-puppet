bf6fd1d0ee791217ec232dcec58edd9d4f7805b4
Bryan Davis <bd808@wikimedia.org>
2013-12-09 10:02:58 -0700

    [WIP] Logstash puppet class
    Puppet class to install and configure logstash. This class assumes that
    a logstash package exists in apt that is built from the Debian package
    components specified in the logstash github repository.
    
    Change-Id: I18800e66b55ad9eb28acae160c88a2b9867231aa

---
+++ modules/logstash/manifests/output/elasticsearch.pp
@@ -0,0 +1,43 @@
+# = Class: logstash::output::elasticsearch
+#
+# Configure logstash to output to elasticsearch
+#
+# == Parameters:
+# - $host: Elasticsearch server to contact
+# - $flush_size: Maximum number of events to buffer before sending
+# - $idle_flush_time: Maxmimum seconds to wait between sends
+# - $index: Index to write events to
+# - $port: Elasticsearch server port
+# - $replication: Type of elasticsearch replication to use ('async', 'sync')
+# - $require_tag: Tag to require on events
+# - $priority: Configuration loading priority. Default '10'.
+# - $ensure: Whether the config should exist.
+#
+# == Sample usage:
+#
+#   class { 'logstash::output::elasticsearch':
+#       host        => '127.0.0.1',
+#       replication => 'async',
+#       require_tag => 'es',
+#   }
+#
+class logstash::output::elasticsearch(
+    $host            = '127.0.0.1',
+    $flush_size      = 100,
+    $idle_flush_time = 1,
+    $index           = 'logstash-%{+YYYY.MM.dd}',
+    $port            = 9200,
+    $replication     = 'sync',
+    $require_tag     = undef,
+    $priority        = '10',
+    $ensure          = present,
+) {
+    require logstash
+
+    @logstash::conf{ 'output-elasticsearch':
+        content  => template('logstash/output/elasticsearch.erb'),
+        priority => $priority,
+        ensure   => $ensure,
+    }
+}
+# vim:sw=4 ts=4 sts=4 et:
