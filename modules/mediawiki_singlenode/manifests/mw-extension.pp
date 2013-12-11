67a703e56227f50b94fa75fe9fdfcb46f41f4acf
Matanya Moses <matanya.moses@gmail.com>
2013-12-11 17:08:25 +0200

    mediawiki_singlenode : lint cleanup
    Change-Id: I5435d91ac0ffe735fc72f2ccb6b68c001e94d9fc

--- modules/mediawiki_singlenode/manifests/mw-extension.pp
+++ modules/mediawiki_singlenode/manifests/mw-extension.pp
@@ -1,15 +1,15 @@
 # definition to clone mediawiki extensions
 define mediawiki_singlenode::mw-extension(
-	$ensure       = present,
-	$branch       = 'master',
-	$install_path = '/srv/mediawiki',
+    $ensure       = present,
+    $branch       = 'master',
+    $install_path = '/srv/mediawiki',
 ) {
-	git::clone { $name:
-		require   => Git::Clone['mediawiki'],
-		directory => "${install_path}/extensions/${name}",
-		origin    => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/${name}.git",
-		branch    => $branch,
-		ensure    => $ensure,
-		notify => Exec['mediawiki_update'],
-	}
+    git::clone { $name:
+        ensure    => $ensure,
+        require   => Git::Clone['mediawiki'],
+        directory => "${install_path}/extensions/${name}",
+        origin    => "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/${name}.git",
+        branch    => $branch,
+        notify    => Exec['mediawiki_update'],
+    }
 }
