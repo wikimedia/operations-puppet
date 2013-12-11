680b779ecd0098ffc4237314b27a8b3d7595f536
Antoine Musso <hashar@free.fr>
2013-12-11 15:27:04 +0100

    lint applicationserver::cron
    * reindent using 4 spaces
    * added in vim modeline
    * each cron is now in its own cron {} statement
    * nicely align arrows
    * use comma instead of semicolon
    * put 'ensure =>' as first statement
    * replace double quotes by single quotes, albeit not for the commands to
      ensure this is not going to be a breaking change
    
    Change-Id: I730767d84d32253b62f07037258a0d27a571a9bb

--- modules/applicationserver/manifests/cron.pp
+++ modules/applicationserver/manifests/cron.pp
@@ -1,21 +1,23 @@
 # application server required cron jobs
+# vim: set ts=4 sw=4 et:
 class applicationserver::cron {
-	cron {
-		"cleanupipc":
-			command => "ipcs -s | grep apache | cut -f 2 -d \\  | xargs -rn 1 ipcrm -s",
-			user => root,
-			minute => 26,
-			ensure => present;
-		"cleantmpphp":
-			command => "find /tmp -name 'php*' -type f -ctime +1 -exec rm -f {} \\;",
-			user => root,
-			hour => 5,
-			minute => 0,
-			ensure => present;
-		"bug55541":   # Hack! Fix the bug and kill this job. --Ori
-			command => 'find /tmp -name \*.tif -mmin +60  -delete',
-			user => root,
-			minute => 34,
-			ensure => present;
-	}
+    cron { 'cleanupipc':
+        ensure  => present,
+        command => "ipcs -s | grep apache | cut -f 2 -d \\  | xargs -rn 1 ipcrm -s",
+        user    => root,
+        minute  => 26,
+    }
+    cron { 'cleantmpphp':
+        ensure  => present,
+        command => "find /tmp -name 'php*' -type f -ctime +1 -exec rm -f {} \\;",
+        user    => root,
+        hour    => 5,
+        minute  => 0,
+    }
+    cron { 'bug55541':   # Hack! Fix the bug and kill this job. --Ori
+        ensure  => present,
+        command => 'find /tmp -name \*.tif -mmin +60  -delete',
+        user    => root,
+        minute  => 34,
+    }
 }
