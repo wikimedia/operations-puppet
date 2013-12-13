aef9e8ca9fdfb085e9bb7241cde151cae0d0dfde
Antoine Musso <hashar@free.fr>
2013-12-13 11:40:43 +0100

    beta: public IP rewriting using DNAT
    MediaWiki on the beta cluster would often attempt to query some API
    using the DNS entry (ie: en.wikipedia.beta.wmflabs.org).  The DNS server
    would serve the public IP address of another instance and NAT would not
    let the instance communicates between each other.
    
    The fix is to rewrite outgoing destination address from the public IP
    address to the private IP address it is bound to.  The iptables rules
    have been applied manually until then and disappeared whenever an
    instance got rebooted.  That caused major headhaces to the Wikidata team
    for a few months now.
    
    We used to maintain iptables rules using Augeas, a system that nobody
    understand.  Nowadays we are using ferm, a tiny wrapper around iptables
    which make it way easier to add iptables rules.
    
    I first define a list of hardcoded mappings then call a custom define
    using the nice create_resources() puppet utility.  I did such a thing
    previously for the protoproxy configuration on beta (see d0b881e6 /
    https://gerrit.wikimedia.org/r/#/c/63431/ ).
    
    Should be the equivalent of generating the following iptables rules:
    
      iptables -t nat -I OUTPUT --dest 208.80.153.219 \
        -j DNAT --to-dest 10.4.1.133
      iptables -t nat -I OUTPUT --dest 208.80.153.242 \
        -j DNAT --to-dest 10.4.0.211
      iptables -t nat -I OUTPUT --dest 208.80.153.243 \
        -j DNAT --to-dest 10.4.0.51
      iptables -t nat -I OUTPUT --dest 208.80.153.244 \
        -j DNAT --to-dest 10.4.0.48
      iptables -t nat -I OUTPUT --dest 208.80.153.243 \
        -j DNAT --to-dest 10.4.1.82
    
    RT #4824
    bug: 45868
    
    Change-Id: I958a6d4aa1fb701aaccb2e9622c5a71e0d3fd93b

--- manifests/misc/beta.pp
+++ manifests/misc/beta.pp
@@ -49,6 +49,48 @@ class misc::beta::autoupdater {
 
 }
 
+# Workaround NAT traversal issue when a beta cluster instance attempt to
+# connect to a beta public IP. The NAT would get the packet loss, instead
+# transparently destination IP of outgoing packets to point directly to the
+# private IP instance instead of the public IP.
+#
+# FIXME should probably be applied by default on ALL beta cluster instances.
+#
+# References:
+#
+# RT #4824   - https://rt.wikimedia.org/Ticket/Display.html?id=4824
+# bug #45868 - https://bugzilla.wikimedia.org/show_bug.cgi?id=45868
+class misc::beta::natfixup {
+
+	# List out the instance public IP and private IP as described in OpenStack
+	# manager interface
+	#
+	# FIXME ideally that should be fetched directly from OpenStack
+	# configuration to make sure the iptables revwrites are always in sync with
+	# the web interface :-D
+	#
+	$nat_mappings = {
+		'deployment-cache-text1'    => { public_ip => '208.80.153.219', private_ip => '10.4.1.133' },
+		'deployment-cache-upload04' => { public_ip => '208.80.153.242', private_ip => '10.4.0.211' },
+		'deployment-cache-bits03'   => { public_ip => '208.80.153.243', private_ip => '10.4.0.51' },
+		'deployment-eventlogging'   => { public_ip => '208.80.153.244', private_ip => '10.4.0.48' },
+		'deployment-cache-mobile01' => { public_ip => '208.80.153.143', private_ip => '10.4.1.82' },
+	}
+	create_resources( 'misc::beta::natdestrewrite', $nat_mappings )
+}
+
+define misc::beta::natdestrewrite( $public_ip, $private_ip ) {
+
+	include base::firewall
+
+	ferm::rule { "nat_rewrite_for_${name}":
+		# iptables -t nat -I OUTPUT --dest $public_ip -j DNAT --to-dest $private_ip
+		rule => "table nat chain OUTPUT { daddr ${public_ip} { DNAT to ${private_ip}; } }",
+	}
+
+}
+
+
 class misc::beta::sync-site-resources {
 	file { "/usr/local/bin/sync-site-resources":
 		ensure => present,
