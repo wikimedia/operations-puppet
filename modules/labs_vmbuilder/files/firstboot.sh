cec6e460743b66d070e2ce015e7c45f64d4d34d9
Ryan Lane <rlane@wikimedia.org>
2013-12-16 16:12:43 -0500

    Add firstboot script and ubuntu-standard package
    This change adds the ubuntu-standard package for bug 54080 and
    also adds the firstboot script that will be called on initial
    instance boot.
    
    Change-Id: If28081eb19917281caf5f8eec085ccec95253b2f

---
+++ modules/labs_vmbuilder/files/firstboot.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ -f '/root/.firstboot' ]
+then
+	# Only run firstboot once
+	exit
+fi
+
+echo 'Enabling console logging for puppet while it does the initial run'
+echo 'daemon.* |/dev/console' > /etc/rsyslog.d/60-puppet.conf
+restart rsyslog
+
+binddn=`grep 'binddn' /etc/ldap.conf | sed 's/.* //'`
+bindpw=`grep 'bindpw' /etc/ldap.conf | sed 's/.* //'`
+hostsou=`grep 'nss_base_hosts' /etc/ldap.conf | sed 's/.* //'`
+id=`curl http://169.254.169.254/1.0/meta-data/instance-id 2> /dev/null`
+domain=`hostname -d`
+idfqdn=${id}.${domain}
+#TODO: get project a saner way
+project=`ldapsearch -x -D ${binddn} -w ${bindpw} -b ${hostsou} "dc=${idfqdn}" puppetvar | grep 'instanceproject' | sed 's/.*=//'`
+saltfinger="c5:b1:35:45:3e:0a:19:70:aa:5f:3a:cf:bf:a0:61:dd"
+if [ "${domain}" == "pmtpa.wmflabs" ]
+then
+	master="virt0.wikimedia.org"
+	master_secondary="virt1000.wikimedia.org"
+elif [ "${domain}" == "eqiad.wmflabs" ]
+then
+	master="virt1000.wikimedia.org"
+	master_secondary="virt0.wikimedia.org"
+fi
+
+# Finish LDAP configuration
+sed -i "s/_PROJECT_/${project}/g" /etc/security/access.conf
+sed -i "s/_PROJECT_/${project}/g" /etc/ldap/ldap.conf
+sed -i "s/_PROJECT_/${project}/g" /etc/sudo-ldap.conf
+sed -i "s/_PROJECT_/${project}/g" /etc/default/autofs
+sed -i "s/_PROJECT_/${project}/g" /etc/nslcd.conf
+sed -i "s/_FQDN_/${idfqdn}/g" /etc/puppet/puppet.conf
+sed -i "s/_MASTER_/${master}/g" /etc/puppet/puppet.conf
+
+/etc/init.d/autofs restart
+dpkg-reconfigure -fnoninteractive -pcritical openssh-server
+/etc/init.d/ssh stop
+/etc/init.d/ssh start
+
+# Initial salt config
+echo -e "master:\n  - ${master}\n  - ${master_secondary}" > /etc/salt/minion
+echo "id: ${idfqdn}" >> /etc/salt/minion
+echo "master_finger: ${saltfinger}" >> /etc/salt/minion
+/etc/init.d/salt-minion restart
+
+# Force initial puppet run
+puppet agent --onetime --verbose --no-daemonize --no-splay --show_diff --waitforcert=10 --certname=${idfqdn} --server=${master}
+
+touch /root/.firstboot
