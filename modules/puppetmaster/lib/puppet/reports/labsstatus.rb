a78a78d80d439e60d757555431787af7a4b9950d
Andrew Bogott <abogott@wikimedia.org>
2013-12-16 20:50:20 +0000

    Include a timestamp for last puppet run
    Change-Id: I40090ff1f899468ea3ebd18b29dfd2186fe537d7

--- modules/puppetmaster/lib/puppet/reports/labsstatus.rb
+++ modules/puppetmaster/lib/puppet/reports/labsstatus.rb
@@ -10,6 +10,20 @@ require 'open3'
 Puppet::Reports.register_report(:labsstatus) do
     desc = 'Record puppet status of labs instances in labs instance metadata'
 
+    def addMeta(key, value)
+            command = "/usr/bin/nova --no-cache --os-region-name #{conf['region']} --os-auth-url #{conf['auth_url']} --os-password #{conf['password']} --os-username #{conf['username']} --os-tenant-name #{project} meta #{hostname} set #{key}=#{value}"
+            #system("echo trying \"#{command}\" >> /var/log/labsstatus.log")
+
+            stdin, stdout, stderr, wait_thr = Open3.popen3(command)
+            std_out = stdout.read
+            std_err = stderr.read
+            stdin.close
+            stdout.close
+            stderr.close
+            insp = std_err.inspect
+            #system("echo insp: #{insp} >> /var/log/labsstatus.log")
+    end
+
     def process
         project = ""
         hostname = ""
@@ -23,17 +37,8 @@ Puppet::Reports.register_report(:labsstatus) do
             end
         end
         if (!project.empty?) and (!hostname.empty?)
-            command = "/usr/bin/nova --no-cache --os-region-name #{conf['region']} --os-auth-url #{conf['auth_url']} --os-password #{conf['password']} --os-username #{conf['username']} --os-tenant-name #{project} meta #{hostname} set puppetstatus=#{self.status}"
-	    #system("echo trying \"#{command}\" >> /var/log/labsstatus.log")
-
-	    stdin, stdout, stderr, wait_thr = Open3.popen3(command)
-	    std_out = stdout.read
-	    std_err = stderr.read
-	    stdin.close
-	    stdout.close
-	    stderr.close
-	    insp = std_err.inspect
-	    #system("echo insp: #{insp} >> /var/log/labsstatus.log")
+            addMeta('puppetstatus', self.status)
+            addMeta('puppettimestamp', self.timestamp)
         end
     end
 end
