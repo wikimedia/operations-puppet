7a7fa2fbc633efa6aae4c6859aaaec8adf3af791
Antoine Musso <hashar@free.fr>
2013-12-18 12:41:50 +0100

    beta: fix up parsoid restarter
    mwdeploy sudo definition had a typo in NOPASSWD and had an extra space
    before the whitelisted command.
    
    wmf-beta-parsoid-remote should be run using jenkins-deploy credential.
    The wmf-beta-autoupdate.py script is run by mwdeploy that has the sudo
    right to run wmf-beta-parsoid-remote has jenkins-deploy.
    
    Change-Id: Iaf7808aab72942549bc0a5f44393d34c53cbd5ad

--- files/misc/beta/wmf-beta-parsoid-remote.sh
+++ files/misc/beta/wmf-beta-parsoid-remote.sh
@@ -4,10 +4,9 @@
 # puppet:///files/misc/beta/wmf-beta-parsoid-restart.sh
 #######################################################################
 
-if [ "$USER" != 'mwdeploy' ]
+if [ "$USER" != 'jenkins-deploy' ]
 then
-    echo "Script MUST be run as mwdeploy user"
-    echo "Additionally the user needs sudo right for ssh as jenkins-deploy"
+    echo "Script MUST be run as jenkins-deploy user to use its credential"
     exit 1
 fi
 
@@ -16,6 +15,5 @@ PARSOID_INSTANCE="deployment-parsoid2.pmtpa.wmflabs"
 # The beta autoupdater runs as mwdeploy. We need jenkins-deploy ssh credentials
 # to be able to connect to the parsoid instance. On there, we restart Parsoid
 # as root.
-sudo -H -u jenkins-deploy \
-    ssh $PARSOID_INSTANCE \
-        sudo -u root /etc/init.d/parsoid $1
+ssh $PARSOID_INSTANCE \
+    sudo -u root /etc/init.d/parsoid $1
