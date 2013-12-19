e453bd54a18244d3b73fd0f99347e9589f26a4b9
Mark Bergsma <mark@wikimedia.org>
2013-12-19 13:05:55 +0100

    Fix parameter name
    Change-Id: I009f748a73c232f89f56e5b14584b37b16ed9eae

--- manifests/role/lvs.pp
+++ manifests/role/lvs.pp
@@ -10,7 +10,7 @@ class role::lvs::balancer {
     # Older PyBal is very dependent on recursive DNS, to the point where it is a SPOF
     # So we'll have every LVS server run their own recursor
     $nameservers_prefix = [ $::ipaddress ]
-    class { 'dns::recursor': listen_address => [ $::ipaddress ] }
+    class { 'dns::recursor': listen_addresses => [ $::ipaddress ] }
 
     include lvs::configuration
     $sip = $lvs::configuration::lvs_service_ips[$::realm]
