61ea4de69c8b0fb3c9423feb0c26714669dd3f14
Mark Bergsma <mark@wikimedia.org>
2013-12-19 16:30:31 +0100

    Memcached: reformat
    Change-Id: I39cb35e0e4c989f9d8db9363c737ed9122a6adab

--- manifests/memcached.pp
+++ manifests/memcached.pp
@@ -1,10 +1,20 @@
 # memcached.pp
 
-class memcached ($memcached_size = '2000', $memcached_port = '11000', $memcached_ip = '0.0.0.0',
-		$version = "present", $memcached_options = {}, $pin=false) {
+class memcached (
+	$memcached_size = '2000',
+	$memcached_port = '11000',
+	$memcached_ip = '0.0.0.0',
+	$version = "present",
+	$memcached_options = {},
+	$pin=false)
+{
 
-	class { "memcached::config": memcached_size => "$memcached_size", memcached_port => "$memcached_port",
-		memcached_ip => "$memcached_ip", memcached_options => $memcached_options }
+	class { "memcached::config":
+		memcached_size => "$memcached_size",
+		memcached_port => "$memcached_port",
+		memcached_ip => "$memcached_ip",
+		memcached_options => $memcached_options
+	}
 
 	if ( $pin ) {
 		apt::pin { 'memcached':
