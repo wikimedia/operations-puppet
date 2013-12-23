fb8952cf5c21258f94a41c6308244554463fb486
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-12-23 12:57:57 +0200

    module/role class for servermon
    Introduce a module class for servermon and a role class for it as well
    Include RSpec
    Include tests
    Using the deployment infrastructure. No upstream for now
    settings.py/urls.py files added
    Linted
    Tested
    apache reverse proxying to gunicorn
    netmon1001 picked as the server to run this on
    module/role documentation
    
    Change-Id: Ib48a0736a1d96e24c33d7a59c75e40eb73e3ceff

---
+++ modules/servermon/spec/classes/servermon_app_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'servermon', :type => :class do
+    let(:params) { {
+        :ensure     => 'present',
+        :directory  => '/tmp/test',
+        :db_name    => 'testdb',
+        :secret_key => 'superdupersecret'
+        }
+    }
+
+    it { should contain_package('python-django') }
+    it { should contain_package('python-django-south') }
+    it { should contain_package('python-ldap') }
+    it { should contain_package('python-ipy') }
+    it { should contain_package('python-whoosh') }
+
+    it { should contain_package('gunicorn') }
+    it { should contain_service('gunicorn') }
+    it { should contain_file('/etc/gunicorn.d/servermon').with_content(/\/tmp\/test/) }
+    it { should contain_file('/tmp/test/settings.py').with_content(/testdb/) }
+    it { should contain_file('/tmp/test/urls.py') }
+end
