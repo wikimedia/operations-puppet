14fcce4b62343b05607b19f40ff74e603e2cdf1b
Matanya Moses <matanya@foss.co.il>
2013-12-29 14:32:51 +0200

    labs_vagrant:lint clean
    Change-Id: Ic1f669a887ecf1d373df591108fbfb5452fa89fd

--- modules/labs_vagrant/manifests/init.pp
+++ modules/labs_vagrant/manifests/init.pp
@@ -1,40 +1,40 @@
 class labs_vagrant {
     user { 'vagrant':
-        ensure => 'present',
+        ensure     => 'present',
         managehome => true
     }
 
     file { '/etc/sudoers.d/vagrant':
-        source => 'puppet:///modules/labs_vagrant/vagrant-sudoers',
-        owner => 'root',
-        group => 'root',
-        mode => '0440',
-        require => User['vagrant']
+        source  => 'puppet:///modules/labs_vagrant/vagrant-sudoers',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0440',
+        require => User['vagrant'],
     }
 
     git::clone { 'vagrant':
         directory => '/mnt/vagrant/',
-        origin => 'https://gerrit.wikimedia.org/r/mediawiki/vagrant'
+        origin    => 'https://gerrit.wikimedia.org/r/mediawiki/vagrant',
     }
 
     # /mnt has way more space than /
     file { '/mnt/vagrant':
         recurse => true,
-        owner => 'vagrant',
-        group => 'www-data',
-        require => [ User['vagrant'], Exec['git_clone_vagrant'] ]
+        owner   => 'vagrant',
+        group   => 'www-data',
+        require => [ User['vagrant'], Exec['git_clone_vagrant'] ],
     }
 
     file { '/vagrant':
-        ensure => 'link',
-        target => '/mnt/vagrant',
-        require => File['/mnt/vagrant']
+        ensure  => 'link',
+        target  => '/mnt/vagrant',
+        require => File['/mnt/vagrant'],
     }
 
     file { '/bin/labs-vagrant':
-        ensure => 'link',
-        target => '/vagrant/lib/labs-vagrant.rb',
-        mode => '0555',
-        require => File['/vagrant']
+        ensure  => 'link',
+        target  => '/vagrant/lib/labs-vagrant.rb',
+        mode    => '0555',
+        require => File['/vagrant'],
     }
 }
