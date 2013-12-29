cb7bc97b1ce5aaf6231607539b91cdbc25e80238
Matanya Moses <matanya@foss.co.il>
2013-12-29 14:25:57 +0200

    labs_vmbuilder: lint clean
    Change-Id: I16dbdd3bd256211405cdb71f0a249acdedee09a0

--- modules/labs_vmbuilder/manifests/init.pp
+++ modules/labs_vmbuilder/manifests/init.pp
@@ -1,68 +1,56 @@
 class labs_vmbuilder($vmbuilder_version) {
     package { 'python-vm-builder':
-        ensure => present;
+        ensure => present,
     }
 
     $vmbuilder_filepath = '/etc/vmbuilder/files'
 
     file { '/etc/vmbuilder.cfg':
-        mode     => 0444,
+        mode     => '0444',
         content  => template('labs_vmbuilder/vmbuilder.cfg.erb'),
-        require  => [
-            Package['python-vm-builder'],
-        ];
+        require  => Package['python-vm-builder'],
     }
 
     file { '/etc/vmbuilder/postinst/postinst.copy':
-        mode    => 0444,
+        mode    => '0444',
         source  => 'puppet:///labs_vmbuilder/postinst.copy',
-        require => [
-            Package['python-vm-builder'],
-        ];
+        require => Package['python-vm-builder'],
     }
+
     file { '/etc/vmbuilder/postinst/postinst.sh':
-        mode    => 0555,
+        mode    => '0555',
         source  => 'puppet:///labs_vmbuilder/postinst.sh',
-        require => [
-            Package['python-vm-builder'],
-        ];
+        require => Package['python-vm-builder'],
     }
+
     file { '/etc/vmbuilder/firstscripts/firstboot.sh':
-        mode    => 0555,
+        mode    => '0555',
         source  => 'puppet:///labs_vmbuilder/firstboot.sh',
-        require => [
-            Package['python-vm-builder'],
-        ];
+        require => Package['python-vm-builder'],
     }
 
     file { "${vmbuilder_filepath}":
         ensure => directory,
-        mode   => 0555,
+        mode   => '0555',
     }
+
     file { "${vmbuilder_filepath}/install_sudo.sh":
-        mode    => 0555,
+        mode    => '0555',
         source  => 'puppet:///labs_vmbuilder/install_sudo.sh',
-        require => [
-            Package['python-vm-builder'],
-            File["${vmbuilder_filepath}"],
-        ];
+        require => [Package['python-vm-builder'], File["${vmbuilder_filepath}"],],
     }
+
     file { "${vmbuilder_filepath}/vmbuilder.partition":
-        mode    => 0555,
+        mode    => '0555',
         source  => 'puppet:///labs_vmbuilder/vmbuilder.partition',
-        require => [
-            Package['python-vm-builder'],
-            File["${vmbuilder_filepath}"],
-        ];
+        require => [Package['python-vm-builder'], File["${vmbuilder_filepath}"],],
     }
+
     file { 'vmbuilder_version':
         path    => '/etc/vmbuilder/files/vmbuilder_version',
-        mode    => 0444,
+        mode    => '0444',
         content => "${vmbuilder_version}",
-        require => [
-            Package['python-vm-builder'],
-            File["${vmbuilder_filepath}"],
-        ];
+        require => [Package['python-vm-builder'], File["${vmbuilder_filepath}"],],
     }
 
     $projectregex = "s/${instanceproject}/_PROJECT_/g"
@@ -73,47 +61,54 @@ class labs_vmbuilder($vmbuilder_version) {
 
     exec { "cp /etc/security/access.conf ${vmbuilder_filepath}/access.conf":
         refreshonly => true,
-        subscribe   => File['vmbuilder_version'];
+        subscribe   => File['vmbuilder_version'],
     } ~>
+
     exec { "sed -i '${projectregex}' ${vmbuilder_filepath}/access.conf":
-        refreshonly => true;
+        refreshonly => true,
     }
 
     exec { "cp /etc/default/autofs ${vmbuilder_filepath}/default_autofs":
         refreshonly => true,
-        subscribe   => File['vmbuilder_version'];
+        subscribe   => File['vmbuilder_version'],
     } ~>
+
     exec { "sed -i '${projectregex}' ${vmbuilder_filepath}/default_autofs":
-        refreshonly => true;
+        refreshonly => true,
     }
 
     exec { "cp /etc/nslcd.conf ${vmbuilder_filepath}/nslcd.conf":
         refreshonly => true,
-        subscribe   => File['vmbuilder_version'];
+        subscribe   => File['vmbuilder_version'],
     } ~>
+
     exec { "sed -i '${projectregex}' ${vmbuilder_filepath}/nslcd.conf":
-        refreshonly => true;
+        refreshonly => true,
     }
 
     exec { "cp /etc/ldap/ldap.conf ${vmbuilder_filepath}/nss_ldap.conf":
         refreshonly => true,
-        subscribe   => File['vmbuilder_version'];
+        subscribe   => File['vmbuilder_version'],
     } ~>
+
     exec { "sed -i '${projectregex}' ${vmbuilder_filepath}/nss_ldap.conf":
-        refreshonly => true;
+        refreshonly => true,
     }
 
     exec { "cp /etc/puppet/puppet.conf ${vmbuilder_filepath}/puppet.conf":
         refreshonly => true,
-        subscribe   => File['vmbuilder_version'];
+        subscribe   => File['vmbuilder_version'],
     } ~>
+
     exec { "sed -i '${projectregex}' ${vmbuilder_filepath}/puppet.conf":
-        refreshonly => true;
+        refreshonly => true,
     } ~>
+
     exec { "sed -i '${fqdnregex}' ${vmbuilder_filepath}/puppet.conf":
-        refreshonly => true;
+        refreshonly => true,
     } ~>
+
     exec { "sed -i '${masterregex}' ${vmbuilder_filepath}/puppet.conf":
-        refreshonly => true;
+        refreshonly => true,
     }
 }
