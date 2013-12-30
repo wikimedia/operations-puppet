4535001ed34b0574f7f75c1333832c9edec950c1
Matanya Moses <matanya@foss.co.il>
2013-12-30 15:00:03 +0200

    ircecho: move to a module
    Change-Id: I9238a052734abcb3f7e4e8412d6b0b5429ebc2ae

---
+++ manifests/role/ircecho.pp
@@ -0,0 +1,7 @@
+class role::ircecho {
+    include ircecho,
+}
+	# bug 26784 - IRC bots process need nagios monitoring
+    monitor_service { "ircecho": description => "ircecho_service_running", check_command => "nrpe_check_ircecho" }
+}
+
