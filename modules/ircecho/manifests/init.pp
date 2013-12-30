610e209a21b9378112a81263d2a95aa7645ce72b
Matanya Moses <matanya@foss.co.il>
2013-12-30 15:00:03 +0200

    ircecho: move to a module
    Change-Id: I9238a052734abcb3f7e4e8412d6b0b5429ebc2ae

---
+++ modules/ircecho/manifests/init.pp
@@ -0,0 +1,31 @@
+# IRC echo class
+
+class ircecho {
+
+    # To use this class, you must define some variables; here's an example
+    # (leading hashes on channel names are added for you if missing):
+    #  $ircecho_logs = {
+    #    "/var/log/nagios/irc.log" =>
+    #    ["wikimedia-operations","#wikimedia-tech"],
+    #    "/var/log/nagios/irc2.log" => "#irc2",
+    #  }
+    #  $ircecho_nick = "nagios-wm"
+    #  $ircecho_server = "chat.freenode.net"
+
+    package { 'ircecho':
+        ensure => latest,
+    }
+
+    service { 'ircecho':
+        require => Package['ircecho'],
+        ensure  => running,
+    }
+
+    file { '/etc/default/ircecho':
+        require => Package['ircecho'],
+        content => template('ircecho/default.erb'),
+        owner   => 'root',
+        mode    => '0755',
+    }
+}
+
