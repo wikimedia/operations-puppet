dccf25e3fef6d647eb1271dc744d291fda19c05c
Antoine Musso <hashar@free.fr>
2013-12-30 21:21:31 +0100

    beta: sudo policy for refreshCdbJsonFiles
    /usr/local/bin/refreshCdbJsonFiles has been added to scap script but was
    lacking a sudo policy to let 'mwdeploy' user to execute it as
    'l10nupdate' user.
    
    Caused spam in cron mail recipients.
    
    Change-Id: Ib7fb882508423e976df92873708d1cc77eeabb10

--- manifests/misc/beta.pp
+++ manifests/misc/beta.pp
@@ -40,6 +40,7 @@ class misc::beta::autoupdater {
 	sudo_user { "mwdeploy" : privileges => [
 		'ALL = (l10nupdate) NOPASSWD:/usr/local/bin/mw-update-l10n',
 		'ALL = (l10nupdate) NOPASSWD:/usr/local/bin/mwscript',
+		'ALL = (l10nupdate) NOPASSWD:/usr/local/bin/refreshCdbJsonFiles',
 		# Some script running as mwdeploy explicily use "sudo -u mwdeploy"
 		# which makes Ubuntu to request a password. The following rule
 		# make sure we are not going to ask the password to mwdeploy when
