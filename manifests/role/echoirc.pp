ccc6c80de945d4943f63b27d64fc5e91a2dc66e5
Alexandros Kosiaris <akosiaris@wikimedia.org>
2013-12-31 14:12:54 +0200

    Migrate ircecho module's nrpe checks to nrpe module
    Use the new nrpe::monitor_service definition to clear up the nrpe
    process check. While at it also scope the template variables for
    ircecho module's default.erb
    
    Change-Id: I111d2e98571de271ee0a31638602578fc7662073

--- manifests/role/echoirc.pp
+++ manifests/role/echoirc.pp
@@ -4,6 +4,9 @@ class role::echoirc {
     system::role { 'ircecho': description => 'ircecho server' }
 
     # bug 26784 - IRC bots process need nagios monitoring
-    monitor_service { "ircecho": description => "ircecho_service_running", check_command => "nrpe_check_ircecho" }
+    nrpe::monitor_service { 'ircecho':
+        description  => 'ircecho_service_running',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:4 -c 1:20 -a ircecho',
+    }
 }
 
