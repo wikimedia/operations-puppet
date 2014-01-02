0202cafa93a0b1c45618f035df3d37e26c9cab4f
andrewbogott <abogott@wikimedia.org>
2014-01-02 14:47:03 -0600

    Add a global that turns off the ssh banner for a bastion.
    For bug 59183
    
    Change-Id: I2cc18ed922661cf31ef6dbedb849dac4d973c8b7

--- manifests/role/labsbastion.pp
+++ manifests/role/labsbastion.pp
@@ -3,11 +3,13 @@ class role::labs::bastion {
         description => "Labs bastion host (with mosh enabled)"
     }
 
-    file { '/etc/ssh/sshd_banner':
-        owner   => root,
-        group   => root,
-        mode    => '0444',
-        content => "\nIf you are having access problems, please see:https://labsconsole.wikimedia.org/wiki/Access#Accessing_public_and_private_instances\n",
+    if ! $::labs_bastion_banner_skip {
+        file { '/etc/ssh/sshd_banner':
+            owner   => root,
+            group   => root,
+            mode    => '0444',
+            content => "\nIf you are having access problems, please see:https://labsconsole.wikimedia.org/wiki/Access#Accessing_public_and_private_instances\n",
+        }
     }
 
     if versioncmp($::lsbdistrelease, "12.04") >= 0 {
