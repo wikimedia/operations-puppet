547d4c4f1a1c2e23d4db9aa7d5515da95ef9c738
Antoine Musso <hashar@free.fr>
2014-01-02 10:09:54 +0100

    applicationserver: pass puppetlint / retab
    Change-Id: I71ff89ec9943050dc9234ba1dd55102394441b9b

--- modules/applicationserver/manifests/service.pp
+++ modules/applicationserver/manifests/service.pp
@@ -1,35 +1,35 @@
 # applicationserver::service
 
 class applicationserver::service {
-	Class["applicationserver::apache_packages"] -> Class["applicationserver::service"]
-	Class["applicationserver::config::base"] -> Class["applicationserver::service"]
-	include mediawiki::sync
+    Class['applicationserver::apache_packages'] -> Class['applicationserver::service']
+    Class['applicationserver::config::base'] -> Class['applicationserver::service']
+    include mediawiki::sync
 
-	# Start apache but not at boot
-	service { 'apache':
-		name => "apache2",
-		enable => false,
-		subscribe => Exec['mw-sync'],
-		require => Exec['mw-sync'],
-		ensure => running;
-	}
+    # Start apache but not at boot
+    service { 'apache':
+        ensure    => running,
+        name      => 'apache2',
+        enable    => false,
+        subscribe => Exec['mw-sync'],
+        require   => Exec['mw-sync'],
+    }
 
-	# Sync the server when we see apache is not running
-	exec { 'apache-trigger-mw-sync':
-		command => '/bin/true',
-		notify => Exec['mw-sync'],
-		unless => "/bin/ps -C apache2 > /dev/null"
-	}
+    # Sync the server when we see apache is not running
+    exec { 'apache-trigger-mw-sync':
+        command => '/bin/true',
+        notify  => Exec['mw-sync'],
+        unless  => '/bin/ps -C apache2 > /dev/null'
+    }
 
-	# Has to be less than apache, and apache has to be nice 0 or less to be
-	# blue in ganglia.
-	if $::lsbdistid == "Ubuntu" and versioncmp($::lsbdistrelease, "12.04") >= 0 {
-		file { "/etc/init/ssh.override":
-			owner => root,
-			group => root,
-			mode => 0444,
-			content => "nice -10",
-			ensure => present;
-		}
-	}
+    # Has to be less than apache, and apache has to be nice 0 or less to be
+    # blue in ganglia.
+    if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '12.04') >= 0 {
+        file { '/etc/init/ssh.override':
+            ensure  => present,
+            owner   => root,
+            group   => root,
+            mode    => '0444',
+            content => 'nice -10',
+        }
+    }
 }
