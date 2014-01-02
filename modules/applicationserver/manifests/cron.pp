cc2d56e729949a591f7c445de4ca6270de78959b
Antoine Musso <hashar@free.fr>
2014-01-02 10:09:54 +0100

    applicationserver: pass puppetlint / retab
    Change-Id: I71ff89ec9943050dc9234ba1dd55102394441b9b

--- modules/applicationserver/manifests/cron.pp
+++ modules/applicationserver/manifests/cron.pp
@@ -1,16 +1,16 @@
 # application server required cron jobs
 class applicationserver::cron {
-	cron {
-		"cleanupipc":
-			command => "ipcs -s | grep apache | cut -f 2 -d \\  | xargs -rn 1 ipcrm -s",
-			user => root,
-			minute => 26,
-			ensure => present;
-		"cleantmpphp":
-			command => "find /tmp -name 'php*' -type f -ctime +1 -exec rm -f {} \\;",
-			user => root,
-			hour => 5,
-			minute => 0,
-			ensure => present;
-	}
+    cron { 'cleanupipc':
+        ensure  => present,
+        command => 'ipcs -s | grep apache | cut -f 2 -d \\  | xargs -rn 1 ipcrm -s',
+        user    => root,
+        minute  => 26,
+    }
+    cron { 'cleantmpphp':
+        ensure  => present,
+        command => 'find /tmp -name "php*" -type f -ctime +1 -exec rm -f {} \\;',
+        user    => root,
+        hour    => 5,
+        minute  => 0,
+    }
 }
--- modules/applicationserver/manifests/syslog.pp
+++ modules/applicationserver/manifests/syslog.pp
@@ -1,20 +1,20 @@
 # syslog instance and configuration for applicationservers
 class applicationserver::syslog( $apache_log_aggregator ) {
-	require base::remote-syslog
+    require base::remote-syslog
 
-	file {
-		"/etc/rsyslog.d/40-appserver.conf":
-			require => Package[rsyslog],
-			owner => root,
-			group => root,
-			mode => 0444,
-			content => template("rsyslog/40-appserver.conf.erb"),
-			ensure => present;
-		"/usr/local/bin/apache-syslog-rotate":
-			owner => root,
-			group => root,
-			mode => 0555,
-			source => "puppet:///files/misc/scripts/apache-syslog-rotate",
-			ensure => present;
-	}
+    file { '/etc/rsyslog.d/40-appserver.conf':
+        ensure  => present,
+        require => Package[rsyslog],
+        owner   => root,
+        group   => root,
+        mode    => '0444',
+        content => template('rsyslog/40-appserver.conf.erb'),
+    }
+    file { '/usr/local/bin/apache-syslog-rotate':
+        ensure => present,
+        owner  => root,
+        group  => root,
+        mode   => '0555',
+        source => 'puppet:///files/misc/scripts/apache-syslog-rotate',
+    }
 }
