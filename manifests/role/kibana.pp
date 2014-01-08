0d3293fd5bbaaf94b0be3918a176709973c0aab6
Bryan Davis <bd808@wikimedia.org>
2014-01-07 17:19:38 -0700

    Kibana puppet class
    Puppet class to install and configure Kibana. The class uses Trebuchet
    to deploy an internal mirror of the Kibana github project [0]. Apache
    and LDAP are used to provide authentication/authorization in front of
    the Elasticsearch cluster.
    
    [0]: https://github.com/elasticsearch/kibana
    
    Change-Id: I504c4c124c2bb01999b10a68c43f360aa2df4645

---
+++ manifests/role/kibana.pp
@@ -0,0 +1,48 @@
+# vim:sw=4 ts=4 sts=4 et:
+
+# == Class: role::kibana
+#
+# Provisions Kibana
+#
+class role::kibana {
+    include ::apache
+    include ::passwords::ldap::production
+
+    $hostname      = 'logstash.wikimedia.org'
+    $deploy_dir    = '/srv/deployment/kibana/kibana'
+    $es_host       = '127.0.0.1'
+    $es_port       = 9200
+    $ldap_authurl  = 'ldaps://virt1000.wikimedia.org virt0.wikimedia.org/ou=people,dc=wikimedia,dc=org?cn'
+    $ldap_bindpass = $passwords::ldap::production::proxypass
+    $ldap_binddn   = 'cn=proxyagent,ou=profile,dc=wikimedia,dc=org'
+    $ldap_group    = 'cn=wmf,ou=groups,dc=wikimedia,dc=org'
+    $auth_realm    = 'WMF Labs (use wiki login name not shell)'
+    $serveradmin   = 'root@wikimedia.org'
+
+    class { '::kibana': }
+
+    apache::mod { [
+        'alias',
+        'authnz_ldap',
+        'headers',
+        'proxy',
+        'proxy_http',
+    ]: }
+
+    file { "/etc/apache2/sites-available/${hostname}":
+        ensure  => present,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => template('kibana/apache.conf.erb'),
+        require => Package['httpd'],
+    }
+
+    file { "/etc/apache2/sites-enabled/${hostname}":
+        ensure  => link,
+        target  => "/etc/apache2/sites-available/${hostname}",
+        require => File["/etc/apache2/sites-available/${hostname}"],
+        notify  => Service['httpd'],
+    }
+}
+
