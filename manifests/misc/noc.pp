99d11112dc19412a9e9f28ebe878430c0f2745fe
Faidon Liambotis <faidon@wikimedia.org>
2014-01-09 01:36:26 +0200

    noc: remove ishmael & cleanup
    ishmael has been moved into a module, a role class and a different box
    (neon) for a while. Remove references to it from noc, and while at it,
    remove a couple of other dangling references (graphite, Rails/Sinatra)
    and some now-unused modules (LDAP, proxy, passenger).
    
    Note that since the class is applied to just one box and for simplicity,
    no ensure => absents are provided but these will be cleaned up manually.
    
    Change-Id: I02dc96298b75c4aa7cf9e46cb006c21e11cdad60

--- manifests/misc/noc.pp
+++ manifests/misc/noc.pp
@@ -3,12 +3,9 @@
 class misc::noc-wikimedia {
 	system::role { "misc::noc-wikimedia": description => "noc.wikimedia.org" }
 
-	package { [ "apache2", "libapache2-mod-php5", "libapache2-mod-passenger", "libsinatra-ruby", "rails" ]:
+	package { [ 'apache2', 'libapache2-mod-php5' ]:
 		ensure => latest;
 	}
-
-	include passwords::ldap::wmf_cluster
-	$proxypass = $passwords::ldap::wmf_cluster::proxypass
 
 	file {
 		"/etc/apache2/sites-available/noc.wikimedia.org":
@@ -18,12 +15,6 @@ class misc::noc-wikimedia {
 			owner => root,
 			group => root,
 			source => "puppet:///files/apache/sites/noc.wikimedia.org";
-		"/etc/apache2/sites-available/ishmael.wikimedia.org":
-			path => "/etc/apache2/sites-available/ishmael.wikimedia.org",
-			content => template('apache/sites/ishmael.wikimedia.org.erb'),
-			mode => 0440,
-			owner => root,
-			group => www-data;
 		"/usr/lib/cgi-bin":
 			source => "puppet:///files/cgi-bin/noc/",
 			recurse => true,
@@ -38,14 +29,9 @@ class misc::noc-wikimedia {
 	apache_module { php5: name => "php5" }
 	apache_module { userdir: name => "userdir" }
 	apache_module { cgi: name => "cgi" }
-	apache_module { ldap: name => "ldap" }
-	apache_module { authnz_ldap: name => "authnz_ldap" }
-	apache_module { proxy: name => "proxy" }
-	apache_module { proxy_http: name => "proxy_http" }
 	apache_module { ssl: name => "ssl" }
 
 	apache_site { noc: name => "noc.wikimedia.org" }
-	apache_site { graphiteproxy: name => "graphite.wikimedia.org" }
 
 	service { apache2:
 		require => [ Package[apache2], Apache_module[userdir], Apache_module[cgi], Apache_site[noc] ],
@@ -63,4 +49,3 @@ class misc::noc-wikimedia {
 		minute => "*";
 	}
 }
-
