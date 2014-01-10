6e854051cb779244f4dd95c2fa62e6d2fc1ad8e0
Faidon Liambotis <faidon@wikimedia.org>
2014-01-10 16:14:44 +0200

    Add librenms module and role class & apply it
    LibreNMS is a GPL Observium fork. Sets up Apache, user accounts, cron
    jobs and provisions via Trebuchet from an internal git mirror.
    
    Change-Id: I61073f915b8c5a5b26b28c60b6888563a268c6b1

---
+++ manifests/role/librenms.pp
@@ -0,0 +1,65 @@
+class role::librenms {
+    system::role { 'librenms': description => 'LibreNMS server' }
+
+    include network::constants
+    include passwords::mysql::librenms
+    include passwords::network
+
+    $hostname = 'librenms.wikimedia.org'
+
+    deployment::target { 'librenms': }
+    $install_dir = '/srv/deployment/librenms/librenms'
+
+    $config = {
+        'install_dir'      => $install_dir,
+        'html_dir'         => "${install_dir}/html",
+        'log_file'         => '/var/log/librenms.log',
+        'rrd_dir'          => '/srv/librenms/rrd',
+
+        'db_host'          => 'db1001.eqiad.wmnet',
+        'db_user'          => $passwords::mysql::librenms::user,
+        'db_pass'          => $passwords::mysql::librenms::pass,
+        'db_name'          => 'librenms',
+
+        'snmp'             => {
+            'community' => [ $passwords::network::snmp ],
+        },
+
+        'enable_inventory' => 1,
+        'enable_syslog'    => 1,
+        'email_backend'    => 'sendmail',
+        'alerts'           => {
+            'port_util_alert' => true,
+            'port_util_perc'  => 85,
+            'email' => {
+                'default' => 'noc@wikimedia.org',
+                'enable'  => true,
+            },
+            'port' => {
+                'ifdown'  => false,
+            },
+        },
+
+        'auth_mechanism'   => 'mysql',
+        'nets'             => $network::constants::external_networks,
+    }
+
+    class { 'librenms':
+        install_dir => $install_dir,
+        config      => $config,
+    }
+
+    @webserver::apache::module { 'php5': }
+    @webserver::apache::site { 'librenms.wikimedia.org':
+        docroot => $install_dir,
+        require => [
+            Webserver::Apache::Module['php5'],
+            Class['librenms'],
+        ],
+    }
+
+    monitor_service { 'librenms':
+        description   => 'HTTP',
+        check_command => "check_http_url!${hostname}!http://${hostname}",
+    }
+}
---
+++ modules/librenms/lib/puppet/parser/functions/phpdump.rb
@@ -0,0 +1,34 @@
+# == Function: phpdump
+#
+# Serialize a hash into PHP array with lexicographically sorted keys.
+#
+
+def phpdump(o, level=1)
+  indent = " "*4
+
+  case o
+  when Hash
+    contents = ''
+    o.sort.each do |k, v|
+      contents += indent*level
+      contents += "\"#{k}\" => " + phpdump(v, level+1)
+      contents += ",\n"
+    end
+    "array(\n" + contents + indent*(level-1) + ")"
+  when Array
+    "array(" + o.map { |x| phpdump(x, level+1) }.join(', ') + ")"
+  when TrueClass
+    "TRUE"
+  when FalseClass
+    "FALSE"
+  else
+    '"' + o.to_s + '"'
+  end
+end
+
+module Puppet::Parser::Functions
+  newfunction(:phpdump, :type => :rvalue) do |args|
+    fail 'phpdump() requires an argument' if args.empty?
+    phpdump(args.inject(:merge))
+  end
+end
