1e867bd4e2be02059d25c3c0cfbbbc264dc62973
RobH <rob@wikimedia.org>
2014-01-10 12:38:49 -0800

    star.wikimedia.org cert chain fix
    the star.wikimedia.org certificate was recently reissued, this has
    caused the certificate chain to break.  I'm not sure how it worked
    before this point, since we reissued, not replaced the cert.  However,
    it is rapidssl, so this should fix it.
    
    bug: 59910
    Change-Id: I1ba6ab4461f94a6cb199b19615be126bde7a3b07

--- manifests/certs.pp
+++ manifests/certs.pp
@@ -132,9 +132,9 @@ define install_certificate( $group="ssl-cert", $ca="", $privatekey=true ) {
 		# have SSL enabled.
 		$cas = $name ? {
 			"unified.wikimedia.org" => "DigiCertHighAssuranceCA-3.pem",
-			"star.wikimedia.org" => "Equifax_Secure_CA.pem",
+			"star.wikimedia.org" => "Equifax Secure Certificate Authority",
 			"star.wikipedia.org" => "DigiCertHighAssuranceCA-3.pem DigiCert_High_Assurance_EV_Root_CA.pem",
-			"star.wiktionary.org" => "RapidSSL_CA.pem GeoTrust_Global_CA.pem",
+			"star.wiktionary.org" => "Equifax Secure Certificate Authority",
 			"star.wikiquote.org" => "RapidSSL_CA.pem GeoTrust_Global_CA.pem",
 			"star.wikibooks.org" => "RapidSSL_CA.pem GeoTrust_Global_CA.pem",
 			"star.wikisource.org" => "RapidSSL_CA.pem GeoTrust_Global_CA.pem",
