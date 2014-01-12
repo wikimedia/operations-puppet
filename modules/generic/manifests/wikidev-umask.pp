e7ab563337d13539095a4e1f5079c2a77eda704a
Matanya Moses <matanya@foss.co.il>
2014-01-12 16:56:54 +0200

    generic: lint clean
    Change-Id: Idaf1638068fc189d8f69be641bb211a419427a34

--- modules/generic/manifests/wikidev-umask.pp
+++ modules/generic/manifests/wikidev-umask.pp
@@ -1,12 +1,10 @@
 class generic::wikidev-umask {
-
-	# set umask to 0002 for wikidev users, per RT-804
-	file {
-		"/etc/profile.d/umask-wikidev.sh":
-			ensure => present,
-			owner => root,
-			group => root,
-			mode => 0444,
-			source => "puppet:///modules/generic/environment/umask-wikidev-profile-d.sh";
-	}
+    # set umask to 0002 for wikidev users, per RT-804
+    file { '/etc/profile.d/umask-wikidev.sh':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/generic/environment/umask-wikidev-profile-d.sh',
+    }
 }
