4887dc90a227227051a30103a664975959b65ddb
Matanya Moses <matanya@foss.co.il>
2014-01-12 16:56:54 +0200

    generic: lint clean
    Change-Id: Idaf1638068fc189d8f69be641bb211a419427a34

--- modules/generic/manifests/higher_min_free_kbytes.pp
+++ modules/generic/manifests/higher_min_free_kbytes.pp
@@ -1,11 +1,9 @@
 class generic::higher_min_free_kbytes {
-	# Set a high min_free_kbytes watermark.
-	# See https://wikitech.wikimedia.org/wiki/Dataset1001#Feb_8_2012
-	# FIXME: Is this setting appropriate to the nodes on which it is applied? Is
-	# the value optimal? Investigate.
-	sysctl::parameters { 'higher_min_free_kbytes':
-		values => {
-			'vm.min_free_kbytes' => 1024 * 256,
-		},
-	}
+# Set a high min_free_kbytes watermark.
+# See https://wikitech.wikimedia.org/wiki/Dataset1001#Feb_8_2012
+# FIXME: Is this setting appropriate to the nodes on which it is applied? Is
+# the value optimal? Investigate.
+    sysctl::parameters { 'higher_min_free_kbytes':
+        values => { 'vm.min_free_kbytes' => 1024 * 256,},
+    }
 }
