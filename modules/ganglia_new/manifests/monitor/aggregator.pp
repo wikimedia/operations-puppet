8c91039191bac90d94471ea0c160c2f4f11bc147
Matanya Moses <matanya@foss.co.il>
2014-01-13 13:26:53 +0200

    ganglia_new: lint clean
    Change-Id: I7753818aaea8f3a82a75e3969efedd41b02ce2ed

--- modules/ganglia_new/manifests/monitor/aggregator.pp
+++ modules/ganglia_new/manifests/monitor/aggregator.pp
@@ -1,40 +1,43 @@
 class ganglia_new::monitor::aggregator($sites) {
-	require ganglia_new::monitor::packages
-	include ganglia_new::configuration
+    require ganglia_new::monitor::packages
+    include ganglia_new::configuration
 
-	system::role { "ganglia::monitor::aggregator": description => "central Ganglia aggregator" }
+    system::role { 'ganglia::monitor::aggregator': description => 'central Ganglia aggregator' }
 
-	file {
-		"/etc/ganglia/aggregators":
-			ensure => directory,
-			mode => 0555;
-		"/etc/init/ganglia-monitor-aggregator.conf":
-			owner  => 'root',
-			group  => 'root',
-			mode   => '0444',
-			source => "puppet:///modules/${module_name}/upstart/ganglia-monitor-aggregator.conf",
-			before => Service['ganglia-monitor-aggregator'];
-		"/etc/init/ganglia-monitor-aggregator-instance.conf":
-			owner  => 'root',
-			group  => 'root',
-			mode   => '0444',
-			source => "puppet:///modules/${module_name}/upstart/ganglia-monitor-aggregator-instance.conf",
-			before => Service['ganglia-monitor-aggregator'];
-	}
+    file { '/etc/ganglia/aggregators':
+        ensure => directory,
+        mode   => '0555',
+    }
 
-	generic::upstart_job { "ganglia-monitor-aggregator-instance": }
+    file { '/etc/init/ganglia-monitor-aggregator.conf':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => "puppet:///modules/${module_name}/upstart/ganglia-monitor-aggregator.conf",
+        before => Service['ganglia-monitor-aggregator'],
+    }
 
-	define site_instances() {
-		# Instantiate aggregators for all clusters for this site ($title)
-		$cluster_list = suffix(keys($ganglia_new::configuration::clusters), "_${title}")
-		instance{ $cluster_list: monitored_site => $title }
-	}
+    file { '/etc/init/ganglia-monitor-aggregator-instance.conf':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => "puppet:///modules/${module_name}/upstart/ganglia-monitor-aggregator-instance.conf",
+        before => Service['ganglia-monitor-aggregator'],
+    }
 
-	site_instances{ $sites: }
+    generic::upstart_job { 'ganglia-monitor-aggregator-instance': }
 
-	service { "ganglia-monitor-aggregator":
-		provider => upstart,
-		name => "ganglia-monitor-aggregator",
-		ensure => running
-	}
+    define site_instances() {
+        # Instantiate aggregators for all clusters for this site ($title)
+        $cluster_list = suffix(keys($ganglia_new::configuration::clusters), "_${title}")
+        instance{ $cluster_list: site => $title }
+    }
+
+    site_instances{ $sites: }
+
+    service { 'ganglia-monitor-aggregator':
+        ensure   => running,
+        provider => 'upstart',
+        name     => 'ganglia-monitor-aggregator',
+    }
 }
