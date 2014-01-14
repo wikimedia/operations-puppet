63328566badbf761a304f32e75b8e634ada86b88
Matanya Moses <matanya@foss.co.il>
2014-01-14 10:33:53 +0200

    download: lint clean
    Change-Id: If7777f31abb96b8cae2d65d3a6ab0a1edda467c6

--- modules/download/manifests/mediawiki.pp
+++ modules/download/manifests/mediawiki.pp
@@ -1,6 +1,6 @@
 class download::mediawiki {
 
-    system::role { "download::mediawiki": description => "MediaWiki download" }
+    system::role { 'download::mediawiki': description => 'MediaWiki download' }
 
     package { 'apache':
         ensure => present,
@@ -8,24 +8,24 @@ class download::mediawiki {
 
     file { '/etc/apache2/sites-available/download.mediawiki.org':
         mode   => '0444',
-        owner  => root,
-        group  => root,
+        owner  => 'root',
+        group  => 'root',
         source => 'puppet:///modules/downloads/apache/download.mediawiki.org',
     }
 
     file { '/srv/org/mediawiki':
         ensure => directory,
-        owner  => root,
-        group  => root,
+        owner  => 'root',
+        group  => 'root',
         mode   => '0775',
     }
 
     file { '/srv/org/mediawiki/download':
         ensure => directory,
-        owner  => mwdeploy,
-        group  => mwdeploy,
+        owner  => 'mwdeploy',
+        group  => 'mwdeploy',
         mode   => '0775',
     }
 
-    apache_site { "download.mediawiki.org": name => "download.mediawiki.org" }
+    apache_site { 'download.mediawiki.org': name => 'download.mediawiki.org' }
 }
