dbc8f63ba36dd7335dd5b8beadea6f4f9054087a
Matanya Moses <matanya@foss.co.il>
2014-01-14 10:33:53 +0200

    download: lint clean
    Change-Id: If7777f31abb96b8cae2d65d3a6ab0a1edda467c6

--- modules/download/manifests/mirror.pp
+++ modules/download/manifests/mirror.pp
@@ -1,27 +1,29 @@
 class download::mirror {
-    system::role { "download::mirror": description => "Service for rsync to external download mirrors" }
+    system::role { 'download::mirror': description => 'Service for rsync to external download mirrors' }
 
     include role::mirror::common
 
     file { '/etc/rsyncd.conf':
         mode    => '0444',
-        owner   => root,
-        group   => root,
+        owner   => 'root',
+        group   => 'root',
         source  => 'puppet:///modules/download/rsync/rsyncd.conf.downloadmirror',
         require => Package['rsync'],
     }
 
     file { '/etc/default/rsync':
         mode    => '0444',
-        owner   => root,
-        group   => root,
+        owner   => 'root',
+        group   => 'root',
         source  => 'puppet:///modules/download/rsync/rsync.default.downloadmirror',
         require => Package['rsync'],
     }
 
     service { 'rsync':
         ensure  => running,
-        require => [ Package['rsync'], File['/etc/rsyncd.conf'],
-File['/etc/default/rsync'] ],
+        require => [Package['rsync'],
+                    File['/etc/rsyncd.conf'],
+                    File['/etc/default/rsync']
+                    ],
     }
 }
