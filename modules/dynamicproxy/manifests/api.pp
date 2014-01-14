114cb20071b787b97c3483a821376674eb2a2b08
Matanya Moses <matanya@foss.co.il>
2014-01-14 10:18:55 +0200

    dynamicproxy: lint clean
    Change-Id: Id80ecd1fd1a4b5d7eea1bfb9d4cee874353ea970

--- modules/dynamicproxy/manifests/api.pp
+++ modules/dynamicproxy/manifests/api.pp
@@ -1,70 +1,74 @@
 class dynamicproxy::api {
     file { '/etc/nginx/sites-available/api':
-        ensure => 'file',
+        ensure  => 'file',
         content => template('dynamicproxy/api.conf'),
         require => Package['nginx-extras'],
-        notify => Service['nginx']
+        notify  => Service['nginx'],
     }
 
     file { '/etc/nginx/sites-enabled/api':
-        ensure => 'link',
-        target => '/etc/nginx/sites-available/api',
-        require => File['/etc/nginx/sites-available/api']
+        ensure  => 'link',
+        target  => '/etc/nginx/sites-available/api',
+        require => File['/etc/nginx/sites-available/api'],
     }
 
     package { 'python-flask':
-        ensure => 'latest',
+        ensure  => 'latest',
         require => Class['misc::labsdebrepo'],
     }
 
     package { ['python-invisible-unicorn', 'python-flask-sqlalchemy', 'uwsgi', 'uwsgi-plugin-python']:
-        ensure => 'present',
+        ensure  => 'present',
         require => Package['python-flask'],
     }
 
     generic::upstart_job{ 'dynamicproxy-api':
         require => Package['python-invisible-unicorn', 'python-flask-sqlalchemy', 'redis', 'python-flask', 'uwsgi', 'uwsgi-plugin-python'],
-        install => 'true',
-        start   => 'true'
+        install => true,
+        start   => true,
     }
 
-    file {
-        '/etc/dynamicproxy-api':
+    file { '/etc/dynamicproxy-api':
             ensure => 'directory',
             owner  => 'www-data',
-            group  => 'www-data';
-        '/data/project/backup':
+            group  => 'www-data',
+    }
+
+    file { '/data/project/backup':
             ensure => 'directory',
             owner  => 'root',
             group  => 'root',
-            mode   => '755';
-        '/data/project/backup/README':
-            source => 'puppet:///modules/dynamicproxy/BackupReadme',
-            require => File['/data/project/backup'];
-        '/usr/local/sbin/proxydb-bak.sh':
-            mode => '555',
-            owner => root,
-            group => root,
+            mode   => '0755',
+    }
+
+    file { '/data/project/backup/README':
+            source  => 'puppet:///modules/dynamicproxy/BackupReadme',
+            require => File['/data/project/backup'],
+    }
+
+    file { '/usr/local/sbin/proxydb-bak.sh':
+            mode   => '0555',
+            owner  => 'root',
+            group  => 'root',
             source => 'puppet:///modules/dynamicproxy/proxydb-bak.sh',
     }
 
-    cron {
-        'proxydb-bak':
+    cron { 'proxydb-bak':
             ensure  => present,
-            user    => root,
+            user    => 'root',
             hour    => 1,
             minute  => 0,
             command => '/usr/local/sbin/proxydb-bak.sh > /dev/null 2>&1',
-            require => File["/data/project/backup"];
+            require => File['/data/project/backup'],
     }
 
     # Create initial db file if it doesn't exist, but don't clobber if it does.
     file { '/etc/dynamicproxy-api/data.db':
-        ensure => 'file',
-        source => 'puppet:///modules/dynamicproxy/initial-data.db',
+        ensure  => 'file',
+        source  => 'puppet:///modules/dynamicproxy/initial-data.db',
         replace => false,
         require => File['/etc/dynamicproxy-api'],
-        owner => 'www-data',
-        group => 'www-data',
+        owner   => 'www-data',
+        group   => 'www-data',
     }
 }
--- modules/dynamicproxy/manifests/init.pp
+++ modules/dynamicproxy/manifests/init.pp
@@ -1,61 +1,61 @@
 class dynamicproxy (
-    $redis_maxmemory="512MB",
+    $redis_maxmemory='512MB',
     $ssl_certificate_name=false,
     $notfound_servers=[],
-    $luahandler="domainproxy.lua",
+    $luahandler='domainproxy.lua',
     $set_xff=false
 ) {
     class { '::redis':
-        persist   => "aof",
-        dir       => "/var/lib/redis",
+        persist   => 'aof',
+        dir       => '/var/lib/redis',
         maxmemory => $redis_maxmemory,
     }
 
     include misc::labsdebrepo
 
     package { 'nginx-extras':
-        ensure => latest,
+        ensure  => latest,
         require => Class['misc::labsdebrepo'],
-     }
+    }
 
     service { 'nginx':
         ensure  => 'running',
-        require => Package['nginx-extras']
+        require => Package['nginx-extras'],
     }
 
     file { '/etc/nginx/sites-available/default':
         ensure  => 'file',
         content => template('dynamicproxy/proxy.conf'),
         require => Package['nginx-extras'],
-        notify  => Service['nginx']
+        notify  => Service['nginx'],
     }
 
     file { '/etc/nginx/sites-enabled/default':
-        ensure => 'link',
-        target => '/etc/nginx/sites-available/default',
-        require => File['/etc/nginx/sites-available/default']
+        ensure  => 'link',
+        target  => '/etc/nginx/sites-available/default',
+        require => File['/etc/nginx/sites-available/default'],
     }
 
     file { '/etc/nginx/lua':
         ensure  => 'directory',
-        require => Package['nginx-extras']
+        require => Package['nginx-extras'],
     }
 
     file { '/etc/nginx/lua/proxy.lua':
         ensure  => 'file',
-        source  => "puppet:///modules/dynamicproxy/$luahandler",
+        source  => "puppet:///modules/dynamicproxy/${luahandler}",
         require => File['/etc/nginx/lua'],
-        notify  => Service['nginx']
+        notify  => Service['nginx'],
     }
 
     file { '/etc/nginx/lua/resty':
         ensure  => 'directory',
-        require => File['/etc/nginx/lua']
+        require => File['/etc/nginx/lua'],
     }
 
     file { '/etc/nginx/lua/resty/redis.lua':
         ensure  => 'file',
         require => File['/etc/nginx/lua/resty'],
-        source  => 'puppet:///modules/dynamicproxy/redis.lua'
+        source  => 'puppet:///modules/dynamicproxy/redis.lua',
     }
 }
