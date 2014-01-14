a5abf7ffc57f843ace7655b3f4973d183ff7307f
Matanya Moses <matanya@foss.co.il>
2014-01-14 11:24:20 +0200

    deployment: lint clean
    Change-Id: I8019ef99cd804b28bb89434d2985eea9302edf2e

--- modules/deployment/manifests/salt_master.pp
+++ modules/deployment/manifests/salt_master.pp
@@ -1,120 +1,155 @@
 class deployment::salt_master(
-  $state_dir="/srv/salt",
-  $runner_dir="/srv/runners",
-  $pillar_dir="/srv/pillars",
-  $module_dir="/srv/salt/_modules",
-  $returner_dir="/srv/salt/_returners",
-  $repo_config,
-  $deployment_config
-  ) {
-  file {
-    "/etc/salt/deploy_runner.conf":
-      content => template("deployment/deploy_runner.conf.erb"),
-      mode => 0444,
-      owner => root,
-      group => root,
-      require => [Package["salt-master"]];
-    "${state_dir}/deploy":
-      ensure => directory,
-      mode => 0555,
-      owner => root,
-      group => root,
-      require => [File["${state_dir}"]];
-    "${state_dir}/top.sls":
-      source => "puppet:///deployment/states/top.sls",
-      mode => 0444,
-      owner => root,
-      group => root,
-      require => [File["${state_dir}/deploy"]];
-    "${state_dir}/deploy/sync_all.sls":
-      source => "puppet:///deployment/states/deploy/sync_all.sls",
-      mode => 0444,
-      owner => root,
-      group => root,
-      require => [File["${state_dir}/deploy"]];
-    "${runner_dir}/deploy.py":
-      source => "puppet:///deployment/runners/deploy.py",
-      mode => 0555,
-      owner => root,
-      group => root,
-      require => File["${runner_dir}"];
-    "${pillar_dir}/deployment":
-      ensure => directory,
-      mode => 0555,
-      owner => root,
-      group => root,
-      require => [File["${pillar_dir}"]];
-    "${pillar_dir}/deployment/repo_config.sls":
-      content => ordered_json({'repo_config' => $repo_config }),
-      mode => 0444,
-      owner => root,
-      group => root,
-      require => [File["${pillar_dir}/deployment"]];
-    "${pillar_dir}/deployment/deployment_config.sls":
-      content => ordered_json({'deployment_config' => $deployment_config}),
-      mode => 0444,
-      owner => root,
-      group => root,
-      require => [File["${pillar_dir}/deployment"]];
-    "${pillar_dir}/top.sls":
-      content => template("deployment/pillars/top.sls.erb"),
-      mode => 0444,
-      owner => root,
-      group => root,
-      require => [File["${pillar_dir}"]];
-    "${module_dir}/deploy.py":
-      source => "puppet:///deployment/modules/deploy.py",
-      mode => 0555,
-      owner => root,
-      group => root,
-      require => [File["${module_dir}"]];
-    "${returner_dir}/deploy_redis.py":
-      source => "puppet:///deployment/returners/deploy_redis.py",
-      mode => 0555,
-      owner => root,
-      group => root,
-      require => [File["${returner_dir}"]];
-    "${module_dir}/parsoid.py":
-      source => "puppet:///deployment/modules/parsoid.py",
-      mode => 0555,
-      owner => root,
-      group => root,
-      require => [File["${module_dir}"]];
-    "${module_dir}/mwprof.py":
-      source => "puppet:///deployment/modules/mwprof.py",
-      mode => 0555,
-      owner => root,
-      group => root,
-      require => [File["${module_dir}"]];
-    "${module_dir}/mediawiki.py":
-      source => "puppet:///deployment/modules/mediawiki.py",
-      mode => 0555,
-      owner => root,
-      group => root,
-      require => [File["${module_dir}"]];
-  }
+    $state_dir='/srv/salt',
+    $runner_dir='/srv/runners',
+    $pillar_dir='/srv/pillars',
+    $module_dir='/srv/salt/_modules',
+    $returner_dir='/srv/salt/_returners',
+    $repo_config,
+    $deployment_config
+    ) {
+    file { '/etc/salt/deploy_runner.conf':
+        content => template('deployment/deploy_runner.conf.erb'),
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        require => [Package['salt-master']],
+    }
+
+    file { "${state_dir}/deploy":
+        ensure  => directory,
+        mode    => '0555',
+        owner   => 'root',
+        group   => 'root',
+        require => [File[$state_dir]],
+    }
+
+    file { "${state_dir}/top.sls":
+        source  => 'puppet:///deployment/states/top.sls',
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        require => [File["${state_dir}/deploy"]],
+    }
+
+    file { "${state_dir}/deploy/sync_all.sls":
+        source  => 'puppet:///deployment/states/deploy/sync_all.sls',
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        require => [File["${state_dir}/deploy"]],
+    }
+
+    file { "${runner_dir}/deploy.py":
+        source  => 'puppet:///deployment/runners/deploy.py',
+        mode    => '0555',
+        owner   => 'root',
+        group   => 'root',
+        require => File[$runner_dir],
+    }
+
+    file { "${pillar_dir}/deployment":
+        ensure  => directory,
+        mode    => '0555',
+        owner   => 'root',
+        group   => 'root',
+        require => [File[$pillar_dir]],
+    }
+
+    file { "${pillar_dir}/deployment/repo_config.sls":
+        content => ordered_json({'repo_config' => $repo_config }),
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        require => [File["${pillar_dir}/deployment"]],
+    }
+
+    file { "${pillar_dir}/deployment/deployment_config.sls":
+        content => ordered_json({'deployment_config' => $deployment_config}),
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        require => [File["${pillar_dir}/deployment"]],
+    }
+
+    file { "${pillar_dir}/top.sls":
+        content => template('deployment/pillars/top.sls.erb'),
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        require => [File[$pillar_dir]],
+    }
+
+    file { "${module_dir}/deploy.py":
+        source  => 'puppet:///deployment/modules/deploy.py',
+        mode    => '0555',
+        owner   => 'root',
+        group   => 'root',
+        require => [File[$module_dir]],
+    }
+
+    file { "${returner_dir}/deploy_redis.py":
+        source  => 'puppet:///deployment/returners/deploy_redis.py',
+        mode    => '0555',
+        owner   => 'root',
+        group   => 'root',
+        require => [File[$returner_dir]],
+    }
+
+    file { "${module_dir}/parsoid.py":
+        source  => 'puppet:///deployment/modules/parsoid.py',
+        mode    => '0555',
+        owner   => 'root',
+        group   => 'root',
+        require => [File[$module_dir]],
+    }
+
+    file { "${module_dir}/mwprof.py":
+        source  => 'puppet:///deployment/modules/mwprof.py',
+        mode    => '0555',
+        owner   => 'root',
+        group   => 'root',
+        require => [File[$module_dir]],
+    }
+
+    file { "${module_dir}/mediawiki.py":
+        source  => 'puppet:///deployment/modules/mediawiki.py',
+        mode    => '0555',
+        owner   => 'root',
+        group   => 'root',
+        require => [File[$module_dir]],
+    }
 
   # If pillars or modules change, we need to sync them with the minions
-  exec {
-    "refresh_deployment_pillars":
-      command => "/usr/bin/salt -C 'G@deployment_server:true or G@deployment_target:*' saltutil.refresh_pillar",
-      subscribe => [File["${pillar_dir}/deployment/deployment_config.sls"], File["${pillar_dir}/deployment/repo_config.sls"], File["${pillar_dir}"]],
-      refreshonly => true,
-      require => [Package["salt-master"]];
-    "deployment_server_init":
-      command => "/usr/bin/salt -G 'deployment_server:true' deploy.deployment_server_init",
-      subscribe => [Exec['refresh_deployment_pillars']],
-      refreshonly => true,
-      require => [File["${module_dir}/deploy.py"]];
-    "refresh_deployment_modules":
-      command => "/usr/bin/salt -G 'deployment_target:*' saltutil.sync_modules",
-      subscribe => [File["${module_dir}/deploy.py"], File["${module_dir}/parsoid.py"], File["${module_dir}/mwprof.py"], File["${module_dir}/mediawiki.py"]],
-      refreshonly => true,
-      require => [Package["salt-master"]];
-    "refresh_deployment_returners":
-      command => "/usr/bin/salt -G 'deployment_target:*' saltutil.sync_returners",
-      subscribe => [File["${returner_dir}/deploy_redis.py"]],
-      refreshonly => true,
-      require => [Package["salt-master"]];
+    exec { 'refresh_deployment_pillars':
+        command     => "/usr/bin/salt -C 'G@deployment_server:true or G@deployment_target:*' saltutil.refresh_pillar",
+        subscribe   => [File["${pillar_dir}/deployment/deployment_config.sls"],
+                        File["${pillar_dir}/deployment/repo_config.sls"],
+                        File[$pillar_dir]],
+        refreshonly => true,
+        require     => [Package['salt-master']],
+    }
+
+    exec { 'deployment_server_init':
+        command     => "/usr/bin/salt -G 'deployment_server:true' deploy.deployment_server_init",
+        subscribe   => [Exec['refresh_deployment_pillars']],
+        refreshonly => true,
+        require     => [File["${module_dir}/deploy.py"]],
+    }
+
+    exec { 'refresh_deployment_modules':
+        command     => "/usr/bin/salt -G 'deployment_target:*' saltutil.sync_modules",
+        subscribe   => [File["${module_dir}/deploy.py"],
+                        File["${module_dir}/parsoid.py"],
+                        File["${module_dir}/mwprof.py"],
+                        File["${module_dir}/mediawiki.py"]],
+        refreshonly => true,
+        require     => [Package['salt-master']],
+    }
+
+    exec { 'refresh_deployment_returners':
+        command     => "/usr/bin/salt -G 'deployment_target:*' saltutil.sync_returners",
+        subscribe   => [File["${returner_dir}/deploy_redis.py"]],
+        refreshonly => true,
+        require     => [Package['salt-master']],
   }
 }
--- modules/deployment/manifests/target.pp
+++ modules/deployment/manifests/target.pp
@@ -1,36 +1,36 @@
 define deployment::target($ensure=present) {
     salt::grain { "deployment_target_${name}":
         ensure => $ensure,
-        grain  => "deployment_target",
+        grain  => 'deployment_target',
         value  => $name,
         notify => [
-            Exec["deployment_target_sync_all"],
-            Exec["deployment_target_refresh_pillars"],
-            Exec["deployment_target_deploy_all"],
+            Exec['deployment_target_sync_all'],
+            Exec['deployment_target_refresh_pillars'],
+            Exec['deployment_target_deploy_all'],
         ];
     }
-    if ! defined(Exec["deployment_target_sync_all"]){
-        exec { "deployment_target_sync_all":
+    if ! defined(Exec['deployment_target_sync_all']){
+        exec { 'deployment_target_sync_all':
             refreshonly => true,
-            path        => ["/usr/bin"],
-            command     => "salt-call saltutil.sync_all";
+            path        => ['/usr/bin'],
+            command     => 'salt-call saltutil.sync_all';
         }
     }
-    if ! defined(Exec["deployment_target_refresh_pillars"]){
-        exec { "deployment_target_refresh_pillars":
+    if ! defined(Exec['deployment_target_refresh_pillars']){
+        exec { 'deployment_target_refresh_pillars':
             refreshonly => true,
-            path        => ["/usr/bin"],
-            command     => "salt-call saltutil.refresh_pillar";
+            path        => ['/usr/bin'],
+            command     => 'salt-call saltutil.refresh_pillar';
         }
     }
-    if ! defined(Exec["deployment_target_deploy_all"]){
-        exec { "deployment_target_deploy_all":
+    if ! defined(Exec['deployment_target_deploy_all']){
+        exec { 'deployment_target_deploy_all':
             refreshonly => true,
-            path        => ["/usr/bin"],
-            command     => "salt-call deploy.sync_all",
+            path        => ['/usr/bin'],
+            command     => 'salt-call deploy.sync_all',
             require     => [
-                Exec["deployment_target_sync_all"],
-                Exec["deployment_target_refresh_pillars"],
+                Exec['deployment_target_sync_all'],
+                Exec['deployment_target_refresh_pillars'],
             ];
         }
     }
