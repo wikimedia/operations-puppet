f8056af991663a1731a129182a2db211866ed2be
Joel Krauska <jkrauska@wikimedia.org>
2014-01-14 20:47:33 +0000

    Initial commit of pmacct module Initial commit of pmacct, with Leslie's comments addressed. Now with Matanya and Jeremyb's comments addressed. Change-Id: I8bb1cfb204d8b7fcf775a549d004edb72f039edf

---
+++ modules/pmacct/manifests/makeconfig.pp
@@ -0,0 +1,21 @@
+# pmacct::makeconfig
+# Generates a unique config file per device
+
+define pmacct::makeconfig ($name, $port, $ip, $samplerate) {
+    # Single confile file per device
+    file { "${pmacct::home}/configs/config-${name}.cfg":
+        ensure  => 'file',
+        owne r  => 'pmacct',
+        grou p  => 'pmacct',
+        mode    => '0750',
+        content => template('pmacct/config.erb'),
+        require => File [ "${pmacct::home}/configs" ],
+    }
+
+    # Corresponding ferm rule for firewall redirect
+    ferm::rule {"${name}-BGP":
+        rule  => "proto tcp dport 179 source ${ip} REDIRECT to-ports ${port}",
+        table => 'nat',
+        chain => 'PREROUTING',
+    }
+}
