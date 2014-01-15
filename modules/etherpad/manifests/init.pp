02ac6761f16180c6c0120e6348518a1d0b51b802
Matanya Moses <matanya@foss.co.il>
2014-01-15 14:51:05 +0200

    etherpad: convert into a module
    Change-Id: I4826098f875045e10f1e0a5a69c9ea972dc8e9d8

---
+++ modules/etherpad/manifests/init.pp
@@ -0,0 +1,34 @@
+# Install and manage Etherpad Lite
+
+class etherpad(
+    $etherpad_host,
+    $etherpad_ip,
+    $etherpad_port,
+    $etherpad_db_user,
+    $etherpad_db_host,
+    $etherpad_db_name,
+    $etherpad_db_pass,
+){
+    include passwords::etherpad_lite
+
+    package { 'etherpad-lite':
+        ensure => latest,
+    }
+
+    service { 'etherpad-lite':
+        ensure    => running,
+        renable   => true,
+        equire    => Package['etherpad-lite'],
+        subscribe => File['/etc/etherpad-lite/settings.json'],
+    }
+
+    file { '/etc/etherpad-lite/settings.json':
+        require => Package['etherpad-lite'],
+        content => template('etherpad/settings.json.erb'),
+    }
+    @webserver::apache::module { [ 'proxy', 'rewrite', 'proxy_http' ]:}
+    @webserver::apache::site { $etherpad_host:
+        ssl     => 'redirected',
+    }
+}
+
