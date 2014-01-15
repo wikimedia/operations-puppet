91ba94c62f7a5ed0bf10303f8eb10eeb2cb09358
Matanya Moses <matanya@foss.co.il>
2014-01-15 14:51:05 +0200

    etherpad: convert into a module
    Use webserver includes and aliases functions, include webserver in order
    to realize the webserver resources
    
    Change-Id: I4826098f875045e10f1e0a5a69c9ea972dc8e9d8

---
+++ modules/etherpad/manifests/init.pp
@@ -0,0 +1,28 @@
+# Install and manage Etherpad Lite
+
+class etherpad(
+            $etherpad_host,
+            $etherpad_ip,
+            $etherpad_port,
+            $etherpad_db_user,
+            $etherpad_db_host,
+            $etherpad_db_name,
+            $etherpad_db_pass,
+            ){
+
+    package { 'etherpad-lite':
+        ensure => 'latest',
+    }
+
+    service { 'etherpad-lite':
+        ensure    => running,
+        enable    => true,
+        require   => Package['etherpad-lite'],
+        subscribe => File['/etc/etherpad-lite/settings.json'],
+    }
+
+    file { '/etc/etherpad-lite/settings.json':
+        require => Package['etherpad-lite'],
+        content => template('etherpad/settings.json.erb'),
+    }
+}
