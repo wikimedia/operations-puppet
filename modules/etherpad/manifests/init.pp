34fcd343e1e9a8c37246a98c7aab5e39d9b585b8
Matanya Moses <matanya@foss.co.il>
2014-01-15 14:51:05 +0200

    etherpad: convert into a module
    Some heavy refactoring to achieve this.
    Use the new ::apache module definitions
    Reorganize into a module and role class
    Drop server alias support
    Update to the latest coding standards
    
    Change-Id: I4826098f875045e10f1e0a5a69c9ea972dc8e9d8

---
+++ modules/etherpad/manifests/init.pp
@@ -0,0 +1,36 @@
+# Install and manage Etherpad Lite
+
+class etherpad(
+            $etherpad_host,
+            $etherpad_ip,
+            $etherpad_port,
+            $etherpad_db_user,
+            $etherpad_db_host,
+            $etherpad_db_name,
+            $etherpad_db_pass,
+            ){
+
+    package { 'etherpad-lite':
+        ensure => 'latest',
+    }
+
+    service { 'etherpad-lite':
+        ensure    => running,
+        enable    => true,
+        require   => Package['etherpad-lite'],
+        subscribe => File['/etc/etherpad-lite/settings.json'],
+    }
+
+    file { '/etc/etherpad-lite/settings.json':
+        require => Package['etherpad-lite'],
+        content => template('etherpad/settings.json.erb'),
+    }
+
+    file { '/usr/share/etherpad-lite/src/static/robots.txt':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/etherpad/etherpad-robots.txt',
+    }
+}
