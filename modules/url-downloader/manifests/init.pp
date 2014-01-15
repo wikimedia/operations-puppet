e862bde87671085744015c435cdd1f285fc4a7d8
Matanya Moses <matanya@foss.co.il>
2014-01-15 17:45:44 +0200

    url-downloader: convert into a module
    Change-Id: Ie2d40a6fce128d33cedca2bbb430ae42768069ac

---
+++ modules/url-downloader/manifests/init.pp
@@ -0,0 +1,31 @@
+class url-downloader {
+    system::role { 'url-downloader': description => 'Upload-by-URL proxy' }
+
+    file { '/etc/squid/squid.conf':
+        require => Package['squid'],
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        path    => '/etc/squid/squid.conf',
+        source  => 'puppet:///url-downloader/copy-by-url-proxy.conf',
+    }
+
+    # pin package to the default, Ubuntu version, instead of our own
+    apt::pin { [ 'squid', 'squid-common' ]:
+        pin      => 'release o=Ubuntu',
+        priority => '1001',
+        before   => Package['squid'],
+    }
+
+    package { 'squid':
+        ensure => latest,
+    }
+
+    service { 'squid':
+        ensure    => running,
+        require   => [  File['/etc/squid/squid.conf'],
+                        Package['squid'],
+                        Interface::Ip['url-downloader']],
+        subscribe => File['/etc/squid/squid.conf'],
+    }
+}
