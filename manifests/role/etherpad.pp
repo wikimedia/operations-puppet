340ccedc319a93551a8f06acb6378e3383727b5e
Matanya Moses <matanya@foss.co.il>
2014-01-15 14:51:05 +0200

    etherpad: convert into a module
    Change-Id: I4826098f875045e10f1e0a5a69c9ea972dc8e9d8

---
+++ manifests/role/etherpad.pp
@@ -0,0 +1,26 @@
+class role::etherpad{
+    include ::etherpad
+    system::role { 'etherpad': description => 'Etherpad-lite server' }
+
+    $etherpad_db_user = $passwords::etherpad_lite::etherpad_db_user
+    $etherpad_db_host = $passwords::etherpad_lite::etherpad_db_host
+    $etherpad_db_name = $passwords::etherpad_lite::etherpad_db_name
+    $etherpad_db_pass = $passwords::etherpad_lite::etherpad_db_pass
+
+    if $::realm == 'labs' {
+        $etherpad_host = $::fqdn
+        $etherpad_ssl_cert = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+        $etherpad_ssl_key = '/etc/ssl/private/ssl-cert-snakeoil.key'
+    } else {
+        $etherpad_host = 'etherpad.wikimedia.org'
+        $etherpad_serveraliases = 'epl.wikimedia.org'
+        install_certificate{ 'etherpad.wikimedia.org': }
+        $etherpad_ssl_cert = '/etc/ssl/certs/etherpad.wikimedia.org.pem'
+        $etherpad_ssl_key = '/etc/ssl/private/etherpad.wikimedia.org.key'
+    }
+    # Icinga process monitoring, RT #5790
+    nrpe::monitor_service { 'etherpad-lite-proc':
+        description   => 'etherpad_lite_process_running',
+        nrpe_command  => "/usr/lib/nagios/plugins/check_procs -c 1:1 --ereg-argument-array='^node node_modules/ep_etherpad-lite/node/server.js'",
+    }
+}
---
+++ modules/etherpad/manifests/init.pp
@@ -0,0 +1,37 @@
+# Install and manage Etherpad Lite
+
+class etherpad(
+    $etherpad_host,
+    $etherpad_ip,
+    $etherpad_db_user,
+    $etherpad_db_host,
+    $etherpad_db_name,
+    $etherpad_db_pass,
+){
+    include passwords::etherpad_lite,
+        etherpad::monitoring
+
+    package { 'etherpad-lite':
+        ensure => latest,
+    }
+
+    service { 'etherpad-lite':
+        ensure    => running,
+        renable   => true,
+        equire    => Package['etherpad-lite'],
+        subscribe => File['/etc/etherpad-lite/settings.json'],
+    }
+
+    file { '/etc/etherpad-lite/settings.json':
+        require => Package['etherpad-lite'],
+        content => template('etherpad/settings.json.erb'),
+    }
+    @webserver::apache::module { [ 'proxy', 'rewrite', 'proxy_http' ]:}
+    @webserver::apache::site { $etherpad_host:
+        ssl     => 'redirected',
+        require => [
+            Install_certificate[$etherpad_host],
+        ],
+    }
+}
+
