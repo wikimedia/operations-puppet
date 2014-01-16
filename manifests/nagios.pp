1ed2adb8dac2e1e9d3101a93041ec4b9caae86b0
Matanya Moses <matanya@foss.co.il>
2014-01-16 11:42:46 +0200

    nagios: puppet 3 compatibility fix: fully qualify variables
    Change-Id: I235798723128473d83b02353c50dcbc4751113b1

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -9,7 +9,7 @@ $ganglia_url = 'http://ganglia.wikimedia.org'
 
 define monitor_host(
     $ip_address    = $::ipaddress,
-    $group         = $nagios_group,
+    $group         = $::nagios_group,
     $ensure        = present,
     $critical      = 'false',
     $contact_group = 'admins'
@@ -22,7 +22,7 @@ define monitor_host(
     # Export the nagios host instance
     @@nagios_host { $title:
         ensure               => $ensure,
-        target               => "${nagios_config_dir}/puppet_hosts.cfg",
+        target               => "${::nagios_config_dir}/puppet_hosts.cfg",
         host_name            => $title,
         address              => $ip_address,
         hostgroups           => $group ? {
@@ -42,7 +42,7 @@ define monitor_host(
     }
 
     if $title == $::hostname {
-        $image = $operatingsystem ? {
+        $image = $::operatingsystem ? {
             'Ubuntu'  => 'ubuntu',
             'Solaris' => 'sunlogo',
             default   => 'linux40'
@@ -51,11 +51,11 @@ define monitor_host(
         # Couple it with some hostextinfo
         @@nagios_hostextinfo { $title:
             ensure          => $ensure,
-            target          => "${nagios_config_dir}/puppet_hostextinfo.cfg",
+            target          => "${::nagios_config_dir}/puppet_hostextinfo.cfg",
             host_name       => $title,
             notes           => $title,
             # Needs c       = cluster parameter. Let's fix this cleanly with Puppet 2.6 hashes
-            notes_url       => "${ganglia_url}/?c=${ganglia::cname}&h=${fqdn}&m=&r=hour&s=descending&hc=4",
+            notes_url       => "${::ganglia_url}/?c=${ganglia_new::monitor::config::cname}&h=${fqdn}&m=&r=hour&s=descending&hc=4",
             icon_image      => "${image}.png",
             vrml_image      => "${image}.png",
             statusmap_image => "${image}.gd2",
@@ -68,7 +68,7 @@ define monitor_service(
     $check_command,
     $host                  = $::hostname,
     $retries               = 3,
-    $group                 = $nagios_group,
+    $group                 = $::nagios_group,
     $ensure                = present,
     $critical              = 'false',
     $passive               = 'false',
@@ -86,7 +86,7 @@ define monitor_service(
         # Export the nagios service instance
         @@nagios_service { "$::hostname $title":
             ensure                        => 'absent',
-            target                        => "${nagios_config_dir}/puppet_checks.d/${host}.cfg",
+            target                        => "${::nagios_config_dir}/puppet_checks.d/${host}.cfg",
             host_name                     => $host,
             servicegroups                 => $group ? {
                 /.+/    => $group,
@@ -111,7 +111,7 @@ define monitor_service(
         # Export the nagios service instance
         @@nagios_service { "$::hostname $title":
             ensure                  => $ensure,
-            target                  => "${nagios_config_dir}/puppet_checks.d/${host}.cfg",
+            target                  => "${::nagios_config_dir}/puppet_checks.d/${host}.cfg",
             host_name               => $host,
             servicegroups           => $group ? {
                 /.+/    => $group,
@@ -158,7 +158,7 @@ define monitor_group ($description, $ensure=present) {
     # Nagios hostgroup instance
     nagios_hostgroup { $title:
         ensure         => $ensure,
-        target         => "${nagios_config_dir}/puppet_hostgroups.cfg",
+        target         => "${::nagios_config_dir}/puppet_hostgroups.cfg",
         hostgroup_name => $title,
         alias          => $description,
     }
@@ -166,12 +166,11 @@ define monitor_group ($description, $ensure=present) {
     # Nagios servicegroup instance
     nagios_servicegroup { $title:
         ensure            => $ensure,
-        target            => "${nagios_config_dir}/puppet_servicegroups.cfg",
+        target            => "${::nagios_config_dir}/puppet_servicegroups.cfg",
         servicegroup_name => $title,
         alias             => $description,
     }
 }
-
 define decommission_monitor_host {
     if defined(Nagios_host[$title]) {
         # Override the existing resources
@@ -249,7 +248,6 @@ class nagios::gsbmonitoring {
     }
 }
 
-
 class nagios::group {
     group { 'nagios':
         ensure    => present,
@@ -266,114 +264,3 @@ class misc::zfs::monitoring {
     }
 }
 
-# == Define monitor_ganglia
-# Wrapper for monitor_service using check_ganglia command.
-# This allows you to monitor arbitrary values in ganglia
-# with icinga without having to add entries to checkcommands.cfg.erb
-#
-# Specifying threshold values
-# ===========================
-#
-# (This is extracted from ``check_gmond.checkval``; see the embedded
-# documentation for the most current version).
-#
-# The arguments to the ``-w`` and ``-c`` options use the following syntax:
-#
-# For numeric values
-# ------------------
-# - 5       -- match if v >= 5
-# - 3:5     -- match if 3 <= v <= 5
-# - :5      -- match if v <=5
-# - 1,2,3   -- match if v in (1,2,3)
-#
-# For string values
-# ------------------
-# - foo     -- match if v == foo
-# - foo,bar -- match if v in (foo, bar)
-#
-# Negation
-# --------
-# You can negate a threshold expression by preceding it with '!'.  For
-# example:
-#
-# - !5      -- match if v < 5
-# - !3:5    -- match if v<3 || v>5
-# - !1,2,3  -- match if v not in (1,2,3)
-#
-# ( Pasted from# https://github.com/wikimedia/operations-debs-check_ganglia#specifying-threshold-values )
-#
-# == Usage
-#   # Alert if free space in HDFS is less than 1TB
-#   monitor_ganglia { 'hdfs-capacity-remaining':
-#       description          => 'GB free in HDFS',
-#       metric               => 'Hadoop.NameNode.FSNamesystem.CapacityRemainingGB',
-#       warning              => ':1024',
-#       critical             => ':512,
-#   }
-#
-# == Parameters
-# $description          - Description of icinga alert
-# $metric               - ganglia metric name
-# $warning              - alert warning threshold
-# $critical             - alert critical threshold
-# $metric_host          - hostname in ganglia we want to monitor.
-#                         Can't use nagios macro in checkcommands.cfg
-#                         because fqdn is not available.
-#                         Default: $::fqdn of this node
-# $gmetad_host          - Default: 'nickel.wikimedia.org'
-# $gmetad_query_port    - gmetad XML query interface port.  Default: 8654
-# $host
-# $retries
-# $group
-# $ensure
-# $critical
-# $passive
-# $freshness
-# $normal_check_interval
-# $retry_check_interval
-# $contact_group
-#
-define monitor_ganglia(
-    $description,
-    $metric,
-    $warning,
-    $critical,
-    $metric_host           = $::fqdn,
-    $gmetad_host           = 'nickel.wikimedia.org',
-    $gmetad_query_port     = 8654,
-    $host                  = $::hostname,
-    $retries               = 3,
-    $group                 = $nagios_group,
-    $ensure                = present,
-    $critical              = 'false',
-    $passive               = 'false',
-    $freshness             = 36000,
-    $normal_check_interval = 1,
-    $retry_check_interval  = 1,
-    $contact_group         = 'admins'
-)
-{
-    # checkcommands.cfg's check_ganglia command has
-    # many positional arguments that
-    # are passed to check_ganglia script:
-    #   $ARG1$  -g gmetad host
-    #   $ARG2$  -p gmetad xml query port
-    #   $ARG3$  -H Host for which we want metrics
-    #   $ARG4$  -m ganglia metric name
-    #   $ARG5$  -w warning threshold
-    #   $ARG6$  -c critical threshold
-
-     monitor_service { $title:
-         ensure                => $ensure,
-         description           => $description,
-         check_command         => "check_ganglia!${gmetad_host}!${gmetad_query_port}!${metric_host}!${metric}!${warning}!${critical}",
-         retries               => $retries,
-         group                 => $group,
-         critical              => $critical,
-         passive               => $passive,
-         freshness             => $freshness,
-         normal_check_interval => $normal_check_interval,
-         retry_check_interval  => $retry_check_interval,
-         contact_group         => $contact_group,
-     }
-}
