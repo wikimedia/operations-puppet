20660d32a5a1a9cba90259263941a18769474b90
Matanya Moses <matanya@foss.co.il>
2014-01-19 00:20:30 +0200

    beta: convert into a module
    Change-Id: I2200ce11e948ba96c1fedd20083db1fd62c64620

--- manifests/role/beta.pp
+++ manifests/role/beta.pp
@@ -6,33 +6,37 @@
 # cluster. This is the lame way to automatically pull any code merged in master
 # branches.
 class role::beta::autoupdater {
-
-    include misc::beta::autoupdater
-
     system::role { 'role::beta::autoupdater':
         description => 'Server is autoupdating MediaWiki core and extension on beta.'
     }
 
+    include beta::autoupdater
 }
 
-class role::beta::natfixup {
+class role::beta::fatalmonitor {
+    system::role { 'role::beta::fatalmonitor':
+        description => 'Monitor fatal errors and mails qa list'
+    }
 
+    include beta::fatalmonitor
+}
+
+class role::beta::natfix {
     system::role { 'role::beta::natfix':
         description => 'Server has beta NAT fixup'
     }
 
-    include misc::beta::natfixup
+    include beta::natfix
 }
 
 class role::beta::maintenance {
     class{ 'misc::maintenance::geodata': enabled => true }
 }
 
-class role::beta::fatalmonitor {
-
-    system::role { 'role::beta::fatalmonitor':
-        description => 'Monitor fatal errors and mails qa list'
+class role::beta::syncsiteresources {
+    system::role { 'role::beta::syncsiteresources':
+        description => 'Sync material from production wikis to beta wikis'
     }
 
-    include misc::beta::fatalmonitor
+    include beta::syncsiteresources
 }
