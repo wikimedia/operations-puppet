e535f9534d47eb1e5913bee928e34da38f163b53
Matanya Moses <matanya@foss.co.il>
2014-01-19 00:20:30 +0200

    beta: convert into a module
    Change-Id: I2200ce11e948ba96c1fedd20083db1fd62c64620

---
+++ modules/beta/manifests/natdestrewrite.pp
@@ -0,0 +1,13 @@
+define beta::natdestrewrite( $public_ip, $private_ip ) {
+
+    include base::firewall
+
+    # iptables -t nat -I OUTPUT --dest $public_ip -j DNAT --to-dest $private_ip
+    ferm::rule { "nat_rewrite_for_${name}":
+        table  => 'nat',
+        chain  => 'OUTPUT',
+        domain => 'ip',
+        rule   => "daddr ${public_ip} { DNAT to ${private_ip}; }",
+    }
+}
+
