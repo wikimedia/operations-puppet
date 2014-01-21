6584cb5a33af27b9c2ddc561bdd65774523a4203
dzahn <dzahn@wikimedia.org>
2014-01-21 05:27:47 -0800

    turn planet into a module
    there has been an older attempt to do this
    that had to be reverted due to a bug causing
    puppet issues
    
    so this is along the lines of
    https://gerrit.wikimedia.org/r/#/c/54502/3
    https://gerrit.wikimedia.org/r/#/c/58495/ etc,
    
    but entirely new and _doesn't_ do things like dropping
    the role class, putting config into module etc.
    
    Change-Id: I980f9dc9f48e80d53dd2c920622ae67672d575df

---
+++ modules/planet/manifests/dirs.pp
@@ -0,0 +1,18 @@
+# sets up needed directories for a planet-venus intall
+class planet::dirs {
+
+    file { [
+        '/var/www/planet',
+        '/var/log/planet',
+        '/usr/share/planet-venus/wikimedia',
+        '/usr/share/planet-venus/theme/wikimedia',
+        '/usr/share/planet-venus/theme/common',
+        '/var/cache/planet'
+        ]:
+        ensure => 'directory',
+        owner  => 'planet',
+        group  => 'planet',
+        mode   => '0755',
+    }
+
+}
---
+++ modules/planet/manifests/locales.pp
@@ -0,0 +1,25 @@
+# sets up international locales for a planet-venus install
+class planet::locales {
+
+    # locales are important for planet
+    # they can be auto-updated though
+    package { 'locales':
+        ensure => 'latest',
+    }
+
+    # uses local_int from module 'generic'
+    file { '/var/lib/locales/supported.d/local':
+        source => 'puppet:///modules/generic/locales/local_int',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
+
+    # generate locales
+    exec { '/usr/sbin/locale-gen':
+        subscribe   => File['/var/lib/locales/supported.d/local'],
+        refreshonly => true,
+        require     => File['/var/lib/locales/supported.d/local'],
+    }
+
+}
