a72753772f5c181477a862e965dc5628e07c2765
dzahn <dzahn@wikimedia.org>
2014-01-21 05:27:47 -0800

    turn planet into a module
    there has been an older attempt to do this
    that had to be reverted due to a bug causing
    puppet issues
    
    so this is along the lines of
    https://gerrit.wikimedia.org/r/#/c/54502/3
    https://gerrit.wikimedia.org/r/#/c/58495/ etc,
    
    but entirely new and _doesn't_ do things like dropping
    the role class, putting config into module etc.
    
    Change-Id: I980f9dc9f48e80d53dd2c920622ae67672d575df

---
+++ modules/planet/manifests/user.pp
@@ -0,0 +1,10 @@
+# adds a system user for a planet-venus install
+class planet::user {
+
+    systemuser { 'planet':
+        name   => 'planet',
+        home   => '/var/lib/planet',
+        groups => [ 'planet' ],
+    }
+
+}
