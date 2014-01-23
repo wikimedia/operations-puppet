624abfc80cc8a539c31691d51b56e555ac6c4867
Matanya Moses <matanya@foss.co.il>
2014-01-23 12:20:39 +0200

    realm: lint clean
    Change-Id: I25ac7a378a44c0ac10b2a78a480eff120cbdaadc

--- manifests/realm.pp
+++ manifests/realm.pp
@@ -3,57 +3,61 @@
 #
 
 if $::realm == undef {
-	$realm = "production"
+    $realm = 'production'
 }
 
 if $::instanceproject == undef {
-	$instanceproject = ''
+    $instanceproject = ''
 }
 
 if $::projectgroup == undef {
-	$projectgroup = "project-$instanceproject"
+    $projectgroup = "project-${instanceproject}"
 }
 
 # TODO: redo this in a much better way
-$all_prefixes = [ "208.80.152.0/22", "91.198.174.0/24", "198.35.26.0/23", "185.15.56.0/22" ]
+$all_prefixes = [ '208.80.152.0/22',
+                '91.198.174.0/24',
+                '198.35.26.0/23',
+                '185.15.56.0/22'
+                ]
 
 # Determine the site the server is in
 if $::ipaddress_eth0 != undef {
-	$main_ipaddress = $ipaddress_eth0
+    $main_ipaddress = $ipaddress_eth0
 } elsif $::ipaddress_bond0 != undef {
-	$main_ipaddress = $ipaddress_bond0
+    $main_ipaddress = $ipaddress_bond0
 } else {
-	$main_ipaddress = $ipaddress
+    $main_ipaddress = $ipaddress
 }
 
 $site = $main_ipaddress ? {
-	/^208\.80\.15[23]\./                      => "pmtpa",
-	/^208\.80\.15[45]\./                      => "eqiad",
-	/^10\.[0-4]\./                            => "pmtpa",
-	/^10\.6[48]\./                            => "eqiad",
-	/^91\.198\.174\./                         => "esams",
-	/^198\.35\.26\.([0-9]|[1-5][0-9]|6[0-2])/ => "ulsfo",
-	/^10\.128\./                              => "ulsfo",
-	default                                   => "(undefined)"
+    /^208\.80\.15[23]\./                      => 'pmtpa',
+    /^208\.80\.15[45]\./                      => 'eqiad',
+    /^10\.[0-4]\./                            => 'pmtpa',
+    /^10\.6[48]\./                            => 'eqiad',
+    /^91\.198\.174\./                         => 'esams',
+    /^198\.35\.26\.([0-9]|[1-5][0-9]|6[0-2])/ => 'ulsfo',
+    /^10\.128\./                              => 'ulsfo',
+    default                                   => '(undefined)'
 }
 
 $mw_primary = $::realm ? {
-	'production' => "eqiad",
-	default => $::site
+    'production' => 'eqiad',
+    default => $::site
 }
 
 $network_zone = $main_ipaddress ? {
-	/^10./			=> "internal",
-	default			=> "public"
+    /^10./  => 'internal',
+    default => 'public'
 }
 
 # TODO: create hash of all LVS service IPs
 
 # Set some basic variables
 $nameservers = $site ? {
-	"esams"	=> [ "91.198.174.6", "208.80.152.131" ],
-	"pmtpa"	=> [ "208.80.152.131", "208.80.152.132" ],
-	default	=> [ "208.80.154.239", "208.80.152.131" ],
+    'esams' => [ '91.198.174.6', '208.80.152.131' ],
+    'pmtpa' => [ '208.80.152.131', '208.80.152.132' ],
+    default => [ '208.80.154.239', '208.80.152.131' ],
 }
 # Allow per-server nameserver prefixes
 $nameservers_prefix = []
@@ -71,106 +75,106 @@ $gid = 500
 ## puppet-accessible list of private wikis
 ## please keep alphabetized
 $private_wikis = [ 'arbcom_dewiki',
-		'arbcom_enwiki',
-		'arbcom_fiwiki',
-		'arbcom_nlwiki',
-		'auditcomwiki',
-		'boardgovcomwiki',
-		'boardwiki',
-		'chairwiki',
-		'chapcomwiki',
-		'checkuserwiki',
-		'collabwiki',
-		'execwiki',
-		'fdcwiki',
-		'grantswiki',
-		'iegcomwiki',
-		'ilwikimedia',
-		'internalwiki',
-		'movementroleswiki',
-		'noboard_chapterswikimedia',
-		'officewiki',
-		'ombudsmenwiki',
-		'otrs_wikiwiki',
-		'searchcomwiki',
-		'spcomwiki',
-		'stewardwiki',
-		'transitionteamwiki',
-		'wg_enwiki',
-		'wikimaniateamwiki', ]
+        'arbcom_enwiki',
+        'arbcom_fiwiki',
+        'arbcom_nlwiki',
+        'auditcomwiki',
+        'boardgovcomwiki',
+        'boardwiki',
+        'chairwiki',
+        'chapcomwiki',
+        'checkuserwiki',
+        'collabwiki',
+        'execwiki',
+        'fdcwiki',
+        'grantswiki',
+        'iegcomwiki',
+        'ilwikimedia',
+        'internalwiki',
+        'movementroleswiki',
+        'noboard_chapterswikimedia',
+        'officewiki',
+        'ombudsmenwiki',
+        'otrs_wikiwiki',
+        'searchcomwiki',
+        'spcomwiki',
+        'stewardwiki',
+        'transitionteamwiki',
+        'wg_enwiki',
+        'wikimaniateamwiki', ]
 
 $private_tables = [ 'accountaudit_login',
-		'arbcom1_vote',
-		'archive_old',
-		'blob_orphans',
-		'blob_tracking',
-		'bv2009_edits',
-		'categorylinks_old',
-		'click_tracking',
-		'cu_changes',
-		'cu_log',
-		'cur',
-		'edit_page_tracking',
-		'email_capture',
-		'exarchive',
-		'exrevision',
-		'filearchive',
-		'filejournal',
-		'globalnames',
-		'hidden',
-		'image_old',
-		'job',
-		'linkscc',
-		'localnames',
-		'log_search',
-		'logging_old',
-		'long_run_profiling',
-		'migrateuser_medium',
-		'moodbar_feedback',
-		'moodbar_feedback_response',
-		'objectcache',
-		'old_growth',
-		'oldimage_old',
-		'optin_survey',
-		'pr_index',
-		'prefstats',
-		'prefswitch_survey',
-		'profiling',
-		'querycache',
-		'querycache_info',
-		'querycache_old',
-		'querycachetwo',
-		'securepoll_cookie_match',
-		'securepoll_elections',
-		'securepoll_entity',
-		'securepoll_lists',
-		'securepoll_msgs',
-		'securepoll_options',
-		'securepoll_properties',
-		'securepoll_questions',
-		'securepoll_strike',
-		'securepoll_voters',
-		'securepoll_votes',
-		'spoofuser',
-		'text',
-		'titlekey',
-		'transcache',
-		'uploadstash',
-		'user_daily_contribs',
-		'user_newtalk',
-		'user_properties',
-		'vote_log',
-		'watchlist' ]
+        'arbcom1_vote',
+        'archive_old',
+        'blob_orphans',
+        'blob_tracking',
+        'bv2009_edits',
+        'categorylinks_old',
+        'click_tracking',
+        'cu_changes',
+        'cu_log',
+        'cur',
+        'edit_page_tracking',
+        'email_capture',
+        'exarchive',
+        'exrevision',
+        'filearchive',
+        'filejournal',
+        'globalnames',
+        'hidden',
+        'image_old',
+        'job',
+        'linkscc',
+        'localnames',
+        'log_search',
+        'logging_old',
+        'long_run_profiling',
+        'migrateuser_medium',
+        'moodbar_feedback',
+        'moodbar_feedback_response',
+        'objectcache',
+        'old_growth',
+        'oldimage_old',
+        'optin_survey',
+        'pr_index',
+        'prefstats',
+        'prefswitch_survey',
+        'profiling',
+        'querycache',
+        'querycache_info',
+        'querycache_old',
+        'querycachetwo',
+        'securepoll_cookie_match',
+        'securepoll_elections',
+        'securepoll_entity',
+        'securepoll_lists',
+        'securepoll_msgs',
+        'securepoll_options',
+        'securepoll_properties',
+        'securepoll_questions',
+        'securepoll_strike',
+        'securepoll_voters',
+        'securepoll_votes',
+        'spoofuser',
+        'text',
+        'titlekey',
+        'transcache',
+        'uploadstash',
+        'user_daily_contribs',
+        'user_newtalk',
+        'user_properties',
+        'vote_log',
+        'watchlist' ]
 
 # Route list for mail coming from MediaWiki mailer
 $exim_mediawiki_route_list = $::realm ? {
-	'production' => 'smtp.pmtpa.wmnet',
-	# FIXME: find some SMTP servers for labs
-	'labs'       => 'mchenry.wikimedia.org:lists.wikimedia.org'
+    'production' => 'smtp.pmtpa.wmnet',
+    # FIXME: find some SMTP servers for labs
+    'labs'       => 'mchenry.wikimedia.org:lists.wikimedia.org'
 }
 # Generic, default servers
 $exim_default_route_list = $::realm ? {
-	'production' => 'mchenry.wikimedia.org:lists.wikimedia.org',
-	# FIXME: find some SMTP servers for labs
-	'labs'       => 'mchenry.wikimedia.org:lists.wikimedia.org',
+    'production' => 'mchenry.wikimedia.org:lists.wikimedia.org',
+    # FIXME: find some SMTP servers for labs
+    'labs'       => 'mchenry.wikimedia.org:lists.wikimedia.org',
 }
