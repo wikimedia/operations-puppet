2116a2b09d30df842b59ab49f5481b7d7c619498
RobH <rob@wikimedia.org>
2014-01-23 12:14:07 -0800

    removing ssl mod load for gallium
    gallium no longer needs to load mod_ssl or listen on port 443, so
    unloading this should stop both.
    
    Change-Id: I53b459529e0077a504043b8290f1c8d3a1dc9816
    RT: 6594

--- modules/contint/manifests/proxy_common.pp
+++ modules/contint/manifests/proxy_common.pp
@@ -1,7 +1,7 @@
 # Basic configuration of Apache as a proxy
 class contint::proxy_common {
 
-  class {'webserver::php5': ssl => true; }
+  class {'webserver::php5': ssl => false; }
 
   apache_module { 'contint_mod_proxy': name => 'proxy' }
   apache_module { 'contint_mod_proxy_http': name => 'proxy_http' }
