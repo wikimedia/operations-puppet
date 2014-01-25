d54ef708f5ad0b6efe7f32d029f2e3c92e31db82
Ori Livneh <ori@wikimedia.org>
2014-01-24 17:21:58 -0800

    txstatsd: max-queue-size = 1m, max-datapoints = 10k
    Configure sensible values for max-queue-size (1,000,000) and
    max-datapoints-per-message (10,000).
    
    Change-Id: I0fe1009522d6e7f16ede0418d064d7bd412f70dd

--- manifests/role/txstatsd.pp
+++ manifests/role/txstatsd.pp
@@ -8,11 +8,13 @@ class role::txstatsd {
     class { '::txstatsd':
         settings => {
             statsd => {
-                'carbon-cache-host' => 'localhost',
-                'carbon-cache-port' => 2004,
-                'listen-port'       => 8125,
-                'statsd-compliance' => 0,
-                'prefix'            => '',
+                'carbon-cache-host'          => 'localhost',
+                'carbon-cache-port'          => 2004,
+                'listen-port'                => 8125,
+                'statsd-compliance'          => 0,
+                'prefix'                     => '',
+                'max-queue-size'             => 1000 * 1000,
+                'max-datapoints-per-message' => 10 * 1000,
             },
         },
     }
