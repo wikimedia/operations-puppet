4635518f83c4ffa5c17857621b0d98e5e38e77a6
Matanya Moses <matanya@foss.co.il>
2014-01-26 13:47:34 +0200

    mobile: lint
    Change-Id: If371fc2efcfaac7d02aa41b017c6bcc1bddc09f5

--- manifests/mobile.pp
+++ manifests/mobile.pp
@@ -1,184 +1,245 @@
 # This file is for mobile classes
 class mobile::vumi::iptables-purges {
-	require "iptables::tables"
+    require 'iptables::tables'
 
-	# The deny_all rule must always be purged, otherwise ACCEPTs can be placed below it
-	iptables_purge_service{ "deny_all_redis": service => "redis" }
+    # The deny_all rule must always be purged,
+    #otherwise ACCEPTs can be placed below it
+    iptables_purge_service{ 'deny_all_redis':
+        service => 'redis',
+    }
 
-	# When removing or modifying a rule, place the old rule here, otherwise it won't
-	# be purged, and will stay in the iptables forever
+    # When removing or modifying a rule,
+    #place the old rule here, otherwise it won't
+    # be purged, and will stay in the iptables forever
 }
 
 class mobile::vumi::iptables-accepts {
-	require "mobile::vumi::iptables-purges"
+    require 'mobile::vumi::iptables-purges'
 
-	# Rememeber to place modified or removed rules into purges!
-	iptables_add_service{ "redis_internal": source => "208.80.152.0/22", service => "redis", jump => "ACCEPT" }
+    # Rememeber to place modified or removed rules into purges!
+    iptables_add_service{ 'redis_internal':
+        source  => '208.80.152.0/22',
+        service => 'redis',
+        jump    => 'ACCEPT',
+    }
 }
 
 class mobile::vumi::iptables-drops {
-	require "mobile::vumi::iptables-accepts"
+    require 'mobile::vumi::iptables-accepts'
 
-	# Deny by default
-	iptables_add_service{ "deny_all_redis": service => "redis", jump => "DROP" }
+    # Deny by default
+    iptables_add_service{ 'deny_all_redis':
+        service => 'redis',
+        jump    => 'DROP',
+    }
 }
 
 class mobile::vumi::iptables  {
-	if $realm == "production" {
-		# We use the following requirement chain:
-		# iptables -> iptables::drops -> iptables::accepts -> iptables::accept-established -> iptables::purges
-		#
-		# This ensures proper ordering of the rules
-		require "mobile::vumi::iptables-drops"
+    if $::realm == 'production' {
+        # We use the following requirement chain:
+        # iptables -> iptables::drops -> iptables::accepts -> iptables::accept-established -> iptables::purges
+        # This ensures proper ordering of the rules
+        require 'mobile::vumi::iptables-drops'
 
-		# This exec should always occur last in the requirement chain.
-		iptables_add_exec{ "${hostname}": service => "vumi" }
-	}
+        # This exec should always occur last in the requirement chain.
+        iptables_add_exec{ $::hostname:
+            service => 'vumi',
+        }
+    }
 
-	# Labs has security groups, and as such, doesn't need firewall rules
+    # Labs has security groups, and as such, doesn't need firewall rules
 }
 
 class mobile::vumi {
 
-	include passwords::mobile::vumi,
-		mobile::vumi::iptables
+    include passwords::mobile::vumi,
+        mobile::vumi::iptables
 
-	$testvumi_pw          = $passwords::mobile::vumi::wikipedia_xmpp_sms_out
-	$vumi_pw              = $passwords::mobile::vumi::wikipedia_xmpp
-	$tata_sms_incoming_pw = $passwords::mobile::vumi::tata_sms_incoming_pw
-	$tata_sms_outgoing_pw = $passwords::mobile::vumi::tata_sms_outgoing_pw
-	$tata_ussd_pw         = $passwords::mobile::vumi::tata_ussd_pw
-	$tata_hyd_ussd_pw     = $passwords::mobile::vumi::tata_hyd_ussd_pw
+    $testvumi_pw          = $passwords::mobile::vumi::wikipedia_xmpp_sms_out
+    $vumi_pw              = $passwords::mobile::vumi::wikipedia_xmpp
+    $tata_sms_incoming_pw = $passwords::mobile::vumi::tata_sms_incoming_pw
+    $tata_sms_outgoing_pw = $passwords::mobile::vumi::tata_sms_outgoing_pw
+    $tata_ussd_pw         = $passwords::mobile::vumi::tata_ussd_pw
+    $tata_hyd_ussd_pw     = $passwords::mobile::vumi::tata_hyd_ussd_pw
 
-	file { "/a":
-		ensure => directory;
-	}
+    file { '/a':
+        ensure => 'directory',
+    }
 
-	class { "redis":
-		maxmemory => "1024Mb",
-	}
-	include redis::ganglia
-	package {
-		"python-iso8601":
-			ensure => "0.1.4-1ubuntu1";
-		"python-redis":
-			ensure => "2.4.9-1";
-		"python-smpp":
-			ensure => "0.1-0~ppa3";
-		"python-ssmi":
-			ensure => "0.0.4-1~ppa3";
-		"python-txamqp":
-			ensure => "0.6.1-1~ppa3";
-		"vumi":
-			ensure => "0.5.0~a+143-0~ppa3";
-		"vumi-wikipedia":
-			ensure => "0.1~a+14-0~ppa3";
-		"python-twisted":
-			ensure => "latest";
-		"python-tz":
-			ensure => "latest";
-		"python-wokkel":
-			ensure => "0.7.0-1";
-		"rabbitmq-server":
-			ensure => "latest";
-		"supervisor":
-			ensure => "latest";
-	}
+    class { 'redis':
+        maxmemory => '1024Mb',
+    }
+    include redis::ganglia
+    package { 'python-iso8601':
+        ensure => '0.1.4-1ubuntu1',
+    }
 
-	service { "supervisor":
-			enable    => true,
-			ensure    => running,
-			require   => [Package['supervisor']];
-	}
+    package { 'python-redis':
+        ensure => '2.4.9-1',
+    }
 
-	file {
-		"/etc/vumi":
-			ensure => "directory",
-			owner => "root";
-		"/var/log/vumi":
-			ensure => "directory",
-			owner => "root";
-		"/etc/vumi/wikipedia.yaml":
-			owner => "root",
-			source => "puppet:///files/mobile/vumi/wikipedia.yaml",
-			require => File["/etc/vumi"],
-			mode => 0444;
-		"/etc/vumi/tata_ussd_dispatcher.yaml":
-			owner => "root",
-			source => "puppet:///files/mobile/vumi/tata_ussd_dispatcher.yaml",
-			require => File["/etc/vumi"],
-			mode => 0444;
-		"/etc/vumi/tata_ussd_hyd.yaml":
-			owner => "root",
-			content => template("mobile/vumi/tata_ussd_hyd.yaml.erb"),
-			require => File["/etc/vumi"],
-			mode => 0444;
-		"/etc/vumi/tata_sms_outgoing.yaml":
-			owner => "root",
-			content => template("mobile/vumi/tata_sms_outgoing.yaml.erb"),
-			require => File["/etc/vumi"],
-			mode => 0444;
-		"/etc/vumi/tata_ussd_delhi.yaml":
-			owner => "root",
-			content => template("mobile/vumi/tata_ussd_delhi.yaml.erb"),
-			require => File["/etc/vumi"],
-			mode => 0444;
-		"/etc/vumi/tata_sms_incoming.yaml":
-			owner => "root",
-			content => template("mobile/vumi/tata_sms_incoming.yaml.erb"),
-			require => File["/etc/vumi"],
-			mode => 0444;
-		"/etc/supervisor/conf.d/supervisord.wikipedia.conf":
-			owner => "root",
-			source => "puppet:///files/mobile/vumi/supervisord.wikipedia.conf",
-			require => Package["supervisor"],
-			mode => 0444;
-		"/etc/vumi/wikipedia_xmpp.yaml":
-			owner => "root",
-			content => template("mobile/vumi/wikipedia_xmpp.yaml.erb"),
-			require => File["/etc/vumi"],
-			mode => 0444;
-		"/etc/vumi/wikipedia_xmpp_sms.yaml":
-			owner => "root",
-			content => template("mobile/vumi/wikipedia_xmpp_sms.yaml.erb"),
-			require => File["/etc/vumi"],
-			mode => 0444;
-		"/usr/local/vumi":
-			owner => "root",
-			ensure => "directory";
-		"/usr/local/vumi/rabbitmq.setup.sh":
-			owner => "root",
-			mode => 0555,
-			require => File["/usr/local/vumi"],
-			source => "puppet:///files/mobile/vumi/rabbitmq.setup.sh";
-	}
+    package { 'python-smpp':
+        ensure => '0.1-0~ppa3',
+    }
 
-	exec {
-		"Set permissions for rabbitmq user":
-			command => "/usr/local/vumi/rabbitmq.setup.sh",
-			user => "root",
-			require => File["/usr/local/vumi/rabbitmq.setup.sh"],
-			unless => "/usr/sbin/rabbitmqctl list_user_permissions vumi | grep develop",
-	}
+    package { 'python-ssmi':
+        ensure => '0.0.4-1~ppa3',
+    }
+
+    package { 'python-txamqp':
+        ensure => '0.6.1-1~ppa3',
+    }
+
+    package { 'vumi':
+        ensure => '0.5.0~a+143-0~ppa3',
+    }
+
+    package { 'vumi-wikipedia':
+        ensure => '0.1~a+14-0~ppa3',
+    }
+
+    package { 'python-twisted':
+        ensure => 'latest',
+    }
+
+    package { 'python-tz':
+        ensure => 'latest',
+    }
+
+    package { 'python-wokkel':
+        ensure => '0.7.0-1',
+    }
+
+    package { 'rabbitmq-server':
+        ensure => 'latest',
+    }
+
+    package { 'supervisor':
+        ensure => 'latest',
+    }
+
+    service { 'supervisor':
+        ensure  => 'running',
+        enable  => true,
+        require => Package['supervisor'],
+    }
+
+    file { '/etc/vumi':
+        ensure => 'directory',
+        owner  => 'root',
+    }
+
+    file { '/var/log/vumi':
+        ensure => 'directory',
+        owner  => 'root',
+    }
+
+    file { '/etc/vumi/wikipedia.yaml':
+        owner   => 'root',
+        source  => 'puppet:///files/mobile/vumi/wikipedia.yaml',
+        require => File['/etc/vumi'],
+        mode    => '0444',
+    }
+
+    file { '/etc/vumi/tata_ussd_dispatcher.yaml':
+        owner   => 'root',
+        source  => 'puppet:///files/mobile/vumi/tata_ussd_dispatcher.yaml',
+        require => File['/etc/vumi'],
+        mode    => '0444',
+    }
+
+    file { '/etc/vumi/tata_ussd_hyd.yaml':
+        owner   => 'root',
+        content => template('mobile/vumi/tata_ussd_hyd.yaml.erb'),
+        require => File['/etc/vumi'],
+        mode    => '0444',
+    }
+
+    file { '/etc/vumi/tata_sms_outgoing.yaml':
+        owner   => 'root',
+        content => template('mobile/vumi/tata_sms_outgoing.yaml.erb'),
+        require => File['/etc/vumi'],
+        mode    => '0444',
+    }
+
+    file { '/etc/vumi/tata_ussd_delhi.yaml':
+        owner   => 'root',
+        content => template('mobile/vumi/tata_ussd_delhi.yaml.erb'),
+        require => File['/etc/vumi'],
+        mode    => '0444',
+    }
+
+    file { '/etc/vumi/tata_sms_incoming.yaml':
+        owner   => 'root',
+        content => template('mobile/vumi/tata_sms_incoming.yaml.erb'),
+        require => File['/etc/vumi'],
+        mode    => '0444',
+    }
+
+    file { '/etc/supervisor/conf.d/supervisord.wikipedia.conf':
+        owner   => 'root',
+        source  => 'puppet:///files/mobile/vumi/supervisord.wikipedia.conf',
+        require => Package['supervisor'],
+        mode    => '0444',
+    }
+
+    file { '/etc/vumi/wikipedia_xmpp.yaml':
+        owner   => 'root',
+        content => template('mobile/vumi/wikipedia_xmpp.yaml.erb'),
+        require => File['/etc/vumi'],
+        mode    => '0444',
+    }
+
+    file { '/etc/vumi/wikipedia_xmpp_sms.yaml':
+        owner   => 'root',
+        content => template('mobile/vumi/wikipedia_xmpp_sms.yaml.erb'),
+        require => File['/etc/vumi'],
+        mode    => '0444',
+    }
+
+    file { '/usr/local/vumi':
+        ensure => 'directory',
+        owner  => 'root',
+    }
+
+    file { '/usr/local/vumi/rabbitmq.setup.sh':
+        owner   => 'root',
+        mode    => '0555',
+        require => File['/usr/local/vumi'],
+        source  => 'puppet:///files/mobile/vumi/rabbitmq.setup.sh',
+    }
+
+    exec { 'Set permissions for rabbitmq user':
+        command => '/usr/local/vumi/rabbitmq.setup.sh',
+        user    => 'root',
+        require => File['/usr/local/vumi/rabbitmq.setup.sh'],
+        unless  => '/usr/sbin/rabbitmqctl list_user_permissions vumi | grep develop',
+    }
 }
 
 class mobile::vumi::udp2log {
-	class { 'misc::udp2log': monitor => false }
+    class { 'misc::udp2log':
+        monitor => false,
+    }
 
-	file { "/var/log/vumi/metrics.log":
-		owner  => "root",
-		group  => "udp2log",
-		mode   => 0664,
-		ensure => present,
-	}
+    file { '/var/log/vumi/metrics.log':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'udp2log',
+        mode   => '0664',
+    }
 
-	# oxygen's udp2log instance
-	# saves logs mainly in /a/squid
-	misc::udp2log::instance { "vumi":
-		port                => 5678,
-		recv_queue          => 1,     # 1KB is the smallest passable receive queue for vumi so logs are flushed more often.
-		monitor_packet_loss => false,
-		monitor_log_age     => false,
-		require             => [File["/var/log/vumi"], File["/var/log/vumi/metrics.log"]],
-	}
+    # oxygen's udp2log instance
+    # saves logs mainly in /a/squid
+    misc::udp2log::instance { 'vumi':
+        port                => '5678',
+    # 1KB is the smallest passable receive queue for vumi
+    # so logs are flushed more often
+        recv_queue          => '1',
+        monitor_packet_loss => false,
+        monitor_log_age     => false,
+        require             => [File['/var/log/vumi'],
+                                File['/var/log/vumi/metrics.log']
+        ],
+    }
 }
