874ecdb8f67bfec294726de6c8757b4277aadca7
Andrew Otto <acotto@gmail.com>
2014-01-27 12:15:22 -0500

    Access for nuria on analytics cluster
    RT 6683
    
    Change-Id: I914bff12960643c2b05e98a4e19ab5ac2a0a0f14

--- manifests/role/analytics.pp
+++ manifests/role/analytics.pp
@@ -69,7 +69,9 @@ class role::analytics::users {
         accounts::dartar,    # RT 5835
         accounts::halfak,    # RT 5836
         accounts::ypanda,    # RT 6103
-        accounts::csalvia    # RT 6664
+        accounts::csalvia,   # RT 6664
+        accounts::nuria      # RT 6683
+
 
     # NOTE:  If you are filling an RT request for Hadoop access,
     # you will need to add the user to the list of accounts above,
@@ -95,6 +97,7 @@ class role::analytics::users {
     User<|title == abaso|>       { groups +> [ 'stats' ] }
     User<|title == qchris|>      { groups +> [ 'stats' ] }
     User<|title == csalvia|>     { groups +> [ 'stats' ] }
+    User<|title == nuria|>       { groups +> [ 'stats' ] }
 
     # Diederik and Otto have sudo privileges on Analytics nodes.
     sudo_user { [ 'diederik', 'otto' ]: privileges => ['ALL = (ALL) NOPASSWD: ALL'] }
