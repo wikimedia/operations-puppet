258f0a791de8e7f122b4ce4fc74cc0c9472e3082
gage <jgerard@wikimedia.org>
2014-01-28 08:05:57 -0800

    once more with commas
    Change-Id: I5b6fa56bfa779c2b883f98bde117263a272ff76e

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -915,8 +915,8 @@ class role::cache {
         # Install a varnishkafka producer to send
         # varnish webrequest logs to Kafka.
         class { 'role::cache::varnish::kafka':
-            topic => 'webrequest_bits'
-            varnish_name => $::hostname
+            topic => 'webrequest_bits',
+            varnish_name => $::hostname,
         }
     }
 
