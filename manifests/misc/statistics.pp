37997492cdcdc7993350098d3eba12fa7dabe256
Matanya Moses <matanya@foss.co.il>
2014-01-28 22:53:34 +0200

    emery: move left emery udp2log logs and sync jobs to erbium
    Per RT #6143 and comments from otto on IRC
    
    Change-Id: Iee16df5a1f044ca6686cc843a9c834fbd8143589

--- manifests/misc/statistics.pp
+++ manifests/misc/statistics.pp
@@ -685,27 +685,15 @@ class misc::statistics::rsync::jobs::webrequest {
         destination => "/a/squid/archive/zero",
     }
 
-    # API logs from emery
+    # API logs from erbium
     misc::statistics::rsync_job { "api":
-        source      => "emery.wikimedia.org::udp2log/webrequest/archive/api-usage*.gz",
+        source      => "erbium.wikimedia.org::udp2log/webrequest/archive/api-usage*.gz",
         destination => "/a/squid/archive/api",
     }
 
-    # teahouse logs from emery
-    misc::statistics::rsync_job { "teahouse":
-        source      => "emery.wikimedia.org::udp2log/webrequest/archive/teahouse*.gz",
-        destination => "/a/squid/archive/teahouse",
-    }
-
-    # arabic banner logs from emery
-    misc::statistics::rsync_job { "arabic_banner":
-        source      => "emery.wikimedia.org::udp2log/webrequest/archive/arabic-banner*.gz",
-        destination => "/a/squid/archive/arabic-banner",
-    }
-
-    # sampled-1000 logs from emery
+    # sampled-1000 logs from erbium
     misc::statistics::rsync_job { "sampled_1000":
-        source      => "emery.wikimedia.org::udp2log/webrequest/archive/sampled-1000*.gz",
+        source      => "erbium.wikimedia.org::udp2log/webrequest/archive/sampled-1000*.gz",
         destination => "/a/squid/archive/sampled",
     }
 
