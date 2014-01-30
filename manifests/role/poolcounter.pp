d12b97d9f879dd66e7e0f841b8072a854a15ec6e
Matanya Moses <matanya@foss.co.il>
2014-01-30 13:09:00 +0200

    poolcounter: convert into a module
    Change-Id: I976edd7743f2139e0bd5a1a7f3d447d4345510d8

--- manifests/role/poolcounter.pp
+++ manifests/role/poolcounter.pp
@@ -1,5 +1,24 @@
-class role::poolcounter {
-	include ::poolcounter
-	system::role { 'role::poolcounter': description => 'PoolCounter server' }
+class role::poolcounter (
+    $poolcounter_ip   = '0.0.0.0'
+    $poolcounter_port = '7531'
+){
+    include ::poolcounter
+    include nrpe
+    system::role { 'role::poolcounter':
+        description => 'PoolCounter server',
+    }
+
+    # Process running
+    nrpe::monitor_service { 'poolcounterd':
+        description   => 'poolcounter',
+        check_command => "/usr/lib/nagios/plugins/check_nrpe -H ${poolcounter_ip} -c check_poolcounterd",
+    }
+
+    # TCP port 7531 reacheable
+    nrpe::monitor_service { 'poolcounterd_port_7531':
+        description   => 'Poolcounter connection',
+        check_command => "/usr/lib/nagios/plugins/check_tcp -H ${poolcounter_ip} -p ${poolcounter_port}",
+    }
+
 }
 
