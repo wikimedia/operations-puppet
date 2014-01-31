7033a246f1cbbaa68b086d9851d67c17587361bb
Andrew Otto <acotto@gmail.com>
2014-01-31 11:21:24 -0500

    Upping varnishkafka produce buffer to keep up with traffic on bits servers
    Change-Id: Id106304895ee4917ec944cac6f70b4f1fd1d6fe5

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -428,7 +428,7 @@ class role::cache {
                 # possible slip ups where varnish only writes the short hostname for %l.
                 format                       => "%{fake_tag0@hostname?${::fqdn}}x %{@sequence!num?0}n %{%FT%T@dt}t %{Varnish:time_firstbyte@time_firstbyte!num?0.0}x %{@ip}h %{Varnish:handling@cache_status}x %{@http_status}s %{@response_size!num?0}b %{@http_method}m %{Host@uri_host}i %{@uri_path}U %{@uri_query}q %{Content-Type@content_type}o %{Referer@referer}i %{X-Forwarded-For@x_forwarded_for}i %{User-Agent@user_agent}i %{Accept-Language@accept_language}i %{X-Analytics@x_analytics}o",
                 message_send_max_retries     => 3,
-                queue_buffering_max_messages => 1000000,
+                queue_buffering_max_messages => 2000000,
                 # large timeout to account for potential cross DC latencies
                 topic_request_timeout_ms     => 30000, # request ack timeout
                 # Write out stats to varnishkafka.stats.json
