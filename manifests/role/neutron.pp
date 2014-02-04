bd5952ebf0be4a3c58add62f44d419d39bb826e8
andrewbogott <abogott@wikimedia.org>
2014-02-04 00:28:18 -0600

    Add another network interface to neutron server.
    Change-Id: I60c406b872a70526842d3d04dbad3006901b3120

--- manifests/role/neutron.pp
+++ manifests/role/neutron.pp
@@ -46,18 +46,18 @@ class role::neutron::server {
 
     class { 'openstack::neutron-service':
         openstack_version => $openstack_version,
-        external_interface => 'eth0',
+        external_interface => 'eth1.1122',
         neutronconfig     => $neutronconfig
     }
 
     if ($::site == 'eqiad') {
         interface::ip { 'openstack::network_service_public_dynamic_snat': interface => 'lo', address => '208.80.155.255' }
 
-        interface::ip { 'openstack::external_interface': interface => 'br-ex', address => '10.64.20.13', prefixlen => '24' }
+        interface::ip { 'openstack::external_interface': interface => 'br-ex', address => '10.64.22.11', prefixlen => '24' }
 
-        # By hand, unpuppetized step: # ip addr del 10.64.20.13/24 dev eth0
-        # and: # ifconfig eth0 promisc
+        # By hand, unpuppetized step: # ifconfig eth1.1122 promisc
 
+        # In Openstack terms, this is the 'data' interface
         interface::tagged { 'eth1.1102':
             base_interface => 'eth1',
             vlan_id => '1102',
@@ -65,5 +65,16 @@ class role::neutron::server {
             up => 'ip link set $IFACE up',
             down => 'ip link set $IFACE down',
         }
+
+        # In Openstack terms, this is the 'external' interface
+        interface::tagged { 'eth1.1122':
+            base_interface => 'eth1',
+            vlan_id => '1122',
+            method => 'manual',
+            up => 'ip link set $IFACE up',
+            down => 'ip link set $IFACE down',
+        }
+
+        # In Openstack terms, eth0 is the management interface.
     }
 }
