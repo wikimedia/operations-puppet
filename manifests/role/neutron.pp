67675405b1671ce35cf195f90e8d17eba42f6f8a
andrewbogott <abogott@wikimedia.org>
2014-02-04 08:42:07 -0600

    Assign eth4.1102 ('data') an IP.
    Change-Id: Ia99e6347af15be39ae483d0418ce9bb60ce4b5b6

--- manifests/role/neutron.pp
+++ manifests/role/neutron.pp
@@ -53,6 +53,8 @@ class role::neutron::server {
     if ($::site == 'eqiad') {
         interface::ip { 'openstack::network_service_public_dynamic_snat': interface => 'lo', address => '208.80.155.255' }
 
+        interface::ip { 'openstack::external_interface': interface => 'eth4.1102', address => '10.68.16.1', prefixlen => '24' }
+
         #interface::ip { 'openstack::external_interface': interface => 'br-ex', address => '10.64.22.11', prefixlen => '24' }
 
         # By hand, unpuppetized step: # ifconfig eth5.1122 promisc
