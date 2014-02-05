e22e1ff5001d08314c085add6a75c16cdb97cddb
Matanya Moses <matanya@foss.co.il>
2014-02-05 16:40:20 +0200

    icinga: remove check_ram.sh
    follwoing : https://gerrit.wikimedia.org/r/#/c/111440/4 which was merged
    
    Change-Id: Id1de083faab62ab034f3beff289f5fb56cacaa41

--- manifests/misc/icinga.pp
+++ manifests/misc/icinga.pp
@@ -587,13 +587,6 @@ class icinga::monitor::files::nagios-plugins {
       owner => 'root',
       group => 'root',
       mode => '0755';
-#after absent everywhere, remove entire resource as it is not used
-    '/usr/lib/nagios/plugins/check_ram.sh':
-      ensure => 'absent',
-      source => 'puppet:///files/icinga/check_ram.sh',
-      owner => 'root',
-      group => 'root',
-      mode => '0755';
     '/usr/lib/nagios/plugins/check_graphite':
       source => 'puppet:///files/icinga/check_graphite',
       owner => 'root',
