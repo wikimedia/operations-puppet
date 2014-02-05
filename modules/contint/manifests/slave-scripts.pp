90acc9180e9a97d444ec366907047e45d5ee3fca
Antoine Musso <hashar@free.fr>
2014-02-05 14:35:36 +0100

    contint: fix slave-scripts deployment on labs
    We can not use git-deploy from tin.eqiad.wmnet to update instances in
    labs.  CI has a bunch of slaves that needs the slave-scripts repository
    to be kept up to date somehow, hence the use of git::clone in
    contint::slave-scripts.
    
    This patch:
    
    * prevents contint::slave-scripts on production since it will conflict
      with git-deploy
    * updates contint::slave-scripts clone path to match production ie:
      /srv/deployment/integration/slave-scripts
    * apply contint::slave-scripts to all labs slave by including it in
      role::ci::slave::labs::common
    
    Change-Id: I1795f325ed8afb8beb3d894ff5b0e8b89e7b085d

--- modules/contint/manifests/slave-scripts.pp
+++ modules/contint/manifests/slave-scripts.pp
@@ -1,8 +1,12 @@
 class contint::slave-scripts {
 
+    if $::realm == 'production' {
+        fail("contint::slave-scripts must not be used in production. Slaves are already git-deploy deployment targets.")
+    }
+
     git::clone { 'jenkins CI slave scripts':
         ensure    => 'latest',
-        directory => '/srv/slave-scripts',
+        directory => '/srv/deployment/integration/slave-scripts',
         origin    => 'https://gerrit.wikimedia.org/r/p/integration/jenkins.git',
     }
 
