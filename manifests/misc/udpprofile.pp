d7988ed434369a345c9f3425c20bb1d0105bd4e3
Matanya Moses <matanya@foss.co.il>
2014-02-05 13:52:19 +0200

    nrpe: move carbon checks to use nrpe::monitor_service
    Change-Id: I7b49723ecd00db17b66916cfe8e1a6387f7ff632

--- manifests/misc/udpprofile.pp
+++ manifests/misc/udpprofile.pp
@@ -13,8 +13,16 @@ class udpprofile::collector {
   }
 
   # Nagios monitoring (RT-2367)
-  monitor_service { "carbon-cache": description => "carbon-cache.py", check_command => "nrpe_check_carbon_cache" }
-  monitor_service { "profiler-to-carbon": description => "profiler-to-carbon", check_command => "nrpe_check_profiler_to_carbon" }
-  monitor_service { "profiling collector": description => "profiling collector", check_command => "nrpe_check_profiling_collector" }
-
+  nrpe::monitor_service { 'carbon-cache':
+    description  => 'carbon-cache.py',
+    nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:2 -a carbon-cache.py',
+  }
+  nrpe::monitor_service { 'profiler-to-carbon':
+    description   => 'profiler-to-carbon',
+    nrpe_command  => "/usr/lib/nagios/plugins/check_procs -c 1:1 --ereg-argument-array='^/usr/bin/python /usr/udpprofile/sbin/profiler-to-carbon",
+  }
+  nrpe::monitor_service { 'profiling collector':
+    description  => 'profiling collector',
+    nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1:20 -C collector",
+  }
 }
