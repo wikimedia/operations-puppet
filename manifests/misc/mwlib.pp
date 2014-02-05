aaa448c2676911a0ff8f06f24676b05bc42b37e9
Matanya Moses <matanya@foss.co.il>
2014-02-06 00:32:54 +0200

    mwlib: lint
    Change-Id: I8a13789f3d543b7a2ae99d039fd0f349bdf49aa9

--- manifests/misc/mwlib.pp
+++ manifests/misc/mwlib.pp
@@ -3,15 +3,40 @@
 # Installs needed packages for pediapress/mwlib.
 class misc::mwlib::packages {
 
-	package { [ "dvipng", "g++", "gcc", "imagemagick", "libfreetype6-dev", "libjpeg-dev",
-			"liblcms-dev", "libxml2-dev", "libxslt-dev", "libz-dev", "make", "ocaml-nox", "pdftk",
-			"ploticus", "python", "python-dev", "python-greenlet", "python-imaging", "python-lxml",
-			"python-pyparsing", "python-pypdf", "python-virtualenv", "texlive-latex-recommended" ]:
-		ensure   => 'installed',
-	}
+    package { [ 
+            'dvipng',
+            'g++',
+            'gcc',
+            'imagemagick',
+            'libfreetype6-dev',
+            'libjpeg-dev',
+            'liblcms-dev',
+            'libxml2-dev',
+            'libxslt-dev',
+            'libz-dev',
+            'make',
+            'ocaml-nox',
+            'pdftk',
+            'ploticus',
+            'python',
+            'python-dev',
+            'python-greenlet',
+            'python-imaging',
+            'python-lxml',
+            'python-pyparsing',
+            'python-pypdf',
+            'python-virtualenv',
+            'texlive-latex-recommended',
+        ]:
+        ensure   => 'installed',
+    }
 
 }
 
 class misc::mwlib::users {
-	generic::systemuser { "pp": name => "pp", home => "/opt/pp", shell => "/bin/bash" }
+    generic::systemuser { 'pp':
+        name  => 'pp',
+        home  => '/opt/pp',
+        shell => '/bin/bash',
+    }
 }
