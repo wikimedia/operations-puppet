df09f41d5331b96ba1d2cba4798d972c6e209968
Christian Aistleitner <christian@quelltextlich.at>
2014-02-06 21:32:45 +0100

    Reflect emery -> erbium move in documentation
    Change-Id: I6548ca51584f65b907aad4269e9c9c2e84350b66

--- manifests/misc/statistics.pp
+++ manifests/misc/statistics.pp
@@ -528,13 +528,13 @@ class misc::statistics::rsync::jobs::webrequest {
         destination => "/a/squid/archive/zero",
     }
 
-    # API logs from emery
+    # API logs from erbium
     misc::statistics::rsync_job { "api":
         source      => "erbium.eqiad.wmnet::udp2log/webrequest/archive/api-usage*.gz",
         destination => "/a/squid/archive/api",
     }
 
-    # sampled-1000 logs from emery
+    # sampled-1000 logs from erbium
     misc::statistics::rsync_job { "sampled_1000":
         source      => "erbium.eqiad.wmnet::udp2log/webrequest/archive/sampled-1000*.gz",
         destination => "/a/squid/archive/sampled",
