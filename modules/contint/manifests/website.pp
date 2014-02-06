a06f79eaa6f1d94f9f7c9457979e7cbc2373bb3f
Bryan Davis <bd808@wikimedia.org>
2014-02-06 15:51:52 -0700

    Send Vary header on http to http redirect
    The doc.wikimedia.org and integration.wikimedia.org hosts are pointing
    on the misc-varnish. HTTPS connections are terminated there and
    forwarded to gallium Apache on port 80.
    
    The Apache should redirect any request originally made with HTTP to its
    HTTPS version (that is the case when X-Forwarded-Proto is NOT https).
    
    The HTTP and HTTPS url thus need to vary in Varnish cache.  We achieve
    that by always adding a header Vary: X-Forwarded-Proto.  That would
    solve redirects loops and should ensure everyone is sent to HTTPS.
    
    Bug: 60822
    Change-Id: I83cef4b4d1c956ede13b9e124a046015962d7458

--- modules/contint/manifests/website.pp
+++ modules/contint/manifests/website.pp
@@ -7,6 +7,10 @@ class contint::website(
 
   require contint::publish-console
 
+  # Need to send Vary: X-Forwarded-Proto since most sites are forced to HTTPS
+  # and behind a varnish cache. See also bug 60822
+  apache_module { 'headers': name => 'headers' }
+
   # Static files in these docroots are in integration/docroot.git
 
   file { '/srv/org':
