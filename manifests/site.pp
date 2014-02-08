c8ec8308f65c70a961b69a87d5314d0ba355a7d5
dzahn <dzahn@wikimedia.org>
2014-02-07 17:48:20 -0800

    decom 'harmon' - rm from site/dsh/partman/dhcp
    RT #6267
    
    Change-Id: Idc44f926f3bf1ccda9065e3fb685672fb051604a

--- manifests/site.pp
+++ manifests/site.pp
@@ -990,13 +990,6 @@ node "gallium.wikimedia.org" {
     }
 }
 
-node "harmon.pmtpa.wmnet" {
-    $cluster = "misc"
-
-    include standard,
-        admins::roots
-}
-
 node "helium.eqiad.wmnet" {
     include standard,
         role::poolcounter,
