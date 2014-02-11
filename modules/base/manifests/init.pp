47239a7d2ca3608caa5687732eb0b8d33b0812bf
Faidon Liambotis <faidon@wikimedia.org>
2014-02-11 18:08:25 +0200

    base: remove initcwnd if-up.d hook for >= oneiric
    We currently have an if-up.d hook to set the TCP initial congestion
    window to 10*MSS on all boxes, but this is currently unneeded on all of
    our recent boxes. Linux upstream made 10 the default with commit
    442b9635c, which is included since 2.6.39, way before any of our precise
    systems.
    
    As checking for a running kernel is kind of evil, make the check
    conditional to lsbdistcodename, and remove the if-up hook in everything
    post-oneiric (natty = 2.6.38, oneiric = 3.0).
    
    Change-Id: Idfe83887b5601fe1c02766ea6d7121a2bf516ef4

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -360,18 +360,25 @@ class base::syslogs($readable = false) {
 class base::tcptweaks {
     Class[base::puppet] -> Class[base::tcptweaks]
 
-    file { '/etc/network/if-up.d/initcwnd':
-        ensure  => present,
-        content => template('base/initcwnd.erb'),
-        mode    => '0555',
-        owner   => 'root',
-        group   => 'root',
-    }
+    # unneeded since Linux 2.6.39, i.e. Ubuntu 11.10 Oneiric Ocelot
+    if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '11.10') < 0 {
+        file { '/etc/network/if-up.d/initcwnd':
+            ensure  => present,
+            content => template('base/initcwnd.erb'),
+            mode    => '0555',
+            owner   => 'root',
+            group   => 'root',
+        }
 
-    exec { '/etc/network/if-up.d/initcwnd':
-        require     => File['/etc/network/if-up.d/initcwnd'],
-        subscribe   => File['/etc/network/if-up.d/initcwnd'],
-        refreshonly => true,
+        exec { '/etc/network/if-up.d/initcwnd':
+            require     => File['/etc/network/if-up.d/initcwnd'],
+            subscribe   => File['/etc/network/if-up.d/initcwnd'],
+            refreshonly => true,
+        }
+    } else {
+        file { '/etc/network/if-up.d/initcwnd':
+            ensure  => absent,
+        }
     }
 }
 
