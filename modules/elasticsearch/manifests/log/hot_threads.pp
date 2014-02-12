5b940c882ecf807a6aab33f28a18087eefea90ab
Nik Everett <neverett@wikimedia.org>
2014-02-12 12:18:17 -0500

    Log Elasticsearch hot_threads
    Logs them once every five minutes when under low load and once every
    minute when load goes above half of CPUs.
    
    Change-Id: I436c91a60bcc93d6b8ca6c40409784b6649f0916

---
+++ modules/elasticsearch/manifests/log/hot_threads.pp
@@ -0,0 +1,24 @@
+# == Class: elasticsearch::log::hot_threads
+#
+# Install a cron job to log the hot threads.
+#
+class elasticsearch::log::hot_threads {
+    $script_name = 'elasticsearch_hot_threads_logger.py'
+    $script = "/usr/local/bin/$script_name"
+    $log = '/var/log/elasticsearch/elasticsearch_hot_threads.log'
+    file { $script:
+        source => "puppet:///modules/elasticsearch/$script_name",
+        mode   => '0555',
+    }
+
+    cron { 'elasticsearch-hot-threads-log':
+        command => "python $script 2>&1 >> $log",
+        #So the destination directory exists
+        require => Package['elasticsearch'],
+        user    => 'elasticsearch',
+        minute  => '*/5',
+    }
+
+    # The logrotate configuration for Elasticsearch will roll these logs just
+    # fine.
+}
