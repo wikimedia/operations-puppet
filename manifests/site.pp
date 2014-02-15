ae2d5e579fca3063e760d9533e91cdc6acadf060
Matanya Moses <matanya@foss.co.il>
2014-02-15 23:45:34 +0200

    remove shell access and key for mgrover
    RT: 6845
    Change-Id: I5598821f8246cb25d1470606f6644d1ff02615a8

--- manifests/site.pp
+++ manifests/site.pp
@@ -2248,8 +2248,8 @@ node "stat1.wikimedia.org" {
         accounts::spage,
         accounts::maryana,   # RT 3517
         accounts::mflaschen, # RT 4796
-        accounts::mgrover,   # RT 4600
         accounts::mlitn,     # RT 4959
+        accounts::mgrover,   # access revoked
         accounts::otto,
         accounts::reedy,
         accounts::rfaulk,    # RT 5040
