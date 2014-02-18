d2829a58cec7203e8a88feb6366df95ddd14d356
Coren <marc@uberbox.org>
2014-02-18 08:59:17 -0500

    Labs: Add labstore100[12] node entry
    Change-Id: Ib391486989e6c2c0a19cc69d8357c3cc8eb85841

--- manifests/site.pp
+++ manifests/site.pp
@@ -1271,6 +1271,27 @@ node /labstore[34]\.pmtpa\.wmnet/ {
     class { "ldap::role::client::labs": ldapincludes => $ldapincludes }
 }
 
+node /labstore100[12]\.eqiad\.wmnet/ {
+
+    $cluster = "labsnfs"
+    $ldapincludes = ['openldap', 'nss', 'utils']
+
+    $ganglia_aggregator = true
+
+    include standard,
+        openstack::project-nfs-storage-service,
+        rsync::server
+
+    rsync::server::module {
+        'pagecounts':
+            path        => '/srv/dumps/pagecounts',
+            read_only   => 'false',
+            hosts_allow => ['208.80.154.11', '208.80.152.185'];
+    }
+
+    class { "ldap::role::client::labs": ldapincludes => $ldapincludes }
+}
+
 node 'lanthanum.eqiad.wmnet' {
     include standard,
         admins::roots,
