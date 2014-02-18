165bb6c1b9649ce2e5361aa6b28403df2b1ff3ef
Ariel T. Glenn <ariel@wikimedia.org>
2014-02-18 13:31:05 +0200

    releases role indentation and style fixes
    Change-Id: I7d0c3b06802b3453dedbf5ffc2034ffae2c9f53c

--- manifests/role/releases.pp
+++ manifests/role/releases.pp
@@ -1,9 +1,9 @@
 class role::releases {
     system::role { 'releases': description => 'Releases webserver' }
 
-    monitor_service {
-        'http': description => 'HTTP',
-        check_command       => 'check_http',
+    monitor_service { 'http':
+        description     => 'HTTP',
+        check_command   => 'check_http',
     }
 
     class { '::releases':
