8ebe68a15efeff1e1182e0a31c7041931a855d83
Sean Pringle <springle@wikimedia.org>
2014-02-18 13:53:18 +1000

    limit to ops for now
    Change-Id: I45f1c50968d1e45bdbf93c9735efbd2ba44d77fa

--- manifests/role/tendril.pp
+++ manifests/role/tendril.pp
@@ -12,8 +12,8 @@ class role::tendril {
         docroot       => '/srv/tendril/web',
         ldap_binddn   => 'cn=proxyagent,ou=profile,dc=wikimedia,dc=org',
         ldap_authurl  => 'ldaps://virt0.wikimedia.org virt1000.wikimedia.org/ou=people,dc=wikimedia,dc=org?cn',
-        ldap_group    => 'cn=wmf,ou=groups,dc=wikimedia,dc=org',
-        auth_name     => 'WMF Labs (use wiki login name not shell)',
+        ldap_group    => 'cn=ops,ou=groups,dc=wikimedia,dc=org',
+        auth_name     => 'WMF Ops',
     }
 
 }
