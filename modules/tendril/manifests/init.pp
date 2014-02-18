45e4e357d0d97521b66ad08511e4ee9078923fd1
Sean Pringle <springle@wikimedia.org>
2014-02-18 12:49:58 +1000

    tendril add missing manifest
    Change-Id: I538ca62ede69a29671395f30eb4f10600d6040a6

---
+++ modules/tendril/manifests/init.pp
@@ -0,0 +1,28 @@
+# tendril: MariaDB Analytics
+
+# NOTE: this does not install tendril.. it could be git deployed,
+# but it hasn't been moved to a wmf repo.
+
+class tendril (
+    $site_name,
+    $docroot,
+    $ldap_binddn,
+    $ldap_authurl,
+    $ldap_group,
+    $auth_name,
+) {
+
+    include passwords::ldap::wmf_cluster
+    $proxypass = $passwords::ldap::wmf_cluster::proxypass
+
+    file { "/etc/apache2/sites-available/${site_name}":
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0440',
+        content => template("tendril/apache/${site_name}.erb");
+    }
+
+    apache_site { 'tendril': name => $site_name }
+
+}
