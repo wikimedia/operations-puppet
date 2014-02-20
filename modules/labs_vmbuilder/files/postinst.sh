249239dc3c555771219ffec57611bf4a4bcd175a
andrewbogott <abogott@wikimedia.org>
2014-02-20 11:47:03 -0600

    Clobber resolv.conf after we're done with our apt magic.
    Change-Id: If9a2fc150d8af7b12efed278859926b8125a6200

--- modules/labs_vmbuilder/files/postinst.sh
+++ modules/labs_vmbuilder/files/postinst.sh
@@ -3,8 +3,6 @@
 mkdir -p $1/root/.ssh
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAgEAqWmdNQm1reC0jxfBof510S6ffw/BDIXyFM38ij3PnkYEBOE4PB3KeHJN7NxUbWaXPBILxtqq5EUnCL9XQnDx82opCC9nChNO5KAoV3VqwNAAfh2UVFPt4PKLXiJjU9k76acp+s3i7uEG0wOVlsggjmbsiBq/Jwkne9oodhA75go1GVeDrP5WsXBGYcXXdjY8uU1P901YIatJoYYQSOOsGngGRZPUbTeFrGBaaoQsRCIiE1/byWs5yUuTbWjsBYDK+FoQsmmG5C98tN2g0llLhdbHTVutDEsvRLf28s3RGTTskfYhMgHbpA3coMIKvG8f2gWuk0wgMzI6G69BO87PsdyaS0aNn/DQILRBGaWTIbZwvjecCxXhXsJx0h1fdhTeXZAHljuY2s/7r8LXLk2eLzDi7RIO4PNtUrlRMZ9DgXFupfJXQSJiE54GCMHIHJ5qqGEveJFg7GZXlyxMDm2wUPyw2YYtQcM/RamnyEjakUM7X3eF45gY4ewabp+3IpP2iqkH3gz7IE/8aXtmqwa+SHNl1KxmGZ+9FFOuByM8wvDWaV320569ZGVAy9dxaH/mClLMx+xUKDxa4JN9dn9Yi+hnKoUhOYaBYXB+aPwydjTLMzp7rr1RgrLVGNk1R9WS2RHnEOSYxDmZbAht3v81F/c4mlzBQHQmXBKnNIxdHis= new_install' >$1/root/.ssh/authorized_keys
 chmod -R go= $1/root/.ssh
-rm -f $1/etc/resolv.conf
-rm -f $1/etc/resolvconf/resolv.conf.d/original
 
 chroot $1 passwd -ld root
 chroot $1 passwd -ld ubuntu
@@ -23,5 +21,8 @@ chroot $1 sed -i 's/console=hvc0/xencons=hvc0 console=hvc0/' /boot/grub/menu.lst
 chroot $1 rm -f /etc/sudo-ldap.conf
 chroot $1 ln -s /etc/ldap/ldap.conf /etc/sudo-ldap.conf
 chroot $1 useradd -r -d /var/lib/icinga -s /bin/false icinga
+
+rm -f $1/etc/resolv.conf
+rm -f $1/etc/resolvconf/resolv.conf.d/original
 # Ensure last call doesn't return a bad exit code
 echo ""
