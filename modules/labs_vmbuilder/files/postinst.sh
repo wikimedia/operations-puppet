616614787bbebbc63833a7df00c66c189aff01a0
Coren <marc@uberbox.org>
2014-02-20 12:15:26 -0500

    Add comment to clarify part of the postint.sh
    So that it's clear why there are no chroots
    in that section.
    
    Change-Id: I28f672c3c53d1f965f03eb5ddc30403aa44c6a59

--- modules/labs_vmbuilder/files/postinst.sh
+++ modules/labs_vmbuilder/files/postinst.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+##
+## Here we do not chroot since we're only manipulating the filesystem.
+## This allows redirection to work as expected.
+##
 mkdir -p $1/root/.ssh
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAgEAqWmdNQm1reC0jxfBof510S6ffw/BDIXyFM38ij3PnkYEBOE4PB3KeHJN7NxUbWaXPBILxtqq5EUnCL9XQnDx82opCC9nChNO5KAoV3VqwNAAfh2UVFPt4PKLXiJjU9k76acp+s3i7uEG0wOVlsggjmbsiBq/Jwkne9oodhA75go1GVeDrP5WsXBGYcXXdjY8uU1P901YIatJoYYQSOOsGngGRZPUbTeFrGBaaoQsRCIiE1/byWs5yUuTbWjsBYDK+FoQsmmG5C98tN2g0llLhdbHTVutDEsvRLf28s3RGTTskfYhMgHbpA3coMIKvG8f2gWuk0wgMzI6G69BO87PsdyaS0aNn/DQILRBGaWTIbZwvjecCxXhXsJx0h1fdhTeXZAHljuY2s/7r8LXLk2eLzDi7RIO4PNtUrlRMZ9DgXFupfJXQSJiE54GCMHIHJ5qqGEveJFg7GZXlyxMDm2wUPyw2YYtQcM/RamnyEjakUM7X3eF45gY4ewabp+3IpP2iqkH3gz7IE/8aXtmqwa+SHNl1KxmGZ+9FFOuByM8wvDWaV320569ZGVAy9dxaH/mClLMx+xUKDxa4JN9dn9Yi+hnKoUhOYaBYXB+aPwydjTLMzp7rr1RgrLVGNk1R9WS2RHnEOSYxDmZbAht3v81F/c4mlzBQHQmXBKnNIxdHis= new_install' >$1/root/.ssh/authorized_keys
 chmod -R go= $1/root/.ssh
