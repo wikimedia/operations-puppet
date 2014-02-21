87987c0d79d5c0c36c1c59a578976b87cd1f87fa
Tim Landscheidt <tim@tim-landscheidt.de>
2014-02-21 12:03:03 +0000

    Fix indentation in role::labs::instance
    Change-Id: I5efc76f3014642e780aab62e85f23c9ae0f9a321

--- manifests/role/labs.pp
+++ manifests/role/labs.pp
@@ -1,11 +1,12 @@
-
 class role::labs::instance {
 
     include ldap::role::client::labs,
-        base::instance-upstarts
+      base::instance-upstarts
 
     # make common logs readable
-    class {'base::syslogs': readable => true }
+    class { 'base::syslogs':
+        readable => true
+    }
 
     # Directory for data mounts
     file { '/data':
@@ -29,72 +30,104 @@ class role::labs::instance {
         $nfs_server = 'labstore.svc.eqiad.wmnet'
 
         mount { '/home':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "rw,${nfs_opts}",
-            device => "${nfs_server}:/project/${instanceproject}/home",
+            ensure  => mounted,
+            atboot  => true,
+            fstype  => 'nfs',
+            options => "rw,${nfs_opts}",
+            device  => "${nfs_server}:/project/${instanceproject}/home",
         }
 
         file { '/data/project':
-            ensure => directory, owner => 'root', group => 'root', mode => '0755',
+            ensure  => directory,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755',
             require => File['/data', '/etc/idmapd.conf'],
         }
         mount { '/data/project':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "rw,${nfs_opts}",
-            device => "${nfs_server}:/project/${instanceproject}/project",
+            ensure  => mounted,
+            atboot  => true,
+            fstype  => 'nfs',
+            options => "rw,${nfs_opts}",
+            device  => "${nfs_server}:/project/${instanceproject}/project",
             require => File['/data/project'],
         }
 
         file { '/data/scratch':
-            ensure => directory, owner => 'root', group => 'root', mode => '0755',
+            ensure  => directory,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755',
             require => File['/data', '/etc/idmapd.conf'],
         }
         mount { '/data/scratch':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "rw,${nfs_opts}",
-            device => "${nfs_server}:/scratch",
+            ensure  => mounted,
+            atboot  => true,
+            fstype  => 'nfs',
+            options => "rw,${nfs_opts}",
+            device  => "${nfs_server}:/scratch",
             require => File['/data/scratch'],
         }
 
         file { '/public/dumps':
-            ensure => directory, owner => 'root', group => 'root', mode => '0755',
+            ensure  => directory,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755',
             require => File['/public'],
         }
         mount { '/public/dumps':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "ro,${nfs_opts}",
-            device => "${nfs_server}:/dumps",
+            ensure  => mounted,
+            atboot  => true,
+            fstype  => 'nfs',
+            options => "ro,${nfs_opts}",
+            device  => "${nfs_server}:/dumps",
             require => File['/public/dumps'],
         }
 
         file { '/public/backups':
-            ensure => directory, owner => 'root', group => 'root', mode => '0755',
+            ensure  => directory,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755',
             require => File['/public'],
         }
         mount { '/public/backups':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "ro,${nfs_opts}",
-            device => "${nfs_server}:/backups",
+            ensure  => mounted,
+            atboot  => true,
+            fstype  => 'nfs',
+            options => "ro,${nfs_opts}",
+            device  => "${nfs_server}:/backups",
             require => File['/public/backups'],
         }
 
-
         file { '/public/keys':
-            ensure => directory, owner => 'root', group => 'root', mode => '0755',
+            ensure  => directory,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755',
             require => File['/public'],
         }
         mount { '/public/keys':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "ro,${nfs_opts}",
-            device => "${nfs_server}:/keys",
+            ensure  => mounted,
+            atboot  => true,
+            fstype  => 'nfs',
+            options => "ro,${nfs_opts}",
+            device  => "${nfs_server}:/keys",
             require => File['/public/keys'],
-            notify => Service['ssh'],
+            notify  => Service['ssh'],
         }
 
         service { 'idmapd':
-            ensure => running,
+            ensure    => running,
             subscribe => File['/etc/idmapd.conf'],
         }
 
         file { '/etc/idmapd.conf':
             ensure => present,
-            owner => root,
-            group => root,
-            mode => 0444,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
             source => 'puppet:///files/nfs/idmapd.conf',
         }
 
@@ -113,4 +146,3 @@ class role::labs::instance {
 
     }
 }
-
