4f6132b8f89bab0a735e998256e9476c6027b831
andrewbogott <abogott@wikimedia.org>
2014-02-24 02:30:03 -0600

    Add a script that updates labs instances after migration.
    This script will do nothing in tampa.  Once an instance
    is moved to eqiad the script will run once, then never again.
    
    It fixes puppet for the new dc.
    
    Change-Id: I7aed392e9935ff26a5f61df6d7487d474d02a731

---
+++ files/labs/migrate/migrate_firstboot.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -x
+
+FLAG="/var/log/migratefirstboot.log"
+if [ ! -f $FLAG ]; then
+    ip=`hostname -i`
+    if ( grep "eqiad" /etc/resolv.conf ); then
+        echo "Detected first boot in eqiad."
+
+        echo "Clearing out resolv.conf"
+        # resolf.conf will contain eqiad.wmflabs but it's otherwise scrambled by the
+        #  change of venue.  So, wipe it out and reboot for a fresh start.
+        rm /etc/resolv.conf
+
+        echo "Updating puppet host and cert"
+        id=`curl http://169.254.169.254/1.0/meta-data/instance-id 2> /dev/null`
+        idfqdn=${id}.eqiad.wmflabs
+        master=virt1000.wikimedia.org
+        sed -i "s/virt0.wikimedia.org/${master}/g" /etc/puppet/puppet.conf
+        sed -i "s/^certname = .*$/certname = ${idfqdn}/g" /etc/puppet/puppet.conf
+        find /var/lib/puppet -type f -print0 |xargs -0r rm
+
+        echo "forcing a puppet run"
+        puppet agent --onetime --verbose --no-daemonize --no-splay --show_diff --waitforcert=10
+
+        echo "Marking our work as done and rebooting"
+        touch $FLAG
+        reboot
+    fi
+fi
