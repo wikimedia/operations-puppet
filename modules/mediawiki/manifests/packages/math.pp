f09bf9c2d318f97e96cd1902421ac185012738ca
Antoine Musso <hashar@free.fr>
2014-02-24 11:39:18 +0100

    Describe Math related packages in a class
    This introduce the new mediawiki::packages::math puppet class which
    migrates the texvc related dependencies out of wikimedia-task-appserver.
    
    We need texlive packages installed on continuous integration server to
    run Math tests (bug 61090). Unfortunately we can not install the
    wikimedia-task-appserver there since it ship php-apc which we do not
    want on contint.
    
    texlive*, dvipng and gsfonts packages are removed from the Debian
    package with https://gerrit.wikimedia.org/r/#/c/115135/
    
    Bug: 61090
    Change-Id: Ifcf184b2f016f3dab6a1d32feab4162b7de51ca6

---
+++ modules/mediawiki/manifests/packages/math.pp
@@ -0,0 +1,48 @@
+# == mediawiki::packages::math
+#
+# Packages needed to render math using texvc.
+#
+# Extracted from the wikimedia-task-appserver package we used before puppet.
+#
+# This is a separate class so we can install the texlive packages without
+# having to install wikimedia-task-appserver, for example when we do not want
+# to get its dependencies installed (such as php5-apc on contint servers).
+
+class mediawiki::packages::math {
+
+    package { [
+        'dvipng',
+        'gsfonts',
+        'texlive',
+        'texlive-bibtex-extra',
+        'texlive-font-utils',
+        'texlive-fonts-extra',
+        'texlive-lang-croatian',
+        'texlive-lang-cyrillic',
+        'texlive-lang-czechslovak',
+        'texlive-lang-danish',
+        'texlive-lang-dutch',
+        'texlive-lang-finnish',
+        'texlive-lang-french',
+        'texlive-lang-german',
+        'texlive-lang-greek',
+        'texlive-lang-hungarian',
+        'texlive-lang-italian',
+        'texlive-lang-latin',
+        'texlive-lang-mongolian',
+        'texlive-lang-norwegian',
+        'texlive-lang-other',
+        'texlive-lang-polish',
+        'texlive-lang-portuguese',
+        'texlive-lang-spanish',
+        'texlive-lang-swedish',
+        'texlive-lang-vietnamese',
+        'texlive-latex-extra',
+        'texlive-math-extra',
+        'texlive-pictures',
+        'texlive-pstricks',
+        'texlive-publishers',
+        ]: ensure => present
+    }
+
+}
