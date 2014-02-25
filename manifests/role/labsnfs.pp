a8a18c2d87c65ef73d35ddfec8a9e86bd98931e7
andrewbogott <abogott@wikimedia.org>
2014-02-24 19:55:34 -0600

    Turn the labs::nfs::client class off for eqiad.
    eqiad is NFS by default, and doesn't need this.
    
    Also some lint cleanup while I'm in here.
    
    Change-Id: Ic1749e3e93808f1bc380a7d975e76fed96b0c10d

--- manifests/role/labsnfs.pp
+++ manifests/role/labsnfs.pp
@@ -2,40 +2,45 @@
 # in LDAP once transition is over.
 
 class role::labsnfs::client {
-	$nfscluster = "labnfs.pmtpa.wmnet"
 
-	file {
-		"/etc/auto.master":
-			ensure => file,
-			owner => root,
-			group => root,
-			mode => 0444,
-			content => template("labsnfs/auto.master.erb");
-	}
-	file { "/etc/auto.space":
-			ensure => file,
-			owner => root,
-			group => root,
-			mode => 0444,
-			content => template("labsnfs/auto.space.erb");
-	}
-	file { "/etc/auto.time.home":
-			ensure => file,
-			owner => root,
-			group => root,
-			mode => 0444,
-			content => template("labsnfs/auto.time.home.erb");
-	}
-	file { "/etc/auto.time.project":
-			ensure => file,
-			owner => root,
-			group => root,
-			mode => 0444,
-			content => template("labsnfs/auto.time.project.erb");
-	}
+    if $::site == 'pmtpa' {
+        $nfscluster = 'labnfs.pmtpa.wmnet'
 
-	file { "/etc/init/nfs-noidmap.conf":
-		source => "puppet:///files/nfs/upstart-nfs-noidmap.conf"
-	}
+        file {
+            '/etc/auto.master':
+                ensure => file,
+                owner => root,
+                group => root,
+                mode => 0444,
+                content => template('labsnfs/auto.master.erb');
+        }
+        file { '/etc/auto.space':
+                ensure => file,
+                owner => root,
+                group => root,
+                mode => 0444,
+                content => template('labsnfs/auto.space.erb');
+        }
+        file { '/etc/auto.time.home':
+                ensure => file,
+                owner => root,
+                group => root,
+                mode => 0444,
+                content => template('labsnfs/auto.time.home.erb');
+        }
+        file { '/etc/auto.time.project':
+                ensure => file,
+                owner => root,
+                group => root,
+                mode => 0444,
+                content => template('labsnfs/auto.time.project.erb');
+        }
+
+        file { '/etc/init/nfs-noidmap.conf':
+            source => 'puppet:///files/nfs/upstart-nfs-noidmap.conf'
+        }
+    } else {
+        warning('Outside of pmtpa the role::labsnfs::client class is deprecated and does nothing.');
+    }
 }
 
