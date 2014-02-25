wikimedia/operations-puppet

commit 49f9f658a79a8d872b69acbc4dc7112ffc8611d4

Adding $brokers_array variable to role kafka config
Change-Id: Iae2a8d5ebd051a4b7905c2ec179b7b279c52bccf

manifests/role/analytics/kafka.pp
@@ -81,6 +81,7 @@ class role::analytics::kafka::config {
     }
 
     $brokers          = $cluster[$kafka_cluster_name]
+    $brokers_array    = keys($brokers)
     $zookeeper_hosts  = $role::analytics::zookeeper::config::hosts_array
     $zookeeper_chroot = "/kafka/${kafka_cluster_name}"
     $zookeeper_url    = inline_template("<%= zookeeper_hosts.sort.join(',') %><%= zookeeper_chroot %>")

commit a9783f21113b7867103a80b7ff47c01193ed37bb

Random stab at fixing breakage in 49f9f658
49f9f658 breaks puppet on prod caches with:
Feb 25 22:33:45 cp4012 puppet-agent[3962]: Could not retrieve catalog from remote server: Error 400 on SERVER: keys(): Requires hash to work with at /etc/puppet/manifests/role/analytics/kafka.pp:84 on node cp4012.ulsfo.wmnet

Change-Id: I876019bb1b6169aee6fb176a86e015c334e6c430

manifests/role/analytics/kafka.pp
@@ -54,9 +54,9 @@ class role::analytics::kafka::config {
                 'analytics1021.eqiad.wmnet' => { 'id' => 21 },
                 'analytics1022.eqiad.wmnet' => { 'id' => 22 },
             },
-            # 'ulsfo' => { },
-            # 'pmtpa' => { },
-            # 'esams' => { },
+            'ulsfo' => { },
+            'pmtpa' => { },
+            'esams' => { },
         }
 
         # production Kafka uses a bunch of JBOD log_dir mounts.