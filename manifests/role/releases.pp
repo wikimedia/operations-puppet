a42ad3c935c84630351d94e6d0224a7fda36dde8
jgreen <jgreen@wikimedia.org>
2014-02-26 10:44:11 -0500

    grant hashar,reedy access to caesium per RT 6861
    Change-Id: If49d13d1bf56eefc1c8f553335c2192bcf0923b4

--- manifests/role/releases.pp
+++ manifests/role/releases.pp
@@ -13,6 +13,8 @@ class role::releases {
 
     role::releases::access { 'brion': group => 'mobileupld' }
     role::releases::access { 'csteipp': group => 'mwupld' }
+    role::releases::access { 'hashar': group => 'mwupld' } # RT 6861
+    role::releases::access { 'reedy': group => 'mwupld' } # RT 6861
 }
 
 class role::releases::groups {
