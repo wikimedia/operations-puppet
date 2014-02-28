8c761f36576d2ae4b30007fc72ad4a4de273d854
dzahn <dzahn@wikimedia.org>
2014-02-27 22:58:27 -0800

    WIP - turn RT from misc/* into puppet module
    move RT from misc/rt-server-apache to module
    structure
    
    add role class, move variables into role,
    formatting, quoting, misc..
    
    seperate into classes by function:
    
    - packages
    - config
    - forms
    - plugins
    - apache
    
    move all the apache related stuff together,
    make separate file for installing more plugins,
    same for the password reset form which still has issues,
    
    move the files into module and fix "source"s
    
    move the templates into module and fix "content"s
    
    remove 2-spaces,.more..
    
    Change-Id: Ib0e7476a612b25f37905f9f475521606d3bb73d7

---
+++ manifests/role/requesttracker.pp
@@ -0,0 +1,29 @@
+# RT: Request Tracker
+# http://www.bestpractical.com/rt/
+class role::requesttracker {
+
+    system::role { 'requesttracker': description => 'RT (Request Tracker) server' }
+
+    # config - labs vs. production
+    case $::realm {
+        'labs': {
+            $rt_site = 'rt.wmflabs.org'
+        }
+        'production': {
+            $rt_site = 'rt.wikimedia.org'
+        }
+        'default': {
+            fail('unknown realm, should be labs or production')
+        }
+    }
+
+    # main (init.pp from module)
+    class { '::requesttracker':
+        apache_site => $rt_site,
+        dbhost      => 'localhost',
+        dbport      => '3306',
+        datadir     => '/var/lib/mysql',
+    }
+
+}
+
