9f3923eb164c5e574bb538c4b037587080d1b4d1
Ori Livneh <ori@wikimedia.org>
2014-03-03 21:23:55 -0800

    Enable GeoIP cookie on Labs
    Change-Id: Id13f23cef9cbf6d5f368d9a47d761919f1f6b431

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -645,6 +645,8 @@ class role::cache {
             wikimedia_networks => $wikimedia_networks,
         }
 
+        $text_enable_geo = ( $::realm == 'labs' )
+
         varnish::instance { "text-frontend":
             name => "frontend",
             vcl => "text-frontend",
@@ -674,7 +676,7 @@ class role::cache {
                     'weight' => $backend_weight,
                 }],
             cluster_options => {
-                'enable_geoiplookup' => false,
+                'enable_geoiplookup' => $text_enable_geo,
             },
         }
 
