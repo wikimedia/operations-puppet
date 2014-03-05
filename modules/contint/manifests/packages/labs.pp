a50167ebe5eaec1eb243e7ac7aa74a76d23d0fe7
Antoine Musso <hashar@free.fr>
2014-03-05 12:44:30 +0100

    contint: install hhvm on labs slaves
    Will let us run the MediaWiki core test suite :-]
    
    Change-Id: I7db6e70ff3d06b3491a4ebbf8a5460473d1c13b7

--- modules/contint/manifests/packages/labs.pp
+++ modules/contint/manifests/packages/labs.pp
@@ -25,6 +25,9 @@ class contint::packages::labs {
         ]: ensure => present,
     }
 
+    # Facebook Hiphop virtual machine
+    package { 'hhvm': ensure => present }
+
     # Bring tox/virtualenv... from pip  bug 44443
     # TODO: Reevaluate this once we switch to trusty. Maybe provider being apt
     # would be better then
