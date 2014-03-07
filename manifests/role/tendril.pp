33e5e8dd733f02154896ee63362d5f712ce68ec2
Sean Pringle <springle@wikimedia.org>
2014-03-07 11:09:36 +1000

    ops ldap group too restrictive
    Change-Id: I85caeecb99fd9b6d36063b9769f06ee7a6f966c0

--- manifests/role/tendril.pp
+++ manifests/role/tendril.pp
@@ -12,7 +12,7 @@ class role::tendril {
         docroot       => '/srv/tendril/web',
         ldap_binddn   => 'cn=proxyagent,ou=profile,dc=wikimedia,dc=org',
         ldap_authurl  => 'ldaps://virt0.wikimedia.org virt1000.wikimedia.org/ou=people,dc=wikimedia,dc=org?cn',
-        ldap_group    => 'cn=ops,ou=groups,dc=wikimedia,dc=org',
+        ldap_group    => 'cn=wmf,ou=groups,dc=wikimedia,dc=org',
         auth_name     => 'WMF Ops',
     }
 
