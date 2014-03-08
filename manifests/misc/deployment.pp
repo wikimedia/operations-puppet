f0ce18ec760daea10d11febaff71e043612907dc
Ori.livneh <ori@wikimedia.org>
2014-03-08 07:51:35 +0000

    Revert "Create global /etc/gitconfig for deployment hosts"
    Git::Clone resources shell out to git as root but have no SSH key for Gerrit.
    
    This reverts commit 20b5573468e32cd1c2a25dfb003b3528f5418289.
    
    Change-Id: Icbf0d05ee5fbfd3d28daedd7dd6b2506d040b6bd

--- manifests/misc/deployment.pp
+++ manifests/misc/deployment.pp
@@ -422,10 +422,3 @@ class misc::deployment::scap_primary {
 			hosts_allow => ['10.0.0.0/16', '10.64.0.0/22', '10.64.16.0/24', '208.80.152.0/22', '10.64.32.0/22'];
     }
 }
-
-class misc::deployment::gitconfig {
-	exec { 'git url rewriting':
-		command => 'git config --global url."ssh://gerrit.wikimedia.org:29418/".insteadOf "https://gerrit.wikimedia.org/r/p/"',
-		unless => 'git config --global url."ssh://gerrit.wikimedia.org:29418/".insteadOf | grep -q "https://gerrit.wikimedia.org/r/p/"';
-	}
-}
