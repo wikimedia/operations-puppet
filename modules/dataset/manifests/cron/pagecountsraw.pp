2b6a87c0a3f9f3aacea620ab2b73f366fb517bde
Ariel T. Glenn <ariel@wikimedia.org>
2014-03-10 14:56:51 +0200

    fix up page count copy script perms, require the user that runs them
    Change-Id: I948357604f1732e147c67b0c93634d5bba36d412

--- modules/dataset/manifests/cron/pagecountsraw.pp
+++ modules/dataset/manifests/cron/pagecountsraw.pp
@@ -17,48 +17,51 @@ class dataset::cron::pagecountsraw(
     }
 
     file { '/usr/local/bin/daily-pagestats-copy.sh':
-        mode    => '0444',
+        mode    => '0755',
         owner   => 'root',
         group   => 'root',
         source  => 'puppet:///modules/dataset/pagecounts/daily-pagestats-copy.sh',
     }
     file { '/usr/local/bin/generate-pagecount-main-index.sh':
-        mode    => '0444',
+        mode    => '0755',
         owner   => 'root',
         group   => 'root',
         source  => 'puppet:///modules/dataset/pagecounts/generate-pagecount-main-index.sh',
     }
     file { '/usr/local/bin/generate-pagecount-year-index.sh':
-        mode    => '0444',
+        mode    => '0755',
         owner   => 'root',
         group   => 'root',
         source  => 'puppet:///modules/dataset/pagecounts/generate-pagecount-year-index.sh',
     }
     file { '/usr/local/bin/generate-pagecount-year-month-index.sh':
-        mode    => '0444',
+        mode    => '0755',
         owner   => 'root',
         group   => 'root',
         source  => 'puppet:///modules/dataset/pagecounts/generate-pagecount-year-month-index.sh',
     }
 
-    include accounts::datasets
+    include "accounts::$user"
 
-    file { '/home/datasets/.ssh/pagecounts_rsync_key':
+    file { "/home/${user}/.ssh/pagecounts_rsync_key":
         mode    => '0400',
-        owner   => 'datasets',
+        owner   => $user,
         group   => 'root',
         source  => 'puppet:///private/datasets/pagecounts_rsync_key',
+        require => User[$user],
     }
 
     cron { 'pagestats-raw':
-        ensure  => $ensure,
-        command => "/usr/local/bin/daily-pagestats-copy.sh ${user} /home/${user}/.ssh/${key} ${from} /a/webstats/dumps /data/pagecounts/incoming /data/xmldatadumps/public/other/pagecounts-raw",
-        user    => $user,
-        minute  => '21',
-        require => [File['/usr/local/bin/daily-pagestats-copy.sh'],
-                   File['/usr/local/bin/generate-pagecount-main-index.sh'],
-                   File['/usr/local/bin/generate-pagecount-year-index.sh'],
-                   File['/usr/local/bin/generate-pagecount-year-month-index.sh'],
-                   File['/home/datasets/.ssh/pagecounts_rsync_key']],
+        ensure      => $ensure,
+        command     => "/usr/local/bin/daily-pagestats-copy.sh ${user} /home/${user}/.ssh/${key} ${from} /a/webstats/dumps /data/pagecounts/incoming /data/xmldatadumps/public/other/pagecounts-raw",
+        environment => 'MAILTO=ops-dumps@wikimedia.org',
+        user        => $user,
+        minute      => '21',
+        require     => [File['/usr/local/bin/daily-pagestats-copy.sh'],
+                       File['/usr/local/bin/generate-pagecount-main-index.sh'],
+                       File['/usr/local/bin/generate-pagecount-year-index.sh'],
+                       File['/usr/local/bin/generate-pagecount-year-month-index.sh'],
+                       File["/home/$user/.ssh/pagecounts_rsync_key"],
+                       User[$user]],
     }
 }
