db35978959f870c105c754046ea46f8d76736dc2
Coren <marc@uberbox.org>
2014-03-11 16:35:01 -0400

    Last tweaks to rsync of dumps to labs
    - Tweak rsync args
    
    Change-Id: I3f353d66e41188f96316af445fcdc9ebd5ecab3f

--- modules/dataset/files/labs/labs-rsync-cron.sh
+++ modules/dataset/files/labs/labs-rsync-cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 running=`pgrep -u root -f   'python /root/wmfdumpsmirror.py --remotedir /data/xmldatadumps/public'`
 if [ -z "$running" ]; then
-    python /usr/local/bin/wmfdumpsmirror.py --remotedir /data/xmldatadumps/public --localdir /mnt/dumps/public --filesperjob 50 --sizeperjob 5G --workercount 3 --rsynclist rsync-list.txt.rsync
+    python /usr/local/bin/wmfdumpsmirror.py --remotedir /data/xmldatadumps/public --localdir /mnt/dumps/public --filesperjob 50 --sizeperjob 5G --workercount 3 --rsynclist rsync-list.txt.rsync --rsyncargs -rlptq
 fi
 running=`pgrep -u root -f -x  '/usr/bin/rsync -rlpt /data/xmldatadumps/public/other/incr /mnt/dumps/'`
 if [ -z "$running" ]; then
