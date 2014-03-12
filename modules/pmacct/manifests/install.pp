6267c4829196bb45eeaf3a4aa3e9a6026dcb041e
Joel Krauska <jkrauska@wikimedia.org>
2014-03-12 15:16:51 -0700

    Initial commit of pmacct module and role
    apply some simple puppet-lint fixes
    
    Change-Id: I44f02a7911ac8f596f78ea0a6ae5a61c72e75e5c

---
+++ modules/pmacct/manifests/install.pp
@@ -0,0 +1,56 @@
+# Class: pmacct::install
+#
+# This installs and mangages pmacct configuration
+# http://www.pmacct.net/
+class pmacct::install {
+
+    # Package
+    # Must be built with these configure flags
+    # --enable-mysql --enable-64bit --enable-threads --enable-geoip
+    package { 'pmacct':
+        ensure => installed,
+    }
+
+    # User creation (not done by package)
+    generic::systemuser { 'pmacct':
+        name    => 'pmacct',
+        home    => '/var/lib/pmacct',
+        shell   => '/bin/bash',
+        require => Package['pmacct'],
+    }
+
+    # Log directory
+    file { '/var/log/pmacct':
+        ensure  => 'directory',
+        owner   => 'pmacct',
+        group   => 'pmacct',
+        mode    => '0750',
+        require => Package['pmacct'],
+    }
+
+    # Config directory
+    file { '/etc/pmacct':
+        ensure  => 'directory',
+        owner   => 'pmacct',
+        group   => 'pmacct',
+        mode    => '0750',
+        require => Package['pmacct'],
+    }
+
+    # Pretag map file
+    file { '/etc/pmacct/pretag.map':
+      ensure  => present,
+      owner   => 'pmacct',
+      group   => 'pmacct',
+      mode    => '0440',
+      require => File['/etc/pmacct'],
+    }
+
+    # Populate pretag file
+    file_line { 'HEADER':
+      line    => '# WARNING: This file is maintained by puppet.',
+      path    => '/etc/pmacct/pretag.map',
+      require => File['/etc/pmacct/pretag.map'],
+    }
+
+}
