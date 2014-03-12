31a3eb40046dde299cdfbedd2e7e468d28832ffb
Leslie <geekgirl@gmail.com>
2014-03-12 15:16:51 -0700

    Initial commit of pmacct module and role
    patchset 2 includes fixes suggested by dzahn
    
    Change-Id: I44f02a7911ac8f596f78ea0a6ae5a61c72e75e5c

---
+++ modules/pmacct/manifests/configs.pp
@@ -0,0 +1,26 @@
+# pmacct::makeconfig
+# Generates a unique config file per device and pretag file.
+define pmacct::configs ($name, $port, $ip, $samplerate) {
+    # Single confile file per device
+    file { "${pmacct::home}/configs/config-${name}.cfg":
+        ensure  => 'file',
+        owner   => 'pmacct',
+        group   => 'pmacct',
+        mode    => '0640',
+        content => template('pmacct/config.erb'),
+        require => File [ "${pmacct::home}/configs" ],
+    }
+
+    # Populate pretag file
+    file_line { "Port ${port}":
+      line => "set_tag=${port} ip=${ip}",
+      path => "${pmacct::home}/configs/pretag.map",
+    }
+
+    # Corresponding ferm rule for firewall redirect
+    ferm::rule {"${name}-BGP":
+        rule  => "proto tcp dport 179 source ${ip} REDIRECT to-ports ${port}",
+        table => 'nat',
+        chain => 'PREROUTING',
+    }
+}
