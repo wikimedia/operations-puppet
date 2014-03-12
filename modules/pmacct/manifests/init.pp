84eb88587b724f0673cf27c677652698f5e404cc
Leslie <geekgirl@gmail.com>
2014-03-12 15:16:51 -0700

    Initial commit of pmacct module and role
    move working directory, change file permissions, moved to role
    
    Change-Id: I44f02a7911ac8f596f78ea0a6ae5a61c72e75e5c

---
+++ modules/pmacct/manifests/init.pp
@@ -0,0 +1,22 @@
+# Class: pmacct
+#
+# This installs and mangages pmacct configuraiton
+# http://www.pmacct.net/
+#
+# Will initially be added to node 'netmon1001'
+
+class pmacct (
+        $pmacct_database, 
+        $pmacct_agents
+) {
+    # Install package and make sure user and directories are set
+    include pmacct::install
+
+    # Iterate over the device list to create new configs
+    # FIXME: Review daniel's different method for iterating over a hash..
+    create_resources(pmacct::configs, $pmacct_agents)
+
+    service {'pmacctd':
+        ensure => running;
+    }
+}
