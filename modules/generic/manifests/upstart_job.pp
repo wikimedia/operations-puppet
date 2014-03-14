wikimedia/operations-puppet

commit 4887dc90a227227051a30103a664975959b65ddb

generic: lint clean
Change-Id: Idaf1638068fc189d8f69be641bb211a419427a34

modules/generic/manifests/upstart_job.pp
@@ -1,30 +1,30 @@
 # Create a symlink in /etc/init.d/ to a generic upstart init script
-define generic::upstart_job($install="false", $start="false") {
-	# Create symlink
-	file { "/etc/init.d/${title}":
-		ensure  => link,
-		owner   => 'root',
-		group   => 'root',
-		target  => '/lib/init/upstart-job',
-	}
+define generic::upstart_job($install=false, $start=false) {
+    # Create symlink
+    file { "/etc/init.d/${title}":
+        ensure  => link,
+        owner   => 'root',
+        group   => 'root',
+        target  => '/lib/init/upstart-job',
+    }
 
-	if $install == "true" {
-		file { "/etc/init/${title}.conf":
-			owner   => 'root',
-			group   => 'root',
-			mode	=> '0444',
-			source  => "puppet:///modules/generic/upstart/${title}.conf",
-		}
-	}
+    if $install == true {
+        file { "/etc/init/${title}.conf":
+            owner    => 'root',
+            group   => 'root',
+            mode    => '0444',
+            source  => "puppet:///modules/generic/upstart/${title}.conf",
+        }
+    }
 
-	if $start == "true" {
-		exec { "start $title":
-			require => File["/etc/init/${title}.conf"],
-			subscribe => File["/etc/init/${title}.conf"],
-			refreshonly => true,
-			command => "start ${title}",
-			unless => "status ${title} | grep -q start/running",
-			path => "/bin:/sbin:/usr/bin:/usr/sbin"
-		}
-	}
+    if $start == true {
+        exec { "start ${title}":
+            require     => File["/etc/init/${title}.conf"],
+            subscribe   => File["/etc/init/${title}.conf"],
+            refreshonly => true,
+            command     => "start ${title}",
+            unless      => "status ${title} | grep -q start/running",
+            path        => '/bin:/sbin:/usr/bin:/usr/sbin',
+        }
+    }
 }

modules/generic/manifests/debconf/set.pp
@@ -3,13 +3,13 @@
 #
 # Parameters:
 # - $title
-#		Debconf setting, e.g. mailman/used_languages
+#       Debconf setting, e.g. mailman/used_languages
 # - $value
-#		The value $title should be set to
+#       The value $title should be set to
 define generic::debconf::set($value) {
-	exec { "debconf-communicate set $title":
-		path => "/usr/bin:/usr/sbin:/bin:/sbin",
-		command => "echo set ${title} \"${value}\" | debconf-communicate",
-		unless => "test \"$(echo get ${title} | debconf-communicate)\" = \"0 ${value}\""
-	}
+    exec { "debconf-communicate set ${title}":
+        path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+        command => "echo set ${title} \"${value}\" | debconf-communicate",
+        unless  => "test \"$(echo get ${title} | debconf-communicate)\" = \"0 ${value}\""
+    }
 }

modules/generic/manifests/higher_min_free_kbytes.pp
@@ -1,11 +1,9 @@
 class generic::higher_min_free_kbytes {
-	# Set a high min_free_kbytes watermark.
-	# See https://wikitech.wikimedia.org/wiki/Dataset1001#Feb_8_2012
-	# FIXME: Is this setting appropriate to the nodes on which it is applied? Is
-	# the value optimal? Investigate.
-	sysctl::parameters { 'higher_min_free_kbytes':
-		values => {
-			'vm.min_free_kbytes' => 1024 * 256,
-		},
-	}
+# Set a high min_free_kbytes watermark.
+# See https://wikitech.wikimedia.org/wiki/Dataset1001#Feb_8_2012
+# FIXME: Is this setting appropriate to the nodes on which it is applied? Is
+# the value optimal? Investigate.
+    sysctl::parameters { 'higher_min_free_kbytes':
+        values => { 'vm.min_free_kbytes' => 1024 * 256,},
+    }
 }

modules/generic/manifests/locales/international.pp
@@ -1,20 +1,20 @@
 # this installs a bunch of international locales, f.e. for "planet" on singer
 class generic::locales::international {
 
-	package { 'locales':
-		ensure => latest;
-	}
+    package { 'locales':
+        ensure => latest,
+    }
 
-	file { "/var/lib/locales/supported.d/local":
-		source => "puppet:///modules/generic/locales/local_int",
-		owner => "root",
-		group => "root",
-		mode => 0444;
-	}
+    file { '/var/lib/locales/supported.d/local':
+        source => 'puppet:///modules/generic/locales/local_int',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
 
-	exec { "/usr/sbin/locale-gen":
-		subscribe => File["/var/lib/locales/supported.d/local"],
-		refreshonly => true,
-		require => File["/var/lib/locales/supported.d/local"];
-	}
+    exec { '/usr/sbin/locale-gen':
+        subscribe   => File['/var/lib/locales/supported.d/local'],
+        refreshonly => true,
+        require     => File['/var/lib/locales/supported.d/local'],
+    }
 }

modules/generic/manifests/systemuser.pp
@@ -1,26 +1,28 @@
 # Creates a system username with associated group, random uid/gid, and /bin/false as shell
-define generic::systemuser($name, $home=undef, $managehome=true, $shell="/bin/false", $groups=undef, $default_group=$name, $ensure=present) {
-	# FIXME: deprecate $name parameter in favor of just using $title
+define generic::systemuser($name, $home=undef, $managehome=true, $shell='/bin/false', $groups=undef, $default_group=$name, $ensure=present) {
+    # FIXME: deprecate $name parameter in favor of just using $title
 
-	if $default_group == $name {
-		group { $default_group:
-			name => $default_group,
-			ensure => present;
-		}
-	}
+    if $default_group == $name {
+        group { $default_group:
+            ensure => present,
+            name   => $default_group,
+        }
+    }
 
-	user { $name:
-		require => Group[$default_group],
-		name => $name,
-		gid => $default_group,
-		home => $home ? {
-			undef => "/var/lib/${name}",
-			default => $home
-		},
-		managehome => $managehome,
-		shell => $shell,
-		groups => $groups,
-		system => true,
-		ensure => $ensure;
-	}
+    $whereis_home = $home ? {
+        undef   => "/var/lib/${name}",
+        default => $home
+    }
+
+    user { $name:
+        ensure     => $ensure,
+        require    => Group[$default_group],
+        name       => $name,
+        gid        => $default_group,
+        home       => $whereis_home,
+        managehome => $managehome,
+        shell      => $shell,
+        groups     => $groups,
+        system     => true,
+    }
 }

modules/generic/manifests/wikidev-umask.pp
@@ -1,12 +1,10 @@
 class generic::wikidev-umask {
-
-	# set umask to 0002 for wikidev users, per RT-804
-	file {
-		"/etc/profile.d/umask-wikidev.sh":
-			ensure => present,
-			owner => root,
-			group => root,
-			mode => 0444,
-			source => "puppet:///modules/generic/environment/umask-wikidev-profile-d.sh";
-	}
+    # set umask to 0002 for wikidev users, per RT-804
+    file { '/etc/profile.d/umask-wikidev.sh':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/generic/environment/umask-wikidev-profile-d.sh',
+    }
 }

commit 7afabf6fec12496dfd12e5efa953381f968f41bf

Restore generic::upstart_job parameters
The lint of the 'generic' module caused the parameters passed to
generic::upstart_job() to be checked as boolean instead of the string as
they used to be.

The parent call have not been updated and are still passing the boolean
as string which cause the condition to never be matched.

This patch let the define to recognize string and boolean values.

Regression introduced by: 4887dc90
  https://gerrit.wikimedia.org/r/#/c/107037/

Change-Id: I03363419c238b696f83b636dc84a79e6a95436d1

modules/generic/manifests/upstart_job.pp
@@ -8,7 +8,7 @@ define generic::upstart_job($install=false, $start=false) {
         target  => '/lib/init/upstart-job',
     }
 
-    if $install == true {
+    if $install == true or $install == 'true' {
         file { "/etc/init/${title}.conf":
             owner    => 'root',
             group   => 'root',
@@ -17,7 +17,7 @@ define generic::upstart_job($install=false, $start=false) {
         }
     }
 
-    if $start == true {
+    if $start == true or $start == 'true' {
         exec { "start ${title}":
             require     => File["/etc/init/${title}.conf"],
             subscribe   => File["/etc/init/${title}.conf"],