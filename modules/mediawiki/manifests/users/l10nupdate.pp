wikimedia/operations-puppet

commit 577ea6aca5327d7b5f962f256748f5c629714be3

beta: skip l10nupdate user/group creation
I created a l10nupdate user on wikitech and Coren created a l10nupdate
group.  Thus we do not need puppet to create them when on labs.

- changed: create home for LDAP account
- changed: fix dependencies accordingly
- spaces!

Bug: 62529
Bug: 62595
Change-Id: I005953dd65579893980a80f3e3341520a0281d18

modules/mediawiki/manifests/users/l10nupdate.pp
@@ -1,24 +1,41 @@
 # mediawiki l10nupdate user
 class mediawiki::users::l10nupdate {
-	## l10nupdate user
-	$authorized_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAzcA/wB0uoU+XgiYN/scGczrAGuN99O8L7m8TviqxgX9s+RexhPtn8FHss1GKi8oxVO1V+ssABVb2q0fGza4wqrHOlZadcFEGjQhZ4IIfUwKUo78mKhQsUyTd5RYMR0KlcjB4UyWSDX5tFHK6FE7/tySNTX7Tihau7KZ9R0Ax//KySCG0skKyI1BK4Ufb82S8wohrktBO6W7lag0O2urh9dKI0gM8EuP666DGnaNBFzycKLPqLaURCeCdB6IiogLHiR21dyeHIIAN0zD6SUyTGH2ZNlZkX05hcFUEWcsWE49+Ve/rdfu1wWTDnourH/Xm3IBkhVGqskB+yp3Jkz2D3Q== l10nupdate@fenari'
+    ## l10nupdate user
+    $authorized_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAzcA/wB0uoU+XgiYN/scGczrAGuN99O8L7m8TviqxgX9s+RexhPtn8FHss1GKi8oxVO1V+ssABVb2q0fGza4wqrHOlZadcFEGjQhZ4IIfUwKUo78mKhQsUyTd5RYMR0KlcjB4UyWSDX5tFHK6FE7/tySNTX7Tihau7KZ9R0Ax//KySCG0skKyI1BK4Ufb82S8wohrktBO6W7lag0O2urh9dKI0gM8EuP666DGnaNBFzycKLPqLaURCeCdB6IiogLHiR21dyeHIIAN0zD6SUyTGH2ZNlZkX05hcFUEWcsWE49+Ve/rdfu1wWTDnourH/Xm3IBkhVGqskB+yp3Jkz2D3Q== l10nupdate@fenari'
 
-	require groups::l10nupdate
+    # On labs l10nupdate user and group are already in LDAP
+    if $::realm != 'labs' {
+        require groups::l10nupdate
 
-	generic::systemuser { 'l10nupdate': name => 'l10nupdate', home => '/home/l10nupdate', default_group => 10002, shell => '/bin/bash' }
+        generic::systemuser { 'l10nupdate':
+            name          => 'l10nupdate',
+            home          => '/home/l10nupdate',
+            uid           => $uid,
+            default_group => 'l10nupdate',
+            shell         => '/bin/bash',
+            before        => '/home/l10nupdate/.ssh',
+        }
+    } else {
+        file { '/home/l10update':
+            owner  => 'l10nupdate',
+            group  => 'l10nupdate',
+            mode   => '0750',
+            ensure => directory,
+            before => '/home/l10nupdate/.ssh',
+        }
+    }
 
-	file {
-		"/home/l10nupdate/.ssh":
-			require => Generic::Systemuser["l10nupdate"],
-			owner => l10nupdate,
-			group => l10nupdate,
-			mode => 0500,
-			ensure => directory;
-		"/home/l10nupdate/.ssh/authorized_keys":
-			require => File["/home/l10nupdate/.ssh"],
-			owner => l10nupdate,
-			group => l10nupdate,
-			mode => 0400,
-			content => $authorized_key;
-	}
+    file {
+        '/home/l10nupdate/.ssh':
+            owner  => 'l10nupdate',
+            group  => 'l10nupdate',
+            mode   => '0500',
+            ensure => directory;
+        '/home/l10nupdate/.ssh/authorized_keys':
+            require => File['/home/l10nupdate/.ssh'],
+            owner   => 'l10nupdate',
+            group   => 'l10nupdate',
+            mode    => '0400',
+            content => $authorized_key;
+    }
 }

commit fc79b36acb386d2ee3bc46376a73501d46b925ad

Fix bug introduced in 577ea6a
generic::systemuser never had a uid parameter. 577ea6a introduced one
for user l10nupdate breaking puppet for some hosts

Change-Id: I01df892443c63d133fd3f78d81cd032754795b72

modules/mediawiki/manifests/users/l10nupdate.pp
@@ -10,7 +10,6 @@ class mediawiki::users::l10nupdate {
         generic::systemuser { 'l10nupdate':
             name          => 'l10nupdate',
             home          => '/home/l10nupdate',
-            uid           => $uid,
             default_group => 'l10nupdate',
             shell         => '/bin/bash',
             before        => File['/home/l10nupdate/.ssh'],