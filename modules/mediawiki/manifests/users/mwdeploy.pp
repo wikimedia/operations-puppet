898bc30102510792c9eb5b1e331f268f346e3928
Antoine Musso <hashar@free.fr>
2014-03-14 15:30:45 +0100

    beta: skip mwdeploy user creation
    In labs, the mwdeploy user is provisionned via LDAP, so skip the user
    creation entirely.  The homedir needs to be created by puppet though.
    
    Change-Id: Ie232d8337e7b4dc4b80b3b43eceae3ab597b47b3

--- modules/mediawiki/manifests/users/mwdeploy.pp
+++ modules/mediawiki/manifests/users/mwdeploy.pp
@@ -1,5 +1,17 @@
 # mediawiki base mw deploy user
 class mediawiki::users::mwdeploy {
-	## mwdeploy user
-	generic::systemuser { 'mwdeploy': name => 'mwdeploy' }
+
+    ## mwdeploy user
+    if $::realm != 'labs' {
+        generic::systemuser { 'mwdeploy': name => 'mwdeploy' }
+    } else {
+        # User created in LDAP
+        file { '/var/lib/mwdeploy':
+            ensure => directory,
+            owner  => 'mwdeploy',
+            group  => 'mwdeploy',
+            mode   => '0755',
+        }
+    }
+
 }
