50633049ff163ad3e26cdfb8af741cb57a7d3a75
Tim Landscheidt <tim@tim-landscheidt.de>
2014-03-15 02:59:11 +0000

    WIP: labsdeprepo: Allow more than one local repository
    Bug: 60925
    Change-Id: I0a7d84579c6148a7249afa68f9b260adaf6a40ca

--- manifests/misc/labsdebrepo.pp
+++ manifests/misc/labsdebrepo.pp
@@ -1,12 +1,12 @@
 # Puppet configs to create a local deb repo and add it to your sources.list
 
-class misc::labsdebrepo {
-    # manage /data/project/repo:
-    # make sure it's a directory and turn it into a deb repo
-    file { '/data/project/repo':
+class misc::labsdebrepo ($dir = '/data/project/repo', $handle = 'labsdebrepo') {
+    # manage /data/project/repo: make sure it's a directory and turn
+    # it into a deb repo.
+    file { $dir:
         ensure => directory,
     }
-    file { '/data/project/repo/Packages.gz':
+    file { "${dir}/Packages.gz":
         ensure => present,
     }
     # run dpkg-scanpackages . /dev/null | gzip -9c > binary/Packages.gz
@@ -14,19 +14,19 @@ class misc::labsdebrepo {
     package { 'dpkg-dev':
         ensure => present,
     }
-    exec { 'Turn dir into deb repo':
-        cwd     => '/data/project/repo',
+    exec { 'Turn ${dir} into deb repo':
+        cwd     => $dir,
         command => '/usr/bin/dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz',
         # only run if Packages.gz is *not* the newest file in the directory
         onlyif  => '/usr/bin/test $(find . -newer Packages.gz | wc -l) -gt 0',
-        require => [Package['dpkg-dev'], File['/data/project/repo']],
+        require => [Package['dpkg-dev'], File[$dir]],
     }
     # add the dir-turned-repo to sources.list
-    file { '/etc/apt/sources.list.d/labsdebrepo.list':
-        source  => 'puppet:///files/misc/labsdebrepo.list',
-        require => Exec['Turn dir into deb repo'],
+    file { '/etc/apt/sources.list.d/${handle}.list':
+        content => template('misc/labsdebrepo.list'),
+        require => Exec['Turn ${dir} into deb repo'],
     }
-    file { '/etc/apt/preferences.d/labsdebrepo.pref':
+    file { '/etc/apt/preferences.d/${handle}.pref':
         content => 'Explanation: Prefer local repo above others
 Package: *
 Pin: origin
@@ -34,4 +34,3 @@ Pin-Priority: 1500
 '
     }
 }
-
