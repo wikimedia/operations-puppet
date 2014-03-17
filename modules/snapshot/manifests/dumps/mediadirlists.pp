bd3c663a6ab5cf4dc2b19df5700787de318dc5f5
Ariel T. Glenn <ariel@wikimedia.org>
2014-03-17 15:37:51 +0200

    puppetize generation of media directory lists for snapshots
    Change-Id: I893cf09894c81e6903749f384b5afa96d99866e1

---
+++ modules/snapshot/manifests/dumps/mediadirlists.pp
@@ -0,0 +1,36 @@
+class snapshot::dumps::mediadirlists(
+    $enable = true,
+    $user   = undef,
+) {
+    include snapshot::dirs
+
+    if ($enable) {
+        $ensure = 'present'
+    }
+    else {
+        $ensure = 'absent'
+    }
+
+    system::role { 'snapshot::dumps::mediadirlists':
+        ensure      => $ensure,
+        description => 'producer of daily lists of media directories'
+    }
+
+    file { '/usr/local/bin/create-mediadir-list.sh':
+        ensure  => 'present',
+        path    => '/usr/local/bin/create-mediadir-list.sh',
+        mode    => '0755',
+        owner   => $user,
+        group   => root,
+        content => template('snapshot/create-mediadir-list.sh.erb'),
+    }
+
+    cron { 'list-media-dirs':
+        ensure      => $ensure,
+        environment => 'MAILTO=ops-dumps@wikimedia.org',
+        user        => $user,
+        command     => '/usr/local/bin/create-mediadir-list.sh',
+        minute      => '10',
+        hour        => '2',
+    }
+}
