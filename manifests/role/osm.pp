wikimedia/operations-puppet

commit dc08f689aab644b3837c7c44c090fb69193f2218

Creating a spatially enabled db on osmlab dbs
Change-Id: Ia27be77e14342989dc31e40c79bec4e6f86903a1

manifests/role/osm.pp
@@ -8,6 +8,7 @@ class role::osm::master {
     include postgresql::master
     include postgresql::postgis
     include passwords::osm
+    postgres::spatialdb { 'gis': }
 
     if $osm_slave_v4 {
         postgresql::user { "replication@${::osm_slave}-v4":
@@ -39,6 +40,9 @@ class role::osm::slave {
     include role::osm::common
     include postgresql::postgis
     include passwords::osm
+    # Note: This is here to illustrate the fact that the slave is expected to
+    # have the same dbs as the master.
+    #postgres::spatialdb { 'gis': }
 
     class {'postgresql::slave':
         master_server    => $osm_master,

commit b709ed4080e8a4c47399cdf1d2639cfb398ae0d1

Fix a typo introduced in dc08f68
It is postgresql, not postgres

Change-Id: Ib4c80643adc0b6d72fc4f5d281685ff80150cc23

manifests/role/osm.pp
@@ -8,7 +8,7 @@ class role::osm::master {
     include postgresql::master
     include postgresql::postgis
     include passwords::osm
-    postgres::spatialdb { 'gis': }
+    postgresql::spatialdb { 'gis': }
 
     if $osm_slave_v4 {
         postgresql::user { "replication@${::osm_slave}-v4":
@@ -42,7 +42,7 @@ class role::osm::slave {
     include passwords::osm
     # Note: This is here to illustrate the fact that the slave is expected to
     # have the same dbs as the master.
-    #postgres::spatialdb { 'gis': }
+    #postgresql::spatialdb { 'gis': }
 
     class {'postgresql::slave':
         master_server    => $osm_master,