wikimedia/operations-puppet

commit d7d97a075a7d11665bb38d6fcc4790d45f786b96

Fix dependency in install-server::dhcp-server
Seems like Package[dhcp3-server] was not converted to a variable in
5a65b94. Also split service/package names. It was a wrong way to do it

Change-Id: Ib015515c4a1542889fcca1defd81e0343d41ef55

modules/install-server/manifests/dhcp-server.pp
@@ -15,14 +15,16 @@
 class install-server::dhcp-server {
     if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '12.04') >= 0 {
         $confdir = '/etc/dhcp/'
-        $package_service_name = 'isc-dhcp-server'
+        $package_name = 'isc-dhcp-server'
+        $service_name = 'isc-dhcp-server'
     } else {
         $confdir = '/etc/dhcp3'
-        $package_service_name = 'dhcp3-server'
+        $package_name = 'dhcp3-server'
+        $service_name = 'dhcp3-server'
     }
     file { $confdir:
         ensure      => directory,
-        require     => Package[dhcp3-server],
+        require     => Package[$package_service_name],
         recurse     => true,
         owner       => 'root',
         group       => 'root',
@@ -30,15 +32,15 @@ class install-server::dhcp-server {
         source      => 'puppet:///modules/install-server/dhcpd',
     }
 
-    package { $package_service_name:
+    package { $package_name:
         ensure => latest;
     }
 
-    service { $package_service_name:
+    service { $service_name:
         ensure    => running,
-        require   => [ Package[$package_service_name],
-                       File[ $confdir ]
+        require   => [ Package[$package_name],
+                       File[$confdir]
                       ],
-        subscribe => File[ $confdir ],
+        subscribe => File[$confdir],
     }
 }

commit 80e17cb3f901bd525ff7c2b3d439c98f38f11e01

Fix typo introduced in d7d97a0
Change-Id: Ib72e0312b12947655822578e3bda289d43f01493

modules/install-server/manifests/dhcp-server.pp
@@ -24,7 +24,7 @@ class install-server::dhcp-server {
     }
     file { $confdir:
         ensure      => directory,
-        require     => Package[$package_service_name],
+        require     => Package[$package_name],
         recurse     => true,
         owner       => 'root',
         group       => 'root',