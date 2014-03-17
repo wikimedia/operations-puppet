f5095af0664ace1bf901249258e0d92d48f5fffe
Bryan Davis <bd808@wikimedia.org>
2014-03-17 13:16:24 -0600

    [WIP] Make logstash and kibana roles work in labs
    Add realm specific configuration following the model of
    role::elasticsearch so that logstash installs in wmflabs can be
    performed with puppet.
    
    Change-Id: I2649590a2f130b5fbf5f4f997dfa4ba18d4c1cbf

--- manifests/role/kibana.pp
+++ manifests/role/kibana.pp
@@ -8,16 +8,52 @@ class role::kibana {
     include ::apache
     include ::passwords::ldap::production
 
-    $hostname      = 'logstash.wikimedia.org'
-    $deploy_dir    = '/srv/deployment/kibana/kibana'
-    $es_host       = '127.0.0.1'
-    $es_port       = 9200
-    $ldap_authurl  = 'ldaps://virt1000.wikimedia.org virt0.wikimedia.org/ou=people,dc=wikimedia,dc=org?cn'
-    $ldap_bindpass = $passwords::ldap::production::proxypass
-    $ldap_binddn   = 'cn=proxyagent,ou=profile,dc=wikimedia,dc=org'
-    $ldap_group    = 'cn=wmf,ou=groups,dc=wikimedia,dc=org'
-    $auth_realm    = 'WMF Labs (use wiki login name not shell)'
-    $serveradmin   = 'root@wikimedia.org'
+    if ($::realm == 'labs') {
+        if ($::hostname =~ /^deployment-/) {
+            # Beta
+            $hostname    = 'logstash.beta.wmflabs.org'
+            $deploy_dir  = '/srv/deployment/kibana/kibana'
+            $auth_realm  = 'Logstash (see [[office:User:BDavis_(WMF)/logstash]])'
+            $auth_users = '/data/project/logstash/.htpasswd'
+            $require_ssl = true
+        } else {
+            # Regular labs instance
+            $hostname = $::kibana_hostname ? {
+                undef   => $::hostname,
+                default => $::kibana_hostname,
+            }
+            $deploy_dir = $::kibana_deploydir ? {
+                undef   => '/srv/deployment/kibana/kibana',
+                default => $::kibana_deploydir,
+            }
+            $auth_realm = $::kibana_authrealm ? {
+                undef   => 'Logstash',
+                default => $::kibana_authrealm,
+            }
+            $auth_file = $::kibana_authfile ? {
+                undef   => '/data/project/logstash/.htpasswd',
+                default => $::kibana_authfile,
+            }
+            $require_ssl = false
+        }
+        $serveradmin = "root@${hostname}"
+        $apache_auth   = template('apache-auth-local.erb')
+    } else {
+        # Production
+        $hostname      = 'logstash.wikimedia.org'
+        $deploy_dir    = '/srv/deployment/kibana/kibana'
+        $serveradmin   = 'root@wikimedia.org'
+        $require_ssl   = true
+
+        $ldap_authurl  = 'ldaps://virt1000.wikimedia.org virt0.wikimedia.org/ou=people,dc=wikimedia,dc=org?cn'
+        $ldap_bindpass = $passwords::ldap::production::proxypass
+        $ldap_binddn   = 'cn=proxyagent,ou=profile,dc=wikimedia,dc=org'
+        $ldap_group    = 'cn=wmf,ou=groups,dc=wikimedia,dc=org'
+        $auth_realm    = 'WMF Labs (use wiki login name not shell)'
+        $apache_auth   = template('apache-auth-ldap.erb')
+    }
+    $es_host = '127.0.0.1'
+    $es_port = 9200
 
     class { '::kibana':
         default_route => '/dashboard/elasticsearch/default',
