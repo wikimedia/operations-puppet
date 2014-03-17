422b50b71556e39e6bf166c6cd84d3dd4dda6eb3
Ariel T. Glenn <ariel@wikimedia.org>
2014-03-17 14:02:52 +0200

    puppetize generation of lists of good dumps for rsyncers
    note that list-last-n-good-dumps.py was stolen from the
    dumps repo and  and I'm undecided about whether to remove
    that copy or leave it for use by dumps users, so I left it
    Change-Id: If6dab53739eba91a6a1f41b819a04276182fe5f2

---
+++ modules/snapshot/manifests/dumps/dumplists.pp
@@ -0,0 +1,46 @@
+class snapshot::dumps::dumplists(
+    $enable = true,
+    $user   = undef,
+) {
+    include snapshot::dirs
+
+    if ($enable) {
+        $ensure = 'present'
+    }
+    else {
+        $ensure = 'absent'
+    }
+
+    system::role { 'snapshot::dumps::dumplists':
+        ensure      => $ensure,
+        description => 'producer of daily lists of good dumps for mirrors'
+    }
+
+    file { '/usr/local/bin/list-last-good-dumps.sh':
+        ensure  => 'present',
+        path    => '/usr/local/bin/list-last-good-dumps.sh',
+        mode    => '0755',
+        owner   => $user,
+        group   => root,
+        content => template('snapshot/list-last-good-dumps.sh.erb'),
+    }
+    file { '/usr/local/bin/list-last-n-good-dumps.py':
+        ensure  => 'present',
+        path    => '/usr/local/bin/list-last-n-good-dumps.py',
+        mode    => '0755',
+        owner   => $user,
+        group   => root,
+        source  => 'puppet:///modules/snapshot/dumps/list-last-n-good-dumps.py',
+    }
+
+    # fixme there is an implicit dependency on
+    # $dumpsdir/confs/wikidump.conf.monitor, make explicit
+    cron { 'list-good-dumps':
+        ensure      => $ensure,
+        environment => 'MAILTO=ops-dumps@wikimedia.org',
+        user        => $user,
+        command     => '/usr/local/bin/list-last-good-dumps.sh',
+        minute      => '55',
+        hour        => '3',
+    }
+}
