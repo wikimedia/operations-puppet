d88468c1cad245a2c3fb07e8a7902ec2550f7571
dzahn <dzahn@wikimedia.org>
2014-03-18 12:05:20 -0700

    let yuvipanda and tfinc upload mobile tarballs
    RT #7068
    
    Change-Id: Iad98ccc8a90b08777d78b52ce0389b9f16b1ae14

--- manifests/role/releases.pp
+++ manifests/role/releases.pp
@@ -12,6 +12,8 @@ class role::releases {
     }
 
     role::releases::access { 'brion': group => 'mobileupld' }
+    role::releases::access { 'yuvipanda': group => 'mobileupld' } # RT 7068
+    role::releases::access { 'tfinc': group => 'mobileupld' } # RT 7068
     role::releases::access { 'csteipp': group => 'mwupld' }
     role::releases::access { 'hashar': group => 'mwupld' } # RT 6861
     role::releases::access { 'mah': group => 'mwupld' } # RT 6861
