426f8ca26022e582d1135cd02ebc5b1d5ef95fe2
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-03-19 00:05:33 +0200

    osm module
    Module to handle installation of osm2pgsql, osmosis, and population and
    syncing with from planet.osm
    
    While at it enabled the hstore extension in postgres module
    Also upload the already populated grants for osmosis and fix a couple of
    bugs in the postgresql module while at it
    
    Change-Id: Id3db056ace7a073831e90ebdaf47f6d9b2596ec5

---
+++ modules/osm/manifests/populatedb.pp
@@ -0,0 +1,39 @@
+#
+# Definition: osm::populatedb
+#
+# This definition provides a way to load planet_osm in a gis enabled db
+# You must have downloaded the pbf first and placed it in a configurable place
+#
+# Parameters:
+#
+# Actions:
+#   load a planet.osm
+#
+# Requires:
+#   Class['postgresql::postgis']
+#   define['postgresql::spatialdb']
+#
+# Sample Usage:
+#  osm::populatedb { 'mydb': input_pbf_file => '/myfile.pbf' }
+#
+define osm::populatedb(
+    $input_pbf_file,
+    ) {
+
+    # Check if our db tables exist
+    $tables_exist = "/usr/bin/psql --tuples-only -c \'SELECT table_catalog,table_name FROM information_schema.tables;\' | /bin/grep \'^ ${name}\' | /bin/grep \'planet_osm\'"
+
+    exec { "load_900913-${name}":
+        command => "/usr/bin/psql -d ${name} -f /usr/share/osm2pgsql/osm2pgsql/900913.sql",
+        user    => 'postgres',
+        unless  => $tables_exist,
+    }
+
+    $load_planet_cmd = inline_template("<% data=@memoryfree.split(' '); multi={'MB' => 1, 'GB' => 1000}[data[1]]%> usr/bin/osm2pgsql -C <%= data[0].to_i*multi %> -d <%= @name %> --number-processes <%= @processorcount %> <%= @input_pbf_file %>")
+    exec { "load_planet_osm-${name}":
+        command     => $load_planet_cmd,
+        user        => 'postgres',
+        refreshonly => true,
+        subscribe   => Exec["load_900913-${name}"],
+    }
+}
