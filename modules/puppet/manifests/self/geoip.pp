3ae805a7381db5805dce6d2731d025a6b8645fea
Bryan Davis <bd808@wikimedia.org>
2014-03-19 14:18:29 -0600

    Make a self-hosted puppetmaster workalike for puppet::geoip
    The new puppet::self::geoip class can be applied to a self-hosted
    puppetmaster to provide work-alike resources for classes that need GeoIP
    data.
    
    Once this class is merged, the class should be added as an option in the
    puppet-development puppet group on wikitech so instances can use it.
    
    Change-Id: I7d561d55cab5d0cad60d88d44fba56ba3562e9b7

---
+++ modules/puppet/manifests/self/geoip.pp
@@ -0,0 +1,29 @@
+class puppet::self::geoip {
+    # Fetch the GeoIP databases into puppet's volatile dir, so that other hosts
+    # can then just sync that directory into their own /usr/share/GeoIP via a
+    # normal puppet File resource (see the geoip module for more)
+
+    $geoip_destdir = '/var/lib/puppet/volatile/GeoIP'
+
+    # geoip::data classes depend on this
+    file { $geoip_destdir:
+        ensure => directory,
+    }
+
+    # fetch the GeoLite databases
+    class { 'geoip::data::lite':
+        data_directory => $geoip_destdir,
+    }
+
+    # compatibility symlinks, so that users can use the stable paths
+    # GeoIP.dat/GeoIPCity.dat between labs and production
+    file { "$geoip_destdir/GeoIP.dat":
+        ensure => link,
+        target => 'GeoLite.dat',
+    }
+    file { "$geoip_destdir/GeoIPCity.dat":
+        ensure => link,
+        target => 'GeoLiteCity.dat',
+    }
+}
+
