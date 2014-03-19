58a6eaee5065e1dfbb72f69a4e837bd6b158b2e3
Bryan Davis <bd808@wikimedia.org>
2014-03-19 12:46:09 -0600

    Support eqiad labs secondary disk
    Use the labs_lvm::volume class to mount varnish cache disk for labs
    instances.
    
    Change-Id: I52403cd1e32770d3c41c930dd6c836b0637e7a08

--- modules/varnish/manifests/setup_filesystem.pp
+++ modules/varnish/manifests/setup_filesystem.pp
@@ -1,20 +1,29 @@
 define varnish::setup_filesystem() {
-    file { "/srv/${title}":
-        ensure => directory,
-        owner  => 'root',
-        group  => 'root',
-    }
-
     $mount_options = $::realm ? {
         'production' => 'noatime,nodiratime,nobarrier,logbufs=8',
         'labs'       => 'noatime,nodiratime,nobarrier,comment=cloudconfig',
     }
 
-    mount { "/srv/${title}":
-        ensure  => mounted,
-        require => File["/srv/${title}"],
-        device  => "/dev/${title}",
-        fstype  => 'auto',
+    if $::realm == 'labs' and $::site == 'eqiad' {
+      include labs_lvm
+      labs_lvm::volume { $title:
+        mountat => "/srv/${title}",
         options => $mount_options,
+        fstype  => 'xfs',
+      }
+    } else {
+      file { "/srv/${title}":
+          ensure => directory,
+          owner  => 'root',
+          group  => 'root',
+      }
+
+      mount { "/srv/${title}":
+          ensure  => mounted,
+          require => File["/srv/${title}"],
+          device  => "/dev/${title}",
+          fstype  => 'auto',
+          options => $mount_options,
+      }
     }
 }
