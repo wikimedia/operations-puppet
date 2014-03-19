wikimedia/operations-puppet

commit 68e354d659035736bb3dd91324f751d5cdfa9270

Add cron job to run characterEditStats.php on multilingual wikis weekly
Publishes the output a wiki page on Meta.  Needs a MediaWiki install (with
Translate), hence on terbium. Inspired partly by misc/beta/sync-site-resources
and files/icinga/check_job_queue.

Bug: 58440
Change-Id: I681539b6cf2463648d4c5669d689143c2f41cc33

manifests/site.pp
@@ -2518,6 +2518,9 @@ node 'terbium.eqiad.wmnet' {
     class { 'misc::maintenance::translationnotifications':
         enabled => true,
     }
+    class { 'misc::maintenance::updatetranslationstats':
+        enabled => true,
+    }
     class { 'misc::maintenance::wikidata':
         enabled => true,
     }

files/misc/scripts/characterEditStatsTranslate
@@ -1,0 +1,18 @@
+#!/bin/bash
+{
+  # We transclude a subpage so that wikignomes can maintain documentation of this
+  # report on wiki without bothering the code. Newlines needs -e.
+  echo -e "{{/Report}} generated on $(date --rfc-3339=seconds)\n"
+
+  # Pick the main multilingual Translate wikis, all closely related to Meta
+  for wiki in commonswiki incubatorwiki mediawikiwiki metawiki outreachwiki ; do
+    echo -e "== $wiki ==\n=== Last 30 days ===\n<pre>\n"
+    /usr/local/bin/mwscript extensions/Translate/scripts/characterEditStats.php \
+        --wiki $wiki --top 50 --days 30
+    echo -e "</pre>\n=== Last year ===\n<pre>"
+    /usr/local/bin/mwscript extensions/Translate/scripts/characterEditStats.php \
+        --wiki $wiki --days 365
+    echo -e "</pre>\n"
+  done
+} | /usr/local/bin/mwscript edit.php --wiki=metawiki --user "FuzzyBot" \
+    --bot --summary "Update stats" "Meta:Babylon/Translation_stats"

manifests/misc/maintenance.pp
@@ -359,6 +359,26 @@ class misc::maintenance::mail_exim_aliases( $enabled = false ) {
 
 }
 
+class misc::maintenance::updatetranslationstats( $ensure = absent ) {
+    # Include this to a maintenance host to update translation stats.
+
+    file { '/usr/local/bin/characterEditStatsTranslate':
+       ensure => $ensure,
+       owner  => 'mwdeploy',
+       group  => 'mwdeploy',
+       mode   => 0775,
+       source => 'puppet:///files/misc/scripts/characterEditStatsTranslate',
+    }
+    cron { 'updatetranslationstats':
+        user    => 'mwdeploy',
+        minute  => 0,
+        hour    => 0,
+        weekday => 1,
+        command => '/usr/local/bin/characterEditStatsTranslate',
+        ensure  => $ensure,
+    }
+}
+
 class misc::maintenance::updatequerypages( $enabled = false ) {
         # Include this to add cron jobs calling updateSpecialPages.php on all clusters.
 

commit 581afed3891bdb846c12860bef351f141ea14a8c

Fix bug introduced in 68e354d
It broke puppet on terbium

Change-Id: I6b2ed821d090ef01ec50012567d4855e47cbd6d3

manifests/site.pp
@@ -2519,7 +2519,7 @@ node 'terbium.eqiad.wmnet' {
         enabled => true,
     }
     class { 'misc::maintenance::updatetranslationstats':
-        enabled => true,
+        ensure => 'present',
     }
     class { 'misc::maintenance::wikidata':
         enabled => true,