c3982e764749081032be3336c140cf987fa5edb2
Bryan Davis <bd808@wikimedia.org>
2014-03-19 12:20:17 -0600

    Allow user to specify mount point for role::labs::lvm::mnt
    Check for a global variable named `lvm_mount_point` to change the
    default mount point from /mnt.
    
    Once this is merged, the variable should be added to the global
    labsdrives group on wikitech so instances can use it.
    
    Change-Id: I9c949788f99b2e524086852c3c3201a64dfc2695

--- manifests/role/labsmnt.pp
+++ manifests/role/labsmnt.pp
@@ -1,5 +1,10 @@
-# Allocate all of the instance's extra space as /mnt
+# Allocate all of the instance's extra space as /mnt or the user specified
+# mount point given in the $::lvm_mount_point global variable.
 class role::labs::lvm::mnt {
     include labs_lvm
-    labs_lvm::volume { 'second-local-disk': mountat => '/mnt' }
+    $mount_point = $::lvm_mount_point ? {
+      undef   => '/mnt',
+      default => $::lvm_mount_point,
+    }
+    labs_lvm::volume { 'second-local-disk': mountat => $mount_point }
 }
