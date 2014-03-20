ecfde3c079df4893ea9872a91dc32d86e94e4e4b
Reedy <reedy@wikimedia.org>
2014-03-20 01:05:37 +0000

    Manage scap proxy rsync config in puppet.
    List mediawiki appserver network ranges in
    network::constants::mediawiki_appservers.
    
    RT #7080
    
    Change-Id: Iba7f8dcd74ad03a0530e9b165bb0cf48a8f8885a

--- manifests/network.pp
+++ manifests/network.pp
@@ -21,6 +21,15 @@ class network::constants {
             '10.0.0.0/8',
             ]
 
+    $mediawiki_appservers [
+            '10.0.0.0/16',
+            '10.64.0.0/22',
+            '10.64.16.0/24',
+            '208.80.152.0/22',
+            '10.64.32.0/22',
+            '10.64.48.0/22'
+            ]
+
     $special_hosts = {
         'production' => {
             'bastion_hosts' => [
