0478fdf26378d4592e899b68a34961a05185a35e
Andrew Otto <acotto@gmail.com>
2014-03-20 15:00:12 -0400

    RT 7090 - adding analytics contact group to researchdb icinga alerts
    Had to add parameterization for contact_group on
    role::coredb::common and mysql_wmf::coredb::monitoring.
    
    Change-Id: I1a01c7d0230c9a24e02f47e5473ba61c4a54e716

--- modules/mysql_wmf/manifests/coredb/monitoring.pp
+++ modules/mysql_wmf/manifests/coredb/monitoring.pp
@@ -1,4 +1,12 @@
-class mysql_wmf::coredb::monitoring( $crit = false, $no_slave = false ) {
+class mysql_wmf::coredb::monitoring(
+  $crit          = false,
+  $no_slave      = false,
+  # Override contact_group if you want different
+  # Icinga contact_groups notified for mysql errors
+  # on this coredb instance.
+  $contact_group = 'admins',
+)
+{
 
     include passwords::nagios::mysql
     $mysql_check_pass = $passwords::nagios::mysql::mysql_check_pass
@@ -25,21 +33,63 @@ class mysql_wmf::coredb::monitoring( $crit = false, $no_slave = false ) {
         description   => 'MySQL disk space',
         nrpe_command  => '/usr/lib/nagios/plugins/check_disk -w 6% -c 3% -l -e',
         critical      => true,
+        contact_group => $contact_group,
     }
     nrpe::monitor_service { 'mysqld':
         description   => 'mysqld processes',
         nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 1:1 -C mysqld',
         critical      => $crit,
+        contact_group => $contact_group,
     }
-    monitor_service { 'mysql recent restart': description => 'MySQL Recent Restart', check_command => 'nrpe_check_mysql_recent_restart', critical => $crit }
-    monitor_service { 'mysql processlist': description => 'MySQL Processlist', check_command => 'nrpe_pmp_check_mysql_processlist', critical => false }
-    monitor_service { 'mysql innodb': description => 'MySQL InnoDB', check_command => 'nrpe_pmp_check_mysql_innodb', critical => false }
+    monitor_service { 'mysql recent restart':
+        description   => 'MySQL Recent Restart',
+        check_command => 'nrpe_check_mysql_recent_restart',
+        critical      => $crit,
+        contact_group => $contact_group,
+    }
+    monitor_service { 'mysql processlist':
+        description   => 'MySQL Processlist',
+        check_command => 'nrpe_pmp_check_mysql_processlist',
+        critical      => false,
+        contact_group => $contact_group,
+    }
+    monitor_service { 'mysql innodb':
+        description   => 'MySQL InnoDB',
+        check_command => 'nrpe_pmp_check_mysql_innodb',
+        critical      => false,
+        contact_group => $contact_group
+    }
 
     if $no_slave == false {
-        monitor_service { 'full lvs snapshot': description => 'Full LVS Snapshot', check_command => 'nrpe_check_lvs', critical => false }
-        monitor_service { 'mysql idle transaction': description => 'MySQL Idle Transactions', check_command => 'nrpe_check_mysql_idle_transactions', critical => false }
-        monitor_service { 'mysql replication heartbeat': description => 'MySQL Replication Heartbeat', check_command => 'nrpe_check_mysql_slave_heartbeat', critical => false }
-        monitor_service { 'mysql slave delay': description => 'MySQL Slave Delay', check_command => 'nrpe_check_mysql_slave_delay', critical => false }
-        monitor_service { 'mysql slave running': description => 'MySQL Slave Running', check_command => 'nrpe_check_mysql_slave_running', critical => false }
+        monitor_service { 'full lvs snapshot':
+            description   => 'Full LVS Snapshot',
+            check_command => 'nrpe_check_lvs',
+            critical      => false,
+            contact_group => $contact_group,
+        }
+        monitor_service { 'mysql idle transaction':
+            description   => 'MySQL Idle Transactions',
+            check_command => 'nrpe_check_mysql_idle_transactions',
+            critical      => false,
+            contact_group => $contact_group,
+        }
+        monitor_service { 'mysql replication heartbeat':
+            description   => 'MySQL Replication Heartbeat',
+            check_command => 'nrpe_check_mysql_slave_heartbeat',
+            critical      => false,
+            contact_group => $contact_group,
+        }
+        monitor_service { 'mysql slave delay':
+            description   => 'MySQL Slave Delay',
+            check_command => 'nrpe_check_mysql_slave_delay',
+            critical      => false,
+            contact_group => 'admins,analytics',
+        }
+        monitor_service { 'mysql slave running':
+            description   => 'MySQL Slave Running',
+            check_command => 'nrpe_check_mysql_slave_running',
+            critical      => false,
+            contact_group => $contact_group,
+        }
     }
 }
