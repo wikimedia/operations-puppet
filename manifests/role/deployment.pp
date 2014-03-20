efe36bf4564ca6ff25eedcf4cc82fa462aa1b4c8
Reedy <reedy@wikimedia.org>
2014-03-20 00:32:25 +0000

    Update docroot_dir_allows to use network::constants::mediawiki_appservers
    Change-Id: Ied8685298798274640a748c6cbb76a83387f96af

--- manifests/role/deployment.pp
+++ manifests/role/deployment.pp
@@ -181,7 +181,7 @@ class role::deployment::deployment_servers::production {
     docroot		=> "/srv/deployment",
     docroot_owner	=> "trebuchet",
     docroot_group	=> "wikidev",
-    docroot_dir_allows  => ["10.0.0.0/16","10.64.0.0/16","208.80.152.0/22"],
+    docroot_dir_allows  => network::constants::mediawiki_appservers,
     serveradmin		=> "noc@wikimedia.org",
     configure_firewall 	=> false,
   }
