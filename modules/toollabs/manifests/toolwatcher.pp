532e6086f33fca50dff0a8dc152c1ea7dd641355
Tim Landscheidt <tim@tim-landscheidt.de>
2014-03-22 03:46:47 +0000

    Tools: Puppetize toolwatcher
    toolwatcher initializes home directories for tool accounts.
    Previously, its setup was done manually.
    
    Rather than hinging this on toollabs::master or some other not
    directly causally related class that is likely to be used only once,
    we define a new role role::labs::tools::toolwatcher.
    
    Change-Id: Ia65b41795ab118be048a5b95536a5f75300d87d9

---
+++ modules/toollabs/manifests/toolwatcher.pp
@@ -0,0 +1,23 @@
+# toollabs::toolwatcher sets up a host with a service that scans every
+# two minutes for tool accounts whose home directory doesn't exist
+# yet.  For each such tool account, the toolwatcher creates the home
+# directory with the subdirectories cgi-bin and public_html owned by
+# the tool account and its group and sets the permissions to
+# g+srwx,o+rx.
+class toollabs::toolwatcher
+inherits toollabs {
+    file { '/etc/init/toolwatcher.conf':
+        ensure => file,
+        source => 'puppet:///modules/toollabs/toolwatcher.conf',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
+
+    service { 'toolwatcher':
+        ensure    => running,
+        provider  => 'upstart',
+        require   => [File['/etc/init/toolwatcher.conf'], Package['misctools']],
+        subscribe => [File['/etc/init/toolwatcher.conf'], Package['misctools']],
+    }
+}
