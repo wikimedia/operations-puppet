f54293dbf5acb72b11cbc9442c85b82471fc6e70
Ariel T. Glenn <ariel@wikimedia.org>
2014-03-24 11:51:26 +0200

    snapshots: move centralauthdump cron out of role into module
    Change-Id: I7fe7d1c3f364ef5a4a9d9ded50ef48086a13b94a

--- manifests/role/snapshot.pp
+++ manifests/role/snapshot.pp
@@ -7,53 +7,10 @@ class role::snapshot::common {
     include sudo::appserver
 }
 
-class role::snapshot::cron::centralauthdump($enable=true) {
-    include role::snapshot::common
-    include snapshot::dirs
-
-    file { '/usr/local/bin/dumpcentralauth.sh':
-        mode    => '0755',
-        owner   => 'root',
-        group   => 'root',
-        source  => 'puppet:///files/misc/scripts/dumpcentralauth.sh',
-    }
-
-    # used by script to find the name of the correspondaing db.php file
-    if ($::site == 'eqiad') {
-        $dbsite = $::site
-    }
-    else {
-        $dbsite = 'secondary'
-    }
-
-    if ($enable == true) {
-        $ensure = 'present'
-    }
-    else {
-        $ensure = 'absent'
-    }
-
-    system::role { 'role::snapshot::cron::centralauthdump':
-        ensure      => $ensure,
-        description => 'mysql dumper of centralauth',
-    }
-
-    cron { 'centralauth-dump':
-        ensure      => $ensure,
-        command     => "/usr/local/bin/dumpcentralauth.sh --site ${dbsite} --config ${snapshot::dirs::dumpsdir}/confs/wikidump.conf",
-        environment => 'MAILTO=ops-dumps@wikimedia.org',
-        user        => datasets,
-        minute      => '15',
-        hour        => '8',
-        weekday     => '6',
-        require     => [File['/usr/local/bin/dumpcentralauth.sh'],
-                       User['datasets']],
-    }
-}
-
 class role::snapshot::cron::primary {
-    class { 'role::snapshot::cron::centralauthdump':
+    class { 'snapshot::centralauthdump':
         enable => true,
+        user   => 'datasets',
     }
     class { 'snapshot::dumps::pagetitles':
         enable => true,
@@ -74,8 +31,9 @@ class role::snapshot::cron::primary {
 }
 
 class role::snapshot::cron::secondary {
-    class { 'role::snapshot::cron::centralauthdump':
+    class { 'snapshot::centralauthdump':
         enable => false,
+        user   => 'datasets',
     }
     class { 'snapshot::dumps::pagetitles':
         enable => false,
---
+++ modules/snapshot/manifests/centralauthdump.pp
@@ -0,0 +1,43 @@
+class snapshot::centralauthdump(
+    $enable = true,
+    $user   = undef,
+) {
+    include snapshot::dirs
+
+    if ($enable == true) {
+        $ensure = 'present'
+    }
+    else {
+        $ensure = 'absent'
+    }
+
+    system::role { 'snapshot::centralauthdump':
+        ensure      => $ensure,
+        description => 'mysql dumper of centralauth',
+    }
+
+    file { '/usr/local/bin/dumpcentralauth.sh':
+        mode    => '0755',
+        owner   => 'root',
+        group   => 'root',
+        source  => 'puppet:///modules/snapshot/dumpcentralauth.sh',
+    }
+
+    # used by script to find the name of the corresponding db.php file
+    if ($::site == 'eqiad') {
+        $dbsite = $::site
+    }
+    else {
+        $dbsite = 'secondary'
+    }
+
+    cron { 'centralauth-dump':
+        ensure      => $ensure,
+        command     => "/usr/local/bin/dumpcentralauth.sh --site ${dbsite} --config ${snapshot::dirs::dumpsdir}/confs/wikidump.conf",
+        environment => 'MAILTO=ops-dumps@wikimedia.org',
+        user        => $user,
+        minute      => '15',
+        hour        => '8',
+        weekday     => '6',
+    }
+}
