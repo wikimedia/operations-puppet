d982ff280f9c5610d7be2ce8cca0f11c72daeb59
Ariel T. Glenn <ariel@wikimedia.org>
2014-03-25 16:45:36 +0200

    add config file for media list generation on snapshots
    Change-Id: I573c4d95e3d8b897342729fa373701ff8788a229

--- modules/snapshot/manifests/mediaperprojectlists.pp
+++ modules/snapshot/manifests/mediaperprojectlists.pp
@@ -24,6 +24,14 @@ class snapshot::mediaperprojectlists(
         group   => root,
         content => template('snapshot/create-media-per-project-lists.sh.erb'),
     }
+    file { "${snapshot::dirs::wikiqueriesdir}/confs/wq.conf.media":
+        ensure  => 'present',
+        path    => "${snapshot::dirs::wikiqueriesdir}/confs/wq.conf.media",
+        mode    => '0644',
+        owner   => $user,
+        group   => root,
+        content => template('snapshot/wq.conf.media.erb'),
+    }
 
     cron { 'list-media-per-project':
         ensure      => $ensure,
