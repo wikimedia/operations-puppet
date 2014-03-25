e3d3a83deede3e0ee97917aca40395fb7ff5a1ae
cpettet <rush@wikimedia.org>
2014-03-24 19:33:06 -0500

    shell for proposed admin module
    Combines user/group/sudo in one place.  Allows many-to-many user/group/permissions matching.
    
    Three phase plan at the moment.
    
    Phase 1: Get user/group/perm definitions moved under admin
    Phase 2: Deal with Per User Group madness
    Phase 3: Secure ssh keys so users can't fiddle
    
    Change-Id: I5c950a916d66d896f491967e6a1c00b9455e4874

---
+++ modules/admin/manifests/sudo.pp
@@ -0,0 +1,68 @@
+# A defined type for user/group sudo priviledge management
+#
+# Manages:
+#       Group sudo rights
+#       User sudo rights
+#
+# === Parameters
+#
+# [*ensure*]
+#   Add or remove the priv definition in /etc/sudoers.d [ "present" | "absent"]
+#
+# [*username*]
+#   The username of the user to be given priviledges.
+#
+#   WARNING:  Use for user oneoffs.  Sudo privs should be handled in
+#   the main user/group definition in almost all cases.
+#
+# [*filename*]
+#   File to create in /etc/sudoers.d/.  This is usually autodetermined.
+#
+# [*comment*]
+#   In case of a non-user definition/non-group definition priv a comment
+#   can be provided.
+#
+# [*privs*]
+#   An array of lines to be included in a sudoers.d/ file
+#
+# [*is_group*]
+#   Boolean value to determine if this is a group.
+#   Group declarations in sudo terms need a '%' prepend
+#
+define admin::sudo(
+                        $ensure='present',
+                        $user=undef,
+                        $filename=undef,
+                        $comment=undef,
+                        $privs=[],
+                        $is_group=false,
+                   )
+    {
+
+    if ($user) and ($is_group) {
+        fail("${user} specified as group")
+    }
+
+    if ($filename) {
+        $suders_d = $filename
+    }
+    else {
+        $sudoers_d = $name
+    }
+
+    if ($user) {
+        $priv_holder = $user
+    }
+    else {
+        $priv_holder = $name
+    }
+
+    file { "/etc/sudoers.d/${sudoers_d}":
+        ensure  => $ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0440',
+        content => template('admin/sudoers.erb'),
+        tag     => 'sudoers',
+    }
+}
