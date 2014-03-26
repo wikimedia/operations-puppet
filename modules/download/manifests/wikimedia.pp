3492a7cd1aa6871e44d053023ca7572b7da775ba
Ariel T. Glenn <ariel@wikimedia.org>
2014-03-26 12:35:00 +0200

    add cert to the downloads wikimedia conf file
    Change-Id: I7c05036dfd2f5ac56a3cc3c1168f8db40ceb8c10

--- modules/download/manifests/wikimedia.pp
+++ modules/download/manifests/wikimedia.pp
@@ -5,6 +5,8 @@ class download::wikimedia {
         ensure => latest,
     }
 
+    install_certificate{ 'dumps.wikimedia.org': ca => 'RapidSSL_CA.pem' }
+
     file { '/etc/lighttpd/lighttpd.conf':
         mode   => '0444',
         owner  => 'root',
