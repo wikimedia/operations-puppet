5fe9eca834481007bb603c6f7dc07ed01e98cb5c
Antoine Musso <hashar@free.fr>
2014-03-27 13:39:49 +0100

    beta: bring in scap related scripts on bastion
    We need mw-update-l10n linked to /srv/scap/ scripts. That is provided by
    misc::deployment::scap_scripts.
    
    Change-Id: I9a0ee0b602d86ad05f2a82e0a3c80491960c4159

--- manifests/role/beta.pp
+++ manifests/role/beta.pp
@@ -10,6 +10,9 @@ class role::beta::bastion {
     include beta::autoupdater
     include beta::syncsiteresources
 
+    # Bring scap related scripts such as mw-update-l10n
+    include misc::deployment::scap_scripts
+
     # Disable fatalmonitor on eqiad beta cluster to avoid duplicate emails.
     # FIXME remove condition once beta cluster has been migrated.
     if $::site == 'pmtpa' {
