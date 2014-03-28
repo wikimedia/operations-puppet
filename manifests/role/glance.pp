d27bdc8d98aa17bb3727446562e1bdcab5fb6758
dzahn <dzahn@wikimedia.org>
2014-03-28 06:35:32 -0700

    retab role/glance
    Change-Id: Ia24c0219c3b3ac3dc3b1f50c963f814b72302da1

--- manifests/role/glance.pp
+++ manifests/role/glance.pp
@@ -1,69 +1,69 @@
 class role::glance::config {
-	include passwords::openstack::glance
+    include passwords::openstack::glance
 
-	$commonglanceconfig = {
-		db_name => "glance",
-		db_user => "glance",
-		db_pass => $passwords::openstack::glance::glance_db_pass,
-	}
+    $commonglanceconfig = {
+        db_name => 'glance',
+        db_user => 'glance',
+        db_pass => $passwords::openstack::glance::glance_db_pass,
+    }
 }
 
 class role::glance::config::pmtpa inherits role::glance::config {
-	include role::keystone::config::pmtpa
+    include role::keystone::config::pmtpa
 
-	$keystoneconfig = $role::keystone::config::pmtpa::keystoneconfig
+    $keystoneconfig = $role::keystone::config::pmtpa::keystoneconfig
 
-	$pmtpaglanceconfig = {
-		db_host => $realm ? {
-			"production" => "virt0.wikimedia.org",
-			"labs" => "localhost",
-		},
-		bind_ip => $realm ? {
-			"production" => "208.80.152.32",
-			"labs" => "127.0.0.1",
-		},
-		keystone_admin_token => $keystoneconfig["admin_token"],
-		keystone_auth_host => $keystoneconfig["bind_ip"],
-		keystone_auth_protocol => $keystoneconfig["auth_protocol"],
-		keystone_auth_port => $keystoneconfig["auth_port"],
-	}
-	$glanceconfig = merge($pmtpaglanceconfig, $commonglanceconfig)
+    $pmtpaglanceconfig = {
+        db_host => $realm ? {
+            'production' => 'virt0.wikimedia.org',
+            'labs'       => 'localhost',
+        },
+        bind_ip => $realm ? {
+            'production' => '208.80.152.32',
+            'labs'       => '127.0.0.1',
+        },
+        keystone_admin_token   => $keystoneconfig['admin_token'],
+        keystone_auth_host     => $keystoneconfig['bind_ip'],
+        keystone_auth_protocol => $keystoneconfig['auth_protocol'],
+        keystone_auth_port     => $keystoneconfig['auth_port'],
+    }
+    $glanceconfig = merge($pmtpaglanceconfig, $commonglanceconfig)
 }
 
 class role::glance::config::eqiad inherits role::glance::config {
-	include role::keystone::config::eqiad
+    include role::keystone::config::eqiad
 
-	$keystoneconfig = $role::keystone::config::eqiad::keystoneconfig
+    $keystoneconfig = $role::keystone::config::eqiad::keystoneconfig
 
-	$eqiadglanceconfig = {
-		db_host => $realm ? {
-			"production" => "virt1000.wikimedia.org",
-			"labs" => "localhost",
-		},
-		bind_ip => $realm ? {
-			"production" => "208.80.154.18",
-			"labs" => "127.0.0.1",
-		},
-        	auth_uri => $::realm ? {
-            		'production' => 'http://virt1000.wikimedia.org:5000',
-            		'labs' => 'http://localhost:5000',
-        	},
-		keystone_admin_token => $keystoneconfig["admin_token"],
-		keystone_auth_host => $keystoneconfig["bind_ip"],
-		keystone_auth_protocol => $keystoneconfig["auth_protocol"],
-		keystone_auth_port => $keystoneconfig["auth_port"],
-	}
-	$glanceconfig = merge($eqiadglanceconfig, $commonglanceconfig)
+    $eqiadglanceconfig = {
+        db_host => $realm ? {
+            'production' => 'virt1000.wikimedia.org',
+            'labs'       => 'localhost',
+        },
+        bind_ip => $realm ? {
+            'production' => '208.80.154.18',
+            'labs'       => '127.0.0.1',
+        },
+            auth_uri => $::realm ? {
+                    'production' => 'http://virt1000.wikimedia.org:5000',
+                    'labs'       => 'http://localhost:5000',
+            },
+        keystone_admin_token   => $keystoneconfig['admin_token'],
+        keystone_auth_host     => $keystoneconfig['bind_ip'],
+        keystone_auth_protocol => $keystoneconfig['auth_protocol'],
+        keystone_auth_port     => $keystoneconfig['auth_port'],
+    }
+    $glanceconfig = merge($eqiadglanceconfig, $commonglanceconfig)
 }
 
 class role::glance::server {
-	include role::glance::config::pmtpa,
-		role::glance::config::eqiad
+    include role::glance::config::pmtpa,
+        role::glance::config::eqiad
 
-	$glanceconfig = $site ? {
-		"pmtpa" => $role::glance::config::pmtpa::glanceconfig,
-		"eqiad" => $role::glance::config::eqiad::glanceconfig,
-	}
+    $glanceconfig = $site ? {
+        'pmtpa' => $role::glance::config::pmtpa::glanceconfig,
+        'eqiad' => $role::glance::config::eqiad::glanceconfig,
+    }
 
-	class { "openstack::glance-service": openstack_version => $openstack_version, glanceconfig => $glanceconfig }
+    class { 'openstack::glance-service': openstack_version => $openstack_version, glanceconfig => $glanceconfig }
 }
