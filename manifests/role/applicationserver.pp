beb6dea175f56b84799035fb9eae65ef1ba7d590
Matanya Moses <matanya@foss.co.il>
2014-03-31 01:00:26 +0300

    applicationserver: lint and tidy
    Change-Id: Iab23a7e17c617d4bbc8ce42c09b83cd6991a1960

--- manifests/role/applicationserver.pp
+++ manifests/role/applicationserver.pp
@@ -1,18 +1,42 @@
 # role/apaches.pp
 
 # Virtual monitor group resources for the monitoring server
-@monitor_group { "appserver_eqiad": description => "eqiad application servers" }
-@monitor_group { "appserver_pmtpa": description => "pmtpa application servers" }
-@monitor_group { "api_appserver_eqiad": description => "eqiad API application servers" }
-@monitor_group { "api_appserver_pmtpa": description => "pmtpa API application servers" }
-@monitor_group { "bits_appserver_eqiad": description => "eqiad Bits application servers" }
-@monitor_group { "bits_appserver_pmtpa": description => "pmtpa Bits application servers" }
-@monitor_group { "imagescaler_eqiad": description => "eqiad image scalers" }
-@monitor_group { "imagescaler_pmtpa": description => "pmtpa image scalers" }
-@monitor_group { "jobrunner_eqiad": description => "eqiad jobrunner application servers" }
-@monitor_group { "jobrunner_pmtpa": description => "pmtpa jobrunner application servers" }
-@monitor_group { "videoscaler_pmtpa": description => "pmtpa video scaler" }
-@monitor_group { "videoscaler_eqiad": description => "eqiad video scaler" }
+@monitor_group { 'appserver_eqiad':
+    description => 'eqiad application servers',
+}
+@monitor_group { 'appserver_pmtpa':
+    description => 'pmtpa application servers',
+}
+@monitor_group { 'api_appserver_eqiad':
+    description => 'eqiad API application servers',
+}
+@monitor_group { 'api_appserver_pmtpa':
+    description => 'pmtpa API application servers',
+}
+@monitor_group { 'bits_appserver_eqiad':
+    description => 'eqiad Bits application servers',
+}
+@monitor_group { 'bits_appserver_pmtpa':
+    description => 'pmtpa Bits application servers',
+}
+@monitor_group { 'imagescaler_eqiad':
+    description => 'eqiad image scalers',
+}
+@monitor_group { 'imagescaler_pmtpa':
+    description => 'pmtpa image scalers',
+}
+@monitor_group { 'jobrunner_eqiad':
+    description => 'eqiad jobrunner application servers',
+}
+@monitor_group { 'jobrunner_pmtpa':
+    description => 'pmtpa jobrunner application servers',
+}
+@monitor_group { 'videoscaler_pmtpa':
+    description => 'pmtpa video scaler',
+}
+@monitor_group { 'videoscaler_eqiad':
+    description => 'eqiad video scaler',
+}
 
 class role::applicationserver {
 
@@ -46,8 +70,8 @@ class role::applicationserver {
     class common(
         $group, # TODO - remove this parameter as it's unused
         $lvs_pool = undef,
-        $hhvm = false
-        ) {
+        $hhvm     = false
+) {
 
         if $hhvm == true and $::realm == 'production' {
             fail( 'hhvm is not ready for production usage yet' )
@@ -56,17 +80,17 @@ class role::applicationserver {
         include standard
 
         if $::realm == 'production' {
-            include admins::roots,
-                admins::mortals,
-                geoip,
-                mediawiki
+            include admins::roots
+            include admins::mortals
+            include geoip
+            include mediawiki
 
-            nrpe::monitor_service { "twemproxy":
-                description => "twemproxy process",
+            nrpe::monitor_service { 'twemproxy':
+                description  => 'twemproxy process',
                 nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1:1 -u nobody -C nutcracker"
             }
             nrpe::monitor_service { 'twemproxy port':
-                description => 'twemproxy port',
+                description   => 'twemproxy port',
                 nrpe_command  => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 11211 --timeout=2',
             }
         }
@@ -77,7 +101,9 @@ class role::applicationserver {
 
             # Eqiad instances do not mount additional disk space
             include labs_lvm
-            labs_lvm::volume { 'second-local-disk': mountat => '/srv' }
+            labs_lvm::volume { 'second-local-disk':
+                mountat => '/srv',
+            }
 
             # FIXME: Each host that has this role applied must also be
             # manually added to the dsh group file found in
@@ -88,100 +114,129 @@ class role::applicationserver {
             }
 
             if $hhvm == true {
-                notify { 'installing_hhvm': message => "Installing HHVM" }
-                include ::applicationserver::hhvm
+                notify { 'installing_hhvm':
+                    message => 'Installing HHVM',
+                }
+            include ::applicationserver::hhvm
             }
         }
 
         if $lvs_pool != undef {
             include lvs::configuration
-            class { "lvs::realserver": realserver_ips => $lvs::configuration::lvs_service_ips[$::realm][$lvs_pool][$::site] }
+            class { 'lvs::realserver':
+                realserver_ips => $lvs::configuration::lvs_service_ips[$::realm][$lvs_pool][$::site],
+            }
         }
 
         if $::realm == 'production' {
-            deployment::target { "mediawiki": }
+            deployment::target { 'mediawiki': }
         }
     }
 
     # This class installs everything necessary for an apache webserver
-    class webserver($maxclients="40") {
-        include ::applicationserver,
-            applicationserver::pybal_check,
-            role::applicationserver,
-            role::applicationserver::configuration::php
+    class webserver($maxclients='40') {
+        include ::applicationserver
+        include applicationserver::pybal_check
+        include role::applicationserver
+        include role::applicationserver::configuration::php
 
-        class { "applicationserver::config::apache": maxclients => $maxclients }
+        class { 'applicationserver::config::apache':
+            maxclients => $maxclients,
+        }
 
         class { '::applicationserver::syslog':
             apache_log_aggregator => $role::applicationserver::mediawiki_log_aggregator,
         }
 
-        monitor_service { "appserver http":
-            description => "Apache HTTP",
-            check_command => $::realm ? {
-                'production' => "check_http_wikipedia",
-                'labs' => "check_http_url!commons.wikimedia.beta.wmflabs.org|http://commons.wikimedia.beta.wmflabs.org/wiki/Main_Page"
-                }
+        $appserver_realm = $::realm ? {
+            'production' => 'check_http_wikipedia',
+            'labs'       => "check_http_url!commons.wikimedia.beta.wmflabs.org|http://commons.wikimedia.beta.wmflabs.org/wiki/Main_Page"
+        }
+
+
+        monitor_service { 'appserver http':
+            description   => 'Apache HTTP',
+            check_command => $appserver_realm,
         }
 
         ## ganglia module for apache webservers
-        file {
-            "/usr/lib/ganglia/python_modules/apache_status.py":
-                owner  => 'root',
-                group  => 'root',
-                mode   => '0444',
-                source => 'puppet:///files/ganglia/plugins/apache_status.py',
-                notify => Service['gmond'];
-            "/etc/ganglia/conf.d/apache_status.pyconf":
-                owner  => 'root',
-                group  => 'root',
-                mode   => '0555',
-                source => 'puppet:///files/ganglia/plugins/apache_status.pyconf',
-                notify => Service['gmond'];
+        file { '/usr/lib/ganglia/python_modules/apache_status.py':
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            source => 'puppet:///files/ganglia/plugins/apache_status.py',
+            notify => Service['gmond'],
+        }
+
+        file { '/etc/ganglia/conf.d/apache_status.pyconf':
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            source => 'puppet:///files/ganglia/plugins/apache_status.pyconf',
+            notify => Service['gmond'],
         }
     }
 
     ## prod role classes
     class appserver{
-        system::role { "role::applicationserver::appserver": description => "Standard Apache Application server" }
-
-        class { "role::applicationserver::common": group => "appserver", lvs_pool => "apaches" }
-
-        if $::site == "eqiad" and $::processorcount == "16" {
-            $maxclients = "60"
+        system::role { 'role::applicationserver::appserver':
+            description => 'Standard Apache Application server',
         }
-        elsif $::processorcount == "12" or $::processorcount == "24" {
-            $maxclients = "50"
+
+        class { 'role::applicationserver::common':
+            group    => 'appserver',
+            lvs_pool => 'apaches',
+        }
+
+        if $::site == 'eqiad' and $::processorcount == '16' {
+            $maxclients = '60'
+        }
+        elsif $::processorcount == '12' or $::processorcount == '24' {
+            $maxclients = '50'
         }
         else {
-            $maxclients = "40"
+            $maxclients = '40'
         }
-        class { "role::applicationserver::webserver": maxclients => $maxclients }
+        class { 'role::applicationserver::webserver':
+            maxclients => $maxclients,
+        }
     }
     # role class specifically for test.w.o apache(s)
     class appserver::test{
-        system::role { "role::applicationserver::appserver::test": description => "Test Apache Application server" }
+        system::role { 'role::applicationserver::appserver::test':
+            description => 'Test Apache Application server',
+        }
 
-        class { "role::applicationserver::common": group => "appserver", lvs_pool => "apaches" }
+        class { 'role::applicationserver::common':
+            group    => 'appserver',
+            lvs_pool => 'apaches',
+        }
 
-        class { "role::applicationserver::webserver": maxclients => "100" }
+        class { 'role::applicationserver::webserver':
+            maxclients => '100',
+        }
     }
     # Class for the beta project
     # The Apaches instances act as webserver AND imagescalers. We cannot
     # apply both roles cause puppet will complains about a duplicate class
     # definition for role::applicationserver::common
     class appserver::beta{
-        system::role { "role::applicationserver::appserver::beta": description => "Beta Apache Application server" }
+        system::role { 'role::applicationserver::appserver::beta':
+            description => 'Beta Apache Application server',
+        }
 
-        class { "role::applicationserver::common": group => "beta_appserver", hhvm => true }
+        class { 'role::applicationserver::common':
+            group => 'beta_appserver',
+            hhvm  => true,
+        }
 
         include role::applicationserver::webserver
 
         # Load the class just like the role::applicationserver::imagescaler
         # role.
-        include imagescaler::cron,
-            imagescaler::packages,
-            imagescaler::files
+        include imagescaler::cron
+        include imagescaler::packages
+        include imagescaler::files
 
         # Beta application servers have some ferm DNAT rewriting rules (bug
         # 45868) so we have to explicitly allow http (port 80)
@@ -191,71 +246,85 @@ class role::applicationserver {
         }
 
     }
-    class appserver::api{
-        system::role { "role::applicationserver::appserver::api": description => "Api Apache Application server" }
-
-        class { "role::applicationserver::common": group => "api_appserver", lvs_pool => "api" }
-
-        class { "role::applicationserver::webserver": maxclients => "100" }
-    }
     class appserver::bits{
-        system::role { "role::applicationserver::appserver::bits": description => "Bits Apache Application server" }
+        system::role { 'role::applicationserver::appserver::bits':
+            description => 'Bits Apache Application server',
+        }
 
-        class { "role::applicationserver::common": group => "bits_appserver", lvs_pool => "apaches" }
+        class { 'role::applicationserver::common':
+            group    => 'bits_appserver',
+            lvs_pool => 'apaches',
+        }
 
-        class { "role::applicationserver::webserver": maxclients => "100" }
+        class { 'role::applicationserver::webserver':
+            maxclients => '100',
+        }
     }
     class imagescaler{
-        system::role { "role::applicationserver::imagescaler": description => "Imagescaler Application server" }
+        system::role { 'role::applicationserver::imagescaler':
+            description => 'Imagescaler Application server',
+        }
 
-        class { "role::applicationserver::common": group => "imagescaler", lvs_pool => "rendering" }
+        class { 'role::applicationserver::common':
+            group    => 'imagescaler',
+            lvs_pool => 'rendering',
+        }
 
-        class { "role::applicationserver::webserver": maxclients => "18" }
+        class { 'role::applicationserver::webserver':
+            maxclients => '18',
+        }
 
         # When adding class there, please also update the appserver::beta
         # class which mix both webserver and imagescaler roles.
-        include imagescaler::cron,
-            imagescaler::packages,
-            imagescaler::files
+        include imagescaler::cron
+        include imagescaler::packages
+        include imagescaler::files
     }
-    class videoscaler( $run_jobs_enabled = true ){
-        system::role { "role::applicationserver::videoscaler": description => "TMH Jobrunner Server" }
-
-        class { "role::applicationserver::common": group => "videoscaler" }
-
-        include imagescaler::cron,
-            imagescaler::packages,
-            imagescaler::files
-
-        class {"mediawiki::jobrunner":
-            run_jobs_enabled => $run_jobs_enabled,
-            dprioprocs => 5,
-            iprioprocs => 0,
-            procs_per_iobound_type => 0,
-            type => "webVideoTranscode",
-            timeout => 14400,
-            extra_args => "-v 0"
+    class videoscaler( $run_jobs_enabled = true){
+        system::role { 'role::applicationserver::videoscaler':
+            description => 'TMH Jobrunner Server',
         }
 
-        include applicationserver::config::base,
-            applicationserver::packages,
-            applicationserver::cron,
-            applicationserver::sudo,
-            role::applicationserver::configuration::php
+        class { 'role::applicationserver::common':
+            group => 'videoscaler',
+        }
+
+        include imagescaler::cron
+        include imagescaler::packages
+        include imagescaler::files
+
+        class { 'mediawiki::jobrunner':
+            run_jobs_enabled       => $run_jobs_enabled,
+            dprioprocs             => 5,
+            iprioprocs             => 0,
+            procs_per_iobound_type => 0,
+            type                   => 'webVideoTranscode',
+            timeout                => 14400,
+            extra_args             => '-v 0',
+        }
+
+        include applicationserver::config::base
+        include applicationserver::packages
+        include applicationserver::cron
+        include applicationserver::sudo
+        include role::applicationserver::configuration::php
 
         # dependency for wikimedia-task-appserver
-        service { 'apache':
-            name => "apache2",
+        service { 'apache2':
+            ensure => stopped,
             enable => false,
-            ensure => stopped;
         }
     }
     class jobrunner( $run_jobs_enabled = true ){
-        system::role { "role::applicationserver::jobrunner": description => "Standard Jobrunner Server" }
+        system::role { 'role::applicationserver::jobrunner':
+            description => 'Standard Jobrunner Server',
+        }
 
         include ::mediawiki
 
-        class { "role::applicationserver::common": group => "jobrunner" }
+        class { 'role::applicationserver::common':
+            group => 'jobrunner',
+        }
 
         if $::realm == 'production' {
             class { 'mediawiki::jobrunner':
@@ -272,18 +341,16 @@ class role::applicationserver {
                 run_jobs_enabled       => $run_jobs_enabled,
             }
         }
-
-        include applicationserver::config::base,
-            applicationserver::packages,
-            applicationserver::cron,
-            applicationserver::sudo,
-            role::applicationserver::configuration::php
+        include applicationserver::config::base
+        include applicationserver::packages
+        include applicationserver::cron
+        include applicationserver::sudo
+        include role::applicationserver::configuration::php
 
         # dependency for wikimedia-task-appserver
-            service { 'apache':
-                name => "apache2",
+            service { 'apache2':
+                ensure => stopped,
                 enable => false,
-                ensure => stopped;
         }
     }
 
@@ -291,12 +358,14 @@ class role::applicationserver {
     # Maintenance servers are sometimes dual-purpose with misc apache, so the
     # apache service installed by wikimedia-task-appserver is not disabled here.
     class maintenance {
-        class { "role::applicationserver::common": group => "misc" }
+        class { 'role::applicationserver::common':
+            group => 'misc',
+        }
 
-        include applicationserver::config::base,
-            applicationserver::packages,
-            applicationserver::cron,
-            applicationserver::sudo,
-            role::applicationserver::configuration::php
+        include applicationserver::config::base
+        include applicationserver::packages
+        include applicationserver::cron
+        include applicationserver::sudo
+        include role::applicationserver::configuration::php
     }
 }
--- modules/applicationserver/manifests/config/apache.pp
+++ modules/applicationserver/manifests/config/apache.pp
@@ -10,20 +10,20 @@ class applicationserver::config::apache(
     Class['applicationserver::config::apache'] -> Class['applicationserver::config::base']
 
     file { '/etc/apache2/apache2.conf':
-        owner   => root,
-        group   => root,
+        owner   => 'root',
+        group   => 'root',
         mode    => '0444',
         content => template('applicationserver/apache/apache2.conf.erb'),
     }
     file { '/etc/apache2/envvars':
-        owner  => root,
-        group  => root,
+        owner  => 'root',
+        group  => 'root',
         mode   => '0444',
         source => 'puppet:///modules/applicationserver/apache/envvars.appserver',
     }
     file { '/etc/cluster':
-        owner   => root,
-        group   => root,
+        owner   => 'root',
+        group   => 'root',
         mode    => '0444',
         content => $::site,
     }
@@ -37,7 +37,7 @@ class applicationserver::config::apache(
             path    => '/bin:/sbin:/usr/bin:/usr/sbin',
             command => 'rsync -av 10.0.5.8::httpdconf/ /usr/local/apache/conf',
             creates => '/usr/local/apache/conf',
-            notify  => Service[apache]
+            notify  => Service['apache']
         }
     } else {  # labs
         # bug 38996 - Apache service does not run on start, need a fake
@@ -46,7 +46,7 @@ class applicationserver::config::apache(
         exec { 'Fake sync apache wmf config on beta':
             command => '/bin/true',
             unless  => '/bin/ps -C apache2 > /dev/null',
-            notify  => Service[apache],
+            notify  => Service['apache'],
         }
     }
 
