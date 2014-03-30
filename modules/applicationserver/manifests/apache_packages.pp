01ecc75696c8094dd0db79a7fb34b060cffba496
Matanya Moses <matanya@foss.co.il>
2014-03-31 01:00:26 +0300

    applicationserver: lint and tidy
    Change-Id: Iab23a7e17c617d4bbc8ce42c09b83cd6991a1960

--- modules/applicationserver/manifests/apache_packages.pp
+++
@@ -1,7 +0,0 @@
-class applicationserver::apache_packages {
-    include applicationserver::packages
-
-    package { 'libapache2-mod-php5':
-        ensure => present,
-    }
-}
--- modules/applicationserver/manifests/config/php.pp
+++ modules/applicationserver/manifests/config/php.pp
@@ -12,44 +12,44 @@ class applicationserver::config::php(
     Class['applicationserver::config::php'] -> Class['applicationserver::config::base']
 
     file { '/etc/php5/apache2/php.ini':
-        owner  => root,
-        group  => root,
+        owner  => 'root',
+        group  => 'root',
         mode   => '0444',
         source => 'puppet:///modules/applicationserver/php/php.ini',
     }
     file { '/etc/php5/cli/php.ini':
-        owner  => root,
-        group  => root,
+        owner  => 'root',
+        group  => 'root',
         mode   => '0444',
         source => 'puppet:///modules/applicationserver/php/php.ini.cli',
     }
     file { '/etc/php5/conf.d/fss.ini':
-        owner  => root,
-        group  => root,
+        owner  => 'root',
+        group  => 'root',
         mode   => '0444',
         source => 'puppet:///modules/applicationserver/php/fss.ini',
     }
     file { '/etc/php5/conf.d/apc.ini':
-        owner  => root,
-        group  => root,
+        owner  => 'root',
+        group  => 'root',
         mode   => '0444',
         source => 'puppet:///modules/applicationserver/php/apc.ini',
     }
     file { '/etc/php5/conf.d/wmerrors.ini':
-        owner   => root,
-        group   => root,
+        owner   => 'root',
+        group   => 'root',
         mode    => '0444',
         content => template('applicationserver/php/wmerrors.ini.erb'),
     }
     file { '/etc/php5/conf.d/igbinary.ini':
-        owner  => root,
-        group  => root,
+        owner  => 'root',
+        group  => 'root',
         mode   => '0444',
         source => 'puppet:///files/php/igbinary.ini',
     }
     file { '/etc/php5/conf.d/mail.ini':
-        owner   => root,
-        group   => root,
+        owner   => 'root',
+        group   => 'root',
         mode    => '0444',
         content => "
 // Force the envelope sender address to empty, since we don't want to receive bounces
