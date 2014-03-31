7b13ce6732d47a5cdf198d453bf69c30a5541d21
dzahn <dzahn@wikimedia.org>
2014-03-31 01:27:55 -0700

    lint fundraising
    21 x "tab character found"
    14 x "double quoted string containing no variables"
     2 x "quoted boolean value found"
     2 x "line has more than 80 characters"
    
     1 x remove commented line installing old star SSL cert
    
    Change-Id: I0b1671bdd54bc1d58327d7ed468b1ddf3bb3a3e8

--- manifests/role/fundraising.pp
+++ manifests/role/fundraising.pp
@@ -1,30 +1,36 @@
-@monitor_group { "fundraising_eqiad": description => "fundraising eqiad" }
-@monitor_group { "fundraising_pmtpa": description => "fundraising pmtpa" }
+@monitor_group { 'fundraising_eqiad': description => 'fundraising eqiad' }
+@monitor_group { 'fundraising_pmtpa': description => 'fundraising pmtpa' }
 
 class role::fundraising::civicrm {
-	# variables used in fundraising exim template
-	$exim_signs_dkim = "true"
-	$exim_bounce_collector = "true"
+    # variables used in fundraising exim template
+    $exim_signs_dkim = true
+    $exim_bounce_collector = true
 
-	$cluster = "fundraising"
-	$nagios_group = "${cluster}_${::site}"
+    $cluster = 'fundraising'
+    $nagios_group = "${cluster}_${::site}"
 
-	#install_certificate{ "star.wikimedia.org": }
+    sudo_user { [ 'khorn' ]: privileges => ['ALL = NOPASSWD: ALL'] }
 
-	sudo_user { [ "khorn" ]: privileges => ['ALL = NOPASSWD: ALL'] }
+    $gid = 500
+    include standard-noexim,
+        #accounts::mhernandez, # migrated to lutetium
+        #accounts::zexley, # migrated to lutetium
+        #accounts::pcoombe, # migrated to lutetium
+        admins::fr-tech,
+        admins::roots,
+        backup::client,
+        misc::fundraising,
+        misc::fundraising::backup::backupmover_user,
+        misc::fundraising::mail,
+        nrpe
 
-	$gid = 500
-	include standard-noexim,
-		#accounts::mhernandez, # migrated to lutetium
-		#accounts::zexley, # migrated to lutetium
-		#accounts::pcoombe, # migrated to lutetium
-		admins::fr-tech,
-		admins::roots,
-		backup::client,
-		misc::fundraising,
-		misc::fundraising::backup::backupmover_user,
-		misc::fundraising::mail,
-		nrpe
-	monitor_service { "smtp": description => "Exim SMTP", check_command => "check_smtp" }
-	monitor_service { "http": description => "HTTP", check_command => "check_http" }
+    monitor_service { 'smtp':
+        description   => 'Exim SMTP',
+        check_command => 'check_smtp',
+    }
+
+    monitor_service { 'http':
+        description   => 'HTTP',
+        check_command => 'check_http',
+    }
 }
