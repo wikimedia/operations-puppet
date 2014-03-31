a3b8c37f0b82a60384a4648f575d24da090d6403
dzahn <dzahn@wikimedia.org>
2014-03-31 02:08:39 -0700

    lint role/deployment
    20 x "tab character found"
    37 x "double quoted string containing no variables"
     2 x "quoted boolean value found"
     1 x "ensure found on line but it's not the first attribute"
    
    --
    13 of 50 x "=> on line isn't properly aligned"
    
    (i know matanya will hate partial work, but try aligning
    all => in this one correctly, heh, and you will see.
    it could be correct but look super ugly anyways, i would
    call it a good example of the limits of following style)
    
    :)
    
    Change-Id: Ia5ac299c4661beef05ac2c6ff704838e98a99193

--- manifests/role/deployment.pp
+++ manifests/role/deployment.pp
@@ -154,55 +154,55 @@ class role::deployment::deployment_servers::common {
   # Can't include this while scap is present on tin:
   # include misc::deployment::scripts
 
-  class { "deployment::deployment_server":
+  class { 'deployment::deployment_server':
     deployer_groups => ['wikidev'],
   }
 
-  class { "apache": }
-  class { "apache::mod::dav": }
-  class { "apache::mod::dav_fs": }
+  class { 'apache': }
+  class { 'apache::mod::dav': }
+  class { 'apache::mod::dav_fs': }
 
-  class { "applicationserver::packages": }
+  class { 'applicationserver::packages': }
 
-  apache::vhost { "default":
-    priority		=> 000,
-    port		=> 80,
-    docroot		=> "/var/www",
-    ensure		=> absent,
-    configure_firewall 	=> false,
+  apache::vhost { 'default':
+    ensure      => absent,
+    priority    => 000,
+    port        => 80,
+    docroot     => '/var/www',
+    configure_firewall  => false,
   }
 }
 
 class role::deployment::deployment_servers::production {
   include role::deployment::deployment_servers::common
 
-  apache::vhost { "tin.eqiad.wmnet":
-    priority		=> 10,
-    vhost_name		=> "10.64.0.196",
-    port		=> 80,
-    docroot		=> "/srv/deployment",
-    docroot_owner	=> "trebuchet",
-    docroot_group	=> "wikidev",
-    docroot_dir_allows  => ["10.0.0.0/16","10.64.0.0/16","208.80.152.0/22"],
-    serveradmin		=> "noc@wikimedia.org",
-    configure_firewall 	=> false,
+  apache::vhost { 'tin.eqiad.wmnet':
+    priority             => 10,
+    vhost_name           => '10.64.0.196',
+    port                 => 80,
+    docroot              => '/srv/deployment',
+    docroot_owner        => 'trebuchet',
+    docroot_group        => 'wikidev',
+    docroot_dir_allows   => ['10.0.0.0/16','10.64.0.0/16','208.80.152.0/22'],
+    serveradmin          => 'noc@wikimedia.org',
+    configure_firewall   => false,
   }
-  class { "redis":
-    dir => "/srv/redis",
-    maxmemory => "500Mb",
-    monitor => "true",
+  class { 'redis':
+    dir => '/srv/redis',
+    maxmemory => '500Mb',
+    monitor => true,
   }
-  package { "percona-toolkit":
+  package { 'percona-toolkit':
     ensure => latest;
   }
-  sudo_group { "wikidev_deployment_server":
+  sudo_group { 'wikidev_deployment_server':
     privileges => [
-      "ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet --out=json pillar.data",
-      "ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet publish.runner deploy.fetch *",
-      "ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet publish.runner deploy.checkout *",
-      "ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet --out=json publish.runner deploy.restart *",
+      'ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet --out=json pillar.data',
+      'ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet publish.runner deploy.fetch *',
+      'ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet publish.runner deploy.checkout *',
+      'ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet --out=json publish.runner deploy.restart *',
     ],
-    group => "wikidev",
+    group => 'wikidev',
   }
 }
 
@@ -215,14 +215,14 @@ class role::deployment::salt_masters::labs {
   }
   $deployment_config = {
     'parent_dir' => '/srv/deployment',
-    'servers'        => {
-        'pmtpa' => $deployment_server,
-        'eqiad' => $deployment_server,
+    'servers'    => {
+        'pmtpa'  => $deployment_server,
+        'eqiad'  => $deployment_server,
     },
-    'redis'          => {
-      'host' => $deployment_server,
-      'port' => '6379',
-      'db'   => '0',
+    'redis'      => {
+      'host'     => $deployment_server,
+      'port'     => '6379',
+      'db'       => '0',
     },
   }
   class { '::role::deployment::config': }
@@ -242,26 +242,26 @@ class role::deployment::deployment_servers::labs {
     $deployment_server = "${::instanceproject}-deploy.eqiad.wmflabs"
   }
   apache::vhost { $deployment_server:
-    priority		=> 10,
-    port		=> 80,
-    docroot		=> "/srv/deployment",
-    docroot_owner	=> 'trebuchet',
-    docroot_group	=> "project-${::instanceproject}",
+    priority            => 10,
+    port                => 80,
+    docroot             => '/srv/deployment',
+    docroot_owner       => 'trebuchet',
+    docroot_group       => "project-${::instanceproject}",
     docroot_dir_allows  => ['10.0.0.0/8'],
-    serveradmin		=> "noc@wikimedia.org",
-    configure_firewall 	=> false,
+    serveradmin         => 'noc@wikimedia.org',
+    configure_firewall  => false,
   }
-  class { "redis":
-    dir => "/srv/redis",
-    maxmemory => "500Mb",
-    monitor => "false",
+  class { 'redis':
+    dir       => '/srv/redis',
+    maxmemory => '500Mb',
+    monitor   => false,
   }
   sudo_group { "project_${::instanceproject}_deployment_server":
     privileges => [
-      "ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet --out=json pillar.data",
-      "ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet publish.runner deploy.fetch *",
-      "ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet publish.runner deploy.checkout *",
-      "ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet --out=json publish.runner deploy.restart *",
+      'ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet --out=json pillar.data',
+      'ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet publish.runner deploy.fetch *',
+      'ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet publish.runner deploy.checkout *',
+      'ALL = (root) NOPASSWD: /usr/bin/salt-call -l quiet --out=json publish.runner deploy.restart *',
     ],
     group => "project-${::instanceproject}",
   }
