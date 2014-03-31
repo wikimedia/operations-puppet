db010237561fa9faa780703b2acd1c1f645a2f04
dzahn <dzahn@wikimedia.org>
2014-03-31 01:42:39 -0700

    lint role/keystone (labs)
    57 x "tab character found"
    48 x "double quoted string containing no variables"
    23 x "=> on line isn't properly aligned"
    
    this trades unaligned => for lines over 80 chars though :p
    
    Change-Id: Id8a90030be50456d6c783bbcbe0ae44ef20f59d7

--- manifests/role/keystone.pp
+++ manifests/role/keystone.pp
@@ -1,67 +1,67 @@
 class role::keystone::config {
-	include passwords::openstack::keystone
+    include passwords::openstack::keystone
 
-	$commonkeystoneconfig = {
-		db_name => "keystone",
-		db_user => "keystone",
-		db_pass => $passwords::openstack::keystone::keystone_db_pass,
-		ldap_base_dn => "dc=wikimedia,dc=org",
-		ldap_user_dn => "uid=novaadmin,ou=people,dc=wikimedia,dc=org",
-		ldap_user_id_attribute => "uid",
-		ldap_tenant_id_attribute => "cn",
-		ldap_user_name_attribute => "uid",
-		ldap_tenant_name_attribute => "cn",
-		ldap_user_pass => $passwords::openstack::keystone::keystone_ldap_user_pass,
-		ldap_proxyagent => "cn=proxyagent,ou=profile,dc=wikimedia,dc=org",
-		ldap_proxyagent_pass => $passwords::openstack::keystone::keystone_ldap_proxyagent_pass,
-		auth_protocol => "http",
-		auth_port => "35357",
-		admin_token => $passwords::openstack::keystone::keystone_admin_token,
-		token_driver_password => $passwords::openstack::keystone::keystone_db_pass,
-	}
+    $commonkeystoneconfig = {
+        db_name                    => 'keystone',
+        db_user                    => 'keystone',
+        db_pass                    => $passwords::openstack::keystone::keystone_db_pass,
+        ldap_base_dn               => 'dc=wikimedia,dc=org',
+        ldap_user_dn               => 'uid=novaadmin,ou=people,dc=wikimedia,dc=org',
+        ldap_user_id_attribute     => 'uid',
+        ldap_tenant_id_attribute   => 'cn',
+        ldap_user_name_attribute   => 'uid',
+        ldap_tenant_name_attribute => 'cn',
+        ldap_user_pass             => $passwords::openstack::keystone::keystone_ldap_user_pass,
+        ldap_proxyagent            => 'cn=proxyagent,ou=profile,dc=wikimedia,dc=org',
+        ldap_proxyagent_pass       => $passwords::openstack::keystone::keystone_ldap_proxyagent_pass,
+        auth_protocol              => 'http',
+        auth_port                  => '35357',
+        admin_token                => $passwords::openstack::keystone::keystone_admin_token,
+        token_driver_password      => $passwords::openstack::keystone::keystone_db_pass,
+    }
 }
 class role::keystone::config::pmtpa inherits role::keystone::config {
-	$pmtpakeystoneconfig = {
-		db_host => $realm ? {
-			"production" => "virt0.wikimedia.org",
-			"labs" => "localhost",
-		},
-		ldap_host => $realm ? {
-			"production" => "virt0.wikimedia.org",
-			"labs" => "localhost",
-		},
-		bind_ip => $realm ? {
-			"production" => "208.80.152.32",
-			"labs" => "127.0.0.1",
-		},
-		token_driver => $realm ? {
-			'production' => 'redis',
-			'labs'       => 'redis',
-		},
-	}
-	$keystoneconfig = merge($pmtpakeystoneconfig, $commonkeystoneconfig)
+    $pmtpakeystoneconfig = {
+        db_host      => $realm ? {
+            'production' => 'virt0.wikimedia.org',
+            'labs'       => 'localhost',
+        },
+        ldap_host    => $realm ? {
+            'production' => 'virt0.wikimedia.org',
+            'labs'       => 'localhost',
+        },
+        bind_ip      => $realm ? {
+            'production' => '208.80.152.32',
+            'labs'       => '127.0.0.1',
+        },
+        token_driver => $realm ? {
+            'production' => 'redis',
+            'labs'       => 'redis',
+        },
+    }
+    $keystoneconfig = merge($pmtpakeystoneconfig, $commonkeystoneconfig)
 }
 
 class role::keystone::config::eqiad inherits role::keystone::config {
-	$eqiadkeystoneconfig = {
-		db_host => $realm ? {
-			"production" => "virt1000.wikimedia.org",
-			"labs" => "localhost",
-		},
-		ldap_host => $realm ? {
-			"production" => "virt1000.wikimedia.org",
-			"labs" => "localhost",
-		},
-		bind_ip => $realm ? {
-			"production" => "208.80.154.18",
-			"labs" => "127.0.0.1",
-		},
-		token_driver => $realm ? {
-			'production' => 'redis',
-			'labs'       => 'redis',
-		},
-	}
-	$keystoneconfig = merge($eqiadkeystoneconfig, $commonkeystoneconfig)
+    $eqiadkeystoneconfig = {
+        db_host      => $realm ? {
+            'production' => 'virt1000.wikimedia.org',
+            'labs'       => 'localhost',
+        },
+        ldap_host    => $realm ? {
+            'production' => 'virt1000.wikimedia.org',
+            'labs'       => 'localhost',
+        },
+        bind_ip      => $realm ? {
+            'production' => '208.80.154.18',
+            'labs'       => '127.0.0.1',
+        },
+        token_driver => $realm ? {
+            'production' => 'redis',
+            'labs'       => 'redis',
+        },
+    }
+    $keystoneconfig = merge($eqiadkeystoneconfig, $commonkeystoneconfig)
 }
 
 class role::keystone::server {
@@ -69,11 +69,11 @@ class role::keystone::server {
             role::keystone::config::eqiad
 
     $keystoneconfig = $site ? {
-        "pmtpa" => $role::keystone::config::pmtpa::keystoneconfig,
-        "eqiad" => $role::keystone::config::eqiad::keystoneconfig,
+        'pmtpa' => $role::keystone::config::pmtpa::keystoneconfig,
+        'eqiad' => $role::keystone::config::eqiad::keystoneconfig,
     }
 
-    class { "openstack::keystone-service": openstack_version => $openstack_version, keystoneconfig => $keystoneconfig }
+    class { 'openstack::keystone-service': openstack_version => $openstack_version, keystoneconfig => $keystoneconfig }
 
     include role::keystone::redis
 }
@@ -91,13 +91,13 @@ class role::keystone::redis {
         }
     }
 
-    class { "::redis":
-        maxmemory                 => "250mb",
-        persist                   => "aof",
+    class { '::redis':
+        maxmemory                 => '250mb',
+        persist                   => 'aof',
         redis_replication         => $replication,
         password                  => $passwords::openstack::keystone::keystone_db_pass,
-        dir                       => "/var/lib/redis/",
-        auto_aof_rewrite_min_size => "64mb",
+        dir                       => '/var/lib/redis/',
+        auto_aof_rewrite_min_size => '64mb',
     }
 }
 
@@ -105,12 +105,12 @@ class role::keystone::redis {
 class role::keystone::redis::labs {
     include passwords::openstack::keystone
 
-    class { "::redis":
-        maxmemory                 => "250mb",
-        persist                   => "aof",
+    class { '::redis':
+        maxmemory                 => '250mb',
+        persist                   => 'aof',
         redis_replication         => { 'nova-precise3' => 'nova-precise2' },
         password                  => $passwords::openstack::keystone::keystone_db_pass,
-        dir                       => "/var/lib/redis/",
-        auto_aof_rewrite_min_size => "64mb",
+        dir                       => '/var/lib/redis/',
+        auto_aof_rewrite_min_size => '64mb',
     }
 }
