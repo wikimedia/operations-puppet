c1f6ff44af929300db4d7d885a763fb28d98a800
Marius Hoch <hoo@online.de>
2014-04-01 02:46:10 +0200

    Puppetize purge-checkuser
    ... without having parts of MediaWiki run as root in production
    
    Earlier try: Iaed6e91
    
    Change-Id: I448bf155c2436ac07506c7390d1d63cb620a0ebf

--- manifests/site.pp
+++ manifests/site.pp
@@ -2562,6 +2562,9 @@ node 'terbium.eqiad.wmnet' {
     class { 'misc::maintenance::purge_abusefilter':
         enabled => true,
     }
+    class { 'misc::maintenance::purge_checkuser':
+        enabled => true,
+    }
 
     # (bug 15434) Periodical run of currently disabled special pages
     # to be run against PMTPA slaves
