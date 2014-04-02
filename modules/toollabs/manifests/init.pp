5c5810add2ef7fa7446e1f3d1d00a2c758041127
Tim Landscheidt <tim@tim-landscheidt.de>
2014-04-02 00:27:38 +0000

    Tools: Alias tools.wmflabs.org to internal webproxy
    The (external) IP for tools.wmflabs.org is not accessible from within
    Labs.  To work around that, this change creates an alias that points
    to tools-webproxy.
    
    Change-Id: Iff526cccf1b5e887da02e32de15339c9c94215d1

--- modules/toollabs/manifests/init.pp
+++ modules/toollabs/manifests/init.pp
@@ -136,4 +136,22 @@ class toollabs {
         force  => true,
         target => "${store}/mail",
     }
+
+    # The (external) IP for tools.wmflabs.org is not accessible from
+    # within Labs.  To work around that, we create an alias that
+    # points to tools-webproxy.  Note that dig is called on the
+    # puppetmaster, not the individual instances.
+    $projectdomain = $::instanceproject ? {
+        'toolsbeta' => 'tools-beta.wmflabs.org',
+        default     => 'tools.wmflabs.org',
+    }
+    $webproxyip = chomp(generate('/usr/bin/dig',
+                                 '+short',
+                                 $projectdomain))
+    host { "${::instanceproject}-webproxy":
+        ensure       => present,
+        host_aliases => ["${::instanceproject}-webproxy.eqiad.wmflabs",
+                         $projectdomain],
+        ip           => $webproxyip,
+    }
 }
