45c1669b5fbcb90f20ef3b55c9973ceebc18727f
Coren <marc@uberbox.org>
2014-04-03 13:59:46 -0400

    Labs: -pmtpa support from role::labs::instance
    Change-Id: I8b669b8f0a20aac69114ead970db71208def9132

--- manifests/role/labs.pp
+++ manifests/role/labs.pp
@@ -23,101 +23,78 @@ class role::labs::instance {
         mode   => '0755',
     }
 
-    if $::site == 'eqiad' {
+    $nfs_opts = 'vers=4,bg,hard,intr,sec=sys,proto=tcp,port=0,noatime,nofsc'
+    $nfs_server = 'labstore.svc.eqiad.wmnet'
 
-        $nfs_opts = 'vers=4,bg,hard,intr,sec=sys,proto=tcp,port=0,noatime,nofsc'
-        $nfs_server = 'labstore.svc.eqiad.wmnet'
-
-        mount { '/home':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "rw,${nfs_opts}",
-            device => "${nfs_server}:/project/${instanceproject}/home",
-        }
-
-        file { '/data/project':
-            ensure => directory,
-            require => File['/data', '/etc/idmapd.conf'],
-        }
-        mount { '/data/project':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "rw,${nfs_opts}",
-            device => "${nfs_server}:/project/${instanceproject}/project",
-            require => File['/data/project'],
-        }
-
-        file { '/data/scratch':
-            ensure => directory,
-            require => File['/data', '/etc/idmapd.conf'],
-        }
-        mount { '/data/scratch':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "rw,${nfs_opts}",
-            device => "${nfs_server}:/scratch",
-            require => File['/data/scratch'],
-        }
-
-        file { '/public/dumps':
-            ensure => directory,
-            require => File['/public'],
-        }
-        mount { '/public/dumps':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "ro,${nfs_opts}",
-            device => "${nfs_server}:/dumps",
-            require => File['/public/dumps'],
-        }
-
-        file { '/public/backups':
-            ensure => directory,
-            require => File['/public'],
-        }
-        mount { '/public/backups':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "ro,${nfs_opts}",
-            device => "${nfs_server}:/backups",
-            require => File['/public/backups'],
-        }
-
-
-        file { '/public/keys':
-            ensure => directory,
-            require => File['/public'],
-        }
-        mount { '/public/keys':
-            ensure => mounted, atboot => true, fstype => 'nfs', options => "ro,${nfs_opts}",
-            device => "${nfs_server}:/keys",
-            require => File['/public/keys'],
-            notify => Service['ssh'],
-        }
-
-        service { 'idmapd':
-            ensure => running,
-            subscribe => File['/etc/idmapd.conf'],
-        }
-
-        file { '/etc/idmapd.conf':
-            ensure => present,
-            owner => root,
-            group => root,
-            mode => 0444,
-            source => 'puppet:///files/nfs/idmapd.conf',
-        }
-
-        # This needs to be disabled for now; it's image-based
-        # not site-based
-        #
-        # class { '::labs_lvm':
-        #     disk => '/dev/vda',
-        # }
-
-    } else {
-
-        include gluster::client
-
-        # Storage backend to use for /home & /data/project
-        # Configured on a per project basis inside puppet since we do not have any
-        # other good way to do so yet.
-        # FIXME  this is ugly and need to be removed whenever we got rid of
-        # the Gluster shared storage.
-        if $::instanceproject == 'deployment-prep' {
-                include role::labsnfs::client
-        }
-
+    mount { '/home':
+        ensure => mounted, atboot => true, fstype => 'nfs', options => "rw,${nfs_opts}",
+        device => "${nfs_server}:/project/${instanceproject}/home",
     }
+
+    file { '/data/project':
+        ensure => directory,
+        require => File['/data', '/etc/idmapd.conf'],
+    }
+    mount { '/data/project':
+        ensure => mounted, atboot => true, fstype => 'nfs', options => "rw,${nfs_opts}",
+        device => "${nfs_server}:/project/${instanceproject}/project",
+        require => File['/data/project'],
+    }
+
+    file { '/data/scratch':
+        ensure => directory,
+        require => File['/data', '/etc/idmapd.conf'],
+    }
+    mount { '/data/scratch':
+        ensure => mounted, atboot => true, fstype => 'nfs', options => "rw,${nfs_opts}",
+        device => "${nfs_server}:/scratch",
+        require => File['/data/scratch'],
+    }
+
+    file { '/public/dumps':
+        ensure => directory,
+        require => File['/public'],
+    }
+    mount { '/public/dumps':
+        ensure => mounted, atboot => true, fstype => 'nfs', options => "ro,${nfs_opts}",
+        device => "${nfs_server}:/dumps",
+        require => File['/public/dumps'],
+    }
+
+    file { '/public/backups':
+        ensure => directory,
+        require => File['/public'],
+    }
+    mount { '/public/backups':
+        ensure => mounted, atboot => true, fstype => 'nfs', options => "ro,${nfs_opts}",
+        device => "${nfs_server}:/backups",
+        require => File['/public/backups'],
+    }
+
+
+    file { '/public/keys':
+        ensure => directory,
+        require => File['/public'],
+    }
+    mount { '/public/keys':
+        ensure => mounted, atboot => true, fstype => 'nfs', options => "ro,${nfs_opts}",
+        device => "${nfs_server}:/keys",
+        require => File['/public/keys'],
+        notify => Service['ssh'],
+    }
+
+    service { 'idmapd':
+        ensure => running,
+        subscribe => File['/etc/idmapd.conf'],
+    }
+
+    file { '/etc/idmapd.conf':
+        ensure => present,
+        owner => root,
+        group => root,
+        mode => 0444,
+        source => 'puppet:///files/nfs/idmapd.conf',
+    }
+
 }
 
