d4a3e51cecd54826ae8bb3c44c6b445a49fc1d45
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-04-03 22:46:11 +0300

    Create and import shapelines from a pre-existing dump
    Change-Id: I9a119ece33f52421dd2ea8da279fcb2aa7d13dbe

--- modules/osm/manifests/populatedb.pp
+++ modules/osm/manifests/populatedb.pp
@@ -18,10 +18,13 @@
 #
 define osm::populatedb(
     $input_pbf_file,
+    $input_shape_file,
+    $shape_table,
     ) {
 
     # Check if our db tables exist
     $tables_exist = "/usr/bin/psql -d ${name} --tuples-only -c \'SELECT table_name FROM information_schema.tables;\' | /bin/grep \'planet_osm\'"
+    $shapelines_exist = "/usr/bin/psql -d ${name} --tuples-only -c \'SELECT table_name FROM information_schema.tables;\' | /bin/grep \'${shape_table}\'"
 
     # Note. This is not needed anymore with osm2pgsql 0.81
     exec { "load_900913-${name}":
@@ -36,5 +39,23 @@ define osm::populatedb(
         user        => 'postgres',
         refreshonly => true,
         subscribe   => Exec["load_900913-${name}"],
+    }
+
+    exec { "create_shapelines-${name}":
+        command     => "/usr/bin/shp2pgsql -D -I ${input_shape_file} ${shape_table} > /tmp/${shape_table}.dump",
+        user        => 'postgres',
+        unless      => $shapelines_exist,
+    }
+    exec { "load_shapefiles-${name}":
+        command     => "/usr/bin/psql -d ${name} -f /tmp/${shape_table}.dump",
+        user        => 'postgres',
+        refreshonly => true,
+        subscribe   => Exec["create_shapelines-${name}"],
+    }
+    exec { "delete_shapefiles-${name}":
+        command     => "/bin/rm /tmp/${shape_table}.dump",
+        user        => 'postgres',
+        refreshonly => true,
+        subscribe   => Exec["load_shapefiles-${name}"],
     }
 }
