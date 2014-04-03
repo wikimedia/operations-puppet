34039eeba78d95354057e0b4269987070e7fca2e
Bryan Davis <bd808@wikimedia.org>
2014-04-03 11:13:56 -0600

    [WIP] Configure scap master and clients in beta
    This will eventually be used in beta to deploy MediaWiki code changes
    from deployment-bastion to the rest of the beta cluster.
    
    beta::scap::target
    * Install authorized_keys for the mwdeploy user
    * Configure pam to allow the mwdeploy user to access hosts from
       deployment-bastion
    * Create /srv/common-local directory that scap will target
    
    beta::scap::master
    * Install ssh private key on deployment-bastion
    * Setup rsync server
    * Install and configure dsh
    * Install wmf-beta-scap for running scap as mwdeploy easily
    
    beta::scap::rsync_slave
    * Configure rsync fanout servers for scap
    
    beta::mwdeploy_sudo
    * Manage sudo rights for mwdeploy which are needed by both
      beta::scap::target and beta::autoupdate
    
    * Include beta::scap::target in role::applicationserver::appserver::beta
    * Include beta::scap::master in role::beta::bastion
    * Add ferm rule to beta::natfix that allows ssh from
      deployment-bastion.eqiad.wmflabs
    * Change misc::deployment::vars to set MW_COMMON to /srv/common-local
    
    This change assumes that a couple of things have been done to the
    labs/private repository on the beta puppetmaster:
    * files/scap/id_rsa and files/scap/id_rsa.pub are present
    * File['/etc/ssh/userkeys'] in passwords::root changed to ensure file
      permissions of 0755.
    
    These non-public changes have been made as a local commit to
    /var/lib/git/labs/private on deployment-salt.eqiad.wmflabs
    
    Bug: 63746
    Change-Id: I8f77e0c131daa6147c22021ef08bf7dd2eab99a2

---
+++ modules/beta/manifests/scap/rsync_slave.pp
@@ -0,0 +1,16 @@
+# == Class: beta::scap::rsync_slave
+#
+# Provisions scap components for a scap slave rsync server.
+#
+class beta::scap::rsync_slave {
+    include ::beta::config
+    include ::beta::scap::target
+    include rsync::server
+
+    # Run an rsync server
+    rsync::server::module { 'common':
+        path        => '/srv/common-local',
+        read_only   => 'true',
+        hosts_allow => $::beta::config::rsync_networks,
+    }
+}
---
+++ modules/beta/manifests/scap/target.pp
@@ -0,0 +1,56 @@
+# == Class: beta::scap::target
+#
+# Provisions scap components for a scap target node.
+#
+class beta::scap::target {
+    include ::beta::config
+    include ::misc::deployment::scap_scripts
+    include ::beta::mwdeploy_sudo
+
+    # Install authorized_keys for mwdeploy user
+    file { '/etc/ssh/userkeys/mwdeploy':
+        ensure  => directory,
+        owner   => 'mwdeploy',
+        group   => 'mwdeploy',
+        mode    => '0700',
+        require => File['/etc/ssh/userkeys'],
+    }
+    file { '/etc/ssh/userkeys/mwdeploy/.ssh':
+        ensure  => directory,
+        owner   => 'mwdeploy',
+        group   => 'mwdeploy',
+        mode    => '0700',
+        require => File['/etc/ssh/userkeys/mwdeploy'],
+    }
+    file { '/etc/ssh/userkeys/mwdeploy/.ssh/authorized_keys':
+        owner   => 'mwdeploy',
+        group   => 'mwdeploy',
+        mode    => '0600',
+        source  => 'puppet:///private/scap/id_rsa.pub',
+        require => File['/etc/ssh/userkeys/mwdeploy/.ssh'],
+    }
+
+    # Hack to replace /etc/security/access.conf (which is managed by the
+    # ldap::client class) with a modified version that includes an access
+    # grant for the mwdeploy user to authenticate from deployment-bastion.
+    file { '/etc/security/access.conf~':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('beta/pam-access.conf.erb'),
+    }
+    File <| title == '/etc/security/access.conf' |> {
+        content => undef,
+        source  => '/etc/security/access.conf~',
+        require => File['/etc/security/access.conf~'],
+    }
+
+    # Target directory for scap
+    file { '/srv/common-local':
+        ensure  => directory,
+        owner   => 'mwdeploy',
+        group   => 'mwdeploy',
+        mode    => '0755',
+    }
+}
+
