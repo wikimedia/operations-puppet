152e14101c94a5653ea32ca994196759c03cd868
Andrew Otto <acotto@gmail.com>
2014-04-04 15:50:48 -0400

    base::firewall always needs defs, but main-input should be absent if ensure => absent
    Change-Id: I153b175f558944a203e9bc923c7baab742f6a82c

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -400,21 +400,22 @@ class base::firewall($ensure = 'present') {
     include network::constants
     include ferm
 
-    ferm::conf { 'main':
-        ensure  => 'present',
-        prio    => '00',
-        source  => 'puppet:///modules/base/firewall/main-input-default-drop.conf',
-    }
-
     $defscontent = $::realm ? {
         'labs'  => template('base/firewall/defs.erb', 'base/firewall/defs.labs.erb'),
         default => template('base/firewall/defs.erb'),
     }
-
     ferm::conf { 'defs':
+        # defs can always be present.
+        # They don't actually do firewalling.
         ensure  => 'present',
         prio    => '00',
         content => $defscontent,
+    }
+
+    ferm::conf { 'main':
+        ensure  => $ensure,
+        prio    => '00',
+        source  => 'puppet:///modules/base/firewall/main-input-default-drop.conf',
     }
 
     ferm::rule { 'bastion-ssh':
