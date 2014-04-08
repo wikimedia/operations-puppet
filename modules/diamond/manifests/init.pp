65fa7805d776f88bb821c9cfd630e8afe1cf4907
cpettet <rush@wikimedia.org>
2014-04-08 18:02:06 +0300

    module to manage new python-diamond package
    Change-Id: Iff78d1a20ea40140a437f8ce317fbdc65e58f741

---
+++ modules/diamond/manifests/init.pp
@@ -0,0 +1,17 @@
+class diamond($statsd_host=undef, $statsd_port=undef) {
+
+        package { 'python-diamond': ensure => present }
+
+        file { '/etc/diamond/diamond.conf':
+            content => template('diamond/diamond.conf.erb'),
+            require => Package['python-diamond'],
+        }
+
+        service { 'diamond':
+            ensure => running,
+            enable => true,
+            hasrestart => true,
+            hasstatus => true,
+            subscribe => File['/etc/diamond/diamond.conf'],
+        }
+}
