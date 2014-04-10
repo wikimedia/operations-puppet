6117d075890e16aa5a68984d7e16fc5d061ef89e
Andrew Otto <acotto@gmail.com>
2014-04-10 17:21:01 -0400

    Installing java version 7u25-2.3.10-1ubuntu0.12.04.2 for elasticsearch
    Change-Id: I50507fe73d6989d774f0bf9774d9b0b1381f9521

--- modules/elasticsearch/manifests/packages.pp
+++ modules/elasticsearch/manifests/packages.pp
@@ -3,13 +3,20 @@
 # Provisions Elasticsearch package and dependencies.
 #
 class elasticsearch::packages {
-    package { 'elasticsearch': }
 
      if ! defined ( Package['openjdk-7-jdk'] ) {
-         package { 'openjdk-7-jdk': ensure => present }
-    }
-     if ! defined ( Package['curl'] ) {
-         package { 'curl': ensure => present }
+         package { 'openjdk-7-jdk':
+             # we want openjdk 1.7.0_25 for elasticsearch
+             ensure => '7u25-2.3.10-1ubuntu0.12.04.2',
+         }
     }
 
+    package { 'elasticsearch':
+        require => Package['openjdk-7-jdk'],
+        ensure  => 'present',
+    }
+
+    if ! defined ( Package['curl'] ) {
+        package { 'curl': ensure => present }
+    }
 }
