74b8d12a292b1d3a5231eaf8c621118607c8a995
Ariel T. Glenn <ariel@wikimedia.org>
2014-04-14 17:41:06 +0300

    ms1001 should no longer sync from nas, long since obsolete
    Change-Id: I6da989d123f4b6df3c01dd55cea334a4d5303f42

--- modules/download/manifests/wikimedia.pp
+++ modules/download/manifests/wikimedia.pp
@@ -1,28 +1,67 @@
-class download::wikimedia {
-    system::role { 'download::wikimedia': description => 'download.wikimedia.org' }
-
-    package { 'lighttpd':
-        ensure => latest,
-    }
+class download::wikimedia($nginx=false) {
+    system::role { 'download::wikimedia':
+        description => 'download.wikimedia.org' }
 
     install_certificate{ 'dumps.wikimedia.org': ca => 'RapidSSL_CA.pem' }
-
-    file { '/etc/lighttpd/lighttpd.conf':
-        mode   => '0444',
-        owner  => 'root',
-        group  => 'root',
-        path   => '/etc/lighttpd/lighttpd.conf',
-        source => 'puppet:///modules/download/lighttpd.conf',
-    }
-
-    service { 'lighttpd':
-        ensure => running,
-    }
-
     include generic::higher_min_free_kbytes
 
-    monitor_service { 'lighttpd http':
-        description   => 'LighttpdHTTP',
-        check_command => 'check_http'
+    if ($nginx) {
+        if ($::lsbdistcodename == 'lucid') {
+            package { 'nginx':
+                ensure => latest,
+            }
+        }
+        else {
+            package { 'nginx-full':
+                ensure => latest,
+            }
+        }
+
+        file { '/etc/nginx/nginx.conf':
+            mode    => '0444',
+            owner   => 'root',
+            group   => 'root',
+            path    => '/etc/nginx/nginx.conf',
+            content => template('download/nginx/nginx.conf.erb'),
+        }
+
+        file { '/etc/nginx/sites-available/dumps.conf':
+            mode   => '0444',
+            owner  => 'root',
+            group  => 'root',
+            path   => '/etc/nginx/sites-available/dumps.conf',
+            source => 'puppet:///modules/download/nginx/dumps.conf',
+        }
+
+        service { 'nginx':
+            ensure => running,
+        }
+
+        monitor_service { 'nginx http':
+            description   => 'nginxHTTP',
+            check_command => 'check_http'
+        }
+    }
+    else {
+        package { 'lighttpd':
+            ensure => latest,
+        }
+
+        file { '/etc/lighttpd/lighttpd.conf':
+            mode   => '0444',
+            owner  => 'root',
+            group  => 'root',
+            path   => '/etc/lighttpd/lighttpd.conf',
+            source => 'puppet:///modules/download/lighttpd.conf',
+        }
+
+        service { 'lighttpd':
+            ensure => running,
+        }
+
+        monitor_service { 'lighttpd http':
+            description   => 'LighttpdHTTP',
+            check_command => 'check_http'
+        }
     }
 }
