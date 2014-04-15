8490c081f5d0460db846373e1bd4bd43878963db
Andrew Otto <acotto@gmail.com>
2014-04-15 14:53:55 -0400

    Prefer Ubuntu's openjdk-7* packages rather than what we have in Wikimedia apt
    Elasticsearch requires a specific version of Java to avoid some crazy bugs.
    This allows us to keep that version of Java in our apt, while still prefering
    Ubuntu's Java by default.  Elasticsearch puppetization manually chooses
    the version of Java it needs.
    
    Change-Id: I068e0b961ca917df1eac6914c10f8805f46ea9eb

--- modules/apt/manifests/init.pp
+++ modules/apt/manifests/init.pp
@@ -39,6 +39,11 @@ class apt {
         pin      => 'release o=Wikimedia',
         priority => 1001,
     }
+    apt::pin { 'openjdk-7':
+        package  => 'openjdk-7*',
+        pin      => 'release o=Ubuntu',
+        priority => 1002,
+    }
 
     $enable_proxy = $::site ? {
         pmtpa   => present,
