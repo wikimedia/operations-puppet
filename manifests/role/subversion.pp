8ccc96e20a692421ff1fcbc799c1553d7bd6f8b7
Ariel T. Glenn <ariel@wikimedia.org>
2014-04-15 08:38:43 +0300

    subversion role lint, pass svn hostname to cert check
    Change-Id: I27ab362127cf3bb0735670ead112731286fb3df9

--- manifests/role/subversion.pp
+++ manifests/role/subversion.pp
@@ -2,15 +2,18 @@
 
 class role::subversion {
 
-    system::role { 'role::subversion': description => 'public, read-only SVN server' }
+    system::role { 'role::subversion':
+        description => 'public, read-only SVN server'
+    }
+    $svnhost = 'svn.wikimedia.org'
 
     class { '::subversion':
-        host => 'svn.wikimedia.org',
+        host => $svnhost,
     }
 
     monitor_service { 'https':
         description   => 'HTTPS',
-        check_command => "check_ssl_cert!${host}"
+        check_command => "check_ssl_cert!${svnhost}",
     }
 }
 
