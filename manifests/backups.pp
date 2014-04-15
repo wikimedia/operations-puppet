3568733d0557644cdbe5b4ca67ea4281c2afc928
Ariel T. Glenn <ariel@wikimedia.org>
2014-04-15 14:16:26 +0300

    show what's backed up in motd (rt #469)
    Change-Id: Id97bef7c69c5b94f1cbf1f3c3419b3b4ab56ff8b

--- manifests/backups.pp
+++ manifests/backups.pp
@@ -10,6 +10,15 @@ define backup::set {
             fileset     => $name,
             jobdefaults => $jobdefaults,
         }
+        $motd_content = "Backed up on this host: $name\n"
+
+        file { "/etc/update-motd.d/06-backups-${name}":
+            ensure  => 'present',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0555',
+            content => $motd_content,
+        }
     }
 }
 
