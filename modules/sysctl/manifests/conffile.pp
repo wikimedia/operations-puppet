945cee48f2348ca50f075d288d562a28637539b0
Ori Livneh <ori@wikimedia.org>
2014-04-16 13:05:09 -0700

    Sysctl: make the default priority 70
    udp2log broke because the kernel parameters it requires were being clobbered.
    This is because both Sysctl::Parameters['big rmem'] and
    Sysctl::parameters['wikimedia base'] were set to the default priority of 60,
    and each was attempting to set net.core.rmem_max to a different value.
    
    In change I50d929847, Andrew B. and I fixed things by setting the priority on
    Sysctl::Parameters['big rmem'] to 70. In hindsight, this doesn't go far enough.
    We can safely assume that in case of conflict between 'wikimedia base' and some
    other Sysctl::Parameters resource, it's the other resource that should win,
    since it is more specific than 'wikimedia base', which specifies cluster-wide
    defaults.
    
    Having the default priority set to 60 is unfortunate, because that is the
    lowest possible value that is not reserved by Debian/Ubuntu for sysctl configs
    bundled with packages, so it doesn't leave room for declaring broad defaults.
    So: make the default 70, and change 'wikimedia base' to 60.
    
    Also add documentation and remove the 'filename' parameter from both
    Sysctl::Parameters and Sysctl::Conffile, since it is both unnecessary and
    leaves a way to circumvent the naming convention.
    
    I tested this on Vagrant. I also confirmed that integer values are
    automatically cast to strings for the purpose of regex matching, so a literal
    '70' (the number, not the string) does indeed match /^\d?\d$/.
    
    Change-Id: I02673456fdaec1e065dff449a12435c5ecccf14a

--- modules/sysctl/manifests/conffile.pp
+++ modules/sysctl/manifests/conffile.pp
@@ -2,17 +2,50 @@
 #
 # Represents a file with sysctl kernel parameters in /etc/sysctl.d.
 #
+# === Parameters
+#
+# [*content*]
+#   The content of the file provided as a string. Either this or
+#   'source' must be specified.
+#
+# [*source*]
+#   The content of the file provided as a puppet:/// file reference.
+#   Either this or 'content' must be specified.
+#
+# [*priority*]
+#   A numeric value in range 60 - 99. In case of conflict, files with a
+#   higher priority override files with a lower priority. Debian
+#   reserves 0 - 59 for sysctl settings that are bundled with individual
+#   packages. The default is 70. Values in 60 - 69 should be reserved
+#   for cluster-wide defaults that should always have a lower priority
+#   than role-specific customizations.
+#
+#   If you're not sure, leave this unspecified. The default value of 60
+#   should suit most cases.
+#
+# === Examples
+#
+#  sysctl::conffile { 'hadoop':
+#    content  => template('hadoop/hadoop.conf.erb'),
+#    priority => 90,
+#  }
+#
 define sysctl::conffile(
     $ensure   = present,
-    $file     = $title,
     $content  = undef,
     $source   = undef,
-    $priority = 60
+    $priority = 70
 ) {
-    include sysctl
+    include ::sysctl
 
-    $basename = regsubst($file, '\W', '-', 'G')
-    file { "/etc/sysctl.d/${priority}-${basename}.conf":
+    if $priority !~ /^\d?\d$/ {
+        fail("'priority' must be an integer between 0 - 99 (got: ${priority}).")
+    }
+
+    $basename = regsubst($title, '\W', '-', 'G')
+    $filename = sprintf('/etc/sysctl.d/%02d-%s.conf', $priority, $basename)
+
+    file { $filename:
         ensure  => $ensure,
         content => $content,
         source  => $source,
