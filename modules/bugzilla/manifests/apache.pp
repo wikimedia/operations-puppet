18febd5d8c193d9fd7f5c967f7d8305e53a0d552
Jan Zerebecki <jan.wikimedia@zerebecki.de>
2014-04-18 17:07:12 +0200

    bugzilla apache: Enable required modules for caching
    The caching directives in the apache configuration for bugzilla are
    currently inactive because at least one of the required apache modules
    is not loaded.
    
    Bug: 49720
    Change-Id: I8e616d3ca006776bf6d450863f62af785171d4a9

--- modules/bugzilla/manifests/apache.pp
+++ modules/bugzilla/manifests/apache.pp
@@ -3,6 +3,10 @@
 # it expects {'webserver::php5': ssl => true; } on the node
 class bugzilla::apache ($svc_name, $attach_svc_name, $docroot, $cipher_suite){
 
+    apache_module { 'headers': name => 'headers' }
+    apache_module { 'expires': name => 'expires' }
+    apache_module { 'env': name => 'env' }
+
     # separate cert and ServerName for attachments for security
     install_certificate{ $svc_name: }
     install_certificate{ $attach_svc_name: }
