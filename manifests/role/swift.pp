ab19c650454cb65be32355d7b9a69a1eff2e2d48
Matanya Moses <matanya@foss.co.il>
2014-04-18 17:28:24 +0300

    swift: remove swift role from tampa
    A prerequisite for RT #7317
    
    Change-Id: I68d9fc342af0660e0bc0abeaeac6272b4aff82ae

--- manifests/role/swift.pp
+++ manifests/role/swift.pp
@@ -9,57 +9,6 @@ class role::swift {
 		include standard
 	}
 	
-	class pmtpa-prod inherits role::swift::base {
-		system::role { "role::swift::pmtpa-prod": description => "Swift pmtpa production cluster" }
-		include passwords::swift::pmtpa-prod
-		class { "::swift::base": hash_path_suffix => "bd51d755d4c53773", cluster_name => "pmtpa-prod" }
-		class ganglia_reporter inherits role::swift::pmtpa-prod {
-			# one host per cluster should report global stats
-			file { "/usr/local/bin/swift-ganglia-report-global-stats":
-				ensure => present,
-				owner  => 'root',
-				group  => 'root',
-				mode   => '0555',
-				source => 'puppet:///files/swift/swift-ganglia-report-global-stats',
-			}
-			# config file to hold the password
-			$password = $passwords::swift::pmtpa-prod::rewrite_password
-			file { "/etc/swift-ganglia-report-global-stats.conf":
-				owner   => 'root',
-				group   => 'root',
-				mode    => '0440',
-				content => template("swift/swift-ganglia-report-global-stats.conf.erb"),
-			}
-			cron { "swift-ganglia-report-global-stats":
-				ensure  => present,
-				command => "/usr/local/bin/swift-ganglia-report-global-stats -C /etc/swift-ganglia-report-global-stats.conf -u 'mw:thumb' -c pmtpa-prod",
-				user    => root,
-			}
-		}
-		class proxy inherits role::swift::pmtpa-prod {
-			class { "::swift::proxy":
-				bind_port => "80",
-				proxy_address => "http://ms-fe.pmtpa.wmnet",
-				num_workers => $::processorcount,
-				memcached_servers => [ "ms-fe1.pmtpa.wmnet:11211", "ms-fe2.pmtpa.wmnet:11211", "ms-fe3.pmtpa.wmnet:11211", "ms-fe4.pmtpa.wmnet:11211" ],
-				auth_backend => 'swauth',
-				super_admin_key => $passwords::swift::pmtpa-prod::super_admin_key,
-				rewrite_account => "AUTH_43651b15-ed7a-40b6-b745-47666abf8dfe",
-				rewrite_password => $passwords::swift::pmtpa-prod::rewrite_password,
-				rewrite_thumb_server => "rendering.svc.eqiad.wmnet",
-				shard_container_list => "wikipedia-commons-local-thumb,wikipedia-de-local-thumb,wikipedia-en-local-thumb,wikipedia-fi-local-thumb,wikipedia-fr-local-thumb,wikipedia-he-local-thumb,wikipedia-hu-local-thumb,wikipedia-id-local-thumb,wikipedia-it-local-thumb,wikipedia-ja-local-thumb,wikipedia-ro-local-thumb,wikipedia-ru-local-thumb,wikipedia-th-local-thumb,wikipedia-tr-local-thumb,wikipedia-uk-local-thumb,wikipedia-zh-local-thumb,wikipedia-commons-local-public,wikipedia-de-local-public,wikipedia-en-local-public,wikipedia-fi-local-public,wikipedia-fr-local-public,wikipedia-he-local-public,wikipedia-hu-local-public,wikipedia-id-local-public,wikipedia-it-local-public,wikipedia-ja-local-public,wikipedia-ro-local-public,wikipedia-ru-local-public,wikipedia-th-local-public,wikipedia-tr-local-public,wikipedia-uk-local-public,wikipedia-zh-local-public,wikipedia-commons-local-temp,wikipedia-de-local-temp,wikipedia-en-local-temp,wikipedia-fi-local-temp,wikipedia-fr-local-temp,wikipedia-he-local-temp,wikipedia-hu-local-temp,wikipedia-id-local-temp,wikipedia-it-local-temp,wikipedia-ja-local-temp,wikipedia-ro-local-temp,wikipedia-ru-local-temp,wikipedia-th-local-temp,wikipedia-tr-local-temp,wikipedia-uk-local-temp,wikipedia-zh-local-temp,wikipedia-commons-local-transcoded,wikipedia-de-local-transcoded,wikipedia-en-local-transcoded,wikipedia-fi-local-transcoded,wikipedia-fr-local-transcoded,wikipedia-he-local-transcoded,wikipedia-hu-local-transcoded,wikipedia-id-local-transcoded,wikipedia-it-local-transcoded,wikipedia-ja-local-transcoded,wikipedia-ro-local-transcoded,wikipedia-ru-local-transcoded,wikipedia-th-local-transcoded,wikipedia-tr-local-transcoded,wikipedia-uk-local-transcoded,wikipedia-zh-local-transcoded,global-data-math-render",
-				backend_url_format => "sitelang"
-			}
-			class { '::swift::proxy::monitoring':
-				host => 'ms-fe.pmtpa.wmnet',
-			}
-
-		}
-		class storage inherits role::swift::pmtpa-prod {
-			include ::swift::storage
-			include ::swift::storage::monitoring
-		}
-	}
 	class eqiad-prod inherits role::swift::base {
 		system::role { "role::swift::eqiad-prod": description => "Swift eqiad production cluster" }
 		include passwords::swift::eqiad-prod
