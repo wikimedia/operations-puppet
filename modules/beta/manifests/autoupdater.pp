54d759c4e4f8871b60ce8bad26740ab7c0f3755f
Bryan Davis <bd808@wikimedia.org>
2014-04-18 19:01:46 -0600

    [WIP] Move beta scap source directory off of NFS
    Change the directory that receives MediaWiki code and configuration via
    Jenkins job updates from the NFS mounted
    /data/project/apache/common-local/ to a local directory on
    deployment-bastion.
    
    Change-Id: I4cbd312e19a9b590760bba8e776fa5f816e20e34

--- modules/beta/manifests/autoupdater.pp
+++ modules/beta/manifests/autoupdater.pp
@@ -4,9 +4,11 @@
 # cluster. This is the lame way to automatically pull any code merged in master
 # branches.
 class beta::autoupdater {
-
+    include ::beta::config
     require misc::deployment::common_scripts
     include ::beta::mwdeploy_sudo
+
+    $stage_dir = $::beta::config::scap_stage_dir
 
     # Parsoid JavaScript dependencies are updated on beta via npm
     package { 'npm':
@@ -19,5 +21,45 @@ class beta::autoupdater {
         mode    => '0555',
         require => Package['git-core'],
         source  => 'puppet:///modules/beta/wmf-beta-autoupdate.py',
+    }
+
+    file { '/usr/local/bin/wmf-beta-mwconfig-update':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        require => Package['git-core'],
+        source  => template('wmf-beta-mwconfig-update.erb'),
+    }
+
+    git::clone { 'operations/mediawiki-config':
+      directory => $stage_dir,
+      branch    => 'master',
+      owner     => 'mwdeploy',
+      group     => 'mwdeploy',
+    }
+
+    git::clone { 'mediawiki/core':
+      directory => "${stage_dir}/php-master",
+      branch    => 'master',
+      owner     => 'mwdeploy',
+      group     => 'mwdeploy',
+      require   => Git::Clone['operations/mediawiki-config'],
+    }
+
+    # Remove the placeholder extension directory of the mediawiki/core
+    # checkout so that we can checkout the complete extension repository.
+    exec { "/bin/rm -r ${stage_dir}/php-master/extensions":
+      refreshonly => true,
+      subscribe   => Git::Clone['mediawiki/core'],
+      before      => Git::Clone['mediawiki/extensions'],
+    }
+
+    git::clone { 'mediawiki/extensions':
+      directory          => "${stage_dir}/php-master/extensions",
+      branch             => 'master',
+      owner              => 'mwdeploy',
+      group              => 'mwdeploy',
+      recurse_submodules => true,
+      require            => Git::Clone['mediawiki/core'],
     }
 }
