d2d2521a4f01e918209d61c670f4162d9cf58f7f
Brandon Black <bblack@wikimedia.org>
2014-04-21 18:21:30 -0500

    Set bnx2x num_queues to match physical cores on lvs nodes
    bnx2x actually defaults to num_queues = num_procs, but doesn't
    account for hyperthreading and caps itself at an internal
    max queue count.  In the case of lvs[34]00x, the simple num_procs
    is 24, the physical core count is 12, and bnx2x ends up defaulting
    to its internal max of 15.  We'd rather have 12 queues to balance
    over the 12 real cores with RPS than 15/12 with three oddballs
    that can't be balanced evenly.
    
    Getting this to take effect requires a reboot or a reload of
    the bnx2x module, which it probably isn't wise to puppet-automate.
    The RPS-balancing script will work regardless, this just makes
    things more-optimal post-reboot/reload.
    
    Change-Id: If88abac93e52147d915e34dc5649cdf8cadf0df3

---
+++ modules/lvs/lib/facter/physicalcorecount.rb
@@ -0,0 +1,12 @@
+# physicalcorecount isn't fooled by hyperthread siblings
+
+require 'facter'
+require 'pathname'
+
+Facter.add("physicalcorecount") do
+    setcode do
+        Pathname::glob('/sys/devices/system/cpu/cpu[0-9]*/topology/thread_siblings_list').map{|x|
+            File.open(x,'r').read().split(',')[0]
+        }.sort.uniq.count
+    end
+end
