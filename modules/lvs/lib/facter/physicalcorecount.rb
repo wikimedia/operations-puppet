56ca643f257183a82ee99ab69cea0caaeb354a60
Brandon Black <bblack@wikimedia.org>
2014-04-21 18:21:30 -0500

    Set bnx2x num_queues to match physical cores on lvs nodes
    Change-Id: If88abac93e52147d915e34dc5649cdf8cadf0df3

---
+++ modules/lvs/lib/facter/physicalcorecount.rb
@@ -0,0 +1,13 @@
+# physicalcorecount isn't fooled by hyperthread siblings
+
+require 'facter'
+require 'pathname'
+
+Facter.add("physicalcorecount") do
+    setcode do
+        Pathname::glob('/sys/devices/system/cpu/cpu[0-9]*').map{|c|
+            File.open(File.join(c, 'topology/thread_siblings_list'), 'r').
+                read().split(',')[0]
+        }.sort.uniq.count
+    end
+end
