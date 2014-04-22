2a55e9e6082229f4d2e1cab8196cb87a92f5c731
Ori Livneh <ori@wikimedia.org>
2014-04-22 16:50:11 -0700

    miscellaneous improvements for diamond module
    * Remove a lot of awful cruft from the sample configuration file that ships
      with Diamond, leaving only what actually needs to be specified.
    * Log to standard out; it will be captured by Upstart and rotated
      automatically.
    * Manage collector and handler configuration files recursively.
    * Accept Puppet hashes for collector and handler configurations.
    * Add documentation and examples.
    * Remove CPU & network collector resources; these belong in a base role class
      instead.
    
    Change-Id: Iee6b0fa772798f75282e639754e8dd8dc8a06eff

--- modules/diamond/manifests/collector.pp
+++ modules/diamond/manifests/collector.pp
@@ -1,11 +1,85 @@
+# == Define: diamond::collector
+#
+# Diamond Collectors are Python modules that run within the Diamond
+# process, collecting metrics about some subsystem, and feeding these
+# metrics into Diamond for aggregation and reporting.
+#
+# If you are familiar with Ganglia, you can think of Collectors as
+# being Diamond's answer to gmond modules.
+#
+# === Parameters
+#
+# [*ensure*]
+#   Specifies whether or not to configure Diamond to use this collector.
+#   May be 'present' or 'absent'. The default is 'present'.
+#
+# [*name*]
+#   The name of the collector class. The 'Collector' suffix may be
+#   omitted from the name.
+#
+# [*settings*]
+#   A hash of configuration settings for the collector.
+#   The 'enabled' setting is set to true by default.
+#
+# [*source*]
+#   A Puppet file reference to the Python collector source file. This parameter
+#   may be omitted if the collector is part of the Diamond distribution. It
+#   should only be set for custom collectors. Unset by default.
+#
+# === Examples
+#
+# Configure an Nginx metric collector:
+#
+#  diamond::collector { 'Nginx':
+#    source   => 'puppet:///modules/nginx/nginx-collector.py',
+#    settings => {
+#      req_host => 'gdash.wikimedia.org',
+#      req_path => '/status',
+#    },
+#  }
+#
+# Configure a CPU utilization metric collector:
+#
+#  diamond::collector { 'CPU':
+#    settings => {
+#      percore   => false,
+#      normalize => true,
+#    },
+#  }
+#
 define diamond::collector(
-    $ensure = 'present',
-    $config = undef,
+    $settings,
+    $ensure  = present,
+    $source  = undef,
 ) {
-    file { "/etc/diamond/collectors/${name}.conf":
-        ensure     => $ensure,
-        content    => $config,
-        subscribe  => Package['diamond'],
-        notify     => Service['diamond'],
+    $collector = regsubst($name, '(Collector)?$', 'Collector')
+
+    file { "/etc/diamond/collectors/${collector}.conf":
+        content => template('diamond/collector.conf.erb'),
+        require => File['/etc/diamond/collectors'],
+        notify  => Service['diamond'],
+    }
+
+    if $source {
+        $directory_ensure = $ensure ? {
+            present => directory,
+            default => absent,
+        }
+
+        file { "/usr/share/diamond/collectors/${name}":
+            ensure => $directory_ensure,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755',
+        }
+
+        file { "/usr/share/diamond/collectors/${name}/${name}.py":
+            ensure => $ensure,
+            before => File["/etc/diamond/collectors/${collector}.conf"],
+            source => $source,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0644',
+        }
     }
 }
--- modules/diamond/manifests/init.pp
+++ modules/diamond/manifests/init.pp
@@ -1,14 +1,67 @@
+# == Class: diamond
+#
+# Diamond is a Python network daemon that collects system metrics and
+# publishes them to a metric aggregator like Graphite or StatsD. Diamond
+# ships with a suite of metric collectors for CPU, memory, network, disk,
+# etc. Diamond also features an API for implementing custom collectors
+# that gather metrics from almost any source.
+#
+# === Parameters
+#
+# [*handler*]
+#   Import path and class name of diamond.handler.Handler subclass to
+#   use as the metric sink. Set to 'diamond.handler.dastatsd.StatsHandler'
+#   by default. To have Diamond write to Graphite instead, set this to
+#   'diamond.handler.graphite.GraphiteHandler'.
+#
+#   See <https://github.com/BrightcoveOS/Diamond/wiki/Handlers>
+#   for a full list of available metric handlers.
+#
+# [*settings*]
+#   A hash of configuration options for the desired handler.
+#   See <https://github.com/BrightcoveOS/Diamond/wiki/Handlers>
+#   for a listing of configuration options.
+#
+# === Examples
+#
+# A Graphite configuration for Diamond:
+#
+#  class { '::diamond':
+#    handler  => 'diamond.handler.graphite.GraphiteHandler',
+#    settings => {
+#      host => 'graphite.wikimedia.org',
+#      port => 2003,
+#    },
+#  }
+#
 class diamond(
-    $statsd_host='127.0.0.1',
-    $statsd_port='8125',
-){
+    $handler  = 'diamond.handler.dastatsd.StatsHandler',
+    $settings = {},
+) {
     package { 'python-diamond':
         ensure => present,
     }
 
     file { '/etc/diamond/diamond.conf':
         content => template('diamond/diamond.conf.erb'),
+        require => File['/etc/diamond/collectors', '/etc/diamond/handlers'],
+    }
+
+    file { [ '/etc/diamond/collectors', '/etc/diamond/handlers' ]:
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        recurse => true,
+        purge   => true,
+        force   => true,
         require => Package['python-diamond'],
+    }
+
+    # Truncate the import path, leaving only the class name.
+    $handler_class = regsubst($handler, '.*\.', '')
+    file { "/etc/diamond/handlers/${handler_class}.conf":
+        content => template('diamond/handler.conf.erb'),
     }
 
     service { 'diamond':
@@ -16,16 +69,7 @@ class diamond(
         enable     => true,
         hasrestart => true,
         hasstatus  => true,
+        require    => Package['python-diamond'],
         subscribe  => File['/etc/diamond/diamond.conf'],
-    }
-
-    diamond::collector {
-        'CPUCollector':
-            config => "enabled = True\npercore = False\nnormalize = True",
-    }
-
-    diamond::collector {
-        'NetworkCollector':
-            config => "enabled = True",
     }
 }
