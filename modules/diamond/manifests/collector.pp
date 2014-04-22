54661436c3285c8446bf9e399cbef29ef0025a30
Ori Livneh <ori@wikimedia.org>
2014-04-22 16:50:11 -0700

    miscellaneous improvements for diamond module
    * Remove a lot of awful cruft from the sample configuration file that ships
      with Diamond, leaving only what actually needs to be specified.
    * Log to standard out; it will be captured by Upstart and rotated
      automatically.
    * Manage collector and handler configuration files recursively.
    * Accept Puppet hashes for collector and handler configurations.
    * Add documentation and examples.
    * Remove CPU & network collector resources; these belong in a base role class
      instead.
    
    Change-Id: Iee6b0fa772798f75282e639754e8dd8dc8a06eff

--- modules/diamond/manifests/collector.pp
+++ modules/diamond/manifests/collector.pp
@@ -1,11 +1,52 @@
-define diamond::collector(
-    $ensure = 'present',
-    $config = undef,
-) {
-    file { "/etc/diamond/collectors/${name}.conf":
-        ensure     => $ensure,
-        content    => $config,
-        subscribe  => Package['diamond'],
-        notify     => Service['diamond'],
+# == Define: diamond::collector
+#
+# Diamond Collectors are Python modules that run within the Diamond
+# process, collecting metrics about some subsystem, and feeding these
+# metrics into Diamond for aggregation and reporting.
+#
+# If you are familiar with Ganglia, you can think of Collectors as
+# being Diamond's answer to gmond modules.
+#
+# === Parameters
+#
+# [*ensure*]
+#   Specifies whether or not to configure Diamond to use this collector.
+#   May be 'present' or 'absent'. The default is 'present'.
+#
+# [*name*]
+#   The name of the collector class. The 'Collector' suffix may be
+#   omitted from the name.
+#
+# [*settings*]
+#   A hash of configuration settings for the collector.
+#   The 'enabled' setting is set to true by default.
+#
+# === Examples
+#
+# Configure an Nginx metric collector:
+#
+#  diamond::collector { 'Nginx':
+#    settings => {
+#      req_host => 'gdash.wikimedia.org',
+#      req_path => '/status',
+#    },
+#  }
+#
+# Configure a CPU utilization metric collector:
+#
+#  diamond::collector { 'CPU':
+#    settings => {
+#      percore   => false,
+#      normalize => true,
+#    },
+#  }
+#
+define diamond::collector( $settings ) {
+    $collector = regsubst($name, '(Collector)?$', 'Collector')
+
+    file { "/etc/diamond/collectors/${collector}.conf":
+        content => template('diamond/collector.conf.erb'),
+        require => File['/etc/diamond/collectors'],
+        notify  => Service['diamond'],
     }
 }
