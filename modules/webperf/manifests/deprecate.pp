6a7bb9c7310bd79feffe1cd0476150645bc8673b
Timo Tijhof <krinklemail@gmail.com>
2014-04-23 20:09:00 +0200

    webperf: Add statsd module for mw-js-deprecate
    Change-Id: I938eb55b6064c2744324bde8ddf48237c1036013

---
+++ modules/webperf/manifests/deprecate.pp
@@ -0,0 +1,43 @@
+# == Class: webperf::deprecate
+#
+# Captures mw.log.depreccate data and sends it to StatsD.
+#
+# See https://meta.wikimedia.org/wiki/Schema:DeprecatedUsage.
+#
+# === Parameters
+#
+# [*endpoint*]
+#   URI of EventLogging event publisher to subscribe to.
+#   Example: 'tcp://eventlogging.corp.org:8600'.
+#
+# [*statsd_host*]
+#   Write stats to this StatsD instance. Default: '127.0.0.1'.
+#
+# [*statsd_port*]
+#   Write stats to this StatsD instance. Default: 8125.
+#
+class webperf::deprecate(
+    $endpoint,
+    $statsd_host = '127.0.0.1',
+    $statsd_port = 8125,
+) {
+    include ::webperf
+
+    file { '/srv/webperf/deprecate.py':
+        source => 'puppet:///modules/webperf/deprecate.py',
+        owner  => 'www-data',
+        group  => 'www-data',
+        mode   => '0755',
+        notify => Service['statsd-mw-js-deprecate'],
+    }
+
+    file { '/etc/init/deprecate.conf':
+        content => template('webperf/deprecate.conf.erb'),
+        notify  => Service['statsd-mw-js-deprecate'],
+    }
+
+    service { 'statsd-mw-js-deprecate':
+        ensure   => running,
+        provider => upstart,
+    }
+}
