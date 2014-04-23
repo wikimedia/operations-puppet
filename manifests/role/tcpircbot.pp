3b70ab019a3ffde1aa8bf425558af29009397d52
Matanya Moses <matanya@foss.co.il>
2014-04-23 13:07:34 +0300

    tcpircbot: add a role
    Change-Id: I78379be791a5216e9af0a26474149e4e52127ca5

---
+++ manifests/role/tcpircbot.pp
@@ -0,0 +1,23 @@
+class role::tcpircbot {
+    include ::tcpircbot
+
+    system::role { 'tcpircbot':
+        description => 'tcpircbot server',
+    }
+
+    nrpe::monitor_service { 'tcpircbot':
+        description  => 'tcpircbot_service_running',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:4 -c 1:20 -a tcpircbot',
+    }
+
+    tcpircbot::instance { 'logmsgbot':
+        channels => '#wikimedia-operations',
+        password => $passwords::logmsgbot::logmsgbot_password,
+        cidr     => [
+            '::ffff:10.64.21.123/128',    # vanadium
+            '::ffff:10.64.0.196/128',     # tin
+            '::ffff:208.80.152.165/128',  # fenari
+            '::ffff:127.0.0.1/128',       # loopback
+        ],
+    }
+}
