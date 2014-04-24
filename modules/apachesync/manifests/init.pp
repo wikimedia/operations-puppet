79a03201c222966624680ecb5ab15f0346ab46ff
dzahn <dzahn@wikimedia.org>
2014-04-24 04:53:02 -0700

    WIP - put apache sync scripts into module
    ..just the Apache scripts, not the mw stuff
    ..to move it away from fenari and put on
    something else without touching fenari
    
    Change-Id: Idb4ced99644c1692c93673ceb15d690a01b7e7d7

---
+++ modules/apachesync/manifests/init.pp
@@ -0,0 +1,35 @@
+# scripts for syncing apache changes
+class apachesync {
+
+    $scriptpath = '/usr/local/bin'
+
+    file {
+        "${scriptpath}/dologmsg":
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            source => 'puppet:///modules/apachesync/dologmsg';
+        "${scriptpath}/sync-apache":
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            source => 'puppet:///modules/apachesync/sync-apache';
+        "${scriptpath}/sync-apache-simulated":
+            ensure => link,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            target => "${scriptpath}/sync-apache";
+        "${scriptpath}/apache-graceful-all":
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0554',
+            source => 'puppet:///modules/apachesync/apache-graceful-all';
+        "${scriptpath}/apache-fast-test":
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            source => 'puppet:///modules/apachesync/apache-fast-test';
+    }
+
+}
