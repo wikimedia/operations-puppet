971a8de28e2e1c96a404cbfd4a68c7922e6c82b2
Bryan Davis <bd808@wikimedia.org>
2014-04-23 20:57:11 -0600

    Exec saltutil.sync_all when adding deployment_server grain
    Follow the pattern of the deployment::target class and ensure that
    saltutil.sync_all has been called after setting the deployment_server
    grain.
    
    Bug: 64348
    Change-Id: I9696d45120d06120795f97d0a9183d116680b0a0

--- modules/deployment/manifests/deployment_server.pp
+++ modules/deployment/manifests/deployment_server.pp
@@ -1,3 +1,10 @@
+# = Class: deployment::deployment_server
+#
+# Provision a trebuchet deployment server.
+#
+# == Parameters:
+# - $deployer_groups: Additional unix groups to add to the trebuchet user
+#
 class deployment::deployment_server($deployer_groups=[]) {
     if ! defined(Package['git-core']){
         package { 'git-core':
@@ -13,7 +20,11 @@ class deployment::deployment_server($deployer_groups=[]) {
     exec { 'eventual_consistency_deployment_server_init':
         path    => ['/usr/bin'],
         command => 'salt-call deploy.deployment_server_init',
-        require => Package['salt-minion'];
+        require => [
+            Package['salt-minion'],
+            Salt::Grain['deployment_server'],
+            Salt::Grain['deployment_repo_user'],
+        ];
     }
 
     file { '/etc/gitconfig':
@@ -39,12 +50,19 @@ class deployment::deployment_server($deployer_groups=[]) {
         grain   => 'deployment_server',
         value   => true,
         replace => true,
+        notify  => Exec['deployment_server_sync_all'],
     }
 
     salt::grain { 'deployment_repo_user':
         grain   => 'deployment_repo_user',
         value   => 'trebuchet',
         replace => true,
+    }
+
+    exec { 'deployment_server_sync_all':
+        refreshonly => true,
+        path        => ['/usr/bin'],
+        command     => 'salt-call saltutil.sync_all';
     }
 
     if $::realm != 'labs' {
