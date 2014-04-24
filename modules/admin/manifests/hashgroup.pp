0918fd24d5862fc73a69d452e89757d45b55fc85
cpettet <rush@wikimedia.org>
2014-04-24 14:29:35 -0500

    admin module for user/group/permissions cleanup
    Users/groups/perms are defined are meant to be defined in yaml.
    
    Applied like:
    
    yaml:
    
      groups:
        test1:
          gid: 1
          members: [foo]
        test2:
          gid: 2
          members: []
    
      users:
        foo:
          uid: 1
          ...
    
    .pp
      node /foo/ {
        #groups create their users (can overlap) and permissions
        #as well as system groups and handling user membership
        class { 'admin': groups => ['test', 'test2'], }
      }
    
    More info see: admin/README
    
    Change-Id: I6982bfced50a22faac37246dff4d52ff163a34ed

---
+++ modules/admin/manifests/hashgroup.pp
@@ -0,0 +1,28 @@
+# A defined type for group creation / user realization from hash
+#
+# === Parameters
+#
+# [*name*]
+#  Hash group name
+#
+# [*phash*]
+#  Hash that contains valid group data
+
+define admin::hashgroup(
+    $phash={},
+)
+    {
+
+    #explicit error as otherwise it goes forward later
+    #complaining of 'invalid hash' which is hard to track down
+    if !has_key($phash['groups'], $name) {
+        fail("${name} is not a valid group name")
+    }
+
+    $gdata = $phash['groups'][$name]
+    admin::group { $name:
+        ensure => $gdata['ensure'],
+        gid    => $gdata['gid'],
+        privs  => $gdata['privs'],
+    }
+}
