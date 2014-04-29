33209430f67e16527f4cf7d078a392d00fb0118e
dzahn <dzahn@wikimedia.org>
2014-04-29 08:22:30 -0700

    unquote Booleans in RT role class
    will this still be fine with exim roled?
    
    just 71 to go until we can merge Id8f7a5ee98
    
    Change-Id: If252dd8c4628ff7fe0070c80bc549c483921be39

--- manifests/role/rt.pp
+++ manifests/role/rt.pp
@@ -16,16 +16,16 @@ class role::rt {
 
 	class { 'exim::roled':
 		local_domains          => [ '+system_domains', '+rt_domains' ],
-		enable_mail_relay      => 'false',
-		enable_external_mail   => 'true',
+		enable_mail_relay      => false,
+		enable_external_mail   => true,
 		smart_route_list       => [
 			'mchenry.wikimedia.org',
 			'lists.wikimedia.org',
 		],
-		enable_mailman         => 'false',
-		rt_relay               => 'true',
-		enable_mail_submission => 'false',
-		enable_spamassassin    => 'false',
+		enable_mailman         => false,
+		rt_relay               => true,
+		enable_mail_submission => false,
+		enable_spamassassin    => false,
 	}
 }
 
