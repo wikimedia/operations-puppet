04d274f6cb628289197ad4a1ada71c445c60c25f
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-04-29 13:52:58 +0300

    Introduce role::puppetmaster classes
    Move the puppetmaster definitions from site.pp into two role classes.
    This should be a noop. The motivation behind the move is to have the
    puppetmaster stuff more manageable and having the usual role classes to
    avoid configuration directly in site.pp
    
    Change-Id: I87554a63dbeeade93d7c760b9b9b851841922fd9

---
+++ manifests/role/puppetmaster.pp
@@ -0,0 +1,51 @@
+# vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab textwidth=80 smarttab
+
+class role::puppetmaster::config {
+        $allow_from = [
+            '*.wikimedia.org',
+            '*.pmtpa.wmnet',
+            '*.eqiad.wmnet',
+            '*.ulsfo.wmnet',
+            '*.esams.wmnet',
+        ]
+}
+
+class role::puppetmaster::frontend {
+    include role::puppetmaster::config
+    include passwords::puppet::database
+
+    class { '::puppetmaster':
+        allow_from  => $role::puppetmaster::config::allow_from,
+        server_type => 'frontend',
+        workers     => ['palladium.eqiad.wmnet',
+                        'strontium.eqiad.wmnet'
+        ],
+        config      => {
+            'thin_storeconfigs' => true,
+            'dbadapter'         => 'mysql',
+            'dbuser'            => 'puppet',
+            'dbpassword'        => $passwords::puppet::database::puppet_production_db_pass,
+            'dbserver'          => 'db1001.eqiad.wmnet',
+        }
+    }
+}
+
+class role::puppetmaster::backend {
+    include role::puppetmaster::config
+    include passwords::puppet::database
+
+    class { '::puppetmaster':
+        allow_from  => $role::puppetmaster::config::allow_from,
+        server_type => 'backend',
+        config      => {
+            'thin_storeconfigs' => true,
+            'ca'                => 'false',
+            'ca_server'         => 'palladium.eqiad.wmnet',
+            'dbadapter'         => 'mysql',
+            'dbuser'            => 'puppet',
+            'dbpassword'        => $passwords::puppet::database::puppet_production_db_pass,
+            'dbserver'          => 'db1001.eqiad.wmnet',
+            'dbconnections'     => '256',
+        }
+    }
+}
