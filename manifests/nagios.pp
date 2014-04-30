ffe97977581bd1828746457074312584bc5a6944
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-04-30 11:23:11 +0200

    Add another check using graphite, small fixes.
    This commit includes a few small things:
    - Corrected the output of check_graphite (now we show two digits instead
      of an arbitrary number)
    - Now check_graphite_anomaly is able to consider either only the upper
      bound or the lower bound of metrics (as this makes sense in most cases)
    - Shortened the window of the requstats.5xx check to the last 15 minutes
    - Added a first (experimental) anomaly detection on the error ratio.
    - Made the 5xx check actually page us as it's usually fired off when
      something is wrong.
    
    Change-Id: Idea08c16164e0280f861b920155f139395eacf8d
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -474,6 +474,7 @@ define monitor_graphite_threshold(
 #       metric               => 'reqstats.5xx',
 #       warning              => 5,
 #       critical             => 10,
+#       over                 => true
 #   }
 #
 # == Parameters
@@ -486,6 +487,8 @@ define monitor_graphite_threshold(
 # $graphite_url         - URL of the graphite server.
 # $timeout              - Timeout for the http query to
 #                         graphite. Defaults to 10 seconds
+# over                  - check only for values above the limit
+# under                 - check only for values below the limit
 # $host
 # $retries
 # $group
@@ -504,6 +507,8 @@ define monitor_graphite_anomaly(
     $check_window          = 100,
     $graphite_url          = 'http://graphite.wikimedia.org',
     $timeout               = 10,
+    $over                  = false,
+    $under                 = false,
     $host                  = $::hostname,
     $retries               = 3,
     $group                 = $nagios_group,
@@ -517,6 +522,15 @@ define monitor_graphite_anomaly(
 )
 {
 
+    if $over == true {
+        $modifier = '--over'
+    }
+    elsif $under == true {
+        $modifier = '--under'
+    }
+    else {
+        $modifier = ''
+    }
     # checkcommands.cfg's check_graphite_anomaly command has
     # many positional arguments that
     # are passed to the check_graphite script:
@@ -526,10 +540,11 @@ define monitor_graphite_anomaly(
     #   $ARG4$  -W warning threshold
     #   $ARG5$  -C critical threshold
     #   $ARG6$  --check_window sampling size
+    #   $ARG7$  --over or --under
     monitor_service { $title:
         ensure                => $ensure,
         description           => $description,
-        check_command         => "check_graphite_anomaly!${graphite_url}!${timeout}!${metric}!${warning}!${critical}!${check_window}",
+        check_command         => "check_graphite_anomaly!${graphite_url}!${timeout}!${metric}!${warning}!${critical}!${check_window}!modifier",
         retries               => $retries,
         group                 => $group,
         critical              => $nagios_critical,
