325d650a7b4fcefbf43410791060f0efbcb2f561
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-04-30 11:08:03 +0200

    Move cluster definition to the node level.
    We happen to use the $cluster variable for a few reasons, mainly for
    ganglia but also for other things. I will remove all declarations below
    node-level as they don't work well with puppet 3; for now I just added
    it at node level for testing purposes.
    
    This should not interfere with any functionality however.
    
    Change-Id: I5bd8cea283e1f196378c8ad956b69b1d3be34065
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -1012,7 +1012,7 @@ class role::cache {
         }
 
         $storage_size_main = $::realm ? { 'labs' => 5, default => 300 }
-        
+
         if $cluster_tier == 1 {
             $director_retries = 2
         } else {
