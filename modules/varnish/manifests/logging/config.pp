b8ea5a845d673a165690dd159400286411247829
Andrew Otto <acotto@gmail.com>
2014-05-02 12:06:28 -0400

    Removing modules/varnish in prep for adding it as a git submodule
    Change-Id: I3a331b40b21a506ed5e270a738ce9342084e8d6b

--- modules/varnish/manifests/logging/config.pp
+++
@@ -1,8 +0,0 @@
-class varnish::logging::config {
-    file { '/etc/default/varnishncsa':
-        source => "puppet:///modules/${module_name}/varnishncsa.default",
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-    }
-}
--- modules/varnish/manifests/packages.pp
+++
@@ -1,9 +0,0 @@
-class varnish::packages($version='installed') {
-    package { [
-        'varnish',
-        'varnish-dbg',
-        'libvarnishapi1',
-        ]:
-        ensure => $version
-    }
-}
