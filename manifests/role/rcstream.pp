4492bda89eb0c63931d4778b1124229793d1f56a
Ori Livneh <ori@wikimedia.org>
2014-05-02 03:21:36 -0700

    Add 'rcstream' module for broadcasting recent changes over WebSockets
    This change introduces an 'rcstream' module, which provisions a small and
    self-contained software stack for broadcasting recent changes using WebSockets.
    It is designed to work with the RedisPubSubFeedEngine class in MediaWiki.
    
    The setup consists of the following components:
    
    * A single-file Python server, powered by gevent and socket.io.
      Each instance of such server can serve multiple clients concurrently.
    
    * An upstart task (rcstream/init) that spawns multiple instances of this server.
    
    * Nginx as load-balancer / reverse proxy. Nginx 1.4+ (and therefore, Trusty) is
      required, as earlier versions are not capable of proxying WebSocket
      connections.
    
    Why WebSockets? Admittedly, it is not the world's prettiest protocol. But it is
    very widely supported, and it is what users want.
    
    Please see the included README file and the in-line documentation for details
    about the setup.
    
    RFCs fulfilled / obsoleted by this change:
    
    * <https://www.mediawiki.org/wiki/Requests_for_comment/Publishing_the_RecentChanges_feed>
    
    Bug: 14045
    Bug: 17450
    Change-Id: Id819246a947dfe1e32d9fd9c13570858cdaaa39d

---
+++ manifests/role/rcstream.pp
@@ -0,0 +1,31 @@
+# == Class: role::rcstream
+#
+# Provisions a recent changes -> redis -> socket.io -> Nginx setup
+# that streams recent changes from MediaWiki to users via socket.io,
+# a convenience layer over the WebSockets protocol.
+#
+class role::rcstream {
+
+    if versioncmp($::lsbdistrelease, '14.04') < 0 {
+        fail('requires 14.04+')
+    }
+
+    class { '::redis':
+        maxmemory         => '100mb',
+        dir               => '/var/run/redis',
+        persist           => false,
+        redis_replication => false,
+    }
+
+    $backends = [ 10080, 10081, 10082 ]
+
+    class { '::rcstream':
+        redis        => 'redis://127.0.0.1:6379',
+        bind_address => '127.0.0.1',
+        ports        => $backends,
+    }
+
+    class { '::rcstream::proxy':
+        backends => $backends,
+    }
+}
