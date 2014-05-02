71bcc03dea1be23fbf149ae79b3bdec4feacaf1f
Ori Livneh <ori@wikimedia.org>
2014-05-02 03:21:36 -0700

    Add 'rcstream' module for broadcasting recent changes over WebSockets
    This change introduces an 'rcstream' module, which provisions a small and
    self-contained software stack for broadcasting recent changes using WebSockets.
    It is designed to work with the RedisPubSubFeedEngine class in MediaWiki.
    
    The setup consists of the following components:
    
    * A single-file Python server, powered by gevent and socket.io.
      Each instance of such server can serve multiple clients concurrently.
    
    * An upstart task (rcstream/init) that spawns multiple instances of this server.
    
    * Nginx as load-balancer / reverse proxy. Nginx 1.4+ (and therefore, Trusty) is
      required, as earlier versions are not capable of proxying WebSocket
      connections.
    
    Why WebSockets? Admittedly, it is not the world's prettiest protocol. But it is
    very widely supported, and it is what users want.
    
    Please see the included README file and the in-line documentation for details
    about the setup.
    
    Change-Id: Id819246a947dfe1e32d9fd9c13570858cdaaa39d

---
+++ manifests/role/rcstream.pp
@@ -0,0 +1,30 @@
+# == Class: role::rcstream
+#
+# Provisions a recent changes -> redis -> socket.io -> Nginx setup
+# that streams recent changes from MediaWiki to users via socket.io,
+# a convenience layer over the WebSockets protocol.
+#
+class role::rcstream {
+
+    if versioncmp($::lsbdistrelease, '14.04') < 0 {
+        fail('requires 14.04+')
+    }
+
+    class { '::redis':
+        maxmemory         => '1Gb',
+        dir               => '/var/run/redis',
+        persist           => false,
+        redis_replication => false,
+    }
+
+    $backends = [ 10080, 10081, 10082 ]
+
+    class { '::rcstream':
+        iface => '127.0.0.1',
+        ports => $backends,
+    }
+
+    class { '::rcstream::proxy':
+        backends => $backends,
+    }
+}
