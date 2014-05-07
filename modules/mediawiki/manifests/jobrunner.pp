b7bfdfd48bd81b9a0fb7ee5311f7a150b73bf4cf
Ori Livneh <ori@wikimedia.org>
2014-05-06 21:59:33 -0700

    tidy mediawiki::jobrunner
    Change-Id: I2c7b753a54bdb4b461179eed2ca62045b1a34e94

--- modules/mediawiki/manifests/jobrunner.pp
+++ modules/mediawiki/manifests/jobrunner.pp
@@ -1,58 +1,43 @@
 class mediawiki::jobrunner (
-	$run_jobs_enabled,
-	$user = "apache",
-	$type = "",
-	$nice = 20,
-	$script = "/usr/local/bin/jobs-loop.sh",
-	$pid_file = "/var/run/mw-jobs.pid",
-	$timeout = 300,
-	$extra_args = "",
-	$dprioprocs = 5,
-	$iprioprocs = 5,
-	$procs_per_iobound_type = 1
+    $run_jobs_enabled,
+    $user                   = 'apache',
+    $type                   = '',
+    $nice                   = 20,
+    $script                 = '/usr/local/bin/jobs-loop.sh',
+    $pid_file               = '/var/run/mw-jobs.pid',
+    $timeout                = 300,
+    $extra_args             = '',
+    $dprioprocs             = 5,
+    $iprioprocs             = 5,
+    $procs_per_iobound_type = 1
 ) {
-	file {
-		"/etc/init.d/mw-job-runner":
-			owner => root,
-			group => root,
-			mode => 0755,
-			source => "puppet:///modules/mediawiki/jobrunner/mw-job-runner.init";
-	}
-	file {
-		"/etc/default/mw-job-runner":
-			content => template("mediawiki/jobrunner/mw-job-runner.default.erb");
-	}
-	file {
-		"/usr/local/bin/jobs-loop.sh":
-			owner => root,
-			group => root,
-			mode => 0755,
-			content => template("mediawiki/jobrunner/jobs-loop.sh.erb");
-	}
-	if $run_jobs_enabled == true {
-		service {
-			"mw-job-runner":
-				require => [
-					File[
-						"/etc/default/mw-job-runner",
-						"/etc/init.d/mw-job-runner",
-						"/usr/local/bin/jobs-loop.sh"
-					],
-				],
-				subscribe => File[
-					"/etc/default/mw-job-runner",
-					"/etc/init.d/mw-job-runner",
-					"/usr/local/bin/jobs-loop.sh"
-				],
-				hasstatus => false,
-				pattern => $script,
-				ensure => running;
-		}
-	} else {
-		service { "mw-job-runner":
-			hasstatus => false,
-			pattern => $script,
-			ensure => stopped;
-		}
-	}
+    file { '/etc/init.d/mw-job-runner':
+        owner  => root,
+        group  => root,
+        mode   => 0755,
+        source => 'puppet:///modules/mediawiki/jobrunner/mw-job-runner.init',
+        notify => Service['mw-job-runner'],
+    }
+
+    file { '/etc/default/mw-job-runner':
+        content => template('mediawiki/jobrunner/mw-job-runner.default.erb'),
+        notify  => Service['mw-job-runner'],
+    }
+
+    file { '/usr/local/bin/jobs-loop.sh':
+        owner   => root,
+        group   => root,
+        mode    => 0755,
+        content => template('mediawiki/jobrunner/jobs-loop.sh.erb'),
+        notify  => Service['mw-job-runner'],
+    }
+
+    service { 'mw-job-runner':
+        hasstatus => false,
+        pattern   => $script,
+        ensure    => $run_jobs_enabled ? {
+            true    => running,
+            default => stopped,
+        },
+    }
 }
