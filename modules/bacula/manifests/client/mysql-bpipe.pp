7271788aa0e8c265d70580054d418ab7bf8ba229
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-05-08 19:47:09 +0300

    bacula: allow mysqldumps to be kept locally
    Should a local_dump_dir parameter be defined and mysqldump version is
    using keep a dump locally in the machine in the directory defined by the
    parameter. While at it create rspec rules for it
    
    Change-Id: Ib13604d83dd2a47467da41e6d6a01f9346b211f9

--- modules/bacula/manifests/client/mysql-bpipe.pp
+++ modules/bacula/manifests/client/mysql-bpipe.pp
@@ -24,6 +24,9 @@
 #   $password_file
 #       A simple my.cnf style file containing the credentials for a valid
 #       account to the database that can be used by mysql and mysqldump
+#   $local_dump_dir
+#       A simple my.cnf style file containing the credentials for a valid
+#       account to the database that can be used by mysql and mysqldump
 #
 # Actions:
 #       Will create a bpipe plugin for bacula
@@ -43,10 +46,11 @@ define bacula::client::mysql-bpipe(
                 $per_database,
                 $xtrabackup,
                 $xbstream_dir='/var/tmp/xbstream',
-                $pigz_level=fast,
+                $pigz_level='fast',
                 $is_slave=false,
                 $mysqldump_innodb_only=false,
                 $password_file=undef,
+                $local_dump_dir=undef,
                 ) {
     file { "/etc/bacula/scripts/bpipe-mysql-${name}":
         ensure  => present,
