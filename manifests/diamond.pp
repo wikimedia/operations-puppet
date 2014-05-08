b5494df14d7e3ebcf2fa0b878165157e55bb33e4
Ori Livneh <ori@wikimedia.org>
2014-05-08 20:07:54 +0200

    Move diamond::generic to manifests/ and lint
    * Add doc header.
    * Move to manifests/
    * Use proper Puppet types for setting vars.
    * Use service alias for statsd rather than hard-code tungsten's ip
    
    Change-Id: I6bb30a09a22eb19f658ab18da7a5a1d906e65a12

---
+++ manifests/diamond.pp
@@ -0,0 +1,36 @@
+# == Class: diamond::generic
+#
+# Provisions Diamond and generic metric modules for monitoring CPU load
+# and network utilization.
+#
+class diamond::generic {
+
+    #these are notes just for initial rollout and testing:
+    #tungsten: 10.64.0.18
+    #(labs) athens graphite server: 10.68.17.73
+    #Once https://gerrit.wikimedia.org/r/#/c/131449/ merges
+    #start batching in groups of 10 to start, for now current statsd can't
+    #accept multiple metrics
+
+    class { 'diamond':
+        settings => {
+            enabled => true,
+            host    => 'statsd.eqiad.wmnet',
+            port    => 8125,
+        },
+    }
+
+    diamond::collector { 'CPU':
+        settings => {
+            enabled   => true,
+            percore   => false,
+            normalize => true,
+        },
+    }
+
+    diamond::collector { 'Network':
+        settings => {
+            enabled => true,
+        },
+    }
+}
