961c2c0d9792b144b4b4ecf1cdbf86bdd1dff8bd
cpettet <rush@wikimedia.org>
2014-05-09 14:46:20 -0500

    ircd-ratbox and udpmxircecho puppetized
    * udpmxircecho is handled by upstart
    * ircd-ratbox is a binary and handled with puppet
    
    Change-Id: I248dac3cacb82471cf672124b9061ac91cdef64f

---
+++ manifests/role/mw-rc-irc.pp
@@ -0,0 +1,13 @@
+class role::mw-rc-irc {
+
+    system::role { 'role::mw-rc-irc': description => 'MW Changes IRC Broadcast Server' }
+
+    include passwords::udpmxircecho
+    $udpmxircecho_pass = $passwords::udpmxircecho::udpmxircecho_pass
+
+    class { 'mw-rc-irc::irc-relay':
+        ircpassword => $udpmxircecho_pass,
+    }
+
+    include mw-rc-irc::ircserver
+}
---
+++ modules/mw-rc-irc/manifests/irc-echo.pp
@@ -0,0 +1,27 @@
+# A basic relay client that accept changes via udp and echo's them to an irc server
+
+class mw-rc-irc::irc-echo(
+    $ircpassword,
+) {
+
+    package { 'python-irclib': ensure => latest; }
+
+    file { '/usr/local/bin/udpmxircecho.py':
+        content => template('mw-rc-irc/udpmxircecho.py.erb'),
+        mode    => '0555',
+        owner   => 'irc',
+        group   => 'irc';
+    }
+
+    file { '/etc/init/ircecho.conf':
+        source  => 'puppet:///modules/mw-rc-irc/upstart/ircecho.conf',
+        require => File['/usr/local/bin/udpmxircecho.py'],
+    }
+
+    # Ensure that the service is running.
+    service { 'ircecho':
+        ensure => running,
+        provider => 'upstart',
+        require => File['/etc/init/ircecho.conf'],
+    }
+}
