52fa7f82cdc82705fbfe905e16446c6dc5171696
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-05-13 12:34:43 +0200

    Make class call work.
    Please note that classes with dash in their name should *not* work in
    puppet. The fact that they work in our environment is due to a
    regression introduced in puppet 2.7.0 and fixed in puppet 2.7.20, see
    http://projects.puppetlabs.com/issues/10146 for details.
    
    Funnily enough, this breaks only if you try to reference variables of
    those classes; you can still happily do "include some-class" and it will
    work (!)
    
    Change-Id: I9872df05d209c33d18306712e23d46136ad33a38
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- manifests/role/swift.pp
+++ manifests/role/swift.pp
@@ -8,7 +8,7 @@ class role::swift {
 
 	class eqiad-prod inherits role::swift::base {
 		system::role { "role::swift::eqiad-prod": description => "Swift eqiad production cluster" }
-		include passwords::swift::eqiad-prod
+		include passwords::swift::eqiad_prod
 		class { "::swift::base": hash_path_suffix => "4f93c548a5903a13", cluster_name => "eqiad-prod" }
 		class ganglia_reporter inherits role::swift::eqiad-prod {
 			# one host per cluster should report global stats
@@ -19,8 +19,8 @@ class role::swift {
 				mode   => '0555',
 				source => "puppet:///files/swift/swift-ganglia-report-global-stats",
 			}
-			# config file to hold the password
-			$password = $passwords::swift::eqiad-prod::rewrite_password
+			# config file to hold the  password
+			$password = $passwords::swift::eqiad_prod::rewrite_password
 			file { "/etc/swift-ganglia-report-global-stats.conf":
 				owner   => 'root',
 				group   => 'root',
@@ -40,9 +40,9 @@ class role::swift {
 				num_workers => $::processorcount,
 				memcached_servers => [ "ms-fe1001.eqiad.wmnet:11211", "ms-fe1002.eqiad.wmnet:11211", "ms-fe1003.eqiad.wmnet:11211", "ms-fe1004.eqiad.wmnet:11211" ],
 				auth_backend => 'tempauth',
-				super_admin_key => $passwords::swift::eqiad-prod::super_admin_key,
+				super_admin_key => $passwords::swift::eqiad_prod::super_admin_key,
 				rewrite_account => 'AUTH_mw',
-				rewrite_password => $passwords::swift::eqiad-prod::rewrite_password,
+				rewrite_password => $passwords::swift::eqiad_prod::rewrite_password,
 				rewrite_thumb_server => "rendering.svc.eqiad.wmnet",
 				shard_container_list => "wikipedia-commons-local-thumb,wikipedia-de-local-thumb,wikipedia-en-local-thumb,wikipedia-fi-local-thumb,wikipedia-fr-local-thumb,wikipedia-he-local-thumb,wikipedia-hu-local-thumb,wikipedia-id-local-thumb,wikipedia-it-local-thumb,wikipedia-ja-local-thumb,wikipedia-ro-local-thumb,wikipedia-ru-local-thumb,wikipedia-th-local-thumb,wikipedia-tr-local-thumb,wikipedia-uk-local-thumb,wikipedia-zh-local-thumb,wikipedia-commons-local-public,wikipedia-de-local-public,wikipedia-en-local-public,wikipedia-fi-local-public,wikipedia-fr-local-public,wikipedia-he-local-public,wikipedia-hu-local-public,wikipedia-id-local-public,wikipedia-it-local-public,wikipedia-ja-local-public,wikipedia-ro-local-public,wikipedia-ru-local-public,wikipedia-th-local-public,wikipedia-tr-local-public,wikipedia-uk-local-public,wikipedia-zh-local-public,wikipedia-commons-local-temp,wikipedia-de-local-temp,wikipedia-en-local-temp,wikipedia-fi-local-temp,wikipedia-fr-local-temp,wikipedia-he-local-temp,wikipedia-hu-local-temp,wikipedia-id-local-temp,wikipedia-it-local-temp,wikipedia-ja-local-temp,wikipedia-ro-local-temp,wikipedia-ru-local-temp,wikipedia-th-local-temp,wikipedia-tr-local-temp,wikipedia-uk-local-temp,wikipedia-zh-local-temp,wikipedia-commons-local-transcoded,wikipedia-de-local-transcoded,wikipedia-en-local-transcoded,wikipedia-fi-local-transcoded,wikipedia-fr-local-transcoded,wikipedia-he-local-transcoded,wikipedia-hu-local-transcoded,wikipedia-id-local-transcoded,wikipedia-it-local-transcoded,wikipedia-ja-local-transcoded,wikipedia-ro-local-transcoded,wikipedia-ru-local-transcoded,wikipedia-th-local-transcoded,wikipedia-tr-local-transcoded,wikipedia-uk-local-transcoded,wikipedia-zh-local-transcoded,global-data-math-render",
 				backend_url_format => "sitelang"
@@ -70,7 +70,7 @@ class role::swift {
 				source => "puppet:///files/swift/swift-ganglia-report-global-stats",
 			}
 			# config file to hold the password
-			$password = $passwords::swift::esams-prod::rewrite_password
+			$password = $passwords::swift::esams_prod::rewrite_password
 			file { "/etc/swift-ganglia-report-global-stats.conf":
 				owner   => 'root',
 				group   => 'root',
@@ -90,9 +90,9 @@ class role::swift {
 				num_workers => $::processorcount,
 				memcached_servers => [ "ms-fe3001.esams.wmnet:11211", "ms-fe3002.esams.wmnet:11211" ],
 				auth_backend => 'tempauth',
-				super_admin_key => $passwords::swift::esams-prod::super_admin_key,
+				super_admin_key => $passwords::swift::esams_prod::super_admin_key,
 				rewrite_account => 'AUTH_mw',
-				rewrite_password => $passwords::swift::esams-prod::rewrite_password,
+				rewrite_password => $passwords::swift::esams_prod::rewrite_password,
 				rewrite_thumb_server => "upload.wikimedia.org",
 				shard_container_list => "",
 				backend_url_format => "asis"
