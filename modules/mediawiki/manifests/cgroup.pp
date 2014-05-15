e21fea75f67bcea02c6da2ff691760b10af6d055
Ori Livneh <ori@wikimedia.org>
2014-05-15 13:59:08 -0700

    Lint mediawiki::cgroup
    Change-Id: I65db4b51c4dfa3b44242dfe36653a4e84bfb9d6e

--- modules/mediawiki/manifests/cgroup.pp
+++ modules/mediawiki/manifests/cgroup.pp
@@ -1,26 +1,28 @@
 class mediawiki::cgroup {
-	package { 'cgroup-bin':
-		ensure => present,
-	}
+    package { 'cgroup-bin':
+        ensure => present,
+    }
 
-	file { '/etc/init/mw-cgroup.conf':
-		owner   => root,
-		group   => root,
-		mode    => '0644',
-		source  => 'puppet:///modules/mediawiki/cgroup/mw-cgroup.conf',
-		require => Package['cgroup-bin'],
-	}
+    file { '/etc/init/mw-cgroup.conf':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        source  => 'puppet:///modules/mediawiki/cgroup/mw-cgroup.conf',
+        require => Package['cgroup-bin'],
+    }
 
-	service { 'mw-cgroup':
-		ensure   => running,
-		provider => upstart,
-		require  => File['/etc/init/mw-cgroup.conf'],
-	}
+    service { 'mw-cgroup':
+        ensure   => running,
+        provider => upstart,
+        require  => File['/etc/init/mw-cgroup.conf'],
+    }
 
-	file { '/usr/local/bin/cgroup-mediawiki-clean':
-		owner  => root,
-		group  => root,
-		mode   => '0755',
-		source => 'puppet:///modules/mediawiki/cgroup/cgroup-mediawiki-clean',
-	}
+    file { '/usr/local/bin/cgroup-mediawiki-clean':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        source => 'puppet:///modules/mediawiki/cgroup/cgroup-mediawiki-clean',
+    }
 }
