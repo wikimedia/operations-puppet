70346aecd610a12846a821fb52bf1e370b52baac
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-05-16 16:02:37 +0300

    Revert "Revert "Introduce role::mariadb::backup""
    Reverting the revert. The passwords::mysql::dump class has been added in
    the private repo
    
    This reverts commit f1d22656f098d617344027f557168eb5aaf90d3c.
    
    Change-Id: I1bc1b33926289e012db82f30ee889e1305914a18

--- manifests/role/mariadb.pp
+++ manifests/role/mariadb.pp
@@ -140,3 +140,43 @@ class role::mariadb::analytics {
 
     mariadb::monitor_replication { ['s1', 'm2' ]: }
 }
+
+class role::mariadb::backup::config {
+    if $mariadb_backups_folder {
+        $folder = $mariadb_backups_folder
+    } else {
+        $folder = '/srv/backups'
+    }
+}
+
+class role::mariadb::backup {
+    include backup::host
+    include passwords::mysql::dump
+
+    include role::mariadb::backup::config
+    $backups_folder = $role::mariadb::backup::config::folder
+
+    file { $backups_folder:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0600', # implicitly 0700 for dirs
+    }
+
+    file { '/etc/mysql/conf.d/dumps.cnf':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0400',
+        content => "[client]\nuser=${passwords::mysql::dump::user}\npassword=${passwords::mysql::dump::pass}\n",
+    }
+
+    backup::mysqlset {'dbstore':
+        xtrabackup     => false,
+        per_db         => true,
+        innodb_only    => true,
+        local_dump_dir => $backups_folder,
+        password_file  => '/etc/mysql/conf.d/dumps.cnf',
+        method         => 'predump',
+    }
+}
