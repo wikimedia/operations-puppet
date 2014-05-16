644b0a4a56606d6c2f2069cb5768af64c9f53385
Sean Pringle <springle@wikimedia.org>
2014-05-16 19:54:06 +1000

    reassign db1056 to S4 commonswiki
    Change-Id: I89c54cbf030db1be49b22067a3e6ea5477645ba3

--- manifests/site.pp
+++ manifests/site.pp
@@ -647,7 +647,7 @@ node /^db(60|7[5-7])\.pmtpa\.wmnet/{
 }
 
 # eqiad dbs
-node /^db10(50|51|52|55|56|61|62|65|66|70|71)\.eqiad\.wmnet/ {
+node /^db10(50|51|52|55|61|62|65|66|70|71)\.eqiad\.wmnet/ {
     $cluster = 'mysql'
     class { 'role::coredb::s1':
         innodb_file_per_table => true,
@@ -673,7 +673,7 @@ node /^db10(03|19|35|38)\.eqiad\.wmnet/ {
     }
 }
 
-node /^db10(04|11|40|42|49|59|64)\.eqiad\.wmnet/ {
+node /^db10(04|11|40|42|49|56|59|64)\.eqiad\.wmnet/ {
     $cluster = 'mysql'
     class { 'role::coredb::s4':
         innodb_file_per_table => true,
