58a10afbbc7cd423ec98d46c62298583adccc699
Ori Livneh <ori@wikimedia.org>
2014-05-16 13:49:41 -0700

    Remove cleanupipc cronjob
    This cron job was part of the initial commit of the public Puppet repo, and no
    one living remembers exactly what purpose it is supposed to serve. It is
    clearing semaphore arrays owned by the Apache process. The best candidate for
    what this is meant to clear are APC semaphores.
    
    I ran this multiple times yesterday and it netted no results:
    
      salt 'mw*' cmd.run 'ipcs -s | grep apache'
    
    So what I'd like to do is merge this, and then keep watching the output of that
    command periodically.
    
    Change-Id: Ie1bfa8f187092214fdeea64f2297db400c76431c

--- modules/applicationserver/manifests/cron.pp
+++ modules/applicationserver/manifests/cron.pp
@@ -1,11 +1,5 @@
 # application server required cron jobs
 class applicationserver::cron {
-    cron { 'cleanupipc':
-        ensure  => present,
-        command => 'ipcs -s | grep apache | cut -f 2 -d \\  | xargs -rn 1 ipcrm -s',
-        user    => root,
-        minute  => 26,
-    }
     cron { 'cleantmpphp':
         ensure  => present,
         command => 'find /tmp -name "php*" -type f -ctime +1 -exec rm -f {} \\;',
