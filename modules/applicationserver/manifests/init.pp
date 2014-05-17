45f15e28fd6dacda5663e21bc14bdbd837cc2393
Ori Livneh <ori@wikimedia.org>
2014-05-17 01:34:45 -0700

    Delete applicationserver::cron
    I determined that the cron job was unnecessary by watching /tmp on the app
    servers over the course of 24H.
    
    Change-Id: I5f20c2ccc8ca7a4953ee220d425d20cada1a9b43

--- modules/applicationserver/manifests/init.pp
+++ modules/applicationserver/manifests/init.pp
@@ -1,6 +1,5 @@
 class applicationserver {
     include applicationserver::config::base
-    include applicationserver::cron
     include applicationserver::service
     include applicationserver::sudo
 }
