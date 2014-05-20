33a1b798d68cfdec00936ef440668e5ce0ef46a7
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-05-20 12:44:47 +0200

    creation of role::puppet_compiler
    Change-Id: Ie8e54555c00478695e81b7b045dd583c6d6a4a18
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ manifests/role/puppet_compiler.pp
@@ -0,0 +1,17 @@
+class role::puppet_compiler {
+
+    system::role { 'role::puppet_compiler': description => 'Puppet compiler jenkins slave'}
+
+    case $::realm {
+        'labs'      : { require role::ci::slave::labs::common }
+        'production': { require role::ci::slave }
+        default     : { fail("Realm ${::realm} NOT supported by this role.") }
+    }
+
+    class {'puppet_compiler':
+        ensure  => 'present',
+        version => '0.2.1',
+        user    => 'jenkins-deploy',
+    }
+
+}
