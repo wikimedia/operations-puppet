daf322ed67cc3173d38aa63d888ba44326b3d32e
Bryan Davis <bd808@wikimedia.org>
2014-05-20 17:24:58 -0600

    labs: Fix beta to work with role::mediawiki
    role::applicationserver was renamed to role::mediawiki and several
    additional cleanup steps were taken in related puppet code. The changed
    code left some issues that need to be cleaned up to work properly in the
    deployment-prep (beta) labs project.
    
    * Don't create mwdeploy user or group in labs. This conflicts with
      LDAP.
    * Don't create l10nupdate user or group in labs. This conflicts with
      LDAP.
    * Apply ::mediawiki::users as a dependency of beta::scap::target. I'm
      not really sure how deployment-bastion use to get
      ::mediawiki::users::sudo applied, but this works to get those sudoer
      rights back on deployment-bastion.
    
    Change-Id: I1e8a28d576cd67348625125238cda47628fa476a

--- modules/beta/manifests/mwdeploy_sudo.pp
+++ modules/beta/manifests/mwdeploy_sudo.pp
@@ -5,7 +5,7 @@
 class beta::mwdeploy_sudo {
     # Grant mwdeploy sudo rights to run anything as itself, apache or
     # l10nupdate. This is a subset of the rights granted to the wmdeploy group
-    # by the mediawiki::users::sudo class
+    # by the ::mediawiki::users class
     sudo_user { 'mwdeploy' :
         privileges => [
             'ALL = (apache,mwdeploy,l10nupdate) NOPASSWD: ALL',
