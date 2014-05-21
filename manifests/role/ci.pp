04d44bf381b35745d2f905c8b89fe97a30dce696
Antoine Musso <hashar@free.fr>
2014-05-21 16:41:11 +0200

    contint: rsync host in labs
    Creates role::ci::publisher::labs, an intermediary rsync hosts to have
    the Jenkins slaves to push their build result.  Can later have the
    production host gallium (which hosts doc.wikimedia.org) to fetch from
    it.
    
    The rsync is wide open. Labs only network communication inside the labs
    project.  I have added a security rule to allow gallium to rsync.
    
    Change-Id: I2044c7a2d3818da5fc9301cf8a36912922821af9

--- manifests/role/ci.pp
+++ manifests/role/ci.pp
@@ -367,6 +367,36 @@ class role::ci::slave::labs {
 
 }
 
+
+# == Class role::ci::publisher::labs
+#
+# Intermediary rsync hosts in labs to let Jenkins slave publish their results
+# safely.  The production machine hosting doc.wikimedia.org can then fetch the
+# doc from there.
+class role::ci::publisher::labs {
+
+    include role::labs::lvm::srv
+    include rsync::server
+
+    file { '/srv/doc':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0775',
+        require => Class['role::labs::lvm::srv'],
+    }
+
+    rsync::server::module { 'doc':
+        path        => '/srv/doc',
+        read_only   => 'no',
+        require => [
+            File['/srv/doc'],
+            Class['role::labs::lvm::srv'],
+        ],
+    }
+
+}
+
 # Website for Continuous integration
 #
 # http://doc.mediawiki.org/
