8e95e7a5e9716393106d8795b6f59ca2db970201
Matt Walker <mwalker@khaosdev.com>
2014-05-22 16:55:46 -0700

    Move OCG default port to 8000
    Because we cannot open a port less than 1024 without being root
    and we really dont want to give NodeJS root, even if I can
    drop permissions later...
    
    Change-Id: I82592e01bcff4556ee3c42fbcfe6ad3a6ec2ca88

--- modules/ocg/manifests/init.pp
+++ modules/ocg/manifests/init.pp
@@ -12,7 +12,7 @@
 
 class ocg (
     $host_name = $::hostname,
-    $service_port = 80,
+    $service_port = 8000,
     $redis_host = 'localhost',
     $redis_port = 6379,
     $redis_password = '',
