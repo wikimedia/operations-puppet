0f4980083ee5ad7d0f254f309568182d4fc8e9b8
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-05-23 17:56:12 +0200

    puppet_compiler: add ferm rule to allow web access
    As role::ci::slave::labs now includes ferm rules, we need to add one
    specifically for the web interface of the compiler.
    
    Change-Id: Ibd233bd1a234924657878b26693671a6b80920de
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- manifests/role/puppet_compiler.pp
+++ manifests/role/puppet_compiler.pp
@@ -3,7 +3,16 @@ class role::puppet_compiler {
     system::role { 'role::puppet_compiler': description => 'Puppet compiler jenkins slave'}
 
     case $::realm {
-        'labs'      : { require role::ci::slave::labs::common }
+        'labs'      : {
+            require role::ci::slave::labs::common
+            ferm::service {'puppet_compiler_web':
+                ensure => 'present',
+                proto  => 'tcp',
+                port   => 'http',
+                prio   => '30',
+                srange => '\$INTERNAL'
+            }
+        }
         'production': { require role::ci::slave }
         default     : { fail("Realm ${::realm} NOT supported by this role.") }
     }
