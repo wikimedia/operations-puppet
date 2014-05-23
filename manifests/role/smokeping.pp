23969bcb9217968d9274d0795f05cb913f91218d
Mark Bergsma <mark@wikimedia.org>
2014-05-23 16:23:41 +0200

    Fix indent
    Change-Id: Id4c503f32ce4c606b449098824ed672050cfae7a

--- manifests/role/smokeping.pp
+++ manifests/role/smokeping.pp
@@ -7,7 +7,7 @@ class role::smokeping {
     include standard,
     misc::smokeping
 
-class {'webserver::php5': ssl => true; }
+    class {'webserver::php5': ssl => true; }
 
     #dependencies for apache
     #apache_module["ssl"] -> Install_certificate["${smokeping_ssl_cert}"] -> Class['webserver::php5']
@@ -35,8 +35,7 @@ class {'webserver::php5': ssl => true; }
     File['/srv/org/'] -> File['/srv/org/wikimedia/'] ->
     File['/srv/org/wikimedia/smokeping/'] -> File["/etc/apache2/sites-available/${smokeping_host}"]
 
-    file {
-        "/etc/apache2/sites-available/${smokeping_host}":
+    file { "/etc/apache2/sites-available/${smokeping_host}":
         ensure  => present,
         mode    => '0444',
         owner   => 'root',
@@ -45,8 +44,7 @@ class {'webserver::php5': ssl => true; }
         content => template('apache/sites/smokeping.wikimedia.org.erb');
     }
 
-    file {
-        ['/srv/org/', '/srv/org/wikimedia/', '/srv/org/wikimedia/smokeping' ]:
+    file { ['/srv/org/', '/srv/org/wikimedia/', '/srv/org/wikimedia/smokeping' ]:
         ensure  => directory,
         recurse => true,
         mode    => '0775',
@@ -54,8 +52,7 @@ class {'webserver::php5': ssl => true; }
         group   => 'root',
     }
 
-    file {
-        '/srv/org/wikimedia/smokeping/index.cgi':
+    file { '/srv/org/wikimedia/smokeping/index.cgi':
         ensure  => 'link',
         target  => '/usr/lib/cgi-bin/smokeping.cgi',
         mode    => '0444',
