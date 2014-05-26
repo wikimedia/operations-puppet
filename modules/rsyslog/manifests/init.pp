5d1803fd85df769eb870e85f1b726d83078fffc4
Ori Livneh <ori@wikimedia.org>
2014-05-26 11:34:52 -0700

    Add rsyslog module and port existing usage
    * Make Puppet manage /etc/rsyslog.d recursively.
    
    * This means that packages that include their own rsyslog.d config will need
      to have that config file declared in Puppet or it will get clobbered on the
      next run.
    
    * This is the same decision we made with respect to sysctl.d.
    
    * Per apt-file, the set of packages with rsyslog.d files are:
    
       arno-iptables-firewall, cloud-init, gforge-shell-postgresql, maas,
       maas-region-controller, mythtv-common, octopussy, postfix, qpidd, sagan,
       sympa, ubuntu-orchestra-client, ubuntu-orchestra-logging-server, and ufw
    
      We don't run any of these in production. (We do have ufw installed because
      it's part of the 'standard' tasksel task for Ubuntu, but we don't use it.)
    
    * Configure a logrotate script for /var/log/local/*.log, which serves as a
      generic destination for a local copy of recent log data that has been emitted
      to a remote host. This is used in the mediawiki module to remove the need for
      apache-syslog-rotate.
    
    * Remove reference to i-000003a9.pmtpa.wmflabs:5544 as a syslog sink for
      non-beta-cluster hosts; the instance no longer exists.
    
    Change-Id: Ibceb48bd8c475696b900e89f38dea686394069d8

---
+++ modules/rsyslog/manifests/init.pp
@@ -0,0 +1,35 @@
+# == Class: rsyslog
+#
+# rsyslogd is a full-featured kernel logging daemon. It is the default
+# syslogd implementation on Debian systems.
+#
+class rsyslog {
+    package { 'rsyslog':
+        ensure => present,
+    }
+
+    file { '/etc/rsyslog.d':
+        ensure  => directory,
+        source  => 'puppet:///modules/rsyslog/rsyslog.d-empty',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        recurse => true,
+        purge   => true,
+        force   => true,
+        require => Package['rsyslog'],
+        notify  => Service['rsyslog'],
+    }
+
+    service { 'rsyslog':
+        ensure    => running,
+        provider  => 'upstart',
+        require   => Package['rsyslog'],
+    }
+
+    rsyslog::conf { 'default':
+        source   => '/usr/share/rsyslog/50-default.conf',
+        priority => 50,
+        require  => Package['rsyslog'],
+    }
+}
