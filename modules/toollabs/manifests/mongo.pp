55b450c607c501c92921da94770877e4f82e0dfc
YuviPanda <yuvipanda@gmail.com>
2014-05-26 18:43:19 +0100

    toollabs: Add MongoDB role
    Change-Id: I832f9017ce51d889ba6e6f20781f78e1da0200a1

---
+++ modules/toollabs/manifests/mongo.pp
@@ -0,0 +1,13 @@
+# Sets up a mongodb master
+class toollabs::mongo::master inherits toollabs {
+    include toollabs::infrastructure
+
+    # We need all the space we can get!
+    include role::labs::lvm::srv
+
+    class { "mongodb":
+        settings => {
+            auth => "true"
+        }
+   }
+}
