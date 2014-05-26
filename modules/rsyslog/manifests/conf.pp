8403c2b4fa0698b9a114785b7133f4ebfd183a58
Ori Livneh <ori@wikimedia.org>
2014-05-26 11:34:52 -0700

    Add rsyslog module and port existing usage
    * Make Puppet manage /etc/rsyslog.d recursively.
    
    * This means that packages that include their own rsyslog.d config will need
      to have that config file declared in Puppet or it will get clobbered on the
      next run.
    
    * This is the same decision we made with respect to sysctl.d.
    
    * Per apt-file, the set of packages with rsyslog.d files are:
    
       arno-iptables-firewall, cloud-init, gforge-shell-postgresql, maas,
       maas-region-controller, mythtv-common, octopussy, postfix, qpidd, sagan,
       sympa, ubuntu-orchestra-client, ubuntu-orchestra-logging-server, and ufw
    
      We don't run any of these in production. (We do have ufw installed because
      it's part of the 'standard' tasksel task for Ubuntu, but we don't use it.)
    
    * Configure a logrotate script for /var/log/local/*.log, which serves as a
      generic destination for a local copy of recent log data that has been emitted
      to a remote host. This is used in the mediawiki module to remove the need for
      apache-syslog-rotate.
    
    * Remove reference to i-000003a9.pmtpa.wmflabs:5544 as a syslog sink for
      non-beta-cluster hosts; the instance no longer exists.
    
    Change-Id: Ibceb48bd8c475696b900e89f38dea686394069d8

---
+++ modules/rsyslog/manifests/conf.pp
@@ -0,0 +1,59 @@
+# == Define: rsyslog::conf
+#
+# Represents an rsyslogd config file. See rsyslog.conf(5).
+#
+# === Parameters
+#
+# [*content*]
+#   The content of the file provided as a string. Either this or
+#   'source' must be specified.
+#
+# [*source*]
+#   The content of the file provided as a puppet:/// file reference.
+#   Either this or 'content' must be specified.
+#
+# [*priority*]
+#   A numeric value in range 0 - 99. Files with a lower priority value
+#   are evaluated first.
+#
+#   If you're not sure, leave this unspecified. The default value of 60
+#   should suit most cases.
+#
+# === Examples
+#
+#  rsyslog::conf { 'hadoop':
+#    content  => template('hadoop/hadoop.conf.erb'),
+#    priority => 90,
+#  }
+#
+define rsyslog::conf(
+    $ensure   = present,
+    $content  = undef,
+    $source   = undef,
+    $priority = 60
+) {
+    include ::rsyslog
+
+    if $priority !~ /^\d?\d$/ {
+        fail("'priority' must be an integer between 0 - 99 (got: ${priority}).")
+    }
+
+    $basename = regsubst($title, '[\W_]', '-', 'G')
+    $filename = sprintf('/etc/rsyslog.d/%02d-%s.conf', $priority, $basename)
+
+    # append a trailing newline if omitted
+    $content_formatted = $content ? {
+        undef   => undef,
+        default => regsubst($content, "\n?$", "\n")
+    }
+
+    file { $filename:
+        ensure  => $ensure,
+        content => $content_formatted,
+        source  => $source,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['rsyslog'],
+    }
+}
