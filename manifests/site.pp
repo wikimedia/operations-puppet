0914412364f3a8d309abe76bd741c62fb268d355
cpettet <rush@wikimedia.org>
2014-05-27 15:09:48 -0500

    admin yaml for labstore100[12]\.eqiad\.wmnet/
    Change-Id: Iad3f2d564eb1449e0082e22d001d28ab6c531e34

--- manifests/site.pp
+++ manifests/site.pp
@@ -1457,6 +1457,7 @@ node /labstore100[12]\.eqiad\.wmnet/ {
 
     $ganglia_aggregator = true
 
+    include admin
     include standard
     include openstack::project-nfs-storage-service
     include rsync::server
