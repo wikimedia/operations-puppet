5d3194309fdaf52be2f3cce12664c55645e78af5
dzahn <dzahn@wikimedia.org>
2014-05-27 13:10:20 -0700

    add new yaml groups for release uploaders
    - add new groups to replace groups used
    in role/releases
    
    - remove all the group stuff from role/releases
    
    Change-Id: I519bf16f1a39550484a76cc04ffba27bc23baa3d

--- manifests/role/releases.pp
+++ manifests/role/releases.pp
@@ -11,18 +11,6 @@ class role::releases {
         docroot      => 'releases',
     }
 
-    # mobile app releases
-    role::releases::access { 'brion': group => 'mobileupld' }
-    role::releases::access { 'yuvipanda': group => 'mobileupld' } # RT 7068
-    role::releases::access { 'dbrant': group => 'mobileupld' } # RT 7399
-
-    # mediawiki releases
-    role::releases::access { 'csteipp': group => 'mwupld' }
-    role::releases::access { 'hashar': group => 'mwupld' } # RT 6861
-    role::releases::access { 'mah': group => 'mwupld' } # RT 6861
-    role::releases::access { 'mglaser': group => 'mwupld' } # RT 6861
-    role::releases::access { 'reedy': group => 'mwupld' } # RT 6861
-
     ferm::service { 'releases_http':
         proto => 'tcp',
         port  => '80',
@@ -35,18 +23,3 @@ class role::releases {
 
 }
 
-class role::releases::groups {
-    group { 'mwupld':
-            ensure => 'present',
-    }
-    group { 'mobileupld':
-            ensure => 'present',
-    }
-}
-
-define role::releases::access ( $user=$title, $group='wikidev' ) {
-    require 'role::releases::groups'
-    require "accounts::${user}"
-    Class['groups::wikidev'] -> Class['role::releases::groups'] -> Class["accounts::${user}"]
-    User<|title == $user|>       { groups +> [ $group ] }
-}
