a308d46d6df3c0733d442850153df29ce8298bd6
cmjohnson <cmjohnson@wikimedia.org>
2014-05-27 09:52:08 -0400

    adding bonded ports to labstore1001
    Change-Id: I9d4de7d24e156b7989201479bce197dec5d41381

--- manifests/ganglia.pp
+++ manifests/ganglia.pp
@@ -294,7 +294,7 @@ class ganglia::collector::config {
                     'API application servers eqiad'  => 'mw1114.eqiad.wmnet mw1115.eqiad.wmnet',
                     'Application servers eqaid'      => 'mw1017.eqiad.wmnet mw1018.eqiad.wmnet',
                     'Jobrunners eqiad'              => 'mw1001.eqiad.wmnet mw1002.eqiad.wmnet',
-                    'Bits application servers eqiad' => 'mw1151.eqiad.wmnet mw1152.eqiad.wmnet',
+                    'Bits application servers eqiad' => 'mw1152.eqiad.wmnet',
                     'MySQL'                          => 'db1050.eqiad.wmnet',
                     'PDF servers'                    => 'pdf2.wikimedia.org pdf3.wikimedia.org',
                     'Miscellaneous'                  => 'tarin.pmtpa.wmnet',
