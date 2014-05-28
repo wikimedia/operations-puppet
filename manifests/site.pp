7efeb32a28d7a5dc8f1a34875987b88c4fd95c21
cpettet <rush@wikimedia.org>
2014-05-28 17:06:50 -0500

    admin yaml analytics1009.eqiad.wmnet
    Change-Id: Ifc297e72c996afe669f8de492670dc129289cef3

--- manifests/site.pp
+++ manifests/site.pp
@@ -143,12 +143,10 @@ node 'analytics1009.eqiad.wmnet' {
     $cluster = 'analytics'
     # analytics1009 is analytics Ganglia aggregator for Row A
     $ganglia_aggregator = true
+
+    class { 'admin': groups => ['stats'] }
+
     include standard
-    include admins::roots
-
-    # include analytics user accounts
-    include role::analytics::users
-
     include role::analytics::kraken
     include role::analytics::hadoop::standby
 }
