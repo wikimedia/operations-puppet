41ca0643201b49d3f5ca30d352628ef12a583be0
dzahn <dzahn@wikimedia.org>
2014-05-28 10:20:06 -0700

    admin yaml for hafnium
    Change-Id: I70d1862876aaa9c74d9c4bc2145b913e44d8a76e

--- manifests/site.pp
+++ manifests/site.pp
@@ -2752,6 +2752,7 @@ node 'vanadium.eqiad.wmnet' {
 # and MediaWiki errors.
 node 'hafnium.wikimedia.org' {
     include standard
+    include admin
     include role::eventlogging::graphite
     include role::webperf
 }
