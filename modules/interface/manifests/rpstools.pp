wikimedia/operations-puppet

commit 1a0605bf52ab4d02ec80c78c50e9dd7dcac57e19

Split interface-rps script File resources to class
The interface-rps script file resource would cause duplicate definition
if more than one interface::rps resource was defined for a host. Fix
this by moving the File resource into its own class and require that
class in the definition. Add a comment about the above in order to avoid
future melding on the class into the define. This fixes a duplicate
definition issue introduced in ab47728

Change-Id: If76dbb328efe45de1ca803f36433b960aa6530aa

modules/interface/manifests/rpstools.pp
@@ -1,0 +1,15 @@
+# Class: interface::rpstools
+#
+# Populate tools used to set RPS for an interface
+#
+# Note that this is explicitly split in a separate class. It is used only in
+# definition interface::rps and this split allows up to avoid duplicate
+# definitions of the file resource.
+define interface::rpstools {
+    file { '/usr/local/sbin/interface-rps':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        source  => 'puppet:///modules/interface/interface-rps.py',
+    }
+}

modules/interface/manifests/rps.pp
@@ -6,14 +6,9 @@
 # - $interface:
 #   The network interface to operate on
 define interface::rps {
-    $interface = $title
+    require interface::rpstools
 
-    file { '/usr/local/sbin/interface-rps':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0555',
-        source  => 'puppet:///modules/interface/interface-rps.py',
-    }
+    $interface = $title
 
     file { "/etc/init/enable-rps-$interface.conf":
         owner   => 'root',

commit 5b5206c684719966fc61627b48bb361a80046f71

Fix a typo introduced in 1a0605b
class, not define

Change-Id: I46cfa976c41a395a1886cdc8970ef5176d3ca062

modules/interface/manifests/rpstools.pp
@@ -5,7 +5,7 @@
 # Note that this is explicitly split in a separate class. It is used only in
 # definition interface::rps and this split allows up to avoid duplicate
 # definitions of the file resource.
-define interface::rpstools {
+class interface::rpstools {
     file { '/usr/local/sbin/interface-rps':
         owner   => 'root',
         group   => 'root',