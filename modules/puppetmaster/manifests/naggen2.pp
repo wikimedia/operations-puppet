c2a1762afe209031d2feeea28aca85d98b548ef9
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-05-29 12:55:15 +0200

    naggen: avoid clash with saltmaster for packages
    Alas, we had to remove ensure_resource because it does not really work
    under puppet 2.7, but this caused a duplicate package declaration on
    saltmasters.
    
    Re-introducing an ugly define should help here.
    
    Change-Id: I9d57baa46efcaecdd3bf3ead43d28e16de6b9f71
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/puppetmaster/manifests/naggen2.pp
+++ modules/puppetmaster/manifests/naggen2.pp
@@ -2,7 +2,15 @@ class puppetmaster::naggen2($ensure = 'present'){
 
     $packages = ['python-jinja2', 'python-mysqldb', 'python-sqlalchemy']
 
-    package{$packages:
+    # When migrated to puppet 3, we can substitute this with ensure_resource
+    # for now, we have this ugly define here.
+    define ensure_package($ensure = 'present') {
+        if ! defined(Package[$title]) {
+            package {$title: ensure => $ensure }
+        }
+    }
+
+    puppetmaster::naggen2::ensure_package {$packages:
         ensure => latest
     }
 
