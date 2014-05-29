483181450e909e038bf39b5f1286a258dd66304a
cpettet <rush@wikimedia.org>
2014-05-29 11:55:29 -0500

    admin yaml node /virt100[1-7].eqiad.wmnet/
    Change-Id: I3a04f791ad0455867587996bf05d03fe03607966

--- manifests/site.pp
+++ manifests/site.pp
@@ -2850,6 +2850,7 @@ node /virt100[1-7].eqiad.wmnet/ {
     $openstack_version = 'havana'
     $use_neutron = false
 
+    include admin
     include standard
     include role::nova::compute
     if $use_neutron == true {
