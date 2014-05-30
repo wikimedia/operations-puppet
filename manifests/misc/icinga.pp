5caaedc3708857213518d77999a39e14fc812ab5
Christopher Johnson (WMDE) <christopher.johnson@wikimedia.de>
2014-05-30 08:43:56 +0200

    Icinga: new command "check_dispatch" for Wikidata
    This command is in Perl and has several external Perl module dependencies.
    Most are available as debian packages, but JSON::Path is not.
    
    Repackaged CPAN modules, JSON::Path, LV, and Exporter::Tiny  as *.deb.
    included in the /files/icinga/packages directory.
    
    The icinga.pp manifest has been updated with package requirements.
    It should be revised with the preferred method for local deb installation by puppet.
    Also, how the command will work should be clarified and a configuration method added.
    
    The function of the command is to check the average dispatch lag and
    establish a critical threshold which will trigger an alert if exceeded.
    The threshold is set with an argument.  The lag is output as performance data.
    
    The command does three things.  Checks to see if the API URL is available with
    a head request using LWP, then it fetches the JSON from the statistics module with WWW::Mechanize,
    and finally it extracts the values from the JSON using JSON::Path.
    
    Bug 65291
    Change-Id: I354bb175e4b8fc12cbaa9699e3dda65ed4a0861b

--- manifests/misc/icinga.pp
+++ manifests/misc/icinga.pp
@@ -733,6 +733,20 @@ class icinga::monitor::packages {
         ensure => latest,
     }
 
+   # Perl modules needed by plugins
+    package { [
+         'libdatetime-perl', # manipulating dates, times and timestamps
+         'libdatetime-format-duration-perl', #Format and parse DateTime::Durations objects in perl
+         'libdatetime-format-strptime-perl',  #Perl module to parse and format strp and strf time patterns
+         'libdata-dumper-simple-perl', #Easily dump variables together with their names (Data::Dumper-like)
+         'libjson-perl', # module for manipulating JSON-formatted data
+         'libwww-mechanize-perl' , # module to automate interaction with websites
+   # local repo debian packages
+   #      'libjson-path-perl_0.205-1_all.deb' , # search nested hashref/arrayref structures using JSONPath
+   #      'libexporter-tiny-perl_0.038-1_all.deb' ,# an exporter with the features of Sub::Exporter but only core dependencies
+   #      'liblv-perl_0.006-1_all.deb' ,  # makes lvalue subroutines easy
+       ]: ensure => present,
+    }
 }
 
 class icinga::monitor::service {
