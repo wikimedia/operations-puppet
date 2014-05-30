19916781161ca0398a54d2d0006fb83615f48db5
cpettet <rush@wikimedia.org>
2014-05-30 17:02:59 -0500

    admin yaml wtp*
    Change-Id: I9cd9d837edd925abe5b4f579b5601cd78663bc04

--- manifests/site.pp
+++ manifests/site.pp
@@ -2911,12 +2911,15 @@ node /^wtp10(0[1-9]|1[0-9]|2[0-4])\.eqiad\.wmnet$/ {
     }
 
     include standard
-    include admins::roots
-    include admins::parsoid
     include role::parsoid::production
 
     class { 'lvs::realserver':
         realserver_ips => ['10.2.2.28'],
+    }
+
+    class { 'admin:
+        groups => ['parsoid-admin',
+                   'parsoid-roots'],
     }
 }
 
