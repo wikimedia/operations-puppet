dbb766ef7a885bb50617fea0ed2946e801b16edd
Ori Livneh <ori@wikimedia.org>
2014-06-02 15:58:05 -0700

    File['/a/common']: symlink to common-local; replace => False
    Before If74ea635f, File['/a/common'] was a directory with replace => false
    because "# FIXME: /a/common is a symlink on some systems". The system
    (singular) was terbium. In fact, it ought to be a symlink everywhere except on
    tin, so make it a symlink and set it to replace => false.
    
    Change-Id: If923e3a6c09ac5255a25828dc56ef903460064ad

--- modules/mediawiki/manifests/sync.pp
+++ modules/mediawiki/manifests/sync.pp
@@ -9,58 +9,68 @@ class mediawiki::sync {
         ensure  => link,
         target  => '/srv/deployment/scap/scap/bin/mwversionsinuse',
     }
+
     file { '/usr/local/bin/scap-rebuild-cdbs':
         ensure  => link,
         target  => '/srv/deployment/scap/scap/bin/scap-rebuild-cdbs',
     }
+
     file { '/usr/local/bin/scap-recompile':
         ensure  => link,
         target  => '/srv/deployment/scap/scap/bin/scap-recompile',
     }
+
     file { '/usr/local/bin/sync-common':
         ensure  => link,
         target  => '/srv/deployment/scap/scap/bin/sync-common',
     }
+
     file { '/usr/local/bin/refreshCdbJsonFiles':
         ensure  => link,
         target  => '/srv/deployment/scap/scap/bin/refreshCdbJsonFiles',
     }
+
     file { '/etc/apache2/wmf':
         ensure => link,
         target => '/usr/local/apache/conf',
     }
+
     file { '/usr/local/apache/common':
         ensure => link,
         target => '/usr/local/apache/common-local',
     }
+
     file { '/apache':
         ensure => link,
         target => '/usr/local/apache',
     }
+
     file { '/a':
         ensure => directory,
         owner  => 'root',
         group  => 'root',
         mode   => '0775',
     }
-    file { '/a/common':
-        ensure => directory,
-        owner  => 'root',
-        group  => 'wikidev',
-        mode   => '0775',
-    }
 
+    file { '/a/common':
+        ensure  => link,
+        target  => '/usr/local/apache/common-local',
+        owner   => 'root',
+        group   => 'wikidev',
+        mode    => '0775',
+        replace => false,
+    }
 
     exec { 'mw-sync':
         command     => '/usr/local/bin/sync-common',
         require     => File['/usr/local/bin/sync-common','/a/common'],
         cwd         => '/tmp',
-        user        => root,
-        group       => root,
+        user        => 'root',
+        group       => 'root',
         path        => '/usr/local/bin:/usr/bin:/usr/sbin',
         refreshonly => true,
         timeout     => 600,
-        logoutput   => on_failure;
+        logoutput   => 'on_failure',
     }
 
     exec { 'mw-sync-rebuild-cdbs':
@@ -71,7 +81,7 @@ class mediawiki::sync {
         path        => '/usr/local/bin:/usr/bin:/usr/sbin',
         refreshonly => true,
         timeout     => 600,
-        logoutput   => on_failure,
+        logoutput   => 'on_failure',
         require     => File['/usr/local/bin/scap-rebuild-cdbs'],
         subscribe   => Exec['mw-sync'],
     }
