4d3f9bab6113e6f6723b1fcc2602b61c7abb71b4
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-06-02 16:01:06 +0300

    Add bgwriter ganglia stats to postgresql
    Background writer buffers inserted into ganglia stats
    
    Change-Id: If12667592094d5a0038d1db3ac695bb4eeca4744

--- modules/postgresql/manifests/ganglia.pp
+++ modules/postgresql/manifests/ganglia.pp
@@ -7,7 +7,7 @@ class postgresql::ganglia(
                     $pgstats_db = 'template1',
                     $pgstats_host = '127.0.0.1',
                     $pgstats_port = '5432',
-                    $pgstats_refresh_rate = 60,
+                    $pgstats_refresh_rate = 15,
                     $ensure='present') {
     Class['postgresql::server'] -> Class['postgresql::ganglia']
 
