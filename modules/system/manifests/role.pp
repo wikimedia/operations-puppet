33816824324ac47ae6aa87a898caf973ca3f36bb
Ori Livneh <ori@wikimedia.org>
2014-06-01 23:09:09 -0700

    Clean up system::role
    * Add documentation
    * Make description parameter optional, to avoid things like:
    
        system::role { 'misc::fundraising::mail': description => 'fundraising mail server' }
        system::role { "misc::management::ipmi": description => "IPMI Management" }
        system::role { "misc::noc-wikimedia": description => "noc.wikimedia.org" }
    
    * Remove dead code
    
    I'm still not sure this deserves to be its own module, but at least it's
    a bit better now.
    
    Change-Id: Ic36f85461f6eff5ef521fef4ef4ab27e91de4a4d

--- modules/system/manifests/role.pp
+++ modules/system/manifests/role.pp
@@ -1,23 +1,42 @@
-# Prints a MOTD message about the role of this system
-define system::role($description, $ensure=present) {
-    $role_script_content = "#!/bin/sh
+# == Define: system::role
+#
+# Adds a banner message to the server MOTD (usually displayed on login)
+# that identifies the role of the server.
+#
+# === Parameters
+#
+# [*ensure*]
+#   Present or absent. (Default: present.)
+#
+# [*description*]
+#   A human-readable description of the role. Optional.
+#
+# === Example
+#
+#  system::role { 'role::analytics::hadoop::master':
+#    description => 'Hadoop Master (NameNode & ResourceManager)'
+#  }
+#
+define system::role(
+    $ensure      = present,
+    $description = undef,
+) {
+    $safename = regsubst($title, '\W', '-', 'G')
 
-echo \"$(hostname) is a Wikimedia ${description} (${title}).\"
-"
+    $message = $description ? {
+        undef   => "${::hostname} is ${title}",
+        default => "${::hostname} is a ${description} (${title})",
+    }
 
-    $rolename = regsubst($title, ':', '-', 'G')
-    $motd_filename = "/etc/update-motd.d/05-role-${rolename}"
     # deactivated for now because it blocks new installs
     # dependency on minion config and having a signed salt key
     # salt::grain { $rolename: grain => 'rolename', value => $name }
 
-    if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '9.10') >= 0 {
-        file { $motd_filename:
-            ensure  => $ensure,
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0555',
-            content => $role_script_content,
-        }
+    file { "/etc/update-motd.d/05-role-${safename}":
+        ensure  => $ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        content => "#!/bin/sh\necho '${message}'\n",
     }
 }
