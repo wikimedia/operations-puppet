842c39f41f6407a11faf214c3b8f2bcda3b62b19
Andrew Otto <acotto@gmail.com>
2014-06-02 11:50:48 -0400

    Not saving Kafka jmx  metrics via jmxtrans to an outfile on disk
    We don't use this file ever, and it just takes up space!
    
    Change-Id: If6af6bb648ced0c134a4eeefe30ad8290ec676d8

--- manifests/role/analytics/kafka.pp
+++ manifests/role/analytics/kafka.pp
@@ -147,34 +147,10 @@ class role::analytics::kafka::server inherits role::analytics::kafka::client {
         require      => Class['::kafka::server'],
     }
 
-    $jmxtrans_outfile = '/var/log/kafka/kafka-jmx.log'
-    file { $jmxtrans_outfile:
-        ensure  => 'present',
-        owner   => 'jmxtrans',
-        group   => 'jmxtrans',
-        mode    => '0644',
-        require => [Package['jmxtrans'], Package['kafka']]
-    }
-
     # Include Kafka Server Jmxtrans class
     # to send Kafka Broker metrics to Ganglia.
-    # We also save metrics to an logfile for easy
-    # debugging.
     class { '::kafka::server::jmxtrans':
         ganglia => "${ganglia_host}:${ganglia_port}",
-        outfile => $jmxtrans_outfile
-    }
-
-    # Install a logrotate.d file for the jmx.log file
-    file { '/etc/logrotate.d/kafka-jmx':
-        content =>
-"${jmxtrans_outfile} {
-    size 100M
-    rotate 2
-    missingok
-    create 0644 jmxtrans jmxtrans
-}
-"
     }
 
     # Generate icinga alert if this jmxtrans instance is not running.
