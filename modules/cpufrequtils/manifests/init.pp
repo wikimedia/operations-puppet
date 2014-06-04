f107bf3a8c92e485bd4ce1cb2e1d8b99504feae0
Brandon Black <bblack@wikimedia.org>
2014-06-04 16:56:40 -0500

    disable ondemand service w/ cpufrequtils
    Change-Id: I600da3744a64db0d169619382b7cc48a17e9e251

--- modules/cpufrequtils/manifests/init.pp
+++ modules/cpufrequtils/manifests/init.pp
@@ -19,6 +19,12 @@ class cpufrequtils (
         require => Package['cpufrequtils'],
     }
 
+    # Ubuntu's default initscripts package includes a service called "ondemand"
+    #   which is a one-shot action invoked at startup which sleeps 60 seconds
+    #   and then sets all CPUs to the ondemand governor, thus undoing the work
+    #   of cpufrequtils :P
+    service { 'ondemand': enable => false }
+
     file { '/etc/default/cpufrequtils':
         content => "GOVERNOR=${governor}\n",
         notify  => Exec['apply cpufrequtils'],
