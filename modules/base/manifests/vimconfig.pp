5bf68099235e962947eebdd45124c2a2370b9781
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-06-04 18:42:18 +0300

    Purge vimrc.local
    Now that anyone can have his own personal account and his own .vimrc
    file this has no reason to be forced on everyone. Purge the file
    
    Change-Id: Id5eb2deb16a368e2bba87d8bdbc52a7c62986e70

--- modules/base/manifests/vimconfig.pp
+++ modules/base/manifests/vimconfig.pp
@@ -1,10 +1,9 @@
 class base::vimconfig {
+    # This is provided for some reason by wikimedia-base. We ensure it purged
+    # but do not remove this resource until wikimedia-base no longer manages
+    # that file #RT 7618 or wikimedia-base no longer exists
     file { '/etc/vim/vimrc.local':
-        ensure => present,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => 'puppet:///modules/base/vimconfig/vimrc.local',
+        ensure => absent,
     }
 
     if $::lsbdistid == 'Ubuntu' {
