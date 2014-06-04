e1ebe00292c75ab7188e9d307863e820eb98deba
Timo Tijhof <krinklemail@gmail.com>
2014-06-04 02:38:13 +0200

    Improvements to asset-check
    * Implement --debug:
       phantomjs has its own debug parameter used like this:
       $ phantomjs --debug true asset-check.js 'http://en.wikipedia.org/?mainpage'
       But that one isn't exposed to JS, so we need our own.
    * Use "response.stage" property to filter out duplicates:
       Document the fact that the 'onResourceReceived' event is fired
       multiple times for reach resource with different 'stage' values
       to indicate its current state.
       Use this value instead of duck-typing via res.bodySize (which
       can be unrealiable, mostly in case of dataURIs, though we filter
       those out already).
    * Use content-length header when response.bodySize is missing
       As of writing, when using phantomjs v1.9.7 and running asset-check.js
       on en.wikipedia.org Main Page, none of the requests passed through the
       onResourceReceived event have bodySize (payload.combined.bytes === 0).
       Use content-length as fallback and work-around to at least have a
       little more data to work with for our trend graphs.
    * Track POST requests, redirects, http4xx, and http5xx
    * Track whether requests are compressed with gzip
    * Track uncaught exceptions in javascript
       Any uncaught exceptions thrown on when viewing the Main Page as
       an anonymous user.
    * Track number of registered modules and their state
    
    Change-Id: If1fd7eea2e525a23835a985a86596c7c0361768b

--- modules/webperf/files/asset-check.js
+++ modules/webperf/files/asset-check.js
@@ -12,6 +12,7 @@
  * Optional arguments:
  *   --timeout=N       specifies timeout, in seconds.
  *   --ua=USER_AGENT   set this User-Agent header.
+ *   --debug           enable verbose output and avoid minification.
  *
  * Copyright (C) 2013, Ori Livneh <ori@wikimedia.org>
  * Licensed under the terms of the GPL, version 2 or later.
@@ -20,6 +21,7 @@
 var system = require( 'system' ),
     webpage = require( 'webpage' ),
     options = {
+        debug: false,
         timeout: 30,
         ua: null,
     };
@@ -27,6 +29,12 @@ var system = require( 'system' ),
 function parseArgument( arg ) {
     if ( arg === '-h' || arg === '--help' ) {
         usage();
+        return;
+    }
+
+    if ( arg === '--debug' ) {
+        options.debug = true;
+        return;
     }
 
     var match = /^--([^=]+)=(.*)/.exec( arg );
@@ -39,7 +47,7 @@ function parseArgument( arg ) {
 
 function usage() {
     console.error( 'Usage: phantomjs ' + system.args[ 0 ] +
-        ' URL [--timeout=N] [--ua=USER_AGENT_STRING]'
+        ' URL [--timeout=N] [--ua=USER_AGENT_STRING] [--debug]'
     );
     phantom.exit( 1 );
 }
@@ -54,11 +62,40 @@ function checkAssets( url ) {
     var payload = {
         javascript: { requests: 0, bytes: 0 },
         html: { requests: 0, bytes: 0 },
-        css: { requests: 0, bytes: 0, rules: 0 },
+        css: {
+            requests: 0,
+            bytes: 0,
+            rules: 0
+        },
         image: { requests: 0, bytes: 0 },
         other: { requests: 0, bytes: 0 },
-        cookies: { set: 0 },
+        cookies: {
+            set: 0
+        },
+        combined: {
+            requests: 0,
+            bytes: 0,
+            post: 0,
+            redirects: 0,
+            uncompressed: 0,
+            http200: 0,
+            http304: 0,
+            http4xx: 0,
+            http5xx: 0,
+            httpOther: 0,
+            uncaughtException: 0
+        },
+        modules: {
+            registered: 0,
+            loading: 0,
+            loaded: 0,
+            ready: 0,
+            error: 0,
+            missing: 0,
+            unregistered: 0
+        }
     };
+    var resourcesRequested = {};
 
     var page = webpage.create();
 
@@ -66,15 +103,92 @@ function checkAssets( url ) {
         page.settings.userAgent = options.ua;
     }
 
-    // Analyze incoming resource
+    page.onResourceRequested = function ( req ) {
+        resourcesRequested[ req.id ] = {
+            method: req.method,
+            isBase64: /^data:/.test( req.url )
+        };
+    };
+
+    /**
+     * Analyze incoming resource
+     *
+     * Called two or more times for every requested resource (e.g. urls and
+     * resolved data uris). Once with stage="start", once with stage="end", and
+     * potentially zero or more times for individual chunks if the server
+     * sent data in multiple chunks.
+     *
+     * @param {Object} res
+     */
     page.onResourceReceived = function ( res ) {
         var match = /javascript|html|css|image/i.exec( res.contentType ) || [ 'other' ],
             type = match[0],
-            resource = payload[ type ];
+            req = resourcesRequested[ res.id ] || {},
+            resource = payload[ type ],
+            compressed = false,
+            size = 0;
 
-        if ( res.bodySize && !/^data:/.test( res.url ) ) {
+        if ( res.stage == 'end' && !req.isBase64 ) {
             resource.requests++;
-            resource.bytes += res.bodySize;
+            payload.combined.requests++;
+
+            // @todo bodySize is not reliable and often missing completely or inaccurate
+            // (e.g. does not take into account gzip compression, or does so when not expected)
+            // - https://github.com/ariya/phantomjs/issues/10156
+            // - https://github.com/ariya/phantomjs/issues/10169
+            if ( res.bodySize ) {
+                size = res.bodySize;
+            } else {
+                res.headers.forEach( function ( header ) {
+                    switch ( header.name.toLowerCase() ) {
+                        case 'content-length':
+                            size = parseInt( header.value, 10 );
+                            break;
+                        case 'content-encoding':
+                            if ( header.value === 'gzip' ) {
+                                compressed = true;
+                            }
+                            break;
+                    }
+                } );
+            }
+
+            resource.bytes += size;
+            payload.combined.bytes += size;
+
+            if ( !compressed ) {
+                payload.combined.uncompressed++;
+            }
+
+            if ( req.method === 'POST' ) {
+                payload.combined.post++;
+            }
+
+            switch ( res.status ) {
+                case 200: // OK
+                    payload.combined.http200++;
+                    break;
+                case 301: // Moved Permanently
+                case 302: // Found
+                case 303: // See Other
+                    payload.combined.redirects++;
+                    break;
+                case 304: // Not Modified
+                    payload.combined.http304++;
+                    break;
+                case 403: // Forbidden
+                case 403: // Not Found
+                    payload.combined.http4xx++;
+                    break;
+                case 500: // Internal Server Error
+                case 502: // Bad Gateway
+                case 503: // Service Unavailable
+                case 504: // Gateway Timeout
+                    payload.combined.http5xx++;
+                    break;
+                default:
+                    payload.combined.httpOther++;
+            }
         }
     };
 
@@ -82,8 +196,40 @@ function checkAssets( url ) {
     page.onLoadFinished = function () {
         payload.cookies.set = page.cookies.length;
         payload.css.rules = page.evaluate( countCssRules );
-        console.log( JSON.stringify( payload ) );
+
+        payload.modules = page.evaluate( function () {
+            /*global mw */
+            var states = {
+                registered: 0,
+                loading: 0,
+                loaded: 0,
+                ready: 0,
+                error: 0,
+                missing: 0,
+                unregistered: 0
+            };
+            mw.loader.getModuleNames().forEach( function ( name ) {
+              var state = mw.loader.getState( name );
+              if ( states[ state ] !== undefined ) {
+                states[ state ]++;
+              }
+            } );
+            return states;
+        } );
+
+        if ( options.debug ) {
+            console.log( JSON.stringify( payload, null, '\t' ) );
+        } else {
+            console.log( JSON.stringify( payload ) );
+        }
         phantom.exit( 0 );
+    };
+
+    page.onError = function () {
+        // For example:
+        // - Uncaught ReferenceError: jQuery not defined
+        // - TypeError: 'undefined' is not a function (evaluating '$(document)')
+        payload.combined.uncaughtException++;
     };
 
     // Abort if 30 seconds elapsed and the page hasn't finished loading
@@ -99,7 +245,7 @@ function checkAssets( url ) {
 }
 
 system.args.slice( 1 ).forEach( parseArgument );
-if ( Object.keys( options ).sort().join(' ') !== 'timeout ua url' ) {
+if ( Object.keys( options ).sort().join(' ') !== 'debug timeout ua url' ) {
     usage();
 } else {
     checkAssets( options.url );
