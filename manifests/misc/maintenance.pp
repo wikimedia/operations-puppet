568a7e6a246f578d7e9a291bdf8e7e1da1c1dd91
aude <aude.wiki@gmail.com>
2014-06-04 12:03:39 +0200

    Log wikidata cron jobs to /var/log/wikidata
    /var/log/mediawiki/wikidata was removed in I96020da
    and now the change dispatcher is not running.
    
    Change-Id: Iadfc63b928fed94ff0f508bc3a2448224c2d9a32

--- manifests/misc/maintenance.pp
+++ manifests/misc/maintenance.pp
@@ -230,7 +230,7 @@ class misc::maintenance::wikidata( $enabled = false ) {
 
     cron { 'wikibase-repo-prune2':
         # prunes the wb_changes table in wikidatawiki db
-        command => '/usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/pruneChanges.php --wiki wikidatawiki --number-of-days=3 2>&1 >> /var/log/mediawiki/wikidata/prune2.log',
+        command => '/usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/pruneChanges.php --wiki wikidatawiki --number-of-days=3 2>&1 >> /var/log/wikidata/prune2.log',
         user    => 'apache',
         minute  => [0,15,30,45],
         ensure  => $wbenabled,
@@ -240,7 +240,7 @@ class misc::maintenance::wikidata( $enabled = false ) {
     # This handles inserting jobs into client job queue, which then process the changes
     cron { 'wikibase-dispatch-changes3':
         # dispatches changes data to wikibase clients (e.g. wikipedia) to be processed as jobs there
-        command => '/usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/lib/maintenance/dispatchChanges.php --wiki wikidatawiki --max-time 900 --batch-size 200 --dispatch-interval 30 2>&1 >> /var/log/mediawiki/wikidata/dispatcher3.log',
+        command => '/usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/lib/maintenance/dispatchChanges.php --wiki wikidatawiki --max-time 900 --batch-size 200 --dispatch-interval 30 2>&1 >> /var/log/wikidata/dispatcher3.log',
         user    => 'apache',
         minute  => '*/5',
         ensure  => $wbenabled,
@@ -248,14 +248,14 @@ class misc::maintenance::wikidata( $enabled = false ) {
 
     cron { 'wikibase-dispatch-changes4':
         # second dispatcher to inject wikidata changes  wikibase clients (e.g. wikipedia) to be processed as jobs there
-        command => '/usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/lib/maintenance/dispatchChanges.php --wiki wikidatawiki --max-time 900 --batch-size 200 --dispatch-interval 30 2>&1 >> /var/log/mediawiki/wikidata/dispatcher4.log',
+        command => '/usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/lib/maintenance/dispatchChanges.php --wiki wikidatawiki --max-time 900 --batch-size 200 --dispatch-interval 30 2>&1 >> /var/log/wikidata/dispatcher4.log',
         user    => 'apache',
         minute  => '*/5',
         ensure  => $wbenabled,
     }
 
     cron { 'wikibase-rebuild-entityperpage':
-        command  => '/usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/rebuildEntityPerPage.php --wiki wikidatawiki --force 2>&1 >> /var/log/mediawiki/wikidata/rebuildEpp.log',
+        command  => '/usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/rebuildEntityPerPage.php --wiki wikidatawiki --force 2>&1 >> /var/log/wikidata/rebuildEpp.log',
         user     => 'apache',
         minute   => 30,
         hour     => 3,
