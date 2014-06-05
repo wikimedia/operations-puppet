7befad9350e4980706994ca1dede6712b1eeb58f
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-06-05 16:17:16 +0200

    puppet: upgrade labs puppetmasters to version 3
    this change will install puppet 3 on the labs puppet masters.
    
    Some config changes are needed in puppet 3 due to a long standing bug,
    see http://docs.puppetlabs.com/guides/file_serving.html#ip-addresses
    
    Change-Id: I7df5d8c4bf86cd24772cca02218d311bc6f72c0d
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/puppetmaster/manifests/config.pp
+++ modules/puppetmaster/manifests/config.pp
@@ -5,18 +5,35 @@
 class puppetmaster::config {
     include base::puppet
 
-    file {
-        '/etc/puppet/puppet.conf.d/20-master.conf':
-            require => File['/etc/puppet/puppet.conf.d'],
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            content => template('puppetmaster/20-master.conf.erb'),
-            notify  => Exec['compile puppet.conf'];
-        '/etc/puppet/fileserver.conf':
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            content => template('puppetmaster/fileserver.conf.erb');
+    file { '/etc/puppet/puppet.conf.d/20-master.conf':
+        require => File['/etc/puppet/puppet.conf.d'],
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('puppetmaster/20-master.conf.erb'),
+        notify  => Exec['compile puppet.conf']
     }
+
+    if $puppet_version == '3' {
+        # See https://tickets.puppetlabs.com/browse/PUP-1237
+        file { '/etc/puppet/fileserver.conf':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            content => template('puppetmaster/fileserver.conf.erb'),
+        }
+        file { '/etc/puppet/auth.conf':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            content => template('puppetmaster/auth.conf.erb'),
+        }
+    } else {
+        file { '/etc/puppet/fileserver.conf':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            content => template('puppetmaster/fileserver.conf.erb'),
+        }
+   }
 }
