590890efdc29a3c8a68bbd002786fa6bf9b93477
Ori Livneh <ori@wikimedia.org>
2014-06-06 08:52:27 -0700

    role::mediawiki::webserver: set maxclients dynamically
    The api and bits application servers already set maxclients to 100. I don't
    think there's a principled reason for the very particular values we assign to
    maxclients on the standard app servers -- they seem like carryovers from old
    configs. This is especiall silly now that the bits app servers are just generic
    app servers (following change I51c6a6c33).
    
    So instead derive an optimum MaxClients value by setting aside 30% of RAM for
    the OS, APC, and misc. services, and divide the remaining 70% by the average
    memory cost of an Apache worker. Take the minimum of the result and 100.
    
    Change-Id: Ia424c6433fb87b565a4bb81cf3eeadce7bac3655

--- manifests/role/mediawiki.pp
+++ manifests/role/mediawiki.pp
@@ -21,14 +21,15 @@ class role::mediawiki::common {
     }
 }
 
-class role::mediawiki::webserver( $pool, $maxclients ) {
+class role::mediawiki::webserver( $pool, $workers_limit = undef ) {
     include ::mediawiki
+    include ::mediawiki::web
     include ::apache::monitoring
     include role::mediawiki::common
     include lvs::configuration
 
     class { '::mediawiki::web':
-        maxclients => $maxclients,
+        workers_limit => $workers_limit
     }
 
     class { 'lvs::realserver':
@@ -44,24 +45,13 @@ class role::mediawiki::webserver( $pool, $maxclients ) {
 class role::mediawiki::appserver {
     system::role { 'role::mediawiki::appserver': }
 
-    class { 'role::mediawiki::webserver':
-        pool       => 'apaches',
-        maxclients => $::processorcount ? {
-            16      => 60,
-            12      => 50,
-            24      => 50,
-            default => 40,
-        }
-    }
+    class { 'role::mediawiki::webserver': pool => 'apaches' }
 }
 
 class role::mediawiki::appserver::api {
     system::role { 'role::mediawiki::appserver::api': }
 
-    class { 'role::mediawiki::webserver':
-        pool       => 'api',
-        maxclients => 100,
-    }
+    class { 'role::mediawiki::webserver': pool => 'api' }
 }
 
 class role::mediawiki::imagescaler {
@@ -69,10 +59,7 @@ class role::mediawiki::imagescaler {
 
     include ::mediawiki::multimedia
 
-    class { 'role::mediawiki::webserver':
-        pool       => 'rendering',
-        maxclients => 18,
-    }
+    class { 'role::mediawiki::webserver': pool => 'rendering', workers_limit => 18 }
 }
 
 class role::mediawiki::videoscaler {
