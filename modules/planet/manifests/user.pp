d372425f6c6fc5cd126a3d3f425e49c1d05f5f53
cpettet <rush@wikimedia.org>
2014-06-06 14:34:26 -0500

    planet - replace generic::systemuser with user
    we want to get rid of generic::systemuser
    and replace it with the regular puppet user type
    again to remove a layer of abstraction that isn't
    really needed
    
    Change-Id: I7de8415110e601ed7fa0caf81c3a2b43abed306b

--- modules/planet/manifests/user.pp
+++ modules/planet/manifests/user.pp
@@ -1,10 +1,11 @@
 # adds a system user for a planet-venus install
 class planet::user {
 
-    generic::systemuser { 'planet':
-        name   => 'planet',
-        home   => '/var/lib/planet',
-        groups => [ 'planet' ],
+    user { 'planet':
+        home       => '/var/lib/planet',
+        groups     => [ 'planet' ],
+        managehome => true,
+        system     => true,
     }
 
 }
