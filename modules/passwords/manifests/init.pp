82b962d2b64958ccaef3975371aa9906ab44a969
cpettet <rush@wikimedia.org>
2014-06-06 14:39:45 -0500

    removing systemuser definition
    Change-Id: I63adc21f709ec01c5011ed17a2b6a35af32f1f36

---
+++ modules/passwords/manifests/init.pp
@@ -0,0 +1,190 @@
+
+class passwords::root {
+}
+
+class passwords::certs {
+}
+
+class passwords::openstack::nova {
+}
+
+class passwords::openstack::glance {
+}
+
+class passwords::openstack::neutron {
+}
+
+class passwords::openstack::keystone {
+}
+
+class passwords::swift::pmtpa-prod {
+}
+
+class passwords::swift::eqiad-prod {
+}
+
+class passwords::swift::pmtpa-test {
+}
+
+class passwords::swift::eqiad-test {
+}
+
+class passwords::ceph::eqiad {
+}
+
+class passwords::ldap::initial_setup {
+}
+
+class passwords::ldap::production {
+}
+
+class passwords::ldap::corp {
+}
+
+class passwords::ldap::labs {
+}
+
+# TODO: remove these when they are no longer referenced
+class passwords::ldap::wmf_cluster {
+}
+
+class passwords::ldap::wmf_corp_cluster {
+}
+
+class passwords::ldap::wmf_test_cluster {
+}
+
+class passwords::puppet::database {
+}
+
+class passwords::nagios::mysql {
+}
+
+class passwords::mysql::querydigest {
+}
+
+
+# username and password for the
+# mysql research user
+class passwords::mysql::research {
+}
+
+class passwords::mysql::research_prod {
+}
+
+
+class passwords::mysql::globaldev {
+}
+
+class passwords::mysql::metrics {
+}
+
+class passwords::mysql::eventlogging {
+}
+
+class passwords::mysql::wikimania_scholarships {
+}
+
+class passwords::mongodb::eventlogging {
+}
+
+class passwords::nagios::monitor {
+}
+
+class passwords::nagios::snmp {
+}
+
+class passwords::lucene {
+}
+
+class passwords::ganglia {
+}
+
+class passwords::graphite {
+}
+
+class passwords::etherpad {
+}
+
+class passwords::etherpad_lite {
+}
+
+class passwords::exim {
+}
+
+class passwords::gerrit {
+}
+
+class passwords::civi {
+}
+
+class passwords::mobile::vumi {
+}
+
+class passwords::network {
+}
+
+class passwords::misc::rt {
+}
+
+class passwords::misc::scripts {
+}
+
+class passwords::puppetmaster::dashboard {
+}
+
+class passwords::udpmxircecho {
+}
+
+class passwords::misc::contint::jenkins {
+}
+
+class passwords::analytics {
+}
+
+
+class passwords::e3::metrics {
+}
+
+class passwords::bugzilla {
+}
+
+class passwords::wikistats {
+}
+
+class passwords::geoip {
+}
+
+class passwords::racktables {
+}
+
+class passwords::redis {
+}
+
+class passwords::logstash {
+}
+
+class passwords::logmsgbot {
+}
+
+class passwords::globaldev {
+	
+}
+
+class passwords::bacula {
+}
+
+class passwords::geowiki {
+}
+
+class passwords::rancid {
+}
+
+class passwords::librenms {
+}
+
+class passwords::pmacct {
+}
+
+class passwords::osm {
+}
