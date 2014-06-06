2e544cf087dc66c83a98c069142a58b90fb2315b
cpettet <rush@wikimedia.org>
2014-06-06 14:35:37 -0500

    search - replace generic::systemuser with user
    We want to get rid of generic::systemuser
    and replace it with the regular puppet user type
    again to remove a layer of abstraction that isn't
    really needed.
    
    Change-Id: I6dde8267df8e3439231c9bfdf57343ce5b578936

--- manifests/search.pp
+++ manifests/search.pp
@@ -152,9 +152,19 @@ class lucene {
     }
 
     class users {
-        generic::systemuser { 'lsearch':
+
+        group { 'lsearch':
+            ensure => present,
+            name   => 'lsearch',
+            system => true,
+        }
+
+        user { 'lsearch':
             name          => 'lsearch',
-            default_group => 'lsearch',
+            gid           => 'lsearch',
+            managehome    => true,
+            system        => true,
+            home          => '/var/lib/search',
         }
     }
 
