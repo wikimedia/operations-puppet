e939c931e5ab6e22945ead028000ae2fb0b534db
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-06-06 09:06:28 +0200

    puppet: ensure defining $puppet_version does upgrade
    As all our precise nodes are on 2.7 and will likely transition to puppet
    3, so we need to be sure we remove the pinning files we had for 2.7.
    
    Change-Id: Id7abade4abee72c19afbb42e0b08e480d04f05aa
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/apt/manifests/puppet.pp
+++ modules/apt/manifests/puppet.pp
@@ -8,7 +8,7 @@ define apt::puppet (
         # with an higher priority than the general rule preferring wikimedia
         # packages
         if $::lsbdistcodename == 'precise' {
-                apt::pin {"puppet_${title}_${version}.pref":
+                apt::pin { "puppet_${title}_${version}.pref":
                     package  => $packages,
                     pin      => 'release o=Ubuntu',
                     priority => 1002
@@ -17,10 +17,18 @@ define apt::puppet (
     } else {
         # For trusty, ubuntu packages are puppet 3
         if $::lsbdistcodename == 'trusty' {
-            apt::pin {"$title":
+            apt::pin { $title:
                 package  => $packages,
                 pin      => 'release o=Ubuntu',
                 priority => 1002
+            }
+        } elsif $::lsbdistcodename == 'precise' {
+            # Ensure the pref file is not there in this case,
+            # so that puppet gets updated.
+            apt::pin { $title:
+                ensure   => 'absent',
+                priority => 0,
+                pin      => 'release o=Ubuntu'
             }
         }
     }
