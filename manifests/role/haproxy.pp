817882e6e6e8b96ff3a0403a3008653b8e9c6ea5
Matanya Moses <matanya@foss.co.il>
2014-06-09 00:47:14 +0300

    puppetproxy: remove firewall
    We don't use puppetproxy since brewster was decommed, and we don't someone
    setting the role to suddenly get blocked by firewall he wasn't planning on
    using. Firewalls go on node level, not role level.
    
    Change-Id: If828eff33fab5f99abcca20a6bc9233970d8e830

--- manifests/role/haproxy.pp
+++ manifests/role/haproxy.pp
@@ -3,7 +3,6 @@ class role::puppetproxy {
         description => 'Puppet proxying through haproxy host',
     }
 
-    include base::firewall
     ferm::rule { 'puppet_haproxy':
         rule => 'proto tcp dport 8140 { saddr $ALL_NETWORKS ACCEPT; }'
     }
