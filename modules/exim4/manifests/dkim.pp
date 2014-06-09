bff981e65a85ad9819e092594a30229269b7ffb7
Faidon Liambotis <faidon@wikimedia.org>
2014-06-09 12:38:34 +0300

    Introduce a new minimal exim4 module
    Introduce a new exim4 module that handles a very basic exim4
    configuration. The module is not attempting to do anything smart with
    the config files (yet) and just accepts a config argument for passing
    the template.
    
    Also move convert half of mail.pp to use the new module; most of the old
    exim::* classes, including exim::roled, persist but use the exim4
    hierarchy for most of their implementation.
    
    Change-Id: I076a849434594efb20da95badc2f31d0bfcb86d4

---
+++ modules/exim4/manifests/dkim.pp
@@ -0,0 +1,51 @@
+# == Define: exim4::dkim
+#
+# This definition installs a DKIM key under the exim4 DKIM directory hierarchy
+#
+# == Parameters
+#
+# [*domain*]
+#   The DKIM key domain. Required.
+#
+# [*Selector*]
+#   The DKIM key selector. Required.
+#
+# [*source*]
+#    Source of the file. Either source or content is required
+#
+# [*content*]
+#    Content of the file. Either source or content is required
+
+define exim4::dkim(
+  $domain,
+  $selector,
+  $source=undef,
+  $content=undef,
+) {
+    if $source != undef and $content != undef {
+        fail('Both source and content attribute have been defined')
+    }
+
+    $keyfile = "/etc/exim4/dkim/${domain}-${selector}.key"
+
+    file { $keyfile:
+        ensure  => present,
+        owner   => 'root',
+        group   => 'Debian-exim',
+        mode    => '0440',
+        require => File['/etc/exim4/dkim'],
+        notify  => Service['exim4'],
+    }
+
+    if $source != undef {
+        File[$keyfile] {
+            source => $source,
+        }
+    } elsif $content != undef {
+        File[$keyfile] {
+            content => $source,
+        }
+    } else {
+        fail('Either source or content attribute needs to be given')
+    }
+}
