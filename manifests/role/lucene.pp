b8a85dd1a7798397e4e38e784891b73405927363
dzahn <dzahn@wikimedia.org>
2014-06-09 11:03:50 -0700

    remove lucene::users adn ::admins remnants
    replaced by admin yaml, lucene::admins removed
    in Ia9191a2369 , remove the includes and ::users
    as well
    
    Change-Id: I21ec50893f10450d520b8e9d86582cbeb3f84f8f

--- manifests/role/lucene.pp
+++ manifests/role/lucene.pp
@@ -148,9 +148,7 @@ class role::lucene {
 		# Include packages needed for MW maintenance scripts
 		include standard,
 			mediawiki,
-			mediawiki::php,
-			role::lucene::admins,
-			lucene::users
+			mediawiki::php
 
 		# dependency for wikimedia-task-appserver
 		service { 'apache':
@@ -171,9 +169,7 @@ class role::lucene {
 			include lvs::configuration
 			class { "lvs::realserver": realserver_ips => [ $lvs::configuration::lvs_service_ips[$::realm][$search_pool][$::site] ] }
 
-			include standard,
-				role::lucene::admins,
-				lucene::users
+			include standard
 
 			class { "lucene::server":
 				udplogging => $search_pool ? {
