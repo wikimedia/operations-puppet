ffcbc38e3f105114c5996a8371aa88c5889f29a0
Faidon Liambotis <faidon@wikimedia.org>
2014-06-09 11:55:30 +0300

    Create a new role::mail hierarchy
    Move exim- and mail-specific manifests from site.pp to a new role::mail
    hierarchy.
    
    This mostly is about sodium. Since the mchenry bits have been
    traditionally unpuppetized, and since it's a bad idea to start
    puppetizing such an old box right now, move the puppetized bits as an
    "oldmx" temporary role.
    
    Change-Id: I994c30ce5dea9b4ce7382ef4cfdc7482ca431f6b

---
+++ manifests/role/mail.pp
@@ -0,0 +1,66 @@
+class role::mail::oldmx {
+    include backup::client
+    include privateexim::aliases::private
+    include exim::stats
+
+    # mails the wikimedia.org mail alias file to OIT once per week
+    class { 'misc::maintenance::mail_exim_aliases':
+        enabled => true,
+    }
+
+    # FIXME: the rest is unpuppetized so far
+}
+
+class role::mail::lists {
+    # FIXME: needs to be split to lists/secondarymx
+
+    system::role { 'role::mail::lists':
+        description => 'Mailing list server',
+    }
+
+    interface::ip { 'lists.wikimedia.org_v4':
+        interface => 'eth0',
+        address   => '208.80.154.4',
+        prefixlen => '32',
+    }
+
+    interface::ip { 'lists.wikimedia.org_v6':
+        interface => 'eth0',
+        address   => '2620:0:861:1::2',
+        prefixlen => '128',
+    }
+
+    include mailman
+    include backup::client
+
+    class { 'spamassassin':
+        required_score   => '4.0',
+        use_bayes        => '0',
+        bayes_auto_learn => '0',
+        trusted_networks => $network::constants::all_networks,
+    }
+
+    class { 'exim::roled':
+        outbound_ips           => [
+                '208.80.154.61',
+                '2620:0:861:1:208:80:154:61'
+            ],
+        list_outbound_ips      => [
+                '208.80.154.4',
+                '2620:0:861:1::2'
+            ],
+        local_domains          => [
+                '+system_domains',
+                '+mailman_domains'
+            ],
+        enable_mail_relay      => 'secondary',
+        enable_mailman         => 'true',
+        enable_mail_submission => 'false',
+        enable_spamassassin    => 'true',
+        require                => [
+            Class['spamassassin'],
+            Interface::Ip['lists.wikimedia.org_v4'],
+            Interface::Ip['lists.wikimedia.org_v6'],
+        ],
+    }
+}
