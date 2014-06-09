50988516f31739d3984b62fd5e9ef04507c6ecd1
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-06-09 12:25:08 +0200

    redis: restart service upon first install
    As per RT #7583, restart the redis service if the daemon is bound to
    listen to localhost only. This is needed upon installation for trusty
    installations only.
    
    Change-Id: I46a8725886f570996a3d6881c8ce42cf76cd8a10
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/redis/manifests/init.pp
+++ modules/redis/manifests/init.pp
@@ -53,6 +53,17 @@ class redis (
         # subscribe => not doing this deliberately
     }
 
+    if $::lsbdistcodename == 'trusty' {
+        # Upon a config change, Redis will be restarted
+        # if it's listening on localhost only, see RT 7583
+        exec {'Restart redis if needed':
+            command     => '/usr/sbin/service redis-server restart',
+            unless      => '/bin/netstat -lp | /bin/grep redis | /usr/bin/awk \'{print $4}\' | /bin/grep -v localhost 2> /dev/null',
+            subscribe   => File['/etc/redis/redis.conf'],
+            refreshonly => true,
+        }
+    }
+
     if $monitor {
         monitor_service { $servicename: description => "Redis", check_command => "check_tcp!${port}" }
     }
