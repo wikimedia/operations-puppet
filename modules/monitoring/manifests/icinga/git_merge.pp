569cbe8ea36f9ae0443fd37b26cce412b7cb6341
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-06-09 11:50:16 +0200

    monitoring: add check for git merging of important repos
    A check for outstanding merges in the puppet tree on the puppet masters
    and of mediawiki-config on the deployment masters is introduced. See
    RT #1658 and #7427.
    
    Also, some reorg of the puppetmaster module resources is done.
    
    Change-Id: I9bd1e7ef79ef148a6c31ab3a538c2d90e70a1c5b
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/monitoring/manifests/icinga/git_merge.pp
@@ -0,0 +1,23 @@
+# This define allows you to monitor for unmerged remote changes to
+# repositories that need manual merge in production as part of our workflow.
+#
+define monitoring::icinga::git_merge (
+    $dir      = "/var/lib/git/operations/${title}",
+    $user     = 'gitpuppet',
+    $warning  = 600,
+    $critical = 900
+) {
+      file { "check_${title}_needs_merge":
+          ensure  => present,
+          path    => "/usr/local/lib/nagios/plugins/check_${title}-needs-merge",
+          owner   => root,
+          group   => root,
+          mode    => '0555',
+          content => template('monitoring/check_git-needs-merge.erb')
+      }
+      nrpe::monitor_service { "${title}_merged":
+          description  => "Unmerged changes on repository ${title}",
+          nrpe_command => "/usr/local/lib/nagios/plugins/check_${title}-needs-merge",
+          require      => File["check_${title}-needs-merge"]
+      }
+}
