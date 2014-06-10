6f024a919492634075c562d78d56da6cbe6f4f75
Ori Livneh <ori@wikimedia.org>
2014-06-10 12:42:00 -0700

    apache: clean up init.pp & params.pp
    * Remove all the RedHat/EC2-specific configurations & logic
    * Replace variable package, service and vhost dirs with their literal value on
      Debian-based systems.
    * Keep the resource names the same for now, so references to Package['httpd'] /
      Service['httpd'] continue working as before.
    
    Change-Id: I419abaf4d48bfef0c5c66e3ee4b518bb7ef9e51c

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -12,61 +12,27 @@
 #
 # Sample Usage:
 #
-class apache (
-  $default_mods = true,
-  $service_enable = true,
-  $serveradmin  = 'root@localhost',
-  $sendfile     = false
-) {
-  include apache::params
+class apache ( $service_enable = true ) {
+    validate_bool($service_enable)
 
-  package { 'httpd':
-    ensure => installed,
-    name   => $apache::params::apache_name,
-  }
-
-  # true/false is sufficient for both ensure and enable
-  validate_bool($service_enable)
-
-  service { 'httpd':
-    ensure    => $service_enable,
-    name      => $apache::params::apache_name,
-    enable    => $service_enable,
-    subscribe => Package['httpd'],
-  }
-
-  file { 'httpd_vdir':
-    ensure  => directory,
-    path    => $apache::params::vdir,
-    recurse => true,
-    purge   => true,
-    notify  => Service['httpd'],
-    require => Package['httpd'],
-  }
-
-  if $apache::params::conf_dir and $apache::params::conf_file {
-    # Template uses:
-    # - $apache::params::user
-    # - $apache::params::group
-    # - $apache::params::conf_dir
-    # - $serveradmin
-    file { "${apache::params::conf_dir}/${apache::params::conf_file}":
-      ensure  => present,
-      content => template("apache/${apache::params::conf_file}.erb"),
-      notify  => Service['httpd'],
-      require => Package['httpd'],
+    package { 'httpd':
+        name   => 'apache2',
+        ensure => installed,
     }
-    if $default_mods == true {
-      include apache::mod::default
+
+    service { 'httpd':
+        name      => 'apache2',
+        ensure    => $service_enable,
+        enable    => $service_enable,
+        subscribe => Package['httpd'],
     }
-  }
-  if $apache::params::mod_dir {
-    file { $apache::params::mod_dir:
-      ensure  => directory,
-      require => Package['httpd'],
-    } -> A2mod <| |>
-    resources { 'a2mod':
-      purge => true,
+
+    file { 'http_vdir':
+        path    => '/etc/apache2/sites-enabled/',
+        ensure  => directory,
+        recurse => true,
+        purge   => true,
+        notify  => Service['httpd'],
+        require => Package['httpd'],
     }
-  }
 }
