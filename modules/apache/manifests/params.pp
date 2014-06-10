113dc97b7521cdf67cd715b1237e8c21c43c6094
Ori Livneh <ori@wikimedia.org>
2014-06-10 12:42:00 -0700

    apache: clean up init.pp & params.pp
    * Remove all the RedHat/EC2-specific configurations & logic
    * Replace variable package, service and vhost dirs with their literal value on
      Debian-based systems.
    * Keep the resource names the same for now, so references to Package['httpd'] /
      Service['httpd'] continue working as before.
    
    Change-Id: I419abaf4d48bfef0c5c66e3ee4b518bb7ef9e51c

--- modules/apache/manifests/params.pp
+++ modules/apache/manifests/params.pp
@@ -31,66 +31,28 @@ class apache::params {
   $override      = 'None'
   $vhost_name    = '*'
 
-  if $::osfamily == 'redhat' or $::operatingsystem == 'amazon' {
-    $user                  = 'apache'
-    $group                 = 'apache'
-    $apache_name           = 'httpd'
-    $php_package           = 'php'
-    $mod_passenger_package = 'mod_passenger'
-    $mod_python_package    = 'mod_python'
-    $mod_wsgi_package      = 'mod_wsgi'
-    $mod_auth_kerb_package = 'mod_auth_kerb'
-    $ssl_package           = 'mod_ssl'
-    $apache_dev            = 'httpd-devel'
-    $httpd_dir             = '/etc/httpd'
-    $conf_dir              = "${httpd_dir}/conf"
-    $mod_dir               = "${httpd_dir}/mod.d"
-    $vdir                  = "${httpd_dir}/conf.d"
-    $conf_file             = 'httpd.conf'
-    $mod_packages          = {
-      'dev'        => 'httpd-devel',
-      'fcgid'      => 'mod_fcgid',
-      'passenger'  => 'mod_passenger',
-      'perl'       => 'mod_perl',
-      'php5'       => 'php',
-      'proxy_html' => 'mod_proxy_html',
-      'python'     => 'mod_python',
-      'ssl'        => 'mod_ssl',
-      'wsgi'       => 'mod_wsgi',
-      'shibboleth' => 'shibboleth',
-    }
-    $mod_libs              = {
-      'php5' => 'libphp5.so',
-    }
-    $mod_identifiers       = {
-      'shibboleth' => 'mod_shib',
-    }
-  } elsif $::osfamily == 'debian' {
-    $user                  = 'www-data'
-    $group                 = 'www-data'
-    $apache_name           = 'apache2'
-    $php_package           = 'libapache2-mod-php5'
-    $mod_passenger_package = 'libapache2-mod-passenger'
-    $mod_python_package    = 'libapache2-mod-python'
-    $mod_wsgi_package      = 'libapache2-mod-wsgi'
-    $mod_uwsgi_package     = 'libapache2-mod-uwsgi'
-    $mod_auth_kerb_package = 'libapache2-mod-auth-kerb'
-    $apache_dev            = ['libaprutil1-dev', 'libapr1-dev', 'apache2-prefork-dev']
-    $vdir                  = '/etc/apache2/sites-enabled/'
-    $proxy_modules         = ['proxy', 'proxy_http']
-    $mod_packages          = {
-      'dev'        => ['libaprutil1-dev', 'libapr1-dev', 'apache2-prefork-dev'],
-      'fcgid'      => 'libapache2-mod-fcgid',
-      'passenger'  => 'libapache2-mod-passenger',
-      'perl'       => 'libapache2-mod-perl2',
-      'php5'       => 'libapache2-mod-php5',
-      'proxy_html' => 'libapache2-mod-proxy-html',
-      'python'     => 'libapache2-mod-python',
-      'wsgi'       => 'libapache2-mod-wsgi',
-    }
-    $mod_libs              = {}
-    $mod_identifiers       = {}
-  } else {
-    fail("Class['apache::params']: Unsupported operatingsystem: $operatingsystem")
+  $user                  = 'www-data'
+  $group                 = 'www-data'
+  $apache_name           = 'apache2'
+  $php_package           = 'libapache2-mod-php5'
+  $mod_passenger_package = 'libapache2-mod-passenger'
+  $mod_python_package    = 'libapache2-mod-python'
+  $mod_wsgi_package      = 'libapache2-mod-wsgi'
+  $mod_uwsgi_package     = 'libapache2-mod-uwsgi'
+  $mod_auth_kerb_package = 'libapache2-mod-auth-kerb'
+  $apache_dev            = ['libaprutil1-dev', 'libapr1-dev', 'apache2-prefork-dev']
+  $vdir                  = '/etc/apache2/sites-enabled/'
+  $proxy_modules         = ['proxy', 'proxy_http']
+  $mod_packages          = {
+    'dev'        => ['libaprutil1-dev', 'libapr1-dev', 'apache2-prefork-dev'],
+    'fcgid'      => 'libapache2-mod-fcgid',
+    'passenger'  => 'libapache2-mod-passenger',
+    'perl'       => 'libapache2-mod-perl2',
+    'php5'       => 'libapache2-mod-php5',
+    'proxy_html' => 'libapache2-mod-proxy-html',
+    'python'     => 'libapache2-mod-python',
+    'wsgi'       => 'libapache2-mod-wsgi',
   }
+  $mod_libs              = {}
+  $mod_identifiers       = {}
 }
