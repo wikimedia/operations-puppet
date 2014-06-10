3f6bae544a5ab8c18db85f6357b6a463bd09d296
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-06-10 12:24:01 +0200

    check_git_merge: fix execution switches and permissions
    Explicitly set the execution switches for the bash script.
    Also made nrpe run with correct permissions as it will need to
    impersonate the git user.
    
    Change-Id: I517abe0b9a30e700db01c94dc70275830423c3fc
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/monitoring/manifests/icinga/git_merge.pp
+++ modules/monitoring/manifests/icinga/git_merge.pp
@@ -6,18 +6,28 @@ define monitoring::icinga::git_merge (
     $user     = 'gitpuppet',
     $warning  = 600,
     $critical = 900
-) {
-      file { "check_${title}_needs_merge":
-          ensure  => present,
-          path    => "/usr/local/lib/nagios/plugins/check_${title}-needs-merge",
-          owner   => root,
-          group   => root,
-          mode    => '0555',
-          content => template('monitoring/check_git-needs-merge.erb')
-      }
-      nrpe::monitor_service { "${title}_merged":
-          description  => "Unmerged changes on repository ${title}",
-          nrpe_command => "/usr/local/lib/nagios/plugins/check_${title}-needs-merge",
-          require      => File["check_${title}_needs_merge"]
-      }
+    ) {
+
+    $sane_title = regsubst($title, '\W', '_', 'G')
+    $filename = "/usr/local/lib/nagios/plugins/check_${sane_title}-needs-merge"
+    $file_resource = "check_${sane_title}_needs_merge"
+
+    file { $file_resource:
+        ensure  => present,
+        path    => $filename,
+        owner   => root,
+        group   => root,
+        mode    => '0555',
+        content => template('monitoring/check_git-needs-merge.erb')
+    }
+
+    nrpe::monitor_service { "${sane_title}_merged":
+        description  => "Unmerged changes on repository ${title}",
+        nrpe_command => "/usr/bin/sudo ${filename}",
+        require      => File[$file_resource]
+    }
+
+    sudo_user { 'nagios':
+        privileges   => ["ALL = NOPASSWD: ${filename}"],
+    }
 }
