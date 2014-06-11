394129381739834e159035b5236e100e61bd6239
Brandon Black <bblack@wikimedia.org>
2014-06-11 13:50:23 -0500

    Add XPS support to interface-rps.py
    ... but only for bnx2x with cards approximately like ours with
    drivers that work for XPS and that aren't using iSCSI or FCoE.
    
    Change-Id: Iefadd2ef5f2318d5b578cd22654320141dd8ffa4

--- modules/interface/files/interface-rps.py
+++ modules/interface/files/interface-rps.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 
-# Sets up Receive Packet Steering (RPS) for a given interface.
+# Sets up scalable network stuff (RPS/RSS/XPS) for a given interface.
 #
 # Tries to allocate separate queues to separate CPUs, rather than follow
 # what's common advice out there (all CPUs to all queues), as experience has
 # shown a tremendous difference.
 #
-# Also sets up matching Receive Side Scaling (RSS) IRQ affinities if
+# Optionally sets up matching Receive Side Scaling (RSS) IRQ affinities if
 # given a second parameter for lookups in /proc/interrupts.  e.g. for
 # bnx2x, this would be "eth0-fp-%d".
+#
+# Sets up matching Transmit Packet Steering (XPS) queues only
+# for cards driven by bnx2x which appear to have a set of tx queues that
+# match up with 3x CoS bands multiplied by the rx queue count.
+# This is the behavior exhibited by current bnx2x drivers that have
+# working XPS implementations (e.g. in the Ubuntu 3.13.0-30 kernel), on
+# our hardware and config.  The CoS band count could vary on different
+# bnx2x cards, and depending on whether you're using stuff like iSCSI/FCoE.
+# I don't yet know of a way to simply query the CoS band count or tx queue
+# mapping directly at runtime from the driver.
+#
+# Different cards/drivers will likely have different XPS mappings that will
+# need to be addressed individually when we encounter them.
 #
 # Authors: Faidon Liambotis and Brandon Black
 # Copyright (c) 2013-2014 Wikimedia Foundation, Inc.
@@ -17,6 +30,7 @@ import os
 import glob
 import sys
 import re
+import warnings
 
 
 def get_value(path):
@@ -48,13 +62,32 @@ def get_cpu_list():
     return sorted(set(cores))
 
 
-def get_rx_queues(device):
-    """Get a list of RX queues for device"""
-    rx_nodes = glob.glob(os.path.join('/sys/class/net', device, 'queues',
-                                      'rx-*'))
-    rx_queues = [int(os.path.basename(q)[3:]) for q in rx_nodes]
+def get_queues(device, qtype):
+    """Get a list of rx or tx queues for device"""
+    nodes = glob.glob(os.path.join('/sys/class/net', device, 'queues',
+                                   '%s-*' % qtype))
+    queues = [int(os.path.basename(q)[3:]) for q in nodes]
 
-    return sorted(rx_queues)
+    return sorted(queues)
+
+
+def get_bnx2x_cos_queue_map(tx_queues, rx_queues):
+    """Map tx:rx queues based on 3x CoS bands, if things look sane"""
+
+    # bnx2x with working XPS should have 3 tx queues for every rx queue,
+    #  representing CoS bands.  So for example if there are 4 rx queues,
+    #  the correct mapping will be 12 tx queues with mapping:
+    #  { 0: [0, 4, 8], 1: [1, 5, 9], 2: [2, 6, 10], 3: [3, 7, 11] }
+
+    cos_bands = 3
+    if len(rx_queues) * cos_bands != len(tx_queues):
+        warnings.warn('bnx2x XPS CoS queue map invalid (upgrade driver?)')
+        return None
+
+    tx_qmap = {}
+    for rxq in rx_queues:
+        tx_qmap[rxq] = [rxq + (c * len(rx_queues)) for c in range(cos_bands)]
+    return tx_qmap
 
 
 def get_rx_irqs(rss_pattern, rx_queues):
@@ -82,23 +115,28 @@ def get_rx_irqs(rss_pattern, rx_queues):
     return irqs
 
 
-def assign_rx_queue_to_cpus(device, cpus, rx_queue, rx_irq):
-    """Assign a device's RX queue to a CPU set"""
+def set_cpus(device, cpus, rxq, rx_irq, txqs):
+    """Assign a device's matching set of [rt]x queues and IRQ to a CPU set"""
     bitmask = 0
     for cpu in cpus:
         bitmask += 2**cpu
-
-    rx_node = os.path.join('/sys/class/net', device, 'queues',
-                           'rx-%s' % rx_queue, 'rps_cpus')
-    write_value(rx_node, format(bitmask, 'x'))
+    txt_bitmask = format(bitmask, 'x')
 
     if rx_irq:
-            irq_node = '/proc/irq/%s/smp_affinity' % rx_irq
-            write_value(irq_node, format(bitmask, 'x'))
+        irq_node = '/proc/irq/%s/smp_affinity' % rx_irq
+        write_value(irq_node, txt_bitmask)
+
+    rx_node = '/sys/class/net/%s/queues/rx-%s/rps_cpus' % (device, rxq)
+    write_value(rx_node, txt_bitmask)
+
+    if txqs:
+        for i in txqs:
+            tx_node = '/sys/class/net/%s/queues/tx-%s/xps_cpus' % (device, i)
+            write_value(tx_node, txt_bitmask)
 
 
-def distribute_rx_queues_to_cpus(device, cpu_list, rx_queues, rx_irqs):
-    """Performs a smart distribution of RX queues to CPUs (or vice-versa)"""
+def dist_queues_to_cpus(device, cpu_list, rx_queues, rx_irqs, tx_qmap):
+    """Smart distribution of queues + IRQs to CPUs (or vice-versa)"""
     if len(rx_queues) >= len(cpu_list):
         # try to divide queues / CPUs and assign N CPUs per queue, isolated
         (quot, rem) = divmod(len(rx_queues), len(cpu_list))
@@ -106,7 +144,7 @@ def distribute_rx_queues_to_cpus(device, cpu_list, rx_queues, rx_irqs):
         for i, cpu in enumerate(cpu_list):
             for j in range(quot):
                 rxq = rx_queues[i*quot + j]
-                assign_rx_queue_to_cpus(device, [cpu], rxq, rx_irqs[rxq])
+                set_cpus(device, [cpu], rxq, rx_irqs[rxq], tx_qmap[rxq])
 
         # if there are remainder queues, split CPU list into rem subgroups
         # (with trailing remainder of CPUs left out), one per queue
@@ -114,7 +152,7 @@ def distribute_rx_queues_to_cpus(device, cpu_list, rx_queues, rx_irqs):
             cquot = len(cpu_list)/rem
             for i, rxq in enumerate(rx_queues[-rem:]):
                 cpu_sublist = cpu_list[i * cquot:(i + 1) * cquot]
-                assign_rx_queue_to_cpus(device, cpu_sublist, rxq, rx_irqs[rxq])
+                set_cpus(device, cpu_sublist, rxq, rx_irqs[rxq], tx_qmap[rxq])
 
     else:
         # do the opposite division
@@ -125,7 +163,7 @@ def distribute_rx_queues_to_cpus(device, cpu_list, rx_queues, rx_irqs):
             cpus = []
             for j in range(quot):
                 cpus.append(cpu_list[i*quot + j])
-            assign_rx_queue_to_cpus(device, cpus, rxq, rx_irqs[rxq])
+            set_cpus(device, cpus, rxq, rx_irqs[rxq], tx_qmap[rxq])
 
 
 def main():
@@ -141,17 +179,26 @@ def main():
         rss_pattern = None
 
     cpu_list = get_cpu_list()
-    rx_queues = get_rx_queues(device)
+    rx_queues = get_queues(device, 'rx')
+    tx_queues = get_queues(device, 'tx')
+    driver = os.path.basename(
+        os.readlink('/sys/class/net/%s/device/driver/module' % device)
+    )
 
     if rss_pattern:
         if rss_pattern.count('%') != 1 or rss_pattern.count('%d') != 1:
             raise Exception('The RSS pattern must contain a single %d')
         rx_irqs = get_rx_irqs(rss_pattern, rx_queues)
     else:
-        # fill in a dict of None to simplify distribution code
         rx_irqs = {rxq: None for rxq in rx_queues}
 
-    distribute_rx_queues_to_cpus(device, cpu_list, rx_queues, rx_irqs)
+    tx_queue_map = None
+    if driver == 'bnx2x':
+        tx_queue_map = get_bnx2x_cos_queue_map(tx_queues, rx_queues)
+    if tx_queue_map is None:
+        tx_queue_map = {rxq: None for rxq in rx_queues}
+
+    dist_queues_to_cpus(device, cpu_list, rx_queues, rx_irqs, tx_queue_map)
 
 if __name__ == '__main__':
     main()
