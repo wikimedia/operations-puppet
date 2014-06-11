da3caa80023e6f999a90d2f0bc59f12e16fe32a0
Ori Livneh <ori@wikimedia.org>
2014-06-10 18:47:29 -0700

    apache: replace apache::mod::* hierarchy with simpler equivalent from MWV
    Change-Id: Iecfa44c925b533dbdc8724a6aa83f924844bfbdd

--- modules/apache/manifests/mod/dev.pp
+++
@@ -1,8 +0,0 @@
-class apache::mod::dev inherits apache::params {
-  # Development packages have no mod to load
-  $packages = $apache::params::mod_packages['dev']
-  package { $packages:
-    ensure  => present,
-    require => Package['httpd'],
-  }
-}
