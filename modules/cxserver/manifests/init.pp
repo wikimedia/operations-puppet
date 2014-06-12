5aba49cdc2412c1f2f5c53530eff8f92868ce9ae
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-06-12 10:46:43 +0000

    cxserver configuration for beta labs
    Based on parsoid configuration. Done together with Kartik Mistry.
    
    Change-Id: I40cf23a6b0d9f8c50447d9e4b77235266e6ffccf

---
+++ modules/cxserver/manifests/init.pp
@@ -0,0 +1,62 @@
+# == Class: cxserver
+#
+# cxserver is a node.js backend for the content translation tool.
+# https://www.mediawiki.org/wiki/Content_translation
+#
+# === Parameters
+#
+# [*base_path*]
+#   Path to the cxserver code.
+# [*node_path*]
+#   Path to the node modules cxserver depends on.
+# [*cxserver_log_dir*]
+#   Place where cxserver can put log files. Assumed to be already existing and
+#   have write access to cxserver user.
+# [*log_file*]
+#   Path where cxserver logs output from console.
+#
+class cxserver(
+    $base_path,
+    $node_path,
+    $cxserver_log_dir,
+    $log_file
+) {
+    # apertium-* packages are for machine translation.
+    # dictd-* packages for dictionary server.
+    package { ['nodejs',
+               'apertium',
+               'apertium-es-ca',
+               'dictd',
+               'dict-freedict-eng-spa',
+               'dict-freedict-spa-eng',
+               'dict-freedict-eng-hin'
+              ]:
+        ensure => present,
+    }
+
+    # The upstart configuration
+    file { '/etc/init/cxserver.conf':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0444',
+        content => template('cxserver/upstart.erb'),
+    }
+
+    file { '/etc/logrotate.d/cxserver':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0444',
+        content => template('cxserver/logrotate.erb'),
+    }
+
+    service { 'cxserver':
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        provider   => 'upstart',
+        require    => File[$cxserver_log_dir],
+        subscribe  => File['/etc/init/cxserver.conf'],
+    }
+}
