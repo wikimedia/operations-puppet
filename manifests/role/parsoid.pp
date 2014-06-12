afdda75184968e7eba4d0b4f8c49967b46583fde
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-06-12 10:46:43 +0000

    cxserver configuration for beta labs
    Based on parsoid configuration. Done together with Kartik Mistry.
    
    Change-Id: I40cf23a6b0d9f8c50447d9e4b77235266e6ffccf

--- manifests/role/parsoid.pp
+++ manifests/role/parsoid.pp
@@ -226,19 +226,14 @@ class role::parsoid::beta {
         proto => 'tcp',
         port  => '8000'
     }
-    ferm::rule { 'ssh-from-beta-bastion':
-        rule => 'proto tcp dport ssh { saddr 10.4.0.58 ACCEPT; }',
-    }
+
+    # And thus allow ssh access from the Jenkins master (gallium) and from beta bastion
+    include contint::firewall::labs
+    include beta::firewall::bastionssh
 
     # Instance got to be a Jenkins slave so we can update Parsoid whenever a
     # change is made on mediawiki/services/parsoid repository
     include role::ci::slave::labs::common
     # Also need the slave scripts for multi-git.sh
     include contint::slave-scripts
-
-    # And thus allow ssh access from the Jenkins master (gallium)
-    ferm::rule { 'ssh-from-gallium':
-        rule => 'proto tcp dport ssh { saddr 208.80.154.135 ACCEPT; }',
-    }
-
 }
