3e621217377084a2472ba0f8e9215d6bc94f5a21
dzahn <dzahn@wikimedia.org>
2014-06-13 12:52:08 -0700

    mirror.pp - retab, unquoted resource titles, lint
    Change-Id: Icfc99ff978837b157d3c6fe23ba45ff1ba41c08a

--- manifests/role/mirror.pp
+++ manifests/role/mirror.pp
@@ -3,32 +3,33 @@
 
 class role::mirror::common {
 
-	package { rsync:
-		ensure => latest;
-	}
+    package { 'rsync':
+        ensure => latest;
+    }
 
-	include vm::higher_min_free_kbytes
+    include vm::higher_min_free_kbytes
 }
 
 class role::mirror::media {
-	include role::mirror::common
+    include role::mirror::common
 
-	system::role { "role::mirror::media": description => "Media mirror (rsync access for external mirrors)" }
+    system::role { 'role::mirror::media':
+        description => 'Media mirror (rsync access for external mirrors)'
+    }
 
-	file {
-		'/root/backups/rsync-media-cron.sh':
-			mode => 0755,
-			source => "puppet:///files/misc/mirror/rsync-media-cron.sh",
-			ensure => present;
-	}
+    file { '/root/backups/rsync-media-cron.sh':
+        ensure => present,
+        mode   => '0755',
+        source => 'puppet:///files/misc/mirror/rsync-media-cron.sh',
 
-	cron {
-		'media_rsync':
-			user => root,
-			minute => '20',
-			hour => '3',
-			command => '/root/backups/rsync-media-cron.sh',
-			environment => 'MAILTO=ops-dumps@wikimedia.org',
-			ensure => present;
-	}
+    }
+
+    cron { 'media_rsync':
+        ensure      => present,
+        user        => 'root',
+        minute      => '20',
+        hour        => '3',
+        command     => '/root/backups/rsync-media-cron.sh',
+        environment => 'MAILTO=ops-dumps@wikimedia.org',
+    }
 }
