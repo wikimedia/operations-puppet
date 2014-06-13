1dcb0de86a578c8bc3ab9f562f673506df5b4bdc
milimetric <dandreescu@wikimedia.org>
2014-06-13 18:07:37 -0400

    Enable the new backup role in wikimetrics if set
    Whether backup will initalize depends on the backup
    environment variable which should only be set in production.
    
    Bug: 66119
    Change-Id: Ie6199de5d03146cf4955e89be0b87ffd331a7b0f

--- manifests/role/wikimetrics.pp
+++ manifests/role/wikimetrics.pp
@@ -61,6 +61,11 @@ class role::wikimetrics {
     $meta_mw_consumer_key  = $::passwords::wikimetrics::meta_mw_consumer_key
     $meta_mw_client_secret = $::passwords::wikimetrics::meta_mw_client_secret
 
+    # base directory settings
+    $var_directory       = '/var/lib/wikimetrics'
+    $public_subdirectory = 'public'
+    $public_directory    = "$var_directory/$public_subdirectory"
+
     # Run as daemon python process or in Apache WSGI.
     $web_mode = $::wikimetrics_web_mode ? {
         undef   => 'apache',
@@ -154,6 +159,9 @@ class role::wikimetrics {
         # where this table is called 'revision_userindex'.
         # The mediawiki database usually calls this 'revision'.
         revision_tablename    => 'revision_userindex',
+
+        var_directory         => $var_directory,
+        public_subdirectory   => $public_subdirectory,
     }
 
     # Run the wikimetrics/scripts/install script
@@ -181,7 +189,14 @@ class role::wikimetrics {
         }
     }
 
+    # Install redis and use a custom config template.
+    # Wikimetrics needs redis to save data for longer
+    # than the default redis.conf.erb template allows.
+    $redis_dir        = '/a/redis'
+    $redis_dbfilename = "$hostname-6379.rdb"
     class { '::redis':
+        dir                         => $redis_dir,
+        dbfilename                  => $redis_dbfilename,
         saves                       => [ "900 1", "300 10", "60 20" ],
         stop_writes_on_bgsave_error => true,
     }
@@ -202,5 +217,16 @@ class role::wikimetrics {
 
     class { '::wikimetrics::scheduler':
         require => Exec['install_wikimetrics_dependencies'],
+    }
+
+    # backup regardless of whether we are in debug mode or not
+    if $::wikimetrics_backup {
+        class { '::wikimetrics::backup':
+            destination   => '/data/project/wikimetrics/backup',
+            db_name       => $db_name_wikimetrics,
+            redis_db_file => "$redis_dir/$redis_dbfilename",
+            public_files  => $public_directory,
+            keep_days     => 10,
+        }
     }
 }
