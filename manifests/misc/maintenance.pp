79b721d77cfac4e92a4f8f501b4717b64aaeea1c
withoutaname <drevitchi@gmail.com>
2014-06-14 00:57:08 -0700

    Remove dependency on echowikis.dblist
    In preparation for Ibde4c0c1. This script should now be runnable without the dblist.
    
    Bug: T59375
    Change-Id: I741d2485e83433ebfbfcb55d17ab026f7dd7ca16

--- manifests/misc/maintenance.pp
+++ manifests/misc/maintenance.pp
@@ -116,7 +116,7 @@ class misc::maintenance::tor_exit_node( $ensure = present ) {
 class misc::maintenance::echo_mail_batch( $ensure = present ) {
     cron { 'echo_mail_batch':
         ensure  => $ensure,
-        command => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/echowikis.dblist extensions/Echo/maintenance/processEchoEmailBatch.php >/dev/null',
+        command => '/usr/local/bin/foreachwiki extensions/Echo/maintenance/processEchoEmailBatch.php >/dev/null',
         user    => $::mediawiki::users::web,
         minute  => 0,
         hour    => 0,
