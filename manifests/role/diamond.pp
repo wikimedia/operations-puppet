7b285c73e3fdf7f0a4179288bdf65d8da691bac6
cpettet <rush@wikimedia.org>
2014-06-16 15:40:46 -0500

    diamond trusty disable default IPVS
    Diamond seems to want to make IPVS a default
    collector in trust.
    
    Change-Id: I4dc577c28dad06638ac6c04e7b2f583806327819

--- manifests/role/diamond.pp
+++ manifests/role/diamond.pp
@@ -9,12 +9,27 @@ class role::diamond {
     #accept multiple metrics
 
     if $::realm == 'production' {
+
         class { '::diamond':
             settings => {
                 enabled => 'true',
                 host    => '10.64.0.18', # tungsten
                 port    => '8125',
             },
+        }
+
+        #IPVS collector seems to be enabled by default on trusty
+        #causes non LVS hosts to spam with sudo violations for
+        #stats collection among other things.  Explicit disable
+        #for now this needs to dealt with upstream.
+        case $::operatingsystemrelease {
+            '14.04': {
+                diamond::collector { 'IPVS':
+                    settings => {
+                        enabled => 'false',
+                    },
+                }
+            }
         }
     }
 }
