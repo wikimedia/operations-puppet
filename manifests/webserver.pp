59408eb0c83e3f68e3c3f0d34f9bc9700468f04e
Ori Livneh <ori@wikimedia.org>
2014-06-16 21:46:34 -0700

    delete webserver::apache2::rpaf
    Usage can be replaced with apache::mod::rpaf
    
    Change-Id: Ie1a7f727697bd176e95c4a7327643f20895b15a8

--- manifests/webserver.pp
+++ manifests/webserver.pp
@@ -130,20 +130,6 @@ class webserver::php5-gd {
     }
 }
 
-class webserver::apache2::rpaf {
-    # NOTE: rpaf.conf defaults to just 127.0.01 - may need to
-    # modify to include squid/varnish/nginx ranges depending
-    # on use.
-    package { 'libapache2-mod-rpaf':
-        ensure => 'present',
-    }
-    apache_module { 'rpaf':
-        name    => 'rpaf',
-        require => Package['libapache2-mod-rpaf'],
-    }
-}
-
-
 # New style attempt at handling misc web servers
 # - keep independent from the existing stuff
 
