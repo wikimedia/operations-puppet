15aa54a1f97786d9cd97e2f1d54d7d7a461e5a03
Ori Livneh <ori@wikimedia.org>
2014-06-16 21:42:33 -0700

    delete webserver::apache2
    Also unused.
    
    Change-Id: I9c769174c5b043ad95d895df1640e4b3e1b2f6ab

--- manifests/webserver.pp
+++ manifests/webserver.pp
@@ -130,26 +130,6 @@ class webserver::php5-gd {
     }
 }
 
-#  Install the 'apache2' package
-class webserver::apache2 {
-
-    include webserver::base
-
-    package { 'apache2':
-        ensure => 'present',
-    }
-
-    # ensure default site is removed
-    apache_site { '000-default':
-        ensure => 'absent',
-        name   => '000-default',
-    }
-    apache_site { '000-default-ssl':
-        ensure => 'absent',
-        name   => '000-default-ssl',
-    }
-}
-
 class webserver::apache2::rpaf {
     # NOTE: rpaf.conf defaults to just 127.0.01 - may need to
     # modify to include squid/varnish/nginx ranges depending
