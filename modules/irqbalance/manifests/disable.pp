65e1c3b955f8c35c8af42a42e2c6b4cf738ddf15
Brandon Black <bblack@wikimedia.org>
2014-06-18 10:43:38 -0500

    *really* disable irqbalance if doing RSS
    Change-Id: I2f871659990efcd0370454682fb7424cf0a03099

--- modules/irqbalance/manifests/disable.pp
+++ modules/irqbalance/manifests/disable.pp
@@ -4,4 +4,13 @@ class irqbalance::disable {
         enable => false,
         ensure => stopped,
     }
+
+    # Apparently puppet can't actually disable this upstart service,
+    #  but this is a decent workaround
+    file { "/etc/default/irqbalance":
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/irqbalance/irqbalance.disabled.default',
+    }
 }
