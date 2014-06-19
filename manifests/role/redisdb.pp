71495548d4c28b7c52b47fe59ef2b2f9b7cb83d3
Matanya Moses <matanya@foss.co.il>
2014-06-19 10:56:46 +0300

    redisdb: lint
    Change-Id: I142e2fa16650b1a47ca1a29c37895462a7860ddc

--- manifests/role/redisdb.pp
+++ manifests/role/redisdb.pp
@@ -2,40 +2,46 @@
 # db::redis
 
 # Virtual resource for the monitoring server
-@monitor_group { "redis_eqiad": description => "eqiad Redis" }
-@monitor_group { "redis_pmtpa": description => "pmtpa Redis" }
+@monitor_group { 'redis_eqiad':
+    description => 'eqiad Redis',
+}
+@monitor_group { 'redis_pmtpa':
+    description => 'pmtpa Redis',
+}
 
 class role::db::redis (
-	$maxmemory = inline_template("<%= (Float(memorysize.split[0]) * 0.82).round %>Gb"),
-	$redis_replication = undef
+    $maxmemory = inline_template("<%= (Float(memorysize.split[0]) * 0.82).round %>Gb"),
+    $redis_replication = undef
 ) {
 
-	system::role { "db::redis": description => "Redis server" }
+    system::role { 'db::redis':
+        description => 'Redis server',
+    }
 
-	include standard,
-		passwords::redis
+    include standard
+    include passwords::redis
 
-	if $::realm == "production" {
+    if $::realm == 'production' {
 
-		class { "::redis":
-			maxmemory => $maxmemory,
-			persist => "aof",
-			redis_replication => $redis_replication,
-			password => $passwords::redis::main_password,
-		}
+        class { '::redis':
+            maxmemory         => $maxmemory,
+            persist           => 'aof',
+            redis_replication => $redis_replication,
+            password          => $passwords::redis::main_password,
+        }
 
-		include redis::ganglia
-	}
+        include redis::ganglia
+    }
 
-	if $::realm == "labs" {
+    if $::realm == 'labs' {
 
-		class { "::redis":
-			maxmemory		  => "500mb",
-			persist			  => "aof",
-			redis_replication	  => undef,
-			password		  => $::passwords::redis::main_password,
-			dir			  => "/var/lib/redis/",
-			auto_aof_rewrite_min_size => "64mb",
-		}
-	}
+        class { '::redis':
+            maxmemory                 => '500mb',
+            persist                   => 'aof',
+            redis_replication         => undef,
+            password                  => $::passwords::redis::main_password,
+            dir                       => '/var/lib/redis/',
+            auto_aof_rewrite_min_size => '64mb',
+        }
+    }
 }
