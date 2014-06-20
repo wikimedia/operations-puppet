e19a4be7ea3e18987615d6efb573a248ed649fc4
dzahn <dzahn@wikimedia.org>
2014-06-20 10:39:16 -0700

    Add puppet module for a tor relay (WIP)
    RT: 7667
    Change-Id: Ifcc6070f6ad62e5dbc03a09e4e6df6e0ae973532

---
+++ modules/tor/manifests/init.pp
@@ -0,0 +1,31 @@
+# sets up a Tor relay
+class tor (
+    $tor_address,
+    $tor_nickname,
+    $tor_contact,
+    $tor_controlport = '9051',
+    $tor_orport = '9001',
+    $tor_dirport = '9030',
+    $tor_exit_policy = 'reject *:*',
+    ) {
+
+    package { 'tor':
+        ensure => 'present',
+    }
+
+    file { '/etc/tor/torrc':
+        ensure  => 'present',
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => template('tor/torrc.erb'),
+        notify  => Service['tor'],
+        require => Package['tor'],
+    }
+
+    service { 'tor':
+        ensure  => 'running',
+        require => Package['tor'],
+    }
+
+}
