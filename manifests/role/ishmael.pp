d12009ada5266bdea6e2394ababd5065578b7f36
Marius Hoch <hoo@online.de>
2014-06-20 03:38:30 +0200

    Allow ldap "wmde" users to access ishmael
    Untested
    
    Change-Id: I9853b77a8f2b38f3aedc6c2c82a279f7510ed255

--- manifests/role/ishmael.pp
+++ manifests/role/ishmael.pp
@@ -13,7 +13,7 @@ class role::ishmael {
         docroot       => '/srv/ishmael',
         ldap_binddn   => 'cn=proxyagent,ou=profile,dc=wikimedia,dc=org',
         ldap_authurl  => 'ldaps://virt0.wikimedia.org virt1000.wikimedia.org/ou=people,dc=wikimedia,dc=org?cn',
-        ldap_group    => 'cn=wmf,ou=groups,dc=wikimedia,dc=org',
+        ldap_groups   => [ 'cn=wmf,ou=groups,dc=wikimedia,dc=org', 'cn=wmde,ou=groups,dc=wikimedia,dc=org' ],
         auth_name     => 'WMF Labs (use wiki login name not shell)',
     }
 
