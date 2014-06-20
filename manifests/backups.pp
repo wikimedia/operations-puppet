30412c110a8f5ddb57e6e032d147c0daed89ac36
dzahn <dzahn@wikimedia.org>
2014-06-20 10:14:53 -0700

    lint backups.pp - part 1
    Change-Id: Icebbef7ab4a0e55908ef9055544be6ebcf071211

--- manifests/backups.pp
+++ manifests/backups.pp
@@ -73,7 +73,7 @@ define backup::mysqlset($method='bpipe',
     }
     if $method == 'predump' {
         $extras = {
-                'ClientRunBeforeJob' => '/etc/bacula/scripts/predump',
+            'ClientRunBeforeJob' =>  '/etc/bacula/scripts/predump',
         }
         $basefileset = regsubst(regsubst($local_dump_dir,'/',''),'/','-','G')
         $fileset = "mysql-${basefileset}"
@@ -126,8 +126,13 @@ define backup::schedule($pool) {
     }
 
     bacula::director::jobdefaults { "Monthly-1st-${name}-${pool}":
+<<<<<<< HEAD
         when => "Monthly-1st-${name}",
         pool => $pool,
+=======
+        when        => "Monthly-1st-${name}",
+        pool        => $pool,
+>>>>>>> 2b185bd... lint backups.pp - part 1
     }
 
 }
@@ -142,8 +147,13 @@ define backup::weeklyschedule($pool) {
     }
 
     bacula::director::jobdefaults { "Weekly-${name}-${pool}":
+<<<<<<< HEAD
         when => "Weekly-${name}",
         pool => $pool,
+=======
+        when        => "Weekly-${name}",
+        pool        => $pool,
+>>>>>>> 2b185bd... lint backups.pp - part 1
     }
 }
 
@@ -202,6 +212,64 @@ class backup::server {
         monthday => 1;
     }
 
+<<<<<<< HEAD
+=======
+    package { [ 'amanda-common', 'amanda-server' ]:
+        ensure => latest;
+    }
+
+    file {
+        '/var/backups/.ssh/authorized_keys':
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0600',
+            source  =>  'puppet:///private/backup/ssh-keys/authorized_keys_server';
+
+        '/var/backups/.ssh/amanda_restore':
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0600',
+            source  =>  'puppet:///private/backup/ssh-keys/amanda_restore';
+        '/etc/amanda/create_vtapes.sh':
+            owner   =>  'root',
+            group   =>  'root',
+            mode    =>  '0700',
+            source  =>  'puppet:///files/backup/create_vtape.sh';
+    }
+    File <<| tag == 'backup_host' |>>
+
+    #service {
+    #   xinetd:
+    #       ensure  =>  stopped;
+    #}
+
+
+    cron {
+        'amanda_daily':
+        command =>  '/usr/sbin/amdump Wikimedia-Daily',
+        require =>  Package['amanda-server'],
+        user    =>  'backup',
+        hour    =>  2,
+        minute  =>  0;
+
+        'amanda_weekly':
+        command =>  '/usr/sbin/amdump Wikimedia-Weekly',
+        require =>  Package['amanda-server'],
+        user    =>  'backup',
+        hour    =>  6,
+        minute  =>  0,
+        weekday =>  'Sunday';
+
+        'amanda_monthly':
+        command =>  '/usr/sbin/amdump Wikimedia-Monthly',
+        require =>  Package['amanda-server'],
+        user    =>  'backup',
+        hour    =>  12,
+        minute  =>  0,
+        monthday => 1;
+    }
+
+>>>>>>> 2b185bd... lint backups.pp - part 1
     include backup::server::daily
     include backup::server::weekly
     include backup::server::monthly
@@ -209,6 +277,7 @@ class backup::server {
 }
 
 class backup::server::daily {
+<<<<<<< HEAD
     $mailto_account  = "ops@wikimedia.org"
     $cycle_type      = "Wikimedia-Daily"
     $vtapes_location = "/data/amanda/Wikimedia-Daily"
@@ -274,11 +343,80 @@ class backup::server::daily {
         command => "/etc/amanda/create_vtapes.sh $cycle_type $tape_num $vtapes_location",
         require => [ File["/etc/amanda/create_vtapes.sh"], File["$conf_location"] ],
         user    => root;
+=======
+    $mailto_account = 'ops@wikimedia.org'
+    $cycle_type = 'Wikimedia-Daily'
+    $vtapes_location = '/data/amanda/Wikimedia-Daily'
+    $conf_location  = '/etc/amanda/Wikimedia-Daily'
+    $tape_length = '200 gbytes'
+    $tape_num = 6
+    $dumpcycle = '6 days'
+    $runspercycle = 1
+    $tapecycle = '6 tapes'
+    $incr_type = 'incronly'
+    $backup_priority = 'high'
+    $compress_type = 'client fast'
+
+    file {
+        "${conf_location}":
+            ensure  =>  directory,
+            owner   =>  'root',
+            group   =>  'root',
+            mode    =>  '0777',
+            purge   =>  false,
+            require =>  Package['amanda-server'];
+        "${conf_location}/amanda.conf":
+            owner   =>  'root',
+            group   =>  'root',
+            mode    =>  '0644',
+            require =>  File[$conf_location],
+            content =>  template('backups/amanda-server.conf.erb');
+        "$conf_location/logs":
+            ensure  =>  directory,
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0755',
+            purge   =>  false,
+            require =>  File[$conf_location];
+        "$conf_location/index":
+            ensure  =>  directory,
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0755',
+            purge   =>  false,
+            require =>  File[$conf_location];
+        "$conf_location/curinfo":
+            ensure  =>  directory,
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0755',
+            purge   =>  false,
+            require =>  File[$conf_location];
+        "$conf_location/tapelist":
+            owner   =>  'backup',
+            group   =>  'backup',
+            require =>  File[$conf_location],
+            mode    =>  '0644';
+        "$conf_location/disklist":
+            owner   =>  'root',
+            group   =>  'root',
+            mode    =>  '0644',
+            require =>  File[$conf_location],
+            source  =>  'puppet:///files/backup/disklist-daily';
+    }
+
+    exec {
+        'create_daily_vtapes':
+            command =>  "/etc/amanda/create_vtapes.sh $cycle_type $tape_num $vtapes_location",
+            require =>  [ File['/etc/amanda/create_vtapes.sh'], File[$conf_location] ],
+            user    =>  'root';
+>>>>>>> 2b185bd... lint backups.pp - part 1
     }
 
 }
 
 class backup::server::weekly {
+<<<<<<< HEAD
     $mailto_account  = "ops@wikimedia.org"
     $cycle_type      = "Wikimedia-Weekly"
     $vtapes_location = "/data/amanda/Wikimedia-Weekly"
@@ -345,11 +483,80 @@ class backup::server::weekly {
             command => "/etc/amanda/create_vtapes.sh $cycle_type $tape_num $vtapes_location",
             require => [ File["/etc/amanda/create_vtapes.sh"], File["$conf_location"] ],
             user    => root;
+=======
+    $mailto_account = 'ops@wikimedia.org'
+    $cycle_type = 'Wikimedia-Weekly'
+    $vtapes_location = '/data/amanda/Wikimedia-Weekly'
+    $conf_location = '/etc/amanda/Wikimedia-Weekly'
+    $tape_length = '200 gbytes'
+    $tape_num = 4
+    $dumpcycle = '4 weeks'
+    $runspercycle = 1
+    $tapecycle = '4 tapes'
+    $incr_type = 'standard'
+    $backup_priority = 'medium'
+    $compress_type = 'client fast'
+
+    file {
+        '$conf_location':
+            owner   =>  'root',
+            group   =>  'root',
+            mode    =>  '0777',
+            purge   =>  false,
+            require =>  Package['amanda-server'],
+            ensure  =>  directory;
+        '$conf_location/amanda.conf':
+            owner   =>  'root',
+            group   =>  'root',
+            mode    =>  '0644',
+            content =>  template('backups/amanda-server.conf.erb'),
+            require =>  File['$conf_location'];
+        '$conf_location/logs':
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0755',
+            purge   =>  false,
+            ensure  =>  directory,
+            require =>  File['$conf_location'];
+        '$conf_location/index':
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0755',
+            purge   =>  false,
+            ensure  =>  directory,
+            require =>  File["$conf_location"];
+        "$conf_location/curinfo":
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0755',
+            purge   =>  false,
+            ensure  =>  directory,
+            require =>  File["$conf_location"];
+        "$conf_location/tapelist":
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0644',
+            require =>  File["$conf_location"];
+        "$conf_location/disklist":
+            owner   =>  'root',
+            group   =>  'root',
+            mode    =>  '0644',
+            source  =>  'puppet:///files/backup/disklist-weekly',
+            require =>  File["$conf_location"];
+    }
+
+    exec {
+        'create_weekly_vtapes':
+            command =>  "/etc/amanda/create_vtapes.sh $cycle_type $tape_num $vtapes_location",
+            require =>  [ File['/etc/amanda/create_vtapes.sh'], File["$conf_location"] ],
+            user    =>  'root';
+>>>>>>> 2b185bd... lint backups.pp - part 1
     }
 
 }
 
 class backup::server::monthly {
+<<<<<<< HEAD
     $mailto_account  = "ops@wikimedia.org"
     $cycle_type      = "Wikimedia-Monthly"
     $vtapes_location = "/data/amanda/Wikimedia-Monthly"
@@ -415,6 +622,74 @@ class backup::server::monthly {
         command => "/etc/amanda/create_vtapes.sh $cycle_type $tape_num $vtapes_location",
         require => [ File["/etc/amanda/create_vtapes.sh"], File["$conf_location"] ],
         user    => root;
+=======
+    $mailto_account = 'ops@wikimedia.org'
+    $cycle_type = 'Wikimedia-Monthly'
+    $vtapes_location = '/data/amanda/Wikimedia-Monthly'
+    $conf_location = '/etc/amanda/Wikimedia-Monthly'
+    $tape_length = '200 gbytes'
+    $tape_num = 12
+    $dumpcycle = '365 days'
+    $runspercycle = 1
+    $tapecycle = '12 tapes'
+    $incr_type = 'noinc'
+    $backup_priority = 'low'
+    $compress_type = 'client best'
+
+    file {
+        "$conf_location":
+            owner   =>  'root',
+            group   =>  'root',
+            mode    =>  '0777',
+            purge   =>  false,
+            require =>  Package['amanda-server'],
+            ensure  =>  directory;
+        "$conf_location/amanda.conf":
+            owner   =>  'root',
+            group   =>  'root',
+            mode    =>  '0644',
+            require =>  File["$conf_location"],
+            content =>  template('backups/amanda-server.conf.erb');
+            "$conf_location/logs":
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0755',
+            purge   =>  false,
+            require =>  File["$conf_location"],
+            ensure  =>  directory;
+        "$conf_location/index":
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0755',
+            purge   =>  false,
+            require =>  File["$conf_location"],
+            ensure  =>  directory;
+        "$conf_location/curinfo":
+            owner   =>  'backup',
+            group   =>  'backup',
+            mode    =>  '0755',
+            purge   =>  false,
+            require =>  File["$conf_location"],
+            ensure  =>  directory;
+        "$conf_location/tapelist":
+            owner   =>  'backup',
+            group   =>  'backup',
+            require =>  File["$conf_location"],
+            mode    =>  '0644';
+        "$conf_location/disklist":
+            owner   =>  'root',
+            group   =>  'root',
+            mode    =>  '0644',
+            require =>  File["$conf_location"],
+            source  =>  'puppet:///files/backup/disklist-monthly';
+    }
+
+    exec {
+        'create_monthly_vtapes':
+            command =>  "/etc/amanda/create_vtapes.sh $cycle_type $tape_num $vtapes_location",
+            require =>  [ File['/etc/amanda/create_vtapes.sh'], File["$conf_location"] ],
+            user    =>  'root';
+>>>>>>> 2b185bd... lint backups.pp - part 1
     }
 
 }
@@ -423,6 +698,7 @@ class backup::client {
 
     define client_backup($host=$hostname, $path) {
         $line_of_text = "$host $path default"
+<<<<<<< HEAD
         @@file { "/etc/amanda/disklist":
             owner   => root,
             group   => root,
@@ -436,10 +712,27 @@ class backup::client {
     $ssh_key_location = "/var/backups/.ssh/id_rsa"
 
     package { ["amanda-common", "amanda-client" ]:
+=======
+        @@file {
+            '/etc/amanda/disklist':
+                owner   =>  'root',
+                group   =>  'root',
+                content =>  $line_of_text,
+                tag =>  'backup_host';
+        }
+    }
+
+    $backup_server = 'tridge.wikimedia.org'
+    $backup_name   = 'Wikimedia-Daily'
+    $ssh_key_location = '/var/backups/.ssh/id_rsa'
+
+    package { ['amanda-common', 'amanda-client' ]:
+>>>>>>> 2b185bd... lint backups.pp - part 1
         ensure => latest;
     }
 
     file {
+<<<<<<< HEAD
     "/etc/amanda/":
         owner   => root,
         group   => root,
@@ -486,6 +779,55 @@ class backup::client {
 
     #service { xinetd:
     #    ensure => stopped;
+=======
+        '/etc/amanda/':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755',
+            purge   => true,
+            require =>  Package['amanda-client'],
+            ensure  => directory;
+        '/etc/amanda/amanda-client.conf':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            require => File['/etc/amanda'],
+            content => template('backups/amanda-client.conf.erb');
+        '/etc/amandahosts':
+            owner   => 'backup',
+            group   => 'backup',
+            mode    => '0644',
+            require => File['/etc/amanda'],
+            content => template('backups/amandahosts-client.erb');
+        '/var/backups/.amandahosts':
+            owner   => 'backup',
+            group   => 'backup',
+            mode    => '0644',
+            content => template('backups/amandahosts-client.erb');
+        '/var/backups/.ssh':
+            owner   => 'backup',
+            group   => 'backup',
+            mode    => '0700',
+            purge   => false,
+            ensure  =>  directory;
+        '/var/backups/.ssh/authorized_keys':
+            owner   => 'backup',
+            group   => 'backup',
+            mode    => '0600',
+            require => File['/var/backups/.ssh'],
+            source  => 'puppet:///private/backup/ssh-keys/authorized_keys_client';
+        '/var/backups/.ssh/id_rsa':
+            owner   => 'backup',
+            group   => 'backup',
+            mode    => '0600',
+            require => File['/var/backups/.ssh'],
+            source  => 'puppet:///private/backup/ssh-keys/amanda_restore';
+    }
+
+    #service {
+    #   xinetd:
+    #       ensure => stopped;
+>>>>>>> 2b185bd... lint backups.pp - part 1
     #}
 }
 
@@ -493,6 +835,7 @@ class backup::mysql {
 
     include backup::client
 
+<<<<<<< HEAD
     file { "/usr/local/sbin/snaprotate.pl":
         owner  => root,
         group  => root,
@@ -505,6 +848,22 @@ class backup::mysql {
         user    => root,
         hour    => 1,
         minute  => 0;
+=======
+    file {
+        '/usr/local/sbin/snaprotate.pl':
+            owner => 'root',
+            group => 'root',
+            mode  => '0755',
+            source => 'puppet:///files/backup/snaprotate.pl';
+    }
+
+    cron {
+        'snaprotate':
+        command =>  '/usr/local/sbin/snaprotate.pl -a swap -V tank -s data -L 20G -c 1',
+        user    =>  'root',
+        hour    =>  1,
+        minute  =>  0;
+>>>>>>> 2b185bd... lint backups.pp - part 1
     }
 
 }
