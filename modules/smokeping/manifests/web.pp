38772503321e44fb106f7a49694de18535a4abd3
dzahn <dzahn@wikimedia.org>
2014-06-20 09:17:55 -0700

    fix path to smokeping.fcgi
    on netmon1001 this caused a puppet error:
     puppet:///smokeping/smokeping.fcgi: Error 400 on SERVER:
    
    because it was missing the "modules" part in the path
    
    also some minor lint fixes while touching it
    
    Change-Id: Iecc1c54276ba6e925cb51855ecbb5d055a9593fc

--- modules/smokeping/manifests/web.pp
+++ modules/smokeping/manifests/web.pp
@@ -1,16 +1,16 @@
 class smokeping::web {
     file { '/usr/share/smokeping/www/smokeping.fcgi':
-        source => "puppet:///${module_name}/smokeping.fcgi",
-        owner => root,
-        group => root,
-        mode => 0555,
+        source => "puppet:///modules/${module_name}/smokeping.fcgi",
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
     }
 
     include ::apache::mod::fcgid
     @webserver::apache::site { 'smokeping.wikimedia.org':
         require => [Class['::apache::mod::fcgid'], File['/usr/share/smokeping/www/smokeping.fcgi']],
         docroot => '/var/www',
-        custom => [
+        custom  => [
             'AliasMatch ^/($|smokeping\.cgi) /usr/share/smokeping/www/smokeping.fcgi',
             'Alias /images /var/cache/smokeping/images/'
             ],
