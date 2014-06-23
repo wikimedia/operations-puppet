2816e440e346b298b3f6bb4124567c4c499d890d
Antoine Musso <hashar@free.fr>
2014-06-23 22:42:54 +0200

    zuul: split conf file for server and merger
    We can get Zuul merger process on different boxes.  The zuul.conf has
    the Jenkins API key and I only want it to be exposed on the Zuul server.
    
    This generates a zuul-merger.conf file that does not have the [jenkins]
    section. Merger only requires the gearman, gerrit, merger, and zuul
    sections per upstream documentation.
    
    Make the merger to subscribe to that configuration file.  We do not
    notify since we might not have a merger process (installed via
    zuul::merger) on the node.
    
    Change-Id: I1cbfa8446311b18c9075da023051b528548ae017

---
+++ modules/zuul/manifests/configfile.pp
@@ -0,0 +1,31 @@
+# == Define zuul::configfile
+#
+# Crafts a Zuul configuration file
+#
+# === Parameters
+#
+# Required:
+#
+# **$name** Full path of the file to be created
+# **$zuul_role** Either 'merger' or 'server'
+#
+# Optional:
+#
+# Parameters passed to file {}:
+# **$owner** Default 'root'
+# **$group** Default 'root'
+# **$mode** Default '0444'
+define zuul::configfile(
+    $zuul_role,
+    $owner = 'root',
+    $group = 'root',
+    $mode  = '0444',
+) {
+    file { $name:
+        ensure  => present,
+        owner   => $owner,
+        group   => $group,
+        mode    => $mode,
+        content => template('zuul/zuul.conf.erb'),
+    }
+}
