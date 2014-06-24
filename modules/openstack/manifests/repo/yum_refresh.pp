12e3019520bdbe8945c08ca3a17f9bafb551deae
Andrew Bogott <abogott@wikimedia.org>
2014-06-24 21:12:49 +0000

    Remove unused upstream Openstack module
    Change-Id: If4ccb291d6d3d3630620ac68e780761554e365f4

--- modules/openstack/manifests/repo/yum_refresh.pp
+++
@@ -1,8 +0,0 @@
-# Make sure to refresh yum database after adding repos and before installing packages
-class openstack::repo::yum_refresh {
-  exec { 'yum_refresh':
-    command     => '/usr/bin/yum clean all',
-    refreshonly => true,
-  }
-  Exec['yum_refresh'] -> Package<||>
-}
