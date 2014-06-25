d0a680d79391e96be4424b9f4cf97d30dfb76d48
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-06-25 12:30:42 +0200

    rcstream: add SSL support
    Add ssl support to rcstream. This also adds support for ssl into the nginx module
    
    Change-Id: I7a5baa3a8f8fde2ae1a8144b7ab2de9795ee1f30
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- manifests/role/rcstream.pp
+++ manifests/role/rcstream.pp
@@ -38,6 +38,10 @@ class role::rcstream {
         backends => $backends,
     }
 
+    class { '::rcstream::proxy::ssl':
+        backends => $backends,
+    }
+
     class { 'lvs::realserver':
         realserver_ips => $lvs::configuration::lvs_service_ips[$::realm]['stream'][$::site],
     }
---
+++ modules/rcstream/manifests/proxy/ssl.pp
@@ -0,0 +1,58 @@
+# == Class: rcstream::proxy::ssl
+#
+# This class provisions an Nginx WebSockets reverse-proxy.
+# Requires Nginx 1.4+. This class uses SSL to connect to the backend.
+#
+# === Parameters
+#
+# [*backends*]
+#   An array of port numbers or strings in 'host:port' format
+#   specifying the WebSocket backend servers to reverse-proxy.
+#
+# [*server_name*]
+#   See <http://nginx.org/en/docs/http/server_names.html>.
+#   Defaults to '_'.
+#
+# [*listen*]
+#   Interface / port to listen on (default: 80, [::]:80),
+#   specified as a single value or an array.
+#   See <http://nginx.org/en/docs/http/ngx_http_core_module.html#listen>.
+#
+# [*location*]
+#   Path WebSocket servers should be mounted at (default: '/').
+#
+# === Examples
+#
+#  class { '::rcstream::proxy':
+#    server_name => 'stream.wikimedia.org',
+#    listen      => 8080,
+#    location    => '/rc',
+#    backends    => [
+#      'websockets.eqiad.wmnet:10080',
+#      'websockets.eqiad.wmnet:10081',
+#      'websockets.eqiad.wmnet:10082',
+#    ],
+#  }
+#
+class rcstream::proxy::ssl(
+    $backends,
+    $ensure      = present,
+    $server_name = 'stream.wikimedia.org',
+    $listen      = ['443', '[::]:443'],
+    $location    = '/'
+) {
+    $use_ssl = true
+
+    install_certificate { $server_name: }
+
+    nginx::site { 'rcstream-ssl':
+        content => template('rcstream/rcstream.nginx.erb'),
+        notify  => Service['nginx'],
+        require => [ Install_certificate['stream.wikimedia.org'], Class['nginx::ssl'] ]
+    }
+
+    class { 'nginx::ssl':
+        ie6_compat => true,
+    }
+
+}
