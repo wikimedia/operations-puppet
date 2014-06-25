6d0c4c135b0184520bd8870edc336e7b8b4a8f7c
cpettet <rush@wikimedia.org>
2014-06-25 15:08:55 -0500

    Phabricator for iridium
    Settings via settings=>{}.  This is just intial and does
    not include some known down the road configuration.
    
    Change-Id: I76268ff089bb941ef9a1170f5eba700cfa57f06a

--- manifests/role/phabricator.pp
+++ manifests/role/phabricator.pp
@@ -33,3 +33,31 @@ class role::phabricator::legalpad {
         }
     }
 }
+
+
+
+class role::phabricator::production {
+
+    #This must exist git to be applicable
+    $current_tag = 'rt7264'
+
+    if $::realm == 'production' {
+
+        class { '::phabricator':
+            git_tag   => $current_tag,
+            lock_file => '/var/run/phab_repo_lock',
+            settings  => {
+                'storage.upload-size-limit'          => '10M',
+                'darkconsole.enabled'                => false,
+                'phabricator.base-uri'               => 'http://phabricator.wikimedia.org',
+                'metamta.mail-adapter'               => 'PhabricatorMailImplementationPHPMailerAdapter',
+                'phpmailer.mailer'                   => 'smtp',
+                'phpmailer.smtp-port'                => '25',
+                'phpmailer.smtp-host'                => 'polonium.wikimedia.org',
+                'mysql.host'                         => 'localhost',
+                'mysql.pass'                         => 'foo',
+                'phabricator.show-beta-applications' =>  true,
+            },
+        }
+    }
+}
