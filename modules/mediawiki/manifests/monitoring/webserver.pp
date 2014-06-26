4040b528a8483034a363dbd13264570c22761ecf
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-06-26 16:57:04 +0200

    mediawiki: collect apc variables via diamond
    We created a class that is a basic framework for setting up monitoring
    endpoints in mediawiki, and that will allow to add other endpoints
    easily in the future. This first patch will only enable the monitoring
    virtual host on one server; after testing, the safeguards will be
    removed and it will be deployed to all mediawikis using the
    mediawiki::web class.
    
    Change-Id: I4f8629fbc9462d84a86db4c0da57b366e7a4cbc4
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/mediawiki/manifests/monitoring/webserver.pp
@@ -0,0 +1,74 @@
+class mediawiki::monitoring::webserver ($ensure = 'present'){
+    $apache_common = 'apache2.2-common' # TODO: make this compatible with apache 2.4
+
+    $dir_ensure = $ensure ? {
+        'present' => 'directory',
+        default   => $ensure
+    }
+
+    $link_ensure = $ensure ? {
+        'present' => 'link',
+        default   => $ensure
+    }
+
+    $endpoints = {
+        'apc' => 'apc_stats.php'
+    }
+
+
+    # Basic vhost files
+    file { '/var/www/monitoring':
+        ensure  => $dir_ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        require => Package[$apache_common]
+    }
+
+
+    file { '/etc/apache2/sites-available/monitoring.conf':
+        ensure => $ensure,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        content => template('mediawiki/apache/monitoring.conf.erb'),
+        require => Package[$apache_common]
+    }
+
+    # we do NOT notify apache, by design.
+    file { '/etc/apache2/sites-enabled/99-monitoring.conf':
+        ensure => $link_ensure,
+        target => '/etc/apache2/sites-available/monitoring.conf',
+        before => Service['apache']
+    }
+
+    # monitor definitions
+
+    # This define is designed to be private to this class,
+    # this is why it's defined within the class itself.
+    # We are choosing convention over configuration here, which is usualy wise.
+    define endpoint ($ensure = $::mediawiki::monitoring::webserver::ensure) {
+
+        $endpoint = $::mediawiki::monitoring::webserver::endpoints[$title]
+
+        file { "/var/www/monitoring/${endpoint}":
+            ensure => $ensure,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            source => "puppet:///modules/mediawiki/monitoring/${endpoint}"
+        }
+
+        diamond::collector {"MW_${title}":
+            ensure   => $ensure,
+            source   => "puppet:///modules/mediawiki/monitoring/collectors/${title}.py",
+            settings => { host => $::fqdn, path => "${title}" }
+        }
+
+    }
+
+    include stdlib
+    $endpoint_list = keys($endpoints)
+    mediawiki::monitoring::webserver::endpoint { $endpoint_list: }
+
+}
