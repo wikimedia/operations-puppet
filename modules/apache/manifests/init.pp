50149bfc178fd30900b0cfc456cd16d27ffbb5ea
Ori Livneh <ori@wikimedia.org>
2014-06-26 13:44:00 -0700

    Add apache::conf
    * I moved the bulk of code in apache::site to a new resource type,
      apache::conf, which can manage either a sites-{enabled,disabled} config file
      or a conf-{enabled,disabled} config file.
    * I made apache::site be a thin wrapper around that.
      (Why keep apache::site at all? Because it's clearer and neater than
      "apache::conf { '..': type => 'sites', ... } ".)
    * Since the module now manages files in conf-enabled as well as sites-enabled,
      declare the former directory too and manage it in the same way.
    * conf-enabled/* is Ubuntu 13.10+; prior releases use conf.d/*. So provide a
      small shim for older versions, in keeping with the philosophy of seamless
      cross-compatibility.
    * Also remove unused service_enabled param and .gemfile, .gitignore,
      .travis.yml files.
    
    Change-Id: I5bf6186d70e47cfc04e9d78da145d5f82b08504d

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -2,20 +2,7 @@
 #
 # Provisions Apache web server package and service.
 #
-# === Parameters
-#
-# [*service_enable*]
-#   Whether the Apache service should be enabled. Boolean; true by default.
-#
-# === Example
-#
-#  class { 'apache':
-#    service_enable => false,
-#  }
-#
-class apache( $service_enable = true ) {
-    validate_bool($service_enable)
-
+class apache {
     # Strive for seamless Apache 2.2 / 2.4 compatibility
     include apache::mod::access_compat
     include apache::mod::filter
@@ -26,19 +13,45 @@ class apache( $service_enable = true ) {
     }
 
     service { 'apache2':
-        ensure  => $service_enable,
-        enable  => $service_enable,
+        ensure  => running,
+        enable  => true,
         require => Package['apache2'],
     }
 
-    file { '/etc/apache2/sites-enabled':
+    file { [ '/etc/apache2/sites-available', '/etc/apache2/conf-available' ]:
         ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        require => Package['apache2'],
+    }
+
+    file { [ '/etc/apache2/sites-enabled', '/etc/apache2/conf-enabled' ]:
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
         recurse => true,
         purge   => true,
         notify  => Service['apache2'],
         require => Package['apache2'],
     }
 
-    # Provision Apache modules before sites
-    Apache::Mod_conf <| |> -> Apache::Site <| |>
+    if versioncmp($::lsbdistrelease, '14.04') < 0 {
+        # Early releases of Apache manage configuration snippets in conf.d/.
+        # We standardize on conf-enabled/*.conf with this small shim.
+        file { '/etc/apache2/conf.d/load-conf-enabled.conf':
+            content => "Include /etc/apache2/conf-enabled/*.conf\n",
+            require => File['/etc/apache2/conf-enabled'],
+            notify  => Service['apache2'],
+        }
+    }
+
+    apache::conf { 'defaults':
+        source   => 'puppet:///modules/apache/defaults.conf',
+        priority => 0,
+    }
+
+    # Provision Apache modules before provisioning sites and config snippets
+    Apache::Mod_conf <| |> -> Apache::Conf <| |>
 }
