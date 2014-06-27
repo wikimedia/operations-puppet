f4383bcc5e2d95e7b9d6c3fe656ca6e9e91f12af
Scott Lee <scott.leea@gmail.com>
2014-06-26 23:31:51 -0400

    Fixed spacing and lint rules for manifests files.
    Change-Id: I8a70b24ed7e2bc16ab1f7ccfabb136f6e1e97d68

--- manifests/realm.pp
+++ manifests/realm.pp
@@ -3,54 +3,54 @@
 #
 
 if $::realm == undef {
-	$realm = "production"
+    $realm = 'production'
 }
 
 if $::instanceproject == undef {
-	$instanceproject = ''
+    $instanceproject = ''
 }
 
 if $::projectgroup == undef {
-	$projectgroup = "project-$instanceproject"
+    $projectgroup = "project-${instanceproject}"
 }
 
 # Determine the site the server is in
 if $::ipaddress_eth0 != undef {
-	$main_ipaddress = $ipaddress_eth0
+    $main_ipaddress = $ipaddress_eth0
 } elsif $::ipaddress_bond0 != undef {
-	$main_ipaddress = $ipaddress_bond0
+    $main_ipaddress = $ipaddress_bond0
 } else {
-	$main_ipaddress = $ipaddress
+    $main_ipaddress = $ipaddress
 }
 
 $site = $main_ipaddress ? {
-	/^208\.80\.15[23]\./                      => "pmtpa",
-	/^208\.80\.15[45]\./                      => "eqiad",
-	/^10\.[0-4]\./                            => "pmtpa",
-	/^10\.6[48]\./                            => "eqiad",
-	/^91\.198\.174\./                         => "esams",
-	/^198\.35\.26\.([0-9]|[1-5][0-9]|6[0-2])/ => "ulsfo",
-	/^10\.128\./                              => "ulsfo",
-	/^10\.20\.0\./                            => "esams",
-	default                                   => "(undefined)"
+    /^208\.80\.15[23]\./                      => 'pmtpa',
+    /^208\.80\.15[45]\./                      => 'eqiad',
+    /^10\.[0-4]\./                            => 'pmtpa',
+    /^10\.6[48]\./                            => 'eqiad',
+    /^91\.198\.174\./                         => 'esams',
+    /^198\.35\.26\.([0-9]|[1-5][0-9]|6[0-2])/ => 'ulsfo',
+    /^10\.128\./                              => 'ulsfo',
+    /^10\.20\.0\./                            => 'esams',
+    default                                   => '(undefined)'
 }
 
 $mw_primary = $::realm ? {
-	'production' => "eqiad",
-	default => $::site
+    'production' => 'eqiad',
+    default => $::site
 }
 
 $network_zone = $main_ipaddress ? {
-	/^10./			=> "internal",
-	default			=> "public"
+    /^10./  => 'internal',
+    default => 'public'
 }
 
 # TODO: create hash of all LVS service IPs
 
 # Set some basic variables
 $nameservers = $site ? {
-	"esams"	=> [ "91.198.174.6", "208.80.154.239" ],   # esams -> esams (nescio), eqiad (LVS)
-	default	=> [ "208.80.154.239", "91.198.174.6" ],   # pmtpa+eqiad+ulsfo -> eqiad (LVS), esams (nescio)
+    'esams' => [ '91.198.174.6', '208.80.154.239' ],   # esams -> esams (nescio), eqiad (LVS)
+    default => [ '208.80.154.239', '91.198.174.6' ],   # pmtpa+eqiad+ulsfo -> eqiad (LVS), esams (nescio)
 }
 
 # Allow per-server nameserver prefixes
@@ -68,106 +68,108 @@ $gid = 500
 
 ## puppet-accessible list of private wikis
 ## please keep alphabetized
-$private_wikis = [ 'arbcom_dewiki',
-		'arbcom_enwiki',
-		'arbcom_fiwiki',
-		'arbcom_nlwiki',
-		'auditcomwiki',
-		'boardgovcomwiki',
-		'boardwiki',
-		'chairwiki',
-		'chapcomwiki',
-		'checkuserwiki',
-		'collabwiki',
-		'execwiki',
-		'fdcwiki',
-		'grantswiki',
-		'iegcomwiki',
-		'ilwikimedia',
-		'internalwiki',
-		'legalteamwiki',
-		'movementroleswiki',
-		'noboard_chapterswikimedia',
-		'officewiki',
-		'ombudsmenwiki',
-		'otrs_wikiwiki',
-		'searchcomwiki',
-		'spcomwiki',
-		'stewardwiki',
-		'transitionteamwiki',
-		'wg_enwiki',
-		'wikimaniateamwiki',
-		'zerowiki' ]
+$private_wikis = [ 
+    'arbcom_dewiki',
+    'arbcom_enwiki',
+    'arbcom_fiwiki',
+    'arbcom_nlwiki',
+    'auditcomwiki',
+    'boardgovcomwiki',
+    'boardwiki',
+    'chairwiki',
+    'chapcomwiki',
+    'checkuserwiki',
+    'collabwiki',
+    'execwiki',
+    'fdcwiki',
+    'grantswiki',
+    'iegcomwiki',
+    'ilwikimedia',
+    'internalwiki',
+    'legalteamwiki',
+    'movementroleswiki',
+    'noboard_chapterswikimedia',
+    'officewiki',
+    'ombudsmenwiki',
+    'otrs_wikiwiki',
+    'searchcomwiki',
+    'spcomwiki',
+    'stewardwiki',
+    'transitionteamwiki',
+    'wg_enwiki',
+    'wikimaniateamwiki',
+    'zerowiki' ]
 
-$private_tables = [ 'accountaudit_login',
-		'arbcom1_vote',
-		'archive_old',
-		'blob_orphans',
-		'blob_tracking',
-		'bv2009_edits',
-		'categorylinks_old',
-		'click_tracking',
-		'cu_changes',
-		'cu_log',
-		'cur',
-		'edit_page_tracking',
-		'email_capture',
-		'exarchive',
-		'exrevision',
-		'filejournal',
-		'globalnames',
-		'hidden',
-		'image_old',
-		'job',
-		'linkscc',
-		'localnames',
-		'log_search',
-		'logging_old',
-		'long_run_profiling',
-		'migrateuser_medium',
-		'moodbar_feedback',
-		'moodbar_feedback_response',
-		'objectcache',
-		'old_growth',
-		'oldimage_old',
-		'optin_survey',
-		'pr_index',
-		'prefstats',
-		'prefswitch_survey',
-		'profiling',
-		'querycache',
-		'querycache_info',
-		'querycache_old',
-		'querycachetwo',
-		'securepoll_cookie_match',
-		'securepoll_elections',
-		'securepoll_entity',
-		'securepoll_lists',
-		'securepoll_msgs',
-		'securepoll_options',
-		'securepoll_properties',
-		'securepoll_questions',
-		'securepoll_strike',
-		'securepoll_voters',
-		'securepoll_votes',
-		'spoofuser',
-		'text',
-		'titlekey',
-		'transcache',
-		'uploadstash',
-		'user_newtalk',
-		'vote_log',
-		'watchlist' ]
+$private_tables = [ 
+    'accountaudit_login',
+    'arbcom1_vote',
+    'archive_old',
+    'blob_orphans',
+    'blob_tracking',
+    'bv2009_edits',
+    'categorylinks_old',
+    'click_tracking',
+    'cu_changes',
+    'cu_log',
+    'cur',
+    'edit_page_tracking',
+    'email_capture',
+    'exarchive',
+    'exrevision',
+    'filejournal',
+    'globalnames',
+    'hidden',
+    'image_old',
+    'job',
+    'linkscc',
+    'localnames',
+    'log_search',
+    'logging_old',
+    'long_run_profiling',
+    'migrateuser_medium',
+    'moodbar_feedback',
+    'moodbar_feedback_response',
+    'objectcache',
+    'old_growth',
+    'oldimage_old',
+    'optin_survey',
+    'pr_index',
+    'prefstats',
+    'prefswitch_survey',
+    'profiling',
+    'querycache',
+    'querycache_info',
+    'querycache_old',
+    'querycachetwo',
+    'securepoll_cookie_match',
+    'securepoll_elections',
+    'securepoll_entity',
+    'securepoll_lists',
+    'securepoll_msgs',
+    'securepoll_options',
+    'securepoll_properties',
+    'securepoll_questions',
+    'securepoll_strike',
+    'securepoll_voters',
+    'securepoll_votes',
+    'spoofuser',
+    'text',
+    'titlekey',
+    'transcache',
+    'uploadstash',
+    'user_newtalk',
+    'vote_log',
+    'watchlist' ]
 
 # Route list for mail coming from MediaWiki mailer
 $wikimail_smarthost = $::realm ? {
-	'production' => [ 'wiki-mail-eqiad.wikimedia.org' ],
-	# FIXME: find some SMTP servers for labs
-	'labs'       => [ 'polonium.wikimedia.org', 'mchenry.wikimedia.org' ],
+    'production' => [ 'wiki-mail-eqiad.wikimedia.org' ],
+    # FIXME: find some SMTP servers for labs
+    'labs'       => [ 'polonium.wikimedia.org', 'mchenry.wikimedia.org' ],
 }
 # Generic, default servers (order matters!)
 $mail_smarthost = $::realm ? {
-	'production' => [ 'polonium.wikimedia.org', 'mchenry.wikimedia.org' ],
-	# FIXME: find some SMTP servers for labs
-	'labs'       => [ 'polonium.wikimedia.org', 'mchenry.wikimedia.org' ],
+    'production' => [ 'polonium.wikimedia.org', 'mchenry.wikimedia.org' ],
+    # FIXME: find some SMTP servers for labs
+    'labs'       => [ 'polonium.wikimedia.org', 'mchenry.wikimedia.org' ],
 }
