f3a4e6e6ae9a4c9ccfd4b1e8327dd255209ec18d
Scott Lee <scott.leea@gmail.com>
2014-06-26 23:59:50 -0400

    Fixed spacing and lint rules for manifests/misc files.
    Change-Id: I49860d60311cbadf6989832a444850c502c169e5

--- manifests/misc/limn.pp
+++ manifests/misc/limn.pp
@@ -16,48 +16,48 @@
 # misc::limn::instance { 'reportcard': }
 #
 define misc::limn::instance(
-  $port           = 8081,
-  $environment    = 'production',
-  $base_directory = '/usr/local/share/limn',
-  $var_directory  = "/var/lib/limn/${name}",
-  $server_name    = undef,
-  $server_aliases = undef)
+    $port           = 8081,
+    $environment    = 'production',
+    $base_directory = '/usr/local/share/limn',
+    $var_directory  = "/var/lib/limn/${name}",
+    $server_name    = undef,
+    $server_aliases = undef)
 {
-  ::limn::instance { $name:
-    port           => $port,
-    environment    => $environment,
-    base_directory => $base_directory,
-    var_directory  => $var_directory,
-  }
+    ::limn::instance { $name:
+        port           => $port,
+        environment    => $environment,
+        base_directory => $base_directory,
+        var_directory  => $var_directory,
+    }
 
-  # If $server_name was not specified,
-  # Pick $name.wikimedia.org for production,
-  # $name.wmflabs.org for labs,
-  # and just $name for anything else.
-  $servername = $server_name ? {
-    undef   => $::realm ? {
-      'production' => "${name}.wikimedia.org",
-      'labs'       => "${name}.wmflabs.org",
-      default      => $name,
-    },
-    default => $server_name,
-  }
+    # If $server_name was not specified,
+    # Pick $name.wikimedia.org for production,
+    # $name.wmflabs.org for labs,
+    # and just $name for anything else.
+    $servername = $server_name ? {
+        undef   => $::realm ? {
+            'production' => "${name}.wikimedia.org",
+            'labs'       => "${name}.wmflabs.org",
+            default      => $name,
+        },
+        default => $server_name,
+    }
 
-  # If $server_aliases was not specified,
-  # Pick ${name}.instance-proxy.wmflabs.org for labs,
-  # and none for anything else.
-  $serveraliases = $server_aliases ? {
-    undef   => $::realm ? {
-      'labs'       => "${name}.instance-proxy.wmflabs.org",
-      default      => '',
-    },
-    default => $server_aliases,
-  }
+    # If $server_aliases was not specified,
+    # Pick ${name}.instance-proxy.wmflabs.org for labs,
+    # and none for anything else.
+    $serveraliases = $server_aliases ? {
+        undef   => $::realm ? {
+            'labs'       => "${name}.instance-proxy.wmflabs.org",
+            default      => '',
+        },
+        default => $server_aliases,
+    }
 
-  limn::instance::proxy { $name:
-    limn_port      => $port,
-    server_name    => $servername,
-    server_aliases => $serveraliases,
-    require        => ::Limn::Instance[$name],
-  }
+    limn::instance::proxy { $name:
+        limn_port      => $port,
+        server_name    => $servername,
+        server_aliases => $serveraliases,
+        require        => ::Limn::Instance[$name],
+    }
 }
