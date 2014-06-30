36eea51ba8a82b2ca85723e2a8018fb4df0e50d0
Ori Livneh <ori@wikimedia.org>
2014-06-30 12:55:16 -0700

    add apache::def resource
    The resource provides a convenient way to define runtime parameters
    than can be checked with <IfDefine> directives.
    
    Change-Id: Ifdd6d2a66c99c3f8c35cf22c8a1db537dba3bf83

---
+++ modules/apache/manifests/def.pp
@@ -0,0 +1,28 @@
+# == Define: apache::def
+#
+# This resource provides an easy way to declare a runtime parameter
+# for Apache. It can then be used in Apache <IfDefine> checks.
+#
+# === Parameters
+#
+# [*ensure*]
+#   If 'present', the environment variable will be defined; if absent,
+#   undefined. The default is 'present'.
+#
+# === Example
+#
+#  apache::def { 'HHVM':
+#    ensure => present,
+#  }
+#
+define apache::def( $ensure = present ) {
+    include ::apache
+    include ::stdlib
+
+    file_line { "apache2_def_${title}":
+        ensure => $ensure,
+        path   => '/etc/apache2/envvars',
+        line   => "export APACHE_ARGUMENTS=\"\$APACHE_ARGUMENTS -D ${title}\"",
+        notify => Service['apache2'],
+    }
+}
