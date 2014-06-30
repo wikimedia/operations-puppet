7864b574562d65721c5b77fcc63f51888b97afd0
Ori Livneh <ori@wikimedia.org>
2014-06-30 12:55:16 -0700

    add apache::param resource
    The resource provides a convenient way to define a runtime parameter than can
    be checked with <IfDefine> directives.
    
    Change-Id: Ifdd6d2a66c99c3f8c35cf22c8a1db537dba3bf83

---
+++ modules/apache/manifests/param.pp
@@ -0,0 +1,28 @@
+# == Define: apache::param
+#
+# This resource provides an easy way to declare a runtime parameter
+# for Apache. It can then be used in Apache <IfDefine> checks.
+#
+# === Parameters
+#
+# [*ensure*]
+#   If 'present', parameter will be defined; if absent, undefined.
+#   The default is 'present'.
+#
+# === Example
+#
+#  apache::param { 'HHVM':
+#    ensure => present,
+#  }
+#
+define apache::param( $ensure = present ) {
+    include ::apache
+    include stdlib
+
+    file_line { "apache2_param_${title}":
+        ensure => $ensure,
+        path   => '/etc/apache2/envvars',
+        line   => "export APACHE_ARGUMENTS=\"\$APACHE_ARGUMENTS -D ${title}\"",
+        notify => Service['apache2']
+    }
+}
