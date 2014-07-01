43f44e6e6acb50207562d7e8a9e48858184808a6
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-07-01 17:02:21 +0200

    mediawiki: manage the apache config via puppet
    This is the first in a series of changes that will allow to manage
    the apache config for mediawiki using a more standard layout. For now we
    just deploy apache-config via puppet using a directory resource, and
    importing apache-config as plain files with a submodule.
    
    We introduce tags so that it should be possible to release apache-config
    by simply:
    
    1) merging the change in apache-config
    2) update the submodule
    3) run on the saltmaster:
       salt -G 'cluster:appserver*' cmd.run 'puppet agent -t --tags mw-apache-config'
    
    which should be simple enough. Also, puppet will ensure and enforce this
    config on servers that were down at the moment of the deploy.
    
    Change-Id: I43126e44e96b8ae88eb5ad1958b1ce6fe5ade9f5
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/mediawiki/manifests/web/config.pp
+++ modules/mediawiki/manifests/web/config.pp
@@ -1,15 +1,18 @@
 class mediawiki::web::config ($use_local_resources = true) {
     tag 'mediawiki', 'mw-apache-config'
-    file { '/etc/apache2/wikimedia':
-        ensure  => directory,
-        recurse => true,
-        source  => 'puppet:///modules/mediawiki/apache/config',
-        notify  => Service['apache'],
-    }
-    exec { 'sync_apache_config':
-        command => '/usr/bin/rsync -av 10.0.5.8::httpdconf/ /usr/local/apache/conf',
-        creates => '/usr/local/apache/conf',
-        require => File['/usr/local/apache'],
-        notify  => Service['apache'],
+    if $use_local_resources {
+        file { '/etc/apache2/wikimedia':
+            ensure  => directory,
+            recurse => true,
+            source  => 'puppet:///modules/mediawiki/apache/config',
+            notify  => Service['apache'],
+        }
+    } else {
+        exec { 'sync_apache_config':
+            command => '/usr/bin/rsync -av 10.0.5.8::httpdconf/ /usr/local/apache/conf',
+            creates => '/usr/local/apache/conf',
+            require => File['/usr/local/apache'],
+            notify  => Service['apache'],
+        }
     }
 }
