46cd71b8aae0e7cab67c491b0a339c4cd8d80948
Matanya Moses <matanya@foss.co.il>
2014-07-01 15:54:19 +0300

    apt::puppet: remove puppet 2.7 conditional
    Change-Id: I74000ab69d7dcc0168deb1ced1570851c1e2a9d6

--- modules/apt/manifests/puppet.pp
+++ modules/apt/manifests/puppet.pp
@@ -3,33 +3,4 @@ define apt::puppet (
     $packages  = 'puppet puppet-common facter'
     ) {
 
-    if ($version == '2.7') {
-        # For precise, we just need to tell apt to use ubuntu provided packages,
-        # with an higher priority than the general rule preferring wikimedia
-        # packages
-        if $::lsbdistcodename == 'precise' {
-                apt::pin { "puppet_${title}_${version}.pref":
-                    package  => $packages,
-                    pin      => 'release o=Ubuntu',
-                    priority => 1002
-                }
-        }
-    } else {
-        # For trusty, ubuntu packages are puppet 3
-        if $::lsbdistcodename == 'trusty' {
-            apt::pin { $title:
-                package  => $packages,
-                pin      => 'release o=Ubuntu',
-                priority => 1002
-            }
-        } elsif $::lsbdistcodename == 'precise' {
-            # Ensure the pref file is not there in this case,
-            # so that puppet gets updated.
-        apt::pin { "puppet_${title}_2.7.pref":
-                ensure   => 'absent',
-                priority => 0,
-                pin      => 'release o=Ubuntu'
-            }
-        }
-    }
 }
