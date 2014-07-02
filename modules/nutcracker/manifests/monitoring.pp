32b600a4be35f89060e231c3c6ebfce14df78a1c
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-07-02 13:22:58 +0200

    nutcracker: move config in puppet, work with new packages
    We are both moving the twemproxy config in puppet, and making the puppet
    class work correctly with the new package, where we repackaged nutcracker with
    its proper name and with saner defaults and init scripts directly in the
    package.
    
    This config can be deployed using tagged runs as well.
    
    This change depends on 143892 being deployed in scap.
    
    Change-Id: I42ee37f1431388e9659c2f44e334ac8db67fd2b3
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/nutcracker/manifests/monitoring.pp
@@ -0,0 +1,15 @@
+# == Class: nutcracker::monitoring
+#
+# Provisions Icinga alerts for twemproxy.
+#
+class nutcracker::monitoring {
+    nrpe::monitor_service { 'nutcracker':
+        description  => 'nutcracker process',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:2 -u nobody -C nutcracker',
+    }
+
+    nrpe::monitor_service { 'nutcracker_port':
+        description  => 'nutcracker port',
+        nrpe_command => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 11212 --timeout=2',
+    }
+}
--- modules/twemproxy/manifests/monitoring.pp
+++ modules/twemproxy/manifests/monitoring.pp
@@ -5,7 +5,7 @@
 class twemproxy::monitoring {
     nrpe::monitor_service { 'twemproxy':
         description  => 'twemproxy process',
-        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u nobody -C nutcracker',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:2 -u nobody -C nutcracker',
     }
 
     nrpe::monitor_service { 'twemproxy_port':
