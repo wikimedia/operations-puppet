1442a908a6cbdc8b031df92a669b0d5995009105
Andrew Otto <acotto@gmail.com>
2014-07-02 14:11:29 -0400

    Use CDH5 for oozie in labs
    Change-Id: I22e0ce33c0a1494798ae429f414c397f61b4123d

--- manifests/role/analytics/oozie.pp
+++ manifests/role/analytics/oozie.pp
@@ -8,14 +8,22 @@ class role::analytics::oozie::client {
     # Need hadoop client before oozie client.
     Class['role::analytics::hadoop::client'] -> Class['role::analytics::oozie::client']
 
-    $oozie_host = $::realm ? {
-        'production' => 'analytics1027.eqiad.wmnet',
-        'labs'       => $role::analytics::hadoop::labs::namenode_hosts[0],
+    # include common labs or production hadoop configs
+    # based on $::realm
+    if ($::realm == 'labs') {
+        $oozie_host      = $role::analytics::hadoop::labs::namenode_hosts[0]
+        $cdh_module_name = 'cdh'
+    }
+    else {
+        $oozie_host      = 'analytics1027.eqiad.wmnet'
+        $cdh_module_name = 'cdh4'
     }
 
-    class { 'cdh4::oozie':
+
+    class { "${cdh_module_name}::oozie":
         oozie_host => $oozie_host,
     }
+
 }
 
 # == Class role::analytics::oozie::server
@@ -29,14 +37,14 @@ class role::analytics::oozie::server inherits role::analytics::oozie::client {
     }
     # make sure mysql-server is installed before
     # we apply our MySQL Oozie database class.
-    Package['mysql-server'] -> Class['::cdh4::oozie::database::mysql']
+    Package['mysql-server'] -> Class["${cdh_module_name}::oozie::database::mysql"]
 
     $jdbc_password = $::realm ? {
         'production' => $passwords::analytics::oozie_jdbc_password,
         'labs'       => 'oozie',
     }
 
-    class { 'cdh4::oozie::server':
+    class { "${cdh_module_name}::oozie::server":
         jdbc_password   => $jdbc_password,
         smtp_host       => $::mail_smarthost[0],
         smtp_from_email => "oozie@$::fqdn",
