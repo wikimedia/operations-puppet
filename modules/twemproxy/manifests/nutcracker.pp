447419cc635ba664b89052ae568e53016d14df18
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-07-02 13:22:58 +0200

    nutcracker: move config in puppet, work with trusty packages
    We are both moving the twemproxy config in puppet, and making the puppet
    class work correctly with trusty, where we repackaged nutcracker with
    its proper name and with saner defaults and init scripts directly in the
    package.
    
    This config can be deployed using tagged runs as well.
    
    Change-Id: I42ee37f1431388e9659c2f44e334ac8db67fd2b3
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/twemproxy/manifests/nutcracker.pp
@@ -0,0 +1,25 @@
+class twemproxy::nutcracker ($config, $server_list) {
+    tag 'twemproxy'
+    package {'nutcracker': }
+
+    file { '/etc/default/nutcracker':
+        ensure  => present,
+        content => 'DAEMON_OPTS="--mbuf-size=65536 "',
+        require => Package['nutcracker'],
+    }
+
+    file { '/etc/nutcracker/nutcracker.yml':
+        ensure  => present,
+        mode    => '0444',
+        content => template('twemproxy/config.yml.erb'),
+        require => File['/etc/default/nutcracker'],
+        notify  => Service['twemproxy'],
+    }
+    # this needs to have the same name as the old twemproxy service,
+    # for compat reasons
+    service { 'twemproxy':
+        ensure   => running,
+        name     => 'nutcracker',
+        provider => upstart,
+    }
+}
