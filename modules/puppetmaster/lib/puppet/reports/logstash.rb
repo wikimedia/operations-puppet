2b12ea0ef6db66e7c79bfbd5bd505bf1a6de2447
Bryan Davis <bd808@wikimedia.org>
2014-07-02 16:52:03 -0600

    [WIP] Allow puppetmaster to send reports to logstash
    Add a Puppet report handler for sending data to logstash. Each report is
    converted to a single log event which is sent as newline delimited json
    to Logstash over a tcp transport.
    
    On the Logstash side, the event's "host" field is fixed to contain the
    hostname of the Puppet agent that originated the report rather than the
    ip address of the Puppetmaster and tags for indexing in ElasticSearch.
    
    Bug: 60690
    Change-Id: I604582a7faf902ba899985ca7127b95de1fa9b20

---
+++ modules/puppetmaster/lib/puppet/reports/logstash.rb
@@ -0,0 +1,68 @@
+# Puppet reporter to log events to logstash
+#
+# Based on https://github.com/logstash/puppet-logstash-reporter
+#
+# Copyright 2013 EvenUp Inc.
+# Copyright © 2014 Bryan Davis and Wikimedia Foundation.
+# License http://www.apache.org/licenses/LICENSE-2.0
+
+require 'puppet'
+require 'socket'
+require 'timeout'
+require 'json'
+require 'yaml'
+
+Puppet::Reports.register_report(:logstash) do
+    desc 'Send logs to a logstash instance'
+
+    config_file = File.join([File.dirname(Puppet.settings[:config]), 'logstash.yaml'])
+    begin
+        CONFIG = YAML.load_file(config_file)
+    rescue Exception => e
+        raise(Puppet::ParseError, "Failed to load logstash report config file #{config_file}: #{e.message}")
+    end
+
+    SERVER_NAME = Socket.gethostbyname(Socket.gethostname).first
+
+    def process
+        # Convert Puppet::Transaction::Report object to a logstash event
+        event = {
+            '@timestamp'            => self.logs.last.time.utc.iso8601,
+            '@version'              => 1,
+            'host'                  => self.host,
+            'client'                => self.host,
+            'server'                => SERVER_NAME,
+            'message'               => "Puppet run on #{self.host} - #{self.status}",
+            'type'                  => 'puppet',
+            'channel'               => self.kind,
+            'environment'           => self.environment,
+            'report_format'         => self.report_format,
+            'transaction_uuid'      => self.transaction_uuid,
+            'configuration_version' => self.configuration_version,
+            'puppet_version'        => self.puppet_version,
+            'status'                => self.status,
+            'start_time'            => self.logs.first.time.utc.iso8601,
+            'end_time'              => self.logs.last.time.utc.iso8601,
+            'log_messages'          => self.logs.map(&:to_report)
+        }
+
+        self.metrics.each do |category,v|
+            event['metrics'][category] = {}
+            v.values.each do |metric|
+                # Each element is of the form [name, titleized_name, value]
+                event['metrics'][category][metric[0]] = metric[2]
+            end
+        end
+
+        begin
+            Timeout::timeout(CONFIG[:timeout]) do
+                sock = TCPSocket.new "#{CONFIG[:host]}" , CONFIG[:port]
+                sock.puts event.to_json
+                sock.flush
+                sock.close
+            end
+        rescue Exception => e
+            Puppet.err("Failed to write to #{CONFIG[:host]} on port #{CONFIG[:port]}: #{e.message}")
+        end
+    end
+end
