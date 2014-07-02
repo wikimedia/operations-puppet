182aa947179ff3d2119b88f844822bb00f0a3bb9
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-07-02 13:22:58 +0200

    nutcracker: move config in puppet, work with trusty packages
    We are both moving the twemproxy config in puppet, and making the puppet
    class work correctly with trusty, where we repackaged nutcracker with
    its proper name and with saner defaults and init scripts directly in the
    package.
    
    This config can be deployed using tagged runs as well.
    
    Change-Id: I42ee37f1431388e9659c2f44e334ac8db67fd2b3
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/twemproxy/manifests/init.pp
+++ modules/twemproxy/manifests/init.pp
@@ -6,36 +6,78 @@
 #
 # === Parameters
 #
-# [*default_file*]
-#   Puppet file URI or local filesystem path to file that should be
-#   used as /etc/default/twemproxy. The file should set a CONFIG var
-#   in the environment that points to the twemproxy YAML config file.
-#   Defaults to 'puppet:///modules/twemproxy/default'.
+# [*config_parameters*]
+#
+# Non-standard config parameters for the memcached section of the
+# configuration.
+#
+# [*servers_list*]
+#
+# List of the servers (in the IP:PORT format) that will be used as
+# backends for memcached.
 #
 # === Examples
 #
 #  class { 'twemproxy':
-#    default_file => 'puppet:///modules/mediawiki/twemproxy.default',
+#    servers_list => ['192.168.0.1:11211', '192.168.0.2:11211'],
 #  }
 #
-class twemproxy( $default_file = 'puppet:///modules/twemproxy/default' ) {
-    package { 'twemproxy': }
+class twemproxy( $servers_list, $config_parameters = {}) {
+    tag 'twemproxy'
 
-    file { '/etc/init/twemproxy.conf':
-        source => 'puppet:///modules/twemproxy/twemproxy.conf',
-        notify => Service['twemproxy'],
+    $default_config = {
+        'listen'               => '127.0.0.1:11211',
+        'hash'                 => 'md5',
+        'distribution'         => 'ketama',
+        'timeout'              => 250,
+        'preconnect'           => 'true',
+        'redis'                => 'false',
+        'auto_eject_hosts'     => 'true',
+        'server_retry_timeout' => 30000,
+        'server_failure_limit' => 3,
+        'server_connections'   => 2,
     }
+    $config = merge($default_config, $config_parameters)
 
-    file { '/etc/default/twemproxy':
-        source  => $default_file,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        notify  => Service['twemproxy'],
-    }
+    if versioncmp($::lsbdistrelease, '14.04') >= 0 {
+        # Ubuntu 14.04 uses a different (saner) package, who has also
+        # been renamed. So twemproxy here acts as a proxy.
+        class {'twemproxy::nutcracker':
+            config       => $::twemproxy::config,
+            servers_list => $::twemproxy::server_list
+        }
+    } else {
+        package { 'twemproxy': }
 
-    service { 'twemproxy':
-        ensure   => running,
-        provider => upstart,
-    }
+        file { '/etc/init/twemproxy.conf':
+            source => 'puppet:///modules/twemproxy/twemproxy.conf',
+            notify => Service['twemproxy'],
+        }
+
+        file { '/etc/default/twemproxy':
+            source  => 'puppet:///modules/twemproxy/default',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            notify  => Service['twemproxy'],
+        }
+
+        file { '/etc/nutcracker':
+            ensure => directory,
+            mode   => '0755',
+            before => Package['twemproxy'],
+        }
+
+        file { '/etc/nutcracker/nutcracker.yml':
+            ensure  => present,
+            mode    => '0444',
+            content => template('twemproxy/config.yml.erb'),
+            notify  => Service['twemproxy'],
+        }
+
+        service { 'twemproxy':
+            ensure   => running,
+            provider => upstart,
+        }
+   }
 }
