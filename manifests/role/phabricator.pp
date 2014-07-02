6cb7997f9f7c3ef7591dca87f68486e7f33be834
cpettet <rush@wikimedia.org>
2014-07-01 19:34:20 -0500

    legalpad to radon
    * uses m3-master.eqiad.wmnet
    * phlegal namespace on m3 (per springle)
    * pass/user from password
    
    Change-Id: Ic2f8cfff9dea699535cb50ad6a98edfa0c7ae567

---
+++ manifests/role/phabricator.pp
@@ -0,0 +1,28 @@
+$current_tag = 'rt7264'
+include passwords::mysql::phabricator
+$mysql_adminuser = $passwords::mysql::phabricator::admin_user
+$mysql_adminpass = $passwords::mysql::phabricator::admin_pass
+$mysql_appuser = $passwords::mysql::phabricator::app_user
+$mysql_apppass = $passwords::mysql::phabricator::app_pass
+
+class role::phabricator::legalpad {
+
+    if $::realm == 'production' {
+
+        class { '::phabricator':
+            git_tag          => $current_tag,
+            lock_file        => '/var/run/phab_repo_lock',
+            mysql_admin_user => $mysql_adminuser,
+            mysql_admin_pass => $mysql_adminpass,
+            settings         => {
+                'darkconsole.enabled'                => true,
+                'phabricator.base-uri'               => 'https://legalpad.wikimedia.org',
+                'phabricator.show-beta-applications' => true,
+                'mysql.user'                         => $mysql_appuser,
+                'mysql.pass'                         => $mysql_apppass,
+                'mysql.host'                         => 'm3-master.eqiad.wmnet',
+                'storage.default-namespace'          => 'phlegal',
+            },
+        }
+    }
+}
