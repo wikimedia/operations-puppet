84e79c267d45bc1b70ab0ebb8e38ccde1768027c
YuviPanda <yuvipanda@gmail.com>
2014-07-03 17:32:17 +0530

    diamond: Let diamond read the puppet state file
    Currently, /var/lib/puppet is set to have no permissions for o,
    causing diamond to not be able to read the last_run_summary.yaml
    file despite that file having appropriate permissions set by
    puppet. This ensures that diamond can actually read that file.
    
    Change-Id: I615963917a5a042e645b2caec441369149a9ea8a

--- modules/diamond/manifests/collector/minimalpuppetagent.pp
+++ modules/diamond/manifests/collector/minimalpuppetagent.pp
@@ -10,6 +10,20 @@
 define diamond::collector::minimalpuppetagent {
     ensure_packages(['python-yaml'])
 
+    # Currently, /var/lib/puppet is set to have no permissions for o,
+    # causing diamond to not be able to read the last_run_summary.yaml
+    # file despite that file having appropriate permissions set by
+    # puppet. This ensures that diamond can actually read that file.
+    # Guard against puppetmasters, where the same directory is
+    # defined with the same permissions
+    if ! is_puppet_master {
+        file { '/var/lib/puppet':
+            mode   => '0751',
+            ensure => present,
+            notify => Service['diamond']
+        }
+    }
+
     diamond::collector { 'MinimalPuppetAgent':
         source  => 'puppet:///modules/diamond/collector/minimalpuppetagent.py',
         require => Package['python-yaml'],
