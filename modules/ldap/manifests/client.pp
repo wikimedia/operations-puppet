c1025dec0fee02c769e35047a41e9ef3737eff5e
Coren <marc@uberbox.org>
2014-07-03 17:28:57 -0400

    Labs: add sync-exports to repo
    (Reflects status quo)
    
    Change-Id: I589a140dee9d77e7d4a60eb3e741c31bfdfed78f

--- modules/ldap/manifests/client.pp
+++ modules/ldap/manifests/client.pp
@@ -183,6 +183,13 @@ class ldap::client::utils($ldapconfig) {
         source => 'puppet:///modules/ldap/scripts/manage-nfs-volumes-daemon',
     }
 
+    file { '/usr/local/sbin/sync-exports':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/ldap/scripts/sync-exports',
+    }
+
     file { '/usr/local/sbin/manage-keys-nfs':
         owner  => 'root',
         group  => 'root',
