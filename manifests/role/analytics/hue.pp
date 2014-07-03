1e10458aa1362a5ef8d81ea311c19bd5c721bdab
Andrew Otto <acotto@gmail.com>
2014-07-03 10:24:37 -0400

    Fix hue error in production
    prod uses cdh4
    labs uses cdh5
    
    Change-Id: I41300de1633b861a7f864d72dd7037a11f1e25f9

--- manifests/role/analytics/hue.pp
+++ manifests/role/analytics/hue.pp
@@ -19,37 +19,45 @@ class role::analytics::hue {
         include passwords::analytics
         $secret_key         = $passwords::analytics::hue_secret_key
         $hive_server_host   = 'analytics1027.eqiad.wmnet'
-        $ssl_private_key    = '/etc/ssl/private/hue.key'
-        $ssl_certificate    = '/etc/ssl/certs/hue.cert'
-        $cdh_module_name    = 'cdh4'
+
+        class { 'cdh4::hue':
+            secret_key             => $secret_key,
+            smtp_host              => $::mail_smarthost[0],
+            smtp_from_email        => "hue@$::fqdn",
+            ldap_url               => inline_template('<%= scope.lookupvar("ldap::role::config::labs::servernames").collect { |host| "ldaps://#{host}" }.join(" ") %>'),
+            ldap_bind_dn           => $ldap::role::config::labs::ldapconfig['proxyagent'],
+            ldap_bind_password     => $ldap::role::config::labs::ldapconfig['proxypass'],
+            ldap_base_dn           => $ldap::role::config::labs::basedn,
+            ldap_username_pattern  => 'uid=<username>,ou=people,dc=wikimedia,dc=org',
+            ldap_user_filter       => 'objectclass=person',
+            ldap_user_name_attr    => 'uid',
+            ldap_group_filter      => 'objectclass=posixgroup',
+            ldap_group_member_attr => 'member',
+        }
     }
     elsif ($::realm == 'labs') {
         $secret_key         = 'oVEAAG5dp02MAuIScIetX3NZlmBkhOpagK92wY0GhBbq6ooc0B3rosmcxDg2fJBM'
         # Assume that in labs, Hue should run on the main master Hadoop NameNode.
         $hive_server_host   = $role::analytics::hadoop::labs::namenode_hosts[0]
-        # Disable ssl in labs.  Labs proxy handles SSL termination.
-        $ssl_private_key    = false
-        $ssl_certificate    = false
-        $cdh_module_name    = 'cdh'
 
-    }
-
-    class { "${cdh_module_name}::hue":
-        hive_server_host       => $hive_server_host,
-        secret_key             => $secret_key,
-        smtp_host              => $::mail_smarthost[0],
-        smtp_from_email        => "hue@$::fqdn",
-        ldap_url               => inline_template('<%= scope.lookupvar("ldap::role::config::labs::servernames").collect { |host| "ldaps://#{host}" }.join(" ") %>'),
-        ldap_bind_dn           => $ldap::role::config::labs::ldapconfig['proxyagent'],
-        ldap_bind_password     => $ldap::role::config::labs::ldapconfig['proxypass'],
-        ldap_base_dn           => $ldap::role::config::labs::basedn,
-        ldap_username_pattern  => 'uid=<username>,ou=people,dc=wikimedia,dc=org',
-        ldap_user_filter       => 'objectclass=person',
-        ldap_user_name_attr    => 'uid',
-        ldap_group_filter      => 'objectclass=posixgroup',
-        ldap_group_member_attr => 'member',
-        ssl_private_key        => $ssl_private_key,
-        ssl_certificate        => $ssl_certificate,
+        class { 'cdh::hue':
+            hive_server_host       => $hive_server_host,
+            secret_key             => $secret_key,
+            smtp_host              => $::mail_smarthost[0],
+            smtp_from_email        => "hue@$::fqdn",
+            ldap_url               => inline_template('<%= scope.lookupvar("ldap::role::config::labs::servernames").collect { |host| "ldaps://#{host}" }.join(" ") %>'),
+            ldap_bind_dn           => $ldap::role::config::labs::ldapconfig['proxyagent'],
+            ldap_bind_password     => $ldap::role::config::labs::ldapconfig['proxypass'],
+            ldap_base_dn           => $ldap::role::config::labs::basedn,
+            ldap_username_pattern  => 'uid=<username>,ou=people,dc=wikimedia,dc=org',
+            ldap_user_filter       => 'objectclass=person',
+            ldap_user_name_attr    => 'uid',
+            ldap_group_filter      => 'objectclass=posixgroup',
+            ldap_group_member_attr => 'member',
+            # Disable ssl in labs.  Labs proxy handles SSL termination.
+            ssl_private_key        => false,
+            ssl_certificate        => false,
+        }
     }
 }
 
