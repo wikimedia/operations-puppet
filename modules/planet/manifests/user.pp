a288bb6f240447cc371f370c41633dbc2c3487fc
dzahn <dzahn@wikimedia.org>
2014-07-03 16:48:56 -0700

    planet - add system group for system user
    Change-Id: I43a1f97776b9caa69361e073f1b3686161339e3b

--- modules/planet/manifests/user.pp
+++ modules/planet/manifests/user.pp
@@ -1,6 +1,12 @@
 # adds a system user for a planet-venus install
 class planet::user {
 
+    group { 'planet':
+        ensure => present,
+        name   => 'planet',
+        system => true,
+    }
+
     user { 'planet':
         home       => '/var/lib/planet',
         groups     => [ 'planet' ],
