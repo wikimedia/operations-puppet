dbd441cfe8549cd6d646c5510a98e56b7e415f4d
Matt Walker <mwalker@khaosdev.com>
2014-07-07 16:20:36 -0700

    New Variables for OCG Service
    The service is now able to store output files in a discrete directory
    for file management purposes. This is the output_dir. These are saved
    for 5 days currently.
    
    It can also save files which caused errors in a discrete directory. This
    is the postmortem_dir. Right now puppet will save these for 3 days.
    
    The service will also now log to logstash via the GELF structured logging
    format.
    
    Change-Id: Id46ce90de1fe8b288c0a205bd3de8362b473e708

--- modules/ocg/manifests/init.pp
+++ modules/ocg/manifests/init.pp
@@ -19,7 +19,11 @@ class ocg (
     $statsd_host = 'localhost',
     $statsd_port = 8125,
     $statsd_is_txstatsd = 0,
-    $temp_dir = '/srv/deployment/ocg/tmp'
+    $graylog_host = 'localhost',
+    $graylog_port = 12201,
+    $temp_dir = '/srv/deployment/ocg/tmp',
+    $output_dir = '/srv/deployment/ocg/output',
+    $postmortem_dir = '/srv/deployment/ocg/postmortem'
 ) {
     deployment::target { 'ocg': }
 
@@ -122,6 +126,34 @@ class ocg (
         ensure  => directory,
         owner   => 'ocg',
         group   => 'ocg',
+    }
+
+    file { $output_dir:
+        ensure  => directory,
+        owner   => 'ocg',
+        group   => 'ocg',
+    }
+
+    cron { "Clean up OCG output directory":
+        ensure  => present,
+        command => "find ${output_dir}* -mtime +5 -exec rm {} \;",
+        user    => 'ocg',
+        hour    => 0,
+        minute  => 0,
+    }
+
+    file { $postmortem_dir:
+        ensure  => directory,
+        owner   => 'ocg',
+        group   => 'ocg',
+    }
+
+    cron { "Clean up OCG postmortem directory":
+        ensure  => present,
+        command => "find ${output_dir}* -mtime +3 -exec rm {} \;",
+        user    => 'ocg',
+        hour    => 0,
+        minute  => 0,
     }
 
     file { '/var/log/ocg':
