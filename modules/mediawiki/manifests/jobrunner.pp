efad090db1c7f5d17a13bfc22d55d74e21295914
Ori Livneh <ori@wikimedia.org>
2014-07-07 16:32:48 -0700

    Add jobrunner class
    'jobrunner' is the projected replacement for the current job-loop.sh shell
    script. This patch configures its repository to be deployed via Trebuchet and
    for a simple service to be managed by Puppet.
    
    Change-Id: Iff2c6c27147592a9d0afc95c9770c9841814c1a1

---
+++ modules/mediawiki/manifests/jobrunner.pp
@@ -0,0 +1,29 @@
+# == Class: mediawiki::jobrunner
+#
+# jobrunner continuously processes the MediaWiki job queue by dispatching
+# workers to perform tasks and monitoring their success or failure.
+#
+class mediawiki::jobrunner {
+    deployment::target { 'jobrunner': }
+
+    file { '/etc/default/jobrunner':
+        source => 'puppet:///modules/mediawiki/jobrunner.default',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        notify => Service['jobrunner'],
+    }
+
+    file { '/etc/init/jobrunner.conf':
+        source => 'puppet:///modules/mediawiki/jobrunner.conf',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        notify => Service['jobrunner'],
+    }
+
+    service { 'jobrunner':
+        ensure   => 'disabled',  # for now!
+        provider => 'upstart',
+    }
+}
