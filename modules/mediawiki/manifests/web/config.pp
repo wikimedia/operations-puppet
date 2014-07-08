4c95a277d5fd3590071c928c0c17bcba8f0ea1b9
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-07-08 12:56:08 +0200

    mediawiki: add apache-config as a submodule, upload it via puppet
    This is an intermediate commit that will allow us to evaluate the behaviour of tagged
    runs to deploy code.
    
    Change-Id: Ic2b455a5461e108c5f1a2ea4a76b5781926e5e8d
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/mediawiki/manifests/web/config.pp
@@ -0,0 +1,15 @@
+class mediawiki::web::config ($use_local_resources = true) {
+    tag 'mediawiki', 'mw-apache-config'
+    file { '/etc/apache2/wikimedia':
+        ensure  => directory,
+        recurse => true,
+        source  => 'puppet:///modules/mediawiki/apache/config',
+        notify  => Service['apache'],
+    }
+    exec { 'sync_apache_config':
+        command => '/usr/bin/rsync -av 10.0.5.8::httpdconf/ /usr/local/apache/conf',
+        creates => '/usr/local/apache/conf',
+        require => File['/usr/local/apache'],
+        notify  => Service['apache'],
+    }
+}
