wikimedia/operations-puppet

commit d7c02f375653399d7a39fe93093c1117c8ccbad2

bacula-dir reads a copy of puppet's private key
Puppet handles the permissions of its private key file aggresively
setting to 600, too restrictive for any other group/user to read it.
Copy it, keep it in sync and set the require permissions for bacula-dir
to be able to read it. This is needed for bacula-dir to reload. The
aggresive handling of permissions might be a change in puppet 3.x

Change-Id: Ibd632a3fd03eea3493b0406e89bfad7e57c74a52

modules/bacula/manifests/director.pp
@@ -44,6 +44,25 @@ class bacula::director(
     File <<| tag == "bacula-client-${::fqdn}" |>>
     File <<| tag == "bacula-storage-${::fqdn}" |>>
 
+    # Puppet manages the permissions of its private key file and they are too
+    # restrictive to allow any other user/group to read it. Copy it, keep it in
+    # sync and set the require permissions for bacula-dir to be able to read it
+    exec { 'bacula_cp_private_key':
+        command => "/bin/cp /var/lib/puppet/ssl/private_keys/${::fqdn}.pem > \
+ /var/lib/puppet/ssl/private_keys/bacula-${::fqdn}.pem",
+        unless => "/usr/bin/cmp /var/lib/puppet/ssl/private_keys/${::fqdn}.pem > \
+ /var/lib/puppet/ssl/private_keys/bacula-${::fqdn}.pem",
+    }
+
+    file { "/var/lib/puppet/ssl/private_keys/bacula-${::fqdn}.pem":
+        ensure  => present,
+        owner   => 'bacula',
+        group   => 'bacula',
+        mode    => '0400',
+        require => Exec['bacula_cp_private_key'],
+        notify  => Service['bacula-director'],
+    }
+
     file { '/etc/bacula/bacula-dir.conf':
         ensure  => present,
         owner   => root,

modules/bacula/templates/bacula-dir.conf.erb
@@ -16,7 +16,7 @@ Director {
     TLS CA Certificate File = "/var/lib/puppet/ssl/certs/ca.pem"
     TLS Verify Peer = no
     TLS Certificate = "/var/lib/puppet/ssl/certs/<%= @fqdn %>.pem"
-    TLS Key = "/var/lib/puppet/ssl/private_keys/<%= @fqdn %>.pem"
+    TLS Key = "/var/lib/puppet/ssl/private_keys/bacula-<%= @fqdn %>.pem"
     # Heartbeat Interval = 0 #in secs
     # DirAddress =
     # DirSourceAddress =

commit fb46b146c47701abca77fcd6aa4783afc08f2eeb

Fix typo in bacula module
Introduced in d7c02f3, note the extra > characters

Change-Id: I9ce9f2f5b094390cb4a66559148a545f1af749c2

modules/bacula/manifests/director.pp
@@ -48,9 +48,9 @@ class bacula::director(
     # restrictive to allow any other user/group to read it. Copy it, keep it in
     # sync and set the require permissions for bacula-dir to be able to read it
     exec { 'bacula_cp_private_key':
-        command => "/bin/cp /var/lib/puppet/ssl/private_keys/${::fqdn}.pem > \
+        command => "/bin/cp /var/lib/puppet/ssl/private_keys/${::fqdn}.pem \
  /var/lib/puppet/ssl/private_keys/bacula-${::fqdn}.pem",
-        unless => "/usr/bin/cmp /var/lib/puppet/ssl/private_keys/${::fqdn}.pem > \
+        unless => "/usr/bin/cmp /var/lib/puppet/ssl/private_keys/${::fqdn}.pem \
  /var/lib/puppet/ssl/private_keys/bacula-${::fqdn}.pem",
     }
 