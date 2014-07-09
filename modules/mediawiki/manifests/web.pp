1009f2f6fa114a34c6a3e7c619ead636a8536c7d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-07-09 10:42:16 +0200

    mediawiki: manage with puppet on all nodes
    Now all the apache config on mediawiki appservers is managed via puppet,
    on all nodes. A change to the apache config will trigger a refresh of
    the apache service.
    
    Change-Id: I282f371685feca8144cacb0092521b0f6a78293c
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/mediawiki/manifests/web.pp
+++ modules/mediawiki/manifests/web.pp
@@ -5,40 +5,10 @@ class mediawiki::web ( $workers_limit = undef ) {
     include ::mediawiki::monitoring::webserver
     include ::mediawiki::web::config
 
-    if is_integer($workers_limit) {
-        $max_req_workers = $workers_limit
-    } else {
-        $mem_available   = to_bytes($::memorytotal) * 0.7
-        $mem_per_worker  = to_bytes('85M')
-        $max_req_workers = inline_template('<%= ( @mem_available / @mem_per_worker ).to_i %>')
-    }
-
-    if $mw_use_local_resources == undef {
-        # This is used in the apache2.conf template to switch from
-        # rsync-based resources to puppet-managed ones
-        $mw_use_local_resources = false
-    }
-
-    file { '/etc/apache2/apache2.conf':
-        content => template('mediawiki/apache/apache2.conf.erb'),
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        before  => Service['apache'],
-    }
-
-    file { '/etc/apache2/envvars':
-        source => 'puppet:///modules/mediawiki/apache/envvars.appserver',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        before => Service['apache'],
-    }
 
     file { '/usr/local/apache':
         ensure => directory,
     }
-
 
     service { 'apache':
         ensure    => running,
@@ -54,6 +24,8 @@ class mediawiki::web ( $workers_limit = undef ) {
         unless  => '/bin/ps -C apache2',
         notify  => Exec['mw-sync'],
     }
+
+
 
     # Has to be less than apache, and apache has to be nice 0 or less to be
     # blue in ganglia.
--- modules/mediawiki/manifests/web/config.pp
+++ modules/mediawiki/manifests/web/config.pp
@@ -1,5 +1,32 @@
 class mediawiki::web::config () {
     tag 'mediawiki', 'mw-apache-config'
+
+    if is_integer($::mediawiki::web::workers_limit) {
+        $max_req_workers = $::mediawiki::web::workers_limit
+    } else {
+        $mem_available   = to_bytes($::memorytotal) * 0.7
+        $mem_per_worker  = to_bytes('85M')
+        $max_req_workers = inline_template('<%= ( @mem_available / @mem_per_worker ).to_i %>')
+    }
+
+    file { '/etc/apache2/apache2.conf':
+        content => template('mediawiki/apache/apache2.conf.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        before  => Service['apache'],
+        notify  => Service['apache'],
+    }
+
+    file { '/etc/apache2/envvars':
+        source => 'puppet:///modules/mediawiki/apache/envvars.appserver',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        before => Service['apache'],
+    }
+
+
     file { '/etc/apache2/wikimedia':
         ensure  => directory,
         recurse => true,
