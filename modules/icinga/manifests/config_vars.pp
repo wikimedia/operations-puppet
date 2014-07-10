2aba93cd6d464ce8de42bc40b4039b7f54d3bfce
dzahn <dzahn@wikimedia.org>
2014-07-10 16:44:51 -0700

    turn icinga into module
    PS1:
    
    for Yuvi :)
    
    turn icinga into module. this will require more work.
    
    but the very first step is to separate
    the classes from one huge misc/file into separate .pp
    files
    
    i wonder why we already had an empty /modules/icinga/
    
    WIP ...won't work yet at all
    
    PS2:
    
    classes renamed as follows:
    
    icinga::monitor::configuration::variables ->  icinga::config_vars
    icinga::monitor::firewall                 ->  icinga::firewall
    icinga::ganglia::ganglios                 ->  icinga::ganglios
    icinga::monitor::logrotate                ->  icinga::logrotate
    icinga::monitor::files::misc              ->  icinga::misc_files
    icinga::monitor                           ->  icinga  (init.pp)
    icinga::monitor::service                  ->  icinga::service
    icinga::monitor::naggen                   ->  icinga::naggen
    icinga::monitor::nsca::daemon             ->  icinga::nsca
    icinga::nsca                              ->  icinga::nsca
    icinga::monitor::packages                 ->  icinga::packages
    icinga::monitor::files::nagios-plugins    ->  icinga::plugins
    icinga::monitor::snmp                     ->  icinga::snmp
    icinga::user                              ->  icinga::user
    
    PS3: move all the files and templates  into module (except snmp)
    
    Change-Id: Ic52822905fbc97d8ece301589b0f64fb0f0f3e55

---
+++ modules/icinga/manifests/config_vars.pp
@@ -0,0 +1,30 @@
+# Nagios/icinga configuration files
+
+class icinga::config_vars {
+
+    # This variable declares the monitoring hosts It is called master hosts as
+    # monitor_host is already a service.
+    $master_hosts = [ 'neon.wikimedia.org' ]
+
+    $icinga_config_dir = '/etc/icinga'
+    $nagios_config_dir = '/etc/nagios'
+
+    # puppet_hosts.cfg must be first
+    $puppet_files = [
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/puppet_hostgroups.cfg",
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/puppet_servicegroups.cfg"]
+
+    $static_files = [
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/puppet_hostextinfo.cfg",
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/puppet_services.cfg",
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/icinga.cfg",
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/analytics.cfg", # TEMP.  This will be removed when analytics puppetization goes to production
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/cgi.cfg",
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/checkcommands.cfg",
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/contactgroups.cfg",
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/contacts.cfg",
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/misccommands.cfg",
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/resource.cfg",
+        "${icinga::monitor::configuration::variables::icinga_config_dir}/timeperiods.cfg"]
+}
+
