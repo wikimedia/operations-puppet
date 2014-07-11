787db7b483b7e1304dd9dddf0380b5a7529a1dbb
Ori Livneh <ori@wikimedia.org>
2014-07-10 22:31:13 -0700

    mediawiki: move SSHD nice override from web.pp to init.pp
    It's a bit bizarre for SSHD to be nice'd -10 on the web appservers but not the
    rest. Applying it across all app servers makes things more uniform.
    
    ::ssh::server is already included on all servers because it's part of ::base.
    
    Change-Id: I37694e4ab186d2ac9ca60c4b1fcd75e38ad9bbd8

--- modules/mediawiki/manifests/web.pp
+++ modules/mediawiki/manifests/web.pp
@@ -5,7 +5,6 @@ class mediawiki::web ( $workers_limit = undef ) {
     include ::mediawiki::monitoring::webserver
     include ::mediawiki::web::config
 
-
     file { '/usr/local/apache':
         ensure => directory,
     }
@@ -23,16 +22,5 @@ class mediawiki::web ( $workers_limit = undef ) {
         command => '/bin/true',
         unless  => '/bin/ps -C apache2',
         notify  => Exec['mw-sync'],
-    }
-
-
-
-    # Has to be less than apache, and apache has to be nice 0 or less to be
-    # blue in ganglia.
-    file { '/etc/init/ssh.override':
-        content => "nice -10\n",
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
     }
 }
