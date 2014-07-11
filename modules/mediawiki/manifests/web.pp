3f8a06b406b4438a35503eafbe233cb52332ec06
Ori Livneh <ori@wikimedia.org>
2014-07-11 12:12:22 -0700

    mediawiki: use apache module
    The MediaWiki module defines and manages its own apache service because it uses
    apache service status as a way of detecting a server that has just been
    newly provisioned or rebooted.
    
    The way it is currently supposed to work is this:
    * the Apache service is not started on boot; it depends on Puppet to start it.
    * but Service['apache'] subscribes to Exec['mw-sync'], so mw-sync is run first.
    * mw-sync only runs unless '/bin/ps -C apache2', so if Apache is already
      running, it doesn't run.
    
    The idea is that the first Puppet run syncs the server and starts Apache.
    
    I say "supposed to work" above because the sync-common script which mw-sync
    invokes is a symlink to a file in /srv/scap which is manually deployed via
    Trebuchet.  So a newly-provisioned server can't bootstrap itself anyhow.
    
    I thought of fixing this comprehensively in a single commit, but it is too
    risky. I think the right approach is to simply remove it for now. Keeping code
    around that is known not to work is pernicious and misleading, and certainly
    much worse than not having it at all.
    
    Change-Id: I17344357a67527fe6f55eb3c9c9bda46de2cd201

--- modules/mediawiki/manifests/web.pp
+++ modules/mediawiki/manifests/web.pp
@@ -10,23 +10,6 @@ class mediawiki::web ( $workers_limit = undef ) {
         ensure => directory,
     }
 
-    service { 'apache':
-        ensure    => running,
-        name      => 'apache2',
-        enable    => false,
-        subscribe => Exec['mw-sync'],
-        require   => File['/etc/cluster'],
-    }
-
-    # Sync the server when we see apache is not running
-    exec { 'apache-trigger-mw-sync':
-        command => '/bin/true',
-        unless  => '/bin/ps -C apache2',
-        notify  => Exec['mw-sync'],
-    }
-
-
-
     # Has to be less than apache, and apache has to be nice 0 or less to be
     # blue in ganglia.
     file { '/etc/init/ssh.override':
