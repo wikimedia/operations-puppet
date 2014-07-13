be68a2f722e00b7d2d0aded8c574311735cff1ff
Bryan Davis <bd808@wikimedia.org>
2014-07-13 14:18:45 -0600

    labs_vagrant: Install to /srv/vagrant
    In pmtpa labs, instances had a large secondary disk mounted on /mnt.
    Yuvi had the labs_vagrant class install to this directory due to the
    potentially large size of a working MediaWiki-Vagrant checkout. In
    eqiad, secondary disks must be mounted explicitly. Update
    role::labs::vagrant to require role::labs::lvm::srv which makes a large
    secondary disk available at /srv. Change ::labs_vagrant to install to
    /srv/vagrant rather than /mnt/vagrant. ::labs_vagrant includes new logic
    to migrate existing /mnt/vagrant deployments to the new /srv/vagrant
    location transparently.
    
    Change-Id: I06c1e5a537bbf923ac5a7b0a787f4e9152bd9045

--- manifests/role/labsvagrant.pp
+++ manifests/role/labsvagrant.pp
@@ -1,5 +1,5 @@
 # Install Mediawiki-Vagrant puppet repo, manage manually from CLI
-
 class role::labs::vagrant {
-    class { '::labs_vagrant': }
+    require ::role::labs::lvm::srv
+    include ::labs_vagrant
 }
