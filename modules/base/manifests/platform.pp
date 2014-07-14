94f140a7e67fbb397728ca71f7f810ab47d54aff
Dzahn <dzahn@wikimedia.org>
2014-07-14 14:27:39 +0000

    Revert "platform: simplify hardware specific configuration"
    This reverts commit 4c5cd6266e45a0d9b0fa7cd5d5bf10ebcc9aff12.
    
    Change-Id: I890ee89e598b947c45605c44891542b2d483e024

--- modules/base/manifests/platform.pp
+++ modules/base/manifests/platform.pp
@@ -4,36 +4,70 @@
 class base::platform {
     case $::productname {
         'PowerEdge C2100': {
-            $startup_drives   = [ '/dev/sda', '/dev/sdb' ]
-            $lom_serial_port  = 'ttyS1'
-            $lom_serial_speed = '115200'
+            $startup_drives = [ '/dev/sda', '/dev/sdb' ]
         }
         'PowerEdge R300': {
-            $startup_drives   = [ '/dev/sda', '/dev/sdb']
-            $lom_serial_port  = 'ttyS1'
-            $lom_serial_speed = '57600'
+            $startup_drives = [ '/dev/sda', '/dev/sdb']
+            include base::platform::dell-r300
         }
         'R250-2480805': {
-            $startup_drives   = [ '/dev/sda', '/dev/sdb' ]
-            $lom_serial_port  = 'ttyS0'
-            $lom_serial_speed = '115200'
+            $startup_drives = [ '/dev/sda', '/dev/sdb' ]
+            include base::platform::cisco-C250-M1
         }
         default: {
             # set something so the logic doesn't puke
             $startup_drives = [ '/dev/sda', '/dev/sdb' ]
         }
     }
+}
+
+class base::platform::common($lom_serial_port, $lom_serial_speed) {
+    $console_upstart_file = "
+# ${lom_serial_port} - getty
+#
+# This service maintains a getty on ${lom_serial_port} from the point the system is
+# started until it is shut down again.
+
+start on stopped rc RUNLEVEL=[2345]
+stop on runlevel [!2345]
+
+respawn
+exec /sbin/getty -L ${lom_serial_port} ${$lom_serial_speed} vt102
+"
 
     if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '10.04') >= 0 {
         file { "/etc/init/${lom_serial_port}.conf":
-            owner   => 'root',
-            group   => 'root',
+            owner   => root,
+            group   => root,
             mode    => '0444',
-            content => template('base/console_upstart.erb'),
+            content => $console_upstart_file;
         }
-        generic::upstart_job { $lom_serial_port:
-            require => File["/etc/init/${lom_serial_port}.conf"],
-        }
+        generic::upstart_job { $lom_serial_port: require => File["/etc/init/${lom_serial_port}.conf"] }
     }
+}
+
+class base::platform::dell-c2100 inherits base::platform::generic::dell {
+    $lom_serial_speed = '115200'
+
+    class { 'common': lom_serial_port => $lom_serial_port, lom_serial_speed => $lom_serial_speed }
+}
+
+class base::platform::dell-r300 inherits base::platform::generic::dell {
+    $lom_serial_speed = '57600'
+
+    class { 'common': lom_serial_port => $lom_serial_port, lom_serial_speed => $lom_serial_speed }
+}
+
+class base::platform::cisco-C250-M1 inherits base::platform::generic::cisco {
+    class { 'common': lom_serial_port => $lom_serial_port, lom_serial_speed => $lom_serial_speed }
+}
+
+class base::platform::generic::dell {
+    $lom_serial_port = 'ttyS1'
+}
+
+class base::platform::generic::cisco {
+    $lom_serial_port = 'ttyS0'
+    $lom_serial_speed = '115200'
 }
 
