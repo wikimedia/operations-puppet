ffbf40607da70d1f2cbb275b7e09c8bf4fbe8bfd
Brandon Black <bblack@wikimedia.org>
2014-07-14 11:01:02 -0500

    beta::natfix removal step 1
    beta::natdestrewrite is only used by beta::natfix.  Switching
    it to ensure => absent first to remove the rules from the hosts
    pending removal of the role/class.  This functionality was
    replaced with dnsmasq hacks in:
    
    aee21e6 Add public->private mappings for labs to dnsmasq aliases
    
    Bonus upside: if anything breaks due to this, it's trivial to
    revert.
    
    Change-Id: Ie8d66c462abdb35c566f1c53607a0d07d84b2d62

--- modules/beta/manifests/natdestrewrite.pp
+++ modules/beta/manifests/natdestrewrite.pp
@@ -4,6 +4,7 @@ define beta::natdestrewrite( $public_ip, $private_ip ) {
 
     # iptables -t nat -I OUTPUT --dest $public_ip -j DNAT --to-dest $private_ip
     ferm::rule { "nat_rewrite_for_${name}":
+        ensure => 'absent',
         table  => 'nat',
         chain  => 'OUTPUT',
         domain => 'ip',
