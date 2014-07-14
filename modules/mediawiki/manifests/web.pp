954e8240101e9fc4604e42f9d906cef53c7920ad
Ori Livneh <ori@wikimedia.org>
2014-07-14 10:57:55 -0700

    mediawiki: move File['/usr/local/apache'] from web.pp -> sync.pp
    sync.pp is included on all app servers, and it depends on /usr/local/apache, so
    we can't only define it on web nodes.
    
    Change-Id: I05bb392c4e7efb14e191bee55d869184eab60459

--- modules/mediawiki/manifests/web.pp
+++ modules/mediawiki/manifests/web.pp
@@ -4,8 +4,4 @@ class mediawiki::web ( $workers_limit = undef ) {
     include ::mediawiki
     include ::mediawiki::monitoring::webserver
     include ::mediawiki::web::config
-
-    file { '/usr/local/apache':
-        ensure => directory,
-    }
 }
