75db0fb89d011de2d3a4f1c9eb529906207949bc
Marius Hoch <hoo@online.de>
2014-07-15 16:59:06 +0200

    Introduce snapshot::wikidatajsondump
    This is to create a weekly json dump of Wikidata.
    
    The current implementation is as simple as possible,
    thus doesn't allow much fancy configuration etc.
    (although I doubt that's even needed).
    
    Change-Id: I54ed9436e4dce8fe1e27456f3603fafec5403299

---
+++ modules/snapshot/files/dumpwikidatajson.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Generate a json dump for Wikidata and remove old ones.
+#
+# @author Marius Hoch < hoo@online.de >
+
+
+configfile="/srv/dumps/confs/wikidump.conf"
+
+apacheDir=`egrep "^dir=" "$configfile" | mawk -Fdir= '{ print $2 }'`
+targetDir=`egrep "^public=" "$configfile" | mawk -Fpublic= '{ print $2 }'`/other/wikidata
+tempDir=`egrep "^temp=" "$configfile" | mawk -Ftemp= '{ print $2 }'`
+
+multiversionscript="${apacheDir}/multiversion/MWScript.php"
+
+targetFile=$targetDir/`date +'%Y%m%d'`.json.gz
+
+i=0
+shards=4
+
+while [ $i -lt $shards ]; do
+	sudo -u apache php $multiversionscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dumpJson.php --wiki wikidatawiki --shard $i --sharding-factor $shards --snippet | gzip > $tempDir/wikidataJson.$i.gz &
+	let i++
+done
+
+wait
+
+i=0
+
+echo '[' | gzip -f > $targetFile
+
+while [ $i -lt $shards ]; do
+	cat $tempDir/wikidataJson.$i.gz >> $targetFile
+	rm $tempDir/wikidataJson.$i.gz
+	let i++
+	if [ $i -lt $shards ]; then
+		# Shards don't end with commas so add commas to separate them
+		echo -n ',' | gzip -f >> $targetFile
+	fi
+done
+
+echo ']' | gzip -f >> $targetFile
+
+# Remove dumps we no longer need (keep 10)
+filesToDelete=`ls -r $targetDir/20*.gz | tail -n +11`
+if [ -n "$filesToDelete" ]; then
+	rm $filesToDelete
+fi
