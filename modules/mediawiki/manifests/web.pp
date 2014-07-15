bc9b47fd3a8ac746785527b3866f660c6036a238
Ori Livneh <ori@wikimedia.org>
2014-07-15 10:00:13 -0700

    Fold mediawiki::web::config back into mediawiki::web
    Change-Id: I722cf4727d1bb26835051599f41665548f840d4f

--- modules/mediawiki/manifests/web.pp
+++ modules/mediawiki/manifests/web.pp
@@ -1,8 +1,45 @@
 # mediawiki::web
 
 class mediawiki::web ( $workers_limit = undef ) {
+    tag 'mediawiki', 'mw-apache-config'
+
     include ::mediawiki
     include ::mediawiki::monitoring::webserver
-    include ::mediawiki::web::config
     include ::apache
+
+    $apache_server_limit = 256
+
+    if is_integer($workers_limit) {
+        $max_req_workers = $workers_limit
+    } else {
+        $mem_available   = to_bytes($::memorytotal) * 0.7
+        $mem_per_worker  = to_bytes('85M')
+        $max_req_workers = inline_template('<%= [( @mem_available / @mem_per_worker ).to_i, @apache_server_limit.to_i].min %>')
+    }
+
+    file { '/etc/apache2/apache2.conf':
+        content => template('mediawiki/apache/apache2.conf.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        before  => Service['apache2'],
+        require => Package['apache2'],
+    }
+
+    file { '/etc/apache2/envvars':
+        source  => 'puppet:///modules/mediawiki/apache/envvars.appserver',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        before  => Service['apache2'],
+        require => Package['apache2'],
+    }
+
+    file { '/etc/apache2/wikimedia':
+        ensure  => directory,
+        source  => 'puppet:///modules/mediawiki/apache/config',
+        recurse => true,
+        before  => Service['apache2'],
+        require => Package['apache2'],
+    }
 }
