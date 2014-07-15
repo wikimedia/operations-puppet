a31603a6ca6f397fb5db6259c6d6875c470a8ca4
Bryan Davis <bd808@wikimedia.org>
2014-07-15 16:28:06 -0600

    labs_vagrant: Ensure that lvm volume is mounted first
    Tell puppet to make sure to mount the LVM managed disk before applying
    Labs_vagrant. There is still a possible issue with this hiding content
    that existed on the /srv mount point before the LVM volume is added, but
    that seems like an issue that may be better solved in Labs_lvm::Volume
    itself.
    
    Change-Id: I8cb5f94fcacd3498823bea1e2de342f2e6a4548b

--- manifests/role/labsvagrant.pp
+++ manifests/role/labsvagrant.pp
@@ -1,5 +1,8 @@
 # Install Mediawiki-Vagrant puppet repo, manage manually from CLI
 class role::labs::vagrant {
-    require ::role::labs::lvm::srv
+    include ::role::labs::lvm::srv
     include ::labs_vagrant
+
+    # Mount secondary disk before applying labs_vagrant
+    Labs_lvm::Volume <| |> -> Labs_vagrant <| |>
 }
