dec71de3f6ea4480a7261bb9053ab4ac9461f883
Marius Hoch <hoo@online.de>
2014-07-15 16:59:06 +0200

    Introduce snapshot::wikidatajsondump
    This is to create a weekly json dump of Wikidata.
    
    The current implementation is as simple as possible,
    thus doesn't allow much fancy configuration etc.
    (although I doubt that's even needed).
    
    Change-Id: I54ed9436e4dce8fe1e27456f3603fafec5403299

---
+++ modules/snapshot/manifests/wikidatajsondumps.pp
@@ -0,0 +1,33 @@
+class snapshot::wikidatajsondump(
+    $enable = true,
+    $user   = undef,
+) {
+    if ($enable == true) {
+        $ensure = 'present'
+    }
+    else {
+        $ensure = 'absent'
+    }
+
+    system::role {
+        'snapshot::wikidatajsondump':
+        ensure => $ensure,
+        description => 'Misc - Maintenance Server: parser cache purging'
+    }
+
+    file { '/usr/local/bin/dumpwikidatajson.sh':
+        mode    => '0755',
+        owner   => 'root',
+        group   => 'root',
+        source  => 'puppet:///modules/snapshot/dumpwikidatajson.sh',
+    }
+
+    cron { 'wikidatajson-dump':
+        ensure      => $ensure,
+        command     => "/usr/local/bin/dumpwikidatajson.sh",
+        user        => $user,
+        minute      => '15',
+        hour        => '3',
+        weekday     => '1',
+    }
+}
