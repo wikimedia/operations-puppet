6fa2005007d7f7b5903853b3fbbae99b4ea1d491
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2014-07-15 16:27:49 +0100

    releases: add reprepro repository
    This change introduces the reprepro module and its usage inside the releases
    role.
    
    Most of the functionality is straightforward, however the gpg keyring
    "management" is something new. The basic idea is to have the ascii export for
    keys and compose them into a keyring to be committed. This means binary files
    in git but that should be okay given that not many keys are expected and thus
    keyring size would be too big and change too often.
    
    If there seem to be a lot of work done inside releases::reprepro as opposed to the
    reprepro module proper, that is the "repository-specific" setup work, in this
    case specific to releases (e.g. gpg keyrings)
    
    Change-Id: I65116dab65306d8be045e1ca7cdbd9ce7d465853

---
+++ modules/reprepro/manifests/distribution.pp
@@ -0,0 +1,35 @@
+# == Class: reprepro::distribution
+#
+#   Configures a reprepro distribution
+#
+# === Parameters
+#
+#   - *basedir*: The reprepro base directory.
+#   - *settings*: A distributions config map, each key => value will be
+#   converted to key: value.
+#
+# === Example
+#
+#   class { 'reprepro::distribution':
+#     basedir => 'foo',
+#     settings => {
+#       'distro1' => {
+#          'Origin' => 'foo',
+#          'Version' => '0.01',
+#       }
+#     }
+#   }
+#
+
+class reprepro::distribution (
+    $basedir,
+    $settings,
+) {
+    file { "${basedir}/conf/distributions":
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        ensure  => file,
+        content => template('reprepro/distributions.erb'),
+    }
+}
---
+++ modules/reprepro/manifests/init.pp
@@ -0,0 +1,160 @@
+# == Class: reprepro
+#
+#   Configures reprepro on a server
+#
+# === Parameters
+#
+#   - *basedir*: The reprepro base directory.
+#   - *homedir*: The reprepro user home directory.
+#   - *user*: The user name to run reprepro under.
+#   - *group*: The group name to run reprepro under.
+#   - *notify_address*: Where to send upload notifications.
+#   - *options*: A list of options for reprepro (see conf/options file).
+#   - *uploaders*: A list of uploaders instructions (see "uploaders file")
+#   - *incomingdir*: Path considered for incoming uploads.
+#   - *default_distro*: The default distribution if none specified.
+#   - *gpg_secring*: The GPG secret keyring for reprepro to use.
+#   - *gpg_pubring*: The GPG public keyring for reprepro to use.
+#
+# === Example
+#
+#   class { 'reprepro':
+#       basedir => "/tmp/reprepro",
+#   }
+#
+class reprepro (
+    $basedir,
+    $homedir        = '/var/lib/reprepro',
+    $user           = 'reprepro',
+    $group          = 'reprepro',
+    $notify_address = 'root@wikimedia.org',
+    $options        = [],
+    $uploaders      = [],
+    $incomingdir    = 'incoming',
+    $default_distro = 'trusty',
+    $gpg_secring    = undef,
+    $gpg_pubring    = undef,
+) {
+
+    package { 'reprepro':
+        ensure => present,
+    }
+
+    group { 'reprepro':
+        ensure => present,
+        name   => $group,
+    }
+
+    user { 'reprepro':
+        ensure     => present,
+        name       => $user,
+        home       => $homedir,
+        shell      => '/bin/bash',
+        comment    => 'Reprepro user',
+        gid        => 'reprepro',
+        managehome => true,
+        require    => Group['reprepro'],
+    }
+
+    file { $basedir:
+        ensure  => directory,
+        owner   => $user,
+        group   => $group,
+        mode    => '0755',
+        require => User['reprepro'],
+    }
+
+    file { "${basedir}/conf":
+        ensure  => directory,
+        owner   => $user,
+        group   => $group,
+        mode    => '0555',
+        require => User['reprepro'],
+    }
+
+    file { "${basedir}/db":
+        ensure  => directory,
+        owner   => $user,
+        group   => $group,
+        mode    => '0755',
+        require => User['reprepro'],
+    }
+
+    file { "${basedir}/logs":
+        ensure  => directory,
+        owner   => $user,
+        group   => $group,
+        mode    => '0755',
+        require => User['reprepro'],
+    }
+
+    file { "${basedir}/tmp":
+        ensure  => directory,
+        owner   => $user,
+        group   => $group,
+        mode    => '0755',
+        require => User['reprepro'],
+    }
+
+    file { "${basedir}/conf/options":
+        ensure  => file,
+        owner   => $user,
+        group   => $group,
+        mode    => '0444',
+        content => inline_template("<%= @options.join(\"\n\") %>\n"),
+    }
+
+    file { "${basedir}/conf/uploaders":
+        ensure  => file,
+        owner   => $user,
+        group   => $group,
+        mode    => '0444',
+        content => inline_template("<%= @uploaders.join(\"\n\") %>\n"),
+    }
+
+    file { "${basedir}/conf/incoming":
+        ensure  => file,
+        owner   => $user,
+        group   => $group,
+        mode    => '0444',
+        content => template("reprepro/incoming.erb"),
+    }
+
+    file { "${basedir}/conf/log":
+        ensure  => file,
+        owner   => $user,
+        group   => $group,
+        mode    => '0755',
+        content => template("reprepro/log.erb"),
+    }
+
+    file { "${homedir}/.gnupg":
+        ensure  => directory,
+        owner   => $user,
+        group   => $group,
+        mode    => '0700',
+        require => User['reprepro'],
+    }
+
+    if $gpg_secring != undef {
+        file { "${homedir}/.gnupg/secring.gpg":
+            ensure  => file,
+            owner   => $user,
+            group   => $group,
+            mode    => '0400',
+            source  => $gpg_secring,
+            require => User['reprepro'],
+        }
+    }
+
+    if $gpg_pubring != undef {
+        file { "${homedir}/.gnupg/pubring.gpg":
+            ensure  => file,
+            owner   => $user,
+            group   => $group,
+            mode    => '0400',
+            source  => $gpg_pubring,
+            require => User['reprepro'],
+        }
+    }
+}
