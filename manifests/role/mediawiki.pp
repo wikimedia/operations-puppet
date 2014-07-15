5fc95c3b73034e4f2b932051b24ca95b508e5b46
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-07-15 19:44:57 +0200

    jobrunner: install on all jobrunners
    Change-Id: I82aeec3ebd3c12f5496476e46b145c98a1833612
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- manifests/role/mediawiki.pp
+++ manifests/role/mediawiki.pp
@@ -82,13 +82,9 @@ class role::mediawiki::jobrunner {
 
     include role::mediawiki::common
 
-    if $::hostname =~ /mw1001/ {
-        class { '::mediawiki::jobrunner':
-            aggr_servers  => [ '10.64.32.76', '10.64.0.201' ],
-            queue_servers => [ '10.64.32.76', '10.64.0.201' ],
-        }
-    } else {
-        deployment::target { 'jobrunner': }
+    class { '::mediawiki::jobrunner':
+        aggr_servers  => [ '10.64.32.76', '10.64.0.201' ],
+        queue_servers => [ '10.64.32.76', '10.64.0.201' ],
     }
 
     class { '::mediawiki::jobqueue':
