0b200c4a7c55d65eae46e23ca28cb1571985f01e
Matanya Moses <matanya@foss.co.il>
2014-07-16 16:01:30 +0300

    mailman: monitor web and archive access
    RT #7878
    
    Change-Id: Ifd3904984bf3417f0dd8fde343d679160b637676

--- manifests/role/mail.pp
+++ manifests/role/mail.pp
@@ -144,6 +144,16 @@ class role::mail::lists {
         nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 9:9 -a mailman',
     }
 
+    monitor_service { 'mailman_archives_web':
+        description   => 'mailman archives',
+        check_command => 'check_https_url!lists.wikimedia.org!/pipermail/wikimedia-l/',
+    }
+
+    monitor_service { 'mailman_web_cgi':
+        description   => 'mailman list info',
+        check_command => 'check_https_url!lists.wikimedia.org!/mailman/listinfo/wikimedia-l',
+    }
+
 }
 
 class role::mail::imap {
