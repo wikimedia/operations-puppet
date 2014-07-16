7475cf722afd96d97b5c7bc5211a6855e8303da5
cpettet <rush@wikimedia.org>
2014-07-16 16:09:30 -0500

    phabricator inbound email handler
    * script to hand off phab bound email to
    * class for setting up config file for email routing
    * template for config file
    
    Change-Id: Ib42473817b6d91cc8ee7889e5bfb228f64474d8f

---
+++ modules/phabricator/files/phab_epipe.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+"""2014 Chase Pettet <cpettet@wikimedia.org>
+
+This script acts as an intermediary between an MTA and phabricator providing
+some extra functionality and raw email piping.
+
+* default behavior is to deliver to phabricator handler
+* address routing allows redirecting an email address to create new tasks in project
+* direct comments allows non-phabricator recognized emails to attach comments
+  to a ticket for a specific project from allowed domains
+
+-> custom attachments to be handled later by direct attach script
+-> http://stackoverflow.com/questions/4067937/getting-mail-attachment-to-python-file-object
+
+Looks for configuration file: /etc/phab_epipe.conf
+
+    [address_routing]
+    <dest_email_address> = <project_id_to_route_for_task_creation>
+
+    [direct_comments_allowed]
+    <project_name> = <comma_separated_list_of_allowed_email_domains>
+
+    [phab_bot]
+    root_dir = <path_on_disk_to_phabricator
+    env = <phab_env>
+    username    = <phab_user>
+    certificate = <phab_user_cert>
+    host        = <phab_host>
+"""
+
+import sys
+import subprocess
+import os
+import re
+from phabricator import Phabricator
+from email.parser import Parser
+import ConfigParser
+
+def extract_direct_task(list_of_dests):
+    """returns a dest ticket number of available
+    :param list_of_dests: list of dest email strings
+    :returns: string
+    """
+    for dest in list_of_dests:
+        task = re.match('^T(\d+)@', dest)
+        if task:
+            return int(task.group(1))
+    return ''
+
+def log(msg):
+    import syslog
+    msg = str(msg)
+    if '-v' in sys.argv:
+        syslog.syslog(msg)
+        print '-> ', msg
+
+def main():
+
+    #if stdin is empty we bail to avoid a loop
+    #of looking for EOF that never comes
+    if sys.stdin.isatty():
+        log('no stdin')
+        exit(1)
+
+    parser = ConfigParser.SafeConfigParser()
+    parser_mode = 'phab_bot'
+    parser.read('/etc/phab_epipe.conf')
+    phab = Phabricator(username=parser.get(parser_mode, 'username'),
+                   certificate=parser.get(parser_mode, 'certificate'),
+                   host=parser.get(parser_mode, 'host'))
+
+    address_routing = {}
+    for name, value in parser.items('address_routing'):
+        address_routing[name] = value
+
+    direct_comments_allowed = {}
+    for name, value in parser.items('direct_comments_allowed'):
+        direct_comments_allowed[name] = value.split(',')
+
+    def get_proj_phid_by_id(pid):
+        """return project name by project id
+        :param pid: int project id
+        :returns: string
+        """
+        return phab.project.query(ids=[pid])['data'].keys()[0]
+
+    def external_user_comment(task, email, text):
+        """update a task with a comment by id
+        :param task: int id
+        :param email: address of source user
+        :param text: main comment string
+        :returns: json
+        :note: https://secure.phabricator.com/book/phabricator/article/remarkup/
+        """
+        external_user_comment_body = """[%s](mailto: %s)\n>%s"""
+        comment = external_user_comment_body % (email, email, text)
+        return phab.maniphest.update(id=task, comments=comment)
+
+    stdin = sys.stdin.read()
+
+    if '-s' in sys.argv:
+        dump = '/tmp/rawmail'
+        log('saving raw email to %s' % dump)
+        with open(dump, 'w') as r:
+            r.write(stdin)
+
+    msg = Parser().parsestr(stdin)
+    src_address = msg['from']
+    dest_addresses = msg['to'].split(',')
+
+    #https://docs.python.org/2/library/email.message.html#email.message.Message.get_payload
+    if msg.is_multipart():
+        for payload in msg.get_payload():
+            if payload.get_content_type() == 'text/plain':
+                body = payload.get_payload()
+    else:
+        body = msg.get_payload()
+
+    #does this email have a direct to task addresss
+    dtask = extract_direct_task(dest_addresses)
+
+    #determine if there is a reroutable address
+    to_addresses = [d.split('@')[0] for d in dest_addresses]
+    routed_addresses = [address for address in address_routing.keys() if address in to_addresses]
+
+    if dtask:
+        log('found direct task: %s' % (dtask,))
+        #extra check for address validity for this process
+        if src_address.count('@') > 1:
+            log('invalid source address')
+            return
+
+        #list of phid's for associated projects
+        task_info = phab.maniphest.info(task_id=dtask)
+        proj_info = task_info['projectPHIDs']
+
+        def dtask_project(project_list):
+            """determine if task has an associate project
+            :param project_list: array of project phid's
+            :returns: string
+            """
+            for p in project_list:
+                pname = phab.phid.info(phid=p)['name']
+                if pname in direct_comments_allowed.keys():
+                    return pname
+            return ''
+
+        dtask_match = dtask_project(proj_info)
+        if dtask_match and src_address.split('@')[1] in direct_comments_allowed[dtask_match]:
+            log(external_user_comment(dtask, src_address, body))
+        else:
+            log('direct task did not match conditions: %s' % (dtask,))
+
+    elif routed_addresses:
+        route_address = routed_addresses[0]
+        log('found routed address: %s' % (route_address,))
+        project_id = get_proj_phid_by_id(address_routing[route_address])
+        log(phab.maniphest.createtask(title=msg['subject'],
+                                        description=">%s" % body,
+                                        projectPHIDs=[project_id]))
+    else:
+        log('found phab default')
+        handler = os.path.join(parser.get(parser_mode, 'root_dir'), "scripts/mail/mail_handler.php")
+        process = subprocess.Popen([handler ,parser.get(parser_mode, 'env')],
+                                    stdin=subprocess.PIPE)
+        process.communicate(stdin)
+
+if __name__ == '__main__':
+    main()
