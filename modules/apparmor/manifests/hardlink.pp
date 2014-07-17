8b9fa704978adb2520e762677b95ea13ff9248e2
Matt Walker <mwalker@khaosdev.com>
2014-07-16 22:30:10 -0700

    AppArmor Profile for OCG
    Change-Id: Ie440d38cd9b792e084b1f3d159a9cbcbe54f3967

---
+++ modules/apparmor/manifests/hardlink.pp
@@ -0,0 +1,14 @@
+# == Define: hardlink
+#
+# In order to create separate profiles for AppArmor protected things, e.g.
+# scripts, that are run by the same interpreter we need to create a "new"
+# root application. Hard links are one way to accomplish that.
+
+define hardlink (source = $name, target) {
+    exec {
+        "hardlink-$name":
+            command => "ln --force $target $source",
+            path    => "/usr/bin:/bin",
+            unless  => "test $source -ef $target";
+    }
+}
