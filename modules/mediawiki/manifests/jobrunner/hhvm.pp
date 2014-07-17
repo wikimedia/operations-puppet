18bc23bc052d133760f45b142e7a2996abefacca
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-07-17 11:49:34 +0200

    jobrunner: create hhvm-only jobrunners
    this introduces the additional resources for running the jobrunners with
    hhvm:
    
    * The legacy job-queue is disabled
    * Hhvm is installed, with extensions and config
    * The fcgi server is stopped as we don't need that
    * hhvm is installed as the alternative providing /usr/bin/php
    
    in practice, this would mean that all jobs on that server will be ran
    with hhvm and the new jobrunner.
    
    Change-Id: I8d3c72452db643ee8eb88d6f6736b040743d7dec
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/mediawiki/manifests/jobrunner/hhvm.pp
@@ -0,0 +1,30 @@
+# == Class: mediawiki::jobrunner::hhvm
+#
+# Installs and configures hhvm so that it's used to run all jobs
+#
+# This class will ensure that hhvm packages are installed, the fastcgi
+# server is turned off and hhvm is providing /usr/bin/php via the
+# alternatives mechanism. Please note that installation of the
+# alternative will be taken care of by the debian package.
+#
+# This class should be included along with the mediawiki::jobrunner
+# class that will install the runner upstart job and all the rest
+# We also store the bytecode cache (which is persistent) in /run/hhvm
+# which is in tmpfs as a bonus.
+#
+class mediawiki::jobrunner::hhvm {
+    class { '::mediawiki::hhvm':
+        service       => 'stopped',
+        ini_overrides => {
+            'hhvm.repo.local.mode' => '--',
+            'max_execution_time'   => 86400, # yes, one day.
+        }
+    }
+
+    # ensure hhvm is the chosen runtime for /usr/bin/php
+    alternatives::config{ 'php':
+        path        => '/usr/bin/hhvm',
+        require     => Package['hhvm']
+    }
+
+}
