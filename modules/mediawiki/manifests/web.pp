3f266c6bb089256c4286f3caae2ec4f06a061ee5
Ori Livneh <ori@wikimedia.org>
2014-07-17 09:01:51 -0700

    mediawiki: use native min/floor funcs rather than inline_template
    ..but be cautious about it by not using the result just yet.
    A follow-up patch will replace the inline_template call.
    
    Change-Id: I20c205d3854aeb76abf2c6cb4404f3a2ba8dc343

--- modules/mediawiki/manifests/web.pp
+++ modules/mediawiki/manifests/web.pp
@@ -10,11 +10,15 @@ class mediawiki::web ( $workers_limit = undef, $use_sites_available = false ) {
     $apache_server_limit = 256
 
     if is_integer($workers_limit) {
-        $max_req_workers = $workers_limit
+        $max_req_workers = min($apache_server_limit, $workers_limit)
     } else {
         $mem_available   = to_bytes($::memorytotal) * 0.7
         $mem_per_worker  = to_bytes('85M')
         $max_req_workers = inline_template('<%= [( @mem_available / @mem_per_worker ).to_i, @apache_server_limit.to_i].min %>')
+
+        # testing min/floor
+        $max_req_workers_test = min(floor($mem_available /$mem_per_worker), $apache_Server_limit)
+        notice("${max_req_workers} == ${max_req_workers_test}")
     }
 
     file { '/etc/apache2/apache2.conf':
