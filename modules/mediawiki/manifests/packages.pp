68d18ae56f075de7142a8074a14496f14f5ed60d
Timo Tijhof <krinklemail@gmail.com>
2014-07-19 01:08:13 +0200

    mediawiki: Fix duplicate imagemagick package definition
    Follows-up 887bca6.
    
    Change-Id: Ieacacc471ae53b55105c24c914c670aee5fbc1d0

--- modules/mediawiki/manifests/packages.pp
+++ modules/mediawiki/manifests/packages.pp
@@ -1,6 +1,5 @@
 class mediawiki::packages {
     package { [
-        'imagemagick',
         'python-imaging',
         'libmemcached10',       # XXX still needed?
         'libmemcached11',
@@ -10,6 +9,13 @@ class mediawiki::packages {
         'php5-common',
     ]:
         ensure => present,
+    }
+
+    # FIXME: This conflicted with contint::packages
+    if ! defined ( Package['imagemagick'] ) {
+        package { 'imagemagick':
+            ensure => present,
+        }
     }
 
     # Standard PHP extensions
