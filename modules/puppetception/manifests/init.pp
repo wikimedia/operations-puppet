7b6a31a59643b6db068815137335904ba52875b1
YuviPanda <yuvipanda@gmail.com>
2014-07-20 00:46:12 +0530

    [WIP] puppetception: Initial commit
    Allows labs projects to use a puppet manifest that's not
    operations/puppet.git
    
    Change-Id: I22b38644c483f10d67833886653d274963b5476e

---
+++ modules/puppetception/manifests/init.pp
@@ -0,0 +1,36 @@
+class puppetception(
+    $git_url,
+    $git_branch='master',
+    $puppet_subdir = '',
+) {
+    include ::role::labs::lvm::srv
+
+    $base_dir = '/srv/puppetception'
+    $install_dir = "$base_dir/git"
+    $puppet_dir = "${install_dir}${puppet_subdir}"
+    file { [$base_dir,
+            $install_dir,
+            $puppet_dir
+    ]:
+        owner   => 'root',
+        group   => 'root',
+        ensure  => directory,
+        require => Mount['/srv'],
+    }
+
+    git::clone { $puppet_dir:
+        directory => $puppet_dir,
+        origin    => $git_url,
+        ensure    => latest,
+        require   => File[$puppet_dir],
+        branch    => $git_branch,
+    }
+
+    file { "/sbin/puppetception":
+        content  => template('puppetception/puppetception.erb'),
+        ensure => present,
+        mode   => '0700',
+        owner  => 'root',
+        group  => 'root',
+    }
+}
