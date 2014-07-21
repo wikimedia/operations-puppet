10f458c8340b53662c906009e40478372021c667
Antoine Musso <hashar@free.fr>
2014-07-21 12:11:21 +0200

    mediawiki: extract nutcracker to its own manifest
    On beta, we need to be able to configure nutcracker without setting up
    the whole MediaWiki.  This extract out nutcracker configuration to a new
    class mediawiki::nutcracker.  Will let us install it on the beta cluster
    bastion.
    
    Change-Id: If58072c96d3d94b99f248a1e4707ebb460c2b889

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -2,6 +2,7 @@ class mediawiki {
     include ::mediawiki::users
     include ::mediawiki::sync
     include ::mediawiki::cgroup
+    include ::mediawiki::nutcracker
     include ::mediawiki::packages
     include ::ssh::server
 
@@ -10,36 +11,6 @@ class mediawiki {
         group   => 'root',
         mode    => '0444',
         content => $::site,
-    }
-
-    if $::realm == 'labs' {
-        $mw_mc_server_list = [
-            '10.68.16.14:11211', # deployment-memc02.eqiad.wmflabs
-            '10.68.16.15:11211', # deployment-memc03.eqiad.wmflabs
-        ]
-    } else {
-        $mw_mc_server_list = [
-            '10.64.0.180:11211', # mc1001
-            '10.64.0.181:11211', # mc1002
-            '10.64.0.182:11211', # mc1003
-            '10.64.0.183:11211', # mc1004
-            '10.64.0.184:11211', # mc1005
-            '10.64.0.185:11211', # mc1006
-            '10.64.0.186:11211', # mc1007
-            '10.64.0.187:11211', # mc1008
-            '10.64.0.188:11211', # mc1009
-            '10.64.0.189:11211', # mc1010
-            '10.64.0.190:11211', # mc1011
-            '10.64.0.191:11211', # mc1012
-            '10.64.0.192:11211', # mc1013
-            '10.64.0.193:11211', # mc1014
-            '10.64.0.194:11211', # mc1015
-            '10.64.0.195:11211', # mc1016
-        ]
-    }
-
-    class { '::nutcracker':
-        server_list => $mw_mc_server_list,
     }
 
     # Increase scheduling priority of SSHD
