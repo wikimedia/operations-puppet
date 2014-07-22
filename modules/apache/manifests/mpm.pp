b8ada9f5ed5f2e692b42de00e599b431fca8c31c
Ori Livneh <ori@wikimedia.org>
2014-07-22 16:07:12 -0700

    apache: add apache::mpm
    Adds a class that lets you select an Apache Multi-Processing Module (MPM).
    Prefork, worker and event are available. Prefork is the default. Trusty and
    Precise are both seamlessly supported.
    
    Tested via Vagrant on both Precise and Trusty.
    
    Change-Id: Ic952146b5eecb1d2e163e91fc08232202c728249

---
+++ modules/apache/manifests/mpm.pp
@@ -0,0 +1,69 @@
+# == Class: apache::mpm
+#
+# This class allows you to select an Apache Multi-Processing Module
+# (or MPM). MPMs are mutually exclusive; you can only enable one.
+# The MPMs available for you to choose from are prefork, worker, and
+# event.
+#
+# Worker and event scale better, but they don't work with any modules
+# which are not thread-safe, like PHP.
+#
+# See <http://httpd.apache.org/docs/current/mpm.html> for details.
+#
+# === Parameters
+#
+# [*mpm*]
+#   Name of the chosen MPM. Must be 'prefork', 'worker', or 'event'.
+#   The default is 'prefork'.
+#
+class apache::mpm( $mpm = 'prefork' ) {
+    include ::apache
+
+    $available_mpms = ['prefork', 'worker', 'event']
+    if ! member($available_mpms, $mpm) {
+        fail('mpm must be prefork, worker or event')
+    }
+
+    $selected_mod = "mpm_${mpm}"
+    $selected_pkg = "apache2-mpm-${mpm}"
+    $selected_cfg = "/etc/apache2/mods-available/mpm_${mpm}.load"
+
+    $rejected_mpms = reject($available_mpms, $mpm)
+    $rejected_mods = prefix($rejected_mpms, 'mpm_')
+    $rejected_pkgs = prefix($rejected_mpms, 'apache2-mpm-')
+
+    if $selected_mod != 'prefork' {
+        # mod_php5 is unsafe for threaded MPMs
+        apache::mod_conf { 'php5':
+            ensure => absent,
+            before => Package[$selected_pkg],
+        }
+    }
+
+    apache::mod_conf { $rejected_mods:
+        ensure => absent,
+        before => Package[$rejected_pkgs],
+    }
+
+    package { $rejected_pkgs:
+        ensure => absent,
+        before  => Package[$selected_pkg],
+    }
+
+    package { $selected_pkg:
+        ensure => present,
+        before => File[$selected_cfg],
+    }
+
+    file { $selected_cfg:
+        ensure => file,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        before => Apache::Mod_conf[$selected_mod],
+    }
+
+    apache::mod_conf { $selected_mod:
+        ensure => present,
+    }
+}
