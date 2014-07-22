e5758243a849292b5303a5ed147a38ffa2dfcf33
Brandon Black <bblack@wikimedia.org>
2014-07-22 00:50:50 -0500

    role::ocg::production -> add LVS config + better icinga check
    Change-Id: If206523bb539ee4984aa21a743dc6af0867cf86d

--- manifests/role/ocg.pp
+++ manifests/role/ocg.pp
@@ -52,8 +52,11 @@ class role::ocg::production {
 
     monitor_service { 'ocg':
         description   => 'Offline Content Generation - Collection',
-        check_command => 'check_http_on_port!80',
+        check_command => "check_http_url_on_port!ocg.svc.${::site}.wmnet!${service_port}!/?command=health"
     }
+
+    include lvs::configuration
+    class { 'lvs::realserver': realserver_ips => $lvs::configuration::lvs_service_ips[$::realm]['ocg'][$::site] }
 }
 
 class role::ocg::test {
