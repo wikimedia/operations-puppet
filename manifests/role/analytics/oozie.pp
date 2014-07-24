e994efeef78a2d051d94abf2391b0868252c02d3
Andrew Otto <acotto@gmail.com>
2014-07-24 16:35:27 -0400

    Set authorization_service_authorization_enabled for Oozie.
    Change-Id: Ic693e84c9a0d26b05537a4c121c51ebb19b92341

--- manifests/role/analytics/oozie.pp
+++ manifests/role/analytics/oozie.pp
@@ -51,5 +51,16 @@ class role::analytics::oozie::server inherits role::analytics::oozie::client {
         jdbc_password   => $jdbc_password,
         smtp_host       => $::mail_smarthost[0],
         smtp_from_email => "oozie@${::fqdn}",
+        # This is not currently working.  Disabling
+        # this allows any user to manage any Oozie
+        # job.  Since access to our cluster is limited,
+        # this isn't a big deal.  But, we should still
+        # figure out why this isn't working and
+        # turn it back on.
+        # I was not able to kill any oozie jobs
+        # with this on, even though the
+        # oozie.service.ProxyUserService.proxyuser.*
+        # settings look like they are properly configured.
+        authorization_service_authorization_enabled => false,
     }
 }
