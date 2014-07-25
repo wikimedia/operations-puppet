822bc119ba752fba9deb21b7d2d8b1076054162b
Matt Walker <mwalker@khaosdev.com>
2014-07-25 11:51:47 -0700

    All OCG servers should have syslog readable
    let OCG servers have readable /var/log/syslog and
    /var/log/messages by using base::syslogs
    
    but needs to have the $realm check because in labs
    the class is used per default there in role/labs
    and it causes duplicate definitions otherwise
    
    thought this was still the better workaround
    than putting it on the node level
    
    Change-Id: I20f3977659783705b1f8174d329a2a609aaa123c

--- modules/ocg/manifests/init.pp
+++ modules/ocg/manifests/init.pp
@@ -189,13 +189,13 @@ class ocg (
         mode    => '0664',
     }
 
-    # horrible hack to work around the fact that we can't currently add users to
-    # group 'adm' alongside system user 'syslog', see RT: 7596
-    file { '/var/log/syslog':
-        mode => '0644',
-    }
-    file { '/var/log/ocg/ocg.log':
-        mode => '0644',
+    # makes some basic logfiles readable for non-roots
+    # in labs this is used by default in role/labs
+    if $::realm == 'production' {
+        class { 'base::syslogs':
+            readable => true,
+            logfiles => ['syslog','messages','ocg'],
+        }
     }
 
     file { '/etc/logrotate.d/ocg':
