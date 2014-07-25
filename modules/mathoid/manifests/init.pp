c39767faaeb5ff0a86b36710ce073b0bfcc1a553
physikerwelt <wiki@physikerwelt.de>
2014-07-25 12:11:18 -0400

    WIP: Draft for Mathoid role
    Now the mathoid role can be run without errors.
    But the files are missing. I think some git deploy magic needs to be done first.
    
    Change-Id: I77f412fc202c187fa37710c6e2c004c7ca94f6c6

---
+++ modules/mathoid/manifests/init.pp
@@ -0,0 +1,77 @@
+# == Class: mathoid
+#
+# mathoid is a node.js backend for the math rendering.
+#
+# === Parameters
+#
+# [*base_path*]
+#   Path to the mathoid code.
+# [*node_path*]
+#   Path to the node modules mathoid depends on.
+# [*conf_path*]
+#   Where to place the config file.
+# [*log_dir*]
+#   Place where mathoid can put log files. Assumed to be already existing and
+#   have write access to mathoid user.
+# [*port*]
+#   Port where to run the mathoid service. Defaults to 10042.
+
+#
+class mathoid(
+    $base_path,
+    $node_path,
+    $conf_path,
+    $log_dir,
+    $port=10042
+) {
+    package { ['nodejs'
+# TODO Add dependency to node-jsdom once
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=742347
+# is fixed
+              ]:
+        ensure => present,
+    }
+
+    $log_file = "${log_dir}/main.log"
+
+    file { $log_dir:
+        ensure => directory,
+        owner  => mathoid,
+        group  => mathoid,
+        mode   => '0775',
+    }
+
+    file { $conf_path:
+        ensure  => present,
+        owner   => mathoid,
+        group   => mathoid,
+        mode    => '0555',
+        content => template('mathoid/config.erb'),
+    }
+
+    # The upstart configuration
+    file { '/etc/init/mathoid.conf':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0444',
+        content => template('mathoid/upstart.erb'),
+    }
+
+    file { '/etc/logrotate.d/mathoid':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0444',
+        content => template('mathoid/logrotate.erb'),
+    }
+
+    service { 'mathoid':
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        provider   => 'upstart',
+        require    => File[$log_dir],
+        subscribe  => File['/etc/init/mathoid.conf'],
+    }
+}
