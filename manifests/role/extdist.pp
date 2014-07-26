f936b9ca3d21ab13a4d1f84094adc36e28e457e9
YuviPanda <yuvipanda@gmail.com>
2014-07-26 06:11:27 +0530

    [WIP] Add extdist role
    Change-Id: I63d285612bd30d24b83718c4c8967144d146afeb

---
+++ manifests/role/extdist.pp
@@ -0,0 +1,28 @@
+class extdist {
+
+    include role::labs::lvm::srv
+
+    git::clone { 'labs/tools/extdist':
+        directory => '/srv/extdist',
+        ensure    => latest,
+        branch    => 'master',
+        require   => Mount['/srv']
+    }
+
+    package{ 'python-flask':
+        ensure => latest
+    }
+
+    uwsgi::app { 'extdist':
+        require => Git::clone['labs/tools/extdist'],
+        settings             => {
+            uwsgi            => {
+                'plugins'    => 'python',
+                'socket'     => '/var/run/extdist.sock',
+                'wsgi-file'  => '/srv/extdist/extdist.wsgi',
+                'master'     => true,
+                'processers' => 4,
+            },
+        },
+    }
+}
