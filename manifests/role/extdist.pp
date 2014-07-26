62792e58d07e5994277b0b6a9f3b60b41009acd6
YuviPanda <yuvipanda@gmail.com>
2014-07-26 06:11:27 +0530

    [WIP] Add extdist role
    Change-Id: I63d285612bd30d24b83718c4c8967144d146afeb

---
+++ manifests/role/extdist.pp
@@ -0,0 +1,47 @@
+class role::labs::extdist {
+
+    include role::labs::lvm::srv
+
+    user { 'extdist':
+        ensure => present,
+        system => true,
+    }
+
+    git::clone {'labs/tools/extdist':
+        directory => '/srv/extdist',
+        ensure    => latest,
+        branch    => 'master',
+        require   => [Mount['/srv'], User['extdist']],
+        user      => 'extdist',
+        group     => 'extdist',
+    }
+
+    package{'python-flask':
+        ensure => latest,
+    }
+
+    uwsgi::app { 'extdist':
+        require              => [Git::Clone['labs/tools/extdist'], Package['python-flask']],
+        settings             => {
+            uwsgi            => {
+                'socket'     => '/run/uwsgi/extdist.sock',
+                'wsgi-file'  => '/srv/extdist/extdist.wsgi',
+                'master'     => true,
+                'processes' => 4,
+                'chdir'      => '/srv/extdist',
+            },
+        },
+    }
+
+    cron { 'extdist-generate-tarballs':
+        command => '/usr/bin/python /srv/extdist/nightly.py',
+        user    => 'extdist',
+        hour    => '0',
+        require => [Git::Clone['labs/tools/extdist'], User['extdist']]
+    }
+
+    nginx::site { 'extdist':
+        require => Uwsgi::App['extdist'],
+        content => template('extdist.nginx.erb'),
+    }
+}
