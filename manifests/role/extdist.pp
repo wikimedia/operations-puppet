79ad7d6f508708a2b83e2acbd81a49a4c99bf9cc
YuviPanda <yuvipanda@gmail.com>
2014-07-26 06:11:27 +0530

    [WIP] Add extdist role
    The extdist role sets up a tarball generator for use
    with the Extension Distributor extension on mediawiki.org
    
    A simple uwsgi application behind a nginx webserver serves
    tarballs to users. A cron job updates local copies of git
    repositories hourly and generates tarballs from them.
    
    Change-Id: I63d285612bd30d24b83718c4c8967144d146afeb

---
+++ manifests/role/extdist.pp
@@ -0,0 +1,58 @@
+# = Class: role::labs::extdist
+#
+# This class sets up a tarball generator for the Extension Distributor
+# extension enabled on mediawiki.org.
+#
+class role::labs::extdist {
+
+    include role::labs::lvm::srv
+
+    user { 'extdist':
+        ensure => present,
+        system => true,
+    }
+
+    exec { 'make-srv-usable-by-everyone':
+        command => '/bin/chmod 0777 /srv',
+        unless  => "/bin/sh -c '[ $(/usr/bin/stat -c %a /srv) == 0777'",
+        require => Mount['/srv']
+    }
+
+    git::clone {'labs/tools/extdist':
+        directory => '/srv/extdist',
+        ensure    => latest,
+        branch    => 'master',
+        require   => [Exec['make-srv-usable-by-everyone'], User['extdist']],
+        owner     => 'extdist',
+        group     => 'extdist',
+    }
+
+    package{'python-flask':
+        ensure => latest,
+    }
+
+    uwsgi::app { 'extdist':
+        require              => [Git::Clone['labs/tools/extdist'], Package['python-flask']],
+        settings             => {
+            uwsgi            => {
+                'socket'     => '/run/uwsgi/extdist.sock',
+                'wsgi-file'  => '/srv/extdist/extdist.wsgi',
+                'master'     => true,
+                'processes' => 4,
+                'chdir'      => '/srv/extdist',
+            },
+        },
+    }
+
+    cron { 'extdist-generate-tarballs':
+        command => '/usr/bin/python /srv/extdist/nightly.py',
+        user    => 'extdist',
+        hour    => '0',
+        require => [Git::Clone['labs/tools/extdist'], User['extdist']]
+    }
+
+    nginx::site { 'extdist':
+        require => Uwsgi::App['extdist'],
+        content => template('extdist.nginx.erb'),
+    }
+}
