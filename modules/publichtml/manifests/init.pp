8d7f6ca4036d1fa9909d2d4977ff3126cbca1bf6
Ariel T. Glenn <ariel@wikimedia.org>
2014-07-28 19:21:22 +0300

    public_html directory service, see RT #6862
    module for public_html service, hostname people.wikimedia.org
    added to misc cache cluster, to be served by terbium,
    caching disabled.  manifests not yet tested.
    
    Change-Id: I2716be4629380404b21aa46ae63e0ce3928a015c

---
+++ modules/publichtml/manifests/init.pp
@@ -0,0 +1,63 @@
+# server with user public_html directories enabled and nothing else
+# no critical services, in fact no services at all should run here
+
+class publichtml (
+    $sitename = undef,
+    $docroot = '/srv/org/wikimedia/publichtml',
+    $server_admin = undef,
+) {
+
+    system::role { 'publichtml':
+        description => 'web server of public_html directories'
+    }
+
+    apache::site { $sitename:
+        content => template('publichtml/apacheconfig.erb')
+        require => [Class['::apache::mod::userdir', '::apache::mod::cgi'],
+                    Package[libapache2-mod-php5]],
+    }
+
+    file { '/etc/apache2/sites-enabled/default':
+        ensure  => 'absent',
+        path    => '/etc/apache2/sites-enabled/default',
+    }
+
+    file { '/srv/org':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    file { '/srv/org/wikimedia':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    file { $docroot:
+        ensure  => 'directory',
+        path    => $docroot,
+        mode    => '0755',
+        owner   => root,
+        group   => root,
+    }
+
+    file { "${docroot}/index.html":
+        ensure  => 'present',
+        path    => "${docroot}/index.html",
+        mode    => '0444',
+        owner   => root,
+        group   => root,
+        content => template('publichtml/index.html.erb'),
+    }
+
+    include ::apache::mod::userdir
+
+    monitor_service { 'http':
+        description   => 'HTTP',
+        check_command => "check_http_url!${sitename}!http://${sitename}"
+    }
+
+}
