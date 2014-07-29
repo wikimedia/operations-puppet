2565943d58fae9df165a4192ea7ccb1629e39f8f
YuviPanda <yuvipanda@gmail.com>
2014-07-30 04:41:02 +0530

    Role + Module for Quarry Labs tool
    - Implements
    https://meta.wikimedia.org/wiki/Research:Ideas/Public_query_interface_for_Labs
    - Deployed via fabric file kept in the source
      repository at analytics/quarry/web
    - NAT rules and /etc/hosts for access to labsdb handled
      manually for now
    - Uses /srv for everything
    
    Change-Id: I6042041ffe853095970296c103d3d2e7e19b79e2

---
+++ modules/quarry/manifests/celeryrunner.pp
@@ -0,0 +1,22 @@
+class quarry::celeryrunner {
+    $clone_path = "/srv/quarry"
+    $result_path = "/data/project/quarry/results"
+    $venv_path = "/srv/venv"
+
+    include quarry::base
+
+    file { '/etc/init.d/celeryd':
+        source => 'puppet:///modules/quarry/celeryd',
+        mode   => '0755',
+    }
+
+    file { '/etc/default/celeryd':
+        content => template('quarry/celeryd.conf.erb'),
+    }
+
+    service { 'celeryd':
+        require => [File['/etc/init.d/celeryd'], File['/etc/default/celeryd'], User['quarry']],
+        ensure  => running
+    }
+
+}
---
+++ modules/quarry/manifests/init.pp
@@ -0,0 +1,86 @@
+# = Class: quarry::base
+#
+# This class sets up the basic underlying structure for both
+# Quarry web frontends and Quarry query runners.
+class quarry::base {
+
+    $clone_path = "/srv/quarry"
+    $result_path_parent = "/data/project/quarry"
+    $result_path = "/data/project/quarry/results"
+    $venv_path = "/srv/venv"
+
+    package { [
+        'python-virtualenv',
+        'python-pip',
+    ]:
+        ensure => latest
+    }
+
+    user { 'quarry':
+        ensure => present,
+        system => true
+    }
+
+    file { [$clone_path, $result_path_parent, $result_path, $venv_path]:
+        ensure  => directory,
+        owner   => 'quarry',
+        require => User['quarry']
+    }
+
+    git::clone { 'analytics/quarry/web':
+        directory => $clone_path,
+        ensure    => present,
+        branch    => 'master',
+        require   => [File[$clone_path], User['quarry']],
+        owner     => 'quarry',
+        group     => 'www-data'
+    }
+
+    # Temp. hack until Coren figures out why normal users
+    # can't do 'sudo -u <user> <command' on labs
+    # Otherwise fabric deployment wokn't work
+    admin::sudo { 'wikidev':
+        is_group => true,
+        comment  => 'Give human users full ability to sudo as anyone without password',
+        privs    => ['ALL=(ALL) NOPASSWD: ALL']
+    }
+
+}
+
+# = Class: quarry::database
+#
+# Sets up a mysql database for use by Quarry web frontends
+# and Quarry query runners
+class quarry::database {
+    $data_path = "/srv/mysql/data"
+
+    class { 'mysql::server':
+        config_hash => {
+            'datadir' => $data_path,
+            'bind_address' => '0.0.0.0'
+        }
+    }
+}
+
+# = Class: quarry::redis
+#
+# Sets up a redis instance for use as caching and session storage
+# by the Quarry frontends and also as working queue & results
+# backend by the query runners.
+class quarry::redis {
+    class { '::redis':
+        dir       => '/srv/redis',
+        maxmemory => '2GB',
+        persist   => 'aof',
+        monitor   => false,
+    }
+
+    # Also monitor local redis
+    package { 'python-redis':
+        ensure => present
+    }
+
+    diamond::collector { 'Redis':
+        require => Package['python-redis']
+    }
+}
