6b3722c3517d3fd13955cb655439d5fa86168de2
YuviPanda <yuvipanda@gmail.com>
2014-07-30 04:41:02 +0530

    Role + Module for Quarry Labs tool
    - Implements
    https://meta.wikimedia.org/wiki/Research:Ideas/Public_query_interface_for_Labs
    - Deployed via fabric file kept in the source
      repository at analytics/quarry/web
    - NAT rules and /etc/hosts for access to labsdb handled
      manually for now
    - Uses /srv for everything
    
    Change-Id: I6042041ffe853095970296c103d3d2e7e19b79e2

---
+++ modules/quarry/manifests/web.pp
@@ -0,0 +1,31 @@
+# = Class: quarry::web
+#
+# This class sets up a web frontend for Quarry, which lets
+# users run SQL queries against LabsDB.
+# Deployment is handled using fabric
+class quarry::web {
+    $clone_path = "/srv/quarry"
+    $result_path = "/data/project/quarry/results"
+    $venv_path = "/srv/venv"
+
+    include quarry::base
+
+    uwsgi::app { 'quarry-web':
+        require             => Git::Clone['analytics/quarry/web'],
+        settings            => {
+            uwsgi           => {
+                'socket'    => '/run/uwsgi/quarry-web.sock',
+                'wsgi-file' => "$clone_path/quarry.wsgi",
+                'master'    => true,
+                'processes' => 8,
+                'venv'      => $venv_path,
+                'chdir'     => $clone_path,
+            }
+        }
+    }
+
+    nginx::site { 'quarry-web-nginx':
+        require => Uwsgi::App['quarry-web'],
+        content => template('quarry/quarry-web.nginx.erb')
+    }
+}
