4390764cb26f4845e5ea5c4adc775acb5b92293a
YuviPanda <yuvipanda@gmail.com>
2014-07-30 04:41:02 +0530

    [Heavy WIP] Role + Module for Quarry Labs tool
    Change-Id: I6042041ffe853095970296c103d3d2e7e19b79e2

---
+++ modules/quarry/manifests/init.pp
@@ -0,0 +1,78 @@
+class quarry::base {
+
+    $clone_path = "/srv/quarry"
+    $result_path = "/srv/results"
+    $venv_path = "/srv/venv"
+
+    package { [
+        'python-virtualenv',
+        'python-pip',
+    ]:
+        ensure => latest
+    }
+
+    user { 'quarry':
+        ensure => present,
+        system => true
+    }
+
+    file { [$clone_path, $result_path, $venv_path]:
+        ensure  => directory,
+        owner   => 'quarry',
+        require => User['quarry']
+    }
+
+    git::clone { 'analytics/quarry/web':
+        directory => $clone_path,
+        ensure    => present,
+        branch    => 'master',
+        require   => [File[$clone_path], User['quarry']],
+        owner     => 'quarry',
+        group     => 'www-data'
+    }
+
+    # Temp. hack until Coren figures out why normal users
+    # can't do 'sudo -u <user> <command' on labs
+    # Otherwise fabric deployment wokn't work
+    admin::sudo { 'wikidev':
+        is_group => true,
+        comment  => 'Give human users full ability to sudo as anyone without password',
+        privs    => ['ALL=(ALL) NOPASSWD']
+    }
+
+}
+
+class quarry::database {
+    $data_path = "/srv/mysql/data"
+
+    class { 'mysql::server':
+        config_hash => {
+            'datadir' => $data_path,
+            'bind_address' => '0.0.0.0'
+        }
+    }
+}
+
+class quarry::redis {
+    class { '::redis':
+        dir       => '/srv/redis',
+        maxmemory => '2GB',
+        persist   => 'aof',
+        monitor   => false,
+    }
+}
+
+class quarry::pip::installer(
+    $venv_path,
+    $requirements_path,
+    $user,
+) {
+    $pip_path = "$venv_path/bin/pip"
+
+    # This is idempotent, so it's alright
+    exec { 'pip-install-things':
+        command => "$pip_path install -r $requirements_path",
+        user    => $user
+    }
+}
+
