8b94d9730cee96d2f5fa5b7afd2193d75a5ee4d6
YuviPanda <yuvipanda@gmail.com>
2014-07-30 04:41:02 +0530

    [Heavy WIP] Role + Module for Quarry Labs tool
    Change-Id: I6042041ffe853095970296c103d3d2e7e19b79e2

---
+++ modules/quarry/manifests/web.pp
@@ -0,0 +1,27 @@
+class quarry::web {
+    $clone_path = "$base_path/web"
+    $result_path = "/data/project/quarry/results"
+    $venv_path = "$base_path/venv"
+
+    include quarry::base
+
+    uwsgi::app { 'quarry-web':
+        require              => Git::Clone['analytics/quarry/web'],
+        settings             => {
+            uwsgi            => {
+                'socket'     => '/run/uwsgi/quarry-web.sock',
+                'wsgi-file'  => "$clone_path/quarry.wsgi",
+                'master'     => true,
+                'processes'  => 8,
+                'virtualenv' => $venv_path,
+                'no-site'    => true
+            }
+        }
+    }
+
+    nginx::site { 'quarry-web-nginx':
+        require => Uwsgi::App['quarry-web'],
+        content => template('quarry/quarry-web.nginx.erb')
+    }
+}
+
