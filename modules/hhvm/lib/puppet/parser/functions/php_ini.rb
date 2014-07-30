077aa813229a61e229c02cd371c8f5958c8013ff
Ori Livneh <ori@wikimedia.org>
2014-07-30 02:54:28 -0700

    Add HHVM module
    * Provision separate set of config files for CLI and FastCGI modes.
    * CLI configs in the default locations (so that running 'php' with no arguments
      "just works"); FastCGI configs in /etc/hhvm/fastcgi.
    * Separate bytecode caches for the two modes.
    * Make start-stop-daemon manage the pidfile so Upstart can inspect it in a
      post-stop stanza.
    * Make Upstart check for a stacktrace in post-stop and invoke
      $HHVM_TRACE_HANDLER (if defined) to process it.
    * Add a prototype Python script for parsing stack traces and reporting them to
      Sentry.
    * Manage /usr/lib/hphp/extensions/current symlink pointing to the extension
      directory for the most recent ABI version.
    
    Change-Id: Ieadc53775978a43f45008522fbc574f80c66e306

---
+++ modules/hhvm/lib/puppet/parser/functions/php_ini.rb
@@ -0,0 +1,44 @@
+# == Function: php_ini
+#
+# Serialize a hash into php.ini-style format. Takes one or more hashes
+# as arguments. If the argument list contains more than one hash, they
+# are merged together. In case of duplicate keys, hashes to the right
+# win.
+#
+# === Example
+#
+#   php_ini({'server' => {'port' => 80}}) # => server.port = 80
+#
+
+def flatten_map(map, prefix=nil)
+    map.inject({}) do |flat,(k,v)|
+        k = [prefix, k].compact.join('.')
+        v = v.include?('.') ? Float(v) : Integer(v) rescue v
+        flat.merge! v.is_a?(Hash) ? flatten_map(v, k) : Hash[k, v]
+    end
+end
+
+module Puppet::Parser::Functions
+  newfunction(
+    :php_ini,
+    :doc  => <<-END
+      Serialize a hash into php.ini-style format. Takes one or more hashes
+      as arguments. If the argument list contains more than one hash, they
+      are merged together. In case of duplicate keys, hashes to the right
+      win.
+
+      Example:
+
+         php_ini({'server' => {'port' => 80}}) # => server.port = 80
+
+    END
+  ) do |args|
+    fail 'php_ini() operates on hashes' if args.map(&:class).uniq != [Hash]
+    args.map { |arg| flatten_map(arg) }
+        .inject(:merge)
+        .sort
+        .map { |kv| kv.join(' = ') }
+        .push('')
+        .join("\n")
+  end
+end
