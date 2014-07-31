a489480ccdf7c9e4139fd139fcd0c1ee8ff0331c
Ori Livneh <ori@wikimedia.org>
2014-07-31 10:59:41 -0700

    mediawiki: use HHVM module on trusty
    * Get rid of various $use_hhvm/$jobrunner_hhvm-style parameters. We can't run
      MediaWiki on Trusty without HHVM, so the distro version check is both
      necessary and sufficient for determining which interpreter we should use.
    * For the same reason: include ::mediawiki::hhvm from ::mediawiki if >= Trusty.
    * Get rid of mediawiki::jobrunner::hhvm. There is no longer anything unique
      about the jobrunner HHVM setup. (We don't want to disable the service either
      since we're transitioning to dispatching jobs via FastCGI).
    * Lint: alphabetize modules in mediawiki::web::modules; fix indent; add
      trailing comma to last param; alphabetize lists.
    * Get rid of duplicate max_req_workers in mediawiki::web::modules (the modules
      class already has to call lookupvar for getting other variables from
      mediawiki::web, so there's no reason to duplicate it).
    * Drop mpm_worker.conf.erb; not in use and doesn't work. (Scope lookup
      failures.)
    * Make mpm_prefork be an apache::conf resource.
    
    Change-Id: I4aa104920d8121033e664458bb72c59233ef3868

--- modules/mediawiki/manifests/hhvm.pp
+++ modules/mediawiki/manifests/hhvm.pp
@@ -1,60 +1,20 @@
-# == Class mediawiki::hhvm
+# == Class: mediawiki::hhvm
 #
-# Base class to run an hhvm mediawiki environment.
+# Configures HHVM to serve MediaWiki in FastCGI mode.
 #
-# This class will ensure that the hhvm packages and basic dirs are installed.
-# It also ensures the FastCGI is in the desired state and can tweak
-# configuration according to the needs of the dependent class.
-#
-class mediawiki::hhvm(
-    $ini_overrides = {
-        'max_execution_time' => 180,
-    },
-    $service       = 'running'
-) {
-    # Install hhvm and all needed packages
-    include mediawiki::packages::hhvm
+class mediawiki::hhvm {
+    requires_ubuntu('>= trusty')
 
-    file { '/etc/hhvm':
-        ensure  => directory,
-        mode    => '0555',
-        require => Package['hhvm']
+    class { '::hhvm':
+        user          => 'apache',
+        group         => 'apache',
+        fcgi_settings => {
+            hhvm => { server => { source_root => '/usr/local/apache/common/docroot' } },
+        },
     }
 
-    file { '/run/hhvm':
-        ensure  => directory,
-        owner   => 'apache',
-        group   => 'apache',
-        mode    => '0755',
-        require => Class['mediawiki::users'],
-    }
-
-    # This directory contains the bytecode cache and should not be
-    # world accessible
-    file { '/run/hhvm/cache':
-        ensure  => directory,
-        owner   => 'apache',
-        group   => 'apache',
-        mode    => '0750',
-        before  => File['hhvm_config_ini'],
-    }
-
-    file { '/etc/hhvm/config.hdf':
-        ensure => present,
-        source => 'puppet:///modules/mediawiki/hhvm/jobrunner.hhvm.hdf',
-    }
-
-    file { 'hhvm_config_ini':
-        path    => '/etc/hhvm/php.ini',
-        ensure  => present,
-        content => template('mediawiki/hhvm/hhvm.ini.erb'),
+    alternatives::config { 'php':
+        path    => '/usr/bin/hhvm',
         require => Package['hhvm'],
-    }
-
-    # Ensure the fcgi server is stopped
-    service { 'hhvm':
-        ensure   => $service,
-        provider => 'upstart',
-        require  => Class['::mediawiki::packages::hhvm'],
     }
 }
