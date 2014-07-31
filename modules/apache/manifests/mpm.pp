84ccf95f91b3fdfc6a2334135ff57b7d7c8434de
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-07-31 15:59:13 +0200

    apache: remove $rejected_pkgs
    - On precise, the module is just a stub and doesn't have anything to do
      with the real work of apache. The version of apache installed will be
      decided by which mpm you chose, and it will automatically remove upon
      installation all other mpms. If config is not badly broken by us, the
      OS will take care of this itself.
    
    - On trusty, removing the module config cannot depend on removing the
      mpm packages, which are just transitional and empty.
    
    Change-Id: I319adfb5769688da802125d3731d609d47b13eb2
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/apache/manifests/mpm.pp
+++ modules/apache/manifests/mpm.pp
@@ -30,7 +30,6 @@ class apache::mpm( $mpm = 'prefork' ) {
 
     $rejected_mpms = reject($available_mpms, $mpm)
     $rejected_mods = prefix($rejected_mpms, 'mpm_')
-    $rejected_pkgs = prefix($rejected_mpms, 'apache2-mpm-')
 
     if $mpm != 'prefork' {
         # mod_php5 is unsafe for threaded MPMs
@@ -42,15 +41,10 @@ class apache::mpm( $mpm = 'prefork' ) {
 
     apache::mod_conf { $rejected_mods:
         ensure => absent,
-        before => Package[$rejected_pkgs],
     }
 
     #Those are not needed in modern apache packages.
     if ubuntu_version('< trusty') {
-        package { $rejected_pkgs:
-            ensure  => absent,
-            before  => Package[$selected_pkg],
-        }
         package { $selected_pkg:
             ensure => present,
             before => File[$selected_cfg],
