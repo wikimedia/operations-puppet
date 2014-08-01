6430eeefe6be7c5a951b7eeb6ad805beffbd2c99
Bryan Davis <bd808@wikimedia.org>
2014-08-01 09:56:32 -0600

    beta: puppet rebase script
    Script to rebase local checkout of operations/puppet.git on
    origin/production branch. The script tries to preserve local changes in
    the form of cherry-picks, local-only commits and dirty working copy
    files. Also adds a cron job to run the script once an hour. Messages
    from the update are logged to /var/log/git-sync-upstream.log and managed
    with logrotate.
    
    If this proves to be mostly stable in beta it might be nice to offer it
    as an optional class for any labs hosts using role::puppet::self.
    
    Bug: 66683
    Change-Id: I8072780a48e27d3d4271a82660417d711b8e2082

---
+++ modules/beta/manifests/puppetmaster/sync.pp
@@ -0,0 +1,27 @@
+# == Class: beta::puppetmaster::sync
+#
+# Sync local operations/puppet.git checkout with upstream
+#
+class beta::puppetmaster::sync {
+
+    file { '/usr/local/bin/git-sync-upstream':
+        ensure => present,
+        source => 'puppet:///modules/beta/git-sync-upstream',
+        mode   => '0555',
+    }
+
+    cron { 'rebase_operations_puppet':
+        ensure  => present,
+        user    => 'root',
+        minute  => 17,
+        command => '/usr/local/bin/git-sync-upstream 2>&1 >>/var/log/git-sync-upstream.log',
+        require => File['/usr/local/bin/git-sync-upstream'],
+    }
+
+    file { '/etc/logrotate.d/git-sync-upstream':
+        ensure  => present,
+        source  => 'puppet:///modules/beta/git-sync-upstream.logrotate',
+        mode    => '0444',
+        require => Cron['rebase_operations_puppet'],
+    }
+}
