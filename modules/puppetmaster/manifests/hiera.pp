daf3be769a91b5858aa4a87949b8b68a8e58ea96
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-08-05 18:09:44 +0200

    puppet: hiera backend for the WMF
    The hiera nuyaml backend will allow us to use hiera with ease and will
    make it both easier to maintain (allowing for more atomic files) and
    simpler to use in our setup where a lot of variables have a second order
    dependency on other data.
    
    A puppet class and conf files for both production and labs are included
    in the patch, but hiera config will only be deployed to labs for now.
    
    In our configurations, all hiera data will be under the hiera
    subdirectory of /etc/puppet, and all private data (like passwords and
    such) will be stored in /etc/puppet/private instead.
    
    Change-Id: I24b1db4d0ef8394115e4830871e71e2d227ec374
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/puppetmaster/manifests/hiera.pp
@@ -0,0 +1,27 @@
+# == Class: puppetmaster::hiera
+#
+# Configures the HIERArchical configuration system for puppet
+#
+# == Parameters
+#
+# [*config*]
+# The configuration file source (must be contained in the puppetmaster
+# module). If undef, the file will just be created empty.
+#
+
+class puppetmaster::hiera (
+    $ensure = 'present',
+    $source = 'puppet:///modules/puppetmaster/prod.hiera.yaml',
+    ) {
+
+    file { '/etc/puppet/hiera.yaml':
+        ensure  => $ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        source  => $source,
+        require => Package['puppetmaster-common']
+    }
+    # We don't need to declare ruby-hiera as a package explicitly as
+    # puppetmaster-common depends on ruby-hiera.
+}
