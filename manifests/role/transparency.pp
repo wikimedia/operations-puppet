f6ee5ae8ffa51e337378593c6ce77db84997b31a
Ori Livneh <ori@wikimedia.org>
2014-08-05 12:47:01 -0700

    provision transparency.wikimedia.org
    * Hosted on Zirconium (which already hosts similar static sites, like
      http://planet.wikimedia.org/, etc.)
    * Served by misc-varnish.
    * Deployed via Git. (It's a static site.)
    
    RT: 7845
    Change-Id: I904f5fc8115ef607202616b985645c7d3e6a2bc3

---
+++ manifests/role/transparency.pp
@@ -0,0 +1,25 @@
+# == Class: role::transparency
+#
+# This role provisions the Wikimedia Transparency Report static site,
+# hosted at <http://transparency.wikimedia.org>.
+#
+class role::transparency {
+    include ::apache
+
+    $repo_dir = '/srv/TransparencyReport'
+    $docroot  = "${repo_dir}/build"
+
+    git::clone { 'wikimedia/TransparencyReport':
+        ensure    => latest,
+        directory => $repo_dir,
+    }
+
+    apache::site { 'transparency.wikimedia.org':
+        content => template('apache/sites/transparency.wikimedia.org.erb'),
+    }
+
+    ferm::service { 'transparency_http':
+        proto => 'tcp',
+        port  => '80',
+    }
+}
