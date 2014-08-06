78fb5f3f6e5333aca5220f39fe978250711c3813
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-08-06 14:42:50 +0300

    postgres: Followup commit for ebcb7cd
    Also allow the slave to have the datadir overriden
    Update rspec tests as well
    
    Change-Id: I0160bd695db0665dd2b4692de41635b37a3d0ba1

--- modules/postgresql/spec/classes/postgresql_slave_spec.rb
+++ modules/postgresql/spec/classes/postgresql_slave_spec.rb
@@ -29,6 +29,32 @@ end
 describe 'postgresql::slave', :type => :class do
     let(:params) { {
         :pgversion        => '9.1',
+        :ensure           => 'present',
+        :master_server    => 'test',
+        :replication_pass => 'pass',
+        :datadir          => '/srv/postgres',
+        }
+    }
+    context 'ensure present' do
+        it { should contain_class('postgresql::server') }
+        it do
+            should contain_file('/etc/postgresql/9.1/main/postgresql.conf').
+                with_ensure('present').
+                with_content(/include 'slave.conf'/)
+        end
+        it { should contain_file('/etc/postgresql/9.1/main/slave.conf').with_ensure('present') }
+        it do
+            should contain_file('/srv/postgres/recovery.conf').
+                with_ensure('present').
+                with_content(/host=test user=replication password=pass/)
+        end
+        it { should contain_exec('pg_basebackup-test').with_command(/-h test -U replication -w/)}
+    end
+end
+
+describe 'postgresql::slave', :type => :class do
+    let(:params) { {
+        :pgversion        => '9.1',
         :ensure           => 'absent',
         :master_server    => 'test',
         :replication_pass => 'pass',
