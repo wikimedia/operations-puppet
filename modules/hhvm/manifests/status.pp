cfb0e8c9b51839f936553228ea1cf78d67963fc3
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-08-06 15:55:35 +0200

    hhvm: fix status
    Unluckily enough, HHVM admin interface speaks FastCGI but... it doesn't
    understand it, so we should ask apache the exact same url we will then
    proxy to the HHVM admin interface (meh!). So, converting this to a
    virtualhost that will listen on a specified port; this is ugly but it
    will at least succeed (note that I could remove the guards on proxying
    since we're using a different port).
    
    Change-Id: Ibf0b86b52e26fc68012969fe9797b370227cec15
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/hhvm/manifests/status.pp
+++ modules/hhvm/manifests/status.pp
@@ -1,13 +1,21 @@
 # == Class: hhvm::status
 #
-# Configures Apache to proxy web requests for </hhvm-status> from
+# Configures Apache to proxy web requests for Port 9002 from
 # internal IPs to HHVM's admin server port.
 #
-class hhvm::status {
+class hhvm::status ($port = 9002){
     include ::network::constants
     include ::apache::mod::proxy_fcgi
 
-    apache::conf { 'hhvm_status':
-        content => template('hhvm/hhvm-status.conf.erb'),
+    file_line { 'add_hhvm_monitoring_port':
+        ensure  => present,
+        path    => '/etc/apache2/ports.conf',
+        line    => "Listen ${port}",
+        require => Package['apache2']
+    }
+
+    apache2::site { 'hhvm_status':
+        content  => template('hhvm/hhvm-status.conf.erb'),
+        priority => 99,
     }
 }
