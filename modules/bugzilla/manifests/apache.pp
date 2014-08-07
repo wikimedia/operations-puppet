5d2e7bb727aeb98debe125b0acf1214897fc7edb
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-08-07 14:58:33 +0200

    bugzilla: use ssl_ciphersuite
    Change-Id: I2bf2a0d8b4f7064615ac31eff78f3237eb26298c
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/bugzilla/manifests/apache.pp
+++ modules/bugzilla/manifests/apache.pp
@@ -1,7 +1,7 @@
 # this class sets up the Apache site config and SSL certs
 # for a Wikimedia Bugzilla installation
 # it expects {'webserver::php5': ssl => true; } on the node
-class bugzilla::apache ($svc_name, $attach_svc_name, $docroot, $cipher_suite){
+class bugzilla::apache ($svc_name, $attach_svc_name, $docroot){
 
     include ::apache::mod::headers
     include ::apache::mod::expires
@@ -11,6 +11,8 @@ class bugzilla::apache ($svc_name, $attach_svc_name, $docroot, $cipher_suite){
     install_certificate{ $svc_name: }
     install_certificate{ $attach_svc_name: }
 
+    $ssl_settings = ssl_ciphersuite('apache-2.2', 'compat')
+
     # this includes them both, 80 and 443
     apache::site { 'bugzilla.wikimedia.org':
         content  => template("bugzilla/apache/${svc_name}.erb"),
@@ -18,4 +20,3 @@ class bugzilla::apache ($svc_name, $attach_svc_name, $docroot, $cipher_suite){
     }
 
 }
-
