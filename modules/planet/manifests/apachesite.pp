e5d262e4ebffa469ad8c43243eee8a7c9d6348d8
dzahn <dzahn@wikimedia.org>
2014-08-07 09:22:14 -0700

    fix apache site setup for planet languages
    follow-up fix to Iddc778a281639
    which put these directly into sites-enabled
    
    we want them to be properly in sites-available
    though and then symlink like they did before with
    the old apache-site method
    
    Change-Id: I306feeb35196e8fd57f7a954483ffff2da38dd18

--- modules/planet/manifests/apachesite.pp
+++ modules/planet/manifests/apachesite.pp
@@ -1,14 +1,8 @@
 # defined type: an apache site config for a planet-venus language version
 define planet::apachesite {
 
-    $sites_directory = '/etc/apache2/sites-enabled'
-
-    file { "${sites_directory}/${title}.planet.${planet::planet_domain_name}":
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
+    apache::site { "${title}.planet.${planet::planet_domain_name}":
         content => template('planet/apache/planet-language.erb'),
-        require => Class['planet::webserver'],
     }
 
 }
