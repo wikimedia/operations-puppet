113ee9647433fb4e45a967cd892d8f6a5c0d877b
Marius Hoch <hoo@online.de>
2014-08-07 16:39:09 +0200

    Allow "hoo" to sudo into datasets
    Needed to keep and eye on and restart the Wikidata json
    dumps (and the future rdf and property dumps).
    
    This is done by introducing a new group snapshot-admin
    which allows me to sudo into the "datasets" user.
    
    RT: 8286
    
    Bug: T86808
    
    Change-Id: I1863679d72aeb4ab9aa527ec24bcbda290a8849f

--- manifests/site.pp
+++ manifests/site.pp
@@ -2300,6 +2300,10 @@ node /^snapshot100[1-4]\.eqiad\.wmnet/ {
     if $::fqdn == 'snapshot1003.eqiad.wmnet' {
         include role::snapshot::cron::primary
     }
+
+    class { 'admin':
+        groups => ['snapshot-admins'],
+    }
 }
 
 node 'terbium.eqiad.wmnet' {
