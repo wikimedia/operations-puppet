838c63d682c994786342e74cc7b83b8370bdc617
jgreen <jgreen@wikimedia.org>
2014-08-07 14:47:50 -0400

    grr, redo ocg nagios check foo, was all wrong
    Change-Id: I47937ac0881ca93637e3c47966fad895f6f3ca73

--- modules/ocg/manifests/nagios/check.pp
+++ modules/ocg/manifests/nagios/check.pp
@@ -2,65 +2,21 @@
 # Sets up icinga alerts for an Offline Content Generator instance.
 #
 class ocg::nagios::check (
-        $warning_temp_dir = '-', # dir size i.e. 4G
-        $critical_temp_dir = '-', # dir size i.e. 5G
-        $warning_output_dir = '-', # dir size i.e. 4G
-        $critical_output_dir = '-', # dir size i.e. 5G
-        $warning_postmortem_dir = '-', # dir size i.e. 1G
-        $critical_postmortem_dir = '-', # dir size i.e. 2G
-        $warning_job_status = '-', # messages i.e. 20000
-        $critical_job_status = '-', # messages i.e. 30000
-        $warning_render_jobs = '-', # messages i.e. 100
-        $critical_render_jobs = '-', # messages i.e. 500
-        $ocg_health_report_url = '-', # http://localhost:8000/?command=health
+        $wtd, # warning temp dir size i.e. 4G
+        $ctd, # critical temp dir size i.e. 5G
+        $wod, # warning output dir size i.e. 4G
+        $cod, # critical output dir size i.e. 5G
+        $wpd, # warning postmortem dir size i.e. 1G
+        $cpd, # critical postmortem dir size i.e. 2G
+        $wjs, # warning job status queue messages i.e. 20000
+        $cjs, # critical job status queue messages i.e. 30000
+        $wrj, # warning render jobs queue messages i.e. 100
+        $crj, # critical render jobs queue messages i.e. 500
+        $url = 'http://localhost:8000/?command=health', # OCG health check URL
     ) {
     include ocg::nagios::plugin
-    $wtd = $warning_temp_dir ? {
-        '-'  => '',
-        default => " --wtd ${warning_temp_dir}",
-    }
-    $ctd = $critical_temp_dir ? {
-        '-'  => '',
-        default => " --ctd ${critical_temp_dir}",
-    }
-    $wod = $warning_output_dir ? {
-        '-'  => '',
-        default => " --wod ${warning_output_dir}",
-    }
-    $cod = $critical_output_dir ? {
-        '-'  => '',
-        default => " --cod ${critical_output_dir}",
-    }
-    $wpd = $warning_postmortem_dir ? {
-        '-'  => '',
-        default => " --wpd ${warning_postmortem_dir}",
-    }
-    $cpd = $critical_postmortem_dir ? {
-        '-'  => '',
-        default => " --cpd ${critical_postmortem_dir}",
-    }
-    $wjs = $warning_job_status ? {
-        '-'  => '',
-        default => " --wjs ${warning_job_status}",
-    }
-    $cjs = $critical_job_status ? {
-        '-'  => '',
-        default => " --cjs ${critical_job_status}",
-    }
-    $wrj = $warning_render_jobs ? {
-        '-'  => '',
-        default => " --wrj ${warning_render_jobs}",
-    }
-    $crj = $critical_render_jobs ? {
-        '-'  => '',
-        default => " --crj ${critical_render_jobs}",
-    }
-    $url = $ocg_health_report_url ? {
-        '-'  => '',
-        default => " --url ${ocg_health_report_url}",
-    }
     monitor_service { 'ocg':
-        check_command => "check_ocg_health ${wtd}${ctd}${wod}${cod}${wpd}${cpd}${wjs}${cjs}${wrj}${crj}${url}",
+        check_command => "check_ocg_health!${wtd}!${ctd}!${wod}!${cod}!${wpd}!${cpd}!${wjs}!${cjs}!${wrj}!${crj}!${url}",
         description   => 'OCG health check',
     }
 }
