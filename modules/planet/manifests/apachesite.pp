518ef71c7767dd119b0ace80cf13d1c7e9ab32d6
dzahn <dzahn@wikimedia.org>
2014-08-09 12:46:09 -0700

    planet - use ssl_ciphersuite
    Change-Id: I9d4b01fdd82a0a97beb899c3e5c6dbd26d8d384f

--- modules/planet/manifests/apachesite.pp
+++ modules/planet/manifests/apachesite.pp
@@ -1,6 +1,8 @@
 # defined type: an apache site config for a planet-venus language version
 define planet::apachesite {
 
+    $ssl_settings = ssl_ciphersuite('apache-2.2', 'compat')
+
     apache::site { "${title}.planet.${planet::planet_domain_name}":
         content => template('planet/apache/planet-language.erb'),
     }
--- modules/planet/manifests/index_site.pp
+++ modules/planet/manifests/index_site.pp
@@ -1,8 +1,10 @@
 # sets up the planet-venus index/portal site
 class planet::index_site {
 
- apache::site { "planet.${planet::planet_domain_name}":
-    content => template('planet/apache/planet.erb'),
- }
+    $ssl_settings = ssl_ciphersuite('apache-2.2', 'compat')
+
+    apache::site { "planet.${planet::planet_domain_name}":
+        content => template('planet/apache/planet.erb'),
+    }
 
 }
