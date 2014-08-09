d97ecd853a5ad28459166de0417c17e302a57a52
dzahn <dzahn@wikimedia.org>
2014-08-09 13:07:09 -0700

    sudo.pp - minor linting fixes
    the extra whitespace causes lint errors
    about unaligned errors
    
    and ensure should always be first
    
    Change-Id: I6221817f6ca2f153b9b15f3ebf46cf07eea5e290

--- manifests/sudo.pp
+++ manifests/sudo.pp
@@ -2,26 +2,26 @@
 
 define sudo_user( $privileges ) {
     $user = $title
- 
+
     file { "/etc/sudoers.d/${user}":
         owner   => root,
         group   => root,
         mode    => '0440',
         content => template('sudo/sudoers.erb');
     }
- 
+
 }
 
 define sudo_group( $privileges=[], $ensure='present', $group = $title ) {
- 
+
     file { "/etc/sudoers.d/${title}":
+        ensure  => $ensure,
         owner   => root,
         group   => root,
         mode    => '0440',
         content => template('sudo/sudoers.erb'),
-        ensure  => $ensure;
     }
- 
+
 }
 
 class sudo::labs_project {
@@ -48,12 +48,12 @@ class sudo::labs_project {
 class sudo::appserver {
 
     file { '/etc/sudoers.d/appserver':
+        ensure => present,
         path   => '/etc/sudoers.d/appserver',
         owner  => root,
         group  => root,
         mode   => '0440',
         source => 'puppet:///files/sudo/sudoers.appserver',
-        ensure => present;
     }
 
 }
