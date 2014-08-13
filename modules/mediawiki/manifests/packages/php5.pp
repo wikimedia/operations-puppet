2be0fc9211d6fdf018912129bb762d69dba0eab4
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-08-13 12:38:27 +0200

    Add mediawiki::packages::php5
    When it's safe to remove PHP5 packages from the Trusty app servers, we'll
    include this class conditionally, based on the distro version.
    
    Change-Id: Ie9cb163d96520de637b9cd94a8f7a42be2219e08
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/mediawiki/manifests/packages/php5.pp
@@ -0,0 +1,49 @@
+# == Class: mediawiki::packages::php5
+#
+# This class declares packages that make up Wikimedia's PHP5-based
+# MediaWiki deployment stack. As of August 2014, this is the most common
+# configuration in production, but new installs are configured for HHVM
+# instead.
+#
+class mediawiki::packages::php5 {
+    # Run-time
+    package { [
+        'php5-common',
+        'php5-cli',
+        'php-apc',
+    ]:
+        ensure => present,
+    }
+
+    # Wikimedia PHP extensions
+    package { [
+        'php-luasandbox',
+        'php-wikidiff2',
+        'php5-fss',
+    ]:
+        ensure => present,
+    }
+
+    # Third-party PHP extensions
+    package { [
+        'php5-curl',
+        'php5-geoip',
+        'php5-intl',
+        'php5-memcached',
+        'php5-mysql',
+        'php5-redis',
+        'php5-xmlrpc',
+    ]:
+        ensure => present,
+    }
+
+    if ubuntu_version('precise') {
+        package { [
+            'libmemcached11',  # Formerly a dependency for php5-memcached
+            'php5-igbinary',   # No longer in use
+            'php5-wmerrors',   # Not packaged for Trusty
+        ]:
+            ensure => present,
+        }
+    }
+}
