d67e8abbf9f68cb4d392687c3a93b78092e42784
dzahn <dzahn@wikimedia.org>
2014-08-13 16:32:02 -0700

    stats.wm.org - use ssl_ciphersuite
    In the past, when we changed SSL cipher settings
    we had to create many changes, usually one per service.
    
    Then Giuseppe introduced ssl_ciphersuite in I9bc1104b7f770d9
    which moves the settings into a centralized function.
    
    This change makes it actually use the new function.
    
    One last time we are going through all of the services,
    but in the future we will be able make a single change
    to update ciphers across misc. services.
    
    Change-Id: I0cb4100a527b7cabb3f0e548d942959bfcf93c0d

--- manifests/misc/statistics.pp
+++ manifests/misc/statistics.pp
@@ -322,8 +322,11 @@ class misc::statistics::sites::stats {
         mode    => '0644',
         owner   => 'root',
         group   => 'root',
-        source  => 'puppet:///files/apache/ports.conf.ssl',
+        source => 'puppet:///files/apache/ports.conf.ssl',
     }
+
+    $ssl_settings = ssl_ciphersuite('apache-2.2', 'compat')
+
 }
 
 # community-analytics.wikimedia.org
