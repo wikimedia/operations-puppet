15a1803cc13c381e86eb81a3d244e6a831a8ac4f
Ori Livneh <ori@wikimedia.org>
2014-08-15 17:14:24 +0300

    MediaWiki/HHVM: log fatals to fluorine:/a/mw-log/hhvm-fatal.log
    * Remove hstr Python script. It's not useful at the moment. It can always be
      ressurected later.
    * Set HHVM_TRACE_HANDLER to "/usr/bin/logger --tag hhvm-fatal --file ".
      This will log the fatal to syslog with the 'hhvm-fatal' tag.
    * Configure MediaWiki to forward syslog messages tagged with 'hhvm-fatal' to
      fluorine.
    * Rename syslog.pp's 'apache_log_aggregator' parameter to 'log_aggregator', to
      make it clear that it is not Apache-specific.
    
    Change-Id: I98f9fc20cbe9c32ddee04a77427e06446e2f6de1

--- modules/mediawiki/manifests/syslog.pp
+++ modules/mediawiki/manifests/syslog.pp
@@ -1,14 +1,15 @@
 # syslog instance and configuration for applicationservers
-class mediawiki::syslog( $apache_log_aggregator ) {
-    file { '/etc/logrotate.d/mediawiki_apache':
-        source  => 'puppet:///modules/mediawiki/logrotate.d_mediawiki_apache',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
+class mediawiki::syslog( $log_aggregator ) {
+    rsyslog::conf { 'mediawiki':
+        content  => template('mediawiki/rsyslog.conf.erb'),
+        priority => 40,  # before 50-default.conf
     }
 
-    rsyslog::conf { 'mediawiki_apache':
-        content  => template('mediawiki/apache/rsyslog.conf.erb'),
-        priority => 40,
+    file { '/etc/logrotate.d/mediawiki_apache':
+        source => 'puppet:///modules/mediawiki/logrotate.d_mediawiki_apache',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        before => Rsyslog::Conf['mediawiki'],
     }
 }
