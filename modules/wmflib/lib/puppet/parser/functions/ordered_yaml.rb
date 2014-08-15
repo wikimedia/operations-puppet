3d6552a6fcd47b4dce2a6cb70d9c28bba0f2f8dc
Ori Livneh <ori@wikimedia.org>
2014-08-16 02:48:03 +0300

    ordered_yaml(): fix for Ruby 1.8
    This patch reverts I62605c2a2, restoring the previous (and known-good) version
    of ordered_json().
    
    It also fixes the implementation of ordered_yaml(), which didn't work properly
    with Ruby 1.8. Hash keys in Ruby 1.8 are not sorted in insertion order, so it's
    not sufficient to recursively copy keys in sorted order into a new hash.
    Instead, we patch the .each_pair method to iterate on keys in sorted order. We
    only do this for hashes we create.
    
    Change-Id: Ieb08e604ef3552c928c78aab2d44a7bae7966dd6

--- modules/wmflib/lib/puppet/parser/functions/ordered_yaml.rb
+++ modules/wmflib/lib/puppet/parser/functions/ordered_yaml.rb
@@ -1,1 +1,35 @@
-ordered_json.rb
+# == Function: ordered_yaml()
+#
+# Emit a hash as YAML with keys (both shallow and deep) in sorted order.
+#
+require 'yaml'
+
+def sort_keys_recursive(value)
+    # Prepare a value for YAML serialization by sorting its keys (if it is
+    # a hash) and the keys of any hash object that is contained within the
+    # value. Returns a new value.
+    case value
+    when Array
+        value.map { |elem| sort_keys_recursive(elem) }
+    when Hash
+        map = {}
+
+        def map.each_pair
+            super.sort
+        end
+
+        value.sort.inject(map) { |h, (k, v)| h[k] = sort_keys_recursive(v); h }
+    when 'true', 'false'
+        value == 'true'
+    when :undef
+        nil
+    else
+        value.include?('.') ? Float(value) : Integer(value) rescue value
+    end
+end
+
+module Puppet::Parser::Functions
+    newfunction(:ordered_yaml, :type => :rvalue, :arity => 1) do |args|
+        sort_keys_recursive(args.first).to_yaml.gsub(/^---\s*/, '') << "\n"
+    end
+end
