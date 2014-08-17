d642130a799f8bfbcfa2844cf2b9edc55a3b7bd8
YuviPanda <yuvipanda@gmail.com>
2014-08-17 16:13:35 +0100

    quarry: Use mariadb 10
    Change-Id: Ibd0636bf680a3f1e8b2cda0d7a735007fe676fb2

--- modules/quarry/manifests/init.pp
+++ modules/quarry/manifests/init.pp
@@ -58,9 +58,10 @@ class quarry::database {
     $data_path = "/srv/mysql/data"
 
     class { 'mysql::server':
-        config_hash => {
-            'datadir' => $data_path,
-            'bind_address' => '0.0.0.0'
+        package_name => 'mariadb-server',
+        config_hash           => {
+            'datadir'         => $data_path,
+            'bind_address'    => '0.0.0.0',
         }
     }
 }
