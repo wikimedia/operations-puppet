8d744b60ae82efc6358aa011fe7a222ef7b2d32e
Antoine Musso <hashar@free.fr>
2014-08-17 21:32:42 +0200

    contint: proxy the colocated Gerrit instance
    Listening on port 8082 and on the host integration.wmflabs.org.
    
    Change-Id: Ib9f31a330c193b807bfb119469b1bdd3155249e9

--- modules/contint/manifests/website/labs.pp
+++ modules/contint/manifests/website/labs.pp
@@ -1,5 +1,8 @@
 class contint::website::labs {
 
+    # The contint dev instance also has Gerrit
+    include contint::proxy_gerrit
+
     apache::site { 'integrationlabs':
         content => template('contint/apache/integration.wmflabs.org.erb'),
     }
