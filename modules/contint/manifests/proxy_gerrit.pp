88e4c1c96df36163ac8fe1ae088054eda4f6805c
Antoine Musso <hashar@free.fr>
2014-08-17 21:32:42 +0200

    contint: proxy the colocated Gerrit instance
    Listening on port 8082 and on the host integration.wmflabs.org.
    
    Change-Id: Ib9f31a330c193b807bfb119469b1bdd3155249e9

---
+++ modules/contint/manifests/proxy_gerrit.pp
@@ -0,0 +1,16 @@
+# A http proxy in front of Gerrit page
+# Only meant to be used on labs
+class contint::proxy_gerrit {
+
+    if $::realm != 'labs' {
+        fail( "contint::proxy_gerrit must only be used on labs")
+    }
+
+    file { '/etc/apache/gerrit_proxy':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/contint/apache/proxy_gerrit',
+    }
+}
+
