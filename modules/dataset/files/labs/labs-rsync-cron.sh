acdb71547c38f657185c90b25392a847a6a2362f
Coren <marc@uberbox.org>
2014-08-19 13:35:41 -0400

    Dump: make labs-rsync-cron.sh paranoid
    Check that the destination mount (/mnt/dumps) is there before
    trying the copy and bail out if it doesn't seem to be there.
    
    Change-Id: I3b69bf5c87d29e94c947ccd6125592ba426f1c8c

--- modules/dataset/files/labs/labs-rsync-cron.sh
+++ modules/dataset/files/labs/labs-rsync-cron.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 running=`pgrep -u root -f   'python /root/wmfdumpsmirror.py --remotedir /data/xmldatadumps/public'`
-if [ -z "$running" ]; then
-    python /usr/local/bin/wmfdumpsmirror.py --remotedir /data/xmldatadumps/public --localdir /mnt/dumps/public --filesperjob 50 --sizeperjob 5G --workercount 1 --rsynclist rsync-filelist-last-2-good.txt.rsync --rsyncargs -rlptq
-fi
-running=`pgrep -u root -f -x  '/usr/bin/rsync -rlpt /data/xmldatadumps/public/other/incr /mnt/dumps/'`
-if [ -z "$running" ]; then
-    /usr/bin/rsync -rlpt /data/xmldatadumps/public/other/incr /mnt/dumps/
-fi
-running=`pgrep -u root -f -x '/usr/bin/rsync -rlpt /data/xmldatadumps/public/other/pagecounts-raw /mnt/dumps/'`
-if [ -z "$running" ]; then
-    /usr/bin/rsync -rlpt /data/xmldatadumps/public/other/pagecounts-raw /mnt/dumps/
+
+if [ -d /mnt/dumps/lost+found ]; then
+
+    if [ -z "$running" ]; then
+        python /usr/local/bin/wmfdumpsmirror.py --remotedir /data/xmldatadumps/public --localdir /mnt/dumps/public --filesperjob 50 --sizeperjob 5G --workercount 1 --rsynclist rsync-filelist-last-2-good.txt.rsync --rsyncargs -rlptq
+    fi
+    running=`pgrep -u root -f -x  '/usr/bin/rsync -rlpt /data/xmldatadumps/public/other/incr /mnt/dumps/'`
+    if [ -z "$running" ]; then
+        /usr/bin/rsync -rlpt /data/xmldatadumps/public/other/incr /mnt/dumps/
+    fi
+    running=`pgrep -u root -f -x '/usr/bin/rsync -rlpt /data/xmldatadumps/public/other/pagecounts-raw /mnt/dumps/'`
+    if [ -z "$running" ]; then
+        /usr/bin/rsync -rlpt /data/xmldatadumps/public/other/pagecounts-raw /mnt/dumps/
+    fi
+
+else
+    echo "$0: mount doesn't appear there.  Bailing out!" >&2
+    exit 1
 fi
