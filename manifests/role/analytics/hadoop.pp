b87ef7b2dc5f0b6b1e329602f62b8e40ad6f9c9d
Andrew Otto <acotto@gmail.com>
2014-08-19 16:06:23 -0400

    Temporary debug notify statements to find 'Infinity' on stat1002
    Change-Id: Idb7306485e78691af026a18a6a717f42bb09bff8

--- manifests/role/analytics/hadoop.pp
+++ manifests/role/analytics/hadoop.pp
@@ -152,6 +152,7 @@ class role::analytics::hadoop::config {
             $reserve_memory_mb = 65536
         }
 
+        notify { "memorysize_mb: $::memorysize_mb, reserve_memory_mb: $reserve_memory_mb": }
         # Memory available for use by Hadoop jobs.
         $available_memory_mb = $::memorysize_mb - $reserve_memory_mb
 
@@ -170,10 +171,18 @@ class role::analytics::hadoop::config {
         else  {
             $min_container_size_mb = 2048 + 0
         }
+
+        notify { "min_container_size_mb: $min_container_size_mb": }
+        $container_by_ram = $available_memory_mb / $min_container_size_mb
+        notify { "container_by_ram: $container_by_ram": }
+
         # number of containers = min (2*CORES, 1.8*DISKS, (Total available RAM) / MIN_CONTAINER_SIZE)
         $number_of_containers                     = floor(min(2 * $::processorcount, 1.8 * size($datanode_mounts), $available_memory_mb / $min_container_size_mb))
+        notify { "number_of_containers: $number_of_containers": }
+
         # RAM-per-container = max(MIN_CONTAINER_SIZE, (Total Available RAM) / containers))
         $memory_per_container_mb                  = max($min_container_size_mb, $available_memory_mb / $number_of_containers)
+        notify { "memory_per_container_mb: $memory_per_container_mb": }
 
         $mapreduce_map_memory_mb                  = floor($memory_per_container_mb)
         $mapreduce_reduce_memory_mb               = floor(2 * $memory_per_container_mb)
@@ -306,6 +315,7 @@ class role::analytics::hadoop::client inherits role::analytics::hadoop::config {
         mapreduce_output_compression             => $mapreduce_output_compression,
         mapreduce_output_compression_codec       => $mapreduce_output_compression_codec,
         mapreduce_output_compression_type        => $mapreduce_output_compression_type,
+
         mapreduce_map_tasks_maximum              => $mapreduce_map_tasks_maximum,
         mapreduce_reduce_tasks_maximum           => $mapreduce_reduce_tasks_maximum,
         mapreduce_job_reuse_jvm_num_tasks        => $mapreduce_job_reuse_jvm_num_tasks,
