57f3be674eb033a7501bc1a8ce738a9533f703f8
Ori Livneh <ori@wikimedia.org>
2014-08-28 11:56:05 -0700

    mediawiki: add packages::fonts and packages::multimedia
    Continue factoring out bunches of package dependencies into discrete subclasses
    of mediawiki::packages.
    
    Change-Id: I27e84f9286a4df60b0eb81a0cd31c13746851102

--- modules/mediawiki/manifests/multimedia.pp
+++ modules/mediawiki/manifests/multimedia.pp
@@ -4,32 +4,8 @@
 # and video processing.
 #
 class mediawiki::multimedia {
-    include ::mediawiki::multimedia::fonts
-
-    if ubuntu_version('>= trusty') {
-        $libav_package   = 'libav-tools'
-        $libvips_package = 'libvips37'
-    } else {
-        $libav_package   = 'ffmpeg'
-        $libvips_package = 'libvips15'
-    }
-
-    package { [
-        $libav_package,
-        $libvips_package,
-        'ffmpeg2theora',
-        'fontconfig-config',
-        'ghostscript',
-        'libjpeg-turbo-progs',
-        'libogg0',
-        'libtheora0',
-        'libvips-tools',
-        'libvorbisenc2',
-        'netpbm',
-        'oggvideotools',
-    ]:
-        ensure => present,
-    }
+    include ::mediawiki::packages::multimedia
+    include ::mediawiki::packages::fonts
 
     file { '/etc/fonts/conf.d/70-no-bitmaps.conf':
         ensure  => link,
@@ -50,61 +26,5 @@ class mediawiki::multimedia {
         type    => 'ctime',
         backup  => false,
         recurse => 1,
-    }
-}
-
-class mediawiki::multimedia::fonts {
-    package { [
-        'fonts-arabeyes',
-        'fonts-arphic-ukai',
-        'fonts-arphic-uming',
-        'fonts-farsiweb',
-        'fonts-kacst',
-        'fonts-khmeros',
-        'fonts-lao',
-        'fonts-liberation',
-        'fonts-linuxlibertine',
-        'fonts-manchufont',
-        'fonts-mgopen',
-        'fonts-nafees',
-        'fonts-sil-abyssinica',
-        'fonts-sil-ezra',
-        'fonts-sil-padauk',
-        'fonts-sil-scheherazade',
-        'fonts-takao-gothic',
-        'fonts-takao-mincho',
-        'fonts-thai-tlwg',
-        'fonts-tibetan-machine',
-        'fonts-unfonts-extra',
-        'texlive-fonts-recommended',
-        'ttf-alee',
-        'ttf-bengali-fonts',
-        'ttf-devanagari-fonts',
-        'ttf-gujarati-fonts',
-        'ttf-kannada-fonts',
-        'ttf-malayalam-fonts',
-        'ttf-oriya-fonts',
-        'ttf-punjabi-fonts',
-        'ttf-tamil-fonts',
-        'ttf-telugu-fonts',
-        'ttf-ubuntu-font-family',
-        'ttf-wqy-zenhei',
-        'xfonts-100dpi',
-        'xfonts-75dpi',
-        'xfonts-base',
-        'xfonts-mplus',
-        'xfonts-scalable',
-        'fonts-sil-nuosusil',        # RT 6500
-        'culmus',                    # Bug 38946
-        'culmus-fancy',              # Bug 38946
-        'fonts-lklug-sinhala',       # Bug 55462
-        'fonts-vlgothic',            # Bug 64002
-        'ttf-dejavu-core',           # Bug 63206
-        'ttf-dejavu-extra',          # Bug 63206
-        'ttf-kochi-gothic',          # Bug 64002
-        'ttf-kochi-mincho',          # Bug 64002
-        'ttf-lyx',                   # Bug 38299
-    ]:
-        ensure => present,
     }
 }
---
+++ modules/mediawiki/manifests/packages/fonts.pp
@@ -0,0 +1,59 @@
+# == Class: mediawiki::packages::fonts
+#
+# Provisions font packages used by MediaWiki.
+#
+class mediawiki::multimedia::fonts {
+    package { [
+        'fonts-arabeyes',
+        'fonts-arphic-ukai',
+        'fonts-arphic-uming',
+        'fonts-farsiweb',
+        'fonts-kacst',
+        'fonts-khmeros',
+        'fonts-lao',
+        'fonts-liberation',
+        'fonts-linuxlibertine',
+        'fonts-manchufont',
+        'fonts-mgopen',
+        'fonts-nafees',
+        'fonts-sil-abyssinica',
+        'fonts-sil-ezra',
+        'fonts-sil-padauk',
+        'fonts-sil-scheherazade',
+        'fonts-takao-gothic',
+        'fonts-takao-mincho',
+        'fonts-thai-tlwg',
+        'fonts-tibetan-machine',
+        'fonts-unfonts-extra',
+        'texlive-fonts-recommended',
+        'ttf-alee',
+        'ttf-bengali-fonts',
+        'ttf-devanagari-fonts',
+        'ttf-gujarati-fonts',
+        'ttf-kannada-fonts',
+        'ttf-malayalam-fonts',
+        'ttf-oriya-fonts',
+        'ttf-punjabi-fonts',
+        'ttf-tamil-fonts',
+        'ttf-telugu-fonts',
+        'ttf-ubuntu-font-family',
+        'ttf-wqy-zenhei',
+        'xfonts-100dpi',
+        'xfonts-75dpi',
+        'xfonts-base',
+        'xfonts-mplus',
+        'xfonts-scalable',
+        'fonts-sil-nuosusil',        # RT 6500
+        'culmus',                    # Bug 38946
+        'culmus-fancy',              # Bug 38946
+        'fonts-lklug-sinhala',       # Bug 55462
+        'fonts-vlgothic',            # Bug 64002
+        'ttf-dejavu-core',           # Bug 63206
+        'ttf-dejavu-extra',          # Bug 63206
+        'ttf-kochi-gothic',          # Bug 64002
+        'ttf-kochi-mincho',          # Bug 64002
+        'ttf-lyx',                   # Bug 38299
+    ]:
+        ensure => present,
+    }
+}
