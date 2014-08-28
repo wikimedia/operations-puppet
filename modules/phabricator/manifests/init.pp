dd17c5bdc11ce373bf53f6844471cfd14d3c91fd
cpettet <rush@wikimedia.org>
2014-08-28 14:47:14 -0500

    phabricator elastic search logic
    Change-Id: Ieb05b5ee067b6612b63cd5b3777904b4cd842937

--- modules/phabricator/manifests/init.pp
+++ modules/phabricator/manifests/init.pp
@@ -182,6 +182,18 @@ class phabricator (
         mode    => '0500',
     }
 
+    # Phabricator needs an initial index built and from there
+    # will update as appropriate.
+    if ($phab_settings['search.elastic.host']) {
+        $create_index = "${phabdir}/phabricator/bin/search index --all"
+        exec { 'elastic_search_setup':
+            command   => "${create_index} && touch ${phabdir}/needs_es_indexed_false",
+            creates   => "${phabdir}/needs_es_indexed_false",
+            logoutput => true,
+            require   => File["${phabdir}/phabricator/conf/local/local.json"],
+        }
+    }
+
     #https://secure.phabricator.com/book/phabricator/article/managing_daemons/
     $phd = "${phabdir}/phabricator/bin/phd"
     service { 'phd':
