1c657b3c78597ff21e49f42bcc17ace5290f810d
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-08-29 10:36:20 +0300

    Add DNS views in ganglia
    Adding recursive and authoritative ganglia views. Introduce a class that
    groups most stuff to not deviate from the rest of the code.
    
    Change-Id: I0683df5a351d6ad6b2b6673bce0ec32234173e4d

--- manifests/misc/monitoring.pp
+++ manifests/misc/monitoring.pp
@@ -122,6 +122,103 @@ class misc::monitoring::views {
         master       => 'analytics1010.eqiad.wmnet',
         worker_regex => 'analytics10(11|[3-9]|20).eqiad.wmnet',
     }
+
+    include misc::monitoring::views::dns
+}
+
+class misc::monitoring::views::dns {
+    $auth_dns_host_regex = '^(rubidium|mexia|eeden.esams).wikimedia.org$'
+    $rec_dns_host_regex = '^(chromium|hydrogen).wikimedia.org$'
+
+    ganglia::view { 'authoritative_dns':
+        ensure => 'present',
+        graphs => [
+            {
+            'title'         => 'DNS UDP Requests',
+            'host_regex'    => $auth_dns_host_regex,
+            'metric_regex'  => '^gdnsd_udp_reqs$',
+            'type'          => 'stack',
+            },
+            {
+            'title'         => 'DNS TCP Requests',
+            'host_regex'    => $auth_dns_host_regex,
+            'metric_regex'  => '^gdnsd_tcp_reqs$',
+            'type'          => 'stack',
+            },
+            {
+            'title'         => 'DNS NXDOMAIN',
+            'host_regex'    => $auth_dns_host_regex,
+            'metric_regex'  => '^gdnsd_stats_nxdomain$',
+            'type'          => 'stack',
+            },
+            {
+            'title'         => 'DNS REFUSED',
+            'host_regex'    => $auth_dns_host_regex,
+            'metric_regex'  => '^gdnsd_stats_refused$',
+            'type'          => 'stack',
+            },
+            {
+            'title'         => 'DNS queries over IPv6',
+            'host_regex'    => $auth_dns_host_regex,
+            'metric_regex'  => '^gdnsd_stats_v6$',
+            'type'          => 'stack',
+            },
+            {
+            'title'         => 'DNS EDNS Client Subnet requests',
+            'host_regex'    => $auth_dns_host_regex,
+            'metric_regex'  => '^gdnsd_stats_edns_clientsub$',
+            'type'          => 'stack',
+            },
+        ]
+    }
+
+    ganglia::view { 'recursive_dns':
+        ensure => 'present',
+        graphs => [
+            {
+            'title'         => 'DNS Outgoing queries',
+            'host_regex'    => $rec_dns_host_regex,
+            'metric_regex'  => '^pdns_all-outqueries$',
+            'type'          => 'stack',
+            },
+            {
+            'title'         => 'DNS Answers',
+            'host_regex'    => $rec_dns_host_regex,
+            'metric_regex'  => '^pdns_answers.*$',
+            'type'          => 'stack',
+            },
+            {
+            'title'         => 'DNS cache',
+            'host_regex'    => $rec_dns_host_regex,
+            'metric_regex'  => '^pdns_cache-.*$',
+            'type'          => 'stack',
+            },
+            {
+            'title'         => 'DNS IPv6 Outgoing queries',
+            'host_regex'    => $rec_dns_host_regex,
+            'metric_regex'  => '^pdns_ipv6-outqueries$',
+            'type'          => 'stack',
+            },
+            {
+            'title'         => 'DNS Incoming queries',
+            'host_regex'    => $rec_dns_host_regex,
+            'metric_regex'  => '^pdns_questions$',
+            'type'          => 'stack',
+            },
+            {
+            'title'         => 'DNS Servfails',
+            'host_regex'    => $rec_dns_host_regex,
+            'metric_regex'  => '^pdns_servfail-asnwers$',
+            'type'          => 'stack',
+            },
+            {
+            'title'         => 'DNS NXDOMAIN',
+            'host_regex'    => $rec_dns_host_regex,
+            'metric_regex'  => '^pdns_nxdomain-asnwers$',
+            'type'          => 'stack',
+            },
+        ]
+    }
 }
 
 # == Define misc:monitoring::view::udp2log
