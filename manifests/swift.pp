efa57f36a6042c2ee12f71ceb3251d39be453f57
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2014-09-01 14:55:37 +0200

    swift: check high load average on backend machines
    use load average as a proxy for an xfs bug we get into from time to time on
    backend machines.
    
    Note: this is a bandaid to at least have notification whenever this occurs, at
    the moment there's no such indication.
    
    RT #8249
    
    Change-Id: I4e1c263951060060d528299cdd04386ebc56ace3

--- manifests/swift.pp
+++ manifests/swift.pp
@@ -313,6 +313,10 @@ class swift::storage {
             }
         }
         include nrpe
+        nrpe::monitor_service { 'load_average':
+            description  => 'very high load average, likely xfs',
+            nrpe_command => '/usr/lib/nagios/plugins/check_load -w 80,80,80 -c 200,100,100',
+        }
 
         # RT-2593. Moved here from nrpe_local.cfg
         monitor_swift_daemon { [
