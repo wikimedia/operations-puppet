a30d987829ca86d87ff63f5f262084b6fd0aa966
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-09-01 18:09:34 +0200

    mediawiki: funnel all php requests through HHVM
    We want to serve all php requests through HHVM in beta (where we default
    to trusty now).
    
    So, we do the following:
    
    - Introduce a mediawiki::site wrapper that injects the hhvm-specific
      directives
    - Use ProxyPass and ProxyPassMatch for the most common requests as they
      are more performant (at least when we ditch prefork, they will be)
    - Use a RewriteRule inside a FilesMatch directive in order to process
      our rewrite rule *after* all general rewrites have been applied. Note
      that you can't use a Proxy directive inside a FilesMatch directive.
    
    Once all this is done, all requests will be correctly served through
    HHVM and not Zend. We'd also be able to switch off mod_php for saving resources.
    
    Change-Id: I959a16ce865bb2da38693f5d3c6825e6b4a359b0
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/mediawiki/manifests/web/site.pp
@@ -0,0 +1,32 @@
+# == Define: mediawiki::site
+#
+# Manages apache virtualhosts for mediawiki-specific configs.
+# This is a tiny wrapper around apache::site, of which it exposes most
+# parameters. It expects to be passed a template, and by default it
+# will precompute two variables:
+# - $hhvm_mw_proxy is a full configuration of the proxying for the
+# main URLs of the site. It should be included in any virtualhost
+# template for a mediawiki instance
+# - $hhvm_catchall is a catchall stanza that will ensure any .php or
+# .hh file will be routed to HHVM, and should be included in any
+# virtualhost used by a HAT server.
+#
+define mediawiki::site (
+    $ensure         = present,
+    $docroot        = '/var/www',
+    $hhvm_host_port = '127.0.0.1:9000',
+    $priority       = undef,
+    $content        = undef,
+    $replaces       = undef,
+    ) {
+
+    $hhvm_mw_proxy = template('mediawiki/apache/hhvm_mw_proxy.erb')
+    $hhvm_catchall = template('mediawiki/apache/hhvm_catchall.erb')
+
+    apache::site { "$title":
+        ensure   => $ensure,
+        priority => $priority,
+        content  => $content,
+        replaces => $replaces,
+    }
+}
