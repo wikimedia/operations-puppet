4768789d9ec288c3a4c3d72ffd1e0da527e9bc70
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-09-01 18:09:34 +0200

    mediawiki: funnel all php requests through HHVM
    We want to serve all php requests through HHVM in beta (where we default
    to trusty now).
    
    So, we do the following:
    
    - Introduce a mediawiki::site wrapper that injects the hhvm-specific
      directives
    - Use ProxyPass and ProxyPassMatch for the most common requests as they
      are more performant (at least when we ditch prefork, they will be)
    - Use a RewriteRule inside a FilesMatch directive in order to process
      our rewrite rule *after* all general rewrites have been applied. Note
      that you can't use a Proxy directive inside a FilesMatch directive.
    
    Once all this is done, we'd be able to switch off mod_php which should
    not run on those hosts.
    
    Change-Id: I959a16ce865bb2da38693f5d3c6825e6b4a359b0
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/mediawiki/manifests/web/site.pp
@@ -0,0 +1,33 @@
+# == Define: mediawiki::site
+#
+# Manages apache virtualhosts for mediawiki-specific configs.
+# This is a tiny wrapper around apache::site, of which it exposes most
+# parameters. It expects to be passed a template, and by default it
+# will precompute a variable $engine_include that can be used in the
+# template to include code that will allow to redirect traffic to HHVM
+# or to the traditional zend mod_php depending on the presence of one
+# variable.
+#
+define mediawiki::site (
+    $ensure         = present,
+    $doc_root       = '/var/www',
+    $hhvm_host_port = '127.0.0.1:9000',
+    $priority       = 50,
+    $content        = undef,
+    $replaces       = undef,
+    $hhvm_rewrite   = 'full',
+    ) {
+
+    $engine_include = $hhvm_rewrite ? {
+        'full'    => template('mediawiki/apache/hhvm_full.erb'),
+        'minimal' => template('mediawiki/apache/hhvm_minimal.erb'),
+        default   => fail('hhvm_rewrite must be either "full" or "minimal"'),
+    }
+
+    apache::site { "$title":
+        ensure   => $ensure,
+        priority => $priority,
+        content  => $content,
+        replaces => $replaces
+    }
+}
