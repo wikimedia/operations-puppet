4a604c834670410b14624c52d10c2d5760ee04d5
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-09-01 18:09:34 +0200

    mediawiki: funnel all php requests through HHVM
    We want to serve all php requests through HHVM in beta (where we default
    to trusty now).
    
    So, we do the following:
    
    - Introduce a mediawiki::site wrapper that injects the hhvm-specific
      directives
    - Use ProxyPass and ProxyPassMatch for the most common requests as they
      are more performant (at least when we ditch prefork, they will be)
    - Use a RewriteRule inside a FilesMatch directive in order to process
      our rewrite rule *after* all general rewrites have been applied. Note
      that you can't use a Proxy directive inside a FilesMatch directive.
    
    Once all this is done, all requests will be correctly served through
    HHVM and not Zend. We'd also be able to switch off mod_php for saving resources.
    
    Change-Id: I959a16ce865bb2da38693f5d3c6825e6b4a359b0
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/mediawiki/manifests/web/beta_sites.pp
+++ modules/mediawiki/manifests/web/beta_sites.pp
@@ -1,14 +1,15 @@
 class mediawiki::web::beta_sites {
     tag 'mediawiki', 'mw-apache-config'
+
     # Now the sites, in strict sequence
     apache::site { 'nonexistent':
         source   => 'puppet:///modules/mediawiki/apache/beta/sites/nonexistent.conf',
         priority => 0,
     }
 
-    apache::site { 'www.wikipedia':
-        source   => 'puppet:///modules/mediawiki/apache/beta/sites/www.wikipedia.conf',
-        priority => 1,
+    mediawiki::web::site { 'www.wikipedia':
+        content      => 'mediawiki/apache/beta/sites/www.wikipedia.conf.erb',
+        priority     => 1,
     }
 
     apache::site { 'main':
