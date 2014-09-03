1c4c386bb5e0605d88a3b1905f06d339247ae062
YuviPanda <yuvipanda@gmail.com>
2014-09-03 17:54:00 +0100

    icinga: Set default value for from in graphite threshold checks
    This is what the docs mention as default, just wasn't
    reflected in the code
    
    Change-Id: I2704e751bedfcf61b7d4ccca3e688d1440499691

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -422,7 +422,7 @@ define monitor_graphite_threshold(
     $metric,
     $warning,
     $critical,
-    $from,
+    $from                  = '-10m',
     $percentage            = 1,
     $under                 = false,
     $graphite_url          = 'http://graphite.wikimedia.org',
