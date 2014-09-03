e124236b5efc4ef648f7652e4a8c2da79d27f87a
dzahn <dzahn@wikimedia.org>
2014-09-03 09:56:41 -0700

    add missing misc_ulsfo hostgroup to Icinga
    same as Change-Id: Icdb57157557928c but for ulsfo
    
    Change-Id: Idca4853dc4a1d71a733ada404776833a61510fed

--- manifests/misc/icinga.pp
+++ manifests/misc/icinga.pp
@@ -921,5 +921,6 @@ class icinga::monitor::logrotate {
 @monitor_group { 'misc_pmtpa': description => 'pmtpa misc servers' }
 @monitor_group { 'misc_codfw': description => 'codfw misc servers' }
 @monitor_group { 'misc_esams': description => 'esams misc servers' }
+@monitor_group { 'misc_ulsfo': description => 'ulsfo misc servers' }
 # This needs to be consolited in the virt cluster probably
 @monitor_group { 'labsnfs_eqiad': description => 'eqiad labsnfs server servers' }
