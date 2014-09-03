c8585529458c0fb7511f513abe3be793ea47237a
dzahn <dzahn@wikimedia.org>
2014-09-03 09:37:19 -0700

    add missing misc_esams hostgroup to icinga
    we recently added all servers to misc hostgroups
    again. when i looked at icinga for an unrelated change
    i noticed icinga could not restart anymore, the reason:
    
    Error: Could not find any hostgroup matching 'misc_esams'
    
    so adding that so that Icinga can reload config again
    
    Change-Id: Icdb57157557928c5de02bd3c26e44605c098c706

--- manifests/misc/icinga.pp
+++ manifests/misc/icinga.pp
@@ -920,5 +920,6 @@ class icinga::monitor::logrotate {
 @monitor_group { 'misc_eqiad': description => 'eqiad misc servers' }
 @monitor_group { 'misc_pmtpa': description => 'pmtpa misc servers' }
 @monitor_group { 'misc_codfw': description => 'codfw misc servers' }
+@monitor_group { 'misc_esams': description => 'esams misc servers' }
 # This needs to be consolited in the virt cluster probably
 @monitor_group { 'labsnfs_eqiad': description => 'eqiad labsnfs server servers' }
