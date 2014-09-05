248ec63395d5f0f02afa3719c902a138b2555256
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-09-05 12:44:37 +0200

    puppet-compiler: alleviate disk space problems
    In order to solve the long-standing disk space problems in the
    puppet-compiler ci job, we do the following:
    
    - mount in bind the space-eating dirs from /mnt, where we have a large
      secondary disk.
    - set up a weekly cronjob that should clean up leftovers
    
    Change-Id: Iee31f83e958a856c46c7318cefa70781d8a5d2bb
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- manifests/role/puppet_compiler.pp
+++ manifests/role/puppet_compiler.pp
@@ -13,7 +13,6 @@ class role::puppet_compiler {
                 srange => '\$INTERNAL'
             }
         }
-        'production': { require role::ci::slave }
         default     : { fail("Realm ${::realm} NOT supported by this role.") }
     }
 
@@ -21,6 +20,44 @@ class role::puppet_compiler {
         ensure  => 'present',
         version => '0.3',
         user    => 'jenkins-deploy',
+    }
+
+    file { '/mnt/puppet-compiler-output':
+        ensure  => directory,
+        owner   => 'jenkins-deploy',
+        group   => 'jenkins-deploy',
+        require => $::role::ci::slave::labs::common::slash_mnt_require
+    }
+
+    mount { "${::puppet_compiler::program_dir}/output":
+        ensure  => mounted,
+        options => "bind",
+        device  => '/mnt/puppet-compiler-output'
+    }
+
+    file { '/mnt/puppet-compiler-external':
+        ensure  => directory,
+        owner   => 'jenkins-deploy',
+        group   => 'jenkins-deploy',
+        require => $::role::ci::slave::labs::common::slash_mnt_require
+    }
+
+    mount { "${::puppet_compiler::program_dir}/external/change":
+        ensure  => mounted,
+        options => "bind",
+        device  => '/mnt/puppet-compiler-external'
+    }
+
+    cron { 'check_compiler_stale_repositories':
+        command => '/usr/bin/find /mnt/puppet-compiler-external -mindepth 1 -maxdepth 1 -ctime +1 -type d | /usr/bin/xargs rm -rf',
+        user    => 'jenkins-deploy',
+        weekday => 0,
+    }
+
+    cron { 'check_compiler_stale_output':
+        command => '/usr/bin/find /mnt/puppet-compiler-output -mindepth 1 -maxdepth 1 -ctime +10 -type d | /usr/bin/xargs rm -rf',
+        user    => 'jenkins-deploy',
+        weekday => 0,
     }
 
 }
