51c797ce0f6a4d20622606d08aa067b087818eb5
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-09-08 19:21:21 +0300

    tcpircbot: add tin's IPv6 in ferm rules
    This was missing and tin was falling back to v4
    
    Change-Id: Ia0f1a4ab6386000d8d9b333a24077ce8d5b4a3ba

--- manifests/role/tcpircbot.pp
+++ manifests/role/tcpircbot.pp
@@ -23,6 +23,7 @@ class role::tcpircbot {
     }
 
     ferm::rule { 'tcpircbot_allowed':
-        rule => 'proto tcp dport 9200 { saddr (10.64.21.123 10.64.0.196 208.80.152.165 127.0.0.1) ACCEPT; }',
+        # Vanadium, tin(v4), fenari, localhost, tin (v6)
+        rule => 'proto tcp dport 9200 { saddr (10.64.21.123 10.64.0.196 208.80.152.165 127.0.0.1 2620:0:861:101:10:64:0:196) ACCEPT; }',
     }
 }
