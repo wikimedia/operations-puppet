79d0ec61a89542de01b6f3689adeb11aa6a43066
jeremyb <jeremy@tuxmachine.com>
2014-09-10 14:17:23 +0000

    new path for beta cluster CommonSettings.php
    Change-Id: I87ec2e96eeaa754ce135c9808cbf3374bd7dc811

--- modules/beta/files/LocalSettings.php
+++ modules/beta/files/LocalSettings.php
@@ -2,4 +2,4 @@
 # Managed by puppet. (modules/beta/files/LocalSettings.php)
 # WARNING: This file is publically viewable on the web. Do not put private
 # data here.
-require_once '/usr/local/apache/common/wmf-config/CommonSettings.php';
+require_once '/a/common/wmf-config/CommonSettings.php';
