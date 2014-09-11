b8ff38b41dba51824946ec9021783eb1f95cdef2
cpettet <rush@wikimedia.org>
2014-09-11 15:04:27 -0500

    WIP
    *.wmfusercontent.org ssl termination on web-misc cluster
    
    Change-Id: Ie6885749a836a0eb16cc1fea0bad35ae4c237fa4

--- modules/protoproxy/manifests/localssl.pp
+++ modules/protoproxy/manifests/localssl.pp
@@ -8,14 +8,29 @@
 # [*upstream_port*]
 #   TCP port to proxy to. Defaults to '80'
 #
+# [*server_name*]
+#   Set servername if not fqdn
+#
+# [*sni_default*]
+#   Boolean indicating if this is the SNI default cert
+#
 
 define protoproxy::localssl(
     $proxy_server_cert_name,
     $enabled       = true,
-    $upstream_port = '80'
+    $upstream_port = '80',
+    $server_name   = ''
+    $sni_default   = false,
 ) {
+    if empty($server_name) {
+        $site_name = $server_name
+    } else {
+        $site_name = $::fqdn
+    }
+
     nginx::site { 'localssl':
         content => template('protoproxy/localssl.erb'),
         enabled => $enabled,
     }
+
 }
