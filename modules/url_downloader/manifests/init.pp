d76d28471a6fb05b24b8cd991b4582ba4a06e8d3
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-09-11 18:10:52 +0300

    module/role for url-downloader
    url-downloader had already a module but it was not in the module/role
    pattern. Split that up and add monitoring and firewall while at it. Also
    add support for Ubuntu >= 12.04 since the squid package has undergone
    some changes. This duplicates a lot of stuff in install server and as
    such a deduplication module is probably warranted. RSpec as well as unit
    tests are included, the module and the role class are documented and
    minimal documentation in the form of a README file is provided
    
    Change-Id: Ia234bdd7109d9419778e87b6dc8d56abc0763fe7

---
+++ modules/url_downloader/manifests/init.pp
@@ -0,0 +1,55 @@
+# Class: url_downloader
+#
+# This class installs squid as a forward proxy for fetching URLs
+#
+# Parameters:
+#   $service_ip
+#       The IP on which the proxy listens on and uses to fetch URLs
+#
+# Actions:
+#       Install squid and configure it as a forward fetching proxy
+#
+# Requires:
+#
+# Sample Usage:
+#       class { '::url_downloader':
+#           service_ip  => '10.10.10.10' # Probably a public ip though
+#       }
+class url_downloader($service_ip) {
+    if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '12.04') >= 0 {
+        $package_name = 'squid3'
+    } else {
+        $package_name = 'squid'
+    }
+
+    $confdir = "/etc/${package_name}"
+    $service_name = $package_name
+
+    file { "${confdir}/squid.conf":
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template("url_downloader/${package_name}.conf.erb"),
+    }
+
+    file { "/etc/logrotate.d/${package_name}":
+        ensure  => present,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        source  => "puppet:///modules/url_downloader/${package_name}-logrotate",
+    }
+
+    package { $package_name:
+        ensure => installed,
+    }
+
+    service { $service_name:
+        ensure => running,
+    }
+
+    Package[$package_name] -> Service[$service_name]
+    Package[$package_name] -> File["/etc/logrotate.d/${package_name}"]
+    Package[$package_name] -> File["${confdir}/squid.conf"]
+    File["${confdir}/squid.conf"] ~> Service[$service_name] # also notify
+}
