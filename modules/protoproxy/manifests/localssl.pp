e7593c0cca422e55fd81a980b2700be32c723afb
Mark Bergsma <mark@wikimedia.org>
2014-09-12 19:26:49 +0200

    Add protoproxy::localssl default_server and server_name parameters
    default_server allows setting this server as the default server,
    e.g. as fallback for SNI.
    server_name allows a custom server name to be set, e.g. used
    for SNI server selection.
    
    Change-Id: Ia2115f2540b261d56221c60fd9b245ee732a832b

--- modules/protoproxy/manifests/localssl.pp
+++ modules/protoproxy/manifests/localssl.pp
@@ -3,16 +3,26 @@
 # This definition creates a SSL proxy to localhost, using an Nginx site.
 #
 # === Parameters:
+# [*server_name*]
+#   Server name, used e.g. for SNI. Defaults to $::fqdn
+#
 # [*proxy_server_cert_name*]
 #
 # [*upstream_port*]
 #   TCP port to proxy to. Defaults to '80'
 #
+# [*default_server*]
+#   Boolean. Adds the 'default_server' option to the listen statement.
+#
+# [*enabled*]
+#   Boolean. Whether the site is enabled in the nginx sites-enabled directory.
 
 define protoproxy::localssl(
+    $server_name    = $::fqdn,
     $proxy_server_cert_name,
-    $enabled       = true,
-    $upstream_port = '80'
+    $default_server = false,
+    $enabled        = true,
+    $upstream_port  = '80'
 ) {
     # TEMP migration
     nginx::site { 'localssl':
