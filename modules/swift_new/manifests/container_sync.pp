b944dc00d49a2908f61f8981584ee954a1ccf183
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2014-09-12 11:36:20 +0200

    swift: add container sync
    The basic concepts of container synchronization are outlined here:
    http://docs.openstack.org/developer/swift/overview_container_sync.html
    
    This part takes care of enabling container sync in the proxy pipeline and
    distribute the credentials to the proxy and container servers.
    
    A separate utility will take care of enabling synchronization on a
    per-container basis.
    
    Change-Id: Ie771a29aa5e99757301f5f7a6ffafda7528e8f55

---
+++ modules/swift_new/manifests/container_sync.pp
@@ -0,0 +1,14 @@
+class swift_new::container_sync (
+  $replication_accounts = $::swift_new::params::replication_accounts,
+  $replication_keys = $::swift_new::params::replication_keys,
+) {
+    $accounts = merge($replication_accounts, $replication_keys)
+
+    file { '/etc/swift/container-sync-realms.conf':
+        ensure  => present,
+        mode    => 0440,
+        owner   => 'swift',
+        group   => 'swift',
+        content => template('swift_new/container-sync-realms.conf.erb'),
+    }
+}
