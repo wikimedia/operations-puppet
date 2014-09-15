571cc1c816e11577d43fe94b46989a31e03e5ddd
Andrew Otto <acotto@gmail.com>
2014-09-15 11:17:26 -0400

    Use $labs_finger as default $master finger in salt role
    This fixes an error in labs where the pick() function failed
    puppet runs:
    
    Sep 15 15:05:39 wikimetrics-staging1 puppet-agent[25708]: Could not retrieve catalog from remote server: Error 400 on SERVER: Must provide non empty value. on node wikimetrics-staging1.eqiad.wmflabs
    
    Change-Id: Id425238a0bd48bffec054c29b30b9019a785789b

--- manifests/role/salt.pp
+++ manifests/role/salt.pp
@@ -93,7 +93,7 @@ class role::salt::minions {
         $labs_masters  = [ 'virt1000.wikimedia.org', 'virt0.wikimedia.org' ]
         $labs_finger   = 'c5:b1:35:45:3e:0a:19:70:aa:5f:3a:cf:bf:a0:61:dd'
         $master        = pick($::salt_master_override, $labs_masters)
-        $master_finger = pick($::salt_master_finger_override, $master_finger)
+        $master_finger = pick($::salt_master_finger_override, $labs_finger)
         $client_id     = "${::ec2id}.${::domain}"
 
         salt::grain { 'instanceproject':
