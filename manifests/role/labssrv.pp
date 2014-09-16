d68315e2b31e4de492adc264d4ccfbac4c435a5e
Andrew Otto <acotto@gmail.com>
2014-09-16 12:45:58 -0400

    Make role::labs::lvm::srv volume size configurable, move make-instance-vol file into labs_lvm base class
    This allows for other uses of the disk space than just for /srv.
    
    Also, in order to use labs_lvm::volume more than once, the make-instance-vol
    must be installed by the module base class, otherwise one will get
    Duplicate declaration: File[/usr/local/sbin/make-instance-vol] errors.
    
    Change-Id: Id042f61127c78f8a80a90c37ba47eb466288a000

--- manifests/role/labssrv.pp
+++ manifests/role/labssrv.pp
@@ -1,5 +1,17 @@
 # Allocate all of the instance's extra space as /srv
 class role::labs::lvm::srv {
     include labs_lvm
-    labs_lvm::volume { 'second-local-disk': mountat => '/srv' }
+
+    # Allow this to be configuable via a global
+    # $::srv_logical_volume_size variable.
+    # Default to using 100% of the vg space.
+    $size = $::srv_logical_volume_size ? {
+        undef   => '100%FREE',
+        default => $::srv_logical_volume_size,
+    }
+
+    labs_lvm::volume { 'second-local-disk':
+        mountat => '/srv',
+        size    => $size,
+    }
 }
