99a6a1145b4e355e5e13efb73babf9a8d133664b
Brandon Black <bblack@wikimedia.org>
2014-09-17 22:41:08 +0000

    use regsubst instead of inline_template
    (Works in "puppet apply" testing, just like the
    last variant that failed in the real world...)
    
    Change-Id: Id11f3df4d0f64545f1df1e9642f4d7a41345483f

--- modules/interface/manifests/tagged.pp
+++ modules/interface/manifests/tagged.pp
@@ -33,8 +33,9 @@ define interface::tagged($base_interface, $vlan_id, $address=undef, $netmask=und
     }
 
     if $v6_token {
-        $v6_token_addr = inline_template("::<%= @address.gsub('.', ':') %>")
-        $v6_token_cmd = "set iface[. = '${intf}']/up /sbin/ip token set '${v6_token_addr}' dev '${intf}'"
+        $v6_token_lower64 = regsubst($address, '\.', ':', 'G')
+        $v6_token_addr = "::${v6_token_lower64}"
+        $v6_token_cmd = "set iface[. = '${intf}']/up /sbin/ip token set ${v6_token_addr} dev ${intf}"
     } else {
         $v6_token_cmd = ''
     }
