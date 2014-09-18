556b37689d746d4c8ddb32718c1b29a9ff3eca0a
YuviPanda <yuvipanda@gmail.com>
2014-09-18 14:38:16 +0100

    Initial shinken setup for labs
    - Sets up a simple server with basic webui and
      a guest account
    - Sets up contactgroups for toollabs & betacluster
    - No autogeneration of hosts yet, modules/roles
      will have to declare them using cfg files
    - Resource collection on labs will have security
      issues, so different method of doing this will
      have to be found
    
    Change-Id: I626438cf194db2e2816aa8cf4a63cdcf64f0154b

---
+++ manifests/role/labsshinken.pp
@@ -0,0 +1,8 @@
+# = Class: role::labs::shinken
+# Sets up a shinken server for labs
+
+class role::labs::shinken {
+    class { 'shinken::server':
+        auth_secret => 'This is insecure, should switch to using private repo',
+    }
+}
