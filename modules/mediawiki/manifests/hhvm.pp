dd89bf206092b19c9cd25d182feb7f0e3ce175dd
Ori Livneh <ori@wikimedia.org>
2014-09-17 20:56:37 -0700

    mediawiki::hhvm: add additional documentation
    Change-Id: I5755df7205059e6a851189e2a63b37afa84dff8e

--- modules/mediawiki/manifests/hhvm.pp
+++ modules/mediawiki/manifests/hhvm.pp
@@ -8,6 +8,7 @@ class mediawiki::hhvm {
     include ::hhvm::monitoring
     include ::mediawiki::users
 
+
     class { '::hhvm':
         user          => 'apache',
         group         => 'apache',
@@ -19,6 +20,11 @@ class mediawiki::hhvm {
             },
         },
     }
+
+
+    # Use Debian's Alternatives system to mark HHVM as the default PHP
+    # implementation for this system. This makes /usr/bin/php a symlink
+    # to /usr/bin/hhvm.
 
     alternatives::select { 'php':
         path    => '/usr/bin/hhvm',
