afcd0d18c21201cbd13fcffdfd6be2162ec30d25
YuviPanda <yuvipanda@gmail.com>
2014-09-18 14:38:16 +0100

    Initial shinken setup for labs
    - Sets up a simple server with basic webui and
      a guest account
    - Sets up contactgroups for toollabs & betacluster
    - No autogeneration of hosts yet, modules/roles
      will have to declare them using cfg files
    - Resource collection on labs will have security
      issues, so different method of doing this will
      have to be found
    
    Change-Id: I626438cf194db2e2816aa8cf4a63cdcf64f0154b

---
+++ modules/shinken/manifests/server.pp
@@ -0,0 +1,84 @@
+# = Class: shinken::server
+# Sets up a shinken monitoring server
+
+class shinken::server(
+    $auth_secret
+) {
+    package { 'shinken':
+        ensure  => present,
+    }
+
+    service { 'shinken':
+        ensure => running,
+    }
+
+    file { '/etc/shinken/modules':
+        ensure  => directory,
+        owner   => 'shinken',
+        group   => 'shinken',
+        require => Package['shinken'],
+    }
+
+    file { '/etc/shinken/modules/webui.cfg':
+        ensure  => present,
+        content => template('shinken/webui.cfg.erb'),
+        owner   => 'shinken',
+        group   => 'shinken',
+        require => File['/etc/shinken/modules'],
+        notify  => Service['shinken'],
+    }
+
+    file { '/etc/shinken/shinken.cfg':
+        ensure  => present,
+        source  => 'puppet:///modules/shinken/shinken.cfg',
+        owner   => 'shinken',
+        group   => 'shinken',
+        require => Package['shinken'],
+        notify  => Service['shinken'],
+    }
+
+    file { '/etc/shinken/contacts.cfg':
+        ensure  => present,
+        source  => 'puppet:///modules/shinken/contacts.cfg',
+        owner   => 'shinken',
+        group   => 'shinken',
+        require => Package['shinken'],
+        notify  => Service['shinken'],
+    }
+
+    file { '/etc/shinken/contactgroups.cfg':
+        ensure  => present,
+        source  => 'puppet:///modules/shinken/contactgroups.cfg',
+        owner   => 'shinken',
+        group   => 'shinken',
+        require => Package['shinken'],
+        notify  => Service['shinken'],
+    }
+
+    # Default localhost config, we do not need this
+    file { '/etc/shinken/hosts/localhost.cfg':
+        ensure  => absent,
+        require => Package['shinken'],
+        notify  => Service['shinken'],
+    }
+}
+
+# = Class: shinken::hosts
+# Setup a shinken hosts definition file
+# FIXME: Autogenerate hosts definitions later on
+class shinken::hosts(
+    $name,
+    $ensure  = present,
+    $source  = undef,
+    $content = undef
+) {
+    file { "/etc/shinken/hosts/$name.cfg":
+        ensure  => ensure,
+        source  => source,
+        content => content,
+        owner   => 'shinken',
+        group   => 'shinken',
+        notify  => Service['shinken'],
+        require => Package['shinken']
+    }
+}
