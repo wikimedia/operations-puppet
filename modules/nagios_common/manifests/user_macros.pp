cfbc7daa08225c90ad3a18aeff90845e138fe2be
YuviPanda <yuvipanda@gmail.com>
2014-09-19 12:10:42 +0100

    nagios_common: Extract user_definitions from icinga
    Change-Id: I00691021a634a8568cc0485e48dbbdb8c59f78fe

---
+++ modules/nagios_common/manifests/user_macros.pp
@@ -0,0 +1,33 @@
+# = Class: nagios_common::user_macros
+# Defines $USERn$ macros for nagios compatible implementations
+# === Parameters
+# [*ensure*]
+#   present or absent, to make the definition
+#   present or absent. Defaults to present
+#
+# [*config_dir*]
+#   The base directory to put configuration in.
+#   Defaults to '/etc/icinga/'
+#
+# [*owner*]
+#   The user which should own the config file.
+#   Defaults to 'root'
+#
+# [*group*]
+#   The group which should own the config file.
+#   Defaults to 'root'
+#
+class nagios_common::user_macros(
+    $ensure = present,
+    $config_dir = '/etc/icinga/',
+    $owner = 'root',
+    $group = 'root',
+){
+    file { "$config_dir/resource.cfg":
+        ensure => $ensure,
+        source => 'puppet:///modules/nagios_common/resource.cfg',
+        owner  => $owner,
+        group  => $group,
+        mode   => '0644',
+    }
+}
