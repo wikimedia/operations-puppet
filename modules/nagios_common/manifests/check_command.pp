c5ab6d6df3d628ec7a11a0c4dff47242c2fc483b
YuviPanda <yuvipanda@gmail.com>
2014-09-19 17:24:06 +0100

    nagios_common: Refactor custom command definitions
    - Setup a check_command define
    - Separate commands config into one file per set of commands
    - Move command config into a folder /etc/icinga/commands
    - Move check_graphite to use the new system
    - Setup a class that'll house all check_commands
    - Setup custom checks in shinken as well
    
    TODO:
    
    - Need to find a way to specify a template as well. See
      PS6 of this change for issues with that
    
    Change-Id: I3c3bc48bf8730cd262ec32785ee6a7360f7e24aa

---
+++ modules/nagios_common/manifests/check_command.pp
@@ -0,0 +1,52 @@
+# = Define: nagios_common::check_command
+# Defines a custom check command and configuration for that
+# command
+
+# [*ensure*]
+#   present or absent, to make the definition
+#   present or absent. Defaults to present
+#
+# [*config_dir*]
+#   The base directory to put configuration in.
+#   Defaults to '/etc/icinga/'
+#
+# [*plugin_source*]
+#   Path to source for the executable plugin file.
+#   Defaults to puppet:///modules/nagios_common/check_commands/$title
+#
+# [*plugin_source*]
+#   Path to source for the plugin configuration.
+#   Defaults to puppet:///modules/nagios_common/check_commands/$title.cfg
+#
+# [*owner*]
+#   The user which should own the config file.
+#   Defaults to 'root'
+#
+# [*group*]
+#   The group which should own the config file.
+#   Defaults to 'root'
+#
+define nagios_common::check_command(
+    $ensure = present,
+    $config_dir = '/etc/icinga',
+    $plugin_source = "puppet:///modules/nagios_common/check_commands/$title",
+    $config_source = "puppet:///modules/nagios_common/check_commands/$title.cfg",
+    $owner = 'root',
+    $group = 'root',
+) {
+
+    file { "/usr/lib/nagios/plugins/$title":
+        ensure => $ensure,
+        source => $plugin_source,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    file { "$config_dir/commands/$title.cfg":
+        ensure => $ensure,
+        source => $config_source,
+        owner  => $owner,
+        group  => $group,
+    }
+}
