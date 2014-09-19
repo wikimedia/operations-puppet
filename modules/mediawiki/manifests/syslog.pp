2b6f7fb4e74d52b8f80ecaa051dff447d5ba1f52
Ori Livneh <ori@wikimedia.org>
2014-09-19 10:31:11 -0700

    mediawiki::syslog: add docs
    And touch-up the docs for mediawiki::cgroup
    
    Change-Id: I0510d38714e43c32e7ccbfbd4e637d14d655c562

--- modules/mediawiki/manifests/syslog.pp
+++ modules/mediawiki/manifests/syslog.pp
@@ -1,8 +1,26 @@
+# == Class: mediawiki::syslog
+#
+# Hosts running MediaWiki configure Apache and HHVM to log to syslog.
+# This Puppet class configures rsyslog to handle log messages from
+# Apache and HHVM by writing them to disk and forwarding them to the
+# MediaWiki log aggregator via UDP. It also sets up log rotation for the
+# local log files.
+#
 class mediawiki::syslog {
+
+    # We assign a priority of 40 to MediaWiki's rsyslog config file
+    # so we can intercept log messages before they fall through to
+    # the firehose in 50-default.conf.
+
     rsyslog::conf { 'mediawiki':
         source   => 'puppet:///modules/mediawiki/rsyslog.conf',
-        priority => 40,  # before 50-default.conf
+        priority => 40,
     }
+
+
+    # Set up log rotation for /var/log/apache2.log. In addition to
+    # cron-triggered rotation, rsyslog will invoke logrotate whenever
+    # apache2.log exceeds 100MB.
 
     file { '/etc/logrotate.d/mediawiki_apache':
         source => 'puppet:///modules/mediawiki/logrotate.d_mediawiki_apache',
