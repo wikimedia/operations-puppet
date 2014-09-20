39ff6d4b80ac1d55995085a1bbe3a6309dd45a98
dzahn <dzahn@wikimedia.org>
2014-09-19 17:11:10 -0700

    icinga - fix syntax error for check_ssl_ldap
    follow-up to I52c19555706fe85b
    
    fix syntax error in icinga config due to missing
    ! character leading to stuff like:
    
    Error: Service check command 'check_ssl_ldaplabcontrol2001.wikimedia.org' specified
    ... not defined anywhere
    
    Change-Id: I472d93de59585c71d8861c0a0482ef476bbe425b

--- modules/ldap/manifests/server.pp
+++ modules/ldap/manifests/server.pp
@@ -217,7 +217,7 @@ set-administration-connector-prop --set key-manager-provider:PKCS12 --set ssl-ce
     # TODO: make this critical (paging) again once confirmed fixed
     monitor_service { 'ldap cert':
         description   => 'Certificate expiration',
-        check_command => "check_ssl_ldap${fqdn}!${ca_name}",
+        check_command => "check_ssl_ldap!${fqdn}!${ca_name}",
         critical      => false
     }
 
