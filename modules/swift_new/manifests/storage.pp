fa9fab7bbd063cbea45996c8300380bb56d49e99
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2014-09-22 10:08:11 +0100

    swift: refactor into module, add codfw
    The new module is aptly named not swift but swift_new, it is transitional only
    however. Initially only codfw is on swift_new and other clusters will be
    migrated to the new module once codfw is up and running. At that point the
    module can be renamed into 'swift' and the legacy removed.
    
    Change-Id: I265b586fb0e54cf1f0e5c1463813986f42b1dd12
    (cherry picked from commit 758a6f81594e97b6592249bda10698a49602d6f3)

---
+++ modules/swift_new/manifests/storage.pp
@@ -0,0 +1,111 @@
+class swift_new::storage {
+    package {
+        [ 'swift-account',
+          'swift-container',
+          'swift-object',
+    ]:
+        ensure => 'present',
+    }
+
+    class { 'rsync::server':
+        log_file => '/var/log/rsyncd.log',
+    }
+
+    rsync::server::module {
+    'account':
+        uid             => 'swift',
+        gid             => 'swift',
+        max_connections => '5',
+        path            => '/srv/swift-storage/',
+        read_only       => 'no',
+        lock_file       => '/var/lock/account.lock';
+    'container':
+        uid             => 'swift',
+        gid             => 'swift',
+        max_connections => '5',
+        path            => '/srv/swift-storage/',
+        read_only       => 'no',
+        lock_file       => '/var/lock/container.lock';
+    'object':
+        uid             => 'swift',
+        gid             => 'swift',
+        max_connections => '10',
+        path            => '/srv/swift-storage/',
+        read_only       => 'no',
+        lock_file       => '/var/lock/object.lock',
+    }
+
+    # set up swift specific configs
+    File {
+        owner => 'swift',
+        group => 'swift',
+        mode  => '0440',
+    }
+
+    file { '/etc/swift/account-server.conf':
+        content => template('swift_new/account-server.conf.erb'),
+    }
+
+    file { '/etc/swift/container-server.conf':
+        content => template('swift_new/container-server.conf.erb'),
+    }
+
+    file { '/etc/swift/object-server.conf':
+        content => template('swift_new/object-server.conf.erb'),
+    }
+
+    file { '/srv/swift-storage':
+        ensure  => 'directory',
+        require => Package['swift'],
+        owner   => 'swift',
+        group   => 'swift',
+        # the 1 is to allow nagios to read the drives for check_disk
+        mode    => '0751',
+    }
+
+    service { [
+        'swift-account',
+        'swift-account-auditor',
+        'swift-account-reaper',
+        'swift-account-replicator',
+        'swift-container',
+        'swift-container-auditor',
+        'swift-container-replicator',
+        'swift-container-updater',
+        'swift-object',
+        'swift-object-auditor',
+        'swift-object-replicator',
+        'swift-object-updater',
+    ]:
+        ensure => 'running',
+    }
+
+
+    # install swift-drive-audit as a cronjob;
+    # it checks the disks every 60 minutes
+    # and unmounts failed disks. It logs its actions to /var/log/syslog.
+
+    # this file comes from the python-swift package
+    # but there are local improvements
+    # that are not yet merged upstream.
+    file { '/usr/bin/swift-drive-audit':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        source => 'puppet:///modules/swift_new/swift-drive-audit',
+    }
+    file { '/etc/swift/swift-drive-audit.conf':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0440',
+        source => 'puppet:///modules/swift_new/swift-drive-audit.conf',
+    }
+    cron { 'swift-drive-audit':
+        ensure  => 'present',
+        command => '/usr/bin/swift-drive-audit /etc/swift/swift-drive-audit.conf',
+        user    => 'root',
+        minute  => '1',
+        require => [File['/usr/bin/swift-drive-audit'],
+                    File['/etc/swift/swift-drive-audit.conf']],
+    }
+}
