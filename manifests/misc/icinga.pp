6fbab75df8dbc624e758a7a2b72db2213e6f52f1
YuviPanda <yuvipanda@gmail.com>
2014-09-23 15:33:25 +0100

    nagios_common: Move real into module
    Change-Id: I5fd02f1d5e290a3bea475cc6c9cf7ee6a366f032

--- manifests/misc/icinga.pp
+++ manifests/misc/icinga.pp
@@ -507,12 +507,6 @@ class icinga::monitor::files::nagios-plugins {
         group  => 'root',
         mode   => '0644',
     }
-    file { '/etc/nagios-plugins/config/real.cfg':
-        source => 'puppet:///files/icinga/plugin-config/real.cfg',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0644',
-    }
     file { '/etc/nagios-plugins/config/rpc-nfs.cfg':
         source => 'puppet:///files/icinga/plugin-config/rpc-nfs.cfg',
         owner  => 'root',
