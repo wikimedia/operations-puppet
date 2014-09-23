66e1dff26bff4f0a099c1e035298ad5d9da70b96
YuviPanda <yuvipanda@gmail.com>
2014-09-23 15:49:41 +0100

    nagios_common: move news into module
    Change-Id: Ifb50318779ec88bec8f581a8aa2bf113936ae174

--- manifests/misc/icinga.pp
+++ manifests/misc/icinga.pp
@@ -357,12 +357,6 @@ class icinga::monitor::files::nagios-plugins {
         group  => 'root',
         mode   => '0755',
     }
-    file { '/etc/nagios-plugins/config/news.cfg':
-        source => 'puppet:///files/icinga/plugin-config/news.cfg',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0644',
-    }
     file { '/etc/nagios-plugins/config/nt.cfg':
         source => 'puppet:///files/icinga/plugin-config/nt.cfg',
         owner  => 'root',
