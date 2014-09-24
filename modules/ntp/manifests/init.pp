35b7c01f66f044546f5ef17222edce8b25a7f094
Brandon Black <bblack@wikimedia.org>
2014-09-24 16:20:58 +0000

    NTP config refactoring + updates
    Puppet-level stuff:
    * Replaces the former classes ntp::client and ntp::server
      with a single define named 'ntp' that covers both cases
      equally via configfile template variables.
    * Adds class role::ntp (to be included on virtually every
      node via base/standard), which includes our full NTP
      layout/configuration for WMF and sorts out the server
      and client variables within itself.
    
    NTP-level stuff:
    * Our new NTP servers are the 5x recdns servers (2x eqiad,
      2x codfw, 1x esams).
    * All our NTP servers peer with each other, and upstream to
      the public NTP pool using a local zone (europe or us).
    * All our regular clients use the internal servers from
      2x datacenters as their servers, and do not peer.
    * Access controls tightened/fixed.
    
    XXX - Work-in-progress, probably broken horribly at this stage!
    
    Change-Id: I797235f7605d274ef26f922f6e48546b0f1e6082

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -1,35 +1,17 @@
-# NTP base class
+define ntp($servers=[], $peers=[], $query_acl=[], $time_acl=[]) {
+    package { 'ntp': ensure => latest }
 
-class ntp {
-  case $::operatingsystem {
-    debian, ubuntu: {
-      $conf    = '/etc/ntp.conf'
-      $package = 'ntp'
+    file { 'ntp.conf':
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        path    => '/etc/ntp.conf',
+        content => template('ntp/ntp-server.erb'),
     }
-    solaris: {
-      $conf    = '/etc/inet/ntp.conf'
-      $package = [ SUNWntpr, SUNWntpu ]
+
+    service { 'ntp':
+        ensure    => running,
+        require   => [ File['ntp.conf'], Package['ntp'] ],
+        subscribe => File['ntp.conf'],
     }
-    default: {
-      fail("Module ${module_name} is not supported on ${::operatingsystem}")
-    }
-  }
-
-  file { 'ntp.conf':
-    mode    => '0644',
-    owner   => root,
-    group   => root,
-    path    => $conf,
-    content => template('ntp/ntp-server.erb'),
-  }
-
-  package { $package:
-    ensure => latest,
-  }
-
-  service { 'ntp':
-    ensure    => running,
-    require   => [ File['ntp.conf'], Package[$package] ],
-    subscribe => File['ntp.conf'],
-  }
 }
