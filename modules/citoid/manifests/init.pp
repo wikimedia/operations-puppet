2732486673595de285d0c9a061e62b343ce20eec
Roan Kattouw <roan.kattouw@gmail.com>
2014-09-24 18:19:57 -0700

    Citoid puppetization
    Mostly copied from the Mathoid puppetization, with
    some improvements suggested by Bryan.
    
    Change-Id: Ib24047737e94230dd3af529f67f4cbacdeea8b4d

---
+++ modules/citoid/manifests/init.pp
@@ -0,0 +1,60 @@
+# == Class: citoid
+#
+# citoid is a node.js backend for citation lookups.
+#
+# === Parameters
+#
+# [*port*]
+#   Port where to run the citoid service. Defaults to 1970.
+#
+class citoid( $port = 1970 ) {
+    ensure_packages( ['nodejs'] )
+
+    package { 'citoid':
+        provider => 'trebuchet',
+    }
+
+    group { 'citoid':
+        ensure => present,
+        name   => 'citoid',
+        system => true,
+    }
+
+    user { 'citoid':
+        gid    => 'citoid',
+        home   => '/nonexistent',
+        shell  => '/bin/false',
+        system => true,
+        before => Service['citoid'],
+    }
+
+    file { '/var/log/citoid':
+        ensure => directory,
+        owner  => 'citoid',
+        group  => 'citoid',
+        mode   => '0775',
+        before => Service['citoid'],
+    }
+
+    file { '/etc/init/citoid.conf':
+        content => template('citoid/upstart.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['citoid'],
+    }
+
+    file { '/etc/logrotate.d/citoid':
+        content => template('citoid/logrotate.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+
+    service { 'citoid':
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        provider   => 'upstart',
+    }
+}
