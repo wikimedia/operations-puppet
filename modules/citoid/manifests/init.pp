568f09e0a23fd58254373f40700daf21fd8ffdfd
Roan Kattouw <roan.kattouw@gmail.com>
2014-09-24 18:19:57 -0700

    WIP Citoid puppetization
    Doesn't have a deploy repo yet.
    
    Mostly copied from the Mathoid puppetization, with
    some improvements suggested by Bryan.
    
    Change-Id: Ib24047737e94230dd3af529f67f4cbacdeea8b4d

---
+++ modules/citoid/manifests/init.pp
@@ -0,0 +1,61 @@
+# == Class: citoid
+#
+# citoid is a node.js backend for citation lookups.
+#
+# === Parameters
+#
+# [*base_path*]
+#   Path to the citoid code.
+# [*node_path*]
+#   Path to the node modules citoid depends on.
+# [*log_dir*]
+#   Place where citoid can put log files. Assumed to be already existing and
+#   have write access to citoid user.
+# [*port*]
+#   Port where to run the citoid service. Defaults to 1970.
+#
+class citoid(
+    $base_path,
+    $node_path,
+    $log_dir,
+    $port=1970
+) {
+    package { ['nodejs']:
+        ensure => present,
+    }
+
+    $log_file = "${log_dir}/main.log"
+
+    file { $log_dir:
+        ensure => directory,
+        owner  => citoid,
+        group  => citoid,
+        mode   => '0775',
+    }
+
+    # The upstart configuration
+    file { '/etc/init/citoid.conf':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0444',
+        content => template('citoid/upstart.erb'),
+    }
+
+    file { '/etc/logrotate.d/citoid':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0444',
+        content => template('citoid/logrotate.erb'),
+    }
+
+    service { 'citoid':
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        provider   => 'upstart',
+        require    => File[$log_dir],
+        subscribe  => File['/etc/init/citoid.conf'],
+    }
+}
