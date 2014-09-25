49b324973fd4f910d81c3db02a391b36d5adb3e3
YuviPanda <yuvipanda@gmail.com>
2014-09-25 14:27:08 +0100

    icinga: Move wikidata monitoring into module
    Change-Id: I001f9356ca565b54d5523204610c150ed9196a6f

--- manifests/misc/icinga.pp
+++ manifests/misc/icinga.pp
@@ -443,22 +443,6 @@ class icinga::ganglia::ganglios {
 
 }
 
-class icinga::monitor::wikidata {
-
-    @monitor_host { 'wikidata':
-        ip_address => '91.198.174.192',
-    }
-
-    monitor_service { 'wikidata.org dispatch lag':
-        description   => 'check if wikidata.org dispatch lag is higher than 2 minutes',
-        check_command => 'check_wikidata',
-        host          => 'wikidata',
-        normal_check_interval => 30,
-        retry_check_interval => 5,
-        contact_group => 'admins,wikidata',
-    }
-}
-
 # == Class icinga::ganglia::check
 #
 # Installs check_ganglia package and sets up symlink into
