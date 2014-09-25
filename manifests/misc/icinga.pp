a3a2333fc441f01db9407c855d4690b9fb29f670
YuviPanda <yuvipanda@gmail.com>
2014-09-25 19:48:08 +0100

    icinga: Move global monitoring hostgroups into module
    Change-Id: Ia7444d761a1de12734d16543ff707ac06833b191

--- manifests/misc/icinga.pp
+++ manifests/misc/icinga.pp
@@ -19,6 +19,7 @@ class icinga::monitor {
     include icinga::monitor::service
     include icinga::monitor::wikidata
     include icinga::user
+    include icinga::global_hostgroups
     include lvs::monitor
     include misc::dsh::files
     include mysql
@@ -371,13 +372,3 @@ class icinga::ganglia::check {
         require => Package['check-ganglia'],
     }
 }
-
-# global monitoring groups - formerly misc/nagios.pp
-
-@monitor_group { 'misc_eqiad': description => 'eqiad misc servers' }
-@monitor_group { 'misc_pmtpa': description => 'pmtpa misc servers' }
-@monitor_group { 'misc_codfw': description => 'codfw misc servers' }
-@monitor_group { 'misc_esams': description => 'esams misc servers' }
-@monitor_group { 'misc_ulsfo': description => 'ulsfo misc servers' }
-# This needs to be consolited in the virt cluster probably
-@monitor_group { 'labsnfs_eqiad': description => 'eqiad labsnfs server servers' }
