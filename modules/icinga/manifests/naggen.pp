4751f04fb78758f726246f1bd64a3407102e0147
YuviPanda <yuvipanda@gmail.com>
2014-09-25 19:06:59 +0100

    icinga: Move naggen into module
    - Setup service notification in the file {} definitions,
      and setup a dependency to make sure it executes in order.
      This is more consistent than using subscribes
    - Remove the 'permissions fix' code, since all the files
      seem to be generated with appropriate permissions anyway
    
    Change-Id: I8536f78ca28447cfbd8acac507758eb1ee33092a

---
+++ modules/icinga/manifests/naggen.pp
@@ -0,0 +1,43 @@
+# = Class: icinga::naggen
+#
+# Runs naggen2 to generate hosts, service and hostext config
+# from exported puppet resources
+class icinga::naggen {
+
+    file { '/etc/icinga/puppet_hosts.cfg':
+        content => generate('/usr/local/bin/naggen2', '--type', 'hosts'),
+        backup  => false,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        notify  => Service['icinga'],
+    }
+    file { '/etc/icinga/puppet_services.cfg':
+        content => generate('/usr/local/bin/naggen2', '--type', 'services'),
+        backup  => false,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        notify  => Service['icinga'],
+    }
+    file { '/etc/icinga/puppet_hostextinfo.cfg':
+        content => generate('/usr/local/bin/naggen2', '--type', 'hostextinfo'),
+        backup  => false,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        notify  => Service['icinga'],
+    }
+
+    # Collect all (virtual) resources
+    Monitor_group <| |> {
+        notify => Service[icinga],
+    }
+    Monitor_host <| |> {
+        notify => Service[icinga],
+    }
+    Monitor_service <| tag != 'nrpe' |> {
+        notify => Service[icinga],
+    }
+
+}
