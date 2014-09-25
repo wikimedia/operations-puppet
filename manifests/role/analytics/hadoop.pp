cd40205072110c9da1ca216f0b63a3a284e9fb53
YuviPanda <yuvipanda@gmail.com>
2014-09-25 13:30:26 +0100

    icinga: Move NSCA code into module
    - nsca::client does not seem to be used anywhere except the
      hadoop nodes
    - the firewall rules misspelt NSCA as NCSA, corrected (I assume,
      since the port numbers match NSCA, and NCSA seems totally
      unrelated)
    
    Change-Id: I4c6e84da376796253689a980a382ff62c8e29f56

--- manifests/role/analytics/hadoop.pp
+++ manifests/role/analytics/hadoop.pp
@@ -508,7 +508,7 @@ class role::analytics::hadoop::worker inherits role::analytics::hadoop::client {
 # and permissions of the /etc/send_nsca.cfg file.  Hadoop
 # processes need to be able to read this file in order to
 # run send_nsca as part of Oozie submitted monitoring jobs.
-class role::analytics::hadoop::monitor::nsca::client inherits icinga::monitor::nsca::client {
+class role::analytics::hadoop::monitor::nsca::client inherits icinga::nsca::client {
     File ['/etc/send_nsca.cfg'] {
         group => 'hadoop',
         mode  => '0440',
