5d190ddc633bba8e9a921998a8b431ec3b61a5da
YuviPanda <yuvipanda@gmail.com>
2014-09-26 18:24:26 +0100

    icinga: Setup icinga role!
    Removed the main body of code from misc/icinga.pp into
    role/icinga.pp. Rest of code in misc/icinga.pp should die soon.
    
    Change-Id: I109db19b284f8abf19a938cae09e2760ee366009

---
+++ manifests/role/icinga.pp
@@ -0,0 +1,32 @@
+# = Class: role::icinga
+#
+# Sets up a icinga instance which checks services
+# and hosts for Wikimedia Production cluster
+class role::icinga {
+    include facilities::pdu_monitoring
+    include icinga::ganglia::ganglios
+    include icinga::monitor::checkpaging
+    include icinga::monitor::files::misc
+    include icinga::nsca::firewall
+    include icinga::nsca::daemon
+    include icinga::monitor::wikidata
+    include icinga::groups::misc
+    include lvs::monitor
+    include dsh::config
+    include mysql
+    include nagios::gsbmonitoring
+    include nrpe
+    include certificates::globalsign_ca
+
+    class { 'icinga':
+    }
+
+    class { 'icinga::web':
+        require => Class['icinga'],
+    }
+
+    class { 'icinga::naggen':
+        require => Class['icinga'],
+        notify  => Service['icinga'],
+    }
+}
