d0cc984229199b13e20e1524890f9339fcef970e
YuviPanda <yuvipanda@gmail.com>
2014-09-26 12:27:30 +0100

    icinga: Move initscript into module
    Ideally we should just use the one that comes with icinga,
    but not in this refactor
    
    Change-Id: Ided256110a32beb6507e64fcb87d5b5f0cbe7ad0

--- modules/icinga/manifests/packages.pp
+++ modules/icinga/manifests/packages.pp
@@ -9,4 +9,12 @@ class icinga::packages {
     ] :
         ensure => latest,
     }
+
+    # FIXME: Unsure why this is required
+    file { '/etc/init.d/icinga':
+        source => 'puppet:///modules/icinga/icinga-init',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
 }
