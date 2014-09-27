97a4af86f4d001f0f5ee86d39f7c65b896e4934f
andrewbogott <abogott@wikimedia.org>
2014-09-27 12:29:56 -0500

    Revert "Include base::firewall in labcontrol2001"
    This reverts commit a5787c94cd9b6e091455f6f0ad4a2abc02ad2b79.
    
    This class clearly doesn't do what I thought it did
    
    Change-Id: I0921890e4125a93113886a414e5f3ee57c7aef80

--- manifests/site.pp
+++ manifests/site.pp
@@ -3052,7 +3052,6 @@ node 'labcontrol2001.wikimedia.org' {
 
     include standard
     include admin
-    include base::firewall
     include role::dns::ldap
     include ldap::role::server::labs
     include ldap::role::client::labs
