888024a6c9f2593e74dd1fcfd7c7673595775edc
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2014-09-29 15:10:39 +0100

    Revert "swift: refactor into module, add codfw"
    makes puppet fail everywhere in codfw:
    
    Error: Could not retrieve catalog from remote server: Error 400 on SERVER:
    Error from DataBinding 'hiera' while looking up 'admin::always_groups': syntax
    error on line 88, col 1: `]' on node ms-be2001.codfw.wmnet
    
    This reverts commit 758a6f81594e97b6592249bda10698a49602d6f3.
    
    Change-Id: If0ba7cfd73857245a4063b69a55fc2431a3a01a0

--- manifests/role/swift.pp
+++ manifests/role/swift.pp
@@ -2,7 +2,6 @@
     description => 'swift servers',
 }
 
-
 class role::swift {
     class base {
         include standard
@@ -315,69 +314,4 @@ class role::swift::icehouse {
         pin      => 'release n=precise-updates/icehouse',
         priority => 1005,
     }
-}
-
-#######
-# NB. new swift module usage
-# initially only codfw, to be followed by esams and eqiad
-
-class role::swift::stats_reporter {
-    system::role { 'role::swift::stats_reporter':
-        description => 'swift statistics reporter',
-    }
-
-    include ::swift_new::params
-    include ::swift_new::stats::dispersion
-    include ::swift_new::stats::accounts
-}
-
-class role::swift::proxy {
-    system::role { 'role::swift::proxy':
-        description => 'swift frontend proxy',
-    }
-
-    include ::swift_new::params
-    include ::swift_new
-    include ::swift_new::ring
-    class { '::swift_new::proxy':
-        statsd_metric_prefix => "swift.${::swift_new::params::swift_cluster}.${::hostname}",
-    }
-
-    class { '::memcached':
-        memcached_size => '128',
-        memcached_port => '11211',
-    }
-
-    monitor_service { 'swift-http-frontend':
-        description   => 'Swift HTTP frontend',
-        check_command => "check_http_url!${swift_check_http_host}!/monitoring/frontend",
-    }
-    monitor_service { 'swift-http-backend':
-        description   => 'Swift HTTP backend',
-        check_command => "check_http_url!${swift_check_http_host}!/monitoring/backend",
-    }
-}
-
-
-class role::swift::storage {
-    system::role { 'role::swift::proxy':
-        description => 'swift storage brick',
-    }
-
-    include ::swift_new::params
-    include ::swift_new
-    include ::swift_new::ring
-    include ::swift_new::storage
-    include ::swift_new::storage::monitoring
-
-    $all_drives = hiera('swift_storage_drives')
-
-    swift_new::init_device { $all_drives:
-        partition_nr => '1',
-    }
-
-    # these are already partitioned and xfs formatted by the installer
-    $aux_partitions = hiera('swift_aux_partitions')
-    swift_new::label_filesystem { $aux_partitions: }
-    swift_new::mount_filesystem { $aux_partitions: }
 }
--- modules/swift_new/manifests/params.pp
+++
@@ -1,3 +0,0 @@
-class swift_new::params ( $swift_cluster = undef, $accounts = {}, $account_keys = {}) {
-    # Noop class
-}
