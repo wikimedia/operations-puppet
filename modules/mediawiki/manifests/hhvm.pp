5fa706eedfe78a77b5aeac8467a0f992e96fc56e
Ori Livneh <ori@wikimedia.org>
2014-09-28 20:16:53 -0700

    Set appropriate 500 and 404 documents for HHVM
    Configure hhvm.server.error_document{404,500} for pretty error pages:
    hhvm-fatal-error.php for 500s (added by MaxSem in change I9e97418aa /
    ce7cc3b5ae9) and the standard 404.php for 404s.
    
    You won't find these configuration keys if you grep the HHVM source tree.
    The ini-style lowercase_with_underscore names are programmatically derived from
    the hdf-style ErrorDocument404 and ErrorDocument500 directives.
    
    Change-Id: I1aa43dbb00745e652ef04c0ee325424167fb2c25

--- modules/mediawiki/manifests/hhvm.pp
+++ modules/mediawiki/manifests/hhvm.pp
@@ -16,7 +16,9 @@ class mediawiki::hhvm {
         fcgi_settings => {
             hhvm => {
                 server => {
-                    source_root => '/srv/mediawiki/docroot',
+                    source_root       => '/srv/mediawiki/docroot',
+                    error_document500 => '/srv/mediawiki/hhvm-fatal-error.php',
+                    error_document404 => '/srv/mediawiki/w/404.php',
                 },
             },
         },
