0759688739e7d34e9f538c6489ef5db8affe8ad4
Ori Livneh <ori@wikimedia.org>
2014-09-29 14:22:46 -0700

    HHVM: Set a fatal handler that logs traces
    Follows change I5ff22ac23 in operations/mediawiki-config. This patch sets
    '/srv/mediawiki/wmf-config/HHVMRequestInit.php' as HHVM's
    request_init_document, which will cause it to run at the very beginning of each
    request, before any other PHP code. The purpose of this file is to register a
    handler for catchable fatal errors that logs the error + stack trace to syslog.
    (We configure rsyslog to forward HHVM log messages to fluorine.)
    
    Change-Id: Id93b9eaa177911a8e5dbb4d01541f57996831057

--- modules/mediawiki/manifests/hhvm.pp
+++ modules/mediawiki/manifests/hhvm.pp
@@ -15,10 +15,14 @@ class mediawiki::hhvm {
         group         => 'apache',
         fcgi_settings => {
             hhvm => {
-                server => {
-                    source_root       => '/srv/mediawiki/docroot',
-                    error_document500 => '/srv/mediawiki/hhvm-fatal-error.php',
-                    error_document404 => '/srv/mediawiki/w/404.php',
+                error_handling => {
+                    call_user_handler_on_fatals => true,
+                },
+                server         => {
+                    source_root           => '/srv/mediawiki/docroot',
+                    error_document500     => '/srv/mediawiki/hhvm-fatal-error.php',
+                    error_document404     => '/srv/mediawiki/w/404.php',
+                    request_init_document => '/srv/mediawiki/wmf-config/HHVMRequestInit.php',
                 },
             },
         },
