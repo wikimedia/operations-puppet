7f198e86b57699417378dc726abf3a1c46b122c6
Chad Horohoe <chadh@wikimedia.org>
2014-09-30 13:52:35 -0700

    First of (hopefully many) Elastic tools
    es-health prints out the server's health as a string of
    "green" "yellow" or "red"
    
    Useful because many other scripts want to wait on green.
    
    Change-Id: I07c4420c4fe482515f7dd9f175ce2e067c817a1b

---
+++ modules/elasticsearch/files/tools/elastictool.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import argparse
+from elasticsearch import Elasticsearch
+
+class ElasticTool:
+    def __init__(self,desc):
+        self.parser = argparse.ArgumentParser(description=desc)
+        self.parser.add_argument("--server", metavar='S', type=str, default="localhost",
+            help="Server to work on, default localhost")
+
+    def health(self,server):
+        es = Elasticsearch(server)
+        return es.cluster.health()["status"]
