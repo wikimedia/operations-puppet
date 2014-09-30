abbf32dec7e5283436628bd83457bbf3aec44670
Christopher Johnson <christopher.johnson@wikimedia.de>
2014-09-30 18:28:48 +0200

    Abstracts Sprint install with defined resource type phabricator::libext
    Change-Id: If2e66a090581e10e350f3e7f9795e3b43c6b25da

--- manifests/role/phabricator.pp
+++ manifests/role/phabricator.pp
@@ -124,6 +124,10 @@ class role::phabricator::labs {
         git_tag          => $current_tag,
         lock_file        => '/var/run/phab_repo_lock',
         auth_type        => 'local',
+        libext_tag       => 'HEAD',
+        libraries        => {
+            'burndown'   => '/srv/phab/libext/Sprint',
+        },
         extension_tag    => 'HEAD',
         extensions       => ['SecurityPolicyEnforcerAction.php'],
         settings         => {
