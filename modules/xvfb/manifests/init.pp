94a53d4d30b46af08deadf96d2f958b00b451156
Timo Tijhof <krinklemail@gmail.com>
2014-09-29 18:59:52 -0700

    contint: Add Xvfb module, role::ci::slave::localbrowser and Chromium
    * Implement Xvfb module (based on Xvfb module in mediawiki-vagrant by ori-l).
    * Move general provision of browsers from contint::browsertests to contint::browsers.
    * Use Xvfb and latest chromium-browser in contint::browsers.
    * Use contint::browsers in role::ci::slave::browsertests.
    * Add new role::ci::slave::localbrowser.
    
    Change-Id: If2e96bfa9a1c46ab34af9526f0c3f241a3d17056

---
+++ modules/xvfb/manifests/init.pp
@@ -0,0 +1,54 @@
+# == Class: xvfb
+#
+# Configure a persistent Xvfb daemon.
+#
+# Xvfb (or X virtual framebuffer) is an X11 server that performs all
+# graphical operations in memory, not showing any screen output.
+#
+# === Parameters
+#
+# [*display*]
+#   X display number. Default: 99.
+#
+# [*resolution*]
+#   Virtual framebuffer resolution, expressed as WIDTHxHEIGHTxDEPTH.
+#   Default: '1024x768x24'.
+#
+# === Examples
+#
+#  class { 'xvfb':
+#    display    => 85,
+#    resolution => '800x600x16',
+#  }
+#
+class xvfb(
+    $display    = 99,
+    $resolution = '1024x768x24',
+) {
+    package { 'xvfb':
+        ensure => present,
+    }
+
+    group { 'xvfb':
+        ensure => present,
+    }
+
+    user { 'xvfb':
+        ensure => present,
+        gid    => 'xvfb',
+        shell  => '/bin/false',
+        home   => '/nonexistent',
+        system => true,
+    }
+
+    file { '/etc/init/xvfb.conf':
+        content => template('xvfb/xvfb.conf.erb'),
+        require => [ Package['xvfb'], User['xvfb'] ],
+    }
+
+    service { 'xvfb':
+        ensure   => running,
+        provider => 'upstart',
+        require  => File['/etc/init/xvfb.conf'],
+    }
+}
