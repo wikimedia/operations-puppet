bb63ce9862d83f8e4a53f38cf6d377593b59d773
dzahn <dzahn@wikimedia.org>
2014-10-02 13:37:35 -0700

    remove deprecated labsnfs class and files
    per comment from Tim Landscheidt on
    Ibd8f7b8b23a69
    
    the entire section that is being removed
    only applied to pmtpa, but pmtpa is gone
    
    also delete all the referenced templates,
    only keep the warning
    
    Change-Id: I7561d5f39086fe6b6fe371aa280ecb1300e730b2

--- manifests/role/labsnfs.pp
+++ manifests/role/labsnfs.pp
@@ -3,43 +3,6 @@
 
 class role::labsnfs::client {
 
-    if $::site == 'pmtpa' {
-        $nfscluster = 'labnfs.pmtpa.wmnet'
-
-        file { '/etc/auto.master':
-            ensure => file,
-            owner => root,
-            group => root,
-            mode => 0444,
-            content => template('labsnfs/auto.master.erb');
-        }
-        file { '/etc/auto.space':
-            ensure => file,
-            owner => root,
-            group => root,
-            mode => 0444,
-            content => template('labsnfs/auto.space.erb');
-        }
-        file { '/etc/auto.time.home':
-            ensure => file,
-            owner => root,
-            group => root,
-            mode => 0444,
-            content => template('labsnfs/auto.time.home.erb');
-        }
-        file { '/etc/auto.time.project':
-            ensure => file,
-            owner => root,
-            group => root,
-            mode => 0444,
-            content => template('labsnfs/auto.time.project.erb');
-        }
-
-        file { '/etc/init/nfs-noidmap.conf':
-            source => 'puppet:///files/nfs/upstart-nfs-noidmap.conf'
-        }
-    } else {
-        warning('Outside of pmtpa the role::labsnfs::client class is deprecated and does nothing.')
-    }
+    warning('Outside of pmtpa the role::labsnfs::client class is deprecated and does nothing.')
 }
 
