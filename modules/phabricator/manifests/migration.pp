8da968ec242ded48442b76f2c1d7c3f2c224733b
cpettet <rush@wikimedia.org>
2014-10-02 17:23:20 -0500

    phab readd migration tools repo
    * add cron for user metadata updating
    
    Change-Id: I98f5ca7af8c008723cac452c8a63a3028f6f6069

--- modules/phabricator/manifests/migration.pp
+++ modules/phabricator/manifests/migration.pp
@@ -11,5 +11,13 @@ class phabricator::migration() {
         git_tag   => 'HEAD',
     }
 
+    fab_lock = '/var/run/fab_update_user.flock'
+    cron { 'fab_user_update':
+        ensure  => present,
+        command => "/usr/bin/flock -n ${fab_lock} -c '/srv/phab/tools/fab_update_user.py -a' 2>&1",
+        user    => root,
+        hour    => '*/1',
+        minute  => 0,
+    }
 }
 
