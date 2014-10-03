58754b33f26952d1c25abc5d4c59a6e725b74e56
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-10-03 17:15:12 +0200

    puppet: drop global variable $puppet_version
    It's time to ditch puppet 2.7 supoport altoghether, most of our
    present-day modules aren't compatible with it anyway.
    
    Change-Id: I0de8237c543cae60b07de2872a1918dfa5bc6688
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/apt/manifests/puppet.pp
+++ modules/apt/manifests/puppet.pp
@@ -1,35 +1,22 @@
 define apt::puppet (
-    $version = $puppet_version,
     $packages  = 'puppet puppet-common facter'
     ) {
 
-    if ($version == '2.7') {
-        # For precise, we just need to tell apt to use ubuntu provided packages,
-        # with an higher priority than the general rule preferring wikimedia
-        # packages
-        if $::lsbdistcodename == 'precise' {
-                apt::pin { "puppet_${title}_${version}.pref":
-                    package  => $packages,
-                    pin      => 'release o=Ubuntu',
-                    priority => 1002
-                }
+    # For trusty, ubuntu packages are puppet 3
+    if $::lsbdistcodename == 'trusty' {
+        apt::pin { $title:
+            package  => $packages,
+            pin      => 'release o=Ubuntu',
+            priority => 1002
         }
-    } else {
-        # For trusty, ubuntu packages are puppet 3
-        if $::lsbdistcodename == 'trusty' {
-            apt::pin { $title:
-                package  => $packages,
-                pin      => 'release o=Ubuntu',
-                priority => 1002
-            }
-        } elsif $::lsbdistcodename == 'precise' {
-            # Ensure the pref file is not there in this case,
-            # so that puppet gets updated.
+    }
+    elsif $::lsbdistcodename == 'precise' {
+        # Ensure the pref file is not there in this case,
+        # so that puppet gets updated.
         apt::pin { "puppet_${title}_2.7.pref":
-                ensure   => 'absent',
-                priority => 0,
-                pin      => 'release o=Ubuntu'
-            }
+            ensure   => 'absent',
+            priority => 0,
+            pin      => 'release o=Ubuntu'
         }
     }
 }
