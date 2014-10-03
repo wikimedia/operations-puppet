ff5e27621e55be1ca984211e81e9e29eda264ed4
YuviPanda <yuvipanda@gmail.com>
2014-10-04 01:03:08 +0530

    toollabs: Remove gridengine ganglia monitoring
    - Ganglia monitoring in labs is dead, and will probably remain
      dead
    - This just sends us broken cron email now and then
    
    Change-Id: If2815201305ffda3351756a07c1baf5929f25824

--- modules/gridengine/manifests/master.pp
+++ modules/gridengine/manifests/master.pp
@@ -9,26 +9,4 @@ class gridengine::master {
         ensure  => latest,
         require => Package['gridengine-common'],
     }
-
-    class monitoring {
-        package { 'gridengine-client':
-            ensure => latest,
-        }
-
-        file { '/usr/local/sbin/grid-ganglia-report':
-            ensure  => present,
-            mode    => '0555',
-            source  => 'puppet:///modules/gridengine/grid-ganglia-report',
-            require => [Package['gridengine-client'], Package['libxml-libxml-perl']],
-        }
-
-        cron { 'grid-ganglia-report':
-            ensure  => present,
-            command => '/usr/local/sbin/grid-ganglia-report',
-            user    => 'root',
-            require => File['/usr/local/sbin/grid-ganglia-report'],
-        }
-    }
-
-    include monitoring
 }
