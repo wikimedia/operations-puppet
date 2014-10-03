33b72ae6dca36c387063c8555b77027699cfba9d
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2014-10-03 15:09:48 +0100

    swift: fix credentials variable
    Change-Id: Ia8d180dab92b2c5a5a17b19e6e55a9d86443c619

--- modules/swift_new/manifests/stats/stats_account.pp
+++ modules/swift_new/manifests/stats/stats_account.pp
@@ -7,7 +7,7 @@ define swift_new::stats::stats_account (
     $account_info = $accounts[$name]
     $auth_url     = $account_info[auth]
     $user         = $account_info[user]
-    $key          = $credentials[$user]
+    $key          = $credentials[user]
     $account_name = $account_info[account_name]
 
     $account_file = "/etc/swift/account_${account_name}.env"
