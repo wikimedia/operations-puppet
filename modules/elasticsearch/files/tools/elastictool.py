6904a5cdd8eb7756d1f9122adf034e283670bb45
Chad Horohoe <chadh@wikimedia.org>
2014-10-03 11:39:05 -0700

    Adding tools for banning/unbanning an ES node
    Change-Id: Iab6eae17a6d17b69e4c8e04ce42d20f670a9669a

--- modules/elasticsearch/files/tools/elastictool.py
+++ modules/elasticsearch/files/tools/elastictool.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import socket
 import sys
 from elasticsearch import Elasticsearch
 from elasticsearch import ConnectionError
@@ -30,13 +31,12 @@ class ElasticTool:
         es = Elasticsearch(self.server)
         return es.cluster.health()["status"]
 
-    def set_replication_state(self, status):
+    def set_setting(self, setting, value, settingtype="transient"):
         es = Elasticsearch(self.server)
         res = es.cluster.put_settings(
             body={
-                # custom analyzer for analyzing file paths
-                "transient": {
-                    "cluster.routing.allocation.enable": status
+                settingtype: {
+                    setting: value
                 }
             }
         )
@@ -44,3 +44,28 @@ class ElasticTool:
             return True
         else:
             return False
+
+    def set_replication_state(self, status):
+        return self.set_setting("cluster.routing.allocation.enable", status)
+
+    def is_valid_ip(self, ip):
+        try:
+            socket.inet_aton(ip)
+            return True
+        except socket.error:
+            return False
+
+    def get_banned_ips(self):
+        es = Elasticsearch(self.server)
+        res = es.cluster.get_settings()
+        try:
+            if res["transient"]["cluster"]["routing"]["allocation"]["exclude"]["_ip"]:
+                return res["transient"]["cluster"]["routing"]["allocation"]["exclude"]["_ip"].split(",")
+        except KeyError:
+            pass
+
+        return []
+
+    def set_banned_ips(self, iplist):
+        return self.set_setting("cluster.routing.allocation.exclude._ip",
+                                ",".join(iplist))
