99477582c5d0751f586eb8fe36077d6532109bf3
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2014-10-06 14:14:50 +0100

    swift: add codfw monitoring and dashboards
    Change-Id: I7ea244d2b49b7f53112808ff45ffff0dc9047abb

---
+++ modules/swift_new/manifests/monitoring/graphite.pp
@@ -0,0 +1,23 @@
+class swift_new::monitoring::graphite (
+    $swift_cluster = $::swift_new::params::swift_cluster,
+) {
+    monitor_graphite_threshold { "swift_${swift_cluster}_dispersion_object":
+        description     => "swift ${swift_cluster} object availability",
+        metric          => "swift.${swift_cluster}.dispersion.object.pct_found.value",
+        from            => '1hours',
+        warning         => 95,
+        critical        => 90,
+        under           => true,
+        nagios_critical => false
+    }
+
+    monitor_graphite_threshold { "swift_$swift_cluster_dispersion_container":
+        description     => "swift $swift_cluster container availability",
+        metric          => "swift.$swift_cluster.dispersion.container.pct_found.value",
+        from            => '30min',
+        warning         => 92,
+        critical        => 88,
+        under           => true,
+        nagios_critical => false
+    }
+}
