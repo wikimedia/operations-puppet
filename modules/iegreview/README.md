f1563789e12e4636c413528fe0d6e96bffa570d4
Bryan Davis <bd808@wikimedia.org>
2014-10-07 10:12:18 -0600

    iegreview: Create module and role for deployment
    Bug: 71597
    RT: 8578
    Change-Id: I69d4a2821db0cc3148963f743fcbdca31208cb9f

---
+++ modules/iegreview/README.md
@@ -0,0 +1,11 @@
+Wikimedia IEG Grant Review
+==========================
+
+The IEG review application is a LAMP application to manage a grant
+review campaign for the Individual Engagement Grants program managed by
+the Wikimedia Foundation.
+
+Links
+-----
+* https://meta.wikimedia.org/wiki/Grants:IEG
+* https://git.wikimedia.org/summary/wikimedia%2Fiegreview
---
+++ modules/iegreview/manifests/init.pp
@@ -0,0 +1,90 @@
+# = Class: iegreview
+#
+# This class provisions the IEG grant review application.
+#
+# == Parameters:
+# [+hostname+]
+#   Hostname for apache vhost. Default iegreview.wikimedia.org.
+#
+# [+deploy_dir+]
+#   Directory application is deployed to.
+#   Default /srv/deployment/iegreview/iegreview.
+#
+# [+cache_dir+]
+#   Directory for caching twig templates. Default /var/cache/iegreview.
+#
+# [+log_dest+]
+#   Log destination. Default udp://udplog:8420/iegreview
+#
+# [+serveradmin+]
+#   Administrative contact email address. Default noc@wikimedia.org.
+#
+# [+mysql_host+]
+#   MySQL database server. Default localhost.
+#
+# [+mysql_db+]
+#   MySQL database. Default iegreview.
+#
+# [+smtp_host+]
+#   Outgoing email relay. Default localhost.
+#
+# [+parsoid_url+]
+#   Parsoid API url for wikitext to html conversion.
+#   Default http://parsoid-lb.eqiad.wikimedia.org/enwiki/
+#
+# [+require_upstream_ssl+]
+#   Should upstream ssl termination be required? Default false.
+#
+# == Sample usage:
+#
+#   class { 'iegreview':
+#       require_upstream_ssl => true,
+#   }
+#
+class iegreview(
+    $hostname             = 'iegreview.wikimedia.org',
+    $deploy_dir           = '/srv/deployment/iegreview/iegreview',
+    $cache_dir            = '/var/cache/iegreview',
+    $log_dest             = 'udp://udplog:8420/iegreview',
+    $mysql_host           = 'localhost',
+    $mysql_db             = 'iegreview',
+    $smtp_host            = 'localhost',
+    $parsoid_url          = 'http://parsoid-lb.eqiad.wikimedia.org/enwiki/',
+    $require_upstream_ssl = false,
+) {
+    include ::apache
+    include ::apache::mod::php5
+    include ::apache::mod::rewrite
+
+    system::role { 'iegreview':
+        description => 'IEG grant review server'
+    }
+
+    package { 'iegreview/iegreview':
+        provider => 'trebuchet',
+    }
+
+    require_package( 'php5-mysql' )
+
+    apache::site { $hostname:
+        content => template('iegreview/apache.conf.erb'),
+    }
+
+    file { "${deploy_dir}/.env":
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('iegreview/env.erb'),
+        require => Package['iegreview/iegreview'],
+        notify  => Service['apache2'],
+    }
+
+    file { $cache_dir:
+        ensure => directory,
+        owner  => 'www-data',
+        group  => 'root',
+        mode   => '0755',
+    }
+}
+# vim:sw=4 ts=4 sts=4 et:
