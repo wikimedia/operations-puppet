f02218aee730a78284a44bdbf0c6197d7f9e5774
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2014-10-08 16:39:42 +0100

    mediawiki: mediawiki-math-texvc is in precise-wikimedia too
    Change-Id: I048b2a8bf040ac7ffd26ff804160ef93cf13114e

--- modules/mediawiki/manifests/packages.pp
+++ modules/mediawiki/manifests/packages.pp
@@ -47,12 +47,6 @@ class mediawiki::packages {
         ensure => present,
     }
 
-    if ubuntu_version('>= trusty') {
-        package { 'mediawiki-math-texvc':
-            ensure => present,
-        }
-    }
-
     # PDF and DjVu
     package { [
         'djvulibre-bin',
