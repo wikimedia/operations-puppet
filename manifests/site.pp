2fc44dae6ce75659935277e4f8023c59746830e9
andrewbogott <abogott@wikimedia.org>
2014-10-10 14:59:12 -0500

    Add deployment access to the tmh boxes.
    Uncomfortably, most deployers already have access to
    these boxes, but there's nothing in puppet to document
    it or provide access for new users.
    
    RT 8481
    
    Change-Id: I1561b061148cd7b04522f731039e34b94d2833be

--- manifests/site.pp
+++ manifests/site.pp
@@ -2706,6 +2706,10 @@ node /^tmh100[1-2]\.eqiad\.wmnet/ {
         $ganglia_aggregator = true
     }
     include role::mediawiki::videoscaler
+
+    class { 'admin':
+        groups => ['deployment']
+    }
 }
 
 # Receives log data from varnishes (udp 8422) and Apaches (udp 8421),
