0e82e5e25515d7cb001f87f962c029f524dccdc1
Mukunda Modell <mmodell@wikimedia.org>
2014-10-12 06:09:09 -0500

    redirector for bugzilla -> phabricator
    This iteration is simply a test for fab-01.wmflabs to validate
    that the redirect rules are working and that the maniphest lookup works.
    
    Change-Id: I5acedd2186d296895baa0a1c1486012a7ce91660

--- modules/phabricator/manifests/init.pp
+++ modules/phabricator/manifests/init.pp
@@ -275,3 +275,14 @@ define phabricator::extension($rootdir='/') {
         target => "${rootdir}/extensions/${name}",
     }
 }
+
+define phabricator::redirector($mysql_user, $mysql_pass, $mysql_host, $rootdir='/') {
+    file { "${rootdir}/phabricator/support/preamble.php":
+        source => 'puppet:///modules/phabricator/preamble.php',
+        require => File["${rootdir}/phabricator/support/redirect_config.json"]
+    }
+
+    file { "${rootdir}/phabricator/support/redirect_config.json":
+        content => template('phabricator/redirect_config.json.erb')
+    }
+}
