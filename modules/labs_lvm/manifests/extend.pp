ca526c69067ef1bf3291c1abf6311a2b4c99e541
Coren <marc@uberbox.org>
2014-10-12 17:16:15 -0400

    Labs: allow for growing volumes
    Also update role::labs::biglog to extend /var/log rather than
    create a new volume (necessary for the new images).  Images
    using the older image but that have not yet created a /var/log
    will need to do so by hand.
    
    Change-Id: I00abd321f3b1c06ce7a563bd3ace28bb9b3bd2d6

---
+++ modules/labs_lvm/manifests/extend.pp
@@ -0,0 +1,34 @@
+# resource: labs_lvm::extend
+#
+# labs_lvm::extend ensures the specified volume has at least
+# the requested size (if possible)
+#
+# Parameters:
+#   mountat  => point where the volume is to be mounted; defaults
+#               to the title
+#   size     => desired size of the volume, using the lvcreate(8)
+#               syntax.  This can only /extend/ the volume size.
+#
+# Requires:
+#   The node must have included the labs_lvm class.
+#
+# Sample Usage:
+#   labs_lvm::extend { '/srv': size => '8G' }
+#
+
+define labs_lvm::extend(
+    $mountat    = $title,
+    $mountowner = 'root',
+    $mountgroup = 'root',
+    $mountmode  = '755',
+    $size       = '100%FREE',
+) {
+
+    exec { "extend-vd-$mountat":
+        logoutput   => 'on_failure',
+        require     => File['/usr/local/sbin/extend-instance-vol'],
+        command     => "/usr/local/sbin/extend-instance-vol '$mountat' '$size'",
+    }
+
+}
+
