d34f660af012f5d247dfaa4d44c3d19f8a0cd502
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-10-14 11:29:01 +0200

    apache: fix diamond stats collections.
    Change-Id: I53ce052a3e6885f12e2f4d8b1e4154e8ff75e66b
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/apache/manifests/monitoring.pp
+++ modules/apache/manifests/monitoring.pp
@@ -9,7 +9,12 @@ class apache::monitoring {
     include ::apache::mod::status
     include ::ganglia
 
-    diamond::collector { 'Httpd': }
+    diamond::collector { 'Httpd':
+        settings => {
+            path => "${cluster}.httpd",
+            urls => "http://127.0.0.1/server-status?auto"
+        }
+    }
 
     file { '/usr/lib/ganglia/python_modules/apache_status.py':
         source  => 'puppet:///modules/apache/apache_status.py',
