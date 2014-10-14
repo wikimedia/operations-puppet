3e6d78e472a5672f05570955eeae1cc76edb9602
andrewbogott <abogott@wikimedia.org>
2014-10-14 13:43:48 -0500

    Another attempt to fix mw-xml.sh for the new wiki setup
    This is used for backups and to populate wikitech-static.
    
    Change-Id: I0a6f80ea7e31e464dc93039a46849f111045d7c5

--- files/openstack/mw-xml.sh
+++ files/openstack/mw-xml.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /srv/mediawiki/php
-php maintenance/dumpBackup.php --full --uploads | nice -n 19 gzip -9 > /a/backup/public/labswiki-$(date '+%Y%m%d').xml.gz
+mwscript maintenance/dumpBackup.php labswiki --full --uploads | nice -n 19 gzip -9 > /a/backup/public/labswiki-$(date '+%Y%m%d').xml.gz
 cd -
