3cbb1eba8ab8dc03834e4abff46d1ffcd70ac325
Gabriel Wicke <wicke@wikidev.net>
2014-10-17 08:01:30 -0700

    Initial RESTBase puppet module
    Change-Id: I2b83590ee432871078a861425e0d0b1fed0440d1

---
+++ modules/restbase/manifests/init.pp
@@ -0,0 +1,111 @@
+# == Class: restbase
+#
+# restbase is a REST API & storage service
+# http://restbase.org
+#
+# === Parameters
+#
+# [*cassandra_user*]
+#   Cassandra user name.
+# [*cassandra_password*]
+#   Cassandra password.
+# [*seeds*]
+#   Array of cassandra hosts (IP or host names) to contact.
+#   Default: ['localhost']
+# [*port*]
+#   Port where to run the restbase service. Default: 7231
+# [*logstash_host*]
+#   GELF logging host. Default: localhost
+# [*logstash_port*]
+#   GELF logging port. Default: 12201
+#
+class restbase(
+    $cassandra_user,
+    $cassandra_password,
+    $seeds          = [$::ipaddress],
+    $cassandra_defaultConsistency = 'localQuorum',
+    $port           = 7231,
+    $logstash_host  = 'localhost',
+    $logstash_port  = '12201',
+) {
+    ensure_packages( ['nodejs', 'nodejs-legacy', 'npm'] )
+
+    package { 'restbase/deploy':
+        provider => 'trebuchet',
+    }
+
+    group { 'restbase':
+        ensure => present,
+        system => true,
+    }
+
+    user { 'restbase':
+        gid    => 'restbase',
+        home   => '/nonexistent',
+        shell  => '/bin/false',
+        system => true,
+        before => Service['restbase'],
+    }
+
+    file { '/var/log/restbase':
+        ensure => directory,
+        owner  => 'restbase',
+        group  => 'restbase',
+        mode   => '0775',
+        before => Service['restbase'],
+    }
+
+    file { '/etc/default/restbase':
+        content => template('restbase/restbase.default.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['restbase'],
+    }
+
+    file { '/etc/init.d/restbase':
+        content => template('restbase/restbase.init'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        notify  => Service['restbase'],
+    }
+
+    file { '/etc/restbase':
+        ensure => directory,
+        owner  => 'restbase',
+        group  => 'restbase',
+        mode   => '0775',
+        before => Service['restbase'],
+    }
+
+    file { '/etc/restbase/config.yaml':
+        content => template('restbase/config.yaml.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['restbase'],
+    }
+
+    file { '/usr/lib/restbase':
+        ensure => directory,
+        owner  => 'restbase',
+        group  => 'restbase',
+        mode   => '0775',
+        before => Service['restbase'],
+    }
+
+    file { '/usr/lib/restbase/deploy':
+        ensure  => link,
+        target  => '/srv/deployment/restbase/deploy',
+        require => File['/usr/lib/restbase'],
+        before  => Service['restbase'],
+    }
+
+    service { 'restbase':
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        provider   => 'init',
+    }
+}
