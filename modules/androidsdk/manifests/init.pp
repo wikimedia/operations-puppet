990d48f03e63c8e207c1cdb98eaf5b6604e7c054
YuviPanda <yuvipanda@gmail.com>
2014-10-17 18:28:47 +0530

    androidsdk: Add class to set up wikipedia app build
    Change-Id: I670800315ad49e6458917c3d168b3ecc201fb73f

---
+++ modules/androidsdk/manifests/init.pp
@@ -0,0 +1,40 @@
+# = Class androidsdk::build::wikipedia
+#
+# Sets up requirements for building the wikipedia android
+# app.
+class androidsdk::build::wikipedia {
+
+    include ::role::labs::lvm::srv
+
+    user { 'android-build':
+        ensure => present,
+        shell  => '/bin/false',
+    }
+
+    git::clone { 'apps/android/wikipedia':
+        directory => '/srv/wikipedia',
+        ensure    => present,
+        owner     => 'android-build',
+        require   => User['android-build'],
+    }
+
+    git::clone { 'labs/tools/wikipedia-android-builds':
+        directory => '/srv/builds',
+        ensure    => present,
+        owner     => 'android-build',
+        require   => User['android-build'],
+    }
+   
+    nginx::site { 'wikipedia-android-build':
+        ensure => present,
+        source => 'puppet:///modules/androidsdk/nginx.conf'
+    }
+
+    cron { 'wikipedia-android-build':
+        ensure  => present,
+        command => '/srv/builds/src/build.py',
+        minute  => [0, 30],
+        user    => 'android-build',
+        require => User['android-build'],
+    }
+}
