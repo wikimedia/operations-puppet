facae3af32d60d078f5f84ec6262249c2eb3bb21
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-10-17 11:36:34 +0200

    sudo: create module, remove old files
    For the sake of structuring our puppet classes/manifests according to
    the "standard" structure expected by puppet.
    
    Change-Id: Ic7e883f85b81b516b424168be2a3380dcc5f3121
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/beta/manifests/mwdeploy_sudo.pp
+++ modules/beta/manifests/mwdeploy_sudo.pp
@@ -6,7 +6,7 @@ class beta::mwdeploy_sudo {
     # Grant mwdeploy sudo rights to run anything as itself, apache or
     # l10nupdate and to (re)start the hhvm fcgi service. This is a subset of
     # the rights granted to the wikidev group by the mediawiki::users class.
-    sudo_user { 'mwdeploy' :
+    sudo::user { 'mwdeploy' :
         privileges => [
             'ALL = (apache,mwdeploy,l10nupdate) NOPASSWD: ALL',
             'ALL = (root) NOPASSWD: /sbin/restart hhvm',
---
+++ modules/sudo/manifests/labs_project.pp
@@ -0,0 +1,19 @@
+class sudo::labs_project {
+    if $::realm != 'labs' {
+        fail('This class is labs-specific')
+    }
+	# labs specific sudo default
+    file { '/etc/sudoers':
+        owner  => root,
+        group  => root,
+        mode   => '0440',
+        source => 'puppet:///modules/sudo/sudoers.labs';
+    }
+
+    # Was handled via sudo ldap, now handled via puppet
+    sudo::group { 'ops': privileges => ['ALL=(ALL) NOPASSWD: ALL'] }
+    # Old way of handling this.
+    sudo::group { $instanceproject: ensure => absent }
+    # Another old way, before per-project sudo
+    sudo::group { $projectgroup: ensure => absent }
+}
