51e65ac257441774c33374c05c9d68941404c2a4
Coren <marc@uberbox.org>
2014-10-16 21:10:38 -0400

    Tool Labs: fix c&p fail in hostgroup::collector
    Change-Id: Ia93c56b94563688c3bfd399bade19ff4d71b571f

--- modules/toollabs/manifests/hostgroup/collector.pp
+++ modules/toollabs/manifests/hostgroup/collector.pp
@@ -6,13 +6,6 @@ define toollabs::hostgroup::collector( $hgname = $title )
 
     $hgrpfile  = "/etc/gridengine/local/${hgname}.hosts"
 
-    file { $dir:
-        ensure  => directory,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0755',
-    }
-
     exec { "make-${hgname}-hosts":
         cwd     => $toollabs::hostgroup::hgstore,
         command => "(echo group_name @{$hgname};echo hostlist \$(/bin/egrep -l '^${hgname}' *)) >'${hgrpfile}'",
