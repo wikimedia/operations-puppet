9e8281202aaba30867a3ae10b1b16a1c1298f0d4
Ori Livneh <ori@wikimedia.org>
2014-10-18 15:56:30 -0700

    mathoid to service::node
    Have mathoid use the service::node paradigm
    This is blocked on:
    	* mathoid being adapted to service-runner
    	* mathoid getting a mathoid/deploy directory
    
    Bug: T97124
    Change-Id: I06f06cf1f516a536b82b286551eda011c02e3a6c

--- modules/mathoid/manifests/init.pp
+++ modules/mathoid/manifests/init.pp
@@ -1,73 +1,17 @@
 # == Class: mathoid
 #
-# mathoid is a node.js backend for the math rendering.
+# Mathoid is an application which takes various forms of math input and
+# converts it to MathML + SVG output. It is a web-service implemented
+# in node.js.
 #
-# === Parameters
-#
-# [*base_path*]
-#   Path to the mathoid code.
-# [*node_path*]
-#   Path to the node modules mathoid depends on.
-# [*conf_path*]
-#   Where to place the config file.
-# [*log_dir*]
-#   Place where mathoid can put log files. Assumed to be already existing and
-#   have write access to mathoid user.
-# [*port*]
-#   Port where to run the mathoid service. Defaults to 10042.
-#
-class mathoid(
-    $base_path = '/srv/deployment/mathoid/mathoid',
-    $node_path = '/srv/deployment/mathoid/mathoid/node_modules',
-    $conf_path = '/srv/deployment/mathoid/mathoid/mathoid.config.json',
-    $log_dir = '/var/log/mathoid',
-    $port=10042
-) {
-    require_package('nodejs')
-    # TODO Add dependency to node-jsdom once
-    # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=742347
-    # is fixed
+class mathoid {
 
-    $log_file = "${log_dir}/main.log"
+    # Pending fix for <https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=742347>
+    # require_package('node-jsdom')
 
-    file { $log_dir:
-        ensure => directory,
-        owner  => mathoid,
-        group  => mathoid,
-        mode   => '0775',
-    }
-
-    file { $conf_path:
-        ensure  => present,
-        owner   => mathoid,
-        group   => mathoid,
-        mode    => '0555',
-        content => template('mathoid/config.erb'),
-    }
-
-    # The upstart configuration
-    file { '/etc/init/mathoid.conf':
-        ensure  => present,
-        owner   => root,
-        group   => root,
-        mode    => '0444',
-        content => template('mathoid/upstart.erb'),
-    }
-
-    file { '/etc/logrotate.d/mathoid':
-        ensure  => present,
-        owner   => root,
-        group   => root,
-        mode    => '0444',
-        content => template('mathoid/logrotate.erb'),
-    }
-
-    service { 'mathoid':
-        ensure     => running,
-        hasstatus  => true,
-        hasrestart => true,
-        provider   => 'upstart',
-        require    => File[$log_dir],
-        subscribe  => File['/etc/init/mathoid.conf'],
+    service::node { 'mathoid':
+        port            => 10042,
+        config          => '{}';
+        healthcheck_url => '/',
     }
 }
