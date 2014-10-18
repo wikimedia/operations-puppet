696ba14431b4e4ca0b602d05a0e5de6666fc7dc5
Ori Livneh <ori@wikimedia.org>
2014-10-18 15:56:30 -0700

    mathoid to service::node
    Have mathoid use the service::node paradigm
    
    Bug: T97124
    Change-Id: I06f06cf1f516a536b82b286551eda011c02e3a6c

--- manifests/role/mathoid.pp
+++ manifests/role/mathoid.pp
@@ -9,34 +9,5 @@ class role::mathoid{
         description => 'mathoid server'
     }
 
-    class { '::mathoid':
-      require   => Package['mathoid/mathoid'],
-    }
-
-    package { 'mathoid/mathoid':
-        provider => 'trebuchet',
-    }
-
-    group { 'mathoid':
-      ensure => present,
-      name   => 'mathoid',
-      system => true,
-    }
-
-    user { 'mathoid':
-      gid        => 'mathoid',
-      home       => '/srv/deployment/mathoid/mathoid',
-      managehome => true,
-      system     => true,
-    }
-
-    ferm::service { 'mathoid':
-      proto => 'tcp',
-      port  => '10042'
-    }
-
-    monitoring::service { 'mathoid':
-      description   => 'mathoid',
-      check_command => 'check_http_on_port!10042',
-    }
+    include ::mathoid
 }
