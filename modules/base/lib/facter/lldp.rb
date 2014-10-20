70a28b99a7f8f394fb0e85bfc5c572510a1e3e47
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-10-20 22:28:57 +0300

    Introduce LLDP facts
    Use lldpctl's xml format to introduce a number of facts
     * lldppeer_ethX => the switch connected on that interface
     * lldpswport_ethX => the switchport description
     * lldpswport_id => The switchport id as reported by the switch
     * lldppeers => Comma separated list of all LLDP peers
    
    where ethX is a given interface that both it and it's connected
    counterpart speak LLDP
    
    Change-Id: Icc744872f8dfed24eea149bc05766053cc2656a5

---
+++ modules/base/lib/facter/lldp.rb
@@ -0,0 +1,61 @@
+require 'facter'
+require 'rexml/document'
+
+if Facter.value('virtual') == 'physical' and File.exists?('/usr/sbin/lldpctl')
+
+    lldpeers = nil
+
+    data = Facter::Util::Resolution.exec('/usr/sbin/lldpctl -f xml')
+    document = REXML::Document.new(data)
+    document.elements.each('lldp/interface') do |iface|
+        eth = iface.attributes['name']
+        iface.elements.each('chassis/name') do |switch|
+            Facter.add('lldppeer_%s' % eth) do
+                confine :kernel => %w{Linux FreeBSD OpenBSD}
+                setcode do
+                    switch.text
+                end
+            end
+            if lldppeers
+                lldppeers = lldppeers + ',' + switch.text
+            else
+                lldppeers = switch.text
+            end
+        end
+        iface.elements.each('port/descr') do |port|
+            Facter.add('lldpswport_%s' % eth) do
+                confine :kernel => %w{Linux FreeBSD OpenBSD}
+                setcode do
+                    port.text
+                end
+            end
+        end
+        iface.elements.each('port/id') do |port|
+            Facter.add('lldpswportid_%s' % eth) do
+                confine :kernel => %w{Linux FreeBSD OpenBSD}
+                setcode do
+                    port.text
+                end
+            end
+        end
+        # VLAN info is also reported by LLDP but for now not parsing it due to some
+        # inconsistencies in reporting depending on vendor and lack of a use case
+        # sample code is provided however
+        #iface.elements.each('vlan') do |vlan|
+        #    Facter.add('lldpswport_vlan_%s' % eth) do
+        #        confine :kernel => %w{Linux FreeBSD OpenBSD}
+        #        setcode do
+        #            vlan.text
+        #        end
+        #    end
+        #end
+    end
+
+    # Aggregate all the lldp peers on one single variable
+    Facter.add('lldppeers') do
+        confine :kernel => %w{Linux FreeBSD OpenBSD}
+        setcode do
+            lldppeers
+        end
+    end
+end
