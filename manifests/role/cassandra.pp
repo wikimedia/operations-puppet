93ec358807b26f33b58fd6942bbb0d107d161c19
Andrew Otto <acotto@gmail.com>
2014-10-20 17:15:13 -0400

    [WIP] Add cassandra role
    Depends on https://gerrit.wikimedia.org/r/#/c/166888/
    
    Change-Id: I7790c71d89f8bced543f51aae59f1f0ca4ad2860

---
+++ manifests/role/cassandra.pp
@@ -0,0 +1,12 @@
+# == Class role::cassandra
+#
+class role::cassandra {
+    sysctl::parameters { 'cassandra':
+        values => {
+            'vm.max_map_count' => 1048575,
+        },
+    }
+
+    # Parameters to be set by Hiera
+    include cassandra
+}
