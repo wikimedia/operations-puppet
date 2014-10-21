wikimedia/operations-puppet

commit 9b8ead055c00a9067095bd97d227508caa103331

remove 10.0.0.0/16 Tampa subnet
update network.pp
dhcp
url_downloader

Change-Id: I58f431eb6f8cf5b3d86429794f068f4bf0eec4d8

manifests/network.pp
@@ -188,9 +188,6 @@ class network::constants {
                     'virt-hosts' => {
                         'ipv4' => '10.4.16.0/24'
                     },
-                    'private' => {
-                        'ipv4' => '10.0.0.0/16'
-                    },
                 },
             },
             'ulsfo' => {

modules/install-server/files/dhcpd/dhcpd.conf
@@ -61,18 +61,6 @@ subnet 208.80.153.128 netmask 255.255.255.192 {
     next-server 208.80.154.10; # carbon (tftp server)
 }
 
-# Private subnet
-subnet 10.0.0.0 netmask 255.255.0.0 {
-	authoritative;
-
-	option subnet-mask 255.255.0.0;
-	option broadcast-address 10.0.255.255;
-	option routers 10.0.0.200;
-	option domain-name "pmtpa.wmnet";
-
-	next-server 208.80.154.10; # carbon (tftp server)
-}
-
 # Management subnet
 subnet 10.1.0.0 netmask 255.255.0.0 {
 	auhhoritative;

modules/url_downloader/templates/squid.conf.erb
@@ -42,7 +42,6 @@ acl purge method PURGE
 acl CONNECT method CONNECT
 
 acl wikimedia src 208.80.152.0/22
-acl wikimedia src 10.0.0.0/16
 acl wikimedia src 10.64.0.0/22
 acl wikimedia src 10.64.16.0/24
 acl wikimedia src 10.64.32.0/22
@@ -50,7 +49,6 @@ acl wikimedia src 10.64.48.0/22
 
 acl to-wikimedia dst 208.80.152.0/22
 acl to-wikimedia dst 91.198.174.0/24
-acl to-wikimedia dst 10.0.0.0/16
 acl to-wikimedia dst 10.64.0.0/22
 acl to-wikimedia dst 10.64.16.0/24
 acl to-wikimedia dst 10.64.32.0/22

commit 70f827e56395ba7fd67b51a333072912c31d6e17

Fix regression introduced in 9b8ead0
Remove references to network subnet 10.0.0.0/16 removed in 9b8ead0

Change-Id: I7426eafe3d985881b9c0eb1da81c28adb3b2ab39

manifests/network.pp
@@ -249,7 +249,6 @@ class network::constants {
     # Networks hosting MediaWiki application servers
     $mw_appserver_networks = [
         '208.80.152.0/22',    # external
-        $all_network_subnets['production']['pmtpa']['private']['private']['ipv4'],
         $all_network_subnets['production']['eqiad']['private']['private1-a-eqiad']['ipv4'],
         $all_network_subnets['production']['eqiad']['private']['private1-b-eqiad']['ipv4'],
         $all_network_subnets['production']['eqiad']['private']['private1-c-eqiad']['ipv4'],