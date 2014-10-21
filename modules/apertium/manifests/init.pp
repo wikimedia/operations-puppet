42dcdfbc27e34748ea339f9ab38b7de52bfa4a46
Kartik Mistry <kartik.mistry@gmail.com>
2014-10-21 11:03:41 +0530

    Beta: Apertium: Remove unused comments for parameters
    Change-Id: I42cf8ce52583cae041799bbab3b0ceaacd5a8d25

--- modules/apertium/manifests/init.pp
+++ modules/apertium/manifests/init.pp
@@ -3,13 +3,6 @@
 # Apertium is a backend Machine Translation service for the Content Translation.
 # https://www.mediawiki.org/wiki/Content_translation/Apertium
 #
-# === Parameters
-#
-# [*log_dir*]
-#   Place where apertium can put log files. Assumed to be already existing and
-#   have write access to apertium user.
-# [*port*]
-#   Port where to run the apertium service. Defaults to 2737.
 class apertium(){
     package { [
         'apertium',
