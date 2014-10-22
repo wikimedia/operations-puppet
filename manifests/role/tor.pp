8a00f70ee58caad8ce24bb932fa28a75fc67af0a
dzahn <dzahn@wikimedia.org>
2014-10-21 18:44:52 -0700

    tor - adjust nickname
    unfortunately we can't have - or _ in the nick
    they are illegal characters
    
    Change-Id: I2252a512f35398bc55f85231d21dcb1fb731ef12

--- manifests/role/tor.pp
+++ manifests/role/tor.pp
@@ -9,7 +9,7 @@ class role::tor {
         tor_orport      => '443',
         tor_dirport     => '80',
         tor_address     => 'tor-eqiad-1.wikimedia.org',
-        tor_nickname    => 'wikimedia-eqiad-1',
+        tor_nickname    => 'wikimediaeqiad1',
         tor_contact     => 'noc@wikimedia.org',
         tor_exit_policy => 'reject *:*', # no exits allowed
     }
