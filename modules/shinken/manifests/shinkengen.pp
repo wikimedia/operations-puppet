30049adf0c6b58a5c15c83f770a9d321b0e5f23c
YuviPanda <yuvipanda@gmail.com>
2014-10-22 22:18:26 +0530

    shinken: Autogenerate config from shinkengen
    python3-shinkengen package currently in labsdebrepo
    at the shinken project
    Change-Id: I9e688be407bed3fd502ff7814a84a4b890f8fcbe

---
+++ modules/shinken/manifests/shinkengen.pp
@@ -0,0 +1,28 @@
+# = Class: shinken::shinkengen
+#
+# Sets up shinkengen python package to generate hosts & services
+# config for Shinken by hittig the wikitech API
+#
+# FIXME: Also restarts shinkin on each run, even if no config
+# files have changed
+class shinken::shinkengen {
+
+    include shinken::server
+
+    package { 'python3-shinkengen':
+        ensure => latest,
+    }
+
+    file { '/etc/shinkengen.yaml':
+        source  => 'puppet:///modules/shinken/shinkengen.yaml',
+        user    => 'shinken',
+        group   => 'shinken',
+    }
+
+    exec { '/usr/bin/shingen':
+        user    => 'shinken',
+        group   => 'shinken',
+        require => [Package['python3-shinkengen'], File['/etc/shinkengen.yaml']],
+        notify  => Service['shinken'],
+    }
+}
