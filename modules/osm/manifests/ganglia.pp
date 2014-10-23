908468fafea46dcad2e5f55a0454d82302fd356a
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-10-23 18:58:50 +0300

    ganglia plugin for openstreetmap sync delay
    Adapted the postgresql one to measure the osm planet lag
    
    Change-Id: I5384372cce10d2108c246408cbd86f7babae05f2

---
+++ modules/osm/manifests/ganglia.pp
@@ -0,0 +1,22 @@
+# == Class osm::ganglia
+# This installs a Ganglia plugin for osm
+#
+class osm::ganglia( $refresh_rate = 15, $ensure = 'present') {
+    file { '/usr/lib/ganglia/python_modules/osm.py':
+        ensure => $ensure,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => "puppet:///modules/${module_name}/ganglia/osm.py",
+        notify => Service['gmond'],
+    }
+
+    file { '/etc/ganglia/conf.d/osm.pyconf':
+        ensure  => $ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('osm/ganglia/osm.pyconf.erb'),
+        notify  => Service['gmond'],
+    }
+}
