c20846d7114718b7424beb46aa2e6edd12e778e4
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2014-10-24 15:01:34 +0100

    handle file missing in projectgid.rb
    a fresh labs instance won't have /etc/ganglia/gmond.conf and thus an error is
    printed if the file is missing
    
    /bin/egrep: /etc/ganglia/gmond.conf: No such file or directory
    /bin/egrep: /etc/ganglia/gmond.conf: No such file or directory
    /bin/egrep: /etc/ganglia/gmond.conf: No such file or directory
    
    Change-Id: Ic91e07eb1e0a887bfb640a035cd7ed140f50eb9d

--- modules/base/lib/facter/projectgid.rb
+++ modules/base/lib/facter/projectgid.rb
@@ -7,6 +7,9 @@ require 'facter'
 
 Facter.add(:project_gid) do
   setcode do
+    if not File.exist? '/etc/ganglia/gmond.conf'
+      return "none"
+    end
     project_name = Facter::Util::Resolution.exec("egrep -A1 '^cluster {' /etc/ganglia/gmond.conf | awk -F '\"' '/name =/ {print $2}'").chomp
     gid = Facter::Util::Resolution.exec("getent group project-#{project_name} | cut -d : -f 3")
     if gid
