63ada93fd8dc330bebf0c40d86103c20de94cdf0
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-10-27 17:22:47 +0200

    ganglia_new: remove manutius, add uranium
    Aaaarg, ganglia_new vs ganglia vs me.
    
    Change-Id: I1a53e5e202bb7e730213e726c68d5e9e68d15f8c

--- modules/ganglia_new/manifests/configuration.pp
+++ modules/ganglia_new/manifests/configuration.pp
@@ -131,9 +131,9 @@ class ganglia_new::configuration {
             # It is not actually a gmetad host, but it should
             # be allowed to query gmond instances for use by
             # neon/icinga.
-            $gmetad_hosts = [ '208.80.152.15', '208.80.154.150', '208.80.154.14' ]
+            $gmetad_hosts = [ '208.80.154.53', '208.80.154.150', '208.80.154.14' ]
             $aggregator_hosts = {
-                'eqiad' => [ '208.80.152.15', '208.80.154.150' ],
+                'eqiad' => [ '208.80.154.53', '208.80.154.150' ],
                 'esams' => [ '91.198.174.113' ],
                 'codfw' => [ '208.80.153.4' ],
             }
