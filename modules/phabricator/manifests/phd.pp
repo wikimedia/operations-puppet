407f0219f0507756bac2e60886516dd3a9943514
cpettet <rush@wikimedia.org>
2014-10-27 12:18:54 -0500

    phab update phd configuration
    * run out of /var/run
    * use /var/log
    * use logrotate
    * set default user / paths
    
    Change-Id: I33147552e81c0fbd8e3c40d17f3f575fae6da8ff

---
+++ modules/phabricator/manifests/phd.pp
@@ -0,0 +1,34 @@
+# == Class: phabricator::phd
+#
+class phabricator::phd($settings = {}) {
+
+    file { $settings['phd.pid-directory']:
+        ensure => 'directory',
+        owner   => $settings['phd.user'],
+        group   => 'phd',
+    }
+
+    file { $settings['phd.log-directory']:
+        ensure => 'directory',
+        owner   => $settings['phd.user'],
+        group   => 'phd',
+    }
+
+    group { 'phd':
+        ensure => present,
+        system => true,
+    }
+
+    user { $settings['phd.user']:
+        gid        => 'phd',
+        shell      => '/bin/false',
+        managehome => false,
+        system     => true,
+    }
+
+    file { '/etc/logrotate.d/phd':
+        ensure  => file,
+        source  => 'puppet:///modules/phabricator/logrotate_phd',
+        mode    => '0644',
+    }
+}
