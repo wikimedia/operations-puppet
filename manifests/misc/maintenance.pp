b6c7378ad49016153dc0f5c1d00b8392cd48327f
Sean Pringle <springle@wikimedia.org>
2014-10-27 15:17:20 +1000

    Increase updatequerypages frequency to twice per month for all wikis.
    This does not increase updatequerypages::enwiki frequency. With every
    shard has a 'vslow' slave we should probably look into that too.
    
    Related to bug 46098 and bug 46094, but not a direct fix.
    
    Change-Id: I7c88eb31b2837d152e0edc4b1358816c5b3d7b01

--- manifests/misc/maintenance.pp
+++ manifests/misc/maintenance.pp
@@ -281,32 +281,32 @@ class misc::maintenance::updatequerypages( $ensure = present ) {
 
                 cron { "cron-updatequerypages-ancientpages-${name}":
                     command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php ${db_cluster}.dblist --override --only=Ancientpages > /var/log/mediawiki/updateSpecialPages/${name}-AncientPages.log 2>&1",
-                    monthday => 11,
+                    monthday => [8, 22],
                 }
 
                 cron { "cron-updatequerypages-deadendpages-${name}":
                     command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php ${db_cluster}.dblist --override --only=Deadendpages > /var/log/mediawiki/updateSpecialPages/${name}-DeadendPages.log 2>&1",
-                    monthday => 12,
+                    monthday => [9, 23],
                 }
 
                 cron { "cron-updatequerypages-mostlinked-${name}":
                     command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php ${db_cluster}.dblist --override --only=Mostlinked > /var/log/mediawiki/updateSpecialPages/${name}-MostLinked.log 2>&1",
-                    monthday => 13,
+                    monthday => [10, 24],
                 }
 
                 cron { "cron-updatequerypages-mostrevisions-${name}":
                     command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php ${db_cluster}.dblist --override --only=Mostrevisions > /var/log/mediawiki/updateSpecialPages/${name}-MostRevisions.log 2>&1",
-                    monthday => 14,
+                    monthday => [11, 25],
                 }
 
                 cron { "cron-updatequerypages-wantedpages-${name}":
                     command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php ${db_cluster}.dblist --override --only=Wantedpages > /var/log/mediawiki/updateSpecialPages/${name}-WantedPages.log 2>&1",
-                    monthday => 15,
+                    monthday => [12, 26],
                 }
 
                 cron { "cron-updatequerypages-fewestrevisions-${name}":
                     command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php ${db_cluster}.dblist --override --only=Fewestrevisions > /var/log/mediawiki/updateSpecialPages/${name}-FewestRevisions.log 2>&1",
-                    monthday => 16,
+                    monthday => [13, 27],
                 }
         }
 
@@ -323,37 +323,37 @@ class misc::maintenance::updatequerypages( $ensure = present ) {
             cron { 'cron-updatequerypages-lonelypages-s1':
                 command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php s1.dblist --override --only=Lonelypages > /var/log/mediawiki/updateSpecialPages/${name}-LonelyPages.log 2>&1",
                 month    => [1, 7],
-                monthday => 18,
+                monthday => 28,
             }
 
             cron { 'cron-updatequerypages-mostcategories-s1':
                 command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php s1.dblist --override --only=Mostcategories > /var/log/mediawiki/updateSpecialPages/${name}-MostCategories.log 2>&1",
                 month    => [2, 8],
-                monthday => 19,
+                monthday => 28,
             }
 
             cron { 'cron-updatequerypages-mostlinkedcategories-s1':
                 command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php s1.dblist --override --only=Mostlinkedcategories > /var/log/mediawiki/updateSpecialPages/${name}-MostLinkedCategories.log 2>&1",
                 month    => [3, 9],
-                monthday => 20,
+                monthday => 28,
             }
 
             cron { 'cron-updatequerypages-mostlinkedtemplates-s1':
                 command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php s1.dblist --override --only=Mostlinkedtemplates > /var/log/mediawiki/updateSpecialPages/${name}-MostLinkedTemplates.log 2>&1",
                 month    => [4, 10],
-                monthday => 21,
+                monthday => 28,
             }
 
             cron { 'cron-updatequerypages-uncategorizedcategories-s1':
                 command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php s1.dblist --override --only=Uncategorizedcategories > /var/log/mediawiki/updateSpecialPages/${name}-UncategorizedCategories.log 2>&1",
                 month    => [5, 11],
-                monthday => 22,
+                monthday => 28,
             }
 
             cron { 'cron-updatequerypages-wantedtemplates-s1':
                 command  => "/usr/local/bin/mwscriptwikiset updateSpecialPages.php s1.dblist --override --only=Wantedtemplates > /var/log/mediawiki/updateSpecialPages/${name}-WantedTemplates.log 2>&1",
                 month    => [6, 12],
-                monthday => 23,
+                monthday => 28,
             }
         }
 
