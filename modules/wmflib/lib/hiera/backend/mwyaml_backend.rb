57b9f91795ba714b90023d78dd31d814c7973e3e
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-10-27 17:23:03 +0100

    hiera: mediawiki-based backend for labs
    This backend will retreive its data from the mediawiki api, and will
    cache results absolutely for cache_ttl seconds not to kill the backend,
    and while they're not changed (based on the mediawiki revision) in order
    to speed up the process.
    
    It will be processed for all labs instances, after the traditional yaml
    backend we use for prod as well.
    
    Change-Id: I3710fdb23828693701025aa3879dd4a5813d4088
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/wmflib/lib/hiera/backend/mwyaml_backend.rb
@@ -0,0 +1,49 @@
+require "hiera/mwcache"
+class Hiera
+  module Backend
+    class Mwyaml_backend
+      def initialize(cache=nil)
+        @cache = cache || Mwcache.new
+      end
+
+      def lookup(key, scope, order_override, resolution_type)
+        answer = nil
+        Hiera.debug("Looking up #{key}")
+
+        Backend.datasources(scope, order_override) do |source|
+          # Small hack: - we don't want to search any datasource but the
+          # labs/%{::instanceproject} hierarchy here; so we plainly exit
+          # in any other case
+          if m = /labs\/([^\/]+)$/.match(source)
+            source = m[1].capitalize
+          else
+            next
+          end
+          data = @cache.read(source, Hash, {}) do |content|
+            YAML.load(content)
+          end
+
+          next if data.empty?
+          next unless data.include?(key)
+
+          new_answer = Backend.parse_answer(data[key], scope)
+          case resolution_type
+          when :array
+            raise Exception, "Hiera type mismatch: expected Array and got #{new_answer.class}" unless new_answer.kind_of? Array or new_answer.kind_of? String
+            answer ||= []
+            answer << new_answer
+          when :hash
+            raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
+            answer ||= {}
+            answer = Backend.merge_answer(new_answer,answer)
+          else
+            answer = new_answer
+            break
+          end
+        end
+
+        return answer
+      end
+    end
+  end
+end
