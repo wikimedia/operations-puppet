9a90c2cd5cb496dec2a8b906a2eb201b13a0ec00
Ori Livneh <ori@wikimedia.org>
2014-10-29 14:43:19 -0700

    Add tmpreaper module
    Adds a tmpreaper module with two custom resource types:
    
    * tmpreaper::dir adds a directory to the set of directories which are 'reaped'
      by tmpreaper during its daily cron run. It does so by appending the directory
      to TMPREAPER_DIRS in /etc/tmpreaper.conf.
    * tmpreaper::reap allows a path to be reaped by Puppet, like Puppet's native
      'tidy' resource, except not insanely broken. This may be used when the age
      setting or other settings in /etc/tmpreaper.conf (which are applide to all
      directories) are not optimal for the given path.
    
    Change-Id: Ibc013bbf58340664213273bbb97954e5a1e08f9c

---
+++ modules/tmpreaper/manifests/init.pp
@@ -0,0 +1,24 @@
+# == Class: tmpreaper
+#
+# This module provides a simple custom resource type for using
+# tmpreaper. tmpreaper recursively searches for and removes files
+# and empty directories which haven't been accessed for a period
+# time.
+#
+class tmpreaper {
+    package { 'tmpreaper':
+        ensure => present,
+    }
+
+    # tmpreaper's cron.daily script declines to run unless the line
+    # below is removed from its config file, indicating that the user
+    # understands the security implications of having tmpreaper run
+    # automatically. See /usr/share/doc/tmpreaper/README.security.gz .
+
+    file_line { 'load_env_enabled':
+        ensure  => absent,
+        line    => 'SHOWWARNING=true',
+        path    => '/etc/tmpreaper.conf',
+        require => Package['tmpreaper'],
+    }
+}
