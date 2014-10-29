369aaf92e35fb7e398414851e8348cff74843f8b
Andrew Otto <acotto@gmail.com>
2014-10-29 11:23:27 -0400

    Add defines for working with mysql config files, and mysql client settings
    Change-Id: I81cf2238346455f229a5cdb79ea93371fdcce2af

---
+++ modules/mysql/manifests/config/client.pp
@@ -0,0 +1,54 @@
+# == Define mysql::config::client
+# Convenience wrapper for mysql::config::file that only set a few [client] settings.
+#
+#
+# Usage:
+#   mysql::config::client { 'myuser':
+#       user => 'myser',
+#       pass => 'mypass',
+#   }
+#
+# This will render /etc/mysql/conf.d/myuser-client.cnf with the content:
+#
+#   [client]
+#   user = myuser
+#   password = mypass
+#   #host=
+#   #port=
+#
+# You can also set host and port if you like.
+#
+define mysql::config::client(
+    $user     = false,
+    $pass     = false,
+    $host     = false,
+    $port     = false,
+    $path     = "/etc/mysql/conf.d/${title}.cnf",
+    $owner    = 'root',
+    $group    = 'root',
+    $mode     = '0444',
+    $template = 'mysql/my.conf.cnf.erb',
+    $ensure   = 'present',
+)
+{
+    if !($user or $pass or $host or $port) {
+        fail('mysql::config::client needs at least one of user, pass, host, or port to be set.')
+    }
+
+    mysql::config::file { "${title}-client":
+        ensure     => $ensure,
+        settings => {
+            'client' => {
+                'user'     => $user,
+                'password' => $pass,
+                'host'     => $host,
+                'port'     => $port,
+            },
+        },
+        path       => $path,
+        owner      => $owner,
+        group      => $group,
+        mode       => $mode,
+        template   => $template,
+    }
+}
---
+++ modules/mysql/manifests/config/file.pp
@@ -0,0 +1,39 @@
+# == Define mysql::config::file
+#
+# == Usage
+#   mysql::config::file { 'myhost':
+#       'settings' => {
+#           'client' => {
+#               'host' => 'myhost.example.org',
+#               'port' => 3307,
+#           },
+#       },
+#   }
+#
+# This will render /etc/mysql/conf.d/myhost.cnf with the content:
+#
+#   [client]
+#   host = myhost.example.org
+#   port = 3307
+#
+# You could then use this file with the mysql CLI by doing:
+#   mysql --defaults-extra-file=/etc/mysql/conf.d/myhost.cnf
+#
+define mysql::config::file(
+    $settings,
+    $path     = "/etc/mysql/conf.d/${title}.cnf",
+    $owner    = 'root',
+    $group    = 'root',
+    $mode     = '0444',
+    $template = 'mysql/my.conf.cnf.erb',
+    $ensure   = 'present',
+)
+{
+    file { $path:
+        ensure  => $ensure,
+        owner   => $owner,
+        group   => $group,
+        mode    => $mode,
+        content => template($template),
+    }
+}
