a14a2e38ea67668215715f05e5e2ac73cd76f035
Faidon Liambotis <faidon@wikimedia.org>
2014-10-29 01:40:25 +0000

    Revert "Add ::apt to stage => first"
    Creates a dependency cycle.
    
    This reverts commit 134de68a53d7d842490ad7579771637c9e20808f.
    
    Change-Id: Iecc000fd0c93a428af9c9e8ea2aefa0dbe03313d

--- manifests/stages.pp
+++ manifests/stages.pp
@@ -1,21 +1,6 @@
-# == Manifest: ::stages
-#
-# Puppet segments its run into run stages. By default, there is only
-# the 'main' stage. We add a 'first' stage (which runs before 'main')
-# and a 'last' stage (which runs after it) as a way of ensuring that
-# certain actions happen at the very beginning or very end of a run.
-#
+stage { 'first': before => Stage[main] }
+stage { 'last': require => Stage[main] }
 
-## Stages
-
-stage { 'first': before => Stage['main'], }
-stage { 'last': require => Stage['main'], }
-
-
-## Run first
-
-class { '::apt': stage => 'first', } ->
-class { '::apt::update': stage => 'first', }
-
-
-## Run last
+class {
+    'apt::update': stage => first;
+}
