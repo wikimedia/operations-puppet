wikimedia/operations-puppet

commit 2d96ee88b56a27dc6fc66b927184ca81ab1dc2f3

osm export the expired tile list
use the rsync module and define a repo to export to labs vms the list of
expired tiles.
While at it, do some minor reorganization of the osm module

Change-Id: I4b686216d167b572e7333521ab54d4e974d0facc

manifests/role/osm.pp
@@ -27,7 +27,7 @@ class role::osm::common {
 class role::osm::master {
     include role::osm::common
     include postgresql::postgis
-    include osm::packages
+    include osm
     include passwords::osm
 
     class { 'postgresql::master':
@@ -141,6 +141,26 @@ class role::osm::master {
             type     => 'host',
             method   => 'md5',
             database => 'wikimaps_atlas',
+    }
+
+    include rsync::server
+    rsync::server::module { 'osm_expired_tiles':
+        path    => '/srv/osm_expire',
+        comment => 'OpenStreetMap expired tile list',
+        uid     => 'postgres',
+        gid     => 'postgres',
+    }
+
+    ferm::service { 'rsync_from_labs':
+        desc   => 'Allow labs machines to get the expired OSM tile list',
+        prio   => '50',
+        proto  => 'tcp',
+        port   => 873,
+        srange => '($EQIAD_PRIVATE_LABS-INSTANCES1-A-EQIAD $EQIAD_PRIVATE_LABS-INSTANCES1-A-EQIAD $EQIAD_PRIVATE_LABS-INSTANCES1-A-EQIAD $EQIAD_PRIVATE_LABS-INSTANCES1-A-EQIAD)',
+    }
+    nrpe::monitor_service { 'check_rsync_server_running':
+        description => 'Check if rsync server is running',
+        command     => "/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:4 --ereg-argument-array 'rsync --daemon'",
     }
 }
 

modules/osm/manifests/init.pp
@@ -1,6 +1,6 @@
 #
 
-class osm::packages($ensure='present') {
+class osm($ensure='present') {
     package { [
         'osm2pgsql',
         'osmosis',

modules/osm/spec/classes/osm_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'osm::packages', :type => :class do
+describe 'osm', :type => :class do
     let(:params) { {
         :ensure           => 'present',
         }
@@ -12,7 +12,7 @@ describe 'osm::packages', :type => :class do
     end
 end
 
-describe 'osm::packages', :type => :class do
+describe 'osm', :type => :class do
     let(:params) { {
         :ensure           => 'absent',
         }

commit 1b839d5d82566b1b3a009ea4590012d4ac420db4

osm: Fix typo introduced in 2d96ee8
command => nrpe_command

Change-Id: I57a795547f8e98d78802021d4287d48be70267c8

manifests/role/osm.pp
@@ -159,8 +159,8 @@ class role::osm::master {
         srange => '($EQIAD_PRIVATE_LABS-INSTANCES1-A-EQIAD $EQIAD_PRIVATE_LABS-INSTANCES1-A-EQIAD $EQIAD_PRIVATE_LABS-INSTANCES1-A-EQIAD $EQIAD_PRIVATE_LABS-INSTANCES1-A-EQIAD)',
     }
     nrpe::monitor_service { 'check_rsync_server_running':
-        description => 'Check if rsync server is running',
-        command     => "/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:4 --ereg-argument-array 'rsync --daemon'",
+        description  => 'Check if rsync server is running',
+        nrpe_command => "/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:4 --ereg-argument-array 'rsync --daemon'",
     }
 }
 