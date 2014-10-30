wikimedia/operations-puppet

commit 9e84081616e32443ee4cb679dc8386d24d02b0d7

Backup user home dirs
Suggested hosts are: bast1001, iron, tin, terbium. Ganglia study showed
a total of 1.3TB total usage and zero growth rate on these hosts. This
is total disk space usage (including home directories) so actual home
dir usage is lower

Change-Id: I4adf7407ba3550b6ecaea939d978b1656ff7504b

manifests/site.pp
@@ -359,6 +359,8 @@ node 'bast1001.wikimedia.org' {
         mountpoint => '/srv/home_pmtpa',
         mount_site => 'pmtpa',
     }
+    include role::backup::host
+    backup-set {'home': }
 }
 
 node 'bast2001.wikimedia.org' {
@@ -1336,6 +1338,8 @@ node 'iron.wikimedia.org' {
     include role::ipmi
     include role::access_new_install
 
+    include role::backup::host
+    backup-set {'home': }
 }
 
 
@@ -2630,6 +2634,8 @@ node 'terbium.eqiad.wmnet' {
         ensure => installed,
     }
 
+    include role::backup::host
+    backup-set {'home': }
 }
 
 node /^elastic10[0-3][0-9]\.eqiad\.wmnet/ {
@@ -2707,6 +2713,8 @@ node 'tin.eqiad.wmnet' {
     interface::add_ip6_mapped { 'main':
         interface => 'eth0',
     }
+    include role::backup::host
+    backup-set {'home': }
 }
 
 # titanium hosts archiva.wikimedia.org

commit 6492faebf462f8969cb36808e0ca7b5ac772579f

Fix typo with backup::set
Change-Id: I82b849db9a6ed38edb82d00b139af27f6088442a
backup::set, not backup-set. Regression was introduced in 9e84081

manifests/site.pp
@@ -359,7 +359,7 @@ node 'bast1001.wikimedia.org' {
         mount_site => 'pmtpa',
     }
     include role::backup::host
-    backup-set {'home': }
+    backup::set {'home': }
 }
 
 node 'bast2001.wikimedia.org' {
@@ -1338,7 +1338,7 @@ node 'iron.wikimedia.org' {
     include role::access_new_install
 
     include role::backup::host
-    backup-set {'home': }
+    backup::set {'home': }
 }
 
 
@@ -2634,7 +2634,7 @@ node 'terbium.eqiad.wmnet' {
     }
 
     include role::backup::host
-    backup-set {'home': }
+    backup::set {'home': }
 }
 
 node /^elastic10[0-3][0-9]\.eqiad\.wmnet/ {
@@ -2713,7 +2713,7 @@ node 'tin.eqiad.wmnet' {
         interface => 'eth0',
     }
     include role::backup::host
-    backup-set {'home': }
+    backup::set {'home': }
 }
 
 # titanium hosts archiva.wikimedia.org