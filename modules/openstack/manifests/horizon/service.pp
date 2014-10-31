ca1b395e31c51fc1755ad7cb80a3cd8dabeea938
Andrew Bogott <abogott@wikimedia.org>
2014-10-31 15:58:08 +0000

    Add class and role for Openstack Horizon
    This should be able to coexist with mediawiki and OpenStackManager
    on virt1000.
    
    Change-Id: Icc77b19820e562e9374311e69f1257cdbb223531

---
+++ modules/openstack/manifests/horizon/service.pp
@@ -0,0 +1,50 @@
+class openstack::horizon::service($openstack_version='havana', $novaconfig) {
+
+    # basic horizon packages and config
+    if ! defined(Class['openstack::repo']) {
+        class { 'openstack::repo': openstack_version => $openstack_version }
+    }
+
+    package { [ 'openstack-dashboard' ]:
+        ensure  => present,
+        require => Class['openstack::repo', 'webserver::php5', 'apache::mod::wsgi'];
+    }
+
+    # web services to host horizon
+    if !defined(Class['webserver::php5']) {
+        class {'webserver::php5': ssl => true; }
+    }
+
+    include apache::mod::wsgi
+
+    if !defined(Class['memcached']) {
+        class { 'memcached':
+            memcached_ip => '127.0.0.1',
+            pin          => true;
+        }
+    }
+
+    # This is installed by the openstack-dashboard package,
+    #  it interferes with our managed 'horizon.conf'
+    file {['/etc/apache2/conf.d/openstack-dashboard.conf',
+          '/etc/apache2/conf-available/openstack-dashboard.conf',
+          '/etc/apache2/conf-enabled/openstack-dashboard.conf']:
+        ensure   => absent,
+        require  => Package['openstack-dashboard'],
+    }
+
+    file {
+        '/etc/openstack-dashboard/local_settings.py':
+            content => template("openstack/${$openstack_version}/horizon/local_settings.py.erb"),
+            owner   => 'horizon',
+            group   => 'horizon',
+            notify  => Service['apache2'],
+            require => File['/etc/apache2/conf.d/openstack-dashboard.conf'],
+            mode    => '0440';
+    }
+
+    apache::conf { 'horizon':
+        content => template("openstack/${$openstack_version}/horizon/openstack-dashboard.conf.erb"),
+        require => File['/etc/openstack-dashboard/local_settings.py'],
+    }
+}
