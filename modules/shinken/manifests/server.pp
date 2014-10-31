3157a846d53507ca2c1456f29029a6bb1ab145db
dzahn <dzahn@wikimedia.org>
2014-10-30 17:41:19 -0700

    shinken - move hosts define to own file
    move the 'hosts' define into it's own file,
    per module conventions defines should have their
    own files like classes or "not in autoload module layout"
    
    Change-Id: Icb15df6f910dcd0cdd81c9cd546a71f48051ce25

--- modules/shinken/manifests/server.pp
+++ modules/shinken/manifests/server.pp
@@ -102,19 +102,3 @@ class shinken::server(
     }
 }
 
-# = Define: shinken::hosts
-# Setup a shinken hosts definition file
-# FIXME: Autogenerate hosts definitions later on
-define shinken::hosts(
-    $ensure  = present,
-    $source  = undef,
-) {
-    file { "/etc/shinken/hosts/$title.cfg":
-        ensure  => $ensure,
-        source  => $source,
-        owner   => 'shinken',
-        group   => 'shinken',
-        notify  => Service['shinken'],
-        require => Package['shinken']
-    }
-}
