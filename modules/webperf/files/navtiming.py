f9b24204b1546e70bdea78370aab2f62f8ab412c
Ori Livneh <ori@wikimedia.org>
2014-10-30 17:05:33 -0700

    webperf: refactor navtiming.py
    This script grew like a wild weed. In this patch I try to rewrite the ad-hoc
    metric processing into something vaguely generic.
    
    Change-Id: I89372aa69ec2c926ba2910d435031da0bad54eb8

--- modules/webperf/files/navtiming.py
+++ modules/webperf/files/navtiming.py
@@ -12,12 +12,6 @@ import socket
 import zmq
 
 
-metrics = ('connecting', 'sending', 'waiting', 'redirecting', 'receiving',
-           'rendering', 'loading', 'dnsLookup', 'pageSpeed',
-           'totalPageLoadTime', 'mediaWikiLoadComplete', 'sslNegotiation')
-prefix = 'frontend.navtiming'
-
-
 ap = argparse.ArgumentParser(description='NavigationTiming Graphite module')
 ap.add_argument('endpoint', help='URI of EventLogging endpoint')
 ap.add_argument('--statsd-host', default='localhost',
@@ -39,77 +33,92 @@ zsock.subscribe = b''
 addr = args.statsd_host, args.statsd_port
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+handlers = {}
+
 
 def dispatch_stat(*args):
-    if len(args) < 2:
-        raise argparse.ArgumentError
     args = list(args)
     value = args.pop()
-    name = '.'.join([prefix] + args)
+    name = '.'.join(args)
     stat = '%s:%s|ms' % (name, value)
     sock.sendto(stat.encode('utf-8'), addr)
 
 
+def handles(schema):
+    def wrapper(f):
+        handlers[schema] = f
+        return f
+    return wrapper
+
+
+def is_sane(value):
+    return isinstance(value, int) and value > 0 and value < 60000
+
+
+@handles('SaveTiming')
 def handle_save_timing(meta):
     event = meta['event']
     duration = event['duration']
     runtime = event['runtime']
-    if duration > 0 and duration < 60000:
-        stat = 'mw.performance.save.%s:%s|ms' % (runtime, duration)
-        sock.sendto(stat.encode('utf-8'), addr)
+    if is_sane(duration):
+        dispatch_stat('mw.performance.save', runtime, duration)
 
 
+@handles('NavigationTiming')
 def handle_navigation_timing(meta):
     event = meta['event']
+    metrics = {}
 
-    if 'fetchStart' in event:
-        event['sending'] = event['fetchStart']
-    if 'loadEventStart' in event:
-        event['loading'] = event['loadEventStart']
-    if 'responseStart' in event and 'requestStart' in event:
-        event['waiting'] = event['responseStart'] - event['requestStart']
-    if 'connectEnd' in event and 'connectStart' in event:
-        event['connecting'] = event['connectEnd'] - event['connectStart']
-    if 'responseEnd' in event and 'responseStart' in event:
-        event['receiving'] = event['responseEnd'] - event['responseStart']
-    if 'loadEventEnd' in event and 'responseEnd' in event:
-        event['rendering'] = event['loadEventEnd'] - event['responseEnd']
-    if 'loadEventEnd' in event and 'domInteractive' in event:
-        event['pageSpeed'] = (
-            event['loadEventEnd'] - event['domInteractive'])
-    if 'loadEventEnd' in event:
-        event['totalPageLoadTime'] = event['loadEventEnd']
-    if 'connectEnd' in event and 'secureConnectionStart' in event:
-        event['sslNegotiation'] = (
-            event['connectEnd'] - event['secureConnectionStart'])
+    for metric, marker in (
+        ('dnsLookup', 'dnsLookup'),
+        ('loading', 'loadEventStart'),
+        ('mediaWikiLoadComplete', 'mediaWikiLoadComplete'),
+        ('redirecting', 'redirecting'),
+        ('sending', 'fetchStart'),
+        ('totalPageLoadTime', 'loadEventEnd')
+    ):
+        if marker in event:
+            metrics[metric] = event[marker]
+
+    for difference, minuend, subtrahend in (
+        ('waiting', 'responseStart', 'requestStart'),
+        ('connecting', 'connectEnd', 'connectStart'),
+        ('receiving', 'responseEnd', 'responseStart'),
+        ('rendering', 'loadEnd', 'responseEnd'),
+        ('pageSpeed', 'loadEnd', 'domInteractive'),
+        ('sslNegotiation', 'connectEnd', 'secureConnectionStart'),
+    ):
+        if minuend in event and subtrahend in event:
+            metrics[difference] = event[minuend] - event[subtrahend]
 
     site = 'mobile' if 'mobileMode' in event else 'desktop'
     auth = 'anonymous' if event.get('isAnon') else 'authenticated'
     https = 'https' if event.get('isHttps') else 'http'
     runtime = event.get('runtime')
 
-    # bits_cache = meta.get('recvFrom', '').split('.')[0]
-    # wiki = meta.get('wiki', '')
+    # Current unused:
+    bits_cache = meta.get('recvFrom', '').split('.')[0]
+    wiki = meta.get('wiki', '')
 
-    for metric in metrics:
-        value = event.get(metric, 0)
-        if value > 0 and value < 60000:
-            dispatch_stat(metric, site, auth, value)
-            dispatch_stat(metric, site, 'overall', value)
-            dispatch_stat(metric, 'overall', value)
+    metrics = {k: v for k, v in metrics.items() if is_sane(v)}
+    prefix = 'frontend.navtiming'
 
-            if metric == 'connecting':
-                dispatch_stat(metric, site, https, value)
+    for metric, value in metrics.items():
+        dispatch_stat(prefix, metric, site, auth, value)
+        dispatch_stat(prefix, metric, site, 'overall', value)
+        dispatch_stat(prefix, metric, 'overall', value)
 
-            if runtime is not None:
-                # PHP5/HHVM-qualified metrics
-                dispatch_stat(runtime, metric, site, auth, value)
-                dispatch_stat(runtime, metric, site, 'overall', value)
-                dispatch_stat(runtime, metric, 'overall', value)
+        if metric == 'connecting':
+            dispatch_stat(prefix, metric, site, https, value)
+
+        if runtime:
+            # PHP5/HHVM-qualified metrics
+            dispatch_stat(prefix, runtime, metric, site, auth, value)
+            dispatch_stat(prefix, runtime, metric, site, 'overall', value)
+            dispatch_stat(prefix, runtime, metric, 'overall', value)
 
 
 for meta in iter(zsock.recv_json, ''):
-    if meta['schema'] == 'NavigationTiming':
-        handle_navigation_timing(meta)
-    if meta['schema'] == 'SaveTiming':
-        handle_save_timing(meta)
+    f = handlers.get(meta['schema'])
+    if f is not None:
+        f(meta)
