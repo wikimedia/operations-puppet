72cd162d8cf9a2d79ecdd6c0c6d565766bc41304
dzahn <dzahn@wikimedia.org>
2014-10-31 18:14:58 -0700

    openstack: lint fixes
    Change-Id: I897642c391f5e5b036a7a2defe4cbf1ea92ef965

--- modules/openstack/manifests/repo.pp
+++ modules/openstack/manifests/repo.pp
@@ -1,9 +1,9 @@
-class openstack::repo($openstack_version="folsom") {
+class openstack::repo($openstack_version='folsom') {
 
     if ($::lsbdistcodename == 'precise') {
         apt::repository { 'ubuntucloud':
             uri        => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-            dist       => "precise-updates/${openstack_version}",
+            dist       => 'precise-updates/${openstack_version}',
             components => 'main',
             keyfile    => 'puppet:///files/misc/ubuntu-cloud.key';
         }
