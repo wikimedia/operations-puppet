848f56b6a96f636e6bc10d1a5ced2fc83464d252
Christian Aistleitner <christian@quelltextlich.at>
2014-11-03 09:14:23 +0100

    Install maven on analytics clients
    Using maven, people no longer need to upload >50MB jars again and
    again when developing for the Analytics cluster, but they can build
    the jars locally.
    
    The system-wide default is to fetch artifacts through our archiva. But
    this is not bullet-proof, as people can override those defaults in
    user settings and use http_proxies to fetch from the web directly.
    
    RT: 8765
    Change-Id: I97e9e5b8e20afedfa66a2da129aa3cae1b626378

---
+++ modules/maven/init.pp
@@ -0,0 +1,19 @@
+# == Class maven
+# Install maven and sets it up with default mirroring to
+# archiva.wikimedia.org
+class maven {
+    if !defined(Package['maven']) {
+        package { 'maven':
+            ensure => present,
+        }
+    }
+
+    file { '/etc/maven/settings.xml':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        source  => 'puppet:///files/analytics/maven/settings.xml',
+        require => Package['maven'],
+    }
+}
