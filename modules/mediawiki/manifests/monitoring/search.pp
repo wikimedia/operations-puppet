d8c6c31322eb7c4762fa061e19b6bc481cc20b1a
dzahn <dzahn@wikimedia.org>
2014-11-04 19:06:10 -0800

    add monitoring for search.wm (Apple dict bridge)
    see RT #6128 for the request for this to be added
    
    adding the check to the mediawiki module, not to
    search module, because search.wikmedia.org is the
    Apple dict bridge and the Apache site for it is configured
    in mw module and unrelated to elastic
    
    adds a virtual host "search.wikimedia.org" to Icinga to
    add the service to, checks this URL
    
    https://search.wikimedia.org/?lang=en&site=wikipedia&search=Wikimedia_Foundation&limit=1
    
    for the string "Wikimedia_Foundation"
    
    Change-Id: I69537a84ba225b51900ae1078cce88ec25d3b514
    RT: 6128

---
+++ modules/mediawiki/manifests/monitoring/search.pp
@@ -0,0 +1,16 @@
+# monitor the Apple dictionary bridge (RT #6128)
+class mediawiki::monitoring::search {
+
+    # add virtual host for icinga to add the service to
+    @monitor_host { 'search.wikimedia.org':
+        ip_address => '208.80.154.224',
+    }
+
+    # https://search.wikimedia.org/?lang=en&site=wikipedia&search=Wikimedia_Foundation&limit=1
+    monitor_service { 'mediawiki-dict-bridge':
+        description   => 'Mediawiki Apple Dictionary Bridge',
+        check_command => 'check_https_dictbridge',
+    }
+
+}
+
