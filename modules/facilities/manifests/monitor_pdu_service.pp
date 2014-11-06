315c0663a6a4ece2a0dae355b4dda76f55d12765
dzahn <dzahn@wikimedia.org>
2014-11-05 18:23:24 -0800

    (WIP) facilities: move to module
    this includes PDU monitoring and an (unused?)
    class for DC camera transcoders
    
    move it from manifests to module structure
    
    should the PDU monitoring stuff be included
    in one of the other monitorin modules instead?
    
    if we'd do that and don't need the "dc-camera" class
    then this module isn't needed
    
    Change-Id: I14ccfae3b714a6b72101c87480d7fe54367ba3fc

---
+++ modules/facilities/manifests/monitor_pdu_service.pp
@@ -0,0 +1,39 @@
+# facilities.pp
+# definition for monitoring PDUs via SNMP
+# RT #308
+# TODO: Monitor infeed status
+define monitor_pdu_service(
+    $host,
+    $ip,
+    $tower,
+    $infeed,
+    $breaker  = '30',
+    $redundant= true
+) {
+
+    include passwords::nagios::snmp
+
+    $servertech_tree = '.1.3.6.1.4.1.1718'
+    $infeedLoad      = '.3.2.2.1.7'
+    $oid = "${servertech_tree}${infeedLoad}.${tower}.${infeed}"
+
+    # The value of infeedLoadValue is given in _hundredths of Amps_,
+    # thats why we multiply here
+
+    if $redundant == false {
+        $warn_hi = $breaker * 0.8 * 100
+        $crit_hi = $breaker * 0.85 * 100
+    } else {
+        $warn_hi = $breaker * 0.4 * 100
+        $crit_hi = $breaker * 0.8 * 100
+    }
+
+    @monitor_service { $title:
+        host          => $host,
+        group         => 'pdus',
+        description   => $title,
+        check_command => "check_snmp_generic!${passwords::nagios::snmp::pdu_snmp_pass}!${oid}!${title}!${warn_hi}!${crit_hi}",
+    }
+
+}
+
