3bd10502d0d6f547238e477bc6c68b05e7fcd514
Gabriel Wicke <wicke@wikidev.net>
2014-11-06 14:16:57 -0800

    WIP: Add restbase role
    Change-Id: Ia607fb50e2ec9066baa0df5306f2a07e24be30e1

---
+++ manifests/role/restbase.pp
@@ -0,0 +1,25 @@
+# == Class role::restbase
+#
+
+class role::restbase::labs::otto_cass {
+    system::role{ 'restbase': description => 'restbase labs::otto_cass' }
+
+    class '::restbase': {
+        case $::realm {
+            'labs': {
+                seeds => ['10.68.17.68','10.68.17.60','10.68.17.71'],
+                cassandra_password => 'test', # $::passwords::cassandra::otto-cass::password,
+                cassandra_password => 'test', # $::passwords::cassandra::otto-cass::password,
+            }
+            #'production': {
+            #    $wikistats_host = 'wikistats.wikimedia.org'
+            #    seeds => ,
+            #    cassandra_password => $::passwords::cassandra::otto-cass::password,
+            #    cassandra_password => $::passwords::cassandra::otto-cass::password,
+            #}
+            default: {
+                fail('unknown realm, should be labs or production')
+            }
+        }
+    }
+}
