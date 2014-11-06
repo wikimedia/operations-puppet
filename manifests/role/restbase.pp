ab3a6b417aa4a03b328db236c6254c5495dba57e
Gabriel Wicke <wicke@wikidev.net>
2014-11-06 14:16:57 -0800

    Add a simple restbase::labs role
    Change-Id: Ia607fb50e2ec9066baa0df5306f2a07e24be30e1

---
+++ manifests/role/restbase.pp
@@ -0,0 +1,24 @@
+# == Class role::restbase
+#
+
+# Labs test role
+# Config should be pulled from hiera
+class role::restbase::labs (
+    cassandra_user     = 'cassandra',
+    cassandra_password = 'cassandra',
+    cassandra_defaultConsistency = 'localQuorum',
+    logstash_host      = 'deployment-logstash1.eqiad.wmflabs',
+    logstash_port      = 12201,
+    seeds              = [ 'localhost' ],
+) {
+    system::role { 'restbase': description => 'restbase labs' }
+
+    class { '::restbase':
+        cassandra_user      => $cassandra_user,
+        cassandra_password  => $cassandra_password,
+        cassandra_defaultConsistency => $cassandra_defaultConsistency,
+        logstash_host       => $logstash_host,
+        logstash_port       => $logstash_port,
+        seeds               => $seeds,
+    }
+}
