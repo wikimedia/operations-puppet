9b6929344b87c7011edfff82879828251de7b614
Andrew Bogott <abogott@wikimedia.org>
2014-11-07 21:55:34 +0000

    Use the ubuntu cloud archive for all Ubuntu versions
    I don't know why this was only marked for 'precise' --
    as we move to Trusty we still want the latest packages.
    
    Change-Id: If191bab1886881dc33f33df39816ad961cb7e754

--- modules/openstack/manifests/repo.pp
+++ modules/openstack/manifests/repo.pp
@@ -1,13 +1,10 @@
 class openstack::repo(
     $openstack_version=$::openstack::version,
 ) {
-
-    if ($::lsbdistcodename == 'precise') {
-        apt::repository { 'ubuntucloud':
-            uri        => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-            dist       => "precise-updates/${openstack_version}",
-            components => 'main',
-            keyfile    => 'puppet:///files/misc/ubuntu-cloud.key';
-        }
+    apt::repository { 'ubuntucloud':
+        uri        => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
+        dist       => "precise-updates/${openstack_version}",
+        components => 'main',
+        keyfile    => 'puppet:///files/misc/ubuntu-cloud.key';
     }
 }
