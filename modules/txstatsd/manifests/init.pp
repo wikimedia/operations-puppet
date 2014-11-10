c1a71a20c8c74d004a9b48fa279933fab81c9430
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2014-11-10 16:10:30 +0000

    txstats/graphite: fix package conflict, use require_package
    Change-Id: Ie7dd5ad5d7437ee36ad056aa72a8f05153524cd2

--- modules/txstatsd/manifests/init.pp
+++ modules/txstatsd/manifests/init.pp
@@ -23,7 +23,7 @@
 #  }
 #
 class txstatsd($settings) {
-    package { [ 'python-txstatsd', 'python-twisted-web', 'graphite-carbon' ]: }
+    require_package(['python-txstatsd', 'python-twisted-web', 'graphite-carbon'])
 
     file { '/etc/init/txstatsd.conf':
         source => 'puppet:///modules/txstatsd/txstatsd.conf',
