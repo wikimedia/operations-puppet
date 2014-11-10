4ef76872d2b00822a4257ad476c2af98bd0c2268
YuviPanda <yuvipanda@gmail.com>
2014-11-11 03:47:18 +0530

    [WIP] shinken: Add basic service checks for all of labs
    Change-Id: I61197ddc264eb4920ef71f41bf2013b9810e77b7

---
+++ modules/shinken/manifests/services.pp
@@ -0,0 +1,17 @@
+# = Define: shinken::services
+# Setup a shinken services definition file
+define shinken::services(
+    $ensure  = present,
+    $source  = undef,
+) {
+    include shinken::server
+
+    file { "/etc/shinken/services/$title.cfg":
+        ensure  => $ensure,
+        source  => $source,
+        owner   => 'shinken',
+        group   => 'shinken',
+        notify  => Service['shinken'],
+        require => Package['shinken']
+    }
+}
