95091dc29542dfa4666b4fe80a8128e4ae59f501
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-11-11 15:09:45 +0200

    Introduce role::ganglia classes
    Introducing role classes for ganglia, based on the ganglia_new module
    which also gets some new functionality. For now the role::ganglia::web
    exists only but more stuff needs to be migrated in the future.
    
    Update ganglia_new module to support:
    * gmetad configuration
    * rrdcached
    * Override the default gmetad upstart script for RRDCACHED support
    * gmetad configuration in the role
    * Some preliminary tests support
    * ganglia web frontend configuration
    
    Change-Id: Iedd12f64b8a733c8c9c7df706d1c33397754cc37

---
+++ modules/ganglia_new/manifests/web.pp
@@ -0,0 +1,41 @@
+# Class for the ganglia frontend machine
+class ganglia_new::web(
+                    $ensure='present',
+                    $rrdcached_socket,
+                    $gmetad_root,
+) {
+    include ::apache
+    include ::apache::mod::php5
+    include ::apache::mod::ssl
+    include ::apache::mod::rewrite
+
+    $ganglia_servername = 'ganglia.wikimedia.org'
+    $ganglia_serveralias = 'nickel.wikimedia.org uranium.wikimedia.org'
+    $ganglia_webdir = '/usr/share/ganglia-webfrontend'
+    $ganglia_confdir = '/var/lib/ganglia-web'
+    $ganglia_ssl_cert = '/etc/ssl/certs/ganglia.wikimedia.org.pem'
+    $ganglia_ssl_key = '/etc/ssl/private/ganglia.wikimedia.org.key'
+    $ssl_settings = ssl_ciphersuite('apache-2.2', 'compat')
+
+    package { [ 'php5-gd',
+                'php5-mysql',
+                'rrdtool',
+                'librrds-perl',
+                'ganglia-webfrontend',
+            ]:
+        ensure => $ensure,
+    }
+
+    apache::site { $ganglia_servername:
+        content => template("ganglia_new/${ganglia_servername}.erb"),
+    }
+
+    file { '/etc/ganglia-webfrontend/conf.php':
+        ensure  => $ensure,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => template('ganglia_new/conf_production.php.erb'),
+        require => Package['ganglia-webfrontend'],
+    }
+}
