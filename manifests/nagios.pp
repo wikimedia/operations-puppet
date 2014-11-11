4e8c9e403d010e91b126fe572e4c56c86b226cba
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-11-11 13:27:22 +0100

    puppet: get rid of the nagios_group global variable
    Change-Id: I8fbef8279d4b7d00997086a2851cb65dcd898823
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -9,7 +9,7 @@ define monitor_service(
     $check_command,
     $host                  = $::hostname,
     $retries               = 3,
-    $group                 = undef,
+    $group                 = hiera('nagios_group', undef),
     $ensure                = present,
     $critical              = 'false',
     $passive               = 'false',
@@ -23,55 +23,44 @@ define monitor_service(
         fail("Parameter $host not defined!")
     }
 
-    if $group != undef {
-        $servicegroup = $group
-    }
-    elsif $nagios_group != undef {
-        # nagios group should be defined at the node level with hiera.
-        $servicegroup = $nagios_group
-    } else {
-        # this check is part of no servicegroup.
-        $servicegroup = undef
-    }
-
-        # Export the nagios service instance
-        @@nagios_service { "$::hostname $title":
-            ensure                  => $ensure,
-            target                  => "${::nagios_config_dir}/puppet_checks.d/${host}.cfg",
-            host_name               => $host,
-            servicegroups           => $servicegroup,
-            service_description     => $description,
-            check_command           => $check_command,
-            max_check_attempts      => $retries,
-            normal_check_interval   => $normal_check_interval,
-            retry_check_interval    => $retry_check_interval,
-            check_period            => '24x7',
-            notification_interval   => $critical ? {
-                'true'  => 240,
-                default => 0,
-            },
-            notification_period     => '24x7',
-            notification_options    => 'c,r,f',
-            contact_groups          => $critical ? {
-                'true'  => 'admins,sms',
-                default => $contact_group,
-            },
-            passive_checks_enabled  => 1,
-            active_checks_enabled   => $passive ? {
-                'true'  => 0,
-                default => 1,
-            },
-            is_volatile             => $passive ? {
-                'true'  => 1,
-                default => 0,
-            },
-            check_freshness         => $passive ? {
-                'true'  => 1,
-                default => 0,
-            },
-            freshness_threshold     => $passive ? {
-                'true'  => $freshness,
-                default => undef,
-            },
+    # Export the nagios service instance
+    @@nagios_service { "${::hostname} ${title}":
+        ensure                  => $ensure,
+        target                  => "${::nagios_config_dir}/puppet_checks.d/${host}.cfg",
+        host_name               => $host,
+        servicegroups           => $group,
+        service_description     => $description,
+        check_command           => $check_command,
+        max_check_attempts      => $retries,
+        normal_check_interval   => $normal_check_interval,
+        retry_check_interval    => $retry_check_interval,
+        check_period            => '24x7',
+        notification_interval   => $critical ? {
+            'true'  => 240,
+            default => 0,
+        },
+        notification_period     => '24x7',
+        notification_options    => 'c,r,f',
+        contact_groups          => $critical ? {
+            'true'  => 'admins,sms',
+            default => $contact_group,
+        },
+        passive_checks_enabled  => 1,
+        active_checks_enabled   => $passive ? {
+            'true'  => 0,
+            default => 1,
+        },
+        is_volatile             => $passive ? {
+            'true'  => 1,
+            default => 0,
+        },
+        check_freshness         => $passive ? {
+            'true'  => 1,
+            default => 0,
+        },
+        freshness_threshold     => $passive ? {
+            'true'  => $freshness,
+            default => undef,
+        },
     }
 }
