99a2a100223f3c96a7ba611491aed2f8bdf14f8d
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-11-12 01:58:28 +0200

    Change postgresql dir to have version and cluster
    Using straight /srv/postgres is not so kind on the various postgresql
    tools which expect version and cluster name. Plus if /srv/postgres is a
    mount point, like in our case, postgres complains about lost+found
    existence
    
    Change-Id: Iebce74d7101299e7dea25c2cb798d674e2343108

--- modules/postgresql/spec/classes/postgresql_slave_spec.rb
+++ modules/postgresql/spec/classes/postgresql_slave_spec.rb
@@ -32,7 +32,7 @@ describe 'postgresql::slave', :type => :class do
         :ensure           => 'present',
         :master_server    => 'test',
         :replication_pass => 'pass',
-        :datadir          => '/srv/postgres',
+        :datadir          => '/srv/postgres/9.1/main',
         }
     }
     context 'ensure present' do
@@ -44,7 +44,7 @@ describe 'postgresql::slave', :type => :class do
         end
         it { should contain_file('/etc/postgresql/9.1/main/slave.conf').with_ensure('present') }
         it do
-            should contain_file('/srv/postgres/recovery.conf').
+            should contain_file('/srv/postgres/9.1/main/recovery.conf').
                 with_ensure('present').
                 with_content(/host=test user=replication password=pass/)
         end
