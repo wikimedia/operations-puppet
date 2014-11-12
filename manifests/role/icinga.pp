2267ffdefff0ad3d88815cc99e3e551741e70eac
YuviPanda <yuvipanda@gmail.com>
2014-11-13 05:22:05 +0530

    icinga: Move ircecho code into module
    Change-Id: I92f850d4e86e13e1af8f775c3e0affed101ead5f

--- manifests/role/icinga.pp
+++ manifests/role/icinga.pp
@@ -2,13 +2,22 @@
 #
 # Sets up a icinga instance which checks services
 # and hosts for Wikimedia Production cluster
-class role::icinga {
+#
+# = Parameters
+#
+# [*ircbot*]
+#   Setup an ircbot using ircecho to support echoing notifications
+#
+class role::icinga(
+    ircbot = true,
+){
     include facilities::pdu_monitoring
     include icinga::monitor::checkpaging
     include icinga::nsca::firewall
     include icinga::nsca::daemon
     include icinga::monitor::wikidata
     include icinga::groups::misc
+    include icinga::ircbot
     include lvs::monitor
     include role::authdns::monitoring
     include network::checks
