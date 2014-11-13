399e61c3d522ebfa8f1a7c6aba806585c09a62ed
YuviPanda <yuvipanda@gmail.com>
2014-11-14 01:28:22 +0530

    shinken: Setup IRC notification for shinken
    Change-Id: I9a9880fdf414cdf23a114651f917e97ea701b3f1

---
+++ modules/shinken/manifests/ircbot.pp
@@ -0,0 +1,20 @@
+# = Class: shinken::ircbot
+#
+# Sets up an ircecho instance that sends shinken alerts to IRC
+class shinken::ircbot {
+
+    $ircecho_logs   = {
+        '/var/log/shinken/irc/irc.log'          => '#wikimedia-operations',
+        '/var/log/shinken/irc/irc-wikidata.log' => '#wikidata',
+        '/var/log/shinken/irc/irc-qa.log'       => '#wikimedia-qa',
+        '/var/log/shinken/irc/irc-labs.log'     => '#wikimedia-labs',
+    }
+    $ircecho_nick   = 'shinken-wm'
+    $ircecho_server = 'chat.freenode.net'
+
+    class { '::ircecho':
+        ircecho_logs    => $ircecho_logs,
+        ircecho_nick    => $ircecho_nick,
+        ircecho_server  => $ircecho_server,
+   }
+}
