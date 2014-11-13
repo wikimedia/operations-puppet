5f31ac231ff1d86f8b40cf35ac3b6537425ef06c
YuviPanda <yuvipanda@gmail.com>
2014-11-14 01:28:22 +0530

    shinken: Setup IRC notification for shinken
    Also remove unused perfdata commands from notification
    config
    Change-Id: I9a9880fdf414cdf23a114651f917e97ea701b3f1

---
+++ modules/shinken/manifests/ircbot.pp
@@ -0,0 +1,38 @@
+# = Class: shinken::ircbot
+#
+# Sets up an ircecho instance that sends shinken alerts to IRC
+#
+# = Parameters
+#
+# [*nick*]
+#   IRC Nick for the bot relaying notifications
+#
+# [*server*]
+#   IRC server the bot should connect to
+class shinken::ircbot(
+    $nick = 'shinken-wm',
+    $server = 'chat.freenode.net',
+){
+    include shinken::server
+
+    file { '/var/log/ircecho':
+        ensure  => directory,
+        owner   => 'shinken',
+        group   => 'shinken',
+        require => Package['shinken'],
+        mode    => '0555',
+    }
+
+    $ircecho_logs   = {
+        '/var/log/ircecho/irc.log'          => '#wikimedia-operations',
+        '/var/log/ircecho/irc-qa.log'       => '#wikimedia-qa',
+        '/var/log/ircecho/irc-labs.log'     => '#wikimedia-labs',
+    }
+
+    class { '::ircecho':
+        ircecho_logs   => $ircecho_logs,
+        ircecho_nick   => $nick,
+        ircecho_server => $server,
+        require        => File['/var/log/shinken/irc'],
+    }
+}
