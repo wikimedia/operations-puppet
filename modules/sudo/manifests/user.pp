92753115f7c42470b11f4a2255493c242b12ca24
Ori Livneh <ori@wikimedia.org>
2014-11-14 21:50:24 -0800

    allow multiple sudo::user grants for same user
    Right now, the user receiving the sudo grant is specified via the resource
    title. Since resource titles must be unique, only one grant may be declared per
    user. That's a stupid limitation, so let's fix it.
    
    Change-Id: Idd809d8c14052d9708531f9a5887727454e421d1

--- modules/sudo/manifests/user.pp
+++ modules/sudo/manifests/user.pp
@@ -1,11 +1,39 @@
-define sudo::user( $privileges ) {
-    $user = $title
+# == Define: sudo::user
+#
+# Manages a sudo specification in /etc/sudoers.d.
+#
+# === Parameters
+#
+# [*privileges*]
+#   Array of sudo privileges.
+#
+# [*user*]
+#   User to which privileges should be assigned.
+#   Defaults to the resource title.
+#
+# === Examples
+#
+#  sudo::user { 'nagios_check_raid':
+#    user       => 'nagios',
+#    privileges => [
+#      'ALL = NOPASSWD: /usr/local/lib/nagios/plugins/check-raid'
+#    ],
+#  }
+#
+define sudo::user(
+    $privileges,
+    $ensure = present,
+    $user   = $title,
+) {
+    validate_ensure($ensure)
 
-    file { "/etc/sudoers.d/${user}":
-        owner   => root,
-        group   => root,
+    $title_safe = regsubst($title, '\W', '-', 'G')
+
+    file { "/etc/sudoers.d/${title_safe}":
+        ensure  => $ensure,
+        content => template('sudo/sudoers.erb'),
+        owner   => 'root',
+        group   => 'root',
         mode    => '0440',
-        content => template('sudo/sudoers.erb');
     }
-
 }
