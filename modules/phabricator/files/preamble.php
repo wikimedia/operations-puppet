16f47b66f7112e455a4eba44e05e750c20b6eb30
Mukunda Modell <mmodell@wikimedia.org>
2014-11-15 00:50:36 -0600

    set up redirects for bugzilla urls to redirect to phabricator.
    This was already set up for testing in labs, this change applies
    the same to production in preparation for the bugzilla migration.
    
    Refs T40
    
    Change-Id: I32cbc453946c5262802ee720644a4460ae375a79

--- modules/phabricator/files/preamble.php
+++ modules/phabricator/files/preamble.php
@@ -1,65 +1,74 @@
 <?php
 
-// this is a test setup to prove the redirection code works as expected
-// this should only respond to requests on the test subdomain
-// phabzilla.wmflabs.org which is a proxy for phab-01
-if ($_SERVER['HTTP_HOST'] == 'phabzilla.wmflabs.org') {
-    $_SERVER['HTTP_HOST'] = 'bugs.wikimedia.org';
-} else {
-    // other hostnames are "NO-OP"
-    return;
+if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+  $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO'])
+    && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+  $_SERVER['HTTPS'] = true;
 }
 
 class redirector {
-    protected $config;
-    protected $mysqli;
+  protected $config;
+  protected $mysqli;
 
-    function __construct() {
-        $this->config = json_decode(file_get_contents(__DIR__.'/redirect_config.json'));
-        $this->mysqli = new mysqli(
-            $this->config->mysql->host,
-            $this->config->mysql->user,
-            $this->config->mysql->pass,
-            'phabricator_maniphest'
-        );
-        if ($this->mysqli->connect_error) {
-           die('Connect Error (' . $this->mysqli->connect_errno . ') '
-            . $this->mysqli->connect_error);
-        }
+  function __construct() {
+    $this->config = json_decode(
+      file_get_contents(__DIR__.'/redirect_config.json'));
+
+    $this->mysqli = new mysqli(
+      $this->config->mysql->host,
+      $this->config->mysql->user,
+      $this->config->mysql->pass,
+      'phabricator_maniphest'
+    );
+    if ($this->mysqli->connect_error) {
+      $msg = 'redirector.php: Connect Error (' . $this->mysqli->connect_errno . ') '
+           . $this->mysqli->connect_error;
+      error_log($msg);
+      die($msg);
     }
-    public function redirect($url) {
+  }
+  public function redirect($url) {
+    foreach ($this->config->urlPatterns as $i=>$p) {
+      $matches = array();
+      $pattern = '#'.$p->pattern.'#';
 
-        foreach ($this->config->urlPatterns as $i=>$p) {
-            $matches = array();
-            $pattern = '#'.$p->pattern.'#';
+      if (preg_match( $pattern, $url, $matches )) {
+        if (isset($p->fieldValue)) {
+          // dynamic redirect, look up $id using cross reference query
+          $fieldValue = isset($matches[1])
+                      ? str_replace('$1', $matches[1], $p->fieldValue)
+                      : $p->fieldValue;
 
-            if (preg_match( $pattern, $url, $matches )) {
+          $sql = str_replace('$fieldValue', $fieldValue, $this->config->query);
+          $sql = str_replace('$fieldIndex', $this->config->fieldIndex, $sql);
 
-                if (isset($p->fieldValue)) {
-                    // dynamic redirect, look up $id using cross reference query
-                    $fieldValue = str_replace('$1', $matches[1], $p->fieldValue);
-                    $sql = $this->config->query;
-                    $sql = str_replace('$fieldValue', $fieldValue, $sql);
-                    $sql = str_replace('$fieldIndex', $this->config->fieldIndex, $sql);
-
-                    if ($res = $this->mysqli->query($sql)) {
-                        $res = $res->fetch_assoc();
-                        // insert the cross referenced id into the redirectUrl
-                        $redirect = str_replace('$id', $res['id'], $p->redirectUrl);
-                        header("Location: $redirect");
-                        echo "Redirecting to $redirect\n";
-                        exit;
-                    }
-                } else {
-                    // static redirect
-                    header("Location: ". $p->redirectUrl);
-                    exit;
-                }
+          if ($res = $this->mysqli->query($sql)) {
+            $res = $res->fetch_assoc();
+            // insert the cross referenced id into the redirectUrl
+            $redirect = str_replace('$id', $res['id'], $p->redirectUrl);
+            header("Location: $redirect");
+            exit;
+          }
+        } else {
+          $redirect = $p->redirectUrl;
+          if (count($matches)>1) {
+            for ($i = 1; $i < count($matches); $i++) {
+              $redirect = str_replace('$'.$i, $matches[$i], $redirect);
             }
+          }
+          // static redirect
+          header("Location: $redirect");
+          exit;
         }
+      }
     }
+  }
 }
 
-$full_uri = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-$r = new redirector();
-$r->redirect($full_uri);
+if ($_SERVER['HTTP_HOST'] !== 'phabricator.wikimedia.org') {
+  $full_uri = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+  $r = new redirector();
+  $r->redirect($full_uri);
+}
