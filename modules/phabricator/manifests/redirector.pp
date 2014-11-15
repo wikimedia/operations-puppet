9a756c08bccd1a7fa9ebbd6a20da7d1812bab847
Mukunda Modell <mmodell@wikimedia.org>
2014-11-15 00:50:36 -0600

    Set up redirects for bugzilla urls to redirect to phabricator.
    This was already set up for testing in labs, this change applies
    the same to production in preparation for the bugzilla migration.
    
    Refs T40
    
    This incorporates previous changes which were either abandoned
     or reverted:
    
    * Clean up some puppet-lint errors and warnings
      (https://gerrit.wikimedia.org/r/#/c/174288/)
    * Set up redirects for bugzilla...
      (https://gerrit.wikimedia.org/r/#/c/173483/)
    
    Change-Id: I12b39f9b81f2cce4982e7fed535c330057317d82

--- modules/phabricator/manifests/redirector.pp
+++ modules/phabricator/manifests/redirector.pp
@@ -1,10 +1,20 @@
-define phabricator::redirector($mysql_user, $mysql_pass, $mysql_host, $rootdir='/') {
-    file { "${rootdir}/phabricator/support/preamble.php":
-        source  => 'puppet:///modules/phabricator/preamble.php',
-        require => File["${rootdir}/phabricator/support/redirect_config.json"]
-    }
+# setup the preamble.php and redirect_config.json to redirect bugzilla
+# (and eventually RT) urls to phabricator
+define phabricator::redirector(
+  $mysql_user,
+  $mysql_pass,
+  $mysql_host,
+  $rootdir     = '/srv/phab',
+  $field_index = '',
+  $phab_host   = 'phabricator.wikimedia.org',
+  $alt_host    = 'fab.wmfusercontent.org'
+) {
+  file { "${rootdir}/phabricator/support/preamble.php":
+    content => template('phabricator/preamble.php.erb'),
+    require => File["${rootdir}/phabricator/support/redirect_config.json"]
+  }
 
-    file { "${rootdir}/phabricator/support/redirect_config.json":
-        content => template('phabricator/redirect_config.json.erb')
-    }
+  file { "${rootdir}/phabricator/support/redirect_config.json":
+    content => template('phabricator/redirect_config.json.erb')
+  }
 }
