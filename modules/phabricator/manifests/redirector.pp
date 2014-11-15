eb5a40f5430696537c596ea533d274f51535ce8a
Mukunda Modell <mmodell@wikimedia.org>
2014-11-15 00:50:36 -0600

    set up redirects for bugzilla urls to redirect to phabricator.
    This was already set up for testing in labs, this change applies
    the same to production in preparation for the bugzilla migration.
    
    Refs T40
    
    Change-Id: I32cbc453946c5262802ee720644a4460ae375a79

--- modules/phabricator/manifests/redirector.pp
+++ modules/phabricator/manifests/redirector.pp
@@ -1,4 +1,4 @@
-define phabricator::redirector($mysql_user, $mysql_pass, $mysql_host, $rootdir='/') {
+define phabricator::redirector($mysql_user, $mysql_pass, $mysql_host, $rootdir='/', $field_index='', $phab_host='phabricator.wikimedia.org') {
     file { "${rootdir}/phabricator/support/preamble.php":
         source  => 'puppet:///modules/phabricator/preamble.php',
         require => File["${rootdir}/phabricator/support/redirect_config.json"]
