815c22a7356aa90515c52c4e85b9fcd3e8affc37
Ori Livneh <ori@wikimedia.org>
2014-11-15 15:34:15 -0800

    keyholder: add icinga check
    Add a check that ensures the keyholder agent is armed with the key.
    
    Change-Id: I4a5d4bf6429ccf2571c27e13a3039324a100a528

---
+++ modules/keyholder/manifests/monitoring.pp
@@ -0,0 +1,32 @@
+# == Class: keyholder::monitoring
+#
+# Provisions an Icinga check that ensures the keyholder is armed
+# with all configured identities.
+#
+class keyholder::monitoring( $ensure = present ) {
+    validate_ensure($ensure)
+
+    $plugin_path = '/usr/lib/nagios/plugins/check_keyholder'
+
+    file { $plugin_path:
+        ensure => $ensure,
+        source => 'puppet:///modules/keyholder/check_keyholder',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+
+    sudo::user { 'nagios_check_keyholder':
+        ensure     => $ensure,
+        user       => 'nagios',
+        privileges => [ "ALL = NOPASSWD: $plugin_path" ],
+        require    => File[$plugin_path],
+    }
+
+    nrpe::monitor_service { 'keyholder':
+        ensure       => $ensure,
+        description  => 'Keyholder SSH agent',
+        nrpe_command => "/usr/bin/sudo $plugin_path",
+        require      => Sudo::User['nagios_check_keyholder'],
+    }
+}
