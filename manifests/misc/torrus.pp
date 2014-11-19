e3f88cd04dab536e2f73392fbe70af6149436faa
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-11-19 12:46:01 +0200

    WIP: Make torrus class naming more clear
    Nothing more that just fully naming every single definition or class
    DO NOT MERGE!
    
    Change-Id: I5fa9063299ac28b6e33f86c110b183a5b6883ce9

--- manifests/misc/torrus.pp
+++ manifests/misc/torrus.pp
@@ -9,50 +9,6 @@ class misc::torrus {
             ensure => latest,
     }
 
-    class web {
-        package { 'torrus-apache2':
-            ensure => latest,
-            before => Service['apache2'],
-        }
-
-        include ::apache::mod::rewrite
-        include ::apache::mod::perl
-
-        @webserver::apache::site { 'torrus.wikimedia.org':
-            require  => Class['::apache::mod::rewrite', '::apache::mod::perl'],
-            docroot  => '/var/www',
-            custom   => ['RedirectMatch ^/$ /torrus'],
-            includes => ['/etc/torrus/torrus-apache2.conf'],
-        }
-    }
-
-    class config {
-        File { require => Package['torrus-common'] }
-
-        file { '/etc/torrus/conf/':
-            source  => 'puppet:///files/torrus/conf/',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            recurse => remote,
-        }
-
-        file { '/etc/torrus/templates/':
-            source  => 'puppet:///files/torrus/templates/',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            recurse => remote,
-        }
-
-        file { '/usr/share/torrus/sup/webplain/wikimedia.css':
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0444',
-            source => 'puppet:///files/torrus/wikimedia.css',
-        }
-    }
-
     exec { 'torrus clearcache':
         command     => '/usr/sbin/torrus clearcache',
         require     => Class['misc::torrus::config'],
@@ -83,112 +39,156 @@ class misc::torrus {
         hasrestart => false,
     }
 
-    class xmlconfig {
-        require misc::torrus::config
-        include passwords::network
+    include misc::torrus::xmlconfig
+    include misc::torrus::discovery
+}
 
-        file { '/etc/torrus/xmlconfig/':
-            source  => 'puppet:///files/torrus/xmlconfig/',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            recurse => remote,
-        }
-
-        file { '/etc/torrus/xmlconfig/site-global.xml':
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            content => template('torrus/site-global.xml.erb'),
-        }
+class misc::torrus::web {
+    package { 'torrus-apache2':
+        ensure => latest,
+        before => Service['apache2'],
     }
 
-    class discovery {
-        require misc::torrus::config
-        require misc::torrus::xmlconfig
+    include ::apache::mod::rewrite
+    include ::apache::mod::perl
 
-        # Definition: misc::torrus::discovery
-        #
-        # This definition generates a torrus discovery DDX file, which Torrus
-        # will use to compile its XML config files from SNMP
-        #
-        # Parameters:
-        #   - $subtree: the Torrus subtree path used in the XML config file
-        #   - $domain: The domain name to use for SNMP host names
-        #   - $snmp_community: The SNMP community needed to query
-        #   - $hosts: A list of hosts
-        define ddxfile(
-            $subtree,
-            $domain= '',
-            $snmp_community= 'public',
-            $hosts= []
-        ) {
-            file { "/etc/torrus/discovery/${title}.ddx":
-                require => File['/etc/torrus/discovery'],
-                content => template('torrus/generic.ddx.erb'),
-                owner   => 'root',
-                group   => 'root',
-                mode    => '0444',
-                before  => Exec['torrus-discovery'],
-                notify  => Exec['torrus-discovery'],
-            }
-        }
+    @webserver::apache::site { 'torrus.wikimedia.org':
+        require  => Class['::apache::mod::rewrite', '::apache::mod::perl'],
+        docroot  => '/var/www',
+        custom   => ['RedirectMatch ^/$ /torrus'],
+        includes => ['/etc/torrus/torrus-apache2.conf'],
+    }
+}
 
-        file { '/etc/torrus/discovery':
-            ensure => directory,
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0750',
-        }
+class misc::torrus::config {
+    File { require => Package['torrus-common'] }
 
-        file { '/etc/cron.daily/torrus-discovery':
-            source => 'puppet:///files/torrus/torrus-discovery',
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0550',
-        }
-
-        exec { 'torrus-discovery':
-            require     => File['/etc/cron.daily/torrus-discovery'],
-            command     => '/etc/cron.daily/torrus-discovery',
-            timeout     => 1800,
-            refreshonly => true,
-            before      => Exec['torrus compile'],
-        }
+    file { '/etc/torrus/conf/':
+        source  => 'puppet:///files/torrus/conf/',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        recurse => remote,
     }
 
-    class xml-generation {
-        # Class: misc::torrus::xml-generation::cdn
-        #
-        # This class automatically generates XML files for
-        # Squid and Varnish servers
-        #
-        # Uses role/cache/cache.pp
-        class cdn {
-            require role::cache::configuration
-
-            File {
-                owner  => 'root',
-                group  => 'root',
-                mode   => '0444',
-                notify => Exec['torrus compile --tree=CDN'],
-            }
-            file { '/etc/torrus/xmlconfig/varnish.xml':
-                content => template('torrus/varnish.xml.erb'),
-            }
-
-            file { '/etc/torrus/xmlconfig/cdn-aggregates.xml':
-                content => template('torrus/cdn-aggregates.xml.erb'),
-            }
-
-            exec { 'torrus compile --tree=CDN':
-                path        => '/bin:/sbin:/usr/bin:/usr/sbin',
-                logoutput   => true,
-                refreshonly => true,
-            }
-        }
+    file { '/etc/torrus/templates/':
+        source  => 'puppet:///files/torrus/templates/',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        recurse => remote,
     }
 
-    include xmlconfig
-    include discovery
+    file { '/usr/share/torrus/sup/webplain/wikimedia.css':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///files/torrus/wikimedia.css',
+    }
+}
+
+class misc::torrus::xmlconfig {
+    require misc::torrus::config
+    include passwords::network
+
+    file { '/etc/torrus/xmlconfig/':
+        source  => 'puppet:///files/torrus/xmlconfig/',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        recurse => remote,
+    }
+
+    file { '/etc/torrus/xmlconfig/site-global.xml':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('torrus/site-global.xml.erb'),
+    }
+}
+
+class misc::torrus::discovery {
+    require misc::torrus::config
+    require misc::torrus::xmlconfig
+
+    file { '/etc/torrus/discovery':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0750',
+    }
+
+    file { '/etc/cron.daily/torrus-discovery':
+        source => 'puppet:///files/torrus/torrus-discovery',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0550',
+    }
+
+    exec { 'torrus-discovery':
+        require     => File['/etc/cron.daily/torrus-discovery'],
+        command     => '/etc/cron.daily/torrus-discovery',
+        timeout     => 1800,
+        refreshonly => true,
+        before      => Exec['torrus compile'],
+    }
+}
+
+# Definition: misc::torrus::discovery
+#
+# This definition generates a torrus discovery DDX file, which Torrus
+# will use to compile its XML config files from SNMP
+#
+# Parameters:
+#   - $subtree: the Torrus subtree path used in the XML config file
+#   - $domain: The domain name to use for SNMP host names
+#   - $snmp_community: The SNMP community needed to query
+#   - $hosts: A list of hosts
+define misc::torrus::discovery::ddxfile(
+    $subtree,
+    $domain= '',
+    $snmp_community= 'public',
+    $hosts= []
+) {
+    file { "/etc/torrus/discovery/${title}.ddx":
+        require => File['/etc/torrus/discovery'],
+        content => template('torrus/generic.ddx.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        before  => Exec['torrus-discovery'],
+        notify  => Exec['torrus-discovery'],
+    }
+}
+
+class misc::torrus::xml-generation {
+}
+# Class: misc::torrus::xml-generation::cdn
+#
+# This class automatically generates XML files for
+# Squid and Varnish servers
+#
+# Uses role/cache/cache.pp
+class misc::torrus::xml-generation::cdn {
+    require role::cache::configuration
+
+    File {
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        notify => Exec['torrus compile --tree=CDN'],
+    }
+    file { '/etc/torrus/xmlconfig/varnish.xml':
+        content => template('torrus/varnish.xml.erb'),
+    }
+
+    file { '/etc/torrus/xmlconfig/cdn-aggregates.xml':
+        content => template('torrus/cdn-aggregates.xml.erb'),
+    }
+
+    exec { 'torrus compile --tree=CDN':
+        path        => '/bin:/sbin:/usr/bin:/usr/sbin',
+        logoutput   => true,
+        refreshonly => true,
+    }
 }
