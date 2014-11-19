741b176b95f7a16ca9055226fc6e891501f7e39a
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-11-19 12:46:01 +0200

    WIP: Modularize torrus
    Nothing more that just some stuff moving around
    Missing reorganizations etc
    DO NOT MERGE!
    
    Change-Id: I5fa9063299ac28b6e33f86c110b183a5b6883ce9

--- manifests/site.pp
+++ manifests/site.pp
@@ -2105,70 +2105,17 @@ node 'netmon1001.wikimedia.org' {
     include smokeping
     include smokeping::web
     include role::librenms
-    include misc::torrus
-    include misc::torrus::web
-    include misc::torrus::xml-generation::cdn
     include passwords::network
     include ganglia::collector
     include role::servermon
+
+    interface::add_ip6_mapped { 'main': }
 
     class { 'ganglia_new::monitor::aggregator':
         sites => ['eqiad', 'codfw'],
     }
 
-    $snmp_ro_community = $passwords::network::snmp_ro_community
 
-    interface::add_ip6_mapped { 'main': }
-
-    $corerouters = [
-        'cr1-eqiad.wikimedia.org',
-        'cr1-esams.wikimedia.org',
-        'cr1-ulsfo.wikimedia.org',
-        'cr1-ulsfo.wikimedia.org',
-        'cr2-eqiad.wikimedia.org',
-        'cr2-knams.wikimedia.org',
-        'cr2-ulsfo.wikimedia.org',
-        'cr2-codfw.wikimedia.org',
-        'pfw1-eqiad.wikimedia.org',
-    ]
-
-    $accessswitches = [
-        'asw2-a5-eqiad.mgmt.eqiad.wmnet',
-        'asw-a-eqiad.mgmt.eqiad.wmnet',
-        'asw-b-eqiad.mgmt.eqiad.wmnet',
-        'asw-c-eqiad.mgmt.eqiad.wmnet',
-        'asw-d-eqiad.mgmt.eqiad.wmnet',
-        'asw-a-codfw.mgmt.codfw.wmnet',
-        'asw-b-codfw.mgmt.codfw.wmnet',
-        'asw-c-codfw.mgmt.codfw.wmnet',
-        'asw-d-codfw.mgmt.codfw.wmnet',
-        'csw2-esams.wikimedia.org',
-        'msw1-eqiad.mgmt.eqiad.wmnet',
-        'psw1-eqiad.mgmt.eqiad.wmnet',
-    ]
-
-    $storagehosts = [
-        'nas1001-a.eqiad.wmnet',
-        'nas1001-b.eqiad.wmnet',
-    ]
-
-    misc::torrus::discovery::ddxfile { 'corerouters':
-        subtree        => '/Core_routers',
-        snmp_community => $snmp_ro_community,
-        hosts          => $corerouters,
-    }
-
-    misc::torrus::discovery::ddxfile { 'accessswitches':
-        subtree        => '/Access_switches',
-        snmp_community => $snmp_ro_community,
-        hosts          => $accessswitches,
-    }
-
-    misc::torrus::discovery::ddxfile { 'storage':
-        subtree        => '/Storage',
-        snmp_community => $snmp_ro_community,
-        hosts          => $storagehosts,
-    }
 }
 
 node 'nitrogen.wikimedia.org' {
