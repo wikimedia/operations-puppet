37a706608452c86392cd355b1287782c6f45f07c
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-11-20 11:03:48 +0100

    deployment: make scap proxies configured in one place
    * moved misc::deployment::scap_proxy to deployment::scap_proxy
    * made dsh/group/scap-proxies configurable via a class variable
    * use the same variable in mediawiki::common to determine if the server
      is a scap proxy
    
    This way we can finally change things in one place only.
    
    Change-Id: I22dd6d44a4574f679451a606fab7750e7345bb6c
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/dsh/manifests/config.pp
+++ modules/dsh/manifests/config.pp
@@ -2,7 +2,7 @@
 #
 # Sets up dsh config files alone, without actually
 # setting up dsh. Useful primarily for monitoring
-class dsh::config {
+class dsh::config ($scap_proxies = []){
     file { '/etc/dsh':
         ensure => directory,
         owner  => 'root',
@@ -16,6 +16,14 @@ class dsh::config {
         source  => 'puppet:///modules/dsh/group',
         recurse => true,
     }
+
+    file { '/etc/dsh/group/scap-proxies':
+        content => template('dsh/scap-proxies.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+
     file { '/etc/dsh/dsh.conf':
         owner  => 'root',
         group  => 'root',
