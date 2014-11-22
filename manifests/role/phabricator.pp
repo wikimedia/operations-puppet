a99558815504c6c2891c0d6813ad7d8154b6e629
cpettet <rush@wikimedia.org>
2014-11-22 16:05:52 -0600

    phab handle cherry-pick for T1343
    Change-Id: I73653c4317a6911d4fdcfdfaf45cae80db5b033c

--- manifests/role/phabricator.pp
+++ manifests/role/phabricator.pp
@@ -42,7 +42,7 @@ class role::phabricator::main {
     system::role { 'role::phabricator::main': description => 'Phabricator (Main)' }
 
     #let's go jenkins
-    $current_tag = 'wmoauth-20141110'
+    $current_tag = 'T1343'
     $domain = 'phabricator.wikimedia.org'
     class { '::phabricator':
         git_tag          => $current_tag,
@@ -124,7 +124,7 @@ class role::phabricator::labs {
 
     #pass not sensitive but has to match phab and db
     $mysqlpass = 'labspass'
-    $current_tag = 'wmoauth-20141110'
+    $current_tag = 'T1343'
     class { '::phabricator':
         git_tag          => $current_tag,
         lock_file        => '/var/run/phab_repo_lock',
