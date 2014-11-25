34351533341dfac97951e71fa7d04d8d9edaeac5
YuviPanda <yuvipanda@gmail.com>
2014-11-25 12:13:28 +0530

    puppetmaster: Add script to track upstream changes
    This is currently in use by deployment-prep, and seems general
    enough for wider use as is
    
    Bug: T1150
    Change-Id: Ifa3229d6fb3424f75d14dd2569efca3b93936aeb

---
+++ modules/puppetmaster/manifests/gitsync.pp
@@ -0,0 +1,33 @@
+# == Class: puppetmaster::gitsync
+#
+# Sync local operations/puppet.git checkout with upstream.
+# Meant for use with local puppetmasters.
+# == Parameters
+#
+# [*repo_path*]
+#   The path to the operations/puppet.git repository
+class puppetmaster::gitsync(
+    $repo_path = '/var/lib/git/operations/puppet'
+){
+
+    file { '/usr/local/bin/git-sync-upstream':
+        ensure  => present,
+        content => template('puppetmaster/git-sync-upstream.erb'),
+        mode    => '0555',
+    }
+
+    cron { 'rebase_operations_puppet':
+        ensure  => present,
+        user    => 'root',
+        minute  => 17,
+        command => '/usr/local/bin/git-sync-upstream >>/var/log/git-sync-upstream.log 2>&1',
+        require => File['/usr/local/bin/git-sync-upstream'],
+    }
+
+    file { '/etc/logrotate.d/git-sync-upstream':
+        ensure  => present,
+        source  => 'puppet:///modules/puppetmaster/git-sync-upstream.logrotate',
+        mode    => '0444',
+        require => Cron['rebase_operations_puppet'],
+    }
+}
