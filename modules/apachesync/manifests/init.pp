f8488f01cecd2f53d8334e48b0627251a1c4b79b
dzahn <dzahn@wikimedia.org>
2014-11-25 15:00:05 -0800

    apachesync - delete sync-apache script
    the sync-apache script(s) are not needed anymore
    because Apache config is now deployed by puppet
    
    in I69150281fb08a Ori suggested to remove this entire module,
    but we pointed out how we still want to keep apache-graceful-all
    (which we wanted to change from dsh to salt) and how apache-fast-test
    is still a useful test script we'd like to have
    
    so this first deletes the obvious to make the module smaller,
    then we can see to move the other 2 scripts elswhere and then
    delete this module
    
    Change-Id: I2f1973c5205f0204ec7307effd21c2b9f496b727

--- modules/apachesync/manifests/init.pp
+++ modules/apachesync/manifests/init.pp
@@ -8,26 +8,11 @@ class apachesync {
         mode   => '0555',
     }
 
-    file { '/usr/local/bin/sync-apache':
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
-        source => 'puppet:///modules/apachesync/sync-apache',
-    }
-
     file { '/usr/local/bin/apache-graceful-all':
         owner  => 'root',
         group  => 'root',
         mode   => '0555',
         source => 'puppet:///modules/apachesync/apache-graceful-all',
-    }
-
-    file { '/usr/local/bin/sync-apache-simulated':
-        ensure => link,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
-        target => '/usr/local/bin/sync-apache',
     }
 
     file  { '/usr/local/bin/apache-fast-test':
