a84d2364d14279bd3bdef549998804048fb31a09
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-11-28 11:54:31 +0100

    hiera: role-based backend, role keyword
    We introduce a new puppet function, role, which can be used ONLY at
    node scope and allows to register that one (or more) specific roles have
    been assigned to the class, and use it to direct hiera lookups made via
    the new "role" backend.
    
    Change-Id: I9a7e54539a421936ba4ced8c4229abacdde0a4fd
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/wmflib/spec/functions/role_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+describe 'role' do
+
+  before :each do
+    @compiler = Puppet::Parser::Compiler.new(Puppet::Node.new("foo"))
+    @scope = Puppet::Parser::Scope.new(@compiler)
+    @scope.source = Puppet::Resource::Type.new(:node, :foo)
+    @scope.stub(:is_nodescope?).and_return(true)
+    @topscope = @scope.compiler.topscope
+    @scope.parent = @topscope
+    roleclasses = ['role::test', 'role::test2']
+    roleclasses.each do |roleclass|
+      unless @compiler.topscope.find_hostclass(roleclass)
+        host_cls = Puppet::Resource::Type.new(:hostclass, roleclass)
+        @scope.known_resource_types.add_hostclass(host_cls)
+      end
+    end
+  end
+
+  it "should be called with one parameter" do
+    should run.and_raise_error(ArgumentError)
+  end
+  it "throws error if called outside of the node scope" do
+    should run.with_params(['cache::text']).and_raise_error(Puppet::ParseError)
+  end
+
+  it "throws error if called on a non-existing role" do
+    expect { @scope.function_role(['foo::bar']) }.to raise_error(Puppet::ParseError)
+  end
+
+  it "includes the role class" do
+    expect { @scope.function_role(['test']) }.to_not raise_error()
+  end
+
+  it "raises an error when called more than once in a scope" do
+    @scope.function_role(['test2'])
+    expect { @scope.function_role(['test']) }.to raise_error(Puppet::ParseError)
+  end
+
+  it "adds the keys to the top-scope variable" do
+    @scope.function_role(['test', 'test2'])
+    expect(@topscope.lookupvar('_roles')).to eq({'test' => true, 'test2' => true})
+  end
+
+  it "includes the role classes" do
+    @scope.function_role(['test'])
+    expect(@scope.find_hostclass('role::test')).to be_an_instance_of(Puppet::Resource::Type)
+  end
+end
