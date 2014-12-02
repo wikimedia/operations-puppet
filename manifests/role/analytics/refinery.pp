3adb21c0624583441f2a40f8653cf6ba00b0a91d
Andrew Otto <acotto@gmail.com>
2014-12-02 15:01:28 -0500

    Disable webrequest partition checks in icinga
    nsca passive checks don't work properly when nsca server is older than
    what is provided in Trusty.
    
    See: https://phabricator.wikimedia.org/T76414
         https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=670373
    
    Change-Id: Ib56dd1b818d40708cd6a475e509e07d11964676f

--- manifests/role/analytics/refinery.pp
+++ manifests/role/analytics/refinery.pp
@@ -99,6 +99,14 @@ class role::analytics::refinery::data::drop {
 # For webrequest imports, the Oozie job that is responsible
 # for adding Hive partitions and checking data integrity
 # is responsible for triggering these passive checks.
+#
+# NOTE:  These are disasbled due to nsca not working
+# properly between versions provided in Precise and Trusty.
+# we may reenable these if the icinga server gets upgraded
+# to Trusty.
+# See: https://phabricator.wikimedia.org/T76414
+#      https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=670373
+#
 class role::analytics::refinery::data::check {
     # We are monitoring hourly datasets.
     # Give Oozie a little time to finish running
@@ -114,6 +122,7 @@ class role::analytics::refinery::data::check {
     # This is a passive check that is triggered by the Oozie
     # webrequest add partition jobs.
     monitoring::service { 'hive_partition_webrequest-bits':
+        ensure          => 'absent',
         description     => 'hive_partition_webrequest-bits',
         check_command   => "analytics_cluster_data_import-FAIL!wmf_raw.webrequest bits!${alert_return_code}",
         passive         => 'true',
@@ -121,6 +130,7 @@ class role::analytics::refinery::data::check {
         retries         => 1,
     }
     monitoring::service { 'hive_partition_webrequest-mobile':
+        ensure          => 'absent',
         description     => 'hive_partition_webrequest-mobile',
         check_command   => "analytics_cluster_data_import-FAIL!wmf_raw.webrequest mobile!${alert_return_code}",
         passive         => 'true',
@@ -128,6 +138,7 @@ class role::analytics::refinery::data::check {
         retries         => 1,
     }
     monitoring::service { 'hive_partition_webrequest-text':
+        ensure          => 'absent',
         description     => 'hive_partition_webrequest-text',
         check_command   => "analytics_cluster_data_import-FAIL!wmf_raw.webrequest text!${alert_return_code}",
         passive         => 'true',
@@ -135,6 +146,7 @@ class role::analytics::refinery::data::check {
         retries         => 1,
     }
     monitoring::service { 'hive_partition_webrequest-upload':
+        ensure          => 'absent',
         description     => 'hive_partition_webrequest-upload',
         check_command   => "analytics_cluster_data_import-FAIL!wmf_raw.webrequest upload!${alert_return_code}",
         passive         => 'true',
