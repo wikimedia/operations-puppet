0274819053654edefc89aa12fb7f42d3add028ab
dzahn <dzahn@wikimedia.org>
2014-12-01 17:46:46 -0800

    facilities: move to module
    after the dc-cam class has been removed
    facilities.pp is just PDU monitoring
    
    first attempt was Ib06d7943e315c0 to move this
    to nagios_common module, but per comments from Yuvi,
    abandoned that and now creating a new module
    'facilities' instead and moving it there
    
    Change-Id: Ia41e114465b61eb9d6d6402a58131a4598f7eab2

---
+++ modules/facilities/manifests/monitor_pdu_3phase.pp
@@ -0,0 +1,64 @@
+define facilities::monitor_pdu_3phase(
+    $ip,
+    $breaker  = '30',
+    $redundant= true
+) {
+    @monitoring::host { $title:
+        ip_address => $ip,
+        group      => 'pdus',
+    }
+
+    facilities::monitor_pdu_service { "${title}-infeed-load-tower-A-phase-X":
+        host      => $title,
+        ip        => $ip,
+        tower     => '1',
+        infeed    => '1',
+        breaker   => $breaker,
+        redundant => $redundant,
+    }
+
+    facilities::monitor_pdu_service { "${title}-infeed-load-tower-A-phase-Y":
+        host      => $title,
+        ip        => $ip,
+        tower     => '1',
+        infeed    => '2',
+        breaker   => $breaker,
+        redundant => $redundant,
+    }
+    facilities::monitor_pdu_service { "${title}-infeed-load-tower-A-phase-Z":
+        host      => $title,
+        ip        => $ip,
+        tower     => '1',
+        infeed    => '3',
+        breaker   => $breaker,
+        redundant => $redundant,
+    }
+
+    if $redundant == true {
+        facilities::monitor_pdu_service { "${title}-infeed-load-tower-B-phase-X":
+            host      => $title,
+            ip        => $ip,
+            tower     => '2',
+            infeed    => '1',
+            breaker   => $breaker,
+            redundant => $redundant,
+        }
+        facilities::monitor_pdu_service { "${title}-infeed-load-tower-B-phase-Y":
+            host      => $title,
+            ip        => $ip,
+            tower     => '2',
+            infeed    => '2',
+            breaker   => $breaker,
+            redundant => $redundant,
+        }
+        facilities::monitor_pdu_service { "${title}-infeed-load-tower-B-phase-Z":
+            host      => $title,
+            ip        => $ip,
+            tower     => '2',
+            infeed    => '3',
+            breaker   => $breaker,
+            redundant => $redundant,
+        }
+    }
+}
+
