f80ba9127ab7262d023bc5bd019c530e45f46e4c
dzahn <dzahn@wikimedia.org>
2014-12-01 17:46:46 -0800

    (WIP) facilities: move to module
    after the dc-cam class has been removed
    facilities.pp is just PDU monitoring
    
    first attempt was Ib06d7943e315c0 to move this
    to nagios_common module, but per comments from Yuvi,
    abandoned that and now creating a new module
    'facilities' instead and moving it there
    
    Change-Id: Ia41e114465b61eb9d6d6402a58131a4598f7eab2

---
+++ modules/facilities/manifests/init.pp
@@ -0,0 +1,93 @@
+# monitoring of non-server data center
+# hardware like power distribution units and cameras
+class facilities {
+
+    @monitoring::group { 'pdus':
+        description => 'PDUs',
+    }
+
+    # The PDUs are queried over SNMP using the snmp command provided by the snmp
+    # package. For now ensure it here but it may need to be put in another place
+    # in the future
+    package { 'snmp':
+        ensure => installed,
+    }
+
+    # eqiad
+    # A
+    facilities::monitor_pdu_3phase { 'ps1-a1-eqiad':
+        ip => '10.65.0.32',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-a2-eqiad':
+        ip => '10.65.0.33',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-a3-eqiad':
+        ip => '10.65.0.34',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-a4-eqiad':
+        ip => '10.65.0.35',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-a5-eqiad':
+        ip => '10.65.0.36',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-a6-eqiad':
+        ip => '10.65.0.37',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-a7-eqiad':
+        ip => '10.65.0.38',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-a8-eqiad':
+        ip => '10.65.0.39',
+    }
+    # B
+    facilities::monitor_pdu_3phase { 'ps1-b1-eqiad':
+        ip => '10.65.0.40',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-b2-eqiad':
+        ip => '10.65.0.41',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-b3-eqiad':
+        ip => '10.65.0.42',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-b4-eqiad':
+        ip => '10.65.0.43',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-b5-eqiad':
+        ip => '10.65.0.44',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-b6-eqiad':
+        ip => '10.65.0.45',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-b7-eqiad':
+        ip => '10.65.0.46',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-b8-eqiad':
+        ip => '10.65.0.47',
+    }
+    # C
+    facilities::monitor_pdu_3phase { 'ps1-c1-eqiad':
+        ip => '10.65.0.48',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-c2-eqiad':
+        ip => '10.65.0.49',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-c3-eqiad':
+        ip => '10.65.0.50',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-c4-eqiad':
+        ip => '10.65.0.51',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-c5-eqiad':
+        ip => '10.65.0.52',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-c6-eqiad':
+        ip => '10.65.0.53',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-c7-eqiad':
+        ip => '10.65.0.54',
+    }
+    facilities::monitor_pdu_3phase { 'ps1-c8-eqiad':
+        ip => '10.65.0.55',
+    }
+}
+
