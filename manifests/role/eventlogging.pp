944e22b4d927996160c1b588453fc1a7aef6d8ff
YuviPanda <yuvipanda@gmail.com>
2014-12-05 04:47:52 +0530

    eventlogging: Explicitly include monitoring class when we require it
    Follow up to Ie5c6a274876b170bf4f7a65d4feeaf17f64610b7
    
    Change-Id: I9593b21f659086a723f050b8ae4195a938394f83

--- manifests/role/eventlogging.pp
+++ manifests/role/eventlogging.pp
@@ -191,6 +191,7 @@ class role::eventlogging::monitoring {
 
 class role::eventlogging::graphite {
     include ::eventlogging
+    include ::eventlogging::monitoring
 
     eventlogging::service::consumer { 'graphite':
         input  => 'tcp://vanadium.eqiad.wmnet:8600',
