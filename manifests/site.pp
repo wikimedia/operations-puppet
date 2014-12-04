9105bb21940dd9013fdbce94050f1810116f7665
Ori Livneh <ori@wikimedia.org>
2014-12-04 04:49:21 -0800

    Emit alert when Ori commits on a weekend
    Provisions an Icinga check on palladium that issues an alert if the time is
    between Friday 21:00 and Monday 01:00 and there exists a commit from me in the
    last hour in operations/puppet.
    
    Change-Id: I2c4fd0d6ef907e4afe91f0248e7039f31d70696c

--- manifests/site.pp
+++ manifests/site.pp
@@ -2162,6 +2162,7 @@ node 'palladium.eqiad.wmnet' {
     include role::access_new_install
     include role::puppetmaster::frontend
     include role::pybal_config
+    include misc::monitoring::ori_weekend_commits
 
     $domain_search = [
         'wikimedia.org',
