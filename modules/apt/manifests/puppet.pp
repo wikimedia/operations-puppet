83562e378b0b934351a5b38aaaa90ba029e29648
Faidon Liambotis <faidon@wikimedia.org>
2014-12-09 10:47:25 +0200

    apt::puppet: ensure => absent
    We don't need to pin to the o=Ubuntu version, as we don't ship packages
    in our repository anymore. ensure => absent both stanzas. Do not remove
    the class just yet, but let it clean up on both production and Labs.
    
    Change-Id: Id67b2270ec937fced0b06af4a875ef199da5a35b

--- modules/apt/manifests/puppet.pp
+++ modules/apt/manifests/puppet.pp
@@ -1,22 +1,17 @@
+# FIXME: remove this class and all of its callsites after a reasonable time has passed -2014-12-09
 define apt::puppet (
     $packages  = 'puppet puppet-common facter'
     ) {
 
-    # For trusty, ubuntu packages are puppet 3
-    if $::lsbdistcodename == 'trusty' {
-        apt::pin { $title:
-            package  => $packages,
-            pin      => 'release o=Ubuntu',
-            priority => 1002
-        }
+    apt::pin { $title:
+        ensure   => 'absent',
+        package  => $packages,
+        pin      => 'release o=Ubuntu',
+        priority => 1002
     }
-    elsif $::lsbdistcodename == 'precise' {
-        # Ensure the pref file is not there in this case,
-        # so that puppet gets updated.
-        apt::pin { "puppet_${title}_2.7.pref":
-            ensure   => 'absent',
-            priority => 0,
-            pin      => 'release o=Ubuntu'
-        }
+    apt::pin { "puppet_${title}_2.7.pref":
+        ensure   => 'absent',
+        priority => 0,
+        pin      => 'release o=Ubuntu'
     }
 }
