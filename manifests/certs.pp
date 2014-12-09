8b08b23b9fdd77d8985db613a0af76acf28da763
Faidon Liambotis <faidon@wikimedia.org>
2014-12-09 03:01:47 +0200

    certs: add missing dep & make apparmor optional
    Add a missing dependency of /etc/ssl/localssl to the ssl-cert package
    which actually creates both /etc/ssl and the ssl-cert group.
    
    Also place the AppArmor conditionals under a Ubuntu if, as a) AppArmor
    is specific to Ubuntu at this point b) there is code already to make it
    fail() post-trusty so presumably there is something funky about it.
    (the whole manifest is a bit of a mess anyway)
    
    Change-Id: Iac1803639e67f715373bbf36d171714afc840980

--- manifests/certs.pp
+++ manifests/certs.pp
@@ -201,8 +201,6 @@ define install_additional_key(
 
 class certificates::base {
 
-    include apparmor
-
     package { [ 'openssl', 'ssl-cert' ]:
         ensure => 'latest',
     }
@@ -219,28 +217,32 @@ class certificates::base {
 
     # Server certificates now uniformly go in there
     file { '/etc/ssl/localcerts':
-        ensure => directory,
-        owner  => 'root',
-        group  => 'ssl-cert',
-        mode   => '0755',
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'ssl-cert',
+        mode    => '0755',
+        require => Package['ssl-cert'],
     }
 
-    ## NOTE: The ssl_certs abstraction for apparmor is known to exist
-    ## and be mutually compatible up to Trusty; new versions will need
-    ## validation before they are cleared.
+    if $::operatingsystem == 'Ubuntu' {
+        ## NOTE: The ssl_certs abstraction for apparmor is known to exist
+        ## and be mutually compatible up to Trusty; new versions will need
+        ## validation before they are cleared.
 
-    if versioncmp($::lsbdistrelease, '14.04') > 0 {
-        fail("The apparmor profile for certificates::base is only known to work up to Trusty")
-    }
-    file { '/etc/apparmor.d/abstractions/ssl_certs':
-        ensure => file,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => 'puppet:///files/ssl/ssl_certs',
-        notify => Service['apparmor'],
-    }
+        include apparmor
 
+        if versioncmp($::lsbdistrelease, '14.04') > 0 {
+            fail("The apparmor profile for certificates::base is only known to work up to Trusty")
+        }
+        file { '/etc/apparmor.d/abstractions/ssl_certs':
+            ensure => file,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            source => 'puppet:///files/ssl/ssl_certs',
+            notify => Service['apparmor'],
+        }
+    }
 }
 
 class certificates::star_wmflabs_org {
