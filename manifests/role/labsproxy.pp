77c21e5681dc841f9474db4624004f95ad251e90
dzahn <dzahn@wikimedia.org>
2014-12-09 14:37:00 +0100

    labsproxy: switch to use ssl_ciphersuite
    we want to remove RC4, per reasoning like in
    Icb7313c6f961b897
    
    but we also want this to use ssl_ciphersuite
    in the first place to use the global settings
    
    so we are doing that and using the "compat" variety
    and nginx obviously, mostly we just used this on Apaches
    so far
    
    there are 2 labsproxies, domainproxy and urlproxy,
    changed both templates, just once in the role
    
    Change-Id: I136fac78b45aaab3566e9876d302fb19727092bb

--- manifests/role/labsproxy.pp
+++ manifests/role/labsproxy.pp
@@ -3,8 +3,10 @@ class role::dynamicproxy::eqiad {
     install_certificate{ 'star.wmflabs.org':
         privatekey => false
     }
+
     class { '::dynamicproxy':
         ssl_certificate_name => 'star.wmflabs.org',
+        ssl_settings         => ssl_ciphersuite('nginx', 'compat'),
         set_xff              => true,
         resolver             => '10.68.16.1',
         require              => Install_certificate['star.wmflabs.org']
