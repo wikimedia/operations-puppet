969929904051ee534b8d78b6398cf9c9de0fa3cd
Faidon Liambotis <faidon@wikimedia.org>
2014-12-09 16:41:23 +0200

    ganglia_new: switch to stock upstart script
    Ganglia's packages in trusty include an upstart script, so there's no
    need to ship our own. For uniformity, replace ours with the trusty one,
    which unfortunately includes a switch to the pidfile's filename, so this
    means that ganglia-monitor will be restared once on all ganglia_new
    hosts.
    
    Make sure to stop provisioning the upstart script on trusty boxes, as
    it's better to rely on the packages doing so, in case they update in the
    future.
    
    This also adds compatibility with non-Ubuntu systems, as it does not
    depend on upstart at all.
    
    Change-Id: I56a5174db86a35647e943a69682caed07a50bac9

--- modules/ganglia_new/manifests/monitor/service.pp
+++ modules/ganglia_new/manifests/monitor/service.pp
@@ -1,18 +1,18 @@
 class ganglia_new::monitor::service() {
     Class[ganglia_new::monitor::config] -> Class[ganglia_new::monitor::service]
 
-    file { '/etc/init/ganglia-monitor.conf':
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => "puppet:///modules/${module_name}/upstart/ganglia-monitor.conf",
+    if $::operatingsystem == 'Ubuntu' and os_version('ubuntu < trusty') {
+        file { '/etc/init/ganglia-monitor.conf':
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            source => "puppet:///modules/${module_name}/upstart/ganglia-monitor.conf",
+            before => Service['ganglia-monitor'],
+        }
     }
-
-    generic::upstart_job { 'ganglia-monitor': }
 
     service { 'ganglia-monitor':
         ensure   => running,
         require  => File['/etc/init/ganglia-monitor.conf'],
-        provider => upstart,
     }
 }
