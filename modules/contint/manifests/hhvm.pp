3f3f21efa3c410953b118cb6b28f540099839d8b
Antoine Musso <hashar@free.fr>
2014-12-10 11:25:13 +0100

    (WIP IGNORE) contint: +hhvm
    Attempting to get hhvm on CI slaves.
    
    Change-Id: I924a42d2001edafa5ab00623b342afd87cfa5334

---
+++ modules/contint/manifests/hhvm.pp
@@ -0,0 +1,31 @@
+class contint::hhvm {
+
+    class { '::hhvm':
+        packages_ensure  => 'latest',
+
+        common_settings => {
+            hhvm => {
+                log => { use_syslog => false },
+            },
+        },
+
+        cli_settings => {
+            hhvm => {
+                repo => {
+                    local   => { mode => 'rw', },
+                    eval    => { mode => 'local' },
+                    journal => 'memory',
+                },
+            },
+        },
+
+        # We will want to bypass the Repo since code keep changing?
+        fcgi_settings => { },
+    }
+
+    alternatives::select { 'php':
+        path    => '/usr/bin/hhvm',
+        require => Package['hhvm'],
+    }
+
+}
