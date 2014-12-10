f2d9cc94d198fdc10c2a210c7743d915fdabfc7e
dzahn <dzahn@wikimedia.org>
2014-12-10 19:08:31 +0100

    move mediawiki maintenance scripts to module
    - delete misc/maintenance.pp because we want to get rid of the entire
    misc/ if possible.
    
    - rename it to mediawiki because all these are mw maintenance scripts.
    
    - move it into the mediawiki module in separate files.
    
    - instead of all the inclusions in site.pp put it in a small role
    
    - move files from files/scripts and other random locations into module.
    
    Bug:T88597
    
    Change-Id: Ie13bd7a80d8dd850cc93103aee174c15e5de469d

---
+++ modules/mediawiki/manifests/maintenance/echo_mail_batch.pp
@@ -0,0 +1,10 @@
+class mediawiki::maintenance::echo_mail_batch( $ensure = present ) {
+    cron { 'echo_mail_batch':
+        ensure  => $ensure,
+        command => '/usr/local/bin/foreachwiki extensions/Echo/maintenance/processEchoEmailBatch.php >/dev/null',
+        user    => $::mediawiki::users::web,
+        minute  => 0,
+        hour    => 0,
+    }
+}
+
