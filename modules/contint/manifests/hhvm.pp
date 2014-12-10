eccc72b1a3893d3bcd54966669c99c5f03cb030e
Antoine Musso <hashar@free.fr>
2014-12-10 11:25:13 +0100

    contint: provision hhvm on CI slaves
    We need some hhvm configuration on the CI Trusty slaves so we can run
    PHPUnit tests (T75521).
    
    Create a new class contint::hhvm which invokes ::hhvm with settings
    appropriate for CI.
    
    Do not use a local repo and only rely on central one. Its path is made
    empty to have hhvm fallback to HHVM_REPO_CENTRAL_PATH env variable. It
    is set in Jenkins global configuration to vary per build using
    $WORKSPACE.
    
    Bug: T75356
    Change-Id: I924a42d2001edafa5ab00623b342afd87cfa5334

---
+++ modules/contint/manifests/hhvm.pp
@@ -0,0 +1,35 @@
+class contint::hhvm {
+
+    class { '::hhvm':
+        # Never use a local repo. The central repo path is set to '' to have
+        # hhvm fall back to the env variable HHVM_REPO_CENTRAL_PATH. It is
+        # defined in the global Jenkins configuration and vary per build thanks
+        # to $WORKSPACE.
+        cli_settings => {
+            hhvm => {
+                repo  => {
+                    central => { path => '' },
+                    eval    => { mode => 'central' },
+                    local   => { mode => '--' },
+                    journal => 'memory',
+                },
+                log   => { use_syslog => false },
+                debug => { core_dump_report => false },
+            },
+        },
+
+        # We will want to bypass the Repo since code keep changing?
+        fcgi_settings => {
+            hhvm => {
+                log   => { use_syslog => false },
+                debug => { core_dump_report => false },
+            },
+        },
+    }
+
+    alternatives::select { 'php':
+        path    => '/usr/bin/hhvm',
+        require => Package['hhvm'],
+    }
+
+}
