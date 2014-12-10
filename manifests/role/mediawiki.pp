6179a7fef3a92c71dcce52245d7e71be4425f646
dzahn <dzahn@wikimedia.org>
2014-12-10 19:08:31 +0100

    move mediawiki maintenance scripts to module
    - delete misc/maintenance.pp because we want to get rid of the entire
    misc/ if possible.
    
    - rename it to mediawiki because all these are mw maintenance scripts.
    
    - move it into the mediawiki module in separate files.
    
    - instead of all the inclusions in site.pp put it in a small role
    
    - move files from files/scripts and other random locations into module.
    
    Bug:T88597
    
    Change-Id: Ie13bd7a80d8dd850cc93103aee174c15e5de469d

--- manifests/role/mediawiki.pp
+++ manifests/role/mediawiki.pp
@@ -179,3 +179,28 @@ class role::mediawiki::appserver::canary_api {
     salt::grain { 'canary': value => 'api_appserver' }
     include role::mediawiki::appserver::api
 }
+
+# mediawiki maintenance scripts
+class role::mediawiki::maintenance {
+
+    include mediawiki::maintenance::pagetriage
+    include mediawiki::maintenance::translationnotifications
+    include mediawiki::maintenance::updatetranslationstats
+    include mediawiki::maintenance::wikidata
+    include mediawiki::maintenance::echo_mail_batch
+    include mediawiki::maintenance::parsercachepurging
+    include mediawiki::maintenance::cleanup_upload_stash
+    include mediawiki::maintenance::tor_exit_node
+    include mediawiki::maintenance::update_flaggedrev_stats
+    include mediawiki::maintenance::refreshlinks
+    include mediawiki::maintenance::update_special_pages
+    include mediawiki::maintenance::update_article_count
+    include mediawiki::maintenance::purge_abusefilter
+    include mediawiki::maintenance::purge_checkuser
+    include mediawiki::maintenance::purge_securepoll
+
+    # (T17434) Periodical run of currently disabled special pages
+    include mediawiki::maintenance::updatequerypages
+
+}
+
