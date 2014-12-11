0a3c2083ae4b1cc8cb795561d1a4e1a72d01b690
Faidon Liambotis <faidon@wikimedia.org>
2014-12-11 07:15:03 +0200

    Add a new squid3 module and replace in-grown use
    install-server::caching-proxy and url_downloader both install &
    configure squid3 and are *very* similar to each other (in fact they seem
    derived from one another, then diverged a bit).
    
    Abstract away the squid3 bits into a simple squid3 module that accepts a
    configuration file or template and use it from the two modules.
    
    The new module doesn't have rspec tests but the callsites do. This
    should probably be fixed, by someone more knowledgeable in rspec, like
    the author of both of these other two modules *cough*.
    
    Change-Id: I86377761019fb65106ce56e99d8ba39f1dc4b10b

--- modules/install-server/manifests/caching-proxy.pp
+++ modules/install-server/manifests/caching-proxy.pp
@@ -13,31 +13,7 @@
 #   include install-server::caching-proxy
 
 class install-server::caching-proxy {
-    file { '/etc/squid3/squid.conf':
-        ensure  => present,
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        source  => 'puppet:///modules/install-server/squid3-apt-proxy.conf',
-        require => Package['squid3'],
-    }
-
-    file { '/etc/logrotate.d/squid3':
-        ensure  => present,
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        source  => 'puppet:///modules/install-server/squid3-logrotate',
-        require => Package['squid3'],
-    }
-
-    package { 'squid3':
-        ensure => installed,
-    }
-
-    service { 'squid3':
-        ensure    => running,
-        require   => [ Package['squid3'], File['/etc/squid3/squid.conf'] ],
-        subscribe => File['/etc/squid3/squid.conf'],
+    class { 'squid3':
+        config_source => 'puppet:///modules/install-server/squid3-apt-proxy.conf',
     }
 }
---
+++ modules/squid3/manifests/init.pp
@@ -0,0 +1,52 @@
+# Class: squid3
+#
+# This class installs squid3 and configures it
+#
+# Parameters:
+#
+# Actions:
+#       Install squid3 and configure it as a caching forward proxy
+#
+# Requires:
+#
+# Sample Usage:
+#   class { 'squid3': config_source => 'puppet:///modules/foo/squid3-foo.conf' }
+#   class { 'squid3': config_content => template('foo/squid3-foo.conf.erb') }
+
+
+class squid3(
+ $ensure  = present,
+ $config_content = undef,
+ $config_source  = undef,
+) {
+    validate_re($ensure, '^(present|absent)$')
+
+    file { '/etc/squid3/squid.conf':
+        ensure  => $ensure,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        source  => $config_source,
+        content => $config_content,
+        require => Package['squid3'],
+    }
+
+    file { '/etc/logrotate.d/squid3':
+        ensure  => $ensure,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        source  => 'puppet:///modules/squid3/squid3-logrotate',
+        require => Package['squid3'],
+    }
+
+    package { 'squid3':
+        ensure => $ensure,
+    }
+
+    service { 'squid3':
+        ensure    => ensure_service($ensure),
+        require   => File['/etc/squid3/squid.conf'],
+        subscribe => File['/etc/squid3/squid.conf'],
+    }
+}
