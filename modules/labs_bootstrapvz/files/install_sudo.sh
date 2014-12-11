0429f5409d365ae77546d94120805073199d6cb2
Andrew Bogott <abogott@wikimedia.org>
2014-12-11 02:36:21 +0000

    Support bootstrap-vz for buildign labs debian images
    Change-Id: I0b635305c0adb9d648fc30a393c22b1d4fc37116

---
+++ modules/labs_bootstrapvz/files/install_sudo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export SUDO_FORCE_REMOVE=yes
+apt-get --force-yes --assume-yes install sudo-ldap
---
+++ modules/labs_bootstrapvz/manifests/init.pp
@@ -0,0 +1,89 @@
+class labs_bootstrapvz() {
+    package { 'bootstrap-vz':
+        ensure => present,
+    }
+
+    $bootstrap_filepath = '/etc/bootstrap-vz/'
+
+    file { $bootstrap_filepath:
+        ensure => directory
+    }
+
+    file { "${bootstrap_filepath}/manifests":
+        ensure => directory,
+        require => File[$bootstrap_filepath],
+    }
+
+    file { "${bootstrap_filepath}/firstscripts":
+        ensure => directory,
+        require => File[$bootstrap_filepath],
+    }
+
+    file { "${bootstrap_filepath}/puppet":
+        ensure => directory,
+        require => File[$bootstrap_filepath],
+    }
+
+    file { "${bootstrap_filepath}/manifests/labs-jessie.manifest.yaml":
+        mode    => '0444',
+        source  => 'puppet:///modules/labs_bootstrapvz/labs-jessie.manifest.yaml',
+        require => File["${bootstrap_filepath}/manifests"],
+    }
+
+    file { "${bootstrap_filepath}/firstscripts/firstboot.sh":
+        mode    => '0555',
+        source  => 'puppet:///modules/labs_bootstrapvz/firstboot.sh',
+        require => File["${bootstrap_filepath}/firstscripts"],
+    }
+
+    file { "${bootstrap_filepath}/firstscripts/firstbootrc":
+        mode    => '0555',
+        source  => 'puppet:///modules/labs_bootstrapvz/firstbootrc',
+        require => File["${bootstrap_filepath}/firstscripts"],
+    }
+
+    file { "${bootstrap_filepath}/install_sudo.sh":
+        mode    => '0555',
+        source  => 'puppet:///modules/labs_bootstrapvz/install_sudo.sh',
+        require => [Package['bootstrap-vz'],
+                    File["${bootstrap_filepath}"],
+                    ],
+    }
+
+    $projectregex = "s/${instanceproject}/_PROJECT_/g"
+    $fqdnregex = "s/${::ec2id}.${::domain}/_FQDN_/g"
+    $masterregex = "s/${servername}/_MASTER_/g"
+
+    Exec { path => '/bin' }
+
+    exec { "cp /etc/security/access.conf ${bootstrap_filepath}/access.conf":
+    } ~>
+
+    exec { "sed -i '${projectregex}' ${bootstrap_filepath}/access.conf":
+    }
+
+    exec { "cp /etc/nslcd.conf ${bootstrap_filepath}/nslcd.conf":
+    } ~>
+
+    exec { "sed -i '${projectregex}' ${bootstrap_filepath}/nslcd.conf":
+    }
+
+    exec { "cp /etc/ldap/ldap.conf ${bootstrap_filepath}/nss_ldap.conf":
+    } ~>
+
+    exec { "sed -i '${projectregex}' ${bootstrap_filepath}/nss_ldap.conf":
+    }
+
+    exec { "cp /etc/puppet/puppet.conf ${bootstrap_filepath}/puppet/puppet.conf":
+        require => File["${bootstrap_filepath}/puppet"],
+    } ~>
+
+    exec { "sed -i '${projectregex}' ${bootstrap_filepath}/puppet/puppet.conf":
+    } ~>
+
+    exec { "sed -i '${fqdnregex}' ${bootstrap_filepath}/puppet/puppet.conf":
+    } ~>
+
+    exec { "sed -i '${masterregex}' ${bootstrap_filepath}/puppet/puppet.conf":
+    }
+}
