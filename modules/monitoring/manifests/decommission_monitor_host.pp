0e88a1a874c0022a9ff758fffbb874b2863329ff
dzahn <dzahn@wikimedia.org>
2014-12-11 21:25:41 +0100

    monitoring: move decom_host define into autoload
    ERROR: decommission_monitor_host not in autoload module layout
    
    make the file name match the class name to fix that
    
    and minor warnings:
     double quoted string containing no variables
     ensure found on line but it's not the first attribute 
    
    Change-Id: I3b852672e910d1efa2f920eb8ed556579d70530c

--- modules/monitoring/manifests/decommission_monitor_host.pp
+++ modules/monitoring/manifests/decommission_monitor_host.pp
@@ -1,5 +1,6 @@
-#TODO: this should probably go away. The cleanup scripts in puppet should do this by themselves.
-define decommission_monitor_host {
+#TODO: this should probably go away.
+# The cleanup scripts in puppet should do this by themselves.
+define monitoring::decommission_monitor_host {
     if defined(Nagios_host[$title]) {
         # Override the existing resources
         Nagios_host <| title == $title |> {
@@ -12,13 +13,13 @@ define decommission_monitor_host {
     else {
         # Resources don't exist in Puppet. Remove from Nagios config as well.
         nagios_host { $title:
+            ensure    => absent,
             host_name => $title,
-            ensure    => absent;
 
         }
         nagios_hostextinfo { $title:
+            ensure    => absent,
             host_name => $title,
-            ensure    => absent;
 
         }
     }
