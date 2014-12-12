66ba3f9ea7f6e53bd24066ed2cbac50ba03e1ba1
Alexandros Kosiaris <akosiaris@wikimedia.org>
2014-12-12 19:34:54 +0200

    Add README, RSpecs and tests for squid3 module
    The squid3 module relies on wmflib and stdlib for validate_re and
    ensure_service. This Rakefile honors that
    
    Change-Id: I2519f3ce6764590e7d7900c3975af80e6354f6a7

---
+++ modules/squid3/README.md
@@ -0,0 +1,27 @@
+# squid3 Puppet Module #
+
+A Puppet module for installing and managing the squid3 software. It is
+tailored to the needs of the Wikimedia Foundation and as such it is not
+very generic
+
+## Requirements ##
+- A Debian like distro (e.g. Ubuntu)
+- An understanding of the squid3 software
+
+## Notes ##
+
+The module relies on you providing the configuration file
+
+## Usage ##
+
+    class { 'squid3':
+        config_source => 'puppet:///files/squid3.conf',
+    }
+
+or
+
+    class { 'squid3':
+        config_content => template('squid.conf.erb'),
+    }
+
+and you are set
---
+++ modules/squid3/spec/classes/squid3_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'squid3', :type => :class do
+    it 'should have squid' do
+        contain_package('squid3').with_ensure('present')
+        contain_service('squid3').with_ensure('running')
+
+        should contain_file('/etc/squid3/squid.conf').with({
+            'ensure' => 'present',
+            'mode'   => '0444',
+            'owner'  => 'root',
+            'group'  => 'root',
+        }).without_path()
+
+        should contain_file('/etc/logrotate.d/squid3').with({
+            'ensure' => 'present',
+            'mode'   => '0444',
+            'owner'  => 'root',
+            'group'  => 'root',
+        }).without_path()
+    end
+end
