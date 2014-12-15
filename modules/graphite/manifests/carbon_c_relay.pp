fb45ca663d7ec3cb06fa0fabafba61d7ff51bb3a
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2014-12-15 17:55:21 +0100

    graphite: introduce carbon-c-relay
    The idea behind this change is to have a local carbon-c-relay instance (on port
    1903) whose sole task is to forward incoming metrics to the local carbon-cache,
    in practice replacing carbon-relay's role.
    
    In turn, this local carbon-c-relay will receive metrics from a frontend
    carbon-c-relay listening on standard port 2003 for line/plaintext protocol
    metrics.
    
    Bug: T85908
    
    Change-Id: I61f687237baed6674fcc9813acc9df0ce40d4fbb

---
+++ modules/graphite/manifests/carbon_c_relay.pp
@@ -0,0 +1,46 @@
+# == Class: graphite::carbon_c_relay
+#
+# This class configures carbon-c-relay to take carbon-relay's place in
+# routing metrics around using line-protocol.
+#
+# Two copies of carbon-c-relay are run:
+# * local-relay: takes metrics on port 1903 and forwards to all configured
+# carbon-cache processes on the local machine ('carbon-cache' list in config)
+# * frontend-relay: listen on standard port 2003 and mirror metrics to all
+# configured backends ('backends' list in config)
+#
+class graphite::carbon_c_relay(
+    $c_relay_settings,
+    ) {
+    package { 'carbon-c-relay':
+      ensure => installed,
+    }
+
+    base::service_unit { 'carbon/frontend-relay':
+        ensure        => present,
+        upstart       => true,
+        template_name => 'frontend-relay',
+    }
+
+    file { '/etc/carbon/frontend-relay.conf':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('graphite/frontend-relay.conf.erb'),
+        notify  => Service['carbon/frontend-relay'],
+    }
+
+    base::service_unit { 'carbon/local-relay':
+        ensure        => present,
+        upstart       => true,
+        template_name => 'local-relay',
+    }
+
+    file { '/etc/carbon/local-relay.conf':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('graphite/local-relay.conf.erb'),
+        notify  => Service['carbon/local-relay'],
+    }
+}
