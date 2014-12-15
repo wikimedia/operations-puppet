6627070f958a337a9e44a246f482d4f138750f17
Faidon Liambotis <faidon@wikimedia.org>
2014-12-15 13:21:38 +0200

    (WIP) Remove support for Ubuntu Lucid/10.04
    We still have three lucid hosts left so DO NOT MERGE.
    
    Change-Id: I49b16d717559c8126e2517bb5131dc0ae88e27bd

--- modules/mysql_wmf/manifests/packages.pp
+++ modules/mysql_wmf/manifests/packages.pp
@@ -7,51 +7,38 @@ class mysql_wmf::packages(
     $mariadb = false,
     ) {
 
-    if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '12.04') >= 0 {
-        if $mariadb == true {
-            file { '/etc/apt/sources.list.d/wikimedia-mariadb.list':
-                group  => 'root',
-                mode   => '0444',
-                owner  => 'root',
-                source => 'puppet:///modules/mysql_wmf/wikimedia-mariadb.list',
-            }
-
-            exec { 'update_mysql_apt':
-                subscribe   => File['/etc/apt/sources.list.d/wikimedia-mariadb.list'],
-                command     => '/usr/bin/apt-get update',
-                refreshonly => true,
-            }
-
-            package { [
-                'libmariadbclient18',
-                'mariadb-client-5.5',
-                'mariadb-server-5.5',
-                'mariadb-server-core-5.5',
-            ]:
-                ensure  => present,
-                require => File['/etc/apt/sources.list.d/wikimedia-mariadb.list'],
-            }
-
-        } else {
-            package { [
-                'libmysqlfbclient16',
-                'mysqlfb-client-5.1',
-                'mysqlfb-server-5.1',
-                'mysqlfb-server-core-5.1',
-            ]:
-                ensure => '5.1.53-fb3875-wm1',
-            }
+    if $mariadb == true {
+        file { '/etc/apt/sources.list.d/wikimedia-mariadb.list':
+            group  => 'root',
+            mode   => '0444',
+            owner  => 'root',
+            source => 'puppet:///modules/mysql_wmf/wikimedia-mariadb.list',
         }
-    }
 
-    if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '10.04') == 0 {
+        exec { 'update_mysql_apt':
+            subscribe   => File['/etc/apt/sources.list.d/wikimedia-mariadb.list'],
+            command     => '/usr/bin/apt-get update',
+            refreshonly => true,
+        }
+
         package { [
-            'libmysqlclient16',
-            'mysql-client-5.1',
-            'mysql-server-5.1',
-            'mysql-server-core-5.1',
+            'libmariadbclient18',
+            'mariadb-client-5.5',
+            'mariadb-server-5.5',
+            'mariadb-server-core-5.5',
         ]:
-            ensure => '5.1.53-fb3753-wm1',
+            ensure  => present,
+            require => File['/etc/apt/sources.list.d/wikimedia-mariadb.list'],
+        }
+
+    } else {
+        package { [
+            'libmysqlfbclient16',
+            'mysqlfb-client-5.1',
+            'mysqlfb-server-5.1',
+            'mysqlfb-server-core-5.1',
+        ]:
+            ensure => '5.1.53-fb3875-wm1',
         }
     }
 
--- modules/trebuchet/manifests/packages.pp
+++ modules/trebuchet/manifests/packages.pp
@@ -5,9 +5,7 @@
 class trebuchet::packages {
     include ::redis::client::python
 
-    if os_version('ubuntu > lucid') {
-        package { 'git-fat':
-            ensure => present,
-        }
+    package { 'git-fat':
+        ensure => present,
     }
 }
