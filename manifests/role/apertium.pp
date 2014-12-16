e2024e2bce6f0890be591152f636506de1b0130f
Kartik Mistry <kartik.mistry@gmail.com>
2014-12-16 13:57:29 +0530

    WIP: Add apertium role for production
    Bug: T78629
    Change-Id: Ic7419a9cb99e4436db96117a803f94d578e79f32

--- manifests/role/apertium.pp
+++ manifests/role/apertium.pp
@@ -1,10 +1,32 @@
 # vim: set ts=4 et sw=4:
 
-# We do not have monitoring yet
-#@monitoring::group { 'apertium_eqiad': description => 'eqiad apertium servers' }
+# Apertium is in a service cluster A.
+#@monitoring::group { 'sca_eqiad': description => 'Service Cluster A servers' }
 
-# Skipping production for now
-#class role::apertium::production {}
+class role::apertium::production {
+    system::role { 'role::apertium::production':
+        description => 'Apertium APY server'
+    }
+
+    include ::apertium
+
+    # Need to allow jenkins-deploy to reload apertium
+    sudo::user { 'jenkins-deploy': privileges => [
+        # Since the "root" user is local, we cant add the sudo policy in
+        # OpenStack manager interface at wikitech
+        'ALL = (root)  NOPASSWD:/usr/sbin/service apertium-apy restart',
+    ] }
+
+    # Define Apertium log directory and port
+    $log_dir = '/var/log/apertium'
+    $apertium_port = '2737'
+
+    # We have to explicitly open the apertium port (bug 45868)
+    ferm::service { 'apertium_http':
+        proto => 'tcp',
+        port  => $apertium_port,
+    }
+}
 
 class role::apertium::beta {
     system::role { 'role::apertium::beta':
