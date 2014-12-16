062686e1b2cfea9fdde051b4266f41d820234b13
Kartik Mistry <kartik.mistry@gmail.com>
2014-12-16 13:08:41 +0530

    WIP: Add cxserver role for production
    Create the cxserver role for production similar to Beta for cxserver. In
    addition this role has,
    
    * No jenkins/contint includes,
    * Added monitoring for port 8080.
    
    Bug: T78629
    Change-Id: If30737aa255ef8eeb7b51a865abef405086a9c97

--- manifests/role/cxserver.pp
+++ manifests/role/cxserver.pp
@@ -1,10 +1,63 @@
 # vim: set ts=4 et sw=4:
 
-# We do not have monitoring yet
-#@monitoring::group { 'cxserver_eqiad': description => 'eqiad cxserver servers' }
+# cxserver is in a service cluster A.
+@monitoring::group { 'sca_eqiad': description => 'Service Cluster A servers' }
 
-# Skipping production for now
-#class role::cxserver::production {}
+class role::cxserver::production {
+    system::role { 'role::cxserver::production':
+        description => 'cxserver server'
+    }
+
+    class { '::cxserver':
+        base_path => '/srv/deployment/cxserver/cxserver',
+        node_path => '/srv/deployment/cxserver/deploy/node_modules',
+        conf_path => '/srv/deployment/cxserver/config.js',
+        log_dir   => '/data/project/cxserver/log',
+        parsoid   => 'http://parsoid-lb.eqiad.wikimedia.org',
+        apertium  => 'http://apertium.wmflabs.org',
+        require   => [
+            File['/srv/deployment/cxserver'],
+            File['/data/project/cxserver']
+        ],
+    }
+
+    # Need to allow jenkins-deploy to reload cxserver
+    sudo::user { 'jenkins-deploy': privileges => [
+        # Since the "root" user is local, we cant add the sudo policy in
+        # OpenStack manager interface at wikitech
+        'ALL = (root)  NOPASSWD:/usr/sbin/service cxserver restart',
+    ] }
+
+    # Make sure the log directory parent exists on beta
+    file { '/data/project/cxserver':
+        ensure => directory,
+        owner  => cxserver,
+        group  => cxserver,
+        mode   => '0775',
+    }
+
+    # cxserver repositories and config under /srv/deployment/cxserver
+    file { '/srv/deployment/cxserver':
+        ensure => directory,
+        owner  => jenkins-deploy,
+        group  => wikidev,
+        mode   => '0755',
+    }
+
+    # Define cxserver port
+    $cxserver_port = '8080'
+
+    # We have to explicitly open the cxserver port (bug 45868)
+    ferm::service { 'cxserver_http':
+        proto => 'tcp',
+        port  => $cxserver_port,
+    }
+
+    monitor_service { 'cxserver':
+        description => 'cxserver',
+        check_command => 'check_http_on_port!8080',
+    }
+}
 
 class role::cxserver::beta {
     system::role { 'role::cxserver::beta':
