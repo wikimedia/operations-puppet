578796f5bddbd706f980884ded8cf79e96bdc365
Andrew Otto <acotto@gmail.com>
2014-12-16 15:12:12 -0500

    Disable automatic Hue user creation in production
    Change-Id: Iec99f2a9f5bffadba08ada9e2eda3edae3a17e29

--- manifests/role/analytics/hue.pp
+++ manifests/role/analytics/hue.pp
@@ -21,33 +21,37 @@ class role::analytics::hue {
         $hive_server_host = 'analytics1027.eqiad.wmnet'
         $ssl_private_key  = '/etc/ssl/private/hue.key'
         $ssl_certificate  = '/etc/ssl/certs/hue.cert'
+        # Disable automatic Hue user creation in production.
+        $ldap_create_users_on_login = false
     }
     elsif ($::realm == 'labs') {
         $secret_key       = 'oVEAAG5dp02MAuIScIetX3NZlmBkhOpagK92wY0GhBbq6ooc0B3rosmcxDg2fJBM'
         # Assume that in Labs, Hue should run on the main master Hadoop NameNode.
         $hive_server_host = $role::analytics::hadoop::config::namenode_hosts[0]
         # Disable ssl in labs.  Labs proxy handles SSL termination.
-        $ssl_private_key  = false
-        $ssl_certificate  = false
+        $ssl_private_key            = false
+        $ssl_certificate            = false
+        $ldap_create_users_on_login = true
     }
 
     class { 'cdh::hue':
-        hive_server_host       => $hive_server_host,
-        secret_key             => $secret_key,
-        smtp_host              => $::mail_smarthost[0],
-        smtp_from_email        => "hue@${::fqdn}",
-        ldap_url               => inline_template('<%= scope.lookupvar("ldap::role::config::labs::servernames").collect { |host| "ldaps://#{host}" }.join(" ") %>'),
-        ldap_bind_dn           => $ldap::role::config::labs::ldapconfig['proxyagent'],
-        ldap_bind_password     => $ldap::role::config::labs::ldapconfig['proxypass'],
-        ldap_base_dn           => $ldap::role::config::labs::basedn,
-        ldap_username_pattern  => 'uid=<username>,ou=people,dc=wikimedia,dc=org',
-        ldap_user_filter       => 'objectclass=person',
-        ldap_user_name_attr    => 'uid',
-        ldap_group_filter      => 'objectclass=posixgroup',
-        ldap_group_member_attr => 'member',
+        hive_server_host           => $hive_server_host,
+        secret_key                 => $secret_key,
+        smtp_host                  => $::mail_smarthost[0],
+        smtp_from_email            => "hue@${::fqdn}",
+        ldap_url                   => inline_template('<%= scope.lookupvar("ldap::role::config::labs::servernames").collect { |host| "ldaps://#{host}" }.join(" ") %>'),
+        ldap_bind_dn               => $ldap::role::config::labs::ldapconfig['proxyagent'],
+        ldap_bind_password         => $ldap::role::config::labs::ldapconfig['proxypass'],
+        ldap_base_dn               => $ldap::role::config::labs::basedn,
+        ldap_username_pattern      => 'uid=<username>,ou=people,dc=wikimedia,dc=org',
+        ldap_user_filter           => 'objectclass=person',
+        ldap_user_name_attr        => 'uid',
+        ldap_group_filter          => 'objectclass=posixgroup',
+        ldap_group_member_attr     => 'member',
+        ldap_create_users_on_login => $ldap_create_users_on_login,
         # Disable ssl in labs.  Labs proxy handles SSL termination.
-        ssl_private_key        => $ssl_private_key,
-        ssl_certificate        => $ssl_certificate,
+        ssl_private_key            => $ssl_private_key,
+        ssl_certificate            => $ssl_certificate,
     }
 }
 
