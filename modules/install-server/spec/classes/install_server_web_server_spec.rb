5d5d238580b1c2f910663ecee18c2e6288686d3a
Faidon Liambotis <faidon@wikimedia.org>
2014-12-17 12:46:49 +0200

    install-server: replace lighttpd with nginx
    nginx won the event-based-webservers war and we're already using it
    widely in other parts of the infrastructure. Kill lighttpd here.
    
    This doesn't handle the replacement gracefully within puppet and will
    fail if applied directly since both servers will try to bind to port 80.
    Since the class is only applied in one host (carbon), it's just simpler
    to do it manually.
    
    Update the RSpecs to test for the /srv/index.html file and update the
    Rakefile to use wmflib and nginx module.
    
    Change-Id: I71ca70321f847ffc6423b827033650407d82c7ef

--- modules/install-server/spec/classes/install_server_web_server_spec.rb
+++ modules/install-server/spec/classes/install_server_web_server_spec.rb
@@ -1,22 +1,12 @@
 require 'spec_helper'
 
 describe 'install-server::web-server', :type => :class do
-
-    it { should contain_package('lighttpd').with_ensure('latest') }
-    it { should contain_service('lighttpd').with_ensure('running') }
-
     it do
-        should contain_file('/etc/lighttpd/lighttpd.conf').with({
-            'mode'   => '0444',
-            'owner'  => 'root',
-            'group'  => 'root',
-        })
-    end
-    it do
-        should contain_file('/etc/logrotate.d/lighttpd').with({
-            'mode'   => '0444',
-            'owner'  => 'root',
-            'group'  => 'root',
+        should contain_file('/srv/index.html').with({
+            'mode'    => '0444',
+            'owner'   => 'root',
+            'group'   => 'root',
+            'content' => '',
         })
     end
 end
