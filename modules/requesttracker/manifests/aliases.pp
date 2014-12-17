ce47789c2ca1e86f873616dcd9fea348cf701007
Mark Bergsma <mark@wikimedia.org>
2014-12-17 22:36:16 +0100

    Create a special alias router for RT
    Moves the existing 'mailalias' Puppet type aliases to a simple
    static file. As in this case there's no existing file to manage,
    it's simpler.
    
    Change-Id: I99292b51dd8448c068f4adbee3773176e06d3063

--- modules/requesttracker/manifests/aliases.pp
+++ modules/requesttracker/manifests/aliases.pp
@@ -1,42 +1,5 @@
 class requesttracker::aliases {
-
-    mailalias { 'ops-request':
-        recipient => 'ops-private@lists.wikimedia.org',
-    }
-
-    mailalias { 'codfw':
-        recipient => 'ops-private@lists.wikimedia.org',
-    }
-
-    mailalias { 'core-ops':
-        recipient => 'ops-private@lists.wikimedia.org',
-    }
-
-    mailalias { 'eqiad':
-        recipient => 'ops-private@lists.wikimedia.org',
-    }
-
-    mailalias { 'esams':
-        recipient => 'ops-private@lists.wikimedia.org',
-    }
-
-    mailalias { 'network':
-        recipient => 'ops-private@lists.wikimedia.org',
-    }
-
-    mailalias { 'pmtpa':
-        recipient => 'ops-private@lists.wikimedia.org',
-    }
-
-    mailalias { 'todo':
-        recipient => 'ops-private@lists.wikimedia.org',
-    }
-
-    mailalias { 'ulsfo':
-        recipient => 'ops-private@lists.wikimedia.org',
-    }
-        
-    mailalias { 'phabricator':
-        recipient => 'rt@phabricator.wikimedia.org',
+    file { '/etc/exim4/aliases/rt':
+        source => "modules/requesttracker/rt.aliases"
     }
 }
