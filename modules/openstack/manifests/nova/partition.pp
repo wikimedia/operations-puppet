e353013c9fe798a497b4f4e73cc238070553ad1a
andrewbogott <abogott@wikimedia.org>
2014-12-17 03:45:39 -0600

    Remove the /srv mount point for /dev/sdb
    /dev/sdb will be /var/lib/nova/instances.  The /srv mount point
    didn't work, and wasn't good for anything anyway.
    
    Change-Id: I87f3708dd1a8ede7c6864ccf7466ef7bce6e11fa

--- modules/openstack/manifests/nova/partition.pp
+++ modules/openstack/manifests/nova/partition.pp
@@ -26,31 +26,4 @@ define openstack::nova::partition($partition_nr='1') {
         require => [Package['xfsprogs'], Exec["parted-${title}"]],
         unless  => "xfs_admin -l ${dev}",
     }
-
-    openstack::nova::mount_filesystem { $dev:
-        require => Exec["mkfs-${dev}"],
-    }
-}
-
-define openstack::nova::mount_filesystem {
-    $dev         = $title
-    $dev_suffix  = regsubst($dev, '^\/dev\/(.*)$', '\1')
-    $mount_point = '/srv'
-
-    file { "mountpoint-${mount_point}":
-        ensure => 'directory',
-        path   => $mount_point,
-        owner  => 'nova',
-        group  => 'nova',
-        mode   => '0750',
-    }
-
-    mount { $mount_point:
-        ensure   => 'present',
-        device   => "LABEL=virt-${dev_suffix}",
-        name     => $mount_point,
-        fstype   => 'xfs',
-        atboot   => true,
-        remounts => true,
-    }
 }
