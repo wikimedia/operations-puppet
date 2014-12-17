5208588afe1d53085ea9bd611f508289d671a45f
cpettet <rush@wikimedia.org>
2014-12-17 14:23:10 -0600

    rt redirect relevant queues to ops-private
    This is temporary as these addresses will go to
    Phabricator post-migration.
    
    Change-Id: Id0ec26dfaeb7c3c4dc4b9f9324e514594719ef7c

--- modules/requesttracker/manifests/aliases.pp
+++ modules/requesttracker/manifests/aliases.pp
@@ -1,11 +1,42 @@
 class requesttracker::aliases {
 
     mailalias { 'ops-request':
-        recipient => 'ops-requests@rt.wikimedia.org',
+        recipient => 'ops-private@lists.wikimedia.org',
     }
 
+    mailalias { 'codfw':
+        recipient => 'ops-private@lists.wikimedia.org',
+    }
+
+    mailalias { 'core-ops':
+        recipient => 'ops-private@lists.wikimedia.org',
+    }
+
+    mailalias { 'eqiad':
+        recipient => 'ops-private@lists.wikimedia.org',
+    }
+
+    mailalias { 'esams':
+        recipient => 'ops-private@lists.wikimedia.org',
+    }
+
+    mailalias { 'network':
+        recipient => 'ops-private@lists.wikimedia.org',
+    }
+
+    mailalias { 'pmtpa':
+        recipient => 'ops-private@lists.wikimedia.org',
+    }
+
+    mailalias { 'todo':
+        recipient => 'ops-private@lists.wikimedia.org',
+    }
+
+    mailalias { 'ulsfo':
+        recipient => 'ops-private@lists.wikimedia.org',
+    }
+        
     mailalias { 'phabricator':
         recipient => 'rt@phabricator.wikimedia.org',
     }
-
 }
