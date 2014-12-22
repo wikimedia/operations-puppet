be1eed25ab012c29fc7e40712fb44622ac6e384c
YuviPanda <yuvipanda@gmail.com>
2014-12-22 14:02:28 +0530

    tools: Add initial set of uwsgi nodes
    Do not add it to any queue yet, still experimental
    
    Change-Id: Ib621945c6ffc6406b0dbc6a51aa98c64bbc99e2b

---
+++ modules/toollabs/manifests/node/web/uwsgi.pp
@@ -0,0 +1,22 @@
+# Class: toollabs::node::web::uwsgi
+#
+# This configures the compute node as an uwsgi web server
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class toollabs::node::web::uwsgi inherits toollabs::node::web {
+
+    package { [ 
+        'uwsgi',
+        'uwsgi-plugin-python',
+        'uwsgi-plugin-python3',
+    ]:
+        ensure => latest,
+    }
+}
