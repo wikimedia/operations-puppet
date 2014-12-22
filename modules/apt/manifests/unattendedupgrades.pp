30ff559b0361784084264a1ac9c09184dea48ee6
andrewbogott <abogott@wikimedia.org>
2014-12-22 04:49:17 -0600

    Don't install update-notifier-common on Jessie
    It's no longer available on Debian:
    https://github.com/opscode-cookbooks/apt/issues/80
    
    Change-Id: I279240727850702cea9609cf2245c9619b32cfab

--- modules/apt/manifests/unattendedupgrades.pp
+++ modules/apt/manifests/unattendedupgrades.pp
@@ -1,6 +1,14 @@
 class apt::unattendedupgrades($ensure=present) {
-    package { [ 'unattended-upgrades', 'update-notifier-common' ]:
+    package { 'unattended-upgrades':
         ensure => $ensure,
+    }
+
+    # Package update-notifier-common is not available on Jessie.  I'm not
+    #  sure what (if anything) we should include instead.
+    if os_version('ubuntu > lucid') {
+        package { 'update-notifier-common':
+            ensure => $ensure,
+        }
     }
 
     apt::conf { 'auto-upgrades':
