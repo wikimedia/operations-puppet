7c35391d1bab3012e59e0e399162a5be4bdda57a
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2014-12-23 12:45:13 +0100

    puppet: enable the role-based backend in production
    Change-Id: I0afcd5799472fe89149ff8c18d9608b530f77683
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- modules/wmflib/lib/hiera/backend/role_backend.rb
+++ modules/wmflib/lib/hiera/backend/role_backend.rb
@@ -75,12 +75,24 @@ class Hiera
       end
 
       def get_path(key, role, source, scope)
+        config_section = :role
+
+        # Special case: 'private' repository.
+        # We use a different datadir in this case.
+        # Example: private/common will search in the role/common source
+        # within the private datadir
+        if m = /private\/(.*)/.match(source)
+          config_section = :private
+          source = m[1]
+        end
+
         # Variables for role::foo::bar will be searched in:
         # role/foo/bar.yaml
         # role/$::site/foo/bar.yaml
         # etc, depending on your hierarchy
         path = role.split('::').join('/')
         src = "role/#{source}/#{path}"
+
         # Use the datadir for the 'role' section of the config
         return Backend.datafile(:role, scope, src, "yaml")
       end
