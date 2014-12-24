ba1d04aae4df2f3e626395707fee667cd8006233
gage <jgerard@wikimedia.org>
2014-12-24 07:42:53 -0800

    Strongswan: IPsec Puppet module
    * Reuses Puppet client's own SSL certs
    * Depends on role::cache::configuration
    * Works on Precise, Trusty, and Jessie
    * See: T81543
    
    Change-Id: Idd6df93fb35bb4a86d0e92f48741a52a34ab25c7

---
+++ modules/strongswan/manifests/init.pp
@@ -0,0 +1,82 @@
+class strongswan {
+    case $::realm {
+        'labs': {
+            $wmf_fqdn = "${ec2_instance_id}.${domain}"
+        }
+        'production': {
+            $wmf_fqdn = "${fqdn}"
+        }
+    }
+
+    # used in template to enumerate hosts
+    # we should probably use hiera instead
+    require role::cache::configuration
+
+    package { [ 'strongswan', 'ipsec-tools' ]:
+    	ensure => present,
+    }
+
+    file { '/etc/ipsec.secrets':
+    	content => template('strongswan/ipsec.secrets.erb'),
+    	owner => 'root',
+    	group => 'root',
+    	mode => '600',
+    	notify => Service['strongswan'],
+    	require => Package['strongswan'],
+    }
+
+    file { '/etc/ipsec.conf':
+    	content => template('strongswan/ipsec.conf.erb'),
+    	owner => 'root',
+    	group => 'root',
+    	mode => '644',
+    	notify => Service['strongswan'],
+    	require => Package['strongswan'],
+    }
+
+    file { "/etc/ipsec.d/cacerts/ca.pem":
+        ensure => present,
+        source => "/var/lib/puppet/ssl/certs/ca.pem",
+    	notify => Service['strongswan'],
+    	require => Package['strongswan'],
+    }
+
+    file { "/etc/ipsec.d/certs/${wmf_fqdn}.pem":
+        ensure => present,
+        source => "/var/lib/puppet/ssl/certs/${wmf_fqdn}.pem",
+    	notify => Service['strongswan'],
+    	require => Package['strongswan'],
+    }
+
+    file { "/etc/ipsec.d/private/${wmf_fqdn}.pem":
+        ensure => present,
+        source => "/var/lib/puppet/ssl/private_keys/${wmf_fqdn}.pem",
+    	notify => Service['strongswan'],
+    	require => Package['strongswan'],
+    }
+
+    if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '14.04') >= 0 {
+        # for the purpose of this puppet module I have named the service strongswan because ipsec is ambiguous
+        service { 'strongswan':
+            # in Ubuntu/Trusty this service is /etc/init/strongswan.conf
+            ensure => running,
+            enable => true,
+            hasstatus => true,
+            hasrestart => true,
+            # upstart service is called strongswan but the IKEv2 daemon itself is called "charon"
+            pattern => "charon",
+        }
+    }
+    else {
+        service { 'strongswan':
+            # in Ubuntu/Preciase and Debian/Jessie this service is /etc/init.d/ipsec
+            name => 'ipsec',
+            ensure => running,
+            enable => true,
+            hasstatus => true,
+            hasrestart => true,
+            # upstart service is called strongswan but the IKEv2 daemon itself is called "charon"
+            pattern => "charon",
+        }
+    }
+}
