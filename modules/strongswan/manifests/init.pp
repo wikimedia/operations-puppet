b658577f6b77bebb4ed8ad8b426376580d92353a
gage <jgerard@wikimedia.org>
2014-12-24 07:42:53 -0800

    Strongswan: IPsec Puppet module
    * Defines role::ipsec
    * Reuses Puppet client's own SSL certs
    * Depends on role::cache::configuration
    * Works on Precise, Trusty, and Jessie
    * Enforces PFS
    * See: T81543
    
    Change-Id: Idd6df93fb35bb4a86d0e92f48741a52a34ab25c7

---
+++ modules/strongswan/manifests/init.pp
@@ -0,0 +1,70 @@
+class strongswan ($fqdn_pem) {
+    package { [ 'strongswan', 'ipsec-tools' ]:
+        ensure => present,
+    }
+
+    file { '/etc/ipsec.secrets':
+        content => template('strongswan/ipsec.secrets.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0400',
+        notify  => Service['strongswan'],
+        require => Package['strongswan'],
+    }
+
+    file { '/etc/ipsec.conf':
+        content => template('strongswan/ipsec.conf.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['strongswan'],
+        require => Package['strongswan'],
+    }
+
+    file { "/etc/ipsec.d/cacerts/ca.pem":
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        ensure  => present,
+        source  => "/var/lib/puppet/ssl/certs/ca.pem",
+        notify  => Service['strongswan'],
+        require => Package['strongswan'],
+    }
+
+    file { "/etc/ipsec.d/certs/${fqdn_pem}":
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        ensure  => present,
+        source  => "/var/lib/puppet/ssl/certs/${fqdn_pem}",
+        notify  => Service['strongswan'],
+        require => Package['strongswan'],
+    }
+
+    file { "/etc/ipsec.d/private/${fqdn_pem}":
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        ensure  => present,
+        source  => "/var/lib/puppet/ssl/private_keys/${fqdn_pem}",
+        notify  => Service['strongswan'],
+        require => Package['strongswan'],
+    }
+
+    $svcname = $::lsbdistcodename ? {
+        # in Ubuntu/Trusty this service is /etc/init/strongswan.conf
+        # in Ubuntu/Precise and Debian/Jessie it's /etc/init.d/ipsec
+        'trusty'  => 'strongswan',
+        'precise' => 'ipsec',
+        'jessie'  => 'ipsec',
+        default   => 'ipsec',
+    }
+    service { 'strongswan':
+        ensure     => running,
+        enable     => true,
+        name       => $svcname,
+        pattern    => "charon",  # Strongswan IKEv2 daemon is called charon
+        hasstatus  => true,
+        hasrestart => true,
+    }
+}
