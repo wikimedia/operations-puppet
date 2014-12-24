009f329387b348c05077eda9c622b2bdb6335bd2
gage <jgerard@wikimedia.org>
2014-12-24 07:42:53 -0800

    Strongswan: IPsec Puppet module
    * Defines role::ipsec
    * Reuses Puppet client's own SSL certs
    * Depends on role::cache::configuration
    * Works on Precise, Trusty, and Jessie
    * Enforces PFS
    * See: T81543
    
    Change-Id: Idd6df93fb35bb4a86d0e92f48741a52a34ab25c7

---
+++ modules/strongswan/lib/puppet/parser/functions/ipresolve.rb
@@ -0,0 +1,87 @@
+# == Function: ipresolve( string $name_to_resolve, bool $ipv6 = false)
+#
+# Performs a name resolution (for A AND AAAA records only) and returns
+# an hash of arrays.
+#
+# Takes one or more names to resolve, and returns an array of all the
+# A or AAAA records found. The resolution is actually only done when
+# the ttl has expired.
+#
+require 'resolv'
+
+class DNSCacheEntry
+  # Data structure for storing a DNS cached result.
+  def new(address, ttl)
+    @value = address
+    @ttl = Time.now.to_i + ttl
+  end
+
+  def is_valid?(time)
+    return @ttl > time
+  end
+
+  def value
+    return @value.to_s
+  end
+end
+
+class BasicTTLCache
+  def initialize
+    @cache = {}
+  end
+
+  def write(key, value, ttl)
+    @cache[key] = DNSCacheEntry.new(value, ttl)
+  end
+
+  def is_valid?(key)
+    # If the key exists, and its ttl has not expired, return true.
+    # Return false (and maybe clean up the stale entry) otherwise.
+    return false unless @cache.exists?key
+    t = Time.now.to_i
+    return true if @cache[key].is_valid?t
+    @cache.delete(key)
+    return false
+  end
+
+  def read(key, type)
+    if is_valid?key
+      return @cache[key].value
+    end
+    return nil
+  end
+end
+
+class DNSCached
+  def initialize(cache = nil)
+    @cache = cache || BasicTTLCache.new
+    @dns = Resolv::DNS.open()
+  end
+
+  def get_resource(name, type)
+    cache_key = "#{name}_#{type}"
+    res = @cache.read(cache_key)
+    if (res.nil?)
+      res = @dns.getresource(name, type)
+      @cache.write(cache_key, res.address, res.ttl)
+    end
+    res.address.to_s
+  end
+end
+
+
+module Puppet::Parser::Functions
+  dns = DNSCached.new
+  newfunction(:ipresolve, :type => :rvalue, :arity => 2) do |args|
+    name = args[0]
+    type = args[1]
+    if type == 4
+      source = Resolv::DNS::Resolve::IN::A
+    elsif type == 6
+      source = Resolv::DNS::Resolve::IN::AAAA
+    else
+      raise ArgumentError, 'Type must be 4 or 6'
+    end
+    return dns.get_resource(name, source)
+  end
+end
