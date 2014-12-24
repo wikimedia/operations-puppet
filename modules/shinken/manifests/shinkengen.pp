8dadd1c323831534dbac18ad5b3ab245a4cd5b85
YuviPanda <yuvipanda@gmail.com>
2014-12-24 18:01:15 +0530

    shinken: Provide ldap information for shinkengen
    Change-Id: I26db091a429ea945ae4d249323ae74478e9565df

--- modules/shinken/manifests/shinkengen.pp
+++ modules/shinken/manifests/shinkengen.pp
@@ -5,7 +5,22 @@
 #
 # FIXME: Also restarts shinkin on each run, even if no config
 # files have changed
-class shinken::shinkengen {
+#
+# = Parameters
+#
+# [*ldap_server*]
+#   LDAP server to use to grab information about hosts from
+#
+# [*ldap_bindas*]
+#   What to bind as when connecting to LDAP server
+#
+# [*ldap_pass*]
+#   Simple LDAP password to use when connecting to LDAP server
+class shinken::shinkengen(
+    $ldap_server = 'ldap-eqiad.wikimedia.org',
+    $ldap_bindas = 'cn=proxyagent,ou=profile,dc=wikimedia,dc=org',
+    $ldap_pass = $passwords::ldap::labs::proxypass,
+){
 
     include shinken
 
@@ -14,7 +29,7 @@ class shinken::shinkengen {
     }
 
     file { '/etc/shinkengen.yaml':
-        source  => 'puppet:///modules/shinken/shinkengen.yaml',
+        content => template('shinken/shinkengen.yaml.erb'),
         owner   => 'shinken',
         group   => 'shinken',
     }
