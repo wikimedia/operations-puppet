8bb87369f6f0675c022675c5a0252caa769a85af
gage <jgerard@wikimedia.org>
2014-12-24 07:42:53 -0800

    Strongswan: IPsec Puppet module
    * Defines role::ipsec
    * Reuses Puppet client's own SSL certs
    * Uses Hiera instead of role::cache::configuration
    * Works on Trusty and Jessie
    * Enforces PFS
    * See: T81543
    
    Change-Id: Idd6df93fb35bb4a86d0e92f48741a52a34ab25c7

---
+++ manifests/role/ipsec.pp
@@ -0,0 +1,49 @@
+class role::ipsec ($hosts = undef) {
+    case $::realm {
+        'labs': {
+            # labs nodes use their EC2 ID as their puppet cert name
+            $puppet_certname = "${ec2id}.${domain}"
+        }
+        default: {
+            $puppet_certname = "${fqdn}"
+        }
+    }
+
+    if $hosts != undef {
+        $targets = $hosts
+    } else {
+        # determine site from domain name
+        case $domain {
+            'eqiad.wmnet':         { $site = 'eqiad' }
+            'codfw.wmnet':         { $site = 'codfw' }
+            'esams.wmnet':         { $site = 'esams' }
+            'esams.wikimedia.org': { $site = 'esams' }
+            'ulsfo.wmnet':         { $site = 'ulsfo' }
+        }
+
+        # determine cache type based on whether it contains the local node
+        if $fqdn in hiera("text_${site}", "")   { $cachetype = "text" }
+        if $fqdn in hiera("bits_${site}", "")   { $cachetype = "bits" }
+        if $fqdn in hiera("upload_${site}", "") { $cachetype = "upload" }
+        if $fqdn in hiera("mobile_${site}", "") { $cachetype = "mobile" }
+
+        # enumerate hosts of the same cache type in other sites
+        if $site == "esams" or $site == "ulsfo" {
+            $targets = concat(
+                hiera("${cachetype}_eqiad", []),
+                hiera("${cachetype}_codfw", [])
+            )
+        }
+        if $site == "eqiad" or $site == "codfw" {
+            $targets = concat(
+                hiera("${cachetype}_esams", []),
+                hiera("${cachetype}_ulsfo", [])
+            )
+        }
+    }
+
+    class { '::strongswan':
+        puppet_certname     => $puppet_certname,
+        hosts               => $targets
+    }
+}
---
+++ modules/strongswan/spec/functions/ipresolve_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe 'ipresolve' do
+  before :each do
+    @compiler = Puppet::Parser::Compiler.new(Puppet::Node.new("foo"))
+    @scope = Puppet::Parser::Scope.new(@compiler)
+  end
+  it 'should be called with two parameters' do
+    should run.and_raise_error(ArgumentError)
+    should run.with_params(['google.com']).and_raise_error(ArgumentError)
+  end
+
+  it 'expects second parameter to be 4 or 6' do
+    should_not run.with_params('google.com', '4').and_raise_error(ArgumentError)
+    should_not run.with_params('google.com', '6').and_raise_error(ArgumentError)
+  end
+
+  it 'returns the resolved address as a string' do
+    r = Resolv::DNS::Resource::IN::A.new(Resolv::IPv4.create('74.125.29.113'))
+    Resolv::DNS.any_instance.stub(:getresource => r)
+    dns = DNSCached.new
+    dns.get_resource('google.com', Resolv::DNS::Resource::IN::A).should eq('74.125.29.113')
+  end
+
+  it 'uses cached results on subsequent lookups' do
+    r = Resolv::DNS::Resource::IN::A.new(Resolv::IPv4.create('74.125.29.113'))
+    resolv = double("Resolv::DNS", :getresource => r)
+    resolv.should_receive(:getresource).once
+    dns = DNSCached.new
+    dns.dns = resolv
+    dns.get_resource('google.com', Resolv::DNS::Resource::IN::A)
+    dns.get_resource('google.com', Resolv::DNS::Resource::IN::A)
+  end
+
+  it 'not uses cached results if ttl is zero' do
+    r = Resolv::DNS::Resource::IN::A.new(Resolv::IPv4.create('74.125.29.113'))
+    resolv = double("Resolv::DNS", :getresource => r)
+    resolv.should_receive(:getresource).twice
+    dns = DNSCached.new(nil, 0)
+    dns.dns = resolv
+    dns.get_resource('google.com', Resolv::DNS::Resource::IN::A)
+    dns.get_resource('google.com', Resolv::DNS::Resource::IN::A)
+  end
+
+  it 'uses cached result in case of failure' do
+    r = Resolv::DNS::Resource::IN::A.new(Resolv::IPv4.create('74.125.29.113'))
+    dns = DNSCached.new(nil,0)
+    dns.dns.stub(:getresource => r)
+    dns.get_resource('google.com', Resolv::DNS::Resource::IN::A)
+    dns.dns.stub(:getresource => 'ciao')
+    dns.get_resource('google.com', Resolv::DNS::Resource::IN::A).should eq('74.125.29.113')
+  end
+end
