f495a75567a19f3982866cc4435a5041ddbba453
Faidon Liambotis <faidon@wikimedia.org>
2014-12-31 21:00:08 +0200

    motd: manage /etc/motd, support Debian
    The history around /var/run/motd, /run/motd.dynamic, /etc/motd &
    /etc/motd.tail is messy and the current status quo in Debian proper an
    even worse mess. I followed-up multiple Debian bugs (#741129, #764841,
    
    https://lists.debian.org/debian-devel/2014/12/msg00368.html
    
    Until this plays out, workaround this on Debian systems from our motd
    module. Also DTRT with the different versions of Ubuntu LTS.
    
    Change-Id: I4a9450c7bc84ef751ca8a2ba30ace967e5ad2035

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -3,6 +3,23 @@
 # Module for customizing MOTD (Message of the Day) banners.
 #
 class motd {
+    # For Ubuntu trusty+, pam_motd is configured by sshd's pam configuration to
+    # read from /run directly and hence has no need for this.
+    #
+    # For Debian jessie (as of 2014-12-31) the situation is quite complicated:
+    # https://lists.debian.org/debian-devel/2014/12/msg00368.html
+    if os_version('debian <= jessie || ubuntu <= precise') {
+        file { '/etc/motd':
+            ensure => link,
+            target => '/var/run/motd',
+            force  => true,
+        }
+    } else {
+        file { '/etc/motd':
+            ensure => absent,
+        }
+    }
+
     file { '/etc/update-motd.d':
         ensure  => directory,
         owner   => 'root',
