6928ca99e2a3ff990cd56acce637b1342b12f3f8
Faidon Liambotis <faidon@wikimedia.org>
2014-12-31 20:46:30 +0200

    base: include the motd class by default
    This includes the motd class which recursively manages
    /etc/update-motd.d and ships common header/footers across distributions.
    
    This *will* remove update-notifier-common's prompts (packages & system
    restart). As we don't include this package by default and doesn't come
    installed by default with trusty it shouldn't matter much. Note that
    update-notifier is also abandoned upstream and Debian has dropped it
    already (in favor of PackageKit).
    
    On the longer-term, we might want to introduce helpers of our own for
    things like package updates and place it under an apt::motd class (and
    possibly coalesce the work in a single suite that also handles
    encompasses apt2xml).
    
    Change-Id: I586db667a79cfd36725de20153a91056f83a34a2

--- modules/base/manifests/motd.pp
+++
@@ -1,6 +0,0 @@
-class base::motd {
-    # Remove the standard help text
-    if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '10.04') >= 0 {
-        file { '/etc/update-motd.d/10-help-text': ensure => absent; }
-    }
-}
