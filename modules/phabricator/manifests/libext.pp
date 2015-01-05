c02dbc1f99d042c645061d381de4be94bfeb0561
cpettet <rush@wikimedia.org>
2015-01-05 16:34:14 -0600

    phab change lockfile name on git::install
    Must match in notify for git::install for the
    Exec resource.
    
    Change-Id: I5636e20e3123cbdb4a081408a0a3ebffe4be4695

--- modules/phabricator/manifests/libext.pp
+++ modules/phabricator/manifests/libext.pp
@@ -5,15 +5,17 @@
 
 define phabricator::libext ($rootdir, $libext_tag, $libext_lock_path, $libname = $name) {
 
+        # Build per extension lock file
+        $libname_lock = "${libext_lock_path}_${libname}"
+
         git::install { "phabricator/extensions/${libname}" :
             directory => "${rootdir}/libext/${libname}",
             git_tag   => $libext_tag,
-            lock_file => $libext_lock_path,
-            notify    => Exec[$libext_lock_path],
+            lock_file => $libname_lock,
+            notify    => Exec[$libname_lock],
             before    => Git::Install['phabricator/phabricator'],
         }
 
-        $libname_lock = "${libext_lock_path}_${libname}"
         exec {$libname_lock:
             command => "touch ${libname_lock}",
             unless  => "test -z ${libname_lock} || test -e ${libname_lock}",
