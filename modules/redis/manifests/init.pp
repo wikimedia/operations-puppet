9df770f4c8bd4eff0187eb6b77b61ab504d6399a
Ori Livneh <ori@wikimedia.org>
2015-01-05 19:24:00 -0800

    redis: Get rid of unused (and unhelpful) module parameters
    * Get rid of 'package', 'package_version', 'service_name' and 'config_template'
      arguments -- they were all unused.
    * Don't alias 'redis-server' to 'redis' -- it just adds misdirection.
    * Get rid of OS guards in redis module. The module is WMF-specific and not
      likely to be reusable by third parties, so there's no need to assert that the
      OS is Ubuntu or Debian.
    
    Change-Id: I4e6a7fbb2b98d8f01d6ad02c4d4a5e631f3af320

--- modules/redis/manifests/init.pp
+++ modules/redis/manifests/init.pp
@@ -1,4 +1,3 @@
-# application server base class
 class redis (
     $port = 6379,
     $dir = '/srv/redis',
@@ -9,28 +8,15 @@ class redis (
     $redis_options = {},
     $rename_commands = {},
     $redis_replication = undef,
-    $package = 'redis-server',
-    $package_version = 'present',
-    $servicename = 'redis-server',
     $monitor = true,
     $password = false,
     $auto_aof_rewrite_min_size = '512mb',
-    $config_template = 'redis/redis.conf.erb',
     $dbfilename = undef, # filename for rdb. If undef, "$hostname-$port.rdb" is used
     $saves = [ "900 1", "300 100", "60 10000" ], # Save points for rdb
     $stop_writes_on_bgsave_error = false
 ) {
-    case $::operatingsystem {
-        debian, ubuntu: {
-        }
-        default: {
-            fail("Module ${module_name} is not supported on ${::operatingsystem}")
-        }
-    }
-
-    package { 'redis':
-        ensure => $package_version,
-        name   => $package,
+    package { 'redis-server':
+        ensure => present,
     }
 
     file { $dir:
@@ -38,29 +24,27 @@ class redis (
         owner   => 'redis',
         group   => 'redis',
         mode    => '0755',
-        require => Package['redis'],
+        require => Package['redis-server'],
     }
 
     file { '/etc/redis/redis.conf':
-        content => template($config_template),
-        mode    => '0444',
+        content => template('redis/redis.conf.erb'),
         owner   => 'root',
         group   => 'root',
-        require => Package['redis'],
+        mode    => '0444',
+        require => Package['redis-server'],
     }
 
-    service { 'redis':
+    service { 'redis-server':
         ensure  => running,
-        name    => $servicename,
         enable  => true,
         require => File['/etc/redis/redis.conf'],
-        # subscribe => not doing this deliberately
     }
 
-    if $::lsbdistcodename == 'trusty' {
+    if os_version('ubuntu >= trusty || debian >= jessie') {
         # Upon a config change, Redis will be restarted
         # if it's listening on localhost only, see RT 7583
-        exec {'Restart redis if needed':
+        exec { 'Restart redis if needed':
             command     => '/usr/sbin/service redis-server restart',
             unless      => '/bin/netstat -lp | /bin/grep redis | /usr/bin/awk \'{print $4}\' | /bin/grep -v localhost 2> /dev/null',
             subscribe   => File['/etc/redis/redis.conf'],
@@ -69,6 +53,9 @@ class redis (
     }
 
     if $monitor {
-        monitoring::service { $servicename: description => "Redis", check_command => "check_tcp!${port}" }
+        monitoring::service { 'redis-server':
+            description   => 'Redis',
+            check_command => "check_tcp!${port}",
+        }
     }
 }
