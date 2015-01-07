c66c87b34e2a63377232bb9aadbeb14f5dab3054
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-01-07 17:16:10 +0200

    Package builder module
    A module used to create a Debian package builder environment base on
    cowbuilder. Intended to be used in Labs for streamlined Debian package
    building. Creates 4 environments to build packages in:
     * precise
     * trusty
     * jessie
     * sid
    
    Respective hooks for the environments are being installed as well and a
    mechanism to easily change between environment via an environment
    variable is provided. Tests included, as well as a README
    
    Change-Id: Ic8452ee447dcf485f6af822627cc117b3931b795

---
+++ modules/package_builder/manifests/init.pp
@@ -0,0 +1,129 @@
+# Class package_builder
+# Packaging environment building class
+#
+# Actions:
+#   Installs Debian package creation/building tools and creates environments to
+#   help with easy package building.
+#
+# Parameters:
+#   $basepath Untested, don't change it
+# Usage:
+#   include package_builder
+class package_builder(
+    $basepath='/var/cache/pbuilder',
+) {
+    package { [
+        'cowbuilder',
+        'build-essential',
+        'fakeroot',
+        'debhelper',
+        'devscripts',
+        'dh-make',
+        'dh-autoreconf',
+        'git-buildpackage',
+        'zip',
+        'unzip',
+        'debian-archive-keyring',
+        'ubuntu-archive-keyring',
+        ]:
+        ensure => present,
+    }
+
+    # Pristine environments
+    # amd64 architecture
+    package_builder::pbuilder_base { 'precise-amd64':
+        distribution => 'precise',
+        components   => 'main universe',
+        architecture => 'amd64',
+        mirror       => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
+        keyring      => '/usr/share/keyrings/ubuntu-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'trusty-amd64':
+        distribution => 'trusty',
+        components   => 'main universe',
+        architecture => 'amd64',
+        mirror       => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
+        keyring      => '/usr/share/keyrings/ubuntu-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'jessie-amd64':
+        distribution => 'jessie',
+        components   => 'main',
+        architecture => 'amd64',
+        mirror       => 'http://mirrors.wikimedia.org/debian',
+        keyring      => '/usr/share/keyrings/debian-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'sid-amd64':
+        distribution => 'sid',
+        components   => 'main',
+        architecture => 'amd64',
+        mirror       => 'http://mirrors.wikimedia.org/debian',
+        keyring      => '/usr/share/keyrings/debian-archive-keyring.gpg',
+    }
+    # i386 architecture
+    package_builder::pbuilder_base { 'precise-i386':
+        distribution => 'precise',
+        components   => 'main universe',
+        architecture => 'i386',
+        mirror       => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
+        keyring      => '/usr/share/keyrings/ubuntu-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'trusty-i386':
+        distribution => 'trusty',
+        components   => 'main universe',
+        architecture => 'i386',
+        mirror       => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
+        keyring      => '/usr/share/keyrings/ubuntu-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'jessie-i386':
+        distribution => 'jessie',
+        components   => 'main',
+        architecture => 'i386',
+        mirror       => 'http://mirrors.wikimedia.org/debian',
+        keyring      => '/usr/share/keyrings/debian-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'sid-i386':
+        distribution => 'sid',
+        components   => 'main',
+        architecture => 'i386',
+        mirror       => 'http://mirrors.wikimedia.org/debian',
+        keyring      => '/usr/share/keyrings/debian-archive-keyring.gpg',
+    }
+
+    # Wikimedia repos hooks
+    # Note: sid does not have a wikimedia repo and will never do
+    package_builder::pbuilder_hook { 'precise':
+        distribution => 'precise',
+        components   => 'main universe non-free thirdparty mariadb',
+    }
+
+    package_builder::pbuilder_hook { 'trusty':
+        distribution => 'trusty',
+        components   => 'main universe non-free thirdparty',
+    }
+
+    package_builder::pbuilder_hook { 'jessie':
+        distribution => 'jessie',
+        components   => 'main universe non-free thirdparty',
+    }
+
+    file { '/var/cache/pbuilder/hooks':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    file { '/etc/pbuilderrc':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('package_builder/pbuilderrc.erb'),
+    }
+
+    Package['cowbuilder'] -> File['/etc/pbuilderrc']
+    File['/etc/pbuilderrc'] -> File['/var/cache/pbuilder/hooks']
+    File['/var/cache/pbuilder/hooks'] -> Pbuilder_hook['precise']
+    File['/var/cache/pbuilder/hooks'] -> Pbuilder_hook['trusty']
+    File['/var/cache/pbuilder/hooks'] -> Pbuilder_hook['jessie']
+}
