9de084ffb8e3899752db5a4e2408901586f787f6
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-01-07 17:16:10 +0200

    Package builder module
    A module used to create a Debian package builder environment base on
    cowbuilder. Intended to be used in Labs for streamlined Debian package
    building. Creates 4 environments to build packages in:
     * precise
     * trusty
     * jessie
     * sid
    
    Change-Id: Ic8452ee447dcf485f6af822627cc117b3931b795

---
+++ modules/package_builder/manifests/pbuilder_base.pp
@@ -0,0 +1,33 @@
+define package_builder::pbuilder_base(
+    $distribution='trusty',
+    $components='main universe',
+    $mirror='http://nova.clouds.archive.ubuntu.com/ubuntu/',
+    $keyring=undef,
+) {
+    if $keyring {
+        $arg = "--debootstrapopts --keyring=$keyring"
+    } else {
+        $arg = ''
+    }
+    $basepath = '/var/cache/pbuilder/base'
+
+    $command = "/usr/sbin/cowbuilder --create \
+                        --mirror ${mirror} \
+                        --distribution ${distribution} \
+                        --components \"${components}\" \
+                        --basepath \"${basepath}-${distribution}.cow\" \
+                        ${arg}"
+
+    exec { "cowbuilder_init_${distribution}":
+        command => $command,
+        creates => "${basepath}-${distribution}.cow",
+    }
+
+    file { "/var/cache/pbuilder/hooks/D01apt.wikimedia.org-${distribution}":
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        content => template('package_builder/D01.apt.wikimedia.org.erb'),
+    }
+}
