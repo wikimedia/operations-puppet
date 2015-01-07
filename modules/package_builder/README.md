5bcce4188ec69d2a6f17fc92d62847be6c556acb
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-01-07 17:16:10 +0200

    Package builder module
    A module used to create a Debian package builder environment base on
    cowbuilder. Intended to be used in Labs for streamlined Debian package
    building. Creates 4 environments to build packages in:
     * precise
     * trusty
     * jessie
     * sid
    
    Respective hooks for the environments are being installed as well and a
    mechanism to easily change between environment via an environment
    variable is provided. Tests included, as well as a README
    
    Change-Id: Ic8452ee447dcf485f6af822627cc117b3931b795

---
+++ modules/package_builder/README.md
@@ -0,0 +1,39 @@
+Introduction
+============
+
+A module used to populate a Debian/Ubuntu package building environment. Meant to
+be used in the Wikimedia Labs environment but could be adapted for other
+environments as well.
+
+How to use
+==========
+
+Include the package\_builder class in your machine. That can be done with whatever
+ENC you have puppet working with but in Wikimedia Labs you should create a
+puppet group, then add the class in the puppet group and just configure your VM
+with that class
+
+After puppet is done you just either download a ready package and either:
+
+    export DIST=jessie
+    sudo -E pbuilder build dh-virtualenv_0.9-1.dsc
+
+or if you are developing a package and are in the package directory:
+
+    DIST=jessie pdebuild
+
+Feel free to change jessie for precise, trusty or sid
+
+Also ARCH=amd64, or ARCH=i386 is supported if you feel like building for
+different architecture versions.
+
+To use packages from the wikimedia repos to satisfy build dependencies during
+building you can use WIKIMEDIA=yes
+
+git-buildpackage
+================
+
+git-pbuilder can be used by git-buildpackage to leverage all of the above. The
+trick is to use GIT\_PBUILDER\_AUTOCONF=no i.e.:
+
+    GIT_PBUILDER_AUTOCONF=no DIST=trusty WIKIMEDIA=yes git-buildpackage -us -uc --git-builder=git-pbuilder
---
+++ modules/package_builder/manifests/environments.pp
@@ -0,0 +1,63 @@
+# Class package_builder::environments
+# A wrapper class for package::pbuilder_base. Mostly exists to make the
+# addition of new distributions as easy as possible
+class package_builder::environments {
+    # amd64 architecture
+    package_builder::pbuilder_base { 'precise-amd64':
+        distribution => 'precise',
+        components   => 'main universe',
+        architecture => 'amd64',
+        mirror       => 'http://mirrors.wikimedia.org/ubuntu',
+        keyring      => '/usr/share/keyrings/ubuntu-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'trusty-amd64':
+        distribution => 'trusty',
+        components   => 'main universe',
+        architecture => 'amd64',
+        mirror       => 'http://mirrors.wikimedia.org/ubuntu',
+        keyring      => '/usr/share/keyrings/ubuntu-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'jessie-amd64':
+        distribution => 'jessie',
+        components   => 'main',
+        architecture => 'amd64',
+        mirror       => 'http://mirrors.wikimedia.org/debian',
+        keyring      => '/usr/share/keyrings/debian-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'sid-amd64':
+        distribution => 'sid',
+        components   => 'main',
+        architecture => 'amd64',
+        mirror       => 'http://mirrors.wikimedia.org/debian',
+        keyring      => '/usr/share/keyrings/debian-archive-keyring.gpg',
+    }
+    # i386 architecture
+    package_builder::pbuilder_base { 'precise-i386':
+        distribution => 'precise',
+        components   => 'main universe',
+        architecture => 'i386',
+        mirror       => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
+        keyring      => '/usr/share/keyrings/ubuntu-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'trusty-i386':
+        distribution => 'trusty',
+        components   => 'main universe',
+        architecture => 'i386',
+        mirror       => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
+        keyring      => '/usr/share/keyrings/ubuntu-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'jessie-i386':
+        distribution => 'jessie',
+        components   => 'main',
+        architecture => 'i386',
+        mirror       => 'http://mirrors.wikimedia.org/debian',
+        keyring      => '/usr/share/keyrings/debian-archive-keyring.gpg',
+    }
+    package_builder::pbuilder_base { 'sid-i386':
+        distribution => 'sid',
+        components   => 'main',
+        architecture => 'i386',
+        mirror       => 'http://mirrors.wikimedia.org/debian',
+        keyring      => '/usr/share/keyrings/debian-archive-keyring.gpg',
+    }
+}
