946a31e14d93fe7af1b751973384115775c65b2f
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-01-07 17:16:10 +0200

    Package builder module
    A module used to create a Debian package builder environment base on
    cowbuilder. Defaulting to trusty for now
    
    Change-Id: Ic8452ee447dcf485f6af822627cc117b3931b795

---
+++ modules/package_builder/manifests/init.pp
@@ -0,0 +1,35 @@
+class package_builder($version='trusty', components='main universe') {
+    package { [
+        'cowbuilder',
+        'build-essential',
+        'fakeroot',
+        'debhelper',
+        'devscripts',
+        'dh-make',
+        'git-buildpackage',
+        'zip',
+        'unzip',
+        ]:
+        ensure => present,
+    }
+
+    exec { 'cowbuilder_init':
+        command => "/usr/sbin/cowbuilder --create --components \"${components}\"",
+        creates => '/var/cache/pbuilder/base.cow',
+    }
+
+    file { '/var/cache/pbuilder/hooks':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    file { '/var/cache/pbuilder/hooks/D01apt.wikimedia.org':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        content => template('package_builder/D01.apt.wikimedia.org.erb'),
+    }
+}
