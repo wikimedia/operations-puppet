242f47787f48e68edef617271ab8999e3a61f8df
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-01-07 17:16:10 +0200

    Package builder module
    A module used to create a Debian package builder environment base on
    cowbuilder. Intended to be used in Labs for streamlined Debian package
    building. Creates 4 environments to build packages in:
     * precise
     * trusty
     * jessie
     * sid
    
    Respective hooks for the environments are being installed as well and a
    mechanism to easily change between environment via an environment
    variable is provided. Tests included, as well as a README
    
    Change-Id: Ic8452ee447dcf485f6af822627cc117b3931b795

---
+++ modules/package_builder/manifests/pbuilder_base.pp
@@ -0,0 +1,29 @@
+# Definition pbuilder_base
+define package_builder::pbuilder_base(
+    $mirror='http://mirrors.wikimedia.org/debian',
+    $distribution='jessie',
+    $components='main',
+    $architecture='amd64',
+    $basepath='/var/cache/pbuilder',
+    $keyring=undef,
+) {
+    if $keyring {
+        $arg = "--debootstrapopts --keyring=${keyring}"
+    } else {
+        $arg = ''
+    }
+
+    $command = "/usr/sbin/cowbuilder --create \
+                        --mirror ${mirror} \
+                        --distribution ${distribution} \
+                        --components \"${components}\" \
+                        --architecture ${architecture} \
+                        --basepath \"${basepath}/base-${distribution}-${architecture}.cow\" \
+                        --debootstrapopts --variant=buildd \
+                        ${arg}"
+
+    exec { "cowbuilder_init_${distribution}-${architecture}":
+        command => $command,
+        creates => "${basepath}/base-${distribution}-${architecture}.cow",
+    }
+}
