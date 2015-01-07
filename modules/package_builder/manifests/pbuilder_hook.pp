01a746cb2c9f0e9f43901093ecff048e01914058
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-01-07 17:16:10 +0200

    Package builder module
    A module used to create a Debian package builder environment base on
    cowbuilder. Intended to be used in Labs for streamlined Debian package
    building. Creates 4 environments to build packages in:
     * precise
     * trusty
     * jessie
     * sid
    
    Respective hooks for the environments are being installed as well and a
    mechanism to easily change between environment via an environment
    variable is provided. Tests included, as well as a README
    
    Change-Id: Ic8452ee447dcf485f6af822627cc117b3931b795

---
+++ modules/package_builder/manifests/pbuilder_hook.pp
@@ -0,0 +1,22 @@
+# Definition pbuilder_hook
+define package_builder::pbuilder_hook(
+    $distribution='jessie',
+    $components='main',
+    $mirror='http://apt.wikimedia.org/wikimedia',
+    $basepath='/var/cache/pbuilder',
+) {
+    file { "${basepath}/hooks/${distribution}":
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+    }
+
+    file { "${basepath}/hooks/${distribution}/D01apt.wikimedia.org":
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        content => template('package_builder/D01apt.wikimedia.org.erb'),
+    }
+}
