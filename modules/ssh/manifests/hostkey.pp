817258a2922bcc55b207d1972b5f5e544bcfae68
Faidon Liambotis <faidon@wikimedia.org>
2015-01-08 18:32:31 +0200

    ssh: use sshkey's host_aliases instead of three keys
    Our intention is to export an SSH host key by hostname, FQDN & IP
    address. Right now, we're exporting these as three separate keys which
    result in three different file entries. There's actually no reason to do
    that, as the sshkey puppet type supports a "host_aliases" argument which
    corresponds to SSH's native aliases support. Use that instead.
    
    Change-Id: I0e91bd8a714350550c8b825effaca9726a44253e

--- modules/ssh/manifests/hostkey.pp
+++ modules/ssh/manifests/hostkey.pp
@@ -2,20 +2,9 @@ define ssh::hostkey($ip, $key, $type) {
     $host = regsubst($title, '^([^\.]+)\..*$', '\1')
 
     sshkey { $title:
-        ensure => present,
-        type   => $type,
-        key    => $key,
-    }
-
-    sshkey { $host:
-        ensure => present,
-        type   => $type,
-        key    => $key,
-    }
-
-    sshkey { $ip:
-        ensure => present,
-        type   => $type,
-        key    => $key,
+        ensure       => present,
+        type         => $type,
+        key          => $key,
+        host_aliases => [ $host, $ip ],
     }
 }
