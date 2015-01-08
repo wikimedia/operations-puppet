cb30e056d2428ec839ccd628c868df7571a271f1
Faidon Liambotis <faidon@wikimedia.org>
2015-01-08 18:39:14 +0200

    ssh: remove ssh::hostkey definition
    Now that we don't really need three different sshkey resources, there is
    really no need to supply our own thin wrapper against sshkey called
    ssh::hostkey. Inline it to the ssh::server class and simplify it. This
    also allows us to use $::hostname directly instead of regexping $::fqdn
    to extract it.
    
    Change-Id: I80bb3f9d91f2e22b3e6c80a777e1c7a4fad69576

--- modules/ssh/manifests/server.pp
+++ modules/ssh/manifests/server.pp
@@ -16,36 +16,32 @@ class ssh::server (
         $ssh_authorized_keys_file ='/etc/ssh/userkeys/%u/.ssh/authorized_keys /public/keys/%u/.ssh/authorized_keys'
     }
 
-    # publish this hosts's host key; prefer ed25519 -> ECDSA -> RSA (no DSA)
-    if $::sshed25519key {
-        debug("Storing ed25519 SSH hostkey for ${::fqdn}")
-        @@ssh::hostkey { $::fqdn:
-            ip   => $::ipaddress,
-            key  => $::sshed25519key,
-            type => 'ssh-ed25519',
-        }
-    } elsif $::sshecdsakey {
-        debug("Storing ECDSA SSH hostkey for ${::fqdn}")
-        @@ssh::hostkey { $::fqdn:
-            ip   => $::ipaddress,
-            key  => $::sshecdsakey,
-            type => 'ssh-ecdsa',
-        }
-    } elsif $::sshrsakey {
-        debug("Storing RSA SSH hostkey for ${::fqdn}")
-        @@ssh::hostkey { $::fqdn:
-            ip   => $::ipaddress,
-            key  => $::sshrsakey,
-            type => 'ssh-rsa',
-        }
-    } else {
-        err("No SSH host key found for ${::fqdn}")
-    }
-
     file { '/etc/ssh/sshd_config':
         owner   => root,
         group   => root,
         mode    => '0444',
-        content => template('ssh/sshd_config.erb');
+        content => template('ssh/sshd_config.erb'),
+    }
+
+    # publish this hosts's host key; prefer ed25519 -> ECDSA -> RSA (no DSA)
+    if $::sshed25519key {
+        $key  = $::sshed25519key
+        $type = 'ssh-ed25519'
+    } elsif $::sshecdsakey {
+        $key  = $::sshecdsakey
+        $type = 'ssh-ecdsa'
+    } elsif $::sshrsakey {
+        $key  = $::sshrsakey
+        $type = 'ssh-rsa'
+    } else {
+        err("No valid SSH host key found for ${::fqdn}")
+    }
+
+    debug("Storing ${type} SSH hostkey for ${::fqdn}")
+    @@sshkey { $::fqdn:
+        ensure       => present,
+        type         => $type,
+        key          => $key,
+        host_aliases => [ $::hostname, $::ipaddress ],
     }
 }
