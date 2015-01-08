bf852092d1498f9de25664722026dee130a13b5d
Faidon Liambotis <faidon@wikimedia.org>
2015-01-08 19:51:09 +0200

    ssh: do not export ssh-ed25519 keys
    Puppet's sshkey provider tries to be smart and hardcodes key types that
    it understands. While facter & puppet support came roughly at the time,
    we export and collect the keys in different systems and a system with an
    older puppet version may not understand a newer keytype.
    
    This is preventing us from exporting ssh-ed25519 keys from jessie as
    consuming them fails from precise & trusty hosts :(
    
    Change-Id: I6e4ba2c958299e06fd5b9140dddb2575956c989f

--- modules/ssh/manifests/server.pp
+++ modules/ssh/manifests/server.pp
@@ -23,11 +23,16 @@ class ssh::server (
         content => template('ssh/sshd_config.erb'),
     }
 
-    # publish this hosts's host key; prefer ed25519 -> ECDSA -> RSA (no DSA)
-    if $::sshed25519key {
-        $key  = $::sshed25519key
-        $type = 'ssh-ed25519'
-    } elsif $::sshecdsakey {
+    # publish this hosts's host key; prefer ECDSA -> RSA (no DSA)
+    #
+    # Puppet's sshkey provider tries to be smart and hardcodes key types that
+    # it understands. While facter & puppet support came roughly at the same
+    # time, here we only export the keys and the system that collects them may
+    # have an older puppet version that doesn't understand a newer keytype.
+    #
+    # This is preventing us from exporting ed25519 keys from jessie hosts as
+    # consuming them fails from precise & trusty hosts :(
+    if $::sshecdsakey {
         $key  = $::sshecdsakey
         $type = 'ecdsa-sha2-nistp256'
     } elsif $::sshrsakey {
