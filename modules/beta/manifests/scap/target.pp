bdb5d55bdbf87e2a520ce34e16e484f0e831df45
Faidon Liambotis <faidon@wikimedia.org>
2015-01-08 19:40:42 +0200

    ssh: introduce ssh::userkey resource
    Labs is currently using /etc/ssh/userkeys as one of the two locations to
    place SSH user keys (the other being /public/keys, on NFS).
    
    The directory is created with a File resource in the private repository
    (because it was initially used only for the root user) but files are
    placed underneath it here as well, in the beta module. Move the
    directory file resource to ssh::server class where it belongs and
    puppet-manage it. WARNING: this needs a corresponding labs-private
    change to avoid a conflict.
    
    Moreover, instead of provisioning three File resources (userkeys/root,
    userkeys/root/.ssh, userkeys/root/.ssh/authorized_keys) for each of root
    & mwdeploy, be DRY and create a new definition, ssh::userkey that does
    this for us. Switch mwdeploy to using that, significantly simplyifying
    it. This also needs an corresponding labs-private change for root.
    
    Bug: T92475b
    Change-Id: I045695dd37b3920ca10be91a28aec5beee709327

--- modules/beta/manifests/scap/target.pp
+++ modules/beta/manifests/scap/target.pp
@@ -8,26 +8,8 @@ class beta::scap::target {
     include ::mediawiki::users
 
     # Install authorized_keys for mwdeploy user
-    file { '/etc/ssh/userkeys/mwdeploy':
-        ensure  => directory,
-        owner   => 'mwdeploy',
-        group   => 'mwdeploy',
-        mode    => '0700',
-        require => File['/etc/ssh/userkeys'],
-    }
-    file { '/etc/ssh/userkeys/mwdeploy/.ssh':
-        ensure  => directory,
-        owner   => 'mwdeploy',
-        group   => 'mwdeploy',
-        mode    => '0700',
-        require => File['/etc/ssh/userkeys/mwdeploy'],
-    }
-    file { '/etc/ssh/userkeys/mwdeploy/.ssh/authorized_keys':
-        owner   => 'mwdeploy',
-        group   => 'mwdeploy',
-        mode    => '0600',
+    ssh::userkey { 'mwdeploy':
         source  => 'puppet:///private/scap/id_rsa.pub',
-        require => File['/etc/ssh/userkeys/mwdeploy/.ssh'],
     }
 
     # Hack to replace /etc/security/access.conf (which is managed by the
