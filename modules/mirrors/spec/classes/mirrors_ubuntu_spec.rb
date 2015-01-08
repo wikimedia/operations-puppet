9fe5241e194e240cac22f68a79fb21b334b9f9a2
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-01-08 16:05:47 +0200

    RSpec install-server fixes
    ubuntu-mirror has been removed from the install-server module and its
    functionality moved into the mirrors module. Update the install-server
    module's RSpec tests to reflect that and create RSpec tests for the
    mirrors module. Update the tests as well and fix a latest=>present
    ensure on atftpd as well
    
    Change-Id: I8f959b6eaae3018b1f479d33557c581320f05b44

---
+++ modules/mirrors/spec/classes/mirrors_ubuntu_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'mirrors::ubuntu', :type => :class do
+
+    it do
+        should contain_file('/srv/ubuntu/').with({
+            'ensure' => 'directory',
+            'owner'  => 'mirror',
+            'group'  => 'mirror',
+            'mode'   => '0755',
+        })
+    end
+    it do
+        should contain_file('/usr/local/sbin/update-ubuntu-mirror').with({
+            'ensure'  => 'present',
+            'owner'   => 'root',
+            'group'   => 'root',
+            'mode'    => '0555',
+            'source'  => 'puppet:///modules/mirrors/update-ubuntu-mirror',
+        })
+    end
+    it do
+        should contain_cron('update-ubuntu-mirror').with({
+            'ensure'  => 'present',
+            'command' => '/usr/local/sbin/update-ubuntu-mirror 1>/dev/null 2>/var/lib/mirror/mirror.err.log',
+            'user'    => 'mirror',
+            'hour'    => '*/6',
+            'minute'  => '43',
+        })
+    end
+end
