a5c44d1c185dc6644b39bded7eb9b10ebb44a53b
Faidon Liambotis <faidon@wikimedia.org>
2015-01-08 03:37:18 +0200

    mirrors: introduce mirrors::debian and use it
    Introduce a mirrors::debian class that mirrors a Debian ftp. This uses
    Debian's official mirrors scripts, archvsync (aka ftpsync). Include the
    class from the respective role class and while at it, add monitoring
    based on the preexisting Ubuntu check_apt_mirror, which now takes a
    parameter. Note that the mirror pulse for Debian is set also every 6
    hours but 40' far apart from the Ubuntu pulse as to avoid overlap
    between the two.
    
    It should be noted that instead of archvsync, debmirror was also
    explored. While debmirror was more well-documented and packaged, in the
    end it seemed more oriented to "local" mirrors and tried to be too smart
    in this regard. There were multiple occurences of this but the tipping
    point was when debmirror choked when "experimental" had failing
    checksums and ended up not syncing *any* suite.
    
    So, stick with archvsync, which while a bit crude will allow us to
    potentially become a (partial) official mirror.
    
    Finally, the mirrors module in general could probably use some extra
    love as it's not very configurable right now and hardcodes a bunch of
    settings. It isn't /too/ bad and should do it for now, though.
    
    Change-Id: Icafeaa3d0c80d5249bcc2027471a56bc8a487395

---
+++ modules/mirrors/manifests/debian.pp
@@ -0,0 +1,55 @@
+# Class: mirrors::debian
+#
+# This class sets up a Debian mirror
+#
+# Parameters:
+#
+# Actions:
+#       Installs Debian's archiving scripts and runs them periodically
+#
+# Requires:
+#
+# Sample Usage:
+#   include mirrors::debian
+
+class mirrors::debian {
+    require mirrors
+
+    file { '/srv/debian/':
+        ensure  => directory,
+        owner   => 'mirror',
+        group   => 'mirror',
+        mode    => '0755',
+    }
+
+    # this is <https://ftp-master.debian.org/git/archvsync.git>
+    # right now we just ship bin/ftpsync & etc/common; if more are needed in
+    # the future this should probably become a git::install resource
+    file { "${mirrors::homedir}/archvsync":
+        ensure  => directory,
+        recurse => true,
+        purge   => true,
+        owner   => 'mirror',
+        group   => 'mirror',
+        mode    => '0755',
+        source  => 'puppet:///modules/mirrors/archvsync',
+    }
+
+    # this is our configuration for archvsync
+    file { "${mirrors::homedir}/archvsync/etc/ftpsync.conf":
+        ensure  => present,
+        owner   => 'mirror',
+        group   => 'mirror',
+        mode    => '0555',
+        source  => 'puppet:///modules/mirrors/ftpsync.conf',
+    }
+
+    cron { 'update-debian-mirror':
+        ensure  => present,
+        command => '/var/lib/mirror/archvsync/bin/ftpsync',
+        user    => 'mirror',
+        hour    => '*/6',
+        minute  => '03',
+        require => File['/var/lib/mirror/archvsync/etc/ftpsync.conf'],
+    }
+}
