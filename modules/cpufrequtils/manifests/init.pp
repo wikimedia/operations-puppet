366f72063569af100dcdc11babe7ad0ff9c8bd75
Faidon Liambotis <faidon@wikimedia.org>
2015-01-09 08:36:21 +0200

    cpufrequtils: move Ubuntu stupidity under if guard
    Change-Id: I24ce0a31221418c90d71337265b1d6468637ca01

--- modules/cpufrequtils/manifests/init.pp
+++ modules/cpufrequtils/manifests/init.pp
@@ -1,14 +1,6 @@
 class cpufrequtils (
     $governor = 'performance',
 ) {
-    case $::operatingsystem {
-        debian, ubuntu: {
-        }
-        default: {
-            fail("Module ${module_name} is not supported on ${::operatingsystem}")
-        }
-    }
-
     package { 'cpufrequtils':
         ensure => present,
     }
@@ -22,8 +14,12 @@ class cpufrequtils (
     # Ubuntu's default initscripts package includes a service called "ondemand"
     #   which is a one-shot action invoked at startup which sleeps 60 seconds
     #   and then sets all CPUs to the ondemand governor, thus undoing the work
-    #   of cpufrequtils :P
-    service { 'ondemand': enable => false }
+    #   of cpufrequtils. Debian has no such stupidity.
+    if $::operatingsystem == 'Ubuntu' {
+        service { 'ondemand':
+            enable => false,
+        }
+    }
 
     file { '/etc/default/cpufrequtils':
         content => "GOVERNOR=${governor}\n",
