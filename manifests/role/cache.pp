41c3484b6cd462e9f72cd5b2f6ccf06020870d2e
Christian Aistleitner <christian@quelltextlich.at>
2015-01-10 22:01:58 +0100

    Add logs from 'misc' caches to kafka pipeline
    Change-Id: Ia3712a0d85ffa893912af2dff312017f03a7a935

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -1590,5 +1590,15 @@ class role::cache {
                 ]
             },
         }
+
+        # ToDo: Remove production conditional once this works
+        # is verified to work in labs.
+        if $::realm == 'production' {
+            # Install a varnishkafka producer to send
+            # varnish webrequest logs to Kafka.
+            class { 'role::cache::varnish::kafka::webrequest':
+                topic => 'webrequest_misc',
+            }
+        }
     }
 }
