5d1277a378931107149cbc021c1a203344299f7f
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-01-13 10:13:02 +0100

    mediawiki: raise local_port_range for the api pool
    The api pool has a higher load per server than the traditional
    appservers, and we see frequent memcached connection errors on the
    higher load servers. We think this is due to starvation of ephemeral
    ports, due also to the fact we're now using ports for connecting via
    fastcgi. We extend the local port range to start just above the
    twemproxy admin port. This should at least alleviate the problem.
    
    Change-Id: Ic2fcc21cd771bc68541167b11d6088e3f661a962
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- manifests/role/mediawiki.pp
+++ manifests/role/mediawiki.pp
@@ -97,6 +97,14 @@ class role::mediawiki::appserver::api {
     system::role { 'role::mediawiki::appserver::api': }
 
     include ::role::mediawiki::webserver
+
+    # Using fastcgi we need more local ports
+    sysctl::parameters { 'raise_port_range':
+        values => {
+            'net.ipv4.local_port_range' => '22500 65535',
+        },
+        priority => 90,
+    }
 }
 
 class role::mediawiki::imagescaler {
