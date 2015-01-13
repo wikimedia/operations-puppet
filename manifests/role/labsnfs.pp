7b10fd23e06b2c9f4212f3b4d0b3ee8ebbbada2d
Faidon Liambotis <faidon@wikimedia.org>
2015-01-13 20:37:05 +0200

    Kill role::labsnfs, deprecated & empty
    role::labsnfs::client is empty with only one warning() statement
    referring to the long-gone pmtpa. Keeping those classes around doesn't
    make sense anyway as Labs configuration can be easily edited via LDAP
    directly and it make even less sense to keep them after 3 months have
    passed. Kill the file.
    
    Change-Id: I69c0fd6f0d3c2a56b750dd09178134f5fd4d6fa1

--- manifests/role/labsnfs.pp
+++
@@ -1,8 +0,0 @@
-# This is a transition role - Eventually this will end up
-# in LDAP once transition is over.
-
-class role::labsnfs::client {
-
-    warning('Outside of pmtpa the role::labsnfs::client class is deprecated and does nothing.')
-}
-
