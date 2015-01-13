cf979278918563da59e4e3cf50611b474f3caf0f
YuviPanda <yuvipanda@gmail.com>
2015-01-13 15:36:06 +0530

    cxserver: Unify production and beta roles
    Bug: T86633
    Change-Id: I74ee57e9e10aac2f7df7c3c09052afbaf901e4d0

--- manifests/role/cxserver.pp
+++ manifests/role/cxserver.pp
@@ -1,7 +1,9 @@
 # vim: set ts=4 et sw=4:
 
-class role::cxserver::production {
-    system::role { 'role::cxserver::production':
+class role::cxserver (
+    $port = 8080,
+) {
+    system::role { 'role::cxserver':
         description => 'content translation server'
     }
 
@@ -14,51 +16,19 @@ class role::cxserver::production {
         apertium  => 'http://apertium.svc.eqiad.wmnet',
     }
 
-    # Define cxserver port
-    $cxserver_port = hiera('role::cxserver::cxserver_port', 8080)
-
     # We have to explicitly open the cxserver port (bug T47868)
     ferm::service { 'cxserver_http':
         proto => 'tcp',
-        port  => $cxserver_port,
+        port  => $port,
     }
 
     monitoring::service { 'cxserver':
         description   => 'cxserver',
-        check_command => "check_http_on_port!${cxserver_port}",
+        check_command => "check_http_on_port!${port}",
     }
 }
 
-class role::cxserver::beta {
-    system::role { 'role::cxserver::beta':
-        description => 'content translation server (on beta)'
-    }
-
-    class { '::cxserver':
-        base_path => '/srv/deployment/cxserver/cxserver',
-        node_path => '/srv/deployment/cxserver/deploy/node_modules',
-        conf_path => '/srv/deployment/cxserver/config.js',
-        log_dir   => '/data/project/cxserver/log',
-        parsoid   => 'http://parsoid-lb.eqiad.wikimedia.org',
-        apertium  => 'http://apertium-beta.wmflabs.org',
-    }
-
-    # Need to allow jenkins-deploy to reload cxserver
-    sudo::user { 'jenkins-deploy': privileges => [
-        # Since the "root" user is local, we cant add the sudo policy in
-        # OpenStack manager interface at wikitech
-        'ALL = (root)  NOPASSWD:/usr/sbin/service cxserver restart',
-    ] }
-
-    # Define cxserver port
-    $cxserver_port = hiera('role::cxserver::cxserver_port', 8080)
-
-    # We have to explicitly open the cxserver port (bug 45868)
-    ferm::service { 'cxserver_http':
-        proto => 'tcp',
-        port  => $cxserver_port,
-    }
-
+class role::cxserver::jenkins_access {
     # Allow ssh access from the Jenkins master to the server where cxserver is
     # running
     include contint::firewall::labs
