17cddfd290c53790d1fb45e9e0cdfa8913283c28
YuviPanda <yuvipanda@gmail.com>
2015-01-13 18:18:00 +0530

    beta: Kill role::syslog::centralserver::beta
    Bug: T86645
    Change-Id: Ic0b49b3c279a9d2d506bf758d0bd24b2894c0d83

--- manifests/role/syslog.pp
+++ manifests/role/syslog.pp
@@ -6,29 +6,5 @@ class role::syslog::centralserver {
 
     system::role { 'role::syslog::centralserver': description => 'Central syslog server' }
 
-
-    class { 'misc::syslog-server':
-        config   => 'nfs',
-        basepath => '/srv/syslog',
-    }
-
-}
-
-# == Class role::syslog::centralserver::beta
-#
-# Setup syslog-ng as a receiver for the beta cluster syslog messages.
-#
-class role::syslog::centralserver::beta {
-
-    system::role { 'role::syslog::centralserver::beta': description => 'Central syslog server for beta cluster' }
-
-    if $::realm == 'production' {
-        fail( 'role::syslog::centralserver::beta must not be used in production' )
-    }
-
-    class { 'misc::syslog-server':
-        config   => 'nfs',
-        basepath => '/data/project/syslog',
-    }
-
+    class { 'misc::syslog-server': }
 }
