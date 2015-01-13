0a14edd6c111c6d9edccf965ce3ca5fc87db858d
YuviPanda <yuvipanda@gmail.com>
2015-01-13 16:53:37 +0530

    citoid: Unify production and beta roles
    Bug: T86633
    Change-Id: I6fabdcc58c2fd80c7215a68f311ba4d23c31d650

--- manifests/role/citoid.pp
+++ manifests/role/citoid.pp
@@ -1,29 +1,22 @@
 # vim: set ts=4 et sw=4:
-
 class role::citoid {
     system::role { 'role::citoid': }
     class { '::citoid': }
-}
-
-class role::citoid::production {
-    require role::citoid
 
     monitoring::service { 'citoid':
-      description   => 'citoid',
-      check_command => 'check_http_on_port!1970',
+        description   => 'citoid',
+        check_command => 'check_http_on_port!1970',
     }
-}
-
-class role::citoid::beta {
-    require role::citoid
 
     # Beta citoid server has some ferm DNAT rewriting rules (bug 45868) so we
     # have to explicitly allow citoid port 1970
     ferm::service { 'citoid':
-      proto => 'tcp',
-      port  => '1970',
+        proto => 'tcp',
+        port  => '1970',
     }
+}
 
+class role::citoid::jenkins_access {
     # Allow ssh access from the Jenkins master to the server where citoid is
     # running
     include contint::firewall::labs
