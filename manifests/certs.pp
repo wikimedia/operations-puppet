8b04720be180a57cf065998e1ce0bc6f40418f45
Faidon Liambotis <faidon@wikimedia.org>
2015-01-15 15:13:17 +0200

    certs: kill create_combined_cert
    create_combined_cert created a private/public keypair bundle in a single
    file that's extremely uncommon. The only piece of software that actually
    expected this keypair in our infrastructure was lighttpd, but all
    lighttpd HTTPS servers are now gone, so we can remove this piece of
    legacy from our infrastructure.
    
    Change-Id: Ief4df7bf9f32231657c5f41d8f6c752e7e2c1446

--- manifests/certs.pp
+++ manifests/certs.pp
@@ -79,39 +79,6 @@ define create_chained_cert(
     }
 }
 
-define create_combined_cert(
-    $certname = $name,
-    $user     = 'root',
-    $group    = 'ssl-cert',
-    $location = '/etc/ssl/private',
-) {
-    # combined cert, used by things like lighttp and nginx
-    exec { "${name}_create_combined_cert":
-        creates => "${location}/${certname}.crt",
-        command => "/bin/cat /etc/ssl/localcerts/${certname}.crt /etc/ssl/private/${certname}.key > ${location}/${certname}.crt",
-        require => [Package['openssl'],
-                    File["/etc/ssl/private/${certname}.key"],
-                    File["/etc/ssl/localcerts/${certname}.crt"],
-        ];
-    }
-    # Fix permissions on the combined file, and make it available as
-    # a puppet resource
-    file { "${location}/${certname}.crt":
-        ensure  => 'file',
-        mode    => '0440',
-        owner   => $user,
-        group   => $group,
-        require => Exec["${name}_create_combined_cert"],
-    }
-
-    # TODO: Remove once nothing references this anymore
-    file { "${location}/${certname}.pem":
-        ensure  => link,
-        target  => "${location}/${certname}.crt",
-        require => File["${location}/${certname}.crt"],
-    }
-}
-
 define install_certificate(
     $group     = 'ssl-cert',
     $ca        = '',
@@ -156,8 +123,15 @@ define install_certificate(
         }
     }
 
+    # create_combined_cert created those
+    file { [
+        "/etc/ssl/private/${name}.crt",
+        "/etc/ssl/private/${name}.pem",
+    ]:
+        ensure => absent,
+    }
+
     create_pkcs12{ $name: }
-    create_combined_cert{ $name: }
     if ( $ca ) {
         $cas = $ca
     } else {
