f47249a00899c725adacdd0c9b0d1c6dcc8e99d4
YuviPanda <yuvipanda@gmail.com>
2015-01-15 15:10:50 +0530

    deployment: Unify salt_masters role for prod / labs
    Bug: T86885
    Change-Id: Id088ab43f6b4cba58552c75a74eb70361faaaff1

--- manifests/site.pp
+++ manifests/site.pp
@@ -2119,7 +2119,7 @@ node 'palladium.eqiad.wmnet' {
     include admin
     include role::ipmi
     include role::salt::masters::production
-    include role::deployment::salt_masters::production
+    include role::deployment::salt_masters
     include role::access_new_install
     include role::puppetmaster::frontend
     include role::pybal_config
@@ -2576,7 +2576,7 @@ node 'virt1000.wikimedia.org' {
     include role::nova::controller
     include role::nova::manager
     include role::salt::masters::labs
-    include role::deployment::salt_masters::labs
+    include role::deployment::salt_masters
     if $use_neutron == true {
         include role::neutron::controller
 
@@ -2600,7 +2600,7 @@ node 'labcontrol2001.wikimedia.org' {
     #include role::nova::controller
     #include role::nova::manager
     #include role::salt::masters::labs
-    #include role::deployment::salt_masters::labs
+    #include role::deployment::salt_masters
 }
 
 # Eqiad ldap server, aka ldap-eqiad
