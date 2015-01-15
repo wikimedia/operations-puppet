468e3bb12ad598ba715cc27eaaaf2acfafba669d
Faidon Liambotis <faidon@wikimedia.org>
2015-01-15 21:10:51 +0200

    Tree-wide certificate path replacement, localcerts
    Replace all occurences of
      /etc/ssl/certs/NAME.pem
    with
      /etc/ssl/localcerts/NAME.crt
    
    The latter exists since a5ccbf7118cbf9ea6a7827588c415de7a409fec5 and is
    more correct, as the /etc/ssl/certs path is really the CA store in
    Debian/Ubuntu systems. Move all references there, with a goal of
    removing the compatibility symlinks placed by that commit above.
    
    Change-Id: I0aa91d8658b19d44154ec4a0f94b48940afeb908

--- modules/ganglia_new/manifests/web.pp
+++ modules/ganglia_new/manifests/web.pp
@@ -12,7 +12,7 @@ class ganglia_new::web(
     $ganglia_servername = 'ganglia.wikimedia.org'
     $ganglia_serveralias = 'uranium.wikimedia.org'
     $ganglia_webdir = '/usr/share/ganglia-webfrontend'
-    $ganglia_ssl_cert = '/etc/ssl/certs/ganglia.wikimedia.org.pem'
+    $ganglia_ssl_cert = '/etc/ssl/localcerts/ganglia.wikimedia.org.crt'
     $ganglia_ssl_key = '/etc/ssl/private/ganglia.wikimedia.org.key'
     $ssl_settings = ssl_ciphersuite('apache-2.4', 'compat')
 
