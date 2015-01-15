7f653cf34390f4b7dc788b24e0358ed555f4eb80
andrewbogott <abogott@wikimedia.org>
2015-01-15 11:40:49 -0600

    Set rootdelay=90
    Change-Id: Id7b07f03f5d683c34dce565871553bf7bd6ad3f0

--- modules/labs_bootstrapvz/files/firstboot.sh
+++ modules/labs_bootstrapvz/files/firstboot.sh
@@ -60,7 +60,7 @@ then
 
   # Debian has an lvm bug that foils many a boot.  This hack should
   # work around that.
-  sed -i '/GRUB_CMDLINE_LINUX_DEFAULT.*/c\GRUB_CMDLINE_LINUX_DEFAULT="console=ttyS0 rootdelay=N"' /etc/default/grub
+  sed -i '/GRUB_CMDLINE_LINUX_DEFAULT.*/c\GRUB_CMDLINE_LINUX_DEFAULT="console=ttyS0 rootdelay=90"' /etc/default/grub
   /usr/sbin/update-grub
 fi
 # At this point, all (the rest of) our disk are belong to LVM.
