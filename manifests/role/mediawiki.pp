17f2bd9697a71d40d5a94eeef835adee6aa9cf79
YuviPanda <yuvipanda@gmail.com>
2015-01-19 15:40:05 -0800

    mediawiki: Move jobrunner config into hiera
    Shared across jobrunner and videoscaler machines
    
    Bug: T87210
    Change-Id: I21396d68e91344d3f269a75667dc5e1cfcdc7dfe

--- manifests/role/mediawiki.pp
+++ manifests/role/mediawiki.pp
@@ -128,14 +128,7 @@ class role::mediawiki::jobrunner {
 
     include ::role::mediawiki::common
 
-    class { '::mediawiki::jobrunner':
-        queue_servers   => ['rdb1001.eqiad.wmnet', 'rdb1003.eqiad.wmnet'],
-        statsd_server   => 'statsd.eqiad.wmnet:8125',
-        runners_basic   => 14,
-        runners_parsoid => 4,
-        runners_upload  => 7,
-        runners_gwt     => 1,
-    }
+    include ::mediawiki::jobrunner
 }
 
 # monitor the Apple dictionary bridge (RT #6128)
