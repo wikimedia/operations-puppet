4940df5787d4f86f287ecd7d1bf95fd8a1aeee45
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-01-19 14:08:27 -0800

    coredb::s1: use hiera, role
    We transform a bit the logic of the role::coredb::s1 so that all
    parameters of the parent class (role::coredb::common) are directly
    configured via hiera, which has the nice advantage of aggregating all
    parameters in a single place, making them more readable
    
    Change-Id: I4dac9f6460dcb3b88d92718140facbe060ae98af
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

--- manifests/role/coredb.pp
+++ manifests/role/coredb.pp
@@ -98,14 +98,8 @@ class role::coredb::config {
     }
 }
 
-class role::coredb::s1( $mariadb = false, $innodb_file_per_table = false ) {
-    class { 'role::coredb::common':
-        shard                 => 's1',
-        slow_query_digest     => false,
-        mariadb               => $mariadb,
-        innodb_file_per_table => $innodb_file_per_table,
-        innodb_log_file_size  => '2000M'
-    }
+class role::coredb::s1{
+    include role::coredb::common
 }
 
 class role::coredb::s2( $mariadb = false, $innodb_file_per_table = false ) {
