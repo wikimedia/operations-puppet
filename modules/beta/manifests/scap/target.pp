44d743176718fec06bd2983969431c1728417c85
YuviPanda <yuvipanda@gmail.com>
2015-01-19 18:23:30 -0800

    beta: Remove explict ssh grant for mwdeploy user
    It's in LDAP now, and is a member of deployment-prep
    
    Change-Id: I22da6b82bfa1e219b48b06b7e104e4728c7f5939

--- modules/beta/manifests/scap/target.pp
+++ modules/beta/manifests/scap/target.pp
@@ -29,20 +29,5 @@ class beta::scap::target {
         source  => 'puppet:///private/scap/id_rsa.pub',
         require => File['/etc/ssh/userkeys/mwdeploy/.ssh'],
     }
-
-    # Hack to replace /etc/security/access.conf (which is managed by the
-    # ldap::client class) with a modified version that includes an access
-    # grant for the mwdeploy user to authenticate from deployment-bastion.
-    file { '/etc/security/access.conf~':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        content => template('beta/pam-access.conf.erb'),
-    }
-    File <| title == '/etc/security/access.conf' |> {
-        content => undef,
-        source  => '/etc/security/access.conf~',
-        require => File['/etc/security/access.conf~'],
-    }
 }
 
