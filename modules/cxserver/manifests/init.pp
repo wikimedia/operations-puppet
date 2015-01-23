ae254e2b53ed5ef2e59c33715fc1c5dbf91bb105
Kartik Mistry <kartik.mistry@gmail.com>
2015-01-24 05:17:37 +0530

    cxserver: Add Yandex support
    * Added Yandex proxy support.
    * Yandex support conditional based on value of yandex_url
    * Proxy support conditional
    * English - Russian MT with yandex provider
    
    Bug: T88512
    Change-Id: I6e76d289fd85adcb396361b07a8dd4d2c1b6eb5b

--- modules/cxserver/manifests/init.pp
+++ modules/cxserver/manifests/init.pp
@@ -17,9 +17,15 @@
 #   Place where cxserver can put log files. Assumed to be already existing and
 #   have write access to cxserver user.
 # [*parsoid*]
-#   Url to parsoid service.
+#   Url to Parsoid service.
 # [*apertium*]
-#   Url to apertium service.
+#   Url to Apertium service.
+# [*yandex*]
+#   Url to Yandex service.
+# [*yandex_api_key*]
+#   API key for Yandex service.
+# [*yandex_proxy*]
+#   Proxy URL for cxserver.
 # [*port*]
 #   Port where to run the cxserver service. Defaults to 8080.
 # [*registry*]
@@ -31,6 +37,9 @@ class cxserver(
     $log_dir = '/var/log/cxserver',
     $parsoid = 'http://parsoid-lb.eqiad.wikimedia.org',
     $apertium = 'http://apertium.svc.eqiad.wmnet:2737',
+    $yandex_url = undef,
+    $yandex_api_key = undef,
+    $proxy = undef,
     $port = 8080,
     $registry = {},
 ) {
