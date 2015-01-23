18ac86b489a09d30c76628ac049205c298787169
Kartik Mistry <kartik.mistry@gmail.com>
2015-01-24 05:17:37 +0530

    cxserver: Add Yandex support
    * Added Yandex proxy support.
    * Yandex support conditional based on value of yandex_url
    * Proxy support conditional
    * English - Russian MT with yandex provider
    * Do not enable in production, but rather only on beta
    
    Bug: T88512
    Change-Id: I6e76d289fd85adcb396361b07a8dd4d2c1b6eb5b

--- manifests/role/cxserver.pp
+++ manifests/role/cxserver.pp
@@ -5,7 +5,12 @@ class role::cxserver {
         description => 'content translation server'
     }
 
-    include ::cxserver
+    include ::passwords::cxserver
+    $yandex_api_key = $::passwords::cxserver::yandex_api_key
+
+    class { '::cxserver':
+        yandex_api_key => $yandex_api_key,
+    }
 
     ferm::service { 'cxserver_http':
         proto => 'tcp',
