wikimedia/operations-puppet

commit 7bfad15490b7429415698dccf3dc36c3702afe10

Remove init script for default udp2log instance on fluorine
Adds a default_instance parameter to misc::udp2log which, if false,
removes the default init script so it won't conflict with any
misc::udp2log::instance instances.

Bug: T87726
Change-Id: Ib7b0a618f221fff982e4683ee378a8300cba8bff

manifests/misc/udp2log.pp
@@ -6,7 +6,11 @@
 #
 # Parameters:
 #    $monitor  - If true, monitoring scripts will be installed.  Default: true
-class misc::udp2log($monitor = true) {
+#    $default_instance  - If false, remove init script for the default instance.  Default: true
+class misc::udp2log(
+    $monitor = true,
+    $default_instance = true
+) {
 
     include contacts::udp2log
     include misc::udp2log::udp_filter
@@ -41,6 +45,17 @@ class misc::udp2log($monitor = true) {
     # make sure the udplog package is installed
     package { 'udplog':
         ensure => present,
+    }
+
+    if !$default_instance {
+        file { '/etc/init.d/udp2log':
+            require => Package[udp2log],
+            ensure  => absent
+        }
+        exec { '/usr/sbin/update-rc.d -f udp2log remove':
+            subscribe   => File['/etc/init.d/udp2log'],
+            refreshonly => true
+        }
     }
 }
 

manifests/role/logging.pp
@@ -35,7 +35,11 @@ class role::logging
 class role::logging::mediawiki($monitor = true, $log_directory = '/home/wikipedia/logs' ) {
     system::role { "role::logging:mediawiki": description => "MediaWiki log collector" }
 
-    class { "misc::udp2log": monitor => $monitor }
+    class { "misc::udp2log":
+        monitor             => $monitor,
+        default_instance    => false
+    }
+
     include misc::udp2log::utilities,
         misc::udp2log::firewall
 

commit 1aeaee731a468555cd044b39effbd753b02bfe57

Fix typo introduced in 7bfad15
It's udplog, not udp2log. Not consistent, should be fixed overall

Change-Id: Ie3a8e59f78deb625a0093607c642330d9d2ffcf6

manifests/misc/udp2log.pp
@@ -49,7 +49,7 @@ class misc::udp2log(
 
     if !$default_instance {
         file { '/etc/init.d/udp2log':
-            require => Package[udp2log],
+            require => Package['udplog'],
             ensure  => absent
         }
         exec { '/usr/sbin/update-rc.d -f udp2log remove':