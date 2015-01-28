74a7e82263707a6e801f7e7026f4b592a40155ca
YuviPanda <yuvipanda@gmail.com>
2015-01-28 11:29:59 -0800

    scap: Add missing role file
    I iz idiot
    
    Bug: T87221
    Change-Id: Ia5856a7fb003ce7f60abfd8a19a4b5017b79f80a

---
+++ manifests/role/scap.pp
@@ -0,0 +1,11 @@
+# = class: role::scap::master
+#
+# Sets up a scap master 
+class role::scap::master {
+    system::role { 'misc::deployment':
+        description => 'Deployment host'
+    }
+
+    include scap::master
+    include scap::l10nupdate
+}
