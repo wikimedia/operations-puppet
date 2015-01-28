77781df664d069a5ab834caa1ab228267683ac2b
Ori Livneh <ori@wikimedia.org>
2015-01-27 19:19:07 -0800

    ve: Add vbench tool
    `vbench` is a Python script that automates Chromium using Chromium's remote
    debugging protocol. It repeatedly loads VisualEditor for a particular URL,
    collecting and aggregating CPU profiles for each load.
    
    Change-Id: I26f98b0eceb7f79145ddfdb18856b2149b5bed2c

--- manifests/role/ve.pp
+++ manifests/role/ve.pp
@@ -6,4 +6,17 @@
 class role::ve {
     class { 'xvfb': resolution => '1366x768x24' }
     class { 'chromium': }
+
+    # vbench is a CLI tool for benchmarking VisualEditor.
+    # It uses `autobahn` and `twisted` for WebSocket support, which
+    # it needs so it can speak Chrome's remote debugging protocol.
+
+    require_package('python-autobahn', 'python-twisted')
+
+    file { '/usr/local/bin/vbench':
+        source => 'puppet:///files/ve/vbench',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
 }
