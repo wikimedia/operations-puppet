e38255caa48e393cad47092f815f11b3345b98e4
YuviPanda <yuvipanda@gmail.com>
2015-01-31 14:40:26 +0530

    toollabs: Add python3 support to uwsgi web services
    Exact same code / params are used.
    
    Change-Id: I5e07730e6fa7055b647f4948cc10b1db8c07e5ec

--- modules/toollabs/manifests/node/web/uwsgi.pp
+++ modules/toollabs/manifests/node/web/uwsgi.pp
@@ -24,7 +24,14 @@ class toollabs::node::web::uwsgi inherits toollabs::node::web {
         queues => [ 'webgrid-uwsgi' ],
     }
 
+    # Exact same file. The script knows to perform different actions based on
+    # what it is called
     file { '/usr/local/bin/tool-uwsgi-python':
+        source => 'puppet:///modules/toollabs/tool-uwsgi-python',
+        mode   => '0555',
+    }
+
+    file { '/usr/local/bin/tool-uwsgi-python3':
         source => 'puppet:///modules/toollabs/tool-uwsgi-python',
         mode   => '0555',
     }
