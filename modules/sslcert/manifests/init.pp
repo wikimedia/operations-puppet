5c3541cefa279dc85e0d31589076320bd126e78c
Faidon Liambotis <faidon@wikimedia.org>
2015-02-02 12:49:15 +0100

    sslcert: generate chained certs automatically
    Instead of manually defining the certificate chain path and the CA
    filenames for each certificate, install a helper Python script that
    automatically creates the chain given the a c_rehashed directory (in
    this case, /etc/ssl/certs, which contains both system CAs and
    puppet-provisioned CAs using sslcert::ca).
    
    The script is smart enough to handle multiple certificate paths (e.g. in
    case of cross-signed CAs) and pick the shortest-one, as well as to
    provide an option to skip the root (top-most) certificate for
    performance reasons, which we use.
    
    Change-Id: I9a3da71bc3d0f3cb22b7183bc050970ace581112

--- modules/sslcert/manifests/init.pp
+++ modules/sslcert/manifests/init.pp
@@ -30,6 +30,15 @@ class sslcert {
         require => Package['ssl-cert'],
     }
 
+    # install our helper that automatically creates certificate chains
+    file { '/usr/local/sbin/x509-bundle':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        source => 'puppet:///modules/sslcert/x509-bundle',
+    }
+
     # Limit AppArmor support to just Ubuntu, for now
     if $::operatingsystem == 'Ubuntu' {
         include apparmor
