b7bcbdb109105a19e3926df4387dfd679eb3c284
Yuvipanda <yuvipanda@gmail.com>
2015-02-02 18:09:46 +0000

    Revert "Revert "dumps: Strengthen ssl settings""
    I mistook a file for a template >_>
    
    This reverts commit d439665d85563971a31abcbd3e2af717f0186aa4.
    
    Change-Id: Ie08bd91b05886b416471168ced882437847c3830

--- modules/dumps/manifests/init.pp
+++ modules/dumps/manifests/init.pp
@@ -2,8 +2,11 @@ class dumps {
     install_certificate{ 'dumps.wikimedia.org': ca => 'RapidSSL_CA.pem' }
 
     include ::nginx
+
+    $ssl_settings = ssl_ciphersuite('nginx', 'compat')
+
     nginx::site { 'dumps':
-        source  => 'puppet:///modules/dumps/nginx.dumps.conf',
+        content => template('dumps/nginx.dumps.conf.erb'),
         notify  => Service['nginx'],
     }
     nginx::site { 'download':
