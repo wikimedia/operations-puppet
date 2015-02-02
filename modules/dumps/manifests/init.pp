419fffbb31eafe9e0e90304ba5e84b80695808df
YuviPanda <yuvipanda@gmail.com>
2015-02-02 06:27:20 +0530

    dumps: Strengthen ssl settings
    Bug: T74072
    Change-Id: I7529dd81d7084bd5dd60ab99ee155c5b88c916a7

--- modules/dumps/manifests/init.pp
+++ modules/dumps/manifests/init.pp
@@ -2,6 +2,9 @@ class dumps {
     install_certificate{ 'dumps.wikimedia.org': ca => 'RapidSSL_CA.pem' }
 
     include ::nginx
+
+    $ssl_settings = ssl_ciphersuite('nginx', 'compat')
+
     nginx::site { 'dumps':
         source  => 'puppet:///modules/dumps/nginx.dumps.conf',
         notify  => Service['nginx'],
