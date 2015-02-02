f185b4d6225136d0c905f889890a75d5b227c46f
Faidon Liambotis <faidon@wikimedia.org>
2015-02-02 12:29:22 +0100

    sslcert: add sslcert::chainedcert
    Create a new definition, responsible for generating chained
    certificates. This replaces the old create_chained_cert definition that
    existed within certs.pp.
    
    Change-Id: Ib99bd2e05e0bb6cb8126b6d2313148f7a3ec67b0

--- manifests/certs.pp
+++ manifests/certs.pp
@@ -1,34 +1,3 @@
-define create_chained_cert(
-    $ca,
-    $certname = $name,
-    $user     = 'root',
-    $group    = 'ssl-cert',
-    $location = '/etc/ssl/localcerts',
-) {
-    # chained cert, used when needing to provide
-    # an entire certificate chain to a client
-    # NOTE: This is annoying because to work right regardless of whether
-    # the root CA comes from the OS or us, we need to use the /etc/ssl/certs/
-    # linkfarm so filenames need to use '*.pem'.
-
-    exec { "${name}_create_chained_cert":
-        creates => "${location}/${certname}.chained.crt",
-        command => "/bin/cat /etc/ssl/localcerts/${certname}.crt ${ca} > ${location}/${certname}.chained.crt",
-        cwd     => '/etc/ssl/certs',
-        require => [Package['openssl'],
-                    File["/etc/ssl/localcerts/${certname}.crt"],
-        ],
-    }
-    # Fix permissions on the chained file, and make it available as
-    file { "${location}/${certname}.chained.crt":
-        ensure  => 'file',
-        mode    => '0444',
-        owner   => $user,
-        group   => $group,
-        require => Exec["${name}_create_chained_cert"],
-    }
-}
-
 define install_certificate(
     $group     = 'ssl-cert',
     $ca        = '',
@@ -73,9 +42,10 @@ define install_certificate(
             default => 'wmf-ca.pem',
         }
     }
-    create_chained_cert{ $name:
+    sslcert::chainedcert { $name:
         ca => $cas,
     }
+
 }
 
 class certificates::base {
