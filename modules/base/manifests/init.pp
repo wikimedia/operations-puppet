70e8eccafc98db5308ecf0304470369b456663b9
John F. Lewis <johnflewis93@gmail.com>
2015-02-03 20:25:22 +0000

    base: move instance-upstarts to manifest
    Change-Id: I819065e24be0bdc626bd3a5a7e7b08760afe5b26

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -70,17 +70,6 @@ class base::remote-syslog {
     }
 }
 
-class base::instance-upstarts {
-
-    file { '/etc/init/ttyS0.conf':
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => 'puppet:///modules/base/upstart/ttyS0.conf';
-    }
-
-}
-
 class base::screenconfig {
     file { '/root/.screenrc':
         ensure => present,
---
+++ modules/base/manifests/instance-upstarts.pp
@@ -0,0 +1,10 @@
+class base::instance-upstarts {
+
+    file { '/etc/init/ttyS0.conf':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/base/upstart/ttyS0.conf';
+    }
+
+}
