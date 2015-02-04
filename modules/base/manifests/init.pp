50274a90929b7701c063aea807a36b6e7654f81a
John F. Lewis <johnflewis93@gmail.com>
2015-02-04 19:18:40 +0000

    base: move base::firewall to manifest
    Change-Id: Ia96a60aea1d51fd264ae2042e5a9109037d900b2

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -42,42 +42,6 @@ class base::syslogs (
     }
 }
 
-
-# Don't include this sub class on all hosts yet
-# NOTE: Policy is DROP by default
-class base::firewall($ensure = 'present') {
-    include network::constants
-    include ferm
-
-    $defscontent = $::realm ? {
-        'labs'  => template('base/firewall/defs.erb', 'base/firewall/defs.labs.erb'),
-        default => template('base/firewall/defs.erb'),
-    }
-    ferm::conf { 'defs':
-        # defs can always be present.
-        # They don't actually do firewalling.
-        ensure  => 'present',
-        prio    => '00',
-        content => $defscontent,
-    }
-
-    ferm::conf { 'main':
-        ensure => $ensure,
-        prio   => '00',
-        source => 'puppet:///modules/base/firewall/main-input-default-drop.conf',
-    }
-
-    ferm::rule { 'bastion-ssh':
-        ensure => $ensure,
-        rule   => 'proto tcp dport ssh saddr $BASTION_HOSTS ACCEPT;',
-    }
-
-    ferm::rule { 'monitoring-all':
-        ensure => $ensure,
-        rule   => 'saddr $MONITORING_HOSTS ACCEPT;',
-    }
-}
-
 class base {
     include apt
 
