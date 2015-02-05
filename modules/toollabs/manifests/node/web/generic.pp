8bd7bf8e181f64096af349aff9a8fe10a48627da
YuviPanda <yuvipanda@gmail.com>
2015-02-05 15:35:38 +0530

    toollabs: Add generic webgrid node type
    Will support push-button deploys of less commonly used languages
    (nodejs, go, etc)
    
    Bug: T1102
    Change-Id: I2fb17b468f066cab9862b4587c3359126ff6c4af

---
+++ modules/toollabs/manifests/node/web/generic.pp
@@ -0,0 +1,14 @@
+# == Class: toollabs::node::web::generic
+# 
+# Sets up a node for running generic webservices.
+# Currently explicitly supports nodejs
+class toollabs::node::web::generic inherits toollabs::node::web {
+    class { 'toollabs::queues':
+        queues => [ 'webgrid-generic' ],
+    }
+
+    file { '/usr/local/bin/tool-nodejs':
+        source => 'puppet:///modules/toollabs/tool-nodejs',
+        mode   => '0555',
+    }
+}
