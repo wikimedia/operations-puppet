d7cc135284e7b1dcda15284305087c07dc73c698
dzahn <dzahn@wikimedia.org>
2015-02-04 16:06:32 -0800

    ci/jenkins: add public key for VE sync to puppet
    This is a key used for jenkins VE sync tasks.
    The private part of the pair is already puppetized. The public part was not.
    
    follow-up to Change-Id: I568367464d8096c9
    
    Just reflects the existing setup, not intended to change anything.
    
    Bug:T84731
    Change-Id: Idafe038e816235e37e44e72dbdeea97de23bf899

--- manifests/role/ci.pp
+++ manifests/role/ci.pp
@@ -110,7 +110,7 @@ class role::ci::master {
         notify => Service['ganglia-monitor'],
     }
 
-    # private key for VE sync tasks (T84731)
+    # key pair for VE sync tasks (T84731)
     file { '/var/lib/jenkins/.ssh/jenkins-mwext-sync_id_rsa':
         ensure  => present,
         owner   => 'jenkins',
@@ -119,6 +119,16 @@ class role::ci::master {
         source  => 'puppet:///private/ssh/ci/jenkins-mwext-sync_id_rsa',
         require => User['jenkins'],
     }
+
+    file { '/var/lib/jenkins/.ssh/jenkins-mwext-sync_id_rsa.pub':
+        ensure  => present,
+        owner   => 'jenkins',
+        group   => 'jenkins',
+        mode    => '0400',
+        source  => 'puppet:///modules/jenkins/jenkins-mwext-sync_id_rsa.pub',
+        require => User['jenkins'],
+    }
+
 
 }
 
