c37f0a5c2cf78d1c7d7abd139987009ba5c66ec6
Timo Tijhof <krinklemail@gmail.com>
2015-02-06 15:49:31 -0800

    contint: Allow ssh between labs slaves
    This is allowed by default through the 'integration' Nova
    security group, but our firewall rules close this, so we need to
    re-open it.
    
    Change-Id: I679e9942c1c3188ae4edd4969883abac89415cf0

--- modules/contint/manifests/firewall/labs.pp
+++ modules/contint/manifests/firewall/labs.pp
@@ -2,10 +2,20 @@
 
 # == Class contint::firewall::labs
 #
-# Firewall rules for contint Jenkins slaves. Basically let the Jenkins master
-# to ssh to the slave box.
+# Firewall rules for contint Jenkins slaves.
 class contint::firewall::labs {
+
+    # Allow Jenkins master
     ferm::rule { 'gallium_ssh_to_slaves':
         rule => 'proto tcp dport ssh { saddr 208.80.154.135 ACCEPT; }'
+    }
+
+    # Allow other labs instances
+    # This is already possible through bastion, but because contint overrides
+    # iptables we have to re-allow it (eventhough our Nova security group already
+    # allows this).
+    # Use case: ssh from integration-dev to integratin-slave100x.
+    ferm::rule { 'from_labs_to_labs':
+        rule => 'proto tcp dport ssh { saddr 10.0.0.0/8 ACCEPT; }'
     }
 }
