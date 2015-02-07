b13a07123106da4488c9a8eb0bbe5912fbc3fec1
Daniel Zahn <dzahn@wikimedia.org>
2015-02-06 16:08:32 -0800

    phab: direct_comments_allowed for Domains tickets
    Let MarkMonitor send comments on tickets in the newly created Domains project.
    
    Is that the syntax to simply add a domain name instead of a wildcard as with
    the ops-* projects above?
    
    Bug:T88842
    Change-Id: I447da64ec4972c46713f3472e59916300ff94dc9

--- manifests/role/phabricator.pp
+++ manifests/role/phabricator.pp
@@ -133,6 +133,7 @@ class role::phabricator::main {
             ops-eqiad => '*',
             ops-esams => '*',
             ops-ulsfo => '*',
+            domains   => 'markmonitor.com',
         },
         phab_bot        => {
             root_dir    => '/srv/phab/phabricator/',
