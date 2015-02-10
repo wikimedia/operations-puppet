d09d9be6c1a23550e4e645faaebfb1fb75734ecb
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-02-10 17:47:05 +0100

    base: add the service_unit init wrapper
    The base::service_unit resource aims at being a init-system-agnostic way
    to manage a service in puppet, including its own init scripts if they
    are created by us. It should also take care of reloading systemd if the
    service file has changed.
    
    As suggested, it allows you to choose wether to notify config changes to
    the daemon or not.
    
    This is a (partial? good enough?) solution for T89086
    
    Change-Id: I5d1472f7c737aa7a917509394ec997b8e25f6d2b
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/base/spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'rspec-puppet'
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
+
+RSpec.configure do |c|
+  c.module_path = File.expand_path("../..", File.dirname(__FILE__))
+  c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.template_dir = File.join(fixture_path, 'templates')
+end
