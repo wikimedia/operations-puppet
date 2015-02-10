9682ed13a7a5d64d5582979ab4d87ae6880565cf
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-02-10 17:47:05 +0100

    base: add the service_unit init wrapper
    The base::service_unit resource aims at being a init-system-agnostic way
    to manage a service in puppet, including its own init scripts if they
    are created by us. It should also take care of reloading systemd if the
    service file has changed.
    
    As suggested, it allows you to choose wether to notify config changes to
    the daemon or not.
    
    This is a (partial? good enough?) solution for T89086
    
    Change-Id: I5d1472f7c737aa7a917509394ec997b8e25f6d2b
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/base/manifests/service_unit.pp
@@ -0,0 +1,105 @@
+# == Base::service_unit ==
+#
+# Allows defining services and their corresponding init scripts in a
+# init-system agnostic way on Debian derivatives.
+#
+# We prefer convention over configuration, so this define will require
+# you to respect those in order to work.
+#
+# === Parameters ===
+#
+#[*ensure*]
+# Is the usual metaparameter, defaults to present
+#
+#[*systemd*]
+# Boolean - set it to true to make the resource include personalized
+# init file. As this is used to You are expected to put them in a
+# specific subdirectory of the current module, which is
+# $module/initscripts/$name.systemd.erb for systemd  (and similarly for
+# other init systems)
+#
+#[*upstart*]
+# As the preceding param, but for upstart scripts
+#
+#[*sysvinit*]
+# As the preceding param, but for traditional sysvinit scripts
+#
+#[*refresh*]
+# Boolean - tells puppet if a change in the config should notify the service directly
+#
+#[*service_params*]
+# An hash of parameters that we want to apply to the service resource
+#
+# === Example ===
+#
+# A init-agnostic class that runs apache, with its own init scripts
+# (please, don't do it at home!)
+#
+# class foo {
+#     base::service_unit { 'apache2':
+#         ensure          => present,
+#         sysvinit        => true,
+#         service_params  => {
+#             hasrestart => true,
+#             restart => '/usr/sbin/service apache2 restart'
+#         }
+#     }
+# }
+define base::service_unit (
+    $ensure           = present,
+    $systemd          = false,
+    $upstart          = false,
+    $sysvinit         = false,
+    $refresh          = true,
+    $service_params   = {},
+    ) {
+
+    validate_ensure($ensure)
+    # Validates the service name, and picks the valid init script
+    $initscript = pick_initscript(
+        $::initsystem, $systemd, $upstart, $sysvinit)
+    # we assume init scripts are templated
+    if $initscript {
+        if $caller_module_name {
+            $template = "${caller_module_name}/initscripts/${name}.${initscript}.erb"
+        }
+        else {
+            $template = "initscripts/${name}.${initscript}.erb"
+        }
+        $path = $initscript ? {
+            'systemd'  => "/etc/systemd/system/${name}.service",
+            'upstart'  => "/etc/init/${name}.conf",
+            default    => "/etc/init.d/${name}"
+        }
+
+        file {$path:
+            ensure  => $ensure,
+            content => template($template),
+            mode    => '0544',
+            owner   => root,
+            group   => root,
+        }
+
+        if $refresh {
+            File[$path] ~> Service[$name]
+        } else {
+            File[$path] -> Service[$name]
+        }
+
+        if $::initsystem == 'systemd' {
+                exec { "systemd reload for ${name}":
+                    refreshonly => true,
+                    command     => '/bin/systemctl daemon-reload',
+                    subscribe   => File[$path],
+                    before      => Service[$name]
+                }
+
+        }
+    }
+
+    $base_params = {
+        ensure => ensure_service($ensure),
+        provider => $::initsystem }
+    $params = merge($base_params, $service_params)
+    ensure_resource('service',$name, $params)
+}
