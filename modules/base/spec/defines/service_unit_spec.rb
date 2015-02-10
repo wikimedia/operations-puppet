a662b71aa5032b404e6e4c7d0cde21037b27dcbc
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-02-10 17:47:05 +0100

    base: add the service_unit init wrapper
    The base::service_unit resource aims at being a init-system-agnostic way
    to manage a service in puppet, including its own init scripts if they
    are created by us. It should also take care of reloading systemd if the
    service file has changed.
    
    As suggested, it allows you to choose wether to notify config changes to
    the daemon or not.
    
    This is a (partial? good enough?) solution for T89086
    
    Change-Id: I5d1472f7c737aa7a917509394ec997b8e25f6d2b
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>

---
+++ modules/base/spec/defines/service_unit_spec.rb
@@ -0,0 +1,88 @@
+require 'spec_helper'
+
+describe 'base::service_unit' do
+  let(:title) { 'nginx' }
+
+  context 'with systemd as init' do
+    let(:facts) { {:initsystem => 'systemd'} }
+    context 'with a systemd unit file' do
+      let(:params) { { :ensure => 'present', :systemd => true}}
+
+      it 'should activate the service' do
+        should contain_service('nginx')
+      end
+
+      it 'should install a service file' do
+        should contain_file('/etc/systemd/system/nginx.service')
+      end
+
+      it 'should execute daemon-reload' do
+        should contain_file('/etc/systemd/system/nginx.service').that_notifies('Exec[systemd reload for nginx]').that_notifies('Service[nginx]')
+        should contain_exec('systemd reload for nginx').that_comes_before('Service[nginx]')
+      end
+    end
+
+    context 'without a unit file' do
+      let(:params) { { :ensure => 'present', :sysvinit => true}}
+      it 'should install an init script' do
+        should contain_file('/etc/init.d/nginx')
+      end
+
+      it 'should execute daemon-reload' do
+        should contain_file('/etc/init.d/nginx').that_notifies('Exec[systemd reload for nginx]').that_notifies('Service[nginx]')
+        should contain_exec('systemd reload for nginx').that_comes_before('Service[nginx]')
+      end
+
+    end
+  end
+  context 'with refresh false' do
+    let(:facts) { {:initsystem => 'sysvinit'} }
+    let(:params) { { :ensure => 'present', :sysvinit => true, :refresh => false}}
+
+    it 'should not refresh service' do
+      expect {
+        should contain_file('/etc/init.d/nginx').that_notifies('Service[nginx]')
+      }.to raise_error()
+    end
+  end
+
+  context 'with refresh true' do
+    let(:facts) { {:initsystem => 'sysvinit'} }
+    let(:params) { { :ensure => 'present', :sysvinit => true, :refresh => true}}
+
+    it 'should refresh service' do
+        should contain_file('/etc/init.d/nginx').that_notifies('Service[nginx]')
+    end
+
+  end
+  context 'with upstart as init' do
+    let(:facts) { {:initsystem => 'upstart'} }
+    context 'with an upstart conf file' do
+      let(:params) { { :ensure => 'present', :upstart => true}}
+      it 'should activate the service' do
+        should contain_service('nginx')
+      end
+
+      it 'should install a service file' do
+        should contain_file('/etc/init/nginx.conf')
+      end
+
+      it 'should notify service' do
+        should contain_file('/etc/init/nginx.conf').that_notifies('Service[nginx]')
+      end
+    end
+
+    context 'without a unit file' do
+      let(:params) { { :ensure => 'present', :sysvinit => true}}
+      it 'should install an init script' do
+        should contain_file('/etc/init.d/nginx')
+      end
+
+      it 'should notify service' do
+        should contain_file('/etc/init.d/nginx').that_notifies('Service[nginx]')
+      end
+    end
+
+    #MARK
+  end
+end
