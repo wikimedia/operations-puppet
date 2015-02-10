1b08881d2f89cec1cd51c480b8345c173f6edbfe
Ariel T. Glenn <ariel@wikimedia.org>
2015-02-10 19:33:23 +0200

    snapshots: move the admin include into the role
    Change-Id: Icc9aba260bd8b10f82e483492953af38b30d31b6

--- manifests/role/snapshot.pp
+++ manifests/role/snapshot.pp
@@ -1,5 +1,5 @@
 class role::snapshot::common {
-    include admin
+    class { 'admin': groups => ['snapshot-admins'] }
     include role::dataset::systemusers
     # The snapshot nodes do not include standard, so this is needed.
     # It also includes some duplication with other modules, and should really go away.
