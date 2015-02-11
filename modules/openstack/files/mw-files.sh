200db673c933dad87c7e53c328af1ae87008619f
andrewbogott <abogott@wikimedia.org>
2015-02-11 15:33:54 -0600

    When backup up images, actually back up the image dir.
    Change-Id: I04e11e88cd0fd932f6599166f3486d6fba32917e

--- modules/openstack/files/mw-files.sh
+++ modules/openstack/files/mw-files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 tar -czvf /a/backup/labswiki-$(date '+%Y%m%d')-files.tar.gz -C /srv mediawiki
-tar -czvf /a/backup/public/labswiki-$(date '+%Y%m%d')-images.tar.gz -C /usr/local/apache images
+tar -czvf /a/backup/public/labswiki-$(date '+%Y%m%d')-images.tar.gz -C /srv/org/wikimedia/controller/wikis images
