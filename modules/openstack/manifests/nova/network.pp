7f596eb14b1044ad31709f4dc533d96ddd05c156
Coren <marc@uberbox.org>
2015-02-12 10:10:07 -0500

    Labs: Increase labnet1001 conntrack tables
    There should also be monitoring of this and, ideally, an
    alert if the tables get overfull.
    
    Change-Id: I29a48adde2fc69a967d24c5ce3613632bc5ec371

--- modules/openstack/manifests/nova/network.pp
+++ modules/openstack/manifests/nova/network.pp
@@ -53,16 +53,23 @@ class openstack::nova::network($openstack_version=$::openstack::version, $novaco
     sysctl::parameters { 'openstack':
         values => {
             # Turn off IP filter
-            'net.ipv4.conf.default.rp_filter' => 0,
-            'net.ipv4.conf.all.rp_filter'     => 0,
+            'net.ipv4.conf.default.rp_filter'    => 0,
+            'net.ipv4.conf.all.rp_filter'        => 0,
 
             # Enable IP forwarding
-            'net.ipv4.ip_forward'         => 1,
-            'net.ipv6.conf.all.forwarding'    => 1,
+            'net.ipv4.ip_forward'                => 1,
+            'net.ipv6.conf.all.forwarding'       => 1,
 
             # Disable RA
-            'net.ipv6.conf.all.accept_ra'     => 0,
+            'net.ipv6.conf.all.accept_ra'        => 0,
+
+            # Increase connection tracking size
+            # and bucket since all of labs is
+            # tracked on the network host
+            'net.netfilter.nf_conntrack_max'     => 262144,
+            'net.netfilter.nf_conntrack_buckets' => 32768,
         },
         priority => 50,
     }
+
 }
