4029ee3777a97d0b6b0d0c8795cef633a3f7201b
Daniel Zahn <dzahn@wikimedia.org>
2015-02-11 16:00:21 -0800

    add index.html for static Bugzilla
    Add an index.html that explains static-bugzilla.wm.org.
    
    Show example links for a bug and an activity now that all are uploaded.
    
    Also, don't show the directory index. Valid HTML5 per W3C.
    
    Bug:T85140
    Change-Id: Ia1170d63692da2e7aacc3a75e4f4c5d6d6615ce0

--- modules/bugzilla/manifests/static.pp
+++ modules/bugzilla/manifests/static.pp
@@ -3,10 +3,18 @@
 class bugzilla::static {
 
     file { '/srv/org/wikimedia/static-bugzilla':
-            ensure => directory,
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0755';
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755';
+    }
+
+    file { '/srv/org/wikimedia/static-bugzilla/index.html':
+        ensure => present,
+        owner  => 'www-data',
+        group  => 'www-data',
+        mode   => '0400',
+        source => 'puppet:///modules/bugzilla/static-bz-index.html';
     }
 
     apache::site { 'static-bugzilla.wikimedia.org':
