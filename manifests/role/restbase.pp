7c519caca802b451a789cb07338086272146e5ae
Ori Livneh <ori@wikimedia.org>
2015-02-14 15:05:17 -0800

    RESTBase: add Icinga check_procs check for Cassandra
    Instruct Icinga to throw a tantrum unless there exists exactly one Java process
    belonging to user 'cassandra' with 'CassandraDaemon' in its argument list.
    
    Change-Id: Iad3951d4a2b9bc39ae860df6ee6f9ca6beea47d3

--- manifests/role/restbase.pp
+++ manifests/role/restbase.pp
@@ -6,4 +6,13 @@ class role::restbase {
     system::role { 'restbase': description => "Restbase ${::realm}" }
 
     include ::restbase
+
+    # Emit an Icinga alert unless there is exactly one Java process belonging
+    # to user 'cassandra' and with 'CassandraDaemon' in its argument list.
+    nrpe::monitor_service { 'cassandra':
+        description  => 'Cassandra database',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u cassandra -C java -a CassandraDaemon',
+        require      => Service['cassandra'],
+        critical     => 'true',
+    }
 }
