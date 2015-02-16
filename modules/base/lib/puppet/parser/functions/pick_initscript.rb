274b99ce621dcc74e2e85b25c5e824244fef611d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-02-16 15:49:59 +0100

    service_unit: allow custom init script in a single initsystem
    Adding the parameter $strict with a default value of true I don't change
    the default behaviour, but allows to set it to false in cases where we
    want to install a custom init script for e.g. systemd alone, a thing
    that may happen in fact quite regularly.
    
    Change-Id: I8990484cac56cb5fae891c9665756ab718d8022f

--- modules/base/lib/puppet/parser/functions/pick_initscript.rb
+++ modules/base/lib/puppet/parser/functions/pick_initscript.rb
@@ -2,29 +2,32 @@
 
 Puppet::Parser::Functions.newfunction(:pick_initscript,
                                        :type => :rvalue,
-                                       :arity => 4,
+                                       :arity => 5,
                                        :doc => <<-'HEREDOC'
 Takes as an input the init system currently installed, the
 available init scripts, and returns the chosen one.
 HEREDOC
 ) do |vals|
-  init_system, has_systemd, has_upstart, has_sysvinit = vals
-  has_custom = has_systemd || has_upstart || has_sysvinit
+  init_system, has_systemd, has_upstart, has_sysvinit, strict  = vals
+  has_custom = (has_systemd || has_upstart || has_sysvinit)
   # if we don't have custom scripts, we use the system defaults
   return false unless has_custom
   case init_system
   when 'systemd'
     return 'systemd' if has_systemd
     return 'sysvinit' if has_sysvinit
+    return false unless strict
     raise(ArgumentError,
           'This service unit has an upstart script but nothing useful for systemd')
   when 'upstart'
     return 'upstart' if has_upstart
     return 'sysvinit' if has_sysvinit
+    return false unless strict
     raise(ArgumentError,
           'This service unit has a systemd script but nothing useful for upstart')
   when 'sysvinit'
     return 'sysvinit' if has_sysvinit
+    return false unless strict
     raise(ArgumentError,
           'This service unit lacks a custom sysvinit script')
   else
