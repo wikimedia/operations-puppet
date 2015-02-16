264b08a4ce1e34931fcc9001b2a00bbb02c24509
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-02-16 10:39:01 +0000

    restbase: add LVS configuration
    note: as a followup to this we'll need a role or variable not to deploy LVS
    realserver during testing
    
    Bug: T89636
    Change-Id: I85d70c5b2aded256ab808789e9ec0e6b8ca3bd8b

--- manifests/role/restbase.pp
+++ manifests/role/restbase.pp
@@ -1,9 +1,14 @@
 # == Class role::restbase
 #
 
+@monitoring::group { 'restbase_eqiad': description => 'Restbase eqiad' }
+
 # Config should be pulled from hiera
 class role::restbase {
     system::role { 'restbase': description => "Restbase ${::realm}" }
 
     include ::restbase
+
+    include lvs::configuration
+    class { 'lvs::realserver': realserver_ips => $lvs::configuration::lvs_service_ips[$::realm]['restbase'][$::site] }
 }
