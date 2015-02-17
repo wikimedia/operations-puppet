cd92bcdeabed7f28dec52ef0c2c9ee2858e17e2b
Christian Aistleitner <christian@quelltextlich.at>
2015-02-17 23:29:58 +0100

    Disable cloning of TransparencyReport until the repo is public again
    Bug: T89640
    Change-Id: I92273b0602febc1ce4d5c8e9181267e497923b51

--- manifests/role/transparency.pp
+++ manifests/role/transparency.pp
@@ -10,10 +10,14 @@ class role::transparency {
     $repo_dir = '/srv/org/wikimedia/TransparencyReport'
     $docroot  = "${repo_dir}/build"
 
-    git::clone { 'wikimedia/TransparencyReport':
-        ensure    => latest,
-        directory => $repo_dir,
-    }
+    # The repo is currently not publicly clonable by private request.
+    # Hence, turning cloning off for now, without removing the content
+    # until the repo is public again
+    # T89640
+    #git::clone { 'wikimedia/TransparencyReport':
+    #    ensure    => latest,
+    #    directory => $repo_dir,
+    #}
 
     apache::site { 'transparency.wikimedia.org':
         content => template('apache/sites/transparency.wikimedia.org.erb'),
