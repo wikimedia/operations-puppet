accd19d6650b18d71797df714c2d1c0d0625d485
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-02-20 11:14:09 +0100

    mediawiki: install extension config files in the correct path
    On trusty/jessie, install the config files in the
    '/etc/php5/mods-available/conf.d' directory; also activate
    them. Finally, do not install apc.ini on newer PHP version as it's
    unstable/unsupported.
    
    Change-Id: Ib5734bb834b63145337d1ef3adce2975b201dd4e

--- modules/contint/manifests/packages.pp
+++ modules/contint/manifests/packages.pp
@@ -8,15 +8,16 @@ class contint::packages {
     include ::mediawiki::packages
     include ::mediawiki::packages::multimedia  # T76661
 
-    # Disable APC entirely it gets confused when files changes often
-    file { '/etc/php5/conf.d/apc.ini':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        source  => 'puppet:///modules/contint/disable-apc.ini',
-        require => Package['php-apc'],
+    if os_version('ubuntu >= trusty || debian >= jessie') {
+        # Disable APC entirely it gets confused when files changes often
+        file { '/etc/php5/conf.d/apc.ini':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            source  => 'puppet:///modules/contint/disable-apc.ini',
+            require => Package['php-apc'],
+        }
     }
-
     # Get several OpenJDK packages including the jdk to build mobile
     # applications.
     # (openjdk is the default distribution for the java define.
