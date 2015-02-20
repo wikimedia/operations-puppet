3b452f2c64157e82f5c1b097df3c40b0501385c0
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-02-20 11:14:09 +0100

    mediawiki: install extension config files in the correct path
    On trusty/jessie, install the config files in the
    '/etc/php5/mods-available/conf.d' directory; also activate
    them. Finally, do not install apc.ini on newer PHP version as it's
    unstable/unsupported.
    
    Change-Id: Ib5734bb834b63145337d1ef3adce2975b201dd4e

---
+++ modules/mediawiki/manifests/php_enmod.pp
@@ -0,0 +1,15 @@
+# === Define mediawiki::php_enmod
+#
+# Enables a php config file in newer ubuntu/debian hosts.
+
+define mediawiki::php_enmod {
+    requires_os('ubuntu >= trusty || debian >= jessie')
+
+    exec { "PHP module ${title} enable":
+        command     => "/usr/sbin/php5enmod -s ALL ${title}",
+        refreshonly => true,
+        user        => 'root',
+        group       => 'root',
+        subscribe   => File["/etc/php5/mods-available/${title}.ini"]
+    }
+}
