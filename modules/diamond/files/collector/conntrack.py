00edaa7c1b0701a83ecef2a0d30eea8f5c65f8a6
Coren <marc@uberbox.org>
2015-02-23 11:46:14 -0500

    Conntrack collector for diamond
    Collect connection tracking statistics from host; mostly useful in
    routers but potentially valuable anywhere iptables are used to stateful
    filtering.
    
    Bug: T90437
    Change-Id: Ic66317c972bcb1e8495ee2930a95ea2ed619814b

---
+++ modules/diamond/files/collector/conntrack.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+
+"""
+Collect connection tracking statistics from host; mostly useful in
+routers but potentially valuable anywhere iptables are used to stateful
+filtering.
+
+Reports:
+    - nf_conntrack_max
+    - nf_conntrack_count
+
+Adapted from PuppetAgentCollector
+
+"""
+
+import string
+import diamond.collector
+
+
+class ConntrackCollector(diamond.collector.Collector):
+
+    def get_default_config_help(self):
+        config_help = super(ConntrackCollector,
+                            self).get_default_config_help()
+        return config_help
+
+    def get_default_config(self):
+        config = super(ConntrackCollector, self).get_default_config()
+        return config
+
+    def _get_sysctl(self, name):
+
+        path = '/proc/sys/' + name.translate(str.maketrans('.', '/'))
+
+        try:
+            with open(path) as f:
+                value = f.read().rstrip('\n')
+            return value
+
+        except IOError:
+            return None
+
+    def collect(self):
+
+        value = self._get_sysctl('net.netfilter.nf_conntrack_max')
+        if value is not None:
+            self.publish('nf_conntrack_max', value)
+
+        value = self._get_sysctl('net.netfilter.nf_conntrack_count')
+        if value is not None:
+            self.publish('nf_conntrack_count', value)
