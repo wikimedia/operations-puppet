145cb3251ba6245427f17f883f9c6fc1b94531a1
Brandon Black <bblack@wikimedia.org>
2015-02-25 17:25:47 +0000

    mkfs for ext4 varnish filesystems on jessie
    Change-Id: Iec7da9408de5d1e045332c63a3d54fd8743b4f99

--- modules/varnish/manifests/setup_filesystem.pp
+++ modules/varnish/manifests/setup_filesystem.pp
@@ -1,41 +1,49 @@
 define varnish::setup_filesystem() {
-    if $::realm == 'labs' {
-        $custom_mount_opts = ',comment=cloudconfig'
-    }
-    elsif os_version('debian >= jessie') {
-        # setup_filesystem is only used on the non-bits caches,
-        #  which are all set up as ext4-on-SSD...
-        $custom_mount_opts = ',nobarrier,data=writeback,discard'
-    }
-    else {
-        # nodiratime is redundant, but I'm hoping to avoid
-        #  pointless puppet-triggered remount attempts on
-        #  the legacy boxes here...
-        $custom_mount_opts = ',nodiratime,nobarrier,logbufs=8'
-    }
-
-    $mount_options = "noatime$custom_mount_opts"
+    $base_mount_options = "noatime"
 
     if $::realm == 'labs' and $::site == 'eqiad' {
-      include labs_lvm
-      labs_lvm::volume { $title:
-        mountat => "/srv/${title}",
-        options => $mount_options,
-        fstype  => 'xfs',
-      }
+        # XXX we probably want to switch these off XFS as well,
+        #  but I'm unsure of the process here and what might
+        #  break if I change the definition for running instances...
+        include labs_lvm
+        labs_lvm::volume { $title:
+            mountat => "/srv/${title}",
+            options => "$base_mount_options,comment=cloudconfig",
+            fstype  => 'xfs',
+        }
     } else {
-      file { "/srv/${title}":
-          ensure => directory,
-          owner  => 'root',
-          group  => 'root',
-      }
+        if os_version('debian >= jessie') {
+            # setup_filesystem is only used on the non-bits caches,
+            #  which are all set up as ext4-on-SSD...
+            $mount_options = "$base_mount_options,nobarrier,data=writeback,discard"
 
-      mount { "/srv/${title}":
-          ensure  => mounted,
-          require => File["/srv/${title}"],
-          device  => "/dev/${title}",
-          fstype  => 'auto',
-          options => $mount_options,
-      }
+            # For the new jessie setup, we mkfs here post-install as well if the
+            #  label hasn't already been set during previous mkfs...
+            exec { "mkfs-${title}-varnish":
+                command => "/sbin/mke2fs -F -F -t ext4 -T huge -O sparse_super2 -m 0 -L ${title}-varnish /dev/${title}",
+                unless => "/sbin/e2label /dev/${title} 2>/dev/null | grep -q '^${title}-varnish$'",
+                before => Mount["/srv/${title}"],
+            }
+        }
+        else {
+            # nodiratime is redundant, but I'm hoping to avoid
+            #  pointless puppet-triggered remount attempts on
+            #  the legacy boxes here...
+            $mount_options = "$base_mount_options,nodiratime,nobarrier,logbufs=8"
+        }
+
+        file { "/srv/${title}":
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+        }
+
+        mount { "/srv/${title}":
+            ensure  => mounted,
+            require => File["/srv/${title}"],
+            device  => "/dev/${title}",
+            fstype  => 'auto',
+            options => $mount_options,
+        }
     }
 }
