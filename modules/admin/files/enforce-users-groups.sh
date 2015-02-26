c3089f96be949b031ae9979c01c3eafa04a50484
cpettet <rush@wikimedia.org>
2015-02-26 11:39:26 -0600

    admin enforce-users-groups exclude known accounts
    Accounts that fall out of the range but would be
    harmful to cleanup.  These will need to be dealt
    with individually.
    
    Change-Id: I3bf65e3e1cb4177eae4c7496316b0cce7aeb0813

--- modules/admin/files/enforce-users-groups.sh
+++ modules/admin/files/enforce-users-groups.sh
@@ -7,12 +7,38 @@
 # a UID above the ID_BOUNDARY. Removals are logged to syslog.
 # with 'dryrun' as first arg exits 1 if cleanup is needed
 
+# /etc/adduser.conf
 ID_BOUNDRY='999'
 ARCHIVE_DIR='/var/userarchive'
+EXCLUDE=("nobody" \
+         "l10nupdate" \
+         "gmetric" \    # nescio.esams.wikimedia.org
+         "mwdeploy" \   # vanadium.eqiad.wmnet
+         "gerrit2" \    # ytterbium.wikimedia.org
+         "spamd" \      # sodium.wikimedia.org:
+         "mwprof" \     # tungsten.eqiad.wmnet
+         "releases" \   # tin.eqiad.wmnet
+         "reprepro" \   # caesium.eqiad.wmnet
+         "mysql" \      # pc1001.eqiad.wmnet
+         "dbmon" \      # db1044.eqiad.wmnet
+         "txstatsd" \   # osmium.eqiad.wmnet
+         "chromium" \   # osmium.eqiad.wmnet
+         "parsoid-rt"); # ruthenium.eqiad.wmnet
 
 function log() {
     logger $1
     echo $1
+}
+
+in_array() {
+    local haystack=${1}[@]
+    local needle=${2}
+    for i in ${!haystack}; do
+        if [[ ${i} == ${needle} ]]; then
+            return 0
+        fi
+    done
+    return 1
 }
 
 if [ ! -d $ARCHIVE_DIR ]
@@ -38,9 +64,14 @@ for var in "${PASSWD_USERS[@]}"
 do
     username=`echo $var | cut -d ':' -f 1`
     uid=`echo $var | cut -d ':' -f 3`
+
+    # A few global accounts of dubious nature are ignored
+    if in_array EXCLUDE $username; then
+        continue
+    fi
+
     if [[ "$uid" -gt "$ID_BOUNDRY" ]]; then
         if [[ `/usr/bin/id $username` != *","* ]]; then
-
             #TEMP
             echo $var >> /var/log/admincleanup
 
