5b5bc6da2ed2317c1bb965a36264d84327fe9f31
Ori Livneh <ori@wikimedia.org>
2015-02-27 15:06:22 -0800

    Add statsdlb role and provision on graphite1001
    * Remove txstatsd role from graphite1001, since the txstatsd module
      is not multi-instance capable.
    * Add statsdlb role that listens on port 8125 and forwards to ports
      8126-8133.
    * Provision the configuratation files and upstart job definitions
      for a multi-instance txstatsd cluster via the role.
    
    Change-Id: I02b0c174e1a6e4d20bb82b6e452bb067e9c73646

---
+++ manifests/role/statsdlb.pp
@@ -0,0 +1,50 @@
+# == Class: role::statsdlb
+#
+# Provisions a statsdlb instance that listens for StatsD metrics on
+# on UDP port 8125 and forwards to backends on UDP ports 8126-8133.
+#
+class role::statsdlb {
+    class { '::statsdlb':
+        server_port   => 8125,
+        backend_ports => range(8126, 8133),
+    }
+
+    package { 'python-txstatsd': }
+
+    file { '/etc/txstatsd':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        recurse => true,
+        purge   => true,
+        force   => true,
+        source  => 'puppet:///files/txstatsd/backends',
+    }
+
+    file { '/etc/init/txstatsd':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        recurse => true,
+        purge   => true,
+        force   => true,
+        source  => 'puppet:///files/txstatsd/init',
+    }
+
+    group { 'txstatsd':
+        ensure => present,
+    }
+
+    user { 'txstatsd':
+        ensure     => present,
+        gid        => 'txstatsd',
+        shell      => '/bin/false',
+        home       => '/nonexistent',
+        system     => true,
+        managehome => false,
+    }
+
+    diamond::collector { 'UDPCollector': }
+}
