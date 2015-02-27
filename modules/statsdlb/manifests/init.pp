92e9cf2a2f4e673e027d30e7e6a59fe3ef5f44cf
Ori Livneh <ori@wikimedia.org>
2015-02-27 13:02:29 -0800

    Add statsdlb module
    Change-Id: I749b7c44be98206bd7d9c305b031735437a51b67

---
+++ modules/statsdlb/manifests/init.pp
@@ -0,0 +1,56 @@
+# == Class: statsdlb
+#
+# statsdlb is a StatsD-compatible proxy which routes metrics to multiple local
+# UDP ports. It chooses the backend port for each metric by hashing the metric
+# name, to ensure aggregation works properly. Its purpose is to allow scaling
+# StatsD implementations which cannot exploit multiple CPU cores.
+#
+# === Parameters
+#
+# [*server_port*]
+#   UDP port to listen on. Example: 9001.
+#
+# [*backend_ports*]
+#   Array of local UDP ports to which incoming metrics should be routed.
+#   Example: [ 9002, 9003, 9004 ].
+#
+# === Examples
+#
+#  # Listen on UDP port 9001 and route to ports 9002-9004:
+#  class { 'statsdlb':
+#    server_port   => 9001,
+#    backend_ports => [ 9002, 9003, 9004 ],
+#  }
+#
+class statsdlb(
+    $ensure = present,
+    $server_port,
+    $backend_ports,
+) {
+    validate_ensure($ensure)
+    validate_re($server_port, '^\d+$', '$server_port must be a port number')
+    validate_array($backend_ports, '$backend_ports must be an array')
+    validate_re("${backend_ports}", '^\d+(\s\d+)*$', '$backend_ports must be an array of port numbers')
+
+    package { 'statsdlb':
+        ensure => $ensure,
+    }
+
+    file { '/etc/default/statsdlb':
+        ensure  => $ensure,
+        content => sprintf("DAEMON_ARGS=%s %s\n", $server_port, join($backend_ports, ' ')),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+
+    service { 'statsdlb':
+        ensure    => ensure_service($ensure),
+        enable    => $ensure == 'present',
+        provider  => $::initsystem,
+        subscribe => [
+            Package['statsdlb'],
+            File['/etc/default/statsdlb'],
+        ],
+    }
+}
