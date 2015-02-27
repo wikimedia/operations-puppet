9a376684f92714abe3f8e3faa54662ecb038ab7f
dzahn <dzahn@wikimedia.org>
2015-02-27 14:43:50 -0800

    puppetize dbtree config file to connect to tendril
    Change-Id: Id1e2309a24e3b9870a293ea59c66c6d9c8d799d5

--- modules/noc/manifests/init.pp
+++ modules/noc/manifests/init.pp
@@ -2,6 +2,10 @@
 class noc {
 
     include ::apache
+    include passwords::tendril
+
+    $tendril_user_web = $passwords::tendril::db_user_web
+    $tendril_pass_web = $passwords::tendril::db_pass_web
 
     $ssl_settings = ssl_ciphersuite('apache-2.2', 'compat')
 
@@ -9,7 +13,6 @@ class noc {
         content => template('noc/noc.wikimedia.org.erb'),
     }
 
-    # ensure default site is removed
     include ::apache::mod::php5
     include ::apache::mod::userdir
     include ::apache::mod::cgi
@@ -18,5 +21,14 @@ class noc {
     # Monitoring
     monitoring::service { 'http-noc':
         description   => 'HTTP-noc',
-        check_command => 'check_http_url!noc.wikimedia.org!http://noc.wikimedia.org' }
+        check_command => 'check_http_url!noc.wikimedia.org!http://noc.wikimedia.org'
+    }
+
+    file { '/srv/mediawiki/docroot/noc/dbtree':
+        ensure => 'directory',
+        owner  => 'mwdeploy',
+        group  => 'mwdeploy',
+    }
+
 }
+
