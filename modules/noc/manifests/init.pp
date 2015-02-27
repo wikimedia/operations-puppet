301a1343c3e2dbc7eb9bebe4ff9fac0ad432ddc5
dzahn <dzahn@wikimedia.org>
2015-02-27 14:43:50 -0800

    puppetize dbtree config file to connect to tendril
    dbtree needs a config file with the db user and password
    to connect to tendril. so puppetize the config file as a template.
    
    Noticed private repo already had class passwords::tendril with a
    db_user and db_pass being used in mariadb module.
    
    But on neon we use a different user, tendril_web (/srv/tendril/lib/config.php)
    I took the user and password from there and added them to private
    puppet repo as db_user_web / db_pass_web.
    
    Bug:T90837
    
    Change-Id: Id1e2309a24e3b9870a293ea59c66c6d9c8d799d5

--- modules/noc/manifests/init.pp
+++ modules/noc/manifests/init.pp
@@ -9,14 +9,28 @@ class noc {
         content => template('noc/noc.wikimedia.org.erb'),
     }
 
-    # ensure default site is removed
     include ::apache::mod::php5
     include ::apache::mod::userdir
     include ::apache::mod::cgi
     include ::apache::mod::ssl
 
+    # dbtree config
+    include passwords::tendril
+    $tendril_user_web = $passwords::tendril::db_user_web
+    $tendril_pass_web = $passwords::tendril::db_pass_web
+
+    file { '/srv/mediawiki/docroot/noc/dbtree/config.php':
+        ensure  => 'present',
+        owner   => 'mwdeploy',
+        group   => 'mwdeploy',
+        content => template('noc/dbtree.config.template.php.erb'),
+    }
+
     # Monitoring
     monitoring::service { 'http-noc':
         description   => 'HTTP-noc',
-        check_command => 'check_http_url!noc.wikimedia.org!http://noc.wikimedia.org' }
+        check_command => 'check_http_url!noc.wikimedia.org!http://noc.wikimedia.org'
+    }
+
 }
+
