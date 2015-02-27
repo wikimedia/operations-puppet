b1a697d0441b3c2776b707122f9eff6e82884c05
dzahn <dzahn@wikimedia.org>
2015-02-27 13:49:40 -0800

    let puppet clone dbtree into noc/docroot/dbtree
    dbtree existed in 2 separate locations and the code had diverged.
    We moved it into a separate repo at operations/software/dbtree and
    now we can let puppet git clone it from there
    
    Bug:T90837
    
    Change-Id: I31d4f34d57808b021dbddcce7c466624a7baf319

--- modules/noc/manifests/init.pp
+++ modules/noc/manifests/init.pp
@@ -9,7 +9,6 @@ class noc {
         content => template('noc/noc.wikimedia.org.erb'),
     }
 
-    # ensure default site is removed
     include ::apache::mod::php5
     include ::apache::mod::userdir
     include ::apache::mod::cgi
@@ -18,5 +17,21 @@ class noc {
     # Monitoring
     monitoring::service { 'http-noc':
         description   => 'HTTP-noc',
-        check_command => 'check_http_url!noc.wikimedia.org!http://noc.wikimedia.org' }
+        check_command => 'check_http_url!noc.wikimedia.org!http://noc.wikimedia.org'
+    }
+
+    file { '/srv/mediawiki/docroot/noc/dbtree':
+        ensure => 'directory',
+        owner  => 'mwdeploy',
+        group  => 'mwdeploy',
+    }
+
+    git::clone { 'operations/software/dbtree':
+        directory => '/srv/mediawiki/docroot/noc/dbtree',
+        branch    => 'master',
+        owner     => 'mwdeploy',
+        group     => 'mwdeploy',
+        require   => File['/srv/mediawiki/docroot/noc/dbtree'],
+    }
+
 }
