2630ffda0ba74c9edceeff8ba43ec7a11920c7ca
Ori Livneh <ori@wikimedia.org>
2015-02-27 17:20:11 -0800

    statsdlb / txstatsd: add txstatsdctl and monitoring
    * Add an apachectl-like txstatsdctl for managing txstatsd instances.
    * Use that to declare a service resource for txstatsd.
    * Add icinga alerts for both the statsdlb front-end and the txstatsd back-ends.
    
    Change-Id: I4586d647c17e41371e9b7069be89ae65088ac4a8

--- manifests/role/statsdlb.pp
+++ manifests/role/statsdlb.pp
@@ -1,13 +1,25 @@
 # == Class: role::statsdlb
 #
 # Provisions a statsdlb instance that listens for StatsD metrics on
-# on UDP port 8125 and forwards to backends on UDP ports 8126-8133.
+# on UDP port 8125 and forwards to backends on UDP ports 8126-8133,
+# as well as the set of txstatsd backends that listen on these ports.
 #
 class role::statsdlb {
+
+    # statsdlb
+
     class { '::statsdlb':
         server_port   => 8125,
         backend_ports => range(8126, 8135),
     }
+
+    nrpe::monitor_service { 'statsdlb':
+        description   => 'statsdlb process',
+        nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 1: -C statsdlb',
+    }
+
+
+    # txstatsd back-ends
 
     package { 'python-txstatsd': }
 
@@ -44,6 +56,29 @@ class role::statsdlb {
         home       => '/nonexistent',
         system     => true,
         managehome => false,
+    }
+
+    file { '/usr/local/sbin/txstatsdctl':
+        source => 'puppet:///files/txstatsd/txstatsdctl',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        before => Service['txstatsd'],
+    }
+
+    service { 'txstatsd':
+        ensure   => 'running',
+        provider => 'base',
+        restart  => '/usr/local/sbin/txstatsdctl restart',
+        start    => '/usr/local/sbin/txstatsdctl start',
+        status   => '/usr/local/sbin/txstatsdctl status',
+        stop     => '/usr/local/sbin/txstatsdctl stop',
+    }
+
+    nrpe::monitor_service { 'txstatsd_backends':
+        description  => 'txstatsd backend instances',
+        nrpe_command => '/usr/local/sbin/txstatsdctl check',
+        require      => Service['txstatsd'],
     }
 
     diamond::collector { 'UDPCollector': }
