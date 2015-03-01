e8c64e206ac573cd2f8f078029258bd60179189d
Bryan Davis <bd808@wikimedia.org>
2015-03-01 07:55:58 +0000

    Add role::labs::mediawiki_vagrant
    Add a role and modules to provision and configure Vagrant to manage an LXC
    container to run as a MediaWiki-Vagrant instance.
    
    * Create a new trusty instance (doesn't work on jessie)
    * Apply ::role::labs::mediawiki_vagrant
    * Force a puppet run
    * Log out and log back in to pick up profile.d magic
    * cd /srv/mediawiki-vagrant
    * vagrant up
    * Setup wikitech to proxy to :8080 on your vm
    * Profit!
    
    Requires: I343c8da6e345933e29f92523638d3b19e133894b
    Requires: I0ee0caf19019ce6415519561ff33b3453b310468
    Bug: T106213
    Change-Id: I04cfa3d143cad92b2be57d3813f0b09ecfb45bf5

---
+++ modules/vagrant/manifests/mediawiki.pp
@@ -0,0 +1,69 @@
+# == Class: vagrant::mediawiki
+#
+# Provision MediaWiki-Vagrant
+#
+# === Parameters:
+# [*install_directory*]
+#   Directory where Vagrant stores global state.
+#   (default: '/srv/vagrant-data')
+#
+class vagrant::mediawiki(
+    $install_directory = '/srv/mediawiki-vagrant',
+) {
+    require ::vagrant::lxc
+
+    # Add a local NFS server to export the /srv/mediawiki-vagrant files to the
+    # LXC container. NFS is actually slower than native LXC sharing but it
+    # allows us to work around permissions problems that would otherwise
+    # require adding various user accounts to the host VM and ensuring that
+    # their UIDs match with the LXC guest.
+    package { 'nfs-kernel-server':
+        ensure => present,
+    }
+
+    # Add custom apparmor profile that allows NFS mounts
+    file { '/etc/apparmor.d/abstractions/lxc/container-base':
+        ensure  => 'present',
+        source  => 'puppet:///modules/vagrant/lxc/container-base',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        notify  => Service['apparmor'],
+    }
+
+    git::clone { 'mediawiki/vagrant':
+        directory => $install_directory,
+        origin    => 'https://gerrit.wikimedia.org/r/mediawiki/vagrant',
+        owner     => 'mwvagrant',
+        group     => 'wikidev',
+        shared    => true,
+        require   => User['mwvagrant'],
+    }
+
+    exec { 'mediawiki_vagrant_setup':
+        command     => "${install_directory}/setup.sh --silent",
+        # The first call to vagrant will fail if the plugin isn't installed at
+        # all. The second call will exit with an error if the plugin is
+        # installed but out of date.
+        unless      => '/usr/bin/vagrant plugin list | /bin/grep -q mediawiki-vagrant && /usr/bin/vagrant config --list',
+        cwd         => $install_directory,
+        user        => 'mwvagrant',
+        environment => [
+            "VAGRANT_HOME=${::vagrant::vagrant_home}",
+        ],
+        require     => Git::Clone['mediawiki/vagrant'],
+    }
+
+    # Create a local.yaml unless one already exists. This allows the user to
+    # modify the defaults and add additional settings as needed without
+    # fighting with Puppet over the contents of the file.
+    file { "${install_directory}/puppet/hieradata/local.yaml":
+        ensure  => 'present',
+        source  => 'puppet:///modules/vagrant/hieradata/local.yaml',
+        owner   => 'mwvagrant',
+        group   => 'wikidev',
+        mode    => '0664',
+        replace => false,
+        require => Git::Clone['mediawiki/vagrant'],
+    }
+}
