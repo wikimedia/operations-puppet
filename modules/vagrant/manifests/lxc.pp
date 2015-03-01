0915cda820a7b8270a8e144fe78237bdcd2d10a0
Bryan Davis <bd808@wikimedia.org>
2015-03-01 07:55:58 +0000

    [WIP] Add role::mediawiki_vagrant_lxc
    Add a role and modules to provision and configure Vagrant to manage an LXC
    container to run as a MediaWiki-Vagrant instance.
    
    Marked as work in progress because it contains some nasty hacks for
    installing the latest version of Vagrant. Backporting the latest Debian
    experimental vagrant package to our Ubuntu 14.04 apt repo would remove
    that need. See T93153 for a request to do just that.
    
    Requires: I343c8da6e345933e29f92523638d3b19e133894b
    Bug: T90892
    Change-Id: I04cfa3d143cad92b2be57d3813f0b09ecfb45bf5

---
+++ modules/vagrant/manifests/lxc.pp
@@ -0,0 +1,60 @@
+# == Class: vagrant::lxc
+#
+# Provision LXC for use as a Vagrant container
+#
+class vagrant::lxc {
+    require ::vagrant
+    require ::lxc
+
+    exec { 'install_vagrant_lxc':
+        command     => '/usr/bin/vagrant plugin install vagrant-lxc',
+        unless      => '/usr/bin/vagrant plugin list | /bin/grep -q vagrant-lxc',
+        user        => 'vagrant',
+        environment => "VAGRANT_HOME=${::vagrant::vagrant_home}",
+        require     => [ Package['lxc'], Package['build-essential'] ],
+    }
+
+    # Allow paswordless sudo for common vagrant lxc commands
+    # See https://github.com/fgrehm/vagrant-lxc/blob/master/lib/vagrant-lxc/command/sudoers.rb
+    sudo::user { 'vagrant-lxc':
+        user       => 'vagrant',
+        privileges => [
+            # Container config file
+            'ALL=(root) NOPASSWD: /bin/cat /var/lib/lxc/*/config',
+            # Shared folders
+            'ALL=(root) NOPASSWD: /bin/mkdir -p /var/lib/lxc/*/rootfs/*',
+            'ALL=(root) NOPASSWD: /bin/sed -r -e * -ibak /etc/exports',
+            'ALL=(root) NOPASSWD: /usr/bin/tee -a /etc/exports',
+            'ALL=(root) NOPASSWD: /usr/sbin/exportfs -ar',
+            # Container config customizations and pruning
+            'ALL=(root) NOPASSWD: /bin/cp -f /tmp/lxc-config* /var/lib/lxc/*/config',
+            'ALL=(root) NOPASSWD: /bin/chown root\:root /var/lib/lxc/*/config*',
+            # Template import
+            "ALL=(root) NOPASSWD: /bin/cp ${::vagrant::vagrant_home}/gems/gems/vagrant-lxc*/scripts/lxc-template /usr/share/lxc/templates/*",
+            'ALL=(root) NOPASSWD: /bin/chmod +x /usr/share/lxc/templates/*',
+            # Template removal
+            'ALL=(root) NOPASSWD: /bin/rm /usr/share/lxc/templates/*',
+            # Private network script and commands
+            'ALL=(root) NOPASSWD: /sbin/ip addr add */24 dev *',
+            'ALL=(root) NOPASSWD: /sbin/ifconfig * down',
+            'ALL=(root) NOPASSWD: /sbin/brctl delbr *',
+            "ALL=(root) NOPASSWD: ${::vagrant::vagrant_home}/gems/gems/vagrant-lxc*/scripts/pipework *",
+            # Driver commands
+            'ALL=(root) NOPASSWD: /usr/bin/which lxc-*',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-attach --name *',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-attach -h',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-config lxc.lxcpath',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-create --version',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-create -B * --template * --name *',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-destroy --name *',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-info --name *',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-ls',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-shutdown --name *',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-start -d --name *',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-stop --name *',
+            'ALL=(root) NOPASSWD: /usr/bin/lxc-version',
+            # Cleanup tmp files
+            'ALL=(root) NOPASSWD: /bin/rm -rf /var/lib/lxc/*/rootfs/tmp/*',
+        ],
+    }
+}
