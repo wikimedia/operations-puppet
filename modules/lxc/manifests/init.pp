26f39bdfde117d8ff7b90d4523b4ff6046e0b066
Bryan Davis <bd808@wikimedia.org>
2015-03-01 07:55:58 +0000

    Add role::mediawiki_vagrant_lxc
    Add a role and modules to provision and configure Vagrant to manage an LXC
    container to run as a MediaWiki-Vagrant instance.
    
    Requires: I343c8da6e345933e29f92523638d3b19e133894b
    Bug: T106213
    Change-Id: I04cfa3d143cad92b2be57d3813f0b09ecfb45bf5

---
+++ modules/lxc/manifests/init.pp
@@ -0,0 +1,38 @@
+# == Class: lxc
+#
+# Provision LXC
+#
+# === Parameters:
+# [*container_root*]
+#   Directory where LXC will store containers (default: '/srv/lxc')
+#
+class lxc(
+    $container_root = '/srv/lxc',
+) {
+    package { [
+        'lxc',
+        'lxc-templates',
+        'cgroup-lite',
+        'redir',
+        'bridge-utils',
+    ]:
+        ensure => present,
+    }
+
+    file { $container_root:
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    if $container_root != '/var/lib/lxc' {
+        # Symlink default LXC container storage directory to configured
+        # location
+        file { '/var/lib/lxc':
+            ensure => 'link',
+            target => $container_root,
+        }
+    }
+}
+
