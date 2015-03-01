7d6d13807fa226174c84b23aff48f747e85fc19e
Bryan Davis <bd808@wikimedia.org>
2015-03-01 07:55:58 +0000

    Add role::labs::mediawiki_vagrant
    Add a role and modules to provision and configure Vagrant to manage an LXC
    container to run as a MediaWiki-Vagrant instance.
    
    * Create a new trusty instance (doesn't work on jessie)
    * Apply ::role::labs::mediawiki_vagrant
    * Force a puppet run
    * Log out and log back in to pick up profile.d magic
    * cd /srv/mediawiki-vagrant
    * vagrant up
    * Setup wikitech to proxy to :8080 on your vm
    * Profit!
    
    Requires: I343c8da6e345933e29f92523638d3b19e133894b
    Requires: I0ee0caf19019ce6415519561ff33b3453b310468
    Bug: T106213
    Change-Id: I04cfa3d143cad92b2be57d3813f0b09ecfb45bf5

---
+++ modules/vagrant/manifests/plugin.pp
@@ -0,0 +1,35 @@
+# == Define: vagrant::plugin
+#
+# Provision a Vagrant plugin
+#
+# == Parameters:
+# [*ensure*]
+#   Whether the plugin should be installed. (default: present)
+#
+# [*plugin*]
+#   Plugin name. (default: $title)
+#
+define vagrant::plugin (
+    $ensure = 'present',
+    $plugin = $title,
+) {
+    require ::vagrant
+
+    Exec {
+        user        => 'mwvagrant',
+        cwd         => $::vagrant::vagrant_home,
+        environment => "VAGRANT_HOME=${::vagrant::vagrant_home}"
+    }
+
+    if $ensure == 'present' {
+        exec { "install_vagrant_plugin_${title}":
+            command => "/usr/bin/vagrant plugin install ${plugin}",
+            unless  => "/usr/bin/vagrant plugin list | /bin/grep -q ${plugin}",
+        }
+    } else {
+        exec { "uninstall_vagrant_plugin_${title}":
+            command => "/usr/bin/vagrant plugin uninstall ${plugin}",
+            onlyif  => "/usr/bin/vagrant plugin list | /bin/grep -q ${plugin}",
+        }
+    }
+}
