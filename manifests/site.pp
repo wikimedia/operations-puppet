45e5e5efa821a540746443012d598fc033970473
Ariel T. Glenn <ariel@wikimedia.org>
2015-03-02 18:16:20 +0200

    set up bonded interface for ms1001 plus ipv6 for it
    Change-Id: I6f5ef573da60f4a5cf83e3382c7f88a702af623b

--- manifests/site.pp
+++ manifests/site.pp
@@ -1772,8 +1772,14 @@ node 'ms1001.wikimedia.org' {
 
     include admin
 
+    interface::aggregate { 'bond0':
+        orig_interface => 'eth0',
+        members        => [ 'eth0', 'eth1' ],
+        lacp_rate      => 'fast'
+    }
     interface::add_ip6_mapped { 'main':
-        interface => 'eth0',
+        interface => 'bond0',
+        require   => Interface::Aggregate['bond0']
     }
     include standard
     include role::dataset::systemusers
