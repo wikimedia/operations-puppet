1983981fe5f40996301b36ff948da7cd93324b21
Brandon Black <bblack@wikimedia.org>
2015-03-03 16:15:07 +0000

    puppet-level dep of vk::instance on v instances
    Change-Id: I01ff65be5fc0857211b9d285724add9b9369172d

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -439,6 +439,10 @@ class role::cache {
         # so that logs will go to rsyslog the first time puppet
         # sets up varnishkafka.
         Rsyslog::Conf['varnishkafka'] -> Varnishkafka::Instance <|  |>
+
+        # Make sure varnishes are configured and started for the first time
+        # before the instances as well, or they fail to start initially...
+        Service <| tag == 'varnish_instance' |> -> Varnishkafka::Instance <| |>
     }
 
     # == Class varnish::kafka::webrequest
