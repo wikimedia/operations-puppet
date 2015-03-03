c782e62d27700c3dfb10131aed22afe5b7f62574
Tim Starling <tstarling@wikimedia.org>
2015-03-03 16:02:06 +1100

    Remove labs sshd_banner
    It was useful for a brief period after the new bastion was introduced,
    but it seems like overkill now to spam stderr every time you log in to
    labs just to provide this documentation link.
    
    Change-Id: I8a87d1836a89669a4cc8ece653f33acac8108d64

--- manifests/role/labsbastion.pp
+++ manifests/role/labsbastion.pp
@@ -3,13 +3,8 @@ class role::labs::bastion {
         description => 'Labs bastion host (with mosh enabled)',
     }
 
-    if ! $::labs_bastion_banner_skip {
-        file { '/etc/ssh/sshd_banner':
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            content => "\nIf you are having access problems, please see: https://wikitech.wikimedia.org/wiki/Access#Accessing_public_and_private_instances\n",
-        }
+    file { '/etc/ssh/sshd_banner':
+        ensure  => absent
     }
 
     package { 'mosh':
