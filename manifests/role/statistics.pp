372117d773c0cc786066a8159c1ec8ae30bba40e
dzahn <dzahn@wikimedia.org>
2015-03-04 09:05:04 -0800

    statistics: remove 2 UTF-8 characters
    fixing one more "invalid byte sequencein US-ASCII"
    that only happens with ruby > 1.9.x
    
    Bug:T91453
    Change-Id: I46112fcd51877ccd340e0141cfc8fd32a6d24c31

--- manifests/role/statistics.pp
+++ manifests/role/statistics.pp
@@ -94,7 +94,7 @@ class role::statistics::private inherits role::statistics {
 
     # aggregating hourly pagecount-all-sites project count files into
     # daily per site csvs.
-    # Although it is in the “private” role, the dataset actually isn't
+    # Although it is in the "private" role, the dataset actually isn't
     # private. We just keep it here to spare adding a separate role.
     include statistics::aggregator
 
