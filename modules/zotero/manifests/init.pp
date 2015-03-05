39c2531d85c7987e7f7cf194099e2d8060223297
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-03-05 13:45:18 +0200

    Puppet module for the zotero service
    * Add a puppet module for the zotero service.
    * Cleanup the citoid module as well since part of the zotero module
    comes from it.
    * Zotero uses xulrunner to execute as an xpcshell javascript module
    that then does XPCOM calls to the zotero firefox extension that
    should be deployed.
    * Deployment system is trebuchet.
    * Amend the role class to have firewall rules and monitoring as well as
    include the zotero module class
    * Supply tests
    * Use trebuchet to deploy zotero deploy code and translators
    * Supports outgoing proxy
    * translators dir is implied due to using trebuchet
    
    Bug: T89867
    
    Change-Id: I1a29116f7b0f62109994a501d8715c3dc7ac4624

---
+++ modules/zotero/manifests/init.pp
@@ -0,0 +1,81 @@
+# == Class: zotero
+#
+# Zotero is a service based on running the Zotero firefox extension via xpcshell
+# and javascript wrappers. It is meant to scrape URLs provided to it and return
+# metadata
+#
+# === Parameters
+#
+class zotero(
+    $proxy=undef,
+    $proxy_port=undef) {
+
+    package { 'xulrunner-dev':
+        ensure => present,
+    }
+
+    package { [
+         'zotero/translation-server',
+         'zotero/translators',
+        ]:
+        ensure   => present,
+        provider => trebuchet,
+    }
+
+    file { '/srv/deployment/zotero/translation-server/defaults/preferences/defaults.js':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('zotero/defaults.js.erb'),
+    }
+
+    group { 'zotero':
+        ensure => present,
+        name   => 'zotero',
+        system => true,
+    }
+
+    user { 'zotero':
+        gid    => 'zotero',
+        home   => '/nonexistent',
+        shell  => '/bin/false',
+        system => true,
+    }
+
+    file { '/var/log/zotero':
+        ensure => directory,
+        owner  => 'zotero',
+        group  => 'zotero',
+        mode   => '0755',
+    }
+
+    file { '/etc/logrotate.d/zotero':
+        content => template('zotero/logrotate.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+
+    file { '/etc/init/zotero.conf':
+        content => template('zotero/upstart.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+
+    service { 'zotero':
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        provider   => 'upstart',
+    }
+
+    Package['zotero/translation-server'] -> Service['zotero']
+    Package['zotero/translators'] -> Service['zotero']
+    Package['xulrunner-dev'] -> Service['zotero']
+    Package['zotero/translation-server'] -> File['/srv/deployment/zotero/translation-server/defaults/preferences/defaults.js']
+    Group['zotero'] -> User['zotero'] -> File['/var/log/zotero']
+    File['/var/log/zotero'] -> Service['zotero']
+    File['/etc/init/zotero.conf'] ~> Service['zotero']
+}
