e0c341a380f221ac376fc3ed36b12760e975f917
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-03-05 13:45:18 +0200

    Puppet module for the zotero service
    * Add a puppet module for the zotero service.
    * Cleanup the citoid module as well since part of the zotero module
    comes from it.
    * Zotero uses xulrunner to execute as an xpcshell javascript module
    that then does XPCOM calls to the zotero firefox extension that
    should be deployed.
    * Deployment system is trebuchet.
    * Amend the role class to have firewall rules and monitoring as well as
    include the zotero module class
    * Supply tests
    * Use trebuchet to deploy zotero deploy code and translators
    * Supports outgoing proxy
    * translators dir is implied due to using trebuchet
    * README file
    
    Bug: T89867
    
    Change-Id: I1a29116f7b0f62109994a501d8715c3dc7ac4624

--- manifests/role/zotero.pp
+++ manifests/role/zotero.pp
@@ -1,19 +1,26 @@
 # == Class: role::zotero
 #
-# Zotero is free and open-source reference management
-# software to manage bibliographic data and related research materials.
-# https://en.wikipedia.org/wiki/Zotero
+# Zotero is a free and open-source reference management software
+# for bibliographic data and related research materials.
+# See <https://en.wikipedia.org/wiki/Zotero>.
 #
-
 @monitoring::group { 'zotero_eqiad': description => 'Zotero eqiad' }
 @monitoring::group { 'zotero_codfw': description => 'Zotero codfw' }
 
 class role::zotero {
     system::role { 'zotero': description => "Zotero ${::realm}" }
 
-    # to be activated once we have the module ready
-    # include ::zotero
+    ferm::service { 'zotero_http_1969':
+        proto => 'tcp',
+        port  => '1969',
+    }
+
+    # The check command is specific enough to warrant its own nagios command
+    monitoring::service { 'zotero':
+        description   => 'zotero',
+        check_command => 'check_http_zotero!1969',
+    }
+
+    include ::zotero
     # include lvs::realserver
-
 }
-
