e9c1bc0607803595063f6aefe791d88557ee26c1
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-03-05 13:45:18 +0200

    Puppet module for the zotero service
    * Add a puppet module for the zotero service.
    * Cleanup the citoid module as well since part of the zotero module
    comes from it.
    * Zotero uses xulrunner to execute as an xpcshell javascript module
    that then does XPCOM calls to the zotero firefox extension that
    should be deployed.
    * Deployment system is trebuchet.
    * Amend the role class to have firewall rules and monitoring as well as
    include the zotero module class
    * Supply tests
    * Use trebuchet to deploy zotero deploy code and translators
    * Supports outgoing proxy
    * translators dir is implied due to using trebuchet
    
    Bug: T89867
    
    Change-Id: I1a29116f7b0f62109994a501d8715c3dc7ac4624

--- manifests/role/zotero.pp
+++ manifests/role/zotero.pp
@@ -1,19 +1,25 @@
 # == Class: role::zotero
 #
-# Zotero is free and open-source reference management
-# software to manage bibliographic data and related research materials.
-# https://en.wikipedia.org/wiki/Zotero
+# Zotero is free and open-source reference management software
+# bibliographic data and related research materials.
+# See <https://en.wikipedia.org/wiki/Zotero>.
 #
-
 @monitoring::group { 'zotero_eqiad': description => 'Zotero eqiad' }
 @monitoring::group { 'zotero_codfw': description => 'Zotero codfw' }
 
 class role::zotero {
     system::role { 'zotero': description => "Zotero ${::realm}" }
 
-    # to be activated once we have the module ready
-    # include ::zotero
+    ferm::service { 'zotero_http_1969':
+        proto => 'tcp',
+        port  => '1969',
+    }
+
+    monitoring::service { 'zotero':
+        description   => 'zotero',
+        check_command => 'check_http_on_port!1969',
+    }
+
+    include ::zotero
     # include lvs::realserver
-
 }
-
---
+++ modules/zotero/manifests/init.pp
@@ -0,0 +1,84 @@
+# == Class: zotero
+#
+# Zotero is a service based on running the Zotero Firefox extension via xpcshell
+# and JavaScript wrappers. It is meant to scrape URLs provided to it and return
+# metadata.
+#
+# === Parameters
+#
+# [*http_proxy*]
+#   HTTP proxy, in "host:port" format. Optional.
+#
+class zotero( $http_proxy = undef ) {
+
+    if $http_proxy =~ /:/ {
+        $http_proxy_port = regsubst($http_proxy, '.*:', '')
+        $http_proxy_host = regsubst($http_proxy, ':.*', '')
+    } else {
+        $http_proxy_port = undef
+        $http_proxy_host = undef
+    }
+
+    package { 'xulrunner-dev':
+        ensure => present,
+        before => Service['zotero'],
+    }
+
+    package { [ 'zotero/translation-server', 'zotero/translators' ]:
+        ensure   => present,
+        provider => 'trebuchet',
+        before   => Service['zotero'],
+    }
+
+    file { '/srv/deployment/zotero/translation-server/defaults/preferences/defaults.js':
+        content => template('zotero/defaults.js.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        require => Package['zotero/translation-server'],
+        before  => Service['zotero'],
+    }
+
+    group { 'zotero':
+        ensure => present,
+        system => true,
+    }
+
+    user { 'zotero':
+        gid    => 'zotero',
+        home   => '/nonexistent',
+        shell  => '/bin/false',
+        system => true,
+    }
+
+    file { '/var/log/zotero':
+        ensure => directory,
+        owner  => 'zotero',
+        group  => 'zotero',
+        mode   => '0755',
+        before => Service['zotero'],
+    }
+
+    file { '/etc/logrotate.d/zotero':
+        content => template('zotero/logrotate.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        before  => Service['zotero'],
+    }
+
+    file { '/etc/init/zotero.conf':
+        content => template('zotero/upstart.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['zotero'],
+    }
+
+    service { 'zotero':
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        provider   => 'upstart',
+    }
+}
