14f98599a8cb0120f6100db98d87f0ff86187d92
YuviPanda <yuvipanda@gmail.com>
2015-03-06 14:17:42 +0530

    labs: Unify lvm roles
    Also introduce a new, generic role that should be used instead
    of the mnt srv ones in the far, far future.
    
    Change-Id: Ie94bc2c6320e5db55001dd07720ea6a8bbdf6093

---
+++ manifests/role/labslvm.pp
@@ -0,0 +1,30 @@
+# Allocate all of the instance's extra space as /mnt or the user specified
+# mount point given in the $::lvm_mount_point global variable.
+# FIXME: Deprecate and kill with mild fire
+class role::labs::lvm::mnt {
+    include labs_lvm
+    $mount_point = $::lvm_mount_point ? {
+        undef   => '/mnt',
+        default => $::lvm_mount_point,
+    }
+    labs_lvm::volume { 'second-local-disk': mountat => $mount_point }
+}
+
+# Allocate all of the instance's extra space as /srv
+class role::labs::lvm::srv {
+    include labs_lvm
+    labs_lvm::volume { 'second-local-disk': mountat => '/srv' }
+}
+
+# Allocate a custom LVM volume
+class role::labs::lvm::volume(
+    $mountat = '/srv',
+    $size = '100%FREE',
+) {
+    include labs_lvm
+
+    labs_lvm::volume { 'second-local-disk':
+        mountat => $mountat,
+        size    => $size,
+    }
+}
--- manifests/role/labssrv.pp
+++
@@ -1,5 +0,0 @@
-# Allocate all of the instance's extra space as /srv
-class role::labs::lvm::srv {
-    include labs_lvm
-    labs_lvm::volume { 'second-local-disk': mountat => '/srv' }
-}
