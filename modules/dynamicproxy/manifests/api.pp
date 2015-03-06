743e0cddf8ab795db42595108e3f9c9b9cbb2648
dzahn <dzahn@wikimedia.org>
2015-03-05 18:39:13 -0800

    move misc/labsdebrepo out of misc to module
    One more thing in ./misc. We would like to get rid
    of the entire ./misc.
    
    Change-Id: If357a88e0b42eafc76d92fba93027a522de68a56

--- modules/dynamicproxy/manifests/api.pp
+++ modules/dynamicproxy/manifests/api.pp
@@ -13,7 +13,7 @@ class dynamicproxy::api(
 
     package { 'python-flask':
         ensure  => latest,
-        require => Class['misc::labsdebrepo'],
+        require => Class['::labs_debrepo'],
     }
 
     package { ['python-invisible-unicorn', 'python-flask-sqlalchemy', 'uwsgi', 'uwsgi-plugin-python']:
