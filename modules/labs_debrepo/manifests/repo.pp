6c147beac82102865065ae6ad0d9759008c7cd79
dzahn <dzahn@wikimedia.org>
2015-03-05 18:39:13 -0800

    move misc/labsdebrepo out of misc to module
    Change-Id: If357a88e0b42eafc76d92fba93027a522de68a56

--- modules/labs_debrepo/manifests/repo.pp
+++ modules/labs_debrepo/manifests/repo.pp
@@ -1,7 +1,4 @@
-# Puppet configuration to create local deb repositories and add them
-# to your sources.list.
-
-define labsdebrepo ($dir = $title, $handle = 'labsdebrepo') {
+define labsdebrepo::repo ($dir = $title, $handle = 'labsdebrepo') {
     # Manage $dir: Make sure it's a directory and turn it into a deb
     # repository.
     file { $dir:
@@ -10,19 +7,18 @@ define labsdebrepo ($dir = $title, $handle = 'labsdebrepo') {
 
     # Run "dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz".
     # dpkg-scanpackages is in dpkg-dev.
-    ensure_packages(['dpkg-dev'])
+    require_package(['dpkg-dev'])
     exec { "Turn ${dir} into deb repo":
         cwd     => $dir,
         command => '/usr/bin/dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz',
         # Only run if Packages.gz is *not* the newest file in the
         # directory or doesn't exist at all.
         onlyif  => '/usr/bin/test ! -e Packages.gz -o $(find . -newer Packages.gz | wc -l) -gt 0',
-        require => [Package['dpkg-dev'], File[$dir]],
     }
 
     # Add the directory-turned-repository to sources.list.
     file { "/etc/apt/sources.list.d/${handle}.list":
-        content => template('misc/labsdebrepo.erb'),
+        content => "deb [trusted=yes] file://<%= @dir %>/ /",
         require => Exec["Turn ${dir} into deb repo"],
     }
     file { "/etc/apt/preferences.d/${handle}.pref":
@@ -34,7 +30,3 @@ Pin-Priority: 1500
     }
 }
 
-class misc::labsdebrepo {
-    labsdebrepo { '/data/project/repo':
-    }
-}
