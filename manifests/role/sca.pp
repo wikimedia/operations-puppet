9074f6d1fb300ef1362831df5736c94c6eeb90fa
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-03-07 17:25:49 +0200

    Include the zotero role in the sca role
    Thus assigning zotero to run on the SCA (Service Cluster A) cluster
    Update the realserver ips for the role as well.
    
    Bug: T89869
    Change-Id: Ie7adfc80ec6f6553bd0cdcbc83b3122642b470b4

--- manifests/role/sca.pp
+++ manifests/role/sca.pp
@@ -1,6 +1,7 @@
-# "Compendium" class for nodes supporting various parsoid services
+# "Compendium" class for nodes supporting various *oid services
+# This class is an intermediate step to better design
 class role::sca {
-    include role::apertium, role::citoid, role::cxserver, role::mathoid
+    include role::apertium, role::citoid, role::cxserver, role::mathoid, role::zotero
     include standard
     include admin
     include lvs::realserver
