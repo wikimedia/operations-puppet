1c12d1cf9ad508442e3eafe5e6a747328373c072
Brandon Black <bblack@wikimedia.org>
2015-03-09 05:32:59 +0000

    repool cp1069
    Change-Id: I847e4ea94e91b6603e98bb90da27f2e3e5a6f72d

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -120,7 +120,7 @@ class role::cache {
                 'bits' => {
                     'eqiad' => ['cp1056.eqiad.wmnet',
                                 'cp1057.eqiad.wmnet',
-                                # 'cp1069.eqiad.wmnet', # Jessie
+                                'cp1069.eqiad.wmnet', # Jessie
                                 'cp1070.eqiad.wmnet', # Jessie
                     ],
                     'esams' => ['cp3019.esams.wikimedia.org',
