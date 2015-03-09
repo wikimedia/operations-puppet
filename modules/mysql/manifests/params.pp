b87ba890b293a98136c10d3fb49034e94094c0b7
YuviPanda <yuvipanda@gmail.com>
2015-03-09 23:56:14 +0530

    mysql: Cleanup mysql::param
    - Remove support for OSes that the WMF isn't going to
      be using anytime soon (FreeBSD?!)
    - Use $::initscript to figure out service provider
    
    Change-Id: I1c01360829c7075cc211d17c0b718a9eeaa1e73e

--- modules/mysql/manifests/params.pp
+++ modules/mysql/manifests/params.pp
@@ -12,155 +12,37 @@
 #
 class mysql::params {
 
-  $bind_address        = '127.0.0.1'
-  $port                = 3306
-  $etc_root_password   = false
-  $ssl                 = false
-  $restart             = false
+    $bind_address        = '127.0.0.1'
+    $port                = 3306
+    $etc_root_password   = false
+    $ssl                 = false
+    $restart             = false
 
-  case $::operatingsystem {
-    'Ubuntu': {
-      $service_provider = upstart
-    }
-    default: {
-      $service_provider = undef
-    }
-  }
+    $service_provider    = $::initsystem
 
-  case $::osfamily {
-    'RedHat': {
-      $basedir               = '/usr'
-      $datadir               = '/var/lib/mysql'
-      $service_name          = 'mysqld'
-      $client_package_name   = 'mysql'
-      $server_package_name   = 'mysql-server'
-      $socket                = '/var/lib/mysql/mysql.sock'
-      $pidfile               = '/var/run/mysqld/mysqld.pid'
-      $config_file           = '/etc/my.cnf'
-      $log_error             = '/var/log/mysqld.log'
-      $ruby_package_name     = 'ruby-mysql'
-      $ruby_package_provider = 'gem'
-      $python_package_name   = 'MySQL-python'
-      $php_package_name      = 'php-mysql'
-      $java_package_name     = 'mysql-connector-java'
-      $root_group            = 'root'
-      $ssl_ca                = '/etc/mysql/cacert.pem'
-      $ssl_cert              = '/etc/mysql/server-cert.pem'
-      $ssl_key               = '/etc/mysql/server-key.pem'
-    }
-
-    'Suse': {
-      $basedir               = '/usr'
-      $datadir               = '/var/lib/mysql'
-      $service_name          = 'mysql'
-      $client_package_name   = $::operatingsystem ? {
-        /OpenSuSE/           => 'mysql-community-server-client',
-        /(SLES|SLED)/        => 'mysql-client',
-        }
-      $server_package_name   = $::operatingsystem ? {
-        /OpenSuSE/           => 'mysql-community-server',
-        /(SLES|SLED)/        => 'mysql',
-        }
-      $socket                = $::operatingsystem ? {
-        /OpenSuSE/           => '/var/run/mysql/mysql.sock',
-        /(SLES|SLED)/        => '/var/lib/mysql/mysql.sock',
-        }
-      $pidfile               = '/var/run/mysql/mysqld.pid'
-      $config_file           = '/etc/my.cnf'
-      $log_error             = $::operatingsystem ? {
-        /OpenSuSE/           => '/var/log/mysql/mysqld.log',
-        /(SLES|SLED)/        => '/var/log/mysqld.log',
-        }
-      $ruby_package_name     = $::operatingsystem ? {
-        /OpenSuSE/           => 'rubygem-mysql',
-        /(SLES|SLED)/        => 'ruby-mysql',
-        }
-      $python_package_name   = 'python-mysql'
-      $java_package_name     = 'mysql-connector-java'
-      $root_group            = 'root'
-      $ssl_ca                = '/etc/mysql/cacert.pem'
-      $ssl_cert              = '/etc/mysql/server-cert.pem'
-      $ssl_key               = '/etc/mysql/server-key.pem'
-    }
-
-    'Debian': {
-
-      # begin WMF customization
-      if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '12.04') >= 0 {
+    if $::lsbdistid == 'Ubuntu' and versioncmp($::lsbdistrelease, '12.04') >= 0 {
         $ver = '5.5'
-      } else {
+    } else {
         $ver = '5.1'
-      }
-      $client_package_name  = "mysql-client-${ver}"
-      $server_package_name  = "mysql-server-${ver}"
-
-      $socket               = "/run/mysqld/mysqld.sock"
-      $pidfile              = "/run/mysqld/mysqld.pid"
-      $datadir              = '/var/lib/mysql'
-      $log_error            = '/var/log/mysql/mysql.err'
-      # end WMF customization
-
-      $basedir              = '/usr'
-      $service_name         = 'mysql'
-      $config_file          = '/etc/mysql/my.cnf'
-      $ruby_package_name    = 'libmysql-ruby'
-      $python_package_name  = 'python-mysqldb'
-      $php_package_name     = 'php5-mysql'
-      $java_package_name    = 'libmysql-java'
-      $root_group           = 'root'
-      $ssl_ca               = '/etc/mysql/cacert.pem'
-      $ssl_cert             = '/etc/mysql/server-cert.pem'
-      $ssl_key              = '/etc/mysql/server-key.pem'
     }
 
-    'FreeBSD': {
-      $basedir               = '/usr/local'
-      $datadir               = '/var/db/mysql'
-      $service_name          = 'mysql-server'
-      $client_package_name   = 'databases/mysql55-client'
-      $server_package_name   = 'databases/mysql55-server'
-      $socket                = '/tmp/mysql.sock'
-      $pidfile               = '/var/db/mysql/mysql.pid'
-      $config_file           = '/var/db/mysql/my.cnf'
-      $log_error             = "/var/db/mysql/${::hostname}.err"
-      $ruby_package_name     = 'ruby-mysql'
-      $ruby_package_provider = 'gem'
-      $python_package_name   = 'databases/py-MySQLdb'
-      $php_package_name      = 'php5-mysql'
-      $java_package_name     = 'databases/mysql-connector-java'
-      $root_group            = 'wheel'
-      $ssl_ca                = undef
-      $ssl_cert              = undef
-      $ssl_key               = undef
-    }
+    $client_package_name  = "mysql-client-${ver}"
+    $server_package_name  = "mysql-server-${ver}"
 
-    default: {
-      case $::operatingsystem {
-        'Amazon': {
-          $basedir               = '/usr'
-          $datadir               = '/var/lib/mysql'
-          $service_name          = 'mysqld'
-          $client_package_name   = 'mysql'
-          $server_package_name   = 'mysql-server'
-          $socket                = '/var/lib/mysql/mysql.sock'
-          $config_file           = '/etc/my.cnf'
-          $log_error             = '/var/log/mysqld.log'
-          $ruby_package_name     = 'ruby-mysql'
-          $ruby_package_provider = 'gem'
-          $python_package_name   = 'MySQL-python'
-          $php_package_name      = 'php-mysql'
-          $java_package_name     = 'mysql-connector-java'
-          $root_group            = 'root'
-          $ssl_ca                = '/etc/mysql/cacert.pem'
-          $ssl_cert              = '/etc/mysql/server-cert.pem'
-          $ssl_key               = '/etc/mysql/server-key.pem'
-        }
+    $socket               = "/run/mysqld/mysqld.sock"
+    $pidfile              = "/run/mysqld/mysqld.pid"
+    $datadir              = '/var/lib/mysql'
+    $log_error            = '/var/log/mysql/mysql.err'
 
-        default: {
-          fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only support osfamily RedHat, Debian, and FreeBSD, or operatingsystem Amazon")
-        }
-      }
-    }
-  }
-
+    $basedir              = '/usr'
+    $service_name         = 'mysql'
+    $config_file          = '/etc/mysql/my.cnf'
+    $ruby_package_name    = 'libmysql-ruby'
+    $python_package_name  = 'python-mysqldb'
+    $php_package_name     = 'php5-mysql'
+    $java_package_name    = 'libmysql-java'
+    $root_group           = 'root'
+    $ssl_ca               = '/etc/mysql/cacert.pem'
+    $ssl_cert             = '/etc/mysql/server-cert.pem'
+    $ssl_key              = '/etc/mysql/server-key.pem'
 }
