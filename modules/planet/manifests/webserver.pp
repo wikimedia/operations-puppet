8fca5e01ea6735941c5257eac77c23e05d50a986
dzahn <dzahn@wikimedia.org>
2015-03-09 14:06:33 -0700

    planet: webserver class, no ports.conf, simplify
    There's no need anymore to have that ports.conf with :443
    now that we don't speak SSL here anymore. Simplify but also
    set the dependencies for both modules, not just one.
    
    Change-Id: Id26eadf146751f9f3ab1c36dd21030eb8c889dc7

--- modules/planet/manifests/webserver.pp
+++ modules/planet/manifests/webserver.pp
@@ -1,24 +1,14 @@
-# sets up Apache with SSL for a planet-venus setup
+# sets up Apache for a planet-venus setup
 class planet::webserver {
 
-    # TODO to be replaced with new method in the future
     class { 'webserver::php5': }
     include ::apache::mod::rewrite
     # so we can vary on X-Forwarded-Proto when behind misc-web
     include ::apache::mod::headers
 
-    # we do this because NameVirtualHost *:443 isn't there by default
-    file { '/etc/apache2/ports.conf':
-        ensure => 'present',
-        mode   => '0444',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/planet/apache/ports.conf.ssl',
-    }
-
     # dependencies for webserver setup
     Class['webserver::php5'] ->
-    File['/etc/apache2/ports.conf'] ->
-    Class['::apache::mod::rewrite']
+    Class['::apache::mod::rewrite'] ->
+    Class['::apache::mod::headers']
 
 }
