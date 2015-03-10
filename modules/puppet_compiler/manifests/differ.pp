e69920605b02b095e48bee3f1b2453970150eacb
Matanya Moses <matanya@foss.co.il>
2015-03-10 22:33:11 +0200

    puppet_compiler: resource attributes quoting and minor lints
    bug: T91908
    Change-Id: I25f6e1b36c7f0870d3ade3356792d22553d3521a

--- modules/puppet_compiler/manifests/differ.pp
+++ modules/puppet_compiler/manifests/differ.pp
@@ -1,10 +1,9 @@
 class puppet_compiler::differ(
-    $envdir = "${::puppet_compiler::program_dir}/shell/env_puppet_3",
+    $envdir     = "${::puppet_compiler::program_dir}/shell/env_puppet_3",
     $modulepath = "${::puppet_compiler::puppetdir}/modules",
-    $user = $::puppet_compiler::user
-    ) {
-
-    exec {'Install catalog diff module':
+    $user       = $::puppet_compiler::user
+) {
+    exec { 'Install catalog diff module':
         command => "/usr/bin/bundle exec puppet module install zack-catalog_diff --modulepath=${modulepath}",
         cwd     => $envdir,
         user    => $user,
