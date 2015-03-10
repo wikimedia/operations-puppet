0b41cbfe2eb3cda6eb03096990142c3ffd3da5ad
Matanya Moses <matanya@foss.co.il>
2015-03-10 22:33:11 +0200

    puppet_compiler: resource attributes quoting and minor lints
    bug: T91908
    Change-Id: I25f6e1b36c7f0870d3ade3356792d22553d3521a

--- modules/puppet_compiler/manifests/bundle.pp
+++ modules/puppet_compiler/manifests/bundle.pp
@@ -1,7 +1,9 @@
 # Installs the deployment bundle needed by puppet
-define puppet_compiler::bundle( $program_dir = $puppet_compiler::program_dir ) {
-    $installer="${program_dir}/shell/installer"
-    exec {"install_puppet_bundle_${title}":
+define puppet_compiler::bundle(
+    $program_dir = $puppet_compiler::program_dir
+) {
+    $installer = "${program_dir}/shell/installer"
+    exec { "install_puppet_bundle_${title}":
         command => "${installer} ${title}",
         user    => $puppet_compiler::user,
         creates => "${program_dir}/shell/env_puppet_${title}/vendor",
