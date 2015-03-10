381436045a8c193f700af4d5e46413389fd7c6a6
Matanya Moses <matanya@foss.co.il>
2015-03-10 11:11:01 +0200

    monitoring: selector outside a resource
    Change-Id: Ie2cfe9c215623b92b090d0bf288b0c21cfcbc0c8

--- modules/monitoring/manifests/service.pp
+++ modules/monitoring/manifests/service.pp
@@ -23,6 +23,36 @@ define monitoring::service(
         default => hiera('nagios_group',"${cluster_name}_${::site}")
     }
 
+    $notification_critical = $critical ? {
+        'true'  => 240,
+        default => 0,
+    }
+
+    $contact_critical = $critical ? {
+        'true'  => 'admins,sms',
+        default => $contact_group,
+    }
+
+    $is_active = $passive ? {
+        'true'  => 0,
+        default => 1,
+    }
+
+    $check_volatile = $passive ? {
+        'true'  => 1,
+        default => 0,
+    }
+
+    $check_fresh = $passive ? {
+        'true'  => 1,
+        default => 0,
+    }
+
+    $is_fresh = $passive ? {
+        'true'  => $freshness,
+        default => undef,
+    }
+
     # Export the nagios service instance
     @@nagios_service { "${::hostname} ${title}":
         ensure                 => $ensure,
@@ -35,32 +65,14 @@ define monitoring::service(
         normal_check_interval  => $normal_check_interval,
         retry_check_interval   => $retry_check_interval,
         check_period           => '24x7',
-        notification_interval  => $critical ? {
-            'true'  => 240,
-            default => 0,
-        },
+        notification_interval  => $notification_critical,
         notification_period    => '24x7',
         notification_options   => 'c,r,f',
-        contact_groups         => $critical ? {
-            'true'  => 'admins,sms',
-            default => $contact_group,
-        },
+        contact_groups         => $contact_critical,
         passive_checks_enabled => 1,
-        active_checks_enabled  => $passive ? {
-            'true'  => 0,
-            default => 1,
-        },
-        is_volatile            => $passive ? {
-            'true'  => 1,
-            default => 0,
-        },
-        check_freshness        => $passive ? {
-            'true'  => 1,
-            default => 0,
-        },
-        freshness_threshold    => $passive ? {
-            'true'  => $freshness,
-            default => undef,
-        },
+        active_checks_enabled  => $is_active,
+        is_volatile            => $check_volatile,
+        check_freshness        => $check_fresh,
+        freshness_threshold    => $is_fresh,
     }
 }
