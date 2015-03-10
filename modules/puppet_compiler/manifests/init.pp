f1002d7ca88b13966cd138fe079f47c4b13b4559
Matanya Moses <matanya@foss.co.il>
2015-03-10 22:33:11 +0200

    puppet_compiler: resource attributes quoting and minor lints
    bug: T91908
    Change-Id: I25f6e1b36c7f0870d3ade3356792d22553d3521a

--- modules/puppet_compiler/manifests/init.pp
+++ modules/puppet_compiler/manifests/init.pp
@@ -2,7 +2,7 @@
 class puppet_compiler(
     $version = '0.1.0',
     $rootdir = '/opt/wmf',
-    $ensure  = present,
+    $ensure  = 'present',
     $user    = 'www-data'
     ) {
 
@@ -10,14 +10,14 @@ class puppet_compiler(
 
     $install_dir = "${rootdir}/software"
     $program_dir = "${install_dir}/compare-puppet-catalogs"
-    $puppetdir = "${program_dir}/external/puppet"
+    $puppetdir   = "${program_dir}/external/puppet"
 
     nginx::site {'puppet-compiler':
         ensure  => $ensure,
         content => template('puppet_compiler/nginx_site.erb'),
     }
 
-    file_line {'modify_nginx_magic_types':
+    file_line { 'modify_nginx_magic_types':
         path    => '/etc/nginx/mime.types',
         line    => "\ttext/plain\t\t\t\ttxt pson warnings out diff formatted;",
         match   => "\ttext/plain\t\t\t\ttxt",
@@ -26,7 +26,7 @@ class puppet_compiler(
     }
 
     # This wrapper defines the env variables for running.
-    file {'run_wrapper':
+    file { 'run_wrapper':
         ensure   => $ensure,
         path     => '/usr/local/bin/puppet-compiler',
         content  => template('puppet_compiler/run_wrapper.erb'),
@@ -35,30 +35,30 @@ class puppet_compiler(
 
 
     if $ensure != 'present' {
-        file{'root_dir':
-            ensure  => 'absent',
+        file { 'root_dir':
+            ensure  => absent,
             path    => $rootdir,
             owner   => $user,
             recurse => true,
             force   => true,
         }
     } else {
-        file{'root_dir':
-            ensure => 'directory',
+        file { 'root_dir':
+            ensure => directory,
             path   => $rootdir,
             owner  => $user,
             before => Git::Install['operations/software'],
         }
 
-        git::install {'operations/software':
-            ensure        => 'present',
+        git::install { 'operations/software':
+            ensure        => present,
             directory     => $install_dir,
             owner         => $user,
             git_tag       => "compare-puppet-catalogs-${version}",
             require       => Nginx::Site['puppet-compiler'],
         }
 
-        exec {'install_puppet_compare_requirements':
+        exec { 'install_puppet_compare_requirements':
             command => '/usr/bin/pip install requests simplediff',
             user    => 'root',
             require => Git::Install['operations/software'],
@@ -68,7 +68,7 @@ class puppet_compiler(
 
         # Now install the puppet repo
 
-        exec {'install_puppet_repositories':
+        exec { 'install_puppet_repositories':
             command => "${program_dir}/shell/helper install",
             user    => $user,
             creates => $puppetdir,
@@ -76,15 +76,19 @@ class puppet_compiler(
             notify  => Class['puppet_compiler::differ']
         }
 
-        class {'::puppetmaster::scripts':
+        class { '::puppetmaster::scripts':
             require => Exec['install_puppet_repositories']
         }
 
-        class {'puppet_compiler::differ':
+        class { 'puppet_compiler::differ':
             require => [Exec['install_puppet_repositories'],Class['::puppetmaster::scripts']]
         }
 
-        file {["${program_dir}/output", "${program_dir}/output/html","${program_dir}/output/diff", "${program_dir}/output/compiled",]:
+        file {[ "${program_dir}/output",
+                "${program_dir}/output/html",
+                "${program_dir}/output/diff",
+                "${program_dir}/output/compiled",
+            ]:
             ensure  => directory,
             owner   => $user,
             mode    => '0775',
@@ -92,7 +96,7 @@ class puppet_compiler(
         }
 
         $mysql_query = template('puppet_compiler/mysql_queries.erb')
-        exec {'mysql queries':
+        exec { 'mysql queries':
             command => "/usr/bin/mysql -NBe ${mysql_query}",
             unless  => "/usr/bin/mysql puppet -NBe 'SELECT 1' ",
             require => Package['mysql-server']
