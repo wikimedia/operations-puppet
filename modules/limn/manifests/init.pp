11a55e56413c9e1c88e370985b12b11c8264df87
Matanya Moses <matanya@foss.co.il>
2015-03-10 20:17:31 +0200

    limn: minor lint and Resource attributes quoting
    bug: T91908
    Change-Id: I72975b079d184809de61800ebfd42a91aee971b0

--- modules/limn/manifests/init.pp
+++ modules/limn/manifests/init.pp
@@ -11,48 +11,48 @@
 # $log_directory  - Default path to Limn server logs.  Default: /var/log/limn
 #
 class limn(
-  $var_directory  = '/var/lib/limn',
-  $log_directory  = '/var/log/limn')
-{
-  $user  = 'limn'
-  $group = 'limn'
+    $var_directory  = '/var/lib/limn',
+    $log_directory  = '/var/log/limn'
+){
+    $user  = 'limn'
+    $group = 'limn'
 
-  # Make sure nodejs is installed.
-  require_package('nodejs')
+    # Make sure nodejs is installed.
+    require_package('nodejs')
 
-  group { $group:
-    ensure => present,
-    system => true,
-  }
+    group { $group:
+        ensure => present,
+        system => true,
+    }
 
-  user { $user:
-    ensure     => present,
-    gid        => $group,
-    home       => $var_directory,
-    managehome => false,
-    system     => true,
-    require    => Group[$group],
-  }
+    user { $user:
+        ensure     => present,
+        gid        => $group,
+        home       => $var_directory,
+        managehome => false,
+        system     => true,
+        require    => Group[$group],
+    }
 
-  # Default limn containing data directory.
-  # Instances default to storing data in
-  # $var_directory/$name
-  file { $var_directory:
-    ensure  => 'directory',
-    owner   => $user,
-    group   => $group,
-    mode    => '0755',
-    require => [User[$user], Group[$group]],
-  }
+    # Default limn containing data directory.
+    # Instances default to storing data in
+    # $var_directory/$name
+    file { $var_directory:
+        ensure  => directory,
+        owner   => $user,
+        group   => $group,
+        mode    => '0755',
+        require => [User[$user], Group[$group]],
+    }
 
-  # Default limn log directory.
-  # Instances will log to
-  # $log_directory/limn-$name.log
-  file { $log_directory:
-    ensure  => 'directory',
-    owner   => $user,
-    group   => $group,
-    mode    => '0755',
-    require => [User[$user], Group[$group]],
-  }
+    # Default limn log directory.
+    # Instances will log to
+    # $log_directory/limn-$name.log
+    file { $log_directory:
+        ensure  => directory,
+        owner   => $user,
+        group   => $group,
+        mode    => '0755',
+        require => [User[$user], Group[$group]],
+    }
 }
