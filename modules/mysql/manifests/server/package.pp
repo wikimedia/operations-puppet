efbf4be9da50d1257070be109385eb4467dc54f6
Matanya Moses <matanya@foss.co.il>
2015-03-10 10:29:53 +0200

    mysql: selector outside a resource + 4 spaces
    Change-Id: I448387252f4650dbb48f1a803bf6498132c18d24

--- modules/mysql/manifests/server/package.pp
+++ modules/mysql/manifests/server/package.pp
@@ -1,22 +1,24 @@
 # This is handled by a separate class in case we want to just
 # install the package and configure elsewhere.
 class mysql::server::package (
-  $package_name     = $mysql::params::server_package_name,
+    $package_name     = $mysql::params::server_package_name,
 ) {
-  if $package_name =~ /mariadb/ {
-    apt::repository { 'wikimedia-mariadb':
-      uri        => 'http://apt.wikimedia.org/wikimedia',
-      dist       => 'precise-wikimedia',
-      components => 'mariadb',
+    if $package_name =~ /mariadb/ {
+        apt::repository { 'wikimedia-mariadb':
+        uri        => 'http://apt.wikimedia.org/wikimedia',
+        dist       => 'precise-wikimedia',
+        components => 'mariadb',
+        }
     }
-  }
 
-  package { 'mysql-server':
-    ensure    => $package_ensure,
-    name      => $package_name,
-    require   => $package_name ? {
-      'mariadb-server-5.5' => Apt::Repository['wikimedia-mariadb'],
-      default              => undef
+    $package_source = $package_name ? {
+        'mariadb-server-5.5' => Apt::Repository['wikimedia-mariadb'],
+        default              => undef
     }
-  }
+
+    package { 'mysql-server':
+        ensure    => $package_ensure,
+        name      => $package_name,
+        require   => $package_source,
+    }
 }
