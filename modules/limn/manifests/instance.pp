0303bf5e87c3a847de4daa315c1ade81db32e88d
Matanya Moses <matanya@foss.co.il>
2015-03-10 20:17:31 +0200

    limn: minor lint and Resource attributes quoting
    bug: T91908
    Change-Id: I72975b079d184809de61800ebfd42a91aee971b0

--- modules/limn/manifests/instance.pp
+++ modules/limn/manifests/instance.pp
@@ -10,83 +10,83 @@
 # $ensure         - present|absent.  Default: present
 #
 define limn::instance (
-  $port           = 8081,
-  $environment    = 'production',
-  $base_directory = '/usr/local/share/limn',
-  $var_directory  = "/var/lib/limn/${name}",
-  $log_file       = "/var/log/limn/limn-${name}.log",
-  $ensure         = 'present')
-{
-  require limn
+    $port           = '8081',
+    $environment    = 'production',
+    $base_directory = '/usr/local/share/limn',
+    $var_directory  = "/var/lib/limn/${name}",
+    $log_file       = "/var/log/limn/limn-${name}.log",
+    $ensure         = present
+){
+    require limn
 
-  validate_ensure($ensure)
+    validate_ensure($ensure)
 
-  if !defined(File[$base_directory]) {
-    file { $base_directory:
-      ensure => 'directory',
-      owner  => $limn::user,
-      group  => $limn::group,
-      mode   => '0775',
+    if !defined(File[$base_directory]) {
+        file { $base_directory:
+            ensure => directory,
+            owner  => $limn::user,
+            group  => $limn::group,
+            mode   => '0775',
+        }
     }
-  }
 
-  file { $var_directory:
-    ensure => 'directory',
-    owner  => $limn::user,
-    group  => $limn::group,
-    mode   => '0775',
-  }
+    file { $var_directory:
+        ensure => directory,
+        owner  => $limn::user,
+        group  => $limn::group,
+        mode   => '0775',
+    }
 
-  # The upstart init conf will start server.co
-  # logging to this file.
-  file { $log_file:
-    ensure => 'file',
-    owner  => $limn::user,
-    group  => $limn::group,
-    mode   => '0775',
-  }
+    # The upstart init conf will start server.co
+    # logging to this file.
+    file { $log_file:
+        ensure => file,
+        owner  => $limn::user,
+        group  => $limn::group,
+        mode   => '0775',
+    }
 
-  # symlink $base_directory/var/{css,js,vendor}
-  # in $var_directory
-  file { "${var_directory}/css":
-    ensure => ensure_link($ensure),
-    target => "${base_directory}/var/css",
-  }
-  file { "${var_directory}/js":
+    # symlink $base_directory/var/{css,js,vendor}
+    # in $var_directory
+    file { "${var_directory}/css":
+        ensure => ensure_link($ensure),
+        target => "${base_directory}/var/css",
+    }
+    file { "${var_directory}/js":
     ensure => ensure_link($ensure),
     target => "${base_directory}/var/js",
-  }
-  file { "${var_directory}/vendor":
-    ensure => ensure_link($ensure),
-    target => "${base_directory}/var/vendor",
-  }
+    }
+    file { "${var_directory}/vendor":
+        ensure => ensure_link($ensure),
+        target => "${base_directory}/var/vendor",
+    }
 
-  # Install an upstart init file for this limn server instance.
-  file { "/etc/init/limn-${name}.conf":
-    ensure    => $ensure,
-    content   => template('limn/init-limn.conf.erb'),
-    owner     => 'root',
-    group     => 'root',
-    mode      => '0444',
-    require   => [File[$var_directory], File[$log_file]],
-  }
+    # Install an upstart init file for this limn server instance.
+    file { "/etc/init/limn-${name}.conf":
+        ensure    => $ensure,
+        content   => template('limn/init-limn.conf.erb'),
+        owner     => 'root',
+        group     => 'root',
+        mode      => '0444',
+        require   => [File[$var_directory], File[$log_file]],
+    }
 
-  # Symlink an /etc/init.d script to upstart-job
-  # for SysV compatibility.
-  $sysv_ensure = $ensure ? {
-    present   => 'link',
-    default   => 'absent',
-  }
-  file { "/etc/init.d/limn-${name}":
-    ensure  => $sysv_ensure,
-    target  => '/lib/init/upstart-job',
-    require => File["/etc/init/limn-${name}.conf"],
-  }
+    # Symlink an /etc/init.d script to upstart-job
+    # for SysV compatibility.
+    $sysv_ensure = $ensure ? {
+        present   => link,
+        default   => absent,
+    }
+    file { "/etc/init.d/limn-${name}":
+        ensure  => $sysv_ensure,
+        target  => '/lib/init/upstart-job',
+        require => File["/etc/init/limn-${name}.conf"],
+    }
 
-  # Start the service.
-  service { "limn-${name}":
-    ensure     => ensure_service($ensure),
-    provider   => 'upstart',
-    subscribe  => File["/etc/init/limn-${name}.conf"],
-  }
+    # Start the service.
+    service { "limn-${name}":
+        ensure     => ensure_service($ensure),
+        provider   => 'upstart',
+        subscribe  => File["/etc/init/limn-${name}.conf"],
+    }
 }
