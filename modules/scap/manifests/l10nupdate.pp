7556994cb0c49c8d4fcb2d3a70c5d8f2d859eff3
Matanya Moses <matanya@foss.co.il>
2015-03-10 23:21:37 +0200

    scap: lint
    Change-Id: I841c880b750921e2dada89a610956ede51cf5b37

--- modules/scap/manifests/l10nupdate.pp
+++ modules/scap/manifests/l10nupdate.pp
@@ -8,32 +8,34 @@ class scap::l10nupdate(
         ensure  => present,
         command => '/usr/local/bin/l10nupdate-1 --verbose >> /var/log/l10nupdatelog/l10nupdate.log 2>&1',
         user    => 'l10nupdate',
-        hour    => 2,
-        minute  => 0;
+        hour    => '2',
+        minute  => '0',
     }
 
-    file {
-        '/usr/local/bin/l10nupdate':
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0555',
-            source => 'puppet:///modules/scap/l10nupdate';
-        '/usr/local/bin/l10nupdate-1':
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0555',
-            source => 'puppet:///modules/scap/l10nupdate-1';
+    file { '/usr/local/bin/l10nupdate':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/scap/l10nupdate',
+    }
+    file { '/usr/local/bin/l10nupdate-1':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/scap/l10nupdate-1',
+    }
         # add ssh keypair for l10nupdate user from fenari for RT-5187
-        '/home/l10nupdate/.ssh/id_rsa':
-            owner  => 'l10nupdate',
-            group  => 'l10nupdate',
-            mode   => '0400',
-            source => 'puppet:///private/ssh/tin/l10nupdate/id_rsa';
-        '/home/l10nupdate/.ssh/id_rsa.pub':
-            owner  => 'l10nupdate',
-            group  => 'l10nupdate',
-            mode   => '0444',
-            source => 'puppet:///private/ssh/tin/l10nupdate/id_rsa.pub';
+    file { '/home/l10nupdate/.ssh/id_rsa':
+        owner  => 'l10nupdate',
+        group  => 'l10nupdate',
+        mode   => '0400',
+        source => 'puppet:///private/ssh/tin/l10nupdate/id_rsa',
+    }
+    file { '/home/l10nupdate/.ssh/id_rsa.pub':
+        owner  => 'l10nupdate',
+        group  => 'l10nupdate',
+        mode   => '0444',
+        source => 'puppet:///private/ssh/tin/l10nupdate/id_rsa.pub',
     }
 
     # Make sure the log directory exists and has adequate permissions.
@@ -41,22 +43,25 @@ class scap::l10nupdate(
     # previously so it'll be an existing file on some systems.
     # Also create the dir for the SVN checkouts, and set up log rotation
     file { '/var/log/l10nupdatelog':
-            ensure => directory,
-            owner  => 'l10nupdate',
-            group  => $deployment_group,
-            mode   => '0664';
-        '/var/lib/l10nupdate':
-            ensure => directory,
-            owner  => 'l10nupdate',
-            group  => $deployment_group,
-            mode   => '0755';
-        '/var/lib/l10nupdate/caches':
-            ensure => directory,
-            owner  => $::mediawiki::users::web,
-            group  => $::mediawiki::users::web,
-            mode   => '0755';
-        '/etc/logrotate.d/l10nupdate':
-            source => 'puppet:///modules/scap/l10nupdate.logrotate',
-            mode   => '0444';
+        ensure => directory,
+        owner  => 'l10nupdate',
+        group  => $deployment_group,
+        mode   => '0664',
+    }
+    file { '/var/lib/l10nupdate':
+        ensure => directory,
+        owner  => 'l10nupdate',
+        group  => $deployment_group,
+        mode   => '0755',
+    }
+    file { '/var/lib/l10nupdate/caches':
+        ensure => directory,
+        owner  => $::mediawiki::users::web,
+        group  => $::mediawiki::users::web,
+        mode   => '0755',
+    }
+    file { '/etc/logrotate.d/l10nupdate':
+        source => 'puppet:///modules/scap/l10nupdate.logrotate',
+        mode   => '0444',
     }
 }
