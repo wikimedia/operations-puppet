8a94abc2d94b34fa6198713c7749eb8668cefb93
Matanya Moses <matanya@foss.co.il>
2015-03-10 23:52:17 +0200

    zuul: lint
    Change-Id: I1d90efb518ecb250304cb77bba9ad706a5d7c1a0

--- modules/zuul/manifests/init.pp
+++ modules/zuul/manifests/init.pp
@@ -17,82 +17,80 @@
 #
 class zuul (
     $git_source_repo = 'https://gerrit.wikimedia.org/r/p/integration/zuul.git',
-) {
+){
 
-  include zuul::user
+    include zuul::user
 
-  # Dependencies as mentionned in zuul:tools/pip-requires
-  $packages = [
-    'python-yaml',
-    'python-webob',
-    'python-daemon',
-    'python-lockfile',
-    'python-paramiko',
-    'python-jenkins',
-    'python-paste',
+    # Dependencies as mentionned in zuul:tools/pip-requires
+    $packages = [
+        'python-yaml',
+        'python-webob',
+        'python-daemon',
+        'python-lockfile',
+        'python-paramiko',
+        'python-jenkins',
+        'python-paste',
 
-    # GitPython at least 0.3.2RC1 which is neither in Lucid nor in Precise
-    # We had to backport it and its dependencies from Quantal:
-    'python-git',
-    'python-gitdb',
-    'python-async',
-    'python-smmap',
+        # GitPython at least 0.3.2RC1 which is neither in Lucid nor in Precise
+        # We had to backport it and its dependencies from Quantal:
+        'python-git',
+        'python-gitdb',
+        'python-async',
+        'python-smmap',
 
-    'python-extras',  # backported in Precise (bug 47122)
+        'python-extras',  # backported in Precise (bug 47122)
 
-    'python-pip',
-    'python-setuptools',
-    'python-voluptuous',
+        'python-pip',
+        'python-setuptools',
+        'python-voluptuous',
 
-    # For Zuul post v1.3.0
-    'python-pbr',
-    'python-gear',
-    'python-apscheduler',
+        # For Zuul post v1.3.0
+        'python-pbr',
+        'python-gear',
+        'python-apscheduler',
 
-    'python-babel',
-    'python-prettytable',
-  ]
+        'python-babel',
+        'python-prettytable',
+    ]
 
-  require_package('python-statsd')
-  ensure_packages($packages)
+    require_package('python-statsd')
+    ensure_packages($packages)
 
-  # Used to be in /var/lib/git/zuul but /var/lib/git can be used
-  # to replicate git bare repositories.
-  $zuul_source_dir = '/usr/local/src/zuul'
+    # Used to be in /var/lib/git/zuul but /var/lib/git can be used
+    # to replicate git bare repositories.
+    $zuul_source_dir = '/usr/local/src/zuul'
 
-  git::clone { 'integration/zuul':
-    ensure    => present,
-    directory => $zuul_source_dir,
-    origin    => $git_source_repo,
-    branch    => 'master',
-  }
+    git::clone { 'integration/zuul':
+        ensure    => present,
+        directory => $zuul_source_dir,
+        origin    => $git_source_repo,
+        branch    => 'master',
+    }
 
-  exec { 'install_zuul':
-    # Make sure to install without downloading from pypi
-    command     => 'python setup.py install',
-    environment => 'HTTP_PROXY=. HTTPS_PROXY=.',
-    cwd         => $zuul_source_dir,
-    path        => '/bin:/usr/bin',
-    umask       => 022,
-    refreshonly => true,
-    subscribe   => Git::Clone['integration/zuul'],
-    require     => [
-      Package[$packages],
-    ],
-  }
+    exec { 'install_zuul':
+        # Make sure to install without downloading from pypi
+        command     => 'python setup.py install',
+        environment => 'HTTP_PROXY=. HTTPS_PROXY=.',
+        cwd         => $zuul_source_dir,
+        path        => '/bin:/usr/bin',
+        umask       => '022',
+        refreshonly => true,
+        subscribe   => Git::Clone['integration/zuul'],
+        require     => Package[$packages],
+    }
 
-  file { '/etc/zuul':
-    ensure => directory,
-  }
+    file { '/etc/zuul':
+        ensure => directory,
+    }
 
-  file { '/var/log/zuul':
-    ensure  => directory,
-    owner   => 'zuul',
-  }
+    file { '/var/log/zuul':
+        ensure  => directory,
+        owner   => 'zuul',
+    }
 
-  file { '/var/lib/zuul':
-    ensure  => directory,
-    owner   => 'zuul',
-  }
+    file { '/var/lib/zuul':
+        ensure  => directory,
+        owner   => 'zuul',
+    }
 
 }
