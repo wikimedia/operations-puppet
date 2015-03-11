255700b95d98697fd39b27acbee966a50ed2293c
Matanya Moses <matanya@foss.co.il>
2015-03-11 09:42:04 +0200

    etherpad: resource attributes quoting
    Bug:T91908
    Change-Id: Ibccdee45684f56dd8536c06f21dcdc91227fc78d

--- modules/etherpad/manifests/init.pp
+++ modules/etherpad/manifests/init.pp
@@ -1,21 +1,21 @@
 # Install and manage Etherpad Lite
 
 class etherpad(
-            $etherpad_host,
-            $etherpad_ip,
-            $etherpad_port,
-            $etherpad_db_user,
-            $etherpad_db_host,
-            $etherpad_db_name,
-            $etherpad_db_pass,
-            ){
+    $etherpad_host,
+    $etherpad_ip,
+    $etherpad_port,
+    $etherpad_db_user,
+    $etherpad_db_host,
+    $etherpad_db_name,
+    $etherpad_db_pass,
+){
 
     package { 'etherpad-lite':
         ensure => 'latest',
     }
 
     service { 'etherpad-lite':
-        ensure    => running,
+        ensure    => 'running',
         enable    => true,
         require   => Package['etherpad-lite'],
         subscribe => File['/etc/etherpad-lite/settings.json'],
@@ -27,7 +27,7 @@ class etherpad(
     }
 
     file { '/usr/share/etherpad-lite/src/static/robots.txt':
-        ensure => present,
+        ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0444',
