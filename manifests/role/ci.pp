5c47238f3f1eafdb7796beb4f9858ca758a023f8
Faidon Liambotis <faidon@wikimedia.org>
2015-03-12 10:44:11 +0200

    jenkins: transition to ssh::userkey
    Bug: T92475
    Change-Id: I240282cb4c065ace16877fdd4124496c46223e03

--- manifests/role/ci.pp
+++ manifests/role/ci.pp
@@ -156,14 +156,11 @@ class role::ci::slave {
     }
 
     class { 'jenkins::slave':
-        ssh_authorized_key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw==',
-        ssh_key_name       => 'jenkins@gallium',
-        # Lamely restrict to master which is gallium
-        ssh_key_options    => [ 'from="208.80.154.135"' ],
-        user               => 'jenkins-slave',
-        workdir            => '/srv/ssd/jenkins-slave',
+        ssh_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw== jenkins@gallium from="208.80.154.135"',
+        user    => 'jenkins-slave',
+        workdir => '/srv/ssd/jenkins-slave',
         # Mount is handled on the node definition
-        require            => Mount['/srv/ssd'],
+        require => Mount['/srv/ssd'],
     }
 
     # .gitconfig file required for rare git write operations
