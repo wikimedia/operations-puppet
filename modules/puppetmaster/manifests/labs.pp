acf55ae7f25720d1a8bbed9c2ad2bd59c5c43414
YuviPanda <yuvipanda@gmail.com>
2015-03-13 12:13:40 +0530

    puppetmaster: Refactor out the auto key signer
    This also auto signs salt keys.
    
    Bug: T92606
    Change-Id: I76bca8c5962ea2f660abd3a4425ab45d5d32c8e4

--- modules/puppetmaster/manifests/labs.pp
+++ modules/puppetmaster/manifests/labs.pp
@@ -3,23 +3,8 @@
 # This class handles the Wikimedia Labs specific bits of a Puppetmaster
 class puppetmaster::labs {
     package { 'libldap-ruby1.8': ensure => latest; }
-    # Use a specific revision for the checkout, to ensure we are using
-    # a known and approved version of this script.
-    file {
-        '/usr/local/sbin/puppetsigner.py':
-            ensure => present,
-            source => 'puppet:///modules/puppetmaster/puppetsigner.py',
-            mode   => '0550',
-            owner  => 'root',
-            group  => 'root'
-    }
 
-    cron {
-        'puppet_certificate_signer':
-            command => '/usr/local/sbin/puppetsigner.py --scriptuser > /dev/null 2>&1',
-            require => File['/usr/local/sbin/puppetsigner.py'],
-            user    => 'root',
-    }
+    include puppetmaster::autosigner
 
     cron { 'update_public_puppet_repos':
         ensure      => present,
