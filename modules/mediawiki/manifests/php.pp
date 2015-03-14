c991bd90498762bb5150cf0cdd5b112e28e5ffbb
YuviPanda <yuvipanda@gmail.com>
2015-03-14 13:15:55 +0530

    mediawiki: Ensure that /etc/php5/apache dir exists
    Is from modphp apache module.
    
    Bug: T88442
    Change-Id: I8fdbdc3fe2d65fa1613ad9b4c26ce21c411f6e2e

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -17,7 +17,7 @@ class mediawiki::php {
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
-        require => Package['php5-common'],
+        require => Package['php5-common', 'libapache2-mod-php5'],
     }
 
     file { '/etc/php5/cli/php.ini':
