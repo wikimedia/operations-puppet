530b218c7f109747c2f652ed3edd472bad10255e
andrewbogott <abogott@wikimedia.org>
2015-03-16 15:19:20 -0500

    Holmium dns should use holmium's ip.
    Change-Id: I5b324ed186074bd809e4d1f633afa2a17c345bc1

--- manifests/role/labsdns.pp
+++ manifests/role/labsdns.pp
@@ -2,8 +2,8 @@ class role::labsdns {
     include passwords::pdns
 
     class { '::labs_dns':
-        dns_auth_ipaddress      => '208.80.154.19 208.80.154.18',
-        dns_auth_query_address  => '208.80.154.19',
+        dns_auth_ipaddress      => '208.80.154.12',
+        dns_auth_query_address  => '208.80.154.12',
         dns_auth_soa_name       => 'labs-ns0.wikimedia.org',
         pdns_db_host            => 'm1-master.eqiad.wmnet',
         pdns_db_password        => $passwords::pdns::db_pass,
