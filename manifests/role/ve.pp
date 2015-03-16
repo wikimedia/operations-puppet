165a2e1e42737a3437a9ee73f6fb79506c3a29ae
Gergő Tisza <gtisza@wikimedia.org>
2015-03-16 22:22:22 +0000

    [WIP] Make vbench more generic
    Separate VE-specific and osmium-specific parts from
    generic JS performance benchmark code.
    
    Could not get this to work for reasons that might or might not
    be related to the patch itself (Chrome just refuses to start),
    hence the [WIP].
    
    Bug: T92701
    Change-Id: I34c13a32300899c7b5cdf98aeb82384bae2ca239

--- manifests/role/ve.pp
+++ manifests/role/ve.pp
@@ -4,60 +4,11 @@
 # Chromium instance that supports remote debugging.
 #
 class role::ve {
+    include ::role::jsbench
     include ::mediawiki
     include ::mediawiki::web
     include ::mediawiki::web::sites
     include ::admin
-
-    # 1366x768 is the most common display resolution, according
-    # to http://gs.statcounter.com/.
-
-    class { 'xvfb':
-        resolution => '1366x768x24',
-    }
-
-    # Instruct Chromium to route all requests to localhost, and to
-    # disable various features that add noise to profiling or that
-    # rely on user input.
-
-    # class { 'chromium':
-    #     extra_args => [
-    #         '--disable-background-networking',
-    #         '--disable-client-side-phishing-detection',
-    #         '--disable-component-update',
-    #         '--disable-default-apps',
-    #         '--disable-extensions',
-    #         '--disable-hang-monitor',
-    #         '--disable-infobars',
-    #         '--disable-plugins-discovery',
-    #         '--disable-prompt-on-repost',
-    #         '--disable-suggestions-service',
-    #         '--disable-sync',
-    #         '--disable-translate',
-    #         '--disable-v8-idle-tasks',
-    #         '--disable-web-resources',
-    #         '--no-default-browser-check',
-    #         '--no-first-run',
-    #         '--host-rules="MAP * localhost, EXCLUDE upload.wikimedia.org"',
-    #         '--safebrowsing-disable-auto-update',
-    #         '--safebrowsing-disable-download-protection',
-    #     ],
-    # }
-
-
-    # vbench is a CLI tool for benchmarking VisualEditor.
-    # It uses `autobahn` and `twisted` for WebSocket support, which
-    # it needs so it can speak Chrome's remote debugging protocol.
-    # It uses `numpy` to calculate summary statistics.
-
-    require_package('python-autobahn', 'python-twisted', 'python-numpy')
-
-    file { '/usr/local/bin/vbench':
-        source => 'puppet:///files/ve/vbench',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
-    }
 
     file { '/usr/local/bin/vb':
         source => 'puppet:///files/ve/vb',
