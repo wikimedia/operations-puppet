a73aa3acdeb2513f2bc9a934640dfa58b4934800
Brandon Black <bblack@wikimedia.org>
2015-03-17 20:04:40 +0000

    Introduce a new sslcert module (to replace certs.pp) (#2)
    Cleanup certificates::base and move it in its init.pp
    
    (this is a reintroduction of this after its reversion in the
    production branch, to fix this branch for further work! The
    original was 0ab7a6b3)
    
    Change-Id: I7a663b827527d6177d64ec356f373d01c82848b2

---
+++ modules/sslcert/manifests/init.pp
@@ -0,0 +1,49 @@
+# == Class: sslcert
+#
+# Base class to manage X.509/TLS/SSL certificates.
+#
+# === Parameters
+#
+# === Examples
+#
+#  include sslcert
+#
+
+class sslcert {
+    package { [ 'openssl', 'ssl-cert', 'ca-certificates' ]:
+        ensure => present,
+    }
+
+    exec { 'update-ca-certificates':
+        command     => '/usr/sbin/update-ca-certificates',
+        refreshonly => true,
+        require     => Package['ca-certificates'],
+    }
+
+    # server certificates go in here; /etc/ssl/certs is a misnomer and actually
+    # is just for CAs. See e.g. <https://bugs.debian.org/608719>
+    file { '/etc/ssl/localcerts':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'ssl-cert',
+        mode    => '0755',
+        require => Package['ssl-cert'],
+    }
+
+    # Limit AppArmor support to just Ubuntu, for now
+    if $::operatingsystem == 'Ubuntu' {
+        include apparmor
+
+        # modify the default ssl_certs abstraction to support
+        # /etc/ssl/localcerts, as defined above
+        file { '/etc/apparmor.d/abstractions/ssl_certs':
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            source  => 'puppet:///modules/sslcert/apparmor/ssl_certs',
+            require => Package['apparmor'],
+            notify  => Service['apparmor'],
+        }
+    }
+}
