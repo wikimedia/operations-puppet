9ad29091b3548f233e760bd9e641302804bf3a71
Brandon Black <bblack@wikimedia.org>
2015-03-17 19:25:09 +0000

    Revert "Introduce a new sslcert module (to replace certs.pp)"
    This reverts commit 0ab7a6b3d636ac7f19512b620fc41586019f7683.
    
    Change-Id: I358a4af69aa96fd75d8f965613f2c63905b52370

--- manifests/certs.pp
+++ manifests/certs.pp
@@ -108,7 +108,49 @@ define install_certificate(
 }
 
 class certificates::base {
-    include ::sslcert
+
+    package { [ 'openssl', 'ssl-cert' ]:
+        ensure => 'latest',
+    }
+
+    exec { 'update-ca-certificates':
+        command => '/usr/sbin/update-ca-certificates',
+        refreshonly => true,
+    }
+
+    package { 'ca-certificates':
+        ensure => 'latest',
+        notify => Exec['update-ca-certificates'],
+    }
+
+    # Server certificates now uniformly go in there
+    file { '/etc/ssl/localcerts':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'ssl-cert',
+        mode    => '0755',
+        require => Package['ssl-cert'],
+    }
+
+    if $::operatingsystem == 'Ubuntu' {
+        ## NOTE: The ssl_certs abstraction for apparmor is known to exist
+        ## and be mutually compatible up to Trusty; new versions will need
+        ## validation before they are cleared.
+
+        include apparmor
+
+        if versioncmp($::lsbdistrelease, '14.04') > 0 {
+            fail("The apparmor profile for certificates::base is only known to work up to Trusty")
+        }
+        file { '/etc/apparmor.d/abstractions/ssl_certs':
+            ensure => file,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            source => 'puppet:///files/ssl/ssl_certs',
+            notify => Service['apparmor'],
+        }
+    }
 }
 
 class certificates::star_wmflabs_org {
