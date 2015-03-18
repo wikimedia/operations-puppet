48f6d3e7c3b4e2f733040903490c82e979465011
Tim Landscheidt <tim@tim-landscheidt.de>
2015-03-18 21:21:55 +0000

    Ignore some warnings about case statements without default matches
    Bug: T87132
    Change-Id: I841bd27e66887d9e71f158bbbe15fc4a5efcc135

--- modules/locales/manifests/all.pp
+++ modules/locales/manifests/all.pp
@@ -8,7 +8,9 @@ class locales::all {
 
     # Debian ships a locales-all package which has all locales pre-generated.
     # Ubuntu doesn't, so we're forced to generate them locally every time :(
+    # lint:ignore:case_without_default
     case $::operatingsystem {
+    # lint:endignore
         'Debian': {
             package { 'locales-all':
                 ensure => present,
