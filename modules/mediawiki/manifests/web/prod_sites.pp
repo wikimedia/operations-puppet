ec99afc2c2ec4890866758e8fbc80be756dcc57b
Chad Horohoe <chadh@wikimedia.org>
2015-03-18 09:42:35 -0700

    Move web::sites to web::prod_sites; begin unification in new class
    Per the fixme, we should unify these. Move the existing production
    sites out of the way so a future unified sites can exist. Nonexistent
    can go initially because the delta is small and we should be able
    to fix any fallout from that 404 directive
    
    Change-Id: I4e30c39aeaf7bd7e70f4ec6982116ba06d77cff2

---
+++ modules/mediawiki/manifests/web/prod_sites.pp
@@ -0,0 +1,72 @@
+class mediawiki::web::sites {
+    tag 'mediawiki', 'mw-apache-config'
+
+    #common code snippets that are included in the virtualhosts.
+    file { '/etc/apache2/sites-enabled/wikimedia-common.incl':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/apache/sites/wikimedia-common.incl',
+        before => Service['apache2'],
+    }
+
+    file { '/etc/apache2/sites-enabled/wikimedia-legacy.incl':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/apache/sites/wikimedia-legacy.incl',
+        before => Service['apache2'],
+    }
+
+    file { '/etc/apache2/sites-enabled/public-wiki-rewrites.incl':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/apache/sites/public-wiki-rewrites.incl',
+        before => Service['apache2'],
+    }
+
+    # Now the sites, in strict sequence
+    include ::mediawiki::web::sites
+
+    apache::site { 'wwwportals':
+        source   => 'puppet:///modules/mediawiki/apache/sites/wwwportals.conf',
+        priority => 1,
+    }
+
+    apache::site { 'redirects':
+        # this must be generated with the redirects.dat file still in apache-config,
+        # then committed to puppet. This will change soon.
+        source   => 'puppet:///modules/mediawiki/apache/sites/redirects.conf',
+        priority => 2,
+    }
+
+    apache::site { 'main':
+        source   => 'puppet:///modules/mediawiki/apache/sites/main.conf',
+        priority => 3,
+    }
+
+    apache::site { 'remnant':
+        source   => 'puppet:///modules/mediawiki/apache/sites/remnant.conf',
+        priority => 4,
+    }
+
+    apache::site { 'search.wikimedia':
+        source   => 'puppet:///modules/mediawiki/apache/sites/search.wikimedia.conf',
+        priority => 5,
+    }
+
+    apache::site { 'secure.wikimedia':
+        source   => 'puppet:///modules/mediawiki/apache/sites/secure.wikimedia.conf',
+        priority => 6,
+    }
+
+    apache::site { 'wikimania':
+        source   => 'puppet:///modules/mediawiki/apache/sites/wikimania.conf',
+        priority => 7,
+    }
+
+    apache::site { 'wikimedia':
+        source   => 'puppet:///modules/mediawiki/apache/sites/wikimedia.conf',
+        priority => 8,
+    }
+
+    apache::site { 'foundation':
+        source   => 'puppet:///modules/mediawiki/apache/sites/foundation.conf',
+        priority => 9,
+    }
+}
