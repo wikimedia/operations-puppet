0ad494b0971e26ac3b8be5b9b72c9f432f3ff3f7
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-03-18 14:24:04 +0200

    Ganeti module/role introduced
    A module for handling ganeti clusters. Includes code to handle KVM
    installation and KSM configuration, ganeti node handling, LVM
    configuration, DRBD installation as well as some useful puppet facts and
    scripts. The facts have no actual use right now but are going to be
    useful in the near future. Tests for the module are provided and are
    passing.
    Role class is included as well providing firewall, monitoring, key handling
    
    Bug: T87258
    Change-Id: I337dd68ec404a2e15ea40358436e177a9a168c3e

---
+++ manifests/role/ganeti.pp
@@ -0,0 +1,69 @@
+# Role classes for ganeti
+
+class role::ganeti {
+    include base::firewall
+    include ::ganeti
+
+    $ganeti_nodes = hiera('ganeti::nodes')
+
+    # Ganeti needs intracluster SSH root access
+    ssh::userkey { 'root':
+        ensure => present,
+        source => 'puppet:///files/ganeti/id_rsa.pub',
+    }
+
+    file { '/home/.ssh/id_rsa':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0400',
+        source => 'puppet:///private/files/ganeti/id_rsa',
+    }
+
+    $ganeti_ferm_nodes = join($ganeti_nodes, ' ')
+
+    # Same ganeti actions require SSH
+    ferm::service { 'ganeti_ssh_cluster':
+        proto  => 'tcp',
+        port   => 'ssh',
+        srange => "@resolv(($ganeti_ferm_nodes))",
+    }
+    # RAPI is the API of ganeti
+    ferm::service { 'ganeti_rapi_cluster':
+        proto  => 'tcp',
+        port   => 5080,
+        srange => "@resolv($ganeti_ferm_nodes))",
+    }
+    # Ganeti noded is responsible for all cluster/node actions
+    ferm::service { 'ganeti_noded_cluster':
+        proto  => 'tcp',
+        port   => 1811,
+        srange => "@resolv(($ganeti_ferm_nodes))",
+    }
+    nrpe::monitor_service{ 'ganeti-noded':
+        description  => 'ganeti-noded running',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -u root -C ganeti-noded'
+    }
+
+    # Ganeti confd provides a HA and fast way to query cluster configuration
+    ferm::service { 'ganeti_noded_cluster':
+        proto  => 'udp',
+        port   => 1814,
+        srange => "@resolv(($ganeti_ferm_nodes))",
+    }
+    nrpe::monitor_service{ 'ganeti-confd':
+        description  => 'ganeti-confd running',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -u gnt-confd -C ganeti-confd'
+    }
+
+    # Ganeti mond is the monitoring daemon. Data is available via port 1815
+    ferm::service { 'ganeti_noded_cluster':
+        proto  => 'tcp',
+        port   => 1815,
+        srange => "@resolv(($ganeti_ferm_nodes))",
+    }
+    nrpe::monitor_service{ 'ganeti-mond':
+        description  => 'ganeti-mond running',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -u root -C ganeti-mond'
+    }
+}
