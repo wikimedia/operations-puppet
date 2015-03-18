d99ee2561eeb6182759c89b151858fe3c3a3edc1
Chmarkine <chmarkine@hotmail.com>
2015-03-18 14:12:23 -0400

    tendril - Increase HSTS max-age to 1 year
    If4f89e34 enabled HSTS one week ago. Let's raise
    the max-age to 1 year now. 
    
    Bug: T40516
    Change-Id: I7373124fd2e098f147582ccbde97a87716b1efc6

--- manifests/role/tendril.pp
+++ manifests/role/tendril.pp
@@ -6,7 +6,7 @@ class role::tendril {
     system::role { 'role::tendril': description => 'tendril server' }
 
     install_certificate{ 'tendril.wikimedia.org': }
-    $ssl_settings = ssl_ciphersuite('apache-2.2', 'compat', '7')
+    $ssl_settings = ssl_ciphersuite('apache-2.2', 'compat', '365')
 
     class { '::tendril':
         site_name    => 'tendril.wikimedia.org',
