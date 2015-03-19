714b3f7f257133db6a8934e0f627a2559936d324
dzahn <dzahn@wikimedia.org>
2015-03-18 19:02:10 -0700

    (WIP) cassandra: add ferm rules
    Adding ferm rules for the cassandra ports to allow connections
    only from (other) restbase servers. Depends on Icd5cd19bbc8125.
    
    Bug:T92680
    Change-Id: I7d077c92641f356d51f59c5749427e53ab1ef453

--- manifests/role/cassandra.pp
+++ manifests/role/cassandra.pp
@@ -15,4 +15,26 @@ class role::cassandra {
         description  => 'Cassandra database',
         nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u cassandra -C java -a CassandraDaemon',
     }
+
+    $cassandra_hosts = hiera('restbase::cassandra::seeds')
+    $cassandra_host_ips = inline_template("<% _erbout.concat(Resolv::DNS.open.getaddress('$cassandra_hosts').to_s) %>")
+
+    # Cassandra intra-node messaging
+    ferm::service { 'cassandra-intra-node':
+        proto  => 'tcp',
+        port   => '7000',
+        srange => $cassandra_host_ips,
+    }
+    # Cassandra JMX/RMI
+    ferm::service { 'cassandra-jmx-rmi':
+        proto  => 'tcp',
+        port   => '7199',
+        srange => $cassandra_host_ips,
+    }
+    # Cassandra CQL query interface
+    ferm::service { 'cassandra-cql':
+        proto  => 'tcp',
+        port   => '9042',
+        srange => $cassandra_host_ips,
+    }
 }
