e9869eddec98537196c9e455c0552048ad79bf71
andrewbogott <abogott@wikimedia.org>
2015-03-20 13:01:46 -0500

    Add process monitoring for nova services.
    - scheduler
    - conductor
    - api
    - network
    
    Change-Id: I5fcc001dafd9be4cf6df408c5f3aca989fdd2553

--- modules/openstack/manifests/nova/conductor.pp
+++ modules/openstack/manifests/nova/conductor.pp
@@ -11,4 +11,9 @@ class openstack::nova::conductor($novaconfig) {
         subscribe => File['/etc/nova/nova.conf'],
         require   => Package["nova-conductor"];
     }
+
+    nrpe::monitor_service { 'check_nova_conductor_process':
+        description  => 'nova-conductor process',
+        nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/nova-conductor'",
+    }
 }
