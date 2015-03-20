fd33ee4d76c7cdf2cc6428452015812298bacd77
Tyler Cipriani <tcipriani@wikimedia.org>
2015-03-19 17:21:20 -0700

    Allow override of sync_common config
    Add an optional override of scap.cfg via hiera.
    
    Needed to automate the setup of appservers in the staging environment
    in labs.
    
    Bug: T91548
    Change-Id: Ia76802ecaee64bee8066ce33724e3c1c6992051e

--- modules/mediawiki/manifests/scap.pp
+++ modules/mediawiki/manifests/scap.pp
@@ -5,13 +5,20 @@
 # scap is installed, that the shell environment is configured for
 # MediaWiki, and that the MediaWiki deployment directory exists and
 # contains a copy of MediaWiki.
+
+# === Parameters:
+# [*defines*]
+#   Optional. Add override parameters for scap's config.
 #
-class mediawiki::scap {
+class mediawiki::scap(
+    $defines = undef,
+) {
     include ::mediawiki::users
 
     $mediawiki_deployment_dir = '/srv/mediawiki'
     $mediawiki_staging_dir    = '/srv/mediawiki-staging'
     $scap_bin_dir             = '/srv/deployment/scap/scap/bin'
+    $scap_cmd                 = "${scap_bin_dir}/sync-common"
     $mediawiki_web_user       = $::mediawiki::users::web
 
     # 'scap' is the command-line tool we use to push MediaWiki changes
@@ -48,8 +55,18 @@ class mediawiki::scap {
     # If this is a new install, populate /srv/mediawiki by retrieving
     # the current MediaWiki deployment tree from the deployment server.
 
+    # override scap config file with hiera variables in labs, e.g.:
+    # mediawiki::scap::defines: master_rsync:staging-tin.eqiad.wmflabs
+    if $defines {
+        $scap_defines = join(concat([], $defines), ' --define ')
+
+        $sync_common = "${scap_cmd} --define ${scap_defines}"
+    } else {
+        $sync_common = $scap_cmd
+    }
+
     exec { 'fetch_mediawiki':
-        command => "${scap_bin_dir}/sync-common",
+        command => $sync_common,
         creates => "${mediawiki_deployment_dir}/docroot",
         require => [ File[$mediawiki_deployment_dir], Package['scap'] ],
         timeout => 30 * 60,  # 30 minutes
