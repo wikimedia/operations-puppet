49df00f793c1d3ab8dda0bfd2176fdf23894b9c0
Chmarkine <chmarkine@hotmail.com>
2015-03-20 22:08:37 -0400

    RT - Enable HSTS max-age=7 days
    https://rt.wikimedia.org/ is HTTPS only.
    
    Bug: T40516
    Change-Id: I0d4d0afe4033a7583a5f8a8042c5a0c28bf84eed

--- manifests/role/requesttracker.pp
+++ manifests/role/requesttracker.pp
@@ -6,7 +6,7 @@ class role::requesttracker {
 
     install_certificate { 'rt.wikimedia.org': }
 
-    $ssl_settings = ssl_ciphersuite('apache-2.2', 'compat')
+    $ssl_settings = ssl_ciphersuite('apache-2.2', 'compat', '7')
 
     class { '::requesttracker':
         apache_site => 'rt.wikimedia.org',
