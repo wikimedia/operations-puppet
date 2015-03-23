482427368b4cb32f02f91d6e66ab361e71fada3a
YuviPanda <yuvipanda@gmail.com>
2015-03-24 00:37:45 +0530

    puppetmaster: Cleanup autosigner script some more
    Change-Id: I68a6794d9978fe3b5877069e498d64f6027cf861

--- modules/puppetmaster/files/puppetsigner.py
+++ modules/puppetmaster/files/puppetsigner.py
@@ -6,80 +6,13 @@
 #####################################################################
 
 import sys
-import traceback
+import re
 import ldapsupportlib
 import subprocess
 import os
 import json
-from optparse import OptionParser
-
-try:
-    import ldap
-except ImportError:
-    sys.stderr.write("Unable to import LDAP library.\n")
-    sys.exit(1)
-
-
-def main():
-    parser = OptionParser(conflict_handler="resolve")
-    parser.set_usage("puppetsigner [options]")
-    ldapSupportLib = ldapsupportlib.LDAPSupportLib()
-    ldapSupportLib.addParserOptions(parser)
-    (options, args) = parser.parse_args()
-    ldapSupportLib.setBindInfoByOptions(options, parser)
-    ds = ldapSupportLib.connect()
-    basedn = ldapSupportLib.getLdapInfo('base')
-    try:
-        proc = subprocess.Popen(['/usr/bin/puppet', 'cert', 'list'], stdout=subprocess.PIPE)
-        hosts = proc.communicate()
-        hosts = hosts[0].split()
-        for host in hosts:
-            if host[0] == "(":
-                continue
-            host = host.strip('"')
-            query = "(&(objectclass=puppetclient)(|(dc=" + host + ")(cnamerecord=" + host + ")(associateddomain=" + host + ")))"
-            PosixData = ds.search_s(basedn, ldap.SCOPE_SUBTREE, query)
-            if not PosixData:
-                path = getPuppetInfo('ssldir') + '/ca/requests/' + host + '.pem'
-                try:
-                    os.remove(path)
-                except Exception:
-                    sys.stderr.write('Failed to remove the certificate: ' + path + '\n')
-            else:
-                subprocess.Popen(['/usr/bin/puppet', 'cert', 'sign', host], stderr=subprocess.PIPE)
-                subprocess.Popen(['/usr/bin/php',
-                                  '/srv/org/wikimedia/controller/wikis/w/extensions/OpenStackManager/maintenance/onInstanceActionCompletion.php',
-                                  '--action', 'build',
-                                  '--instance', host], stderr=subprocess.PIPE)
-        proc = subprocess.Popen(['/usr/bin/salt-key',
-                                 '--list', 'unaccepted',
-                                 '--out', 'json'], stdout=subprocess.PIPE)
-        hosts = proc.communicate()
-        hosts = json.loads(hosts[0])
-        for host in hosts["minions_pre"]:
-            query = "(&(objectclass=puppetclient)(|(dc=" + host + ")(cnamerecord=" + host + ")(associateddomain=" + host + ")))"
-            PosixData = ds.search_s(basedn, ldap.SCOPE_SUBTREE, query)
-            if not PosixData:
-                subprocess.Popen(['/usr/bin/salt-key', '-y', '-d', host], stdout=subprocess.PIPE)
-            else:
-                subprocess.Popen(['/usr/bin/salt-key', '-y', '-a', host], stderr=subprocess.PIPE)
-    except ldap.PROTOCOL_ERROR:
-        sys.stderr.write("There was an LDAP protocol error; see traceback.\n")
-        traceback.print_exc(file=sys.stderr)
-        ds.unbind()
-        sys.exit(1)
-    except Exception:
-        try:
-            sys.stderr.write("There was a general error, this is unexpected; see traceback.\n")
-            traceback.print_exc(file=sys.stderr)
-            ds.unbind()
-        except Exception:
-            sys.stderr.write("Also failed to unbind.\n")
-            traceback.print_exc(file=sys.stderr)
-        sys.exit(1)
-
-    ds.unbind()
-    sys.exit(0)
+import ldap
+from optparse import OptionParser # FIXME: Use argparse
 
 
 def getPuppetInfo(attr, conffile="/etc/puppet/puppet.conf"):
@@ -87,7 +20,58 @@ def getPuppetInfo(attr, conffile="/etc/puppet/puppet.conf"):
     for line in f:
         if line.split('=', 1)[0].strip() == attr:
             return line.split('=', 1)[1].strip()
-            break
 
-if __name__ == "__main__":
-    main()
+
+parser = OptionParser(conflict_handler="resolve")
+parser.set_usage("puppetsigner [options]")
+ldapSupportLib = ldapsupportlib.LDAPSupportLib()
+ldapSupportLib.addParserOptions(parser)
+(options, args) = parser.parse_args()
+
+ldapSupportLib.setBindInfoByOptions(options, parser)
+ds = ldapSupportLib.connect()
+basedn = ldapSupportLib.getLdapInfo('base')
+
+try:
+    puppet_output = subprocess.check_output(['/usr/bin/puppet', 'cert', 'list'])
+    hosts = puppet_output.split()
+    for host in hosts:
+        # check to make sure hostname is actual hostname, to prevent
+        # ldap injection attacks
+        if host[0] == "(":
+            continue # FIXME: WAT
+        host = host.strip('"') # FIXME: WAT
+        if not re.match(r'^[a-zA-Z0-9_-]+\.eqiad\.wmflabs$', host):
+            print 'Invalid hostname', host
+            sys.exit(-1)
+        query = "(&(objectclass=puppetclient)(|(dc=" + host + ")(cnamerecord=" + host + ")(associateddomain=" + host + ")))"
+        host_info = ds.search_s(basedn, ldap.SCOPE_SUBTREE, query)
+        if not host_info:
+            path = getPuppetInfo('ssldir') + '/ca/requests/' + host + '.pem'
+            try:
+                os.remove(path)
+            except Exception:
+                # FIXME: WAT
+                sys.stderr.write('Failed to remove the certificate: ' + path + '\n')
+        else:
+            subprocess.check_call(['/usr/bin/puppet', 'cert', 'sign', host])
+            subprocess.check_call(['/usr/bin/php',
+                                   '/srv/org/wikimedia/controller/wikis/w/extensions/OpenStackManager/maintenance/onInstanceActionCompletion.php',
+                                   '--action', 'build',
+                                   '--instance', host])
+    salt_output = subprocess.check_call(['/usr/bin/salt-key',
+                                         '--list', 'unaccepted',
+                                         '--out', 'json'])
+    hosts = json.loads(salt_output)
+    for host in hosts["minions_pre"]:
+        if not re.match(r'^[a-zA-Z0-9_-]+\.eqiad\.wmflabs$', host):
+            print 'Invalid hostname', host
+            sys.exit(-1)
+        query = "(&(objectclass=puppetclient)(|(dc=" + host + ")(cnamerecord=" + host + ")(associateddomain=" + host + ")))"
+        host_result = ds.search_s(basedn, ldap.SCOPE_SUBTREE, query)
+        if not host_result:
+            subprocess.check_call(['/usr/bin/salt-key', '-y', '-d', host])
+        else:
+            subprocess.check_call(['/usr/bin/salt-key', '-y', '-a', host])
+finally:
+    ds.unbind()
