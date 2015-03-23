5e194f8e2995d413c5937375b534c7ef9fec8776
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-03-23 15:23:52 +0100

    ganglia: autogenerate datasources from the list of clusters
    Bug:T93776
    Change-Id: Ia32ea48028ff3b082f240cc01c602b0e9a6587f9

---
+++ modules/ganglia_new/lib/puppet/parser/functions/ganglia_aggregator_config.rb
@@ -0,0 +1,33 @@
+#
+# ganglia_aggregator_config - generates the aggregator config
+#
+def calc_url(aggregator, ip_octet)
+  url, port = aggregator.split(':')
+  port = port.to_i + ip_octet.to_i
+  return sprintf("%s:%d", url, port)
+end
+
+
+module Puppet::Parser::Functions
+  newfunction(:ganglia_aggregator_config, :type => :rvalue) do |args|
+    config = {}
+    site_config = {}
+    clusters = function_hiera(['ganglia_clusters'])
+    clusters.each do |cluster, data|
+      data['sites'].each do |site, aggregators|
+        name = sprintf("%s %s", data['name'], site)
+        unless site_config.include? site
+          site_config[site] = function_hiera(['ganglia_class'], nil, [site])
+        end
+        if site_config[site] == 'old'
+          aggregator = aggregators.join(' ')
+        else
+          # Compute the port to use
+          aggregator = calc_url(aggregators, data['ip_oct'])
+        end
+        config[name] = aggregator
+      end
+    end
+    return config
+  end
+end
