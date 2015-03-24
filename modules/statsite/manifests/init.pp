291a45dd45bb89774e85290f3f322cc08380cd39
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-03-24 16:11:59 +0000

    statsite: new module
    statsite provides a txstatsd replacement, the module allows for multiple
    instances to be provisioned with statsite::instance and controlled using the
    '*ctl pattern'.
    
    Bug: T90111
    Change-Id: I265f5307f73f69c242832d148e14595bfccfdba9

---
+++ modules/statsite/manifests/init.pp
@@ -0,0 +1,55 @@
+# == Class: statsite
+#
+# Configure statsite https://github.com/armon/statsite
+# To add individual instances, use statsite::instance
+#
+# === Parameters
+#
+# [*port*]
+#   Port to listen for messages on over UDP.
+#
+# [*graphite_host*]
+#   Send metrics to graphite on this host
+#
+# [*graphite_port*]
+#   Send metrics to graphite on this port
+#
+# [*input_counter*]
+#   Use this metric to report self-statistics
+#
+# [*extended_counters*]
+#   Export additional metrics for counters
+
+class statsite {
+    package { 'statsite':
+        ensure => present,
+    }
+
+    file { '/sbin/statsitectl':
+        source => 'puppet:///modules/statsite/statsitectl',
+        mode   => '0755',
+    }
+
+    file { '/etc/init/statsite':
+        source  => 'puppet:///modules/statsite/init',
+        recurse => true,
+        purge   => true,
+        force   => true,
+    }
+
+    # prevent the system-wide statsite from starting
+    file { '/etc/init/statsite.override':
+        content => 'manual',
+        before  => Package['statsite'],
+    }
+
+    service { 'statsite':
+        ensure   => 'running',
+        provider => 'base',
+        restart  => '/sbin/statsitectl restart',
+        start    => '/sbin/statsitectl start',
+        status   => '/sbin/statsitectl status',
+        stop     => '/sbin/statsitectl stop',
+        require  => Package['statsite'],
+    }
+}
