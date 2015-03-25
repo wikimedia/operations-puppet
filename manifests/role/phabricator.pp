7112be58bc80345a21f774f9039769f3eafba3ec
cpettet <rush@wikimedia.org>
2015-03-25 11:56:34 -0500

    Revert "exim4.conf.SMTP_IMAP_MM.erb local mail can cause loops"
    This reverts commit c104dd8f3da7519109cf9f55db650424936e7ffc.
    
    Change-Id: Icdbeed96dccdac87a9e965fa766672baa89abc32

--- manifests/role/phabricator.pp
+++ manifests/role/phabricator.pp
@@ -25,10 +25,6 @@ class role::phabricator::main {
         description => 'Phabricator (Main)'
     }
 
-    mailalias { 'root':
-        recipient => 'root@wikimedia.org',
-    }
-
     include passwords::phabricator
     include role::phabricator::config
     include phabricator::monitoring
