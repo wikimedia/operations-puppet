0369be5843c7e434e34bb22357c4214c71046165
dzahn <dzahn@wikimedia.org>
2015-03-26 10:11:50 -0700

    move jobqueue monitoring out of ganglia.pp
    This class doesn't even belong into ganglia as a misc::monitoring class
    and as part of the effort to get rid of (old) ganglia.pp move this out
    of here. And yes,  misc/monitoring.pp should also be removed or moved
    at some point.
    
    Bug:T93776
    Change-Id: Ifd7762d7f8a109849efe83964ca8cb10f3653114

--- manifests/ganglia.pp
+++ manifests/ganglia.pp
@@ -221,22 +221,3 @@ define ganglia::plugin::python( $plugin = $title, $opts = {} ) {
     }
 }
 
-# Copied from nagios::ganglia::monitor::enwiki
-# Will run on terbium to use the local MediaWiki install so that we can use
-# maintenance scripts recycling DB connections and taking a few secs, not mins
-class misc::monitoring::jobqueue {
-
-    cron { 'all_jobqueue_length':
-        ensure  => present,
-        command => "/usr/bin/gmetric --name='Global JobQueue length' --type=int32 --conf=/etc/ganglia/gmond.conf --value=$(/usr/local/bin/mwscript extensions/WikimediaMaintenance/getJobQueueLengths.php --totalonly | grep -oE '[0-9]+') > /dev/null 2>&1",
-        user    => 'mwdeploy',
-    }
-
-    # duplicating the above job to experiment with gmetric's host spoofing so
-    # as to gather these metrics in a fake host called "www.wikimedia.org"
-    cron { 'all_jobqueue_length_spoofed':
-        ensure  => present,
-        command => "/usr/bin/gmetric --name='Global JobQueue length' --type=int32 --conf=/etc/ganglia/gmond.conf --spoof 'www.wikimedia.org:www.wikimedia.org' --value=$(/usr/local/bin/mwscript extensions/WikimediaMaintenance/getJobQueueLengths.php --totalonly | grep -oE '[0-9]+') > /dev/null 2>&1",
-        user    => 'mwdeploy',
-    }
-}
