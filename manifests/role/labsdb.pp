f9e737e2c873f8b9957fddf2873c99ec14a78072
Coren <marc@uberbox.org>
2015-03-27 11:55:41 -0400

    Labs: puppetize labstore1005's mysql setup
    This mostly replicates the current config, but brings it in
    to the standard for passwords, etc.
    
    Bug: T88234
    Change-Id: I8ce32a37aa517baa9d21ef9bb9d03f50ea9917ef

--- manifests/role/labsdb.pp
+++ manifests/role/labsdb.pp
@@ -2,60 +2,21 @@
 
 class role::labs::db::master {
 
-    system::role { 'role::labs::db::master': description => "Labs user database master" }
-
-    class { 'mysql::server':
-        config_hash => {
-            'datadir'                 => '/srv/labsdb',
-            'port'                    => '3306',
-            'bind_address'            => '0.0.0.0',
-            'max_connections'         => '4096',
-
-            'server-id'               => '1',
-            'report_host'             => 'labsdbmaster',
-            'log_bin'                 => true,
-            'max_binlog_size'         => '100M',
-
-            'innodb_log_file_size'    => '64M',
-            'innodb_buffer_pool_size' => '13G',
-            'innodb_log_buffer_size'  => '64M',
-            'innodb_file_per_table'   => '1',
-            'innodb_open_files'       => '512',
-            'innodb_flush_method'     => 'O_DIRECT',
-
-            # More tuning will certainly be needed
-        }
+    system::role { 'role::labs::db::master':
+        description => 'Labs user database master',
     }
-}
 
+    include standard
+    include mariadb::packages_wmf
+    include role::mariadb::grants
+    include role::mariadb::monitor
 
-class role::labs::db::slave {
-
-    system::role { 'role::labs::db::slave': description => "Labs user database slave" }
-
-    class { 'mysql::server':
-        config_hash => {
-            'datadir'                 => '/srv/labsdb',
-            'port'                    => '3306',
-            'bind_address'            => '0.0.0.0',
-            'max_connections'         => '4096',
-
-            'server-id'               => '2',
-            'report_host'             => 'labsdbslave',
-            'log_bin'                 => true,
-            'max_binlog_size'         => '100M',
-            'relay_log'               => '/srv/labsdb/relay',
-            'relay_log_index'         => '/srv/labsdb/relay.index',
-            'relay_log_info_file'     => '/srv/labsdb/relay.info',
-
-            'innodb_log_file_size'    => '64M',
-            'innodb_buffer_pool_size' => '13G',
-            'innodb_log_buffer_size'  => '64M',
-            'innodb_file_per_table'   => '1',
-            'innodb_open_files'       => '512',
-            'innodb_flush_method'     => 'O_DIRECT',
-
-            # More tuning will certainly be needed
-        }
+    class { 'mariadb::config':
+        prompt   => 'TOOLSDB',
+        config   => 'mariadb/toolsmaster.my.cnf.erb',
+        password => $passwords::misc::scripts::mysql_root_pass,
+        datadir  => '/srv/labsdb/data',
+        tmpdir   => '/tmp',
     }
+
 }
