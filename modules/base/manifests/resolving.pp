3bf512e93aeb1a2f1d201ae5c1c628dad826c157
andrewbogott <abogott@wikimedia.org>
2015-03-30 11:00:50 -0500

    Make labs resolv.conf play nice with resolvconf
    Bug: T93691
    Change-Id: I43cc48dd6e4e67a2bfeae1a9ff71aeaf806332bc

--- modules/base/manifests/resolving.pp
+++ modules/base/manifests/resolving.pp
@@ -5,13 +5,39 @@ class base::resolving (
         error("Variable ${::nameservers} is not defined!")
     }
     else {
-        file { '/etc/resolv.conf':
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            content => $::realm? {
-                'labs'  => template('base/resolv.conf.labs.erb'),
-                default => template('base/resolv.conf.erb'),
+        if $::realm == 'labs' {
+            file { '/etc/resolv.conf':
+                ensure => 'link',
+                target => '/run/resolvconf/resolv.conf'
+            }
+
+            file { '/etc/resolvconf/resolv.conf.d/tail':
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0444',
+                content => template('base/resolv.conf.tail.labs.erb'),
+            }
+
+            file { '/etc/network/interfaces':
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0444',
+                content => template('base/interfaces.labs.erb'),
+            }
+
+            exec { 'resolvconf':
+                subscribe   => File['/etc/network/interfaces',
+                                    '/etc/resolvconf/resolv.conf.d/tail'],
+                creates     => '/run/resolvconf/resolv.conf',
+                refreshonly => true,
+                command     => '/sbin/resolvconf -u',
+            }
+        } else {
+            file { '/etc/resolv.conf':
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0444',
+                content => template('base/resolv.conf.erb'),
             }
         }
     }
