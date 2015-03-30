830e25e324e226a394fb2a18a15af55b7517a44b
andrewbogott <abogott@wikimedia.org>
2015-03-30 11:00:50 -0500

    Make labs resolv.conf play nice with resolvconf updates
    Change-Id: I43cc48dd6e4e67a2bfeae1a9ff71aeaf806332bc

--- modules/base/manifests/resolving.pp
+++ modules/base/manifests/resolving.pp
@@ -5,13 +5,26 @@ class base::resolving (
         error("Variable ${::nameservers} is not defined!")
     }
     else {
-        file { '/etc/resolv.conf':
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            content => $::realm? {
-                'labs'  => template('base/resolv.conf.labs.erb'),
-                default => template('base/resolv.conf.erb'),
+        if $$:realm == 'labs' {
+            file { '/etc/resolv.conf':
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0444',
+                content => template('base/resolv.conf.erb'),
+            }
+        } else {
+            file { '/etc/resolv.conf':
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0444',
+                content => template('base/resolv.conf.labs.erb'),
+            }
+
+            file { '/etc/resolvconf/resolv.conf.d/tail':
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0444',
+                content => template('base/resolv.conf.tail.labs.erb'),
             }
         }
     }
