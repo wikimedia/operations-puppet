0c87603b0fa0e00cf4b0e216202fc9c6440863f5
Brandon Black <bblack@wikimedia.org>
2015-03-30 14:39:33 +0000

    add_ip6_mapped: no-op refactor to make +token commit simpler
    Change-Id: I82b1fd15c7bc5d33b580d5897f9efdf9b65adccf

--- modules/interface/manifests/add_ip6_mapped.pp
+++ modules/interface/manifests/add_ip6_mapped.pp
@@ -18,8 +18,10 @@ define interface::add_ip6_mapped($interface=undef, $ipv4_address=undef) {
             $ip4_address = "::${ipv4_address}"
         }
 
-        $ipv6_address = inline_template("<%= require 'ipaddr'; (IPAddr.new(scope.lookupvar(\"::ipaddress6_${intf}\")).mask(64) | IPAddr.new(ip4_address.gsub('.', ':'))).to_s() %>")
+        # $v6_mapped_lower64 looks like '::10:0:2:1' for a v4 of '10.0.2.1'
+        $v6_mapped_lower64 = regsubst($ip4_address, '\.', ':', 'G')
 
+        $ipv6_address = inline_template("<%= require 'ipaddr'; (IPAddr.new(scope.lookupvar(\"::ipaddress6_${intf}\")).mask(64) | IPAddr.new(v6_mapped_lower64)).to_s() %>")
         interface::ip { $title:
             interface => $intf,
             address   => $ipv6_address,
