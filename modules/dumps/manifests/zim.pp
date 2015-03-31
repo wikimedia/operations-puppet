83e387b1bc9966564af5c8538a2905f8a950862a
dzahn <dzahn@wikimedia.org>
2015-03-30 17:41:56 -0700

    ZIM dumps: install nodejs and libsqlite
    install nodejs packages, incl. npm
    "npm start to start services and npm test to run tests"
    (not to install more software)
    
    install libsqlite packages
    
    as listed on:
    
    Bug:T94457
    Change-Id: Iea35ab941d1a29624f4d91a019ae2313300ecb00

--- modules/dumps/manifests/zim.pp
+++ modules/dumps/manifests/zim.pp
@@ -5,4 +5,12 @@ class dumps::zim {
         ensure => present,
     }
 
+    package { [ 'nodejs', 'nodejs-legacy', 'nodejs-dev', 'npm' ]:
+        ensure => present,
+    }
+
+    package { [ 'libsqlite3', 'libsqlite3-dev' ]:
+        ensure => present,
+    }
+
 }
