4910a7552a3b0e214b215059ff98b959bd55ea50
Marius Hoch <hoo@online.de>
2015-04-01 17:57:03 +0200

    Add new wikidata folders, define dataset folders in puppet
    Initial step for moving the Wikibase dumps to the new location
    per T72385.
    
    I hope I got the permissions/ owners/ groups right for the
    directories that already exist.
    
    Bug: T72385
    Change-Id: Idc6efba9143734511dc414c34b655f0cd6a83073

--- modules/dataset/manifests/dirs.pp
+++ modules/dataset/manifests/dirs.pp
@@ -1,5 +1,59 @@
 class dataset::dirs {
+    # Please note that this is incomplete, but new directories
+    # should be defined in puppet (here).
     $datadir = '/data/xmldatadumps'
     $publicdir = '/data/xmldatadumps/public'
-    $otherdir =  '/data/xmldatadumps/public/other'
+    $otherdir = '/data/xmldatadumps/public/other'
+    $otherdir_wikidata_legacy = '/data/xmldatadumps/public/other/wikidata'
+    $otherdir_wikibase = '/data/xmldatadumps/public/other/wikibase/'
+    $otherdir_wikibase_wikidatawiki = '/data/xmldatadumps/public/other/wikibase/wikidatawiki'
+
+    file { $datadir:
+        mode    => '0755',
+        ensure  => 'directory',
+        owner   => 'root',
+        group   => 'root',
+    }
+
+    file { $publicdir:
+        mode    => '0775',
+        ensure  => 'directory',
+        owner   => 'datasets',
+        group   => 'datasets',
+    }
+
+    file { $otherdir:
+        mode    => '0755',
+        ensure  => 'directory',
+        owner   => 'datasets',
+        group   => 'datasets',
+    }
+
+    file { $otherdir_wikibase:
+        mode    => '0755',
+        ensure  => 'directory',
+        owner   => 'datasets',
+        group   => 'datasets',
+    }
+
+    file { $otherdir_wikibase_wikidatawiki:
+        mode    => '0755',
+        ensure  => 'directory',
+        owner   => 'datasets',
+        group   => 'datasets',
+    }
+
+    # T72385
+    file { "$publicdir/wikidatawiki/entities":
+        ensure  => 'link',
+        target  => $otherdir_wikibase_wikidatawiki
+    }
+
+    # Legacy
+    file { $otherdir_wikidata_legacy:
+        mode    => '0755',
+        ensure  => 'directory',
+        owner   => 'datasets',
+        group   => 'datasets',
+    }
 }
