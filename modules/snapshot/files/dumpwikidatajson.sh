4ebc3cfd1633c4376049e90f6c52fa552c5f6235
Marius Hoch <hoo@online.de>
2015-04-01 19:51:04 +0200

    Adopt dumpwikidatajson.sh to the new naming pattern
    Also some code style changes.
    
    Bug: T72385
    Change-Id: I5df824839e09c5237e22e1b8f4bea54aa6b9a255

--- modules/snapshot/files/dumpwikidatajson.sh
+++ modules/snapshot/files/dumpwikidatajson.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 #
 # Generate a json dump for Wikidata and remove old ones.
+# To be run weekly.
 #
 # @author Marius Hoch < hoo@online.de >
 
-
 configfile="/srv/dumps/confs/wikidump.conf"
 
-apacheDir=`egrep "^dir=" "$configfile" | mawk -Fdir= '{ print $2 }'`
-targetDir=`egrep "^public=" "$configfile" | mawk -Fpublic= '{ print $2 }'`/other/wikidata
-tempDir=`egrep "^temp=" "$configfile" | mawk -Ftemp= '{ print $2 }'`
+apacheDir=`awk -Fdir= '/^dir=/ { print $2 }' "$configfile"`
+targetDirBase=`awk -Fpublic= '/^public=/ { print $2 }' "$configfile"`/other/wikibase/wikidatawiki
+targetDir=$targetDirBase/`date +'%Y%m%d'`
+legacyDirectory=`awk -Fpublic= '/^public=/ { print $2 }' "$configfile"`/other/wikidata
+tempDir=`awk -Ftemp= '/^temp=/ { print $2 }' "$configfile"`
+
+if [ -z "$targetDirBase" ]; then
+	echo "Empty targetDirBase"
+	exit 1
+fi
+
+# Create the dir for the day: This may or may not already exist, we don't care
+mkdir -p $targetDir
 
 multiversionscript="${apacheDir}/multiversion/MWScript.php"
 
-filename=`date +'%Y%m%d'`
+filename=wikidata-`date +'%Y%m%d'`-all
 targetFile=$targetDir/$filename.json.gz
 
 i=0
@@ -26,11 +36,10 @@ done
 
 wait
 
-i=0
-
 # Open the json list
 echo '[' | gzip -f > $targetFile
 
+i=0
 while [ $i -lt $shards ]; do
 	cat $tempDir/wikidataJson.$i.gz >> $targetFile
 	rm $tempDir/wikidataJson.$i.gz
@@ -44,8 +53,15 @@ done
 # Close the json list
 echo -e '\n]' | gzip -f >> $targetFile
 
-# Remove dumps we no longer need (keep 10 => last 70 days)
-find $targetDir -name '20*.gz' -mtime +71 -delete
+# Remove dump-folders we no longer need (keep 70 days)
+# Note: We use mtime here, thus if something in the folder has been altered after
+# the date suggested by the name of the folder the folder will stick around longer
+# than 70 days.
+find $targetDirBase -maxdepth 1 -type d -mtime +71 -delete # $targetDirBase is guaranteed to be non empty (see above)
+
+# Legacy directory (with legacy naming scheme)
+ln -s $targetFile "$legacyDirectory/`date +'%Y%m%d'`.json"
+find $legacyDirectory -name '*.json.gz' -mtime +71 -delete
 
 # Remove old logs (keep 5 => last 35 days)
 find /var/log/wikidatadump/ -name 'dumpwikidatajson-*-*.log' -mtime +36 -delete
