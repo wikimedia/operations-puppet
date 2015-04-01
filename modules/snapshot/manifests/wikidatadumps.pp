06e108fd83b0ca1df81c21fd6db8f7d012c59e20
Marius Hoch <hoo@online.de>
2015-04-02 01:13:18 +0200

    Add a script to create Wikidata ttl dumps
    Not yet enabled as the required code is only going
    to be deployed on 2015-04-07.
    
    Bug: T93658
    Change-Id: Ia9c648ba6cc6482b09ccf38505437c4ac61bf15b

--- modules/snapshot/manifests/wikidatadumps.pp
+++ modules/snapshot/manifests/wikidatadumps.pp
@@ -40,7 +40,7 @@ class snapshot::wikidatadumps::json(
         $ensure = 'absent'
     }
 
-    system::role { 'snapshot::wikidatajsondump':
+    system::role { 'snapshot::wikidatadumps::json':
         ensure => $ensure,
         description => 'producer of weekly wikidata json dumps'
     }
@@ -59,5 +59,48 @@ class snapshot::wikidatadumps::json(
         minute      => '15',
         hour        => '3',
         weekday     => '1',
+    }
+
+    # Purge system::role with former class name
+    system::role { 'snapshot::wikidatajsondump':
+        ensure => 'absent'
+    }
+}
+
+class snapshot::wikidatadumps::ttl(
+    $enable = true,
+    $user   = undef,
+) {
+    class { 'snapshot::wikidatadumps::common':
+        enable => $enable,
+        user   => $user
+    }
+
+    if ($enable == true) {
+        $ensure = 'present'
+    }
+    else {
+        $ensure = 'absent'
+    }
+
+    system::role { 'snapshot::wikidatadumps::ttl':
+        ensure => $ensure,
+        description => 'producer of weekly wikidata ttl dumps'
+    }
+
+    file { '/usr/local/bin/dumpwikidatattl.sh':
+        mode    => '0755',
+        owner   => 'root',
+        group   => 'root',
+        source  => 'puppet:///modules/snapshot/dumpwikidatattl.sh',
+    }
+
+    cron { 'wikidatattl-dump':
+        ensure      => $ensure,
+        command     => "/usr/local/bin/dumpwikidatattl.sh",
+        user        => $user,
+        minute      => '15',
+        hour        => '3',
+        weekday     => '3',
     }
 }
