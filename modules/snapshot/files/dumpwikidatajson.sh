a0a409cd03374f577e0f5de932abaef6227ae072
Marius Hoch <hoo@online.de>
2015-04-01 19:51:04 +0200

    Adopt dumpwikidatajson.sh to the new naming pattern
    Also some code style changes.
    
    Bug: T72385
    Change-Id: I5df824839e09c5237e22e1b8f4bea54aa6b9a255

--- modules/snapshot/files/dumpwikidatajson.sh
+++ modules/snapshot/files/dumpwikidatajson.sh
@@ -4,16 +4,25 @@
 #
 # @author Marius Hoch < hoo@online.de >
 
-
 configfile="/srv/dumps/confs/wikidump.conf"
 
-apacheDir=`egrep "^dir=" "$configfile" | mawk -Fdir= '{ print $2 }'`
-targetDir=`egrep "^public=" "$configfile" | mawk -Fpublic= '{ print $2 }'`/other/wikidata
-tempDir=`egrep "^temp=" "$configfile" | mawk -Ftemp= '{ print $2 }'`
+apacheDir=`awk -Fdir= '/^dir=/ { print $2 }' "$configfile"`
+targetDirBase=`awk -Fpublic= '/^public=/ { print $2 }' "$configfile"`/other/wikibase/wikidatawiki
+targetDir=$targetDirBase/`date +'%Y%m%d'`
+legacyDirectory=`awk -Fpublic= '/^public=/ { print $2 }' "$configfile"`/other/wikidata
+tempDir=`awk -Ftemp= '/^temp=/ { print $2 }' "$configfile"`
+
+if [ -z "$targetDirBase" ]; then
+	echo "Empty targetDirBase";
+	exit 1
+fi
+
+# Create the dir for the day: This may or may not already exist, we don't care
+mkdir $targetDir 2> /dev/null
 
 multiversionscript="${apacheDir}/multiversion/MWScript.php"
 
-filename=`date +'%Y%m%d'`
+filename=wikidata-`date +'%Y%m%d'`-all
 targetFile=$targetDir/$filename.json.gz
 
 i=0
@@ -44,8 +53,15 @@ done
 # Close the json list
 echo -e '\n]' | gzip -f >> $targetFile
 
-# Remove dumps we no longer need (keep 10 => last 70 days)
-find $targetDir -name '20*.gz' -mtime +71 -delete
+# Remove dump-folders we no longer need (keep 10 => last 70 days)
+foldersToDelete=`ls -d -r $targetDirBase/* | tail -n +11` # $targetDirBase is known to be non-empty (per the check above)
+if [ -n "$foldersToDelete" ]; then
+	rm -rf $foldersToDelete
+fi
+
+# Legacy directory (with legacy naming scheme)
+ln -s $targetFile "$legacyDirectory/`date +'%Y%m%d'`.json"
+find $legacyDirectory -name '*.json.gz' -mtime +70 -delete
 
 # Remove old logs (keep 5 => last 35 days)
 find /var/log/wikidatadump/ -name 'dumpwikidatajson-*-*.log' -mtime +36 -delete
