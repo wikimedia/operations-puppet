5125e64514c94dbf6a9ab8ca5b9159b082de7777
Marius Hoch <hoo@online.de>
2015-04-02 01:13:18 +0200

    Add a script to create Wikidata ttl dumps
    Not yet enabled as the required code is only going
    to be deployed on 2015-04-07.
    
    Bug: T93658
    Change-Id: Ia9c648ba6cc6482b09ccf38505437c4ac61bf15b

---
+++ modules/snapshot/files/dumpwikidatattl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Generate a json dump for Wikidata and remove old ones.
+#
+# Marius Hoch < hoo@online.de >
+
+. /usr/local/bin/wikidatadumps-shared.sh
+
+filename=wikidata-`date +'%Y%m%d'`-all-BETA
+targetFile=$targetDir/$filename.ttl.gz
+
+i=0
+shards=4
+
+while [ $i -lt $shards ]; do
+	php $multiversionscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dumpRdf.php --wiki wikidatawiki --shard $i --sharding-factor $shards --format ttl 2>> /var/log/wikidatadump/dumpwikidatattl-$filename-$i.log | gzip > $tempDir/wikidataTTL.$i.gz &
+	let i++
+done
+
+wait
+
+i=0
+while [ $i -lt $shards ]; do
+	cat $tempDir/wikidataTTL.$i.gz >> $targetFile
+	rm $tempDir/wikidataTTL.$i.gz
+	let i++
+done
+
+pruneOldDirectories
+pruneOldLogs
