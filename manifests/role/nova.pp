1b15325dbbd0964e48d9a94d215edb8ad7307327
Coren <marc@uberbox.org>
2015-04-01 11:20:37 -0400

    Labs: monitor and alert labnet1001 for conntrack
    Bug: T90437
    Change-Id: I0b3ff3199ca4637a6161fbdc95bb788188561644

--- manifests/role/nova.pp
+++ manifests/role/nova.pp
@@ -356,6 +356,19 @@ class role::nova::network {
     }
 
     diamond::collector::conntrack { 'conntrack_collector': }
+
+    # The current maximum is 262144; the threshold should be set
+    # to a faction of *.conntrack_max rather than a literal here
+    # but graphite_threshold doesn't support that.
+    monitoring::graphite_threshold { 'conntrack_saturated':
+        description => 'Connection tracking saturation'
+        metric      => "servers.${::hostname}.ConntrackCollector.network.netfilter.conntrack_count.value",
+        from        => '5min',
+        warning     => '241664', # (~90%)
+        critical    => '258048', # (~98%)
+        percentage  => '1',
+    }
+
 }
 
 class role::nova::wikiupdates {
