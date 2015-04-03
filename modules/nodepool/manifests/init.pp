ce010809b8fb6ae122600fb328313aea6b148505
Antoine Musso <hashar@free.fr>
2015-04-03 18:39:02 +0200

    (wip) nodepool yaml conf file (wip)
    Bug: T89143
    Change-Id: Icb2f9b9fe7dbef60f293870e223edfa849cf0951

---
+++ modules/nodepool/manifests/init.pp
@@ -0,0 +1,16 @@
+class nodepool( $nova_controller_hostname ) {
+
+    package { 'nodepool':
+        ensure => present,
+    }
+
+    # OpenStack CLI
+    package { 'python-openstackclient':
+        ensure => present,
+    }
+
+    file { '/etc/nodepool/nodepool.yaml':
+        content => template('nodepool/nodepool.yaml.erb')
+        require => Package['nodepool'],
+    }
+}
