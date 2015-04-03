58ae68addc8dea50bdef7d341f68ae7fbf649676
Antoine Musso <hashar@free.fr>
2015-04-03 18:39:02 +0200

    nodepool: preliminary role and config file
    Bug: T89143
    Change-Id: Icb2f9b9fe7dbef60f293870e223edfa849cf0951

---
+++ manifests/role/nodepool.pp
@@ -0,0 +1,20 @@
+# Migrate under role::ci ??
+class role::nodepool {
+
+    system::role { 'role::nodepool': description => 'CI Nodepool' }
+
+    include role::nova::config
+    include passwords::nodepool
+
+    class { '::nodepool':
+        jenkins_api_user               => 'nodepoolmanager',
+        jenkins_api_key                => $passwords::nodepool::jenkins_api_key,
+        jenkins_credentials_id         => 'nodepool-dib-jenkins',
+        jenkins_ssh_private_key_source => 'puppet:///private/nodepool/dib_jenkins_id_rsa',
+        nova_controller_hostname       => $role::nova::config::novaconfig['controller_hostname'],
+        openstack_username             => 'nodepoolmanager',
+        openstack_password             => $passwords::nodepool::manager_pass,
+        openstack_tenant_id            => 'contintcloud',
+    }
+
+}
