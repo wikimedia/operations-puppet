4f834ec21ff480b71378ba0eeba481ab6682d076
Tim Landscheidt <tim@tim-landscheidt.de>
2015-04-03 22:50:54 +0000

    Tools: Update toollabs::toolwatcher documentation
    With change I650aaa726068da096630f4378faac7b6244e3992 toolwatcher no
    longer creates cgi-bin directories for new tools.
    
    Change-Id: I1875e8908c478dab30b977e0dcb73510cf1b498a

--- modules/toollabs/manifests/toolwatcher.pp
+++ modules/toollabs/manifests/toolwatcher.pp
@@ -1,9 +1,8 @@
 # toollabs::toolwatcher sets up a host with a service that scans every
 # two minutes for tool accounts whose home directory doesn't exist
 # yet.  For each such tool account, the toolwatcher creates the home
-# directory with the subdirectories cgi-bin and public_html owned by
-# the tool account and its group and sets the permissions to
-# g+srwx,o+rx.
+# directory with the subdirectory public_html owned by the tool
+# account and its group and sets the permissions to g+srwx,o+rx.
 class toollabs::toolwatcher
 inherits toollabs {
     file { '/etc/init/toolwatcher.conf':
