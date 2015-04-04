98165984171d45dc80c12280fa2aac792a16a437
dzahn <dzahn@wikimedia.org>
2015-04-03 17:06:55 -0700

    openstack firewall: avoid hardcoding tendril IP
    Instead of hardcoding tendril's IP use @resolve (because now
    we can since we have libnet-dns-perl everywhere) to let
    ferm resolve the IP from hostname.
    
    Change-Id: I56879378a07a2696fb5573b15043d16788454ad3

--- modules/openstack/manifests/firewall.pp
+++ modules/openstack/manifests/firewall.pp
@@ -18,7 +18,6 @@ class openstack::firewall {
     }
 
     $iron = '208.80.154.151'
-    $tendril = '10.64.0.15'
 
     # Wikitech ssh
     ferm::rule { 'ssh_public':
@@ -81,7 +80,9 @@ class openstack::firewall {
     }
 
     # mysql monitoring access from tendril (db1011)
-    ferm::rule { 'mysql_tendril':
-        rule => "saddr ${tendril} proto tcp dport (3306) ACCEPT;",
+    ferm::service { 'mysql_tendril':
+        proto  => 'tcp',
+        port   => '3306',
+        srange => "@resolve(tendril.wikimedia.org)",
     }
 }
