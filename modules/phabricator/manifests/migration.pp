691f213af309b22713a724af056c0b64a1e5f41f
Faidon Liambotis <faidon@wikimedia.org>
2015-04-05 10:01:05 +0300

    Silence Phabricator/Bugzilla migration cronspam
    Results in at least a one-by-minute cronspam with an output of:
    
    /srv/phab/tools/bugzilla_update_user_comments.py completed 0, failed 0
    
    Change-Id: Iccb1dfeeb788009c65ad69e8bef29d96013d1db1

--- modules/phabricator/manifests/migration.pp
+++ modules/phabricator/manifests/migration.pp
@@ -31,7 +31,7 @@ class phabricator::migration (
     $fab_lock = '/var/run/fab_update_user.flock'
     cron { 'fab_user_update':
         ensure  => absent,
-        command => "/usr/bin/flock -n ${fab_lock} -c '/srv/phab/tools/fab_update_user.py -a' 2>&1",
+        command => "/usr/bin/flock -n ${fab_lock} -c '/srv/phab/tools/fab_update_user.py -a' >/dev/null 2>&1",
         user    => root,
         hour    => '*/1',
         minute  => 0,
@@ -40,7 +40,7 @@ class phabricator::migration (
     $bz_header = '/var/run/bz_header.flock'
     cron { 'bz_header_update':
         ensure  => present,
-        command => "/usr/bin/flock -n ${bz_header} -c '/srv/phab/tools/bugzilla_update_user_header.py -a' 2>&1",
+        command => "/usr/bin/flock -n ${bz_header} -c '/srv/phab/tools/bugzilla_update_user_header.py -a' >/dev/null 2>&1",
         user    => root,
         hour    => '0',
     }
@@ -48,7 +48,7 @@ class phabricator::migration (
     $bz_comments = '/var/run/bz_comments.flock'
     cron { 'bz_comment_update':
         ensure  => present,
-        command => "/usr/bin/flock -n ${bz_comments} -c '/srv/phab/tools/bugzilla_update_user_comments.py -a' 2>&1",
+        command => "/usr/bin/flock -n ${bz_comments} -c '/srv/phab/tools/bugzilla_update_user_comments.py -a' >/dev/null 2>&1",
         user    => root,
         hour    => '1',
     }
