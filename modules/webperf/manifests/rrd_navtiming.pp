069e91e91284130320a1f1a414f91cbf717e3659
Ori Livneh <ori@wikimedia.org>
2015-04-07 02:47:00 -0700

    Add a script for storing NavTiming metrics using RRD
    The front-end metric dashboards at
    https://gdash.wikimedia.org/dashboards/frontend/ are riddled with spikes and
    gaps, which is embarassing and unprofessional. The high-level reason for their
    existence is the fact that the Navigation Timing logging data pipeline is made
    up of nearly a dozen components, each with its own failure mode.
    
    I like <https://status.github.com/>, and I wanted to create something similar:
    a rock-solid web interface providing a small set of views over a small set of
    key performance metrics. This patch is the first step towards that.
    
    This script provides simple, minimal and robust storage for a small set of
    time-series metrics. The idea is not to replace a full-fledged
    time-series database but to implement the bare minimum subset of
    features required to power a site like <https://status.github.com/>, and to
    have as few frills and external dependencies as possible.
    
    rrd-navtiming subscribes to NavigationTiming events via EventLogging and it
    updates a pair of RRD files in its working directory: mobile.rrd
    and desktop.rrd. If the files do not exist, they are created.
    
    Change-Id: Id6e662a77d024363882bd54bd45c4aa5445d5503

---
+++ modules/webperf/manifests/rrd_navtiming.pp
@@ -0,0 +1,41 @@
+# == Class: webperf::rrd_navtiming
+#
+# Store a basic set of Navigation Timing metrics in RRD files.
+# See https://meta.wikimedia.org/wiki/Schema:NavigationTiming &
+# http://www.mediawiki.org/wiki/Extension:NavigationTiming
+#
+# === Parameters
+#
+# [*endpoint*]
+#   URI of EventLogging event publisher to subscribe to.
+#   For example, 'tcp://eventlogging.eqiad.wmnet:8600'.
+#
+class webperf::rrd_navtiming( $endpoint ) {
+    include ::webperf
+
+    file { '/srv/webperf/rrd-navtiming.py':
+        source => 'puppet:///modules/webperf/rrd-navtiming.py',
+        owner  => 'webperf',
+        group  => 'webperf',
+        mode   => '0755',
+        notify => Service['rrd-navtiming'],
+    }
+
+    file { '/var/lib/rrd-navtiming':
+        ensure => directory,
+        owner  => 'webperf',
+        group  => 'webperf',
+        mode   => '0755',
+        before => Service['rrd-navtiming'],
+    }
+
+    file { '/etc/init/rrd-navtiming.conf':
+        content => template('webperf/rrd-navtiming.conf.erb'),
+        notify  => Service['rrd-navtiming'],
+    }
+
+    service { 'rrd-navtiming':
+        ensure   => running,
+        provider => upstart,
+    }
+}
