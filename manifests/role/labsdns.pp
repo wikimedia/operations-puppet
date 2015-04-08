1786b488e7d2502df6f797d03f22601451c961f5
dzahn <dzahn@wikimedia.org>
2015-04-07 18:30:04 -0700

    labsdns: lint fixes
    WARNING: indentation of => is not properly aligned
    puppet-lint 1.1.0
    
    Bug:T93645
    Change-Id: Ife77df84d823ee1e7db73b91c4ce7f0dd6f03160

--- manifests/role/labsdns.pp
+++ manifests/role/labsdns.pp
@@ -2,13 +2,13 @@ class role::labsdns {
     include passwords::pdns
 
     class { '::labs_dns':
-        dns_auth_ipaddress      => '208.80.154.12',
-        dns_auth_query_address  => '208.80.154.12',
-        dns_auth_soa_name       => 'labs-ns2.wikimedia.org',
-        pdns_db_host            => 'm1-master.eqiad.wmnet',
-        pdns_db_password        => $passwords::pdns::db_pass,
-        pdns_recursor           => '208.80.154.239',
-        recursor_ip_range       => '10.68.16.0/21',
+        dns_auth_ipaddress     => '208.80.154.12',
+        dns_auth_query_address => '208.80.154.12',
+        dns_auth_soa_name      => 'labs-ns2.wikimedia.org',
+        pdns_db_host           => 'm1-master.eqiad.wmnet',
+        pdns_db_password       => $passwords::pdns::db_pass,
+        pdns_recursor          => '208.80.154.239',
+        recursor_ip_range      => '10.68.16.0/21',
     }
 
     ferm::service { 'udp_dns_rec':
