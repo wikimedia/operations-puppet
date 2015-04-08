039979fcac860ceff72bfe472b493b75dfa88416
andrewbogott <abogott@wikimedia.org>
2015-04-08 16:28:13 -0500

    For cert names, use the fqdn instead of the ec2id.
    This is safe now because fqdn includes project name, hence unique.
    
    Bug T95480
    
    Change-Id: Ifb387bffcb23c8fa9dcfba3fb602b2c97d8cb263

--- modules/puppet/manifests/self/master.pp
+++ modules/puppet/manifests/self/master.pp
@@ -47,7 +47,7 @@ class puppet::self::master(
     # Else certname should be the labs instanceid. ($::ec2id comes from instance metadata.)
     $certname = $server ? {
         'localhost' => 'localhost',
-        default     => "${::ec2id}.${::domain}"
+        default     => $fqdn
     }
 
     # We'd best be sure that our ldap config is set up properly
