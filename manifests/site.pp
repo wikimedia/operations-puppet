66e1348bab8f4e8c3c4a646772308cc8dd351f45
Brandon Black <bblack@wikimedia.org>
2015-04-09 15:42:52 +0000

    remove pointless (I think!) esams $ganglia_aggregator and cache_upload def for esams
    Change-Id: Ifed83da3b74cb7b5d78388a7bb65b09cd27b24d4

--- manifests/site.pp
+++ manifests/site.pp
@@ -418,12 +418,7 @@ node /^cp301[1234]\.esams\.wmnet$/ {
 }
 
 node /^cp30(0[3-9]|10)\.esams\.wmnet$/ {
-    if $::hostname =~ /^cp300[34]$/ {
-        $ganglia_aggregator = true
-    }
-    $cluster = 'cache_upload'
     interface::add_ip6_mapped { 'main': }
-
     role cache::upload
 }
 
@@ -433,9 +428,6 @@ node /^cp301[5678]\.esams\.wmnet$/ {
 }
 
 node /^cp30(19|2[0-2])\.esams\.wmnet$/ {
-    if $::hostname =~ /^cp(3019|3020)$/ {
-        $ganglia_aggregator = true
-    }
     interface::add_ip6_mapped { 'main': }
     role cache::bits
 }
