ad56d6b03f4d5d41332b634c68e8be982853e84c
YuviPanda <yuvipanda@gmail.com>
2015-04-08 19:03:05 -0700

    tools: Make services host a submit host
    Also put webservice file on such hosts
    
    Bug: T95210
    Change-Id: I827ce872a62dfe6d887acd90b2e91007ebc5dee0

--- modules/toollabs/manifests/services.pp
+++ modules/toollabs/manifests/services.pp
@@ -1,6 +1,9 @@
 # = Class: toollabs::services
 # Provides various services based off tools manifests
 class toollabs::services {
+
+    include gridengine::submit_host
+
     package { 'tools-manifest':
         ensure => latest,
     }
@@ -8,5 +11,14 @@ class toollabs::services {
     service { 'webservicemonitor':
         ensure    => running,
         subscribe => Package['tools-manifest'],
+    }
+
+    file { '/usr/local/bin/webservice':
+        ensure  => present,
+        source  => 'puppet:///modules/toollabs/webservice2',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        require => Package['python-yaml'], # Present on all hosts, defined for puppet diamond collector
     }
 }
