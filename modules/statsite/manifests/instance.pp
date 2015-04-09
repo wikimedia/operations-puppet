f951a83ef8a45762c087c78780dc762fce6c2e16
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-04-09 12:54:34 +0100

    statsite: fix hostname variable
    Change-Id: Ib6c82067691a596ef069c602788e3204ab3f8356

--- modules/statsite/manifests/instance.pp
+++ modules/statsite/manifests/instance.pp
@@ -23,7 +23,7 @@ define statsite::instance(
     $port              = 8125,
     $graphite_host     = 'graphite-in.eqiad.wmnet',
     $graphite_port     = 2003,
-    $input_counter     = "statsd.$::{hostname}.received",
+    $input_counter     = "statsd.${::hostname}.received",
     $extended_counters = 0,
 ) {
     $stream_cmd = "python /usr/lib/statsite/sinks/graphite.py $graphite_host $graphite_port"
