25181e64b3fd6b930506942120dd97b5c58f477d
Brandon Black <bblack@wikimedia.org>
2015-04-10 13:53:56 +0000

    T86663 5.8: repool cp3010
    Change-Id: Ifde228d4a3fdae69b1edcadc6f30e028ba5217a2

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -77,7 +77,7 @@ class role::cache {
                         'cp3007.esams.wmnet',
                         'cp3008.esams.wmnet',
                         'cp3009.esams.wmnet',
-                        # 'cp3010.esams.wmnet',
+                        'cp3010.esams.wmnet',
                         # T92306 'cp3011.esams.wmnet', # needs-jessie-install
                         'cp3012.esams.wmnet',
                         'cp3013.esams.wmnet',
