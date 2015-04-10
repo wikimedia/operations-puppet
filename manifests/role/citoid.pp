c98bc85db89becf7f04f238190460c532cde4b15
Marko Obrovac <mobrovac@wikimedia.org>
2015-04-10 15:49:44 +0200

    Add a generic node service module
    service::node is a generic Puppet resource that allows Node.js services to be
    easily set up on the SCA cluster. Since all such services are set up in
    the exact same way, most of the code can be shared between them. The
    only specific parameters are a service's port and own configuration
    directives. The other parameters of service are controlled by
    role::sca's Hiera.
    
    As an example, role::citoid has been converted to using service::node
    instead of its own citoid module.
    
    Bug: T95533
    Change-Id: Ic6c9d4f8d0b086b94d167fd9449377a87734456b

--- manifests/role/citoid.pp
+++ manifests/role/citoid.pp
@@ -1,16 +1,19 @@
 # vim: set ts=4 et sw=4:
 class role::citoid {
+
     system::role { 'role::citoid': }
-
-    include ::citoid
-
-    monitoring::service { 'citoid':
-        description   => 'citoid',
-        check_command => 'check_http_on_port!1970',
-    }
 
     ferm::service { 'citoid':
         proto => 'tcp',
         port  => '1970',
     }
+
+    $zotero_host = hiera('citoid::zotero_host')
+    $zotero_port = hiera('citoid::zotero_port')
+
+    service::node { 'citoid':
+        port   => 1970,
+        config => template('service/node/citoid/config.yaml.erb'),
+    }
+
 }
---
+++ modules/service/lib/puppet/parser/functions/merge_config.rb
@@ -0,0 +1,23 @@
+# == Function: merge_config(string|hash main_conf, string|hash service_conf)
+#
+# Merges the service-specific service_conf into main_conf. Both arguments
+# can be either hashes or YAML-formatted strings. It returns the merged
+# configuration hash.
+#
+
+def config_to_hash(conf)
+  return YAML.load(conf) unless conf.is_a?(Hash)
+  conf
+end
+
+module Puppet::Parser::Functions
+  newfunction(:merge_config, :type => :rvalue, :arity => 2) do |args|
+    main_conf, service_conf = *args.map { |arg|  config_to_hash(arg)}
+    begin
+      main_conf['services'][0]['conf'].update service_conf
+    rescue
+      fail('Badly formatted configuration.')
+    end
+    function_ordered_yaml([main_conf])
+  end
+end
