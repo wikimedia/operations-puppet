4e7d5a1da82bb16364607c03bb29386209316cd1
Brandon Black <bblack@wikimedia.org>
2015-04-11 17:05:06 +0000

    cache.pp cleanup: decompress ssl def usage
    Change-Id: Ibadd74b2853b8e6b4641f463cf5d3d05bb967123

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -660,20 +660,23 @@ class role::cache::ssl::misc {
     include certificates::wmf_ca_2014_2017
     include role::protoproxy::ssl::common
 
-    role::cache::ssl::local {
-        'wikimedia.org':
-            certname => 'sni.wikimedia.org',
-            server_name => 'wikimedia.org',
-            server_aliases => ['*.wikimedia.org'],
-            default_server => true;
-        'wmfusercontent.org':
-            certname => 'star.wmfusercontent.org',
-            server_name => 'wmfusercontent.org',
-            server_aliases => ['*.wmfusercontent.org'];
-        'planet.wikimedia.org':
-            certname       => 'star.planet.wikimedia.org',
-            server_name    => 'planet.wikimedia.org',
-            server_aliases => ['*.planet.wikimedia.org'];
+    role::cache::ssl::local { 'wikimedia.org':
+        certname       => 'sni.wikimedia.org',
+        server_name    => 'wikimedia.org',
+        server_aliases => ['*.wikimedia.org'],
+        default_server => true;
+    }
+
+    role::cache::ssl::local { 'wmfusercontent.org':
+        certname       => 'star.wmfusercontent.org',
+        server_name    => 'wmfusercontent.org',
+        server_aliases => ['*.wmfusercontent.org'];
+    }
+
+    role::cache::ssl::local { 'planet.wikimedia.org':
+        certname       => 'star.planet.wikimedia.org',
+        server_name    => 'planet.wikimedia.org',
+        server_aliases => ['*.planet.wikimedia.org'];
     }
 }
 
