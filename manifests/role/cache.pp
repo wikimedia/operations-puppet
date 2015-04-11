33453fc6f9bee46fad2f2e2e26003b149d0ffb07
Brandon Black <bblack@wikimedia.org>
2015-04-11 16:34:44 +0000

    cache.pp cleanup: a little formatting
    Change-Id: I289bb07b370fbaaedf164c3f758a1678f6d995b5

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -2,53 +2,20 @@
 # cache::varnish role classes
 
 # Virtual resources for the monitoring server
-@monitoring::group { 'cache_text_eqiad':
-    description => 'eqiad text Varnish',
-}
-@monitoring::group { 'cache_text_esams':
-    description => 'esams text Varnish',
-}
-@monitoring::group { 'cache_text_ulsfo':
-    description => 'ulsfo text Varnish',
-}
-
-@monitoring::group { 'cache_upload_eqiad':
-    description => 'eqiad upload Varnish',
-}
-@monitoring::group { 'cache_upload_esams':
-    description => 'esams upload Varnish',
-}
-@monitoring::group { 'cache_upload_ulsfo':
-    description => 'ulsfo upload Varnish',
-}
-
-@monitoring::group { 'cache_bits_eqiad':
-    description => 'eqiad bits Varnish',
-}
-@monitoring::group { 'cache_bits_esams':
-    description => 'esams bits Varnish',
-}
-@monitoring::group { 'cache_bits_ulsfo':
-    description => 'ulsfo bits Varnish',
-}
-
-@monitoring::group { 'cache_mobile_eqiad':
-    description => 'eqiad mobile Varnish',
-}
-@monitoring::group { 'cache_mobile_esams':
-    description => 'esams mobile Varnish',
-}
-@monitoring::group { 'cache_mobile_ulsfo':
-    description => 'ulsfo mobile Varnish',
-}
-
-@monitoring::group { 'cache_parsoid_eqiad':
-    description => 'Parsoid caches eqiad',
-}
-
-@monitoring::group { 'cache_misc_eqiad':
-    description => 'Misc caches eqiad',
-}
+@monitoring::group { 'cache_text_eqiad': description => 'eqiad text Varnish' }
+@monitoring::group { 'cache_text_esams': description => 'esams text Varnish' }
+@monitoring::group { 'cache_text_ulsfo': description => 'ulsfo text Varnish' }
+@monitoring::group { 'cache_upload_eqiad': description => 'eqiad upload Varnish' }
+@monitoring::group { 'cache_upload_esams': description => 'esams upload Varnish' }
+@monitoring::group { 'cache_upload_ulsfo': description => 'ulsfo upload Varnish' }
+@monitoring::group { 'cache_bits_eqiad': description => 'eqiad bits Varnish' }
+@monitoring::group { 'cache_bits_esams': description => 'esams bits Varnish' }
+@monitoring::group { 'cache_bits_ulsfo': description => 'ulsfo bits Varnish' }
+@monitoring::group { 'cache_mobile_eqiad': description => 'eqiad mobile Varnish' }
+@monitoring::group { 'cache_mobile_esams': description => 'esams mobile Varnish' }
+@monitoring::group { 'cache_mobile_ulsfo': description => 'ulsfo mobile Varnish' }
+@monitoring::group { 'cache_parsoid_eqiad': description => 'Parsoid caches eqiad' }
+@monitoring::group { 'cache_misc_eqiad': description => 'Misc caches eqiad' }
 
 class role::cache::configuration {
     include lvs::configuration
@@ -101,7 +68,8 @@ class role::cache::configuration {
                 'ulsfo' => [],
             },
             'bits' => {
-                'eqiad' => ['cp1056.eqiad.wmnet',
+                'eqiad' => [
+                            'cp1056.eqiad.wmnet',
                             'cp1057.eqiad.wmnet',
                             'cp1069.eqiad.wmnet',
                             'cp1070.eqiad.wmnet',
