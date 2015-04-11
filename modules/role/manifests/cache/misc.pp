0a391bbf7cdc8cb64790dba6aa7ad439c472d415
Brandon Black <bblack@wikimedia.org>
2015-04-11 17:37:22 +0000

    cache.pp cleanup: split one def per file, move to module loader
    Note @monitoring::group bits get left behind in cache.pp for now.
    
    Change-Id: I4842498db8054b620d5be525e129ef76be407d3a

---
+++ modules/role/manifests/cache/misc.pp
@@ -0,0 +1,105 @@
+class role::cache::misc inherits role::cache::1layer {
+
+    class { 'lvs::realserver':
+        realserver_ips => $lvs::configuration::lvs_service_ips[$::realm]['misc_web'][$::site],
+    }
+
+    system::role { 'role::cache::misc':
+        description => 'misc Varnish cache server'
+    }
+
+    include standard
+    include nrpe
+    include role::cache::ssl::misc
+
+    $memory_storage_size = 8
+
+    varnish::instance { 'misc':
+        name            => '',
+        vcl             => 'misc',
+        port            => 80,
+        admin_port      => 6082,
+        storage         => "-s malloc,${memory_storage_size}G",
+        vcl_config      => {
+            'retry503'        => 4,
+            'retry5xx'        => 1,
+            'cache4xx'        => '1m',
+            'layer'           => 'frontend',
+            'ssl_proxies'     => $wikimedia_networks,
+            'default_backend' => 'antimony',    # FIXME
+            'allowed_methods' => '^(GET|HEAD|POST|PURGE|PUT)$',
+        },
+        backends        => [
+            'antimony.wikimedia.org',
+            'caesium.eqiad.wmnet',
+            'californium.wikimedia.org',
+            'dataset1001.wikimedia.org',
+            'gallium.wikimedia.org',  # CI server
+            'ytterbium.wikimedia.org', # Gerrit
+            'graphite1001.eqiad.wmnet',
+            'zirconium.wikimedia.org',
+            'ruthenium.eqiad.wmnet', # parsoid rt test server
+            'logstash1001.eqiad.wmnet',
+            'logstash1002.eqiad.wmnet',
+            'logstash1003.eqiad.wmnet',
+            'netmon1001.wikimedia.org', # servermon
+            'iridium.eqiad.wmnet', # main phab
+            'terbium.eqiad.wmnet', # public_html
+            'neon.wikimedia.org', # monitoring tools (icinga et al)
+            'magnesium.wikimedia.org', # RT and racktables
+            'stat1001.eqiad.wmnet', # metrics and metrics-api
+            'palladium.eqiad.wmnet',
+            'analytics1027.eqiad.wmnet', # Hue (Hadoop GUI)
+            'analytics1001.eqiad.wmnet', # Hadoop Yarn ResourceManager GUI
+        ],
+        backend_options => [
+        {
+            'backend_match' => '^(antimony|ytterbium)',
+            'port'          => 8080,
+        },
+        {
+            'backend_match' => '^(ruthenium)',
+            'port'          => 8001,
+        },
+        {
+            'backend_match' => '^logstash',
+            'probe'         => 'logstash',
+        },
+        {
+            # hue serves requests on port 8888
+            'backend_match' => '^analytics1027',
+            'port'          => 8888,
+        },
+        {
+            # Yarn ResourceManager UI serves requests on port 8088
+            'backend_match' => '^analytics1001',
+            'port'          => 8088,
+        },
+        {
+            'port'                  => 80,
+            'connect_timeout'       => '5s',
+            'first_byte_timeout'    => '35s',
+            'between_bytes_timeout' => '4s',
+            'max_connections'       => 100,
+        }],
+        directors       => {
+            'logstash' => [
+                'logstash1001.eqiad.wmnet',
+                'logstash1002.eqiad.wmnet',
+                'logstash1003.eqiad.wmnet',
+            ]
+        },
+    }
+
+    # ToDo: Remove production conditional once this works
+    # is verified to work in labs.
+    if $::realm == 'production' {
+        # Install a varnishkafka producer to send
+        # varnish webrequest logs to Kafka.
+        class { 'role::cache::kafka::webrequest':
+            topic => 'webrequest_misc',
+            varnish_name => $::hostname,
+            varnish_svc_name => 'varnish',
+        }
+    }
+}
