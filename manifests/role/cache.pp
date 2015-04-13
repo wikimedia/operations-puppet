a2e654d0af2b1b65ba3f524d81911a6f5fd15caf
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-04-13 16:14:30 +0100

    varnishkafka: fix misplaced parenthesis
    Change-Id: I2902d7dc5a93a9a0b409ca2a373d687e0d86d6fb

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -495,7 +495,7 @@ class role::cache::kafka::webrequest(
     # (logster only reports once a minute)
     monitoring::graphite_threshold { 'varnishkafka-kafka_drerr':
         description     => 'Varnishkafka Delivery Errors per minute',
-        metric          => "derivative(transformNull(${graphite_metric_prefix}.varnishkafka.kafka_drerr), 0)",
+        metric          => "derivative(transformNull(${graphite_metric_prefix}.varnishkafka.kafka_drerr, 0))",
         # warn if more than 0 errors per minute in the last 10 minutes
         warning         => 0,
         # critical if more than 20000 errors per minute in the last 10 minutes
