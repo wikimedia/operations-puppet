19ededad036011f30bb80fc7c65e19441c0873f8
Gabriel Wicke <gwicke@wikimedia.org>
2015-04-13 11:05:13 -0700

    Set up /api/v1/ entry point for restbase
    There are major performance benefits in minimizing the number of domains we
    use to serve content. As documented in T95229, total cold-start overheads for
    a new domain (DNS, TCP & TLS) seem to exceed 1s with mean VE user's connection
    latencies.
    
    This patch sets up a path-based entry point for the REST API at /api/v1/,
    which avoids this issue by sharing the existing connection(s) to the main
    project domain. Requests matching ^/api/v1/ are diverted to the restbase
    cluster in the backend Varnishes. While there might be performance advantages
    in doing this in the frontend Varnish (especially for esams, where we'd reduce
    the number of Varnish hops from 3 to 1), other considerations like IPSec make
    traversing the second layer more practical for now.
    
    Additional minor tweaks:
    
    - Configure beta labs restbase instance
    
    Bug: T95229
    
    Change-Id: I46e422825af2cf6f972b64e6d50040220ab08995

--- modules/role/manifests/cache/text.pp
+++ modules/role/manifests/cache/text.pp
@@ -31,6 +31,7 @@ class role::cache::text inherits role::cache::2layer {
             'api'               => $role::cache::configuration::backends[$::realm]['api'][$::mw_primary],
             'rendering'         => $role::cache::configuration::backends[$::realm]['rendering'][$::mw_primary],
             'test_wikipedia'    => $role::cache::configuration::backends[$::realm]['test_appservers'][$::mw_primary],
+            'restbase'          => $role::cache::configuration::backends[$::realm]['restbase'][$::mw_primary],
         },
         2 => {
             'eqiad' => $text_nodes['eqiad'],
@@ -98,6 +99,11 @@ class role::cache::text inherits role::cache::2layer {
             {
                 'backend_match'   => '^mw1017\.eqiad\.wmnet$',
                 'max_connections' => 20,
+            },
+            {
+                'backend_match'   => '^restbase\.svc\.|^deployment-restbase',
+                'port'            => 7231,
+                'max_connections' => 5000,
             },
             {
                 'port'                  => 80,
