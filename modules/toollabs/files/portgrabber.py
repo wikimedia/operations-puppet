4fc2b9c5449847c5c889ee774f3702d6a9d695a1
YuviPanda <yuvipanda@gmail.com>
2015-04-13 20:01:43 -0700

    tools: Have uwsgi and nodejs attempt to pick a random port
    Instead of always using the uid for port.
    
    Bug: T93046
    Change-Id: Ib9395817f431d73820e08b893355327298f6963c

--- modules/toollabs/files/portgrabber.py
+++ modules/toollabs/files/portgrabber.py
@@ -7,16 +7,29 @@ import yaml
 socks = []
 
 
-def getProxies():
+def get_proxies():
     """Return the list of proxies to register with."""
     with open('/etc/portgrabber.yaml', 'r') as f:
         config = yaml.safe_load(f)
         return config['proxies']
 
 
+def get_open_port():
+    """Tries to get a random open port to listen on
+
+    It does this by starting to listen on a socket, letting the kernel determine
+    the open port. It then immediately closes it and returns the port.
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 0))
+    port = s.getsockname()[1]
+    s.close()
+    return port
+
+
 def register(port):
     """Register with the proxies."""
-    for proxy in getProxies():
+    for proxy in get_proxies():
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((proxy, 8282))
         sock.sendall(".*\nhttp://%s:%u\n" % (socket.getfqdn(), port))
