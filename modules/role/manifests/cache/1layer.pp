5f6e58bea5854a4d5cc3bf63dddbda8d668b5a0e
Brandon Black <bblack@wikimedia.org>
2015-04-16 14:03:40 +0000

    s/inheritance/include/ for role::cache::1layer
    Change-Id: Ic3a5829031d890befe4fbb3e1c6d0a5ad0612b01

--- modules/role/manifests/cache/1layer.pp
+++ modules/role/manifests/cache/1layer.pp
@@ -1,9 +1,6 @@
-# Ancestor class for common resources of 1-layer clusters
+# To be included by all concrete 1layer cache roles
 class role::cache::1layer {
     include role::cache::base
-
-    # Any changes here will affect all descendent Varnish clusters
-    # unless they're overridden!
 
     if $::role::cache::configuration::has_ganglia {
         include varnish::monitoring::ganglia
