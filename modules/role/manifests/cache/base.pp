dcde867a6d77cf65fd7c34f4fc9324fa68342e0e
Brandon Black <bblack@wikimedia.org>
2015-04-16 12:57:56 +0000

    move prof perf tweaks to separate include
    Change-Id: Ieb78f79c45bf74f70b3669f228491b2503d0d2e7

--- modules/role/manifests/cache/base.pp
+++ modules/role/manifests/cache/base.pp
@@ -4,6 +4,11 @@ class role::cache::base {
     include role::cache::configuration
     include network::constants
 
+    # Only production needs these system perf tweaks
+    if $::realm == 'production' {
+        include role::cache::perf
+    }
+
     # Any changes here will affect all descendent Varnish clusters
     # unless they're overridden!
     if $::site in ['eqiad'] {
@@ -18,53 +23,6 @@ class role::cache::base {
     $storage_partitions = $::realm ? {
         'production' => ['sda3', 'sdb3'],
         'labs'       => ['vdb'],
-    }
-
-    # This seems to prevent long term memory fragmentation issues that
-    #  can cause VM perf issues.  This seems to be less necessary on jessie
-    #  with other assorted fixes in place, and we could experiment with
-    #  removing it entirely at a later time when things are more stable.
-    #  (watch for small but increasing sys% spikes on upload caches if so,
-    #  may take days to have real effect).
-    cron { 'varnish_vm_compact_cron':
-        command => 'echo 1 >/proc/sys/vm/compact_memory',
-        user    => 'root',
-        minute  => '*',
-    }
-
-    # Prod-specific performance tweaks
-    if $::realm == 'production' {
-        include cpufrequtils # defaults to "performance"
-
-        # Bump min_free_kbytes to ensure network buffers are available quickly
-        #   without having to evict cache on the spot
-        vm::min_free_kbytes { 'cache':
-            pct => 2,
-            min => 131072,
-            max => 1048576,
-        }
-
-        # RPS/RSS to spread network i/o evenly
-        interface::rps { 'eth0': }
-
-        # flush vm more steadily in the background. helps avoid large performance
-        #   spikes related to flushing out disk write cache.
-        sysctl::parameters { 'cache_role_vm_settings':
-            values => {
-                'vm.dirty_ratio'            => 40,  # default 20
-                'vm.dirty_background_ratio' => 5,   # default 10
-                'vm.dirty_expire_centisecs' => 500, # default 3000
-            },
-        }
-
-        # Disable TCP SSR (slow-start restart). SSR resets the congestion
-        # window of connections that have gone idle, which means it has a
-        # tendency to reset the congestion window of HTTP keepalive and SPDY
-        # connections, which are characterized by short bursts of activity
-        # separated by long idle times.
-        sysctl::parameters { 'disable_ssr':
-            values => { 'net.ipv4.tcp_slow_start_after_idle' => 0 },
-        }
     }
 
     # mma: mmap addrseses for fixed persistent storage on x86_64 Linux:
