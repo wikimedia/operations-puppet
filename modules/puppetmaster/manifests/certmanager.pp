b02c803699cfd8863e1737db5d519f35c64ba1b0
andrewbogott <abogott@wikimedia.org>
2015-04-15 19:02:56 -0500

    We need -y when calling salt-key -d remotely
    Change-Id: Iee93599c653e498954b012a560667de98322db2d

--- modules/puppetmaster/manifests/certmanager.pp
+++ modules/puppetmaster/manifests/certmanager.pp
@@ -16,7 +16,7 @@ class puppetmaster::certmanager(
     sudo::user { 'certmanager':
         privileges => [
             'ALL = (root) NOPASSWD: /usr/bin/puppet cert clean *',
-            'ALL = (root) NOPASSWD: /usr/bin/salt-key -d *'
+            'ALL = (root) NOPASSWD: /usr/bin/salt-key -y -d *'
         ]
     }
 }
