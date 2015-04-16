a7fd6ed22904f0ad2a46972569d079d4b50ecc93
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-04-16 12:12:52 +0100

    restbase: add ganglia aggregators
    Change-Id: I34b29af7e15c672c5195cff385a1b7d0cd5d6376

--- manifests/site.pp
+++ manifests/site.pp
@@ -2093,6 +2093,9 @@ node /^rdb200[1-4]\.codfw\.wmnet/ {
 
 # restbase eqiad cluster
 node /^restbase100[1-6]\.eqiad\.wmnet$/ {
+    if $::hostname == 'restbase1001' or $::hostname == 'restbase1006' {
+        $ganglia_aggregator = true
+    }
     role restbase, cassandra
     include base::firewall
     include standard
