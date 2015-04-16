1b2d5f35abcc4353477c46ae7bdc62e21e5824c6
Brandon Black <bblack@wikimedia.org>
2015-04-16 17:39:40 +0000

    creatively kill $realm storage conditionals
    Change-Id: Id7eaa2bd832334aa523acfcbd64d27d888931e50

--- modules/role/manifests/cache/2layer.pp
+++ modules/role/manifests/cache/2layer.pp
@@ -19,49 +19,43 @@ class role::cache::2layer {
     # https://phabricator.wikimedia.org/P485
     $backend_weight_avg = 100
 
-    if $::realm == 'production' {
-        $storage_size = $::hostname ? {
-            /^cp10(08|4[34])$/      => 117, # Intel X-25M 160G
-            /^cp30(0[3-9]|1[0-4])$/ => 460, # Intel M320 600G via H710
-            /^cp301[5-8]$/          => 225, # Intel M320 300G via H710
-            default                 => 360, # Intel S3700 400G
-        }
-
-        # This scales backend weights proportional to node storage size,
-        # with the default value of 100 corresponding to the most
-        # common/current case of 360G storage size on Intel S3700's.
-        $backend_scaled_weights = [
-            { backend_match => '^cp10(08|4[34])\.',      weight => 32  },
-            { backend_match => '^cp30(0[3-9]|1[0-4])\.', weight => 128 },
-            { backend_match => '^cp301[5-8]\.',          weight => 63  },
-            { backend_match => '.',                      weight => 100 },
-        ]
-
-        # These variables are unused, they just serve as documentation of
-        # manual things for now.  This is the equivalent of
-        # $backend_scaled_weights above for pybal frontend weighting.
-        #
-        # The eqiad/upload, esams/upload, and esams/text clusters need
-        # differential weighting in pybal due to variance in their nodes'
-        # class of CPU power for HTTPS.  This documents the necessary
-        # weight ratios, which are created from 'openssl speed aes-128-cbc'
-        # for 1K blocksize multiplied by CPU core count then GCD-scaled
-        # down within each datacenter to keep weight sums below ipvs sh
-        # limits (256 total).
-        $pybal_weight_esams_upload_text = [
-            '^cp30[34][0-9]'       => 10, # newer esams cp30xx
-            '^cp30(0[3-9]|1[0-8])' => 3,  # older esams cp30xx
-        ]
-        $pybal_weight_eqiad_upload = [
-            '^cp107[1-4]'          => 9, # newer eqiad cp10xx
-            '^cp10..'              => 4, # older eqiad cp10xx
-        ]
+    $storage_size = $::hostname ? {
+        /^cp10(08|4[34])$/      => 117, # Intel X-25M 160G
+        /^cp30(0[3-9]|1[0-4])$/ => 460, # Intel M320 600G via H710
+        /^cp301[5-8]$/          => 225, # Intel M320 300G via H710
+        /^cp[0-9]{4}$/          => 360, # Intel S3700 400G (prod default)
+        default                 => 6,   # 6 is the bare min, for e.g. virtuals
     }
-    else {
-        # for upload on jessie, bigobj is main/6, so 6 is functional minimum here.
-        $storage_size = 6
-        $backend_scaled_weights = [ { backend_match => '.', weight => 100 } ]
-    }
+
+    # This scales backend weights proportional to node storage size,
+    # with the default value of 100 corresponding to the most
+    # common/current case of 360G storage size on Intel S3700's.
+    $backend_scaled_weights = [
+        { backend_match => '^cp10(08|4[34])\.',      weight => 32  },
+        { backend_match => '^cp30(0[3-9]|1[0-4])\.', weight => 128 },
+        { backend_match => '^cp301[5-8]\.',          weight => 63  },
+        { backend_match => '.',                      weight => 100 },
+    ]
+
+    # These variables are unused, they just serve as documentation of
+    # manual things for now.  This is the equivalent of
+    # $backend_scaled_weights above for pybal frontend weighting.
+    #
+    # The eqiad/upload, esams/upload, and esams/text clusters need
+    # differential weighting in pybal due to variance in their nodes'
+    # class of CPU power for HTTPS.  This documents the necessary
+    # weight ratios, which are created from 'openssl speed aes-128-cbc'
+    # for 1K blocksize multiplied by CPU core count then GCD-scaled
+    # down within each datacenter to keep weight sums below ipvs sh
+    # limits (256 total).
+    $pybal_weight_esams_upload_text = [
+        '^cp30[34][0-9]'       => 10, # newer esams cp30xx
+        '^cp30(0[3-9]|1[0-8])' => 3,  # older esams cp30xx
+    ]
+    $pybal_weight_eqiad_upload = [
+        '^cp107[1-4]'          => 9, # newer eqiad cp10xx
+        '^cp10..'              => 4, # older eqiad cp10xx
+    ]
 
     # mma: mmap addrseses for fixed persistent storage on x86_64 Linux:
     #  This scheme fits 4x fixed memory mappings of up to 4TB each
