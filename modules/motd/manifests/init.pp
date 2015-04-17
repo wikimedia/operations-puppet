dbfdd90849d993cbb3aa915df0576e7cd5eb3725
Tim Landscheidt <tim@tim-landscheidt.de>
2015-04-17 11:23:47 +0000

    Labs: Create /etc/motd unconditionally
    Bug: T85307
    Change-Id: Iceb1b0f0782a2c9fd36004fda80dff3ad2125008

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -3,8 +3,9 @@
 # Module for customizing MOTD (Message of the Day) banners.
 #
 class motd {
-    # For Ubuntu trusty+, pam_motd is configured by sshd's pam configuration to
-    # read from /run directly and hence has no need for this.
+    # For Ubuntu trusty+, pam_motd is configured by sshd's pam
+    # configuration to read from /run directly and hence has no need
+    # for this.
     #
     # For Debian jessie (as of 2014-12-31) the situation is quite complicated:
     # https://lists.debian.org/debian-devel/2014/12/msg00368.html
@@ -12,6 +13,15 @@ class motd {
         file { '/etc/motd':
             ensure => link,
             target => '/var/run/motd',
+            force  => true,
+        }
+    } elsif $::realm == 'labs' and os_version('ubuntu == trusty') {
+        # In Labs the pam configuration is overwritten, effectively
+        # disabling motds on Trusty instances.  Thus we create it
+        # there as a workaround for T85910.
+        file { '/etc/motd':
+            ensure => link,
+            target => '/run/motd.dynamic',
             force  => true,
         }
     } else {
