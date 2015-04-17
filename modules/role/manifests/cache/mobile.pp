545ecfb62dcd2e58fe36874b82c979c1cc8bd260
Brandon Black <bblack@wikimedia.org>
2015-04-17 12:46:30 +0000

    get rid of realm conditionals on $memory_storage_size
    (and incidentally, improve accuracy of upload memory_storage_size
    calculation for large memory sizes)
    
    Change-Id: Idebbf01bc4dc82616905f7841e46d23e1a333e95

--- modules/role/manifests/cache/mobile.pp
+++ modules/role/manifests/cache/mobile.pp
@@ -4,7 +4,7 @@ class role::cache::mobile {
     $mobile_nodes = hiera('cache::mobile::nodes')
     $site_mobile_nodes = $mobile_nodes[$::site]
 
-    if $::realm == 'production' {
+    if $::memorysize_mb >= 16384 {
         $memory_storage_size = floor((0.125 * $::memorysize_mb / 1024.0) + 0.5) # 1/8 of total mem
     }
     else {
