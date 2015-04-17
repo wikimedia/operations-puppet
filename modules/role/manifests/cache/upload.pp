f17ffbb4d6ea027b5bc43a1bac835d7f02308f6e
Brandon Black <bblack@wikimedia.org>
2015-04-17 14:46:35 +0000

    normalize on $cluster_tier as string words
    This should reduce any confusion from type conversion, which seems
    to be an issue in this instance during some refactoring...
    
    Change-Id: If323f9cd7ef1c328e4c557bef09d86647c456233

--- modules/role/manifests/cache/upload.pp
+++ modules/role/manifests/cache/upload.pp
@@ -24,16 +24,16 @@ class role::cache::upload {
     }
 
     $varnish_be_directors = {
-        1 => {
+        'one' => {
             'backend'   => $lvs::configuration::lvs_service_ips[$::realm]['swift'][$::mw_primary],
             'rendering' => $role::cache::configuration::backends[$::realm]['rendering'][$::mw_primary],
         },
-        2 => {
+        'two' => {
             'eqiad' => $upload_nodes['eqiad'],
         }
     }
 
-    if $::role::cache::base::cluster_tier == 1 {
+    if $::role::cache::base::cluster_tier == 'one' {
         $director_retries = 2
     } else {
         $director_retries = $::role::cache::2layer::backend_weight_avg * 4
@@ -84,7 +84,7 @@ class role::cache::upload {
     ], ' ')
 
     $director_type_cluster = $::role::cache::base::cluster_tier ? {
-        1       => 'random',
+        'one'   => 'random',
         default => 'chash',
     }
 
