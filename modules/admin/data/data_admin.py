d2f581665b98c78edfb43fee8fee6cffbe20e765
Kunal Mehta <legoktm@gmail.com>
2015-04-17 21:30:40 -0700

    admin: Turn data_admin into a real unit test
    This splits the data_admin script into two, one for sorting (still in
    data_admin.py) and data_test.py for linting.
    
    data_test.py uses the unittest library and is run by nosetests.
    
    At the same time, stop using a custom tox enviornment and use a standard
    one like py27. This will simplify configuration on jenkins side, and
    make it easier to add other tests in the future.
    
    Change-Id: I23ef32b981ca30b3bd1e8e9a9062906a2c5b32d2

--- modules/admin/data/data_admin.py
+++ modules/admin/data/data_admin.py
@@ -1,27 +1,11 @@
 #!/usr/bin/env python
 
 # 2014 Chase Pettet
-# beginnings of a linter for data.yaml
+# Reads data.yaml and sorts it
 
 import sys
 import yaml
-import itertools
-from collections import Counter
 import collections
-
-
-def flatten(lists):
-    """flattens nested arrays"""
-    return list(itertools.chain.from_iterable(lists))
-
-
-def all_assigned_users(admins):
-    """unique assigned users
-    :param admins: hash from valid data.yaml
-    :returns: list
-    """
-    nested_users_list = map(lambda u: u['members'], admins['groups'].values())
-    return list(set(flatten(nested_users_list)))
 
 
 def dict_sort(dictionary):
@@ -37,30 +21,6 @@ def main():
     if 'sort' in sys.argv:
         print yaml.dump({'groups': dict_sort(admins['groups'])})
         print yaml.dump({'users': dict_sort(admins['users'])})
-
-    if 'lint' in sys.argv:
-        lint_error = False
-
-        all_users = admins['users'].keys()
-        grouped_users = all_assigned_users(admins)
-
-        # ensure all assigned users exist
-        non_existent_users = [u for u in grouped_users if u not in all_users]
-        if non_existent_users:
-            lint_error = True
-            print "Users assigned that do not exist: %s" % (
-                non_existent_users,)
-
-        # ensure no two groups uses the same gid
-        gids = filter(None, [
-            v.get('gid', None) for k, v in admins['groups'].iteritems()])
-        dupes = [k for k, v in Counter(gids).items() if v > 1]
-        if dupes:
-            lint_error = True
-            print "Duplicate group GIDs: %s" % (dupes,)
-
-        if lint_error:
-            sys.exit(1)
 
 if __name__ == '__main__':
     main()
