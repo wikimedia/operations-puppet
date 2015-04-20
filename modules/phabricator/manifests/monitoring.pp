20c4c93fc7f2cbc34ed4d051d17d1da0d3b40650
RobH <rob@wikimedia.org>
2015-04-20 14:32:56 -0700

    adding phabricator paging group
    I added in a phabricator paging contact group in previous changes, which
    now will page mukunda and chad with phabricator issues, as well as echo
    in IRC and page Ops.
    
    T96613
    
    Change-Id: Ie698fa7f6c685ecbad6a74a0110a3c1bfd8303ec

--- modules/phabricator/manifests/monitoring.pp
+++ modules/phabricator/manifests/monitoring.pp
@@ -5,11 +5,13 @@ class phabricator::monitoring {
     nrpe::monitor_service { 'check_phab_taskmaster':
         description  => 'check if phabricator taskmaster is running',
         nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 10:30 -c 10:30 --ereg-argument-array PhabricatorTaskmasterDaemon',
+        contact_group => 'admins,phabricator,sms',
     }
 
     monitoring::service { 'phabricator-https':
         description   => 'https://phabricator.wikimedia.org',
         check_command => 'check_https_phabricator',
+        contact_group => 'admins,phabricator,sms',
     }
 
 }
