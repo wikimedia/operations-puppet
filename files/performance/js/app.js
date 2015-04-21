1101cd8ce4af783c1dec76f5042842a831b17a57
Ori Livneh <ori@wikimedia.org>
2015-04-21 15:43:40 -0700

    coal: allow 'hour' resolution to be reselected after being deselected
    Change-Id: I3d5606c193f8828b952dd33464eb6541b02c7197

--- files/performance/js/app.js
+++ files/performance/js/app.js
@@ -1,60 +1,68 @@
 /*global d3, MG */
-function drawCharts( period ) {
-    d3.json( '/coal/v1/metrics?period=' + period, function ( data ) {
-        var metric, div, points;
+( function () {
+    'use strict';
 
-        function preprocessPoint( value, index ) {
-            var date = new Date( 1000 * ( data.start + index * data.step ) );
-            return { date: date, value: value };
-        }
+    function identity( x ) {
+        return x;
+    }
 
-        var graphs = d3.select( '#metrics' )
-            .selectAll( 'div' )
-            .data( d3.keys( data.points ) );
+    function drawCharts( period ) {
+        d3.json( '/coal/v1/metrics?period=' + period, function ( data ) {
+            var charts = d3.select( '#metrics' )
+                .selectAll( 'div' )
+                .data( d3.keys( data.points ) );
 
-        graphs.enter()
-            .append( 'div' )
-            .attr( 'class', 'metric' )
-            .attr( 'id', function ( d ) { return d; } );
+            charts.enter()
+                .append( 'div' )
+                .attr( 'class', 'metric' )
+                .attr( 'id', identity );
 
-        graphs.each( function ( metric ) {
-            MG.data_graphic( {
-                title: metric,
-                target: this,
-                data: d3.values( data.points[metric ] ).map( preprocessPoint ),
-                width: 680,
-                height: 200,
-                left: 60,
-                show_tooltips: false,
-                show_rollover_text: false,
+            charts.each( function ( metric ) {
+                var points = d3.values( data.points[metric] ).map( function ( value, idx ) {
+                    var epochSeconds = data.start + idx * data.step;
+                    return { date: new Date( 1000 * epochSeconds ), value: value };
+                } );
+
+                MG.data_graphic( {
+                    title: metric,
+                    target: this,
+                    data: points,
+                    width: 680,
+                    height: 200,
+                    left: 60,
+                    show_tooltips: false,
+                    show_rollover_text: false
+                } );
             } );
         } );
-
-    } );
-}
-
-function selectTab( tab ) {
-    d3.select( 'li.active' ).classed( 'active', false );
-    tab.className = 'active';
-    drawCharts( tab.id );
-}
-
-d3.selectAll( '.nav li:not(.active)' ).on( 'click', function () {
-    selectTab( this );
-} );
-
-function init() {
-    if ( /^#!\/./.test( location.hash ) ) {
-        // Handle permalink
-        var id = location.hash.slice( 3 );
-        var navItem = document.getElementById( id );
-        if ( navItem ) {
-           selectTab( navItem );
-        }
-    } else {
-        // Default
-        drawCharts( 'hour' );
     }
-}
 
-init();
+    function selectTab( tab ) {
+        if ( d3.select( tab ).classed( 'active' ) ) {
+            return;
+        }
+        d3.select( 'li.active' ).classed( 'active', false );
+        tab.className = 'active';
+        drawCharts( tab.id );
+    }
+
+    d3.selectAll( '.nav li' ).on( 'click', function () {
+        selectTab( this );
+    } );
+
+    function init() {
+        if ( /^#!\/./.test( location.hash ) ) {
+            // Handle permalink
+            var id = location.hash.slice( 3 );
+            var navItem = document.getElementById( id );
+            if ( navItem ) {
+               selectTab( navItem );
+            }
+        } else {
+            // Default
+            drawCharts( 'hour' );
+        }
+    }
+
+    init();
+} () );
