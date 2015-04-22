0bae990a99cb6d904db4436a6bcca36cfec6b00c
Andrew Otto <acotto@gmail.com>
2015-04-22 16:57:24 -0400

    Use direct hiera lookup of impala master_host variable
    We didn't want to use the module parameter to set this
    
    Change-Id: Icf12f60ed937cd3634361040892f378998a602b6

--- manifests/role/analytics/impala.pp
+++ manifests/role/analytics/impala.pp
@@ -1,20 +1,22 @@
 # Impala role classes.
 #
-# NOTE: Be sure that $cdh::impala::master_host is set in hiera!
-# In production this is set in hieradata/eqiad/cdh/impala.yaml.
-
+# NOTE: Be sure that $analytics::impala::master_host is set in hiera!
+# In production this is set in hieradata/eqiad/analytics/impala.yaml.
 
 # == Class role::analytics::impala
 # Installs base impala packages and the impala-shell client.
 #
 class role::analytics::impala {
-    include cdh::impala
+    class { cdh::impala:
+        master_host => hiera('analytics::impala::master_host')
+    }
 }
 
 # == Class role::analytics::impala::worker
 # Installs and configures the impalad server.
 #
 class role::analytics::impala::worker {
+    include role::analytics::impala
     include cdh::impala::worker
 }
 
@@ -22,5 +24,6 @@ class role::analytics::impala::worker {
 # Installs and configures llama, impala-state-store and impala-catalog
 #
 class role::analytics::impala::master {
+    include role::analytics::impala
     include cdh::impala::master
 }
