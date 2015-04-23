7eb2107d6b14609594a95b10a394681947fee84f
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2015-04-23 15:57:34 +0200

    tools: Extended Exim diamond collector for Tool Labs
     - forked and extended existing exim collector
     - extended queue information
     - added paniclog information
     - moved sudo configuration to collector manifest
     - updated tool labs mail relay for new collector
    
    Bug: T96898
    Change-Id: I9010886ecac20c94a3916b908910853e26241318
    Original-Change-Id: I1d0517e41d61201e7f6c9b85c116f952658f73a3

---
+++ modules/diamond/manifests/collector/extendedexim.pp
@@ -0,0 +1,37 @@
+# == Define: diamond::collector::extendedexim
+#
+# Exim collector. Collects queue properties and paniclog size.
+#
+# Queue properties:
+#     - queue.oldest: age of oldest e-mail in queue (seconds)
+#     - queue.youngest: age of youngest e-mail in queue (seconds)
+#     - queue.size: total size of the queue (bytes)
+#     - queue.length: total number of e-mails in the queue
+#     - queue.num_frozen: number of frozen e-mails in the queue
+#
+# Paniclog properties:
+#     - paniclog.length: number of lines in /var/log/exim4/paniclog
+
+include stdlib
+
+define diamond::collector::extendedexim(
+    $settings = {},
+    $ensure   = present,
+) {
+    $default_settings = {'use_sudo' => 'true'}
+    $merged_settings = merge($default_settings, $settings)
+
+    diamond::collector { 'ExtendedExim':
+        settings => $merged_settings,
+        source   => 'puppet:///modules/diamond/collector/extendedexim.py',
+        ensure   => $ensure,
+    }
+
+    if str2bool($merged_settings[use_sudo]) {
+        sudo::user { 'diamond_sudo_for_exim':
+            user       => 'diamond',
+            privileges => ['ALL=(root) NOPASSWD: /usr/sbin/exim, /bin/cat /var/log/exim4/paniclog'],
+            ensure     => $ensure,
+        }
+    }
+}
