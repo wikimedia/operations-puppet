5042881183b2c279a0a6f38b1153e353c2793c21
dzahn <dzahn@wikimedia.org>
2015-04-23 13:10:33 -0700

    backup: add new fileset for contacts.wm
    Adds a new fileset for Bacula to be used to backup
    the former contacts.wm.org files and mysqldumps.
    
    Bug:T90679
    Change-Id: I73ba68b69cc59b716daf0fedc2b3d1b322d97a4e

--- manifests/role/backup.pp
+++ manifests/role/backup.pp
@@ -201,6 +201,9 @@ class role::backup::director {
     bacula::director::fileset { 'srv-org-wikimedia-blog':
         includes => [ '/srv/org/wikimedia/blog' ]
     }
+    bacula::director::fileset { 'srv-org-wikimedia-contacts':
+        includes => [ '/srv/org/wikimedia/contacts', '/srv/org/wikimedia/contacts-dump' ]
+    }
     # As all /a this will hopefully no longer be needed at some point and will
     # be killed with fire
     bacula::director::fileset { 'mysql-a-backups':
