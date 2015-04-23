5424d2428f0ccc4bb76a998791906a3c30d4b7df
dzahn <dzahn@wikimedia.org>
2015-04-23 13:13:56 -0700

    contacts: remove Apache, add Backup
    Since contacts.wm.org has been retired, remove the Apache site,
    but add backup client to ensure the files and mysqldumps are saved.
    
    Bug:T90679
    Change-Id: I7c9411ab5a63d9cf04287e8e5125eb5486619503

--- manifests/role/contacts.pp
+++ manifests/role/contacts.pp
@@ -3,13 +3,7 @@ class role::contacts {
 
     system::role { 'role::contacts': description => 'contacts.wikimedia.org - Drupal/CiviCRM' }
 
-    apache::site { 'contacts.wikimedia.org':
-        content => template('apache/sites/contacts.wikimedia.org.erb'),
-    }
-
-    ferm::service { 'contacts_http':
-        proto => 'tcp',
-        port  => '80',
-    }
+        include role::backup::host
+        backup::set {'srv-org-wikimedia-contacts': }
 
 }
