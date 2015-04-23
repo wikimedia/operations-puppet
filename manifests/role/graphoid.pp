99a85439dd8575197e91add9859d60de50b6a98c
Marko Obrovac <mobrovac@wikimedia.org>
2015-04-23 14:41:43 +0200

    Graphoid: service deployment on SCA
    This patch introduces the role::graphoid and graphoid classes,
    responsable for deploying graphoid on the SCA cluster, together with its
    configuration. Concretely, the graphoid class installs the package
    dependencies and uses service::node to create the rest. Hiera is
    configured so that graphoid works both in production and
    deployment-prep.
    
    Bug: T90487
    Change-Id: I1d3edad9f5df5b3a9ea9443411f228fd24e74ed0

---
+++ manifests/role/graphoid.pp
@@ -0,0 +1,15 @@
+# vim: set ts=4 et sw=4:
+class role::graphoid {
+
+    system::role { 'role::graphoid':
+        description => 'node.js service converting graph definitions into PNG'
+    }
+
+    ferm::service { 'graphoid':
+        proto => 'tcp',
+        port  => 19000,
+    }
+
+    include ::graphoid
+
+}
