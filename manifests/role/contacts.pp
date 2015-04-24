5daf039ea9a16a11d3a7d5aa770b9624df9e6f3a
dzahn <dzahn@wikimedia.org>
2015-04-24 11:08:35 -0700

    contacts: delete Apache template, adjust role
    This service has been retired. The Apache site isn't used anymore,
    and we just keep the role for the backup for now.
    
    Bug:T90679
    Change-Id: I8b8c13dea31703d9cd2a98e0d189009b0b180e02

--- manifests/role/contacts.pp
+++ manifests/role/contacts.pp
@@ -1,7 +1,6 @@
-# https://contacts.wikimedia.org | http://en.wikipedia.org/wiki/CiviCRM
 class role::contacts {
 
-    system::role { 'role::contacts': description => 'contacts.wikimedia.org - Drupal/CiviCRM' }
+    system::role { 'role::contacts': description => '(retired) contacts.wikimedia.org - Drupal/CiviCRM' }
 
         include role::backup::host
         backup::set {'srv-org-wikimedia-contacts': }
