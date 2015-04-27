9e60144d5dcbb2a1d490caaaddb03b9b879cddee
Andrew Otto <acotto@gmail.com>
2015-04-27 12:34:03 -0400

    Set up rsync module on copper to allow easy copying of pbuilder built packages to carbon and elsewhere
    Change-Id: Ic08cdd7dbe7c9cb23a3fa60b4fd7cc6d575e1246

--- manifests/role/package.pp
+++ manifests/role/package.pp
@@ -9,4 +9,22 @@ class role::package::builder {
     system::role { 'role::package::builder':
         description => 'Debian package builder'
     }
+
+
+    # this uses modules/rsync to
+    # set up an rsync daemon service
+    include rsync::server
+
+    # Set up an rsync module to allow easy copying of pbuilder
+    # results to carbon or elsewhere.  You can rsync from this like:
+    #   rsync copper.eqiad.wmnet::pbuilder-result/jessie-amd64/mypackage* ./mypackage/
+    rsync::server::module { 'pbuilder-result':
+        path        => '/var/cache/pbuilder/result',
+    }
+
+    # Allow rsync
+    ferm::service { 'rsync':
+        proto => 'tcp',
+        port  => '873',
+    }
 }
