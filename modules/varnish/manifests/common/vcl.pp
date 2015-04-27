c2abfef8d4b399a8c9bedb9922241f52ede9d1ed
Brandon Black <bblack@wikimedia.org>
2015-04-27 19:15:42 +0000

    de-dupe /static hashing for text/mobile
    Bug: T95448
    
    Change-Id: Ie7b742cd8aa3722f61be97bd29e023590b4264d2

--- modules/varnish/manifests/common/vcl.pp
+++ modules/varnish/manifests/common/vcl.pp
@@ -15,6 +15,13 @@ class varnish::common::vcl {
         content => template('varnish/last-access.inc.vcl.erb'),
     }
 
+    file { '/etc/varnish/static-hash.inc.vcl':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('varnish/static-hash.inc.vcl.erb'),
+    }
+
     file { '/etc/varnish/provenance.inc.vcl':
         owner   => 'root',
         group   => 'root',
