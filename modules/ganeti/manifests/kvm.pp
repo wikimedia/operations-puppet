888f1de771c5fc8c2259e6dc61c6d1a9a61455c6
Faidon Liambotis <faidon@wikimedia.org>
2015-04-27 20:00:23 +0300

    ganeti: switch to the sysfs module
    Change-Id: I4cdaeeff28293df182b20b59a2ec5c456f4b61da

--- modules/ganeti/manifests/kvm.pp
+++ modules/ganeti/manifests/kvm.pp
@@ -17,14 +17,10 @@ class ganeti::kvm {
         ensure => present,
     }
 
-    exec { 'ksm_sleep':
-        path    => '/bin',
-        command => 'echo 100 > /sys/kernel/mm/ksm/sleep_millisecs',
-        unless  => 'grep -q 100 /sys/kernel/mm/ksm/sleep_millisecs',
-    }
-    exec { 'ksm_run':
-        path    => '/bin',
-        command => 'echo 1 > /sys/kernel/mm/ksm/run',
-        unless  => 'grep -q 1 /sys/kernel/mm/ksm/run',
+    sysfs::parameters { 'ksm':
+        values => {
+            'kernel.mm.ksm.run'             => '1',
+            'kernel.mm.ksm.sleep_millisecs' => '100',
+        },
     }
 }
