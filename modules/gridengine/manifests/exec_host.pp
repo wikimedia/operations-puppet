20b94763fa9590bbc8cc114c223b6ea10e7a291d
Yuvipanda <yuvipanda@gmail.com>
2015-04-28 23:44:26 +0000

    Revert "tools: Move compute node code into tools::compute"
    I massively underestimaged how much work this is going to be.
    
    This reverts commit b423e64c91e085b64dce6d5ae52b79f996eff3b2.
    
    Change-Id: Id76597bbd7876eaba4c476dd0c3b0f93b004e093

--- modules/gridengine/manifests/exec_host.pp
+++ modules/gridengine/manifests/exec_host.pp
@@ -4,6 +4,8 @@ class gridengine::exec_host(
     $config = undef,
 )
 {
+    include gridengine
+
     package { 'gridengine-exec':
         ensure  => latest,
         require => Package['gridengine-common'],
