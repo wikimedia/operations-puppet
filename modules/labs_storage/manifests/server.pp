c8bac029a82725266b6ccd0e226c5280682ec5c2
Coren <marc@uberbox.org>
2015-04-29 14:43:38 -0400

    Labs: Reduce labstore* LDAP config to the minimum
    Because of group permission checking, the NFS server must have
    the list of users and group memberships of file owners on the
    filesystem (vide the linked bug), but this interferes with
    applying the ::admin class to unify the security model of
    the labstores with the rest of production.
    
    This changeset alters the LDAP configuration to a minimalist
    version that:
    
     (a) prevents authentication
     (b) 'renames' LDAP users to their numerical UID (as strings)
         so that they do not conflict with puppet-managed accounts.
    
    Because the LDAP users come without shadow entries or usable
    shells, they cannot be authenticated against nor used to set
    sessions up (and PAM is not configured to attempt either), leaving
    only the files users (managed by ::admin).
    
    Bug: T95559
    Change-Id: I29fe6fdec8a49e333d2b696afc64b772a99aed9b

--- modules/labs_storage/manifests/server.pp
+++ modules/labs_storage/manifests/server.pp
@@ -37,5 +37,37 @@ class labs_storage::server {
         require => File['/etc/replication-rsync.conf'],
     }
 
+    # We do not include ldap::client here because it brings in
+    # complete auth for LDAP users intended for labs instances.
+    #
+    # Rather, we include the configuration, then explicitly set up
+    # a minimalist configuration that is just sufficient to
+    # bring in the (redacted) list of users and groups for NFS
+    # permission checks, without setting up or permitting any
+    # authentication or session setup for those users.
+
+    include ldap::role::config::labs
+
+    file { '/etc/nsswitch.conf':
+        source => 'puppet:///modules/labs_storage/nsswitch.conf',
+    }
+
+    package { 'nslcd':
+        ensure => present,
+    }
+
+    file { '/etc/nslcd.conf':
+        notify  => Service['nslcd'],
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0440',
+        content => template('labs_storage/nslcd.conf.erb'),
+    }
+
+    service { 'nslcd':
+        ensure  => running,
+        require => [ Package['nslcd'], File['/etc/nslcd.conf'], ],
+    }
+
 }
 
