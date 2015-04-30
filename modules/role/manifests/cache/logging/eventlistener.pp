ac0fb7a2f05d306e20590fa2e5dfdc0466035344
Ori Livneh <ori@wikimedia.org>
2015-04-29 17:21:55 -0700

    add role::logging::eventlistener to text and mobile varnishes
    Doing this requires parametrizing the Varnish instance name whose log
    varnishncsa should tail. On bits varnishes, it is the default instance, but on
    text and mobile, it needs to be 'frontend'.
    
    The regex is updated by this patch to be compatible with both /event.gif and
    /beacon/event.gif.
    
    Change-Id: I2af6fb3d71ca978374b975d44c77f7f82536e38a

--- modules/role/manifests/cache/logging/eventlistener.pp
+++ modules/role/manifests/cache/logging/eventlistener.pp
@@ -1,4 +1,4 @@
-class role::cache::logging::eventlistener {
+class role::cache::logging::eventlistener( $instance_name = '' ) {
     $event_listener = $::realm ? {
         'production' => '10.64.32.167',  # eventlog1001
         'labs'       => '10.68.16.52',   # deployment-eventlogging02
@@ -7,8 +7,8 @@ class role::cache::logging::eventlistener {
     varnish::logging { 'eventlogging' :
         listener_address => $event_listener,
         port             => '8422',
-        instance_name    => '',
-        cli_args         => '-m RxURL:^/event\.gif\?. -D',
+        instance_name    => $instance_name,
+        cli_args         => '-m RxURL:^/\(beacon/\)?event\.gif\?. -D',
         log_fmt          => '%q\t%l\t%n\t%t\t%h\t"%{User-agent}i"',
         monitor          => false,
     }
