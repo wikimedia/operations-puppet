fe0637d7ccd92b47451180c9890f1dea1f114fc7
Dan Duvall <dduvall@wikimedia.org>
2015-04-30 14:21:50 -0700

    ci: Role for running Raita
    Raita is a log database + dashboard for Cucumber test results. Results
    are submitted by MW-Selenium to an Elasticsearch index and
    consumed/displayed by a small client-side application.
    
    For now both ES and Apache will run on its own instance and will be
    mostly isolated from other CI instances until fully tested.
    
    Apache is configured to only proxy requests to specific documents and
    search endpoints.
    
    Change-Id: I9f6816096d86ec9cb85796dc6cb177f2f295e4fa

--- manifests/role/ci.pp
+++ manifests/role/ci.pp
@@ -545,3 +545,48 @@ class role::ci::jenkins_access {
     # Also need the slave scripts for multi-git.sh
     include contint::slave-scripts
 }
+
+# Raita dashboard for displaying and taking action on Cucumber test results
+# logged to Elasticsearch via MW-Selenium's custom logger.
+#
+class role::ci::raita {
+    ensure_packages(['nodejs', 'npm'])
+
+    include ::elasticsearch
+    include ::apache::mod::proxy
+    include ::apache::mod::proxy_http
+
+    $dir = '/srv/raita'
+    $docroot = '/srv/raita/docroot'
+    $index_url = 'http://127.0.0.1:9200/raita'
+
+    system::role { 'role::ci::raita':
+        description => 'Dashboard for Cucumber test results',
+    }
+
+    git::clone { 'integration/raita':
+        ensure    => 'latest',
+        directory => $dir,
+        owner     => 'root',
+        group     => 'www-data',
+        mode      => '0755',
+    }
+
+    exec { 'raita riot precompilation':
+        command     => '/usr/bin/npm run build',
+        cwd         => $dir,
+        subscribe   => Git::Clone['integration/raita'],
+        refreshonly => true,
+    }
+
+    exec { 'raita import elasticsearch mappings':
+        command => "/usr/bin/node scripts/mappings.js import ${index_url}",
+        unless  => "/usr/bin/node scripts/mappings.js check ${index_url}",
+        cwd     => $dir,
+        require => Class['elasticsearch'],
+    }
+
+    apache::site { 'raita.wmflabs.org':
+        content => template('contint/apache/raita.wmflabs.org.erb'),
+    }
+}
