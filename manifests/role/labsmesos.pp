f1adbb6ba462419adb80d5fd5fcfddf186a3265c
YuviPanda <yuvipanda@gmail.com>
2015-05-02 15:02:03 -0700

    mesos: import module + add simple role
    Module improted from https://github.com/deric/puppet-mesos as
    submodule
    
    Change-Id: I400d0ca2e390bb336f5c308b5e143f49e61af029

---
+++ manifests/role/labsmesos.pp
@@ -0,0 +1,13 @@
+class role::labs::mesos {
+    include misc::labsdebrepo
+}
+
+class role::labs::mesos::master {
+
+    include role::labs::mesos
+
+    # Host zookeeper on itself
+    include role::analytics::zookeeper::server
+
+    include ::mesos::master
+}
