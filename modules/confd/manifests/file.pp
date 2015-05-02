c2e9cfd8e1efb6f93a8ed4813e7ea6d292661911
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-05-02 20:27:22 +0200

    confd: create module
    This module allows to define configuration files to be
    monitored/generated via confd.
    
    Bug: T97974
    Change-Id: Icc0e87e274d2a53a3308f4411a98d16c37314f8e

---
+++ modules/confd/manifests/file.pp
@@ -0,0 +1,36 @@
+# == Define confd::file
+#
+# Defines a service template to be monitored by confd,
+# and the corresponding geneated config file.
+define confd::file (
+    $watch_keys,
+    $ensure  = 'present',
+    $uid     = undef,
+    $gid     = undef,
+    $mode    = '0444',
+    $reload  = undef,
+    $check   = undef,
+    $source  = undef,
+    $content = undef
+    ) {
+    $name = regsubst('\/', '_', $title)
+
+
+    unless ($source or $content) {
+        fail('We either need a source file or a content for the config file')
+    }
+    #TODO validate at least uid and guid
+
+    file { "/etc/confd/conf.d/${name}.toml":
+        ensure  => $ensure,
+        content => template('confd/service_template.toml.erb'),
+    }
+
+    file { "/etc/confd/templates/${name}.tmpl":
+        ensure  => $ensure,
+        mode    => '0400',
+        source  => $source,
+        content => $content,
+        notify  => Service['confd'],
+    }
+}
