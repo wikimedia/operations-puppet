1ddd33fc70c2a1acfc09f1d5f463e5cb0ed7a04c
YuviPanda <yuvipanda@gmail.com>
2015-05-02 17:32:44 -0700

    [WIP]mesos: Add simple mesos module
    Change-Id: Ia423fcb2f1fe7eca944c443cb57500c332bcff00

---
+++ modules/mesos/manifests/master.pp
@@ -0,0 +1,47 @@
+class mesos::master(
+    $zookeeper,
+    $quorum,
+) {
+
+    require_package('mesos')
+
+    file { '/etc/mesos/zk':
+        content => $zookeeper,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['mesos-master'],
+    }
+
+    file { '/etc/mesos-master/quorum':
+        content => $quorum,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['mesos-master'],
+    }
+
+    file { '/etc/default/mesos':
+        source => 'puppet:///modules/mesos/mesos.default',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        notify => Service['mesos-master'],
+    }
+
+    file { '/etc/default/mesos-master':
+        source => 'puppet:///modules/mesos/mesos-master.default',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        notify => Service['mesos-master'],
+    }
+
+    service { 'mesos-master':
+        ensure => running,
+    }
+
+    service { 'mesos-slave':
+        ensure => stopped,
+    }
+}
