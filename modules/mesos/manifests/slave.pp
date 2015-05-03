799848785e9cce699d8e876582a5c8a10a16a36f
YuviPanda <yuvipanda@gmail.com>
2015-05-02 17:32:44 -0700

    mesos: Add simple mesos module
    - Picks up packages from labsdebrepo (for labs)
    - Picks up zookeeper url from hiera settings
    
    Change-Id: Ia423fcb2f1fe7eca944c443cb57500c332bcff00

---
+++ modules/mesos/manifests/slave.pp
@@ -0,0 +1,38 @@
+class mesos::slave(
+    $zookeeper_url,
+) {
+
+    require_package('mesos')
+
+    file { '/etc/mesos/zk':
+        content => $zookeeper_url,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['mesos-slave'],
+    }
+
+    file { '/etc/default/mesos':
+        source => 'puppet:///modules/mesos/mesos.default',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        notify => Service['mesos-slave'],
+    }
+
+    file { '/etc/default/mesos-slave':
+        source => 'puppet:///modules/mesos/mesos-slave.default',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        notify => Service['mesos-slave'],
+    }
+
+    service { 'mesos-master':
+        ensure => stopped,
+    }
+
+    service { 'mesos-slave':
+        ensure => running,
+    }
+}
