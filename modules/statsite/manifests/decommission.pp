2beb6686e2558f3d72e5e76d2664a8f579b72d49
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-05-04 11:16:59 +0200

    statsite: decommission class
    Bug: T95687
    Change-Id: Ic2ff480a28fa4825864454a73daa9cabd524e3ec

---
+++ modules/statsite/manifests/decommission.pp
@@ -0,0 +1,53 @@
+# == Class: statsite::decommission
+#
+# Decommission statsite
+
+class statsite::decommission {
+    package { 'statsite':
+        ensure => purged,
+    }
+
+    if os_version('ubuntu >= precise') {
+        file { '/etc/statsite':
+            ensure  => absent,
+            recurse => true,
+            purge   => true,
+            force   => true,
+        }
+
+        file { '/sbin/statsitectl':
+            ensure => absent,
+        }
+
+        file { '/etc/init/statsite':
+            ensure  => absent,
+            recurse => true,
+            purge   => true,
+            force   => true,
+        }
+
+        file { '/etc/init/statsite.override':
+            ensure  => absent,
+        }
+
+        service { 'statsite':
+            ensure   => 'stopped',
+            provider => 'base',
+            restart  => '/sbin/statsitectl restart',
+            start    => '/sbin/statsitectl start',
+            status   => '/sbin/statsitectl status',
+            stop     => '/sbin/statsitectl stop',
+            before   => [ Package['statsite'],
+                          File['/sbin/statsitectl'],
+                          File['/etc/statsite'],
+                          File['/etc/init/statsite'] ],
+        }
+    }
+
+    if os_version('debian >= jessie') {
+        service { 'statsite':
+            ensure => 'stopped',
+            before => Package['statsite'],
+        }
+    }
+}
