e4e6b1797985dba48e6964df018e40bbe68b47a0
Brandon Black <bblack@wikimedia.org>
2015-05-04 16:03:18 +0000

    refactor varnish::logging for icinga check
    Previously, text/mobile/upload had 2x logging instances (standard udp2log
    stuff) and bits had one (eventlistener).  The icinga check was hardcoded for
    checking two procs, and was applied to text/mobile/upload but not bits.
    
    Currently, upload still has 2x, bits still has 1x, and text/mobile have 3x
    (they gained the eventlistener one as well), so all of the text/mobile nodes
    were warning in icinga about excessive instances...
    
    This refactors the related classes to parameterize the instance count for
    icinga.  It's a little ugly, but at least it should come out ok functionally
    and cover all the cases.
    
    Change-Id: Id3dd287f707421931297ce0302e602db831fdd48

--- modules/role/manifests/cache/logging.pp
+++ modules/role/manifests/cache/logging.pp
@@ -1,18 +1,56 @@
-class role::cache::logging {
-    if $::realm == 'production' {
-        $webrequest_multicast_relay_host = '208.80.154.73' # gadoinium
+class role::cache::logging(
+    $udp2log = false,
+    $eventlogging = false,
+    $eventlogging_instance_name = '',
+) {
+    if $udp2log and $eventlistener {
+        $monitor_instances = 3
+    }
+    elsif $udp2log {
+        $monitor_instances = 2
+    }
+    elsif $eventlistener {
+        $monitor_instances = 1
+    }
+    else {
+        error('udp2log and/or eventlogging must be true for role::cache::logging')
+    }
 
-        $cliargs = '-m RxRequest:^(?!PURGE$) -D'
-        varnish::logging { 'multicast_relay':
-                listener_address => $webrequest_multicast_relay_host,
-                port             => '8419',
-                cli_args         => $cliargs,
+    if $udp2log {
+        if $::realm == 'production' {
+            $webrequest_multicast_relay_host = '208.80.154.73' # gadoinium
+
+            $cliargs = '-m RxRequest:^(?!PURGE$) -D'
+            varnish::logging { 'multicast_relay':
+                    listener_address => $webrequest_multicast_relay_host,
+                    port             => '8419',
+                    cli_args         => $cliargs,
+            }
+
+            varnish::logging { 'erbium':
+                    listener_address => '10.64.32.135',
+                    port             => '8419',
+                    cli_args         => $cliargs,
+            }
+        }
+    }
+
+    if $eventlogging {
+        $event_listener = $::realm ? {
+            'production' => '10.64.32.167',  # eventlog1001
+            'labs'       => '10.68.16.52',   # deployment-eventlogging02
         }
 
-        varnish::logging { 'erbium':
-                listener_address => '10.64.32.135',
-                port             => '8419',
-                cli_args         => $cliargs,
+        varnish::logging { 'eventlogging' :
+            listener_address => $event_listener,
+            port             => '8422',
+            instance_name    => $eventlogging_instance_name,
+            cli_args         => '-m RxURL:^/(beacon/)?event\.gif\?. -D',
+            log_fmt          => '%q\t%l\t%n\t%t\t%h\t"%{User-agent}i"',
         }
+    }
+
+    class { '::varnish::logging::monitor':
+        num_instances => $monitor_instances
     }
 }
