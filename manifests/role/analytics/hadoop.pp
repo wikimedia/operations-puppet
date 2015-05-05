5f601f4f23b1ea8d2af464f4307b000504869321
Andrew Otto <acotto@gmail.com>
2015-05-05 14:17:34 -0400

    [WIP] Puppetize HA YARN ResourceManager for Hadoop
    Change-Id: I6c1803c2699dc398d56be779e3d82ad4753926f4

--- manifests/role/analytics/hadoop.pp
+++ manifests/role/analytics/hadoop.pp
@@ -222,6 +222,7 @@ class role::analytics::hadoop::config {
             undef   => [$::fqdn],
             default => split($::hadoop_namenodes, ','),
         }
+        $resourcemanager_hosts = $namenode_hosts
 
         $journalnode_hosts = $::hadoop_journalnodes ? {
             undef   => undef,
@@ -232,6 +233,8 @@ class role::analytics::hadoop::config {
             undef   => undef,
             default => $::hadoop_cluster_name,
         }
+
+        $zookeeper_hosts = keys(hiera('zookeeper_hosts'))
 
         # Allow labs users to configure their Hadoop daemon
         # Heapsize.  NOTE:  This will be applied to
@@ -301,6 +304,8 @@ class role::analytics::hadoop::client inherits role::analytics::hadoop::config {
         cluster_name                             => $cluster_name,
         namenode_hosts                           => $namenode_hosts,
         journalnode_hosts                        => $journalnode_hosts,
+        resourcemanager_hosts                    => $resourcemanager_hosts,
+        zookeeper_hosts                          => $zookeeper_hosts,
         datanode_mounts                          => $datanode_mounts,
         dfs_name_dir                             => [$hadoop_name_directory],
         dfs_journalnode_edits_dir                => $hadoop_journal_directory,
@@ -612,6 +617,14 @@ class role::analytics::hadoop::standby inherits role::analytics::hadoop::client 
             require      => Class['cdh::hadoop::namenode::standby'],
             critical     => 'true',
         }
+    }
+
+
+    # if resourcemanager_hosts is set, then go ahead
+    # and include a resourcemanager on all standby nodes as well
+    # as the master node.
+    if $resourcemanager_hosts {
+        include cdh::hadoop::resourcemanager
     }
 }
 
