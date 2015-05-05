ea9a6d7fb1f7b8cdfeef52a22536c5333e684457
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-05-05 12:25:08 +0200

    etcd: create puppet module
    Bug: T97973
    Change-Id: Ib80e3d3c3e46f9ff0b81c5dd675ce79184324576

---
+++ modules/etcd/manifests/logging.pp
@@ -0,0 +1,21 @@
+# == Class etcd::logging
+#
+# Manages all the logging logic for etcd.
+class etcd::logging {
+
+    file { '/etc/logrotate.d/etcd':
+        source => 'puppet:///modules/etcd/logrotate.conf',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        before => Service['etcd'],
+    }
+
+
+    rsyslog::conf { 'etcd':
+        source   => 'puppet:///modules/etcd/etcd/rsyslog.conf',
+        priority => 20,
+        require  => File['/etc/logrotate.d/etcd'],
+        before   => Service['etcd']
+    }
+}
