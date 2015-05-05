118fb60b386287aa4787c109322e2021e8d47206
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-05-05 12:25:08 +0200

    etcd: create puppet module
    Bug: T97973
    Change-Id: Ib80e3d3c3e46f9ff0b81c5dd675ce79184324576

---
+++ modules/etcd/manifests/ssl.pp
@@ -0,0 +1,47 @@
+# == Class etcd::ssl
+#
+# Copies the relevant certificates from the puppet/ssl directory to here.
+class etcd::ssl($puppet_cert_name = $::fqdn,) {
+
+    file { '/var/lib/etcd/ssl':
+        ensure  => directory,
+        owner   => 'etcd',
+        group   => 'etcd',
+        mode    => '0500',
+        require => Package['etcd']
+    }
+
+    file { '/var/lib/etcd/ssl/ca.pem':
+        ensure => present,
+        owner  => 'etcd',
+        group  => 'etcd',
+        mode   => '0400',
+        source => "${settings::ssldir}/certs/ca.pem",
+        notify => Service['etcd'],
+    }
+
+    file { '/var/lib/etcd/ssl/cert.pem':
+        ensure  => present,
+        owner   => 'etcd',
+        group   => 'etcd',
+        mode    => '0400',
+        source  => "${settings::ssldir}/certs/${puppet_cert_name}.pem",
+        require => File['/var/lib/etcd/ssl/ca.pem'],
+        notify  => Service['etcd']
+    }
+
+    file { '/var/lib/etcd/ssl/keys':
+        ensure => directory,
+        owner   => 'etcd',
+        group   => 'etcd',
+        mode    => '0500',
+    }
+
+    file { '/var/lib/etcd/ssl/keys/server.pem':
+        ensure => present,
+        owner  => 'etcd',
+        group  => 'etcd',
+        mode   => '0500',
+        source => "${settings::ssldir}/private_keys/${puppet_cert_name}.pem"
+    }
+}
