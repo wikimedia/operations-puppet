e218684a2d8eb618c3ba6914f90a0f64d7ed7ecb
Ori Livneh <ori@wikimedia.org>
2015-05-05 13:03:15 -0700

    cpufrequtils: ensure configure governor is in use
    * Ensure the configured cpufreq governor is in effect, rather than rely on
      Puppet event notification / boot-time setting.
    * Add documentation.
    * Use 'require_package' as a shorthand for making the package a requirement for
      all the class's resources.
    * Rename 'apply cpufrequtils' to 'set_cpufreq_governor', which is more
      informative and does not contain whitespace.
    
    Change-Id: I0a775fb9fb4a03142dc60946eb3ae0b5e6af606c

--- modules/cpufrequtils/manifests/init.pp
+++ modules/cpufrequtils/manifests/init.pp
@@ -1,14 +1,25 @@
-class cpufrequtils (
-    $governor = 'performance',
-) {
-    package { 'cpufrequtils':
-        ensure => present,
-    }
+# == Class: cpufrequtils
+#
+# This class installs the cpufrequtils package and ensures a configured
+# CPU frequency governor is set.
+#
+# === Parameters
+#
+# [*governor*]
+#   Which governor to use. Defaults to 'performance'. Run 'cpufreq-info -g'
+#   to obtain a list of available governors.
+#
+# === Examples
+#
+#  class { 'cpufrequtils':
+#    governor => 'powersave',
+#  }
+#
+class cpufrequtils( $governor = 'performance' ) {
+    require_package('cpufrequtils')
 
-    # start at boot
     service { 'cpufrequtils':
-        enable  => true,
-        require => Package['cpufrequtils'],
+        enable => true,
     }
 
     # Ubuntu's default initscripts package includes a service called "ondemand"
@@ -23,12 +34,11 @@ class cpufrequtils (
 
     file { '/etc/default/cpufrequtils':
         content => "GOVERNOR=${governor}\n",
-        notify  => Exec['apply cpufrequtils'],
-        require => Package['cpufrequtils'],
+        notify  => Exec['set_cpufreq_governor'],
     }
 
-    exec { 'apply cpufrequtils':
-        command     => '/etc/init.d/cpufrequtils start',
-        refreshonly => true
+    exec { 'set_cpufreq_governor':
+        command => '/etc/init.d/cpufrequtils restart',
+        unless  => "/usr/bin/cpufreq-info -p | /bin/grep -wq ${governor}",
     }
 }
