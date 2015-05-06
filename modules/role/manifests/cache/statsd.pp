a8a6046a9133a1808eddd9e293cead2a3a0156c7
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-05-06 10:48:16 +0200

    role::cache: decommission statsite
    logster traffic hits statsd.eqiad.wmnet already thus a local statsite instance
    is not needed anymore.
    
    Bug: T95687
    Change-Id: I948bfb69978a17dcbf40e7ba33a7449303dcb6b2

--- modules/role/manifests/cache/statsd.pp
+++ modules/role/manifests/cache/statsd.pp
@@ -2,6 +2,5 @@
 # Installs a local statsd instance for aggregating and serializing
 # stats before sending them off to a remote statsd instance.
 class role::cache::statsd {
-    class { '::txstatsd::decommission': }
-    include role::statsite
+    class { '::statsite::decommission': }
 }
