0a39d3c1533e982b71012e21e66c6720ab22bcef
Mukunda Modell <mmodell@wikimedia.org>
2015-05-06 10:41:08 -0500

    Symlink for sprint extension static files.
    Bug: T91207
    
    Change-Id: I29904737b60000398662c51ef2faee64a22d6177

--- modules/phabricator/manifests/libext.pp
+++ modules/phabricator/manifests/libext.pp
@@ -19,20 +19,35 @@
 
 define phabricator::libext ($rootdir, $libext_tag, $libext_lock_path, $libname = $name) {
 
-        # Build per extension lock file
-        $libname_lock = "${libext_lock_path}_${libname}"
+    # Build per extension lock file
+    $libname_lock = "${libext_lock_path}_${libname}"
 
-        git::install { "phabricator/extensions/${libname}" :
-            directory => "${rootdir}/libext/${libname}",
-            git_tag   => $libext_tag,
-            lock_file => $libname_lock,
-            notify    => Exec[$libname_lock],
-            before    => Git::Install['phabricator/phabricator'],
-        }
+    git::install { "phabricator/extensions/${libname}" :
+        directory => "${rootdir}/libext/${libname}",
+        git_tag   => $libext_tag,
+        lock_file => $libname_lock,
+        notify    => Exec[$libname_lock],
+        before    => Git::Install['phabricator/phabricator'],
+    }
 
-        exec {$libname_lock:
-            command => "touch ${libname_lock}",
-            unless  => "test -z ${libname_lock} || test -e ${libname_lock}",
-            path    => '/usr/bin:/bin',
-        }
+    exec {$libname_lock:
+        command => "touch ${libname_lock}",
+        unless  => "test -z ${libname_lock} || test -e ${libname_lock}",
+        path    => '/usr/bin:/bin',
+    }
+
+
+    # symlink static directories for extensions
+    $static_dir = "${rootdir}/libext/${libname}/rsrc/webroot-static"
+
+    exec { "${libname}_static_dir_exists":
+        command => '/bin/true',
+        onlyif => "/usr/bin/test -e $static_dir",
+    }
+
+    file { "${rootdir}/phabricator/webroot/rsrc/${libname}":
+        ensure => 'link',
+        target => $static_dir,
+        require => Exec["${libname}_static_dir_exists"],
+    }
 }
