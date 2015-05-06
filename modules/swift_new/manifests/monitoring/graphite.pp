514f5e6ab801e8a43af6c44f3e284daeed4b891b
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-05-06 15:05:25 +0200

    icinga: unify swift alerts
    Bug: T88974
    Change-Id: Id56a129aa5d509df041c751f8feb16caad1bcd1f

--- modules/swift_new/manifests/monitoring/graphite.pp
+++ modules/swift_new/manifests/monitoring/graphite.pp
@@ -1,9 +1,7 @@
-class swift_new::monitoring::graphite (
-    $swift_cluster = $::swift_new::params::swift_cluster,
-) {
-    monitoring::graphite_threshold { "swift_${swift_cluster}_dispersion_object":
-        description     => "swift ${swift_cluster} object availability",
-        metric          => "keepLastValue(swift.${swift_cluster}.dispersion.object.pct_found)",
+class swift_new::monitoring::graphite {
+    monitoring::graphite_threshold { "swift_${name}_dispersion_object":
+        description     => "swift ${name} object availability",
+        metric          => "keepLastValue(swift.${name}.dispersion.object.pct_found)",
         from            => '1hours',
         warning         => 95,
         critical        => 90,
@@ -11,9 +9,9 @@ class swift_new::monitoring::graphite (
         nagios_critical => false,
     }
 
-    monitoring::graphite_threshold { "swift_${swift_cluster_dispersion_container}":
-        description     => "swift ${swift_cluster} container availability",
-        metric          => "keepLastValue(swift.${swift_cluster}.dispersion.container.pct_found)",
+    monitoring::graphite_threshold { "swift_${name}_dispersion_container":
+        description     => "swift ${name} container availability",
+        metric          => "keepLastValue(swift.${name}.dispersion.container.pct_found)",
         from            => '30min',
         warning         => 92,
         critical        => 88,
