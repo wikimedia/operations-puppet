7dbf601bdf209d8c97419e6ddfb2e342d083b04e
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-05-06 19:02:01 +0300

    puppetmaster: use require_packages
    We 've migrated to puppet 3, so substitute the in-class definition with
    the use of require_package
    
    Change-Id: I4183c1b8ffa2720541a32506b03df081e6bbf2c0

--- modules/puppetmaster/manifests/naggen2.pp
+++ modules/puppetmaster/manifests/naggen2.pp
@@ -1,18 +1,7 @@
 class puppetmaster::naggen2($ensure = 'present'){
 
     $packages = ['python-jinja2', 'python-mysqldb', 'python-sqlalchemy']
-
-    # When migrated to puppet 3, we can substitute this with ensure_resource
-    # for now, we have this ugly define here.
-    define ensure_package($ensure = 'present') {
-        if ! defined(Package[$title]) {
-            package {$title: ensure => $ensure }
-        }
-    }
-
-    puppetmaster::naggen2::ensure_package {$packages:
-        ensure => latest
-    }
+    require_package($packages)
 
     file {'/usr/local/bin/naggen2':
         ensure  => 'present',
