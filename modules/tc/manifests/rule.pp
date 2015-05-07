24c389cb74cbc82db12a66d9711759c8812cdb70
Coren <marc@uberbox.org>
2015-05-07 14:58:34 -0400

    Creat tc class analogous to ferm for traffic control
    Change-Id: I2fdf0421b87acc20e6cb320f25dd765cfa8aa606

---
+++ modules/tc/manifests/rule.pp
@@ -0,0 +1,21 @@
+
+
+define tc::rule(
+    $rule,
+    $iface,
+    $action = 'add',
+    $type   = 'qdisc',
+    $prio   = 10,
+) {
+
+    file { "/etc/tc/${iface}.d/${prio}_${name}":
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0500',
+        content => template('tc/rule.erb'),
+        require => File["/etc/tc/${iface}.d"]
+        notify  => Service['tc'],
+    }
+
+}
+
