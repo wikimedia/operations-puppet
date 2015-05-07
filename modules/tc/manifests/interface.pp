5b4384a471a599e12902cac565eed88648286f8e
Coren <marc@uberbox.org>
2015-05-07 14:58:34 -0400

    Creaet tc class analogous to ferm for traffic control
    Change-Id: I2fdf0421b87acc20e6cb320f25dd765cfa8aa606

---
+++ modules/tc/manifests/interface.pp
@@ -0,0 +1,25 @@
+
+
+define tc::interface($iface) {
+
+    file { "/etc/rc/${iface}.d":
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        recurse => true,
+        purge   => true,
+    }
+
+    # Rules are applied additively; insert a 00_flush
+    # rule to clean the slate before anything else
+
+    tc::rule { 'flush':
+        iface  => $iface,
+        action => 'del',
+        rule   => 'root',
+        prio   => '00',
+    }
+
+}
+
