8c4b2fac8f1fddcf335168154dd3473a02458b87
Andrew Otto <acotto@gmail.com>
2015-05-08 13:44:46 -0400

    Be explicit about spark worker memory in hiera
    Change-Id: I5fb0b2c99517f8e8ebc492e09b197c4957fdaf20

--- manifests/role/analytics/spark.pp
+++ manifests/role/analytics/spark.pp
@@ -10,16 +10,11 @@ class role::analytics::spark {
 # do not include role::analytics::spark
 # and role::analytics::spark::standalone on the same node.
 class role::analytics::spark::standalone {
-    $default_worker_memory = $::realm ? {
-        production => join([$::memorysize_mb - 2048, 'm'], ''),
-        default    => undef,
-    }
-
     class { 'cdh::spark':
         master_host      => hiera('spark_master_host',      $::fqdn),
         worker_instances => hiera('spark_worker_instances', undef),
         worker_cores     => hiera('spark_worker_cores',     floor($::processorcount / hiera('spark_worker_instances', 1))),
-        worker_memory    => hiera('spark_worker_memory',    $default_worker_memory_mb),
+        worker_memory    => hiera('spark_worker_memory',    undef)
     }
 }
 
