ac30915d4ae6a395db1fa4519b3601b92d71be76
Andrew Otto <acotto@gmail.com>
2015-05-08 12:22:50 -0400

    Puppetize standalone cluster on test Ciscos
    This will be used for testing out Spark Streaming
    
    Change-Id: Icea3cf8df94fb3164722d5bd05f5b3f89537189a

--- manifests/role/analytics/spark.pp
+++ manifests/role/analytics/spark.pp
@@ -3,3 +3,32 @@
 class role::analytics::spark {
     include cdh::spark
 }
+
+# == Class role::analytics::spark::standalone
+# Configures a spark standalone cluster.
+# This runs spark daemons outside of YARN.
+# do not include role::analytics::spark
+# and role::analytics::spark::standalone on the same node.
+class role::analytics::spark::standalone {
+    $default_worker_memory = $::realm ? {
+        production => join([$::memorysize_mb - 2048, 'm'], ''),
+        default    => undef,
+    }
+
+    class { 'cdh::spark':
+        master_host      => hiera('spark_master_host',      $::fqdn),
+        worker_instances => hiera('spark_worker_instances', undef),
+        worker_cores     => hiera('spark_worker_cores',     floor($::processorcount / hiera('spark_worker_instances', 1))),
+        worker_memory    => hiera('spark_worker_memory',    $default_worker_memory_mb),
+    }
+}
+
+class role::analytics::spark::standalone::master {
+    require role::analytics::spark::standalone
+    include cdh::spark::master
+}
+
+class role::analytics::spark::standalone::worker {
+    require role::analytics::spark::standalone
+    include cdh::spark::worker
+}
