982d724ad2a9fc6c1714895de776c73159743b53
Tim Landscheidt <tim@tim-landscheidt.de>
2015-05-11 03:26:37 +0000

    Tools: Puppetize database aliases as host resources
    Bug: T63897
    Change-Id: I14b8b4a691f82d8eacfbc7cf5fee6d4d739c7a74

--- modules/toollabs/manifests/init.pp
+++ modules/toollabs/manifests/init.pp
@@ -190,14 +190,6 @@ class toollabs (
         content => $active_proxy,
     }
 
-    # puppetized until we can setup proper DNS for .labsdb entries
-    file { '/etc/hosts':
-        source => 'puppet:///modules/toollabs/hosts',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0644'
-    }
-
     # Silence e-mails sent when regular users try to sudo (T95882)
     file { '/etc/sudoers.d/40-tools-sudoers-no-warning':
         ensure => file,
@@ -205,6 +197,13 @@ class toollabs (
         owner  => 'root',
         group  => 'root',
         source => 'puppet:///modules/toollabs/40-tools-sudoers-no-warning',
+    }
+
+    # Set up aliases for Tools database server.
+    host { 'tools.labsdb':
+        ensure       => present,
+        host_aliases => 'tools-db',
+        ip           => '10.64.37.9',
     }
 
     diamond::collector::localcrontab { 'localcrontabcollector': }
