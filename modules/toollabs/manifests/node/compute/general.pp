f28986238acc4a50adbc4bab26fffb54197ed33e
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2015-05-14 17:08:45 +0200

    tools: make seperate /tmp hiera-configurable
    This enables us to re-enable puppet on specialty exec hosts without
     a) requiring them to be m1.large, and
     b) requiring a restart + manual fix of /tmp
    
    Bug: T99069
    Bug: T97445
    Change-Id: I1e55facacd9082ab57baaa1b91b60061c7b58244

--- modules/toollabs/manifests/node/compute/general.pp
+++ modules/toollabs/manifests/node/compute/general.pp
@@ -10,7 +10,9 @@
 #
 # Sample Usage:
 #
-class toollabs::node::compute::general {
+class toollabs::node::compute::general (
+    $separate_tmp = true,
+){
 
     system::role { 'toollabs::node::compute::general': description => 'General computation node' }
 
@@ -28,16 +30,21 @@ class toollabs::node::compute::general {
         source => 'puppet:///modules/toollabs/jobkill',
     }
 
-    # 16G /tmp for everyone! Note that we need new nodes to be at least a large (80G total space)
-    labs_lvm::volume { 'separate-tmp':
-        size      => '16GB',
-        mountat   => '/tmp',
-        mountmode => '1777',
-        options   => 'nosuid,noexec,nodev,rw',
+    if $separate_tmp {
+        # add seperate 16G /tmp! Note that this needs nodes to be at least
+        # m1.large (80G total space). Old smaller nodes can keep their
+        # /tmp on / by providing 
+        #   toollabs::node::compute::general::separate_tmp: false
+        # via hiera.
+        labs_lvm::volume { 'separate-tmp':
+            size      => '16GB',
+            mountat   => '/tmp',
+            mountmode => '1777',
+            options   => 'nosuid,noexec,nodev,rw',
+        }   
     }
-
+    
     labs_lvm::swap { 'big':
         size => inline_template('<%= @memorysize_mb.to_i * 3 %>MB'),
     }
-
 }
