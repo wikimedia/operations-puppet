c05e2a3eb372cce0a16c50aa8598030265e82764
Andrew Bogott <abogott@wikimedia.org>
2015-05-14 23:00:25 +0000

    Added a simple IP-aliasing script for the pdns recursor.
    Bug T95288
    
    Change-Id: Ib90776608d140db4aa8298bbef1b14256d56afb7

--- manifests/role/labsdns.pp
+++ manifests/role/labsdns.pp
@@ -1,4 +1,5 @@
 class role::labsdns {
+    system::role { 'role::labsdns': description => 'DNS server for Labs instances' }
     include passwords::pdns
 
     class { '::labs_dns':
@@ -10,6 +11,69 @@ class role::labsdns {
         pdns_recursor          => '208.80.154.239',
         recursor_ip_range      => '10.68.16.0/21',
     }
+
+    ferm::service { 'udp_dns_rec':
+        proto => 'udp',
+        port  => '53',
+    }
+
+    ferm::service { 'tcp_dns_rec':
+        proto => 'tcp',
+        port  => '53',
+    }
+
+    ferm::rule { 'skip_dns_conntrack-out':
+        desc  => 'Skip DNS outgoing connection tracking',
+        table => 'raw',
+        chain => 'OUTPUT',
+        rule  => 'proto udp sport 53 NOTRACK;',
+    }
+
+    ferm::rule { 'skip_dns_conntrack-in':
+        desc  => 'Skip DNS incoming connection tracking',
+        table => 'raw',
+        chain => 'PREROUTING',
+        rule  => 'proto udp dport 53 NOTRACK;',
+    }
+}
+
+class role::labsdnsrecursor {
+    system::role { 'role::labsdnsrecursor': description => 'Recursive DNS server for Labs instances' }
+
+    $nova_dnsmasq_aliases = {
+        # eqiad
+        'deployment-cache-text02'   => {public_ip  => '208.80.155.135',
+                                        private_ip => '10.68.16.16' },
+        'deployment-cache-upload02' => {public_ip  => '208.80.155.136',
+                                        private_ip => '10.68.17.51' },
+        'deployment-cache-bits01'   => {public_ip  => '208.80.155.137',
+                                        private_ip => '10.68.16.12' },
+        'deployment-stream'         => {public_ip  => '208.80.155.138',
+                                        private_ip => '10.68.17.106' },
+        'deployment-cache-mobile03' => {public_ip  => '208.80.155.139',
+                                        private_ip => '10.68.16.13' },
+        'relic'                     => {public_ip  => '208.80.155.197',
+                                        private_ip => '10.68.16.162' },
+        'tools-webproxy'            => {public_ip  => '208.80.155.131',
+                                        private_ip => '10.68.17.139' },
+        'udplog'                    => {public_ip  => '208.80.155.191',
+                                        private_ip => '10.68.16.58' },
+
+        # A wide variety of hosts are reachable via a public web proxy.
+        'labs_shared_proxy' => {public_ip  => '208.80.155.156',
+                                private_ip => '10.68.16.65'},
+    }
+
+    class { ::dnsrecursor:
+            listen_addresses    => $::realm ? {
+                                     'labs' => [$::ipaddress],
+                                     default => [$::ipaddress, $::ipaddress6_eth0]
+                                   },
+            allow_from          => ['10.68.16.0/21'],
+            ip_aliases          => $nova_dnsmasq_aliases
+    }
+
+    ::dnsrecursor::monitor { [ $::ipaddress, $::ipaddress6_eth0 ]: }
 
     ferm::service { 'udp_dns_rec':
         proto => 'udp',
