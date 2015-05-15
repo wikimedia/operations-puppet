c748b6e6886cebd0a8353518437897eedd24a6ac
dzahn <dzahn@wikimedia.org>
2015-05-15 16:19:36 -0700

    install-server: indentation fixes
    fixes a bunch of "WARNING: indentation of => is not properly aligned"
    for T93645
    
    Change-Id: I1cc1acef351a8d4ebecdb1576e153772b45534fc

--- modules/install-server/manifests/apt-repository.pp
+++ modules/install-server/manifests/apt-repository.pp
@@ -25,46 +25,46 @@ class install-server::apt-repository {
     # TODO: add something that sets up /etc/environment for reprepro
 
     file { '/srv/wikimedia':
-        ensure  => directory,
-        mode    => '0755',
-        owner   => 'root',
-        group   => 'root',
+        ensure => directory,
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
     }
 
     # reprepro configuration
     file { '/srv/wikimedia/conf':
-        ensure  => directory,
-        mode    => '0755',
-        owner   => 'root',
-        group   => 'root',
+        ensure => directory,
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
     }
 
     file { '/srv/wikimedia/conf/log':
-        ensure  => present,
-        mode    => '0755',
-        owner   => 'root',
-        group   => 'root',
-        source  => 'puppet:///modules/install-server/reprepro/log',
+        ensure => present,
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/install-server/reprepro/log',
     }
     file { '/srv/wikimedia/conf/distributions':
-        ensure  => present,
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        source  => 'puppet:///modules/install-server/reprepro/distributions',
+        ensure => present,
+        mode   => '0444',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/install-server/reprepro/distributions',
     }
     file { '/srv/wikimedia/conf/updates':
-        ensure  => present,
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        source  => 'puppet:///modules/install-server/reprepro/updates',
+        ensure => present,
+        mode   => '0444',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/install-server/reprepro/updates',
     }
     file { '/srv/wikimedia/conf/incoming':
-        ensure  => present,
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        source  => 'puppet:///modules/install-server/reprepro/incoming',
+        ensure => present,
+        mode   => '0444',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/install-server/reprepro/incoming',
     }
 }
