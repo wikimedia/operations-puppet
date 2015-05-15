15b8df9a43de65cd2bf9e4f060d0ca41391a8f9c
Andrew Otto <acotto@gmail.com>
2015-05-15 10:18:36 -0400

    Rsync CirrusSearchRequests.log from fluorine to stat1002
    Bug: T98383
    Change-Id: I11e48d07b00ba36cb510839dd23fd1a14af340bb

---
+++ modules/statistics/manifests/rsync/mediawiki.pp
@@ -0,0 +1,27 @@
+# == Class statistics::rsync::mediawiki
+#
+# Sets up daily cron jobs to rsync log files from remote
+# logging hosts to a local destination for further processing.
+#
+class statistics::rsync::mediawiki {
+    Class['::statistics'] -> Class['::statistics::rsync::mediawiki']
+    $working_path = $::statistics::working_path
+
+    # Any logs older than this will be pruned by
+    # the rsync_job define.
+    $retention_days = 90
+
+    file { ["${working_path}/mw-log", "${working_path}/mw-log/archive", :
+        ensure  => 'directory',
+        owner   => 'stats',
+        group   => 'wikidev',
+        mode    => '0775',
+    }
+
+    # Search request logs from fluorine
+    statistics::rsync_job { 'CirrusSearchRequests':
+        source         => 'fluorine.eqiad.wmnet::mw-log/archive/CirrusSearchRequests.*.gz',
+        destination    => "${working_path}/mw-log/archive",
+        retention_days => $retention_days,
+    }
+}
