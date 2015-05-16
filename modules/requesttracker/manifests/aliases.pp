d288cffd8338526a0aa533aa53ccf1caac5eac9a
dzahn <dzahn@wikimedia.org>
2015-05-15 17:47:26 -0700

    monitoring, RT: lint fixes
    unquoted file modes and users, double quoted
    strings without variables
    
    fixes more puppet-lint warnings
    for T93645
    
    Change-Id: I19dec3c1c3e3de33a1677d3bec9a0f7851b3253b

--- modules/requesttracker/manifests/aliases.pp
+++ modules/requesttracker/manifests/aliases.pp
@@ -1,8 +1,8 @@
 class requesttracker::aliases {
     file { '/etc/exim4/aliases/rt':
-        owner   => root,
-        group   => root,
-        mode    => 0444,
-        source  => 'puppet:///modules/requesttracker/rt.aliases',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/requesttracker/rt.aliases',
     }
 }
