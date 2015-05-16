b3438ef7975bd19649730d36611912e0c98763b6
dzahn <dzahn@wikimedia.org>
2015-05-15 19:18:52 -0700

    openstack: lint fixes
    "double quoted string containing no variables"
    "indentation of => is not properly aligned"
    
    and misc.
    
    fixes more puppet-lint warnings
    for T93645
    
    Change-Id: I4e3bd90a402bcecf12b64af5d6ad8fa29df6f358

--- modules/openstack/manifests/glance/service.pp
+++ modules/openstack/manifests/glance/service.pp
@@ -14,54 +14,54 @@ class openstack::glance::service(
         mode    => '0775',
     }
 
-    package { [ "glance" ]:
+    package { 'glance':
         ensure  => present,
-        require => Class["openstack::repo"];
+        require => Class['openstack::repo'],
     }
 
-    service { "glance-api":
+    service { 'glance-api':
         ensure  => running,
-        require => Package["glance"];
+        require => Package['glance'],
     }
 
-    service { "glance-registry":
+    service { 'glance-registry':
         ensure  => running,
-        require => Package["glance"];
+        require => Package['glance'],
     }
 
     file {
-        "/etc/glance/glance-api.conf":
+        '/etc/glance/glance-api.conf':
             content => template("openstack/${$openstack_version}/glance/glance-api.conf.erb"),
             owner   => 'glance',
             group   => nogroup,
-            notify  => Service["glance-api"],
-            require => Package["glance"],
+            notify  => Service['glance-api'],
+            require => Package['glance'],
             mode    => '0440';
-        "/etc/glance/glance-registry.conf":
+        '/etc/glance/glance-registry.conf':
             content => template("openstack/${$openstack_version}/glance/glance-registry.conf.erb"),
             owner   => 'glance',
             group   => nogroup,
-            notify  => Service["glance-registry"],
-            require => Package["glance"],
+            notify  => Service['glance-registry'],
+            require => Package['glance'],
             mode    => '0440';
     }
-    if ($openstack_version == "essex") {
+    if ($openstack_version == 'essex') {
         # Keystone config was (thankfully) moved out of the paste config
         # So, past essex we don't need to change these.
         file {
-            "/etc/glance/glance-api-paste.ini":
+            '/etc/glance/glance-api-paste.ini':
                 content => template("openstack/${$openstack_version}/glance/glance-api-paste.ini.erb"),
                 owner   => 'glance',
                 group   => 'glance',
-                notify  => Service["glance-api"],
-                require => Package["glance"],
+                notify  => Service['glance-api'],
+                require => Package['glance'],
                 mode    => '0440';
-            "/etc/glance/glance-registry-paste.ini":
+            '/etc/glance/glance-registry-paste.ini':
                 content => template("openstack/${$openstack_version}/glance/glance-registry-paste.ini.erb"),
                 owner   => 'glance',
                 group   => 'glance',
-                notify  => Service["glance-registry"],
-                require => Package["glance"],
+                notify  => Service['glance-registry'],
+                require => Package['glance'],
                 mode    => '0440';
         }
     }
