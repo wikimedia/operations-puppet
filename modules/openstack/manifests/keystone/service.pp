057392516df132f368e082a8869039a976d37b78
dzahn <dzahn@wikimedia.org>
2015-05-15 19:18:52 -0700

    openstack: lint fixes
    "double quoted string containing no variables"
    "indentation of => is not properly aligned"
    
    and misc.
    
    fixes more puppet-lint warnings
    for T93645
    
    Change-Id: Iee2fd2441936f1c782534f66e86b9ec5655ddead

--- modules/openstack/manifests/keystone/service.pp
+++ modules/openstack/manifests/keystone/service.pp
@@ -1,31 +1,31 @@
 class openstack::keystone::service($openstack_version=$::openstack::version, $keystoneconfig, $glanceconfig) {
     include openstack::repo
 
-    package { [ "keystone" ]:
+    package { [ 'keystone' ]:
         ensure  => present,
-        require => Class["openstack::repo"];
+        require => Class['openstack::repo'];
     }
 
     if $keystoneconfig['token_driver'] == 'redis' {
-        package { [ "python-keystone-redis" ]:
+        package { [ 'python-keystone-redis' ]:
             ensure => present;
         }
     }
 
 
-    service { "keystone":
+    service { 'keystone':
         ensure    => running,
         subscribe => File['/etc/keystone/keystone.conf'],
-        require   => Package["keystone"];
+        require   => Package['keystone'];
     }
 
     file {
-        "/etc/keystone/keystone.conf":
+        '/etc/keystone/keystone.conf':
             content => template("openstack/${openstack_version}/keystone/keystone.conf.erb"),
             owner   => keystone,
             group   => keystone,
-            notify  => Service["keystone"],
-            require => Package["keystone"],
+            notify  => Service['keystone'],
+            require => Package['keystone'],
             mode    => '0440';
     }
 }
