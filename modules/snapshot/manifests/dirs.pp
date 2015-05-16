48029ecb43801eedb520903ae25bed7e43e77c85
dzahn <dzahn@wikimedia.org>
2015-05-15 18:27:35 -0700

    snapshot: lint fixes
    "indentation of => is not properly aligned"
    "ensure found on line but it's not the first attribute"
    
    fixes more puppet-lint warnings
    for T93645
    
    Change-Id: I2af3270989fe4b8d6a5b68a87a79c69d8a4bebdb

--- modules/snapshot/manifests/dirs.pp
+++ modules/snapshot/manifests/dirs.pp
@@ -4,31 +4,31 @@ class snapshot::dirs {
     $apachedir = '/srv/mediawiki'
 
     file { $dumpsdir:
-        ensure  => 'directory',
-        path    => $dumpsdir,
-        mode    => '0644',
-        owner   => 'root',
-        group   => 'root',
+        ensure => 'directory',
+        path   => $dumpsdir,
+        mode   => '0644',
+        owner  => 'root',
+        group  => 'root',
     }
 
     $addschangesdir = '/srv/addschanges'
 
     file { $addschangesdir:
-        ensure  => 'directory',
-        path    => $addschangesdir,
-        mode    => '0644',
-        owner   => 'root',
-        group   => 'root',
+        ensure => 'directory',
+        path   => $addschangesdir,
+        mode   => '0644',
+        owner  => 'root',
+        group  => 'root',
     }
 
     $wikiqueriesdir = '/srv/wikiqueries'
 
     file { $wikiqueriesdir:
-        ensure  => 'directory',
-        path    => $wikiqueriesdir,
-        mode    => '0644',
-        owner   => 'root',
-        group   => 'root',
+        ensure => 'directory',
+        path   => $wikiqueriesdir,
+        mode   => '0644',
+        owner  => 'root',
+        group  => 'root',
     }
 
 }
