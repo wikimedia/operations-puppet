94d7cbec18790c5c9154360f86902bdc0b1a231c
Andrew Otto <acotto@gmail.com>
2015-05-19 15:49:59 -0400

    Add varnish request stats diamond collector
    This will run varnishlog (python version) in a background thread and maintain a dict
    of HTTP request counts grouped by things like http status and http method.
    
    Bug: T83580
    Change-Id: Icbe6c7d4185cd59f2e0b0626b9579180a828fcc7

---
+++ modules/varnish/manifests/monitoring/varnishreqstats.pp
@@ -0,0 +1,28 @@
+# == Define varnish::monitoring::varnishreqstats:
+# Installs a Diamond collector that sends varnish request stats to statsd/graphite.
+#
+# === Parameters
+# [*instance_name*]
+#   Name of the varnish instance from which to gather request stats.
+#   Default: $name.
+#
+#
+# [*metric_path*]
+#   Prefix of the stats that will be sent to statsd/graphite.
+#   Default: varnish.$site.$instance_name.request.
+#   This will create graphite keys that look like:
+#   servers.cp1052.varnish.eqiad.text.frontend.request.client.status.2xx
+#
+define varnish::monitoring::varnishreqstats(
+    $instance_name = $name,
+    $metric_path   = "varnish.${::site}.${name}.request",
+) {
+    diamond::collector { "varnishreqstats-${name}":
+        source   => 'puppet:///modules/varnish/varnishreqstats-diamond.py',
+        custom_name => 'varnishreqstats',
+        settings => {
+            'varnish_name' => $instance_name,
+            'path'         => $metric_path,
+        },
+    }
+}
