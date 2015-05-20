c3fdfb4a1c477f861a1285e793a221307858ae0c
dzahn <dzahn@wikimedia.org>
2015-05-19 17:50:20 -0700

    fundraising.pp: indentation fixes
    Change-Id: I07c2167d89ea0e4f1e841892efd7c292ec16055e

--- manifests/misc/fundraising.pp
+++ manifests/misc/fundraising.pp
@@ -6,17 +6,17 @@ class misc::fundraising::udp2log_rotation {
         privileges => ['ALL = NOPASSWD: /usr/bin/killall -HUP udp2log'] }
 
     file { '/usr/local/bin/rotate_fundraising_logs':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0555',
-        source  => 'puppet:///files/misc/scripts/rotate_fundraising_logs',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///files/misc/scripts/rotate_fundraising_logs',
     }
 
     file { '/a/log/fundraising/logs/buffer':
-        ensure  => directory,
-        owner   => 'file_mover',
-        group   => 'wikidev',
-        mode    => '0750',
+        ensure => directory,
+        owner  => 'file_mover',
+        group  => 'wikidev',
+        mode   => '0750',
     }
 
     cron { 'rotate_fundraising_logs':
