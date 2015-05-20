03f10b2705f7b6c10d5cdfdbda2d79c111d22545
Ariel T. Glenn <ariel@wikimedia.org>
2015-05-20 17:43:52 +0300

    git deploy: don't fetch/checkout/restart on the deployment server
    Change-Id: Ib206f3820e5aa11ff6d26e777609b5692f74dd4f

--- modules/deployment/files/runners/deploy.py
+++ modules/deployment/files/runners/deploy.py
@@ -12,12 +12,14 @@ def fetch(repo):
     '''
     Fetch from a master, for the specified repo
     '''
-    grain = "deployment_target:" + repo
+    deployment_target = "deployment_target:" + repo
+    deployment_server = "deployment_server:*"
+    targets = "G@{0} and not G@{1}".format(deployment_target, deployment_server)
     client = salt.client.LocalClient(__opts__['conf_file'])
     cmd = 'deploy.fetch'
     # comma in the tuple is a workaround for a bug in salt
     arg = (repo,)
-    client.cmd(grain, cmd, expr_form='grain', arg=arg, timeout=1,
+    client.cmd(targets, cmd, expr_form='compound', arg=arg, timeout=1,
                ret='deploy_redis')
     print "Fetch completed"
 
@@ -26,11 +28,13 @@ def checkout(repo, reset=False):
     '''
     Checkout from a master, for the specified repo
     '''
-    grain = "deployment_target:" + repo
+    deployment_target = "deployment_target:" + repo
+    deployment_server = "deployment_server:*"
+    targets = "G@{0} and not G@{1}".format(deployment_target, deployment_server)
     client = salt.client.LocalClient(__opts__['conf_file'])
     cmd = 'deploy.checkout'
     arg = (repo, reset)
-    client.cmd(grain, cmd, expr_form='grain', arg=arg, timeout=1,
+    client.cmd(targets, cmd, expr_form='compound', arg=arg, timeout=1,
                ret='deploy_redis')
     print "Checkout completed"
 
@@ -40,13 +44,15 @@ def restart(repo, batch='10%'):
     Restart the service associated with this repo. If no service is associated
     this call will do nothing.
     '''
-    grain = "deployment_target:" + repo
+    deployment_target = "deployment_target:" + repo
+    deployment_server = "deployment_server:*"
+    targets = "G@{0} and not G@{1}".format(deployment_target, deployment_server)
     client = salt.client.LocalClient(__opts__['conf_file'])
     cmd = 'deploy.restart'
     # comma in the tuple is a workaround for a bug in salt
     arg = (repo,)
     ret = []
-    for data in client.cmd_batch(grain, cmd, expr_form='grain', arg=arg,
+    for data in client.cmd_batch(targets, cmd, expr_form='compound', arg=arg,
                                  timeout=60, ret='deploy_redis', batch=batch):
         ret.append(data)
     print "Restart completed"
