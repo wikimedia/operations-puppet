d2f856d35e04dc6ccc054410f32e9845d4ca4bc3
Brandon Black <bblack@wikimedia.org>
2015-05-20 07:40:32 +0000

    enable OCSP for misc cluster certs T97506
    Change-Id: Ie804f1d3a1a46badcf5b0fc16b3c81b4b3b6d950

--- modules/role/manifests/cache/ssl/misc.pp
+++ modules/role/manifests/cache/ssl/misc.pp
@@ -6,6 +6,7 @@ class role::cache::ssl::misc {
     include role::protoproxy::ssl::common
 
     role::cache::ssl::local { 'wikimedia.org':
+        do_ocsp        => true,
         certname       => 'sni.wikimedia.org',
         server_name    => 'wikimedia.org',
         server_aliases => ['*.wikimedia.org'],
@@ -13,12 +14,14 @@ class role::cache::ssl::misc {
     }
 
     role::cache::ssl::local { 'wmfusercontent.org':
+        do_ocsp        => true,
         certname       => 'star.wmfusercontent.org',
         server_name    => 'wmfusercontent.org',
         server_aliases => ['*.wmfusercontent.org'];
     }
 
     role::cache::ssl::local { 'planet.wikimedia.org':
+        do_ocsp        => true,
         certname       => 'star.planet.wikimedia.org',
         server_name    => 'planet.wikimedia.org',
         server_aliases => ['*.planet.wikimedia.org'];
