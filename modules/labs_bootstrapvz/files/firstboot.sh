6e19b74a3674155140cf07f6c9acd69f7346cd59
andrewbogott <abogott@wikimedia.org>
2015-05-20 16:08:29 -0500

    Replace many references to virt1000 and labcontrol2001 with hiera lookups
    Requires a dns patch as well:  https://gerrit.wikimedia.org/r/#/c/214079/
    
    Change-Id: Ic2b08ef592719a4d4cbefe47f34b65a85cad8f13

--- modules/labs_bootstrapvz/files/firstboot.sh
+++ modules/labs_bootstrapvz/files/firstboot.sh
@@ -79,7 +79,7 @@ project=`ldapsearch -x -D ${binddn} -w ${bindpw} -b ${hostsou} "dc=${idfqdn}" pu
 saltfinger="c5:b1:35:45:3e:0a:19:70:aa:5f:3a:cf:bf:a0:61:dd"
 if [ "${domain}" == "eqiad.wmflabs" ]
 then
-	master="virt1000.wikimedia.org"
+	master="labs-puppetmaster.wikimedia.org"
 	master_secondary="labcontrol2001.wikimedia.org"
 fi
 
