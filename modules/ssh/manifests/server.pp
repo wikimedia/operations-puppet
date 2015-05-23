47f5c988c12815ae45dd0ea0545fd3e61bacd3b5
YuviPanda <yuvipanda@gmail.com>
2015-05-23 15:10:55 +0200

    ssh: Allow temporary opt out from more secure ssh
    Complaints on IRC and in person about people unable to ssh to
    labs, even on not-so-out-of-date OSes (Debian Squeeze). This allows
    labs projects to opt out of the more secure ssh defaults temporarily
    while announcements and user education is made.
    
    Change-Id: Ib9f3c3ab8e69a6d5aeeb73bfc4c5d7578e6f0703

--- modules/ssh/manifests/server.pp
+++ modules/ssh/manifests/server.pp
@@ -3,6 +3,8 @@ class ssh::server (
     $listen_address = undef,
     $permit_root = true,
     $authorized_keys_file = undef,
+    $disable_nist_kex = true, # Allow labs projects to temporarily opt out of nist kex disabling
+    $explicit_macs = true, # Allow labs projects to temporarily opt out of more secure MACs
 ) {
     package { 'openssh-server':
         ensure => latest;
