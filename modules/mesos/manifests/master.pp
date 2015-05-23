3cb8112f35dd95799adaa195e40073d5fee75d28
YuviPanda <yuvipanda@gmail.com>
2015-05-24 01:50:27 +0200

    mesos: Setup marathon properly
    - Make sure the service is running
    - Add /etc/default/marathon for setting up ENV variables
    - Setup /etc/default/marathon-private for unpuppetized private
      variables like HTTP credentials
    
    Change-Id: I84d6c93051a601b487f067d87b660f9589020b05

--- modules/mesos/manifests/master.pp
+++ modules/mesos/manifests/master.pp
@@ -3,7 +3,7 @@ class mesos::master(
     $quorum,
 ) {
 
-    require_package('mesos')
+    require_package('mesos', 'marathon')
 
     file { '/etc/mesos/zk':
         content => $zookeeper_url,
@@ -43,5 +43,25 @@ class mesos::master(
 
     service { 'mesos-slave':
         ensure => stopped,
+    }
+
+    file { '/etc/init/marathon.conf':
+        source => 'puppet:///modules/mesos/marathon.conf',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        notify => Service['marathon']
+    }
+
+    file { '/etc/default/marathon':
+        content => template('mesos/marathon.default.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['marathon'],
+    }
+
+    service { 'marathon':
+        ensure => running,
     }
 }
