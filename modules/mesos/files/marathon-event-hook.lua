3ff7e2d7e58b0a47216269b7a1c1ba01aa758c10
YuviPanda <yuvipanda@gmail.com>
2015-05-24 02:18:14 +0200

    mesos: Setup a marathon event reciever on mesos proxy
    And make marathon post events to it
    Change-Id: I2c93bb37dc65bb64af63a91374c8fdfdcc49ea94

---
+++ modules/mesos/files/marathon-event-hook.lua
@@ -0,0 +1,34 @@
+-- 
+--
+
+local redis = require 'resty.redis'
+local json = require 'cjson'
+local red = redis:new()
+red:set_timeout(1000)
+
+red:connect('127.0.0.1', 6379)
+
+ngx.req.read_body()
+local raw_event_data = ngx.req.get_body_data()
+
+local data = json.decode(raw_event_data)
+
+if data['taskStatus'] == 'TASK_RUNNING' then
+    key = 'prefix:' .. data['appId']:gsub('/', '', 1)
+    host = data['host']
+    port = data['ports'][1]
+    proxy_url = 'http://' .. host .. ':' .. tostring(port)
+    red:sadd(key, proxy_url)
+else if data['taskStatus'] == 'TASK_KILLED' then:
+    key = 'prefix:' .. data['appId']:gsub('/', '', 1)
+    host = data['host']
+    port = data['ports'][1]
+    proxy_url = 'http://' .. host .. ':' .. tostring(port)
+    red:srem(key, proxy_url)
+end
+
+
+
+-- Use a connection pool of 256 connections with a 32s idle timeout
+-- This also closes the current redis connection.
+red:set_keepalive(1000 * 32, 256)
