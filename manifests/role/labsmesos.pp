86774adb5fbb320bfa90eeee3fe98eb235cde49b
YuviPanda <yuvipanda@gmail.com>
2015-05-24 02:18:14 +0200

    mesos: Setup a marathon event reciever on mesos proxy
    And make marathon post events to it
    Change-Id: I2c93bb37dc65bb64af63a91374c8fdfdcc49ea94

--- manifests/role/labsmesos.pp
+++ manifests/role/labsmesos.pp
@@ -3,10 +3,13 @@ class role::labs::mesos::master {
 
     $zookeeper_hosts = join(suffix(keys(hiera('zookeeper_hosts')), ':2181'), ',')
     $zookeeper_url = "zk://${zookeeper_hosts}/mesos"
+    $proxy_host = hiera('proxy_host', 'marathon-proxy-01')
+    $proxy_hook_url = "http://${proxy_host}:8081/receive-hook"
 
     class { '::mesos::master':
-        zookeeper_url => $zookeeper_url,
-        quorum       => hiera('quorum', 1),
+        zookeeper_url  => $zookeeper_url,
+        quorum         => hiera('quorum', 1),
+        proxy_hook_url => $proxy_hook_url,
     }
 }
 
@@ -22,7 +25,5 @@ class role::labs::mesos::slave {
 }
 
 class role::labs::mesos::proxy {
-    class { '::dynamicproxy':
-        luahandler   => 'redundanturlproxy',
-    }
+    include ::mesos::proxy
 }
