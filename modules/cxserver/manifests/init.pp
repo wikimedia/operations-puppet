b2300369195136a804b1739afe5f8de0698fffe3
Kartik Mistry <kartik.mistry@gmail.com>
2015-05-26 21:06:06 +0530

    CX: Log to logstash
    Bug: T89265
    Depends on: If4cea7fe90d72dedd6420fb0a6d8721b4b7598c3
    
    Change-Id: I7b9de5b0143a2420b8c2caf4f33d25f978c3fb4f

--- modules/cxserver/manifests/init.pp
+++ modules/cxserver/manifests/init.pp
@@ -16,6 +16,10 @@
 # [*log_dir*]
 #   Place where cxserver can put log files. Assumed to be already existing and
 #   have write access to cxserver user.
+# [*logstash_host*]
+#   GELF logging host. Default: localhost
+# [*logstash_port*]
+#   GELF logging port. Default: 12201
 # [*parsoid*]
 #   Url to Parsoid service.
 # [*restbase*]
@@ -37,6 +41,8 @@ class cxserver(
     $node_path = '/srv/deployment/cxserver/deploy/node_modules',
     $conf_path = '/srv/deployment/cxserver/deploy/src/config.js',
     $log_dir = '/var/log/cxserver',
+    $logstash_host  = undef,
+    $logstash_port  = 12201,
     $parsoid = 'http://parsoid-lb.eqiad.wikimedia.org',
     $restbase = 'https://@lang.wikipedia.org/api/rest_v1/page/html/@title',
     $apertium = 'http://apertium.svc.eqiad.wmnet:2737',
@@ -67,13 +73,12 @@ class cxserver(
         system     => true,
     }
 
-    $log_file = "${log_dir}/main.log"
-
     file { $log_dir:
         ensure => directory,
         owner  => 'cxserver',
         group  => 'cxserver',
         mode   => '0775',
+        before => Service['cxserver'],
     }
 
     file { $conf_path:
