3a7d0a3d47ad90943c95ca7f86890400f1c3fb1c
YuviPanda <yuvipanda@gmail.com>
2015-05-27 15:20:12 +0100

    ores: Add simple nginx load balancer
    Change-Id: Iaf033a6de308e88ccadf9a4e131ae232926a571f

--- manifests/role/labsores.pp
+++ manifests/role/labsores.pp
@@ -1,3 +1,11 @@
 class role::labs::ores::web {
     include ::ores::web
 }
+
+class role::labs::ores::lb(
+    $realservers,
+) {
+    class { '::ores::lb':
+        realserveres => $realservers,
+    }
+}
