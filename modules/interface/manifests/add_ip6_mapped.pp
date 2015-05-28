cf067c28c5384dfcf678847765599ef4500256e3
Brandon Black <bblack@wikimedia.org>
2015-05-28 14:13:45 +0000

    Deploy v6 token approach to some jessie/trusty hosts
    Change-Id: I3cb9332962335fad248c027790c8a13646406a8e

--- modules/interface/manifests/add_ip6_mapped.pp
+++ modules/interface/manifests/add_ip6_mapped.pp
@@ -28,8 +28,8 @@ define interface::add_ip6_mapped($interface=undef, $ipv4_address=undef) {
             prefixlen => '64'
         }
 
-        # XXX Temporary: testing on cp1008 first in a couple of different scenarios...
-        if $::hostname == 'cp1008' {
+        # XXX Temporary: testing on jessie/trusty hostnames ending in the digit 1
+        if $::hostname =~ /1$/ {
 
         # The above sets up an "up" command to add the fixed IPv6 mapping of the v4
         # address statically, and also executes the command to add the address
