0f18bc4add1d595d176928621fbb655ed5b70224
Faidon Liambotis <faidon@wikimedia.org>
2015-05-28 17:52:51 +0300

    ssh: guard ssh_known_hosts generation behind $::realm
    An if $::realm makes me sad but this needs to be fixed ASAP.
    
    Change-Id: I4d36e86559d45c7f7406008ac9acfe44eb92c4db

--- modules/ssh/manifests/client.pp
+++ modules/ssh/manifests/client.pp
@@ -3,12 +3,14 @@ class ssh::client {
         ensure => latest,
     }
 
-    file { '/etc/ssh/ssh_known_hosts':
-        content => generate('/usr/local/bin/sshknowngen'),
-        backup  => false,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
+    # no exported resources on Labs == no sshknowngen
+    if $::realm == 'production' {
+        file { '/etc/ssh/ssh_known_hosts':
+            content => generate('/usr/local/bin/sshknowngen'),
+            backup  => false,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+        }
     }
-
 }
