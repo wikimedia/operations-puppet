a361072135f42aedadbef924076b067f1da5d9a4
Faidon Liambotis <faidon@wikimedia.org>
2015-05-28 17:28:33 +0300

    sudo: fix sudo::user/group's ensure => absent
    We don't currently use ensure => absent for sudo::user or sudo::group
    anywhere yet, but it was broken (the template gets evaluated anyway and
    borks if there are no privileges defined) and needed fixing anyway.
    
    Change-Id: I706e891c8ce7d858d8ef59a1fabbc03a84ac2858

--- modules/sudo/manifests/user.pp
+++ modules/sudo/manifests/user.pp
@@ -32,20 +32,24 @@ define sudo::user(
     $title_safe = regsubst($title, '\W', '-', 'G')
     $filename = "/etc/sudoers.d/${title_safe}"
 
-    file { $filename:
-        ensure  => $ensure,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0440',
-        content => template('sudo/sudoers.erb'),
-    }
-
     if $ensure == 'present' {
+        file { $filename:
+            ensure  => $ensure,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0440',
+            content => template('sudo/sudoers.erb'),
+        }
+
         exec { "sudo_user_${title}_linting":
             command     => "/bin/rm -f ${filename} && /bin/false",
             unless      => "/usr/sbin/visudo -cqf ${filename}",
             refreshonly => true,
             subscribe   => File[$filename],
+        }
+    } else {
+        file { $filename:
+            ensure => $ensure,
         }
     }
 }
