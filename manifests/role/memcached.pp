b253adddb3febbdf1b8c8b9623453dcaf3507c8d
Ori Livneh <ori@wikimedia.org>
2015-05-29 13:56:07 -0700

    Log a 20s sample of memcached usage to a file once a day
    Run memkeys once a day, for 20 seconds, at a random minute / hour on each host,
    logging the output to a file. This way, whenever we spot a spike in memcached
    usage, we can diff logs to isolate the culrpits.
    
    Change-Id: Ic1c8f10ff6f934d46b84cb779577441cfd5e1c4f

--- manifests/role/memcached.pp
+++ manifests/role/memcached.pp
@@ -35,7 +35,39 @@ class role::memcached {
         }
     }
 
+    # `memkeys` is a `top`-like tool for inspecting memcache key usage in real time.
+    # In addition to making it available for interactive use, we configure a cronjob
+    # to run once a day and log 20 seconds' worth of memcached usage stats to a CSV
+    # file. That way, if there is a spike in memcached usage, we can diff the logs
+    # and see which keys are suspect.
+
     package { 'memkeys':
         ensure => present,
+        before => Cron['memkeys'],
+    }
+
+    file { '/var/log/memkeys':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        before => Cron['memkeys'],
+    }
+
+    file { '/etc/logrotate.d/memkeys':
+        source  => 'puppet:///files/memcached/memkeys.logrotate',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        before  => Cron['memkeys'],
+        require => File['/var/log/memkeys'],
+    }
+
+    cron { 'memkeys':
+        ensure  => present,
+        command => '/usr/bin/timeout --kill-after=25s 20s /usr/bin/memkeys --interface=eth0 --report=csv 2>/dev/null >> /var/log/memkeys/`date +"%m_%d_%Y"`.csv',
+        user    => 'root',
+        hour    => fqdn_rand(23, 'memkeys'),
+        minute  => fqdn_rand(59, 'memkeys'),
     }
 }
