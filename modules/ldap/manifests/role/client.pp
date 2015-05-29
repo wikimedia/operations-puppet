fe89d7c2eb142ba06a0e6f60a0384011e9e5ba57
Faidon Liambotis <faidon@wikimedia.org>
2015-05-29 23:29:50 +0300

    certs: remove random certificates::* includes
    These are all require()d from install_certificate, so external callers
    are redundant. The exception to this is the GlobalSign CA, which was not
    required by install_certificate before, so this was added now.
    
    Change-Id: I84ed8671b363371993731a3fb709bfb0aec76a04

--- modules/ldap/manifests/role/client.pp
+++ modules/ldap/manifests/role/client.pp
@@ -3,10 +3,6 @@ class ldap::role::client::labs($ldapincludes=['openldap', 'utils']) {
 
     if ( $::realm == 'labs' ) {
         $includes = ['openldap', 'pam', 'nss', 'sudo', 'utils', 'access']
-
-        # Cert for the old virt1000/virt0 ldap servers:
-        include certificates::wmf_labs_ca
-
     } else {
         $includes = $ldapincludes
     }
