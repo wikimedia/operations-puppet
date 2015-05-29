dbde6de8ce3d4acddf154237039f51223ee1c7dd
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-05-29 18:20:51 +0300

    role::puppet::server::labs Remove unused configuration
    These have not been used in a very long time. Also $realm is a global
    level variable
    
    Change-Id: Ie3d772a0e14b4be893fb0aa0ebd0316f2f2c4fca

--- manifests/role/puppet.pp
+++ manifests/role/puppet.pp
@@ -3,15 +3,11 @@ class role::puppet::server::labs {
     include ldap::role::config::labs
     $novaconfig = $role::nova::config::novaconfig
 
-    $puppet_db_name = $novaconfig['puppet_db_name']
-    $puppet_db_user = $novaconfig['puppet_db_user']
-    $puppet_db_pass = $novaconfig['puppet_db_pass']
-
     $ldapconfig = $ldap::role::config::labs::ldapconfig
     $basedn = $ldapconfig['basedn']
 
     # Only allow puppet access from the instances
-    $puppet_passenger_allow_from = $realm ? {
+    $puppet_passenger_allow_from = $::realm ? {
         'production' => [ '10.68.16.0/21', '208.80.154.14' ],
         'labs' => [ '192.168.0.0/21' ],
     }
@@ -21,10 +17,6 @@ class role::puppet::server::labs {
         allow_from  => $puppet_passenger_allow_from,
         config      => {
             'thin_storeconfigs' => false,
-            # 'dbadapter' => "mysql",
-            # 'dbuser' => $novaconfig["puppet_db_user"],
-            # 'dbpassword' => $novaconfig["puppet_db_pass"],
-            # 'dbserver' => $novaconfig["puppet_db_host"],
             'node_terminus'     => 'ldap',
             'ldapserver'        => $ldapconfig['servernames'][0],
             'ldapbase'          => "ou=hosts,${basedn}",
