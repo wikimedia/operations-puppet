13b153d3857e2b45ee1fa073e5630494ee0ce4a7
Ori Livneh <ori@wikimedia.org>
2015-05-29 09:32:28 -0700

    Provision varnishstatsd
    Re-purpose role::cache::statsd to provision varnishstatsd on the Varnishes.
    Right now, we mostly want this in eqiad, because the backend latency to the
    MediaWiki cluster (for example) will be dominated by network latency in CODFW /
    ESAMS / ULSFO. But in the interest of being prepared for the future in which we
    serve MediaWiki from CODFW as well, and in the interest of having the
    configuration consistent across varnishes, we provision varnishstatsd
    everywhere but incorporate the $::site variable into the metric key.
    
    For an initial roll-out, to prevent a freakout in case of some annoying Puppet
    failure, this is confined to cp1048. I will follow-up with another patch to
    apply it on other hosts if this goes smoothly.
    
    Change-Id: I93a2d3048db6c7b00763ece64aba5f33362c03a2

--- modules/role/manifests/cache/statsd.pp
+++ modules/role/manifests/cache/statsd.pp
@@ -2,5 +2,10 @@
 # Installs a local statsd instance for aggregating and serializing
 # stats before sending them off to a remote statsd instance.
 class role::cache::statsd {
-    class { '::statsite::decommission': }
+    if $::hostname == 'cp1048' {
+        ::varnish::logging::statsd { 'default':
+            statsd_server => 'statsd.eqiad.wmnet',
+            metric_prefix => "varnish.${::site}.backends",
+        }
+    }
 }
