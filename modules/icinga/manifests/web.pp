5e3f45eb79c18e5017d27b78bb094cba801ed7dd
RobH <rob@wikimedia.org>
2015-05-29 12:11:24 -0700

    icinga.wikimedia.org cert sha1 to sha256
    DO NOT MERGE THIS PATCHSET without updating it with the associated
    configuration changes for icinga's use, or you will break icinga https.
    
    (dzahn) - changed intermediate cert to RapidSSL_SHA256_CA_-_G3.pem per Robh
    
    Bug:T100830
    Change-Id: Ic8baf0f715da4793ef64525edca2e5fe893f3f14

--- modules/icinga/manifests/web.pp
+++ modules/icinga/manifests/web.pp
@@ -45,6 +45,6 @@ class icinga::web {
         ensure => absent,
     }
 
-    install_certificate{ 'icinga.wikimedia.org':       ca => 'RapidSSL_CA.pem' }
+    install_certificate{ 'icinga.wikimedia.org':       ca => 'RapidSSL_SHA256_CA_-_G3.pem' }
 
 }
