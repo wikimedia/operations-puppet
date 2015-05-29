dcc211a355defde00fdc1c5ff7759e68c04bc349
Ori Livneh <ori@wikimedia.org>
2015-05-29 10:10:45 -0700

    Apply ::varnish::logging::statsd on all varnishes, not just cp1048
    Things look good on cp1048, so go for the rest of the varnishes as well.
    
    Change-Id: Icd84ea3599f56c9fb356f2764130cbcc9972cfb4

--- modules/role/manifests/cache/statsd.pp
+++ modules/role/manifests/cache/statsd.pp
@@ -2,10 +2,8 @@
 # Installs a local statsd instance for aggregating and serializing
 # stats before sending them off to a remote statsd instance.
 class role::cache::statsd {
-    if $::hostname == 'cp1048' {
-        ::varnish::logging::statsd { 'default':
-            statsd_server => 'statsd.eqiad.wmnet',
-            key_prefix    => "varnish.${::site}.backends",
-        }
+    ::varnish::logging::statsd { 'default':
+        statsd_server => 'statsd.eqiad.wmnet',
+        key_prefix    => "varnish.${::site}.backends",
     }
 }
