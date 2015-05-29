e8d7e3accdd8233edc760348d656b20de7cc6b03
Ori Livneh <ori@wikimedia.org>
2015-05-29 10:47:31 -0700

    Apply varnishstatsd on cp1066 (text varnish) as well
    Change-Id: I7f9121890095236ecfdefee25cb1452e5d458d6a

--- modules/role/manifests/cache/statsd.pp
+++ modules/role/manifests/cache/statsd.pp
@@ -2,7 +2,7 @@
 # Installs a local statsd instance for aggregating and serializing
 # stats before sending them off to a remote statsd instance.
 class role::cache::statsd {
-    if $::hostname == 'cp1048' {
+    if $::hostname == 'cp1048' or $::hostname == 'cp1066' {
         ::varnish::logging::statsd { 'default':
             statsd_server => 'statsd.eqiad.wmnet',
             key_prefix    => "varnish.${::site}.backends",
