39aa4b90a09ed4dcf49dc125ed8abea64b556a10
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-05-29 19:12:21 +0300

    Rename role::puppet::self to role::puppetmaster::self
    Move it over to role::puppetmaster::self for consistency. This IS a
    breaking. Should be merged after wikitech is update with the new value
    LDAP must be updated right after this gets merged
    
    Change-Id: I41adebb9d20b37336da4e302c8fb9b6e4f5255da

--- manifests/role/puppetmaster.pp
+++ manifests/role/puppetmaster.pp
@@ -114,3 +114,63 @@ class role::puppetmaster::labs {
         }
     }
 }
+
+# == Class role::puppet::self
+# role class for puppet::self::master
+# and puppet::self::client.
+# If $::puppetmaster is localhost or matches the $::fqdn of this node,
+# then this node will be configured as a puppetmaster.
+# NOTE:  $::puppetmaster == 'localhost' (or undef) does the exact same
+# thing as the original puppetmaster::self class used to do.
+#
+# $::puppetmaster must be set as a global variable.
+# This allows puppet classes to be configured via LDAP
+# and wikitech instance configuration.
+class role::puppetmaster::self(
+    $master = $::puppetmaster,
+    $autoupdate_master = $::puppetmaster_autoupdate,
+    $enc = 'ldap', # 'ldap' or 'yaml+ldap'
+) {
+    # If $::puppetmaster is not set, assume
+    # this is a self hosted puppetmaster, not allowed
+    # to serve any other puppet clients.
+    $server = $master ? {
+          undef       => 'localhost',
+          'localhost' => 'localhost',
+          ''          => 'localhost',
+          # if has . characters in in, assume fqdn.
+          /\./        => $master,
+          # else assume short hostname and append domain.
+          default     => "${master}.${::domain}",
+    }
+
+    # If localhost or if $server matches this node's
+    # $fqdn, then this is a puppetmaster.
+    if ($server == 'localhost' or $server == $::fqdn) {
+        if $enc == 'yaml+ldap' {
+            $enc_script_path = '/usr/local/bin/ldap-yaml-enc.py'
+            file { $enc_script_path:
+                source => 'puppet:///modules/puppetmaster/ldap-yaml-enc.py',
+                owner  => 'root',
+                group  => 'root',
+                mode   => '0555',
+            }
+        }
+        class { 'puppet::self::master':
+            server          => $server,
+            enc_script_path => $enc_script_path,
+        }
+        # If this parameter / variable variable is set, then
+        # run a cron job that automatically tries to update the local
+        # git repository, while trying to keep intact cherry picks
+        if $autoupdate_master {
+            include puppetmaster::gitsync
+        }
+    }
+    # Else this is a puppet client.
+    else {
+        class { 'puppet::self::client':
+            server => $server,
+        }
+    }
+}
