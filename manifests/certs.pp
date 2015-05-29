26353e2fd505caa8a01bc6ee6c54ef07a2051a9a
Faidon Liambotis <faidon@wikimedia.org>
2015-05-29 23:26:11 +0300

    certs: inline certificate::<ca> classes to ::base
    We don't really need all those different classes anymore.
    
    Change-Id: I8a1cecfed4b26be189defa5a2316740a94127ff2

--- manifests/certs.pp
+++ manifests/certs.pp
@@ -4,14 +4,6 @@ define install_certificate(
 ) {
 
     require certificates::base
-    require certificates::rapidssl_ca
-    require certificates::rapidssl_ca_2
-    require certificates::rapidssl_sha256_ca_G3
-    require certificates::digicert_ca
-    require certificates::digicert_sha2_ca
-    require certificates::globalsign_ca
-    require certificates::wmf_ca
-    require certificates::wmf_ca_2014_2017
 
     sslcert::certificate { $name:
         group  => $group,
@@ -32,51 +24,28 @@ define install_certificate(
 
 class certificates::base {
     include ::sslcert
-}
 
-class certificates::wmf_ca {
     sslcert::ca { 'wmf-ca':
         source  => 'puppet:///files/ssl/wmf-ca.crt',
     }
-}
-
-class certificates::wmf_ca_2014_2017 {
     sslcert::ca { 'wmf_ca_2014_2017':
         source  => "puppet:///files/ssl/wmf_ca_2014_2017.crt",
     }
-}
-
-class certificates::rapidssl_ca {
     sslcert::ca { 'RapidSSL_CA':
         source  => 'puppet:///files/ssl/RapidSSL_CA.crt',
     }
-}
-
-class certificates::rapidssl_ca_2 {
     sslcert::ca { 'RapidSSL_CA_2':
         source  => 'puppet:///files/ssl/RapidSSL_CA_2.crt',
     }
-}
-
-class certificates::rapidssl_sha256_ca_G3 {
     sslcert::ca { 'RapidSSL_SHA256_CA_-_G3':
         source  => 'puppet:///files/ssl/RapidSSL_SHA256_CA_-_G3.crt',
     }
-}
-
-class certificates::digicert_ca {
     sslcert::ca { 'DigiCertHighAssuranceCA-3':
         source  => 'puppet:///files/ssl/DigiCertHighAssuranceCA-3.crt',
     }
-}
-
-class certificates::digicert_sha2_ca {
     sslcert::ca { 'DigiCertSHA2HighAssuranceServerCA':
         source => 'puppet:///files/ssl/DigiCertSHA2HighAssuranceServerCA.crt',
     }
-}
-
-class certificates::globalsign_ca {
     sslcert::ca { 'GlobalSign_CA':
         source  => 'puppet:///files/ssl/GlobalSign_CA.crt',
     }
