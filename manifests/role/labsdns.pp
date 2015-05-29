171108ed5f42c2433460bd509c980fba226acd98
andrewbogott <abogott@wikimedia.org>
2015-05-29 01:10:34 -0500

    Switch labs pdns/mysql to the new db host
    Change-Id: Ie1b303c0939898ce03590715cd3c9ce33e464bdf

--- manifests/role/labsdns.pp
+++ manifests/role/labsdns.pp
@@ -5,7 +5,7 @@ class role::labsdns {
         dns_auth_ipaddress     => '208.80.154.12',
         dns_auth_query_address => '208.80.154.12',
         dns_auth_soa_name      => 'labs-ns2.wikimedia.org',
-        pdns_db_host           => 'm1-master.eqiad.wmnet',
+        pdns_db_host           => 'm5-master.eqiad.wmnet',
         pdns_db_password       => $passwords::pdns::db_pass,
         pdns_recursor          => '208.80.154.239',
         recursor_ip_range      => '10.68.16.0/21',
