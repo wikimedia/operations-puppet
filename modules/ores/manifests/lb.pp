7bc204449cea6c467b3084452be9b10bd9d9c200
YuviPanda <yuvipanda@gmail.com>
2015-05-31 12:14:55 +0100

    ores: Add experimental nginx proxy caching
    Change-Id: Iab2191205c2fcfc2c78e8f851cc0e191bfcc8507

--- modules/ores/manifests/lb.pp
+++ modules/ores/manifests/lb.pp
@@ -2,6 +2,17 @@
 class ores::lb(
     $realservers,
 ) {
+    file { [
+        '/srv/nginx/',
+        '/srv/nginx/cache',
+        '/srv/nginx/tmp',
+    ]:
+        ensure => directory,
+        owner  => 'www-data',
+        group  => 'www-data',
+        mode   => '0775',
+    }
+
     nginx::site { 'ores-lb':
         content => template('ores/lb.nginx.erb'),
     }
