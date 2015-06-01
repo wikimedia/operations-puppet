65858989483ac322fb31254a772d66cbd3d2d5ab
Andrew Otto <acotto@gmail.com>
2015-06-01 12:37:05 -0400

    Make it possible to install multiple custom diamond collectors that use the same source
    This decouples the installation of custom diamond collector code from the
    diamond collector config.
    
    Change-Id: I4d10726fa97f935234d6a0d7b0939d97b4fd65bc

---
+++ modules/diamond/manifests/collector/custom.pp
@@ -0,0 +1,32 @@
+# == Class: diamond::collector::custom
+# Installs a custom diamond collector python plugin.
+#
+# === Parameters
+#
+# [*source*]
+#   A Puppet file reference to the Python collector source file. This parameter
+#   may be omitted if the collector is part of the Diamond distribution. It
+#   should only be set for custom collectors. Unset by default.
+#
+# [*ensure*]
+#   Specifies whether or not to configure Diamond to use this collector.
+#   May be 'present' or 'absent'. The default is 'present'.
+#
+define diamond::collector::custom(
+    $source,
+    $ensure = 'present'
+) {
+    file { "/usr/share/diamond/collectors/${name}":
+        ensure => ensure_directory($ensure),
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+    file { "/usr/share/diamond/collectors/${name}/${name}.py":
+        ensure => $ensure,
+        source => $source,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
+}
