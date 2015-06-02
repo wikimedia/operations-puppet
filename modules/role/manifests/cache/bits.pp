6b422cc8dd6d0f1052110286710c9bea43e71aa5
Ori Livneh <ori@wikimedia.org>
2015-06-02 10:26:59 -0700

    Apply role::cache::statsd on bits
    * Don't include varnishstatsd in kafka role. The repurposed role::cache::statsd
      is no longer directly related to kafka, so including it there is misleading.
    * As part of a gradual general rollout, apply role::cache::statsd on bits.
    
    Change-Id: I7ffd74997540a809bf7b3cdfa255ba62da5d5509

--- modules/role/manifests/cache/bits.pp
+++ modules/role/manifests/cache/bits.pp
@@ -7,6 +7,7 @@ class role::cache::bits (
     }
 
     include role::cache::1layer
+    include role::cache::statsd
 
     class { 'lvs::realserver':
         realserver_ips => $lvs::configuration::lvs_service_ips[$::realm]['bits'][$::site],
