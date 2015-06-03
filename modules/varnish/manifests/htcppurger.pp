a6d96362a1990356892eb718f18c5a6232d178b2
Matanya Moses <matanya@foss.co.il>
2015-06-04 00:35:35 +0300

    varnish_instances: qualify var
    bug: T97251
    Change-Id: Ie7156c7519ecdd2fb92f2ef8627ccd45bfca5e5a

--- modules/varnish/manifests/htcppurger.pp
+++ modules/varnish/manifests/htcppurger.pp
@@ -11,7 +11,7 @@ class varnish::htcppurger($varnish_instances=['localhost:80']) {
         mode    => '0444',
         require => Package['vhtcpd'], # if we go first, we get overwritten
         # TODO: -r ^upload\\.wikimedia\\.org\$ (POSIX ERE, new param for class, quoting/escaping will be tricky...)
-        content => inline_template('DAEMON_OPTS="-m 239.128.0.112<% varnish_instances.each do |inst| -%> -c <%= inst %><% end -%>"'),
+        content => inline_template('DAEMON_OPTS="-m 239.128.0.112<% @varnish_instances.each do |inst| -%> -c <%= inst %><% end -%>"'),
     }
 
     # Wikimedia used to provide vhtcpd under the name varnishhtcpd with an
