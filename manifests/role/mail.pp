ddef0154ef14af7f5962cf970706a4bb3d1a23ce
dzahn <dzahn@wikimedia.org>
2015-06-02 18:47:49 -0700

    mailman monitoring: adjusting thresholds
    We did not have criticals anymore after recent adjustments but
    2 soft state warnings were left about KBytes_Written/Sec around 6k.
    
    (T84150)
    
    Change-Id: Id8b68f29b20a875a042110c2244b602f94b6d00a

--- manifests/role/mail.pp
+++ manifests/role/mail.pp
@@ -189,7 +189,7 @@ class role::mail::lists {
     # values chosen based on i/o averages for sodium
     nrpe::monitor_service { 'mailman_iostat':
         description   => 'mailman I/O stats',
-        nrpe_command  => '/usr/local/lib/nagios/plugins/check_iostat -i -w 250,350,300,14000,5500 -c 500,400,600,28000,11000',
+        nrpe_command  => '/usr/local/lib/nagios/plugins/check_iostat -i -w 250,350,300,14000,7500 -c 500,400,600,28000,11000',
         timeout       => '30',
     }
 
