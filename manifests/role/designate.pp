39c0925dc73a77e495d9a80b31050ba263e58e22
YuviPanda <yuvipanda@gmail.com>
2015-06-03 19:11:27 +0100

    designate: Use ferm's resolve function than puppet ipresolve
    ipresolve seems a lot more flaky
    
    Bug: T101281
    Change-Id: I919e6ec7d11ce13cbda9a94d9c1e75b4ca0bdc0f

--- manifests/role/designate.pp
+++ manifests/role/designate.pp
@@ -84,11 +84,10 @@ class role::designate::server {
     # Firewall
     $wikitech = '208.80.154.136'
     $horizon = '208.80.154.147'
-    $controller = ipresolve($nova_controller, 4)
 
     # Poke a firewall hole for the designate api
     ferm::rule { 'designate-api':
-        rule => "saddr (${wikitech} ${horizon} ${controller}) proto tcp dport (9001) ACCEPT;",
+        rule => "saddr (${wikitech} ${horizon} @resolve(${nova_controller})) proto tcp dport (9001) ACCEPT;",
     }
 
     file { '/var/lib/designate/.ssh/':
