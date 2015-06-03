6da5b0eb4b2b023ff6bd26b25e951a44a10e0f7c
Faidon Liambotis <faidon@wikimedia.org>
2015-06-03 12:40:02 +0300

    base::certs: rename CA filenames to their CNs
    A few of our CAs were not named with their CN, creating confusion in the
    process (e.g. GlobalSign_CA was not actually the GlobalSign root CA but
    the OV intermediate CA).
    
    Rename them to be consistent and clear.
    
    Change-Id: Ia4c41213166d8a227e9cae08a011f9d63338c22f

--- modules/base/manifests/certificates.pp
+++ modules/base/manifests/certificates.pp
@@ -10,19 +10,37 @@ class base::certificates {
     sslcert::ca { 'RapidSSL_CA':
         source  => 'puppet:///modules/base/ca/RapidSSL_CA.crt',
     }
-    sslcert::ca { 'RapidSSL_CA_2':
-        source  => 'puppet:///modules/base/ca/RapidSSL_CA_2.crt',
-    }
     sslcert::ca { 'RapidSSL_SHA256_CA_-_G3':
         source  => 'puppet:///modules/base/ca/RapidSSL_SHA256_CA_-_G3.crt',
     }
+    sslcert::ca { 'GeoTrust_Global_CA':
+        source  => 'puppet:///modules/base/ca/GeoTrust_Global_CA.crt',
+    }
+    sslcert::ca { 'DigiCert_High_Assurance_CA-3':
+        source  => 'puppet:///modules/base/ca/DigiCert_High_Assurance_CA-3.crt',
+    }
+    sslcert::ca { 'DigiCert_SHA2_High_Assurance_Server_CA':
+        source => 'puppet:///modules/base/ca/DigiCert_SHA2_High_Assurance_Server_CA.crt',
+    }
+    sslcert::ca { 'GlobalSign_Organization_Validation_CA_-_SHA256_-_G2':
+        source  => 'puppet:///modules/base/ca/GlobalSign_Organization_Validation_CA_-_SHA256_-_G2.crt',
+    }
+
+    # FIXME: remove after a while
+    sslcert::ca { 'GlobalSign_CA':
+        ensure  => absent,
+        require => Sslcert::Ca['GlobalSign_Organization_Validation_CA_-_SHA256_-_G2'],
+    }
     sslcert::ca { 'DigiCertHighAssuranceCA-3':
-        source  => 'puppet:///modules/base/ca/DigiCertHighAssuranceCA-3.crt',
+        ensure  => absent,
+        require => Sslcert::Ca['DigiCert_High_Assurance_CA-3'],
     }
     sslcert::ca { 'DigiCertSHA2HighAssuranceServerCA':
-        source => 'puppet:///modules/base/ca/DigiCertSHA2HighAssuranceServerCA.crt',
+        ensure  => absent,
+        require => Sslcert::Ca['DigiCert_SHA2_High_Assurance_Server_CA'],
     }
-    sslcert::ca { 'GlobalSign_CA':
-        source  => 'puppet:///modules/base/ca/GlobalSign_CA.crt',
+    sslcert::ca { 'RapidSSL_CA_2':
+        ensure  => absent,
+        require => Sslcert::Ca['GeoTrust_Global_CA'],
     }
 }
