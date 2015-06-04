7c2fa5f567d386db76f60446b532ba762b938d43
Chad Horohoe <chadh@wikimedia.org>
2015-06-04 10:47:13 -0700

    Phabricator: Enable the webserver to serve git repos
    Sudo bits are untested, roughly following upstream's suggested
    config. The symlink I've already tested live and should work
    as long as permissions get set right.
    
    Change-Id: I1c6cd0eb868307a0e4c733e3ab680fd64d82a118

---
+++ modules/phabricator/manifests/vcs.pp
@@ -0,0 +1,18 @@
+# == Class: phabricator::vcs
+#
+# Setup Phabricator to properly act as a VCS host
+#
+
+class phabricator::vcs (
+    $settings = {},
+) {
+    # git-http-backend needs to be in $PATH
+    file { '/usr/local/bin/git-http-backend':
+        ensure => 'link',
+        target => '/usr/lib/git-core/git-http-backend',
+    }
+
+    sudo::user { 'www-data': privileges => [
+        "ALL=(${settings['phd.user']}) SETENV: NOPASSWD: /usr/bin/git-http-backend",
+    ] }
+}
