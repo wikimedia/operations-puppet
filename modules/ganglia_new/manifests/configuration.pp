53bba352fe82ed9cb16206c704b1c0b5e4690f60
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-06-05 16:35:21 +0200

    ganglia: configure the ganglia aggregator for eqiad
    This is used by ganglia_new
    
    Change-Id: Iabb3283d1bb4f5deaf17e69d3bc74e3000198b58

--- modules/ganglia_new/manifests/configuration.pp
+++ modules/ganglia_new/manifests/configuration.pp
@@ -10,7 +10,7 @@ class ganglia_new::configuration {
     # neon/icinga.
     $gmetad_hosts = [ '208.80.154.53', '208.80.154.150', '208.80.154.14' ]
     $aggregator_hosts = {
-        'eqiad' => [ '208.80.154.53', '208.80.154.150' ],
+        'eqiad' => [ ipresolve('carbon.wikimedia.org') ],
         'esams' => [ '91.198.174.113' ],
         'codfw' => [ '208.80.153.4' ],
     }
