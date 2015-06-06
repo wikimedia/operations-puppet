d3ff57743a6fd211c59f50fd7574fb7010e49c5f
Stanislav Malyshev <smalyshev@gmail.com>
2015-06-06 00:57:04 -0700

    Add definitions for WDQS
    Change-Id: I21e17e5e7d56d536f4cc34894ddb4fd0a7ea9846

---
+++ modules/wdqs/manifests/packages.pp
@@ -0,0 +1,22 @@
+# == Class: wdqs::packages
+#
+# Provisions WDQS package and dependencies.
+#
+class wdqs::packages {
+    include ::java::tools
+
+    if ! defined ( Package['openjdk-7-jdk'] ) {
+        package { 'openjdk-7-jdk':
+            ensure => 'present',
+        }
+    }
+
+    if ! defined ( Package['curl'] ) {
+        package { 'curl': ensure => present }
+    }
+
+    if ! defined ( Package['maven'] ) {
+        package { 'maven': ensure => present }
+    }
+	
+}
