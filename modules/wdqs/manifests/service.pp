e82069174f2286ea4d05b869fda1024081d019d1
Stanislav Malyshev <smalyshev@gmail.com>
2015-06-06 00:57:04 -0700

    Add definitions for WDQS
    Change-Id: I21e17e5e7d56d536f4cc34894ddb4fd0a7ea9846

---
+++ modules/wdqs/manifests/service.pp
@@ -0,0 +1,36 @@
+# == Class: wdqs::service
+#
+# Provisions WDQS service package
+#
+# == Parameters:
+# - $package_dir:  Directory where the service should be installed.
+# Should have enough space to hold the database (>50G)
+# - $version: Version of the service to install
+# - $username: Username owning the service
+#
+class wdqs::service(
+    $package_dir,
+    $version,
+    $username,
+) {
+    # Horrible hack, find better way?
+    exec { 'download_package':
+        command => "/usr/bin/mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:unpack -Dartifact=org.wikidata.query.rdf:service:${version}:zip:dist -DoutputDirectory=${package_dir} -Dtransitive=false -Dproject.basedir=/tmp",
+        cwd     => '/tmp',
+        creates => "${package_dir}/service-${version}",
+        require => Package['maven'],
+        before  => File['/tmp/target'],
+    }
+
+    file { '/tmp/target':
+        ensure => absent,
+        force  => true,
+    }
+
+    file { "${package_dir}/service-${version}":
+        owner   => $username,
+        group   => 'wikidev',
+        mode    => '0775',
+        require => Exec['download_package'],
+    }
+}
