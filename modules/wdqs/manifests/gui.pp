d6b5305d7782511ab6395acc21049c2f246845cb
Stanislav Malyshev <smalyshev@gmail.com>
2015-06-06 00:57:04 -0700

    Add definitions for WDQS
    Change-Id: I21e17e5e7d56d536f4cc34894ddb4fd0a7ea9846

---
+++ modules/wdqs/manifests/gui.pp
@@ -0,0 +1,31 @@
+# == Class: wdqs::packages
+#
+# Provisions WDQS GUI
+#
+# == Parameters:
+# - $package_dir:  Directory where the service is installed. 
+# GUI files are expected to be under its gui/ directory.
+# - $log_aggregator: Where to send the logs for the service.
+class wdqs::gui(
+    $package_dir,
+    $log_aggregator
+) {
+
+    if ! defined ( Package['nginx'] ) {
+        package { 'nginx': ensure => present }
+    }
+    file { '/etc/nginx/sites-enabled/default':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        content => template('wdqs/nginx.erb'),
+        mode    => '0444',
+        require => Package['nginx'],
+        notify => Service['nginx'],
+    }
+    service { 'nginx':
+        enable => true,
+        require => File['/etc/nginx/sites-enabled/default']
+    }
+    
+}
