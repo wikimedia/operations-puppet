e95a3d7e0fd5be7aabb1d4cfe444e175207297d5
Faidon Liambotis <faidon@wikimedia.org>
2015-06-06 10:19:41 +0200

    mail: remove secondary MX role from sodium (2nd take)
    Now that we have both polonium & lead, we can make our set up simpler
    and remove the secondary MX role from sodium, that from now on can just
    serve as a lists server and that alone.
    
    This is the second attempt at doing so, the first one was
    a94c939f81a496ee14664ee022211ec2776b8c37 that was reverted because the
    old exim4.conf.SMTP_IMAP_MM.erb template generated an invalid
    configuation. Now that a vastly simplified template, modify it
    accordingly, without fussing too much with template conditionals.
    
    Change-Id: I4ffba5fc52e044f9e7508c9d5008acbf27651418

--- manifests/role/mail.pp
+++ manifests/role/mail.pp
@@ -69,8 +69,6 @@ class role::mail::mx(
 class role::mail::lists {
     include network::constants
 
-    # FIXME: needs to be split to lists/secondarymx
-
     system::role { 'role::mail::lists':
         description => 'Mailing list server',
     }
@@ -115,7 +113,6 @@ class role::mail::lists {
                 '+system_domains',
                 '+mailman_domains'
             ],
-        enable_mail_relay      => 'secondary',
         enable_mailman         => true,
         enable_spamassassin    => true,
         require                => [
