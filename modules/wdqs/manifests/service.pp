1022513e204ecaaeacf022f79274781ac31d22fc
Stanislav Malyshev <smalyshev@gmail.com>
2015-06-06 00:57:04 -0700

    Add definitions for WDQS
    Task: T95679
    
    Change-Id: I21e17e5e7d56d536f4cc34894ddb4fd0a7ea9846

---
+++ modules/wdqs/manifests/service.pp
@@ -0,0 +1,41 @@
+# == Class: wdqs::service
+#
+# Provisions WDQS service package
+#
+# == Parameters:
+# - $package_dir:  Directory where the service should be installed.
+# Should have enough space to hold the database (>50G)
+# - $version: Version of the service to install
+# - $username: Username owning the service
+#
+class wdqs::service(
+    $package_dir,
+    $version,
+    $username,
+) {
+    # Horrible hack, find better way?
+    exec { 'download_package':
+        command => "/usr/local/sbin/install_wdqs.sh ${version} ${package_dir}",
+        cwd     => '/tmp',
+        creates => "${package_dir}/service-${version}",
+        require => [
+          Package['maven'],
+          File['/usr/local/sbin/install_wdqs.sh']
+        ],
+    }
+
+    file { '/usr/local/sbin/install_wdqs.sh':
+        ensure => file,
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/wdqs/install_wdqs.sh',
+        mode   => '0755',
+    }
+
+    file { "${package_dir}/service-${version}":
+        owner   => $username,
+        group   => 'wikidev',
+        mode    => '0775',
+        require => Exec['download_package'],
+    }
+}
