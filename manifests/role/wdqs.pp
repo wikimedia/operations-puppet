ce0097dd7c790155d783908189ec1d38e2a6431f
Stanislav Malyshev <smalyshev@gmail.com>
2015-06-06 00:57:04 -0700

    Add definitions for WDQS
    Change-Id: I21e17e5e7d56d536f4cc34894ddb4fd0a7ea9846

---
+++ manifests/role/wdqs.pp
@@ -0,0 +1,26 @@
+# = Class: role::wdqs
+#
+# This class sets up Wikidata Query Service
+#
+class role::wdqs{
+    include standard
+    include role::labs::lvm::srv
+
+    system::role { 'role::wdqs':
+        ensure      => 'present',
+        description => 'WDQS server',
+    }
+
+    # Install service
+    class { '::wdqs':
+        package_dir => '/srv',
+        username    => 'blazegraph',
+    }
+
+    # Service GUI
+    class { '::wdqs::gui':
+        package_dir    => '/srv/blazegraph',
+        log_aggregator => '10.68.16.147:10514',
+        require        => Class['::wdqs'],
+    }
+}
