e052b70da0e1a41653b31587dd7dc20b1069c6e9
Coren <marc@uberbox.org>
2015-06-09 10:15:35 -0400

    Labs: move set-stripe-cache to cron
    So that it gets applied reliably whether puppet is working
    correctly or not, even if md devices are added after boot.
    
    Bug: T96045
    Change-Id: Ic7c4927d8d296c1addb9beda1a6d722b404b4d6b

--- modules/openstack/manifests/project-nfs-storage-service.pp
+++ modules/openstack/manifests/project-nfs-storage-service.pp
@@ -24,8 +24,16 @@ class openstack::project-nfs-storage-service {
         source  => 'puppet:///modules/openstack/set-stripe-cache',
     }
 
-    exec { 'set-stripe-caches':
+    # This is done unconditionally to all the md devices at
+    # interval to guard against (a) puppet not applying for
+    # any reason, and (b) the fact that the set of started
+    # md devices on a labstore* is ultimately variable and
+    # dynamic depending on its current role.
+    #
+    cron { 'set-stripe-caches':
         command => '/usr/local/sbin/set-stripe-cache 4096',
+        user    => 'root',
+        minute  => '*/5',
         require => File['/usr/local/sbin/set-stripe-cache'],
     }
 
