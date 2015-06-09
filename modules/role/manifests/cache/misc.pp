69ee8290567acdc7833130983a0fe66c4a0d5c15
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-06-09 15:53:55 +0300

    Sort misc varnish backends
    Just minor lint to make my life easier
    
    Change-Id: Ic64b401bb2b4c9b333ae53f0603def7e5b386a69

--- modules/role/manifests/cache/misc.pp
+++ modules/role/manifests/cache/misc.pp
@@ -29,27 +29,27 @@ class role::cache::misc {
             'allowed_methods' => '^(GET|HEAD|POST|PURGE|PUT)$',
         },
         backends        => [
+            'analytics1001.eqiad.wmnet', # Hadoop Yarn ResourceManager GUI
+            'analytics1027.eqiad.wmnet', # Hue (Hadoop GUI)
             'antimony.wikimedia.org',
             'caesium.eqiad.wmnet',
             'californium.wikimedia.org',
             'dataset1001.wikimedia.org',
             'gallium.wikimedia.org',  # CI server
-            'ytterbium.wikimedia.org', # Gerrit
             'graphite1001.eqiad.wmnet',
-            'zirconium.wikimedia.org',
-            'ruthenium.eqiad.wmnet', # parsoid rt test server
+            'iridium.eqiad.wmnet', # main phab
             'logstash1001.eqiad.wmnet',
             'logstash1002.eqiad.wmnet',
             'logstash1003.eqiad.wmnet',
-            'netmon1001.wikimedia.org', # servermon
-            'iridium.eqiad.wmnet', # main phab
-            'terbium.eqiad.wmnet', # public_html
-            'neon.wikimedia.org', # monitoring tools (icinga et al)
             'magnesium.wikimedia.org', # RT and racktables
-            'stat1001.eqiad.wmnet', # metrics and metrics-api
+            'neon.wikimedia.org', # monitoring tools (icinga et al)
+            'netmon1001.wikimedia.org', # servermon
             'palladium.eqiad.wmnet',
-            'analytics1027.eqiad.wmnet', # Hue (Hadoop GUI)
-            'analytics1001.eqiad.wmnet', # Hadoop Yarn ResourceManager GUI
+            'ruthenium.eqiad.wmnet', # parsoid rt test server
+            'stat1001.eqiad.wmnet', # metrics and metrics-api
+            'terbium.eqiad.wmnet', # public_html
+            'ytterbium.wikimedia.org', # Gerrit
+            'zirconium.wikimedia.org',
         ],
         backend_options => [
         {
