5608ad3a27563bd0cd464d4f5b08bc1ab56e7a6d
YuviPanda <yuvipanda@gmail.com>
2015-06-10 17:11:30 +0100

    ldap: Move ldap key lookup ssh helper to /usr/sbin
    Renames script & user to more descriptive to ssh-key-ldap-lookup
    
    Change-Id: If994b6fb8b4ab2ff45cd7fcd1685e941554196fa

--- modules/ldap/manifests/client.pp
+++ modules/ldap/manifests/client.pp
@@ -177,16 +177,20 @@ class ldap::client::utils($ldapconfig) {
 
     if $::realm == 'labs' {
         if os_version('debian >= jessie || ubuntu >= trusty') {
-            # The 'ldapkeys' tool is called during login ssh via AuthorizedKeysCommand.  It
+            # The 'ssh-key-ldap-lookup' tool is called during login ssh via AuthorizedKeysCommand.  It
             #  returns public keys from ldap for the specified username.
-            file { '/usr/local/sbin/ldapkeys':
+            # It is in /usr/sbin and not /usr/local/sbin because on Debian /usr/local is 0775
+            # and sshd refuses to use anything under /usr/local because of the permissive group
+            # permission there (and group is set to 'staff', slightly different from root).
+            # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=538392
+            file { '/usr/sbin/ssh-key-ldap-lookup':
                 owner  => 'root',
                 group  => 'root',
                 mode   => '0555',
-                source => 'puppet:///modules/ldap/scripts/ldapkeys',
+                source => 'puppet:///modules/ldap/scripts/ssh-key-ldap-lookup',
             }
-            # For security purposes, sshd will only run ldapkeys as the 'ldapkeys' user.
-            user { 'ldapkeys':
+            # For security purposes, sshd will only run ssh-key-ldap-lookup as the 'ssh-key-ldap-lookup' user.
+            user { 'ssh-key-ldap-lookup':
                 ensure => present,
                 system => true,
             }
