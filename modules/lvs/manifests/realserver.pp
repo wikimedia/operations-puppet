00f6e2d44eb3f5b4a6730ed9e709731016d77ae1
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-06-10 18:59:22 +0300

    lvs::realserver: Lint it
    * Double quotes to single quotes
    * Quote various strings
    * Reorder parameters for a more readable output
    * Reorder resources
    
    Change-Id: Idfe694bd726174ed36ab4f05241b4a46734c8ddd

--- modules/lvs/manifests/realserver.pp
+++ modules/lvs/manifests/realserver.pp
@@ -9,22 +9,23 @@
 #       Array or hash (name => ip) of service IPs to answer on
 class lvs::realserver($realserver_ips=[]) {
 
+    package { 'wikimedia-lvs-realserver':
+        ensure  => present,
+        require => File['/etc/default/wikimedia-lvs-realserver'],
+    }
+
     file { "/etc/default/wikimedia-lvs-realserver":
-        mode => 0444,
-        owner => root,
-        group => root,
-        content => template("${module_name}/wikimedia-lvs-realserver.erb");
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('lvs/wikimedia-lvs-realserver.erb'),
     }
 
-    exec { "/usr/sbin/dpkg-reconfigure -p critical -f noninteractive wikimedia-lvs-realserver":
-        require => Package["wikimedia-lvs-realserver"],
-        path => "/bin:/sbin:/usr/bin:/usr/sbin",
-        subscribe => File["/etc/default/wikimedia-lvs-realserver"],
-        refreshonly => true;
-    }
-
-    package { wikimedia-lvs-realserver:
-        ensure => latest,
-        require => File["/etc/default/wikimedia-lvs-realserver"];
+    exec { '/usr/sbin/dpkg-reconfigure -p critical -f noninteractive wikimedia-lvs-realserver':
+        path        => '/bin:/sbin:/usr/bin:/usr/sbin',
+        refreshonly => true,
+        require     => Package['wikimedia-lvs-realserver'],
+        subscribe   => File['/etc/default/wikimedia-lvs-realserver'],
     }
 }
