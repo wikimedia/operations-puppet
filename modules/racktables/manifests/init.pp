fc833181deed86422fd11311881f4856b39bdfa6
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-06-11 15:29:26 -0700

    racktables: increase default php memory limit
    Bug: T102092
    Change-Id: Iacce6f1ffd43eee6797de4a74c9a6ba7b3a0a278

--- modules/racktables/manifests/init.pp
+++ modules/racktables/manifests/init.pp
@@ -36,6 +36,12 @@ class racktables (
         source => 'puppet:///files/apache/conf.d/namevirtualhost',
     }
 
-
+    # Increase the default memory limit
+    file_line { 'racktables_php_memory':
+        path    => '/etc/php5/apache2/php.ini',
+        line    => 'memory_limit = 256M',
+        match   => '^\s*memory_limit',
+        require => Class['::apache::mod::php5'],
+        notify  => Class['::apache'],
+    }
 }
-
