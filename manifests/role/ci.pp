f6d6e04aadcd6d7bb1b2862ec8f544a9d9c5957d
Antoine Musso <hashar@free.fr>
2015-06-11 11:26:20 +0200

    contint: role::ci::slave::labs::light
    A light CI labs slave intended to be used for Jessie.  We can't yet make
    them full CI slaves since puppet does not pass (T94836) but this class
    will let us migrate some specific jobs.
    
    Bug: T94836
    Change-Id: Ic356b0b694216b1edac627b1462989a858db400a

--- manifests/role/ci.pp
+++ manifests/role/ci.pp
@@ -482,6 +482,26 @@ class role::ci::slave::labs {
 
 }
 
+# == Class role::ci::slave::labs::light
+#
+# Transient role that setup a slave labs for Jessie.  Regular slaves include
+# mediawiki::packages and over cmaterials which are not yet ready on Jessie
+# (tracking task is https://phabricator.wikimedia.org/T94836).
+#
+# Let us migrate some jobs to Jessie since production is moving toward it.
+#
+class role::ci::slave::labs::light {
+
+    requires_realm('labs')
+
+    system::role { 'role::ci::slave::labs::light':
+        description => 'CI *LIGHT* Jenkins slave on labs' }
+
+    # Trebuchet replacement on labs
+    include contint::slave-scripts
+    include role::ci::slave::labs::common
+
+}
 
 # == Class role::ci::publisher::labs
 #
