da7682e352931f4cce172173747d0b5fdae89610
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-06-11 12:21:28 +0200

    conftool: create puppet module
    This module installs conftool, sets up the configuration, copies the ca
    file and also creates a small wrapper script.
    
    Change-Id: I54eb9feedc3936b70b89913b30cb59eca101901a

---
+++ modules/conftool/manifests/init.pp
@@ -0,0 +1,48 @@
+# == Class conftool
+#
+# Installs conftool, and creates a wrapper script that can be run in git as
+# a post-receive hook.
+
+class conftool(
+    $sync_dir_root = '/var/lib/operations/puppet/conftool',
+    $config_file   = 'puppet:///modules/conftool/production.config.yaml',
+    $ssl_dir       = '/var/lib/puppet',
+    $use_ssl       = true,
+    ) {
+    require_package 'conftool'
+
+    file { '/usr/local/bin/conftool-merge':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0500',
+        content => template('conftool/conftool-merge.erb')
+    }
+
+    file { '/etc/conftool':
+        ensure => directory,
+        owner  => root,
+        group  => root,
+        mode   => '0755',
+    }
+
+    file { '/etc/conftool/config.yaml':
+        ensure => present,
+        owner  => root,
+        group  => root,
+        mode   => '0444',
+        source => $config_file,
+        before => File['/usr/local/bin/conftool-merge'],
+    }
+
+    if $use_ssl {
+        file { '/etc/conftool/ca.pem':
+            ensure => present,
+            owner  => root,
+            group  => root,
+            mode   => '0444',
+            source => "${ssl_dir}/certs/ca.pem",
+            before => File['/usr/local/bin/conftool-merge'],
+        }
+    }
+}
