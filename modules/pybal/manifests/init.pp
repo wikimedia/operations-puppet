2e8f22994fbb600e2a4c0805acaedf06fb497500
cpettet <rush@wikimedia.org>
2015-06-12 18:09:20 -0500

    confd additions for pybal
    * A golang template for generating external
      node pool definition files that are already
      accepted by pybal
    
    * A brief script to validate the definition file
    
    Change-Id: I634bbf70bea2865bdf3e012c5f7c8b4a75450a46

--- modules/pybal/manifests/init.pp
+++ modules/pybal/manifests/init.pp
@@ -1,5 +1,12 @@
 class pybal {
+
     package { [ 'ipvsadm', 'pybal' ]:
         ensure => installed;
+    }
+
+    file { '/usr/local/bin/pybal-eval-check':
+        ensure => file,
+        mode   => '0555',
+        source => 'puppet:///modules/pybal/pybal-eval-check.py',
     }
 }
