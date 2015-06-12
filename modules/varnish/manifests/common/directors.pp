a65b5a951642db7fa1e45b239feafea4314a4099
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-06-12 13:19:50 +0200

    varnish: actually use the dynamic directors list
    Bug: T97975
    Change-Id: Ib0a363bfbe44ae526fb1bd2bba12b4e41830a68d

--- modules/varnish/manifests/common/directors.pp
+++ modules/varnish/manifests/common/directors.pp
@@ -9,46 +9,40 @@ define varnish::common::directors(
     $options,
     $extraopts)
 {
-    include role::cache::base
-    $tier = $::role::cache::base::cluster_tier
     $conftool_namespace = '/conftool/v1/pools'
 
     # backends in the primary DC don't need this, so we bail out
-    unless defined(Class['Role::Cache::1layer'])
-        or ($instance == 'backend'and $tier == 'one') {
+    require varnish::common::director_scripts
 
-        require varnish::common::director_scripts
+    $director_list = $instance ? {
+        'frontend' => keys($directors),
+        'backend'  => $directors[$::mw_primary],
+        default    => undef
+    }
 
-        $director_list = $instance ? {
-            'frontend' => keys($directors),
-            'backend'  => $directors[$::mw_primary],
-            default    => undef
-        }
+    if $director_list == undef {
+        fail("Invalid instance type ${instance}")
+    }
 
-        if $director_list == undef {
-            fail("Invalid instance type ${instance}")
-        }
+    $dc = $instance ? {
+        'frontend' => $::site,
+        default    => $::mw_primary,
+    }
 
-        $dc = $instance ? {
-            'frontend' => $::site,
-            default    => $::mw_primary,
-        }
+    $service_name = $instance ? {
+        'frontend' => 'varnish-frontend',
+        default    => 'varnish'
+    }
 
-        $service_name = $instance ? {
-            'frontend' => 'varnish-frontend',
-            default    => 'varnish'
-        }
+    # usual old trick
+    $group = hiera('cluster', $cluster)
 
-        # usual old trick
-        $group = hiera('cluster', $cluster)
+    $keyspace = "${conftool_namespace}/${dc}/${group}/varnish-be"
 
-        $keyspace = "${conftool_namespace}/${dc}/${group}/varnish-be"
-
-        confd::file { "/etc/varnish/directors.${instance}.vcl":
-            ensure     => present,
-            watch_keys => [$keyspace],
-            content    => template('varnish/vcl/directors.vcl.tpl.erb'),
-            reload     => "/usr/local/bin/confd-reload-vcl ${service_name} ${extraopts}",
-        }
+    confd::file { "/etc/varnish/directors.${instance}.vcl":
+        ensure     => present,
+        watch_keys => [$keyspace],
+        content    => template('varnish/vcl/directors.vcl.tpl.erb'),
+        reload     => "/usr/local/bin/confd-reload-vcl ${service_name} ${extraopts}",
     }
 }
