62e1214e97d96078dd7f59666532a45607970627
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-06-12 11:51:03 +0200

    varnish: add generation of the dynamic list of directors
    For all 2-layer cache frontends, and for the tier-2 backends as well, we
    need to be able to depool backends syncronously with depooling happening
    in pybal, so that any maintenance activity can proceed.
    
    For now, we just add a confd::file define (which is opt-in, with a hiera
    variable) that allows doing that.
    
    Bug: T97975
    Change-Id: I78f0a2afb319712c150c0d5c69b32e1b6c2905fe

---
+++ modules/varnish/manifests/common/directors.pp
@@ -0,0 +1,51 @@
+# Varnish::directors
+#
+# Writes a dedicated file with nodes and weights
+
+define varnish::common::directors(
+    $instance,
+    $directors,
+    $director_type,
+    $options,
+    $extraopts)
+{
+    include role::cache::base
+    $tier = $::role::cache::base::cluster_tier
+    $conftool_namespace = '/conftool/v1/pools'
+
+    # backends in the primary DC don't need this, so we bail out
+    unless defined(Class['Role::Cache::1layer'])
+        or ($instance == 'backend'and $tier == 'one') {
+
+        require varnish::common::director_scripts
+
+        $director_list = $instance ? {
+            'frontend' => keys($directors),
+            'backend'  => keys($directors[$tier]),
+            default    => undef
+        }
+
+        if $director_list == undef {
+            fail("Invalid instance type ${instance}")
+        }
+
+        $dc = $instance ? {
+            'frontend' => $::site,
+            default    => $::mwprimary,
+        }
+
+        # usual old trick
+        $group = hiera('cluster', $cluster)
+
+        $keyspace = "${conftool_namespace}/${dc}/${group}/varnish-be"
+
+
+        # The list of directors here is made of one element.
+        $director = $directors[0]
+        confd::file { "/etc/varnish/directors.${instance}.vcl":
+            ensure     => present,
+            watch_keys => [$keyspace],
+            content    => template('varnish/vcl/directors.vcl.tpl.erb'),
+        }
+    }
+}
