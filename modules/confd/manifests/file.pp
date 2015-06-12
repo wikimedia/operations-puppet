894d2ba0d5497e265b829a91009048cd536a5c72
cpettet <rush@wikimedia.org>
2015-06-12 18:13:45 -0500

    confd additions for pybal
    * template-ize a passed in string for
      go lang template inside of the defined type
      to get the correct scope variables
    
    * no nrpe::monitor_systemd_unit for precise
    
    This is a companion change to 217973
    
    Change-Id: Ic9367777879dbcebae72ef5c21b28899e9c6344b

--- modules/confd/manifests/file.pp
+++ modules/confd/manifests/file.pp
@@ -2,24 +2,21 @@
 #
 # Defines a service template to be monitored by confd,
 # and the corresponding geneated config file.
+
 define confd::file (
-    $watch_keys,
-    $ensure  = present,
-    $uid     = undef,
-    $gid     = undef,
-    $mode    = '0444',
-    $reload  = undef,
-    $check   = undef,
-    $source  = undef,
-    $content = undef,
+    $ensure     = 'present',
+    $watch_keys = [],
+    $uid        = undef,
+    $gid        = undef,
+    $mode       = '0444',
+    $reload     = undef,
+    $check      = undef,
+    $tmpl       = undef,
 ) {
-    $safe_name = regsubst($title, '\/', '_')
 
-    unless ($source or $content) {
-        fail('We either need a source file or a content for the config file')
-    }
+    $safe_name = regsubst($name, '/', '_', 'G')
+
     #TODO validate at least uid and guid
-
     file { "/etc/confd/conf.d/${safe_name}.toml":
         ensure  => $ensure,
         content => template('confd/service_template.toml.erb'),
@@ -28,8 +25,7 @@ define confd::file (
     file { "/etc/confd/templates/${safe_name}.tmpl":
         ensure  => $ensure,
         mode    => '0400',
-        source  => $source,
-        content => $content,
+        content => template($tmpl),
         notify  => Service['confd'],
     }
 }
