af2c95bd313338e7f59470404d59fbba2238ba00
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-06-12 11:51:03 +0200

    varnish: add generation of the dynamic list of directors
    For all 2-layer cache frontends, and for the tier-2 backends as well, we
    need to be able to depool backends syncronously with depooling happening
    in pybal, so that any maintenance activity can proceed.
    
    For now, we just add a confd::file define (which is opt-in, with a hiera
    variable) that allows doing that.
    
    Bug: T97975
    Change-Id: I78f0a2afb319712c150c0d5c69b32e1b6c2905fe

---
+++ modules/varnish/manifests/common/director_scripts.pp
@@ -0,0 +1,25 @@
+class varnish::common::director_scripts {
+    # The script that should restart varnish once the file is written.
+    file { '/usr/local/bin/confd-reload-vcl':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0544',
+        source => 'puppet:///modules/varnish/confd-reload-vcl'
+    }
+
+    file { '/usr/local/lib/nagios/plugins/check_vcl_reload':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/varnish/check_vcl_reload';
+    }
+
+
+    # TODO: extend the check to everything including the exec in puppet?
+    nrpe::monitor_service { 'confd_vcl_reload':
+        description  => 'Confd vcl based reload',
+        nrpe_command => '/usr/local/lib/nagios/plugins/check_vcl_reload',
+    }
+}
