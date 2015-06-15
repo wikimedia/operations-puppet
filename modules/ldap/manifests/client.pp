42df0d3f57b049f545cd258ce02fdb64509f2e78
YuviPanda <yuvipanda@gmail.com>
2015-06-15 20:01:47 +0100

    ssh: Remove differences between ssh config for precise vs trusty
    The backport of sshd (T102401) helps fix all these.
    
    Bug: T102401
    Change-Id: I88d6ee3633a592c3dfeaa9613db311941c6485fd

--- modules/ldap/manifests/client.pp
+++ modules/ldap/manifests/client.pp
@@ -176,33 +176,31 @@ class ldap::client::utils($ldapconfig) {
     }
 
     if $::realm == 'labs' {
-        if os_version('debian >= jessie || ubuntu >= trusty') {
-            # The 'ssh-key-ldap-lookup' tool is called during login ssh via AuthorizedKeysCommand.  It
-            #  returns public keys from ldap for the specified username.
-            # It is in /usr/sbin and not /usr/local/sbin because on Debian /usr/local is 0775
-            # and sshd refuses to use anything under /usr/local because of the permissive group
-            # permission there (and group is set to 'staff', slightly different from root).
-            # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=538392
-            file { '/usr/sbin/ssh-key-ldap-lookup':
-                owner  => 'root',
-                group  => 'root',
-                mode   => '0555',
-                source => 'puppet:///modules/ldap/scripts/ssh-key-ldap-lookup',
-            }
-            # For security purposes, sshd will only run ssh-key-ldap-lookup as the 'ssh-key-ldap-lookup' user.
-            user { 'ssh-key-ldap-lookup':
-                ensure => present,
-                system => true,
-            }
+        # The 'ssh-key-ldap-lookup' tool is called during login ssh via AuthorizedKeysCommand.  It
+        #  returns public keys from ldap for the specified username.
+        # It is in /usr/sbin and not /usr/local/sbin because on Debian /usr/local is 0775
+        # and sshd refuses to use anything under /usr/local because of the permissive group
+        # permission there (and group is set to 'staff', slightly different from root).
+        # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=538392
+        file { '/usr/sbin/ssh-key-ldap-lookup':
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            source => 'puppet:///modules/ldap/scripts/ssh-key-ldap-lookup',
+        }
+        # For security purposes, sshd will only run ssh-key-ldap-lookup as the 'ssh-key-ldap-lookup' user.
+        user { 'ssh-key-ldap-lookup':
+            ensure => present,
+            system => true,
+        }
 
-            # Old names for the script and user ^
-            file { '/usr/local/sbin/ldapkeys':
-                ensure => absent,
-            }
+        # Old names for the script and user ^
+        file { '/usr/local/sbin/ldapkeys':
+            ensure => absent,
+        }
 
-            user { 'ldapkeys':
-                ensure => absent,
-            }
+        user { 'ldapkeys':
+            ensure => absent,
         }
     }
 
