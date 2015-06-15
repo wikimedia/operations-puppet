cba55d069b32ad9b9a981ce0884c34700a9c5f7c
andrewbogott <abogott@wikimedia.org>
2015-06-15 14:17:51 -0500

    Work around ipresolve's intolerance of IPs
    Change-Id: I5c99acd12d5dec9e8938e67b9d303167542e9893

--- manifests/role/dns.pp
+++ manifests/role/dns.pp
@@ -5,7 +5,11 @@ class role::dns::ldap {
 
     $ldapconfig = $ldap::role::config::labs::ldapconfig
 
-    $primary_ldap_dns = ipresolve(hiera('labs_ldap_dns_host'),4)
+    if hiera('labs_ldap_dns_ip_override') == undef {
+        $primary_ldap_dns = ipresolve(hiera('labs_ldap_dns_host'),4)
+    } else {
+        $primary_ldap_dns = hiera('labs_ldap_dns_ip_override')
+    }
 
     interface::ip { 'role::dns::ldap':
         interface => 'eth0',
