d463d096d545f0eda2275545603a6cf88d93209e
dzahn <dzahn@wikimedia.org>
2015-06-15 18:19:42 -0700

    labs/virtualization: switch to ganglia_new
    Change-Id: Ibe391052b274ac27f4176ce88ed8a11444fa75dd

--- manifests/site.pp
+++ manifests/site.pp
@@ -1235,7 +1235,6 @@ node 'iron.wikimedia.org' {
 
 node 'labcontrol1001.wikimedia.org' {
     $cluster               = 'virt'
-    $ganglia_aggregator    = true
     $is_puppet_master      = true
     $is_labs_puppet_master = true
     $use_neutron           = false
@@ -1281,8 +1280,6 @@ node 'labmon1001.eqiad.wmnet' {
 node 'labnet1001.eqiad.wmnet' {
     $cluster = 'virt'
     $use_neutron = false
-
-    $ganglia_aggregator = true
 
     include standard
     include role::nova::api
@@ -1357,8 +1354,6 @@ node /labstore100[12]\.eqiad\.wmnet/ {
 }
 
 node 'labstore1003.eqiad.wmnet' {
-    $ganglia_aggregator = true
-
     role labs::nfs::dumps
 }
 
