16983ab36527a68f678d308af1620ac1cc1b981e
YuviPanda <yuvipanda@gmail.com>
2015-06-16 15:19:41 +0100

    labs: Centralize config of which projects have NFS enabled
    This introduces:
      - A config file that is the canonical source of truth about
        which projects have which NFS mounts enabled.
      - A puppet function (nfs_volume_mounted) which can be called
        to check if a particular volume should be mounted in a
        particular project.
    
    This replaces the current setup, where the data is contained in
    LDAP (set via 'configure' in wikitech) and in hiera (to set
    which volumes puppet will actually try to mount).
    
    This also will introduce policy changes:
       - New projects will not get NFS by default anymore
       - Opting into NFS requires that the project owner pokes
         someone from the Ops team and explains their rationale.
         This should be reflected in the instructions for requesting
         a new project at T76375
    
    Follow up changes:
      - Update manage-nfs-volumes deamon to read from this config
        file than from LDAP
      - Continue auditing projects, removing NFS when not needed
        (See T102240)
    
    Bug: T102403
    Change-Id: I79b71ccd1b1b7c31aa28d590dcc82a9332a28928

---
+++ modules/openstack/lib/puppet/parser/functions/mount_nfs_volume.rb
@@ -0,0 +1,30 @@
+# == Function: mount_nfs_volume( project, mount )
+#
+# Copyright (c) 2015 Wikimedia Foundation Inc.
+#
+# Returns true if the mount should be mounted in
+# instance of the project
+#
+# Reads this information from openstack/files/nfs-mounts-config.yaml
+# in the openstack module of operations/puppet.git
+module Puppet::Parser::Functions
+  @@labs_nfs_config_touched = nil
+  @@labs_nfs_config = nil
+  newfunction(:mount_nfs_volume, :type => :rvalue, :arity => 2) do |args|
+    module_path = function_get_module_path(['openstack'])
+    path = "#{module_path}/files/nfs-mounts-config.yaml"
+    mtime = File.stat(path).mtime
+    if @@labs_nfs_config_touched.nil? || mtime != @@labs_nfs_config_touched
+        @@labs_nfs_config = function_loadyaml([path])
+        @@labs_nfs_config_touched = mtime
+    end
+    config = @@labs_nfs_config
+    project = args[0]
+    mount = args[1]
+    if config.has_key? project and config[project].has_key? mount
+      config[project][mount]
+    else
+      false
+    end
+  end
+end
