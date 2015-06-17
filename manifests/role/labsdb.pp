cb90d729e4c4c94d6c79d25bca8e3bcd6fa41e35
jcrespo <jcrespo@wikimedia.org>
2015-06-17 12:21:47 +0200

    Populate labsdb1004 with mariadb
    labsdb1004 intends to be a replica of labsdb1005 in order to
    provide at least hardware redundancy.
    
    labsdb1005 and labsdb1004 will be populated with a very similar-
    to-production puppet template for mariadb, including software
    installation, dependencies an minimal configuration files.
    
    Toolsmaster.my.cnf has been renamed as the configuration will be
    the same on both hosts, except for the role, prompt and read-only
    status (labsdb1004) will be in read-only mode.
    
    Data and replication topology is on purpose not done in puppet as
    that would risk data integrity.
    
    Replication monitoring is not enabled yet.
    
    Bug: T88718
    Change-Id: I643de5d9fcca2e6f68987821e068ad63fe55298f

--- manifests/role/labsdb.pp
+++ manifests/role/labsdb.pp
@@ -3,7 +3,7 @@
 class role::labs::db::master {
 
     system::role { 'role::labs::db::master':
-        description => 'Labs user database master',
+        description => "Labs user database master",
     }
 
     include standard
@@ -12,11 +12,37 @@ class role::labs::db::master {
     include role::mariadb::monitor
 
     class { 'mariadb::config':
-        prompt   => 'TOOLSDB',
-        config   => 'mariadb/toolsmaster.my.cnf.erb',
-        password => $passwords::misc::scripts::mysql_root_pass,
-        datadir  => '/srv/labsdb/data',
-        tmpdir   => '/tmp',
+        prompt    => "TOOLSDB master",
+        config    => "mariadb/tools.my.cnf.erb",
+        password  => $passwords::misc::scripts::mysql_root_pass,
+        datadir   => '/srv/labsdb/data',
+        tmpdir    => '/tmp',
+        read_only => 'OFF',
+    }
+}
+
+class role::labs::db::slave {
+
+    system::role { 'role::labs::db::slave':
+        description => "Labs user database slave",
     }
 
+    include standard
+    include mariadb::packages_wmf
+    include role::mariadb::grants
+    include role::mariadb::monitor
+
+    class { 'mariadb::config':
+        prompt    => "TOOLSDB slave",
+        config    => "mariadb/tools.my.cnf.erb",
+        password  => $passwords::misc::scripts::mysql_root_pass,
+        datadir   => '/srv/labsdb/data',
+        tmpdir    => '/tmp',
+        read_only => 'ON',
+    }
+
+    #mariadb::monitor_replication { 'tools':
+    #    multisource   => false,
+    #    contact_group => 'labs',
+    #}
 }
