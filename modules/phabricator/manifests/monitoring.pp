316ddff7423bd8db8ea569956f1809e2e376937f
cpettet <rush@wikimedia.org>
2015-06-17 12:21:48 -0500

    phab: update phd monitoring
    per upstream: "it's normal for the taskmasters
    to restart when there's an exception so it can
    show up as a failure when it's really
    working as intended"
    
    thus we are adjusting how we track procs
    
    Change-Id: Id6435deec2dd70aefd5f130715da3941285d0dfd

--- modules/phabricator/manifests/monitoring.pp
+++ modules/phabricator/manifests/monitoring.pp
@@ -3,8 +3,8 @@
 class phabricator::monitoring {
 
     nrpe::monitor_service { 'check_phab_taskmaster':
-        description   => 'check if phabricator taskmaster is running',
-        nrpe_command  => '/usr/lib/nagios/plugins/check_procs -w 1:3 -c 1:5 --ereg-argument-array PhabricatorTaskmasterDaemon',
+        description   => 'PHD should be supervising processes',
+        nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 3:150 -u phd'
         contact_group => 'admins,phabricator,sms',
     }
 
