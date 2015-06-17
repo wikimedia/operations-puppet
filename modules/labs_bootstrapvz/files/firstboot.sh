182cb605abdab591f20c3e206f3b15899ecce03a
andrewbogott <abogott@wikimedia.org>
2015-06-17 02:37:30 -0500

    Use 'jq' to get the project name from metadata on first boot.
    Change-Id: I1356b9f43eec389f0089bd4de14385f9002548f4

--- modules/labs_bootstrapvz/files/firstboot.sh
+++ modules/labs_bootstrapvz/files/firstboot.sh
@@ -65,11 +65,8 @@ then
 fi
 # At this point, all (the rest of) our disk are belong to LVM.
 
-binddn=`grep 'binddn' /etc/ldap.conf | sed 's/.* //'`
-bindpw=`grep 'bindpw' /etc/ldap.conf | sed 's/.* //'`
-hostsou=`grep 'nss_base_hosts' /etc/ldap.conf | sed 's/.* //'`
 ip=`curl http://169.254.169.254/1.0/meta-data/local-ipv4 2> /dev/null`
-project=`curl http://169.254.169.254/1.0/meta-data/project-id 2> /dev/null`
+project=`curl http://169.254.169.254/openstack/latest/meta_data.json/ | jq -r '.meta.project_id'`
 hostname=`hostname`
 # domain is the last two domain sections, e.g. eqiad.wmflabs
 domain=`hostname -d | sed -r 's/.*\.([^.]+\.[^.]+)$/\1/'`
