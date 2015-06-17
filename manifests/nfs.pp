f9a598950f5dcc1307072deeaaa985ccc33730de
Faidon Liambotis <faidon@wikimedia.org>
2015-06-17 23:03:49 +0300

    nfs: kill most classes, now defunct
    We aren't using /home on NFS for a long time now.
    
    Change-Id: I612a7c93e641bd60bbdbb39adba13eccd1aae47a

--- manifests/nfs.pp
+++ manifests/nfs.pp
@@ -9,69 +9,6 @@ class nfs::common {
     }
 }
 
-class nfs::server {
-
-    include nfs::common
-
-    package { 'nfs-kernel-server':
-        ensure => 'latest',
-    }
-
-    if $static_nfs {
-        file { '/etc/default/nfs-common':
-            ensure  => 'present',
-            mode    => '0444',
-            owner   => 'root',
-            group   => 'root',
-            source  => 'puppet:///files/nfs/nfs-common',
-            require => Package['nfs-common'],
-        }
-
-        file { '/etc/default/nfs-kernel-server':
-            ensure  => 'present',
-            mode    => '0444',
-            owner   => 'root',
-            group   => 'root',
-            source  => 'puppet:///files/nfs/nfs-kernel-server',
-            require => Package['nfs-kernel-server'],
-        }
-
-        file { '/etc/default/quota':
-            ensure  => 'present',
-            mode    => '0444',
-            owner   => 'root',
-            group   => 'root',
-            source  => 'puppet:///files/nfs/quota',
-            require => Package['nfs-kernel-server'],
-        }
-
-        file { '/etc/modprobe.d/lockd.conf':
-            ensure  => 'present',
-            mode    => '0444',
-            owner   => 'root',
-            group   => 'root',
-            source  => 'puppet:///files/nfs/lockd.conf',
-            require => Package['nfs-kernel-server'],
-        }
-
-    }
-
-}
-
-class nfs::home {
-    include nfs::common
-    include nfs::home::wikipedia
-
-    # Class admins tests whether Mount["/home"] is defined
-    mount { '/home':
-        ensure  => 'mounted',
-        device  => '10.0.5.8:/home',
-        fstype  => 'nfs',
-        name    => '/home',
-        options => 'bg,tcp,rsize=8192,wsize=8192,timeo=14,intr',
-    }
-}
-
 # Classes for NetApp mounts used on multiple servers
 
 class nfs::netapp::common {
@@ -128,44 +65,6 @@ class nfs::netapp::home::othersite($ensure='mounted', $mountpoint=undef) {
     }
 }
 
-# Historical /home/wikipedia
-class nfs::home::wikipedia {
-
-    # lint:ignore:case_without_default
-    case $::realm {
-    # lint:endignore
-        'production': {
-            require nfs::home
-            file { '/home/wikipedia':
-                ensure => 'directory',
-                mode   => '0755',
-                owner  => 'root',
-                group  => 'root',
-            }
-        } # /production
-        'labs': {
-
-            group { 'wikipediauser':
-                ensure => present,
-                name   => 'wikipediauser',
-                system => true,
-            }
-
-            user { 'wikipediauser':
-                home       => '/home/wikipedia',
-                managehome => true,
-                system     => true,
-            }
-
-            file { '/home/wikipedia':
-                ensure  => 'directory',
-                require => User['wikipediauser'],
-            }
-        }
-    }
-
-}
-
 class nfs::data {
     include nfs::common
 
@@ -209,53 +108,3 @@ class nfs::netapp::fr_archive(
     }
 }
 
-# moved here from misc-servers.pp
-class misc::nfs-server::home {
-    system::role { 'misc::nfs-server::home': description => '/home NFS' }
-
-    class backup {
-        file { '/root/.ssh/home-rsync':
-            ensure => absent,
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0400',
-            source => 'puppet:///private/backup/ssh-keys/home-rsync',
-        }
-    }
-
-    package { 'nfs-kernel-server':
-        ensure => 'latest',
-    }
-
-    file { '/etc/exports':
-        require => Package['nfs-kernel-server'],
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        source  => 'puppet:///files/nfs/exports.home',
-    }
-
-    service { 'nfs-kernel-server':
-        require   => [ Package['nfs-kernel-server'], File['/etc/exports'] ],
-        subscribe => File['/etc/exports'],
-    }
-
-    class monitoring {
-        monitoring::service { 'nfs': description => 'NFS', check_command => 'check_tcp!2049' }
-    }
-
-    include monitoring
-}
-
-class misc::nfs-server::home::rsyncd {
-    system::role { 'misc::nfs-server::home::rsyncd': description => '/home rsync daemon' }
-
-    include rsync::server
-    include network::constants
-
-    rsync::server::module { 'httpdconf':
-        path        => '/home/wikipedia/conf/httpd',
-        read_only   => 'yes',
-        hosts_allow => $::network::constants::mw_appserver_networks,
-    }
-}
