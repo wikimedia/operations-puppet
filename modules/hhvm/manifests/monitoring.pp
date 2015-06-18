2cfdf6c7b41da9168b8af556e62f0943feb06966
Ori Livneh <ori@wikimedia.org>
2015-06-17 17:03:24 -0700

    Revert "hhvm: add monitoring for translation cache space"
    The alerts are neither informative nor actionable.
    
    Change-Id: I7124c0055ca882da91df540ddf98e635f9bdc174

--- modules/hhvm/manifests/monitoring.pp
+++ modules/hhvm/manifests/monitoring.pp
@@ -43,20 +43,4 @@ class hhvm::monitoring {
         require => File['/usr/lib/ganglia/python_modules/hhvm_health.py'],
         notify  => Service['ganglia-monitor'],
     }
-
-    ## NRPE check for TC cache state
-    file { '/usr/local/bin/check_tc_space':
-        ensure => present,
-        source => 'puppet:///modules/hhvm/monitoring/hhvm_tc_space.py',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
-    }
-
-    nrpe::monitor_service { 'hhvm_tc_space':
-        ensure       => present,
-        nrpe_command => '/usr/local/bin/check_tc_space -w 80 -c 90',
-        description  => 'Translation cache space',
-        require      => File['/usr/local/bin/check_tc_space']
-    }
 }
