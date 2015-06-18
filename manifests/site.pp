9c698922c22ebf0f355a98189360279b016ed3a3
Dzahn <dzahn@wikimedia.org>
2015-06-18 03:02:23 +0000

    Revert "logstash: switch to ganglia_new"
    This reverts commit 917a7394527b8ef0d966e199e4ab952631bf8643.
    
    Change-Id: Ib85c9b91adba99e58056adb9745c36279486d9ad

--- manifests/site.pp
+++ manifests/site.pp
@@ -1392,6 +1392,9 @@ node 'lithium.eqiad.wmnet' {
 }
 
 node /^logstash100[1-3]\.eqiad\.wmnet$/ {
+    if $::hostname =~ /^logstash100[13]$/ {
+        $ganglia_aggregator = true
+    }
     role logstash, kibana, logstash::apifeatureusage
 }
 node /^logstash100[4-6]\.eqiad\.wmnet$/ {
