651d5d150cac597ec70c5da2d5d16d7ed6bffd29
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2015-06-18 18:02:33 +0200

    dynamicproxy/tools: set up outage error system
    This produces a prod-like error message.
    
    Configured using Hiera:
    "dynamicproxy::error_enabled": true
    "dynamicproxy::error_message": "Our servers are experiencing technical issues ...etc"
    "dynamicproxy::error_details": "..."
    
    Bug: T102971
    Change-Id: Ie040cb539cc402b526a18216595f75e9505dff12

--- modules/dynamicproxy/manifests/init.pp
+++ modules/dynamicproxy/manifests/init.pp
@@ -20,6 +20,15 @@ class dynamicproxy (
     $notfound_servers     = [],
     $set_xff              = false,
     $redis_replication    = undef,
+    $error_enabled        = false,
+    $error_config         = {
+        title => "Wikimedia Labs Error",
+        logo => "/labs-logo.png",
+        logo_2x => "/labs-logo-2x.png",
+        logo_alt => "Wikimedia Labs",
+    },
+    $error_description    = "Our servers are currently experiencing a technical problem. This is probably temporary and should be fixed soon. Please try again later.",
+    $error_details        = undef,
 ) {
     if $ssl_certificate_name != false and $ssl_settings == undef {
         fail('ssl_certificate_nme set but ssl_settings not set')
@@ -59,6 +68,19 @@ class dynamicproxy (
         content => template('dynamicproxy/nginx.conf'),
         require => Package['nginx-common'],
         notify  => Service['nginx'],
+    }
+
+    file { '/var/www-error':
+        ensure => directory,
+    }
+
+    file { '/var/www-error/errorpage.html':
+        ensure => file,
+        content => template('dynamicproxy/errorpage.erb'),
+        require => [File['/var/www-error'],
+                    File['/var/www-error/labs-logo.png'],
+                    File['/var/www-error/labs-logo-2x.png']
+                   ]
     }
 
     file { '/etc/security/limits.conf':
