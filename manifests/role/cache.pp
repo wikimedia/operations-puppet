dd0afbc62b28204815705ce7162fd4e21850e4d1
Brandon Black <bblack@wikimedia.org>
2015-06-19 12:44:35 +0000

    add codfw caches icinga monitoring groups
    Change-Id: Ie4ccd80f9434b0afeb00b54e0616d24647b2445e

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -1,14 +1,18 @@
 # XXX this needs to be refactored Elsewhere
 # Virtual resources for the monitoring server
+@monitoring::group { 'cache_text_codfw': description => 'codfw text Varnish' }
 @monitoring::group { 'cache_text_eqiad': description => 'eqiad text Varnish' }
 @monitoring::group { 'cache_text_esams': description => 'esams text Varnish' }
 @monitoring::group { 'cache_text_ulsfo': description => 'ulsfo text Varnish' }
+@monitoring::group { 'cache_upload_codfw': description => 'codfw upload Varnish' }
 @monitoring::group { 'cache_upload_eqiad': description => 'eqiad upload Varnish' }
 @monitoring::group { 'cache_upload_esams': description => 'esams upload Varnish' }
 @monitoring::group { 'cache_upload_ulsfo': description => 'ulsfo upload Varnish' }
+@monitoring::group { 'cache_bits_codfw': description => 'codfw bits Varnish' }
 @monitoring::group { 'cache_bits_eqiad': description => 'eqiad bits Varnish' }
 @monitoring::group { 'cache_bits_esams': description => 'esams bits Varnish' }
 @monitoring::group { 'cache_bits_ulsfo': description => 'ulsfo bits Varnish' }
+@monitoring::group { 'cache_mobile_codfw': description => 'codfw mobile Varnish' }
 @monitoring::group { 'cache_mobile_eqiad': description => 'eqiad mobile Varnish' }
 @monitoring::group { 'cache_mobile_esams': description => 'esams mobile Varnish' }
 @monitoring::group { 'cache_mobile_ulsfo': description => 'ulsfo mobile Varnish' }
