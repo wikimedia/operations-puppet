59f6fd4a7e337b018e4ec9e7e62f0c556cea6e50
Alex Monk <krenair@gmail.com>
2015-06-19 17:23:08 +0100

    Get rid of unnecessary WikitechPrivateSettings.php
    wgSecretKey and wgCaptchaSecret were just overriding the existing private
    settings, and wgUpgradeKey should not have been getting used in production.
    
    Bug: T102361
    Change-Id: I33108ee1d2b59b028de43cab044fe8c1f7f7d2b2

--- modules/wikitech/manifests/wiki/passwords.pp
+++ modules/wikitech/manifests/wiki/passwords.pp
@@ -1,24 +1,10 @@
-
 class wikitech::wiki::passwords {
-    include passwords::wikitech
     include passwords::openstack::nova
-
-    $wikitech_secret_key     = $passwords::wikitech::wikitech_secret_key
-    $wikitech_upgrade_key    = $passwords::wikitech::wikitech_upgrade_key
-    $wikitech_captcha_secret = $passwords::wikitech::wikitech_captcha_secret
 
     $wikitech_nova_ldap_proxyagent_pass = $passwords::openstack::nova::nova_ldap_proxyagent_pass
     $wikitech_nova_ldap_user_pass       = $passwords::openstack::nova::nova_ldap_user_pass
 
     # Drop this file onto the mediawiki deployment host so that the passwords are deployed
-    file { '/srv/mediawiki/private/WikitechPrivateSettings.php':
-        ensure  => present,
-        content => template('wikitech/wikitech_private.php.erb'),
-        mode    => '0444',
-        owner   => 'mwdeploy',
-        group   => 'mwdeploy',
-    }
-
     file { '/srv/mediawiki/private/WikitechPrivateLdapSettings.php':
         ensure  => present,
         content => template('wikitech/wikitech_ldap.php.erb'),
