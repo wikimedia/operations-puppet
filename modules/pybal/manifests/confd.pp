3466453a9d6285d50b496f17832351ea354c97d6
cpettet <rush@wikimedia.org>
2015-06-19 17:00:43 -0500

    Setup a node pool file from etcd for lvs cluster
    With a conftool cluster and service definition
    included in the lvs::configuration cluster hash
    we setup a confd toml and template to allow
    external definition of nodes in the pool.  The
    config line change to actually use this etcd
    sourced config is commented out as this will
    rollout slowly.
    
    Example of confd files created for 'text':
    
    /etc/confd/
    ├── conf.d
    │   └── _etc_pybal_pools_text.toml
    └── templates
        └── _etc_pybal_pools_text.tmpl
    
    Example toml file:
    
    [template]
    src = "_etc_pybal_pools_text.tmpl"
    dest = "/etc/pybal/pools/text"
    mode = "0444"
    keys = [
            "/conftool/v1/pools/eqiad/cache_text/varnish-fe/",
        ]
    check_cmd = "/usr/local/bin/pybal-eval-check {{.src}}"
    
    This change assumes each lvs service has a conftool service
    and cluster reference.
    
    Change-Id: I6c0179738bc3dab0170a5ccba20f53cd42a06b79

--- modules/pybal/manifests/confd.pp
+++ modules/pybal/manifests/confd.pp
@@ -1,5 +1,10 @@
 class pybal::confd {
 
+    file { '/etc/pybal/pools':
+        ensure => directory,
+        require => Package[pybal],
+    }
+
     file { '/usr/local/bin/pybal-eval-check':
         ensure => file,
         mode   => '0555',
---
+++ modules/pybal/manifests/pool.pp
@@ -0,0 +1,26 @@
+define pybal::pool(
+    $lvs_services={},
+    $lvs_class_hosts={},
+) {
+
+    $service_config = $lvs_services[$name]
+
+    # This shadows pybal template for selection
+    if member($service_config['sites'], $::site) {
+        if member($lvs_class_hosts[$service_config['class']], $::hostname) {
+
+            $cluster = $service_config['conftool']['cluster']
+            $service = $service_config['conftool']['service']
+
+            $watch_keys = ["/conftool/v1/pools/$::site/$cluster/$service/"]
+            $tmpl = template('pybal/host-pool.tmpl.erb')
+
+            confd::file{ "/etc/pybal/pools/$name":
+                watch_keys => $watch_keys,
+                content    => $tmpl,
+                check      => '/usr/local/bin/pybal-eval-check',
+                require    => File['/etc/pybal/pools'],
+            }
+        }
+    }
+}
