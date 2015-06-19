6f6413a474655ecffb2ebac1923f837e08c4d59f
cpettet <rush@wikimedia.org>
2015-06-19 17:00:43 -0500

    WIP: Setup a node pool from etcd for lvs cluster
    With a conftool cluster and service definition
    included in the lvs::configuration cluster hash
    we setup a confd toml and template to allow
    external definition of nodes in the pool.  The
    config line change is commented out as this will
    rollout slowly.
    
    Example of confd files created for 'text':
    
    /etc/confd/
    ├── conf.d
    │   └── _etc_pybal_pools_text.toml
    └── templates
        └── _etc_pybal_pools_text.tmpl
    
    Example toml file:
    
    [template]
    src = "_etc_pybal_pools_text.tmpl"
    dest = "/etc/pybal/pools/text"
    mode = "0444"
    keys = [
            "/conftool/v1/pools/eqiad/cache_text/varnish-fe/",
        ]
    check_cmd = "/usr/local/bin/pybal-eval-check {{.src}}"
    
    Change-Id: I6c0179738bc3dab0170a5ccba20f53cd42a06b79

---
+++ modules/pybal/manifests/pool.pp
@@ -0,0 +1,21 @@
+define pybal::pool($lvs_services={}) {
+    $service_config = $lvs_services[$name]
+
+    # this check should only exist for as long as
+    # we have clusters not using etcd.
+    if has_key($service_config, 'conftool') {
+
+        $cluster = $service_config['conftool']['cluster']
+        $service = $service_config['conftool']['service']
+
+        $watch_keys = ["/conftool/v1/pools/$::site/$cluster/$service/"]
+        $tmpl = template('pybal/host-pool.tmpl.erb')
+
+        confd::file{ "/etc/pybal/pools/$name":
+            watch_keys => $watch_keys,
+            content    => $tmpl,
+            check      => '/usr/local/bin/pybal-eval-check',
+            require    => File['/etc/pybal/pools'],
+        }
+    }
+}
