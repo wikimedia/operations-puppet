f13cf95b62bdafb0020ff7d58356dbad580f350b
andrewbogott <abogott@wikimedia.org>
2015-06-22 09:19:31 -0500

    Only back up the current state of wikitech.
    This will make our sync to wikitech-static much less painful.
    
    Bug: T101803
    Change-Id: Ida4a8f47546d70a6f4e43a8a816e58ec19e32b58

--- modules/openstack/files/mw-xml.sh
+++ modules/openstack/files/mw-xml.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /srv/mediawiki/php
-/usr/local/bin/mwscript maintenance/dumpBackup.php labswiki --full --uploads | nice -n 19 gzip -9 > /a/backup/public/labswiki-$(date '+%Y%m%d').xml.gz
+/usr/local/bin/mwscript maintenance/dumpBackup.php labswiki --current --uploads | nice -n 19 gzip -9 > /a/backup/public/labswiki-$(date '+%Y%m%d').xml.gz
 cd -
