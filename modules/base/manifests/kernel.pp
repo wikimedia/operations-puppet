0d3e7ec3412e26e700ef134905fb86bc493858dd
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2015-06-22 10:03:51 +0200

    Blacklist kernel modules
    Maintain a list of kernel modules blacklisted from autoloading through udev
    or other automated loading mechanisms. Initially only blacklist overlayfs,
    but more to follow in a later commit.
    
    Bug: T102600
    Change-Id: I3325cf131e9e18078bcdf657e14aab0b4ba27872

---
+++ modules/base/manifests/kernel.pp
@@ -0,0 +1,14 @@
+# == Class: base::kernel
+#
+# Settings related to the Linux kernel (currently only blacklisting
+# risky kernel modules)
+#
+class base::kernel
+{
+    file { '/etc/modprobe.d/blacklist-wmf.conf':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/base/kernel/blacklist-wmf.conf',
+}
