fd6f3d08e351bd2ace390343113500e7bc43861a
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-06-22 16:33:33 -0700

    puppetmaster: split frontend scripts
    Backend puppetmaster don't have new_install key installed, thus we should copy
    dependent scripts only if the key is available. This fixes puppet broken on the
    backends after https://gerrit.wikimedia.org/r/#/c/217016/
    
    Change-Id: Ifa91e035fa37f538804d8ac6a881422cda97ee32

---
+++ modules/puppetmaster/manifests/scripts/frontend.pp
@@ -0,0 +1,25 @@
+# Class: puppetmaster::scripts::frontend
+#
+# This class installs some puppetmaster server side scripts required by
+# frontend puppetmaster.
+#
+class puppetmaster::scripts::frontend {
+    # Helper script to clean stored data about a server we're reimaging.
+    file { '/usr/local/bin/wmf-reimage':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0544',
+        source  => 'puppet:///modules/puppetmaster/wmf-reimage',
+        require => Class['role::access_new_install'],
+    }
+
+    file { '/usr/local/sbin/install-console':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0544',
+        source  => 'puppet:///modules/puppetmaster/install-console',
+        require => Class['role::access_new_install'],
+    }
+}
