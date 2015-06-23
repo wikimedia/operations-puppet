ab17058cbd701afda069f49a714967a2cb35db05
YuviPanda <yuvipanda@gmail.com>
2015-06-23 12:06:34 +0100

    quarry: Add query killer role
    Change-Id: I731d7010243bd94cd9d74768f0cf3732168bd5c6

--- manifests/role/labsquarry.pp
+++ manifests/role/labsquarry.pp
@@ -31,3 +31,9 @@ class role::labs::quarry::redis {
         require => Mount['/srv']
     }
 }
+
+# Should be included on an instance that already has
+# a Quarry install (celery or web) setup
+class role::labs::quarry::killer {
+    include quarry::querykiller
+}
