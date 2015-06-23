8a8c068c5a76775a716218084b81b4a8b2a31ab5
Antoine Musso <hashar@free.fr>
2015-06-23 23:07:27 +0200

    contint: for Jessie s/ruby1.9.3/ruby2.1/
    ruby1.9.3 is on Ubuntu Precise and Trusty.
    Debian Jessie has ruby2.1
    
    Same for the -dev package.
    
    Bug: T103600
    Change-Id: Ifa94a2038d47d2976374f7b0790fa6374c742677

--- modules/contint/manifests/packages.pp
+++ modules/contint/manifests/packages.pp
@@ -59,7 +59,6 @@ class contint::packages {
 
         'asciidoc',
         'rake',
-        'ruby1.9.3',  # To let us syntax check scripts using 1.9
 
         'pep8',
         'pyflakes',
@@ -112,6 +111,18 @@ class contint::packages {
         # already has a package called 'node'
         package { 'nodejs-legacy':
             ensure => latest,
+        }
+    }
+
+    # Ruby
+    if os_version('ubuntu <= trusty') {
+        package { 'ruby1.9.3':
+            ensure => present,
+        }
+    }
+    if os_version('debian >= jessie') {
+        package { 'ruby2.1':
+            ensure => present,
         }
     }
 
