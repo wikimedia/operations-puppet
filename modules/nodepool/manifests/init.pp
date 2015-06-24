63758b08c77105a2ade9584c9ad78f46ac4b9b38
Antoine Musso <hashar@free.fr>
2015-06-24 14:15:58 +0200

    nodepool: provide openstack env variables to system user
    Set some OS_* env variables in nodepool .profile, let us easily interact
    with the OpenStack API since the CLI utilities recognizes the variables.
    
    Using shell_exports() from puppet wmflib module which has been used in
    our apache module already.
    
    Bug: T103673
    Change-Id: Ia912f444af2dd2a0fab3b257e01b317f880e69f9

--- modules/nodepool/manifests/init.pp
+++ modules/nodepool/manifests/init.pp
@@ -1,8 +1,33 @@
-class nodepool( $nova_controller_hostname ) {
+class nodepool(
+    $nova_controller_hostname,
+    $openstack_username,
+    $openstack_password,
+    $openstack_tenant_id,
+) {
 
     package { 'nodepool':
         ensure => present,
     }
+
+    $openstack_auth_url = "http://${nova_controller_hostname}:35357/v2.0/"
+
+    $nodepool_user_env = {
+        os_auth_url  => $openstack_auth_url,
+        os_username  => $openstack_username,
+        os_password  => $openstack_password,
+        os_tenant_id => $openstack_tenant_id,
+    }
+    validate_hash($nodepool_user_env)
+
+    file { '/var/lib/nodepool/.profile':
+        ensure  => present,
+        require => Package['nodepool'],  # provides nodepool user and homedir
+        owner   => 'nodepool',
+        group   => 'nodepool',
+        mode    => '0440',
+        content => shell_exports($nodepool_user_env),
+    }
+
 
     # OpenStack CLI
     package { 'python-openstackclient':
