6992e867b1df75db3fb96dfe6c0676f8da1e1084
Antoine Musso <hashar@free.fr>
2015-06-24 14:36:18 +0200

    nodepool: element to prepare an image for Jenkins
    A basic diskimage-builder element that takes care of copying materials
    to an image and setting up Java.  That lets Nodepool pool the instance
    in Jenkins and Jenkins to connect to it over ssh.
    
    Change-Id: I2acbc07823297990cd1ed7686e6e89150560b1ce

--- modules/nodepool/manifests/init.pp
+++ modules/nodepool/manifests/init.pp
@@ -130,8 +130,21 @@ class nodepool(
         mode    => '0600',
     }
 
+    file { '/etc/nodepool/elements':
+        ensure  => directory,
+        owner   => 'nodepool',
+        group   => 'nodepool',
+        recurse => true,
+        pruge   => true,
+        source  => 'puppet:///modules/nodepool/elements',
+        require => Package['nodepool'],
+    }
+
     file { '/etc/nodepool/nodepool.yaml':
         content => template('nodepool/nodepool.yaml.erb'),
-        require => Package['nodepool'],
+        require => [
+            Package['nodepool'],
+            File['/etc/nodepool/elements'],
+        ]
     }
 }
