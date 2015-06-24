222afd5ddbdd485c5ed325478addd05d9e36d8a7
Antoine Musso <hashar@free.fr>
2015-06-24 14:15:58 +0200

    nodepool: provide openstack env variables to system user
    Set some OS_* env variables in nodepool .profile, let us easily interact
    with the OpenStack API since the CLI utilities recognizes the variables.
    
    Using shell_exports() from puppet wmflib module which has been used in
    our apache module already.
    
    Bug: T103673
    Change-Id: Ia912f444af2dd2a0fab3b257e01b317f880e69f9

--- manifests/role/nodepool.pp
+++ manifests/role/nodepool.pp
@@ -4,9 +4,13 @@ class role::nodepool {
     system::role { 'role::nodepool': description => 'CI Nodepool' }
 
     include role::nova::config
+    include passwords::nodepool
 
     class { '::nodepool':
         nova_controller_hostname => $role::nova::config::novaconfig['controller_hostname'],
+        openstack_username       => 'nodepoolmanager',
+        openstack_password       => $passwords::nodepool::manager_pass,
+        openstack_tenant_id      => 'contintcloud',
     }
 
 }
