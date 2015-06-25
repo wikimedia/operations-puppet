2df531c0cb06088734feb34337e0cbd072e16afd
joal <joal@wikimedia.org>
2015-06-25 14:34:44 +0200

    Add new projectview to projectcounts aggregation
    Remove projectcounts data git repo and cron jobs from aggregator.pp, keeping only code git repo.
    Add aggregator/projectcounts.pp class, taking care of legacy projectcounts data git repo and cron jobs.
    Add aggregator/projectview.pp class, taking care of new projectview data git repo and cron jobs.
    
    Bug: T101118
    Change-Id: I98bc48535720935ace4a888680320602619dcbb1

---
+++ modules/statistics/manifests/aggregator/projectview.pp
@@ -0,0 +1,76 @@
+# == Class statistics::aggregator::projectview
+# Handles aggregation of projectview_hourly files
+#
+# WARNING - Files aggregated by this instance are using the
+# new pageview definition. The legacy ones are managed by
+# projectcounts.pp in the same folder.
+#
+class statistics::aggregator::projectview {
+    Class['::statistics::aggregator'] -> Class['::statistics::aggregator::projectview']
+
+    # This class uses the cdh::hadoop::mount in order to get
+    # data files out of HDFS.
+    Class['cdh::hadoop::mount'] -> Class['::statistics::aggregator::projectview']
+
+    $script_path      = $::statistics::aggregator::script_path
+    $working_path     = "${::statistics::aggregator::working_path}/projectview"
+    $data_repo_path   = "${working_path}/data"
+    $data_path        = "${data_repo_path}/projectview"
+    $log_path         = "${working_path}/log"
+    # This should not be hardcoded.  Instead, one should be able to use
+    # $::cdh::hadoop::mount::mount_point to reference the user supplied
+    # parameter when the cdh::hadoop::mount class is evaluated.
+    # I am not sure why this is not working.
+    $hdfs_mount_point = '/mnt/hdfs'
+    $hdfs_source_path = "${hdfs_mount_point}/wmf/data/archive/projectview/webstatcollector/hourly"
+    $user             = $::statistics::user::username
+    $group            = $::statistics::user::username
+
+    file { $working_path:
+        ensure => 'directory',
+        owner  => $user,
+        group  => $group,
+        mode   => '0755'
+    }
+
+    git::clone { 'aggregator_projectview_data':
+        ensure    => 'latest',
+        directory => $data_repo_path,
+        origin    => 'https://gerrit.wikimedia.org/r/p/analytics/aggregator/projectview/data.git',
+        owner     => $user,
+        group     => $group,
+        mode      => '0755',
+        require   => File[$working_path],
+    }
+
+    file { $log_path:
+        ensure  => 'directory',
+        owner   => $user,
+        group   => $group,
+        mode    => '0755',
+        require => File[$working_path],
+
+    }
+
+    # Cron for doing the basic aggregation step itself
+    cron { 'aggregator projectview aggregate':
+        command => "${script_path}/bin/aggregate_projectcounts --source ${hdfs_source_path} --target ${data_path} --first-date=`date --date='-8 day' +\\%Y-\\%m-\\%d` --last-date=`date --date='-1 day' +\\%Y-\\%m-\\%d` --push-target --log ${log_path}/`date +\\%Y-\\%m-\\%d--\\%H-\\%M-\\%S`.log",
+        user    => $user,
+        hour    => '13',
+        minute  => '0',
+        require => [
+            Git::Clone['aggregator_code'],
+            Git::Clone['aggregator_projectview_data'],
+            File[$log_path],
+        ],
+    }
+
+    # Cron for basing monitoring of the aggregated data
+    cron { 'aggregator projectview monitor':
+        command => "${script_path}/bin/check_validity_aggregated_projectcounts --data ${data_path}",
+        user    => $user,
+        hour    => '13',
+        minute  => '45',
+        require => Cron['aggregator projectview aggregate'],
+    }
+}
