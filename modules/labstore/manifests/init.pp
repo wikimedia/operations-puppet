90736f9b7f6982f7ba159e16743ee80e7e1e05a7
Coren <marc@uberbox.org>
2015-06-25 10:41:31 -0400

    Add a cross-labstore key for backups
    For unattended backups.
    
    Change-Id: I82fcf8774b1ee5d652af175ba3831f343195dbb1

--- modules/labstore/manifests/init.pp
+++ modules/labstore/manifests/init.pp
@@ -9,5 +9,23 @@ class labstore {
     
     require_package('nfs-kernel-server')
 
+    # Labstores need to be able to scp from one server
+    # to the other (in order to do backups)
+
+    ssh::userkey { 'root-labstore':
+        ensure => present,
+        user   => 'root',
+        skey   => 'labstore',
+        source => 'puppet:///modules/labstore/id_labstore.pub',
+    }
+
+    file { '/root/.ssh/id_labstore':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0400',
+        source => 'puppet:///private/labstore/id_labstore',
+    }
+
 }
 
