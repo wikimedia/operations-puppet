5288cfebc3dfdfecb0819e81b2b15d235ab6b04d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-06-25 18:24:56 +0200

    varnish: add service to the directors options
    In some cases, we define additional directors based off of the backends
    we use for another director, but we may want to keep weighting
    separated. In this second case - this additional (optional) argument
    of the directors data structure will allow this. This is a fixup of
    I9f856c54493cc which made the "random backend" of the text caches use
    uneven weights.
    
    Change-Id: Ica50ecaa73362326e3da7e2b943346094c036f63

--- modules/varnish/manifests/common/directors.pp
+++ modules/varnish/manifests/common/directors.pp
@@ -34,12 +34,14 @@ define varnish::common::directors(
 
     # usual old trick
     $group = hiera('cluster', $cluster)
+    $keyspace_base = "${conftool_namespace}/${dc}/${group}"
+    $default_service = 'varnish-be'
 
-    $keyspace = "${conftool_namespace}/${dc}/${group}/varnish-be"
-
+    $keyspaces_str = inline_template("<%= @directors.values.map{ |v| \"#{@keyspace_base}/#{v['service'] || @default_service}\" }.join('|') %>")
+    $keyspaces = sort(unique(split($backends_str, '\|')))
     confd::file { "/etc/varnish/directors.${instance}.vcl":
         ensure     => present,
-        watch_keys => [$keyspace],
+        watch_keys => [$keyspaces],
         content    => template('varnish/vcl/directors.vcl.tpl.erb'),
         reload     => "/usr/local/bin/confd-reload-vcl ${service_name} ${extraopts}",
     }
