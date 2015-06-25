d7ef3312f16b6ab5f854da3372683cd59fbb9506
Andrew Bogott <abogott@wikimedia.org>
2015-06-25 22:22:23 +0000

    Add a labsproject fact that doesn't rely on ldap config.
    Bug: T93684
    Change-Id: If6815494038a4c5173ca1d484f68c7e5c3c5ecd2

---
+++ modules/base/lib/facter/labsproject.rb
@@ -0,0 +1,19 @@
+# labsproject.rb
+#
+# This fact pulls the labs project out of instance metadata
+
+require 'facter'
+
+Facter.add(:labsproject) do
+  setcode do
+    domain = Facter::Util::Resolution.exec("hostname -d").chomp
+    if domain.end_with? ".wmflabs"
+      # query the nova metadata service at 169.254.169.254
+      metadata = Facter::Util::Resolution.exec("curl -f http://169.254.169.254/openstack/2013-10-17/meta_data.json/ 2> /dev/null").chomp
+      metadata[/project-id\":\ \"(.*?)\"/,1]
+    else
+      nil
+    end
+  end
+end
+
