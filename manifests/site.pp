b57112d8166785e7f5a0e4200221f1ac831c11ad
dzahn <dzahn@wikimedia.org>
2015-06-24 17:00:05 -0700

    backup home dirs on bastion hosts per role
    It should be a property of the role bastionhost that home
    directories are being backed up.
    
    As opposed to indidivual hosts (but not all) having it added in site.pp
    
    Change-Id: Ie0129346e9116520caacadb63f0e63fa08aa9fbf

--- manifests/site.pp
+++ manifests/site.pp
@@ -248,8 +248,6 @@ node 'bast1001.wikimedia.org' {
         mountpoint => '/srv/home_pmtpa',
         mount_site => 'pmtpa',
     }
-    include role::backup::host
-    backup::set {'home': }
 }
 
 node 'bast2001.wikimedia.org' {
@@ -1140,8 +1138,6 @@ node 'iron.wikimedia.org' {
     include standard
     include role::ipmi
     include role::access_new_install
-    include role::backup::host
-    backup::set {'home': }
 }
 
 node 'labcontrol1001.wikimedia.org' {
