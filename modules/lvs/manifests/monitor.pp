1af1988936f92b3deb301a86b8dbc64e45211bc7
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-06-25 14:10:02 +0300

    lvs::monitor: merge configuration hashes
    A lot of the hashes in lvs::monitor are similar in nature and the
    corresponding create_resources call as well. Merge the hashes and amend
    names and calling respectively
    
    Change-Id: I3d8f2ab7e391c8f33ed4f581ab2418ba3297b253

--- modules/lvs/manifests/monitor.pp
+++ modules/lvs/manifests/monitor.pp
@@ -7,8 +7,8 @@ class lvs::monitor {
 
     # WARNING: Temporary, do not lint this, it is going in hiera anyway
     # lint:ignore:80chars
-    # INTERNAL EQIAD
-    $monitors_internal_eqiad = {
+    # INTERNAL EQIAD/CODFW
+    $monitors_internal= {
         'appservers.svc.eqiad.wmnet' => { ip_address => $ip['apaches']['eqiad'], check_command => 'check_http_lvs!en.wikipedia.org!/wiki/Main_Page' },
         'api.svc.eqiad.wmnet' => { ip_address => $ip['api']['eqiad'], check_command => 'check_http_lvs!en.wikipedia.org!/w/api.php?action=query&meta=siteinfo' },
         'rendering.svc.eqiad.wmnet' => { ip_address => $ip['rendering']['eqiad'], check_command => 'check_http_lvs!en.wikipedia.org!/wiki/Main_Page' },
@@ -22,45 +22,28 @@ class lvs::monitor {
         'graphoid.svc.eqiad.wmnet' => { ip_address => $ip['graphoid']['eqiad'], check_command => 'check_http_lvs_on_port!graphoid.svc.eqiad.wmnet!19000!/_info', contact_group => 'admins,parsoid' },
         'restbase.svc.eqiad.wmnet' => { ip_address => $ip['restbase']['eqiad'], check_command => 'check_http_lvs_on_port!restbase.svc.eqiad.wmnet!7231!/' },
         'zotero.svc.eqiad.wmnet' => { ip_address => $ip['zotero']['eqiad'], check_command => 'check_http_zotero_lvs_on_port!zotero.svc.eqiad.wmnet!1969!/export?format=wikipedia' },
-    }
-    create_resources(lvs::monitor_service_http, $monitors_internal_eqiad)
-
-    # INTERNAL CODFW
-    $monitors_internal_codfw = {
         'ms-fe.svc.codfw.wmnet' => { ip_address => $ip['swift']['codfw'], check_command => 'check_http_lvs!ms-fe.svc.codfw.wmnet!/monitoring/backend' },
     }
-    create_resources(lvs::monitor_service_http, $monitors_internal_codfw)
+    create_resources(lvs::monitor_service_http, $monitors_internal)
 
-    # EQIAD
-    $monitors_eqiad = {
+    # EXTERNAL EQIAD/ESAMS/ULSFO
+    $monitors_external = {
         'text-lb.eqiad.wikimedia.org' => { ip_address => $ip['text']['eqiad']['textlb'], uri => 'en.wikipedia.org!/wiki/Main_Page' },
         'bits-lb.eqiad.wikimedia.org' => { ip_address => $ip['bits']['eqiad']['bitslb'], uri => 'bits.wikimedia.org!/static-current/resources/assets/poweredby_mediawiki_88x31.png'},
         'upload-lb.eqiad.wikimedia.org' => { ip_address => $ip['upload']['eqiad']['uploadlb'], uri => 'upload.wikimedia.org!/monitoring/backend'},
         'mobile-lb.eqiad.wikimedia.org' => { ip_address => $ip['mobile']['eqiad']['mobilelb'], uri => 'en.m.wikipedia.org!/wiki/Main_Page'},
         'misc-web-lb.eqiad.wikimedia.org' => { ip_address => $ip['misc_web']['eqiad']['misc_web'], uri => 'varnishcheck!/'},
         'parsoid-lb.eqiad.wikimedia.org' => { ip_address => $ip['parsoidcache']['eqiad']['parsoidlb'], check_command => 'check_http_on_port!80', contact_group => 'admins,parsoid' },
-    }
-    create_resources(lvs::monitor_service_http_https, $monitors_eqiad)
-    create_resources(lvs::monitor_service6_http_https, $monitors_eqiad)
-
-    # ESAMS
-    $monitors_esams = {
         'text-lb.esams.wikimedia.org' => { ip_address => $ip['text']['esams']['textlb'], uri => 'en.wikipedia.org!/wiki/Main_Page' },
         'bits-lb.esams.wikimedia.org' => { ip_address => $ip['bits']['esams']['bitslb'], uri => 'bits.wikimedia.org!/static-current/resources/assets/poweredby_mediawiki_88x31.png'},
         'upload-lb.esams.wikimedia.org' => { ip_address => $ip['upload']['esams']['uploadlb'], uri => 'upload.wikimedia.org!/monitoring/backend'},
         'mobile-lb.esams.wikimedia.org' => { ip_address => $ip['mobile']['esams']['mobilelb'], uri => 'en.m.wikipedia.org!/wiki/Main_Page'},
-    }
-    create_resources(lvs::monitor_service_http_https, $monitors_esams)
-    create_resources(lvs::monitor_service6_http_https, $monitors_esams)
-
-    # ULSFO
-    $monitors_ulsfo = {
         'text-lb.ulsfo.wikimedia.org' => { ip_address => $ip['text']['ulsfo']['textlb'], uri => 'en.wikipedia.org!/wiki/Main_Page' },
         'bits-lb.ulsfo.wikimedia.org' => { ip_address => $ip['bits']['ulsfo']['bitslb'], uri => 'bits.wikimedia.org!/static-current/resources/assets/poweredby_mediawiki_88x31.png'},
         'upload-lb.ulsfo.wikimedia.org' => { ip_address => $ip['upload']['ulsfo']['uploadlb'], uri => 'upload.wikimedia.org!/monitoring/backend'},
         'mobile-lb.ulsfo.wikimedia.org' => { ip_address => $ip['mobile']['ulsfo']['mobilelb'], uri => 'en.m.wikipedia.org!/wiki/Main_Page'},
     }
-    create_resources(lvs::monitor_service_http_https, $monitors_ulsfo)
-    create_resources(lvs::monitor_service6_http_https, $monitors_ulsfo)
+    create_resources(lvs::monitor_service_http_https, $monitors_external)
+    create_resources(lvs::monitor_service6_http_https, $monitors_external)
     # lint:endignore
 }
