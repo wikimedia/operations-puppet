abbee98b4ebb5e0e663056e552071ad5311275ef
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-06-27 11:23:48 +0300

    Merge all lvs::monitor_service manifests into one
    All the lvs::monitor* manifests are very similar. Merge their
    functionality into lvs::monitor_service_http_https
    
    Change-Id: Ib4259e9c833e8f13b85d058d1a0001118cfcb4bb

--- modules/lvs/manifests/monitor_service_http_https.pp
+++ modules/lvs/manifests/monitor_service_http_https.pp
@@ -1,6 +1,8 @@
 define lvs::monitor_service_http_https (
     $ip_address,
-    $uri,
+    $ip6_address   = undef,
+    $uri           = undef,
+    $check_command = undef,
     $critical      = 'true',
     $contact_group = 'admins'
 ) {
@@ -12,19 +14,68 @@ define lvs::monitor_service_http_https (
         contact_group => $contact_group,
     }
 
-    @monitoring::service { $title:
-        host          => $title,
-        group         => 'lvs',
-        description   => 'LVS HTTP IPv4',
-        check_command => "check_http_lvs!${uri}",
-        critical      => $critical,
-    }
+    if $check_command {
+        @monitoring::service { $title:
+            host          => $title,
+            group         => 'lvs',
+            description   => 'LVS HTTP IPv4',
+            check_command => $check_command,
+            critical      => $critical,
+            contact_group => $contact_group,
+        }
+    } else {
+        @monitoring::service { $title:
+            host          => $title,
+            group         => 'lvs',
+            description   => 'LVS HTTP IPv4',
+            check_command => "check_http_lvs!${uri}",
+            critical      => $critical,
+            contact_group => $contact_group,
+        }
 
-    @monitoring::service { "${title}_https":
-        host          => $title,
-        group         => 'lvs',
-        description   => 'LVS HTTPS IPv4',
-        check_command => "check_https_url!${uri}",
-        critical      => $critical,
+        @monitoring::service { "${title}_https":
+            host          => $title,
+            group         => 'lvs',
+            description   => 'LVS HTTPS IPv4',
+            check_command => "check_https_url!${uri}",
+            critical      => $critical,
+            contact_group => $contact_group,
+        }
+    }
+    if $ip6_address {
+        @monitoring::host { "${title}_ipv6":
+            ip_address    => $ip6_address,
+            group         => 'lvs',
+            critical      => 'true',
+            contact_group => $contact_group,
+        }
+        if $check_command {
+            @monitoring::service { $title:
+                host          => $title,
+                group         => 'lvs',
+                description   => 'LVS HTTP IPv4',
+                check_command => $check_command,
+                critical      => $critical,
+                contact_group => $contact_group,
+            }
+        } else {
+            @monitoring::service { "${title}_ipv6":
+                host          => "${title}_ipv6",
+                group         => 'lvs',
+                description   => 'LVS HTTP IPv6',
+                check_command => "check_http_lvs!${uri}",
+                critical      => $critical,
+                contact_group => $contact_group,
+            }
+
+            @monitoring::service { "${title}_ipv6_https":
+                host          => "${title}_ipv6",
+                group         => 'lvs',
+                description   => 'LVS HTTPS IPv6',
+                check_command => "check_https_url!${uri}",
+                critical      => $critical,
+                contact_group => $contact_group,
+            }
+        }
     }
 }
