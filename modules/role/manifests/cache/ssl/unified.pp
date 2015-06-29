85b8a0b2b10bbb5bade9e96fdde5f0401613d4d6
Brandon Black <bblack@wikimedia.org>
2015-06-29 19:41:33 +0000

    create ssl::unified as a non-SNI alternative to ssl::sni
    If this plays well in live testing, we'll likely remove
    role::cache::ssl::sni afterwards.
    
    Change-Id: Ibb7d9b28690695eaf2ac6aaa5c899a74b2634020

---
+++ modules/role/manifests/cache/ssl/unified.pp
@@ -0,0 +1,19 @@
+class role::cache::ssl::unified {
+    include role::protoproxy::ssl::common
+
+    role::cache::ssl::local { 'unified':
+        certname       => 'uni.wikimedia.org',
+        default_server => true,
+        do_ocsp        => true,
+    }
+
+    monitoring::service { 'https':
+        description   => 'HTTPS',
+        check_command => 'check_sslxNN',
+    }
+
+    # ordering ensures nginx/varnish config/service-start are
+    #  not intermingled during initial install where they could
+    #  have temporary conflicts on binding port 80
+    Service['nginx'] -> Service<| tag == 'varnish_instance' |>
+}
