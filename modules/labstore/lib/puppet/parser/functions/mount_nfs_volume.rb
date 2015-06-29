4c8c0de501ca5c4476ff143eb042cc76a1d5bfff
YuviPanda <yuvipanda@gmail.com>
2015-06-29 22:37:47 +0100

    labstore: Move projects config to labstore module
    Doesn't belong in the openstack module
    
    Change-Id: Ib0048bb2825a472eeb5554724ff38287002b2050

--- modules/labstore/lib/puppet/parser/functions/mount_nfs_volume.rb
+++ modules/labstore/lib/puppet/parser/functions/mount_nfs_volume.rb
@@ -5,14 +5,14 @@
 # Returns true if the mount should be mounted in
 # instance of the project
 #
-# Reads this information from openstack/files/nfs-mounts-config.yaml
+# Reads this information from labstore/files/nfs-mounts-config.yaml
 # in the openstack module of operations/puppet.git
 module Puppet::Parser::Functions
   @@labs_nfs_config_touched = nil
   @@labs_nfs_config = nil
   newfunction(:mount_nfs_volume, :type => :rvalue, :arity => 2) do |args|
-    module_path = function_get_module_path(['openstack'])
-    path = "#{module_path}/files/nfs-mounts-config.yaml"
+    module_path = function_get_module_path(['labstore'])
+    path = "#{module_path}/files/projects-config.yaml"
     mtime = File.stat(path).mtime
     if @@labs_nfs_config_touched.nil? || mtime != @@labs_nfs_config_touched
         @@labs_nfs_config = function_loadyaml([path])
