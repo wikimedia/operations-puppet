a4af41254d5cb6dff7169bd3ad55765e58778f1f
YuviPanda <yuvipanda@gmail.com>
2015-06-30 17:08:34 +0100

    [WIP] labstore: Rewrite of manage-nfs-volumes-daemon
    Functionality required for this deamon:
      - ALlow instances in projects with NFS enabled to
        be able to mount the volumes enabled, explicitly via
        whitelisting their IPs.
      - Make sure that IP reuse does not leak NFS exports
        from one project to another
    
    To do so, this has to:
      - Gather list of projects with NFS enabled
      - Generate entries in exports.d with appropriate IP
        whitelist (and appropriate gid set)
    
    This version does not depend on LDAP at all, using a
    YAML file to figure out list of projects with NFS enabled
    and a wikitech API call to find list of instances.
    
    Switches to python3, since that is available in jessie and
    gives us ipaddress package for free.
    
    Also refactor the exports / imports into separate classes
    
    TODO:
      - Write a systemd unit file for this
      - Clean up exports entries for projects that do not
        have NFS enabled anymore
      - Figure out what 'sync-exports' is, see if it
        needs to be folded into this
    
    Bug: T102782
    Change-Id: I294a0cb31d7f5f7c598761b905b6c6c62acdcb8e

---
+++ modules/labstore/manifests/fileserver/exports/private.pp
@@ -0,0 +1,53 @@
+# = Class labstore::fileserver::exports::private
+#
+# Sets up per-project private NFS exports so that
+# instances in a project have access to NFS mounts
+# specifically set aside for them
+class labstore::fileserver::exports::private {
+    require_package('python3', 'python3-yaml')
+
+    require ::labstore::fileserver::exports
+
+    group { 'nfsmanager':
+        ensure => present,
+        name   => 'nfsmanager',
+        system => true,
+    }
+
+    user { 'nfsmanager':
+        home       => '/var/lib/nfsmanager',
+        shell      => '/bin/bash',
+        managehome => true,
+        system     => true,
+    }
+
+    sudo::user { 'nfsmanager':
+        privileges => [
+            'ALL = NOPASSWD: /bin/mkdir -p /srv/*',
+            'ALL = NOPASSWD: /bin/rmdir /srv/*',
+            'ALL = NOPASSWD: /usr/local/sbin/sync-exports'
+        ],
+        require => User['nfsmanager'],
+    }
+
+    file { '/usr/local/sbin/sync-exports':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/labstore/sync-exports',
+    }
+
+    file { '/usr/local/sbin/nfs-project-exports-daemon':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/labstore/nfs-project-exports-daemon',
+    }
+
+    file { '/usr/local/sbin/archive-project-volumes':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/labstore/archive-project-volumes',
+    }
+}
