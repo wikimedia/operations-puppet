de0edeed5236f03fc85e61f7e2fdc8eff950f591
cpettet <rush@wikimedia.org>
2015-06-30 18:40:08 -0500

    confd: track per template run error state files
    Change-Id: I9fd8f0df98e8067db1f215e2ca16fc1d8dea7c40

--- modules/confd/files/check_confd_lint.sh
+++ modules/confd/files/check_confd_lint.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-# Check for existence of confd template
-# linting err state file and report
-# last modtime which should equate to
-# last error.  This file _does not exist_
-# without errant state
+# Check for existence of confd template linting err state
+# files and report last modtime which should equate to
+# last error.  Files _do not exist_ without errant state
 #
 # If confd is reporting a bad lint for a template
 # the best way to narrow it down is to run 'confd'
 # with the appropriate options for srv and
 # '-onetime=true' which will output the individual
 # template health
+#
+# * A global linting error is surfaced and not per template
 
-target='/var/run/confd_template_lint.err'
-if [ -e "$target" ]; then
+/bin/ls /var/run/confd-template/*.err &>/dev/null
+if [ $? -eq 0 ]; then
+    target=$(/bin/ls -t /var/run/confd-template/*.err | head -1)
     mtime=$(/usr/bin/stat -c "%n last %y" $target)
     echo "CRITICAL - ${mtime}"
     exit 2
