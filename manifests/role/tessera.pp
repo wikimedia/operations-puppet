a4b476272bd57c7d071e5c74e6a0de500bca8279
Ori Livneh <ori@wikimedia.org>
2015-06-30 11:36:46 -0700

    Tessera: Restrict POST / PUT / DELETE to wmf / ops / nda LDAP groups
    Change-Id: I885cf46c7ee1c1930cb056f4bbc1deb0a9ca3fde

--- manifests/role/tessera.pp
+++ manifests/role/tessera.pp
@@ -4,11 +4,25 @@
 # It powers <https://tessera.wikimedia.org>.
 #
 class role::tessera {
+    include ::apache::mod::authnz_ldap
     include ::apache::mod::headers
     include ::apache::mod::rewrite
     include ::apache::mod::uwsgi
 
     include ::passwords::tessera
+    include ::passwords::ldap::production
+
+    $auth_ldap = {
+        name          => 'nda/ops/wmf',
+        bind_dn       => 'cn=proxyagent,ou=profile,dc=wikimedia,dc=org',
+        bind_password => $passwords::ldap::production::proxypass,
+        url           => 'ldaps://ldap-eqiad.wikimedia.org ldap-codfw.wikimedia.org/ou=people,dc=wikimedia,dc=org?cn',
+        groups        => [
+            'cn=ops,ou=groups,dc=wikimedia,dc=org',
+            'cn=nda,ou=groups,dc=wikimedia,dc=org',
+            'cn=wmf,ou=groups,dc=wikimedia,dc=org',
+        ],
+    }
 
     class { '::tessera':
         graphite_url => 'https://graphite.wikimedia.org',
