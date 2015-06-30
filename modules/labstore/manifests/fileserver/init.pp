5b67b2a8d913cbe2a165bba388871bc3e7ded5ed
YuviPanda <yuvipanda@gmail.com>
2015-06-30 17:08:34 +0100

    [WIP] labstore: Rewrite of manage-nfs-volumes-daemon
    Functionality required for this deamon:
      - ALlow instances in projects with NFS enabled to
        be able to mount the volumes enabled, explicitly via
        whitelisting their IPs.
      - Make sure that IP reuse does not leak NFS exports
        from one project to another
    
    To do so, this has to:
      - Gather list of projects with NFS enabled
      - Generate entries in exports.d with appropriate IP
        whitelist (and appropriate gid set)
    
    This version does not depend on LDAP at all, using a
    YAML file to figure out list of projects with NFS enabled
    and a wikitech API call to find list of instances.
    
    Switches to python3, since that is available in jessie and
    gives us ipaddress package for free.
    
    Also refactor the exports / imports into separate classes
    
    TODO:
      - Clean up exports entries for projects that do not
        have NFS enabled anymore
      - Figure out what 'sync-exports' is, see if it
        needs to be folded into this
    
    Bug: T102782
    Change-Id: I294a0cb31d7f5f7c598761b905b6c6c62acdcb8e

---
+++ modules/labstore/manifests/fileserver/init.pp
@@ -0,0 +1,42 @@
+# == class labstore::fileserver
+#
+# This configures a server for serving NFS filesystems to Labs
+# instances.  Applying this classes suffices to make a server
+# capable of serving this function, but neither activates nor
+# enables it to do so by itself (as this requires manual
+# intervention at this time because of the shared storage).
+class labstore::fileserver {
+
+    include ::labstore
+
+    file { '/usr/local/sbin/replica-addusers.pl':
+        source => 'puppet:///modules/labstore/replica-addusers.pl',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0550',
+    }
+
+    file { '/etc/init/replica-addusers.conf':
+        source => 'puppet:///modules/labstore/replica-addusers.conf',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        require => File['/usr/local/sbin/replica-addusers.pl'],
+    }
+
+    # There is no service {} stanza on purpose -- this service
+    # must *only* be started by a manual operation because it must
+    # run exactly once on whichever NFS server is the current
+    # active one.
+
+    file { '/usr/local/sbin/start-nfs':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0550',
+        source  => 'puppet:///modules/labstore/start-nfs',
+    }
+    
+    include ::labstore::fileserver::exports::private
+    include ::labstore::fileserver::exports::public
+}
