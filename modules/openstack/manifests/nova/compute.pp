c412c828d2ba2a1e2f8885b5401e85d2f52d007b
Brandon Black <bblack@wikimedia.org>
2015-07-01 23:48:20 +0000

    bugfixes for b13b9157 (dependencies)
    Change-Id: I4f50b376a5addfeb2d6f004863963bcc29aac94a

--- modules/openstack/manifests/nova/compute.pp
+++ modules/openstack/manifests/nova/compute.pp
@@ -28,12 +28,12 @@ class openstack::nova::compute(
         file { '/var/lib/nova/clientcert.pem':
             ensure  => link,
             target  => "/etc/ssl/localcerts/${certname}.crt",
-            require => Install_certificate[$certname],
+            require => Sslcert::Certificate[$certname],
         }
         file { '/var/lib/nova/cacert.pem':
             ensure  => link,
             target  => $ca_target,
-            require => Install_certificate[$certname],
+            require => Sslcert::Certificate[$certname],
         }
         file { '/var/lib/nova/.ssh':
             ensure  => directory,
