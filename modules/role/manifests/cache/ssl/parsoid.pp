5bbe9dd480990c1957beab44643bade3432147d2
Brandon Black <bblack@wikimedia.org>
2015-07-01 07:12:50 +0000

    tlsproxy: multi-cert support, including ocsp
    This updates tlsproxy::localssl and all callers to support an
    array of certs for a single nginx-level "site", for e.g. matching
    ECDSA+RSA keys that are otherwise-identical.
    
    This includes updates to the stapling stuff to support multiple
    certificates in a single ssl_stapling_file response.  The stapling
    updater cron was relatedly refactored to use a set of input
    configuration files instead of just refreshing whatever it finds
    in the ouput directory.
    
    Actually using more than one certificate in a site's cert array
    requires special non-standard nginx patches at this time.
    
    Bug: T86654
    Change-Id: I996b7f958d17df1516c8f011001518c5397cd5e2

--- modules/role/manifests/cache/ssl/parsoid.pp
+++ modules/role/manifests/cache/ssl/parsoid.pp
@@ -1,15 +1,15 @@
 class role::cache::ssl::parsoid {
     role::cache::ssl::local { 'unified':
-        certname       => 'uni.wikimedia.org',
+        certs          => ['uni.wikimedia.org'],
         default_server => true,
     }
     role::cache::ssl::local { 'wikimedia.org':
-        certname       => 'sni.wikimedia.org',
+        certs          => ['sni.wikimedia.org'],
         server_name    => 'wikimedia.org',
         server_aliases => ['*.wikimedia.org'],
     }
     role::cache::ssl::local { 'm.wikimedia.org':
-        certname       => 'sni.m.wikimedia.org',
+        certs          => ['sni.m.wikimedia.org'],
         server_name    => 'm.wikimedia.org',
         server_aliases => ['*.m.wikimedia.org'],
     }
