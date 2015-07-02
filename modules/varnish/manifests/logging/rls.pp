13adc5b94174f8e7b8e06d6b4226dad374992391
Ori Livneh <ori@wikimedia.org>
2015-07-02 11:34:20 -0700

    Add varnish stats reporter for ResourceLoader requests
    This patch adds an additional Python Varnish log tailer / stats reporter
    script. Both the script and the Puppetization are nearly identical to the
    X-Connection-Properties logger script, except the purpose of this one is to log
    stats about browser cache hit ratio and total request volume for ResourceLoader
    requests (/w/load.php).
    
    Frequently Anticipated Questions:
    
    * Why add a new daemon rather than extend varnishxcps to capture these metrics as well?
    
    These scripts attain low resource utilisation by using the varnishlog API to
    subscribe to a tiny subset of the total volume of log records generated by
    varnish. Combining use-cases requires moving the filtering logic to Python
    code, which makes CPU utilisation explode. (By comparison, this script uses
    1-3% of a single CPU core on cp1066).
    
    * Are we going to keep adding such scripts to the Varnishes willy-nily?
    
    No. load.php requests are special because (a) they are critical to front-end
    performance, (b) their performance profile is poorly understood, (c) they are
    the subject of ongoing research and work by the performance team (Timo,
    specifically).
    
    This patch applies it only on cp1066.
    
    Bug: T104277
    Change-Id: I83e27c954ff1b490374dcfc202f1499e4fb0e48c

---
+++ modules/varnish/manifests/logging/rls.pp
@@ -0,0 +1,40 @@
+# == Define: varnish::logging::rls
+#
+#  Accumulate browser cache hit ratio and total request volume statistics
+#  for ResourceLoader requests (/w/load.php) and report to StatsD.
+#
+# === Parameters
+#
+# [*statsd_server*]
+#   StatsD server address, in "host:port" format.
+#   Defaults to localhost:8125.
+#
+# === Examples
+#
+#  varnish::logging::rls {
+#    statsd_server => 'statsd.eqiad.wmnet:8125
+#  }
+#
+define varnish::logging::rls( $statsd_server = 'statsd' ) {
+    include varnish::common
+
+    file { '/usr/local/bin/varnishrls':
+        source  => 'puppet:///modules/varnish/varnishrls',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        require => File['/usr/local/lib/python2.7/dist-packages/varnishlog.py'],
+        notify  => Service['varnishrls'],
+    }
+
+    base::service_unit { 'varnishrls':
+        ensure         => present,
+        systemd        => true,
+        strict         => false,
+        template_name  => 'varnishrls',
+        require        => File['/usr/local/bin/varnishrls'],
+        service_params => {
+            enable => true,
+        },
+    }
+}
