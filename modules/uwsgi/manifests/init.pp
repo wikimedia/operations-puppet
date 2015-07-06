d910f3feacd8db715cadeaa6ee15bb0f053d9bf5
YuviPanda <yuvipanda@gmail.com>
2015-07-06 21:49:34 +0100

    uwsgi: Fix ruby plugin version for jessie
    Change-Id: Ie1fce75666ec80998120fb7e14f4758d27b9b79c

--- modules/uwsgi/manifests/init.pp
+++ modules/uwsgi/manifests/init.pp
@@ -8,7 +8,11 @@ class uwsgi {
     # There are 30+ uWSGI plug-ins, installable via the dependency package
     # 'uwsgi-plugins-all'. But I'm going to go out on a limb here and predict
     # that we won't use any except these two.  -- OL
-    $plugins = [ 'uwsgi-plugin-python', 'uwsgi-plugin-rack-ruby1.9.1' ]
+    if os_version('debian >= jessie') {
+        $plugins = [ 'uwsgi-plugin-python', 'uwsgi-plugin-rack-ruby2.1' ]
+    } else {
+        $plugins = [ 'uwsgi-plugin-python', 'uwsgi-plugin-rack-ruby1.9.1' ]
+    }
 
     package { [ 'uwsgi', 'uwsgi-dbg' ]: }
     package { $plugins: }
