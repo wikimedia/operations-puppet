a0b82c2ef15efb4400c7e328a0798d521cc3143f
Chad Horohoe <chadh@wikimedia.org>
2015-07-07 10:10:07 -0700

    Phabricator: Clean up vcs manifest with proper dependencies
    We can't create the symlink to git-http-backend until Git is
    actually installed. Theoretically this could fail (it doesn't
    because Git is in base, but race conditions happen :))
    
    We also shouldn't attempt to grant sudo privs to the phd user
    until we've created the symlink. Bogus sudo privs are bad.
    
    Change-Id: Ia4f775beba932c38541e54dbb6bd657239692350

--- modules/phabricator/manifests/vcs.pp
+++ modules/phabricator/manifests/vcs.pp
@@ -8,11 +8,15 @@ class phabricator::vcs (
 ) {
     # git-http-backend needs to be in $PATH
     file { '/usr/local/bin/git-http-backend':
-        ensure => 'link',
-        target => '/usr/lib/git-core/git-http-backend',
+        ensure  => 'link',
+        target  => '/usr/lib/git-core/git-http-backend',
+        require => Package['Git'],
     }
 
-    sudo::user { 'www-data': privileges => [
-        "ALL=(${settings['phd.user']}) SETENV: NOPASSWD: /usr/local/bin/git-http-backend",
-    ] }
+    sudo::user { 'www-data':
+        privileges => [
+            "ALL=(${settings['phd.user']}) SETENV: NOPASSWD: /usr/local/bin/git-http-backend",
+        ]
+        require    => File['/usr/local/bin/git-http-backend'],
+    }
 }
