6bd6532ff61751bf04bf8eabe539a0df37afb33c
John F. Lewis <johnflewis93@gmail.com>
2015-07-07 12:40:07 +0100

    mail: ferm rules for mailman
    Add basic ferm rules for mailman service (web and smtp). TODO: check if any
    other ports are open on sodium and add them.
    
    Bug: T104980
    Change-Id: If26277b0322e01491236f037d6ab764d63e0f33e

--- manifests/role/mail.pp
+++ manifests/role/mail.pp
@@ -193,4 +193,22 @@ class role::mail::lists {
         timeout       => '30',
     }
 
+    ferm::service { 'mailman-smtp':
+        proto => 'tcp',
+        port  => '25',
+    }
+
+    ferm::service { 'mailman-http':
+        proto => 'tcp',
+        port  => '80',
+    }
+
+    ferm::service { 'mailman-https':
+        proto => 'tcp',
+        port  => '443',
+    }
+
+    ferm::rule { 'mailman-spamd-local':
+        rule => 'proto tcp dport 783 { saddr (127.0.0.1 ::1) ACCEPT; }'
+    }
 }
