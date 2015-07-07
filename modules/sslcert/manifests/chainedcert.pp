dc37ca526505f36ba5907c21cf254378098b233b
Brandon Black <bblack@wikimedia.org>
2015-07-07 20:57:23 +0000

    sslcert: output both kinds of chain(ed) files
    Change-Id: Iff9caf764a99495a972d2816191501edc0323c15

--- modules/sslcert/manifests/chainedcert.pp
+++ modules/sslcert/manifests/chainedcert.pp
@@ -37,29 +37,44 @@ define sslcert::chainedcert(
 
     validate_ensure($ensure)
 
-    $chainfile = "/etc/ssl/localcerts/${title}.chained.crt"
+    $chainedfile = "/etc/ssl/localcerts/${title}.chained.crt"
+    $chainfile = "/etc/ssl/localcerts/${title}.chain.crt"
 
     if $ensure == 'present' {
         $inpath = "/etc/ssl/localcerts/${title}.crt"
         $script = "/usr/local/sbin/x509-bundle"
-        exec { "x509-bundle ${title}":
+        exec { "x509-bundle ${title}-chained":
             path    => 'bin:/usr/bin',
             cwd     => '/etc/ssl/localcerts',
-            command => "$script --skip-root -c $inpath -o $chainfile",
+            command => "$script --skip-root -c $inpath -o $chainedfile",
+            unless  => "[ $chainedfile -nt $inpath -a $chainedfile -nt $script ]",
+            require => [ File[$inpath], File[$script] ],
+        }
+        exec { "x509-bundle ${title}-chain":
+            path    => 'bin:/usr/bin',
+            cwd     => '/etc/ssl/localcerts',
+            command => "$script --skip-root --skip-first -c $inpath -o $chainfile",
             unless  => "[ $chainfile -nt $inpath -a $chainfile -nt $script ]",
             require => [ File[$inpath], File[$script] ],
         }
 
-        # set owner/group/permissions on the chained file
+        # set owner/group/permissions on the chained/chain files
+        file { $chainedfile:
+            ensure  => $ensure,
+            mode    => '0444',
+            owner   => 'root',
+            group   => $group,
+            require => Exec["x509-bundle ${title}-chained"],
+        }
         file { $chainfile:
             ensure  => $ensure,
             mode    => '0444',
             owner   => 'root',
             group   => $group,
-            require => Exec["x509-bundle ${title}"],
+            require => Exec["x509-bundle ${title}-chain"],
         }
     } else {
-        file { $chainfile:
+        file { [$chainedfile, $chainfile]:
             ensure => $ensure,
         }
     }
