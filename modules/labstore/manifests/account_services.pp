35e9093b6653334f17797cfafe088b4c5cf007dc
YuviPanda <yuvipanda@gmail.com>
2015-07-08 16:05:16 +0100

    labstore: Rewrite of replica-addusers.pl
    - Switch it to opt-in per project rather than turned on by
      default for all projects. That caused a deluge of unnecessary
      and unused mysql accounts, and also presumed that all projects
      got NFS by default (which is not the case).
    - Opt-in only tools by default.
    - Keeps state only in the replica.my.cnf files themselves - no
      state kept elsewhere (other than in the mysql dbs themselves, ofc)
    
    TODO:
    - Add support for user accoutns, not just service groups
    - Figure out what to do if the replica.my.cnf file is missing but
      the user / grant already exists.
    
    Bug: T104453
    Change-Id: I90dc98401b89e769fa058943e3714e383dfe25ea

---
+++ modules/labstore/manifests/account_services.pp
@@ -0,0 +1,52 @@
+# == Class: labstore::account_services
+# Provides account services for labs user accounts,
+# currently in the labstore module because we put these
+# on the user's homedirs on NFS.
+#
+# Currently provides:
+#   - MySQL replica / toolsdb accounts
+#
+class labstore::account_services {
+
+    require_package('python3-yaml', 'python3-ldap3')
+
+    include passwords::ldap::labs
+    include passwords::mysql::labsdb
+
+    $creds = {
+        'ldap' => {
+            'host'     => 'ldap-eqiad.wikimedia.org',
+            'username' => 'cn=proxyagent,ou=profile,dc=wikimedia,dc=org',
+            'password' => $::passwords::ldap::labs::proxypass,
+        },
+        'mysql' => {
+            'hosts' => [
+                'labsdb1001.eqiad.wmnet',
+                'labsdb1002.eqiad.wmnet',
+                'labsdb1003.eqiad.wmnet',
+                'labsdb1005.eqiad.wmnet',
+            ],
+            'username' => $::passwords::mysql::labsdb::user,
+            'password' => $::passwords::mysql::labsdb::password,
+        }
+    }
+
+    file { '/etc/create-dbusers.yaml':
+        content => ordered_json($creds),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+
+    file { '/usr/local/sbin/create-dbusers':
+        source => 'puppet:///modules/labstore/create-dbusers',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+
+    base::service_unit { 'create-dbusers':
+        systemd         => true,
+        declare_service => false,
+    }
+}
