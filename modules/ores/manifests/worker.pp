c9d01d1f512221960a577f51af09e5cc52adf4e9
YuviPanda <yuvipanda@gmail.com>
2015-07-08 14:25:38 +0100

    ores: Fix typo
    Change-Id: I1b5a1ebd62c92c816c8777431a2df7f2fa779110

--- modules/ores/manifests/worker.pp
+++ modules/ores/manifests/worker.pp
@@ -1,11 +1,9 @@
 class ores::worker {
-
-
     celery::worker { 'ores-worker':
         app             => 'ores_celery.application',
         working_dir     => $ores::base::config_path,
         user            => 'www-data',
         group           => 'www-data',
-        celery_bin_path => "${ores::base::venve_path}/bin/celery",
+        celery_bin_path => "${ores::base::venv_path}/bin/celery",
     }
 }
