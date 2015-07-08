749b648c74347963096525978e0a08d9b537e2db
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-07-08 11:15:13 +0200

    pybal: refactor pybal::pool, print pools in pybal::web
    Since we're moving away from out traditional schema where the pybal
    pools are directly operated on via raw files, we still want to present
    what's pooled and what's not pooled via an http request. So we:
    - Factor out the confd file generation from pybal::pool
    - Create the resources for all pools on hosts with the pybal::web
    definition
    - Avoid monitoring all of those on palladium, as that's not really critical.
    
    Change-Id: I30dd4ae6a0bce64c02c809e214fd868a897a97e7

---
+++ modules/pybal/manifests/web/service.pp
@@ -0,0 +1,17 @@
+# prints all the pybal
+define pybal::web::service($config) {
+    $dc = inline_template("<%= @name.split('/')[0]")
+    $service = inline_template("<%= @name.split('/')[1]")
+
+    $service_config = $config[$service]
+    $cluster = $service_config['conftool']['cluster']
+    $service = $service_config['conftool']['service']
+
+    $path = "${::pybal::web::conftool_dir}/${name}"
+
+    pybal::conf_file { $path:
+        dc      => $dc,
+        cluster => $cluster,
+        service => $service,
+    }
+}
