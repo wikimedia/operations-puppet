c51eaa31b59b5239ad31c3f0bd8a0e48524b994e
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-07-08 11:15:13 +0200

    pybal: refactor pybal::pool, print pools in pybal::web
    Since we're moving away from out traditional schema where the pybal
    pools are directly operated on via raw files, we still want to present
    what's pooled and what's not pooled via an http request. So we:
    - Factor out the confd file generation from pybal::pool
    - Create the resources for all pools on hosts with the pybal::web
    definition
    - Avoid monitoring all of those on palladium, as that's not really critical.
    
    Change-Id: I30dd4ae6a0bce64c02c809e214fd868a897a97e7

--- modules/pybal/manifests/pool.pp
+++ modules/pybal/manifests/pool.pp
@@ -19,18 +19,11 @@ define pybal::pool(
     if member($service_config['sites'], $::site) {
         if member($lvs_class_hosts[$service_config['class']], $::hostname) {
 
-            $cluster = $service_config['conftool']['cluster']
-            $service = $service_config['conftool']['service']
-
-            $watch_keys = ["/conftool/v1/pools/$::site/$cluster/$service/"]
-            $tmpl = template('pybal/host-pool.tmpl.erb')
-
-            confd::file{ "/etc/pybal/pools/$name":
-                watch_keys => $watch_keys,
-                content    => $tmpl,
-                check      => '/usr/local/bin/pybal-eval-check',
-                require    => File['/etc/pybal/pools'],
+            pybal::conf_file { "/etc/pybal/pools/${name}":
+                cluster => $service_config['conftool']['cluster'],
+                service => $service_config['conftool']['service'],
             }
+
         }
     }
 }
