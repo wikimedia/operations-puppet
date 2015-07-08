b9dc291a2fe83fa2b88f0abdbfd55d5910e14622
Matanya Moses <matanya@foss.co.il>
2015-07-08 17:53:28 +0300

    monitoring: detect saturation of nf_conntrack table
    bug: T105154
    Change-Id: Idf21587618d76dc0fb685f17320c4004cd37c05c

---
+++ modules/base/files/firewall/check_conntrack.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import string
+import sys
+
+
+def get_sysctl(name):
+
+    path = '/proc/sys/' + name()
+
+    try:
+        with open(path) as f:
+            value = f.read().rstrip('\n')
+        return int(value)
+
+    except IOError:
+        return None
+
+
+max_value = get_sysctl('net/netfilter/nf_conntrack_max')
+if max_value is not None and max_value > 0:
+    count_value = get_sysctl('net/netfilter/nf_conntrack_count')
+    full = int(count_value/max_value*100)
+if full >= 80 and full <= 90:
+    print("Warning: nf_conntrack is %d %% full" % (full))
+    sys.exit(1)
+elif full > 90 or max_value == 0:
+    print("Critical: nf_conntrack is %d %% full" % (full))
+    sys.exit(2)
+elif full < 80:
+    print("OK: nf_conntrack is %d %% full" % (full))
+    sys.exit(0)
+else:
+    print("UNKNOWN: error reading nf_conntrack")
+    sys.exit(3)
