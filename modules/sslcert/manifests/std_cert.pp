b0446685936f735836c07e0b096a6041f94b9c18
Brandon Black <bblack@wikimedia.org>
2015-07-08 03:34:10 +0000

    sslcert: refactor std_cert
    This refactors std_cert, splitting it into a separate thing
    instead of wrapping sslcert::certificate.  It also now uses
    secret() to fetch secret key contents.
    
    Change-Id: I6e6701361044557a86400fc2534c949241a4dfc8

--- modules/sslcert/manifests/std_cert.pp
+++ modules/sslcert/manifests/std_cert.pp
@@ -1,20 +1,67 @@
 # == Define: sslcert::std_cert
 #
-# This is a wrapper for sslcert::certificate which defines a different set of
-# defaults for "normal" certificate usage.  There are no parameters aside from
-# "ensure", the resource title is used to source the private and public keys
-# from our standard paths, and all other parameters are defaulted at the
-# sslcert::certificate level.
+# Installs a X.509 certificate and its private key to the system's predefined
+# local certificate directory.
 #
-# This makes array usage much easier for the common case, as in:
-# $foo = [ 'bar.org', 'baz.org' ]
-# sslcert::std_cert { $foo: }
+# Certificates are installed to the custom-made directory /etc/ssl/localcerts
+# rather than /etc/ssl/certs, as the latter is used often as the CA path in
+# many default configurations and examples on the web.
+#
+# The input pathnames for the cert and the private key are fixed at our
+# standard locations and based on the resource's name.  For example, if the
+# resource name is "foo", the cert source will be "files/ssl/foo.crt", and
+# the private key should be located at "secrets/ssl/foo.key" in the private
+# repository.
+#
+# === Parameters
+#
+# [*ensure*]
+#   If 'present', the certificate will be installed; if 'absent', it will be
+#   removed. The default is 'present'.
+#
+# [*group*]
+#   The group name the resulting key and cert file will be owned by. Defaults
+#   to the well-known 'ssl-cert'.
+#
+# [*chain*]
+#   If true, create also a chained version of the certificate, by calling into
+#   sslcert::chainedcert. The default is true.
+#
+# === Examples
+#
+#  sslcert::std_cert { 'www.example.org': }
 #
 
-define sslcert::std_cert($ensure=present) {
-    sslcert::certificate { $title:
-        ensure  => $ensure,
-        source  => "puppet:///files/ssl/${title}.crt",
-        private => "puppet:///private/ssl/${title}.key",
+define sslcert::std_cert(
+  $ensure=present,
+  $group='ssl-cert',
+  $chain=true,
+) {
+    require sslcert
+    require sslcert::dhparam
+
+    file { "/etc/ssl/localcerts/${name}.crt":
+        ensure => $ensure,
+        owner  => 'root',
+        group  => $group,
+        mode   => '0444',
+        source => "puppet:///files/ssl/${name}.crt",
+    }
+
+    file { "/etc/ssl/private/${name}.key":
+        ensure    => $ensure,
+        owner     => 'root',
+        group     => $group,
+        mode      => '0440',
+        show_diff => false,
+        backup    => false,
+        content   => secret("ssl/${name}.key"),
+    }
+
+    if $chain {
+        sslcert::chainedcert { $name:
+            ensure => $ensure,
+            group  => $group,
+        }
     }
 }
