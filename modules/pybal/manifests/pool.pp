f8928c13787f951491495c575ab54638070511f6
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-07-09 09:07:59 +0200

    pybal: fix dependencies
    Change-Id: Id66b443981a51d3dce2a3851f2e6edb9626dafa1

--- modules/pybal/manifests/pool.pp
+++ modules/pybal/manifests/pool.pp
@@ -22,6 +22,7 @@ define pybal::pool(
             pybal::conf_file { "/etc/pybal/pools/${name}":
                 cluster => $service_config['conftool']['cluster'],
                 service => $service_config['conftool']['service'],
+                require => File['/etc/pybal/pools'],
             }
 
         }
