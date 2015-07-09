99902718a9f7b02c657efe8431f80d2db0665258
dzahn <dzahn@wikimedia.org>
2015-07-08 17:34:00 -0700

    ganglia_new: add aggregator config for ulsfo
    Add config for ulsfo aggregator now on bast4001.
    
    Bug:T93776
    Change-Id: I1b73e9d1e015f1e5bbddb5cb40a7a6914892068c

--- modules/ganglia_new/manifests/configuration.pp
+++ modules/ganglia_new/manifests/configuration.pp
@@ -13,12 +13,14 @@ class ganglia_new::configuration {
         'eqiad' => [ ipresolve('carbon.wikimedia.org') ],
         'esams' => [ '91.198.174.113' ],
         'codfw' => [ '208.80.153.4' ],
+        'ulsfo' => [ ipresolve('bast4001.wikimedia.org') ],
     }
     $base_port = 8649
     $id_prefix = {
         eqiad => 1000,
         codfw => 2000,
         esams => 3000,
+        ulsfo => 4000,
     }
     $default_sites = ['eqiad','codfw']
 }
