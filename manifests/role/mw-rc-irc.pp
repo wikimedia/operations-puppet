4fd5f6ed94803ed3e097a21dda83b5391bb088a6
dzahn <dzahn@wikimedia.org>
2015-07-09 11:53:40 -0700

    ferm rules for IRCd
    also see T13906
    
    Bug:T104943
    Change-Id: Ib105af30c5591ff9133bd2c35ae8a35895a56992

--- manifests/role/mw-rc-irc.pp
+++ manifests/role/mw-rc-irc.pp
@@ -11,4 +11,9 @@ class role::mw-rc-irc {
 
     include mw-rc-irc::ircserver
     include mw-rc-irc::apache
+
+    ferm::service { 'ircd_public':
+        proto => 'tcp',
+        port   => '(6664 6665 6666 6667 6668 6669 8001)',
+    }
 }
