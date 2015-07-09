addccd2a5b6532a00acb4c115471dff4efe2dfdd
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-07-09 09:54:34 +0200

    pybal::web: only generate existing pools
    Change-Id: I5dd74c16082d84a7e113ce7688dfad04c4261f6e

--- modules/pybal/manifests/web/service.pp
+++ modules/pybal/manifests/web/service.pp
@@ -3,16 +3,19 @@ define pybal::web::service($config) {
     $dc = inline_template("<%= @name.split('/')[0] %>")
     $service = inline_template("<%= @name.split('/')[1] %>")
 
+
     $service_config = $config[$service]
-    $cluster = $service_config['conftool']['cluster']
-    $service_name = $service_config['conftool']['service']
+    if member($service_config['sites'], $dc) {
+        $cluster = $service_config['conftool']['cluster']
+        $service_name = $service_config['conftool']['service']
 
-    $path = "${::pybal::web::conftool_dir}/${name}"
+        $path = "${::pybal::web::conftool_dir}/${name}"
 
-    pybal::conf_file { $path:
-        dc      => $dc,
-        cluster => $cluster,
-        service => $service_name,
-        require => File[$::pybal::web::conftool_dir]
+        pybal::conf_file { $path:
+            dc      => $dc,
+            cluster => $cluster,
+            service => $service_name,
+            require => File[$::pybal::web::conftool_dir]
+        }
     }
 }
