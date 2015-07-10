2a6a3d02ebe4fbeeb27f5cb603329d03076e7301
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-07-10 16:16:02 +0200

    ferm rules for elasticsearch
    PS2 also adds rules for Elastic Zen Discovery
    
    Bug: T104962
    Change-Id: I7fe52ad7745cd352501876d73df8786e7adb9282

--- manifests/role/elasticsearch.pp
+++ manifests/role/elasticsearch.pp
@@ -26,6 +26,27 @@ class role::elasticsearch::server{
         include lvs::realserver
     }
 
+    ferm::service { 'elastic-http':
+        proto => 'tcp',
+        port  => '9200',
+        srange => '$INTERNAL',
+    }
+
+    $elastic_nodes = hiera('cluster: elasticsearch')
+    $elastic_nodes_ferm = join($elastic_nodes, ' ')
+
+    ferm::service { 'elastic-inter-node':
+        proto => 'tcp',
+        port  => '9300',
+        srange  => "@resolve(($elastic_nodes_ferm))",
+    }
+
+    ferm::service { 'elastic-zen-discovery':
+        proto => 'udp',
+        port  => '54328',
+        srange => '$INTERNAL',
+    }
+
     system::role { 'role::elasticsearch::server':
         ensure      => 'present',
         description => 'elasticsearch server',
