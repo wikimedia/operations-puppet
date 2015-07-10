6a02363e8cb7a903d8d1f44f77526d8cf6e27673
Eric Evans <eevans@wikimedia.org>
2015-07-10 10:32:02 -0500

    WIP: Cassanra logstash setup
    Bug: T100970
    Change-Id: Idc5cfc8c5b53e8dac88dbed9e506eee79568903e

---
+++ modules/cassandra/manifests/logging.pp
@@ -0,0 +1,41 @@
+# == Class: cassandra::logging
+#
+# Configure remote logging for Cassandra
+#
+# === Usage
+# class { '::cassandra::logging':
+#     logstash_host => 'logstash1001.eqiad.wmnet',
+# }
+#
+# === Parameters
+# [*logstash_host*]
+#   The logstash logging server to send to.
+#
+# [*logstash_port*]
+#   The logstash logging server port number.
+
+class cassandra::logging(
+    $logstash_host  = 'logstash1001.eqiad.wmnet',
+    $logstash_port  = 514,
+) {
+    validate_string($logstash_host)
+    validate_re("${logstash_port}", '^[0-9]+$')
+
+    file { '/etc/cassandra/logback.xml':
+        content => template("${module_name}/logback.xml.erb"),
+        owner   => 'cassandra',
+        group   => 'cassandra',
+        mode    => '0444',
+    }
+
+    package { 'cassandra/logstash-logback-encoder':
+        ensure   => present,
+        provider => 'trebuchet',
+    }
+
+    file { '/usr/share/cassandra/lib/logstash-logback-encoder.jar':
+        ensure => 'link',
+        target => '/srv/deployment/cassandra/logstash-logback-encoder/lib/logstash-logback-encoder-4.2.jar',
+        require => Package['cassandra/logstash-logback-encoder'],
+    }
+}
