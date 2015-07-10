bbbc07d54b6083c31deb2a274c7091305e6678fe
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-07-10 10:51:45 +0200

    BOGUS: attempt at making nodepool compile in my tests
    This will be compiled as a a change.
    
    Change-Id: I3cac60ef5d61808d97d8b0b3ae0a10a72aab9edc

--- manifests/role/nodepool.pp
+++ manifests/role/nodepool.pp
@@ -8,17 +8,19 @@ class role::nodepool {
 
     system::role { 'role::nodepool': description => 'CI Nodepool' }
 
-    include role::nova::config
+#    include role::nova::config
     include passwords::nodepool
 
     $novaconfig = $role::nova::config::novaconfig
 
     class { '::nodepool':
+        dib_base_path                  => 'whatever',
+        jenkins_ssh_public_key         => 'srsly',
         jenkins_api_user               => 'nodepoolmanager',
         jenkins_api_key                => $passwords::nodepool::jenkins_api_key,
         jenkins_credentials_id         => 'nodepool-dib-jenkins',
         jenkins_ssh_private_key_source => 'puppet:///private/nodepool/dib_jenkins_id_rsa',
-        openstack_auth_uri             => $novaconfig['auth_uri'],
+        openstack_auth_uri             => '1232',
         openstack_username             => 'nodepoolmanager',
         openstack_password             => $passwords::nodepool::manager_pass,
         openstack_tenant_id            => 'contintcloud',
