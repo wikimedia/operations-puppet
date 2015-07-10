8f44bf3902860698369cce7e234f75649ade42fc
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-07-10 11:29:58 +0100

    diamond: service stats puppet integration
    Provide diamond::collector::servicestats to be used to collect service
    statistics, likely we'd want to add it to base::service_unit
    
    Change-Id: I8dc70cd66d9934519959818c8a827862823f8de4

---
+++ modules/diamond/manifests/collector/servicestats.pp
@@ -0,0 +1,60 @@
+# == Class: diamond::collector::servicestats
+#
+# ServiceStats will report system-level statistics for services ran by systemd
+# and upstart. Default metrics include: CPU %, memory % and uptime.
+#
+# === Parameters
+#
+#   systemd_name: service name to be used with systemd, defaults to title
+#   upstart_name: service name to be used with upstart, defaults to title
+#
+# === Examples
+#
+#   diamond::collector::servicestats { 'rsyslog': }
+
+define diamond::collector::servicestats (
+    $systemd_name = $title,
+    $upstart_name = $title,
+    $ensure = 'present',
+) {
+    validate_ensure($ensure)
+
+    include ::diamond::collector::servicestats_lib
+
+    file { "/etc/diamond/servicestats.d/$title.conf":
+      content  => "[systemd]\nname=$systemd_name\n[upstart]\nname=$upstart_name\n",
+      owner    => 'root',
+      group    => 'root',
+      mode     => '0444',
+      ensure   => $ensure,
+    }
+}
+
+# support class, to be include'd multiple times
+class diamond::collector::servicestats_lib {
+    diamond::collector { 'ServiceStats':
+        source       => 'puppet:///modules/diamond/collector/servicestats.py',
+        settings     => {
+          initsystem => $::initsystem
+        }
+    }
+
+    file { '/usr/share/diamond/collectors/servicestats/servicestats_lib.py':
+        owner   => 'root',
+        group   => 'root',
+        mode    => 0444,
+        source  => 'puppet:///modules/diamond/collector/servicestats_lib.py',
+        require => Diamond::Collector['ServiceStats'],
+    }
+
+    file { '/etc/diamond/servicestats.d':
+        owner  => 'root',
+        group  => 'root',
+        mode   => 0444,
+        ensure => directory,
+    }
+
+    package { ['python-psutil', 'python-configparser']:
+        before => Diamond::Collector['ServiceStats'],
+    }
+}
