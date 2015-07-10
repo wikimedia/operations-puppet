08c48985b0ae2eb7268bb09e312a3d8314566cdc
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-07-10 12:17:45 +0200

    role::nodepool: use secret()
    A module named nodepool was erroneously added to the private repository,
    causing a parsing error. We move to using the secret() function instead.
    
    Bug: T105406
    Change-Id: Icf6ed10d328a809a662fa862d23a16a031ce22ca

--- manifests/role/nodepool.pp
+++ manifests/role/nodepool.pp
@@ -17,7 +17,7 @@ class role::nodepool {
         jenkins_api_user               => 'nodepoolmanager',
         jenkins_api_key                => $passwords::nodepool::jenkins_api_key,
         jenkins_credentials_id         => 'nodepool-dib-jenkins',
-        jenkins_ssh_private_key_source => 'puppet:///private/nodepool/dib_jenkins_id_rsa',
+        jenkins_ssh_private_key_source => secret('nodepool/dib_jenkins_id_rsa'),
         openstack_auth_uri             => $novaconfig['auth_uri'],
         openstack_username             => 'nodepoolmanager',
         openstack_password             => $passwords::nodepool::manager_pass,
