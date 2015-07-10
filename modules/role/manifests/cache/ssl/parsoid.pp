dc100a4038ac7ceaa8892dc51856e06842601fb4
Brandon Black <bblack@wikimedia.org>
2015-07-10 16:21:50 +0000

    parsoid SSL: remove pointless SNI, add OCSP
    Change-Id: I778de52b75112261a22da7de96bbacb6e1ea400c

--- modules/role/manifests/cache/ssl/parsoid.pp
+++ modules/role/manifests/cache/ssl/parsoid.pp
@@ -2,15 +2,6 @@ class role::cache::ssl::parsoid {
     role::cache::ssl::local { 'unified':
         certs          => ['uni.wikimedia.org'],
         default_server => true,
-    }
-    role::cache::ssl::local { 'wikimedia.org':
-        certs          => ['sni.wikimedia.org'],
-        server_name    => 'wikimedia.org',
-        server_aliases => ['*.wikimedia.org'],
-    }
-    role::cache::ssl::local { 'm.wikimedia.org':
-        certs          => ['sni.m.wikimedia.org'],
-        server_name    => 'm.wikimedia.org',
-        server_aliases => ['*.m.wikimedia.org'],
+        do_ocsp        => true,
     }
 }
