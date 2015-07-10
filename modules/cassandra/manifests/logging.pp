6cf6103188db4bf80e231ff643e5cefe676dbc2d
Eric Evans <eevans@wikimedia.org>
2015-07-10 10:32:02 -0500

    Cassanra logstash setup
    - Sets up a logstash syslog input that uses the json codec, on UDP 11514
    - Deploys logstash-logback-encoder artifacts on Cassandra hosts
    - Configures Cassandra to use UDP appender
    
    Bug: T100970
    Change-Id: Idc5cfc8c5b53e8dac88dbed9e506eee79568903e

---
+++ modules/cassandra/manifests/logging.pp
@@ -0,0 +1,61 @@
+# == Class: cassandra::logging
+#
+# Configure remote logging for Cassandra
+#
+# === Usage
+# class { '::cassandra::logging':
+#     logstash_host => 'logstash1001.eqiad.wmnet',
+#     logstash_port => 11514,
+# }
+#
+# === Parameters
+# [*logstash_host*]
+#   The logstash logging server to send to.
+#
+# [*logstash_port*]
+#   The logstash logging server port number.
+
+class cassandra::logging(
+    $logstash_host  = 'logstash1003.eqiad.wmnet',
+    $logstash_port  = 11514,
+) {
+    validate_string($logstash_host)
+    validate_re("${logstash_port}", '^[0-9]+$')
+
+    file { '/etc/cassandra/logback.xml':
+        content => template("${module_name}/logback.xml.erb"),
+        owner   => 'cassandra',
+        group   => 'cassandra',
+        mode    => '0444',
+        require => Class['cassandra'],
+    }
+
+    package { 'cassandra/logstash-logback-encoder':
+        ensure   => present,
+        provider => 'trebuchet',
+    }
+
+    file { '/usr/share/cassandra/lib/logstash-logback-encoder.jar':
+        ensure => 'link',
+        target => '/srv/deployment/cassandra/logstash-logback-encoder/lib/logstash-logback-encoder-4.2.jar',
+        require => Package['cassandra/logstash-logback-encoder'],
+    }
+
+    file { '/usr/share/cassandra/lib/jackson-annotations-2.4.0.jar':
+        ensure => 'link',
+        target => '/srv/deployment/cassandra/logstash-logback-encoder/lib/jackson-annotations-2.4.0.jar',
+        require => Package['cassandra/logstash-logback-encoder'],
+    }
+
+    file { '/usr/share/cassandra/lib/jackson-core-2.4.0.jar':
+        ensure => 'link',
+        target => '/srv/deployment/cassandra/logstash-logback-encoder/lib/jackson-core-2.4.0.jar',
+        require => Package['cassandra/logstash-logback-encoder'],
+    }
+
+    file { '/usr/share/cassandra/lib/jackson-databind-2.4.0.jar':
+        ensure => 'link',
+        target => '/srv/deployment/cassandra/logstash-logback-encoder/lib/jackson-databind-2.4.0.jar',
+        require => Package['cassandra/logstash-logback-encoder'],
+    }
+}
