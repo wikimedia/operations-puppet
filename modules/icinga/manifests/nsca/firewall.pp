762a3eb45f095a2774521513e89c3b0b4ca2b0b3
Jeff Green <jgreen@wikimedia.org>
2015-07-13 17:31:46 -0400

    add frack subnets to network.pp, add frack-codfw to icinga firewall policy
    Change-Id: I67a16556f92bebb4a334585dcc2ace6e539ba99a

--- modules/icinga/manifests/nsca/firewall.pp
+++ modules/icinga/manifests/nsca/firewall.pp
@@ -6,10 +6,13 @@ class icinga::nsca::firewall {
     # NSCA on port 5667
     ferm::rule { 'ncsa_allowed':
         rule => 'saddr (127.0.0.1 \
+          $CODFW_PRIVATE_FRACK_EQIAD \
+          $CODFW_PUBLIC_FRACK_EQIAD \
           $EQIAD_PRIVATE_ANALYTICS1_A_EQIAD \
           $EQIAD_PRIVATE_ANALYTICS1_B_EQIAD \
           $EQIAD_PRIVATE_ANALYTICS1_C_EQIAD \
           $EQIAD_PRIVATE_ANALYTICS1_D_EQIAD \
+          $EQIAD_PRIVATE_FRACK_EQIAD \
           $EQIAD_PRIVATE_LABS_HOSTS1_A_EQIAD \
           $EQIAD_PRIVATE_LABS_HOSTS1_B_EQIAD \
           $EQIAD_PRIVATE_LABS_HOSTS1_D_EQIAD \
@@ -18,6 +21,7 @@ class icinga::nsca::firewall {
           $EQIAD_PRIVATE_PRIVATE1_B_EQIAD \
           $EQIAD_PRIVATE_PRIVATE1_C_EQIAD \
           $EQIAD_PRIVATE_PRIVATE1_D_EQIAD \
+          $EQIAD_PUBLIC_FRACK_EQIAD \
           $EQIAD_PUBLIC_PUBLIC1_A_EQIAD \
           $EQIAD_PUBLIC_PUBLIC1_B_EQIAD \
           $EQIAD_PUBLIC_PUBLIC1_C_EQIAD \
@@ -26,8 +30,6 @@ class icinga::nsca::firewall {
           $ESAMS_PUBLIC_PUBLIC1_ESAMS \
           $ULSFO_PRIVATE_PRIVATE1_ULSFO \
           $ULSFO_PUBLIC_PUBLIC1_ULSFO \
-          208.80.155.0/27 \
-          10.64.40.0/24) \
           proto tcp dport 5667 ACCEPT;'
     }
 }
