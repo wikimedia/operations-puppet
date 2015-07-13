74a92b6c35fa42b21c3450c0cc7e5b92cdff6d21
Antoine Musso <hashar@free.fr>
2015-07-13 10:25:58 +0200

    nodepool: fix authentication URL
    The env variable must be OS_AUTH_URL. Previously I tried with
    OS_AUTH_URI but forgot to reload the env.
    Adjust the auth URL to use the path '/v2.0' or neither the openstack CLI
    nor nodepool are able to authenticate.
    
    Change-Id: I8bf3cd68beed69b1d8032fa00cd5982f36b70c7f

--- manifests/role/nodepool.pp
+++ manifests/role/nodepool.pp
@@ -20,7 +20,7 @@ class role::nodepool {
         jenkins_credentials_id         => 'nodepool-dib-jenkins',
         jenkins_ssh_public_key         => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDrERFfjRBOIoI5ASW0gx/rxSeJX/ThszByypsoA80jCfbcjrfsG94WtOGXYQw4Zjs/8u4DYMfI5aHEZKvk/K4jTAR09J9swFash9ML60AvQx/VFC5ZEDHMBa7dYyzxspDX5v73QEDYG9Hhxo6qfFOLO3IvYfat9CfwQR4/oS2lzV+oIsD68lSy/OoKCpywMs0/pExdP65RHR7xpvAlrgehzKoayfHo5Vzg9dCawj4ZoHsqwCnKG4ctMflyzyN/Lwgniv/+GSjgqf/FNXDCMDJCh+d410IXLS7szY3JTzpWekF82SxIM19CdwKh1R2zPVjUT6hvbm9kOo8Y72ORL2yj nodepool@labnodepool1001',
         jenkins_ssh_private_key_source => secret('nodepool/dib_jenkins_id_rsa'),
-        openstack_auth_uri             => $novaconfig['auth_uri'],
+        openstack_auth_url             => "${novaconfig['auth_uri']}/2.0",
         openstack_username             => 'nodepoolmanager',
         openstack_password             => $passwords::nodepool::manager_pass,
         openstack_tenant_id            => 'contintcloud',
