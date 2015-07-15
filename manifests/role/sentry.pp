baebb34294701ea67b2b0db2209e0cbaf146cb6d
Gergő Tisza <gtisza@wikimedia.org>
2015-07-15 23:42:35 +0000

    Basic role for Sentry
    Bug: T84956
    Change-Id: I1207235f0ee97c253b1a1e9e2bfa89eb79665290

---
+++ manifests/role/sentry.pp
@@ -0,0 +1,13 @@
+# vim:sw=4 ts=4 sts=4 et:
+
+# == Class: role::sentry
+#
+# Provisions Sentry
+#
+class role::sentry {
+    include ::sentry
+
+    system::role { 'role::sentry':
+        description => 'Sentry server (error aggregation & presentation service)',
+    }
+}
