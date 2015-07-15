ae8962203e6d15d98aa3c28aa486cb468f1bb781
Gergő Tisza <gtisza@wikimedia.org>
2015-07-15 23:42:35 +0000

    Basic role for Sentry
    Depends on I7a0ce9e087dee1b95f916288785470b1423f5435
    and I2cfacb4544f99df9f5e6c30dc2824e8b153e27b2
    
    Bug: T84956
    Change-Id: I1207235f0ee97c253b1a1e9e2bfa89eb79665290

---
+++ modules/postgresql/manifests/db.pp
@@ -0,0 +1,55 @@
+#
+# Definition: postgresql::db
+#
+# Manages a PostgreSQL database.
+#
+# Does not do anything unless the postgresql::db::enabled
+# hiera key is true (this is used to disallow automated
+# DB creation in production).
+#
+# Parameters:
+#
+# [*ensure*]
+#   'present' to create the database, 'absent' to delete it
+#
+# [*name*]
+#   Database name (falls back to the resource title).
+#
+# [*owner*]
+#   User who will own the database (defaults to 'postgres')
+#
+# Actions:
+#   Create/drop database
+#
+# Requires:
+#   Class['postgresql::server']
+#
+# Sample Usage:
+#  postgresql::db { 'mydb': }
+#
+define postgresql::db(
+    $ensure = present,
+    $name   = $title,
+    $owner  = 'postgres',
+) {
+    if hiera('postgresql::db::enabled') {
+
+      # Check if our db exists and store it
+      $db_exists = "/usr/bin/psql --tuples-only --command 'SELECT datname FROM pg_catalog.pg_database;' | /bin/grep -q '^ ${name}'"
+
+      if $ensure == 'present' {
+          exec { "create_postgres_db_${name}":
+              command => "/usr/bin/createdb --owner='${owner}' '${name}'",
+              user    => 'postgres',
+              unless  => $db_exists,
+          }
+      } else {
+          exec { "drop_postgres_db_${name}":
+              command => "/usr/bin/dropdb '${name}'",
+              user    => 'postgres',
+              onlyif  => $db_exists,
+          }
+      }
+
+  }
+}
