63840197b04ae7433fe728a3eab12c9937ea8959
YuviPanda <yuvipanda@gmail.com>
2015-07-16 14:27:54 -0500

    ores: Add indonesian spell check package for ores
    Change-Id: Ifc922e438717d5a91e144020975f418ea81226f1

--- modules/ores/manifests/base.pp
+++ modules/ores/manifests/base.pp
@@ -24,7 +24,8 @@ class ores::base(
     # Spellcheck packages for supported languages
     require_package('myspell-pt', 'myspell-fa', 'myspell-en-au',
                     'myspell-en-gb', 'myspell-en-us',
-                    'myspell-en-za', 'myspell-fr')
+                    'myspell-en-za', 'myspell-fr',
+                    'aspell-id')
 
     file { '/srv':
         ensure => directory,
