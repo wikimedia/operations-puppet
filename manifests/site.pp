d0880f17f9162a6dbda6c6ffdad97f0a49ac1fea
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-07-16 16:53:31 -0500

    Assign roles to maps-test200{1,2,3,4}
    Assign role to the maps-test machines. maps-test2001 is the master and
    the rest are slaves
    
    Change-Id: I88b1241edb867c68afb9cc3ec8cc95354b49c4e8

--- manifests/site.pp
+++ manifests/site.pp
@@ -1643,6 +1643,18 @@ node 'magnesium.wikimedia.org' {
 
 }
 
+node 'maps-test2001.codfw.wmnet' {
+    include standard
+
+    role role::maps::master
+}
+
+node /^maps-test200[2-4]\.codfw\.wmnet/ {
+    include standard
+
+    role role::maps::slave
+}
+
 node /^mc(10[01][0-9])\.eqiad\.wmnet/ {
 
     role memcached
