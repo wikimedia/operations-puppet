a4c1d89c2e01f2216ce1146a524d32e88db64213
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2015-07-16 11:50:46 -0500

    wikilabels: Puppetizing wikilabels infrastructure
     - Add session class that uses memcached (for now)
     - Add web class that sets up uwsgi and wsgi config
     - Add hosts entry for wikilabels-database
     - Add role for staging and deploy
     - Set up roles to use the central postgres server
    
    Wikilabels code: https://github.com/wiki-ai/wikilabels
    Wikilabels config: https://github.com/wiki-ai/wikilabels-wikimedia-config
    
    Change-Id: I49ea516b0ee177f706be289a7b73be07995e73c7

---
+++ modules/wikilabels/manifests/web.pp
@@ -0,0 +1,58 @@
+# = Class: wiklabels::web
+# Sets up a uwsgi based web server for Wikilabels running python3
+#
+class wikilabels::web (
+    $branch = 'deploy',
+) {
+
+    $venv_path = '/srv/wikilabels/venv'
+    $config_path = '/srv/wikilabels/config'
+
+    # Let's use a virtualenv for maximum flexibility - we can convert
+    # this to deb packages in the future if needed. 
+    # FIXME: Use debian packages for all the packages needing compilation
+    ensure_packages(['virtualenv', 'python3-dev', 'libffi-dev',
+        'libpq-dev', 'g++', 'libmemcached-dev'])
+
+    # Wikilabels is a python3 application
+    ensure_packages(['uwsgi-plugin-python3'])
+
+    file { '/srv':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0775',
+    }
+
+    file { '/srv/wikilabels':
+        ensure  => directory,
+        owner   => 'www-data',
+        group   => 'www-data',
+        mode    => '0775',
+        require => File['/srv'],
+    }
+
+    git::clone { 'wikilabels-wikimedia-config':
+        origin    => 'https://github.com/wiki-ai/wikilabels-wikimedia-config.git',
+        ensure    => present,
+        directory => $config_path,
+        branch    => $branch,
+        owner     => 'www-data',
+        group     => 'www-data',
+        require   => File['/srv/wikilabels'],
+    }
+
+    uwsgi::app { 'wikilabels-web':
+        settings => {
+            uwsgi => {
+                plugins     => 'python3',
+                'wsgi-file' => "${config_path}/labels.wmflabs.org.wsgi",
+                master      => true,
+                chdir       => $config_path,
+                http-socket => '0.0.0.0:8080',
+                venv        => $venv_path,
+                processes   => inline_template('<%= @processorcount.to_i * 4 %>'),
+            }
+        }
+    }
+}
