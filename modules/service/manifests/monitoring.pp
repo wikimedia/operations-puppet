207b3be2b883b8e924b3a81bbd7679cc18655e7f
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-07-16 16:12:52 +0200

    service::monitoring: fix typo
    Change-Id: Ibe10e859d1c3e98b4fc9e6b6d06ed31e4161fc2c

--- modules/service/manifests/monitoring.pp
+++ modules/service/manifests/monitoring.pp
@@ -4,7 +4,7 @@
 # services defined via service::node
 
 class service::monitoring {
-    require_packages 'python-yaml', 'python-urllib3'
+    require_package 'python-yaml', 'python-urllib3'
 
     file { '/usr/local/lib/nagions/plugins/service_checker':
         ensure => present,
