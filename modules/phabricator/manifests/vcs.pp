ce02774dceb4f943e1b082afac0879bccca0ff86
cpettet <rush@wikimedia.org>
2015-07-16 10:26:36 -0500

    phab: create vcs user
    * Permission to sudo to phd user for git needs
    * set a default to phab expected key
    
    Change-Id: I43c7080aabbbaaf50cba81cd529214149ee748fc

--- modules/phabricator/manifests/vcs.pp
+++ modules/phabricator/manifests/vcs.pp
@@ -6,13 +6,28 @@
 class phabricator::vcs (
     $settings = {},
 ) {
+
     # git-http-backend needs to be in $PATH
     file { '/usr/local/bin/git-http-backend':
         ensure => 'link',
         target => '/usr/lib/git-core/git-http-backend',
     }
 
-    sudo::user { 'www-data': privileges => [
-        "ALL=(${settings['phd.user']}) SETENV: NOPASSWD: /usr/local/bin/git-http-backend",
-    ] }
+    user { ${settings['diffusion.ssh-user']}:
+        home   => "/var/lib/${settings['diffusion.ssh-user']}",
+        shell  => '/bin/sh',
+        system => true,
+    }
+
+    sudo::user { ${settings['diffusion.ssh-user']}:
+        privileges => [
+            "ALL=(${settings['phd.user']}) SETENV: NOPASSWD: /usr/bin/git-upload-pack, /usr/bin/git-receive-pack, /usr/bin/svnserve",
+        ]
+    }
+
+    sudo::user { 'www-data':
+        privileges => [
+            "ALL=(${settings['phd.user']}) SETENV: NOPASSWD: /usr/local/bin/git-http-backend",
+        ]
+    }
 }
