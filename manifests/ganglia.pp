b37c1b4683cfe7b6778ba86fe9f332d3ff8df9e9
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-07-20 15:05:16 +0200

    ganglia: move ganglia::collector to ganglia::deprecated::collector
    Change-Id: Ia47420d7150f4523a98862072147f26cb60eef5f

--- manifests/ganglia.pp
+++ manifests/ganglia.pp
@@ -30,52 +30,6 @@ class ganglia {
     }
 }
 
-# == Class ganglia::collector::config
-# Ganglia gmetad config.  This class does not start
-# gmetad.  Include ganglia::collector instead if you want to do that.
-class ganglia::collector::config {
-    package { 'gmetad':
-        ensure => present,
-    }
-
-    $gridname = 'Wikimedia'
-    $authority_url = 'http://ganglia.wikimedia.org'
-    case $::hostname {
-        # netmon1001 runs gmetad to get varnish data into torrus
-        # unlike other servers, netmon1001 uses the default rrd_rootdir
-        /^netmon1001$/: {
-            $data_sources = {
-                'Upload caches eqiad' => 'cp1048.eqiad.wmnet cp1061.eqiad.wmnet'
-            }
-            $rra_sizes = '"RRA:AVERAGE:0:1:4032" "RRA:AVERAGE:0.17:6:2016" "RRA:MAX:0.17:6:2016" "RRA:AVERAGE:0.042:288:732" "RRA:MAX:0.042:288:732"'
-        }
-        default: {
-            fail("ganglia::collector is deprecated and only applied to netmon1001")
-        }
-    }
-
-    file { "/etc/ganglia/gmetad.conf":
-        ensure  => present,
-        require => Package['gmetad'],
-        content => template('ganglia/gmetad.conf.erb'),
-        mode    => '0444',
-    }
-}
-
-# == Class ganglia::collector
-# This class inherits ganglia::collector::config
-# to install gmetad.conf, and then ensures that
-# gmetad is running.
-class ganglia::collector inherits ganglia::collector::config {
-    system::role { 'ganglia::collector': description => 'Ganglia gmetad aggregator' }
-
-    service { 'gmetad':
-        ensure    => running,
-        require   => File["/etc/ganglia/gmetad.conf"],
-        subscribe => File["/etc/ganglia/gmetad.conf"],
-        hasstatus => false,
-    }
-}
 
 # == Class: ganglia::aggregator
 # for the machine class which listens on multicast and
