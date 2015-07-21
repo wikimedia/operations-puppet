d3c3b0faa2934ab31a04000f1def5684ff79ae20
YuviPanda <yuvipanda@gmail.com>
2015-07-21 01:12:42 -0700

    ores: Add missing file
    Grrr git commit -p
    
    Change-Id: I83ffaae859ba332ba7a888322e7eee0bef1397f2

---
+++ modules/ores/manifests/flower.pp
@@ -0,0 +1,14 @@
+# = Class: ores::flower
+# Sets up a private flower instance to monitor celery
+#
+# Users should use ssh port forwarding to access the
+# instance.
+class ores::flower {
+    celery::flower { 'ores':
+        app             => 'ores_celery.application',
+        working_dir     => $ores::base::config_path,
+        user            => 'www-data',
+        group           => 'www-data',
+        celery_bin_path => "${ores::base::venv_path}/bin/celery",
+    }
+}
