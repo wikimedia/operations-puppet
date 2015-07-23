0850fa298b6cfcb156fed8d48b7791ec825faccd
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2015-07-23 12:25:42 +0200

    Add ferm rules for HHVM admin site
    Change-Id: Ic497ed9278d753e6bb06fb13607a20ea62cb132d

--- modules/hhvm/manifests/admin.pp
+++ modules/hhvm/manifests/admin.pp
@@ -27,4 +27,10 @@ class hhvm::admin(
         ensure  => $ensure,
         content => template('hhvm/hhvm-admin.conf.erb'),
     }
+
+    ferm::service { 'hhvm_admin':
+        proto  => 'tcp',
+        port   => $port,
+        srange => '$INTERNAL',
+    }
 }
