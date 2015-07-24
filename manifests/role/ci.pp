480b75c73d1fe8cfc2891ae1a2421d69f86f15dc
Antoine Musso <hashar@free.fr>
2015-07-24 15:57:25 +0200

    contint: apt conf and python packages for light slaves
    etcd python tests needs to be on Jessie. Take advantages of the
    contint::packages::labs split to include apt configuration and python
    packages on the light slaves.
    
    Bug: T103972
    Change-Id: I749181222a4cd1ae90e66cedb6e107d6714993ae

--- manifests/role/ci.pp
+++ manifests/role/ci.pp
@@ -456,6 +456,8 @@ class role::ci::slave::labs::light {
     # Production DNS migrated to Jessie T98003
     include authdns::lint
 
+    include contint::packages::apt
+    include contint::packages::python
     # Can not easily build etcd on Trusty
     package { 'etcd':
         ensure => latest,
