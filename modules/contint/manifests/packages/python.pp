a1e93b870658ee85421f9ef302c460832cc9c130
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-07-24 14:41:12 +0200

    contint::packages: factor c::p::python out
    We want to factor a few functionality out of the contint::packages::labs
    as having that all inside one single class could make it harder to have
    dedicated slaves that are simpler to build.
    
    Change-Id: I3949a7e65c662d2c800965fb8d7c895bac0194e6

---
+++ modules/contint/manifests/packages/python.pp
@@ -0,0 +1,27 @@
+# === Class contint::packages::python
+#
+# This class sets up packages needed for general python testing
+#
+class contint::packages::python {
+    require_package( # Let us compile python modules:
+        'python-dev',
+        'python-pip',
+        'libmysqlclient-dev',  # For python SQLAlchemy
+        'libxml2-dev',   # For python lxml
+        'libxslt1-dev',  # For python lxml
+        'libffi-dev', # For python requests[security]
+    )
+    # Bring tox/virtualenv... from pip  T46443
+    package { 'tox':
+        ensure   => '1.9.2',
+        provider => 'pip',
+        require  => Package['python-pip'],
+    }
+
+    # Python 3
+    require_package(
+        'python3',
+        'python3-dev',
+        'python3-tk',  # For pywikibot/core running tox-doc-trusty
+    )
+}
