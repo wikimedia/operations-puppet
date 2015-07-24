8d4a80ddc247edb8b0f0d83acfad6663f529a6ac
Antoine Musso <hashar@free.fr>
2015-07-24 17:22:36 +0200

    contint: install pip 7.1.0 from pypi
    pip evolves quickly, install 7.1.0from pypi. Note that requires the
    system package python-pip to be able to install pip.
    
    Left a link to upstream ChangeLog for later reference:
    https://pip.pypa.io/en/latest/news.html
    
    Change-Id: Ida0a79fa8d7258b1a7fe0ba79921eb667bfed03b

--- modules/contint/manifests/packages/python.pp
+++ modules/contint/manifests/packages/python.pp
@@ -6,17 +6,26 @@ class contint::packages::python {
     require_package( # Let us compile python modules:
         'build-essential',
         'python-dev',
-        'python-pip',
+        'python-pip',  # Needed to install pip from pypi
         'libmysqlclient-dev',  # For python SQLAlchemy
         'libxml2-dev',   # For python lxml
         'libxslt1-dev',  # For python lxml
         'libffi-dev', # For python requests[security]
     )
+
+    # Bring in fresh pip. The Trusty package does not provide wheels cache
+    # https://pip.pypa.io/en/latest/news.html
+    package { 'pip':
+        ensure   => '7.1.0',
+        provider => 'pip',
+        require  => Package['python-pip'],  # eggs and chicken
+    }
+
     # Bring tox/virtualenv... from pip  T46443
     package { 'tox':
         ensure   => '1.9.2',
         provider => 'pip',
-        require  => Package['python-pip'],
+        require  => Package['pip'],  # Fresh pip version
     }
 
     # Python 3
