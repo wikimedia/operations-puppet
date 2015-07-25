773ca3cbe3fb027dbfde140d2f1510cd6deff051
Bryan Davis <bd808@wikimedia.org>
2015-07-25 10:01:44 -0600

    Update configuration for logstash 1.5.3
    * Remove obsolete logstash-contrib package
    * Setup trebuchet package for distributing logstash plugins
    * Use require_package() to clean up dependencies
    * Switch from deprecated elasticsearch_http to elasticsearch output
    * Change logstash::output::elasticsearch from class to define
    * Reuse logstash::output::elasticsearch for apifeatureusage output
    
    Bug: T99735
    Bug: T74930
    Change-Id: I24c049139f7ff4b70a4504eea4e4d49d40e79827

---
+++ modules/logstash/manifests/output/elasticsearch/scripts.pp
@@ -0,0 +1,30 @@
+# == Class: logstash::output::elasticsearch::scripts
+#
+# Provision utility scripts for Logstash Elasticsearch output
+#
+class logstash::output::elasticsearch::scripts {
+    file { '/usr/local/bin/logstash_delete_index.sh':
+        ensure => $ensure,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/logstash/logstash_delete_index.sh',
+    }
+
+    file { '/usr/local/bin/logstash_optimize_index.sh':
+        ensure => $ensure,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/logstash/logstash_optimize_index.sh',
+    }
+
+    file { '/usr/local/bin/logstash_clear_cache.sh':
+        ensure => $ensure,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/logstash/logstash_clear_cache.sh',
+    }
+}
+# vim:sw=4 ts=4 sts=4 et:
