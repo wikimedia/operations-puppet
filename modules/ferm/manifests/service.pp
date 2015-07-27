e6ec2138f111c016a073b528c1c09318c26db7b0
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2015-07-27 15:15:21 +0200

    WIP/RfC: Allow multiple/dynamic range of ports for ferm services
    rcstream uses a dynamic range of ports depending on how many CPUs/core
    the server has. Since Puppet doesn't have builtin iteration in the
    current version, the expansion occurs in the ERB template.
    
    If anyone has a alternate suggestion to implement this, I'd be
    interested to learn about it.
    
    This introduces a bit of code duplication in form of a separate
    ferm::service_multiport, an alternative would be to also move
    existing call sites of ferm::service to use an array instead of
    a string to specify the ports. The current approach is less intrusive,
    though.
    
    Bug: T104981
    Change-Id: I0e31307b46ff56286dd6f17595ed620682482628

--- modules/ferm/manifests/service.pp
+++ modules/ferm/manifests/service.pp
@@ -25,3 +25,31 @@ define ferm::service(
         tag     => 'ferm',
     }
 }
+
+# == Define ferm::service_multiport
+# Uses ferm def &SERVICE or &R_SERVICE to allow incoming
+# connections on the specific protocol and ports. This is similar
+# to ferm::service, but allows to specify a list of ports
+#
+# If $srange is not provided, all source addresses will be allowed.
+# otherwise only traffic coming from $srange will be allowed.
+define ferm::service_multiport(
+    $proto,
+    $ports,
+    $ensure  = present,
+    $desc    = '',
+    $prio    = '10',
+    $srange  = undef,
+    $notrack = false,
+) {
+    @file { "/etc/ferm/conf.d/${prio}_${name}":
+        ensure  => $ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0400',
+        content => template('ferm/service-multi.erb'),
+        require => File['/etc/ferm/conf.d'],
+        notify  => Service['ferm'],
+        tag     => 'ferm',
+    }
+}
