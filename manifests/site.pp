ea4494192a052684f163cf20ee268356155d4b6e
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2015-07-29 16:19:08 +0200

    Enable base::firewall on ms1001
    Bug: T104991
    Change-Id: Icceabdf24cc014df8b320edfff2b77677ed3bce3

--- manifests/site.pp
+++ manifests/site.pp
@@ -1695,6 +1695,7 @@ node 'ms1001.wikimedia.org' {
 
     role dataset::systemusers, dataset::secondary, dumps
     include standard
+    include base::firewall
 
     interface::add_ip6_mapped { 'main':
         interface => 'eth0',
