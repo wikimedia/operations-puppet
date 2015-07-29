feac9d8d9458d10dc1b333f9a44ae3c421487f99
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2015-07-29 12:12:22 +0200

    Add a role to run a debdeploy master
    Change-Id: Ic3055b52a7090ef5a60230ab364589624b4100a5

---
+++ modules/debdeploy/manifests/init.pp
@@ -0,0 +1,13 @@
+class debdeploy::master {
+
+    package { 'debdeploy-master':
+        ensure => present,
+    }
+
+    file { '/etc/debdeploy.conf':
+            mode    => '0444',
+            owner   => 'root',
+            group   => 'root',
+            content => template('debdeploy/debdeploy.erb');
+    }
+}
