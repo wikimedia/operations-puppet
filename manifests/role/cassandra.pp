1181c39c8f56adc127524df1b8d24dca8055f579
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-07-30 10:04:50 +0100

    cassandra: check for heap dumps
    We should get notified (but not paged!) when there are heap dumps generated by
    cassandra, usually after OOM, since they can be a valuable investigation tool.
    
    Also introduce cassandra::monitoring and refactor some checks in it.
    
    Bug: T106346
    Change-Id: I9960a4101946f68af6948f08a3ca3e021161af13

--- manifests/role/cassandra.pp
+++ manifests/role/cassandra.pp
@@ -5,6 +5,7 @@ class role::cassandra {
     class { '::cassandra': }
     class { '::cassandra::metrics': }
     class { '::cassandra::logging': }
+    class { '::cassandra::monitoring': }
 
     # temporary collector, T78514
     diamond::collector { 'CassandraCollector':
@@ -13,20 +14,6 @@ class role::cassandra {
 
     system::role { 'role::cassandra':
         description => 'Cassandra server',
-    }
-
-    # Emit an Icinga alert unless there is exactly one Java process belonging
-    # to user 'cassandra' and with 'CassandraDaemon' in its argument list.
-    nrpe::monitor_service { 'cassandra':
-        description  => 'Cassandra database',
-        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u cassandra -C java -a CassandraDaemon',
-    }
-
-    # CQL query interface monitoring (T93886)
-    monitoring::service { 'cassandra-cql':
-        description   => 'Cassanda CQL query interface',
-        check_command => 'check_tcp!9042',
-        contact_group => 'admins,team-services',
     }
 
     $cassandra_hosts = hiera('cassandra::seeds')
