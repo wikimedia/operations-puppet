0a66ca466eb8998f1b6c363804c6c2d4dd6b3f82
Dzahn <dzahn@wikimedia.org>
2015-08-03 15:25:01 +0000

    Revert "Enable base::firewall on dataset1001"
    This reverts commit afa9718f4e5d83d3afd90696430b93f78ed114ca.
    
    Change-Id: I99dea84ac13ccc536acedbe7565d4628b5549f23

--- manifests/site.pp
+++ manifests/site.pp
@@ -495,7 +495,6 @@ node 'dataset1001.wikimedia.org' {
 
     role dataset::systemusers, dataset::primary, dumps
     include standard
-    include base::firewall
 
     interface::add_ip6_mapped { 'eth2':
         interface => 'eth2',
