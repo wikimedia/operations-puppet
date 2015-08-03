c866a10c3744bc12308751e0a6d6cc37ee258b8a
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2015-08-03 12:01:37 +0200

    Add ferm rules for Hive server
    Bug: T83597
    Change-Id: I8aca4ac03060cda0d6c3f60cb67c4318e7864116

--- manifests/role/analytics/hive.pp
+++ manifests/role/analytics/hive.pp
@@ -97,4 +97,10 @@ class role::analytics::hive::server inherits role::analytics::hive::client {
     class { 'cdh::hive::master':
         heapsize => '512',
     }
+
+    ferm::service{ 'hive_server':
+        proto  => 'tcp',
+        port   => '10000',
+        srange => '$INTERNAL',
+    }
 }
