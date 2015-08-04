86d89bea8ce29bfad8ba1755cbcdb85f03acf748
Faidon Liambotis <faidon@wikimedia.org>
2015-08-04 12:47:17 +0300

    pybal: add Service, check_procs nrpe check
    If pybal dies now for whatever reason (e.g. OOM), it never gets
    restarted and we are not getting an alert for it (other than the easy to
    miss BGP session down from the router side).
    
    Add a Service['pybal'] that is ensured running but _not_ notified upon
    by the configuration file on purpose. Also add a nrpe check that is set
    to CRITICAL if the number of /usr/sbin/pybals running is different than
    exactly 1.
    
    Change-Id: Ic68073bf3d2924ddd2c088b0d56bc08c8ec017d9

--- modules/pybal/manifests/configuration.pp
+++ modules/pybal/manifests/configuration.pp
@@ -2,16 +2,18 @@ class pybal::configuration(
     $global_options={},
     $lvs_services,
     $lvs_class_hosts,
-    $site) {
+    $site
+) {
 
     # Generate PyBal config file
     file { '/etc/pybal/pybal.conf':
         require => Package['pybal'],
         content => template("${module_name}/pybal.conf.erb");
+        # do not notify => Service['pybal'] on purpose
     }
 
     $service_keys = keys($lvs_services)
-    pybal::pool {$service_keys:
+    pybal::pool { $service_keys:
         lvs_services => $lvs_services,
         lvs_class_hosts => $lvs_class_hosts
     }
