7d1ab6c409b8daaf7dbbc689d39595d51b3506d4
Ariel T. Glenn <ariel@wikimedia.org>
2015-08-04 17:48:18 +0300

    dumps: generate conf files for dump stage scheduler
    Change-Id: I8db89b6a1e4d4bc6996246d221b3c6c8ad068753

---
+++ modules/snapshot/manifests/dumps/stagesconfig.pp
@@ -0,0 +1,62 @@
+class snapshot::dumps::stagesconfig(
+    $enable = true;
+) {
+
+    include snapshot::dirs
+
+    $firststage_args = '--cutoff today'
+    $rest_args= '--date last --onepass'
+    $wikiargs = 'continue none /bin/bash ./worker --skipdone --exclusive --log'
+
+    $args_smallwikis = "${wikiargs} --configfile confs/wikidump.conf"
+    $args_bigwikis = "${wikiargs} --configfile confs/wikidump.conf.bigwikis"
+    $args_hugewikis = "${wikiargs} --configfile confs/wikidump.conf.hugewikis"
+
+    $jobs_to_skip = join(['metahistorybz2dump',
+                          'metahistorybz2dumprecombine',
+                          'metahistory7zdump',
+                          'metahistory7zdumprecombine'])
+
+    $stages = {
+        smallwikis   => {
+            firststage => "${args_smallwikis} ${firststage_args}",
+            rest       => "${args_smallwikis} ${rest_args}",
+        },
+        bigwikis     => {
+            firststage => "${args_bigwikis} ${firststage_args}",
+            rest       => "${args_bigwikis} ${rest_args}",
+        },
+        hugewikis    => {
+            firststage => "${args_hugewikis} ${firststage_args}",
+            rest       => "${args_hugewikis} ${rest_args}",
+        },
+        skipjob_args => "--skipjobs ${jobs_to_skip}",
+    }
+
+    if ($enable) {
+        file { "${snapshot::dirs::dumpsdir}/stages":
+            ensure => 'directory',
+            path   => "${snapshot::dirs::dumpsdir}/stages",
+            mode   => '0755',
+            owner  => 'root',
+            group  => 'root',
+        }
+
+        snapshot::dumps::stagesconf { 'stages_normal':
+            stagestype => 'normal',
+            stages     => $stages,
+        }
+        snapshot::dumps::stagesconf { 'stages_partial':
+            stagestype => 'partial',
+            stages     => $stages,
+        }
+        snapshot::dumps::stagesconf { 'stages_normal_hugewikis':
+            stagestype => 'normal_huge',
+            stages     => $stages,
+        }
+        snapshot::dumps::stagesconf { 'stages_partial_hugewikis':
+            stagestype => 'partial_huge',
+            stages     => $stages,
+        }
+    }
+}
