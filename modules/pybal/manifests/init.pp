29e7dc40e5dd35acc797fa4cfd9e54d5eaab601a
Faidon Liambotis <faidon@wikimedia.org>
2015-08-04 12:47:17 +0300

    pybal: add Service, check_procs nrpe check
    If pybal dies now for whatever reason (e.g. OOM), it never gets
    restarted and we are not getting an alert for it (other than the easy to
    miss BGP session down from the router side).
    
    Add a Service['pybal'] that is ensured running but _not_ notified upon
    by the configuration file on purpose. Also add a nrpe check that is set
    to CRITICAL if the number of /usr/sbin/pybals running is different than
    exactly 1.
    
    Change-Id: Ic68073bf3d2924ddd2c088b0d56bc08c8ec017d9

--- modules/pybal/manifests/init.pp
+++ modules/pybal/manifests/init.pp
@@ -1,6 +1,15 @@
 class pybal {
-
     package { [ 'ipvsadm', 'pybal' ]:
-        ensure => installed;
+        ensure => installed,
+    }
+
+    service { 'pybal':
+        ensure => running,
+        enable => true,
+    }
+
+    nrpe::monitor_service { 'pybal':
+        description  => 'pybal',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -a /usr/sbin/pybal',
     }
 }
