a6d48b1d02512be107e9c9ca440e346570a38d3c
YuviPanda <yuvipanda@gmail.com>
2015-08-05 14:52:38 -0700

    k8s: Hook up docker to flannel
    Change-Id: If4a5c3bd134011058166b32858c83858ae6f15fb

--- modules/k8s/manifests/docker.pp
+++ modules/k8s/manifests/docker.pp
@@ -3,4 +3,12 @@
 # Sets up docker as used by kubernetes
 class k8s::docker {
     require_package('docker.io')
+
+    file { '/etc/default/docker':
+        source => 'puppet:///modules/k8s/docker.default',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        notify => Service['docker'],
+    }
 }
