cd67a9c6f43753c695bb8bc776807b852c250941
YuviPanda <yuvipanda@gmail.com>
2015-08-06 12:05:42 -0700

    k8s: Create worker role and include that in master too
    Change-Id: Ib0a17a954b7f82fc9954001bc93528dc23007118

--- manifests/role/labsk8s.pp
+++ manifests/role/labsk8s.pp
@@ -2,4 +2,10 @@ class role::labs::k8s::master {
     class { 'k8s::kubelet':
         master_host => hiera('k8s_master', $::fqdn),
     }
+
+    include role::labs::k8s::worker
+}
+
+class role::labs::k8s::worker {
+    include k8s::flannel
 }
