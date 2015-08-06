182246a5fbb4c3c3fdfb5ec9f18a6074996653a9
dzahn <dzahn@wikimedia.org>
2015-08-06 16:11:05 -0700

    fermium: enable in site.pp, include admin
    Bug:T108070
    Change-Id: Ic4272748d3a066f8bc38d6c3d2fa068dd7ae325e

--- manifests/site.pp
+++ manifests/site.pp
@@ -963,10 +963,11 @@ node 'eventlog1001.eqiad.wmnet' {
 }
 
 # virtual machine for testing mailman migration
-#node 'fermium.eqiad.wmnet' {
-#    include standard
-#    include base::firewall
-#}
+node 'fermium.eqiad.wmnet' {
+    include standard
+    include admin
+    include base::firewall
+}
 
 node 'fluorine.eqiad.wmnet' {
     $cluster = 'misc'
