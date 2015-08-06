7b96654ee47098fa4f7be37dad019edad3805413
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2015-08-06 15:10:41 +0200

    Add ferm rules for Hadoop worker nodes
    Bug: T83597
    Change-Id: If36667152b3bcc679922f47cc0197faa4ecf2d93

--- manifests/role/analytics/hadoop.pp
+++ manifests/role/analytics/hadoop.pp
@@ -621,6 +621,60 @@ class role::analytics::hadoop::worker inherits role::analytics::hadoop::client {
         srange => '$INTERNAL',
     }
 
+    ferm::service{ 'hadoop_jmx_datanode':
+        proto  => 'tcp',
+        port   => '9981',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop_jmx_yarn_nodemanager':
+        proto  => 'tcp',
+        port   => '9984',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop_yarn_nodemanager':
+        proto  => 'tcp',
+        port   => '8041',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop_yarn_nodemanager_localizer':
+        proto  => 'tcp',
+        port   => '8040',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop_yarn_nodemanager_http_ui':
+        proto  => 'tcp',
+        port   => '8042',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop_hdfs_hd_journalnode':
+        proto  => 'tcp',
+        port   => '8485',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop_hdfs_datanode_http_ui':
+        proto  => 'tcp',
+        port   => '50075',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop_hdfs_datanode_ipc':
+        proto  => 'tcp',
+        port   => '50020',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop_hdfs_datanode_dfs_transfer':
+        proto  => 'tcp',
+        port   => '50010',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
     # Install hive client on worker nodes to get
     # hive-hcatalog package.  hive-catalog depends
     # on hive package, so we might as well
