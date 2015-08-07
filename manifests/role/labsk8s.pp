1a32d5c3d2c723dc0b0474525328a393f2a6f7db
YuviPanda <yuvipanda@gmail.com>
2015-08-07 03:57:07 -0700

    k8s: Provision worker nodes properly
    Change-Id: I68397ca0553717f376928fe52e4c321d2c4e7ece

--- manifests/role/labsk8s.pp
+++ manifests/role/labsk8s.pp
@@ -2,18 +2,10 @@ class role::labs::k8s::master {
     $master_host = hiera('k8s_master', $::fqdn)
     $etcd_servers = hiera('etcd_servers')
 
-    class { 'k8s::kubelet':
-        master_host => $master_host,
-    }
-
     class { 'k8s::apiserver':
         master_host => $master_host,
         etcd_servers => $etcd_servers,
 
-    }
-
-    class { 'k8s::proxy':
-        master_host => $master_host,
     }
 
     class { 'k8s::scheduler':
@@ -25,4 +17,14 @@ class role::labs::k8s::master {
 
 class role::labs::k8s::worker {
     include k8s::docker
+
+    $master_host = hiera('k8s_master', $::fqdn)
+
+    class { 'k8s::proxy':
+        master_host => $master_host,
+    }
+
+    class { 'k8s::kubelet':
+        master_host => $master_host,
+    }
 }
