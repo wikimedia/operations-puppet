cc123e9e25cd5c64e2a3ab619f07f4eeb1c79298
YuviPanda <yuvipanda@gmail.com>
2015-08-06 18:01:30 -0700

    k8s: Remove useless $master_ip ipresolve
    Change-Id: I2fe864eed44c53d2004c906174b60957aba932c4

--- modules/k8s/manifests/apiserver.pp
+++ modules/k8s/manifests/apiserver.pp
@@ -8,7 +8,6 @@ class k8s::apiserver(
         mode   => '0555',
     }
 
-    $master_ip = ipresolve($master_host, 4, $::nameservers[0])
     base::service_unit { 'kube-apiserver':
         systemd => true,
         require => File['/usr/local/bin/kube-apiserver'],
