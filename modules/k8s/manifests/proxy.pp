c5c452f1f7eabbeae38ba1f0f6210857ddfb3cdd
YuviPanda <yuvipanda@gmail.com>
2015-08-07 03:31:40 -0700

    k8s: Add kube-proxy class
    Change-Id: Ieaacca38f1e609b07cf298c14003bc498895747e

---
+++ modules/k8s/manifests/proxy.pp
@@ -0,0 +1,17 @@
+class k8s::proxy((
+    $master_host,
+) {
+    file { '/usr/local/bin/kube-proxy':
+        source => '/data/scratch/k8s/kubernetes/server/bin/kube-proxy',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+
+    $master_ip = ipresolve($master_host, 4, $::nameservers[0])
+
+    base::service_unit { 'kube-proxy':
+        systemd => true,
+        require => File['/usr/local/bin/kube-proxy'],
+    }
+}
