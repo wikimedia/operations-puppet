a7cefe0b4a9d790465abf1c8a8ac52ef9cfda6f6
Kartik Mistry <kartik.mistry@gmail.com>
2015-08-07 20:24:40 +0530

    Apertium: Add -j -m and parameters
    -j - Number of APY instance processes to run
    -m - Seconds to wait before shutdown idle process
    
    We've set -j 1 and -m 300 for production.
    
    Change-Id: Id386c9964c844c9a37e4c2f311b61c17e1b02154

--- modules/apertium/manifests/init.pp
+++ modules/apertium/manifests/init.pp
@@ -3,8 +3,17 @@
 # Apertium is a backend Machine Translation service for the Content Translation.
 # https://www.mediawiki.org/wiki/Content_translation/Apertium
 #
-class apertium(){
-    package { [
+# === Parameters
+#
+# [*num_of_processes*]
+# Number of APY instance processes to run
+# [*max_idle_seconds*]
+# Seconds to wait before shutdown idle process
+class apertium(
+    $num_of_processes = 1,
+    $max_idle_seconds = 300,
+) {
+    require_package { [
         'apertium',
         'apertium-af-nl',
         'apertium-apy',
@@ -54,8 +63,28 @@ class apertium(){
         notify => Service['apertium-apy'],
     }
 
+    # The upstart configuration
+    file { '/etc/init/apertium.conf':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444'
+    }
+
+    # Link with upstart-job
+    file { '/etc/init.d/apertium-apy':
+        ensure => 'link',
+        target => '/lib/init/upstart-job',
+    }
+
     service { 'apertium-apy':
         ensure  => running,
-        require => Package['apertium-apy'],
+        hasstatus  => true,
+        hasrestart => true,
+        provider   => 'upstart',
+        require    => [
+            File['/etc/init.d/apertium-apy']
+        ],
+        subscribe  => File['/etc/init/apertium.conf'],
     }
 }
