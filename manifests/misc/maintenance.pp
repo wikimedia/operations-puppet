fee9554d2176e5bc1c6a9e01b24598cdc69dfddd
Marius Hoch <hoo@online.de>
2015-08-08 13:06:50 +0200

    Set a higher lock-grace-interval for wikidata dispatchers
    Seems that it can take several minutes to dispatch to a single
    wiki, thus it would be very unfortunate to poke at these locks
    already.
    Should not be an issue as dispatchers die very rarely and we
    only alert after 300s of dispatch lag.
    
    Change-Id: I0bbe0f76be9fe55db89af7909571660a256b6328

--- manifests/misc/maintenance.pp
+++ manifests/misc/maintenance.pp
@@ -196,7 +196,7 @@ class misc::maintenance::wikidata( $ensure = present ) {
     # This handles inserting jobs into client job queue, which then process the changes
     cron { 'wikibase-dispatch-changes4':
         ensure  => $ensure,
-        command => '/usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dispatchChanges.php --wiki wikidatawiki --max-time 1600 --batch-size 275 --dispatch-interval 25 2>&1 >> /dev/null',
+        command => '/usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dispatchChanges.php --wiki wikidatawiki --max-time 1600 --batch-size 275 --dispatch-interval 25 --lock-grace-interval 200 2>&1 >> /dev/null',
         user    => $::mediawiki::users::web,
         minute  => '*/3',
     }
