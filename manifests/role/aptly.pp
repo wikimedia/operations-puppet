898bf2a3eda13dd96c482980c139f18750bee5fd
YuviPanda <yuvipanda@gmail.com>
2015-08-08 21:37:45 -0700

    aptly: Add module + simple role
    Bug: T104194
    Change-Id: If8683243afac2720f48e94ff053c1f0b43ba8bad

---
+++ manifests/role/aptly.pp
@@ -0,0 +1,6 @@
+# = Class: role::aptly
+#
+# Sets up a simple aptly repo server serving over http on port 80
+class role::aptly {
+    include aptly
+}
---
+++ modules/aptly/manifests/init.pp
@@ -0,0 +1,26 @@
+# Class: aptly
+# Sets up a simple aptly repo server serving over http
+#
+# Set up to only allow root to add packages
+class aptly {
+    require_package('aptly')
+
+    file { '/srv/packages':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    file { '/etc/aptly.conf':
+        ensure  => present,
+        source  => 'puppet:///modules/aptly/aptly.conf',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+
+    nginx::site { 'aptly-server':
+        source => 'puppet:///modules/aptly/aptly.nginx.conf',
+    }
+}
