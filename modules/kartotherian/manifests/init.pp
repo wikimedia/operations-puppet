6878f8659324790c04e9d5eacf1e68da9560ed54
Yuri Astrakhan <yurik@wikimedia.org>
2015-08-10 16:27:31 +0200

    Add usernames+passwords to kartotherian config erb file
    * In order for the kartotherian service to connect to the DB & cassandra, provide it with the passwords
       in the generated service config files.
    
    Bug: T108610
    Change-Id: Ib18328e86f15ddf5505bcb0b443b02f63c5b95b8

--- modules/kartotherian/manifests/init.pp
+++ modules/kartotherian/manifests/init.pp
@@ -7,6 +7,12 @@
 # classes as well as conform to a de-facto standard of having a module for every
 # service
 class kartotherian() {
+
+    $cassandra_kartotherian_user = hiera('cassandra::kartotherian_user')
+    $cassandra_kartotherian_pass = hiera('cassandra::kartotherian_pass')
+    $pgsql_kartotherian_user = hiera('postgresql::master::kartotherian_user')
+    $pgsql_kartotherian_pass = hiera('postgresql::master::kartotherian_pass')
+
     service::node { 'kartotherian':
         port   => 4000,
         config => template('kartotherian/config.yaml.erb'),
