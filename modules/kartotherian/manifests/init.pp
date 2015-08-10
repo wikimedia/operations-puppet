982466a8fe9d4d27a3a59f98af5b2a0ebb2e8fc4
Yuri Astrakhan <yurik@wikimedia.org>
2015-08-10 16:27:31 +0200

    Add account info to kartotherian config erb file
    * In order for the kartotherian service to connect to the DB & cassandra, provide it with the passwords
       in the generated service config files.
    
    Change-Id: Ib18328e86f15ddf5505bcb0b443b02f63c5b95b8

--- modules/kartotherian/manifests/init.pp
+++ modules/kartotherian/manifests/init.pp
@@ -7,6 +7,21 @@
 # classes as well as conform to a de-facto standard of having a module for every
 # service
 class kartotherian() {
+
+
+    # TODO: Switch to these passwords: non-default cassandra, and dedicated postgresql for kartotherian
+#    $cassandra_kartotherian_user = 'cassandra'
+#    $cassandra_kartotherian_pass = hiera('cassandra::kartotherian_pass')
+#    $pgsql_kartotherian_user = 'kartotherian'
+#    $pgsql_kartotherian_pass = hiera('postgresql::master::kartotherian_pass')
+
+    # TODO: Remove these once above are ready
+    # For now, using tilerator pgsql account is ok because it has identical rights (read-only db access)
+    $cassandra_kartotherian_user = 'cassandra'
+    $cassandra_kartotherian_pass = 'cassandra'
+    $pgsql_kartotherian_user = 'tilerator'
+    $pgsql_kartotherian_pass = hiera('postgresql::master::tilerator_pass')
+
     service::node { 'kartotherian':
         port   => 4000,
         config => template('kartotherian/config.yaml.erb'),
