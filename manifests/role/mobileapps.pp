aca0a9268c017a3438c57c13fe6ee433ff3898d2
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-08-11 18:26:32 +0300

    Introducing mobileapps role and puppet module
    Bug: T105538
    Change-Id: I4cbc5a3bc1b96332d06db4b8a7b7e1f1f9ed95f6

---
+++ manifests/role/mobileapps.pp
@@ -0,0 +1,10 @@
+# Role class for mobileapps
+class role::mobileapps {
+
+    system::role { 'role::mobileapps':
+        description => 'A service for use by mobile apps. Provides DOM manipulation, aggregation, JSON flattening'
+    }
+
+    include ::mobileapps
+}
+
