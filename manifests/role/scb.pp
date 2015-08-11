2be8bbf62ada03bf1d3f23942a0181af68cc9a5d
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-08-11 19:17:43 +0300

    Get scb up to par with sca
    Create relevant yaml structures for conftool, ganglia, icinga. Minor
    cleanup of icinga structures as well while at it.
    
    Change-Id: Icb4ef8f188da206d8047730823d7c8b775fb7f8c

--- manifests/role/scb.pp
+++ manifests/role/scb.pp
@@ -1,5 +1,6 @@
 # "Compendium" class for nodes supporting various *oid services
 # This class is an intermediate step to better design
+@monitoring::group { 'scb_eqiad': description => 'Service Cluster B servers' }
 class role::scb {
     include standard
     include base::firewall
