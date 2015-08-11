9788fec428bae129bfad93e99f38fc699edfa6e9
YuviPanda <yuvipanda@gmail.com>
2015-08-11 13:53:20 -0700

    ores: Mark all roles requiring ores::base properly
    Change-Id: I0ed748b1feb5bbe9e9ebfd277b40c0d38528c9b4

--- modules/ores/manifests/precached.pp
+++ modules/ores/manifests/precached.pp
@@ -1,6 +1,8 @@
 # = Class: ores::precached
 # Run a pre-caching daemon that listens to RCStream
 class ores::precached {
+    require ores::base
+
     $working_dir = $::ores::base::config_path
     $venv_path  = $::ores::base::venv_path
 
