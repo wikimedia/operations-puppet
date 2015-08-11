38aca28c5e35679a55dad2654012ad30d006b2c5
YuviPanda <yuvipanda@gmail.com>
2015-08-11 13:50:17 -0700

    ores: Add role+class for the precached daemon
    It reads new revisions from RCStream and makes ORES
    pre-compute the required scores before they are
    requested by actual clients (hopefully)
    
    Only one instance of this should be running.
    
    Change-Id: Idd419dd3ecc02a9caa2540744994a479417afe3b

---
+++ modules/ores/manifests/precached.pp
@@ -0,0 +1,10 @@
+# = Class: ores::precached
+# Run a pre-caching daemon that listens to RCStream
+class ores::precached {
+    $working_dir = $::ores::base::config_path
+    $venv_path  = $::ores::base::venv_path
+
+    base::service_unit { 'precached':
+        systemd => true,
+    }
+}
