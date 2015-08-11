1cd724ac39a49f9c0cab4359a0c9a8ed9a4e4d7b
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-08-11 19:17:43 +0300

    Get scb up to par with sca
    Create relevant yaml structures for conftool, ganglia, icinga. Minor
    cleanup of icinga structures as well while at it.
    
    Change-Id: Icb4ef8f188da206d8047730823d7c8b775fb7f8c

--- manifests/role/mathoid.pp
+++ manifests/role/mathoid.pp
@@ -1,9 +1,5 @@
 # vim: set ts=4 et sw=4:
 
-# TODO: now that other services inhabit service cluster A, move this definition in a
-# better place
-@monitoring::group { 'sca_eqiad': description => 'Service Cluster A servers' }
-
 class role::mathoid{
     system::role { 'role::mathoid':
         description => 'mathoid server'
