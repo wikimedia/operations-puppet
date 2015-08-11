3c0d3d92568f2cb5d3f875f38a1bdf7ca3edc106
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-08-11 18:26:32 +0300

    Assign mobileapps service to sca cluster
    Bug: T105538
    Change-Id: Ic35916933cbcf25b16e01a846f74afc9fa3a79b1

--- manifests/role/scb.pp
+++ manifests/role/scb.pp
@@ -1,6 +1,8 @@
 # "Compendium" class for nodes supporting various *oid services
 # This class is an intermediate step to better design
 class role::scb {
+    include role::mobileapps
+
     include standard
     include base::firewall
     if $::realm == 'production' {
