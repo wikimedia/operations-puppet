48d5d32cbe918687e5bfe356d30a89f4e7389d6b
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-08-11 18:26:32 +0300

    Introducing mobileapps role and puppet module
    Bug: T105538
    Change-Id: I4cbc5a3bc1b96332d06db4b8a7b7e1f1f9ed95f6

---
+++ modules/mobileapps/manifests/init.pp
@@ -0,0 +1,25 @@
+
+# Class: mobileapps
+#
+# This class installs and configures mobileapps
+#
+# While only being a thin wrapper around service::node, this class exists to
+# accomodate future mobileapps needs that are not suited for the service module
+# classes as well as conform to a de-facto standard of having a module for every
+# service
+#
+# === Parameters
+#
+# [*restbase_uri*]
+#   The host/IP where to reach RESTBase
+#
+class mobileapps(
+    $restbase_uri = 'http://restbase.svc.eqiad.wmnet',
+) {
+    service::node { 'mobileapps':
+        port            => 8888,
+        config          => template('mobileapps/config.yaml.erb'),
+        has_spec        => true,
+        healthcheck_url => '',
+    }
+}
