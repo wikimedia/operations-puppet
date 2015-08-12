d3ce329dc704b2b2f967a6ecb439f914fbde6332
Ori Livneh <ori@wikimedia.org>
2015-08-12 13:57:15 -0700

    Make snapshot::packages compatible with trusty
    Change-Id: Ifd8a713a9f76d586dfa164e88ca0f9b0188c51fe

--- modules/snapshot/manifests/packages.pp
+++ modules/snapshot/manifests/packages.pp
@@ -1,33 +1,11 @@
 class snapshot::packages {
 
     # pick up various users, twemproxy
-    include mediawiki
+    include ::mediawiki
 
-    if ($::lsbdistcodename == 'precise') {
-        package { [
-            'subversion',
-            'mysql-client-5.5',
-            'p7zip-full',
-            'libicu42',
-            'utfnormal',
-            'mwbzutils'
-            ]: ensure => 'present',
-        }
-    }
-    else {
-        package { [
-            'subversion',
-            'mysql-client-5.1',
-            'p7zip-full',
-            'libicu42',
-            'wikimedia-php5-utfnormal',
-            ]: ensure => 'present',
-        }
-    }
-
-    # want mediawiki but no running webserver
-    exec { 'stop-apache-service':
-        command => '/etc/init.d/apache2 stop',
-        onlyif  => '/etc/init.d/apache2 status',
-    }
+    require_package('mwbzutils')
+    require_package('mysql-client')
+    require_package('p7zip-full')
+    require_package('subversion')
+    require_package('utfnormal')
 }
