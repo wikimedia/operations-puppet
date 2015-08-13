d04aa0569f476bc464f2bafff8dda40fb0488a17
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-08-13 16:23:55 +0300

    Include role mobileapps in role scb
    Change-Id: I1e922fa9d2bb56ab08879ce5021457ff5451b97c

--- manifests/role/scb.pp
+++ manifests/role/scb.pp
@@ -2,6 +2,8 @@
 # This class is an intermediate step to better design
 @monitoring::group { 'scb_eqiad': description => 'Service Cluster B servers' }
 class role::scb {
+    include role::mobileapps
+
     include standard
     include base::firewall
     if $::realm == 'production' {
