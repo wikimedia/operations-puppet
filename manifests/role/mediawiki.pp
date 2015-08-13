5100fc381515819afc202d76932cb120901789c3
dzahn <dzahn@wikimedia.org>
2015-08-13 07:56:01 -0700

    mediawiki: include font packages on all appservers
    Currently ::mediawiki::packages::fonts does not get included
    on all appservers, but it probably should for bugs like T84777.
    
    Bug:T84777
    Change-Id: I83e5329a45c42b93abafa1e4466852c83d00b768

--- manifests/role/mediawiki.pp
+++ manifests/role/mediawiki.pp
@@ -136,7 +136,7 @@ class role::mediawiki::appserver {
     system::role { 'role::mediawiki::appserver': }
 
     include ::role::mediawiki::webserver
-
+    include ::mediawiki::packages::fonts
 }
 
 class role::mediawiki::appserver::api {
