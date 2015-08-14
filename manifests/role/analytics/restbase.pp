189c93108308b92eeffe81de5c2c529d2ed7ecf2
milimetric <dandreescu@wikimedia.org>
2015-08-14 11:25:31 -0400

    [WIP] Add an Analytics specific instance of RESTBase
    The goal of this patch is to host a separate instance of RESTBase that
    the current instance of RESTBase will proxy for analytics requests.  A
    new domain would be set up on the existing instance, and called
    something like analytics.wikimedia.org and requests would look like
    this:
    
      /analytics.wikimedia.org/v1/pageviews/top/all/year
    
    We talked to the services team and the reason for a separate instance is
    because they expressed concern about running this alongside the existing
    RESTBase.  Just in case something goes wrong, they didn't want one to
    potentially take down the other, nor use up each other's resources.  The
    way Cassandra storage is configured with both of these instances is that
    it just runs on the same machines that RESTBase runs on.  Gabriel
    recommended we stick to that pattern instead of using a remote Cassandra
    cluster.
    
    Bug: T107056
    Change-Id: I29c872ed6a811cf1fd1ca9e5242bf513cba401ba

---
+++ manifests/role/analytics/restbase.pp
@@ -0,0 +1,34 @@
+# == Class role::restbase
+#
+
+@monitoring::group { 'restbase_analytics_eqiad': description => 'Analytics Restbase eqiad' }
+@monitoring::group { 'restbase_analytics_codfw': description => 'Analytics Restbase codfw' }
+
+# Config should be pulled from hiera
+class role::analytics::restbase {
+    system::role { 'restbase-analytics': description => "Analytics Restbase ${::realm}" }
+
+    include ::restbase
+    include ::restbase::monitoring
+
+    include lvs::realserver
+
+
+    ferm::service {'restbase_web':
+        proto => 'tcp',
+        port  => '7231',
+    }
+
+    /*
+    TODO: add monitoring once we figure out what metrics we want
+    monitoring::graphite_threshold { 'restbase_analytics_<<some-metric-name>>':
+        description   => 'Analytics RESTBase req/s returning 5xx http://grafana.wikimedia.org/#/dashboard/db/restbase',
+        metric        => '<<the metric and any transformations>>',
+        from          => '10min',
+        warning       => '<<warning threshold>>', # <<explain>>
+        critical      => '<<critical threshold>>', # <<explain>>
+        percentage    => '20',
+        contact_group => 'analytics',
+    }
+    */
+}
