18c9097fade22a1b16ff851e5ced3bdb73f8d03b
milimetric <dandreescu@wikimedia.org>
2015-08-14 11:25:31 -0400

    Add Analytics Query Service role
    The goal of this patch is to host a separate instance of RESTBase that
    the current instance of RESTBase will proxy for analytics requests.  A
    new domain would be set up on the existing instance, and called
    something like analytics.wikimedia.org and requests would look like
    this:
    
      /analytics.wikimedia.org/v1/pageviews/top/desktop/2015/02/all-days
    
    We talked to the services team and the reason for a separate instance is
    because they expressed concern about running this alongside the existing
    RESTBase.  Just in case something goes wrong, they didn't want one to
    potentially take down the other, nor use up each other's resources.  The
    way Cassandra storage is configured with both of these instances is that
    it just runs on the same machines that RESTBase runs on.  Gabriel
    recommended we stick to that pattern instead of using a remote Cassandra
    cluster.
    
    Bug: T107056
    Change-Id: I29c872ed6a811cf1fd1ca9e5242bf513cba401ba

---
+++ manifests/role/aqs.pp
@@ -0,0 +1,88 @@
+@monitoring::group { 'aqs_eqiad': description => 'Analytics Query Service eqiad' }
+@monitoring::group { 'aqs_codfw': description => 'Analytics Query Service codfw' }
+
+# == Class role::aqs
+# Analytics Query Service
+#
+# AQS is made up of a RESTBase instance backed by Cassandra.
+# Each AQS node is has both colocated.
+#
+class role::aqs {
+    system::role { 'role::aqs':
+        description => 'Analytics Query Service Node',
+    }
+
+
+    #
+    # Set up Cassandra for AQS.
+    #
+
+    # Parameters to be set by Hiera
+    include ::cassandra
+    include ::cassandra::metrics
+    include ::cassandra::logging
+
+    # Emit an Icinga alert unless there is exactly one Java process belonging
+    # to user 'cassandra' and with 'CassandraDaemon' in its argument list.
+    nrpe::monitor_service { 'cassandra-analytics':
+        description  => 'Analytics Cassandra database',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u cassandra -C java -a CassandraDaemon',
+    }
+
+    # CQL query interface monitoring (T93886)
+    monitoring::service { 'cassandra-analytics-cql':
+        description   => 'Analytics Cassanda CQL query interface',
+        check_command => 'check_tcp!9042',
+        contact_group => 'admins,analytics',
+    }
+
+    $cassandra_hosts = hiera('cassandra::seeds')
+    $cassandra_hosts_ferm = join($cassandra_hosts, ' ')
+
+    # Cassandra intra-node messaging
+    ferm::service { 'cassandra-analytics-intra-node':
+        proto  => 'tcp',
+        port   => '7000',
+        srange => "@resolve((${cassandra_hosts_ferm}))",
+    }
+    # Cassandra JMX/RMI
+    ferm::service { 'cassandra-analytics-jmx-rmi':
+        proto  => 'tcp',
+        port   => '7199',
+        srange => "@resolve((${cassandra_hosts_ferm}))",
+    }
+    # Cassandra CQL query interface
+    ferm::service { 'cassandra-analytics-cql':
+        proto  => 'tcp',
+        port   => '9042',
+        srange => "@resolve((${cassandra_hosts_ferm}))",
+    }
+
+
+    #
+    # Set up RESTBase for AQS
+    #
+
+    include ::restbase
+    include ::restbase::monitoring
+
+    include lvs::realserver
+
+    ferm::service {'restbase_web':
+        proto => 'tcp',
+        port  => '7231',
+    }
+
+    /*
+    TODO: add monitoring once we figure out what metrics we want
+    monitoring::graphite_threshold { 'restbase_analytics_<<some-metric-name>>':
+        description   => 'Analytics RESTBase req/s returning 5xx http://grafana.wikimedia.org/#/dashboard/db/restbase',
+        metric        => '<<the metric and any transformations>>',
+        from          => '10min',
+        warning       => '<<warning threshold>>', # <<explain>>
+        critical      => '<<critical threshold>>', # <<explain>>
+        percentage    => '20',
+        contact_group => 'analytics',
+    }
+    */
+}
