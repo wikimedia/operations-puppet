320913fb130e4bf50f901790fdbb789655ddb43b
milimetric <dandreescu@wikimedia.org>
2015-08-14 11:25:31 -0400

    [WIP] Add an Analytics specific instance of RESTBase
    Very much a work in progress.  I am not at all sure how hiera works and
    if I've copy/pasted properly here.  I'll work on this hopefully with
    some help.
    
    Bug: T107056
    Change-Id: I29c872ed6a811cf1fd1ca9e5242bf513cba401ba

--- manifests/site.pp
+++ manifests/site.pp
@@ -2157,6 +2157,13 @@ node /^restbase100[1-9]\.eqiad\.wmnet$/ {
     include standard
 }
 
+# analytics restbase eqiad cluster
+node /^restbase-analytics100[1-2]\.eqiad\.wmnet$/ {
+    role analytics::restbase, analytics::cassandra
+    include base::firewall
+    include standard
+}
+
 # network insights (netflow/pmacct, etc.)
 node 'rhenium.wikimedia.org' {
     role pmacct
