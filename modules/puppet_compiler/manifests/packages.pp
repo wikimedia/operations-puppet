f2316049259426cb2881b8e12e7542e679ed0444
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-08-14 11:10:35 +0200

    puppet_compiler: refresh module/role
    Since we rebuilt the puppet compiler, we refresh the puppet module as
    well.
    
    Bug: T96802
    Change-Id: Ie3fc79eec44667f3bee4f5cbb9130f3826e94964

--- modules/puppet_compiler/manifests/packages.pp
+++ modules/puppet_compiler/manifests/packages.pp
@@ -1,16 +1,8 @@
-class puppet_compiler::packages($ensure = $puppet_compiler::ensure) {
-    include stdlib
-    $list = [
-        'curl',
-        'python-dev',
-        'rubygems',
-        'ruby-bundler',
-        'ruby1.8-dev',
-        'mysql-server',
-        'mysql-client',
-        'ruby-mysql',
-        'ruby-bcrypt',
-        'nginx'
-        ]
-    ensure_packages([$list])
+# === Class puppet_compiler::packages
+#
+# Installs all the needed packages
+class puppet_compiler::packages {
+
+    require_package 'python-yaml', 'python-requests', 'python-jinja2', 'nginx'
+
 }
