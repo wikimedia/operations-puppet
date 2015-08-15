18d7735e6549abea1d33243574ead3e17710c553
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-08-15 19:29:59 +0200

    service: add deployment_script define
    We introduce a define that helps releasing the code at a specific
    version, locally, by first checking out the code, then refreshing
    the config file with a tagged puppet run, and finally checking the
    health of the system.
    
    Change-Id: Id226d368378c9223ce96e0993b0e668397337ccb

---
+++ modules/service/manifests/deployment_script.pp
@@ -0,0 +1,20 @@
+# === Define service::deployment_script
+#
+# Creates a script that should make deploying a config+code change easier.
+#
+define service::deployment_script(
+    $monitor_url,
+    $release_dir="/srv/deployment/${title}/deploy",
+    $provider='git',
+    $has_autorestart=false,
+){
+    $service = $title
+    file { "/usr/local/bin/${service}-deploy":
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        content => template('service/deployment_script.sh.erb')
+    }
+
+}
