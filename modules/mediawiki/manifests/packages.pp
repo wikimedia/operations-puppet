eb46578619b30fc4fbe303e395988334e137e138
Reedy <reedy@wikimedia.org>
2015-08-16 16:28:54 +0100

    Install texlive-extra-utils on mw appservers
    Bug: T109195
    Change-Id: I72c821001d2fa9ab1f872299f60cf91ab2484d2d

--- modules/mediawiki/manifests/packages.pp
+++ modules/mediawiki/manifests/packages.pp
@@ -24,6 +24,7 @@ class mediawiki::packages {
     package { [
         'texlive',
         'texlive-bibtex-extra',
+        'texlive-extra-utils',
         'texlive-font-utils',
         'texlive-fonts-extra',
         'texlive-lang-all',
