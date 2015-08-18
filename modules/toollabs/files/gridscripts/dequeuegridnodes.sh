58d8a8cdad8de3ee2d15f8590472580812611ea5
andrewbogott <abogott@wikimedia.org>
2015-08-18 11:05:22 -0500

    Add some crappy but handy scripts for managing the grid during reboots.
    Change-Id: Ib5a61dc192b41395e8a36302a841e0a9a6c8cdce

---
+++ modules/toollabs/files/gridscripts/dequeuegridnodes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# THIS FILE IS MAINTAINED BY PUPPET
+# source: modules/toollabs/files/gridscripts/
+#
+# Disable job queue for all args
+#
+# Example:
+#
+# ./dequeue.sh tools-exec-1211 tools-exec-1212 tools-exec-1215
+#
+if [ `hostname` != 'tools-master' ]; then
+    echo "This can only be run on tools-master."
+    exit 1
+fi
+for node in "$@"
+do
+    qmod -d "*@${node}"
+done
