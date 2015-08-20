b3c1d2aa1ef49a4ffdf8ad714300aa8adbc4a159
Alex Monk <krenair@gmail.com>
2015-08-20 03:57:23 +0100

    Make foreachwikiindblist accept dblist expressions
    See I81df3a61, I0a460754
    
    Bug: T101213
    Change-Id: I1026070e83a8f7e7559b6f37d99a968e0e833bf2

--- modules/scap/manifests/scripts.pp
+++ modules/scap/manifests/scripts.pp
@@ -38,6 +38,12 @@ class scap::scripts {
         mode    => '0555',
         content => template('scap/foreachwikiindblist.erb'),
     }
+    file { '/usr/local/bin/expanddblist':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        source => 'puppet:///modules/scap/expanddblist',
+    }
     file { '/usr/local/bin/mwscript':
         owner   => 'root',
         group   => 'root',
