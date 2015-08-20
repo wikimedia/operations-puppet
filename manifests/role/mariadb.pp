9912534802eb8b7e7cf84c2d9d27a545bb7708b2
jcrespo <jcrespo@wikimedia.org>
2015-08-20 18:15:11 +0200

    Add extra grants for labswiki from tin
    A wikiadmin user will be created so that it can query from tin.
    
    I do not like the current grants model, but I will continue
    using the current standard until I can refactor it.
    
    Bug: T98682
    Change-Id: I1055a8a388a088012b57637995e20b3ac7fd102c

--- manifests/role/mariadb.pp
+++ manifests/role/mariadb.pp
@@ -393,6 +393,20 @@ class role::mariadb::grants::core {
     }
 }
 
+class role::mariadb::grants::wikitech {
+
+    include passwords::misc::scripts
+    $wikiadmin_pass = $passwords::misc::scripts::wikiadmin_pass
+
+    file { '/etc/mysql/grants-wikitech.sql':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0400',
+        content => template('mariadb/grants-wikitech.sql.erb'),
+    }
+}
+
 class role::mariadb::core(
     $shard
     ) {
