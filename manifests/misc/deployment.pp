538200a02e547c83ce3d77c90b6ecbe903e6e2fc
Faidon Liambotis <faidon@wikimedia.org>
2015-08-20 15:27:24 +0200

    Remove/ensure=> absent *_pass scripts
    Those are not used anywhere as far as I know, they seem unmaintained
    (webshop pass?) and they are generally a bad way to handout passwords in
    general from a security perspective.
    
    Ensure them absent for now, the removal of the class
    (misc::deployment::passwordscripts) will happen in a subsequent commit.
    
    Change-Id: I0a206318cd83d8cc7b71f80f7db4cde435ebb940

--- manifests/misc/deployment.pp
+++ manifests/misc/deployment.pp
@@ -3,63 +3,17 @@
 # deployment hosts
 
 class misc::deployment::passwordscripts {
-    include passwords::misc::scripts
-    $cachemgr_pass = $passwords::misc::scripts::cachemgr_pass
-    $mysql_root_pass = $passwords::misc::scripts::mysql_root_pass
-    $nagios_sql_pass = $passwords::misc::scripts::nagios_sql_pass
-    $webshop_pass = $passwords::misc::scripts::webshop_pass
-    $wikiadmin_pass = $passwords::misc::scripts::wikiadmin_pass
-    $wikiuser2_pass = $passwords::misc::scripts::wikiuser2_pass
-    $wikiuser_pass = $passwords::misc::scripts::wikiuser_pass
-    $wikiuser_pass_nagios = $passwords::misc::scripts::wikiuser_pass_nagios
-    $wikiuser_pass_real = $passwords::misc::scripts::wikiuser_pass_real
-
-    file {
-        '/usr/local/bin/cachemgr_pass':
-            owner   => 'root',
-            group   => 'wikidev',
-            mode    => '0550',
-            content => template('misc/passwordScripts/cachemgr_pass.erb');
-        '/usr/local/bin/mysql_root_pass':
-            owner   => 'root',
-            group   => 'wikidev',
-            mode    => '0550',
-            content => template('misc/passwordScripts/mysql_root_pass.erb');
-        '/usr/local/bin/nagios_sql_pass':
-            owner   => 'root',
-            group   => 'wikidev',
-            mode    => '0550',
-            content => template('misc/passwordScripts/nagios_sql_pass.erb');
-        '/usr/local/bin/webshop_pass':
-            owner   => 'root',
-            group   => 'wikidev',
-            mode    => '0550',
-            content => template('misc/passwordScripts/webshop_pass.erb');
-        '/usr/local/bin/wikiadmin_pass':
-            owner   => 'root',
-            group   => 'wikidev',
-            mode    => '0550',
-            content => template('misc/passwordScripts/wikiadmin_pass.erb');
-        '/usr/local/bin/wikiuser2_pass':
-            owner   => 'root',
-            group   => 'wikidev',
-            mode    => '0550',
-            content => template('misc/passwordScripts/wikiuser2_pass.erb');
-        '/usr/local/bin/wikiuser_pass':
-            owner   => 'root',
-            group   => 'wikidev',
-            mode    => '0550',
-            content => template('misc/passwordScripts/wikiuser_pass.erb');
-        '/usr/local/bin/wikiuser_pass_nagios':
-            owner   => 'root',
-            group   => 'wikidev',
-            mode    => '0550',
-            content => template('misc/passwordScripts/wikiuser_pass_nagios.erb');
-        '/usr/local/bin/wikiuser_pass_real':
-            owner   => 'root',
-            group   => 'wikidev',
-            mode    => '0550',
-            content => template('misc/passwordScripts/wikiuser_pass_real.erb');
+    file { [
+        '/usr/local/bin/cachemgr_pass',
+        '/usr/local/bin/mysql_root_pass',
+        '/usr/local/bin/nagios_sql_pass',
+        '/usr/local/bin/webshop_pass',
+        '/usr/local/bin/wikiadmin_pass',
+        '/usr/local/bin/wikiuser2_pass',
+        '/usr/local/bin/wikiuser_pass',
+        '/usr/local/bin/wikiuser_pass_nagios',
+        '/usr/local/bin/wikiuser_pass_real',
+        ]:
+            ensure => absent,
     }
 }
-
