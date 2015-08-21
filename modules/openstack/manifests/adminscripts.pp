c55b5ad265cbef31235d28d22fdd114a0aedb3ba
Andrew Bogott <abogott@wikimedia.org>
2015-08-21 21:35:05 +0000

    Revert "Add root keypair connecting labvirt hosts."
    This doesn't work -- pub keys have to be in /etc/ssh.
    
    This reverts commit 6dc1195d3f2ede2f11492fcba251a87c2452fb40.
    
    Change-Id: I86af18822d141e224ca376c37036a53d942d5823

--- modules/openstack/manifests/adminscripts.pp
+++ modules/openstack/manifests/adminscripts.pp
@@ -96,10 +96,4 @@ class openstack::adminscripts(
         group   => 'nova',
         mode    => '0600',
     }
-    file { '/root/.ssh/novamigrate':
-        content => secret('ssh/nova/novamigrate'),
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0600',
-    }
 }
