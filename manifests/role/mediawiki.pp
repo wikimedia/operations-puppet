8db374961e1a1fa7a4cc2c0397927ce087f1490b
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-08-25 19:09:07 +0200

    deployment::server: re-puppetize nutcracker config
    It got lost and unmanaged sometimes in the past, that is obviously wrong
    and causing issues and slowness
    
    Bug: T103198
    Change-Id: Ia950a56726bd6eb42ee4867c7672903693e71788

--- manifests/role/mediawiki.pp
+++ manifests/role/mediawiki.pp
@@ -14,55 +14,8 @@ class role::mediawiki::common {
     include ::standard
     include ::geoip
     include ::mediawiki
-    include ::nutcracker::monitoring
+    include ::mediawiki::nutcracker
     include ::tmpreaper
-    include ::passwords::redis
-
-    $nutcracker_pools = {
-        'memcached'     => {
-            auto_eject_hosts     => true,
-            distribution         => 'ketama',
-            hash                 => 'md5',
-            listen               => '127.0.0.1:11212',
-            preconnect           => true,
-            server_connections   => 1,
-            server_failure_limit => 3,
-            server_retry_timeout => to_milliseconds('30s'),
-            timeout              => 250,
-            servers              => hiera('mediawiki_memcached_servers'),
-        },
-        'mc-unix'       => {
-            auto_eject_hosts     => true,
-            distribution         => 'ketama',
-            hash                 => 'md5',
-            listen               => '/var/run/nutcracker/nutcracker.sock 0666',
-            preconnect           => true,
-            server_connections   => 1,
-            server_failure_limit => 3,
-            server_retry_timeout => to_milliseconds('30s'),
-            timeout              => 250,
-            servers              => hiera('mediawiki_memcached_servers'),
-        },
-        'session-redis' => {
-            auto_eject_hosts     => true,
-            distribution         => 'ketama',
-            redis                => true,
-            redis_auth           => $passwords::redis::main_password,
-            hash                 => 'md5',
-            listen               => '127.0.0.1:6380',
-            preconnect           => true,
-            server_connections   => 1,
-            server_failure_limit => 3,
-            server_retry_timeout => to_milliseconds('30s'),
-            timeout              => 1000,
-            servers              => hiera('mediawiki_session_redis_servers'),
-        },
-    }
-
-    class { '::nutcracker':
-        mbuf_size => '64k',
-        pools     => $nutcracker_pools,
-    }
 
     # Nutcracker/Redis only listens on localhost, but exempt it from connection tracking
     ferm::rule { 'skip_nutcracker_redis_conntrack_out':
