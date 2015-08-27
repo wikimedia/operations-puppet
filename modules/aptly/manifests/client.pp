63baf8d5dce17a0d25912ace11978fac0e845403
YuviPanda <yuvipanda@gmail.com>
2015-08-27 11:19:11 +0200

    aptly: s/url/url/
    Change-Id: Ib21b08129bc4695bcc3fe0d658e0f9433e19cbd6

--- modules/aptly/manifests/client.pp
+++ modules/aptly/manifests/client.pp
@@ -2,7 +2,7 @@ class aptly::client(
     $servername,
 ) {
     apt::repository { 'project-aptly':
-        url        => "http://${servername}/repo",
+        uri        => "http://${servername}/repo",
         dist       => $::lsbdistcodename,
         components => $::labsproject,
         source     => true,
