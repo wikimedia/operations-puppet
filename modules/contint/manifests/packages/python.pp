62c6f66c810c94fa7233a0d25d32ff09090f985c
Antoine Musso <hashar@free.fr>
2015-08-27 13:42:20 +0200

    contint: upgrade setuptools from pypi
    On Precise, setuptools is quite old and does not support dist-info
    format.  Install it via pypi.
    
    Bug: T110506
    Change-Id: I1f8f841db48751c9220f362a8e5a9bce774a9019

--- modules/contint/manifests/packages/python.pp
+++ modules/contint/manifests/packages/python.pp
@@ -18,6 +18,11 @@ class contint::packages::python {
         provider => 'pip',
         require  => Package['python-pip'],
     }
+    package { 'setuptools':
+        ensure   => present,
+        provider => 'pip',
+        require  => Package['python-pip'],
+    }
 
     # Python 3
     require_package(
