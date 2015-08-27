5998d024270d7c113c5d8938f0d73b4ddaf1b26d
YuviPanda <yuvipanda@gmail.com>
2015-08-27 13:31:27 +0200

    aptly: Get rid of 'main' in aptly repo name
    Just cluttery
    
    Change-Id: I48329e28f0abf5a345b2eadd3bb58f058bf2fd49

--- modules/aptly/manifests/repo.pp
+++ modules/aptly/manifests/repo.pp
@@ -5,10 +5,9 @@ define aptly::repo (
 ) {
     require ::aptly
 
-    $repo_name = "${title}-${component}"
     exec { "create-aptly-repo-${title}":
-        command => "/usr/bin/aptly repo create -component=${component} -distribution=${distribution} ${repo_name}",
-        unless  => "/usr/bin/aptly repo show ${repo_name} > /dev/null",
+        command => "/usr/bin/aptly repo create -component=${component} -distribution=${distribution} ${title}",
+        unless  => "/usr/bin/aptly repo show ${title} > /dev/null",
         user    => 'root',
     }
 
@@ -16,8 +15,8 @@ define aptly::repo (
         # Pubish the repo directly, without snapshots
         # This isn't reccomended by aptly for production uses, but is perfect for labs :D
         exec { "publish-aptly-repo-${title}":
-            command => "/usr/bin/aptly -architectures=amd64,all publish -skip-signing repo ${repo_name}",
-            unless  => "/usr/bin/aptly publish list | /bin/grep -F '[${repo_name}]' > /dev/null",
+            command => "/usr/bin/aptly -architectures=amd64,all publish -skip-signing repo ${title}",
+            unless  => "/usr/bin/aptly publish list | /bin/grep -F '[${title}]' > /dev/null",
             user    => 'root',
             require => Exec["create-aptly-repo-${title}"],
         }
