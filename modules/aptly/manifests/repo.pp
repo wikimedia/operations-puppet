cb400738da734a9029f65512854b9343bd9df388
YuviPanda <yuvipanda@gmail.com>
2015-08-27 13:03:08 +0200

    aptly: Specify arch explicitly for publishing
    Change-Id: If2fa2c50bd5ec036155e5bed67888e1673c62edb

--- modules/aptly/manifests/repo.pp
+++ modules/aptly/manifests/repo.pp
@@ -16,7 +16,7 @@ define aptly::repo (
         # Pubish the repo directly, without snapshots
         # This isn't reccomended by aptly for production uses, but is perfect for labs :D
         exec { "publish-aptly-repo-${title}":
-            command => "/usr/bin/aptly publish -skip-signing repo ${repo_name}",
+            command => "/usr/bin/aptly -architectures=amd64,all publish -skip-signing repo ${repo_name}",
             unless  => "/usr/bin/aptly publish list | /bin/grep -F '[${repo_name}]' > /dev/null",
             user    => 'root',
         }
