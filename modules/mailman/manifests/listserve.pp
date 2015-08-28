f7ea563f7e284b3f63bd6b9fa8c18a8fc5d7ba44
dzahn <dzahn@wikimedia.org>
2015-08-27 19:30:50 -0700

    mailman: remove gate_news debconf option
    "gate_news" is mailman's newsgroup to mail gateway.
    
    This option does not exist anymore in the jessie version of the
    package.
    
    @fermium:~# echo get mailman/gate_news | debconf-communicate
    10 mailman/gate_news doesn't exist
    
    @sodium:~# echo get mailman/gate_news | debconf-communicate
    0 false
    
    It causes puppet errors if we still try to set it:
    
    Mailman::Listserve/Exec[dpkg-reconfigure mailman]:
    Dependency Exec[debconf-communicate set mailman/gate_news] has failures: true
    
    Bug:T109925
    Change-Id: Ib01baf00aca8c978e34b9553e29d1ef1bffee702

--- modules/mailman/manifests/listserve.pp
+++ modules/mailman/manifests/listserve.pp
@@ -13,11 +13,6 @@ class mailman::listserve {
     # Install as many languages as possible
     include locales::extended
 
-    debconf::set { 'mailman/gate_news':
-        value  => 'false',
-        notify => Exec['dpkg-reconfigure mailman'],
-    }
-
     debconf::set { 'mailman/site_languages':
         value  => 'ar, ast, ca, cs, da, de, en, es, et, eu, fi, fr, gl, he, hr, hu, ia, it, ja, ko, lt, nl, no, pl, pt, pt_BR, ro, ru, sk, sl, sr, sv, tr, uk, vi, zh_CN, zh_TW',
         notify => Exec['dpkg-reconfigure mailman'],
