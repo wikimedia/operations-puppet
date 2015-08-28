3e80371f186565346337a6dfc9822a934b29aa80
dzahn <dzahn@wikimedia.org>
2015-08-27 17:51:33 -0700

    mailman: fix I/O monitoring on virtual machine
    On sodium we did not have to specify a device name,
    on fermium we have to, because it's /dev/vda, or:
    
    ERROR: Device incorrectly specified
    
    Let's try and use "is_virtual" here to know if we
    are on a VM or not and then set the device to be checked
    based on that.
    
    https://docs.puppetlabs.com/facter/1.6/core_facts.html#isvirtual
    
    Bug:T109925
    Change-Id: I6c453046b00ca1b12db354f1bcd938d01029948c

--- manifests/role/lists.pp
+++ manifests/role/lists.pp
@@ -125,10 +125,16 @@ class role::lists {
         source => 'puppet:///files/icinga/check_iostat',
     }
 
+    $iostat_device = $is_virtual ? {
+        'true'    => 'vda',
+        'false'   => 'sda',
+        default   => 'sda',
+    }
+
     # values chosen based on i/o averages for sodium
     nrpe::monitor_service { 'mailman_iostat':
         description   => 'mailman I/O stats',
-        nrpe_command  => '/usr/local/lib/nagios/plugins/check_iostat -i -w 250,350,300,14000,7500 -c 500,400,600,28000,11000',
+        nrpe_command  => "/usr/local/lib/nagios/plugins/check_iostat -i -w 250,350,300,14000,7500 -c 500,400,600,28000,11000 -d ${iostat_device}",
         timeout       => '30',
     }
 
