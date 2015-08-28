8dcfbee91bdb1f508a1d8123e49f77c4d6189819
Tim Landscheidt <tim@tim-landscheidt.de>
2015-08-28 23:14:42 +0000

    Tools: Replace reference to tools. in motd-tips.sh
    Bug: T87387
    Change-Id: I6a2544228eb7451058eb7cf1f59142d3d27f6310

--- modules/toollabs/files/motd-tips.sh
+++ modules/toollabs/files/motd-tips.sh
@@ -15,18 +15,22 @@ if [ ! -f /data/project/.system/tips ]; then
     return
 fi
 
-# Don't display tips for root and tool accounts.
-if [ $UID -eq 0 ] || [ "${USER:0:6}" = "tools." ]; then
-    return
-fi
-
 # Check shell level.
 if [ "$SHLVL" -gt 1 ]; then
     return
 fi
 
+# Read project name.
+labsproject="$(cat /etc/wmflabs-project)"
+
+# Don't display tips for root and tool accounts.
+if [ $UID -eq 0 ] || [ "${USER:0:$((${#labsproject} + 1))}" = "$labsproject." ]; then
+    unset labsproject
+    return
+fi
+
 # If user is sysadmin print the sysadmin motd instead.
-if groups | fgrep -qw tools.admin; then
+if groups | fgrep -qw -- "$labsproject.admin"; then
     if [ -f /etc/motd.sysadmin ]; then
         echo
         echo
@@ -34,7 +38,10 @@ if groups | fgrep -qw tools.admin; then
     fi
 
     # We don't want to show tips to sysadmins.
+    unset labsproject
     return
 fi
+
+unset labsproject
 
 echo -e "\n\033[0;1;4mDid you know\033[0m that `shuf -n 1 /data/project/.system/tips`\n"
