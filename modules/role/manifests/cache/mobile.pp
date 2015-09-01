8b163e135c81a817f386f7a91001274ae7167911
Brandon Black <bblack@wikimedia.org>
2015-09-01 15:42:27 +0000

    cache_mobile: limit 4xx to 1m like text
    Bug: T109286
    Change-Id: Ic7d33a86c4379ee16e731d857fb4871417549d37

--- modules/role/manifests/cache/mobile.pp
+++ modules/role/manifests/cache/mobile.pp
@@ -80,6 +80,7 @@ class role::cache::mobile {
     }
 
     $common_vcl_config = {
+        'cache4xx'           => '1m',
         'purge_host_regex'   => $::role::cache::base::purge_host_not_upload_re,
         'static_host'        => $::role::cache::base::static_host,
         'bits_domain'        => $::role::cache::base::bits_domain,
