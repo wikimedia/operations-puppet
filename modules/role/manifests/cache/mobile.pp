2625160fad9ea36ad913c0581b9a49db1b417ec5
Brandon Black <bblack@wikimedia.org>
2015-09-01 05:38:28 +0000

    add zero updater to text.pp, align position
    Bug: T109286
    Change-Id: I7de633b3a900d53fde6c60348d41e75a3cabe497

--- modules/role/manifests/cache/mobile.pp
+++ modules/role/manifests/cache/mobile.pp
@@ -22,6 +22,11 @@ class role::cache::mobile {
     require geoip
     require geoip::dev # for VCL compilation using libGeoIP
 
+    class { 'varnish::zero_update':
+        site         => $::role::cache::base::zero_site,
+        auth_content => secret('misc/zerofetcher.auth'),
+    }
+
     $varnish_be_directors = {
         'one' => {
             'backend'          => {
@@ -92,11 +97,6 @@ class role::cache::mobile {
         'enable_geoiplookup' => true,
         'https_redirects'    => true,
     })
-
-    class { 'varnish::zero_update':
-        site         => $::role::cache::base::zero_site,
-        auth_content => secret('misc/zerofetcher.auth'),
-    }
 
     varnish::instance { 'mobile-backend':
         name               => '',
