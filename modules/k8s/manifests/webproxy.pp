e404518bb1007c46f3e052c706901fea53358e85
YuviPanda <yuvipanda@gmail.com>
2015-09-03 19:50:12 +0200

    k8s: Run kube-proxy on the webproxy hosts
    Change-Id: I52d61d2c6c32f3bdf076e0314ac2fd43ce377a8b

--- modules/k8s/manifests/webproxy.pp
+++ modules/k8s/manifests/webproxy.pp
@@ -2,6 +2,7 @@ class k8s::webproxy(
     $dns_ip='192.168.0.100',
 ) {
     require k8s::flannel
+    require k8s::proxy
 
     nginx::site { 'proxy':
         content => template('k8s/webproxy.conf.erb'),
