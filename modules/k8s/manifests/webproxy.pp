c845f08a4b0c7e828abd09e7a2b989a755b533ba
YuviPanda <yuvipanda@gmail.com>
2015-09-03 19:29:02 +0200

    k8s: s/contents/content/
    Change-Id: I91604445d2834d06af0a730832e188a4694c12fa

--- modules/k8s/manifests/webproxy.pp
+++ modules/k8s/manifests/webproxy.pp
@@ -1,10 +1,9 @@
 class k8s::webproxy(
     $dns_ip='192.168.0.100',
-)
-{
+) {
     require k8s::flannel
 
     nginx::site { 'proxy':
-        contents => template('k8s/webproxy.conf.erb'),
+        content => template('k8s/webproxy.conf.erb'),
     }
 }
