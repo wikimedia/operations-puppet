fec5bca3f3352b89619c1ad484ee5f59dfd7e617
Peter Hedenskog <phedenskog@wikimedia.org>
2015-09-04 16:26:11 +0200

    Collect missing Navigation Timing metrics
    Added missing nav timing metrics: unloadEventStart, unloadEventEnd
    redirectStart, redirectEnd, domainLookupStart, domainLookupEnd,
    domLoading, domContentLoadedEventStart, domContentLoadedEventEnd
    and changed mediaWikiLoadComplete to mediaWikiLoadStart and
    mediaWikiLoadEnd.
    
    Depends on 5d4824f41e in the NavigationTiming extension.
    
    Bug: T109756
    Change-Id: I556ba997a68da7eea142c44dfd504a05aa130ca5

--- modules/webperf/files/navtiming.py
+++ modules/webperf/files/navtiming.py
@@ -73,7 +73,8 @@ def handle_navigation_timing(meta):
     for metric, marker in (
         ('dnsLookup', 'dnsLookup'),
         ('loading', 'loadEventStart'),
-        ('mediaWikiLoadComplete', 'mediaWikiLoadComplete'),
+        ('mediaWikiLoadStart', 'mediaWikiLoadStart'),
+        ('mediaWikiLoadEnd', 'mediaWikiLoadEnd'),
         ('redirecting', 'redirecting'),
         ('sending', 'fetchStart'),
         ('totalPageLoadTime', 'loadEventEnd'),
