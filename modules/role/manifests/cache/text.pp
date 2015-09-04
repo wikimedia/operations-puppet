767abce74c7944dd8a7989ec95a462845f9fdcec
Brandon Black <bblack@wikimedia.org>
2015-09-04 19:38:18 +0000

    Disable zerofetcher on text caches T111045
    Change-Id: I04a90d24283b77c1d7bb265de7f136944b2a9f37

--- modules/role/manifests/cache/text.pp
+++ modules/role/manifests/cache/text.pp
@@ -22,10 +22,11 @@ class role::cache::text {
     require geoip
     require geoip::dev # for VCL compilation using libGeoIP
 
-    class { 'varnish::zero_update':
-        site         => $::role::cache::base::zero_site,
-        auth_content => secret('misc/zerofetcher.auth'),
-    }
+# Commented out - not in use on text yet, and causing rate issues: T111045
+#    class { 'varnish::zero_update':
+#        site         => $::role::cache::base::zero_site,
+#        auth_content => secret('misc/zerofetcher.auth'),
+#    }
 
     $varnish_be_directors = {
         'one' => {
