374cb3203ab155f0a70cff6f1edb2062737dea68
dzahn <dzahn@wikimedia.org>
2015-09-04 18:41:01 -0700

    wikistats: crons for db backup
    Create and keep some db backups, but also
    make sure older ones get deleted again.
    
    Change-Id: I0636faff52b778f62d5bd1ea80da6740fc9369a9

--- modules/wikistats/manifests/db.pp
+++ modules/wikistats/manifests/db.pp
@@ -4,4 +4,24 @@ class wikistats::db {
     package { [ 'mariadb-server', 'php5-mysql']:
         ensure => present,
     }
+
+    $backupdir = '/root/wsbackup'
+
+    # db backup
+    cron { 'mysql-dump-wikistats':
+        ensure  => 'present',
+        command => "/usr/bin/mysqldump -u root -p$ wikistats > ${wikistats::db::backupdir}/wikistats_db_${date}.sql && gzip ${backupdir}/wikistats_db_${date}.sql",
+        user    => 'root',
+        hour    => '0',
+        minute  => '15',
+    }
+
+    # don't run out of disk
+    cron {'mysql-dump-wikistats-clean':
+        ensure  => 'present',
+        command => "find ${backupdir} -name \"*.sql.gz\" -mtime +7 -exec rm {} \;",
+        user    => 'root',
+        hour    => '23',
+        minute  => '23',
+    }
 }
