dcfbd2a1611f8a8ce5c15d9019962458fdbd3481
dzahn <dzahn@wikimedia.org>
2015-09-04 18:41:01 -0700

    wikistats: crons for db backup (WIP)
    Change-Id: I0636faff52b778f62d5bd1ea80da6740fc9369a9

--- modules/wikistats/manifests/db.pp
+++ modules/wikistats/manifests/db.pp
@@ -4,4 +4,22 @@ class wikistats::db {
     package { [ 'mariadb-server', 'php5-mysql']:
         ensure => present,
     }
+
+    # db backup
+    cron { 'mysql-dump-wikistats':
+        ensure  => 'present',
+        command => "/usr/bin/mysqldump -u wikistatsuser -p${db_pass} wikistats > ${backup_dir}/wikistats_db_${date}.sql",
+        user    => 'wikistatsusers',
+        hour    => '0',
+        minute  => '15',
+    }
+
+    # don't run out of disk
+    cron {'mysql-dump-wikistats-clean':
+        ensure  => 'present',
+        command => "find ${backupdir} -name \"*.sql.gz\" -mtime +7 -exec rm {} \;",
+        user    => 'wikistatsuser',
+        hour    => '23',
+        minute  => '23',
+    }
 }
