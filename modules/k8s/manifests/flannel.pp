4f202bb4e7378e7fb4617819b1df2b2672eefff2
YuviPanda <yuvipanda@gmail.com>
2015-09-07 19:38:58 +0200

    k8s: Use debian package for flannel
    Is in gerrit, at operations/debs/flannel
    
    Package does not include a systemd unit
    
    Change-Id: I8ac3c05d5194c7eab618f5964522aed17bc1d7c9

--- modules/k8s/manifests/flannel.pp
+++ modules/k8s/manifests/flannel.pp
@@ -1,15 +1,7 @@
 class k8s::flannel(
     $etcd_endpoints,
-    $etcd_prefix='/coreos.com/network',
 ) {
-    # Ugly ugly haaaacccckkkkkk
-    # This will eventually be packaged
-    file { '/usr/local/bin/flanneld':
-        source => '/data/scratch/k8s/flannel/bin/flanneld',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
-    }
+    require_package('flannel')
 
     base::service_unit { 'flannel':
         systemd => true,
