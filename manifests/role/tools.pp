d0103025aefbe7b53134f95d0e59059f3025023f
YuviPanda <yuvipanda@gmail.com>
2015-09-07 20:34:17 +0200

    tools: Do not poke holes in firewall for etcd
    Since k8s msater and etcd will be on the same host, at least
    to start with.
    
    Change-Id: I5d91157c518d5d8bc4411665dd95ec5cebc21b42

--- manifests/role/tools.pp
+++ manifests/role/tools.pp
@@ -1,6 +1,10 @@
 # Roles for Kubernetes and co on Tool Labs
 class role::toollabs::etcd {
+    # To deny access to etcd - atm the kubernetes master
+    # and etcd will be on the same host, so ok to just deny
+    # access to everyone else
+    include base::firewall
     include toollabs::infrastructure
 
-    include role::etcd
+    include etcd
 }
