0ad966217cbe8d7ed644478521947f542fe53445
YuviPanda <yuvipanda@gmail.com>
2015-09-08 10:38:28 +0200

    aptly: Force remove multiarch support in precise
    Bug: T111760
    Change-Id: I5c9fac2224affae4edc47fb698ba30de465679e8

--- modules/aptly/manifests/client.pp
+++ modules/aptly/manifests/client.pp
@@ -2,6 +2,15 @@ class aptly::client(
     $servername,
     $source=false
 ) {
+    if os_version('ubuntu <= precise') {
+        # Remove multiarch support in precise aptly clients.
+        # Can be removed when T111760 is fixed
+        file { '/etc/dpkg/dpkg.cfg.d/multiarch':
+            ensure => absent,
+            notify => Exect['apt-get update'],
+        }
+    }
+
     apt::repository { 'project-aptly':
         uri        => "http://${servername}/repo",
         dist       => "${::lsbdistcodename}-${::labsproject}",
