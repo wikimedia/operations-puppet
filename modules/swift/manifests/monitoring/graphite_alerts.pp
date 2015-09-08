66ee80f0dfa20b8c1c18bde72ed71949d2ce918f
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-09-08 11:52:23 +0100

    swift: graphite_alerts should be a define
    Change-Id: I7b84de4e9b6c751ca7c0679b7697ad7f30fc66cd

--- modules/swift/manifests/monitoring/graphite_alerts.pp
+++ modules/swift/manifests/monitoring/graphite_alerts.pp
@@ -1,7 +1,9 @@
-class swift::monitoring::graphite_alerts {
-    monitoring::graphite_threshold { "swift_${title}_dispersion_object":
-        description     => "swift ${title} object availability",
-        metric          => "keepLastValue(swift.${title}.dispersion.object.pct_found)",
+define swift::monitoring::graphite_alerts (
+    $cluster = $title,
+) {
+    monitoring::graphite_threshold { "swift_${cluster}_dispersion_object":
+        description     => "swift ${cluster} object availability",
+        metric          => "keepLastValue(swift.${cluster}.dispersion.object.pct_found)",
         from            => '1hours',
         warning         => 95,
         critical        => 90,
@@ -9,9 +11,9 @@ class swift::monitoring::graphite_alerts {
         nagios_critical => false,
     }
 
-    monitoring::graphite_threshold { "swift_${title}_dispersion_container}":
-        description     => "swift ${title} container availability",
-        metric          => "keepLastValue(swift.${title}.dispersion.container.pct_found)",
+    monitoring::graphite_threshold { "swift_${cluster}_dispersion_container}":
+        description     => "swift ${cluster} container availability",
+        metric          => "keepLastValue(swift.${cluster}.dispersion.container.pct_found)",
         from            => '30min',
         warning         => 92,
         critical        => 88,
