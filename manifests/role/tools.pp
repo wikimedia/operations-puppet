0108c0c3f7f4efc5a041241bf6a6d4718c80c2ff
YuviPanda <yuvipanda@gmail.com>
2015-09-08 15:13:39 +0200

    tools: Add k8s tools master role
    Change-Id: I57fe8ce0db97b04cef4b165fbb40c1c46c4e7d14

--- manifests/role/tools.pp
+++ manifests/role/tools.pp
@@ -8,3 +8,29 @@ class role::toollabs::etcd {
 
     include etcd
 }
+
+class role::toollabs::k8s::master {
+    # This requires that etcd is on the same host
+    # And is not HA. Will re-evaluate when it is HA
+
+    include base::firewall
+    include toollabs::infrastructure
+
+    include role::toollabs::etcd
+
+    $master_host = hiera('k8s_master', $::fqdn)
+    $etcd_url = join(prefix(suffix(hiera('etcd_hosts', [$::fqdn]), ':2379'), 'https://')',')
+
+    class { 'k8s::apiserver':
+        master_host => $master_host,
+        etcd_servers => $etcd_url,
+
+    }
+    class { 'k8s::scheduler':
+        master_host => $master_host,
+    }
+
+    class { 'k8s::controller':
+        master_host => $master_host,
+    }
+}
