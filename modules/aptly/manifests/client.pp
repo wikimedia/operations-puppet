4e2ea1086aea751f9434120f689308ff839f016c
YuviPanda <yuvipanda@gmail.com>
2015-09-08 10:03:51 +0200

    aptly: Make client default to not having source packages
    Change-Id: I55ba2f9b8f31317fd5960dd36b677968f28a2897

--- modules/aptly/manifests/client.pp
+++ modules/aptly/manifests/client.pp
@@ -1,10 +1,11 @@
 class aptly::client(
     $servername,
+    $source=false
 ) {
     apt::repository { 'project-aptly':
         uri        => "http://${servername}/repo",
         dist       => "${::lsbdistcodename}-${::labsproject}",
         components => 'main',
-        source     => true,
+        source     => $source,
     }
 }
