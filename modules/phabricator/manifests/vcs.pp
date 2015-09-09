56d272a19f2109d014f53b7df85041de397584e1
Mukunda Modell <mmodell@wikimedia.org>
2015-09-09 08:04:28 -0500

    SSH repo hosting support for phabricator.
    Just adds some basic prerequisites for phabricator to support hosting
    git repos over ssh.
    
    Bug: T128
    
    Change-Id: Ic4460020204c7633be85b7fc00c8e336d5a314f5

--- modules/phabricator/manifests/vcs.pp
+++ modules/phabricator/manifests/vcs.pp
@@ -5,7 +5,13 @@
 
 class phabricator::vcs (
     $settings = {},
+    $ssh_pid = '/var/run/sshd/phab.pid',
+    $ssh_port = '22'
 ) {
+
+    $ssh_user = $settings['diffusion.ssh-user']
+    $phd_user = $settings['phd.user']
+    $ssh_hook_path = '/usr/libexec/phabricator-ssh-hook.sh'
 
     # git-http-backend needs to be in $PATH
     file { '/usr/local/bin/git-http-backend':
@@ -14,26 +20,39 @@ class phabricator::vcs (
         require => Package['git-core'],
     }
 
-
-    user { $settings['diffusion.ssh-user']:
-        home   => "/var/lib/${settings['diffusion.ssh-user']}",
+    user { $ssh_user:
+        home   => "/var/lib/${ssh_user}",
         shell  => '/bin/sh',
         system => true,
+    }
+
+    file { $ssh_hook_path:
+        content => template('/phabricator/phabricator-ssh-hook.sh.erb'),
+        mode    => '0755',
+        owner   => 'root',
+        group   => 'root',
+    }
+
+    file { '/etc/ssh/sshd_config.phabricator':
+        content => template('/phabricator/sshd_config.phabricator.erb'),
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
     }
 
     # phd.user owns repo resources and both vcs and web user
     # must sudo to phd to for repo work.
 
-    sudo::user { $settings['diffusion.ssh-user']:
+    sudo::user { $ssh_user:
         privileges => [
-            "ALL=(${settings['phd.user']}) SETENV: NOPASSWD: /usr/bin/git-upload-pack, /usr/bin/git-receive-pack, /usr/bin/svnserve",
+            "ALL=(${phd_user}) SETENV: NOPASSWD: /usr/bin/git-upload-pack, /usr/bin/git-receive-pack, /usr/bin/svnserve",
         ],
-        require => User[$settings['diffusion.ssh-user']],
+        require => User[$ssh_user],
     }
 
     sudo::user { 'www-data':
         privileges => [
-            "ALL=(${settings['phd.user']}) SETENV: NOPASSWD: /usr/local/bin/git-http-backend",
+            "ALL=(${phd_user}) SETENV: NOPASSWD: /usr/local/bin/git-http-backend",
         ],
         require    => File['/usr/local/bin/git-http-backend'],
     }
