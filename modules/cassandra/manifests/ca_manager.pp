6eae3163be71c3f265aa211292afa1563daa8f2e
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-09-10 15:05:33 +0100

    cassandra: new class ca_manager
    Also rename cassandra-ca-mgr into cassandra-ca-manager, introduce a related
    class to install it on the node hosting private.git
    
    The CA manager will generate a CA, plus all related public/private
    material and the related keystores to be used by Cassandra. The list of
    certificates to generate is supplied to the manager by a yaml manifest, both
    the manifest and the resulting material are stored in private.git.
    
    Bug: T108953
    Change-Id: I33800945788e5f9a81def508b9456d259ad608da

---
+++ modules/cassandra/manifests/ca_manager.pp
@@ -0,0 +1,24 @@
+# == Class: cassandra::ca_manager
+#
+# Install Cassandra CA manager.
+#
+# The manager will accept a manifest file as input and generate a CA plus all
+# related certificates to be installed on cassandra nodes.
+# Note: per-cluster manifests and secrets live in private.git.
+#
+# === Usage
+# class { '::cassandra::ca_manager': }
+
+class cassandra::ca_manager {
+    file { '/usr/local/bin/cassandra-ca-manager':
+        source  => 'puppet:///modules/cassandra-ca-manager',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+    }
+
+    # keytool dependency
+    package { 'default-jre':
+        ensure   => present,
+    }
+}
