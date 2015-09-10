0d833f96ef06ad5bfe8e68ea49bb56b5e86eebac
dzahn <dzahn@wikimedia.org>
2015-09-09 18:25:13 -0700

    mailman: remove import scripts
    We won't use these anymore. Instead we are rsyncing
    directly in place and adjusted the scripts on the source side
    as well as the rsyncd config.
    
    Bug:T110131
    Change-Id: I6f0b30c6330d1f08294e9106ae22cd0f72fef8ce

--- modules/mailman/manifests/scripts.pp
+++ modules/mailman/manifests/scripts.pp
@@ -9,22 +9,6 @@ class mailman::scripts {
         source => 'puppet:///modules/mailman/scripts/remove_from_private.sh'
     }
 
-    file { '/var/lib/mailman/bin/import_list.sh':
-        ensure => 'present',
-        owner  => 'root',
-        group  => 'list',
-        mode   => '0550',
-        source => 'puppet:///modules/mailman/scripts/import_list.sh'
-    }
-
-    file { '/var/lib/mailman/bin/import_all_lists.sh':
-        ensure => 'present',
-        owner  => 'root',
-        group  => 'list',
-        mode   => '0550',
-        source => 'puppet:///modules/mailman/scripts/import_all_lists.sh'
-    }
-
     file { '/var/lib/mailman/bin/disable_list.sh':
         ensure => 'present',
         owner  => 'root',
@@ -49,11 +33,4 @@ class mailman::scripts {
         source => 'puppet:///modules/mailman/scripts/rsync_exim.sh'
     }
 
-    file { '/var/lib/mailman/bin/import_exim.sh':
-        ensure => 'present',
-        owner  => 'root',
-        group  => 'list',
-        mode   => '0550',
-        source => 'puppet:///modules/mailman/scripts/import_exim.sh'
-    }
 }
