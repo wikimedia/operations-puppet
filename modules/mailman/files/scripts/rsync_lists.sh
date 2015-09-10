5320d8be0a9197200fa4960ed4e885e4104afd2c
dzahn <dzahn@wikimedia.org>
2015-09-10 12:27:22 -0700

    mailman: don't use "c" option with rsync
    We don't want to use this option anymore as it would
    make things slower and was a hack for messed-up timestamps.
    
    Bug:TT110138
    Change-Id: Ib3ac11ced24bcdb44f0c055b94fa53965405d88b

--- modules/mailman/files/scripts/rsync_lists.sh
+++ modules/mailman/files/scripts/rsync_lists.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # rsync lists - T108071, T109399#1561586
 
-/usr/bin/rsync -avzc --delete /var/lib/mailman/lists/ rsync://fermium.wikimedia.org/lists
+/usr/bin/rsync -avz --delete /var/lib/mailman/lists/ rsync://fermium.wikimedia.org/lists
 
-/usr/bin/rsync -avzc --delete /var/lib/mailman/archives/ rsync://fermium.wikimedia.org/archives
+/usr/bin/rsync -avz --delete /var/lib/mailman/archives/ rsync://fermium.wikimedia.org/archives
 
-/usr/bin/rsync -avzc --delete /var/lib/mailman/archives/qfiles/ rsync://fermium.wikimedia.org/qfiles
+/usr/bin/rsync -avz --delete /var/lib/mailman/archives/qfiles/ rsync://fermium.wikimedia.org/qfiles
 
-/usr/bin/rsync -avzc --delete /var/lib/mailman/data/ \
+/usr/bin/rsync -avz --delete /var/lib/mailman/data/ \
 --include="heldmsg-*" \
 --exclude="*.pw" \
 --exclude="bounce-*" \
