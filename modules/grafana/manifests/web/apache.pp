0257652f1f969c76a2f17e60a7589d9d51938c78
Ori Livneh <ori@wikimedia.org>
2015-09-10 11:17:03 -0700

    Put Grafana behind password authentication
    Change-Id: I8d52500424144425b19938f3498d5a43fd9970f4

--- modules/grafana/manifests/web/apache.pp
+++ modules/grafana/manifests/web/apache.pp
@@ -25,10 +25,25 @@ class grafana::web::apache(
     $listen           = '*:80',
     $elastic_backends = undef,
 ) {
+    include ::apache::mod::authnz_ldap
     include ::apache::mod::proxy_balancer
     include ::apache::mod::proxy_http
     include ::apache::mod::lbmethod_byrequests
     include ::apache::mod::headers
+
+    include ::passwords::ldap::production
+
+    $auth_ldap = {
+        name          => 'nda/ops/wmf',
+        bind_dn       => 'cn=proxyagent,ou=profile,dc=wikimedia,dc=org',
+        bind_password => $passwords::ldap::production::proxypass,
+        url           => 'ldaps://ldap-eqiad.wikimedia.org ldap-codfw.wikimedia.org/ou=people,dc=wikimedia,dc=org?cn',
+        groups        => [
+            'cn=ops,ou=groups,dc=wikimedia,dc=org',
+            'cn=nda,ou=groups,dc=wikimedia,dc=org',
+            'cn=wmf,ou=groups,dc=wikimedia,dc=org',
+        ],
+    }
 
     apache::site { 'grafana':
         ensure  => $ensure,
