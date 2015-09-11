a946601df35157f213f6170a69b8529899d43a0c
jcrespo <jcrespo@wikimedia.org>
2015-09-11 19:04:30 +0200

    Add scap scripts to all canary app servers
    As per Krenair petition, have the scap::scripts
    on canary servers, that includes mw1017.
    
    Or should we add it on mw1017 only?
    
    Bug: T112174
    Change-Id: Ia12cc7477292f440c16fb30950f0fb51e67dad59

--- manifests/role/mediawiki.pp
+++ manifests/role/mediawiki.pp
@@ -184,6 +184,10 @@ class role::mediawiki::canary_appserver {
     # salt -G 'canary:appserver' will select servers with this role.'
     salt::grain { 'canary': value => 'appserver' }
     include role::mediawiki::appserver
+
+    # include the deployment scripts because mwscript can occasionally be useful
+    # here: T112174
+    include scap::scripts
 }
 
 # Ditto, for api
