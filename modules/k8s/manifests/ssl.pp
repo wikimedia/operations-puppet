4c76cf7595c8718d4e1f6f3b2c3c38b2ab5ec008
YuviPanda <yuvipanda@gmail.com>
2015-09-10 21:31:05 -0700

    k8s: Setup ssl certificates properly
    Change-Id: Ie3956095ad5cedbc681666becba9edfcce9aa1ec

--- modules/k8s/manifests/ssl.pp
+++ modules/k8s/manifests/ssl.pp
@@ -1,7 +1,12 @@
 # Copy of etcd::ssl
 # Copies appropriate cert files from the puppet CA infrastructure
-# To be usable by the k8s apiserver
-class k8s::apiserver_ssl($puppet_cert_name = $::fqdn, $ssldir = '/var/lib/puppet/ssl') {
+# To be usable by the k8s binaries
+# Note: Only copies public components, no private keys
+class k8s::ssl(
+    $provide_private = false,
+) {
+    $puppet_cert_name = $::fqdn
+    $ssldir = '/var/lib/puppet/ssl'
 
     file { [
         '/var/lib/kubernetes',
@@ -34,12 +39,14 @@ class k8s::apiserver_ssl($puppet_cert_name = $::fqdn, $ssldir = '/var/lib/puppet
         require => File['/var/lib/kubernetes/ssl/certs/ca.pem'],
     }
 
-    file { '/var/lib/kubernetes/ssl/private_keys/server.key':
-        ensure  => present,
-        owner   => 'kube-apiserver',
-        group   => 'kube-apiserver',
-        mode    => '0400',
-        source  => "${ssldir}/private_keys/${puppet_cert_name}.pem",
-        require => File['/var/lib/kubernetes/ssl/private_keys'],
+    if $provide_private {
+        file { '/var/lib/kubernetes/ssl/private_keys/server.key':
+            ensure  => present,
+            owner   => 'kube-apiserver',
+            group   => 'kube-apiserver',
+            mode    => '0400',
+            source  => "${ssldir}/private_keys/${puppet_cert_name}.pem",
+            require => File['/var/lib/kubernetes/ssl/private_keys'],
+        }
     }
 }
