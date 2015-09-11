81e88a5f30681904c35965fbccf39ebbb4afdc8a
YuviPanda <yuvipanda@gmail.com>
2015-09-10 17:06:31 -0700

    k8s: Use insecure port for controller / scheduler
    They are all on the same host, so it is ok
    
    Change-Id: I80330779440e920439e13cc62c57b9ba0c75abac

--- modules/k8s/manifests/scheduler.pp
+++ modules/k8s/manifests/scheduler.pp
@@ -1,9 +1,5 @@
-class k8s::scheduler(
-    $master_host,
-) {
+class k8s::scheduler {
     require_package('kube-scheduler')
-
-    $master_ip = ipresolve($master_host, 4, $::nameservers[0])
 
     base::service_unit { 'kube-scheduler':
         systemd => true,
