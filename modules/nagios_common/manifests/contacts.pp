11771fb98fa1957dee17505f17f85497edb8408a
Tim Landscheidt <tim@tim-landscheidt.de>
2015-09-11 02:09:26 +0000

    nagios_common: Delay default evaluation of template()
    Change 9fc237ba098ed3404309a4f18b3be06b4bca978a changed the default of
    the content parameter for the class nagios_common::contacts to a call
    of template().  This causes Puppet failures on the Labs shinken server
    where the class shinken includes the class nagios_common::contacts
    with the parameter source, but neither content nor contacts.  In that
    case, Puppet tries to evaluate the default template and fails because
    contacts is undef.
    
    This change fixes the error by evaluating the default template only if
    the parameters source and content are both undef.
    
    Bug: T111982
    Change-Id: I88999b3807f715d2d5395b33a5e7d87a2168f9e7

--- modules/nagios_common/manifests/contacts.pp
+++ modules/nagios_common/manifests/contacts.pp
@@ -32,7 +32,7 @@ class nagios_common::contacts(
     $ensure = present,
     $config_dir = '/etc/icinga',
     $source = undef,
-    $content = template('nagios_common/contacts.cfg.erb'),
+    $content = undef,
     $owner = 'icinga',
     $group = 'icinga',
     $contacts = [],
@@ -46,6 +46,10 @@ class nagios_common::contacts(
             mode   => '0600', # Only $owner:$group can read/write
         }
     } else {
+        if ($content == undef) {
+            $content = template('nagios_common/contacts.cfg.erb')
+        }
+
         file { "${config_dir}/contacts.cfg":
             ensure  => $ensure,
             content => $content,
