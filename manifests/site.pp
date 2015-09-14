245e6398c2c80cc565716ae34bc2c14f604a6152
Andrew Otto <acotto@gmail.com>
2015-09-14 12:53:59 -0400

    Remove analytics1011, 1016, and 1019 as Hadoop workers
    Bug: T112113
    Change-Id: I0449d9ca684626f946a7fe099d5ddab84a16e024

--- manifests/site.pp
+++ manifests/site.pp
@@ -115,14 +115,14 @@ node 'analytics1015.eqiad.wmnet' {
     include base::firewall
 }
 
-# analytics1011, analytics1016-analytics1017, analytics1019,
+# analytics1017
 # analytics1028-analytics1057 are Hadoop worker nodes.
 #
 # NOTE:  If you add, remove or move Hadoop nodes, you should edit
 # templates/hadoop/net-topology.py.erb to make sure the
 # hostname -> /datacenter/rack/row id is correct.  This is
 # used for Hadoop network topology awareness.
-node /analytics10(11|1[67]|19|2[89]|3[0-9]|4[0-9]|5[0-7]).eqiad.wmnet/ {
+node /analytics10(17|2[89]|3[0-9]|4[0-9]|5[0-7]).eqiad.wmnet/ {
 
     role analytics::hadoop::worker, analytics::impala::worker
     include base::firewall
