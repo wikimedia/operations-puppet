49a977f5498f98826b6113196cd626dd2e034a25
Andrew Otto <acotto@gmail.com>
2015-09-15 14:08:09 -0400

    Puppetize MySQL/MariaDB server on analytics1015 in prep for moving Hive and Oozie
    Bug: T110090
    Change-Id: I8fb17717aa8fcc3c8f3e93d28f0e46a8fdffc051

---
+++ manifests/role/analytics/mysql.pp
@@ -0,0 +1,23 @@
+# == Class role::analytics::mysql::meta
+# Installs a MySQL/MariaDB server for use with Hive and Oozie
+#
+class role::analytics::mysql::meta {
+    include mariadb::packages_wmf
+    # This will be included once analytics1015 has been productionized
+    # - otto 2015-09-15
+    # include role::mariadb::monitor
+
+    class { 'mariadb::config':
+        config    => 'mariadb/analytics-meta.my.cnf.erb',
+        datadir   => '/var/lib/mysql',
+        require   => Class['mariadb::packages_wmf'],
+    }
+
+    service { 'mysql':
+        ensure     => 'running',
+        enable     => true,
+        hasrestart => true,
+        hasstatus  => true,
+        require    => Class['mariadb::config']
+    }
+}
