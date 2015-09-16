54ba5c573685db60ecf9e52676e68c09df82476f
andrewbogott <abogott@wikimedia.org>
2015-09-15 21:12:18 -0500

    Openstack: Clarify differences between the controller and the spare:
    - Stop glance and nova services on the spare
    - Ensure rabbit running on controller, not running on spare
    - Sync glance files from the primary to the spare only
    - Disable service monitoring on the spare
    
    Also, re-enable the nova controller role on the spare
    so that everything is installed and ready.
    
    Change-Id: I857555755b916fc6c5f039cc52b01d97e8bd1396

--- modules/openstack/manifests/nova/conductor.pp
+++ modules/openstack/manifests/nova/conductor.pp
@@ -6,14 +6,21 @@ class openstack::nova::conductor($novaconfig) {
         require => Class['openstack::repo'];
     }
 
-    service { 'nova-conductor':
-        ensure    => running,
-        subscribe => File['/etc/nova/nova.conf'],
-        require   => Package['nova-conductor'];
-    }
+    if $::hostname == hiera('labs_nova_controller') {
+        service { 'nova-conductor':
+            ensure    => running,
+            subscribe => File['/etc/nova/nova.conf'],
+            require   => Package['nova-conductor'];
+        }
 
-    nrpe::monitor_service { 'check_nova_conductor_process':
-        description  => 'nova-conductor process',
-        nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/nova-conductor'",
+        nrpe::monitor_service { 'check_nova_conductor_process':
+            description  => 'nova-conductor process',
+            nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/nova-conductor'",
+        }
+    } else {
+        service { 'nova-conductor':
+            ensure    => stopped,
+            require   => Package['nova-conductor'];
+        }
     }
 }
--- modules/openstack/manifests/queue-server.pp
+++ modules/openstack/manifests/queue-server.pp
@@ -7,4 +7,16 @@ class openstack::queue-server(
         ensure  => present,
         require => Class['openstack::repo'];
     }
+
+    if $::hostname == hiera('labs_nova_controller') {
+        service { 'rabbitmq-server':
+            ensure    => running,
+            require   => Package['rabbitmq-server'];
+        }
+    } else {
+        service { 'rabbitmq-server':
+            ensure    => stopped,
+            require   => Package['rabbitmq-server'];
+        }
+    }
 }
