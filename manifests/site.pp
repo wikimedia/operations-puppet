80260de12d222460ba13f51a8618db6a65b3fffe
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-09-17 13:12:24 +0200

    videoscaler: reimage tmh1001 as mw1259
    Bug: T104747
    Change-Id: I118cefb17e2cebcd282e8d9c2b6250a34adf0fd9

--- manifests/site.pp
+++ manifests/site.pp
@@ -2067,6 +2067,11 @@ node /^mw12(3[6-9]|4[0-9]|5[0-8])\.eqiad\.wmnet$/ {
     }
 }
 
+#mw1259 is a videoscaler
+node 'mw1259.eqiad.wmnet' {
+    role mediawiki::vidoescaler
+}
+
 # ROW A codfw appservers: mw2001-mw2079
 #mw2001-mw2006 are jobrunners
 node /^mw200[0-6]\.codfw\.wmnet$/ {
