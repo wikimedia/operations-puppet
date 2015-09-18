bd3c2dcd86f799c1a66cda0870db97c8edad68d3
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-09-18 13:16:55 +0300

    WIP: modularize otrs
    This was a single role before, move it to the module/role paradigm,
    splitting up the various classes and moving templates/files around. Add
    tests as well
    TODO: Write a better commit message for this
    
    Change-Id: I04cc8d734cd83acc2ce4c4d26cea9c0fbd1da2ec

---
+++ modules/otrs/manifests/init.pp
@@ -0,0 +1,95 @@
+# vim: set ts=4 et sw=4:
+
+class otrs(
+    $otrs_database_host,
+    $otrs_database_name,
+    $otrs_database_user,
+    $otrs_database_pw,
+    $exim_database_name,
+    $exim_database_user,
+    $exim_database_pass,
+    $trusted_networks,
+) {
+    # Implementation classes
+    include otrs::web
+    class { 'otrs::mail':
+        otrs_mysql_database => $exim_database_name,
+        otrs_mysql_user     => $exim_database_user,
+        otrs_mysql_password => $exim_database_pass,
+        trusted_networks    => $trusted_networks,
+    }
+
+    # Installation
+    $packages = [
+        'libapache-dbi-perl',
+        'libdbd-mysql-perl',
+        'libgd-graph-perl',
+        'libgd-text-perl',
+        'libio-socket-ssl-perl',
+        'libjson-xs-perl',
+        'libnet-ldap-perl',
+        'libpdf-api2-perl',
+        'libsoap-lite-perl',
+        'libtext-csv-xs-perl',
+        'libtimedate-perl',
+        'mysql-client',
+        'perl-doc',
+    ]
+
+    package { $packages:
+        ensure => 'present',
+    }
+
+    user { 'otrs':
+        home       => '/var/lib/otrs',
+        groups     => 'www-data',
+        shell      => '/bin/bash',
+        managehome => true,
+        system     => true,
+    }
+
+    file { '/opt/otrs/Kernel/Config.pm':
+        ensure  => 'file',
+        owner   => 'otrs',
+        group   => 'www-data',
+        mode    => '0440',
+        content => template('otrs/Config.pm.erb'),
+    }
+
+    file { '/etc/cron.d/otrs':
+        ensure => 'file',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/otrs/crontab.otrs',
+    }
+
+    file { '/opt/otrs/bin/otrs.TicketExport2Mbox.pl':
+        ensure => 'file',
+        owner  => 'otrs',
+        group  => 'www-data',
+        mode   => '0755',
+        source => 'puppet:///modules/otrs/otrs.TicketExport2Mbox.pl',
+    }
+
+    file { '/opt/otrs/bin/cgi-bin/idle_agent_report':
+        ensure => 'file',
+        owner  => 'otrs',
+        group  => 'www-data',
+        mode   => '0755',
+        source => 'puppet:///modules/otrs/idle_agent_report',
+    }
+
+    file { '/opt/otrs/var/httpd/htdocs/skins/Agent/default/img/icons/product.ico':
+        ensure => 'file',
+        owner  => 'otrs',
+        group  => 'www-data',
+        mode   => '0664',
+        source => 'puppet:///modules/otrs/wmf.ico',
+    }
+
+    file { '/opt/otrs/Kernel/Output/HTML/OTRS':
+        ensure => link,
+        target => '/opt/otrs/Kernel/Output/HTML/Standard',
+    }
+}
