af705eb507f60d8383f4db921b3ca8b6ff0125f1
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-09-18 13:16:55 +0300

    WIP: modularize otrs
    This was a single role before, move it to the module/role paradigm,
    splitting up the various classes and moving templates/files around. Add
    tests as well
    TODO: Write a better commit message for this
    
    Change-Id: I04cc8d734cd83acc2ce4c4d26cea9c0fbd1da2ec

---
+++ modules/otrs/manifests/mail.pp
@@ -0,0 +1,67 @@
+class otrs::mail(
+    $otrs_mysql_database,
+    $otrs_mysql_user,
+    $otrs_mysql_password,
+    $trusted_networks,
+){
+    include clamav
+    include exim4::ganglia
+
+    class { 'exim4':
+        variant => 'heavy',
+        config  => template('otrs/exim4.conf.otrs.erb'),
+        filter  => template('otrs/system_filter.conf.otrs.erb'),
+        require => [
+            Class['spamassassin'],
+            Class['clamav'],
+        ]
+    }
+    class { 'spamassassin':
+        required_score        => '3.5',# (5.0)
+        use_bayes             => '1',  # 0|(1)
+        bayes_auto_learn      => '0',  # 0|(1)
+        short_report_template => true, # true|(false)
+        trusted_networks      => $trusted_networks,
+        custom_scores         => {
+            'RP_MATCHES_RCVD'   => '-0.500',
+            'SPF_SOFTFAIL'      => '2.000',
+            'SUSPICIOUS_RECIPS' => '2.000',
+            'DEAR_SOMETHING'    => '1.500',
+        },
+        debug_logging         => '--debug spf',
+    }
+
+    mailalias { 'root':
+        recipient => 'root@wikimedia.org',
+    }
+
+    file { '/etc/exim4/defer_domains':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'Debian-exim',
+        mode    => '0444',
+        require => Class['exim4'],
+    }
+
+    file { '/usr/local/bin/train_spamassassin':
+        ensure => 'file',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/otrs/train_spamassassin',
+    }
+
+    cron { 'otrs_train_spamassassin':
+        ensure  => 'present',
+        user    => 'root',
+        minute  => '5',
+        command => '/usr/local/bin/train_spamassassin',
+    }
+
+    file { '/var/spool/spam':
+        ensure => 'directory',
+        owner  => 'otrs',
+        group  => 'www-data',
+        mode   => '0775',
+    }
+}
