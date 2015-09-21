c79f539bcd05bc305a848e6a4339e4827203a46b
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-09-21 10:52:21 +0200

    Enable ferm on mw1120-mw1129
    Change-Id: I3b7336162f30a273a543c8953b00319d306fbbe6

--- manifests/site.pp
+++ manifests/site.pp
@@ -1989,6 +1989,11 @@ node /^mw111[4-9]\.eqiad\.wmnet$/ {
 # mw1120-1148 are api apaches
 node /^mw11([23][0-9]|4[0-8])\.eqiad\.wmnet$/ {
     role mediawiki::appserver::api
+
+    if $::hostname =~ /^mw112[0-9]$/ {
+        include base::firewall
+    }
+
 }
 
 
