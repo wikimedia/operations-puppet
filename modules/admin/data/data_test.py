3117a6ef785544a7336683080cc331aef97c79fd
Kunal Mehta <legoktm@gmail.com>
2015-09-21 22:17:34 +0000

    Add tests to make sure production ssh keys are not in labs
    Change-Id: Ia979a4a9236ce3e54a907a7e5f2ca574f85fe090

--- modules/admin/data/data_test.py
+++ modules/admin/data/data_test.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
 # 2014 Chase Pettet
+# 2015 Alex Monk
 # Tests to perform basic validation on data.yaml
 
 from collections import Counter
 import itertools
+import ldap
 import os
+import sys
 import unittest
 import yaml
+
+sys.path.append('../../ldap/files/scripts')
+
+import ldapsupportlib
 
 
 class DataTest(unittest.TestCase):
@@ -25,11 +32,14 @@ class DataTest(unittest.TestCase):
         )
         return list(set(self.flatten(nested_users_list)))
 
-    def testDataDotYaml(self):
-
+    def open_data(self):
         with open(os.path.join(os.path.dirname(__file__), 'data.yaml')) as f:
             admins = yaml.safe_load(f)
 
+        return admins
+
+    def testDataDotYaml(self):
+        admins = self.open_data()
         all_users = list(admins['users'])
         grouped_users = self.all_assigned_users(admins)
 
@@ -46,6 +56,29 @@ class DataTest(unittest.TestCase):
             v.get('gid', None) for k, v in admins['groups'].iteritems()])
         dupes = [k for k, v in Counter(gids).items() if v > 1]
         self.assertEqual([], dupes, 'Duplicate group GIDs')
+
+    def testLabsSshKeys(self):
+        # Make sure no users have the same ssh key in labs and production
+
+        ldapSupportLib = ldapsupportlib.LDAPSupportLib()
+        ds = ldapSupportLib.connect()
+        base = "ou=people," + ldapSupportLib.getBase()
+
+        keys = set()
+        for entry in ds.search_s(base, ldap.SCOPE_SUBTREE, "(objectclass=inetOrgPerson)"):
+            for k, v in entry[1].items():
+                if k == "sshPublicKey":
+                    for v2 in v:
+                        key = ' '.join(v2.split(' ')[:2])
+                        keys.update([key])
+
+        prodData = self.open_data()
+        for userName, userData in prodData['users'].items():
+            badKeys = list(set(userData['ssh_keys']).intersection(keys))
+            if len(badKeys) > 0:
+                print('%s: %s' % (userName, userData))
+            self.assertEqual(badKeys, 0)
+        ds.unbind()
 
 if __name__ == '__main__':
     unittest.main()
