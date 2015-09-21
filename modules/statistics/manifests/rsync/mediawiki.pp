9f96d90a85882858fd7d73bdb4338e3b505297ae
Andrew Otto <acotto@gmail.com>
2015-09-21 10:18:13 -0400

    Fix identation and wildcard in rsync for api.log
    Bug: T112744
    Change-Id: Ibb7e6df52b385dc4d816811e5f5ec6310324d657

--- modules/statistics/manifests/rsync/mediawiki.pp
+++ modules/statistics/manifests/rsync/mediawiki.pp
@@ -31,10 +31,10 @@ class statistics::rsync::mediawiki {
     }
 
     # Api logs from fluorine
-        statistics::rsync_job { 'mw-api':
-            source         => 'fluorine.eqiad.wmnet::udp2log/archive/api.log.*.gz',
-            destination    => "${working_path}/mw-log/archive",
-            # Retention of 30 days to save disk space
-            retention_days => 30,
-        }
+    statistics::rsync_job { 'mw-api':
+        source         => 'fluorine.eqiad.wmnet::udp2log/archive/api.log-*.gz',
+        destination    => "${working_path}/mw-log/archive",
+        # Retention of 30 days to save disk space
+        retention_days => 30,
+    }
 }
