97930d3b393fca028acae1d3e746a44be3c9c8de
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-09-21 13:13:12 +0200

    Enable ferm on mw1156, mw1157
    Change-Id: I6309e7eb9bfd90a1174fc50ecad1fc81d2dbe8f3

--- manifests/site.pp
+++ manifests/site.pp
@@ -2006,7 +2006,7 @@ node 'mw1152.eqiad.wmnet' {
 node /^mw11(5[3-9]|60)\.eqiad\.wmnet$/ {
     role mediawiki::imagescaler
 
-    if $::hostname =~ /^mw115[3-5]$/ {
+    if $::hostname =~ /^mw115[3-7]$/ {
         include base::firewall
     }
 }
