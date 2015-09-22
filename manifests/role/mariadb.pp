2b8d2493e6ee7dd612981dff054b02c5ff0c41bd
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-09-22 09:16:23 +0200

    Add ferm rules for role::mariadb::misc::eventlogging
    Only the standard mysql DB ports are in use.
    
    Bug: T104699
    Change-Id: I90425ae32f4419e53f119e5ec13aafbf1ecb5e5d

--- manifests/role/mariadb.pp
+++ manifests/role/mariadb.pp
@@ -208,6 +208,7 @@ class role::mariadb::misc::eventlogging(
     include standard
     include role::mariadb::monitor::dba
     include passwords::misc::scripts
+    include role::mariadb::ferm
 
     class { 'mariadb::packages_wmf':
         mariadb10 => true,
