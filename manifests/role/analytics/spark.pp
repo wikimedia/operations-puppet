5313f35ddfcb02bdf2709bbb4d7d2658fe72b06d
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-09-23 14:13:49 +0200

    Add ferm rules for Spark
    Bug: T83597
    
    Change-Id: Icadb64ca8ee994b81a55e21fabf295fa270b2992

--- manifests/role/analytics/spark.pp
+++ manifests/role/analytics/spark.pp
@@ -28,9 +28,39 @@ class role::analytics::spark::standalone {
 class role::analytics::spark::standalone::master {
     require role::analytics::spark::standalone
     include cdh::spark::master
+
+    ferm::service{ 'spark-master-web-ui':
+        proto  => 'tcp',
+        port   => '18080',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'spark-master-rpc':
+        proto  => 'tcp',
+        port   => '7077',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'spark-rest-server':
+        proto  => 'tcp',
+        port   => '6066',
+        srange => '$ANALYTICS_NETWORKS',
+    }
 }
 
 class role::analytics::spark::standalone::worker {
     require role::analytics::spark::standalone
     include cdh::spark::worker
+
+    ferm::service{ 'spark-worker-web-ui':
+        proto  => 'tcp',
+        port   => '18081',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'spark-worker-rpc':
+        proto  => 'tcp',
+        port   => '7078',
+        srange => '$ANALYTICS_NETWORKS',
+    }
 }
