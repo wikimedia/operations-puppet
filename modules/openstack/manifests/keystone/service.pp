ddeb1b38ff13ce5c08b7a994cf330eb927466f74
andrewbogott <abogott@wikimedia.org>
2015-09-24 08:42:31 -0500

    Openstack: Add monitoring for the keystone service
    Change-Id: Ic2a539084a8f1bac00432ffd184b98e159995d06

--- modules/openstack/manifests/keystone/service.pp
+++ modules/openstack/manifests/keystone/service.pp
@@ -41,6 +41,11 @@ class openstack::keystone::service($openstack_version=$::openstack::version, $ke
                 ensure  => present,
                 command => "/usr/bin/mysql $keystone_db_name -h${keystone_db_host} -u${keystone_db_user} -p${keystone_db_pass} -e 'DELETE FROM token WHERE NOW() - INTERVAL 2 day > expires LIMIT 10000;'",
         }
+
+        nrpe::monitor_service { 'check_keystone_process':
+            description  => 'keystone process',
+            nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/keystone-all'",
+        }
     } else {
         service { 'keystone':
             ensure    => stopped,
