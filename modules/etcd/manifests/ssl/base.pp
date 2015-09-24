cff64bde7b58f0b79228e6ddcf06d8069390c80f
Andrew Otto <acotto@gmail.com>
2015-09-24 19:06:06 -0400

    Puppetize etcd use for eventlogging processor
    This will allow eventlogging to consistently hash client IPs even
    when parallelized.
    
    Requires that https://gerrit.wikimedia.org/r/#/c/238854/ is deployed.
    
    This change also adds a provide_private parameter to etcd::ssl so that
    the class can be used for etcd clients, not just etcd servers.
    
    Bug: T112688
    Change-Id: I8ba49df87b4edb080d3e4786b4f884a61ef003e6

---
+++ modules/etcd/manifests/ssl/base.pp
@@ -0,0 +1,28 @@
+class etcd::ssl::base ( $ssldir ) {
+    $basedir = '/var/lib/etcd/ssl'
+    $pubdir = "${basedir}/certs"
+    $cacert = "${pubdir}/ca.pem"
+    file { $basedir:
+        ensure  => directory,
+        owner   => 'etcd',
+        group   => 'etcd',
+        mode    => '0500',
+        require => Package['etcd']
+    }
+
+    file { $pubdir:
+        ensure  => directory,
+        owner   => 'etcd',
+        group   => 'etcd',
+        mode    => '0500',
+    }
+
+    file { $cacert:
+        ensure => present,
+        owner  => 'etcd',
+        group  => 'etcd',
+        mode   => '0400',
+        source => "${ssldir}/certs/ca.pem",
+    }
+
+}
