05a71ed879c9888ac1977be4beba75c685ac58ed
dzahn <dzahn@wikimedia.org>
2015-09-26 08:33:28 -0700

    analytics: impala,refinery: small lint fixes
    Change-Id: I0de9a9841c7aff8159fa7741a3626dc3d40fcbdd

--- manifests/role/analytics/impala.pp
+++ manifests/role/analytics/impala.pp
@@ -7,7 +7,7 @@
 # Installs base impala packages and the impala-shell client.
 #
 class role::analytics::impala {
-    class { cdh::impala:
+    class { 'cdh::impala':
         master_host => hiera('analytics::impala::master_host')
     }
 }
