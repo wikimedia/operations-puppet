b76153c6daeea5bf5e37c11c6d22cf454de2302d
Jcrespo <jcrespo@wikimedia.org>
2015-09-29 06:44:43 +0000

    Revert "phab: re-enable dump script"
    This reverts commit 653001f171966e4b9e317211d4e3eca85c84ce09.
    
    Connection spike reports happened at the same time than the dump process. Blocking it until it is setup in the slave.
    
    Change-Id: I29464fecf15951e2e577d9bab1bbdada079be880

--- modules/phabricator/manifests/tools.pp
+++ modules/phabricator/manifests/tools.pp
@@ -41,7 +41,7 @@ class phabricator::tools (
         }
 
         cron { $dump_script:
-            ensure  => present,
+            ensure  => absent,
             command => $dump_script,
             user    => root,
             hour    => '2',
