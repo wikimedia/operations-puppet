c06baee3b8fbfdf233bb984ce251d5838a6a4c36
Andrew Otto <acotto@gmail.com>
2015-09-29 10:57:51 -0700

    Ensure /var/lib/etcd exists even if etcd package is not installed
    Bug: T112688
    Change-Id: I26246d73c42a55f6e485817da2227c36ebbb66f0

--- modules/etcd/manifests/ssl/base.pp
+++ modules/etcd/manifests/ssl/base.pp
@@ -2,6 +2,14 @@ class etcd::ssl::base ($ssldir = '/var/lib/puppet/ssl') {
     $basedir = '/var/lib/etcd/ssl'
     $pubdir = "${basedir}/certs"
     $cacert = "${pubdir}/ca.pem"
+
+    file { [dirname($basedir), $basedir]:
+        ensure  => directory,
+        owner   => 'etcd',
+        group   => 'etcd',
+        mode    => '0500',
+    }
+
     file { $basedir:
         ensure  => directory,
         owner   => 'etcd',
