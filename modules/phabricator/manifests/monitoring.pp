36dc4c72c57a4cc15af4d597c1d6b71ffb22f479
cpettet <rush@wikimedia.org>
2015-09-29 17:15:20 -0500

    phab: basic apache status metrics to graphite
    Change-Id: I276290788e8923469d73a456509dd77887a56fcd

--- modules/phabricator/manifests/monitoring.pp
+++ modules/phabricator/manifests/monitoring.pp
@@ -3,6 +3,10 @@
 class phabricator::monitoring {
 
     include apache::mod::status
+    diamond::collector { 'ApacheStatusSimple':
+        source   => 'puppet:///modules/phabricator/monitor/apache_status.py',
+    }
+
     nrpe::monitor_service { 'check_phab_taskmaster':
         description   => 'PHD should be supervising processes',
         nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 3:150 -u phd',
