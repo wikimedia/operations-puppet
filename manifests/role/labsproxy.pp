323f5b22aee99199bc2582a400d782ba315bebd7
YuviPanda <yuvipanda@gmail.com>
2015-09-29 18:06:50 -0700

    dynamicproxy: Enable redis replication for novaproxy
    Change-Id: I23c075ce7c673ea9cb2e8481e9d80b6a8735c231

--- manifests/role/labsproxy.pp
+++ manifests/role/labsproxy.pp
@@ -1,14 +1,34 @@
 # A dynamic HTTP routing proxy, based on nginx+lua+redis
-class role::dynamicproxy {
+class role::dynamicproxy(
+    $all_proxies,
+    $active_proxy,
+) {
     include base::firewall
 
     sslcert::certificate { 'star.wmflabs.org': skip_private => true }
+
+    $proxy_nodes = join($all_proxies, ' ')
+    # Open up redis to all proxies!
+    ferm::service { 'redis-replication':
+        proto  => 'tcp',
+        port   => '6379',
+        srange => "@resolve(($proxy_nodes))",
+    }
+
+    if $::hostname != $active_proxy {
+        $redis_replication = {
+            "${::hostname}" => $active_proxy
+        }
+    } else {
+        $redis_replication = undef
+    }
 
     class { '::dynamicproxy':
         ssl_certificate_name => 'star.wmflabs.org',
         ssl_settings         => ssl_ciphersuite('nginx', 'compat'),
         set_xff              => true,
         luahandler           => 'domainproxy',
+        redis_replication    => $redis_replication,
         require              => Sslcert::Certificate['star.wmflabs.org'],
     }
     include dynamicproxy::api
