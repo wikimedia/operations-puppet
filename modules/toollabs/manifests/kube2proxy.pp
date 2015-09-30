242439e352a261607a37542f8874a2a542030b33
YuviPanda <yuvipanda@gmail.com>
2015-09-30 15:22:36 -0700

    toollabs: Cleanup kube2proxy
    - Move include to appropriate role
    - Use python3 not python2
    - Remove pip install provider
    - Remove kube2proxy upstart script
    - Use kubernetes general user than confusingly named kubeproxy user
    
    Change-Id: I697161cc75e2f31a8de38b92ede69892b98ef36f

--- modules/toollabs/manifests/kube2proxy.pp
+++ modules/toollabs/manifests/kube2proxy.pp
@@ -1,42 +1,19 @@
 class toollabs::kube2proxy(
-    $ensure='present',
-    $ssldir='/var/lib/puppet/ssl/',
-    $kubemaster='https://tools-k8s-master-01.tools.eqiad.wmflabs:6443',
+    $master_host,
     $kube_token='test',
-    ){
+) {
+    include k8s::users
+    include k8s::ssl
 
     file { '/usr/local/sbin/kube2proxy':
-        ensure => $ensure,
+        ensure => present,
         owner  => 'root',
         group  => 'root',
         mode   => '0555',
         source => 'puppet:///modules/toollabs/kube2dynproxy.py',
     }
 
-    group { 'kubeproxy':
-        ensure => present,
-        system => true,
-    }
-
-    user { 'kubeproxy':
-        ensure  => present,
-        gid     => 'kubeproxy',
-        shell   => '/bin/false',
-        home    => '/nonexistent',
-        system  => true,
-        require => Group['kubeproxy']
-    }
-
-    require_package(['python-requests', 'python-redis', 'python-yaml'])
-
-    # Trusty and jessie's python-requests package is buggy
-    # and would break watching kubernetes for changes
-    package { 'requests':
-        provider => 'pip',
-        ensure   => latest,
-    }
-
-    include k8s::ssl
+    require_package(['python3-requests', 'python3-redis', 'python3-yaml'])
 
     # Temporarily not run on non-active proxies
     # note that having redis based replication
@@ -48,20 +25,18 @@ class toollabs::kube2proxy(
             $::hostname => 'running',
             default     => 'stopped'
         }
-        $params = {'ensure' => $should_run}
+        $service_params = {'ensure' => $should_run}
     } else {
-        $params = {}
+        $service_params = {}
     }
 
     base::service_unit{ 'kube2proxy':
         ensure         => $ensure,
         refresh        => true,
         systemd        => true,
-        upstart        => true,
-        service_params => $params,
+        service_params => $service_params,
         subscribe => [
                       File['/usr/local/sbin/kube2proxy'],
-                      File['/var/lib/kubernetes/ssl/certs/ca.pem']
                       ],
     }
 
