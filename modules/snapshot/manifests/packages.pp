74f0155444b859c743c9f46a7d674a2a171b17d9
Marius Hoch <hoo@online.de>
2015-10-03 16:04:00 +0200

    Include nutcracker in snapshot::packages
    Needed after 9a77edb629e
    
    Bug: T114571
    Change-Id: I7b61e0c56836c5bf4878ba5566fee0b5a7a2f0a9

--- modules/snapshot/manifests/packages.pp
+++ modules/snapshot/manifests/packages.pp
@@ -2,6 +2,7 @@ class snapshot::packages {
 
     # pick up various users, twemproxy
     include ::mediawiki
+    include mediawiki::nutcracker
 
     require_package('mwbzutils')
     require_package('mysql-client')
