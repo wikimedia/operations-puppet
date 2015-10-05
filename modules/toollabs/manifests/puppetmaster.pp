5a0926320d2206772dc60f26a4ca867788300ce6
YuviPanda <yuvipanda@gmail.com>
2015-10-05 13:31:01 -0700

    tools: Add puppetmaster/client roles
    - Only applied on k8s related nodes
    - Deny access to all non-admins
    - Also make gitsync configurable so it can sync the
      puppet repositories more frequently
    
    Bug: T112005
    Change-Id: I23e64a138a287fa1e69b4d2ce894cf95f1c22592

---
+++ modules/toollabs/manifests/puppetmaster.pp
@@ -0,0 +1,11 @@
+# Puppetmaster used in toollabs only for k8s nodes
+class toollabs::puppetmaster {
+    class { '::puppet::self::master':
+        server => $::fqdn,
+    }
+
+    # Auto pull everything every minute!
+    class { 'puppetmaster::gitsync':
+        run_every_minutes => '1',
+    }
+}
