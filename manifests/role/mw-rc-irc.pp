162f1789ad9d8ed9ec38c7d2c748c3115062d0e4
dzahn <dzahn@wikimedia.org>
2015-10-06 15:16:25 -0700

    mw-rc-irc: firewall hole for RC IRC bot
    We added firewall holes for the public IRCd ports,
    but there is also the RecentChanges IRC bot, who
    gets updates from appservers and listens on 9390/udp.
    
    Bug:T104943
    Change-Id: Id02c1ea5ea57e3f323ab7eda16d383e536329396

--- manifests/role/mw-rc-irc.pp
+++ manifests/role/mw-rc-irc.pp
@@ -12,8 +12,17 @@ class role::mw-rc-irc {
     include mw-rc-irc::ircserver
     include mw-rc-irc::apache
 
+    # IRCd - public access
     ferm::service { 'ircd_public':
-        proto => 'tcp',
+        proto  => 'tcp',
         port   => '(6664 6665 6666 6667 6668 6669 8001)',
     }
+
+    # IRC RecentChanges bot - gets updates from appservers
+    ferm::service { 'udpmxircecho':
+        proto  => 'udp',
+        port   => '9390',
+        srange => 'FIXME',
+    }
+
 }
