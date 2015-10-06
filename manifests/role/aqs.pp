wikimedia/operations-puppet

commit 22ead0c75c0e79c0d8a20b2a81c7ba7fe36c601c

aqs: Allow CQL access from analytics
Allow analytics machines to push data to aqs cassandra

Bug: T107056
Change-Id: I222fe054a3bd1d2750eb06484726782534c358be

manifests/role/aqs.pp
@@ -21,6 +21,9 @@ class role::aqs {
     include ::cassandra
     include ::cassandra::metrics
     include ::cassandra::logging
+    include networks::constants
+
+    $analytics_networks = $network::constants::analytics_networks
 
     # Emit an Icinga alert unless there is exactly one Java process belonging
     # to user 'cassandra' and with 'CassandraDaemon' in its argument list.
@@ -55,7 +58,7 @@ class role::aqs {
     ferm::service { 'cassandra-analytics-cql':
         proto  => 'tcp',
         port   => '9042',
-        srange => "@resolve((${cassandra_hosts_ferm}))",
+        srange => "@resolve((${cassandra_hosts_ferm} ${analytics_networks}))",
     }
 
 

commit 1894f9458e9666cbd276d7e518946195569c2856

aqs: Fix typo introduced in 22ead0c
s/networks/network/

Change-Id: Idddba9520757065a0ee5d64c9fdef9168636cb02

manifests/role/aqs.pp
@@ -21,7 +21,7 @@ class role::aqs {
     include ::cassandra
     include ::cassandra::metrics
     include ::cassandra::logging
-    include networks::constants
+    include network::constants
 
     $analytics_networks = $network::constants::analytics_networks
 