111cf1e6cecd966f5201691d60f83565c0da4104
Antoine Musso <hashar@free.fr>
2015-10-06 15:01:10 +0200

    contint: restore unattended upgrade on slaves
    Originally I tried to get it to run hourly, but that did not work. We
    also had a broken HHVM package (T98876).  It has been disabled via
    https://gerrit.wikimedia.org/r/#/c/210391/
    
    It is a huge trouble to upgrade the package on all the slaves.
    
    Restore unattended upgrade on Jenkins slaves with the default daily run.
    
    contint::packages::apt is applied on labs slave via
    contint::packages::labs.
    
    Bug: T98885
    Change-Id: I0b30b75a8c22ddc32b9a43938a491ae639e0d562

--- modules/contint/manifests/packages/apt.pp
+++ modules/contint/manifests/packages/apt.pp
@@ -3,27 +3,14 @@
 # Apt configuration needed for contint hosts
 #
 class contint::packages::apt {
-    class { 'apt::unattendedupgrades':
-        ensure => absent,
-    }
+    include apt::unattendedupgrades
 
     apt::conf { 'unattended-upgrades-wikimedia':
-        ensure   => absent,
         priority => '51',
         key      => 'Unattended-Upgrade::Allowed-Origins',
         # lint:ignore:single_quote_string_with_variables
         value    => 'Wikimedia:${distro_codename}-wikimedia',
         # lint:endignore
     }
-    apt::conf { 'lower-periodic-randomsleep':
-        ensure   => absent,
-        priority => '51',
-        key      => 'APT::Periodic::RandomSleep',
-        value    => '300',
-    }
 
-    # Not meant to run hourly :/
-    file { '/etc/cron.hourly/apt':
-        ensure  => absent,
-    }
 }
