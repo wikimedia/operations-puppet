678d24924978732385f573678843cb35e825a082
John F. Lewis <johnflewis93@gmail.com>
2015-10-07 19:55:43 +0100

    wdq_mm: rename module to standard naming (underscores)
    Change-Id: Ifad416c7eaadc227fbcb806596821c40d59190a5

--- manifests/role/labswdq_mm.pp
+++ manifests/role/labswdq_mm.pp
@@ -1,7 +1,7 @@
-# == Class: role::labs::wdq-mm
+# == Class: role::labs::wdq_mm
 # Role class for setting up an instance of
 # Magnus' WDQ on labs
-class role::labs::wdq-mm {
+class role::labs::wdq_mm {
     include misc::labsdebrepo
 
     include labs_lvm
@@ -10,7 +10,7 @@ class role::labs::wdq-mm {
         size    => '80%FREE',
     }
 
-    class { '::wdq-mm':
+    class { '::wdq_mm':
         require => [
             Class['misc::labsdebrepo'],
             Labs_lvm::Volume['instance-storage'],
@@ -18,10 +18,10 @@ class role::labs::wdq-mm {
     }
 }
 
-# == Class: role::labs::wdq-mm::lb
+# == Class: role::labs::wdq_mm::lb
 # Load balancer for balancing across multiple instances
-# of role::labs::wdq-mm
-class role::labs::wdq-mm::lb {
-    class { '::wdq-mm::lb':
+# of role::labs::wdq_mm
+class role::labs::wdq_mm::lb {
+    class { '::wdq_mm::lb':
     }
 }
