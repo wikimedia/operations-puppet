b10cc0bbc8d66b98c9c068e4639980123d6f6679
Antoine Musso <hashar@free.fr>
2015-10-07 17:09:56 +0200

    nodepool: monitor nodepoold is present
    We want one and only one nodepoold to be present on the server.
    
    Add 'contint' as a contact group.
    
    Bug: T113806
    Change-Id: Id19e6965ca7fbda64920ff911cbd92b8b82863fe

--- manifests/role/nodepool.pp
+++ manifests/role/nodepool.pp
@@ -40,4 +40,12 @@ class role::nodepool {
         openstack_tenant_id     => 'contintcloud',
     }
 
+    monitoring::service { 'nodepoold':
+        description   => 'nodepoold running',
+        contact_group => 'contint',
+        nrpe_command  => '/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -u nodepool --ereg-argument-array="^/usr/bin/python /usr/bin/nodepoold -d"',
+    }
+
+
+
 }
