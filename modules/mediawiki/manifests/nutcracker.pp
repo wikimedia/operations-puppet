cfa62b233e767d954ea9cc95c513a0fc3e5c2c79
Ori Livneh <ori@wikimedia.org>
2015-10-07 12:53:37 -0700

    Make the 'sessions' redis cache multi-DC ready
    Currently, nutcracker on both codfw and eqiad is proxying client connections on
    port 6380 to a redis clustered labeled 'session-redis' and whose membership
    varies by cluster. For multi-DC, MediaWiki needs to be aware of there being
    multiple clusters, and it needs to be able to address requests to the primary
    redis cluster.
    
    To achieve this, add two new pools to the nutcracker configuration:
    'redis_eqiad' and 'redis_codfw'. To make migration easy and safe, keep the
    current 'session-redis' pool for now.
    
    The new pools listen on a UNIX domain socket. For the time being, they don't
    set 'preconnect', to avoid inundating the redis hosts with idle connections.
    
    Bug: T111575
    Change-Id: I64bc52ca95d47bf30e3c0b7d2a0d02e0c66245da

--- modules/mediawiki/manifests/nutcracker.pp
+++ modules/mediawiki/manifests/nutcracker.pp
@@ -18,6 +18,7 @@ class mediawiki::nutcracker {
             timeout              => 250,
             servers              => hiera('mediawiki_memcached_servers'),
         },
+
         'mc-unix'       => {
             auto_eject_hosts     => true,
             distribution         => 'ketama',
@@ -30,6 +31,37 @@ class mediawiki::nutcracker {
             timeout              => 250,
             servers              => hiera('mediawiki_memcached_servers'),
         },
+
+        'redis_eqiad'           =>  {
+            auto_eject_hosts     => true,
+            distribution         => 'ketama',
+            redis                => true,
+            redis_auth           => $passwords::redis::main_password,
+            hash                 => 'md5',
+            listen               => '/var/run/nutcracker/redis_eqiad.sock 0666',
+            # preconnect           => true,
+            server_connections   => 1,
+            server_failure_limit => 3,
+            server_retry_timeout => to_milliseconds('30s'),
+            timeout              => 1000,
+            servers              => hiera('mediawiki::redis_servers::eqiad'),
+        },
+
+        'redis_codfw'           =>  {
+            auto_eject_hosts     => true,
+            distribution         => 'ketama',
+            redis                => true,
+            redis_auth           => $passwords::redis::main_password,
+            hash                 => 'md5',
+            listen               => '/var/run/nutcracker/redis_codfw.sock 0666',
+            # preconnect           => true,
+            server_connections   => 1,
+            server_failure_limit => 3,
+            server_retry_timeout => to_milliseconds('30s'),
+            timeout              => 1000,
+            servers              => hiera('mediawiki::redis_servers::codfw'),
+        },
+
         'session-redis' => {
             auto_eject_hosts     => true,
             distribution         => 'ketama',
