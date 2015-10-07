06aa85191adfff35b96470aa552bd4359f4de556
John F. Lewis <johnflewis93@gmail.com>
2015-10-07 19:39:04 +0100

    mw_rc_irc: rename to standard naming scheme (underscores)
    Change-Id: I649cd02a9601da66c8eaec3e86e6065a09a7ac72

--- modules/mw_rc_irc/manifests/apache.pp
+++ modules/mw_rc_irc/manifests/apache.pp
@@ -1,7 +1,7 @@
 # redirect http://irc.wikimedia.org to http://meta.wikimedia.org/wiki/IRC
-class mw-rc-irc::apache {
+class mw_rc_irc::apache {
     apache::site { 'irc.wikimedia.org':
-        content => template('mw-rc-irc/apache/irc.wikimedia.org.erb'),
+        content => template('mw_rc_irc/apache/irc.wikimedia.org.erb'),
     }
 }
 
