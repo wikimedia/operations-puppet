22babaf7776eea860ffd11afc336fb49ce0dfd60
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-10-07 11:38:32 +0100

    cassandra: fix config_directory ownership
    Change-Id: I3735c5da4de0f2630caf34cd521f9805f402b02b

--- modules/cassandra/manifests/instance.pp
+++ modules/cassandra/manifests/instance.pp
@@ -76,8 +76,8 @@ define cassandra::instance(
 
     file { $config_directory:
         ensure  => directory,
-        owner   => 'cassandra',
-        group   => 'cassandra',
+        owner   => 'root',
+        group   => 'root',
         mode    => '0755',
         require => Package['cassandra'],
     }
