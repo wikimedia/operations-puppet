8ba89294d5ddf1336a42321f2d1a928e357f4db9
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-10-08 14:46:44 +0300

    maps: Add tileratorui service
    tileratorui is a service to be used to allow a human to view tiles and
    schedule build/rebuild/deletes and so on.
    
    Bug: T116062
    Change-Id: Ifd1f3d586bc8794c59c768424f804bcaf445f06e

---
+++ modules/tileratorui/manifests/init.pp
@@ -0,0 +1,22 @@
+# Class: tileratorui
+#
+# This class installs and configures tileratorui
+#
+# While only being a thin wrapper around service::node, this class exists to
+# accomodate future tileratorui needs that are not suited for the service module
+# classes as well as conform to a de-facto standard of having a module for every
+# service
+class tileratorui {
+    $cassandra_tileratorui_user = 'tileratorui'
+    $cassandra_tileratorui_pass = hiera('maps::cassandra_tileratorui_pass')
+    $pgsql_tileratorui_user = 'tileratorui'
+    $pgsql_tileratorui_pass = hiera('maps::postgresql_tileratorui_pass')
+
+    # NOTE: tileratorui does not have an LVS service associated with it. It is
+    # only meant to be used through an SSH tunnel
+    service::node { 'tileratorui':
+        port       => 6535,
+        config     => template('tileratorui/config.yaml.erb'),
+        no_workers => 0, # 0 on purpose to only have one instance running
+    }
+}
