9efed5c7ea7f84d8433af280272641b2fae6fac9
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-10-08 14:54:50 +0300

    tilerator: comment about the port argument
    tilerator is not designed to listen to requests. Add a comment for that
    behavior
    
    Bug: T112914
    Change-Id: I3e363856ce65f4aad66c5c88d834e08b1df40f8f

--- modules/tilerator/manifests/init.pp
+++ modules/tilerator/manifests/init.pp
@@ -13,6 +13,10 @@ class tilerator() {
     $pgsql_tilerator_user = 'tilerator'
     $pgsql_tilerator_pass = hiera('maps::postgresql_tilerator_pass')
 
+    # NOTE: The port here is only used for health monitoring. tilerator is a
+    # daemon executing tasks from a queue, it does not realy listen to requests.
+    # So there will never be LVS or anything else than health check requests to
+    # this port
     service::node { 'tilerator':
         port       => 6534,
         config     => template('tilerator/config.yaml.erb'),
