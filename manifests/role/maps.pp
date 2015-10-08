1ac0a21e55611bd2e9918e2572d7de52cdde8c19
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-10-08 14:46:44 +0300

    maps: Add tileratorui service
    tileratorui is a service to be used to allow a human to view tiles and
    schedule build/rebuild/deletes and so on.
    
    Bug: T116062
    Change-Id: Ifd1f3d586bc8794c59c768424f804bcaf445f06e

--- manifests/role/maps.pp
+++ manifests/role/maps.pp
@@ -66,6 +66,7 @@ class role::maps::master {
     # Grants
     $kartotherian_pass = hiera('maps::postgresql_kartotherian_pass')
     $tilerator_pass = hiera('maps::postgresql_tilerator_pass')
+    $tileratorui_pass = hiera('maps::postgresql_tileratorui_pass')
     $osmimporter_pass = hiera('maps::postgresql_osmimporter_pass')
     $osmupdater_pass = hiera('maps::postgresql_osmupdater_pass')
     file { '/usr/local/bin/maps-grants.sql':
@@ -77,6 +78,7 @@ class role::maps::master {
     # Cassandra grants
     $cassandra_kartotherian_pass = hiera('maps::cassandra_kartotherian_pass')
     $cassandra_tilerator_pass = hiera('maps::cassandra_tilerator_pass')
+    $cassandra_tileratorui_pass = hiera('maps::cassandra_tileratorui_pass')
     file { '/usr/local/bin/maps-grants.cql':
         owner   => 'root',
         group   => 'root',
