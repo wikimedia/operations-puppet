79d861de1e399480a1edaf404de63756ec94da65
Andrew Otto <acotto@gmail.com>
2015-10-09 10:37:58 -0400

    Add camus module, use camus::job to import eventlogging data
    I will move the webrequest import job once this is verified to work.
    
    Bug: T115114
    Change-Id: I6ce581943d3bcadc2efdb01ef3e13a96cc723e71

---
+++ modules/camus/manifests/init.pp
@@ -0,0 +1,25 @@
+# == Class camus
+#
+class camus {
+    # Require that an HDFS client is installed by ensuring that cdh::hadoop
+    # is included on this node.
+    Class['cdh::hadoop'] -> Class['camus']
+
+    $config_directory = '/etc/camus.d'
+    $log_directory    = '/var/log/camus'
+
+    file { $config_directory:
+        ensure => 'directory',
+    }
+
+    file { $log_directory:
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'hdfs',
+        # setgid bit here to make camus log files writeable
+        # by users in the hdfs group.
+        mode   => '2775',
+    }
+
+    # TODO: Add logrotate for camus logs
+}
