5d9f59b82e2911d275d353ab8361204476fe9902
John F. Lewis <johnflewis93@gmail.com>
2015-10-09 16:45:25 +0100

    mw_rc_irc: rename module to standard naming
    Change-Id: I9c855665f3c924e7852123a8a29d35e108aa6c10

--- manifests/site.pp
+++ manifests/site.pp
@@ -217,7 +217,10 @@ node 'antimony.wikimedia.org' {
 
 # irc.wikimedia.org
 node 'argon.wikimedia.org' {
-    role mw-rc-irc
+    role mw_rc_irc
+
+    include standard
+    include base::firewall
 }
 
 node 'baham.wikimedia.org' {
