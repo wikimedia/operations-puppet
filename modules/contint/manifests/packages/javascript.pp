ff20603e710a501882cc43a6f9555ed989836127
Antoine Musso <hashar@free.fr>
2015-10-09 22:00:53 +0200

    contint: install npm/grunt-cli with npm
    We require very specific versions of npm and grunt-cli on the CI slaves
    which are not matched by Precise/Trusty/Jessie Debian packages.
    
    Puppetize the installation described:
    https://wikitech.wikimedia.org/wiki/Nova_Resource:Integration/Setup
    
    Basically get npm from the distribution and use it to install pinned
    versions of npm/grunt-cli.
    Override the /usr/bin/npm link to point to the newly installed version.
    
    Bug: T113903
    Change-Id: I0d58b643ac677cba4b8000fbdaa936ee378263df

---
+++ modules/contint/manifests/packages/javascript.pp
@@ -0,0 +1,46 @@
+# == Class contint::packages::javascript
+#
+# Install npm/grunt-cli from npmjs.com with pinned versions
+#
+class contint::packages::javascript {
+
+    require_realms('labs')
+
+    package { 'npm':
+        ensure => present,
+    }
+
+    # DO NOT CHANGE VERSIONS
+    #
+    # https://wikitech.wikimedia.org/wiki/Nova_Resource:Integration/Setup
+    $versions = {
+        'npm'       => '2.7.6',
+        'grunt-cli' => '0.1.13',
+    }
+
+    $npm_options = $::lsbdistcodename ? {
+        # npm is terribly outdated
+        'precise' => '--ca=null',
+        default   => '',
+    }
+
+    exec { 'pin npm':
+        path    => '/usr/local/bin:/usr/bin',
+        command => "npm install ${npm_options} -g npm@${versions['npm']}",
+        onlyif  => "/usr/bin/test \"`/usr/local/bin/npm --version`\" != \"${versions['npm']}\"",
+        require => Package['npm'],
+    }
+
+    file { '/usr/bin/npm':
+        ensure  => link,
+        target  => '/usr/local/bin/npm',
+        require => Exec['pin npm'],
+    }
+
+    exec { 'pin grunt-cli':
+        command => "/usr/local/bin/npm install -g grunt-cli@${versions['grunt-cli']}",
+        onlyif  => "/usr/bin/test \"`/usr/local/bin/grunt --version`\" != \"grunt-cli v${versions['grunt-cli']}\"",
+        require => File['/usr/local/bin/npm'],
+    }
+
+}
