200be9947add82f8c4a0610d5991a6fcade516eb
YuviPanda <yuvipanda@gmail.com>
2015-10-09 19:09:51 -0700

    k8s: Use secret hieradata for managing users and tokens
    Change-Id: I7f590663206bdccf44b41d412d4b0c4422d17fb9

--- modules/k8s/manifests/apiserver.pp
+++ modules/k8s/manifests/apiserver.pp
@@ -13,16 +13,15 @@ class k8s::apiserver(
         mode   => '0700',
     }
 
+    $users = hiera('k8s_users')
     file { '/etc/kubernetes/tokenauth':
-        source => '/srv/kube-tokenauth',
-        owner  => 'kubernetes',
-        group  => 'kubernetes',
-        mode   => '0400',
-        notify => Base::Service_unit['kube-apiserver'],
+        content => template('k8s/tokenauth.csv.erb'),
+        owner   => 'kubernetes',
+        group   => 'kubernetes',
+        mode    => '0400',
+        notify  => Base::Service_unit['kube-apiserver'],
     }
 
-    $regular_users = hiera('k8s_regular_users')
-    $admin_users = hiera('k8s_admin_users')
     file { '/etc/kubernetes/abac':
         content => template('k8s/abac.json.erb'),
         owner   => 'kubernetes',
