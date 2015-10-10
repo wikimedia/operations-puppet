8277ad1e05d06eccd5e2da99e8baedf1dfc43521
YuviPanda <yuvipanda@gmail.com>
2015-10-09 19:36:34 -0700

    k8s: Pick up client password from secret hieradata
    Also make sure to restart kubelet and kubeproxy when the
    password changes
    
    Change-Id: I58a3c25bdb81197770175fffeca40aaad5ae66c1

--- modules/k8s/manifests/proxy.pp
+++ modules/k8s/manifests/proxy.pp
@@ -1,6 +1,7 @@
 class k8s::proxy(
     $master_host,
 ) {
+    include ::k8s::infrastructure_config
     require_package('kube-proxy')
 
     $master_ip = ipresolve($master_host, 4, $::nameservers[0])
@@ -8,6 +9,7 @@ class k8s::proxy(
     include k8s::users
 
     base::service_unit { 'kube-proxy':
-        systemd => true,
+        systemd   => true,
+        subscribe => File['/etc/kubernetes/kubeconfig'],
     }
 }
