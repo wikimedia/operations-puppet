6889ddcb88cb4e23f73912cbaee06270bbd7f0cd
Faidon Liambotis <faidon@wikimedia.org>
2015-10-12 21:41:36 -0400

    Remove classes snapshot::common, snapshot::packages
    Having a class snapshot whose sole contents is an "include
    snapshot::common" (and snapshot:common not being used anywhere else) is
    silly; merge those two into just the snapshot class.
    
    Moreover, snapshot::packages was also being used only by
    snapshot::common; inline that as well.
    
    Finally, don't include "standard" from the snapshot module, but move it
    into site.pp instead.
    
    Change-Id: Ie4e7999ce368364993cc3426b7c743c61e284bee

--- manifests/site.pp
+++ manifests/site.pp
@@ -2377,12 +2377,14 @@ node /^snapshot100[124]\.eqiad\.wmnet/ {
     role snapshot::common
     include snapshot
     include snapshot::dumps
+    include standard
 }
 
 node /^snapshot1003\.eqiad\.wmnet/ {
     role snapshot::common, snapshot::cron::primary
     include snapshot
     include snapshot::dumps
+    include standard
 }
 
 # codfw poolcounters
