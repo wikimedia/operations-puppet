4fb290ac46ea739a08a68aaa1eb9db71041a77f1
Faidon Liambotis <faidon@wikimedia.org>
2015-10-12 21:41:36 -0400

    Remove classes snapshot::common, snapshot::packages
    Having a class snapshot whose sole contents is an "include
    snapshot::common" (and snapshot:common not being used anywhere else) is
    silly; merge those two into just the snapshot class.
    
    Moreover, snapshot::packages was also being used only by
    snapshot::common; inline that as well.
    
    Finally, don't include "standard" from the snapshot module, but move it
    into site.pp instead.
    
    (these are by no means exhaustive of fixes in the snapshot module -- the
    snapshot module/role is all kinds of crazy and needs further cleanup)
    
    Change-Id: Ie4e7999ce368364993cc3426b7c743c61e284bee

--- manifests/site.pp
+++ manifests/site.pp
@@ -2475,6 +2475,8 @@ node 'stat1003.eqiad.wmnet' {
 }
 
 node /^snapshot100[1-4]\.eqiad\.wmnet/ {
+    include standard
+
     # NOTE: New snapshot hosts must also be manually added
     # to modules/dataset/files/exports. One must also manually
     # run `exportfs -r` on dataset1001. (T111586)
--- modules/snapshot/manifests/init.pp
+++ modules/snapshot/manifests/init.pp
@@ -1,3 +1,32 @@
 class snapshot {
-    include snapshot::common
+    include ::mediawiki
+    include mediawiki::nutcracker
+    include mediawiki::scap
+
+    require_package('mwbzutils')
+    require_package('mysql-client')
+    require_package('p7zip-full')
+    require_package('subversion')
+    require_package('utfnormal')
+
+    require_package('nfs-common')
+
+    file { [ '/mnt/data' ]:
+        ensure => 'directory',
+    }
+
+    $datasetserver = $::site ? {
+        'eqiad' => 'dataset1001.wikimedia.org',
+        default => 'dataset1001.wikimedia.org',
+    }
+
+    mount { '/mnt/data':
+        ensure   => 'mounted',
+        device   => "${datasetserver}:/data",
+        fstype   => 'nfs',
+        name     => '/mnt/data',
+        options  => 'bg,hard,tcp,rsize=8192,wsize=8192,intr,nfsvers=3',
+        require  => File['/mnt/data'],
+        remounts => false,
+    }
 }
