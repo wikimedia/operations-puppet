5bb6b43e82d88feadce516195ce6d359bfa6d892
Faidon Liambotis <faidon@wikimedia.org>
2015-10-12 21:37:13 -0400

    Inline class nfs::data to snapshot::common
    Ambiguously named class "nfs::data" is only used in one place: the
    snapshot::common class under the snapshot module. Inline it and remove
    it from nfs.pp.
    
    Change-Id: I0afebb5ca1e1b589295e8e59f1237856841e47c9

--- manifests/nfs.pp
+++ manifests/nfs.pp
@@ -2,29 +2,6 @@
 
 # Classes for NetApp mounts used on multiple servers
 
-class nfs::data {
-    require_package('nfs-common')
-
-    file { [ '/mnt/data' ]:
-        ensure => 'directory',
-    }
-
-        $datasetserver = $::site ? {
-            'eqiad' => 'dataset1001.wikimedia.org',
-            default => 'dataset1001.wikimedia.org',
-        }
-
-    mount { '/mnt/data':
-        ensure   => 'mounted',
-        device   => "${datasetserver}:/data",
-        fstype   => 'nfs',
-        name     => '/mnt/data',
-        options  => 'bg,hard,tcp,rsize=8192,wsize=8192,intr,nfsvers=3',
-        require  => File['/mnt/data'],
-        remounts => false,
-    }
-}
-
 class nfs::netapp::fr_archive(
         $ensure= 'mounted',
         $mountpoint= '/archive/udplogs'
