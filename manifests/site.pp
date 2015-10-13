47d5906e29b5e0045404225bd96bb15c51f722ed
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-10-13 20:57:35 +0200

    Move base:firewall include into the memcached role
    Change-Id: Ieb889b1feed47ad53a5fc8fa42bec289564ea957

--- manifests/site.pp
+++ manifests/site.pp
@@ -1778,7 +1778,6 @@ node /^mc(10[01][0-9])\.eqiad\.wmnet/ {
         ensure => 'directory',
     }
 
-    include base::firewall
     include redis
     include redis::ganglia
 }
@@ -1789,6 +1788,7 @@ node /^mc20[01][0-9]\.codfw\.wmnet/ {
     include redis
     include redis::ganglia
     include base::firewall
+
 }
 
 # OTRS evaluation upgrade
