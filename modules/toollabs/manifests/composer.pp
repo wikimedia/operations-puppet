a586635bdb02d8ddd7fb618d6576f4351cb05193
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2015-10-13 21:08:30 +0200

    toollabs: add composer to dev hosts
    Re-use the git repository used by the CI team.
    
    Bug: T104789
    Change-Id: I277d84cef3c9f0f05ca9fe96a780eae44d36386a

---
+++ modules/toollabs/manifests/composer.pp
@@ -0,0 +1,25 @@
+class toollabs::composer {
+    # based on contint::slave-scripts
+
+    file { '/srv/composer':
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    git::clone { 'composer':
+        ensure             => 'latest',
+        directory          => '/srv/composer',
+        origin             => 'https://gerrit.wikimedia.org/r/p/integration/composer.git',
+        recurse_submodules => true,
+        require            => File['/srv/composer'],
+    }
+
+    # Create a symlink for the composer executable
+    file { '/usr/local/bin/composer':
+        ensure => 'link',
+        target => '/srv/composer/vendor/bin/composer',
+        require => Git::Clone['composer'],
+    }
+}
