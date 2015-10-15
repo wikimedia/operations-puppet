b0d2eeacb5d749c3ca68250b9e283ee4c26940c2
Marko Obrovac <mobrovac@wikimedia.org>
2015-10-15 17:15:45 +0200

    RESTBase: make the domain to monitor configurable
    Up till now the domain to monitor for RESTBase was hard-coded into the
    restbase::monitoring class. However, with the creation of the second
    RESTBase cluster, AQS, we need to be able to configure it (via Hiera) as
    not all RESTBase instances expose the same domains. Concretely, AQS
    exposes only the analytics.wikimedia.org domain.
    
    Bug: T115588
    Change-Id: I1446fc10a76af6229f4858ee00df7d6ee5678f9f

--- modules/restbase/manifests/monitoring.pp
+++ modules/restbase/manifests/monitoring.pp
@@ -1,7 +1,9 @@
 # === Class restbase::monitoring
 #
 # For now, a rudimentary check for restbase
-class restbase::monitoring {
+class restbase::monitoring(
+    $monitor_domain = 'en.wikipedia.org',
+) {
     require ::restbase
 
     monitoring::service { 'restbase_http_root':
@@ -13,7 +15,7 @@ class restbase::monitoring {
     # Spec checking
     require service::monitoring
 
-    $monitor_url = 'http://127.0.0.1:7231/en.wikipedia.org/v1'
+    $monitor_url = "http://127.0.0.1:7231/${monitor_domain}/v1"
     nrpe::monitor_service { 'endpoints_restbase':
         description  => 'Restbase endpoints health',
         nrpe_command => "/usr/local/lib/nagios/plugins/service_checker -t 5 127.0.0.1 ${monitor_url}"
