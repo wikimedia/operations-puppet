92eb68515205511f87cd0eb3e3dca3caf7441c4b
Faidon Liambotis <faidon@wikimedia.org>
2015-10-16 08:37:36 -0400

    dataset: inline the non-role role classes
    Inline the classes
     - role::dataset::pagecountsraw
     - role::dataset::pagecounts_all_sites
     - role::dataset::mediacounts
    to role::dataset::primary, as:
     * they are not really role classes in the first place
     * they are only being used by role::dataset::primary
     * it makes for simpler and cleaner code
    
    Change-Id: I1e7e00549efff853eca3bd2ffc4c4da5f7cd22d2

--- manifests/role/dataset.pp
+++ manifests/role/dataset.pp
@@ -1,43 +1,5 @@
 # role classes for dataset servers
 
-class role::dataset::pagecountsraw($enable = true) {
-    class { '::dataset::cron::pagecountsraw':
-        enable  => $enable,
-        source  => 'stat1002.eqiad.wmnet::hdfs-archive/pagecounts-raw/*/*/',
-    }
-}
-
-# == Class role::dataset::pagecounts_all_sites
-#
-# NOTE: this requires that an rsync server
-# module named 'hdfs-archive' is configured on stat1002.
-#
-# This will make these files available at
-# http://dumps.wikimedia.org/other/pagecounts-all-sites/
-#
-class role::dataset::pagecounts_all_sites($enable = true) {
-    class { '::dataset::cron::pagecounts_all_sites':
-        source =>  'stat1002.eqiad.wmnet::hdfs-archive/pagecounts-all-sites',
-        enable => $enable,
-    }
-}
-
-# == Class role::dataset::mediacounts
-#
-# NOTE: this requires that an rsync server
-# module named 'hdfs-archive' is configured on stat1002.
-#
-# This will make these files available at
-# http://dumps.wikimedia.org/other/mediacounts/
-#
-class role::dataset::mediacounts($enable = true) {
-    class { '::dataset::cron::mediacounts':
-        source =>  'stat1002.eqiad.wmnet::hdfs-archive/mediacounts',
-        enable => $enable,
-    }
-}
-
-
 # a dumps primary server has dumps generated on this host; other directories
 # of content may or may not be generated here (but should all be eventually)
 # mirrors to the public should not be provided from here via rsync
@@ -54,19 +16,30 @@ class role::dataset::primary {
         'pagecounts' => true,
         'phab'       => true,
     }
+
     class { 'dataset':
         rsync   => $rsync,
         grabs   => $grabs,
         uploads => $uploads,
     }
-    class { 'role::dataset::pagecountsraw': enable => true }
 
-    class { 'role::dataset::pagecounts_all_sites':
-        enable => true,
+    # NOTE: these requires that an rsync server module named 'hdfs-archive' is
+    # configured on stat1002.
+
+    class { '::dataset::cron::pagecountsraw':
+        source => 'stat1002.eqiad.wmnet::hdfs-archive/pagecounts-raw/*/*/',
     }
 
-    class { 'role::dataset::mediacounts':
-        enable => true,
+    # This will make these files available at
+    # http://dumps.wikimedia.org/other/pagecounts-all-sites/
+    class { '::dataset::cron::pagecounts_all_sites':
+        source => 'stat1002.eqiad.wmnet::hdfs-archive/pagecounts-all-sites',
+    }
+    
+    # This will make these files available at
+    # http://dumps.wikimedia.org/other/mediacounts/
+    class { '::dataset::cron::mediacounts':
+        source => 'stat1002.eqiad.wmnet::hdfs-archive/mediacounts',
     }
 }
 
@@ -78,17 +51,14 @@ class role::dataset::secondary {
         'public' => true,
         'peers'  => true,
     }
-    $uploads = {
-#        'pagecounts' => true,
-#        'phab'       => true,
-    }
     $grabs = {
-#        'kiwix' => true,
     }
+    $uploads = {
+    }
+
     class { 'dataset':
         rsync   => $rsync,
         grabs   => $grabs,
         uploads => $uploads,
     }
-    class { 'role::dataset::pagecountsraw': enable => false }
 }
