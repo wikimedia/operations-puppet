a238d4b679d0700fd87e9496ccb53448d014b40a
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-10-16 22:41:28 +0200

    bromine: Move to using the role keyword
    We need this for Hiera-based assignment of Salt grains for debdeploy
    
    Change-Id: Ibaa8d2314a35a7eb04f620b9de3c00afc18a3b1f

--- manifests/site.pp
+++ manifests/site.pp
@@ -279,10 +279,8 @@ node /^(berkelium|curium)\.eqiad\.wmnet$/ {
 
 # virtual machine for static misc. services
 node 'bromine.eqiad.wmnet' {
+    role bugzilla_static, annualreport, transparency
     include standard
-    include role::bugzilla_static
-    include role::annualreport
-    include role::transparency
 }
 
 # http://releases.wikimedia.org
