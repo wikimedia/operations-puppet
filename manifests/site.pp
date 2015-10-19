a7ab6fe7d298f62bfccc8548ff7621f233b6a1f4
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-10-19 09:06:59 +0200

    install2001: Use the role keyword
    We need this for Hiera-based assignment of Salt grains for debdeploy
    
    Change-Id: Ic460bf010e7ab7891373aced13f9bf9607d859dc

--- manifests/site.pp
+++ manifests/site.pp
@@ -1129,6 +1129,7 @@ node 'graphite2001.codfw.wmnet' {
 }
 
 node 'install2001.wikimedia.org' {
+    role installserver::tftp-server
     $cluster = 'misc'
     $ganglia_aggregator = true
 
@@ -1137,7 +1138,6 @@ node 'install2001.wikimedia.org' {
     }
 
     include standard
-    include role::installserver::tftp-server
 
     class { 'ganglia::monitor::aggregator':
         sites =>  'codfw',
