8b74a72fb3c90d6dd7e184d04f6311d645149377
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-10-19 09:38:33 +0200

    tor: Use the role keyword
    We need this for Hiera-based assignment of Salt grains for debdeploy
    
    Change-Id: I75b115a1780b3849595d1723ff65cece25373808

--- manifests/site.pp
+++ manifests/site.pp
@@ -2266,9 +2266,10 @@ node /^pybal-test200[123]\.codfw\.wmnet$/ {
 
 # Tor relay
 node 'radium.wikimedia.org' {
+    role tor
+
     include base::firewall
     include standard
-    include role::tor
 
     interface::add_ip6_mapped { 'main':
         interface => 'eth0',
