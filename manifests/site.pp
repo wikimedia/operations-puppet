517d8603faa7aae0e1143c70f79035c263f8ef6b
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-10-19 09:19:25 +0200

    lithium: Use the role keyword
    We need this for Hiera-based assignment of Salt grains for debdeploy
    
    Change-Id: Id1d9e2551f07751d1f942d4127908cbade0fcc40

--- manifests/site.pp
+++ manifests/site.pp
@@ -1390,10 +1390,8 @@ node 'lead.wikimedia.org' {
 }
 
 node 'lithium.eqiad.wmnet' {
-
+    role backup::host, syslog::centralserver
     include standard
-    include role::backup::host
-    include role::syslog::centralserver
     include base::firewall
 }
 
