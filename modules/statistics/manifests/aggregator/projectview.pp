d2140935172b7574b31136440f8f744d0f4097ed
milimetric <dandreescu@wikimedia.org>
2015-10-19 16:42:30 -0400

    Aggregate from projectviews-*, not projectcounts-*
    Together with https://gerrit.wikimedia.org/r/#/c/247323/ and
    https://gerrit.wikimedia.org/r/#/c/246149/, this change solidifies the
    distinction between datasets computed using the old pageview definition
    and those computed using the new pageview definition:
    
    *counts*: old pageview definition
    *views*: new pageview definition
    
    Bug: T114379
    Change-Id: I9ca7c71324f0a354d5450239dd74700fcabd4fea

--- modules/statistics/manifests/aggregator/projectview.pp
+++ modules/statistics/manifests/aggregator/projectview.pp
@@ -54,8 +54,9 @@ class statistics::aggregator::projectview {
 
     # Cron for doing the basic aggregation step itself
     # Note that the --all-projects flag is set to compute aggregates across all projects.
+    # Note that the --output-pageviews flag is set to use input files that look like pageviews-*
     cron { 'aggregator projectview aggregate':
-        command => "log_file=\"${log_path}/`date +\\%Y-\\%m-\\%d--\\%H-\\%M-\\%S`.log\" && ${script_path}/bin/aggregate_projectcounts --source ${hdfs_source_path} --target ${data_path} --first-date=`date --date='-8 day' +\\%Y-\\%m-\\%d` --last-date=`date --date='-1 day' +\\%Y-\\%m-\\%d` --all-projects --push-target --log \${log_file} 2>> \${log_file}",
+        command => "log_file=\"${log_path}/`date +\\%Y-\\%m-\\%d--\\%H-\\%M-\\%S`.log\" && ${script_path}/bin/aggregate_projectcounts --source ${hdfs_source_path} --target ${data_path} --first-date=`date --date='-8 day' +\\%Y-\\%m-\\%d` --last-date=`date --date='-1 day' +\\%Y-\\%m-\\%d` --all-projects --output-pageviews --push-target --log \${log_file} 2>> \${log_file}",
         user    => $user,
         hour    => '13',
         minute  => '0',
