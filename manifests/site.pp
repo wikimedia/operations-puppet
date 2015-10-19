dd885d54405ca30a3514dd4211af7e2be1df566e
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-10-19 09:09:34 +0200

    krypton: Use the role keyword
    We need this for Hiera-based assignment of Salt grains for debdeploy
    
    Change-Id: I71928a325647fa737de13fd2c353582ac77436af

--- manifests/site.pp
+++ manifests/site.pp
@@ -1194,10 +1194,8 @@ node /kafka10(12|13|14|18|20|22)\.eqiad\.wmnet/ {
 
 # virtual machine for misc. PHP apps
 node 'krypton.eqiad.wmnet' {
+    role wikimania_scholarships, iegreview, grafana
     include standard
-    include role::wikimania_scholarships
-    include role::iegreview
-    include role::grafana
 }
 
 node 'labcontrol1001.wikimedia.org' {
