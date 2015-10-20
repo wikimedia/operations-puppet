1b4bde6062d9c0d3e6f8665be5cdb90c90aec8bc
Alex Monk <krenair@gmail.com>
2015-10-20 16:33:35 +0100

    beta: Use Let's Encrypt for upload, and new self-signed SSL certificate for most of text
    We could make this self-signed cert use SAN, so it actually matches the domains,
    but browsers still wouldn't trust it.
    
    There is an extremely nasty dependency bug hidden here - to get this working, you have to:
    * Enable acme_challenge in nginx config, restart nginx
    * Ensure it starts - e.g. you already have a valid cert
    * Apply the puppet patch
    * Run puppet
    * Run puppet again
    
    TODO: Find some way to template this mess of a subjects list.
    
    Bug: T50501
    Change-Id: I450316d795046ed49d057b221433ce9b172b9d6b

--- modules/role/manifests/cache/ssl/unified.pp
+++ modules/role/manifests/cache/ssl/unified.pp
@@ -15,14 +15,48 @@ class role::cache::ssl::unified {
         }
     }
     else {
+        $subjects = hiera('cache::cluster') ? {
+            'upload' => 'upload.beta.wmflabs.org',
+            'text'   => 'beta.wmflabs.org,bits.beta.wmflabs.org,www.wikipedia.beta.wmflabs.org,www.wikimedia.beta.wmflabs.org,commons.wikipedia.beta.wmflabs.org,commons.wikimedia.beta.wmflabs.org,deployment.wikimedia.beta.wmflabs.org,en.wikibooks.beta.wmflabs.org,en.wikinews.beta.wmflabs.org,en.wikiquote.beta.wmflabs.org,en.wikisource.beta.wmflabs.org,en.wikiversity.beta.wmflabs.org,en.wikivoyage.beta.wmflabs.org,en.wiktionary.beta.wmflabs.org,login.wikimedia.beta.wmflabs.org,meta.wikimedia.beta.wmflabs.org,test.wikimedia.beta.wmflabs.org,wikidata.beta.wmflabs.org,zero.wikimedia.beta.wmflabs.org,aa.wikipedia.beta.wmflabs.org,ar.wikipedia.beta.wmflabs.org,ca.wikipedia.beta.wmflabs.org,de.wikipedia.beta.wmflabs.org,en-rtl.wikipedia.beta.wmflabs.org,en.wikipedia.beta.wmflabs.org,eo.wikipedia.beta.wmflabs.org,es.wikipedia.beta.wmflabs.org,fa.wikipedia.beta.wmflabs.org,he.wikipedia.beta.wmflabs.org,hi.wikipedia.beta.wmflabs.org,ja.wikipedia.beta.wmflabs.org,ko.wikipedia.beta.wmflabs.org,ru.wikipedia.beta.wmflabs.org,simple.wikipedia.beta.wmflabs.org,sq.wikipedia.beta.wmflabs.org,uk.wikipedia.beta.wmflabs.org,zh.wikipedia.beta.wmflabs.org,commons.m.wikimedia.beta.wmflabs.org,deployment.m.wikimedia.beta.wmflabs.org,en.m.wikibooks.beta.wmflabs.org,en.m.wikinews.beta.wmflabs.org,en.m.wikiquote.beta.wmflabs.org,en.m.wikisource.beta.wmflabs.org,en.m.wikiversity.beta.wmflabs.org,en.m.wikivoyage.beta.wmflabs.org,en.m.wiktionary.beta.wmflabs.org,login.m.wikimedia.beta.wmflabs.org,meta.m.wikimedia.beta.wmflabs.org,test.m.wikimedia.beta.wmflabs.org,m.wikidata.beta.wmflabs.org,zero.m.wikimedia.beta.wmflabs.org,aa.m.wikipedia.beta.wmflabs.org,ar.m.wikipedia.beta.wmflabs.org,ca.m.wikipedia.beta.wmflabs.org,de.m.wikipedia.beta.wmflabs.org,en-rtl.m.wikipedia.beta.wmflabs.org,en.m.wikipedia.beta.wmflabs.org,eo.m.wikipedia.beta.wmflabs.org,es.m.wikipedia.beta.wmflabs.org,fa.m.wikipedia.beta.wmflabs.org,he.m.wikipedia.beta.wmflabs.org,hi.m.wikipedia.beta.wmflabs.org,ja.m.wikipedia.beta.wmflabs.org,ko.m.wikipedia.beta.wmflabs.org,ru.m.wikipedia.beta.wmflabs.org,simple.m.wikipedia.beta.wmflabs.org,sq.m.wikipedia.beta.wmflabs.org,uk.m.wikipedia.beta.wmflabs.org,zh.m.wikipedia.beta.wmflabs.org,aa.zero.wikipedia.beta.wmflabs.org,ar.zero.wikipedia.beta.wmflabs.org,ca.zero.wikipedia.beta.wmflabs.org,de.zero.wikipedia.beta.wmflabs.org,en-rtl.zero.wikipedia.beta.wmflabs.org,en.zero.wikipedia.beta.wmflabs.org,eo.zero.wikipedia.beta.wmflabs.org,es.zero.wikipedia.beta.wmflabs.org,fa.zero.wikipedia.beta.wmflabs.org,he.zero.wikipedia.beta.wmflabs.org,hi.zero.wikipedia.beta.wmflabs.org,ja.zero.wikipedia.beta.wmflabs.org,ko.zero.wikipedia.beta.wmflabs.org,ru.zero.wikipedia.beta.wmflabs.org,simple.zero.wikipedia.beta.wmflabs.org,sq.zero.wikipedia.beta.wmflabs.org,uk.zero.wikipedia.beta.wmflabs.org,zh.zero.wikipedia.beta.wmflabs.org'
+        }
+        $server_name = hiera('cache::cluster') ? {
+            'upload' => 'upload.beta.wmflabs.org',
+            'text'   => 'beta.wmflabs.org'
+        }
+
+        letsencrypt::cert::integrated { 'unified':
+            subjects   => $subjects,
+            puppet_svc => 'nginx',
+            system_svc => 'nginx',
+        }
+        file { '/etc/ssl/localcerts/unified.crt':
+            ensure => 'link',
+            target => '/etc/acme/cert/unified.crt',
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0640',
+        }
+        file { '/etc/ssl/private/unified.key':
+            ensure => 'link',
+            target => '/etc/acme/key/unified.key',
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0640',
+        }
         tlsproxy::localssl { 'unified':
-            server_name    => 'www.wikimedia.beta.wmflabs.org',
-            certs          => ['star.wmflabs.org'],
+            server_name    => $server_name,
+            certs          => ['unified'],
             default_server => true,
             do_ocsp        => false,
             skip_private   => true,
             upstream_port  => 3127,
             redir_port     => 8080,
+            from_puppet    => false,
+            acme_challenge => true,
+            require        => [
+                File['/etc/ssl/localcerts/unified.crt'],
+                File['/etc/ssl/private/unified.key'],
+            ],
         }
     }
 
