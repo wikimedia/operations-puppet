24b36130efbafd483d3b7e3022fb0910ee808d3f
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2015-10-20 16:28:59 -0700

    [WIP] burrow: Add new module for burrow
    Burrow(https://github.com/linkedin/burrow) is a consumer lag
    monitoring tool for Kafka, it will monitor the consumer groups on our
    Kafka clusters and send emails on their status.
    
    Bug: T115669
    Change-Id: Iaf6b2ee804df8f5e951fc7ced770b2565e3adb65

---
+++ manifests/role/analytics/burrow.pp
@@ -0,0 +1,20 @@
+# role/analytics/burrow.pp
+# Role classes for burrow, a consumer lag monitoring tool
+# for Kafka
+class role::analytics::burrow {
+
+    # Include the kafka config role to get all configuration data
+    include role::analytics::kafka::config
+
+    class {'::burrow':
+        ensure             => 'present',
+        client_id          => 'burrow-client-00',
+        zk_hosts           => $role::analytics::kafka::config['zookeeper_hosts'],
+        zk_path            => $role::analytics::kafka::config['zookeeper_chroot'],
+        kafka_cluster_name => $role::analytics::kafka::config['kafka_cluster_name'],
+        kafka_hosts        => $role::analytics::kafka::config['brokers_array'],
+        smtp_server        => 'polonium.wikimedia.org',
+        from_email         => "burrow@${::fqdn}",
+        to_emails          => 'madhuvishy@wikimedia.org, otto@wikimedia.org',
+    }
+}
