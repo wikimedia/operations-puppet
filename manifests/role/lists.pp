e2484515fba8fe4e9a48a7ab0fced09359463034
John F. Lewis <johnflewis93@gmail.com>
2015-10-20 17:22:43 +0100

    mailman: increase out queue to 300 check
    Bug: T114861
    Change-Id: Ie01411c627d17f5947505b517dabdfacb43a9116

--- manifests/role/lists.pp
+++ manifests/role/lists.pp
@@ -116,7 +116,7 @@ class role::lists {
 
     nrpe::monitor_service { 'mailman_queue':
         description   => 'mailman_queue_size',
-        nrpe_command  => '/usr/bin/sudo -u list /usr/local/lib/nagios/plugins/check_mailman_queue 25 25 200 25',
+        nrpe_command  => '/usr/bin/sudo -u list /usr/local/lib/nagios/plugins/check_mailman_queue 25 25 300 25',
     }
 
     # on list servers we monitor I/O with iostat
