e2f4100e72f4599bb67ee53343902a540e611b03
Max Semenik <maxsem.wiki@gmail.com>
2015-10-20 16:00:43 -0700

    Update safe browsing checks
    Bug: T116099
    Change-Id: I50e5de56b36ebe51fb42368c9a0a1381ba962c1b

--- modules/icinga/manifests/gsbmonitoring.pp
+++ modules/icinga/manifests/gsbmonitoring.pp
@@ -5,47 +5,47 @@ class icinga::gsbmonitoring {
 
     @monitoring::service { 'GSB_mediawiki':
         description   => 'check google safe browsing for mediawiki.org',
-        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=mediawiki.org/!\'This site is not currently listed as suspicious\'',
+        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=mediawiki.org/!\'Safe Browsing has not recently seen malicious content\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikibooks':
         description   => 'check google safe browsing for wikibooks.org',
-        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikibooks.org/!\'This site is not currently listed as suspicious\'',
+        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikibooks.org/!\'Safe Browsing has not recently seen malicious content\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikimedia':
         description   => 'check google safe browsing for wikimedia.org',
-        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikimedia.org/!\'This site is not currently listed as suspicious\'',
+        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikimedia.org/!\'Safe Browsing has not recently seen malicious content\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikinews':
         description   => 'check google safe browsing for wikinews.org',
-        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikinews.org/!\'This site is not currently listed as suspicious\'',
+        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikinews.org/!\'Safe Browsing has not recently seen malicious content\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikipedia':
         description   => 'check google safe browsing for wikipedia.org',
-        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikipedia.org/!\'This site is not currently listed as suspicious\'',
+        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikipedia.org/!\'Safe Browsing has not recently seen malicious content\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikiquotes':
         description   => 'check google safe browsing for wikiquotes.org',
-        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikiquotes.org/!\'This site is not currently listed as suspicious\'',
+        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikiquotes.org/!\'Safe Browsing has not recently seen malicious content\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikisource':
         description   => 'check google safe browsing for wikisource.org',
-        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikisource.org/!\'This site is not currently listed as suspicious\'',
+        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikisource.org/!\'Safe Browsing has not recently seen malicious content\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikiversity':
         description   => 'check google safe browsing for wikiversity.org',
-        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikiversity.org/!\'This site is not currently listed as suspicious\'',
+        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wikiversity.org/!\'Safe Browsing has not recently seen malicious content\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wiktionary':
         description   => 'check google safe browsing for wiktionary.org',
-        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wiktionary.org/!\'This site is not currently listed as suspicious\'',
+        check_command => 'check_http_url_for_string!www.google.com!/safebrowsing/diagnostic?site=wiktionary.org/!\'Safe Browsing has not recently seen malicious content\'',
         host          => 'google',
     }
 }
