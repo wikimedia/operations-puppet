534c1a92a7c11716fedcc05c0f54bcdc06f42290
Alex Monk <krenair@gmail.com>
2015-10-20 16:33:35 +0100

    beta: Use Let's Encrypt for upload, and new self-signed SSL certificate for most of text
    We could make this self-signed cert use SAN, so it actually matches the domains,
    but browsers still wouldn't trust it.
    
    There is an extremely nasty dependency bug hidden here - to get this working, you have to:
    * Enable acme_challenge in nginx config, restart nginx
    * Ensure it starts - e.g. you already have a valid cert
    * Apply the puppet patch
    * Run puppet
    * Run puppet again
    
    TODO: Tidy up text vs. upload split
    TODO: Deal with all of the remaining text domains
    
    Bug: T50501
    Change-Id: I450316d795046ed49d057b221433ce9b172b9d6b

--- modules/role/manifests/cache/ssl/unified.pp
+++ modules/role/manifests/cache/ssl/unified.pp
@@ -15,14 +15,77 @@ class role::cache::ssl::unified {
         }
     }
     else {
-        tlsproxy::localssl { 'unified':
-            server_name    => 'www.wikimedia.beta.wmflabs.org',
-            certs          => ['star.wmflabs.org'],
-            default_server => true,
-            do_ocsp        => false,
-            skip_private   => true,
-            upstream_port  => 3127,
-            redir_port     => 8080,
+        if ( hiera('cache::cluster') == 'upload' ) {
+            letsencrypt::cert::integrated { 'upload':
+                subjects   => 'upload.beta.wmflabs.org',
+                puppet_svc => 'nginx',
+                system_svc => 'nginx',
+            }
+            file { '/etc/ssl/localcerts/upload.crt':
+                ensure => 'link',
+                target => '/etc/acme/cert/upload.crt',
+                owner  => 'root',
+                group  => 'root',
+                mode   => '0640',
+            }
+            file { '/etc/ssl/private/upload.key':
+                ensure => 'link',
+                target => '/etc/acme/key/upload.key',
+                owner  => 'root',
+                group  => 'root',
+                mode   => '0640',
+            }
+            tlsproxy::localssl { 'unified':
+                server_name    => 'upload.beta.wmflabs.org',
+                certs          => ['upload'],
+                default_server => true,
+                do_ocsp        => false,
+                skip_private   => true,
+                upstream_port  => 3127,
+                redir_port     => 8080,
+                from_puppet    => false,
+                acme_challenge => true,
+                require        => [
+                    File['/etc/ssl/localcerts/upload.crt'],
+                    File['/etc/ssl/private/upload.key'],
+                ],
+            }
+        } else {
+            # TODO: Needs the ability to use a *LOT* of domains, no wildcards!
+            letsencrypt::cert::integrated { 'text':
+                subjects   => 'meta.wikimedia.beta.wmflabs.org,meta.m.wikimedia.beta.wmflabs.org,deployment.wikimedia.beta.wmflabs.org,deployment.m.wikimedia.beta.wmflabs.org',
+                puppet_svc => 'nginx',
+                system_svc => 'nginx',
+            }
+            file { '/etc/ssl/localcerts/text.crt':
+                ensure => 'link',
+                target => '/etc/acme/cert/text.crt',
+                owner  => 'root',
+                group  => 'root',
+                mode   => '0640',
+            }
+            file { '/etc/ssl/private/text.key':
+                ensure => 'link',
+                target => '/etc/acme/key/text.key',
+                owner  => 'root',
+                group  => 'root',
+                mode   => '0640',
+            }
+            tlsproxy::localssl { 'unified':
+                server_name    => 'deployment.wikimedia.beta.wmflabs.org',
+                certs          => ['text'],
+                default_server => true,
+                do_ocsp        => false,
+                skip_private   => true,
+                upstream_port  => 3127,
+                redir_port     => 8080,
+                from_puppet    => false,
+                acme_challenge => true,
+                require        => [
+                    File['/etc/ssl/localcerts/text.crt'],
+                    File['/etc/ssl/private/text.key'],
+                ],
+            }
         }
     }
 
