88ea067000da53b8f7c98191be65a629d200da70
Kartik Mistry <kartik.mistry@gmail.com>
2015-10-21 17:31:04 +0530

    cxserver: Add JWT token support
    Bug: T116134
    Change-Id: I338070364e5b723274c9627bfae42a1975011a9a

--- manifests/role/cxserver.pp
+++ manifests/role/cxserver.pp
@@ -7,9 +7,11 @@ class role::cxserver {
 
     include ::passwords::cxserver
     $yandex_api_key = $::passwords::cxserver::yandex_api_key
+    $jwt_secret = $::passwords::cxserver::jwt_secret
 
     class { '::cxserver':
         yandex_api_key => $yandex_api_key,
+        jwt_secret     => $jwt_secret,
     }
 
     ferm::service { 'cxserver_http':
