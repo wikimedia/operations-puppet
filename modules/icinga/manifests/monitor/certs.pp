f8e3bdd341c771e9f3f04f0551aa5e26f352960c
dzahn <dzahn@wikimedia.org>
2015-10-21 12:42:59 -0700

    icinga: ssl cert monitoring for external services
    Adds a virtual host in Icinga to add these SSL cert expiry
    checks to that are not associated with one of our nodes via
    a role class, because they live externally.
    
    This way we can still have monitoring, just needs to belong to something
    in Icinga.
    
    Adds a new class to Icinga and checks for blog.wikimedia.org and policy.wikimedia.org.
    Other special cases will follow-up when these work.
    
    Bug:T114059
    Change-Id: I7f97dda664aedefc391898308cae74ca5fb33f8a

---
+++ modules/icinga/manifests/monitor/certs.pp
@@ -0,0 +1,25 @@
+# = Class: icinga::monitor::certs
+# monitoring for SSL cert expiry for services that are not
+# associated with a single host via a role class but are either
+# external or live on a cluster.
+class icinga::monitor::certs {
+
+    @monitoring::host { 'virtual-ssl-host':
+        host_fqdn => 'icinga.wikimedia.org'
+    }
+
+    # blog.wikimedia.org (external, Wordpress/Automattic)
+    monitoring::service { 'https_blog':
+        description   => 'HTTPS-blog',
+        check_command => 'check_ssl_http!blog.wikimedia.org',
+        host          => 'virtual-ssl-host',
+    }
+
+    # policy.wikimedia.org (external, Wordpress/Automattic)
+    monitoring::service { 'https_policy':
+        description   => 'HTTPS-policy',
+        check_command => 'check_ssl_http!policy.wikimedia.org',
+        host          => 'virtual-ssl-host',
+    }
+
+}
