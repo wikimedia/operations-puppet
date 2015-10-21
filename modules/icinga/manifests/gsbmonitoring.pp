d0abb2593770c71f2425f51eab4d3ee69f4e0fab
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-10-21 21:01:53 +0300

    gsb: Another try at fixing it
    Need to quote the url now due to the & character in it
    
    Change-Id: I7eceb7977e0db1ca5bea31eb7c520f1afb6bcf51

--- modules/icinga/manifests/gsbmonitoring.pp
+++ modules/icinga/manifests/gsbmonitoring.pp
@@ -5,47 +5,47 @@ class icinga::gsbmonitoring {
 
     @monitoring::service { 'GSB_mediawiki':
         description   => 'check google safe browsing for mediawiki.org',
-        check_command => 'check_https_url_for_string!www.google.com!/safebrowsing/diagnostic?output=jsonp&site=mediawiki.org/!\'"type": 4\'',
+        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=mediawiki.org/\'!\'"type": 4\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikibooks':
         description   => 'check google safe browsing for wikibooks.org',
-        check_command => 'check_https_url_for_string!www.google.com!/safebrowsing/diagnostic?output=jsonp&site=wikibooks.org/!\'"type": 4\'',
+        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikibooks.org/\'!\'"type": 4\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikimedia':
         description   => 'check google safe browsing for wikimedia.org',
-        check_command => 'check_https_url_for_string!www.google.com!/safebrowsing/diagnostic?output=jsonp&site=wikimedia.org/!\'"type": 4\'',
+        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikimedia.org/\'!\'"type": 4\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikinews':
         description   => 'check google safe browsing for wikinews.org',
-        check_command => 'check_https_url_for_string!www.google.com!/safebrowsing/diagnostic?output=jsonp&site=wikinews.org/!\'"type": 4\'',
+        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikinews.org/\'!\'"type": 4\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikipedia':
         description   => 'check google safe browsing for wikipedia.org',
-        check_command => 'check_https_url_for_string!www.google.com!/safebrowsing/diagnostic?output=jsonp&site=wikipedia.org/!\'"type": 4\'',
+        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikipedia.org/\'!\'"type": 4\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikiquote':
         description   => 'check google safe browsing for wikiquote.org',
-        check_command => 'check_https_url_for_string!www.google.com!/safebrowsing/diagnostic?output=jsonp&site=wikiquote.org/!\'"type": 4\'',
+        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikiquote.org/\'!\'"type": 4\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikisource':
         description   => 'check google safe browsing for wikisource.org',
-        check_command => 'check_https_url_for_string!www.google.com!/safebrowsing/diagnostic?output=jsonp&site=wikisource.org/!\'"type": 4\'',
+        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikisource.org/\'!\'"type": 4\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikiversity':
         description   => 'check google safe browsing for wikiversity.org',
-        check_command => 'check_https_url_for_string!www.google.com!/safebrowsing/diagnostic?output=jsonp&site=wikiversity.org/!\'"type": 4\'',
+        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikiversity.org/\'!\'"type": 4\'',
         host          => 'google',
     }
     @monitoring::service { 'GSB_wiktionary':
         description   => 'check google safe browsing for wiktionary.org',
-        check_command => 'check_https_url_for_string!www.google.com!/safebrowsing/diagnostic?output=jsonp&site=wiktionary.org/!\'"type": 4\'',
+        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wiktionary.org/\'!\'"type": 4\'',
         host          => 'google',
     }
 }
