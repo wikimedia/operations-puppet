b94438097888ee98d03ded1376ba3803fba25614
dzahn <dzahn@wikimedia.org>
2015-10-21 12:51:52 -0700

    toolserver.org - add SSL cert expiry check
    Another one on the list for T114059.
    
    Change-Id: Ide5fa5f4466464514dc875eea742a13732dc829a

--- modules/icinga/manifests/monitor/certs.pp
+++ modules/icinga/manifests/monitor/certs.pp
@@ -1,14 +1,13 @@
 # = Class: icinga::monitor::certs
 # monitoring for SSL cert expiry for services that are not
 # associated with a single host via a role class but are either
-# external or live on a cluster.
+# external or live on a cluster. T114059
 class icinga::monitor::certs {
 
     # blog.wikimedia.org (external, Wordpress/Automattic)
     @monitoring::host { 'blog.wikimedia.org':
-        host_fqdn => 'blog.wikimedia.org'
+        host_fqdn     => 'blog.wikimedia.org'
     }
-
     monitoring::service { 'https_blog':
         description   => 'HTTPS-blog',
         check_command => 'check_ssl_http!blog.wikimedia.org',
@@ -17,9 +16,8 @@ class icinga::monitor::certs {
 
     # policy.wikimedia.org (external, Wordpress/Automattic)
     @monitoring::host { 'policy.wikimedia.org':
-        host_fqdn => 'policy.wikimedia.org'
+        host_fqdn     => 'policy.wikimedia.org'
     }
-
     monitoring::service { 'https_policy':
         description   => 'HTTPS-policy',
         check_command => 'check_ssl_http!policy.wikimedia.org',
@@ -28,12 +26,22 @@ class icinga::monitor::certs {
 
     # eventdonations.wikimedia.org (Fundraising)
     @monitoring::host { 'eventdonations.wikimedia.org':
-        host_fqdn => 'eventdonations.wikimedia.org'
+        host_fqdn     => 'eventdonations.wikimedia.org'
     }
     monitoring::service { 'https_eventdonations':
         description   => 'HTTPS-eventdonations',
         check_command => 'check_ssl_http!eventdonations.wikimedia.org',
         host          => 'eventdonations.wikimedia.org',
+    }
+
+    # toolserver.org (redirect page to Tool Labs)
+    @monitoring::host { 'www.toolserver.org':
+        host_fqdn     => 'www.toolserver.org'
+    }
+    monitoring::service { 'https_toolserver':
+        description   => 'HTTPS-toolserver',
+        check_command => 'check_ssl_http!www.toolserver.org',
+        host          => 'www.toolserver.org',
     }
 
 }
