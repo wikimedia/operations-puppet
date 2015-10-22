f02d298074049736e05653b4decfecfd333d8c2c
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-10-22 17:43:51 +0300

    icinga: Provide a check_gsb command and replace old commands
    A client for the API to check Google's safebrowsing lookup API. It needs
    a client_id and an API key and a URL as arguments. Populate it on the
    icinga server and create a check command to use with it. Use that
    command to replace the current gsb checks
    
    Bug: T116099
    
    Change-Id: Iad79713b9b952929b8286674a2c574566cfe310c

--- modules/icinga/manifests/gsbmonitoring.pp
+++ modules/icinga/manifests/gsbmonitoring.pp
@@ -1,51 +1,54 @@
 class icinga::gsbmonitoring {
+    $client_id = hiera('gsb::client_id')
+    $api_key = hiera('gsb::api_key')
+
     @monitoring::host { 'google':
         host_fqdn => 'google.com'
     }
 
     @monitoring::service { 'GSB_mediawiki':
         description   => 'check google safe browsing for mediawiki.org',
-        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=mediawiki.org/\'!\'"type": 4\'',
+        check_command => "check_google_safebrowsing!${client_id}!${api_key}!https://mediawiki.org",
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikibooks':
         description   => 'check google safe browsing for wikibooks.org',
-        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikibooks.org/\'!\'"type": 4\'',
+        check_command => "check_google_safebrowsing!${client_id}!${api_key}!https://wikibooks.org",
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikimedia':
         description   => 'check google safe browsing for wikimedia.org',
-        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikimedia.org/\'!\'"type": 4\'',
+        check_command => "check_google_safebrowsing!${client_id}!${api_key}!https://wikimedia.org.org",
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikinews':
         description   => 'check google safe browsing for wikinews.org',
-        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikinews.org/\'!\'"type": 4\'',
+        check_command => "check_google_safebrowsing!${client_id}!${api_key}!https://wikinews.org",
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikipedia':
         description   => 'check google safe browsing for wikipedia.org',
-        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikipedia.org/\'!\'"type": 4\'',
+        check_command => "check_google_safebrowsing!${client_id}!${api_key}!https://wikipedia.org",
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikiquote':
         description   => 'check google safe browsing for wikiquote.org',
-        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikiquote.org/\'!\'"type": 4\'',
+        check_command => "check_google_safebrowsing!${client_id}!${api_key}!https://wikiquote.org",
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikisource':
         description   => 'check google safe browsing for wikisource.org',
-        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikisource.org/\'!\'"type": 4\'',
+        check_command => "check_google_safebrowsing!${client_id}!${api_key}!https://wikisource.org",
         host          => 'google',
     }
     @monitoring::service { 'GSB_wikiversity':
         description   => 'check google safe browsing for wikiversity.org',
-        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wikiversity.org/\'!\'"type": 4\'',
+        check_command => "check_google_safebrowsing!${client_id}!${api_key}!https://wikiversity.org",
         host          => 'google',
     }
     @monitoring::service { 'GSB_wiktionary':
         description   => 'check google safe browsing for wiktionary.org',
-        check_command => 'check_https_url_for_string!www.google.com!\'/safebrowsing/diagnostic?output=jsonp&site=wiktionary.org/\'!\'"type": 4\'',
+        check_command => "check_google_safebrowsing!${client_id}!${api_key}!https://wiktionary.org",
         host          => 'google',
     }
 }
