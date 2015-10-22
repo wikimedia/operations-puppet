c3440e85aa2c92daa3e7e1cb508c97bb7e37f687
Andrew Otto <acotto@gmail.com>
2015-10-22 12:45:15 -0400

    The varnish reqstats diamond collector does not work, emit to statsd directly instead
    This also adds a main statsd hiera variable for general use.
    
    Bug: T83580
    Change-Id: Ife12f04c2b12f582a2338cc5791de1e75ef9114a

---
+++ modules/varnish/manifests/logging/reqstats.pp
@@ -0,0 +1,56 @@
+# == Define varnish::logging::reqstats:
+# Installs a Diamond collector that sends varnish request stats to statsd/graphite.
+#
+# === Parameters
+# [*instance_name*]
+#   Name of the varnish instance from which to gather request stats.
+#   Default: $name.
+#
+# [*interval*]
+#   Send counters to statsd every $interval seconds.  Default 60
+#
+# [*statsd*]
+#   StatsD server address, in "host:port" format.
+#   Defaults to localhost:8125
+#
+# [*metric_prefix*]
+#   Prefix of the stats that will be sent to statsd.
+#   Default: varnish.$site.$name.request.
+#   If $name is text.frontend,  this will create graphite keys that look like:
+#   servers.cp1052.varnish.eqiad.text.frontend.request.client.status.2xx
+#
+define varnish::logging::reqstats(
+    $instance_name = $name,
+    $interval      = 60,
+    $statsd        = 'localhost:8125',
+    $metric_prefix = "varnish.${::site}.${name}.request",
+    $ensure        = 'present',
+) {
+    if $instance_name {
+        $service_unit_name = "varnishreqstats-${instance_name}"
+    } else {
+        $service_unit_name = 'varnishreqstats-default'
+    }
+
+    if ! defined(File['/usr/local/bin/varnishreqstats']) {
+        file { '/usr/local/bin/varnishreqstats':
+            source  => 'puppet:///modules/varnish/varnishreqstats',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0555',
+            require => File['/usr/local/lib/python2.7/dist-packages/varnishlog.py'],
+            notify  => Service[$service_unit_name],
+        }
+    }
+
+    base::service_unit { $service_unit_name:
+        ensure         => present,
+        systemd        => true,
+        strict         => false,
+        template_name  => 'varnishreqstats',
+        require        => File['/usr/local/bin/varnishreqstats'],
+        service_params => {
+            enable => true,
+        },
+    }
+}
