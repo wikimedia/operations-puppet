8a920b7bf43107b047787dbc9a079563342bab8d
YuviPanda <yuvipanda@gmail.com>
2015-10-23 11:34:47 -0700

    scap: Allow nobelium to run scap
    Change-Id: Ib185430a5e42b17d5a20049084dc2455f39527a0

--- manifests/network.pp
+++ manifests/network.pp
@@ -257,6 +257,7 @@ class network::constants {
             [
                 '208.80.152.0/22',    # external
                 '2620:0:860::/46',    # all external previous was for silver
+                '10.64.37.14/32',     # nobelium, temporary mw install to copy over es indices
                 $all_network_subnets['production']['eqiad']['private']['private1-a-eqiad']['ipv4'],
                 $all_network_subnets['production']['eqiad']['private']['private1-a-eqiad']['ipv6'],
                 $all_network_subnets['production']['eqiad']['private']['private1-b-eqiad']['ipv4'],
