477fa05a5d35b47d211cd855fd988c8fd6f7933d
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-10-23 22:19:05 -0700

    Generate weekly cirrussearch dumps
    Bug: T109690
    Change-Id: I3e8c36d170ec4edabffa0d159afc3cc97ed4b774

---
+++ modules/snapshot/manifests/cirrusdumps.pp
@@ -0,0 +1,35 @@
+class snapshot::cirrusjson-dump(
+  $enable = true,
+  $user   = undef,
+) {
+  if ($enable == true) {
+    $ensure = 'present'
+  } else {
+    $ensure = 'absent'
+  }
+
+  system::role { 'snapshot::cirrussearch':
+    ensure      => $ensure,
+    description => 'producer of weekly cirrussearch json dumps'
+  }
+
+  $scriptPath = '/usr/local/bin/dumpcirrussearch.sh'
+  file { $scriptPath:
+    mode => '0755',
+    owner => 'root',
+    group => 'root',
+    source => 'puppet:///modules/snapshot/dumpcirrussearch.sh',
+  }
+
+  cron { 'cirrussearch-dump':
+    ensure      => $ensure,
+    command     => "${scriptPath} --config ${snapshot::dirs::dumpsdir}/confs/wikidump.conf",
+    environment => 'MAILTO=ops-dumps@wikimedia.org',
+    user        => $user,
+    minute      => '15',
+    hour        => '16',
+    weekday     => '1',
+    require     => File[$scriptPath],
+  }
+}
+
