5e2d93c6438b1c6773ebbeb1ea7e4f6c52244b73
cpettet <rush@wikimedia.org>
2015-10-26 10:08:41 -0500

    openstack: cleanup up old repo setups
    We are solid on trusty/kilo now
    
    Change-Id: Ice2e75e49887148f09296e012c0d418a9d35c5a9

--- modules/openstack/manifests/repo.pp
+++ modules/openstack/manifests/repo.pp
@@ -1,25 +1,13 @@
 class openstack::repo(
     $openstack_version=$::openstack::version,
 ) {
-
-    if ($::lsbdistcodename == 'precise') {
+    # As of 26/10/2015 we support kilo on trusty (lsb_release -c)
+    if ($::lsbdistcodename == 'trusty') {
         apt::repository { 'ubuntucloud':
             uri        => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-            dist       => "precise-updates/${openstack_version}",
+            dist       => "trusty-updates/${openstack_version}",
             components => 'main',
             keyfile    => 'puppet:///files/misc/ubuntu-cloud.key';
-        }
-    }
-    if ($::lsbdistcodename == 'trusty') {
-        # Icehouse is the default on trusty, no need to use the cloud archive.
-        if ($openstack_version != 'icehouse')
-        {
-            apt::repository { 'ubuntucloud':
-                uri        => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-                dist       => "trusty-updates/${openstack_version}",
-                components => 'main',
-                keyfile    => 'puppet:///files/misc/ubuntu-cloud.key';
-            }
         }
     }
 }
