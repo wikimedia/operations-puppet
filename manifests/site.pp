88624a808a0ac5aac6165250319582ee0ea4335a
dzahn <dzahn@wikimedia.org>
2015-10-26 20:59:03 -0700

    (WIP) maps: move roles into autoloader layout
    ERROR: role::maps not in autoload module layout on line 4
    ERROR: role::maps::master not in autoload module layout on line 45
    ERROR: role::maps::slave not in autoload module layout on line 90
    
    How do i move "role::maps"? Wouldn't that be init.pp?
    But that doesn't seem correct yet.
    
    Change-Id: I2a44ee9c026d4290b932d8ccac15410fb6d4b00e

--- manifests/site.pp
+++ manifests/site.pp
@@ -1739,11 +1739,11 @@ node 'magnesium.wikimedia.org' {
 }
 
 node 'maps-test2001.codfw.wmnet' {
-    role maps, maps::master
+    role maps::master
 }
 
 node /^maps-test200[2-4]\.codfw\.wmnet/ {
-    role maps, maps::slave
+    role maps::slave
 }
 
 node /^mc(10[01][0-9])\.eqiad\.wmnet/ {
