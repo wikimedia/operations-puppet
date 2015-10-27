99d4ba1ddffbd87959b38f2ca30bb31816329881
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-10-27 09:44:22 +0000

    cassandra: switch to monitor_systemd_unit_state
    cassandra has been switched to systemd units, switch also service monitoring
    
    Change-Id: I2c909cd1dd46b7d62952d4d07be82a5b6162c356

--- manifests/role/cassandra.pp
+++ manifests/role/cassandra.pp
@@ -18,13 +18,6 @@ class role::cassandra {
         description => 'Cassandra server',
     }
 
-    # Emit an Icinga alert unless there is exactly one Java process belonging
-    # to user 'cassandra' and with 'CassandraDaemon' in its argument list.
-    nrpe::monitor_service { 'cassandra':
-        description  => 'Cassandra database',
-        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u cassandra -C java -a CassandraDaemon',
-    }
-
     # CQL query interface monitoring (T93886)
     monitoring::service { 'cassandra-cql':
         description   => 'Cassandra CQL query interface',
