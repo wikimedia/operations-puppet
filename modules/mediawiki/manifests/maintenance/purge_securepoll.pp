1231cb73e94a2a4c32922c4f8779ca2d37420f02
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-10-29 08:31:27 +0100

    maintenance: run purge_securepoll once, not every minute for one hour
    Change-Id: Ia80c111419062eb07bb07c0e816b1c6bb6499874

--- modules/mediawiki/manifests/maintenance/purge_securepoll.pp
+++ modules/mediawiki/manifests/maintenance/purge_securepoll.pp
@@ -2,7 +2,8 @@ class mediawiki::maintenance::purge_securepoll( $ensure = present ) {
     cron { 'purge_securepollvotedata':
         ensure  => $ensure,
         user    => $::mediawiki::users::web,
-        hour    => '1',
+        hour    => 1,
+        minute  => 0,
         command => '/usr/local/bin/foreachwiki extensions/SecurePoll/cli/purgePrivateVoteData.php 2>&1 > /dev/null',
     }
 }
