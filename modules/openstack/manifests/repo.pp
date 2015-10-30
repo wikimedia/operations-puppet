dc6adf4e98492d8f1ba9a0cd70ec36b1636f5459
dzahn <dzahn@wikimedia.org>
2015-10-30 13:04:38 -0700

    openstack: some more lint fixes
    http://puppet-compiler.wmflabs.org/1156/
    
    Change-Id: I4707f12653df9100f56d7327244cdde725728667

--- modules/openstack/manifests/repo.pp
+++ modules/openstack/manifests/repo.pp
@@ -10,7 +10,9 @@ class openstack::repo(
             uri        => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
             dist       => "trusty-updates/${openstack_version}",
             components => 'main',
+            # lint:ignore:puppet_url_without_modules
             keyfile    => 'puppet:///files/misc/ubuntu-cloud.key';
+            # lint:endignore
         }
     }
 }
