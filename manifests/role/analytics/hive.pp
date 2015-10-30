1140e69afe7ccc9cc92c822a2ab8bffed0b6492c
Andrew Otto <acotto@gmail.com>
2015-10-30 10:56:47 -0400

    Increase hive-server2 heap size and update cdh submodule to allow this
    Bug: T76343
    Change-Id: Idcb9dd179cef35b23a2e9a65e27322812729899c

--- manifests/role/analytics/hive.pp
+++ manifests/role/analytics/hive.pp
@@ -93,9 +93,22 @@ class role::analytics::hive::server inherits role::analytics::hive::client {
     # MySQL Hive Metastore database class is applied.
     Package['mysql-server'] -> Class['cdh::hive::metastore::mysql']
 
+    # TODO: Set these better once hive is on its own server.
+    # See: https://phabricator.wikimedia.org/T110090
+    # http://www.cloudera.com/content/www/en-us/documentation/enterprise/latest/topics/cdh_ig_hive_install.html#concept_alp_4kl_3q_unique_1
+    # TODO: Use hiera.
+    $server_heapsize = $::realm ? {
+        'production' => 1024,
+        default      => undef,
+    }
+    $metastore_heapsize = $::realm ? {
+        'production' => 256,
+        default      => undef,
+    }
     # Setup Hive server and Metastore
     class { 'cdh::hive::master':
-        heapsize => '512',
+        server_heapsize    => $server_heapsize,
+        metastore_heapsize => $metastore_heapsize,
     }
 
     ferm::service{ 'hive_server':
