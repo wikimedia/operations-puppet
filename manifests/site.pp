091f2b2833d83cc67fb57bb35cd18faba0f4a42f
dzahn <dzahn@wikimedia.org>
2015-10-30 13:46:22 -0700

    tin,mira: move base::firewall to deployment role
    no diff in compiler:
    http://puppet-compiler.wmflabs.org/1143/
    
    Change-Id: Idb5e434ae495db7b151dc9623edacd497df09fba

--- manifests/site.pp
+++ manifests/site.pp
@@ -2426,7 +2426,6 @@ node 'tin.eqiad.wmnet', 'mira.codfw.wmnet' {
 
     role deployment::server, labsdb::manager
     include standard
-    include base::firewall
 
     interface::add_ip6_mapped { 'main':
         interface => 'eth0',
