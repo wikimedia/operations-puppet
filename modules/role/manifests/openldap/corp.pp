5bf965c8588c8fbff0cdd954a6dabc3dd0a4d37d
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-11-02 15:31:02 +0200

    role::openldap::corp: move into role module
    Long time coming
    
    Change-Id: I9bcfdce1b5bd413dd9d9a9b52ffcab411278b055

---
+++ modules/role/manifests/openldap/corp.pp
@@ -0,0 +1,39 @@
+# A class to setup the corp OIT LDAP mirror. This is used for cheap recipient
+# verification during email accept
+# vim: set ts=4 et sw=4:
+class role::openldap::corp {
+    include passwords::openldap::corp
+    include base::firewall
+
+    system::role { 'role::openldap::corp':
+        description => 'Corp OIT openldap Mirror server'
+    }
+
+    $master = 'ldap1.corp.wikimedia.org'
+    $sync_pass = $passwords::openldap::corp::sync_pass
+
+    sslcert::certificate { 'ldap-mirror.wikimedia.org': }
+
+    class { '::openldap':
+        server_id   => 3, # 1 and 2 used in OIT
+        suffix      => 'dc=corp,dc=wikimedia,dc=org',
+        datadir     => '/var/lib/ldap/corp',
+        master      => $master,
+        sync_pass   => $sync_pass,
+        ca          => '/etc/ssl/certs/ca-certificates.crt',
+        certificate => '/etc/ssl/localcerts/ldap-mirror.wikimedia.org.crt',
+        key         => '/etc/ssl/private/ldap-mirror.wikimedia.org.key',
+    }
+
+    ferm::service { 'corp_ldap':
+        proto  => 'tcp',
+        port   => '389', # Yes, explicitly not supporting LDAPS (port 636)
+        srange => '$ALL_NETWORKS',
+    }
+
+    monitoring::service { 'corp_ldap_mirror':
+        description   => 'Corp OIT LDAP Mirror ',
+        check_command => 'check_ldap!dc=corp,dc=wikimedia,dc=org',
+        critical      => true,
+    }
+}
