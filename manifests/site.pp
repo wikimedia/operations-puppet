8d66a98457b9fddc1a5f39ac83e6fec6bf159f05
Ori Livneh <ori@wikimedia.org>
2015-11-03 10:35:34 -0800

    Do not apply eventlogging and webperf roles on hafnium
    I will re-add them once the machine is reimaged.
    
    Change-Id: Ic1b6c9ff34535b95b77f7c1afec0ed66960ada0e

--- manifests/site.pp
+++ manifests/site.pp
@@ -1037,7 +1037,8 @@ node /^ganeti[12]00[0-9]\.(codfw|eqiad)\.wmnet$/ {
 # Hosts visualization / monitoring of EventLogging event streams
 # and MediaWiki errors.
 node 'hafnium.wikimedia.org' {
-    role eventlogging, webperf
+    # Roles disabled during re-image -- Ori, Nov 3 2015
+    # role eventlogging, webperf
 
     include standard
     include base::firewall
