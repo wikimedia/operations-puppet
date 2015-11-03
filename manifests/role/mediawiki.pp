329f19f9561e7afba17210447125b457ab1617bf
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-11-03 13:45:47 +0100

    ganglia: remove jobqueue stats
    Since we now have a better jobqueue dashboard on
    grafana (https://grafana.wikimedia.org/dashboard/db/job-queue-health)
    there is no point in reporting the same data in a different way to
    ganglia too.
    
    Change-Id: I596e56f275db64db148057aee746edb43e97748b

--- manifests/role/mediawiki.pp
+++ manifests/role/mediawiki.pp
@@ -171,20 +171,6 @@ class role::mediawiki::generic_monitoring {
         description   => 'Mediawiki Apple Dictionary Bridge',
         check_command => 'check_https_dictbridge',
     }
-
-    cron { 'all_jobqueue_length':
-        ensure  => present,
-        command => "/usr/bin/gmetric --name='Global JobQueue length' --type=int32 --conf=/etc/ganglia/gmond.conf --value=$(/usr/local/bin/mwscript extensions/WikimediaMaintenance/getJobQueueLengths.php --totalonly | grep -oE '[0-9]+') > /dev/null 2>&1",
-        user    => 'mwdeploy',
-    }
-
-    # duplicating the above job to experiment with gmetric's host spoofing so
-    # as to gather these metrics in a fake host called "www.wikimedia.org"
-    cron { 'all_jobqueue_length_spoofed':
-        ensure  => present,
-        command => "/usr/bin/gmetric --name='Global JobQueue length' --type=int32 --conf=/etc/ganglia/gmond.conf --spoof 'www.wikimedia.org:www.wikimedia.org' --value=$(/usr/local/bin/mwscript extensions/WikimediaMaintenance/getJobQueueLengths.php --totalonly | grep -oE '[0-9]+') > /dev/null 2>&1",
-        user    => 'mwdeploy',
-    }
 }
 
 # Class for a subgroup of appservers where we can test experimental features
