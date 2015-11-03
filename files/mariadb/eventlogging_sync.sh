e82adf24ed6e6d06939b14d4513185e2db19cae8
Jcrespo <jcrespo@wikimedia.org>
2015-11-03 12:13:23 +0100

    Migrate eventlogging_sync process from terbium to the slaves
    eventlogging_sync.sh is a custom-made replication substitution that
    has better performance than regular replication for append-only
    and purging-only operations. This is currenty being executed on
    terbium in a screen session, unpuppetized.
    
    Migrate the whole process to being controlled by puppet, and
    running on each slave instead, currently dbstore1002 and db1047.
    Basic monitoring for the process has been added, while there is
    lag monitoring planned for later.
    
    This requires stopping the processes on terbium before deploy.
    
    Change-Id: I5d5b9be6a9e34b1d5f90b8dfcc1744c025eeafef
    Ref: T116599

---
+++ files/mariadb/eventlogging_sync.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Event Logging tables hold time-series data which is not updated, but may be purged.
+# Every table includes an auto-inc id, a uuid, and a timestamp. The INSERT stream
+# does not currently take advantage of bulk insert batching which often leads to
+# replication lag. Adding DELETE into the mix makes it extra painful.
+#
+# This script sychronizes a slave using bulk inserts with mysqldump where id > N, and
+# purges old data with throttled bulk deletes where id < N order by id limit M.
+# Useful for creating a new slave, syncing a broken one, or replacing replication
+# with a cron job.
+
+while true; do
+## execute endlesly
+
+
+db='log'
+ls="regexp '^[A-Z0-9].*[0-9]+$'"
+mhost='m4-master.eqiad.wmnet'
+shost="localhost"
+
+slave="mysql -h $shost --compress --skip-column-names"
+master="mysql -h $mhost --compress --skip-column-names"
+dump="mysqldump -h $mhost --skip-opt --single-transaction --quick --skip-triggers"
+dumpdata="$dump --no-create-info --order-by-primary --insert-ignore --extended-insert --compress --hex-blob"
+querytables="select table_name from information_schema.tables where table_schema = '$db' and table_name"
+
+script=$(basename ${BASH_SOURCE})
+# Multi-execution is controlled by init.d
+#if [ $(ps ax | grep $script | grep -v grep | wc -l) -gt 2 ]; then
+#    echo "duplicate process" 1>&2
+#    exit 1
+#fi
+
+set -e
+
+for table in $($master $db -e "$querytables $ls"); do
+
+    echo -n "$shost $table"
+
+    if [ $($slave $db -e "$querytables = '$table'" | wc -l) -eq 0 ]; then
+        echo -n ", create"
+        $dump --no-data $db $table | $slave $db
+    fi
+
+    #id=$($master $db -e "select min(id) from \`$table\`")
+
+    #if [ ! $id = "NULL" ]; then
+        #echo -n ", purge < $id"
+        #$slave $db -e "delete from \`$table\` where id < $id order by id limit 100000"
+    #fi
+
+    ts=$($slave $db -e "select ifnull(max(timestamp),0) from \`$table\`")
+
+    echo -n " >= $ts"
+    # mysqldump has overhead with information_schema queries, so do a quick check for a noop
+    if [ ! $($master $db -e "select ifnull(max(timestamp),0) from \`$table\`") = $ts ]; then
+        echo -n " (rows!)"
+        $dumpdata --insert-ignore --where="timestamp >= '$ts'" $db "$table" | $slave $db
+        #$dumpdata --insert-ignore --where="timestamp >= '$ts'" $db "$table" >tmp/$table.sql
+    else
+        echo -n " (nothing)"
+    fi
+
+    echo " ok"
+
+done
+
+echo "Sleeping for 10 seconds before the next batch..."
+sleep 10
+
+## infinite loop
+done
