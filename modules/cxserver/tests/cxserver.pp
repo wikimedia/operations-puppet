4a6afc07d0bf374c6fbfc85d1f401274cbce51a8
Kartik Mistry <kartik.mistry@gmail.com>
2015-11-04 11:59:27 +0530

    service-runner migration for cxserver
    Bug: T117657
    Depends: Ia5e0950b314f54e10e50230af9a3761be6a8ee0a
    Change-Id: I72f84ec489686cd38f22dbfc150ec12f1d8dcf87

--- modules/cxserver/tests/cxserver.pp
+++
@@ -1,8 +0,0 @@
-class { 'cxserver':
-    base_path => '/tmp/cxserver/',
-    node_path => '/tmp/cxserver/node_modules',
-    conf_path => '/tmp/cxserver/config.js',
-    log_dir   => '/var/log/cxserver',
-    parsoid   => 'http://127.0.0.1',
-    apertium  => 'http://apertium-beta.wmflabs.org',
-}
