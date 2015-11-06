3f566359345caccda8c0001622fb1953ca03bce7
Hashar <hashar@free.fr>
2015-11-06 09:49:16 +0000

    Revert "Add libcurl-dev to Python Jenkins"
    This reverts commit ab5e658c61572170739d4e5e5d971871c1b6105c.
    
    Change-Id: I5e8cbb6af9b462bd9df1b0c39819f18c61e39580

--- modules/contint/manifests/packages/python.pp
+++ modules/contint/manifests/packages/python.pp
@@ -12,7 +12,6 @@ class contint::packages::python {
         'libxslt1-dev',  # For python lxml
         'libffi-dev', # For python requests[security]
         'libssl-dev', # python cryptography
-        'libcurl-dev', # For pycurl
     )
 
     # Bring in fresh pip. The Trusty package does not provide wheels cache
