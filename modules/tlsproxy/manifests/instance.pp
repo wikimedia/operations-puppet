3ca2f0f9673d0d9e971389a14e2f000a0b7b7365
Brandon Black <bblack@wikimedia.org>
2015-11-07 13:44:00 +0000

    set cache_misc to "mid" ciphersuite
    Rationale here is that "misc" services are not general-purpose
    production wikis: they are tooling used by employees and
    more-advanced volunteers.  We can set and/or expect a higher
    standard here since some of these services are fairly
    sercurity-sensitive anyways.
    
    Change-Id: Ib33432ddd40915b5e61a5cfc61cd5b79d865c985

--- modules/tlsproxy/manifests/instance.pp
+++ modules/tlsproxy/manifests/instance.pp
@@ -1,10 +1,12 @@
 # This defines the actual nginx daemon/instance which tlsproxy "sites" belong to
-class tlsproxy::instance {
+class tlsproxy::instance(
+    $ciphersuite = 'compat'
+) {
     # Tune kernel settings
     include webserver::sysctl_settings
 
     $nginx_worker_connections = '32768'
-    $nginx_ssl_conf = ssl_ciphersuite('nginx', 'compat')
+    $nginx_ssl_conf = ssl_ciphersuite('nginx', $ciphersuite)
 
     class { 'nginx': managed => false, }
 
