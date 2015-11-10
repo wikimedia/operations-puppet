3b9f7895f626421df2374ba582f2c7f2f559be0b
Tyler Cipriani <tcipriani@wikimedia.org>
2015-11-10 10:34:08 -0800

    RESTBase configuration for scap3 deployment
    Creates a RESTBase config class to allow configuration ownership to vary
    based on deployment method.
    
    Introduces the `$deployment` class parameter to the main `restbase`
    class. That parameter is used to determine:
    
      - Ownership of configuration files (via `restbase::config`)
      - Installation of scap (and dependencies on the scap target)
      - Creation and ownership of the
        `/srv/deployment/restbase/{deploy,deploy-cache}` directories
    
    Change-Id: I0e5b2a76edb45b0f21c42cd3ef2328307f0436dd

---
+++ modules/restbase/manifests/deploy/trebuchet.pp
@@ -0,0 +1,7 @@
+# == Class restbase::deploy::trebuchet
+#
+# Ensures that restbase target is setup correctly for deployment via trebuchet
+#
+class restbase::deploy::trebuchet {
+    include restbase::config
+}
