c047f8e680b7cb49831e77e014630add5dcf0f59
Jcrespo <jcrespo@wikimedia.org>
2015-11-10 13:29:30 +0100

    [WIP] Adding tendril::maintenance class
    This puppetizes the cron jobs that are currently running on
    terbium, related to refresh some statistics about tendril.
    
    Not finished yet, it requires managing of the configuration
    and mysql access.
    
    Change-Id: I8b5e6c6c0073804188ac562eb8839ce9157491ca

---
+++ modules/tendril/manifests/maintenance.pp
@@ -0,0 +1,76 @@
+class tendril::maintenance {
+
+    group { 'tcpircbot':
+        ensure  => present,
+        name    => 'tendril',
+    }
+
+    user { 'tcpircbot':
+        ensure     => present,
+        gid        => 'tendril',
+        shell      => '/bin/false',
+        home       => /tmp,
+        system     => true,
+        require    => Group['tendril'],
+    }
+
+    file { '/usr/local/bin/tendril-cron-5m.pl':
+        ensure => file,
+        owner  => 'tendril',
+        group  => 'tendril',
+        mode   => '0750',
+        source => 'puppet:///modules/tendril/tendril-cron-5m.pl',
+    }
+
+    file { '/var/log/tendril-cron-5m.log':
+        ensure => present,
+        owner  => 'tendril',
+        group  => 'tendril',
+        mode   => '0640',
+    }
+
+    file { '/usr/local/bin/tendril-queries.pl':
+        ensure => file,
+        owner  => 'tendril',
+        group  => 'tendril',
+        mode   => '0750',
+        source => 'puppet:///modules/tendril/tendril-queries.pl',
+    }
+
+    file { '/var/log/tendril-queries.log':
+        ensure => present,
+        owner  => 'tendril',
+        group  => 'tendril',
+        mode   => '0640',
+    }
+
+    file { '/var/log/tendril-queries.err':
+        ensure => present,
+        owner  => 'tendril',
+        group  => 'tendril',
+        mode   => '0640',
+    }
+    
+    cron { 'tendril-cron-5m':
+        ensure  => present,
+        user    => 'tendril',
+        minute  => '*/5',
+        command => '/usr/local/bin/tendril-cron-5m.pl > /var/log/tendril-cron-5m.log 2> /var/log/tendril-cron-5m.err',
+        require => [ File['/usr/local/bin/tendril-cron-5m.pl'],
+                     File['/var/log/tendril-cron-5m.log'],
+                     File['/var/log/tendril-cron-5m.err'],
+                   ]
+    }
+
+    cron { 'tendril-queries':
+        ensure  => present,
+        user    => 'tendril',
+        minute  => '*/15',
+        command => '',
+        require => [ File['/usr/local/bin/tendril-queries.pl'],
+                     File['/var/log/tendril-queries.log'],
+                     File['/var/log/tendril-queries.err'],
+                   ]
+
+    }
+
