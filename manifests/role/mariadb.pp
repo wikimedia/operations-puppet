518df0ee41ce2811581ff59cfbb3cd24027a8c84
Jcrespo <jcrespo@wikimedia.org>
2015-11-10 13:29:30 +0100

    Adding tendril::maintenance class
    This puppetizes the cron jobs that are currently running on
    terbium, related to refresh some statistics about tendril.
    
    This module has also been added to mariadb::maintenance,
    and this one is being applied to terbium (and temporarelly,
    to a separate mw host).
    
    Change-Id: I8b5e6c6c0073804188ac562eb8839ce9157491ca

--- manifests/role/mariadb.pp
+++ manifests/role/mariadb.pp
@@ -754,3 +754,20 @@ class role::mariadb::parsercache {
         rule => 'saddr 10.64.0.15 proto tcp dport (3306) ACCEPT;',
     }
 }
+
+class role::mariadb::maintenance {
+    # TODO: check if both of these are still needed
+    include mysql
+    package { 'percona-toolkit':
+        ensure => latest,
+    }
+
+    # place from which tendril-related cron jobs are run
+    include passwords::tendril
+
+    class { 'tendril::maintenance':
+        tendril_host => 'db1011.eqiad.wmnet',
+        tendril_user => 'watchdog',
+        tendril_password => $passwords::tendril::db_pass,
+    }
+}
