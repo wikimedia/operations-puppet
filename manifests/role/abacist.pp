de9e46e3473d2d49ed46e8dc2b81a10569ce3a6b
Ori Livneh <ori@wikimedia.org>
2015-11-11 18:27:59 -0800

    decom abacist role
    Set ensure => absent on the ::abcist class resource so that its sub-resources
    are purged from stat1001.
    
    Change-Id: Iaa994005905c841cfc65ae162aeaeb919a13b79d

--- manifests/role/abacist.pp
+++ manifests/role/abacist.pp
@@ -3,16 +3,8 @@
 # Abacist is a simple, redis-backed web analytics framework.
 #
 class role::abacist {
-    class { '::redis':
-        maxmemory => '1Gb',
-        before    => Class['::abacist'],
-    }
-
     class { '::abacist':
+        ensure                 => absent,
         eventlogging_publisher => 'tcp://eventlog1001.eqiad.wmnet:8600',
     }
-
-    # The redis server is only accessed from localhost (and monitoring), so
-    # no further ferm rules are needed
-    Service['redis-server'] ~> Service['abacist']
 }
