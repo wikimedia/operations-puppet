8205f6ef4c56f76a72a24b2da184d629bffb028c
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-11-12 14:12:46 +0100

    base::certificates: add puppet's CA to the trusted store
    Also, add a puppet_ssldir() function to unify the logic to pick the
    puppet ssl dir everywhere. Actually apply it only where it will be
    needed in the future.
    
    Bug: T114638
    Change-Id: I40e6673811b76aad4ea85d8e7b257e078baa7a8a

---
+++ modules/wmflib/lib/puppet/parser/functions/puppet_ssldir.rb
@@ -0,0 +1,53 @@
+# == Function puppet_ssldir( string $override = nil )
+#
+# Returns puppet's configured ssldir, using some heuristics.
+# This function is needed because we have a separate configurations
+# for the self-hosted puppetmasters ssl directory compared to the
+# standard setup. If we're ever able to simplify or remove such
+# differences, this function might become way simpler, or even
+# disappear.
+#
+# It's possible to override the heuristics and provide an override
+# parameter, which if set to 'master' will assume you are on a
+# self-hosted puppetmaster.
+#
+# == Examples
+#
+# # returns the default result based on the catalog
+# $ssldir = puppet_ssldir()
+# # Forces ssldir to be the one of a self-hosted puppetmaster
+# $ssldir = puppet_ssldir('master')
+#
+module Puppet::Parser::Functions
+  newfunction(:puppet_ssldir, :type => :rvalue) do |overrides|
+    # Check arguments
+    override = overrides[0]
+
+    fail("Only 'master', 'client' and undef " \
+         "are valid arguments of puppet_ssldir") unless
+        ['master', 'client', nil].include?override
+
+    default =  '/var/lib/puppet/ssl'
+    self_master = '/var/lib/puppet/server/ssl'
+    self_client = '/var/lib/puppet/client/ssl'
+
+    # Production uses the standard layout
+    return default if lookupvar('::realm') != 'labs'
+
+    # Since all self-hosted puppetmasters are in .eqiad.wmflabs, while
+    # the labs masters don't
+    return default if lookupvar('::settings::certname') =~ /\.wikimedia\.org$/ &&
+                      override.nil?
+
+
+    # Non-self-hosted puppetmasters all use the default ssldir
+    puppetmaster = lookupvar('puppetmaster')
+    puppetmaster ||= function_hiera(['role::puppet::self::master', ''])
+    if [lookupvar('hostname'), 'localhost', '', nil].include?puppetmaster ||
+       override == 'master'
+      self_master
+    else
+      self_client
+    end
+  end
+end
