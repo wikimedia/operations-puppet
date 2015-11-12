3a2d5afdf4e1528b671258b8862de17055ce1295
Tyler Cipriani <tcipriani@wikimedia.org>
2015-11-12 10:19:26 -0800

    Revert "deployment::server: move IPv6 int to role"
    This reverts commit f39aea8daab5d39655c712245c845dcffc965d29.
    Breaks deployment-bastion puppet in beta cluster.
    
    Bug: T118422
    Change-Id: Ieb481b7d383ba7779cd07ce7aee3598f900177f4

--- manifests/site.pp
+++ manifests/site.pp
@@ -2436,6 +2436,10 @@ node 'terbium.eqiad.wmnet' {
 # deployment servers
 node 'tin.eqiad.wmnet', 'mira.codfw.wmnet' {
     role deployment::server, labsdb::manager
+
+    interface::add_ip6_mapped { 'main':
+        interface => 'eth0',
+    }
 }
 
 # titanium hosts archiva.wikimedia.org
