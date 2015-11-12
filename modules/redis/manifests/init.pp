33c597e36f7670ebd68ba28d9bc14a4463872de7
Ori Livneh <ori@wikimedia.org>
2015-11-11 23:34:45 -0800

    redis: add doc-block
    Change-Id: Ie4fc5c8e416792198d7b9e7db12e1fdfe37cfb37

--- modules/redis/manifests/init.pp
+++ modules/redis/manifests/init.pp
@@ -1,9 +1,14 @@
+# == Class: redis
+#
+# Redis is an in-memory data store with support for rich data structures,
+# scripting, transactions, persistence, and high availability.
+#
 class redis {
     require_package('redis-server')
 
     file { '/srv/redis':
-        owner   => 'redis',
-        group   => 'redis',
-        mode    => '0755',
+        owner => 'redis',
+        group => 'redis',
+        mode  => '0755',
     }
 }
