ab5240a0a9baabb80b53f4f4b0a3c22ddde3c67d
YuviPanda <yuvipanda@gmail.com>
2015-11-11 22:35:52 -0800

    docker: Add role + class for setting up registry
    Change-Id: I7e99eb94d525fb758a8dc07f21d54cf858961dd4

---
+++ modules/docker/manifests/registry/init.pp
@@ -0,0 +1,43 @@
+class docker::registry(
+    $datapath = '/srv/registry',
+){
+
+    require_package('docker-distribution')
+    
+    $config = {
+        'version' => '0.1',
+        'storage' => {
+            'filesystem' => {
+                'rootdirectory' => $datapath,
+            },
+            'cache' => {
+                'blobdescriptor' => 'inmemory',
+            },
+        },
+        'http' => {
+            'addr' => ':5000',
+        },
+    }
+
+    # This is by default 0700 for some reason - nothing sensitive inside
+    # that doesn't have additional protection
+    file { '/etc/docker':
+        ensure => directory,
+        mode   => '0555',
+    }
+
+    file { '/etc/docker/registry/config.yml':
+        content => ordered_yaml($config),
+        owner   => 'docker-distributions',
+        group   => 'docker-distribution',
+        mode    => '0444',
+    }
+
+    service { 'docker-registry':
+        ensure  => running,
+        require => File[
+            '/etc/docker',
+            '/etc/docker/registry/config.yml'
+        ]
+    }
+}
