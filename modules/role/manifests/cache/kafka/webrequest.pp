d0f52aa5940ee20a8d73b745affe6e6be9b4b029
Brandon Black <bblack@wikimedia.org>
2015-11-13 13:10:32 +0000

    Add resp.http.X-Client-IP -> webrequest:client_ip
    Bug: T118557
    Change-Id: I50d04b776cb7661af06562d204e6d8e979fd0944

--- modules/role/manifests/cache/kafka/webrequest.pp
+++ modules/role/manifests/cache/kafka/webrequest.pp
@@ -23,7 +23,7 @@ class role::cache::kafka::webrequest(
         # Note: fake_tag tricks varnishkafka into allowing hardcoded string into a JSON field.
         # Hardcoding the $fqdn into hostname rather than using %l to account for
         # possible slip ups where varnish only writes the short hostname for %l.
-        format                       => "%{fake_tag0@hostname?${::fqdn}}x %{@sequence!num?0}n %{%FT%T@dt}t %{Varnish:time_firstbyte@time_firstbyte!num?0.0}x %{@ip}h %{Varnish:handling@cache_status}x %{@http_status}s %{@response_size!num?0}b %{@http_method}m %{Host@uri_host}i %{@uri_path}U %{@uri_query}q %{Content-Type@content_type}o %{Referer@referer}i %{X-Forwarded-For@x_forwarded_for}i %{User-Agent@user_agent}i %{Accept-Language@accept_language}i %{X-Analytics@x_analytics}o %{Range@range}i %{X-Cache@x_cache}o",
+        format                       => "%{fake_tag0@hostname?${::fqdn}}x %{@sequence!num?0}n %{%FT%T@dt}t %{Varnish:time_firstbyte@time_firstbyte!num?0.0}x %{@ip}h %{Varnish:handling@cache_status}x %{@http_status}s %{@response_size!num?0}b %{@http_method}m %{Host@uri_host}i %{@uri_path}U %{@uri_query}q %{Content-Type@content_type}o %{Referer@referer}i %{X-Forwarded-For@x_forwarded_for}i %{User-Agent@user_agent}i %{Accept-Language@accept_language}i %{X-Analytics@x_analytics}o %{Range@range}i %{X-Cache@x_cache}o %{X-Client-IP@client_ip}o",
         message_send_max_retries     => 3,
         # At ~6000 msgs per second, 500000 messages is over 1 minute
         # of buffering, which should be more than enough.
