f73cb4f61296f62ee7b2c05d2e9659dad5cb989f
Ori Livneh <ori@wikimedia.org>
2015-11-13 17:38:18 -0800

    Add redis::instance
    Round off a series of patches to make the redis module capable of provisioning
    multiple instances by finally a redis::instance resource, which uses systemd
    unit template files to provide a concise and integrated mechanism for
    provisioning and managing multiple redis instances.
    
    Temporarily provision two redis instances on pybal-test2003 in ganeti to test
    this.
    
    Bug: T100714
    Change-Id: I3419de0efd4123cb5e5a41e90d27b9f340383496

---
+++ modules/redis/manifests/instance.pp
@@ -0,0 +1,90 @@
+# == Define: redis::instance
+#
+# Provisions a Redis instance.
+#
+# === Parameters
+#
+# [*title*]
+#   Instance name. To avoid collisions, this value must either be a TCP
+#   port number or the absolute path to a UNIX socket. Defaults to the
+#   resource title.
+#
+# [*settings*]
+#   A map of Redis configuration directives to their desired value.
+#   See <http://redis.io/topics/config> for documentation.
+#
+# === Examples
+#
+#  # Configure a Redis instance on TCP port 6379:
+#  redis::instance { '6379':
+#    settings => { maxmemory => '2mb' },
+#  }
+#
+#  # Configure a Redis instance on UNIX socket:
+#  redis::instance { '/var/run/redis-primary.sock':
+#    settings => { maxmemory => '2mb' },
+#  }
+#
+#
+define redis::instance(
+    $ensure   = present,
+    $settings = {}
+) {
+    conflicts(Class['redis::legacy'])
+
+    validate_ensure($ensure)
+    validate_hash($settings)
+    validate_re($::initsystem, 'systemd')
+
+    include ::redis
+
+    # Disable the system-global redis service that the redis-server
+    # package configures by default.
+    if ! defined(Service['redis-server']) {
+        service { 'redis-server':
+            ensure    => stopped,
+            enable    => false,
+            subscribe => Package['redis-server'],
+        }
+    }
+
+    if $title =~ /^[1-9]\d*/ {
+        # Listen on TCP port
+        $instance_name = "tcp_${title}"
+        $port          = $title
+        $unixsocket    = ''
+    } elsif $title =~ /^\/.*\.sock/ {
+        # Listen on UNIX domain socket
+        $instance_name = sprintf('unix_%s', basename($title, '.sock'))
+        $port          = 0
+        $unixsocket    = $title
+    } else {
+        fail('redis::instance title must be a TCP port or absolute path to UNIX socket.')
+    }
+
+    $defaults = {
+        pidfile    => "/var/run/redis/${instance_name}.pid",
+        logfile    => "/var/log/redis/${instance_name}.log",
+        port       => $port,
+        unixsocket => $unixsocket,
+    }
+
+    file { "/etc/redis/${instance_name}.conf":
+        ensure  => $ensure,
+        content => template('redis/instance.conf.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+
+    file { "/etc/systemd/system/multi-user.target.wants/redis-instance@${instance_name}.service":
+        ensure => ensure_link($ensure),
+        target => '/lib/systemd/system/redis-instance@.service',
+    }
+
+    service { "redis-instance@${instance_name}":
+        ensure    => ensure_service($ensure),
+        provider  => 'systemd',
+        subscribe => "/etc/redis/${instance_name}.conf",
+    }
+}
