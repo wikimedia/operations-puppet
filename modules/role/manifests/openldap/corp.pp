49d73c88b6a7b6d797f6609be0c88d145719da4a
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-11-16 17:39:42 +0100

    WIP: labs openldap role
    Needs additional changes
    
    Change-Id: I9f4c97be1e9aab9f6acbcff0550c497b3746bf12

--- modules/role/manifests/openldap/corp.pp
+++ modules/role/manifests/openldap/corp.pp
@@ -41,3 +41,39 @@ class role::openldap::corp {
         critical      => true,
     }
 }
+
+class role::openldap::labs {
+    include passwords::openldap::labs
+    include base::firewall
+
+    system::role { 'role::openldap::labs':
+        description => 'LDAP servers for labs (based on OpenLDAP)'
+    }
+
+    # Certificate needs to be readable by slapd
+    sslcert::certificate { "ldap-labs.${::site}.wikimedia.org":
+        group => 'openldap',
+    }
+
+    class { '::openldap':
+        server_id   => 1, 
+        suffix      => 'dc=wikimedia,dc=org',
+        datadir     => '/var/lib/ldap/labs',
+        ca          => '/etc/ssl/certs/ca-certificates.crt',
+        certificate => "/etc/ssl/localcerts/ldap-labs.${::site}.wikimedia.org.crt",
+        key         => "/etc/ssl/private/ldap-labs.${::site}.wikimedia.org.key",
+    }
+
+    ferm::service { 'corp_ldap':
+        proto  => 'tcp',
+        port   => '389',
+        srange => '(($INTERNAL @resolve(ldap-eqiad.wikimedia.org) @resolve(ldap-codfw.wikimedia.org)))',
+        # TODO: Replace by assigned hostnames until the DNS aliases are flipped
+    }
+
+    monitoring::service { 'labs_ldap_check':
+        description   => 'Labs LDAP ',
+        check_command => 'check_ldap!dc=wikimedia,dc=org',
+        critical      => true,
+    }
+}
