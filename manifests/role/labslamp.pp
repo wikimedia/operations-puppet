f89f2702c4b50da3a16da838ab86ad2cdd4cbcfe
YuviPanda <yuvipanda@gmail.com>
2015-11-16 13:25:01 -0800

    labs: Add a simple LAMP role
    Need to deprecate the older and much worse LAMP role
    
    Change-Id: I8de60f13fbe8b95dda180b3b1f7926436d7b7aaa

--- manifests/role/labslamp.pp
+++ manifests/role/labslamp.pp
@@ -1,11 +1,30 @@
-#  Installs basic LAMP services on an instance:
+# = class: role::simplelamp
 #
-#  - Apache
-#  - Mysql
-#  - PHP5
-#
-#  The root mysql password is empty to start.  You should
-#  change it!
+# Sets up a simple LAMP server for use by arbitrary php applications
+class role::simplelamp(
+    $mysql_local = true,
+) {
+    include ::apache
+    include ::apache::mod::php5
+    include ::apache::mod::rewrite
+
+    require_package('php5-mysql', 'php5-cli')
+
+    $bind_address = $mysql_local ? {
+        true   => '127.0.0.1',
+        false  => '0.0.0.0',
+    }
+
+    # Simple mysql
+    class { '::mysql::server':
+        config_hash        => {
+            'datadir'      => '/srv/mysql',
+            'bind_address' => $bind_address,
+        }
+    }
+}
+
+# Deprecated older role that no longer works
 class role::lamp::labs {
 
     include role::labs-mysql-server
