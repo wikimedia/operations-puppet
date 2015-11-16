c4c5070bcc3c97098287fced7be488d9361b3dc5
Andrew Otto <acotto@gmail.com>
2015-11-16 17:03:56 -0500

    [WIP] Puppetize eventlogging-service with systemd in role::eventbus
    Attempting to deploy eventlogging for eventbus service via scap3
    
    TODO: Move role::eventbus::eventbus back to role::eventbus when
    T119042 works.
    
    Bug: T118780
    Change-Id: I621de844ed7a5bd1ac532b52058925350d9e5337

--- modules/beta/manifests/deployaccess.pp
+++ modules/beta/manifests/deployaccess.pp
@@ -1,3 +1,5 @@
+# TODO: remove this if https://phabricator.wikimedia.org/T121721
+# is fixed.
 class beta::deployaccess(
     $bastion_ip = '10.68.16.58', # ip of deployment-bastion
 ) {
@@ -5,6 +7,12 @@ class beta::deployaccess(
     security::access::config { 'beta-allow-mwdeploy':
         content  => "+ : deploy-service mwdeploy : ${bastion_ip}\n",
         priority => 50,
+    }
+
+    # Allow eventlogging user to deploy.
+    security::access::config { 'beta-allow-eventlogging':
+        content  => "+ : eventlogging : ${bastion_ip}\n",
+        priority => 51,
     }
 
 }
