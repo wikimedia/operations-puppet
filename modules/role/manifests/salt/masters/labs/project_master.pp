b857e21c2b5ac8a4936188a04d05e29ac4ee6037
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-11-16 18:13:59 +0200

    salt: Move the role manifests into role module
    Create the, rather badly named, salt role hierarchy classes manifest
    files in the role module. This will unblock Id1d3cfaa01
    
    Change-Id: I92c44cf77ab342e5b61a1320f5999277d8085fee

---
+++ modules/role/manifests/salt/masters/labs/project_master.pp
@@ -0,0 +1,27 @@
+# A salt master manages minions within a project
+class role::salt::masters::labs::project_master {
+
+    $salt_state_roots    = { 'base'=>['/srv/salt']}
+    $salt_file_roots     = { 'base'=>['/srv/salt']}
+    $salt_pillar_roots   = { 'base'=>['/srv/pillars']}
+    $salt_module_roots   = { 'base'=>['/srv/salt/_modules']}
+    $salt_returner_roots = { 'base'=>['/srv/salt/_returners']}
+
+    class { 'salt::master':
+        salt_runner_dirs    => ['/srv/runners'],
+                # For simplicity of test/dev we trust all of labs
+                # to run deploy module calls, but rely on security groups
+                # to secure this.
+        salt_peer_run       => {
+            '.*.eqiad.wmflabs' => ['deploy.*'],
+        },
+        salt_file_roots     => $salt_file_roots,
+        salt_pillar_roots   => $salt_pillar_roots,
+        salt_worker_threads => '10',
+        salt_state_roots    => $salt_state_roots,
+        salt_module_roots   => $salt_module_roots,
+        salt_returner_roots => $salt_returner_roots,
+        salt_auto_accept    => true,
+    }
+
+}
