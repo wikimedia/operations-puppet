3a04de2f2879440f6dab1136b49e52d614b67b51
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-11-16 17:39:42 +0100

    labs openldap role
    To be used for the replacement servers for neptunium / nembus.
    
    Change-Id: I9f4c97be1e9aab9f6acbcff0550c497b3746bf12

---
+++ modules/role/manifests/openldap/labs.pp
@@ -0,0 +1,37 @@
+# LDAP servers for labs (based on OpenLDAP)
+
+class role::openldap::labs {
+    include passwords::openldap::labs
+    include base::firewall
+
+    system::role { 'role::openldap::labs':
+        description => 'LDAP servers for labs (based on OpenLDAP)'
+    }
+
+    # Certificate needs to be readable by slapd
+    sslcert::certificate { "ldap-labs.${::site}.wikimedia.org":
+        group => 'openldap',
+    }
+
+    class { '::openldap':
+        server_id   => 1,
+        suffix      => 'dc=wikimedia,dc=org',
+        datadir     => '/var/lib/ldap/labs',
+        ca          => '/etc/ssl/certs/ca-certificates.crt',
+        certificate => "/etc/ssl/localcerts/ldap-labs.${::site}.wikimedia.org.crt",
+        key         => "/etc/ssl/private/ldap-labs.${::site}.wikimedia.org.key",
+    }
+
+    ferm::service { 'corp_ldap':
+        proto  => 'tcp',
+        port   => '389',
+        srange => '(($INTERNAL @resolve(seaborgium.wikimedia.org) @resolve(serpens.wikimedia.org)))',
+        # TODO: Replace with ldap-labs.eqiad.wikimedia.org and ldap-labs.codfw.wikimedia.org
+    }
+
+    monitoring::service { 'labs_ldap_check':
+        description   => 'Labs LDAP ',
+        check_command => 'check_ldap!dc=wikimedia,dc=org',
+        critical      => true,
+    }
+}
