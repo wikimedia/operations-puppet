261baec6144787b127aa4d7e88cbd5addda79495
YuviPanda <yuvipanda@gmail.com>
2015-11-16 16:17:57 -0800

    planet: Stop using webserver::php5
    Bug: T118786
    Change-Id: I5c124f5bc3dad1e5952cf4f59ccc5bdd382ece60

--- modules/planet/manifests/webserver.pp
+++ modules/planet/manifests/webserver.pp
@@ -1,14 +1,8 @@
 # sets up Apache for a planet-venus setup
 class planet::webserver {
-
-    class { 'webserver::php5': }
+    include ::apache
     include ::apache::mod::rewrite
     # so we can vary on X-Forwarded-Proto when behind misc-web
     include ::apache::mod::headers
-
-    # dependencies for webserver setup
-    Class['webserver::php5'] ->
-    Class['::apache::mod::rewrite'] ->
-    Class['::apache::mod::headers']
-
+    include ::apache::mod::php5
 }
