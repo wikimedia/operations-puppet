49487e32be3ef76d489c8601c940baf9187fb23e
YuviPanda <yuvipanda@gmail.com>
2015-11-16 17:24:37 -0800

    Put simplelamp/simplelap roles in autolayout
    Change-Id: I7b1dc45affec823bfae54b70f6b6f0f735394799

---
+++ manifests/role/simplelamp.pp
@@ -0,0 +1,25 @@
+# = class: role::simplelamp
+#
+# Sets up a simple LAMP server for use by arbitrary php applications
+class role::simplelamp(
+    $mysql_local = true,
+) {
+    include ::apache
+    include ::apache::mod::php5
+    include ::apache::mod::rewrite
+
+    require_package('php5-mysql', 'php5-cli')
+
+    $bind_address = $mysql_local ? {
+        true   => '127.0.0.1',
+        false  => '0.0.0.0',
+    }
+
+    # Simple mysql
+    class { '::mysql::server':
+        config_hash        => {
+            'datadir'      => '/srv/mysql',
+            'bind_address' => $bind_address,
+        }
+    }
+}
