991f16d149f2ee0162c4f2b5addfa708c330e563
YuviPanda <yuvipanda@gmail.com>
2015-11-16 16:19:13 -0800

    contint: Stop using webserver::php5
    Bug: T118786
    Change-Id: I58acf848d55f785cc9c23fd7366c905ff92d2589

--- modules/contint/manifests/proxy_common.pp
+++ modules/contint/manifests/proxy_common.pp
@@ -1,8 +1,8 @@
 # Basic configuration of Apache as a proxy
 class contint::proxy_common {
 
-  class {'webserver::php5': ssl => false; }
-
+  include ::apache
+  include ::apache::mod::php5
   include ::apache::mod::proxy
   include ::apache::mod::proxy_http
 
