6342d9257c7674b84525fb1f023a4a91e3ac51ad
YuviPanda <yuvipanda@gmail.com>
2015-11-16 16:49:58 -0800

    labs: Remove webserver::php5 from old labslamp
    Bug: T118786
    Change-Id: I80ebc5f0d3340c3c80fab81c1e2be742966584c1

--- manifests/role/labslamp.pp
+++ manifests/role/labslamp.pp
@@ -28,7 +28,8 @@ class role::simplelamp(
 class role::lamp::labs {
 
     include role::labs-mysql-server
-    include webserver::php5
+    include ::apache
+    include ::apache::mod::php5
     require_package('php5-mysql')
     require_package('php5-cli')
 
