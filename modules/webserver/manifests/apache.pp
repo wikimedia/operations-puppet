f87f3c3c6984f7f2da403e5b52a806e3534db126
YuviPanda <yuvipanda@gmail.com>
2015-11-16 17:26:58 -0800

    webserver: Remove webserver::apache:*
    All instances of webserver::apache in labs have been
    changed to use role::simplelap
    
    Bug: T118786
    Change-Id: I3c955aba28a71837564d739bd897c184b33a4dd3

--- modules/webserver/manifests/apache.pp
+++
@@ -1,9 +0,0 @@
-# New style attempt at handling misc web servers
-# - keep independent from the existing stuff
-class webserver::apache {
-
-    # Realize virtual resources for enabling virtual hosts
-    Webserver::Apache::Site <| |>
-
-    include webserver::sysctl_settings
-}
