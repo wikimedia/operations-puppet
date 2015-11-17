fc2698ee8a0811c18a28147b4faf4eb775e34029
YuviPanda <yuvipanda@gmail.com>
2015-11-16 22:47:05 -0800

    labs: Remove the biglogs class
    - Is redundant now since / is 20G with /var folded in, as opposed
      to something much smaller with a tiny 2G /var when this was
      introduced
    - It isn't used anymore: http://tools.wmflabs.org/watroles/role/role::labs::lvm::biglogs
      I cleaned out the two instances that were currently using this,
      and it shouldn't affect them - their current lvm volumes
      will stay intact, and new instances created will not need it
      anyway
    
    Change-Id: I2069a28aa21c3b79c5b1b41b56b45201442373c3

--- manifests/role/labsbiglogs.pp
+++
@@ -1,7 +0,0 @@
-# Allocate an 8G partition at /var/log for instances with noisy logs
-class role::labs::lvm::biglogs(
-    $size = '8G'
-) {
-    include labs_lvm
-    labs_lvm::extend { '/var/log': size => $size }
-}
