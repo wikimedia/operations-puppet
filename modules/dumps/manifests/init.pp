3f8eb5a8726af2abf2dff717abb4a4eff34e4674
Ariel T. Glenn <ariel@wikimedia.org>
2015-11-17 14:21:17 +0200

    keep fewer dataset web server logs, add date to filename
    this is preparatory to setting up rsync of logs elsewhere
    for analysis
    
    Bug: T118739
    Change-Id: I10fc7159e46ef6fa61d1704e9c30810073e7e05c

--- modules/dumps/manifests/init.pp
+++ modules/dumps/manifests/init.pp
@@ -4,7 +4,6 @@ class dumps {
     class { '::nginx':
         variant => 'extras',
     }
-
     $ssl_settings = ssl_ciphersuite('nginx', 'compat')
 
     nginx::site { 'dumps':
@@ -14,5 +13,13 @@ class dumps {
     nginx::site { 'download':
         source => 'puppet:///modules/dumps/nginx.download.conf',
         notify => Service['nginx'],
+    }
+
+    file { '/etc/logrotate.d/nginx':
+        source  => 'puppet:///modules/dumps/logrotate.conf',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        require => Package['nginx-extras'],
     }
 }
