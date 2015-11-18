e4afc190476f65ee8969ae1ab8bcdeca4ffa189a
YuviPanda <yuvipanda@gmail.com>
2015-11-17 22:33:20 -0800

    mattermost: Introduce module
    Super simple module for setting up a mattermost instance
    
    Change-Id: Icd17a0a1905819d4319446489d9565bd41905603

---
+++ manifests/role/mattermost.pp
@@ -0,0 +1,14 @@
+class role::mattermost {
+    class { '::mattermost::server':
+        sitename          => 'Wikimedia Mattermost (Testing)',
+        mysql_host        => '127.0.0.1',
+        mysql_user        => 'root',
+        mysql_password    => '',
+        mysql_db          => 'mattermost',
+        file_storage_path => '/srv/data',
+    }
+
+    class { '::mysql::server':
+        package_name => 'mariadb-server-10',
+    }
+}
