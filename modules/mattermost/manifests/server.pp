d26b508f6e5159272ec1932bd31ae0d48e030100
YuviPanda <yuvipanda@gmail.com>
2015-11-17 22:33:20 -0800

    mattermost: Introduce module
    Super simple module for setting up a mattermost instance
    
    Change-Id: Icd17a0a1905819d4319446489d9565bd41905603

---
+++ modules/mattermost/manifests/server.pp
@@ -0,0 +1,31 @@
+class mattermost::server(
+    $sitename,
+    $mysql_host,
+    $mysql_user,
+    $mysql_password,
+    $mysql_db,
+    $file_storage_path,
+    $allow_team_creation = false,
+    $allow_user_creation = true,
+) {
+    git::clone { 'operations/software/mattermost':
+        ensure    => present,
+        directory => '/srv/mattermost',
+        owner     => 'www-data',
+        group     => 'www-data',
+        mode      => '0555',
+    }
+
+    file { '/srv/mattermost/config/config.json':
+        ensure  => present,
+        owner   => 'www-data',
+        group   => 'www-data',
+        mode    => '0444',
+        content => template('mattermost/config.json.erb'),
+    }
+
+    base::service_unit { 'mattermost':
+        systemd => true,
+        require => Git::Clone['operations/software/mattermost'],
+    }
+}
