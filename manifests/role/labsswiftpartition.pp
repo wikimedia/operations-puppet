073b21250f4dfff1a5f0c0a1254da03d4ec2c6c4
YuviPanda <yuvipanda@gmail.com>
2015-11-19 02:11:25 -0800

    labs: Removed unused 'swiftpartition' role
    Change-Id: Iff6f3a6604b4769f332ebb1c6ffc99daf2d6d69a

--- manifests/role/labsswiftpartition.pp
+++
@@ -1,8 +0,0 @@
-class role::labs::lvm::swift {
-    include labs_lvm
-    labs_lvm::volume { 'second-local-disk':
-        mountat    => '/srv/swift-storage/swiftstore',
-        mountowner => 'swift',
-        mountgroup => 'swift',
-    }
-}
