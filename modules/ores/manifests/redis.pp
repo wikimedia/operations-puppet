a177de03f9e3a38d9081c719f40c617017017634
YuviPanda <yuvipanda@gmail.com>
2015-11-19 00:54:44 -0800

    ores: Move to using redis::instance
    Change-Id: I975f3f9d6cdb2842ddad47217f65bb3ff8ec2657

--- modules/ores/manifests/redis.pp
+++ modules/ores/manifests/redis.pp
@@ -1,10 +1,13 @@
 class ores::redis(
     $maxmemory
 ) {
-    class { '::redis::legacy':
-        maxmemory => $maxmemory,
-        redis_options  => {
-            'tcp-keepalive' => 60,
+    redis::instance { '6379':
+        settings => {
+            dir            => '/srv/redis',
+            maxmemory      => $maxmemory,
+            appendonly     => 'yes',
+            appendfilename => "${hostname}-6379.aof",
+            tcp_keepalive  => 60,
         }
     }
 }
