5866f4b91816719d42d02afdf2b2682bc961e1be
cpettet <rush@wikimedia.org>
2015-11-19 15:46:59 -0600

    nodepool add password include
    Change-Id: I3ded7509c8be64d5b26b6c243c47d3eb721ba5b8

--- manifests/role/nodepool.pp
+++ manifests/role/nodepool.pp
@@ -7,7 +7,7 @@
 class role::nodepool {
 
     system::role { 'role::nodepool': description => 'CI Nodepool' }
-
+    include passwords::nodepool
     $nova_controller = hiera('labs_nova_controller')
 
     # dib scripts
