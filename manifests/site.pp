3525d82be6a881fd10f71ba0ee9fb33ad0f80cd2
andrewbogott <abogott@wikimedia.org>
2015-11-20 14:02:15 -0600

    Rename holmium to labservices1002
    Depends-On: I3b96d37deb6a36a64445bcf9ceb39a575b18e5db
    bug: T106303
    Change-Id: I8e43e6a9c050d630c4e7e90913e7117ae95ee523

--- manifests/site.pp
+++ manifests/site.pp
@@ -1190,8 +1190,7 @@ node 'heze.codfw.wmnet' {
     include standard
 }
 
-# Holmium will soon be renamed labservices1002
-node 'holmium.wikimedia.org' {
+node 'labservices1002.wikimedia.org' {
     role labs::dns, labs::openstack::designate::server, labs::dnsrecursor
     include standard
     include base::firewall
