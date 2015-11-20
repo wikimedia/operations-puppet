f93f77587a0253aed2865e0e6ed0311a65c48ce9
Max Semenik <maxsem.wiki@gmail.com>
2015-11-20 13:07:59 -0800

    OSM replication for maps
    * Don't run updates as a PG superuser
    * Add support for --flat-nodes to reduce DB size
    * Make some things configurable:
     * Proxy
     * Number of threads and memory usage, to avoid overloading servers shared
       with Kartotherian
     * Zoom levels for which expiry files are generated
    * Make certain files/dirs world readable to assist debugging and because
      they have nothing secret anyway.
    
    Bug: T110262
    Change-Id: Id3a6264e1a069434221e62b9a292b34f5ff3e9d5

--- modules/osm/manifests/planet_sync.pp
+++ modules/osm/manifests/planet_sync.pp
@@ -4,6 +4,32 @@
 # This definition provides a way to sync planet_osm in a gis enabled db
 #
 # Parameters:
+#   $pg_password
+#      PostgreSQL password
+#   $ensure
+#      present or absent, just like for standard resources
+#   $osmosis_dir
+#      Directory that stores osmosis data, including replication state
+#   $expire_dir
+#      Directory for expiry files
+#   $period
+#      OSM replication interval: 'minute', 'hour' or 'day'
+#   $hour
+#      Hour for cronjob, format is the same as for cron resource
+#   $minute
+#      Minute for cronjob, format is the same as for cron resource
+#   $proxy
+#      Web proxy for accessing the outside of the cluster
+#   $flat_nodes
+#      Whether osm2pgsql --flat-nodes parameter should be used
+#   $expire_levels
+#      For which levels should expiry files be generated. Corresponds to
+#      osm2pgslq option -e and can be in format "<level>" or
+#      "<from level>-<to level>"
+#   $memory_limit
+#      Memory in megabytes osm2pgsql should occupy
+#   $num_threads
+#      Number of threads to use during sync
 #
 # Actions:
 #   sync with planet.osm
@@ -15,46 +41,69 @@
 # Sample Usage:
 #  osm::planet_sync { 'mydb': }
 define osm::planet_sync(
+                $pg_password,
+                $ensure=present,
                 $osmosis_dir='/srv/osmosis',
                 $expire_dir='/srv/osm_expire',
                 $period='minute',
                 $hour='*',
-                $minute='*/30'
+                $minute='*/30',
+                $proxy='webproxy.eqiad.wmnet:8080',
+                $flat_nodes=false,
+                $expire_levels='15',
+                $memory_limit=floor($::memoryfree_mb)/10,
+                $num_threads=$::processorcount,
 ) {
+    include ::osm::users
+
+    file { '/srv/downloads':
+        ensure => 'directory',
+        owner  => 'osmupdater',
+        group  => 'osm',
+        mode   => '0755',
+    }
 
     file { $expire_dir:
         ensure => directory,
-        owner  => 'postgres',
-        group  => 'postgres',
-        mode   => '0700',
+        owner  => 'osmupdater',
+        group  => 'osm',
+        mode   => '0755',
     }
 
     file { $osmosis_dir:
         ensure => directory,
-        owner  => 'postgres',
-        group  => 'postgres',
+        owner  => 'osmupdater',
+        group  => 'osm',
         mode   => '0755',
+    }
+
+    file { '/usr/local/bin/replicate-osm':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        content => template('osm/replicate-osm.erb'),
     }
 
     file { "${osmosis_dir}/configuration.txt":
         ensure  => present,
-        owner   => 'postgres',
-        group   => 'postgres',
-        mode    => '0400',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
         content => template('osm/osmosis_configuration.txt.erb'),
     }
 
-    $sync_planet_cmd = inline_template("<%- data=@memoryfree.split(' '); multi={'MB' => 1, 'GB' => 1000}[data[1]]-%>/usr/bin/osmosis --read-replication-interval workingDirectory=<%= @osmosis_dir %> --simplify-change --write-xml-change - 2>/dev/null | /usr/bin/osm2pgsql -k -s -C <%= data[0].to_i/10*multi %> --number-processes <%= @processorcount %> -e15 -o <%= @expire_dir %>/expire.list.$(date \"+\\%Y\\%m\\%d\\%H\\%M\") --append -")
     cron { "planet_sync-${name}":
-        environment => "JAVACMD_OPTIONS='-Dhttp.proxyHost=webproxy.eqiad.wmnet -Dhttp.proxyPort=8080'",
-        command     => "${sync_planet_cmd} > /tmp/osmosis.log 2>&1",
-        user        => 'postgres',
+        command     => '/usr/local/bin/replicate-osm > /tmp/osmosis.log 2>&1',
+        user        => 'osmupdater',
         hour        => $hour,
         minute      => $minute,
+        environment => "PGPASSWORD=${pg_password}",
     }
     cron { "expire_old_planet_syncs-${name}":
-        command => "/usr/bin/find ${expire_dir} -mtime +5 -exec rm {} \;",
-        user    => 'postgres',
+        ensure  => $ensure,
+        command => "/usr/bin/find ${expire_dir} -mtime +5 -exec rm {} \\;",
+        user    => 'osmupdater',
         hour    => $hour,
         minute  => $minute,
     }
