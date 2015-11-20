4f0dd0557edf2f4ac1a3f2fb1d336b025f4695dd
cpettet <rush@wikimedia.org>
2015-11-20 10:55:29 -0600

    Further hiera-ize role/labs/openstack/
    There is still some low hanging consolidation fruit
    left here.  This isn't the final form
    
    * moving values into yaml
    * moving private values into hiera private
    * using role keyword in site consistently (needed for lookup)
    * refactor class relationships based on the reduced
      need for classes as param stores
    
    Change-Id: Ib7080dce519c99c5368d01380c8708ec43c069ce

--- manifests/role/labs/openstack/glance.pp
+++ manifests/role/labs/openstack/glance.pp
@@ -1,46 +1,14 @@
-class role::labs::openstack::glance::config {
-
-    include passwords::openstack::glance
-    include passwords::labs::rabbitmq
-
-    $commonglanceconfig = {
-        db_name     => 'glance',
-        db_user     => 'glance',
-        db_pass     => $passwords::openstack::glance::glance_db_pass,
-        rabbit_user => $passwords::labs::rabbitmq::rabbit_userid,
-        rabbit_pass => $passwords::labs::rabbitmq::rabbit_password,
-    }
-}
-
-class role::labs::openstack::glance::config::eqiad inherits role::labs::openstack::glance::config {
-
-    include role::labs::openstack::keystone::config::eqiad
-
-    $keystoneconfig = $role::labs::openstack::keystone::config::eqiad::keystoneconfig
-    $keystone_host  = hiera('labs_keystone_host')
-    $db_host        = 'm5-master.eqiad.wmnet'
-    $bind_ip        = $::ipaddress_eth0
-    $auth_uri       = "http://${keystone_host}:5000"
-
-    $eqiadglanceconfig = {
-        db_host                => $db_host,
-        bind_ip                => $bind_ip,
-        auth_uri               => $auth_uri,
-        keystone_admin_token   => $keystoneconfig['admin_token'],
-        keystone_auth_host     => $keystoneconfig['bind_ip'],
-        keystone_auth_protocol => $keystoneconfig['auth_protocol'],
-        keystone_auth_port     => $keystoneconfig['auth_port'],
-    }
-    $glanceconfig = merge($eqiadglanceconfig, $commonglanceconfig)
-}
-
 class role::labs::openstack::glance::server {
 
-    include role::labs::openstack::glance::config::eqiad
+    $keystone_host   = hiera('labs_keystone_host')
+    $glanceconfig    = hiera_hash('glanceconfig', {})
+    $keystoneconfig  = hiera_hash('keystoneconfig', {})
 
-    $glanceconfig = $::site ? {
-        'eqiad' => $role::labs::openstack::glance::config::eqiad::glanceconfig,
-    }
+    $glanceconfig['auth_uri']               = "http://${keystone_host}:5000"
+    $glanceconfig['keystone_auth_host']     = ipresolve($keystone_host,4)
+    $glanceconfig['keystone_auth_port']     = $keystoneconfig['auth_port']
+    $glanceconfig['keystone_admin_token']   = $keystoneconfig['admin_token']
+    $glanceconfig['keystone_auth_protocol'] = $keystoneconfig['auth_protocol']
 
     class { 'openstack::glance::service':
         glanceconfig      => $glanceconfig,
