d4fc5013532aa0bec45a5b50c34e75b2026fa587
dzahn <dzahn@wikimedia.org>
2015-11-23 10:13:24 -0800

    magnesium: include standard
    standard was only included via the racktables
    role class, which i moved to krypton.
    
    so needs standard on the node level (or in the rt role)
    or it will not get Icinga monitoring and cause puppet
    errors on neon
    
    Change-Id: Iac7fba9222db97777f4f25e27dd015db2f9b13be

--- manifests/site.pp
+++ manifests/site.pp
@@ -1681,6 +1681,7 @@ node 'magnesium.wikimedia.org' {
     $cluster = 'misc'
 
     role requesttracker
+    include standard
 
     interface::add_ip6_mapped { 'main':
         interface => 'eth0',
