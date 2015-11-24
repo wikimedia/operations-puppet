0c8c52ed745c1c8d28f780a7a1e4462287af48fc
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-11-24 19:47:24 +0100

    etcd: auth puppettization [WiP]
    Add basic resources and providers to create and manage users and roles
    on etcd; also create a class to enable auth.
    
    Bug: T97972
    Change-Id: I84e5c8ef98ccabed9edfdb8bdaf072f74c0adb3e

---
+++ modules/etcd/lib/puppet/provider/etcd_role/default.rb
@@ -0,0 +1,47 @@
+require 'puppet'
+require 'json'
+
+Puppet::Type.type(:etcd_role).provide(:default) do
+
+  commands :etcd_cmd => 'etcd-manage'
+
+  @role_data = nil
+
+  # Calls etcd-manage, returns the result
+  def etcdrole(*args)
+    etcd_cmd(['role'] + args)
+  end
+
+  def exists?
+    if @role_data
+      return true
+    else
+      @role_data = JSON.load(etcdrole('get', resource[:name]))
+      return true
+    end
+  rescue Puppet::ExecutionFailure
+    return false
+  end
+
+  def create
+    acls = resource[:acls].map{ |k,v| "#{k}=#{v}" }
+    etcdrole('set', resource[:name], '--acls', *acls)
+  end
+
+  def destroy
+    return unless exists?
+    etcdrole('delete', resource[:name])
+    @role_data = nil
+  end
+
+  def acls
+    return unless exists?
+    @role_data['acls']
+  end
+
+  def acls=(value)
+    if value != acls
+      create
+    end
+  end
+end
---
+++ modules/etcd/lib/puppet/type/etcd_role.rb
@@ -0,0 +1,18 @@
+Puppet::Type.newtype(:etcd_role) do
+  @doc = "Puppet type to manage etcd roles."
+
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc "Name of the role on etcd"
+  end
+
+  newproperty(:acls) do
+    desc "Hash of paths and permissions to grant to the user"
+    def insync?(is)
+      should.each{ |k,v| should[k] = v.upcase }
+      is == should
+    end
+  end
+
+end
