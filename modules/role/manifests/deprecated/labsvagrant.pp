1f048e0926c0a50b21530e9d71da73af10599f20
YuviPanda <yuvipanda@gmail.com>
2015-11-24 14:36:37 -0800

    labsvagrant: Do not require secondary disk
    Is unneeded use of space...
    
    Change-Id: I903b76287d958584f5c06134ed4a3a4fbc7a8d95

--- modules/role/manifests/deprecated/labsvagrant.pp
+++ modules/role/manifests/deprecated/labsvagrant.pp
@@ -1,9 +1,5 @@
 # DEPRECATED, DO NOT USE!
 # Install Mediawiki-Vagrant puppet repo, manage manually from CLI
 class role::deprecated::labsvagrant {
-
-    # Mount secondary disk before applying labs_vagrant
-    class { '::labs_vagrant':
-        require => Class['::role::labs::lvm::srv'],
-    }
+    class { '::labs_vagrant':}
 }
