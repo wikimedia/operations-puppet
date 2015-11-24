29d59e3ee240730adee32a9d811cf08d0026f8e1
Ori Livneh <ori@wikimedia.org>
2015-11-24 12:42:44 -0800

    Migrate rdb1008 to redis::instance
    rdb1008 is currently a slave. MediaWiki can fail over to it but it requires
    uncommenting a line in jobqueue-eqiad.php. So this is safe.
    
    Bug: T100714
    Change-Id: I86cbeff3854e50a14dcea60a236f27b8b337d459

--- manifests/role/redisdb.pp
+++ manifests/role/redisdb.pp
@@ -29,16 +29,35 @@ class role::redisdb (
         srange => '$ALL_NETWORKS',
     }
 
-    class { '::redis::legacy':
-        maxmemory         => $maxmemory,
-        dir               => $dir,
-        persist           => 'aof',
-        redis_replication => $redis_replication,
-        password          => $passwords::redis::main_password,
-    }
+    if $::hostname == 'rdb1008' {
+        redis::instance { 6379:
+            settings => {
+                appendfilename              => "${::hostname}_6379.aof",
+                appendonly                  => true,
+                auto_aof_rewrite_min_size   => '512mb',
+                client_output_buffer_limit  => 'slave 512mb 200mb 60',
+                dbfilename                  => "${::hostname}_6379.rdb",
+                dir                         => '/srv/redis',
+                masterauth                  => $passwords::redis::main_password,
+                maxmemory                   => $maxmemory,
+                no_appendfsync_on_rewrite   => true,
+                requirepass                 => $passwords::redis::main_password,
+                save                        => '',
+                slave_read_only             => false,
+                slaveof                     => 'rdb1007 6379',
+                stop_writes_on_bgsave_error => false,
+            }
+        }
+    } else {
+        class { '::redis::legacy':
+            maxmemory         => $maxmemory,
+            dir               => $dir,
+            persist           => 'aof',
+            redis_replication => $redis_replication,
+        }
 
-    if $::standard::has_ganglia {
-        include redis::ganglia
+        if $::standard::has_ganglia {
+            include redis::ganglia
+        }
     }
-
 }
