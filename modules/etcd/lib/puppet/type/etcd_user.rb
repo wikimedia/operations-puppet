e03408506b958334601d193e7780c240fb74fdd8
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-11-24 19:47:24 +0100

    etcd: auth puppettization [WiP]
    Add basic resources and providers to create and manage users and roles
    on etcd; also create a class to enable auth.
    
    Bug: T97972
    Change-Id: I84e5c8ef98ccabed9edfdb8bdaf072f74c0adb3e

---
+++ modules/etcd/lib/puppet/type/etcd_user.rb
@@ -0,0 +1,29 @@
+require 'shellwords'
+
+Puppet::Type.newtype(:etcd_user) do
+  @doc = "Puppet type to manage etcd users"
+
+  # TODO: make ensure do the work
+  ensurable
+
+  newproperty(:name, :namevar => true) do
+    desc "Name of the user on etcd"
+  end
+
+  newparam(:password) do
+    desc "The password for the etcd user. Will NOT be updated once the user exists,
+         unless forcerefresh is set to true"
+  end
+
+  newparam(:forcerefresh) do
+    desc "Force a refresh of the password"
+    newvalues(:true, :false)
+    defaultto :false
+  end
+
+  newproperty(:roles, :array_matching => :all) do
+    desc "List of roles the user is member of"
+  end
+
+  # TODO: autorequire etcd roles
+end
