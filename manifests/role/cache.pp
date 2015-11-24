1c8d91c2ca4a74c02f612d79cb905828f5c859a3
Brandon Black <bblack@wikimedia.org>
2015-11-24 21:10:24 +0000

    add cache_misc monitor groups for other DCs
    Change-Id: Id668d0a98f8a8b1658f4d6480201c31587b096c5

--- manifests/role/cache.pp
+++ manifests/role/cache.pp
@@ -12,9 +12,12 @@
 @monitoring::group { 'cache_mobile_eqiad': description => 'eqiad mobile Varnish' }
 @monitoring::group { 'cache_mobile_esams': description => 'esams mobile Varnish' }
 @monitoring::group { 'cache_mobile_ulsfo': description => 'ulsfo mobile Varnish' }
-@monitoring::group { 'cache_parsoid_eqiad': description => 'Parsoid caches eqiad' }
 @monitoring::group { 'cache_parsoid_codfw': description => 'Parsoid caches codfw' }
+@monitoring::group { 'cache_parsoid_eqiad': description => 'Parsoid caches eqiad' }
+@monitoring::group { 'cache_misc_codfw': description => 'Misc caches codfw' }
 @monitoring::group { 'cache_misc_eqiad': description => 'Misc caches eqiad' }
+@monitoring::group { 'cache_misc_esams': description => 'Misc caches esams' }
+@monitoring::group { 'cache_misc_ulsfo': description => 'Misc caches ulsfo' }
 @monitoring::group { 'cache_maps_eqiad': description => 'Maps caches eqiad' }
 
 # If you're looking for something that used to be here, check modules/role/manifests/cache/...
