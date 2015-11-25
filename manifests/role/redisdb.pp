9a92495a7dc670b4a5e1ed88981ebf2c292e5b5a
Ori Livneh <ori@wikimedia.org>
2015-11-25 15:25:28 -0800

    Migrate rdb1007 to jobqueue_redis
    Change-Id: If438ab38415ceaccec92a50aac176a5841714882

--- manifests/role/redisdb.pp
+++ manifests/role/redisdb.pp
@@ -15,81 +15,19 @@ class role::redisdb (
 
     ferm::service { 'redis-server':
         proto  => 'tcp',
-        port   => '(6379 6380 6381)',
+        port   => '6379',
         srange => '$ALL_NETWORKS',
     }
 
-    $defaults = {
-        bind                        => '0.0.0.0',
-        appendonly                  => true,
-        auto_aof_rewrite_min_size   => '512mb',
-        client_output_buffer_limit  => 'slave 512mb 200mb 60',
-        dir                         => '/srv/redis',
-        masterauth                  => $passwords::redis::main_password,
-        maxmemory                   => '10Gb',
-        no_appendfsync_on_rewrite   => true,
-        requirepass                 => $passwords::redis::main_password,
-        save                        => '',
-        stop_writes_on_bgsave_error => false,
-        slave_read_only             => false,
+    class { '::redis::legacy':
+        maxmemory         => $maxmemory,
+        dir               => $dir,
+        persist           => 'aof',
+        redis_replication => $redis_replication,
+        password          => $passwords::redis::main_password,
     }
 
-    if $::hostname == 'rdb1008' {
-        redis::instance { 6379:
-            settings => merge($defaults, {
-                appendfilename              => "${::hostname}-6379.aof",
-                dbfilename                  => "${::hostname}-6379.rdb",
-                slaveof                     => 'rdb1007 6379',
-            }),
-        }
-
-        redis::instance { 6380:
-            settings => merge($defaults, {
-                appendfilename              => "${::hostname}-6380.aof",
-                dbfilename                  => "${::hostname}-6380.rdb",
-                slaveof                     => 'rdb1007 6380',
-            }),
-        }
-
-        redis::instance { 6381:
-            settings => merge($defaults, {
-                appendfilename              => "${::hostname}-6381.aof",
-                dbfilename                  => "${::hostname}-6381.rdb",
-                slaveof                     => 'rdb1007 6381',
-            }),
-        }
-    } elsif $::hostname == 'rdb1007' {
-        redis::instance { 6379:
-            settings => merge($defaults, {
-                appendfilename              => "${::hostname}-6379.aof",
-                dbfilename                  => "${::hostname}-6379.rdb",
-            }),
-        }
-
-        redis::instance { 6380:
-            settings => merge($defaults, {
-                appendfilename              => "${::hostname}-6380.aof",
-                dbfilename                  => "${::hostname}-6380.rdb",
-            }),
-        }
-
-        redis::instance { 6381:
-            settings => merge($defaults, {
-                appendfilename              => "${::hostname}-6381.aof",
-                dbfilename                  => "${::hostname}-6381.rdb",
-            }),
-        }
-    } else {
-        class { '::redis::legacy':
-            maxmemory         => $maxmemory,
-            dir               => $dir,
-            persist           => 'aof',
-            redis_replication => $redis_replication,
-            password          => $passwords::redis::main_password,
-        }
-
-        if $::standard::has_ganglia {
-            include redis::ganglia
-        }
+    if $::standard::has_ganglia {
+        include redis::ganglia
     }
 }
