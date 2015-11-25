8eeff1f642c2c6e4aeeed5de3f7f342eb6287f98
Brandon Black <bblack@wikimedia.org>
2015-11-25 03:34:43 +0000

    Remove unused r::c::1layer
    Change-Id: Iab27b1eb3340e8969f209ab867fa6386245957e4

--- modules/role/manifests/cache/1layer.pp
+++
@@ -1,8 +0,0 @@
-# To be included by all concrete 1layer cache roles
-class role::cache::1layer {
-    include role::cache::base
-
-    if $::role::cache::configuration::has_ganglia {
-        include varnish::monitoring::ganglia
-    }
-}
