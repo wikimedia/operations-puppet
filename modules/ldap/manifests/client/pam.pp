2bbae59c2ab48d2a9944437dc323f69a7e496db4
Coren <marc@uberbox.org>
2015-11-26 12:22:40 -0500

    Labs: switch PAM handling to use pam-auth-update
    This avoids having local modifications in /etc/pam.d that do
    not play nice with the distro-provided config (and prevents
    distro-specific breaking).  Rather than modify the pam.d
    config directly we add a wikimedia-labs specific configuration
    in /usr/share/pam-configs/ which is then merged properly
    by pam-auth-update.
    
    This changeset also includes an (ugly) exec stanza that
    reverts the manual changes that were done to /etc/pam.d/sshd
    which is not managed by pam-auth-update.
    
    Bug: T85910
    Change-Id: I1cf70b11c494ed010f14a3734b514dde36f6cf74

--- modules/ldap/manifests/client/pam.pp
+++ modules/ldap/manifests/client/pam.pp
@@ -9,27 +9,31 @@ class ldap::client::pam($ldapconfig) {
         mode  => '0444',
     }
 
-    file { '/etc/pam.d/common-auth':
-            source => 'puppet:///modules/ldap/common-auth',
+    #
+    # XXX: This forcicbly restores /etc/pam.d/sshd to the package's
+    #      version and makes a backup of the local changes. (This
+    #      uses the presence of that backup as a signal to not try
+    #      again).
+    #
+    #      That whole thing is obviously cruddy and causes a restart
+    #      of the sshd and so should almost certainly be removed as
+    #      quickly as we can once we are convinced that no locally
+    #      modified version remains
+    #
+    exec { 'restore-default-pamd-sshd':
+        creates => '/etc/pam.d/sshd.orig',
+        command => '/bin/mv /etc/pam.d/sshd /etc/pam.d/sshd.orig && ( sudo apt-get -o Dpkg::Options::="--force-confmiss" install --reinstall openssh-server || /bin/mv /etc/pam.d/sshd.orig /etc/pam.d/sshd'
     }
 
-    file { '/etc/pam.d/sshd':
-            source => 'puppet:///modules/ldap/sshd',
+    exec { 'pam-auth-update':
+        command     => '/usr/sbin/pam-auth-update --package --force',
+        refreshonly => true,
+        require     => Exec['restore-default-pamd-sshd'],
     }
 
-    file { '/etc/pam.d/common-account':
-            source => 'puppet:///modules/ldap/common-account',
+    file { '/usr/share/pam-configs/wikimedia-labs-pam':
+        source  => 'puppet:///modules/ldap/wikimedia-labs-pam',
+        notify  => Exec['pam-auth-update'],
     }
 
-    file { '/etc/pam.d/common-password':
-            source => 'puppet:///modules/ldap/common-password',
-    }
-
-    file { '/etc/pam.d/common-session':
-            source => 'puppet:///modules/ldap/common-session',
-    }
-
-    file { '/etc/pam.d/common-session-noninteractive':
-            source => 'puppet:///modules/ldap/common-session-noninteractive',
-    }
 }
