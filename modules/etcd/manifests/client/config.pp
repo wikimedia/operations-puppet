700448c8f6914ee09b4c756e12d4970ad14ba389
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-11-30 13:17:35 +0100

    etcd: add client configuration facility
    Since more and more programs will need access (direct or not) to etcd,
    we create a simple configuration format and puppet facilities to install
    basic configurations system-wide.
    
    The idea is that any node would not need to do more than
      include etcd::client::globalconfig
    
    to get an unauthenticated access working.
    
    Change-Id: I49c6e0ce0dfe6e30cfd504afd92e673f45b3708e

---
+++ modules/etcd/manifests/client/config.pp
@@ -0,0 +1,38 @@
+# === define etcd::client::config
+# Allows to create an etcd config file that can be read by our own clients
+#
+# This file is by default owned by root and will not be world-readable
+define etcd::client::config(
+    $ensure = present,
+    $owner = 'root',
+    $group = 'root',
+    $world_readable = false,
+    $settings = {
+        username => undef,
+        password => undef,
+        host     => undef,
+        port     => undef,
+        srv_dns  => undef,
+        ca_cert  => undef,
+        protocol => undef,
+    },
+    ) {
+
+    $dir_perms = $world_readable ? {
+        true    => '0755',
+        default => '0750',
+    }
+
+    $file_perms = $world_readable ? {
+        true    => '0444',
+        default => '0440',
+    }
+
+    file { $title:
+        ensure  => $ensure,
+        owner   => $owner,
+        group   => $group,
+        mode    => $file_perms,
+        content => oredered_yaml($settings),
+    }
+}
---
+++ modules/etcd/manifests/client/globalconfig.pp
@@ -0,0 +1,42 @@
+# class etcd::client::globalconfig
+# Simple class that installs a world-readable basic config for
+# etcd in yaml format. All our software that interacts with etcd
+# can understand the configuration from this file.
+class etcd::client::globalconfig(
+    $host = undef,
+    $port = undef,
+    $srv_dns = "${::site}.wmnet",
+    $protocol = 'https',
+    $ssl_dir = undef,
+    ) {
+
+    if $ssl_dir {
+        file { '/etc/etcd/ca.pem':
+            ensure => present,
+            owner  => root,
+            group  => root,
+            mode   => '0444',
+            source => "${ssl_dir}/certs/ca.pem",
+        }
+        $ca_cert = '/etc/etcd/ca.pem'
+    } else {
+        $ca_cert = undef
+    }
+
+    file { '/etc/etcd':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    etcd::client::config { '/etc/etcd/etcdrc':
+        world_readable => true,
+        settings       => {
+            host    => $host,
+            port    => $port,
+            srv_dns => $srv_dns,
+            ca_cert => $ca_cert,
+        },
+    }
+}
