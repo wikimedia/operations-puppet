228ee598bf73c0e0f73ef9584cae3a0053cfa80f
Ori Livneh <ori@wikimedia.org>
2015-12-01 10:52:00 -0800

    Configure Redis diamond collector to observe all jobqueue instances
    Make Diamond on the jobqueue redis backends collect stats about all three redis
    instances.
    
    Change-Id: I819ef7c4822037d4edc9514a84426369c8c8c99f

--- manifests/role/jobqueue_redis.pp
+++ manifests/role/jobqueue_redis.pp
@@ -1,11 +1,18 @@
 class role::jobqueue_redis {
     include ::standard
+    include ::passwords::redis
 
     system::role { 'role::jobqueue_redis': }
 
+    $password = $passwords::redis::main_password
     $slaveof = hiera('jobqueue_redis_slaveof', undef)
+    $instances = apply_format("localhost:%s/${password}", [ 6379, 6380, 6381 ])
 
     mediawiki::jobqueue_redis { 6379: slaveof => $slaveof }
     mediawiki::jobqueue_redis { 6380: slaveof => $slaveof }
     mediawiki::jobqueue_redis { 6381: slaveof => $slaveof }
+
+    diamond::collector { 'Redis':
+        settings => { instances => join($instances, ', ') }
+    }
 }
