58cdaba53909b31e5570ddd0231c3a3bf8d5db1b
YuviPanda <yuvipanda@gmail.com>
2015-11-30 17:37:59 -0800

    ores: Add paging icinga check for home page
    Bug: T119340
    Change-Id: Ie6bf0aafd70f882ef0b2c6750f657349050b063e

---
+++ modules/ores/manifests/monitoring.pp
@@ -0,0 +1,14 @@
+# Monitoing checks that live in icinga and page people
+class ores::monitoring {
+    # Paging checks!
+    @monitoring::host { 'ores.wmflabs.org':
+        host_fqdn => 'ores.wmflabs.org',
+    }
+
+    monitoring::service { 'main_page':
+        description    => 'ORES home page',
+        check_command  => 'check_http',
+        host           => 'ores.wmflabs.org',
+        contact_groups => 'ores',
+    }
+}
