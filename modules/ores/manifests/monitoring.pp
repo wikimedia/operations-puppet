feac59b1f06502c7b0717fd2e6d7e97a45c44407
dzahn <dzahn@wikimedia.org>
2015-11-30 19:38:13 -0800

    ores: disable new monitoring check
    For some reason the virtual host does not get created.
    
    Error: Could not find any host matching 'ores.wmflabs.org'
    
    This is strange because we do _the exact same thing_ in other
    places and it works just fine.
    
    We need to look at this more but until then i don't want to
    let icinga config be broken. It prevents any icinga reloads.
    
    Change-Id: I04e09a0bf2fc94e938ca20aea8090cfed92a52fd

--- modules/ores/manifests/monitoring.pp
+++ modules/ores/manifests/monitoring.pp
@@ -1,14 +1,15 @@
 # Monitoing checks that live in icinga and page people
 class ores::monitoring {
-    # Paging checks!
+
     @monitoring::host { 'ores.wmflabs.org':
         host_fqdn => 'ores.wmflabs.org',
     }
 
-    monitoring::service { 'ores_main_page':
-        description    => 'ORES home page',
-        check_command  => 'check_http',
-        host           => 'ores.wmflabs.org',
-        contact_group  => 'team-ores',
-    }
+#    monitoring::service { 'ores_main_page':
+#        description    => 'ORES home page',
+#        check_command  => 'check_http',
+#        host           => 'ores.wmflabs.org',
+#        contact_group  => 'team-ores',
+#    }
+
 }
