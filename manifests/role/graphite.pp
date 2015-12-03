4c66c7face24f052d644f3a1c5356e205335a54d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-12-03 08:48:58 +0100

    graphite::alerts::reqstats: improve alerts
    1) Remove the old global threshold alert, it's useless right now
    2) Raise the critical threshold to 1000; we don't realistically need to
    worry much if we're below that limit for any of these metrics
    3) Raise the warning threshold to 250 - this is now much lower than the
    critical one as we still want to be able to notice if something is being
    fishy; we just avoid spamming of notifications.
    4) Reduce the sampling to 10 minutes - I don't really see a reason not
    to be more reactive here
    
    Change-Id: I29f107ccf6c371b1fce69f872d67236e6737d616

--- manifests/role/graphite.pp
+++ manifests/role/graphite.pp
@@ -298,51 +298,44 @@ class role::graphite::alerts {
 
 class role::graphite::alerts::reqstats {
 
-    # Global threshold alarm as we had with reqstats.5xx
     # Monitor production 5xx rates
-    monitoring::graphite_threshold { 'reqstats-5xx-global':
-        description     => 'HTTP 5xx reqs/min (https://grafana.wikimedia.org/dashboard/db/varnish-http-errors)',
-        metric          => 'sumSeries(varnish.*.*.frontend.request.client.status.5xx.sum)',
-        warning         => 250,
-        critical        => 500,
-        from            => '15min',
-        nagios_critical => false,
-    }
+
+    $settings = {'warning' => 250, 'critical' => 1000, from => '10min'}
 
     # sites aggregates
     monitoring::graphite_threshold { 'reqstats-5xx-eqiad':
         description     => 'Eqiad HTTP 5xx reqs/min',
         metric          => 'sumSeries(varnish.eqiad.*.frontend.request.client.status.5xx.sum)',
-        warning         => 75,
-        critical        => 150,
-        from            => '15min',
+        warning         => $settings['warning'],
+        critical        => $settings['critical'],
+        from            => $settings['cron'],
         nagios_critical => false,
     }
 
     monitoring::graphite_threshold { 'reqstats-5xx-esams':
         description     => 'Esams HTTP 5xx reqs/min',
         metric          => 'sumSeries(varnish.esams.*.frontend.request.client.status.5xx.sum)',
-        warning         => 75,
-        critical        => 150,
-        from            => '15min',
+        warning         => $settings['warning'],
+        critical        => $settings['critical'],
+        from            => $settings['cron'],
         nagios_critical => false,
     }
 
     monitoring::graphite_threshold { 'reqstats-5xx-codfw':
         description     => 'Codfw HTTP 5xx reqs/min',
         metric          => 'sumSeries(varnish.codfw.*.frontend.request.client.status.5xx.sum)',
-        warning         => 75,
-        critical        => 150,
-        from            => '15min',
+        warning         => $settings['warning'],
+        critical        => $settings['critical'],
+        from            => $settings['cron'],
         nagios_critical => false,
     }
 
     monitoring::graphite_threshold { 'reqstats-5xx-ulsfo':
         description     => 'Ulsfo HTTP 5xx reqs/min',
         metric          => 'sumSeries(varnish.ulsfo.*.frontend.request.client.status.5xx.sum)',
-        warning         => 75,
-        critical        => 150,
-        from            => '15min',
+        warning         => $settings['warning'],
+        critical        => $settings['critical'],
+        from            => $settings['cron'],
         nagios_critical => false,
     }
 
@@ -350,54 +343,54 @@ class role::graphite::alerts::reqstats {
     monitoring::graphite_threshold { 'reqstats-5xx-text':
         description     => 'Text HTTP 5xx reqs/min',
         metric          => 'sumSeries(varnish.*.text.frontend.request.client.status.5xx.sum)',
-        warning         => 75,
-        critical        => 150,
-        from            => '15min',
+        warning         => $settings['warning'],
+        critical        => $settings['critical'],
+        from            => $settings['cron'],
         nagios_critical => false,
     }
 
     monitoring::graphite_threshold { 'reqstats-5xx-mobile':
         description     => 'Mobile HTTP 5xx reqs/min',
         metric          => 'sumSeries(varnish.*.mobile.frontend.request.client.status.5xx.sum)',
-        warning         => 75,
-        critical        => 150,
-        from            => '15min',
+        warning         => $settings['warning'],
+        critical        => $settings['critical'],
+        from            => $settings['cron'],
         nagios_critical => false,
     }
 
     monitoring::graphite_threshold { 'reqstats-5xx-uploads':
         description     => 'Uploads HTTP 5xx reqs/min',
         metric          => 'sumSeries(varnish.*.uploads.frontend.request.client.status.5xx.sum)',
-        warning         => 75,
-        critical        => 150,
-        from            => '15min',
+        warning         => $settings['warning'],
+        critical        => $settings['critical'],
+        from            => $settings['cron'],
         nagios_critical => false,
     }
 
     monitoring::graphite_threshold { 'reqstats-5xx-misc':
         description     => 'Misc HTTP 5xx reqs/min',
         metric          => 'sumSeries(varnish.*.misc.frontend.request.client.status.5xx.sum)',
-        warning         => 75,
-        critical        => 150,
-        from            => '15min',
+        warning         => $settings['warning'],
+        critical        => $settings['critical'],
+        from            => $settings['cron'],
         nagios_critical => false,
     }
 
     monitoring::graphite_threshold { 'reqstats-5xx-parsoid':
         description     => 'Parsoid HTTP 5xx reqs/min',
         metric          => 'sumSeries(varnish.*.parsoid.frontend.request.client.status.5xx.sum)',
-        warning         => 75,
-        critical        => 150,
-        from            => '15min',
+        warning         => $settings['warning'],
+        critical        => $settings['critical'],
+        from            => $settings['cron'],
         nagios_critical => false,
     }
 
     monitoring::graphite_threshold { 'reqstats-5xx-maps':
         description     => 'Maps HTTP 5xx reqs/min',
         metric          => 'sumSeries(varnish.*.maps.frontend.request.client.status.5xx.sum)',
-        warning         => 75,
-        critical        => 150,
-        from            => '15min',
+        warning         => $settings['warning'],
+        critical        => $settings['critical'],
+        from            => $settings['cron'],
         nagios_critical => false,
     }
 }
