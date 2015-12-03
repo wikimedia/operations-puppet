45e93e551f528d834dbd665025d95267c3255fa3
YuviPanda <yuvipanda@gmail.com>
2015-12-02 19:57:34 -0800

    puppet: Kill puppet::self::geoip
    Was a copy of puppetmaster::geoip with minor modifications,
    said modifications rolled into parameters now
    
    Bug: T120159
    Change-Id: Ib5cefcc78803013044e6f9b13be7c7029a269e72

--- modules/puppetmaster/manifests/geoip.pp
+++ modules/puppetmaster/manifests/geoip.pp
@@ -1,17 +1,46 @@
-class puppetmaster::geoip {
+class puppetmaster::geoip(
+    $fetch_private = true,
+    $use_proxy = true,
+) {
     # Fetch the GeoIP databases into puppet's volatile dir, so that other hosts
     # can then just sync that directory into their own /usr/share/GeoIP via a
     # normal puppet File resource (see the geoip module for more)
 
     $geoip_destdir = "${puppetmaster::volatiledir}/GeoIP"
-    $webproxy = "http://webproxy.${::site}.wmnet:8080"
 
     # geoip::data classes depend on this
     file { $geoip_destdir:
         ensure => directory,
     }
 
-    if $is_labs_puppet_master {
+    if $use_proxy {
+        $webproxy = "http://webproxy.${::site}.wmnet:8080"
+    } else {
+        $webproxy = undef
+    }
+
+    if $fetch_private {
+        # Fetch the proprietary paid-for MaxMind database
+        include passwords::geoip
+
+        class { 'geoip::data::maxmind':
+            data_directory => $geoip_destdir,
+            proxy          => $webproxy,
+            user_id        => $passwords::geoip::user_id,
+            license_key    => $passwords::geoip::license_key,
+            product_ids    => [
+                '106', # GeoIP.dat
+                '115', # GeoIPRegion.dat
+                '132', # GeoIPCity.dat
+                '133', # GeoIPCity.dat
+                '171', # GeoIPNetSpeed.dat
+                '177', # GeoIPNetSpeedCell.dat
+                'GeoIP2-City',
+                'GeoIP2-Connection-Type',
+                'GeoIP2-Country',
+                ],
+        }
+    } else {
         class { 'geoip::data::maxmind':
             data_directory => $geoip_destdir,
             proxy          => $webproxy,
@@ -41,27 +70,6 @@ class puppetmaster::geoip {
         file { "${geoip_destdir}/GeoIP2-City.mmdb":
             ensure => link,
             target => 'GeoLite2-City.mmdb',
-        }
-    } else {
-        # Fetch the proprietary paid-for MaxMind database
-        include passwords::geoip
-
-        class { 'geoip::data::maxmind':
-            data_directory => $geoip_destdir,
-            proxy          => $webproxy,
-            user_id        => $passwords::geoip::user_id,
-            license_key    => $passwords::geoip::license_key,
-            product_ids    => [
-                '106', # GeoIP.dat
-                '115', # GeoIPRegion.dat
-                '132', # GeoIPCity.dat
-                '133', # GeoIPCity.dat
-                '171', # GeoIPNetSpeed.dat
-                '177', # GeoIPNetSpeedCell.dat
-                'GeoIP2-City',
-                'GeoIP2-Connection-Type',
-                'GeoIP2-Country',
-                ],
         }
     }
 }
