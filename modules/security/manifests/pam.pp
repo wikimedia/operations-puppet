4da03b3e0ec97faf1e5e8407a4aa175c54b4bc56
Coren <marc@uberbox.org>
2015-12-03 10:17:03 -0500

    Add a new security module with ::pam and ::access
    This helps clean up security configuration mostly in labs right
    now, but provides general-use classes and defines to customize
    PAM configuration cleanly (including access.conf handling)
    
    This changeset does not replace current instances of manual
    handling of access.conf in the manifests, only creates the
    infrastructure to do so.
    
    Bug: T120106
    Change-Id: Id0183e2bc677c6d4893aeb2956c3e3650b174da6

---
+++ modules/security/manifests/pam.pp
@@ -0,0 +1,28 @@
+
+
+define security::pam::config(
+    $source = undef,
+    $contents = undef,
+)
+{
+    include security::pam::configs
+
+    file { "/usr/share/pam-configs/${name}":
+        ensure   => present,
+        source   => $source,
+        contents => $contents,
+        owner    => 'root',
+        group    => 'root',
+        mode     => '0444',
+        notify   => Exec['pam-auth-update'],
+    }
+}
+
+class security::pam::configs
+{
+    exec { 'pam-auth-update':
+        command     => '/usr/sbin/pam-auth-update --package',
+        refreshonly => true,
+    }
+}
+
