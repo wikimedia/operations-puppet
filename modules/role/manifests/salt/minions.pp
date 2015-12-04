c5355fb7152020bbd5467c62155084ebe4490047
YuviPanda <yuvipanda@gmail.com>
2015-12-03 19:36:56 -0800

    labs: Setup saltmaster separately from puppetmaster
    Change-Id: I748d3e54009f29776c08b20af209fa0e0b10f5ac

--- modules/role/manifests/salt/minions.pp
+++ modules/role/manifests/salt/minions.pp
@@ -4,7 +4,7 @@ class role::salt::minions(
     $salt_master_key = $::salt_master_key,
 ) {
     if $::realm == 'labs' {
-        $labs_master = hiera('puppetmaster')
+        $labs_master = hiera('saltmaster')
 
         $labs_finger   = 'c5:b1:35:45:3e:0a:19:70:aa:5f:3a:cf:bf:a0:61:dd'
         $master        = pick($salt_master, $labs_master)
