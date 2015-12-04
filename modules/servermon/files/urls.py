1d51321d48256101a21bffad426a94ef424b8174
Chad Horohoe <chadh@wikimedia.org>
2015-12-04 12:40:42 -0800

    servermon/urls.py: minor pep8 fixes
    Change-Id: I286e4b167e527a45093c1a3100ea003ee0ba8e23

--- modules/servermon/files/urls.py
+++ modules/servermon/files/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     (r'^/?$',           'servermon.projectwide.views.index'),
     (r'^hosts/$',       'servermon.updates.views.hostlist'),
     (r'^hosts/(.*)',    'servermon.updates.views.host'),
@@ -17,9 +18,9 @@ urlpatterns = patterns('',
     (r'^query/?$',      'servermon.puppet.views.query'),
     # Opensearch
     url(r'^opensearch.xml$', 'servermon.projectwide.views.opensearch',
-                            name="opensearch"),
+        name="opensearch"),
     url(r'^suggest/$', 'servermon.projectwide.views.suggest',
-                            name="opensearchsuggestions"),
+        name="opensearchsuggestions"),
     # Uncomment the next lines to enable the admin & hwdoc:
     (r'^admin/',        include(admin.site.urls)),
     # (r'^admin/doc/',    include('django.contrib.admindocs.urls')),
@@ -29,6 +30,6 @@ urlpatterns = patterns('',
 # Static files
 if settings.DEBUG:
     urlpatterns += patterns('django.views.static',
-    (r'^static/(?P<path>.*)$', 'serve',
-        {'document_root': settings.MEDIA_ROOT}),
-    )
+                            (r'^static/(?P<path>.*)$', 'serve',
+                                {'document_root': settings.MEDIA_ROOT}),
+                            )
