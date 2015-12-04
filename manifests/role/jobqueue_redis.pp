adc2843046a1a9f365d286f37ae953ef9d879040
Ori Livneh <ori@wikimedia.org>
2015-12-04 12:08:54 -0800

    Keep jobqueue aggregator and queue data on separate instances
    To make debugging the job queue easier, keep aggregator and queue data on
    separate redis instances. Create a new instance on port 6378 to serve as the
    aggregator redis instance.
    
    Change-Id: I35c0188fe9e5f13fa7ca57159b8ff8eb430c751c

--- manifests/role/jobqueue_redis.pp
+++ manifests/role/jobqueue_redis.pp
@@ -6,8 +6,12 @@ class role::jobqueue_redis {
 
     $password = $passwords::redis::main_password
     $slaveof = hiera('jobqueue_redis_slaveof', undef)
-    $instances = apply_format("localhost:%s/${password}", [ 6379, 6380, 6381 ])
+    $instances = apply_format("localhost:%s/${password}", range(6378, 6382))
 
+    # Aggregator backend
+    mediawiki::jobqueue_redis { 6378: slaveof => $slaveof }
+
+    # Queues
     mediawiki::jobqueue_redis { 6379: slaveof => $slaveof }
     mediawiki::jobqueue_redis { 6380: slaveof => $slaveof }
     mediawiki::jobqueue_redis { 6381: slaveof => $slaveof }
