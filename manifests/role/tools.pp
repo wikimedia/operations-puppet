9395958f94b1df423bd80aaca85960726d50f67f
YuviPanda <yuvipanda@gmail.com>
2015-12-05 23:09:32 -0800

    k8s: Stop including puppet::self related classes
    Change-Id: I54502f130f76bd32bf42fa6ea913d597c89b7304

--- manifests/role/tools.pp
+++ manifests/role/tools.pp
@@ -1,14 +1,10 @@
 # Roles for Kubernetes and co on Tool Labs
 class role::toollabs::etcd {
-    include role::toollabs::puppet::client
-
     # To deny access to etcd - atm the kubernetes master
     # and etcd will be on the same host, so ok to just deny
     # access to everyone else
     include base::firewall
     include toollabs::infrastructure
-
-    include role::toollabs::puppet::client
 
     include etcd
 
@@ -19,7 +15,6 @@ class role::toollabs::etcd {
 }
 
 class role::toollabs::docker::registry {
-    include role::toollabs::puppet::client
     include ::toollabs::infrastructure
 
     require role::labs::lvm::srv
@@ -34,16 +29,7 @@ class role::toollabs::puppet::master {
     include ::toollabs::puppetmaster
 }
 
-class role::toollabs::puppet::client(
-    $puppetmaster,
-) {
-    class { '::puppet::self::client':
-        server => $puppetmaster,
-    }
-}
-
 class role::toollabs::k8s::master {
-    include role::toollabs::puppet::client
     # This requires that etcd is on the same host
     # And is not HA. Will re-evaluate when it is HA
 
@@ -77,7 +63,6 @@ class role::toollabs::k8s::master {
 }
 
 class role::toollabs::k8s::worker {
-    include role::toollabs::puppet::client
     # NOTE: No base::firewall!
     # ferm and kube-proxy will conflict
 
@@ -106,7 +91,6 @@ class role::toollabs::k8s::worker {
 }
 
 class role::toollabs::k8s::webproxy {
-    include role::toollabs::puppet::client
 
     $master_host = hiera('k8s_master')
     $etcd_url = join(prefix(suffix(hiera('etcd_hosts', [$master_host]), ':2379'), 'https://'), ',')
@@ -131,7 +115,6 @@ class role::toollabs::k8s::webproxy {
 }
 
 class role::toollabs::k8s::bastion {
-    include role::toollabs::puppet::client
 
     # kubectl and things
     include k8s::client
