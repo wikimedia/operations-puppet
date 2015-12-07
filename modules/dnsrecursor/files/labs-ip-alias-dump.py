3c73dc41ee447d7d2e6d6f0dfc24104f4ebdc657
Yuvipanda <yuvipanda@wikimedia.org>
2015-12-07 17:07:59 +0000

    Revert "Labs: make labs-ip-alias-dump ignore addressless hosts"
    IMO it *should* fail loudly and explicitly. Failing also
    prevents puppet from restarting pdns-recursor, which is
    important.
    
    This reverts commit 4dca32bbdef2f24971f07db3e0e389f1b4389958.
    
    Change-Id: Id59341dc80183c9c3c81548dce3dfadbf53421ab

--- modules/dnsrecursor/files/labs-ip-alias-dump.py
+++ modules/dnsrecursor/files/labs-ip-alias-dump.py
@@ -37,22 +37,17 @@ for project in config['projects']:
 
     for server in client.servers.list():
         serverAddresses = {}
-        try:
-            private = [
-                str(ip['addr']) for ip in server.addresses['public'] if ip['OS-EXT-IPS:type'] == 'fixed'
-            ]
-            public = [
-                str(ip['addr']) for ip in server.addresses['public'] if ip['OS-EXT-IPS:type'] == 'floating'
-            ]
-            if public:
-                # Match all possible public IPs to all possible private ones
-                # Technically there can be more than one floating IP and more than one private IP
-                # Although this is never practically the case...
-                aliases[server.name] = list(itertools.product(public, private))
-        except KeyError:
-            # This can happen if a server doesn't (yet) have any addresses, while it's being
-            # constructed.  In which case we simply harmlessly ignore it.
-            pass
+        private = [
+            str(ip['addr']) for ip in server.addresses['public'] if ip['OS-EXT-IPS:type'] == 'fixed'
+        ]
+        public = [
+            str(ip['addr']) for ip in server.addresses['public'] if ip['OS-EXT-IPS:type'] == 'floating'
+        ]
+        if public:
+            # Match all possible public IPs to all possible private ones
+            # Technically there can be more than one floating IP and more than one private IP
+            # Although this is never practically the case...
+            aliases[server.name] = list(itertools.product(public, private))
 
 output = 'aliasmapping = {}\n'
 # Sort to prevent flapping around due to random ordering
