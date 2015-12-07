cf6af910e4ac2830b750595eb27fadf66b940b32
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-12-07 10:27:43 +0100

    pybal::monitoring: install libnagios-plugin-perl
    Also fix arguments to check-pybal
    
    Change-Id: I68ef6caee01b530094ed8817bc8f8dcec2e0efd2

--- modules/pybal/manifests/monitoring.pp
+++ modules/pybal/manifests/monitoring.pp
@@ -3,6 +3,8 @@
 
 class pybal::monitoring {
 
+    require_package 'libnagios-plugin-perl'
+
     diamond::collector { 'PyBalState':
         source => 'puppet:///modules/pybal/pybal_state.py'
     }
@@ -17,7 +19,7 @@ class pybal::monitoring {
 
     nrpe::monitor_service { 'pybal_backends':
         description  => 'PyBal backends health check',
-        nrpe_command => '/usr/local/lib/nagios/plugins/check_pybal',
+        nrpe_command => '/usr/local/lib/nagios/plugins/check_pybal --url http://localhost:9090/alerts',
         require      => File['/usr/local/lib/nagios/plugins/check_pybal']
     }
 
