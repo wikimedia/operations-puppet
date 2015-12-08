7b8937cc809701e8d57123b4b8ac126dde72a204
Ori Livneh <ori@wikimedia.org>
2015-12-07 16:47:53 -0800

    role::labs::openstack::keystone::server: decom redis instance
    We used redis replication when we migrated from pmtpa to eqiad. Now that Labs
    is single datacenter this feature isn't used (and the upstream Keystone broke
    the entrypoint interface that it relied on.)
    
    I'll clean up the leftovers manually.
    
    Change-Id: I015847282f8dd2608b0eafe341980d92cfceebc6

--- modules/role/manifests/labs/openstack/keystone.pp
+++ modules/role/manifests/labs/openstack/keystone.pp
@@ -8,17 +8,4 @@ class role::labs::openstack::keystone::server {
     class { 'openstack::keystone::service':
         keystoneconfig => $keystoneconfig,
     }
-
-    $replication = {
-        'labcontrol2001' => $nova_controller
-    }
-
-    class { '::redis::legacy':
-        maxmemory                 => '250mb',
-        persist                   => 'aof',
-        redis_replication         => $replication,
-        password                  => $keystoneconfig['db_pass'],
-        dir                       => '/var/lib/redis/',
-        auto_aof_rewrite_min_size => '64mb',
-    }
 }
