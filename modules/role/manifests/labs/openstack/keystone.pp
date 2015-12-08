f0b3900dd99aec62b323a4b8ac0e48f26188748f
Ori Livneh <ori@wikimedia.org>
2015-12-07 16:47:53 -0800

    keystone: migrate to redis::instance
    Change-Id: I015847282f8dd2608b0eafe341980d92cfceebc6

--- modules/role/manifests/labs/openstack/keystone.pp
+++ modules/role/manifests/labs/openstack/keystone.pp
@@ -4,21 +4,31 @@ class role::labs::openstack::keystone::server {
 
     $nova_controller   = hiera('labs_nova_controller')
     $keystoneconfig    = hiera_hash('keystoneconfig', {})
+    $slaveof           = hiera('keystone_redis_slaveof', undef)
 
     class { 'openstack::keystone::service':
         keystoneconfig => $keystoneconfig,
     }
 
-    $replication = {
-        'labcontrol2001' => $nova_controller
-    }
-
-    class { '::redis::legacy':
-        maxmemory                 => '250mb',
-        persist                   => 'aof',
-        redis_replication         => $replication,
-        password                  => $keystoneconfig['db_pass'],
-        dir                       => '/var/lib/redis/',
-        auto_aof_rewrite_min_size => '64mb',
+    redis::instance { 6379:
+	settings => {
+	    appendfilename              => "${::hostname}-6379.aof"
+	    appendonly                  => true,
+	    client_output_buffer_limit  => 'slave 512mb 200mb 60',
+	    dbfilename                  => "${::hostname}-6379.rdb"
+	    dir                         => '/var/lib/redis/',
+	    logfile                     => '/var/log/redis/redis.log',
+	    masterauth                  => $keystoneconfig['db_pass'],
+	    maxmemory                   => '250mb',
+	    maxmemory_policy            => 'volatile-lru',
+	    maxmemory_samples           => 5,
+	    no_appendfsync_on_rewrite   => true,
+	    requirepass                 => $keystoneconfig['db_pass'],
+	    save                        => '""',
+	    slave_read_only             => false,
+	    slaveof                     => $slaveof,
+	    stop_writes_on_bgsave_error => false,
+	    auto_aof_rewrite_min_size   => '64mb',
+	},
     }
 }
