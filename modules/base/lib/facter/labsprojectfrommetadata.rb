052d77550edb94d15bbc7a36e88d59e89916be86
andrewbogott <abogott@wikimedia.org>
2015-12-09 16:40:02 -0600

    Allow hiera to override $::labsproject
    Previously $::labsproject came directly from a fact.  Now
    we check hiera first, and if it's undefined fall back
    on a fact, now named $::labsprojectfrommetadata
    
    Bug: T95185
    Change-Id: I2811d2ba6a7263ac2b380c6f2bc355501f753fcc

--- modules/base/lib/facter/labsprojectfrommetadata.rb
+++ modules/base/lib/facter/labsprojectfrommetadata.rb
@@ -1,16 +1,16 @@
-# labsproject.rb
+# labsprojectfrommetadata.rb
 #
 # This fact pulls the labs project out of instance metadata
 
 require 'facter'
 
-Facter.add(:labsproject) do
+Facter.add(:labsprojectfrommetadata) do
   setcode do
     domain = Facter::Util::Resolution.exec("hostname -d").chomp
     if domain.end_with? ".wmflabs"
       # query the nova metadata service at 169.254.169.254
       metadata = Facter::Util::Resolution.exec("curl -f http://169.254.169.254/openstack/2013-10-17/meta_data.json/ 2> /dev/null").chomp
-      metadata[/project-id\":\ \"(.*?)\"/,1]
+      metadata[/project-id\":\ \"(.*?)\"/, 1]
     else
       nil
     end
