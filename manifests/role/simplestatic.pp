399aa5796104a5ab5dbfab60da6e6b5fa3000098
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2015-12-09 17:29:19 -0800

    apache: Add role to serve static sites on multiple hosts using apache
    Change-Id: I1319883fbe6fc7162c148cd584503eeb83506158

---
+++ manifests/role/simplestatic.pp
@@ -0,0 +1,17 @@
+# == Class: role::simplestatic
+# Role that takes a list of hostnames and a basepath
+# and sets up Apache to serve a static site from
+# each basepath/hostname location.
+
+class role::simplestatic (
+    $host_names = [],
+    $base_path = '/srv',
+) {
+    # This role is expected to be used only in labs
+    requires_realm('labs')
+
+    apache::site { 'simplestatic':
+        ensure  => present,
+        content => template('apache/sites/simplestatic.erb'),
+    }
+}
