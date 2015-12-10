de58ed89ca1702af8fe8e45404569c5d194794ae
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2015-12-09 17:29:19 -0800

    [WIP] apache: Add role to serve static sites on multiple hosts using apache
    Change-Id: I1319883fbe6fc7162c148cd584503eeb83506158

---
+++ manifests/role/simplestatic.pp
@@ -0,0 +1,15 @@
+# == Class: role::apache::static_multihost
+# Role that takes a list of hostnames and a basepath
+# and sets up Apache to serve a static site from
+# basepath/hostname.
+
+class role::simplestatic (
+    $host_names = [],
+    $base_path = '/srv',
+) {
+
+    apache::site { 'static-multihost':
+        ensure  => present,
+        content => template('apache/sites/static-multihost.erb'),
+    }
+}
