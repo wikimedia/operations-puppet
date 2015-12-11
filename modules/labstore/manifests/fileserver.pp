b980d94f7ec19c3b2893b07212452da2ebdc8b8b
Coren <marc@uberbox.org>
2015-12-11 08:45:50 -0500

    Labs: monitor getent timing only on active fileserver
    The test only makes sense from actual fileservers, and is only
    really meaningful on one at a time (since they would fail
    together).
    
    Change-Id: Ic2da87fce4ef8f6f6b81d68de1d1f317dfd88734

--- modules/labstore/manifests/fileserver.pp
+++ modules/labstore/manifests/fileserver.pp
@@ -76,6 +76,22 @@ class labstore::fileserver(
         labstore::fileserver::cleanup_snapshots { 'labstore':
             keep_free => '6',
         }
+
+        # Monitor that getent passwd over LDAP resolves in reasonable time
+        # (this being the mechanism that NFS uses to fetch groups)
+        nrpe::monitor_service { 'getent_check':
+            nrpe_command => '/usr/local/bin/getent_check',
+            description  => 'Getent speed check',
+            require      => File['/usr/local/bin/getent_check'],
+        }
+
+        file { '/usr/local/bin/getent_check':
+            ensure => present,
+            source => 'puppet:///modules/labstore/getent_check',
+            mode   => '0755',
+            owner  => 'root',
+            group  => 'root',
+        }
     }
 
     # There is no service {} stanza on purpose -- this service
--- modules/labstore/manifests/monitoring.pp
+++ modules/labstore/manifests/monitoring.pp
@@ -47,20 +47,4 @@ class labstore::monitoring(
         critical    => '24',
         percentage  => '50', # Don't freak out on spikes
     }
-
-    # Monitor that getent passwd over LDAP resolves in reasonable time
-    # (this being the mechanism that NFS uses to fetch groups)
-    nrpe::monitor_service { 'getent_check':
-        nrpe_command => '/usr/local/bin/getent_check',
-        description  => 'Getent speed check',
-        require      => File['/usr/local/bin/getent_check'],
-    }
-
-    file { '/usr/local/bin/getent_check':
-        ensure => present,
-        source => 'puppet:///modules/labstore/getent_check',
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
-    }
 }
