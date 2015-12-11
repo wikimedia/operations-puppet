583e4d92e60ed478a1b89da6bb231a2d1487bfdb
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-12-11 13:45:25 +0000

    ganglia: add ganglia::cluster exported resource
    the sole purpose of this resource is to get exported and thus end up in
    storedconfigs. From there we can pick it up via SQL and export it in other
    formats, e.g. a mapping from cluster to list of hosts in that cluster.
    
    Bug: T119520
    Change-Id: I037108db9ec003b6f54dc386c19ab82093c3f166

---
+++ modules/ganglia/manifests/cluster.pp
@@ -0,0 +1,7 @@
+# placeholder/shim define to be exported
+define ganglia::cluster (
+    $cluster,
+    $site,
+) {
+    debug("exporting ganglia cluster ${cluster} in site ${site} for ${title}")
+}
