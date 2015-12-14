232696697d8c1a8a87a2004acd1b1d1e19bbf0e3
Jaime Crespo <jcrespo@wikimedia.org>
2015-12-14 19:34:50 +0100

    Reconfiguring db1018 (performance_schema, ferm, SSL, upgrade)
    Change-Id: I73c66d803c80d620350bef3e0ac88879b8e3a1e3
    References: T120122

--- manifests/site.pp
+++ manifests/site.pp
@@ -536,10 +536,17 @@ node /^db20(16|34|42|48|55|62|69|70)\.codfw\.wmnet/ {
     include base::firewall
 }
 
-node /^db10(18|21|36|54|60|63|67)\.eqiad\.wmnet/ {
+node /^db10(21|36|54|60|63|67)\.eqiad\.wmnet/ {
     class { 'role::mariadb::core':
         shard => 's2',
     }
+}
+node /^db10(18)\.eqiad\.wmnet/ {
+    class { 'role::mariadb::core':
+        shard => 's2',
+        p_s   => 'on',
+    }
+    include base::firewall
 }
 
 node /^db20(17|35|41|49|56|63|64)\.codfw\.wmnet/ {
