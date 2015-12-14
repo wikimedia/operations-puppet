9fa4da6c9b3f5fb276aa5dac48126df563694e69
dzahn <dzahn@wikimedia.org>
2015-12-14 12:14:18 -0800

    labtest: don't send SMS for test machines
    Adding an option in hiera to mark a machine as a test machine
    (first i was about to just check hostname for "test" but this might be better).
    
    Then disable SMS paging for the nova-conductor service based on this.
    
    Bug:T120047
    Change-Id: Ic4bec99c5ea1d3e0e16fa009876c4372fcae8ba7

--- modules/openstack/manifests/nova/conductor.pp
+++ modules/openstack/manifests/nova/conductor.pp
@@ -18,10 +18,16 @@ class openstack::nova::conductor {
             require   => Package['nova-conductor'];
         }
 
+        if $is_test_machine {
+            $is_paging = false
+        } else {
+            $is_paging = true
+        }
+
         nrpe::monitor_service { 'check_nova_conductor_process':
             description  => 'nova-conductor process',
             nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/nova-conductor'",
-            critical     => true,
+            critical     => $is_paging,
         }
     } else {
         service { 'nova-conductor':
