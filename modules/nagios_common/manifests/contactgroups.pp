b4d6b55a73c5c7e257de5e65d500ffd4d1dc6c8c
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-12-14 18:44:27 +0200

    monitoring: Use nagios_common for contactgroups
    Deduplicate contactgroups by moving the stanza to the abstracting module
    nagios_common
    
    Change-Id: I37995f98b67a360abbb592b2f6601d9f7c9ef122

---
+++ modules/nagios_common/manifests/contactgroups.pp
@@ -0,0 +1,44 @@
+# = Class: nagios_common::contactgroups
+#
+# Sets up appropriate contacts for notifications
+#
+# [*ensure*]
+#   present or absent, to make the definition
+#   present or absent. Defaults to present
+#
+# [*config_dir*]
+#   The base directory to put configuration directory in.
+#   Defaults to '/etc/icinga/'
+#
+# [*owner*]
+#   The user which should own the check config files.
+#   Defaults to 'icinga'
+#
+# [*group*]
+#   The group which should own the check config files.
+#   Defaults to 'icinga'
+#
+# [*contacts*]
+#   The list of contacts to include in the configuration.
+#
+# [*source*]
+#   Allows to input a prewritten file as a source.  Overrides "content" if
+#   defined, but "content" is used if this is undefined.
+# [*content*]
+#   Allows to input the data as a content string.  The default is
+#   template('nagios_common/contacts.cfg.erb')
+#
+class nagios_common::contactgroups(
+    $ensure = present,
+    $source,
+    $config_dir = '/etc/icinga',
+    $owner = 'icinga',
+    $group = 'icinga',
+) {
+    file { "${config_dir}/contactgroups.cfg":
+        source  => $source,
+        owner   => $owner,
+        group   => $group,
+        mode    => '0644',
+    }
+}
