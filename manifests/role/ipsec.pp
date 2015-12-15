96bb68757e364d455f88f50cebfc52add2f55979
Brandon Black <bblack@wikimedia.org>
2015-12-15 13:33:48 +0000

    apply ipsec associations to kafka brokers
    Bug: T92602
    Change-Id: I8b25106cbb55dfd7ed596ba8eb037ff118753c00

--- manifests/role/ipsec.pp
+++ manifests/role/ipsec.pp
@@ -5,27 +5,45 @@ class role::ipsec ($hosts = undef) {
     if $hosts != undef {
         $targets = $hosts
     } else {
-        # if $cluster == 'cache_text', $ipsec_cluster = 'cache::ipsec::text'
-        # This duplication of nodelist data in the ::ipsec:: case in
-        # hieradata is so that we can depool cache nodes in the primary
-        # hieradata lists without de-configuring the ipsec associations,
-        # which could cause a traffic-leaking race.  This will go away once
-        # etcd replaces hieradata comments for varnish-level depooling.
+        if $::hostname =~ /^cp/ {
+            # if $cluster == 'cache_text', $ipsec_cluster = 'cache::ipsec::text'
+            # This duplication of nodelist data in the ::ipsec:: case in
+            # hieradata is so that we can depool cache nodes in the primary
+            # hieradata lists without de-configuring the ipsec associations,
+            # which could cause a traffic-leaking race.  This will go away once
+            # etcd replaces hieradata comments for varnish-level depooling.
 
-        $ipsec_cluster = regsubst(hiera('cluster'), '_', '::ipsec::')
-        $cluster_nodes = hiera("${ipsec_cluster}::nodes")
-        # for 'left' nodes in cache sites, enumerate 'right' nodes in "main" sites
+            $ipsec_cluster = regsubst(hiera('cluster'), '_', '::ipsec::')
+            $cluster_nodes = hiera("${ipsec_cluster}::nodes")
+            $kafka_nodes = hiera('cache::ipsec::kafka::nodes')
 
-        # Note codfw tier2, which isn't controlled by $site_tier... T110065
-        if $::site == 'esams' or $::site == 'ulsfo' or $::site == 'codfw' {
-            $targets = $cluster_nodes['eqiad']
-        }
-        # for 'left' nodes in "main" sites, enumerate 'right' nodes in cache sites
-        if $::site == 'eqiad' {
+            # tier-2 sites associate with tier-1 kafka and tier-1 same-cluster cache nodes
+            if $::site == 'esams' or $::site == 'ulsfo' or $::site == 'codfw' {
+                $targets = array_concat(
+                    $cluster_nodes['eqiad'],
+                    $kafka_nodes['eqiad'],
+                )
+
+            }
+            # tier-1 sites associate with tier-2 same-cluster cache nodes
+            if $::site == 'eqiad' {
+                $targets = array_concat(
+                    $cluster_nodes['esams'],
+                    $cluster_nodes['ulsfo'],
+                    $cluster_nodes['codfw']
+                )
+            }
+        } else if $hostname =~ /^kafka10/ {
+            # kafka brokers (only in eqiad for now) associate with all tier-two caches
+            $text    = hiera('cache::ipsec::text::nodes')
+            $misc    = hiera('cache::ipsec::misc::nodes')
+            $upload  = hiera('cache::ipsec::upload::nodes')
+            $mobile  = hiera('cache::ipsec::mobile::nodes')
             $targets = array_concat(
-                $cluster_nodes['esams'],
-                $cluster_nodes['ulsfo'],
-                $cluster_nodes['codfw']
+                $text['esams'], $text['ulsfo'], $text['codfw'],
+                $misc['esams'], $misc['ulsfo'], $misc['codfw'],
+                $upload['esams'], $upload['ulsfo'], $upload['codfw'],
+                $mobile['esams'], $mobile['ulsfo'], $mobile['codfw']
             )
         }
     }
