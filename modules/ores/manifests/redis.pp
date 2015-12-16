ebcf848a84bb1fcdb9a65b98eb9f8b88ad68fffd
YuviPanda <yuvipanda@gmail.com>
2015-12-16 13:45:19 -0800

    ores: Stop using aof for redis persistance
    The defaults already include rdf, so just use that. Right now
    we ended up using both rdb and aof, which isn't that useful
    in our case.
    
    Bug: T121658
    Change-Id: I56f99f487e35e443862fef3e13ec884242c05661

--- modules/ores/manifests/redis.pp
+++ modules/ores/manifests/redis.pp
@@ -1,12 +1,11 @@
 class ores::redis(
     $maxmemory
 ) {
+    # Uses the default rdb settings, which are good enough
     redis::instance { '6379':
         settings => {
             dir            => '/srv/redis',
             maxmemory      => $maxmemory,
-            appendonly     => 'yes',
-            appendfilename => "${hostname}-6379.aof",
             tcp_keepalive  => 60,
         }
     }
