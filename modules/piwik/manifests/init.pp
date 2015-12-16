786eb7e774988c9745891ba9ed34c3600528f44c
Ori Livneh <ori@wikimedia.org>
2015-12-16 14:20:32 -0800

    [WIP] Add piwik module and role
    * Provision behind misc-varnish as piwik.wikimedia.org.
    * Restrict access at Apache level by using mod_authnz_ldap.
    
    TODO:
    * Figure out where database will be hosted.
    
    Bug: T103577
    Change-Id: I136ab0a38339544f461a73154d1e4ad5a0679b0e

---
+++ modules/piwik/manifests/init.pp
@@ -0,0 +1,16 @@
+# == Class: piwik
+#
+# Piwik is an open-source analytics platform.
+# FIXME: document
+#
+class piwik( $settings ) {
+    require_package('piwik')
+
+    file { '/etc/piwik/config.ini.php':
+	content => php_ini($settings),
+	owner   => 'root',
+	group   => 'www-data',
+	mode    => '0444',
+	require => Package['piwik'],
+    }
+}
