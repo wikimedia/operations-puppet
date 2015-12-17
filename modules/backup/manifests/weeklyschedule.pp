756a5faebb6dedf9a047c257612fb2ff6d0a1986
dzahn <dzahn@wikimedia.org>
2015-12-17 13:40:03 -0800

    backup: ignore arrow alignment warnings
    Change-Id: Ic156a8659607eb69b298d9280a18e18ca9c0fb5f

--- modules/backup/manifests/weeklyschedule.pp
+++ modules/backup/manifests/weeklyschedule.pp
@@ -1,11 +1,13 @@
 # Same for weekly backups
 define backup::weeklyschedule($pool) {
     bacula::director::schedule { "Weekly-${name}":
+        # lint:ignore:arrow_alignment
         runs => [
             { 'level' => 'Full',
               'at'    => "${name} at 02:05",
             },
                 ],
+        # lint:endignore
     }
 
     bacula::director::jobdefaults { "Weekly-${name}-${pool}":
