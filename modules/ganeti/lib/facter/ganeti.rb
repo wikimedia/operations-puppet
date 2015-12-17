5157b81dcf079637285073c0566d7f807c3832c1
Željko Filipin <zeljko.filipin@gmail.com>
2015-12-17 16:29:11 +0100

    RuboCop: fixed Style/MultilineIfThen offense
    Do not use then for multi-line if.
    
    Change-Id: Ie6ea010901b04c960e8942834ef733db28b39a58
    See: https://github.com/bbatsov/ruby-style-guide#no-then
    Bug: T112651

--- modules/ganeti/lib/facter/ganeti.rb
+++ modules/ganeti/lib/facter/ganeti.rb
@@ -2,7 +2,7 @@ Facter.add("ganeti_cluster") do
     confine :kernel => :linux
 
     setcode do
-        if File.exists?("/var/lib/ganeti/ssconf_cluster_name") then
+        if File.exists?("/var/lib/ganeti/ssconf_cluster_name")
             cmdline = %x{cat /var/lib/ganeti/ssconf_cluster_name}.chomp
         end
     end
