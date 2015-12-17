f25b659739dd785daa64e1008e27781dc092426f
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2015-12-17 11:18:08 +0000

    swift: adjust mount options for debian and ubuntu
    Bug: T117972
    Change-Id: I715937b4b07a2dbd4702ab4cf73e1005680e6198

--- modules/swift/manifests/mount_filesystem.pp
+++ modules/swift/manifests/mount_filesystem.pp
@@ -13,6 +13,14 @@ define swift::mount_filesystem (
         mode   => '0750',
     }
 
+    # systemd < 218-4 fails to handle 'nobootwait' in fstab
+    # http://metadata.ftp-master.debian.org/changelogs/main/s/systemd/unstable_changelog
+    if $::operatingsystem == 'Ubuntu' and os_version('ubuntu <= trusty') {
+        $mount_options = 'nobootwait,noatime,nodiratime,nobarrier,logbufs=8'
+    } else {
+        $mount_options = 'nofail,noatime,nodiratime,nobarrier,logbufs=8'
+    }
+
     mount { $mount_point:
         # XXX this won't mount the disks the first time they are added to
         # fstab.
@@ -23,7 +31,7 @@ define swift::mount_filesystem (
         device   => "LABEL=swift-${dev_suffix}",
         name     => $mount_point,
         fstype   => 'xfs',
-        options  => 'nobootwait,noatime,nodiratime,nobarrier,logbufs=8',
+        options  => $mount_options,
         atboot   => true,
         remounts => true,
     }
