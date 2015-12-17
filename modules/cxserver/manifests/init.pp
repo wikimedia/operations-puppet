378c1ac77c12ae9d8ad07b43cf603cee5c9118a0
Alexandros Kosiaris <akosiaris@wikimedia.org>
2015-12-17 18:41:12 +0200

    Revert "CXServer: Do not use the proxy for RESTBase and Apertium"
    This reverts commit 8a1289d4bd1335f4f89042addb7a4c92f5693548.
    
    Change-Id: I70efa4fa4916d26e81a135e8897afd38e867dca9

--- modules/cxserver/manifests/init.pp
+++ modules/cxserver/manifests/init.pp
@@ -5,10 +5,8 @@
 #
 # === Parameters
 #
-# [*restbase_host*]
-#   The host/port where to reach RESTBase
-# [*restbase_path*]
-#   The URI path to append to *restbase_host*
+# [*restbase_url*]
+#   Url to Restbase API.
 # [*apertium*]
 #   Url to Apertium service.
 # [*yandex_url*]
@@ -20,8 +18,7 @@
 # [*jwt_secret*]
 #   JWT secret token.
 class cxserver(
-    $restbase_host = 'http://restbase.svc.eqiad.wmnet:7231',
-    $restbase_path = '/@lang.wikipedia.org/v1/page/html/@title',
+    $restbase_url = 'http://restbase.svc.eqiad.wmnet:7231/@lang.wikipedia.org/v1/page/html/@title',
     $apertium = 'http://apertium.svc.eqiad.wmnet:2737',
     $yandex_url = undef,
     $yandex_api_key = undef,
@@ -31,8 +28,6 @@ class cxserver(
     if $registry {
         $ordered_registry = ordered_json($registry)
     }
-
-    $restbase_url = "${restbase_host}${restbase_path}"
 
     service::node { 'cxserver':
         port   => 8080,
