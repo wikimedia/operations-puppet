b627e603dbddcbf8621fad213774387e39fd345f
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-12-17 16:00:15 +0100

    pybal: introduce role for testing machines
    This should guarantee that a functioning setup of pybal is provided,
    including a pybal configuration that should include all services from
    the whole datacenter.
    
    Change-Id: I0882db68c5a8d87b4312925a666ef13250f8305c

---
+++ modules/role/manifests/pybal/testing.pp
@@ -0,0 +1,24 @@
+# == Class role::pybal::testing
+#
+# Class for a pybal test host
+
+class role::pybal::testing {
+    include ::pybal
+    $opts = {
+        'instrumentation' => 'yes',
+        'bgp'             => 'no',
+        'dry-run'         => 'yes',
+    }
+
+    $lvs_class_hosts_stub = {
+        'high-traffic1' => [$::hostname],
+        'high-traffic2' => [$::hostname],
+        'low-traffic'   => [$::hostname],
+    }
+
+    class { 'pybal::configuration':
+        global_options  => $opts,
+        lvs_services    => hiera('lvs::configuration::lvs_services'),
+        lvs_class_hosts => $lvs_class_hosts_stub,
+    }
+}
