d2fe2bb8a12ce31958102b262080685cb72f8d54
andrewbogott <abogott@wikimedia.org>
2015-12-16 18:23:50 -0600

    Insert dns entries for labs bare-metal systems.
    Since the Powerdns recursor only allows one lua hook file,
    this patch also creates a new single-point-of-entry file,
    recursorhooks.lua, which selectively imports other
    lua scripts as specified in the $lua_hooks param.
    
    Change-Id: I20453be668236dd86f1ca8a2ada18e997c10d8fc

---
+++ modules/dnsrecursor/manifests/metalresolver.pp
@@ -0,0 +1,14 @@
+class dnsrecursor::metalresolver {
+
+    $labs_metal = hiera('labs_metal',[])
+
+    file { "/etc/powerdns/metaldns.lua":
+        ensure  => present,
+        require => Package['pdns-recursor'],
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['pdns-recursor'],
+        content => template('dnsrecursor/metaldns.lua.erb'),
+    }
+}
