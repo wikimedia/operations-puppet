67ef0f6bfc855ec4797ef41b4fa78a06d814b232
Željko Filipin <zeljko.filipin@gmail.com>
2015-12-17 16:40:23 +0100

    RuboCop: fixed Style/ParallelAssignment offense
    Do not use parallel assignment.
    
    Change-Id: I49839b8386a54caad5f978421e06a197895b8fba
    See: https://github.com/bbatsov/ruby-style-guide#parallel-assignment
    Bug: T112651

--- modules/wmflib/lib/puppet/parser/functions/to_milliseconds.rb
+++ modules/wmflib/lib/puppet/parser/functions/to_milliseconds.rb
@@ -13,7 +13,8 @@ module Puppet::Parser::Functions
   newfunction(:to_milliseconds, :type => :rvalue, :arity => 1) do |args|
     time_spec = args.first
     /^([0-9.+e]+)\s*(.*).?$/ =~ time_spec.downcase
-    count, unit = $1, $2
+    count = $1
+    unit = $2
     factor = case unit
              when /^n/         then 1.0e-6      # nanoseconds
              when /^u/         then 1.0e-3      # microseconds
