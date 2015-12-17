5cc711bc2c683439000e1ec4805cad8919cec563
Željko Filipin <zeljko.filipin@gmail.com>
2015-12-17 16:09:45 +0100

    RuboCop: fixed Style/EmptyLiteral offense
    Use array literal [] instead of Array.new.
    
    Change-Id: If51b9bd88023b5fa1ae89dd8352c5d49df931598
    See: https://github.com/bbatsov/ruby-style-guide#literal-array-hash
    Bug: T112651

--- modules/admin/lib/puppet/parser/functions/unique_users.rb
+++ modules/admin/lib/puppet/parser/functions/unique_users.rb
@@ -2,7 +2,7 @@ module Puppet::Parser::Functions
     newfunction(:unique_users, :type => :rvalue) do |args|
         myhash = args[0]
         applied_groups = args[1]
-        users = Array.new
+        users = []
         for group in applied_groups
             if myhash['groups'].key?(group)
                 users.push(myhash['groups'][group]['members'])
