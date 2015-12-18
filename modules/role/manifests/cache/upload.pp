48b2547b798e8396822ad77ae10b3c455d01923f
Brandon Black <bblack@wikimedia.org>
2015-12-18 17:10:53 +0000

    varnish: appropriate t2-fe -> t1-be backend_options
    When t2 frontends were contact t1 backends directly (for pass),
    they now use the same backend options that t2 backends would use
    to contact t1 backends.  The key difference is a doubling of
    between_bytes_timeout, and a lowering of max_connections.
    
    Bug: T121564
    Bug: T96847
    Change-Id: Ibbc167be55b7bb7e5bc2a77a6c40527ef2f0c01c

--- modules/role/manifests/cache/upload.pp
+++ modules/role/manifests/cache/upload.pp
@@ -48,6 +48,32 @@ class role::cache::upload {
         include varnish::monitoring::ganglia::vhtcpd
     }
 
+    $fe_t1_be_opts = array_concat(
+        $::role::cache::2layer::backend_scaled_weights,
+        [{
+            'port'                  => 3128,
+            'connect_timeout'       => '5s',
+            'first_byte_timeout'    => '35s',
+            'between_bytes_timeout' => '2s',
+            'max_connections'       => 100000,
+            'probe'                 => 'varnish',
+        }]
+    )
+
+    $fe_t2_be_opts = array_concat(
+        [{
+            'backend_match'         => '^cp[0-9]+\.eqiad.wmnet$',
+            'between_bytes_timeout' => '4s',
+            'max_connections'       => 1000,
+        }],
+        $fe_t1_be_opts
+    )
+
+    $fe_be_opts = $::site_tier ? {
+        'one'   => $fe_t1_be_opts,
+        default => $fe_t2_be_opts,
+    }
+
     $common_vcl_config = {
         'cache4xx'         => '1m',
         'purge_host_regex' => $::role::cache::base::purge_host_only_upload_re,
@@ -120,16 +146,7 @@ class role::cache::upload {
             },
         },
         vcl_config         => $fe_vcl_config,
-        backend_options    => array_concat($::role::cache::2layer::backend_scaled_weights, [
-            {
-                'port'                  => 3128,
-                'connect_timeout'       => '5s',
-                'first_byte_timeout'    => '35s',
-                'between_bytes_timeout' => '2s',
-                'max_connections'       => 100000,
-                'probe'                 => 'varnish',
-            },
-        ]),
+        backend_options    => $fe_be_opts,
     }
 
     # ToDo: Remove production conditional once this works
