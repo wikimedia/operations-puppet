56e971332b557d58af188453a1ccd68b20c4056b
YuviPanda <yuvipanda@gmail.com>
2015-12-17 16:52:28 -0800

    ores: Setup separate staging config
    Change-Id: If03ca94060845457a34f152c1902dacfe7aad8b4

--- manifests/role/labsores.pp
+++ manifests/role/labsores.pp
@@ -57,7 +57,21 @@ class role::labs::ores::staging {
         queue_maxmemory => '256M',
     }
 
-    class { '::ores::redisproxy':
-        server => 'localhost',
+    ores::config::staging { 'staging':
+        settings => {
+            'score_caches'     => {
+                'ores_redis' => {
+                    'host' => '127.0.0.1',
+                    'port' => '6380',
+                }
+            },
+            'score_processors' => {
+                'ores_celery' => {
+                    'BROKER_URL'            => 'redis://127.0.0.1:6379',
+                    'CELERY_RESULT_BACKEND' => 'redis://127.0.0.1:6379',
+                }
+            }
+        },
+        priority => '99',
     }
 }
