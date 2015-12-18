743aaf4c72c892e30b858d86c04036ad55a10662
YuviPanda <yuvipanda@gmail.com>
2015-12-17 16:11:23 -0800

    ores: Split cache and queue redis
    Bug: T121658
    Change-Id: I1af00a72e28f5b0252d0e9bef4da98406ec7974c

--- modules/ores/manifests/redis.pp
+++ modules/ores/manifests/redis.pp
@@ -1,11 +1,22 @@
 class ores::redis(
-    $maxmemory
+    $queue_maxmemory,
+    $cache_maxmemory,
 ) {
-    # Uses the default rdb settings, which are good enough
+    # FIXME: Tune the individual redises better for their use case
+    # For the queue
     redis::instance { '6379':
         settings => {
-            dir            => '/srv/redis',
-            maxmemory      => $maxmemory,
+            dir            => '/srv/redis/queue',
+            maxmemory      => $queue_maxmemory,
+            tcp_keepalive  => 60,
+        }
+    }
+
+    # For the cache
+    redis::instance { '6380':
+        settings => {
+            dir            => '/srv/redis/cache',
+            maxmemory      => $cache_maxmemory,
             tcp_keepalive  => 60,
         }
     }
