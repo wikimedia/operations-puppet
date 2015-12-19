f50febc17dc2624e1e2a40266c0ea6e9c083df19
dzahn <dzahn@wikimedia.org>
2015-12-19 06:21:35 -0800

    contint: drop publish-console
    Was meant to eventually capture console logs and have them published
    under https://integration.wikimedia.org/logs/ . We never went to it
    though so the puppet class can be removed.
    
    Docroot is cleaned with:
    https://gerrit.wikimedia.org/r/262441
    
    Change-Id: I8188d5c562065ac77c49ad8b5be01680570c269f

--- modules/contint/manifests/publish-console.pp
+++
@@ -1,9 +0,0 @@
-# Dependencies for the Jenkins console publisher
-# Files are made available under:
-# https://integration.wikimedia.org/logs/
-class contint::publish-console {
-
-    # publish-console.py dependency
-    require_package('python-requests')
-
-}
