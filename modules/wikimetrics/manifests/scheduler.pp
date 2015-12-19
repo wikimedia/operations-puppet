f1a7fefa881113195d73dee15ffb6b0652a799bb
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2015-12-19 13:44:29 -0800

    wikimetrics: Puppet module for wikimetrics
    - Deleted old submodule entry from .gitmodules
    - Added base.pp for new module, sets up user and config repo
    - Old role class is being replaced
    - Add web, queue, and scheduler service classes
    
    Bug: T101763
    Change-Id: Ibe7d751b4a1a3b2339af55dcbfba2fb3cde10f0f

---
+++ modules/wikimetrics/manifests/scheduler.pp
@@ -0,0 +1,16 @@
+# == Class: wikimetrics::scheduler
+# Sets up the celery beat based scheduler for wikimetrics
+# This is done by launching the wikimetrics init script,
+# with the scheduler mode and passing relevant config files.
+# The scheduler runs recurrent reports
+
+class wikimetrics::scheduler {
+    require wikimetrics::base
+
+    $config_path      = $::wikimetrics::base::config_path
+    $venv_path        = $::wikimetrics::base::venv_path
+
+    base::service_unit { 'wikimetrics-scheduler':
+        systemd => true,
+    }
+}
