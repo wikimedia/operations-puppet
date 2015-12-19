eb8f425bcb7b115759b5839c450e2948f3cbe63c
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2015-12-19 13:44:29 -0800

    [WIP] wikimetrics: Puppet module for wikimetrics
    - Deleted old submodule entry from .gitmodules
    - Added base.pp for new module, sets up user and config repo
    - Old role class is being replaced
    - Add web, queue, and scheduler service classes
    
    Change-Id: Ibe7d751b4a1a3b2339af55dcbfba2fb3cde10f0f

---
+++ modules/wikimetrics/manifests/web.pp
@@ -0,0 +1,22 @@
+# == Class: wikimetrics::web
+# Sets up a uwsgi based web server for Wikimetrics
+class wikimetrics::web(
+    $workers = 1,
+) {
+    require wikimetrics::base
+
+    uwsgi::app { 'wikimetrics-web':
+        settings => {
+            uwsgi => {
+                plugins      => 'python, router_redirect',
+                'wsgi-file'  => "${wikimetrics::base::source_path}/wikimetrics/api.wsgi",
+                master       => true,
+                chdir        => $wikimetrics::base::source_path,
+                http-socket  => '0.0.0.0:8080',
+                venv         => $wikimetrics::base::venv_path,
+                processes    => $workers,
+                route-if     => 'equal:${HTTP_X_FORWARDED_PROTO};http redirect-permanent:https://${HTTP_HOST}${REQUEST_URI}',
+            }
+        }
+    }
+}
