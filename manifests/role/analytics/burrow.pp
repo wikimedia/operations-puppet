dc763b54538b7f4424c927d15df1305618aa9b31
Andrew Otto <acotto@gmail.com>
2015-12-21 12:56:24 -0500

    Replace burrow kafka config with new role
    Bug: T121659
    Change-Id: I7409dc28fd3f797753d56796793e681fc5c635e2

--- manifests/role/analytics/burrow.pp
+++ manifests/role/analytics/burrow.pp
@@ -4,15 +4,15 @@
 class role::analytics::burrow {
 
     # Include the kafka config role to get all configuration data
-    include role::analytics::kafka::config
+    include role::kafka::analytics::config
 
     # One of the params for Class: burrow is consumer_groups, this is configured
     # through hiera and hence not explicitly passed here
     class { '::burrow':
-        zookeeper_hosts    => $role::analytics::kafka::config::zookeeper_hosts,
-        zookeeper_path     => $role::analytics::kafka::config::zookeeper_chroot,
-        kafka_cluster_name => $role::analytics::kafka::config::kafka_cluster_name,
-        kafka_brokers      => $role::analytics::kafka::config::brokers_array,
+        zookeeper_hosts    => $role::kafka::analytics::config::zookeeper_hosts,
+        zookeeper_path     => $role::kafka::analytics::config::zookeeper_chroot,
+        kafka_cluster_name => $role::kafka::analytics::config::cluster_name,
+        kafka_brokers      => $role::kafka::analytics::config::brokers_array,
         smtp_server        => 'mx1001.wikimedia.org',
         from_email         => "burrow@${::fqdn}",
         to_emails          => ['analytics-alerts@wikimedia.org']
