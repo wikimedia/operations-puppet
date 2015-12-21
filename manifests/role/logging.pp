0a3dfb457a49f23cd6df31c17e618b42f64de487
Andrew Otto <acotto@gmail.com>
2015-12-21 11:45:48 -0500

    Remove misc/logging.pp and ::relay related classes that are no longer used
    Change-Id: I1b0704b365136097a1b569f1cf8322d808bf3ad4

--- manifests/role/logging.pp
+++ manifests/role/logging.pp
@@ -112,40 +112,6 @@ class role::logging::mediawiki::errors {
     class { 'mediawiki::monitoring::errors': }
 }
 
-# == Class role::logging::relay::webrequest-multicast
-# Sets up a multicast relay using socat for
-# webrequest log streams (squid, varnish, nginx etc.).
-# Anything sent to this node on port 8419 will be
-# relayed to the 233.58.59.1:8420 multicast group.
-#
-class role::logging::relay::webrequest-multicast {
-    system::role { 'role::logging::relay::webrequest-multicast':
-        description => 'Webrequest log stream unicast to multicast relay',
-    }
-
-    misc::logging::relay { 'webrequest':
-        listen_port      => '8419',
-        destination_ip   => '233.58.59.1',
-        destination_port => '8420',
-        multicast        => true,
-    }
-}
-
-# == Class role::logging::relay::eventlogging
-# Relays EventLogging traffic over to eventlog*.
-#
-class role::logging::relay::eventlogging {
-    system::role { 'misc::logging::relay::eventlogging':
-        description => 'esams bits event logging to eventlog* relay',
-    }
-
-    misc::logging::relay { 'eventlogging':
-        listen_port      => '8422',
-        destination_ip   => '10.64.32.167', # eventlog1001
-        destination_port => '8422',
-    }
-}
-
 # == Class role::logging::kafkatee::webrequest
 # TODO: This needs a not-stupid name.
 #
