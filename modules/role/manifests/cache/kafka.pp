53079199bd04e849d8ba1452c104c3d8451c4406
Andrew Otto <acotto@gmail.com>
2015-12-21 14:14:51 -0500

    Use new kafka role in role::cache::kafka
    Bug: T121659
    Change-Id: I755e1422b35e8b0b04cbd9a6c4fbd9091f6c861f

--- modules/role/manifests/cache/kafka.pp
+++ modules/role/manifests/cache/kafka.pp
@@ -2,19 +2,8 @@
 # Base class for instances of varnishkafka on cache servers.
 #
 class role::cache::kafka {
-    require role::analytics::kafka::config
-
-    # Get a list of kafka brokers for the currently configured $kafka_cluster_name.
-    # In production this will be 'eqiad' always, since we only have one Kafka cluster there.
-    # Even though $kafka_cluster_name should hardcoded to 'eqiad' in ...kafka::config,
-    # we hardcode it again here, just to be sure it doesn't accidentally get changed
-    # if we add new Kafka clusters later.
-    $kafka_cluster_name = $::realm ? {
-        'production' => 'eqiad',
-        'labs'       => $role::analytics::kafka::config::kafka_cluster_name,
-    }
-
-    $kafka_brokers = keys($role::analytics::kafka::config::cluster_config[$kafka_cluster_name])
+    require role::kafka::analytics::config
+    $kafka_brokers = $::role::kafka::analytics::config::brokers_array
 
     # Make sure varnishkafka rsyslog file is in place properly.
     rsyslog::conf { 'varnishkafka':
