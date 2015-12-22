6ad13ee6a8a6361a9601a1e425bec60f5a52ef42
dzahn <dzahn@wikimedia.org>
2015-12-22 10:05:54 -0800

    ceilometer: move roles to modules/role/
    Split the file into one file per class and move
    it into proper autoload layout in the role module.
    
    Change-Id: I3f985795d4e63268823e684f7821e68b2c2c9884

--- modules/role/manifests/ceilometer/controller.pp
+++ modules/role/manifests/ceilometer/controller.pp
@@ -8,12 +8,3 @@ class role::ceilometer::controller {
     }
 }
 
-class role::ceilometer::compute {
-    include role::labs::openstack::nova::common
-    $novaconfig = $role::labs::openstack::nova::common::novaconfig
-
-    class { 'openstack::ceilometer::compute':
-        openstack_version => $::openstack_version,
-        novaconfig        => $novaconfig,
-    }
-}
