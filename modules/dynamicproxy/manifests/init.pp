9a5e3d402edd82381719d270195253c208a171ac
YuviPanda <yuvipanda@gmail.com>
2015-12-22 10:57:46 -0800

    dynamicproxy: Add error graphite counts too
    Change-Id: I456ee0bf503290899e5fa8c230ae709fe07706d1

--- modules/dynamicproxy/manifests/init.pp
+++ modules/dynamicproxy/manifests/init.pp
@@ -194,6 +194,13 @@ class dynamicproxy (
         minute          => '*/1',
         parser          => 'LineCountLogster', # Nothing more specific yet
         logfile         => '/var/log/nginx/access.log',
-        logster_options => "-o statsd --statsd-host=labmon1001.eqiad.wmnet:8125 --metric-prefix=${graphite_metric_prefix}",
+        logster_options => "-o statsd --statsd-host=labmon1001.eqiad.wmnet:8125 --metric-prefix=${graphite_metric_prefix}.all",
+    }
+
+    logster::job { 'proxy-errors':
+        minute          => '*/1',
+        parser          => 'LineCountLogster', # Nothing more specific yet
+        logfile         => '/var/log/nginx/error.log',
+        logster_options => "-o statsd --statsd-host=labmon1001.eqiad.wmnet:8125 --metric-prefix=${graphite_metric_prefix}.errors",
     }
 }
