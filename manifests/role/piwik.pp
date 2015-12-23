243586d36f59aad6b420544449fce4b93e190887
Ori Livneh <ori@wikimedia.org>
2015-12-23 13:46:18 -0800

    Add some explanatory comments to Piwik role
    Change-Id: I310f4467375a11d52c66a757623908d5ccc6b9a6

--- manifests/role/piwik.pp
+++ manifests/role/piwik.pp
@@ -4,7 +4,22 @@
 # It powers <https://piwik.wikimedia.org>.
 #
 # Q: Why is there no piwik module?
-# A: Piwik has no good configuration mechanism apart from the web installer.
+# A: The only sanctioned way of configuring Piwik is via the web
+#    installer. It is possible to provision a config.ini.php via Puppet,
+#    but then you can't get to the web installer, so you are left with
+#    no way to initialize the database, short of doing a bulk MySQL
+#    import of a dump of an already-initialized Piwik database.
+#
+#    See #1586: Headless install / command line piwik remote install
+#    <https://github.com/piwik/piwik/issues/1586>.
+#    Closed with "We have implemented this plugin for Piwik PRO, please
+#    get in touch if you are interested."
+#
+# Q: So where are the credentials?
+# A: In pwstore.
+#
+# Q: Where did the package come from?
+# A: http://debian.piwik.org/, imported to jessie-wikimedia.
 #
 class role::piwik {
     include ::apache::mod::authnz_ldap
