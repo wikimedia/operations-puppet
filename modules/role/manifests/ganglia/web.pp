780a6deb4c51748938fcb88505bdaa558a81c59c
dzahn <dzahn@wikimedia.org>
2015-12-22 16:22:15 -0800

    ganglia: move roles to modules/role/
    Change-Id: I82ed200ddfc5c500e9ad801a37a92a4270340bde

--- modules/role/manifests/ganglia/web.pp
+++ modules/role/manifests/ganglia/web.pp
@@ -1,9 +1,3 @@
-class role::ganglia::config {
-    # TODO: hiera this
-    $rra_sizes = '"RRA:AVERAGE:0.5:1:360" "RRA:AVERAGE:0.5:24:245" "RRA:AVERAGE:0.5:168:241" "RRA:AVERAGE:0.5:672:241" "RRA:AVERAGE:0.5:5760:371"'
-    $data_sources = ganglia_aggregator_config()
-}
-
 # A role that includes all the needed stuff to run a ganglia web frontend
 class role::ganglia::web {
     include role::ganglia::config
