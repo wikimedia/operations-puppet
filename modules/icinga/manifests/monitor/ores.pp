4bd545b2e1a3b42e90de71ae78001f5b64f8b7c8
dzahn <dzahn@wikimedia.org>
2015-12-22 17:28:54 -0800

    ores: do not check for a string on /
    Just leave it as it was before and just use the default
    check_http for the home page. The content will change soon.
    
    We still want this in addition to the new worker check though.
    
    Change-Id: I5dfa65184a5e8cf86b7c8002d7910387bf2a5493

--- modules/icinga/manifests/monitor/ores.pp
+++ modules/icinga/manifests/monitor/ores.pp
@@ -7,7 +7,7 @@ class icinga::monitor::ores {
 
     monitoring::service { 'ores_main_page':
         description    => 'ORES home page',
-        check_command  => 'check_http_url_for_string!ores.wmflabs.org!/!ORES',
+        check_command  => 'check_http',
         host           => 'ores.wmflabs.org',
         contact_group  => 'team-ores',
     }
