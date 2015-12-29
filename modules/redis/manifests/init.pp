f4dac624e0aa52cd4328588ad9bc94a3a46bed21
Ori Livneh <ori@wikimedia.org>
2015-12-28 17:17:14 -0800

    redis: use file_line on redis.conf to enable latency monitor
    Making it part of the default configuration for instances would cause them to
    refresh.
    
    Change-Id: Icfa1a930dfa73ef79e2060393d26b539936b6f5e

--- modules/redis/manifests/init.pp
+++ modules/redis/manifests/init.pp
@@ -11,7 +11,7 @@ class redis {
         owner  => 'redis',
         group  => 'redis',
         mode   => '0755',
-    } -> Redis::Instance <| |>
+    }
 
     # Disable the default, system-global redis service,
     # because it's incompatible with a multi-instance setup.
@@ -21,7 +21,18 @@ class redis {
         subscribe => Package['redis-server'],
     }
 
+    # Disabling transparent hugepages is strongly recommended
+    # in http://redis.io/topics/latency.
     sysfs::parameters { 'disable_transparent_hugepages':
         values => { 'kernel/mm/transparent_hugepage/enabled' => 'never' }
+    }
+
+    if os_version('debian >= jessie || ubuntu >= trusty') {
+        file_line { 'enable_latency_monitor':
+            line    => 'latency-monitor-threshold 100',
+            match   => 'latency-monitor-threshold',
+            path    => '/etc/redis/redis.conf',
+            require => Package['redis-server'],
+        }
     }
 }
