126c94baaee3bb42ea7b49f659205cac554e2267
Tim Landscheidt <tim@tim-landscheidt.de>
2015-12-31 09:24:28 +0000

    Tools: Allow proxymanager to add and remove proxy forward entries
    Currently proxy forward entries are managed by a separate
    daemon "proxylistener" running on the proxy server.  Web
    services connect to this daemon with a custom protocol.
    
    This change adds this functionality to the nginx service
    itself in the form of a RESTful API.  Web services can
    manage their forward at
    http://$proxy_server:8081/v1/proxy-forwards/$toolname.
    
    As with proxylistener, proxymanager restricts changes of the
    forward to requests made by the corresponding tool account
    as identified by an ident query (RFC 1413).
    
    Change-Id: I2d643fc902208eafaaa0d7814e586f0c326f16b5

--- modules/dynamicproxy/manifests/init.pp
+++ modules/dynamicproxy/manifests/init.pp
@@ -156,6 +156,20 @@ class dynamicproxy (
             notify  => Service['nginx'],
         }
 
+        file { '/etc/nginx/lua/proxymanager-v1-proxy-forwards.lua':
+            ensure  => 'file',
+            source  => 'puppet:///modules/dynamicproxy/proxymanager/v1/proxy-forwards.lua',
+            require => [File['/etc/nginx/lua'], Package['lua-json']],
+            notify  => Service['nginx'],
+        }
+
+        file { '/etc/nginx/lua/proxymanager-v1-proxy-forwards-entry.lua':
+            ensure  => 'file',
+            source  => 'puppet:///modules/dynamicproxy/proxymanager/v1/proxy-forwards-entry.lua',
+            require => [File['/etc/nginx/lua'], Package['lua-json']],
+            notify  => Service['nginx'],
+        }
+
         package { 'lua-json':
             ensure => installed,
         }
@@ -163,7 +177,9 @@ class dynamicproxy (
         nginx::site { 'proxymanager':
             content => template('dynamicproxy/proxymanager.conf.erb'),
             require => [Ferm::Service['proxymanager'],
-                        File['/etc/nginx/lua/list-proxy-entries.lua']],
+                        File['/etc/nginx/lua/list-proxy-entries.lua'],
+                        File['/etc/nginx/lua/proxymanager-v1-proxy-forwards.lua'],
+                        File['/etc/nginx/lua/proxymanager-v1-proxy-forwards-entry.lua']],
         }
 
         ferm::service { 'proxymanager':
