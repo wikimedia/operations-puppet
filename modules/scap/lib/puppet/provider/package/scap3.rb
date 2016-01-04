885d6f35856e2c040b2299b05f443731a9582842
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-01-04 14:55:59 -0800

    Puppet provider for scap3
    This provider uses scap3's `deploy-local` command to install projects
    from a previously used deploy master. It assumes a two-part repo name
    (e.g. `mockbase/deploy`) and creates the parent directory (e.g.
    `/srv/deployment/mockbase`) before delegating creation of everything
    else under this root directory to `deploy-local`.
    
    For now, it does not support upgrading as this would require some
    additional functionality in scap to retrieve the latest available
    version from the master.
    
    Ownership should be set using the `package_settings` parameter which
    will dictate initial deployment root directory ownership as well as the
    `deploy-local` process owner.
    
    Bug: T113072
    Change-Id: I278d9e5e2e94c1c1fa9067209d462ccba527f775

---
+++ modules/scap/lib/puppet/provider/package/scap3.rb
@@ -0,0 +1,118 @@
+# Puppet package provider for `scap3`, a modern deployment system, based on
+# previous version of scap. This is mostly a spinoff from the puppet trebuchet
+# provider written by Ori Livneh
+#
+# Copyright 2014 Ori Livneh
+# Copyright 2016 Alexandros Kosiaris
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require 'puppet/provider/package'
+
+require 'etc'
+require 'fileutils'
+
+Puppet::Type.type(:package).provide(
+  :scap3,
+  :parent => Puppet::Provider::Package
+) do
+  desc 'Puppet package provider for `scap3`.'
+
+  has_feature :installable, :uninstallable, :package_settings
+
+  commands :deploy_local => '/usr/bin/deploy-local'
+
+  BASE_PATH = '/srv/deployment'
+
+  # Reads the current owner of the deploy root or, if the package hasn't been
+  # installed yet, the owner specified for the resource, or 'root'.
+  #
+  def package_settings
+    if File.exists?(deploy_root)
+      owner = Etc.getpwuid(File.stat(deploy_root).uid).name
+    else
+      settings = @resource[:package_settings] || {}
+      owner = settings['owner'] || 'root'
+    end
+
+    { 'owner' => owner }
+  end
+
+  # Checks whether the desired deploy root owner is the same as the current
+  # one.
+  #
+  def package_settings_insync?(goal, have)
+    goal['owner'] == have['owner']
+  end
+
+  # Sets a new deploy root owner by recursively chown'ing the directory.
+  #
+  def package_settings=(goal)
+    FileUtils.chown_R(goal['owner'], nil, deploy_root)
+  end
+
+  # Performs project installation via scap3's deploy-local command. The
+  # target's parent directory is created if it doesn't already exist, but the
+  # cache and final directory creation is left up to scap.
+  #
+  def install
+    unless Dir.exists?(deploy_root)
+      FileUtils.makedirs(deploy_root)
+      FileUtils.chown_R(package_settings['owner'], nil, deploy_root)
+    end
+
+    uid = Etc.getpwnam(package_settings['owner']).uid
+    cmd = [self.class.command(:deploy_local), '--repo', repo]
+
+    execute(cmd, uid: uid)
+  end
+
+  # Performs project uninstallation by removing the deploy root directory.
+  #
+  def uninstall
+    Puppet.warning("Deleting #{deploy_root}")
+    FileUtils.rm_rf(deploy_root)
+  end
+
+  private
+
+  def chown_deploy_root(user)
+    FileUtils.chown_R(user, nil, deploy_root)
+  end
+
+  def deploy_root
+    @deploy_root ||= File.dirname(target_path)
+  end
+
+  def deploy_user
+    settings = @resource[:package_settings] || {}
+    settings['owner'] || 'root'
+  end
+
+  def repo
+    case @resource[:name]
+    when %r{/} then @resource[:name]
+    else ([@resource[:name]] * 2).join('/')
+    end
+  end
+
+  def target_path
+    path = File.expand_path(File.join(BASE_PATH, repo))
+
+    unless path.start_with?(BASE_PATH)
+      raise Puppet::Error, "Target path '#{path}' is invalid."
+    end
+
+    path
+  end
+end
