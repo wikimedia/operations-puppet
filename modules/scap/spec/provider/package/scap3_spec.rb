37e457e8bea0f18243b2d3b0ad90f8306e35e228
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-01-04 14:55:59 -0800

    Puppet provider for scap3
    This provider uses scap3's `deploy-local` command to install projects
    from a previously used deploy master. It assumes a two-part repo name
    (e.g. `mockbase/deploy`) and creates the parent directory (e.g.
    `/srv/deployment/mockbase`) before delegating creation of everything
    else under this root directory to `deploy-local`.
    
    For now, it does not support upgrading as this would require some
    additional functionality in scap to retrieve the latest available
    version from the master.
    
    Ownership should be set using the `install_options` parameter which
    will dictate initial deployment root directory ownership as well as the
    `deploy-local` process owner.
    
    Implemented unit tests and added puppet/rspec-puppet to the Gemfile.
    
    Bug: T113072
    Change-Id: I278d9e5e2e94c1c1fa9067209d462ccba527f775

---
+++ modules/scap/spec/provider/package/scap3_spec.rb
@@ -0,0 +1,85 @@
+require_relative '../../spec_helper'
+require_relative '../../../lib/puppet/provider/package/scap3'
+
+provider_class = Puppet::Type.type(:package).provider(:scap3)
+
+describe provider_class do
+  before do
+    @resource = Puppet::Type.type(:package).new(name: 'foo/deploy')
+    @provider = provider_class.new(@resource)
+    @resource.provider = @provider
+    @resource[:install_options] = [{ 'owner' => 'mwdeploy' }]
+
+    # Stub all filesystem operations
+    allow(FileUtils).to receive(:chown_R)
+    allow(FileUtils).to receive(:makedirs)
+    allow(FileUtils).to receive(:rm_rf)
+
+    # Stub our mwdeploy user
+    allow(Etc).to receive(:getpwnam).with('mwdeploy').and_return(OpenStruct.new(uid: 666))
+
+    # Stub the existance of our deploy-local command
+    allow(@provider.class).to receive(:command).
+      with(:deploy_local).
+      and_return('/usr/bin/deploy-local')
+  end
+
+  describe '#install' do
+    it 'should specify the right repo' do
+      expect(@provider).to receive(:execute).
+        with(['/usr/bin/deploy-local', '--repo', 'foo/deploy', '-D', 'log_json:False'], uid: 666)
+      @provider.install
+    end
+  end
+
+  describe '#query' do
+    subject { @provider.query }
+
+    context 'when the package is installed' do
+      before do
+        expect(@provider).to receive(:git).
+          with('-C', '/srv/deployment/foo/deploy', 'tag', '--points-at', 'HEAD').
+          and_return(tag)
+      end
+
+      context 'and the tag exists' do
+        let(:tag) { 'scap/sync/2016-02-06/0001' }
+
+        it 'returns ensure: {tag} where {tag} is the scap tag for HEAD' do
+          expect(subject).to eq({ ensure: tag, name: 'foo/deploy' })
+        end
+      end
+
+      context 'but the tag does not exist' do
+        let(:tag) { '' }
+
+        it 'returns ensure: :installed' do
+          expect(subject).to eq({ ensure: :installed, name: 'foo/deploy' })
+        end
+      end
+    end
+
+    context 'when the package is not installed' do
+      before do
+        expect(@provider).to receive(:git) { raise Puppet::ExecutionFailure, 'fail' }
+      end
+
+      it 'returns ensure: absent' do
+        expect(subject).to eq({ ensure: :absent, name: 'foo/deploy' })
+      end
+    end
+  end
+
+  describe '#uninstall' do
+    it 'should delete the entire parent directory' do
+      expect(FileUtils).to receive(:rm_rf).with('/srv/deployment/foo')
+      @provider.uninstall
+    end
+  end
+
+  describe '#install_option' do
+    subject { @provider.install_option('owner', 'root') }
+
+    it { is_expected.to eq('mwdeploy') }
+  end
+end
