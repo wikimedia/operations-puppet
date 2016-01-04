3f93ec3ca5061f30eebd9aee76b6631f2c0f3c8f
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-01-04 14:55:59 -0800

    Puppet provider for scap3
    This provider uses scap3's `deploy-local` command to install projects
    from a previously used deploy master. It assumes a two-part repo name
    (e.g. `mockbase/deploy`) and creates the parent directory (e.g.
    `/srv/deployment/mockbase`) before delegating creation of everything
    else under this root directory to `deploy-local`.
    
    For now, it does not support upgrading as this would require some
    additional functionality in scap to retrieve the latest available
    version from the master.
    
    Ownership should be set using the `install_options` parameter which
    will dictate initial deployment root directory ownership as well as the
    `deploy-local` process owner.
    
    Implemented unit tests and added puppet/rspec-puppet to the Gemfile.
    
    Bug: T113072
    Change-Id: I278d9e5e2e94c1c1fa9067209d462ccba527f775

---
+++ modules/scap/lib/puppet/provider/package/scap3.rb
@@ -0,0 +1,148 @@
+# Puppet package provider for `scap3`, a modern deployment system, based on
+# previous version of scap. This is mostly a spinoff from the puppet trebuchet
+# provider written by Ori Livneh
+#
+# Copyright 2014 Ori Livneh
+# Copyright 2016 Alexandros Kosiaris
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require 'puppet/provider/package'
+
+require 'etc'
+require 'fileutils'
+
+Puppet::Type.type(:package).provide(
+  :scap3,
+  :parent => Puppet::Provider::Package
+) do
+  desc 'Puppet package provider for `scap3`.'
+
+  has_feature :installable, :uninstallable, :install_options
+
+  commands deploy_local: '/usr/bin/deploy-local', git: '/usr/bin/git',
+           sudo: '/usr/bin/sudo'
+
+  BASE_PATH = '/srv/deployment'
+
+  # This provider currently maintains no internal resources.
+  #
+  def self.instances
+    []
+  end
+
+  # Performs project installation via scap3's deploy-local command. The
+  # target's parent directory is created if it doesn't already exist, but the
+  # cache and final directory creation is left up to scap.
+  #
+  def install
+    unless Dir.exists?(deploy_root)
+      FileUtils.makedirs(deploy_root)
+    end
+
+    FileUtils.chown_R(deploy_user, nil, deploy_root)
+
+    Puppet.debug "scap pkg [#{repo}] root=#{deploy_root}, user=#{deploy_user}"
+
+    if deploy_user == "root"
+      deploy_local('--repo', repo, '-D', 'log_json:False', 'fetch')
+    else
+      sudo('-u', deploy_user, self.class.command(:deploy_local),
+           '--repo', repo, '-D', 'log_json:False', 'fetch')
+    end
+  end
+
+  # Queries the current state of a scap3 managed package. The package is
+  # assumed to be installed if `git rev-parse HEAD` outputs anything. Once we
+  # have some sort of `--diff` functionality in `deploy-local`, we can improve
+  # this.
+  #
+  def query
+    result = { ensure: :installed, name: resource[:name] }
+
+    begin
+      sha1 = git('-C', target_path, 'tag', '--points-at', 'HEAD').strip
+      result[:ensure] = sha1 unless sha1.empty?
+    rescue Puppet::ExecutionFailure
+      result[:ensure] = :absent
+    end
+
+    result
+  end
+
+  # Performs project uninstallation by removing the deploy root directory.
+  def uninstall
+    Puppet.warning("Deleting #{deploy_root}")
+    FileUtils.rm_rf(deploy_root)
+  end
+
+  private
+
+  # top level directory of this package.
+  # (the first subdirectory below /srv/deployment)
+  def deploy_root
+    @deploy_root ||= File.dirname(target_path)
+  end
+
+  def deploy_user
+    @deploy_user ||= install_option('owner', 'root')
+  end
+
+  def repo
+    resource[:name]
+  end
+
+  def repo_path
+    if repo.include?('/')
+      repo
+    else
+      [repo, repo].join('/')
+    end
+  end
+
+  def target_path
+    path = File.expand_path(File.join(BASE_PATH, repo_path))
+
+    unless path.start_with?(BASE_PATH)
+      raise Puppet::Error, "Target path '#{path}' is invalid."
+    end
+
+    path
+  end
+
+  def install_options
+    resource[:install_options]
+  end
+
+  # extract the value given for the first matching named key within
+  # any hash passed to the package resource's install_options parameter.
+  # this is used to pass the 'owner' argument so that the deployment root
+  # can be owned by the right user. This is necessary in for deploy-local, as
+  # it does not typically run as root.
+  #
+  # Note: package_settings would be a cleaner solution for this, however,
+  # that feature requires puppet >= 3.5 and we must support version 3.4
+  def install_option(key, default=nil)
+    return unless install_options
+    install_options.each { |val|
+      case val
+      when Hash
+        if val[key]
+          return val[key]
+        end
+      end
+    }
+    default
+  end
+
+end
