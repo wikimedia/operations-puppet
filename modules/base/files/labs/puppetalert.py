a221336668a93f6532059be807824732d1b66969
andrewbogott <abogott@wikimedia.org>
2016-01-05 18:43:09 -0600

    Send email to project admins if puppet runs are failing.
    Install a daily cron that sends an eail to all project admins if
    it has been more than 24 hours since the last puppet run.
    
    This is only enabled in tools for the time being.  If it doesn't
    run horribly amok then we'll enable it labs-wide.
    
    Bug: T121773
    Change-Id: I20c60008cfc46842cc1b91a2087ccb4b77c7173a

---
+++ modules/base/files/labs/puppetalert.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+# Copyright 2016 Andrew Bogott <andrewbogott@gmail.com> and
+#   Yuvi Panda <yuvipanda@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Send an alert email to project admins about a puppet failure.  This is
+meant to be run on the affected instance.
+"""
+import argparse
+import calendar
+import time
+import ldap
+import socket
+import yaml
+import subprocess
+
+# Nag if it's been 24 hours since the last puppet run
+NAG_INTERVAL=60*60*24
+
+with open('/etc/wmflabs-project') as f:
+    PROJECT_NAME = f.read().strip()
+
+
+def connect(server, username, password):
+    conn = ldap.initialize('ldap://%s:389' % server)
+    conn.protocol_version = ldap.VERSION3
+    conn.start_tls_s()
+    conn.simple_bind_s(username, password)
+    return conn
+
+
+def get_user_keys(conn, user):
+    response = conn.search_s(
+        user,
+        ldap.SCOPE_BASE
+    )
+    for _, user in response:
+        return user['sshPublicKey']
+
+
+def get_group_keys(conn, groupname):
+    response = conn.search_s(
+        groupname,
+        ldap.SCOPE_BASE
+    )
+    # only one service group can have that name
+    assert len(response) <= 1
+    if response:
+        sg = response[0][1]
+        keys = []
+        for member in sg['member']:
+            keys += get_user_keys(conn, member)
+        return keys
+    else:
+        return []
+
+
+def scold():
+    with open('/etc/ldap.yaml') as f:
+        config = yaml.safe_load(f)
+
+    conn = connect(config['servers'][0], config['user'], config['password'])
+    roledn = "cn=projectadmin,cn=%s,ou=projects,%s" % (PROJECT_NAME,
+                                                       config['basedn'])
+
+    hostname = socket.gethostname()
+
+    body = "Puppet is failing to run on the %s instance in the "\
+           "Wikimedia Labs project %s."\
+           "\n\n"\
+           "Working puppet runs are needed to maintain instance security and "\
+           "logins.  As long as puppet\ncontinues to fail, this system is in "\
+           "danger of becoming unreachable."\
+           "\n\n"\
+           "You are receiving this email because you are listed as an "\
+           "administrator for the project that\ncontains this instance. "\
+           " Please take steps to repair this instance or contact a Labs "\
+           "admin\nfor assistance."\
+           "\n\n"\
+           "For further support, visit #wikimedia-labs on freenode or visit "\
+           "http://www.wikitech.org"\
+           % (hostname, PROJECT_NAME)
+
+    subject = "Alert:  puppet failed on %s.%s.eqiad.wmflabs" % (hostname,
+                                                                PROJECT_NAME)
+
+    adminrec = conn.search_s(
+        roledn,
+        ldap.SCOPE_BASE
+    )
+    admins = adminrec[0][1]['roleOccupant']
+
+    for admin in admins:
+        userrec = conn.search_s(admin, ldap.SCOPE_BASE)
+        email = userrec[0][1]['mail'][0]
+
+        args = ['/usr/bin/mail', '-s', subject, email]
+
+        p = subprocess.Popen(args, stdout=subprocess.PIPE,
+                             stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+        p.communicate(input=body)[0]
+
+
+def lastrun():
+    datafile = file('/var/lib/puppet/state/last_run_summary.yaml')
+    for line in datafile:
+        fields = line.strip().split(': ')
+        if fields[0] == 'last_run':
+            return int(fields[1])
+    return 0
+
+    
+def main():
+    elapsed = calendar.timegm(time.gmtime()) - lastrun()
+    if elapsed > NAG_INTERVAL:
+        scold()
+
+
+if __name__ == '__main__':
+    main()
--- modules/base/manifests/labs.pp
+++ modules/base/manifests/labs.pp
@@ -22,4 +22,21 @@ class base::labs inherits base {
             source => 'puppet:///modules/base/labs/nfs-common.default',
         }
     }
+
+    file { '/usr/local/sbin/puppetalert.py'
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0544',
+        source => 'puppet:///modules/base/labs/puppetalert.py',
+    }
+
+    if hiera('send_puppet_failure_emails', false) {
+        cron { 'send_puppet_failure_emails':
+            ensure  => present,
+            command => '/usr/local/sbin/puppetalert.py',
+            hour    => 8,
+            user    => 'root',
+        }
+    }
 }
