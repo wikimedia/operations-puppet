a183beb1689d18d7816c44f3e596547f6f7b97c0
Subramanya Sastry <ssastry@wikimedia.org>
2016-01-11 00:03:43 -0800

    Make testreduce generic and instantiate parsoid-rt services
    * Testreduce is used both for parsoid-rt testing and parsoid
      visual diff testing. In the future, it will be used for
      other visual diff testing.
    
    * Define instantiable testreduce::server and testreduce::client
      services.
    
    * Create a parsoid-rt-server and parsoid-rt-client roles.
      Right now, ruthenium runs both, but this is not going to be
      the case for long. We are going to move them to different
      nodes.
    
      Copied over some of the files from ruthenium into the
      puppet repo. Later patches can improve on this and
      make some of these hardcoded files templates.
      But, good enough for now.
    
    * Later patches will create generic visual diffing modules and
      use the visual diff and testreduce modules to instantiate the
      parsoid visual diffing services.
    
    Bug: T118778
    Change-Id: I4af27c21f31c9ab5b4fd351dd948866cc3604433

---
+++ manifests/role/parsoid-rt-client.pp
@@ -0,0 +1,16 @@
+# This instantiates testreduce::client
+class role::parsoid-rt-client {
+    include ::testreduce
+
+    file { '/srv/testreduce/parsoid-rt-client.rttest.localsettings.js':
+        source => 'puppet:///modules/testreduce/parsoid-rt-client.rttest.localsettings.js',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        notify => Service['parsoid-rt-client'],
+    }
+
+    testreduce::client { 'parsoid-rt-client':
+        instance_name => 'parsoid-rt-client',
+    }
+}
