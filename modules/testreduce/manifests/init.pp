db5465eb54642d4eca4efe45ac5b8003d839206a
Subramanya Sastry <ssastry@wikimedia.org>
2016-01-11 00:03:43 -0800

    Make testreduce generic and instantiate parsoid-rt services
    * Testreduce is used both for parsoid-rt testing and parsoid
      visual diff testing. In the future, it will be used for
      other visual diff testing.
    
    * Define instantiable testreduce::server and testreduce::client
      services.
    
    * Create a parsoid-rt-server and parsoid-rt-client roles.
      Right now, ruthenium runs both, but this is not going to be
      the case for long. We are going to move them to different
      nodes.
    
      Copied over some of the files from ruthenium into the
      puppet repo. Later patches can improve on this and
      make some of these hardcoded files templates.
      But, good enough for now.
    
    * Later patches will create generic visual diffing modules and
      use the visual diff and testreduce modules to instantiate the
      parsoid visual diffing services.
    
    Bug: T118778
    Change-Id: I4af27c21f31c9ab5b4fd351dd948866cc3604433

--- modules/testreduce/manifests/init.pp
+++ modules/testreduce/manifests/init.pp
@@ -1,37 +1,11 @@
 # == Class: testreduce
 #
-# Parsoid round-trip test result aggregator.
+# This file provides the following:
+# - the testreduce code repository
+# - definition for instantiating a testreduce server
+# - definition for instantiating a testreduce client
 #
-# === Parameters
-#
-# [*db_name*]
-#   Database name for storing results.
-#
-# [*db_user*]
-#   Database user.
-#
-# [*db_host*]
-#   MySQL host. Default: 'localhost'.
-#
-# [*db_port*]
-#   MySQL port. Default: 3306.
-#
-# [*coord_port*]
-#   The result aggregator will listen on this port. Default: 8002.
-#
-# [*webapp_port*]
-#   The user-facing webapp that displays test results will listen on
-#   this port. Default: 8003.
-#
-class testreduce(
-    $db_name,
-    $db_user,
-    $db_pass,
-    $db_host     = 'localhost',
-    $db_port     = 3306,
-    $coord_port  = 8002,
-    $webapp_port = 8003,
-) {
+class testreduce {
     require_package('nodejs')
     require_package('npm')
 
@@ -61,32 +35,103 @@ class testreduce(
         mode   => '0755',
     }
 
-    file { '/etc/testreduce/settings.js':
-        content => template('testreduce/settings.js.erb'),
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        notify  => Service['testreduce'],
-    }
-
-    file { '/etc/init/testreduce.conf':
-        source => 'puppet:///modules/testreduce/testreduce.upstart.conf',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        notify => Service['testreduce'],
-    }
-
     git::clone { 'mediawiki/services/parsoid/testreduce':
         ensure    => latest,
         owner     => 'root',
         group     => 'wikidev',
         directory => '/srv/testreduce',
-        notify    => Service['testreduce'],
+        # FIXME: Is this notification required?
+        # There can be multiple services that might
+        # be instantiated using the code from this
+        # repository. The only way to notify all those
+        # services would be hardcode all their names here.
+        #
+        # notify    => Service[$instance_name],
     }
 
-    service { 'testreduce':
-        ensure   => running,
-        provider => upstart,
+    # === Parameters
+    #
+    # [*instance_name*]
+    #   Name of the testreduce service
+    #
+    # [*db_name*]
+    #   Database name for storing results.
+    #
+    # [*db_user*]
+    #   Database user.
+    #
+    # [*db_host*]
+    #   MySQL host. Default: 'localhost'.
+    #
+    # [*db_port*]
+    #   MySQL port. Default: 3306.
+    #
+    # [*coord_port*]
+    #   The result aggregator will listen on this port. Default: 8002.
+    #
+    # [*webapp_port*]
+    #   The user-facing webapp that displays test results will listen on
+    #   this port. Default: 8003.
+    #
+    define testreduce::server(
+        $instance_name,
+        $db_name,
+        $db_user,
+        $db_pass,
+        $db_host     = 'localhost',
+        $db_port     = 3306,
+        $coord_port  = 8002,
+        $webapp_port = 8003,
+    ) {
+        file { "/etc/testreduce/${instance_name}.settings.js":
+            content => template("testreduce/${instance_name}.settings.js.erb"),
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            notify  => Service[$instance_name],
+        }
+
+        file { "/etc/init/${instance_name}.conf":
+            source => "puppet:///modules/testreduce/${instance_name}.upstart.conf",
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            notify => Service[$instance_name],
+        }
+
+        service { $instance_name:
+            ensure   => running,
+            provider => upstart,
+        }
+    }
+
+    # === Parameters
+    #
+    # [*instance_name*]
+    #   Name of the testreduce client service
+    #
+    define testreduce::client(
+        $instance_name
+    ) {
+        file { "/etc/testreduce/${instance_name}.config.js":
+            source => "puppet:///modules/testreduce/${instance_name}.config.js",
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            notify => Service[$instance_name],
+        }
+
+        file { "/etc/init/${instance_name}.conf":
+            source => "puppet:///modules/testreduce/${instance_name}.upstart.conf",
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            notify => Service[$instance_name],
+        }
+
+        service { $instance_name:
+            ensure   => running,
+            provider => upstart,
+        }
     }
 }
