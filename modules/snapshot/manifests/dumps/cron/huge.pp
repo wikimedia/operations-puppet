1488d8988170ffbabe24647cca5e858dd2a4c418
Ariel T. Glenn <ariel@wikimedia.org>
2016-01-13 06:06:33 +0200

    dumps: set up but don't enable script for dumps to run from cron
    also add separate creation stages for small, big wikis.
    we must check each group separately to see if the directories
    exist, so we'll be creating each group of dirs separately if
    needed.
    
    Bug: T107750
    
    Change-Id: I20bc0c2e7fa0a4781343fe852bdb36ded197343d

---
+++ modules/snapshot/manifests/dumps/cron/huge.pp
@@ -0,0 +1,20 @@
+class snapshot::dumps::cron::huge(
+    $enable = true,
+    $user   = undef,
+) {
+    include snapshot::dirs
+    include snapshot::dumps::cron
+
+    # fixme there is an implicit dependency on
+    # $dumpsdir/confs/wikidump.conf.* plus some stage files, make explicit
+    cron { 'fulldumps_huge':
+        # not ready yet
+        ensure      => 'absent',
+        environment => 'MAILTO=ops-dumps@wikimedia.org',
+        user        => $user,
+        command     => "${snapshot::dirs::dumpsdir}/fulldumps.sh 01 14 hugewikis >/dev/null",
+        minute      => '05',
+        hour        => '02',
+        monthday    => '01-14',
+    }
+}
