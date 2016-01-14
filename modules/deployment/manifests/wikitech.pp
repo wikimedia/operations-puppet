581ba15f488f980d90623723e85e21cdd7691bcb
andrewbogott <abogott@wikimedia.org>
2016-01-14 09:07:33 -0600

    Remove use of passwords::openstack::nova::nova_ldap_user_pass
    This value was duplicated in heira.  Standardizing on a hiera lookup
    will allow us to remove
    
    nova_ldap_user_pass
    and
    keystone_ldap_user_pass
    
    from the password module.
    
    Change-Id: I28d876ea8c3b8f15168b944de508d42675d5dcb2

--- modules/deployment/manifests/wikitech.pp
+++ modules/deployment/manifests/wikitech.pp
@@ -1,10 +1,10 @@
 # === Class deployment::wikitech
 # Installs the private settings file for wikitech connection to ldap
 class deployment::wikitech {
-    include passwords::openstack::nova
+    $keystoneconfig = hiera_hash('keystoneconfig', {})
 
-    $wikitech_nova_ldap_proxyagent_pass = $passwords::openstack::nova::nova_ldap_proxyagent_pass
-    $wikitech_nova_ldap_user_pass       = $passwords::openstack::nova::nova_ldap_user_pass
+    $wikitech_nova_ldap_proxyagent_pass = $keystoneconfig['ldap_proxyagent_pass']
+    $wikitech_nova_ldap_user_pass       = $keystoneconfig['ldap_user_pass']
 
     # Drop this file onto the mediawiki deployment host so that the passwords are deployed
     file { '/srv/mediawiki/private/WikitechPrivateLdapSettings.php':
