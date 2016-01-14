36ff3e207e3063a17313dacaf7c6a7745c1f96cd
Subramanya Sastry <ssastry@wikimedia.org>
2016-01-13 16:23:42 -0800

    WIP: Add the visualdiff module; instantiate psd visualdiffing services
    * Several FIXMEs that need resolving.
    * Initial patch for feedback.
    
    Change-Id: Iae0a9515ff43d81185ee11c103deb20002f8516a

---
+++ manifests/role/parsoid-vd-client.pp
@@ -0,0 +1,11 @@
+# This instantiates testreduce::client for visual diff testing
+class role::parsoid-vd-client {
+	# FIXME Are these includes required?
+	# All I want to do is ensure that these repositories are initialized
+    include ::testreduce
+	include ::visualdiff
+
+    testreduce::client { 'parsoid-vd-client':
+        instance_name => 'parsoid-vd-client',
+    }
+}
