04640ffb58513764c0e491b7d28fab41e3d7da8b
Subramanya Sastry <ssastry@wikimedia.org>
2016-01-13 16:23:42 -0800

    Add the visualdiff module + instantiate visualdiffing services
    * The visualdiff module provides:
    - diffserver: a standalone webapp for one-off visualdiffing
      not very useful and is not instantiated in this patch.
    - parsoid-vd and parsoid-vd-client: testreduce-based server
      and client for comparing Parsoid HTML output with PHP parser
      output.
    
    * Several minor non-functional tweaks to testreduce puppet code.
    
    * Replaced upstart config files with systemd native config files.
    
    Bug: T118778
    Change-Id: Iae0a9515ff43d81185ee11c103deb20002f8516a

--- manifests/role/parsoid-rt-server.pp
+++ manifests/role/parsoid-rt-server.pp
@@ -6,6 +6,8 @@ class role::parsoid-rt-server {
 
     testreduce::server { 'parsoid-rt':
         instance_name => 'parsoid-rt',
+        # FIXME: Maybe rename db to parsoid_rt
+        # (or parsoid-rt if hyphens are allowed in db names)
         db_name => 'testreduce_0715',
         db_user => 'testreduce',
         db_pass => '',  # FIXME
