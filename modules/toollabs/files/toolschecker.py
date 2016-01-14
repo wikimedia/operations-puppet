bdfa2a8f70ac268800f6d8ed0fffee0983a340e0
YuviPanda <yuvipanda@gmail.com>
2016-01-13 16:56:42 -0800

    toolschecker: Should use webservice rather than webservice2
    webservice2 is just a symlink
    
    Change-Id: I941c0358d5aa5cf4c9e324d0c1b111a07b394e8d

--- modules/toollabs/files/toolschecker.py
+++ modules/toollabs/files/toolschecker.py
@@ -338,7 +338,7 @@ def service_start_test():
     success = False
     with open(os.devnull, 'w') as devnull:
         subprocess.check_call(
-            ['/usr/local/bin/webservice2', 'uwsgi-python', 'start'],
+            ['/usr/local/bin/webservice', 'uwsgi-python', 'start'],
             stderr=devnull, stdout=devnull)
 
     for i in range(0, 10):
@@ -350,7 +350,7 @@ def service_start_test():
 
     with open(os.devnull, 'w') as devnull:
         subprocess.check_call(
-            ['/usr/local/bin/webservice2', 'uwsgi-python', 'stop'],
+            ['/usr/local/bin/webservice', 'uwsgi-python', 'stop'],
             stderr=devnull, stdout=devnull)
 
     # Make sure it really stopped
