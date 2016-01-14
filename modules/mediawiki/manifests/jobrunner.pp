98ae0ad44c2a30bdbc7128f74aeb3a2d09ec8226
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-01-14 09:17:25 +0100

    jobrunner: contain gwt jobs to run on two specific hosts
    Since GWT jobs are leaking memory and it has caused instabilities on the
    jobrunners, we do as follows:
    
    * Remove the hack introduced in I44990808 to contain running GWT runners
      on odd-numbered jobrunners
    * Contain gwt jobs to run on two specific servers, which will be running
      those jobs exclusively. We use two newer, high-memory ones.
    * Modify the cronjob hack to run each hour on those machines, and to
      restart HHVM once it occupies a sizeable amount of the memory
    
    Bug: T122069
    Change-Id: I3ff20b324ee98e93a926e0aa1ed568cc9343ae90

--- modules/mediawiki/manifests/jobrunner.pp
+++ modules/mediawiki/manifests/jobrunner.pp
@@ -107,11 +107,14 @@ class mediawiki::jobrunner (
             content  => template('mediawiki/jobrunner/site.conf.erb')
         }
 
-        # Hack for T122069: Once a day, check the uptime of HHVM. If HHVM has
-        # been running for more than a day, restart it.
-        cron { 'periodic_hhvm_restart':
-            command => '/bin/ps -C hhvm -o etime= | /bin/grep -q - && /sbin/initctl restart hhvm >/dev/null 2>/dev/null',
-            hour    => fqdn_rand(23, 'periodic_hhvm_restart'),
+        # Hack for T122069: on servers running GWT jobs, restart HHVM
+        # once it occupies more than 60% of the available memory
+        if ($runners_gwt > 0) {
+            cron { 'periodic_hhvm_restart':
+                command => '/bin/ps -C hhvm -o pmem= | awk \'{sum+=$1} END { if (sum <= 60.0) exit 1  }\'  && /usr/sbin/service hhvm restart >/dev/null 2>/dev/null',
+                minute  => fqdn_rand(59, 'periodic_hhvm_restart'),
+            }
+
         }
     }
 
