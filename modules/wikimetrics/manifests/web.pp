18c3bc63f996296f9f8f1824270f34b8ea0664fa
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-01-15 15:02:20 -0800

    wikimetrics: Allow CORS for /static/public
    Change-Id: Ifd1f65e72bdb7d782cab6592e0b596d546cce784

--- modules/wikimetrics/manifests/web.pp
+++ modules/wikimetrics/manifests/web.pp
@@ -18,6 +18,8 @@ class wikimetrics::web(
                 processes    => $workers,
                 # Serve static files on /static
                 static-map   => "/static=${wikimetrics::base::source_path}/wikimetrics/static",
+                # Add Allow CORS header for /static/public
+                route-if     => "equal:${PATH_INFO};/static/public addheader:Access-Control-Allow-Origin: *",
                 # Look at the X_FORWARDED_PROTO, and if it's http, redirect to https
                 route-if     => "equal:${HTTP_X_FORWARDED_PROTO};http redirect-permanent:https://${HTTP_HOST}${REQUEST_URI}",
             }
