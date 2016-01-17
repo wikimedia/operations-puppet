2f2f456fe9a5d7e9d886546e65aa46de0e64b47e
andrewbogott <abogott@wikimedia.org>
2016-01-17 02:02:08 -0600

    Tidy up wiki crons for labtestwiki:
    - Don't run backups or -static syncs on labtestwiki
    - Refer to the proper db name when running jobs and updating smw
    
    Change-Id: Ib9f10461e44bf7a6e270cff96b56ea6f52a73521

--- modules/openstack/manifests/openstack_manager.pp
+++ modules/openstack/manifests/openstack_manager.pp
@@ -104,44 +104,59 @@ class openstack::openstack_manager(
             target => '/srv/mediawiki';
     }
 
+    case $::realm {
+        'production': {
+            $wikidb = 'labswiki'
+
+            cron {
+                'db-bak':
+                    ensure  => present,
+                    user    => 'root',
+                    hour    => 1,
+                    minute  => 0,
+                    command => '/usr/local/sbin/db-bak.sh > /dev/null 2>&1',
+                    require => File['/a/backup'];
+                'mw-xml':
+                    ensure  => present,
+                    user    => 'root',
+                    hour    => 1,
+                    minute  => 30,
+                    command => '/usr/local/sbin/mw-xml.sh > /dev/null 2>&1',
+                    require => File['/a/backup'];
+                'mw-files':
+                    ensure  => present,
+                    user    => 'root',
+                    hour    => 2,
+                    minute  => 0,
+                    command => '/usr/local/sbin/mw-files.sh > /dev/null 2>&1',
+                    require => File['/a/backup'];
+                'backup-cleanup':
+                    ensure  => present,
+                    user    => 'root',
+                    hour    => 3,
+                    minute  => 0,
+                    command => 'find /a/backup -type f -mtime +4 -delete',
+                    require => File['/a/backup'];
+            }
+        }
+        'labtest': {
+            $wikidb = 'labtestwiki'
+        }
+        default: {
+            fail ("unknown realm ${::realm}")
+        }
+    }
+
     cron {
         'run-jobs':
             ensure  => present,
             user    => $::mediawiki::users::web,
-            command => '/usr/local/bin/mwscript maintenance/runJobs.php --wiki=labswiki > /dev/null 2>&1';
+            command => "/usr/local/bin/mwscript maintenance/runJobs.php --wiki=${wikidb} > /dev/null 2>&1";
         'update-smw':
             ensure  => present,
             user    => $::mediawiki::users::web,
             hour    => '*/6',
             minute  => 20,
-            command => '/usr/local/bin/mwscript extensions/SemanticMediaWiki/maintenance/SMW_refreshData.php --wiki=labswiki > /dev/null 2>&1';
-        'db-bak':
-            ensure  => present,
-            user    => 'root',
-            hour    => 1,
-            minute  => 0,
-            command => '/usr/local/sbin/db-bak.sh > /dev/null 2>&1',
-            require => File['/a/backup'];
-        'mw-xml':
-            ensure  => present,
-            user    => 'root',
-            hour    => 1,
-            minute  => 30,
-            command => '/usr/local/sbin/mw-xml.sh > /dev/null 2>&1',
-            require => File['/a/backup'];
-        'mw-files':
-            ensure  => present,
-            user    => 'root',
-            hour    => 2,
-            minute  => 0,
-            command => '/usr/local/sbin/mw-files.sh > /dev/null 2>&1',
-            require => File['/a/backup'];
-        'backup-cleanup':
-            ensure  => present,
-            user    => 'root',
-            hour    => 3,
-            minute  => 0,
-            command => 'find /a/backup -type f -mtime +4 -delete',
-            require => File['/a/backup'];
+            command => "/usr/local/bin/mwscript extensions/SemanticMediaWiki/maintenance/SMW_refreshData.php --wiki=${wikidb} > /dev/null 2>&1";
     }
 }
