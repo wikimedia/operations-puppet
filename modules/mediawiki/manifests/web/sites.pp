4758c1d476f77441239d1bdbaa2afd7a3e9ebf09
Alex Monk <krenair@gmail.com>
2016-01-19 18:07:59 +0000

    Move portals into generic sites.pp
    Bug: T86644
    Change-Id: Ibf26a1fc51edf48a2c4bd7c5cf82f2a7a5225e9c

--- modules/mediawiki/manifests/web/sites.pp
+++ modules/mediawiki/manifests/web/sites.pp
@@ -1,8 +1,19 @@
 class mediawiki::web::sites {
     tag 'mediawiki', 'mw-apache-config'
 
+    if $::realm == 'labs' {
+        $domain_suffix = 'beta.wmflabs.org'
+    } else {
+        $domain_suffix = 'org'
+    }
+
     apache::site { 'nonexistent':
         source   => 'puppet:///modules/mediawiki/apache/sites/nonexistent.conf',
         priority => 0,
+    }
+
+    apache::site { 'wwwportals':
+        content  => template('mediawiki/apache/sites/wwwportals.conf.erb'),
+        priority => 1,
     }
 }
