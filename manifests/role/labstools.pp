affd737a8267d1bb849d3d669ac0fe45b7a73375
YuviPanda <yuvipanda@gmail.com>
2016-01-19 19:09:40 -0800

    toollabs: Point shadow to correct master host
    Change-Id: I2f2d629d26175eced48eddd18da1a132cf19a129

--- manifests/role/labstools.pp
+++ manifests/role/labstools.pp
@@ -27,7 +27,7 @@ class role::labs::tools {
         system::role { 'role::labs::tools::shadow': description => 'Tool Labs gridengine shadow (backup) master' }
 
         class { 'toollabs::shadow':
-            gridmaster => $role::labs::tools::common::gridmaster,
+            gridmaster => hiera('gridengine::gridmaster'),
         }
     }
 
