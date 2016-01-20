b99f9af0eabc45739794407b453e9c7bba85597a
YuviPanda <yuvipanda@gmail.com>
2016-01-19 17:20:24 -0800

    toollabs: Add cronrunner role
    Splitting up submit_host to clearer sub-roles
    
    Bug: T123873
    Change-Id: Ic1e0e1d593974dca1979034acfb84b97e86db94f

---
+++ modules/toollabs/manifests/cronrunner.pp
@@ -0,0 +1,17 @@
+class toollabs::cronrunner {
+    include gridengine::submit_host,
+            toollabs::hba
+
+    file { '/etc/ssh/ssh_config':
+        ensure => file,
+        mode   => '0444',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/toollabs/submithost-ssh_config',
+    }
+
+    motd::script { 'submithost-banner':
+        ensure => present,
+        source => "puppet:///modules/toollabs/40-${::labsproject}-submithost-banner",
+    }
+}
