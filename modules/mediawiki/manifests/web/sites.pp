622ca683f8320d01b1d455a008f0373b2bcb29d9
Alex Monk <krenair@gmail.com>
2016-01-21 18:43:42 +0000

    Move apache includes into generic sites.pp
    Bug: T86644
    Change-Id: I32d3dc13ca857e61f9d296382892592e27807b92

--- modules/mediawiki/manifests/web/sites.pp
+++ modules/mediawiki/manifests/web/sites.pp
@@ -3,6 +3,37 @@ class mediawiki::web::sites (
 ) {
     tag 'mediawiki', 'mw-apache-config'
 
+    #common code snippets that are included in the virtualhosts.
+    file { '/etc/apache2/sites-enabled/wikimedia-common.incl':
+        ensure  => present,
+        content => template('mediawiki/apache/sites/wikimedia-common.incl.erb'),
+        before  => Service['apache2'],
+    }
+
+    file { '/etc/apache2/sites-enabled/wikimedia-legacy.incl':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/apache/sites/wikimedia-legacy.incl',
+        before => Service['apache2'],
+    }
+
+    file { '/etc/apache2/sites-enabled/public-wiki-rewrites.incl':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/apache/sites/public-wiki-rewrites.incl',
+        before => Service['apache2'],
+    }
+
+    file { '/etc/apache2/sites-enabled/api-rewrites.incl':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/apache/sites/api-rewrites.incl',
+        before => Service['apache2'],
+    }
+
+    file { '/etc/apache2/sites-enabled/wikidata-uris.incl':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/apache/sites/wikidata-uris.incl',
+        before => Service['apache2'],
+    }
+
     apache::site { 'nonexistent':
         source   => 'puppet:///modules/mediawiki/apache/sites/nonexistent.conf',
         priority => 0,
