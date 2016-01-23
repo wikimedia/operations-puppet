111471f52407facad28a4cda29b729ea7c0b00b1
Erik Bernhardson <ebernhardson@wikimedia.org>
2016-01-23 11:57:11 -0800

    Add alert for elasticsearch 50th percentile prefix search time
    Typically prefix search is around 10-30ms. If it hits 75 or 150ms
    there is almost certainly something wrong that should be addressed.
    
    Requires adding discovery-alerts@lists.wikimedia.org to the private
    puppet repo containing alerting email addresses (contacts.cfg)
    
    Change-Id: I9c7b79f7af221c0d32ba1c6baa39c55f1bc92d8d

---
+++ modules/role/manifests/elasticsearch/alerts.pp
@@ -0,0 +1,11 @@
+class role::elasticsearch::alerts {
+    monitoring::graphite_threshold { 'prefix_search_50th_percentile':
+        description    => 'Prefix search 50th percentile latency'
+        metric         => 'transformNull(MediaWiki.CirrusSearch.requestTimeMs.prefix.p50, 0)',
+        from           => '10min',
+        warning        => '75',
+        critical       => '150',
+        percentage     => '20',
+        contact_group  => 'team-discovery',
+    }
+}
