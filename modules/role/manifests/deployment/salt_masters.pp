d3d99a3e356eab34e3658edbdd6d478db788b36b
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-01-25 13:26:06 +0100

    role::deployment::salt_masters: correct a hiera lookup
    Change-Id: I03ac55c0ad598d1f24cf467c39c239d6f3c7659b

--- modules/role/manifests/deployment/salt_masters.pp
+++ modules/role/manifests/deployment/salt_masters.pp
@@ -1,17 +1,21 @@
 # === Class role::deployment::salt_masters
 # Installs deployment-related data to the salt master
 class role::deployment::salt_masters(
-    $deployment_server = hiera('deployment_server', 'tin.eqiad.wmnet'),
-) {
+    $deployment_server = undef
+    ) {
 
+    $deployment_host = $deployment_server ? {
+        undef   => hiera('deployment_server', 'tin.eqiad.wmnet'),
+        default => $deployment_server
+    }
     $deployment_config = {
         'parent_dir' => '/srv/deployment',
         'servers'    => {
-            'eqiad'  => $deployment_server,
-            'codfw'  => $deployment_server,
+            'eqiad'  => $deployment_host,
+            'codfw'  => $deployment_host,
         },
         'redis'      => {
-            'host'                   => $deployment_server,
+            'host'                   => $deployment_host,
             'port'                   => '6379',
             'db'                     => '0',
             'socket_connect_timeout' => '5',
