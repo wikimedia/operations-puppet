748cce95cac1c8b01ca0a5744c91a83f4661a8be
Ariel T. Glenn <ariel@wikimedia.org>
2016-01-25 13:19:45 +0200

    dumps: rebalance page ranges for dump jobs that run in parallel
    do not merge until Jan dump run is complete
    
    Bug: T123571
    
    Change-Id: I0ece4aa98183c776e9c5666a201d5212761f6d1c

--- modules/snapshot/manifests/dumps/configs.pp
+++ modules/snapshot/manifests/dumps/configs.pp
@@ -20,69 +20,69 @@ class snapshot::dumps::configs(
             recombineHistory => '0',
             wikis            => {
                 ruwiki => {
-                    pagesPerChunkHistory  => '256900,895900,1280900,2000000',
+                    pagesPerChunkHistory  => '311181,1142420,1627923,3122803'
                     pagesPerChunkAbstract => '1200000',
                     chunksForAbstract     => '4',
                 },
                 eswiki => {
-                    pagesPerChunkHistory  => '190300,671500,1627200,2000000',
+                    pagesPerChunkHistory  => '229078,854374,2324057,3860337',
                     pagesPerChunkAbstract => '1500000',
                     chunksForAbstract     => '4',
                 },
                 dewiki => {
-                    pagesPerChunkHistory  => '33640,1446760,2569200,3000000',
+                    pagesPerChunkHistory  => '425449,1451596,3087456,4274631',
                     pagesPerChunkAbstract => '2000000',
                     chunksForAbstract     => '4',
                     checkpointTime        => '720',
                 },
                 ptwiki => {
-                    pagesPerChunkHistory  => '137500,668700,1208900,2000000',
+                    pagesPerChunkHistory  => '158756,789502,1361027,2669897',
                     pagesPerChunkAbstract => '1000000',
                     chunksForAbstract     => '4',
                 },
                 plwiki => {
-                    pagesPerChunkHistory  => '208000,422000,818900,2000000',
+                    pagesPerChunkHistory  => '235373,514490,957079,1923498',
                     pagesPerChunkAbstract => '800000',
                     chunksForAbstract     => '4',
                 },
                 nlwiki => {
-                    pagesPerChunkHistory  => '200800,490800,934600,2000000',
+                    pagesPerChunkHistory  => '224145,552515,1098243,2570924',
                     pagesPerChunkAbstract => '1000000',
                     chunksForAbstract     => '4',
                 },
                 frwiki => {
-                    pagesPerChunkHistory  => '348700,965200,2331100,3000000',
+                    pagesPerChunkHistory  => '412303,1235589,2771967,5313378',
                     pagesPerChunkAbstract => '1900000',
                     chunksForAbstract     => '4',
                     checkpointTime        => '720',
                 },
                 itwiki => {
-                    pagesPerChunkHistory  => '335400,941600,1180900,2000000',
+                    pagesPerChunkHistory  => '442893,1049883,1381705,2929437'
                     pagesPerChunkAbstract => '1200000',
                     chunksForAbstract     => '4',
                 },
                 jawiki => {
-                    pagesPerChunkHistory  => '149600,801600,408900,2000000',
+                  pagesPerChunkHistory  => '168815,480631,943865,1760565',
                     pagesPerChunkAbstract => '800000',
                     chunksForAbstract     => '4',
                 },
                 commonswiki => {
-                    pagesPerChunkHistory  => '6440000,8960000,11260000,20000000',
+                    pagesPerChunkHistory  => '6457504,9672260,12929298,17539944',
                     pagesPerChunkAbstract => '11000000',
                     chunksForAbstract     => '4',
                 },
                 wikidatawiki => {
-                    pagesPerChunkHistory  => '2300000,4500000,8600000,10000000',
+                    pagesPerChunkHistory  => '2421529,4883997,8784997,8199134',
                     pagesPerChunkAbstract => '5800000',
                     chunksForAbstract     => '4',
                 },
                 zhwiki => {
-                    pagesPerChunkHistory  => '280000,740000,2100000,3000000',
+                    pagesPerChunkHistory  => '231819,564192,1300322,3112369'
                     pagesPerChunkAbstract => '1300000',
                     chunksForAbstract     => '4',
                 },
                 metawiki => {
-                    pagesPerChunkHistory  => '810000,2700000,3200000,3400000',
+                    pagesPerChunkHistory  => '823386,2594392,3242670,3284506',
                     pagesPerChunkAbstract => '2500000',
                     chunksForAbstract     => '4',
                 },
@@ -97,7 +97,7 @@ class snapshot::dumps::configs(
             checkpointTime   => '720',
             wikis => {
                 enwiki => {
-                    pagesPerChunkHistory  => '10000,15000,30000,50000,80000,120000,160000,200000,260000,400000,500000,600000,700000,800000,900000,1200000,1500000,1700000,1900000,2200000,2400000,2500000,2700000,2800000,2900000,3000000,3000000',
+                    pagesPerChunkHistory  => '30303,58141,112065,152180,212624,327599,375779,522388,545343,710090,880349,1113575,1157158,1547206,1773248,2021218,2153807,2427469,2634193,2467421, 2705827,2895677,3679790,3449365,4114387,4596259,6533612',
                     pagesPerChunkAbstract => '2000000',
                 },
             },
