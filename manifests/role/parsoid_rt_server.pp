3e49c920d2422872d15d281f3900f47e5b8f01bc
Subramanya Sastry <ssastry@wikimedia.org>
2016-01-27 09:32:37 -0600

    ruthenium services: Use puppetized db credentials for testreduce dbs
    * Also use m5-master.eqiad.wmnet as the db host
    
    Bug: T124704
    Change-Id: Ib5bf3489da9a3262605a0b78c7e8dd36e3d12265

--- manifests/role/parsoid_rt_server.pp
+++ manifests/role/parsoid_rt_server.pp
@@ -3,14 +3,14 @@
 # This instantiates testreduce::server
 class role::parsoid_rt_server {
     include ::testreduce
+    include ::passwords::testreduce::mysql
 
     testreduce::server { 'parsoid-rt':
         instance_name => 'parsoid-rt',
-        # FIXME: Maybe rename db to parsoid_rt
-        # (or parsoid-rt if hyphens are allowed in db names)
+        db_host => 'm5-master.eqiad.wmnet',
         db_name => 'testreduce_0715',
         db_user => 'testreduce',
-        db_pass => '',  # FIXME
+        db_pass => $db_pass,
     }
 
 }
--- manifests/role/parsoid_vd_server.pp
+++ manifests/role/parsoid_vd_server.pp
@@ -3,14 +3,14 @@
 # This instantiates testreduce::server
 class role::parsoid_vd_server {
     include ::testreduce
+    include ::passwords::testreduce::mysql
 
     testreduce::server { 'parsoid-vd':
         instance_name => 'parsoid-vd',
-        # FIXME: Should we use parsoid_vd ?
-        # (or parsoid-vd if hyphens are allowed in db names)
+        db_host => 'm5-master.eqiad.wmnet',
         db_name => 'testreduce_vd',
         db_user => 'testreduce',
-        db_pass => '',  # FIXME
+        db_pass => $db_pass,
         webapp_port => 8010,
         coord_port => 8011,
     }
