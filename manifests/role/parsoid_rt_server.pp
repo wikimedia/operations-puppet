2b7607e637d60b2f12e54ed9bb524e189987ab24
Subramanya Sastry <ssastry@wikimedia.org>
2016-01-27 09:32:37 -0600

    ruthenium services: Use puppetized db credentials for testreduce dbs
    Bug: T124704
    Change-Id: Ib5bf3489da9a3262605a0b78c7e8dd36e3d12265

--- manifests/role/parsoid_rt_server.pp
+++ manifests/role/parsoid_rt_server.pp
@@ -3,14 +3,14 @@
 # This instantiates testreduce::server
 class role::parsoid_rt_server {
     include ::testreduce
+    include ::passwords::testreduce::mysql
 
+    # FIXME: Maybe rename db to parsoid_rt?
     testreduce::server { 'parsoid-rt':
         instance_name => 'parsoid-rt',
-        # FIXME: Maybe rename db to parsoid_rt
-        # (or parsoid-rt if hyphens are allowed in db names)
         db_name => 'testreduce_0715',
-        db_user => 'testreduce',
-        db_pass => '',  # FIXME
+        db_user => $db_user,
+        db_pass => $db_pass,
     }
 
 }
