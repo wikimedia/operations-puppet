986233bf0fd253b0f450d908848313d2861dc9c0
Andrew Otto <acotto@gmail.com>
2016-01-29 17:15:31 -0500

    [WIP] Refactor manifests/role/analytics/* into modules/role, use hiera to configure
    Bug: T109859
    Change-Id: If66d8e3662cda6344fc5786c82f2a4cc05c9d1f6

---
+++ modules/role/manifests/analytics_new/hadoop/ferm/resourcemanager.pp
@@ -0,0 +1,61 @@
+# == Class role::analytics::hadoop::ferm::resourcemanager
+#
+class role::analytics_new::hadoop::ferm::resourcemanager {
+
+    ferm::service{ 'hadoop-yarn-resourcemanager-scheduler':
+        proto  => 'tcp',
+        port   => '8030',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop-yarn-resourcemanager-tracker':
+        proto  => 'tcp',
+        port   => '8031',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop-yarn-resourcemanager':
+        proto  => 'tcp',
+        port   => '8032',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop-yarn-resourcemanager-admin':
+        proto  => 'tcp',
+        port   => '8033',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop-yarn-resourcemanager-http-ui':
+        proto  => 'tcp',
+        port   => '8088',
+        srange => '$INTERNAL',
+    }
+
+    ferm::service{ 'hadoop-mapreduce-historyserver':
+        proto  => 'tcp',
+        port   => '10020',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop-mapreduce-historyserver-admin':
+        proto  => 'tcp',
+        port   => '10033',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop-mapreduce-historyserver-http-ui':
+        proto  => 'tcp',
+        port   => '19888',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+    ferm::service{ 'hadoop-yarn-resourcemanager-jmx':
+        proto  => 'tcp',
+        port   => '9983',
+        srange => '$ANALYTICS_NETWORKS',
+    }
+
+
+}
+
---
+++ modules/role/manifests/analytics_new/hive/metastore/database.pp
@@ -0,0 +1,38 @@
+# == Class role::analytics::hive::metastore::database
+# Includes the role::analytics::database::meta class
+# to install a database for analytics cluster meta data,
+# includes the cdh::hive::metastore::mysql
+# to ensure that the hive_metastore database is created,
+# and then finally ensures grants and permissions are
+# set so that configured hosts can properly connect to this database.
+#
+class role::analytics_new::hive::metastore::database {
+    require role::analytics_new::hive::client
+
+    # Install a database server (MariaDB)
+    include role::analytics_new::database::meta
+
+    # Ensure that the hive_metastore db is created.
+    # TODO: In CDH 5.4,
+    # /usr/lib/hive/bin/schematool -dbType mysql -initSchema
+    # doesn't seem to be working with MariaDB properly.
+    # For now, run:
+    #   cd /usr/lib/hive/scripts/metastore/upgrade/mysql && sudo mysql hive_metastore < hive-schema-1.1.0.mysql.sql
+    # after cdh::hive::metastore::mysql makes puppet fail.
+    class { 'cdh::hive::metastore::mysql':
+        require => Class['role::analytics_new::database::meta'],
+    }
+
+    # cdh::hive::metastore::mysql only ensures that
+    # the Hive MySQL user has permissions to connect
+    # via localhost.  If you plan on running
+    # the hive-metastore daemon on a different node
+    # than where you run this MySQL database, then you need
+    # to make sure that node has proper permissions to
+    # access MySQL via the hive user and pw.
+
+    # If labs, just allow access from all hosts
+    if $::realm == 'labs' {
+        cdh::hive::metastore::mysql::grant { '%': }
+    }
+}
