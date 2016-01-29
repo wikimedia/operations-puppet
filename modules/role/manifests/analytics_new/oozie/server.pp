f0d7d4c2f5ba6b50262008096d8ce76e5abacf8c
Andrew Otto <acotto@gmail.com>
2016-01-29 17:15:31 -0500

    [WIP] Refactor manifests/role/analytics/* into modules/role, use hiera to configure
    Bug: T109859
    Change-Id: If66d8e3662cda6344fc5786c82f2a4cc05c9d1f6

---
+++ modules/role/manifests/analytics_new/oozie/server.pp
@@ -0,0 +1,32 @@
+# == Class role::analytics::oozie::server
+# Installs Oozie server
+# Make sure you set hiera variables for cdh::oozie::server appropriately,
+# especially if you are hosting
+class role::analytics_new::oozie::server {
+    system::role { 'role::analytics::oozie::server':
+        description => 'Oozie Server',
+    }
+    require role::analytics_new::oozie::client
+
+    class { 'cdh::oozie::server':
+        smtp_host       => $::mail_smarthost[0],
+        smtp_from_email => "oozie@${::fqdn}",
+        # This is not currently working.  Disabling
+        # this allows any user to manage any Oozie
+        # job.  Since access to our cluster is limited,
+        # this isn't a big deal.  But, we should still
+        # figure out why this isn't working and
+        # turn it back on.
+        # I was not able to kill any oozie jobs
+        # with this on, even though the
+        # oozie.service.ProxyUserService.proxyuser.*
+        # settings look like they are properly configured.
+        authorization_service_authorization_enabled => false,
+    }
+
+    ferm::service{ 'oozie_server':
+        proto  => 'tcp',
+        port   => '11000',
+        srange => '$INTERNAL',
+    }
+}
