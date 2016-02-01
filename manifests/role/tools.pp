1b33b65c9eebefce743b839ca29b4e3eb7908690
YuviPanda <yuvipanda@gmail.com>
2016-02-01 15:48:24 -0800

    tools: Allow k8s etcd peers to access client port too
    Change-Id: I49d194781b9bf05f867457c4d0b64241ea1297a6

--- manifests/role/tools.pp
+++ manifests/role/tools.pp
@@ -16,10 +16,10 @@ class role::toollabs::etcd::k8s {
     $peer_nodes = join(hiera('k8s::etcd_hosts'), ' ')
     $k8s_master = hiera('k8s::master_host')
 
-    ferm::service { 'k8s-master':
+    ferm::service { 'etcd-clients':
         proto  => 'tcp',
         port   => '2379',
-        srange => "@resolve((${k8s_master}))"
+        srange => "@resolve((${k8s_master} ${peer_nodes}))"
     }
 
     ferm::service { 'etcd-peers':
