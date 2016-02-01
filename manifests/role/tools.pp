3344e141587687e1fd7f4960e6562041a355b0cd
YuviPanda <yuvipanda@gmail.com>
2016-02-01 13:30:35 -0800

    toollabs: Add proxy nodes to flanel etcd access list
    Change-Id: I6f79e6b807186bfd512b09c40dccefd0a6a6679f

--- manifests/role/tools.pp
+++ manifests/role/tools.pp
@@ -16,11 +16,12 @@ class role::toollabs::etcd::flannel {
 
     $worker_nodes = join(hiera('k8s::worker_hosts'), ' ')
     $peer_nodes = join(hiera('flannel::etcd_hosts'), ' ')
+    $proxy_nodes = join(hiera('toollabs::proxy::proxies'), ' ')
 
     ferm::service { 'flannel-clients':
         proto  => 'tcp',
         port   => '2379',
-        srange => "@resolve((${worker_nodes} ${peer_nodes}))"
+        srange => "@resolve((${worker_nodes} ${peer_nodes} ${proxy_nodes}))"
     }
 
     ferm::service { 'flannel-peers':
