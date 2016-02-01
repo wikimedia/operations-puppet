dafe707988bfc773659211a023f8c93dc6628fef
Tim Landscheidt <tim@tim-landscheidt.de>
2016-02-01 18:36:22 +0000

    Tools: Create separate partitions only in the Tools project
    Currently on each execution node a 16 GByte partition for /tmp and a
    swap partition of three times the physical memory are created.  In
    practice this means that all execution nodes must be of type m1.large
    or m1.xlarge because only they provide enough disk space.
    
    This makes testing setups involving multiple execution nodes in
    Toolsbeta more difficult as those types of instances consume a huge
    portion of the CPU and RAM quotas, while for testing the interactions
    between grid nodes instances of type m1.small would be more than
    sufficient.
    
    This change conditions the creation of those partitions on class
    parameters which can be disabled for Toolsbeta via wikitech.
    
    Change-Id: Ia2505aaa96cca1e0fb0799e29dfaba68a0bb3c9e

--- modules/toollabs/manifests/node/all.pp
+++ modules/toollabs/manifests/node/all.pp
@@ -10,19 +10,24 @@
 #
 # Sample Usage:
 #
-class toollabs::node::all {
-
+class toollabs::node::all(
+    $swap_partition = true,
+    $tmp_partition = true,
+) {
     include toollabs
 
-    labs_lvm::volume { 'separate-tmp':
-        size      => '16GB',
-        mountat   => '/tmp',
-        mountmode => '1777',
-        options   => 'nosuid,noexec,nodev,rw',
+    if $tmp_partition {
+        labs_lvm::volume { 'separate-tmp':
+            size      => '16GB',
+            mountat   => '/tmp',
+            mountmode => '1777',
+            options   => 'nosuid,noexec,nodev,rw',
+        }
     }
 
-    labs_lvm::swap { 'big':
-        size => inline_template('<%= @memorysize_mb.to_i * 3 %>MB'),
+    if $swap_partition {
+        labs_lvm::swap { 'big':
+            size => inline_template('<%= @memorysize_mb.to_i * 3 %>MB'),
+        }
     }
-
 }
