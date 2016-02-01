765cf375fa902c6c797cb33d5a04126a5cc97ea4
Ariel T. Glenn <ariel@wikimedia.org>
2016-02-01 15:32:06 +0200

    new salt runner to sign key for a specific minion
    this is intended for use by remote scripts such as wmf-reimage
    
    still needed: auth in salt config to permit remote hosts to run this command
    
    Bug: T124761
    Change-Id: Id6911f7606fe785c7fcd1f27b7505181574b16ba

---
+++ modules/salt/files/signkey.py
@@ -0,0 +1,23 @@
+'''
+runner: key signer, assumes caller has checked that minion key is safe to sign
+'''
+
+import logging
+import salt.key
+
+def sign(minion):
+    '''
+    sign salt key for the specified minion
+    '''
+    pending = 'minions_pre'
+
+    key = salt.key.Key(__opts__)
+    matches = key.name_match(minion)
+    keys = {}
+    if pending in matches:
+        keys[pending] = matches[pending]
+    if not keys:
+        log = logging.getLogger(__name__)
+        log.warn("No unsigned key found for minion: {0}".format(minion))
+        return
+    key.accept(match_dict=keys)
