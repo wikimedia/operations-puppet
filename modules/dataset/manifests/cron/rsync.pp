e2604104ef3e5b08adc48d44e29089a69997d33f
Ariel T. Glenn <ariel@wikimedia.org>
2016-02-03 17:39:09 +0200

    send web server logs from dataset hosts to stat1002
    add generic rsync class
    WIP: needs the declaration as soon as we know where the
    files should end up on stat1002
    
    Bug: T118739
    
    Change-Id: I18a31bc95c4ac656ea9dcdb3a603344d14036d03

---
+++ modules/dataset/manifests/cron/rsync.pp
@@ -0,0 +1,37 @@
+class dataset::cron::rsync (
+    $source = '/var/log/nginx/',
+    $enable = true,
+    $dest   = undef,
+    $user   = 'root',
+    $hour   = undef,
+    $minute = undef,
+)
+{
+    include dataset::common
+
+    if ($enable) {
+        $ensure = 'present'
+    }
+    else {
+        $ensure = 'absent'
+    }
+
+    $rsync_args = '-rt --delete --bwlimit=50000'
+    if ($enable) {
+        cron { "$source_$dest_rsync":
+            ensure      => $ensure,
+            user        => $user,
+            minute      => $minute,
+            hour        => $hour,
+            command     => "/usr/bin/rsync ${rsync_args} ${source}/ ${dest}/",
+            environment => 'MAILTO=ops-dumps@wikimedia.org',
+            require     => User[$user],
+        }
+    }
+    else {
+        cron { "$source_$dest_rsync":
+            ensure      => absent,
+        }
+    }
+}
+
