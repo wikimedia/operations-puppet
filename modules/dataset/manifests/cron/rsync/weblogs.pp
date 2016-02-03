416a614686646def0ec92ed004202b71a4ebdb83
Ariel T. Glenn <ariel@wikimedia.org>
2016-02-03 17:39:09 +0200

    send web server logs from dataset hosts to stat1002
    WIP:
    this is only the dataset half, no role yet either
    we need the rsyncd conf setup on stat1002
    
    Bug: T118739
    
    Change-Id: I18a31bc95c4ac656ea9dcdb3a603344d14036d03

---
+++ modules/dataset/manifests/cron/rsync/weblogs.pp
@@ -0,0 +1,35 @@
+class dataset::cron::rsync::weblogs (
+    $source = '/var/log/nginx/',
+    $enable = true,
+    $dest   = undef,
+    $user   = 'root',
+)
+{
+    include dataset::common
+
+    if ($enable) {
+        $ensure = 'present'
+    }
+    else {
+        $ensure = 'absent'
+    }
+
+    $rsync_args = '-rt --delete --bwlimit=50000'
+    if ($enable) {
+        cron { 'weblogs_stat1002_rsync':
+            ensure      => $ensure,
+            user        => $user,
+            minute      => '50',
+            hour        => '4',
+            command     => "/usr/bin/rsync ${rsync_args} ${source}/ ${dest}/",
+            environment => 'MAILTO=ops-dumps@wikimedia.org',
+            require     => User[$user],
+        }
+    }
+    else {
+        cron { 'weblogs_stat1002_rsync':
+            ensure      => absent,
+        }
+    }
+}
+
