dfaf2aa7f7703a8622cbf3d73d3011960a0fb7d7
Erik Bernhardson <ebernhardson@wikimedia.org>
2016-02-04 13:13:03 -0800

    Better mediawiki REPL
    This sets up the hhvm interactive mode to be used as a repl to help
    debugging. Because the repl is always run as the mediawiki web user
    shell users don't have their own configuration file. This works
    around that limitation by storing per-user configuration files in
    /var/lib/hphpd/<user>.hphpd.ini all owned by the mediawiki web
    user.
    
    Change-Id: I33966cc28a62d48642f3e8d875558c6e24205ba4

---
+++ modules/mediawiki/manifests/mwrepl.pp
@@ -0,0 +1,28 @@
+# == Class: mediawiki::mwrepl
+#
+# 'mwrepl' is a command line REPL, read-eval-print-loop, utility. This
+# module ensures that mwrepl is installed, and that the per-user 
+# configuration is in place.
+
+class mediawiki::mwrepl {
+    include ::mediawiki::users
+
+    file { '/var/lib/hphpd':
+        ensure => directory,
+        owner  => $::mediawiki::users::web,
+        group  => $::mediawiki::users::web,
+        mode   => '0775',
+    }
+    file { '/var/lib/hphpd/hphpd.ini':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/mediawiki/hphpd.ini',
+    }
+    file { '/usr/local/bin/mwrepl':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/mediawiki/mwrepl',
+    }
+}
