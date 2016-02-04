ee1a4f5242d0ecb7e69ee91e97230bdf566d2866
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-02-04 09:50:36 +0000

    diamond: send labs instance metrics via graphite/carbon
    use carbon line protocol, to be received by carbon-c-relay
    
    Bug: T121861
    Change-Id: I2b7abaa44678a7b26c988cade898313d1e42085b

--- manifests/role/diamond.pp
+++ manifests/role/diamond.pp
@@ -4,20 +4,25 @@ class role::diamond {
     case $::realm {
         'labs': {
             $host          = '10.64.37.13' # labmon1001
+            $port          = '2003'
             # Prefix labs metrics with project name
             $path_prefix   = $::labsproject
             $keep_logs_for = '0' # Current day only
             $service       = true
+            $handler       = 'diamond.handler.graphite.GraphiteHandler',
         }
         default: {
             $host          = '10.64.32.155' # graphite1001
+            $port          = '8125',
             $path_prefix   = 'servers'
             $keep_logs_for = '5'
             $service       = true
+            $handler       = 'diamond.handler.stats_d.StatsdHandler',
         }
     }
 
     class { '::diamond':
+        handler       => $handler,
         path_prefix   => $path_prefix,
         keep_logs_for => $keep_logs_for,
         service       => $service,
@@ -27,7 +32,7 @@ class role::diamond {
             enabled => 'true',
             # lint:endignore
             host    => $host,
-            port    => '8125',
+            port    => $port,
             batch   => '20',
         },
     }
