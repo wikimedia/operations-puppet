c0209cdc42d9876975fc9c31f804fdff3f6701c0
Alex Monk <krenair@gmail.com>
2016-02-06 22:39:57 +0000

    labs dnsrecursor IP aliasing: work on all projects, not just some arbitrary ones
    Change-Id: Icbb352a8c3b4b9a860f3d31ed68b4478c1056f62

--- modules/role/manifests/labs/dnsrecursor.pp
+++ modules/role/manifests/labs/dnsrecursor.pp
@@ -62,14 +62,9 @@ class role::labs::dnsrecursor {
     class { '::dnsrecursor::labsaliaser':
         username     => 'novaadmin',
         password     => $wikitech_nova_ldap_user_pass,
-        projects     => [
-            'tools',
-            'deployment-prep',
-            'toolserver-legacy',
-            'project-proxy',
-        ],
         nova_api_url => "http://${nova_controller_hostname}:35357/v2.0",
         alias_file   => $alias_file,
+        admin_project_name => $wikitech_nova_admin_project_name
     }
     require dnsrecursor::metalresolver
 
