0e3dec347d29e131b4924757e6ebd8c005eef32b
Alex Monk <krenair@gmail.com>
2016-02-06 22:39:57 +0000

    labs dnsrecursor IP aliasing: work on all projects, not just some arbitrary ones
    Change-Id: Icbb352a8c3b4b9a860f3d31ed68b4478c1056f62

--- modules/dnsrecursor/manifests/labsaliaser.pp
+++ modules/dnsrecursor/manifests/labsaliaser.pp
@@ -4,16 +4,17 @@ class dnsrecursor::labsaliaser(
     $projects,
     $nova_api_url,
     $alias_file,
+    $admin_project_name,
 ) {
 
     require_package('python-novaclient')
 
     $config = {
-        'username'     => $username,
-        'password'     => $password,
-        'projects'     => $projects,
-        'output_path'  => $alias_file,
-        'nova_api_url' => $nova_api_url,
+        'username'           => $username,
+        'password'           => $password,
+        'output_path'        => $alias_file,
+        'nova_api_url'       => $nova_api_url,
+        'admin_project_name' => $admin_project_name,
     }
 
     file { '/etc/labs-dns-alias.yaml':
