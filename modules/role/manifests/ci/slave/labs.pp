68632a56b51ad4643ccae920b664b793d48b167d
Antoine Musso <hashar@free.fr>
2016-02-08 12:01:32 +0100

    contint: set pbuilder basepath to actual directory
    On ci slaves, /var/cache/pbuilder is symlinked to /mnt/pbuilder because
    /var is too small and /mnt has extended disk space.
    
    The symlink apparently confuse cowbuilder/cowdancer when it is trying to
    --update a base cow image. At the end of the run we have output such as:
    
     I: unmounting dev/pts filesystem
     I: unmounting run/shm filesystem
     I: unmounting proc filesystem
     vvvvvvvvvvvvvvvvvvvvvvv
     rename: Not a directory
     ^^^^^^^^^^^^^^^^^^^^^^^
      -> removing cowbuilder working copy
      -> Moving work directory [/var/cache/pbuilder/build//cow.21918]
         to final location
    [/var/cache/pbuilder/base-unstable-amd64.cow/] and cleaning up old copy
    
    I confirmed that the move never occurs for some reason ... Maybe the
    double slashes does not help either.
    
    Set basepath for package_builder puppet class. That will also update the
    pbuilderrc file to reference the canonical directory instead of the
    symlink.
    
    Bug: T125999
    Change-Id: Id364404b6c86aa9b5d4a2597a406a97feef9758c

--- modules/role/manifests/ci/slave/labs.pp
+++ modules/role/manifests/ci/slave/labs.pp
@@ -12,7 +12,10 @@ class role::ci::slave::labs {
         class { '::package_builder':
             # We need /var/cache/pbuilder to be a symlink to /mnt
             # before cowbuilder/pbuilder is installed
-            require => Class['contint::packages::labs'],
+            require  => Class['contint::packages::labs'],
+            # Cowdancer is confused by /var/cache/pbuilder being a symlink
+            # causing it to fail to properly --update cow images. T125999
+            basepath => '/mnt/pbuilder'
         }
     }
 
