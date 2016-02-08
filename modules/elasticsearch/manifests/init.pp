8c5f09701d60250425d6bcfc00cf595461e0482f
Guillaume Lederrey <glederrey@wikimedia.org>
2016-02-08 11:44:39 +0100

    Ship Elasticsearch logs to logstash
    WIP - do not merge yet
    
    Introduce gelf4j as a log4j appender to send logs to logstash. Sending log to logstash is disabled by default and is activated if $elasticsearch::graylog_host is not `undef`.
    
    * deploy necessary packages (liblogstash-gelf-java, libjson-simple-java)
    * create symlinks from jars to /usr/share/elasticsearch/lib
    * add necessary configuration to /etc/elasticsearch/logging.yml
    * hiera config to activate this in labs (deployment-prep)
    
    Still to do:
    
    * test in labs
    
    Bug: T109101
    Change-Id: I683bcb75c91e6c2f4dd648517435a477ab23c2a7

--- modules/elasticsearch/manifests/init.pp
+++ modules/elasticsearch/manifests/init.pp
@@ -61,6 +61,9 @@
 # - $load_fixed_bitset_filters_eagerly: set to false to disable loading
 #        bitsets in memory when opening indices will slowdown queries but can
 #        significantly reduce heap usage.
+# - $send_logs_to_logstash: send logs from elastic search to logstash
+# - $gelf_host: host to which logs will be sent
+# - $gelf_port: port on which the logs will be sent
 #
 # == Sample usage:
 #
@@ -89,13 +92,21 @@ class elasticsearch($cluster_name,
                     $bulk_thread_pool_capacity = undef,
                     $statsd_host = undef,
                     $merge_threads = 3,
-                    $load_fixed_bitset_filters_eagerly = true) {
+                    $load_fixed_bitset_filters_eagerly = true,
+                    $graylog_host = undef,
+                    $graylog_port = 12201) {
 
     include ::elasticsearch::packages
 
     # Check arguments
     if $cluster_name == 'elasticsearch' {
         fail('$cluster_name must not be set to "elasticsearch"')
+    }
+
+    $send_logs_to_logstash = $graylog_host != undef
+    if $send_logs_to_logstash {
+      validate_string($graylog_host)
+      validate_integer($graylog_port)
     }
 
     file { '/etc/elasticsearch/elasticsearch.yml':
