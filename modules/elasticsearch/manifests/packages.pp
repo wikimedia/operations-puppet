d196692bc112ec83fdfaeccd1fa398286851d5df
Guillaume Lederrey <glederrey@wikimedia.org>
2016-02-08 11:44:39 +0100

    Ship Elasticsearch logs to logstash
    WIP - do not merge yet
    
    Introduce gelf4j as a log4j appender to send logs to logstash. Sending log
    to logstash is disabled by default and is activated if
    $elasticsearch::graylog_hosts is not `undef`.
    
    * deploy necessary packages (liblogstash-gelf-java, libjson-simple-java)
    * create symlinks from jars to /usr/share/elasticsearch/lib
    * add necessary configuration to /etc/elasticsearch/logging.yml
    * hiera config to activate this in labs (deployment-prep)
    
    Still to do:
    
    * test in labs
    
    Bug: T109101
    Change-Id: I683bcb75c91e6c2f4dd648517435a477ab23c2a7

--- modules/elasticsearch/manifests/packages.pp
+++ modules/elasticsearch/manifests/packages.pp
@@ -19,4 +19,28 @@ class elasticsearch::packages {
         require_package('python-elasticsearch')
         require_package('python-ipaddr')
     }
+
+
+    ### install and link additional log4j appender to send logs over GELF
+    $logging_package_ensure = $::elasticsearch::send_logs_to_logstash ? {
+        true    => 'present',
+        default => 'absent',
+    }
+    $logging_link_ensure = $::elasticsearch::send_logs_to_logstash ? {
+        true    => 'link',
+        default => 'absent',
+    }
+
+    package { [ 'liblogstash-gelf-java', 'libjson-simple-java' ]:
+        ensure => $logging_package_ensure,
+    }
+    file { '/usr/share/elasticsearch/lib/logstash-gelf.jar':
+        type   => $logging_link_ensure,
+        target => '/usr/share/java/logstash-gelf.jar',
+    }
+    file { '/usr/share/elasticsearch/lib/json-simple.jar':
+        type   => $logging_link_ensure,
+        target => '/usr/share/java/json-simple.jar',
+    }
+
 }
