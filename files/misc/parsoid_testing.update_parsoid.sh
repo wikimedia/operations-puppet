34569b47ea2bb0511d957976d6fd4f0fe1f0ca58
Subramanya Sastry <ssastry@wikimedia.org>
2016-02-09 17:28:53 -0600

    ruthenium: puppetize script to update parsoid + restart services
    Used to be on /home/parsoid-rt/update_code.sh on ruthenium before
    reimage and I saved it in /home/ssastry/bin/update_code.sh after reimage.
    But, on mutante's advice, moving this to /usr/local/bin and having
    user parsoid own it and updates previously granted access path to
    the path of this script.
    
    Change-Id: Ie4f8b5a5481fb1bdb82de99d762bdc31c07ec27e

---
+++ files/misc/parsoid_testing.update_parsoid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo 'Updating deploy repo checkout ...'
+cd /usr/lib/parsoid
+git pull
+echo 'Updating parsoid repo checkout ...'
+cd src
+git pull
+git log -n 1
+cd ..
+echo 'Restarting parsoid ...'
+service parsoid restart
+echo 'Restarting parsoid-rt testreduce clients ...'
+service parsoid-rt-client restart
+echo 'All done!'
