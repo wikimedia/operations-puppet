b8220ce8e01b7c8ca31f198be32e481f786bbc40
Mukunda Modell <mmodell@wikimedia.org>
2016-02-09 17:32:03 -0600

    Clean up phabricator roles in puppet to remove tag pinning.
    Phabricator is now deployed with scap3, puppet should not
    manage the checked out tags on phabricator and related repos.
    
    Bug: T125851
    Change-Id: I0bdf3266c85883871e6f9417a1ca77cc317c5906

--- modules/phabricator/manifests/libext.pp
+++ modules/phabricator/manifests/libext.pp
@@ -1,41 +1,16 @@
 # == Define: phabricator::libext
 #
-# Installs libphutil library extensions
+# Installs phutil library extensions
 #
 # === Parameters
 #
 # [*rootdir*]
 #    The path on disk to clone the needed repositories
-#
-# [*libext_tag*]
-#    Git tag to hold repo at
-#
-# [*libext_lock_path*]
-#    Path to local file that provides local settings lock
-#
 # [*libname*]
 #    Case sensitive extension name
 
 
-define phabricator::libext ($rootdir, $libext_tag, $libext_lock_path, $libname = $name) {
-
-    # Build per extension lock file
-    $libname_lock = "${libext_lock_path}_${libname}"
-
-    git::install { "phabricator/extensions/${libname}" :
-        directory => "${rootdir}/libext/${libname}",
-        git_tag   => $libext_tag,
-        lock_file => $libname_lock,
-        notify    => Exec[$libname_lock],
-        before    => Git::Install['phabricator/phabricator'],
-    }
-
-    exec {$libname_lock:
-        command => "touch ${libname_lock}",
-        unless  => "test -z ${libname_lock} || test -e ${libname_lock}",
-        path    => '/usr/bin:/bin',
-    }
-
+define phabricator::libext ($rootdir, $libname = $name) {
 
     # symlink static directories for extensions
     $static_dir  = "${rootdir}/libext/${libname}/rsrc/webroot-static"
@@ -46,7 +21,7 @@ define phabricator::libext ($rootdir, $libext_tag, $libext_lock_path, $libname =
         command => "/bin/ln -s ${static_dir} ${symlink_dir}",
         onlyif  => "/usr/bin/test -e ${static_dir}",
         creates => $symlink_dir,
-        require => Git::Install["phabricator/extensions/${libname}"],
+        require => File[$rootdir],
     }
 
 }
