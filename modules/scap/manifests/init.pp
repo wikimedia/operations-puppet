9ebd75cd752c99dd860cbd857dd9fb63ff48ee86
Mukunda Modell <mmodell@wikimedia.org>
2016-02-11 04:30:34 -0600

    Install the scap package from deb instead of trebuchet
    This switches scap to use the apt provider and fixes paths to point to
    `/usr/bin/$binary` instead of `/srv/deployment/scap/scap/bin/$binary`
    
    Bug: T114363
    
    Change-Id: Iaa518889fa2c8b3d81db9e466cb63d0fcbc30cc4

--- modules/scap/manifests/init.pp
+++ modules/scap/manifests/init.pp
@@ -3,14 +3,9 @@
 # Common role for scap masters and targets
 
 class scap {
-    # Using trebuchet provider while scap service deployment is under
-    # development -- chicken and egg things
-    #
-    # This should be removed once scap3 is in a final state (i.e. packaged
-    # or deployed via another method)
+
     package { 'scap':
-        ensure   => latest,
-        provider => 'trebuchet',
+        ensure   => 'present',
     }
 
     file { '/etc/scap.cfg':
