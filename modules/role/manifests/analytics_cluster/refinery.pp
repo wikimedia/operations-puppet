91a83c59a93bfdd689a27772461230dca117de61
Andrew Otto <acotto@gmail.com>
2016-02-11 15:58:16 -0500

    Create refinery classes in analytics_cluster role, apply them to stat1002
    Also move the analytics::users class to the analytics_cluster::users class, this
    is applied on stat1002 and analytics100[12]
    
    Bug: T109859
    Change-Id: I4e8278e6efe1f7515f149a62f55330586a92a50a

---
+++ modules/role/manifests/analytics_cluster/refinery.pp
@@ -0,0 +1,49 @@
+# == Class role::analytics_cluster::refinery
+# Includes configuration and resources needed for deploying
+# and using the analytics/refinery repository.
+#
+class role::analytics_cluster::refinery {
+    # Make this class depend on hadoop::client.  Refinery
+    # is intended to work with Hadoop, and many of the
+    # role classes here use the hdfs user, which is created
+    # by the CDH packages.
+    Class['role::analytics::hadoop::client'] -> Class['role::analytics::refinery']
+
+    # Some refinery python scripts use docopt for CLI parsing.
+    if !defined(Package['python-docopt']) {
+        package { 'python-docopt':
+            ensure => 'installed',
+        }
+    }
+    # refinery python module uses dateutil
+    if !defined(Package['python-dateutil']) {
+        package { 'python-dateutil':
+            ensure => 'installed',
+        }
+    }
+
+    # analytics/refinery will deployed to this node.
+    package { 'analytics/refinery':
+        provider => 'trebuchet',
+    }
+
+    # analytics/refinery repository is deployed via git-deploy at this path.
+    # You must deploy this yourself; puppet will not do it for you.
+    $path = '/srv/deployment/analytics/refinery'
+
+    # Put refinery python module in user PYTHONPATH
+    file { '/etc/profile.d/refinery.sh':
+        content => "export PYTHONPATH=\${PYTHONPATH}:${path}/python"
+    }
+
+    # Create directory in /var/log for general purpose Refinery job logging.
+    $log_dir = '/var/log/refinery'
+    file { $log_dir:
+        ensure => 'directory',
+        owner  => 'hdfs',
+        group  => 'analytics-admins',
+        # setgid bit here to make refinery log files writeable
+        # by users in the analytics-admins group.
+        mode   => '2775',
+    }
+}
