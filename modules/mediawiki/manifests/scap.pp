e9a4eab7434b3040f83bb71569d0d202fe36701c
ArielGlenn <ariel@wikimedia.org>
2016-02-11 11:37:54 +0000

    Revert "Install the scap package from deb instead of trebuchet"
    This reverts commit 9ebd75cd752c99dd860cbd857dd9fb63ff48ee86.
    
    move from scap2 to scap3 not cleared
    
    Change-Id: I89c35283fe9b9041da45468a1b3bb3b745e026d1

--- modules/mediawiki/manifests/scap.pp
+++ modules/mediawiki/manifests/scap.pp
@@ -12,7 +12,7 @@ class mediawiki::scap {
 
     $mediawiki_deployment_dir = '/srv/mediawiki'
     $mediawiki_staging_dir    = '/srv/mediawiki-staging'
-    $scap_bin_dir             = '/usr/bin'
+    $scap_bin_dir             = '/srv/deployment/scap/scap/bin'
     $mediawiki_web_user       = $::mediawiki::users::web
 
     # /srv/mediawiki is the root path of the MediaWiki deployment tree.
--- modules/scap/manifests/init.pp
+++ modules/scap/manifests/init.pp
@@ -3,9 +3,14 @@
 # Common role for scap masters and targets
 
 class scap {
-
+    # Using trebuchet provider while scap service deployment is under
+    # development -- chicken and egg things
+    #
+    # This should be removed once scap3 is in a final state (i.e. packaged
+    # or deployed via another method)
     package { 'scap':
-        ensure   => 'present',
+        ensure   => latest,
+        provider => 'trebuchet',
     }
 
     file { '/etc/scap.cfg':
