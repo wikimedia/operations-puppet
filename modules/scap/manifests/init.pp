43be851174a027f74a49c3b9bbd798e0165fa3de
dzahn <dzahn@wikimedia.org>
2016-02-10 22:16:18 -0800

    scap: fix lint warnings, alignment
    fix a couple puppet-lint warnings of the type:
    "WARNING: indentation of => is not properly aligned"
    
    Change-Id: Ib1506ff36297dc1d3deef5a2abe388edfad4406d

--- modules/scap/manifests/init.pp
+++ modules/scap/manifests/init.pp
@@ -14,10 +14,10 @@ class scap {
     }
 
     file { '/etc/scap.cfg':
-        source  => 'puppet:///modules/scap/scap.cfg',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
+        source => 'puppet:///modules/scap/scap.cfg',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
     }
 
     require_package([
