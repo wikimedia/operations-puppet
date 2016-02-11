970e9378fc24c46d30ef610c0cc49ca701696da3
Andrew Otto <acotto@gmail.com>
2016-02-11 15:58:16 -0500

    Create refinery classes in analytics_cluster role, apply them to stat1002
    Also move the analytics::users class to the analytics_cluster::users class, this
    is applied on stat1002 and analytics100[12]
    
    Bug: T109859
    Change-Id: I4e8278e6efe1f7515f149a62f55330586a92a50a

---
+++ modules/role/manifests/analytics_cluster/refinery/camus.pp
@@ -0,0 +1,42 @@
+# == Class role::analytics_cluster::refinery::camus
+# Uses camus::job to set up cron jobs to
+# import data from Kafka into Hadoop.
+#
+class role::analytics_cluster::refinery::camus {
+    require role::role::analytics_cluster::refinery
+    include role::kafka::role::analytics::config
+
+    # Make all uses of camus::job set default kafka_brokers and camus_jar.
+    # If you build a new camus or refinery, and you want to use it, you'll
+    # need to change these.  You can also override these defaults
+    # for a particular camus::job instance by setting the parameter on
+    # the camus::job declaration.
+    Camus::Job {
+        kafka_brokers => suffix($role::kafka::analytics::config::brokers_array, ':9092'),
+        camus_jar     => "${role::analytics::refinery::path}/artifacts/org/wikimedia/analytics/camus-wmf/camus-wmf-0.1.0-wmf6.jar",
+        check_jar     => "${role::analytics::refinery::path}/artifacts/org/wikimedia/analytics/refinery/refinery-job-0.0.26.jar",
+    }
+
+    # Import webrequest_* topics into /wmf/data/raw/webrequest
+    # every 10 minutes, check runs and flag fully imported hours.
+    camus::job { 'webrequest':
+        check  => true,
+        minute => '*/10',
+    }
+
+    # Import eventlogging_* topics into /wmf/data/raw/eventlogging
+    # once every hour.
+    camus::job { 'eventlogging':
+        minute => '5',
+    }
+
+    # Import mediawiki_* topics into /wmf/data/raw/mediawiki
+    # once every hour.  This data is expected to be Avro binary.
+    camus::job { 'mediawiki':
+        check  => true,
+        minute  => '15',
+        # refinery-camus contains some custom decoder classes which
+        # are needed to import Avro binary data.
+        libjars => "${role::analytics::refinery::path}/artifacts/org/wikimedia/analytics/refinery/refinery-camus-0.0.23.jar",
+    }
+}
