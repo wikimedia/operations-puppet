e9f5bef577e29da5749fa3448f34244f5624de20
Antoine Musso <hashar@free.fr>
2016-02-11 12:19:45 +0100

    contint: disable HHVM background service
    CI does not need a HHVM service running in the background. Disable the
    service entirely on boot and later.
    
    Bug: T126594
    Depends-On: I4bc1ab34093b017329e6c2fe95ae6a4625674e5d
    Change-Id: I2e83287898b7f994d9cfcf10ed8602820d92839b

--- modules/contint/manifests/hhvm.pp
+++ modules/contint/manifests/hhvm.pp
@@ -1,6 +1,14 @@
 class contint::hhvm {
 
     class { '::hhvm':
+        # No need for a hhvm service on CI slaves T126594
+        # lint:ignore:ensure_first_param
+        service_params => {
+            ensure => 'stopped',
+            enable => false,
+        },
+        # lint:endignore
+
         # Never use a local repo. The central repo path is set to '' to have
         # hhvm fall back to the env variable HHVM_REPO_CENTRAL_PATH. It is
         # defined in the global Jenkins configuration and vary per build thanks
