638c7cf80a92d91cd9308badda473a3ee17313c1
cpettet <rush@wikimedia.org>
2016-02-12 16:15:13 -0600

    grub: allow setting ioscheduler
    Bug: T126090
    Change-Id: I53bffa8442245ffed346b7bc8dde2d005b7a5215

--- modules/base/manifests/grub.pp
+++ modules/base/manifests/grub.pp
@@ -1,4 +1,4 @@
-class base::grub {
+class base::grub($ioscheduler = 'deadline') {
     # The shellvars_list lens is broken with backticks on older versions of
     # Augeas (< jessie), so keep a compatibility version with test/grep/sed
     if versioncmp($::augeasversion, '1.3.0') >= 0 {
@@ -13,9 +13,9 @@ class base::grub {
                 # removes quiet, splash from default kopts
                 'rm GRUB_CMDLINE_LINUX_DEFAULT/value[. = "quiet"]',
                 'rm GRUB_CMDLINE_LINUX_DEFAULT/value[. = "splash"]',
-                # The CFQ I/O scheduler is rather suboptimal for some of our I/O
-                # workloads. Override with deadline. (the installer does this too)
-                'set GRUB_CMDLINE_LINUX/value[. = "elevator=deadline"] elevator=deadline',
+                # CFQ I/O scheduler is the default allow override
+                # with class default to deadline (the installer does this too)
+                "set GRUB_CMDLINE_LINUX/value[. = \"elevator=${ioscheduler}\"] elevator=deadline",
             ],
             notify  => Exec['update-grub'],
         }
@@ -42,8 +42,8 @@ class base::grub {
         # workloads. Override with deadline. (the installer does this too)
         exec { 'grub2 iosched deadline':
             path    => '/bin:/usr/bin',
-            command => "sed -i '/^GRUB_CMDLINE_LINUX=/s/\\\"\$/ elevator=deadline\\\"/' /etc/default/grub",
-            unless  => "grep -q '^GRUB_CMDLINE_LINUX=.*elevator=deadline' /etc/default/grub",
+            command => "sed -i '/^GRUB_CMDLINE_LINUX=/s/\\\"\$/ elevator=${ioscheduler}\\\"/' /etc/default/grub",
+            unless  => "grep -q '^GRUB_CMDLINE_LINUX=.*elevator=${ioscheduler}' /etc/default/grub",
             onlyif  => 'test -f /etc/default/grub',
             notify  => Exec['update-grub'];
         }
