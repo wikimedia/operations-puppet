5fc176ae63b2e51008d8a8ae6c25e1e21f793e45
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-02-15 14:45:53 +0100

    Puppetise yhsm-daemon
    Part of the Yubi 2FA auth servers.
    
    Change-Id: I788972c138a8812877c7a841c74d34c436f1332c

---
+++ modules/yubiauth/manifests/yhsm-daemon.pp
@@ -0,0 +1,27 @@
+class yubiauth::yhsm_daemon(
+    $yhsmdevice = '/dev/ttyACM0',
+) {
+
+    require_package('yhsm-daemon')
+
+    file { 'yhsm-daemon-default':
+        mode    => '0440',
+        owner   => root,
+        group   => root,
+        path    => '/etc/default/yhsm-daemon',
+        content => template('yubiauth/yhsm-daemon-default.erb'),
+    }
+
+    service { 'yhsm-daemon':
+        enable  => true,
+        require => [
+                    Package['yhsm-daemon'],
+                    File['/etc/default/yhsm-daemon'],
+                    ],
+    }
+}
+
+
+
+
+
