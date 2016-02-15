9cdc56a104542944a0d69ac49407ea2488d02691
Guillaume Lederrey <glederrey@wikimedia.org>
2016-02-15 22:13:47 +0100

    Ship Elasticsearch logs to logstash
    Introduce gelf4j as a log4j appender to send logs to logstash. Sending log
    to logstash is disabled by default and is activated if
    $elasticsearch::graylog_hosts is not `undef`.
    
    * deploy necessary packages (liblogstash-gelf-java, libjson-simple-java)
    * create symlinks from jars to /usr/share/elasticsearch/lib
    * add necessary configuration to /etc/elasticsearch/logging.yml
    * hiera config to activate this in labs (deployment-prep)
    * hiera config to activate in prod/codfw
    
    Bug: T109101
    Change-Id: I683bcb75c91e6c2f4dd648517435a477ab23c2a7

---
+++ modules/elasticsearch/spec/defines/ship_logs_to_logstash_rspec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'elasticsearch', :type => :class do
+
+  describe 'when NOT sending logs to logstash' do
+    let(:params) { { :cluster_name  => 'my_cluster_name' } }
+    let(:facts) { { :lsbdistrelease => 'ubuntu',
+                    :lsbdistid      => 'trusty'
+    } }
+
+    it { should_not contain_package('liblogstash-gelf-java') }
+  end
+
+  describe 'when sending logs to logstash' do
+    let(:params) { { :cluster_name  => 'my_cluster_name',
+                     :graylog_hosts => [ 'logstash.example.net', 'other' ] } }
+    let(:facts) { { :lsbdistrelease => 'ubuntu',
+                    :lsbdistid      => 'trusty'
+    } }
+
+    it { should contain_package('liblogstash-gelf-java').with({ :ensure => 'present' }) }
+  end
+end
