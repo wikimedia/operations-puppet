d0a283fc48f5aeea120dbb8f7303c42553eba835
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-02-16 19:18:53 +0100

    role::memcached: create cross-dc replication for sessions
    In order to have all the defined shards (that we already use for ipsec
    cross-connections) being replicated from the master dc to all the
    others, we do the following:
    
    * Add a "map" parameter to redis::instance, allowing to add variable
      settings for different instances on the same node, much alike what we
      do with varnish::instance
    
    * Add a redis::multidc::instances function that, given the ip address of
      the server we're on, will extract which instances should be present on
      the machine and which host/port they must replicate from if the
      datacenter is not currently the primary.
    
    Bug: T126470
    Change-Id: Ibdcf252f8b0e6490db5998a57d83e578d8a1338a

--- modules/redis/manifests/instance.pp
+++ modules/redis/manifests/instance.pp
@@ -13,6 +13,11 @@
 #   A map of Redis configuration directives to their desired value.
 #   See <http://redis.io/topics/config> for documentation.
 #
+# [*map*]
+#  A value can be a hash of settings for the various instances
+#  we are defining at the same time, in the form of instance_name => settings.
+#  Only overrides to what is the default in settings needs to be defined.
+#
 # === Examples
 #
 #  # Configure a Redis instance on TCP port 6379:
@@ -25,13 +30,22 @@
 #    settings => { maxmemory => '2mb' },
 #  }
 #
+#  # Configure two instances, with 6380 being a slave
+#  redis::instance { ['6379', '6380']:
+#    settings => { maxmemory => '2mb', slaveof => undef},
+#    map      => {
+#      '6380'    => { 'slaveof' => '127.0.0.1 6379'}
+#    }
+#  }
 #
 define redis::instance(
     $ensure   = present,
-    $settings = {}
-) {
+    $settings = {},
+    $map = {}
+    ) {
     validate_ensure($ensure)
     validate_hash($settings)
+    validate_hash($map)
 
     require ::redis
 
@@ -56,6 +70,7 @@ define redis::instance(
         unixsocket => $unixsocket,
         daemonize  => true,
     }
+
 
     file { "/etc/redis/${instance_name}.conf":
         ensure  => $ensure,
