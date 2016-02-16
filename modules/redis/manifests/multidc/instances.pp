1c901a179d4f140582898152d9287cd59cc4951a
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-02-16 19:18:53 +0100

    role::memcached: create cross-dc replication for sessions
    Bug: T126470
    Change-Id: Ibdcf252f8b0e6490db5998a57d83e578d8a1338a

---
+++ modules/redis/manifests/multidc/instances.pp
@@ -0,0 +1,28 @@
+# === class redis::multidc::instances
+class redis::multidc::instances($shards, $settings) {
+    #################################################################
+    # Set up IPsec between hosts (for encrypting the redis replica) #
+    #################################################################
+
+    # This is actually more reliable than $::ipaddress for our uses
+    $my_ip = ipresolve($::hostname, 4)
+
+    $instances = redis_get_instances($my_ip, $shards)
+    # To know we're the master
+    # TODO: maybe define a variable that can be tested separately from
+    # mwprimary if we're switching just part of the datacenter
+    if ($::site == $::mwprimary) {
+        redis::instance{ $instances:
+            ensure   => present,
+            settings => $settings
+        }
+    }
+    else {
+        $replica_map = redis_get_masters($my_ip, $shards, $::mwprimary)
+        redis::instance{ $instances:
+            ensure   => present,
+            settings => $settings,
+            replica  => $replica_map
+        }
+    }
+}
