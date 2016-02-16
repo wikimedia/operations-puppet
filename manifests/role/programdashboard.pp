7583785f6d265559866357a490af4ea1fbf2f27a
Dan Duvall <dduvall@wikimedia.org>
2016-02-16 15:21:19 -0800

    Program Dashboard configuration for initial labs rollout
    Defined role::programdashboard::app and programdashboard::app which
    provision Rails application dependencies, Passenger setup, and the
    deployment directory. This setup assumes that deployments will be
    executed using Capistrano and the existing configuration within the
    application repo.
    
    A labs specific database setup will be included in a follow-up patch.
    
    Bug: T105967
    Change-Id: If6d8deed74dd6def372dee6df786c7594bddbe0c

---
+++ manifests/role/programdashboard.pp
@@ -0,0 +1,12 @@
+# = Class: role::programdashboard::app
+#
+# This role sets up Program Dashboard dependencies and an Apache/Passenger
+# configuration for running the Rails application.
+#
+class role::programdashboard::app {
+    include programdashboard::app
+
+    system::role { 'role::programdashboard::app':
+        description => 'Program Dashboard application server',
+    }
+}
