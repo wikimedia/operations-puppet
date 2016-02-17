c80b7423f5f59c2c8ec523c23fd5d929d7377013
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-02-17 18:22:20 +0200

    etherpad: Simplify module and role
    These days we no longer have apache for etherpad. Remove all apache
    configuration from role, and simplify the module by setting some
    sensible defaults for some parameters and stop declaring them on the
    role level. Also:
    
    * Remove the old apache configuration file
    * Remove the autorestarter that never really worked
    * Set default charset to utf8mb4
    
    Change-Id: I54a8c3314f4dc34f7af8325ca722e90b5241a953

--- manifests/role/etherpad.pp
+++ manifests/role/etherpad.pp
@@ -1,49 +1,17 @@
 class role::etherpad{
     include standard
     include base::firewall
-
     include passwords::etherpad_lite
 
     system::role { 'etherpad':
         description => 'Etherpad-lite server'
     }
 
-    $etherpad_ip = '0.0.0.0'
-    $etherpad_port = '9001'
-
-    case $::realm {
-        'labs': {
-            $etherpad_host = $::fqdn
-        }
-        'production': {
-            $etherpad_host = 'etherpad.wikimedia.org'
-        }
-        default: {
-            fail('unknown realm, should be labs or production')
-        }
-    }
-
     class { '::etherpad':
-        etherpad_host    => $etherpad_host,
-        etherpad_ip      => $etherpad_ip,
-        etherpad_port    => $etherpad_port,
         etherpad_db_user => $passwords::etherpad_lite::etherpad_db_user,
         etherpad_db_host => $passwords::etherpad_lite::etherpad_db_host,
         etherpad_db_name => $passwords::etherpad_lite::etherpad_db_name,
         etherpad_db_pass => $passwords::etherpad_lite::etherpad_db_pass,
-    }
-
-    include ::apache::mod::rewrite
-    include ::apache::mod::proxy
-    include ::apache::mod::proxy_http
-    include ::apache::mod::remoteip
-
-    # this site's misc-lb caching proxies hostnames
-    $cache_misc_nodes = hiera('cache::misc::nodes')
-    $trusted_proxies = $cache_misc_nodes[$::site]
-
-    ::apache::site { 'etherpad.wikimedia.org':
-        content => template('apache/sites/etherpad.wikimedia.org.erb'),
     }
 
     # Icinga process monitoring, T82936
@@ -57,13 +25,6 @@ class role::etherpad{
         check_command => 'check_http_url!etherpad.wikimedia.org!/',
     }
 
-    # Autorestart because etherpad sucks
-    include ::etherpad::autorestarter
-
-    ferm::service { 'etherpad_http':
-        proto => 'tcp',
-        port  => 'http',
-    }
     ferm::service { 'etherpad_service':
         proto => 'tcp',
         port  => '9001',
