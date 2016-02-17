afe6fab2a763c8ce45c8ed6121a53a65762f98f4
Subramanya Sastry <ssastry@wikimedia.org>
2016-02-17 13:25:19 -0600

    ruthenium: Tweak the update_parsoid.sh script to make it more robust
    * Eliminate the remote possibility of a race condition.
    * Ensure that parsoid repos are in master branch before git pull.
    
    Change-Id: I0ed6f622b5f4bc4829674414ceb0fc8a6db1539d

--- modules/parsoid/files/parsoid_testing.update_parsoid.sh
+++ modules/parsoid/files/parsoid_testing.update_parsoid.sh
@@ -3,16 +3,32 @@
 set -e
 set -u
 
+# Stop parsoid-rt testreduce clients to eliminate
+# any possibility of a race condition where the test clients
+# notice the code change, restart, and query the parsoid service
+# before it restarts with the new code. This should be a very very
+# remote possibility, but doesn't hurt to do this right.
+echo 'Stopping parsoid-rt testreduce clients ...'
+sudo service parsoid-rt-client stop
+
 echo 'Updating deploy repo checkout ...'
 cd /srv/parsoid
+# Ensure we are in the master branch -- the repo has been
+# left in a non-master checkout on occasion when a previous
+# git pull might have failed for whatever reason.
+git checkout master
 git pull
+
 echo 'Updating parsoid repo checkout ...'
 cd src
+git checkout master
 git pull
 git log -n 1
-cd ..
+
 echo 'Restarting parsoid ...'
 sudo service parsoid restart
-echo 'Restarting parsoid-rt testreduce clients ...'
-sudo service parsoid-rt-client restart
+
+echo 'Starting parsoid-rt testreduce clients ...'
+sudo service parsoid-rt-client start
+
 echo 'All done!'
