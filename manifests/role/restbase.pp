42513988c209334883183271fc3c410cd697379b
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-02-18 17:30:38 +0000

    restbase: adjust alarms with new metric names
    following the internal/external split
    
    Change-Id: I237e8a27ab3eaa5f700a84b4609442cbcf2b761e

--- manifests/role/restbase.pp
+++ manifests/role/restbase.pp
@@ -22,7 +22,7 @@ class role::restbase {
 class role::restbase::alerts {
     monitoring::graphite_threshold { 'restbase_request_5xx_rate':
         description   => 'RESTBase req/s returning 5xx http://grafana.wikimedia.org/#/dashboard/db/restbase',
-        metric        => 'transformNull(restbase.v1_page_html_-title-_-revision--_tid-.GET.5xx.sample_rate, 0)',
+        metric        => 'transformNull(sumSeries(restbase.{internal,external}.v1_page_html_-title-_-revision--_tid-.GET.5xx.sample_rate), 0)',
         from          => '10min',
         warning       => '1', # 1 5xx/s
         critical      => '3', # 5 5xx/s
@@ -32,7 +32,7 @@ class role::restbase::alerts {
 
     monitoring::graphite_threshold { 'restbase_html_storage_hit_latency':
         description   => 'RESTBase HTML storage load mean latency ms http://grafana.wikimedia.org/#/dashboard/db/restbase',
-        metric        => 'movingMedian(restbase.sys_key-rev-value_-bucket-_-key--_revision--_tid-.GET.2xx.mean, 15)',
+        metric        => 'movingMedian(sumSeries(restbase.{internal,external}.sys_key-rev-value_-bucket-_-key--_revision--_tid-.GET.2xx.mean), 15)',
         from          => '10min',
         warning       => '25', # 25ms
         critical      => '50', # 50ms
@@ -42,7 +42,7 @@ class role::restbase::alerts {
 
     monitoring::graphite_threshold { 'restbase_html_storage_hit_latency_99p':
         description   => 'RESTBase HTML storage load 99p latency ms http://grafana.wikimedia.org/#/dashboard/db/restbase',
-        metric        => 'movingMedian(restbase.sys_key-rev-value_-bucket-_-key--_revision--_tid-.GET.2xx.p99, 15)',
+        metric        => 'movingMedian(sumSeries(restbase.{internal,external}.sys_key-rev-value_-bucket-_-key--_revision--_tid-.GET.2xx.p99), 15)',
         from          => '10min',
         warning       => '1500', # 1.5s
         critical      => '3000', # 3s
