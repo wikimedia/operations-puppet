a4e6eed69766238e3999b7dbb83a469c8e448b02
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-02-18 18:10:46 +0200

    otrs: Provision mpm_prefork.conf
    Configure mpm_prefork's MaxConnectionsPerChild stanza to a value that
    will allow to mitigate the effects of OTRS memory leaks
    
    Change-Id: Ib07f0941417b7c218a8aa85ba01dd3d540f88ea7

--- modules/otrs/manifests/web.pp
+++ modules/otrs/manifests/web.pp
@@ -18,6 +18,17 @@ class otrs::web {
     include ::apache::mod::rewrite
     include ::apache::mod::headers
 
+    # We override the default mpm_prefork to set the apache setting for
+    # MaxConnectionsPerChild. The chosen number is experimentally derived from
+    # an OTRS suggested apache configuration example along with some experiments
+    # Otherwise, OTRS memory leaks through the roof causing OOM to show up
+    # We use the declarative form of the class instead of the inclusion to
+    # explicitly show that we use the prefork mpm
+    class { 'apache::mpm':
+        mpm    => 'prefork',
+        source => 'puppet:///modules/otrs/mpm_prefork.conf',
+    }
+
     # this site's misc-lb caching proxies hostnames
     $cache_misc_nodes = hiera('cache::misc::nodes')
     $trusted_proxies = $cache_misc_nodes[$::site]
