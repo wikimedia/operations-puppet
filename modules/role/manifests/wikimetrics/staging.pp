9884f8a861dd398f88147eb96afd66c5e9f4d4b2
dzahn <dzahn@wikimedia.org>
2016-02-18 21:14:06 -0800

    wikimetrics: rename prod role, move to module/role
    - rename production role to ::production just like
      the description right below says, for consistent
      structure, like wikimetrics::staging
    - mode to module/role into autoload layout
    
    Change-Id: I3c0546bcba23b81ca636ca75efe2192ed15db0b0

--- modules/role/manifests/wikimetrics/staging.pp
+++ modules/role/manifests/wikimetrics/staging.pp
@@ -1,29 +1,3 @@
-# == Class role::wikimetrics
-# This is the production wikimetrics role
-class role::wikimetrics {
-    include ::wikimetrics::base
-
-    # Production wikimetrics instance (in labs) needs a mysql client
-    # to access labsdb
-    require_package('mysql-client')
-
-    class { '::wikimetrics::web':
-        workers => 4,
-    }
-
-    class { '::wikimetrics::redis':
-        queue_maxmemory => '1Gb',
-    }
-
-    class { '::wikimetrics::queue':
-        require => Class['::wikimetrics::redis']
-    }
-
-    class { '::wikimetrics::scheduler':
-        require => Class['::wikimetrics::redis']
-    }
-}
-
 # == Class role::wikimetrics::staging
 # This is the staging specific wikimetrics role
 class role::wikimetrics::staging {
