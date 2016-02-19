39af495bef48fc3d2c114228ebcbdaf0ef12c569
dzahn <dzahn@wikimedia.org>
2016-02-18 20:43:19 -0800

    role/dns: move to module role, rename ::dnsrecursor
    - split role class file into one file per class
    - move into autoload layout in module/role
      (like we do everywhere else too and fixes lint)
    - rename dnsrecursor to dns::recursor for consistency
      to fit into this schema
    
    Change-Id: I9e090bfc67cea7e0da5885776b6ea3943b3ca9ab

---
+++ modules/role/manifests/dns/recursor.pp
@@ -0,0 +1,44 @@
+class role::dns::recursor {
+    system::role { 'role::dns::recursor': description => 'Recursive DNS server' }
+
+    include lvs::configuration, network::constants
+    include base::firewall
+
+    class {
+        'lvs::realserver':
+            realserver_ips => $lvs::configuration::service_ips['dns_rec'][$::site];
+        '::dnsrecursor':
+            require             => Class['lvs::realserver'],
+            listen_addresses    => [$::ipaddress,
+                                    $::ipaddress6_eth0,
+                                    $lvs::configuration::service_ips['dns_rec'][$::site],
+                                  ],
+            allow_from          => $network::constants::all_networks;
+    }
+
+    ::dns::recursor::monitor { [ $::ipaddress, $::ipaddress6_eth0 ]: }
+
+    ferm::service { 'udp_dns_rec':
+        proto => 'udp',
+        port  => '53',
+    }
+
+    ferm::service { 'tcp_dns_rec':
+        proto => 'tcp',
+        port  => '53',
+    }
+
+    ferm::rule { 'skip_dns_conntrack-out':
+        desc  => 'Skip DNS outgoing connection tracking',
+        table => 'raw',
+        chain => 'OUTPUT',
+        rule  => 'proto udp sport 53 NOTRACK;',
+    }
+
+    ferm::rule { 'skip_dns_conntrack-in':
+        desc  => 'Skip DNS incoming connection tracking',
+        table => 'raw',
+        chain => 'PREROUTING',
+        rule  => 'proto udp dport 53 NOTRACK;',
+    }
+}
