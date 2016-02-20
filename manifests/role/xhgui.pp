b67b5f158d776e076a51c1120ee2d0bfbd32c55c
Ori Livneh <ori@wikimedia.org>
2016-02-20 00:01:55 -0800

    Complete xhgui role
    - Deploy xhgui with Git.
    - Serve xhgui via Apache.
    - Proxy performance.wikimedia.org/xhgui to xhgui vhost.
    
    Change-Id: Idc306585643229439a0a992d160e6c114efe11b3

--- manifests/role/xhgui.pp
+++ manifests/role/xhgui.pp
@@ -1,11 +1,39 @@
 # == Class: role::xhgui
 #
-# Aggregates XHProf profiling data from the app servers.
+# XHGUI is a MongoDB-backed PHP webapp for viewing and analyzing
+# PHP profiling data.
 #
 class role::xhgui {
-    class { 'mongodb': }
+    include ::apache::mod::php5
+    include ::apache::mod::rewrite
 
-    ferm::rule { 'xhgui_mongodb':
-        rule => 'proto tcp dport 27017 { saddr $INTERNAL ACCEPT; }',
+    include ::mongodb
+
+    # TODO: set up indexing on mongodb
+    # TODO: set NUMA arg
+
+    system::role { 'role::xhgui': }
+
+    ferm::service { 'xhgui_mongodb':
+        port   => 27017,
+        proto  => 'tcp',
+        srange => '$INTERNAL',
+    }
+
+    ferm::service { 'xhgui_http':
+        port   => 80,
+        proto  => 'tcp',
+        srange => '$INTERNAL',
+    }
+
+    git::clone { 'operations/software/xhgui':
+        ensure    => 'latest',
+        directory => '/srv/xhgui',
+        branch    => 'wmf_deploy',
+    }
+
+    apache::site { 'xhgui_apache_site':
+        content => template('apache/sites/xhgui.erb'),
+        require => Git::Clone['vendor/xhgui'],
     }
 }
