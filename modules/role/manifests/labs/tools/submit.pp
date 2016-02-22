928e651b0b94c7639a8b3ca86e67b6e4fa16c2bf
Tim Landscheidt <tim@tim-landscheidt.de>
2016-02-22 06:53:25 +0000

    Tools: Remove obsolete classes
    role::labs::tools::submit and toollabs::submit have been replaced by
    the respective cronrunner classes and are no longer in use.
    
    Change-Id: I2399ef5d7302c320aca914151fad9e5b56193cd7

--- modules/role/manifests/labs/tools/submit.pp
+++
@@ -1,7 +0,0 @@
-class role::labs::tools::submit {
-    include ::toollabs::submit
-
-    system::role { 'role::labs::tools::submit':
-        description => 'Tool Labs job submit (cron) host',
-    }
-}
