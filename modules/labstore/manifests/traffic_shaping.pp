d60539d7a9720eb17afb7e2f164a10a55059ba76
cpettet <rush@wikimedia.org>
2016-02-23 16:40:46 -0600

    labstore: real time application of tc setup
    This is intended to be merged post careful manual post
    staggered rollout of 272891
    
    Change-Id: Id3892fc74cb42cbb92622eebf783979ce119d70d

--- modules/labstore/manifests/traffic_shaping.pp
+++ modules/labstore/manifests/traffic_shaping.pp
@@ -6,12 +6,20 @@ class labstore::traffic_shaping {
         owner  => 'root',
         group  => 'root',
         source => 'puppet:///modules/labstore/tc-setup.sh',
+        notify => Exec['apply_tc_config'],
+    }
+
+    # if native qdisc of pfifo_fast is applied then run tc setup
+    exec { 'apply_tc_config':
+        command => '/sbin/modprobe ifb numifbs=1 && /sbin/modprobe act_mirred && /usr/sbin/tc-setup',
+        onlyif  => '/sbin/tc -s qdisc show | /bin/grep "qdisc pfifo"',
     }
 
     # run when interfaces come up.
     file { '/etc/network/if-up.d/tc':
-        ensure => 'link',
-        target => '/usr/local/sbin/tc-setup',
+        ensure  => 'link',
+        target  => '/usr/local/sbin/tc-setup',
+        require => File['/usr/local/sbin/tc-setup'],
     }
 
     # under systemd either /etc/modules or /etc/load-modules.d works
