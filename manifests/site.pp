17404eae8a9f19b02b37432b3eebfc99fe4e5a43
Brandon Black <bblack@wikimedia.org>
2016-02-23 14:05:16 +0000

    cache_misc: remove cp1057, cp1070
    Bug: T125486
    Change-Id: I2a2e2f8e90f71a35359799397e9b50a9e792ca3b

--- manifests/site.pp
+++ manifests/site.pp
@@ -351,14 +351,9 @@ node /^cp10(5[2-5]|6[5-8])\.eqiad\.wmnet$/ {
     role cache::text, ipsec
 }
 
-node 'cp1056.eqiad.wmnet', 'cp1069.eqiad.wmnet' {
+node 'cp1056.eqiad.wmnet', 'cp1057.eqiad.wmnet, 'cp1069.eqiad.wmnet', 'cp1070.eqiad.wmnet' {
     interface::add_ip6_mapped { 'main': }
     include standard # ex-misc
-}
-
-node 'cp1057.eqiad.wmnet', 'cp1070.eqiad.wmnet' {
-    interface::add_ip6_mapped { 'main': }
-    role cache::misc, ipsec
 }
 
 node /^cp20(0[147]|1[0369]|23)\.codfw\.wmnet$/ {
