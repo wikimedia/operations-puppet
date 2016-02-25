97316607cdee0d1adb4148c82b8b5563843e231a
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-02-25 17:47:43 +0100

    role::mail::sender: move to standard
    This is not a role and should really be part of the "standard" module.
    
    Change-Id: Iabdf6bb29299d757e1a0c6b81c2490d206ac6430

--- modules/standard/manifests/init.pp
+++ modules/standard/manifests/init.pp
@@ -10,9 +10,10 @@ class standard(
     include ::standard::ntp
 
     unless $::fqdn in $::standard::ntp::wmf_all_peers {
-        include standard::ntp::client
+        include ::standard::ntp::client
     }
-    include standard::diamond
+
+    include ::standard::diamond
 
     if $has_ganglia {
         include ::ganglia
@@ -21,7 +22,7 @@ class standard(
     # Some instances have their own exim definition that
     # will conflict with this
     if $has_default_mail_relay {
-        include role::mail::sender
+        include ::standard::mail::sender
     }
 
     # Some instances in production (ideally none) and labs do not use
@@ -29,7 +30,5 @@ class standard(
     if $has_admin {
         include ::admin
     }
-
-
 
 }
