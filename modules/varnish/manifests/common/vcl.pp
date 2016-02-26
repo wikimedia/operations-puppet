26956726f3a9d23e892c93073b82f6703f941210
Emanuele Rocca <ema@wikimedia.org>
2016-02-26 18:34:08 +0100

    Maps VCL forward-port to Varnish 4
    This patch uses a boolean hiera attribute, varnish_version4, to
    distinguish between Varnish 3 and Varnish 4 VCL syntax.
    
    With these changes applied, Varnish 4 starts properly on machines
    deployed with role cache::maps.
    
    The included test cases pass with Varnish 4.
    
    Bug: T124279
    Change-Id: Iee05d5f712093c0a1d939e74a340627982979404

--- modules/varnish/manifests/common/vcl.pp
+++ modules/varnish/manifests/common/vcl.pp
@@ -1,6 +1,8 @@
 class varnish::common::vcl {
     require varnish::common
 
+    $varnish_version4 = hiera('varnish_version4', false)
+
     file { '/etc/varnish/geoip.inc.vcl':
         owner   => 'root',
         group   => 'root',
@@ -22,11 +24,23 @@ class varnish::common::vcl {
         content => template('varnish/analytics.inc.vcl.erb'),
     }
 
+    file { '/etc/varnish/errorpage_head.html':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+	source  => 'puppet:///files/varnish/errorpage_head.html',
+    }
+
     # VCL unit tests
     file { '/usr/local/sbin/varnish-test-geoip':
         owner  => 'root',
         group  => 'root',
         mode   => '0555',
         source => 'puppet:///modules/varnish/varnish-test-geoip',
+    }
+
+    file { '/usr/share/varnish/tests/':
+        source  => 'puppet:///modules/varnish/tests',
+        recurse => true,
     }
 }
