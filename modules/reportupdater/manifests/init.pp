abf7ab2385f244134e7354a8dee88768ae8852e0
Marcel Ruiz Forns <mforns@wikimedia.org>
2016-02-26 13:44:03 +0100

    Replace limn::data::generate by reportupdater
    Moves the generation of reports out of the statistics module.
    And makes it more generic, now supporting both report generators:
    generate.py and reportupdater. The latter has now a dedicated
    repository.
    
    In the future we might want to change all report jobs to use
    reportupdater and completely remove the generate.py option.
    
    Bug: T127327
    Change-Id: I79cafe41b1e6002ee3e6c9087809ed9515424ad6

---
+++ modules/reportupdater/manifests/init.pp
@@ -0,0 +1,76 @@
+# == Class reportupdater
+#
+# Sets up base directories and repositories for using reportupdater.
+# See: https://wikitech.wikimedia.org/wiki/Analytics/Reportupdater
+#
+# == Parameters
+#   $user           - string. User to clone the repositories and attribute
+#                     the permits to.
+#   $working_path   - string. Base path where to put the necessary repositories.
+#   $log_path       - string. [optional] Path where to write the generator logs.
+#                     Default: '/var/log/reportupdater'.
+#   $output_path    - string. [optional] Path to output the generated reports.
+#                     Default: "${working_path}/reportupdater-output".
+#   $rsync_to       - string. [optional] If defined, all what is in the output
+#                     path will be rsync'd to $rsync_to.
+#   $generator      - string. The generator that will manage the reports.
+#                     Either 'generate' or 'reportupdater'.
+#
+class reportupdater(
+    $user,
+    $working_path,
+    $log_path          = '/var/log/reportupdater',
+    $output_path       = "${working_path}/reportupdater-output",
+    $rsync_to          = undef,
+    $generator         = 'reportupdater',
+) {
+
+    # There are 2 generator scripts for now. Each one has its own:
+    #   $git_remote   - Repository where to pull the generator from.
+    #   $source_path  - Directory where to clone the repository to.
+    #   $command      - Command to execute the generator.
+    #
+    case $generator {
+        generate: {
+            $git_remote  = 'https://gerrit.wikimedia.org/r/p/analytics/limn-mobile-data.git'
+            $source_path = "${working_path}/limn-mobile-data"
+            $command     = "python ${source_path}/generate.py"
+        }
+        reportupdater: {
+            $git_remote  = 'https://gerrit.wikimedia.org/r/p/analytics/reportupdater.git'
+            $source_path = "${working_path}/reportupdater"
+            $command     = "python ${source_path}/update_reports.py"
+        }
+        default: {
+            fail('generator must be either "generate" or "reportupdater"')
+        }
+    }
+
+    # Ensure the generator is cloned and latest version.
+    if !defined(Git::Clone['analytics/reportupdater']) {
+        git::clone { 'analytics/reportupdater':
+            ensure    => 'latest',
+            directory => $source_path,
+            origin    => $git_remote,
+            owner     => $user,
+            require   => [User[$user]],
+        }
+    }
+
+    # Make sure these are writeable by $user.
+    file { [$log_path, $output_path]:
+        ensure => 'directory',
+        owner  => $user,
+        group  => wikidev,
+        mode   => '0775',
+    }
+
+    # If specified, rsync anything generated in $public_dir to $rsync_to.
+    if $rsync_to != undef {
+        cron { 'rsync_reportupdater_output':
+            command => "/usr/bin/rsync -rt ${output_path}/* ${rsync_to}",
+            user    => $user,
+            minute  => 15,
+        }
+    }
+}
