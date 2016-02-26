d6ea2439f3215b129bb26b22f629a53293038885
Marcel Ruiz Forns <mforns@wikimedia.org>
2016-02-26 13:44:03 +0100

    Replace limn::data::generate by reportupdater
    Moves the generation of reports out of the statistics module.
    And makes it more generic, now supporting both report generators:
    generate.py and reportupdater. The latter has now a dedicated
    repository.
    
    In the future we might want to change all report jobs to use
    reportupdater and completely remove the generate.py option.
    
    Bug: T127327
    Change-Id: I79cafe41b1e6002ee3e6c9087809ed9515424ad6

---
+++ modules/reportupdater/manifests/job.pp
@@ -0,0 +1,58 @@
+# == Define reportupdater::job
+#
+# Sets up hourly cron jobs to run a script which generates and updates
+# tsv datafiles for a set of given queries.
+#
+# This requires that a repository with config and queries for the script
+# exists at https://gerrit.wikimedia.org/r/p/analytics/${repository}.git.
+#
+# == Parameters
+#   repository   - string. [optional] Name of the repository holding the
+#                  queries and the config. Default: "limn-${title}-data".
+#   query_dir    - string. [optional] Path of the directory holding the
+#                  queries and the config within the mentioned repository.
+#                  Default: "${title}".
+#
+# == Usage
+#   reportupdater::job { 'mobile': }
+#   reportupdater::job { 'browser':
+#       repository => 'reportupdater-queries',
+#   }
+#
+define reportupdater::job(
+    $repository = "limn-${title}-data",
+    $query_dir  = "${title}",
+) {
+    Class['::reportupdater'] -> Reportupdater::Job[$title]
+
+    $user    = $::reportupdater::user
+    $command = $::reportupdater::command
+
+    # A repo at analytics/${repository}.git had better exist!
+    $git_remote = "https://gerrit.wikimedia.org/r/p/analytics/${repository}.git"
+
+    # Directory at which to clone $git_remote.
+    $source_path = "${::reportupdater::working_path}/${repository}"
+
+    # Config directory for this report generating job.
+    $query_path = "${$source_path}/${query_dir}"
+
+    # Log file for the generate/reportupdater cron job.
+    $log_file = "${::reportupdater::log_path}/${repository}_${title}.log"
+
+    if !defined(Git::Clone["analytics/${repository}"]) {
+        git::clone { "analytics/${repository}":
+            ensure    => 'latest',
+            directory => $source_path,
+            origin    => $git_remote,
+            owner     => $user,
+            require   => [User[$user]],
+        }
+    }
+
+    cron { "generate_${repository}_${title}":
+        command => "${command} ${query_path} >> ${log_file} 2>&1",
+        user    => $user,
+        minute  => 0,
+    }
+}
