5ee265104eb2c775255cedcdd7843179a27aacc4
Marcel Ruiz Forns <mforns@wikimedia.org>
2016-02-26 13:44:03 +0100

    Replace limn::data::generate by reportupdater
    Moves the generation of reports out of the statistics module.
    Reportupdater now has a dedicated repository.
    
    Bug: T127327
    Change-Id: I79cafe41b1e6002ee3e6c9087809ed9515424ad6

---
+++ modules/reportupdater/manifests/job.pp
@@ -0,0 +1,71 @@
+# == Define reportupdater::job
+#
+# Sets up hourly cron jobs to run reportupdater, which generates
+# and updates tsv reports for a set of given queries.
+#
+# This requires that a repository with config and queries for the script
+# exists at https://gerrit.wikimedia.org/r/p/analytics/${repository}.git.
+#
+# == Parameters
+#   title        - string. Name of query dir inside of $repository.
+#                  a $title directory with reportupdater query config
+#                  must exist inside of $repository.
+#
+#   repository   - string. Name of the query repository in gerrit in the
+#                  analytics/ namespace.  All reportupdater job
+#                  repositories must be in analytics/
+#                  E.g. analytics/reportupdater-queries
+#
+#   output_dir   - string. [optional] Relative path where to write the reports.
+#                  This will be relative to $::reportupdater::base_path/output
+#                  Default: $title
+#
+# == Usage
+#   reportupdater::job { 'browser': }
+#
+#   reportupdater::job { 'mobile':
+#       repository  => 'limn-mobile-data',
+#       output_dir  => "mobile/datafiles",
+#   }
+#
+define reportupdater::job(
+    $repository,
+    $output_dir  = $title,
+)
+{
+    Class['::reportupdater'] -> Reportupdater::Job[$title]
+
+    # Name of the repository in gerrit.
+    # All reportupdater job repositories are in the analytics/ namespace.
+    $repository_name = "analytics/${repository}"
+
+    # Path at which this reportupdater job repository will be cloned.
+    $path            = "${::reportupdater::job_repositories_path}/${repository}"
+
+    # Path of the query configuration directory inside of $repository_name.
+    $query_path      = "${path}/${title}"
+
+    # Path at which the job will store logs.
+    $log_file        = "${$::reportupdater::log_path}/${repository}-${title}.log"
+
+    # Path at which the job will store its report output.
+    $output_path     = "${::reportupdater::output_path}/${output_dir}"
+
+    # Ensure the query repository is cloned and latest version.
+    # It is possible that multiple jobs will use the same repository,
+    # so wrap this in an if !defined.
+    if !defined(Git::Clone[$repository_name]) {
+        git::clone { $repository_name:
+            ensure    => 'latest',
+            directory => $path,
+            origin    => "https://gerrit.wikimedia.org/r/p/${repository_name}.git",
+            owner     => $::reportupdater::user
+        }
+    }
+
+    cron { "reportupdater_${repository}-${title}":
+        command => "python ${::reportupdater::path}/update_reports.py ${query_path} ${output_path} >> ${log_file} 2>&1",
+        user    => $user,
+        minute  => 0,
+    }
+}
