ba0d726eb977e6428dd8d454c952acc8a20e0b5d
YuviPanda <yuvipanda@gmail.com>
2016-02-29 15:15:23 -0800

    k8s: Add auth for docker client to authenticate to registry
    Change-Id: I2c78b5174d53620ed0189f4a8ebe05f3f306ee34

--- modules/k8s/manifests/docker.pp
+++ modules/k8s/manifests/docker.pp
@@ -20,6 +20,37 @@ class k8s::docker {
         require => Package['docker.io'],
     }
 
+    $docker_username = hiera('docker::username')
+    $docker_password = hiera('docker::password')
+    $docker_registry = hiera('docker::registry_url')
+
+    # uses strict_encode64 since encode64 adds newlines?!
+    $docker_auth = inline_template("<%= require 'base64'; Base64.strict_encode64('${docker_username}:${docker_password}') -%>")
+
+    $docker_config = {
+        'auths' => {
+            "${docker_registry}" => {
+                'auth' => $docker_auth,
+            }
+        }
+    }
+
+    file { '/root/.docker':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'docker',
+        mode   => '0550',
+    }
+
+    file { '/root/.docker/config.json':
+        content => ordered_json($docker_config),
+        owner   => 'root',
+        group   => 'docker',
+        mode    => '0440',
+        notify  => Base::Service_unit['docker'],
+        require => File['/root/.docker'],
+    }
+
     base::service_unit { 'docker':
         systemd   => true,
         subscribe => Package['docker-engine'],
