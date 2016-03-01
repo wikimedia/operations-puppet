0ee0a17088cf9744ce0de5e522e188f768fe2c0b
Ariel T. Glenn <ariel@wikimedia.org>
2016-03-01 15:37:18 +0200

    re-enable rsyncs between dataset1001 and ms1001
    Change-Id: Ib5d9df312f26f6c931064539cb8aa82949a5cc7d

--- modules/role/manifests/dataset/primary.pp
+++ modules/role/manifests/dataset/primary.pp
@@ -10,7 +10,7 @@ class role::dataset::primary {
 
     $rsync = {
         'public' => true,
-        # 'peers'  => true,
+        'peers'  => true,
         'labs'   => true,
     }
     $grabs = {
