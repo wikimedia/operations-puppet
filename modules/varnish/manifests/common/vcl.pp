147d243c248671cf21ca8d092244bf9f9afd2ac3
Brandon Black <bblack@wikimedia.org>
2016-03-01 23:46:21 +0000

    geoip.inc.vcl.erb: move to text extra_vcl
    Only text-frontend uses this now.  Keeping it out of vcl::common
    reduces puppet and reload-vcl churn for other instances/daemons.
    
    Bug: T127481
    Change-Id: I705f9d9a6b82709bd7bd3b5c603f532a366f3d7d

--- modules/varnish/manifests/common/vcl.pp
+++ modules/varnish/manifests/common/vcl.pp
@@ -1,13 +1,6 @@
 class varnish::common::vcl {
     require varnish::common
 
-    file { '/etc/varnish/geoip.inc.vcl':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        content => template('varnish/geoip.inc.vcl.erb'),
-    }
-
     file { '/etc/varnish/errorpage.inc.vcl':
         owner   => 'root',
         group   => 'root',
