69cccfffd6816400eef3c657ce1b75b1c7a96076
Mukunda Modell <mmodell@wikimedia.org>
2016-03-02 15:55:59 -0600

    Add a deployment source & target class for phabricator
    This also adds support for key_content => secret('...') in
    keyholder so that we can use a newer/cleaner pattern for
    specifying the private key.
    
    Refs T125851
    
    Bug: T114363
    Change-Id: I06eee23c338840fbba8ca81270f5ec0c81e02869

---
+++ modules/phabricator/manifests/deployment/target.pp
@@ -0,0 +1,18 @@
+# == Class phabricator::deployment::target
+# This sets up sudo rules and the deploy_user for phabricator deployments with
+# scap3.
+
+class phabricator::deployment::target(
+  $deploy_user,
+  $deploy_key,
+  $deploy_target= 'phabricator/deployment',
+) {
+    scap::target { $deploy_target:
+        deploy_user       => $deploy_user,
+        public_key_source => $deploy_key,
+        sudo_rules        => [
+            'ALL=(root) NOPASSWD: /usr/sbin/service phd *',
+            'ALL=(root) NOPASSWD: /usr/sbin/service apache2 *',
+        ]
+    }
+}
