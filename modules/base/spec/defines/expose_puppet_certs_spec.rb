f357c830e341f20c3dabb0aa4134388427d3193c
Guillaume Lederrey <glederrey@wikimedia.org>
2016-03-02 13:51:02 +0100

    Factorized code exposing Puppet SSL certs
    Minimal tests have been written before the refactoring to ensure behaviour
    is unchanged.
    
    Bug: T124444
    Change-Id: I0d6976675f48679cfec78120d4a09cfef73970bd

---
+++ modules/base/spec/defines/expose_puppet_certs_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe 'base::expose_puppet_certs', :type => :define do
+
+  describe 'directory structure is created' do
+    let(:title) { '/my/ssl/dir' }
+
+    it { should contain_file('/my/ssl/dir/ssl').with({ 'ensure' => 'directory', 'mode' => '0555' }) }
+    it { should contain_file('/my/ssl/dir/ssl/certs').with({ 'ensure' => 'directory', 'mode' => '0555' }) }
+    it { should contain_file('/my/ssl/dir/ssl/private_keys').with({ 'ensure' => 'directory', 'mode' => '0555' }) }
+  end
+
+  describe 'ca certificate is created' do
+    let(:title) { '/my/ssl/dir' }
+
+    it { should contain_file('/my/ssl/dir/ssl/certs/ca.pem').with({ 'ensure' => 'present', 'mode' => '0444'  }) }
+  end
+
+  describe 'host certificate is exposed' do
+    let(:title) { '/my/ssl/dir' }
+    let(:facts) { { :fqdn => 'host.example.net'} }
+
+    it { should contain_file('/my/ssl/dir/ssl/certs/cert.pem')
+                    .with({
+                              'ensure' => 'present',
+                              'mode' => '0400',
+                              'source' => '/var/lib/puppet/ssl/certs/host.example.net.pem',
+                          })
+    }
+  end
+
+  describe 'private key is not exposed by default' do
+    let(:title) { '/my/ssl/dir' }
+    let(:facts) { { :fqdn => 'host.example.net'} }
+
+    it { should_not contain_file('/my/ssl/dir/ssl/private_keys/server.key') }
+  end
+
+  describe 'private key is exposed if required' do
+    let(:title) { '/my/ssl/dir' }
+    let(:facts) { { :fqdn => 'host.example.net'} }
+    let(:params) { { :provide_private => true } }
+
+    it { should contain_file('/my/ssl/dir/ssl/private_keys/server.key')
+                    .with({
+                              'ensure' => 'present',
+                              'mode' => '0400',
+                              'source' => '/var/lib/puppet/ssl/private_keys/host.example.net.pem',
+                          })
+    }
+  end
+
+end
