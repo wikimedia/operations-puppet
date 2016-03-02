721e6bb3a3ae428af66c34e021c905bf50201395
Brandon Black <bblack@wikimedia.org>
2016-03-02 14:56:48 +0000

    wmflib: add hash_select_re and hash_deselect_re
    Bug: T127481
    Change-Id: I6c6928fc0800670213cfa977b2bede8573f39061

---
+++ modules/wmflib/lib/puppet/parser/functions/hash_deselect_re.rb
@@ -0,0 +1,34 @@
+#
+# hash_deselect_re.rb
+#
+
+module Puppet::Parser::Functions
+  newfunction(:hash_deselect_re, :type => :rvalue, :doc => <<-EOS
+This function creates a new hash from the input hash, filtering out keys which
+match the provided regex.
+
+*Examples:*
+
+    $in = { 'abc' => 1, 'def' => 2, 'asdf' => 3 }
+    $out = hash_deselect_re('^a', $in);
+    # $out == { 'def' => 2 }
+    $out2 = hash_deselect_re('^(?!a)', $in);
+    # $out2 == { 'abc' => 1, 'asdf' => 3 }
+
+    EOS
+  ) do |arguments|
+
+    raise(Puppet::ParseError, "hash_deselect_re(): Wrong number of arguments " +
+      "given (#{arguments.size} for 2)") if arguments.size != 2
+
+    pattern = Regexp.new(arguments[0])
+    in_hash = arguments[1]
+    unless in_hash.is_a?(Hash)
+      raise(Puppet::ParseError, 'hash_deselect_re(): Argument 2 must be a hash')
+    end
+
+    in_hash.select { |k, _v| !pattern.match(k) }
+  end
+end
+
+# vim: set ts=2 sw=2 et :
