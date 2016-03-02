da8ad87958338f67ca9bf5ec94a75700350268d3
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-03-02 11:48:33 +0100

    Puppetise yubikey key storage module
    Change-Id: I926f943ed217dc051cdbd746ac58b8aa445d652b

---
+++ modules/yubiauth/manifests/yhsm_yubikey_ksm.pp
@@ -0,0 +1,23 @@
+# yhsm-yubikey-ksm ‐ Yubikey key storage module using a YubiHSM
+class yubiauth::yhsm_yubikey_ksm(
+    $yhsmdevice = '/dev/ttyACM0',
+) {
+
+    require_package('yhsm-yubikey-ksm')
+
+    file { 'yhsm-yubikey-ksm-default':
+        mode    => '0440',
+        owner   => root,
+        group   => root,
+        path    => '/etc/default/yhsm-yubikey-ksm',
+        content => template('yubiauth/yhsm-yubikey-ksm-default.erb'),
+    }
+
+    service { 'yhsm-yubikey-ksm':
+        enable  => true,
+        require => [
+                    Package['yhsm-yubikey-ksm'],
+                    File['/etc/default/yhsm-yubikey-ksm'],
+                    ],
+    }
+}
