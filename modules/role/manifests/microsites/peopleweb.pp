458d692a18a774a9ad4073e1a8afdff5e03fb310
dzahn <dzahn@wikimedia.org>
2016-03-04 11:35:09 -0800

    move microsite roles into a common place
    Move roles that setup microsites into one a common place.
    
    This makes it easier to find similar roles in one place,
    and it also moves more roles into proper autoloader layout.
    
    Also move the peopleweb::migration class into a separate file.
    
    Change-Id: I74c16340c44318876db701ff37f84f780a40c5d3

--- modules/role/manifests/microsites/peopleweb.pp
+++ modules/role/manifests/microsites/peopleweb.pp
@@ -1,4 +1,5 @@
-class role::peopleweb {
+# let users publish their own HTML in their home dirs
+class role::microsites::peopleweb {
 
     include standard
 
@@ -18,25 +19,5 @@ class role::peopleweb {
     }
 
     backup::set {'home': }
-}
-
-class role::peopleweb::migration {
-
-    $sourceip='10.64.32.13'
-
-    ferm::service { 'peopleweb-migration-rysnc':
-        proto  => 'tcp',
-        port   => '873',
-        srange => "${sourceip}/32",
-    }
-
-    include rsync::server
-
-    rsync::server::module { 'people-homes':
-        path        => '/home',
-        read_only   => 'no',
-        hosts_allow => $sourceip,
-    }
-
 }
 
