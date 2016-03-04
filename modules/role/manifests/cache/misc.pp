0907e98bcc2730ab1120323c40ed3c3341b29783
Brandon Black <bblack@wikimedia.org>
2016-03-04 21:35:45 +0000

    varnish: get rid of backend_options
    This eliminates the confusing, complex mess of backend_option() in
    VCL and the cascading definitions that came from puppet data
    defaults at various layers, regex matches on backend hostnames
    (which always matched all the hosts of one director), and then
    finally defaults for undefined options encoded in erb directly.
    
    Now, from the template or varnish::instance perspective, every
    director must define 'be_opts', which lists all options for the
    backends in that director (no defaulting).
    
    Further up in the cache roles, simple defaulting is done for
    applayer definitions with common attributes using merge().
    
    Bug: T127484
    Change-Id: Ibb2fae6bd2a2842ae49cece754bb7e809d0970b8

--- modules/role/manifests/cache/misc.pp
+++ modules/role/manifests/cache/misc.pp
@@ -10,131 +10,182 @@ class role::cache::misc {
         realserver_ips => $lvs::configuration::service_ips['misc_web'][$::site],
     }
 
+    $fe_cache_be_opts = {
+        'port'                  => 3128,
+        'connect_timeout'       => '5s',
+        'first_byte_timeout'    => '185s',
+        'between_bytes_timeout' => '2s',
+        'max_connections'       => 100000,
+        'probe'                 => 'varnish',
+    }
+
+    $be_cache_be_opts = {
+        'port'                  => 3128,
+        'connect_timeout'       => '5s',
+        'first_byte_timeout'    => '185s',
+        'between_bytes_timeout' => '4s',
+        'max_connections'       => 100,
+        'probe'                 => 'varnish',
+    }
+
+    $app_def_be_opts = {
+        'port'                  => 80,
+        'connect_timeout'       => '5s',
+        'first_byte_timeout'    => '185s',
+        'between_bytes_timeout' => '4s',
+        'max_connections'       => 100,
+    }
+
     $app_directors = {
         'analytics1001' => { # Hadoop Yarn ResourceManager GUI
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['analytics1001.eqiad.wmnet'],
+            'be_opts'  => merge($app_def_be_opts, { 'port' => 8088 }),
         },
         'analytics1027' => { # Hue (Hadoop GUI)
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['analytics1027.eqiad.wmnet'],
+            'be_opts'  => merge($app_def_be_opts, { 'port' => 8888 }),
         },
         'antimony' => {
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['antimony.wikimedia.org'],
+            'be_opts'  => merge($app_def_be_opts, { 'port' => 8080 }),
         },
         'bromine' => { # ganeti VM for misc. static HTML sites
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['bromine.eqiad.wmnet'],
+            'be_opts'  => $app_def_be_opts,
         },
         'bohrium' => {
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['bohrium.eqiad.wmnet'],
+            'be_opts'  => $app_def_be_opts,
         },
         'californium' => {
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['californium.wikimedia.org'],
+            'be_opts'  => $app_def_be_opts,
         },
         'labtestweb2001' => {
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['labtestweb2001.wikimedia.org'],
+            'be_opts'  => $app_def_be_opts,
         },
         'dataset1001' => {
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['dataset1001.wikimedia.org'],
+            'be_opts'  => $app_def_be_opts,
         },
         'etherpad1001' => {
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['etherpad1001.eqiad.wmnet'],
+            'be_opts'  => merge($app_def_be_opts, { 'port' => 9001 }),
         },
         'gallium' => { # CI server
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['gallium.wikimedia.org' ],
+            'be_opts'  => $app_def_be_opts,
         },
         'graphite1001' => {
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['graphite1001.eqiad.wmnet'],
+            'be_opts'  => $app_def_be_opts,
         },
         'iridium' => { # main phab
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['iridium.eqiad.wmnet'],
+            'be_opts'  => $app_def_be_opts,
         },
         'krypton' => { # ganeti VM for misc. PHP apps
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['krypton.eqiad.wmnet'],
+            'be_opts'  => $app_def_be_opts,
         },
         'magnesium' => { # RT and racktables
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['magnesium.wikimedia.org'],
+            'be_opts'  => $app_def_be_opts,
         },
         'neon' => { # monitoring tools (icinga et al)
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['neon.wikimedia.org'],
+            'be_opts'  => $app_def_be_opts,
         },
         'netmon1001' => { # servermon
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['netmon1001.wikimedia.org'],
+            'be_opts'  => $app_def_be_opts,
         },
         'noc' => { # noc.wikimedia.org and dbtree.wikimedia.org
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['mw1152.eqiad.wmnet'],
+            'be_opts'  => $app_def_be_opts,
         },
         'palladium' => {
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['palladium.eqiad.wmnet'],
+            'be_opts'  => $app_def_be_opts,
         },
         'planet1001' => {
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['planet1001.eqiad.wmnet'],
+            'be_opts'  => $app_def_be_opts,
         },
         'ruthenium' => { # parsoid rt test server
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['ruthenium.eqiad.wmnet'],
+            'be_opts'  => merge($app_def_be_opts, { 'port' => 8001 }),
         },
         'rutherfordium' => { # people.wikimedia.org
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['rutherfordium.eqiad.wmnet'],
+            'be_opts'  => $app_def_be_opts,
         },
         'stat1001' => { # metrics and metrics-api
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['stat1001.eqiad.wmnet'],
+            'be_opts'  => $app_def_be_opts,
         },
         'terbium' => { # noc.wikimedia.org
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['terbium.eqiad.wmnet'],
+            'be_opts'  => $app_def_be_opts,
         },
-        'mendelevium' => { # OTRS
+        'mendelevium' => { # OTRS (search is really slow, hence bbt=60s below)
             'dynamic'  => 'no',
             'type'     => 'random',
             'backends' => ['mendelevium.eqiad.wmnet'],
+            'be_opts'  => merge($app_def_be_opts, { 'between_bytes_timeout' => '60s' })
         },
         'ytterbium' => { # Gerrit
             'dynamic' => 'no',
             'type' => 'random',
             'backends' => ['ytterbium.wikimedia.org'],
+            'be_opts'  => merge($app_def_be_opts, { 'port' => 8080 }),
         },
         'logstash' => {
             'dynamic'  => 'no',
@@ -144,68 +195,14 @@ class role::cache::misc {
                 'logstash1002.eqiad.wmnet',
                 'logstash1003.eqiad.wmnet',
             ],
+            'be_opts'  => merge($app_def_be_opts, { 'probe' => 'logstash' }),
         },
         'wdqs' => {
-            'dynamic' => 'no',
-            'type'    => 'random',
-            backends  => ['wdqs1001.eqiad.wmnet', 'wdqs1002.eqiad.wmnet'],
+            'dynamic'  => 'no',
+            'type'     => 'random',
+            'backends' => ['wdqs1001.eqiad.wmnet', 'wdqs1002.eqiad.wmnet'],
+            'be_opts'  => merge($app_def_be_opts, { 'probe' => 'wdqs' }),
         },
-    }
-
-    $app_be_opts = [
-        {
-            'backend_match' => '^(antimony|ytterbium)',
-            'port'          => 8080,
-        },
-        {
-            'backend_match' => '^(ruthenium)',
-            'port'          => 8001,
-        },
-        {
-            'backend_match' => '^logstash',
-            'probe'         => 'logstash',
-        },
-        {
-            'backend_match' => '^wdqs',
-            'probe'         => 'wdqs',
-        },
-        {
-            # hue serves requests on port 8888
-            'backend_match' => '^analytics1027',
-            'port'          => 8888,
-        },
-        {
-            # Yarn ResourceManager UI serves requests on port 8088
-            'backend_match' => '^analytics1001',
-            'port'          => 8088,
-        },
-        {
-            # etherpad nodejs listens on 9001
-            'backend_match' => '^etherpad1001',
-            'port'          => 9001,
-        },
-        {
-            # OTRS search is really slow, increase timeout
-            'backend_match'         => '^mendelevium',
-            'between_bytes_timeout' => '60s',
-        },
-    ]
-
-    $fe_def_beopts = {
-        'port'                  => 3128,
-        'connect_timeout'       => '5s',
-        'first_byte_timeout'    => '185s',
-        'between_bytes_timeout' => '2s',
-        'max_connections'       => 100000,
-        'probe'                 => 'varnish',
-    }
-
-    $be_def_beopts = {
-        'port'                  => 80,
-        'connect_timeout'       => '5s',
-        'first_byte_timeout'    => '185s',
-        'between_bytes_timeout' => '4s',
-        'max_connections'       => 100,
     }
 
     $common_vcl_config = {
@@ -217,18 +214,18 @@ class role::cache::misc {
     }
 
     role::cache::instances { 'misc':
-        fe_mem_gb      => 8,
-        runtime_params => [],
-        app_directors  => $app_directors,
-        app_be_opts    => $app_be_opts,
-        fe_vcl_config  => $common_vcl_config,
-        be_vcl_config  => $common_vcl_config,
-        fe_extra_vcl   => ['misc-common'],
-        be_extra_vcl   => ['misc-common'],
-        fe_def_beopts  => $fe_def_beopts,
-        be_def_beopts  => $be_def_beopts,
-        be_storage     => $::role::cache::2layer::persistent_storage_args,
-        cluster_nodes  => hiera('cache::misc::nodes'),
+        fe_mem_gb        => 8,
+        runtime_params   => [],
+        app_directors    => $app_directors,
+        app_be_opts      => $app_be_opts,
+        fe_vcl_config    => $common_vcl_config,
+        be_vcl_config    => $common_vcl_config,
+        fe_extra_vcl     => ['misc-common'],
+        be_extra_vcl     => ['misc-common'],
+        be_storage       => $::role::cache::2layer::persistent_storage_args,
+        fe_cache_be_opts => $fe_cache_be_opts,
+        be_cache_be_opts => $be_cache_be_opts,
+        cluster_nodes    => hiera('cache::misc::nodes'),
     }
 
     # Install a varnishkafka producer to send
