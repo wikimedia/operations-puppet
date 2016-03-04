1dc40afa935586d8f4daf104bc750415f95e5cb2
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-03-04 16:23:28 +0100

    Move dynamicproxy ferm rules into role::labs::novaproxy and role::labs::tools::proxy
    ferm rules/services should be defined in the roles, not the individual
    modules (since the access restrictions might differ per role).
    
    Also drop dependency on ferm service in proxymanager's nginx::site,
    ferm is started during early boot.
    
    Change-Id: Ied72f52aa3f5b72f546dc31eab39b46392de6f94

--- modules/role/manifests/labs/tools/proxy.pp
+++ modules/role/manifests/labs/tools/proxy.pp
@@ -2,5 +2,24 @@ class role::labs::tools::proxy {
     include toollabs::proxy
     include role::toollabs::k8s::webproxy
 
+    ferm::service { 'proxymanager':
+        proto  => 'tcp',
+        port   => '8081',
+        desc   => 'Proxymanager service for Labs instances',
+        srange => '$INTERNAL',
+    }
+
+    ferm::service{ 'http':
+        proto => 'tcp',
+        port  => '80',
+        desc  => 'HTTP webserver for the entire world',
+    }
+
+    ferm::service{ 'https':
+        proto => 'tcp',
+        port  => '443',
+        desc  => 'HTTPS webserver for the entire world',
+    }
+
     system::role { 'role::labs::tools::proxy': description => 'Tool labs generic web proxy' }
 }
