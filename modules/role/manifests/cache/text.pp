8d541daf9a6a5429fe0ff1b637abd54770b713de
Brandon Black <bblack@wikimedia.org>
2016-03-04 21:35:45 +0000

    varnish: get rid of backend_options
    This eliminates the confusing, complex mess of backend_option() in
    VCL and the cascading definitions that came from puppet data
    defaults at various layers, regex matches on backend hostnames
    (which always matched all the hosts of one director), and then
    finally defaults for undefined options encoded in erb directly.
    
    Now, from the template or varnish::instance perspective, every
    director must define 'be_opts', which lists all options for the
    backends in that director (no defaulting).
    
    Further up in the cache roles, simple defaulting is done for
    applayer definitions with common attributes using merge().
    
    Bug: T127484
    Change-Id: Ibb2fae6bd2a2842ae49cece754bb7e809d0970b8

--- modules/role/manifests/cache/text.pp
+++ modules/role/manifests/cache/text.pp
@@ -19,72 +19,7 @@ class role::cache::text {
         realserver_ips => $lvs::configuration::service_ips['text'][$::site]
     }
 
-    $app_directors = {
-        'appservers'       => {
-            'dynamic'  => 'no',
-            'type'     => 'random',
-            'backends' => $role::cache::configuration::backends[$::realm]['appservers'][$::mw_primary],
-        },
-        'api'              => {
-            'dynamic'  => 'no',
-            'type'     => 'random',
-            'backends' => $role::cache::configuration::backends[$::realm]['api'][$::mw_primary],
-        },
-        'rendering'        => {
-            'dynamic'  => 'no',
-            'type'     => 'random',
-            'backends' => $role::cache::configuration::backends[$::realm]['rendering'][$::mw_primary],
-        },
-        'security_audit'   => {
-            'dynamic'  => 'no',
-            'type'     => 'random',
-            'backends' => $role::cache::configuration::backends[$::realm]['security_audit'][$::mw_primary],
-        },
-        'appservers_debug'   => {
-            'dynamic'  => 'no',
-            'type'     => 'random',
-            'backends' => $role::cache::configuration::backends[$::realm]['appservers_debug'][$::mw_primary],
-        },
-        'restbase_backend' => {
-            'dynamic'  => 'no',
-            'type'     => 'random',
-            'backends' => $role::cache::configuration::backends[$::realm]['restbase'][$::mw_primary],
-        },
-        'cxserver_backend' => { # LEGACY: should be removed eventually
-            'dynamic'  => 'no',
-            'type'     => 'random',
-            'backends' => $::role::cache::configuration::backends[$::realm]['cxserver'][$::mw_primary],
-        },
-        'citoid_backend'   => { # LEGACY: should be removed eventually
-            'dynamic'  => 'no',
-            'type'     => 'random',
-            'backends' => $::role::cache::configuration::backends[$::realm]['citoid'][$::mw_primary],
-        },
-    }
-
-    $app_be_opts = [
-        {
-            'backend_match'   => '^appservers-debug\.svc\.eqiad\.wmnet$',
-            'max_connections' => 20,
-        },
-        {
-            'backend_match'   => '^restbase\.svc\.|^deployment-restbase',
-            'port'            => 7231,
-            'max_connections' => 5000,
-        },
-        { # LEGACY: should be removed eventually
-            'backend_match' => '^cxserver',
-            'port'          => 8080,
-            'probe'         => false,
-        },
-        { # LEGACY: should be removed eventually
-            'backend_match' => '^citoid',
-            'port'          => 1970,
-            'probe'         => false,
-        },
-    ]
-
-    $fe_def_beopts = {
+    $fe_cache_be_opts = {
         'port'                  => 3128,
         'connect_timeout'       => '5s',
         'first_byte_timeout'    => '185s',
@@ -93,12 +28,72 @@ class role::cache::text {
         'probe'                 => 'varnish',
     }
 
-    $be_def_beopts = {
+    $be_cache_be_opts = {
+        'port'                  => 3128,
+        'connect_timeout'       => '5s',
+        'first_byte_timeout'    => '180s',
+        'between_bytes_timeout' => '4s',
+        'max_connections'       => 1000,
+        'probe'                 => 'varnish',
+    }
+
+    $app_def_be_opts = {
         'port'                  => 80,
         'connect_timeout'       => '5s',
         'first_byte_timeout'    => '180s',
         'between_bytes_timeout' => '4s',
         'max_connections'       => 1000,
+    }
+
+    $app_directors = {
+        'appservers'       => {
+            'dynamic'  => 'no',
+            'type'     => 'random',
+            'backends' => $role::cache::configuration::backends[$::realm]['appservers'][$::mw_primary],
+            'be_opts'  => $app_def_be_opts,
+        },
+        'api'              => {
+            'dynamic'  => 'no',
+            'type'     => 'random',
+            'backends' => $role::cache::configuration::backends[$::realm]['api'][$::mw_primary],
+            'be_opts'  => $app_def_be_opts,
+        },
+        'rendering'        => {
+            'dynamic'  => 'no',
+            'type'     => 'random',
+            'backends' => $role::cache::configuration::backends[$::realm]['rendering'][$::mw_primary],
+            'be_opts'  => $app_def_be_opts,
+        },
+        'security_audit'   => {
+            'dynamic'  => 'no',
+            'type'     => 'random',
+            'backends' => $role::cache::configuration::backends[$::realm]['security_audit'][$::mw_primary],
+            'be_opts'  => $app_def_be_opts,
+        },
+        'appservers_debug'   => {
+            'dynamic'  => 'no',
+            'type'     => 'random',
+            'backends' => $role::cache::configuration::backends[$::realm]['appservers_debug'][$::mw_primary],
+            'be_opts'  => merge($app_def_be_opts, { 'max_connections' => 20 }),
+        },
+        'restbase_backend' => {
+            'dynamic'  => 'no',
+            'type'     => 'random',
+            'backends' => $role::cache::configuration::backends[$::realm]['restbase'][$::mw_primary],
+            'be_opts'  => merge($app_def_be_opts, { 'port' => 7231, 'max_connections' => 5000 }),
+        },
+        'cxserver_backend' => { # LEGACY: should be removed eventually
+            'dynamic'  => 'no',
+            'type'     => 'random',
+            'backends' => $::role::cache::configuration::backends[$::realm]['cxserver'][$::mw_primary],
+            'be_opts'  => merge($app_def_be_opts, { 'port' => 8080 }),
+        },
+        'citoid_backend'   => { # LEGACY: should be removed eventually
+            'dynamic'  => 'no',
+            'type'     => 'random',
+            'backends' => $::role::cache::configuration::backends[$::realm]['citoid'][$::mw_primary],
+            'be_opts'  => merge($app_def_be_opts, { 'port' => 1970 }),
+        },
     }
 
     $common_vcl_config = {
@@ -120,18 +115,18 @@ class role::cache::text {
     })
 
     role::cache::instances { 'text':
-        fe_mem_gb      => ceiling(0.125 * $::memorysize_mb / 1024.0),
-        runtime_params => ['default_ttl=2592000'],
-        app_directors  => $app_directors,
-        app_be_opts    => $app_be_opts,
-        fe_vcl_config  => $fe_vcl_config,
-        be_vcl_config  => $be_vcl_config,
-        fe_extra_vcl   => ['text-common', 'zero', 'normalize_path', 'geoip'],
-        be_extra_vcl   => ['text-common', 'normalize_path'],
-        be_storage     => $::role::cache::2layer::persistent_storage_args,
-        fe_def_beopts  => $fe_def_beopts,
-        be_def_beopts  => $be_def_beopts,
-        cluster_nodes  => hiera('cache::text::nodes'),
+        fe_mem_gb        => ceiling(0.125 * $::memorysize_mb / 1024.0),
+        runtime_params   => ['default_ttl=2592000'],
+        app_directors    => $app_directors,
+        app_be_opts      => $app_be_opts,
+        fe_vcl_config    => $fe_vcl_config,
+        be_vcl_config    => $be_vcl_config,
+        fe_extra_vcl     => ['text-common', 'zero', 'normalize_path', 'geoip'],
+        be_extra_vcl     => ['text-common', 'normalize_path'],
+        be_storage       => $::role::cache::2layer::persistent_storage_args,
+        fe_cache_be_opts => $fe_cache_be_opts,
+        be_cache_be_opts => $be_cache_be_opts,
+        cluster_nodes    => hiera('cache::text::nodes'),
     }
 
     # varnishkafka statsv listens for special stats related requests
