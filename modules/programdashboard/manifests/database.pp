878cea4474876ab0b6f580e8855d32cb2c68a4f3
Dan Duvall <dduvall@wikimedia.org>
2016-03-04 16:25:46 -0800

    labs: Database server to support Program Dashboard
    Provision a basic MariaDB server to store Program Dashboard data. The
    data directory is kept on an LVM volume to allow for periodic
    snapshotting.
    
    Bug: T127105
    Change-Id: Ie816ea8f4127c8a2c9a817c8d815f3b7956b0cde

---
+++ modules/programdashboard/manifests/database.pp
@@ -0,0 +1,40 @@
+# = Class: programdashboard::database
+#
+# A database server for the the Program Dashboard Rails application.
+#
+# === Parameters
+#
+# [*datadir*]
+#   MariaDB data directory. An XFS formatted LVM volume will be created from
+#   80% of the instance volume's free space and mounted at this directory.
+#
+# [*tmpdir*]
+#   MariaDB temporary directory. An XFS formatted LVM volume will be created
+#   from 20% of the instance volume's free space and mounted at this
+#   directory.
+#
+class programdashboard::database(
+    $datadir,
+    $tmpdir,
+) {
+    include mariadb::packages
+
+    labs_lvm::volume { 'dashboard-data':
+        mountat => $datadir,
+        size    => '80%FREE',
+    }
+
+    labs_lvm::volume { 'dashboard-tmp':
+        mountat => $tmpdir,
+        size    => '100%FREE',
+        require => Labs_lvm::Volume['dashboard-db'],
+    }
+
+    class { 'mariadb::config':
+        prompt   => 'DASHBOARD',
+        config   => 'programdashboard/my.cnf.erb',
+        datadir  => "${datadir}/sql",
+        tmpdir   => "${tmpdir}/sql",
+        require  => Labs_lvm::Volume['dashboard-db', 'dashboard-tmp'],
+    }
+}
