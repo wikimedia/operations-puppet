604e2d6d72c087d3ddf34876590b7dee470c2d85
Ori Livneh <ori@wikimedia.org>
2016-03-06 12:37:05 -0800

    Add debug_proxy module, for X-Wikimedia-Debug request routing
    The module configures an nginx instance that listens on port 80 and acts
    as a transparent proxy which passes requests to a set of un-pooled app
    servers that are reserved for debugging, based on the content of the
    X-Wikimedia-Debug HTTP header. The set of permissable backends which may
    be selected via the header is configurable via a regexp.
    
    Bug: T129000
    Bug: T129003
    Change-Id: I2ef40124c891c7f9039850f90c7dc2f6e2641487

---
+++ modules/debug_proxy/manifests/init.pp
@@ -0,0 +1,43 @@
+# == Class: debug_proxy
+#
+# Transparent proxy which passes requests to a set of un-pooled
+# application servers that are reserved for debugging, based on
+# the value of the X-Wikimedia-Debug HTTP header.
+#
+# === Parameters
+#
+# [*backend_regexp*]
+#   If the value of X-Wikimedia-Debug matches this regular expression,
+#   it will be used as the backend address, verbatim.
+#
+# [*backend_aliases*]
+#   If the value of X-Wikimedia-Debug is equal to a defined alias,
+#   the alias's target will be used as the backend address.
+#
+# === Examples
+#
+# Allow requests to select mw1017 / mw1018 / mw1019 explicitly,
+# and map 'X-Wikimedia-Debug: profile' to mw1020:
+#
+#  class { '::debug_proxy':
+#      backend_regexp  => '^mw101[789]',
+#      backend_aliases => { 'profile' => 'mw1020.eqiad.wmnet' },
+#  }
+#
+class debug_proxy(
+    $backend_regexp,
+    $backend_aliases,
+) {
+    nginx::site { 'debug_proxy':
+        content => template('debug_proxy/debug_proxy.nginx.erb'),
+        notify  => Service['nginx'],
+    }
+
+    diamond::collector::nginx { 'debug_proxy': }
+
+    ferm::service { 'debug_proxy':
+        proto  => 'tcp',
+        port   => '80',
+        srange => '$INTERNAL',
+    }
+}
