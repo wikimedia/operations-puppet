7674c34f91475bc044c5aecdaa007a6ce36bd5a0
Chad Horohoe <chadh@wikimedia.org>
2016-03-08 12:05:34 -0800

    WIP: Gerrit manifest cleanup
    - Remove two unused parameters: apache_ssl (always on) and slave (not supported)
    - Narrow scope in which password and ldap info is included, proxy doesn't need
      to know
    - Standardize the parameters to use underscores
    - Include gerrit::crons as part of gerrit::instance and require gerrit::jetty first
    
    Change-Id: I845c451fdc41b28ffd2e1b461f8cfc213419cb39

--- modules/gerrit/manifests/instance.pp
+++ modules/gerrit/manifests/instance.pp
@@ -1,42 +1,14 @@
 # Manifest to setup a Gerrit instance
 
 class gerrit::instance(
-    $apache_ssl  = false,
-    $slave       = false,
-    $ssh_port    = '29418',
     $db_host     = '',
-    $db_name     = 'reviewdb',
     $host        = '',
-    $db_user     = 'gerrit',
     $ssh_key     = '',
     $ssl_cert    = 'ssl-cert-snakeoil',
     $ssl_cert_key= 'ssl-cert-snakeoil',
     $replication = '',
     $smtp_host   = ''
     ) {
-
-    include standard,
-        ldap::role::config::labs
-
-    # Main config
-    include passwords::gerrit
-    $email_key = $passwords::gerrit::gerrit_email_key
-    $sshport = $ssh_port
-    $dbhost = $db_host
-    $dbname = $db_name
-    $dbuser = $db_user
-    $dbpass = $passwords::gerrit::gerrit_db_pass
-    $bzpass = $passwords::gerrit::gerrit_bz_pass
-    $phabcert = $passwords::gerrit::gerrit_phab_cert
-
-    # Setup LDAP
-    include ldap::role::config::labs
-    $ldapconfig = $ldap::role::config::labs::ldapconfig
-
-    $ldap_hosts = $ldapconfig['servernames']
-    $ldap_base_dn = $ldapconfig['basedn']
-    $ldap_proxyagent = $ldapconfig['proxyagent']
-    $ldap_proxyagent_pass = $ldapconfig['proxypass']
 
     # Configure the base URL
     $url = "https://${host}/r"
@@ -48,18 +20,15 @@ class gerrit::instance(
     }
 
     class { 'gerrit::jetty':
-        ldap_hosts           => $ldap_hosts,
-        ldap_base_dn         => $ldap_base_dn,
-        url                  => $url,
-        dbhost               => $dbhost,
-        dbname               => $dbname,
-        dbuser               => $dbuser,
-        hostname             => $host,
-        ldap_proxyagent      => $ldap_proxyagent,
-        ldap_proxyagent_pass => $ldap_proxyagent_pass,
-        sshport              => $sshport,
-        replication          => $replication,
-        smtp_host            => $smtp_host,
-        ssh_key              => $ssh_key,
+        url         => $url,
+        db_host     => $db_host,
+        hostname    => $host,
+        replication => $replication,
+        smtp_host   => $smtp_host,
+        ssh_key     => $ssh_key,
+    }
+
+    class { 'gerrit::crons':
+        require => Class['gerrit::jetty'],
     }
 }
