85b7d7499babdfc1b3649c38d49affefff7218c2
Ariel T. Glenn <ariel@wikimedia.org>
2016-03-08 15:07:13 +0200

    fix up rsync args for datset1001 rsync to labs
    Bug: T128945
    Change-Id: I0e3c5033b5a9795eef4693c440f44ca3cbd810a3

--- modules/dataset/files/labs/labs-rsync-cron.sh
+++ modules/dataset/files/labs/labs-rsync-cron.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 desthost="labstore1003.eqiad.wmnet"
 otherdir="/data/xmldatadumps/public/other"
-bwlimit=50000
+bwlimit="--bwlimit=50000"
 
 do_rsync (){
     srcdir=$1
     destdir=$2
 
-    running=`pgrep -u root -f -x "/usr/bin/rsync -rlpt $bwlimit ${otherdir}/${srcdir} ${desthost}::dumps/${destdir}"`
+    running=`pgrep -u root -f -x "/usr/bin/rsync -rlptqgo $bwlimit ${otherdir}/${srcdir} ${desthost}::dumps/${destdir}"`
     if [ -z "$running" ]; then
-        /usr/bin/rsync -rlpt "$bwlimit" "${otherdir}/${srcdir}" "${desthost}::dumps/${destdir}"
+        /usr/bin/rsync -rlptqgo "$bwlimit" "${otherdir}/${srcdir}" "${desthost}::dumps/${destdir}"
     fi
 }
 
-# fixme these are wrong for sure, so I need to figure out the 'right' thing here
+
 running=`pgrep -u root -f   "python /usr/local/bin/wmfdumpsmirror.py --dest_hostname labstore1003.eqiad.wmnet"`
 if [ -z "$running" ]; then
-    python /usr/local/bin/wmfdumpsmirror.py --dest_hostname labstore1003.eqiad.wmnet --sourcedir /data/xmldatadumps/public --destdir dumps/public --filesperjob 50 --sizeperjob 5G --workercount 1 --rsynclist rsync-filelist-last-3-good.txt.rsync --rsyncargs -rlptq,--bwlimit=50000
+    python /usr/local/bin/wmfdumpsmirror.py --dest_hostname labstore1003.eqiad.wmnet --sourcedir /data/xmldatadumps/public --destdir dumps/public --filesperjob 50 --sizeperjob 5G --workercount 1 --rsynclist rsync-filelist-last-3-good.txt.rsync --rsyncargs -rlptqgo,--bwlimit=50000
 fi
 
 # fixme need to ensure ${desthost}::dumps/public/wikidatawiki/entities/ exists
