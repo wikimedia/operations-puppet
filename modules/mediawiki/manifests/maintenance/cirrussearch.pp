1445d4a1f390e2f37c7f12e528316da35d306369
Erik Bernhardson <ebernhardson@wikimedia.org>
2016-03-07 20:42:59 -0800

    Build cirrus completion indices daily
    We have made some optimizations to the index building process,
    bringing runtime down from over 12 hours to under 5 hours. This
    should be low enough we can afford to rebuild these indices on
    a daily basis.
    
    Change-Id: I5d3ed3aada0fe05ce36e11d65c27b36c19076bbd

--- modules/mediawiki/manifests/maintenance/cirrussearch.pp
+++ modules/mediawiki/manifests/maintenance/cirrussearch.pp
@@ -1,41 +1,22 @@
 class mediawiki::maintenance::cirrussearch( $ensure = present ) {
     require mediawiki::users
 
-    # Rebuilds the completion suggester indices once a week. This is scheduled
-    # to run during the low period of cirrus usage, which is generally 12am
+    # Rebuilds the completion suggester indices daily. This is scheduled
+    # to run during the low period of cirrus usage, which is generally 3am
     # to 7am UTC. The `|| true` statement ensures one failing wiki doesn't
-    # fail the entire job. This job, as of dec 2015, takes around 12 hours to
+    # fail the entire job. This job, as of mar 2015, takes around 5 hours
     # to run.
-    cron { 'cirrus_build_completion_indices_eqiad':
+    Cron {
         ensure  => $ensure,
         user    => $::mediawiki::users::web,
-        minute  => 20,
-        hour    => 0,
-        weekday => 2,
-        command => '/usr/local/bin/expanddblist all | xargs -I{} -P 4 sh -c \'/usr/local/bin/mwscript extensions/CirrusSearch/maintenance/updateSuggesterIndex.php --wiki={} --masterTimeout=10m --cluster=eqiad --optimize > /var/log/mediawiki/cirrus-suggest/{}.eqiad.log 2>&1 || true\''
+        minute  => 30,
+        hour    => 2,
     }
 
-    cron { 'cirrus_build_completion_indices_codfw':
-        ensure  => $ensure,
-        user    => $::mediawiki::users::web,
-        minute  => 20,
-        hour    => 0,
-        weekday => 2,
-        command => '/usr/local/bin/expanddblist all | xargs -I{} -P 4 sh -c \'/usr/local/bin/mwscript extensions/CirrusSearch/maintenance/updateSuggesterIndex.php --wiki={} --masterTimeout=10m --cluster=codfw --optimize > /var/log/mediawiki/cirrus-suggest/{}.codfw.log 2>&1 || true\''
-    }
-
-    file { '/var/log/mediawiki/cirrus-suggest':
-        ensure => ensure_directory($ensure),
-        owner  => $::mediawiki::users::web,
-        group  => $::mediawiki::users::web,
-        mode   => '0775'
-    }
-
-    file { '/etc/logrotate.d/cirrus-suggest':
-        ensure => $ensure,
-        source => 'puppet:///modules/mediawiki/maintenance/logrotate.d_cirrus-suggest',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
+    cron {
+        'cirrus_build_completion_indices_eqiad':
+            command => '/usr/local/bin/expanddblist all | xargs -I{} -P 4 sh -c \'/usr/local/bin/mwscript extensions/CirrusSearch/maintenance/updateSuggesterIndex.php --wiki={} --masterTimeout=10m --cluster=eqiad --optimize > /var/log/mediawiki/cirrus-suggest/{}.eqiad.log 2>&1 || true\'';
+        'cirrus_build_completion_indices_codfw':
+            command => '/usr/local/bin/expanddblist all | xargs -I{} -P 4 sh -c \'/usr/local/bin/mwscript extensions/CirrusSearch/maintenance/updateSuggesterIndex.php --wiki={} --masterTimeout=10m --cluster=codfw --optimize > /var/log/mediawiki/cirrus-suggest/{}.codfw.log 2>&1 || true\''
     }
 }
