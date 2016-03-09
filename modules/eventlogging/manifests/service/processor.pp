c931d33425311b1452f88f3656280d6298fc77cb
Andrew Otto <acotto@gmail.com>
2016-03-09 14:24:55 -0500

    Remove etcd usage from eventlogging processor
    This is no longer supported, clientIps are no longer collected so they don't need hashed
    
    Bug: T128407
    Change-Id: Ia6dae7c81c63dc54b61f35e97f1aab3ff0c63834

--- modules/eventlogging/manifests/service/processor.pp
+++ modules/eventlogging/manifests/service/processor.pp
@@ -44,13 +44,6 @@
 #   Specifies the socket ID the processor will use to identify itself
 #   when subscribing to input streams. Defaults to the resource title.
 #
-#
-# [*etcd_uri*]
-#   If given, a shared token in etcd will be used to hash client IPs
-#   for anonymization.  This should look like other EventLogging URIs,
-#   with python-etcd client parameters given via query params, e.g.
-#   https://hostA:1234,hostB:2345?cert=/my/cert.  Default: undef
-#
 # [*ensure*]
 #   If 'present' (the default), sets up the processor. If 'absent',
 #   destroys it.
@@ -69,7 +62,6 @@ define eventlogging::service::processor(
     $outputs,
     $output_invalid = undef,
     $sid            = $title,
-    $etcd_uri       = undef,
     $ensure         = present,
 ) {
     include ::eventlogging
