f6174ca0b365a7fb1c56473d8353ed99a5ddf22c
YuviPanda <yuvipanda@gmail.com>
2016-03-08 19:32:24 -0800

    tools: Add specific paging monitoring for PAWS too
    Bug: T129209
    Change-Id: I271c754a18a73653ba0bcd5af64ceff3df7a2f5f

--- modules/toollabs/manifests/monitoring/icinga.pp
+++ modules/toollabs/manifests/monitoring/icinga.pp
@@ -25,4 +25,11 @@ class toollabs::monitoring::icinga {
         critical      => false,
         host          => 'tools.wmflabs.org',
     }
+
+    monitoring::service { 'paws_main_page':
+        description   => 'PAWS Main page',
+        check_command => 'check_http_url!paws.wmflabs.org!/paws/hub/login',
+        host          => 'paws.wmflabs.org',
+        contact_group => 'team-paws',
+    }
 }
