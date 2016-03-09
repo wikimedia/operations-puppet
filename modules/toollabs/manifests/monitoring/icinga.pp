56fb59424340da70fb013cc6738463817148b682
YuviPanda <yuvipanda@gmail.com>
2016-03-08 20:13:28 -0800

    Revert "tools: Add paws as a separate host"
    Somehow this kills icinga.
    
    I hate computers.
    
    This reverts commit 617f626de400a53620d1b39d4a0d00f418512b77.
    This reverts commit f6174ca0b365a7fb1c56473d8353ed99a5ddf22c.
    
    Change-Id: Ibd13bf618277c05466f1fcca43033d9bde1c381d

--- modules/toollabs/manifests/monitoring/icinga.pp
+++ modules/toollabs/manifests/monitoring/icinga.pp
@@ -25,15 +25,4 @@ class toollabs::monitoring::icinga {
         critical      => false,
         host          => 'tools.wmflabs.org',
     }
-
-    @monitoring::host { 'paws.wmflabs.org':
-        host_fqdn => 'paws.wmflabs.org',
-    }
-
-    monitoring::service { 'paws_main_page':
-        description   => 'PAWS Main page',
-        check_command => 'check_http_url!paws.wmflabs.org!/paws/hub/login',
-        host          => 'paws.wmflabs.org',
-        contact_group => 'team-paws',
-    }
 }
