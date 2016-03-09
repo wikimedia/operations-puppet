4da059fa2731dd624f3a318dd545c777ad08d1ac
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-03-09 19:24:27 +0000

    prometheus: add node_exporter support
    the prometheus-node-exporter package provides node (machine, not the javascript
    framework) level metrics.
    
    Bug: T92813
    Change-Id: I6832dcf31b16d5d9dd876dcdd538269e4107a090

---
+++ modules/role/manifests/prometheus/node_exporter.pp
@@ -0,0 +1,14 @@
+# == Class: role::prometheus::node_exporter
+#
+# Role to provision prometheus machine metrics exporter. See also
+# https://github.com/prometheus/node_exporter and the module's documentation.
+
+class role::prometheus::node_exporter {
+    include ::prometheus::node_exporter
+
+    ferm::service { 'prometheus-node-exporter':
+        proto  => 'tcp',
+        port   => '9100',
+        srange => '$INTERNAL',
+    }
+}
