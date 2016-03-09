617f626de400a53620d1b39d4a0d00f418512b77
YuviPanda <yuvipanda@gmail.com>
2016-03-08 19:42:39 -0800

    tools: Add paws as a separate host
    Bug: T129209
    Change-Id: I97618a1f9369e57e232c243b66d6dc896717ce62

--- modules/toollabs/manifests/monitoring/icinga.pp
+++ modules/toollabs/manifests/monitoring/icinga.pp
@@ -26,6 +26,10 @@ class toollabs::monitoring::icinga {
         host          => 'tools.wmflabs.org',
     }
 
+    @monitoring::host { 'paws.wmflabs.org':
+        host_fqdn => 'paws.wmflabs.org',
+    }
+
     monitoring::service { 'paws_main_page':
         description   => 'PAWS Main page',
         check_command => 'check_http_url!paws.wmflabs.org!/paws/hub/login',
