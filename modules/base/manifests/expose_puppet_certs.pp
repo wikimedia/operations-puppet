378dd618e384b422962b7238ce98c05db4b85569
Guillaume Lederrey <glederrey@wikimedia.org>
2016-03-10 11:04:37 +0100

    Factorized code exposing Puppet SSL certs
    k8s module has been taken as a model for how to expose certificates
    
    The way certificates are exposed has been simplified:
    
    * single directory with all certs
    * CA cert is already available at '/etc/ssl/certs/Puppet_Internal_CA.pem'
      so we do not expose it again
    
    Refactoring of the k8s module has been split to another commit as it require
    more work than initially planned.
    
    Bug: T124444
    Change-Id: I0d6976675f48679cfec78120d4a09cfef73970bd

---
+++ modules/base/manifests/expose_puppet_certs.pp
@@ -0,0 +1,66 @@
+# == Define: base::expose_puppet_certs
+# Copies appropriate cert files from the puppet CA infrastructure
+# To be usable by the other applications
+# Note: Only copies public components, no private keys, unless specifically
+# asked.
+#
+# === Parameters
+#
+# [*title*]
+#   The directory in which the certificates will be exported. A subdirectory
+#   named "ssl" will be created.
+#
+# [*ensure*]
+#   If 'present', certificates will be copied, otherwise they will be removed.
+#
+# [*provide_private*]
+#   Should the private keys also be exposed?
+#
+# [*user/group*]
+#   User who will own the exposed SSL certificates.
+#
+# [*ssldir*]
+#   The source directory containing the original SSL certificates.
+#
+define base::expose_puppet_certs(
+    $ensure          = 'present',
+    $provide_private = false,
+    $user            = 'root',
+    $group           = 'root',
+    $ssldir          = '/var/lib/puppet/ssl',
+) {
+    validate_absolute_path($ssldir)
+
+    $target_basedir = $title
+    $puppet_cert_name = $::fqdn
+
+    File {
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { "${target_basedir}/ssl":
+        ensure  => ensure_directory($ensure),
+        mode    => '0555',
+    }
+
+    file { "${target_basedir}/ssl/cert.pem":
+        ensure  => $ensure,
+        mode    => '0444',
+        source  => "${ssldir}/certs/${puppet_cert_name}.pem",
+    }
+
+    $private_key_ensure = $ensure ? {
+        'present' => $provide_private ? {
+            true    => 'present',
+            default => 'absent',
+        },
+        default => 'absent',
+    }
+
+    file { "${target_basedir}/ssl/server.key":
+        ensure  => $private_key_ensure,
+        mode    => '0400',
+        source  => "${ssldir}/private_keys/${puppet_cert_name}.pem",
+    }
+}
