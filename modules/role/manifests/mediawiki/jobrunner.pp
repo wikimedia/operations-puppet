edc93845ccdc8109966fa4599c9d54e2fecdeabc
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-03-11 09:28:48 +0000

    jobrunner: monitor the HHVM server health
    We've been in the blind about the jobrunners not working for about a day
    because there is no current way to monitor the HHVM server that is being
    fed jobs from the jobqueue process. This patch introduces a simple check
    that will hit HHVM and warn us in case of problems.
    
    Change-Id: I6b99bba4eb3459f90bd27c0456308ebeedca3410

--- modules/role/manifests/mediawiki/jobrunner.pp
+++ modules/role/manifests/mediawiki/jobrunner.pp
@@ -3,5 +3,11 @@ class role::mediawiki::jobrunner {
 
     include ::role::mediawiki::common
     include ::mediawiki::jobrunner
-}
 
+    monitoring::service { 'jobrunner_http_hhvm':
+        description   => 'HHVM jobrunner',
+        check_command => 'check_http_jobrunner',
+        retries       => 2,
+    }
+
+}
