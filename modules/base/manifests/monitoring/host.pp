75ec13a2c3d5eb3fecad49745e23f3afa3a5b7a6
Emanuele Rocca <ema@wikimedia.org>
2016-03-11 10:40:00 +0100

    Disk space icinga check: pass --exclude-type=tracefs
    The check_disk plugin tries to enumerate all filesystems, failing on
    /sys/kernel/debug/tracing that cannot be accessed by the nagios
    user.
    
    Explicitly exclude tracefs.
    
    Ref: https://bugs.launchpad.net/ubuntu/+source/nagios-plugins/+bug/1516451
    Change-Id: I28f1c314370a34732b2ae75a0e7c4c0b9ceb97fe

--- modules/base/manifests/monitoring/host.pp
+++ modules/base/manifests/monitoring/host.pp
@@ -25,7 +25,7 @@ class base::monitoring::host(
     $contact_group = hiera('contactgroups', 'admins'),
     # the -A -i ... part is a gross hack to workaround Varnish partitions
     # that are purposefully at 99%. Better ideas are welcome.
-    $nrpe_check_disk_options = '-w 6% -c 3% -l -e -A -i "/srv/sd[a-b][1-3]"',
+    $nrpe_check_disk_options = '-w 6% -c 3% -l -e -A -i "/srv/sd[a-b][1-3]" --exclude-type=tracefs',
     $nrpe_check_disk_critical = false,
 ) {
     include base::puppet::params # In order to be able to use some variables
