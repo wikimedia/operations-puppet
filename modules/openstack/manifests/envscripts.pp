8fb10c70973675c48a420e1f09c6d63b5781db7b
andrewbogott <abogott@wikimedia.org>
2016-03-11 14:26:40 -0600

    Add designateenv.sh script and refactor novaenv.sh classes
    We're going to need special credentials for designate's handling
    of the wmflabs.org domain; this patch sets up credentials for
    that and adds a new script, 'designateenv.sh' to use with
    commandline tools.
    
    This patch also creates a new class to create both novaenv.sh
    and designateenv.sh, and adds those files to the horizon host.
    
    Change-Id: Iba8f6ad97807fafc36b4995750a208801b4019d9

---
+++ modules/openstack/manifests/envscripts.pp
@@ -0,0 +1,22 @@
+# Scripts to set up shell environment for openstack commandline
+class openstack::envscripts(
+    $novaconfig,
+    $designateconfig
+    ) {
+
+    # Handy script to set up environment for commandline nova magic
+    file { '/root/novaenv.sh':
+        content => template('openstack/novaenv.sh.erb'),
+        mode    => '0755',
+        owner   => 'root',
+        group   => 'root',
+    }
+
+    # Handy script to set up environment for commandline nova magic
+    file { '/root/wmflabsorg-domainadminenv.sh':
+        content => template('openstack/wmflabsorg-domainadminenv.sh.erb'),
+        mode    => '0755',
+        owner   => 'root',
+        group   => 'root',
+    }
+}
