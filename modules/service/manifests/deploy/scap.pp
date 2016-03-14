4284dd15bc1b9a8f8dd23e68b3f81324459c5c46
Tyler Cipriani <tcipriani@wikimedia.org>
2016-03-14 15:03:27 -0700

    Pass deploy user from service::node
    scap::target allows a user to be specified, pass that from service::node
    
    Change-Id: Ibc76c5be838172f3036bc3dc8d1fe0cafb1214df

--- modules/service/manifests/deploy/scap.pp
+++ modules/service/manifests/deploy/scap.pp
@@ -23,10 +23,12 @@ define service::deploy::scap(
     $public_key_file = 'puppet:///modules/service/servicedeploy_rsa.pub',
     $user            = 'deploy-service',
     $service_name    = undef,
+    $manage_user     = false,
 ) {
     scap::target { $title:
         public_key_source => $public_key_file,
         deploy_user       => $user,
         service_name      => $service_name,
+        manage_user       => $manage_user,
     }
 }
