575bdafd3e542f468b9ccfdc7c22d7dc963fe989
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-03-13 23:12:09 -0700

    [WIP] ifttt: Set up Wikimedia IFTTT channel service using puppet on labs
    Change-Id: I4460e1886e376a526df4e8babfce92d516a2b484

---
+++ modules/role/manifests/ifttt/staging.pp
@@ -0,0 +1,9 @@
+# == Class role::ifttt::staging
+# This is the staging specific ifttt role
+class role::ifttt::staging {
+    include ::ifttt::base
+
+    class { '::ifttt::web':
+        workers => 1,
+    }
+}
