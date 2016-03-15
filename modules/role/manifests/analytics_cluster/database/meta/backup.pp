621d565560fe8d01a00d04be411cbcc2c8db79f9
Andrew Otto <acotto@gmail.com>
2016-03-15 17:51:13 -0400

    Add mysql_wmf::mylvmbackup define, use this for backups of analytics-meta mysql instance
    mylvmbackup's prebackup hook allows us to define our own backup method.  This hook runs after
    the LVM snapshot has been taken and mounted at $mountdir.  We rsync $mountdir to $dest at this
    time.
    
    Bug: T127991
    Change-Id: I6b0c6e0b819c837b8dbffc97be0c08ee4f66a452

---
+++ modules/role/manifests/analytics_cluster/database/meta/backup.pp
@@ -0,0 +1,19 @@
+# == Class role::analytics_cluster::database::meta::backup
+# Uses mysql_wmf::mylvmbackup to take backups of the
+# analytics-meta mysql instance.
+#
+# == Parameters
+#
+# [*dest*]
+#   Rsync path for backup.  Default: /srv/backups/mysql/analytics-meta
+#
+class role::analytics_cluster::database::meta::backup(
+    $dest = '/srv/backup/mysql/analytics-meta'
+) {
+    # Take hourly backups of the analytics-meta instance
+    # and rsync those backups to $dest.
+    mysql_wmf::mylvmbackup { 'analytics-meta':
+        dest => $dest,
+        hour => 0,
+    }
+}
