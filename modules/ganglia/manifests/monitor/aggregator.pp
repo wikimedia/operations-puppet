2d519f68abadcc8d5b0296df59bb50043a9f9bf7
dzahn <dzahn@wikimedia.org>
2016-03-14 19:13:04 -0700

    ganglia: don't install old init scripts if systemd is used
    These scripts used to be what starts multiple instances of the
    ganglia-monitor-aggregator service on older systems with upstart.
    
    Since we are using systemd now/soon, we start each instance as separate
    service from a unit file template, using the proper systemd/puppet
    abstraction and this file is not needed and conflicts with puppet
    dependencies on the service name.
    
    Bug:T124197
    Change-Id: Ibdb63a4b900d6dea8723f4eb65b7a66f5503a0cc

--- modules/ganglia/manifests/monitor/aggregator.pp
+++ modules/ganglia/manifests/monitor/aggregator.pp
@@ -9,20 +9,27 @@ class ganglia::monitor::aggregator($sites) {
         mode   => '0555',
     }
 
-    file { '/etc/init/ganglia-monitor-aggregator.conf':
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => "puppet:///modules/${module_name}/upstart/ganglia-monitor-aggregator.conf",
-        before => Service['ganglia-monitor-aggregator'],
-    }
+    # These files used to start multiple instances of the aggregator service.
+    # Since using systemd they are not needed, now each instance is a separate
+    # service created from a unit file template.
+    # T124197 - see aggregator/instance.pp now
+    if $::initsystem == 'upstart' {
+        file { '/etc/init/ganglia-monitor-aggregator.conf':
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            source => "puppet:///modules/${module_name}/upstart/ganglia-monitor-aggregator.conf",
+            before => Service['ganglia-monitor-aggregator'],
+        }
 
-    file { '/etc/init/ganglia-monitor-aggregator-instance.conf':
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => "puppet:///modules/${module_name}/upstart/ganglia-monitor-aggregator-instance.conf",
-        before => Service['ganglia-monitor-aggregator'],
+        file { '/etc/init/ganglia-monitor-aggregator-instance.conf':
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            source => "puppet:///modules/${module_name}/upstart/ganglia-monitor-aggregator-instance.conf",
+            before => Service['ganglia-monitor-aggregator'],
+        }
+
     }
 
     define site_instances() {
