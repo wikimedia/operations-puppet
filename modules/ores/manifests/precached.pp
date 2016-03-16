52e28f698cac00b8a40ffd474418442c1229f5eb
sabya <sabyasachi.ruj@webyog.com>
2016-03-16 23:30:19 +0530

    Add support for running preached as a systemd unit
    Change-Id: I5a36af9f4cf545157728f7e57719be0fdd08d7d3

--- modules/ores/manifests/precached.pp
+++ modules/ores/manifests/precached.pp
@@ -1,12 +1,10 @@
 # = Class: ores::precached
 # Run a pre-caching daemon that listens to RCStream
 class ores::precached {
-    require ores::base
-
     $working_dir = $::ores::base::config_path
     $venv_path  = $::ores::base::venv_path
-
     base::service_unit { 'precached':
-        systemd => true,
+    template_name => 'precached',
+    systemd       => true,
     }
 }
