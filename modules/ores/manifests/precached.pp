0f2b812881c7216027b7738491b9a63bc1b4ce4d
sabya <sabyasachi.ruj@webyog.com>
2016-03-16 23:30:19 +0530

    ores: Add support for running precached as a systemd unit
    Precaching as a daemon is vital for ORES
    
    Change-Id: I6c16cec2d6f5d17e94d1e0b68aa670d5e3ac1399

--- modules/ores/manifests/precached.pp
+++ modules/ores/manifests/precached.pp
@@ -1,12 +1,21 @@
 # = Class: ores::precached
 # Run a pre-caching daemon that listens to RCStream
 class ores::precached {
-    require ores::base
+    include ores::web
 
     $working_dir = $::ores::base::config_path
     $venv_path  = $::ores::base::venv_path
+    $host = 'https://ores.wmflabs.org'
+    $config_dir = "${working_dir}/config"
 
     base::service_unit { 'precached':
-        systemd => true,
+        require        => Class['ores::web'],
+        template_name  => 'precached',
+        systemd        => true,
+        service_params => {
+            enable     => true,
+            hasstatus  => true,
+            hasrestart => true,
+        },
     }
 }
