39854dc2e4c53c922be977ce8d9224a2d15fd8b7
Andrew Otto <acotto@gmail.com>
2016-03-16 15:56:26 -0400

    Use direct hiera lookup instead of parameter in role::analytics_cluster::database::meta::backup
    Couldn't get parameter on role class to work properly
    
    Change-Id: I67822e6f40295dc9b9d22b95c9a2a5c1545483bc

--- modules/role/manifests/analytics_cluster/database/meta/backup.pp
+++ modules/role/manifests/analytics_cluster/database/meta/backup.pp
@@ -2,18 +2,21 @@
 # Uses mysql_wmf::mylvmbackup to take backups of the
 # analytics-meta mysql instance.
 #
-# == Parameters
+# == Hiera Variables
 #
-# [*dest*]
-#   Rsync path for backup.  Default: /srv/backups/mysql/analytics-meta/
+# [*analytics_cluster_meta_database_backup_rsync_dest*]
+#   Rsync path for backup.  Default: /srv/backups/mysql/analytics-meta
 #   You could include role::analytics_cluster::database::meta::backup_dest
 #   on some other node, and then set $dest here to
 #   other.node.org::backup/mysql/analytics-meta/ and backups would
 #   be rsynced elsewhere.
 #
-class role::analytics_cluster::database::meta::backup(
-    $dest = '/srv/backup/mysql/analytics-meta'
-) {
+class role::analytics_cluster::database::meta::backup {
+    $dest = hiera(
+        'analytics_cluster_meta_database_backup_rsync_dest',
+        '/srv/backups/mysql/analytics-meta'
+    )
+
     # Take hourly backups of the analytics-meta instance
     # and rsync those backups to $dest.
     mysql_wmf::mylvmbackup { 'analytics-meta':
