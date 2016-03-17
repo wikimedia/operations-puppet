393f93a32a8c7843b3f62d1a3c12ee5130d4f416
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-03-17 20:11:48 +0200

    ores: define slaveof as a parameter
    Bug: T124200
    Change-Id: I3b77167c105127fe9ccb410c337444423c6a7873

--- modules/ores/manifests/redis.pp
+++ modules/ores/manifests/redis.pp
@@ -1,6 +1,7 @@
 class ores::redis(
     $queue_maxmemory,
     $cache_maxmemory,
+    $slaveof=undef,
 ) {
     file { [
         '/srv/redis/queue',
@@ -12,6 +13,30 @@ class ores::redis(
         mode   => '0774'
     }
 
+    $premap = {
+        '6379' => {
+            dir       => '/srv/redis/queue',
+            maxmemory => $queue_maxmemory,
+        },
+        '6380' => {
+            dir       => '/srv/redis/cache',
+            maxmemory => $cache_maxmemory,
+        },
+    }
+    if $slaveof {
+        $slave_map = {
+            '6379' => {
+                slaveof   => "${slaveof} 6379",
+            },
+            '6380' => {
+                slaveof   => "${slaveof} 6380",
+            },
+        }
+        $map = deep_merge($slave_map, $premap)
+    } else {
+        $map = $premap
+    }
+
     # FIXME: Tune the individual redises better for their use case
     # queue: 6379, cache: 6380
     redis::instance { ['6379', '6380']:
@@ -19,16 +44,7 @@ class ores::redis(
             bind          => '0.0.0.0',
             tcp_keepalive => 60,
         },
-        map => {
-            '6379' => {
-                dir       => '/srv/redis/queue',
-                maxmemory => $queue_maxmemory,
-            },
-            '6380' => {
-                dir       => '/srv/redis/cache',
-                maxmemory => $cache_maxmemory,
-            },
-        },
+        map => $map,
         require => [
             File['/srv/redis/queue'],
             File['/srv/redis/cache'],
