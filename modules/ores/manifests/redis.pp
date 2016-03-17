36d13817068697832d8a9a5ebbee1bd55981944a
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-03-17 20:03:11 +0200

    ores: Collapse the redis configs into one stanza
    Since redis::instance allows us to use map to override configs per redis
    instance, take advantage of that to collapse the config in order to add
    support for replication in an easier fashion
    
    Bug: T124200
    Change-Id: Ic1b6ab146145873df7abf3d7a6fea89f5f146489

--- modules/ores/manifests/redis.pp
+++ modules/ores/manifests/redis.pp
@@ -13,25 +13,25 @@ class ores::redis(
     }
 
     # FIXME: Tune the individual redises better for their use case
-    # For the queue
-    redis::instance { '6379':
+    # queue: 6379, cache: 6380
+    redis::instance { ['6379', '6380']:
         settings => {
             bind          => '0.0.0.0',
-            dir           => '/srv/redis/queue',
-            maxmemory     => $queue_maxmemory,
             tcp_keepalive => 60,
         },
-        require  => File['/srv/redis/queue']
-    }
-
-    # For the cache
-    redis::instance { '6380':
-        settings => {
-            bind          => '0.0.0.0',
-            dir           => '/srv/redis/cache',
-            maxmemory     => $cache_maxmemory,
-            tcp_keepalive => 60,
+        map => {
+            '6379' => {
+                dir       => '/srv/redis/queue',
+                maxmemory => $queue_maxmemory,
+            },
+            '6380' => {
+                dir       => '/srv/redis/cache',
+                maxmemory => $cache_maxmemory,
+            },
         },
-        require  => File['/srv/redis/cache']
+        require => [
+            File['/srv/redis/queue'],
+            File['/srv/redis/cache'],
+        ],
     }
 }
