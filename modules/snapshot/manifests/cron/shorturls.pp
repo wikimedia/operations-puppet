9e5ddfa7d8bdc99d2ac53aff9242943805cf1cf5
Ariel T. Glenn <ariel@wikimedia.org>
2016-03-18 23:59:01 +0200

    dump url shorteners for wiki projects
    All wikis have the same data so just choose one arbitrarily
    
    This does a weekly dump and adds a link to the directory on the
    appropriate index.html page
    
    Bug: T116986
    Change-Id: I5c1c5878da1b72f9339950f3c78acc48670e3a40

---
+++ modules/snapshot/manifests/cron/shorturls.pp
@@ -0,0 +1,44 @@
+class snapshot::cron::shorturls(
+    $enable=true,
+    $user=undef,
+) {
+    include snapshot::dumps::dirs
+    include snapshot::cron::wikiqueryskip
+
+    if ($enable) {
+        $ensure = 'present'
+    }
+    else {
+        $ensure = 'absent'
+    }
+
+    $shorturlsdir = "${snapshot::dumps::dirs::datadir}/public/other/shorturls"
+
+    file { $shorturlsdir:
+        ensure => 'directory',
+        path   => $shorturlsdir,
+        mode   => '0755',
+        owner  => $user,
+        group  => root,
+    }
+
+    cron { 'shorturls-cleanup':
+        ensure      => $ensure,
+        environment => 'MAILTO=ops-dumps@wikimedia.org',
+        user        => $user,
+        command     => "find ${shorturlsdir}/ -maxdepth 1 -type d -mtime +90 -exec rm -rf {} \\;",
+        minute      => '0',
+        hour        => '8',
+    }
+
+    cron { 'shorturls':
+        ensure      => $ensure,
+        environment => 'MAILTO=ops-dumps@wikimedia.org',
+        user        => $user,
+        command     => "cd ${snapshot::dumps::dirs::scriptsdir}; python onallwikis.py --wiki metawiki --configfile confs/wikidump.conf.monitor  --filenameformat 'shorturls-{d}.txt' --outdir '${shorturlsdir}' --script extensions/UrlShortener/maintenance/dumpURLs.php '{DIR}'",
+        minute      => '5',
+        hour        => '8',
+        weekday     => '1',
+        require     => File[$shorturlsdir],
+    }
+}
