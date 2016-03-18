8739c767d13626e876033f0b4efb0c7cd4455300
dzahn <dzahn@wikimedia.org>
2016-03-17 19:40:26 -0700

    base/syslogs: fix "defined typed defined inside a class"
    Change-Id: I1690f29f7d86a92cc2aecbfad17bf7b5db57c07a

--- modules/base/manifests/syslogs.pp
+++ modules/base/manifests/syslogs.pp
@@ -4,13 +4,6 @@ class base::syslogs (
     $logfiles = [ 'syslog', 'messages' ],
     ) {
 
-    define syslogs::readable() {
-
-        file { "/var/log/${name}":
-            mode => '0644',
-        }
-    }
-
     if $readable == true {
         syslogs::readable { $logfiles: }
     }
