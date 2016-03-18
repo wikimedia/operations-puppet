6f47045e1cc83c735bb17fb109ef31115a1f3051
dzahn <dzahn@wikimedia.org>
2016-03-17 19:17:24 -0700

    ganglia: fix "defined type defined inside a class"
    Change-Id: I5e44cd0704ae8dc9514d3c08c5c40f5190bba9b8

---
+++ modules/ganglia/manifests/monitor/aggregator/site_instances.pp
@@ -0,0 +1,7 @@
+# Instantiate aggregators for all clusters for this site ($title)
+define ganglia::monitor::aggregator::site_instances() {
+    $cluster_list = suffix(keys($ganglia::configuration::clusters), "_${title}")
+    instance { $cluster_list:
+        monitored_site => $title
+    }
+}
