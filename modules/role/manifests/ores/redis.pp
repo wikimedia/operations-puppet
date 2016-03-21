36b6d9f2916a4b8decdefa7e0b438b4aeaf7bf79
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-03-21 21:22:56 +0200

    Add the role::ores::redis class
    Used to populate a replicated redis database pair
    
    Bug: T124200
    Change-Id: Ia9d710864e130a6f3b3cf2484897d4a0d5713dc1

---
+++ modules/role/manifests/ores/redis.pp
@@ -0,0 +1,11 @@
+# Setting up ORES Redis database in a replicated way in order to facilitate
+# failover if required
+class role::ores::redis {
+    # NOTE: We make this lookup explicit to avoid configuring by mistake a
+    # passwordless ores (::ores::redis password parameter defaults to undef)
+    $password = hiera('ores::redis::password')
+    # We rely on hiera for the slaveof parameter
+    class { '::ores::redis':
+        password        => $password,
+    }
+}
