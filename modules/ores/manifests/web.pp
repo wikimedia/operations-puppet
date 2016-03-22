341d5c81b1bf6e9d2550cd99a2cecbb4acd7ea39
halfak <aaron.halfaker@gmail.com>
2016-03-22 09:42:28 -0500

    Adds https only redirect to ores-web
    Change-Id: I36f6c905ee7d50417a053f859e66a19cef19bbd9

--- modules/ores/manifests/web.pp
+++ modules/ores/manifests/web.pp
@@ -11,13 +11,16 @@ class ores::web(
     uwsgi::app { 'ores-web':
         settings => {
             uwsgi => {
-                plugins     => 'python3',
+                plugins     => 'python3, router_redirect',
                 'wsgi-file' => "${ores::base::config_path}/ores_wsgi.py",
                 master      => true,
                 chdir       => $ores::base::config_path,
                 http-socket => '0.0.0.0:8080',
                 venv        => $ores::base::venv_path,
                 processes   => inline_template("<%= @processorcount.to_i * ${workers_per_core} %>"),
+                # lint:ignore:single_quote_string_with_variables
+                route-if    => 'equal:${HTTP_X_FORWARDED_PROTO};http redirect-permanent:https://${HTTP_HOST}${REQUEST_URI}',
+                # lint:endignore
             }
         }
     }
