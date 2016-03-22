a4857f162f0c4a0a76ded16811a4c7fc432564fe
YuviPanda <yuvipanda@gmail.com>
2016-03-22 14:29:16 -0700

    docker: Start the docker service by default
    Change-Id: I48cd4087fa78f83fa52f3ed2d9ea4461f2593d80

--- modules/k8s/manifests/docker.pp
+++ modules/k8s/manifests/docker.pp
@@ -2,7 +2,9 @@
 #
 # Sets up docker as used by kubernetes
 class k8s::docker {
-    class { '::docker::engine': }
+    class { '::docker::engine':
+        declare_service => false,
+    }
 
     base::service_unit { 'docker':
         systemd   => true,
