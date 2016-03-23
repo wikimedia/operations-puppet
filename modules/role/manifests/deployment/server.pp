0a1429b329541a0880879ac431fe7bf8ff5146df
Mukunda Modell <mmodell@wikimedia.org>
2016-03-23 16:14:17 -0500

    Hieraize keyholder::agent configuration
    Move keyholder::agent configuration to hiera to simplify keyholder
    configuration. Proliferation of *::deploy::source classes is messy
    and complicates the already too complex process of adding a new
    service group/key to the deployment masters.
    
    Change-Id: Id5002cc9449deb0223f27af67a30a86c6187bfd9

--- modules/role/manifests/deployment/server.pp
+++ modules/role/manifests/deployment/server.pp
@@ -7,11 +7,6 @@ class role::deployment::server(
     # Can't include this while scap is present on the deployment server:
     # include misc::deployment::scripts
     include role::deployment::mediawiki
-    # NOTE: keyholder_group for role::deployment::services
-    #       is overridden in hieradata/common/deployment/server.yaml
-    include role::deployment::services
-    include eventlogging::deployment::source
-    include phabricator::deployment::source
 
     class { 'deployment::deployment_server':
         deployment_group => $deployment_group,
