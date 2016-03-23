77dcdac70d3f94be33510ddfbfde5b9819a6b314
Guillaume Lederrey <glederrey@wikimedia.org>
2016-03-23 19:00:54 +0100

    elasticsarch: add Icinga check for SSL certificate
    This is based on
    https://gerrit.wikimedia.org/r/#/c/244610/3/manifests/role/nova.pp
    which implements a similar check.
    
    Bug: T130366
    Change-Id: I841b26396382411e660629775c99a6a9c031b011

--- modules/elasticsearch/manifests/https.pp
+++ modules/elasticsearch/manifests/https.pp
@@ -3,10 +3,22 @@
 # This class configures HTTPS for elasticsearch
 #
 # == Parameters:
-# - ensure: self explanatory
+# [*ensure*]
+#   self explanatory
+#
+# [*certificate_name*]
+#   name that will be checked in the SSL certificate. This should probably
+#   match the value configured in `base::puppet::dns_alt_names` if it is set,
+#   unless the service is accessed directly by FQDN.
+
 class elasticsearch::https (
-    $ensure = absent,
+    $ensure           = absent,
+    $certificate_name = $::fqdn,
 ){
+
+    if $ensure == present {
+        validate_string($certificate_name)
+    }
 
     class { [ 'nginx', 'nginx::ssl' ]:
         ensure => $ensure,
@@ -24,6 +36,12 @@ class elasticsearch::https (
     ::nginx::site { 'elasticsearch-ssl-termination':
         ensure  => $ensure,
         content => template('elasticsearch/nginx/es-ssl-termination.nginx.conf.erb'),
+    }
+
+    ::monitoring::service { 'elasticsearch-https':
+        ensure        => $ensure,
+        description   => 'Elasticsearch HTTPS',
+        check_command => "check_ssl_http!${certificate_name}",
     }
 
     ::ferm::service { 'elastic-https':
