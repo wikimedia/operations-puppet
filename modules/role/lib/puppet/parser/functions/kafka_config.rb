e669629f1be0f1284067bcc0c1e246820ce779bc
Marko Obrovac <mobrovac@wikimedia.org>
2016-03-24 00:00:48 +0100

    Kafka config: Add config functions
    Up until now, kafka configuration has been held in
    role::kafka::(main|analytics)::config, which are mostly copy/pastes of
    each other. This patch consolidates them by introducing two functions:
    kafka_config and kafka_cluster_name, the former using the latter. This
    allows us to elegantly get the config by simply using the cluster prefix
    we are interested in:
    
      $analytics_conf = kafka_config('analytics')
      $main_conf      = kafka_config('main')
    
    Bug: T130371
    Change-Id: I206b54d10e8188e7775d3f6621c8d4cc60e0d269

---
+++ modules/role/lib/puppet/parser/functions/kafka_config.rb
@@ -0,0 +1,40 @@
+# == Function: kakfa_config(string cluster_prefix[, string site])
+#
+# Reworks various variables to be in a format suitable for supplying them
+# to the kafka module classes. If the optional site argument is given, the
+# actual site where the function is evaluated is ignored
+#
+
+module Puppet::Parser::Functions
+  newfunction(:kafka_config, :type => :rvalue, :arity => -2) do |args|
+    clusters = function_hiera(['kafka_clusters', {}])
+    cluster_name = clusters[args[0]] || function_kafka_cluster_name(args)
+    zk_hosts = function_hiera(['zookeeper_hosts', [scope.lookupvar('fqdn')]])
+    zk_hosts = zk_hosts.keys.sort if zk_hosts.kind_of?(Hash)
+    cluster = clusters[cluster_name] || {
+      'brokers' => {
+        scope.lookupvar('fqdn').to_s => { 'id' => '1' }
+      }
+    }
+    brokers = cluster['brokers']
+    jmx_port = 9999
+    {
+      'name'      => cluster_name,
+      'brokers'   => {
+        'hash'     => brokers,
+        'array'    => brokers.keys,
+        # list of comma-separated host:port broker pairs
+        'string'   => brokers.map { |host, port| "#{host}:#{port || 9092}" }.sort.join(','),
+        # list of comma-separated host_9999 broker pairs used as graphite wildcards
+        'graphite' => brokers.keys.map { |b| "#{b.tr '.', '_'}_#{jmx_port}" }.join(','),
+        'size'     => brokers.keys.size
+      },
+      'jmx_port'  => jmx_port,
+      'zookeeper' => {
+        'hosts'  => zk_hosts,
+        'chroot' => "/kafka/#{cluster_name}",
+        'url'    => "#{zk_hosts.join(',')}/kafka/#{cluster_name}"
+      }
+    }
+  end
+end
