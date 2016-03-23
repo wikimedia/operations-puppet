5574e5590669eaaa75ab7276e3d485c64b82d4be
Marko Obrovac <mobrovac@wikimedia.org>
2016-03-24 00:00:48 +0100

    Kafka config: Add config functions
    Up until now, kafka configuration has been held in
    role::kafka::(main|analytics)::config, which are mostly copy/pastes of
    each other. This patch consolidates them by introducing two functions:
    kafka_config and kafka_cluster_name, the former using the latter. This
    allows us to elegantly get the config by simply using the cluster prefix
    we are interested in:
    
      $analytics_conf = kafka_config('analytics')
      $main_conf      = kafka_config('main')
    
    Bug: T130371
    Change-Id: I206b54d10e8188e7775d3f6621c8d4cc60e0d269

---
+++ modules/role/lib/puppet/parser/functions/kafka_cluster_name.rb
@@ -0,0 +1,24 @@
+# == Function: kakfa_cluster_name(string prefix[, string site])
+#
+# Determines the Kafka cluster name based on the supplied prefix.
+# NOTE: this function is WMF-specific and takes into account the fact that the
+# analytics cluster's name in production has historically been 'eqiad'
+#
+
+module Puppet::Parser::Functions
+  newfunction(:kafka_cluster_name, :type => :rvalue, :arity => -2) do |args|
+    name = function_hiera(['kafka_cluster_name'])
+    return name if name
+    prefix = args.pop
+    realm = scope.lookup_var('realm')
+    site = args.pop || scope.lookup_var('site')
+    labsp = scope.lookup_var('labsproject')
+    if realm == 'labs'
+      "#{prefix}-#{labsp}"
+    elsif prefix == 'analytics' and site == 'eqiad'
+      'eqiad'
+    else
+      "#{prefix}-#{site}"
+    end
+  end
+end
