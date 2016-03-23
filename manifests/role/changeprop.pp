3f2ef05b78b3769d7e9420e41a7c778430672e0d
Marko Obrovac <mobrovac@wikimedia.org>
2016-03-24 00:00:48 +0100

    Kafka config: Add config functions
    Up until now, kafka configuration has been held in
    role::kafka::(main|analytics)::config, which are mostly copy/pastes of
    each other. This patch consolidates them by introducing two functions:
    kafka_config and kafka_cluster_name, the former using the latter. This
    allows us to elegantly get the config by simply using the cluster prefix
    we are interested in:
    
      $analytics_conf = kafka_config('analytics')
      $main_conf      = kafka_config('main')
    
    Bug: T130371
    Change-Id: I206b54d10e8188e7775d3f6621c8d4cc60e0d269

--- manifests/role/changeprop.pp
+++ manifests/role/changeprop.pp
@@ -2,18 +2,14 @@
 # Role class for changeprop
 class role::changeprop {
 
-    # NOTE: Doing a hiera lookup on purpose instead of the implicit one in order
-    # to force the key. Also as a note to revisit this and
-    # role::kafka::main::config in order to tidy them up and not have to compose
-    # role classes which can turn out to be messy
-    $zk_uri = hiera('zookeeper_url')
+    $kafka_config = kafka_config('main')
 
     system::role { 'role::changeprop':
         description => 'propagates events from the EventBus',
     }
 
     class { '::changeprop':
-        zk_uri => $zk_uri,
+        zk_uri => $kafka_config['zookeper']['url'],
     }
 
 }
