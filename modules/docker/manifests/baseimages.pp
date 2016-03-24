633c849707f84e681bd2455f363c7e755bdec988
YuviPanda <yuvipanda@gmail.com>
2016-03-24 13:23:03 -0700

    docker: Add a class that facilitates building base images
    Change-Id: Ice8f3220268934d018b5297396f1c4b2327bb38a

---
+++ modules/docker/manifests/baseimages.pp
@@ -0,0 +1,40 @@
+# Classs: docker::baseimages
+#
+# Helper class that builds standard base images
+class docker::baseimages {
+
+    require ::docker::engine
+
+    require_package('python-bootstrap-vz')
+
+    file { '/srv/images':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    file { '/srv/images/base':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        require => File['/srv/images'],
+    }
+
+    $docker_registry = hiera('docker::registry')
+    file { '/srv/images/base/jessie.yaml':
+        content => template('docker/images/jessie.yaml.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0544',
+        require => File['/srv/images/base']
+    }
+
+    file { '/usr/local/bin/build-base-images':
+        content => template('docker/images/build-base-images.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0544',
+    }
+}
