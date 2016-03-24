647b60044dae7cdb5b4c179aab2ca55deded1891
Marko Obrovac <mobrovac@wikimedia.org>
2016-03-24 19:20:45 +0100

    Scap3: chown the target root dir if owned by root
    Switching from Trebuchet to Scap3 requires the repository on the target
    node to be owned by the user Scap3 is using for its operation. Alas, in
    the current state of affairs, the repository is still owned by root
    after switching the deployment method. This patch represents a temporary
    hack in scap::target that checks whether the target dir is owned by
    root, and if so recursively chowns it to $deploy_user. Once all of the
    repositories have been switched to actively use Scap3 for deployment,
    the hack can be removed, even though this code fragment will become a
    no-op at that point.
    
    Change-Id: I5e303a24ffa461758a203c7f6302b17573a04b19
    Note: the hack is extremely ugly, but Puppet ...

--- modules/scap/manifests/target.pp
+++ modules/scap/manifests/target.pp
@@ -90,6 +90,25 @@ define scap::target(
         }
     }
 
+    # XXX: Temporary work-around for switching services from Trebuchet to Scap3
+    # The Scap3 provider doesn't touch the target dir if it's already a git repo
+    # which means that even after switching the provider we end up with the
+    # wrong user (root) owning it. Therefore, as a temporary measure we chown
+    # the target dir's parent so that the subsequent invocation of deploy-local
+    # is able to create the needed dirs and symlinks
+    $chown_user = "${deploy_user}:${deploy_user}"
+    $name_array = split($package_name, '/')
+    $pkg_root = inline_template(
+        '<%= @name_array[0,@name_array.size - 1].join("/") %>'
+    )
+    $chown_target = "/srv/deployment/${pkg_root}"
+    exec { "chown ${chown_target} for ${deploy_user}":
+        command => "/bin/chown -R ${chown_user} ${chown_target}",
+        # perform the chown only if root is the effective owner
+        onlyif  => "/usr/bin/test -O /srv/deployment/${package_name}",
+        require => [User[$deploy_user], Group[$deploy_user]]
+    }
+
     # Allow deploy user user to sudo -u $user, and to sudo /usr/sbin/service
     # if $service_name is defined.
     #
