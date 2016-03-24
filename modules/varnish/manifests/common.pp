3aa384ca4fa81290b922bb563a53f7f3f525d9ad
Emanuele Rocca <ema@wikimedia.org>
2016-03-24 19:44:47 +0100

    apt_preferences for v3/v4 varnish packages
    We are going to use 'experimental' as the apt component for varnish 4
    and v4-compatible varnishkafka, and we want to make sure that on v3
    machines v4 packages are not pulled in because of an apt-upgrade.
    
    This patch installs two apt preferences fragments for varnish and
    varnishkafka giving higher priority to v3 versions on hosts where the
    varnish_version4 hiera attribute is not set.
    
    Bug: T122880
    Change-Id: Iadc942017a6e7f3b4f956e30298bc58b29bb80a9

--- modules/varnish/manifests/common.pp
+++ modules/varnish/manifests/common.pp
@@ -61,6 +61,20 @@ class varnish::common {
             require => File['/usr/local/lib/python2.7/dist-packages/varnishapi.py'],
         }
     } else {
+        # Prefer v3 varnish packages
+        apt::pin { 'varnish':
+            package  => 'varnish varnish-dbg libvarnishapi1 libvarnishapi-dev',
+            pin      => 'version 3.*',
+            priority => 1002,
+        }
+
+        # Prefer varnishkafka 1.0.7, compatible with varnish 3
+        apt::pin { 'varnishkafka':
+            package  => 'varnishkafka*',
+            pin      => 'version 1.0.7*',
+            priority => 1002,
+        }
+
         file { '/usr/local/lib/python2.7/dist-packages/varnishlog.py':
             source => 'puppet:///modules/varnish/varnishlog.py',
             owner  => 'root',
