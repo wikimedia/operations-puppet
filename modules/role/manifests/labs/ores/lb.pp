addf4f0ae36e07d48c95a4d4f2f082bb2a4a8604
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-03-26 13:54:48 +0200

    ores: Move the lb functionality into the role
    The loadbalancer functionality present in the ores module is never going
    to be used in production and was added specifically for labs. Move the
    LOC that implement that functionality along with the template into the
    labs role
    
    Change-Id: I4471463e6e839b9275783b8d7bfc005d60f1373c

--- modules/role/manifests/labs/ores/lb.pp
+++ modules/role/manifests/labs/ores/lb.pp
@@ -1,13 +1,25 @@
-class role::labs::ores::lb(
-    $realservers,
-) {
+# Simple nginx HTTP load balancer for ores
+class role::labs::ores::lb {
     labs_lvm::volume { 'srv':
         mountat => '/srv',
     }
+    $realservers = hiera('role::labs::ores::lb::realservers')
+    $cache = hiera('role::labs::ores::lb::cache', '')
 
-    class { '::ores::lb':
-        realservers => $realservers,
-        cache       => false,
-        require     => Labs_lvm::Volume['srv'],
+    if $cache {
+        file { [
+            '/srv/nginx/',
+            '/srv/nginx/cache',
+            '/srv/nginx/tmp',
+        ]:
+            ensure => directory,
+            owner  => 'www-data',
+            group  => 'www-data',
+            mode   => '0775',
+        }
+    }
+    nginx::site { 'ores-lb':
+        content => template('role/ores/lb.nginx.erb'),
+        require => Labs_lvm::Volume['srv'],
     }
 }
