0d2621859ef72ff9bfe5ee90d6e74df1bd248025
Marko Obrovac <mobrovac@wikimedia.org>
2016-03-26 18:00:57 +0100

    scap::target: Allow scap's user to restart all services on a node
    scap::target's set-up of sudo rules implicitly assumed that it would be
    deploying only one service per node, so only one sudo::user resource was
    being set. However, we have nodes where multiple services are
    collocated, such as the SCA and SCB clusters. This patch allows the same
    Scap deployment user to restart multiple services on the same machine.
    
    Bug: T130948
    Change-Id: Id13f35ec2cf4e32e4931ffdc9df69425d433aad8

--- modules/scap/manifests/target.pp
+++ modules/scap/manifests/target.pp
@@ -1,11 +1,12 @@
-# == Define scap::target
+# == Define: scap::target
 #
 # Sets up a scap3 target for a deployment repository.
 # This will include ths scap package and ferm fules,
 # ensure that the $deploy_user has proper sudo rules
 # and public key installed.
 #
-# == Params
+# === Parameters
+#
 # [*deploy_user*]
 #   user that will be used for deployments
 #
@@ -87,22 +88,36 @@ define scap::target(
     # Allow deploy user user to sudo -u $user, and to sudo /usr/sbin/service
     # if $service_name is defined.
     #
+    # Two sets of privileges are defined: one for scap to able to sudo -u $user,
+    # which should be defined only once per node, and another for restarting
+    # whichever services are being deployed.
+    #
     # NOTE: sudo -u $user is currently needed by scap3.
     # TODO: Remove this when it is no longer needed.
-    $privileges = $service_name ? {
-        undef   => [
-            "ALL=(${deploy_user}) NOPASSWD: ALL",
-        ],
-        default => [
-            "ALL=(${deploy_user}) NOPASSWD: ALL",
-            "ALL=(root) NOPASSWD: /usr/sbin/service ${service_name} *",
-        ],
-    }
 
     if !defined(Sudo::User["scap_${deploy_user}"]) {
         sudo::user { "scap_${deploy_user}":
             user       => $deploy_user,
-            privileges => concat($privileges, $sudo_rules),
+            privileges => ["ALL=(${deploy_user}) NOPASSWD: ALL"],
+        }
+    }
+
+    if $service_name {
+        $privileges = array_concat([
+            "ALL=(root) NOPASSWD: /usr/sbin/service ${service_name} *",
+        ], $sudo_rules)
+        $rule_name = "scap_${deploy_user}_${service_name}"
+    } else {
+        $privileges = $sudo_rules
+        $rule_name = inline_template(
+            '<%= "scap_#{@deploy_user}_#{@title.gsub("/", "_")}" %>'
+        )
+    }
+
+    if size($privileges) > 0 {
+        sudo::user { $rule_name:
+            user       => $deploy_user,
+            privileges => $privileges,
         }
     }
 
