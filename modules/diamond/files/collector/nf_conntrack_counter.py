e6c2e7b40e9c85c7a80baadb45f1c1e9d196eb63
elukey <ltoscano@wikimedia.org>
2016-03-29 20:02:46 +0200

    Add diamond nf_conntrack counter.
    Bug: T131150
    Change-Id: Ia3a24171dacd1801e53a1346bfdcdb0cc5d938e2

---
+++ modules/diamond/files/collector/nf_conntrack_counter.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+  Diamond collector that reports the count of connections
+  tracked by netfilter (nf_conntrack_count).
+"""
+
+import subprocess
+import diamond.collector
+
+
+class NfConntrackCountCollector(diamond.collector.Collector):
+    """Diamond collector that reports the count of the count
+    of connections tracked by netfilter."""
+
+    def get_default_config(self):
+        config = super(NfConntrackCountCollector, self).get_default_config()
+        config['path'] = 'network'
+        return config
+
+    def count_nf_connections_tracked(self):
+        # Output example 'net.ipv4.netfilter.ip_conntrack_count = 130698'
+        output = subprocess.check_output(['/sbin/sysctl', \
+            'net.ipv4.netfilter.ip_conntrack_count'])
+        return int(output.split("=")[1].strip())
+
+    def collect(self):
+        count = self.count_nf_connections_tracked()
+        self.publish('nf_conntrack_count', count)
