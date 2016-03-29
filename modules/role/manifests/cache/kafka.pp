fc90259e2046244ded06467954aca75a30266628
Emanuele Rocca <ema@wikimedia.org>
2016-03-29 11:14:49 +0200

    Move Varnishkafka APT pinning to role definition
    We want to make sure the pinning file for varnishkafka on Varnish 3
    instances is present before the package is installed.
    
    Bug: T122880
    Change-Id: Ia9a5a334dad27a9cd915139b477adcc3fe403546

--- modules/role/manifests/cache/kafka.pp
+++ modules/role/manifests/cache/kafka.pp
@@ -17,6 +17,27 @@ class role::cache::kafka {
     # sets up varnishkafka.
     Rsyslog::Conf['varnishkafka'] -> Varnishkafka::Instance <|  |>
 
+    # APT pinning for Varnish 3
+    if (hiera('varnish_version4', false)) {
+        apt::pin { 'varnishkafka':
+            ensure   => 'absent',
+            pin      => '',
+            priority => '',
+        }
+    } else {
+        # Prefer varnishkafka 1.0.7, compatible with varnish 3
+        apt::pin { 'varnishkafka':
+            package  => 'varnishkafka*',
+            pin      => 'version 1.0.7*',
+            priority => 1002,
+        }
+    }
+
+    # Make the Varnishkafka class depend on APT pinning. We want to ensure
+    # varnishkafka is not apt-get installed before the pinning file is
+    # created/removed.
+    Apt::Pin['varnishkafka'] -> Class['Varnishkafka']
+
     # Make sure varnishes are configured and started for the first time
     # before the instances as well, or they fail to start initially...
     Service <| tag == 'varnish_instance' |> -> Varnishkafka::Instance <| |>
--- modules/varnish/manifests/apt_preferences.pp
+++ modules/varnish/manifests/apt_preferences.pp
@@ -6,24 +6,11 @@ class varnish::apt_preferences {
             pin      => '',
             priority => '',
         }
-
-        apt::pin { 'varnishkafka':
-            ensure   => 'absent',
-            pin      => '',
-            priority => '',
-        }
     } else {
         # Prefer v3 varnish packages
         apt::pin { 'varnish':
             package  => 'varnish varnish-dbg varnish-doc libvarnishapi1 libvarnishapi-dev',
             pin      => 'version 3.*',
-            priority => 1002,
-        }
-
-        # Prefer varnishkafka 1.0.7, compatible with varnish 3
-        apt::pin { 'varnishkafka':
-            package  => 'varnishkafka*',
-            pin      => 'version 1.0.7*',
             priority => 1002,
         }
     }
