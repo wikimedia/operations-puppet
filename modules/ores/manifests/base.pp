aacace634dcc2c1105a928f135fcae688ead275a
amir <ladsgroup@gmail.com>
2016-03-30 10:02:21 +0000

    [WIP] Scap3 deployment configurations for ores
    Change-Id: Ia133277a17a5757f81f47fed2146e406f443c135

--- modules/ores/manifests/base.pp
+++ modules/ores/manifests/base.pp
@@ -32,6 +32,17 @@ class ores::base(
                     'myspell-pt',
                     'myspell-uk')
 
+    # Deployment configurations
+    include scap
+    scap::target { 'ores/deploy':
+        deploy_user => 'deploy-service',
+        sudo_rules  => [
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-ores-web *',
+            'ALL=(root) NOPASSWD: /usr/sbin/service celery-ores-worker *',
+            'ALL=(root) NOPASSWD: /usr/sbin/service flower-ores *',
+        ],
+    }
+
     file { '/srv/ores':
         ensure => directory,
         owner  => 'www-data',
