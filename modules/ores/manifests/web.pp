baae83d476422b56aa8ee9c22bd3a4c6d4c4bca4
amir <ladsgroup@gmail.com>
2016-03-30 10:02:21 +0000

    ores: Scap3 deployment configurations
    Change-Id: Ia133277a17a5757f81f47fed2146e406f443c135

--- modules/ores/manifests/web.pp
+++ modules/ores/manifests/web.pp
@@ -2,6 +2,7 @@
 # Sets up a uwsgi based web server for ORES running python3
 class ores::web(
     $workers_per_core = 4,
+    $redis_host = '127.0.0.1',
 ) {
     require ores::base
 
@@ -16,5 +17,34 @@ class ores::web(
             logformat   => '[pid: %(pid)] %(addr) (%(user)) {%(vars) vars in %(pktsize) bytes} [%(ctime)] %(method) %(uri) => generated %(rsize) bytes in %(msecs) msecs (%(proto) %(status)) %(headers) headers in %(hsize) bytes (%(switches) switches on core %(core)) user agent "%(uagent)"',
             processes   => $processes,
         }
+    }
+
+    # For now puppet ships the config until we migrate it to scap3 as well
+    ores::config { 'main':
+        # lint:ignore:arrow_alignment
+        config => {
+            'ores' => {
+                'data_paths' => {
+                    'nltk' => "${::ores::base::config_path}/submodules/wheels/nltk/",
+                }
+            },
+            'score_caches' => {
+                'ores_redis' => {
+                    'host' => $redis_host,
+                    'port' => '6380',
+                }
+            },
+            'score_processors' => {
+                'ores_celery' => {
+                    'BROKER_URL'            => "redis://${redis_host}:6379",
+                    'CELERY_RESULT_BACKEND' => "redis://${redis_host}:6379",
+                }
+            },
+        },
+        priority => '99',
+        mode     => '0444',
+        owner    => 'deploy-service',
+        group    => 'deploy-service',
+        # lint:endignore
     }
 }
