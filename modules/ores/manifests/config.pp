4e8ad10f6d706062551be3cbe30475065f1090f0
amir <ladsgroup@gmail.com>
2016-03-30 10:02:21 +0000

    ores: Scap3 deployment configurations
    Change-Id: Ia133277a17a5757f81f47fed2146e406f443c135

--- modules/ores/manifests/config.pp
+++ modules/ores/manifests/config.pp
@@ -1,13 +1,16 @@
 define ores::config(
     $priority,
     $config,
+    $owner='www-data',
+    $group='www-data',
+    $mode='0660',
 ) {
     require ::ores::base
 
     file { "${::ores::base::config_path}/config/${priority}-${title}.yaml":
         content => ordered_yaml($config),
-        owner   => 'www-data',
-        group   => 'www-data',
-        mode    => '0660',
+        owner   => $owner,
+        group   => $group,
+        mode    => $mode,
     }
 }
