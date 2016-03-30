9ad55f52a24dd947b05a2a831b3d1cdbaf0c4a73
amir <ladsgroup@gmail.com>
2016-03-30 10:02:21 +0000

    ores: Scap3 deployment configurations
    Change-Id: Ia133277a17a5757f81f47fed2146e406f443c135

---
+++ modules/ores/manifests/scapdeploy.pp
@@ -0,0 +1,77 @@
+class ores::scapdeploy(
+    $deploy_user = 'deploy-service',
+    $deploy_group = 'deploy-service',
+    $redis_host = '127.0.0.1',
+) {
+    require ores::base
+
+    # Deployment configurations
+    include scap
+    scap::target { 'ores/deploy':
+        deploy_user => $deploy_user,
+        sudo_rules  => [
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-ores-web *',
+            'ALL=(root) NOPASSWD: /usr/sbin/service celery-ores-worker *',
+        ],
+        require     => File['/srv/deployment/ores/deploy-cache'],
+        manage_user => false,
+    }
+
+    group { $deploy_group:
+        ensure => present,
+        system => true,
+        before => User[$deploy_user],
+    }
+
+    user { $deploy_user:
+        ensure     => present,
+        shell      => '/bin/bash',
+        home       => '/var/lib/scap',
+        system     => true,
+        managehome => true,
+    }
+
+    file { '/srv/deployment/ores':
+        ensure  => directory,
+        owner   => $deploy_user,
+        group   => $deploy_group,
+        mode    => '0775',
+        recurse => true,
+        require => User[$deploy_user],
+    }
+
+    file { '/srv/deployment/ores/deploy-cache':
+        ensure  => directory,
+        owner   => $deploy_user,
+        group   => $deploy_group,
+        mode    => '0775',
+        require => File['/srv/deployment/ores'],
+    }
+
+    ores::config { 'main':
+        config   => {
+            'ores'             => {
+                'data_paths' => {
+                    'nltk' => "${::ores::base::config_path}/submodules/wheels/nltk/",
+                }
+            },
+            'score_caches'     => {
+                'ores_redis' => {
+                    'host' => $redis_host,
+                    'port' => '6380',
+                }
+            },
+            'score_processors' => {
+                'ores_celery' => {
+                    'BROKER_URL'            => "redis://${redis_host}:6379",
+                    'CELERY_RESULT_BACKEND' => "redis://${redis_host}:6379",
+                }
+            },
+        },
+        priority => '99',
+        mode     => '0644',
+        owner    => 'deploy-service',
+        group    => 'deploy-service',
+        require  => Package['ores/deploy'],
+    }
+}
