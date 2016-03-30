0d5993e5da75a1d3202023aefa547b14c3d060ea
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-03-30 12:09:12 +0200

    Fix ferm rules for postgres and redis access on maps master
    These are specified as host names in Hiera, not IP addresses.
    
    Change-Id: I728eb20e891707f4f0ae6a1373b9459edd29d0b7

--- modules/role/manifests/maps/master.pp
+++ modules/role/manifests/maps/master.pp
@@ -64,14 +64,14 @@ class role::maps::master {
     ferm::service { 'tilerator_redis':
         proto  => 'tcp',
         port   => '6379',
-        srange => "(${maps_hosts_ferm})",
+        srange => "@resolve((${maps_hosts_ferm}))",
     }
 
     # Access to postgres master from postgres slaves
     ferm::service { 'postgres_maps':
         proto  => 'tcp',
         port   => '5432',
-        srange => "(${maps_hosts_ferm})",
+        srange => "@resolve((${maps_hosts_ferm}))",
     }
 }
 
