8a26644a6c06b7746dd511613da8198eac87e461
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-03-30 12:54:18 +0300

    maps: Fix mess introduced in I2a44ee9
    In I2a44ee9, server.pp was just moved instead of properly modified,
    while contained classes were also split up into respective files as per
    puppet autoload layout guidelines. Since server.pp, introduced in that
    commit also contained maps::master and maps::slave that lead to
    inconsistent puppet lookups leading to very bizarre behavior. To further
    make things weird, the definitions of role::maps::master in server.pp
    and master.pp diverged causing even more confusion. This commit removes
    role::maps::master and role::maps::slave from server.pp and merges all
    the changes in the between in master.pp. In addition some minor grouping
    of resources to more clearly show their relationships is done
    
    Change-Id: I04bc50ff069822fa89d272273b25ffcc2058ed07

--- modules/role/manifests/maps/master.pp
+++ modules/role/manifests/maps/master.pp
@@ -3,13 +3,28 @@ class role::maps::master {
     include ::postgresql::master
     include ::osm
     include ::osm::import_waterlines
-    include ::redis
 
     system::role { 'role::maps::master':
         ensure      => 'present',
         description => 'Maps Postgres master',
     }
+    redis::instance { 6379: }
 
+    # DB passwords
+    $kartotherian_pass = hiera('maps::postgresql_kartotherian_pass')
+    $tilerator_pass = hiera('maps::postgresql_tilerator_pass')
+    $tileratorui_pass = hiera('maps::postgresql_tileratorui_pass')
+    $osmimporter_pass = hiera('maps::postgresql_osmimporter_pass')
+    $osmupdater_pass = hiera('maps::postgresql_osmupdater_pass')
+    # Grants
+    file { '/usr/local/bin/maps-grants.sql':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0400',
+        content => template('role/maps/grants.sql.erb'),
+    }
+
+    # Db setup
     postgresql::spatialdb { 'gis':
         require => Class['::postgresql::postgis'],
     }
@@ -20,18 +35,17 @@ class role::maps::master {
         create_resources(postgresql::user, $postgres_slaves)
     }
 
-    # Grants
-    $kartotherian_pass = hiera('maps::postgresql_kartotherian_pass')
-    $tilerator_pass = hiera('maps::postgresql_tilerator_pass')
-    $tileratorui_pass = hiera('maps::postgresql_tileratorui_pass')
-    $osmimporter_pass = hiera('maps::postgresql_osmimporter_pass')
-    $osmupdater_pass = hiera('maps::postgresql_osmupdater_pass')
-    file { '/usr/local/bin/maps-grants.sql':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0400',
-        content => template('role/maps/grants.sql.erb'),
+    osm::planet_sync { 'gis':
+        ensure        => present,
+        flat_nodes    => true,
+        expire_levels => '16',
+        num_threads   => 4,
+        pg_password   => $osmupdater_pass,
+        period        => 'day', # Remove thse as soon as we get down to minute
+        hour          => '1',
+        minute        => '27',
     }
+
     # Cassandra grants
     $cassandra_kartotherian_pass = hiera('maps::cassandra_kartotherian_pass')
     $cassandra_tilerator_pass = hiera('maps::cassandra_tilerator_pass')
@@ -41,6 +55,23 @@ class role::maps::master {
         group   => 'root',
         mode    => '0400',
         content => template('role/maps/grants.cql.erb'),
+    }
+
+    # Ferm rules
+    $maps_hosts = hiera('maps::hosts')
+    $maps_hosts_ferm = join($maps_hosts, ' ')
+
+    ferm::service { 'tilerator_redis':
+        proto  => 'tcp',
+        port   => '6379',
+        srange => "(${maps_hosts_ferm})",
+    }
+
+    # Access to postgres master from postgres slaves
+    ferm::service { 'postgres_maps':
+        proto  => 'tcp',
+        port   => '5432',
+        srange => "(${maps_hosts_ferm})",
     }
 }
 
