731257f3cd71cea6ef7ebbc60f38bde5c86ed896
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-03-30 15:57:16 +0100

    prometheus: add server support
    a prometheus::server define is provided that will install and setup a
    prometheus server instance.
    
    additionally, an example of labs deployment is provided with
    role::prometheus::labs_project, currently running at
    https://prometheus-staging.wmflabs.org/labs. The provided script will
    autodiscover and scrape/poll metrics from all labs instances running
    prometheus's node_exporter in the same project. (see also I6832dcf31)
    
    Bug: T126785
    Change-Id: Ia2a8f204ea0e3ac865a6ab8bd7b4af6c7915bcef

---
+++ modules/prometheus/manifests/scripts.pp
@@ -0,0 +1,18 @@
+# = Class: prometheus::scripts
+#
+# This class includes accessory prometheus scripts, usually to generate targets
+# for prometheus to pick up.
+
+class prometheus::scripts {
+    require_package('python3-yaml')
+
+    # output all nova instances for the current labs project as prometheus
+    # 'targets'
+    file { '/usr/local/bin/prometheus-labs-targets':
+        ensure => file,
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/prometheus/usr/local/bin/prometheus-labs-targets',
+    }
+}
---
+++ modules/role/manifests/prometheus/labs_project.pp
@@ -0,0 +1,25 @@
+# == Class: role::prometheus::labs_project
+#
+# This class provides a prometheus server to do node (host) monitoring for all
+# instances of the labs projects it is running in.
+# Instance autodiscovery is accomplished by querying wikitech HTTP API for
+# instances list and writing a list of <instance>:9100 'targets' for prometheus
+# to pick up. See also prometheus::node_exporter.
+
+class role::prometheus::labs_project {
+  prometheus::server { 'labs':
+    listen_address => '127.0.0.1:9901'
+  }
+
+  $targets_file = '/srv/prometheus/labs/targets/node_project.yml'
+
+  include ::prometheus::scripts
+
+  cron { 'prometheus_labs_project_targets':
+    ensure  => present,
+    command => "/usr/local/bin/prometheus-labs-targets > ${targets_file}.$$ && mv ${targets_file}.$$ ${targets_file}",
+    minute  => '*/10',
+    hour    => '*',
+    user    => 'prometheus',
+  }
+}
