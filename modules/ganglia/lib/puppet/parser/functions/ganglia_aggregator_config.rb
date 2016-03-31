ae60e9484681be22a9ea12e6da9ccac7311559c7
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-03-31 11:02:36 +0200

    ganglia_aggregator_config: fixup for I6523610b5, linting
    The removal of |args| from the code in I6523610b5 made puppet > 3.4
    fail; also fix some ruby linting
    
    Change-Id: Iecbe0e58885ded786a2c5ad2a1ab6cca0f2c2645

--- modules/ganglia/lib/puppet/parser/functions/ganglia_aggregator_config.rb
+++ modules/ganglia/lib/puppet/parser/functions/ganglia_aggregator_config.rb
@@ -4,23 +4,24 @@
 def calc_url(aggregator, ip_octet)
   url, port = aggregator.split(':')
   port = port.to_i + ip_octet.to_i
-  return sprintf("%s:%d", url, port)
+  format('%s:%d', url, port)
 end
 
 
 module Puppet::Parser::Functions
-  newfunction(:ganglia_aggregator_config, :type => :rvalue) do
+  newfunction(:ganglia_aggregator_config, :type => :rvalue) do |_|
     config = {}
     site_wide_aggregators = {}
     clusters = function_hiera(['ganglia_clusters'])
     clusters.each do |_cluster, data|
       data['sites'].each do |site, aggregators|
-        name = sprintf("%s %s", data['name'], site)
-        if not aggregators.empty?
+        name = format('%s %s', data['name'], site)
+        if !aggregators.empty?
           aggregator = aggregators.join(' ')
         else
           unless site_wide_aggregators.include? site
-            site_wide_aggregators[site] = function_hiera(['ganglia_aggregators', nil, site])
+            site_wide_aggregators[site] = function_hiera(
+              ['ganglia_aggregators', nil, site])
           end
           # Compute the port to use
           aggregator = calc_url(site_wide_aggregators[site], data['id'])
@@ -28,6 +29,6 @@ module Puppet::Parser::Functions
         config[name] = aggregator
       end
     end
-    return config
+    config
   end
 end
