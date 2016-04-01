37ef80eb9392bb8b8c2a5a6df2395652c99365ee
dzahn <dzahn@wikimedia.org>
2016-04-01 13:52:37 -0700

    ganglia: switch esams aggregator to bast3001
    Switch the ganglia aggregator for esams from hooft
    to newly installed bast3001.
    
    Also use "ipresolve" now instead of IP like it's done for the
    other upgraded hosts.
    
    Bug:T123712
    Change-Id: I599b83354d22e354d4751a161a90536aa90ff4b7

--- modules/ganglia/manifests/configuration.pp
+++ modules/ganglia/manifests/configuration.pp
@@ -11,7 +11,7 @@ class ganglia::configuration {
     $gmetad_hosts = [ '208.80.154.53', '208.80.154.150', '208.80.154.14' ]
     $aggregator_hosts = {
         'eqiad' => [ ipresolve('carbon.wikimedia.org') ],
-        'esams' => [ '91.198.174.113' ],
+        'esams' => [ ipresolve('bast3001.wikimedia.org') ],
         'codfw' => [ '208.80.153.4' ],
         'ulsfo' => [ ipresolve('bast4001.wikimedia.org') ],
     }
