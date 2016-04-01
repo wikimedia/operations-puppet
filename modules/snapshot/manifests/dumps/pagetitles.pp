dbcfc838f97084c0c3d1703018729ee126b51c25
Ariel T. Glenn <ariel@wikimedia.org>
2016-04-01 12:09:26 +0300

    dumps: make dir var references easier on the eyes in the manifests
    Change-Id: Ib617e602512e4d8749e168525331e22f3b5c4167

--- modules/snapshot/manifests/dumps/pagetitles.pp
+++ modules/snapshot/manifests/dumps/pagetitles.pp
@@ -12,17 +12,19 @@ class snapshot::dumps::pagetitles(
         $ensure = 'absent'
     }
 
-    file { "${snapshot::dumps::dirs::datadir}/public/other/pagetitles":
+    $otherdir = "${snapshot::dumps::dirs::datadir}/public/other/pagetitles"
+
+    file { $otherdir:
         ensure => 'directory',
-        path   => "${snapshot::dumps::dirs::datadir}/public/other/pagetitles",
+        path   => $otherdir,
         mode   => '0755',
         owner  => $user,
         group  => root,
     }
 
-    file { "${snapshot::dumps::dirs::datadir}/public/other/mediatitles":
+    file { "${otherdir}/mediatitles":
         ensure => 'directory',
-        path   => "${snapshot::dumps::dirs::datadir}/public/other/mediatitles",
+        path   => "${otherdir}/mediatitles",
         mode   => '0755',
         owner  => $user,
         group  => root,
@@ -32,29 +34,31 @@ class snapshot::dumps::pagetitles(
         ensure      => $ensure,
         environment => 'MAILTO=ops-dumps@wikimedia.org',
         user        => $user,
-        command     => "find ${snapshot::dumps::dirs::datadir}/public/other/pagetitles/ -maxdepth 1 -type d -mtime +90 -exec rm -rf {} \\; ; find ${snapshot::dumps::dirs::datadir}/public/other/mediatitles/ -maxdepth 1 -type d -mtime +90 -exec rm -rf {} \\;",
+        command     => "find ${otherdir}/pagetitles/ -maxdepth 1 -type d -mtime +90 -exec rm -rf {} \\; ; find ${otherdir}/mediatitles/ -maxdepth 1 -type d -mtime +90 -exec rm -rf {} \\;",
         minute      => '0',
         hour        => '8',
     }
+
+    $scriptsdir = $snapshot::dumps::dirs::scriptsdir
 
     cron { 'pagetitles-ns0':
         ensure      => $ensure,
         environment => 'MAILTO=ops-dumps@wikimedia.org',
         user        => $user,
-        command     => "cd ${snapshot::dumps::dirs::scriptsdir}; python onallwikis.py --configfile confs/wikidump.conf.monitor  --filenameformat '{w}-{d}-all-titles-in-ns-0.gz' --outdir '${snapshot::dumps::dirs::datadir}/public/other/pagetitles/{d}' --query \"'select page_title from page where page_namespace=0;'\"",
+        command     => "cd ${scriptsdir}; python onallwikis.py --configfile confs/wikidump.conf.monitor  --filenameformat '{w}-{d}-all-titles-in-ns-0.gz' --outdir '${otherdir}/pagetitles/{d}' --query \"'select page_title from page where page_namespace=0;'\"",
         minute      => '10',
         hour        => '8',
-        require     => File["${snapshot::dumps::dirs::datadir}/public/other/pagetitles"],
+        require     => File["${otherdir}/pagetitles"],
     }
 
     cron { 'pagetitles-ns6':
         ensure      => $ensure,
         environment => 'MAILTO=ops-dumps@wikimedia.org',
         user        => $user,
-        command     => "cd ${snapshot::dumps::dirs::scriptsdir}; python onallwikis.py --configfile confs/wikidump.conf.monitor  --filenameformat '{w}-{d}-all-media-titles.gz' --outdir '${snapshot::dumps::dirs::datadir}/public/other/mediatitles/{d}' --query \"'select page_title from page where page_namespace=6;'\"",
+        command     => "cd ${scriptsdir}; python onallwikis.py --configfile confs/wikidump.conf.monitor  --filenameformat '{w}-{d}-all-media-titles.gz' --outdir '${otherdir}/mediatitles/{d}' --query \"'select page_title from page where page_namespace=6;'\"",
         minute      => '50',
         hour        => '8',
-        require     => File["${snapshot::dumps::dirs::datadir}/public/other/mediatitles"],
+        require     => File["${otherdir}/mediatitles"],
     }
 
 }
