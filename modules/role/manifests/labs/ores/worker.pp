482fab0c870dbc235abd97c42e972a40fe023441
amir <ladsgroup@gmail.com>
2016-04-02 14:05:48 +0000

    Puppetize ORES redis configs
    See https://github.com/wiki-ai/ores-wikimedia-config/pull/52
    
    Change-Id: I2f61978cfc3a998b2e36e1929d119f2fe1c83b7d

--- modules/role/manifests/labs/ores/worker.pp
+++ modules/role/manifests/labs/ores/worker.pp
@@ -1,4 +1,27 @@
 class role::labs::ores::worker {
     include ::ores::worker
     include ::role::labs::ores::redisproxy
+
+    ores::config { "main":
+        config   => {
+            "ores"             => {
+                "data_paths" => {
+                    "nltk" => "${ores::base::config_path}/submodules/wheels/nltk/",
+                }
+            },
+            "score_caches"     => {
+                "ores_redis" => {
+                    "host" => ores::redisproxy::server,
+                    "port" => "6380",
+                }
+            },
+            "score_processors" => {
+                "ores_celery" => {
+                    "BROKER_URL"            => "redis://${ores::redisproxy::server}:6379",
+                    "CELERY_RESULT_BACKEND" => "redis://${ores::redisproxy::server}:6379",
+                }
+            },
+        },
+        priority => "99",
+    }
 }
