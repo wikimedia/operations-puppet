105eaf4ca85cb2a2d7d082e0db1ebb9924017b70
Ariel T. Glenn <ariel@wikimedia.org>
2016-04-04 16:47:11 +0300

    fix typo in name of private key file for dumps dpeloyment
    Change-Id: Iea676da2829b74207d655e0a5496def1eb2ff153

--- modules/snapshot/manifests/deployment/source.pp
+++ modules/snapshot/manifests/deployment/source.pp
@@ -4,7 +4,7 @@ class snapshot::deployment::source {
     require ::keyholder
     require ::keyholder::monitoring
 
-    keyholder::agent { 'dumpsdeployment':
+    keyholder::agent { 'dumpsdeploy':
         trusted_group   => 'ops',
         key_fingerprint => '86:c9:17:ab:b7:00:79:b5:8a:c5:b5:ee:29:24:c9:2f',
     }
