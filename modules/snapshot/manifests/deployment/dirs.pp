2e01578bdda74e276eadf8a1f87a7eee38675402
Ariel T. Glenn <ariel@wikimedia.org>
2016-04-04 12:59:55 +0300

    add scap config file for dumps
    prep for scap3 deployment of dumps scripts
    
    Change-Id: I55447d08563d7e98bf2f3d29c3ff58351d5b6c9f

---
+++ modules/snapshot/manifests/deployment/dirs.pp
@@ -0,0 +1,27 @@
+class snapshot::deployment::common {
+{
+    $repodir = "/srv/deployment/dumps/dumps"
+
+    file { "${repodir}/scap":
+      ensure => 'directory',
+      path   => "${subdir}/scap",
+      mode   => '0755',
+      owner  => root,
+      group  => root,
+    }
+
+    file { "${repodir}/scap/scap.cfg":
+      mode   => '0644',
+      owner  => root,
+      group  => root,
+      source => 'puppet:///modules/snapshot/deployment/scap.cfg'
+    }
+
+    $target_list = 'snapshot1005.eqiad.wmnet\n'
+    file { "${repodir}/scap/dumps_targets":
+      mode    => '0644',
+      owner   => root,
+      group   => root,
+      content => ${target_list},
+    }
+}
