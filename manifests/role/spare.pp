39e2d798068ec53d48bf8385b0b8c28e09d0296b
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-04-05 13:08:33 +0200

    Clarify role description
    Change-Id: Ic2ae514e00aea2d554c6d8f96f1408b2ed985c4b

--- manifests/role/spare.pp
+++ manifests/role/spare.pp
@@ -1,3 +1,10 @@
+# This is a role for systems which are in the process of being reclaimed
+# or decommissioned. IOW. the host once had a feature role assigned, but
+# currently no has it. If such hosts are simply reset to "include standard"
+# in site.pp, they are no longer matched by role-based grains for debdeploy.
+#
+# This role is entirely transient. Once a system has been reclaimed to spares
+# or decomissioned, this role is removed from site.pp along with the host entry.
 class role::spare {
     include standard
     include base::firewall
