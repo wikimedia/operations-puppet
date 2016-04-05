f111e2fff3d3efed09ff206e7378d07bf96bd9ac
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-04-05 11:07:43 -0700

    ci: Add gerrit as a known host for Jenkins slaves
    As part of T130576, trying to make Jenkins push commits to gerrit
    as part of the maven release process. This currently fails because
    the Jenkins slaves don't trust the gerrit host.
    
    Change-Id: I57a7f79fe6545dcd99f5a20fa52d9e2fbd8b1523

--- modules/role/manifests/ci/slave/labs.pp
+++ modules/role/manifests/ci/slave/labs.pp
@@ -53,5 +53,22 @@ class role::ci::slave::labs {
         ]
     }
 
-}
+    # The sshkey resource seems to modify file permissions and make it
+    # unreadable - this is a known bug (https://tickets.puppetlabs.com/browse/PUP-2900)
+    # Trying to define this file resource, and notify the resource to be ensured
+    # from the sshkey resource, to see if it fixes the problem
+    file { '/etc/ssh/ssh_known_hosts':
+        ensure  => file,
+        mode    => '0644',
+    }
 
+    # Add gerrit as a known host
+    sshkey { 'gerrit':
+        ensure        => 'present',
+        name          => 'gerrit.wikimedia.org:29148',
+        host_aliases  => ['208.80.154.81:29148'],
+        key           => 'AAAAB3NzaC1yc2EAAAADAQABAAAAgQCF8pwFLehzCXhbF1jfHWtd9d1LFq2NirplEBQYs7AOrGwQ/6ZZI0gvZFYiEiaw1o+F1CMfoHdny1VfWOJF3mJ1y9QMKAacc8/Z3tG39jBKRQCuxmYLO1SWymv7/Uvx9WQlkNRoTdTTa9OJFy6UqvLQEXKYaokfMIUHZ+oVFf1CgQ==',
+        type          => 'ssh-rsa',
+        notify        => File['/etc/ssh/ssh_known_hosts'],
+    }
+}
