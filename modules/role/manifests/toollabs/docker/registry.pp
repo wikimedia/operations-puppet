28baf2e8ef6959ec4f1646219a60751407a02141
YuviPanda <yuvipanda@gmail.com>
2016-04-06 13:44:31 -0700

    docker: Add nginx frontend for registry
    - Limits PUT, POST, etc behind auth
    - Allows anonymous GET access
    - Put in a nginx hack to make it work
    
    Change-Id: I7b9e3b5ff2e9821a7b1da9f4c981efab515dd8bb

--- modules/role/manifests/toollabs/docker/registry.pp
+++ modules/role/manifests/toollabs/docker/registry.pp
@@ -3,7 +3,17 @@ class role::toollabs::docker::registry {
 
     require role::labs::lvm::srv
 
+    sslcert::certificate { 'star.tools.wmflabs.org':
+        skip_private => true,
+        before       => Class['::docker::registry'],
+    }
+
+    $builder = ipresolve(hiera('docker::builder_host'), 4, $::nameservers[0])
+
     class { '::docker::registry':
-        datapath => '/srv/registry',
+        datapath             => '/srv/registry',
+        allow_push_from      => $builder,
+        ssl_certificate_name => 'star.tools.wmflabs.org',
+        ssl_settings         => ssl_ciphersuite('nginx', 'compat'),
     }
 }
