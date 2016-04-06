ca7234b01b91b63764266c78822bec8563d105a5
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-04-06 09:30:28 +0200

    hhvm: add systemd/jessie support
    Change-Id: Id654cfb6f0a501172e4511f6d4656ef2ff48b423

---
+++ modules/hhvm/files/check-hhvm-stacktraces.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -u
+# Find any stacktraces we still did not act upon
+for TRACE in "/var/log/hhvm/stacktrace.*.log";
+do
+    # Log the stack trace
+    /usr/bin/logger --tag hhvm-fatal --file ${TRACE}
+
+    # Append the current UTC date (in YYYYMMDD format) to the file
+    # name, so it doesn't get clobbered when the PID is recycled.
+    mv --backup "$TRACE" "${TRACE}.$(date -u +%Y%m%d)"
+done
