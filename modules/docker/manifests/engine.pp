0c2c6914411db7bb89643488377a09d0b9f7beff
YuviPanda <yuvipanda@gmail.com>
2016-04-06 15:11:04 -0700

    docker: Don't setup credentials on all hosts
    Only on the docker builder hosts, since everyone else will
    only be pulling, not pushing.
    
    Change-Id: I9939bccb48f38b2c6a1f03655520525e2cd3b29a

--- modules/docker/manifests/engine.pp
+++ modules/docker/manifests/engine.pp
@@ -17,37 +17,6 @@ class docker::engine(
         require => Apt::Repository['docker'],
     }
 
-    $docker_username = hiera('docker::username')
-    $docker_password = hiera('docker::password')
-    $docker_registry = hiera('docker::registry')
-
-    # uses strict_encode64 since encode64 adds newlines?!
-    $docker_auth = inline_template("<%= require 'base64'; Base64.strict_encode64('${docker_username}:${docker_password}') -%>")
-
-    $docker_config = {
-        'auths' => {
-            "${docker_registry}" => {
-                'auth' => $docker_auth,
-            }
-        }
-    }
-
-    file { '/root/.docker':
-        ensure => directory,
-        owner  => 'root',
-        group  => 'docker',
-        mode   => '0550',
-    }
-
-    file { '/root/.docker/config.json':
-        content => ordered_json($docker_config),
-        owner   => 'root',
-        group   => 'docker',
-        mode    => '0440',
-        notify  => Service['docker'],
-        require => File['/root/.docker'],
-    }
-
     if $declare_service {
         service { 'docker':
             ensure    => running,
