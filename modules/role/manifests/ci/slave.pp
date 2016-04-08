ffd85fcbab673acb13f8e651417178f6c34cbb11
Antoine Musso <hashar@free.fr>
2016-04-08 11:33:41 +0200

    contint: clean up role::ci::slave
    The role::ci::slave is used on the Production CI slave gallium. Most of
    the jobs have been migrated out of that machine in favor of using labs
    instance.
    
    Remove:
    
    * Maven settings, has been ensure absent for a while.
    * integration/* repos via Trebuchet
    * contint::worker_localhost
    * tmpfs
    
    Update role::ci::slave class comment to indicate that class is solely
    for gallium and hint about role::ci::labs::slave.
    
    Change-Id: Ide33a166faeba58be9c5ac46f351cfa6ddc945ab

--- modules/role/manifests/ci/slave.pp
+++ modules/role/manifests/ci/slave.pp
@@ -1,21 +1,17 @@
-# Set up a Jenkins slave suitable for Continuous Integration jobs execution.
+# Set up a Jenkins slave on PRODUCTION
+#
+# Most jobs are running on labs instances, but we still have a few runnining on
+# gallium for example to publish content under doc.wikimedia.org or
+# integration.wikimedia.org
+#
+# You should look instaed at role::ci::labs::slave
+#
 class role::ci::slave {
 
     system::role { 'role::ci::slave': description => 'CI slave runner' }
 
     include contint::packages
     include role::zuul::install
-
-    package {
-        [
-            'integration/mediawiki-tools-codesniffer',
-            'integration/phpunit',
-            'integration/phpcs',
-            'integration/php-coveralls',
-            'integration/slave-scripts',
-        ]:
-        provider => 'trebuchet',
-    }
 
     class { 'jenkins::slave':
         ssh_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw== jenkins@gallium from="208.80.154.135"',
@@ -35,21 +31,6 @@ class role::ci::slave {
             },  # end of [user] section
         },  # end of settings
         require  => User['jenkins-slave'],
-    }
-
-    # drop settings file with old proxy settings
-    file { '/var/lib/jenkins-slave/.m2/settings.xml':
-        ensure => absent,
-    }
-
-    contint::tmpfs { 'tmpfs for jenkins CI slave':
-        mount_point => '/var/lib/jenkins-slave/tmpfs',
-        size        => '384M',
-    }
-
-    nrpe::monitor_service { 'ci_tmpfs':
-        description  => 'CI tmpfs disk space',
-        nrpe_command => '/usr/lib/nagios/plugins/check_disk -w 20% -c 5% -e -p /var/lib/jenkins-slave/tmpfs',
     }
 
     # user and private key for Travis integration
@@ -74,14 +55,6 @@ class role::ci::slave {
         content => secret('ssh/ci/npmtravis_id_rsa'),
         require => File['/home/npmtravis/.ssh'],
     }
-
-    file { '/srv/localhost-worker':
-        ensure => directory,
-        mode   => '0775',
-        owner  => 'jenkins-slave',
-        group  => 'jenkins-slave',
-    }
-    include contint::worker_localhost
 
     # Ganglia diskstat plugin is being evaluated on contint production slaves
     # servers merely to evaluate it for the standard role. -- hashar, 23-Oct-2013
