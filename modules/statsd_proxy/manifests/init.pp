52527c28eace79a2b7722f34394038aa858b0d37
Faidon Liambotis <faidon@wikimedia.org>
2016-04-08 16:11:16 +0300

    Add a new statsd_proxy module and replace statsdlb
    statslb is a trivial program that is, thus, not very performant (it's a
    single-threaded loop that calls recv(), hashes and sends ). I started
    hacking on it to improve its performance, by for example adding multiple
    threads or multiple processes (via SO_REUSEPORT) to it, or using epoll()
    or recvmmsg(). While doing that, I felt a bit NIH and started Googling a
    little bit, until I discovered statsd-proxy: a statsd proxy that someone
    else wrote, that uses multiple threads with SO_REUSEPORTS, hashes using
    a consistently hashing algorithm (Ketana), supports weights etc.
    
    So, a Debian package, systemd unit, upstart job and puppet module later
    (all trivial): replace statsdlb with a drop-in statsd_proxy replacement,
    statically configured to use 4 threads for now. This should be a no-op
    from the infrastructure PoV, just faster.
    
    Bug: T126447
    Change-Id: Ic0cbdf30f2785b6a0097089e3e9762178222cbb9

---
+++ modules/statsd_proxy/manifests/init.pp
@@ -0,0 +1,60 @@
+# == Class: statsd_proxy
+#
+# statsd-proxy is a StatsD-compatible proxy which routes metrics to multiple
+# local UDP ports. It consistently hashes metrics uses Ketama and is built for
+# high-performance.
+#
+#
+# === Parameters
+#
+# [*server_port*]
+#   UDP port to listen on. Example: 9001.
+#
+# [*backend_ports*]
+#   Array of local UDP ports to which incoming metrics should be routed.
+#   Example: [ 9002, 9003, 9004 ].
+#
+# [*threads*]
+#   Number of threads to spawn. Defaults to 4.
+#
+# === Examples
+#
+#  # Listen on UDP port 9001 and route to ports 9002-9004:
+#  class { 'statsd_proxy':
+#    server_port   => 9001,
+#    backend_ports => [ 9002, 9003, 9004 ],
+#  }
+#
+class statsd_proxy(
+    $ensure = present,
+    $threads = 4,
+    $server_port,
+    $backend_ports,
+) {
+    validate_ensure($ensure)
+    validate_array($backend_ports)
+    validate_re(join($backend_ports, ' '), '^\d+( \d+)*$', '$backend_ports must be an array of port numbers')
+    validate_re($server_port, '^\d+$', '$server_port must be a port number')
+
+    package { 'statsd-proxy':
+        ensure => $ensure,
+    }
+
+    file { '/etc/statsd-proxy.cfg':
+        ensure  => $ensure,
+        content => template('statsd_proxy/statsd-proxy.cfg.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+
+    service { 'statsd-proxy':
+        ensure    => ensure_service($ensure),
+        enable    => $ensure == 'present',
+        provider  => $::initsystem,
+        subscribe => [
+            Package['statsd-proxy'],
+            File['/etc/default/statsd-proxy'],
+        ],
+    }
+}
