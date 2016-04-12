ad9534270c2effd8fb9a22cdd76a8450581149bc
Emanuele Rocca <ema@wikimedia.org>
2016-04-12 11:51:54 +0200

    Port varnishxcps to new VSL API
    Bug: T131353
    Change-Id: I157ec0bcaf77625526410170a5a56a41c3317d42

--- modules/varnish/manifests/logging/xcps.pp
+++ modules/varnish/manifests/logging/xcps.pp
@@ -18,8 +18,15 @@
 define varnish::logging::xcps( $statsd_server = 'statsd' ) {
     include varnish::common
 
+    if (hiera('varnish_version4', false)) {
+        # Use v4 version of varnishxcps
+        $varnish4_python_suffix = '4'
+    } else {
+        $varnish4_python_suffix = ''
+    }
+
     file { '/usr/local/bin/varnishxcps':
-        source  => 'puppet:///modules/varnish/varnishxcps',
+        source  => "puppet:///modules/varnish/varnishxcps${varnish4_python_suffix}",
         owner   => 'root',
         group   => 'root',
         mode    => '0555',
