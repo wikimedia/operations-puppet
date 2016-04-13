4ad53689523f122d3e2f4d0853869636467f8fc6
Alex Monk <krenair@gmail.com>
2016-04-13 04:03:29 +0100

    [WIP] deployment-prep: keyholder shinken monitoring
    Bug: T111064
    Change-Id: Iee4486128d0daa6ae5c7672ebeeefc3b9fc2dbb0

---
+++ modules/keyholder/files/collector.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+"Collect keyholder status code"
+import subprocess
+import diamond.collector
+
+class KeyholderStatusCollector(diamond.collector.Collector):
+    def collect(self):
+        child = subprocess.Popen('/usr/lib/nagios/plugins/check_keyholder')
+        self.publish('keyholder_status', child.wait())
