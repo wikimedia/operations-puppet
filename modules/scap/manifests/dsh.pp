331eb24b94ab2453451d8c1fc511ec6c7f51b2fe
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-04-13 17:18:05 +0200

    scap: use conftool data to populate dsh groups
    Populate the dsh groups from puppet based on their status in conftool:
    
    Any server in conftool data that is also not marked as 'inactive' will
    be included in the dsh list.
    
    Bug: T132529
    Change-Id: Ibad2c11c85dd3be10aae6e4fcde52d61da80ca13

--- modules/scap/manifests/dsh.pp
+++ modules/scap/manifests/dsh.pp
@@ -4,9 +4,6 @@
 # setting up dsh. Useful primarily for monitoring and deploy servers.
 #
 # == Paramters:
-# [*group_source*]
-#   Puppet file source for /etc/dsh/group.
-#   Default 'puppet:///modules/dsh/group'
 #
 # [*scap_proxies*]
 #   List of FQDNs for servers to be used as scap rsync proxies. Default []
@@ -15,7 +12,6 @@
 #   List of FQDNs for servers to be used as scap masters. Default []
 #
 class scap::dsh (
-    $group_source = 'puppet:///modules/scap/dsh/group',
     $scap_proxies = [],
     $scap_masters = [],
 ){
@@ -26,12 +22,16 @@ class scap::dsh (
         mode   => '0444',
     }
     file { '/etc/dsh/group':
+        ensure  => directory,
         owner   => 'root',
         group   => 'root',
-        mode    => '0444',
-        source  => $group_source,
+        mode    => '0555',
         recurse => true,
     }
+
+    # Base dsh groups currently used
+    $groups = hiera('scap::dsh::groups', {})
+    create_resources('scap::dsh::group', $groups)
 
     file { '/etc/dsh/group/scap-proxies':
         content => join($scap_proxies, "\n"),
