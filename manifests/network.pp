8a01cd9d991d3f56b2df2f83545adc411d31e9b6
dzahn <dzahn@wikimedia.org>
2016-04-13 16:48:52 -0700

    network: remove bast4001 SLAAC IPs
    These were only needed for precise and now bast4001
    is on jessie.
    
    Bug:T123674
    Change-Id: I80ffbfc302f93ffdcc6c1fd77843405c08a757e4

--- manifests/network.pp
+++ manifests/network.pp
@@ -28,7 +28,6 @@ class network::constants {
                     '2620:0:862:1:91:198:174:112',      # bast3001.wikimedia.org
                     '198.35.26.5',                      # bast4001.wikimedia.org
                     '2620:0:863:1:198:35:26:5',         # bast4001.wikimedia.org
-                    '2620:0:863:1:92b1:1cff:fe4d:4249', # bast4001.wikimedia.org SLAAC
                     '208.80.154.151',                   # iron.wikimedia.org
                     '2620:0:861:2:208:80:154:151',      # iron.wikimedia.org
                     '2620:0:861:2:7a2b:cbff:fe09:d5c',  # iron.wikimedia.org SLAAC
@@ -57,7 +56,6 @@ class network::constants {
                     '2620:0:862:1:91:198:174:112',      # bast3001.wikimedia.org
                     '198.35.26.5',                      # bast4001.wikimedia.org
                     '2620:0:863:1:198:35:26:5',         # bast4001.wikimedia.org
-                    '2620:0:863:1:92b1:1cff:fe4d:4249', # bast4001.wikimedia.org SLAAC
                     '208.80.154.151',                   # iron.wikimedia.org
                     '2620:0:861:2:208:80:154:151',      # iron.wikimedia.org
                     '2620:0:861:2:7a2b:cbff:fe09:d5c',  # iron.wikimedia.org SLAAC
