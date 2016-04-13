a5e6cded0de89cca19e6862aacbf047af8c858a8
Dzahn <dzahn@wikimedia.org>
2016-04-13 19:15:37 +0000

    Revert "Revert "bast1001: rsync home dirs back from tungsten""
    This reverts commit e02dea6dad771761d4951fca9a1c163451798377.
    
    Change-Id: I180685110737daf7c015fdb9d9eb263149fdf57c

--- manifests/site.pp
+++ manifests/site.pp
@@ -2611,7 +2611,7 @@ node 'titanium.wikimedia.org' {
 
 # test system for performance team (T117888)
 node 'tungsten.eqiad.wmnet' {
-    role testsystem, xhgui, bastionhost::migration
+    role testsystem, xhgui
 }
 
 node 'uranium.wikimedia.org' {
