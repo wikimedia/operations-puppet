c550fab689039b3e0902453a16b8433a6f9abc9c
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-04-13 12:31:41 +0200

    puppet: add a function for performing conftool lookups
    It can be needed to fetch the dynamic state of conftool for some
    non-dynamic operations; a perfect example could be knowing which servers
    are out of commission for scap. This function gives us a (somewhat
    crude) interface to conftool.
    
    Change-Id: I1c3b89b45e62591595c2faec793e35ed66e4cda7

---
+++ modules/wmflib/lib/puppet/parser/functions/conftool.rb
@@ -0,0 +1,62 @@
+# == Function: conftool( string $tags, string $selector, string $object_type='node')
+#
+# Fetch values from conftool. This should be used only for things that depend on the dynamic
+# state from conftool but do not require the tight coordination.
+#
+# It will get (and json parse) values from conftool and return them, based on the specified
+# selector.
+#
+# === Examples
+#
+# # get the current status of a node in confctl
+# $status = conftool({ name => 'cp1052.*', service => 'varnish.*'}) # returns a list of associated services
+#
+require 'json'
+
+module Puppet::Parser::Functions
+  newfunction(:conftool, :type => :rvalue, :arity => -1) do |args|
+    case args.length
+    when 2
+      tags, type = args
+    when 1
+      tags = args[0]
+      type = 'node'
+    else
+      raise Puppet::ParseError, "Wrong number of arguments for conftool()"
+    end
+
+    # Raise an error if tags is empty or not an hash
+    if not tags.is_a?Hash or tags == {}
+      raise Puppet::ParseError, "tags should be in hash format"
+    end
+
+    # get the data and return them parsed as json
+    begin
+      selector = tags.map { |k,v| "#{k}=#{v}" }.join ","
+      result = []
+      data = function_generate(
+        [
+          '/usr/bin/conftool',
+          '--object-type', type,
+          'select', selector,
+          'get'
+        ]
+      )
+
+      # No result returns the empty list
+      if data.to_s == ''
+        return []
+      end
+
+      data.split("\n").each do |line|
+        entry = JSON.load(line)
+        tags = entry.delete 'tags'
+        obj_name = entry.keys.pop
+        result.push({'name' => obj_name, 'tags' => tags, 'value' => entry[obj_name]})
+      end
+      result
+    rescue
+      raise Puppet::ParseError, "Unable to read data from conftool"
+    end
+  end
+end
