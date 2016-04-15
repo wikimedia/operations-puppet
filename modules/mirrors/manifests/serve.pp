20c0a0c37f8c8d960b271cf72ebe3b5adaf87563
Brandon Black <bblack@wikimedia.org>
2016-04-15 11:14:34 +0000

    mirrors::serve: split mirrors/ubuntu site configs
    Bug: T132450
    Change-Id: I4c693daf76098365cee88f0d17a8dc466e7e780d

--- modules/mirrors/manifests/serve.pp
+++ modules/mirrors/manifests/serve.pp
@@ -3,7 +3,11 @@ class mirrors::serve {
     include ::nginx
 
     nginx::site { 'mirrors':
-        source => 'puppet:///modules/mirrors/nginx.conf',
+        source => 'puppet:///modules/mirrors/mirrors.wikimedia.org.conf',
+    }
+
+    nginx::site { 'ubuntu':
+        source => 'puppet:///modules/mirrors/ubuntu.wikimedia.org.conf',
     }
 
     file { '/srv/mirrors/index.html':
