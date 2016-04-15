be7417959557097b16c7c784842fcb7e2dedecb9
Brandon Black <bblack@wikimedia.org>
2016-04-15 10:53:53 +0000

    refactor install_server web stuff towards SSL config
    * makes nginx.conf explicit, with SSL params included
    * old site config had no server_name, serviced
      carbon.wikimedia.org + apt.wikimedia.org in practice
    * new is split in two: one with $fqdn for the carbon name and
      default_server, the other for apt.wikimedia.org
    
    Bug: T132450
    Change-Id: I7ac66091125a7a236ad2c7b808ab44d6ad7689d8

--- modules/install_server/manifests/web_server.pp
+++ modules/install_server/manifests/web_server.pp
@@ -16,8 +16,18 @@
 class install_server::web_server {
     include ::nginx
 
+    $nginx_ssl_conf = ssl_ciphersuite('nginx', 'compat')
+    file { '/etc/nginx/nginx.conf':
+        content => template('install_server/nginx.conf.erb'),
+        tag     => 'nginx',
+    }
+
     nginx::site { 'install_server':
-        source  => 'puppet:///modules/install_server/nginx.conf';
+        content => template('install_server/install_server.conf.erb'),
+    }
+
+    nginx::site { 'apt.wikimedia.org':
+        source  => 'puppet:///modules/install_server/apt.wikimedia.org.conf',
     }
 
     # prevent a /srv root autoindex; empty for now.
