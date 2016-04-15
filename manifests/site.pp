a0659f3d58be8b98aaaebc10f74459c360875a55
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-04-15 11:01:55 +0200

    Enable base::firewall for mariadb s5 shard in eqiad
    Change-Id: I4189ba3f15f5c48d342da4a58a69019840c22b00

--- manifests/site.pp
+++ manifests/site.pp
@@ -591,6 +591,7 @@ node /^db10(26|45|70|71)\.eqiad\.wmnet/ {
     class { 'role::mariadb::core':
         shard => 's5',
     }
+    include base::firewall
 }
 node /^db10(49)\.eqiad\.wmnet/ {
     class { 'role::mariadb::core':
