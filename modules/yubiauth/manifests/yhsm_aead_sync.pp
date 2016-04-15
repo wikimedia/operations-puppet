da4cd1e0c9d906a7f975e81483e54b1dc0eec66e
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-04-15 15:47:20 +0200

    Configure an rsync server which is used to synchronise the AEAD key files between the auth servers
    After creation on the key generation site (laptop at OIT), they are synchronised to the server
    specified in Hiera via yubiauth_server_primary. All other Yubico authentication servers synchronise
    from that host.
    
    This class configures an rsync server which makes the AEAD spool accessible.
    
    Change-Id: I3b971d09c1edc7bcf88de71be752017fdaed192f

---
+++ modules/yubiauth/manifests/yhsm_aead_sync.pp
@@ -0,0 +1,12 @@
+# This sets up an rsync server, which can be used to synchronise the AEADs between the Yubico authentication servers.
+class yubiauth::yhsm_aead_sync(
+    $sync_allowed = '127.0.0.1',
+) {
+    include rsync::server
+
+    rsync::server::module { 'aead_sync':
+        path        => '/var/cache/yubikey-ksm/aeads',
+        read_only   => 'yes',
+        hosts_allow => $sync_allowed,
+    }
+}
