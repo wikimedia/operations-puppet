6bb4f2de2205f1918c7b7accbca59261d0534295
dzahn <dzahn@wikimedia.org>
2016-04-15 17:01:56 -0700

    create letsencrypt module,  install acme-tiny
    One very first step for T132812, create module
    letsencrypt and let it install acme-tiny,
    taken from current master on github.
    
    Bug:T132812
    Change-Id: I7f68efb798858f29cf09d312b0c01dea99d0c992

---
+++ modules/letsencrypt/manifests/init.pp
@@ -0,0 +1,13 @@
+# setup a TLS cert from letsencrypt.org
+class letsencrypt {
+
+    # https://github.com/diafygi/acme-tiny
+    file { '/usr/local/bin/acme_tiny.py':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/letsencrypt/acme_tiny.py'
+    }
+
+}
