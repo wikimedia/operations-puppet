86abc1340540b573bea577c6cae175cf644e6c1f
dzahn <dzahn@wikimedia.org>
2016-04-18 14:36:56 -0700

    apache: ignore lint issues in mod.pp
    Change-Id: I832692cca8ae2ee0e52c0a9bf43455c8e90599e6

--- modules/apache/manifests/mod.pp
+++ modules/apache/manifests/mod.pp
@@ -7,6 +7,8 @@
 class apache::mod {}
 
 # Modules that are bundled with the apache2 package
+# lint:ignore:autoloader_layout
+# ^ FIXABLE ?
 class apache::mod::actions         { apache::mod_conf { 'actions':        } }
 class apache::mod::alias           { apache::mod_conf { 'alias':          } }
 class apache::mod::auth_basic      { apache::mod_conf { 'auth_basic':     } }
@@ -33,9 +35,11 @@ class apache::mod::rewrite         { apache::mod_conf { 'rewrite':        } }
 class apache::mod::setenvif        { apache::mod_conf { 'setenvif':       } }
 class apache::mod::ssl             { apache::mod_conf { 'ssl':            } }
 class apache::mod::userdir         { apache::mod_conf { 'userdir':        } }
+# lint:endignore
 
 # Modules that depend on additional packages
-# lint:ignore:right_to_left_relationship
+# lint:ignore:right_to_left_relationship lint:ignore:autoloader_layout
+# FIXABLE ^ ?
 class apache::mod::authz_svn       { apache::mod_conf { 'authz_svn':      } <- package { 'libapache2-svn':           } }
 class apache::mod::fastcgi         { apache::mod_conf { 'fastcgi':        } <- package { 'libapache2-mod-fastcgi':   } }
 class apache::mod::fcgid           { apache::mod_conf { 'fcgid':          } <- package { 'libapache2-mod-fcgid':     } }
@@ -47,7 +51,6 @@ class apache::mod::rpaf            { apache::mod_conf { 'rpaf':           } <- p
 class apache::mod::security2       { apache::mod_conf { 'security2':      } <- package { 'libapache2-mod-security2': } }
 class apache::mod::uwsgi           { apache::mod_conf { 'uwsgi':          } <- package { 'libapache2-mod-uwsgi':     } }
 class apache::mod::wsgi            { apache::mod_conf { 'wsgi':           } <- package { 'libapache2-mod-wsgi':      } }
-# lint:endignore
 
 # Modules that target a specific distribution
 class apache::mod::access_compat { if os_version('debian >= jessie || ubuntu >= 13.10') { apache::mod_conf { 'access_compat': } } }
@@ -55,6 +58,7 @@ class apache::mod::proxy_fcgi    { if os_version('debian >= jessie || ubuntu >= 
 class apache::mod::remoteip      { if os_version('debian >= jessie || ubuntu >= 13.10') { apache::mod_conf { 'remoteip':      } } }
 class apache::mod::version       { if os_version('ubuntu < 13.10')  { apache::mod_conf { 'version':       } } }
 class apache::mod::lbmethod_byrequests  { if os_version('debian >= jessie || ubuntu >= 13.10') { apache::mod_conf { 'lbmethod_byrequests': } } }
+# lint:endignore
 
 # == Class: apache::mod::status
 #
@@ -66,7 +70,7 @@ class apache::mod::lbmethod_byrequests  { if os_version('debian >= jessie || ubu
 # a more conservative configuration that makes /server-status accessible
 # only to requests made via the loopback interface. See T113090.
 #
-class apache::mod::status {
+class apache::mod::status { # lint:ignore:autoloader_layout
     file { [
         '/etc/apache2/mods-available/status.conf',
         '/etc/apache2/mods-enabled/status.conf',
