a0f8c9976d396875cbff20fd4ff3b80ce9a5733b
Brandon Black <bblack@wikimedia.org>
2016-04-18 15:41:00 +0000

    letsencrypt module guts + acme-setup script
    Bug: T132812
    Change-Id: I08c0c87516533fd334d1ae3e1f0282b907369f9a

---
+++ modules/letsencrypt/manifests/cert/integrated.pp
@@ -0,0 +1,97 @@
+# == Define: letsencrypt::cert::integrated
+#
+# This defines a TLS certificate which is automatically acquired
+# and renewed from letsencrypt.org.  This particular puppetization
+# ("integrated") requires that:
+#
+# 1. The certificate hostnames are publicly-accessible over HTTP on the same
+#    machine the certificate is being provisioned on.
+# 2. The machine has public network access.
+# 3. The server software is capable of mapping a URI path to an arbitrary
+#    filesystem path, and is configured to do so, mapping
+#    '/.well-known/acme-challenge' to '/var/acme/challenge'.
+# 4. The software loads new certs when commanded via 'service foo reload'
+#
+# The actual output key/cert files will be at the follow paths for
+# the web server's configuration to consume.
+#
+# Private Key:       /etc/acme/key/${title}.key
+# Public Cert:       /etc/acme/cert/${title}.crt
+# Intermediate:      /etc/acme/cert/${title}.chain.crt
+# Cert+Intermediate: /etc/acme/cert/${title}.chained.crt
+#
+# Notes:
+# 1) this does not puppetize OCSP Stapling - probably best doing this in
+#    apache/nginx config with existing built-in mechanisms where applicable,
+#    and maybe puppetizing that better in shared way.
+# 2) This intentionally does not scale for a many certs on one host situation.
+#    It would be messy and inefficient, especially on the service reload side.
+#    This class is only for the case of one or a few certs on a server - we can
+#    write better wrappers for those kinds of special cases...
+#
+# See also: the general LE notes in modules/letsencrypt/init.pp
+#
+# === Parameters
+#
+# [*subjects*]
+#   The list of FQDNs the cert should be valid for, comma-separated.  The ACME
+#   challenge must work for all of these hostnames (they must all be configured
+#   in the webserver as legitimate HTTP hostnames).
+#
+# [*puppet_svc*]
+#   The puppet service name of the webserver for use in Service[] dependency
+#   references, usually 'nginx' or 'apache2'.
+#
+# [*system_svc*]
+#   The system-level service name for the same as the above, for use in commands
+#   like: 'service $system_service reload'
+#
+# === Examples
+#
+# letsencrypt::cert::integrated { 'example':
+#     subjects   => 'www.example.com,foo.example.com',
+#     puppet_svc => 'nginx',
+#     system_svc => 'nginx',
+# }
+#
+# integrated webserver configuration:
+#   nginx:
+#     An include file will exist as /etc/acme/challenge-nginx.conf
+#     It defines a 'location' for '/.well-known/acme-challenge', and should be
+#     put it within 'server' for port 80.  If port 80 redirects to HTTPS, this
+#     must be excluded from the redirect by putting the redirect in a separate
+#     location block for '/', as in:
+#       server {
+#         listen 80 ...
+#         server_name ...
+#         ...
+#         include /etc/acme/challenge-nginx.conf;
+#         location / {
+#           return 301 https://example.wikimedia.org$request_uri
+#         }
+#       }
+#
+#   apache: XXX TODO config frag + instructions as above
+
+define letsencrypt::cert::integrated($subjects, $puppet_svc, $system_svc) {
+    require ::letsencrypt
+
+    $safe_title = regsubst($title, '\W', '_', 'G')
+    $base_cmd = "/usr/local/sbin/acme-setup -i ${safe_title} -s ${subjects}"
+
+    # Pre-setup with self-signed cert if necessary, to let $puppet_svc start
+    exec { "acme-setup-self-${safe_title}":
+        command => $base_cmd,
+        creates => "/etc/acme/certs/${safe_title}.crt",
+        before  => Service[$puppet_svc],
+    }
+
+    # Post-setup and renewal - runs on every puppet run, creates a new ACME
+    # cert and reloads the webserver iff existing cert is self-signed from
+    # above or reaches expiry threshold (30-44 days left, deterministically
+    # random per unique certificate (will change on renewal)).
+    exec { "acme-setup-acme-${safe_title}":
+        command => "${base_cmd} -m acme -w ${system_svc}",
+        require => Service[$puppet_svc],
+    }
+}
