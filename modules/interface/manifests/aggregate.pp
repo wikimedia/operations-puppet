9436ef4e38db70ea8d189c26ad67e9f0fe4ff710
dzahn <dzahn@wikimedia.org>
2016-04-18 14:32:20 -0700

    interface: move aggregate_member to own file
    Another one to move a class into proper autoloader layout
    and its own file and fix the lint error about that.
    
    Change-Id: I6ab3ad8e4cd9c88f08ff813924fbed0dcadbf8ad

--- modules/interface/manifests/aggregate.pp
+++ modules/interface/manifests/aggregate.pp
@@ -1,26 +1,3 @@
-define interface::aggregate_member($master) {
-    require_package('ifenslave-2.6')
-
-    $interface = $title
-
-    augeas { "aggregate member ${interface}":
-        context => '/files/etc/network/interfaces/',
-        changes => [
-                "set auto[./1 = '${interface}']/1 '${interface}'",
-                "set iface[. = '${interface}'] '${interface}'",
-                "set iface[. = '${interface}']/family 'inet'",
-                "set iface[. = '${interface}']/method 'manual'",
-        ],
-        notify  => Exec["ifup ${interface}"]
-    }
-
-    exec { "ifup ${interface}":
-        command     => "/sbin/ifup --force ${interface}; /sbin/ip link set dev ${interface} up",
-        require     => Augeas["aggregate member ${interface}"],
-        refreshonly => true
-    }
-}
-
 define interface::aggregate($orig_interface=undef, $members=[], $lacp_rate='fast', $hash_policy='layer2+3') {
     require_package('ifenslave-2.6')
 
