ca50e605cc6cdf6b96207f737aaf82ae90511c5d
Jaime Crespo <jcrespo@wikimedia.org>
2016-04-18 16:47:11 +0200

    [WIP] Install pt-heartbeat-wikimedia on all relevant servers
    Any host that that is a candidate master will have it installed,
    only enabled on the actual master.
    
    Convert pc* hosts into 3 shards, with potentially one master
    running pt-heartbeat.
    
    Change-Id: Ie0be85409da0782c0f9b5b25d9ec3dc922b3e7ee

--- manifests/role/mariadb.pp
+++ manifests/role/mariadb.pp
@@ -131,10 +131,9 @@ class role::mariadb::misc(
         shard => $shard,
     }
 
-    if $master {
-        class { 'mariadb::heartbeat':
-            shard => $shard,
-        }
+    class { 'mariadb::heartbeat':
+        shard   => $shard,
+        enabled => $master,
     }
 }
 
@@ -207,10 +206,9 @@ class role::mariadb::misc::phabricator(
         include coredb_mysql::snapshot
     }
 
-    if $master {
-        class { 'mariadb::heartbeat':
-            shard => $shard,
-        }
+    class { 'mariadb::heartbeat':
+        shard   => $shard,
+        enabled => $master,
     }
 
     unless $master {
@@ -262,10 +260,9 @@ class role::mariadb::misc::eventlogging(
         shard => $shard,
     }
 
-    if $master {
-        class { 'mariadb::heartbeat':
-            shard => $shard,
-        }
+    class { 'mariadb::heartbeat':
+        shard   => $shard,
+        enabled => $master,
     }
 }
 
@@ -537,10 +534,9 @@ class role::mariadb::core(
         contact_group => $contact_group,
     }
 
-    if $master {
-        class { 'mariadb::heartbeat':
-            shard => $shard,
-        }
+    class { 'mariadb::heartbeat':
+        shard   => $shard,
+        enabled => $master,
     }
 }
 
@@ -787,7 +783,9 @@ class role::mariadb::proxy::slaves(
 }
 
 # parsercache (pc) specific configuration
-class role::mariadb::parsercache {
+class role::mariadb::parsercache
+    $shard,
+    ) {
 
     include standard
 
@@ -821,6 +819,12 @@ class role::mariadb::parsercache {
         tmpdir   => "${basedir}/tmp",
         ssl      => 'on',
         p_s      => 'off',
+    }
+
+    $heartbeat_enabled = ($::mw_primary in $::domain)
+    class { 'mariadb::heartbeat':
+        shard   => $shard,
+        enabled => $heartbeat_enabled,
     }
 
     # mysql monitoring access from tendril (db1011)
