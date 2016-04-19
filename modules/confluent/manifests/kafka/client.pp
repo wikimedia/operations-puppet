8e4f01c5e29b62ebd0800dd69f2abc546aec9a5d
Andrew Otto <acotto@gmail.com>
2016-04-19 16:55:38 -0400

    [WIP] Add new confluent module and puppetization for using confluent Kafka
    Bug: T132631
    Change-Id: I36d9e6f50da554f1674294e805fe03f1071c5016

---
+++ modules/confluent/manifests/kafka/client.pp
@@ -0,0 +1,20 @@
+# == Class confluent::kafka::client
+# Installs the confluent-kafka package and a handy kafka wrapper script
+#
+class confluent::kafka::client(
+    $java_package  = 'openjdk-7-jdk',
+    $scala_version = '2.11.7'
+) {
+    require_package($java_package)
+
+    $package = "confluent-kafka-${scala_version}"
+    require_package($package)
+
+    file { '/usr/local/bin/kafka':
+        source  => 'puppet:///modules/confluent/kafka/kafka.sh',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        require => [Package[$package], Package[$java_package]],
+    }
+}
