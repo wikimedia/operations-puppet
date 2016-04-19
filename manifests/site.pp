14c42b1a4719e3ec2088444c4fb47aec49245804
Riccardo Coccioli <rcoccioli@wikimedia.org>
2016-04-19 11:30:18 +0200

    MariaDB: set codfw local masters as masters (s1-s7)
    - Set master=true and binlog_format=STATEMENT for s1-s7 codfw local
      masters to  become masters
    
    Bug: T124699
    Change-Id: I4a07204323b0ffd5cbffb02e44587c4b61b2b49f

--- manifests/site.pp
+++ manifests/site.pp
@@ -461,7 +461,7 @@ node /^db10(33)\.eqiad\.wmnet/ {
 
 # MariaDB 10
 
-# s1 (enwiki) core production slave dbs on eqiad
+# s1 (enwiki) core production dbs on eqiad
 node /^db10(51|55|66|72|73)\.eqiad\.wmnet/ {
     class { 'role::mariadb::core':
         shard => 's1',
@@ -494,7 +494,19 @@ node /^db10(57)\.eqiad\.wmnet/ {
     include base::firewall
 }
 
-node /^db20(16|34|42|48|55|62|69|70)\.codfw\.wmnet/ {
+# codfw master
+node 'db2016.codfw.wmnet' {
+    class { 'role::mariadb::core':
+        shard         => 's1',
+        master        => true,
+        p_s           => 'on',
+        binlog_format => 'STATEMENT',
+        ssl           => 'puppet-cert',
+    }
+    include base::firewall
+}
+
+node /^db20(34|42|48|55|62|69|70)\.codfw\.wmnet/ {
     class { 'role::mariadb::core':
         shard         => 's1',
         p_s           => 'on',
@@ -504,11 +516,11 @@ node /^db20(16|34|42|48|55|62|69|70)\.codfw\.wmnet/ {
     include base::firewall
 }
 
-# s2 (large wikis) core production slave dbs on eqiad
+# s2 (large wikis) core production dbs on eqiad
 node 'db1018.eqiad.wmnet' {
     class { 'role::mariadb::core':
         shard         => 's2',
-        master        => true,
+        master        => false,
         p_s           => 'on',
         binlog_format => 'STATEMENT',
     }
@@ -523,11 +535,13 @@ node /^db10(21|24|36|54|60|63|67|74|76)\.eqiad\.wmnet/ {
     include base::firewall
 }
 
-node /^db20(17)\.codfw\.wmnet/ {
+# codfw master
+node 'db2017.codfw.wmnet' {
     class { 'role::mariadb::core':
         shard         => 's2',
+        master        => true,
         p_s           => 'on',
-        binlog_format => 'ROW',
+        binlog_format => 'STATEMENT',
         ssl           => 'multiple-ca',
     }
     include base::firewall
@@ -538,11 +552,12 @@ node /^db20(35|41|49|56|63|64)\.codfw\.wmnet/ {
         shard         => 's2',
         p_s           => 'on',
         binlog_format => 'ROW',
+        ssl           => 'puppet-cert',
     }
     include base::firewall
 }
 
-# s3 (default) core production slave dbs on eqiad
+# s3 (default) core production dbs on eqiad
 node /^db10(15|27|35|44)\.eqiad\.wmnet/ {
     class { 'role::mariadb::core':
         shard => 's3',
@@ -565,8 +580,9 @@ node /^db10(75|77|78)\.eqiad\.wmnet/ {
 node 'db2018.codfw.wmnet' {
     class { 'role::mariadb::core':
         shard         => 's3',
+        master        => true,
         p_s           => 'on',
-        binlog_format => 'MIXED',
+        binlog_format => 'STATEMENT',
         ssl           => 'puppet-cert',
     }
     include base::firewall
@@ -582,7 +598,7 @@ node /^db20(36|43|50|57)\.codfw\.wmnet/ {
     include base::firewall
 }
 
-# s4 (commons) core production slave dbs on eqiad
+# s4 (commons) core production dbs on eqiad
 node /^db10(19|56|59|64|68)\.eqiad\.wmnet/ {
     class { 'role::mariadb::core':
         shard => 's4',
@@ -597,8 +613,20 @@ node /^db10(42)\.eqiad\.wmnet/ {
     include base::firewall
 }
 
-# s4 (commons) core production slave dbs on codfw
-node /^db20(19|37|44|51|58|65)\.codfw\.wmnet/ {
+# s4 (commons) core production dbs on codfw
+# codfw master
+node 'db2019.codfw.wmnet' {
+    class { 'role::mariadb::core':
+        shard         => 's4',
+        master        => true,
+        p_s           => 'on',
+        binlog_format => 'STATEMENT',
+        ssl           => 'puppet-cert',
+    }
+    include base::firewall
+}
+
+node /^db20(37|44|51|58|65)\.codfw\.wmnet/ {
     class { 'role::mariadb::core':
         shard         => 's4',
         p_s           => 'on',
@@ -608,7 +636,7 @@ node /^db20(19|37|44|51|58|65)\.codfw\.wmnet/ {
     include base::firewall
 }
 
-# s5 (wikidata/dewiki) core production slave dbs on eqiad
+# s5 (wikidata/dewiki) core production dbs on eqiad
 node /^db10(26|45|70|71)\.eqiad\.wmnet/ {
     class { 'role::mariadb::core':
         shard => 's5',
@@ -623,8 +651,20 @@ node /^db10(49)\.eqiad\.wmnet/ {
     include base::firewall
 }
 
-# s5 (wikidata/dewiki) core production slave dbs on codfw
-node /^db20(23|38|45|52|59|66)\.codfw\.wmnet/ {
+# s5 (wikidata/dewiki) core production dbs on codfw
+# codfw master
+node 'db2023.codfw.wmnet' {
+    class { 'role::mariadb::core':
+        shard         => 's5',
+        master        => true,
+        p_s           => 'on',
+        binlog_format => 'STATEMENT',
+        ssl           => 'puppet-cert',
+    }
+    include base::firewall
+}
+
+node /^db20(38|45|52|59|66)\.codfw\.wmnet/ {
     class { 'role::mariadb::core':
         shard         => 's5',
         p_s           => 'on',
@@ -634,7 +674,7 @@ node /^db20(23|38|45|52|59|66)\.codfw\.wmnet/ {
     include base::firewall
 }
 
-# s6 core production slave dbs on eqiad
+# s6 core production dbs on eqiad
 node /^db10(30|37|61)\.eqiad\.wmnet/ {
     class { 'role::mariadb::core':
         shard => 's6',
@@ -658,8 +698,20 @@ node /^db10(50)\.eqiad\.wmnet/ {
     include base::firewall
 }
 
-# s6 core production slave dbs on codfw
-node /^db20(28|39|46|53|60|67)\.codfw\.wmnet/ {
+# s6 core production dbs on codfw
+# codfw master
+node 'db2028.codfw.wmnet' {
+    class { 'role::mariadb::core':
+        shard         => 's6',
+        master        => true,
+        p_s           => 'on',
+        binlog_format => 'STATEMENT',
+        ssl           => 'puppet-cert',
+    }
+    include base::firewall
+}
+
+node /^db20(39|46|53|60|67)\.codfw\.wmnet/ {
     class { 'role::mariadb::core':
         shard         => 's6',
         p_s           => 'on',
@@ -669,7 +721,7 @@ node /^db20(28|39|46|53|60|67)\.codfw\.wmnet/ {
     include base::firewall
 }
 
-# s7 (centralauth, meta et al.) core production slave dbs on eqiad
+# s7 (centralauth, meta et al.) core production dbs on eqiad
 node /^db10(28|34|39|62)\.eqiad\.wmnet/ {
     class { 'role::mariadb::core':
         shard => 's7',
@@ -684,8 +736,20 @@ node /^db10(41)\.eqiad\.wmnet/ {
     include base::firewall
 }
 
-# s7 (centralauth, meta et al.) core production slave dbs on codfw
-node /^db20(29|40|47|54|61|68)\.codfw\.wmnet/ {
+# s7 (centralauth, meta et al.) core production dbs on codfw
+# codfw master
+node 'db2029.codfw.wmnet' {
+    class { 'role::mariadb::core':
+        shard         => 's7',
+        master        => true,
+        p_s           => 'on',
+        binlog_format => 'STATEMENT',
+        ssl           => 'puppet-cert',
+    }
+    include base::firewall
+}
+
+node /^db20(40|47|54|61|68)\.codfw\.wmnet/ {
     class { 'role::mariadb::core':
         shard         => 's7',
         p_s           => 'on',
