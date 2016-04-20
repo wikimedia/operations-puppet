1117d52531ee9b60b5306868e1739eab515c7c04
cpettet <rush@wikimedia.org>
2016-04-20 15:58:18 -0500

    lshell scaffolding for restricting Labs users
    Bug: T102395
    Change-Id: Iece9c2fb54a9376e6d59ddcb4bc120f29d4efc58

---
+++ modules/lshell/manifests/init.pp
@@ -0,0 +1,62 @@
+# Limited shell environment
+#
+#  * Anything that allows shelling out escapes
+#  * This is not a full shell replacment
+#  * Combine with @shell_override in nslcd.conf.erb
+#
+# === Parameters
+#
+# [*$allowed_cmds*]
+#  Whitelist of allowed interactive shell commands
+#  No specified commands will result in permissive
+#
+# [*$allowed_cmd_path*]
+#  Whitelist of all commands in an array of paths
+#
+# [*$forbidden*]
+#  Invalid characters
+#
+# [*$banner*]
+#  Display text upon login
+#
+# [*$overssh*]
+#  Noninteractive shell commands allowed
+#
+# [*$timer*]
+#  Session duration length allowed
+#
+# [*$path]
+#  Array of paths user is allowed to interact with
+#
+# [*$prompt]
+#  Shell prompt
+#
+
+class lshell (
+    $allowed_cmds     = [],
+    $allowed_cmd_path = [],
+    $banner           = 'This is a limited shell.',
+    $overssh          = [],
+    $timer            = 86400,
+    $path             = ['/home'],
+    $prompt           = '%u',
+    $exempt_grps      = ['ops'],
+) {
+
+    # allowed_cmd_paths with permissive allowed_cmds
+    # is disingenious as it won't actually restrict
+    if empty($allowed_cmds) and !empty($allowed_cmd_path){
+        fail('allowed_cmd_path will have no effect')
+    }
+
+    package { 'lshell':
+        ensure => present
+    }
+
+    file { '/etc/lshell.conf':
+        content => template('lshell/lshell.conf.erb'),
+        owner   => root,
+        group   => root,
+        mode    => '0444',
+    }
+}
