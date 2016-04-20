dfa6e1ce5235ac40930da7bdb705dce7770d88e2
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-04-20 18:55:49 +0200

    bastionhost::general: include dsh configuration
    Bastionhosts are used by parsoid deployers to restart parsoid machines
    and they use the dsh groups that are maintained in scap::dsh.
    
    Note that this was historically present on the bastions and then has
    been removed somewhow.
    
    Change-Id: Ib8e603cf4f14bb6cd35ffe15deab8c3324b4d832

--- modules/role/manifests/bastionhost/general.pp
+++ modules/role/manifests/bastionhost/general.pp
@@ -9,6 +9,9 @@ class role::bastionhost::general {
     include base::firewall
     include role::backup::host
 
+    # Used by parsoid deployers
+    include scap::dsh
+
     backup::set {'home': }
 
     ferm::service { 'ssh':
@@ -19,4 +22,3 @@ class role::bastionhost::general {
     }
 
 }
-
