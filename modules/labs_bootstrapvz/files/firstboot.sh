a9de2f6d28c2e8e9099e00542137c10e3683444c
andrewbogott <abogott@wikimedia.org>
2016-04-21 13:13:10 -0500

    labs_bootstrapvz: A few tweaks to improve image behavior in testlabs
    - Grab the current domain from the dhcp lease if hostname -d fails
    - Actually set the testlabs puppetmaster if we're in testlabs
    
    Change-Id: If514fdbe50c8051901575101863bd111abaa3928

--- modules/labs_bootstrapvz/files/firstboot.sh
+++ modules/labs_bootstrapvz/files/firstboot.sh
@@ -70,6 +70,17 @@ ip=`curl http://169.254.169.254/1.0/meta-data/local-ipv4 2> /dev/null`
 hostname=`hostname`
 # domain is the last two domain sections, e.g. eqiad.wmflabs
 domain=`hostname -d | sed -r 's/.*\.([^.]+\.[^.]+)$/\1/'`
+
+if [ -z $domain ]; then
+   echo "hostname -d failed, trying to parse dhcp lease"
+   domain=`grep "option domain-name " /var/lib/dhcp/dhclient.eth0.leases | head -n1 | cut -d \" -f2`
+fi
+
+if [ -z $domain ]; then
+    echo "Unable to determine domain; all is lost."
+fi
+
+
 fqdn=${hostname}.${project}.${domain}
 saltfinger="c5:b1:35:45:3e:0a:19:70:aa:5f:3a:cf:bf:a0:61:dd"
 if [ "${domain}" == "eqiad.wmflabs" ]
@@ -79,6 +90,10 @@ fi
 if [ "${domain}" == "codfw.wmflabs" ]
 then
 	master="labs-puppetmaster-codfw.wikimedia.org"
+fi
+if [ "${domain}" == "codfw.labtest" ]
+then
+	master="labtestcontrol2001.wikimedia.org"
 fi
 
 # Finish LDAP configuration
