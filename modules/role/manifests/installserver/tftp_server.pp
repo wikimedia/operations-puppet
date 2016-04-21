0bca0263562eb5ed17b85a877bcfe1aaa5947c4b
dzahn <dzahn@wikimedia.org>
2016-04-21 13:56:53 -0700

    install_server: move tftp role to autoloader layout
    Bug:T132757
    Change-Id: I06c515c761e65d3f4c98d6d24c7c06fdf02e186a

---
+++ modules/role/manifests/installserver/tftp_server.pp
@@ -0,0 +1,31 @@
+# Class: role::install_server::tftp_server
+#
+# A WMF role class used to install all the install_server TFTP stuff
+#
+# Parameters:
+#
+# Actions:
+#       Install and configure all needed software to have an installation server
+#       TFTP server ready
+#
+# Requires:
+#
+#   Class['install-_server::tftp_server']
+#   Class['base::firewall']
+#   Define['ferm::rule']
+#
+# Sample Usage:
+#       include role::installserver::tftp_server
+
+class role::installserver::tftp_server {
+    system::role { 'role::install_server::tftp_server':
+        description => 'WMF TFTP server',
+    }
+
+    include base::firewall
+    include install_server::tftp_server
+
+    ferm::rule { 'tftp':
+        rule => 'proto udp dport tftp { saddr $ALL_NETWORKS ACCEPT; }'
+    }
+}
