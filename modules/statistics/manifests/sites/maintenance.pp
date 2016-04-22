7ee8d48b204733b699f7b8b11b67aa3e6f7cf3cc
elukey <ltoscano@wikimedia.org>
2016-04-22 12:04:36 +0200

    Allow basic apache maintenace webpages for the statistics::web role.
    stat1001 needs to be re-imaged with Debian and it holds important websites
    for the Analytics team. A connect timeout is not a good user experience so
    a basic sorry/maintenance page for each of the websites is needed. The goal
    is to add the new maintenance role to another apache based host under
    cache::misc only for the time needed by the reimage.
    
    Bug: T76348
    Change-Id: Id989431c1dac0920b98e4beabc146974dc712802

---
+++ modules/statistics/manifests/sites/maintenance.pp
@@ -0,0 +1,40 @@
+# == Class: statistics::sites::maintenance
+#
+# This role provisions a webserver with a static sorry page
+# meant to be used for Analytics websites under maintenance.
+# Example: OS re-image of the server hosting the official
+# websites.
+#
+class statistics::sites::maintenance {
+
+    include ::apache
+    include base::firewall
+
+    $docroot  = '/srv/maintenance'
+
+    file { '/sr/analytics':
+        ensure => 'directory',
+        purge  => true,
+    }
+
+    file { '/sr/analytics/maintenance':
+        ensure => 'directory',
+        purge  => true,
+    }
+
+    git::clone { 'analytics/websites_maintenance':
+        ensure    => latest,
+        directory => '/srv/analytics/maintenance',
+    }
+
+    # A single vhost containing multiple ServerAliases will be created
+    # for datasets.w.o, metrics.w.o, stats.w.o
+    apache::site { 'analytics_maintenance':
+        content => template('statistics/analytics_maintenance_vhost.erb'),
+    }
+
+    ferm::service { 'analytics_http':
+        proto => 'tcp',
+        port  => '80',
+    }
+}
