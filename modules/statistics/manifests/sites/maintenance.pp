086930bb231131d3b4879f92b3b2f026712093fd
elukey <ltoscano@wikimedia.org>
2016-04-22 12:04:36 +0200

    Add a simple class to the statistics module to allow basic apache maintenace.
    stat1001 needs to be re-imaged with Debian and it holds important websites
    for the Analytics team. A connect timeout is not a good user experience so
    a basic sorry/maintenance page for each of the websites is needed.
    
    Bug: T76348
    Change-Id: Id989431c1dac0920b98e4beabc146974dc712802

---
+++ modules/statistics/manifests/sites/maintenance.pp
@@ -0,0 +1,40 @@
+# == Class: role::analytics_maintenance::websites
+#
+# This role provisions a webserver with a static sorry page
+# meant to be used for Analytics websites under maintenance.
+# Example: OS re-image of the server hosting the official
+# websites.
+#
+class statistics::sites::maintenance {
+
+    include ::apache
+    include base::firewall
+
+    $repo_dir = '/srvhttps://httpd.apache.org/docs/current/mod/mod_proxy.html#request-bodies/analytics/maintenance'
+    $docroot  = "${repo_dir}"
+
+    git::clone { 'analytics/websites_maintenance':
+        ensure    => latest,
+        directory => $repo_dir,
+    }
+
+    apache::site { 'datasets.wikimedia.org':
+        ensure  => present,
+        content => template('statistics/maintenance.erb'),
+    }
+
+    apache::site { 'metrics.wikimedia.org':
+        ensure  => present,
+        content => template('statistics/maintenance.erb'),
+    }
+
+    apache::site { 'stats.wikimedia.org':
+        ensure  => present,
+        content => template('statistics/maintenance.erb'),
+    }
+
+    ferm::service { 'analytics_http':
+        proto => 'tcp',
+        port  => '80',
+    }
+}
