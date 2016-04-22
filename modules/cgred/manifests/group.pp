97844aea10d14658daa29a9f6770a0fa908841d2
cpettet <rush@wikimedia.org>
2016-04-22 10:11:01 -0500

    labs: setting up to use cgrules engine
    Bug: T131541
    Change-Id: If0610ebe28d6af7beaa7c99339ed6f1a50b4f1ad

---
+++ modules/cgred/manifests/group.pp
@@ -0,0 +1,49 @@
+# Establish a cgroup with parameters and rules to enforce
+#
+# cgroup-lite is a dependency pulled in
+# by cgroup-bin in init.  cgroup-lite
+# takes care of mounting the initial cgroup
+# subsystems upon boot.
+#
+# [*config]
+#  Hash of values for cgroup settings
+#
+#    cgred::group {'mygrouping':
+#        config => {
+#            cpu => {
+#                'cpu.shares' => '1',
+#            },
+#        }
+#        rules  => [
+#            '*:foo.sh subsystem /cgroup',
+#        ]
+#    }
+
+define cgred::group (
+    $ensure = 'present',
+    $config = {},
+    $rules  = [],
+)
+    {
+
+    include cgred
+    file {"/etc/cgconfig.d/${name}.conf":
+        ensure  => $ensure,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => template('cgred/cgconfig.conf.erb'),
+        require => File['/etc/cgconfig.d/'],
+        notify  => Base::Service_unit['cgrulesengd'],
+    }
+
+    file {"/etc/cgrules.d/${name}.conf":
+        ensure  => $ensure,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => template('cgred/cgrules.conf.erb'),
+        require => File['/etc/cgrules.d/'],
+        notify  => Base::Service_unit['cgrulesengd'],
+    }
+}
---
+++ modules/cgred/manifests/init.pp
@@ -0,0 +1,44 @@
+# Establishing cgroups and enforcing them using cgrules engine
+#
+# This module is oriented towards the workflow of defining
+# cgroups and settings by logical unit and using the
+# cgrulesengd to enforce.  The init script for cgrulesengd
+# applies cgroups to this effect.
+#
+
+class cgred {
+
+    package { [
+        'cgroup-bin',
+        'libpam-cgroup']:
+            ensure => present;
+    }
+
+    file { [
+        '/etc/cgconfig.d/',
+        '/etc/cgrules.d/']:
+            ensure => directory,
+            mode   => '0554',
+            owner  => 'root',
+            group  => 'root',
+    }
+
+    file { '/etc/cgrules.d/README':
+        ensure  => present,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        source  => 'puppet:///modules/cgred/cgrules_readme',
+        require => File['/etc/cgrules.d/'],
+    }
+
+    base::service_unit { 'cgrulesengd':
+        ensure          => present,
+        sysvinit        => true,
+        refresh         => true,
+        service_params  => {
+            hasrestart => true,
+        },
+        require         => File['/etc/cgrules.d/'];
+    }
+}
