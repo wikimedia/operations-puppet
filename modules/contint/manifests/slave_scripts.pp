ae61b3e70bea2982715cc3112a692b1bc3502784
Antoine Musso <hashar@free.fr>
2016-04-25 21:21:10 +0200

    contint: decouple slave_scripts and composer
    contint::slave_scripts installed a lot of stuff. With the recent removal
    of phpunit/phpcs, the class ended up:
    
    * creating /srv/deployment/integration
    * installing composer
    * clone slave scripts
    
    Factor out /srv/deployment/integration creation to
    contint::deployment_dir
    
    Move composer installation to contint::composer. Update a toolllabs
    reference.
    
    While at it remove the git::clone ensure => absent. No more needed.
    
    Finally, the sole place where we need composer is on the CI labs slaves,
    thus only add contint::composer on the role::ci::slave::labs.
    
    Bug: T128092
    Change-Id: I4063ff1c1576b2bfc400cdb7a85c6b39ce0e44ca

--- modules/contint/manifests/slave_scripts.pp
+++ modules/contint/manifests/slave_scripts.pp
@@ -1,51 +1,19 @@
+# == Class contint::slave_scripts
+#
+# Scripts and helpers used by CI.
+#
 class contint::slave_scripts {
 
     if $::realm == 'production' {
         fail('contint::slave_scripts must not be used in production. Slaves are already Trebuchet deployment targets.')
     }
 
-    # Hack: faking directories that Trebuchet would normally manage.
-    # The integration project in labs does not use Trebuchet to manage these
-    # packages, but in production we do.
-    if ! defined(File['/srv/deployment']) {
-        file { '/srv/deployment':
-            ensure => 'directory',
-            mode   => '0755',
-        }
-    }
-    if ! defined(File['/srv/deployment/integration']) {
-        file { '/srv/deployment/integration':
-            ensure => 'directory',
-            mode   => '0755',
-        }
-    }
+    require contint::deployment_dir
 
     git::clone { 'jenkins CI slave scripts':
         ensure             => 'latest',
         directory          => '/srv/deployment/integration/slave-scripts',
         origin             => 'https://gerrit.wikimedia.org/r/p/integration/jenkins.git',
         recurse_submodules => true,
-    }
-    git::clone { 'jenkins CI Composer':
-        ensure             => 'latest',
-        directory          => '/srv/deployment/integration/composer',
-        origin             => 'https://gerrit.wikimedia.org/r/p/integration/composer.git',
-        recurse_submodules => true,
-    }
-
-    # Create a symlink for the composer executable
-    file { '/usr/local/bin/composer':
-        ensure => 'link',
-        target => '/srv/deployment/integration/composer/vendor/bin/composer',
-    }
-
-    # We have migrated jobs to use composer instead
-    git::clone { 'jenkins CI phpcs':
-        ensure    => 'absent',
-        directory => '/srv/deployment/integration/phpcs',
-    }
-    git::clone { 'jenkins CI phpunit':
-        ensure    => 'absent',
-        directory => '/srv/deployment/integration/phpunit',
     }
 }
