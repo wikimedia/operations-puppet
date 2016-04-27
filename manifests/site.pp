782db121ef405dc108091ef9da5423b2876ed2d5
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-04-27 18:11:28 +0200

    palladium: add v6 address
    This is needed for allowing palladium to speak to tcpircbot on neon
    
    Change-Id: I567bd9c55380caa230c822dd2ea2b835296cf726

--- manifests/site.pp
+++ manifests/site.pp
@@ -2388,6 +2388,9 @@ node 'palladium.eqiad.wmnet' {
     role ipmi, access_new_install, puppetmaster::frontend, pybal_config
     include standard
     include role::conftool::master
+    interface::add_ip6_mapped { 'main':
+        interface => 'eth0',
+    }
 }
 
 # parser cache databases
