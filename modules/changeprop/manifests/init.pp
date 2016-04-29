220e2736b700f82afeaad28ee7785cf4d9235170
Marko Obrovac <mobrovac@wikimedia.org>
2016-04-29 15:06:42 +0200

    Changeprop: Set hyperswitch as the start-up module
    There was a bug in the service's configuration where the service was
    started using src/app.js. Alas, that file doesn't even exist. Change
    propagation uses hyperswitch as its load module, so use it.
    
    Also, update the config as v0.2.0's config is not compatible with the
    old one.
    
    Change-Id: Iaea347eb56448e36fc7372f839aee41d7bb0f368

--- modules/changeprop/manifests/init.pp
+++ modules/changeprop/manifests/init.pp
@@ -11,13 +11,18 @@
 #   The URI (host:port) of the Zookeeper broker(s) controlling the Kafka cluster
 #   from which to receive the events.
 #
+# [*mwapi_uri*]
+#   The full URI of the MW API contact point. Default:
+#   http://api.svc.${::mw_primary}.wmnet/w/api.php
 class changeprop(
     $zk_uri,
+    $mwapi_uri = "http://api.svc.${::mw_primary}.wmnet/w/api.php",
 ) {
 
     service::node { 'changeprop':
         port            => 7272,
         config          => template('changeprop/config.yaml.erb'),
+        starter_module  => 'hyperswitch',
         healthcheck_url => '',
         has_spec        => true,
         deployment      => 'scap3',
