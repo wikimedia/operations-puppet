6bb60fa83d96efe92ce364ceeea5104a6e00a5e5
Stanislav Malyshev <smalyshev@gmail.com>
2016-05-02 17:53:17 -0700

    [WIP] Add configs for kafka-watcher tool
    Change-Id: Ib387e247b3913eeddd53d511a73383ae433dd8eb
    Bug: T97562

---
+++ modules/kafkawatcher/manifests/init.pp
@@ -0,0 +1,34 @@
+# = Class: kafkawatcher
+#
+# == Parameters:
+# - $username: Username owning the service
+# - $package_dir:  Directory where the service should be installed.
+class kafkawatcher(
+    $username = 'kafkawatcher',
+    $package_dir = '/srv/deployment/wikimedia/kafkawatcher',
+    ) {
+
+        require_package('python-kafka', 'python-pymemcache', 'python-yaml')
+        
+        group { $username:
+            ensure => present,
+            system => true,
+        }
+
+        user { $username:
+            ensure     => present,
+            name       => $username,
+            comment    => 'KafkaWatcher user',
+            forcelocal => true,
+            system     => true,
+            home       => $package_dir,
+            managehome => no,
+        }
+        
+        file { '/etc/kafkawatcher':
+            ensure  => directory,
+            mode    => '0755',
+            owner   => root,
+            group   => root,
+        }
+}
