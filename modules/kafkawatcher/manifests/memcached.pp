16702a4b590719aa4c46c552ea91e2b99d99b908
Stanislav Malyshev <smalyshev@gmail.com>
2016-05-02 17:53:17 -0700

    [WIP] Add configs for kafka-watcher tool
    Change-Id: Ib387e247b3913eeddd53d511a73383ae433dd8eb
    Bug: T97562

---
+++ modules/kafkawatcher/manifests/memcached.pp
@@ -0,0 +1,30 @@
+# = Class: kafkawatcher::memcached
+# Watching memcache events
+class kafkawatcher::memcached(
+    $kafka_server,
+    $memcache_topic,
+    $username = $::kafkawatcher::username,
+    $package_dir = $::kafkawatcher::package_dir,
+    ) {
+        # Service configuration
+        file { '/etc/kafkawatcher/memcache.yaml':
+          ensure  => present,
+          content => template('kafkawatcher/memcache.yaml.erb'),
+          mode    => '0444',
+          owner   => root,
+          group   => root,
+          notify  => Service['kafka-watcher-memcache'],
+          require => File['/etc/kafkawatcher'],
+        }
+
+        # The system service
+        base::service_unit { 'kafka-watcher-memcache':
+          template_name  => 'kafka-watcher-memcache',
+          systemd        => true,
+          upstart        => true,
+          service_params => {
+            enable => true,
+          },
+          require        => File['/etc/kafkawatcher/memcache.yaml'],
+        }
+}
