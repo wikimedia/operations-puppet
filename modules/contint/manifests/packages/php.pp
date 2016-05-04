ade938cca333bafb9d1856ed892a2f9144764a39
Antoine Musso <hashar@free.fr>
2016-05-04 15:10:23 +0200

    contint: drop libcurl4-gnutls-dev
    It conflicts with HHVM build dependency libcurl4-openssl-dev.  We have
    added the gnutls flavor to build pycurl which nowadays build just fine
    with OpenSSL.
    
    The conflict caused the libraries to be uninstalled/installed over and
    over again.
    
    Ensure libcurl4-gnutls-dev is absent from the contint::packages::php
    class which install the HHVM build dependencies.
    
    Bug: T134378
    Change-Id: I1e1bcc36bc9e369a4d5861916ea0851c0d9b4eda

--- modules/contint/manifests/packages/php.pp
+++ modules/contint/manifests/packages/php.pp
@@ -29,6 +29,12 @@ class contint::packages::php {
         }
     }
 
+    package { 'libcurl4-gnutls-dev':
+        # Conflict with HHVM build dependency libcurl4-openssl-dev.
+        # Was For pycurl which now build with openssl just fine.
+        ensure =>  absent,
+    }
+
     if os_version('ubuntu >= trusty || debian >= jessie') {
         exec { '/usr/bin/apt-get -y build-dep hhvm':
             onlyif => '/usr/bin/apt-get -s build-dep hhvm | /bin/grep -Pq "will be (installed|upgraded)"',
