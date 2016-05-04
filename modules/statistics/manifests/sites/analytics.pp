20767ec867c54e2a841e5ae0dfec844bc8a984ab
Andrew Otto <acotto@gmail.com>
2016-05-04 16:02:39 -0400

    Set up analytics.wikimedia.org site on stat1001
    Bug: T132407
    Change-Id: I27cb2da9382a49c27bc8d9c3067ede1ab6c5a228

---
+++ modules/statistics/manifests/sites/analytics.pp
@@ -0,0 +1,29 @@
+# == Class statistics::sites::analytics
+# analytics.wikimedia.org
+#
+# This site will eventually supercede both stats.wikimedia.org
+# and datasets.wikimedia.org.  For now it is used to productionize
+# various frontend dashboards that have historicaly been running in labs.
+#
+# Bug: T132407
+#
+class statistics::sites::analytics {
+    require statistics::web
+
+    # /srv/analytics.wikimedia.org
+    $document_root = "${$::statistics::working_path}/analytics.wikimedia.org"
+
+    # Allow statistics-web-users to modify files in this directory.
+    file { $document_root:
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'statistics-web-users',
+        mode   => '0775',
+    }
+
+    include apache::mod::headers
+    apache::site { 'analytics':
+        source  => 'statistics/analytics.wikimedia.org.erb',
+        require => File[$document_root],
+    }
+}
