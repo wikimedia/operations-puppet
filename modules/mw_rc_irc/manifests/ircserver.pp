e0986233409ad729a192905e43b7729de7f03d33
Daniel Zahn <dzahn@wikimedia.org>
2016-05-03 17:51:57 -0700

    ircserver: move ircd.conf to public repo
    Move the ircd.conf into the public repo after removing
    the "auth" and "operator" blocks and putting them in
    separate files in the private repo. Then include them in here.
    
    Like T1234271 says "the config file should be split into a
    public section, checked in to puppet.."
    
    Added the 2 new files, auth.conf and operator.conf in private repo
    under secrets/mw_rc_irc/. Afterwards we can delete misc/ircd.conf there.
    
    labs/private has placeholder files without real secrets too,
    so that we can still compile changes.
    
    https://gerrit.wikimedia.org/r/#/c/287136/
    
    http://puppet-compiler.wmflabs.org/2694/
    
    Bug:T134271
    Change-Id: I3b9fe33d896c15de41900bc069c32e85dd1f7aea

--- modules/mw_rc_irc/manifests/ircserver.pp
+++ modules/mw_rc_irc/manifests/ircserver.pp
@@ -7,17 +7,36 @@ class mw_rc_irc::ircserver {
 
     require_package('ircd-ratbox', 'irssi')
 
-    file {
-        '/usr/etc/ircd.conf':
-            mode   => '0444',
-            owner  => 'irc',
-            group  => 'irc',
-            content => secret('misc/ircd.conf');
-        '/usr/etc/ircd.motd':
-            mode    => '0444',
-            owner   => 'irc',
-            group   => 'irc',
-            content => template('mw_rc_irc/motd.erb');
+    # public part of the ircd config
+    file { '/usr/etc/ircd.conf':
+        mode    => '0444',
+        owner   => 'irc',
+        group   => 'irc',
+        content => template('mw_rc_irc/ircd.conf.erb');
+    }
+
+    # private config block for auth/allowed users
+    file { '/usr/etc/auth.conf':
+        mode    => '0444',
+        owner   => 'irc',
+        group   => 'irc',
+        content => secret('mw_rc_irc/auth.conf');
+    }
+
+    # private config block for operators and their passwords
+    file { '/usr/etc/operator.conf':
+        mode    => '0444',
+        owner   => 'irc',
+        group   => 'irc',
+        content => secret('mw_rc_irc/operator.conf');
+    }
+
+    # message of the day / connect banner
+    file { '/usr/etc/ircd.motd':
+        mode    => '0444',
+        owner   => 'irc',
+        group   => 'irc',
+        content => template('mw_rc_irc/motd.erb');
     }
 
     if os_version('debian >= jessie') {
