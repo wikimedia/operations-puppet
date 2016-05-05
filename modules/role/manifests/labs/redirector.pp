3e226d9dde09af5a1e00896944ccbc64a90bfba4
Bryan Davis <bd808@wikimedia.org>
2016-05-05 17:05:27 -0600

    New role: role::labs::redirector
    Role installs nginx and configures multiple named vhosts which each map
    to a single 301 status redirect.
    
    Bug: T134508
    Change-Id: Ifdcea0a65077e7d5e4e0bb2139ddf9a8bfb43511

---
+++ modules/role/manifests/labs/redirector.pp
@@ -0,0 +1,24 @@
+# Provision nginx as a http redirector
+#
+# Installs nginx and configures multiple named vhosts which each map to a single
+# 301 status redirect. Can be used to setup permanent redirects from Labs web
+# proxies to new locations such as Tool Lab tools.
+#
+# [*default_url*]
+#   Default URL to redirect unconfigured Hosts to. Will emit a 302 redirect
+#   rathter than a 301.
+#
+# [*redirects*]
+#   Hash of (Host, target URL) values
+#
+class role::labs::redirector(
+    $default_url,
+    $redirects,
+) {
+    class { '::nginx':
+        variant => 'light',
+    }
+    nginx::site { 'redirect':
+        content => template('role/labs/redirector/nginx.conf.erb'),
+    }
+}
