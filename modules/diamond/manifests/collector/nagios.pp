a22717af9670ef8ce3fde7e0400c18e6ab210400
Alex Monk <krenair@gmail.com>
2016-05-05 18:19:44 +0100

    [WIP] Diamond collector for nagios plugin return codes
    Bug: T111064
    Change-Id: If09d5a34bc99733dc7fd9e13f2756f7975802d50

---
+++ modules/diamond/manifests/collector/nagios.pp
@@ -0,0 +1,51 @@
+# == Define: diamond::collector::nagios
+#
+# Report the return code of a nagios plugin
+#
+# === Parameters
+#
+#   command: List of arguments to run as a command
+#
+# === Examples
+#
+#   diamond::collector::nagios { 'keyholder_status':
+#       command => [ '/usr/bin/sudo', '/usr/lib/nagios/plugins/check_keyholder' ]
+#   }
+
+define diamond::collector::nagios (
+    $command = undef,
+    $ensure = 'present',
+) {
+    validate_ensure($ensure)
+    if $command == undef {
+        fail('Command must be defined')
+    }
+
+    include ::diamond::collector::nagios_lib
+
+    file { "/etc/diamond/nagios.d/${title}.json":
+        ensure  => $ensure,
+        content => ordered_json({
+            name    => $title,
+            command => $command
+        }),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['diamond']
+    }
+}
+
+# support class, to be include'd multiple times
+class diamond::collector::nagios_lib {
+    diamond::collector { 'Nagios':
+        source   => 'puppet:///modules/diamond/collector/nagios.py'
+    }
+
+    file { '/etc/diamond/nagios.d':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
+}
