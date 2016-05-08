63a32e3f5600511c595b494491c67292c814fc1d
YuviPanda <yuvipanda@gmail.com>
2016-05-09 00:21:24 +0530

    k8s: Allow pod infra image to be overriden with hiera
    Bug: T133873
    Change-Id: If5a007ad180b0b700f9763824d937ff68ac995c6

--- modules/k8s/manifests/kubelet.pp
+++ modules/k8s/manifests/kubelet.pp
@@ -1,6 +1,7 @@
 class k8s::kubelet(
     $master_host,
     $cluster_dns_ip = '192.168.0.100',
+    $pod_infra_container_image = 'gcr.io/google_containers/pause:2.0',
 ) {
     include ::k8s::infrastructure_config
 
