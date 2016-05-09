0d1d724e9ff7d9c8918fdac74f4b9de9489b5900
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-05-09 09:52:41 +0100

    prometheus: add tools role
    Change-Id: Ia5e1c50a64268422b5aabeffdf8ba23ca5f8b247

---
+++ modules/role/manifests/prometheus/tools.pp
@@ -0,0 +1,53 @@
+# == Class: role::prometheus::tools
+#
+# This role provides both project-wide host discovery/monitoring (via cron
+# prometheus-labs-targets) and kubernetes discovery/monitoring via Prometheus'
+# native k8s support.
+
+class role::prometheus::tools {
+    $bearer_token_file = '/srv/prometheus/tools/k8s.token'
+    $targets_file = '/srv/prometheus/tools/targets/node_project.yml'
+
+    prometheus::server { 'tools':
+        listen_address      => '127.0.0.1:9902',
+        scrape_config_extra => [
+          {
+            'job_name'              => 'k8s',
+            'bearer_token_file'     => $bearer_token_file,
+            'kubernetes_sd_configs' => [
+              {
+                'api_servers'       => hiera('k8s::master_host'),
+                'bearer_token_file' => $bearer_token_file,
+              },
+            ]
+          }
+        ]
+    }
+
+    prometheus::web { 'tools':
+        proxy_pass => 'http://localhost:9902/tools',
+    }
+
+    # Ugly hack, ugh! (from modules/toollabs/manifests/kube2proxy.pp)
+    $users = hiera('k8s_users')
+    $client_token = inline_template("<%= @users.select { |u| u['name'] == 'prometheus' }[0]['token'] %>")
+
+    file { $bearer_token_file:
+        ensure  => present,
+        content => $client_token,
+        mode    => '0400',
+        owner   => 'prometheus',
+        group   => 'prometheus',
+        require => Prometheus::Server['tools'],
+    }
+
+    include ::prometheus::scripts
+
+    cron { 'prometheus_tools_project_targets':
+        ensure  => present,
+        command => "/usr/local/bin/prometheus-labs-targets > ${targets_file}.$$ && mv ${targets_file}.$$ ${targets_file}",
+        minute  => '*/10',
+        hour    => '*',
+        user    => 'prometheus',
+    }
+}
