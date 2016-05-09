43159d639d00a3dc0b2ffaaa854aeb03ef083675
cpettet <rush@wikimedia.org>
2016-05-09 15:08:51 -0500

    change up alerting for services within tools in icinga
    * The admin tool that runs the tools.wmflabs.org page is
      flaky and is in a state of disrepair. Still alert but
      don't page people for this atm.  More direct checks are added
      to cover some of the useful canary cases.
    * We have been using the checker.tools.wmflabs cluster from
      catchpoint primarily (though we already have some of these
      checks in icinga like NFS). this adds more of them covering
      cases previously thought to be caught by the general failure
      of the Tools homepage.  these are the checks we use to calculate
      Tools SLA's so having this divorced from icinga has been less
      than ideal.
    * Define the checker.tools.wmflabs.org host
    * Reassign the tools checker tests away from a host
      defined as tools.wmflabs.org.  this is not really true and confusing.
    
    I plan on enabling alerting for '/self' shortly after this merge. In a
    separate commit for revert if needed.
    
    I plan on enabling alerting for these after a grace period
    to verify and fix any stability issues:
    
    - tools-checker-grid-start-trusty
    - tools-checker-grid-start-trusty
    - tools-checker-ldap
    
    Change-Id: I6797bdbd21552fe84c50baf1ee8cb74c1a7cd9d3

--- modules/toollabs/manifests/monitoring/icinga.pp
+++ modules/toollabs/manifests/monitoring/icinga.pp
@@ -1,29 +1,97 @@
-# Monitoring checks that live in icinga and page people
 class toollabs::monitoring::icinga {
-    # Paging checks!
+
+    # this homepage is served by a tool running within tools
+    # itself. on a bastion 'become admin' to see source and
+    # 'webservices restart' if needed.
     @monitoring::host { 'tools.wmflabs.org':
         host_fqdn => 'tools.wmflabs.org',
     }
 
-    monitoring::service { 'tools_main_page':
-        description   => 'tools-home',
+    monitoring::service { 'tools.wmflabs.org':
+        description   => 'tools homepage (admin tool)',
         check_command => 'check_http_slow!20',
         host          => 'tools.wmflabs.org',
-        critical      => true,
     }
 
-    monitoring::service { 'nfs-on-labs-instances':
+    # complex checks via a wsgi app running on a cluster for this purpose.
+    # these checks are used to track uptime and availability via
+    # catchpoint as well as for general alerting and administration.
+    $test_entry_host = 'checker.tools.wmflabs.org'
+    $test_entry_host_ip = ipresolve($ext_check_host,4)
+
+    @monitoring::host { 'checker.tools.wmflabs.org':
+        host_fqdn => $test_entry_host_ip,
+    }
+
+    # tests are pass/fail based on string return check
+    $checker="check_http_url_at_address_for_string!${test_entry_host}"
+
+    monitoring::service { 'tools-checker-self':
+        description   => 'toolschecker service itself needs to return OK',
+        check_command => "${checker}!/self!OK",
+        critical      => true,
+        host          => $test_entry_host,
+    }
+
+    monitoring::service { 'tools-checker-dumps':
+        description   => 'Make sure enwiki dumps are not empty',
+        check_command => "${checker}!/dumps!OK",
+        host          => $test_entry_host,
+    }
+
+    monitoring::service { 'tools-checker-redis':
+        description   => 'Redis set/get',
+        check_command => "${checker}!/redis!OK",
+        host          => $test_entry_host,
+    }
+
+    monitoring::service { 'tools-checker-ldap':
+        description   => 'Test LDAP for query',
+        check_command => "${checker}!/ldap!OK",
+        host          => $test_entry_host,
+    }
+
+    monitoring::service { 'tools-checker-puppetmaster-eqiad':
+        description   => 'Puppet catalogue fetch',
+        check_command => "${checker}!/labs-puppetmaster/eqiad!OK",
+        host          => $test_entry_host,
+    }
+
+    monitoring::service { 'tools-checker-labs-dns-private':
+        description   => 'Verify internal DNS from within Tools',
+        check_command => "${checker}!/labs-dns/private!OK",
+        host          => $test_entry_host,
+    }
+
+    monitoring::service { 'tools-checker-nfs-home':
         description   => 'NFS read/writeable on labs instances',
-        check_command => 'check_http_url_at_address_for_string!checker.tools.wmflabs.org!/nfs/home!OK',
+        check_command => "${checker}!/nfs/home!OK",
         critical      => true,
-        host          => 'tools.wmflabs.org',
+        host          => $test_entry_host,
     }
 
-    monitoring::service { 'nfs-showmount-on-labs-instances':
+    # new instances will block on this for spinup if failing
+    monitoring::service { 'tools-checker-nfs-showmount':
         description   => 'showmount succeeds on a labs instance',
-        check_command => 'check_http_url_at_address_for_string!checker.tools.wmflabs.org!/nfs/showmount!OK',
-        critical      => false,
-        host          => 'tools.wmflabs.org',
+        check_command => "${checker}!/nfs/showmount!OK",
+        host          => $test_entry_host,
     }
 
+    monitoring::service { 'tools-checker-toolscron':
+        description   => 'check mtime mod from tools cron job',
+        check_command => "${checker}!/nfs/showmount!OK",
+        host          => $test_entry_host,
+    }
+
+    monitoring::service { 'tools-checker-grid-start-trusty':
+        description   => 'Start a job and verify on Trusty',
+        check_command => "${checker}!/grid/start/trusty!OK",
+        host          => $test_entry_host,
+    }
+
+    monitoring::service { 'tools-checker-grid-start-precise':
+        description   => 'Start a job and verify on Precise',
+        check_command => "${checker}!/grid/start/precise!OK",
+        host          => $test_entry_host,
+    }
 }
