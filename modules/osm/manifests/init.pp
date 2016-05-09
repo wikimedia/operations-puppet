53977e01be67c549755a622ae074d30b7cb19dd5
Guillaume Lederrey <glederrey@wikimedia.org>
2016-05-09 13:28:44 +0200

    WIP - Upgrade osm2pgsql to 0.90.0
    0.90.0 is available in Jessie backports.
    
    I took eventlogging::service::service as an example of how we pull packages
    from backports.
    
    Still need to check how to validate this version before merging this change.
    
    Bug: T112423
    Change-Id: I13cc0a9566a32cdf8f4ce7a85e22bfdab1c50173

--- modules/osm/manifests/init.pp
+++ modules/osm/manifests/init.pp
@@ -1,10 +1,14 @@
 #
 
-class osm($ensure='present') {
-    package { [
-        'osm2pgsql',
-        'osmosis',
-        ]:
-        ensure => $ensure,
+class osm {
+
+    # Can't use require_package here because we need to specify version
+    # from jessie-backports:
+    if !defined(Package['osm2pgsql']) {
+        package { 'osm2pgsql':
+            ensure => '0.90.0+ds-1~bpo8+1',
+        }
     }
+
+    require_package('osmosis')
 }
