7fbdfbe59bee9b18e000f91fac27b58b098d4c21
Guillaume Lederrey <glederrey@wikimedia.org>
2016-05-09 13:28:44 +0200

    Upgrade osm2pgsql to 0.90.0
    0.90.0 is available in Jessie backports.
    
    I took eventlogging::service::service as an example of how we pull packages
    from backports.
    
    This new version has been tested on new maps2??? servers and show that initial
    import and replication are working fine.
    
    Bug: T112423
    Change-Id: I13cc0a9566a32cdf8f4ce7a85e22bfdab1c50173

--- modules/osm/manifests/init.pp
+++ modules/osm/manifests/init.pp
@@ -1,10 +1,21 @@
 #
 
-class osm($ensure='present') {
-    package { [
-        'osm2pgsql',
-        'osmosis',
-        ]:
-        ensure => $ensure,
+class osm {
+
+    # osm2pgsql 0.90 is only available on jessie at the moment
+    # there is no need for 0.90 on labs machines (precise)
+    $osm2pgsql_ensure = os_version('debian == jessie') ? {
+        true => '0.90.0+ds-1~bpo8+1',
+        default => 'present',
     }
+
+    # Can't use require_package here because we need to specify version
+    # from jessie-backports:
+    if !defined(Package['osm2pgsql']) {
+        package { 'osm2pgsql':
+            ensure => $osm2pgsql_ensure,
+        }
+    }
+
+    require_package('osmosis')
 }
