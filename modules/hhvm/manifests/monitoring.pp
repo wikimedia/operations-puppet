3305930dea4550969036b0901c6ed6cb677f92cd
Antoine Musso <hashar@free.fr>
2016-05-09 22:21:06 +0200

    hhvm: skip ganglia when it is unwanted
    On labs ganglia is no more available.  Drop it from hhvm::monitoring
    when it is known to not be here ( $::standard::has_ganglia ).
    
    See also change to the apache module:
        https://gerrit.wikimedia.org/r/287695
    
    Bug: T134808
    Change-Id: I2bfc4a4364619d33112df270ee4cca127bceff72

--- modules/hhvm/manifests/monitoring.pp
+++ modules/hhvm/manifests/monitoring.pp
@@ -3,48 +3,56 @@
 # Provisions Ganglia metric-gathering modules for HHVM.
 #
 class hhvm::monitoring {
-    include ::ganglia
 
     ## Memory statistics
-
-    file { '/usr/lib/ganglia/python_modules/hhvm_mem.py':
-        source  => 'puppet:///modules/hhvm/monitoring/hhvm_mem.py',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0555',
-        require => Package['ganglia-monitor'],
-    }
-
-    file { '/etc/ganglia/conf.d/hhvm_mem.pyconf':
-        source  => 'puppet:///modules/hhvm/monitoring/hhvm_mem.pyconf',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        require => File['/usr/lib/ganglia/python_modules/hhvm_mem.py'],
-        notify  => Service['ganglia-monitor'],
-    }
 
     diamond::collector { 'HhvmApc':
         source  => 'puppet:///modules/hhvm/monitoring/hhvm_apc.py',
     }
 
+    if $::standard::has_ganglia {
+
+        include ::ganglia
+
+        file { '/usr/lib/ganglia/python_modules/hhvm_mem.py':
+            source  => 'puppet:///modules/hhvm/monitoring/hhvm_mem.py',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0555',
+            require => Package['ganglia-monitor'],
+        }
+
+        file { '/etc/ganglia/conf.d/hhvm_mem.pyconf':
+            source  => 'puppet:///modules/hhvm/monitoring/hhvm_mem.pyconf',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            require => File['/usr/lib/ganglia/python_modules/hhvm_mem.py'],
+            notify  => Service['ganglia-monitor'],
+        }
+
+    }
 
     ## Health statistics
 
-    file { '/usr/lib/ganglia/python_modules/hhvm_health.py':
-        source  => 'puppet:///modules/hhvm/monitoring/hhvm_health.py',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0555',
-        require => Package['ganglia-monitor'],
-    }
+    if $::standard::has_ganglia {
 
-    file { '/etc/ganglia/conf.d/hhvm_health.pyconf':
-        source  => 'puppet:///modules/hhvm/monitoring/hhvm_health.pyconf',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        require => File['/usr/lib/ganglia/python_modules/hhvm_health.py'],
-        notify  => Service['ganglia-monitor'],
+        file { '/usr/lib/ganglia/python_modules/hhvm_health.py':
+            source  => 'puppet:///modules/hhvm/monitoring/hhvm_health.py',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0555',
+            require => Package['ganglia-monitor'],
+        }
+
+        file { '/etc/ganglia/conf.d/hhvm_health.pyconf':
+            source  => 'puppet:///modules/hhvm/monitoring/hhvm_health.pyconf',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            require => File['/usr/lib/ganglia/python_modules/hhvm_health.py'],
+            notify  => Service['ganglia-monitor'],
+        }
+
     }
 }
