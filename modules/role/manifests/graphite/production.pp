4974b38294c8ed4f53eb5908dd87fce71f0ecf90
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-05-09 13:53:07 +0100

    graphite: export /var/lib/carbon via rsync
    needed to transfer whisper files across machines
    
    Change-Id: Ib48aa8445ec29b16c3c0fb57b7eba426ac22d211

--- modules/role/manifests/graphite/production.pp
+++ modules/role/manifests/graphite/production.pp
@@ -16,16 +16,39 @@ class role::graphite::production {
         }
     }
 
+    $graphite_hosts = [
+        'graphite1001.eqiad.wmnet',
+        'graphite1003.eqiad.wmnet',
+        'graphite2001.eqiad.wmnet',
+        'graphite2002.eqiad.wmnet',
+    ]
+    $graphite_hosts_ferm = join($graphite_hosts, ' ')
+
+    include rsync::server
+
+    rsync::server::module { 'carbon':
+        path        => '/var/lib/carbon',
+        uid         => '_graphite',
+        gid         => '_graphite',
+        hosts_allow => $graphite_hosts,
+    }
+
+    ferm::service { 'rsync-graphite':
+        proto  => 'tcp',
+        port   => '873',
+        srange => "@resolve((${graphite_hosts_ferm}))",
+    }
+
     ferm::service { 'carbon_c_relay-local_relay_udp':
         proto  => 'udp',
         port   => '1903',
-        srange => '@resolve((graphite1001.eqiad.wmnet graphite2001.codfw.wmnet))',
+        srange => "@resolve((${graphite_hosts_ferm}))",
     }
 
     ferm::service { 'carbon_c_relay-local_relay_tcp':
         proto  => 'tcp',
         port   => '1903',
-        srange => '@resolve((graphite1001.eqiad.wmnet graphite2001.codfw.wmnet))',
+        srange => "@resolve((${graphite_hosts_ferm}))",
     }
 }
 
