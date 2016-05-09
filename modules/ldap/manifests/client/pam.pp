9be62cbcca96e695be4418910a7121e6bae64f2a
YuviPanda <yuvipanda@gmail.com>
2016-05-09 18:04:28 +0530

    ldap: Get rid of cleanup-pam-config script
    Is no longer needed
    
    Change-Id: I23ce57b762f1d8851119bb77343b8e28b8037e90

--- modules/ldap/manifests/client/pam.pp
+++ modules/ldap/manifests/client/pam.pp
@@ -7,13 +7,4 @@ class ldap::client::pam($ldapconfig) {
     security::pam::config { 'wikimedia-labs-pam':
         source => 'puppet:///modules/ldap/wikimedia-labs-pam',
     }
-
-    file { '/usr/local/sbin/cleanup-pam-config':
-        ensure => present,
-        source => 'puppet:///modules/ldap/cleanup-pam-config',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
-    }
-
 }
