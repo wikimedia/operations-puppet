33e28354fbc40aa75e94db6b97a915de35fecf00
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-05-10 16:43:40 -0400

    jupyterhub: Add module to set up Jupyterhub for paws-internal
    Change-Id: I33104206fe75d817531c23f10e4ccf225da81fa0

---
+++ modules/role/manifests/jupyterhub/paws_internal.pp
@@ -0,0 +1,9 @@
+# == Class role::paws_internal
+# Role for PAWS Internal (Jupyterhub service running on analytics cluster)
+class role::paws_internal::jupyterhub {
+
+    class jupyterhub::base {
+        $base_dir         => '/srv/paws-internal',
+        $wheels_repo_url  => 'https://gerrit.wikimedia.org/r/p/operations/wheels/paws-internal.git',
+    }
+}
