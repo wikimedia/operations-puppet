wikimedia/operations-puppet

commit ebe965885e38da39d50be7cd882173a6777cd00f

Workaround for mdadm boot-time race condition
The boot-time race condition assembling raid devices reported in T131961
can be avoided by sleeping a few seconds in initramfs-tools' local-top
stage, after which the root device is expected to be present.

See FAQ 26. in /usr/share/doc/mdadm/FAQ.gz.

This patch introduces a new puppet module called initramfs, allowing to
define initramfs hooks and scripts.

Bug: T131961
Change-Id: I12db18dee30c67f98f7dd567e7527d43c134c5a7

modules/base/manifests/initramfs.pp
@@ -1,0 +1,6 @@
+class base::initramfs($localtop_sleep = '5s') {
+    initramfs::script { 'mdadm-sleep':
+        boot_stage => 'local-top',
+        content    => template('base/initramfs_sleep.erb'),
+    }
+}

modules/base/templates/initramfs_sleep.erb
@@ -1,0 +1,11 @@
+#!/bin/sh
+#
+# Workaround for boot-time race condition assembling raid arrays:
+# https://phabricator.wikimedia.org/T131961
+#
+# This file is managed by Puppet.
+
+echo "Waiting <%= @localtop_sleep -%> for disks to show up (T131961)"
+sleep <%= @localtop_sleep -%>
+
+exit 0

modules/base/manifests/init.pp
@@ -34,6 +34,10 @@ class base {
     include base::kernel
     include base::debdeploy
 
+    if os_version('debian >= jessie') {
+        include base::initramfs
+    }
+
     # include base::monitor::host.
     # if contactgroups is set, then use it
     # as the monitor host's contact group.

modules/initramfs/manifests/hook.pp
@@ -1,0 +1,11 @@
+define initramfs::hook($content='') {
+    include initramfs
+
+    file { "/etc/initramfs-tools/hooks/${title}":
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0544',
+        content => $content,
+        notify  => Exec['update-initramfs'],
+    }
+}

modules/initramfs/manifests/init.pp
@@ -1,0 +1,12 @@
+class initramfs {
+    package { 'initramfs-tools':
+        ensure => installed,
+    }
+
+    exec { 'update-initramfs':
+        refreshonly => true,
+        command     => 'update-initramfs -u -k all',
+        path        => '/bin:/usr/bin:/sbin:/usr/sbin',
+        require     => Package['initramfs-tools'],
+    }
+}

modules/initramfs/manifests/script.pp
@@ -1,0 +1,16 @@
+define initramfs::script($boot_stage='', $content='') {
+    include initramfs
+
+    case $boot_stage {
+        'init-bottom', 'init-premount', 'init-top', 'local-bottom', 'local-premount', 'local-top', 'nfs-bottom', 'nfs-premount', 'nfs-top', 'panic': {}
+        default: { fail("Unsupported initramfs stage: ${boot_stage}") }
+    }
+
+    file { "/etc/initramfs-tools/scripts/${boot_stage}/${title}":
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0544',
+        content => $content,
+        notify  => Exec['update-initramfs'],
+    }
+}

commit 8b96b73953c7302ac3c1c499b90e4de3ef337618

mdadm boot-time race condition: sleep in init-premount
The change introduced in ebe9658 does not seem to fix T131961.

As mentioned in https://phabricator.wikimedia.org/T131961#2274674, this
is probably due to the fact that systems could attempt mounting root
before the local-top boot stage. Sleep in init-premount instead.

Bug: T131961
Change-Id: I08a07d375971bde53b5b621a1712f2031591e4cc

modules/base/manifests/initramfs.pp
@@ -1,6 +1,6 @@
-class base::initramfs($localtop_sleep = '5s') {
+class base::initramfs($sleep = '5s') {
     initramfs::script { 'mdadm-sleep':
-        boot_stage => 'local-top',
+        boot_stage => 'init-premount',
         content    => template('base/initramfs_sleep.erb'),
     }
 }

modules/base/templates/initramfs_sleep.erb
@@ -5,7 +5,7 @@
 #
 # This file is managed by Puppet.
 
-echo "Waiting <%= @localtop_sleep -%> for disks to show up (T131961)"
-sleep <%= @localtop_sleep -%>
+echo "Waiting <%= @sleep -%> for disks to show up (T131961)"
+sleep <%= @sleep -%>
 
 exit 0