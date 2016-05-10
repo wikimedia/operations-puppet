5dc9f98dcce88073a6042390cf1153dcd8472b6b
Andrew Otto <acotto@gmail.com>
2016-05-10 17:18:58 -0400

    Druid module and analytics_cluster role class
    Bug: T131974
    Change-Id: I65cb169ac2701e754cd6a68e31903bcb25644290

---
+++ modules/druid/manifests/historical.pp
@@ -0,0 +1,98 @@
+# == Class druid::historical
+# Configures and runs a Druid Historical Node.
+# http://druid.io/docs/latest/design/historical.html
+#
+# == Parameters
+#
+# [*properties*]
+#   Hash of runtime.properties
+#   See: Default $properties
+#
+# [*env*]
+#   Hash of shell environment variables.
+#   Default: {
+#       'JMX_PORT'             => 9663,
+#       'DRUID_HEAP_OPTS'      => '-Xmx256m -Xms256m',
+#       'DRUID_EXTRA_JVM_OPTS' => '-XX:MaxDirectMemorySize=256m',
+#   }
+#
+# [*should_subscribe*]
+#   True if the service should refresh if any of its config files change.
+#   Default: false
+#
+# === Default $properties
+#
+# The properties listed here are only the defaults.
+# For a full list of configuration properties, see
+# http://druid.io/docs/latest/configuration/historical.html
+#
+# [*druid.port*]
+#   Default: 8083
+#
+# [*druid.server.http.numThreads*]
+#   Number of threads for HTTP requests. Default: 4
+#
+# [*druid.processing.buffer.sizeBytes*]
+#   This specifies a buffer size for the storage of intermediate results. The
+#   computation engine in both the Historical and Realtime nodes will use a
+#   scratch buffer of this size to do all of their intermediate computations
+#   off-heap. Larger values allow for more aggregations in a single pass over
+#   the data while smaller values can require more passes depending on the
+#   query that is being executed. Default: 134217728,
+#
+# [*druid.processing.numThreads*]
+#   The number of processing threads to have available for parallel processing
+#   of segments. Default: 1
+#
+#
+# [*druid.segmentCache.locations*]
+#   Segments assigned to a Historical node are first stored on the local file
+#   system (in a disk cache) and then served by the Historical node. These
+#   locations define where that local cache resides.
+#   Default: '[{"path":"/var/lib/druid/segment-cache","maxSize"\:2147483648}]'
+#
+# [*druid.server.maxSize*]
+#   The maximum number of bytes-worth of segments that the node wants assigned
+#   to it. This is not a limit that Historical nodes actually enforces, just a
+#   value published to the Coordinator node so it can plan accordingly.
+#   Default: 2147483648,
+#
+class druid::historical(
+    $properties       = {},
+    $env              = {},
+    $should_subscribe = false,
+)
+{
+    require druid
+
+    $default_properties = {
+        'druid.port'                        => 8083,
+        # HTTP server threads
+        'druid.server.http.numThreads'      => 4,
+
+        # Processing threads and buffers
+        'druid.processing.buffer.sizeBytes' => 134217728,
+        'druid.processing.numThreads'       => 1,
+
+        # Segment storage
+        'druid.segmentCache.locations'      => '[{"path":"/var/lib/druid/segment-cache","maxSize"\:2147483648}]',
+        'druid.server.maxSize'              => 2147483648,
+    }
+
+    $default_env = {
+        'JMX_PORT'             => 9663,
+        'DRUID_HEAP_OPTS'      => '-Xmx256m -Xms256m',
+        'DRUID_EXTRA_JVM_OPTS' => '-XX:MaxDirectMemorySize=256m',
+    }
+
+    # Save these in variables so the properties can be referenced
+    # from outside of this class.
+    $runtime_properties = merge($default_properties, $properties)
+    $environment        = merge($default_env, $env)
+
+    druid::service { 'historical':
+        runtime_properties => $runtime_properties,
+        env                => $environment,
+        should_subscribe   => $should_subscribe,
+    }
+}
---
+++ modules/druid/manifests/init.pp
@@ -0,0 +1,268 @@
+# == Class druid
+#
+# Installs druid-common and configures common runtime properties.
+#
+# This class tries to set convenient defaults based on provided $properties.
+# $properties always directly overrides any of the defaults configured here.
+#
+#
+# === MySQL Metadata Storage Configuration ===
+#
+# The default druid.metadata.storage.type is 'derby'.  If you set this to
+# to 'mysql', this class will configure MySQL defaults for the
+# druid.metadata.storage.connector.* properties.  E.g.
+#
+#   {
+#       'druid.metadata.storage.type'          => 'mysql',
+#       'druid.metadata.storage.connector.host => 'mysql.server.org',
+#   }
+#
+# By setting storage.type to mysql, the mysql-metadata-storage extension
+# will be included by default.  If you need to override any
+# of these defaults, you may provide them in $properties directly.
+#
+# Since the database name is not directly a Druid property, you must
+# provide a full druid.metadata.storage.connector.connectURI to override that.
+# The default MySQL db name is 'druid'.
+#
+# NOTE that this class will not create the database or grant user permissions
+# for you.  You must do that manually.
+# See: http://druid.io/docs/0.9.0/development/extensions-core/mysql.html for
+# instructions.
+#
+# Likely, you will just need to:
+#
+#   CREATE DATABASE druid DEFAULT CHARACTER SET utf8;
+#   GRANT ALL ON druid.* TO 'druid'@'%' IDENTIFIED BY 'druid';
+#
+#
+# === Deep Storage Configuration ===
+#
+# By default deep storage is disabled.  Set druid.metadata.storage.type to
+# 'local' to configure local deep storage in /var/lib/druid/deep-storage.
+#
+# Set druid.metadata.storage.type to 'hdfs' to configure HDFS based deep
+# storage in /user/druid/deep-storage.  Make sure that a Hadoop client
+# is configured on this node, and that the /user/druid/deep-storage
+# directory exists.  The druid-hdfs-storage extension will be included by
+# default when druid.metadata.storage.type is 'hdfs'.
+#
+#
+# == Parameters
+#
+# [*properties*]
+#   Hash of runtime.properties
+#   See: Default $properties
+#
+# [*use_cdh*]
+#   If this is true, the druid::cdh::hadoop::dependencies class
+#   will be included, and deep storage will be configured to use
+#   a special druid-hdfs-storage-cdh extension.  If you set this,
+#   make sure to include the druid::cdh::hadoop::setup class on your
+#   Hadoop NameNodes.  Default: false.
+#
+#
+# === Default $properties
+#
+# The properties listed here are only the defaults.
+# For a full list of configuration properties, see
+# http://druid.io/docs/0.9.0/configuration/index.html
+#
+# [*druid.extensions.directory*]
+#   Druid extensions are installed here.  Only extensions listed in
+#   druid.extensions.loadList will be automatically loaded into the classpath.
+#   Default: /usr/share/druid/extensions
+#
+# [*druid.extensions.loadList*]
+#   List extensions to load.  Directories matching these names must exist
+#   in druid.extensions.directory.
+#   Default: [
+#       'druid-histogram',
+#       'druid-datasketches',
+#       'druid-namespace-lookup'
+#       'mysql-metadata-storage', # only if druid.metadata.storage.type == mysql
+#       'druid-hdfs-storage',     # only if druid.storage.type == hdfs
+#   ],
+#
+# [*druid.extensions.hadoopDependenciesDir*]
+#   If you have a different version of Hadoop, place your Hadoop client jar
+#   files in your hadoop-dependencies directory and uncomment the line below to
+#   point to your directory.  Or you may manually include them in
+#   DRUID_CLASSPATH.
+#   Default: /usr/share/druid/hadoop-dependencies
+#
+# [*druid.startup.logging.logProperties*]
+#   Log all runtime properties on startup. Disable to avoid logging properties
+#   on startup. Default: true
+#
+# [*druid.zk.service.host*]
+#   Zookeeper hostnames. Default: localhost:2181
+#
+# [*druid.zk.paths.base*]
+#   Chroot to druid in zookeeper. Default: /druid
+#
+# [*druid.metadata.storage.type*]
+#   For Derby server on your Druid Coordinator (only viable in a cluster with
+#   single Coordinator, no fail-over).  Default: derby
+#
+# [*druid.metadata.storage.connector.connectURI*]
+#   Default: jdbc:derby://localhost:1527/var/lib/druid/metadata.db;create=true
+#
+# [*druid.metadata.storage.connector.host*]
+#   Default: localhost
+#
+# [*druid.metadata.storage.connector.port*]
+#   Default: 1527
+#
+# [*druid.storage.type*]
+#   Default: local
+#
+# [*druid.storage.storageDirectory*]
+#   Directory to use as deep storage.  Default: /var/lib/druid/deep-storage
+#
+# [*druid.indexer.logs.type*]
+#   This property must be set for both overlord and middlemanager, hence
+#   it is present in common.runtime.properties.
+#   Default: file
+#
+# [*druid.indexer.logs.directory*]
+#   This property must be set for both overlord and middlemanager, hence
+#   it is present in common.runtime.properties.
+#   Default: /var/lib/druid/indexing-logs
+#
+# [*druid.monitoring.monitors*]
+#   Default: ["com.metamx.metrics.JvmMonitor"]
+#
+# [*druid.emitter*]
+#   Default: logging
+#
+# [*druid.emitter.logging.logLevel*]
+#   Default: info
+#
+class druid(
+    $properties = {},
+    $use_cdh = false,
+)
+{
+    if $use_cdh {
+        # Create links to CDH Hadoop Client dependencies.
+        include druid::cdh::hadoop::dependencies
+    }
+
+    # If metadata storage is in MySQL, set some nice defaults.  Note that
+    # these can still be overridden by setting them in $properties.
+    if $properties['druid.metadata.storage.type'] == 'mysql' {
+        $default_metadata_properties = {
+            'druid.metadata.storage.type'       => 'mysql',
+            'druid.metadata.storage.connector.user'       => 'druid',
+            'druid.metadata.storage.connector.password'   => 'druid',
+            'druid.metadata.storage.connector.host'       => 'localhost',
+            'druid.metadata.storage.connector.port'       => 3306,
+            # Let's be nice and set connectURI based on passed in properties, or
+            # the defaults here, so things like host and port don't have to be
+            # passed in more than once.  Note that you'll have to override
+            # this if you want to change the database name.
+            'druid.metadata.storage.connector.connectURI' => inline_template(
+                'jdbc:mysql://<%= @properties.fetch("druid.metadata.storage.connector.host", "localhost") %>:<%= @properties.fetch("druid.metadata.storage.connector.port", "3306") %>/druid'
+            )
+        }
+        # Set this variable so it is included in the union
+        # for $extensions below.
+        $metadata_extensions = ['mysql-metadata-storage']
+    }
+    # Default to using derby for metadata storage.
+    else {
+        $default_metadata_properties = {
+            'druid.metadata.storage.type'                 => 'derby',
+            'druid.metadata.storage.connector.host'       => 'localhost',
+            'druid.metadata.storage.connector.port'       => 1527,
+            # Let's be nice and set connectURI based on passed in properties, or
+            # the defaults here, so things like host and port don't have to be
+            # passed in more than once.  Note that you'll have to override
+            # this if you want to change the path to the derby database file.
+            'druid.metadata.storage.connector.connectURI' => inline_template(
+                'jdbc:derby://<%= @properties.fetch("druid.metadata.storage.connector.host", "localhost") %>:<%= @properties.fetch("druid.metadata.storage.connector.port", "1527") %>/var/lib/druid/metadata.db;create=true'
+            )
+        }
+        # No extra metadata extensions needed
+        $metadata_extensions         = []
+    }
+
+    # If deep storage is HDFS, then default to storing it in
+    # /user/druid/deep-storage.  Make sure this diretory exists in HDFS
+    # and is writable by druid!  Note that these can still be overridden by
+    # setting them in $properties.
+    if $properties['druid.storage.type'] == 'hdfs' {
+        # If using CDH, then use special CDH settings and dependencies.
+        if $use_cdh {
+            $default_deep_storage_properties = {
+                # Make sure these directories exists in HDFS by including
+                # druid::cdh::hadoop::setup on your Hadoop NameNodes.
+                'druid.storage.storageDirectory'       => '/user/druid/deep-storage',
+            }
+            # Load the special druid-hdfs-storage-cdh extension created by
+            # the druid::cdh::hadoop::dependencies class.
+            $storage_extensions = ['druid-hdfs-storage-cdh']
+        }
+        # Else just use the hadoop dependencies shipped by the druid package
+        else {
+            $storage_extensions = ['druid-hdfs-storage']
+        }
+    }
+    # Else use local deep storage defaulting storageDirectory to
+    # /var/lib/druid/deep-storage.
+    elsif $properties['druid.storage.type'] == 'local' or $properties['druid.storage.type'] == undef {
+        $default_deep_storage_properties = {
+            'druid.storage.type'             => 'local',
+            'druid.storage.storageDirectory' => '/var/lib/druid/deep-storage',
+        }
+        # No extra storage extensions needed
+        $storage_extensions              = []
+    }
+
+
+    $default_extensions = [
+        'druid-histogram',
+        'druid-datasketches',
+        'druid-namespace-lookup'
+    ]
+    # Get a unique list of extensions to load built up from
+    # the defaults configured here.  Note that if
+    # druid.extensions.loadList is set in $properties
+    # it will override any of these.
+    $extensions = sort(union(
+        $default_extensions,
+        $metadata_extensions,
+        $storage_extensions
+    ))
+
+    $default_properties = {
+        'druid.indexer.logs.type'                     => 'file',
+        'druid.indexer.logs.directory'                => '/var/lib/druid/indexing-logs',
+        'druid.extensions.directory'                  => '/usr/share/druid/extensions',
+        'druid.extensions.loadList'                   => $extensions,
+        'druid.extensions.hadoopDependenciesDir'      => '/usr/share/druid/hadoop-dependencies',
+        'druid.startup.logging.logProperties'         => true,
+        'druid.zk.service.host'                       => 'localhost:2181',
+        'druid.zk.paths.base'                         => '/druid',
+        'druid.monitoring.monitors'                   => ['com.metamx.metrics.JvmMonitor'],
+        'druid.emitter'                               => 'logging',
+        'druid.emitter.logging.logLevel'              => 'info',
+    }
+
+    # Finally, make a good list of properties with nice defaults for different
+    # metadata and deep storage types, loading extensions appropriately.
+    # This will be rendered into common.runtime.properties.
+    $runtime_properties = merge(
+        $default_properties,
+        $default_metadata_properties,
+        $default_deep_storage_properties,
+        $properties
+    )
+
+    require_package('druid-common')
+
+    file { '/etc/druid/common.runtime.properties':
+        content => template('druid/runtime.properties.erb')
+    }
+}
