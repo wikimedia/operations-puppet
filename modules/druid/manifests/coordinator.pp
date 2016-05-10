12ba54b0c623a18f51caae6ffa6070fce4a50527
Andrew Otto <acotto@gmail.com>
2016-05-10 17:18:58 -0400

    Druid module and analytics_cluster role class
    Bug: T131974
    Change-Id: I65cb169ac2701e754cd6a68e31903bcb25644290

---
+++ modules/druid/manifests/coordinator.pp
@@ -0,0 +1,77 @@
+# == Class druid::coordinator
+# Configures and runs a Druid Coordinator.
+# http://druid.io/docs/latest/design/coordinator.html
+#
+# == Parameters
+#
+# [*properties*]
+#   Hash of runtime.properties
+#   See: Default $properties
+#
+# [*env*]
+#   Hash of shell environment variables.
+#   Default: {
+#       'JMX_PORT'             => 9662,
+#       'DRUID_HEAP_OPTS'      => '-Xmx128m -Xms128m',
+#       'DRUID_EXTRA_JVM_OPTS' => '-Dderby.stream.error.file=/var/log/druid/coordinator-derby.log',
+#   }
+#
+# [*should_subscribe*]
+#   True if the service should refresh if any of its config files change.
+#   Default: false
+#
+# === Default $properties
+#
+# The properties listed here are only the defaults.
+# For a full list of configuration properties, see
+# http://druid.io/docs/latest/configuration/coordinator.html
+#
+# [*druid.port*]
+#   Default: 8081
+#
+# [*druid.coordinator.startDelay*]
+#   The operation of the Coordinator works on the assumption that it has an
+#   up-to-date view of the state of the world when it runs.  The current ZK
+#   interaction code, however, is written in a way that doesn’t allow the
+#   Coordinator to know for a fact that it’s done loading the current state of
+#   the world. This delay is a hack to give it enough time to believe that it
+#   has all the data. Default: PT30S
+#
+# [*druid.coordinator.period*]
+#   The run period for the coordinator. The coordinator’s operates by
+#   maintaining the current state of the world in memory and periodically
+#   looking at the set of segments available and segments being served to make
+#   decisions about whether any changes need to be made to the data topology.
+#   This property sets the delay between each of these runs. Default: PT30S
+#
+class druid::coordinator(
+    $properties       = {},
+    $env              = {},
+    $should_subscribe = false,
+)
+{
+    require druid
+
+    $default_properties = {
+        'druid.port'                   => 8081,
+        'druid.coordinator.startDelay' => 'PT30S',
+        'druid.coordinator.period'     => 'PT30S',
+    }
+
+    $default_env = {
+        'JMX_PORT'             => 9662,
+        'DRUID_HEAP_OPTS'      => '-Xmx128m -Xms128m',
+        'DRUID_EXTRA_JVM_OPTS' => '-Dderby.stream.error.file=/var/log/druid/coordinator-derby.log',
+    }
+
+    # Save these in variables so the properties can be referenced
+    # from outside of this class.
+    $runtime_properties = merge($default_properties, $properties)
+    $environment        = merge($default_env, $env)
+
+    druid::service { 'coordinator':
+        runtime_properties => $runtime_properties,
+        env                => $environment,
+        should_subscribe   => $should_subscribe,
+    }
+}
---
+++ modules/druid/manifests/service.pp
@@ -0,0 +1,94 @@
+# == Define druid::service
+# Abstraction to ease standing up Druid services.
+#
+# Each Druid service consists of
+# /etc/druid/$service/{env.sh,log4j2.xml,runtime.properties}.
+# This define renders each of those files, and then starts the service.
+#
+# == Parameters
+#
+# [*runtime_properties*]
+#   Hash of properties to render in the runtime.properties file.
+#
+# [*service*]
+#   Name of the service.  This defaults to $title.
+#
+# [*env*]
+#   Hash of shell environment variables to render in env.sh
+#
+# [*enable*]
+#   True if the service should be started, false otherwise.
+#   Default: true
+#
+# [*should_subscribe*]
+#   True if the service should refresh if any of its config files change.
+#   Default: false
+#
+define druid::service(
+    $runtime_properties,
+    $service          = $title,
+    $env              = undef,
+    $enable           = true,
+    $should_subscribe = false,
+)
+{
+    file { "/etc/druid/${service}/runtime.properties":
+        content => template('druid/runtime.properties.erb'),
+    }
+
+    $env_ensure = $env ? {
+        undef   => 'absent',
+        default => 'present',
+    }
+
+    file { "/etc/druid/${service}/env.sh":
+        ensure => $env_ensure,
+        content => template('druid/env.sh.erb'),
+    }
+
+    file { "/etc/druid/${service}/log4j2.xml":
+        content => template('druid/log4j2.xml.erb'),
+    }
+
+    service { "druid-${service}":
+        ensure      => ensure_service($enable),
+        enable      => $enable,
+        hasrestart  => true,
+    }
+
+    # Subscribe the Service its config files if $should_subscribe.
+    if $should_subscribe {
+        Class['::druid']                                 ~> Service["druid-${service}"]
+        File["/etc/druid/${service}/runtime.properties"] ~> Service["druid-${service}"]
+        File["/etc/druid/${service}/env.sh"]             ~> Service["druid-${service}"]
+        File["/etc/druid/${service}/log4j2.xml"]         ~> Service["druid-${service}"]
+    }
+    # Else just make the Service require its config files.
+    else {
+        Class['::druid']                                 -> Service["druid-${service}"]
+        File["/etc/druid/${service}/runtime.properties"] -> Service["druid-${service}"]
+        File["/etc/druid/${service}/env.sh"]             -> Service["druid-${service}"]
+        File["/etc/druid/${service}/log4j2.xml"]         -> Service["druid-${service}"]
+    }
+
+
+    ferm::service { "druid-${service}":
+        proto  => 'tcp',
+        port   =>  $runtime_properties['druid.port'],
+        srange => '$ALL_NETWORKS',
+    }
+    if $::realm == 'production' {
+        $ensure_monitor_service = $enable ? {
+            false   => 'absent',
+            default => 'present',
+        }
+        nrpe::monitor_service { "druid-${service}":
+            ensure       => $ensure_monitor_service,
+            description  => "Druid ${service}",
+            nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1:1 -C java -a 'io.druid.cli.Main server ${service}'",
+            # TODO: parameterize this,
+            # or move monitoring/ferm into its own class.
+            critical     => false,
+        }
+    }
+}
