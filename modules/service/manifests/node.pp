f2d7dfdb7ea508bfd713eab0571690fc8136f6ed
Marko Obrovac <mobrovac@wikimedia.org>
2016-05-10 09:08:42 +0200

    service::node: Add a convenience script to pretty-tail logs
    Node.JS services' logs are stored as JSON blobs on disk, which makes
    them not-so-easy to follow in real time. This patch adds a convenience
    script that pretty-prints the logs either using bunyan (the default log
    formatter for service-runner-based services) or jq in case the former
    cannot be found.
    
    Change-Id: I5dd30a7513370e6831b816479e762eaef9dcb02d

--- modules/service/manifests/node.pp
+++ modules/service/manifests/node.pp
@@ -237,6 +237,15 @@ define service::node(
             group   => 'root',
             mode    => '0444',
         }
+        # convenience script to pretty-print logs
+        if !defined(File["/usr/bin/tail-${title}"]) {
+            file { "/usr/bin/tail-${title}":
+                content => template('service/node/tail-log.erb'),
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0755'
+            }
+        }
     }
 
     # service init script and activation
@@ -290,4 +299,5 @@ define service::node(
             check_command => "check_http_port_url!${port}!${healthcheck_url}",
         }
     }
+
 }
