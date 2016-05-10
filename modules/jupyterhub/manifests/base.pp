9fb36f836091ac2832622a94dc59a4dbfcdd848b
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-05-10 16:43:40 -0400

    [WIP] jupyterhub: Add module to set up Jupyterhub
    This module is very WIP
    
    Change-Id: I33104206fe75d817531c23f10e4ccf225da81fa0

---
+++ modules/jupyterhub/manifests/base.pp
@@ -0,0 +1,38 @@
+# == Class: jupyterhub::base
+# Base class for setting up JupyterHub - very WIP
+# Pip3 packages to be installed
+# jupyterhub-ldapauthenticator
+# jupyterhub-simplespawner (from github atm)
+# jsonschema
+# zmq?
+# jupyterhub
+# notebook
+
+class jupyterhub::base {
+    ensure_packages([
+                    'npm',
+                    'nodejs-legacy',
+                    'python3-dev',
+                    'build-essential',
+                    'python3-pip',
+                    'libmysqlclient-dev'])
+
+    file { '/etc/jupyterhub':
+        ensure => directory,
+        owner  => 'www-data',
+        group  => 'www-data',
+        mode   => '0775',
+    }
+
+    file { '/etc/jupyterhub/jupyterhub_config.py':
+        ensure  => present,
+        content => template('jupyterhub/jupyterhub_config.py.erb'),
+        owner   => 'www-data',
+        group   => 'www-data',
+        mode    => '0644',
+    }
+
+    base::service_unit { 'jupyterhub':
+        systemd => true,
+    }
+}
---
+++ modules/jupyterhub/manifests/db.pp
@@ -0,0 +1,11 @@
+# == Class: jupyterhub::db
+# Set up mysql server for persisting user information
+class jupyterhub::db {
+
+  class { '::mysql::server':
+      config_hash => {
+          'datadir'      => '/srv/mysql',
+          'bind_address' => '127.0.0.1',
+      },
+  }
+}
