8a7c225531e574c9aa9b086b6db6ac0640fb3e63
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-05-10 16:43:40 -0400

    jupyterhub: Add module to set up Jupyterhub for paws-internal
    See https://wikitech.wikimedia.org/wiki/PAWS/Internal for more
    information.
    
    Bug: T149543
    Change-Id: I33104206fe75d817531c23f10e4ccf225da81fa0

---
+++ modules/role/manifests/paws_internal/jupyterhub.pp
@@ -0,0 +1,11 @@
+# == Class role::paws_internal::jupyterhub
+# Role for setting up PAWS Internal - Jupyterhub service running on analytics cluster
+#
+# See https://wikitech.wikimedia.org/wiki/PAWS/Internal for more info
+class role::paws_internal::jupyterhub {
+
+    class { '::jupyterhub':
+        base_path   => '/srv/paws-internal',
+        wheels_repo => 'operations/wheels/paws-internal',
+    }
+}
