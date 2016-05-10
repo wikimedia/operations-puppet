65c4c109937e460d61e8b0b763fb99cb3e9f5932
YuviPanda <yuvipanda@gmail.com>
2016-05-10 11:38:13 +0530

    tools: 'backup' packages
    Copying what we do for crons which are plain text files
    for binary deb packages makes perfect sense.
    
    I don't actually know how we can recover from this backup,
    but we probably can. I'm selling this well, am I not? :)
    
    Change-Id: I016e3c94cea20bb46e57680150bf4f115913c86f

--- modules/role/manifests/labs/tools/services.pp
+++ modules/role/manifests/labs/tools/services.pp
@@ -5,6 +5,26 @@ class role::labs::tools::services(
         description => 'Tool Labs manifest based services',
     }
 
+    include role::aptly
+    # Backup packages!
+    # FIXME: Find out if we deserve better than this
+    file { '/data/project/.system/aptly':
+        ensure => directory,
+        owner  => 'root',
+        group  => "${::labsproject}.admin",
+        mode   => '0770',
+    }
+
+    file { "/data/project/.system/aptly/${::fqdn}":
+        ensure    => directory,
+        source    => '/srv/packages',
+        owner     => 'root',
+        group     => "${::labsproject}.admin",
+        mode      => '0440',
+        recurse   => true,
+        show_diff => false,
+    }
+
     class { 'toollabs::services':
         active => ($::fqdn == $active_host),
     }
