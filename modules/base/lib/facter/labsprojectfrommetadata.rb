5bce4b3a52328cb0a396a496f11791aebf28bcaa
andrewbogott <abogott@wikimedia.org>
2016-05-12 15:44:13 -0500

    Detect labsproject for labtest instances
    Change-Id: Iac31100ae7736200f5a24310a743be4de3535a24

--- modules/base/lib/facter/labsprojectfrommetadata.rb
+++ modules/base/lib/facter/labsprojectfrommetadata.rb
@@ -7,7 +7,7 @@ require 'facter'
 Facter.add(:labsprojectfrommetadata) do
   setcode do
     domain = Facter::Util::Resolution.exec("hostname -d").chomp
-    if domain.end_with? ".wmflabs"
+    if domain.end_with? ".wmflabs" or domain.end_with? ".labtest"
       # query the nova metadata service at 169.254.169.254
       metadata = Facter::Util::Resolution.exec("curl -f http://169.254.169.254/openstack/2013-10-17/meta_data.json/ 2> /dev/null").chomp
       metadata[/project-id\":\ \"(.*?)\"/, 1]
