baaa8ea1a7ccc1243d64e190e64809d39f192b0d
andrewbogott <abogott@wikimedia.org>
2016-05-12 05:05:29 -0500

    Create the labsldapconfig hiera struct
    So far this only contains server names and passwords.  This should
    be a no-op in 'real' labs and fix some things in labtest.
    
    Change-Id: I5034c9c93966a3926c2212c62f5aaee42add63cf

--- modules/ldap/manifests/role/config.pp
+++ modules/ldap/manifests/role/config.pp
@@ -1,11 +1,8 @@
 class ldap::role::config::labs {
-    include passwords::ldap::labs
+    $ldapconfig = hiera_hash('labsldapconfig', {})
 
     $basedn = 'dc=wikimedia,dc=org'
-    $servernames = $::site ? {
-        'eqiad' => [ 'ldap-labs.eqiad.wikimedia.org', 'ldap-labs.codfw.wikimedia.org' ],
-        'codfw' => [ 'ldap-labs.codfw.wikimedia.org', 'ldap-labs.eqiad.wikimedia.org' ],
-    }
+    $servernames = [ $ldapconfig['hostname'], $ldapconfig['secondary_hostname'] ]
     $sudobasedn = $::realm ? {
         'labtest'       => "ou=sudoers,cn=${::labsproject},ou=projects,${basedn}",
         'labs'       => "ou=sudoers,cn=${::labsproject},ou=projects,${basedn}",
@@ -19,11 +16,10 @@ class ldap::role::config::labs {
         'users_rdn'            => 'ou=people',
         'domain'               => 'wikimedia',
         'proxyagent'           => "cn=proxyagent,ou=profile,${basedn}",
-        'proxypass'            => $passwords::ldap::labs::proxypass,
+        'proxypass'            => $ldapconfig['proxypass'],
         'writer_dn'            => "uid=novaadmin,ou=people,${basedn}",
-        'writer_pass'          => $passwords::ldap::labs::writerpass,
         'script_user_dn'       => "cn=scriptuser,ou=profile,${basedn}",
-        'script_user_pass'     => $passwords::ldap::labs::script_user_pass,
+        'script_user_pass'     => $ldapconfig['script_user_pass'],
         'user_id_attribute'    => 'uid',
         'tenant_id_attribute'  => 'cn',
         'ca'                   => 'ca-certificates.crt',
