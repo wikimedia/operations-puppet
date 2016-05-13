10f8d3b648f5b5914c81d9b0e67a280d039aa18f
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-05-13 17:53:52 +0300

    ores: Use the new service::uwsgi define
    Since we want to move ores into production, we want it to conform to the
    standards of the rest of the services. Use service::uwsgi which
    satisfies most of those
    
    Change-Id: I95453c074fef57dda03d00a4c33a87a0803609a7

--- modules/ores/manifests/web.pp
+++ modules/ores/manifests/web.pp
@@ -5,21 +5,15 @@ class ores::web(
 ) {
     require ores::base
 
-    uwsgi::app { 'ores-web':
-        settings => {
-            uwsgi => {
-                plugins     => 'python3, router_redirect',
-                'wsgi-file' => "${ores::base::config_path}/ores_wsgi.py",
-                master      => true,
-                chdir       => $ores::base::config_path,
-                http-socket => '0.0.0.0:8080',
-                venv        => $ores::base::venv_path,
-                processes   => inline_template("<%= @processorcount.to_i * ${workers_per_core} %>"),
-                # lint:ignore:single_quote_string_with_variables
-                route-if    => 'equal:${HTTP_X_FORWARDED_PROTO};http redirect-permanent:https://${HTTP_HOST}${REQUEST_URI}',
-                # lint:endignore
-                logformat   => '[pid: %(pid)] %(addr) (%(user)) {%(vars) vars in %(pktsize) bytes} [%(ctime)] %(method) %(uri) => generated %(rsize) bytes in %(msecs) msecs (%(proto) %(status)) %(headers) headers in %(hsize) bytes (%(switches) switches on core %(core)) user agent "%(uagent)"',
-            }
+    $processes = $::processorcount * $workers_per_core
+    service::uwsgi { 'ores':
+        port   => 8080,
+        config => {
+            'wsgi-file' => "${ores::base::config_path}/ores_wsgi.py",
+            chdir       => $ores::base::config_path,
+            venv        => $ores::base::venv_path,
+            logformat   => '[pid: %(pid)] %(addr) (%(user)) {%(vars) vars in %(pktsize) bytes} [%(ctime)] %(method) %(uri) => generated %(rsize) bytes in %(msecs) msecs (%(proto) %(status)) %(headers) headers in %(hsize) bytes (%(switches) switches on core %(core)) user agent "%(uagent)"',
+            processes   => $processes,
         }
     }
 }
