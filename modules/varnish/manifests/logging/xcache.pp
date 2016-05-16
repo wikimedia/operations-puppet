49671768397bdef81ee762485073f5d6d5ba7dc2
Brandon Black <bblack@wikimedia.org>
2016-05-16 21:30:45 +0000

    varnishxcache - new monitoring script for hit/miss stuff
    Change-Id: Ia8d5366b1fb0c2a82dc9fa340fdeddbf98f02fd8

---
+++ modules/varnish/manifests/logging/xcache.pp
@@ -0,0 +1,61 @@
+# == Define varnish::logging::xcache:
+# Logs X-Cache hit-related stats to statsd
+#
+# === Parameters
+#
+# [*statsd_server*]
+#   StatsD server address, in "host:port" format.
+#   Defaults to localhost:8125.
+#
+# [*key_prefix*]
+#   Prefix of the stats that will be sent to statsd.
+#   Within this prefix, it will create counters named:
+#   hit-front, hit-local, hit-remote, int-front, int-local, int-remote, misspass
+#   Default 'test.varnish.xcache'
+#
+# === Examples
+#
+#  varnish::logging::xcache {
+#    statsd_server => 'statsd.eqiad.wmnet:8125',
+#    key_prefix => "varnish.${::site}.${::cluster}.xcache"
+#  }
+#
+define varnish::logging::xcache(
+    $statsd_server = 'localhost:8125',
+    $key_prefix = 'test.varnish.xcache',
+) {
+    include varnish::common
+
+    if (hiera('varnish_version4', false)) {
+        $varnish4_python_suffix = '4'
+    } else {
+        $varnish4_python_suffix = ''
+    }
+
+    file { '/usr/local/bin/varnishxcache':
+        source  => "puppet:///modules/varnish/varnishxcache${varnish4_python_suffix}",
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        require => File['/usr/local/lib/python2.7/dist-packages/varnishlog.py'],
+        notify  => Service['varnishcache'],
+    }
+
+    base::service_unit { 'varnishcache':
+        ensure         => present,
+        systemd        => true,
+        strict         => false,
+        template_name  => 'varnishxcache',
+        require        => File['/usr/local/bin/varnishxcache'],
+        service_params => {
+            require => Service['varnish-frontend'],
+            enable  => true,
+        },
+    }
+
+    nrpe::monitor_service { 'varnishxcache':
+        ensure       => present,
+        description  => 'Varnish traffic logger - varnishxcache',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -a "/usr/local/bin/varnishxcache" -u root',
+    }
+}
