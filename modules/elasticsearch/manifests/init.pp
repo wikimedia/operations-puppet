f3040ae43ad389c10e1026cdc5ed889167db18f0
Guillaume Lederrey <glederrey@wikimedia.org>
2016-05-17 15:32:31 +0200

    Elasticsearch - use unicast for discovery by default
    This simplifies a bit the configuration now that all elasticsearch instances
    use unicast for node discovery.
    
    Change-Id: Idfd1a8833bea11b6bb568e747dc8a05c21f630ab

--- modules/elasticsearch/manifests/init.pp
+++ modules/elasticsearch/manifests/init.pp
@@ -50,7 +50,7 @@
 #       Note: It make sense to have multicast configuration separated from
 #       unicast. It is valid to have both unicast and multicast enabled at the
 #       same time and can be useful as a transition state.
-#       Defaults to 'true'
+#       Defaults to 'false'
 # - $unicast_hosts: hosts to seed Elasticsearch's unicast discovery mechanism.
 #       In an environment without reliable multicast (OpenStack) add all the
 #       hosts in the cluster to this list.  Elasticsearch will still use
@@ -94,7 +94,7 @@ class elasticsearch($cluster_name,
                     $awareness_attributes = undef,
                     $row = undef,
                     $rack = undef,
-                    $multicast_enabled = true,
+                    $multicast_enabled = false,
                     $unicast_hosts = undef,
                     $filter_cache_size = '10%',
                     $bulk_thread_pool_executors = undef,
