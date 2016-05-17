2011dd4c8364d172467e18a82f7f573b79e1fdd8
Mukunda Modell <mmodell@wikimedia.org>
2016-05-17 11:29:53 -0500

    keyholder key cleanup
    * Clean up the mess that is scap::target and keyholder::agent
    * Make ssh_agent_proxy look up keys by name instead of fingerprint
    ** This avoids needing to store fingerprints in puppet manifests
    * password-protected key test is now optional and skipped on beta
    
    Bug: T132747
    
    Change-Id: Id298a3e0f12e31afd7ea83970e192330ffbd4243

--- modules/keyholder/manifests/agent.pp
+++ modules/keyholder/manifests/agent.pp
@@ -2,40 +2,38 @@
 #
 # Resource for creating keyholder agents on a node
 #
+# Most instances of this resource are created from hiera, see scap::server
+# and scap/server.yaml
+#
 # === Parameters
 #
 # [*name*]
-#   Used for service names, socket names, and default key name
+#   This is the name of the ssh key managed by this agent. The key comes from
+#   a call to secret which translates to:
+#   puppet/private/modules/secret/secrets/keyholder/${name}[.pub]
 #
-# [*key_file*]
-#   The name of the key file stored in puppet private
-#   Should exist prior to running a defined resource
+# [*ensure*]
+#   Defaults to 'present', this is passed directly to the file resources
+#   that this resource manages.
 #
 # [*trusted_groups*]
-#   An array of group names or GIDs of the trusted user groups with which the agent
-#   should be shared. It is the caller's responsibility to ensure
+#   An array of group names or GIDs of the trusted user groups with which the
+#   agent should be shared. It is the caller's responsibility to ensure
 #   the groups exist.
-#
-# [*key_fingerprint*]
-#   Fingerprint of the public half of the private keyfile specified
-#   by $key_file
 #
 # === Examples
 #
 #  keyholder::agent { 'mwdeploy':
-#      key_file         => 'mwdeploy_key_rsa',
-#      trusted_group   => ['wikidev', 'mwdeploy'],
-#      key_fingerprint => '00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00'
-#      require         => Group['wikidev'],
+#      trusted_groups   => ['wikidev', 'mwdeploy'],
 #  }
 #
 define keyholder::agent(
-    $key_fingerprint,
     $trusted_groups = ['ops'],
-    $key_file = "${name}_rsa",
-    $key_content = undef,
-    $key_secret = undef,
+    $ensure = 'present',
+    $key_name = $name,
 ) {
+    validate_ensure($ensure)
+
     require ::keyholder
     require ::keyholder::monitoring
 
@@ -46,26 +44,30 @@ define keyholder::agent(
         $real_trusted_groups = $trusted_groups
     }
 
-    file { "/etc/keyholder-auth.d/${name}.yml":
-        content => inline_template("---\n<% [*@real_trusted_groups].each do |g| %><%= g %>: ['<%= @key_fingerprint %>']\n<% end %>"),
+    $key_name_safe = regsubst($key_name, '\W', '_', 'G')
+
+    file { "/etc/keyholder.d/${key_name_safe}":
+        ensure  => $ensure,
+        content => secret("keyholder/${key_name_safe}"),
         owner   => 'root',
         group   => 'keyholder',
         mode    => '0440',
     }
 
-    # lint:ignore:puppet_url_without_modules
-    if $key_content {
-        keyholder::private_key { $key_file:
-            content  => $key_content,
-        }
-    } elsif $key_secret {
-        keyholder::private_key { $key_file:
-            content => secret($key_secret)
-        }
-    } else {
-        keyholder::private_key { $key_file:
-            source  => "puppet:///private/ssh/tin/${key_file}",
-        }
+    file { "/etc/keyholder.d/${key_name_safe}.pub":
+        ensure  => $ensure,
+        content => secret("keyholder/${key_name_safe}.pub"),
+        owner   => 'root',
+        group   => 'keyholder',
+        mode    => '0440',
     }
-    # lint:endignore
+
+    # generate the mapping between groups and keys. Used by ssh-agent-proxy
+    file { "/etc/keyholder-auth.d/${key_name_safe}.yml":
+        ensure  => $ensure,
+        content => inline_template("---\n<%= [*@real_trusted_groups].map { |g| \"#{g}: [#{@key_name_safe}]\" }.join(\"\\n\") %>\n"),
+        owner   => 'root',
+        group   => 'keyholder',
+        mode    => '0440',
+    }
 }
