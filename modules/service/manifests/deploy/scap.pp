a6aa0ba64b29235578d279a7742ca5141465b657
Mukunda Modell <mmodell@wikimedia.org>
2016-05-17 11:29:53 -0500

    keyholder key cleanup
    * Clean up the mess that is scap::target and keyholder::agent
    * Make ssh_agent_proxy look up keys by name instead of fingerprint
    ** This avoids needing to store fingerprints in puppet manifests
    * password-protected key test is now optional and skipped on beta
    
    Bug: T132747
    
    Change-Id: Id298a3e0f12e31afd7ea83970e192330ffbd4243

--- modules/service/manifests/deploy/scap.pp
+++ modules/service/manifests/deploy/scap.pp
@@ -3,14 +3,9 @@
 # Creates user and permissions for deploy user
 # on service hosts
 #
-# === Parameters
+# Deprecated: you should use scap::target directly instead of this wrapper.
 #
-# [*public_key_file*]
-#   This is the public_key for the deploy-service user. The private part of this
-#   key should reside in the private puppet repo for the environment. By default
-#   this public key is set to the deploy-service user's public key for production
-#   private puppet—it should be overwritten using hiera in non-production
-#   environements.
+# === Parameters
 #
 # [*user*]
 #   the user to create for deployment
@@ -20,15 +15,14 @@
 #   user.  Default: undef.
 #
 define service::deploy::scap(
-    $public_key_file = 'puppet:///modules/service/servicedeploy_rsa.pub',
     $user            = 'deploy-service',
     $service_name    = undef,
     $manage_user     = false,
 ) {
+    warning('service::deploy::scap is deprecated. Use scap::target instead')
     scap::target { $title:
-        public_key_source => $public_key_file,
-        deploy_user       => $user,
-        service_name      => $service_name,
-        manage_user       => $manage_user,
+        deploy_user  => $user,
+        service_name => $service_name,
+        manage_user  => $manage_user,
     }
 }
