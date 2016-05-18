9a4f05af72521bdad010892d14bc301f0cd9a620
Ariel T. Glenn <ariel@wikimedia.org>
2016-05-18 14:20:54 +0300

    filter out rsync warnings about vanishing files, for kiwix rsync
    another step in reducing cronspam
    
    Change-Id: Ia11ed945adaea49dc4e359e089d04036373d069e

--- modules/dataset/files/kiwix-rsync-cron.sh
+++ modules/dataset/files/kiwix-rsync-cron.sh
@@ -11,7 +11,10 @@ do_rsync (){
 
     running=`/usr/bin/pgrep -f -x "/usr/bin/rsync -rlptq $bwlimit ${sourcehost}::${srcpath} ${destroot}/${destpath}"`
     if [ -z "$running" ]; then
-        /usr/bin/rsync -rlptq "$bwlimit" "${sourcehost}::${srcpath}" "${destroot}/${destpath}"
+        # filter out messages of the type
+        #   file has vanished: "/zim/wikipedia/.wikipedia_tg_all_nopic_2016-05.zim.TQH5Zv" (in download.kiwix.org)
+        #   rsync warning: some files vanished before they could be transferred (code 24) at main.c(1655) [generator=3.1.1]
+        /usr/bin/rsync -rlptq "$bwlimit" "${sourcehost}::${srcpath}" "${destroot}/${destpath}" 2>&1 | grep -v 'vanished'
     fi
 }
 
