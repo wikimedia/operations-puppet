2c43995dc7ca688ce46b8d4314e86d08ef30c523
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-05-19 00:10:40 +0300

    cassandra::metrics: Refresh the collector on a unit file change
    There is no reason to not refresh the collector when we update the unit
    file, since it holds not state or data. Hence removing the refresh
    parameter and setting it to the default
    
    Change-Id: Iacbe2db95b54c24dfb6a87bc4c35dd0db3cc0d8e

--- modules/cassandra/manifests/metrics.pp
+++ modules/cassandra/manifests/metrics.pp
@@ -87,7 +87,6 @@ class cassandra::metrics(
         ensure        => present,
         template_name => 'cassandra-metrics-collector',
         systemd       => true,
-        refresh       => false,
         require       => [
             File[$collector_jar],
             File[$filter_file],
