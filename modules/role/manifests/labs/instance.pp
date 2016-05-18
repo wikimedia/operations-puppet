979e1c27a8a4bb31487441a2e2bc8d1db3a01d98
cpettet <rush@wikimedia.org>
2016-05-18 13:20:31 -0500

    labs nfs client instance refactor
    Change-Id: I426a0311b641901241f259241a6371f1ece92994

--- modules/role/manifests/labs/instance.pp
+++ modules/role/manifests/labs/instance.pp
@@ -32,150 +32,11 @@ class role::labs::instance {
         ensure => present,
     }
 
+    # Allows per-host overriding of NFS mounts
+    $mount_nfs = hiera('mount_nfs', true)
     # No NFS on labs metal for now.
-    if $::virtual == 'kvm' {
-
-        # This script will block until the NFS volume is available
-        file { '/usr/local/sbin/block-for-export':
-            ensure => present,
-            owner  => root,
-            mode   => '0555',
-            source => 'puppet:///files/nfs/block-for-export',
-        }
-
-        if hiera('mount_nfs_hard_mode', true) {
-            $mode = 'hard'
-        } else {
-            $mode = 'soft,timeo=10'
-        }
-        $nfs_opts = "vers=4,bg,${mode},intr,sec=sys,proto=tcp,port=0,noatime,lookupcache=none,nofsc"
-        $nfs_server = 'labstore.svc.eqiad.wmnet'
-        $dumps_server = 'labstore1003.eqiad.wmnet'
-
-        # Allows per-host overriding of NFS mounts
-        $mount_nfs = hiera('mount_nfs', true)
-
-        # ideally this is only on NFS enabled hosts in the future
-        diamond::collector { 'Nfsiostat':
-            source  => 'puppet:///modules/diamond/collector/nfsiostat.py',
-            require => Package['diamond'],
-        }
-
-        if mount_nfs_volume($::labsproject, 'home') and $mount_nfs {
-            include labstore::traffic_shaping
-            # Note that this is the same export as for /data/project
-            exec { 'block-for-home-export':
-                command => "/usr/local/sbin/block-for-export ${nfs_server} project/${::labsproject} 180",
-                require => [File['/etc/modprobe.d/nfs-no-idmap.conf'], File['/usr/local/sbin/block-for-export']],
-                unless  => '/bin/mountpoint -q /home',
-            }
-
-            mount { '/home':
-                ensure  => mounted,
-                atboot  => true,
-                fstype  => 'nfs',
-                options => "rw,${nfs_opts}",
-                device  => "${nfs_server}:/project/${::labsproject}/home",
-                require => [File['/etc/modprobe.d/nfs-no-idmap.conf'], Exec['block-for-home-export']],
-            }
-        }
-
-        if mount_nfs_volume($::labsproject, 'project') or mount_nfs_volume($::labsproject, 'scratch') {
-            include labstore::traffic_shaping
-            # Directory for data mounts
-            file { '/data':
-                ensure => directory,
-                owner  => 'root',
-                group  => 'root',
-                mode   => '0755',
-            }
-        }
-
-        if mount_nfs_volume($::labsproject, 'project') and $mount_nfs {
-            include labstore::traffic_shaping
-            exec { 'block-for-project-export':
-                command => "/usr/local/sbin/block-for-export ${nfs_server} project/${::labsproject} 180",
-                require => [File['/etc/modprobe.d/nfs-no-idmap.conf'], File['/usr/local/sbin/block-for-export']],
-                unless  => '/bin/mountpoint -q /data/project',
-            }
-
-            file { '/data/project':
-                ensure  => directory,
-                require => File['/data'],
-            }
-
-            mount { '/data/project':
-                ensure  => mounted,
-                atboot  => true,
-                fstype  => 'nfs',
-                options => "rw,${nfs_opts}",
-                device  => "${nfs_server}:/project/${::labsproject}/project",
-                require => [File['/data/project', '/etc/modprobe.d/nfs-no-idmap.conf'], Exec['block-for-project-export']],
-            }
-        }
-
-        if mount_nfs_volume($::labsproject, 'scratch') and $mount_nfs {
-            include labstore::traffic_shaping
-            # We don't need to block for this one because it's always exported for everyone.
-            file { '/data/scratch':
-                ensure  => directory,
-                require => File['/data'],
-            }
-
-            mount { '/data/scratch':
-                ensure  => mounted,
-                atboot  => true,
-                fstype  => 'nfs',
-                options => "rw,${nfs_opts}",
-                device  => "${nfs_server}:/scratch",
-                require => File['/data/scratch', '/etc/modprobe.d/nfs-no-idmap.conf'],
-            }
-        }
-
-        if mount_nfs_volume($::labsproject, 'dumps') or mount_nfs_volume($::labsproject, 'statistics') {
-            include labstore::traffic_shaping
-            # Directory for public (readonly) mounts
-            file { '/public':
-                ensure => directory,
-                owner  => 'root',
-                group  => 'root',
-                mode   => '0755',
-            }
-        }
-
-        if mount_nfs_volume($::labsproject, 'statistics') and $mount_nfs {
-            include labstore::traffic_shaping
-            file { '/public/statistics':
-                ensure  => directory,
-                require => File['/public'],
-            }
-
-            mount { '/public/statistics':
-                ensure  => mounted,
-                atboot  => true,
-                fstype  => 'nfs',
-                options => "ro,${nfs_opts}",
-                device  => "${dumps_server}:/statistics",
-                require => File['/public/statistics', '/etc/modprobe.d/nfs-no-idmap.conf'],
-            }
-        }
-
-        if mount_nfs_volume($::labsproject, 'dumps') and $mount_nfs {
-            include labstore::traffic_shaping
-            file { '/public/dumps':
-                ensure  => directory,
-                require => File['/public'],
-            }
-
-            mount { '/public/dumps':
-                ensure  => mounted,
-                atboot  => true,
-                fstype  => 'nfs',
-                options => "ro,${nfs_opts}",
-                device  => "${dumps_server}:/dumps",
-                require => File['/public/dumps', '/etc/modprobe.d/nfs-no-idmap.conf'],
-            }
-        }
+    if $::virtual == 'kvm' and $mount_nfs{
+        include role::labs::nfsclient
     }
 
     if os_version('ubuntu <= precise') {
@@ -190,21 +51,6 @@ class role::labs::instance {
         mount { '/public/keys':
             ensure  => absent,
         }
-    }
-
-    # While the default on kernels >= 3.3 is to have idmap disabled,
-    # doing so explicitly does no harm and ensures it is everywhere.
-
-    file { '/etc/modprobe.d/nfs-no-idmap.conf':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        content => "options nfs nfs4_disable_idmapping=1\n",
-    }
-
-    file { '/etc/idmapd.conf':
-        ensure => absent,
     }
 
     # In production, we try to be punctilious about having Puppet manage
