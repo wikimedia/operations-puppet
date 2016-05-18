93a628770d789ffdd3e3a57b50a33e7f809622a8
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-05-18 12:59:36 +0200

    Blacklist asn1_decoder kernel module
    Blacklist the in-kernel ASN1 decoder. ASN1 decoders are tricky business in
    general, but having them run in kernel level opens a whole new can of worms
    (such as CVE-2016-0758)
    
    This feature is not in use in the WMF cluster since we neither use signed
    kernel packages nor ecryptfs at the moment (if we decide to use it at some
    point we can re-evaluate the blacklist).
    
    The check is limited to >= 4.4 since our 3.19 kernels have the option entirely
    disabled and Ubuntu trusty kernels have it enabled statically (IOW not loaded
    as a kernel module). The precise kernel doesn't have the code at all.
    
    Change-Id: I768dad2c6f67e527782d396721a7d6ffe17b293c

--- modules/base/manifests/kernel.pp
+++ modules/base/manifests/kernel.pp
@@ -12,4 +12,14 @@ class base::kernel
         mode   => '0444',
         source => 'puppet:///modules/base/kernel/blacklist-wmf.conf',
     }
+
+    if (versioncmp($::kernelversion, '4.4') >= 0) {
+        file { '/etc/modprobe.d/blacklist-linux44.conf':
+            ensure => present,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            source => 'puppet:///modules/base/kernel/blacklist-linux44.conf',
+        }
+    }
 }
