cb92bf7fad310897f7da5089d3f863d61c541327
Daniel Zahn <dzahn@wikimedia.org>
2016-05-19 12:50:35 -0700

    temp. setup to use db2007 for RT upgrade test
    This is a temp. setup to use db2007 with a copy
    of the RT database to test changes during a version
    upgrade. The server is in the process of being decom'ed
    and this will be removed again.
    
    Bug:T119112
    Bug:T125827
    Change-Id: Iebdbebd7aacb07051785afcb795a652c505e19ba

---
+++ modules/role/manifests/requesttracker/upgradetest.pp
@@ -0,0 +1,38 @@
+#  temp. setup for testing RT migration to jessie
+class role::requesttracker::upgradetest {
+    system::role { 'role::requesttracker::upgradetest': description => 'temp test setup for RT migration to jessie' }
+
+    include base::firewall
+    include rsync::server
+
+    # copy db dump from slave via rsync
+    $sourceip='10.64.0.20' # m1-slave.eqiad
+
+    ferm::service { 'rt-db-rsync':
+        proto  => 'tcp',
+        port   => '873',
+        srange => "${sourceip}/32",
+    }
+
+    rsync::server::module { 'rtdb':
+        path        => '/srv/rt',
+        read_only   => 'no',
+        hosts_allow => $sourceip,
+    }
+
+    # allow mysql connect from new jessie box
+    ferm::service { 'rt-db-mysql':
+        proto  => 'tcp',
+        port   => '3306',
+        srange => '208.80.154.84/32', # ununpentium
+    }
+
+    package { 'mariadb-server':
+        ensure => 'present',
+    }
+
+    service { 'mysql':
+        ensure => 'running',
+    }
+
+}
