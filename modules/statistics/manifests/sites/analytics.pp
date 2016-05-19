a1c43481791706bb787ff76b6f970b1c4abd9c49
Nuria Ruiz <nuria@wikimedia.org>
2016-05-19 08:36:19 -0700

    Cloning analytics.wikimedia.org repo
    Static content of analytics.wikimedia.org is
    stored on gerrit repo. This change clones repo
    to the location in which apache expects it to be to serve files.
    
    Bug: T134506
    Change-Id: I20493ecffa7800813467d3e601402430c27dae5e

--- modules/statistics/manifests/sites/analytics.pp
+++ modules/statistics/manifests/sites/analytics.pp
@@ -12,11 +12,12 @@ class statistics::sites::analytics {
 
     # /srv/analytics.wikimedia.org
     $document_root = "${$::statistics::working_path}/analytics.wikimedia.org"
-
     # Allow statistics-web-users to modify files in this directory.
-    file { $document_root:
-        ensure => 'directory',
-        owner  => 'root',
+
+    git::clone { 'analytics.wikimedia.org':
+        directory => $document_root,
+        origin    => 'https://gerrit.wikimedia.org/r/analytics/analytics.wikimedia.org',
+        owner     => 'root',
         group  => 'statistics-web-users',
         mode   => '0775',
     }
