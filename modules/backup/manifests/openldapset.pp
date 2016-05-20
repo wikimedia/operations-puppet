71b6efc28664c11ce30ce43e2fda2e8755feae8a
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-05-20 09:52:37 +0200

    Add a new backup set to backup openldap databases
    Run slapcat to create a snapshot of the LDAP data in the pre-run script
    and a cleanup script after the backup run.
    
    Bug: T120919
    Change-Id: I2ba5436a356b729b22dd8d7dcb5c64f11cb36e1f

---
+++ modules/backup/manifests/openldapset.pp
@@ -0,0 +1,29 @@
+define backup::openldapset(){
+
+    require_package('slapd') # slapcat is provided by the slapd package
+    file { '/etc/bacula/scripts/openldap-pre':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0500',
+        source  => 'puppet:///modules/backup/openldap-pre',
+    }
+
+    file { '/etc/bacula/scripts/openldap-post':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0500',
+        source  => 'puppet:///modules/backup/openldap-post',
+    }
+
+    $run_scripts = {
+        'ClientRunBeforeJob' => '/etc/bacula/scripts/openldap-pre',
+        'ClientRunAfterJob' => '/etc/bacula/scripts/openldap-post',
+    }
+    bacula::client::job { "openldap-backup":
+        fileset     => 'openldap',
+        jobdefaults => $role::backup::host::jobdefaults,
+        extras      => $run_scripts,
+    }
+}
