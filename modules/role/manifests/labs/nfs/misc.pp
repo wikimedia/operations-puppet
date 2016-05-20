d3b78cd8d6c2c62890b96346a2d51f6e8176c25b
cpettet <rush@wikimedia.org>
2016-05-20 16:21:04 -0500

    labstore cleanup and role vs module arrange
    This more accurately reflects the module and role
    schemes used for other config.  We are also staging
    to have primary/second/misc nfs setups so rename as
    primary and misc to that effect.
    
    Change-Id: Id66499d22a7f40df8174fdfab8a89e10899beb7c

--- modules/role/manifests/labs/nfs/misc.pp
+++ modules/role/manifests/labs/nfs/misc.pp
@@ -1,3 +1,4 @@
+#
 # NFS misc server:
 # - dumps
 # - statistics data shuffle
@@ -7,12 +8,19 @@
 # must be set at the node level or via hiera.
 #
 
-
 class role::labs::nfs::misc($dump_servers_ips) {
-    include standard
+
+    system::role { 'role::labs::nfs::misc':
+        description => 'Labs NFS service (misc)',
+    }
+
     include labstore
-    include labstore::monitoring
     include rsync::server
+    include labstore::backup_keys
+    include labstore::monitoring::interfaces
+    include labstore::monitoring::nfsd
+    # to be included post ldap integration
+    # include labstore::monitoring::ldap
 
     rsync::server::module { 'pagecounts':
         path        => '/srv/dumps/pagecounts',
