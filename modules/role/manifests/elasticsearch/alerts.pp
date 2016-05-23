0b5adfbc2129f62b7d696fe43af632bad7233c8a
Erik Bernhardson <ebernhardson@wikimedia.org>
2016-05-23 09:02:11 -0700

    Point CirrusSearch alerting at more useful metrics
    We had an event a couple weeks ago where the cluster started performing
    erratically. As part of this we noticed that the icinga alerts here did
    not trigger, because they were pointed at metrics that no longer exist.
    
    After looking over the recorded metrics I think alerting on the total
    95th percentile latency seems to be one of the better indicators that
    something is going wrong in the cluster.  Additionally the number of
    backend failures logged by CirrusSearch seem quite relevant.
    
    Additonally i've added irc reporting to the team-discovery contact
    group. IRC is a great way for instant feedback, and if somehow someone
    from discovery isn't arround to notice the alert mails going out someone
    in operations certainly will.
    
    Bug: T134852
    Change-Id: I038ac1f9169b2ab5829c16dbc830eedc8ef9b49a

--- modules/role/manifests/elasticsearch/alerts.pp
+++ modules/role/manifests/elasticsearch/alerts.pp
@@ -1,10 +1,31 @@
 class role::elasticsearch::alerts {
-    monitoring::graphite_threshold { 'prefix_search_50th_percentile':
-        description   => 'Prefix search 50th percentile latency',
-        metric        => 'transformNull(MediaWiki.CirrusSearch.requestTimeMs.prefix.p50, 0)',
+    monitoring::graphite_threshold { 'cirrussearch_eqiad_95th_percentile':
+        description   => 'CirrusSearch eqiad 95th percentile latency',
+        metric        => 'transformNull(MediaWiki.CirrusSearch.eqiad.requestTime.p95, 0)',
         from          => '10min',
-        warning       => '75',
-        critical      => '150',
+        warning       => '500',
+        critical      => '1000',
+        percentage    => '20',
+        contact_group => 'team-discovery',
+    }
+
+    monitoring::graphite_threshold { 'cirrussearch_codfw_95th_percentile':
+        description   => 'CirrusSearch codfw 95th percentile latency',
+        metric        => 'transformNull(MediaWiki.CirrusSearch.codfw.requestTime.p95, 0)',
+        from          => '10min',
+        warning       => '500',
+        critical      => '1000',
+        percentage    => '20',
+        contact_group => 'team-discovery',
+    }
+
+    # warning level is ~1% of peak traffic failing
+    monitoring::graphite_threshold { 'search_backend_failure_count':
+        description   => 'Number of backend failures per minute from CirrusSearch',
+        metric        => 'transformNull(MediaWiki.CirrusSearch.eqiad.backend_failure.failed.count, 0)',
+        from          => '10min',
+        warning       => '300',
+        critical      => '600',
         percentage    => '20',
         contact_group => 'team-discovery',
     }
