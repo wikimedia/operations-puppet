241a0f58e5ae40cc909dfc79a7dc5df8bd7675ed
Daniel Zahn <dzahn@wikimedia.org>
2016-05-24 15:55:07 -0700

    add icinga monitoring for content on commons
    Add a virtual host for commons.wm.org to Icinga.
    
    Add a check that, besides the https(!) connection,
    also looks at the content, hence check_command
    'check_https_url_for_string" is used which translates to:
    
    command_line    $USER1$/check_http -H $ARG1$ -I $HOSTADDRESS$ -S
                    -u $ARG2$ -s $ARG3$
    
    where -S also makes it use https.
    
    Check for "Picture of the day" on the main page.
    
    This is all re: the explanation what happened on T124812.
    
    @neon:/usr/lib/nagios/plugins# ./check_http -H commons.wikimedia.org
    -I 198.35.26.96 -S -u "/wiki/Main_Page" -s "Picture of the day"
    
    HTTP OK: HTTP/1.1 200 OK - 132756 bytes in 0.635 second response time
    |time=0.634539s;;;0.000000 size=132756B;;;0
    
    @neon:/usr/lib/nagios/plugins# ./check_http -H commons.wikimedia.org
    -I 198.35.26.96 -S -u "/wiki/Main_Page" -s "Picture NOT of the day"
    
    HTTP CRITICAL: HTTP/1.1 200 OK - string 'Picture NOT of the day' not
    found on 'https://commons.wikimedia.org:443/wiki/Main_Page'
    - 132756 bytes in 0.634 second response time ...
    
    Bug:T124812
    Change-Id: Ie03fccb3de2e5827600b5a58a8f7de5870411298

---
+++ modules/icinga/manifests/monitor/commons.pp
@@ -0,0 +1,15 @@
+# monitoring of content on commons (T124812)
+class icinga::monitor::commons {
+
+    @monitoring::host { 'commons.wikimedia.org':
+        host_fqdn => 'commons.wikimedia.org',
+    }
+
+    monitoring::service { 'commons_content':
+        description   => 'wiki content on commons',
+        check_command => 'check_https_url_for_string!commons.wikimedia.org!/wiki/Main_Page!Picture of the day',
+        host          => 'commons.wikimedia.org',
+        contact_group => 'admins',
+    }
+
+}
