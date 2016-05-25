4981b1ba52020c73638bfbc62236e95e8624e2d2
Jaime Crespo <jcrespo@wikimedia.org>
2016-05-25 14:05:55 +0200

    Reintroduce es200[1234] in puppet, without specific roles
    Also force start of regex (^) on some es hosts, to avoid
    potential extra matches. Plus cosmetic title changes.
    
    Bug: T134755
    Change-Id: I9893f80d719f9eed9f5006459f5ee425b4bc1c0e

--- manifests/site.pp
+++ manifests/site.pp
@@ -958,7 +958,7 @@ node /^es101[268]\.eqiad\.wmnet/ {
 }
 
 ## codfw servers
-node /es201[123]\.codfw\.wmnet/ {
+node /^es201[123]\.codfw\.wmnet/ {
     class { 'role::mariadb::core':
         shard         => 'es1',
         binlog_format => 'ROW',
@@ -1000,7 +1000,7 @@ node 'es2015.codfw.wmnet' {
     include base::firewall
 }
 
-node /es201[46]\.codfw\.wmnet/ {
+node /^es201[46]\.codfw\.wmnet/ {
     class { 'role::mariadb::core':
         shard         => 'es2',
         binlog_format => 'ROW',
@@ -1011,6 +1011,7 @@ node /es201[46]\.codfw\.wmnet/ {
 
 # External Storage, Shard 3 (es3) databases
 
+## eqiad servers
 node 'es1019.eqiad.wmnet' {
     class { 'role::mariadb::core':
         shard         => 'es3',
@@ -1021,7 +1022,6 @@ node 'es1019.eqiad.wmnet' {
     include base::firewall
 }
 
-## eqiad servers
 node /^es101[47]\.eqiad\.wmnet/ {
     class { 'role::mariadb::core':
         shard         => 'es3',
@@ -1042,7 +1042,7 @@ node 'es2018.codfw.wmnet' {
     include base::firewall
 }
 
-node /es201[79]\.codfw\.wmnet/ {
+node /^es201[79]\.codfw\.wmnet/ {
     class { 'role::mariadb::core':
         shard         => 'es3',
         binlog_format => 'ROW',
@@ -1050,6 +1050,13 @@ node /es201[79]\.codfw\.wmnet/ {
     }
     include base::firewall
 }
+
+# Disaster recovery hosts for external storage
+node /^es200[1234]\.codfw\.wmnet/ {
+    include standard
+    include base::firewall
+}
+
 
 # Etherpad (virtual machine)
 node 'etherpad1001.eqiad.wmnet' {
