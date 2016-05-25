5b9fbf2b6b5125859b800152841847fa09e85b37
Jcrespo <jcrespo@wikimedia.org>
2016-05-25 10:17:09 +0000

    Revert "Apply firewall to db1016"
    This reverts commit d4366aef0ece807ce3efb748446b636e9bd7f202.
    
    Change-Id: Id5614f1b6f16cfcbf6c45c0e13c74ea8b9236515

--- manifests/site.pp
+++ manifests/site.pp
@@ -728,7 +728,13 @@ node 'db1001.eqiad.wmnet' {
     }
 }
 
-node /^db(1016|2010)\.codfw\.wmnet/ {
+node 'db1016.eqiad.wmnet' {
+    class { 'role::mariadb::misc':
+        shard  => 'm1',
+    }
+}
+
+node 'db2010.codfw.wmnet' {
     class { 'role::mariadb::misc':
         shard => 'm1',
     }
