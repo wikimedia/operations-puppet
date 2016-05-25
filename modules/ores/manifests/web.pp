0215863211fb98567e23108ceba73a2bc6544385
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-05-25 22:10:49 +0300

    ores: Specify uwsgi plugins to override the default
    ORES is python3 only, no reason for python 2 support
    
    Change-Id: I41d9f51ec5757e1f8de4f7d96b82b9c36730a435

--- modules/ores/manifests/web.pp
+++ modules/ores/manifests/web.pp
@@ -11,6 +11,7 @@ class ores::web(
         config => {
             'wsgi-file' => "${ores::base::config_path}/ores_wsgi.py",
             chdir       => $ores::base::config_path,
+            plugins     => 'python3',
             venv        => $ores::base::venv_path,
             logformat   => '[pid: %(pid)] %(addr) (%(user)) {%(vars) vars in %(pktsize) bytes} [%(ctime)] %(method) %(uri) => generated %(rsize) bytes in %(msecs) msecs (%(proto) %(status)) %(headers) headers in %(hsize) bytes (%(switches) switches on core %(core)) user agent "%(uagent)"',
             processes   => $processes,
