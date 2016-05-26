6d5c3ec1d318caa7fa78e938080890d01a95b476
Faidon Liambotis <faidon@wikimedia.org>
2016-05-26 20:48:30 +0300

    raid: vary package installation on the RAID installed
    Installing all RAID controller packages on all hosts is kind of a waste.
    Use the newly-written $raid fact to differentiate systems based on what
    kind of RAID controller they have installed and install the appropriate
    packages.
    
    Bug: T84050
    Change-Id: Ia16b7ad8ad281640fe18fe77cb781d2480af54dc

--- modules/raid/manifests/init.pp
+++ modules/raid/manifests/init.pp
@@ -9,17 +9,37 @@
 #  include raid
 
 class raid {
-    package { [ 'megacli', 'arcconf', 'mpt-status' ]:
-        ensure => 'latest',
+    # unfortunately, we don't support stringify_facts=false yet; when we
+    # eventually do, the fact should be adjusted to not join with ",", and the
+    # following line should be then removed.
+    $raid = split($::raid, ',')
+
+    if 'megaraid' in $raid {
+        require_package('megacli')
     }
 
-    file { '/etc/default/mpt-statusd':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0555',
-        content => "RUN_DAEMON=no\n",
-        before  => Package['mpt-status'],
+    if 'mpt' in $raid {
+        require_package('mpt-status')
+
+        file { '/etc/default/mpt-statusd':
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0555',
+            content => "RUN_DAEMON=no\n",
+            before  => Package['mpt-status'],
+        }
+    }
+    if 'md' in $raid {
+        # if there is an "md" RAID configured, mdadm is already installed
+    }
+
+    if 'aac' in $raid {
+        require_package('arcconf')
+    }
+
+    if 'twe' in $raid {
+        require_package('tw-cli')
     }
 
     file { '/usr/local/lib/nagios/plugins/check_raid':
