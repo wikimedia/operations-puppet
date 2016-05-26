feabb2bbb3c2a63cff2b8566a554a000643afbb7
Faidon Liambotis <faidon@wikimedia.org>
2016-05-26 20:04:53 +0300

    raid: add a new "raid" fact
    This holds a array of all the possible RAID configurations that can be
    found in one server. Hence, it will support configurations such as
    software RAID on top or on the side of hardware RAID, or multiple RAID
    controllers in one server.
    
    Unused for now -- it's still useful to populate the database with it, so
    that errors can be found before we can start using it.
    
    Bug: T84050
    Change-Id: Ie3b2ed86e088655b5361b2c45c80a2e0acdfc1cc

---
+++ modules/raid/lib/facter/raid.rb
@@ -0,0 +1,60 @@
+Facter.add('raid') do
+  confine :kernel => :linux
+  setcode do
+    raids = []
+
+    if FileTest.exist?('/dev/cciss/') || FileTest.exist?('/sys/module/hpsa/')
+      raids.push('hpsa')
+    end
+
+    if FileTest.exist?('/dev/megadev0') ||
+       Dir.glob('/sys/bus/pci/drivers/megaraid_sas/00*').length > 0
+      raids.push('megaraid')
+    end
+
+    if FileTest.exist?('/dev/mptctl') ||
+       FileTest.exist?('/dev/mpt0') ||
+       FileTest.exist?('/proc/mpt/summary') ||
+       FileTest.exist?('/proc/scsi/mptsas/0')
+      raids.push('mpt')
+    end
+
+    if FileTest.exist?('/dev/aac0')
+      raids.push('aacraid')
+    end
+
+    if FileTest.exist?('/proc/scsi/scsi')
+      IO.foreach('/proc/scsi/scsi') do |x|
+        if x =~ /Vendor: 3ware/
+          raids.push('3ware')
+          break
+        end
+      end
+    end
+
+    supported_devs = [ 'aac', 'twe' ]
+    IO.foreach('/proc/devices') do |x|
+      if /^\s*\d+\s+(?<dev>\w+)/ =~ x
+        raids.push(dev) if supported_devs.include?(dev)
+      end
+    end
+
+    if FileTest.exist?('/proc/mdstat')
+      IO.foreach('/proc/mdstat') do |x|
+        if x =~ /md[0-9]+ : active/
+          raids.push('md')
+          break
+        end
+      end
+    end
+
+    raids.sort!.uniq!
+
+    if Facter.version < '2.0.0'
+      raids.join(',')
+    else
+      raids
+    end
+
+  end
+end
