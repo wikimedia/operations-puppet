b1e087edc337b96cf3b15588fcef936ba326cacd
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-05-26 10:00:20 +0200

    Drop deployment-ssh rules from role::snapshot::dumper
    Already provided via modules/scap/manifests/ferm.pp
    
    Change-Id: I481dfee6bcf74cf6131324136c6338d700d2b97e

--- modules/role/manifests/snapshot/dumper.pp
+++ modules/role/manifests/snapshot/dumper.pp
@@ -2,11 +2,6 @@
 # meaning sql/xml dumps every couple of weeks or so
 class role::snapshot::dumper {
 
-    # Allow SSH from deployment hosts
-    ferm::rule { 'deployment-ssh':
-        rule   => 'proto tcp dport ssh saddr $DEPLOYMENT_HOSTS ACCEPT;',
-    }
-
     # mw packages and dependencies, dataset server nfs mount
     include snapshot::dumps::packages
 
