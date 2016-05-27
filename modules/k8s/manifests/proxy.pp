bbe7e95d63515dbc2f968c8361bd055313105432
YuviPanda <yuvipanda@gmail.com>
2016-05-27 09:15:02 -0500

    k8s: Decouple kubelet from kube-proxy
    Only kubelet requires the private certificates, but we can't
    just put that in there since kube-proxy requires the public
    certificates. We'll eventually fix this by getting rid of
    the k8s::ssl class, but until then....
    
    Bug: T136413
    Change-Id: Id24eb002d876c0a857c223add8832bc48aa5e6a5

--- modules/k8s/manifests/proxy.pp
+++ modules/k8s/manifests/proxy.pp
@@ -5,8 +5,6 @@ class k8s::proxy(
 
     $master_ip = ipresolve($master_host, 4, $::nameservers[0])
 
-    include k8s::users
-
     base::service_unit { 'kube-proxy':
         systemd   => true,
         subscribe => File['/etc/kubernetes/kubeconfig'],
