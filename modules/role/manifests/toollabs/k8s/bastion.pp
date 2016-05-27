c3ce7b7f637d00ac16db410235b687748e75f280
YuviPanda <yuvipanda@gmail.com>
2016-05-27 09:29:44 -0500

    tools: Fixup k8s bastion role
    - Adds a simple script to deploy appropriate binaries
    - Adds flannel + kube-proxy for access to pod/svc ips
    - Installs kubectl for use by everyone
    
    Bug: T136413
    Change-Id: I3b106fe670a1e4f6a0a23a978b50ed211e85f31b

--- modules/role/manifests/toollabs/k8s/bastion.pp
+++ modules/role/manifests/toollabs/k8s/bastion.pp
@@ -1,5 +1,30 @@
 class role::toollabs::k8s::bastion {
 
-    # kubectl and things
-    include k8s::client
+    $master_host = hiera('k8s::master_host')
+    $etcd_url = join(prefix(suffix(hiera('flannel::etcd_hosts', [$master_host]), ':2379'), 'https://'), ',')
+
+    ferm::service { 'flannel-vxlan':
+        proto => udp,
+        port  => 8472,
+    }
+
+    class { '::k8s::flannel':
+        etcd_endpoints => $etcd_url,
+    }
+
+    class { '::k8s::ssl':
+        notify          => Class['k8s::proxy'],
+    }
+
+    class { 'k8s::proxy':
+        master_host => $master_host,
+    }
+
+    # Deployment script (for now!)
+    file { '/usr/local/bin/deploy-bastion':
+        source => 'puppet:///modules/role/toollabs/deploy-bastion.bash',
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+    }
 }
