6560371e688f4f714e815c1cc6d0f74842b34e10
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-05-27 16:58:30 +0300

    network: add $production_networks
    This includes all the networks under the "production" realm (public +
    private).
    
    Accomplish this through a puppet parser function that allows arbitrary
    lookups into our network data structure, e.g. per realm, per sphere
    (public, private), per address family.
    
    Bug: T122396
    Change-Id: I4369dfed0f0645ae37432cbfbe172e38330fa57e

---
+++ modules/network/lib/puppet/parser/functions/slice_network_constants.rb
@@ -0,0 +1,62 @@
+# == Function: slice_network_constants(realm)
+#
+# Return a specific port of network::constants::all_network_subnets as a
+# flattened array.
+# Optionally return only a specific sphere (public/private) by passing an
+# optional second argument
+# Optionally return only an address family, but passing ipv4 or ipv6 as an
+# optional third argument
+# realm needs to be a key in networks::constants::all_network_subnets
+# sphere has to be public/private, defaults to both if not specified. Also
+# 'both' is accepted
+# address family needs to be ipv4 or ipv6, defaults to both if not specified
+#
+# == Examples
+# # Get all ipv4 subnets that belong in production, public sphere
+# slice_network_constants('production', 'public', 'ipv4')
+#
+# # Get IPv4/IPv6 subnets that belong in labs
+# slice_network_constants('labs')
+#
+# # Get private only production networks
+# slice_network_constants('labs', 'private')
+#
+module Puppet::Parser::Functions
+  newfunction(:slice_network_constants, :type => :rvalue, :arity => -2) do |args|
+    fail 'slice_network_constants() requires an argument' if args.empty?
+    fail 'slice_network_constants() cannot handle more than 3 values' if args.length > 3
+    all_network_subnets = function_hiera(['network::all_network_subnets'])
+    realm = args[0]
+    requested_sphere = args[1] if args.length > 1
+    af = args[2] if args.length > 2
+
+    # Make sure we got sane arguments
+    if defined? requested_sphere && (!['public', 'private', 'both'].include? requested_sphere)
+        fail "slice_network_constants() sphere can only be public/private/both. #{requested_sphere} was provided"
+    end
+    if defined? af && (!['ipv4', 'ipv6'].include? af)
+        fail "slice_network_constants() address family specified can only be ipv4 or ipv6. #{af} was provided"
+    end
+    if !all_network_subnets.key?(realm)
+        fail 'slice_network_constants() realm non existant in networks::constants::all_network_subnets'
+    end
+
+    # And let's get our data back
+    result = all_network_subnets[realm].collect { |_, value_site|
+        value_site.collect { |sphere, value_sphere|
+            if defined? requested_sphere && requested_sphere != 'both' && sphere != requested_sphere
+                next
+            end
+            value_sphere.collect { |_, value_subnet|
+                if defined? af
+                    value_subnet[af]
+                else
+                    # flatten will take care of this below
+                    [value_subnet['ipv4'], value_subnet['ipv6']]
+                end
+            }
+        }
+    }
+    result.flatten.compact
+  end
+end
