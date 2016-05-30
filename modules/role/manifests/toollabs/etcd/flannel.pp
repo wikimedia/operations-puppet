c630257f944c0e278ae826b9d2a433757364eafd
YuviPanda <yuvipanda@gmail.com>
2016-05-30 15:28:55 -0700

    tools: Allow bastions to talk to flannel etcd
    Bug: T136413
    Change-Id: Ic9954742f46aab94effbb686365197601888d824

--- modules/role/manifests/toollabs/etcd/flannel.pp
+++ modules/role/manifests/toollabs/etcd/flannel.pp
@@ -3,19 +3,20 @@ class role::toollabs::etcd::flannel {
 
     include base::firewall
 
-    $worker_nodes = join(hiera('k8s::worker_hosts'), ' ')
-    $peer_nodes = join(hiera('flannel::etcd_hosts'), ' ')
-    $proxy_nodes = join(hiera('toollabs::proxy::proxies'), ' ')
+    $worker_hosts = join(hiera('k8s::worker_hosts'), ' ')
+    $bastion_hosts = join(hiera('k8s::bastion_hosts'), ' ')
+    $peer_hosts = join(hiera('flannel::etcd_hosts'), ' ')
+    $proxy_hosts = join(hiera('toollabs::proxy::proxies'), ' ')
 
     ferm::service { 'flannel-clients':
         proto  => 'tcp',
         port   => '2379',
-        srange => "@resolve((${worker_nodes} ${peer_nodes} ${proxy_nodes}))"
+        srange => "@resolve((${worker_hosts} ${peer_hosts} ${proxy_hosts} ${bastion_hosts}))"
     }
 
     ferm::service { 'flannel-peers':
         proto  => 'tcp',
         port   => '2380',
-        srange => "@resolve((${peer_nodes}))"
+        srange => "@resolve((${peer_hosts}))"
     }
 }
