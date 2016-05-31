cbbcac261bb38d5f610b2b33183738398ec6b8d8
elukey <ltoscano@wikimedia.org>
2016-05-31 15:27:05 +0200

    Deploy memcached 1.4.25 to mc1010 as part of a performance experiment.
    Bug: T129963
    Change-Id: Iaa47685d681fe17cdc7bc2db02ac2c2c59176e08

--- manifests/role/memcached.pp
+++ manifests/role/memcached.pp
@@ -12,11 +12,11 @@ class role::memcached {
         'labs'       => 3000,
     }
 
-    # mc[12]009 will get the 1.4.25-2~wmf1 package version
+    # The following hosts will get the 1.4.25-2~wmf1 package version
     # deployed manually. This workaround should avoid
     # disabling puppet for the whole duration of the test.
     # More info: T129963
-    if $::hostname == 'mc2009' or $::hostname == 'mc1009' {
+    if $::hostname == 'mc2009' or $::hostname =~ /mc10(09|10)/ {
         $version =  '1.4.25-2~wmf1'
     } else {
         $version = os_version('debian >= jessie || ubuntu >= trusty') ? {
@@ -25,9 +25,10 @@ class role::memcached {
         }
     }
 
-    # mc[12]009 are configured with the latest memcached (version 1.4.25-2~wmf1)
-    # as part of a performance experiment. More info: T129963
-    if $::hostname == 'mc2009' or $::hostname == 'mc1009' {
+    # The following hosts are configured with the latest memcached
+    # (version 1.4.25-2~wmf1) as part of a performance experiment.
+    # More info: T129963
+    if $::hostname == 'mc2009' or $::hostname =~ /mc10(09|10)/ {
         $growth_factor    = 1.15
         $extended_options = [
             'slab_reassign',
