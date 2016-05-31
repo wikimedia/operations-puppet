2bfe1a0073cb48005b716333173dfdf6f97305da
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-05-31 14:06:03 -0700

    uwsgi: Allow specifying plugins optionally as a uwsgi command line option
    Allow passing the uwsgi plugins option in the command line which takes
    precedence over ini - as the order of the options is not preserved by
    the puppet module while writing the ini file, but the order is taken into
    account at the uwsgi option parsing end.
    
    Change-Id: I1b022644f38828ddbec3b8f343b1b63b1426de96

--- modules/uwsgi/manifests/app.pp
+++ modules/uwsgi/manifests/app.pp
@@ -10,12 +10,24 @@
 #   level key has a value that is an Array, that key is repeated for each
 #   value of the array
 #
+# [*plugins*]
+#   Comma-separated string list of plugins that will be passed to uwsgi as a
+#   cmd line parameter instead of being written to the ini file.
+#   Uwsgi uses a top-bottom, expand asap processing for options specified in the
+#   config files, and sometimes does things like setting python environments
+#   based on the occurrence of options like callable before it encounters the
+#   plugin option that declares whether to use python 2 or 3.
+#   The order of the config specified in a puppet dict (that has no ordering
+#   guarantees) is lost prior to it being written to ini config in the
+#   app.ini.erb template, and options aren't ordered in the intended manner.
+#   Let's override the plugins option by passing it in the command line which
+#   takes precedence over the options in the config file.
+#
 # === Examples
 #
 #  uwsgi::app { 'graphite-web':
 #    settings => {
 #      uwsgi => {
-#          'plugins'     => 'python',
 #          'socket'      => '/var/run/graphite-web/graphite-web.sock',
 #          'wsgi-file'   => '/usr/share/graphite-web/graphite.wsgi',
 #          'master'      => true,
@@ -23,10 +35,12 @@
 #          'env'         => ['MYSQL_HOST=localhost', 'STATSD_PREFIX=wat'],
 #      },
 #    },
+#    plugins => 'python, router_redirect'
 #  }
 #
 define uwsgi::app(
     $settings,
+    $plugins  = undef,
     $ensure   = present,
     $enabled  = true,
 ) {
