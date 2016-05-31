35f6983b1a60b9f7cf1fa120dcf2f1df8fc8374c
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-05-31 13:44:18 +0200

    Stop installing PHP on jessie app servers
    Moving to jessie seems like a good point to stop pulling in php.
    
    CI still needs it, though.
    
    Change-Id: Iac029f584e2c3592a4fa88eb857d83462ef60171

--- modules/contint/manifests/packages.pp
+++ modules/contint/manifests/packages.pp
@@ -8,6 +8,12 @@ class contint::packages {
     # Basic utilites needed for all Jenkins slaves
     include ::contint::packages::base
 
+    # We're no longer installing PHP on app servers starting with
+    # jessie, but we still need it for CI
+    if os_version('debian == jessie') {
+        include ::mediawiki::packages::php5
+    }
+
     require_package('openjdk-7-jdk')
 
     # MediaWiki doc is built directly on contint1001
