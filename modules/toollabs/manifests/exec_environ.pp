456adf4054b5e9db048c724de02591b845a111ee
andrewbogott <abogott@wikimedia.org>
2016-06-02 00:17:24 -0500

    Add 'libertine' fonts to tools exec nodes.
    Bug: T137121
    Change-Id: I1e4f3e8d59db04a5bcb401cb1d5867439720e5de

--- modules/toollabs/manifests/exec_environ.pp
+++ modules/toollabs/manifests/exec_environ.pp
@@ -165,6 +165,9 @@ class toollabs::exec_environ {
         'fonts-ipafont-gothic',        # Japanese fonts.
         'ttf-indic-fonts-core',        # Many Indic languages.
 
+        # Fonts for latex
+        'fonts-linuxlibertine',        # T137121
+
         # tcl packages
         'mysqltcl',
         'tcl-tls',                     # T58995
