e55d6fc8c1e16d314380bb0b8235b6a99271b26c
Daniel Zahn <dzahn@wikimedia.org>
2016-06-02 15:59:15 -0700

    aptrepo: mv wikimedia-specific distributions file to role
    This file is specific to wikimedia and should be realized in the role
    class not the module itself.
    
    When, like on bromine, aptrepo::distribution is used, this creates a
    conflict and puppet error.
    
    It could also be replaced with aptrepo::distribution instead but
    some options are missing for now.
    
    Bug:T132757
    Bug:T136793
    Change-Id: Ib98634da6745278ab0948ddb14aaf69434fc93a4

--- modules/role/manifests/aptrepo/wikimedia.pp
+++ modules/role/manifests/aptrepo/wikimedia.pp
@@ -1,8 +1,18 @@
 # http://apt.wikimedia.org/wikimedia/
 class role::aptrepo::wikimedia {
 
+    $basedir = '/srv/wikimedia'
+
     class { '::aptrepo':
-        basedir => '/srv/wikimedia',
+        basedir => $basedir,
+    }
+
+    file { "${basedir}/conf/distributions":
+        ensure => present,
+        mode   => '0444',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/aptrepo/distributions-wikimedia',
     }
 
 }
