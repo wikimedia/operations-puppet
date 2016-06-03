3d243e322d528183ecce9b63e70929b03b748622
elukey <ltoscano@wikimedia.org>
2016-06-03 17:04:31 +0200

    Remove old and redundant AQS specific alarms.
    From https://gerrit.wikimedia.org/r/#/c/250439 it seems that
    cassandra already offers basic CQL checks/alerts
    (that are multi-instance aware). I don't see a huge value in having
    java process checks since CQL ones should be already enough for
    alarming.
    
    Bug: T135145
    Change-Id: I97fc114c7968f338f2d2f162e7090f8549607145

--- manifests/role/aqs.pp
+++ manifests/role/aqs.pp
@@ -21,20 +21,6 @@ class role::aqs {
     include ::cassandra::metrics
     include ::cassandra::logging
 
-    # Emit an Icinga alert unless there is exactly one Java process belonging
-    # to user 'cassandra' and with 'CassandraDaemon' in its argument list.
-    nrpe::monitor_service { 'cassandra-analytics':
-        description  => 'Analytics Cassandra database',
-        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u cassandra -C java -a CassandraDaemon',
-    }
-
-    # CQL query interface monitoring (T93886)
-    monitoring::service { 'cassandra-analytics-cql':
-        description   => 'Analytics Cassanda CQL query interface',
-        check_command => 'check_tcp!9042',
-        contact_group => 'admins,analytics',
-    }
-
     $cassandra_hosts = hiera('cassandra::seeds')
     $cassandra_hosts_ferm = join($cassandra_hosts, ' ')
 
