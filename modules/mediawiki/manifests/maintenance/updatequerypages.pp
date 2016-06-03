b30786cb38213bbd36f01cd3bfb148f196f47942
Reedy <reedy@wikimedia.org>
2016-06-03 10:50:46 +0100

    Disable updatequerypage for wikitech running on non silver host
    Has no access to Wikitech Private Settings, nor to Wikitech db
    
    Bug: T136926
    Change-Id: I61eca3dbb036e1933c449f2e5cee13f1563b3819

--- modules/mediawiki/manifests/maintenance/updatequerypages.pp
+++ modules/mediawiki/manifests/maintenance/updatequerypages.pp
@@ -9,6 +9,8 @@ class mediawiki::maintenance::updatequerypages( $ensure = present ) {
     }
 
     # add cron jobs - usage: <cluster>@<day of month> (monthday currently unused, only sets cronjob name)
-    updatequerypages::cronjob { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 'silver@18']: }
+    updatequerypages::cronjob { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17']: }
+    # T136926 Don't run Wikitech query page updates on non Silver hosts
+    updatequerypages::cronjob { ['silver@18']: false }
     updatequerypages::enwiki::cronjob { ['updatequerypages-enwiki-only']: }
 }
