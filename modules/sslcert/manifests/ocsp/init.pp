7160b641e75d6651cf6a6e0ca76f278710dfce43
Emanuele Rocca <ema@wikimedia.org>
2016-06-06 12:39:07 +0200

    update-ocsp-all: write output to logfile
    Use a logfile (and rotate it) instead of cronspamming.
    
    Bug: T132835
    Bug: T132324
    Change-Id: Id4a73844d6218a0e257191690feec898f48db371

--- modules/sslcert/manifests/ocsp/init.pp
+++ modules/sslcert/manifests/ocsp/init.pp
@@ -50,12 +50,22 @@ class sslcert::ocsp::init {
     }
 
     cron { 'update-ocsp-all':
-        command => '/usr/local/sbin/update-ocsp-all',
+        command => '/usr/local/sbin/update-ocsp-all 2>&1 | logger -t update-ocsp-all',
         minute  => fqdn_rand(60, '1adf3dd699e51805'),
         hour    => '*',
         require => [
             File['/usr/local/sbin/update-ocsp-all'],
             File['/etc/update-ocsp.d'],
         ],
+    }
+
+    rsyslog::conf { 'update-ocsp-all':
+        source   => 'puppet:///modules/sslcert/update-ocsp-all.rsyslog.conf',
+    }
+
+    # Rotate /var/log/update-ocsp-all.log
+    logrotate::conf { 'update-ocsp-all':
+        ensure => present,
+        source => 'puppet:///modules/sslcert/update-ocsp-all-logrotate',
     }
 }
