76c8e27915864aed150da9a498ff5921233f416d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-06-06 11:25:27 +0200

    hhvm: fix stacktraces script for jessie
    Change-Id: Ibdab29a2f80a67e474a44b2acf1b7587345d0464

--- modules/hhvm/files/check-hhvm-stacktraces.sh
+++ modules/hhvm/files/check-hhvm-stacktraces.sh
@@ -3,10 +3,12 @@ set -u
 # Find any stacktraces we still did not act upon
 for TRACE in "/var/log/hhvm/stacktrace.*.log";
 do
-    # Log the stack trace
-    /usr/bin/logger --tag hhvm-fatal --file ${TRACE}
+    if [ -e "${TRACE}" ]; then
+        # Log the stack trace
+        /usr/bin/logger --tag hhvm-fatal --file ${TRACE}
 
-    # Append the current UTC date (in YYYYMMDD format) to the file
-    # name, so it doesn't get clobbered when the PID is recycled.
-    mv --backup "$TRACE" "${TRACE}.$(date -u +%Y%m%d)"
+        # Append the current UTC date (in YYYYMMDD format) to the file
+        # name, so it doesn't get clobbered when the PID is recycled.
+        mv --backup "$TRACE" "${TRACE}.$(date -u +%Y%m%d)"
+    fi;
 done
