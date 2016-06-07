66ce6e420c12a87ce676a3ba4a0055fc5aab6da2
Faidon Liambotis <faidon@wikimedia.org>
2016-06-07 18:22:45 +0300

    smokeping: various cleanups
    I'm suspecting this class has never worked properly (the suspicion
    arises by the fact that we define two System::Role['smokeping'] in what
    should be the same catalog, but this doesn't result in a conflict).
    Cleanup a things a little bit. More specificallY:
    - include ::smokeping, not smokeping, from role::smokeping (typical
      recursion error)
    - Remove the system::role from the module; it only belongs in the role
      class
    - Fold class smokeping::config into class smokeping (no point in having
      it different). Also drop the Package -> Class dependency which is a
      no-op given where it's defined.
    
    Change-Id: Ic7777b8ab39e3ac7939a95ca8761c01bc736980c

--- modules/smokeping/manifests/init.pp
+++ modules/smokeping/manifests/init.pp
@@ -1,19 +1,28 @@
 class smokeping {
-    system::role { 'smokeping': description => 'Smokeping' }
-
-    include config
-
     package { 'smokeping':
-        ensure => present;
+        ensure => present,
     }
 
     package { 'curl':
-        ensure => present;
+        ensure => present,
+    }
+
+    file { '/etc/smokeping/config.d':
+        ensure  => directory,
+        recurse => true,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        source  => 'puppet:///modules/smokeping/config.d',
+        require => Package['smokeping'],
     }
 
     service { 'smokeping':
         ensure    => running,
-        require   => [ Package['smokeping'], File['/etc/smokeping/config.d'] ],
+        require   => [
+            Package['smokeping'],
+            File['/etc/smokeping/config.d'],
+        ],
         subscribe => File['/etc/smokeping/config.d'],
     }
 }
