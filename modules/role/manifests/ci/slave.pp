4b27d7aa70e76f5d40e2c39deb10106b2ea089c1
Antoine Musso <hashar@free.fr>
2016-06-08 12:42:33 +0200

    contint: cleanup gallium / use contint1001
    [hiera]
    zuul_merger_host: zuul-merger is not on the new server, solely rely on
    scandium.eqiad.wmnet for now.
    Update some comments for the sshd MAC/KEX legacy algorithms
    
    [contint::firewall::labs]
    (impacts CI and deployment-prep slaves on labs)
    Rename ferm rule and update IP
    
    [jenkins]
    Change jenkins-mwext-sync_id_rsa.pub comment.
    
    [nodepool]
    (impacts labnodepool1001.eqiad.wmnet)
    Switch ZeroMq end point
    Rename CI system name
    
    [zuul]
    Gearman is on the private 10.x IP
    
    [role::ci::slave]
    Update ssh public key to allow connections from contint1001
    
    [role::ci::master]
    Update git email address for the jenkins user.
    
    Varnish misc is NOT covered.
    
    Bug: T137265
    Change-Id: I0a4722b509cfe76ee7f5badbf63b26d05d0f92b0

--- modules/role/manifests/ci/slave.pp
+++ modules/role/manifests/ci/slave.pp
@@ -1,7 +1,7 @@
 # Set up a Jenkins slave on PRODUCTION
 #
 # Most jobs are running on labs instances, but we still have a few runnining on
-# gallium for example to publish content under doc.wikimedia.org or
+# contint1001 for example to publish content under doc.wikimedia.org or
 # integration.wikimedia.org
 #
 # You should look instaed at role::ci::labs::slave
@@ -14,7 +14,7 @@ class role::ci::slave {
     include role::zuul::install
 
     class { 'jenkins::slave':
-        ssh_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw== jenkins@gallium from="208.80.154.135"',
+        ssh_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw== jenkins@contint1001 from="XXXX_contint1001"',
         user    => 'jenkins-slave',
         workdir => '/srv/ssd/jenkins-slave',
         # Mount is handled on the node definition
