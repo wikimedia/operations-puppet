f25c17be70cfa5726834fc1e3a8110ca80631277
Marius Hoch <hoo@online.de>
2016-06-09 01:35:26 +0200

    Retry Wikidata dump creation up to three times
    Related to T137366
    
    Change-Id: I257efd74ff770dbdec0e6856b3be8dfc30b0168d

--- modules/snapshot/files/cron/dumpwikidatattl.sh
+++ modules/snapshot/files/cron/dumpwikidatattl.sh
@@ -3,6 +3,7 @@
 # This file is maintained by puppet!
 # puppet:///modules/snapshot/cron/dumpwikidatattl.sh
 ##############################################################
+#
 # Generate a json dump for Wikidata and remove old ones.
 #
 # Marius Hoch < hoo@online.de >
@@ -17,39 +18,55 @@ failureFile=/tmp/dumpwikidatattl-failure
 i=0
 shards=4
 
-rm -f $failureFile
+# Try to create the dump (up to three times).
+retries=0
 
-while [ $i -lt $shards ]; do
-	(
-		set -o pipefail
-		php $multiversionscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dumpRdf.php --wiki wikidatawiki --shard $i --sharding-factor $shards --format ttl 2>> /var/log/wikidatadump/dumpwikidatattl-$filename-$i.log | gzip > $tempDir/wikidataTTL.$i.gz
-		if [ $? -gt 0 ]; then
-			echo 1 > $failureFile
-		fi
-	) &
-	let i++
-done
-
-wait
-
-if [ -f $failureFile ]; then
-	# Something went wrong, let's clean up and give up for now. Leave logs in place.
+while true; do
 	rm -f $failureFile
-	rm $tempDir/wikidataTTL.*.gz
-else
-	i=0
+
 	while [ $i -lt $shards ]; do
-		cat $tempDir/wikidataTTL.$i.gz >> $tempDir/wikidataTtl.gz
-		rm $tempDir/wikidataTTL.$i.gz
+		(
+			set -o pipefail
+			php $multiversionscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dumpRdf.php --wiki wikidatawiki --shard $i --sharding-factor $shards --format ttl 2>> /var/log/wikidatadump/dumpwikidatattl-$filename-$i.log | gzip > $tempDir/wikidataTTL.$i.gz
+			if [ $? -gt 0 ]; then
+				echo 1 > $failureFile
+			fi
+		) &
 		let i++
 	done
 
-	mv $tempDir/wikidataTtl.gz $targetFileGzip
+	wait
 
-	gzip -dc $targetFileGzip | bzip2 -c > $tempDir/wikidataTtl.bz2
-	mv $tempDir/wikidataTtl.bz2 $targetFileBzip2
+	if [ -f $failureFile ]; then
+		# Something went wrong, let's clean up and maybe retry. Leave logs in place.
+		rm -f $failureFile
+		rm $tempDir/wikidataTTL.*.gz
+		let retries++
 
-	pruneOldDirectories
-	pruneOldLogs
-	runDcat
-fi
+		if [ $retries -eq 3 ]; then
+			exit 1
+		fi
+
+		# Another attempt
+		continue
+	fi
+
+	break
+
+done
+
+i=0
+while [ $i -lt $shards ]; do
+	cat $tempDir/wikidataTTL.$i.gz >> $tempDir/wikidataTtl.gz
+	rm $tempDir/wikidataTTL.$i.gz
+	let i++
+done
+
+mv $tempDir/wikidataTtl.gz $targetFileGzip
+
+gzip -dc $targetFileGzip | bzip2 -c > $tempDir/wikidataTtl.bz2
+mv $tempDir/wikidataTtl.bz2 $targetFileBzip2
+
+pruneOldDirectories
+pruneOldLogs
+runDcat
