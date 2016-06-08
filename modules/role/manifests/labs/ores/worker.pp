78119152c47b7873fdd7bd0c38a356b5bff27226
amir <Ladsgroup@gmail.com>
2016-06-08 21:36:45 +0000

    ores: Add redis settings to worker nodes in labs
    Per changes we made for prod, now worker nodes in labs doesn't have
    proper config files.
    
    Change-Id: I26d9eeb8f410276e47eb4f5caa09bc1cd05362c8

--- modules/role/manifests/labs/ores/worker.pp
+++ modules/role/manifests/labs/ores/worker.pp
@@ -1,4 +1,22 @@
 class role::labs::ores::worker {
     include ::ores::worker
     include ::role::labs::ores::redisproxy
+
+    ores::config { 'redis':
+        config   => {
+            'score_caches'     => {
+                'ores_redis' => {
+                    'host' => '127.0.0.1',
+                    'port' => '6380',
+                }
+            },
+            'score_processors' => {
+                'ores_celery' => {
+                    'BROKER_URL'            => 'redis://127.0.0.1:6379',
+                    'CELERY_RESULT_BACKEND' => 'redis://127.0.0.1:6379',
+                }
+            }
+        },
+        priority => '99',
+    }
 }
