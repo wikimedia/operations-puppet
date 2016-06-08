565181a90eb58ad3b2129f68e112813e8fe015a2
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-06-08 15:53:10 +0200

    role::ci::slave: require /srv/ssd as a file, not mountpoint
    Change-Id: I6184de472c31301d1bcbbaf7661cf1ed6918b39d

--- modules/role/manifests/ci/slave.pp
+++ modules/role/manifests/ci/slave.pp
@@ -17,8 +17,7 @@ class role::ci::slave {
         ssh_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw== jenkins@gallium from="208.80.154.135"',
         user    => 'jenkins-slave',
         workdir => '/srv/ssd/jenkins-slave',
-        # Mount is handled on the node definition
-        require => Mount['/srv/ssd'],
+        require => File['/srv/ssd'],
     }
 
     # .gitconfig file required for rare git write operations
