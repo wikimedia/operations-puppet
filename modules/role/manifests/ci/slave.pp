fce440ec2c3c1fc196939d169ca3dff9d6028506
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-06-08 16:25:43 +0200

    role::contint::*: try to resolve circular dependencies correctly
    Change-Id: Ie0ea276575272af94d395b8a59fa7b42e93f5982

--- modules/role/manifests/ci/slave.pp
+++ modules/role/manifests/ci/slave.pp
@@ -13,6 +13,8 @@ class role::ci::slave {
     include contint::packages
     include role::zuul::install
 
+    require contint::master_dir
+
     class { 'jenkins::slave':
         ssh_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw== jenkins@gallium from="208.80.154.135"',
         user    => 'jenkins-slave',
