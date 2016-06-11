33c8adf375f04aaf3b769fe4f4835aac6835616e
amir <Ladsgroup@gmail.com>
2016-06-11 15:17:10 +0000

    ores: fix workers and config
    Change-Id: I8ac9b4bdf924b408e3409863bcb52a92f3cac342

--- modules/role/manifests/labs/ores/worker.pp
+++ modules/role/manifests/labs/ores/worker.pp
@@ -2,18 +2,25 @@ class role::labs::ores::worker {
     include ::ores::worker
     include ::role::labs::ores::redisproxy
 
+    file { '/etc/ores/':
+        ensure => 'directory',
+        owner  => $owner,
+        group  => $group,
+        mode   => $mode,
+    }
+
     ores::config { 'redis':
         config   => {
             'score_caches'     => {
                 'ores_redis' => {
-                    'host' => '127.0.0.1',
+                    'host' => 'ores-redis-01',
                     'port' => '6380',
                 }
             },
             'score_processors' => {
                 'ores_celery' => {
-                    'BROKER_URL'            => 'redis://127.0.0.1:6379',
-                    'CELERY_RESULT_BACKEND' => 'redis://127.0.0.1:6379',
+                    'BROKER_URL'            => 'redis://ores-redis-01:6379',
+                    'CELERY_RESULT_BACKEND' => 'redis://ores-redis-01:6379',
                 }
             }
         },
