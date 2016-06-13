220ae31fe6835952a6e1e522c04b46959c20c73e
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-06-13 11:57:28 +0200

    Only install PHP configuration files on trusty
    Change-Id: Ibbea40da58622a8dcfa6db6e01f5a576a910d503

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -9,20 +9,24 @@ class mediawiki::php {
     $php_module_conf_dir = '/etc/php5/mods-available'
     mediawiki::php_enmod { ['mail']: }
 
-    file { '/etc/php5/apache2/php.ini':
-        source  => 'puppet:///modules/mediawiki/php/php.ini',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        require => Package['php5-common'],
-    }
+    # Only install PHP configuration files on Trusty, jessie onwards
+    # no longer installs Zend PHP
+    if os_version('ubuntu == trusty') {
+        file { '/etc/php5/apache2/php.ini':
+            source  => 'puppet:///modules/mediawiki/php/php.ini',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            require => Package['php5-common'],
+        }
 
-    file { '/etc/php5/cli/php.ini':
-        source  => 'puppet:///modules/mediawiki/php/php.ini.cli',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        require => Package['php5-cli'],
+        file { '/etc/php5/cli/php.ini':
+            source  => 'puppet:///modules/mediawiki/php/php.ini.cli',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            require => Package['php5-cli'],
+        }
     }
 
     file { "${php_module_conf_dir}/mail.ini":
