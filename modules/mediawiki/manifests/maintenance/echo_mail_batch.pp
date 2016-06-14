79654f3e3fc07c452fccf91dca21fc133829a9fd
Roan Kattouw <roan.kattouw@gmail.com>
2016-06-14 10:43:19 +0100

    Only run processEchoEmailBatch.php on Echo-enabled wikis
    Bug: T137771
    Change-Id: I61ea420fe913f5f81cc2445a1868ed9f5988bc3c
    Depends-On: Iad1695b9e99d89cca76d14514cb050b130961d34

--- modules/mediawiki/manifests/maintenance/echo_mail_batch.pp
+++ modules/mediawiki/manifests/maintenance/echo_mail_batch.pp
@@ -1,7 +1,7 @@
 class mediawiki::maintenance::echo_mail_batch( $ensure = present ) {
     cron { 'echo_mail_batch':
         ensure  => $ensure,
-        command => '/usr/local/bin/foreachwiki extensions/Echo/maintenance/processEchoEmailBatch.php >/dev/null',
+        command => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/echo.dblist extensions/Echo/maintenance/processEchoEmailBatch.php >/dev/null',
         user    => $::mediawiki::users::web,
         minute  => 0,
         hour    => 0,
