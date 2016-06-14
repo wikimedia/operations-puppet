8d090beada965d04c20391acf31903fafd74f8c9
Andrew Bogott <abogott@wikimedia.org>
2016-06-14 10:52:09 -0500

    WIP:  Horizon tab for modifying instance puppet config
    Does:
    - Display available roles with docs and params
    - Display set roles with params
    - Display miscellaneous hiera settings
    - Edit miscellaneous hiera settings
    - Applies/Removes puppet roles
    
    Needs:
    - Filter for labs-appropriate available roles
    - Sort available roles in some sensible manner
    - Better highlight applied vs. potential roles
    - A bit more exception handling
    
    Bug: T91990
    Change-Id: I7f064073ba93ffb53369117f30db14772b0ab2de

---
+++ modules/openstack/files/liberty/horizon/puppettab/puppet_tables.py
@@ -0,0 +1,128 @@
+# Copyright (c) 2016 Andrew Bogott for Wikimedia Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from django.core import urlresolvers
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import tables
+
+import puppet_roles
+
+logging.basicConfig()
+LOG = logging.getLogger(__name__)
+
+
+def get_formatted_name(classrecord):
+    return classrecord.name.split('role::')[1]
+
+
+def get_formatted_params(classrecord):
+    if classrecord.params:
+        keysanddefaults = []
+        for key in classrecord.params.keys():
+            keysanddefaults.append("%s: %s" % (key, classrecord.params[key]))
+        return(";\n".join(keysanddefaults))
+
+
+def get_docs_for_class(classname):
+    allroles = puppet_roles.available_roles()
+    for role in allroles:
+        if role.name.split('role::')[1] == classname:
+            if role.docs:
+                return {'title': role.docs}
+            break
+    return {'title': '(No docs available)'}
+
+
+class RemoveRole(tables.LinkAction):
+    name = 'remove'
+    verbose_name = _("Remove Role")
+    classes = ("ajax-modal",)
+    data_type_singular = _("Role")
+
+    policy_rules = (("compute", "compute:delete"),)
+
+    def get_link_url(self, datum):
+        url = "horizon:project:puppet:removepuppetrole"
+        kwargs = {
+            'fqdn': datum.fqdn,
+            'tenantid': datum.tenant_id,
+            'instanceid': datum.instance_id,
+            'roleid': datum.name,
+        }
+        return urlresolvers.reverse(url, kwargs=kwargs)
+
+    def allowed(self, request, record=None):
+        return record.applied
+
+
+class ApplyRole(tables.LinkAction):
+    name = 'apply_role'
+    verbose_name = _("Apply Role")
+    classes = ("ajax-modal",)
+    icon = "plus"
+    policy_rules = (("compute", "compute:delete"),)
+
+    def get_link_url(self, datum):
+        url = "horizon:project:puppet:applypuppetrole"
+        kwargs = {
+            'fqdn': datum.fqdn,
+            'tenantid': datum.tenant_id,
+            'instanceid': datum.instance_id,
+            'roleid': datum.name,
+        }
+        return urlresolvers.reverse(url, kwargs=kwargs)
+
+    def allowed(self, request, record=None):
+        return (not record.applied)
+
+
+class PuppetTable(tables.DataTable):
+    applied = tables.Column('applied', verbose_name=_('Applied'), status=True)
+    name = tables.Column(get_formatted_name,
+                         verbose_name=_('Name'),
+                         cell_attributes_getter=get_docs_for_class)
+    params = tables.Column(get_formatted_params,
+                           verbose_name=_('Parameters'),
+                           sortable=False)
+    instance = tables.Column('instance',
+                             verbose_name=_('Instance'),
+                             hidden=True)
+    tenant = tables.Column('tenant',
+                           verbose_name=_('Tenant'),
+                           hidden=True)
+    tenant = tables.Column('fqdn',
+                           verbose_name=_('FQDN'),
+                           hidden=True)
+    roleid = tables.Column('name', verbose_name=_('ID'), hidden=True)
+
+    class Meta(object):
+        name = 'puppet'
+        row_actions = (ApplyRole, RemoveRole,)
+
+        # FIXME:  Add CSS magic to use this to highlight
+        #         applied records in the table
+        row_attrs = {
+            'role_applied': lambda record: record.applied
+        }
+
+    def get_object_id(self, datum):
+        return datum.name
+
+    def render_to_response(self, context, **response_kwargs):
+        LOG.warn("render_to_response 2: %s" %
+                 self.request.GET.get('format', 'html'))
