dc4c87b1ad53cc5ce111c9edb22b85b8f734db33
Andrew Bogott <abogott@wikimedia.org>
2016-06-14 10:52:09 -0500

    Horizon tab for modifying instance puppet config
    - Display available roles with docs and params
    - Display applied roles with params
    - Display miscellaneous hiera settings
    - Edit miscellaneous hiera settings
    - Apply/Removes puppet roles
    
    Bug: T91990
    Change-Id: I7f064073ba93ffb53369117f30db14772b0ab2de

---
+++ modules/openstack/files/liberty/horizon/puppet_tab_enable.py
@@ -0,0 +1,7 @@
+PANEL = 'puppettabdummypanel'
+PANEL_GROUP = 'compute'
+PANEL_DASHBOARD = 'project'
+ADD_PANEL = ('wikimediapuppettab.panel.PuppetTabDummyPanel')
+ADD_INSTALLED_APPS = ['wikimediapuppettab']
+AUTO_DISCOVER_STATIC_FILES = True
+ADD_SCSS_FILES = ['dashboard/puppet/puppet.scss']
---
+++ modules/openstack/manifests/horizon/puppetpanel.pp
@@ -0,0 +1,23 @@
+# Install a custom-for-wikimedia Horizon panel
+#  to manage instance puppet configuration
+class openstack::horizon::puppetpanel(
+    $openstack_version  = $::openstack::version)
+{
+    file { '/usr/lib/python2.7/dist-packages/wikimediapuppettab':
+        source  => "puppet:///modules/openstack/${openstack_version}/horizon/puppettab",
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['openstack-dashboard'],
+        notify  => Exec['djangorefresh'],
+        recurse => true
+    }
+    file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled/_1923_puppet_role_panel.py':
+        source  => "puppet:///modules/openstack/${openstack_version}/horizon/puppet_tab_enable.py",
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        notify  => Exec['djangorefresh'],
+        require => Package['openstack-dashboard'],
+    }
+}
