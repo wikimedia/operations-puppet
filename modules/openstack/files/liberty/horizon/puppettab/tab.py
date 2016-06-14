ae5fc0a1ad0d53bffa6953637d93c5529e1cbaa2
Andrew Bogott <abogott@wikimedia.org>
2016-06-14 10:52:09 -0500

    WIP:  Horizon tab for modifying instance puppet config
    Does:
    - Query a puppetmaster for available roles
    - Display available roles with docs and params
    
    Needs:
    - Actually apply/remove roles via the new puppet config API
    - Sort available roles in some sensible manner
    - Better highlight applied vs. potential roles
    - (maybe) Support arbitary hiera configs
    
    Bug: T91990
    Change-Id: I7f064073ba93ffb53369117f30db14772b0ab2de

---
+++ modules/openstack/files/liberty/horizon/puppettab/tab.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2016 Andrew Bogott for Wikimedia Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import tabs
+
+import puppet_tables as p_tables
+import puppet_roles
+
+logging.basicConfig()
+LOG = logging.getLogger(__name__)
+
+
+class PuppetTab(tabs.TableTab):
+    name = _("Puppet Configuration")
+    slug = "puppet"
+    table_classes = (p_tables.PuppetTable,)
+    template_name = "project/puppet/_detail_puppet.html"
+    preload = False
+
+    def get_instance_id(self):
+        return self.tab_group.kwargs['instance']
+
+    def get_puppet_data(self):
+        return [role.update_instance(self.get_instance_id()) for
+                role in puppet_roles.available_roles()]
