5630b3ec7aef337b872ad58dede89a3176921ee4
Marko Obrovac <mobrovac@wikimedia.org>
2016-06-16 09:56:40 +0200

    Parsoid: Move to service::node
    As of I41265ecd955259f29af0fc84a53a0af489a8ec79 and
    I84d47a2cacb8bacaa653c689028111645f536c1f, Parsoid is using
    service-runner as its underlying service-management library. This allows
    us to consolidate the Puppet manifests for it and use the service::node
    define to manage it in production.
    
    Note that the old role and class manifests have been preserved at this
    time. They should be cleaned up in a separate effort.
    
    Bug: T90668
    Change-Id: I3f4a56964c5af9d2e7321b72a30426ef54712324

--- modules/role/manifests/parsoid/testing.pp
+++ modules/role/manifests/parsoid/testing.pp
@@ -5,51 +5,9 @@ class role::parsoid::testing {
         description => 'Parsoid server (rt-testing, visual-diffing, etc.)'
     }
 
-    # Some visual diff node modules reference 'node' which this package provides
-    require_package('nodejs-legacy')
-
-    include role::parsoid::common
-
-    group { 'parsoid':
-        ensure => present,
-        name   => 'parsoid',
-        system => true,
-    }
-
-    user { 'parsoid':
-        gid        => 'parsoid',
-        home       => '/var/lib/parsoid',
-        managehome => true,
-        system     => true,
-    }
-
-    # Clone the git repo. Omit ensure => latest since
-    # we want to update the repository when necessary.
-    # Use mode 0775 so that members of wikidev group
-    # can update the code via git pull.
-    git::clone { 'mediawiki/services/parsoid/deploy':
-        owner              => 'root',
-        group              => 'wikidev',
-        recurse_submodules => true,
-        directory          => '/srv/parsoid',
-        mode               => '0775',
-        before             => Service['parsoid'],
-    }
-
-    file { '/lib/systemd/system/parsoid.service':
-        source => 'puppet:///modules/parsoid/parsoid_testing.systemd.service',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        before => Service['parsoid'],
-    }
-
-    file { '/var/log/parsoid':
-        ensure => directory,
-        owner  => 'parsoid',
-        group  => 'parsoid',
-        mode   => '0775',
-        before => Service['parsoid'],
+    class { '::parsoid':
+        port          => 8142,
+        settings_file => '/srv/deployment/parsoid/deploy/src/localsettings.js',
     }
 
     file { '/usr/local/bin/update_parsoid.sh':
@@ -60,20 +18,12 @@ class role::parsoid::testing {
     }
 
     # Use this parsoid instance for parsoid rt-testing
-    file { '/srv/parsoid/src/localsettings.js':
+    file { '/srv/deployment/parsoid/deploy/src/localsettings.js':
         content => template('testreduce/parsoid-rt-client.rttest.localsettings.js.erb'),
         owner   => 'root',
         group   => 'wikidev',
         mode    => '0444',
         before  => Service['parsoid'],
-    }
-
-    service { 'parsoid':
-        hasstatus  => true,
-        hasrestart => true,
-        subscribe  => [
-            File['/lib/systemd/system/parsoid.service'],
-        ],
     }
 
     # mysql client and configuration to provide command line access to
