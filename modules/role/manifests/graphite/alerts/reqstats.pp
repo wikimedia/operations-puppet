825a79a4173b2d5b1828adaefbd7e8fb893ad9d2
Guillaume Lederrey <glederrey@wikimedia.org>
2016-06-16 11:18:55 +0200

    Fixed typo (cron instead of from).
    This should be a noop as the default value for 'from' is '10min' anyway.
    Still it make sense to fix it.
    
    Reduced verbosity by using create_resources().
    
    Change-Id: I71d90be11c93cd32fc0ddba371f21cf00925372c

--- modules/role/manifests/graphite/alerts/reqstats.pp
+++ modules/role/manifests/graphite/alerts/reqstats.pp
@@ -2,80 +2,60 @@ class role::graphite::alerts::reqstats {
 
     # Monitor production 5xx rates
 
-    $settings = {'warning' => 250, 'critical' => 1000, from => '10min'}
-
-    # sites aggregates
-    monitoring::graphite_threshold { 'reqstats-5xx-eqiad':
-        description     => 'Eqiad HTTP 5xx reqs/min',
-        metric          => 'sumSeries(varnish.eqiad.*.frontend.request.client.status.5xx.sum)',
-        warning         => $settings['warning'],
-        critical        => $settings['critical'],
-        from            => $settings['cron'],
-        nagios_critical => false,
+    $default_settings = {
+        warning         => 250,
+        critical        => 1000,
+        from            => '10min',
+        nagios_critical => false
     }
 
-    monitoring::graphite_threshold { 'reqstats-5xx-esams':
-        description     => 'Esams HTTP 5xx reqs/min',
-        metric          => 'sumSeries(varnish.esams.*.frontend.request.client.status.5xx.sum)',
-        warning         => $settings['warning'],
-        critical        => $settings['critical'],
-        from            => $settings['cron'],
-        nagios_critical => false,
+    $per_sites_aggregates = {
+        'reqstats-5xx-eqiad' => {
+            description => 'Eqiad HTTP 5xx reqs/min',
+            metric      => 'sumSeries(varnish.eqiad.*.frontend.request.client.status.5xx.sum)',
+        },
+        'reqstats-5xx-esams' => {
+            description => 'Esams HTTP 5xx reqs/min',
+            metric      => 'sumSeries(varnish.esams.*.frontend.request.client.status.5xx.sum)',
+        },
+        'reqstats-5xx-codfw' => {
+            description => 'Codfw HTTP 5xx reqs/min',
+            metric      => 'sumSeries(varnish.codfw.*.frontend.request.client.status.5xx.sum)',
+        },
+        'reqstats-5xx-ulsfo' => {
+            description => 'Ulsfo HTTP 5xx reqs/min',
+            metric      => 'sumSeries(varnish.ulsfo.*.frontend.request.client.status.5xx.sum)',
+        },
     }
 
-    monitoring::graphite_threshold { 'reqstats-5xx-codfw':
-        description     => 'Codfw HTTP 5xx reqs/min',
-        metric          => 'sumSeries(varnish.codfw.*.frontend.request.client.status.5xx.sum)',
-        warning         => $settings['warning'],
-        critical        => $settings['critical'],
-        from            => $settings['cron'],
-        nagios_critical => false,
+    $per_cache_aggregates = {
+        'reqstats-5xx-text' => {
+            description => 'Text HTTP 5xx reqs/min',
+            metric      => 'sumSeries(varnish.*.text.frontend.request.client.status.5xx.sum)',
+        },
+        'reqstats-5xx-uploads' => {
+            description => 'Uploads HTTP 5xx reqs/min',
+            metric      => 'sumSeries(varnish.*.uploads.frontend.request.client.status.5xx.sum)',
+        },
+        'reqstats-5xx-misc' => {
+            description => 'Misc HTTP 5xx reqs/min',
+            metric      => 'sumSeries(varnish.*.misc.frontend.request.client.status.5xx.sum)',
+        },
+        'reqstats-5xx-maps' => {
+            description   => 'Maps HTTP 5xx reqs/min',
+            metric        => 'sumSeries(varnish.*.maps.frontend.request.client.status.5xx.sum)',
+            contact_group => 'admins,interactive-team',
+        },
     }
 
-    monitoring::graphite_threshold { 'reqstats-5xx-ulsfo':
-        description     => 'Ulsfo HTTP 5xx reqs/min',
-        metric          => 'sumSeries(varnish.ulsfo.*.frontend.request.client.status.5xx.sum)',
-        warning         => $settings['warning'],
-        critical        => $settings['critical'],
-        from            => $settings['cron'],
-        nagios_critical => false,
-    }
-
-    # per-cache aggregates
-    monitoring::graphite_threshold { 'reqstats-5xx-text':
-        description     => 'Text HTTP 5xx reqs/min',
-        metric          => 'sumSeries(varnish.*.text.frontend.request.client.status.5xx.sum)',
-        warning         => $settings['warning'],
-        critical        => $settings['critical'],
-        from            => $settings['cron'],
-        nagios_critical => false,
-    }
-
-    monitoring::graphite_threshold { 'reqstats-5xx-uploads':
-        description     => 'Uploads HTTP 5xx reqs/min',
-        metric          => 'sumSeries(varnish.*.uploads.frontend.request.client.status.5xx.sum)',
-        warning         => $settings['warning'],
-        critical        => $settings['critical'],
-        from            => $settings['cron'],
-        nagios_critical => false,
-    }
-
-    monitoring::graphite_threshold { 'reqstats-5xx-misc':
-        description     => 'Misc HTTP 5xx reqs/min',
-        metric          => 'sumSeries(varnish.*.misc.frontend.request.client.status.5xx.sum)',
-        warning         => $settings['warning'],
-        critical        => $settings['critical'],
-        from            => $settings['cron'],
-        nagios_critical => false,
-    }
-
-    monitoring::graphite_threshold { 'reqstats-5xx-maps':
-        description     => 'Maps HTTP 5xx reqs/min',
-        metric          => 'sumSeries(varnish.*.maps.frontend.request.client.status.5xx.sum)',
-        warning         => $settings['warning'],
-        critical        => $settings['critical'],
-        from            => $settings['cron'],
-        nagios_critical => false,
-        contact_group   => 'admins,team-interactive',
-    }
+    create_resources(
+        monitoring::graphite_threshold,
+        $per_sites_aggregates,
+        $default_settings,
+    )
+    create_resources(
+        monitoring::graphite_threshold,
+        $per_cache_aggregates,
+        $default_settings,
+    )
 }
