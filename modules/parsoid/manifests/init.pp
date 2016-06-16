50421e0f7fd8b2791f96636b8525079797154fef
Marko Obrovac <mobrovac@wikimedia.org>
2016-06-16 09:56:40 +0200

    WIP: Parsoid: Move to service::node
    Change-Id: I3f4a56964c5af9d2e7321b72a30426ef54712324

---
+++ modules/parsoid/manifests/init.pp
@@ -0,0 +1,20 @@
+# == Class: parsoid
+class parsoid(
+    $port          => 8000,
+    $settings_file => 'conf/wmf/localsettings.js',
+) {
+
+    service::node { 'parsoid':
+        port            => $port,
+        starter_module  => 'src/lib/index.js',
+        entrypoint      => 'apiServiceWorker',
+        starter_script  => 'src/bin/server.js',
+        config          => {
+            localsettings => $settings_file,
+        },
+        heartbeat_to    => 300000,
+        healthcheck_url => '/',
+        has_spec        => false,
+    }
+
+}
