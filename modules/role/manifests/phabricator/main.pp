49a44e9604df7b2414c459d196749d91cd6e921f
Danny B <danny.b@email.cz>
2016-06-16 20:00:19 -0500

    Rewrite rules for git.wikimedia.org
    Redirect gitblit urls to phabricator (diffusion) equivalents.
    
    Bug: T137224
    Change-Id: I67ad308f9e6373e5234cb2d83006457d6f467bf8

--- modules/role/manifests/phabricator/main.pp
+++ modules/role/manifests/phabricator/main.pp
@@ -54,6 +54,7 @@ class role::phabricator::main {
             'events.listeners'                       => [],
             'diffusion.allow-http-auth'              => true,
             'diffusion.ssh-host'                     => 'git-ssh.wikimedia.org',
+            'gitblit.hostname'                       => 'git.wikimedia.org',
         },
     }
     # lint:endignore
