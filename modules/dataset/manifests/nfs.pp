aca1216052616fb4605cad0d9b1ac42f5695edb4
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-06-17 18:08:02 +0200

    Move dataset ferm rules into the role
    Ferm rules/services should be defined in the roles, not the modules.
    
    Change-Id: I1647c44b30861c47bba92d05d154acd7f2b18fe7

--- modules/dataset/manifests/nfs.pp
+++ modules/dataset/manifests/nfs.pp
@@ -53,28 +53,4 @@ class dataset::nfs($enable=true) {
         description   => 'NFS',
         check_command => 'check_tcp!2049',
     }
-
-    ferm::service { 'nfs_rpc_mountd':
-        proto  => 'tcp',
-        port   => '32767',
-        srange => '$INTERNAL',
-    }
-
-    ferm::service { 'nfs_rpc_statd':
-        proto  => 'tcp',
-        port   => '32765',
-        srange => '$INTERNAL',
-    }
-
-    ferm::service { 'nfs_portmapper_udp':
-        proto  => 'udp',
-        port   => '111',
-        srange => '$INTERNAL',
-    }
-
-    ferm::service { 'nfs_portmapper_tcp':
-        proto  => 'tcp',
-        port   => '111',
-        srange => '$INTERNAL',
-    }
 }
