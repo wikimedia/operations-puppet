75b58068e05e9642b1eefcdbf9a7d2b4e142fd5a
Faidon Liambotis <faidon@wikimedia.org>
2016-06-17 16:48:11 +0300

    ripeatlas: add IPv6 measurements as well
    Change-Id: Iff1ae0f2b0eea0a7558ada85f252f82572b5a547

--- modules/icinga/manifests/monitor/ripeatlas.pp
+++ modules/icinga/manifests/monitor/ripeatlas.pp
@@ -2,25 +2,35 @@
 #
 class icinga::monitor::ripeatlas {
 
+    # eqiad
     @monitoring::host { 'ripe-atlas-eqiad':
         ip_address => '208.80.155.69',
     }
 
-    monitoring::service { 'eqiad-icmp-ipv4-reachability':
-        description           => 'test icmp reachability to eqiad',
+    monitoring::service { 'atlas-ping-eqiad-ipv4':
+        description           => 'IPv4 ping to eqiad',
         check_command         => 'check_ripe_atlas!1790945!50!19',
         host                  => 'ripe-atlas-eqiad',
         normal_check_interval => 10,
         retry_check_interval  => 5,
         contact_group         => 'admins',
     }
+    monitoring::service { 'atlas-ping-eqiad-ipv6':
+        description           => 'IPv6 ping to eqiad',
+        check_command         => 'check_ripe_atlas!1790947!50!19',
+        host                  => 'ripe-atlas-eqiad',
+        normal_check_interval => 10,
+        retry_check_interval  => 5,
+        contact_group         => 'admins',
+    }
 
+    # codfw
     @monitoring::host { 'ripe-atlas-codfw':
         ip_address => '208.80.152.244',
     }
 
-    monitoring::service { 'codfw-icmp-ipv4-reachability':
-        description           => 'test icmp reachability to codfw',
+    monitoring::service { 'atlas-ping-codfw-ipv4'
+        description           => 'IPv4 ping to codfw',
         check_command         => 'check_ripe_atlas!1791210!50!19',
         host                  => 'ripe-atlas-codfw',
         normal_check_interval => 10,
@@ -28,13 +38,32 @@ class icinga::monitor::ripeatlas {
         contact_group         => 'admins',
     }
 
+    monitoring::service { 'atlas-ping-codfw-ipv6'
+        description           => 'IPv6 ping to codfw',
+        check_command         => 'check_ripe_atlas!1791212!50!19',
+        host                  => 'ripe-atlas-codfw',
+        normal_check_interval => 10,
+        retry_check_interval  => 5,
+        contact_group         => 'admins',
+    }
+
+    # ulsfo
     @monitoring::host { 'ripe-atlas-ulsfo':
         ip_address => '198.35.26.244',
     }
 
-    monitoring::service { 'ulsfo-icmp-ipv4-reachability':
-        description           => 'test icmp reachability to ulsfo',
+    monitoring::service { 'atlas-ping-ulsfo-ipv4'
+        description           => 'IPv4 ping to ulsfo',
         check_command         => 'check_ripe_atlas!1791307!50!19',
+        host                  => 'ripe-atlas-ulsfo',
+        normal_check_interval => 10,
+        retry_check_interval  => 5,
+        contact_group         => 'admins',
+    }
+
+    monitoring::service { 'atlas-ping-ulsfo-ipv6'
+        description           => 'IPv6 ping to ulsfo',
+        check_command         => 'check_ripe_atlas!1791309!50!19',
         host                  => 'ripe-atlas-ulsfo',
         normal_check_interval => 10,
         retry_check_interval  => 5,
