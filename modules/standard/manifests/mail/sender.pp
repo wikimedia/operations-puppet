78b5e6ae5f379d5e4caebb2c1ae3e9026f7ce30d
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-06-17 17:08:42 +0200

    Restart exim daily on Monday to Friday
    This allows to pick up updated libraries automatically (e.g. in case of security
    updates or other bugfix updates in a stable release).
    
    Exim restarts are reliable and stateless.
    
    Bug: T135991
    Change-Id: Iefc9517fe870a5a3e1ac1d448a17d6b65c569037

--- modules/standard/manifests/mail/sender.pp
+++ modules/standard/manifests/mail/sender.pp
@@ -3,4 +3,15 @@ class standard::mail::sender {
         queuerunner => 'queueonly',
         config      => template("mail/exim4.minimal.${::realm}.erb"),
     }
+
+    # Perform a daily restart on Monday to Friday to pick up updated
+    # versions in linked-in libraries etc.
+    cron { 'exim4_daily_restart':
+        ensure  => present,
+        command => '(pidof -x /usr/sbin/exim4 1> /dev/null) && /usr/sbin/service exim4 restart',
+        user    => 'root',
+        hour    => fqdn_rand(23, 'exim4_daily_restart'),
+        minute  => fqdn_rand(59, 'exim4_daily_restart'),
+        weekday => '1-5',
+    }
 }
