056a3661c9ef78c99d76ef8890243bb3b7cf9bd8
rolf jordi <maturain@gmail.com>
2016-06-19 17:39:05 +0200

    fix puppet unit test in elastic search
    default values have changed in elastic search module but test was not updated
    
     Change-Id: Icbfa32091e1f3dc653ffcd12bcd6def5755ef48f
    
    Change-Id: I78518f5e34e22d5974d0c9912a6c388d729305e2

--- modules/elasticsearch/spec/defines/https_rspec.rb
+++ modules/elasticsearch/spec/defines/https_rspec.rb
@@ -7,6 +7,19 @@ describe 'elasticsearch::https', :type => :class do
   } }
 
   context 'with default parameters' do
+    it 'should ensure that certificates are present' do
+      should contain_file('/etc/nginx/ssl/cert.pem').with({ 'ensure' => 'present' })
+      should contain_file('/etc/nginx/ssl/server.key').with({ 'ensure' => 'present' })
+    end
+
+    it 'should ensure that nginx package is present' do
+      should contain_package('nginx-full').with({ 'ensure' => 'present' })
+    end
+  end
+
+  context 'with ensure => absent' do
+    let(:params) { { :ensure => 'absent' } }
+
     it 'should ensure that certificates are absent' do
       should contain_file('/etc/nginx/ssl/cert.pem').with({ 'ensure' => 'absent' })
       should contain_file('/etc/nginx/ssl/server.key').with({ 'ensure' => 'absent' })
@@ -14,19 +27,6 @@ describe 'elasticsearch::https', :type => :class do
 
     it 'should ensure that nginx package is absent' do
       should contain_package('nginx-full').with({ 'ensure' => 'absent' })
-    end
-  end
-
-  context 'with ensure => present' do
-    let(:params) { { :ensure => 'present' } }
-
-    it 'should ensure that certificates are present' do
-      should contain_file('/etc/nginx/ssl/cert.pem').with({ 'ensure' => 'present' })
-      should contain_file('/etc/nginx/ssl/server.key').with({ 'ensure' => 'present' })
-    end
-
-    it 'should ensure that nginx package is installed' do
-      should contain_package('nginx-full').with({ 'ensure' => 'present' })
     end
   end
 
