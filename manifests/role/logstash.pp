09a2c3e04c898b0bd57e9c967db9987437b71275
Erik Bernhardson <ebernhardson@wikimedia.org>
2016-06-21 14:47:13 -0700

    Duplicate logstash output to alternate elasticsearch cluster
    To test the migration to elasticsearch 2.3 we need to start sending
    some real-ish data at it. Add definitions for deployment-logstash3
    which will be installed with elasticsearch 2.x, and necessary config
    for logstash on deployment-logstash2 to duplicate insert events to it.
    
    Change-Id: I28af9ed96516efcbae94dc89144442f6043eb512

--- manifests/role/logstash.pp
+++ manifests/role/logstash.pp
@@ -5,9 +5,12 @@
 #
 # == Parameters:
 # - $statsd_host: Host to send statsd data to.
+# - $logstash_alt_host: Additional host to send logstash elasticsearch
+#    output to. Used for testing elasticsearch 2.3 deployment.
 #
 class role::logstash (
     $statsd_host,
+    $logstash_alt_host = undef,
 ) {
     include ::role::logstash::elasticsearch
     include ::logstash
@@ -138,6 +141,17 @@ class role::logstash (
         priority        => 90,
         template        => '/etc/logstash/elasticsearch-template.json',
         require         => File['/etc/logstash/elasticsearch-template.json'],
+    }
+
+    if ($logstash_alt_host) {
+        logstash::output::elasticsearch { 'logstash-alt':
+            host            => $logstash_alt_host,
+            guard_condition => '"es" in [tags]',
+            manage_indices  => true,
+            priority        => 90,
+            template        => '/etc/logstash/elasticsearch-template.json',
+            require         => File['/etc/logstash/elasticsearch-template.json'],
+        }
     }
 
     logstash::output::statsd { 'MW_channel_rate':
