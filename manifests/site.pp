375a7d6e27a1b5c8371bdb6ac49c1fdf841524c1
Guillaume Lederrey <glederrey@wikimedia.org>
2016-06-22 15:16:49 +0200

    Configuring new elastic1038-1042 servers
    Bug: T138329
    Change-Id: I4463385b41e9b29b02fee17630b7c28f0fcd47f6

--- manifests/site.pp
+++ manifests/site.pp
@@ -948,12 +948,12 @@ node 'einsteinium.wikimedia.org' {
     include base::firewall
 }
 
-node /^elastic10[0-2][0-9]\.eqiad\.wmnet/ {
+node /^elastic10[0-3][0-9]\.eqiad\.wmnet/ {
     role elasticsearch::server
     include base::firewall
     include standard
 }
-node /^elastic103[0-7]\.eqiad\.wmnet/ {
+node /^elastic104[0-2]\.eqiad\.wmnet/ {
     role elasticsearch::server
     include base::firewall
     include standard
