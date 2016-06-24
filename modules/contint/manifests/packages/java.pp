a91c0061d8d3b51c9eec561a8b1fba8392947da9
Antoine Musso <hashar@free.fr>
2016-06-24 11:34:00 +0200

    contint: Java 8 on Jessie slaves
    Some Android related utilities are now requiring Java 8. It is available
    in Jessie via jessie-backports (8u91-b14-1~bpo8+1).  The Trusty version
    is most probably stall/unmaintained (8u40~b09-1+wm1).
    
    Install Java 8 solely on Jessie slaves.
    
    Bug: T138506
    Change-Id: Ifa9a0c8e3bc3b7e4cd45e6268f502f15ff06afe4

--- modules/contint/manifests/packages/java.pp
+++ modules/contint/manifests/packages/java.pp
@@ -2,6 +2,10 @@ class contint::packages::java {
 
     require_package('openjdk-7-jdk')
 
+    if os_version('debian >= jessie') {
+        require_package('openjdk-8-jdk')
+    }
+
     package { 'maven2':
         ensure => present,
     }
