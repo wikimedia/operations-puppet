f8ab24cc29774636faa10ccded1b063adedb940f
Daniel Zahn <dzahn@wikimedia.org>
2016-06-25 02:20:34 -0700

    diamond: move _lib classes to own files
    Each class should have its own file, per style guide
    and lint check.
    
    Change-Id: Ibbdab575b26b3f06d9986351ac01b5db53534872

--- modules/diamond/manifests/collector/nagios.pp
+++ modules/diamond/manifests/collector/nagios.pp
@@ -36,16 +36,3 @@ define diamond::collector::nagios (
     }
 }
 
-# support class, to be include'd multiple times
-class diamond::collector::nagios_lib {
-    diamond::collector { 'Nagios':
-        source   => 'puppet:///modules/diamond/collector/nagios.py'
-    }
-
-    file { '/etc/diamond/nagios.d':
-        ensure => directory,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-    }
-}
