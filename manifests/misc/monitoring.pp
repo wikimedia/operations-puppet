9932d37afcf1de33ad2216b7b80e1cb32173d7fa
Daniel Zahn <dzahn@wikimedia.org>
2016-06-25 03:14:36 -0700

    rm misc::monitoring::view::hadoop
    Looking at the view in Ganglia at
    https://ganglia.wikimedia.org/latest/?r=hour&cs=&ce=&m=cpu_report&s=by+name&c=&tab=v&vn=&hide-hf=false
    
    it is not working "No matching metrics detected .."
    
    and we would like to get rid of the remnants in misc/monitoring
    
    Change-Id: I56b7a200bceaf7ddbc118665c387d51c29889755

--- manifests/misc/monitoring.pp
+++ manifests/misc/monitoring.pp
@@ -7,11 +7,6 @@ class misc::monitoring::views {
         topic_regex => 'webrequest_.+',
     }
 
-    class { 'misc::monitoring::view::hadoop':
-        master       => 'analytics1001.eqiad.wmnet',
-        worker_regex => 'analytics10(11|[3-9]|20).eqiad.wmnet',
-    }
-
     include misc::monitoring::views::dns
 }
 
@@ -229,102 +224,6 @@ class misc::monitoring::view::kafkatee($kafkatee_host_regex, $topic_regex = '.+'
         ],
     }
 }
-
-
-# == Class misc::monitoring::view::hadoop
-#
-class misc::monitoring::view::hadoop($master, $worker_regex, $ensure = 'present') {
-    ganglia::web::view { 'hadoop':
-        ensure => $ensure,
-        graphs => [
-            # ResourceManager active applications
-            {
-                'host_regex'   => $master,
-                'metric_regex' => 'Hadoop.ResourceManager.QueueMetrics.*ActiveApplications',
-                'type'         => 'stack',
-            },
-            # ResourceManager failed applications
-            {
-                'host_regex'   => $master,
-                'metric_regex' => 'Hadoop.ResourceManager.QueueMetrics.*AppsFailed',
-                'type'         => 'stack',
-            },
-            # NodeManager containers running
-            {
-                'host_regex'   => $worker_regex,
-                'metric_regex' => 'Hadoop.NodeManager.NodeManagerMetrics.ContainersRunning',
-                'type'         => 'stack',
-            },
-            # NodeManager Allocated Memeory GB
-            {
-                'host_regex'   => $worker_regex,
-                'metric_regex' => 'Hadoop.NodeManager.NodeManagerMetrics.AllocatedGB',
-                'type'         => 'stack',
-            },
-            # Worker Node bytes_in
-            {
-                'host_regex'   => $worker_regex,
-                'metric_regex' => 'bytes_in',
-                'type'         => 'stack',
-            },
-            # Worker Node bytes_out
-            {
-                'host_regex'   => $worker_regex,
-                'metric_regex' => 'bytes_out',
-                'type'         => 'stack',
-            },
-            # Primary NameNode File activity
-            {
-                'host_regex'   => $master,
-                'metric_regex' => 'Hadoop.NameNode.NameNodeActivity.Files(Created|Deleted|Renamed|Appended)',
-                'type'         => 'line',
-            },
-            # Worker Node /proc/diskstat bytes written per second
-            {
-            # FIXME - top-scope var without namespace ($worker_regex), will break in puppet 2.8
-            # lint:ignore:variable_scope
-                'host_regex'   => $worker_regex,
-                'metric_regex' => "diskstat_(${kafka_log_disks_regex})_write_bytes_per_sec",
-                'type'         => 'stack',
-            },
-            # /proc/diskstat bytes read per second
-            {
-                'host_regex'   => $worker_regex,
-                'metric_regex' => "diskstat_(${kafka_log_disks_regex})_read_bytes_per_sec",
-                'type'         => 'stack',
-            },
-            # Worker Node /proc/diskstat disk utilization %
-            {
-                'host_regex'   => $worker_regex,
-                'metric_regex' => "diskstat_(${kafka_log_disks_regex})_percent_io_time",
-                'type'         => 'line',
-            },
-            # Worker Node /proc/diskstat IO time
-            {
-                'host_regex'   => $worker_regex,
-                'metric_regex' => "diskstat_(${kafka_log_disks_regex})_io_time",
-                'type'         => 'line',
-            },
-            # lint:endignore
-            # Worker Node 15 minute load average
-            {
-                'host_regex'   => $worker_regex,
-                'metric_regex' => 'load_fifteen',
-                'type'         => 'line',
-            },
-            # Worker Node IO wait
-            {
-                'host_regex'   => $worker_regex,
-                'metric_regex' => 'cpu_wio',
-                'type'         => 'line',
-            },
-        ],
-    }
-}
-
-
-
-
 
 
 # == Class misc::monitoring::view::analytics::data
