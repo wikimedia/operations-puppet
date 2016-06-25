3656375ccdb433f9f452dc9ac2d195c3faa252cc
Daniel Zahn <dzahn@wikimedia.org>
2016-06-25 02:39:49 -0700

    openstack: move instancersync define to own file
    Change-Id: Ibb86285d99123c3e04f5a60d04c98864d8ca4e26

--- modules/openstack/manifests/nova/compute.pp
+++ modules/openstack/manifests/nova/compute.pp
@@ -207,13 +207,3 @@ class openstack::nova::compute(
     }
 }
 
-# defines an rsync server on an instance
-define instancersync (
-    $hostname = undef) {
-
-    rsync::server::module { "nova_instance_rsync_${hostname}":
-        path        => '/var/lib/nova/instances',
-        read_only   => 'no',
-        hosts_allow => ["${hostname}.${::site}.wmnet"],
-    }
-}
