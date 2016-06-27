124c6c008d23e221477a3f963a92cddf00a87cd7
Guillaume Lederrey <glederrey@wikimedia.org>
2016-06-27 21:05:15 +0200

    Elasticsearch - Increase alerting threshold for alerts on codfw response time
    We have seen an increase in alerts from codfw response times. Those alerts
    are transient and generate unwanted noise.
    
    Codfw sees much less traffic than eqiad and thus response time are more
    subject to fluctuation due to indexing. Raising the threshold for codfw
    does make sense and is not just a way to hide an issue.
    
    Change-Id: Ie38750175da0d8abf5abf05d76ac80a4c83c5399

--- modules/role/manifests/elasticsearch/alerts.pp
+++ modules/role/manifests/elasticsearch/alerts.pp
@@ -13,8 +13,8 @@ class role::elasticsearch::alerts {
         description   => 'CirrusSearch codfw 95th percentile latency',
         metric        => 'transformNull(MediaWiki.CirrusSearch.codfw.requestTime.p95, 0)',
         from          => '10min',
-        warning       => '500',
-        critical      => '1000',
+        warning       => '1000',
+        critical      => '1500',
         percentage    => '20',
         contact_group => 'team-discovery',
     }
