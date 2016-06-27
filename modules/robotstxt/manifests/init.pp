1203a8c57a1a98605502ce78073bc06e9ff853c1
Chad Horohoe <chadh@wikimedia.org>
2016-06-27 15:35:42 -0700

    WIP: Trying to automate generation of robots.txt files
    Change-Id: Ic96df2d1b256048dfc22cb547837dfe48503b597

---
+++ modules/robotstxt/manifests/init.pp
@@ -0,0 +1,40 @@
+# == Class: robotstxt
+#
+# This is a utility module to easily write a robots.txt file
+#
+# == Parameters
+#
+# [*title*]
+#   The class title is used as the path to place the file
+#
+# [*ensure*]
+#   Ensure the rule file exists (or not)
+#
+# [*rules*]
+#   An array of hash tables that can define comments/useragents/paths to filter.
+#   The agents and paths can be arrays. The format is as follows:
+#     [
+#       {
+#         'comment'    => 'Block the bad bots from everything!',
+#         'useragents' => ['FooBot', 'BarBot'],
+#         'paths'      => '/',
+#       },
+#       {
+#         'comment'    => 'Block the bad path from everyone!',
+#         'useragents' => '*',
+#         'paths'      => ['/badpath', '/reallybadpaths'],
+#       }
+#     ]
+#
+class robotstxt (
+    $ensure = present,
+    $rules  = [],
+) {
+    file { "${title}/robots.txt":
+        ensure  => $ensure,
+        content => template('robotstxt/robots.txt.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+}
