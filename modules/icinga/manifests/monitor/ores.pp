054dd9f54f93fecc6aed02eacf06ee081186422e
Nathaniel Schaaf <nschaaf@wikimedia.org>
2016-06-29 12:03:58 +0200

    Check all ores web nodes
    Bug: T134782
    Change-Id: I99b30d0999f7af124d1a354bd414cb948c70b868

--- modules/icinga/manifests/monitor/ores.pp
+++ modules/icinga/manifests/monitor/ores.pp
@@ -16,6 +16,21 @@ class icinga::monitor::ores {
         contact_group => 'team-ores',
     }
 
+    $realservers = hiera('role::labs::ores::lb::realservers')
+
+    define monitor_ores_labs_web_node ($realserver = $title) {
+        $server_parts = split($realserver, ':')
+        $server = $server_parts[0]
+        monitoring::service { "ores_web_node_labs_${server}":
+            description   => "ORES web node labs ${server}",
+            check_command => "check_ores_workers!oresweb/${server}",
+            host          => 'ores.wmflabs.org',
+            contact_group => 'team-ores',
+        }
+    }
+
+    monitor_ores_labs_web_node { $realservers: }
+
     # T121656
     monitoring::service { 'ores_worker_labs':
         description   => 'ORES worker labs',
