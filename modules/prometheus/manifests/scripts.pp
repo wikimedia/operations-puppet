b5b0215a7fb073f36431b743846dda19253f17fd
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-06-29 15:05:37 +0100

    prometheus: generate mysql targets from mw config
    Introduce a PHP script used to parse mediawiki-config and output a suitable
    prometheus targets configuration. Note that even though the output is JSON and
    prometheus expects YAML, it works because valid JSON is valid YAML.
    
    The labels and the grouping is a first iteration and can be changed, at the
    moment it looks like this e.g. for s6:
    
    {
        "labels": {
            "shard": "s6",
            "role": "master"
        },
        "targets": [
            "db1050.eqiad.wmnet:9104"
        ]
    },
    {
        "labels": {
            "shard": "s6",
            "role": "slave"
        },
        "targets": [
            "db1050.eqiad.wmnet:9104",
            "db1022.eqiad.wmnet:9104",
            "db1030.eqiad.wmnet:9104",
            "db1037.eqiad.wmnet:9104",
            "db1085.eqiad.wmnet:9104",
            "db1088.eqiad.wmnet:9104",
            "db1093.eqiad.wmnet:9104"
        ]
    },
    
    Bug: T126757
    Change-Id: I681e2728fdd2dfa411c41b4024409f0b5847dcba

--- modules/prometheus/manifests/scripts.pp
+++ modules/prometheus/manifests/scripts.pp
@@ -7,12 +7,23 @@ class prometheus::scripts {
     require_package('python3-yaml')
 
     # output all nova instances for the current labs project as prometheus
-    # 'targets'
+    # 'targets' for 'file service discovery' to pick up
     file { '/usr/local/bin/prometheus-labs-targets':
         ensure => file,
         mode   => '0555',
         owner  => 'root',
         group  => 'root',
         source => 'puppet:///modules/prometheus/usr/local/bin/prometheus-labs-targets',
+    }
+
+    require_package('php5-cli')
+    # output prometheus 'file service discovery' configuration based on
+    # mediawiki MySQL configuration
+    file { '/usr/local/bin/prometheus-mysql-mw-targets':
+        ensure => file,
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/prometheus/usr/local/bin/prometheus-mysql-mw-targets',
     }
 }
