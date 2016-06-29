fbf493441263453cda2e06e48d55be13da0a6a58
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-06-29 15:05:49 +0100

    prometheus: add mysql mediawiki production db discovery
    Use git::clone to make sure there is an updated copy of mediawiki-config and
    update prometheus mysql targets from that.
    
    Bug: T126757
    Change-Id: Iabbecb32d2beeb6ffd4909a06fe1bb4295c79c3a

--- modules/role/manifests/prometheus/ops.pp
+++ modules/role/manifests/prometheus/ops.pp
@@ -1,9 +1,41 @@
 class role::prometheus::ops {
+    $mysql_mw_targets_file = '/srv/prometheus/ops/targets/mysql-mw.yml'
+    $mysql_mw_db_config = "/srv/mediawiki-config/wmf-config/db-${::site}.php"
+
     prometheus::server { 'ops':
-        listen_address => '127.0.0.1:9900',
+        listen_address       => '127.0.0.1:9900',
+        scrape_configs_extra => [
+            {
+                'job_name'        => 'mysql-mw',
+                'file_sd_configs' => [ {
+                    'names' => [ $mysql_mw_targets_file ]
+                } ],
+            },
+        ],
     }
 
     prometheus::web { 'ops':
         proxy_pass => 'http://localhost:9900/ops',
+    }
+
+    include ::prometheus::scripts
+
+    git::clone { 'operations/mediawiki-config':
+        ensure    => 'latest',
+        directory => '/srv/mediawiki-config',
+        notify    => Exec['prometheus_mysql_mw_targets'],
+    }
+
+    $mysql_mw_targets_command = "/usr/local/bin/prometheus-mysql-mw-targets \
+ --db-config ${mysql_mw_db_config} \
+ --target-suffix .${::site}.wmnet:9104 > ${mysql_mw_targets_file}.$$ \
+ && mv ${mysql_mw_targets_file}.$$ ${mysql_mw_targets_file}"
+
+    exec { 'prometheus_mysql_mw_targets':
+        command => $mysql_mw_targets_command,
+        require => [
+            File['/usr/local/bin/prometheus-mysql-mw-targets'],
+            Git::Clone['operations/mediawiki-config'],
+        ],
     }
 }
