c770d451477dad39ede9fbfcf5f529aed03c1cc0
Chad Horohoe <chadh@wikimedia.org>
2016-06-29 12:00:02 -0700

    Gerrit: minor (no-op) manifest cleanup
    - Remove unused robots.txt stuff
    - Rename main class to gerrit instead of gerrit::instance
    
    Change-Id: I760dbf277e05482ab23e30a9ca85a0b9274f4d04

--- modules/gerrit/manifests/proxy.pp
+++ modules/gerrit/manifests/proxy.pp
@@ -8,16 +8,6 @@ $ssl_cert_key= '') {
         content => template('gerrit/gerrit.wikimedia.org.erb'),
     }
 
-    # We don't use gitweb anymore, so we're going to allow spiders again
-    # If it becomes a problem, just set ensure => present again
-    file { '/var/www/robots.txt':
-        ensure => absent,
-        mode   => '0444',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/gerrit/robots-txt-disallow',
-    }
-
     include ::apache::mod::rewrite
 
     include ::apache::mod::proxy
