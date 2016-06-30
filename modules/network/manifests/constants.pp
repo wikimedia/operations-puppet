8b3b033c7b2a5f1109ce6c5a5758171a287eee39
Faidon Liambotis <faidon@wikimedia.org>
2016-06-30 15:12:39 +0200

    network: move external_networks to hiera as well
    Change-Id: Ice9eeeda63b9647ef4c8fb505ce7ba327b3165ec

--- modules/network/manifests/constants.pp
+++ modules/network/manifests/constants.pp
@@ -8,14 +8,7 @@ class network::constants {
     # Note this name is misleading.  Most of these are "external" networks,
     # but some subnets of the IPv6 space are not externally routed, even if
     # they're externally route-able (the ones used for private vlans).
-    $external_networks = [
-        '91.198.174.0/24',
-        '208.80.152.0/22',
-        '2620:0:860::/46',
-        '198.35.26.0/23',
-        '185.15.56.0/22',
-        '2a02:ec80::/32',
-    ]
+    $external_networks = hiera('network::external')
 
     # are you really sure you want to use this? maybe what you really
     # the trusted/production networks. See $production_networks for this.
