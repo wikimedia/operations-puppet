085af91dc0a91bab6ee6ec19f216399b9372e39a
Faidon Liambotis <faidon@wikimedia.org>
2016-06-30 14:20:33 +0200

    Create a new grub module
    This provides an almost dummy (but reusable) grub class, a
    grub::defaults class that is used to remove quiet and splash from the
    default (Ubuntu) config. Much more importantly, for newer systems it
    provides a grub::bootparam define that can be used to set kernel
    parameters from all over the tree, instead of relying on hacky
    base::grub parameters.
    
    Change-Id: If03604a07be59ac2be30ec14b1a532c2f16fabc7

---
+++ modules/grub/manifests/bootparam.pp
@@ -0,0 +1,72 @@
+# == Define: grub::bootparam
+#
+# Sets up grub to pass the specified boot parameter to Linux.
+# and/or userland.
+#
+# === Parameters
+#
+# [*ensure*]
+#   If 'present', the parameter will be provisioned, otherwise it will be
+#   removed. The default is 'present'.
+#
+# [*key*]
+#   The parameter name to pass. Defaults to $title.
+#
+# [*value*]
+#   The parameter value to pass. Optional.
+#
+# [*glob*]
+#   Whether to replace or remove any value assigned to the specified $key.
+#   Defaults to true.
+#
+# === Examples
+#
+#  grub::bootparam { 'quiet':
+#      ensure => present,
+#  }
+#
+#  grub::bootparam { 'i/o scheduler':
+#      ensure   => present,
+#      elevator => 'elevator',
+#      value    => 'deadline',
+#  }
+#
+
+class grub::bootparam(
+  $ensure=present,
+  $key=$title,
+  $value=undef,
+  $glob=true,
+) {
+    include grub
+
+    if versioncmp($::augeasversion, '1.2.0') < 0 {
+        fail('not supported on systems running an old augeas')
+    }
+
+    $param = $value ? {
+        undef   => $key,
+        default => "${key}=${value}",
+    }
+
+    if $glob or $value == undef {
+        $change = $ensure ? {
+            'present' => "set GRUB_CMDLINE_LINUX/value[. = \"${param}\"] ${param}",
+            'absent'  => "rm GRUB_CMDLINE_LINUX/value[. = \"${param}\"]",
+        }
+    } else {
+        $change = $ensure ? {
+            'present' => "set GRUB_CMDLINE_LINUX/value[. =~ glob(\"${key}=*\")] ${param}",
+            'absent'  => "rm GRUB_CMDLINE_LINUX/value[. =~ glob(\"${key}=*\")]",
+        }
+    }
+
+    augeas { 'grub2':
+        incl    => '/etc/default/grub',
+        lens    => 'Shellvars_list.lns',
+        changes => [
+            $change,
+        ],
+        notify  => Exec['update-grub'],
+    }
+}
