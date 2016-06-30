0e02b8635b022e10d1a84ac3cb27e59b4d532a67
YuviPanda <yuvipanda@gmail.com>
2016-06-30 15:13:58 +0100

    [WIP] tools: Provision accounts for all tools
    maintain-kubeusers does the following:
    
    1. Generate accounts for all tools
    2. Generate abac file granting appropriate access
       to all tools
    3. Read up infrastructure user config from a different
       file and make sure they are present in final token auth
       file list
    4. Restart kube-apiserver if needed
    
    Change-Id: I7a7a3dd951db2209e820752c4d14d77eb836b929

---
+++ modules/toollabs/manifests/maintain_kubeusers.pp
@@ -0,0 +1,22 @@
+class toollabs::maintain_kubeusers(
+    $k8s_master,
+) {
+    file { '/usr/local/bin/maintain-kubeusers':
+        source => 'puppet:///modules/toollabs/maintain-kubeusers',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+
+    sudo::user { 'restart_kube_apiserver':
+        ensure     => present,
+        user       => 'kubernetes',
+        privileges => [ 'ALL = (root) NOPASSWD: /bin/systemctl restart kube-apiserver' ],
+    }
+
+    base::service_unit { 'maintain-kubeusers':
+        systemd => True,
+        ensure  => absent,
+        require => Sudo::User['restart_kube_apiserver'],
+    }
+}
