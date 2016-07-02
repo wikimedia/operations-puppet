5dd8a5b617564f4bf1a2833142da017ef33b439f
Antoine Musso <hashar@free.fr>
2016-07-02 17:36:04 +0200

    wmflib: update spec to rspec 3.x
    The 'should' syntax is deprecated in favor of 'is_expected'. One
    deprecation is left around in role_spec which is related to the
    stubbing.
    
    Change-Id: I1704db4936fb4921f25b6b79e909c90484e8d088

--- modules/wmflib/spec/functions/hash_deselect_re_spec.rb
+++ modules/wmflib/spec/functions/hash_deselect_re_spec.rb
@@ -2,24 +2,30 @@ require 'spec_helper'
 
 describe "the hash_deselect_re function" do
   it "should exist" do
-    Puppet::Parser::Functions.function("hash_deselect_re").should == "function_hash_deselect_re"
+    expect(Puppet::Parser::Functions.function("hash_deselect_re")).to eq("function_hash_deselect_re")
   end
 
   it "should raise a ParseError if there are less than 2 arguments" do
-    ->{ scope.function_hash_deselect_re(['a']) }.should(raise_error(Puppet::ParseError))
+    expect {
+      scope.function_hash_deselect_re(['a'])
+    }.to raise_error(Puppet::ParseError)
   end
 
   it "should raise a ParseError if there are more than 2 arguments" do
-    ->{ scope.function_hash_deselect_re(['a', 'b', 'c']) }.should(raise_error(Puppet::ParseError))
+    expect {
+      scope.function_hash_deselect_re(['a', 'b', 'c'])
+    }.to raise_error(Puppet::ParseError)
   end
 
   it "should select the right keys (simple)" do
-    result = scope.function_hash_deselect_re(['^a', {'abc' => 1, 'def' => 2, 'asdf' => 3}])
-    result.should(eq({'def' => 2}))
+    expect(
+      scope.function_hash_deselect_re(['^a', {'abc' => 1, 'def' => 2, 'asdf' => 3}])
+    ).to eq({'def' => 2})
   end
 
   it "should select the right keys (neg lookahead)" do
-    result = scope.function_hash_deselect_re(['^(?!a)', {'abc' => 1, 'def' => 2, 'asdf' => 3}])
-    result.should(eq({'abc' => 1, 'asdf' => 3}))
+    expect(
+      scope.function_hash_deselect_re(['^(?!a)', {'abc' => 1, 'def' => 2, 'asdf' => 3}])
+    ).to eq({'abc' => 1, 'asdf' => 3})
   end
 end
