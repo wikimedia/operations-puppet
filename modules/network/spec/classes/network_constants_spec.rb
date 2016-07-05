abe8d42c69eca1519c5440b7e24d23938225b1ea
Antoine Musso <hashar@free.fr>
2016-07-05 12:59:58 +0200

    network: fix spec varying based on $::realm
    The network::constants class would no more compile due to it now varying
    based on $::realm.  Ensure the class compile for both known realm
    values.
    
    Change-Id: Iaf2ea9e4408bee61ff343ac9148eae6b31bed4f7

--- modules/network/spec/classes/network_constants_spec.rb
+++ modules/network/spec/classes/network_constants_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe 'network::constants', :type => :class do
-    it { should compile }
+    context "on production" do
+        let(:facts) {{ :realm => 'production' }}
+        it { should compile }
+    end
+    context "on labs" do
+        let(:facts) {{ :realm => 'labs' }}
+        it { should compile }
+    end
 end
