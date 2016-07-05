ebe697c643492a145572d417c28e02d5160d44fc
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-07-05 17:52:40 +0100

    prometheus: monitor hosts in the current site
    Generate a list of hosts with their respective ganglia cluster for the current
    site.
    
    Bug: T126785
    Change-Id: I1963f9c3dd36323ddd8a2e0ec21ed7ce4eb8cad4

--- modules/role/manifests/prometheus/ops.pp
+++ modules/role/manifests/prometheus/ops.pp
@@ -6,4 +6,16 @@ class role::prometheus::ops {
     prometheus::web { 'ops':
         proxy_pass => 'http://localhost:9900/ops',
     }
+
+    # Query puppet exported resources and generate a list of hosts for
+    # prometheus to poll metrics from. Ganglia::Cluster is used to generate the
+    # mapping from cluster to a list of its members.
+    file { "/srv/prometheus/ops/targets/node_site_${::site}.yaml":
+        content => generate('/usr/local/bin/prometheus-ganglia-gen',
+                            "--site=${::site}"),
+        backup  => false,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
 }
