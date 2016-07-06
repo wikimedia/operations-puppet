3734d85b0ec725edef282e3b5371e4aca6e3847c
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-07-06 11:53:44 +0100

    swift: adjust group ownership/perms for /var/log/swift
    Make it work on ubuntu too, on ubuntu rsyslog runs as syslog:syslog, on debian
    runs as root.
    
    Bug: T137397
    Change-Id: I4f6e6df334d75e2676d651a5e31140a3f3015441

--- modules/swift/manifests/init.pp
+++ modules/swift/manifests/init.pp
@@ -71,7 +71,9 @@ class swift (
     file { '/var/log/swift':
         ensure  => directory,
         require => Package['swift'],
-        mode    => '0755',
+        owner   => 'root',
+        group   => 'syslog',
+        mode    => '0775',
     }
 
     logrotate::conf { 'swift':
