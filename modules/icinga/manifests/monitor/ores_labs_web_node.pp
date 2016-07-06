f86af8305808564104e6286a53cedcc3ea2c868b
Nathaniel Schaaf <nschaaf@wikimedia.org>
2016-07-06 16:39:05 +0200

    Change path to proxy node requests
    Bug: T134782
    Change-Id: I5c972cc1ebd8445f7d89db1acbc710b75526429a

--- modules/icinga/manifests/monitor/ores_labs_web_node.pp
+++ modules/icinga/manifests/monitor/ores_labs_web_node.pp
@@ -2,7 +2,7 @@
 define icinga::monitor::ores_labs_web_node () {
     monitoring::service { "ores_web_node_labs_${title}":
         description   => "ORES web node labs ${title}",
-        check_command => "check_ores_workers!oresweb/^~node/${title}",
+        check_command => "check_ores_workers!oresweb/node/${title}",
         host          => 'ores.wmflabs.org',
         contact_group => 'team-ores',
     }
