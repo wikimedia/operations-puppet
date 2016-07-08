d157135a227a529a61d9e90f54e73b2ef04e5db7
YuviPanda <yuvipanda@gmail.com>
2016-07-08 17:45:40 +0100

    labs: Allow explicitly specifying lookupcache via hiera
    Bug: T139769
    Change-Id: I9458334ff21f5646ff1ade2f5885e16966f6bad4

--- modules/role/manifests/labs/nfsclient.pp
+++ modules/role/manifests/labs/nfsclient.pp
@@ -1,41 +1,46 @@
 class role::labs::nfsclient(
     $mode = 'hard',
+    $lookupcache = 'none',
 ) {
 
     $nfs_server = 'labstore.svc.eqiad.wmnet'
     $misc_nfs = 'labstore1003.eqiad.wmnet'
 
     labstore::nfs_mount { 'project':
-        project    => $::labsproject,
-        options    => ['rw', $mode],
-        mount_path => '/data/project',
-        share_path => "/project/${::labsproject}/project",
-        server     => $nfs_server,
-        block      => true,
+        project     => $::labsproject,
+        options     => ['rw', $mode],
+        mount_path  => '/data/project',
+        share_path  => "/project/${::labsproject}/project",
+        server      => $nfs_server,
+        block       => true,
+        lookupcache => $lookupcache,
     }
 
     labstore::nfs_mount { 'home':
-        project    => $::labsproject,
-        options    => ['rw', 'hard'],
-        mount_path => '/home',
-        share_path => "/project/${::labsproject}/home",
-        server     => $nfs_server,
-        block      => true,
+        project     => $::labsproject,
+        options     => ['rw', 'hard'],
+        mount_path  => '/home',
+        share_path  => "/project/${::labsproject}/home",
+        server      => $nfs_server,
+        block       => true,
+        lookupcache => $lookupcache,
     }
 
     labstore::nfs_mount { 'scratch':
-        project    => $::labsproject,
-        options    => ['rw', 'soft', 'timeo=300', 'retrans=3'],
-        mount_path => '/data/scratch',
-        server     => $nfs_server,
-        share_path => '/scratch',
+        project     => $::labsproject,
+        options     => ['rw', 'soft', 'timeo=300', 'retrans=3'],
+        mount_path  => '/data/scratch',
+        server      => $nfs_server,
+        share_path  => '/scratch',
+        lookupcache => $lookupcache,
     }
 
     labstore::nfs_mount { 'dumps':
-        project    => $::labsproject,
-        options    => ['ro', 'soft', 'timeo=300', 'retrans=3'],
-        mount_path => '/public/dumps',
-        share_path => '/dumps',
-        server     => $misc_nfs,
+        project     => $::labsproject,
+        options     => ['ro', 'soft', 'timeo=300', 'retrans=3'],
+        mount_path  => '/public/dumps',
+        share_path  => '/dumps',
+        server      => $misc_nfs,
+        lookupcache => $lookupcache,
     }
 }
