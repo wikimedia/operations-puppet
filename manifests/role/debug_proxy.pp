823877d3af19e4a42c36cff3f278753620d72bf2
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-07-08 15:32:12 +0200

    debug_proxy: Limit to production networks
    Only accessed from hosts in production networks.
    
    Change-Id: Icaca1ba98a2844d6b8afa3540b1451fc49ff2ab9

--- manifests/role/debug_proxy.pp
+++ manifests/role/debug_proxy.pp
@@ -23,6 +23,6 @@ class role::debug_proxy {
     ferm::service { 'debug_proxy':
         proto  => 'tcp',
         port   => '80',
-        srange => '$ALL_NETWORKS',
+        srange => '$PRODUCTION_NETWORKS',
     }
 }
