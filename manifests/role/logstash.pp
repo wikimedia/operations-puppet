e84182e8f7f78f50e587ca63d334eaa6fe352260
Bryan Davis <bd808@wikimedia.org>
2016-07-11 15:09:01 -0600

    logstash: Remove normalize_fields fitler
    Follow up to I638d88e1d874fdb8be211bd74a1e36998d42dc09 that removes the
    filter introduced in 99dd186 to correct type mappings that vary from
    application to application. These fields and all others not in the
    explicit mappings are now indexed as strings via mapping rules in the
    default mapping template for logstash-* indices.
    
    Change-Id: Ibe1fcac7805a814d116288da3ce211143855cab1

--- manifests/role/logstash.pp
+++ manifests/role/logstash.pp
@@ -120,11 +120,6 @@ class role::logstash (
         priority => 70,
     }
 
-    logstash::conf { 'filter_normalize_fields':
-        source   => 'puppet:///files/logstash/filter-normalize_fields.conf',
-        priority => 70,
-    }
-
     logstash::conf { 'filter_de_dot':
         source   => 'puppet:///files/logstash/filter-de_dot.conf',
         priority => 70,
