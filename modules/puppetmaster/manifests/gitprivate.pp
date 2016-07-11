c33a51e3c1155af136aea72f60c59feadfd580b8
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-07-11 11:14:07 +0200

    puppetmaster: correct puppetization of the private repo
    The puppetization of the private repository was almost not existent,
    and mostly broken.
    
    Do what follows:
    - Distinguish the private master from the other servers
    - Automatically init a repository in /srv/puppet, where the git data are
    supposed to reside
    - Add a post-receive hook that syncs data to
    /var/lib/git/operations/private
    - Rationalize installation of the git hooks
    - set up /var/lib/git/operations/private to use /srv/puppet as a git
    remote
    
    The only detail that is still missing here is the post-commit hook on
    the private master, which will be fixed in a new, later commit.
    
    Bug: T98173
    Change-Id: I95b9e56c3c79e20dfca564634799a1e48b5d5a52

---
+++ modules/puppetmaster/manifests/gitprivate.pp
@@ -0,0 +1,63 @@
+# Sets up the private repo dir and all the corresponding git hooks
+define puppetmaster::gitprivate (
+    $bare=false,
+    $owner='root',
+    $group='root',
+    $dir_mode='0750',
+    ){
+
+    if $bare {
+        $init = '/usr/bin/git --bare init'
+        $creates = "${title}/config"
+    } else {
+        $init = '/usr/bin/git init'
+        $creates = "${title}/.git"
+    }
+
+    # Create the directory
+    file { $title:
+        ensure => directory,
+        owner  => $owner,
+        group  => $group,
+        mode   => $dir_mode,
+    }
+
+    # Initialize a git repository (bare or otherwise)
+    exec { "git init for ${title}":
+        command => $init,
+        user    => $owner,
+        group   => $group,
+        cwd     => $title,
+        creates => $creates,
+        require => File[$title]
+    }
+
+    # Now all the common hooks there
+    file {
+        "${title}/.git/hooks/post-merge":
+            source  => 'puppet:///modules/puppetmaster/git/private/post-merge',
+            owner   => $owner,
+            group   => $group,
+            mode    => '0550',
+            require => Exec["git init for ${title}"];
+        "${title}/.git/hooks/pre-commit":
+            source  => 'puppet:///modules/puppetmaster/git/private/pre-commit',
+            owner => $owner,
+            group   => $group,
+            mode    => '0550',
+            require => Exec["git init for ${title}"];
+        "${title}/.git/hooks/pre-merge":
+            source  => 'puppet:///modules/puppetmaster/git/private/pre-merge',
+            owner => $owner,
+            group   => $group,
+            mode    => '0550',
+            require => Exec["git init for ${title}"];
+        "${title}/.git/hooks/pre-rebase":
+            source  => 'puppet:///modules/puppetmaster/git/private/pre-rebase',
+            owner => $owner,
+            group   => $group,
+            mode    => '0550',
+            require => Exec["git init for ${title}"];
+    }
+
+}
