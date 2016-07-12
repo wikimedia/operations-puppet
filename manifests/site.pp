805fee7b2743c35f5d066a616ed7343d3b0d2877
Daniel Zahn <dzahn@wikimedia.org>
2016-07-11 19:22:56 -0700

    iegreview: move role to module structure
    Move the role class to the role module and
    rename to a foo::bar schema which fixes autoloader warnings.
    
    Change-Id: I152c7b8ffda24322909b04811dfd693b2e070b40

--- manifests/site.pp
+++ manifests/site.pp
@@ -1414,7 +1414,7 @@ node 'krypton.eqiad.wmnet' {
     # kafka::analytics::burrow is a Kafka consumer lag monitor.
     # Running this here because krypton is a 'misc' Jessie
     # <s>monitoring host</s> (not really, it's just misc apps)
-    role wikimania_scholarships, iegreview, grafana, kafka::analytics::burrow, racktables::server
+    role wikimania_scholarships, iegreview::app, grafana, kafka::analytics::burrow, racktables::server
     include standard
 }
 
