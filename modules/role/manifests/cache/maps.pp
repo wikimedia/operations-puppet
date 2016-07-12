a6615b5c4d74ce1a42b20f7c04dddf01cc044d6c
Brandon Black <bblack@wikimedia.org>
2016-07-12 12:38:24 +0000

    VCL: raise 4xx TTL cap from 1m to 10m
    Given our global 4xx rate is on the order of ~600/s, it seems
    worth it to cache these a little bit longer when the backend
    allows (keep in mind this is a TTL cap, the server can set lower
    or uncacheable).  10 minutes is still a reasonable wait time for
    4xx to catch up after deploying new URLs or sites, etc.
    
    This also removes the related vcl_config, since all four clusters
    are identical at both the old and new values, and this doesn't
    seem like an important per-cluster tunable if the rest of the VCL
    is functioning correctly (e.g. passing on authorization headers
    when necc).  If the VCL wasn't functioning correctly for a
    low-rate cache_misc site and the 1-minute was a bandaid, it's a
    poor one.
    
    Change-Id: I4c240cb715d615f510a785407bc24631b504e4b6

--- modules/role/manifests/cache/maps.pp
+++ modules/role/manifests/cache/maps.pp
@@ -45,7 +45,6 @@ class role::cache::maps {
     }
 
     $common_vcl_config = {
-        'cache4xx'         => '1m',
         'purge_host_regex' => $::role::cache::base::purge_host_not_upload_re,
         'ttl_cap'          => '1d',
         'pass_random'      => true,
