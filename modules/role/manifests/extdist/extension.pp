ef99627cf1ef87dff8eecad72cbe3fd473304cd6
Daniel Zahn <dzahn@wikimedia.org>
2016-07-13 16:47:51 -0700

    extdist: move role to module, fix class name, labs-only
    Change-Id: Ia20ab1b01b7a598bf50106b8aaf8d508ff8cb6a3

--- modules/role/manifests/extdist/extension.pp
+++ modules/role/manifests/extdist/extension.pp
@@ -1,9 +1,9 @@
-# = Class: role::labs::extdist
+# = Class: role::extdist::extension
 #
 # This class sets up a tarball generator for the Extension Distributor
 # extension enabled on mediawiki.org.
 #
-class role::extdist {
+class role::extdist::extension {
 
     include role::labs::lvm::srv
 
