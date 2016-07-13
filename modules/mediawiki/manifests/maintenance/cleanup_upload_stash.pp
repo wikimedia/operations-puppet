838f68754b8f8e4fa0205440d66606e2d6c816ea
elukey <ltoscano@wikimedia.org>
2016-07-13 17:49:40 +0200

    Reduce cronspam from terbium related to the echo_mail_batch cron script
    Daily spam like:
    Warning: require_once(/etc/mediawiki/WikitechPrivateSettings.php):
      failed to open stream: No such file or directory
      in /srv/mediawiki/wmf-config/wikitech.php on line 183
    Fatal error: require_once(): Failed opening required '/etc/mediawiki/WikitechPrivateSettings.php'
      (include_path='/srv/mediawiki/php-1.28.0-wmf.8:/usr/local/lib/php:/usr/share/php')
      in /srv/mediawiki/wmf-config/wikitech.php on line 183
    
    Bug: T132324
    Change-Id: I25cedde86d22fa18db30de89a585ebcc002236fc

--- modules/mediawiki/manifests/maintenance/cleanup_upload_stash.pp
+++ modules/mediawiki/manifests/maintenance/cleanup_upload_stash.pp
@@ -1,7 +1,7 @@
 class mediawiki::maintenance::cleanup_upload_stash( $ensure = present ) {
     cron { 'cleanup_upload_stash':
         ensure  => $ensure,
-        command => '/usr/local/bin/foreachwiki maintenance/cleanupUploadStash.php > /dev/null',
+        command => '/usr/local/bin/foreachwiki maintenance/cleanupUploadStash.php > /dev/null 2>&1',
         user    => $::mediawiki::users::web,
         hour    => 1,
         minute  => 0,
