26831db1871c3313ccdc6d30cf908f4d3eb2a5a0
Daniel Zahn <dzahn@wikimedia.org>
2016-07-13 16:43:09 -0700

    servermon: move role to module, add system::role
    Change-Id: I43762fc0996335538a86b12e31a91eebfa03824c

--- manifests/site.pp
+++ manifests/site.pp
@@ -2361,7 +2361,7 @@ node 'nescio.wikimedia.org' {
 
 # network monitoring tool server
 node 'netmon1001.wikimedia.org' {
-    role rancid::server, librenms, servermon, torrus, smokeping
+    role rancid::server, librenms, servermon::wmf, torrus, smokeping
     include standard
     include passwords::network
     include ganglia::deprecated::collector
