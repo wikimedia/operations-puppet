3d088a6e31d924cebe1f1ed3b514be866e6ca2d4
Chad Horohoe <chadh@wikimedia.org>
2016-07-13 11:02:06 -0700

    Gerrit: Vary extension listing cron on git_dir
    Change-Id: Ia87eb02b2325b1918e28edd0c43287de4f1388b5

--- modules/gerrit/manifests/crons.pp
+++ modules/gerrit/manifests/crons.pp
@@ -10,7 +10,7 @@ class gerrit::crons($ssh_key) {
     cron { 'list_mediawiki_extensions':
     # Gerrit is missing a public list of projects.
     # This hack list MediaWiki extensions repositories
-        command => "/bin/ls -1d /var/lib/gerrit2/review_site/git/mediawiki/extensions/*.git | sed 's#.*/##' | sed 's/\\.git//' > /var/www/mediawiki-extensions.txt",
+        command => "/bin/ls -1d ${::gerrit::jetty::git_dir}/mediawiki/extensions/*.git | sed 's#.*/##' | sed 's/\\.git//' > /var/www/mediawiki-extensions.txt",
         user    => 'root',
         minute  => [0, 15, 30, 45],
     }
