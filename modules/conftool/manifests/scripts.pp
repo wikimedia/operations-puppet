2be61e4a947c827b9813b0d280806d3f931c1e26
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-07-14 12:19:18 +0200

    mediawiki::conftool: add mw-pool
    This is a specialized script that you should use when pooling an
    appserver for the first time or repooling it after it was set to "inactive".
    
    Change-Id: I36420cd1533940baf4e0cd7960d37cc6c04e1cbc

--- modules/conftool/manifests/scripts.pp
+++ modules/conftool/manifests/scripts.pp
@@ -39,4 +39,12 @@ class conftool::scripts {
         content => template('conftool/drain.erb')
     }
 
+    file { '/usr/local/bin/decommission':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        content => template('conftool/decommission.erb')
+    }
+
 }
--- modules/mediawiki/manifests/conftool.pp
+++ modules/mediawiki/manifests/conftool.pp
@@ -4,4 +4,13 @@
 class mediawiki::conftool {
     include ::conftool::scripts
     conftool::credentials { 'mwdeploy': }
+
+    # Will re-enable a mediawiki appserver after running scap pull
+    file { '/usr/local/bin/mw-pool':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/mw-pool',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
 }
