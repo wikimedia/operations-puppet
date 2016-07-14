e1e727d50a2f35ad9adf3c25f1230c3dcc631041
Brandon Black <bblack@wikimedia.org>
2016-07-14 14:09:49 +0000

    cache_misc: raise default_ttl to 1h
    text and upload use 3d, maps uses 1d currently.  The default is a
    mere 5m.  If we're relying on this to not break a cache_misc site,
    it's still broken, just on shorter timescales.  I'd like to raise
    this up to 1d eventually, but let's get there in stages in case
    there is some unexpected fallout for cache_misc apps with poor
    cacheability metadata.
    
    Bug: T124954
    Change-Id: Ie6005a7975fa1b459919fbaa2ac2914f9efb73b2

--- modules/role/manifests/cache/misc.pp
+++ modules/role/manifests/cache/misc.pp
@@ -276,7 +276,7 @@ class role::cache::misc {
     role::cache::instances { 'misc':
         fe_mem_gb        => ceiling(0.5 * $::memorysize_mb / 1024.0),
         fe_jemalloc_conf => 'lg_dirty_mult:8,lg_chunk_size:17',
-        runtime_params   => [],
+        runtime_params   => ['default_ttl=3600'],
         app_directors    => $app_directors,
         fe_vcl_config    => $fe_vcl_config,
         be_vcl_config    => $be_vcl_config,
