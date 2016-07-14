4edb0e4ac85a28fb7edd6da826f6c9cc5ec9f22d
amir <Ladsgroup@gmail.com>
2016-07-14 10:05:47 +0000

    ores: add File['/srv/log'] for web nodes in labs
    I wanted to put it in modules/service/manifests/uwsgi.pp
    but it might be declared somewhere else in scb roles causing a mess
    (because we have this directory in scb nodes already, no idea why)
    
    Bug: T140265
    Change-Id: I1f2600a52265661e7e1eebf353edf6af86cc16b4

--- modules/role/manifests/labs/ores/web.pp
+++ modules/role/manifests/labs/ores/web.pp
@@ -1,4 +1,13 @@
 class role::labs::ores::web {
     include ::ores::web
     include ::role::labs::ores::redisproxy
+
+    file { '/srv/log':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        before => File['/srv/log/ores'],
+    }
+
 }
