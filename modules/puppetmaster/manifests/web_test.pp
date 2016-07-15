60dc38e72f29aa1fcfcbc78bae35b776b0f46a76
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-07-15 15:42:28 +0200

    puppetmaster: add test site to palladium
    The easiest way to validate that a puppet master works as expected is to
    compare catalogs generated from it and from the other backends; we thus
    create a 'puppet.test' virtualhost that points all its traffic to
    rhodium, the new puppetmaster we want to test right now.
    
    This will allow to run catalog compilations against the standard
    backends and against rhodium and check for errors/differences.
    
    Bug: T98173
    Change-Id: Ie40e9608047cc834528cffe7f7b861c1f7a62085

---
+++ modules/puppetmaster/manifests/web_test.pp
@@ -0,0 +1,23 @@
+# === Class puppetmaster::web_test
+#
+# Installs a virtualhost (and the corresponding certificate) for
+# an alternative puppetmaster frontend which can have different workers than
+# the standard one.
+#
+class puppetmaster::web_test ($server_name, $workers = {}) {
+    Class['::puppetmaster'] -> Class['::puppetmaster::web_test']
+    $ssl_settings = ssl_ciphersuite('apache', 'compat')
+    $ssldir = $::puppetmaster::ssl::ssldir
+
+    # Have puppet generate the certificate for this virtualhost
+    exec { 'generate hostcert':
+        require => File["${ssldir}/certs"],
+        command => "/usr/bin/puppet cert generate ${server_name}",
+        creates => "${ssldir}/certs/${server_name}.pem";
+    }
+
+    apache::site { 'puppetmaster-test':
+        content => template('puppetmaster/puppetmaster-test.erb'),
+        reqire  => Exec['generate hostcert'],
+    }
+}
