90dcc925a844cf40ee48b8cded28c39fb7c82f53
Andrew Otto <acotto@gmail.com>
2016-07-15 11:13:18 -0400

    Include icinga alerts for zookeeper
    Alert on process running and if num client connections gets too high.
    
    Bug: T137302
    Change-Id: I6f5c5cc00bb51f814e5a496d9c9e14cea63c1eb6

--- modules/role/manifests/zookeeper/server.pp
+++ modules/role/manifests/zookeeper/server.pp
@@ -33,7 +33,6 @@ class role::zookeeper::server {
     }
 
     include role::zookeeper::client
-
     class { '::zookeeper::server': }
 
     ferm::service { 'zookeeper':
@@ -43,9 +42,34 @@ class role::zookeeper::server {
         srange => '($INTERNAL)',
     }
 
+    $group_prefix = "zookeeper.cluster.${cluster_name}."
     # Use jmxtrans for sending metrics to ganglia
     class { 'zookeeper::jmxtrans':
-        group_prefix => "zookeeper.cluster.${cluster_name}.",
+        group_prefix => $group_prefix,
         statsd       => hiera('statsd', undef),
+    }
+
+    if $::realm == 'production' {
+        # Alert if Zookeeper Server is not running.
+        nrpe::monitor_service { 'zookeeper':
+            description   => 'Zookeeper Server',
+            nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 1:1 -C java -a "org.apache.zookeeper.server.quorum.QuorumPeerMain /etc/zookeeper/conf/zoo.cfg"',
+            critical      => true,
+        }
+
+        # jmxtrans statsd writer emits fqdns in keys
+        # by substituting '.' with '_' and suffixing the jmx port.
+        $graphite_broker_key = regsubst("${::fqdn}_${::zookeeper::jmxtrans::jmx_port}", '\.', '_', 'G')
+
+        # Alert if NumAliveConnections approaches max client connections
+        # Alert if any Kafka Broker replica lag is too high
+        monitoring::graphite_threshold { 'zookeeper-client-connections':
+            description => 'Zookeeper Alive Client Connections too high',
+            metric      => "${group_prefix}zookeeper.${graphite_broker_key}.zookeeper.NumAliveConnections",
+            # Warn if we go over 50% of max
+            warning     => $::zookeeper::max_client_connections * 0.5,
+            # Critical if we go over 90% of max
+            critical    => $::zookeeper::max_client_connections * 0.9,
+        }
     }
 }
