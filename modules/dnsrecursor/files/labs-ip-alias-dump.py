dcc1deaee7e529c2c9687908df3f1c9c29a677d2
Alex Monk <alex@wikimedia.org>
2016-07-18 02:20:32 +0100

    dnsrecursor labsaliaser: Set up instance-$instance.$project.wmflabs.org domains for every instance with a public IP
    Bug: T104521
    Change-Id: I63c51bc74fb2199bda31940e0fe8bde1bd41d6b5

--- modules/dnsrecursor/files/labs-ip-alias-dump.py
+++ modules/dnsrecursor/files/labs-ip-alias-dump.py
@@ -25,6 +25,9 @@ argparser.add_argument(
 )
 
 LUA_LINE_TEMPLATE = '{table}["{key}"] = "{value}" -- {comment}\n'
+LUA_LINE_TEMPLATE_PUBLIC_AS = '{table}["{key}"] = {value}\n'
+LUA_LINE_TEMPLATE_PUBLIC_PTRS = '{table}["{key}"] = "{value}"\n'
+FQDN_TEMPLATE = "instance-{server}.{project}.wmflabs.org."
 
 args = argparser.parse_args()
 config = yaml.safe_load(args.config_file)
@@ -43,6 +46,7 @@ for tenant in keystoneClient.tenants.list():
     projects.append(tenant.name)
 
 aliases = {}
+public_addrs = {}
 for project in projects:
     client = novaclient.Client(
         "1.1",
@@ -68,6 +72,8 @@ for project in projects:
                 # Technically there can be more than one floating IP and more than one private IP
                 # Although this is never practically the case...
                 aliases[server.name] = list(itertools.product(public, private))
+                fqdn = FQDN_TEMPLATE.format(server=server.name, project=project)
+                public_addrs[fqdn] = public
         except KeyError:
             # This can happen if a server doesn't (yet) have any addresses, while it's being
             # constructed.  In which case we simply harmlessly ignore it.
@@ -118,6 +124,41 @@ function preresolve(remoteip, domain, qtype)
         return 0, {
             {qtype=pdns.A, content=extra_records[domain], ttl=300, place="1"},
         }
+    end
+    return -1, {}
+end
+"""
+
+output += 'public_As = {}\npublic_PTRs = {}\n'
+# Sort to prevent flapping around due to random ordering
+for A_FQDN in sorted(public_addrs.keys()):
+    IPv4s = public_addrs[A_FQDN]
+    output += LUA_LINE_TEMPLATE_PUBLIC_AS.format(
+        table='public_As',
+        key=A_FQDN,
+        value="{'" + "', '".join(IPv4s) + "'}"
+    )
+
+    for IPv4 in IPv4s:
+        # Assuming dotted-quad notation is probably fine
+        PTR_FQDN = '.'.join(reversed(IPv4.split('.'))) + '.in-addr.arpa.'
+        output += LUA_LINE_TEMPLATE_PUBLIC_PTRS.format(
+            table='public_PTRs',
+            key=PTR_FQDN,
+            value=A_FQDN
+        )
+
+output += """
+function nxdomain(remoteip, domain, qtype)
+    if qtype == pdns.A and public_As[domain] then
+        ret = {}
+        for i, IP in ipairs(public_As[domain]) do
+            ret[i] = {qtype=pdns.A, content=IP, ttl=300}
+        end
+        return 0, ret
+    end
+    if qtype == pdns.PTR and public_PTRs[domain] then
+        return 0, {{qtype=pdns.PTR, content=(public_PTRs[domain]), ttl=300}}
     end
     return -1, {}
 end
