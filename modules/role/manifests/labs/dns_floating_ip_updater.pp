887b64748fc402f9faf51630979ffadf0e3d34ad
Alex Monk <alex@wikimedia.org>
2016-07-20 00:25:23 +0100

    [WIP/POC/POS] Puppetise script to manage labs floating IP PTR records
    Requires creation of a '128-25.155.80.208.in-addr.arpa.' zone under the wmflabsdotorg project.
    
    TODO: Use wmflabsdotorgadmin credentials for that?
    TODO: Hardcodes IP range data so we can mess with them to handle RFC 2317 delegation :(
          This means it'll have to be changed to work in labtest in a useful way
    TODO: Test it... This is completely and utterly 100% untested.
    
    Bug: T104521
    Change-Id: Ibe5a64ea472f230f5c959baa75b8cbb6883d7cbb

---
+++ modules/role/manifests/labs/dns_floating_ip_updater.pp
@@ -0,0 +1,35 @@
+class role::labs::dns_floating_ip_updater {
+    $keystoneconfig = hiera_hash('keystoneconfig', {})
+    $wikitech_nova_ldap_user_pass = $keystoneconfig['ldap_user_pass']
+    $wikitech_nova_admin_project_name = $keystoneconfig['admin_project_name']
+    $nova_controller_hostname = hiera('labs_nova_controller')
+
+    $config = {
+        'username'           => 'novaadmin',
+        'password'           => $wikitech_nova_ldap_user_pass,
+        'nova_api_url'       => "http://${nova_controller_hostname}:35357/v2.0",
+        'admin_project_name' => $wikitech_nova_admin_project_name,
+    }
+
+    file { '/etc/labs-floating-ips-dns-config.yaml':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0440',
+        content => ordered_yaml($config),
+    }
+
+    file { '/etc/dns-floating-ip-updater.py':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0750',
+        source => 'puppet:///modules/role/labs/dns-floating-ip-updater.py'
+    }
+
+    cron { 'floating-ip-ptr-record-updater':
+        minute  => '*/10',
+        user    => 'root',
+        command => 'python /etc/dns-floating-ip-updater.py',
+    }
+}
