179d68f432975a495f6a054545367d8e989ddb08
Daniel Zahn <dzahn@wikimedia.org>
2016-07-18 17:10:05 -0700

    gerrit: fix ssh port monitoring
    follow-up to If23443bca48f45e3
    
    The command definition is:
    
    /usr/lib/nagios/plugins/check_ssh -p '$ARG1$' '$HOSTADDRESS$'
    
    so the host address is already automatic and there is just one arg,
    the port.
    
    Without this we are getting "check_ssh: Port number must be a positive integer"
    
    Change-Id: Ibe2048713bfe5bc2c83489aa00eec229220e1369

--- modules/role/manifests/gerrit/server.pp
+++ modules/role/manifests/gerrit/server.pp
@@ -24,7 +24,7 @@ class role::gerrit::server($ipv4, $ipv6) {
 
         nrpe::monitor_service { 'ssh':
             description   => 'SSH access',
-            check_command => "check_ssh_port!${host}!29418",
+            check_command => 'check_ssh_port!29418',
             contact_group => 'admins,gerrit',
         }
 
