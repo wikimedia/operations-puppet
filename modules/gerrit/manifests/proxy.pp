8e7743b6f584ffbb87be09d689c03ed7f3ed4a93
Chad Horohoe <chadh@wikimedia.org>
2016-07-18 18:27:28 -0700

    Gerrit: Introduce comment of $maint_mode for the web UI
    This allows Gerrit to disappear with 503s while providing a useful
    error message back to the user. Don't just rely on ErrorDocument,
    as Gerrit might be going away and coming back several times.
    
    Should be a no-op (config-wise) on both ytterbium and lead until
    we actually use $maint_mode.
    
    Change-Id: Ideebea72ddbd8a9b919ffa319724ef1caecde7df

--- modules/gerrit/manifests/proxy.pp
+++ modules/gerrit/manifests/proxy.pp
@@ -1,4 +1,8 @@
-class gerrit::proxy($host = $::gerrit::host, $lets_encrypt = true) {
+class gerrit::proxy(
+    $host         = $::gerrit::host,
+    $lets_encrypt = true,
+    $maint_mode   = false,
+    ) {
 
     $ssl_settings = ssl_ciphersuite('apache', 'compat', true)
 
@@ -14,6 +18,39 @@ class gerrit::proxy($host = $::gerrit::host, $lets_encrypt = true) {
 
     apache::site { $host:
         content => template('gerrit/gerrit.wikimedia.org.erb'),
+    }
+
+    # Error page just in case we're dying
+    $ensure_maint = $maint_mode ? {
+        true    => present,
+        default => absent,
+    }
+    file { '/var/www/maintenance.html':
+        ensure => $ensure_maint,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/gerrit/maintenance.html',
+    }
+
+    $ensure_link = $maint_mode ? {
+        true    => 'link',
+        default => absent
+    }
+    file { '/var/www/page-bkg.cache.jpg':
+        ensure => $ensure_link,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        target => '/var/lib/gerrit2/review_site/static/page-bkg.cache.jpg',
+    }
+
+    file { '/var/www/wikimedia-codereview-logo.cache.png':
+        ensure => $ensure_link,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => '/var/lib/gerrit2/review_site/static/wikimedia-codereview-logo.cache.png',
     }
 
     include ::apache::mod::rewrite
