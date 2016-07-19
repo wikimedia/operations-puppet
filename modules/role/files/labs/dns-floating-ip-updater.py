e2282c276132904c80e23146d77f832baee92b04
Alex Monk <alex@wikimedia.org>
2016-07-20 00:25:23 +0100

    [WIP/POC/POS] Puppetise script to manage labs floating IP PTR records
    Requires creation of a '128-25.155.80.208.in-addr.arpa.' zone under the wmflabsdotorg project.
    
    TODO: Use wmflabsdotorgadmin credentials for that?
    TODO: Hardcodes IP range data so we can mess with them to handle RFC 2317 delegation :(
          This means it'll have to be changed to work in labtest in a useful way
    TODO: Test it... This is completely and utterly 100% untested.
    
    Bug: T104521
    Change-Id: Ibe5a64ea472f230f5c959baa75b8cbb6883d7cbb

---
+++ modules/role/files/labs/dns-floating-ip-updater.py
@@ -0,0 +1,195 @@
+#!/usr/bin/python
+import argparse
+import re
+import yaml
+
+from keystoneclient.session import Session as KeystoneSession
+from keystoneclient.auth.identity.v2 import Password as KeystonePassword
+
+from keystoneclient.client import Client as KeystoneClient
+from novaclient import client as novaclient
+
+import designateclient.v2 as designateclient
+
+PROJECT_ZONE_TEMPLATE = "{project}.wmflabs.org."
+FQDN_TEMPLATE = "instance-{server}.{project}.wmflabs.org."
+FQDN_REGEX = FQDN_TEMPLATE.replace('.', '\.').format(server='.*', project='{project}')
+managed_description = "MANAGED BY dns-floating-ip-updater.py IN PUPPET - DO NOT UPDATE OR DELETE"
+
+# labs zone is 208.80.155.128/25, so we have delegation set up as suggested by RFC 2317
+orig_floating_ip_zone = "155.80.208.in-addr.arpa."
+floating_ip_zone = "128-25." + orig_floating_ip_zone
+matching_regex = re.compile(r"^(\d{1,3})\." + orig_floating_ip_zone.replace(".", "\."))
+replacement_pattern = "\\1." + floating_ip_zone
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument(
+    '--config-file',
+    help='Path to config file',
+    default='/etc/labs-floating-ips-dns.yaml',
+    type=argparse.FileType('r')
+)
+args = argparser.parse_args()
+config = yaml.safe_load(args.config_file)
+
+
+def getKeystoneSession(config, project):
+    return KeystoneSession(auth=KeystonePassword(
+        auth_url=config['nova_api_url'],
+        username=config['username'],
+        password=config['password'],
+        tenant_name=project
+    ))
+
+keystone_admin_session = getKeystoneSession(config, config['admin_project_name'])
+keystone_client = KeystoneClient(session=keystone_admin_session, endpoint=config['nova_api_url'])
+
+keystone_sessions = {}
+project_main_zone_ids = {}
+public_addrs = {}
+existing_As = []
+for tenant in keystone_client.tenants.list():
+    keystone_sessions[tenant.name] = getKeystoneSession(config, tenant.name)
+
+    server_addresses = {}
+    nova_client = novaclient.Client("1.1", session=keystone_sessions[tenant.name])
+    for server in nova_client.servers.list():
+        if 'addresses' in server and 'public' in server.addresses:
+            public = [
+                str(ip['addr']) for ip in server.addresses['public']
+                if ip['OS-EXT-IPS:type'] == 'floating'
+            ]
+            if public:
+                # Technically there can be more than one floating IP and more than one private IP
+                # Although this is never practically the case...
+                server_addresses[server.name] = public
+                A_FQDN = FQDN_TEMPLATE.format(server=server.name, project=tenant.name)
+                public_addrs[A_FQDN, tenant.name, True] = public
+
+    designate_client = designateclient.Client(session=keystone_sessions[tenant.name])
+    existing_match_regex = re.compile(FQDN_REGEX.format(project=tenant.name))
+    for zone in designate_client.zones.list():
+        if zone['name'] == PROJECT_ZONE_TEMPLATE.format(project=tenant.name):
+            project_main_zone_ids[tenant.name] = zone['id']
+
+        for recordset in designate_client.recordsets.list(zone['id']):
+            existing_As.append(recordset['name'])
+            if recordset['type'] == 'A': # No IPv6 support in labs so no AAAA
+                match = existing_match_regex.match(recordset['name'])
+                assert len(recordset['records']) == 1
+                if match:
+                    # Matches instances for this project, managed by this script
+                    if match.group(1) not in server_addresses:
+                        # ... But instance does not actually exist. Delete!
+                        if recordset['description'] == managed_description:
+                            designate_client.recordsets.delete(zone['id'], recordset['id'])
+                        else:
+                            print(
+                                "Did not delete A record for " +
+                                recordset['name'] +
+                                " due to lack of managed_description!"
+                            )
+                    elif recordset['records'] != server_addresses[match.group(1)]:
+                        # ... But instance has a different set of IPs. Update!
+                        new_records = set(server_addresses[match.group(1)])
+                        if new_records != set(recordset['records']):
+                            if recordset['description'] == managed_description:
+                                # Update!
+                                recordset['records'] = list(new_records)
+                                wmflabsdotorg_designate_client.recordsets.update(
+                                    zone['id'],
+                                    recordset['id'],
+                                    recordset
+                                )
+                            else:
+                                print(
+                                    "Did not update A record for " +
+                                    recordset['name'] +
+                                    " due to lack of managed_description!"
+                                )
+                else:
+                    public_addrs[recordset['name'], tenant.name, False] = [recordset['data']]
+
+public_PTRs = {}
+for (A_FQDN, project, managed_here), IPv4s in public_addrs.items():
+    designate_client = designateclient.Client(session=keystone_sessions[project])
+
+    for IPv4 in IPv4s:
+        if managed_here and A_FQDN not in existing_As:
+            # Create instance-$instance.$project.wmflabs.org 120 IN A $IP
+            designate_client.recordsets.create(
+                project_main_zone_ids[project],
+                A_FQDN,
+                'A',
+                [IPv4], # TODO: needs to be factored out
+                description=managed_description
+            )
+
+        # Now deal with the reverse
+        PTR_FQDN = '.'.join(reversed(IPv4.split('.'))) + '.in-addr.arpa.'
+        delegated_PTR_FQDN = matching_regex.sub(replacement_pattern, PTR_FQDN)
+        if delegated_PTR_FQDN in public_PTRs:
+            public_PTRs[delegated_PTR_FQDN].append(A_FQDN)
+        else:
+            public_PTRs[delegated_PTR_FQDN] = [A_FQDN]
+
+# Set up designate client to write zones/recordsets with
+wmflabsdotorg_designate_client = designateclient.Client(session=keystone_sessions['wmflabsdotorg'])
+# Find the correct zone ID for the floating IP zone
+floating_ip_zone_id = None
+for zone in wmflabsdotorg_designate_client.zones.list():
+    if zone['name'] == floating_ip_zone:
+        floating_ip_zone_id = zone['id']
+        break
+
+assert floating_ip_zone_id is not None
+
+existing_public_PTRs = {}
+# Go through each record in the delegated PTR zone, deleting any with our managed_description
+# that don't match our public_PTRs data.
+for recordset in wmflabsdotorg_designate_client.recordsets.list(floating_ip_zone_id):
+    existing_public_PTRs[recordset['name']] = recordset
+    if recordset['type'] == 'PTR':
+        if recordset['name'] not in public_PTRs:
+            if recordset['description'] == managed_description:
+                # Delete whole recordset
+                wmflabsdotorg_designate_client.recordsets.delete(
+                    floating_ip_zone_id,
+                    recordset['id']
+                )
+            else:
+                print(
+                    "Did not delete PTR record for " +
+                    recordset['name'] +
+                    " due to lack of managed_description!"
+                )
+            continue
+        new_records = set(public_PTRs[recordset['name']])
+        if new_records != set(recordset['records']):
+            # Effectively, removes any records in the recordset that didn't match our public_PTRs data
+            if recordset['description'] == managed_description:
+                # Update!
+                recordset['records'] = list(new_records)
+                wmflabsdotorg_designate_client.recordsets.update(
+                    floating_ip_zone_id,
+                    recordset['id'],
+                    recordset
+                )
+            else:
+                print(
+                    "Did not update PTR record for " +
+                    recordset['name'] +
+                    " due to lack of managed_description!"
+                )
+
+# Create PTRs in delegated PTR zone
+for delegated_PTR_FQDN, records in public_PTRs:
+    if delegated_PTR_FQDN not in existing_public_PTRs:
+        # Create!
+        wmflabsdotorg_designate_client.recordsets.create(
+            floating_ip_zone_id,
+            delegated_PTR_FQDN,
+            'PTR',
+            records,
+            description=managed_description
+        )
