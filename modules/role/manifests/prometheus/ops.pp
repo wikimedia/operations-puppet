dae57fab55532c28dd542546ead81319ee0ccf42
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-07-20 10:47:03 +0100

    prometheus: fix ferm::service and include node_exporter
    Change-Id: I17c2cbf0b80a0e32c41348e32e594905d459bf73

--- modules/role/manifests/prometheus/ops.pp
+++ modules/role/manifests/prometheus/ops.pp
@@ -1,9 +1,17 @@
 class role::prometheus::ops {
+    include base::firewall
+
     prometheus::server { 'ops':
         listen_address => '127.0.0.1:9900',
     }
 
     prometheus::web { 'ops':
         proxy_pass => 'http://localhost:9900/ops',
+    }
+
+    ferm::service { 'prometheus-web':
+        proto  => 'tcp',
+        port   => '80',
+        srange => '$INTERNAL',
     }
 }
