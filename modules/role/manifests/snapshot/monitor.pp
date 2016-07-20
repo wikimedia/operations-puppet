9282e1cdbc193c1c02d92f7474d35cbfd233abf7
Ariel T. Glenn <ariel@wikimedia.org>
2016-07-20 16:27:24 +0300

    set up proper dump monitor role and add to snapshot1007
    Change-Id: Ibf89f1754c26390f675741cc3d70137a0857954f

---
+++ modules/role/manifests/snapshot/monitor.pp
@@ -0,0 +1,21 @@
+class role::snapshot::monitor {
+
+    if hiera('snapshot::dumps::monitor', false) {
+        # mw packages and dependencies, dataset server nfs mount
+        include snapshot::dumps::packages
+
+        # config, stages files, dblists, html templates
+        include snapshot::dumps
+
+        # scap3 deployment of dump scripts
+        include dataset::user
+        include snapshot::deployment
+
+        # monitor job
+        include snapshot::dumps::monitor
+
+        system::role { 'role::snapshot::dumps::monitor':
+            description => 'monitor of XML dumps',
+        }
+    }
+}
