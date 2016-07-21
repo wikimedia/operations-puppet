2ff0a4d524b589bee572ab3e18e50983bf532c1c
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-07-21 16:00:37 +0200

    restbase: have systemd restart failed nodes
    since we're no longer in the nineties and we have init systems able to
    keep services up for us, avoiding outages in case of mass crashes.
    
    Bug: T136957
    Change-Id: Idecf0f66ad7f2214efcceaeeca8da6e85defbc0f

--- modules/restbase/manifests/init.pp
+++ modules/restbase/manifests/init.pp
@@ -124,7 +124,7 @@ class restbase(
         starter_script  => 'restbase/server.js',
         local_logging   => false,
         auto_refresh    => false,
-        init_restart    => false,
+        init_restart    => true,
     }
 
 }
