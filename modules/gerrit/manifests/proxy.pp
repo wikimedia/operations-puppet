c32f80e9d2c64ee3b30a39c19382dc4fdd7a0ae4
Chad Horohoe <chadh@wikimedia.org>
2016-07-21 11:18:00 -0700

    Gerrit: Further tweaks to down/maintenance mode
    When Gerrit is actually down, Apache serves 503s. Let's at least
    pretty those up rather than spewing a black and white 503 with
    no context.
    
    When we go into maintenance mode, make @maint_mode a string of
    when we expect it to be back online. This allows us to set
    nice expectations.
    
    Change-Id: Ic0cb8e2a5060da40a5079adf3ed12338a5a72e2b

--- modules/gerrit/manifests/proxy.pp
+++ modules/gerrit/manifests/proxy.pp
@@ -20,33 +20,23 @@ class gerrit::proxy(
         content => template('gerrit/gerrit.wikimedia.org.erb'),
     }
 
-    # Error page just in case we're dying
-    $ensure_maint = $maint_mode ? {
-        true    => present,
-        default => absent,
-    }
-    file { '/var/www/maintenance.html':
-        ensure => $ensure_maint,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => 'puppet:///modules/gerrit/maintenance.html',
-    }
-
-    $ensure_link = $maint_mode ? {
-        true    => 'link',
-        default => absent
+    # Error page stuff
+    file { '/var/www/error.html':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('gerrit/error.html.erb'),
     }
     file { '/var/www/page-bkg.cache.jpg':
-        ensure => $ensure_link,
+        ensure => 'link',
         owner  => 'root',
         group  => 'root',
         mode   => '0444',
         target => '/var/lib/gerrit2/review_site/static/page-bkg.cache.jpg',
     }
-
     file { '/var/www/wikimedia-codereview-logo.cache.png':
-        ensure => $ensure_link,
+        ensure => 'link',
         owner  => 'root',
         group  => 'root',
         mode   => '0444',
