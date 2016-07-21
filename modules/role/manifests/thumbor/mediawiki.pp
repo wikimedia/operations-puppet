e959e6a1a9270627fe1891690a09e3509d1e06c4
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-07-21 16:24:53 +0100

    puppetization for thumbor
    Bug: T139606
    Change-Id: I0b8794836eed2cb2b707f442f66c09ead6886252

---
+++ modules/role/manifests/thumbor/mediawiki.pp
@@ -0,0 +1,25 @@
+# == Class: role::thumbor::mediawiki
+#
+# Installs a Thumbor image scaling server to be used with MediaWiki.
+#
+
+class role::thumbor::mediawiki {
+    include base::firewall
+    include role::statsite
+
+
+    class { '::thumbor': }
+
+    $swift_account_keys = hiera('swift::params::account_keys')
+
+    class { '::thumbor::swift':
+        swift_key                => $swift_account_keys['thumbor'],
+        swift_sharded_containers => hiera_array('swift::proxy::shard_container_list'),
+    }
+
+    ferm::service { 'thumbor':
+        proto  => 'tcp',
+        port   => '8800',
+        srange => '$DOMAIN_NETWORKS',
+    }
+}
