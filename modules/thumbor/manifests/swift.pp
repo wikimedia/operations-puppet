2b3782b80ce871413fcc81600f85693d1dfd0a1e
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-07-21 16:24:53 +0100

    puppetization for thumbor
    Bug: T139606
    Change-Id: I0b8794836eed2cb2b707f442f66c09ead6886252

---
+++ modules/thumbor/manifests/swift.pp
@@ -0,0 +1,37 @@
+# == Define: thumbor::swift
+#
+# Sets up Thumbor's result storage to write to a Swift cluster.
+#
+# === Parameters
+#
+# [*name*]
+#   Service port.
+#    $swift_key,
+#    $swift_account = 'thumbor',
+#    $swift_user = 'thumbor',
+#    $swift_sharded_containers = [],
+#    $swift_host = "ms-fe.svc.${::site}.wmnet",
+#
+# === Examples
+#
+#   thumbor::instance { '8888':
+#   }
+#
+
+class thumbor::swift (
+    $swift_key,
+    $swift_account = 'thumbor',
+    $swift_user = 'thumbor',
+    $swift_sharded_containers = [],
+    $swift_host = "ms-fe.svc.${::site}.wmnet",
+) {
+    file { '/etc/thumbor.d/80-thumbor-swift.conf':
+        ensure  => present,
+        owner   => 'thumbor',
+        group   => 'thumbor',
+        mode    => '0440',
+        content => template('thumbor/swift.conf.erb'),
+        require => Package['python-thumbor-wikimedia'],
+        notify  => Service['thumbor-instances'],
+    }
+}
