761234b8cd164fa44950223b91930fb07d67906e
Ariel T. Glenn <ariel@wikimedia.org>
2016-07-25 16:04:38 +0300

    move dump of cirrus search data to snapshot1007
    take the opportunity to do small amount of cleanup on the
    cirrus dump manifest: no more 'enable' variable, no more
    separate system roles per cron job
    
    Change-Id: Id6ba40759254a345d29025eb337872143bfd514b

--- modules/role/manifests/snapshot/cronjobs.pp
+++ modules/role/manifests/snapshot/cronjobs.pp
@@ -2,4 +2,5 @@ class role::snapshot::cronjobs {
     include dataset::user
     class { '::snapshot::cron::mediaperprojectlists': user => 'datasets' }
     class { '::snapshot::cron::pagetitles': user   => 'datasets' }
+    class { '::snapshot::cron::cirrussearch': user   => 'datasets' }
 }
