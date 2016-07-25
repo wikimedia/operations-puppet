430f3fb1fba92792981c1977a4503493560f9b84
Andrew Otto <acotto@gmail.com>
2016-07-25 09:50:44 -0400

    Upgrade Kafka main-codfw to 0.9
    Bug: T138265
    Change-Id: Ic7c612550138a33faf25e50748b02b0a09537502

--- modules/role/manifests/kafka/main/broker.pp
+++ modules/role/manifests/kafka/main/broker.pp
@@ -17,15 +17,6 @@ class role::kafka::main::broker {
         description => "Kafka Broker Server in the ${cluster_name} cluster",
     }
 
-    # export ZOOKEEPER_URL and BROKER_LIST user environment variable.
-    # This makes it much more convenient to run kafka commands without having
-    # to specify the --zookeeper or --brokers flag every time.
-    file { '/etc/profile.d/kafka.sh':
-        owner   => 'root',
-        mode    => '0444',
-        content => template('role/kafka/kafka-profile.sh.erb'),
-    }
-
     $nofiles_ulimit = $::realm ? {
         # Use default ulimit for labs kafka
         'labs'       => 8192,
@@ -38,57 +29,113 @@ class role::kafka::main::broker {
         mode   => '0755',
     }
 
-    class { '::kafka::server':
-        log_dirs                     => ['/srv/kafka/data'],
-        brokers                      => $config['brokers']['hash'],
-        zookeeper_hosts              => $config['zookeeper']['hosts'],
-        zookeeper_chroot             => $config['zookeeper']['chroot'],
-        nofiles_ulimit               => $nofiles_ulimit,
-        jmx_port                     => $config['jmx_port'],
+    if $::site == 'codfw' or $::realm == 'labs' {
+        class { '::confluent::kafka::broker':
+            # NOTE: This will be removed once all brokers are on 0.9
+            inter_broker_protocol_version => '0.8.2.X',
 
-        # Enable auto creation of topics.
-        auto_create_topics_enable    => true,
+            log_dirs                      => ['/srv/kafka/data'],
+            brokers                       => $config['brokers']['hash'],
+            zookeeper_connect             => $config['zookeeper']['url'],
+            nofiles_ulimit                => $nofiles_ulimit,
+            jmx_port                      => $config['jmx_port'],
 
-        # (Temporarily?) disable auto leader rebalance.
-        # I am having issues with analytics1012, and I can't
-        # get Camus to consume properly for its preferred partitions
-        # if it is online and the leader.  - otto
-        auto_leader_rebalance_enable => false,
+            # I don't trust auto.leader.rebalance :)
+            auto_leader_rebalance_enable  => false,
 
-        default_replication_factor   => min(3, $config['brokers']['size']),
-        # Start with a low number of (auto created) partitions per
-        # topic.  This can be increased manually for high volume
-        # topics if necessary.
-        num_partitions               => 1,
+            default_replication_factor    => min(3, $config['brokers']['size']),
 
-        # Use LinkedIn recommended settings with G1 garbage collector,
-        jvm_performance_opts         => '-server -XX:PermSize=48m -XX:MaxPermSize=48m -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35',
+            # Start with a low number of (auto created) partitions per
+            # topic.  This can be increased manually for high volume
+            # topics if necessary.
+            num_partitions                => 1,
+
+            # Use LinkedIn recommended settings with G1 garbage collector,
+            jvm_performance_opts          => '-server -XX:PermSize=48m -XX:MaxPermSize=48m -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35',
+        }
+
+
+        # Include Kafka Server Jmxtrans class
+        # to send Kafka Broker metrics to Ganglia and statsd.
+        $group_prefix = "kafka.cluster.${cluster_name}."
+        class { '::confluent::kafka::broker::jmxtrans':
+            group_prefix => $group_prefix,
+            statsd       => hiera('statsd', undef),
+        }
+
+        # Monitor kafka in production
+        if $::realm == 'production' {
+            class { '::confluent::kafka::broker::alerts': }
+        }
+    }
+
+    # TODO: This conditional will be removed once both eqiad and codfw main
+    # cluster have been upgraded to confluent Kafka 0.9
+    else {
+        # export ZOOKEEPER_URL and BROKER_LIST user environment variable.
+        # This makes it much more convenient to run kafka commands without having
+        # to specify the --zookeeper or --brokers flag every time.
+        file { '/etc/profile.d/kafka.sh':
+            owner   => 'root',
+            mode    => '0444',
+            content => template('role/kafka/kafka-profile.sh.erb'),
+        }
+
+        class { '::kafka::server':
+            log_dirs                     => ['/srv/kafka/data'],
+            brokers                      => $config['brokers']['hash'],
+            zookeeper_hosts              => $config['zookeeper']['hosts'],
+            zookeeper_chroot             => $config['zookeeper']['chroot'],
+            nofiles_ulimit               => $nofiles_ulimit,
+            jmx_port                     => $config['jmx_port'],
+
+            # Enable auto creation of topics.
+            auto_create_topics_enable    => true,
+
+            # (Temporarily?) disable auto leader rebalance.
+            # I am having issues with analytics1012, and I can't
+            # get Camus to consume properly for its preferred partitions
+            # if it is online and the leader.  - otto
+            auto_leader_rebalance_enable => false,
+
+            default_replication_factor   => min(3, $config['brokers']['size']),
+            # Start with a low number of (auto created) partitions per
+            # topic.  This can be increased manually for high volume
+            # topics if necessary.
+            num_partitions               => 1,
+
+            # Use LinkedIn recommended settings with G1 garbage collector,
+            jvm_performance_opts         => '-server -XX:PermSize=48m -XX:MaxPermSize=48m -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35',
+        }
+
+        # Include Kafka Server Jmxtrans class
+        # to send Kafka Broker metrics to Ganglia and statsd.
+        $group_prefix = "kafka.cluster.${cluster_name}."
+        class { '::kafka::server::jmxtrans':
+            group_prefix => $group_prefix,
+            statsd       => hiera('statsd', undef),
+            jmx_port     => $config['jmx_port'],
+            require      => Class['::kafka::server'],
+        }
+
+        # Monitor kafka in production
+        if $::realm == 'production' {
+            class { '::kafka::server::monitoring':
+                jmx_port     => $config['jmx_port'],
+                group_prefix => $group_prefix,
+            }
+        }
     }
 
     # firewall Kafka Broker
     ferm::service { 'kafka-broker':
         proto  => 'tcp',
-        port   => $::kafka::server::broker_port,
-        srange => '$DOMAIN_NETWORKS',
+        # TODO: $::confluent::kafka::broker::port doesn't
+        # seem to work as expected.  Hardcoding this for now.
+        port   => 9092,
+        srange => '$ALL_NETWORKS',
     }
 
-    # Include Kafka Server Jmxtrans class
-    # to send Kafka Broker metrics to Ganglia and statsd.
-    $group_prefix = "kafka.cluster.${cluster_name}."
-    class { '::kafka::server::jmxtrans':
-        group_prefix => $group_prefix,
-        statsd       => hiera('statsd', undef),
-        jmx_port     => $config['jmx_port'],
-        require      => Class['::kafka::server'],
-    }
-
-    # Monitor kafka in production
-    if $::realm == 'production' {
-        class { '::kafka::server::monitoring':
-            jmx_port     => $config['jmx_port'],
-            group_prefix => $group_prefix,
-        }
-    }
 
     # Monitor TCP Connection States
     diamond::collector { 'TcpConnStates':
