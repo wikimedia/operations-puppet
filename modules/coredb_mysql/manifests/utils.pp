8457290aa994237fe83d691b7d3bc8f3f5dbc0c3
Jcrespo <jcrespo@wikimedia.org>
2016-07-26 10:50:05 +0200

    Delete coredb_mysql module and dependent roles and modules
    * mysql_wmf
    * db role
    * coredb role
    * mha
    * mysql_multi_instance
    
    After all MySQL 5.5 instances have been upgraded to Mariadb 10,
    delete the coredb_mysql module.
    
    It had some interesting things that have not been ported but:
    
    * Snapshots: we do not do those anymore; a delayed slave +
      xtrabackup is more useful
    * Query digest functionality has been transferred to performance_
      schema
    * MHA (the original tool) is unsupported, and unneded with GTID
    * mysql_multi_instance: replaced by mariadb::sanitarium
    * Other things such as extra icinga checks may be unnecesary or
      can be ported later from git history
    
    Change-Id: Id43776eb209f8984b501c3f9a4361f1cf9290720

--- modules/coredb_mysql/manifests/utils.pp
+++
@@ -1,9 +0,0 @@
-# various utility scripts for core dbs
-class coredb_mysql::utils {
-    file { '/usr/local/bin/master_id.py':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0555',
-        content => template('coredb_mysql/master_id.py.erb'),
-    }
-}
--- modules/mysql_wmf/manifests/mysqlpath.pp
+++
@@ -1,9 +0,0 @@
-class mysql_wmf::mysqlpath {
-    file { '/etc/profile.d/mysqlpath.sh':
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => 'puppet:///modules/mysql_wmf/mysqlpath.sh',
-    }
-}
-
