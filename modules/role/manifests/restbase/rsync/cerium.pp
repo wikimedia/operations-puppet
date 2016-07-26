79920dc7f1a2d7e1bf376009e194c0dcb7df2e6b
Daniel Zahn <dzahn@wikimedia.org>
2016-07-26 15:08:53 -0700

    restbase-test: setup rsync for data from cassandra-test
    urandom has requested to rsync data from cassandra-test over
    to restbase-test, as per the pastebin.
    
    https://phabricator.wikimedia.org/P3575
    
    This sets up temp rsyncd config to allow that.
    
    Change-Id: I2d4ef676d98ec9cc4d00c27446a4aa9f098a6bbf

---
+++ modules/role/manifests/restbase/rsync/cerium.pp
@@ -0,0 +1,33 @@
+# rsync cassandra data from cerium to restbase-test machines
+class role::restbase::rsync::cerium {
+
+    $sourceip='10.64.16.147' # cerium.eqiad.wmnet
+
+    $basepath='/srv/cassandra-a/data/local_group_wikipedia_T_parsoid_'
+
+    ferm::service { 'restbase-rsync-cerium':
+        proto  => 'tcp',
+        port   => '873',
+        srange => "${sourceip}/32",
+    }
+
+    include rsync::server
+
+    rsync::server::module { 'parsoid-html':
+        path        => "${basepath}_html/data-f3c38310f28b11e4852cad59d68785c5/snapshots/1469551242786/",
+        read_only   => 'no',
+        hosts_allow => $sourceip,
+    }
+
+    rsync::server::module { 'parsoid-data':
+        path        => "${basepath}_dataW4ULtxs1oMqJ/data-f5269580f28b11e4a47df15be73644e3/snapshots/1469551242786/",
+        read_only   => 'no',
+        hosts_allow => $sourceip,
+    }
+
+    rsync::server::module { 'parsoid-section':
+        path        => "${basepath}_section_offsets/data-9ec361001b7b11e59825ad59d68785c5/snapshots/1469551242786/",
+        read_only   => 'no',
+        hosts_allow => $sourceip,
+    }
+}
