604e1037a9019caa81c66e20d8c124671f0bba56
Tim Starling <tstarling@wikimedia.org>
2016-07-26 14:48:49 +1000

    Add Html5Depurate module and role
    Change-Id: I606a3645b865ef5d9c96c8f44b0f0cc4b9e3122b

---
+++ modules/role/manifests/html5depurate/server.pp
@@ -0,0 +1,16 @@
+# Class for installing an Html5Depurate service
+# https://www.mediawiki.org/wiki/Html5Depurate
+class role::html5depurate::server {
+    system::role { 'role::html5depurate::server':
+        description => 'Html5Depurate server'
+    }
+
+    $max_memory_mb = ceiling($::memorysize_mb * 0.5)
+
+    class { '::html5depurate':
+        listen_host   => '0.0.0.0',
+        max_memory_mb => $max_memory_mb
+    }
+
+    include ::html5depurate::monitoring
+}
