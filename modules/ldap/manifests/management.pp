342c73f40273ffd54713be9c424304f08058fa06
YuviPanda <yuvipanda@gmail.com>
2016-07-25 19:16:51 -0700

    ldap: Replace change-ldap-password with reset-ldap-password
    Change-Id: If32be1f4dbf4e5c235f7e82004a835135f2c6583

--- modules/ldap/manifests/management.pp
+++ modules/ldap/manifests/management.pp
@@ -1,4 +1,6 @@
 # Bunch of utilities for managing LDAP users
+# Note: We explicitly only use one server here, since we don't want to modify on both
+# servers at the same time
 class ldap::management(
     $server,
     $basedn,
@@ -7,6 +9,8 @@ class ldap::management(
 ) {
     require_package([
         'ldapvi',
+        'python3-ldap3',
+        'python3-yaml',
     ])
   
     file { '/etc/ldapvi.conf':
@@ -14,5 +18,23 @@ class ldap::management(
         mode    => '0440',
         owner   => 'root',
         group   => 'ldap-admins',
+    }
+
+    $yaml_config = {
+        servers  => [$server],
+        basedn   => $basedn,
+        user     => $user,
+        password => $password,
+    }
+
+    file { '/etc/ldap.scriptuser.yaml':
+        content => ordered_yaml($yaml_config),
+    }
+
+    file { '/usr/local/bin/reset-ldap-password':
+        source => 'puppet:///modules/ldap/reset-password',
+        mode   => '0554',
+        owner  => 'root',
+        group  => 'root',
     }
 }
