d9723374c0aa729a07656ba645903248f1ec2b7f
Ariel T. Glenn <ariel@wikimedia.org>
2016-07-26 15:16:06 +0300

    move cronjobs class from role to snapshot module and add user param
    this leaves us with (mostly) only role classes proper, plus one common
    class for incusion in all of them
    
    Change-Id: I98c071af5b10fe4713c1ac3acf85abfadb8957ed

---
+++ modules/snapshot/manifests/cron.pp
@@ -0,0 +1,12 @@
+class snapshot::cron(
+    $user = undef,
+) {
+    class { '::snapshot::cron::mediaperprojectlists': user => $user }
+    class { '::snapshot::cron::pagetitles': user   => $user }
+    class { '::snapshot::cron::cirrussearch': user   => $user }
+    class { '::snapshot::cron::centralauthdump': user   => $user }
+    class { '::snapshot::cron::dumplists': user   => $user }
+    class { '::snapshot::cron::wikidatadumps::json': user   => $user }
+    class { '::snapshot::cron::wikidatadumps::ttl': user   => $user }
+    class { '::snapshot::addschanges': user   => $user }
+}
