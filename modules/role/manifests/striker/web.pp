4800e2f643eb84547b8535a43f3ee4c85b1f8249
Bryan Davis <bd808@wikimedia.org>
2016-07-27 16:21:35 -0600

    [WIP] Provision Striker via scap3
    Provision the Striker Django web application using scap3.
    
    Bug: T141014
    Change-Id: I855f9484f799a6847590b5d1196abf903114fa34

---
+++ modules/role/manifests/striker/web.pp
@@ -0,0 +1,11 @@
+# == Class: role::striker::web
+#
+# Striker is a Django application for managing data related to Tool Labs
+# tools.
+#
+class role::striker::web {
+    include ::striker::uwsgi
+
+    # TODO add nginx vhost (::striker::nginx)
+}
+# vim:sw=4:ts=4:sts=4:ft=puppet:
