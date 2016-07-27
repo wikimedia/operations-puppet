d08e8b60bec6d6b6528fd5458ea2f2a4aae07b69
Bryan Davis <bd808@wikimedia.org>
2016-07-27 11:28:54 -0600

    scap: Allow configuration of the master rsync server in wmflabs
    Make using scap/scap3 in Labs outside of the deployment-prep project
    easier by exposing a class parameter to control the master_rsync setting
    for the wmflabs domain. In a more perfect world the whole /etc/scap.cfg
    file would be generated from hiera data. That refactoring is left for
    another day.
    
    Change-Id: I9f1763f6c16dd0b34b94a27f6f2afa0f69bae78d

--- modules/scap/manifests/init.pp
+++ modules/scap/manifests/init.pp
@@ -1,8 +1,18 @@
-# == Class scap
+# == Class: scap
 #
 # Common role for scap masters and targets
-
-class scap ($deployment_server = 'deployment') {
+#
+# == Parameters:
+#  [*deployment_server*]
+#    Server that provides git repositories for scap3. Default 'deployment'.
+#
+#  [*wmflabs_master*]
+#    Master scap rsync host in the wmflabs domain.
+#    Default 'deployment-tin.eqiad.wmflabs'.
+class scap (
+    $deployment_server = 'deployment',
+    $wmflabs_master = 'deployment-tin.eqiad.wmflabs',
+) {
     package { 'scap':
         ensure => '3.2.0-1',
     }
