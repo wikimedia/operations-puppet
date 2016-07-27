d0e117a9ab7b28fea917c9e8a2ed69330d83cc1d
Bryan Davis <bd808@wikimedia.org>
2016-07-27 16:21:35 -0600

    [WIP] Provision Striker via scap3
    Provision the Striker Django web application using scap3:
    * uwsgi service running Striker
    * memcached server for session cache storage
    ** TODO: Do we have an existing memcached in labs-support that can be used
       instead?
    * nginx vhost for static content and reverse proxy to uwsgi
    
    Configuration of the Striker Django app is done via hiera settings. The
    role settings file at hieradata/role/common/striker/web.yaml contains
    dummy values for the striker::uwsgi::secret_config hiera hash. The
    actual values for these production secrets should be configured via
    non-public hiera settings files.
    
    Bug: T141014
    Change-Id: I855f9484f799a6847590b5d1196abf903114fa34

---
+++ modules/striker/manifests/uwsgi.pp
@@ -0,0 +1,78 @@
+# = Class: striker::uwsgi
+#
+# Striker is a Django application for managing data related to Tool Labs
+# tools.
+#
+# == Parameters:
+# [*port*]
+#   Port that uWSGI demon should listen on
+# [*config*]
+#   Configuration for the deployed Django application. Value should be
+#   a multi-dimentional hash of values.
+# [*deploy_dir*]
+#   Directory that Striker will be deployed to via scap3.
+#   Default /srv/deployment/striker/deploy.
+# [*venv_dir*]
+#   Directory to create/manage Python virtualenv in.
+#   Default /srv/deployment/striker/venv.
+# [*secret_config*]
+#   Additional configuration for the deployed Django application. Value should
+#   be a multi-dimentional hash of values. Useful for setting configuration
+#   values that for one reason or another cannot be provided via `config`
+#   (probably because the values need to be kept out of public hiera files).
+#   Default {}.
+#
+class striker::uwsgi(
+    $port,
+    $config,
+    $deploy_dir    = '/srv/deployment/striker/deploy',
+    $venv_dir      = '/srv/deployment/striker/venv',
+    $secret_config = {},
+){
+    requires_os('Debian >= jessie')
+
+    # Packages needed by python wheels
+    require_package(
+        'libffi6',
+        'libldap-2.4-2',
+        'libmysqlclient18',
+        'libsasl2-2',
+        'libssl1.0.0',
+        'python3',
+        'python3-wheel',
+        'virtualenv',
+    )
+
+    service::uwsgi { 'striker':
+        port            => $port,
+        config          => {
+            'need-plugins' => 'python3',
+            'chdir'        => "${deploy_dir}/striker",
+            'venv'         => $venv_dir,
+            'wsgi'         => 'striker.wsgi',
+            'vacuum'       => true,
+        },
+        healthcheck_url => '/',
+        repo            => 'striker/deploy',
+        sudo_rules      => [
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-striker restart',
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-striker start',
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-striker status',
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-striker stop',
+        ],
+    }
+
+    # OUr ini() function does a shallow merge rather than a deep merge, so
+    # merge the config sections before passing to ini() below.
+    $complete_config = deep_merge($config, $secret_config)
+
+    file { '/etc/striker/striker.ini':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'www-data',
+        mode    => '0440',
+        content => ini($complete_config),
+        notify  => Uwsgi::App['striker'],
+    }
+}
+# vim:sw=4:ts=4:sts=4:ft=puppet:
