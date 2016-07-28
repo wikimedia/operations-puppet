f906f8dc5b404fcb4658a0f0324265a478ac8ee7
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-07-28 11:13:30 +0200

    puppetmaster: add alt names support to puppetmaster.test
    Change-Id: I8e7a70b402102a1b3aa53f8a105f8207aea82e03

--- modules/puppetmaster/manifests/web_test.pp
+++ modules/puppetmaster/manifests/web_test.pp
@@ -4,15 +4,17 @@
 # an alternative puppetmaster frontend which can have different workers than
 # the standard one.
 #
-class puppetmaster::web_test ($server_name, $workers = {}) {
+class puppetmaster::web_test ($server_name, $workers = {}, $alt_names=[]) {
     Class['::puppetmaster'] -> Class['::puppetmaster::web_test']
     $ssl_settings = ssl_ciphersuite('apache', 'compat')
     $ssldir = $::puppetmaster::ssl::ssldir
 
+    $alt_names_list = join(sort($alt_names), ',')
     # Have puppet generate the certificate for this virtualhost
+    # BEWARE: SSL key length cannot be controlled here
     exec { 'generate hostcert':
         require => File["${ssldir}/certs"],
-        command => "/usr/bin/puppet cert generate ${server_name}",
+        command => "/usr/bin/puppet cert generate ${server_name} --dns_alt_names=${alt_names_list}",
         creates => "${ssldir}/certs/${server_name}.pem";
     }
 
