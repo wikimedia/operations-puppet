8ee4515947a9a31b09d68bd6ca5b5e5b060f44d0
Ariel T. Glenn <ariel@wikimedia.org>
2016-07-29 12:10:36 +0300

    add cron job for Content Translation dumps
    Bug: T127793
    Change-Id: I77ee28e9e08e365d2044a54eadfcc9d7f30a26d3

--- modules/snapshot/manifests/cron.pp
+++ modules/snapshot/manifests/cron.pp
@@ -8,5 +8,6 @@ class snapshot::cron(
     class { '::snapshot::cron::dumplists': user   => $user }
     class { '::snapshot::cron::wikidatadumps::json': user   => $user }
     class { '::snapshot::cron::wikidatadumps::ttl': user   => $user }
+    class { '::snapshot::cron::contentxlation': user   => $user }
     class { '::snapshot::addschanges': user   => $user }
 }
