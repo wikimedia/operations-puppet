0b25e0005fb77fffdee2dfe64dd4edca39d448a3
Chad Horohoe <chadh@wikimedia.org>
2016-08-01 10:06:13 -0700

    Phab: Properly remove public_task_dump.py if $dump = false
    Right now we vary installing the script and cron if $dump is set to
    true or not. Instead, always install the script and just vary the
    cron's ensure attribute.
    
    Change-Id: I5597484ae08b92208a1db580f351c09cf5d6d58a

--- modules/phabricator/manifests/tools.pp
+++ modules/phabricator/manifests/tools.pp
@@ -29,22 +29,23 @@ class phabricator::tools (
         require => Package[$deploy_target],
     }
 
-    if ($dump) {
-        $dump_script = "${directory}/public_task_dump.py"
+    $dump_script = "${directory}/public_task_dump.py"
 
-        file { $dump_script:
-            mode    => '0555',
-            require => Package[$deploy_target],
-        }
+    file { $dump_script:
+        mode    => '0555',
+        require => Package[$deploy_target],
+    }
 
-        cron { $dump_script:
-            ensure  => present,
-            command => $dump_script,
-            user    => root,
-            hour    => '2',
-            minute  => '0',
-            require => Package[$deploy_target],
-        }
+    cron { $dump_script:
+        ensure  => $dump ? {
+            false   => absent,
+            default => present,
+        },
+        command => $dump_script,
+        user    => root,
+        hour    => '2',
+        minute  => '0',
+        require => Package[$deploy_target],
     }
 
     # These bz_*_update jobs require the bugzilla_migration DB
