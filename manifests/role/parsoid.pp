71291e9623b0a3bbf27a96af6c117e5f09255db0
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-08-02 13:01:12 +0300

    parsoid: Remove disk space alert
    It was a reaction to
    https://wikitech.wikimedia.org/wiki/Incident_documentation/20140211-Parsoid.
    Things have changed a lot since then, parsoid now uses service-runner
    and the logging infra service-runner provides. The issue described in
    the wikitech page above was anyway due to a program error that
    never manifested in the 2,5 years since then. Remove the check, we will
    fall back to the usual checks we have
    
    Change-Id: Icc8e0cc91718307c310468afcfe010c2dd0d774f

--- manifests/role/parsoid.pp
+++ manifests/role/parsoid.pp
@@ -9,14 +9,6 @@ class role::parsoid {
     include lvs::realserver
 
     include ::parsoid
-
-    # until logging is handled differently, rt 6851
-    nrpe::monitor_service { 'parsoid_disk_space':
-        description  => 'parsoid disk space',
-        nrpe_command => '/usr/lib/nagios/plugins/check_disk -w 40% -c 3% -l -e',
-        critical     => true,
-    }
-
     # Monitor TCP Connection States
     diamond::collector { 'TcpConnStates':
         source => 'puppet:///modules/diamond/collector/tcpconnstates.py',
