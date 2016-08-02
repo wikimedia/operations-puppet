8af41f2f84ef6e1d63d4f58b8d7a75d509f060cd
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-08-02 15:03:11 +0300

    role::labsdb::manager: contain variables in class
    Instead of having the template lookup variables in another namespace,
    one not included by the class itself, include the namespace, declare 2
    new local to the namespace variables and use them instead
    
    Change-Id: I708a99178083e2fad7f704c48e1540bc7659af65

--- modules/role/manifests/labsdb/manager.pp
+++ modules/role/manifests/labsdb/manager.pp
@@ -1,6 +1,9 @@
 # vim: set ts=4 et sw=4:
 class role::labsdb::manager {
     require_package(['python-mysqldb', 'python-yaml'])
+    include passwords::misc::scripts
+    $mysql_root_pass = $::passwords::misc::scripts::mysql_root_pass
+    $mysql_labsdb_root_pass = $::passwords::misc::scripts::mysql_labsdb_root_pass
 
     file { '/usr/local/sbin/skrillex.py':
         owner  => 'root',
