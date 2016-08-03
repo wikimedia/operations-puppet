5648f358564ae2cf06986be3f852f3233934755e
Ariel T. Glenn <ariel@wikimedia.org>
2016-08-04 00:26:09 +0300

    capture dumps cron job output in log and add log rotation
    Change-Id: If90a3237ecb46a83bc7ce56e153334b8ea76f384

--- modules/snapshot/manifests/dumps/cron.pp
+++ modules/snapshot/manifests/dumps/cron.pp
@@ -21,22 +21,35 @@ class snapshot::dumps::cron(
         content => template('snapshot/dumps/fulldumps.sh.erb'),
     }
 
-    file { '/var/log/dumps':
+    $logdir = '/var/log/dumps'
+    file { $logdir:
       ensure => 'directory',
       path   => '/var/log/dumps',
       mode   => '0755',
       owner  => $user,
     }
 
+    file { '/etc/logrotate.d/xmldumps':
+        mode   => '0444',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/snapshot/dumps/logrotate.xmldumps',
+    }
+
+    $runtype = hiera('snapshot::dumps::runtype', 'regular')
+
+    $fullcommand = "/usr/local/bin/fulldumps.sh 01 14 ${runtype}"
+    $partialcommand = "/usr/local/bin/fulldumps.sh 20 25 ${runtype}"
+    $output = "${logdir}/xmldumps_\$(date -u +\%s).log"
+
     # fixme there is an implicit dependency on
     # wikidump.conf.* plus some stage files, make explicit
-    $runtype = hiera('snapshot::dumps::runtype', 'regular')
 
     cron { 'fulldumps_rest':
         ensure      => 'present',
         environment => 'MAILTO=ops-dumps@wikimedia.org',
         user        => $user,
-        command     => "/usr/local/bin/fulldumps.sh 01 14 ${runtype} full > /dev/null",
+        command     => "${fullcommand} full > ${output}",
         minute      => '05',
         hour        => [8, 20],
         monthday    => '01-14',
@@ -46,10 +59,11 @@ class snapshot::dumps::cron(
         ensure      => 'present',
         environment => 'MAILTO=ops-dumps@wikimedia.org',
         user        => $user,
-        command     => "/usr/local/bin/fulldumps.sh 20 25 ${runtype} partial > /dev/null",
+        command     => "${partialcommand} full > ${output}",
         minute      => '05',
         hour        => [8, 20],
         monthday    => '20-25',
     }
+
 
 }
