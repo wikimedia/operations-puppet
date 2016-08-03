bc76c43d0a0876da761a63ec5cf9ca81a00e87dc
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2016-08-03 12:19:24 +0200

    puppet_compiler: add packages for labs realm
    Bug: T97081
    Change-Id: I732bcb92d065bf60ead9f3213f8409a624d399ef

--- modules/puppet_compiler/manifests/packages.pp
+++ modules/puppet_compiler/manifests/packages.pp
@@ -3,6 +3,6 @@
 # Installs all the needed packages
 class puppet_compiler::packages {
 
-    require_package 'python-yaml', 'python-requests', 'python-jinja2', 'nginx'
+    require_package 'python-yaml', 'python-requests', 'python-jinja2', 'nginx', 'ruby-httpclient', 'ruby-ldap'
 
 }
