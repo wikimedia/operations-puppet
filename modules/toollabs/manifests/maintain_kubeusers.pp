f5da83fbac28cdcc97d249a18e040efa9c274479
YuviPanda <yuvipanda@gmail.com>
2016-08-08 12:07:27 -0700

    tools: Use LDAP servers in HA manner for maintain-kubeusers
    Bug: T142394
    Change-Id: I813efa7903c9a438edc59dfaab88c6beee8df751

--- modules/toollabs/manifests/maintain_kubeusers.pp
+++ modules/toollabs/manifests/maintain_kubeusers.pp
@@ -1,6 +1,18 @@
 class toollabs::maintain_kubeusers(
     $k8s_master,
 ) {
+    # We need a newer version of python3-ldap3 than what is in Jessie
+    # For the connection time out / server pool features
+    apt::pin { 'python3-ldap3':
+        pin      => 'release a=jessie-backports',
+        priority => '1001',
+        before   => Package['python3-ldap3'],
+    }
+
+    package { 'python3-ldap3':
+        ensure => present,
+    }
+
     file { '/usr/local/bin/maintain-kubeusers':
         source => 'puppet:///modules/toollabs/maintain-kubeusers',
         owner  => 'root',
