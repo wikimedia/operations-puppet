16c1e499594c2694bd8cd2b5d376465bcd845c11
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-08-09 15:37:36 +0200

    puppetmaster: add role for puppetdb
    Bug: T142363
    Change-Id: I5358e28edfa3cb448540f9b42e210829c0b35947

---
+++ modules/puppetmaster/manifests/puppetdb.pp
@@ -0,0 +1,111 @@
+# Class puppetmaster::puppetdb
+#
+# Sets up a puppetdb instance and the corresponding database server.
+class puppetmaster::puppetdb($master) {
+    require_os('Debian >= jessie')
+
+    $puppetdb_pass = hiera('puppetdb::password::rw')
+
+    # set up the postgres database
+    class { 'puppetmaster::puppetdb::databases':
+        master => $master,
+    }
+
+    # Set up nginx as a reverse-proxy
+    class { 'nginx::simple_tlsproxy':
+        backend_port => 8080,
+        site_name    => $::fqdn,
+    }
+
+    package { 'puppetdb':
+        ensure => present,
+    }
+
+
+    ## Configuration
+
+    file { '/etc/puppetdb/conf.d':
+        ensure  => directory,
+        owner   => 'puppetdb',
+        group   => 'root',
+        mode    => '0750',
+        recurse => true,
+        require => Package['puppetdb']
+    }
+
+    $default_db_settings = {
+        'classname'   => 'org.postgresql.Driver',
+        'subprotocol' => 'postgresql',
+        'username'    => 'puppetdb',
+        'password'    => $puppetdb_pass,
+    }
+
+    # Read-write connections go to the master.
+    # Only the master should be performing housekeeping work
+    if ($master == $::fqdn) {
+        $db_settings = merge(
+            $default_db_settings,
+            {
+                'node-ttl'    => '14d',
+                'report-ttl'  => '1d',
+                'gc-interval' => '20m',
+                'subname'     => "//${::master}:5432/puppetdb?ssl=true",
+            }
+        )
+    } else {
+        $db_settings = merge(
+            $default_db_settings,
+            {'subname' => "//${::master}:5432/puppetdb?ssl=true"}
+        )
+    }
+
+    puppetmaster::puppetdb::config { 'database':
+        settings => $db_settings,
+    }
+
+    # The read database is always the local one.
+    puppetmaster::puppetdb::config { 'read-database':
+        settings => merge(
+            $default_db_settings,
+            {
+                'subname' => "//${::fqdn}:5432/puppetdb?ssl=true"
+            }
+        ),
+    }
+
+    ::base::expose_puppet_certs { '/etc/puppetdb':
+        ensure          => present,
+        provide_private => true,
+        user            => 'puppetdb',
+        group           => 'puppetdb',
+
+    }
+
+    puppetmaster::puppetdb::config { 'jetty':
+        settings => {
+            'port'        => 8080,
+            'ssl-port'    => 8081,
+            'ssl-key'     => '/etc/puppetdb/ssl/server.key',
+            'ssl-cert'    => '/etc/puppetdb/ssl/cert.pem',
+            'ssl-ca-cert' => '/etc/ssl/certs/Puppet_Internal_CA.pem',
+        },
+        require  => Base::Expose_puppet_certs['/etc/puppetdb']
+    }
+
+    puppetmaster::puppetdb::config { 'global':
+        settings => {
+            'vardir'         => '/var/lib/puppetdb',
+            'logging-config' => '/etc/puppetdb/logback.xml'
+        }
+    }
+
+    puppetmaster::puppetdb::config { 'repl':
+        settings => {'enabled' => false}
+    }
+
+    ## Enable the service itself
+    base::service_unit { 'puppetdb':
+        ensure  => present,
+        systemd => true,
+    }
+}
---
+++ modules/role/manifests/puppetmaster/puppetdb.pp
@@ -0,0 +1,50 @@
+class role::puppetmaster::puppetdb {
+    include standard
+    include ::base::firewall
+    include ::passwords::postgres
+
+
+    system::role { 'role::puppetmaster::puppetdb':
+        ensure      => 'present',
+        description => 'PuppetDB server',
+    }
+
+    ganglia::plugin::python { 'diskstat': }
+
+    ferm::service { 'postgresql':
+        proto  => 'tcp',
+        port   => 5432,
+        srange => '$DOMAIN_NETWORKS',
+    }
+
+    # Only the TLS-terminating nginx proxy will be exposed
+    ferm::service { 'puppetdb':
+        proto  => 'tcp',
+        port   => 443,
+        srange => '$DOMAIN_NETWORKS',
+    }
+
+    class { 'postgresql::ganglia':
+        pgstats_user => $passwords::postgres::ganglia_user,
+        pgstats_pass => $passwords::postgres::ganglia_pass,
+    }
+
+    # Tuning
+    file { '/etc/postgresql/9.4/main/tuning.conf':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/role/puppetdb/tuning.conf',
+    }
+
+    sysctl::parameters { 'postgres_shmem':
+        values => {
+            # That is derived after tuning postgresql, deriving automatically is
+            # not the safest idea yet.
+            'kernel.shmmax' => 8388608000,
+        },
+    }
+
+    include ::puppetmaster::puppetdb
+}
