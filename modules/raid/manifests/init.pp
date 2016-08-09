038e5e85e38a2422cc66a0c6ce5944fbfd872ee3
Riccardo Coccioli <rcoccioli@wikimedia.org>
2016-08-10 00:30:06 +0200

    Monitoring: avoid NRPE limit for RAID get status
    NRPE has a limit of 1024 bytes and the get RAID status scripts output
    were truncated. Compressing the output to reduce the probability to
    reach the limit. Encoding the NULL bytes that NRPE doesn't handle. Given
    the specific domain there is no need of a full yEnc encoding or similar.
    
    Bug: T142085
    Change-Id: I585f1cc8d4eaff408c8ebc43252e09770d10f3cd

--- modules/raid/manifests/init.pp
+++ modules/raid/manifests/init.pp
@@ -34,7 +34,7 @@ class raid {
         }
 
         nrpe::check { 'get_raid_status_megacli':
-            command => "/usr/bin/sudo ${get_raid_status_megacli}",
+            command => "/usr/bin/sudo ${get_raid_status_megacli} -c",
         }
 
         nrpe::monitor_service { 'raid_megaraid':
@@ -94,7 +94,7 @@ class raid {
         }
 
         nrpe::check { 'get_raid_status_hpssacli':
-            command => $get_raid_status_hpssacli,
+            command => "${get_raid_status_hpssacli} -c",
         }
     }
 
