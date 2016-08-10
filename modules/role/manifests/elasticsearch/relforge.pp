97bdf3a8a33484f2e7c595564bb5c24bdd7c5ba4
Guillaume Lederrey <glederrey@wikimedia.org>
2016-08-10 22:09:38 +0200

    elasticsearch - move relforge to its own role
    As suggested during an IRC discussion, create a specific role for relforge,
    move all common bits in role::elasticsearch::common and simplify the hiera
    configuration.
    
    A few other clean up / improvements are part of this change:
    
    * add icinga checks for relforge cluster
    * update debdeploy configuration for relforge cluster to make it part
      of elastic canaries
    
    Change-Id: I83d5ef5728aa5851ccf2d4d9cfde803bba0e9c6a

---
+++ modules/role/manifests/elasticsearch/relforge.pp
@@ -0,0 +1,10 @@
+# = Class: role::elasticsearch::relforge
+#
+# This class sets up Elasticsearch for relevance forge.
+#
+class role::elasticsearch::relforge {
+
+    include ::role::elasticsearch::common
+    include ::elasticsearch::nagios::check
+
+}
