21b6b276f419f743fe1a7a45ca74ad29a9cf1602
Daniel Zahn <dzahn@wikimedia.org>
2016-08-12 09:12:10 -0700

    remove exception for gerrit from enforce-users-groups.sh
    Nowadays gerrit2 is using 444:444 which is ok (system users < 1000).
    So there does not have to be an exception for it anymore here.
    
    Change-Id: Ib303b4ffb2931f7a6905610acb8359a5ada71fc2

--- modules/admin/files/enforce-users-groups.sh
+++ modules/admin/files/enforce-users-groups.sh
@@ -14,7 +14,6 @@ ARCHIVE_DIR='/var/userarchive'
 EXCLUDE=("nobody" \
          "l10nupdate" \
          "mwdeploy" \   # eventlog*
-         "gerrit2" \    # lead.wikimedia.org
          "releases" \   # tin.eqiad.wmnet/mira.codfw.wmnet
          "reprepro" \   # bromine.eqiad.wmnet
          "mysql" \      # pc1001.eqiad.wmnet
