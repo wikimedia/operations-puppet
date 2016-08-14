ab769d5cf9f22403ccff8b949d35f9744d67102c
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-08-14 12:11:28 +0200

    ssh::client: allow using puppetdb
    Bug: T142846
    Change-Id: I91320b04c97a742cd367f122a504f79b7056f8fd

--- modules/ssh/manifests/client.pp
+++ modules/ssh/manifests/client.pp
@@ -5,12 +5,22 @@ class ssh::client {
 
     # no exported resources on Labs == no sshknowngen
     if $::realm == 'production' {
-        file { '/etc/ssh/ssh_known_hosts':
-            content => generate('/usr/local/bin/sshknowngen'),
-            backup  => false,
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0644',
+        if $::settings::storeconfigs_backend == 'puppetdb' {
+            file { '/etc/ssh/ssh_known_hosts':
+                content => template('ssh/known_hosts.erb'),
+                backup  => false,
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0644',
+            }
+        } else {
+            file { '/etc/ssh/ssh_known_hosts':
+                content => generate('/usr/local/bin/sshknowngen'),
+                backup  => false,
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0644',
+            }
         }
     }
 }
