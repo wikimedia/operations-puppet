bae4fdde97bbafff23d3a35deb6aa126af2d0e4e
Alex Monk <alex@wikimedia.org>
2016-08-15 00:09:42 +0100

    labsprojectfrommetadata: Pull project_id from new field
    This was added in upstream. Once this is done we can pull our custom code
    that was setting up the custom attribute out of nova_ldap.
    
    Bug: T105891
    Change-Id: I806c2b4538e4097b0dc489b54a56e45ce178645b

--- modules/base/lib/facter/labsprojectfrommetadata.rb
+++ modules/base/lib/facter/labsprojectfrommetadata.rb
@@ -9,8 +9,8 @@ Facter.add(:labsprojectfrommetadata) do
     domain = Facter::Util::Resolution.exec("hostname -d").chomp
     if (domain.end_with? ".wmflabs") || (domain.end_with? ".labtest")
       # query the nova metadata service at 169.254.169.254
-      metadata = Facter::Util::Resolution.exec("curl -f http://169.254.169.254/openstack/2013-10-17/meta_data.json/ 2> /dev/null").chomp
-      metadata[/project-id\":\ \"(.*?)\"/, 1]
+      metadata = Facter::Util::Resolution.exec("curl -f http://169.254.169.254/openstack/2015-10-15/meta_data.json/ 2> /dev/null").chomp
+      metadata[/project_id\":\ \"(.*)\",/, 1]
     else
       nil
     end
