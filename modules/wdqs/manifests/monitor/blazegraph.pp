7485ac017f5d7f9fd72225e21103680c2f73e1e6
Guillaume Lederrey <glederrey@wikimedia.org>
2016-08-16 15:00:09 +0200

    WDQS - fix icinga graphite check, metric has been renamed
    Metrics published by varnish have been renamed. Check needs to be updated.
    
    Bug: T138546
    Change-Id: I58108005bd2145a4b09d78c4605424d63d430f62

--- modules/wdqs/manifests/monitor/blazegraph.pp
+++ modules/wdqs/manifests/monitor/blazegraph.pp
@@ -28,9 +28,10 @@ class wdqs::monitor::blazegraph {
 
     # raise a warning / critical alert if response time was over 2 minutes / 5 minutes
     # more than 5% of the time during the last minute
+    $sanitized_hostname = regsubst($::fqdn, '\.', '_', 'G')
     monitoring::graphite_threshold { 'wdqs-response-time':
         description   => 'Response time of WDQS',
-        metric        => "varnish.eqiad.backends.be_${::hostname}.GET.p99",
+        metric        => "varnish.eqiad.backends.be_${sanitized_hostname}.GET.p99",
         warning       => 120000, # 2 minutes
         critical      => 300000, # 5 minutes
         from          => '10min',
