ba59831e8ccc3165879822578099c2887288d705
Mukunda Modell <mmodell@wikimedia.org>
2016-08-16 18:05:06 -0500

    Make phabricator monitoring dependent on $::site
    For now we don't want to be monitoring phab2001
    
    Change-Id: I3c9a3bda0e6f2b3b20b7049c18c450ad4722a754

--- modules/phabricator/manifests/monitoring.pp
+++ modules/phabricator/manifests/monitoring.pp
@@ -1,21 +1,22 @@
 # == Class: phabricator::monitoring
 #
 class phabricator::monitoring {
+    if $::site == 'eqiad' {
+        include apache::mod::status
+        diamond::collector { 'ApacheStatusSimple':
+            source   => 'puppet:///modules/phabricator/monitor/apache_status.py',
+        }
 
-    include apache::mod::status
-    diamond::collector { 'ApacheStatusSimple':
-        source   => 'puppet:///modules/phabricator/monitor/apache_status.py',
-    }
+        nrpe::monitor_service { 'check_phab_taskmaster':
+            description   => 'PHD should be supervising processes',
+            nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 3:150 -u phd',
+            contact_group => 'admins,phabricator,sms',
+        }
 
-    nrpe::monitor_service { 'check_phab_taskmaster':
-        description   => 'PHD should be supervising processes',
-        nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 3:150 -u phd',
-        contact_group => 'admins,phabricator,sms',
-    }
-
-    monitoring::service { 'phabricator-https':
-        description   => 'https://phabricator.wikimedia.org',
-        check_command => 'check_https_phabricator',
-        contact_group => 'admins,phabricator,sms',
+        monitoring::service { 'phabricator-https':
+            description   => 'https://phabricator.wikimedia.org',
+            check_command => 'check_https_phabricator',
+            contact_group => 'admins,phabricator,sms',
+        }
     }
 }
