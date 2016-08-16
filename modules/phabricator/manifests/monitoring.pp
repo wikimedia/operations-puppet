c835f6d0438cf71c5ff68c1fda59ed5737408be0
Mukunda Modell <mmodell@wikimedia.org>
2016-08-16 18:05:06 -0500

    phabricator: only send SMS for issues on active server
    Add to hiera which server is the current active server.
    
    Change the contact groups based on the hostname being
    the active server or not.
    
    If group "sms" is used, that means paging.
    
    This way we will not get paged for the inactive server
    and if we switch it's in this one place in hiera, not
    in .pp files.
    
    Change-Id: I3c9a3bda0e6f2b3b20b7049c18c450ad4722a754

--- modules/phabricator/manifests/monitoring.pp
+++ modules/phabricator/manifests/monitoring.pp
@@ -7,15 +7,24 @@ class phabricator::monitoring {
         source   => 'puppet:///modules/phabricator/monitor/apache_status.py',
     }
 
+    $phabricator_active_server = hiera('phabricator_active_server')
+
+    if $::hostname == $phabricator_active_server {
+        $phab_contact_groups = 'admins,phabricator,sms'
+    } else {
+        $phab_contact_groups = 'admins,phabricator'
+    }
+
     nrpe::monitor_service { 'check_phab_taskmaster':
         description   => 'PHD should be supervising processes',
         nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 3:150 -u phd',
-        contact_group => 'admins,phabricator,sms',
+        contact_group => $phabricator_contact_groups,
     }
 
     monitoring::service { 'phabricator-https':
         description   => 'https://phabricator.wikimedia.org',
         check_command => 'check_https_phabricator',
-        contact_group => 'admins,phabricator,sms',
+        contact_group => $phabricator_contact_groups,
     }
+
 }
