45d874d8fc9837fb79558e37d6d2b3bc7e872a11
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-08-17 17:37:24 +0300

    maps: Set up postgres replication lag monitoring
    Add an NRPE check used to check the Postgres replication lag. Add
    critical/warning thresholds of 10 and 5 minutes respectively
    
    Change-Id: I6692d472aacd85b23db2c4ce50c86834d0fa6959

--- modules/role/manifests/maps/slave.pp
+++ modules/role/manifests/maps/slave.pp
@@ -7,5 +7,15 @@ class role::maps::slave {
         ensure      => 'present',
         description => 'Maps Postgres slave',
     }
+
+    $pg_password = hiera('postgresql::slave::replication_pass')
+    $critical = 1800
+    $warning = 300
+    $command = "/usr/lib/nagios/plugins/check_postgres_replication_lag.py \
+-U replication -P ${pg_password} -D template1 -C ${critical} -W ${warning}"
+    nrpe::monitor_service { 'postgres-rep-lag':
+        description  => 'Postgres Replication Lag',
+        nrpe_command => $command,
+    }
 }
 
