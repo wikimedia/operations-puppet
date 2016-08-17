wikimedia/operations-puppet

commit a19cbb741c3b0d30ec246c0fa37b1c273c0abd20

postgres: Provision a replication lag check script
Have the postgres::slave class ship a replication lag check script that
can be used to monitor the lag exhibited by postgres slaves

Change-Id: I2f5965eea89d4a9362a37baa1a98ee15bfecd10a

modules/postgresql/manifests/slave.pp
@@ -83,4 +83,13 @@ class postgresql::slave(
             require     => Class['postgresql::server'],
         }
     }
+
+    # Provisioning a script to conduct replication lag checks
+    file { '/usr/lib/nagios/plugins/check_postgres_replication_lag.py':
+        ensure => $ensure,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        source => 'puppet:///modules/postgres/check_postgres_replication_lag.py',
+    }
 }

modules/postgresql/files/check_postgres_replication_lag.py
@@ -1,0 +1,105 @@
+#!/usr/bin/env python
+# written in 2.6.6 on CentOS 6. All other versions untested.
+#Header Info
+__author__= 'Kirk Hammond'
+__email__ = 'kirkdhammond@gmail.com'
+__version__ = '1.0'
+__license__ = "GPLv3"
+__maintainer__ = "Kirk Hammond"
+__status__ = "Production"
+__credits__ = "Kirk Hammond"
+
+"""
+This script will check the hot standby replication delay of a postgresql database.
+It is more secure to provide the user executing the script with a .pgpass file than to include the password in the script.
+"""
+
+
+#import libraries
+from optparse import OptionParser, OptionGroup
+import psycopg2
+import sys
+
+
+# parse command arguemnts and return options
+def parse_args():
+    parser = OptionParser()
+    parser.description = "Check streaming replication delay"
+    parser.version = __version__
+    parser.add_option("-H", "--host", dest="hostname", default="127.0.0.1",
+                      help="Name of the host you are checking")
+    parser.add_option("-O", "--port", dest="port", default="5432",
+                       help="Port you will connect to the database with")
+    parser.add_option("-U", "--user", dest="username", default="postgres",
+                       help="Username for the database")
+    parser.add_option("-P", "--password", dest="password",
+                       help="Password the database")
+    parser.add_option("-D", "--database", dest="database",
+                       help="Datbase you are checking") 
+    parser.add_option("-W", "--warn", dest="warn", default="300",
+                       help="Warning alert delay in seconds") 
+    parser.add_option("-C", "--crit", dest="crit", default="1800",
+                       help="Critical alert delay in seconds") 
+    (options, args) = parser.parse_args()
+    return options
+
+
+# check delay using options from parse_args
+def check_delay(options):
+    username = str(options.username)
+    password = str(options.password)
+    port = str(options.port)
+    hostname = str(options.hostname)
+    database = str(options.database)
+    conn_string = "host=" + hostname + " dbname=" + database + " user=" + username + " password=" + password
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute('SELECT CASE WHEN pg_last_xlog_receive_location() = pg_last_xlog_replay_location() THEN 0 ELSE EXTRACT (EPOCH FROM now() - pg_last_xact_replay_timestamp()) END AS log_delay;')
+    delay = cursor.fetchall()
+    delay = delay.pop()
+    delay = delay[0]
+    return delay
+
+
+# return results and graphing data to Nagios
+def nagios(delay,options):
+    #nagios return codes
+    UNKNOWN = -1
+    OK = 0
+    WARNING = 1
+    CRITICAL = 2
+    warn = float(options.warn)
+    crit = float(options.crit)
+    #pop delay out of list and get float out of tuple for direct comparison to warn/crit float values
+    if delay > crit:
+        print  "CRITICAL - Rep Delay is:", str(delay), 'Seconds', '| Seconds=' + str(delay) + 's' + str(";") + str(warn) + str(";") + str(crit) + str(";" ) + str("14400")
+        sys.exit(CRITICAL)
+    elif delay > warn:
+        print "WARNING  - Rep Delay is:", str(delay), 'Seconds', '| Seconds=' + str(delay) + 's' + str(";") + str(warn) + str(";") + str(crit) + str(";" ) + str("14400")
+        sys.exit(WARNING)
+    elif delay < warn and delay < crit:
+        print "OK - Rep Delay is:", str(delay), 'Seconds', '| Seconds=' + str(delay) + 's' + str(";") + str(warn) + str(";") + str(crit) + str(";" ) + str("14400")
+        sys.exit(OK)
+    else:
+        print  "UNKNOWN"
+        sys.exit(UNKNOWN)
+
+
+
+
+# main function, controls flow of script
+def main():
+
+    #call parse_arges and return options for script
+    options = parse_args()
+
+    # execute command using options from parse_args
+    delay = check_delay(options)
+
+    #call nagios process
+    nagios(delay,options)
+
+
+# call main function
+if __name__ == '__main__':
+  main()

tox.ini
@@ -10,7 +10,7 @@ max-line-length = 100
 # E402: module level import not at top of file
 ignore = E123,E133,E226,E241,E242,E402
 # Upstream files that don't pass flake8 but should not be locally modified
-exclude = modules/letsencrypt/files/acme_tiny.py,modules/varnish/files/varnishapi.py
+exclude = modules/letsencrypt/files/acme_tiny.py,modules/varnish/files/varnishapi.py,modules/postgresql/files/check_postgres_replication_lag.py
 
 [testenv]
 deps =

commit 138a631037ad7afb4c3d1439f9d1c854c20d5421

postgresql: Fix silly typo introduced in a19cbb7
Missed the ql suffix

Change-Id: I43dc673c69804bdeb23b6b51b5587d5ece258fc8

modules/postgresql/manifests/slave.pp
@@ -90,6 +90,6 @@ class postgresql::slave(
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => 'puppet:///modules/postgres/check_postgres_replication_lag.py',
+        source => 'puppet:///modules/postgresql/check_postgres_replication_lag.py',
     }
 }