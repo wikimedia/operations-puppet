882d7f134feac2ed0d664740b83e04db940d9040
Guillaume Lederrey <glederrey@wikimedia.org>
2016-08-18 17:25:38 +0200

    elasticsearch - check shards via the service, not via each individual node
    Checking cluster state on each node is redundant and generate a lot of noise.
    Cluster wide checks are now done on the service only.
    
    The logstash cluster is left unchanged as it does not have LVS.
    
    The relforge cluster is at the moment left unmonitored. This will be
    fixed once a cleanup of the different elasticsearch roles is done (see
    https://gerrit.wikimedia.org/r/#/c/304067/).
    
    Bug: T133844
    Change-Id: Ica721152c10d777003726e80fa03ed82c69c8a10

--- manifests/role/icinga.pp
+++ manifests/role/icinga.pp
@@ -24,6 +24,7 @@ class role::icinga(
     include icinga::monitor::certs
     include icinga::monitor::gsb
     include icinga::monitor::commons
+    include icinga::monitor::elasticsearch
     include lvs::monitor
     include role::authdns::monitoring
     include netops::monitoring
