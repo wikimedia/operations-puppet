03d72a95dd99787af51c6614c668bf666e3c37a7
Ariel T. Glenn <ariel@wikimedia.org>
2016-08-19 10:14:39 +0300

    reduce cronspam, eliminate dataset rsync whines about vanished files
    followup to e6f42d4b602246b5e225378871cb2eef72ad7706
    
    Change-Id: I2b6f678e8cc2b9d3f7b5c33b783bbc938892cfa9

--- modules/dataset/manifests/cron/rsync/peers.pp
+++ modules/dataset/manifests/cron/rsync/peers.pp
@@ -20,7 +20,7 @@ class dataset::cron::rsync::peers($enable=true) {
     cron { 'rsync-dumps':
         ensure  => $ensure,
         # filter out error messages about vanishing files, we don't want email for that
-        command => '/usr/bin/python /usr/local/bin/rsync-dumps.py | grep -v "vanished" ',
+        command => '/usr/bin/python /usr/local/bin/rsync-dumps.py 2>&1 | grep -v "vanished" ',
         user    => 'root',
         minute  => '0',
         hour    => '*/2',
