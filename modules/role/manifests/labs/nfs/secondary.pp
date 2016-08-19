af80afd5536fc6b8008466775cd1ee4c21daf931
cpettet <rush@wikimedia.org>
2016-08-19 16:07:25 -0500

    labstore100[4|5]: define initial labstore::drbd::resource
    Bug: T126083
    Change-Id: I038a55884eadcae39f327339fee76909e6c31d49

--- modules/role/manifests/labs/nfs/secondary.pp
+++ modules/role/manifests/labs/nfs/secondary.pp
@@ -9,4 +9,25 @@ class role::labs::nfs::secondary($monitor = 'eth0') {
 
     # Enable RPS to balance IRQs over CPUs
     interface::rps { $monitor: }
+
+    labstore::drbd::resource {'test':
+        nodes  => ['labstore1004', 'labstore1005'],
+        port   => '7790',
+        device => '/dev/drbd1',
+        disk   => '/dev/tools-project/tools-project',
+    }
+
+    labstore::drbd::resource {'tools':
+        nodes  => ['labstore1004', 'labstore1005'],
+        port   => '7791',
+        device => '/dev/drbd2',
+        disk   => '/dev/tools-project/tools-project',
+    }
+
+    labstore::drbd::resource {'others':
+        nodes  => ['labstore1004', 'labstore1005'],
+        port   => '7792',
+        device => '/dev/drbd3',
+        disk   => '/dev/misc/others',
+    }
 }
