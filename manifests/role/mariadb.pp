5a7065196084eaef3215ccce231227e62c22a8d9
Dan Duvall <dduvall@wikimedia.org>
2016-08-19 11:16:52 -0700

    beta: Create and mount LVM volumes for mariadb
    Previous beta database instances have manually provisioned LVM volumes
    for the mariadb data directory. Provision these volumes automatically
    using 80% of available space.
    
    An additional LVM volume was added using 100% of the remaining space
    (20%) for the tmp directory.
    
    Bug: T138778
    Change-Id: I4b856c0c7de52f7c16e75acf581443238b07419d

--- manifests/role/mariadb.pp
+++ manifests/role/mariadb.pp
@@ -368,6 +368,19 @@ class role::mariadb::beta {
     include mariadb::packages
     include passwords::misc::scripts
 
+    labs_lvm::volume { 'sqldata':
+        mountat => '/mnt/sqldata',
+        size    => '80%FREE',
+        before  => Class['mariadb::packages'],
+    }
+
+    labs_lvm::volume { 'tmp':
+        mountat => '/mnt/tmp',
+        size    => '100%FREE',
+        before  => Class['mariadb::packages'],
+        require => Labs_lvm::Volume['sqldata'],
+    }
+
     class { 'mariadb::config':
         prompt   => 'BETA',
         config   => 'mariadb/beta.my.cnf.erb',
