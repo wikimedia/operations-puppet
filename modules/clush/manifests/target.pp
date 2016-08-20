d92089d35315c8f356c436047ea543d6cd816a31
YuviPanda <yuvipanda@gmail.com>
2016-08-20 11:59:36 -0700

    [WIP] Introduce 'clush' module and toollabs role
    Change-Id: I6f3ffa16b759ddb4c2fd6bf5753b82796d84c6d8

---
+++ modules/clush/manifests/target.pp
@@ -0,0 +1,19 @@
+define clush::target(
+    $username = $title,
+    $ensure = present,
+) {
+
+    user { $username:
+        ensure     => $ensure,
+        system     => true,
+        home       => '/var/lib/clush',
+        managehome => true,
+        shell      => '/bin/bash',
+    }
+
+    ssh::userkey { $username:
+        ensure  => $ensure,
+        content => secret("clush/${username}.pub"),
+        require => User[$username],
+    }
+}
