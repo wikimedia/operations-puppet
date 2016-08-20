1cc85330c645d7e3162e263bebb60d797c08b0b5
YuviPanda <yuvipanda@gmail.com>
2016-08-20 11:59:36 -0700

    [WIP] Introduce 'clush' module and toollabs role
    Change-Id: I6f3ffa16b759ddb4c2fd6bf5753b82796d84c6d8

---
+++ modules/role/manifests/toollabs/clush/master.pp
@@ -0,0 +1,47 @@
+class role::toollabs::clush::master {
+    class { '::clush::master':
+        username => 'clushuser',
+    }
+
+    require_package('python3-yaml')
+
+    file { '/usr/local/sbin/tools-clush-generator':
+        ensure => file,
+        source => 'puppet:///modules/role/toollabs/clush/tools-clush-generator',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+
+    file { '/usr/local/sbin/tools-clush-interpreter':
+        ensure => file,
+        source => 'puppet:///modules/role/toollabs/clush/tools-clush-interpreter',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+
+    cron { 'update_tools_clush':
+        ensure  => present,
+        command => '/usr/local/sbin/tools-clush-generator /etc/clustershell/tools.yaml',
+        hour    => '*/1',
+        user    => 'root'
+    }
+
+    $groupsConfig = {
+        'Main' => {
+            'default' => 'Tools',
+        },
+        'Tools' => {
+            'map' => '/usr/local/sbin/tools-clush-interpreter /etc/clustershell/tools.yaml map $GROUP',
+        }
+    }
+
+    file { '/etc/clustershell/groups.conf':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => ini($groupsConfig),
+    }
+}
