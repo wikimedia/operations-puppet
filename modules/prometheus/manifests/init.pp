c97185e6bf02b54f59b01d545093018bbc59db89
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-08-23 10:33:37 +0100

    prometheus: add logging and rotation
    Bug: T126785
    Change-Id: I64f7315872766291a7769599a4dbd491e29e3d1b

--- modules/prometheus/manifests/init.pp
+++ modules/prometheus/manifests/init.pp
@@ -9,4 +9,14 @@ class prometheus {
     file { '/srv/prometheus':
         ensure => directory,
     }
+
+    logrotate::conf { 'prometheus':
+        ensure => present,
+        source => 'puppet:///modules/prometheus/prometheus.logrotate.conf',
+    }
+
+    rsyslog::conf { 'prometheus':
+        source   => 'puppet:///modules/prometheus/prometheus.rsyslog.conf',
+        priority => 40,
+    }
 }
