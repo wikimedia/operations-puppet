1090619ae54a5aa666e22360565caad6389f1a73
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-08-24 15:15:58 +0200

    scap::source: also define the corresponding dsh group
    Currently every repository deployed via scap keeps the list of scap
    targets within the repository itself. We want instead to be able to link
    the list of hosts to deploy to with the conftool dynamic state, and to
    expliticly declare every additional host in a single place (hiera), so
    that in case of decommissioning/maintenance the operations team won't
    need to go and change the scap targets in 10 different repositories.
    
    Change-Id: I1fd03f65f74fb613f021e0121e4d3f7a8e673ad5

--- modules/scap/manifests/source.pp
+++ modules/scap/manifests/source.pp
@@ -45,6 +45,15 @@
 #   Base path for deployments.
 #   Default: /srv/deployment
 #
+# [*lvs_service*]
+#   Name of the lvs service associated with this deployment, if any
+#
+# [*canaries*]
+#   If this source has canary deployments, list those here
+#
+# [*hosts*]
+#   If the software must be deployed to hosts not in a pool, add them here
+#
 # == Usage
 #
 #   # Clones the 'repo/without/external/scap' repsitory into
@@ -81,13 +90,51 @@ define scap::source(
     # how to bootstrap itself properly without trebuchet.
     $owner                = 'trebuchet',
     $group                = 'wikidev',
-    $base_path            = '/srv/deployment'
-) {
+    $base_path            = '/srv/deployment',
+    $canaries             = undef,
+    $lvs_service          = undef,
+    $hosts                = undef,
+    ) {
+
+    # Checkout and prepare the scap repositories
     scap_source { $title:
         repository      => $repository,
         scap_repository => $scap_repository,
         owner           => $owner,
         group           => $group,
         base_path       => $base_path,
+    }
+
+    # Scap dsh lists.
+    #
+    # Each scap installation in production should be tied to a dsh group
+    # defined via puppet.
+    #
+    # If you have a manual list of hosts, they should go in hiera under
+    # "scap::dsh::${dsh_groupname}".
+    #
+    $dsh_groupname = regsubst($title, '/', '-', 'G')
+
+    # If this deployment is linked to an lvs service, let's find out which conftool
+    # cluster / service it's referring to.
+    if $lvs_service {
+        include ::lvs::configuration
+        $service = $::lvs::configuration::lvs_config[$lvs_service]
+        $conftool = merge($service['conftool'], {'datacenters' => $service['sites']})
+    } else {
+        $conftool = undef
+    }
+
+    if ($conftool or $hosts){
+        ::scap::dsh::group { $dsh_groupname:
+            conftool => $conftool,
+            hosts    => $hosts,
+        }
+    }
+
+    if ($canaries) {
+        ::scap::dsh::group { "${dsh_groupname}_canaries":
+            hosts => $canaries,
+        }
     }
 }
