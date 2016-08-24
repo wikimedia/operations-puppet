3e3504c8c0b3120894794f4542a8e8ad109b775b
Jcrespo <jcrespo@wikimedia.org>
2016-08-24 11:34:50 +0200

    Configuration changes regarding firewall and mysql for prometheus
    * Limit the firewall to expose the port to the monitoring VMs
    * Add processlist monitoring
    * Add a very limited slow query monitoring (only 10 entries)
    * MySQL grant changes to enable this monitoring
    
    Bug: T126757
    Change-Id: I0d1feabd2f515ada346c99a7effb9ae94e05e995

--- modules/role/manifests/prometheus/mysqld_exporter.pp
+++ modules/role/manifests/prometheus/mysqld_exporter.pp
@@ -3,6 +3,21 @@ class role::prometheus::mysqld_exporter {
 
     prometheus::mysqld_exporter { 'default':
         client_password => $passwords::prometheus::db_pass,
+        # TODO: collect also TokuDB metrics, but only from
+        # selected nodes
+        # TODO: collect table stats, but less frequently,
+        # and avoid s3/dbstore/labsdb hosts
+        arguments       => @(END)
+        -collect.global_status
+        -collect.global_variables
+        -collect.info_schema.processlist
+        -collect.info_schema.processlist.min_time 0
+        -collect.slave_status
+        -collect.perf_schema.eventsstatements
+        -collect.perf_schema.eventsstatements.digest_text_limit 255
+        -collect.perf_schema.eventsstatements.limit 10
+        -collect.perf_schema.eventsstatements.timelimit 3600
+        | END
     }
 
     $prometheus_nodes = hiera('prometheus_nodes')
