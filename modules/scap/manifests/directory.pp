111593aff50d91f4eccabcac0480d9e3e38b5d71
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-08-24 12:58:27 +0200

    scap::source: use puppet to manage directory creation
    We use the puppet file resource instead of an ugly mkdir -p exec
    
    Change-Id: I16c4dc00003f63a01cdc16b45cb0dd6846173e50

---
+++ modules/scap/manifests/directory.pp
@@ -0,0 +1,9 @@
+# Helper define to be used only in scap::source
+# Creates a directory with default params, only if it wasn't created before
+define scap::directory($params) {
+    if !defined_with_params(File[$title], $params) {
+        $myparams = merge({ensure => directory}, $params)
+        $fullargs = { "${title}" => $myparams }
+        create_resource(file, $fullargs)
+    }
+}
