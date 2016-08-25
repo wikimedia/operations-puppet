89484e97021d0b0761416d70b449486711fc948b
Bryan Davis <bd808@wikimedia.org>
2016-08-24 19:04:26 -0600

    striker: Replace nginx with apache
    Replace the nginx reverse proxy with an equivalent apache vhost.
    
    Note: on any host where the prior striker::nginx class was applied,
    nginx will need to be cleaned up manually:
    
      $ sudo service nginx stop
      $ sudo apt-get purge nginx-light nginx-common
      $ sudo rm -rf /etc/nginx /var/log/nginx
    
    Bug: T136256
    Change-Id: I358722da216a230586d182c6ad707704d0b61b2e

---
+++ modules/striker/manifests/apache.pp
@@ -0,0 +1,33 @@
+# = Class: striker::apache
+#
+# Serve static assets for Striker and route other requests to the Django
+# backend.
+#
+# == Parameters:
+# [*server_name*]
+#   vhost name
+# [*docroot*]
+#   Document root for vhost
+# [*servers*]
+#   List of URLs of backend servers to forward non-static asset requests to
+# [*port*]
+#   Port for apache to listen on. Default 80.
+#
+class striker::apache(
+    $server_name,
+    $docroot,
+    $servers,
+    $port = 80,
+){
+    include ::apache
+    include ::apache::mod::expires
+    include ::apache::mod::headers
+    include ::apache::mod::proxy
+    include ::apache::mod::proxy_balancer
+    include ::apache::mod::proxy_http
+
+    apache::site { 'striker':
+        content => template('striker/apache.conf.erb'),
+    }
+}
+# vim:sw=4:ts=4:sts=4:ft=puppet:
