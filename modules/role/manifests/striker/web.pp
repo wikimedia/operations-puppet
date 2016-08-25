0306c554e5e15d3571e213cd5ad2827a6b7aae6c
Bryan Davis <bd808@wikimedia.org>
2016-08-24 19:04:26 -0600

    striker: Replace nginx with apache
    Replace the nginx reverse proxy with an equivalent apache vhost.
    
    Note: on any host where the prior striker::nginx class was applied,
    nginx will need to be cleaned up manually:
    
      $ sudo service nginx stop
      $ sudo apt-get purge nginx-light nginx-common
      $ sudo rm -rf /etc/nginx /var/log/nginx
    
    Bug: T136256
    Change-Id: I358722da216a230586d182c6ad707704d0b61b2e

--- modules/role/manifests/striker/web.pp
+++ modules/role/manifests/striker/web.pp
@@ -5,7 +5,7 @@
 #
 class role::striker::web {
     include ::memcached
-    include ::striker::nginx
+    include ::striker::apache
     include ::striker::uwsgi
 }
 # vim:sw=4:ts=4:sts=4:ft=puppet:
---
+++ modules/striker/manifests/apache.pp
@@ -0,0 +1,30 @@
+# = Class: striker::apache
+#
+# Serve static assets for Striker and route other requests to the Django
+# backend.
+#
+# == Parameters:
+# [*server_name*]
+#   vhost name
+# [*docroot*]
+#   Document root for vhost
+# [*backend*]
+#   URL for backend server to forward non-static asset requests to
+# [*port*]
+#   Port for apache to listen on. Default 80.
+#
+class striker::apache(
+    $server_name,
+    $docroot,
+    $backend,
+    $port = 80,
+){
+    include ::apache
+    include ::apache::mod::rewrite
+    include ::apache::mod::headers
+
+    apache::site { 'striker':
+        content => template('striker/apache.conf.erb'),
+    }
+}
+# vim:sw=4:ts=4:sts=4:ft=puppet:
