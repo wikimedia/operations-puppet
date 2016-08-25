e347c4248f10c1de2af3133b5a9038094b8ab4f1
andrewbogott <abogott@wikimedia.org>
2016-08-25 05:54:17 -0500

    Set up a root password for Labs instances
    This is only done if the current puppetmaster == the official
    labs puppet master defined in Hiera to avoid hitting up every
    random puppet::self master for a root password.
    
    Bug: T142531
    
    Change-Id: I45c2a2e4b87aebc650f7bf5c4926e6b2cf24625c

---
+++ modules/base/lib/facter/puppetmastername.rb
@@ -0,0 +1,12 @@
+# puppetmastername.rb
+#
+# Answers the question "what puppetmaster is set in puppet.conf?"
+
+require 'facter'
+
+Facter.add(:puppetmastername) do
+  setcode do
+    serverline = Facter::Util::Resolution.exec('grep -e "^server " /etc/puppet/puppet.conf').chomp
+    serverline.sub!(/server\s*=\s*/, "") 
+  end
+end
