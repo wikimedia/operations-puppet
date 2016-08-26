827249b7566b798bc975c77cf651d1a54cb7b9f7
David RACODON <david.racodon@gmail.com>
2016-08-26 11:05:03 +0200

    WIP - Install SonarQube server
    Change-Id: I46592ccd2a09c15ffcfad98ec58bb45077c6ef47

---
+++ modules/sonarqube/manifest/init.pp
@@ -0,0 +1,12 @@
+class sonarqube {
+
+  package { 'openjdk-8-jdk-headless':
+    ensure => present,
+  }
+
+  package { 'sonar':
+    ensure => present,
+    require => Package['openjdk-8-jdk-headless'],
+  }
+
+}
