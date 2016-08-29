070246d9b9782b8f706633f3de2619f9226aed91
Antoine Musso <hashar@free.fr>
2016-08-29 23:20:02 +0200

    (WIP) Crazy rspec for the role module (WIP)
    They should compile.
    
    290 examples, 132 failures
    
    Change-Id: Ibcb7fe8d5c86b7ab1e99e0b5633d89bce3b21cfb

---
+++ modules/role/spec/unit/they_should_compile_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+require 'pathname'
+
+manifests_dir = Pathname.new(File.expand_path(
+        File.dirname(__FILE__) + '/../../manifests'))
+
+# We default to 'production' realm
+labs_only_roles = [
+    'role::ci::castor::server',
+    'role::ci::jenkins_access',
+    'role::ci::slave::browsertests',
+    'role::ci::slave::labs',
+    'role::ci::slave::localbrowser',
+    'role::phragile::labs',
+    'role::toollabs::clush::master',
+]
+
+Dir.glob(File.join(manifests_dir, '**/*.pp')).sort.each do |role_file|
+    relative = Pathname.new(role_file).relative_path_from(manifests_dir)
+    role_name = 'role::' + relative.to_s.gsub(/(\/|\.pp$)/, {'/' => '::', '\.pp' => ''})
+
+    describe role_name, :type => :class do
+        facts = {
+            # Path for hiera lookup
+            :wmf_hiera_role_dir => role_name.gsub(/::/, '/'),
+
+            :initsystem => 'systemd',
+            :interfaces => 'eth0,lo',
+            :ipaddress => '127.0.0.1',
+            :ipv4 => '127.0.0.1',
+            :lsbdistcodename => 'jessie',
+            :lsbdistid => 'Debian',
+            :lsbdistrelease => '8.5',
+            :mail_smarthost => ['smtp1.example.org'],
+            :memorysize_mb => 1024,
+            :memoryfree => '512 MB',
+            :nameservers => ['127.0.0.1'],
+            :operatingsystem => 'Debian',
+            :processorcount => 1,
+            :realm => 'production',
+            :site => 'eqiad',
+            :uniqueid => '00000000',
+        }
+
+        before(:each) {
+            Puppet::Parser::Functions.newfunction(:secret, :type => :rvalue) { |args|
+                return ''
+            }
+            Puppet::Parser::Functions.newfunction(:ipresolve, :type => :rvalue) { |args|
+                return ''
+            }
+            Puppet::Parser::Functions.newfunction(:generate, :type => :rvalue) { |args|
+                return ''
+            }
+            Puppet::Parser::Functions.newfunction(:slice_network_constants, :type => :rvalue) { |args|
+                return ''
+            }
+            Puppet::Parser::Functions.newfunction(:kafka_config, :type => :rvalue) { |args|
+                return {
+                    'name' => 'fake_kafka_cluster',
+                    'brokers' => {
+                        'string' => '',
+                        'array' => [],
+                    },
+                    'zookeeper' => {
+                        'url' => '',
+                        'hosts' => [],
+                    }
+                }
+            }
+        }
+        if labs_only_roles.include?(role_name)
+            describe "On labs realm" do
+                let(:facts) do
+                    facts.merge({  :realm => 'labs' })
+                end
+                it { is_expected.to compile }
+            end
+        else
+            let(:facts) { facts }
+            it { is_expected.to compile }
+        end
+    end
+end
