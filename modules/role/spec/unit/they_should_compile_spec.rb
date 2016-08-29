54d072ffe5cc1352d75d05f0d89751a57dd44196
Antoine Musso <hashar@free.fr>
2016-08-29 23:20:02 +0200

    (WIP) Crazy rspec for the role module (WIP)
    They should compile.
    
    Change-Id: Ibcb7fe8d5c86b7ab1e99e0b5633d89bce3b21cfb

---
+++ modules/role/spec/unit/they_should_compile_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'pathname'
+
+manifests_dir = Pathname.new(
+    File.expand_path(
+        File.dirname(__FILE__) + '/../../manifests'))
+
+
+Dir.glob(File.join(manifests_dir, '**/*.pp')).sort.each do |role_file|
+    relative = Pathname.new(role_file).relative_path_from(manifests_dir)
+    role_name = 'role::' + relative.to_s.gsub(/(\/|\.pp$)/, {'/' => '::', '\.pp' => ''})
+
+    describe role_name, :type => :class do
+        let(:facts) { {
+            # Path for hiera lookup
+            :wmf_hiera_role_dir => role_name.gsub(/::/, '/'),
+            :uniqueid => '00000000',
+            :realm => 'production',
+            :ipaddress => '127.0.0.1',
+            :ipv4 => '127.0.0.1',
+            :initsystem => 'systemd',
+            :nameservers => ['127.0.0.1'],
+            :operatingsystem => 'Debian',
+            :lsbdistrelease => '8.5',
+            :lsbdistid => 'Debian',
+            :processorcount => 1,
+        } }
+
+        generate = {}
+        before(:each) {
+            Puppet::Parser::Functions.newfunction(:secret, :type => :rvalue) { |args|
+                return ''
+            }
+            Puppet::Parser::Functions.newfunction(:generate) { |args|
+                generate.call(args)
+            }
+            generate.stubs(:call).returns('')
+        }
+        it { is_expected.to compile }
+    end
+end
