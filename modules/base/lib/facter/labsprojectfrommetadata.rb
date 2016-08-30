9fb1da6b07a4cff72bb41c38c3bb4ac051264c07
Alex Monk <alex@wikimedia.org>
2016-08-31 00:49:37 +0100

    labsprojectfrommetadata: use jq instead of trying to parse JSON with regex
    Change-Id: I0dbaf6d9304dfce1542bc17a96dccd20df3fe33a

--- modules/base/lib/facter/labsprojectfrommetadata.rb
+++ modules/base/lib/facter/labsprojectfrommetadata.rb
@@ -9,8 +9,8 @@ Facter.add(:labsprojectfrommetadata) do
     domain = Facter::Util::Resolution.exec("hostname -d").chomp
     if (domain.end_with? ".wmflabs") || (domain.end_with? ".labtest")
       # query the nova metadata service at 169.254.169.254
-      metadata = Facter::Util::Resolution.exec("curl -f http://169.254.169.254/openstack/2015-10-15/meta_data.json/ 2> /dev/null").chomp
-      metadata[/project_id\":\ \"(.*)\",/, 1]
+      metadata = Facter::Util::Resolution.exec("curl -f http://169.254.169.254/openstack/2015-10-15/meta_data.json/ 2> /dev/null | jq '.project_id'").chomp
+      metadata.slice(1, metadata.length - 2) # strip quotes
     else
       nil
     end
