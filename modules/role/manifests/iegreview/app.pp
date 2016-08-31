d5c192900a753936d50dd85532449084d30aa7db
Bryan Davis <bd808@wikimedia.org>
2016-08-31 10:10:59 -0600

    iegreview: Switch from Parsoid to RESTBase
    Change the wikitext to html endpoint used from Parsoid to RESTBase.
    
    Bug: T114186
    Change-Id: I2101dcd910aa9a4c52ff9322120236dd77d8b0c8

--- modules/role/manifests/iegreview/app.pp
+++ modules/role/manifests/iegreview/app.pp
@@ -7,16 +7,15 @@ class role::iegreview::app {
     include base::firewall
 
     class { '::iegreview':
-        hostname    => 'iegreview.wikimedia.org',
-        deploy_dir  => '/srv/deployment/iegreview/iegreview',
-        cache_dir   => '/var/cache/iegreview',
+        hostname   => 'iegreview.wikimedia.org',
+        deploy_dir => '/srv/deployment/iegreview/iegreview',
+        cache_dir  => '/var/cache/iegreview',
         # Send logs to udp2log relay
-        log_dest    => 'udp://udplog.eqiad.wmnet:8420/iegreview',
+        log_dest   => 'udp://udplog.eqiad.wmnet:8420/iegreview',
         # Misc MySQL shard
-        mysql_host  => 'm2-master.eqiad.wmnet',
-        mysql_db    => 'iegreview',
-        smtp_host   => $::mail_smarthost[0],
-        parsoid_url => "http://parsoid.svc.${::parsoid_site}.wmnet:8000/enwiki/",
+        mysql_host => 'm2-master.eqiad.wmnet',
+        mysql_db   => 'iegreview',
+        smtp_host  => $::mail_smarthost[0],
     }
 
     ferm::service { 'iegreview_http':
