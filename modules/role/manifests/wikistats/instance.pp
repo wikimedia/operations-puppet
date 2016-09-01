90b2d031d3ebcf86af610a026f484bca5be8aa04
Daniel Zahn <dzahn@wikimedia.org>
2016-08-31 18:45:44 -0700

    wikistats: remove $realm check, simplify role
    This will never be production, remove the $realm
    check, simplify it.
    
    Change-Id: I9bc1ca7723c2fec0250daf2346ac4ec2640f7663

--- modules/role/manifests/wikistats/instance.pp
+++ modules/role/manifests/wikistats/instance.pp
@@ -1,25 +1,13 @@
 # wikistats host role class
-# this is labs only! - wikistats.wmflabs.org (dzahn)
+# this is labs-only - wikistats.wmflabs.org (dzahn)
 # NOT stats.wikimedia.org (analytics/ezachte)
-# realm case is just here for compatibility
+# these projects are often confused
 class role::wikistats::instance {
 
     system::role { 'wikistats': description => 'wikistats instance' }
 
-    # config - labs vs. production
-    case $::realm {
-        'labs': {
-            $wikistats_host = 'wikistats.wmflabs.org'
-        }
-        'production': {
-            $wikistats_host = 'wikistats.wikimedia.org'
-        }
-        default: {
-            fail('unknown realm, should be labs or production')
-        }
-    }
+    $wikistats_host = 'wikistats.wmflabs.org'
 
-    # main, ::wikistats refers to the module class in init.pp
     class { '::wikistats':
         wikistats_host => $wikistats_host,
     }
