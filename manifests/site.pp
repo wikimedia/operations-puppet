c70d745232c52ed1572f7dc605f13d1a9425774b
Dzahn <dzahn@wikimedia.org>
2016-09-01 01:42:52 +0000

    Revert "archiva: migration class to rsync data to new host"
    This reverts commit 582caa3e4aa7eded13df604eea58c93f2d029c42.
    
    This is a reminder to me that i should revert this and also manually cleanup the rsyncd and config on the new host,
    after we have shutdown the old host.
    
    Bug: T123725
    Change-Id: I90e3b04c4f1baaa6c6d3d980f00f8cd401c701df

--- manifests/site.pp
+++ manifests/site.pp
@@ -1942,7 +1942,7 @@ node /^mc20[01][0-9]\.codfw\.wmnet/ {
 
 node 'meitnerium.wikimedia.org' {
     $cluster = 'misc'
-    role archiva, archiva::migration
+    role archiva
     include standard
 }
 
