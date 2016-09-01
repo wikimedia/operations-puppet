06d6a8072f9137d2a236807c3ba9cc9a5bf16648
Marko Obrovac <mobrovac@wikimedia.org>
2016-09-01 21:28:23 +0100

    service::node: Compile the file holding puppet-controlled vars
    Enabling configuration deploys via Scap3 implies that the configuration
    file will be controlled by deployers (by having it in the deploy
    repository). However, certain variable values are set in ops/puppet
    (such as host/port settings for various common parts of the
    infrastructure). To address this issue, Scap3 allows one to specify the
    file name of a file residing on the target node which contains variables
    which are to be used in the final version of the configuration file.
    This commit creates this file as `/etc/${title}/config-vars.yaml`.
    
    Also, in order to activate it, this commit switches Mathoid (it having
    the simplest of all configs) to use the feature.
    
    Bug: T144542
    Change-Id: I4bb8de3085c995574ccfe2d1875a65493dd5e172

--- modules/mathoid/manifests/init.pp
+++ modules/mathoid/manifests/init.pp
@@ -4,45 +4,15 @@
 # converts it to MathML + SVG output. It is a web-service implemented
 # in node.js.
 #
-# === Parameters
-#
-# [*svg_generation*]
-#   Enable SVG generation. Default: true
-# [*img_generation*]
-#   Enable IMG generation. Default: true
-# [*png_generation*]
-#   Enable PNG generation, accomplished via a librsvg2 node binding. Default:
-#   true
-# [*speakText_generation*]
-#   Enable speakText generation. Default: false
-# [*textvcinfo_generation*]
-#   Enable texvcinfo generation. Default: true
-# [*render_no_check*]
-#   Whether not to perform input checks on renders. Default: true
-#
-class mathoid(
-    $svg_generation=true,
-    $img_generation=true,
-    $png_generation=true,
-    $speakText_generation=false,
-    $texvcinfo_generation=true,
-    $render_no_check=true,
-) {
+class mathoid {
 
     require ::mathoid::packages
 
     service::node { 'mathoid':
-        port            => 10042,
-        config          => {
-            svg       => $svg_generation,
-            img       => $img_generation,
-            png       => $png_generation,
-            speech_on => $speakText_generation,
-            texvcinfo => $texvcinfo_generation,
-            no_check  => $render_no_check,
-        },
-        healthcheck_url => '',
-        has_spec        => true,
-        deployment      => 'scap3',
+        port              => 10042,
+        healthcheck_url   => '',
+        has_spec          => true,
+        deployment        => 'scap3',
+        deployment_config => true,
     }
 }
