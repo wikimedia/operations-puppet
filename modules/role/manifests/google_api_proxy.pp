b0c3de5d5290eeb4c6a9a0dc5faee7281082351f
Bryan Davis <bd808@wikimedia.org>
2016-09-01 18:38:05 -0600

    google_api_proxy: Add role for Google-api-proxy Labs project
    Add a role that can be used to provision one or more nginx reverse
    proxies in the Google-api-proxy Labs project. This project provides
    static outbound IP addresses for Tool Labs tools and other Labs projects
    that are using Google APIs.
    
    Expected to be used with wikitech managed hiera configuration similar to:
    
      role::google_api_proxy::instances:
        google-api-proxy.wmflabs.org:
          proxied: https://www.googleapis.com
          default: true
        googlevision-api-proxy.wmflabs.org:
          proxied: https://vision.googleapis.com
    
    Bug: T144290
    Change-Id: I2fe92633d716b0bfb4f9b9928cd7bbc7d3977be0

---
+++ modules/role/manifests/google_api_proxy.pp
@@ -0,0 +1,40 @@
+# == Class: role::google_api_proxy
+#
+# Provision nginx reverse proxy vhosts for accessing Google API endpoints from
+# inside Labs via a fixed IP address.
+#
+# == Parameters:
+# [*instances*]
+#   A hash mapping vhost names to external_proxy::instance parameters.
+#
+# == Usage
+# class { 'role::google_api_proxy'
+#     $instances => {
+#         'google-api-proxy.wmflabs.org' => {
+#             'proxied' => 'https://www.googleapis.com',
+#             'default' => true,
+#         },
+#         'googlevision-api-proxy.wmflabs.org' => {
+#             'proxied' => 'https://vision.googleapis.com',
+#         },
+#     },
+# }
+#
+# filtertags: labs-project-google-api-proxy
+class role::google_api_proxy (
+    $instances,
+) {
+    create_resources(
+        '::external_proxy::instance',
+        $instances,
+        {
+            'acls': [
+                'deny 10.68.21.68; # IP we see if XFF unwrapping didn't work',
+                'allow 10.68.16.0/21; # All of Labs',
+                'allow 127.0.0.1;',
+                'deny all;',
+            ],
+        }
+    )
+}
+# vim:sw=4:ts=4:sts=4:ft=puppet:
