4237761ee19a3fec27e133fb82dc1cd6b236b8fd
Paladox <thomasmulhall410@yahoo.com>
2016-09-03 15:59:29 +0000

    nagios_common: Fix optional parameter listed before required parameter
    Bug: T93645
    Change-Id: I2b02c493f303ff2000e609635be660ce1e5257bf

--- modules/nagios_common/manifests/contactgroups.pp
+++ modules/nagios_common/manifests/contactgroups.pp
@@ -2,6 +2,10 @@
 #
 # Sets up appropriate contacts for notifications
 #
+# [*source*]
+#   Allows to input a prewritten file as a source.  Overrides "content" if
+#   defined, but "content" is used if this is undefined.
+#
 # [*ensure*]
 #   present or absent, to make the definition
 #   present or absent. Defaults to present
@@ -21,16 +25,13 @@
 # [*contacts*]
 #   The list of contacts to include in the configuration.
 #
-# [*source*]
-#   Allows to input a prewritten file as a source.  Overrides "content" if
-#   defined, but "content" is used if this is undefined.
 # [*content*]
 #   Allows to input the data as a content string.  The default is
 #   template('nagios_common/contacts.cfg.erb')
 #
 class nagios_common::contactgroups(
-    $ensure = present,
     $source,
+    $ensure = present,
     $config_dir = '/etc/icinga',
     $owner = 'icinga',
     $group = 'icinga',
