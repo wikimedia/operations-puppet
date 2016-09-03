195f747948010ba4520a786d7ac00f1b085a1bfc
Paladox <thomasmulhall410@yahoo.com>
2016-09-03 16:08:10 +0000

    openstack: Fix indentation of =>
    Bug: T93645
    Change-Id: Iacb704ea2cc4309c3bfdcfdad32b19f5667e7590

--- modules/openstack/manifests/keystone/service.pp
+++ modules/openstack/manifests/keystone/service.pp
@@ -68,8 +68,8 @@ class openstack::keystone::service($keystoneconfig, $openstack_version=$::openst
         }
     } else {
         service { 'keystone':
-            ensure    => stopped,
-            require   => Package['keystone'];
+            ensure  => stopped,
+            require => Package['keystone'];
         }
     }
 }
