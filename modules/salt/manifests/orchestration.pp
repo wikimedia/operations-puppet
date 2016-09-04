0ba664b8ae3dbbb2ea33e6ec7d30f1990b793298
Riccardo Coccioli <rcoccioli@wikimedia.org>
2016-09-05 01:15:14 +0200

    Automation: automatically reimage host
    - add a Python script to be run from the Salt master that automate a
      series of steps before and after calling the wmf_reimage script to
      allow to reimage a WMF server almost entirely automatically.
    - this is a first "quick and dirty" approach to try to speedup the
      current process in T143536.
    
    Bug: T143536
    Change-Id: I6fd0ed6a4e764e64d688e952072b849646bda8e0

---
+++ modules/salt/manifests/orchestration.pp
@@ -0,0 +1,23 @@
+class salt::orchestration() {
+    package { [
+        'python-dnspython',
+        'python-phabricator',
+    ]:
+        ensure => present;
+    }
+
+    file { '/usr/local/bin/wmf-auto-reimage':
+        ensure => present,
+        source => 'puppet:///modules/salt/wmf-auto-reimage.py',
+        mode   => '0544',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    class { '::phabricator::bot':
+        username => 'ops-monitoring-bot',
+        token    => $passwords::phabricator::ops_monitoring_bot_token,
+        owner    => 'root',
+        group    => 'root',
+    }
+}
