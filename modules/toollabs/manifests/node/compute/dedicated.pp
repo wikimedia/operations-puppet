4d222d2305c2a951a057e574daceb6e929a30c53
andrewbogott <abogott@wikimedia.org>
2016-09-06 03:03:03 -0500

    toollabs:  Allow $::node_dedicated_tool to be set from hiera
    Bug: T101447
    Change-Id: I61133a9c469dc98740bc81e1a866bf72cf1bad43

--- modules/toollabs/manifests/node/compute/dedicated.pp
+++ modules/toollabs/manifests/node/compute/dedicated.pp
@@ -10,22 +10,22 @@
 #
 # Sample Usage:
 #
-class toollabs::node::compute::dedicated {
+class toollabs::node::compute::dedicated($dedicated_tool = $::node_dedicated_tool) {
 
     include toollabs::node::all
 
-    if $::node_dedicated_tool {
+    if $dedicated_tool {
 
         system::role { 'toollabs::node::compute::dedicated':
-            description => "Computation node dedicated to ${::labsproject}.${::node_dedicated_tool}",
+            description => "Computation node dedicated to ${::labsproject}.${dedicated_tool}",
         }
 
-        class { 'toollabs::queues': queues => [ $::node_dedicated_tool ] }
+        class { 'toollabs::queues': queues => [ $dedicated_tool ] }
 
         # Having the collector in the instance means that, for the time
         # being, dedicated queues can have just the one one in them.
 
-        gridengine::collectors::queues { $::node_dedicated_tool:
+        gridengine::collectors::queues { $dedicated_tool:
             store   => "${toollabs::collectors}/queues",
             config  => 'toollabs/gridengine/queue-dedicated.erb',
             require => Class['toollabs::queues'],
