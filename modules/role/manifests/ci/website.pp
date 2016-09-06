069ce10aec4d2a6cd9a8021b3d12665d4161b2fe
Antoine Musso <hashar@free.fr>
2016-09-06 19:27:46 +0200

    zuul: refactor to use hiera
    The manifests have been written pre hiera.  Migrate to it.
    
    Drop role::zuul::hiera::configuration
    
    Change-Id: Ibe39ccc3a482b3aec7d0e8624324be1c809f4cbd

--- modules/role/manifests/ci/website.pp
+++ modules/role/manifests/ci/website.pp
@@ -11,9 +11,5 @@ class role::ci::website {
     # Needed at least for the jenkins-slave user
     require ::role::ci::slave
 
-    include role::zuul::configuration
-
-    class { 'contint::website':
-        zuul_git_dir => $role::zuul::configuration::zuul_git_dir,
-    }
+    include contint::website
 }
