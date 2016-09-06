05eeb83e56e99b1cf051e32ec7700b1d3a6aeec9
Alex Monk <alex@wikimedia.org>
2016-09-06 16:15:52 +0100

    POC: Secure redirect service
    This commit includes:
    
    * Some config for testing on a specific labs instance
    * Initial empty config for production
    * A script to convert the existing redirects.dat file to the new config structure
    * A ruby function that chunks the list of configured domains for use with Let's Encrypt
    * Nginx site config and new puppet role
    
    Copies some nginx config from the tlsproxy module.
    
    Bug: T133548
    Change-Id: If9199c914716fb165e7d6249a1fa5479a25d5859

---
+++ dump_secureredir_config.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby
+domains = {}
+File.read('modules/mediawiki/files/apache/sites/redirects/redirects.dat').each_line do |line|
+  if line[0] == '#' || line.strip == '' then
+    next
+  end
+  parts = line.strip.split("\t")
+  type = parts[0]
+  domain = parts[1]
+  if domain.include? "*" then
+    next # TODO: Figure out these cases
+  end
+
+  target = parts[2]
+  if target[0..1] == '//' then
+    target = 'https:' + target
+  end
+
+  if type == 'override'
+    urlparts = domain.split("/")
+    domain = urlparts[0]
+    path = "/" + urlparts[1..urlparts.length - 1].join("/")
+  elsif type == 'rewrite'
+    path = nil
+    target += "/$1"
+  elsif type == 'funnel' then
+    path = nil
+  end
+
+  if !domains.include? domain then
+    domains[domain] = {}
+  end
+  domains[domain][path] = target
+end
+end
+
+require 'yaml'
+puts YAML.dump(domains)
