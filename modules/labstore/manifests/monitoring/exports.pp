7b194d5f496369c7d0f513d2203ab77bf6a8cd8f
cpettet <rush@wikimedia.org>
2016-09-07 11:12:14 -0500

    labstore: fix manifest declaraton path for labstore::monitoring::exports
    Change-Id: I18d61045ff75f3286cd41779547eadfa885f8836

--- modules/labstore/manifests/monitoring/exports.pp
+++ modules/labstore/manifests/monitoring/exports.pp
@@ -1,4 +1,4 @@
-class labstore::monitoring::nfsd {
+class labstore::monitoring::exports {
 
     nrpe::monitor_systemd_unit_state { 'nfs-exportd':
         description => 'Ensure NFS exports are maintained for new instances with NFS',
