762772102b7f4d0c055e7f8761d1f96011913a1f
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-09-07 11:48:46 +0200

    scap: introduce scap_source type
    As scap::source is a mess of nested execs, which is hard to change
    without changing a few fundamental classes like git::clone, move to
    using a custom puppet type, which should also allow us to define
    multiple providers for various sources and add new providers for
    different sources if we need to.
    
    Advantages at the moment compared to scap::source:
    
    - It's faster
    - It cleanly checks if the git repo is present
    - It's absentable
    - It can be easily extended and could, in the future, also check that
     the origin url is correct and change it in case of need.
    
    Change-Id: If8ed793759ebce183ef7ec8f36decfd287438d4d

---
+++ modules/scap/lib/puppet/type/scap_source.rb
@@ -0,0 +1,148 @@
+# == resource scap_source
+#
+# Sets up scap3 deployment source on a deploy server.
+# This will clone $repository at /srv/deployment/$title.
+# If $scap_repository is set it will clone it at
+# /srv/deployment/$title/scap.  If you set $scap_repository to true,
+# this will assume that your scap repository is named $title/scap.
+#
+# To use this in conjunction with scap::target, make sure the $title here
+# matches a scap::target's $title on your target hosts, or at least matches
+# the $package_name provided to scap::target (which defaults to $title).
+#
+# NOTE: This define is compatible with trebuchet's deployment.yaml file.
+# If trebuchet has already cloned a source repository in /srv/deployment,
+# this clone will do nothing, as it only executes if .git/config
+# doesn't already exist.
+#
+# == Parameters
+#
+# [*repository*]
+#   Repository name in gerrit.  Default: $title
+#
+# [*scap_repository*]
+#   String or boolean.
+#
+#   If you set this to a string, it will be assumed to be a repository name
+#   This scap repository will then be cloned into /srv/deployment/$title/scap.
+#   If this is set to true your scap_repository will be assumed to
+#   live at $title/scap in gerrit.
+#
+#   You can use this keep your scap configs separate from your source
+#   repositories.
+#
+#   Default: false.
+#
+# [*owner*]
+#   Owner of cloned repository,
+#   Default: trebuchet
+#
+# [*group*]
+#   Group owner of cloned repository.
+#   Default: wikidev
+#
+# == Usage
+#
+#   # Clones the 'repo/without/external/scap' repsitory into
+#   # /srv/deployment/repo/without/external/scap.
+#
+#   scap_source { 'repo/without/external/scap': }
+#
+#
+#   # Clones the 'eventlogging' repository into
+#   # /srv/deployment/eventlogging/eventbus and
+#   # clones the 'eventlogging/eventbus/scap' repository
+#   # into /srv/deployment/eventlogging/eventbus/scap
+#
+#   scap_source { 'eventlogging/eventbus':
+#       repository         => 'eventlogging',
+#       scap_repository    => true,
+#   }
+#
+#
+#   # Clones the 'myproject/myrepo' repository into
+#   # /srv/deployment/myproject/myrepo, and
+#   # clones the custom scap repository at
+#   # 'my/custom/scap/repo' from gerrit into
+#   # /srv/deployment/myproject/myrepo/scap
+#
+#   scap_source { 'myproject/myrepo':
+#       scap_repository    => 'my/custom/scap/repo',
+#   }
+#
+Puppet::Type.newtype(:scap_source) do
+  @doc = "Puppet type to set up scap repositories on the scap master"
+
+  ensurable do
+    desc <<-EOT
+If the repository must be set up or not
+EOT
+    newvalue(:present, :event => :scap_source_created) do
+      provider.create
+    end
+
+    newvalue(:absent, :event => :scap_source_removed) do
+      provider.destroy
+    end
+
+    defaultto do
+      if @resource.managed?
+        :present
+      else
+        nil
+      end
+    end
+  end
+
+  newparam(:name, :namevar => true) do
+    desc "Name of the scap source"
+  end
+
+  newparam(:owner) do
+    desc "Owner of the cloned repository. Defaults to 'trebuchet'"
+
+    defaultto 'trebuchet'
+  end
+
+  newparam(:group) do
+    desc "Group owner of the cloned repository. Defaults to 'wikidev'"
+
+    defaultto 'wikidev'
+  end
+
+  newparam(:repository) do
+    desc <<-EOT
+Repository name in the VCS. Defaults to the resource name
+EOT
+    defaultto do
+      @resource[:name]
+    end
+  end
+
+
+  newparam(:scap_repository) do
+    desc <<-EOT
+String or boolean.
+
+If you set this to a string, it will be assumed to be a repository name
+This scap repository will then be cloned into /srv/deployment/$title/scap.
+If this is set to true your scap_repository will be assumed to
+live at $title/scap in gerrit.
+
+You can use this keep your scap configs separate from your source
+repositories.
+Default: false.
+EOT
+    defaultto false
+    munge do |value|
+      case value
+      when false, :false
+        false
+      when true, :true
+        File.join(@resource[:name], 'scap')
+      else
+        value
+      end
+    end
+  end
+end
