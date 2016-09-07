d4e1c35aa24debccb942a6228178b2ab6a0cc517
Antoine Musso <hashar@free.fr>
2016-09-07 22:44:50 +0200

    contint: vary ssh from= for prod slave
    The Jenkins master adds its host as a slave using the fqdn/ipaddress.
    The ssh public key uses the from=  option to limit access, it was
    hardcoded to gallium IP address.
    
    We are moving to contint1001 208.80.154.17, make the from vary by the
    host ip address. We use main_ipaddress which is provided by realm.pp and
    rely on the ipaddress fact:
    
        contint1001:~$ facter ipaddress
        208.80.154.17
    
        gallium$ facter ipaddress
        208.80.154.135
    
    Bug: T137323
    Change-Id: I917fd47adad661d8e06aba770ac1a7181b039fa1

--- modules/role/manifests/ci/slave.pp
+++ modules/role/manifests/ci/slave.pp
@@ -1,8 +1,8 @@
 # Set up a Jenkins slave on PRODUCTION
 #
-# Most jobs are running on labs instances, but we still have a few runnining on
-# gallium for example to publish content under doc.wikimedia.org or
-# integration.wikimedia.org
+# Most jobs are running on labs instances, but we still have a few runnining
+# directly on the Jenkins master for example to publish publish content under
+# https://doc.wikimedia.org/
 #
 # You should look instaed at role::ci::labs::slave
 #
@@ -16,7 +16,8 @@ class role::ci::slave {
     require contint::master_dir
 
     class { 'jenkins::slave':
-        ssh_key => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw== jenkins@gallium from="208.80.154.135"',
+        # Master connect to itself via the fqdn / primary IP ipaddress
+        ssh_key => "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw== jenkins@gallium from=\"${::main_ipaddress}\"",
         user    => 'jenkins-slave',
         workdir => '/srv/ssd/jenkins-slave',
         require => File['/srv/ssd'],
