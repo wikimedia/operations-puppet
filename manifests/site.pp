5d7acf61608100fea5bff4d13ed91302b3e27a50
Antoine Musso <hashar@free.fr>
2016-09-07 20:57:12 +0200

    sites.pp: rename contint1001 and drop role
    Server has been reimaged and moved to the public network. Remove the
    role classes from it.  We do not want to have puppet magically spawn
    Jenkins and Zuul lacking a proper configuration.
    
    Leave contint::firewall which is base::firewall + custom ferm rules.
    Will let us verify the network flows with other servers work fine.
    
    Bug: T140257
    Change-Id: I477a135993bc4ded6273d03ace9f77da2feca6fb

--- manifests/site.pp
+++ manifests/site.pp
@@ -272,20 +272,9 @@ node /^conf200[123]\.codfw\.wmnet$/ {
 }
 
 # New CI master
-node 'contint1001.eqiad.wmnet' {
-    role(ci::master, zuul::server)
-
-    # T51846, let us sync VisualEditor in mediawiki/extensions.git
-    sudo::user { 'jenkins-slave':
-        privileges => [
-            'ALL = (jenkins) NOPASSWD: /srv/deployment/integration/slave-scripts/bin/gerrit-sync-ve-push.sh',
-        ]
-    }
-
-
+node 'contint1001.wikimedia.org' {
     include standard
     include contint::firewall
-
 }
 
 # Debian package building host in production
