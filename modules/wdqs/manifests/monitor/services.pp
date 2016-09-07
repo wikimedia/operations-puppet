86196b644f83df21a25d8cd18ef38f9c7d27350d
Marius Hoch <hoo@online.de>
2016-09-07 15:48:00 +0200

    Specify contact groups in wdqs::monitor::services
    Change-Id: Iedd617e596c080f339e65c2b496c836835771adb

--- modules/wdqs/manifests/monitor/services.pp
+++ modules/wdqs/manifests/monitor/services.pp
@@ -7,41 +7,48 @@ class wdqs::monitor::services(
 ) {
 
     nrpe::monitor_service { 'WDQS_Internal_HTTP_endpoint':
-        description  => 'WDQS HTTP Port',
-        nrpe_command => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 80',
+        description   => 'WDQS HTTP Port',
+        nrpe_command  => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 80',
+        contact_group => $nagios_contact_group,
     }
 
     nrpe::monitor_service { 'WDQS_Local_Blazegraph_endpoint':
-        description  => 'Blazegraph Port',
-        nrpe_command => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 9999',
+        description   => 'Blazegraph Port',
+        nrpe_command  => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 9999',
+        contact_group => $nagios_contact_group,
     }
 
     monitoring::service { 'WDQS_External_HTTP_Endpoint':
         description   => 'WDQS HTTP',
         check_command => 'check_http!query.wikidata.org!/!Welcome',
+        contact_group => $nagios_contact_group,
     }
 
     monitoring::service { 'WDQS_External_SPARQL_Endpoint':
         description   => 'WDQS SPARQL',
         check_command => 'check_http!query.wikidata.org!/bigdata/namespace/wdq/sparql?query=prefix%20schema:%20%3Chttp://schema.org/%3E%20SELECT%20*%20WHERE%20%7B%3Chttp://www.wikidata.org%3E%20schema:dateModified%20?y%7D&format=json!"xsd:dateTime"',
+        contact_group => $nagios_contact_group,
     }
 
     nrpe::monitor_service { 'WDQS_Blazegraph_process':
-        description  => 'Blazegraph process',
-        nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1:1 -u ${username} --ereg-argument-array '^java .* blazegraph-service-.*-dist.war'",
+        description   => 'Blazegraph process',
+        nrpe_command  => "/usr/lib/nagios/plugins/check_procs -c 1:1 -u ${username} --ereg-argument-array '^java .* blazegraph-service-.*-dist.war'",
+        contact_group => $nagios_contact_group,
     }
 
     nrpe::monitor_service { 'WDQS_Updater_process':
-        description  => 'Updater process',
-        nrpe_command => "/usr/lib/nagios/plugins/check_procs -w 1:1 -u ${username} --ereg-argument-array '^java .* org.wikidata.query.rdf.tool.Update'",
+        description   => 'Updater process',
+        nrpe_command  => "/usr/lib/nagios/plugins/check_procs -w 1:1 -u ${username} --ereg-argument-array '^java .* org.wikidata.query.rdf.tool.Update'",
+        contact_group => $nagios_contact_group,
     }
 
     monitoring::graphite_threshold { 'WDQS_Lag':
-        description => 'High lag',
-        metric      => "wikidata.query.lag.${::hostname}",
-        from        => '30min',
-        warning     => '600', # 10 minutes
-        critical    => '1800', # 30 minutes
-        percentage  => '30', # Don't freak out on spikes
+        description   => 'High lag',
+        metric        => "wikidata.query.lag.${::hostname}",
+        from          => '30min',
+        warning       => '600', # 10 minutes
+        critical      => '1800', # 30 minutes
+        percentage    => '30', # Don't freak out on spikes
+        contact_group => $nagios_contact_group,
     }
 }
