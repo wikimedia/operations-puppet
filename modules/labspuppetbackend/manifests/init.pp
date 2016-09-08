0b256ede0411c02abb9fe51973c1482a20be51a2
andrewbogott <abogott@wikimedia.org>
2016-09-07 19:41:59 -0500

    labspuppetbackend: only allow POST from Horizon's IP
    Change-Id: I181274d6816a70ba3ae55e454998aa701784f514

--- modules/labspuppetbackend/manifests/init.pp
+++ modules/labspuppetbackend/manifests/init.pp
@@ -18,15 +18,16 @@ class labspuppetbackend(
         mode   => '0444',
     }
 
+    $horizon_host_ip = ipresolve(hiera('labs_horizon_host'), 4)
     uwsgi::app { 'labspuppetbackend':
         settings  => {
             uwsgi => {
-                plugins     => 'python3',
-                'wsgi-file' => '/usr/local/lib/python3.4/dist-packages/labspuppetbackend.py',
-                callable    => 'app',
-                master      => true,
-                http-socket => "${::ipaddress}:8100",
-                env         => [
+                plugins             => 'python3',
+                'wsgi-file'         => '/usr/local/lib/python3.4/dist-packages/labspuppetbackend.py',
+                callable            => 'app',
+                master              => true,
+                http-socket         => "${::ipaddress}:8100",
+                env                 => [
                     "MYSQL_HOST=${mysql_host}",
                     "MYSQL_DB=${mysql_db}",
                     "MYSQL_USERNAME=${mysql_username}",
@@ -34,6 +35,16 @@ class labspuppetbackend(
                     "STATSD_HOST=${statsd_host}",
                     "STATSD_PREFIX=${statsd_prefix}",
                 ],
+                # This next rule is actually two rules jammed together -- they have to be
+                #  sequential and Puppet can't be trusted to insert them in the correct order.
+                #
+                # The first rule says "If the request is from the horizon host, anything goes."
+                #
+                # The second rule says "If this is a post, throw a 403"
+                #
+                # The sum effect is to allow POSTs only from horizon.
+                #
+                'route-remote-addr' => "^${horizon_host_ip}\$ continue:\nroute-if=equal:\${REQUEST_METHOD};POST break:403 Forbidden",
             }
         },
         subscribe => File['/usr/local/lib/python3.4/dist-packages/labspuppetbackend.py']
