52dd9af6bcb47881e880fdb1784e13e707bcc4da
elukey <ltoscano@wikimedia.org>
2016-09-08 15:00:23 +0200

    Add the pivot.wikimedia.org VHost to stat1001
    This site will be a simple reverse proxy to the nodejs service serving
    the pivot UI, used to limit the access to authenticated clients (via LDAP).
    
    Context up to September 2016:
    There is a current dispute between Imply and Metamarkets about a possible
    copyright infringement related to Imply's pivot UI.
    The Analytics team set a while back a goal to provide a Pivot UI
    to their users with the assumption that all the code
    used/deployed was open souce and freely available. If this assumption will
    change in the future, for example after a legal sentence, the Analytics team
    will take the necessary actions.
    For any question please reach out to the Analytics team:
    https://www.mediawiki.org/wiki/Analytics#Contact
    
    Bug: T138262
    Change-Id: Iefd2893a1dab5e2da660ba83d42813ad891ee8e2

---
+++ modules/statistics/manifests/sites/pivot.pp
@@ -0,0 +1,41 @@
+# == Class statistics::sites::yarn
+# pivot.wikimedia.org
+#
+# This site will be a simple reverse proxy to the nodejs service serving
+# the pivot UI, used to limit the access to authenticated clients (via LDAP).
+#
+# Context up to September 2016:
+# There is a current dispute between Imply and Metamarkets about a possible
+# copyright infringement related to Imply's pivot UI.
+# The Analytics team set a while back a goal to provide a Pivot UI
+# to their users with the assumption that all the code
+# used/deployed was open souce and freely available. If this assumption will
+# change in the future, for example after a legal sentence, the Analytics team
+# will take the necessary actions.
+# For any question please reach out to the Analytics team:
+# https://www.mediawiki.org/wiki/Analytics#Contact
+#
+# Bug: T138262
+#
+class statistics::sites::pivot {
+    require statistics::web
+
+    include ::apache::mod::proxy_http
+    include ::apache::mod::proxy
+    include ::apache::mod::auth_basic
+    include ::apache::mod::authnz_ldap
+    include ::passwords::ldap::production
+
+    $proxypass = $passwords::ldap::production::proxypass
+
+    # Set up the VirtualHost
+    apache::site { 'pivot.wikimedia.org':
+        content => template('statistics/pivot.wikimedia.org.erb'),
+    }
+
+    ferm::service { 'pivot-http':
+        proto => 'tcp',
+        port  => '80',
+    }
+
+}
