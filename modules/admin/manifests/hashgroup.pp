6adfb684d1bf7de14cc3f8d2173bc2ac516c8665
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-09-08 11:56:11 +0200

    admin: stop passing around a huge data structure
    Our current puppet catalogs are huge becuse for every group and every
    user we have a specialized copy of $::admin::data, which is a very large
    hash indeed. Use direct references to it instead.
    
    Change-Id: I526f659dccabb1726c7dda14b3a50cc6fd5a0828

--- modules/admin/manifests/hashgroup.pp
+++ modules/admin/manifests/hashgroup.pp
@@ -5,21 +5,17 @@
 # [*name*]
 #  Hash group name
 #
-# [*phash*]
-#  Hash that contains valid group data
-
 define admin::hashgroup(
-    $phash={},
 )
 {
 
     #explicit error as otherwise it goes forward later
     #complaining of 'invalid hash' which is hard to track down
-    if !has_key($phash['groups'], $name) {
+    if !has_key($::admin::data['groups'], $name) {
         fail("${name} is not a valid group name")
     }
 
-    $gdata = $phash['groups'][$name]
+    $gdata = $::admin::data['groups'][$name]
     if has_key($gdata, 'posix_name') {
         $group_name = $gdata['posix_name']
     } else {
