1bcdb5f4764758d19b5a935e08b0b5ff1ba06610
Alex Monk <alex@wikimedia.org>
2016-09-09 23:26:12 +0100

    Remove references to the old virt* servers
    Prefix used is labvirt now, these are all gone. Racktables shows all of the
    ones I could find as decommissioned.
    
    Change-Id: I3906d850ef4a59e3a5dbf6004b5c7966d98f39a5

--- modules/ntp/manifests/daemon.pp
+++ modules/ntp/manifests/daemon.pp
@@ -1,4 +1,4 @@
-define ntp::daemon($servers=[], $peers=[], $query_acl=[], $time_acl=[], $servers_opt='',
+define ntp::daemon($servers=[], $peers=[], $query_acl=[], $time_acl=[],
     $ensure=hiera('ntp::daemon::ensure', 'present')) {
     package { 'ntp': ensure => present }
 
