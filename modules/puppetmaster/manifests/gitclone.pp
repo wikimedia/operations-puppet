d13acce24bf1024d16cb83d631c68d8a677575d2
Riccardo Coccioli <rcoccioli@wikimedia.org>
2016-09-12 18:07:08 +0200

    Git: Add username into commit message in private repo
    Change-Id: Iae3a3824299dc9b781d01ae7a115c4484606ce80

--- modules/puppetmaster/manifests/gitclone.pp
+++ modules/puppetmaster/manifests/gitclone.pp
@@ -111,6 +111,16 @@ class puppetmaster::gitclone(
                 require => Exec['/srv/private init'],
             }
 
+            # Audit hook, add username of commiter in message
+            file { '/srv/private/.git/hooks/commit-msg':
+                ensure  => present,
+                content => 'puppet:///modules/puppetmaster/git/private/commit-msg-master',
+                owner   => 'gitpuppet',
+                group   => 'gitpuppet',
+                mode    => '0550',
+                require => Exec['/srv/private init'],
+            }
+
             # Syncing hooks
             # This hook updates /var/lib and pushes changes to the backend workers
             file { '/srv/private/.git/hooks/post-commit':
