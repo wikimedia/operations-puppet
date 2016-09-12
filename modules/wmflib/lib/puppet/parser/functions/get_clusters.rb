754b174ebc95315d9cde69780ecc5fa29483f90c
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-09-12 10:49:05 +0200

    wmflib: fix get_clusters calls to custom functions
    Change-Id: I9f6cfcda6aecfcf628eb855bc9a99e108ffdd5b5

--- modules/wmflib/lib/puppet/parser/functions/get_clusters.rb
+++ modules/wmflib/lib/puppet/parser/functions/get_clusters.rb
@@ -24,7 +24,7 @@ module Puppet::Parser::Functions
   newfunction(:get_clusters, :type => :rvalue) do |args|
     all = {}
     # Ganglia config is the source of truth about clusters/site
-    cluster_config = function_hiera('ganglia_clusters', {})
+    cluster_config = function_hiera(['ganglia_clusters', {}])
 
     # Arguments are an hash of selectors
     selector ||= {}
@@ -41,7 +41,7 @@ module Puppet::Parser::Functions
       sites = false
     end
 
-    function_query_resources(false, '@@Ganglia::Cluster', false).each do |node|
+    function_query_resources([false, '@@Ganglia::Cluster', false]).each do |node|
       cluster = node['parameters']['cluster']
       site = node['parameters']['site']
       fqdn = node['title']
