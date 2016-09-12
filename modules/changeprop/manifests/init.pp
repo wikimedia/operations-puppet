9af6841811b0fc90ae3069bdf7c65b84c1874fb7
Marko Obrovac <mobrovac@wikimedia.org>
2016-09-12 13:36:07 +0100

    Change-Prop: Use Scap3 for config deploys
    Bug: T144595
    Change-Id: Ibadfbfac4f680c56bf1d875e3e28ebf152d25fee

--- modules/changeprop/manifests/init.pp
+++ modules/changeprop/manifests/init.pp
@@ -18,39 +18,35 @@
 # [*ores_uri*]
 #   The host where ORES service is running. Default: http://ores.svc.eqiad.wmnet:8081
 #
-# [*concurrency*]
-#   The maximum number of tasks the service can execute at any given point in
-#   time. Default: 30
-#
-
 class changeprop(
     $broker_list,
     $purge_host   = '239.128.0.112',
     $ores_uri     = 'http://ores.svc.eqiad.wmnet:8081',
     $purge_port   = 4827,
-    $concurrency  = 50,
 ) {
 
     include ::service::configuration
 
     require ::changeprop::packages
 
-    $restbase_uri = $::service::configuration::restbase_uri
-    $mwapi_uri = $::service::configuration::mwapi_uri
-
     service::node { 'changeprop':
-        enable          => true,
-        port            => 7272,
-        heap_limit      => 750,
-        config          => template('changeprop/config.yaml.erb'),
-        no_workers      => 8,  # TEMP
-        starter_module  => 'hyperswitch',
-        healthcheck_url => '',
-        has_spec        => true,
-        deployment      => 'scap3',
-        auto_refresh    => false,
-        init_restart    => false,
-        environment     => {
+        enable            => true,
+        port              => 7272,
+        healthcheck_url   => '',
+        has_spec          => true,
+        deployment        => 'scap3',
+        deployment_config => true,
+        deployment_vars   => {
+            broker_list  => $broker_list,
+            mwapi_uri    => $::service::configuration::mwapi_uri,
+            restbase_uri => $::service::configuration::restbase_uri,
+            ores_uri     => $ores_uri,
+            purge_host   => $purge_host,
+            purge_port   => $purge_port,
+        },
+        auto_refresh      => false,
+        init_restart      => false,
+        environment       => {
             'UV_THREADPOOL_SIZE' => 128
         },
     }
