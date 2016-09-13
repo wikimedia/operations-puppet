2884d86184447d2d8870c2c78d651ba17b0ba265
Marko Obrovac <mobrovac@wikimedia.org>
2016-09-13 23:28:30 +0100

    Conftool: Create script that checks the state after (de)pooling
    On certain occasions, hosts do not get (de)pooled from Pybal. This patch
    adds a script to all hosts that include conftool::scripts that checks
    the LVS state after issuing a command. The script is configurable in
    that it can wait up to a certain interval before retrying the command,
    and can also repeat the whole procedure up to N times (if needed).
    
    This commit also adds service::node wrapper scripts for (de)pooling a
    specific service.
    
    Bug: T145518
    Change-Id: If1be5238493960d272ba692094cc900c6aa5e779

--- manifests/role/mathoid.pp
+++ manifests/role/mathoid.pp
@@ -4,6 +4,14 @@ class role::mathoid{
     system::role { 'role::mathoid':
         description => 'mathoid server'
     }
+    include ::scap::conftool
+
+    # LVS pooling/depoling scripts
+    include ::lvs::configuration
+    conftool::scripts::service { 'mathoid':
+        lvs_services_config => $::lvs::configuration::lvs_services,
+        lvs_class_hosts     => $::lvs::configuration::lvs_class_hosts,
+    }
 
     include ::mathoid
 }
