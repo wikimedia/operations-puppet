243fa6d8590aa04736c4188e300057a09188062f
Riccardo Coccioli <rcoccioli@wikimedia.org>
2016-09-13 13:13:29 +0200

    Salt: fix source path typo
    Bug: T143536
    Change-Id: I42b08d818553954ea04a8f0aeb18434bcb34a8ff

--- modules/salt/manifests/orchestration.pp
+++ modules/salt/manifests/orchestration.pp
@@ -8,7 +8,7 @@ class salt::orchestration() {
 
     file { '/usr/local/sbin/wmf-auto-reimage':
         ensure => present,
-        source => 'puppet:///modules/salt/wmf-auto-reimage.py',
+        source => 'puppet:///modules/salt/wmf_auto_reimage.py',
         mode   => '0544',
         owner  => 'root',
         group  => 'root',
