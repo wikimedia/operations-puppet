24c5a272cdbb3bdd072d672fdb2ca4ca8aff895f
Marko Obrovac <mobrovac@wikimedia.org>
2016-09-13 23:28:30 +0100

    Conftool: Create script that checks the state after (de)pooling
    On certain occasions, hosts do not get (de)pooled from Pybal. This patch
    adds a script to all hosts that include conftool::scripts that checks
    the LVS state after issuing a command. The script is configurable in
    that it can wait up to a certain interval before retrying the command,
    and can also repeat the whole procedure up to N times (if needed).
    
    This commit also adds service::node wrapper scripts for (de)pooling a
    specific service.
    
    Bug: T145518
    Change-Id: If1be5238493960d272ba692094cc900c6aa5e779

---
+++ modules/conftool/files/pooler-loop.rb
@@ -0,0 +1,126 @@
+#!/usr/bin/env ruby
+require 'optparse'
+require 'net/http'
+require 'facter'
+
+
+PATH_ROOT = '/usr/local/bin/'
+
+# default values
+args = {
+  host: Facter.value(:fqdn),
+  command: PATH_ROOT + 'pool',
+  pool: true,
+  depool: false,
+  lvs_uri: [],
+  pool_name: nil,
+  pool_subject: '',
+  interval: 1,
+  wait: 9,
+  cycles: 3
+}
+
+# parse the command-line arguments
+OptionParser.new do |opts|
+  opts.banner = "Usage: pooler-loop [options] [service]"
+  opts.on('-p', '--pool', '(Re)pool the host') do |v|
+    args[:command] = PATH_ROOT + 'pool'
+    args[:pool] = true
+    args[:depool] = false
+  end
+  opts.on('-d', '--depool', 'Depool the host') do |v|
+    args[:command] = PATH_ROOT + 'depool'
+    args[:depool] = true
+    args[:pool] = false
+  end
+  opts.on('-l', '--lvs-ips IP1,IP2', 'The LVS IPs to contact') do |ips|
+    args[:lvs_uris] = ips.split(/,\s*/).map{ |ip| [ip, 9090]}
+  end
+  opts.on('-P', '--pool-name POOL', 'The pool name to use') do |pool|
+    args[:pool_name] = pool
+  end
+  opts.on('-i', '--interval INTERVAL', Float,
+      'The check interval, in seconds') do |i|
+    args[:interval] = i
+  end
+  opts.on('-w', '--wait TIMES', Integer, 'The number of intervals to wait ' +
+      'before', 're-issuing the (de)pool command') do |w|
+    args[:wait] = w
+  end
+  opts.on('-c', '--cycles N', Integer, 'The number of times to repeat',
+      'the process') do |n|
+    args[:cycles] = n
+  end
+  opts.on_tail('-h', '--help', 'Show this text and exit') do
+    puts opts
+    exit
+  end
+end.parse!
+
+args[:pool_subject] = ARGV.shift || ''
+
+unless args[:pool_name]
+  puts 'You have to specify the pool name! Use pooler-loop -h for help'
+  exit 1
+end
+
+class PybalError < StandardError
+end
+
+def check_pooled_state(ip, port, pool, host, pooled)
+  resp = Net::HTTP.start(ip, port) do |http|
+    http.get "/pools/#{pool}/#{host}"
+  end
+  # ignore 404s
+  return true unless resp === Net::HTTPSuccess
+  enabled, active, pooled = resp.body.strip.split '/'
+  case pooled
+  when true
+    # A pooled server must be enabled/up/pooled
+    # anything else is not ok
+    if enabled != 'enabled'
+      raise PybalError, "#{host} not logically pooled, lb #{ip}"
+    elsif active != 'up'
+      raise PybalError, "The service is not up on #{host}, lb #{ip}"
+    elsif pooled != 'pooled'
+      raise PybalError, "Service not pooled"
+    end
+  else
+    # A disabled server should be disabled/{up,down}/not pooled
+    if enabled == 'enabled'
+      raise PybalError, "#{host} is still logically pooled, lb #{ip}"
+    elsif pooled == 'pooled'
+      raise PybalError, "#{host} is still pooled, maybe too many depooled? lb #{ip}"
+    end
+  end
+end
+
+
+# do the whole procedure args[:cycles] times
+(1..args[:cycles]).each do
+  # try to (de)pool the host we are on
+  res = %x{#{args[:command]} #{args[:pool_subject]}}
+
+  # If the script exited with status !=0, we surely had an error; retry directly.
+  if $?.exitstatus != 0
+    sleep args[:interval]
+    next
+  end
+  # check at most args[:wait] times each args[:interval]
+  # seconds with the LVS server to confirm the host is in
+  # the desired state
+  (1..args[:wait]).each do
+    sleep args[:interval]
+    begin
+      args[:lvs_uris].each do |uri|
+        ip, port = uri
+        check_pooled_state(ip, port, args[:pool_name], args[:host], args[:pool])
+      end
+      exit 0
+    rescue PybalError => e
+      puts "Found an error: #{e.message}"
+    end
+  end
+end
+
+exit 2
