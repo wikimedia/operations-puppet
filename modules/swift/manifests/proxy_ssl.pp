23467150f905fa412048349ebded3a36928b946a
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-09-14 15:02:17 +0100

    swift: terminate https with nginx
    Bug: T127455
    Change-Id: Ia13d19e8e6eda562a90740bec097418dd7ad91e5

---
+++ modules/swift/manifests/proxy_ssl.pp
@@ -0,0 +1,29 @@
+# Class swift::proxy_ssl
+#
+# Set up nginx to terminate SSL connections, using the local machine's puppet
+# certificates.
+class swift::proxy_ssl (
+  $ensure = 'present',
+  $client_max_body_size='4G',
+) {
+    validate_ensure($ensure)
+    validate_re($client_max_body_size, '[0-9]+[MGmg]')
+
+    ::base::expose_puppet_certs { '/etc/nginx':
+        ensure          => $ensure,
+        provide_private => true,
+        require         => Class['nginx'],
+    }
+
+    $ssl_settings = ssl_ciphersuite('nginx', 'strong')
+    include ::sslcert::dhparam
+    ::nginx::site { 'swift-proxy':
+        ensure  => $ensure,
+        content => template('swift/nginx.conf.erb'),
+        require => Class['::sslcert::dhparam']
+    }
+
+    diamond::collector::nginx{ 'swift-proxy':
+        ensure => $ensure,
+    }
+}
