e959321aa620b77403cc9379db2e86080323c6e8
Jcrespo <jcrespo@wikimedia.org>
2016-09-14 17:16:57 +0200

    mariadb: control db1082's mysqld_safe file with puppet
    Also merge pending mariadb changes not yet live:
    * mysqld_safe patches
    * lvmbackup class
    
    Bug: T145378
    Change-Id: I9b1c56d7c0d6016fc8439c87f496bc6e6f3c126d

--- manifests/role/mariadb.pp
+++ manifests/role/mariadb.pp
@@ -1044,3 +1044,10 @@ class role::mariadb::client {
         ensure => latest,
     }
 }
+
+# control mysqld_safe with puppet instead of with
+# the mariadb package for flexibility
+class role::mariadb::custom_mysqld_safe {
+
+    include mariadb::mysqld_safe
+}
--- manifests/site.pp
+++ manifests/site.pp
@@ -547,7 +547,14 @@ node 'db1049.eqiad.wmnet' {
     }
 }
 
-node /^db10(26|45|70|71|82|87|92)\.eqiad\.wmnet/ {
+node /^db10(26|45|70|71|87|92)\.eqiad\.wmnet/ {
+    class { 'role::mariadb::core':
+        shard => 's5',
+    }
+}
+
+node 'db1082.eqiad.wmnet' {
+    role mariadb::custom_mysqld_safe
     class { 'role::mariadb::core':
         shard => 's5',
     }
