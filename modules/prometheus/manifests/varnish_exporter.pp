ae77d6928a202fe46de0cc4bdd3c3fbe0ba5955b
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-09-14 15:52:52 +0100

    [WIP] prometheus: add varnish_exporter
    Change-Id: I325ccc028da833da917514885cfa6bca998cc533

---
+++ modules/prometheus/manifests/varnish_exporter.pp
@@ -0,0 +1,29 @@
+# Prometheus Varnish metrics exporter.
+
+# === Parameters
+#
+# [*$instance*]
+#  The varnish instance to use, passed to varnishstat -n
+#
+# [*$listen_address*]
+#  The host:port tuple to listen on, host can be omitted.
+
+class prometheus::varnish_exporter (
+    $instance  = $::hostname,
+    $listen_address = ':9131',
+) {
+    require_package('prometheus-varnish-exporter')
+
+    exec { 'mask_default_varnish_exporter':
+        command => '/bin/systemctl mask prometheus-varnish-exporter.service',
+        creates => '/etc/systemd/system/prometheus-varnish-exporter.service',
+    }
+
+    base::service_unit { "prometheus-varnish-exporter@${instance}":
+        ensure        => present,
+        refresh       => true,
+        systemd       => true,
+        template_name => 'prometheus-varnish-exporter@',
+        require       => Package['prometheus-varnish-exporter'],
+    }
+}
