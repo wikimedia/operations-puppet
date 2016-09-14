6364994d11a1bf08469c425e985a2e3d613e23ef
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-09-14 18:28:47 +0300

    palladium: Set up a motd warning
    We don't want people to any longer use palladium
    
    Change-Id: I6818fa479b75da3df07062e279aa7808d56c8206

--- manifests/site.pp
+++ manifests/site.pp
@@ -2330,6 +2330,11 @@ node 'palladium.eqiad.wmnet' {
     interface::add_ip6_mapped { 'main':
         interface => 'eth0',
     }
+    motd::script { 'deprecation_warning':
+        ensure   => present,
+        priority => 01,
+        source   => 'puppet:///files/palladium_deprecation',
+    }
     # Add a site for temporary puppet backend tests
     ::puppetmaster::web_frontend { 'puppetmaster.test.eqiad.wmnet':
         master    => 'palladium.eqiad.wmnet',
