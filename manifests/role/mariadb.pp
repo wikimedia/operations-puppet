9c9fc46476434b3abf0c05e821c9235835f47c39
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-09-14 11:06:44 +0200

    Make /opt/wmf-mariadb10/bin/mysqld_safe managed by puppet
    This file is shipped by the custom wmf-mariadb10 package, but make it managed
    via puppet to be able to quickly make changes without rebuilding the package.
    
    This introduces the current, pristine version as shipped by wmf-mariadb10, to
    make followup changes more transparent.
    
    Note that /opt/wmf-mariadb10/bin/mysqld_safe on the servers is owned by
    jynus:mysql, but this patch makes it owned by root:mysql (the ownership can
    be cleaned up in a future update of wmf-mariadb10.
    
    Thus patch doesn't add the class to any of the sub mariadb roles yet, this
    can be done gradually with some non-core DBs first.
    
    Bug: T145378
    Change-Id: I807e41baee0d7455d0d2f9cd528a50b04c44fba6

--- manifests/role/mariadb.pp
+++ manifests/role/mariadb.pp
@@ -82,6 +82,22 @@ class role::mariadb::ferm {
     }
 }
 
+
+class role::mariadb::custom_mysql_safed {
+    # Make /opt/wmf-mariadb10/bin/mysqld_safe managed by puppet. This allows us to make quick
+    # changes to harden the wrapper without rebuilding the custom wmf-mariabd10 package
+    # Once all trusty dbs are gone, we can hopefully discard mysqld_safe in favour of a custom
+    # systemd service unit
+
+    file { '/opt/wmf-mariadb10/bin/mysqld_safe':
+        ensure => present,
+        owner  => 'root',
+        group  => 'mysql',
+        mode   => '0755',
+        source => 'puppet:///files/mariadb/mysqld_safe',
+    }
+}
+
 # Annoy people in #wikimedia-operations
 class role::mariadb::monitor {
 
