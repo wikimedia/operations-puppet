e277f0c0d27164dd0ebbd121fa6b63d1394148a4
Andrew Otto <acotto@gmail.com>
2016-09-15 10:49:08 -0400

    Fix puppet warnings in confluent module
    Change-Id: I858daf247ed9a0ce08205350d3934eaa7bd525b6

--- modules/confluent/manifests/kafka/mirror/alerts.pp
+++ modules/confluent/manifests/kafka/mirror/alerts.pp
@@ -20,7 +20,7 @@ define confluent::kafka::mirror::alerts(
     # Generate icinga alert if Kafka Server is not running.
     nrpe::monitor_service { "kafka-mirror-${title}":
         description   => "Kafka MirrorMaker ${title}",
-        nrpe_command  => "/usr/lib/nagios/plugins/check_procs -c 1:1 -C java  --ereg-argument-array 'kafka.tools.MirrorMaker.+/etc/kafka/mirror/${title}/producer\.properties'",
+        nrpe_command  => "/usr/lib/nagios/plugins/check_procs -c 1:1 -C java  --ereg-argument-array 'kafka.tools.MirrorMaker.+/etc/kafka/mirror/${title}/producer\\.properties'",
         contact_group => $contact_group,
         require       => Base::Service_unit["kafka-mirror-${title}"],
     }
