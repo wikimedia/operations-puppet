0d8226ba0a8d55eaa410f6651a47ff489f39f803
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-09-15 13:05:04 +0300

    check_postgres_replication_lag.py: Rewrite parts of it
    Add the following changes:
     * Check that indeed the DB is in recovery mode
     * Check that the master actually reports the slave as active.
     * Pass the required arguments for the above to work as this requires
       for the script to connect to master
     * Fix the using classes to pass the correct master argument
    
    Change-Id: I723bce6af13245785055f5a76d0713097c4e258e

--- modules/role/manifests/maps/slave.pp
+++ modules/role/manifests/maps/slave.pp
@@ -7,12 +7,13 @@ class role::maps::slave {
         ensure      => 'present',
         description => 'Maps Postgres slave',
     }
+    $master = hiera('postgresql::slave::master_server')
 
     $pg_password = hiera('postgresql::slave::replication_pass')
     $critical = 1800
     $warning = 300
     $command = "/usr/lib/nagios/plugins/check_postgres_replication_lag.py \
--U replication -P ${pg_password} -D template1 -C ${critical} -W ${warning}"
+-U replication -P ${pg_password} -m ${master} -D template1 -C ${critical} -W ${warning}"
     nrpe::monitor_service { 'postgres-rep-lag':
         description  => 'Postgres Replication Lag',
         nrpe_command => $command,
