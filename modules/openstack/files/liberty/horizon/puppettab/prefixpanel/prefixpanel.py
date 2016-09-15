ada08befb860a1ce86e720f382622c1372a33faf
Andrew Bogott <abogott@wikimedia.org>
2016-09-14 20:16:20 -0500

    Puppet Panel:  Actually populate the prefix panel with prefixes.
    Bug: T91990
    Change-Id: I1a22aa6cad93da0bfe3c4f41748fd0a7566a1f61

--- modules/openstack/files/liberty/horizon/puppettab/prefixpanel/prefixpanel.py
+++ modules/openstack/files/liberty/horizon/puppettab/prefixpanel/prefixpanel.py
@@ -24,6 +24,7 @@ from horizon.tabs import TabGroup
 
 from wikimediapuppettab.tab import PuppetTab
 from wikimediapuppettab.prefixpanel.plustab import PlusTab
+from wikimediapuppettab.puppet_config import puppet_config
 
 logging.basicConfig()
 LOG = logging.getLogger(__name__)
@@ -32,9 +33,6 @@ LOG = logging.getLogger(__name__)
 class PrefixPuppetPanel(horizon.Panel):
     name = _("Prefix Puppet")
     slug = "prefixpuppet"
-
-    def handle(self, request, data):
-        LOG.warning('PrefixPuppetPanel is handling')
 
 
 class PrefixTabs(tabs.TabGroup):
@@ -47,42 +45,44 @@ class PrefixTabs(tabs.TabGroup):
         self.request = request
         self.kwargs = kwargs
         self._data = None
-        tab_instances = []
+        self.request = request
 
-        tenant_id = self.request.user.tenant_id
-
-        # demo #1
-        prefix = 'prefixone'
-        tab_instances.append(("puppet-%s" % prefix,
-                              PuppetTab(self,
-                                        request,
-                                        prefix=prefix,
-                                        tenant_id=tenant_id)))
-        # demo #2
-        prefix = 'prefixtwo'
-        tab_instances.append(("puppet-%s" % prefix,
-                              PuppetTab(self,
-                                        request,
-                                        prefix=prefix,
-                                        tenant_id=tenant_id)))
-        # + tab
-        tab_instances.append(('puppetprefixplus',
-                              PlusTab(self, request, tenant_id=tenant_id)))
-
-        self._tabs = OrderedDict(tab_instances)
+        self.tenant_id = self.request.user.tenant_id
+        self._tabs = OrderedDict(self.get_dynamic_tab_list())
         if self.sticky:
             self.attrs['data-sticky-tabs'] = 'sticky'
         if not self._set_active_tab():
             self.tabs_not_available()
 
-    def handle(self, request, data):
-        LOG.warning('PrefixTabs is handling')
+    def get_dynamic_tab_list(self):
+        prefixlist = puppet_config.get_prefixes(self.tenant_id)
+        LOG.warning("prefixlist: %s" % prefixlist)
+
+        tab_instances = []
+        # One tab per prefix
+        for prefix in prefixlist:
+            # exclude anything with a '.' as those are instance names
+            #  handled on a different UI
+            if '.' not in prefix:
+                tab_instances.append(("puppet-%s" % prefix,
+                                      PuppetTab(self,
+                                                self.request,
+                                                prefix=prefix,
+                                                tenant_id=self.tenant_id)))
+
+        # + tab
+        tab_instances.append(('puppetprefixplus',
+                              PlusTab(self, self.request,
+                                      tenant_id=self.tenant_id)))
+        return tab_instances
+
+    def load_tab_data(self):
+        # This ensures that the tab list is updated without
+        #  having to rebuild the whole object.
+        self._tabs = OrderedDict(self.get_dynamic_tab_list())
 
 
 class IndexView(tabs.TabbedTableView):
     tab_group_class = PrefixTabs
     template_name = 'project/puppet/prefix_panel.html'
     page_title = _("Prefix Puppet")
-
-    def handle(self, request, data):
-        LOG.warning('IndexView is handling')
