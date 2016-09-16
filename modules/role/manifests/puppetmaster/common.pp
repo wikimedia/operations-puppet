96d90f49a17aefee951c0c8f11ddb0ac9988f397
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-09-16 17:42:47 +0300

    puppetmaster: Send reports to puppetdb
    We 'd like to evaluate the reports handling of puppet agent run by
    PuppetDB. Enable it on puppetDB enabled puppetmasters
    
    Change-Id: I8015e6ee6e397027579841b8113497d6130da401

--- modules/role/manifests/puppetmaster/common.pp
+++ modules/role/manifests/puppetmaster/common.pp
@@ -13,6 +13,7 @@ class role::puppetmaster::common ( $base_config ) {
     $puppetdb_config = {
         storeconfigs         => true,
         storeconfigs_backend => 'puppetdb',
+        reports              => 'store,puppetdb',
     }
 
     $use_puppetdb = hiera('puppetmaster::config::use_puppetdb', false)
