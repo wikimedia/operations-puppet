dfdfb577a3953adcc89d09a414e9789718c01487
YuviPanda <yuvipanda@gmail.com>
2016-09-16 10:41:12 -0700

    labs: Add a per-project puppetmaster role
    Uses the puppetmaster module rather than the puppet module.
    
    DEATH TO THE PUPPET MODULE
    
    Change-Id: I5f11761bdd2a1f292d3d061363fa53346d1eb768

---
+++ modules/role/manifests/labs/project_puppetmaster.pp
@@ -0,0 +1,32 @@
+class role::labs::project_puppetmaster(
+    $autosign = false,
+) {
+    include ldap::role::config::labs
+
+    $ldapconfig = $ldap::role::config::labs::ldapconfig
+    $basedn = $ldapconfig['basedn']
+
+    $encconfig = {
+        'ldapserver'    => $ldapconfig['servernames'][0],
+        'ldapbase'      => "ou=hosts,${basedn}",
+        'ldapstring'    => '(&(objectclass=puppetClient)(associatedDomain=%s))',
+        'ldapuser'      => $ldapconfig['proxyagent'],
+        'ldappassword'  => $ldapconfig['proxypass'],
+        'ldaptls'       => true,
+        'node_terminus' => 'ldap'
+    }
+
+    # Allow access from everywhere! Use certificates to
+    # control access
+    $allow_from = '10.0.0.0/8'
+
+    class { '::puppetmaster':
+        server_name    => $::fqdn,
+        allow_from     => $allow_from,
+        secure_private => false,
+        config         => merge($encconfig, {
+            'thin_storeconfigs' => false,
+            'autosign'          => $autosign,
+        })
+    }
+}
