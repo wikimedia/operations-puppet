16aadffe48e70cf4fee7b4cfcf52cc5824ae91e5
andrewbogott <abogott@wikimedia.org>
2016-09-16 10:36:18 -0500

    Set CHARSET=utf8mb4 for the labspuppetbackend tables.
    Previously things were 'binary' which made debugging
    unpleasant.
    
    Bug: T133412
    Change-Id: Id0e97dd3876d7f4b8bdf0e167717542c3c782654

--- modules/labspuppetbackend/files/schema.sql
+++ modules/labspuppetbackend/files/schema.sql
@@ -1,19 +1,19 @@
 CREATE TABLE prefix (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    project VARCHAR(64) BINARY NOT NULL, -- 63 dns label length limit
-    prefix VARCHAR(64) BINARY NOT NULL -- 63 dns label length limit + 1 for trailing .
-);
+    project VARCHAR(64) NOT NULL, -- 63 dns label length limit
+    prefix VARCHAR(64) NOT NULL -- 63 dns label length limit + 1 for trailing .
+) CHARSET=utf8mb4;
 CREATE UNIQUE INDEX project_prefix ON prefix(project, prefix);
 
 CREATE TABLE roleassignment(
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     prefix_id INT UNSIGNED NOT NULL,
-    role VARCHAR(255) BINARY NOT NULL
-);
+    role VARCHAR(255) NOT NULL
+) CHARSET=utf8mb4;
 
 CREATE TABLE hieraassignment(
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     prefix_id INT UNSIGNED NOT NULL,
     hiera_data TEXT NOT NULL
-);
+) CHARSET=utf8mb4;
 CREATE UNIQUE INDEX hiera_prefix ON hieraassignment(prefix_id);
