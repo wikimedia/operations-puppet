51d0706648f89166ec80eac595beaec71439716a
cpettet <rush@wikimedia.org>
2016-09-20 11:10:44 -0500

    labstore: use secondary interface for DRBD replication
    * Assign IP as reserved in DNS
    * Put the right ip in the right place
    * Use it as the peer identifier for the config
    
    Change-Id: I404f340f23db4bb49e99240c2d88cf989b3f17c9

--- modules/role/manifests/labs/nfs/secondary.pp
+++ modules/role/manifests/labs/nfs/secondary.pp
@@ -11,24 +11,44 @@ class role::labs::nfs::secondary($monitor = 'eth0') {
     # Enable RPS to balance IRQs over CPUs
     interface::rps { $monitor: }
 
+    if $hostname == 'labstore1005' {
+        interface::ip { 'drbd-replication':
+            interface => 'eth1',
+            address   => '10.64.37.26',
+            prefixlen => '24',
+        }
+    }
+
+    if $hostname == 'labstore1004' {
+        interface::ip { 'drbd-replication':
+            interface => 'eth1',
+            address   => '10.64.37.25',
+            prefixlen => '24',
+       }
+   }
+
+    $drbd_nodes = ['eth1.labstore1004.eqiad.wmnet', 'eth1.labstore1005.eqiad.wmnet']
     labstore::drbd::resource {'test':
-        nodes  => ['labstore1004', 'labstore1005'],
-        port   => '7790',
-        device => '/dev/drbd1',
-        disk   => '/dev/misc/test',
+        nodes   => $drbd_nodes,
+        port    => '7790',
+        device  => '/dev/drbd1',
+        disk    => '/dev/misc/test',
+        require => Interface::Ip['drbd-replication'],
     }
 
     labstore::drbd::resource {'tools':
-        nodes  => ['labstore1004', 'labstore1005'],
+        nodes  => $drbd_nodes,
         port   => '7791',
         device => '/dev/drbd2',
         disk   => '/dev/tools-project/tools-project',
+        require => Interface::Ip['drbd-replication'],
     }
 
     labstore::drbd::resource {'others':
-        nodes  => ['labstore1004', 'labstore1005'],
+        nodes  => $drbd_nodes,
         port   => '7792',
         device => '/dev/drbd3',
         disk   => '/dev/misc/others',
+        require => Interface::Ip['drbd-replication'],
     }
 }
