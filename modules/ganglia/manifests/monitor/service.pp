8ab99c44a06bdf4e9da13ba70c955609cc8a0989
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-09-21 13:07:41 +0100

    ganglia: ship native systemd service unit
    Note I haven't tackled service_unit vs service, out of scope for this
    particular change.
    
    Bug: T144778
    Change-Id: I23731924393eadc1d1fe92c4432bbd3cb554a21d

--- modules/ganglia/manifests/monitor/service.pp
+++ modules/ganglia/manifests/monitor/service.pp
@@ -19,6 +19,14 @@ class ganglia::monitor::service() {
             source => "puppet:///modules/${module_name}/systemd/ganglia-monitor-aggregator@.service",
             before => Service['ganglia-monitor'],
         }
+
+        file { '/etc/systemd/system/ganglia-monitor.service':
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            source => "puppet:///modules/${module_name}/systemd/ganglia-monitor.service",
+            before => Service['ganglia-monitor'],
+        }
     }
 
     service { 'ganglia-monitor':
