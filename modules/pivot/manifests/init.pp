76566f095bb44e36b68c294151f67a094ad3dbb3
elukey <ltoscano@wikimedia.org>
2016-09-23 10:20:52 +0200

    Imply Pivot UI's puppetization
    This nodejs service is a UI to explore the data stored in the
    Analytics's Druid cluster.
    Remarks:
    1) We tried to use service-runner but in the end we were not
       able to integrate its code with Pivot's one, so we'll work on it
       (maybe with upstream) later on.
    2) For clarity and simplicity the only supported init is systemd so
       Pivot needs to be deployed to a Debian based host.
    3) Scap configuration has been provided. After a chat with Marko
       the proposal is to have analytics_deploy to own the files and
       the deployment key, but let pivot/pivot run the nodejs service.
    4) The best thing to do, since the target host also runs other stuff,
       is to use a Firejail profile.
    
    Bug: T138262
    Change-Id: I0898ddf86bc8074b772cfa26d04a90a76cac52ce

---
+++ modules/pivot/manifests/init.pp
@@ -0,0 +1,94 @@
+# == Class: pivot
+#
+# This class installs and configures the Imply Pivot nodejs application.
+#
+# Context up to September 2016:
+# There is a current dispute between Imply and Metamarkets about a possible
+# copyright infringement related to Imply's pivot UI.
+# The Analytics team set a while back a goal to provide a Pivot UI
+# to their users with the assumption that all the code
+# used/deployed was open souce and freely available. If this assumption will
+# change in the future, for example after a legal sentence, the Analytics team
+# will take the necessary actions.
+# For any question please reach out to the Analytics team:
+# https://www.mediawiki.org/wiki/Analytics#Contact
+#
+# Bug: T138262
+#
+# === Parameters
+#
+# $port              The port used by Pivot to accept HTTP connections.
+#                    Default: 9090
+# $druid_broker      The fully qualified domain name (like druid1001.eqiad.wmnet)
+#                    of the Druid Broker that the Pivot UI will contact.
+#                    Default: undef
+# $deployment_user   Scap deployment user.
+#                    Default: 'analytics_deploy'
+# $scap_repo         Scap repository.
+#                    Default: 'analytics/pivot/deploy'
+# $contact_group     Contact group for alerts.
+#                    Default: 'admins'
+
+class pivot(
+    $port            = 9090,
+    $druid_broker    = undef,
+    $deployment_user = 'analytics_deploy',
+    $scap_repo       = 'analytics/pivot/deploy',
+    $contact_group   = 'admins',
+) {
+
+    requires_os('debian >= jessie')
+    require_package('nodejs', 'firejail')
+
+    $scap_deployment_base_dir = '/srv/deployment'
+    $pivot_deployment_dir = "${scap_deployment_base_dir}/${scap_repo}"
+
+    scap::target { 'analytics/pivot/deploy':
+        deploy_user  => $deployment_user,
+        service_name => 'pivot',
+    }
+
+    group { 'pivot':
+        ensure => present,
+        system => true,
+    }
+
+    user { 'pivot':
+        gid     => 'pivot',
+        shell   => '/bin/bash',
+        system  => true,
+        require => Group['pivot'],
+    }
+
+    file { '/etc/firejail/pivot.profile':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/pivot/pivot.profile.firejail',
+    }
+
+    systemd::syslog { 'pivot':
+        readable_by => 'all',
+        base_dir    => '/var/log',
+        group       => 'root',
+    }
+
+    base::service_unit { 'pivot':
+        ensure  => present,
+        systemd => true,
+        require => [
+            Scap['analytics/pivot/deploy'],
+            File['/etc/firejail/pivot.profile'],
+            User['pivot'],
+            Systemd::Syslog['pivot'],
+        ],
+    }
+
+    monitoring::service { 'pivot':
+        description   => 'pivot',
+        check_command => "check_tcp!${port}",
+        contact_group => $contact_group,
+        require       => Base::Service_unit['pivot'],
+    }
+}
