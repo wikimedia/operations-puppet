02596fe49122675be44eb67afde232a93cc5e266
elukey <ltoscano@wikimedia.org>
2016-09-23 10:20:52 +0200

    First draft of the Pivot UI's puppetization
    Remarks:
    1) we tried to use service-runner but in the end we were not
       able to integrate its code with Pivot's one, so we'll work on it
       (maybe with upstream) later on.
    2) for clarity and simplicity the only supported init is systemd so
       Pivot needs to be deployed to Debia.
    3) Scap configuration has been provided. After a chat with Marko
       the proposal is to have analytics_deploy to own the files and
       the deployment key, but let pivot/pivot run the nodejs service.
    4) The best thing to do, since the target host also runs other stuff,
       is imho to use Firejail.
    
    Bug: T138262
    Change-Id: I0898ddf86bc8074b772cfa26d04a90a76cac52ce

---
+++ modules/role/manifests/pivot/pivot.pp
@@ -0,0 +1,18 @@
+# == Class: role::pivot
+class role::pivot {
+
+    system::role { 'role::pivot':
+        description => 'Imply Pivot UI'
+    }
+
+    class { '::pivot':
+        port          => 9090,
+        druid_broker  => 'druid1001.eqiad.wmnet:8082',
+        contact_group => 'analytics',
+    }
+
+    ferm::service { 'pivot':
+        proto => 'tcp',
+        port  => '9090',
+    }
+}
