1cfe1cca453a55df28011efa07c5ab6d807321f9
andrewbogott <abogott@wikimedia.org>
2016-10-03 16:36:17 -0500

    Move contint::slave_scripts from a class to a role.
    Bug: T147233
    Change-Id: Ia1dd7ce75f033985bdd64320c7ab2ee9a2cd916c

---
+++ modules/role/manifests/contint/slave_scripts.pp
@@ -0,0 +1,19 @@
+# == Class contint::slave_scripts
+#
+# Scripts and helpers used by CI.
+#
+class role::contint::slave_scripts {
+
+    if $::realm == 'production' {
+        fail('contint::slave_scripts must not be used in production. Slaves are already Trebuchet deployment targets.')
+    }
+
+    require contint::deployment_dir
+
+    git::clone { 'jenkins CI slave scripts':
+        ensure             => 'latest',
+        directory          => '/srv/deployment/integration/slave-scripts',
+        origin             => 'https://gerrit.wikimedia.org/r/p/integration/jenkins.git',
+        recurse_submodules => true,
+    }
+}
