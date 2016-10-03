92be4c499e8985bc8fdbb081270fbada6ec4d1a6
Tyler Cipriani <tcipriani@wikimedia.org>
2016-10-03 15:25:07 -0700

    Beta: Move beta::deployaccess to role::beta::deployaccess
    Change-Id: I16cb7cc2e8f9ef81cec41ce9a29e00d2c34e64a3

---
+++ modules/role/manifests/beta/mediawiki.pp
@@ -0,0 +1,12 @@
+# == Class role::beta::mediawiki
+#
+# Allow mwdeploy to login from scap deployment host. Adds an exception in
+# /etc/security/access.conf to work around labs-specific restrictions
+class role::beta::mediawiki {
+    $deployment_host = hiera('scap::deployment_server')
+    $deployment_ip = ipresolve($deployment_host)
+    security::access::config { 'scap-allow-mwdeploy':
+        content  => "+ : mwdeploy : ${deployment_ip}\n",
+        priority => 60,
+    }
+}
