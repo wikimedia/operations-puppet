7a90c4fd5265e8ad88dc9e1880599d83c8847b92
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-10-03 16:34:57 +0100

    prometheus: add beta-specific instance
    Allow testing more specific exporters in Beta (e.g. varnish)
    
    Bug: T144502
    Change-Id: I5420e75458eff7a62881260d9e6d190724eee859

---
+++ modules/role/manifests/prometheus/beta.pp
@@ -0,0 +1,84 @@
+# == Class: role::prometheus::beta
+#
+# This class provides a Prometheus server used to monitor Beta
+# (deployment-prep) labs project.
+
+class role::prometheus::beta {
+    $targets_path = '/srv/prometheus/beta/targets'
+    $rules_path = '/srv/prometheus/beta/rules'
+
+    # one job per varnish cache 'role'
+    $varnish_jobs = [
+      {
+        'job_name'        => 'varnish-text',
+        'file_sd_configs' => [
+          { 'names' => [ "${targets_path}/varnish-text_*.yaml"] },
+        ]
+      },
+      {
+        'job_name'        => 'varnish-upload',
+        'file_sd_configs' => [
+          { 'names' => [ "${targets_path}/varnish-upload_*.yaml"] },
+        ]
+      },
+    ]
+
+    $mysql_jobs = [
+      {
+        'job_name'        => 'mysql-core',
+        'file_sd_configs' => [
+          { 'names' => [ "${targets_path}/mysql-core_*.yaml"] },
+        ]
+      },
+    ]
+
+    $web_jobs = [
+      {
+        'job_name'        => 'apache',
+        'file_sd_configs' => [
+          { 'names' => [ "${targets_path}/apache_*.yaml"] },
+        ]
+      },
+      {
+        'job_name'        => 'hhvm',
+        'file_sd_configs' => [
+          { 'names' => [ "${targets_path}/hhvm_*.yaml"] },
+        ]
+      },
+      {
+        'job_name'        => 'memcache',
+        'file_sd_configs' => [
+          { 'names' => [ "${targets_path}/memcache_*.yaml"] },
+        ]
+      },
+    ]
+
+    prometheus::server { 'beta':
+        listen_address => '127.0.0.1:9903',
+        scrape_configs_extra => concat($varnish_jobs, $mysql_jobs, $web_jobs),
+    }
+
+    prometheus::web { 'beta':
+        proxy_pass => 'http://127.0.0.1:9903/beta',
+    }
+
+    file { "${rules_path}/rules_beta.conf":
+        source => 'puppet:///modules/role/prometheus/rules_beta.conf',
+    }
+
+    file { "${rules_path}/alerts_beta.conf":
+        source => 'puppet:///modules/role/prometheus/alerts_beta.conf',
+    }
+
+    $targets_file = "${targets_path}/node_project.yml"
+
+    include ::prometheus::scripts
+
+    cron { 'prometheus_labs_project_targets':
+        ensure  => present,
+        command => "/usr/local/bin/prometheus-labs-targets > ${targets_file}.$$ && mv ${targets_file}.$$ ${targets_file}",
+        minute  => '*/10',
+        hour    => '*',
+        user    => 'prometheus',
+    }
+}
