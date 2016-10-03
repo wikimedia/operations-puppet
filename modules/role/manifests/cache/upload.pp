f4d826ba4385130acb95c5e6a21acbc875c93876
Brandon Black <bblack@wikimedia.org>
2016-10-03 18:03:52 +0000

    cache_upload: jemalloc chunk size: s/1MB/128KB/
    1MB was a conservative value (better than the default 4MB for
    sure).  Since then we've made other changes to the upload
    frontends: we no longer cache objects >= 256KB.
    
    Further, we know from the statistics gathered in T145661#2649474
    that the majority of the objects and requests are < 16KB.
    Therefore, 128KB should be a fairly safe upper end for tuning this
    parameter now; the ideal value might be even lower, but this is
    safe and a significant efficiency gain.
    
    This won't take effect until the relevant (upload) frontends are
    next restarted.
    
    Change-Id: I7c321bd2deded8e90c3b7560af42aeddd8a378b8

--- modules/role/manifests/cache/upload.pp
+++ modules/role/manifests/cache/upload.pp
@@ -106,7 +106,7 @@ class role::cache::upload(
 
     role::cache::instances { 'upload':
         fe_mem_gb         => ceiling(0.4 * $::memorysize_mb / 1024.0),
-        fe_jemalloc_conf  => 'lg_dirty_mult:8,lg_chunk:20',
+        fe_jemalloc_conf  => 'lg_dirty_mult:8,lg_chunk:17',
         fe_runtime_params => $common_runtime_params,
         be_runtime_params => concat($common_runtime_params, $be_runtime_params),
         app_directors     => $app_directors,
