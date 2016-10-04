70c98d85981bcace95eb0868d3589500891fb772
andrewbogott <abogott@wikimedia.org>
2016-10-04 02:33:04 -0500

    Remove role::deployment::server from deploymentserver.pp
    This belongs in this class.  But, the deployment servers are
    a spaghetti of redundant includes and including it here
    causes some undefined behavior to change to slightly
    different yet still undefined behavior.
    
    Change-Id: I68242d5b6fa9e5d89c0d7c4fd353e105cc3bf8f6

--- modules/role/manifests/beta/deploymentserver.pp
+++ modules/role/manifests/beta/deploymentserver.pp
@@ -1,6 +1,5 @@
 # Role class for deployment servers in deployment-prep
 class role::beta::deploymentserver {
     include beta::autoupdater
-    include role::deployment::server
     include role::beta::mediawiki
 }
