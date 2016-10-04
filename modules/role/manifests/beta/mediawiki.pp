d4e3aff18fbb25fcbfd91467a825307e2839ffe2
Mukunda Modell <mmodell@wikimedia.org>
2016-10-04 10:13:19 -0500

    Pass nameserver to ipresolve
    Hopefully fixes this error I'm seeing on newly created
    instances in deployment-prep:
    
     Error 400 on SERVER: DNS lookup failed for
     deployment-tin.deployment-prep.eqiad.wmflabs
     Resolv::DNS::Resource::IN::A
     at /etc/puppet/modules/scap/manifests/target.pp:98
     on node deployment-phab02.deployment-prep.eqiad.wmflabs
    
    Change-Id: I95349e72ebe615c36c9fbfbf7c329f62cbdf6e6f

--- modules/role/manifests/beta/mediawiki.pp
+++ modules/role/manifests/beta/mediawiki.pp
@@ -4,7 +4,7 @@
 # /etc/security/access.conf to work around labs-specific restrictions
 class role::beta::mediawiki {
     $deployment_host = hiera('scap::deployment_server')
-    $deployment_ip = ipresolve($deployment_host)
+    $deployment_ip = ipresolve($deployment_host, 4, $::nameservers[0])
     security::access::config { 'scap-allow-mwdeploy':
         content  => "+ : mwdeploy : ${deployment_ip}\n",
         priority => 60,
