c7184375f30a3612a1b33cbe9ae216f78577202b
Mukunda Modell <mmodell@wikimedia.org>
2016-10-03 22:24:09 -0500

    Configuration for Aphlict
    Aphlict is the node.js-based websocket server that powers the
    Phabricator real-time notification service refs T112765, T765
    
    Bug: T112765
    Change-Id: I182e2e28e38514cb03e8ac79192fe165ad6e6ee0

--- modules/role/manifests/phabricator/labs.pp
+++ modules/role/manifests/phabricator/labs.pp
@@ -1,28 +1,33 @@
 # phabricator instance on wmflabs at phab-0[1-9].wmflabs.org
 
-class role::phabricator::labs {
+class role::phabricator::labs(
+        $settings,
+) {
+    $role_settings = {
+        'darkconsole.enabled'             => true,
+        'phabricator.base-uri'            => "https://${::hostname}.wmflabs.org",
+        'mysql.pass'                      => $mysqlpass,
+        'auth.require-email-verification' => false,
+        'metamta.mail-adapter'            => 'PhabricatorMailImplementationTestAdapter',
+        'repository.default-local-path'   => '/srv/repos',
+        'phd.taskmasters'                 => 1,
+        'config.ignore-issues'            => '{
+                                                  "security.security.alternate-file-domain": true
+                                              }',
+    }
 
+    $phab_settings = merge($settings, $role_settings)
     # pass not sensitive but has to match phab and db
     $mysqlpass = 'labspass'
     $phab_root_dir = '/srv/phab'
+
     class { '::phabricator':
         deploy_target => 'phabricator/deployment',
         phabdir       => $phab_root_dir,
         libraries     => ["${phab_root_dir}/libext/Sprint/src",
                           "${phab_root_dir}/libext/security/src",
                           "${phab_root_dir}/libext/misc/"],
-        settings      => {
-            'darkconsole.enabled'             => true,
-            'phabricator.base-uri'            => "https://${::hostname}.wmflabs.org",
-            'mysql.pass'                      => $mysqlpass,
-            'auth.require-email-verification' => false,
-            'metamta.mail-adapter'            => 'PhabricatorMailImplementationTestAdapter',
-            'repository.default-local-path'   => '/srv/repos',
-            'phd.taskmasters'                 => 1,
-            'config.ignore-issues'            => '{
-                                                      "security.security.alternate-file-domain": true
-                                                  }',
-        },
+        settings      => $phab_settings,
     }
 
     package { 'mysql-server': ensure => present }
