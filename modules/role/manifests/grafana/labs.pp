53cb654adf73eaa99c714bf5c12e2f69b9ddd8c4
addshore <addshorewiki@gmail.com>
2016-10-04 17:22:27 +0100

    Enable simple-json-datasource on prod Grafana
    Bug: T147329
    Change-Id: I9b99d9fd394b1cc88e9c62f0d33bf55a7b33a06f

--- modules/role/manifests/grafana/labs.pp
+++ modules/role/manifests/grafana/labs.pp
@@ -21,18 +21,4 @@ class role::grafana::labs {
         ]
     }
 
-    # https://phabricator.wikimedia.org/T141636
-    # We clone this, but need to symlink into the 'dist' directory for plugin to actually work
-    git::clone { 'operations/software/grafana/simple-json-datasource':
-        ensure    => present,
-        branch    => '3.0',
-        directory => '/usr/share/grafana/public/app/plugins/datasource/simple-json-datasource',
-        require   => Package['grafana'],
-    }
-
-    file { '/usr/share/grafana/public/app/plugins/datasource/datasource-plugin-genericdatasource':
-        ensure  => link,
-        target  => '/usr/share/grafana/public/app/plugins/datasource/simple-json-datasource/dist',
-        require => Git::Clone['operations/software/grafana/simple-json-datasource'],
-    }
 }
