bdf82d5283ad5194dab0b9b8039e936357f99cb7
Mukunda Modell <mmodell@wikimedia.org>
2016-10-03 22:24:09 -0500

    phabricator: Configuration for Aphlict
    Aphlict is the node.js-based websocket server that powers the
    Phabricator real-time notification service refs T112765, T765
    
    Bug: T112765
    Change-Id: I182e2e28e38514cb03e8ac79192fe165ad6e6ee0

---
+++ modules/phabricator/manifests/aphlict.pp
@@ -0,0 +1,110 @@
+# == Class: phabricator::aphlict
+# Aphlict is the phabricator real-time notification relay service.
+# Docs: https://secure.phabricator.com/book/phabricator/article/notifications/
+class phabricator::aphlict(
+    $ensure = 'present',
+    $user   = 'aphlict',
+    $group  = 'aphlict',
+) {
+    validate_ensure($ensure)
+
+    # packages
+    require_package('nodejs')
+
+    # paths
+    $basedir = $phabricator::phabdir
+    $phabdir = "${basedir}/phabricator/"
+    $aphlict_dir = "${phabdir}/support/aphlict/server"
+    $node_modules = "${aphlict_dir}/node_modules"
+    $aphlict_conf = "${basedir}/aphlict/config.json"
+    $aphlict_cmd = "${phabdir}/bin/aphlict start --config ${aphlict_conf}"
+
+    # Ordering
+    Package['nodejs'] -> File[$aphlict_conf] ~> Service['aphlict']
+    File['/var/run/aphlict/'] -> File['/var/log/aphlict/'] -> Service['aphlict']
+    User[$user] -> Service['aphlict']
+    File[$node_modules] ~> Service['aphlict']
+
+    if $ensure == 'present' {
+        $link = 'link'
+        $directory = 'directory'
+    } else {
+        $link = 'absent'
+        $directory = 'absent'
+    }
+
+
+    # Defines
+    file { $node_modules:
+        ensure => 'link',
+        target => "${basedir}/aphlict/node_modules",
+    }
+
+    file { $aphlict_conf:
+        ensure  => $ensure,
+        content => template('phabricator/aphlict-config.json.erb'),
+        owner   => $user,
+        group   => $group,
+        mode    => '0644',
+    }
+
+    if $::initsystem == 'upstart' {
+        # upstart init conf file
+        $init_file = '/etc/init/aphlict.conf'
+        $init_source = 'aphlict-upstart.conf.erb'
+    } else {
+        # systemd service unit
+        $init_file = '/etc/systemd/system/aphlict.service'
+        $init_source = 'aphlict.service.erb'
+    }
+
+    file { '/etc/init.d/aphlict':
+        ensure => $link,
+        target => "${phabdir}/bin/aphlict",
+    }
+
+    file { $init_file:
+        content => template("phabricator/${init_source}"),
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+    }
+
+    service { 'aphlict':
+        ensure     => running,
+        provider   => $::initsystem,
+        hasrestart => true,
+    }
+
+    file { '/var/run/aphlict/':
+        ensure => $directory,
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { '/var/log/aphlict/':
+        ensure => $directory,
+        owner  => $user,
+        group  => $group,
+    }
+
+    logrotate::conf { 'aphlict':
+        ensure  => $ensure,
+        source  => 'puppet:///modules/phabricator/logrotate_aphlict',
+        require => File['/var/log/aphlict/']
+    }
+
+    # accounts
+    group { $group:
+        ensure => $ensure,
+        system => true,
+    }
+
+    user { $user:
+        gid    => 'aphlict',
+        shell  => '/bin/false',
+        home   => '/var/run/aphlict',
+        system => true,
+    }
+
+}
