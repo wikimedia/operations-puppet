78f0c63c2679d5b8559314bd9fc64b35c48186c0
Guillaume Lederrey <glederrey@wikimedia.org>
2016-10-05 12:10:46 +0200

    osm - use osm2pgsql from jessie-backport
    Pinning osm2pgsql to jessie-backports is a cleaner solution then
    explicitely ensuring a specific version of osm2pgsql.
    
    Change-Id: I11ba34e6a8e5be9b465d57d617fb498bb954bea8

--- modules/osm/manifests/init.pp
+++ modules/osm/manifests/init.pp
@@ -6,25 +6,13 @@ class osm (
 
     # osm2pgsql 0.90 is only available on jessie at the moment
     # there is no need for 0.90 on labs machines (precise)
-    $osm2pgsql_ensure = $ensure ? {
-        'present' => os_version('Debian >= Jessie') ? {
-            true => '0.90.0+ds-1~bpo8+1',
-            default => 'present',
-        },
-        default   => $ensure,
-    }
-
-    # Can't use require_package here because we need to specify version
-    # from jessie-backports:
-    if !defined(Package['osm2pgsql']) {
-        package { 'osm2pgsql':
-            ensure => $osm2pgsql_ensure,
+    if os_version('Debian == Jessie') {
+        apt::pin { 'osm2pgsql':
+            pin      => 'release a=jessie-wikimedia c=backports',
+            priority => '1001',
+            before   => Package['osm2pgsql'],
         }
     }
 
-    if !defined(Package['osmosis']) {
-        package { 'osmosis':
-            ensure => $ensure,
-        }
-    }
+    require_package('osm2pgsql', 'osmosis')
 }
