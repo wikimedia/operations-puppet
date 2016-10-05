78515c6bb5e4baf887e951e93142d80d989f0d1c
andrewbogott <abogott@wikimedia.org>
2016-10-05 13:51:33 -0500

    Add base::firewall to role::beta::mediawiki
    This is NOT a no-op, but may be safe anyway.
    
    Bug: T147233
    Change-Id: I3ccc78b44094958e699ee7cc03c366d7e9ba1285

--- modules/role/manifests/beta/mediawiki.pp
+++ modules/role/manifests/beta/mediawiki.pp
@@ -3,6 +3,8 @@
 # Allow mwdeploy to login from scap deployment host. Adds an exception in
 # /etc/security/access.conf to work around labs-specific restrictions
 class role::beta::mediawiki {
+    include base::firewall
+
     $deployment_host = hiera('scap::deployment_server')
     $deployment_ip = ipresolve($deployment_host, 4, $::nameservers[0])
     security::access::config { 'scap-allow-mwdeploy':
