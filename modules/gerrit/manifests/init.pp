ba346d590d8cfdc98eed5a89577c3aa457075df6
Chad Horohoe <chadh@wikimedia.org>
2016-10-06 16:24:20 -0700

    Gerrit: provide auto-detection of slave status
    This way we can provision as many as nodes as we want and they'll
    always be slaves unless they're the one we specify as the master.
    
    Change-Id: If9c9ea200a3b437d48fd9dd7c2b27fcf9238ea17

--- modules/gerrit/manifests/init.pp
+++ modules/gerrit/manifests/init.pp
@@ -1,15 +1,25 @@
 # Manifest to setup a Gerrit instance
 
-class gerrit($host = undef) {
+class gerrit($host = undef, $master_node = $::fqdn) {
 
     if $host == undef {
         fail('$gerrit::host must be set!')
     }
 
-    class { 'gerrit::jetty': }
+    # Are we a slave or a master?
+    $slave = $master_node ? {
+        $::fqdn => false,
+        default => true,
+    }
 
-    class { 'gerrit::proxy':
-        require => Class['gerrit::jetty'],
+    class { 'gerrit::jetty':
+        slave => $slave,
+    }
+
+    if !$slave {
+        class { 'gerrit::proxy':
+            require => Class['gerrit::jetty'],
+        }
     }
 
     class { 'gerrit::crons':
