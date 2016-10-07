b362ac18ab71d563116edfb1f2ae325700d7e944
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-10-07 16:35:44 +0300

    Use the distribution's check_nrpe binary and stop shipping our own
    check_nrpe is a dynamically linked, ELF 64-bit LSB executable. Shipping
    it via puppet with no respect at all about distribution versions is
    clearly a way to insanity. Even more due to the fact that the binary is
    actually shipped in the nagios-nrpe-plugin, a package name that does not
    seem to have changed naming since at least 2005 if not 2004. So stop
    shipping it, remove it from the repo and just replace it with a package
    resource for it
    
    Along side this patch, revert "icinga: libssl0.9.8 for NRPE checks to run"
    For the same reason, plus puppetizing a versioned library that was not
    going to be present in future versions of the distributions was not the
    cleanest way to go around this one.
    
    This reverts commit cfff82c9abd40868ec5ee897715e1270b2eada3f.
    
    Change-Id: I15ffd1e046ff7443582fe8adbb09c93e436e4260

--- modules/icinga/manifests/init.pp
+++ modules/icinga/manifests/init.pp
@@ -29,11 +29,6 @@ class icinga($enable_notifications=1) {
         ensure => 'present',
     }
 
-    # for nrpe checks to run per T110893
-    package { 'libssl0.9.8':
-        ensure => present,
-    }
-
     # Setup icinga custom init script
     # FIXME: This should be provided by the package
     file { '/etc/init.d/icinga':
