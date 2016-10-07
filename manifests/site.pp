ff1ad0b7fd64873ab45d57084a4f24af523058eb
Daniel Zahn <dzahn@wikimedia.org>
2016-10-07 10:15:03 -0700

    gerrit: add rsyncd on cobalt for migrating data
    Add rsyncd, an rsync module to sync /srv/gerrit and
    the needed firewall hole on cobalt to sync gerrit data
    over from lead.
    
    Bug: T147597
    Change-Id: I984397882be61e6e3d8b87b42586cc33d063168d

--- manifests/site.pp
+++ manifests/site.pp
@@ -258,6 +258,7 @@ node 'chromium.wikimedia.org' {
 # gerrit warm stand-by (T147597)
 node 'cobalt.wikimedia.org' {
     # role(gerrit::server)
+    role(gerrit::migration)
     include standard
     include base::firewall
     include role::backup::host
---
+++ modules/role/manifests/gerrit/migration.pp
@@ -0,0 +1,19 @@
+class role::gerrit::migration {
+
+    $sourceip='208.80.154.82'
+
+    ferm::service { 'gerrit-migration-rysnc':
+        proto  => 'tcp',
+        port   => '873',
+        srange => "${sourceip}/32",
+    }
+
+    include rsync::server
+
+    rsync::server::module { 'srv-gerrit':
+        path        => '/srv/gerrit',
+        read_only   => 'no',
+        hosts_allow => $sourceip,
+    }
+
+}
