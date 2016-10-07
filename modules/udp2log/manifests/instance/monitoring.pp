83af851fb881179316df9c64c3176adee9c6799b
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-10-07 13:24:43 +0300

    udp2log: Remove the $monitor_packet_loss and $packet_loss_log parameters
    There is but one caller of udp2log instance and it does not specify the
    $packet_loss_log parameter and specifies $monitor_packet_loss as false.
    That latter parameter is passed verbatim to from udp2log::instance to
    udp2log::instance::monitoring and is only used in one if clause which
    clearly does not ever happen in our environment. Killing all this
    functionality which is never used means less code to maintain and also
    removes code dependent on monitoring::ganglia which we also want to
    remove since we are deprecating ganglia
    
    Change-Id: Ic72d58410556727b1b3743d26b70f7d340e32635

--- modules/udp2log/manifests/instance/monitoring.pp
+++ modules/udp2log/manifests/instance/monitoring.pp
@@ -10,8 +10,6 @@
 define udp2log::instance::monitoring(
     $log_directory       = '/var/log/udp2log',
     $ensure              = 'running',
-    $packet_loss_log     = undef,
-    $monitor_packet_loss = true,
     $monitor_processes   = true,
     $monitor_log_age     = true,
 ) {
@@ -21,7 +19,6 @@ define udp2log::instance::monitoring(
     # There are 3 ways udp2log instances are currently defined:
     # - Check age of udp2log files.
     # - Check that udp2log filter processes are running.
-    # - Check the packet-loss log file for execessive packet loss.
     # These different monitors are enabled or disabled using
     # their corresponding $monitor_xxxxx arguments passed into
     # this class.
@@ -50,56 +47,6 @@ define udp2log::instance::monitoring(
             nrpe_command  => "/usr/lib/nagios/plugins/check_udp2log_procs ${name}",
             contact_group => 'admins,analytics',
             retries       => 10,
-        }
-    }
-    # TODO else ensure absent,
-    # can't do this right now due to missing dependencies
-
-    # Monitor packet loss using the $packet_loss_log.
-    # This requires that filters.$name.erb has a
-    # packet-loss filter defined and outputting
-    # to $packet_loss_log_file.
-    if ($monitor_packet_loss == true and $ensure == 'running') {
-        $ensure_monitor_packet_loss = 'present'
-    }
-    else {
-        $ensure_monitor_packet_loss = 'absent'
-    }
-
-    # The packet loss file by default is in
-    # $log_directory/packet-loss.log.  If it was
-    # passed in explicitly, then use the value given.
-    $packet_loss_log_file = $packet_loss_log ? {
-        undef   => "${log_directory}/packet-loss.log",
-        default => $packet_loss_log,
-    }
-
-    if ($monitor_packet_loss == true) {
-        # Set up a cron to tail the packet loss log for this
-        # instance into ganglia.
-        cron { "ganglia-logtailer-udp2log-${name}":
-            ensure  => $ensure_monitor_packet_loss,
-            command => "/usr/sbin/ganglia-logtailer --classname PacketLossLogtailer --log_file ${packet_loss_log_file} --mode cron >> /var/log/ganglia/ganglia-logtailer.log 2>&1 ",
-            user    => 'root',
-            minute  => '*/5',
-        }
-
-        # Set up nagios monitoring of packet loss
-        # for this udp2log instance.
-        monitoring::ganglia{ "udp2log-${name}-packetloss":
-            ensure                => $ensure_monitor_packet_loss,
-            description           => 'Packetloss_Average',
-            metric                => 'packet_loss_average',
-            warning               => '4',
-            critical              => '8',
-            contact_group         => 'admins,analytics',
-            # ganglia-logtailer only runs every 5.
-            # let's make nagios check every 2 minutes (to match ganglia_parser)
-            # and retry 4 times (total 8 minutes) before
-            # declaring a hard failure.
-            normal_check_interval => 2,
-            retry_check_interval  => 2,
-            retries               => 4,
         }
     }
     # TODO else ensure absent,
