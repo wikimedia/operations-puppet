d2e2425916a095e947a546226f23adda6201ad28
Antoine Musso <hashar@free.fr>
2016-10-07 22:43:23 +0200

    (WIP) contint: Sonatype Nexus (WIP)
    Bug: T147635
    Change-Id: I23c1bd2d007d30517d9b987267e9ad58e660c04f

---
+++ modules/nexus/manifests/init.pp
@@ -0,0 +1,39 @@
+class nexus (
+    $data_dir
+) {
+
+    require_package('openjdk-8-jre-headless')
+
+    group { 'nexus':
+        ensure => present,
+    }
+
+    user { 'nexus':
+        home       => '/var/lib/nexus',
+        managehome => true,
+        system     => true,
+    }
+
+    exec { 'create nexus data_dir':
+        command => "/bin/mkdir -p ${data_dir}",
+        creates => $data_dir,
+    }
+    file { $data_dir:
+        ensure  => directory,
+        owner   => 'nexus',
+        group   => 'nexus',
+        require => Exec['create nexus data_dir'],
+    }
+
+    # Where the .tar.gz is uncompressed
+    file { '/var/lib/nexus/repo':
+        ensure => directory,
+        owner  => 'nexus',
+        group  => 'nexus',
+    }
+
+    file { '/var/lib/nexus/repo/bin/nexus.vmoptions':
+        content => template('nexus/bin/nexus.vmoptions.erb'),
+    }
+
+}
