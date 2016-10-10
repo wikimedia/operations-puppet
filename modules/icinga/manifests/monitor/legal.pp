11c76e5fd01a8c43f7f1f89e84dd6ec02c519821
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-10-10 16:24:45 +0300

    icinga: retry_check_interval => retry_interval
    The directive has changed name in newer versions and the former is
    deprecated
    
    Change-Id: Ie19d6cc4ed6315f56fcc62ffa276e51746ac7b5c

--- modules/icinga/manifests/monitor/legal.pp
+++ modules/icinga/manifests/monitor/legal.pp
@@ -15,29 +15,29 @@ class icinga::monitor::legal {
     }
 
     monitoring::service { 'en.wp.o-legal-html':
-        description          => 'Ensure legal html en.wp',
-        check_command        => 'check_legal_html!https://en.wikipedia.org/wiki/Main_Page!desktop_enwp',
-        host                 => 'en.wikipedia.org',
-        check_interval       => 1440,
-        retry_check_interval => 30,
-        contact_group        => 'admins,legal',
+        description    => 'Ensure legal html en.wp',
+        check_command  => 'check_legal_html!https://en.wikipedia.org/wiki/Main_Page!desktop_enwp',
+        host           => 'en.wikipedia.org',
+        check_interval => 1440,
+        retry_interval => 30,
+        contact_group  => 'admins,legal',
     }
 
     monitoring::service { 'en.m.wp.o-legal-html':
-        description          => 'Ensure legal html en.m.wp',
-        check_command        => 'check_legal_html!https://en.m.wikipedia.org/wiki/Main_Page!mobile',
-        host                 => 'en.m.wikipedia.org',
-        check_interval       => 1440,
-        retry_check_interval => 30,
-        contact_group        => 'admins,legal',
+        description    => 'Ensure legal html en.m.wp',
+        check_command  => 'check_legal_html!https://en.m.wikipedia.org/wiki/Main_Page!mobile',
+        host           => 'en.m.wikipedia.org',
+        check_interval => 1440,
+        retry_interval => 30,
+        contact_group  => 'admins,legal',
     }
 
     monitoring::service { 'en.wb.o-legal-html':
-        description          => 'Ensure legal html en.wb',
-        check_command        => 'check_legal_html!https://en.wikibooks.org!desktop_enwb',
-        host                 => 'en.wikibooks.org',
-        check_interval       => 1440,
-        retry_check_interval => 30,
-        contact_group        => 'admins,legal',
+        description    => 'Ensure legal html en.wb',
+        check_command  => 'check_legal_html!https://en.wikibooks.org!desktop_enwb',
+        host           => 'en.wikibooks.org',
+        check_interval => 1440,
+        retry_interval => 30,
+        contact_group  => 'admins,legal',
     }
 }
