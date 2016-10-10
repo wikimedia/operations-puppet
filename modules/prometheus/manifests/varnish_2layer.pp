28ae1e3ad543145b36cfb1ae855afc7bd1689380
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-10-10 15:40:02 +0100

    [WIP] prometheus: generate Varnish targets
    Bug: T147424
    Change-Id: I45ef6f6ba0b5aebb1abcf8d0120051f4e83871a3

---
+++ modules/prometheus/manifests/varnish_2layer.pp
@@ -0,0 +1,23 @@
+# Generate Prometheus targets configuration for varnish frontend and backend
+# instances, using data from get_clusters().
+
+define prometheus::varnish_2layer(
+    $targets_path,
+    $cache_name,
+) {
+    prometheus::varnish_config{ $cache_name:
+        dest    => "${targets_path}/varnish-${cache_name}_${::site}_backend.yaml",
+        site    => $::site,
+        cluster => "varnish_${cache_name}",
+        port    => '9331',
+        labels  => {'layer' => 'backend' },
+    }
+
+    prometheus::varnish_config{ $cache_name:
+        dest    => "${targets_path}/varnish-${cache_name}_${::site}_frontend.yaml",
+        site    => $::site,
+        cluster => "varnish_${cache_name}",
+        port    => '9131',
+        labels  => {'layer' => 'frontend' },
+    }
+}
