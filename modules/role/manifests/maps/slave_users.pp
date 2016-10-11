01d7418c811ec4901f6c8f57a58df60fc44c69a0
Guillaume Lederrey <glederrey@wikimedia.org>
2016-10-11 16:01:23 +0200

    Maps - cleanup postgres user creation
    Previous hiera configuration contained some duplication (the attributes of
    replication users are all the same) and failed to expose higher level
    abstraction. This change allow to just have a list of the postgresql slaves
    and their IP address in hiera, and treat other parameters as our puppet
    model, not as configuration.
    
    Previous hiera structure:
    
      maps::postgres_slaves:
        replication@maps-test2002-v4:
          user: replication
          password: 123456
          cidr: 4.3.2.1/32
          pgversion: 9.4
          attrs: REPLICATION
          database: replication
        replication@maps-test2003-v4:
          user: replication
          password: 123456
          cidr: 4.3.2.2/32
          pgversion: 9.4
          attrs: REPLICATION
          database: replication
    
    New hiera structure:
    
      maps::postgresql_replication_pass: 123456
      maps::postgresql_monitoring_pass: 654321
      maps::postgres_slaves:
        maps-test2002:
          ip_address: 4.3.2.1
        maps-test2003:
          ip_address: 4.3.2.2
    
    The passwords need to stay in private repo, but most of the slave
    configuration can now be moved in the standard hiera files.
    
    The hiera configuration in private repo needs to be adapted before merging
    this change.
    
    Bug: T147194
    Change-Id: I7fa7b5cc202dd17de007bb0f4e10ffefa7187c1b

---
+++ modules/role/manifests/maps/slave_users.pp
@@ -0,0 +1,25 @@
+# Create users for PostgreSQL Replication / Monitoring
+# This inner define should probably be replaced by iterator once we activate future parser
+define role::maps::postgresql_slave_users(
+    $ip_address,
+    $replication_pass,
+    $monitoring_pass,
+    $pg_version,
+) {
+    ::postgresql::user { "replication@${title}":
+        user      => 'replication',
+        password  => $replication_pass,
+        cidr      => "${ip_address}/32",
+        pgversion => $pg_version,
+        attrs     => 'REPLICATION',
+        database  => 'replication',
+    }
+    ::postgresql::user { "icinga@${title}":
+        user      => 'icinga',
+        password  => $monitoring_pass,
+        cidr      => "${ip_address}/32",
+        pgversion => $pg_version,
+        database  => 'all',
+    }
+}
+
