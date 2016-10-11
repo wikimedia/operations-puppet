789ccdeb02f5ad0e49efb6a996d982fbc4e43270
YuviPanda <yuvipanda@gmail.com>
2016-10-11 12:18:15 -0700

    authdns: Move roles to module
    Change-Id: Iea37a0c308fa4d20578394eb070d87c782b2d23b

---
+++ modules/role/manifests/authdns/testns.pp
@@ -0,0 +1,9 @@
+# For deploying the basic software config without participating in the full
+# role for e.g. public addrs, monitoring, authdns-update, etc.
+class role::authdns::testns {
+    include role::authdns::data
+    class { 'authdns':
+        gitrepo    => $role::authdns::data::gitrepo,
+        monitoring => false,
+    }
+}
