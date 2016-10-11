3633dc8b54e1da009b137f348fcff5a5ae14d818
YuviPanda <yuvipanda@gmail.com>
2016-10-11 12:50:41 -0700

    Move logging.pp roles into role module
    Also remove use of inheritance with include, to satisfy puppetlint
    
    Change-Id: Iebe43ac87b13eec6b782e3665638eeca68734f71

---
+++ modules/role/manifests/logging/mediawiki.pp
@@ -0,0 +1,112 @@
+# mediawiki udp2log instance.  Does not use monitoring.
+class role::logging::mediawiki(
+    $monitor = true,
+    $log_directory = '/srv/mw-log',
+    $rotate = 1000,
+) {
+    system::role { 'role::logging:mediawiki':
+        description => 'MediaWiki log collector',
+    }
+
+    include base::firewall
+
+    # Rsync archived slow-parse logs to dumps.wikimedia.org.
+    # These are available for download at http://dumps.wikimedia.org/other/slow-parse/
+    include ::dataset::user
+    cron { 'rsync_slow_parse':
+        command     => "/usr/bin/rsync -rt ${log_directory}/archive/slow-parse.log*.gz dumps.wikimedia.org::slow-parse/",
+        hour        => 23,
+        minute      => 15,
+        environment => 'MAILTO=ops-dumps@wikimedia.org',
+        user        => 'datasets',
+    }
+
+    class { 'udp2log':
+        monitor          => $monitor,
+        default_instance => false,
+    }
+
+    ferm::rule { 'udp2log_accept_all_wikimedia':
+        rule => 'saddr ($DOMAIN_NETWORKS) proto udp ACCEPT;',
+    }
+
+    ferm::rule { 'udp2log_notrack':
+        table => 'raw',
+        chain => 'PREROUTING',
+        rule  => 'saddr ($DOMAIN_NETWORKS) proto udp NOTRACK;',
+    }
+
+    file { '/usr/local/bin/demux.py':
+        mode   => '0544',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/udp2log/demux.py',
+    }
+
+    $logstash_host = $::realm ? {
+        # TODO: Find a way to use multicast that doesn't cause duplicate
+        # messages to be stored in logstash. This is a SPOF.
+        production => 'logstash1001.eqiad.wmnet',
+        labs       => 'deployment-logstash2.deployment-prep.eqiad.wmflabs',
+    }
+
+    $logstash_port = 8324
+
+    udp2log::instance { 'mw':
+        log_directory      =>    $log_directory,
+        monitor_log_age    =>    false,
+        monitor_processes  =>    false,
+        rotate             =>    $rotate,
+        template_variables => {
+            error_processor_host => 'eventlog1001.eqiad.wmnet',
+            error_processor_port => 8423,
+
+            # forwarding to logstash
+            logstash_host        => $logstash_host,
+            logstash_port        => $logstash_port,
+        },
+    }
+
+    # Allow rsyncing of udp2log generated files to
+    # analysis hosts.
+    class { 'udp2log::rsyncd':
+        path => $log_directory,
+    }
+
+    cron { 'mw-log-cleanup':
+        command => '/usr/local/bin/mw-log-cleanup',
+        user    => 'root',
+        hour    => 2,
+        minute  => 0
+    }
+
+    # move files to module?
+    # lint:ignore:puppet_url_without_modules
+    file { '/usr/local/bin/mw-log-cleanup':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/role/logging/mw-log-cleanup',
+    }
+
+    file { '/usr/local/bin/exceptionmonitor':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        content => template('role/logging/exceptionmonitor.erb'),
+    }
+
+    file { '/etc/profile.d/mw-log.sh':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        content => "MW_LOG_DIRECTORY=${log_directory}\n",
+    }
+
+    file { '/usr/local/bin/fatalmonitor':
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/role/logging/fatalmonitor',
+    }
+}
