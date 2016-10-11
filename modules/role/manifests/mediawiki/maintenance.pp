179bee3aa7fb259972a8715a3cc0aa553b5b2d4b
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2016-10-11 15:38:03 +0200

    Provide PHP packages for mwscript
    On jessie we're restricting PHP packages to a bare minimum since it's
    unused on the app servers, but on the maintenance servers PHP is still
    needed for mwscript.
    
    Bug: T146286
    Change-Id: I4cc6d06681df3bb56fb6f025080f0ce322e7b77e

--- modules/role/manifests/mediawiki/maintenance.pp
+++ modules/role/manifests/mediawiki/maintenance.pp
@@ -3,6 +3,8 @@ class role::mediawiki::maintenance {
     include scap::scripts
     include role::mediawiki::common
 
+    include ::mediawiki::packages::php5
+
     file { $::mediawiki::scap::mediawiki_staging_dir:
         ensure => link,
         target => '/srv/mediawiki'
