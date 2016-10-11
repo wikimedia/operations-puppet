88a5a7c39fb64b4017731bfe76ba8162c10ecaea
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-10-11 15:15:34 +0300

    icinga: Remove the last vestiges of hostextinfo
    No longer used by anything, remove them
    
    Change-Id: I91548b0ffeadcc8dc6601d8bf91c499e8d48f92b

--- modules/monitoring/manifests/decommission_monitor_host.pp
+++ modules/monitoring/manifests/decommission_monitor_host.pp
@@ -6,18 +6,10 @@ define monitoring::decommission_monitor_host {
         Nagios_host <| title == $title |> {
             ensure => absent
         }
-        Nagios_hostextinfo <| title == $title |> {
-            ensure => absent
-        }
     }
     else {
         # Resources don't exist in Puppet. Remove from Nagios config as well.
         nagios_host { $title:
-            ensure    => absent,
-            host_name => $title,
-
-        }
-        nagios_hostextinfo { $title:
             ensure    => absent,
             host_name => $title,
 
