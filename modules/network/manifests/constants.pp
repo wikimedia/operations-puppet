1060e5621a64cb7bf5e8cebaf86db081d98099dd
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-10-11 16:14:49 +0300

    monitoring_hosts: Add tegmen/einsteinium
    Adding tegmen and and einsteinium in the monitoring hosts alongside
    neon. They will take over in an active/passive scenario. Also remove
    neon's SLAAC since it is no longer set on the interface anyway
    
    Change-Id: I75e5a10d426707add4c06a41ce18e5fba1c899ee

--- modules/network/manifests/constants.pp
+++ modules/network/manifests/constants.pp
@@ -45,7 +45,10 @@ class network::constants {
             'monitoring_hosts' => [
                     '208.80.154.14',                    # neon.wikimedia.org
                     '2620:0:861:1:208:80:154:14',       # neon.wikimedia.org
-                    '2620:0:861:1:7a2b:cbff:fe08:a42f', # neon.wikimedia.org SLAAC
+                    '208.80.153.74',                    # tegmen.wikimedia.org
+                    '2620:0:860:3:208:80:153:74/64',    # tegmen.wikimedia.org
+                    '208.80.155.119',                   # einsteinium.wikimedia.org
+                    '2620:0:860:3:208:80:155:119/64',   # einsteinium.wikimedia.org
                     '208.80.154.53',                    # uranium.wikimedia.org (ganglia, gmetad needs it)
                     '2620:0:861:1:208:80:154:53',       # uranium.wikimedia.org
                 ],
