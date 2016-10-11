3ab2fa6d967282f8cb807eace8792ac882e656f9
YuviPanda <yuvipanda@gmail.com>
2016-10-11 13:31:11 -0700

    Move wdq_mm roles to role module
    Change-Id: Ib56236b0b28fa8f64f45e53fdb46550429f4fa64

--- modules/role/manifests/wdq_mm/server.pp
+++ modules/role/manifests/wdq_mm/server.pp
@@ -1,7 +1,7 @@
-# == Class: role::wdq_mm
+# == Class: role::wdq_mm::server
 # Role class for setting up an instance of
 # Magnus' WDQ on labs
-class role::wdq_mm {
+class role::wdq_mm::server {
     requires_realm('labs')
 
     include ::labs_debrepo
@@ -17,15 +17,5 @@ class role::wdq_mm {
             Class['::labs_debrepo'],
             Labs_lvm::Volume['instance-storage'],
         ]
-    }
-}
-
-# == Class: role::wdq_mm::lb
-# Load balancer for balancing across multiple instances
-# of role::labs::wdq_mm
-class role::wdq_mm::lb {
-    requires_realm('labs')
-
-    class { '::wdq_mm::lb':
     }
 }
