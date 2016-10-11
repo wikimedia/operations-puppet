14d2abe903e1f05d02c98f784935240dc1949ebe
YuviPanda <yuvipanda@gmail.com>
2016-10-11 12:18:15 -0700

    authdns: Move roles to module
    Change-Id: Iea37a0c308fa4d20578394eb070d87c782b2d23b

---
+++ modules/role/manifests/authdns/monitoring.pp
@@ -0,0 +1,5 @@
+# This is for the monitoring host to monitor the shared public addrs
+class role::authdns::monitoring {
+    include role::authdns::data
+    create_resources(authdns::monitoring::global, $role::authdns::data::ns_addrs)
+}
