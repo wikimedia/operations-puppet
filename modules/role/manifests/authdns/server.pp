81f367a3a96aa16452b41874758848ad0daf5569
YuviPanda <yuvipanda@gmail.com>
2016-10-11 12:18:15 -0700

    authdns: Move roles to module
    Change-Id: Iea37a0c308fa4d20578394eb070d87c782b2d23b

---
+++ modules/role/manifests/authdns/server.pp
@@ -0,0 +1,37 @@
+# This is for an authdns server to use
+class role::authdns::server {
+    system::role { 'authdns': description => 'Authoritative DNS server' }
+
+    include base::firewall
+    include authdns::ganglia
+    include role::authdns::data
+
+    create_resources(
+        interface::ip,
+        $role::authdns::data::ns_addrs,
+        { interface => 'lo' }
+    )
+
+    class { 'authdns':
+        nameservers => $role::authdns::data::nameservers,
+        gitrepo     => $role::authdns::data::gitrepo,
+    }
+
+    ferm::service { 'udp_dns_auth':
+        proto   => 'udp',
+        notrack => true,
+        port    => '53',
+    }
+
+    ferm::service { 'tcp_dns_auth':
+        proto => 'tcp',
+        port  => '53',
+    }
+
+    $authdns_ns_ferm = join($role::authdns::data::nameservers, ' ')
+    ferm::service { 'authdns_update_ssh':
+        proto  => 'tcp',
+        port   => '22',
+        srange => "(@resolve((${authdns_ns_ferm})) @resolve((${authdns_ns_ferm}), AAAA))",
+    }
+}
