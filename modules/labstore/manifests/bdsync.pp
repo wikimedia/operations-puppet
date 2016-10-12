d9a24ba6ab23e470b12af423a4592536436974f1
cpettet <rush@wikimedia.org>
2016-10-12 17:39:36 -0500

    bdsync backup setup for labstore
    Change-Id: I67cb9b5579788ae4542f86646d01f502f95733df

---
+++ modules/labstore/manifests/bdsync.pp
@@ -0,0 +1,14 @@
+class labstore::bdsync {
+
+    package { 'bdsync':
+        ensure => present,
+        before => File['/usr/local/sbin/block_sync'],
+    }
+
+    file { '/usr/local/sbin/block_sync':
+        source => 'puppet:///modules/labstore/block_sync.sh',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+}
