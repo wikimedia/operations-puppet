d60645bef6b60d3803ba38c5914f2331650edb36
cpettet <rush@wikimedia.org>
2016-10-12 17:39:36 -0500

    bdsync backup setup for labstore
    Change-Id: I67cb9b5579788ae4542f86646d01f502f95733df

---
+++ modules/labstore/manifests/device_backup.pp
@@ -0,0 +1,31 @@
+define labstore::device_backup (
+    $remotehost,
+    $remotedev,
+    $localdev,
+    $weekday,
+    $hour=0,
+) {
+
+    include labstore::bdsync
+    $remote_ip = ipresolve($remotehost, 4)
+
+    $day = {
+        'sunday'    => 0,
+        'monday'    => 1,
+        'tuesday'   => 2,
+        'wednesday' => 3,
+        'thursday'  => 4,
+        'friday'    => 5,
+        'saturday'  => 6,
+    }
+    
+    # Establish what list these alerts should go to
+    # environment => 'MAILTO=labs-admins@wikimedia.org',
+    cron { "bdsync-${remotehost}-${remotedev}":
+        ensure  => 'present',
+        user    => 'root',
+        command => "/usr/local/sbin/block_sync ${remote_ip} ${remotedev} ${localdev}",
+        weekday => $day[$weekday],
+        hour    => $hour,
+    }
+}
