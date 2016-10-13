e4131219c2739fbb58c3152fc75d2738ea7ab0e3
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-10-13 20:14:02 +0200

    kubernetes: introduce 1st-stage worker role
    For now, just the docker installation is provided, via a
    profile::docker::engine that can be potentially reused for labs as well.
    
    We also add a simple partman recipe that should work well with this
    role.
    
    Bug: T147181
    Change-Id: I213286e00e0e2fb1c9398d734b0bbc0a55ced9f9

---
+++ modules/profile/manifests/docker/engine.pp
@@ -0,0 +1,90 @@
+# == Profile docker::engine
+#
+# Installs docker, along with setting up the volume group needed for the
+# devicemapper storage driver to work.
+# to work
+class profile::docker::engine {
+    # Parameters that need to be defined in hiera
+    # Volume group to substitute
+    $vg_to_remove = hiera('profile::docker::engine::vg_to_remove', undef)
+    $physical_volumes = hiera('profile::docker::engine::physical_volumes')
+
+    # Optional parameters
+    $docker_settings = hiera('profile::docker::engine::settings', {})
+    # Size of the thin pool and the metadata pool.
+    $lv_extents = hiera('profile::docker::engine::lv_extents', '95%VG')
+    $pool_metadata_size = hiera('profile::docker::engine::lvm_metadata_size', undef)
+    # Version to install; the default is not to pick one.
+    $docker_version = hiera('profile::docker::engine::version', 'present')
+    $apt_proxy_settings = hiera('profile::docker::engine::proxy', undef)
+    $service_ensure = hiera('profile::docker::engine::service', 'running')
+
+    # Install docker
+    class { 'docker':
+        version        => $docker_version,
+        proxy_settings => $apt_proxy_settings
+    }
+
+    # Storage
+    if $vg_to_remove {
+        volume_group { $vg_to_remove:
+            ensure           => absent,
+            physical_volumes => [],
+        }
+    }
+    $basic_lv_params = {
+        extents  => $lv_extents,
+        thinpool => true,
+    }
+
+    $lv_params = $pool_metadata_size ? {
+        undef   => $basic_lv_params,
+        default => merge($basic_lv_params, {'poolmetadatasize' => $pool_metadata_size}),
+    }
+
+    $logical_volumes = {
+        'thinpool'     => $lv_params,
+    }
+
+    lvm::volume_group { 'docker':
+        ensure           => present,
+        physical_volumes => $physical_volumes,
+        logical_volumes  => $logical_volumes,
+    }
+
+    file { '/etc/lvm/profile/docker-thinpool.profile':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/profile/docker/lvm.profile',
+    }
+
+    exec { 'Attach profile to docker thinpool':
+        command => '/sbin/lvchange --metadataprofile docker-thinpool docker/thinpool',
+        unless  => '/sbin/lvs -o lv_profile docker/thinpool | grep -q docker',
+        require => [
+            File['/etc/lvm/profile/docker-thinpool.profile'],
+            Logical_volume['thinpool']
+        ],
+    }
+
+    $docker_storage_options = {
+        'storage-driver' => 'devicemapper',
+        'storage-opts'   =>  [
+            'dm.thinpooldev=/dev/mapper/docker-thinpool',
+            'dm.use_deferred_removal=true',
+            'dm.use_deferred_deletion=true'
+        ]
+    }
+
+
+    # Docker config
+    docker::config { 'thinpool':
+        settings => merge($docker_settings, $docker_storage_options),
+    }
+
+    # Service declaration
+    service { 'docker':
+        ensure => $service_ensure,
+    }
+}
