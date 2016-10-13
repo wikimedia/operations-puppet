78865e3c7a972209e0439dbcc3a4ccd276ea2dfc
Guillaume Lederrey <glederrey@wikimedia.org>
2016-10-13 11:44:17 +0200

    wdqs - move monitoring of response time to service, not individual hosts
    As varnish is now configured to use the wdqs LVS service, monitoring needs
    to be adapted as well.
    
    At this point, only the eqiad service is monitored as the codfw service
    does not receive traffic, and thus response times are meaningless.
    
    Bug: T148015
    Change-Id: Ifc86e4b60e8a67bb03e648271c8ffea0bbdf4551

---
+++ modules/icinga/manifests/monitor/wdqs.pp
@@ -0,0 +1,17 @@
+# Monitor Wikidata query service
+class icinga::monitor::wdqs {
+
+    # raise a warning / critical alert if response time was over 2 minutes / 5 minutes
+    # more than 5% of the time during the last minute
+    monitoring::graphite_threshold { 'wdqs-response-time':
+        description   => 'Response time of WDQS',
+        host          => 'wdqs.svc.eqiad.wmnet',
+        metric        => "varnish.eqiad.backends.be_wdqs_svc_eqiad_wmnet.GET.p99",
+        warning       => 120000, # 2 minutes
+        critical      => 300000, # 5 minutes
+        from          => '10min',
+        percentage    => 5,
+        contact_group => 'wdqs-admins',
+    }
+
+}
--- modules/wdqs/manifests/monitor/blazegraph.pp
+++ modules/wdqs/manifests/monitor/blazegraph.pp
@@ -26,18 +26,5 @@ class wdqs::monitor::blazegraph {
         source   => 'puppet:///modules/wdqs/monitor/blazegraph.py',
     }
 
-    # raise a warning / critical alert if response time was over 2 minutes / 5 minutes
-    # more than 5% of the time during the last minute
-    $sanitized_hostname = regsubst($::fqdn, '\.', '_', 'G')
-    monitoring::graphite_threshold { 'wdqs-response-time':
-        description   => 'Response time of WDQS',
-        metric        => "varnish.eqiad.backends.be_${sanitized_hostname}.GET.p99",
-        warning       => 120000, # 2 minutes
-        critical      => 300000, # 5 minutes
-        from          => '10min',
-        percentage    => 5,
-        contact_group => 'wdqs-admins',
-    }
-
     # TODO: add monitoring of the http and https endpoints, and of the service
 }
