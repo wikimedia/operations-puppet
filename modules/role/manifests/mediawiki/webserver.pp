a84607967901984308a2b84cd543f1c92ee1ee26
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-10-14 13:45:56 +0200

    role::mediawiki::webserver: restart hhvm routinely
    This cron will restart HHVM if it's running since more than 3 days.
    
    Bug: T147773
    Change-Id: I204331607ba80169fafdd205ba9bffeeabf9a443

--- modules/role/manifests/mediawiki/webserver.pp
+++ modules/role/manifests/mediawiki/webserver.pp
@@ -12,10 +12,30 @@ class role::mediawiki::webserver {
     }
 
     if hiera('has_lvs', true) {
+        include ::lvs::configuration
         include ::role::lvs::realserver
 
         # Conftool config
         include ::mediawiki::conftool
+
+        # Restart HHVM if it is running since more than 3 days or
+        # memory occupation exceeds 50% of the available RAM
+        # This should prevent a series of cpu usage surges we've been seeing
+        # on long-running HHVM processes. T147773
+        $pool = $::role::lvs::realserver::lvs_pools['hhvm']['lvs_name']
+        $lvs_service = pick($::lvs::configuration::lvs_services[$pool], {})
+        $conftool_config = pick($lvs_service['conftool'], {'cluster' => 'appserver'})
+        $module_path = get_module_path($module_name)
+        $site_nodes = loadyaml("${module_path}/../../conftool-data/nodes/${::site}.yaml")
+        $pool_nodes = keys($site_nodes[$conftool_config['cluster']])
+        if member($pool_nodes, $::fqdn) {
+            $times = cron_splay($pool_nodes, 'daily', 'hhvm-conditional-restarts')
+            cron { 'hhvm-conditional-restart':
+                command => '/usr/local/bin/hhvm-needs-restart && /usr/local/bin/run-no-puppet /usr/local/bin/restart-hhvm > /dev/null',
+                hour    => $times['hour'],
+                minute  => $times['minute'],
+            }
+        }
     }
 
     ferm::service { 'mediawiki-http':
