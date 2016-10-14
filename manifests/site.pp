a881a28facde2e4e8fedbf2787818fbb0c28cd50
Daniel Zahn <dzahn@wikimedia.org>
2016-10-13 18:04:15 -0700

    maps::server: move base::firewall to role
    Move the "base::firewall" from node level to role,
    because all nodes have it and should have it in the future
    and it simplifies site.pp.
    
    Change-Id: Iec2455ececf663d1f073d1e7640e8314f04abb28

--- manifests/site.pp
+++ manifests/site.pp
@@ -1923,32 +1923,26 @@ node 'maerlant.wikimedia.org' {
 
 node 'maps-test2001.codfw.wmnet' {
     role(maps::server, maps::master, prometheus::node_exporter)
-    include base::firewall
 }
 
 node /^maps-test200[2-4]\.codfw\.wmnet/ {
     role(maps::server, maps::slave, prometheus::node_exporter)
-    include base::firewall
 }
 
 node 'maps1001.eqiad.wmnet' {
     role(maps::server, maps::master)
-    include base::firewall
 }
 
 node /^maps100[2-4]\.eqiad\.wmnet/ {
     role(maps::server, maps::slave)
-    include base::firewall
 }
 
 node 'maps2001.codfw.wmnet' {
     role(maps::server, maps::master)
-    include base::firewall
 }
 
 node /^maps200[2-4]\.codfw\.wmnet/ {
     role(maps::server, maps::slave)
-    include base::firewall
 }
 
 node /^mc(10[01][0-9])\.eqiad\.wmnet/ {
