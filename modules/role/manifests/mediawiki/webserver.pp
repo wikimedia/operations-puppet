b2e01fcfc7ccd3331cfae1afe6f7869547c92ab1
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-10-14 13:45:56 +0200

    role::mediawiki::webserver: restart hhvm routinely
    This cron will restart HHVM if it's running since more than 3 days.
    
    Bug: T147773
    Change-Id: I204331607ba80169fafdd205ba9bffeeabf9a443

--- modules/role/manifests/mediawiki/webserver.pp
+++ modules/role/manifests/mediawiki/webserver.pp
@@ -16,6 +16,19 @@ class role::mediawiki::webserver {
 
         # Conftool config
         include ::mediawiki::conftool
+
+        # Restart HHVM if it is running since more than 3 days.
+        # This should prevent a series of cpu usage surges we've been seeing
+        # on long-running HHVM processes. T147773
+        $conftool_config = $lvs::configuration::lvs_services['conftool']
+        $site_nodes = loadyaml("conftool-data/nodes/${::site}.yaml")
+        $pool_nodes = keys($site_nodes[$conftool_config['cluster']])
+        $times = cron_splay($pool_nodes, 'daily', 'hhvm-conditional-restarts')
+        cron { 'hhvm-conditional-restart':
+            command => '/usr/local/bin/hhvm-needs-restart && /usr/local/bin/run-no-puppet /usr/local/bin/restart-hhvm > /dev/null',
+            hour    => $times['hour'],
+            minute  => $times['minute'],
+        }
     }
 
     ferm::service { 'mediawiki-http':
