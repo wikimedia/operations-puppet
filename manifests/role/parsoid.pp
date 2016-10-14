d482bdf9d80a6ab6a339143ef1f54a74e9dbebd7
Marko Obrovac <mobrovac@wikimedia.org>
2016-10-14 14:43:33 +0300

    Parsoid: Install the conftool service scripts
    Bug: T145518
    Change-Id: Ic7920e7ab737dc0001e0abe3b5b379a581383273

--- manifests/role/parsoid.pp
+++ manifests/role/parsoid.pp
@@ -8,6 +8,13 @@ class role::parsoid {
     include base::firewall
     include lvs::realserver
 
+    # LVS pooling/depoling scripts
+    include ::lvs::configuration
+    conftool::scripts::service { 'parsoid':
+        lvs_services_config => $::lvs::configuration::lvs_services,
+        lvs_class_hosts     => $::lvs::configuration::lvs_class_hosts,
+    }
+
     include ::parsoid
     # Monitor TCP Connection States
     diamond::collector { 'TcpConnStates':
