459f5d112ec2ed5db7f75c4c4b0a0e9cd96373f6
Paladox <thomasmulhall410@yahoo.com>
2016-10-16 17:02:37 +0100

    contint: add postgresql to contint::packages
    this would be for these tickets:
    
    Jenkins: Set up PHPUnit testing on PostgreSQL backend)
    Jenkins: Run PHPUnit tests on MySQL, PostgreSQL and SQLite
    
    both are low priority though.
    
    Bug: T39602
    Bug: T22343
    Change-Id: I0cf75b7bd831055d2bcd71fc65078a1cbc028ed9

---
+++ modules/contint/manifests/packages.pp
@@ -0,0 +1,35 @@
+#
+# Holds all the packages needed for continuous integration.
+#
+# FIXME: split this!
+#
+class contint::packages {
+
+    # Basic utilites needed for all Jenkins slaves
+    include ::contint::packages::base
+
+    # We're no longer installing PHP on app servers starting with
+    # jessie, but we still need it for CI
+    if os_version('debian == jessie') {
+        include ::mediawiki::packages::php5
+
+        package { [
+            'postgresql',
+            'postgresql-contrib',
+            ]:
+            ensure => present,
+        }
+    }
+
+    require_package('openjdk-7-jdk')
+
+    # MediaWiki doc is built directly on contint1001
+    require_package('doxygen')
+
+    # For Doxygen based documentations
+    require_package('graphviz')
+
+    # VisualEditor syncing
+    require_package('python-requests')
+
+}
