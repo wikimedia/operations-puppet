a0d2955bbb1a543d706dc0ebf5aebfb33d453098
Daniel Zahn <dzahn@wikimedia.org>
2016-10-17 16:20:21 -0700

    tcpircbot: improve firewall rule setup
    - Replace ferm::rule with ferm::service.
    - Use @resolve instead of IP addresses.
    - resolve v4 and v6 addresses from DNS instead of having to list both
    - make everything more readable
    
    Also per comments on Ic7bf1903f8a04649ea7ec.
    
    Change-Id: Ia611b075d18a91630fd1700ddfcc8bc9fee64486

--- modules/role/manifests/tcpircbot.pp
+++ modules/role/manifests/tcpircbot.pp
@@ -1,4 +1,4 @@
-class role::tcpircbot($ensure='present') {
+class role::tcpircbot {
     include ::tcpircbot
     include passwords::logmsgbot
 
@@ -6,37 +6,47 @@ class role::tcpircbot($ensure='present') {
         description => 'tcpircbot server',
     }
 
-
-    if $ensure == 'present' {
-        nrpe::monitor_service { 'tcpircbot':
-            description  => 'tcpircbot_service_running',
-            nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -C python -a tcpircbot.py',
-        }
-        tcpircbot::instance { 'logmsgbot':
-            channels => '#wikimedia-operations',
-            password => $passwords::logmsgbot::logmsgbot_password,
-            cidr     => [
-                '::ffff:127.0.0.1/128',             # loopback
-                '::ffff:10.64.32.167/128',          # logging: eventlog1001
-                '::ffff:10.64.0.196/128',           # deployment eqiad v4: tin
-                '2620:0:861:101:10:64:0:196/128',   # deployment eqiad v6: tin
-                '::ffff:10.192.16.132/128',         # deployment codfw v4: mira
-                '2620:0:860:102:10:192:16:132/128', # deployment codfw v6: mira
-                '::ffff:10.64.32.13/128',           # maintenance eqiad v4: terbium
-                '2620:0:861:103:10:64:32:13/64',    # maintenance eqiad v6: terbium
-                '::ffff:10.192.48.45/128',          # maintenance codfw v4: wasat
-                '2620:0:860:104:10:192:48:45/64',   # maintenance codfw v6: wasat
-                '::ffff:10.64.16.73/128',           # puppetmaster1001.eqiad.wmnet
-                '2620:0:861:102:10:64:16:73/128',   # puppetmaster1001.eqiad.wmnet
-                '::ffff:10.192.0.27/128',           # puppetmaster2001.codfw.wmnet
-                '2620:0:860:101:10:192:0:27/128',   # puppetmaster2001.codfw.wmnet
-
-            ],
-        }
-        ferm::rule { 'tcpircbot_allowed':
-            # eventlog1001 (v4), tin (v4), mira (v4), puppetmaster1001 (v4), tin (v6), mira (v6), puppetmaster1001 (v6, unnamed in DNS), terbium (v4), terbium (v6), wasat (v4), wasat (v6), puppetmaster2001 (v4), puppetmaster2001 (v6, unnamed in DNS)
-            # Please DO NOT change the IPs in the rule below without updating the comment above
-            rule => 'proto tcp dport 9200 { saddr (10.64.32.167/32 10.64.0.196/32 10.192.16.132/32 10.64.16.73/32 2620:0:861:101:10:64:0:196/128 2620:0:860:102:10:192:16:132/128 2620:0:861:102:10:64:16:73/128 10.64.32.13/32 2620:0:861:103:10:64:32:13/64 10.192.48.45/32 2620:0:860:104:10:192:48:45/64 10.192.0.27/32 2620:0:860:101:10:192:0:27/128) ACCEPT; }',
-        }
+    nrpe::monitor_service { 'tcpircbot':
+        description  => 'tcpircbot_service_running',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -C python -a tcpircbot.py',
     }
+
+    tcpircbot::instance { 'logmsgbot':
+        channels => '#wikimedia-operations',
+        password => $passwords::logmsgbot::logmsgbot_password,
+        cidr     => [
+            '::ffff:127.0.0.1/128',             # loopback
+            '::ffff:10.64.32.167/128',          # logging: eventlog1001
+            '::ffff:10.64.0.196/128',           # deployment eqiad v4: tin
+            '2620:0:861:101:10:64:0:196/128',   # deployment eqiad v6: tin
+            '::ffff:10.192.16.132/128',         # deployment codfw v4: mira
+            '2620:0:860:102:10:192:16:132/128', # deployment codfw v6: mira
+            '::ffff:10.64.32.13/128',           # maintenance eqiad v4: terbium
+            '2620:0:861:103:10:64:32:13/64',    # maintenance eqiad v6: terbium
+            '::ffff:10.192.48.45/128',          # maintenance codfw v4: wasat
+            '2620:0:860:104:10:192:48:45/64',   # maintenance codfw v6: wasat
+            '::ffff:10.64.16.73/128',           # puppetmaster1001.eqiad.wmnet
+            '2620:0:861:102:10:64:16:73/128',   # puppetmaster1001.eqiad.wmnet
+            '::ffff:10.192.0.27/128',           # puppetmaster2001.codfw.wmnet
+            '2620:0:860:101:10:192:0:27/128',   # puppetmaster2001.codfw.wmnet
+
+        ],
+    }
+
+    $allowed_hosts = [
+        'eventlog1001.eqiad.wmnet',     # logging eqiad
+        'tin.eqiad.wmnet',              # deployment eqiad
+        'mira.codfw.wmnet',             # deployment codfw
+        'puppetmaster1001.eqiad.wmnet', # puppet eqiad
+        'puppetmaster2001.codfw.wmnet', # puppet codfw
+        'terbium.eqiad.wmnet',          # maintenance eqiad
+        'wasat.codfw.wmnet',            # maintenance codfw
+    ]
+
+    ferm::service { 'tcpircbot_allowed':
+        proto  => 'tcp',
+        port   => '9200',
+        srange => "(@resolve((${allowed_hosts})) @resolve((${allowed_hosts}), AAAA))",
+    }
+
 }
