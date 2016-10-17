2d088f62ef6e86a81d164a86e293bb461efef671
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-10-17 13:50:16 +0200

    role::scb: use role::lvs::realserver
    Change-Id: I10cc378aa0573d397e3703dfa3b28d560ac14ff0

--- manifests/role/graphoid.pp
+++ manifests/role/graphoid.pp
@@ -5,12 +5,5 @@ class role::graphoid {
         description => 'node.js service converting graph definitions into PNG'
     }
 
-    # LVS pooling/depoling scripts
-    include ::lvs::configuration
-    conftool::scripts::service { 'graphoid':
-        lvs_services_config => $::lvs::configuration::lvs_services,
-        lvs_class_hosts     => $::lvs::configuration::lvs_class_hosts,
-    }
-
     include ::graphoid
 }
