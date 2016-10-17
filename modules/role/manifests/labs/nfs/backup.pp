456c8398cdfb3ce99980a156e7c56d858e2da5f0
cpettet <rush@wikimedia.org>
2016-10-17 11:07:47 -0500

    labstore: bdsync backup 'test' drbd volume from secondary
    Change-Id: I0b4aa4b9a913049546516212f07a254c7917ad14
    Bugs: T127567

--- modules/role/manifests/labs/nfs/backup.pp
+++ modules/role/manifests/labs/nfs/backup.pp
@@ -3,4 +3,13 @@ class role::labs::nfs::backup {
         description => 'NFS shares backup dest',
     }
     include labstore::backup_keys
+
+    labstore::device_backup { 'secondary-test':
+        remotehost      => 'labstore1005.eqiad.wmnet',
+        remote_vg       => 'misc',
+        remote_lv       => 'test',
+        remote_snapshot => 'testsnap',
+        localdev        => '/dev/backup/test',
+        weekday         => 'monday',
+    }
 }
