0d2b1a6f1224b46a04b5ab97c674f32e627bad35
Guillaume Lederrey <glederrey@wikimedia.org>
2016-10-18 14:44:51 +0200

    service::node: Adding minimal test
    This does not add much value yet, but opens the way to writing more tests
    for the service module.
    
    Change-Id: I5e58d2d9efe9194ba6e4061a49a974384dfeb084

---
+++ modules/service/spec/defines/service_node_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'service::node', :type => :define do
+
+  let(:title) { 'my_service_name' }
+  let(:facts) { { :initsystem => 'systemd' } }
+
+  context 'when only port is given' do
+    let(:params) { { :port => 1234 } }
+
+    it 'create application config file' do
+      is_expected.to contain_file('/etc/my_service_name/config.yaml')
+    end
+    it 'contains a service named after the application' do
+      is_expected.to contain_file('/etc/my_service_name/config.yaml')
+                         .with_content(/name: my_service_name/)
+    end
+  end
+
+end
