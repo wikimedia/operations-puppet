e308ae2f4ed9b2f20895207bd32459ed0b801e0f
Jcrespo <jcrespo@wikimedia.org>
2016-10-18 15:29:41 +0200

    New labs::db::proxy role to load balance and failover replicas
    Bug: T148500
    Bug: T141097
    Bug: T148507
    Change-Id: I8f7c8b4da37a3f804e1034f337ff5fbced1b6c81

---
+++ modules/role/manifests/labs/db/proxy.pp
@@ -0,0 +1,38 @@
+# role::labs::db::proxy
+# Manages proxysql for the replica and other sql services on
+# labs-support network
+
+class role::labs::db::proxy {
+    system::role { 'role::labs::db::proxy':
+        description => 'LabsDB proxy',
+    }
+
+    include standard
+    include passwords::labs::db::proxy
+
+    $admin_user = $passwords::labs::db::proxy::admin_user
+    $admin_password = $passwords::labs::db::proxy::admin_password
+
+    class { 'proxysql':
+        admin_user     => $admin_user,
+        admin_password => $admin_password,
+        admin_socket   => '/var/run/proxysql/proxysql_admin.sock',
+        mysql_socket   => '/var/run/proxysql/proxysql_admin.sock',
+        mysql_port     => 3306,
+    }
+
+    file {'/var/run/proxysql':
+        ensure => directory,
+        user   => 'proxysql',
+        group  => 'root',
+        mode   => '755',
+    }
+
+    nrpe::monitor_service { 'proxysql':
+        description   => 'proxysql processes',
+        nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 1: \
+-a "proxysql"',
+        critical      => false,
+        contact_group => 'admins', # show on icinga/irc only
+    }
+}
