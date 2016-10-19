afb7fe4151fb144d0d989a9284260f76a64be477
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-10-19 16:46:38 +0300

    Add tendril role to tegmen
    Merging the tendril role to tegmen
    
    Change-Id: I0e512c946078c865bfcd1b1b8edda7254393b9cc

--- manifests/site.pp
+++ manifests/site.pp
@@ -2767,7 +2767,7 @@ node /(subra|suhail)\.codfw\.wmnet/ {
 
 # icinga based monitoring host in codfw
 node 'tegmen.wikimedia.org' {
-    role(icinga) #, tendril, tcpircbot)
+    role(icinga, tendril) #, tcpircbot)
 }
 
 # https://wikitech.wikimedia.org/wiki/Terbium
