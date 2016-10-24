069428f8582ad296dd9ce509245e028608b7388a
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-10-24 12:56:30 +0300

    icinga::ircbot: Add an ensure parameter
    This will allow to not run the ircbot based on a flag. Set the flag to
    false for tegmen, einsteinium
    
    Change-Id: I483e81e070999a9e8077c093892561530a29751c

--- modules/icinga/manifests/ircbot.pp
+++ modules/icinga/manifests/ircbot.pp
@@ -1,30 +1,32 @@
 # = Class: icinga::ircbot
 #
 # Sets up an ircecho instance that sends icinga alerts to IRC
-class icinga::ircbot {
+class icinga::ircbot(
+    $ensure='present'
+) {
+    if $ensure == 'present' {
+        $ircecho_logs   = {
+            '/var/log/icinga/irc.log'             => '#wikimedia-operations',
+            '/var/log/icinga/irc-wikidata.log'    => '#wikidata',
+            '/var/log/icinga/irc-releng.log'      => '#wikimedia-releng',
+            '/var/log/icinga/irc-labs.log'        => '#wikimedia-labs',
+            '/var/log/icinga/irc-analytics.log'   => '#wikimedia-analytics',
+            '/var/log/icinga/irc-ores.log'        => '#wikimedia-ai',
+            '/var/log/icinga/irc-interactive.log' => '#wikimedia-interactive',
+        }
+        $ircecho_nick   = 'icinga-wm'
+        $ircecho_server = 'chat.freenode.net'
 
-    $ircecho_logs   = {
-        '/var/log/icinga/irc.log'             => '#wikimedia-operations',
-        '/var/log/icinga/irc-wikidata.log'    => '#wikidata',
-        '/var/log/icinga/irc-releng.log'      => '#wikimedia-releng',
-        '/var/log/icinga/irc-labs.log'        => '#wikimedia-labs',
-        '/var/log/icinga/irc-analytics.log'   => '#wikimedia-analytics',
-        '/var/log/icinga/irc-ores.log'        => '#wikimedia-ai',
-        '/var/log/icinga/irc-interactive.log' => '#wikimedia-interactive',
+        class { '::ircecho':
+            ircecho_logs   => $ircecho_logs,
+            ircecho_nick   => $ircecho_nick,
+            ircecho_server => $ircecho_server,
+        }
+
+        # T28784 - IRC bots process need nagios monitoring
+        nrpe::monitor_service { 'ircecho':
+            description  => 'ircecho_service_running',
+            nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:4 -c 1:20 -a ircecho',
+        }
     }
-    $ircecho_nick   = 'icinga-wm'
-    $ircecho_server = 'chat.freenode.net'
-
-    class { '::ircecho':
-        ircecho_logs   => $ircecho_logs,
-        ircecho_nick   => $ircecho_nick,
-        ircecho_server => $ircecho_server,
-    }
-
-    # T28784 - IRC bots process need nagios monitoring
-    nrpe::monitor_service { 'ircecho':
-        description  => 'ircecho_service_running',
-        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:4 -c 1:20 -a ircecho',
-    }
-
 }
