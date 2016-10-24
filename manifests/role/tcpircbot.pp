539bfeea86c25d3f1dc67d130fc57e575e5c8de2
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-10-25 01:25:08 +0300

    role::tcpircbot: Add an ensure parameter
    Allow role::tcpircbot to be selectively disabled, based on an ensure
    parameter. Set this to absent for einstenium and tegmen
    
    Change-Id: Icfb4cbc96d83939eb9568d5df7208e3a4eddf250

--- manifests/role/tcpircbot.pp
+++ manifests/role/tcpircbot.pp
@@ -1,4 +1,4 @@
-class role::tcpircbot {
+class role::tcpircbot($ensure='present') {
     include ::tcpircbot
     include passwords::logmsgbot
 
@@ -6,36 +6,37 @@ class role::tcpircbot {
         description => 'tcpircbot server',
     }
 
-    nrpe::monitor_service { 'tcpircbot':
-        description  => 'tcpircbot_service_running',
-        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -C python -a tcpircbot.py',
-    }
 
-    tcpircbot::instance { 'logmsgbot':
-        channels => '#wikimedia-operations',
-        password => $passwords::logmsgbot::logmsgbot_password,
-        cidr     => [
-            '::ffff:127.0.0.1/128',             # loopback
-            '::ffff:10.64.32.167/128',          # logging: eventlog1001
-            '::ffff:10.64.0.196/128',           # deployment eqiad v4: tin
-            '2620:0:861:101:10:64:0:196/128',   # deployment eqiad v6: tin
-            '::ffff:10.192.16.132/128',         # deployment codfw v4: mira
-            '2620:0:860:102:10:192:16:132/128', # deployment codfw v6: mira
-            '::ffff:10.64.32.13/128',           # maintenance eqiad v4: terbium
-            '2620:0:861:103:10:64:32:13/64',    # maintenance eqiad v6: terbium
-            '::ffff:10.192.48.45/128',          # maintenance codfw v4: wasat
-            '2620:0:860:104:10:192:48:45/64',   # maintenance codfw v6: wasat
-            '::ffff:10.64.16.73/128',           # puppetmaster1001.eqiad.wmnet
-            '2620:0:861:102:10:64:16:73/128',   # puppetmaster1001.eqiad.wmnet
-            '::ffff:10.192.0.27/128',           # puppetmaster2001.codfw.wmnet
-            '2620:0:860:101:10:192:0:27/128',   # puppetmaster2001.codfw.wmnet
+    if $ensure == 'present' {
+        nrpe::monitor_service { 'tcpircbot':
+            description  => 'tcpircbot_service_running',
+            nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -C python -a tcpircbot.py',
+        }
+        tcpircbot::instance { 'logmsgbot':
+            channels => '#wikimedia-operations',
+            password => $passwords::logmsgbot::logmsgbot_password,
+            cidr     => [
+                '::ffff:127.0.0.1/128',             # loopback
+                '::ffff:10.64.32.167/128',          # logging: eventlog1001
+                '::ffff:10.64.0.196/128',           # deployment eqiad v4: tin
+                '2620:0:861:101:10:64:0:196/128',   # deployment eqiad v6: tin
+                '::ffff:10.192.16.132/128',         # deployment codfw v4: mira
+                '2620:0:860:102:10:192:16:132/128', # deployment codfw v6: mira
+                '::ffff:10.64.32.13/128',           # maintenance eqiad v4: terbium
+                '2620:0:861:103:10:64:32:13/64',    # maintenance eqiad v6: terbium
+                '::ffff:10.192.48.45/128',          # maintenance codfw v4: wasat
+                '2620:0:860:104:10:192:48:45/64',   # maintenance codfw v6: wasat
+                '::ffff:10.64.16.73/128',           # puppetmaster1001.eqiad.wmnet
+                '2620:0:861:102:10:64:16:73/128',   # puppetmaster1001.eqiad.wmnet
+                '::ffff:10.192.0.27/128',           # puppetmaster2001.codfw.wmnet
+                '2620:0:860:101:10:192:0:27/128',   # puppetmaster2001.codfw.wmnet
 
-        ],
-    }
-
-    ferm::rule { 'tcpircbot_allowed':
-        # eventlog1001 (v4), tin (v4), mira (v4), puppetmaster1001 (v4), tin (v6), mira (v6), puppetmaster1001 (v6, unnamed in DNS), terbium (v4), terbium (v6), wasat (v4), wasat (v6), puppetmaster2001 (v4), puppetmaster2001 (v6, unnamed in DNS)
-        # Please DO NOT change the IPs in the rule below without updating the comment above
-        rule => 'proto tcp dport 9200 { saddr (10.64.32.167/32 10.64.0.196/32 10.192.16.132/32 10.64.16.73/32 2620:0:861:101:10:64:0:196/128 2620:0:860:102:10:192:16:132/128 2620:0:861:102:10:64:16:73/128 10.64.32.13/32 2620:0:861:103:10:64:32:13/64 10.192.48.45/32 2620:0:860:104:10:192:48:45/64 10.192.0.27/32 2620:0:860:101:10:192:0:27/128) ACCEPT; }',
+            ],
+        }
+        ferm::rule { 'tcpircbot_allowed':
+            # eventlog1001 (v4), tin (v4), mira (v4), puppetmaster1001 (v4), tin (v6), mira (v6), puppetmaster1001 (v6, unnamed in DNS), terbium (v4), terbium (v6), wasat (v4), wasat (v6), puppetmaster2001 (v4), puppetmaster2001 (v6, unnamed in DNS)
+            # Please DO NOT change the IPs in the rule below without updating the comment above
+            rule => 'proto tcp dport 9200 { saddr (10.64.32.167/32 10.64.0.196/32 10.192.16.132/32 10.64.16.73/32 2620:0:861:101:10:64:0:196/128 2620:0:860:102:10:192:16:132/128 2620:0:861:102:10:64:16:73/128 10.64.32.13/32 2620:0:861:103:10:64:32:13/64 10.192.48.45/32 2620:0:860:104:10:192:48:45/64 10.192.0.27/32 2620:0:860:101:10:192:0:27/128) ACCEPT; }',
+        }
     }
 }
