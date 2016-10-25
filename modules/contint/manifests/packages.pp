f846305d83ac89ebb97bc27670a4cffbf0906499
Antoine Musso <hashar@free.fr>
2016-10-25 23:24:32 +0200

    contint: drop useless require_package
    contint::packages is meant to be shared between production and labs
    slaves. It installs openjdk-7-jdk which is redundant with:
    
     jenkins::slave which installs the openjdk-7-jdk-headless for the
    jenkins agent that runs on the instances.
    
     contint::packages::java installed on all slaved and meant to represent
    the requirement to run a java/maven project.
    
    Drop openjdk-7-jdk from contint::packages.
    
    Change-Id: I1659311d10eaeed62816f38652cbdeeb341a8d1e

--- modules/contint/manifests/packages.pp
+++ modules/contint/manifests/packages.pp
@@ -14,8 +14,6 @@ class contint::packages {
         include ::mediawiki::packages::php5
     }
 
-    require_package('openjdk-7-jdk')
-
     # MediaWiki doc is built directly on contint1001
     require_package('doxygen')
 
