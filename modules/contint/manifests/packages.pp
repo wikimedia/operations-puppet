23227e1f4058923fdf4baa1e08c9be7c0cd542c0
Antoine Musso <hashar@free.fr>
2016-10-25 23:29:05 +0200

    contint: move php5 install on jessie to nearest user
    On Debian Jessie, mediawiki::packages no more install the Zend PHP. That
    has to be done explicitly via mediawiki::packages:php5.
    
    contint::packages was/is meant to be shared between production and labs
    slaves, however everything now runs on labs.
    
    Move the definition to contint::packages::labs next to the include of
    mediawiki::packages.
    
    Change-Id: Ide58718c9da3cf1ada60e304c082704b63aa66d4

--- modules/contint/manifests/packages.pp
+++ modules/contint/manifests/packages.pp
@@ -8,10 +8,4 @@ class contint::packages {
     # Basic utilites needed for all Jenkins slaves
     include ::contint::packages::base
 
-    # We're no longer installing PHP on app servers starting with
-    # jessie, but we still need it for CI
-    if os_version('debian == jessie') {
-        include ::mediawiki::packages::php5
-    }
-
 }
