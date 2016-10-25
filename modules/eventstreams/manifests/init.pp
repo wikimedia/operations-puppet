bcc1314694a61e40b886159132a041da31338cd0
Andrew Otto <acotto@gmail.com>
2016-10-25 17:20:52 -0400

    [WIP] evenstreams puppetization
    Bug: T148779
    Change-Id: I2667830eb995bdc7ddc87359157d28c5f03f285c

---
+++ modules/eventstreams/manifests/init.pp
@@ -0,0 +1,40 @@
+# == Class: eventstreams
+#
+# === Parameters
+#
+# [*broker_list*]
+#   Comma-separated list of Kafka broker URIs
+#
+class eventstreams(
+    $broker_list,
+    $allowed_topics = undef,
+) {
+
+    include ::service::configuration
+
+    service::packages { 'eventstreams':
+        pkgs     => ['librdkafka++1', 'librdkafka1'],
+        dev_pkgs => ['librdkafka-dev'],
+    }
+
+    service::node { 'eventstreams':
+        enable            => true,
+        port              => 6947,
+        healthcheck_url   => '',
+        has_spec          => false, # I should make one!
+        deployment        => 'scap3',
+        deployment_config => true,
+        deployment_vars   => {
+            log_level      => 'info',
+            broker_list    => $broker_list,
+            allowed_topics => $allowed_topics
+            site           => $::site,
+        },
+        auto_refresh      => false,
+        init_restart      => false,
+        environment       => {
+            'UV_THREADPOOL_SIZE' => 128
+        },
+    }
+
+}
