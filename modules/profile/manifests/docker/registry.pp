103a807f1afc4634abfa2b7bb46ca9f43756c515
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-10-25 13:38:55 +0200

    docker::registry: puppetization for production
    Bug: T148966
    Change-Id: Iddbac366bde6010b87f7032ea7950a0b3590ff20

---
+++ modules/profile/manifests/docker/registry.pp
@@ -0,0 +1,56 @@
+class profile::docker::registry {
+    require ::network::constants
+    # Hiera configurations
+    # The following variables might be useful elsewhere too
+    $username = hiera('docker::registry::username')
+    $hash = hiera('docker::registry::hash')
+    # Which machines are allowed to build images
+    $image_builders = hiera(
+        'profile::docker::registry::image_builders',
+        $network::constants::special_hosts[$::realm]['deployment_hosts'])
+    $config = hiera('profile::docker::registry::config', {})
+
+    # Storage configuration
+    $storage_backend = hiera('profile::docker::registry::storage_backend', 'filebackend')
+
+    if $storage_backend == 'filebackend' {
+        $params = {
+            'config'   => $config,
+            'datapath' => hiera('profile::docker::registry::datapath', '/srv/registry'),
+        }
+    } else {
+        $params = {
+            'config'          => $config,
+            'storage_backend' => $storage_backend,
+            'swift_user'      => hiera('profile::docker::registry::swift_username'),
+            'swift_password'  => hiera('profile::docker::registry::swift_password'),
+            'swift_url'       => hiera('profile::docker::registry::swift_auth_url'),
+            'container'       => hiera('profile::docker::registry::swift_container'),
+        }
+    }
+
+    create_resources('class', {'::docker::registry' => $params})
+
+
+    # Nginx frontend
+    base::expose_puppet_certs { '/etc/nginx':
+        ensure          => present,
+        provide_private => true,
+        require         => Class['nginx'],
+    }
+
+    class { '::sslcert::dhparam': }
+
+
+    class { '::docker::registry::web':
+        docker_username      => $user,
+        docker_password_hash => $hash,
+        allow_push_from      => $image_builders,
+        ssl_settings         => ssl_ciphersuite('nginx', 'mid'),
+        use_puppet_certs     => true,
+    }
+
+    diamond::collector::nginx{ $::fqdn:
+        port => 10080,
+    }
+}
