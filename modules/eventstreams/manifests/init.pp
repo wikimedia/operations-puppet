4b3f31d3c84e6452c86b924c1bba3ac47630c9a1
Andrew Otto <acotto@gmail.com>
2016-10-25 17:20:52 -0400

    eventstreams puppetization
    Bug: T148779
    Change-Id: I2667830eb995bdc7ddc87359157d28c5f03f285c

---
+++ modules/eventstreams/manifests/init.pp
@@ -0,0 +1,50 @@
+# == Class: eventstreams
+#
+# === Parameters
+#
+# [*broker_list*]
+#   Comma-separated list of Kafka broker URIs
+#
+# [*streams*]
+#   Hash of stream route config and their composite topics. E.g.
+#
+#   streamName1:
+#       topics: [topicA, topicB]
+#   streamName2:
+#       topics: [topicC, topicD]
+#
+# [*log_level*]
+#   Log level for service logger. Default: info
+#
+class eventstreams(
+    $broker_list,
+    $streams,
+    $port      = 8095
+    $log_level = 'info'
+) {
+    service::packages { 'eventstreams':
+        pkgs     => ['librdkafka++1', 'librdkafka1'],
+    }
+
+    service::node { 'eventstreams':
+        enable            => true,
+        port              => $port,
+        healthcheck_url   => '',
+        has_spec          => false, # TODO: figure out how to monitor stream with spec x-amples
+        deployment        => 'scap3',
+        deployment_config => true,
+        deployment_vars   => {
+            log_level      => $log_level,
+            site           => $::site,
+            broker_list    => $broker_list,
+            streams        => $streams,
+        },
+        auto_refresh      => false,
+        init_restart      => false,
+        environment       => {
+            'UV_THREADPOOL_SIZE' => 128
+        },
+        require => Service::Packages['eventstreams']
+    }
+
+}
