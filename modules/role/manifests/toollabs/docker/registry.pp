1ff13c4eaaca471707ce5b9e9cbb43665cfb1e2a
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-10-26 10:09:15 +0200

    docker::web: allow defining multiple build servers
    Change-Id: I64edad7ee3cdc694d6f1a82afdce1d7d7e1208c4

--- modules/role/manifests/toollabs/docker/registry.pp
+++ modules/role/manifests/toollabs/docker/registry.pp
@@ -7,7 +7,7 @@ class role::toollabs::docker::registry {
         before       => Class['::docker::registry'],
     }
 
-    $builder = ipresolve(hiera('docker::builder_host'), 4, $::nameservers[0])
+    $builders = [ipresolve(hiera('docker::builder_host'), 4, $::nameservers[0])]
 
     $user = hiera('docker::username')
     $hash = hiera('docker::password_hash')
@@ -20,7 +20,7 @@ class role::toollabs::docker::registry {
     class { '::docker::registry::web':
         docker_username      => $user,
         docker_password_hash => $hash,
-        allow_push_from      => $builder,
+        allow_push_from      => $builders,
         ssl_certificate_name => 'star.tools.wmflabs.org',
         ssl_settings         => ssl_ciphersuite('nginx', 'compat'),
     }
