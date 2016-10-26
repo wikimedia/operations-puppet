93dab9d24b810fa14b95114e4e063417deb24b03
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-10-26 10:20:14 +0200

    docker::registry::web: allow using puppet certs
    As in production we will use the puppet certs, let's offer the
    alternative here.
    
    Change-Id: I7a7093bc88152ff09b8da60333b4c7b023c8668e

--- modules/docker/manifests/registry/web.pp
+++ modules/docker/manifests/registry/web.pp
@@ -2,9 +2,22 @@ class docker::registry::web(
     $docker_username,
     $docker_password_hash,
     $allow_push_from,
-    $ssl_certificate_name,
     $ssl_settings,
+    $use_puppet_certs=false,
+    $ssl_certificate_name=undef,
 ) {
+    if !$use_puppet_certs and !defined($ssl_certificate_name) {
+        fail('Either puppet certs should be used, or an ssl cert name should be provided')
+    }
+
+    if $use_puppet_certs {
+        base::expose_puppet_certs { '/etc/nginx':
+            ensure          => present,
+            provide_private => true,
+            require         => Class['nginx'],
+        }
+    }
+
     file { '/etc/nginx/htpasswd.registry':
         content => "${docker_username}:${docker_password_hash}",
         owner   => 'www-data',
