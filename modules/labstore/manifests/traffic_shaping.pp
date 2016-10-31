662b3519aa1939bd7b14061e1f3f88f94a4034b8
cpettet <rush@wikimedia.org>
2016-10-31 11:05:02 -0500

    labs: tc-setup param thresholds applied
    We have a use case for per-project settings here and this
    is a prereq.
    
    Change-Id: I31785c7a4412d350f09b7c012226b66cccb69a81

--- modules/labstore/manifests/traffic_shaping.pp
+++ modules/labstore/manifests/traffic_shaping.pp
@@ -1,11 +1,16 @@
-class labstore::traffic_shaping {
+class labstore::traffic_shaping(
+    $nfs_write='7000kbps',
+    $nfs_read='9500kbps',
+    $nfs_dumps_read='15000kbps',
+    $eth0_egress='30000kbps',
+) {
 
     file { '/usr/local/sbin/tc-setup':
         ensure => present,
         mode   => '0554',
         owner  => 'root',
         group  => 'root',
-        source => 'puppet:///modules/labstore/tc-setup.sh',
+        content => template('labstore/tc-setup.sh.erb'),
         notify => Exec['apply_tc_config'],
     }
 
