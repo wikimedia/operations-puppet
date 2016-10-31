41e6e1b0695a8d04f2093e4f589dd09e2f1bf280
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-10-31 10:09:23 -0700

    nfs: Add script to manage NFS server on labstore secondary cluster
    Change-Id: I395d2e487459c0aa6ddf315b88db0450b1e1c43d

--- modules/role/manifests/labs/nfs/secondary.pp
+++ modules/role/manifests/labs/nfs/secondary.pp
@@ -45,31 +45,47 @@ class role::labs::nfs::secondary($monitor = 'eth0') {
         'labstore1005' => 'eth1.labstore1005.eqiad.wmnet',
     }
 
-    labstore::drbd::resource {'test':
-        drbd_cluster => $drbd_cluster,
-        port         => '7790',
-        device       => '/dev/drbd1',
-        disk         => '/dev/misc/test',
-        require      => Interface::Ip['drbd-replication'],
+    # Floating IP assigned to drbd primary(active NFS server). Should come from hiera
+    $cluster_ip = '10.64.37.18'
+
+    $subnet_gateway_ip = '10.64.37.1'
+
+    $drbd_resource_config = {
+        test   => {
+            port   => '7790',
+            device => '/dev/drbd1',
+            disk   => '/dev/misc/test',
+        },
+        tools  => {
+            port   => '7791',
+            device => '/dev/drbd4',
+            disk   => '/dev/tools/tools-project',
+        },
+        others => {
+            port   => '7792',
+            device => '/dev/drbd3',
+            disk   => '/dev/misc/others',
+        },
     }
 
-    labstore::drbd::resource {'tools':
-        drbd_cluster => $drbd_cluster,
-        port         => '7791',
-        device       => '/dev/drbd4',
-        disk         => '/dev/tools/tools-project',
-        require      => Interface::Ip['drbd-replication'],
-    }
+    $drbd_resources = keys($drbd_resource_config)
 
-    labstore::drbd::resource {'others':
+    labstore::drbd::resource { $drbd_resources:
         drbd_cluster => $drbd_cluster,
-        port         => '7792',
-        device       => '/dev/drbd3',
-        disk         => '/dev/misc/others',
+        port         => $drbd_resource_config[$title][port],
+        device       => $drbd_resource_config[$title][device],
+        disk         => $drbd_resource_config[$title][disk],
         require      => Interface::Ip['drbd-replication'],
     }
 
     class { 'labstore::monitoring::drbd':
         drbd_role  => $drbd_role,
+    }
+
+    file { '/usr/local/sbin/nfs-manage':
+        content => template('labs/nfs/nfs-manage.sh.erb'),
+        mode    => '0744',
+        owner   => 'root',
+        group   => 'root',
     }
 }
