9789b54765a8006d93393fbd5d687ab3f4554ab9
Faidon Liambotis <faidon@wikimedia.org>
2016-11-01 17:51:34 +0200

    netops (etc.): add asw2-d-eqiad
    Change-Id: I779561919640298473be8f72b41e36e5cf637b40

--- modules/role/manifests/torrus.pp
+++ modules/role/manifests/torrus.pp
@@ -27,6 +27,7 @@ class role::torrus {
         'asw-b-eqiad.mgmt.eqiad.wmnet',
         'asw-c-eqiad.mgmt.eqiad.wmnet',
         'asw-d-eqiad.mgmt.eqiad.wmnet',
+        'asw2-d-eqiad.mgmt.eqiad.wmnet',
         'asw-a-codfw.mgmt.codfw.wmnet',
         'asw-b-codfw.mgmt.codfw.wmnet',
         'asw-c-codfw.mgmt.codfw.wmnet',
