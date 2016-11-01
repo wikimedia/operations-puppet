14b44eac2c5f8b680978e5c71ea4e50888d0fe39
Emanuele Rocca <ema@wikimedia.org>
2016-11-01 15:15:11 +0100

    SystemTap Puppet module and role::systemtap::devserver
    The systemtap puppet module allows configuring a SystemTap development
    server as well as the runtime environment required to run compiled
    SystemTap probes.
    
    A role called systemtap::devserver is also introduced by this commit.
    
    Change-Id: I24ad8d60a0ccc7616c33af4b0a323c39344a1444

---
+++ modules/systemtap/manifests/devserver.pp
@@ -0,0 +1,33 @@
+# Class systemtap::devserver
+# Development environment for SystemTap probes
+#
+# Actions:
+#   Installs the systemtap package, the debugging symbols for the Linux kernel
+#   and the kernel headers matching the currently running kernel.
+#
+# Usage:
+#   include systemtap::devserver
+class systemtap::devserver {
+    require_package([
+        'build-essential',
+        "linux-image-${::kernelrelease}-dbg",
+        "linux-headers-${::kernelrelease}"
+    ])
+
+    # require_package creates a dynamic intermediate class that makes declaring
+    # dependencies a bit strange. Let's use package directly here.
+    if !defined(Package['systemtap']) {
+        package { 'systemtap':
+            ensure => 'present',
+        }
+    }
+
+    if os_version('debian jessie') {
+        apt::pin { 'systemtap':
+            package  => 'systemtap systemtap-common systemtap-runtime',
+            pin      => 'release a=jessie-backports',
+            priority => '1001',
+            before   => Package['systemtap'],
+        }
+    }
+}
