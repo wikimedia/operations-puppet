d11731ec6e7a5c742589827f6a68ee41c9a9ac7a
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-11-01 10:17:46 -0700

    nfs backup: Add mount definitions for backup volumes
    Change-Id: I8a91f3f02bb0d472fa1f5f34bb46a38d72d113aa

--- modules/role/manifests/labs/nfs/backup.pp
+++ modules/role/manifests/labs/nfs/backup.pp
@@ -4,6 +4,45 @@ class role::labs::nfs::backup {
     }
     include labstore::backup_keys
 
+    file { '/srv/eqiad/maps':
+        ensure => 'directory',
+    }
+
+    file { '/srv/eqiad/tools':
+        ensure => 'directory',
+    }
+
+    file { '/srv/eqiad/others':
+        ensure => 'directory',
+    }
+
+    mount { '/srv/eqiad/maps':
+        ensure  => mounted,
+        fstype  => ext4,
+        options => 'defaults,noatime',
+        atboot  => true,
+        device  => '/dev/backup/maps',
+        require => File['/srv/eqiad/maps'],
+    }
+
+    mount { '/srv/eqiad/tools':
+        ensure  => mounted,
+        fstype  => ext4,
+        options => 'defaults,noatime',
+        atboot  => true,
+        device  => '/dev/backup/tools',
+        require => File['/srv/scratch'],
+    }
+
+    mount { '/srv/eqiad/others':
+        ensure  => mounted,
+        fstype  => ext4,
+        options => 'defaults,noatime',
+        atboot  => true,
+        device  => '/dev/backup/others',
+        require => File['/srv/statistics'],
+    }
+
     labstore::device_backup { 'secondary-test':
         remotehost      => 'labstore1005.eqiad.wmnet',
         remote_vg       => 'misc',
