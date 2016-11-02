2ac1eac514f04a06c54bbc629308a130b4310e75
cpettet <rush@wikimedia.org>
2016-11-02 12:22:16 -0500

    labstore: 2003/2004 hiera additons to allow labstore backup key
    Change-Id: I1f45862c271e2d92851dee89c021f592d6c5eb5d

--- modules/labstore/manifests/backup_keys.pp
+++ modules/labstore/manifests/backup_keys.pp
@@ -1,5 +1,10 @@
 class labstore::backup_keys {
 
+    # Notice requires hiera addition at the moment:
+    # Because we have a ssh::userkey stanza
+    # and it will not be used without this hack.  Same pattern ganeti uses.
+    # ssh::server::authorized_keys_file: /etc/ssh/userkeys/%u /etc/ssh/userkeys/%u.d/labstore
+
     # Labstores need to be able to scp from one server
     # to the other (in order to do backups)
     ssh::userkey { 'root-labstore':
