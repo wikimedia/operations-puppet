b4a9927eca8f7e463d3c9193629f95cce6fb8a64
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-11-02 11:54:45 +0100

    docker::baseimages: depend on service, not docker::engine
    This is both more correct and allows us to reuse this in production.
    
    Change-Id: I17453a19df2b2b074e95a25cd859276f29bdde58

--- modules/toollabs/manifests/images.pp
+++ modules/toollabs/manifests/images.pp
@@ -1,5 +1,6 @@
 # Helper class to setup building toollabs related images
 class toollabs::images {
+    require ::docker::engine
     require ::docker::baseimages
 
     git::clone { 'operations/docker-images/toollabs-images':
