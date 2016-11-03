wikimedia/operations-puppet

commit 6ffcac5717517746673b3a51e6f5130d2089912a

puppetmaster: Update ferm rules for IPv6 as well
Allow backends and frontends to be contacted over IPv6 as well by the
frontend puppetmasters.

Change-Id: I35df5dbba916ab5636a229d5b66fb093acc44a9d

modules/role/manifests/puppetmaster/backend.pp
@@ -25,12 +25,12 @@ class role::puppetmaster::backend {
     ferm::service { 'ssh_puppet_merge':
         proto  => 'tcp',
         port   => '22',
-        srange => "@resolve((${puppetmaster_frontend_ferm}))"
+        srange => "(@resolve(${puppetmaster_frontend_ferm}) @resolve(${puppetmaster_frontend_ferm}, AAAA))"
     }
     ferm::service { 'puppetmaster-backend':
         proto  => 'tcp',
         port   => 8141,
-        srange => "@resolve((${puppetmaster_frontend_ferm}))"
+        srange => "(@resolve(${puppetmaster_frontend_ferm}) @resolve(${puppetmaster_frontend_ferm}, AAAA))"
     }
 
 }

modules/role/manifests/puppetmaster/frontend.pp
@@ -77,17 +77,17 @@ class role::puppetmaster::frontend {
     ferm::service { 'ssh_puppet_merge':
         proto  => 'tcp',
         port   => '22',
-        srange => "@resolve((${puppetmaster_frontend_ferm}))"
+        srange => "(@resolve(${puppetmaster_frontend_ferm}) @resolve(${puppetmaster_frontend_ferm}, AAAA))"
     }
 
     ferm::service { 'rsync_puppet_frontends':
         proto  => 'tcp',
         port   => '873',
-        srange => "@resolve((${puppetmaster_frontend_ferm}))"
+        srange => "(@resolve(${puppetmaster_frontend_ferm}) @resolve(${puppetmaster_frontend_ferm}, AAAA))"
     }
     ferm::service { 'puppetmaster-backend':
         proto  => 'tcp',
         port   => 8141,
-        srange => "@resolve((${puppetmaster_frontend_ferm}))"
+        srange => "(@resolve(${puppetmaster_frontend_ferm}) @resolve(${puppetmaster_frontend_ferm}, AAAA))"
     }
 }

commit 79d7f1e5df6590e1e0fac10d130e5570391ab6d4

Fix ferm typo introduced in 6ffcac5717 (parentheses)
Add the missing parentheses from the rules introduced in 6ffcac5717

Change-Id: I7d1c98b828e8270bd34faaf13673606d6c89aa9e

modules/role/manifests/puppetmaster/backend.pp
@@ -25,12 +25,12 @@ class role::puppetmaster::backend {
     ferm::service { 'ssh_puppet_merge':
         proto  => 'tcp',
         port   => '22',
-        srange => "(@resolve(${puppetmaster_frontend_ferm}) @resolve(${puppetmaster_frontend_ferm}, AAAA))"
+        srange => "(@resolve((${puppetmaster_frontend_ferm})) @resolve((${puppetmaster_frontend_ferm}), AAAA))"
     }
     ferm::service { 'puppetmaster-backend':
         proto  => 'tcp',
         port   => 8141,
-        srange => "(@resolve(${puppetmaster_frontend_ferm}) @resolve(${puppetmaster_frontend_ferm}, AAAA))"
+        srange => "(@resolve((${puppetmaster_frontend_ferm})) @resolve((${puppetmaster_frontend_ferm}), AAAA))"
     }
 
 }

modules/role/manifests/puppetmaster/frontend.pp
@@ -77,17 +77,17 @@ class role::puppetmaster::frontend {
     ferm::service { 'ssh_puppet_merge':
         proto  => 'tcp',
         port   => '22',
-        srange => "(@resolve(${puppetmaster_frontend_ferm}) @resolve(${puppetmaster_frontend_ferm}, AAAA))"
+        srange => "(@resolve((${puppetmaster_frontend_ferm})) @resolve((${puppetmaster_frontend_ferm}), AAAA))"
     }
 
     ferm::service { 'rsync_puppet_frontends':
         proto  => 'tcp',
         port   => '873',
-        srange => "(@resolve(${puppetmaster_frontend_ferm}) @resolve(${puppetmaster_frontend_ferm}, AAAA))"
+        srange => "(@resolve((${puppetmaster_frontend_ferm})) @resolve((${puppetmaster_frontend_ferm}), AAAA))"
     }
     ferm::service { 'puppetmaster-backend':
         proto  => 'tcp',
         port   => 8141,
-        srange => "(@resolve(${puppetmaster_frontend_ferm}) @resolve(${puppetmaster_frontend_ferm}, AAAA))"
+        srange => "(@resolve((${puppetmaster_frontend_ferm})) @resolve((${puppetmaster_frontend_ferm}), AAAA))"
     }
 }