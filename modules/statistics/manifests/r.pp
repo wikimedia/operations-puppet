f5f0456da6effee66a3232efcdc7fad18f5c14cc
YuviPanda <yuvipanda@gmail.com>
2016-11-03 14:42:32 -0700

    statistics: Bring in R debs from upstream
    Bug: T149949
    Change-Id: Ief13e4d7e9652ee1e0da090559b32626e5e4c8fc

---
+++ modules/statistics/manifests/r.pp
@@ -0,0 +1,37 @@
+# = Class: statistics::r
+#
+# Sets up R from upstream for statistics users
+#
+# == Parameters
+# [*proxy*]
+#  Proxy to usu with apt. Set to undef to not use a proxy.
+class statistics::r(
+    $proxy = undef,
+) {
+    apt::repository { 'cran-rstudio':
+        uri     => 'http://cran.rstudio.com/bin/linux/debian',
+        dist    => 'jessie-cran3',
+        source  => false,
+        keyfile => 'puppet:///modules/statistics/r-debs.gpg',
+    }
+
+    $proxy_ensure = $proxy ? {
+        undef   => 'absent',
+        default => 'present'
+    }
+
+    apt::conf { 'cran-org-proxy':
+        ensure   => $proxy_ensure,
+        priority => '80',
+        key      => 'Acquire::http::Proxy::cran.rstudio.com',
+        value    => $proxy,
+    }
+
+    ensure_packages([
+        'r-base',
+        'r-base-dev',      # Needed for R packages that have to compile C++ code; see T147682
+        'r-cran-rmysql',
+        'r-recommended'    # CRAN-recommended packages (e.g. MASS, Matrix, boot)
+    ])
+
+}
