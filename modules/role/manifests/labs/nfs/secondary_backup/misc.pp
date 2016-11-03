5e319e64af2732ac5a553cc1836cab958bbca9db
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-11-03 01:34:02 -0700

    labstore: Remove device mounts from secondary backup servers
    Change-Id: I94bb4d3acfd70e0bd5f7f3d20c24bf61b2e6f527

--- modules/role/manifests/labs/nfs/secondary_backup/misc.pp
+++ modules/role/manifests/labs/nfs/secondary_backup/misc.pp
@@ -7,15 +7,6 @@ class role::labs::nfs::secondary_backup::misc {
         require => File['/srv/backup'],
     }
 
-    mount { '/srv/backup/misc':
-        ensure  => mounted,
-        fstype  => ext4,
-        options => 'defaults,noatime',
-        atboot  => true,
-        device  => '/dev/backup/misc-project',
-        require => File['/srv/backup/misc'],
-    }
-
     labstore::device_backup { 'secondary-misc':
         remotehost      => 'labstore1005.eqiad.wmnet',
         remote_vg       => 'misc',
--- modules/role/manifests/labs/nfs/secondary_backup/tools.pp
+++ modules/role/manifests/labs/nfs/secondary_backup/tools.pp
@@ -7,15 +7,6 @@ class role::labs::nfs::secondary_backup::tools {
         require => File['/srv/backup'],
     }
 
-    mount { '/srv/backup/tools':
-        ensure  => mounted,
-        fstype  => ext4,
-        options => 'defaults,noatime',
-        atboot  => true,
-        device  => '/dev/backup/tools-project',
-        require => File['/srv/backup/tools'],
-    }
-
     labstore::device_backup { 'secondary-tools':
         remotehost      => 'labstore1005.eqiad.wmnet',
         remote_vg       => 'tools',
