06ca6a3f5edb75aabac3a7259494da03e88813b3
YuviPanda <yuvipanda@gmail.com>
2016-11-04 00:12:40 -0700

    labs: Add a script that dumps instance info onto a public url
    Wikitech API needs to die.
    
    Bug: T143136
    Change-Id: I52180fb42a4a849e4ef49de23dbeec44ea5ab1d3

---
+++ modules/role/manifests/labs/instance_info_dumper.pp
@@ -0,0 +1,37 @@
+# == Class: role::labs::instance_info_dumper
+#
+# Sets up a small python script that dumps all info about instances
+# into a JSON file periodically
+class role::labs::instance_info_dumper {
+    require_package('python-requests')
+
+    $novaconfig = hiera_hash('novaconfig', {})
+
+    file { '/usr/local/sbin/instance-info-dumper':
+        ensure => present,
+        source => 'puppet:///modules/role/labs/instance-info-dumper.py',
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    $config = {
+        'credentials' => {
+            'username' => 'novaadmin',
+            'api_key'  => $novaconfig['ldap_user_pass'],
+            'auth_url' => "http://${novaconfig['controller_hostname']} %>:35357/v2.0",
+        },
+    'enc_host'    => hiera('labs_puppet_master'),
+    # FIXME: This is terrible, since this will get wiped at every deploy
+    'output_path' => '/srv/mediawiki/docroot/instance-info-v1.json',
+    }
+
+
+    file { '/etc/instance-dumper.yaml':
+        ensure  => present,
+        content => ordered_yaml($config),
+        mode    => '0400',
+        owner   => 'root',
+        group   => 'root',
+    }
+}
