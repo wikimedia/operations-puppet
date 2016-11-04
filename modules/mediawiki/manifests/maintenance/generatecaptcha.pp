6eb6d2a747cd4683a4617f018cbe0d37f6e6e209
Reedy <reedy@wikimedia.org>
2016-11-04 16:54:25 +0000

    Add cronjob for regenerating captchas
    Bug: T150029
    Change-Id: I55394941748d2760d0fa4f8df67c801cbc4bf69c

---
+++ modules/mediawiki/manifests/maintenance/generatecaptcha.pp
@@ -0,0 +1,27 @@
+class mediawiki::maintenance::generatecaptcha( $ensure = present ) {
+    file { '/etc/fancycaptcha/words':
+        ensure  => $ensure,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => secret('fancycaptcha/words');
+    }
+
+    file { '/etc/fancycaptcha/badwords':
+        ensure  => $ensure,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => secret('fancycaptcha/badwords');
+    }
+
+    cron { 'generatecaptcha':
+        ensure   => $ensure,
+        user     => $::mediawiki::users::web,
+        monthday => '*/1',
+        hour     => 1,
+        minute   => 0,
+        require  => File['/etc/fancycaptcha/words', '/etc/fancycaptcha/badwords'],
+        command  => '/usr/local/bin/mwscript extensions/ConfirmEdit/maintenance/GenerateFancyCaptchas.php aawiki --wordlist=/etc/fancycaptcha/words --font=/usr/share/fonts/truetype/freefont/FreeMonoBoldOblique.ttf --blacklist=/etc/fancycaptcha/badwords --fill=10000 --oldcaptcha --delete >/dev/null 2>&1',
+    }
+}
