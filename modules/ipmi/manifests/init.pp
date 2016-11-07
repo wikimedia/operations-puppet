119b00ccc5e0cd5e76012fb087078e482060ec05
Daniel Zahn <dzahn@wikimedia.org>
2016-11-07 11:01:42 -0800

    base/ipmi: install freeipmi globally, move to ipmi module
    We have been installing freeipmi (for temperature
    and other monitoring) on all jessie hosts for a while
    since Change-Id: Ibfd62a979f8.
    
    Back then i claimed the package was not available on trusty
    but turns out i was just wrong about that, it exists for trusty too.
    
    volans has asked for this on trusty for T150160.
    
    As per the comment from Faidon, moving this from standard_packages
    to the existing ipmi module, requiring individual packages
    rather than the meta package and dropping the "if" because
    like this the packages also exist on precise.
    
    Bug: T150160
    Change-Id: I5c1230a069d155d4d489e5daa7ca67bd401a90a9

--- modules/ipmi/manifests/init.pp
+++ modules/ipmi/manifests/init.pp
@@ -5,11 +5,4 @@ class ipmi {
         ensure => present,
     }
 
-    file { '/usr/local/sbin/ipmi_mgmt':
-        path   => '/usr/local/sbin/ipmi_mgmt',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0550',
-        source => 'puppet:///modules/ipmi/ipmi_mgmt',
-    }
 }
