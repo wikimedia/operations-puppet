a7474525588b11b253be9a0eb70e4be37d5b82ab
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-11-09 12:59:38 -0800

    prometheus: add memcached_exporter
    Similarly to other exporters, memcached_exporter by default reads
    statistics from the local memcached server and exports Prometheus
    metrics on port 9150.
    
    Bug: T147326
    Change-Id: Ibf3829afac6b46215bad3371c452ca226ae65431

---
+++ modules/prometheus/manifests/memcached_exporter.pp
@@ -0,0 +1,28 @@
+# == Define: prometheus::memcached_exporter
+#
+# Prometheus exporter for memcached server metrics.
+#
+# = Parameters
+#
+# [*arguments*]
+#   Additional command line arguments for prometheus-memcached-exporter.
+
+define prometheus::memcached_exporter (
+    $arguments = '',
+) {
+    require_package('prometheus-memcached-exporter')
+
+    file { '/etc/default/prometheus-memcached-exporter':
+        ensure  => present,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => "ARGS=\"${arguments}\"",
+        notify  => Service['prometheus-memcached-exporter'],
+    }
+
+    service { 'prometheus-memcached-exporter':
+        ensure  => running,
+        require => Package['prometheus-memcached-exporter'],
+    }
+}
