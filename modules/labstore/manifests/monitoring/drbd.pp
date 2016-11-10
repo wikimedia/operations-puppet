150024a21260a8aa3897149d06f1288b9a79ba8c
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-11-10 15:06:58 -0800

    labstore: Add cluster ip assignment monitoring to drbd cluster
    Change-Id: I26614670413637508fd941971977b69a31c02c1c

--- modules/labstore/manifests/monitoring/drbd.pp
+++ modules/labstore/manifests/monitoring/drbd.pp
@@ -3,7 +3,7 @@
 # Installs icinga checks to make sure resource status on a drbd node is OK,
 # and to check that the nodes conform to the expected drbd roles.
 
-class labstore::monitoring::drbd($drbd_role, $resource = 'all') {
+class labstore::monitoring::drbd($drbd_role, $cluster_ip, $resource = 'all') {
 
     sudo::user { 'nagios_check_drbd':
         user       => 'nagios',
@@ -36,6 +36,19 @@ class labstore::monitoring::drbd($drbd_role, $resource = 'all') {
     nrpe::monitor_service { 'check_drbd_role':
         description  => 'Check DRBD role',
         nrpe_command => "/usr/bin/sudo /usr/local/sbin/check_drbd_role ${::hostname} ${drbd_role}",
+        require      => File['/usr/local/sbin/check_drbd_role'],
+    }
+
+    file { '/usr/local/sbin/check_drbd_cluster_ip':
+        source => 'puppet:///modules/labstore/monitor/check_drbd_cluster_ip',
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    nrpe::monitor_service { 'check_drbd_cluster_ip':
+        description  => 'Check DRBD Cluster IP assignment',
+        nrpe_command => "/usr/bin/sudo /usr/local/sbin/check_drbd_cluster_ip ${::hostname} ${drbd_role} ${cluster_ip}",
         require      => File['/usr/local/sbin/check_drbd_role'],
     }
 
