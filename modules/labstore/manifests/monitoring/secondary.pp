7f5861f823e6010346e989759c39243447e9ba00
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-11-10 17:41:56 -0800

    labstore: Check that NFS is being served over Cluster IP for secondary cluster
    Bug: T144633
    Change-Id: I7ba09c727be21a01746dbe5c955f2c96e3dc1dfc

--- modules/labstore/manifests/monitoring/secondary.pp
+++ modules/labstore/manifests/monitoring/secondary.pp
@@ -1,9 +1,13 @@
-# == Class: labstore::monitoring::drbd
+# == Class: labstore::monitoring::secondary
 #
-# Installs icinga checks to make sure resource status on a drbd node is OK,
-# and to check that the nodes conform to the expected drbd roles.
+# Installs icinga checks to
+# - make sure resource status on a drbd node is OK,
+# - check that the nodes conform to the expected drbd roles.
+# - drbd service status
+# - check that cluster ip is assigned to DRBD primary
+# - NFS is being served over cluster IP
 
-class labstore::monitoring::drbd($drbd_role, $cluster_ip, $resource = 'all') {
+class labstore::monitoring::secondary($drbd_role, $cluster_ip, $resource = 'all') {
 
     sudo::user { 'nagios_check_drbd':
         user       => 'nagios',
@@ -11,6 +15,7 @@ class labstore::monitoring::drbd($drbd_role, $cluster_ip, $resource = 'all') {
                       'ALL = NOPASSWD: /usr/local/sbin/check_drbd_status',
                       'ALL = NOPASSWD: /usr/local/sbin/check_drbd_role',
                       'ALL = NOPASSWD: /usr/local/sbin/check_drbd_cluster_ip',
+                      'ALL = NOPASSWD: /usr/local/sbin/check_nfs_status',
                       ],
     }
 
@@ -50,11 +55,25 @@ class labstore::monitoring::drbd($drbd_role, $cluster_ip, $resource = 'all') {
     nrpe::monitor_service { 'check_drbd_cluster_ip':
         description  => 'Check DRBD Cluster IP assignment',
         nrpe_command => "/usr/bin/sudo /usr/local/sbin/check_drbd_cluster_ip ${::hostname} ${drbd_role} ${cluster_ip}",
-        require      => File['/usr/local/sbin/check_drbd_role'],
+        require      => File['/usr/local/sbin/check_drbd_cluster_ip'],
     }
 
     # Set up DRBD service monitoring
     nrpe::monitor_systemd_unit_state { 'drbd':
         require => Service['drbd'],
     }
+
+    file { '/usr/local/sbin/check_nfs_status':
+        source => 'puppet:///modules/labstore/monitor/check_nfs_status',
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    nrpe::monitor_service { 'check_nfs_status':
+        description  => 'Check if NFS is served over cluster IP',
+        nrpe_command => "/usr/bin/sudo /usr/local/sbin/check_nfs_status ${cluster_ip}",
+        require      => File['/usr/local/sbin/check_nfs_status'],
+    }
+
 }
