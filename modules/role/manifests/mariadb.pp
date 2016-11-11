c411b102a29630716d05833ef8178df41dfe7872
Manuel Arostegui <marostegui@wikimedia.org>
2016-11-11 16:30:14 +0100

    mariadb: Split backup and otrsbackups classes into a different file
    After some discussions with Jaime we thought that we could slowly
    start moving things out from mariadb.pp file into single files
    which will make things clearer and easy to manage.
    
    For this first approach I took backup and otrsbackups classes
    into a different files in a new directory "mariadb"
    
    Bug: T150851
    Change-Id: I804b6df9f9d4c584dc78f5053b18a0a11e335b4a

--- modules/role/manifests/mariadb.pp
+++ modules/role/manifests/mariadb.pp
@@ -571,40 +571,6 @@ class role::mariadb::analytics::custom_repl_slave {
     }
 }
 
-class role::mariadb::backup {
-    include role::backup::host
-    include passwords::mysql::dump
-
-    file { '/srv/backups':
-        ensure => directory,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0600', # implicitly 0700 for dirs
-    }
-
-    file { '/etc/mysql/conf.d/dumps.cnf':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0400',
-        content => "[client]\nuser=${passwords::mysql::dump::user}\npassword=${passwords::mysql::dump::pass}\n",
-    }
-
-    backup::mysqlset {'dbstore':
-        xtrabackup       => false,
-        per_db           => true,
-        innodb_only      => true,
-        binlog           => false,
-        slave            => true,
-        local_dump_dir   => '/srv/backups',
-        password_file    => '/etc/mysql/conf.d/dumps.cnf',
-        method           => 'predump',
-        mysql_binary     => '/usr/local/bin/mysql',
-        mysqldump_binary => '/usr/local/bin/mysqldump',
-        jobdefaults      => "Weekly-${role::backup::host::day}-${role::backup::host::pool}",
-    }
-}
-
 class role::mariadb::core(
     $shard,
     $ssl           = 'puppet-cert',
@@ -1005,40 +971,6 @@ class role::mariadb::maintenance {
         tendril_user     => 'watchdog',
         tendril_password => $passwords::tendril::db_pass,
     }
-}
-
-class role::mariadb::otrsbackups {
-    include role::backup::host
-
-    file { '/srv/backups':
-        ensure => directory,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0600', # implicitly 0700 for dirs
-    }
-
-    file { '/usr/local/bin/dumps-otrs.sh':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0755',
-        content => template('mariadb/dumps-otrs.sh.erb'),
-    }
-
-    cron { 'otrsbackups':
-        minute   => '0',
-        hour     => '0',
-        monthday => '*',
-        month    => '*',
-        weekday  => '3',
-        command  => '/usr/local/bin/dumps-otrs.sh > /srv/backups/dump.log 2>&1',
-        user     => 'root',
-        require  => [
-            File['/usr/local/bin/dumps-otrs.sh'],
-            File['/srv/backups'],
-        ],
-    }
-
-    backup::set {'otrsdb': }
 }
 
 # hosts with client utilities to conect to remote servers
