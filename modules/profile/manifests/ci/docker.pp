6c2e2df8e8e71acd34d23483210c7f57c2f625bc
Dan Duvall <dduvall@wikimedia.org>
2016-11-10 17:02:46 -0800

    CI/integration: Create role for docker CI agent
    New role for a jenkins agent that executes builds within the
    context of a Docker container. Dependencies are greatly simplified over
    the traditional CI agent.
    
    Bug: T150502
    Change-Id: I6c6583790a981d20bdb727e3a3722a8fc419572a

---
+++ modules/profile/manifests/ci/docker.pp
@@ -0,0 +1,21 @@
+# == Class profile::ci::docker
+#
+# Configures a host to be a docker-backed Jenkins agent
+#
+
+class profile::ci::docker {
+    include ::docker
+    include phabricator::arcanist
+    include ::zuul
+
+    class { 'contint::worker_localhost':
+        owner => 'jenkins-deploy',
+    }
+
+    # Ensure jenkins-deploy membership in the docker group
+    exec { 'jenkins-deploy docker membership':
+        unless  => '/usr/bin/id -Gn jenkins-deploy | /bin/grep -q "\bdocker\b"',
+        command => '/usr/sbin/usermod -aG docker jenkins-deploy',
+        require => [User['jenkins-deploy'], Class['docker']],
+    }
+}
