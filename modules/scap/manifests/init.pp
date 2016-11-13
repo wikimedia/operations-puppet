66ff05897e364bcd08b3a4e5a64cb1ec7bac1dfa
Tyler Cipriani <tcipriani@wikimedia.org>
2016-11-13 15:42:59 -0800

    Bump scap version to 3.3.1-1
    Change-Id: I0f65bd55c0a80cc38a41792217425a2b752810e8

--- modules/scap/manifests/init.pp
+++ modules/scap/manifests/init.pp
@@ -12,7 +12,7 @@
 class scap (
     $deployment_server = 'deployment',
     $wmflabs_master = 'deployment-tin.eqiad.wmflabs',
-    $version = '3.3.0-1',
+    $version = '3.3.1-1',
 ) {
     package { 'scap':
         ensure => $version,
