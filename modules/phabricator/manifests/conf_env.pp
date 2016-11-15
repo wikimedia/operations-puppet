c0d6c216e7d64743728dc33fc40742a609517b29
Mukunda Modell <mmodell@wikimedia.org>
2016-11-15 07:46:31 -0600

    Enable multiple config files in phabricator
    This enables separation of privileges so that (for example)
    daemons (phd) and www (apache) can each use separate db credentials.
    
    Related changes in {rPHAB}:
    * {41a7167}
    * {f38f9e1}
    
    Bug: T146055
    Change-Id: I1f6b5503cc4cbf8d6b3f08e6b183345caccd0270

---
+++ modules/phabricator/manifests/conf_env.pp
@@ -0,0 +1,20 @@
+# == Define: phabricator::conf_env
+# Creates a environment-specific config file which can be activated with
+# the environment variable `PHABRICATOR_ENV`
+#
+# When active, any config keys in this file override keys in the standard
+# phabricator config file `local.json`
+#
+define phabricator::conf_env(
+    $environment,
+    $phab_settings  = {},
+    $owner          = 'root',
+    $group          = 'root',
+) {
+    file { "${phabricator::phabdir}/phabricator/conf/local/${environment}.json":
+        owner   => $owner,
+        group   => $group,
+        mode    => '0640',
+        content => template('phabricator/local.json.erb'),
+    }
+}
