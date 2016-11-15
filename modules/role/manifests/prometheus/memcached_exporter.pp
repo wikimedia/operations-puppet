564c744d152ba1bb630d4660456dabeb2a95b369
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-11-15 12:06:43 -0800

    role: account for labs in memcached_exporter
    Yay special cases
    
    Bug: T147326
    Change-Id: Ice86ff96ce9a7b4029f3b62ebb984ef3ea439bf2

--- modules/role/manifests/prometheus/memcached_exporter.pp
+++ modules/role/manifests/prometheus/memcached_exporter.pp
@@ -1,12 +1,17 @@
 class role::prometheus::memcached_exporter {
     prometheus::memcached_exporter { 'default': }
 
-    $prometheus_nodes = hiera('prometheus_nodes')
-    $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
+    if $::realm == 'labs' {
+        $ferm_srange = '$LABS_NETWORKS'
+    } else {
+        $prometheus_nodes = hiera('prometheus_nodes')
+        $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
+        $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
+    }
 
     ferm::service { 'prometheus-memcached-exporter':
         proto  => 'tcp',
         port   => '9150',
-        srange => "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
+        srange => $ferm_srange,
     }
 }
