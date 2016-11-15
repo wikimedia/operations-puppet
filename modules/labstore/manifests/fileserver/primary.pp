c89a41b4d673afe037494b1a0e0912b8a1df9eef
cpettet <rush@wikimedia.org>
2016-11-15 10:51:12 -0600

    labstore1001: clean up unused jobs and legacy
    * These backup jobs don't really work, and now 2001 is not
    in a state to receive them
    
    * now we are doing canonical
    backups from the new cluster.  others/misc things are not fully
    migrated but we are staging there prior to offsite backups.
    
    * set-stripe-cache is a piece of logic that is not applicable
      to any remaining host.
    
    Bug: T127567
    Change-Id: I6bba24f968fd2bc4171275cd906e6bb9b45074bd

--- modules/labstore/manifests/fileserver/primary.pp
+++ modules/labstore/manifests/fileserver/primary.pp
@@ -13,59 +13,6 @@ class labstore::fileserver::primary {
     require_package('python3-paramiko')
     require_package('python3-pymysql')
 
-    file { '/etc/replication-rsync.conf':
-        source => 'puppet:///modules/labstore/replication-rsync.conf',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-    }
-
-    file { '/usr/local/sbin/storage-replicate':
-        source  => 'puppet:///modules/labstore/storage-replicate',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0544',
-        require => File['/etc/replication-rsync.conf'],
-    }
-
-    file { '/usr/local/sbin/cleanup-snapshots':
-        source => 'puppet:///modules/labstore/cleanup-snapshots',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0544',
-    }
-
-    if $is_active {
-        # These should only run on the active host
-
-        # They are staggered by one hour to avoid contention during the write-barriers caused
-        # by the creation of snapshots during the backup process.
-        labstore::fileserver::replicate { 'tools':
-            src_path  => '/srv/project/tools',
-            dest_path => '/srv/eqiad/tools',
-            dest_host => 'labstore2001.codfw.wmnet',
-            calendar  => '02:00',
-        }
-
-        labstore::fileserver::replicate { 'others':
-            src_path  => '/srv/others',
-            dest_path => '/srv/eqiad/others',
-            dest_host => 'labstore2001.codfw.wmnet',
-            calendar  => '03:00',
-        }
-
-        labstore::fileserver::replicate { 'maps':
-            src_path  => '/srv/project/maps',
-            dest_path => '/srv/eqiad/maps',
-            dest_host => 'labstore2001.codfw.wmnet',
-            calendar  => '04:00',
-        }
-
-        labstore::fileserver::cleanup_snapshots { 'labstore':
-            keep_free => '6',
-        }
-    }
-
     # There is no service {} stanza on purpose -- this service
     # must *only* be started by a manual operation because it must
     # run exactly once on whichever NFS server is the current
--- modules/labstore/manifests/init.pp
+++ modules/labstore/manifests/init.pp
@@ -22,27 +22,6 @@ class labstore {
         source => 'puppet:///modules/labstore/snapshot-manager.py',
     }
 
-    file { '/usr/local/sbin/set-stripe-cache':
-        ensure => present,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
-        source => 'puppet:///modules/labstore/set-stripe-cache',
-    }
-
-    # This is done unconditionally to all the md devices at
-    # interval to guard against (a) puppet not applying for
-    # any reason, and (b) the fact that the set of started
-    # md devices on a labstore* is ultimately variable and
-    # dynamic depending on its current role.
-    #
-    cron { 'set-stripe-caches':
-        command => '/usr/local/sbin/set-stripe-cache 4096',
-        user    => 'root',
-        minute  => '*/5',
-        require => File['/usr/local/sbin/set-stripe-cache'],
-    }
-
     file { '/etc/default/nfs-common':
         ensure => present,
         owner  => 'root',
