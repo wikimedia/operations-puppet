9fa91251feee4f359bd0551ed76bd39279c9d764
Alex Monk <alex@wikimedia.org>
2016-11-19 12:44:19 +0000

    Kill beta::deployaccess
    Seems unused, appears to have been a duplicate of role::beta::mediawiki, just
    with a lower priority and deploy-service as well as mwdeploy.
    
    Change-Id: Ida19797ed9893bdf785b2707b8179a1a67e7677a

--- modules/beta/manifests/deployaccess.pp
+++
@@ -1,9 +0,0 @@
-# TODO: remove this if https://phabricator.wikimedia.org/T121721
-# is fixed.
-class beta::deployaccess {
-    $ips = join($network::constants::special_hosts[$::realm]['deployment_hosts'], ' ')
-    security::access::config { 'beta-allow-mwdeploy':
-        content  => "+ : deploy-service mwdeploy : ${ips}\n",
-        priority => 50,
-    }
-}
