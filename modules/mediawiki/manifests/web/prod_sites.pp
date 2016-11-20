edcd0ace2df991974b2045cfedf80051824bdc9c
Alex Monk <alex@wikimedia.org>
2016-11-20 22:37:40 +0000

    Move production apache config files to templates
    So we can add beta suffixes later
    
    Bug: T1256
    Change-Id: I15ff34ce1c125949b78c1953de407a6fb97c766b

--- modules/mediawiki/manifests/web/prod_sites.pp
+++ modules/mediawiki/manifests/web/prod_sites.pp
@@ -2,42 +2,42 @@ class mediawiki::web::prod_sites {
     tag 'mediawiki', 'mw-apache-config'
 
     apache::site { 'redirects':
-        source   => 'puppet:///modules/mediawiki/apache/sites/redirects.conf',
+        source   => 'puppet:///mediawiki/apache/sites/redirects.conf',
         priority => 2,
     }
 
     apache::site { 'main':
-        source   => 'puppet:///modules/mediawiki/apache/sites/main.conf',
+        content  => template('mediawiki/apache/sites/main.conf.erb'),
         priority => 3,
     }
 
     apache::site { 'remnant':
-        source   => 'puppet:///modules/mediawiki/apache/sites/remnant.conf',
+        content  => template('mediawiki/apache/sites/remnant.conf.erb'),
         priority => 4,
     }
 
     apache::site { 'search.wikimedia':
-        source   => 'puppet:///modules/mediawiki/apache/sites/search.wikimedia.conf',
+        source   => 'puppet:///mediawiki/apache/sites/search.wikimedia.conf',
         priority => 5,
     }
 
     apache::site { 'secure.wikimedia':
-        source   => 'puppet:///modules/mediawiki/apache/sites/secure.wikimedia.conf',
+        source   => 'puppet:///mediawiki/apache/sites/secure.wikimedia.conf',
         priority => 6,
     }
 
     apache::site { 'wikimania':
-        source   => 'puppet:///modules/mediawiki/apache/sites/wikimania.conf',
+        content  => template('mediawiki/apache/sites/wikimania.conf.erb'),
         priority => 7,
     }
 
     apache::site { 'wikimedia':
-        source   => 'puppet:///modules/mediawiki/apache/sites/wikimedia.conf',
+        content   => template('mediawiki/apache/sites/wikimedia.conf.erb'),
         priority => 8,
     }
 
     apache::site { 'foundation':
-        source   => 'puppet:///modules/mediawiki/apache/sites/foundation.conf',
+        source   => 'puppet:///mediawiki/apache/sites/foundation.conf',
         priority => 9,
     }
 }
