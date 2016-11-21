a55aa039eae8bd09e2bea6e95a299b776106c711
Emanuele Rocca <ema@wikimedia.org>
2016-11-21 17:11:59 +0100

    cache: remove persistent storage support
    We've switched to file storage on all systems running Varnish 4 in
    50fdd2e. As we're not running Varnish 3 anymore, remove persistent
    storage support altogether.
    
    Bug: T150660
    Change-Id: I27ffdf044128c5f7775609093fc433ce19784ca2

--- modules/role/manifests/cache/base.pp
+++ modules/role/manifests/cache/base.pp
@@ -133,22 +133,6 @@ class role::cache::base(
     # Storage configuration
     ###########################################################################
 
-    # mma: mmap addrseses for fixed persistent storage on x86_64 Linux:
-    #  This scheme fits 4x fixed memory mappings of up to 4TB each
-    #  into the range 0x500000000000 - 0x5FFFFFFFFFFF, which on
-    #  x86_64 Linux is in the middle of the user address space and thus
-    #  unlikely to ever be used by normal, auto-addressed allocations,
-    #  as those grow in from the edges (typically from the top, but
-    #  possibly from the bottom depending).  Regardless of which
-    #  direction heap grows from, there's 32TB or more for normal
-    #  allocations to chew through before they reach our fixed range.
-    $mma = [
-        '0x500000000000',
-        '0x540000000000',
-        '0x580000000000',
-        '0x5C0000000000',
-    ]
-
     # everything from here down is related to backend storage/weight config
 
     $storage_size = $::hostname ? {
@@ -162,16 +146,6 @@ class role::cache::base(
     $filesystems = unique($storage_parts)
     varnish::setup_filesystem { $filesystems: }
     Varnish::Setup_filesystem <| |> -> Varnish::Instance <| |>
-
-    # https://www.varnish-cache.org/docs/trunk/phk/persistent.html
-    $persistent_name = 'deprecated_persistent'
-
-    # This is the "normal" persistent storage varnish args, for consuming all available space
-    # (upload uses something more complex than this based on our storage vars above as well!)
-    $persistent_storage_args = join([
-        "-s main1=${persistent_name},/srv/${storage_parts[0]}/varnish.main1,${storage_size}G,${mma[0]}",
-        "-s main2=${persistent_name},/srv/${storage_parts[1]}/varnish.main2,${storage_size}G,${mma[1]}",
-    ], ' ')
 
     $file_storage_args = join([
         "-s main1=file,/srv/${storage_parts[0]}/varnish.main1,${storage_size}G",
