f6156ad3fef01eb94372739e2be537c1eb4e23d8
Emanuele Rocca <ema@wikimedia.org>
2016-11-22 17:12:43 +0100

    Remove Varnishkafka APT pinning
    Bug: T150660
    Change-Id: If49fd22de7037ee3d2fbd2b50161a6b2a59ff421

--- modules/role/manifests/cache/kafka.pp
+++ modules/role/manifests/cache/kafka.pp
@@ -7,17 +7,6 @@ class role::cache::kafka {
     # NOTE: This is used by inheriting classes role::cache::kafka::*
     $kafka_brokers = $kafka_config['brokers']['array']
 
-    apt::pin { 'varnishkafka':
-        ensure   => 'absent',
-        pin      => '',
-        priority => '',
-    }
-
-    # Make the Varnishkafka class depend on APT pinning. We want to ensure
-    # varnishkafka is not apt-get installed before the pinning file is
-    # created/removed.
-    Apt::Pin['varnishkafka'] -> Class['Varnishkafka']
-
     # Make sure varnishes are configured and started for the first time
     # before the instances as well, or they fail to start initially...
     Service <| tag == 'varnish_instance' |> -> Varnishkafka::Instance <| |>
