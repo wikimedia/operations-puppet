47657683f444b7798b1389548cd20689657a091e
Daniel Zahn <dzahn@wikimedia.org>
2016-11-22 10:01:55 -0800

    openstack: split nova.pp into one class per file (autoload layout)
    All this does is split the file into one file per class, instead of
    multiple classes in a single file. None of the class names or content
    are changed. It will fix warnings about not being in the correct layout.
    
    Bug: T93645
    Change-Id: I3a4ed74c2daf612ffc39aa9b8c367b04c69fdcb3

---
+++ modules/role/manifests/labs/openstack/nova/manager.pp
@@ -0,0 +1,122 @@
+# This is the wikitech UI
+class role::labs::openstack::nova::manager {
+    system::role { $name: }
+    include ::nutcracker::monitoring
+    include ::mediawiki::packages::php5
+    include ::mediawiki::packages::math
+    include ::mediawiki::packages::tex
+    include ::mediawiki::cgroup
+    include ::scap::scripts
+
+    include role::labs::openstack::nova::common
+    $novaconfig = $role::labs::openstack::nova::common::novaconfig
+
+    case $::realm {
+        'production': {
+            $sitename = 'wikitech.wikimedia.org'
+            $certificate = $sitename
+            sslcert::certificate { $sitename: }
+            $cert_type = ''
+        }
+        'labtest': {
+            $sitename = 'labtestwikitech.wikimedia.org'
+            $certificate = 'labtestwikitech'
+            letsencrypt::cert::integrated { $certificate:
+                subjects   => $sitename,
+                puppet_svc => 'apache2',
+                system_svc => 'apache2',
+            }
+            $cert_type = '_letsencrypt'
+        }
+        default: {
+            notify {"unknown realm ${::realm}; https cert will not be installed.":}
+            $cert_type = ''
+        }
+    }
+
+    monitoring::service { 'https':
+        description   => 'HTTPS',
+        check_command => "check_ssl_http${cert_type}!${sitename}",
+    }
+
+    $ssl_settings = ssl_ciphersuite('apache', 'compat', true)
+
+    ferm::service { 'wikitech_http':
+        proto => 'tcp',
+        port  => '80',
+    }
+
+    ferm::service { 'wikitech_https':
+        proto => 'tcp',
+        port  => '443',
+    }
+
+    ferm::service { 'deployment-ssh':
+        proto  => 'tcp',
+        port   => '22',
+        srange => '$DEPLOYMENT_HOSTS',
+    }
+
+    # allow keystone to query the wikitech db
+    $keystone_host = hiera('labs_keystone_host')
+    ferm::service { 'mysql_keystone':
+        proto  => 'tcp',
+        port   => '3306',
+        srange => "@resolve(${keystone_host})",
+    }
+
+    class { '::openstack::openstack_manager':
+        novaconfig         => $novaconfig,
+        webserver_hostname => $sitename,
+        certificate        => $certificate,
+    }
+
+    # T89323
+    monitoring::service { 'wikitech-static-sync':
+        description   => 'are wikitech and wt-static in sync',
+        check_command => 'check_wikitech_static',
+    }
+
+    # For Math extensions file (T126628)
+    file { '/srv/math-images':
+        ensure => 'directory',
+        owner  => 'www-data',
+        group  => 'www-data',
+        mode   => '0755',
+    }
+
+    # On app servers and image scalers, convert(1) from imagemagick is
+    # contained in a firejail profile. Silver receives the same setting
+    # in wmf-config/CommonSettings.php via $wgImageMagickConvertCommand
+    # and since we also need to scale graphics on wikitech, provide them here
+    file { '/usr/local/bin/mediawiki-firejail-convert':
+        source => 'puppet:///modules/mediawiki/mediawiki-firejail-convert',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+
+    file { '/etc/firejail/mediawiki-converters.profile':
+        source => 'puppet:///modules/mediawiki/mediawiki-converters.profile',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+    }
+
+    class { '::nutcracker':
+        mbuf_size => '64k',
+        verbosity => 2,
+        pools     => {
+            'memcached' => {
+                distribution       => 'ketama',
+                hash               => 'md5',
+                listen             => '127.0.0.1:11212',
+                server_connections => 2,
+                servers            => [
+                    '127.0.0.1:11000:1',
+                ],
+            },
+        },
+    }
+}
+
---
+++ modules/role/manifests/labs/openstack/nova/wikiupdates.pp
@@ -0,0 +1,27 @@
+class role::labs::openstack::nova::wikiupdates {
+    require openstack
+    if ! defined(Package['python-mwclient']) {
+        package { 'python-mwclient':
+            ensure => latest,
+        }
+    }
+
+    file { '/usr/lib/python2.7/dist-packages/wikistatus':
+        source  => "puppet:///modules/openstack/${::openstack::version}/nova/wikistatus",
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['python-mwclient'],
+        recurse => true,
+    }
+
+    file { '/usr/lib/python2.7/dist-packages/wikistatus.egg-info':
+        source  => "puppet:///modules/openstack/${::openstack::version}/nova/wikistatus.egg-info",
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['python-mwclient'],
+        recurse => true,
+    }
+}
+
