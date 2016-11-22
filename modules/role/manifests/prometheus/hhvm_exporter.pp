d923d9916746c48dcf1e54414f75ebd1e4c4829a
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-11-22 15:25:53 -0800

    Add hhvm_exporter role and class
    Bug: T147423
    Change-Id: I100912e31347e8734bd6ff2556c14cfb719310e9

---
+++ modules/role/manifests/prometheus/hhvm_exporter.pp
@@ -0,0 +1,17 @@
+class role::prometheus::hhvm_exporter {
+    prometheus::hhvm_exporter { 'default': }
+
+    if $::realm == 'labs' {
+        $ferm_srange = '$LABS_NETWORKS'
+    } else {
+        $prometheus_nodes = hiera('prometheus_nodes')
+        $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
+        $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
+    }
+
+    ferm::service { 'prometheus-hhvm-exporter':
+        proto  => 'tcp',
+        port   => '9192',
+        srange => $ferm_srange,
+    }
+}
