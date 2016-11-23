97dfa83a7f71cc1131a55b1c461f5e2fc942bb7e
Bryan Davis <bd808@wikimedia.org>
2016-11-23 16:09:11 -0700

    logstash: Break logstash.pp up into individual classes
    Break the legacy all-in-one logstash.pp file up into proper one class
    per file organization. The class formerly known as ::role::logstash is
    now named ::role::logstash::collector. The site.pp config has been
    modified for this but an associated manual change will be needed for the
    deployment-logstash2.deployment-prep instance in Labs.
    
    Change-Id: Ifc02cb52cc6a49611fbc6c1158f6fd87b74abb16

---
+++ modules/role/manifests/logstash/elasticsearch.pp
@@ -0,0 +1,33 @@
+# vim:sw=4 ts=4 sts=4 et:
+# == Class: role::logstash::elasticsearch
+#
+# Provisions Elasticsearch backend node for a Logstash cluster.
+#
+class role::logstash::elasticsearch {
+    include standard
+    include ::elasticsearch::nagios::check
+    include ::elasticsearch::monitor::diamond
+    include base::firewall
+
+    if $::standard::has_ganglia {
+        include ::elasticsearch::ganglia
+    }
+
+    package { 'elasticsearch/plugins':
+        provider => 'trebuchet',
+    }
+
+    class { '::elasticsearch':
+        require => Package['elasticsearch/plugins'],
+    }
+
+    $logstash_nodes = hiera('logstash::cluster_hosts')
+    $logstash_nodes_ferm = join($logstash_nodes, ' ')
+
+    ferm::service { 'logstash_elastic_internode':
+        proto   => 'tcp',
+        port    => 9300,
+        notrack => true,
+        srange  => "@resolve((${logstash_nodes_ferm}))",
+    }
+}
