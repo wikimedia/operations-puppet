43c7ad837bf3fa9ac8f70efc3d25b2b94a1d3d43
Bryan Davis <bd808@wikimedia.org>
2016-11-23 16:09:11 -0700

    logstash: Break logstash.pp up into individual classes
    Break the legacy all-in-one logstash.pp file up into proper one class
    per file organization. The class formerly known as ::role::logstash is
    now named ::role::logstash::collector. The site.pp config has been
    modified for this but an associated manual change will be needed for the
    deployment-logstash2.deployment-prep instance in Labs.
    
    Change-Id: Ifc02cb52cc6a49611fbc6c1158f6fd87b74abb16

---
+++ modules/role/manifests/logstash/apifeatureusage.pp
@@ -0,0 +1,43 @@
+# vim:sw=4 ts=4 sts=4 et:
+# == Class: role::logstash::apifeatureusage
+#
+# Builds on role::logstash to insert sanitized data for
+# Extension:ApiFeatureUsage into Elasticsearch.
+#
+class role::logstash::apifeatureusage {
+    include ::role::logstash::collector
+
+    # FIXME: make this a param and use hiera to vary by realm
+    $host            = $::realm ? {
+        'production' => '10.2.2.30', # search.svc.eqiad.wmnet
+        'labs'       => 'deployment-elastic05', # Pick one at random
+    }
+
+    # Template for Elasticsearch index creation
+    # lint:ignore:puppet_url_without_modules
+    file { '/etc/logstash/apifeatureusage-template.json':
+        ensure => present,
+        source => 'puppet:///modules/role/logstash/apifeatureusage-template.json',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
+
+    # Add configuration to logstash
+    # Needs to come after 'filter_mediawiki' (priority 50)
+    logstash::conf { 'filter_apifeatureusage':
+        source   => 'puppet:///modules/role/logstash/filter-apifeatureusage.conf',
+        priority => 55,
+    }
+    # lint:endignore
+
+    # Output destined for separate Elasticsearch cluster from Logstash cluster
+    logstash::output::elasticsearch { 'apifeatureusage':
+        host            => $host,
+        guard_condition => '[type] == "api-feature-usage-sanitized"',
+        manage_indices  => true,
+        priority        => 95,
+        template        => '/etc/logstash/apifeatureusage-template.json',
+        require         => File['/etc/logstash/apifeatureusage-template.json'],
+    }
+}
