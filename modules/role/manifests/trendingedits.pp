9884313f0b68cc766ced966caa567dfe76fcef75
Marko Obrovac <mobrovac@wikimedia.org>
2016-11-23 19:43:22 +0100

    Trending Edits: Role and module
    Bug: T150043
    Change-Id: Ia76bca7c91d8e9c15926dec8aa9d60cc06cc245c

---
+++ modules/role/manifests/trendingedits.pp
@@ -0,0 +1,17 @@
+# Role class for trendingedits
+class role::trendingedits {
+
+    $kafka_config = kafka_config('main')
+    $port = 6699
+
+    system::role { 'role::trendingedits':
+        description => 'computes the list of currently-trending articles',
+    }
+
+    class { '::trendingedits':
+        port        => $port,
+        broker_list => $kafka_config['brokers']['string'],
+    }
+
+}
+
