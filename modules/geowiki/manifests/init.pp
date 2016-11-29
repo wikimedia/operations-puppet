78d091a06c237546bbe2d40f6fbb372ef04d1365
Andrew Otto <acotto@gmail.com>
2016-11-29 12:33:28 -0500

    Ensure geowiki base path exists
    Bug: T149438
    Change-Id: Ieaff42579f99c935368690ffc2af2354ee1c3170

--- modules/geowiki/manifests/init.pp
+++ modules/geowiki/manifests/init.pp
@@ -3,11 +3,17 @@
 #
 class geowiki {
     include ::geowiki::params
+
+    file { $::geowiki::params::path:
+        ensure => 'directory',
+    }
+
     git::clone { 'geowiki-scripts':
         ensure    => 'latest',
         directory => $::geowiki::params::scripts_path,
         origin    => 'https://gerrit.wikimedia.org/r/p/analytics/geowiki.git',
         owner     => $::geowiki::params::user,
         group     => $::geowiki::params::user,
+        require   => File[$::geowiki::params::path],
     }
 }
