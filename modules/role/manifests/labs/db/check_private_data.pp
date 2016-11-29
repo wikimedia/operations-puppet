71d7636605c5f8970117c89212499a2dd21ebac9
Jaime Crespo <jcrespo@wikimedia.org>
2016-11-29 10:44:40 +0100

    [WIP] labsdb: Move repo from views to common; add private data check
    * Move wmf-config local repository syncing from labs::db::views.pp
      to common.pp
    * Create check_private_data.pp to check private data (requires
      common.pp. It deploys check_private_data.py and the necessary
      files
    * Change site.pp to only include one role (now mariadb::labs,
      in the future labs::db::replica)
    
    Bug: T150802
    Change-Id: If0da07ea3a29377218e0bfe04a35b61ae912889b

---
+++ modules/role/manifests/labs/db/check_private_data.pp
@@ -0,0 +1,31 @@
+class role::labs::db::check_private_data {
+
+    file { '/etc/mysql/private_tables.txt':
+        ensure  => file,
+        content => template('role/mariadb/private_tables.txt.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+    }
+
+    file { '/etc/mysql/filtered_columns.txt':
+        ensure => file,
+        source => 'puppet:///modules/role/mariadb/filtered_columns.txt',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+    }
+
+    file { '/usr/local/sbin/check_private_data.py':
+        ensure  => file,
+        source  => 'puppet:///modules/role/mariadb/check_private_data.py',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0744',
+        require => [ Package['python3-yaml', 'python3-pymysql'],
+                     Git::clone['operations/mediawiki-config'],
+                     File['/etc/mysql/filtered_columns.txt'],
+                     File['/etc/mysql/private_tables.txt'],
+        ],
+    }
+}
