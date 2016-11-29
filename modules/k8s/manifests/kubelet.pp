739933d61c4672b0797718adc356898afa9d2484
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-11-29 15:31:14 +0200

    kubelet: Amend to support more than labs
    * Allow using packages, still supporting labs via symlink
    * Allow specifying listen address and listen port
    * Allow specifying TLS certs/keys
    * Allow specifying the cluster domain
    * Use /etc/default/kubelet to host these variables
    
    Change-Id: Iaf884dc6deb4e6c692909d71f368d12a8fb5d5ca

--- modules/k8s/manifests/kubelet.pp
+++ modules/k8s/manifests/kubelet.pp
@@ -1,9 +1,33 @@
 class k8s::kubelet(
     $master_host,
+    $listen_address = '0.0.0.0',
+    $listen_port = undef,
     $cluster_dns_ip = '192.168.0.100',
+    $use_package = false,
     $pod_infra_container_image = 'gcr.io/google_containers/pause:2.0',
+    $cluster_domain = 'kube',
+    $tls_cert = '/var/lib/kubernetes/ssl/certs/cert.pem',
+    $tls_key = '/var/lib/kubernetes/ssl/private_keys/server.key',
 ) {
     include ::k8s::infrastructure_config
+
+    # Use this to maintain compatibility with labs for now
+    if $use_package {
+        require_package('kubernetes-node')
+    } else {
+        file { '/usr/bin/kubelet':
+            ensure => link,
+            target => '/usr/local/bin/kubelet',
+        }
+    }
+
+    file { '/etc/default/kubelet':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('k8s/kubelet.default.erb'),
+    }
 
     file { [
         '/var/run/kubernetes',
