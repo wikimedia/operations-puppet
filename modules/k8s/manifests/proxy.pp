30cf4acbf962141a6bc64305da826261a88d4b7e
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-11-29 17:38:45 +0200

    Kube-proxy: Amend to support more than labs
    * Support using packages without breaking labs compatibility, via a
    symlink
    * Support configuring proxy mode and masquerade
    * Use /etc/default/kube-proxy to host variables
    
    Change-Id: I196125d92a039ebc032c8a94e8d8b82b438f9f68

--- modules/k8s/manifests/proxy.pp
+++ modules/k8s/manifests/proxy.pp
@@ -1,9 +1,29 @@
 class k8s::proxy(
     $master_host,
+    $use_package = false,
+    $proxy_mode = 'iptables',
+    $masquerade_all = true,
 ) {
     include ::k8s::infrastructure_config
 
     $master_ip = ipresolve($master_host, 4, $::nameservers[0])
+
+    if $use_package {
+        require('kubernetes-node')
+    } else {
+        file { '/usr/bin/kube-proxy':
+            ensure => link,
+            target => '/usr/local/bin/kube-proxy',
+        }
+    }
+
+    file { '/etc/default/kube-proxy':
+        ensure   => file,
+        owner    => 'root',
+        group    => 'root',
+        mode     => '0644',
+        template => template('k8s/kube-proxy.default.erb'),
+    }
 
     base::service_unit { 'kube-proxy':
         systemd   => true,
