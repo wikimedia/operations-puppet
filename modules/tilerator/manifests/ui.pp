fc738578c93584ce31b4daa363c62ff829657a04
Guillaume Lederrey <glederrey@wikimedia.org>
2016-12-01 19:21:53 +0100

    tilerator: deploy config with scap3
    Bug: T150021
    Change-Id: I0150abceb82c85a14b3fa62c6c7a3c30174e4def

--- modules/tilerator/manifests/ui.pp
+++ modules/tilerator/manifests/ui.pp
@@ -63,6 +63,7 @@
 #   directory in which osmosis keeps its state
 #
 class tilerator::ui(
+    $cassandra_servers,
     $port           = 6535,
     $conf_sources   = 'sources.prod.yaml',
     $contact_groups = 'admins',
@@ -84,10 +85,20 @@ class tilerator::ui(
 
     service::node { 'tileratorui':
         port           => $port,
-        config         => template('tilerator/config_ui.yaml.erb'),
+        deployment_config => true,
         no_workers     => 0, # 0 on purpose to only have one instance running
         repo           => 'tilerator/deploy',
         deployment     => 'scap3',
+        deployment_vars   => {
+            entrypoint         => '""',
+            all_sources_public => true,
+            conf_sources       => $conf_sources,
+            cassandra_user     => $cassandra_tileratorui_user,
+            cassandra_password => $cassandra_tileratorui_pass,
+            cassandra_servers  => $cassandra_servers,
+            osmdb_user         => $pgsql_tileratorui_user,
+            osmdb_password     => $pgsql_tileratorui_pass,
+        },
         contact_groups => $contact_groups,
     }
 
