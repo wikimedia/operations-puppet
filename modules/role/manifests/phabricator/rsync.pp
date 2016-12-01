392f907b84680a6a13902c953b00bcd1f17f1715
Paladox <thomasmulhall410@yahoo.com>
2016-12-01 20:48:41 +0000

    Phabricator: allow rsyncing /srv/repos from active to passive server
    Check which server is set as active server in Hiera,
    then include rsyncd and config on the non-active server
    and allow (manual) rsyncing from active server pushing up
    to passive server.
    
    Avoid hardcoding server names or IPs, we should never have to edit this
    besides the one "active server" switch in Hiera we already use
    for other things like keeping the services stopped and skipping
    monitoring stuff on the non-active server.
    
    This is not an automated sync, it just allows an admin to do it in
    a fully puppetized way.
    
    Bug: T137928
    Change-Id: Iff3b06395a6149820014cbcfac86c27c53bdf186

---
+++ modules/role/manifests/phabricator/rsync.pp
@@ -0,0 +1,23 @@
+class role::phabricator::rsync {
+
+    $phabricator_active_server = hiera('phabricator_active_server')
+
+    if $::hostname != $phabricator_active_server {
+        $hosts_allow = "@resolve((${phabricator_active_server}))"
+
+        ferm::service { 'phabricator-migration-rysnc':
+            proto  => 'tcp',
+            port   => '873',
+            srange => "${hosts_allow}/32",
+        }
+
+        include rsync::server
+
+        rsync::server::module { 'srv-phabricator':
+            path        => '/srv/repos',
+            read_only   => 'no',
+            hosts_allow => $hosts_allow,
+        }
+    }
+
+}
