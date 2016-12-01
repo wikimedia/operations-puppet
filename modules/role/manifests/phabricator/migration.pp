484b3db76a73c388eb83a738b1aa0399795a1e6d
Paladox <thomasmulhall410@yahoo.com>
2016-12-01 20:48:41 +0000

    Phabricator: rsync /srv/repos from iridium to phab2001
    Bug: T137928
    Change-Id: Iff3b06395a6149820014cbcfac86c27c53bdf186

---
+++ modules/role/manifests/phabricator/migration.pp
@@ -0,0 +1,23 @@
+class role::phabricator::migration {
+
+    $phabricator_active_server = hiera('phabricator_active_server')
+
+    if $::hostname != $phabricator_active_server {
+        $sourceip='10.64.32.150'
+
+        ferm::service { 'phabricator-migration-rysnc':
+            proto  => 'tcp',
+            port   => '873',
+            srange => "${sourceip}/32",
+        }
+
+        include rsync::server
+
+        rsync::server::module { 'srv-phabricator':
+            path        => '/srv/repos',
+            read_only   => 'no',
+            hosts_allow => $sourceip,
+        }
+    }
+
+}
