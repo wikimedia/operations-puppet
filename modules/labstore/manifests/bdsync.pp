2ba52df4cb533ec77f5e434a164ceda04b93f8fd
Rush <rush@wikimedia.org>
2016-12-02 20:03:05 +0000

    Revert "labstore: centralize snapshot-manager into bsync module"
    Hold off until bdsync is packaged for trusty.
    
    This reverts commit 365805c5367fdf640826d67b97b292b54cc13f8c.
    
    Change-Id: I3a6893b0fbcde0b5ca0941cff6c69b99747b2aa9

--- modules/labstore/manifests/bdsync.pp
+++ modules/labstore/manifests/bdsync.pp
@@ -5,14 +5,6 @@ class labstore::bdsync {
         before => File['/usr/local/sbin/block_sync'],
     }
 
-    file { '/usr/local/sbin/snapshot-manager':
-        ensure => present,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-        source => 'puppet:///modules/labstore/snapshot-manager.py',
-    }
-
     file { '/usr/local/sbin/block_sync':
         source => 'puppet:///modules/labstore/block_sync.sh',
         owner  => 'root',
