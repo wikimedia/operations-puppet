674bb5376a2f8d6e6387c09d6851992adf814a66
Merlijn van Deen <valhallasw@arctus.nl>
2016-12-02 22:54:43 +0000

    puppet_compiler: include puppet-enc
    The Labs classifier was switched from ldap to puppet-enc,
    and puppet-enc therefore needs to be included in puppet_compiler
    as well.
    
    Change-Id: I719791a50ee41e5eeb999763d055ec92784d0020

--- modules/puppet_compiler/manifests/packages.pp
+++ modules/puppet_compiler/manifests/packages.pp
@@ -2,6 +2,7 @@
 #
 # Installs all the needed packages
 class puppet_compiler::packages {
+    include puppetmaster::enc
 
     require_package('python-yaml', 'python-requests', 'python-jinja2', 'nginx', 'ruby-httpclient', 'ruby-ldap')
 
