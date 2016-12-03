6eb9badbad4e29566c74608798231095a3a34d63
Daniel Zahn <dzahn@wikimedia.org>
2016-12-02 18:03:04 -0800

    phab2001: allow rsync from iridium over IPv6 too
    Another follow-up to Change-Id Iff3b06395a614982,
    we need to add the same ferm rule for IPv6 that we
    added for IPv4 or we can't rsync yet since IPv6 is
    preferred.
    
    Bug: T137928
    Change-Id: I354597803ff9e0320b92156adac16ba78bedf3a4

--- modules/role/manifests/phabricator/rsync.pp
+++ modules/role/manifests/phabricator/rsync.pp
@@ -5,12 +5,14 @@ class role::phabricator::rsync {
     $phabricator_active_server_fqdn = hiera('phabricator_active_server_fqdn')
 
     if $::fqdn != $phabricator_active_server_fqdn {
-        $hosts_allow_ferm = "@resolve((${phabricator_active_server_fqdn}))"
+
+        $hosts_allow_ferm_v4 = "@resolve((${phabricator_active_server_fqdn}))"
+        $hosts_allow_ferm_v6 = "@resolve((${phabricator_active_server_fqdn}), AAAA)"
 
         ferm::service { 'phabricator-repo-rsync':
             proto  => 'tcp',
             port   => '873',
-            srange => $hosts_allow_ferm,
+            srange => "({$hosts_allow_ferm_v4} ${hosts_allow_ferm_v6})",
         }
 
         include rsync::server
