d4a8c83586022c1cd0dba54cde51447caa91c478
Tim Landscheidt <tim@tim-landscheidt.de>
2016-12-05 02:31:25 +0000

    Tools: Install php5-readline
    Bug: T136519
    Change-Id: Ieb6fe7670fe358035186f47b4c2af1f27f08e7a9

--- modules/toollabs/manifests/exec_environ.pp
+++ modules/toollabs/manifests/exec_environ.pp
@@ -348,6 +348,7 @@ class toollabs::exec_environ {
             'libthreads-perl',             # now part of perl
             'libvips15',
             'mysql-client',                # mariadb-client just... doesn't work on precise. Apt failures
+            # No php5-readline (T136519).
             'pyflakes',                    # T59863
             'tclthread',                   # now called tcl-thread
             # no nodejs-legacy             (presumably, -legacy makes a symlink that is default in precise)
@@ -366,6 +367,7 @@ class toollabs::exec_environ {
             'libvips37',
             'nodejs-legacy',               # T1102
             'mariadb-client',              # For /usr/bin/mysql, is broken on precise atm
+            'php5-readline',               # T136519.
             'python-flake8',
             'python3-flake8',
             'tcl-thread',
@@ -404,6 +406,7 @@ class toollabs::exec_environ {
             'libvips38',
             'nodejs-legacy',               # T1102
             'mariadb-client',              # For /usr/bin/mysql, is broken on precise atm
+            'php5-readline',               # T136519.
             'python-flake8',
             'python3-flake8',
             'tcl-thread',
