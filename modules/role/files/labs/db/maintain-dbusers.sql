84e91852dfed980cf689388855f756a6e33b36ac
YuviPanda <yuvipanda@gmail.com>
2016-12-05 14:32:46 +0530

    labs: Add db structure for keeping info about labsdb accounts
    Bug: T149933
    Change-Id: If98e7877151173623b96b168d44a82464b6e59e0

---
+++ modules/role/files/labs/db/maintain-dbusers.sql
@@ -0,0 +1,20 @@
+-- Keep state about tool / user accounts on tool labs and their mysql
+-- account counterparts. This is the canonical store of this information.
+-- Schema is set up to be able to trivially query the following things:
+--  1. What labsdb hosts does this tool / user *not* have an account on.
+--  2. What's the mysql username / password for this tool / user.
+CREATE TABLE accounts(
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    mysql_username VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL, -- 'tool' or 'user'
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+) CHARSET=utf8mb4;
+CREATE UNIQUE INDEX account_type ON accounts(type, username);
+
+CREATE TABLE accounts_hosts(
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    account_id INT UNSIGNED NOT NULL,
+    hostname VARCHAR(255) NOT NULL,
+) CHARSET=utf8mb4;
+CREATE INDEX account_host_status ON accounts(account_id, hostname);
