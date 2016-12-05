09e85951cf821ef88a2879f8c42449afc94f61a4
Jaime Crespo <jcrespo@wikimedia.org>
2016-12-05 11:34:44 +0100

    Renew expired TLS certificate for eventlogging hosts
    Bug: T152364
    Bug: T152188
    Change-Id: I93e6084d3a2981482ba7ace70525eef8e1d8394e

--- modules/role/manifests/mariadb.pp
+++ modules/role/manifests/mariadb.pp
@@ -388,7 +388,7 @@ class role::mariadb::misc::eventlogging(
         datadir       => '/srv/sqldata',
         tmpdir        => '/srv/tmp',
         read_only     => $read_only,
-        ssl           => 'on',
+        ssl           => 'puppet-cert',
         p_s           => 'off',
         binlog_format => 'MIXED',
     }
