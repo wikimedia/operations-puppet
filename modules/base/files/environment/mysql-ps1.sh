f011761e99d8ab5156712d8dd77d7c1f83df3ea1
Tim Landscheidt <tim@tim-landscheidt.de>
2016-12-06 01:57:27 +0000

    base: Fix puppet URLs in comments
    Change-Id: I52f01d2ff70668b4d953f21fd4bc8c8998d85e76

--- modules/base/files/environment/mysql-ps1.sh
+++ modules/base/files/environment/mysql-ps1.sh
@@ -1,5 +1,5 @@
 #####################################################################
 ### THIS FILE IS MANAGED BY PUPPET
-### puppet:///modules/base/environment/mysql-ps1.sh
+### puppet:///base/environment/mysql-ps1.sh
 #####################################################################
 export MYSQL_PS1='mysql:\u@\h [\d]> '
