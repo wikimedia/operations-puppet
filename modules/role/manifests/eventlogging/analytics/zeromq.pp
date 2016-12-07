56a67c1aade4ce8e9a18d8431f65af5de72eb417
Andrew Otto <acotto@gmail.com>
2016-12-07 15:37:36 -0500

    Refactor eventlogging analytics role classes into many files
    The now depreacted eventlogging.pp role file will be removed
    once these classes are included and working on eventlog1001
    instead.
    
    Bug: T152621
    Change-Id: If3e2e96b3a934866f55e0ca2e8c90f269b14b85e

---
+++ modules/role/manifests/eventlogging/analytics/zeromq.pp
@@ -0,0 +1,28 @@
+# == Class role::eventlogging::analytics::zeromq
+# Runs an eventlogging-forwarder to forward events from
+# the Kafka topic eventlogging-valid-mixed to a zeromq
+# Pub/Sub port, so that webperf zeromq consumers keep working.
+#
+# This class exists only for backwards compatibility for services
+# consuming from the legacy ZMQ stream now.
+class role::eventlogging::analytics::zeromq {
+    include role::eventlogging::analytics::server
+
+    $eventlogging_host    = hiera('eventlogging_host', $::ipaddress)
+
+    $kafka_mixed_uri = $role::eventlogging::analytics::server::kafka_mixed_uri
+
+    # This forwards the kafka eventlogging-valid-mixed topic to
+    # ZMQ port 8600 for backwards compatibility.
+    eventlogging::service::forwarder { 'legacy-zmq':
+        input   => "${$kafka_mixed_uri}&enable_auto_commit=False&identity=eventlogging_legacy_zmq",
+        outputs => ["tcp://${eventlogging_host}:8600"],
+    }
+
+    ferm::service { 'eventlogging-zmq-legacy-stream':
+        proto   => 'tcp',
+        notrack => true,
+        port    => '8600',
+        srange  => '@resolve((hafnium.eqiad.wmnet graphite1001.eqiad.wmnet))',
+    }
+}
