df2bece8bad358a89749cbc65a718673345dc0de
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-12-06 17:50:53 -0800

    package_builder: add stretch
    Change-Id: Ia6b56c8b435f2fa979b4f2f2931a64b6380f705a

--- modules/package_builder/manifests/environments.pp
+++ modules/package_builder/manifests/environments.pp
@@ -29,6 +29,14 @@ class package_builder::environments(
         keyring      => '/usr/share/keyrings/debian-archive-keyring.gpg',
         basepath     => $basepath,
     }
+    package_builder::pbuilder_base { 'stretch-amd64':
+        distribution => 'stretch',
+        components   => 'main',
+        architecture => 'amd64',
+        mirror       => 'http://mirrors.wikimedia.org/debian',
+        keyring      => '/usr/share/keyrings/debian-archive-keyring.gpg',
+        basepath     => $basepath,
+    }
     package_builder::pbuilder_base { 'sid-amd64':
         distribution       => 'sid',
         distribution_alias => 'unstable',
@@ -57,6 +65,14 @@ class package_builder::environments(
     }
     package_builder::pbuilder_base { 'jessie-i386':
         distribution => 'jessie',
+        components   => 'main',
+        architecture => 'i386',
+        mirror       => 'http://mirrors.wikimedia.org/debian',
+        keyring      => '/usr/share/keyrings/debian-archive-keyring.gpg',
+        basepath     => $basepath,
+    }
+    package_builder::pbuilder_base { 'stretch-i386':
+        distribution => 'stretch',
         components   => 'main',
         architecture => 'i386',
         mirror       => 'http://mirrors.wikimedia.org/debian',
