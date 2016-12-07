bb0f15e5bb056de704fec335a5402c5166ce77c9
Manuel Arostegui <marostegui@wikimedia.org>
2016-12-07 13:10:13 +0100

    mariadb: Split eventlogging, misc, monitor classes
    We thought that we could slowly start moving things out from mariadb.pp
    into single files which will make things clearer and easier to manage
    
    So moving eventlogging, misc and monitor classes into its own files under the mariadb
    directory
    
    Bug: T152081
    Change-Id: I4b59573462882823cb0d1993d7149024cd59cf2e

--- modules/role/manifests/mariadb.pp
+++ modules/role/manifests/mariadb.pp
@@ -127,25 +127,6 @@ class role::mariadb::ferm {
     }
 }
 
-# Annoy people in #wikimedia-operations
-class role::mariadb::monitor {
-
-    class { 'mariadb::monitor_disk':
-        contact_group => 'admins',
-    }
-
-    class { 'mariadb::monitor_process':
-        contact_group => 'admins',
-    }
-}
-
-# Annoy Sean
-class role::mariadb::monitor::dba {
-
-    include mariadb::monitor_disk
-    include mariadb::monitor_process
-}
-
 # mysql groups for monitoring and salt
 # * mysql_group (required): general usage of the server, for example:
 #   - 'core': production mediawiki servers
@@ -197,60 +178,6 @@ class role::mariadb::groups(
     if os_version('debian >= jessie || ubuntu >= trusty') {
         include role::prometheus::node_exporter
         include role::prometheus::mysqld_exporter
-    }
-}
-
-# miscellaneous services clusters
-class role::mariadb::misc(
-    $shard  = 'm1',
-    $master = false,
-    ) {
-
-    system::role { 'role::mariadb::misc':
-        description => "Misc Services Database ${shard}",
-    }
-
-    $read_only = $master ? {
-        true  => 0,
-        false => 1,
-    }
-
-    $mysql_role = $master ? {
-        true  => 'master',
-        false => 'slave',
-    }
-
-    include standard
-    include role::mariadb::monitor
-    include passwords::misc::scripts
-    include role::mariadb::ferm
-    class { 'role::mariadb::groups':
-        mysql_group => 'misc',
-        mysql_shard => $shard,
-        mysql_role  => $mysql_role,
-    }
-
-    include mariadb::packages_wmf
-    include mariadb::service
-
-    class { 'mariadb::config':
-        config    => 'mariadb/misc.my.cnf.erb',
-        datadir   => '/srv/sqldata',
-        tmpdir    => '/srv/tmp',
-        ssl       => 'puppet-cert',
-        read_only => $read_only,
-    }
-
-    class { 'role::mariadb::grants::production':
-        shard    => $shard,
-        prompt   => "MISC ${shard}",
-        password => $passwords::misc::scripts::mysql_root_pass,
-    }
-
-    class { 'mariadb::heartbeat':
-        shard      => $shard,
-        datacenter => $::site,
-        enabled    => $master,
     }
 }
 
@@ -348,65 +275,6 @@ class role::mariadb::misc::phabricator(
             contact_group => 'admins',
             multisource   => false,
         }
-    }
-}
-
-# Eventlogging needs to be sandboxed by itself. It can consume resources
-# unpredictably, especially during backfilling. It also benefits greatly
-# from a setup tuned for TokuDB.
-class role::mariadb::misc::eventlogging(
-    $shard  = 'm4',
-    $master = false,
-    ) {
-
-    system::role { 'role::mariadb::misc':
-        description => 'Eventlogging Database',
-    }
-
-    $mysql_role = $master ? {
-        true  => 'master',
-        false => 'slave',
-    }
-
-    include standard
-    include role::mariadb::monitor::dba
-    include passwords::misc::scripts
-    include role::mariadb::ferm
-
-    class {'role::mariadb::groups':
-        mysql_group => 'misc',
-        mysql_shard => $shard,
-        mysql_role  => $mysql_role,
-    }
-
-    include mariadb::packages_wmf
-    include mariadb::service
-
-    $read_only = $master ? {
-        true  => 0,
-        false => 1,
-    }
-
-    class { 'mariadb::config':
-        config        => 'mariadb/eventlogging.my.cnf.erb',
-        datadir       => '/srv/sqldata',
-        tmpdir        => '/srv/tmp',
-        read_only     => $read_only,
-        ssl           => 'puppet-cert',
-        p_s           => 'off',
-        binlog_format => 'MIXED',
-    }
-
-    class { 'role::mariadb::grants::production':
-        shard    => $shard,
-        prompt   => "EVENTLOGGING ${shard}",
-        password => $passwords::misc::scripts::mysql_root_pass,
-    }
-
-    class { 'mariadb::heartbeat':
-        shard      => $shard,
-        datacenter => $::site,
-        enabled    => $master,
     }
 }
 
