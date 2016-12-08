7a620238edb090bc2dbfe0417a3ebe1eb7028f09
Andrew Otto <acotto@gmail.com>
2016-12-08 10:22:32 -0500

    role eventlogging::consumer::files -> eventlogging::analytics::files
    Also comment out duplicate declarations in deprecated eventlogging role.
    These should now be taken care of by the included
    role::eventlogging::analytics::server
    
    Bug: T152621
    Change-Id: I93a663ba59f9be8a29fab61526c370b159bc9c50

--- modules/role/manifests/eventlogging.pp
+++ modules/role/manifests/eventlogging.pp
@@ -28,10 +28,10 @@ class role::eventlogging {
 
     # EventLogging for analytics processing is deployed
     # as the eventlogging/analytics scap target.
-    eventlogging::deployment::target { 'analytics': }
-    class { 'eventlogging::server':
-        eventlogging_path => '/srv/deployment/eventlogging/analytics'
-    }
+    # eventlogging::deployment::target { 'analytics': }
+    # class { 'eventlogging::server':
+        # eventlogging_path => '/srv/deployment/eventlogging/analytics'
+    # }
 
     # Get the Kafka configuration
     $kafka_config         = kafka_config('analytics')
@@ -49,29 +49,29 @@ class role::eventlogging {
     $kafka_mixed_uri = "${kafka_consumer_scheme}/${kafka_brokers_string}?topic=eventlogging-valid-mixed"
     $kafka_client_side_raw_uri = "${kafka_consumer_scheme}/${kafka_brokers_string}?topic=eventlogging-client-side"
 
-    # This check was written for eventlog1001, so only include it there.,
-    if $::hostname == 'eventlog1001' {
-
-        # Alert when / gets low. (eventlog1001 has a 9.1G /)
-        nrpe::monitor_service { 'eventlogging_root_disk_space':
-            description   => 'Eventlogging / disk space',
-            nrpe_command  => '/usr/lib/nagios/plugins/check_disk -w 1024M -c 512M -p /',
-            contact_group => 'analytics',
-        }
-
-        # Alert when /srv gets low. (eventlog1001 has a 456G /srv)
-        # Currently, /srv/log/eventlogging grows at about 500kB / s.
-        # Which is almost 2G / hour.  100G gives us about 2 days to respond,
-        # 50G gives us about 1 day.  Logrotate should keep enough disk space free.
-        nrpe::monitor_service { 'eventlogging_srv_disk_space':
-            description   => 'Eventlogging /srv disk space',
-            nrpe_command  => '/usr/lib/nagios/plugins/check_disk -w 100000M -c 50000M -p /srv',
-            contact_group => 'analytics',
-        }
-    }
-
-    # make sure any defined eventlogging services are running
-    class { '::eventlogging::monitoring::jobs': }
+    # # This check was written for eventlog1001, so only include it there.,
+    # if $::hostname == 'eventlog1001' {
+    #
+    #     # Alert when / gets low. (eventlog1001 has a 9.1G /)
+    #     nrpe::monitor_service { 'eventlogging_root_disk_space':
+    #         description   => 'Eventlogging / disk space',
+    #         nrpe_command  => '/usr/lib/nagios/plugins/check_disk -w 1024M -c 512M -p /',
+    #         contact_group => 'analytics',
+    #     }
+    #
+    #     # Alert when /srv gets low. (eventlog1001 has a 456G /srv)
+    #     # Currently, /srv/log/eventlogging grows at about 500kB / s.
+    #     # Which is almost 2G / hour.  100G gives us about 2 days to respond,
+    #     # 50G gives us about 1 day.  Logrotate should keep enough disk space free.
+    #     nrpe::monitor_service { 'eventlogging_srv_disk_space':
+    #         description   => 'Eventlogging /srv disk space',
+    #         nrpe_command  => '/usr/lib/nagios/plugins/check_disk -w 100000M -c 50000M -p /srv',
+    #         contact_group => 'analytics',
+    #     }
+    # }
+    #
+    # # make sure any defined eventlogging services are running
+    # class { '::eventlogging::monitoring::jobs': }
 }
 
 
