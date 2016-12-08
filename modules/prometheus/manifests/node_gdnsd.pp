155335b59ea273d04dfd0ef0c2a6fd143bc8bf4c
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2016-12-08 09:56:49 -0800

    prometheus: export gdnsd stats via node_exporter
    Bug: T147426
    Change-Id: I7be4c12e73760859316ae29a3772d7066d02dc8a

---
+++ modules/prometheus/manifests/node_gdnsd.pp
@@ -0,0 +1,34 @@
+# = Class: prometheus::node_gdnsd
+#
+# Periodically export gdnsd stats via node-exporter textfile collector.
+#
+# Why not a gdnsd_exporter?
+#
+# In WMF's case there aren't a lot of machines with gdnsd deployed as of Dec 2016.
+# Also, having an exporter would have the added benefit of being able to
+# aggregate stats on other dimensions rather than per-cluster or per-site.
+
+class prometheus::node_gdnsd (
+    $outfile = '/var/lib/prometheus/node.d/gdnsd.prom',
+    $ensure = 'present',
+) {
+    validate_re($outfile, '\.prom$')
+    validate_ensure($ensure)
+
+    require_package('python-prometheus-client')
+
+    file { '/usr/local/bin/prometheus-gdnsd-stats':
+        ensure => file,
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/prometheus/usr/local/bin/prometheus-gdnsd-stats',
+    }
+
+    # Collect every minute
+    cron { 'prometheus_gdnsd_stats':
+        user    => 'root',
+        command => "/usr/local/bin/prometheus-gdnsd-stats --outfile ${outfile}"
+        ensure  => $ensure,
+    }
+}
