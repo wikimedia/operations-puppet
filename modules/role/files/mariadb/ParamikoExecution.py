e8f14a827816d19273ea500c9aa4ff0f70ff656e
Jcrespo <jcrespo@wikimedia.org>
2016-12-09 19:46:36 +0100

    [WIP] Create framework to transfer files over the LAN
    The transfer method must:
    
    * Be as fast as the network bandwidth allows it, with
      configurable throttling
    * Be easy to use (take automatic decisions when safe)
    * Allow for both single files and entire directories to be
      synced. The directories can be of thousands of small files
    * Keep the original permissions and ownership
    * Have in-place checks to avoid doing damaging stuff
    * Allow encryption
    * Allow configurable compression
    * Allos configurable resource taking (e.g. number of CPUs)
    * Hash contents before and after the copy to check it has been
      done successfuly
    * Allow multicast-like transfers from 1 server to many
    * Report the status at any time, and if it fails, why
    * Handle the firewall automatically
    * Not require a constantly open port or service
    
    The current code is just the barebones, it has to be integrated
    with volan's packages for remote code execution.
    
    Because the above, rsync is not enough. We have to give a look to
    multi-thread FTP, tar + socat with user encryption, and bittorent.
    
    Change-Id: I2add041f7884e61c81436119a23e14fd94db7d5d

---
+++ modules/role/files/mariadb/ParamikoExecution.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+from RemoteExecution import RemoteExecution, CommandReturn
+
+from multiprocessing import Process, Pipe
+import paramiko
+import shlex
+import time
+
+
+def run_subprocess(host, command, user, port, host_keys, input_pipe):
+    e = ParamikoExecution()
+    e.user = user
+    e.port = port
+    e.host_keys = host_keys
+    result = e.run(host, command)
+    input_pipe.send(result)
+
+
+class ParamikoExecution(RemoteExecution):
+
+    def __init__(self, user='root', port=22,
+                 host_keys='.ssh/known_hosts'):
+        self.user = user
+        self.port = port
+        self.host_keys = host_keys
+
+    def run(self, host, command):
+        client = paramiko.SSHClient()
+        try:
+            client.load_host_keys(self.host_keys)
+        except OSError:
+            pass
+        client.set_missing_host_key_policy(paramiko.WarningPolicy())
+        client.connect(host, username=self.user, port=self.port)
+        try:
+            stdinfile, stdoutfile, stderrfile = client.exec_command(
+                ' '.join([shlex.quote(x) for x in command])
+            )
+            with stdoutfile as f:
+                stdout = f.read()
+            with stderrfile as f:
+                stderr = f.read()
+            stdinfile.close()
+            returncode = stdoutfile.channel.recv_exit_status()
+        except paramiko.SSHException:
+            returncode = -1
+            stdout = None
+            stderr = None
+        client.close()
+        return CommandReturn(returncode, stdout, stderr)
+
+    def start_job(self, host, command):
+        output_pipe, input_pipe = Pipe()
+        job = Process(target=run_subprocess,
+                      args=(host, command, self.user, self.port,
+                            self.host_keys, input_pipe)
+                      )
+        job.start()
+        input_pipe.close()
+        return {'process': job, 'pipe': output_pipe}
+
+    def monitor_job(self, host, job):
+        if job['process'].is_alive():
+            return CommandReturn(None, None, None)
+        else:
+            result = job['pipe'].recv()
+            job['pipe'].close()
+            return result
+
+    def kill_job(self, host, job):
+        if job['process'].is_alive():
+            job.terminate()
+
+    def wait_job(self, host, job):
+        while job['process'].is_alive():
+            time.sleep(1)
+        result = job['pipe'].recv()
+        job['pipe'].close()
+        return result
