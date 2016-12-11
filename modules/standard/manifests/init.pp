7caa3f2f9cd43df55e747cb03a27190239f0b662
Tim Landscheidt <tim@tim-landscheidt.de>
2016-12-11 05:30:59 +0000

    Set SYS_UID_MAX and SYS_GID_MAX to 499
    For historical reasons, users in modules/admin/data/data.yaml (for
    production) and in LDAP (for Labs) start at uid/gid 500.  However by
    default SYS_UID_MAX and SYS_GID_MAX are set to 999, i. e. there is the
    theoretical possibility for system users being created with uids/gids
    clashing with existing normal users.
    
    This change sets SYS_UID_MAX and SYS_GID_MAX to 499.
    
    Bug: T45795
    Change-Id: Ib2b6c353c0f802f77f900e4330e4eb85c096d67e

--- modules/standard/manifests/init.pp
+++ modules/standard/manifests/init.pp
@@ -34,4 +34,18 @@ class standard(
         include ::admin
     }
 
+    # For historical reasons, users in modules/admin/data/data.yaml
+    # (for production) and in LDAP (for Labs) start at uid/gid 500, so
+    # we need to guard against system users being created in that
+    # range.
+    file_line { 'login.defs-SYS_UID_MAX':
+        path     => '/etc/login.defs',
+        match    => '#?SYS_UID_MAX\b',
+        line     => 'SYS_UID_MAX               499',
+    }
+    file_line { 'login.defs-SYS_GID_MAX':
+        path     => '/etc/login.defs',
+        match    => '#?SYS_GID_MAX\b',
+        line     => 'SYS_GID_MAX               499',
+    }
 }
