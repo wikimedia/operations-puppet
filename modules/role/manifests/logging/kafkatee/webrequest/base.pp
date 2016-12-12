7fcd593032c864af779529932573181275c72c50
Andrew Otto <acotto@gmail.com>
2016-12-12 10:38:42 -0500

    Remove webrequest_bits config from kafkatee input
    bits no longer exists
    
    Change-Id: Ia695007b45d52c53616938283d42d7a429f517aa

--- modules/role/manifests/logging/kafkatee/webrequest/base.pp
+++ modules/role/manifests/logging/kafkatee/webrequest/base.pp
@@ -20,21 +20,11 @@ class role::logging::kafkatee::webrequest::base {
     }
     include kafkatee::monitoring
 
-    # TODO: Do we need all topics for ops debugging of webrequest logs?
-
     # Include all webrequest topics as inputs.
     # Note:  we used offset => 'end' rather than 'stored'
     # because we don't need to backfill these files from
     # buffered kafka data if kafkatee goes down.
     # These are just logs for ops debugging.
-    kafkatee::input { 'kafka-webrequest_bits':
-        topic      => 'webrequest_bits',
-        partitions => '0-11',
-        options    => {
-            'encoding' => 'json',
-        },
-        offset     => 'end',
-    }
     kafkatee::input { 'kafka-webrequest_misc':
         topic      => 'webrequest_misc',
         partitions => '0-11',
