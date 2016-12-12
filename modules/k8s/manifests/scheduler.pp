c4b5bb2587b7ea14008cd7d759ae1332bef40977
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-12-12 15:16:50 +0200

    kube-scheduler: Amend to support more than labs
    * Allow using packages, still supporting labs via symlink
    * Use /etc/default/kube-scheduler
    
    Change-Id: Ibec9fa8ad6a8d66ec6bb5f2e27e9cf1a9efacc2d

--- modules/k8s/manifests/scheduler.pp
+++ modules/k8s/manifests/scheduler.pp
@@ -1,5 +1,24 @@
-class k8s::scheduler {
+class k8s::scheduler(
+    $use_package = false,
+) {
     include k8s::users
+
+    if $use_package {
+        require_package('kubernetes-master')
+    } else {
+        file { '/usr/local/bin/kube-scheduler':
+            ensure => link,
+            target => '/usr/bin/kube-scheduler',
+        }
+    }
+
+    file { '/etc/default/kube-scheduler':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('k8s/kube-scheduler.default.erb'),
+    }
 
     base::service_unit { 'kube-scheduler':
         systemd => true,
