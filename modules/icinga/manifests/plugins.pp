40008b9b299c693bdb6e24de113f61cfff79d5ef
andrewbogott <abogott@wikimedia.org>
2016-12-12 16:23:05 -0600

    Keystone: Monitor project membership
    This warns unless:
    
    - novaobserver has (and only has) the 'observer' role in all projects
    - novaadmin has (and only has) the 'user' and 'projectadmin' role in all projects
    
    Bug: T152708
    Change-Id: I932f51a9adf82944792c238f4d121dea5c6cfb6e

--- modules/icinga/manifests/plugins.pp
+++ modules/icinga/manifests/plugins.pp
@@ -73,6 +73,12 @@ class icinga::plugins {
         group  => 'root',
         mode   => '0755',
     }
+    file { '/usr/lib/nagios/plugins/keystonerolechecks':
+        source => 'puppet:///modules/icinga/keystonerolechecks',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
     file { '/usr/lib/nagios/plugins/check_mysql-replication.pl':
         source => 'puppet:///modules/icinga/check_mysql-replication.pl',
         owner  => 'root',
