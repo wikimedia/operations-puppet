3c0a5a02240c7367935d2e82c09653a04f875eea
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-12-12 16:10:28 +0200

    k8s::controller: Amend to support more than labs
    * Allow using packages, still supporting labs via symlink
    * Use /etc/default/kube-controller-manager
    * Change the systemd unit name to comply with the rest of the unit
    naming scheme
    * Add a cluster-cidr parameter to allow overriding the default
    
    Change-Id: I18e0a603193ddbd259bde504bfd29775d9036463

--- modules/k8s/manifests/controller.pp
+++ modules/k8s/manifests/controller.pp
@@ -1,7 +1,27 @@
-class k8s::controller {
+class k8s::controller(
+    $use_package = false,
+    $cluster_cidr = '192.168.0.0/24',
+){
     include k8s::users
 
-    base::service_unit { 'controller-manager':
+    if $use_package {
+        require_package('kubernetes-master')
+    } else {
+        file { '/usr/bin/kube-controller-manager':
+            ensure => link,
+            target => '/usr/local/bin/kube-controller-manager',
+        }
+    }
+
+    file { '/etc/default/kube-controller-manager':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('k8s/kube-controller-manager.default.erb'),
+    }
+
+    base::service_unit { 'kube-controller-manager':
         systemd => true,
     }
 }
