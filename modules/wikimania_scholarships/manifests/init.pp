1239fb4e17e46f1e6a0229762cb7e616eb516b20
niharika29 <nkohli@wikimedia.org>
2016-12-12 16:20:22 +0000

    Deploy scholarships with scap3
    Bug: T129134
    Change-Id: I24094dd9335b930f9933d8f58d53f5be3ae25e23

--- modules/wikimania_scholarships/manifests/init.pp
+++ modules/wikimania_scholarships/manifests/init.pp
@@ -43,21 +43,16 @@ class wikimania_scholarships(
         description => 'Wikimania Scholarships server'
     }
 
-    package { 'scholarships':
-        provider => 'trebuchet',
+    scap::target { 'wikimedia/wikimania-scholarships':
+        service_name => 'scholarships',
+        deploy_user  => 'deploy-service'
     }
 
     apache::site { 'scholarships.wikimedia.org':
         content => template('wikimania_scholarships/apache.conf.erb'),
     }
 
-    ensure_resource('file', '/srv/deployment', {'ensure' => 'directory' })
-
-    file { [ '/srv/deployment/scholarships', $deploy_dir ]:
-        ensure  => directory,
-    }
-
-    file { "${deploy_dir}/.env":
+    file { '/etc/wikimania-scholarships.ini':
         ensure  => present,
         mode    => '0444',
         owner   => 'root',
