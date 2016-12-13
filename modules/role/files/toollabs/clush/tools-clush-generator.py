472396f0f42312d341ee65b05055327add55aaa1
Tim Landscheidt <tim@tim-landscheidt.de>
2016-12-13 04:34:10 +0000

    Tools: Generate node sets dynamically
    Currently, the list of nodes per hostgroup is regenerated every
    minute.  This is far more often than clush is actually used according
    to the log file, and additionally this two-step process can result in
    confusing behaviour in cases where the cron job fails in some way.
    
    This change generates the node sets dynamically, on demand.  Thus they
    are always up-to-date and errors in generating the list make
    themselves known immediately.  Technically, the generated "load" on
    wikitech will be reduced, but given the low frequency of API calls
    (once per clush invocation) this is not a factor.
    
    In addition, the script tools-clush-generator.py does not need to run
    in Wikimedia Labs.  This makes it possible to set up
    ~/.config/clustershell/groups.conf as:
    
    | [Main]
    | default: Tools
    
    | [Tools]
    | map: /path/to/tools-clush-generator.py map --project tools $GROUP
    | list: /path/to/tools-clush-generator.py list
    
    | [Toolsbeta]
    | map: /path/to/tools-clush-generator.py map --project toolsbeta $GROUP
    | list: /path/to/toollabs/clush/tools-clush-generator.py list
    
    and subsequently use "clush -g webgrid-lighttpd-precise $COMMAND" or
    "clush -s Toolsbeta -g all $COMMAND" from outside Labs.
    
    Change-Id: I5bb630836beb1c6a2aa2b346bef03687db9f4732

---
+++ modules/role/files/toollabs/clush/tools-clush-generator.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+
+
+"""Simple generator script for clustershell to dynamically list all
+   instances and classify them into groups based on their hostname.
+
+"""
+
+
+import argparse
+import json
+import urllib.request
+
+
+# Maps hostgroup names to prefixes.
+TOOLS_HOSTGROUPS = {
+    'all': '',
+    'bastion': 'bastion-',
+    'checker': 'checker-',
+    'cron': 'cron-',
+    'docker-builder': 'docker-builder',
+    'exec': 'exec-',
+    'exec-precise': 'exec-12',
+    'exec-trusty': 'exec-14',
+    'flannel-etcd': 'flannel-etcd',
+    'grid-master': 'grid-master',
+    'grid-shadow': 'grid-shadow',
+    'k8s-etcd': 'k8s-etcd',
+    'k8s-master': 'k8s-master',
+    'k8s-worker': 'worker',
+    'logs': 'logs',
+    'mail': 'mail',
+    'precise-dev': 'precise-dev',
+    'prometheus': 'prometheus',
+    'redis': 'redis-',
+    'services': 'services-',
+    'static': 'static-',
+    'webgrid': 'webgrid-',
+    'webgrid-generic': 'webgrid-generic',
+    'webgrid-lighttpd-precise': 'webgrid-lighttpd-12',
+    'webgrid-lighttpd-trusty': 'webgrid-lighttpd-14',
+    'webproxy': 'proxy-',
+}
+
+
+def list_hostgroups():
+    """List all hostgroups on stdout."""
+    for hostgroup in sorted(TOOLS_HOSTGROUPS):
+        print(hostgroup)
+
+
+def list_nodes(project_name, hostgroup):
+    """List all nodes of a hostgroup on stdout."""
+    prefix = '%s-%s' % (project_name, TOOLS_HOSTGROUPS[hostgroup])
+
+    api_url = 'https://wikitech.wikimedia.org/w/api.php' \
+              '?action=query&list=novainstances&niregion=eqiad&format=json' \
+              '&niproject=' + project_name
+
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent', __file__)]
+    data = json.loads(opener.open(api_url).read().decode('utf-8'))
+
+    for instance in data['query']['novainstances']:
+        name = instance['name']
+        if name.startswith(prefix):
+            print('%s.%s.eqiad.wmflabs' % (name, project_name))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='action')
+    subparsers.required = True
+
+    parser_map = subparsers.add_parser(
+        'map',
+        help='Print list of hosts in a hostgroup'
+    )
+    parser_map.add_argument(
+        '--project',
+        help='Name of project whose instances should be printed',
+        required=True
+    )
+    parser_map.add_argument(
+        'group',
+        help='Name of group whose instances should be printed'
+    )
+
+    parser_list = subparsers.add_parser(
+        'list',
+        help='List all hostgroups',
+    )
+
+    args = parser.parse_args()
+    if args.action == 'list':
+        list_hostgroups()
+    elif args.action == 'map':
+        list_nodes(args.project, args.group)
