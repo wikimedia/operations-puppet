6960802c6c41dd9ea77460280fd520d4d9b52ce8
Kaldari <rkaldari@wikimedia.org>
2016-12-12 16:02:23 -0800

    mediawiki: Add cron job for PageAssessments maintenance script
    Bug: T153026
    Change-Id: I6e76452a310a054bc99a41947a786ca999bb436f

--- modules/role/manifests/mediawiki/maintenance.pp
+++ modules/role/manifests/mediawiki/maintenance.pp
@@ -28,6 +28,7 @@ class role::mediawiki::maintenance {
     include mediawiki::maintenance::jobqueue_stats
     include mediawiki::maintenance::cirrussearch
     include mediawiki::maintenance::generatecaptcha
+    include mediawiki::maintenance::pageassessments
 
     # backup home directories to bacula, people work on these
     include backup::host
