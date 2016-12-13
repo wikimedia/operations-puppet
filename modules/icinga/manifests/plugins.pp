4e0fe444165bf5d9cc7ba5f12114c193645e19d9
andrewbogott <abogott@wikimedia.org>
2016-12-12 20:08:32 -0600

    Keystone:  add existence checks for critical projects
    Bug: T152708
    Change-Id: I8bb8b64fb45caf455b45f9632b9f0d43e2ebd3e7

--- modules/icinga/manifests/plugins.pp
+++ modules/icinga/manifests/plugins.pp
@@ -79,6 +79,12 @@ class icinga::plugins {
         group  => 'root',
         mode   => '0755',
     }
+    file { '/usr/lib/nagios/plugins/check_keystone_projects':
+        source => 'puppet:///modules/icinga/check_keystone_projects',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
     file { '/usr/lib/nagios/plugins/check_mysql-replication.pl':
         source => 'puppet:///modules/icinga/check_mysql-replication.pl',
         owner  => 'root',
@@ -164,6 +170,14 @@ class icinga::plugins {
     nagios_common::check_command::config { 'check_keystone_roles.cfg':
         ensure     => present,
         content    => template('icinga/check_commands/check_keystone_roles.cfg.erb'),
+        config_dir => '/etc/icinga',
+        owner      => 'icinga',
+        group      => 'icinga'
+    }
+
+    nagios_common::check_command::config { 'check_keystone_projects.cfg':
+        ensure     => present,
+        content    => template('icinga/check_commands/check_keystone_projects.cfg.erb'),
         config_dir => '/etc/icinga',
         owner      => 'icinga',
         group      => 'icinga'
