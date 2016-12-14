229b8b306b8dddc7259d31dc90febd9bf1c87df0
elukey <ltoscano@wikimedia.org>
2016-12-14 18:40:09 +0100

    Add the prometheus-apache-exporter module and role
    Bug: T147316
    Change-Id: If66ba8e532f3d9ea7b925e44ae360dbffd3b330a

---
+++ modules/prometheus/manifests/apache_exporter.pp
@@ -0,0 +1,28 @@
+# == Define: prometheus::apache_exporter
+#
+# Prometheus exporter for Apache httpd server metrics.
+#
+# = Parameters
+#
+# [*arguments*]
+#   Additional command line arguments for prometheus-apache-exporter.
+
+define prometheus::apache_exporter (
+    $arguments = '-scrape_uri http://127.0.0.1/server-status/?auto',
+) {
+    require_package('prometheus-apache-exporter')
+
+    file { '/etc/default/prometheus-apache-exporter':
+        ensure  => present,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => "ARGS=\"${arguments}\"",
+        notify  => Service['prometheus-apache-exporter'],
+    }
+
+    service { 'prometheus-apache-exporter':
+        ensure  => running,
+        require => Package['prometheus-apache-exporter'],
+    }
+}
