e3bbad8e4ea78e9d5fb570f365a5c8a17ba3fcd8
elukey <ltoscano@wikimedia.org>
2016-12-14 18:40:09 +0100

    Add the prometheus-apache-exporter module and role
    Bug: T147316
    Change-Id: If66ba8e532f3d9ea7b925e44ae360dbffd3b330a

---
+++ modules/role/manifests/prometheus/apache_exporter.pp
@@ -0,0 +1,14 @@
+class role::prometheus::apache_exporter {
+    prometheus::apache_exporter { 'default': }
+
+    $prometheus_nodes = hiera('prometheus_nodes')
+    $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
+    $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
+
+    ferm::service { 'prometheus-apache_exporter':
+        proto  => 'tcp',
+        port   => '9117',
+        srange => $ferm_srange,
+    }
+}
+
