ff9246bab355beaf6195c89dc38eea2ea69c63b2
Daniel Zahn <dzahn@wikimedia.org>
2016-12-13 17:40:44 -0800

    lists/exim: move files from /files to role module
    Change-Id: Iabed0cace1d5d68d8095695ff04881f6cda557ef

--- modules/role/manifests/mail/mx.pp
+++ modules/role/manifests/mail/mx.pp
@@ -55,10 +55,7 @@ class role::mail::mx(
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
-        # move to exim module?
-        # lint:ignore:puppet_url_without_modules
-        source  => 'puppet:///files/exim/wikimedia_domains',
-        # lint:endignore
+        source  => 'puppet:///modules/role/exim/wikimedia_domains',
         require => Class['exim4'],
     }
 
@@ -67,10 +64,7 @@ class role::mail::mx(
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
-        # move to mailman module?
-        # lint:ignore:puppet_url_without_modules
-        source  => 'puppet:///files/exim/legacy_mailing_lists',
-        # lint:endignore
+        source  => 'puppet:///modules/role/exim/legacy_mailing_lists',
         require => Class['exim4'],
     }
     exim4::dkim { 'wikimedia.org':
