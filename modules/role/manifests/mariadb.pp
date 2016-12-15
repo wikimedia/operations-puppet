c3a5e55cda912fa1ae2af8438720c827d95ed62e
Jaime Crespo <jcrespo@wikimedia.org>
2016-12-15 19:04:12 +0100

    mariadb: Move role::mariadb::client to a separate file
    Bug: T150850
    Change-Id: I357fe30fa3c3f7d757e4c403daeced5af84e10d8

--- modules/role/manifests/mariadb.pp
+++ modules/role/manifests/mariadb.pp
@@ -1026,25 +1026,3 @@ class role::mariadb::maintenance {
         tendril_password => $passwords::tendril::db_pass,
     }
 }
-
-# hosts with client utilities to conect to remote servers
-# This role provides remote access to mysql server
-# **DO NOT ADD** to non-dedicated hosts**
-class role::mariadb::client {
-    include mariadb::packages_client
-    include passwords::misc::scripts
-
-    class { 'mariadb::config':
-        config => 'role/mariadb/mysqld_config/client.my.cnf.erb',
-        ssl    => 'puppet-cert',
-    }
-
-    $password = $passwords::misc::scripts::mysql_root_pass
-    file { '/root/.my.cnf':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0400',
-        content => template('mariadb/root.my.cnf.erb'),
-    }
-
-}
