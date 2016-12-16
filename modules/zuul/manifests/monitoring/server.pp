98bd5caa72467e14be008e3dcd973c712391912e
Daniel Zahn <dzahn@wikimedia.org>
2016-12-15 18:40:47 -0800

    contint/zuul: skip Icinga monitoring if server not master
    For zuul, if the FQDN of the current server does not match
    the one set as "master" in Hiera, then skip the Icinga monitoring
    part since the zuul service will not be running.
    
    If one of the server becomes active, automatically add the monitoring
    there without having to care about hostnames.
    
    Bug: T150771
    Change-Id: I50db45303a6b466abb15d7a8a5f61e75d2947cc7

--- modules/zuul/manifests/monitoring/server.pp
+++ modules/zuul/manifests/monitoring/server.pp
@@ -4,18 +4,29 @@
 #
 class zuul::monitoring::server {
 
-    nrpe::monitor_service { 'zuul':
-        description   => 'zuul_service_running',
-        contact_group => 'contint',
-        # Zuul has a main process and a fork which is the gearman
-        # server. Thus we need two process running.
-        nrpe_command  => "/usr/lib/nagios/plugins/check_procs -w 2:2 -c 2:2 --ereg-argument-array '^/usr/share/python/zuul/bin/python /usr/bin/zuul-server'",
-    }
+    $master_host = hiera(contint::master_host)
 
-    nrpe::monitor_service { 'zuul_gearman':
-        description   => 'zuul_gearman_service',
-        contact_group => 'contint',
-        nrpe_command  => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 4730 --timeout=2',
+    $monitoring_active = $master_host ? {
+        $::fqdn => true,
+        default => false
     }
+    # only monitor these on the active master host
+    # zuul service will be stopped on the warm standby server
+    if $monitoring_active {
 
+        nrpe::monitor_service { 'zuul':
+            description   => 'zuul_service_running',
+            contact_group => 'contint',
+            # Zuul has a main process and a fork which is the gearman
+            # server. Thus we need two process running.
+            nrpe_command  => "/usr/lib/nagios/plugins/check_procs -w 2:2 -c 2:2 --ereg-argument-array '^/usr/share/python/zuul/bin/python /usr/bin/zuul-server'",
+        }
+
+        nrpe::monitor_service { 'zuul_gearman':
+            description   => 'zuul_gearman_service',
+            contact_group => 'contint',
+            nrpe_command  => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 4730 --timeout=2',
+        }
+
+    }
 }
