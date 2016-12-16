81dd3015a5a0f06374c0b6586f4324cbb86818d2
elukey <ltoscano@wikimedia.org>
2016-12-16 14:59:55 +0100

    Apply the hhvm/apache prometheus exporter roles to the MW appservers
    Bug: T147423
    Bug: T147316
    Change-Id: I8c8bf1d8fc96386b56fc455f7b3991638356d35a

--- modules/role/manifests/mediawiki/appserver.pp
+++ modules/role/manifests/mediawiki/appserver.pp
@@ -3,4 +3,9 @@ class role::mediawiki::appserver {
 
     include ::role::mediawiki::webserver
 
+    # Test of the new HHVM and Apache Prometheus exporters
+    # Bug: T147423, T147316
+    include ::role::prometheus::apache_exporter
+    include ::role::prometheus::hhvm_exporter
+
 }
