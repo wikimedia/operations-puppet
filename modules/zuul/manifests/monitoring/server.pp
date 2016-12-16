3d5b6bdba4ea3c33cdac0cde82b0df29e185c0c6
Daniel Zahn <dzahn@wikimedia.org>
2016-12-15 18:40:47 -0800

    contint/zuul: skip Icinga monitoring if server not master
    For zuul, if the FQDN of the current server does not match
    the one set as "master" in Hiera, then skip the Icinga monitoring
    part since the zuul service will not be running.
    
    If one of the server becomes active, automatically add the monitoring
    there without having to care about hostnames.
    
    Bug: T150771
    Signed-off-by: Antoine Musso <hashar@free.fr>
    Change-Id: I50db45303a6b466abb15d7a8a5f61e75d2947cc7

--- modules/zuul/manifests/monitoring/server.pp
+++ modules/zuul/manifests/monitoring/server.pp
@@ -2,9 +2,19 @@
 #
 # Icinga monitoring for the Zuul server
 #
-class zuul::monitoring::server {
+# == Parameters
+#
+# [*ensure*]
+#
+class zuul::monitoring::server (
+    $ensure = present,
+) {
+    validate_ensure($ensure)
 
+    # only monitor these on the active master host
+    # zuul service will be stopped on the warm standby server
     nrpe::monitor_service { 'zuul':
+        ensure        => $ensure,
         description   => 'zuul_service_running',
         contact_group => 'contint',
         # Zuul has a main process and a fork which is the gearman
@@ -13,9 +23,9 @@ class zuul::monitoring::server {
     }
 
     nrpe::monitor_service { 'zuul_gearman':
+        ensure        => $ensure,
         description   => 'zuul_gearman_service',
         contact_group => 'contint',
         nrpe_command  => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 4730 --timeout=2',
     }
-
 }
