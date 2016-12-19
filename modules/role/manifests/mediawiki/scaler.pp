33dc562e37a506f82ce13c95a523ead6de7ccc8d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-12-19 17:41:58 +0100

    mediawiki::scaler: check orphaned HHVM threads
    Due to discrepancies in the timeouts, we might have jobs piling up on
    HHVM that are long gone in apache2; since this can be a problem (esp. on
    videoscalers), we start monitorning this.
    
    Bug: T153488
    Change-Id: Ibcee6af40c678c835c741c100d9fa4df1d99fd86

--- modules/role/manifests/mediawiki/scaler.pp
+++ modules/role/manifests/mediawiki/scaler.pp
@@ -8,5 +8,20 @@ class role::mediawiki::scaler {
         group  => 'root',
         mode   => '0444',
     }
-}
 
+    # monitor orphaned HHVM threads/requests that are no longer in apache
+    # see https://phabricator.wikimedia.org/T153488
+    file { '/usr/local/lib/nagios/plugins/check_leaked_hhvm_threads':
+        ensure => present,
+        source => 'puppet:///modules/role/mediawiki/check_leaked_hhvm_threads.py',
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    nrpe::monitor_service { 'check_leaked_hhvm_threads':
+        description  => 'Check HHVM threads for leakage',
+        nrpe_command => '/usr/local/lib/nagios/plugins/check_leaked_hhvm_threads',
+        require      => File['/usr/local/lib/nagios/plugins/check_leaked_hhvm_threads'],
+    }
+}
