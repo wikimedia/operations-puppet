8f12ab08767bfd9b4f7319c3fa5b9921b013c69d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2016-12-19 17:41:58 +0100

    mediawiki::scaler: check orphaned HHVM threads
    Due to discrepancies in the timeouts, we might have jobs piling up on
    HHVM that are long gone in apache2; since this can be a problem (esp. on
    videoscalers), we start monitorning this.
    
    Bug: T153488
    Change-Id: Ibcee6af40c678c835c741c100d9fa4df1d99fd86

---
+++ modules/role/files/mediawiki/check_leaked_hhvm_threads.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2016 Giuseppe Lavagetto <joe@wikimedia.org>
+#
+#  Permission to use, copy, modify, and/or distribute this software for any
+#  purpose with or without fee is hereby granted, provided that the above
+#  copyright notice and this permission notice appear in all copies.
+#
+#  THE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+#  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+#  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+#  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+#  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+'''
+check_leaked_hhvm_threads
+
+usage: check_leaked_hhvm_threads
+
+Checks that there aren't many orphaned HHVM threads that are still running detached
+from the apache frontend.
+'''
+# This can happen because of bugs in apache, see
+# e.g. https://bz.apache.org/bugzilla/show_bug.cgi?id=56188
+import re
+import requests
+import sys
+
+# I know there is a race condition here. But we can live with that.
+try:
+    apache_status = requests.get('http://127.0.0.1/server-status?auto')
+    match = re.search('BusyWorkers: (\d)', apache_status.text)
+    if not match:
+        print('UNKNOWN - Could not find apache busy workers in apache status')
+        sys.exit(3)
+    busy_workers = int(match.group(1))
+except:
+    print('UNKNOWN - Error fetching apache status')
+    sys.exit(3)
+
+
+try:
+    hhvm_health = requests.get('http://127.0.0.1:9002/check-health').json()
+    hhvm_reqs = hhvm_health['load'] + hhvm_health['queued']
+except:
+    print('UNKNOWN - Error fetching the HHVM status')
+
+if hhvm_reqs <= busy_workers * 1.2:
+    print('OK')
+    sys.exit(0)
+elif hhvm_reqs <= busy_workers * 2.4:
+    print('WARNING: hhvm is leaking a few threads')
+    sys.exit(1)
+else:
+    print('CRITICAL: HHVM has more than double threads running or queued than apache has busy workers')
+    sys.exit(2)
