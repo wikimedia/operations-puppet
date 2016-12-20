97b57fbdbe9f52abc5eac1b5645d19de9ae6fbf2
andrewbogott <abogott@wikimedia.org>
2016-12-20 01:09:02 -0600

    Keystone:  Move api service to uwsgi/nginx
    WIP
    
    Bug: T150774
    Change-Id: I89e9076cd2c830f43b6fabe8ed545d84c425f178

--- modules/role/manifests/labs/openstack/keystone.pp
+++ modules/role/manifests/labs/openstack/keystone.pp
@@ -30,4 +30,39 @@ class role::labs::openstack::keystone::server {
         description   => 'Keystone admin and observer projects exist',
         check_command => 'check_keystone_projects',
     }
+
+    # Keystone admin API
+    service::uwsgi { 'keystone_admin':
+        port            => $keystoneconfig['auth_port'],
+        healthcheck_url => '/',
+        deployment      => None,
+        config          => {
+            wsgi-file    => '/usr/bin/keystone-wsgi-public',
+            uid          => 'keystone',
+            processes    => '10',
+            threads      => '2',
+            chmod-socket => '666',
+            logger       => [
+                'local file:/var/log/keystone/keystone-admin.log',
+            ],
+            log-route    => ['local .*'],
+        },
+    }
+    service::uwsgi { 'keystone_public':
+        port            => $keystoneconfig['public_port'],
+        healthcheck_url => '/',
+        deployment      => None,
+        config          => {
+            wsgi-file    => '/usr/bin/keystone-wsgi-public',
+            uid          => 'keystone',
+            processes    => '10',
+            threads      => '2',
+            chmod-socket => '666',
+            logger       => [
+                'local file:/var/log/keystone/keystone-public.log',
+            ],
+            log-route    => ['local .*'],
+
+        },
+    }
 }
