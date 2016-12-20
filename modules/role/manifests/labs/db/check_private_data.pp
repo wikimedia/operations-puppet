11e876174f00329d8a75a44ac5a8bca8c48ea824
Manuel Arostegui <marostegui@wikimedia.org>
2016-12-20 13:02:25 +0100

    [WIP] Reporting tests with the private data script
    * For now just run the private data script and  email me
    once something is found so it can be polished.
    * Scheduled to run once per week now: every Monday.
    
    Ideally it should be an icinga check eventually.
    
    Bug: T153680
    Change-Id: I7796d6860f70c34b1758655f18a4ed8196724e97

--- modules/role/manifests/labs/db/check_private_data.pp
+++ modules/role/manifests/labs/db/check_private_data.pp
@@ -30,4 +30,24 @@ class role::labs::db::check_private_data {
                     File['/etc/mysql/private_tables.txt'],
         ],
     }
+
+    file { '/usr/local/sbin/check_private_data_report.sh':
+        ensure => file,
+        source => 'puppet:///modules/role/mariadb/check_private_data_report.sh',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0744',
+    }
+
+    cron { 'check-private-data':
+        minute  => 0,
+        hour    => 5,
+        weekday => 1,
+        user    => 'root',
+        command => '/usr/local/sbin/check_private_data_report.sh',
+        require => [File['/usr/local/sbin/check_private_data_report.sh'],
+                    File['/usr/local/sbin/check_private_data.py'],
+        ],
+    }
+
 }
