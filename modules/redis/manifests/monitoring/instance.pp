626b08c6e527a68f91402926a9ce216b6016eb60
elukey <ltoscano@wikimedia.org>
2016-12-21 16:12:10 +0100

    Increase Redis Replica Sync retry interval to 2 minutes
    We usually get false alarms that nobody really pay attention to
    about Redis replicas not being in sync with their master. The alarms
    always resolve by themselves in a couple of minutes.
    
    Change-Id: I1359ca052bacd59eccb6265713029b4c51d9e9aa

--- modules/redis/manifests/monitoring/instance.pp
+++ modules/redis/manifests/monitoring/instance.pp
@@ -38,8 +38,9 @@ define redis::monitoring::instance(
 
     if ($slaveof) {
         monitoring::service{ "redis.${instance_name}":
-            description   => "Redis status ${instance_name}",
-            check_command => "check_redis_replication!${port}!${lag_warning}!${lag_critical}"
+            description    => "Redis status ${instance_name}",
+            check_command  => "check_redis_replication!${port}!${lag_warning}!${lag_critical}",
+            retry_interval => 2,
 
         }
     } else {
