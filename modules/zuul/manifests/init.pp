3de12386afc0234e5aab5de577ee2283a5fe2975
andrewbogott <abogott@wikimedia.org>
2016-12-21 11:29:30 -0600

    Try to pin python-pbr
    WIP, doesn't work
    
    Change-Id: If7edd5eaed50561b0c0ce57d64f4544eedce8dbd

--- modules/zuul/manifests/init.pp
+++ modules/zuul/manifests/init.pp
@@ -11,6 +11,15 @@ class zuul ( ){
 
     include zuul::user
 
+    if os_version('debian == jessie') {
+        apt::pin { 'python-pbr':
+            pin      => 'release a=jessie-main',
+            priority => '1002',
+            before   => Package['zuul'],
+        }
+    }
+
+
     package { 'zuul':
         ensure => present,
     }
