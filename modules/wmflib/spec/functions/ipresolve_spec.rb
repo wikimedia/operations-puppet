4d24d547f5e3136636415e122f7cbb061f86d052
Daniel Zahn <dzahn@wikimedia.org>
2016-12-21 16:28:35 -0800

    wmflib: replace carbon with install1001 in ipresolve tests
    Bug: T123733
    Change-Id: I2f2d84e6d9acded711d809607bb395201ca2581c

--- modules/wmflib/spec/functions/ipresolve_spec.rb
+++ modules/wmflib/spec/functions/ipresolve_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 describe 'ipresolve' do
 
   it "should resolve ipv4 addresses by default" do
-    should run.with_params('carbon.wikimedia.org').and_return('208.80.154.10')
+    should run.with_params('carbon.wikimedia.org').and_return('208.80.154.83')
   end
   it "should resolve ipv4 addresses when explicitly asked to" do
-    should run.with_params('carbon.wikimedia.org', '4').and_return('208.80.154.10')
+    should run.with_params('install1001.wikimedia.org', '4').and_return('208.80.154.83')
   end
 
   it "should resolve ipv6 addresses" do
-    should run.with_params('carbon.wikimedia.org', '6').and_return('2620::861:1:208:80:154:10')
+    should run.with_params('install1001.wikimedia.org', '6').and_return('2620::861:1:208:80:154:83')
   end
 
   it "should be able to perform a reverse DNS lookup" do
-    should run.with_params('2620::861:1:208:80:154:10', 'ptr').and_return('carbon.wikimedia.org')
-    should run.with_params('208.80.154.10', 'ptr').and_return('carbon.wikimedia.org')
+    should run.with_params('2620::861:1:208:80:154:83', 'ptr').and_return('install1001.wikimedia.org')
+    should run.with_params('208.80.154.83', 'ptr').and_return('install1001.wikimedia.org')
   end
 
   it "fails when resolving an inexistent name" do
