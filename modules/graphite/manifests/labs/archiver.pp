e529701e96bcb3f074ed130ac704f59061f04ebc
Alex Monk <alex@wikimedia.org>
2016-12-22 02:47:52 +0100

    graphite: Don't use wikitech API to find labs projects/instances
    Use the OpenStack API instead. The new observer credentials will do
    for this purpose.
    
    Also get rid of the (seemingly now unused) HOSTNAME_MAPPING.
    
    Some parts tested.
    
    Bug: T104575
    Change-Id: Ia7ae8975bb79a1587ae5cbbb66849f9d042e7ae2

--- modules/graphite/manifests/labs/archiver.pp
+++ modules/graphite/manifests/labs/archiver.pp
@@ -10,9 +10,12 @@ class graphite::labs::archiver {
         mode   => '0700',
     }
 
+    $novaconfig = hiera_hash('novaconfig', {})
+    $observer_pass = $novaconfig['observer_password']
+    include ::openstack::clientlib
     cron { 'archive-deleted-instances':
         ensure  => present,
-        command => '/usr/local/bin/archive-instances',
+        command => "/usr/local/bin/archive-instances ${observer_pass}",
         user    => '_graphite',
         minute  => 0,
         hour    => 13,
