5884532e595d437becbce353f0322027f0f2ae04
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-12-23 13:34:29 +0200

    profile::kubernetes::master: Specify no authz_mode
    For production, we are going to rely for starters on ferm rules to block
    access to the apiserver and evaluate authorization models later on. So
    for now specify authz_mode to empty string to default to the AllowAll mode
    and fix the ferm rules
    
    Change-Id: Ia17f68ea6a58d75ffd350d123fc31f35f24496d7

--- modules/profile/manifests/kubernetes/master.pp
+++ modules/profile/manifests/kubernetes/master.pp
@@ -1,5 +1,6 @@
 class profile::kubernetes::master(
     $etcd_urls=hiera('profile::kubernetes::master::etcd_urls'),
+    $kubenodes=hiera('profile::kubernetes::master::kubenodes'),
     $docker_registry=hiera('profile::kubernetes::master::docker_registry'),
 ){
     base::expose_puppet_certs { '/etc/kubernetes':
@@ -15,15 +16,18 @@ class profile::kubernetes::master(
         ssl_cert_path        => '/etc/kubernetes/ssl/cert.pem',
         ssl_key_path         => '/etc/kubernetes/ssl/server.key',
         ssl_certificate_name => '',
+        authz_mode           => '',
     }
 
     class { '::k8s::scheduler': use_package => true }
     class { '::k8s::controller': use_package => true }
 
+    $kubenodes_ferm = join($kubenodes, ' ')
+
     ferm::service { 'apiserver-https':
         proto  => 'tcp',
         port   => '6443',
-        srange => '$DOMAIN_NETWORKS',
+        srange => "(@resolve((${kubenodes_ferm})))",
     }
 
     diamond::collector { 'Kubernetes':
