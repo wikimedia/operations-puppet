c7b252d3236df67826a34cb556689f87953f9410
Bryan Davis <bd808@wikimedia.org>
2016-12-30 16:24:18 -0700

    vagrant: remove setup.sh call
    The mediawiki-vagrant Vagrant plugin is no longer installed as a true
    plugin (c8ca59051837). The unless condition on the setup.sh exec does
    not keep the exec from firing anymore.
    
    Change-Id: I74901190d640016356794d1229e13fe36f3788b5

--- modules/vagrant/manifests/mediawiki.pp
+++ modules/vagrant/manifests/mediawiki.pp
@@ -41,27 +41,12 @@ class vagrant::mediawiki(
         require   => User['mwvagrant'],
     }
 
-    exec { 'mediawiki_vagrant_setup':
-        command     => "${install_directory}/setup.sh --silent",
-        # The first call to vagrant will fail if the plugin isn't installed at
-        # all. The second call will exit with an error if the plugin is
-        # installed but out of date.
-        unless      => '/usr/bin/vagrant plugin list | /bin/grep -q mediawiki-vagrant && /usr/bin/vagrant config --list',
-        cwd         => $install_directory,
-        user        => 'mwvagrant',
-        environment => [
-            "VAGRANT_HOME=${::vagrant::vagrant_home}",
-        ],
-        require     => Git::Clone['mediawiki/vagrant'],
-    }
-
     file { "${install_directory}/.settings.yaml":
         ensure  => 'present',
         source  => 'puppet:///modules/vagrant/default-settings.yaml',
         owner   => 'mwvagrant',
         group   => 'wikidev',
         replace => false,
-        before  => Exec['mediawiki_vagrant_setup'],
         require => Git::Clone['mediawiki/vagrant'],
     }
 
