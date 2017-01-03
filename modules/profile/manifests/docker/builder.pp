c380154c421cbbf88db72cc3360ddde6a0bed5ae
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-01-03 11:50:31 +0100

    profile::docker::builder: add alpine linux
    Also, adapt the coding to our own coding standards.
    
    Change-Id: I82dc465018184d558a12864fd9a150b4782cd100

--- modules/profile/manifests/docker/builder.pp
+++ modules/profile/manifests/docker/builder.pp
@@ -1,16 +1,31 @@
-class profile::docker::builder {
-    $proxy_address = hiera('profile::docker::builder::proxy_address')
-    $proxy_port = hiera('profile::docker::builder::proxy_port')
+# == Class profile::docker::builder
+#
+# This class sets up a docker builder server, where our base images can be built
+# and uploaded to the docker registry.
+#
+# === Parameters
+#
+# [*proxy_address*] The http proxy address, set to undef if you don't want to use item
+#
+# [*proxy_port*] The http proxy port; set to undef if not needed
+#
+# [*registry*] Address of the docker registry.
+#
+class profile::docker::builder(
+    $proxy_address = hiera('profile::docker::builder::proxy_address'),
+    $proxy_port = hiera('profile::docker::builder::proxy_port'),
+    $registry = hiera('docker::registry'),
+    ) {
 
     system::role { 'role::docker::builder':
         description => 'Docker images builder'
     }
 
     class { 'docker::baseimages':
-        docker_registry => hiera('docker::registry'),
+        docker_registry => $registry,
         proxy_address   => $proxy_address,
         proxy_port      => $proxy_port,
-        distributions   => ['jessie'],
+        distributions   => ['jessie', 'alpine'],
     }
 
     # TODO: create a repo for base images in prod for this
