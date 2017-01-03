dfccd1def469b5beea026ef8188299de5089e7f5
andrewbogott <abogott@wikimedia.org>
2017-01-03 13:52:10 -0600

    Add mirantis backports repo for Openstack classes on Jessie
    This won't be used in production for a good long while,
    but should help with testing.
    
    Change-Id: I16fabeaf49d6897a1772b540fccc83297b1ea850

--- modules/openstack/manifests/repo.pp
+++ modules/openstack/manifests/repo.pp
@@ -12,5 +12,18 @@ class openstack::repo(
             components => 'main',
             keyfile    => 'puppet:///modules/openstack/ubuntu-cloud.key';
         }
+    } elsif os_version('debian jessie') {
+        apt::repository { 'mirantis':
+            uri        => "http://${openstack_version}-jessie.pkgs.mirantis.com/debian",
+            dist       => "jessie-${openstack_version}-backports",
+            components => 'main',
+            keyfile    => "puppet:///modules/openstack/mirantis-${openstack_version}.key";
+        }
+        apt::repository { 'mirantis-nochange':
+            uri        => "http://${openstack_version}-jessie.pkgs.mirantis.com/debian",
+            dist       => "jessie-${openstack_version}-backports-nochange",
+            components => 'main',
+            keyfile    => "puppet:///modules/openstack/mirantis-${openstack_version}.key";
+        }
     }
 }
