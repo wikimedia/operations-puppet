dce03f31ddb465ed4753b9c3abfa11bcf4925d0a
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-01-04 16:54:45 +0000

    Cumin: allow connection to the targets
    Cumin automation framework need to connect via SSH to all hosts.
    This change covers the part to allow the cumin masters to connect to all
    hosts.
    
    Bug: T154588
    Change-Id: Id91cc18b1d426061d7c9c297ab275669d9369a42

--- modules/standard/manifests/init.pp
+++ modules/standard/manifests/init.pp
@@ -8,6 +8,7 @@ class standard(
     ) {
     include ::profile::base
     include ::standard::ntp
+    include ::profile::cumin::target
 
     if hiera('use_timesyncd', false) or (os_version('debian >= stretch')) {
         unless $::fqdn in $::standard::ntp::wmf_peers[$::site] {
