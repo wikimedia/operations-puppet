6a043041e7f31ae294034e7ddd0f2e88184b1c04
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-01-04 16:54:45 +0000

    Cumin: allow connection to the targets
    Cumin automation framework need to connect via SSH to all hosts.
    This change covers the part to allow the cumin masters to connect to all
    hosts.
    
    Bug: T154588
    Change-Id: Id91cc18b1d426061d7c9c297ab275669d9369a42

--- manifests/site.pp
+++ manifests/site.pp
@@ -2258,13 +2258,10 @@ node 'notebook1002.eqiad.wmnet' {
     include ::standard
 }
 
-# salt master
+# cluster management (salt master, cumin master) + other management tools
 node 'neodymium.eqiad.wmnet' {
-    role(salt::masters::production, deployment::salt_masters,
-      debdeploy::master, ipmi::mgmt, access_new_install, mariadb::client,
-      mgmt::drac_ilo)
-    include ::standard
-    include ::base::firewall
+    role(deployment::salt_masters, debdeploy::master, ipmi::mgmt,
+      access_new_install, mgmt::drac_ilo, cluster::management)
 }
 
 node 'nescio.wikimedia.org' {
@@ -2539,11 +2536,9 @@ node 'ruthenium.eqiad.wmnet' {
 
 }
 
-# salt master fallback
+# cluster management (salt master, cumin master)
 node 'sarin.codfw.wmnet' {
-    role(salt::masters::production, mariadb::client)
-    include ::standard
-    include ::base::firewall
+    role(cluster::management)
 }
 
 # T95046 install/deploy scandium as zuul merger (ci) server
---
+++ modules/profile/manifests/cumin/master.pp
@@ -0,0 +1,30 @@
+class profile::cumin::master (
+    $puppetdb_host  = hiera('puppetmaster::puppetdb::master'),
+    $cumin_log_path = '/var/log/cumin',
+) {
+    ::keyholder::agent { 'cumin_master':
+        trusted_groups => ['root'],
+    }
+
+    file { $cumin_log_path:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0750',
+    }
+
+    file { '/etc/cumin':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0750',
+    }
+
+    file { '/etc/cumin/config.yaml':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        content => template('profile/cumin/config.yaml.erb'),
+    }
+}
