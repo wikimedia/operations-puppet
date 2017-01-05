57ef63681a10bba25b41954ff4cbccd8b3beb306
Daniel Zahn <dzahn@wikimedia.org>
2017-01-04 22:06:22 -0800

    icinga: use Letsencrypt for SSL cert, spend less donor money on prime numbers
    On 2017-02-06 the icinga.wikimedia.org cert will expire.
    
    Instead of renewing and paying for a new icinga SSL cert
    as we did in the past, start using Letsencrypt for Icinga.
    
    We have been using Letsencrypt for Gerrit and RT for a while
    now and haven't had any complaints or problems.
    
    This saves money and having to deal with the cert renewal at all.
    
    Also see general tracking task to make all the things use LE.
    
    Bug: T133717
    Change-Id: I39abcfcc26461933e9afeb93bd229ce1f25d1266

--- modules/icinga/manifests/web.pp
+++ modules/icinga/manifests/web.pp
@@ -45,6 +45,7 @@ class icinga::web {
 
     apache::site { 'icinga.wikimedia.org':
         content => template('icinga/icinga.wikimedia.org.erb'),
+        require => Letsencrypt::Cert::Integrated['icinga'],
     }
 
     # remove icinga default config
