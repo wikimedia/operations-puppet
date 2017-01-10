3ffdc1a6dead552d08eccaa42a10f8eb5094e216
Antoine Musso <hashar@free.fr>
2017-01-10 10:44:35 +0100

    mailman: fix Unrecognized escape sequence '\;'
    Causes an error when using puppet parser validate under Puppet 3.7
    
    Change-Id: I2499ac6181a3a2204241e34112e765da831e842d

--- modules/mailman/manifests/cron.pp
+++ modules/mailman/manifests/cron.pp
@@ -3,7 +3,7 @@ class mailman::cron {
     # delete un-moderated held messages after 90 days (T109838)
     cron { 'delete_held_messages':
         ensure  => 'present',
-        command => "/usr/bin/find /var/lib/mailman/data/ -name 'heldmsg-*' -type f -mtime +90 -exec rm {} \; > /dev/null 2>&1",
+        command => "/usr/bin/find /var/lib/mailman/data/ -name 'heldmsg-*' -type f -mtime +90 -exec rm {} \\; > /dev/null 2>&1",
         user    => 'root',
         hour    => '3',
         minute  => '0',
