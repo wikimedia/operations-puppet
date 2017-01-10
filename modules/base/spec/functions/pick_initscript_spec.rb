aadb9406dc3b2a22ff0ca63ee3282af258c689b2
Antoine Musso <hashar@free.fr>
2017-01-10 18:26:59 +0100

    base: fix pick_initscript spec
    bf86143048c introduced has_systemd_override but hasn't updated the spec
    to add the new argument.  That caused the spec to fails because of an
    incorrect number of argument.
    
    Adjust the spec so that pick_initscript is always called with
    has_systemd_override = false
    
    Change-Id: I6c12bcc90dd4b138569541a6c4a1b9350f415f07

--- modules/base/spec/functions/pick_initscript_spec.rb
+++ modules/base/spec/functions/pick_initscript_spec.rb
@@ -8,25 +8,25 @@ describe 'pick_initscript' do
   end
 
   it 'Returns false if no init script provided' do
-    should run.with_params('apache2', 'systemd', false, false, false, true).and_return(false)
+    should run.with_params('apache2', 'systemd', false, false, false, false, true).and_return(false)
   end
 
   it 'Returns systemd if provided, sysvinit otherwise' do
-    should run.with_params('apache2', 'systemd', true, true, true, true).and_return('systemd')
-    should run.with_params('apache2', 'systemd', false, true, true, true).and_return('sysvinit')
+    should run.with_params('apache2', 'systemd', true, false, true, true, true).and_return('systemd')
+    should run.with_params('apache2', 'systemd', false, false, true, true, true).and_return('sysvinit')
   end
 
   it 'Returns upstart if provided, sysvinit otherwise' do
-    should run.with_params('apache2', 'upstart', true, true, true, true).and_return('upstart')
-    should run.with_params('apache2', 'upstart', true, false, true, true).and_return('sysvinit')
+    should run.with_params('apache2', 'upstart', true, false, true, true, true).and_return('upstart')
+    should run.with_params('apache2', 'upstart', true, false, false, true, true).and_return('sysvinit')
   end
 
   it 'Fails if on systemd and only upstart is provided, and vice versa' do
-    should run.with_params('apache2', 'upstart', true, false, false, true).and_raise_error(ArgumentError)
-    should run.with_params('apache2', 'systemd', false, true, false, true).and_raise_error(ArgumentError)
+    should run.with_params('apache2', 'upstart', true, false, false, false, true).and_raise_error(ArgumentError)
+    should run.with_params('apache2', 'systemd', false, false, true, false, true).and_raise_error(ArgumentError)
   end
 
   it 'Returns false on upstart if only systemd is provided, and strict is false' do
-    should run.with_params('apache2', 'upstart', true, false, false, false).and_return(false)
+    should run.with_params('apache2', 'upstart', true, false, false, false, false).and_return(false)
   end
 end
