2544943dd55f7622afc05539e8e42dad90e4b0e2
Brandon Black <bblack@wikimedia.org>
2017-01-11 22:39:09 +0000

    DNS: service discovery
    Bug: T156100
    Change-Id: I6726955110867db54a1c581ed7dc45ac7f3bfe28

---
+++ modules/authdns/manifests/discovery_statefile.pp
@@ -0,0 +1,18 @@
+
+define authdns::discovery_statefile($lvs, $lvs_services, $active_active) {
+    $check = $active_active ? {
+        false => '/usr/local/bin/authdns-check-active-passive',
+        true  => undef,
+    }
+
+    $keyspace = '/discovery'
+
+    confd::file { "/var/lib/gdnsd/discovery-${title}.state":
+        uid        => '0',
+        gid        => '0',
+        mode       => '0444',
+        content    => template("${module_name}/discovery-statefile.tpl.erb"),
+        watch_keys => "${keyspace}/${title}",
+        check      => $check,
+    }
+}
