39ae6dcd672d63bcfe712dce2ae46ae70e7fafea
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-01-11 17:43:31 +0200

    osm: Add a prometheus textfile exporter
    Adding a prometheus textfile export for the osm sync lag metric.
    
    Change-Id: Ib8ad17bdf535b2eade7b4f6057d75ef757363aea

---
+++ modules/osm/files/osm_sync_lag.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -u
+
+state_path=${1:-"/srv/osmosis/state.txt"}
+prometheus_path=${2:-"/var/lib/prometheus/node-exporter/osm_sync_lag.prom"}
+
+timestamp=$(awk -F= '/^timestamp=/ { print $2 }' state.txt | tr -d \\\\)
+lag=$(date "+%s" --date=${timestamp})
+echo "osm_sync_timestamp" $lag > $prometheus_path.$$
+mv $prometheus_path.$$ $prometheus_path
