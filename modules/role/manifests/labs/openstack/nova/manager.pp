b2c40542cf1dae60f68135f84fdcea59e5bef245
Alex Monk <krenair@gmail.com>
2017-01-11 17:25:35 +0000

    Use LE for wikitech
    Bug: T154913
    Change-Id: I325ab9c1f2a56e42f1460435958d27445f5a1a6b

--- modules/role/manifests/labs/openstack/nova/manager.pp
+++ modules/role/manifests/labs/openstack/nova/manager.pp
@@ -13,24 +13,19 @@ class role::labs::openstack::nova::manager {
     $novaconfig = $role::labs::openstack::nova::common::novaconfig
 
     $sitename = hiera('labs_osm_host')
-    if hiera('wikitech_use_letsencrypt', false) {
-        $sitename_split = split($sitename, '\.')
-        $certificate = $sitename_split[0]
-        letsencrypt::cert::integrated { $certificate:
-            subjects   => $sitename,
-            puppet_svc => 'apache2',
-            system_svc => 'apache2',
-        }
-        $cert_type = '_letsencrypt'
-    } else {
-        $certificate = $sitename
-        sslcert::certificate { $sitename: }
-        $cert_type = ''
+    $sitename_split = split($sitename, '\.')
+    $certificate = $sitename_split[0]
+    letsencrypt::cert::integrated { $certificate:
+        subjects   => $sitename,
+        puppet_svc => 'apache2',
+        system_svc => 'apache2',
     }
+
+    sslcert::certificate { $sitename: ensure => absent }
 
     monitoring::service { 'https':
         description   => 'HTTPS',
-        check_command => "check_ssl_http${cert_type}!${sitename}",
+        check_command => "check_ssl_http_letsencrypt!${sitename}",
     }
 
     $ssl_settings = ssl_ciphersuite('apache', 'compat', true)
