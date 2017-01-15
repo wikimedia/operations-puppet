febf259165c9e14e6f8a2a70375320071c382388
juniorsys <juniorsys_wikimedia@outlook.com>
2017-01-15 13:57:11 +1100

    postgresql module: Linting changes
    Use full names for class names, as relative names are not allowed in
    future Puppet versions
    
    Quote values in selectors
    
    Add missing trailing comma
    
    Bug: T93645
    
    Change-Id: If0b97e0ee2f597f479e48861c45d6cbf50100fd2

--- modules/postgresql/manifests/postgis.pp
+++ modules/postgresql/manifests/postgis.pp
@@ -15,15 +15,15 @@
 class postgresql::postgis(
     $ensure = 'present',
     $postgresql_postgis_package = $::lsbdistcodename ? {
-        jessie  => 'postgresql-9.4-postgis-2.3',
-        precise => 'postgresql-9.1-postgis',
-        trusty  => 'postgresql-9.3-postgis-2.1',
+        'jessie'  => 'postgresql-9.4-postgis-2.3',
+        'precise' => 'postgresql-9.1-postgis',
+        'trusty'  => 'postgresql-9.3-postgis-2.1',
     },
     $install_postgis_scripts = $::lsbdistcodename ? {
-        jessie  => true,
-        precise => false,
-        trusty  => true,
-        default => true,
+        'jessie'  => true,
+        'precise' => false,
+        'trusty'  => true,
+        default   => true,
     },
 ) {
     validate_bool($install_postgis_scripts)
