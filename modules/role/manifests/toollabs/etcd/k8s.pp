89b6ae41f1d3a7fd2893700b2b251228666c0f9c
juniorsys <juniorsys_wikimedia@outlook.com>
2017-01-15 14:35:08 +1100

    toollabs role modules: Linting changes
    Use full names for class names, as relative names are not allowed in
    future Puppet versions
    
    Add trailing commas to abide by the Coding Style guidelines
    
    Bug: T93645
    
    Change-Id: I63c46f70481387788cdba11de77d2473cdc8a767

--- modules/role/manifests/toollabs/etcd/k8s.pp
+++ modules/role/manifests/toollabs/etcd/k8s.pp
@@ -1,7 +1,7 @@
 # filtertags: labs-project-tools
 class role::toollabs::etcd::k8s {
     include ::etcd
-    include base::firewall
+    include ::base::firewall
 
     include ::role::toollabs::etcd::expose_metrics
 
@@ -12,12 +12,12 @@ class role::toollabs::etcd::k8s {
     ferm::service { 'etcd-clients':
         proto  => 'tcp',
         port   => '2379',
-        srange => "@resolve((${master_hosts} ${peer_nodes} ${checker_hosts}))"
+        srange => "@resolve((${master_hosts} ${peer_nodes} ${checker_hosts}))",
     }
 
     ferm::service { 'etcd-peers':
         proto  => 'tcp',
         port   => '2380',
-        srange => "@resolve((${peer_nodes}))"
+        srange => "@resolve((${peer_nodes}))",
     }
 }
