56481581bd3ab1f99acd7514abd78522aa73f900
Manuel Arostegui <marostegui@wikimedia.org>
2017-01-16 12:28:27 +0100

    mariadb: Split dbstore role classes
    * Split the dbstore class out from mariadb.pp into two differnt files
    
    * dbstore.pp will be used for the original dbstore hosts that still
    run TokuDB as an engine.
    
    * dbstore2.pp will be used for the new dbstore servers that no longer
    use TokuDB as an engine but use InnoDB.
    
    * dbstore2.my.cnf.erb: Enabled gtid_domain_id flag. Currently this only
    applies to dbstore2001 and dbstore2002 which already have it enabled
    manually
    
    Bug: T130128
    Bug: T150850
    Bug: T149418
    Change-Id: I2e87640257fc58ec16a02c825927129cfd4520eb

--- modules/role/manifests/mariadb.pp
+++ modules/role/manifests/mariadb.pp
@@ -479,55 +479,6 @@ class role::mariadb::tendril {
     }
 }
 
-# MariaDB 10 slaves replicating all shards
-class role::mariadb::dbstore(
-    $lag_warn = 300,
-    $lag_crit = 600,
-    $warn_stopped = true,
-    ) {
-
-    system::role { 'role::mariadb::dbstore':
-        description => 'Delayed Slave',
-    }
-
-    include mariadb::packages_wmf
-    include mariadb::service
-
-    include standard
-    include passwords::misc::scripts
-
-    class { 'role::mariadb::grants::production':
-        password => $passwords::misc::scripts::mysql_root_pass,
-        prompt   => 'DBSTORE',
-    }
-
-    include role::mariadb::monitor::dba
-    include passwords::misc::scripts
-    include role::mariadb::ferm
-
-    class {'role::mariadb::groups':
-        mysql_group => 'dbstore',
-        mysql_role  => 'slave',
-    }
-
-    class { 'mariadb::config':
-        config  => 'mariadb/dbstore.my.cnf.erb',
-        datadir => '/srv/sqldata',
-        tmpdir  => '/srv/tmp',
-        ssl     => 'puppet-cert',
-        p_s     => 'off',
-    }
-
-    mariadb::monitor_replication {
-        ['s1','s2','s3','s4','s5','s6','s7','m2','m3','x1']:
-        is_critical   => false,
-        contact_group => 'admins', # only show on nagios/irc
-        lag_warn      => $lag_warn,
-        lag_crit      => $lag_crit,
-        warn_stopped  => $warn_stopped,
-    }
-}
-
 # MariaDB 10 Analytics all-shards slave, with scratch space and TokuDB
 # analytics slaves are already either dbstores or eventlogging slaves
 # so they just need the extra core monitoring
