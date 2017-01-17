c2ceb7ef8a3725de8f769b48282c4eb166a83618
Daniel Zahn <dzahn@wikimedia.org>
2017-01-17 11:38:21 -0800

    dumps: switch to Letsencrypt for TLS cert
    puppet:
    - replace sslcert::certificate with letsencrypt::cert::integrated
    - add monitoring::service for Icinga cert check
    - add comment line to fix "class not documented on line 1"
    - also add "download.wikimedia.org" to cert subjects, which is
      an existing vhost but never had a valid cert
    
    nginx:
    - NOT YET, second PS: adjust path to cert and key in /etc/acme/ (LE)
    - include challenge-nginx.conf snippet which lets LE download
      challenge response file from .well-known/acme-challenge/
    - do _not_ redirect to https for acme-challenge URL
    
    Bug: T154940
    Change-Id: I9119f00f74eba4e0b4ef97e34db8ecdbf74b7650

--- modules/dumps/manifests/init.pp
+++ modules/dumps/manifests/init.pp
@@ -1,15 +1,28 @@
+# https://wikitech.wikimedia.org/wiki/Dumps
 class dumps {
-    sslcert::certificate { 'dumps.wikimedia.org': }
 
     class { '::nginx':
         variant => 'extras',
     }
+
     $ssl_settings = ssl_ciphersuite('nginx', 'mid', true)
+
+    letsencrypt::cert::integrated { 'dumps':
+        subjects   => 'dumps.wikimedia.org, download.wikimedia.org',
+        puppet_svc => 'nginx',
+        system_svc => 'nginx',
+    }
+
+    monitoring::service { 'https':
+        description   => 'HTTPS',
+        check_command => 'check_ssl_http_letsencrypt!dumps.wikimedia.org',
+    }
 
     nginx::site { 'dumps':
         content => template('dumps/nginx.dumps.conf.erb'),
         notify  => Service['nginx'],
     }
+
     nginx::site { 'download':
         source => 'puppet:///modules/dumps/nginx.download.conf',
         notify => Service['nginx'],
