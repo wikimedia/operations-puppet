874a4d152c560f133d04a8f7bd04b95929eb27c9
Eric Evans <eevans@wikimedia.org>
2017-01-17 10:18:07 -0800

    Enable Prometheus JMX exporter on Cassandra nodes
    Strawman proposal for selectively enabling the JMX exporter agent; I propose
    that once vetted for production use, we simply install/enable it everywhere.
    
    Bug: T155120
    Change-Id: I8b70ada1ede13338f36b90ce704e8bbdfb468889

--- modules/cassandra/manifests/init.pp
+++ modules/cassandra/manifests/init.pp
@@ -485,4 +485,9 @@ class cassandra(
         group  => 'cassandra',
         mode   => '0755',
     }
+
+    scap::target { 'prometheus/jmx_exporter':
+        deploy_user => 'deploy-service',
+        manage_user => true,
+    }
 }
