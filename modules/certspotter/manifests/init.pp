b2b749102a65f0a88d228a837fc57147148db5ca
Faidon Liambotis <faidon@wikimedia.org>
2017-01-20 12:53:41 +0200

    Setup & configure certspotter
    certspotter is a Certificate Transparency (CT) log monitor. It parses
    various different public logs (currently the ones trusted by
    Chrome/Chromium) and checks them for certificates issued for a
    predefined list of domains. CT currently includes all EV certificates,
    all certificates from a few CAs (Let's Encrypt, StartCom/WoSign,
    Symantec), DV certificates from GlobalSign, as well as randomly detected
    certificates by browsers, automated scans etc. It is expected that it
    will become mandatory for all CAs to report their new certificates by
    October 2017.
    
    This sets up certspotter to run once per hour on each of our two
    monitoring hosts. It only logs output when a certificate is found, at
    which point cron will send an email to $MAILTO, currently set to
    noc@wikimedia.org.
    
    Bug: T155807
    Change-Id: Iabdce7f541ec7211262215d26b61f6c53bf75153

---
+++ modules/certspotter/manifests/init.pp
@@ -0,0 +1,78 @@
+# == Class: certspotter
+#
+# Installs and sets up certspotter for the specified domains and runs it
+# periodically, sending notifications to a predefined address.
+#
+# === Parameters
+#
+# [*domains*]
+#   An array of domains to monitor certificates for.
+#
+# [*address*]
+#   An email address to send alerts to.
+#
+# === Examples
+#
+#  class { 'certspotter':
+#      domains => [ 'example.com', 'example.org' ],
+#      address => 'webmaster@example.org',
+#  }
+#
+
+class certspotter(
+  $domains,
+  $address,
+) {
+    package { 'certspotter':
+        ensure => present,
+    }
+
+    $homedir = '/var/lib/certspotter'
+    $statedir = "${homedir}/state"
+    $configdir = '/etc/certspotter'
+    $watchlist = "${configdir}/watchlist"
+
+    user { 'certspotter':
+        ensure     => present,
+        home       => $homedir,
+        shell      => '/bin/sh',
+        comment    => 'certspotter user',
+        gid        => 'certspotter',
+        managehome => true,
+        require    => Group['certspotter'],
+    }
+
+    group { 'certspotter':
+        ensure => present,
+    }
+
+    file { $configdir:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    file { $watchlist:
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => inline_template("<%= @domains.join(\"\n\") %>\n"),
+    }
+
+    $cmd = "/usr/bin/certspotter -watchlist ${watchlist} -state_dir ${statedir}"
+    cron { 'certspotter':
+        command     => $cmd,
+        environment => "MAILTO=${address}",
+        user        => 'certspotter',
+        minute      => fqdn_rand(30, 'certspotter'),
+        hour        => '*',
+        require     => [
+            User['certspotter'],
+            Package['certspotter'],
+            File[$watchlist],
+        ],
+    }
+
+}
