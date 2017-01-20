411c5275c0fdefc74ff50351b4e868cc0a987182
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-01-20 14:51:00 +0100

    tlsproxy: add nginx_bootstrap define
    The "another process is already running on port 80, nginx installation
    will fail" pattern is likely to be common (case in point, swift), thus
    move nginx "bootstrap" to a define for easy reuse.
    
    Change-Id: I3ce44be210609112d95055022f0cce667e4a4b98

--- modules/role/manifests/mediawiki/webserver.pp
+++ modules/role/manifests/mediawiki/webserver.pp
@@ -78,20 +78,7 @@ class role::mediawiki::webserver {
             $certs = [pick($pooldata['icinga']['sites'][$::site]['hostname'], $::fqdn)]
         }
 
-        # Given nginx will be installed on a system where apache is already
-        # running, the postinst script will fail to start it with the default
-        # configuration as port 80 is already in use. This is considered working
-        # as designed by Debian, see
-        #    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=754407
-        # However, we need the installation to complete correctly for puppet to
-        # work as expected, hence we pre-install a configuration that will make
-        # that possible. Note this file will be overwritten by puppet when
-        # the nginx configuration gets installed properly.
-        exec { 'Dummy nginx.conf for installation':
-            command => '/bin/mkdir -p /etc/nginx && /bin/echo -e "events { worker_connections 1; }\nhttp{ server{ listen 443; }}\n" > /etc/nginx/nginx.conf',
-            creates => '/etc/nginx/nginx.conf',
-            before  => Package['nginx-full'],
-        }
+        include ::tlsproxy::nginx_bootstrap
         tlsproxy::localssl { 'unified':
             server_name    => 'www.wikimedia.org',
             certs          => $certs,
