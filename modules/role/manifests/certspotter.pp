08c4c41a2586b947c2e1fa1b1d1fbcab67a61a24
Faidon Liambotis <faidon@wikimedia.org>
2017-01-20 12:53:41 +0200

    Setup & configure certspotter
    certspotter is a Certificate Transparency (CT) log monitor. It parses
    various different public logs (currently the ones trusted by
    Chrome/Chromium) and checks them for certificates issued for a
    predefined list of domains. CT currently includes all EV certificates,
    all certificates from a few CAs (Let's Encrypt, StartCom/WoSign,
    Symantec), DV certificates from GlobalSign, as well as randomly detected
    certificates by browsers, automated scans etc. It is expected that it
    will become mandatory for all CAs to report their new certificates by
    October 2017.
    
    This sets up certspotter to run once per hour on each of our two
    monitoring hosts. It only logs output when a certificate is found, at
    which point cron will send an email to $MAILTO, currently set to
    noc@wikimedia.org.
    
    Bug: T155807
    Change-Id: Iabdce7f541ec7211262215d26b61f6c53bf75153

---
+++ modules/role/manifests/certspotter.pp
@@ -0,0 +1,26 @@
+# = Class: role::certspotter
+#
+# Sets up certspotter for Wikimedia prod
+#
+class role::certspotter {
+    class { '::certspotter':
+        address => 'noc@wikimedia.org',
+        # cf. role::cache::ssl::unified
+        domains => [
+            'wikipedia.org',
+            'wikimedia.org',
+            'mediawiki.org',
+            'wikibooks.org',
+            'wikidata.org',
+            'wikinews.org',
+            'wikiquote.org',
+            'wikisource.org',
+            'wikiversity.org',
+            'wikivoyage.org',
+            'wiktionary.org',
+            'wikimediafoundation.org',
+            'wmfusercontent.org',
+            'w.wiki',
+        ],
+    }
+}
