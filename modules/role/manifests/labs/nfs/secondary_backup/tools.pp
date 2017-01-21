5f8d62e61d556421249ad8b5f63c76a62ee31093
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2017-01-20 22:34:56 -0800

    nfs: Make labstore1004 source for backups to secondary DC
    labstore1004 is the drbd secondary host right now, so we'll
    have the backups to codfw set up here
    
    Change-Id: Ic406babfe0c6a1ec83c7442efcc5787479867283

--- modules/role/manifests/labs/nfs/secondary_backup/tools.pp
+++ modules/role/manifests/labs/nfs/secondary_backup/tools.pp
@@ -8,12 +8,13 @@ class role::labs::nfs::secondary_backup::tools {
     }
 
     labstore::device_backup { 'secondary-tools':
-        remotehost      => 'labstore1005.eqiad.wmnet',
+        remotehost      => 'labstore1004.eqiad.wmnet',
         remote_vg       => 'tools',
         remote_lv       => 'tools-project',
         remote_snapshot => 'tools-snap',
         localdev        => '/dev/backup/tools-project',
         weekday         => 'monday',
+        hour            => 0,
     }
 
 }
