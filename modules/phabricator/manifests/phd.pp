a9885451377df3c25b75c7afcf4c36f9a4f5d7f6
Paladox <thomasmulhall410@yahoo.com>
2017-01-21 15:51:51 +0000

    Phabricator: Fix phd init script, also use systemd script if the os is cable of it
    This makes sure the init script wont be installed if you use the systemd
    one.
    
    This also fixes warnnings like
    
    insserv: warning: script 'phd' missing LSB tags and overrides
    
    Change-Id: I1fa2092e036ac2fca0b72a54d0cdff1f03c2cf09

--- modules/phabricator/manifests/phd.pp
+++ modules/phabricator/manifests/phd.pp
@@ -24,9 +24,32 @@ class phabricator::phd (
         recurse => true,
     }
 
-    file { '/etc/init.d/phd':
-        ensure => 'link',
-        target => "${basedir}/phabricator/bin/phd",
+    # phd service is only running on active server set in Hiera
+    # will be changed after cluster setup is finished
+    $phabricator_active_server = hiera('phabricator_active_server')
+    if $::hostname == $phabricator_active_server {
+        $phd_service_ensure = 'running'
+    } else {
+        $phd_service_ensure = 'stopped'
+    }
+
+    # This needs to become <s>Upstart</s> systemd managed
+    # https://secure.phabricator.com/book/phabricator/article/managing_daemons/
+    # Meanwhile upstream has a bug to make an LSB friendly wrapper
+    # https://secure.phabricator.com/T8129
+    # see examples of real-word unit files in comments of:
+    # https://secure.phabricator.com/T4181
+    base::service_unit { 'phd':
+        ensure         => 'present',
+        systemd        => true,
+        sysvinit       => true,
+        strict         => false,
+        service_params => {
+            ensure     => $phd_service_ensure,
+            hasrestart => true,
+            status     => '/usr/bin/pgrep -f phd-daemon',
+            start      => '/usr/sbin/service phd start --force',
+        },
     }
 
     file { '/var/run/phd':
