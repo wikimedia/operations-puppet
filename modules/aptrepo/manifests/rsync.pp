917b67c71dd5092e803e890ab62bc1f2cf3d7173
Daniel Zahn <dzahn@wikimedia.org>
2017-01-23 08:41:59 -0800

    aptrepo: setup rsync between 2 APT servers
    So far we just have a single APT repo, apt.wikimedia.org
    points to carbon.
    
    But we want to retire carbon and replace it fully with 2 servers,
    one per each DC, install1001 and install2001.
    
    As a first step one of them will be the "live" server
    that DNS points to and the other one will be warm stand-by.
    
    To ensure it is "warm" we setup rsyncd to keep /srv/wikimedia
    with the actual package data in sync between the 2 servers.
    
    For this we put rsyncd on the target, the one that is NOT live,
    and add a firewall hole to allow connections from the source,
    the live server, which pushes data to the failover server.
    
    Decide which server is which with a simple switch in Hiera.
    
    Bug: T84380
    Bug: T132757
    Change-Id: Idd9c51f93b8cb46a7f00fa44f9061ff5a8e133cb

---
+++ modules/aptrepo/manifests/rsync.pp
@@ -0,0 +1,23 @@
+# sets up rsync of APT repos between 2 servers
+# activates rsync for push from the primary to secondary
+class aptrepo::rsync {
+
+    $primary_server = hiera('aptrepo::primary_server', 'install1001.wikimedia.org')
+
+    if $::hostname != $primary_server {
+
+        ferm::service { 'aptrepo-rysnc':
+            proto  => 'tcp',
+            port   => '873',
+            srange => "@resolve(${primary_server})/32",
+        }
+
+        include rsync::server
+
+        rsync::server::module { 'aptrepo-basedir':
+            path        => $aptrepo::basedir,
+            read_only   => 'no',
+            hosts_allow => "@resolve(${primary_server})",
+        }
+    }
+}
