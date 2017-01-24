c3dce3f1842a87566a76a29eedd0392177d4617b
andrewbogott <abogott@wikimedia.org>
2017-01-24 15:20:20 -0600

    Horizon:  Forward some custom files from liberty
    Change-Id: I99b29bc0195f243c13dfd313c23bb0140c820604

---
+++ modules/openstack/files/mitaka/horizon/puppet_tab_enable.py
@@ -0,0 +1,7 @@
+PANEL = 'projectpuppetpanel'
+PANEL_GROUP = 'puppet'
+PANEL_DASHBOARD = 'project'
+ADD_PANEL = ('wikimediapuppettab.projectpanel.ProjectPuppetPanel')
+ADD_INSTALLED_APPS = ['wikimediapuppettab']
+AUTO_DISCOVER_STATIC_FILES = True
+ADD_SCSS_FILES = ['dashboard/puppet/puppet.scss']
