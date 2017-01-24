f78ea0f59bfac79e7e4b1f24d505b2eed13d1a46
elukey <ltoscano@wikimedia.org>
2017-01-24 11:44:29 +0100

    Refactor role memcached in multiple profiles
    Role memcached is responsible to configure memcached and redis
    instances for the object and session caches. While the role name
    is still confusing, breaking it down in multiple profiles should
    help improve readability and maintainability.
    The redis profile has two classes (and not only the multidc one
    used by role memcached) since it will be useful for the mc*
    migration to new hosts.
    
    Change-Id: Iacc881be358d8ef32ff893d6975a7e581ba239be

---
+++ modules/profile/manifests/redis/instance.pp
@@ -0,0 +1,44 @@
+# == Class: profile::redis::instance
+#
+# Installs and configures a Redis instance.
+#
+# === Parameters
+#
+# [*settings*]
+#   A hash containing configuration values for Redis. It will override
+#   the related parameters of the default base settings.
+#
+# [*port*]
+#   TCP port that Redis will listen on.
+#   Default: 6379
+#
+class profile::redis::instance (
+    $settings = {},
+    $port     = 6379,
+) {
+    include passwords::redis
+
+    $base_settings = {
+        bind                        => '0.0.0.0',
+        auto_aof_rewrite_min_size   => '512mb',
+        client_output_buffer_limit  => 'slave 512mb 200mb 60',
+        dir                         => '/srv/redis',
+        dbfilename                  => "${::hostname}-${port}.rdb",
+        masterauth                  => $passwords::redis::main_password,
+        maxmemory                   => '500Mb',
+        maxmemory_policy            => 'volatile-lru',
+        maxmemory_samples           => 5,
+        no_appendfsync_on_rewrite   => true,
+        requirepass                 => $passwords::redis::main_password,
+        save                        => '300 100',
+        slave_read_only             => false,
+        stop_writes_on_bgsave_error => false,
+    }
+
+    $redis_settings = merge($base_settings, $settings)
+
+    redis::instance{ $port:
+        ensure   => present,
+        settings => $redis_settings,
+    }
+}
