ece414f472f1e255f131179eb1e0ecffaa1df75c
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-01-24 11:47:30 -0800

    Update elasticsearch module for es5 compatability
    The end result of this patch is that elasticsearch 2 and 5 must have
    separate configuration and defaults files. This is needed due to 5.x
    renaming and removing some values in the config file.
    
    The defaults file for ES5 was sourced from upstream and modified to
    meet our previous configuration. The main elasticsearch.yml file is
    a copy of our elasticsearch 2 config updated for compatibility with 5.
    
    Additionally this patch configures relforge to use elasticsearch 5. That
    doesn't necessarily need to be in the final merged patch, but is done
    to allow puppet-compiler to report on how things change with the 2->5
    upgrade.
    
    * ES_HEAP_SIZE is no longer supported in 5.x. 2.x retains the setting,
      in 5.x this was moved to Setting -Xmx and -Xms in jvm.options
    * 5.x now uses log4j2.properties, instead of a custom logging.yml file.
      On updating to 5.x logging.yml will be removed and log4j2.properties
      will be created.
    * shipping logs to logstash is commented out in the log4j2 configuration as
      it requires further testing. And that testing requires a deployment of ES5.
    * ES 5.x no longer has a path.plugins config value. When running 5.x
      that will now be symlinked into /usr/share/elasticsearch/plugins
    * ES 5.x renamed threadpool to thread_pool
    * ES 5.x renamed mlockall to memory_lock
    * ES 5.x disallows setting thread_pool types. fixed was already the
      default, so simply removed the explicit setting.
    * Global index settings can no longer be set in elasticsearch.yml, they
      must be set per-index. They are now set by CirrusSearch at index
      creation time via settings in mediawiki-config repo.
    * disable_shutdown setting no longer exists. The _shutdown API has been
      removed without replacement.
    * cluster.routing.allocation.primary was deprecated in 1.3.8, and was
      removed in 2.x. This hasn't done anything for some time, but 5.x
      finally checks for unknown configuration so it needs to be removed.
    * multicast was removed from elasticsearch 5. Remove the relevant
      configuration from puppet as well. Default to off for elasticsearch 2
      (we wern't using it anymore).
    * indices.cache.filter.size renamed to indices.queries.cache.size in
      2.0, so this setting hasn't been doing anything for some time. Patch
      fixes the name, but perhaps we should remove instead?
    * removed indices.recovery.concurrent_streams which was deprecated in
      1.x, removed in 5.x.  superseded by
      cluster.routing.allocation.node_concurrent_recoveries which we already
      set.
    * removed profile setting, this was for the language detect plugin which
      we tested but decided against and no longer install.
    
    Bug: T155578
    Change-Id: I51fa32aecdbbaad3766522c8b3a1e3adafd7c7fa

--- modules/elasticsearch/spec/classes/elasticsearch_rspec.rb
+++ modules/elasticsearch/spec/classes/elasticsearch_rspec.rb
@@ -5,28 +5,6 @@ describe 'elasticsearch', :type => :class do
                   :lsbdistid      => 'trusty'
   } }
 
-  describe 'when multicast enabled' do
-    let(:params) { {
-        :cluster_name      => 'my_cluster_name',
-        :multicast_enabled => true,
-    } }
-    it { is_expected.to contain_file('/etc/elasticsearch/elasticsearch.yml')
-                            .with_content(/^#discovery\.zen\.ping\.multicast\.enabled: false$/)
-                            .without_content(/^discovery\.zen\.ping\.multicast\.enabled: false$/)
-    }
-  end
-
-  describe 'when multicast disabled' do
-    let(:params) { {
-        :cluster_name      => 'my_cluster_name',
-        :multicast_enabled => false,
-    } }
-    it { is_expected.to contain_file('/etc/elasticsearch/elasticsearch.yml')
-                            .with_content(/^discovery\.zen\.ping\.multicast\.enabled: false$/)
-                            .without_content(/^#discovery\.zen\.ping\.multicast\.enabled: false$/)
-    }
-  end
-
   describe 'when GC logging is enabled' do
     let(:params) { {
         :cluster_name => 'my_cluster_name',
