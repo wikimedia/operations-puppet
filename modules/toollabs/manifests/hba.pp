7202ff80c964adecf132c0a0ca63a3ca9922eba9
cpettet <rush@wikimedia.org>
2017-01-25 15:36:59 -0600

    tool: convert HBA source host mechanism to static
    The existing process relies on Puppet managing state files
    on NFS and then individual nodes building their own version of
    the required SSH and pam files from those state files via
    bash scripts that depend on lengthy 'test' evaulations to determine
    if changes have been made.  As an aside, we are still relying on
    NFS to do perform a similar function for known_hosts.
    
    This fell apart yesterday because of being predicated on the factor
    fact $ipaddress representing eth0.  We fixed that in the short term
    by qualifying with $ipaddress_eth0 but the whole thing is fragile.
    This all seems to be in service of not  managing two files explicitly and
    that I believe the cure here is worse than the disease.
    The last time these files would have legitamitely changed is 2016-03-30
    with the introduction of bastion-03.
    
    For now this seems like a reasonable price to avoid the calamity here.
    
    There are additionally issues with the process itself where
    the test logic does not account for locally modified state files
    (such as /etc/project) that are products of the 'resource collection' on
    NFS making this whole process not idempotent at all.
    
    Bugs: T156168
    
    Change-Id: Ia16cd1a487c0bb201c6556a0b763cbe76a062f56

--- modules/toollabs/manifests/hba.pp
+++ modules/toollabs/manifests/hba.pp
@@ -1,19 +1,25 @@
-# Establish the ability to do Host Based Auth from bastions to execs/webgrid
+# Establish the ability to do Host Based Auth from bastions/submit hosts to execs/webgrid
+#
+# Add hosts we want to be able to SSH to all hosts not protected by the
+# toollabs::infrastructure mechanism to both the shosts.equiv file
+# and the pam access control line manually.
 
 class toollabs::hba {
 
     file { '/usr/local/sbin/project-make-shosts':
-        ensure => file,
+        ensure => absent,
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
         source => 'puppet:///modules/toollabs/project-make-shosts',
     }
 
-    exec { 'make-shosts':
-        command => '/usr/local/sbin/project-make-shosts >/etc/ssh/shosts.equiv~',
-        onlyif  => "/usr/bin/test -n \"\$(/usr/bin/find /data/project/.system/store -maxdepth 1 \\( -type d -or -type f -name submithost-\\* \\) -newer /etc/ssh/shosts.equiv~)\" -o ! -s /etc/ssh/shosts.equiv~",
-        require => File['/usr/local/sbin/project-make-shosts'],
+    file { '/usr/local/sbin/project-make-access':
+        ensure => absent,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        source => 'puppet:///modules/toollabs/project-make-shosts',
     }
 
     file { '/etc/ssh/shosts.equiv':
@@ -21,26 +27,12 @@ class toollabs::hba {
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
-        source  => '/etc/ssh/shosts.equiv~',
-        require => Exec['make-shosts'],
+        source  => 'puppet:///modules/toollabs/ssh_shosts.equiv',
     }
 
-    file { '/usr/local/sbin/project-make-access':
-        ensure => file,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-        source => 'puppet:///modules/toollabs/project-make-access',
-    }
-
-    exec { 'make-access':
-        command => '/usr/local/sbin/project-make-access >/etc/project.access',
-        onlyif  => "/usr/bin/test -n \"\$(/usr/bin/find /data/project/.system/store -maxdepth 1 \\( -type d -or -type f -name submithost-\\* \\) -newer /etc/project.access)\" -o ! -s /etc/project.access",
-        require => File['/usr/local/sbin/project-make-access'],
-    }
-
+    $pam_access_d_for_hba = '+:ALL: 10.68.16.44 10.68.23.58 10.68.23.74 10.68.16.228 10.68.16.17 10.68.16.31'
     security::access::config { 'toollabs-hba':
-        source  => '/etc/project.access',
+        content => $pam_access_d_for_hba,
         require => Exec['make-access'],
     }
 }
