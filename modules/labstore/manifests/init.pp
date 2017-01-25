dcd77a3e212e531e66ff5e3544714721baff6a52
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2017-01-25 15:09:12 -0800

    labstore: Install package nethogs from jessie-backports
    Change-Id: I88318b3a399c15b0588052c00fefea22fec389f2

--- modules/labstore/manifests/init.pp
+++ modules/labstore/manifests/init.pp
@@ -11,6 +11,21 @@ class labstore {
     require_package('lvm2')
     require_package('nfsd-ldap')
 
+    # Nethogs is useful to monitor NFS client resource utilization
+    # The version in jessie has a bug that shows up in linux kernel 4.2+,
+    # so using newer version from backports.
+    if os_version('debian jessie') {
+        apt::pin {'nethogs':
+            pin      => 'release a=jessie-backports',
+            priority => '1001',
+            before   => Package['nethogs'],
+        }
+    }
+
+    package { 'nethogs':
+        ensure => present,
+    }
+
     $ldapincludes = ['openldap', 'nss', 'utils']
     class { '::ldap::role::client::labs': ldapincludes => $ldapincludes }
 
