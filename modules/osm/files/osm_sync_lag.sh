b78c92f36999d8ea9846b3ab9083b96a7c9b8b29
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-01-26 17:53:37 +0200

    osm prometheus: Fix bug with hardcoded state file
    Use the variable instead.
    
    Change-Id: Ib318d88049affd883cad8468346852f4466aaeca

--- modules/osm/files/osm_sync_lag.sh
+++ modules/osm/files/osm_sync_lag.sh
@@ -6,7 +6,7 @@ set -u
 state_path=${1:-"/srv/osmosis/state.txt"}
 prometheus_path=${2:-"/var/lib/prometheus/node-exporter/osm_sync_lag.prom"}
 
-timestamp=$(awk -F= '/^timestamp=/ { print $2 }' state.txt | tr -d \\\\)
+timestamp=$(awk -F= '/^timestamp=/ { print $2 }' $state_path | tr -d \\\\)
 lag=$(date "+%s" --date=${timestamp})
 echo "osm_sync_timestamp" $lag > $prometheus_path.$$
 mv $prometheus_path.$$ $prometheus_path
