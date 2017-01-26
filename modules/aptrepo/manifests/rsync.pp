dcd4490da2fd8e9b520d13255b4abe0e322824b7
Daniel Zahn <dzahn@wikimedia.org>
2017-01-25 20:42:10 -0800

    aptrepo: rsync cron (WIP)
    Change-Id: I0f3570890d5c949db98763b3f1ad5bcb0f35d23c

--- modules/aptrepo/manifests/rsync.pp
+++ modules/aptrepo/manifests/rsync.pp
@@ -3,27 +3,41 @@
 class aptrepo::rsync {
 
     $primary_server = hiera('install_server', 'install1001.wikimedia.org')
+    $secondary_server = hiera('install_server_failover', 'install2001.wikimedia.org')
 
     # only activate rsync/firewall hole on the server that is NOT active
     if $::fqdn != $primary_server {
 
-        $ensure = 'present'
+        $ensure_ferm = 'present'
+        $ensure_cron = 'absent'
+        $ensure_sync = 'present'
+
         include rsync::server
 
         rsync::server::module { 'aptrepo':
-            ensure      => $aptrepo::rsync::ensure,
+            ensure      => $ensure_sync,
             path        => $aptrepo::basedir,
             read_only   => 'no',
             hosts_allow => $primary_server,
         }
     } else {
-        $ensure = 'absent'
+        $ensure_ferm = 'absent'
+        $ensure_cron = 'present'
+        $ensure_sync = 'absent'
     }
 
     ferm::service { 'aptrepo-rsync':
-        ensure => $aptrepo::rsync::ensure,
+        ensure => $ensure_ferm,
         proto  => 'tcp',
         port   => '873',
         srange => "@resolve(${primary_server})",
+    }
+
+    cron { 'rsync-aptrepo':
+        ensure  => $ensure_cron,
+        user    => 'root',
+        command => "rsync -avp ${aptrepo::basedir} rsync://${secondary_server}/aptrepo",
+        hour    => '*/6',
+        minute  => '42',
     }
 }
