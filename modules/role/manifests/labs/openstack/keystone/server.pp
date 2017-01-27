53389f4794eb5999d39ab78151e0a9b33531e751
Andrew Bogott <abogott@wikimedia.org>
2017-01-27 14:55:33 +0000

    Revert "labs: remove keystone alerts"
    This reverts commit 3dfa92b5838693ebab92cff383d30bc1f61218bd.
    
    Change-Id: I98cecd9d65b5452add633109a72a22e16c0bf0a2

--- modules/role/manifests/labs/openstack/keystone/server.pp
+++ modules/role/manifests/labs/openstack/keystone/server.pp
@@ -11,4 +11,23 @@ class role::labs::openstack::keystone::server {
 
     # Monitor project membership
     include ::openstack::clientlib
+
+    # Make sure novaobserver is in every project and only has observer rights
+    monitoring::service { 'novaobserver project roles':
+        description   => 'novaobserver has only observer role',
+        check_command => 'check_keystone_roles!novaobserver!observer',
+    }
+
+    # Make sure novaadmin is in every project with 'user' and 'projectadmin'
+    monitoring::service { 'novaadmin project roles':
+        description   => 'novaadmin has roles in every project',
+        check_command => 'check_keystone_roles!novaadmin!user!projectadmin',
+    }
+
+    # Make sure keystone admin and observer projects exist, and that
+    #  keystone project ids == keystone project names
+    monitoring::service { 'keystone projects exist':
+        description   => 'Keystone admin and observer projects exist',
+        check_command => 'check_keystone_projects',
+    }
 }
