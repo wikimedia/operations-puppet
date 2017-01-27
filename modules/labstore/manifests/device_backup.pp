42b9b9d025523fc69e3197bae5b7c99d80047f44
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2017-01-27 10:37:04 -0800

    nfs: Snapshot backup device on secondary DC before replicating latest from remote
    Bug: T149870
    Change-Id: Ic485bf5bce49581a71457f9dcf9802367e3c1c6e

--- modules/labstore/manifests/device_backup.pp
+++ modules/labstore/manifests/device_backup.pp
@@ -3,7 +3,10 @@ define labstore::device_backup (
     $remote_vg,
     $remote_lv,
     $remote_snapshot,
-    $localdev,
+    $local_vg,
+    $local_lv,
+    $local_snapshot,
+    $local_snapshot_size,
     $weekday,
     $hour=0,
     $minute=0,
@@ -23,13 +26,22 @@ define labstore::device_backup (
     }
 
     $block_sync='/usr/local/sbin/block_sync'
-    cron { "block_sync-${remote_vg}/${remote_lv}=>${localdev}":
+    cron { "block_sync-${remote_vg}/${remote_lv}=>${local_vg}/${local_lv}":
         ensure      => 'present',
         user        => 'root',
-        command     => "${block_sync} ${remote_ip} ${remote_vg} ${remote_lv} ${remote_snapshot} ${localdev}",
+        command     => "${block_sync} ${remote_ip} ${remote_vg} ${remote_lv} ${remote_snapshot} ${local_vg} ${local_lv} ${local_snapshot} ${local_snapshot_size}",
         weekday     => $day[$weekday],
         hour        => $hour,
         minute      => $minute,
         environment => 'MAILTO=labs-admin@lists.wikimedia.org',
+        require     => File['/usr/local/sbin/snapshot-manager'],
+    }
+
+    file { '/usr/local/sbin/snapshot-manager':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        source => 'puppet:///modules/labstore/snapshot-manager.py',
     }
 }
