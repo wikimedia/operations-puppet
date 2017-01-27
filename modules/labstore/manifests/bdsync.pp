9d8f6125445852e46a33c3f0084264a3e51b89a5
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2017-01-27 10:37:04 -0800

    nfs: Snapshot backup device on secondary DC before replicating latest from remote
    Bug: T149870
    Change-Id: Ic485bf5bce49581a71457f9dcf9802367e3c1c6e

--- modules/labstore/manifests/bdsync.pp
+++ modules/labstore/manifests/bdsync.pp
@@ -1,12 +1,12 @@
 class labstore::bdsync {
 
-    package { 'bdsync':
-        ensure => present,
+    package { ['python3', 'python3-dateutil', 'bdsync']:
+        ensure  => present,
         before => File['/usr/local/sbin/block_sync'],
     }
 
     file { '/usr/local/sbin/block_sync':
-        source => 'puppet:///modules/labstore/block_sync.sh',
+        source => 'puppet:///modules/labstore/block_sync.py',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
