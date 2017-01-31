2f4ce472360b46c272bdf579e6e06e85baa45fb1
Guillaume Lederrey <glederrey@wikimedia.org>
2017-01-31 14:25:13 +0100

    elasticsearch - ensure data directory with puppet
    Default data directory is created by elasticsearch .deb package. If we move
    data to /srv, the data directory needs to be created.
    
    Bug: T151328
    Change-Id: Ie11729b6af6215106c4db202267efb7ac16fb6c7

--- modules/elasticsearch/manifests/init.pp
+++ modules/elasticsearch/manifests/init.pp
@@ -200,6 +200,12 @@ class elasticsearch(
         mode   => '0444',
         source => 'puppet:///modules/elasticsearch/logrotate',
     }
+    file { $data_dir:
+      ensure => directory,
+      owner  => 'elasticsearch',
+      group  => 'elasticsearch',
+      mode   => '0755',
+    }
     # GC logs rotation is done by the JVM, but on JVM restart, the logs left by
     # the previous instance are left alone. This cron takes care of cleaning up
     # GC logs older than 30 days.
