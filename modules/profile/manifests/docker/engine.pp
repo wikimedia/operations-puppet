81dac0df1a54aa4355a9a6af9cce11314a661b57
YuviPanda <yuvipanda@gmail.com>
2017-01-31 13:31:43 -0800

    tools: Use docker engine profile in tools builder
    - Remove some choices that we aren't making, thus leading to dead
      code being removed (can no longer use debian docker image)
    - Use lvm module to setup devicemapper docker storage backend,
      not bash script
    - Upgrade version of docker we use
    
    Change-Id: I6b5bcd6837509e21d1e1e65efa29cd3785e70a6f

--- modules/profile/manifests/docker/engine.pp
+++ modules/profile/manifests/docker/engine.pp
@@ -3,19 +3,17 @@
 # Installs docker, along with setting up the volume group needed for the
 # devicemapper storage driver to work.
 # to work
-class profile::docker::engine {
-
-    # Optional parameters
+class profile::docker::engine(
     # We want to get settings across the hierarchy, some per host, some fleet
     # wide. So use hiera_hash to merge keys across the hierarchy
-    $docker_settings = hiera_hash('profile::docker::engine::settings', {})
+    $settings = hiera_hash('profile::docker::engine::settings'),
     # Version to install; the default is not to pick one.
-    $docker_version = hiera('profile::docker::engine::version', 'present')
-    $service_ensure = hiera('profile::docker::engine::service', 'running')
+    $version = hiera('profile::docker::engine::version'),
+) {
 
     # Install docker
     class { 'docker':
-        version => $docker_version,
+        version => $version,
     }
 
     # Docker config
@@ -32,7 +30,7 @@ class profile::docker::engine {
 
     # We need to import one storage config
     class { 'docker::configuration':
-        settings => merge($docker_settings, $docker_storage_options),
+        settings => merge($settings, $docker_storage_options),
     }
 
     # Enable memory cgroup
@@ -46,6 +44,6 @@ class profile::docker::engine {
 
     # Service declaration
     service { 'docker':
-        ensure => $service_ensure,
+        ensure => 'running',
     }
 }
