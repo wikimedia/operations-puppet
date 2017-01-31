a16f23ccdee6e288bea27749dfe0632310eb8710
YuviPanda <yuvipanda@gmail.com>
2017-01-31 13:31:43 -0800

    docker: Allow using docker.io's repo directly for debs
    We'll use this in tools for now. I can work towards getting rid
    of it once rest of puppet code for k8s between tools and prod is
    unified
    
    Change-Id: I6b5bcd6837509e21d1e1e65efa29cd3785e70a6f

--- modules/profile/manifests/docker/engine.pp
+++ modules/profile/manifests/docker/engine.pp
@@ -3,19 +3,25 @@
 # Installs docker, along with setting up the volume group needed for the
 # devicemapper storage driver to work.
 # to work
-class profile::docker::engine {
-
+class profile::docker::engine(
     # Optional parameters
     # We want to get settings across the hierarchy, some per host, some fleet
     # wide. So use hiera_hash to merge keys across the hierarchy
-    $docker_settings = hiera_hash('profile::docker::engine::settings', {})
+    $docker_settings = hiera_hash('profile::docker::engine::settings', {}),
     # Version to install; the default is not to pick one.
-    $docker_version = hiera('profile::docker::engine::version', 'present')
-    $service_ensure = hiera('profile::docker::engine::service', 'running')
+    $docker_version = hiera('profile::docker::engine::version', 'present'),
+    $service_ensure = hiera('profile::docker::engine::service', 'running'),
+    # Wether to use the debs from upstream dockerproject
+    $use_dockerproject = hiera('profile::docker::engine::use_dockerproject', true),
+    # Set to true to use upstream's deb repo directly
+    $use_docker_io_repo = hiera('profile::docker::engine::use_docker_io_repo', false)
+) {
 
     # Install docker
     class { 'docker':
-        version => $docker_version,
+        version            => $docker_version,
+        use_dockerproject  => $use_dockerproject,
+        use_docker_io_repo => $use_docker_io_repo,
     }
 
     # Docker config
