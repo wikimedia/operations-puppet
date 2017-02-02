f389a3242f3f396e157dde47d07382bde577ba8e
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2017-02-02 15:02:41 +0100

    WDQS - move metric collection to diamond
    Lag and number of tuples are at the moment published via a custom PHP script
    and not via diamond. Moving everything to diamond ensures coherence and
    automatic configuration of new nodes.
    
    This is a direct port of the PHP implementation available at
    https://phabricator.wikimedia.org/diffusion/ADES/browse/production/src/wikidata/sparql/minutely.php
    
    Bug: T146468
    Change-Id: I802bc5bc5324c052137f99eb8fdd1fee1b57e3b2

--- modules/wdqs/files/monitor/blazegraph.py
+++ modules/wdqs/files/monitor/blazegraph.py
@@ -1,9 +1,30 @@
 # coding=utf-8
 
-import diamond.collector
+import datetime
+import json
 import urllib2
+import urllib
+
+from datetime import timedelta, tzinfo
+from dateutil.parser import parse
 from xml.etree import ElementTree
-from urllib import urlencode
+
+import diamond.collector
+
+ZERO = timedelta(0)
+
+
+class UTC(tzinfo):
+    def utcoffset(self, dt):
+        return ZERO
+
+    def tzname(self, dt):
+        return "UTC"
+
+    def dst(self, dt):
+        return ZERO
+
+utc = UTC()
 
 
 class BlazegraphCollector(diamond.collector.Collector):
@@ -13,6 +34,7 @@ class BlazegraphCollector(diamond.collector.Collector):
         chelp.update({
             'url': 'URL of the blazegraph instance',
             'counters': 'List of counters to report',
+            'sparql_endpoint': 'HTTP endpoint which can be used to query blazegraph',
         })
         return chelp
 
@@ -24,6 +46,7 @@ class BlazegraphCollector(diamond.collector.Collector):
         config.update({
             'url': 'http://localhost:9999/bigdata/',
             'counters': ["/Query Engine/queryDoneCount"],
+            'sparql_endpoint': 'http://localhost:9999/bigdata/namespace/wdq/sparql',
         })
         return config
 
@@ -38,7 +61,7 @@ class BlazegraphCollector(diamond.collector.Collector):
     def get_counter(self, cnt_name):
         # Not sure why we need depth but some counters don't work without it
         url = self.config['url'] + "counters?depth=10&" + \
-            urlencode({'path': cnt_name})
+            urllib.urlencode({'path': cnt_name})
 
         req = urllib2.Request(url)
         req.add_header('Accept', 'application/xml')
@@ -52,9 +75,38 @@ class BlazegraphCollector(diamond.collector.Collector):
                 return cnt.attrib['value']
         return None
 
-    def collect(self):
+    def collect_jolokia(self):
         for counter in self.config['counters']:
             metric_name = self.query_to_metric(counter)
             metric_value = self.get_counter(counter)
             if metric_value is not None:
                 self.publish(metric_name, metric_value)
+
+    def execute_sparql(self, query):
+        params = urllib.urlencode({'format': 'json', 'query': query})
+        request = urllib2.Request(self.config['sparql_endpoint'] + "?" + params)
+        response = urllib2.urlopen(request)
+        return json.loads(response.read())
+
+    def collect_sparql(self):
+        query = """ prefix schema: <http://schema.org/>
+                    SELECT * WHERE { {
+                      SELECT ( COUNT( * ) AS ?count ) { ?s ?p ?o }
+                    } UNION {
+                      SELECT * WHERE { <http://www.wikidata.org> schema:dateModified ?y }
+                    } }"""
+        data = self.execute_sparql(query)
+        for binding in data['results']['bindings']:
+            if 'count' in binding:
+                triple_count = binding['count']['value']
+                self.publish('triples', triple_count)
+            elif 'y' in binding:
+                lastUpdated = parse(binding['y']['value'])
+                lag = datetime.datetime.now(utc) - lastUpdated
+                self.publish('lag', lag.total_seconds())
+            else:
+                raise ValueError('SPARQL binding returned with unexpected key')
+
+    def collect(self):
+        self.collect_jolokia()
+        self.collect_sparql()
