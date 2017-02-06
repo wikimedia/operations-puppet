3359f72dbfb754ff27f694a95f815ac2cdddde92
Paladox <thomasmulhall410@yahoo.com>
2017-02-06 21:38:11 +0000

    Testing: do not merge
    Im cherry picking this onto phabricator puppet master labs
    
    Change-Id: I3620df9c1bd15de99ebf8af1a784b8f6e44ac905

--- modules/role/manifests/phabricator/main.pp
+++ modules/role/manifests/phabricator/main.pp
@@ -147,7 +147,7 @@ class role::phabricator::main {
                               "${phab_root_dir}/libext/misc/" ],
         settings         => {
             'search.elastic.host'                    => $elasticsearch_host,
-            'search.elastic.version'                 => $elasticsearch_version,
+            'search.elasticsearch.version'           => $elasticsearch_version,
             'search.elastic.enabled'                 => $elasticsearch_enabled,
             'darkconsole.enabled'                    => false,
             'differential.allow-self-accept'         => true,
