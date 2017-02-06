022cefe3344325889544f0f3db869319422635cd
elukey <ltoscano@wikimedia.org>
2017-02-06 12:57:57 +0100

    Raise the retry interval of the Apache/HHVM leak monitor
    The first attempt was https://gerrit.wikimedia.org/r/#/c/334505,
    in which the check_leaked_hhvm_threads.py script was modified
    to avoid any check if the httpd's uptime was less than two hours.
    The solution turned out to be a failure since httpd does not reset
    its uptime counter when a graceful reload happens (only after
    a restart). The alternative solution is to transfer the delay
    to the monitor, allowing more time after the first failure
    detection to avoid false positives.
    
    Change-Id: I76e9ef08c440378a13da96f54a22b8e00378161f

--- modules/role/manifests/mediawiki/scaler.pp
+++ modules/role/manifests/mediawiki/scaler.pp
@@ -19,9 +19,18 @@ class role::mediawiki::scaler {
         group  => 'root',
     }
 
+    # For some versions of httpd (like 2.4.7), BusyWorkers are set to zero when
+    # a graceful restart happens, even if outstanding requests are not dropped
+    # or marked as Graceful closing.
+    # This means that daily tasks like logrotate cause false positives.
+    # A quick workaround is to use a high enough retry interval monitoring check,
+    # to give httpd time to restore its busy workers.
+    # This is not an ideal solution but a constant rate of false positives
+    # decreases the perceived importance of the alarm over time.
     nrpe::monitor_service { 'check_leaked_hhvm_threads':
-        description  => 'Check HHVM threads for leakage',
-        nrpe_command => '/usr/local/lib/nagios/plugins/check_leaked_hhvm_threads',
-        require      => File['/usr/local/lib/nagios/plugins/check_leaked_hhvm_threads'],
+        description    => 'Check HHVM threads for leakage',
+        nrpe_command   => '/usr/local/lib/nagios/plugins/check_leaked_hhvm_threads',
+        retry_interval => 30,
+        require        => File['/usr/local/lib/nagios/plugins/check_leaked_hhvm_threads'],
     }
 }
