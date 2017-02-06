4e739006240fc004ba60e02cbfdf40ed1284ba32
Ariel T. Glenn <ariel@wikimedia.org>
2017-02-06 13:44:39 +0200

    remove python script for cron rsync of dumps from dataset1001 to labstore
    No longer needed once Id23a1952f3664941877ee9242bc3d11743fff964
    is merged.
    
    Change-Id: I869c5acbebf906223e9f4b8b47abec9dbcfc17ad

--- modules/dataset/manifests/cron/rsync/labs.pp
+++ modules/dataset/manifests/cron/rsync/labs.pp
@@ -8,12 +8,6 @@ class dataset::cron::rsync::labs($enable=true) {
         $ensure = 'absent'
     }
 
-    file { '/usr/local/bin/wmfdumpsmirror.py':
-        ensure => 'present',
-        mode   => '0755',
-        source => 'puppet:///modules/dataset/labs/wmfdumpsmirror.py',
-    }
-
     file{ '/usr/local/sbin/labs-rsync-cron.sh':
         ensure => 'present',
         mode   => '0755',
@@ -28,8 +22,7 @@ class dataset::cron::rsync::labs($enable=true) {
             hour        => '3',
             command     => '/usr/local/sbin/labs-rsync-cron.sh',
             environment => 'MAILTO=ops-dumps@wikimedia.org',
-            require     => File['/usr/local/bin/wmfdumpsmirror.py',
-                                '/usr/local/sbin/labs-rsync-cron.sh'],
+            require     => File['/usr/local/sbin/labs-rsync-cron.sh'],
         }
     }
     else {
