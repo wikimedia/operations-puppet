74c1b266b766cac40d4b30a81deee9ed8d04736a
Ariel T. Glenn <ariel@wikimedia.org>
2017-02-06 13:41:11 +0200

    dataset1001 rsync to labs of dumps can now use explicit inclusion list
    We can invoke rsync directly with the right include/exclude args.
    Remove the complicated python script for copying, no longer needed.
    
    We must clean up old dumps manually now, because we can't rsync --delete,
    so include manifests for that.
    
    The copy to labstore1003, including the hiera setting for this config,
    "labscopy", will go away once labstore1006,7 are in full production.
    
    While we're here, clean up var names etc in the rsync script. Yeah it's
    a one-off with an expiration date but still.
    
    Bug: T154798
    Change-Id: Id23a1952f3664941877ee9242bc3d11743fff964

--- modules/profile/manifests/dumps/web/cleanup.pp
+++ modules/profile/manifests/dumps/web/cleanup.pp
@@ -1,11 +1,13 @@
 class profile::dumps::web::cleanup(
     $isreplica = hiera('profile::dumps::cleanup::isreplica'),
+    $labscopy = hiera('profile::dumps::cleanup::labscopy'),
     $miscdumpsdir = hiera('profile::dumps::miscdumpsdir'),
     $xmldumpsdir = hiera('profile::dumps::xmldumpspublicdir'),
     $dumpstempdir = hiera('profile::dumps::dumpstempdir'),
 ) {
     class {'::dumps::web::cleanup':
         isreplica    => $isreplica,
+        labscopy     => $labscopy,
         miscdumpsdir => $miscdumpsdir,
         xmldumpsdir  => $xmldumpsdir,
         dumpstempdir => $dumpstempdir,
