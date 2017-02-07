29d2cac1d4ea23be02b5c8d3ded305b007692933
Daniel Zahn <dzahn@wikimedia.org>
2017-02-06 17:49:07 -0800

    add prometheus1003/1004 to site.pp
    Bug: T152504
    Change-Id: Iab83f351fbc90bca2904cfe8f66ac36cdad58c92

--- manifests/site.pp
+++ manifests/site.pp
@@ -2403,7 +2403,7 @@ node /^prometheus200[12]\.codfw\.wmnet$/ {
     }
 }
 
-node /^prometheus100[12]\.eqiad\.wmnet$/ {
+node /^prometheus100[1234]\.eqiad\.wmnet$/ {
     role(prometheus::ops)
 
     include ::base::firewall
