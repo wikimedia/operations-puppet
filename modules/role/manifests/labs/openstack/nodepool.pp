6b1bfcd6fa99b01806de58ed134f2d4622ad7718
cpettet <rush@wikimedia.org>
2017-02-07 07:19:36 -0600

    nodepool: active check for node pool instance states
    Check defined in changeset 335373
    
    Change-Id: I072de34fd57dc81b572baf7437c1dda7f566363e
    Bugs: T156636

--- modules/role/manifests/labs/openstack/nodepool.pp
+++ modules/role/manifests/labs/openstack/nodepool.pp
@@ -43,4 +43,10 @@ class role::labs::openstack::nodepool {
         contact_group => 'contint',
         nrpe_command  => '/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:1 -u nodepool --ereg-argument-array="^/usr/bin/python /usr/bin/nodepoold -d"',
     }
+
+    nrpe::monitor_service { 'nodepoold_instance_state':
+        description   => 'Check for valid instance states',
+        contact_group => 'contint',
+        nrpe_command  => '/usr/local/bin/check_nodepool_states',
+    }
 }
