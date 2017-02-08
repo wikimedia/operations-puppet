93eb3828a86d644a195bdc9d83f4fee2ab4243ef
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-02-08 10:51:54 +0100

    role::configcluster: enable replication from eqiad to codfw
    Also introduce the etcdmirror::instance define
    
    Bug: T156009
    Change-Id: Ibbd209c58d38dcb2f4ee279e9a6991da5cf7099a

---
+++ modules/etcdmirror/manifests/instance.pp
@@ -0,0 +1,25 @@
+define etcdmirror::instance($sources, $dst) {
+    require_package('etcd-mirror')
+
+    # Source host
+    $src = $sources[$title]
+
+    $data = split($title, '@')
+    $src_path = $data[0]
+    $dst_prefix = $data[1]
+    $dst_path = "/${dst_prefix}${src_path}"
+    # safe version of the title
+    $prefix = regsubst("etcdmirror${title}", '\W', '-', 'G')
+
+    base::service_unit { $prefix:
+        ensure        => present,
+        systemd       => true,
+        template_name => 'etcdmirror',
+    }
+
+    systemd::syslog { $prefix:
+        owner        => 'root',
+        group        => 'root',
+        log_filename => 'syslog.log',
+    }
+}
