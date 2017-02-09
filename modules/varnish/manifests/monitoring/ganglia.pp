abb53c35b3710571348f76a487357bc4a72fca74
Emanuele Rocca <ema@wikimedia.org>
2017-02-09 11:13:38 +0100

    varnish: remove ganglia python module
    We are now using prometheus+graphana to plot graphs based on
    varnishstat: https://grafana.wikimedia.org/dashboard/db/varnish-machine-stats
    
    Remove the ganglia python module.
    
    Change-Id: I6b48216745a7c1a6e478e562ab1230072cf210fb

--- modules/varnish/manifests/monitoring/ganglia.pp
+++ modules/varnish/manifests/monitoring/ganglia.pp
@@ -1,46 +1,18 @@
 class varnish::monitoring::ganglia($varnish_instances=['']) {
-    $instances = join($varnish_instances, ',')
-
     file { '/usr/lib/ganglia/python_modules/varnish.py':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        source  => "puppet:///modules/${module_name}/ganglia/ganglia-varnish.py",
-        require => File['/usr/lib/ganglia/python_modules'],
+        ensure => absent,
     }
 
-    exec { 'generate varnish.pyconf':
-        require => [
-            File['/usr/lib/ganglia/python_modules/varnish.py'],
-            File['/etc/ganglia/conf.d'],
-        ],
-        command => "/usr/bin/python /usr/lib/ganglia/python_modules/varnish.py \"${instances}\" > /etc/ganglia/conf.d/varnish.pyconf.new",
-    }
-
-    exec { 'replace varnish.pyconf':
-        cwd     => '/etc/ganglia/conf.d',
-        path    => '/bin:/usr/bin',
-        unless  => 'diff -q varnish.pyconf.new varnish.pyconf && rm varnish.pyconf.new',
-        command => 'mv varnish.pyconf.new varnish.pyconf',
-        notify  => Service['ganglia-monitor'],
+    file { '/etc/ganglia/conf.d/varnish.pyconf':
+        ensure => absent,
+        notify => Service['ganglia-monitor'],
     }
 
     file { '/usr/local/sbin/check-gmond-restart':
-        ensure => present,
-        source => 'puppet:///modules/varnish/ganglia/check-gmond-restart',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0544',
+        ensure => absent,
     }
 
     file { '/etc/cron.d/check-gmond-restart':
-        ensure  => present,
-        content => "*/5 * * * * root /usr/local/sbin/check-gmond-restart > /dev/null 2>&1\n",
-        require => File['/usr/local/sbin/check-gmond-restart'],
+        ensure => absent,
     }
-
-    # Dependencies
-    # Exec the config generation AFTER all varnish instances have started
-    Service <| tag == 'varnish_instance' |> -> Exec['generate varnish.pyconf']
-    Exec['generate varnish.pyconf'] -> Exec['replace varnish.pyconf']
 }
