ec0cf6aa1752fb0cb6549874ff8c77d650e1ee67
Antoine Musso <hashar@free.fr>
2017-02-12 22:05:50 +0100

    jenkins: support variable prefix setting
    The Jenkins master is assumed to serve requests under /ci. To allow
    multiple Jenkins to work in parallel, that needs to be adjustable. This
    is done in two places:
    
    * the default file for the jenkins service (jenkins puppet module)
    * the Apache proxy configuration (contint puppet module)
    
    == jenkins module ==
    
    Make the Jenkins default file an erb template.  Make the web prefix a
    parameter to the template.  $prefix is required for the class.
    
    Update the only call of class to pass a prefix of '/ci'.
    Change the JENKINS_ARGS to use the PREFIX variable for --prefix.
    Must be a noop for Wikimedia production.
    
    Remove modules/jenkins/README.md prone to be out of date, the class
    inline documentation should be the reference and is online at:
    https://doc.wikimedia.org/puppet/puppet_classes/jenkins.html
    
    Drop some extra lines in rdoc documentation for nicer online rendering
    of the class documentation.
    
    == contint module ==
    
    Make the Apache configuration snippet for Jenkins an erb template. Pass
    the prefix as well.
    
    Finally, pass /ci to the role::ci::master class.
    
    Change-Id: I65a3535777e7593a9eb9a809b29a2da984863e0d

--- modules/contint/manifests/proxy_jenkins.pp
+++ modules/contint/manifests/proxy_jenkins.pp
@@ -1,5 +1,13 @@
+# == Class contint::proxy_jenkins
+#
 # A http proxy in front of Jenkins
-class contint::proxy_jenkins {
+#
+# [*prefix*]
+# The HTTP path used to reach the Jenkins instance. Must have a leading slash.
+# Example: /ci
+class contint::proxy_jenkins (
+    $prefix,
+) {
 
   include ::contint::proxy_common
 
@@ -16,7 +24,7 @@ class contint::proxy_jenkins {
       owner  => 'root',
       group  => 'root',
       mode   => '0444',
-      source => 'puppet:///modules/contint/apache/proxy_jenkins',
+      conent => template('contint/apache/proxy_jenkins.erb'),
   }
 
 }
