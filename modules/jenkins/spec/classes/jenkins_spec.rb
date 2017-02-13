1d9bd24d818197257e97c72afd826823ff9091e4
Antoine Musso <hashar@free.fr>
2017-02-13 16:36:48 +0100

    jenkins: migrate to systemd
    Migrate bits from upstream sysvinit script to systemd. The
    /etc/default/jenkins is still used for now.
    
    Port support to have the service unmanaged passing to service ensure =>
    undef.
    
    The maximum number of file descriptor allowed is defined in the default
    file, it is loaded via EnvironmentFile but I dont think the variable
    will be available to Service.LimitNOFILE. Hence:
    
    Add a $max_open_files parameter to Jenkins class
    Use it in the default template and in systemd for LimitNOFILE.
    
    Jenkins spurts out to standard output which systemd collects to syslog.
    Use systemd::syslog() with programname_compare to ensure we only capture
    logs for a program named exactly 'jenkins'.  Introduced by
    I4e46cf4c20d98e8947e16c174cacea0f173b603d .
    
    Update JENKINS_LOG in default file to point to daemon.info for
    consistency.
    
    Remove our custom logrotate, rely on the one from systemd::syslog which
    rotates all log files and has almost the same settings.
    
    Depends-On: I4e46cf4c20d98e8947e16c174cacea0f173b603d
    Change-Id: I82ec3fa8ab209c449678e663822d9ac507cbb208

--- modules/jenkins/spec/classes/jenkins_spec.rb
+++ modules/jenkins/spec/classes/jenkins_spec.rb
@@ -6,7 +6,30 @@ describe 'jenkins' do
     } }
     let(:params) { {
         :prefix => '/ci',
-
     } }
     it { should compile }
+
+    describe 'when service_ensure' do
+        context "is 'unmanaged'" do
+            let(:params) { {
+                :prefix => '/ci',
+                :service_ensure => 'unmanaged',
+            } }
+            it { should contain_service('jenkins').without_ensure }
+        end
+        context "is 'running'" do
+            let(:params) { {
+                :prefix => '/ci',
+                :service_ensure => 'running',
+            } }
+            it { should contain_service('jenkins') .with_ensure('running') }
+        end
+        context "is 'stopped'" do
+            let(:params) { {
+                :prefix => '/ci',
+                :service_ensure => 'stopped',
+            } }
+            it { should contain_service('jenkins') .with_ensure('stopped') }
+        end
+    end
 end
