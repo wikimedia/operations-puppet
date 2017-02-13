435e455f9186f02c783e9ff45e45d9d154f6bd60
Antoine Musso <hashar@free.fr>
2017-02-13 16:36:48 +0100

    jenkins: migrate to systemd
    Migrate bits from upstream sysvinit script to systemd.
    
    Drop /etc/default/jenkins entirely. systemd EnvironmentFile reads the
    assignement literally and does not do any string interpolation this
    something like:
    
     JENKINS_ARGS="--httpPort=$HTTP_PORT"
    
    Ends up spawning a process with '$HTTP_PORT' literally regardless of a
    matching environment variable being set.  Thus pass the template
    variables directly in ExecStart.
    
    Expose JENKINS_HOME in the environment.  The init.d script makes daemon
    to expose it and the Jenkins source code shows that it is relying on it.
    
    The maximum number of file descriptor was passed to daemon. It is now
    set via Service.LimitNOFILE.
    
    The init script uses daemon which SIGTERM the service. That is the
    default behavior for systemd as well.  However java returns an exit code
    of 128 + SIGTERM = 143 which causes the unit to be marked as failling.
    Accept 143 as a valid exit code on stop (SuccessExitStatus=143).
    
    ajp13 is disabled by default, do not pass it explicitly.
    
    Expand jenkins and plugins war files under /var/run/jenkins/* instead of
    under JENKINS_HOME (by setting --webroot and --pluginroot).
    
    Port support to have the service unmanaged passing to service ensure =>
    undef.
    
    Jenkins spurts out to standard output which systemd collects to syslog.
    Set systemd.Service.SyslogIdentifier to 'jenkins'.
    
    Use systemd::syslog() with programname_compare to ensure we only capture
    logs for a program named exactly 'jenkins'.  Introduced by
    I4e46cf4c20d98e8947e16c174cacea0f173b603d .  Covered by a spec.
    
    Remove our custom logrotate, rely on the one from systemd::syslog which
    rotates all log files and has almost the same settings.
    
    Add a few specs to cover the erb template expansion.
    
    Depends-On: I4e46cf4c20d98e8947e16c174cacea0f173b603d
    Change-Id: I82ec3fa8ab209c449678e663822d9ac507cbb208

--- modules/jenkins/spec/classes/jenkins_spec.rb
+++ modules/jenkins/spec/classes/jenkins_spec.rb
@@ -6,7 +6,75 @@ describe 'jenkins' do
     } }
     let(:params) { {
         :prefix => '/ci',
-
     } }
     it { should compile }
+
+    describe 'when service_ensure' do
+        context "is 'unmanaged'" do
+            let(:params) { {
+                :prefix => '/ci',
+                :service_ensure => 'unmanaged',
+            } }
+            it { should contain_service('jenkins').without_ensure }
+        end
+        context "is 'running'" do
+            let(:params) { {
+                :prefix => '/ci',
+                :service_ensure => 'running',
+            } }
+            it { should contain_service('jenkins') .with_ensure('running') }
+        end
+        context "is 'stopped'" do
+            let(:params) { {
+                :prefix => '/ci',
+                :service_ensure => 'stopped',
+            } }
+            it { should contain_service('jenkins') .with_ensure('stopped') }
+        end
+    end
+    describe 'systemd jenkins service' do
+        it 'should set Umask' do
+            should contain_file('/lib/systemd/system/jenkins.service')
+                .with_content(/^UMask=0002$/)
+        end
+        it 'should set LimitNOFILE' do
+            should contain_file('/lib/systemd/system/jenkins.service')
+                .with_content(/^LimitNOFILE=8192$/)
+        end
+        it 'should expose JENKINS_HOME in environment' do
+            # Required by Jenkins
+            should contain_file('/lib/systemd/system/jenkins.service')
+                .with_content(%r%^Environment=JENKINS_HOME=/var/lib/jenkins$%)
+        end
+        it 'should pass prefix to jenkins' do
+            should contain_file('/lib/systemd/system/jenkins.service')
+                .with_content(%r% --prefix=/ci$%)
+        end
+        context 'when http port is given' do
+            let(:params) { {
+                :prefix => '/ci',
+                :http_port => 8042,
+            } }
+            it 'should set http port' do
+                should contain_file('/lib/systemd/system/jenkins.service')
+                    .with_content(/ --httpPort=8042 /)
+            end
+        end
+        context 'when access log is enabled' do
+            let(:params) { {
+                :prefix => '/ci',
+                :access_log => true,
+            } }
+            it 'should set Jenkins access logger' do
+                should contain_file('/lib/systemd/system/jenkins.service')
+                    .with_content(/^ExecStart=.+SimpleAccessLogger.+/)
+            end
+        end
+    end
+    describe 'rsyslog configuration' do
+        it 'should match programname with a strict equality' do
+            should contain_file('/etc/rsyslog.d/20-jenkins.conf')
+                .with_content(/^:programname, isequal, "jenkins"/)
+        end
+    end
 end
