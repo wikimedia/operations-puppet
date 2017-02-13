0911e0743f553c90119a7ec1fe1eccab1737bccc
Antoine Musso <hashar@free.fr>
2017-02-13 12:49:05 +0100

    jenkins: allow access log to be flipped
    We have the web service logging all access under access.log which is
    quite spammy. For some instances of Jenkins we probably do not need to
    log them at all.
    
    Add a class parameter 'access_log' that lets one enable/disable logging.
    Default: false.
    Let the current Jenkins instance to log via role::ci::master.
    
    Also vary the log dir based on the service $NAME.
    
    Change-Id: I18c00429f171e887d59fbaa50d52404938d2ba61

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -8,6 +8,9 @@
 # Prefix for web access to use with Apache proxying. Must be the full path with
 # a leading slash. Example: /ci
 #
+# [*access_log*]
+# Whether to enable the web service access.log. Boolean. Default: false
+#
 # [*service_ensure*]
 # Passed to Puppet Service['jenkins']. If set to 'unmanaged', pass undef to
 # prevent Puppet from managing the service. Default: 'running'.
@@ -20,6 +23,7 @@
 # Default: '0002'
 class jenkins(
     $prefix,
+    $access_log = false,
     $service_ensure  = 'running',
     $service_enable = true,
     $umask = '0002'
