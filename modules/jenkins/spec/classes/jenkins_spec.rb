67467b6ec9d6417936119c06c35be0f7e528ee5c
Antoine Musso <hashar@free.fr>
2017-02-13 16:36:48 +0100

    jenkins: migrate to systemd
    Migrate bits from upstream sysvinit script to systemd.
    Drop /etc/default/jenkins entirely, relevant parts are set in puppet and
    exposed as environment variables in systemd.service.
    
    Port support to have the service unmanaged passing to service ensure =>
    undef.
    
    The maximum number of file descriptor was passed to daemon. It is now
    set via Service.LimitNOFILE.
    
    Jenkins spurts out to standard output which systemd collects to syslog.
    Set systemd.Service.SyslogIdentifier to 'jenkins'.
    
    Use systemd::syslog() with programname_compare to ensure we only capture
    logs for a program named exactly 'jenkins'.  Introduced by
    I4e46cf4c20d98e8947e16c174cacea0f173b603d .  Covered by a spec.
    
    Remove our custom logrotate, rely on the one from systemd::syslog which
    rotates all log files and has almost the same settings.
    
    Add a few specs to cover the erb template expansion.
    
    Depends-On: I4e46cf4c20d98e8947e16c174cacea0f173b603d
    Change-Id: I82ec3fa8ab209c449678e663822d9ac507cbb208

--- modules/jenkins/spec/classes/jenkins_spec.rb
+++ modules/jenkins/spec/classes/jenkins_spec.rb
@@ -6,7 +6,62 @@ describe 'jenkins' do
     } }
     let(:params) { {
         :prefix => '/ci',
-
     } }
     it { should compile }
+
+    describe 'when service_ensure' do
+        context "is 'unmanaged'" do
+            let(:params) { {
+                :prefix => '/ci',
+                :service_ensure => 'unmanaged',
+            } }
+            it { should contain_service('jenkins').without_ensure }
+        end
+        context "is 'running'" do
+            let(:params) { {
+                :prefix => '/ci',
+                :service_ensure => 'running',
+            } }
+            it { should contain_service('jenkins') .with_ensure('running') }
+        end
+        context "is 'stopped'" do
+            let(:params) { {
+                :prefix => '/ci',
+                :service_ensure => 'stopped',
+            } }
+            it { should contain_service('jenkins') .with_ensure('stopped') }
+        end
+    end
+    describe 'systemd jenkins service' do
+        it 'should set Umask' do
+            should contain_file('/lib/systemd/system/jenkins.service')
+                .with_content(/^UMask=0002$/)
+        end
+        it 'should set LimitNOFILE' do
+            should contain_file('/lib/systemd/system/jenkins.service')
+                .with_content(/^LimitNOFILE=8192$/)
+        end
+        it 'should have Environment directives set' do
+            should contain_file('/lib/systemd/system/jenkins.service')
+                .with_content(/^Environment="JENKINS_JAVA_ARGS=[^"]+"$/)
+                .with_content(/^Environment=JENKINS_HTTP_PORT=8080$/)
+                .with_content(%r%^Environment=JENKINS_PREFIX=/ci$%)
+        end
+        context 'when access log is enabled' do
+            let(:params) { {
+                :prefix => '/ci',
+                :access_log => true,
+            } }
+            it 'should set Jenkins access logger' do
+                should contain_file('/lib/systemd/system/jenkins.service')
+                    .with_content(/^Environment="JENKINS_JAVA_ARGS=[^"]+SimpleAccessLogger[^"]*"$/)
+            end
+        end
+    end
+    describe 'rsyslog configuration' do
+        it 'should match programname with a strict equality' do
+            should contain_file('/etc/rsyslog.d/20-jenkins.conf')
+                .with_content(/^:programname, isequal, "jenkins"/)
+        end
+    end
 end
