c47bb220740697916fca3ef6c25542870697ba8a
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2017-02-15 01:00:45 -0800

    labstore: Read directory size diamond collector config from external file
    Change-Id: I599df0119bab8732a76e20d655139e2714568106

--- modules/role/manifests/labs/nfs/secondary.pp
+++ modules/role/manifests/labs/nfs/secondary.pp
@@ -112,42 +112,10 @@ class role::labs::nfs::secondary($monitor = 'eth0') {
             privileges => ['ALL = NOPASSWD: /usr/bin/timeout 10m /usr/bin/nice -n 19 /usr/bin/ionice -c 3 /usr/bin/du -k -s'],
         }
 
-        $dir_size_collector_config = [
-            {
-                base_glob_pattern          => '/exp/project/*/home',
-                base_glob_exclude          => '/tools/',
-                build_prefix_from_dir_path => True,
-                build_prefix_depth         => 2,
-                custom_prefix              => 'misc',
-            },
-            {
-                base_glob_pattern          => '/exp/project/*/project',
-                base_glob_exclude          => '/tools/',
-                build_prefix_from_dir_path => True,
-                build_prefix_depth         => 2,
-                custom_prefix              => 'misc',
-            },
-            {
-                base_glob_pattern          => '/exp/project/tools/home/*',
-                build_prefix_from_dir_path => True,
-                build_prefix_depth         => 2,
-            },
-            {
-                base_glob_pattern          => '/exp/project/tools/project/*',
-                build_prefix_from_dir_path => True,
-                build_prefix_depth         => 2,
-            },
-        ]
-
         diamond::collector { 'DirectorySize':
-            source   => 'puppet:///modules/labstore/monitor/dir_size_tracker.py',
-            settings => {
-                interval                  => 86400,
-                hostname                  => 'labstore-secondary',
-                path_prefix               => 'labstore',
-                dir_size_collector_config => $dir_size_collector_config,
-            },
-            require  => Sudo::User['diamond_dir_size_tracker'],
+            source      => 'puppet:///modules/labstore/monitor/dir_size_tracker.py',
+            config_file => 'puppet:///modules/labstore/monitor/DirectorySizeCollector.conf',
+            require     => Sudo::User['diamond_dir_size_tracker'],
         }
     }
 }
