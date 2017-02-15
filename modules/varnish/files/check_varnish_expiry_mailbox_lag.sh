140638674e87e7094eac96d2ba35778ce4313a8f
Emanuele Rocca <ema@wikimedia.org>
2017-02-15 12:10:23 +0100

    varnish: icinga check for expiry mailbox lag
    We have found a correlation between the 503 errors described in T145661
    and the varnish expiry thread not being able to catch up with its
    mailbox.
    
    Add an icinga check alerting when the lag grows beyond certain
    thresholds.
    
    Bug: T145661
    Change-Id: I5e76b594d8c57fa9a679088c794b04c7879be715

---
+++ modules/varnish/files/check_varnish_expiry_mailbox_lag.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cmd="/usr/bin/varnishstat -t off -1"
+
+if ! $cmd > /dev/null 2>&1;
+then
+    echo "UNKNOWN: cannot run varnishstat"
+    exit 3
+fi
+
+$cmd | awk '
+/exp_mailed/ { m = $2 }
+/exp_received/ { r = $2 }
+
+END {
+    msg = "expiry mailbox lag is "
+    lag = m - r
+
+    if (lag > 10000) {
+        print "CRITICAL: " msg lag
+        exit 2
+    }
+    else if (lag > 1000) {
+        print "WARNING: " msg lag
+        exit 1
+    } else {
+        print "OK: " msg lag
+        exit 0
+    }
+}'
