4ee863e4cb566c631dd45618432c96bc0ef3308e
elukey <ltoscano@wikimedia.org>
2017-02-15 11:03:47 +0100

    Set maximum JVM heap size for Zookeeper
    Bug: T157968
    Change-Id: I8be06e2bd9152d69c74e56ca063443661c204400

--- modules/role/manifests/zookeeper/server.pp
+++ modules/role/manifests/zookeeper/server.pp
@@ -33,7 +33,8 @@ class role::zookeeper::server {
         # There seems to be no elegant way to avoid the JVM spam,
         # so until somebody finds a better way we redirect stdout to /dev/null
         # and we filter out JAVA_TOOL_OPTIONS messages from stderr.
-        cleanup_script_args => '-n 10 2>&1 > /dev/null | grep -v JAVA_TOOL_OPTIONS'
+        cleanup_script_args => '-n 10 2>&1 > /dev/null | grep -v JAVA_TOOL_OPTIONS',
+        java_opts           => '-Xms1g -Xmx1g',
     }
 
     ferm::service { 'zookeeper':
