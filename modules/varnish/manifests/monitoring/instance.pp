f9b8776a6942d5be22c6189e83b85df9a907de8f
Emanuele Rocca <ema@wikimedia.org>
2017-02-15 12:10:23 +0100

    varnish: icinga check for expiry mailbox lag
    We have found a correlation between the 503 errors described in T145661
    and the varnish expiry thread not being able to catch up with its
    mailbox.
    
    Add an icinga check alerting when the lag grows beyond certain
    thresholds.
    
    Bug: T145661
    Change-Id: I5e76b594d8c57fa9a679088c794b04c7879be715

--- modules/varnish/manifests/monitoring/instance.pp
+++ modules/varnish/manifests/monitoring/instance.pp
@@ -4,4 +4,22 @@ define varnish::monitoring::instance($instance) {
         description   => "Varnish HTTP ${instance} - port ${port}",
         check_command => "check_http_varnish!varnishcheck!${port}",
     }
+
+    # We have found a correlation between the 503 errors described in T145661
+    # and the expiry thread not being able to catch up with its mailbox
+    file { '/usr/local/lib/nagios/plugins/check_varnish_expiry_mailbox_lag':
+        ensure => present,
+        source => 'puppet:///modules/role/varnish/check_varnish_expiry_mailbox_lag.sh',
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    nrpe::monitor_service { 'check_varnish_expiry_mailbox_lag':
+        description    => "Check Varnish ${instance} expiry mailbox lag",
+        nrpe_command   => '/usr/local/lib/nagios/plugins/check_varnish_expiry_mailbox_lag',
+        retry_interval => 30,
+        retries        => 3,
+        require        => File['/usr/local/lib/nagios/plugins/check_varnish_expiry_mailbox_lag'],
+    }
 }
