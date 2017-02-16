75131bbd5af63121614d767187593c4c49e6e6fe
Jcrespo <jcrespo@wikimedia.org>
2017-02-16 17:05:52 +0000

    Revert "contint: git-daemon service is 'sysvinit'"
    This reverts commit 40bbe4363a38757ff9f44ed614ba737e76675ca1.
    
    Change-Id: I97a03b8453a798e28c30bcd59de17b916d1b5260

--- modules/contint/manifests/zuul/git_daemon.pp
+++ modules/contint/manifests/zuul/git_daemon.pp
@@ -24,7 +24,6 @@ class contint::zuul::git_daemon(
 
   service { 'git-daemon':
     ensure     => running,
-    provider   => 'sysvinit',
     enable     => true,
     hasrestart => true,
     subscribe  => File['/etc/default/git-daemon'],
