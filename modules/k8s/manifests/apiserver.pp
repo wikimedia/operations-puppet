ca8ce5c9193bde5a184ca441eb4c0be6cd5174cb
YuviPanda <yuvipanda@gmail.com>
2017-02-17 14:02:03 -0800

    k8s: Fix puppet resource conflict that is absolutely stupid
    WHY CAN PUPPET NOT JUST DO mkdir -p
    
    Change-Id: Ia7e18d2e31926815452fe691c28694bd73f20670

--- modules/k8s/manifests/apiserver.pp
+++ modules/k8s/manifests/apiserver.pp
@@ -24,11 +24,14 @@ class k8s::apiserver(
 ) {
     include ::k8s::users
 
-    file { '/etc/kubernetes':
-        ensure => directory,
-        owner  => 'kubernetes',
-        group  => 'kubernetes',
-        mode   => '0700',
+    if !defined(File['/etc/kubernetes']) {
+        # because base::expose_puppet_certs also does this
+        file { '/etc/kubernetes':
+            ensure => directory,
+            owner  => 'kubernetes',
+            group  => 'kubernetes',
+            mode   => '0700',
+        }
     }
 
     if $use_package {
