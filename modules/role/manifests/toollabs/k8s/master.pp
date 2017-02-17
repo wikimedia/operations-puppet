f548022b443b7519913faf1cf50ded15e43a464a
YuviPanda <yuvipanda@gmail.com>
2017-02-17 13:27:50 -0800

    tools: Allow setting up k8s master without tools star cert
    Uses puppet certs
    
    Change-Id: Ib2633f71464940c5fee5809cba94e59dc572cb07

--- modules/role/manifests/toollabs/k8s/master.pp
+++ modules/role/manifests/toollabs/k8s/master.pp
@@ -1,13 +1,31 @@
 # filtertags: labs-project-tools
-class role::toollabs::k8s::master {
+class role::toollabs::k8s::master(
+    $use_puppet_certs = false,
+) {
     include ::base::firewall
     include ::toollabs::infrastructure
 
     $master_host = hiera('k8s::master_host', $::fqdn)
     $etcd_url = join(prefix(suffix(hiera('k8s::etcd_hosts'), ':2379'), 'https://'), ',')
 
-    sslcert::certificate { 'star.tools.wmflabs.org':
-        before       => Class['::k8s::apiserver'],
+    if $use_puppet_certs {
+        base::expose_puppet_certs { '/etc/kubernetes':
+            provide_private => true,
+            user            => 'kubernetes',
+            group           => 'kubernetes',
+            before          => Class['::k8s::apiserver']
+        }
+        $ssl_cert_path = '/etc/kubernetes/ssl/cert.pem'
+        $ssl_key_path = '/etc/kubernetes/ssl/server.key'
+
+    } else {
+        $ssl_certificate_name = 'star.tools.wmflabs.org'
+        sslcert::certificate { $ssl_certificate_name:
+            before       => Class['::k8s::apiserver'],
+        }
+
+        $ssl_cert_path = "/etc/ssl/localcerts/${ssl_certificate_name}.chained.crt"
+        $ssl_key_path = "/etc/ssl/private/${ssl_certificate_name}.key"
     }
 
     class { '::k8s::apiserver':
@@ -15,7 +33,8 @@ class role::toollabs::k8s::master {
         use_package                => true,
         docker_registry            => hiera('docker::registry'),
         host_automounts            => ['/var/run/nslcd/socket'],
-        ssl_certificate_name       => 'star.tools.wmflabs.org',
+        ssl_cert_path              => $ssl_cert_path,
+        ssl_key_path               => $ssl_key_path,
         host_path_prefixes_allowed => [
             '/data/project/',
             '/data/scratch/',
