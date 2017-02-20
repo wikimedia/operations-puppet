ec6774c131d1cc1bd202005b6fd9a01de0ce536c
Jaime Crespo <jcrespo@wikimedia.org>
2017-02-20 18:00:21 +0100

    mariadb: rebase module to the latest version and separate labs pass
    Explicitly setup separate account credentials for labs from mysql
    clients. This will not be used automatically by mysql client, but
    it will be integrated into a future sql.py client script.
    
    This is the first step into many into hardending and separating
    mysql credentials on the cluster. Note root access with passwords
    is disabled (we use unix_socket authentication), and access is
    already limited from a bunch of selected clients.
    
    Bug: T104900
    Bug: T146149
    Depends-on: I3993cb0aff5ba
    Change-Id: I90148cc93e5b5c054c8c63d1207a90f99f695f2d

--- modules/role/manifests/mariadb/client.pp
+++ modules/role/manifests/mariadb/client.pp
@@ -2,6 +2,13 @@
 # This role provides remote access to mysql server
 # **DO NOT ADD** to non-dedicated hosts**
 class role::mariadb::client {
+
+    # prevent accidental addition on a db server or a non-dedicated client
+    if !($::fqdn in ['neodymium.eqiad.wmnet', 'sarin.codfw.wmnet']) {
+        fail('role::mariadb::client should only be used on root-owned, \
+             dedicated servers.')
+    }
+
     include mariadb::packages_client
     include passwords::misc::scripts
 
@@ -11,6 +18,7 @@ class role::mariadb::client {
     }
 
     $password = $passwords::misc::scripts::mysql_root_pass
+    $labsdb_password = $passwords::misc::scripts::mysql_labsdb_root_pass
     file { '/root/.my.cnf':
         owner   => 'root',
         group   => 'root',
