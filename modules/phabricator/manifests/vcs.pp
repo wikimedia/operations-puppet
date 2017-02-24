e30a49f8ee34466c4e29af429ed1dfafbf4b545c
Paladox <thomasmulhall410@yahoo.com>
2017-02-24 21:45:29 +0000

    Phabricator: Migrate to base::service_unit for ssh-phab
    This is part 1 of the migration.
    
    Bug: T137928
    Bug: T158434
    Change-Id: Ia7a6200fab11c4bad51ee0e0163fad17f83fa5a6

--- modules/phabricator/manifests/vcs.pp
+++ modules/phabricator/manifests/vcs.pp
@@ -92,26 +92,11 @@ class phabricator::vcs (
         require    => File['/usr/local/bin/git-http-backend'],
     }
 
-    if $::initsystem == 'upstart' {
-        $init_file = '/etc/init/ssh-phab.conf'
-        $init_template = 'phabricator/sshd-phab.conf.erb'
-    } else {
-        $init_file = '/etc/systemd/system/ssh-phab.service'
-        $init_template = 'phabricator/sshd-phab.service.erb'
-    }
-
-    file { $init_file:
-        content => template($init_template),
-        mode    => '0644',
-        owner   => 'root',
-        group   => 'root',
+    base::service_unit { 'ssh-phab':
+        ensure  => 'present',
+        systemd => true,
+        upstart => true,
+        strict  => false,
         require => Package['openssh-server'],
-    }
-
-    service { 'ssh-phab':
-        ensure     => running,
-        provider   => $::initsystem,
-        hasrestart => true,
-        require    => File[$init_file],
     }
 }
