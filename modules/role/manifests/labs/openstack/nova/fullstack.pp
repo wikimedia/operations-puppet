3697c31792d0550f1c3989b406e4693749463fa4
cpettet <rush@wikimedia.org>
2017-02-24 09:48:44 -0600

    nova: monitor for fullstack test daemon
    * Change max_pool to 3
    * Watch for 1 proc of nova-fullstack
    * bump up creation timeout as we are leaking
      instances at 180s with sometimes crashes
      waiting on creation.
    * use image debian-8.7
    
    See instances in flight:
    OS_TENANT_ID=admin-monitoring openstack server list
    
    Change-Id: I616edb424def0011368999ec9a0997d4db7d2a95

--- modules/role/manifests/labs/openstack/nova/fullstack.pp
+++ modules/role/manifests/labs/openstack/nova/fullstack.pp
@@ -7,4 +7,9 @@ class role::labs::openstack::nova::fullstack {
     class { '::openstack::nova::fullstack':
         password => $fullstack_pass,
     }
+
+    nrpe::monitor_service { 'nova-fullstack':
+        description  => 'nova instance creation test',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1: -C python -a nova-fullstack',
+    }
 }
