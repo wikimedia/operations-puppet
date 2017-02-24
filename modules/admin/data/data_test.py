932fa63979fd77f5f0d0bab075cee7eff8a3c96f
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-02-24 09:10:15 +0100

    Remove absense check from data_test.yaml
    The cross verification script added in cd2557fbc50ebe79f78b54e2980490c65cf5f749
    validates more account features. Remove the absense check from this script
    since otherwise new features like the tracking of absent users with LDAP-only
    access need to be covered twice. Also, the check never really worked anyway;
    the cross-verification script found several absent users which were not correctly
    tracked.
    
    The GID check can be kept since it prevents malformed group changes when adding
    a new group.
    
    Change-Id: I9168c5ab07a4b288ce89c9f31d697435b476bae2

--- modules/admin/data/data_test.py
+++ modules/admin/data/data_test.py
@@ -11,49 +11,10 @@ import yaml
 
 
 class DataTest(unittest.TestCase):
-    def flatten(self, lists):
-        """flattens nested arrays"""
-        return list(itertools.chain.from_iterable(lists))
-
-    def all_assigned_users(self, admins):
-        """unique assigned users
-        :param admins: hash from valid data.yaml
-        :returns: list
-        """
-        nested_users_list = map(
-            lambda u: u['members'], admins['groups'].values()
-        )
-        return list(set(self.flatten(nested_users_list)))
-
     def testDataDotYaml(self):
 
         with open(os.path.join(os.path.dirname(__file__), 'data.yaml')) as f:
             admins = yaml.safe_load(f)
-
-        all_users = list(admins['users'])
-        absented_group_members = admins['groups']['absent']['members']
-        absented_users = [k for k, v in admins['users'].iteritems()
-                          if v.get('ensure', '') == 'absent']
-        absented_users = [x for x in absented_users if x not in
-                          absented_group_members]
-        grouped_users = self.all_assigned_users(admins)
-
-        # ensure all assigned users exist
-        non_existent_users = [u for u in grouped_users if u not in all_users]
-        self.assertEqual(
-            [],
-            non_existent_users,
-            'Users assigned that do not exist: %r' % non_existent_users
-        )
-
-        # ensure absented users are not assigned
-        assigned_absented = set(grouped_users).intersection(
-            set(absented_users))
-        self.assertEqual(
-            set(),
-            assigned_absented,
-            'Users absent but not in "absent" group: %r' % assigned_absented,
-        )
 
         # ensure no two groups uses the same gid
         gids = filter(None, [
