a2cc78096ca797ee599a0489525a2a5aa1013c18
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-02-27 12:11:47 +0100

    role::mediawiki::webserver: adapt to new conftool-data structure
    Change-Id: Iddf2fb5da6b4367ce05efea739a75825e4a1db70

--- modules/role/manifests/mediawiki/webserver.pp
+++ modules/role/manifests/mediawiki/webserver.pp
@@ -26,8 +26,8 @@ class role::mediawiki::webserver {
         $lvs_service = pick($::lvs::configuration::lvs_services[$pool], {})
         $conftool_config = pick($lvs_service['conftool'], {'cluster' => 'appserver'})
         $module_path = get_module_path($module_name)
-        $site_nodes = loadyaml("${module_path}/../../conftool-data/nodes/${::site}.yaml")
-        $pool_nodes = keys($site_nodes[$conftool_config['cluster']])
+        $site_nodes = loadyaml("${module_path}/../../conftool-data/node/${::site}.yaml")
+        $pool_nodes = keys($site_nodes[$::site][$conftool_config['cluster']])
         if member($pool_nodes, $::fqdn) {
             $times = cron_splay($pool_nodes, 'daily', 'hhvm-conditional-restarts')
             cron { 'hhvm-conditional-restart':
