3f5a56c3bdbec659f944c5dca5ed31bc7745dd04
Faidon Liambotis <faidon@wikimedia.org>
2017-02-27 16:03:24 +0200

    apache: remove precise (and Apache 2.2) support
    Remove support for Ubuntu precise, not used anymore. Since this was the
    last distribution to support Apache 2.2, also remove any Apache 2.2
    compatibility statements from the module.
    
    Change-Id: I640f18bc1eafb1d4cfbaa3577959482340d5026d

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -2,25 +2,16 @@
 #
 # Provisions Apache web server package and service.
 #
-# This module was designed to provide a consistent interface over a
-# mixed production environment which includes both Precise / Apache 2.2
-# hosts and Trusty / Apache 2.4 hosts, and which utilizes Apache for
-# both mission-critical services (serving MediaWiki) and small, and
-# internal services.
-#
 # It accomodates different use-cases by expecting the caller to pass in
 # full configuration files, rather than generating configuration files
 # based on complex parameters and switches.
 #
-# The module provides forward- and back-compatibility by enabling
-# mod_version, mod_filter and mod_access_compat by default, and by using
-# /etc/apache2/conf-{enabled,available} to manage configuration snippets
-# on both Precise and Trusty.
+# The module provides backwards-compatibility with Apache 2.2 by enabling
+# mod_filter and mod_access_compat by default.
 #
 class apache {
     include ::apache::mod::access_compat  # enables allow/deny syntax in 2.4
     include ::apache::mod::filter         # enables AddOutputFilterByType in 2.4
-    include ::apache::mod::version        # enables <IfVersion> config guards
     include ::apache::monitoring          # send metrics to Diamond and Ganglia
     include ::apache::mpm                 # prefork by default
 
@@ -78,16 +69,6 @@ class apache {
         match   => 'env-enabled',
         path    => '/etc/apache2/envvars',
         require => Package['apache2'],
-    }
-
-    if os_version('ubuntu < trusty') {
-        # Early releases of Apache manage configuration snippets in conf.d/.
-        # We standardize on conf-enabled/*.conf with this small shim.
-        file { '/etc/apache2/conf.d/load-conf-enabled.conf':
-            content => "Include /etc/apache2/conf-enabled/*.conf\n",
-            require => File['/etc/apache2/conf-enabled'],
-            notify  => Service['apache2'],
-        }
     }
 
     apache::conf { 'defaults':
