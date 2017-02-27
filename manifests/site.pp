18a16e79a7b7aaf353cc581d594e86acce82ebe0
Daniel Zahn <dzahn@wikimedia.org>
2017-02-27 11:57:19 -0800

    install/bast: move tftp server from bast3001 to bast3002
    Since bast3001 is dying, move TFTP server for esams
    to bast3002.
    
    Switch other esams subnets to use it in DHCP config.
    
    Remove bast3001 itself from linux-host-entries.
    
    Bug: T156506
    Change-Id: I0833cb4bee62c6008dad06d1c8363e6a011e4df3

--- manifests/site.pp
+++ manifests/site.pp
@@ -165,7 +165,6 @@ node 'bast2001.wikimedia.org' {
 # Bastion in the Netherlands
 node 'bast3001.wikimedia.org' {
     role(bastionhost::general,
-        installserver::tftp,
         prometheus::ops)
 
     interface::add_ip6_mapped { 'main': interface => 'eth0', }
@@ -173,7 +172,8 @@ node 'bast3001.wikimedia.org' {
 }
 
 node 'bast3002.wikimedia.org' {
-    role(bastionhost::general)
+    role(bastionhost::general,
+         installserver::tftp)
 
     interface::add_ip6_mapped { 'main': interface => 'eth0', }
     class { '::ganglia::monitor::aggregator': sites =>  'esams', }
