dd5b2ce3f84667dd2ee5952266604a7044be49e4
Faidon Liambotis <faidon@wikimedia.org>
2017-02-27 16:08:10 +0200

    ganglia: remove precise support
    Remove support for Ubuntu precise, not used anymore.
    
    Change-Id: I6a3f8fc40d03b3f9c0c3bec8c20cc30815e1781b

--- modules/ganglia/manifests/monitor/service.pp
+++ modules/ganglia/manifests/monitor/service.pp
@@ -1,16 +1,6 @@
 class ganglia::monitor::service() {
     Class[ganglia::monitor::config] -> Class[ganglia::monitor::service]
 
-    if $::operatingsystem == 'Ubuntu' and os_version('ubuntu < trusty') {
-        file { '/etc/init/ganglia-monitor.conf':
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0444',
-            source => "puppet:///modules/${module_name}/upstart/ganglia-monitor.conf",
-            before => Service['ganglia-monitor'],
-        }
-    }
-
     if os_version('debian >= jessie') {
         file { '/etc/systemd/system/ganglia-monitor-aggregator@.service':
             owner  => 'root',
