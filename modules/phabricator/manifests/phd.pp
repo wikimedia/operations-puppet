91e762aecb4c1b5ecd7603ae491923896cab99c8
Paladox <thomasmulhall410@yahoo.com>
2017-02-27 18:57:10 +0000

    Phabricator: Migrate to base::service_unit for phd
    This is part 2 of the migration.
    
    Bug: T137928
    Bug: T158434
    Change-Id: I8ca4e605c324c326a533bdd6c83392d7a8518190

--- modules/phabricator/manifests/phd.pp
+++ modules/phabricator/manifests/phd.pp
@@ -6,6 +6,8 @@
 class phabricator::phd (
     $settings = {},
     $basedir  = '/',
+    $base_requirements = '',
+    $phabricator_active_server = '',
 ) {
     group { 'phd':
         ensure => present,
@@ -57,5 +59,41 @@ class phabricator::phd (
     logrotate::conf { 'phd':
         ensure => present,
         source => 'puppet:///modules/phabricator/logrotate_phd',
+    }
+
+    if $::hostname == $phabricator_active_server {
+        $phd_service_ensure = 'running'
+    } else {
+        $phd_service_ensure = 'stopped'
+    }
+
+    if $::initsystem == 'systemd' {
+      base::service_unit { 'phd':
+          ensure         => 'present',
+          systemd        => true,
+          upstart        => false,
+          sysvinit       => false,
+          strict         => false,
+          require        => Package['openssh-server'],
+          service_params => {
+              ensure     => $phd_service_ensure,
+              provider   => $::initsystem,
+              hasrestart => true,
+          },
+      }
+    } else {
+      # This needs to become <s>Upstart</s> systemd managed
+      # https://secure.phabricator.com/book/phabricator/article/managing_daemons/
+      # Meanwhile upstream has a bug to make an LSB friendly wrapper
+      # https://secure.phabricator.com/T8129
+      # see examples of real-word unit files in comments of:
+      # https://secure.phabricator.com/T4181
+      service { 'phd':
+          ensure     => $phd_service_ensure,
+          start      => '/usr/sbin/service phd start --force',
+          status     => '/usr/bin/pgrep -f phd-daemon',
+          hasrestart => true,
+          require    => $base_requirements,
+      }
     }
 }
