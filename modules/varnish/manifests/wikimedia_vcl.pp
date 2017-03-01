1db0da6a27614b74157aac4c61ae9a68be7a0136
Emanuele Rocca <ema@wikimedia.org>
2017-03-01 16:29:48 +0100

    varnishtest: mock VCL configuration
    Change-Id: Ifde3e3c0dd7843655c4c2e3ff084b7400a4b7273

--- modules/varnish/manifests/wikimedia_vcl.pp
+++ modules/varnish/manifests/wikimedia_vcl.pp
@@ -3,6 +3,18 @@ define varnish::wikimedia_vcl($varnish_testing, $template_path) {
         $varnish_include_path = '/usr/share/varnish/tests/'
         $dynamic_backend_caches = false
         $netmapper_dir = $varnish_include_path
+
+        # Mock VCL config for varnishtest
+        $vcl_config = {
+            'top_domain'       => 'org',
+            'shortener_domain' => 'w.wiki',
+            'upload_domain'    => 'upload.wikimedia.org',
+            'purge_host_regex' => '^(?!upload\.wikimedia\.org)',
+            'static_host'      => 'en.wikipedia.org',
+            'grace_healthy'    => '5s',
+            'grace_sick'       => '10s',
+            'keep'             => '15s',
+        }
     }
 
     file { $title:
