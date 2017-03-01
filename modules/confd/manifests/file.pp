b254889b486dc2dd3917508c604f9cab1daa2f69
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-01 17:26:49 +0100

    confd: add ability to define a global and per-template prefix
    The global prefix can be useful for the case in which we want to switch
    the etcd cluster we're using as a master, and in general to abstract the
    conftool prefix out of templates, while the per-template prefix can save
    us some boilerplate.
    
    Change-Id: Ib6ea9134daecce6ce7c65ad34ab4724634ae4bf5

--- modules/confd/manifests/file.pp
+++ modules/confd/manifests/file.pp
@@ -2,9 +2,30 @@
 #
 # Defines a service template to be monitored by confd,
 # and the corresponding geneated config file.
-
+#
+# === Parameters
+#
+# [*prefix*] Prefix to use for all keys; it will actually be joined with the global
+#            confd prefix
+#
+# [*watch_keys*] list of keys to watch relative to the value assigned in
+#                $prefix.
+#
+# [*uid*] Numeric uid of the owner of the file produced. Default: 0
+#
+# [*gid*] Numeric gid of the owner of the produced file. Default: 0
+#
+# [*mode*] File mode for the generated file.
+#
+# [*reload*] Command to execute when the produced file changes
+#
+# [*check*] Check to execute when the produced file changes
+#
+# [*content*] The actual go text/template used for generating the file
+#
 define confd::file (
     $ensure     = 'present',
+    $prefix     = undef,
     $watch_keys = [],
     $uid        = undef,
     $gid        = undef,
@@ -12,9 +33,10 @@ define confd::file (
     $reload     = undef,
     $check      = undef,
     $content    = undef,
+
 ) {
 
-    include ::confd
+    require ::confd
 
     $safe_name = regsubst($name, '/', '_', 'G')
 
