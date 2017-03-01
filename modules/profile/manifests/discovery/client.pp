728b8d6eea7ac3bfbc2af95a8cc21da9bdc6d628
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-01 19:18:13 +0100

    profile::discovery::client: create confd-generate files for discovery
    This profile will install all the files that can be consumed locally for
    reading our global discovery data, stored in etcd. These files,
    typically in yaml format, will be generated via confd templates.
    
    Bug: T149617
    Change-Id: I545530e1f6632626623bad57828a62fe5e845639

---
+++ modules/profile/manifests/discovery/client.pp
@@ -0,0 +1,38 @@
+# == Class profile::discovery::client
+#
+# Will use confd to watch our discovery system and save the result as a json file in a chosen directory.
+#
+# === Parameters
+#
+# [*path*] The directory where the file should go.
+#
+# [*watch_interval*] The interval in seconds for checks on etcd. Defaults to 5
+#
+class profile::discovery::client(
+    $path=hiera('profile::discovery::path'),
+    $watch_interval=hiera('profile::discovery::watch_interval', 5),
+    $conftool_prefix = hiera('conftool_prefix'),
+){
+    file { $path:
+        ensure => directory,
+        owner  => root,
+        group  => root,
+        mode   => '0755',
+    }
+
+    class { 'confd':
+        interval => $watch_interval,
+        prefix   => $conftool_prefix,
+    }
+
+    $keyspace = '/discovery'
+
+    confd::file { "${path}/discovery-basic.yaml":
+        ensure     => present,
+        content    => template('profile/discovery/basic.yaml.tpl.erb'),
+        watch_keys => ['/'],
+        prefix     => $keyspace,
+        mode       => '0444',
+        check      => 'ruby -e \"require \'yaml\'; YAML.load_file(\'{{ .src }}\')\"',
+    }
+}
