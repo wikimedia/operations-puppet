6e7dc3846698493ac11024b9ca7c404e9e65e00f
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-03-03 17:07:28 +0100

    prometheus: add snmp_exporter module and profile
    The SNMP exporter provides an HTTP endpoint to poll SNMP devices and
    transform the answers into Prometheus metrics.
    
    The transformation is driven by 'modules' instructing the exporter which OIDs
    to poll and how to construct metrics from received OIDs.
    
    Additional modules can be added with prometheus::snmp_exporter::module and
    queried via HTTP by using 'module=<name>' on the query string. Under the hood
    each module is generated by a template into a yaml file (a dictionary) and
    all fragments are assembled into the final exporter configuration (i.e. a
    concatenation of all fragments/dictionaries).
    
    When deployed, netmon1001 will run the only copy of snmp_exporter since the
    configuration also contains the SNMP communities. Prometheus servers in codfw
    and eqiad will poll netmon1001 for their respective PDUs. To keep all polling
    local to the site it is possible to deploy netmon in codfw too though.
    
    Bug: T148541
    Change-Id: I2e3e07535745d1f317377ad6dd56ccb0540fa602

---
+++ modules/prometheus/manifests/snmp_exporter.pp
@@ -0,0 +1,54 @@
+# == Class: prometheus::snmp_exporter
+#
+# The SNMP exporter provides an HTTP endpoint to poll SNMP devices
+# and transform the result into Prometheus metrics.
+#
+# The transformation is driven by 'modules' instructing snmp_exporter which
+# OIDs to poll and how to construct metrics from received OIDs.
+#
+# Additional modules can be added with prometheus::snmp_exporter::module and
+# queried via HTTP by using 'module=<name>' on the query string.
+
+class prometheus::snmp_exporter {
+    require_package(['prometheus-snmp-exporter', 'python3', 'python3-yaml'])
+
+    prometheus::snmp_exporter::module { 'default':
+        template => 'default',
+    }
+
+    base::service_unit { 'prometheus-snmp-exporter':
+        ensure    => present,
+        refresh   => true,
+        strict    => false,
+        require   => Package['prometheus-snmp-exporter'],
+        subscribe => Exec['prometheus-snmp-exporter-config'],
+    }
+
+    file { '/etc/prometheus/snmp.yml.d':
+        ensure => directory,
+        mode   => '0500',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    file { '/etc/prometheus/snmp.yml':
+        ensure => present,
+        mode   => '0400',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    file { '/usr/local/bin/prometheus-snmp-exporter-config':
+        ensure => present,
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/prometheus/usr/local/bin/prometheus-snmp-exporter-config',
+    }
+
+    exec { 'prometheus-snmp-exporter-config':
+        refreshonly => true,
+        command     => '/usr/local/bin/prometheus-snmp-exporter-config',
+        require     => File['/usr/local/bin/prometheus-snmp-exporter-config'],
+    }
+}
