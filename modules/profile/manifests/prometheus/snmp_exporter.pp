fa2c50de33cd51dd5918009bcfe1ffc40a187b30
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-03-03 17:07:28 +0100

    prometheus: add snmp_exporter module and profile
    The SNMP exporter provides an HTTP endpoint to poll SNMP devices and transform
    the answers into Prometheus metrics.
    
    The transformation is driven by 'modules' instructing the exporter which
    OIDs to poll and how to construct metrics from received OIDs.
    
    Additional modules can be added with prometheus::snmp_exporter::module and
    queried via HTTP by using 'module=<name>' on the query string. Under the hood
    each module is generated by a template into a yaml file (a dictionary) and all
    fragments are assembled into the final exporter configuration (i.e. a
    concatenation of all fragments/dictionaries).
    
    When deployed, netmon1001 will run the only copy of snmp_exporter since the
    configuration also contains the SNMP communities. Prometheus servers in codfw
    and eqiad will poll netmon1001 for their respective PDUs. To keep all polling
    local to the site it is possible to deploy netmon in codfw too though.
    
    Bug: T148541
    Change-Id: I2e3e07535745d1f317377ad6dd56ccb0540fa602

---
+++ modules/profile/manifests/prometheus/snmp_exporter.pp
@@ -0,0 +1,30 @@
+class profile::prometheus::snmp_exporter (
+    $prometheus_nodes = hiera('prometheus_nodes'),
+) {
+    include passwords::network
+
+    class { '::prometheus::snmp_exporter': }
+
+    prometheus::snmp_exporter::module { 'pdu_codfw':
+        template  => 'servertech_sentry3',
+        community => $passwords::network::snmp_ro_community_pdus_codfw,
+    }
+
+    prometheus::snmp_exporter::module { 'pdu_eqiad':
+        template  => 'servertech_sentry3',
+        community => $passwords::network::snmp_ro_community,
+    }
+
+    if $::realm == 'labs' {
+        $ferm_srange = '$LABS_NETWORKS'
+    } else {
+        $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
+        $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
+    }
+
+    ferm::service { 'prometheus-snmp-exporter':
+        proto  => 'tcp',
+        port   => '9116',
+        srange => $ferm_srange,
+    }
+}
