d4f8a0ab9c8355fcf79b35f01194d05b9062ebe4
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-03-03 17:07:28 +0100

    [WIP] prometheus: add snmp_exporter module and role
    Bug: T148541
    Change-Id: I2e3e07535745d1f317377ad6dd56ccb0540fa602

---
+++ modules/prometheus/manifests/snmp_exporter/module.pp
@@ -0,0 +1,25 @@
+# === Parameters
+#
+# [*$title*]
+#  The module name snmp_exporter should use, it needs to be passed on the url.
+#
+# [*$template*]
+#  Which template to use for this module.
+#
+# [*$community*]
+#  If defined, the SNMPv2c community to use.
+
+define prometheus::snmp_exporter::module (
+  $template,
+  $community = undef,
+  $ensure = present,
+) {
+    $exporter_module = $title
+
+    # prometheus-snmp-exporter-config will take care of assembling all
+    # modules yaml files into snmp.yml for snmp_exporter to consume.
+    file { "/etc/prometheus/snmp.yml.d/${exporter_module}.yml":
+        contents => template("prometheus/snmp_exporter/${template}.yml.erb"),
+        notify   => Exec['prometheus-snmp-exporter-config'],
+    }
+}
