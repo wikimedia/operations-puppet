55b0a18a1a5358597f162f5556637d7a2d9bb7bf
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-03 14:09:55 +0100

    realm: remove graphoid_site
    Change-Id: I2875e7878ff7d7ba74edd225e190957d6a91eac4

--- manifests/realm.pp
+++ manifests/realm.pp
@@ -74,7 +74,6 @@ $app_routes = hiera('discovery::app_routes')
 $mw_primary = $app_routes['mediawiki']
 $rb_site = $app_routes['restbase']
 $mbapps_site = $app_routes['mobileapps']
-$graphoid_site = $app_routes['graphoid']
 $mathoid_site = $app_routes['mathoid']
 $aqs_site = $app_routes['aqs']
 
