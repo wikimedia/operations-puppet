50055dc87d2f6270409691ef5111f2188e9a04b3
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-03 14:14:45 +0100

    realm: get rid of more entries
    Change-Id: I7b5283771e67dbdd8dc481d94a1a77a0bad272ab

--- manifests/realm.pp
+++ manifests/realm.pp
@@ -73,8 +73,6 @@ $app_routes = hiera('discovery::app_routes')
 # Shortcut variables to use e.g. in hiera
 $mw_primary = $app_routes['mediawiki']
 $rb_site = $app_routes['restbase']
-$mbapps_site = $app_routes['mobileapps']
-$mathoid_site = $app_routes['mathoid']
 $aqs_site = $app_routes['aqs']
 
 $network_zone = $main_ipaddress ? {
