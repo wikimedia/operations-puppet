09413d13464abcfcfaedef897e9580b756c65d01
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-03-03 17:07:28 +0100

    prometheus: add snmp_exporter module and role
    The SNMP exporter provides an HTTP endpoint to poll SNMP devices and transform
    the answers into Prometheus metrics.
    
    The transformation is driven by 'modules' instructing the exporter which
    OIDs to poll and how to construct metrics from received OIDs.
    
    Additional modules can be added with prometheus::snmp_exporter::module and
    queried via HTTP by using 'module=<name>' on the query string. Under the hood
    each module is generated by a template into a yaml file (a dictionary) and all
    fragments are assembled into the final exporter configuration (i.e. a
    concatenation of all fragments/dictionaries).
    
    When deployed, netmon1001 will run the only copy of snmp_exporter since the
    configuration also contains the SNMP communities. Prometheus servers in codfw
    and eqiad will poll netmon1001 for their respective PDUs. To keep all polling
    local to the site it is possible to deploy netmon in codfw too though.
    
    Bug: T148541
    Change-Id: I2e3e07535745d1f317377ad6dd56ccb0540fa602

--- manifests/site.pp
+++ manifests/site.pp
@@ -2266,7 +2266,8 @@ node 'nescio.wikimedia.org' {
 
 # network monitoring tool server
 node 'netmon1001.wikimedia.org' {
-    role(rancid::server, librenms, servermon::wmf, torrus, smokeping)
+    role(rancid::server, librenms, servermon::wmf, torrus, smokeping,
+      prometheus::snmp_exporter)
     include ::standard
     include ::passwords::network
     include ::base::firewall
