d9b2b01f50e0005315c3e17999a430d7f1900fdb
Michael Holloway <mholloway@wikimedia.org>
2017-03-05 15:01:37 -0500

    [Android] Create symlink to repo licenses dir in the SDK on CI
    Gradle should automatically download missing SDK packages when a build is
    run, but only provided the relevant licenses are accepted.  We can avoid
    the need for manual intervention for license acceptance in our CI
    environment by keeping an up-to-date copy of a /licenses directory with
    the latest licenses hashes in our code repo, that we can then symlink to
    from the SDK directory on our CI machines.[1]
    
    The licenses directory was added to the code repo in
    I1cad5f62404096d59ad3b37672c2405730b97226.
    
    [1] https://code.google.com/p/android/issues/detail?id=212128#c21
    
    Bug: T147099
    Bug: T158456
    Change-Id: I57007dfb55f88eee07559de6c7b34fe4ec900787

--- modules/role/manifests/ci/slave/android.pp
+++ modules/role/manifests/ci/slave/android.pp
@@ -17,9 +17,17 @@ class role::ci::slave::android {
     include contint::packages::androidsdk
     include contint::packages::java
 
+    $repo_license_dir = '/srv/jenkins-workspace/workspace/apps-android-wikipedia-periodic-test/licenses'
+    $sdk_license_dir = '/srv/jenkins-workspace/tools/android-sdk/licenses'
+
     exec {'jenkins-deploy kvm membership':
         unless  => "/bin/grep -q 'kvm\\S*jenkins-deploy' /etc/group",
         command => '/usr/sbin/usermod -aG kvm jenkins-deploy',
     }
 
+    exec {'create link to app repo license directory':
+        command => "/bin/ln -s ${repo_license_dir} ${sdk_license_dir}",
+        onlyif  => "/usr/bin/test -e ${repo_license_dir}",
+        creates => $sdk_license_dir,
+    }
 }
