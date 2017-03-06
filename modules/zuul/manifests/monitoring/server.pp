62dd0f22712ad15422ebf1ec18055b723490b8bc
Antoine Musso <hashar@free.fr>
2017-03-06 22:00:18 +0100

    Change Zuul Gearman alarm to a simple threshold
    Our check anomaly plugin for Holt-Winters doesn't quite do what I
    thought.  Apparently the delta=5 is too large to find an issue.
    
    Switch to the simpler threshold check with arbitrary values for a start:
    
    Grab 15 data points (minutes)
    Warning on 30 of them being over 90 jobs and critical for 140.
    
    That should be fine from a quick look at the history for the metric on:
    https://grafana.wikimedia.org/dashboard/db/zuul-gearman
    
    Test command:
    ./modules/nagios_common/files/check_commands/check_graphite -U
    https://graphite.wikimedia.org check_threshold zuul.geard.queue.waiting
    --from 15min --warning=90 --critical=140 --perc 30
    
    Bug: T70113
    Change-Id: I666402b06761cc6b225a329ea9d0d53e0f9e9d59

--- modules/zuul/manifests/monitoring/server.pp
+++ modules/zuul/manifests/monitoring/server.pp
@@ -29,16 +29,14 @@ class zuul::monitoring::server (
         nrpe_command  => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 4730 --timeout=2',
     }
 
-    monitoring::graphite_anomaly { 'zuul_gearman_wait_queue':
+    monitoring::graphite_threshold{ 'zuul_gearman_wait_queue':
         ensure        => $ensure,
         description   => 'Work requests waiting in Zuul Gearman server https://grafana.wikimedia.org/dashboard/db/zuul-gearman',
         metric        => 'zuul.geard.queue.waiting',
-        check_window  => 30,  # HoltWinters window in minutes
-        # Alarms when metric is above the HoltWinters upper confidence band
-        warning       => 5,
-        critical      => 10,
-        # Only alarms when the metric goes above the upper confident band
-        over          => true,
         contact_group => 'contint',
+        from          => '15min',
+        percentage    => 30,
+        warning       => 90,
+        criticial     => 140,
     }
 }
