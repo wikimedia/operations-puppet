77039bffa2c2856e75e143dcdac2d3b3a797e30b
Antoine Musso <hashar@free.fr>
2017-03-07 21:37:44 +0100

    authdns: basic spec
    Change-Id: I66797e9cf2747932bc1deedce9bd1926ba124a6d

---
+++ modules/authdns/spec/classes/authdns_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'authdns' do
+    let(:node) { 'testhost.eqiad.wmnet' }
+    let(:params) { {
+        :lvs_services => {},
+        :discovery_services => {},
+    } }
+    let(:pre_condition) { [
+        'define git::clone($directory, $origin, $branch,$owner,$group) {}',
+        'define monitoring::service($description,$check_command) {}',
+        'define ssh::userkey($content) {}',
+        'define sudo::user($privileges) {}',
+        'class confd($prefix) {}',
+        'package{ "git": }',
+    ] }
+    it { should compile }
+end
+
+describe 'authdns::lint' do
+    it { should compile }
+end
