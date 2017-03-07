542a84f7b4cba55487eefe04e58e7a98fd82d4f7
Chad Horohoe <chadh@wikimedia.org>
2017-03-07 12:03:45 -0800

    Gerrit: Remove reviewer counts cron, nobody is using it
    Literally: no requests in last 30 days
    
    Change-Id: Ie3439b9873deb4f2531258f34955e64ce2ff73d9

--- modules/gerrit/manifests/crons.pp
+++ modules/gerrit/manifests/crons.pp
@@ -6,20 +6,4 @@ class gerrit::crons() {
         user    => 'root',
         minute  => [0, 15, 30, 45],
     }
-
-    # This is useful information about the distribution of reviewers.
-    # Gerrit's rest api doesn't provide an easy way to get this data.
-    file { '/var/www/reviewer-counts.json':
-        ensure => 'present',
-        owner  => 'gerrit2',
-        group  => 'root',
-        mode   => '0644',
-    }
-
-    cron { 'list_reviewer_counts':
-        command => "/usr/bin/java -jar /var/lib/gerrit2/review_site/bin/gerrit.war gsql -d /var/lib/gerrit2/review_site/ --format JSON_SINGLE -c 'SELECT changes.change_id AS change_id, COUNT(DISTINCT patch_set_approvals.account_id) AS reviewer_count FROM changes LEFT JOIN patch_set_approvals ON (changes.change_id = patch_set_approvals.change_id) GROUP BY changes.change_id' > /var/www/reviewer-counts.json",
-        user    => 'gerrit2',
-        hour    => 1,
-        require => File['/var/www/reviewer-counts.json'],
-    }
 }
