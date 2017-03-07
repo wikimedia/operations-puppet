320e2342d4d1259243fefbf7e21eac41c89e8f9f
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-03-07 15:11:34 +0100

    facilities: add row and site parameters for pdus
    Augment PDU information with site and row information. This is a
    stepping stone to help generating targets for Prometheus to pick up.
    
    Bug: T148541
    Change-Id: I7f093fbb1d8dfe65050f2d1a2c28713baf790e89

--- modules/facilities/manifests/init.pp
+++ modules/facilities/manifests/init.pp
@@ -12,103 +12,166 @@ class facilities {
     # eqiad
     # A
     facilities::monitor_pdu_3phase { 'ps1-a1-eqiad':
-        ip => '10.65.0.32',
+        ip   => '10.65.0.32',
+        row  => 'a',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-a2-eqiad':
-        ip => '10.65.0.33',
+        ip   => '10.65.0.33',
+        row  => 'a',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-a3-eqiad':
-        ip => '10.65.0.34',
+        ip   => '10.65.0.34',
+        row  => 'a',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-a4-eqiad':
-        ip => '10.65.0.35',
+        ip   => '10.65.0.35',
+        row  => 'a',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-a5-eqiad':
-        ip => '10.65.0.36',
+        ip   => '10.65.0.36',
+        row  => 'a',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-a6-eqiad':
-        ip => '10.65.0.37',
+        ip   => '10.65.0.37',
+        row  => 'a',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-a7-eqiad':
-        ip => '10.65.0.38',
+        ip   => '10.65.0.38',
+        row  => 'a',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-a8-eqiad':
-        ip => '10.65.0.39',
+        ip   => '10.65.0.39',
+        row  => 'a',
+        site => 'eqiad',
     }
     # B
     facilities::monitor_pdu_3phase { 'ps1-b1-eqiad':
-        ip => '10.65.0.40',
+        ip   => '10.65.0.40',
+        row  => 'b',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-b2-eqiad':
-        ip => '10.65.0.41',
+        ip   => '10.65.0.41',
+        row  => 'b',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-b3-eqiad':
-        ip => '10.65.0.42',
+        ip   => '10.65.0.42',
+        row  => 'b',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-b4-eqiad':
-        ip => '10.65.0.43',
+        ip   => '10.65.0.43',
+        row  => 'b',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-b5-eqiad':
-        ip => '10.65.0.44',
+        ip   => '10.65.0.44',
+        row  => 'b',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-b6-eqiad':
-        ip => '10.65.0.45',
+        ip   => '10.65.0.45',
+        row  => 'b',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-b7-eqiad':
-        ip => '10.65.0.46',
+        ip   => '10.65.0.46',
+        row  => 'b',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-b8-eqiad':
-        ip => '10.65.0.47',
+        ip   => '10.65.0.47',
+        row  => 'b',
+        site => 'eqiad',
     }
     # C
     facilities::monitor_pdu_3phase { 'ps1-c1-eqiad':
-        ip => '10.65.0.48',
+        ip   => '10.65.0.48',
+        row  => 'c',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-c2-eqiad':
-        ip => '10.65.0.49',
+        ip   => '10.65.0.49',
+        row  => 'c',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-c3-eqiad':
-        ip => '10.65.0.50',
+        ip   => '10.65.0.50',
+        row  => 'c',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-c4-eqiad':
-        ip => '10.65.0.51',
+        ip   => '10.65.0.51',
+        row  => 'c',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-c5-eqiad':
-        ip => '10.65.0.52',
+        ip   => '10.65.0.52',
+        row  => 'c',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-c6-eqiad':
-        ip => '10.65.0.53',
+        ip   => '10.65.0.53',
+        row  => 'c',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-c7-eqiad':
-        ip => '10.65.0.54',
+        ip   => '10.65.0.54',
+        row  => 'c',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-c8-eqiad':
-        ip => '10.65.0.55',
+        ip   => '10.65.0.55',
+        row  => 'c',
+        site => 'eqiad',
     }
     # D
     facilities::monitor_pdu_3phase { 'ps1-d1-eqiad':
-        ip => '10.65.0.56',
+        ip   => '10.65.0.56',
+        row  => 'd',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-d2-eqiad':
-        ip => '10.65.0.57',
+        ip   => '10.65.0.57',
+        row  => 'd',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-d3-eqiad':
-        ip => '10.65.0.58',
+        ip   => '10.65.0.58',
+        row  => 'd',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-d4-eqiad':
-        ip => '10.65.0.59',
+        ip   => '10.65.0.59',
+        row  => 'd',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-d5-eqiad':
-        ip => '10.65.0.60',
+        ip   => '10.65.0.60',
+        row  => 'd',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-d6-eqiad':
-        ip => '10.65.0.61',
+        ip   => '10.65.0.61',
+        row  => 'd',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-d7-eqiad':
-        ip => '10.65.0.62',
+        ip   => '10.65.0.62',
+        row  => 'd',
+        site => 'eqiad',
     }
     facilities::monitor_pdu_3phase { 'ps1-d8-eqiad':
-        ip => '10.65.0.63',
+        ip   => '10.65.0.63',
+        row  => 'd',
+        site => 'eqiad',
     }
 }
-
