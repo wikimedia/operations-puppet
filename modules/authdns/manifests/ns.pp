c3d694d9478bb733f760a557b1d5bad218b66710
Brandon Black <bblack@wikimedia.org>
2017-03-07 16:20:18 +0000

    authdns lint support for full puppetized config
    Bug: T156100
    Change-Id: Iae9d35fcbcd46e11940d0040b13143d65df25d77

---
+++ modules/authdns/manifests/ns.pp
@@ -0,0 +1,31 @@
+# == Class authdns::ns
+# A class to implement Wikimedia's authoritative DNS servers
+#
+class authdns::ns(
+    $nameservers = [ $::fqdn ],
+    $gitrepo = undef,
+    $monitoring = true,
+    $conftool_prefix = hiera('conftool_prefix'),
+    $lvs_services,
+    $discovery_services,
+) {
+    require ::geoip::data::puppet
+
+    class { 'authdns':
+        nameservers => $nameservers,
+        gitrepo     => $gitrepo,
+        config_dir  => '/etc/gdnsd',
+        clone_data  => true,
+        run_service => true,
+    }
+
+    if $monitoring {
+        include ::authdns::monitoring
+    }
+
+    # confd statefile templating for discovery
+    class { 'confd':
+        prefix => $conftool_prefix,
+    }
+    create_resources(::authdns::discovery_statefile, $discovery_services, { lvs_services => $lvs_services })
+}
