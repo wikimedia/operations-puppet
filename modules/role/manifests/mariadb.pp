5897b00c4076c4c6064ca9eb05e9d30a390361d6
Jaime Crespo <jcrespo@wikimedia.org>
2017-03-07 17:57:30 +0100

    mariadb: Decouple beta role to a separate file
    Bug: T150850
    Change-Id: I4011cbf53078beb3b3298e420239fcf06c427a78

--- modules/role/manifests/mariadb.pp
+++ modules/role/manifests/mariadb.pp
@@ -359,51 +359,6 @@ class role::mariadb::misc::eventlogging(
     }
 }
 
-# Beta Cluster Master
-# Should add separate role for slaves
-#
-# filtertags: labs-project-deployment-prep
-class role::mariadb::beta {
-
-    system::role { 'role::mariadb::beta':
-        description => 'beta cluster database server',
-    }
-
-    include ::standard
-    include mariadb::packages_wmf
-    include passwords::misc::scripts
-
-    # This is essentially the same volume created by role::labs::lvm::srv but
-    # ensures it will be created before mariadb is installed and leaves some
-    # LVM extents free for a possible second volume for the tmpdir.
-    # (see T117446)
-    labs_lvm::volume { 'second-local-disk':
-        mountat => '/srv',
-        size    => '80%FREE',
-        before  => Class['mariadb::packages_wmf'],
-    }
-
-    class { 'mariadb::config':
-        config  => 'role/mariadb/mysqld_config/beta.my.cnf.erb',
-    }
-
-    class { 'mariadb::service':
-        ensure  => 'running',
-        manage  => true,
-        enable  => true,
-        require => Class['mariadb::config'],
-    }
-
-    $password = $passwords::misc::scripts::mysql_beta_root_pass
-    $prompt = 'BETA'
-    file { '/root/.my.cnf':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0400',
-        content => template('mariadb/root.my.cnf.erb'),
-    }
-}
-
 # tendril.wikimedia.org db
 class role::mariadb::tendril {
 
