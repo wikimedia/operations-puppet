8dfb387079789144572b506d8b3fcdf24dc9e1df
Antoine Musso <hashar@free.fr>
2017-03-07 21:37:44 +0100

    authdns: basic spec
    Change-Id: I66797e9cf2747932bc1deedce9bd1926ba124a6d

---
+++ modules/authdns/spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require 'rspec-puppet'
+require 'puppetlabs_spec_helper/module_spec_helper'
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
+RSpec.configure do |c|
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.hiera_config = File.join(fixture_path, 'hiera.yaml')
+  c.before(:each) do
+    # rubocop:disable Lint/UnusedBlockArgument
+    Puppet::Parser::Functions.newfunction(:secret, :type => :rvalue) { |args|
+      'secret_test_password'
+    }
+  end
+end
