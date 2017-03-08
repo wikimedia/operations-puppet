77fe59f75fef5f27a56795798aeeb9ee4d385cc4
BBlack <bblack@wikimedia.org>
2017-03-08 03:15:01 +0000

    Revert "authdns lint support for full puppetized config"
    This reverts commit d79dc312355c67ec42021f1081245d3b8377b6e5.
    
    Change-Id: I1cdf890d343dca8dd9874a3330af937a16fe548a

--- modules/authdns/manifests/init.pp
+++ modules/authdns/manifests/init.pp
@@ -1,152 +1,136 @@
 # == Class authdns
-# Base authdns setup shared by authdns::ns and authdns::lint
-#
-# $nameservers - Array of FQDN of nameservers participating in
-#     "authdns-update" scheme for syncing zone data.  If left un-set, will
-#     default to only the local machine's FQDN, creating what is effectively
-#     an isolated test server.
-# $gitrepo - If defined, this is the repo to pull DNS zone data from, and
-#     this code assumes setup of a live gdnsd server and ensures config is
-#     templated and git data is pulled into place before the package is
-#     installed to avoid potentially issuing REFUSED for unknown zones, etc.
-#     If undefined (the default), the gdnsd OS-level service is
-#     disabled/stopped, and no data syncing for zone data is set up at all.
-# $config_dir - default "/etc/gdnsd" - place where config files deployed.
+# A class to implement Wikimedia's authoritative DNS system
 #
 class authdns(
     $nameservers = [ $::fqdn ],
     $gitrepo = undef,
-    $config_dir='/etc/gdnsd',
+    $monitoring = true,
+    $conftool_prefix = hiera('conftool_prefix'),
+    $lvs_services,
+    $discovery_services,
 ) {
+    require ::authdns::account
     require ::authdns::scripts
-
-    if $gitrepo {
-        require ::geoip::data::puppet
-        $svc_ensure = 'running'
-        $svc_enable = true
-    }
-    else {
-        include ::geoip
-        $svc_ensure = 'stopped'
-        $svc_enable = false
-    }
+    require ::geoip::data::puppet
 
     package { 'gdnsd':
         ensure => installed,
     }
 
     service { 'gdnsd':
-        ensure     => $svc_ensure,
-        enable     => $svc_enable,
+        ensure     => 'running',
         hasrestart => true,
         hasstatus  => true,
         require    => Package['gdnsd'],
     }
 
-    file { $config_dir:
+    # the package creates this, but we want to set up the config before we
+    # install the package, so that the daemon starts up with a well-known
+    # config that leaves no window where it'd refuse to answer properly
+    file { '/etc/gdnsd':
         ensure => 'directory',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
     }
-
-    file { "${config_dir}/config":
+    file { '/etc/gdnsd/config':
         ensure  => 'present',
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
         content => template("${module_name}/config.erb"),
-        require => File[$config_dir],
+        require => File['/etc/gdnsd'],
         notify  => Service['gdnsd'],
     }
-
-    file { "${config_dir}/discovery-geo-resources":
-        ensure  => 'present',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        content => template("${module_name}/discovery-geo-resources.erb"),
-        require => File[$config_dir],
-        notify  => Service['gdnsd'],
-    }
-
-    file { "${config_dir}/discovery-metafo-resources":
-        ensure  => 'present',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        content => template("${module_name}/discovery-metafo-resources.erb"),
-        require => File[$config_dir],
-        notify  => Service['gdnsd'],
-    }
-
-    file { "${config_dir}/discovery-states":
-        ensure  => 'present',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        content => template("${module_name}/discovery-states.erb"),
-        require => File[$config_dir],
-        notify  => Service['gdnsd'],
-    }
-
-    file { "${config_dir}/discovery-map":
-        ensure  => 'present',
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        source  => "puppet:///modules/${module_name}/discovery-map",
-        require => File[$config_dir],
-        notify  => Service['gdnsd'],
-    }
-
-    file { "${config_dir}/zones":
+    file { '/etc/gdnsd/zones':
         ensure => 'directory',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
     }
 
-    if $gitrepo {
-        require ::authdns::account
+    $workingdir = '/srv/authdns/git' # export to template
 
-        $workingdir = '/srv/authdns/git' # export to template
+    file { '/etc/wikimedia-authdns.conf':
+        ensure  => 'present',
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => template("${module_name}/wikimedia-authdns.conf.erb"),
+    }
 
-        file { '/etc/wikimedia-authdns.conf':
-            ensure  => 'present',
-            mode    => '0444',
-            owner   => 'root',
-            group   => 'root',
-            content => template("${module_name}/wikimedia-authdns.conf.erb"),
-        }
+    # do the initial clone via puppet
+    git::clone { $workingdir:
+        directory => $workingdir,
+        origin    => $gitrepo,
+        branch    => 'master',
+        owner     => 'authdns',
+        group     => 'authdns',
+        notify    => Exec['authdns-local-update'],
+    }
 
-        # do the initial clone via puppet
-        git::clone { $workingdir:
-            directory => $workingdir,
-            origin    => $gitrepo,
-            branch    => 'master',
-            owner     => 'authdns',
-            group     => 'authdns',
-            notify    => Exec['authdns-local-update'],
-        }
-
-        # we prepare the config even before the package gets installed, leaving
-        # no window where service would be started and answer with REFUSED
-        exec { 'authdns-local-update':
-            command     => '/usr/local/sbin/authdns-local-update --skip-review',
-            user        => root,
-            refreshonly => true,
-            timeout     => 60,
-            before      => Package['gdnsd'],
-            require     => [
+    exec { 'authdns-local-update':
+        command     => '/usr/local/sbin/authdns-local-update --skip-review',
+        user        => root,
+        refreshonly => true,
+        timeout     => 60,
+        require     => [
                 File['/etc/wikimedia-authdns.conf'],
-                File["${config_dir}/config"],
-                File["${config_dir}/discovery-geo-resources"],
-                File["${config_dir}/discovery-metafo-resources"],
-                File["${config_dir}/discovery-states"],
-                File["${config_dir}/discovery-map"],
+                File['/etc/gdnsd/config'],
                 Git::Clone['/srv/authdns/git'],
             ],
-        }
+        # we prepare the config even before the package gets installed, leaving
+        # no window where service would be started and answer with REFUSED
+        before      => Package['gdnsd'],
     }
+
+    if $monitoring {
+        include ::authdns::monitoring
+    }
+
+    # Discovery Magic
+
+    file { '/etc/gdnsd/discovery-geo-resources':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template("${module_name}/discovery-geo-resources.erb"),
+        require => File['/etc/gdnsd'],
+        notify  => Service['gdnsd'],
+    }
+
+    file { '/etc/gdnsd/discovery-metafo-resources':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template("${module_name}/discovery-metafo-resources.erb"),
+        require => File['/etc/gdnsd'],
+        notify  => Service['gdnsd'],
+    }
+
+    file { '/etc/gdnsd/discovery-states':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template("${module_name}/discovery-states.erb"),
+        require => File['/etc/gdnsd'],
+        notify  => Service['gdnsd'],
+    }
+
+    file { '/etc/gdnsd/discovery-map':
+        ensure => 'present',
+        mode   => '0444',
+        owner  => 'root',
+        group  => 'root',
+        source => "puppet:///modules/${module_name}/discovery-map",
+    }
+
+    class { 'confd':
+        prefix => $conftool_prefix,
+    }
+
+    create_resources(::authdns::discovery_statefile, $discovery_services, { lvs_services => $lvs_services })
 }
