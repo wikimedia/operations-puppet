70b8138874ca00d3cf9c4a3fb05824cf2db47c7b
Guillaume Lederrey <glederrey@wikimedia.org>
2017-03-08 12:48:10 +0100

    WIP - logrotate - introduce a generic logrotate template
    We seem to have a lot of logrotate templates, most of which are fairly
    similar. We already have a logrotate module with a very basic defined
    type which manages the creation of the logrotate configuration files but
    not their content. It seems natural to extend this defined type to provide
    a default template.
    
    The approach taken here is to provide a set of parameters to configure
    logrotate, but not to provide an open ended hash of parameters. This makes
    the parameters more discoverable and ensures some validity checking.
    
    Change-Id: Ic7b09086397d8b86fbcde3ceee1fa762c904b7f1

--- modules/logrotate/manifests/conf.pp
+++ modules/logrotate/manifests/conf.pp
@@ -8,7 +8,25 @@ define logrotate::conf (
     $ensure = present,
     $source = undef,
     $content = undef,
+    $file_pattern = undef,
+    $not_if_empty = false,
+    $max_age = undef,
+    $rotate = undef,
+    $date_ext = false,
+    $compress = false,
+    $delay_compress = false,
+    $missing_ok = false,
+    $size = undef,
 ) {
+
+    if $source or $content {
+        $real_content = $content
+    } else {
+        if $file_pattern == undef {
+            fail('$file_pattern needs to be defined when using default template')
+        }
+        $real_content = template('logrotate/logrotate.erb')
+    }
 
     file { "/etc/logrotate.d/${title}":
         ensure  => $ensure,
@@ -16,6 +34,6 @@ define logrotate::conf (
         group   => 'root',
         mode    => '0444',
         source  => $source,
-        content => $content,
+        content => $real_content,
     }
 }
