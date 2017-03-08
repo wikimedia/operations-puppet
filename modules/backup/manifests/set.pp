784fd0ae07bbcd77f2b6e3798e0b0828de66e781
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-03-08 19:08:45 +0200

    backup::set: Allow passing an $extras config hash
    Allow backup::set to accept a config Hash that is pass directly to
    bacula::client::job. That does expose a bit of bacula's Job Resource
    configuration to the caller but it allows for more flexiblity. Default
    to under to maintain compatibility. Amend openldapset to use the
    backup::set now instead
    
    Change-Id: I200662d81df7a733e7a4a63ffb3d146212dab51b

--- modules/backup/manifests/set.pp
+++ modules/backup/manifests/set.pp
@@ -1,10 +1,11 @@
 # backup::set is probably what you want.
-define backup::set {
+define backup::set($extras=undef) {
     if defined(Class['profile::backup::host']) {
         $jobdefaults=$profile::backup::host::jobdefaults
         @bacula::client::job { "${name}-${jobdefaults}":
             fileset     => $name,
             jobdefaults => $jobdefaults,
+            extras      => $extras,
         }
 
         $motd_content = "#!/bin/sh\necho \"Backed up on this host: ${name}\""
