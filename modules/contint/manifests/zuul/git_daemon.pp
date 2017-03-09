322b493adf287275eefbd5ed324ac3bfe7d839c3
Antoine Musso <hashar@free.fr>
2017-03-09 23:27:56 +0100

    (WIP) contint: migrate git-daemon to systemd
    Bug: T157785
    Change-Id: Ic23155f6900babba99f9333a797d9bdae31ba6ec

--- modules/contint/manifests/zuul/git_daemon.pp
+++ modules/contint/manifests/zuul/git_daemon.pp
@@ -2,32 +2,32 @@
 class contint::zuul::git_daemon(
     $zuul_git_dir = '/var/lib/zuul/git'
 ) {
-
-  package { 'git-daemon-sysvinit': ensure => present }
-
   # Point both git daemon paths to the same dir, this way we do not have a
   # /git/ prefix in the git:// URLs.
-  $git_daemon_directory = $zuul_git_dir
-  $git_daemon_base_path = $zuul_git_dir
+  $directory = $zuul_git_dir
+  $base_path = $zuul_git_dir
 
+  # Additional options passed to the Daemon.
+  #
   # We dont want to honor `git send-pack` commands so make sure the receive-pack
   # service is always disabled.
-  $git_daemon_options = '--export-all --forbid-override=receive-pack'
+  $daemon_options = '--export-all --forbid-override=receive-pack'
 
-  file { '/etc/default/git-daemon':
-    mode    => '0444',
-    owner   => 'root',
-    group   => 'root',
-    content => template('contint/default.git-daemon.erb'),
-    require => Package['git-daemon-sysvinit'],
+  user { 'gitdaemon':
+    system =>  true,
   }
 
-  service { 'git-daemon':
-    ensure     => 'running',
-    enable     => true,
-    hasrestart => true,
-    subscribe  => File['/etc/default/git-daemon'],
-    require    => Package['git-daemon-sysvinit'],
+  systemd::syslog { 'git-daemon':
+      base_dir    => '/var/lib/log',
+      owner       => 'gitdaemon',
+      group       => 'gitdaemon',
+      readable_by => 'all',
+  }
+
+  base::service_unit { 'git-daemon':
+    ensure  => present,
+    systemd => true,
+    require =>  User['gitdaemon'],
   }
 
 }
