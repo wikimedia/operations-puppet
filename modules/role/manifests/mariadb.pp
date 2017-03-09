0bcf0d050b95914dd2db31f19a00e5db18b1b0f6
Jaime Crespo <jcrespo@wikimedia.org>
2017-03-09 18:26:18 +0100

    mariadb: Decouple tendril mariadb role to a separate file
    Bug: T150850
    Change-Id: I64ac021c4f833cf344b2c3f5d56ace6f6fd067be

--- modules/role/manifests/mariadb.pp
+++ modules/role/manifests/mariadb.pp
@@ -203,34 +203,6 @@ class role::mariadb::groups(
     }
 }
 
-# tendril.wikimedia.org db
-class role::mariadb::tendril {
-
-    system::role { 'role::mariadb::tendril':
-        description => 'tendril database server',
-    }
-
-    include mariadb::packages_wmf
-    include mariadb::service
-
-    include ::standard
-    include role::mariadb::monitor::dba
-    include passwords::misc::scripts
-    include role::mariadb::ferm
-
-    class {'role::mariadb::groups':
-        mysql_group => 'tendril',
-        mysql_role  => 'standalone',
-    }
-
-    class { 'mariadb::config':
-        config  => 'role/mariadb/mysqld_config/tendril.my.cnf.erb',
-        datadir => '/srv/sqldata',
-        tmpdir  => '/srv/tmp',
-        ssl     => 'puppet-cert',
-    }
-}
-
 # MariaDB 10 Analytics all-shards slave, with scratch space and TokuDB
 # analytics slaves are already either dbstores or eventlogging slaves
 # so they just need the extra core monitoring
