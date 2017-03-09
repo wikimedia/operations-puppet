413c1f24361f4bd7bc38ba9ac3cc7efecd3bd0e2
Jaime Crespo <jcrespo@wikimedia.org>
2017-03-09 19:00:07 +0100

    mariadb: Decouple mariadb wikitech role to a separate file
    Bug: T150850
    Change-Id: Id35a7db74eaad8ff3baea462635942f9ad3cd5ed

--- modules/role/manifests/mariadb.pp
+++ modules/role/manifests/mariadb.pp
@@ -247,49 +247,6 @@ class role::mariadb::analytics::custom_repl_slave {
     }
 }
 
-# wikitech instance (silver)
-class role::mariadb::wikitech {
-
-    system::role { 'role::mariadb::wikitech':
-        description => 'Wikitech Database',
-    }
-
-    include ::standard
-    include role::mariadb::grants::wikitech
-    include role::mariadb::monitor
-    include passwords::misc::scripts
-    class { 'role::mariadb::groups':
-        mysql_group => 'wikitech',
-        mysql_role  => 'standalone',
-    }
-
-    include mariadb::packages_wmf
-    include mariadb::service
-
-    class { 'mariadb::config':
-        config  => 'role/mariadb/mysqld_config/wikitech.my.cnf.erb',
-        datadir => '/srv/sqldata',
-        tmpdir  => '/srv/tmp',
-    }
-
-    # mysql monitoring access from tendril (db1011)
-    ferm::rule { 'mysql_tendril':
-        rule => 'saddr 10.64.0.15 proto tcp dport (3306) ACCEPT;',
-    }
-
-    # mysql from deployment master servers and terbium (T98682, T109736)
-    ferm::service { 'mysql_deployment_terbium':
-        proto  => 'tcp',
-        port   => '3306',
-        srange => '@resolve((tin.eqiad.wmnet mira.codfw.wmnet terbium.eqiad.wmnet wasat.codfw.wmnet))',
-    }
-
-    service { 'mariadb':
-        ensure  => running,
-        require => Class['mariadb::packages_wmf', 'mariadb::config'],
-    }
-}
-
 class role::mariadb::proxy(
     $shard
     ) {
