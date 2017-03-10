39f9636afca9443f98a800eb6c88a70203fd5e06
Jaime Crespo <jcrespo@wikimedia.org>
2017-03-10 11:28:59 +0100

    mariadb: Decouple maintenance mariadb role to a separate file
    Bug: T150850
    Change-Id: I5e6603dda9ac0fa42d405ff619d87d66c7caac67

--- modules/role/manifests/mariadb.pp
+++ modules/role/manifests/mariadb.pp
@@ -369,22 +369,4 @@ class role::mariadb::proxy::slaves(
     }
 }
 
-# the contents of the next 2 classes should go over to
-# db_maintenance module on puppet db-classes refactoring
-class role::mariadb::maintenance {
-    # TODO: check if both of these are still needed
-    include mysql
-    package { 'percona-toolkit':
-        ensure => latest,
-    }
-
-    # place from which tendril-related cron jobs are run
-    include passwords::tendril
-
-    class { 'tendril::maintenance':
-        tendril_host     => 'db1011.eqiad.wmnet',
-        tendril_user     => 'watchdog',
-        tendril_password => $passwords::tendril::db_pass,
-    }
-}
 # lint:endignore
