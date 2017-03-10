135f8689b6758f7f36862191bc55c0b13dd2fec8
Daniel Zahn <dzahn@wikimedia.org>
2017-03-09 19:41:27 -0800

    microsites: convert to profile/role-structure
    These are the roles for misc. static websites
    used on bromine, a ganeti VM with Apache.
    
    Convert code to profile/role-structure and
    follow the new Puppet coding style on
    
    https://wikitech.wikimedia.org/wiki/Puppet_coding#Profiles
    
    Change-Id: Id29e32021e41fbff4ac6e3dfe7618a3d09fde720

---
+++ modules/role/manifests/webserver_misc_static.pp
@@ -0,0 +1,17 @@
+# a webserver for misc. static sites
+class role::webserver_misc_static {
+
+    include ::standard
+
+    include ::apache
+    include ::apache::mod::authnz_ldap
+    include ::apache::mod::rewrite
+    include ::apache::mod::headers
+
+    include ::profile::microsites::annualreport    # https://annual.wikimedia.org
+    include ::profile::microsites::endowment       # https://endowment.wikimedia.org
+    include ::profile::microsites::releases        # https://releases.wikimedia.org
+    include ::profile::microsites::static_bugzilla # https://static-bugzilla.wikimedia.org
+    include ::profile::microsites::transparency    # https://transparency.wikimedia.org
+
+}
