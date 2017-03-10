7931a86460c945ce812c000ef31e99bd981402c2
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2017-03-10 18:31:03 +0100

    elasticsearch - move role::elasticsearch::common to a profile
    Applying the principles defined in T147718 to the elasticsearch role.
    
    Bug: T147718
    Change-Id: If8a5f9827e2780f0cff53b1ad6cf6de5e3ccf43b

--- modules/role/manifests/elasticsearch/cirrus.pp
+++ modules/role/manifests/elasticsearch/cirrus.pp
@@ -2,9 +2,9 @@
 #
 # This class sets up Elasticsearch specifically for CirrusSearch.
 #
-# filtertags: labs-project-deployment-prep labs-project-search labs-project-math
 class role::elasticsearch::cirrus {
-
-    include ::role::elasticsearch::common
-
+    include ::standard
+    include ::base::firewall
+    include ::role::lvs::realserver
+    include ::profile::elasticsearch
 }
