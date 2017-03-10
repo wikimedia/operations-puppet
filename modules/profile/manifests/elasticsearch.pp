d0511a33204aed9c42c2ea058bdeb3b7bd71db22
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2017-03-10 18:31:03 +0100

    elasticsearch - move role::elasticsearch::common to a profile
    Trying to apply the principles defined in T147718 to the elasticsearch role.
    
    Bug: T147718
    Change-Id: If8a5f9827e2780f0cff53b1ad6cf6de5e3ccf43b

---
+++ modules/profile/manifests/elasticsearch.pp
@@ -0,0 +1,113 @@
+#
+# This class configures elasticsearch
+#
+# == Parameters:
+# [*ferm_srange*]
+#   The network range that should be allowed access to elasticsearch. This
+#   needs to be customized for elasticsearch clusters serving non production
+#   traffic. The relforge cluster is an example.
+#   Default: $DOMAIN_NETWORKS
+#
+# For documentation of other parameters, see the elasticsearch class.
+#
+class profile::elasticsearch {
+    $cluster_name = hiera('profile::elasticsearch::cluster_name')
+    $ferm_srange = hiera('profile::elasticsearch::ferm_srange')
+    $cluster_hosts = hiera('profile::elasticsearch::cluster_hosts')
+    $minimum_master_nodes = hiera('profile::elasticsearch::minimum_master_nodes')
+    $heap_memory = hiera('profile::elasticsearch::heap_memory')
+    $expected_nodes = hiera('profile::elasticsearch::expected_nodes')
+    $graylog_hosts = hiera('profile::elasticsearch::graylog_hosts')
+    $master_eligible = hiera('profile::elasticsearch::master_eligible')
+    $rack = hiera('profile::elasticsearch::rack', undef) # FIXME: default to undef does not work as expected
+    $row = hiera('profile::elasticsearch::row', undef)  # FIXME: default to undef does not work as expected
+    $awareness_attributes = hiera('profile::elasticsearch::awareness_attributes', undef)
+    $bulk_thread_pool_executors = hiera('profile::elasticsearch::bulk_thread_pool_executors', 6)
+    $certificate_name = hiera('profile::elasticsearch::certificate_name', $::fqdn)
+    $recover_after_time = hiera('profile::elasticsearch::recover_after_time', '1s')
+    $recover_after_nodes = hiera('profile::elasticsearch::recover_after_nodes', 1)
+
+    if ($::realm == 'production' and $row == undef) {
+        fail("Don't know row for ${::hostname} and row awareness should be turned on")
+    }
+
+    $master_eligible = $::fqdn in $unicast_hosts
+
+    ferm::service { 'elastic-http':
+        proto   => 'tcp',
+        port    => '9200',
+        notrack => true,
+        srange  => $ferm_srange,
+    }
+
+    $elastic_nodes_ferm = join($cluster_hosts, ' ')
+
+    ferm::service { 'elastic-inter-node':
+        proto   => 'tcp',
+        port    => '9300',
+        notrack => true,
+        srange  => "@resolve((${elastic_nodes_ferm}))",
+    }
+
+    system::role { 'role::elasticsearch::server':
+        ensure      => 'present',
+        description => 'elasticsearch server',
+    }
+
+    package { 'elasticsearch/plugins':
+        provider => 'trebuchet',
+    }
+    # Elasticsearch 5 doesn't allow setting the plugin path, we need
+    # to symlink it into place. The directory already exists as part of the
+    # debian package, so we need to force the creation of the symlink.
+    $plugins_dir = '/srv/deployment/elasticsearch/plugins'
+    file { '/usr/share/elasticsearch/plugins':
+        ensure  => 'link',
+        target  => $plugins_dir,
+        force   => true,
+        require => Package['elasticsearch/plugins'],
+    }
+
+    # Install
+    class { '::elasticsearch':
+        require                    => [
+            Package['elasticsearch/plugins'],
+            File['/usr/share/elasticsearch/plugins'],
+        ],
+        # Production elasticsearch needs these plugins to be loaded in order
+        # to work properly.  This will keep elasticsearch from starting
+        # if these plugins are  not available.
+        plugins_mandatory          => [
+            'experimental-highlighter',
+            'extra',
+            'analysis-icu',
+        ],
+        plugins_dir                => $plugins_dir,
+        # Let apifeatureusage create their indices
+        auto_create_index          => '+apifeatureusage-*,-*',
+        # Production can get a lot of use out of the filter cache.
+        filter_cache_size          => '20%',
+        bulk_thread_pool_executors => $bulk_thread_pool_executors,
+        bulk_thread_pool_capacity  => 1000,
+        rack                       => $rack,
+        row                        => $row,
+        awareness_attributes       => $awareness_attributes,
+        cluster_name               => $cluster_name,
+        unicast_hosts              => $unicast_hosts,
+        minimum_master_nodes       => $minimum_master_nodes,
+        recover_after_time         => $recover_after_time,
+        recover_after_nodes        => $recover_after_nodes,
+        heap_memory                => $heap_memory,
+        expected_nodes             => $expected_nodes,
+        master_eligible            => $master_eligible,
+        graylog_hosts              => $graylog_hosts,
+    }
+
+    class { '::elasticsearch::https':
+        ferm_srange      => $ferm_srange,
+        certificate_name => $certificate_name,
+    }
+    class { '::elasticsearch::monitor::diamond': }
+    class { '::elasticsearch::log::hot_threads': }
+
+}
--- modules/role/manifests/elasticsearch/relforge.pp
+++ modules/role/manifests/elasticsearch/relforge.pp
@@ -3,19 +3,20 @@
 # This class sets up Elasticsearch for relevance forge.
 #
 class role::elasticsearch::relforge {
-
-    class { '::role::elasticsearch::common':
-        ferm_srange => '$LABS_NETWORKS',
-    }
+    include ::standard
+    include ::base::firewall
+    include ::profile::elasticsearch
     include ::elasticsearch::nagios::check
 
     # the relforge cluster is serving labs, it should never be connected from
     # production, except from terbium to import production indices.
+    $maintenance_hosts = join($network::constants::special_hosts['production']['maintenance_hosts'], ',')
+
     ::ferm::service { 'elastic-https-terbium':
       ensure => present,
       proto  => 'tcp',
       port   => '9243',
-      srange => '@resolve(terbium.eqiad.wmnet)',
+      srange => "@resolve(${maintenance_hosts})",
     }
 
 }
