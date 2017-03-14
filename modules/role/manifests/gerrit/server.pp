923ae064a5cfe4d435641309f79c2808b6d4b142
Daniel Zahn <dzahn@wikimedia.org>
2017-03-14 13:44:29 -0700

    gerrit: convert to profile/role structure
    Converting role to profile/role structure, following
    the newer puppet style guide on
    
    https://wikitech.wikimedia.org/wiki/Puppet_coding
    
    Change-Id: Ifce502db939f3c6a9e26c7516c9fd5b18a876392

--- modules/role/manifests/gerrit/server.pp
+++ modules/role/manifests/gerrit/server.pp
@@ -1,50 +1,9 @@
-# modules/role/manifests/gerrit/production.pp
+# server running Gerrit code review software
+# https://en.wikipedia.org/wiki/Gerrit_%28software%29
 #
-# filtertags: labs-project-git labs-project-ci-staging
-class role::gerrit::server($ipv4, $ipv6 = undef, $bacula = undef) {
-        system::role { 'role::gerrit::server': description => 'Gerrit server' }
+class role::gerrit::server {
 
-        include ::standard
-        include ::role::backup::host
-        include ::base::firewall
-
-        monitoring::service { 'gerrit_ssh':
-            description   => 'SSH access',
-            check_command => 'check_ssh_port!29418',
-            contact_group => 'admins,gerrit',
-        }
-
-        if $bacula != undef {
-            backup::set { $bacula: }
-        }
-
-        interface::ip { 'role::gerrit::server_ipv4':
-            interface => 'eth0',
-            address   => $ipv4,
-            prefixlen => '32',
-        }
-        if $ipv6 != undef {
-            interface::ip { 'role::gerrit::server_ipv6':
-                interface => 'eth0',
-                address   => $ipv6,
-                prefixlen => '128',
-            }
-        }
-
-        ferm::service { 'gerrit_ssh':
-            proto => 'tcp',
-            port  => '29418',
-        }
-
-        ferm::service { 'gerrit_http':
-            proto => 'tcp',
-            port  => 'http',
-        }
-
-        ferm::service { 'gerrit_https':
-            proto => 'tcp',
-            port  => 'https',
-        }
-
-        class { '::gerrit': }
+    include ::standard
+    include ::profile::gerrit::server
+    include ::role::backup::host
 }
