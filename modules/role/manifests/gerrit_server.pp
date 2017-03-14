1016900251aa117c79854ee81bb6c9ca8071996a
Daniel Zahn <dzahn@wikimedia.org>
2017-03-14 13:44:29 -0700

    gerrit: convert to profile/role structure
    Converting role to profile/role structure, following
    the newer puppet style guide on
    
    https://wikitech.wikimedia.org/wiki/Puppet_coding
    
    Change-Id: Ifce502db939f3c6a9e26c7516c9fd5b18a876392

---
+++ modules/role/manifests/gerrit_server.pp
@@ -0,0 +1,13 @@
+# server running Gerrit code review software
+# https://en.wikipedia.org/wiki/Gerrit_%28software%29
+#
+class role::gerrit_server {
+
+    include ::standard
+    include ::profile::gerrit::server
+
+    include ::role::backup::host
+    if $::bacula != undef {
+        backup::set { $::bacula: }
+    }
+}
