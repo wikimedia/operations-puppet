3cb3eb698d38d2c6f5f9a88007b5a1ada698ef1a
Brandon Black <bblack@wikimedia.org>
2017-03-15 18:33:23 +0000

    [WIP/POC] DNS zones to puppet repo
    * puppet deploys config+zones+admin_state to /srv/authdns/staging-puppet/
    * rsync server on all authdns hosts, serving /srv/authdns/staging-rsync/ to all prod
    * Killed entirely:
    ** the dns repo git::clone stuff
    ** jinja templating
    ** authdns-gen-zones
    ** authdns-git-pull
    
    Unfixed yet:
    * authdns-local-update:
        default behavior:
            run puppet agent
            human review/confirm /srv/authdns/staging-puppet vs /etc/gdnsd/
            sync /srv/authdns/staging-puppet -> /srv/authdns/staging-rsync
            sync /srv/authdns/staging-rsync -> /etc/gdnsd/
            reload/restart as appropriate
        --skip-agent: as above, but no agent run (assume agent orchestrated elsewhere)
        $ORIGIN supplied: (does not touch puppet agent or read puppet staging dir)
            rsync $ORIGIN:/srv/authdns/staging-rsync -> /srv/authdns/staging-rsync
            sync /srv/authdns/staging-rsync -> /etc/gdnsd/
            reload/restart as appropriate
    * authdns-update:
         authdns-local-update on this host
         ssh to other nameservers executing "authdns-local-update $THIS_HOST"
         (very similar to existing behavior, just some args fiddling)
    * lint is untouched, but needs fixing (needs to be a puppet repo
      linter which can template the config/zones from commit under
      review and checkconf them)
    
    Beyond this commit:
    * wmnet discovery entries can be created with a ruby template loop over hieradata
    * We could potentially re-think the authdns-update ssh-based mechanism,
      replacing it with cumin-based automation (e.g. have cumin and/or scripts it
      invokes: run agent on all authdns, check that configdir checksums match, show
      diff, then copy into place on all w/ reload)
    
    The config+zones copied from operations/dns were taken from commit
    bf913d958d87 .  This part will need further updates from ops/dns diffs.
    
    Change-Id: I00404a48d3aab50ded0e0d26f681aff5ac4ca533

---
+++ modules/authdns/manifests/rsync.pp
@@ -0,0 +1,30 @@
+class authdns::rsync {
+    require authdns::config
+    include rsync::server
+    include network::constants
+
+    $staging_dir = "/srv/authdns/staging-rsync"
+
+    file { $staging_dir:
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    rsync::server::module { 'authdns-rsync':
+        path            => $staging_dir,
+        read_only       => 'yes',
+        uid             => 'nobody',
+        gid             => 'nogroup',
+        hosts_allow     => $::network::constants::production_networks
+        max_connections => 25,
+        require         => File[$staging_dir],
+    }
+
+    ferm::service { 'authdns-rsync':
+        proto  => 'tcp',
+        port   => '873',
+        srange => '$PRODUCTION_NETWORKS',
+    }
+}
