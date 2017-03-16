4d62848101da6c226e89988d838ec59d900693c3
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-03-15 17:15:31 -0700

    [WIP] Upgrade logstash to 5.x
    Logstash 5.x is compatible with elasticsearch 2.x through 5.x, whereas
    the currently deployed version of logstash is not compatible with
    elasticsearch 5.x. This should roll out prior to upgrading elasticsearch
    in the logging cluster to 5.x.
    
    * Depends on updated operations/software/logstash/plugins repo to
      provide a 'plugin pack'.
    
    Bug: T154473
    Change-Id: Ica36805c6fb09a33e9cae6788711d4ed3d7beb9b

--- modules/logstash/manifests/init.pp
+++ modules/logstash/manifests/init.pp
@@ -6,18 +6,18 @@
 #
 # == Parameters:
 # - $heap_memory_mb: amount of memory to allocate to logstash in megabytes.
-# - $filter_workers: number of worker threads to run to process filters
+# - $pipeline_workers: number of worker threads to run to process filters
 #
 # == Sample usage:
 #
 #   class { 'logstash':
-#       heap_memory_mb => 128,
-#       filter_workers => 3,
+#       heap_memory_mb => 192,
+#       pipeline_workers => 3,
 #   }
 #
 class logstash(
-    $heap_memory_mb = 64,
-    $filter_workers = 1,
+    $heap_memory_mb = 192,
+    $pipeline_workers = 1,
     $java_package   = 'openjdk-8-jdk',
 ) {
     require_package($java_package)
@@ -31,8 +31,41 @@ class logstash(
         provider => 'trebuchet',
     }
 
+    $plugin_zip_path = '/srv/deployment/logstash/plugins/target/release/plugins-latest.zip'
+    exec { 'install-logstash-plugins':
+        command => "/usr/share/logstash/bin/logstash-plugin install ${plugin_zip_path} && /usr/bin/sha256sum ${plugin_zip_path} > /etc/logstash/plugins.sha256sum",
+        # Only install plugins if hash of latest does not match stored state
+        unless  => "/usr/bin/test \"$(/bin/cat /etc/logstash/plugins.sha256sum)\" = \"$(/usr/bin/sha256sum ${plugin_zip_path})\"",
+        # Intentionally does not notify Service['logstash'], preferring a manual rolling restart of logstash servers
+        require => Package['logstash'],
+        before  => Service['logstash'],
+    }
+
     file { '/etc/default/logstash':
-        content => template('logstash/default.erb'),
+        content => '',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        require => Package['logstash'],
+        notify  => Service['logstash'],
+    }
+
+    file { '/etc/logstash/jvm.options':
+        content => template('logstash/jvm.options.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        require => Package['logstash'],
+        notify  => Service['logstash'],
+    }
+
+    file { '/etc/logstash/logstash.yml':
+        content => ordered_yaml({
+            'path.data'        => '/var/lib/logstash',
+            'path.config'      => '/etc/logstash/conf.d',
+            'path.logs'        => '/var/log/logstash',
+            'pipeline.workers' => $pipeline_workers,
+        }),
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
@@ -51,9 +84,11 @@ class logstash(
         require => Package['logstash'],
     }
 
+    # Older 1.x versions of logstash needed this file deployed,
+    # but 5.x comes with a sensible service definition for systemd
+    # in /etc/systemd/logstash.service
     file { '/lib/systemd/system/logstash.service':
-        content => template('logstash/logstash.service.erb'),
-        notify  => Service['logstash'],
+        ensure  => absent,
     }
 
     service { 'logstash':
