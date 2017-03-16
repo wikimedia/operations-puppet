c50916a76221741540f7c36b0a007f694f90962f
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-03-15 17:21:33 -0700

    [WIP] Update elk stack to 5.x
    * Upgrades elasticsearch to 5.1.2
    * Upgrades kibana to 5.1.2
    * Uses experimental wikimedia apt repository for all logstash nodes
      to access new versions of kibana and elasticsearch packages
    * Updates templates used for logstash for compatability with es 5.x
    ** index is now a boolean, instead of analyzed/not_analyzed/no. New
       settings are basically: analyzed => type: text index: true,
       not_analyzed => type:keyword index:true, no => index:false
    
    Bug: T154473
    Change-Id: I5ee2ace057ef9a7398d7c896fa513fb292050929

--- modules/kibana/manifests/init.pp
+++ modules/kibana/manifests/init.pp
@@ -19,7 +19,7 @@ class kibana (
     require_package('kibana')
 
     # kibana 4
-    file { '/opt/kibana/config/kibana.yml':
+    file { '/etc/kibana/kibana.yml':
         ensure  => file,
         owner   => 'root',
         group   => 'root',
@@ -36,7 +36,7 @@ class kibana (
         enable  => true,
         require => [
             Package['kibana'],
-            File['/opt/kibana/config/kibana.yml'],
+            File['/etc/kibana/kibana.yml'],
         ],
     }
 }
