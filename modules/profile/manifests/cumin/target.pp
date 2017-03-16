29fc568fa35b56cb4c2aa26c330d468cbe416882
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-16 10:39:54 +0100

    profile::cumin::target: add parameters/tags to allow easy selection
    Much alike salt grains, we need a simple way to select cumin targets
    based on:
    
    - site
    - cluster
    - its roles (as tags)
    
    Change-Id: I01a10b4f8192be81a9ea6a7da195e6a58f1d5f8c

--- modules/profile/manifests/cumin/target.pp
+++ modules/profile/manifests/cumin/target.pp
@@ -1,4 +1,20 @@
-class profile::cumin::target() {
+# Parameters here are just a hotfix to the issue of not being able to select
+# by cluster/role/site easily, and also allows to add arbitrary tags to single
+# servers or classes of servers.
+# Note that once the role/profile transition is complete, we should not need
+# those anymore.
+class profile::cumin::target(
+    $cluster = hiera('cluster', 'misc'),
+    $site = $::site,
+) {
+    if $::_roles {
+        $roles = prefix(keys($::_roles), 'role::')
+    } else {
+        $roles = ['role::undef']
+    }
+
+    tag $roles
+
     require ::network::constants
 
     $ssh_authorized_sources = join($::network::constants::special_hosts[$::realm]['cumin_masters'], ',')
