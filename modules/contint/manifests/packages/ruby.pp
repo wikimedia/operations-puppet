647578ad4864c6433e0daad83c83b00031799452
Antoine Musso <hashar@free.fr>
2017-03-17 15:01:32 +0100

    contint: remove Precise related switches
    Clean out contint modules from any conditional switches that were used
    to support Precise.  All Jenkins jobs have been migrated and the
    instances will be deleted on March 20th 2017.
    
    Bug: T158652
    Change-Id: Ibf647d711d2b6cb3021b4e160cb2ccea20de0243

--- modules/contint/manifests/packages/ruby.pp
+++ modules/contint/manifests/packages/ruby.pp
@@ -15,27 +15,6 @@ class contint::packages::ruby {
         ensure => present,
     }
 
-    if os_version('ubuntu < trusty') {
-        package { [
-            'ruby1.9.3',
-            'ruby1.9.1-dev',
-            # Ruby gems is provided within ruby since Trusty
-            'rubygems',
-        ]:
-            ensure => present,
-        }
-        # Ubuntu Precise version is too old.  Instead use either:
-        # /srv/deployment/integration/slave-scripts/tools/bundler/bundle
-        # or:
-        # gem1.9.3 install bundle
-        #
-        # See JJB configuration files.
-        package { [
-            'ruby-bundler',
-        ]:
-            ensure => absent,
-        }
-    }
     if os_version('ubuntu >= trusty') {
         # ruby defaults to 1.9.3 we want 2.0
         package { [
