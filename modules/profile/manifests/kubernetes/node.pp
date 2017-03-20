f7d0d26e9a4731bcd24ffcd91cb7a828688bc970
yuvipanda <yuvipanda@gmail.com>
2017-03-20 11:29:38 -0700

    tools: Use profile::kubernetes::node for worker role
    Bug: T158452
    Change-Id: I6ad769d0225c434c3b72b30f8b91a1d9d1ec57e4

--- modules/profile/manifests/kubernetes/node.pp
+++ modules/profile/manifests/kubernetes/node.pp
@@ -1,6 +1,8 @@
-class profile::kubernetes::node {
-    $master_hosts = hiera('profile::kubernetes::master_hosts')
-    $infra_pod = hiera('profile::kubernetes::infra_pod')
+class profile::kubernetes::node(
+  $master_hosts = hiera('profile::kubernetes::master_hosts'),
+  $infra_pod = hiera('profile::kubernetes::infra_pod'),
+  $use_cni = hiera('profile::kubernetes::use_cni')
+  ) {
 
     base::expose_puppet_certs { '/etc/kubernetes':
         provide_private => true,
@@ -15,7 +17,7 @@ class profile::kubernetes::node {
         listen_address            => '0.0.0.0',
         cluster_dns_ip            => '192.168.0.100',
         use_package               => true,
-        cni                       => true,
+        cni                       => $use_cni,
         pod_infra_container_image => $infra_pod,
         cluster_domain            => undef,
         tls_cert                  => '/etc/kubernetes/ssl/cert.pem',
