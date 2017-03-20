66d3b3cafa335931bc8baadc4ba69afc91780873
Andrew Otto <acotto@gmail.com>
2017-03-20 17:09:11 -0400

    Load wiki project namespace map into HDFS weekly, sqoop mediawiki monthly
    This starts the process of moving Hadoop crons off of analytics1027 and onto anlytics1003 (T159527).
    Bug: T160083
    
    Change-Id: I08a39a5b68cb33dea5b60c6527dfa62d6f3a41e5

---
+++ modules/role/manifests/analytics_cluster/refinery/job/project_namespace_map.pp
@@ -0,0 +1,22 @@
+# == Class role::analytics_cluster::refinery::job::project_namespace_map
+# Installs a weekly cron job to download the Wikimedia sitematrix project
+# namespace map file so that other refinery jobs know about what wiki projects
+# exist.
+#
+class role::analytics_cluster::refinery::job::project_namespace_map {
+    require ::role::analytics_cluster::refinery
+
+    # Shortcut var to DRY up cron commands.
+    $env = "export PYTHONPATH=\${PYTHONPATH}:${role::analytics_cluster::refinery::path}/python"
+
+    $output_directory = '/wmf/data/raw/mediawiki/project_namespace_map'
+
+    # This downloads the project namespace map for a 'labsdb' public import.
+    cron { 'refinery-download-project-namespace':
+        command => "${env} && ${role::analytics_cluster::refinery::path}/bin/download-project-namespace-map -x ${output_directory} -s \$(/bin/date '+%Y-%m')",
+        user    => 'hdfs',
+        minute  => '0',
+        hour    => '12',
+        weekday => '6', # Saturday
+    }
+}
