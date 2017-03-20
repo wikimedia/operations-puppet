7ddbd96105577efa081c1aaf7317d07301fac292
yuvipanda <yuvipanda@gmail.com>
2017-03-20 16:18:03 -0700

    k8s: Make kubernetes master profile flexible enough for tools use
    Bug: T158452
    Change-Id: I1a79db1eb794c39033d266e8dd3e836e9651322a

--- modules/profile/manifests/kubernetes/master.pp
+++ modules/profile/manifests/kubernetes/master.pp
@@ -5,23 +5,34 @@ class profile::kubernetes::master(
     $service_cluster_ip_range=hiera('profile::kubernetes::master::service_cluster_ip_range'),
     $apiserver_count=hiera('profile::kubernetes::master::apiserver_count'),
     $admission_controllers=hiera('profile::kubernetes::master::admission_controllers'),
+    $expose_puppet_certs=hiera('profile::kubernetes::master::use_puppet_certs'),
+    $ssl_cert_path=hiera('profile::kubernetes::master::ssl_cert_path'),
+    $ssl_key_path=hiera('profile::kubernetes::master::ssl_cert_path'),
+    $authz_mode=hiera('profile::kubernetes::master::authz_mode'),
+    $host_automounts=hiera('profile::kubernetes::master::host_automounts'),
+    $host_path_prefixes_allowed=hiera('profile::kubernetes::master::host_path_prefixes_allowed'),
 ){
-    base::expose_puppet_certs { '/etc/kubernetes':
-        provide_private => true,
-        user            => 'kubernetes',
-        group           => 'kubernetes',
+    if $expose_puppet_certs {
+        base::expose_puppet_certs { '/etc/kubernetes':
+            provide_private => true,
+            user            => 'kubernetes',
+            group           => 'kubernetes',
+        }
     }
+
     $etcd_servers = join($etcd_urls, ',')
     class { '::k8s::apiserver':
-        use_package              => true,
-        etcd_servers             => $etcd_servers,
-        docker_registry          => $docker_registry,
-        ssl_cert_path            => '/etc/kubernetes/ssl/cert.pem',
-        ssl_key_path             => '/etc/kubernetes/ssl/server.key',
-        authz_mode               => '',
-        service_cluster_ip_range => $service_cluster_ip_range,
-        apiserver_count          => $apiserver_count,
-        admission_controllers    => $admission_controllers,
+        use_package                => true,
+        etcd_servers               => $etcd_servers,
+        docker_registry            => $docker_registry,
+        ssl_cert_path              => $ssl_cert_path,
+        ssl_key_path               => $ssl_key_path,
+        authz_mode                 => $authz_mode,
+        service_cluster_ip_range   => $service_cluster_ip_range,
+        apiserver_count            => $apiserver_count,
+        admission_controllers      => $admission_controllers,
+        host_path_prefixes_allowed => $host_path_prefixes_allowed,
+        host_automounts            => $host_automounts,
     }
 
     class { '::k8s::scheduler': use_package => true }
