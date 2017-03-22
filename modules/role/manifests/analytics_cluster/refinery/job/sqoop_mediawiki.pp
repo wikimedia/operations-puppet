b177657f4e13ba48bf6d2563a4addb82024bd30d
Andrew Otto <acotto@gmail.com>
2017-03-22 12:24:24 -0400

    Add analytics labsdb pw in hdfs, add logrotate for refinery logs
    Bug: T160083
    Change-Id: I9bd4025117d8dfe85102133503e640e495e51639

--- modules/role/manifests/analytics_cluster/refinery/job/sqoop_mediawiki.pp
+++ modules/role/manifests/analytics_cluster/refinery/job/sqoop_mediawiki.pp
@@ -8,6 +8,8 @@
 class role::analytics_cluster::refinery::job::sqoop_mediawiki {
     require ::role::analytics_cluster::refinery
 
+    include ::passwords::mysql::analytics_labsdb
+
     # Shortcut var to DRY up cron commands.
     $env = "export PYTHONPATH=\${PYTHONPATH}:${role::analytics_cluster::refinery::path}/python"
 
@@ -15,11 +17,13 @@ class role::analytics_cluster::refinery::job::sqoop_mediawiki {
     $wiki_file        = '/mnt/hdfs/wmf/refinery/current/static_data/mediawiki/grouped_wikis/labs_grouped_wikis.csv'
     # We regularly sqoop out of labsdb so that data is pre-sanitized.
     $db_host          = 'labsdb-analytics.eqiad.wmnet'
-    $db_user          = 's53272'
-    $db_password_file = '/user/hdfs/mysql-analytics-research-client-pw.txt'
+    $db_user          = $::passwords::mysql::analytics_labsdb::user
+    # This is rendered elsewhere by role::analytics_cluster::mysql_password.
+    $db_password_file = '/user/hdfs/mysql-analytics-labsdb-client-pw.txt'
+    $log_file         = "${::role::analytics_cluster::refinery::log_dir}/sqoop-mediawiki.log"
 
     cron { 'refinery-sqoop-mediawiki':
-        command  => "${env} && /usr/bin/python3 ${role::analytics_cluster::refinery::path}/bin/sqoop-mediawiki-tables --job-name sqoop-mediawiki-monthly-$(/bin/date '+%Y-%m') --labs --jdbc-host ${db_host} --output-dir ${$output_directory} --wiki-file  ${wiki_file} --user ${db_user} --password-file ${db_password_file} --timestamp \$(/bin/date '+%Y%m01000000') --snapshot \$(/bin/date '+%Y-%m')",
+        command  => "${env} && /usr/bin/python3 ${role::analytics_cluster::refinery::path}/bin/sqoop-mediawiki-tables --job-name sqoop-mediawiki-monthly-$(/bin/date '+%Y-%m') --labs --jdbc-host ${db_host} --output-dir ${$output_directory} --wiki-file  ${wiki_file} --user ${db_user} --password-file ${db_password_file} --timestamp \$(/bin/date '+%Y%m01000000') --snapshot \$(/bin/date '+%Y-%m') >> ${log_file} 2>&1",
         user     => 'hdfs',
         minute   => '0',
         hour     => '0',
