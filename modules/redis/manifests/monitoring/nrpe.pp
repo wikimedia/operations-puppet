1fc05f28b0a92f2090f5175c79b907a2bcd324b1
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-22 18:22:34 +0100

    role::jobqueue_redis::master: depend on etcd, not mw_primary
    Change-Id: I8c3bc639ea04b47f514cee3dabf54ead7b634763

---
+++ modules/redis/manifests/monitoring/nrpe.pp
@@ -0,0 +1,22 @@
+class redis::monitoring::nrpe {
+    # TODO: move the source here once we're done with the migration
+    file { '/usr/lib/nagios/plugins/check_redis':
+        ensure => present,
+        source => 'puppet:///modules/nagios_common/check_commands/check_redis',
+        mode   => '0755',
+    }
+
+    $nrpe_command = '/usr/lib/nagios/plugins/nrpe_check_redis'
+    file { $nrpe_command:
+        ensure => present,
+        source => 'puppet:///modules/redis/nrpe_check_redis.sh',
+        mode   => '0755',
+    }
+
+    sudo::user { 'nagios_check_redis':
+        ensure     => present,
+        user       => 'nagios',
+        privileges => ["ALL = NOPASSWD: ${nrpe_command}"],
+        require    => File[$nrpe_command],
+    }
+}
