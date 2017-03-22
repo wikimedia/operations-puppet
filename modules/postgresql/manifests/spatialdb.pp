1cc75c17b15a98f7a8c42ac001cfccea90b4f560
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2017-03-22 18:59:36 +0100

    postgresql - drop support for postgis 1.5
    it seems that all our postgresql databases have upgraded to postgis > 1.5,
    we can now drop some code...
    
    Change-Id: If8c72e8af8d4a55a4ca8dd091f7a0fa54a797451

--- modules/postgresql/manifests/spatialdb.pp
+++ modules/postgresql/manifests/spatialdb.pp
@@ -16,9 +16,7 @@
 #
 define postgresql::spatialdb(
     $ensure = 'present',
-    $pgversion = hiera('postgresql::spatialdb::pgversion', '9.1'),
-    $postgis_version = hiera('postgresql::spatialdb::postgis_version', '1.5'),
-    ) {
+) {
 
     require ::postgresql::packages
     require ::postgresql::postgis
@@ -31,59 +29,20 @@ define postgresql::spatialdb(
     $postgis_exists_sql = 'SELECT table_catalog FROM information_schema.tables where table_name=\'geometry_columns\';'
     $postgis_exists = "/usr/bin/psql --tuples-only -c \"${postgis_exists_sql}\" ${name} | /bin/grep \'^ ${name}\'"
 
-    $postgres_basedir = "/usr/share/postgresql/${pgversion}"
-
     if $ensure == 'present' {
         exec { "create_db-${name}":
             command => "/usr/bin/createdb ${name}",
             user    => 'postgres',
             unless  => $db_exists,
         }
-        if $postgis_version == '1.5' {
-            exec { "create_plpgsql_lang-${name}":
-                command => "/usr/bin/createlang plpgsql ${name}",
-                user    => 'postgres',
-                unless  => $plpgsql_exists,
-            }
-            exec { "create_postgis-${name}":
-                command => "/usr/bin/psql -d ${name} -f ${postgres_basedir}/contrib/postgis-${postgis_version}/postgis.sql",
-                user    => 'postgres',
-                unless  => $postgis_exists,
-            }
-            # Create spatial_ref_sys
-            exec { "create_spatial_ref_sys-${name}":
-                command     => "/usr/bin/psql -d ${name} -f ${postgres_basedir}/contrib/postgis-${postgis_version}/spatial_ref_sys.sql",
-                user        => 'postgres',
-                refreshonly => true,
-                subscribe   => Exec["create_postgis-${name}"],
-            }
-            exec { "grant_ref_sys_cmd-${name}":
-                command     => "/usr/bin/psql -d ${name} -c \"GRANT SELECT ON spatial_ref_sys TO PUBLIC;\"",
-                user        => 'postgres',
-                refreshonly => true,
-                subscribe   => Exec["create_spatial_ref_sys-${name}"],
-            }
-            exec { "create_comments-${name}":
-                command     => "/usr/bin/psql -d ${name} -f ${postgres_basedir}/contrib/postgis_comments.sql",
-                user        => 'postgres',
-                refreshonly => true,
-                subscribe   => Exec["create_spatial_ref_sys-${name}"],
-            }
-            # Create comments
-            exec { "grant_comments_cmd-${name}":
-                command     => "/usr/bin/psql -d ${name} -c \"GRANT ALL ON geometry_columns TO PUBLIC;\"",
-                user        => 'postgres',
-                refreshonly => true,
-                subscribe   => Exec["create_comments-${name}"],
-            }
-        } else {
-            exec { "create_postgis-${name}":
-                command     => "/usr/bin/psql -d ${name} -c \"CREATE EXTENSION postgis;\"",
-                user        => 'postgres',
-                refreshonly => true,
-                subscribe   => Exec["create_db-${name}"],
-            }
+
+        exec { "create_postgis-${name}":
+            command     => "/usr/bin/psql -d ${name} -c \"CREATE EXTENSION postgis;\"",
+            user        => 'postgres',
+            refreshonly => true,
+            subscribe   => Exec["create_db-${name}"],
         }
+
         exec { "create_extension_hstore-${name}":
             command     => "/usr/bin/psql -d ${name} -c \"CREATE EXTENSION hstore;\"",
             user        => 'postgres',
