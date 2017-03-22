61514492c96e1426e5db6b1f0ad1d5be6c6ffb49
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-22 18:22:34 +0100

    role::jobqueue_redis::master: depend on etcd, not mw_primary
    Change-Id: I8c3bc639ea04b47f514cee3dabf54ead7b634763

---
+++ modules/profile/manifests/jobqueue_redis/instances.pp
@@ -0,0 +1,33 @@
+define profile::jobqueue_redis::instances($ip, $shards = {}) {
+    include ::passwords::redis
+    $replica_state_file = "/etc/redis/replica/${title}-state.conf"
+    redis::instance { $title:
+        settings => {
+            bind                        => '0.0.0.0',
+            appendonly                  => true,
+            auto_aof_rewrite_min_size   => '512mb',
+            client_output_buffer_limit  => 'slave 2048mb 200mb 60',
+            dir                         => '/srv/redis',
+            masterauth                  => $passwords::redis::main_password,
+            maxmemory                   => '8Gb',
+            no_appendfsync_on_rewrite   => true,
+            requirepass                 => $passwords::redis::main_password,
+            save                        => '""',
+            stop_writes_on_bgsave_error => false,
+            slave_read_only             => false,
+            appendfilename              => "${::hostname}-${title}.aof",
+            dbfilename                  => "${::hostname}-${title}.rdb",
+            'include'                   => $replica_state_file,
+        },
+    }
+
+    confd::file { $replica_state_file:
+        ensure     => present,
+        prefix     => '/discovery/appservers-rw',
+        watch_keys => ['/'],
+        uid        => 'root',
+        gid        => 'root',
+        content    => template('profile/jobqueue_redis/statefile.tpl.erb'),
+    }
+
+}
---
+++ modules/redis/files/nrpe_check_redis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+REDIS_HOST="127.0.0.1"
+REDIS_PORT=$1
+REPLICA_WARN=${2:-60}
+REPLICA_CRIT=${3:-600}
+_config_file="/etc/redis/tcp_${REDIS_PORT}.conf"
+
+# TODO: make this scan included files as well in general
+function get_in_redis_config {
+    local _what=$1;
+    local _config=${2:-$_config_file};
+    awk "{if (\$1 == \"${_what}\") {for (i=2; i<NF; i++) printf \$i \" \"; print \$NF}}" $_config_file
+}
+
+_opts="-H ${REDIS_HOST} -p ${REDIS_PORT}"
+
+# Find the master password from the config
+_masterpass=$(get_in_redis_config "requirepass")
+test "$_masterpass" && _opts="${_opts} -x ${_masterpass}"
+
+# Now check the config file and the includes of it for a "slaveof" directive
+for incl in $(get_in_redis_config "include");
+do
+    _slaveof=$(get_in_redis_config "slaveof" | tr ' ' ':')
+    test "$_slaveof" && break
+done
+test "$_slaveof" && _opts="${_opts} -r ${REPLICA_WARN},${REPLICA_CRIT}"
+
+# Run the check
+/usr/lib/nagios/plugins/check_redis $_opts
