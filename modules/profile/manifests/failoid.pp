2310aed2d462fef94c88b54b1f95a582974d1f8c
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-03-23 16:39:05 +0100

    Failoid: reject all TCP traffic
    Bug: T160994
    Change-Id: Ie24ae98ca750cd736411fb3cb11d63009dbfa69a

--- modules/profile/manifests/failoid.pp
+++ modules/profile/manifests/failoid.pp
@@ -1,12 +1,7 @@
-class profile::failoid (
-    $tcp_rejected_ports = hiera('failoid::tcp_rejected_ports'),
-) {
-    validate_array($tcp_rejected_ports)
-
-    if ! empty($tcp_rejected_ports) {
-        $tcp_dports = join($tcp_rejected_ports, ' ')
-        ferm::rule { 'failoid-rejected':
-            rule => "proto tcp dport (${tcp_dports}) REJECT;",
-        }
+class profile::failoid {
+    # As last rule reject all TCP traffic
+    ferm::rule { 'failoid-reject_all_tcp':
+        rule => 'proto tcp REJECT;',
+        prio => '99',
     }
 }
