a616f5c65257c54ba8eb40922ed4d4cf4e72f075
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-23 20:43:23 +0100

    role::memcached: convert to use profile::multidc::redis
    Change-Id: I8868d4c597e275003fe1425abc98e7b344a37422

--- modules/role/manifests/memcached.pp
+++ modules/role/manifests/memcached.pp
@@ -82,64 +82,6 @@ class role::memcached {
         minute  => fqdn_rand(59, 'memkeys'),
     }
 
-
-    $base_settings = {
-        bind                        => '0.0.0.0',
-        auto_aof_rewrite_min_size   => '512mb',
-        client_output_buffer_limit  => 'slave 512mb 200mb 60',
-        dir                         => '/srv/redis',
-        dbfilename                  => "${::hostname}-6379.rdb",
-        masterauth                  => $passwords::redis::main_password,
-        maxmemory                   => '500Mb',
-        maxmemory_policy            => 'volatile-lru',
-        maxmemory_samples           => 5,
-        no_appendfsync_on_rewrite   => true,
-        requirepass                 => $passwords::redis::main_password,
-        save                        => '300 100',
-        slave_read_only             => false,
-        stop_writes_on_bgsave_error => false,
-    }
-
-    $shards = {
-        'eqiad' => hiera('mediawiki::redis_servers::eqiad'),
-        'codfw' => hiera('mediawiki::redis_servers::codfw')
-    }
-
-    if os_version('Debian >= jessie') {
-        class { 'redis::multidc::ipsec':
-            shards => $shards
-        }
-    }
-
-    class { 'redis::multidc::instances':
-        shards   => $shards,
-        settings => $base_settings,
-        map      => {
-            '6380' => {
-                dbfilename => "${::hostname}-6380.rdb",
-            }
-        }
-    }
-
-
-    # Monitoring
-
-    # Declare monitoring for all redis instances
-    redis::monitoring::instance { $::redis::multidc::instances::instances:
-        settings => $base_settings,
-        map      => $::redis::multidc::instances::replica_map,
-    }
-
-    # Firewall rules
-    include ::ferm::ipsec_allow
-
-    $redis_ports = join($::redis::multidc::instances::instances, ' ')
-
-    ferm::service { 'redis_memcached_role':
-        proto => 'tcp',
-        port  => inline_template('(<%= @redis_ports %>)'),
-    }
-
     ferm::service { 'memcached_memcached_role':
         proto => 'tcp',
         port  => '11211',
@@ -149,4 +91,7 @@ class role::memcached {
         content  => template('role/memcached/rsyslog.conf.erb'),
         priority => 40,
     }
+
+    # Include redis for sessions
+    include profile::redis::multidc
 }
