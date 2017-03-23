74c74865140b914da09e4bba9bb2b844dbc01aa0
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-23 18:40:28 +0100

    profile::jobqueue_redis::master: refactoring
    In order to use this profile for all the multi-dc replicated redis
    servers, we rename the profile to profile::multidc_redis::instances and
    allow to pass through a set of common settings as an argument.
    
    This makes the define leaner and not as awkwardly specific.
    
    Change-Id: I183c4d51f945a3263ecc09bfacfec9b421e2cfcd

--- modules/profile/manifests/jobqueue_redis/master.pp
+++ modules/profile/manifests/jobqueue_redis/master.pp
@@ -32,9 +32,25 @@ class profile::jobqueue_redis::master(
         prefix   => $conftool_prefix,
     }
 
-    profile::jobqueue_redis::instances{ $instances:
-        ip     => $ip,
-        shards => $shards,
+    profile::multidc_redis::instances{ $instances:
+        ip        => $ip,
+        shards    => $shards,
+        discovery => 'appservers-rw',
+        aof       => true,
+        settings  => {
+            bind                        => '0.0.0.0',
+            appendonly                  => true,
+            auto_aof_rewrite_min_size   => '512mb',
+            client_output_buffer_limit  => 'slave 2048mb 512mb 60',
+            dir                         => '/srv/redis',
+            masterauth                  => $passwords::redis::main_password,
+            maxmemory                   => '8Gb',
+            no_appendfsync_on_rewrite   => true,
+            requirepass                 => $passwords::redis::main_password,
+            save                        => '""',
+            stop_writes_on_bgsave_error => false,
+            slave_read_only             => false,
+        },
     }
 
     # Add monitoring, using nrpe and not remote checks anymore
---
+++ modules/profile/manifests/multidc_redis/instances.pp
@@ -0,0 +1,46 @@
+define profile::multidc_redis::instances(
+    $ip,
+    $settings = {},
+    $shards = {},
+    $discovery = undef,
+    $aof = false,
+) {
+    $replica_state_file = "/etc/redis/replica/${title}-state.conf"
+
+    if $aof {
+        $base_settings = {
+            appendfilename => "${::hostname}-${title}.aof",
+            dbfilename     => "${::hostname}-${title}.rdb",
+            'include'                   => $replica_state_file,
+        }
+    } else {
+        $base_settings = {
+            dbfilename     => "${::hostname}-${title}.rdb",
+            'include'                   => $replica_state_file,
+        }
+    }
+
+    redis::instance { $title:
+        settings => merge($base_settings, $settings)
+    }
+
+    if $discovery {
+        confd::file { $replica_state_file:
+            ensure     => present,
+            prefix     => "/discovery/${discovery}",
+            watch_keys => ['/'],
+            content    => template('profile/jobqueue_redis/statefile.tpl.erb'),
+        }
+        Base::Service_unit['confd'] -> Base::Service_unit["redis-instance-tcp_${title}"]
+
+    } else {
+        file { $replica_state_file:
+            ensure  => 'present',
+            content => "# This is a placeholder file, do not remove\n",
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            before  => Base::Service_unit["redis-instance-tcp_${title}"]
+        }
+    }
+}
