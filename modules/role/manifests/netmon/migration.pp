7a20b70da2297de52b07c30e9bffdee9e057394b
Daniel Zahn <dzahn@wikimedia.org>
2017-03-22 20:00:02 -0700

    netmon1001 migration: additional paths to backup
    Bug: T125020
    Change-Id: I503b188ef1891750ca30fec2723085045c934c80

--- modules/role/manifests/netmon/migration.pp
+++ modules/role/manifests/netmon/migration.pp
@@ -13,25 +13,50 @@ class role::netmon::migration {
 
     file { [ '/srv/netmon1001',
             '/srv/netmon1001/librenms',
+            '/srv/netmon1001/librenms/var',
+            '/srv/netmon1001/librenms/var/lib',
+            '/srv/netmon1001/librenms/var/lib/librenms',
             '/srv/netmon1001/smokeping',
-            '/srv/netmon1001/torrus' ]:
+            '/srv/netmon1001/smokeping/var',
+            '/srv/netmon1001/smokeping/var/lib',
+            '/srv/netmon1001/smokeping/var/lib/smokeping',
+            '/srv/netmon1001/smokeping/var/cache',
+            '/srv/netmon1001/smokeping/var/cache/smokeping',
+            '/srv/netmon1001/torrus',
+            '/srv/netmon1001/torrus/var',
+            '/srv/netmon1001/torrus/var/cache',
+            '/srv/netmon1001/torrus/var/cache/torrus',
+            '/srv/netmon1001/torrus/var/lib',
+            '/srv/netmon1001/torrus/var/lib/torrus', ]:
         ensure => 'directory',
     }
 
-    rsync::server::module { 'librenms':
-        path        => '/srv/netmon1001/librenms',
+    rsync::server::module { 'librenms-lib':
+        path        => '/srv/netmon1001/librenms/var/lib/librenms',
         read_only   => 'no',
         hosts_allow => $sourceip,
     }
 
-    rsync::server::module { 'smokeping':
-        path        => '/srv/netmon1001/smokeping',
+    rsync::server::module { 'smokeping-lib':
+        path        => '/srv/netmon1001/smokeping/var/lib/smokeping',
         read_only   => 'no',
         hosts_allow => $sourceip,
     }
 
-    rsync::server::module { 'torrus':
-        path        => '/srv/netmon1001/torrus',
+    rsync::server::module { 'smokeping-cache':
+        path        => '/srv/netmon1001/smokeping/var/cache/smokeping',
+        read_only   => 'no',
+        hosts_allow => $sourceip,
+    }
+
+    rsync::server::module { 'torrus-lib':
+        path        => '/srv/netmon1001/torrus/var/lib/torrus',
+        read_only   => 'no',
+        hosts_allow => $sourceip,
+    }
+
+    rsync::server::module { 'torrus-cache':
+        path        => '/srv/netmon1001/torrus/var/cache/torrus',
         read_only   => 'no',
         hosts_allow => $sourceip,
     }
