94e0f4e6770e65ea6a2da53281f750df2e9dd601
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-24 10:59:11 +0100

    profile::redis: add local master and slave profiles
    Change-Id: I4b4de9b92dffd322b04fd6bcae510d1d86528e2d

---
+++ modules/profile/manifests/redis/slave.pp
@@ -0,0 +1,42 @@
+class profile::redis::slave(
+    $settings = hiera('profile::redis::slave::settings'),
+    $master = hiera('profile::redis::slave::master'),
+    $aof = hiera('profile::redis::slave::aof', false),
+){
+    # Figure out the redis instances running on the master from Puppetdb
+    $resources = query_resources(
+        "fqdn='${master}'",
+        'Redis::Instance', false)
+    $password = $resources[0]['parameters']['settings']
+    $redis_ports = inline_template("<%= @resources.map{|r| r['title']}.join ' ' -%>")
+    $instances = split($redis_ports, ' ')
+    $uris = apply_format("localhost:%s/${password}", $instances)
+
+    system::role {'profile::redis::slave': }
+    $auth_settings = {
+        'masterauth'  => $password,
+        'requirepass' => $password,
+    }
+
+    $slaveof = ipresolve($master, 4)
+
+    profile::redis::instance{ $instances:
+        settings => $settings,
+        slaveof  => $slaveof,
+        aof      => true,
+    }
+
+    # Add monitoring, using nrpe and not remote checks anymore
+    redis::monitoring::nrpe_instance { $instances: }
+
+    diamond::collector { 'Redis':
+        settings => { instances => join($uris, ', ') }
+    }
+
+    ferm::service { 'redis_slave_role':
+        proto   => 'tcp',
+        notrack => true,
+        port    => inline_template('(<%= @redis_ports %>)'),
+    }
+
+}
