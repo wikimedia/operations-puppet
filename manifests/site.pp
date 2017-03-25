98e8888b8a31dc824e4d7df78e4b7937ce6929ef
Daniel Zahn <dzahn@wikimedia.org>
2017-03-24 17:02:19 -0700

    deployment::server: convert to profile/role
    Moving this role towards a role/profile structure
    to follow current puppet code organization practices.
    
    https://wikitech.wikimedia.org/wiki/Puppet_coding#Organization
    
    Change-Id: Ieac6487d603b1edf715364bec2a0ef8c04be94ea

--- manifests/site.pp
+++ manifests/site.pp
@@ -2693,12 +2693,7 @@ node /^thumbor100[12].eqiad.wmnet/ {
 
 # deployment servers
 node 'tin.eqiad.wmnet', 'mira.codfw.wmnet', 'naos.codfw.wmnet' {
-    role(deployment::server)
-    include ::base::firewall
-
-    interface::add_ip6_mapped { 'main':
-        interface => 'eth0',
-    }
+    role(deployment_server)
 }
 
 # test system for performance team (T117888)
