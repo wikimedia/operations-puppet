a62c4a0f95fe4d3964d4df45b92b84d9ab9dfe63
Emanuele Rocca <ema@wikimedia.org>
2017-03-28 13:01:15 +0200

    tlsproxy: simplify prometheus metrics gathering
    Focus on HTTP status codes only. Use a dictionary shared among nginx
    worker processes to store the values of nginx_http_requests_total.
    
    More complicated approaches, such as keeping a per-worker count and then
    aggregating it at certain intervals do not give any significant
    performance advantage as described in T161101.
    
    Bug: T161101
    Change-Id: I1ba231f7e6ceef24813fcde627256314718224d5

--- modules/tlsproxy/manifests/prometheus.pp
+++ modules/tlsproxy/manifests/prometheus.pp
@@ -1,14 +1,6 @@
 # == Class: tlsproxy::prometheus
 
 class tlsproxy::prometheus {
-    file { '/etc/nginx/lua/prometheus.lua':
-        source  => 'puppet:///modules/tlsproxy/nginx-lua-prometheus.lua',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        require => File['/etc/nginx/lua'],
-    }
-
     $prometheus_nodes = hiera('prometheus_nodes', [])
 
     nginx::site { 'tlsproxy-prometheus':
