18bbb16c37b2d57b3ded71173f74c471bd98b982
Daniel Zahn <dzahn@wikimedia.org>
2017-03-29 07:50:58 -0700

    aptly: remove special case to remove multiarch support on precise
    Bug: T111760
    Change-Id: I06721dd0766afb719b41306abdcd4125fdaba952

--- modules/aptly/manifests/client.pp
+++ modules/aptly/manifests/client.pp
@@ -2,15 +2,6 @@ class aptly::client(
     $servername,
     $source=false
 ) {
-    if os_version('ubuntu <= precise') {
-        # Remove multiarch support in precise aptly clients.
-        # Can be removed when T111760 is fixed
-        file { '/etc/dpkg/dpkg.cfg.d/multiarch':
-            ensure => absent,
-            notify => Exec['apt-get update'],
-        }
-    }
-
     apt::repository { 'project-aptly':
         uri        => "http://${servername}/repo",
         dist       => "${::lsbdistcodename}-${::labsproject}",
