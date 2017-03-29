ce14cc8dc3e010d46992c79442ded7aed7644dee
Dan Duvall <dduvall@wikimedia.org>
2017-03-29 12:40:26 -0700

    ci: Docker registry for container builds
    Docker registry for hosting base and newly built container images. Down
    the road this may not be necessary as Ops has suggested that we may
    simply be able to share the production registry, but we'll still need
    something for proofing and experimentation.
    
    Some hieradata is stubbed and must be provided via secret hieradata on
    the puppetmaster and/or via horizon.
    
    Bug: T161657
    Change-Id: I03378f977f671d5a7e4aa54d631056325b31bed0

---
+++ modules/role/manifests/ci/docker/registry.pp
@@ -0,0 +1,11 @@
+class role::ci::docker::registry {
+    require ::role::labs::lvm::srv
+
+    include ::docker::registry
+    include ::sslcert::dhparam
+
+    class { '::docker::registry::web':
+        ssl_settings => ssl_ciphersuite('nginx', 'mid'),
+        require      => Class['::sslcert::dhparam'],
+    }
+}
