802698b6834f3e763df849d8e8d3151cf29fab5f
Daniel Zahn <dzahn@wikimedia.org>
2017-03-29 07:34:03 -0700

    osm: remove "if os_version('ubuntu <= precise')"-stanza
    Change-Id: I727b5e8ca6cc2935bf6c176adbe9fe15a65787ed

--- modules/osm/manifests/planet_import.pp
+++ modules/osm/manifests/planet_import.pp
@@ -22,13 +22,4 @@ define osm::planet_import(
 
     # Check if our db tables exist
     $tables_exist="/usr/bin/psql -d ${name} --tuples-only -c \'SELECT table_name FROM information_schema.tables;\'|/bin/grep \'planet_osm\'"
-
-    # Note. This is not needed anymore with osm2pgsql 0.81
-    if os_version('ubuntu <= precise') {
-        exec { "load_900913-${name}":
-            command => "/usr/bin/psql -d ${name} -f /usr/share/osm2pgsql/osm2pgsql/900913.sql",
-            user    => 'postgres',
-            unless  => $tables_exist,
-        }
-    }
 }
