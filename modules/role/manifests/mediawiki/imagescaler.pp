0805b9be27265d28bc8dba039fc2427f67d85c24
Mark Holmquist <mholmquist@wikimedia.org>
2017-03-29 10:04:38 -0500

    Add 3d2png deploy repo to image scalers
    Bug: T160185
    Change-Id: Ib48d47bb0afef51ad710a0578427eb43d46b6b10

--- modules/role/manifests/mediawiki/imagescaler.pp
+++ modules/role/manifests/mediawiki/imagescaler.pp
@@ -6,5 +6,8 @@ class role::mediawiki::imagescaler {
     include ::role::prometheus::apache_exporter
     include ::role::prometheus::hhvm_exporter
     include ::base::firewall
+    scap::target: { '3d2png/deploy':
+        deploy_user => 'mwdeploy',
+    }
 }
 
