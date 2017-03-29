07d3c723c2df0cef9563274cd179e7c9fca307de
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2017-03-29 13:11:04 +0200

    postgresql - simplify creation of databases
    * extract a postresql::extension defined type to manage extensions
    * make postgresql::spatialdb reuse postgresql::db
    * align the different checks for existence with the same pattern
    * remove the management of plpgsql language, it was only managed to
      remove it, which is useless as the related database is removed
      anyway
    
    This cleanup will help integrating new import scripts for maps.
    
    Bug: T157613
    
    Change-Id: I3620376d24d21e7aa41bbefff592bd7b39b4539d

---
+++ modules/postgresql/manifests/db/extension.pp
@@ -0,0 +1,29 @@
+define postgresql::db::extension (
+    $extname,
+    $database,
+    $ensure = present,
+) {
+
+    $db_sql = "SELECT datname from pg_catalog.pg_database where datname = '${database}'"
+    $db_exists = "/usr/bin/test -n \"\$( /usr/bin/psql -At -c \"${db_sql}\")\""
+
+    $extension_sql = "SELECT extname FROM pg_extension WHERE extname = '${extname}'"
+    $extension_exists = "/usr/bin/test -n \"\$( /usr/bin/psql -At -d ${database} -c \"${extension_sql}\")\""
+
+    case $ensure {
+        absent: {
+            exec { "drop_${extname}_on_${database}":
+                command => "/usr/bin/psql -d ${database} -c \"DROP EXTENSION ${extname};\"",
+                user    => 'postgres',
+                onlyif  => "${db_exists} && ${extension_exists}",
+            }
+        }
+        default: {
+            exec { "create_${extname}_on_${database}":
+                command => "/usr/bin/psql -d ${database} -c \"CREATE EXTENSION ${extname};\"",
+                user    => 'postgres',
+                unless  => "${db_exists} && ${extension_exists}",
+            }
+        }
+    }
+}
