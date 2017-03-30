b57ac92d6b1b1d56b6c2c9e175ca0a2e64e68cbc
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-30 09:14:43 +0200

    role::mediawiki::common: convert to profile (step 1)
    Change-Id: I2518162bd170e93d6c5bfa0dfb1fb92e92a52a97

---
+++ modules/profile/manifests/mediawiki/common.pp
@@ -0,0 +1,57 @@
+class profile::mediawiki::common(
+    $forward_syslog = hiera('logstash_syslog'),
+    $log_aggregator = hiera('udp2log_aggregator'),
+    ){
+
+    # GeoIP is needed for MW
+    class { '::geoip':
+    }
+
+    class { '::mediawiki':
+        forward_syslog => $forward_syslog,
+        log_aggregator => $log_aggregator,
+    }
+
+    class { '::mediawiki::nutcracker':
+    }
+
+    class { '::tmpreaper':
+    }
+
+    ferm::rule { 'skip_nutcracker_conntrack_out':
+        desc  => 'Skip outgoing connection tracking for Nutcracker',
+        table => 'raw',
+        chain => 'OUTPUT',
+        rule  => 'proto tcp sport (6378:6382 11212) NOTRACK;',
+    }
+
+    ferm::rule { 'skip_nutcracker_conntrack_in':
+        desc  => 'Skip incoming connection tracking for Nutcracker',
+        table => 'raw',
+        chain => 'PREROUTING',
+        rule  => 'proto tcp dport (6378:6382 11212) NOTRACK;',
+    }
+
+    ferm::service{ 'ssh_pybal':
+        proto  => 'tcp',
+        port   => '22',
+        srange => '$PRODUCTION_NETWORKS',
+        desc   => 'Allow incoming SSH for pybal health checks',
+    }
+
+    # Allow sockets in TIME_WAIT state to be re-used.
+    # This helps prevent exhaustion of ephemeral port or conntrack sessions.
+    # See <http://vincent.bernat.im/en/blog/2014-tcp-time-wait-state-linux.html>
+    sysctl::parameters { 'tcp_tw_reuse':
+        values => { 'net.ipv4.tcp_tw_reuse' => 1 },
+    }
+
+    include scap::ferm
+
+    monitoring::service { 'mediawiki-installation DSH group':
+        description    => 'mediawiki-installation DSH group',
+        check_command  => 'check_dsh_groups!mediawiki-installation',
+        check_interval => 60,
+    }
+
+}
