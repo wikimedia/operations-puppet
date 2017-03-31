530e1b68fb88074058dba19b73304f1e1d804492
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2017-03-31 19:07:21 +0200

    osm - trying to fix tests
    Change-Id: If4f207752fcfc11575a9732b6fa88c55462377b7

--- modules/osm/spec/classes/osm_spec.rb
+++ modules/osm/spec/classes/osm_spec.rb
@@ -1,46 +1,24 @@
 require 'spec_helper'
 
 describe 'osm', :type => :class do
-    context 'ensure present' do
-        let(:params) { {
-            :ensure => 'present',
-        } }
 
-        context 'on Ubuntu Precise' do
-            let(:facts) { {
-                :lsbdistrelease => 'Precise',
-                :lsbdistid      => 'Ubuntu',
-            } }
-
-            context 'ensure present' do
-                it { should contain_package('osm2pgsql').with_ensure('present') }
-                it { should contain_package('osmosis').with_ensure('present') }
-            end
-        end
-
-        context 'on Debian Jessie' do
-            let(:facts) { {
-                :lsbdistrelease => 'Jessie',
-                :lsbdistid      => 'Debian',
-            } }
-
-            context 'ensure present' do
-                it { should contain_package('osm2pgsql').with_ensure('0.90.0+ds-1~bpo8+1') }
-                it { should contain_package('osmosis').with_ensure('present') }
-            end
-        end
-    end
-
-    context 'ensure absent' do
-        let(:params) { {
-            :ensure => 'absent',
-        } }
+    context 'on Ubuntu Precise' do
         let(:facts) { {
             :lsbdistrelease => 'Precise',
             :lsbdistid      => 'Ubuntu',
         } }
 
-        it { should contain_package('osm2pgsql').with_ensure('absent') }
-        it { should contain_package('osmosis').with_ensure('absent') }
+        it { should contain_package('osm2pgsql').with_ensure('present') }
+        it { should contain_package('osmosis').with_ensure('present') }
     end
+
+    context 'on Debian Jessie' do
+        let(:facts) { {
+            :lsbdistrelease => 'Jessie',
+            :lsbdistid      => 'Debian',
+        } }
+
+        it { should contain_apt__pin('osm2pgsql').with_pin('release a=jessie-backports') }
+    end
+
 end
