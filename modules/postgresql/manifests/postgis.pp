1f64fedfb4dcb4d0960a77b5a1579590ac2d9fc6
Faidon Liambotis <faidon@wikimedia.org>
2017-03-31 16:23:50 +0300

    postgresql: remove precise support
    Remove support for Ubuntu precise, not used anymore.
    
    Change-Id: Ifc4192fa9cd8f148c8f27e7780565936522c4efc

--- modules/postgresql/manifests/postgis.pp
+++ modules/postgresql/manifests/postgis.pp
@@ -16,28 +16,14 @@ class postgresql::postgis(
     $ensure = 'present',
     $postgresql_postgis_package = $::lsbdistcodename ? {
         'jessie'  => 'postgresql-9.4-postgis-2.3',
-        'precise' => 'postgresql-9.1-postgis',
         'trusty'  => 'postgresql-9.3-postgis-2.1',
     },
-    $install_postgis_scripts = $::lsbdistcodename ? {
-        'jessie'  => true,
-        'precise' => false,
-        'trusty'  => true,
-        default   => true,
-    },
 ) {
-    validate_bool($install_postgis_scripts)
-
     package { [
         $postgresql_postgis_package,
+        "${postgresql_postgis_package}-scripts",
         'postgis',
     ]:
         ensure  => $ensure,
-    }
-
-    if $install_postgis_scripts {
-        package { "${postgresql_postgis_package}-scripts":
-            ensure  => $ensure,
-        }
     }
 }
