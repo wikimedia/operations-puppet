644bdb63bbcb41c8acceff990646f1132607879d
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-03-31 14:29:14 +0300

    changeprop: Add an ores_uris parameter
    Per T159615, one approach to solving the issue of having ORES's cache
    warmed up in both datacenters, is to duplicate changeprop's requests to
    both ORES clusters. In order to facilitate that, add an ores_uris
    parameter that is meant to be a list of the URLs changeprop should
    contact ORES clusters. For now we don't remove the ores_uri parameter on
    purpose to make the migration easier, since the config is tightly
    coupled with the jinja template generating the final configuration on
    the host by scap. Should be removed in a followup commit
    
    Bug: T159615
    Change-Id: I3282c00754da7cff4e8c62359806a110f67e9921

--- modules/changeprop/manifests/init.pp
+++ modules/changeprop/manifests/init.pp
@@ -20,9 +20,9 @@
 #   requests can be sent to the inactive DC. Default:
 #   'http://restbase.svc.eqiad.wmnet:7231'
 #
-#
-# [*ores_uri*]
-#   The host where ORES service is running. Default: http://ores.svc.eqiad.wmnet:8081
+# [*ores_uris*]
+#   A list of urls for the ORES service. Defaults to:
+#   [http://ores.svc.eqiad.wmnet:8081, http://ores.svc.codfw.wmnet:8081]
 #
 class changeprop(
     $broker_list,
@@ -30,6 +30,10 @@ class changeprop(
     $purge_port   = 4827,
     $restbase_uri = 'http://restbase.svc.eqiad.wmnet:7231',
     $ores_uri     = 'http://ores.svc.eqiad.wmnet:8081',
+    $ores_uris    = [
+        'http://ores.svc.eqiad.wmnet:8081',
+        'http://ores.svc.codfw.wmnet:8081',
+    ],
 ) {
 
     include ::service::configuration
@@ -48,6 +52,7 @@ class changeprop(
             mwapi_uri    => $::service::configuration::mwapi_uri,
             restbase_uri => $restbase_uri,
             ores_uri     => $ores_uri,
+            ores_uris    => $ores_uris,
             purge_host   => $purge_host,
             purge_port   => $purge_port,
             site         => $::site,
