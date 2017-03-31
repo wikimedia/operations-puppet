b958133868cf295573c5d4364a43369e9ca6b922
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-03-31 12:31:44 +0200

    role::mediawiki::videoscaler: deduce parameters from number of cpus
    Change-Id: I3d28dc6a78e7207934f629f2504ea7998f3c6c37

--- modules/role/manifests/mediawiki/videoscaler.pp
+++ modules/role/manifests/mediawiki/videoscaler.pp
@@ -5,6 +5,17 @@ class role::mediawiki::videoscaler {
     # Parent role
     include ::role::mediawiki::scaler
 
+    # Runners configuration for videoscalers
+
+    # We have multiple long-running processes occupying as much as 2 cores
+    $total_runners = floor(0.7 * $facts['processorcount'])
+    # We need some hhvm threads to be free to account for monitoring
+    # requests
+    $hhvm_threads = $total_runners + 5
+    # Have at max 8 runners for normal jobs
+    $runners = min(ceiling(0.5 * $total_runners), 8)
+    # Leave the rest to prioritized jobs
+    $prioritized_runners = $total_runners - $runners
     # Profiles
     include ::role::prometheus::apache_exporter
     include ::role::prometheus::hhvm_exporter
