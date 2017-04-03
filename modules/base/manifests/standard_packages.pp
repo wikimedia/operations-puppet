b52aa6d9588b4a41cd8fd5987f5738f783683a68
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-04-03 16:27:41 +0200

    Disable wireshark-common/install-setuid to avoid debconf prompt
    We don't want tshark be to setuid root and this annoying debconf
    prompt is otherwise shown all the time unless using
    DEBCONF_FROTNEND=noninteractive
    
    Change-Id: I0866c48f30202e50dfbdf5d38a760e34521308d6

--- modules/base/manifests/standard_packages.pp
+++ modules/base/manifests/standard_packages.pp
@@ -79,4 +79,10 @@ class base::standard_packages {
     # lint:endignore
         require_package('mcelog')
     }
+
+    # Pulled in via tshark above
+    debconf::set { 'wireshark-common/install-setuid':
+        value   => 'false',
+        require => Package['tshark']
+    }
 }
