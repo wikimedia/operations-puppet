cf9a6a6898efc0e28178b1ccb8bf18b070fd9559
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-04-04 13:21:28 +0000

    Revert "Revert "Revert "Revert "Add the LVS blocks to url_downloader""""
    This reverts commit 43c7024bd415b7baa28ce6e029bab5daf4099c2c.
    
    Change-Id: I3dcac7164cdb43defeca8b435c2274dd5a195904

--- modules/profile/manifests/url_downloader.pp
+++ modules/profile/manifests/url_downloader.pp
@@ -65,6 +65,12 @@ class profile::url_downloader (
             $network::constants::all_network_subnets['production']['codfw']['private']['private1-d-codfw']['ipv4'],
             $network::constants::all_network_subnets['production']['codfw']['private']['private1-d-codfw']['ipv6'],
 
+            # Block access to LVS IPs. TODO: This is still incomplete
+            $network::constants::all_network_subnets['production']['codfw']['public']['public1-lvs-codfw']['ipv4'],
+            $network::constants::all_network_subnets['production']['eqiad']['public']['public1-lvs-eqiad']['ipv4'],
+            $network::constants::all_network_subnets['production']['ulsfo']['public']['public1-lvs-ulsfo']['ipv4'],
+            $network::constants::all_network_subnets['production']['esams']['public']['public1-lvs-esams']['ipv4'],
+
             $network::constants::all_network_subnets['production']['esams']['public']['public1-esams']['ipv4'], #TODO: Do we need this ?
             $network::constants::all_network_subnets['production']['esams']['public']['public1-esams']['ipv6'], #TODO: Do we need this ?
 
