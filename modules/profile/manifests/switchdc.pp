d1c285f26f16856631d284b7302db1210a1f03e3
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-04-04 13:51:28 +0200

    Switchdc: add profile to install and configure it
    Bug: T160178
    Change-Id: I0c46a7085b24f1a2e1768c42fcef28178e805140

---
+++ modules/profile/manifests/switchdc.pp
@@ -0,0 +1,114 @@
+# == Class profile::switchdc
+#
+# Installs the switchdc software and its configuration
+#
+# === Parameters
+#
+# [*redis_password*]
+#   The Redis password to connect to it.
+#
+# [*redis_shards*]
+#   The hash with the Redis shards.
+#
+# [*tcpircbot_host*]
+#   Hostname for the IRC bot.
+#
+# [*tcpircbot_port*]
+#   Port to use with the IRC bot.
+#
+class profile::switchdc(
+    # Configuration
+    $redis_password = hiera('passwords::redis::main_password'),
+    $redis_shards   = hiera('redis::shards'),
+    $tcpircbot_host = hiera('profile::conftool::client::tcpircbot_host', 'icinga.wikimedia.org'),
+    $tcpircbot_port = hiera('profile::conftool::client::tcpircbot_port', 9200),
+) {
+    include ::passwords::redis
+
+    # Install dependencies
+    require_package([
+        'cumin',
+        'python-conftool',
+        'python-dnspython',
+        'python-redis',
+        'python-requests',
+        'python-yaml',
+    ])
+
+    # Setup directories
+    file { ['/srv/deployment', '/etc/switchdc', '/etc/switchdc/stages.d']:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0640',
+    }
+
+    $checkout_dir = '/srv/deployment/switchdc'
+
+    # Install the software
+    git::clone { 'operations-switchdc':
+        directory => $checkout_dir,
+        origin    => 'https://gerrit.wikimedia.org/r/p/operations/switchdc.git',
+        branch    => 'master',
+        owner     => 'root',
+        group     => 'ops',
+        require   => File['/srv/deployment'],
+    }
+
+    # Install the entry point
+    file { '/usr/local/sbin/switchdc':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0700',
+        content => template('profile/switchdc/switchdc.erb'),
+    }
+
+    # Install the global configuration
+    file { '/etc/switchdc/config.yaml':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        content => template('profile/switchdc/config.yaml.erb'),
+        require => File['/etc/switchdc'],
+    }
+
+    # Setup stage's configuration directories
+    file { ['/etc/switchdc/stages.d/t05_redis']:
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        require => File['/etc/switchdc/stages.d'],
+    }
+
+    # Install t05_redis configuration
+    file { '/etc/switchdc/stages.d/t05_redis/config.yaml':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        content => template('profile/switchdc/t05_redis/config.yaml.erb'),
+        require => File['/etc/switchdc/stages.d/t05_redis'],
+    }
+
+    file { '/etc/switchdc/stages.d/t05_redis/jobqueue.yaml':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        content => ordered_yaml($redis_shards['jobqueue']),
+        require => File['/etc/switchdc/stages.d/t05_redis'],
+    }
+
+    file { '/etc/switchdc/stages.d/t05_redis/sessions.yaml':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        content => ordered_yaml($redis_shards['sessions']),
+        require => File['/etc/switchdc/stages.d/t05_redis'],
+    }
+
+}
