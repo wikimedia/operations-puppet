1e1360699e51fa081c9f62e9fd874e35e9ba7119
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-04-06 12:32:58 +0300

    Remove system::role {"profile::redis::${category}":}
    There is no such role. This define is included in a profile class,
    and using system::role in a profile is clearly wrong naming wise.
    Moreover, it does not really add any tangible benefit to have all
    profile classes add motd entries, whereas having a role class add
    one is useful.
    
    This system::role is templated and included in 2 roles. Add a correctly
    named system::role to each of these two
    
    Change-Id: I24a4ac453b45e4033a58749fdae9bd3b88840113

--- modules/role/manifests/mediawiki/memcached.pp
+++ modules/role/manifests/mediawiki/memcached.pp
@@ -1,4 +1,8 @@
 # Role for the MediaWiki memcached+redis-sessions role for production.
 class role::mediawiki::memcached inherits role::memcached {
     include profile::redis::multidc
+
+    system::role { 'role::mediawiki::memcached':
+        description => 'memcached+redis sessions',
+    }
 }
