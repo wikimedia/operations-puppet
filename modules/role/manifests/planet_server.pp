0e04f8179ebc601fb8329e3467855edff3f4d0d2
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-04-06 12:26:22 +0300

    Move system::role { 'role::planet::venus': }
    There is no such role. This define is included in a profile class,
    and using system::role in a profile is clearly wrong naming wise.
    Moreover, it does not really add any tangible benefit to have all
    profile classes add motd entries, whereas having a role class add
    one is useful.
    
    This system::role is moved and renamed to role::planet_server
    
    Change-Id: I28e3f973cd8aab456edbcdcc3f6b323781e2cd14

--- modules/role/manifests/planet_server.pp
+++ modules/role/manifests/planet_server.pp
@@ -4,5 +4,9 @@ class role::planet_server {
     include standard
     include profile::planet::venus
 
+    system::role { 'role::planet_server':
+        description => 'Planet (venus) weblog aggregator'
+    }
+
     interface::add_ip6_mapped { 'main': interface => 'eth0', }
 }
