de39548c0102456343ce06cfe8fe2f069b0a92dd
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2017-04-07 12:07:11 +0200

    postgresql - use the check-postgres package for icinga checks
    This moves replication checks from being time based to being size based.
    This should work better on cluster which don't see much activity.
    
    Bug: T162345
    Change-Id: I98dd0bf16a16fb96994e0c7b049a4ae37d059cb6

--- modules/role/manifests/maps/slave.pp
+++ modules/role/manifests/maps/slave.pp
@@ -9,17 +9,12 @@ class role::maps::slave {
     }
     $master = hiera('postgresql::slave::master_server')
 
-    # check_postgres_replication_lag script relies on values that are only
-    # readable by superuser or replication user. This prevents using a
-    # dedicated user for monitoring.
     $replication_pass = hiera('postgresql::slave::replication_pass')
-    $critical = 1800
-    $warning = 300
-    $icinga_command = "/usr/lib/nagios/plugins/check_postgres_replication_lag.py \
--U replication -P ${replication_pass} -m ${master} -D template1 -C ${critical} -W ${warning}"
-    nrpe::monitor_service { 'postgres-rep-lag':
-        description  => 'Postgres Replication Lag',
-        nrpe_command => $icinga_command,
+
+    class { 'postgresql::slave::monitoring':
+        pg_master   => $master,
+        pg_user     => 'replication',
+        pg_password => $replication_pass,
     }
 
     $prometheus_command = "/usr/bin/prometheus_postgresql_replication_lag -m ${master} -P ${replication_pass}"
