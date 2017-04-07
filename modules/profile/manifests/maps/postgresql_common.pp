2f09bd58ee17ce368e3216a6fcc89e0916d1c8c2
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2017-04-07 17:05:09 +0200

    [WIP] maps - move to role / profile
    Move to role / profile as documented on
    https://phabricator.wikimedia.org/T147718.
    
    This includes a fair amount of changes in hiera configuration. Some
    duplication between maps master and maps slave is needed, but even with this
    duplication, the overall code seems easier to maintain.
    
    Change-Id: Ie78a30163ccde9922396676a8ad529561729a7a2

---
+++ modules/profile/manifests/maps/postgresql_common.pp
@@ -0,0 +1,28 @@
+class profile::maps::postgresql_common {
+    include ::postgresql::postgis
+
+    # Tuning
+    file { '/etc/postgresql/9.4/main/tuning.conf':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/profile/maps/tuning.conf',
+    }
+
+    sysctl::parameters { 'postgres_shmem':
+        values => {
+            # That is derived after tuning postgresql, deriving automatically is
+            # not the safest idea yet.
+            'kernel.shmmax' => 8388608000,
+        },
+    }
+
+    # TODO: Figure out a better way to do this
+    # Ensure postgresql logs as maps-admin to allow maps-admin to read them
+    # Rely on logrotate's copytruncate policy for postgres for the rest of the
+    # log file
+    file { '/var/log/postgresql/postgresql-9.4-main.log':
+        group => 'maps-admins',
+    }
+}
