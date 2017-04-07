c4f977a6a1b89c20d6b0cf2d033e8e8acb85aa0f
Guillaume Lederrey <glederrey@wikimedia.org>
2017-04-07 17:05:09 +0200

    maps - move to role / profile
    Move to role / profile as documented on
    https://phabricator.wikimedia.org/T147718.
    
    This includes a fair amount of changes in hiera configuration. Some
    duplication between maps master and maps slave is needed, but even with this
    duplication, the overall code seems easier to maintain.
    
    Change-Id: Ie78a30163ccde9922396676a8ad529561729a7a2

--- modules/role/manifests/maps/test/master.pp
+++ modules/role/manifests/maps/test/master.pp
@@ -1,15 +1,14 @@
 # Sets up a maps server master
-class role::maps::master {
+class role::maps::test::master {
     include ::standard
     include ::base::firewall
-    include ::role::lvs::realserver
 
     include ::profile::maps::apps
     include ::profile::maps::cassandra
     include ::profile::maps::osm_master
     include ::profile::maps::redis
 
-    system::role { 'role::maps::master':
+    system::role { 'role::maps::test::master':
       ensure      => 'present',
       description => 'Maps master (postgresql, cassandra, redis, tilerator, kartotherian)',
     }
