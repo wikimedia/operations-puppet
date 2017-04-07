bd4d58e67737ef43df037e6480ead4c5240bf981
Daniel Zahn <dzahn@wikimedia.org>
2017-04-06 17:19:05 -0700

    lists: convert to role/profile structure
    Change-Id: Ifdffd277d091b31f500084dfb3037e159b2b5ddd

---
+++ modules/role/manifests/lists.pp
@@ -0,0 +1,9 @@
+# sets up a mailing list server
+class role::lists {
+
+    system::role { 'role::lists': description => 'Mailing list server', }
+
+    include ::standard
+    include ::profile::backup::host
+    include ::profile::lists
+}
