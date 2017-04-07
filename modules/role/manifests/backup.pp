65a34b03fa5bd9cdace7046ac15303f60a1459a6
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-04-07 17:37:41 +0300

    Make role::backup::storage a profile
    Create a new profile profile::backup::storage, use it in role::backup
    and role::backup::offsite
    
    Change-Id: I814ef74d3cce6f486a2b9da0d1bc8c7815d3f6da

--- modules/role/manifests/backup.pp
+++ modules/role/manifests/backup.pp
@@ -2,6 +2,7 @@ class role::backup {
     # We actually want to be able to backup ourselves
     include ::profile::backup::host
     include ::profile::backup::director
+    include ::profile::backup::storage
     include ::standard
 
     system::role { 'role::backup':
