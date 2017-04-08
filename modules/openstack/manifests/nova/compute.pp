432ebf5ff8a5183a0a2872a1c49c289737c235e6
andrewbogott <abogott@wikimedia.org>
2017-04-08 15:07:09 -0500

    nova-compute monitoring:  Check for one /and only one/ nova-compute process
    Bug: T162640
    Change-Id: I7b6c4b5f1ceb9bc0f3e2e88a1aac4185423e756f

--- modules/openstack/manifests/nova/compute.pp
+++ modules/openstack/manifests/nova/compute.pp
@@ -190,6 +190,6 @@ class openstack::nova::compute(
 
     nrpe::monitor_service { 'check_nova_compute_process':
         description  => 'nova-compute process',
-        nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/nova-compute'",
+        nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1:1 --ereg-argument-array '^/usr/bin/python /usr/bin/nova-compute'",
     }
 }
