1b07dc83ab3aa2f4e623bc9629b0add2785e1fd1
Ottomata <aotto@wikimedia.org>
2017-04-11 13:27:03 +0000

    Revert "Move hadooop python package declaration into client"
    stat100[24] are jessie, and don't have these packages.
    
    
    This reverts commit f72ae307f496e7742cd6aef433be65a14e6e5c3d.
    
    Change-Id: I7697b7bc6b027f1b7aa793082457cf0dfde8036b

--- modules/role/manifests/analytics_cluster/hadoop/worker.pp
+++ modules/role/manifests/analytics_cluster/hadoop/worker.pp
@@ -114,6 +114,45 @@ class role::analytics_cluster::hadoop::worker {
     # Install MaxMind databases for geocoding UDFs
     include ::geoip
 
+    # Install packages that are useful for distributed
+    # computation in Hadoop, and thus should be available on
+    # any Hadoop nodes.
+    require_package(
+        'python-pandas',
+        'python-scipy',
+        'python-requests',
+        'python-matplotlib',
+        'python-dateutil',
+        'python-sympy',
+        'python-docopt',
+        'python3',
+        'python3-tabulate',
+        'python3-scipy',
+        'python3-enchant',
+        'python3-tz',
+        'python3-nltk',
+        'python3-nose',
+        'python3-setuptools',
+        'python3-requests',
+        'python3-mmh3',
+        'python3-docopt',
+        'libgomp1'
+    )
+
+    # Need a specifc version of python-numpy for sklearn.
+    # There are some weird dependency / require_package
+    # issues that force us to use the package resource
+    # directly.
+    package { ['python-numpy', 'python3-numpy']:
+        ensure => '1:1.12.0-2~bpo8+1',
+    }
+    package { ['python3-sklearn','python3-sklearn-lib']:
+        ensure  => 'installed',
+        require => Package['python3-numpy'],
+    }
+
+
+
     # This allows Hadoop daemons to talk to each other.
     ferm::service{ 'hadoop-access':
         proto  => 'tcp',
