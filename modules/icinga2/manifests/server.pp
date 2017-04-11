bc78fb8e3c3046589b8a3bbd3c033918215c7c34
Paladox <thomasmulhall410@yahoo.com>
2017-04-11 16:56:35 +0000

    Create an icinga2 class
    This is a class that will hopefully replace icinga 1.x in wikimedia but manly for us to use in labs to begin with.
    
    Change-Id: I869a5d7f443d8f5b5ef28a5857dc8c698c37e9a9

---
+++ modules/icinga2/manifests/server.pp
@@ -0,0 +1,18 @@
+
+class icinga2::server {
+
+    apt::repository { 'icinga2':
+        uri        => 'http://packages.icinga.com/debian',
+        dist       => 'icinga-jessie',
+        components => 'main',
+        source     => false,
+        keyfile    => 'puppet:///modules/icinga2/icinga2.pgp',
+    }
+
+    package { ['icinga2', 'icingaweb2'] :
+        ensure  => present,
+        require => [
+            Apt::Repository['icinga2'],
+        ],
+    }
+}
