ba79a07849db9bcd51d21f729ef3cd7e28673dc1
Paladox <thomasmulhall410@yahoo.com>
2017-04-12 19:07:31 +0000

    icinga: Remove icinga-init.sh file, its now provided by the package
    Change-Id: Iebe609f56430811fd62af69bba60ef61b4e44aa7

--- modules/icinga/manifests/init.pp
+++ modules/icinga/manifests/init.pp
@@ -36,16 +36,6 @@ class icinga(
         ensure => 'present',
     }
 
-    # Setup icinga custom init script
-    # FIXME: This should be provided by the package
-    file { '/etc/init.d/icinga':
-        source  => 'puppet:///modules/icinga/icinga-init.sh',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0755',
-        require => Package['icinga'],
-    }
-
     file { '/etc/icinga/cgi.cfg':
         source  => 'puppet:///modules/icinga/cgi.cfg',
         owner   => 'icinga',
