c1146ca791fbb9703b811d2c2ca734eef7afc2aa
Daniel Zahn <dzahn@wikimedia.org>
2017-04-12 14:25:55 -0700

    dnsrec/icinga: add child/parent rel between monitor hosts
    dnsrec hosts exist in Icinga in 2 ways.
    
    First as regular hosts with default checks incl. ping and
    second there are "virtual hosts" added in with a monitoring::host
    in modules/dnsrecursor/manifests/monitor.pp which adds one host
    per IP address.
    
    This is for the "check_dns!www.wikipedia.org" check which is applied
    to BOTH the IPv4 and the IPv6 address of the host by:
    
    modules/role/manifests/dnsrecursor.pp:
    ::dnsrecursor::monitor { [ $::ipaddress, $::ipaddress6_eth0 ]: }
    
    Today we had a reinstall of one of these servers and even though the
    first kind of hosts were downtimed in Icinga we still did get alerts
    about those other virtual IP hosts not being reachable.
    
    This change should tell Icinga to consider these hosts as a child
    of their parent host.
    
    This should result in a status "UNREACHABLE" instead of "DOWN" because
    Icinga then understands it just can't get to them because the other host
    is down per its reachability logic.
    
    This in turn should mean that icinga-wm is not reporting it on IRC and spamming
    us because notification settings are to just report DOWN/UP/FLAP for hosts.
    (and if not we can adjust that)
    
    https://docs.icinga.com/latest/en/networkreachability.html#parentchildrelations
    
    Change-Id: I7ab92a15f51f038316fc5505ab0d737d93ee52d9

--- modules/dnsrecursor/manifests/monitor.pp
+++ modules/dnsrecursor/manifests/monitor.pp
@@ -3,6 +3,7 @@ define dnsrecursor::monitor() {
     # Monitoring
     monitoring::host { $title:
         ip_address => $title,
+        parent     => $::fqdn,
     }
     monitoring::service { "recursive dns ${title}":
         host          => $title,
