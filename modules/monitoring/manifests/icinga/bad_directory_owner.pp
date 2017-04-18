a27e703f0278df555ab3f45e075a9586918072a9
Daniel Zahn <dzahn@wikimedia.org>
2017-04-18 11:55:36 -0700

    monitoring:bad_directory_owner: set default timeout 10s
    It turns out this only took so long because tin was running
    on 200MHz. So we did not have to raise it and it's way faster
    again.
    
    Keeping the parameter though and just setting it to 10s
    which is the default for all NRPE checks using
    nrpe::monitor_service. Just passing it through.
    
    Change-Id: I4730d3a5c0595b7ab8231e1054450981801fef08

--- modules/monitoring/manifests/icinga/bad_directory_owner.pp
+++ modules/monitoring/manifests/icinga/bad_directory_owner.pp
@@ -6,12 +6,12 @@
 # $uid      - User id to check for
 # $gid      - Group id to check for
 # $interval - How often to check (default 10)
-# $timeout  - How long to wait before giving up (default 20s)
+# $timeout  - How long to wait before giving up (default 10s)
 define monitoring::icinga::bad_directory_owner (
     $uid      = 0,
     $gid      = 0,
     $interval = 10,
-    $timeout  = 20,
+    $timeout  = 10,
     ) {
 
     $safe_title = regsubst($title, '\/', '_', 'G')
