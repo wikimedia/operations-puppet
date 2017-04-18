8d4d6b5914b6055cedebbdd3524633838a3131b2
andrewbogott <abogott@wikimedia.org>
2017-04-18 15:16:00 -0500

    Keystone:  Use keystone-manage token_flush rather than a mysql call
    token_flush has been around for a while but didn't exist when we first
    started doing this.  Now that we have a proper upstream way to
    purge tokens that seems better than monkeying directly with the db.
    
    https://blueprints.launchpad.net/keystone/+spec/keystone-manage-token-flush
    
    Change-Id: Id6c454b339dc3110d4bd0431ed906bd58cd16652

--- modules/openstack/manifests/keystone/service.pp
+++ modules/openstack/manifests/keystone/service.pp
@@ -93,17 +93,14 @@ class openstack::keystone::service($keystoneconfig, $openstack_version=$::openst
     }
 
     if $::fqdn == hiera('labs_nova_controller') {
-        # Clean up expired keystone tokens, because keystone seems to leak them
-        $keystone_db_name = $keystoneconfig['db_name']
-        $keystone_db_user = $keystoneconfig['db_user']
-        $keystone_db_pass = $keystoneconfig['db_pass']
-        $keystone_db_host = $keystoneconfig['db_host']
+        # Clean up expired keystone tokens, because otherwise keystone leaves them
+        #  around forever.
         cron {
             'cleanup_expired_keystone_tokens':
                 ensure  => present,
                 user    => 'root',
                 minute  => 20,
-                command => "/usr/bin/mysql ${keystone_db_name} -h${keystone_db_host} -u${keystone_db_user} -p${keystone_db_pass} -e 'DELETE FROM token WHERE NOW() - INTERVAL 2 day > expires LIMIT 10000;'",
+                command => '/usr/bin/keystone-manage token_flush',
         }
 
         monitoring::service { 'keystone-http-35357':
