5ae01e6fb98e3c224e9c7e32cb0d0da2269bfa0f
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2017-04-19 15:16:47 -0700

    tools-proxy: Ensure kubelet is stopped on tools proxy nodes
    Bug: T163391
    Change-Id: I36295d6d280a12fa67d74e7911e731c655ed3e3f

--- modules/role/manifests/toollabs/k8s/webproxy.pp
+++ modules/role/manifests/toollabs/k8s/webproxy.pp
@@ -20,4 +20,11 @@ class role::toollabs::k8s::webproxy {
     class { '::k8s::proxy':
         master_host => $master_host,
     }
+
+    # The kubelet service is installed automatically as part of the kubernetes-node
+    # deb, we don't want it to be running on tools-proxy - so we are explicitly ensuring
+    # it's stopped
+    service { 'kubelet':
+        ensure => stopped,
+    }
 }
