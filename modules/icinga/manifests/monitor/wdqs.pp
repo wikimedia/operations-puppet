742af97de331ddb7cb08f7d662a301a02b463bab
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-04-20 18:13:14 +0200

    wdqs - monitor response times for both eqiad and codfw
    Now that we are in an active / active configuration, we should have the same
    monitoring on eqiad and codfw
    
    Change-Id: I93778c444fd554926cd787a895fd24a1bb4eb008

--- modules/icinga/manifests/monitor/wdqs.pp
+++ modules/icinga/manifests/monitor/wdqs.pp
@@ -4,7 +4,18 @@ class icinga::monitor::wdqs {
     # raise a warning / critical alert if response time was over 2 minutes / 5 minutes
     # more than 5% of the time during the last minute
     monitoring::graphite_threshold { 'wdqs-response-time':
-        description   => 'Response time of WDQS',
+        description   => 'Response time of WDQS codfw',
+        host          => 'wdqs.svc.codfw.wmnet',
+        metric        => 'varnish.codfw.backends.be_wdqs_svc_eqiad_wmnet.GET.p99',
+        warning       => 120000, # 2 minutes
+        critical      => 300000, # 5 minutes
+        from          => '10min',
+        percentage    => 5,
+        contact_group => 'wdqs-admins',
+    }
+
+    monitoring::graphite_threshold { 'wdqs-response-time':
+        description   => 'Response time of WDQS eqiad',
         host          => 'wdqs.svc.eqiad.wmnet',
         metric        => 'varnish.eqiad.backends.be_wdqs_svc_eqiad_wmnet.GET.p99',
         warning       => 120000, # 2 minutes
