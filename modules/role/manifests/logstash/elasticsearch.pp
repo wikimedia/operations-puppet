c4958e65e841875075ed9c5478e5ed9a07bcfe10
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-04-20 10:01:22 +0200

    logstash - raise elasticsearch shard alert threshold to 34
    After some discussion, this seems to be a good compromise. It allows to loose
    one node (1/3 of the shards) without raising an alert.
    
    Some cleanup of the check_elasticsearch.py script is done as well:
    
    * drop the support of "%"
    * treat all threshold as ratio
    
    Previously, that script treated ">=0.1%" as a check that more than 10% of
    shards were unassigned, which is a bit misleading.
    
    Change-Id: I352e7230c6bd42f5d1a2fa84f33675e0a6ce8225

--- modules/role/manifests/logstash/elasticsearch.pp
+++ modules/role/manifests/logstash/elasticsearch.pp
@@ -5,9 +5,16 @@
 #
 class role::logstash::elasticsearch {
     include ::standard
-    include ::elasticsearch::nagios::check
     include ::elasticsearch::monitor::diamond
     include ::base::firewall
+
+    # the logstash cluster has 3 data nodes, and each shard has 3 replica (each
+    #shard is present on each node). If one node is lost, 1/3 of the shards
+    # will be unassigned, with no way to reallocate them on another node, which
+    # is fine and should not raise an alert. So threshold needs to be > 1/3.
+    class { '::elasticsearch::nagios::check':
+        threshold => '>=0.34',
+    }
 
     if $::standard::has_ganglia {
         include ::elasticsearch::ganglia
