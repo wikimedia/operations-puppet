23cc75dcdc35e23377a10de4b9b0cb9b9d693964
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-04-21 14:00:54 +0200

    Puppet: run-puppet-agent, add --failed-only option
    - Add the --failed-only option to the run-puppet-agent script to run
      puppet only if it's enabled and failed in the last run.
      This should allow to quickly and easily recover from deploys that
      causes a puppet failure on a large number of hosts. After fixing the
      underlying issue, it should be enough to run from one of the cumin
      masters the command:
    
        sudo cumin -b 25 -s 1 -p 95 '*' 'run-puppet-agent --failed-only'
    
      to rolling force a puppet run only on the hosts where it failed.
    
    Change-Id: Ide1246b256a4fd1b48ed660b3e87f77e91c1b5b4

--- modules/base/files/puppet/puppet-common.sh
+++ modules/base/files/puppet/puppet-common.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 # Common bash functions and variables to use for scripts
-PUPPETLOCK=$(puppet agent --configprint agent_catalog_run_lockfile)
-PUPPET_DISABLEDLOCK=$(puppet agent --configprint agent_disabled_lockfile)
+PUPPET_CONFIG="$(puppet config print)"
+
+# Function to get a puppet config variable
+# Parameters:
+#  $1: the name of the variable to get from the config
+get_puppet_config() {
+    # Using xargs to trim the string
+    echo "${PUPPET_CONFIG}" | grep "${1}" | cut -d "=" -f2- | xargs
+}
+
+PUPPETLOCK="$(get_puppet_config agent_catalog_run_lockfile)"
+PUPPET_DISABLEDLOCK="$(get_puppet_config agent_disabled_lockfile)"
+PUPPET_SUMMARY="$(get_puppet_config lastrunfile)"
+PUPPET_REPORT="$(get_puppet_config lastrunreport)"
 
 # Function to test if puppet is running or not
 puppet_is_running() {
@@ -35,5 +47,25 @@ wait_for_puppet() {
         sleep 10
     done
     # If puppet is still running at this point, report an error
+    return 1
+}
+
+last_run_success() {
+    local failures
+    local status
+
+    failures=$(ruby -e "require 'safe_yaml'; SafeYAML::OPTIONS[:default_mode] = :safe; puts YAML.load(STDIN.read)['events']['failure']" < "${PUPPET_SUMMARY}")
+
+    if [[ "${failures}" -eq "0" ]]; then
+        # No failures in the summary, check the status too
+        status=$(ruby -e "require 'safe_yaml'; SafeYAML::OPTIONS[:default_mode] = :safe; puts YAML.load(STDIN.read)['status']" < "${PUPPET_REPORT}")
+
+        if [[ "${status}" != "failed" ]]; then
+            # No failed status
+            return 0
+        fi
+    fi
+
+    # Either puppet has failures or failed to run
     return 1
 }
