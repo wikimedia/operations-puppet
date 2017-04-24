093fbb8e0df9a57ba03546b88ef40f8d4db88e12
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-04-24 11:23:36 +0200

    Setup rsync server to sync home dirs from terbium to wasat
    Temporary change for reimage, will be reverted once completed.
    
    Change-Id: I0be429d13fb7f513e8e7853d85243666b0179668

---
+++ modules/role/manifests/mediawiki/migrate.pp
@@ -0,0 +1,17 @@
+class role::mediawiki::migrate {
+
+    $sourceip='10.64.32.13'
+
+    ferm::service { 'mw-maintenance-migration-rsync':
+        proto  => 'tcp',
+        port   => '873',
+        srange => "${sourceip}/32",
+    }
+
+    include rsync::server
+    rsync::server::module { 'terbium-home':
+        path        => '/home',
+        read_only   => 'no',
+        hosts_allow => $sourceip,
+    }
+}
