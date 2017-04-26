79068d44f599d4b954988d08ca6c998f178a0be1
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-04-26 12:05:50 +0200

    profile: introduce swift::storage::labs
    Labs uses lvm for its virtual disks, thus introduce a labs-specific profile to
    create the required LV and symlink it into /dev and simulate a real
    disk/partition.
    
    Bug: T162247
    Change-Id: If292ad0069ec5083959e963620bd25ad4127bf37

---
+++ modules/profile/manifests/swift/storage/labs.pp
@@ -0,0 +1,27 @@
+# Create LV needed by swift and instruct udev to symlink it to its expected
+# location to simulate a disk partition.
+class profile::swift::storage::labs {
+  include ::lvm
+
+  lvm::logical_volume { 'lv-a1':
+    volume_group => 'vd',
+    createfs     => false,
+    mounted      => false,
+    extents      => '80%FREE',
+  }
+
+  file { '/etc/udev/rules.d/99-swift-lvm.rules':
+    ensure  => present,
+    user    => 'root',
+    group   => 'root',
+    mode    => '0444',
+    content => 'ENV{DM_LV_NAME}=="lv-a1", ENV{DM_VG_NAME}=="vd", SYMLINK+="lv-a1"',
+    notify  => Exec['swift_udev_reload'],
+    require => Lvm::Logical_volume['lv-a1'],
+  }
+
+  exec { 'swift_udev_reload':
+    command     => '/sbin/udevadm control --reload && /sbin/udevadm trigger',
+    refreshonly => true,
+  }
+}
