17925e6c88ff4824fa63c1432269cb68ac6a4c3e
Timo Tijhof <krinklemail@gmail.com>
2017-04-26 15:18:56 -0700

    varnish: Convert errorpage into re-usable template
    * Move varnish/files/errorpage.html
      to mediawiki/templates/errorpage.html.erb.
    * Turn fixed strings into variables.
    
    Bug: T113114
    Change-Id: I6b63bcef8d625c80d7391165237d2797999bf92b

--- modules/varnish/manifests/common/vcl.pp
+++ modules/varnish/manifests/common/vcl.pp
@@ -15,11 +15,17 @@ class varnish::common::vcl {
         content => template('varnish/analytics.inc.vcl.erb'),
     }
 
+    $errorpage  = {
+        title       => 'Wikimedia Error',
+        content     => '<p>Our servers are currently under maintenance or experiencing a technical problem. Please <a href="" title="Reload this page" onclick="window.location.reload(false); return false">try again</a> in a few&nbsp;minutes.</p><p>See the error message at the bottom of this page for more&nbsp;information.</p>',
+        # Placeholder "%error%" substituted at runtime in errorpage.inc.vcl
+        footer      => '<p>If you report this error to the Wikimedia System Administrators, please include the details below.</p><p class="text-muted"><code>%error%</code></p>',
+    }
     file { '/etc/varnish/errorpage.html':
         owner  => 'root',
         group  => 'root',
         mode   => '0444',
-        source => 'puppet:///modules/varnish/errorpage.html',
+        source => template('mediawiki/errorpage.html.erb'),
     }
 
     # VTC tests
