b5fa0d22952215b97eefb34af589b93b83722f76
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-04-26 13:48:54 +0300

    ores: Move the application (web+worker) to profiles
    Move the old web and worker role classes that comprised the ores roles
    to profiles and include them in the scb role class. That makes the code
    more conformant to our Puppet coding guidelines, decouples more
    effectively the worker from the web app and recouples them at the role
    level.
    
    Change-Id: I2a03d911a3a3bcd2f11658c8fbbbaede4ba473ea

--- modules/role/manifests/scb.pp
+++ modules/role/manifests/scb.pp
@@ -1,14 +1,15 @@
 # "Compendium" class for nodes supporting various *oid services
 # This class is an intermediate step to better design
 class role::scb {
+    include ::profile::ores::worker
+    include ::profile::ores::web
+
     include role::mobileapps
     include role::mathoid
     include role::graphoid
     include role::citoid
     include role::cxserver
     include role::changeprop
-    include role::ores::web
-    include role::ores::worker
     include role::apertium
     include role::eventstreams
     include role::pdfrender
