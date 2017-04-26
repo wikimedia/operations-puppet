7a1e95eec1b598f9518408bbdc5c05c00363d304
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-04-26 16:42:31 +0300

    ores: Add twemproxy support
    Implement a sharding policy for ORES's redis databases. This is expected
    to increase throughput as well as redundancy. The former is clear why,
    the latter is due to the way ORES handles the redis databases where data
    loss due to a redis db being down, while bad, is not a SPOF, at least
    not directly. That is, it might lead to an ORES overload and thus to an
    indirect outage, but not a direct outage. That, if needed can be solved,
    for the current traffic at least, by adding more redis databases.
    
    Twemproxy listens over TCP sockets in this setup since ORES due to a set
    of bugs related to celery and python-redis discussed in T167149, can't
    use unix sockets.
    
    * Amend the hieradata for codfw,eqiad,common
    * Split per DC the other redises
    * Move the redis password one level up in the hierarchy so that it is
    clear that it is shared by more than one profile class
    
    Bug: T122676
    Change-Id: I97f535a5e7c8eb34b9c0555d3c84d55c59703ea5

--- modules/profile/manifests/ores/web.pp
+++ modules/profile/manifests/ores/web.pp
@@ -1,3 +1,9 @@
-class profile::ores::web {
-    include ::ores::web
+class profile::ores::web(
+    $redis_host = hiera('profile::ores::web::redis_host'),
+    $redis_password = hiera('profile::ores::web::redis_password'),
+){
+    class { '::ores::web':
+        redis_password => $redis_password,
+        redis_host     => $redis_host,
+    }
 }
