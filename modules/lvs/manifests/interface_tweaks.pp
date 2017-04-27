501e8e7ff364ac68df95b22e8e456b51b4dc2f5c
Faidon Liambotis <faidon@wikimedia.org>
2017-04-28 00:56:08 +0300

    lvs: remove support for <= trusty
    All LVS systems are now running jessie, so remove legacy/dead code that
    was there for older systems. This removes the only "if ! os_version()"
    occurence in the tree which is probably an antipattern as it's harder to
    grep for.
    
    Change-Id: I68acf28e70f7aca5f98525c8faef41aa76f5ac83

--- modules/lvs/manifests/interface_tweaks.pp
+++ modules/lvs/manifests/interface_tweaks.pp
@@ -1,29 +1,9 @@
 # This gathers up various ethernet settings for functional
 #  fixups and optimizations specific to LVS in a single location
-# Note this define contains an implicit assumption that eth0
-#  is the primary interface and is treated differently wrt
-#  forcing GRO off...
 
 define lvs::interface_tweaks($bnx2x=false, $txqlen=false, $rss_pattern=false) {
-    if ! os_version('debian >= jessie') {
-        # Disable GRO (generically incompatible with LVS due to kernel issues, I believe this
-        #   is now fixed upstream for both ipv4 and ipv6 as of kernel 3.7 or higher, but
-        #   that idea needs testing!)
-        interface::offload { "${name} gro": interface => $name, setting => 'gro', value => 'off' }
-        if $name != 'eth0' {
-            # Make sure GRO is off for the non-primary interfaces...
-            interface::manual { $name: interface => $name, before => Interface::Offload["${name} gro"] }
-        }
-
-        # bnx2x is buggy with TPA (LRO) + LVS
-        if $bnx2x {
-            interface::offload { "${name} lro": interface => $name, setting => 'lro', value => 'off' }
-        }
-    }
-    else {
-        if $name != 'eth0' {
-            interface::manual { $name: interface => $name }
-        }
+    if $name != 'eth0' {
+        interface::manual { $name: interface => $name }
     }
 
     # RSS/RPS/XPS-type perf stuff ( https://www.kernel.org/doc/Documentation/networking/scaling.txt )
