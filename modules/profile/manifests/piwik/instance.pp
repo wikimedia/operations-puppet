15c196a821fa3b7b7ff1e7a00bc169c7940e3bc6
elukey <ltoscano@wikimedia.org>
2017-04-27 17:11:50 +0200

    Set trusted hosts for Piwik in its profile
    Bug: T159136
    Change-Id: I51b4b412ea62b83e5b7f05c076196db4a8b977dd

--- modules/profile/manifests/piwik/instance.pp
+++ modules/profile/manifests/piwik/instance.pp
@@ -7,12 +7,13 @@ class profile::piwik::instance (
     $admin_username    = hiera('profile::piwik::admin_username'),
     $admin_password    = hiera('profile::piwik::admin_password'),
     $password_salt     = hiera('profile::piwik::password_salt'),
-    $trusted_hosts = ['piwik.wikimedia.org', 'wikimediafoundation.org'],
+    $trusted_hosts     = ['piwik.wikimedia.org', 'wikimediafoundation.org'],
 ) {
     class { 'piwik':
         database_password => $database_password,
         admin_username    => $admin_username,
         admin_password    => $admin_password,
         password_salt     => $password_salt,
+        trusted_hosts     => $trusted_hosts,
     }
 }
