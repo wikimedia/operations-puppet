e974e3200c5f95822ea38b0022a304324943416c
Faidon Liambotis <faidon@wikimedia.org>
2017-04-28 01:57:37 +0300

    Add a new interface::alias definition
    Add a new interface::alias definition that can be used to add secondary
    IPs (like service IPs) to host interfaces. This is a light wrapper
    around interface::ip, but with the following things hardcoded:
    - It uses $facts['interface_primary'] for the $interface by default (but
      leaves it configurable). No more "eth0" all over the tree.
    - It gets both an IPv4 and IPv6 argument, as in the majority of the
      existing cases in the tree, users want to pass both and this makes it
      easier (both are optional).
    - It uses /32 and /128 for IPv4 and IPv6 as the prefix length
      respectively. This is not configurable.
    
    Use it for the wiki-mail-eqiad and wiki-mail-codfw service IPs to
    demonstrate that it works. More patches will follow.
    
    Change-Id: I26a0f6d882fb25bb6603779afdc878ce867b2713

---
+++ modules/interface/manifests/alias.pp
@@ -0,0 +1,25 @@
+# Simplified version of interface::ip, only used for adding secondary IPs to hosts
+#
+define interface::alias(
+  $interface=$facts['interface_primary'],
+  $ipv4=undef,
+  $ipv6=undef,
+) {
+    if $ipv4 != undef {
+        interface::ip { "${title} ipv4":
+            interface => $interface,
+            address   => $ipv4,
+            prefixlen => 32,
+        }
+    }
+
+    if $ipv6 != undef {
+        interface::ip { "${title} ipv6":
+            interface => $interface,
+            address   => $ipv6,
+            prefixlen => 128,
+            # mark as deprecated = never pick this address unless explicitly asked
+            options   => 'preferred_lft 0',
+        }
+    }
+}
