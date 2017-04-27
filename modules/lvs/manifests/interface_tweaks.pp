4c6d538200cba29d765dbb1ee8c510e74c5e69c5
Faidon Liambotis <faidon@wikimedia.org>
2017-04-28 00:58:46 +0300

    interface/lvs: add an $interface parameter, remove hardcoded eth0
    Reformat many of the similar modules/interface manifests and add an
    $interface parameter that defaults to $name, rather than using
    $name/$title all over the place.
    
    Remove the hardcoded comparison with eth0, and use the interface_primary
    fact instead.
    
    Bug: T163196
    Change-Id: I619037a0adaf827c7a511468a7aa428d3c840cde

--- modules/lvs/manifests/interface_tweaks.pp
+++ modules/lvs/manifests/interface_tweaks.pp
@@ -1,20 +1,41 @@
 # This gathers up various ethernet settings for functional
 #  fixups and optimizations specific to LVS in a single location
 
-define lvs::interface_tweaks($bnx2x=false, $txqlen=false, $rss_pattern=false) {
-    if $name != 'eth0' {
-        interface::manual { $name: interface => $name }
+define lvs::interface_tweaks(
+  $interface=$name,
+  $bnx2x=false,
+  $txqlen=false,
+  $rss_pattern=false,
+) {
+    if $interface != $facts['interface_primary'] {
+        interface::manual { $name:
+            interface => $interface,
+        }
     }
 
     # RSS/RPS/XPS-type perf stuff ( https://www.kernel.org/doc/Documentation/networking/scaling.txt )
-    if $rss_pattern { interface::rps { $name: rss_pattern => $rss_pattern } }
+    if $rss_pattern {
+        interface::rps { $name:
+            interface   => $interface,
+            rss_pattern => $rss_pattern,
+        }
+    }
 
     # Optional larger TX queue len for 10Gbps+
-    if $txqlen { interface::txqueuelen { $name: len => $txqlen } }
+    if $txqlen {
+        interface::txqueuelen { $name:
+            interface => $interface,
+            len       => $txqlen,
+        }
+    }
 
     # bnx2x-specific stuff
     if $bnx2x {
         # Max for bnx2x/BCM57800, seems to eliminate the spurious rx drops under heavy traffic
-        interface::ring { "${name} rxring": interface => $name, setting => 'rx', value => 4078 }
+        interface::ring { "${name} rxring":
+            interface => $interface,
+            setting   => 'rx',
+            value     => 4078,
+        }
     }
 }
