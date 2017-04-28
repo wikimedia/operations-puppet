1c62ead668993de0a0662d31add93c040e63502f
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-04-28 11:54:39 +0200

    scap::dsh: transition to using confd
    Instead of calling conftool from puppet, which is typically a bit goofy,
    build the dsh list that depend on conftool using confd instead.
    
    Change-Id: If320eecfd2a6164387e06a120012d9a603175a87

--- modules/scap/manifests/dsh/group.pp
+++ modules/scap/manifests/dsh/group.pp
@@ -13,12 +13,26 @@ define scap::dsh::group($hosts=undef, $conftool=undef,) {
         default => $hosts,
     }
 
-    file { "/etc/dsh/group/${title}":
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        content => template('scap/dsh/dsh-group.erb'),
+    if $conftool {
+        # Usual dirty trick to avoid a parser function
+        $keys = split(
+            inline_template('<%= @conftool.map{|x| [x["cluster"], x["service"]].join "/"}.join("|")'),
+            '|')
+
+        confd::file { "/etc/dsh/group/${title}":
+            ensure     => present,
+            prefix     => '/pools',
+            watch_keys => $keys,
+            content    => template('scap/dsh/dsh-group-conftool.tpl.erb')
+        }
+    } else {
+        file { "/etc/dsh/group/${title}":
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            content => template('scap/dsh/dsh-group.erb'),
+        }
     }
 
 }
