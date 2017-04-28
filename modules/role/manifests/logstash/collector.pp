9367e498203565276370d9ae3c9974b4620e2b4c
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-04-28 12:35:15 +0200

    [WIP] Send 5xx from kafkatee to logstash
    Straw man patch to send 5xx read from webrequest kafka topics with kafkatee to
    logstash.
    
    The 'type' field is used to distinguish existing 'json_lines' filters and be
    able to further filter webrequest in the future (e.g. to redact fields)
    
    Bug: T149451
    Change-Id: I451160b0089daf8242a8b49737052f61300b1e7e

--- modules/role/manifests/logstash/collector.pp
+++ modules/role/manifests/logstash/collector.pp
@@ -152,6 +152,11 @@ class role::logstash::collector (
         priority => 50,
     }
 
+    logstash::conf { 'filter_webrequest':
+        source   => 'puppet:///modules/role/logstash/filter-webrequest.conf',
+        priority => 50,
+    }
+
     ## Global post-processing (70)
 
     logstash::conf { 'filter_add_normalized_message':
