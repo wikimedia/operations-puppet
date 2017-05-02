29e3a4768efe552c0ad317e290fc81c5d8555718
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-05-02 16:47:40 +0200

    profile::etcd::replication: make replication errors page
    Change-Id: I85b4b843177ab72ccb53ade9130c347c537f03bd

--- modules/profile/manifests/etcd/replication.pp
+++ modules/profile/manifests/etcd/replication.pp
@@ -43,6 +43,7 @@ class profile::etcd::replication(
         monitoring::service{ 'etcd_replication_lag':
             description   => 'Etcd replication lag',
             check_command => "check_http_url_for_regexp_on_port!${::fqdn}!${etcdmirror_web_port}!/lag!'^(-1|[0-5])[^0-9]+'",
+            critical      => true,
         }
     }
 
