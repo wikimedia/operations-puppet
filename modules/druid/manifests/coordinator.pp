4506f132991ee5fa854db9658f38f30be1bda68b
Andrew Otto <acotto@gmail.com>
2017-05-03 14:55:19 -0400

    Changes needed for upgrading to Druid 0.10
    Druid 0.10 depends on Java 8.  The package should install it as a dependency
    when installing, but it will not be the default Java.  Setting
    JAVA_HOME explicitly in daemon env.sh files.
    
    Reduce middlemanager indexer peon task druid.processing.buffer.sizeBytes to 356515840
    for 'non-zero default value for druid.processing.numMergeBuffers'.
    
    Change default extensions to ones included with Druid 0.10.
    
    This reverts a revert of https://gerrit.wikimedia.org/r/#/c/351691.
    
    We need to upgrade Hadoop cluster to Java 8 before proceeding with this change.
    
    Bug: T164008
    Change-Id: Ic9a091c3b601dd382b66770baf1e2494fb77eef9

--- modules/druid/manifests/coordinator.pp
+++ modules/druid/manifests/coordinator.pp
@@ -59,6 +59,7 @@ class druid::coordinator(
     }
 
     $default_env = {
+        'JAVA_HOME'            => $::druid::java_home,
         'JMX_PORT'             => 9662,
         'DRUID_HEAP_OPTS'      => '-Xmx128m -Xms128m',
         'DRUID_EXTRA_JVM_OPTS' => '-Dderby.stream.error.file=/var/log/druid/coordinator-derby.log',
