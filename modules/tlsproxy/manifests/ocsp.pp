0620b9b3c07fd5ac057ccb28996ee6bd109f73f3
Brandon Black <bblack@wikimedia.org>
2017-05-05 18:41:32 +0000

    Align OCSP file freshness check with new cron timing
    Should've been fixed during the cron fixup in 13da1ecd
    
    Bug: T164579
    Change-Id: Ib30bc7993f29af84d8b85a75c759d367770566a5

--- modules/tlsproxy/manifests/ocsp.pp
+++ modules/tlsproxy/manifests/ocsp.pp
@@ -15,17 +15,17 @@ class tlsproxy::ocsp {
     # it will generate warnings if you don't clean up old /var/cache/ocsp/
     # entries after removing a tlsproxy::ocsp_stapler cert from a server!
     #
-    # The cron above attempts to get fresh data every hour, and a good fresh
-    # fetch of data has a 12H lifetime with the windows we're seeing from
-    # GlobalSign today.
+    # The cron above attempts to get fresh data once a day, and a good fresh
+    # fetch of data has a 4-7 day lifetime depending on the vendor (GlobalSign
+    # or Digicert)
     #
-    # The crit/warn values of 18300 and 11100 correspond to "5h5m" and
-    # "3h5m", so those are basically warning if 3 updates in a row failed
-    # for a given cert, and critical if 5 updates in a row failed (at which
-    # point we have ~7h left to fix the situation before the validity window
+    # The crit/warn values of 259500 and 86700 correspond to "1d5m" and
+    # "3d5m", so those are basically warning if 1 updates in a row failed
+    # for a given cert, and critical if 3 updates in a row failed (at which
+    # point we have ~24h left to fix the situation before the validity window
     # expires).
 
-    $check_args = '-c 18300 -w 11100 -d /var/cache/ocsp -g "*.ocsp"'
+    $check_args = '-c 259500 -w 86700 -d /var/cache/ocsp -g "*.ocsp"'
     nrpe::monitor_service { 'ocsp-freshness':
         description  => 'Freshness of OCSP Stapling files',
         nrpe_command => "/usr/lib/nagios/plugins/check-fresh-files-in-dir.py ${check_args}",
