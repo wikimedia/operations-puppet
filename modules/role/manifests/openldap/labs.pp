ed5b9aec57e2484543652ee03891cc7ea1157b5d
Brandon Black <bblack@wikimedia.org>
2017-05-05 13:39:37 +0000

    fqdn_rand cleanup: fix bad hour=>23 and minute=>59
    fqdn_rand(x) means [0, x)
    
    Change-Id: I698443919406af3541e37211dc1c8466ab9ffb01

--- modules/role/manifests/openldap/labs.pp
+++ modules/role/manifests/openldap/labs.pp
@@ -49,7 +49,7 @@ class role::openldap::labs {
     # restart slapd if it uses more than 50% of memory (T130593)
     cron { 'restart_slapd':
         ensure  => present,
-        minute  => fqdn_rand(59, $title),
+        minute  => fqdn_rand(60, $title),
         command => "/bin/ps -C slapd -o pmem= | awk '{sum+=\$1} END { if (sum <= 50.0) exit 1 }' \
         && /bin/systemctl restart slapd >/dev/null 2>/dev/null",
     }
