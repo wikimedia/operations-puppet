0450ea36e36c1eaade2ca827473966d476652a01
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2017-05-09 13:28:15 -0700

    sge: Revamp queue configuration puppet
    Change-Id: I424f59aab5785991bee6324a46cb7f1872f4baf9

--- modules/role/manifests/toollabs/node/compute/dedicated.pp
+++ modules/role/manifests/toollabs/node/compute/dedicated.pp
@@ -21,6 +21,20 @@ class role::toollabs::node::compute::dedicated($dedicated_tool = $::node_dedicat
             description => "Computation node dedicated to ${::labsproject}.${dedicated_tool}",
         }
 
+        gridengine::queue { "${dedicated_tool}":
+            config_path           => '/var/lib/gridengine/etc/queues',
+            hostlist              => 'NONE', #FIXME: Make hostgroups for these queues
+            seq_no                => 6,
+            np_load_avg_threshold => 2.0,
+            priority              => 10,
+            qtype                 => 'BATCH',
+            rerun                 => true,
+            slots                 => 1000,
+            ckpt_list             => 'continuous',
+            owner_list            => $dedicated_tool,
+            user_lists            => $dedicated_tool,
+        }
+
     } else {
 
         system::role { 'toollabs::node::compute::dedicated':
