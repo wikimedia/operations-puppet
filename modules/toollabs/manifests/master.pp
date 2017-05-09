ee12cb43d76d6dd439ee4ddbc5954286f51dd209
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2017-05-09 13:28:15 -0700

    sge: Revamp queue configuration puppet
    Change-Id: I424f59aab5785991bee6324a46cb7f1872f4baf9

--- modules/toollabs/manifests/master.pp
+++ modules/toollabs/manifests/master.pp
@@ -4,9 +4,56 @@ class toollabs::master inherits toollabs {
 
     include ::gridengine::master
     include ::toollabs::infrastructure
-    include ::toollabs::queue::continuous
-    include ::toollabs::queue::task
 
+    $etcdir = '/var/lib/gridengine/etc'
+
+    # Set up queues
+    $queue_config_defaults = {
+        'conf_path' => '/var/lib/gridengine/etc/queues',
+    }
+
+    $queue_config = {
+        'task'             => {
+            hostlist              => '@general',
+            seq_no                => 0,
+            terminate_method      => '/usr/local/bin/jobkill $job_pid',
+        },
+        'continuous'       => {
+            hostlist              => '@general',
+            seq_no                => 1,
+            priority              => 10,
+            qtype                 => 'BATCH',
+            rerun                 => true,
+            ckpt_list             => 'continuous',
+            terminate_method      => '/usr/local/bin/jobkill $job_pid',
+        },
+        'webgrid-lighttpd' => {
+            hostlist              => '@webgrid',
+            seq_no                => 2,
+            np_load_avg_threshold => 2.75,
+            qtype                 => 'BATCH',
+            rerun                 => true,
+            slots                 => 256,
+            terminate_method      => 'SIGTERM',
+            epilog                => '/usr/local/bin/portreleaser',
+        },
+        'webgrid-generic'  => {
+            # FIXME: webgrid-generic is set up with a list of hosts instead of hostgroups.
+            # It should just be another hostgroup
+            hostlist              => 'NONE',
+            seq_no                => 3,
+            np_load_avg_threshold => 2.75,
+            qtype                 => 'BATCH',
+            rerun                 => true,
+            slots                 => 256,
+            terminate_method      => 'SIGTERM',
+            epilog                => '/usr/local/bin/portreleaser',
+        },
+    }
+
+    create_resources(gridengine::queue, $queue_config, $queue_config_defaults)
+
+    # Set up complexes
     gridengine_resource { 'h_vmem':
         ensure      => present,
         requestable => 'FORCED',
