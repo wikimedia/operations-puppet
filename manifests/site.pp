21d7c5f6538416ea5e033aaefe806c83648da0a2
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-05-09 17:24:08 +0200

    restbase: migration to role/profile for the dev cluster
    * Created profiles for restbase/cassandra, loosely couple to the old
      corresponding roles
    * Unified the logic for cassandra::seeds and cassandra::instances by
      using a shared data structure and a template to modify items
    * Created a base role for restbase, with no LVS, to include in all the
      other roles.
    * Created a role for restbase::dev_cluster and defined its hiera
      variables
    * Consequently dropped all of the data in regex.yaml and in
      hosts/* (apart from rack positioning) for the dev cluster
    
    Change-Id: I817204597d421c3bc52a7778e992bd0c1dc473c7

--- manifests/site.pp
+++ manifests/site.pp
@@ -2460,7 +2460,7 @@ node /^restbase-test200[1-3]\.codfw\.wmnet$/ {
 
 # cassandra/restbase dev cluster
 node /^restbase-dev100[1-3]\.eqiad\.wmnet$/ {
-    role(restbase::server, cassandra)
+    role(restbase::dev_cluster)
 }
 
 # network insights (netflow/pmacct, etc.)
