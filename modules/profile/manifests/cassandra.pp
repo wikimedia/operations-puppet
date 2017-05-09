e4c76a7abeb2a8301122f5fbbb6b32091deb69e2
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-05-09 17:24:08 +0200

    restbase: migration to role/profile for the dev cluster
    * Created profiles for restbase/cassandra, loosely couple to the old
      corresponding roles
    * Unified the logic for cassandra::seeds and cassandra::instances by
      using a shared data structure and a template to modify items
    * Created a role for restbase::dev_cluster and defined its hiera
      variables
    * Consequently dropped all of the data in regex.yaml and in
      hosts/* (apart from rack positioning)
    * LVS is still included but might probably be removed from here
    
    Change-Id: I817204597d421c3bc52a7778e992bd0c1dc473c7

---
+++ modules/profile/manifests/cassandra.pp
@@ -0,0 +1,98 @@
+# == Class profile::cassandra
+#
+class profile::cassandra(
+    $all_instances = hiera('profile::cassandra::instances'),
+    $rack = hiera('profile::cassandra::rack'),
+    $cassandra_settings = hiera('profile::cassandra::settings'),
+    $metrics_blacklist = hiera('profile::cassandra::metrics_blacklist'),
+    $metrics_whitelist = hiera('profile::cassandra::metrics_whitelist'),
+    $graphite_host = hiera('graphite_host'),
+    $prometheus_nodes = hiera('prometheus_nodes'),
+) {
+    include ::passwords::cassandra
+    $instances = $all_instances[$::fqdn]
+    # We get the cassandra seeds from $all_instances, with a template hack
+    # This is preferred over a very specialized parser function.
+    $seeds = split(template('profile/cassandra/seeds.erb'), '\|')
+
+    $base_settings = {
+        'instances' => $instances,
+        'rack'      => $rack,
+        'seeds'     => $seeds,
+    }
+    $cassandra_real_settings = merge($base_settings, $cassandra_settings)
+
+    create_resources('class', {'::cassandra' => $cassandra_real_settings})
+
+
+    class { '::cassandra::metrics':
+        graphite_host => $graphite_host,
+        whitelist     => $metrics_whitelist,
+        blacklist     => $metrics_blacklist,
+    }
+    class { '::cassandra::logging': }
+    class { '::cassandra::twcs': }
+
+    class { '::cassandra::sysctl':
+        # Queue page flushes at 24MB intervals
+        vm_dirty_background_bytes => 25165824,
+    }
+
+    if $instances {
+        $instance_names = keys($instances)
+        ::cassandra::instance::monitoring{ $instance_names: }
+    } else {
+        $default_instances = {
+            'default' => {
+                'listen_address' => $::cassandra::listen_address,
+        }}
+        ::cassandra::instance::monitoring{ 'default':
+            instances => $default_instances,
+        }
+    }
+
+    # temporary collector, T78514
+    diamond::collector { 'CassandraCollector':
+        ensure => absent,
+    }
+
+    system::role { 'role::cassandra':
+        description => 'Cassandra server',
+    }
+
+    $cassandra_hosts_ferm = join($seeds, ' ')
+    $prometheus_nodes_ferm = join($prometheus_nodes, ' ')
+
+    # Cassandra intra-node messaging
+    ferm::service { 'cassandra-intra-node':
+        proto  => 'tcp',
+        port   => '7000',
+        srange => "@resolve((${cassandra_hosts_ferm}))",
+    }
+    # Cassandra intra-node SSL messaging
+    ferm::service { 'cassandra-intra-node-ssl':
+        proto  => 'tcp',
+        port   => '7001',
+        srange => "@resolve((${cassandra_hosts_ferm}))",
+    }
+    # Cassandra JMX/RMI
+    ferm::service { 'cassandra-jmx-rmi':
+        proto  => 'tcp',
+        # hardcoded limit of 4 instances per host
+        port   => '7199:7202',
+        srange => "@resolve((${cassandra_hosts_ferm}))",
+    }
+    # Cassandra CQL query interface
+    ferm::service { 'cassandra-cql':
+        proto  => 'tcp',
+        port   => '9042',
+        srange => "@resolve((${cassandra_hosts_ferm}))",
+    }
+    # Prometheus jmx_exporter for Cassandra
+    ferm::service { 'cassandra-jmx_exporter':
+        proto  => 'tcp',
+        port   => '7800',
+        srange => "@resolve((${prometheus_nodes_ferm}))",
+    }
+
+}
