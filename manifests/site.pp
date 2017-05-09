e1dd52b2e8e0b312ac880ecf175222014c77a23b
Brandon Black <bblack@wikimedia.org>
2017-05-09 14:16:53 +0000

    maps->upload: delete maps-specific things
    If previous commit is a success, we can then merge this to clean
    up the old cache_maps cluster from puppet and turn the cache_maps
    machines into spares (for now).
    
    Bug: T164608
    Change-Id: Ic6f1863540b9e14a62b0199767560a3b92c41f78

--- manifests/site.pp
+++ manifests/site.pp
@@ -264,8 +264,7 @@ node /^cp10(45|5[18]|61)\.eqiad\.wmnet$/ {
 }
 
 node 'cp1046.eqiad.wmnet', 'cp1047.eqiad.wmnet', 'cp1059.eqiad.wmnet', 'cp1060.eqiad.wmnet' {
-    interface::add_ip6_mapped { 'main': }
-    role(cache::maps, ipsec)
+    role(spare::system)
 }
 
 node /^cp10(4[89]|50|6[234]|7[1-4]|99)\.eqiad\.wmnet$/ {
@@ -289,8 +288,7 @@ node /^cp20(0[258]|1[147]|2[0246])\.codfw\.wmnet$/ {
 }
 
 node /^cp20(0[39]|15|21)\.codfw\.wmnet$/ {
-    interface::add_ip6_mapped { 'main': }
-    role(cache::maps, ipsec)
+    role(spare::system)
 }
 
 node /^cp20(06|1[28]|25)\.codfw\.wmnet$/ {
@@ -299,8 +297,7 @@ node /^cp20(06|1[28]|25)\.codfw\.wmnet$/ {
 }
 
 node /^cp300[3-6]\.esams\.wmnet$/ {
-    interface::add_ip6_mapped { 'main': }
-    role(cache::maps, ipsec)
+    role(spare::system)
 }
 
 node /^cp30(0[789]|10)\.esams\.wmnet$/ {
@@ -342,8 +339,7 @@ node /^cp40(0[89]|1[0678])\.ulsfo\.wmnet$/ {
 }
 
 node /^cp40(1[129]|20)\.ulsfo\.wmnet$/ {
-    interface::add_ip6_mapped { 'main': }
-    role(cache::maps, ipsec)
+    role(spare::system)
 }
 
 # temporary entry for testing new cache node hardware setup...
