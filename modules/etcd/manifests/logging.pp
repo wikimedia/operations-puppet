1a22ffe66316e43fc44362fd9219efd1903b3c6d
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-05-09 12:53:23 +0000

    Revert "etcd: use logrotate::conf for logrotate"
    Breaks puppet with an
    
       Invalid relationship: Rsyslog::Conf[etcd] {require => 
       File[/etc/logrotate.d/etcd}, because File doesn't seem to 
       be in the catalogue
    
    This reverts commit c069081def926877434a1d712a2c188335851cf4.
    
    Change-Id: I16322550f433d82e6b774e707d0a44f1481ab269

--- modules/etcd/manifests/logging.pp
+++ modules/etcd/manifests/logging.pp
@@ -3,10 +3,13 @@
 # Manages all the logging logic for etcd.
 class etcd::logging {
 
-    logrotate::conf { '/etcd':
-        ensure => present,
+    file { '/etc/logrotate.d/etcd':
         source => 'puppet:///modules/etcd/logrotate.conf',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
     }
+
 
     rsyslog::conf { 'etcd':
         source   => 'puppet:///modules/etcd/rsyslog.conf',
