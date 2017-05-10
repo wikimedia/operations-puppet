c34ca8c3168d19449ad9db98e9736126fcda93fb
Daniel Zahn <dzahn@wikimedia.org>
2017-05-10 10:17:18 -0700

    syslog::centralserver: move 'include standard' to role
    Change-Id: I66b77fdd86ff5d499dda75ceaa01e9809243fbf5

--- manifests/site.pp
+++ manifests/site.pp
@@ -1427,7 +1427,6 @@ node 'labstore2004.codfw.wmnet' {
 
 node 'lithium.eqiad.wmnet' {
     role(syslog::centralserver)
-    include ::standard
 }
 
 node /^logstash100[1-2]\.eqiad\.wmnet$/ {
@@ -2660,7 +2659,6 @@ node /^wdqs200[1-3]\.codfw\.wmnet$/ {
 
 node 'wezen.codfw.wmnet' {
     role(syslog::centralserver)
-    include ::standard
 }
 
 # https://www.mediawiki.org/wiki/Parsoid
