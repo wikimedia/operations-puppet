76508e252c8d952992cf7b1e2e66f623c1bf4492
Daniel Zahn <dzahn@wikimedia.org>
2017-05-10 10:14:09 -0700

    puppetmaster::backend: move 'include standard' to role
    Change-Id: I09d073bd10232a0fba51890cdfe2661c0ef95856

--- manifests/site.pp
+++ manifests/site.pp
@@ -2377,10 +2377,7 @@ node /^puppetmaster[12]001\.(codfw|eqiad)\.wmnet$/ {
 
 node /^puppetmaster[12]002\.(codfw|eqiad)\.wmnet$/ {
     role(puppetmaster::backend)
-    include ::standard
-    interface::add_ip6_mapped { 'main':
-        interface => 'eth0',
-    }
+    interface::add_ip6_mapped { 'main': interface => 'eth0', }
 }
 
 
@@ -2530,11 +2527,7 @@ node 'sodium.wikimedia.org' {
 
 node /^rhodium.eqiad.wmnet/ {
     role(puppetmaster::backend)
-    include ::standard
-
-    interface::add_ip6_mapped { 'main':
-        interface => 'eth0',
-    }
+    interface::add_ip6_mapped { 'main': interface => 'eth0', }
 }
 
 
--- modules/role/manifests/puppetmaster/backend.pp
+++ modules/role/manifests/puppetmaster/backend.pp
@@ -1,6 +1,7 @@
 # vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab textwidth=80 smarttab
 
 class role::puppetmaster::backend {
+    include ::standard
     include ::base::firewall
 
     system::role { 'puppetmaster':
