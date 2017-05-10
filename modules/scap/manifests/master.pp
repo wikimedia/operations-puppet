5e61dafd52a4392dd69429ebf982a66ee4fd2ea9
RobH <rob@wikimedia.org>
2017-05-10 10:28:28 -0700

    decommission mira
    out of warranty host, no longer in service for deployment master,
    replaced by naos.
    
    removal of all old puppet references
    
    Bug:T164588
    Change-Id: Iaa81d059c1105897edbeedf9c64d7cb2b81d22d6

--- modules/scap/manifests/master.pp
+++ modules/scap/manifests/master.pp
@@ -1,6 +1,6 @@
 # = class: scap::master
 #
-# Sets up a scap master (currently tin and mira)
+# Sets up a scap master (currently tin and naos)
 class scap::master(
     $common_path        = '/srv/mediawiki',
     $common_source_path = '/srv/mediawiki-staging',
