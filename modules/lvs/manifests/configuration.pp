5d62720c7a5140c9afd7d449c7061130d865c997
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-05-11 10:29:27 +0200

    LVS: move pybal config to a separate class
    - lvs::configuration is included in many places, but the $pybal
      configuration is actually used only by pybal. Moreover the values in
      there makes no sense when applied to non-LVS hosts.
    - this fixes also the errors in deployment-prep due to missing IPv6
    
    Bug: T163196
    Change-Id: I035e0192faea4385a470451ddce88afa4b736eb8

--- modules/lvs/manifests/configuration.pp
+++ modules/lvs/manifests/configuration.pp
@@ -67,26 +67,6 @@ class lvs::configuration {
         default           => 'unknown',
     }
 
-    $pybal = {
-        'bgp' => hiera('lvs::configuration::bgp', 'yes'),
-        'bgp-peer-address' => $::hostname ? {
-            /^lvs100[1-3]$/ => '208.80.154.196', # cr1-eqiad
-            /^lvs100[4-6]$/ => '208.80.154.197', # cr2-eqiad
-            /^lvs100[789]$/ => '208.80.154.196', # cr1-eqiad
-            /^lvs101[012]$/ => '208.80.154.197', # cr2-eqiad
-            /^lvs200[1-3]$/ => '208.80.153.192', # cr1-codfw
-            /^lvs200[4-6]$/ => '208.80.153.193', # cr2-codfw
-            /^lvs300[12]$/  => '91.198.174.244',  # cr2-esams
-            /^lvs300[34]$/  => '91.198.174.245',  # cr1-esams
-            /^lvs400[12]$/  => '198.35.26.192',   # cr1-ulsfo
-            /^lvs400[34]$/  => '198.35.26.193',   # cr2-ulsfo
-            default         => '(unspecified)'
-            },
-        'bgp-nexthop-ipv4' => $facts['ipaddress'],
-        'bgp-nexthop-ipv6' => inline_template("<%= require 'ipaddr'; (IPAddr.new(@ipaddress6).mask(64) | IPAddr.new(\"::\" + @ipaddress.gsub('.', ':'))).to_s() %>"),
-        'instrumentation' => 'yes',
-    }
-
     # NOTE! This hash is referenced in many other manifests
     $service_ips = hiera('lvs::configuration::lvs_service_ips')
     $lvs_services = hiera('lvs::configuration::lvs_services')
