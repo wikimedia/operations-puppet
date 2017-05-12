c6c1eaa85a17a8d2103f469685b56a6c070ec2c0
Daniel Zahn <dzahn@wikimedia.org>
2017-05-12 12:44:38 -0700

    phabricator: convert to profile/role-structure
    Another conversion from "role" to "profile/role"-
    structure in Puppet, following our newer puppet
    coding standards.
    
    https://wikitech.wikimedia.org/wiki/Puppet_coding#Profiles
    
    Change-Id: If2eeff9f844a223fff460b7fe713be14f3f00ccf

--- modules/profile/manifests/phabricator/rsync.pp
+++ modules/profile/manifests/phabricator/rsync.pp
@@ -1,6 +1,6 @@
 # setup rsyncd and config on the _non_-active phab server
 # to allow pushing of /srv/repos from active to passive  (T137928)
-class role::phabricator::rsync {
+class profile::phabricator::rsync {
 
     $phabricator_active_server_fqdn = hiera('phabricator_active_server_fqdn')
 
