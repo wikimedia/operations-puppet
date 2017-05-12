6b78c3d88deb9e45d41540c5b28f6d0bd3ec2588
Daniel Zahn <dzahn@wikimedia.org>
2017-05-12 12:28:13 -0700

    bastionhost: convert to role/profile structure
    Change-Id: I03826628a3eea91e6f23630f94b23a0cbbd1d55b

---
+++ modules/role/manifests/bastionhost/install.pp
@@ -0,0 +1,24 @@
+# a combination of bastion host and install server
+# on a single machine
+class role::bastionhost::install {
+
+    system::role { 'bastionhost::install':
+        description => 'Bastion host and install server',
+    }
+
+    include ::standard
+    include ::base::firewall
+    include ::bastionhost
+    include ::profile::backup::host
+    include ::profile::bastionhost::general
+    include ::profile::scap::dsh  # Used by parsoid deployers
+
+    include ::role::installserver::tftp
+    include ::ipmi::mgmt
+    include ::prometheus::ops
+
+    backup::set {'home': }
+
+    class { '::ganglia::monitor::aggregator': sites => $::site, }
+
+}
