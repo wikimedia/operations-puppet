e830f615871f2859e2d8dfd9ac50aca1460f2e1c
Daniel Zahn <dzahn@wikimedia.org>
2017-05-12 12:44:38 -0700

    phabricator: convert to profile/role-structure
    Another conversion from "role" to "profile/role"-
    structure in Puppet, following our newer puppet
    coding standards.
    
    https://wikitech.wikimedia.org/wiki/Puppet_coding#Profiles
    
    Change-Id: If2eeff9f844a223fff460b7fe713be14f3f00ccf

---
+++ modules/role/manifests/phabricator_server.pp
@@ -0,0 +1,20 @@
+# phabricator instance
+#
+# filtertags: labs-project-deployment-prep labs-project-phabricator
+class role::phabricator_server {
+
+    system::role { 'phabricator_server':
+        description => 'Phabricator (Main) Server'
+    }
+
+    include ::standard
+    include ::lvs::realserver
+    include ::base::firewall
+    include ::apache::mod::remoteip
+    include ::profile::backup::host
+    include ::profile::phabricator::main
+    include ::profile::phabricator::rsync
+    include ::phabricator::monitoring
+    include ::phabricator::mpm
+    include ::exim4::ganglia
+}
