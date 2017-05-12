7bd7ba813063202e7caf294efb96b6b63859a85e
Daniel Zahn <dzahn@wikimedia.org>
2017-05-12 12:28:13 -0700

    bastionhost: convert to role/profile structure
    Change-Id: I03826628a3eea91e6f23630f94b23a0cbbd1d55b

---
+++ modules/profile/manifests/bastionhost/general.pp
@@ -0,0 +1,11 @@
+# bastion host for all users
+class profile::bastionhost::general {
+
+    ferm::service { 'ssh':
+        desc  => 'SSH open from everywhere, this is a bastion host',
+        prio  => '01',
+        proto => 'tcp',
+        port  => 'ssh',
+    }
+
+}
