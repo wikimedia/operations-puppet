49dc3e4cd48088ac55bf2fa85e07f778b06e4164
Daniel Zahn <dzahn@wikimedia.org>
2017-05-12 12:44:38 -0700

    phabricator: convert to profile/role-structure
    Change-Id: If2eeff9f844a223fff460b7fe713be14f3f00ccf

--- manifests/site.pp
+++ manifests/site.pp
@@ -1240,10 +1240,9 @@ node /^install[12]002\.wikimedia\.org$/ {
 
 # Phabricator
 node /^(iridium\.eqiad|phab1001\.eqiad|phab2001\.codfw)\.wmnet$/ {
-    interface::add_ip6_mapped { 'main': }
-    role(phabricator::main)
-    include ::standard
+    role(phabricator_server)
     include ::ganglia
+    interface::add_ip6_mapped { 'main': }
 }
 
 node 'iron.wikimedia.org' {
--- modules/profile/manifests/phabricator/rsync.pp
+++ modules/profile/manifests/phabricator/rsync.pp
@@ -1,7 +1,8 @@
 # setup rsyncd and config on the _non_-active phab server
 # to allow pushing of /srv/repos from active to passive  (T137928)
-class role::phabricator::rsync {
+class profile::phabricator::rsync {
 
+    include rsync::server
     $phabricator_active_server_fqdn = hiera('phabricator_active_server_fqdn')
 
     if $::fqdn != $phabricator_active_server_fqdn {
@@ -14,8 +15,6 @@ class role::phabricator::rsync {
             port   => '873',
             srange => "(${hosts_allow_ferm_v4} ${hosts_allow_ferm_v6})",
         }
-
-        include rsync::server
 
         rsync::server::module { 'phab-srv-repos':
             path        => '/srv/repos',
