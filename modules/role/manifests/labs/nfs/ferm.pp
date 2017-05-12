23ddf506f03d48018a279c810e46e94d34c69910
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-05-12 10:51:25 +0200

    labstore: initial ferm rules shared by all labstore hosts
    rpcbind and NFS itself use a common port in all labstore NFS classes.
    
    (Not yet included in the labstore roles at this point)
    
    Bug: T165136
    Change-Id: I4bdd1eb9285c311041ac4501c4f7a1693066dc7f

---
+++ modules/role/manifests/labs/nfs/ferm.pp
@@ -0,0 +1,40 @@
+# Common ferm class for labstore servers.
+
+class role::labs::nfs::ferm {
+
+    ferm::service { 'labstore_nfs_portmapper_udp':
+        proto  => 'udp',
+        port   => '111',
+        srange => '$LABS_NETWORKS',
+    }
+
+    ferm::service { 'labstore_nfs_portmapper_tcp':
+        proto  => 'tcp',
+        port   => '111',
+        srange => '$LABS_NETWORKS',
+    }
+
+    ferm::service { 'labstore_nfs_nfs_service':
+        proto  => 'tcp',
+        port   => '2049',
+        srange => '$LABS_NETWORKS',
+    }
+
+    ferm::service { 'labstore_nfs_rpc_statd_tcp':
+        proto  => 'tcp',
+        port   => '55659',
+        srange => '$LABS_NETWORKS',
+    }
+
+    ferm::service { 'labstore_nfs_rpc_statd_udp':
+        proto  => 'udp',
+        port   => '55659',
+        srange => '$LABS_NETWORKS',
+    }
+
+    ferm::service { 'labstore_nfs_rpc_mountd':
+        proto  => 'tcp',
+        port   => '38466',
+        srange => '$LABS_NETWORKS',
+    }
+}
