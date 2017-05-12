90714e7759cb20b40e19d3e93815eecb82ae8c6e
Daniel Zahn <dzahn@wikimedia.org>
2017-05-12 12:28:13 -0700

    bastionhost: convert to role/profile structure
    Change-Id: I03826628a3eea91e6f23630f94b23a0cbbd1d55b

--- modules/role/manifests/bastionhost/twofa.pp
+++ modules/role/manifests/bastionhost/twofa.pp
@@ -1,33 +1,15 @@
-class role::bastionhost::twofa {
-    system::role { 'bastionhost::twofa':
-        description => 'Bastion host using two factor authentication',
+class role::bastionhost_twofa {
+
+    system::role { 'bastionhost_twofa':
+        description => 'experimental Bastion host using YubiKey two factor authentication',
     }
 
-    include ::bastionhost
     include ::standard
     include ::base::firewall
+    include ::bastionhost
+    include ::profile::bastionhost::twofa
     include ::profile::backup::host
     include ::passwords::yubiauth
-
+    include ::role::access_new_install
     backup::set {'home': }
-
-    require_package('libpam-yubico')
-
-    ferm::service { 'ssh':
-        desc  => 'SSH open from everywhere, this is a bastion host',
-        prio  => '01',
-        proto => 'tcp',
-        port  => 'ssh',
-    }
-
-    $api_key = $passwords::yubiauth::api_key
-
-    file { '/etc/pam.d/sshd':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0440',
-        content => template('role/bastionhost/pam-sshd.erb'),
-        require => Package['openssh-server'],
-    }
 }
