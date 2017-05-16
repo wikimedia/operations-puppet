bc4c5c8914d1fa10760b6e6b8cd05ff2bb0b67a9
Andrew Otto <acotto@gmail.com>
2017-05-16 15:04:05 +0200

    Allow setting of zookeeper_version in hiera
    Newer versions of Debian Jessie have an updated package version.
    Since we don't want to upgrade everything in prod (yet), but still want
    to use zookeeper on later Jessie images in labs, we allow overriding via hiera.
    
    Change-Id: I0e69fa992b1076063a04547308812124e514ead8

--- modules/role/manifests/zookeeper/client.pp
+++ modules/role/manifests/zookeeper/client.pp
@@ -2,6 +2,12 @@
 # These role classes will configure Zookeeper properly in either
 # the labs or production environments.
 #
+# To avoid version conflics with Cloudera zookeeper package, this
+# class manually specifies which debian package version should be installed.
+# There are sane defaults for Ubunty Trusty and Debian Jessie
+# (as of 2017-05 the Jessie version needs updated to 3.4.5+dfsg-2+deb8u1).
+# If you need to override this, you may set 'zookeeper_version' in hiera.
+#
 # Usage:
 #
 # If you only need Zookeeper client configs to talk to Zookeeper servers:
@@ -33,10 +39,12 @@
 #
 class role::zookeeper::client {
 
-    $version = $::lsbdistcodename ? {
-        'jessie'  => '3.4.5+dfsg-2',
-        'trusty'  => '3.4.5+dfsg-1',
-    }
+    $version = hiera('zookeeper_version',
+        $::lsbdistcodename ? {
+            'jessie'  => '3.4.5+dfsg-2',
+            'trusty'  => '3.4.5+dfsg-1',
+        }
+    )
 
     $clusters     = hiera('zookeeper_clusters')
     $cluster_name = hiera('zookeeper_cluster_name')
