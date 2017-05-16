670c6e85b4e80a1bcfcedbd78c50e67ef557beea
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-05-16 17:08:43 +0200

    prometheus: report puppet agent stats
    Introduce prometheus::node_puppet_agent to parse puppet state directory
    and report status (disabled, failed, etc) as metrics.
    
    Change-Id: I0eeb80cf68addacdd651b8eb80c159c711498a1f

---
+++ modules/prometheus/manifests/node_puppet_agent.pp
@@ -0,0 +1,29 @@
+# = Class: prometheus::node_puppet_agent
+#
+# Periodically export puppet agent stats via node-exporter textfile collector.
+#
+
+class prometheus::node_puppet_agent (
+    $ensure = 'present',
+    $outfile = '/var/lib/prometheus/node.d/puppet_agent.prom',
+) {
+    validate_re($outfile, '\.prom$')
+    validate_ensure($ensure)
+
+    require_package('python-prometheus-client')
+
+    file { '/usr/local/bin/prometheus-puppet-agent-stats':
+        ensure => file,
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/prometheus/usr/local/bin/prometheus-puppet-agent-stats',
+    }
+
+    # Collect every minute
+    cron { 'prometheus_puppet_agent_stats':
+        ensure  => $ensure,
+        user    => 'root',
+        command => "/usr/local/bin/prometheus-puppet-agent-stats --outfile ${outfile}",
+    }
+}
