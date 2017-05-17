9734cc197496804da1245c029bcbd997d6392307
Andrew Otto <acotto@gmail.com>
2017-05-17 11:32:27 +0200

    Add kafka_version parameter, s/java_packaage/java_home/ in confluent::kafka::client
    Also add profile::kafka::simple::broker for testing in labs.
    
    This allows for configuration of which Java is used, and which Kafka version will be installed.
    
    Change-Id: I577ed3f0fd9c95ba7ba5df1b49ce1e1680754dfb

--- modules/confluent/manifests/kafka/client.pp
+++ modules/confluent/manifests/kafka/client.pp
@@ -5,36 +5,44 @@
 # Most likely you will not use this class directly, and instead
 # just use confluent::kafka::broker to install and start a Kafka broker.
 # You will only use confluent::kafka::client directly if you need to
-# change the version of java or scala that is being installed, or if
+# change the version of java, kafka, or scala that is being installed, or if
 # you want to install the confluent-kafka package without puppet managing
 # a Kafka broker.
 #
 # == Parameters
 #
-# [*java_package*]
-#   Name of java package to require.  This will be passed to require_package().
-#   Default: openjdk-7-jdk
+# [*java_home*]
+#   Path to JAVA_HOME.  This class does not manage installation of Java.
+#   You must do that elsewhere.  Default: undef (will use system default).
+#
+# [*kafka_version*]
+#   Ensure this version of a confluent-kafka package is installed.
+#   Default: undef
 #
 # [*scala_version*]
 #   confluent-kafka-$scala_version will be installed.
 #   Default: 2.11.7
 #
 class confluent::kafka::client(
-    $java_package  = 'openjdk-7-jdk',
+    $java_home     = undef,
+    $kafka_version = undef,
     $scala_version = '2.11.7'
 ) {
     $package = "confluent-kafka-${scala_version}"
-    require_package([$java_package, $package])
+    require_package($package)
+
+    # If $kafka_version was given,
+    # make sure that a specific debian package version was installed.
+    if $kafka_version {
+        Package[$package] { ensure => $kafka_version }
+    }
 
     file { '/usr/local/bin/kafka':
-        source  => 'puppet:///modules/confluent/kafka/kafka.sh',
+        content => template('confluent/kafka/kafka.sh.erb'),
         owner   => 'root',
         group   => 'root',
         mode    => '0755',
-        require => [
-            Package[$java_package],
-            Package[$package],
-        ],
+        require => Package[$package],
     }
 
     # Have puppet manage totally manage this directory.
