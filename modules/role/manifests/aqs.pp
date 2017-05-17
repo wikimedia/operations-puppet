919a70722db7f05609c1ba61b4ffb0ce86edc353
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-05-17 13:14:18 +0200

    role::aqs: use profile::cassandra
    Also add a switch to profile::cassandra to allow opening connections to
    the analytics network in case of need, and fix handling of the case we
    have no TLS encryption.
    
    Change-Id: I727779606ff6c83f95e2dd308df3fa34f14e3f77

--- modules/role/manifests/aqs.pp
+++ modules/role/manifests/aqs.pp
@@ -18,59 +18,7 @@ class role::aqs {
     #
     # Set up Cassandra for AQS.
     #
-
-    # Parameters to be set by Hiera
-    include ::cassandra
-    include ::cassandra::metrics
-    include ::cassandra::logging
-
-    $cassandra_instances = $::cassandra::instances
-
-    if $cassandra_instances {
-        $instance_names = keys($cassandra_instances)
-        ::cassandra::instance::monitoring { $instance_names:
-            contact_group => 'admins,team-services,analytics',
-        }
-    } else {
-        $default_instances = {
-            'default' => {
-                'listen_address' => $::cassandra::listen_address,
-            }
-        }
-        ::cassandra::instance::monitoring { 'default':
-            instances     => $default_instances,
-            contact_group => 'admins,team-services,analytics',
-        }
-    }
-
-    $cassandra_hosts_ferm = join(hiera('cassandra::seeds'), ' ')
-
-    # Cassandra intra-node messaging
-    ferm::service { 'cassandra-analytics-intra-node':
-        proto  => 'tcp',
-        port   => '7000',
-        srange => "@resolve((${cassandra_hosts_ferm}))",
-    }
-    # Cassandra JMX/RMI
-    ferm::service { 'cassandra-analytics-jmx-rmi':
-        proto  => 'tcp',
-        port   => '7199',
-        srange => "@resolve((${cassandra_hosts_ferm}))",
-    }
-    # Allow analytics networks to populate cassandra
-    include network::constants
-    $analytics_networks = join($network::constants::analytics_networks, ' ')
-
-    # In addition to the IP assigned to the Cassandra multi instances, these rules
-    # grant access from the actual AQS hosts
-    $aqs_hosts_ferm = join(hiera('aqs_hosts'), ' ')
-
-    # Cassandra CQL query interface
-    ferm::service { 'cassandra-analytics-cql':
-        proto  => 'tcp',
-        port   => '9042',
-        srange => "(@resolve((${cassandra_hosts_ferm})) @resolve((${aqs_hosts_ferm})) ${analytics_networks})",
-    }
+    include ::profile::cassandra
 
     #
     # Set up AQS
