a0dc8e4a1bf94c79e244d39b97c4618d1351c4f4
Andrew Otto <acotto@gmail.com>
2017-05-17 11:32:27 +0200

    Add kafka_version parameter, s/java_packaage/java_home/ in confluent::kafka::client
    Also add profile::kafka::simple::broker for testing in labs.
    
    This allows for configuration of which Java is used, and which Kafka version will be installed.
    
    Change-Id: I577ed3f0fd9c95ba7ba5df1b49ce1e1680754dfb

---
+++ modules/profile/manifests/kafka/simple/broker.pp
@@ -0,0 +1,43 @@
+# == Class profile::kafka::simple::broker
+# Sets up a simple Kafka broker instance.
+# This is useful for testing Kafka in labs.
+#
+class profile::kafka::simple::broker {
+    system::role { 'profile::kafka::simple::broker':
+        description => "Kafka Broker Server in the ${cluster_name} cluster",
+    }
+
+    require_package('openjdk-8-jdk')
+
+    # kafkacat is handy!
+    require_package('kafkacat')
+
+    $config         = kafka_config('simple')
+    $cluster_name   = $config['name']
+    $zookeeper_url  = $config['zookeeper']['url']
+    $brokers_string = $config['brokers']['string']
+
+    # If we've got at least 3 brokers, set default replication factor to 3.
+    $replication_factor  = min(3, $config['brokers']['size'])
+
+    file { '/srv/kafka':
+        ensure => 'directory',
+        mode   => '0755',
+    }
+
+    class { '::confluent::kafka::client':
+        java_home     => '/usr/lib/jvm/java-1.8.0-openjdk-amd64',
+        kafka_version => undef,
+    }
+
+    class { '::confluent::kafka::broker':
+        log_dirs                     => ['/srv/kafka/data'],
+        brokers                      => $config['brokers']['hash'],
+        zookeeper_connect            => $config['zookeeper']['url'],
+        default_replication_factor   => $replication_factor,
+        # Use Kafka/LinkedIn recommended settings with G1 garbage collector.
+        # https://kafka.apache.org/documentation/#java
+        # Note that MetaspaceSize is a Java 8 setting.
+        jvm_performance_opts         => '-server -XX:MetaspaceSize=96m -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:G1HeapRegionSize=16M -XX:MinMetaspaceFreeRatio=50 -XX:MaxMetaspaceFreeRatio=80',
+    }
+}
