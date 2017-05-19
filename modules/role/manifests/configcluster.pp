7be746a6b80ab5158b5ce5fa9e7e7f8b8b85b372
elukey <ltoscano@wikimedia.org>
2017-05-19 12:47:33 +0200

    role::zookeeper: refactor to multiple profiles
    Change-Id: If511394ac4a80287dd44f0c89d9f29fcf9477ad1

--- modules/role/manifests/configcluster.pp
+++ modules/role/manifests/configcluster.pp
@@ -1,7 +1,7 @@
 class role::configcluster {
     include ::standard
     include ::base::firewall
-    include ::role::zookeeper::server
+    include ::profile::zookeeper::server
     include ::profile::etcd
     include ::profile::etcd::tlsproxy
     include ::profile::etcd::replication
