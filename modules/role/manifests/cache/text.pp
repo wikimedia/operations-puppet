b10535bf4401c7c924652ce262606b358103f4e1
Timo Tijhof <krinklemail@gmail.com>
2017-05-23 23:06:47 +0200

    varnish: Switch browsersec to use errorpage template
    Follows-up Id77d23a442ab9ba3.
    
    Bug: T113114
    Change-Id: I70dfabe043f1fc270c59b60e57d5bff20ceb3c84

--- modules/role/manifests/cache/text.pp
+++ modules/role/manifests/cache/text.pp
@@ -56,6 +56,17 @@ class role::cache::text(
 
     $text_storage_args = $::role::cache::base::file_storage_args
 
+    $errorpage = {
+        title       => 'Browser Connection Security Issues',
+        pagetitle   => "Your Browser's Connection Security is Outdated",
+        logo_link   => 'https://www.wikimedia.org',
+        logo_src    => 'https://www.wikimedia.org/static/images/wmf.png',
+        logo_srcset => 'https://www.wikimedia.org/static/images/wmf-2x.png 2x',
+        logo_alt    => 'Wikimedia',
+        content     => template('varnish/browsersec.body.html.erb'),
+    }
+    $error_browsersec_html = template('mediawiki/errorpage.html.erb')
+
     role::cache::instances { 'text':
         fe_jemalloc_conf  => 'lg_dirty_mult:8,lg_chunk:16',
         fe_runtime_params => $common_runtime_params,
