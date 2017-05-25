46fe5d6db579d29fe7d1b3f41f880a384f70653a
Antoine Musso <hashar@free.fr>
2017-05-25 22:54:16 +0200

    Fix spec for various modules
    base::service seems to now invoke logstash::conf, add 'logstash' to the
    list of fixtures.
    nrpe specs were failling with 'unknown init system'. Set the fact to
    'systemd'.
    
    Change-Id: I0a4e9c41329bf71c9a5dc11ef3c033fb8ce0c74f

--- modules/nrpe/spec/classes/nrpe_spec.rb
+++ modules/nrpe/spec/classes/nrpe_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe 'nrpe', :type => :class do
+    let(:facts) { { :initsystem => 'systemd' } }
     it { should contain_package('nagios-nrpe-server') }
     it { should contain_package('nagios-plugins') }
     it { should contain_package('nagios-plugins-basic') }
@@ -11,7 +12,10 @@ describe 'nrpe', :type => :class do
 end
 
 describe 'nrpe', :type => :class do
-    let(:facts) { { :realm => 'production' } }
+    let(:facts) { {
+        :realm      => 'production',
+        :initsystem => 'systemd',
+    } }
 
     it 'should generate valid content for nrpe_local.cfg in production' do
         should contain_file('/etc/nagios/nrpe_local.cfg').with_content(/allowed_hosts=127.0.42.42/)
@@ -19,14 +23,20 @@ describe 'nrpe', :type => :class do
 end
 
 describe 'nrpe', :type => :class do
-    let(:facts) { { :realm => 'labs' } }
+    let(:facts) { {
+        :realm      => 'labs',
+        :initsystem => 'systemd',
+    } }
     it 'should generate valid content for nrpe_local.cfg in labs' do
         should contain_file('/etc/nagios/nrpe_local.cfg').with_content(/allowed_hosts=10.68.42.42/)
     end
 end
 
 describe 'nrpe', :type => :class do
-    let(:facts) { { :realm => 'labs' } }
+    let(:facts) { {
+        :realm      => 'labs',
+        :initsystem => 'systemd',
+    } }
     let(:params) { { :allowed_hosts => '10.10.10.10' } }
 
     it 'should generate valid content for nrpe_local.cfg in labs with allowed_hosts defined' do
