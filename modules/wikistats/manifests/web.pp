e755fcacbdf2ca7bbfb4acb1680c5094ab177d4b
Daniel Zahn <dzahn@wikimedia.org>
2017-05-25 13:52:00 -0700

    wikistats: ensure Apache PHP module is installed before site
    With the current setup puppet runs and libapache2-mod-php-* gets
    installed but the PHP site won't work until a manual
    'dpkg-reconfigure libapache2-mod-php7.0'.
    
    This is because it gets installed before the rest of the Apache
    packages get pulled in. Add missing Require to fix that, also
    move things around a bit and make more obvious how
    the PHP version influences package names.
    
    Change-Id: Idb4205bee82051a36c3b22588a2fce98c975e89f

--- modules/wikistats/manifests/web.pp
+++ modules/wikistats/manifests/web.pp
@@ -3,22 +3,27 @@ class wikistats::web (
     $wikistats_host,
     ) {
 
-    # Apache site from template
-    apache::site { $wikistats_host:
-        content => template('wikistats/apache/wikistats.erb'),
+    if os_version('debian >= stretch') {
+        $php_version = '7.0'
+    } else {
+        $php_version = '5'
     }
 
-    # document root
+    $apache_php_package = "libapache2-mod-php${php_version}"
+
+    require_package($apache_php_package)
+
+    include ::apache::mod::rewrite
+
+    apache::site { $wikistats_host:
+        content => template('wikistats/apache/wikistats.erb'),
+        require => Package[$apache_php_package],
+    }
+
     file { '/var/www/wikistats':
         ensure => directory,
         mode   => '0755',
         owner  => 'wikistatsuser',
         group  => 'www-data',
-    }
-
-    include ::apache::mod::rewrite
-
-    if os_version('debian >= stretch') {
-        require_package('libapache2-mod-php7.0')
     }
 }
