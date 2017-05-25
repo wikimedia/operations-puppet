2fedaf9457151936459d50b4589d16a4028c0f69
Timo Tijhof <krinklemail@gmail.com>
2017-05-25 22:13:09 +0200

    dynamicproxy: Centralise error page template and use it
    This was originally based on the Varnish errorpage, but I'm starting with
    dynamicproxy first because it's a simple case where the file is just on disk.
    Later patches will re-use some of this inside the VCL use cases.
    
    This change also affects toollabs proxy, since it uses dynamicproxy.
    
    Defining 'mediawiki::errorpage' to abstract default parameters and simplify
    usage in multiple places.
    
    Bug: T113114
    Change-Id: I764d00c7b40ad0931590f04ed2f76ecbd84b33ba

---
+++ modules/mediawiki/manifests/errorpage.pp
@@ -0,0 +1,83 @@
+# == Define: mediawiki::errorpage
+#
+# Creates a file based on the error page template.
+#
+# === Usage
+# mediawiki::errorpage { '/tmp/error-example.html':
+#     content => '<p>Example</p>'
+# }
+#
+# === Parameters
+#
+# [*filepath*]
+#   The file path for File resource. (Required)
+#
+# [*favicon*]
+#   URL for favicon. (Optional)
+#
+# [*doctitle*]
+#   HTML Document title. (Required)
+#
+#   Default: 'Wikimedia Error'
+#
+# [*pagetitle*]
+#   Page heading. (Required)
+#
+#   Default: 'Error'
+#
+# [*content*]
+#   Main HTML content, after logo and first heading. (Required)
+#
+#   Default: ''
+#
+# [*logo_link*]
+#   URL for anchor link around logo. Set to undef to disable link. (Optional)
+#
+#   Default: 'https://www.wikimedia.org'
+#
+#   Default: 'Wikimedia Error'
+#
+# [*logo_src*]
+#   URL for logo image. (Required)
+#
+#   Default: 'https://www.wikimedia.org/static/images/wmf.png'
+#
+# [*logo_srcset*]
+#   HTML srcset attribute value for logo image. (Required)
+#
+#   Default: 'https://www.wikimedia.org/static/images/wmf-2x.png 2x'
+#
+# [*footer*]
+#   Optional HTML content for the footer. (Optional)
+#
+define mediawiki::errorpage(
+    $filepath = $name,
+    $owner = 'root',
+    $group = 'root',
+    $mode = '0444',
+    $favicon = undef,
+    $doctitle = 'Wikimedia Error',
+    $pagetitle = 'Error',
+    $logo_link = 'https://www.wikimedia.org',
+    $logo_src = 'https://www.wikimedia.org/static/images/wmf.png',
+    $logo_srcset = 'https://www.wikimedia.org/static/images/wmf-2x.png 2x',
+    $content = '',
+    $footer = undef,
+) {
+    $errorpage  = {
+        favicon     => $favicon,
+        title       => $doctitle,
+        pagetitle   => $pagetitle,
+        logo_link   => $logo_link,
+        logo_src    => $logo_src,
+        logo_srcset => $logo_srcset,
+        content     => $content,
+        footer      => $footer,
+    }
+    file { $filepath:
+        owner  => $owner,
+        group  => $group,
+        mode   => $mode,
+        source => template('mediawiki/errorpage.html.erb'),
+    }
+}
