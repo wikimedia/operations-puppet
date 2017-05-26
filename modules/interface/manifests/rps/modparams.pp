ebfe747736651938b6a69b162edbddd997e73f2f
Brandon Black <bblack@wikimedia.org>
2017-05-26 16:54:20 +0000

    NUMA via facter+hiera for RPS
    Change-Id: I75978cd6f1344c4c18ecb66b5bec6ecfae22367b

--- modules/interface/manifests/rps/modparams.pp
+++ modules/interface/manifests/rps/modparams.pp
@@ -1,6 +1,13 @@
 class interface::rps::modparams {
     include initramfs
 
+    if hiera('numa_networking', false) {
+        $num_queues = $facts['physicalcorecount'] / $facts['numa']['node_count']
+    }
+    else {
+        $num_queues = $facts['physicalcorecount']
+    }
+
     file { '/etc/modprobe.d/rps.conf':
         content => template("${module_name}/rps.conf.erb"),
         notify  => Exec['update-initramfs']
