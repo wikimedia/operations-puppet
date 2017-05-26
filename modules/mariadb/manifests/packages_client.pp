984cb451a7508c55e580e51e899c2954f00e1de1
Jaime Crespo <jcrespo@wikimedia.org>
2017-05-26 16:16:24 +0200

    mysql-client: Install colordiff (neodymium & sarin)
    The colordiff utility will make easier to detect differences on sql output,
    one of the things done on the client servers.
    
    diff --color was only added on diffutils 3.4 (debian >= stretch).
    
    Change-Id: I600b582ce5089c23630cd93ad0ee82cc03dfb989

--- modules/mariadb/manifests/packages_client.pp
+++ modules/mariadb/manifests/packages_client.pp
@@ -5,14 +5,21 @@
 class mariadb::packages_client {
 
     package { [
-        'wmf-mariadb101-client',
-        'percona-toolkit',
-        'percona-xtrabackup',
-        'grc',
-        'python3-pymysql',
-        'python3-tabulate',
+        'wmf-mariadb101-client', # mariadb client, custom wmf package
+        'percona-toolkit',       # very useful client utilities
+        'percona-xtrabackup',    # backup tool, useful even outside of the server
+        'grc',                   # used to colorize paged sql output
+        'python3-pymysql',       # dependency for some utilities- TODO: delete & add as dependecy
+        'python3-tabulate',      # dependency for some utilities- TODO: delete & add as dependecy
     ]:
         ensure => present,
+    }
+
+    if os_version('debian <= stretch') {
+        package { [
+            'colordiff',             # useful to colorize diff output, use diff --color in strech
+        ]:
+            ensure => present,
     }
 
 }
