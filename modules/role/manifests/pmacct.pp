5edf52433690201877b9999b21ded59531bba24d
Faidon Liambotis <faidon@wikimedia.org>
2017-05-29 15:57:00 +0300

    pmacct: fix firewall rules
    We want routers to be be able to communicate to pmacct (netflows and
    BGP), but router loopbacks aren't in $PRODUCTION_NETWORKS which is what
    the firewall rules currently allow.
    
    Add a list of loopbacks per site, hardcoded within the pmacct manifest.
    Adding it to the network data Hiera was considered but rejected as an
    option, since it would fit poorly into that structure and (depending on
    where they would be added) open up connections to random services from
    the networking equipment. Hardcoding it into pmacct should be OK for
    now, and we can think about factoring it out when we'll need to reuse
    this information elsewhere.
    
    Change-Id: I898b7df9d502b842e00676e6efd5a0b2a0467669

--- modules/role/manifests/pmacct.pp
+++ modules/role/manifests/pmacct.pp
@@ -1,3 +1,4 @@
+#
 class role::pmacct {
     system::role { 'role::pmacct':
         description => 'pmacct netflow accounting',
@@ -7,17 +8,32 @@ class role::pmacct {
     include ::base::firewall
     include ::standard
 
+    $loopbacks = [
+        # eqiad
+        '208.80.154.196/30',
+        '2620:0:861:ffff::/64',
+        # codfw
+        '208.80.153.192/29',
+        '2620:0:860:ffff::/64',
+        # esams
+        '91.198.174.244/30',
+        '2620:0:862:ffff::/64',
+        # ulsfo
+        '198.35.26.192/30',
+        '2620:0:863:ffff::/64',
+    ]
+
     ferm::service { 'bgp':
         proto  => 'tcp',
         port   => '179',
         desc   => 'BGP',
-        srange => '$PRODUCTION_NETWORKS',
+        srange => inline_template('(<%= @loopbacks.join(" ") %>)'),
     }
 
     ferm::service { 'netflow':
         proto  => 'udp',
         port   => '2100',
         desc   => 'NetFlow',
-        srange => '$PRODUCTION_NETWORKS',
+        srange => inline_template('(<%= @loopbacks.join(" ") %>)'),
     }
 }
