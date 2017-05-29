432fec442d134b248a78cb531cffe09a9c3edba3
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-05-29 18:18:51 +0200

    logstash - cleanup dead code
    Cleanup related to https://gerrit.wikimedia.org/r/#/c/356063/
    
    Bug: T166154
    Change-Id: I904c24bea5161f3f4ad413423a02848e3f0f19de

--- modules/logstash/manifests/output/elasticsearch/scripts.pp
+++ modules/logstash/manifests/output/elasticsearch/scripts.pp
@@ -3,11 +3,6 @@
 # Provision utility scripts for Logstash Elasticsearch output
 #
 class logstash::output::elasticsearch::scripts {
-    #TODO: to remove once cleanup is done
-    file { '/usr/local/bin/logstash_delete_index':
-        ensure => absent,
-    }
-
     file { '/usr/local/bin/logstash_clear_cache.sh':
         ensure => absent, # T144396 - removing the clear cache mechanism to validate it is not needed anymore
         owner  => 'root',
