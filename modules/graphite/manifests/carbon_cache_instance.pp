7f302588fe5f9477a38276a50b7649826cc53cd5
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-05-29 15:23:55 +0200

    Don't symlink systemd service instances
    In stretch systemctl will explicitly refuse to operate on symlink'ed units. For
    templated services the symlink isn't necessary because systemctl already DTRTs
    
    Bug: T166389
    Change-Id: I034eb945189f3aec693a15994e9b29e1b9dcb2a6

--- modules/graphite/manifests/carbon_cache_instance.pp
+++ modules/graphite/manifests/carbon_cache_instance.pp
@@ -1,13 +1,8 @@
 define graphite::carbon_cache_instance {
-    file { "/lib/systemd/system/carbon-cache@${title}.service":
-        ensure  => 'link',
-        target  => '/lib/systemd/system/carbon-cache@.service',
-        require => File['/lib/systemd/system/carbon-cache@.service'],
-    }
-
     service { "carbon-cache@${title}":
         ensure   => 'running',
         provider => 'systemd',
         enable   => true,
+        require  => File['/lib/systemd/system/carbon-cache@.service'],
     }
 }
