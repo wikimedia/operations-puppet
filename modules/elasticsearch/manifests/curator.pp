09265a7f8ea8c134fd642a14ada96fea0f064e96
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-05-29 14:28:10 +0200

    elasticsearch - introduce curator configs to enable / disable replication
    Bug: T166154
    Change-Id: I9a074b31fba7764838a6a7114c47ebc1110e59d3

---
+++ modules/elasticsearch/manifests/curator.pp
@@ -0,0 +1,21 @@
+class elasticsearch::curator (
+    $hosts,
+) {
+
+    require_package('elasticsearch-curator')
+
+    file { '/etc/curator/':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+    elasticsearch::curator::config {
+        'config':
+            content => template('elasticsearch/curator/config.yaml.erb');
+        'disable-shard-allocation.yaml':
+            source => 'puppet:///modules/elasticsearch/curator/disable-shard-allocation.yaml';
+        'enable-shard-allocation.yaml':
+            source => 'puppet:///modules/elasticsearch/curator/enable-shard-allocation.yaml';
+    }
+}
