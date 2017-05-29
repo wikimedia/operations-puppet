9774ff7928474c306f2598b15713d7129d5382a9
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-05-29 17:59:14 +0200

    profile::nutcracker: correctly handle monitoring, refactoring
    * Add a monitoring_port parameter to set which port to monitor, if any
    * Add base settings for both redis and memcached, allowing only to
      override those, allowing a more DRY approach.
    * Add a parser function to merge the hashes, this could be avoided with
      either the future parser or a better base class.
    
    Change-Id: I35ff9fecc6b342de640e6e37e5b7764797080603

---
+++ modules/profile/lib/puppet/parser/functions/nutcracker_pools.rb
@@ -0,0 +1,32 @@
+module Puppet::Parser::Functions
+  newfunction(:nutcracker_pools, :type => :rvalue, :arity => 4, :doc => <<-EOS
+  Given a list of redis pools, memcached pools, and base settings for both,
+  extracts a server list that is then fed to class nutcracker.
+  EOS
+    ) do |args|
+    if args.size < 4 then
+      raise(Puppet::ParseError, "nutcracker_pools: 4 arguments needed, (#{args.size} given)")
+    end
+    redis, memc, redis_settings, memc_settings = args
+
+    pools = {}
+    if redis.is_a?(Hash)
+      redis.keys.each do |k|
+        if pools.include?(k)
+          raise(Puppet::ParseError, "Pool #{k} already defined")
+        end
+        pools[k] = merge(redis_settings, redis[k])
+      end
+    end
+    if memc.is_a?(Hash)
+      memc.keys.each do |k|
+        if pools.include?(k)
+          raise(Puppet::ParseError, "Pool #{k} already defined")
+        end
+        pools[k] = merge(memc_settings, memc[k])
+      end
+    end
+
+    pools
+  end
+end
--- modules/profile/manifests/nutcracker.pp
+++ modules/profile/manifests/nutcracker.pp
@@ -2,16 +2,48 @@
 #
 # Configures a generic nutcracker instance
 class profile::nutcracker(
-    $pools = hiera('profile::nutcracker::pools')
+    $redis_pools = hiera('profile::nutcracker::redis_pools'),
+    $memcached_pools = hiera('profile::nutcracker::memcached_pools'),
+    $monitor_port = hiera('profile::nutcracker::monitor_port'), # set to 0 if no port available
 ) {
     include ::passwords::redis
 
+    # Default settings that should be ok for any pool
+    # pools *need* to define just the listen and server_map entries
+    $redis_base_settings = {
+        auto_eject_hosts     => true,
+        distribution         => 'ketama',
+        redis                => true,
+        redis_auth           => $::passwords::redis::main_password,
+        hash                 => 'md5',
+        server_connections   => 1,
+        server_failure_limit => 3,
+        server_retry_timeout => to_milliseconds('30s'),
+        timeout              => 1000,
+    }
+
+    $memcached_base_settings = {
+        auto_eject_hosts     => true,
+        distribution         => 'ketama',
+        hash                 => 'md5',
+        preconnect           => true,
+        server_connections   => 1,
+        server_failure_limit => 3,
+        server_retry_timeout => to_milliseconds('30s'),
+        timeout              => 250,
+    }
+
+    # TODO: this is now a parser function, should be doable with map()
+    # when we enable the future parser
+    $pools = nutcracker_pools($redis_pools, $memcached_pools, $redis_base_settings, $memcached_base_settings)
     class { '::nutcracker':
         mbuf_size => '64k',
         pools     => $pools,
     }
 
-    class { '::nutcracker::monitoring': }
+    class { '::nutcracker::monitoring':
+        port => $monitor_port,
+    }
 
 
     ferm::rule { 'skip_nutcracker_conntrack_out':
