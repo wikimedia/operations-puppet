e0753f382424bb93c1b2f360bd3b07e8cd19ca39
Paladox <thomasmulhall410@yahoo.com>
2017-05-30 19:54:57 +0000

    jenkins: Install java 8 on stretch and jessie
    Also only install java 7 if you are on trusty or jessie, stretch does not have java 7
    
    Bug: T166611
    Bug: T162828
    Change-Id: If6a134bbaa3bb879a11921b6c667932c198da9a2

--- modules/jenkins/manifests/slave/requisites.pp
+++ modules/jenkins/manifests/slave/requisites.pp
@@ -4,6 +4,10 @@
 #
 class jenkins::slave::requisites() {
 
-    ensure_packages(['openjdk-7-jre-headless'])
+    if os_version('ubuntu == trusty || debian == jessie') {
+        ensure_packages(['openjdk-7-jre-headless'])
+    }
+
+    ensure_packages(['openjdk-8-jre-headless'])
 
 }
