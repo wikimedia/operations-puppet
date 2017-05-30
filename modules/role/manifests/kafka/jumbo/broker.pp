d886c129511b9747bc0b11362c73cd6b6bf671b2
Andrew Otto <acotto@gmail.com>
2017-05-30 14:57:02 -0400

    Kafka broker profile and roles for new 'jumbo' cluster and 'simple' cluster
    jumbo will be the analytlics replacement cluster.  simple is useful for testing in labs.
    
    Bug: T166162
    Change-Id: I491c07964c8f2f4cc0d7be7760decead17c97bdd

---
+++ modules/role/manifests/kafka/jumbo/broker.pp
@@ -0,0 +1,12 @@
+# == Class role::kafka::aggregate::broker
+# Sets up a Kafka broker in the 'jumbo' Kafka cluster.
+#
+class role::kafka::jumbo::broker {
+    system::role { 'role::kafka::aggregate::broker':
+        description => "Kafka Broker in an 'aggregate' Kafka cluster",
+    }
+
+    include standard
+    include base::firewall
+    include profile::kafka::broker
+}
