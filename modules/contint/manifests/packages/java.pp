533aa4def2202314f76ded1c27038e13da3a9298
Paladox <thomasmulhall410@yahoo.com>
2017-05-30 19:51:24 +0000

    contint: Only install java 7 on trusty and jessie
    stretch does not have the package java 7.
    
    Bug: T166611
    Change-Id: If58499a2a9ab88a614232cfbde4f35256c749407

--- modules/contint/manifests/packages/java.pp
+++ modules/contint/manifests/packages/java.pp
@@ -1,9 +1,9 @@
 class contint::packages::java {
 
-    require_package('openjdk-7-jdk')
-
     if os_version('debian >= jessie') {
         require_package('openjdk-8-jdk')
+    } else {
+        require_package('openjdk-7-jdk')
     }
 
     package { 'maven2':
