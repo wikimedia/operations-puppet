7c474c87e92968acd38096a1ad72d38e6ac980a1
Andrew Otto <acotto@gmail.com>
2017-05-30 14:57:02 -0400

    Add new kafka broker profile, add confluent ssl and rack settings
    This also adds support for $listeners, rather than 'port' in kafka_cluster config hash.
    This will modify the configs on the running production brokers to use listeners,
    but should functionally be a no-op.
    
    Adds jumbo and simple kafka broker roles.
    jumbo will be the analytlics replacement cluster.  simple is useful for testing in labs.
    
    Bug: T166162
    Change-Id: I491c07964c8f2f4cc0d7be7760decead17c97bdd

---
+++ modules/role/manifests/kafka/simple/broker.pp
@@ -0,0 +1,13 @@
+# == Class role::kafka::simple::broker
+# Sets up a Kafka broker in a 'simple' Kafka cluster.
+# This is useful for spinning up simple Kafka clusters in labs.
+#
+class role::kafka::simple::broker {
+    system::role { 'role::kafka::simple::broker':
+        description => "Kafka Broker in a 'simple' Kafka cluster",
+    }
+
+    include standard
+    include base::firewall
+    include profile::kafka::broker
+}
