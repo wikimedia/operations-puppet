15a7c9493b4a928d4f6a559e909e68351d8a3386
Francisco Dans <fdans@wikimedia.org>
2017-06-01 18:33:23 +0200

    Add exception for events tagged as coming from MW
    Will let events tagged as `is_mediawiki` go through to mysql
    
    Bug: T67508
    Change-Id: I11778eac51e60fcca5ab39ebe5af0e9be14a524e

--- modules/eventlogging/files/filters.py
+++ modules/eventlogging/files/filters.py
@@ -2,7 +2,8 @@ import json
 
 
 def is_not_bot(e):
+    user_agent_dict = json.loads(e['userAgent'])
     try:
-        return not json.loads(e['userAgent'])['is_bot']
+        return (not user_agent_dict['is_bot']) or (user_agent_dict['is_mediawiki'])
     except (ValueError, KeyError):
         return True
