699c545182f93fea4d8d6b9224c0a101dd7ce77b
Tyler Cipriani <tcipriani@wikimedia.org>
2017-06-05 13:22:48 -0600

    Scap: Bump version to 3.5.8-1
    Bug: T127762
    Change-Id: I1ea0bc432909167c65a2a04dd87b7bc71a51829b

--- modules/scap/manifests/init.pp
+++ modules/scap/manifests/init.pp
@@ -12,7 +12,7 @@
 class scap (
     $deployment_server = 'deployment',
     $wmflabs_master = 'deployment-tin.deployment-prep.eqiad.wmflabs',
-    $version = '3.5.7-1',
+    $version = '3.5.8-1',
 ) {
     package { 'scap':
         ensure => $version,
