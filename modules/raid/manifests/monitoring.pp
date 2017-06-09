f27393119c1ff43de6c54c8f0432f17aa9b989a7
Faidon Liambotis <faidon@wikimedia.org>
2017-06-09 11:39:25 +0300

    raid: split parts of raid into raid::monitoring
    To more accurately reflect what it does and separate concerns.
    
    Change-Id: I198062415977a409e4b244ce0547860c9122a355

---
+++ modules/raid/manifests/monitoring.pp
@@ -0,0 +1,150 @@
+# == Class: raid::monitoring
+#
+# Class to set up monitoring for software and hardware RAID
+#
+# === Parameters
+#
+# === Examples
+#
+#  include raid::monitoring
+
+class raid::monitoring {
+    require raid
+
+    $check_raid = '/usr/bin/sudo /usr/local/lib/nagios/plugins/check_raid'
+
+    # for 'forking' checks (i.e. all but mdadm, which essentially just reads
+    # kernel memory from /proc/mdstat) check every $check_interval
+    # minutes instead of default of one minute. If the check is non-OK, retry
+    # every $retry_interval.
+    $check_interval = 10
+    $retry_interval = 5
+
+    if 'megaraid' in $facts['raid'] {
+        $get_raid_status_megacli = '/usr/local/lib/nagios/plugins/get-raid-status-megacli'
+
+        file { $get_raid_status_megacli:
+            ensure => present,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            source => 'puppet:///modules/raid/get-raid-status-megacli.py';
+        }
+
+        sudo::user { 'nagios_megaraid':
+            user       => 'nagios',
+            privileges => ["ALL = NOPASSWD: ${get_raid_status_megacli}"],
+        }
+
+        nrpe::check { 'get_raid_status_megacli':
+            command => "/usr/bin/sudo ${get_raid_status_megacli} -c",
+        }
+
+        nrpe::monitor_service { 'raid_megaraid':
+            description    => 'MegaRAID',
+            nrpe_command   => "${check_raid} megacli",
+            check_interval => $check_interval,
+            retry_interval => $retry_interval,
+            event_handler  => "raid_handler!megacli!${::site}",
+        }
+    }
+
+    if 'hpsa' in $facts['raid'] {
+        file { '/usr/local/lib/nagios/plugins/check_hpssacli':
+            ensure => present,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            source => 'puppet:///modules/raid/dsa-check-hpssacli',
+        }
+
+        sudo::user { 'nagios_hpssacli':
+            user       => 'nagios',
+            privileges => [
+                'ALL = NOPASSWD: /usr/sbin/hpssacli controller all show',
+                'ALL = NOPASSWD: /usr/sbin/hpssacli controller slot=[0-9] ld all show',
+                'ALL = NOPASSWD: /usr/sbin/hpssacli controller slot=[0-9] ld all show detail',
+                'ALL = NOPASSWD: /usr/sbin/hpssacli controller slot=[0-9] ld * show',
+                'ALL = NOPASSWD: /usr/sbin/hpssacli controller slot=[0-9] pd all show',
+                'ALL = NOPASSWD: /usr/sbin/hpssacli controller slot=[0-9] pd [0-9]\:[0-9] show',
+                'ALL = NOPASSWD: /usr/sbin/hpssacli controller slot=[0-9] pd [0-9][EIC]\:[0-9]\:[0-9] show',
+                'ALL = NOPASSWD: /usr/sbin/hpssacli controller slot=[0-9] pd [0-9][EIC]\:[0-9]\:[0-9][0-9] show',
+                'ALL = NOPASSWD: /usr/sbin/hpssacli controller slot=[0-9] show status',
+                'ALL = NOPASSWD: /usr/sbin/hpssacli controller slot=[0-9] show detail',
+                'ALL = NOPASSWD: /usr/sbin/hpacucli controller all show',
+                'ALL = NOPASSWD: /usr/sbin/hpacucli controller slot=[0-9] ld all show',
+                'ALL = NOPASSWD: /usr/sbin/hpacucli controller slot=[0-9] ld * show',
+                'ALL = NOPASSWD: /usr/sbin/hpacucli controller slot=[0-9] pd all show',
+                'ALL = NOPASSWD: /usr/sbin/hpacucli controller slot=[0-9] pd [0-9]\:[0-9] show',
+                'ALL = NOPASSWD: /usr/sbin/hpacucli controller slot=[0-9] pd [0-9][EIC]\:[0-9]\:[0-9] show',
+                'ALL = NOPASSWD: /usr/sbin/hpacucli controller slot=[0-9] pd [0-9][EIC]\:[0-9]\:[0-9][0-9] show',
+                'ALL = NOPASSWD: /usr/sbin/hpacucli controller slot=[0-9] show status',
+                'ALL = NOPASSWD: /usr/sbin/hpacucli controller slot=[0-9] show detail',
+            ],
+        }
+
+        nrpe::monitor_service { 'raid_hpssacli':
+            description    => 'HP RAID',
+            nrpe_command   => '/usr/local/lib/nagios/plugins/check_hpssacli',
+            timeout        => 60, # can take > 10s on servers with lots of disks
+            check_interval => $check_interval,
+            retry_interval => $retry_interval,
+            event_handler  => "raid_handler!hpssacli!${::site}",
+        }
+
+        $get_raid_status_hpssacli = '/usr/local/lib/nagios/plugins/get-raid-status-hpssacli'
+
+        file { $get_raid_status_hpssacli:
+            ensure => present,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            source => 'puppet:///modules/raid/get-raid-status-hpssacli.sh';
+        }
+
+        nrpe::check { 'get_raid_status_hpssacli':
+            command => "${get_raid_status_hpssacli} -c",
+        }
+    }
+
+    if 'mpt' in $facts['raid'] {
+        nrpe::monitor_service { 'raid_mpt':
+            description    => 'MPT RAID',
+            nrpe_command   => "${check_raid} mpt",
+            check_interval => $check_interval,
+            retry_interval => $retry_interval,
+            event_handler  => "raid_handler!mpt!${::site}",
+        }
+
+        nrpe::check { 'get_raid_status_mpt':
+            command => "${check_raid} mpt",
+        }
+    }
+
+    if 'md' in $facts['raid'] {
+        # if there is an "md" RAID configured, mdadm is already installed
+
+        nrpe::monitor_service { 'raid_md':
+            description   => 'MD RAID',
+            nrpe_command  => "${check_raid} md",
+            event_handler => "raid_handler!md!${::site}",
+        }
+
+        nrpe::check { 'get_raid_status_md':
+            command => 'cat /proc/mdstat',
+        }
+    }
+
+    file { '/usr/local/lib/nagios/plugins/check_raid':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/raid/check-raid.py';
+    }
+
+    sudo::user { 'nagios_raid':
+        user       => 'nagios',
+        privileges => ['ALL = NOPASSWD: /usr/local/lib/nagios/plugins/check_raid'],
+    }
+}
