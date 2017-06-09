7ed7d1dccbbef8c16d5406d76eb616a41ef506e3
Nathaniel Schaaf <nschaaf@wikimedia.org>
2017-06-09 16:17:48 +0200

    Update recommendation-api module and role
    Bug: T167113
    Change-Id: I993ca34edcaf33d660cbb217fbeacd184af2e6d2

--- modules/role/manifests/recommendation_api.pp
+++ modules/role/manifests/recommendation_api.pp
@@ -1,18 +1,7 @@
-# == Class: role::recommendation_api
-#
-# Provisions a JSON API for personalized recommendations.
-#
+# Role class for recommendation_api
 class role::recommendation_api {
+
+    system::role { 'role::recommendation_api': }
+
     include ::recommendation_api
-
-    include ::apache::mod::uwsgi
-
-    apache::site { 'recommendation_api':
-        content => template('role/apache/sites/recommendation_api.erb'),
-    }
-
-    ferm::service { 'recommendation_api_http':
-        proto => 'tcp',
-        port  => '80',
-    }
 }
