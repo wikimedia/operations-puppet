6e8d8a41801d1f1b629d9a967743896dd9f539e0
Nathaniel Schaaf <nschaaf@wikimedia.org>
2017-06-09 16:17:48 +0200

    (in progress) Update recommendation-api module and role
    Bug: T167113
    Change-Id: I993ca34edcaf33d660cbb217fbeacd184af2e6d2

--- modules/recommendation_api/manifests/init.pp
+++ modules/recommendation_api/manifests/init.pp
@@ -1,75 +1,33 @@
 # == Class: recommendation_api
 #
-# The Wikimedia Recommendation API is a JSON API that provides personalized
+# The Wikimedia Recommendation API is an API that provides personalized
 # recommendations of wiki content (to discover, edit, translate, etc.).
-# It is a Flask webapp running under uWSGI.
+# It is a webapp based on ServiceTemplateNode
 #
 # See <https://meta.wikimedia.org/wiki/Recommendation_API> for more info.
 #
-class recommendation_api {
-    requires_os('debian >= jessie')
+# === Parameters
+#
+# [*wdqs_uri*]
+#   The full URI of the WDQS API endpoint to contact when issuing direct
+#   requests to it.
+#
+class recommendation_api(
+    $wdqs_uri = 'http://wdqs.svc.eqiad.wmnet:????',
+) {
+    include ::service::configuration
 
-    require_package(
-        'python3-bs4',
-        'python3-dateutil',
-        'python3-flask',
-        'python3-numpy',
-        'python3-pip',
-        'python3-requests',
-        'python3-simplejson',
-        'python3-wheel',
-        'python3-yaml',
-    )
-
-    $base_dir   = '/srv/recommendation-api'  # top-level dir for app
-    $core_dir   = "${base_dir}/core"         # source code
-    $wheels_dir = "${base_dir}/wheels"       # *.whl files
-    $lib_dir    = "${base_dir}/lib"          # dependencies (installed from *.whl)
-
-    file { [ $base_dir, $lib_dir ]:
-        ensure => directory,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-    }
-
-    git::clone { 'research/recommendation-api/wheels':
-        ensure    => 'latest',
-        directory => $wheels_dir,
-        branch    => 'master',
-    }
-
-    git::clone { 'research/recommendation-api':
-        ensure    => 'latest',
-        directory => $core_dir,
-        branch    => 'master',
-    }
-
-    exec { 'pip_install_wheels':
-        command     => "/usr/bin/pip3 install --target=${lib_dir} --no-deps ${wheels_dir}/wheels/*.whl",
-        require     => File[$lib_dir],
-        user        => 'root',
-        group       => 'root',
-        subscribe   => Git::Clone['research/recommendation-api/wheels'],
-        refreshonly => true,
-    }
-
-    uwsgi::app { 'recommendation_api':
-        settings => {
-            uwsgi => {
-                'plugin'      => 'python3',
-                'wsgi-file'   => "${core_dir}/recommendation/data/recommendation.wsgi",
-                'master'      => true,
-                'chdir'       => $core_dir,
-                'http-socket' => '0.0.0.0:8080',
-                'socket'      => '/run/uwsgi/recommendation_api.sock',
-                'pythonpath'  => $lib_dir,
-                'processes'   => $facts['processorcount'],
-            },
+    service::node { 'recommendation-api':
+        port              => 1970,
+        healthcheck_url   => '',
+        has_spec          => true,
+        deployment        => 'scap3',
+        deployment_config => true,
+        deployment_vars   => {
+            mwapi_uri    => $::service::configuration::mwapi_uri,
+            restbase_uri => $::service::configuration::restbase_uri,
+            wdqs_uri     => $wdqs_uri,
         },
-        require  => [
-            Exec['pip_install_wheels'],
-            Git::Clone['research/recommendation-api'],
-        ],
     }
 }
+
