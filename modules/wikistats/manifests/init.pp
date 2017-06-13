b5c29bc2fe86cf99387c534de5fe0ec839b2b27b
Daniel Zahn <dzahn@wikimedia.org>
2017-06-12 20:52:18 -0700

    wikistats: move dump dir out of define, duplicate declaration
    Change-Id: Icf3e9b1e306139043f3ac49b99b1592a96dd160f

--- modules/wikistats/manifests/init.pp
+++ modules/wikistats/manifests/init.pp
@@ -79,6 +79,13 @@ class wikistats (
     }
 
     # scripts and crons to update data and dump XML files
+    file { $file_path:
+        ensure => directory,
+        owner  => 'wikistatsuser',
+        group  => 'wwww-data',
+        mode   => '0644',
+    }
+
     class { 'wikistats::updates':
         db_pass => $db_pass,
     }
