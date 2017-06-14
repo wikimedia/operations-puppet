f9e28e0f122fb90b412e2c897c58337b49ca4898
Andrew Otto <acotto@gmail.com>
2017-06-14 15:19:28 -0400

    Insert select eventbus generated event topics into eventlogging MySQL database
    Bug: T150369
    
    Change-Id: I63bbc5d5fe2f17b4b8e0cba5514e27bd08c35498

--- modules/role/manifests/eventlogging/analytics/mysql.pp
+++ modules/role/manifests/eventlogging/analytics/mysql.pp
@@ -6,6 +6,13 @@
 class role::eventlogging::analytics::mysql {
     include role::eventlogging::analytics::server
 
+    # We use the mediawiki/event-schemas to support insertion of events from EventBus
+    # that use those schemas not on meta.wikimedia.org.
+    # NOTE: If an event schema changes, the eventlogging-consumer process(es) will
+    # not be automatically restarted.  You must manually restart this for the consumer
+    # process to pick up changes to local schemas.
+    require eventschemas
+
     ## MySQL / MariaDB
 
     # Log strictly valid events to the 'log' database on m4-master.
@@ -41,7 +48,26 @@ class role::eventlogging::analytics::mysql {
     }
 
     # mixed_uri URI is defined for DRY purposes in role::eventlogging::analytics::server.
-    $kafka_mixed_uri = $role::eventlogging::analytics::server::kafka_mixed_uri
+    $kafka_consumer_scheme = $role::eventlogging::analytics::server::kafka_consumer_scheme
+
+    # Add more here as requested.
+    # NOTE: The datacenter prefixed topics are produced via EventBus, and are of
+    # schemas in the mediawiki/event-schemas repository.
+    $topics_to_consume = [
+        # Valid eventlogging analytics events are all in this one topic.
+        'eventlogging-valid-mixed',
+        # Various mediawiki events (via EventBus)
+        'eqiad.mediawiki.revision-create',
+        'codfw.mediawiki.revision-create',
+        'eqiad.mediawiki.page-move',
+        'codfw.mediawiki.page-move',
+        'eqiad.mediawiki.page-delete',
+        'codfw.mediawiki.page-delete',
+        'eqiad.mediawiki.page-undelete',
+        'codfw.mediawiki.page-undelete',
+    ]
+    $topics_string = join($topics_to_consume, ',')
+    $kafka_consumer_uri = "${kafka_consumer_scheme}/${kafka_brokers_string}?topics=${topics_string}"
 
     # Define statsd host url to send mysql insert metrics.
     # For beta cluster, set in https://wikitech.wikimedia.org/wiki/Hiera:Deployment-prep
@@ -56,12 +82,16 @@ class role::eventlogging::analytics::mysql {
     # Kafka consumer group for this consumer is mysql-m4-master
     eventlogging::service::consumer { $mysql_consumers:
         # auto commit offsets to kafka more often for mysql consumer
-        input  => "${filter_scheme}${kafka_mixed_uri}&auto_commit_interval_ms=1000${$kafka_api_version_param}${filter_function}",
-        output => "mysql://${mysql_user}:${mysql_pass}@${mysql_db}?charset=utf8&statsd_host=${statsd_host}&replace=True",
-        sid    => $kafka_consumer_group,
+        input        => "${filter_scheme}${kafka_consumer_uri}&auto_commit_interval_ms=1000${$kafka_api_version_param}${filter_function}",
+        output       => "mysql://${mysql_user}:${mysql_pass}@${mysql_db}?charset=utf8&statsd_host=${statsd_host}&replace=True",
+        # Load and cache local (EventBus) schemas so those events can be inserted into MySQL too.
+        # This will require a restart of the consumer process(es) when there are any new schemas.
+        schemas_path => "${::eventschemas::path}/jsonschema",
+        sid          => $kafka_consumer_group,
         # Restrict permissions on this config file since it contains a password.
-        owner  => 'root',
-        group  => 'eventlogging',
-        mode   => '0640',
+        owner        => 'root',
+        group        => 'eventlogging',
+        mode         => '0640',
+
     }
 }
