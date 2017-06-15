1e09c4d8664e06a7b805b8657fd3c4bbcb2ec5af
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-06-15 14:15:07 +0300

    Assign kubernetes::staging::worker role
    Add to kubestage100X the role::kubernetes::staging::worker class and
    provide the correct hiera data for it
    
    Change-Id: Id411a38fff5be5eed2c7ba194e51574e0a117b3b

---
+++ modules/role/manifests/kubernetes/staging/worker.pp
@@ -0,0 +1,11 @@
+# filtertags: labs-project-packaging
+class role::kubernetes::staging::worker {
+    include ::standard
+    include ::base::firewall
+
+    # Sets up docker on the machine
+    include ::profile::docker::storage
+    include ::profile::docker::engine
+    include ::profile::kubernetes::node
+    include ::profile::calico::kubernetes
+}
