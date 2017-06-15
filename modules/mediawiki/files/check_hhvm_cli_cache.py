dfd732af293372b729c5b7352df2e24362a9e379
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-06-15 11:48:40 +0200

    Add Icinga check for depletion of HHVM CLI cache
    The HHVM byte code cache doesn't get evicted, so continues to grow with
    each deployment. At various times it tripped over the 512 MiB file limit
    imposed by wfShellExec, which caused various issues in the past.
    
    Add a simple Icinga check to alert for that (can be run daily), since
    it may e.g. lead to failing maintenance scripts.
    
    (hhvm_cleanup_cache can be used to prune the cache, but it takes a few
    minutes so the servers needs to be depooled and downtimed. Doesn't
    happen very often, when I upgraded to HHVM 3.18 I piggybacked the
    bytecode cleanup with the upgrade)).
    
    Bug: T161598
    Change-Id: Ifcfd604203725c5972f9d33f6eeceb290c89b86e

---
+++ modules/mediawiki/files/check_hhvm_cli_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import argparse
+import sys
+import os
+
+hhvm_cli_cache = "/var/cache/hhvm/cli.hhbc.sq3"
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Check HHVM CLI cache")
+    parser.add_argument('-w', type=int, help="Warning threshold in MiB", required=True)
+    parser.add_argument('-c', type=int, help="Critical threshold in MiB", required=True)
+    args = parser.parse_args()
+
+    warning_threshold = args.w * 1048576
+    critical_threshold = args.c * 1048576
+
+    if not os.path.exists(hhvm_cli_cache):
+        print("UNKNOWN: error reading CLI cache")
+        sys.exit(3)
+
+    cache_size = os.path.getsize(hhvm_cli_cache)
+
+    if cache_size > critical_threshold:
+        print("CRITICAL: HHVM CLI cache is %d bytes" % cache_size)
+        sys.exit(2)
+    elif cache_size > warning_threshold:
+        print("WARNING: HHVM CLI cache is %d bytes" % cache_size)
+        sys.exit(1)
+    else:
+        print("OK: HHVM CLI cache is %d bytes" % cache_size)
+        sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
