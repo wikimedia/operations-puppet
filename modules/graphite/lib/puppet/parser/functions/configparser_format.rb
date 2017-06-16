6cb1d5807a15843948f3593d7087e8308ba79f45
Faidon Liambotis <faidon@wikimedia.org>
2017-06-16 17:30:56 +0300

    graphite: cleanup configparser_format a little bit
    Fix indentation and Rubocop infractions:
    - Style/NestedTernaryOperator
    - Style/RedundantReturn
    - Layout/SpaceAfterComma
    - Layout/SpaceInsideParens
    
    Still needs a little bit of cleanup work, though.
    
    Change-Id: Idda59e236e3bbc82bd55cacd03c79f57f47237db

--- modules/graphite/lib/puppet/parser/functions/configparser_format.rb
+++ modules/graphite/lib/puppet/parser/functions/configparser_format.rb
@@ -4,31 +4,36 @@
 # See <http://docs.python.org/2/library/configparser.html>
 #
 def rmerge(*args)
-    # Recursively merge hashes.
-    merged = args.shift.clone
-    args.each { |hash|
-        merged.merge!(hash) { |k, old, new|
-            merged[k] = old.is_a?(Hash) && new.is_a?(Hash) ? rmerge(old, new) : new
-        }
-    }
-    return merged
+  # Recursively merge hashes.
+  merged = args.shift.clone
+  args.each do |hash|
+    merged.merge!(hash) do |k, old, new|
+      merged[k] = old.is_a?(Hash) && new.is_a?(Hash) ? rmerge(old, new) : new
+    end
+  end
+  merged
 end
 
 def configparser_format(config)
-    # Serialize a hash to Python ConfigParser format.
-    config.sort.map { |section,items|
-            ["[#{section}]"].concat items.sort.map { |k,v|
-                v = v.is_a?(Array) ? v.join(',') : ( v == :undef ? '' : v )
-                "#{k} = #{v}"
-            }.push []
-    }.join("\n")
+  # Serialize a hash to Python ConfigParser format.
+  config.sort.map { |section, items|
+    ["[#{section}]"].concat items.sort.map { |k, v|
+      if v.is_a?(Array)
+        v = v.join(',')
+      else
+        v = v == :undef ? '' : v
+      end
+
+      "#{k} = #{v}"
+    }.push []
+  }.join("\n")
 end
 
 module Puppet::Parser::Functions
-    newfunction(:configparser_format, :type => :rvalue) { |args|
-        unless args.any? && args.all? { |a| a.is_a? Hash }
-            fail 'configparser_format() requires one or more hash arguments'
-        end
-        configparser_format(rmerge(*args))
-    }
+  newfunction(:configparser_format, :type => :rvalue) do |args|
+    unless args.any? && args.all? { |a| a.is_a? Hash }
+      fail 'configparser_format() requires one or more hash arguments'
+    end
+    configparser_format(rmerge(*args))
+  end
 end
