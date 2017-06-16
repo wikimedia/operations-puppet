3d309223881b6c2f233a4bcb4a5312ff5f1571dd
Faidon Liambotis <faidon@wikimedia.org>
2017-06-16 16:22:33 +0300

    Fix 3 CommandLiteral and MultilineIfThen infractions
    Rubocop complains about %x{} compared to backticks, so fix that in a
    couple of places. Also, while at it, rewrite the ganeti_cluster fact to
    avoid shelling out just for running cat.
    
    Change-Id: I6ca2d51d4f58a2e6f7a26c8d499ad0013bba00ab

--- modules/apt/lib/facter/apt.rb
+++ modules/apt/lib/facter/apt.rb
@@ -1,6 +1,6 @@
 Facter.add("package_updates") do
   confine :operatingsystem => %w{Debian Ubuntu}
   setcode do
-    %x{/usr/local/bin/apt2xml 2>/dev/null}.chomp
+    `/usr/local/bin/apt2xml 2>/dev/null`.chomp
   end
 end
