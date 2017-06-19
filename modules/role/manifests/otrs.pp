1c0dd6f01b1f26125881c5c0a402520354dd4be6
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-06-19 17:40:38 +0300

    otrs: Refactor in the profile/role pattern
    Add a profile class for otrs, include it in the role alongside standard.
    Move the hieradata from the host level to the role level and change the
    keys so they corresponding to the same hierarchy level as puppet. Fix
    the name of the role as well in the process
    
    Change-Id: I62d53da2719ffd9c3a0d3fbf6f294e99d857b5af

---
+++ modules/role/manifests/otrs.pp
@@ -0,0 +1,12 @@
+# vim: set ts=4 et sw=4:
+# sets up an instance of the 'Open-source Ticket Request System'
+# https://en.wikipedia.org/wiki/OTRS
+#
+# filtertags: labs-project-otrs
+class role::otrs {
+    system::role { 'otrs':
+        description => 'OTRS Web Application Server',
+    }
+    include ::standard
+    include ::profile::otrs
+}
