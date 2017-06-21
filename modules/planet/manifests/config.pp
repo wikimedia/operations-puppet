a1f60739359b1ad634819d93a7530c0e2d5960d4
Daniel Zahn <dzahn@wikimedia.org>
2017-06-20 21:01:58 -0700

    planet: add support/compat for stretch and rawdog
    Add more compatibility code for using the rawdog
    package on stretch to replace missing planet-venus.
    
    Make the config and theme directories configurable
    and more.
    
    Bug: T168490
    Change-Id: Ie5cff18625de3c8faa7ad5f84268216ba88fd27e

--- modules/planet/manifests/config.pp
+++ modules/planet/manifests/config.pp
@@ -1,17 +1,26 @@
 # defined type: a config dir and file for a planet-venus language version
 define planet::config {
 
-    file { "/usr/share/planet-venus/wikimedia/${title}":
+    if os_version('debian == stretch') {
+        file { '/etc/rawdog':
+             ensure => 'directory',
+        }
+        $config_path = "/etc/rawdog"
+    } else {
+        $config_path = "/usr/share/planet-venus/wikimedia"
+    }
+
+    file { "${config_path}/${title}":
         ensure => 'directory',
-        path   => "/usr/share/planet-venus/wikimedia/${title}",
+        path   => "${config_path/${title}",
         mode   => '0755',
         owner  => 'planet',
         group  => 'planet',
     }
 
-    file { "/usr/share/planet-venus/wikimedia/${title}/config.ini":
+    file { "${config_path}/${title}/config.ini":
         ensure  => 'present',
-        path    => "/usr/share/planet-venus/wikimedia/${title}/config.ini",
+        path    => "${config_path}/${title}/config.ini",
         owner   => 'planet',
         group   => 'planet',
         mode    => '0444',
