a4792458d349d89fb998d2df67de10fdfaf57d6c
RobH <rob@wikimedia.org>
2017-06-21 09:17:16 -0700

    decommission of ms-be1001 thorugh ms-be1012
    removal of system info from repo, as they are being fully decommissioned
    and removed from the racks
    
    Bug:T166489
    Change-Id: Iea82d6df3cf7e5b18c8e714a6105861fb625556b

--- manifests/site.pp
+++ manifests/site.pp
@@ -1602,11 +1602,7 @@ node /^ms-fe100[6-8]\.eqiad\.wmnet$/ {
     include ::lvs::realserver
 }
 
-node /^ms-be10(0[0-9]|1[0-2])\.eqiad\.wmnet$/ {
-    role(spare::system)
-}
-
-node /^ms-be101[3-5]\.eqiad\.wmnet$/ {
+node /^ms-be1001[3-5]\.eqiad\.wmnet$/ {
     role(swift::storage)
 }
 
