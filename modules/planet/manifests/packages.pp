cf8f4f0b114ddfa4343991c2588799509daa597a
Daniel Zahn <dzahn@wikimedia.org>
2017-06-20 18:13:41 -0700

    planet: planet-venus not in stretch, try using "rawdog" instead
    The planet-venus package does not exist in stretch anymore.
    (http://tracker.debian.org/planet-venus).
    
    "rawdog" is another RSS aggregator to produce a 'planet-style' static
    site from feeds, as opposed to "Planet Pluto" it's also using Python
    and Feedparser, like planet-venus and most of all, it's a package
    in stable.
    
    Try using it to replace the mostly unmaintained planet-venus.
    
    http://offog.org/code/rawdog/
    
    Bug: T168490
    Change-Id: I299a9bf802d97bf020885ac287804388fb0835f7

--- modules/planet/manifests/packages.pp
+++ modules/planet/manifests/packages.pp
@@ -1,15 +1,25 @@
 # installs required packages for a planet-venus server
 class planet::packages {
 
-    # the main package
-    # prefer to update this manually
-    package { 'planet-venus':
-        ensure => 'present',
+    if os_version('debian == jessie') {
+        # the main package
+        # prefer to update this manually
+        package { 'planet-venus':
+            ensure => 'present',
+        }
+
+        # XSLT 1.0 command line processor
+        package { 'xsltproc':
+            ensure => 'present',
+        }
     }
 
-    # XSLT 1.0 command line processor
-    package { 'xsltproc':
-        ensure => 'present',
+    # planet-venus does not exist anymore in stretch
+    # rawdog is another RSS aggregator using Python and Feedparser
+    # to produce a "planet"-like static site
+    if os_version('debian == stretch') {
+        package { 'rawdog':
+            ensure => 'present',
+        }
     }
-
 }
