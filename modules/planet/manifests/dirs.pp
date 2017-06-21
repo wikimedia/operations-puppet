192a5906491061d0f8c18f667acf9b9a54cb25e6
Daniel Zahn <dzahn@wikimedia.org>
2017-06-20 22:46:09 -0700

    planet: fix invalid relationship in cronjob, dirs on stretch
    Bug: T168490
    Change-Id: Iaea31c85ef8444a7e3d95d0f2714d42a6dba2f01

--- modules/planet/manifests/dirs.pp
+++ modules/planet/manifests/dirs.pp
@@ -1,18 +1,35 @@
 # sets up needed directories for a planet-venus intall
 class planet::dirs {
 
-    file { [
-        '/var/www/planet',
-        '/var/log/planet',
-        '/usr/share/planet-venus/wikimedia',
-        '/usr/share/planet-venus/theme/wikimedia',
-        '/usr/share/planet-venus/theme/common',
-        '/var/cache/planet',
-        ]:
-        ensure => 'directory',
-        owner  => 'planet',
-        group  => 'planet',
-        mode   => '0755',
-    }
+    if os_version('debian == stretch') {
 
+        file { [
+            '/var/www/planet',
+            '/var/log/planet',
+            '/etc/rawdog/',
+            '/etc/rawdog/theme',
+            '/etc/rawdog/theme/wikimedia',
+            ]:
+            ensure => 'directory',
+            owner  => 'planet',
+            group  => 'planet',
+            mode   => '0755',
+        }
+
+    } else {
+
+        file { [
+            '/var/www/planet',
+            '/var/log/planet',
+            '/usr/share/planet-venus/wikimedia',
+            '/usr/share/planet-venus/theme/wikimedia',
+            '/usr/share/planet-venus/theme/common',
+            '/var/cache/planet',
+            ]:
+            ensure => 'directory',
+            owner  => 'planet',
+            group  => 'planet',
+            mode   => '0755',
+        }
+    }
 }
