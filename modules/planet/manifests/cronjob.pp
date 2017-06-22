f54ec3e0795d77de2a0adffcea14dac2e6be1ecb
Daniel Zahn <dzahn@wikimedia.org>
2017-06-22 12:57:09 -0700

    planet: make config templates and crons for rawdog
    Make config location, templates and cron commands flexible to
    add support for using rawdog instead of planet-venus on stretch.
    
    Convert all configs from old template to new format.
    
    The old ones will be deleted once migration is completed.
    
    Bug: T168490
    Change-Id: Ief9ec73379c5b2eb2016bc025f09d22032ecb564

--- modules/planet/manifests/cronjob.pp
+++ modules/planet/manifests/cronjob.pp
@@ -1,12 +1,12 @@
 # defined type: a cronjob for planet-venus or rawdog feed updates per language
 define planet::cronjob {
 
-    $planet_bin = '/usr/bin/planet'
-
     if os_version('debian == stretch') {
-        $planet_config = "/etc/rawdog/${title}/config.ini"
+        $planet_bin = '/usr/bin/rawdog'
+        $planet_cmd = "-d /etc/rawdog/${title}/ -v -u -w"
     } else {
-        $planet_config = "/usr/share/planet-venus/wikimedia/${title}/config.ini"
+        $planet_bin = '/usr/bin/planet'
+        $planet_cmd = "-v /usr/share/planet-venus/wikimedia/${title}/config.ini"
     }
 
     $planet_logfile = "/var/log/planet/${title}-planet.log"
@@ -16,13 +16,12 @@ define planet::cronjob {
 
     cron { "update-${title}-planet":
         ensure  => 'present',
-        command => "http_proxy=\"${planet::planet_http_proxy}\" https_proxy=\$http_proxy ${planet_bin} -v ${planet_config} > ${planet_logfile} 2>&1",
+        command => "http_proxy=\"${planet::planet_http_proxy}\" https_proxy=\$http_proxy ${planet_bin} ${planet_cmd} > ${planet_logfile} 2>&1",
         user    => 'planet',
         minute  => $minute,
         require => [
             Class['planet::packages'],
             File['/var/log/planet'],
-            File[$planet_config],
             User['planet'],
         ],
     }
