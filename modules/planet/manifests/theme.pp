207471ac31eb55d8ad7999427fbde838ede8fe7d
Daniel Zahn <dzahn@wikimedia.org>
2017-06-23 14:14:06 -0700

    planet: add HTML page/item/feed templates for rawdog
    Add page,item,feedlist,feeditem HTML templates to be used
    by rawdog. For nicer design than the defaults.
    
    Bug: T168490
    Change-Id: I91624618794f24c5078259f45350169622ebc23d

--- modules/planet/manifests/theme.pp
+++ modules/planet/manifests/theme.pp
@@ -14,33 +14,46 @@ define planet::theme {
         default => 'planet.css',
     }
 
-
     if os_version('debian == stretch') {
-        $theme_path = '/etc/rawdog/theme/wikimedia'
-        # style sheet for rawdog
+        file { "/etc/rawdog/theme/wikimedia/${title}":
+            ensure  => 'directory',
+        }
         file { "/var/www/planet/${title}/style.css":
             ensure => 'present',
             source => 'puppet:///modules/planet/theme/rawdog_style.css';
         }
+        file { "/etc/rawdog/theme/wikimedia/${title}/rd_page.tmpl":
+            ensure => 'present',
+            source => template('planet/html/rd_page.html.tmpl.erb');
+        }
+        file { "/etc/rawdog/theme/wikimedia/${title}/rd_item.tmpl":
+            ensure => 'present',
+            source => template('planet/html/rd_item.html.tmpl.erb');
+        }
+        file { "/etc/rawdog/theme/wikimedia/${title}/rd_feedlist.tmpl":
+            ensure => 'present',
+            source => template('planet/html/rd_feedlist.html.tmpl.erb');
+        }
+        file { "/etc/rawdog/theme/wikimedia/${title}/rd_feeditem.tmpl":
+            ensure => 'present',
+            source => template('planet/html/rd_feeditem.html.tmpl.erb');
+        }
     } else {
-        $theme_path = '/usr/share/planet-venus/theme/wikimedia'
-    }
-
-    # theme directory
-    file { "${theme_path}/${title}":
-        ensure  => 'directory',
-    }
-    # index.html template
-    file { "${theme_path}/${title}/index.html.tmpl":
-        ensure  => 'present',
-        content => template('planet/html/index.html.tmpl.erb');
-    }
-    # theme config file
-    file { "${theme_path}/${title}/config.ini":
-        source  => 'puppet:///modules/planet/theme/config.ini';
-    }
-    # style sheet for planet-venus
-    file { "${theme_path}/${title}/planet.css":
-        source  => "puppet:///modules/planet/theme/${css_file}";
+        file { "/usr/share/planet-venus/theme/wikimedia/${title}":
+            ensure  => 'directory',
+        }
+        # index.html template
+        file { "${theme_path}/${title}/index.html.tmpl":
+            ensure  => 'present',
+            content => template('planet/html/index.html.tmpl.erb');
+        }
+        # theme config file
+        file { "${theme_path}/${title}/config.ini":
+            source  => 'puppet:///modules/planet/theme/config.ini';
+        }
+        # style sheet for planet-venus
+        file { "${theme_path}/${title}/planet.css":
+            source  => "puppet:///modules/planet/theme/${css_file}";
+        }
     }
 }
