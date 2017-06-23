38c9b164446f441e6d18c9a7a14d2ef13a2d33df
Daniel Zahn <dzahn@wikimedia.org>
2017-06-23 11:29:12 -0700

    planet: give higher priority to English Apache site
    The English site is the one with by far the most feeds,
    and for debugging purposes it's useful to have it as the
    default Apache site when accessing a labs instance or prod
    server using the IP address, due to NameBased virtual hosts.
    
    Change-Id: Iec500411071311aa8fcfe72dd95821f9ef097d04

--- modules/planet/manifests/apachesite.pp
+++ modules/planet/manifests/apachesite.pp
@@ -1,8 +1,15 @@
 # defined type: an apache site config for a planet-venus language version
 define planet::apachesite {
 
+    if $title == "en" {
+        $priority = "10"
+    } else {
+        $priority = "50"
+    }
+
     apache::site { "${title}.planet.${planet::planet_domain_name}":
-        content => template('planet/apache/planet-language.erb'),
+        content  => template('planet/apache/planet-language.erb'),
+        priority => $priority,
     }
 
 }
