2be5c78cf115c5a7c8553a3b344b5c7cdd5403df
Daniel Zahn <dzahn@wikimedia.org>
2017-06-22 21:35:47 -0700

    icinga/role:mail::mx: add monitoring of exim queue size
    Adds a new Icinga plugin check_exim_queue using bash and
    exipick -i | wc -l as suggested on T133110.
    
    Adds NRPE monitoring service in MX role class assuming it
    is prefered that i put it there vs the module.
    
    Using 1000 for WARN and 3000 for CRIT. I observed values around
    300 on mx1001 the other day when testing it.
    
    Bug: T133110
    Change-Id: I70bdef87eed6902ad27c92f2fa0e19b3d2274d7d

---
+++ modules/icinga/files/check_exim_queue.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Nagios/Icinga plugin to check for oversized exim4 queues.
+#
+# Daniel Zahn - Wikimedia Foundation Inc.
+#
+# https://phabricator.wikimedia.org/T133110
+#
+# ./check_exim_queue -w <warn> -c <crit>
+#
+# <warn> = number of mails in queue that trigger a WARN (int)
+# <crit> = number of mails in queue that trigger a CRIT (int)
+#
+# dependencies: exipick, sudo
+
+usage() { echo "Usage: $0 -w <warn> -c <crit>" 1>&2; exit 1; }
+
+set -euo pipefail
+
+declare -i WARN_LIMIT
+declare -i CRIT_LIMIT
+
+while getopts "w:c:" o; do
+    case "${o}" in
+    w)
+       WARN_LIMIT=${OPTARG}
+       ;;
+    c)
+       CRIT_LIMIT=${OPTARG}
+       ;;
+    *)
+       usage
+       ;;
+    esac
+done
+
+if [ -z "${WARN_LIMIT}" ] || [ -z "${CRIT_LIMIT}" ]; then
+    usage
+fi
+
+declare -i QSIZE
+
+SUDO="/usr/bin/sudo"
+EXIPICK="/usr/sbin/exipick"
+
+QSIZE="$(${SUDO} ${EXIPICK} -bpc)"
+
+# echo "QSIZE: ${QSIZE} WARN: ${WARN_LIMIT} CRIT: ${CRIT_LIMIT}"
+
+if [ "$QSIZE" -ge "$CRIT_LIMIT" ] ; then
+    echo "CRITICAL: ${QSIZE} mails in exim queue."
+    exit 2
+fi
+
+if [ "$QSIZE" -ge "$WARN_LIMIT" ] ; then
+    echo "WARNING: ${QSIZE} mails in exim queue."
+    exit 1
+fi
+
+if [ "$QSIZE" -lt "$WARN_LIMIT" ] && [ "$QSIZE" -lt "$CRIT_LIMIT" ] ; then
+    echo "OK: Less than ${WARN_LIMIT} mails in exim queue."
+    exit 0
+fi
+
+echo "UNKNOWN: something went wrong. check plugin ($0)."
+exit 3
+
