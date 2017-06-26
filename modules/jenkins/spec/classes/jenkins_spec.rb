5bd53046228cf186bb0eb69b9a7bdbb95c14652d
Antoine Musso <hashar@free.fr>
2017-06-26 23:51:56 +0200

    jenkins: fix spec
    os_version needs lsbdistid/lisbdistrelease. Provide them as default
    facts targetting Jessie.
    
    Change-Id: I0a3f898ff46e2b80dec47d4e10e9a764361cc93e

--- modules/jenkins/spec/classes/jenkins_spec.rb
+++ modules/jenkins/spec/classes/jenkins_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
 describe 'jenkins' do
-    let(:facts) { {
-        :initsystem => 'systemd',  # For systemd::syslog
-    } }
     let(:params) { {
         :prefix => '/ci',
     } }
