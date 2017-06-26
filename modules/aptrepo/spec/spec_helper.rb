c31a798cf764c431a0b691b96ff0d389b159a356
Antoine Musso <hashar@free.fr>
2017-06-26 23:45:58 +0200

    aptrepo: fix spec
    Tests depends on wmflib and apt (for apt::pin)
    Stub Exec['apt-get update']
    
    Provide default facts for Jessie. A better fix would be to use
    https://github.com/mcanevet/rspec-puppet-facts and run tests against
    multiple supported operating systems.
    
    Change-Id: I95af3d6ee189495de4c3bd7cc4107164b85e7803

--- modules/aptrepo/spec/spec_helper.rb
+++ modules/aptrepo/spec/spec_helper.rb
@@ -5,4 +5,9 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.default_facts = {
+    :initsystem => 'systemd',
+    :lsbdistid => 'Debian',
+    :lsbdistrelease => '8.0',
+  }
 end
