2094af8e32dcc6e9a1ca6628b101c854016f6929
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-06-27 12:51:54 +0200

    swift: use implicit /dev/swift prefix for swift devices
    Remove /dev prefix from puppet-managed swift disks and use '/dev/swift' as
    implicit prefix instead.
    
    Bug: T163673
    Change-Id: I90632b779ec271612843d0bb80aa733b7c0070f0

--- modules/swift/manifests/init_device.pp
+++ modules/swift/manifests/init_device.pp
@@ -1,15 +1,16 @@
 define swift::init_device($partition_nr='1') {
-    if ($title !~ /^\/dev\/([hvs]d[a-z]+|md[0-9]+)$/) {
-        fail("unable to init ${title} for swift")
+    if ($title !~ /^([hvs]d[a-z]+|md[0-9]+)$/) {
+        fail("Invalid name ${title} for swift::init_device")
     }
 
-    $dev           = "${title}${partition_nr}"
-    $dev_suffix    = regsubst($dev, '^\/dev\/(.*)$', '\1')
+    $dev           = "/dev/swift/${title}${partition_nr}"
+    $dev_nopart    = "/dev/swift/${title}"
+    $dev_suffix    = "${title}${partition_nr}"
     $fs_label      = "swift-${dev_suffix}"
-    $parted_cmd    = "parted --script --align optimal ${title}"
+    $parted_cmd    = "parted --script --align optimal ${dev_nopart}"
     $parted_script = "mklabel gpt mkpart ${fs_label} 1M 100%"
 
-    exec { "parted-${title}":
+    exec { "parted-${dev}":
         path    => '/usr/bin:/bin:/usr/sbin:/sbin',
         require => Package['parted'],
         command => "${parted_cmd} ${parted_script}",
@@ -19,11 +20,11 @@ define swift::init_device($partition_nr='1') {
     exec { "mkfs-${dev}":
         command => "mkfs -t xfs -L ${fs_label} -i size=512 ${dev}",
         path    => '/sbin/:/usr/sbin/',
-        require => [Package['xfsprogs'], Exec["parted-${title}"]],
+        require => [Package['xfsprogs'], Exec["parted-${dev}"]],
         unless  => "xfs_admin -l ${dev}",
     }
 
-    swift::mount_filesystem { $dev:
+    swift::mount_filesystem { $dev_suffix:
         require => Exec["mkfs-${dev}"],
     }
 }
