a365eddca287bbfb8218b988109162727c6cd60a
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-06-27 15:35:38 +0200

    profile: update labs swift storage symlink
    Bug: T163673
    Change-Id: I7469865e738beaecf712c311f4fcf863b7d475a2

--- modules/profile/manifests/swift/storage/labs.pp
+++ modules/profile/manifests/swift/storage/labs.pp
@@ -10,12 +10,12 @@ class profile::swift::storage::labs {
     extents      => '80%FREE',
   }
 
-  file { '/etc/udev/rules.d/99-swift-lvm.rules':
+  file { '/etc/udev/rules.d/90-swift-lvm.rules':
     ensure  => present,
     user    => 'root',
     group   => 'root',
     mode    => '0444',
-    content => 'ENV{DM_LV_NAME}=="lv-a1", ENV{DM_VG_NAME}=="vd", SYMLINK+="lv-a1"',
+    content => 'ENV{DM_LV_NAME}=="lv-a1", ENV{DM_VG_NAME}=="vd", SYMLINK+="swift/lv-a1"',
     notify  => Exec['swift_udev_reload'],
     require => Lvm::Logical_volume['lv-a1'],
   }
