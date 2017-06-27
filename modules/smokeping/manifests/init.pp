6804fa7e71b46fd77e40ce58e3b9e3fe641e15d1
Daniel Zahn <dzahn@wikimedia.org>
2017-06-26 17:22:59 -0700

    smokeping: allow rsync of data from netmon1001 to netmon1002
    Bug: T159756
    Change-Id: Id4ebd415e7eb6542e6e0226ce113bb54e7be1d47

--- modules/smokeping/manifests/init.pp
+++ modules/smokeping/manifests/init.pp
@@ -20,4 +20,23 @@ class smokeping {
         ],
         subscribe => File['/etc/smokeping/config.d'],
     }
+
+
+    $sourceip='208.80.154.159'
+
+    ferm::service { 'smokeping-migration-rsync':
+        ensure => present,
+        proto  => 'tcp',
+        port   => '873',
+        srange => "${sourceip}/32",
+    }
+
+    include rsync::server
+
+    rsync::server::module { 'smokeping_data':
+        ensure      => present,
+        path        => '/var/lib/smokeping',
+        read_only   => 'no',
+        hosts_allow => $sourceip,
+    }
 }
