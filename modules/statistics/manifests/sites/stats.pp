ba02db5c84d4b44e923f68b9bcaae2e4b2ec6fa9
milimetric <dandreescu@wikimedia.org>
2017-06-28 21:49:26 -0400

    [WIP don't merge yet, still working on the actual code to deploy] Clone wikistats v2 repository and link it to v2
    This sets up wikistats v2 to serve at stats.wikimedia.org/v2
    
    NOTE: the depth argument I used is supported here, not sure if I used it
    properly as I didn't find any examples of it in the rest of the repo:
    https://github.com/wikimedia/puppet/blob/production/modules/git/manifests/clone.pp#L127
    
    Bug: T167684
    Change-Id: I6cff8fcf70a91a922f9c37ba3b5f48dee2bd7835

--- modules/statistics/manifests/sites/stats.pp
+++ modules/statistics/manifests/sites/stats.pp
@@ -6,7 +6,10 @@ class statistics::sites::stats {
     require ::geowiki::private_data
     include ::geowiki::params
 
-    $geowiki_private_directory     = '/srv/stats.wikimedia.org/htdocs/geowiki-private'
+    $wikistats_web_directory       = '/srv/stats.wikimedia.org'
+    $wikistats_v2_link             = "${wikistats_web_directory}/v2"
+    $wikistats_source_directory    = '/srv/src/wikistats-v2'
+    $geowiki_private_directory     = "${wikistats_web_directory}/htdocs/geowiki-private"
     $geowiki_private_htpasswd_file = '/etc/apache2/htpasswd.stats-geowiki'
 
     # add htpasswd file for stats.wikimedia.org
@@ -36,5 +39,26 @@ class statistics::sites::stats {
 
     apache::site { 'stats.wikimedia.org':
         content => template('statistics/stats.wikimedia.org.erb'),
+    }
+
+    # stats.wikimedia.org/v2 (Wikistats 2.0) setup
+    # wikistats v2 is cloned and has a built version available in /dist
+    git::clone { 'wikistats-v2':
+        ensure    => 'latest',
+        depth     => '1',
+        directory => $wikistats_source_directory,
+        origin    => 'https://phabricator.wikimedia.org/source/wikistats.git',
+        owner     => 'root',
+        group     => 'statistics-web-users',
+        mode      => '0775',
+    }
+
+    # it is then linked to serve at http://stats.wikimedia.org/v2
+    file { $wikistats_v2_link:
+        ensure => 'link',
+        target => "${wikistats_source_directory}/dist",
+        owner  => 'root',
+        group  => 'www-data',
+        mode   => '0750',
     }
 }
