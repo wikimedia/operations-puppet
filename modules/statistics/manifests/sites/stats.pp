61ed358b8e5fa445e950f9dcc71a37746bb48df0
milimetric <dandreescu@wikimedia.org>
2017-06-28 21:49:26 -0400

    Clone wikistats v2 repository and link it to v2
    This sets up wikistats v2 to serve at stats.wikimedia.org/v2
    
    Bug: T167684
    Change-Id: I6cff8fcf70a91a922f9c37ba3b5f48dee2bd7835

--- modules/statistics/manifests/sites/stats.pp
+++ modules/statistics/manifests/sites/stats.pp
@@ -11,7 +11,10 @@ class statistics::sites::stats {
     }
     require ::geowiki::private_data
 
-    $geowiki_private_directory     = '/srv/stats.wikimedia.org/htdocs/geowiki-private'
+    $wikistats_web_directory       = '/srv/stats.wikimedia.org'
+    $wikistats_v2_link             = "${wikistats_web_directory}/v2"
+    $wikistats_source_directory    = '/srv/src/wikistats-v2'
+    $geowiki_private_directory     = "${wikistats_web_directory}/htdocs/geowiki-private"
     $geowiki_private_htpasswd_file = '/etc/apache2/htpasswd.stats-geowiki'
 
     # add htpasswd file for stats.wikimedia.org
@@ -41,5 +44,34 @@ class statistics::sites::stats {
 
     apache::site { 'stats.wikimedia.org':
         content => template('statistics/stats.wikimedia.org.erb'),
+    }
+
+    file { $wikistats_source_directory:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'www-data',
+        mode   => '0775',
+    }
+
+    # stats.wikimedia.org/v2 (Wikistats 2.0) setup
+    # wikistats v2 is cloned and has a built version available in /dist
+    git::clone { 'wikistats-v2':
+        ensure    => 'latest',
+        require   => File[$wikistats_source_directory],
+        directory => $wikistats_source_directory,
+        branch    => 'master',
+        origin    => 'https://phabricator.wikimedia.org/source/wikistats.git',
+        owner     => 'root',
+        group     => 'www-data',
+        mode      => '0775',
+    }
+
+    # it is then linked to serve at http://stats.wikimedia.org/v2
+    file { $wikistats_v2_link:
+        ensure => 'link',
+        target => "${wikistats_source_directory}/dist",
+        owner  => 'root',
+        group  => 'www-data',
+        mode   => '0750',
     }
 }
