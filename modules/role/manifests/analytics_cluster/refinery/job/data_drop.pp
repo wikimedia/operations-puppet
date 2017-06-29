6eb7ef77d8177a3fd39c517dd4460057784b4f91
joal <joal@wikimedia.org>
2017-06-29 10:05:48 +0200

    Add cron job dropping webrequest from druid
    We put sampled webrequest data in druid for fast analysis.
    This patch provides a cron job deleting data from druid deep-storage
    after 60 days to enforce our data rentention policy.
    This patch also adds a MAILTO parameter to data-dropping
    crons, sending emails to analytics-alert.
    
    Bug: T168614
    Change-Id: I7770655b8f2b98fad751a90aee6e8b771a0851b5

--- modules/role/manifests/analytics_cluster/refinery/job/data_drop.pp
+++ modules/role/manifests/analytics_cluster/refinery/job/data_drop.pp
@@ -5,18 +5,23 @@
 class role::analytics_cluster::refinery::job::data_drop {
     require ::role::analytics_cluster::refinery
 
-    $webrequest_log_file     = "${role::analytics_cluster::refinery::log_dir}/drop-webrequest-partitions.log"
-    $eventlogging_log_file   = "${role::analytics_cluster::refinery::log_dir}/drop-eventlogging-partitions.log"
-    $wdqs_extract_log_file   = "${role::analytics_cluster::refinery::log_dir}/drop-wdqs-extract-partitions.log"
-    $mediawiki_log_file      = "${role::analytics_cluster::refinery::log_dir}/drop-mediawiki-log-partitions.log"
+    $webrequest_log_file       = "${role::analytics_cluster::refinery::log_dir}/drop-webrequest-partitions.log"
+    $eventlogging_log_file     = "${role::analytics_cluster::refinery::log_dir}/drop-eventlogging-partitions.log"
+    $wdqs_extract_log_file     = "${role::analytics_cluster::refinery::log_dir}/drop-wdqs-extract-partitions.log"
+    $mediawiki_log_file        = "${role::analytics_cluster::refinery::log_dir}/drop-mediawiki-log-partitions.log"
+    $druid_webrequest_log_file = "${role::analytics_cluster::refinery::log_dir}/drop-druid-webrequest.log"
 
     # Shortcut var to DRY up cron commands.
     $env = "export PYTHONPATH=\${PYTHONPATH}:${role::analytics_cluster::refinery::path}/python"
+
+    # Send an email to analytics in case of failure
+    $mail_to = 'analytics-alerts@wikimedia.org'
 
     # Keep this many days of raw webrequest data.
     $raw_retention_days = 31
     cron { 'refinery-drop-webrequest-raw-partitions':
         command => "${env} && ${role::analytics_cluster::refinery::path}/bin/refinery-drop-webrequest-partitions -d ${raw_retention_days} -D wmf_raw -l /wmf/data/raw/webrequest -w raw >> ${webrequest_log_file} 2>&1",
+        environment => "MAILTO=${mail_to}",
         user    => 'hdfs',
         minute  => '15',
         hour    => '*/4',
@@ -26,6 +31,7 @@ class role::analytics_cluster::refinery::job::data_drop {
     $refined_retention_days = 90
     cron { 'refinery-drop-webrequest-refined-partitions':
         command => "${env} && ${role::analytics_cluster::refinery::path}/bin/refinery-drop-webrequest-partitions -d ${refined_retention_days} -D wmf -l /wmf/data/wmf/webrequest -w refined >> ${webrequest_log_file} 2>&1",
+        environment => "MAILTO=${mail_to}",
         user    => 'hdfs',
         minute  => '45',
         hour    => '*/4',
@@ -35,6 +41,7 @@ class role::analytics_cluster::refinery::job::data_drop {
     $eventlogging_retention_days = 90
     cron {'refinery-drop-eventlogging-partitions':
         command => "${env} && ${role::analytics_cluster::refinery::path}/bin/refinery-drop-eventlogging-partitions -d ${eventlogging_retention_days} -l /wmf/data/raw/eventlogging >> ${eventlogging_log_file} 2>&1",
+        environment => "MAILTO=${mail_to}",
         user    => 'hdfs',
         minute  => '15',
         hour    => '*/4',
@@ -44,6 +51,7 @@ class role::analytics_cluster::refinery::job::data_drop {
     $wdqs_extract_retention_days = 90
     cron {'refinery-drop-wdqs-extract-partitions':
         command => "${env} && ${role::analytics_cluster::refinery::path}/bin/refinery-drop-hourly-partitions -d ${wdqs_extract_retention_days} -p hive -D wmf -t wdqs_extract -l /wmf/data/wmf/wdqs_extract >> ${wdqs_extract_log_file} 2>&1",
+        environment => "MAILTO=${mail_to}",
         user    => 'hdfs',
         minute  => '0',
         hour    => '1',
@@ -53,14 +61,25 @@ class role::analytics_cluster::refinery::job::data_drop {
     $mediawiki_log_retention_days = 90
     cron {'refinery-drop-apiaction-partitions':
         command => "export PYTHONPATH=\${PYTHONPATH}:${role::analytics_cluster::refinery::path}/python && ${role::analytics_cluster::refinery::path}/bin/refinery-drop-hourly-partitions -d ${mediawiki_log_retention_days} -D wmf_raw -t apiaction -p camus -l /wmf/data/raw/mediawiki/mediawiki_ApiAction/hourly >> ${mediawiki_log_file} 2>&1",
+        environment => "MAILTO=${mail_to}",
         user    => 'hdfs',
         minute  => '15',
         hour    => '*/4',
     }
     cron {'refinery-drop-cirrussearchrequestset-partitions':
         command => "export PYTHONPATH=\${PYTHONPATH}:${role::analytics_cluster::refinery::path}/python && ${role::analytics_cluster::refinery::path}/bin/refinery-drop-hourly-partitions -d ${mediawiki_log_retention_days} -D wmf_raw -t cirrussearchrequestset -p camus -l /wmf/data/raw/mediawiki/mediawiki_CirrusSearchRequestSet/hourly >> ${mediawiki_log_file} 2>&1",
+        environment => "MAILTO=${mail_to}",
         user    => 'hdfs',
         minute  => '25',
         hour    => '*/4',
+    }
+    # keep this many days of druid webrequest sampled
+    $druid_webrequest_sampled_retention_days = 60
+    cron {'refinery-drop-webrequest-sampled-druid':
+        command => "${env} && ${role::analytics_cluster::refinery::path}/bin/refinery-drop-druid-deep-storage-data -d ${druid_webrequest_sampled_retention_days} webrequest >> ${$druid_webrequest_log_file} 2>&1",
+        environment => "MAILTO=${mail_to}",
+        user    => 'hdfs',
+        minute  => '15',
+        hour    => '5'
     }
 }
