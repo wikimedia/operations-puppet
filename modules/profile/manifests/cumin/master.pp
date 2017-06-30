c8e6312aa2fd473cc006fa36626c13d8c2f16032
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-06-30 12:21:39 +0200

    Cumin: add cache generator for emergencies
    - In case of emergencies in which PuppetDB might have an outage too, it
      will be useful to have precompiled lists of hosts to be used with
      Cumin's '--backend direct' option to quickly target large batch of
      hosts. The list of generated host lists is configurable.
    
    Bug: T169304
    Change-Id: I94d980b9ded939fe6b35e5871b8ea0d5eb231c2e

--- modules/profile/manifests/cumin/master.pp
+++ modules/profile/manifests/cumin/master.pp
@@ -1,12 +1,13 @@
 class profile::cumin::master (
     $puppetdb_host  = hiera('puppetmaster::puppetdb::master'),
     $cumin_log_path = '/var/log/cumin',
+    $datacenters    = hiera('datacenters'),
 ) {
     ::keyholder::agent { 'cumin_master':
         trusted_groups => ['root'],
     }
 
-    require_package('cumin')
+    require_package(['cumin', 'python-yaml'])
 
     file { $cumin_log_path:
         ensure => directory,
@@ -22,6 +23,13 @@ class profile::cumin::master (
         mode   => '0750',
     }
 
+    file { '/etc/cumin/host_lists':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0750',
+    }
+
     file { '/etc/cumin/config.yaml':
         ensure  => present,
         owner   => 'root',
@@ -30,4 +38,36 @@ class profile::cumin::master (
         content => template('profile/cumin/config.yaml.erb'),
         require => File['/etc/cumin'],
     }
+
+    file { '/etc/cumin/cache-generator-config.yaml':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        content => template('profile/cumin/cache-generator-config.yaml.erb'),
+        require => File['/etc/cumin'],
+    }
+
+    file { '/usr/local/sbin/cumin-cache-generator':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0750',
+        source => 'puppet:///modules/profile/cumin/cumin_cache_generator.py',
+        require => [
+            File['/etc/cumin/host_lists'],
+            File['/etc/cumin/cache-generator-config.yaml'],
+            Package['python-yaml'],
+        ],
+    }
+
+    cron { 'cumin-cache-generator':
+        ensure  => present,
+        user    => 'root',
+        command => '/usr/local/sbin/cumin-cache-generator',
+        hour    => '4',
+        minute  => '19',
+        require => File['/usr/local/sbin/cumin-cache-generator'],
+    }
+
 }
