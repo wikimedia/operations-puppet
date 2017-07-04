b7b7ee2d9f71fb9273b9fd298e3c5475a7bcc8bd
Jcrespo <jcrespo@wikimedia.org>
2017-07-04 16:43:27 +0200

    mariadb: Support multiple instances directly on the module
    Also use it on a new role dbstore_multiinstance as a test.
    
    The idea is to have a common mariadb::config, and then one
    resouce mariadb::instance usage for each mysql run, with its own
    datadir, tmpdir, port and socket. The custom config should include
    the extra config files on /etc/mysql/mysqld.config.d and either
    init.d or systemd (with mariadb@.service) should be able to
    handle the different configs.
    
    Many questions are still left about the details, such as monitoring
    - this is just a first test approximation.
    
    Bug: T169514
    Change-Id: Ib313d12e6784700e0afaccd443acb181b7a0e1d4

---
+++ modules/mariadb/manifests/instance.pp
@@ -0,0 +1,39 @@
+# Setups additional instances for hosts that hosts more
+# than one instance
+define mariadb::instance(
+    $port,
+    $datadir = 'undefined',
+    $tmpdir  = 'undefined',
+    $socket  = 'undefined',
+) {
+    if $datadir == 'undefined' {
+        $datadir = "/srv/sqldata.${title}"
+    }
+    if $tmpdir == 'undefined' {
+        $tmpdir  = "/srv/tmp.${title}"
+    }
+    if $tmpdir == 'undefined' {
+        $socket  = "/run/mysqld/mysqld.${title}.sock"
+    }
+
+    file { $datadir:
+        ensure => directory,
+        owner  => 'mysql',
+        group  => 'mysql',
+        mode   => '0755',
+    }
+
+    file { $tmpdir:
+        ensure => directory,
+        owner  => 'mysql',
+        group  => 'mysql',
+        mode   => '0755',
+    }
+
+    file { "/etc/mysql/mysqld.conf.d/${title}.cnf":
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('mariadb/instance.cnf.erb'),
+    }
+}
