ffdb9c5e2be54b02ae0ae43d8f451ed92a20e749
Jcrespo <jcrespo@wikimedia.org>
2017-07-04 16:43:27 +0200

    [WIP] Support multiple instances on the mariadb module
    This is just a first idea that doesn't work yet, but could be
    one of the ways to implement it.
    
    The idea is to have a common mariadb::config, and then one
    resouces mariadb::instance usage for each mysql run, with its own
    datadir, tmpdir, port and socket. The custom config should include
    the extra config files on /etc/mysql/mysqld.config.d and either
    init.d or systemd (with mariadb@.service) should be able to
    handle the different configs.
    
    Many questions are still left about the details.
    
    Bug: T169514
    Change-Id: Ib313d12e6784700e0afaccd443acb181b7a0e1d4

---
+++ modules/role/manifests/mariadb/dbstore3.pp
@@ -0,0 +1,55 @@
+class role::mariadb::dbstore3 {
+    system::role { 'mariadb::core':
+        description => "DBStore multi-instance Server",
+    }
+
+    include ::standard
+    include ::base::firewall
+    include role::mariadb::monitor
+    #TODO: Custom firewall rules
+
+    #TODO: define one group per shard
+    class {'mariadb::groups':
+        mysql_group => 'dbstore',
+        mysql_shard => 's1',
+        mysql_role  => 'slave',
+        socket      => '/run/mysqld/mysqld.s1.sock',
+    }
+
+    class {'mariadb::packages_wmf': }
+    class {'mariadb::service':
+        # for now we will keep things simple, we probably should have a
+        # higher-level interface with templates
+        override => "[Service]\nLimitNOFILE=200000",
+    }
+
+    # Read only forced on also for the masters of the primary datacenter
+    class { 'mariadb::config':
+        config           => 'role/mariadb/mysqld_config/dbstore3.my.cnf.erb',
+        p_s              => 'on',
+        ssl              => 'puppet-cert',
+        binlog_format    => 'ROW',
+    }
+
+    mariadb::instance {'s1':
+        port => 3311,
+    }
+    mariadb::instance {'s2':
+        port => 3312,
+    }
+    mariadb::instance {'s3':
+        port => 3313,
+    }
+    mariadb::instance {'s4':
+        port => 3314,
+    }
+    mariadb::instance {'s5':
+        port => 3315,
+    }
+    mariadb::instance {'s6':
+        port => 3316,
+    }
+    mariadb::instance {'s7':
+        port => 3317,
+    }
+}
