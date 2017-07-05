9752efbba13ce66c09722d4cc0f413f1ae9cc551
Antoine Musso <hashar@free.fr>
2017-07-05 14:38:12 +0200

    contint: profile, role, and packages for R language
    Bug: T153856
    Change-Id: I72825701f624b495376cd9967dfff53b91632b97

---
+++ modules/profile/manifests/rlang/dev.pp
@@ -0,0 +1,18 @@
+# == Class profile::rlang::dev
+#
+# A profile that configures the environment for installing R packages from
+# sources like Git/GitHub and enables checking package sources with unit tests
+# and lint checking.
+#
+class profile::rlang::dev {
+
+    # `include ::r` would not install devtools, which would mean that we could
+    # not install R packages from Git/GitHub
+    class { 'r':
+        devtools => true,
+    }
+
+    # For unit testing and lint checking:
+    r::cran(['testthat', 'lintr'])
+
+}
