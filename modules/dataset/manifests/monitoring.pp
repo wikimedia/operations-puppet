cb7621fd8a6adbf4d845af1cb06c4ae7bcc298c2
Ariel T. Glenn <ariel@wikimedia.org>
2017-07-06 11:15:34 +0300

    monitor dataset hosts for nfsd cpu usage
    Bug: 169680
    Change-Id: I3710b749481035cb59850da44c08eb94c98b2086

---
+++ modules/dataset/manifests/monitoring.pp
@@ -0,0 +1,19 @@
+class dataset::monitoring {
+    Class['dataset::nfs'] -> Class['dataset::monitoring']
+
+    file { 'check_nfsd':
+        path   => '/usr/lib/nagios/plugins/check_nfsd',
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/dataset/check_nfsd',
+    }
+
+    nrpe::monitor_service { "nfsd":
+        ensure        => 'present',
+        description   => "nfsd cpu usage",
+        nrpe_command  => "/usr/lib/nagios/plugins/check_nfsd",
+        contact_group => 'admins',
+        retries       => 10,
+    }
+}
