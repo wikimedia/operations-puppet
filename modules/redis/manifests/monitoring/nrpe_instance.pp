9c72275b7da7a87134aba67a19ef9a28696735a7
elukey <ltoscano@wikimedia.org>
2017-07-07 11:42:00 +0200

    redis::monitoring::nrpe_instance: set retry_interval to 2 mins
    By default this check is retried 3 times with a retry_interval of 1 min.
    When a slave restarts and issue a SYNC to the master it will take
    a couple of minutes for it to bootstrap correctly (sync with the master
    and load data in memory).
    This new setting should remove daily false positives due to Redis
    instance reboots (and similar use cases).
    
    Change-Id: Ie05d8a093389169098db0cb307c968a5284fa5fd

--- modules/redis/manifests/monitoring/nrpe_instance.pp
+++ modules/redis/manifests/monitoring/nrpe_instance.pp
@@ -3,9 +3,10 @@ define redis::monitoring::nrpe_instance($replica_warning=60, $replica_critical=6
     $port = $title
     $cmd = $::redis::monitoring::nrpe::nrpe_command
     nrpe::monitor_service { "redis_status_on_port_${port}":
-        ensure        => present,
-        description   => "Check health of redis instance on ${port}",
-        nrpe_command  => "/usr/bin/sudo ${cmd} ${port} ${replica_warning} ${replica_critical}",
-        contact_group => 'admins',
+        ensure         => present,
+        description    => "Check health of redis instance on ${port}",
+        nrpe_command   => "/usr/bin/sudo ${cmd} ${port} ${replica_warning} ${replica_critical}",
+        contact_group  => 'admins',
+        retry_interval => 2,
     }
 }
