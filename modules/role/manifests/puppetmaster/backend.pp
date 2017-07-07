54dac98ceaade5cd643dd0e6c375f29fdfbcc49b
andrewbogott <abogott@wikimedia.org>
2017-07-06 21:07:16 -0500

    Refactor puppetmaster roles into role/profiles.
    Soon I want to create a frontend/backend pair of puppetmasters
    for labs.  This patch adds a few new params to the new puppetmaster
    profiles which currently have no-op defaults but which I will
    override for labs puppetmasters.
    
    Change-Id: Id31dd7a6a5fecf039af013eef5fdca9b02ccae95

--- modules/role/manifests/puppetmaster/backend.pp
+++ modules/role/manifests/puppetmaster/backend.pp
@@ -4,35 +4,7 @@ class role::puppetmaster::backend {
     include ::standard
     include ::base::firewall
 
-    system::role { 'puppetmaster':
-        description => 'Puppetmaster backend'
-    }
+    include ::profile::pupetmaster::backend
 
-    $ca_server = hiera('puppetmaster::ca_server', 'puppetmaster1001.eqiad.wmnet')
-
-    class { '::role::puppetmaster::common':
-        base_config           => {
-            'ca'              => false,
-            'ca_server'       => $ca_server,
-            'stringify_facts' => false,
-        }
-    }
-
-    class { '::puppetmaster':
-        server_type => 'backend',
-        config      => $::role::puppetmaster::common::config
-    }
-
-    $puppetmaster_frontend_ferm = join(keys(hiera('puppetmaster::servers')), ' ')
-    ferm::service { 'ssh_puppet_merge':
-        proto  => 'tcp',
-        port   => '22',
-        srange => "(@resolve((${puppetmaster_frontend_ferm})) @resolve((${puppetmaster_frontend_ferm}), AAAA))"
-    }
-    ferm::service { 'puppetmaster-backend':
-        proto  => 'tcp',
-        port   => 8141,
-        srange => "(@resolve((${puppetmaster_frontend_ferm})) @resolve((${puppetmaster_frontend_ferm}), AAAA))"
-    }
     require ::profile::conftool::client
 }
