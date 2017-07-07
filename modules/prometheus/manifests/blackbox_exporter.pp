da05acca25b20e3ff1b39016f20bcd534d940027
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-07-07 13:13:09 +0200

    prometheus: use blackbox-exporter package from Debian
    Drop our custom systemd unit in favour of the standard one shipped by Debian
    package. In the past we used to have a custom package for blackbox-exporter.
    Also adjust the configuration path.
    
    Bug: T169860
    Change-Id: Id5238f79af0ca8b1010e28d54082fa07f543581f

--- modules/prometheus/manifests/blackbox_exporter.pp
+++ modules/prometheus/manifests/blackbox_exporter.pp
@@ -9,7 +9,7 @@ class prometheus::blackbox_exporter{
 
     require_package('prometheus-blackbox-exporter')
 
-    file { '/etc/prometheus-blackbox_exporter.yml':
+    file { '/etc/prometheus/blackbox.yml':
         ensure  => present,
         mode    => '0444',
         owner   => 'root',
@@ -18,10 +18,10 @@ class prometheus::blackbox_exporter{
         notify  => Service['prometheus-blackbox-exporter'],
     }
 
-    systemd::service { 'prometheus-blackbox-exporter':
-        ensure  => present,
-        restart => true,
-        content => systemd_template('prometheus-blackbox-exporter'),
-        require => Package['prometheus-blackbox-exporter'],
+    service { 'prometheus-blackbox-exporter':
+        ensure     => running,
+        hasrestart => true,
+        provider   => 'systemd',
+        require    => Package['prometheus-blackbox-exporter'],
     }
 }
