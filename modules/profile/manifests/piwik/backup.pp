de82c346d1d5356b18affa84de5293522188abfb
elukey <ltoscano@wikimedia.org>
2017-07-10 14:50:08 +0200

    role::piwik::server: add regular bacula backups
    Bug: T164073
    Change-Id: Id35e45c1fa848c634b6dd893927b139276c66f35

---
+++ modules/profile/manifests/piwik/backup.pp
@@ -0,0 +1,47 @@
+# == Class: profile::piwik::backup
+#
+# Periodical backups of the Piwik database tables using Bacula.
+#
+class profile::piwik::backup (
+    $backup_username    = hiera('profile::piwik::backup::username'),
+    $backup_password    = hiera('profile::piwik::backup::password'),
+    $retention_days     = 14,
+) {
+    include ::profile::backup::host
+
+    file { '/srv/backups':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0600', # implicitly 0700 for dirs
+    }
+
+    file { '/etc/mysql/conf.d/dumps.cnf':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0400',
+        content => "[client]\nuser=${backup_username}\npassword=${backup_password}\n",
+    }
+
+    cron { 'delete_old_backups':
+        command => "/usr/bin/find /srv/backup -mtime +${retention_days} -exec /bin/rm {} \\;",
+        hour    => 0,
+        minute  => 0,
+        user    => 'root',
+    }
+
+    backup::mysqlset {'piwik':
+        xtrabackup       => false,
+        per_db           => true,
+        innodb_only      => true,
+        binlog           => false,
+        slave            => false,
+        local_dump_dir   => '/srv/backups',
+        password_file    => '/etc/mysql/conf.d/dumps.cnf',
+        method           => 'predump',
+        mysql_binary     => '/usr/bin/mysql',
+        mysqldump_binary => '/usr/bin/mysqldump',
+        jobdefaults      => "Weekly-${profile::backup::host::day}-${profile::backup::host::pool}",
+    }
+}
