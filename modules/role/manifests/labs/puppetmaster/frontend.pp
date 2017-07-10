b09c1ee66c07cf84887a1fced6731685067e0ee5
andrewbogott <abogott@wikimedia.org>
2017-07-10 12:08:55 -0500

    Rough in new labs puppetmaster roles
    Change-Id: I1ff3033c547e050e643f3e832d55d6ab83c69ebf

---
+++ modules/role/manifests/labs/puppetmaster/frontend.pp
@@ -0,0 +1,32 @@
+# vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab textwidth=80 smarttab
+
+class role::labs::puppetmaster::frontend() {
+    system::role { 'puppetmaster':
+        description => 'Puppetmaster frontend'
+    }
+
+    include ::base::firewall
+
+    include ::profile::backup::host
+    include ::profile::puppetmaster::labsenc
+    include ::profile::puppetmaster::labsencapi
+
+    $config = {
+        'node_terminus'     => 'exec',
+        'external_nodes'    => '/usr/local/bin/puppet-enc',
+        'thin_storeconfigs' => false,
+        'autosign'          => true,
+    }
+
+    class { '::profile::puppetmaster::frontend':
+        config => $config,
+        secure_private => false,
+    }
+
+    include ::profile::conftool::client
+    include ::profile::conftool::master
+
+    # config-master.wikimedia.org
+    include ::profile::configmaster
+    include ::profile::discovery::client
+}
