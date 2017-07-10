0e85a73f3252aacf46fa031bf32124808207a915
andrewbogott <abogott@wikimedia.org>
2017-07-10 14:10:11 -0500

    Move labs puppet enc into a profile
    Change-Id: Ib05efca8e49580042129b1add5f25c300e9ef4db

--- modules/role/manifests/puppetmaster/standalone.pp
+++ modules/role/manifests/puppetmaster/standalone.pp
@@ -47,26 +47,7 @@ class role::puppetmaster::standalone(
         fail('Ldap puppet node definitions are no longer supported.  The $use_enc param must be true.')
     }
 
-    # Setup ENC
-    require_package('python3-yaml', 'python3-ldap3')
-
-    include ldap::yamlcreds
-
-    file { '/etc/puppet-enc.yaml':
-        content => ordered_yaml({
-            host => hiera('labs_puppet_master'),
-        }),
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-    }
-
-    file { '/usr/local/bin/puppet-enc':
-        source => 'puppet:///modules/role/labs/puppet-enc.py',
-        mode   => '0555',
-        owner  => 'root',
-        group  => 'root',
-    }
+    include ::profile::puppetmaster::labsenc
 
     $config = {
         'node_terminus'     => 'exec',
