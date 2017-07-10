6334b2c53ad2bc9a1b33f378bb15276594d9d767
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-07-10 11:30:01 +0200

    Remove ferm service for striker / 8081
    Only accessed on localhost, so the ferm service can be removed now (earlier
    it was shared with other services using service::uwsgi)
    
    Bug: T169070
    Change-Id: I46a5207088f4c7ea18d3a70aa5980790ee068ce6

--- modules/role/manifests/striker/web.pp
+++ modules/role/manifests/striker/web.pp
@@ -9,10 +9,5 @@ class role::striker::web {
     include ::striker::apache
     include ::striker::uwsgi
     require ::passwords::striker
-
-    ferm::service { 'striker':
-        proto => 'tcp',
-        port  => '8081',
-    }
 }
 # vim:sw=4:ts=4:sts=4:ft=puppet:
