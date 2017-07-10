d5488e70d5f6c8716f0131f7b06a3ff3a5b10b99
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-07-10 08:34:03 +0200

    recommendation api: refactor profile, remove module
    The dedicated module was of not much use at this point, just remove it.
    
    Bug: T148129
    Change-Id: I604520ad6e05cfe0e8cc7da0558bb02d2e99c9cc

--- modules/profile/manifests/recommendation_api.pp
+++ modules/profile/manifests/recommendation_api.pp
@@ -1,6 +1,15 @@
 # Profile class for recommendation_api
-class profile::recommendation_api {
+class profile::recommendation_api($wdqs_uri=hiera('profile::recommendation_api::wdqs_uri')) {
 
-    include ::recommendation_api
-
+    service::node { 'recommendation_api':
+        port              => 9632,
+        repo              => 'recommendation-api/deploy',
+        healthcheck_url   => '',
+        has_spec          => true,
+        deployment        => 'scap3',
+        deployment_config => true,
+        deployment_vars   => {
+            wdqs_uri => $wdqs_uri,
+        },
+    }
 }
