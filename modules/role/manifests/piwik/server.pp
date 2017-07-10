d6ab1c3a5ea74de946c70e0809c48cecc7164b82
elukey <ltoscano@wikimedia.org>
2017-07-10 14:50:08 +0200

    role::piwik::server: add regular bacula backups
    Bug: T164073
    Change-Id: Id35e45c1fa848c634b6dd893927b139276c66f35

--- modules/role/manifests/piwik/server.pp
+++ modules/role/manifests/piwik/server.pp
@@ -4,9 +4,10 @@ class role::piwik::server {
     include ::standard
     include ::profile::piwik::webserver
     include ::profile::piwik::instance
+    # override profile::backup::enable to disable regular backups
+    include ::profile::piwik::backup
 
     # TODO - puppetization of mysql instance
-    # Ref: T159136
     require_package('mysql-server')
 
     system::role { 'piwik::server':
