937dbeafef297bc0995a051b92a9f6c67225f29a
Jaime Crespo <jcrespo@wikimedia.org>
2017-07-11 11:51:16 +0200

    [WIP]prometheus: Convert mysqld-exporter into multi-instance
    We need to monitor several independent mysql instances on the same
    host. Add an additional resource that actually can be duplicated
    so that more than one instance can be run at the same time.
    
    This is still work in progress, but uploaded so it can be discussed
    before more refactoring is done. Needs architecture feedback.
    
    Bug: T170666
    Change-Id: I664f04c7b026aedf32a1ef1ec648fea81dd47436

---
+++ modules/role/manifests/prometheus/mysqld_exporter_instance.pp
@@ -0,0 +1,30 @@
+define role::prometheus::mysqld_exporter_instance (
+    $socket ='/run/mysqld/mysqld.${title}.sock',
+    $port = 13306,
+    ) {
+
+    prometheus::mysqld_exporter { "${title}":
+        client_socket   => $socket,
+        # TODO: collect also TokuDB metrics, but only from
+        # selected nodes
+        # TODO: collect table stats, but less frequently,
+        # and avoid s3/dbstore/labsdb hosts
+        arguments       => "-collect.global_status \
+-collect.global_variables \
+-collect.info_schema.processlist \
+-collect.info_schema.processlist.min_time 0 \
+-collect.slave_status \
+-collect.info_schema.tables false \
+",
+        port            => "${port}", 
+    }
+
+    $prometheus_nodes = hiera('prometheus_nodes')
+    $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
+
+    ferm::service { "prometheus-mysqld-exporter@${title}":
+        proto  => 'tcp',
+        port   => "${port}",
+        srange => "@resolve((${prometheus_ferm_nodes}))",
+    }
+}
