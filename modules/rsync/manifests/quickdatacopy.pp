2d274577f806cded849ae47d13ec1a9ba772641b
Daniel Zahn <dzahn@wikimedia.org>
2017-07-11 16:42:56 -0700

    rsync::quickdatacopy: make auto-sync via cron optional
    Add a parameter to disable the cronjob that automatically syncs
    date every 10 minutes.
    
    Sometimes i just want manual copy for a migration and not constant
    background syncing.
    
    But leaving it the default to be enabled.
    
    Change-Id: I14a3faa9341419dbec848b91a6271d1b82d66599

--- modules/rsync/manifests/quickdatacopy.pp
+++ modules/rsync/manifests/quickdatacopy.pp
@@ -16,6 +16,8 @@
 #
 # [*file_path*] What file within that document root do we need? (currently not used)
 #
+# [*auto_sync*] Whether to also have a cronjob that automatically syncs data or not (default: true)
+#
 # [*ensure*] The usual meaning, set to absent to clean up when done
 #
 define rsync::quickdatacopy(
@@ -23,6 +25,7 @@ define rsync::quickdatacopy(
   $dest_host,
   $module_path,
   $file_path = '*',
+  $auto_sync = 'true',
   $ensure = present,
   ) {
 
@@ -53,11 +56,13 @@ define rsync::quickdatacopy(
               mode    => '0755',
               content => template('rsync/quickdatacopy.erb'),
           }
-
-          cron { 'sync-rsync-data':
-              ensure  => $ensure,
-              minute  => '*/10',
-              command => "/usr/local/sbin/${title} >/dev/null 2>&1",
+ 
+          if $auto_sync {
+              cron { 'sync-rsync-data':
+                  ensure  => $ensure,
+                  minute  => '*/10',
+                  command => "/usr/local/sbin/${title} >/dev/null 2>&1",
+              }
           }
       }
 }
