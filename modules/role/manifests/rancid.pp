2d77335ee2dae0ed9ec6b678060d42d699df4825
Daniel Zahn <dzahn@wikimedia.org>
2017-07-11 16:29:54 -0700

    rancid: add rsync::quickdatacopy to sync /var/lib/rancid
    netmon1002 needs to be reinstalled again.
    
    rancid has already switched to it from netmon1001.
    
    rsync /var/lib/rancid data (GIT repo etc) from netmon1002
    to new netmon2001 before reinstalling netmon1002 another time,
    then copy data back later.
    
    Also, copy it to netmon2001 for new setup there anyways.
    
    Bug: T166180
    Bug: T159756
    Change-Id: I0d9aa1e9f55d26f7eececbde98036e59ede7b1d4

--- modules/role/manifests/rancid.pp
+++ modules/role/manifests/rancid.pp
@@ -10,4 +10,11 @@ class role::rancid {
     include ::profile::backup::host
 
     backup::set { 'rancid': }
+
+    rsync::quickdatacopy { 'var-lib-rancid':
+      ensure      => present,
+      source_host => 'netmon1002.wikimedia.org',
+      dest_host   => 'netmon2001.wikimedia.org',
+      module_path => '/var/lib/rancid',
+    }
 }
