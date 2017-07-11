e992c0b019ea2ca3c38673f9f42fe7b4dc1d01ce
Jaime Crespo <jcrespo@wikimedia.org>
2017-07-11 11:51:16 +0200

    prometheus: Convert mysqld-exporter into multi-instance
    We need to monitor several independent mysql instances on the same
    host. Add an additional resource that actually can be duplicated
    so that more than one instance can be run at the same time.
    
    For now this is implemented on a separate module and role manifest,
    but it should eventually substitute the real classes.
    
    A bug is corrected at the same time on the arguments, which will
    is not yet applied to the generic class.
    
    Bug: T170666
    Change-Id: I664f04c7b026aedf32a1ef1ec648fea81dd47436

---
+++ modules/prometheus/manifests/mysqld_exporter/instance.pp
@@ -0,0 +1,60 @@
+# == Define: prometheus::mysqld_exporter::instance
+#
+# Prometheus exporter for MySQL server metrics. The exporter is most effective
+# when ran alongside the MySQL server to be monitored, connecting via a local
+# UNIX socket is supported.
+#
+# = Parameters
+#
+# [*client_socket*]
+#   The socket to connect to.
+#
+# [*client_user*]
+#   MySQL user
+#
+# [*client_password*]
+#   MySQL password
+#
+# [*listen_address*]
+#   ip/host and port, colon separated, where the prometheus exporter will listen for
+#   http metrics requests. Host can be omitted.
+#
+# [*arguments*]
+#   Additional command line arguments for prometheus-mysqld-exporter.
+
+define prometheus::mysqld_exporter::instance (
+    $client_socket = '/run/mysqld/mysqld.sock',
+    $client_user = 'prometheus',
+    $client_password = '',
+    $listen_address = ':9104',
+    $arguments = '',
+) {
+    include prometheus::mysqld_exporter::common
+
+    $my_cnf = "/var/lib/prometheus/.my.${title}.cnf"
+    $service = "prometheus-mysqld-exporter@${title}"
+
+    file { "/etc/default/prometheus-mysqld-exporter@${title}":
+        ensure  => present,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => "ARGS=\"-web.listen-address=${listen_address} -config.my-cnf=${my_cnf} ${arguments}\"",
+        notify  => Service[$service],
+    }
+
+    # a separate database config (.my.<instance_name>cnf) for each instance monitored
+    # change the systemd unit if the patch changes here, as it depends on it
+    file { $my_cnf:
+        ensure  => present,
+        mode    => '0400',
+        owner   => 'prometheus',
+        group   => 'prometheus',
+        content => template('prometheus/mysqld_exporter.cnf.erb'),
+        notify  => Service[$service],
+    }
+
+    service { $service:
+        ensure  => running,
+    }
+}
