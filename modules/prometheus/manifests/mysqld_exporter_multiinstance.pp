989deb1c5302f0c20ef9a29443fcc7d5e58835e2
Jaime Crespo <jcrespo@wikimedia.org>
2017-07-11 11:51:16 +0200

    [WIP]prometheus: Convert mysqld-exporter into multi-instance
    We need to monitor several independent mysql instances on the same
    host. Add an additional resource that actually can be duplicated
    so that more than one instance can be run at the same time.
    
    This is still work in progress, but uploaded so it can be discussed
    before more refactoring is done. Needs architecture feedback.
    
    Bug: T169514
    Change-Id: I664f04c7b026aedf32a1ef1ec648fea81dd47436

---
+++ modules/prometheus/manifests/mysqld_exporter_multiinstance.pp
@@ -0,0 +1,65 @@
+# == Define: prometheus::mysqld_exporter
+#
+# Prometheus exporter for MySQL server metrics. The exporter is most effective
+# when ran alongside the MySQL server to be monitored, connecting via a local
+# UNIX socket is supported.
+#
+# = Parameters
+#
+# [*client_socket*]
+#   The socket to connect to.
+#
+# [*client_user*]
+#   MySQL user
+#
+# [*client_password*]
+#   MySQL password
+#
+# [*arguments*]
+#   Additional command line arguments for prometheus-mysqld-exporter.
+
+define prometheus::mysqld_exporter_multiinstance (
+    $client_socket = '/tmp/mysql.sock',
+    $client_user = 'prometheus',
+    $client_password = '',
+    $port = 9104,
+    $arguments = '',
+) {
+    #require_package('prometheus-mysqld-exporter')
+    #
+    #file { '/var/lib/prometheus':
+    #    ensure => directory,
+    #    mode   => '0550',
+    #    owner  => 'prometheus',
+    #    group  => 'prometheus',
+    #}
+
+    # default .my.cnf location (i.e. $HOME/.my.cnf)
+    file { "/var/lib/prometheus/${title}.my.cnf":
+        ensure  => present,
+        mode    => '0400',
+        owner   => 'prometheus',
+        group   => 'prometheus',
+        content => template('prometheus/mysqld_exporter.cnf.erb'),
+        require => [
+          Package['prometheus-mysqld-exporter'],
+          File['/var/lib/prometheus'],
+        ],
+        notify  => Service["prometheus-mysqld-exporter@${title}"],
+    }
+
+    #file { '/etc/default/prometheus-mysqld-exporter':
+    #    ensure  => present,
+    #    mode    => '0444',
+    #    owner   => 'root',
+    #    group   => 'root',
+    #    content => "ARGS=\"${arguments}\"",
+    #    notify  => Service['prometheus-mysqld-exporter'],
+    #}
+
+    service { "prometheus-mysqld-exporter@${title}":
+        ensure  => running,
+        require => [Package['prometheus-mysqld-exporter'],
+                    File["/var/lib/prometheus/${title}.my.cnf"]],
+    }
+}
