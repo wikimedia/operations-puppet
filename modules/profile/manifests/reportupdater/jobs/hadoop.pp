47c6da2934239574da9678ab12be1823d8ecc13e
Ottomata <aotto@wikimedia.org>
2017-07-12 09:16:55 -0400

    Create reportupdater::jobs profiles for stat boxes
    This makes it a little easier to conditionally include reportupdater in different roles,
    and helps with the new stat box migration.
    
    Bug: T152712
    Change-Id: I51f5b85f7c378f250e477dcca625cc3e9b3ae99b

---
+++ modules/profile/manifests/reportupdater/jobs/hadoop.pp
@@ -0,0 +1,43 @@
+# == Class profile::reportupdater::jobs::hadoop
+# Installs reportupdater jobs that run on Hadoop/Hive.
+# This profile should only be included in a single role.
+#
+# This requires that a Hadoop client is installed and the statistics compute role
+# for the published_datasets_path.
+class profile::reportupdater::jobs::hadoop {
+    # TODO: it would be better to depend on role::analytics_cluster::client, but
+    # that seems wrong from a profile.  Perhaps when analytics_cluster roles
+    # have been refactored to profiles.
+    Class['cdh::hadoop'] -> Class['profile::reportupdater::jobs::hive']
+
+    require ::statistics::compute
+
+    # Set up reportupdater.
+    # Reportupdater here launches Hadoop jobs, and
+    # the 'hdfs' user is the only 'system' user that has
+    # access to required files in Hadoop.
+    class { 'reportupdater':
+        user      => 'hdfs',
+    }
+
+    # And set up a link for periodic jobs to be included in published reports.
+    # Because periodic is in published_datasets_path, files will be synced to
+    # analytics.wikimedia.org/datasets/periodic/reports
+    file { "${::statistics::compute::published_datasets_path}/periodic":
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'wikidev',
+        mode   => '0775',
+    }
+    file { "${::statistics::compute::published_datasets_path}/periodic/reports":
+        ensure  => 'link',
+        target  => "${::statistics::working_path}/reportupdater/output",
+        require => Class['reportupdater'],
+    }
+
+    # Set up a job to create browser reports on hive db.
+    reportupdater::job { 'browser':
+        repository => 'reportupdater-queries',
+        output_dir => 'metrics/browser',
+    }
+}
---
+++ modules/profile/manifests/reportupdater/jobs/mysql.pp
@@ -0,0 +1,74 @@
+# == Class profile::reportupdater::jobs::mysql
+#
+# Installs reportupdater package, and sets up jobs that run reports and generate output from
+# MySQL analytics slaves.  This profile should only be included in a single role.
+#
+# This requires the statistics module for the stats user and the published_datasets_path.
+#
+class profile::reportupdater {
+    require statistics
+    require statistics::compute
+
+    # Set up reportupdater to be executed on this machine
+    class { 'reportupdater':
+        user      => $::statistics::user::username,
+    }
+
+    # And set up a link for periodic jobs to be included in published reports.
+    # Because periodic is in published_datasets_path, files will be synced to
+    # analytics.wikimedia.org/datasets/periodic/reports
+    file { "${::statistics::compute::published_datasets_path}/periodic":
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'wikidev',
+        mode   => '0775',
+    }
+    file { "${::statistics::compute::published_datasets_path}/periodic/reports":
+        ensure  => 'link',
+        target  => '/srv/reportupdater/output',
+        require => Class['reportupdater'],
+    }
+
+    # Set up various jobs to be executed by reportupdater
+    # creating several reports on mysql research db.
+    reportupdater::job { 'flow':
+        repository => 'limn-flow-data',
+        output_dir =>  'flow/datafiles',
+    }
+    reportupdater::job { 'flow-beta-features':
+        repository => 'limn-flow-data',
+        output_dir =>  'metrics/beta-feature-enables',
+    }
+    reportupdater::job { 'edit':
+        repository => 'limn-edit-data',
+        output_dir => 'metrics',
+    }
+    reportupdater::job { 'edit-beta-features':
+        repository => 'limn-edit-data',
+        output_dir => 'metrics/beta-feature-enables',
+    }
+    reportupdater::job { 'language':
+        repository => 'limn-language-data',
+        output_dir => 'metrics/beta-feature-enables',
+    }
+    reportupdater::job { 'ee':
+        repository => 'limn-ee-data',
+        output_dir => 'metrics/echo',
+    }
+    reportupdater::job { 'ee-beta-features':
+        repository => 'limn-ee-data',
+        output_dir => 'metrics/beta-feature-enables',
+    }
+    reportupdater::job { 'multimedia':
+        repository => 'limn-multimedia-data',
+        output_dir => 'metrics/multimedia-health',
+    }
+    reportupdater::job { 'ee-migration':
+        repository => 'limn-ee-data',
+        output_dir => 'metrics/ee',
+    }
+    reportupdater::job { 'interactive':
+        repository => 'discovery-stats',
+        output_dir => 'metrics/interactive',
+    }
+}
