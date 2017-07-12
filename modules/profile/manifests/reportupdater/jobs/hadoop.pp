3b51994a9e9fbeb61cfb0df9192b159b710b7484
Ottomata <aotto@wikimedia.org>
2017-07-12 09:16:55 -0400

    Create reportupdater::jobs profiles for stat boxes
    This makes it a little easier to conditionally include reportupdater in different roles,
    and helps with the new stat box migration.
    
    Bug: T152712
    Change-Id: I51f5b85f7c378f250e477dcca625cc3e9b3ae99b

---
+++ modules/profile/manifests/reportupdater/jobs/hadoop.pp
@@ -0,0 +1,50 @@
+# == Class profile::reportupdater::jobs::hadoop
+# Installs reportupdater jobs that run on Hadoop/Hive.
+# This profile should only be included in a single role.
+#
+# This requires that a Hadoop client is installed and the statistics compute role
+# for the published_datasets_path.
+class profile::reportupdater::jobs::hadoop {
+    # TODO: it would be better to depend on role::analytics_cluster::client, but
+    # that seems wrong from a profile.  Perhaps when analytics_cluster roles
+    # have been refactored to profiles.
+    Class['cdh::hadoop'] -> Class['profile::reportupdater::jobs::hadoop']
+
+    require ::statistics::compute
+
+    # TODO: This can be removed once stat1002 is gone.  T152712
+    $base_path = $::hostname ? {
+        'stat1002' => '/a/reportupdater',
+        default => '/srv/reportupdater',
+    }
+
+    # Set up reportupdater.
+    # Reportupdater here launches Hadoop jobs, and
+    # the 'hdfs' user is the only 'system' user that has
+    # access to required files in Hadoop.
+    class { 'reportupdater':
+        user      => 'hdfs',
+        base_path => $base_path,
+    }
+
+    # And set up a link for periodic jobs to be included in published reports.
+    # Because periodic is in published_datasets_path, files will be synced to
+    # analytics.wikimedia.org/datasets/periodic/reports
+    file { "${::statistics::compute::published_datasets_path}/periodic":
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'wikidev',
+        mode   => '0775',
+    }
+    file { "${::statistics::compute::published_datasets_path}/periodic/reports":
+        ensure  => 'link',
+        target  => "${::statistics::working_path}/reportupdater/output",
+        require => Class['reportupdater'],
+    }
+
+    # Set up a job to create browser reports on hive db.
+    reportupdater::job { 'browser':
+        repository => 'reportupdater-queries',
+        output_dir => 'metrics/browser',
+    }
+}
--- modules/role/manifests/statistics/private.pp
+++ modules/role/manifests/statistics/private.pp
@@ -29,50 +29,16 @@ class role::statistics::private inherits role::statistics::base {
     # private. We just keep it here to spare adding a separate role.
     include ::statistics::aggregator::projectview
 
-    include passwords::mysql::research
     # This file will render at
     # /etc/mysql/conf.d/statistics-private-client.cnf.
     # This is so that users in the statistics-privatedata-users
     # group who want to access the research slave dbs do not
     # have to be in the research group, which is not included
     # in the private role.
-    mysql::config::client { 'statistics-private':
-        user  => $::passwords::mysql::research::user,
-        pass  => $::passwords::mysql::research::pass,
+    statistics::mysql_credentials { 'statistics-private':
         group => 'statistics-privatedata-users',
-        mode  => '0440',
     }
 
-    # Set up reportupdater to be executed on this machine.
-    # Reportupdater launches Hadoop jobs, and
-    # the 'hdfs' user is the only 'system' user that has
-    # access to required files in Hadoop.
-    class { 'reportupdater':
-        base_path => "${::statistics::working_path}/reportupdater",
-        user      => 'hdfs',
-        # We know that this is included, but unfortunetly
-        # it is done so outside of this role.  Perhaps
-        # reportupdater should have its own role!
-        require   => Class['cdh::hadoop'],
-    }
-    # And set up a link for periodic jobs to be included in published reports.
-    # Because periodic is in published_datasets_path, files will be synced to
-    # analytics.wikimedia.org/datasets/periodic/reports
-    file { "${::statistics::compute::published_datasets_path}/periodic":
-        ensure => 'directory',
-        owner  => 'root',
-        group  => 'wikidev',
-        mode   => '0775',
-    }
-    file { "${::statistics::compute::published_datasets_path}/periodic/reports":
-        ensure  => 'link',
-        target  => "${::statistics::working_path}/reportupdater/output",
-        require => Class['reportupdater'],
-    }
-
-    # Set up a job to create browser reports on hive db.
-    reportupdater::job { 'browser':
-        repository => 'reportupdater-queries',
-        output_dir => 'metrics/browser',
-    }
+    # Run Hadoop/Hive reportupdater jobs here.
+    include ::profile::reportupdater::jobs::hadoop
 }
