6340b5a768f0b01fe85b55838e3287333ef75c33
Ema <ema@wikimedia.org>
2017-07-12 08:58:16 +0000

    Revert "Temporarily remove achernar from lvs2* resolv.conf"
    This reverts commit 17a24eeb0f203efbb6555bc8331b5a888e70aeea.
    
    Change-Id: Ic70c7e15df2223bed0bbbdd53fc016562290dae2

--- manifests/site.pp
+++ manifests/site.pp
@@ -1353,7 +1353,7 @@ node /lvs200[1-6]\.codfw\.wmnet/ {
     #   so they need to use the recursive DNS backends directly
     #   (acamar and achernar) with fallback to eqiad
     # (doing this for all lvs for now, see T103921)
-    $nameservers_override = [ '208.80.153.12', '208.80.154.254' ]
+    $nameservers_override = [ '208.80.153.12', '208.80.153.42', '208.80.154.254' ]
     role(lvs::balancer)
     lvs::interface_tweaks {
         'eth0': bnx2x => true, txqlen => 10000;
