de430461b92b791c8b41da476d3bb197835eba67
Ottomata <aotto@wikimedia.org>
2017-07-12 10:35:34 -0400

    Add statistics private, cruncher and web profiles, with a little refactoring
    Bug: T152712
    Change-Id: I24b283b2a76234ff3c7933ca19acbe693eaa45e1

--- modules/role/manifests/statistics/cruncher.pp
+++ modules/role/manifests/statistics/cruncher.pp
@@ -1,23 +1,10 @@
 # (stat1003 / stat1006)
-class role::statistics::cruncher inherits role::statistics::base {
-    system::role { 'statistics::cruncher':
+class role::statistics::cruncher {
+    system::role { 'role::statistics::cruncher':
         description => 'Statistics general compute node (non private data)'
     }
 
-    include ::standard
-    include ::base::firewall
-    include ::profile::backup::host
-    backup::set { 'home' : }
-
-    statistics::mysql_credentials { 'research':
-        group => 'researchers',
-    }
-
-    # include stuff common to statistics compute nodes
-    include ::statistics::compute
-
-    # rsync logs from logging hosts
-    include ::statistics::rsync::eventlogging
+    include ::profile::statistics::cruncher
 
     # Reportupdater jobs that get data from MySQL analytics slaves
     include ::profile::reportupdater::jobs::mysql
