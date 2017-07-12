cc0d0c171097ada8b7e250c18cd229d1f53ce83b
Ottomata <aotto@wikimedia.org>
2017-07-12 09:16:55 -0400

    Create reportupdater::jobs profiles for stat boxes
    This makes it a little easier to conditionally include reportupdater in different roles,
    and helps with the new stat box migration.
    
    Bug: T152712
    Change-Id: I51f5b85f7c378f250e477dcca625cc3e9b3ae99b

--- modules/statistics/manifests/init.pp
+++ modules/statistics/manifests/init.pp
@@ -14,21 +14,6 @@ class statistics(
 ) {
     include ::statistics::user
 
-    file { $working_path:
-        ensure => 'directory',
-        owner  => 'root',
-        group  => 'wikidev',
-        mode   => '0775',
-    }
-
-    if $working_path == '/srv' {
-        # symlink /a to /srv for backwards compatibility
-        file { '/a':
-            ensure => 'link',
-            target => '/srv',
-        }
-    }
-
     # set up rsync modules for copying files
     # on statistic servers in $working_path
     class { '::statistics::rsyncd':
