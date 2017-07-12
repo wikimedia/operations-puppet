8dc833eb70f20318823d33258eb1ff0c80a979a3
Ottomata <aotto@wikimedia.org>
2017-07-12 10:35:34 -0400

    Add statistics private, cruncher and web profiles, with a little refactoring
    Bug: T152712
    Change-Id: I24b283b2a76234ff3c7933ca19acbe693eaa45e1

--- modules/role/manifests/statistics/private.pp
+++ modules/role/manifests/statistics/private.pp
@@ -1,43 +1,10 @@
-# (stat1002)
-class role::statistics::private inherits role::statistics::base {
+# (stat1002 / stat1005)
+class role::statistics::private {
     system::role { 'statistics::private':
         description => 'Statistics private data host and general compute node'
     }
 
-    include ::profile::backup::host
-    backup::set { 'home' : }
-
-    # include stuff common to statistics compute nodes
-    include ::statistics::compute
-
-    # rsync mediawiki logs from logging hosts
-    include ::statistics::rsync::mediawiki
-
-    # WMDE statistics scripts and cron jobs
-    include ::statistics::wmde
-
-    # Discovery statistics generating scripts
-    include ::statistics::discovery
-
-    # eventlogging logs are not private, but they
-    # are here for convenience
-    include ::statistics::rsync::eventlogging
-    # backup eventlogging logs
-    backup::set { 'a-eventlogging' : }
-
-    # Although it is in the "private" role, the dataset actually isn't
-    # private. We just keep it here to spare adding a separate role.
-    include ::statistics::aggregator::projectview
-
-    # This file will render at
-    # /etc/mysql/conf.d/statistics-private-client.cnf.
-    # This is so that users in the statistics-privatedata-users
-    # group who want to access the research slave dbs do not
-    # have to be in the research group, which is not included
-    # in the private role.
-    statistics::mysql_credentials { 'statistics-private':
-        group => 'statistics-privatedata-users',
-    }
+    include ::profile::statistics::private
 
     # Run Hadoop/Hive reportupdater jobs here.
     include ::profile::reportupdater::jobs::hadoop
