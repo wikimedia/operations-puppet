7bdbb8d8c69a0810198f9a00dd8a3d9e48fa6709
Ottomata <aotto@wikimedia.org>
2017-07-12 09:16:55 -0400

    Create reportupdater::jobs profiles for stat boxes
    This makes it a little easier to conditionally include reportupdater in different roles,
    and helps with the new stat box migration.
    
    Bug: T152712
    Change-Id: I51f5b85f7c378f250e477dcca625cc3e9b3ae99b

--- modules/role/manifests/statistics/cruncher.pp
+++ modules/role/manifests/statistics/cruncher.pp
@@ -1,4 +1,4 @@
-# (stat1003)
+# (stat1003 / stat1006)
 class role::statistics::cruncher inherits role::statistics::base {
     system::role { 'statistics::cruncher':
         description => 'Statistics general compute node (non private data)'
@@ -19,6 +19,7 @@ class role::statistics::cruncher inherits role::statistics::base {
     # rsync logs from logging hosts
     include ::statistics::rsync::eventlogging
 
+    include ::profile::reportupdater::jobs::mysql
 
     # geowiki: bringing data from production slave db to research db
     include geowiki::job::data
@@ -27,68 +28,4 @@ class role::statistics::cruncher inherits role::statistics::base {
     # geowiki: monitors the geowiki files of http://gp.wmflabs.org/
     include geowiki::job::monitoring
 
-
-    # Set up reportupdater to be executed on this machine
-    class { 'reportupdater':
-        base_path => "${::statistics::working_path}/reportupdater",
-        user      => $::statistics::user::username,
-    }
-
-    # And set up a link for periodic jobs to be included in published reports.
-    # Because periodic is in published_datasets_path, files will be synced to
-    # analytics.wikimedia.org/datasets/periodic/reports
-    file { "${::statistics::compute::published_datasets_path}/periodic":
-        ensure => 'directory',
-        owner  => 'root',
-        group  => 'wikidev',
-        mode   => '0775',
-    }
-    file { "${::statistics::compute::published_datasets_path}/periodic/reports":
-        ensure  => 'link',
-        target  => "${::statistics::working_path}/reportupdater/output",
-        require => Class['reportupdater'],
-    }
-
-    # Set up various jobs to be executed by reportupdater
-    # creating several reports on mysql research db.
-    reportupdater::job { 'flow':
-        repository => 'limn-flow-data',
-        output_dir =>  'flow/datafiles',
-    }
-    reportupdater::job { 'flow-beta-features':
-        repository => 'limn-flow-data',
-        output_dir =>  'metrics/beta-feature-enables',
-    }
-    reportupdater::job { 'edit':
-        repository => 'limn-edit-data',
-        output_dir => 'metrics',
-    }
-    reportupdater::job { 'edit-beta-features':
-        repository => 'limn-edit-data',
-        output_dir => 'metrics/beta-feature-enables',
-    }
-    reportupdater::job { 'language':
-        repository => 'limn-language-data',
-        output_dir => 'metrics/beta-feature-enables',
-    }
-    reportupdater::job { 'ee':
-        repository => 'limn-ee-data',
-        output_dir => 'metrics/echo',
-    }
-    reportupdater::job { 'ee-beta-features':
-        repository => 'limn-ee-data',
-        output_dir => 'metrics/beta-feature-enables',
-    }
-    reportupdater::job { 'multimedia':
-        repository => 'limn-multimedia-data',
-        output_dir => 'metrics/multimedia-health',
-    }
-    reportupdater::job { 'ee-migration':
-        repository => 'limn-ee-data',
-        output_dir => 'metrics/ee',
-    }
-    reportupdater::job { 'interactive':
-        repository => 'discovery-stats',
-        output_dir => 'metrics/interactive',
-    }
 }
