0f33d9daee6e2a64ff0f97a46843580fce753dac
Emanuele Rocca <ema@wikimedia.org>
2017-07-13 16:55:33 +0200

    Linux kernel module handling
    Add a new puppet module called 'kmod' allowing to:
    
    - insert/remove a Linux kernel module
    - blacklist the given modules
    - set module options
    
    Change-Id: Ib2bd63df23dcc6e33ad51b444c45cc731ea25f0c

---
+++ modules/kmod/manifests/module.pp
@@ -0,0 +1,40 @@
+# == Define: kmod::module
+#
+# Make sure that the given kernel module is loaded (or not).
+#
+# === Parameters
+#
+# [*ensure*]
+#   If 'present', the module will be loaded. If 'absent', unloaded.
+#   The default is 'present'.
+#
+define kmod::module($ensure=present) {
+    validate_ensure($ensure)
+
+    if $ensure == 'present' {
+        $modprobe_cmd = "/sbin/modprobe ${name}"
+    } else {
+        $modprobe_cmd = "/sbin/modprobe -r ${name}"
+    }
+
+    file { "/etc/modules-load.d/${name}.conf":
+        ensure  => $ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => "${name}\n",
+        notify  => Exec[$modprobe_cmd],
+    }
+
+    if $ensure == 'present' {
+        exec { $modprobe_cmd:
+            unless      => "/bin/lsmod | /bin/grep -q '^${name} '",
+            refreshonly => true,
+        }
+    } else {
+        exec { $modprobe_cmd:
+            onlyif      => "/bin/lsmod | /bin/grep -q '^${name} '",
+            refreshonly => true,
+        }
+    }
+}
