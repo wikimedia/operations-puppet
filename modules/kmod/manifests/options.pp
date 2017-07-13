c5839a8c66b46e03c48365b3509ff431eee0d711
Emanuele Rocca <ema@wikimedia.org>
2017-07-13 16:55:33 +0200

    Linux kernel module handling
    Add a new puppet module called 'kmod' allowing to:
    
    - insert/remove a Linux kernel module
    - blacklist the given modules
    - set module options
    
    Change-Id: Ib2bd63df23dcc6e33ad51b444c45cc731ea25f0c

---
+++ modules/kmod/manifests/options.pp
@@ -0,0 +1,24 @@
+# == Define: kmod::options
+#
+# Add options to the given module every time it is inserted into the kernel.
+#
+# === Parameters
+#
+# [*options*]
+#   The options to add.
+#
+# === Example
+#
+# kmod::options { "nf_conntrack":
+#     options => 'hashsize=32768',
+# }
+#
+define kmod::options($options) {
+    file { "/etc/modprobe.d/options-${name}.conf":
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('kmod/options.conf.erb'),
+    }
+}
