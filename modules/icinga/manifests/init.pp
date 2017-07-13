77abced40cf872305f707f4b3d64c4b6485f1084
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-07-13 14:57:26 +0300

    icinga: Bump icinga-tmpfs size to 1024m
    We had this way too low, meaning depending on circumstances it could
    cause truncation of some files like status.dat. While the disk usage in
    this filesystems varies greatly, it's possible it gets full from time to
    time
    
    Bug: T164206
    Change-Id: Ib9a1f17ca25d24f2ebfe8892e814bda0da85095e

--- modules/icinga/manifests/init.pp
+++ modules/icinga/manifests/init.pp
@@ -119,7 +119,7 @@ class icinga(
         atboot  => true,
         fstype  => 'tmpfs',
         device  => 'none',
-        options => 'size=128m,uid=icinga,gid=icinga,mode=755',
+        options => 'size=1024m,uid=icinga,gid=icinga,mode=755',
         require => File['/var/icinga-tmpfs'],
     }
     # Fix the ownerships of some files. This is ugly but will do for now
