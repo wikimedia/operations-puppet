7d9553c2472c126d5c067716a8e6ab5225880031
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-07-17 18:12:08 +0200

    systemd: add defines to manage systemd units
    A simpler, less integrated approach to manage systemd-related files than
    the one proposed by base::system_unit, which is mostly a (complicated)
    attempt at supporting multiple init systems.
    
    For systemd-only catalogs, a simpler, leaner series of defines should
    make things clearer and reduce the level of indirection we have.
    
    Specifically:
    
    * A generic systemd::unit resource for defining arbitrary systemd
      units. It supports both full units and overrides to system-provided
      ones. We do not handle the Service definition there, just restarts
    
    * A more integrated systemd::service definition allowing not only to
      manage the unit file but the corresponding `service` (in puppet
      terms), that is, the unit state.
    
    Change-Id: Ia4199b149804641bcc7d1957e7a0065fb7d5fa8b

---
+++ modules/systemd/manifests/service.pp
@@ -0,0 +1,56 @@
+# == systemd::service ===
+#
+# Manages a systemd-based unit as a puppet service, properly handling:
+# - the unit file
+# - the puppet service definition and state
+#
+# === Parameters ===
+# [*unit_type*]
+#   The unit type we are defining as a service
+# [*content*]
+#   The content of the file. Required.
+# [*ensure*]
+#   The usual meta-parameter, defaults to present. Valid values are
+#   'absent' and 'present'
+# [*restart*]
+#   Whether to handle restarting the service when the file changes.
+# [*override*]
+#   If the are creating an override to system-provided units or not.
+#   Defaults to false
+# [*service_params*]
+#   Additional service parameters we want to specify
+#
+define systemd::service(
+    $content,
+    $unit_type = 'service',
+    $ensure  = 'present',
+    $restart = false,
+    $override = false,
+    $service_params = {},
+){
+    require ::systemd
+    validate_ensure($ensure)
+    unless ($unit_type in $::systemd::unit_types) {
+        fail("Unsupported systemd unit type ${unit_type}")
+    }
+
+    $label = "${title}.${unit_type}"
+    $enable = $ensure ? {
+        'present' => true,
+        default   => false,
+    }
+    $base_params = {
+        ensure   => ensure_service($ensure),
+        provider => 'systemd',
+        enable   => $enable,
+    }
+    $params = merge($base_params, $service_params)
+    ensure_resource('service', $label, $params)
+
+    systemd::unit { $label:
+        ensure   => $ensure,
+        content  => $content,
+        override => $override,
+        restart  => $restart
+    }
+}
