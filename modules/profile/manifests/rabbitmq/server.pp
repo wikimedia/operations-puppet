9d31d5cd476b34514b3270e57c0f1e96d6b039eb
cpettet <rush@wikimedia.org>
2017-07-17 17:33:31 -0500

    labtest: rabbitmq for openstack control node
    refactor as module/profile/role model
    
    We are untangling the 'openstack' module, this
    supercedes the rabbitmq logic mixed in there and
    that logic will be removed once this is ready.
    
    Bug: T167559
    Change-Id: If2a5b78856104d9b75d4d3e2c83923628fb2d3be

---
+++ modules/profile/manifests/rabbitmq/server.pp
@@ -0,0 +1,15 @@
+class profile::rabbitmq::server(
+    $monitor_user = hiera('profile::rabbitmq::monitor::user'),
+    $monitor_password = hiera('profile::rabbitmq::monitor::password'),
+    $file_handles = hiera('profile::rabbitmq::file_handles'),
+){
+
+    class { 'rabbitmq':
+        file_handles => $file_handles,
+    }
+
+    class { 'rabbitmq::monitor':
+        rabbit_monitor_username => $monitor_user,
+        rabbit_monitor_password     => $monitor_password,
+    }
+}
---
+++ modules/rabbitmq/manifests/monitor.pp
@@ -0,0 +1,15 @@
+class rabbitmq::monitor(
+    $rabbit_monitor_username,
+    $rabbit_monitor_password,
+    $rabbit_host='localhost:15672',
+    ) {
+
+    diamond::collector { 'RabbitMQ':
+        settings => {
+            'host'     => $rabbit_host,
+            'user'     => $rabbit_monitor_username,
+            'password' => $rabbit_monitor_password,
+        },
+        source   => 'puppet:///modules/openstack/rabbitmq/rabbitmq.py',
+    }
+}
