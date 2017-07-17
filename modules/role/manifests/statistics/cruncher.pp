58cee6c9cea56a21a98efb279f3091991acd4cc0
Ottomata <aotto@wikimedia.org>
2017-07-17 15:15:28 -0400

    Move reportupdater jobs from stat1003 -> stat1006
    Bug: T152712
    Change-Id: Ic58c4c42890c51b056adf27e4489393fb3e426c7

--- modules/role/manifests/statistics/cruncher.pp
+++ modules/role/manifests/statistics/cruncher.pp
@@ -7,15 +7,12 @@ class role::statistics::cruncher {
     include ::profile::statistics::cruncher
 
     # will be removed as part of T152712
-    if $::hostname == 'stat1003' {
-        # Reportupdater jobs that get data from MySQL analytics slaves
-        include ::profile::reportupdater::jobs::mysql
-
-    }
-    # else moved to stat1006
-    else {
+    if $::hostname == 'stat1006' {
         # Geowiki jobs that get data from MySQL analytics slave and save the data
         # in a private locally hosted git repository.
         include ::profile::geowiki
+
+        # Reportupdater jobs that get data from MySQL analytics slaves
+        include ::profile::reportupdater::jobs::mysql
     }
 }
