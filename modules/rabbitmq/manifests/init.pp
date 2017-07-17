6044ed37ce771e490a300f902190c1b150a81d18
cpettet <rush@wikimedia.org>
2017-07-17 17:33:31 -0500

    labtest: rabbitmq for openstack control node
    refactor as module/profile/role model
    
    We are untangling the 'openstack' module, this
    supercedes the rabbitmq logic mixed in there and
    that logic will be removed once this is ready.
    
    Bug: T167559
    Change-Id: If2a5b78856104d9b75d4d3e2c83923628fb2d3be

---
+++ modules/rabbitmq/manifests/init.pp
@@ -0,0 +1,49 @@
+# https://www.rabbitmq.com/
+#
+# User MAC's are not handled by Puppet
+#
+# Changing a user password
+#  rabbitmqctl change_password <user> <password>
+# Adding a user
+#  rabbitmqctl add_user <user> <password>
+#
+# Creating user "<user>" ...
+#  rabbitmqctl change_password <user> <password>
+#  rabbitmqctl set_user_tags <user> administrator
+# Setting tags for user "<user>" to [administrator] ...
+#  rabbitmqctl set_permissions -p / <user> ".*" ".*" ".*"
+#
+# The management plugin may be desired
+#  rabbitmq-plugins enable rabbitmq_management
+
+class rabbitmq(
+    $file_handles='1024',
+    ) {
+
+    package { [ 'rabbitmq-server' ]:
+        ensure  => present,
+    }
+
+    file { '/etc/default/rabbitmq-server':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('rabbitmq/rabbitmq-server.default.erb'),
+        require => Package['rabbitmq-server'],
+        notify  => Service['rabbitmq-server'],
+    }
+
+    file { '/usr/local/sbin/rabbitmqadmin':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0655',
+        source => 'puppet:///modules/rabbitmq/rabbitmqadmin',
+    }
+
+    service { 'rabbitmq-server':
+        ensure  => running,
+        require => Package['rabbitmq-server'],
+    }
+}
