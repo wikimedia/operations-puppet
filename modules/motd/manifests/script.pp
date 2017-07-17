b65bc099574a53745e5537bb14e7aad532226532
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-07-17 12:19:09 +0200

    motd::script: use validate_numeric for priority
    This also should avoid failures with the future parser, and is also
    marginally more correct than what we had now.
    
    Change-Id: Ib36acb64337f673f20831b5e332bad578d7298d3

--- modules/motd/manifests/script.pp
+++ modules/motd/manifests/script.pp
@@ -7,7 +7,7 @@
 # [*priority*]
 #   If you need this script to load before or after other scripts, you
 #   can make it do so by manipulating this value. In most cases, the
-#   default value of 50 should be fine.
+#   default value of 50 should be fine. Value should be between 0 and 99
 #
 # [*content*]
 #   If defined, will be used as the content of the motd script.
@@ -33,8 +33,9 @@ define motd::script(
 ) {
     include ::motd
 
+    # TODO/puppet4 - make all of these checks parameter type definitions instead
     validate_ensure($ensure)
-    validate_re($priority, '^\d?\d$', '"priority" must be between 0 - 99')
+    validate_numeric($priority, 99, 0)
     if $source == undef and $content == undef  { fail('you must provide either "source" or "content"') }
     if $source != undef and $content != undef  { fail('"source" and "content" are mutually exclusive') }
 
