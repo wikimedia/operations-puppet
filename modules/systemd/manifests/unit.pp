5b254b218f6789f7af21a4fe68f09477e58c8445
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-07-17 18:12:08 +0200

    systemd: add defines to manage systemd units
    A simpler, less integrated approach to manage systemd-related files than
    the one proposed by base::system_unit, which is mostly a (complicated)
    attempt at supporting multiple init systems.
    
    For systemd-only catalogs, a simpler, leaner series of defines should
    make things clearer and reduce the level of indirection we have.
    
    Specifically:
    
    * A generic systemd::unit resource for defining arbitrary systemd
      units. It supports both full units and overrides to system-provided
      ones. We do not handle the Service definition there, just restarts
    
    * A more integrated systemd::service definition allowing not only to
      manage the unit file but the corresponding `service` (in puppet
      terms), that is, the unit state.
    
    Change-Id: Ia4199b149804641bcc7d1957e7a0065fb7d5fa8b

---
+++ modules/systemd/manifests/unit.pp
@@ -0,0 +1,99 @@
+# == systemd::unit ==
+#
+# This define creates a file on the filesystem at $path,
+# schedules a daemon-reload of systemd and, if requested,
+# schedules a subsequent refresh of the service.
+#
+# === Parameters ===
+# The resource title is assumed to be the corresponding full unit
+# name. If no valid unit suffix is present, 'service' will be assumed.
+#
+# [*content*]
+#   The content of the file. Required.
+# [*ensure*]
+#   The usual meta-parameter, defaults to present. Valid values are
+#   'absent' and 'present'
+# [*restart*]
+#   Whether to handle restarting the service when the file changes.
+# [*override*]
+#   If the are creating an override to system-provided units or not.
+#   Defaults to false
+#
+# === Examples ===
+#
+# A systemd override for the hhvm.service unit
+#
+# systemd::unit { 'hhvm':
+#     ensure   => present,
+#     content  => template('hhvm/initscripts/hhvm.systemd.erb'),
+#     restart  => false,
+#     override => true,
+# }
+#
+# # A socket for nginx
+# systemd::unit { 'nginx.socket':
+#     ensure   => present,
+#     content  => template('nginx/nginx.socket.erb'),
+#     restart  => true, # This will work only if you have service{ `nginx.socket`: }
+# }
+#
+define systemd::unit(
+    $content,
+    $ensure=present,
+    $restart=false,
+    $override=false,
+){
+    require ::systemd
+
+    if ($title =~ /^(.+)\.(\w+)$/ and $2 in $::systemd::unit_types){
+        $unit_name = $title
+    } else {
+        $unit_name = "${title}.service"
+    }
+
+    if ($override) {
+        # Define the override dir if not defined.
+        $override_dir = "${::systemd::override_dir}/${unit_name}.d"
+        file { $override_dir:
+            ensure => ensure_directory($ensure),
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+        }
+        $path = "${override_dir}/puppet-override.conf"
+    } else {
+        $path = "${::systemd::base_dir}/${unit_name}"
+    }
+
+    $exec_label = "systemd daemon-reload for ${unit_name}"
+    file { $path:
+        ensure  => $ensure,
+        content => $content,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        notify  => Exec[$exec_label],
+    }
+
+    exec { $exec_label:
+        refreshonly => true,
+        command     => '/bin/systemctl daemon-reload',
+    }
+
+    # If the service is defined, add a dependency.
+
+    if defined(Service[$title]) {
+        if $ensure == 'present' {
+            # systemd must reload units before the service is managed
+            if $restart {
+                # Refresh the service if restarts are required
+                Exec[$exec_label] ~> Service[$title]
+            } else {
+                Exec[$exec_label] -> Service[$title]
+            }
+        } else {
+            # the service should be managed before the daemon-reload
+            Service[$title] -> Exec[$exec_label]
+        }
+    }
+}
