80ba5ca4d636b946d613841b0b1f470094104899
elukey <ltoscano@wikimedia.org>
2017-07-17 16:41:15 +0200

    profile::piwik::database: puppetize Piwik's database config
    For historic reasons Piwik has been created using a standard
    Debian configuration, not puppetized and not following
    Wikimedia's standards. While this is not the best solution it
    allow us to keep bohrium consistent.
    
    Bug: T164073
    Change-Id: I0a28aba3eda5df8e12bb5a0b006ce367519f8e29

---
+++ modules/profile/manifests/piwik/database.pp
@@ -0,0 +1,20 @@
+# == Class: profile::piwik::database
+#
+# Set up a simple mysql database for Piwik. This config is not standard
+# (as in following Wikimedia's puppet classes) because of historic reasons,
+# but it will refactored in the future. For the moment it contains the very
+# basic configs added to the standard Debian mysql deployment.
+#
+class profile::piwik::backup {
+
+    require_package('mysql-server')
+
+    file { '/etc/mysql/my.cnf':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        source  => 'puppet:///modules/profile/piwik/my.cnf',
+    }
+
+}
