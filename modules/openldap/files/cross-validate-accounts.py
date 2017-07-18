b44167af8680f9dd67333c67bee17539e93cd920
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-07-18 09:15:22 +0200

    Reduce account expiration warning to seven days
    With 14 days we've often pinged people while the renewal was already
    in progress, reduce this to seven days from now on.
    
    Change-Id: Ib977fe566104372809b54c397594885eab53ed5c

--- modules/openldap/files/cross-validate-accounts.py
+++ modules/openldap/files/cross-validate-accounts.py
@@ -189,7 +189,7 @@ def validate_all_ldap_group_members_are_defined(known_users):
     return log
 
 
-# Warn if an account expires in 14 days or less
+# Warn if an account expires in 7 days or less
 def print_pending_account_expirys(users):
     log = ""
     current_date = datetime.datetime.now().date()
@@ -198,7 +198,7 @@ def print_pending_account_expirys(users):
         if 'expiry_date' in attrs.keys():
             expiry = datetime.datetime.strptime(str(attrs['expiry_date']), "%Y-%m-%d").date()
             delta = expiry - current_date
-            if delta.days <= 14:
+            if delta.days <= 7:
                 log += "The NDA/MOU for " + i + " will lapse in " + str(delta.days) + " days.\n"
                 log += "  Please get in touch with " + str(attrs['expiry_contact']) + "\n"
     return log
