a496beca8498ccfc467caf68e3fd0800ae15744f
Marko Obrovac <mobrovac@wikimedia.org>
2017-07-19 18:48:33 -0300

    Cassandra: Switch logback-encoder to Scap3
    Bug: T116340
    Depends-On: I0726e183e494471dee6abc6d1eeb110113bad3f1
    Change-Id: Iccc142cf6a97291e90db1bfb063543883b8bf431

--- modules/cassandra/manifests/logging.pp
+++ modules/cassandra/manifests/logging.pp
@@ -10,33 +10,33 @@ class cassandra::logging(
 ) {
     require ::cassandra
 
-    package { 'cassandra/logstash-logback-encoder':
-        ensure   => present,
-        provider => 'trebuchet',
+    scap::target { 'cassandra/logstash-logback-encoder':
+        deploy_user => 'deploy-service',
+        manage_user => true,
     }
 
     file { '/usr/share/cassandra/lib/logstash-logback-encoder.jar':
         ensure  => 'link',
         target  => '/srv/deployment/cassandra/logstash-logback-encoder/lib/logstash-logback-encoder-4.2.jar',
-        require => Package['cassandra/logstash-logback-encoder'],
+        require => Scap::Target['cassandra/logstash-logback-encoder'],
     }
 
     file { '/usr/share/cassandra/lib/jackson-annotations-2.4.0.jar':
         ensure  => 'link',
         target  => '/srv/deployment/cassandra/logstash-logback-encoder/lib/jackson-annotations-2.4.0.jar',
-        require => Package['cassandra/logstash-logback-encoder'],
+        require => Scap::Target['cassandra/logstash-logback-encoder'],
     }
 
     file { '/usr/share/cassandra/lib/jackson-core-2.4.0.jar':
         ensure  => 'link',
         target  => '/srv/deployment/cassandra/logstash-logback-encoder/lib/jackson-core-2.4.0.jar',
-        require => Package['cassandra/logstash-logback-encoder'],
+        require => Scap::Target['cassandra/logstash-logback-encoder'],
     }
 
     file { '/usr/share/cassandra/lib/jackson-databind-2.4.0.jar':
         ensure  => 'link',
         target  => '/srv/deployment/cassandra/logstash-logback-encoder/lib/jackson-databind-2.4.0.jar',
-        require => Package['cassandra/logstash-logback-encoder'],
+        require => Scap::Target['cassandra/logstash-logback-encoder'],
     }
 
     # removes unnecessary dependencies, deprecates T128787; the following code
