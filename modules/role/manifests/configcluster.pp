324b80d9ddf01cce7e0a633b0073f4dbeed3957b
elukey <ltoscano@wikimedia.org>
2017-07-19 11:09:45 +0200

    Add a new profile for zookeeper firewall rules
    This attempt is an alternative for gerrit/356548 meant
    to create a separate and configurable zookeeper firewall
    profile. This should allow us to be very specific on
    what hosts are allowed to contact each zookeeper cluster,
    rather than relying on a generic catch-all rule.
    
    Bug: T114815
    Change-Id: I7a81b88e5b178f25d580aa7d3d19afcbbb0c1064

--- modules/role/manifests/configcluster.pp
+++ modules/role/manifests/configcluster.pp
@@ -2,6 +2,7 @@ class role::configcluster {
     include ::standard
     include ::base::firewall
     include ::profile::zookeeper::server
+    include ::profile::zookeeper::firewall::main
     include ::profile::etcd
     include ::profile::etcd::tlsproxy
     include ::profile::etcd::replication
