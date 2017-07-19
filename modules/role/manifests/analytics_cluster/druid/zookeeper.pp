883efd9ca8df5cb1691c29a6c6bb9015bf239f30
elukey <ltoscano@wikimedia.org>
2017-07-19 11:09:45 +0200

    Add profile::zookeeper::firewall
    This refactor should be a no-op to allow a better
    configuration of the zookeeper firewall rules in the
    future.
    
    Bug: T114815
    Change-Id: I7a81b88e5b178f25d580aa7d3d19afcbbb0c1064

--- modules/role/manifests/analytics_cluster/druid/zookeeper.pp
+++ modules/role/manifests/analytics_cluster/druid/zookeeper.pp
@@ -1,3 +1,4 @@
 class role::analytics_cluster::druid::zookeeper {
     include profile::zookeeper::server
+    include profile::zookeeper::firewall
 }
--- modules/role/manifests/configcluster.pp
+++ modules/role/manifests/configcluster.pp
@@ -2,6 +2,7 @@ class role::configcluster {
     include ::standard
     include ::base::firewall
     include ::profile::zookeeper::server
+    include ::profile::zookeeper::firewall
     include ::profile::etcd
     include ::profile::etcd::tlsproxy
     include ::profile::etcd::replication
