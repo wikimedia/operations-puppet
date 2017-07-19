fe740f24bc2dc2d9918bd0c6ef175ddd064ca819
Ariel T. Glenn <ariel@wikimedia.org>
2017-07-19 21:51:28 +0300

    setup for dumpsdata hosts to serve dumps work area via nfs to snapshots
    [WIP] so draft, much fixmes
    This is here as a placeholder while pieces of it get filled
    in further
    
    Architecture notes are on the phab task.
    
    Bug: T169849
    Change-Id: I5f52a1d3725e5f794573ea293c8978ceeb9027a8

---
+++ modules/dumpsdata/manifests/rsync.pp
@@ -0,0 +1,22 @@
+class dumpsdata::cron::rsync() {
+    include ::dataset::common
+
+    file { '/usr/local/bin/rsync_completed_dumpjobs.py':
+        ensure => 'present',
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+        path   => '/usr/local/bin/rsync_completed_dumpjobs.py',
+        source => 'puppet:///modules/dumpsdata/rsync_completed_dumpjobs.py',
+    }
+
+    cron { 'rsync-dumps':
+        ensure  => present,
+        # filter out error messages about vanishing files, we don't want email for that
+        command => '/usr/bin/python /usr/local/bin/rsync-completed_dumpjobs.py 2>&1 | grep -v "vanished" ',
+        user    => 'root',
+        minute  => '10',
+        hour    => '*',
+        require => File['/usr/local/bin/rsync-completed_dumpjobs.py'],
+    }
+}
