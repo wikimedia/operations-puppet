e61603a15186cc5cc4db2c64e838687543b36c3b
Antoine Musso <hashar@free.fr>
2017-07-24 16:50:54 +0200

    contint: webperformance Jenkins slave
    webperformance has jobs running WebPageTest and they are introducing
    more. Each run for more than 30 minutes which consumes a slot on
    Nodepool.
    
    Add a role that installs nodejs/npm.
    
    Bug: T166756
    Change-Id: I63a4d031321ef7c2118144d8d44c7590aadc81a7

---
+++ modules/role/manifests/ci/slave/webperformance.pp
@@ -0,0 +1,15 @@
+# == Class role:ci::slave::webperformance
+#
+# Configure an instance to run WebPageTest performance tests. The runner uses
+# NodeJS and the jobs typically have a long run time (30+ minutes).
+#
+# filtertags: labs-project-git labs-project-integration labs-project-ci-staging
+class role::ci::slave::webperformance {
+
+    system::role { 'ci::slave::webperformance':
+        description => 'CI Jenkins slave for WebPageTest jobs',
+    }
+
+    include ::role::ci::slave::labs::common
+    include ::contint::packages::javascript
+}
