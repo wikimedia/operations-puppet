2f8b9de3e5f077c710a98b0325ca1418649e9e41
Faidon Liambotis <faidon@wikimedia.org>
2017-07-26 10:35:16 +0000

    Revert "Kill module puppet_statsd"
    This reverts commit 995500b495e19866f346a66d28cfd8f91d9e5cc4.
    
    Change-Id: I3d1d3cc27d0d7d42d097b8b02715402f8160e3c3

---
+++ modules/puppet_statsd/lib/puppet/reports/statsd.rb
@@ -0,0 +1,30 @@
+# Puppet StatsD reporter
+# Sends timing measurements for Puppet runs to StatsD
+require 'puppet'
+require 'yaml'
+require 'erb'
+
+Puppet::Reports.register_report(:statsd) do
+    desc = 'Send Puppet metrics to StatsD'
+
+    def load_config
+        config_file = File.join Puppet.settings[:confdir], 'statsd.yaml'
+        raise Puppet::ParseError, "#{config_file} is missing" unless File.exists? config_file
+        YAML.load_file(config_file)
+    end
+
+    def process
+        config = load_config
+        hostname = self.host
+        Puppet.notice "Sending metrics for #{self.host} to "\
+            "#{config[:statsd_host]}:#{config[:statsd_port]}.."
+
+        socket = UDPSocket.new
+        self.metrics['time'].values.each do |metric,description,value|
+            value = (value * 1000).round  # Convert fractional seconds to whole miliseconds
+            name = ERB.new(config[:metric_format]).result(binding)
+            socket.send("#{name}:#{value}|ms", 0, config[:statsd_host], config[:statsd_port])
+        end
+        socket.close
+    end
+end
