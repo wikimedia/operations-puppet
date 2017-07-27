71d948560a9c97efafbb4e605f2cb80a42aa8b38
andrewbogott <abogott@wikimedia.org>
2017-07-27 17:35:14 -0500

    labs puppetmaster: allow new puppetmasters to talk to the enc
    I don't like having the new masters talk to the enc on the old
    master, but the enc address is hardcoded in /etc/puppet/hiera.yaml
    and making that a template turns out to be ugly and dangerous.
    
    Change-Id: I0128765a9c0c4cb693bd887f2bb2cbe5fbf29cee

--- modules/role/manifests/labs/puppetmaster.pp
+++ modules/role/manifests/labs/puppetmaster.pp
@@ -43,6 +43,12 @@ class role::labs::puppetmaster(
     $labs_vms = $novaconfig['fixed_range']
     $monitoring = '208.80.154.14 208.80.155.119 208.80.153.74'
 
+    # temporarily open up the enc to the old puppetmasters.
+    # I hate this, but the enc url is hardcoded in hiera.yaml and difficult
+    # to modify per puppetmaster.
+    #  FIXME:  Remove this after we've standardized on labs-puppetmaster.wikimedia.org
+    $new_puppetmasters = '208.80.154.158 208.80.155.120'
+
     $fwrules = {
         puppetmaster => {
             rule => "saddr (${labs_vms} ${labs_metal} ${monitoring} ${horizon_host_ip}) proto tcp dport 8140 ACCEPT;",
@@ -51,7 +57,7 @@ class role::labs::puppetmaster(
             rule => "saddr (${horizon_host_ip} ${designate_host_ip}) proto tcp dport 8101 ACCEPT;",
         },
         puppetbackendgetter => {
-            rule => "saddr (${labs_vms} ${labs_metal} ${monitoring} ${horizon_host_ip}) proto tcp dport 8100 ACCEPT;",
+            rule => "saddr (${labs_vms} ${labs_metal} ${monitoring} ${horizon_host_ip} ${new_puppetmasters}) proto tcp dport 8100 ACCEPT;",
         },
     }
     create_resources (ferm::rule, $fwrules)
