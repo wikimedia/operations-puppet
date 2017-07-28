985a7b72902b1b6fb85b57e8fe7ca8aaefeb4f95
Jaime Crespo <jcrespo@wikimedia.org>
2017-07-28 16:32:33 +0200

    sanitarium3: Convert db1102 into a proper multi-instance host
    Like dbstore2002, test the (mostly WIP) new classes on db1102,
    the sanitarium3, now renamed as sanitarium_multiinstance. Despite
    there are some things about multi-instance puppetization that we
    will have to tweak, upgrading it and having the instances
    puppetized will be better than the current state.
    
    Aside from the rename, the 3 existing instances have been
    configured (s2, s6 and s7) and in general used the same model than
    dbstore_multiinstance. Taking the time to move the socket to the
    right location, too.
    
    Bug: T169514
    Bug: T148507
    Change-Id: I558345d0cefb13b20e806eea88f984a32339f2ba

---
+++ modules/role/manifests/mariadb/sanitarium_multiinstance.pp
@@ -0,0 +1,83 @@
+# sanitarium_multiinstance: it replicates from all core shards (except x1),
+# and sanitizes most data on production on 7 shards, before the data
+# arrives to labs
+# This role installs a 10.1 version which is needed for rbr triggers for
+# the new sanitarium server, which runs multi-instance and mariadb 10.1
+# Eventually, this role will deprecate the original sanitarium and
+# sanitarium2/sanitarium_multisource
+
+class role::mariadb::sanitarium_multiinstance {
+
+    system::role { 'mariadb::sanitarium':
+        description => 'Sanitarium DB Server',
+    }
+
+    include ::standard
+    include ::base::firewall
+    #FIXME:
+    ferm::service { 'sanitarium_multiinstance':
+        proto  => 'tcp',
+        port   => '3311:3320',
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    #TODO: define one group per shard
+    class { 'role::mariadb::groups':
+        mysql_group => 'labs',
+        mysql_role  => 'slave',
+        socket      => '/run/mysqld/mysqld.s2.sock',
+    }
+
+    include role::labs::db::common
+    include role::labs::db::check_private_data
+
+    class { 'mariadb::packages_wmf': }
+    # disable starting a default instance
+    class {'mariadb::service':
+        override => "[Service]\nExecStartPre=/bin/sh -c \"echo 'mariadb main service is \
+disabled, use mariadb@<instance_name> instead'; exit 1\"",
+    }
+
+    if os_version('debian >= stretch') {
+        $basedir = '/opt/wmf-mariadb101'
+    } else {
+        $basedir = '/opt/wmf-mariadb10'
+    }
+    class { 'mariadb::config':
+        basedir       => $basedir,
+        config        => 'role/mariadb/mysqld_config/sanitarium_multiinstance.my.cnf.erb',
+        p_s           => 'on',
+        ssl           => 'puppet-cert',
+        binlog_format => 'ROW',
+    }
+
+    mariadb::instance {'s2':
+        port => 3312,
+    }
+    role::prometheus::mysqld_exporter_instance {'s2':
+        port => 13312,
+    }
+    mariadb::instance {'s6':
+        port => 3316,
+    }
+    role::prometheus::mysqld_exporter_instance {'s6':
+        port => 13316,
+    }
+    mariadb::instance {'s7':
+        port => 3317,
+    }
+    role::prometheus::mysqld_exporter_instance {'s7':
+        port => 13317,
+    }
+
+    class { 'mariadb::monitor_disk':
+        is_critical   => false,
+        contact_group => 'admins',
+    }
+
+    class { 'mariadb::monitor_process':
+        process_count => 3,
+        contact_group => 'admins',
+    }
+}
+
