6a043874035e2c25a56ff98b110133ab0ae41465
andrewbogott <abogott@wikimedia.org>
2017-07-27 23:39:47 -0500

    labs puppetmaster: validate cert name before autosigning
    Bug: T171289
    Change-Id: Ie057b49a304c646511cc46672965379ede862aca

---
+++ modules/puppetmaster/files/validatelabsfqdn.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import sys
+
+import mwopenstackclients
+
+
+certname = sys.argv[1]
+clients = mwopenstackclients.clients(envfile='/etc/novaobserver.yaml')
+
+pieces = certname.split('.')
+if len(pieces) != 4:
+    sys.exit('certname %s is formatted incorrectly' % certname)
+
+if pieces[2] != 'eqiad':
+    sys.exit('certname %s is for an invalid site' % certname)
+
+if pieces[3] != 'wmflabs':
+    sys.exit('certname %s does not end with wmflabs' % certname)
+
+certhostname = pieces[0]
+certproject = pieces[1]
+
+projects = [project.id for project in clients.allprojects()]
+if certproject not in projects:
+    sys.exit('certname %s is not for a real project' % certname)
+
+instances = [instance.name
+             for instance in clients.novaclient(certproject).servers.list()]
+if certhostname not in instances:
+    sys.exit('certname %s is not for a real instance' % certname)
+
+sys.exit(0)
