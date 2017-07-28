cc2e9b94939fdef4c43f5cd787a436c6d100d95d
cpettet <rush@wikimedia.org>
2017-07-28 17:40:11 -0500

    openstack: move rabbitmq to module/profile/role
    Requires the private 'rabbit_monitor_pass' value
    to be set for each deployment.
    
    - ferm components remain in the old role for now
    
    Bug: T171494
    Change-Id: I2e58497fbaf0a0866942d2e9caed5eae9a92c5b4

---
+++ modules/profile/manifests/openstack/main/rabbitmq.pp
@@ -0,0 +1,15 @@
+class profile::openstack::main::rabbitmq(
+    $nova_controller = hiera('profile::openstack::main::nova_controller'),
+    $monitor_user = hiera('profile::openstack::main::rabbit_monitor_user'),
+    $monitor_password = hiera('profile::openstack::main::rabbit_monitor_pass'),
+    $file_handles = hiera('profile::openstack::main::rabbit_file_handles'),
+){
+    require ::profile::openstack::main::cloudrepo
+
+    class {'::profile::openstack::base::rabbitmq':
+        nova_controller  => $nova_controller,
+        monitor_user     => $monitor_user,
+        monitor_password => $monitor_password,
+        file_handles     => $file_handles,
+    }
+}
