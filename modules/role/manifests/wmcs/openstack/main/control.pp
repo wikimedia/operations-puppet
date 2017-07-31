32ab62dfa126c67e35a6a61f0d04c59e772857f8
cpettet <rush@wikimedia.org>
2017-07-31 09:03:36 -0500

    openstack: replace cloudrepo placeholders for rabbitmq
    Bug: T171494
    Change-Id: Iae8bad9706faf06e3ba0b8afc7d0052333459a9e

--- modules/role/manifests/wmcs/openstack/main/control.pp
+++ modules/role/manifests/wmcs/openstack/main/control.pp
@@ -1,4 +1,4 @@
 class role::wmcs::openstack::main::control {
     include profile::openstack::main::cloudrepo
-    include profile::openstack::main::cloudrepo
+    include profile::openstack::main::rabbitmq
 }
