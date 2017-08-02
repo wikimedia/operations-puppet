e6e9edfba87a0b31116e3348c69b859ca8526440
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-08-02 18:38:53 +0200

    wdqs - remove upstart configuration files
    Now that all WDQS nodes are on Jessie, we can remove Trusty related
    configurations and move to systemd::unit instead of base::service_unit
    
    Change-Id: Ia5249b1af63fb70c32522ef81a3e1bfde967fee0

--- modules/wdqs/manifests/updater.pp
+++ modules/wdqs/manifests/updater.pp
@@ -4,18 +4,20 @@
 #
 class wdqs::updater(
     $options,
-    $package_dir = $::wdqs::package_dir,
-    $username = $::wdqs::username,
+    $package_dir,
+    $data_dir,
+    $username,
 ){
 
-    base::service_unit { 'wdqs-updater':
-        template_name  => 'wdqs-updater',
-        systemd        => true,
-        upstart        => true,
-        service_params => {
-            enable => true,
-        },
-        require        => [ File['/etc/wdqs/updater-logs.xml'],
-                            Service['wdqs-blazegraph'] ],
+    # Blazegraph service
+    systemd::unit { 'wdqs-updater':
+        content => template('wdqs/initscripts/wdqs-updater.systemd.erb'),
+        require => [
+            File['/etc/wdqs/updater-logs.xml'],
+            Service['wdqs-blazegraph']
+        ],
+    }
+    service { 'wdqs-updater':
+        ensure => 'running',
     }
 }
