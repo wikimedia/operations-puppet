55d3f35c16c9fd76eeaa4b7f6b960884744e60ae
addshore <addshorewiki@gmail.com>
2017-08-03 14:08:39 +0100

    Add ::statistics::wmde::wdcm
    Bug: T171258
    Change-Id: I9c8599bb94d94ccff34483a7dff1b0da2b78e55e

---
+++ modules/statistics/manifests/wmde/wdcm.pp
@@ -0,0 +1,62 @@
+# Licence AGPL version 3 or later
+#
+# @author Addshore
+#
+# Related task: https://phabricator.wikimedia.org/T171258
+#
+# == Parameters
+#   dir           - string. Directory to use.
+#   user          - string. User to use.
+class statistics::wmde::wdcm(
+    $dir,
+    $user  = 'analytics-wmde'
+) {
+
+    $src_dir  = "${dir}/src"
+
+    file { $dir:
+        ensure  => 'directory',
+        owner   => $user,
+        group   => $user,
+        mode    => '0644',
+        require => User[$user],
+    }
+
+    include ::r_lang
+
+    $cran_packages = [
+        # Needed by wmde/WDCM scripts:
+        # - WDCM_Sqoop_Clients.R
+        # - WDCM_Search_Clients.R
+        # - WDCM_Pre-Process.R
+        'httr',
+        'stringr',
+        'XML',
+        'readr',
+        'data.table',
+        'tidyr',
+        'maptpx',
+        'Rtsne',
+        'proxy',
+        'dplyr',
+        'htmltab',
+        'snowfall'
+    ]
+
+    r_lang::cran { $cran_packages:
+        mirror => 'https://cran.cnr.berkeley.edu',
+    }
+
+    git::clone { 'wmde/WDCM':
+        # TODO do we want a similar latest & production branch here? Or just manually pulling? scap?
+        # Currently when we update the code in the repo we will have to pull the updates ourselves.
+        ensure    => 'present',
+        branch    => 'master',
+        directory => $src_dir,
+        origin    => 'https://gerrit.wikimedia.org/r/analytics/wmde/WDCM',
+        owner     => $user,
+        group     => $user,
+        require   => File[$dir],
+    }
+
+}
