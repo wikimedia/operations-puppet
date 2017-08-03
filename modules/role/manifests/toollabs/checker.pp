669e4668d930827852d2df2effc0772256d40806
cpettet <rush@wikimedia.org>
2017-08-03 14:14:36 -0500

    openstack: toolschecker requires openstack clientlibs
    Bug: T171494
    Change-Id: Idffeec73413740f336339aaddb6d5533609489b9

--- modules/role/manifests/toollabs/checker.pp
+++ modules/role/manifests/toollabs/checker.pp
@@ -1,8 +1,9 @@
 # filtertags: labs-project-tools
 class role::toollabs::checker {
-    include ::toollabs::checker
+    require ::profile::openstack::main::clientlib
 
     system::role { 'toollabs::checker':
         description => 'Exposes end points for external monitoring of internal systems',
     }
+    include ::toollabs::checker
 }
