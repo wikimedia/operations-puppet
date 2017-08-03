4f3a0e2cfb7dc4741d909c3b6e8491ae1d752b20
andrewbogott <abogott@wikimedia.org>
2017-08-03 13:35:45 -0500

    labs puppetmaster: further attempt to allow ipv6 access to labservices
    Change-Id: Ia8ea5138f650e9d6be69e02382e2514d1799010f

--- modules/role/manifests/labs/puppetmaster/frontend.pp
+++ modules/role/manifests/labs/puppetmaster/frontend.pp
@@ -88,7 +88,7 @@ class role::labs::puppetmaster::frontend() {
             rule => "saddr (${horizon_host_ip} ${designate_host_ip}) proto tcp dport 8101 ACCEPT;",
         },
         puppetcertcleaning => {
-            rule => "saddr (${designate_host_ip}) proto tcp dport 22 ACCEPT;",
+            rule => "saddr (${designate_host_ip} @resolve(hiera('labs_designate_hostname'), AAAA)) proto tcp dport 22 ACCEPT;",
         },
         puppetbackendgetter => {
             rule => "saddr (${labs_vms} ${labs_metal} ${monitoring} ${horizon_host_ip} @resolve((${all_puppetmasters})) @resolve((${all_puppetmasters}), AAAA)) proto tcp dport 8100 ACCEPT;",
