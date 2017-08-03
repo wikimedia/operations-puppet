7099a25e12de9cb69ca0926cd553f347fb918f2d
addshore <addshorewiki@gmail.com>
2017-08-03 14:08:39 +0100

    WIP DNM Add ::statistics::wmde::wikidata_concepts
    Bug: T171258
    Change-Id: I9c8599bb94d94ccff34483a7dff1b0da2b78e55e

---
+++ modules/statistics/manifests/wmde/wikidata_concepts.pp
@@ -0,0 +1,36 @@
+# Licence AGPL version 3 or later
+#
+# @author Addshore
+#
+# Related task: https://phabricator.wikimedia.org/T171258
+#
+# == Parameters
+#   dir           - string. Directory to use.
+#   user          - string. User to use.
+class statistics::wmde::wikidata_concepts(
+    $dir,
+    $user  = 'analytics-wmde'
+) {
+
+    $src_dir  = "${dir}/src"
+
+    file { $dir:
+        ensure  => 'directory',
+        owner   => $user,
+        group   => $user,
+        mode    => '0644',
+        require => User[$user],
+    }
+
+    git::clone { 'wmde/WDCM':
+        # TODO do we want a similar latest & production branch here? Or just manually pulling? scap?
+        ensure    => 'present',
+        branch    => 'master',
+        directory => $src_dir,
+        origin    => 'https://gerrit.wikimedia.org/r/analytics/wmde/WDCM',
+        owner     => $user,
+        group     => $user,
+        require   => File["${dir}"],
+    }
+
+}
