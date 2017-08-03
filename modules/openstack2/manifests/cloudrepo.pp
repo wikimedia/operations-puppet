ef4b50606af8dce11fc49231552bfad23168e347
cpettet <rush@wikimedia.org>
2017-08-03 16:33:16 -0500

    openstack: clean up openstack::repo
    * Move key to openstack2
    * remove openstack::repo
    
    Bug: T171494
    Change-Id: Icd76c5ec4fbfc297baeb7877123070c440306217

--- modules/openstack2/manifests/cloudrepo.pp
+++ modules/openstack2/manifests/cloudrepo.pp
@@ -1,6 +1,4 @@
-# sets upt APT repository for labs openstack.
-#  We use the Ubuntu cloud archive for this -- this repo points us to the
-#  package versions specified in $::openstack::version
+# sets upt APT repository for labs openstack
 
 # class default kept here until components not yet moved to profiles
 # for parameterization are completed in modules/openstack
@@ -14,10 +12,9 @@ class openstack2::cloudrepo(
             uri        => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
             dist       => "trusty-updates/${version}",
             components => 'main',
-            keyfile    => 'puppet:///modules/openstack/ubuntu-cloud.key';
+            keyfile    => 'puppet:///modules/openstack2/cloudrepo/ubuntu-cloud.key';
         }
     } elsif os_version('debian jessie') and ($version != 'liberty') {
-        # Stock Jessie seems to come with Liberty packages
         fail("T169099: There is no plan for ${version} on Jessie")
     }
 }
