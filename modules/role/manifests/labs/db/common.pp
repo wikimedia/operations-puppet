531517e9b4259597388d84417280a26b7993a24c
Jaime Crespo <jcrespo@wikimedia.org>
2017-08-03 16:59:44 +0200

    labsdb/mariadb: Change package resource for require_package function
    This will avoid current and future collisions when the same package
    is required multiple times.
    
    Change-Id: I8ec85f6e9b35560ee413bbacf4f3519adcbe03ce

--- modules/role/manifests/labs/db/common.pp
+++ modules/role/manifests/labs/db/common.pp
@@ -1,9 +1,9 @@
 # class of common setups needed by several roles on labsdb
 class role::labs::db::common {
-    package {
-        ['python3-yaml', 'python3-pymysql']:
-            ensure => present,
-    }
+    require_package (
+        'python3-yaml',
+        'python3-pymysql',
+    )
 
     git::clone { 'operations/mediawiki-config':
         ensure             => 'latest',
