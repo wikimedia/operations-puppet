91626adaad744bfd85d8754b632fc489bde8c9d7
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-08-04 15:02:16 +0200

    puppetdb: spin off module, support hsqldb
    We start generalizing the puppetdb setup, allowing use of the hsqldb
    setup so that we can have a simplified setup for labs or other uses,
    like the puppet-compiler.
    
    Bug: T150456
    Change-Id: I314e4ed6b29c69c2b1e517745553c84ca2bbfe02

---
+++ modules/puppetdb/manifests/app.pp
@@ -0,0 +1,119 @@
+# ==  Class puppetdb::app
+#
+# Sets up the puppetdb clojure app.
+# This assumes you're using
+#
+class puppetdb::app(
+    $db_rw_host,
+    $ca_path='/etc/ssl/certs/Puppet_Internal_CA.pem',
+    $db_driver='postgres',
+    $db_ro_host=undef,
+    $db_user='puppetdb',
+    $db_password=undef,
+    $perform_gc=false,
+    $heap_size='4G',
+) {
+    requires_os('debian >= jessie')
+
+    ## PuppetDB installation
+
+    package { 'puppetdb':
+        ensure => present,
+    }
+
+    ## Configuration
+
+    file { '/etc/puppetdb/conf.d':
+        ensure  => directory,
+        owner   => 'puppetdb',
+        group   => 'root',
+        mode    => '0750',
+        recurse => true,
+        require => Package['puppetdb'],
+    }
+
+    # Ensure the default debian config file is not there
+
+    file { '/etc/puppetdb/conf.d/config.ini':
+        ensure => absent,
+    }
+
+    if $db_driver == 'postgres' {
+        $default_db_settings = {
+            'classname'   => 'org.postgresql.Driver',
+            'subprotocol' => 'postgresql',
+            'username'    => 'puppetdb',
+            'password'    => $db_password,
+            'subname'     => "//${db_rw_host}:5432/puppetdb?ssl=true",
+        }
+    } elsif $db_driver == 'hsqldb' {
+        $default_db_settings = {
+            'classname'   => 'org.hsqldb.jdbcDriver',
+            'subprotocol' => 'hsqldb',
+            'subname'     => 'file:/var/lib/puppetdb/db/puppet.hsql;hsqldb.tx=mvcc;sql.syntax_pgs=true',
+        }
+    } else {
+        fail("Unsupported db driver ${db_driver}")
+    }
+
+    if $perform_gc {
+        $db_settings = merge(
+            $default_db_settings,
+            { 'report-ttl' => '1d', 'gc-interval' => '20' }
+        )
+    } else {
+        $db_settings = $default_db_settings
+    }
+
+    puppetmaster::puppetdb::config { 'database':
+        settings => $db_settings,
+    }
+
+    #read db settings
+    if $db_ro_host and $db_driver == 'postgres' {
+        $read_db_settings = merge(
+            $default_db_settings,
+            {'subname' => "//${db_ro_host}:5432/puppetdb?ssl=true"}
+        )
+        puppetmaster::puppetdb::config { 'read-database':
+            settings => $read_db_settings,
+        }
+    }
+
+    puppetmaster::puppetdb::config { 'global':
+        settings => {
+            'vardir'         => '/var/lib/puppetdb',
+            'logging-config' => '/etc/puppetdb/logback.xml',
+        },
+    }
+
+    puppetmaster::puppetdb::config { 'repl':
+        settings => {'enabled' => false},
+    }
+
+    ::base::expose_puppet_certs { '/etc/puppetdb':
+        ensure          => present,
+        provide_private => true,
+        user            => 'puppetdb',
+        group           => 'puppetdb',
+
+    }
+
+    puppetmaster::puppetdb::config { 'jetty':
+        settings => {
+            'port'        => 8080,
+            'ssl-port'    => 8081,
+            'ssl-key'     => '/etc/puppetdb/ssl/server.key',
+            'ssl-cert'    => '/etc/puppetdb/ssl/cert.pem',
+            'ssl-ca-cert' => $ca_path,
+        },
+        require  => Base::Expose_puppet_certs['/etc/puppetdb'],
+    }
+
+    # Systemd unit and service declaration
+    systemd::service { 'puppetdb':
+        ensure  => present,
+        content => template('puppetdb/puppetdb.service.erb'),
+        restart => true,
+    }
+}
--- modules/puppetmaster/manifests/puppetdb.pp
+++ modules/puppetmaster/manifests/puppetdb.pp
@@ -33,101 +33,11 @@ class puppetmaster::puppetdb(
 
     ## PuppetDB installation
 
-    package { 'puppetdb':
-        ensure => present,
-    }
-
-
-    ## Configuration
-
-    file { '/etc/puppetdb/conf.d':
-        ensure  => directory,
-        owner   => 'puppetdb',
-        group   => 'root',
-        mode    => '0750',
-        recurse => true,
-        require => Package['puppetdb'],
-    }
-
-    # Ensure the default debian config file is not there
-
-    file { '/etc/puppetdb/conf.d/config.ini':
-        ensure => absent,
-    }
-
-    $default_db_settings = {
-        'classname'   => 'org.postgresql.Driver',
-        'subprotocol' => 'postgresql',
-        'username'    => 'puppetdb',
-        'password'    => $puppetdb_pass,
-    }
-
-    # Read-write connections go to the master.
-    # Only the master should be performing housekeeping work
-    if ($master == $::fqdn) {
-        $db_settings = merge(
-            $default_db_settings,
-            {
-                'report-ttl'  => '1d',
-                'gc-interval' => '20',
-                'subname'     => "//${master}:5432/puppetdb?ssl=true",
-            }
-        )
-    } else {
-        $db_settings = merge(
-            $default_db_settings,
-            {'subname' => "//${master}:5432/puppetdb?ssl=true"}
-        )
-    }
-
-    puppetmaster::puppetdb::config { 'database':
-        settings => $db_settings,
-    }
-
-    # The read database is always the local one.
-    $read_db_settings = merge(
-        $default_db_settings,
-        {
-            'subname' => "//${::fqdn}:5432/puppetdb?ssl=true"
-        }
-    )
-    puppetmaster::puppetdb::config { 'read-database':
-        settings => $read_db_settings,
-    }
-
-    ::base::expose_puppet_certs { '/etc/puppetdb':
-        ensure          => present,
-        provide_private => true,
-        user            => 'puppetdb',
-        group           => 'puppetdb',
-
-    }
-
-    puppetmaster::puppetdb::config { 'jetty':
-        settings => {
-            'port'        => 8080,
-            'ssl-port'    => 8081,
-            'ssl-key'     => '/etc/puppetdb/ssl/server.key',
-            'ssl-cert'    => '/etc/puppetdb/ssl/cert.pem',
-            'ssl-ca-cert' => '/etc/ssl/certs/Puppet_Internal_CA.pem',
-        },
-        require  => Base::Expose_puppet_certs['/etc/puppetdb'],
-    }
-
-    puppetmaster::puppetdb::config { 'global':
-        settings => {
-            'vardir'         => '/var/lib/puppetdb',
-            'logging-config' => '/etc/puppetdb/logback.xml',
-        },
-    }
-
-    puppetmaster::puppetdb::config { 'repl':
-        settings => {'enabled' => false},
-    }
-
-    ## Enable the service itself
-    base::service_unit { 'puppetdb':
-        ensure  => present,
-        systemd => true,
+    class { 'puppetdb::app':
+        db_rw_host  => $master,
+        db_ro_host  => $::fqdn,
+        db_password => $puppetdb_pass,
+        perform_gc  => ($master == $::fqdn), # only the master must perform GC
+        heap_size   => $heap_size,
     }
 }
