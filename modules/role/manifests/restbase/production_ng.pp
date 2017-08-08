bc940cda4b742c517af179846e9fa81dd73de6f4
Eric Evans <eevans@wikimedia.org>
2017-08-08 18:02:50 -0400

    Reshape RESTBase Cassandra production cluster; Provision new 3.x cluster
      Basic steps:
    
      - 9T of storage will be freed
      - restbase2001.codfw.wmnet is decommissioned
      - remove/depool restbase2001 from LVS
      - this changeset is merged
      - restbase2001.codfw.wmnet is re-imaged
      - restbase2001.codfw.wmnet is bootstrapped
      - new changeset to comment/uncomment instances is merged
        - lather
        - rinse
        - repeat
    
    Bug: T169939
    Change-Id: Ife09538ec52e8f48b8d0899d81b1c961a8476b94

---
+++ modules/role/manifests/restbase/production_ng.pp
@@ -0,0 +1,10 @@
+# == Class role::restbase::production_ng
+#
+# Configures the production cluster (next-gen)
+class role::restbase::production_ng {
+    include ::passwords::cassandra
+    include ::base::firewall
+    include ::standard
+    include ::profile::cassandra
+    system::role { 'restbase': description => 'Restbase (Cassandra 3.x-only)' }
+}
