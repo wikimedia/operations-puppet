ba0fa487489fad8b37527b7ca7a977af2a4534d3
cpettet <rush@wikimedia.org>
2017-08-09 15:45:11 +0000

    openstack: keystone as module/profile/role for deployments
    Bug: T171494
    Change-Id: I1167b5dac438f2127def1009c07410b31aa0127f

--- modules/role/manifests/wmcs/openstack/labtest/control.pp
+++ modules/role/manifests/wmcs/openstack/labtest/control.pp
@@ -3,4 +3,5 @@ class role::wmcs::openstack::labtest::control {
     include ::profile::openstack::labtest::clientlib
     include ::profile::openstack::labtest::observerenv
     include ::profile::openstack::labtest::rabbitmq
+    include ::profile::openstack::labtest::keystone::service
 }
