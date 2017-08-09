330f19fa638b02cfefa11116262ac34bd50b13b5
elukey <ltoscano@wikimedia.org>
2017-08-09 15:08:44 +0200

    role::analytics_cluster::hadoop::client: move to profiles (p2)
    Bug: T167790
    Change-Id: Ia2dc439dc885953c78f35425a39bd7b9ed8ef109

--- modules/role/manifests/analytics_cluster/mysql_password.pp
+++ modules/role/manifests/analytics_cluster/mysql_password.pp
@@ -5,7 +5,7 @@
 # out of MySQL into Hadoop.
 #
 class role::analytics_cluster::mysql_password {
-    Class['role::analytics_cluster::hadoop::client'] -> Class['role::analytics_cluster::mysql_password']
+    require profile::hadoop::client
 
     include ::passwords::mysql::research
     $research_user = $::passwords::mysql::research::user
