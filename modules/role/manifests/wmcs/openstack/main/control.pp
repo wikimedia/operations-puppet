bab419042ce0ca5e8b5187463bcfc30f25161e78
cpettet <rush@wikimedia.org>
2017-08-09 15:45:11 +0000

    openstack: keystone as module/profile/role for deployments
    Bug: T171494
    Change-Id: I1167b5dac438f2127def1009c07410b31aa0127f

--- modules/role/manifests/wmcs/openstack/main/control.pp
+++ modules/role/manifests/wmcs/openstack/main/control.pp
@@ -3,4 +3,5 @@ class role::wmcs::openstack::main::control {
     include ::profile::openstack::main::clientlib
     include ::profile::openstack::main::observerenv
     include ::profile::openstack::main::rabbitmq
+    include ::profile::openstack::main::keystone::service
 }
