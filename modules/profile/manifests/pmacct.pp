1ca2c7f5650842013e09de454267e3acd573e337
elukey <ltoscano@wikimedia.org>
2017-08-14 13:47:01 +0200

    role::pmacct: explicitly set librkafka version
    rhenium.wikimedia.org runs Debian Stretch and librdkafka
    0.11, which turns on by default the Kafka API protocol
    auto-discovery:
    https://github.com/edenhill/librdkafka/releases/tag/v0.11.0
    
    The Kafka Analytics cluster is currently running 0.9.0.1,
    forcing this version as attempt to avoid errors logged
    in the brokers logs (and timeouts for other producers).
    
    Bug: T172681
    Change-Id: I4e77b1207d646f2b9a101fa4dc827ee9341e8a90

---
+++ modules/profile/manifests/pmacct.pp
@@ -0,0 +1,62 @@
+# == Class profile::pmacct
+# Sets up a pmacct passive monitoring deployment (http://www.pmacct.net/).
+# It can also produce statistics/data to kafka if configured.
+#
+# [*kafka_config*]
+#   Kafka cluster configuration to use.
+#   FIXME: The default version uses an indirect hiera call via kafka_config(),
+#   so eventually this parameter would need to be replaced with an explicit
+#   hiera call. It seems good though to explicitly isolate hiera calls in the
+#   parameters as the current Puppet coding standards suggest.
+#
+# [*librdkafka_config*]
+#   List of librdkafka configs settings specified in the format indicated
+#   by upstream:
+#   topic, settingX, valueX
+#   global, settingY, valueY
+#
+class profile::pmacct (
+    $kafka_config      = kafka_config('analytics'),
+    $librdkafka_config = hiera('profile::pmacct::librdkafka_config'),
+) {
+    system::role { 'pmacct':
+        description => 'pmacct netflow accounting',
+    }
+
+    class { '::pmacct':
+        kafka_brokers     => $kafka_config['brokers']['string'],
+        librdkafka_config => $librdkafka_config,
+    }
+
+    include ::standard
+    include ::base::firewall
+
+    $loopbacks = [
+        # eqiad
+        '208.80.154.196/30',
+        '2620:0:861:ffff::/64',
+        # codfw
+        '208.80.153.192/29',
+        '2620:0:860:ffff::/64',
+        # esams
+        '91.198.174.244/30',
+        '2620:0:862:ffff::/64',
+        # ulsfo
+        '198.35.26.192/30',
+        '2620:0:863:ffff::/64',
+    ]
+
+    ferm::service { 'bgp':
+        proto  => 'tcp',
+        port   => '179',
+        desc   => 'BGP',
+        srange => inline_template('(<%= @loopbacks.join(" ") %>)'),
+    }
+
+    ferm::service { 'netflow':
+        proto  => 'udp',
+        port   => '2100',
+        desc   => 'NetFlow',
+        srange => inline_template('(<%= @loopbacks.join(" ") %>)'),
+    }
+}
