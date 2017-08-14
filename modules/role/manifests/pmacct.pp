37106367ab4bf8f6769545925b467314f924c9e4
elukey <ltoscano@wikimedia.org>
2017-08-14 13:47:01 +0200

    role::pmacct: explicitly set librkafka version
    rhenium.wikimedia.org runs Debian Stretch and librdkafka
    0.11, which turns on by default the Kafka API protocol
    auto-discovery:
    https://github.com/edenhill/librdkafka/releases/tag/v0.11.0
    
    The Kafka Analytics cluster is currently running 0.9.0.1,
    forcing this version as attempt to avoid errors logged
    in the brokers logs (and timeouts for other producers).
    
    Bug: T172681
    Change-Id: I4e77b1207d646f2b9a101fa4dc827ee9341e8a90

--- modules/role/manifests/pmacct.pp
+++ modules/role/manifests/pmacct.pp
@@ -1,43 +1,4 @@
 #
 class role::pmacct {
-    system::role { 'pmacct':
-        description => 'pmacct netflow accounting',
-    }
-
-    $kafka_config  = kafka_config('analytics')
-    class { '::pmacct':
-        kafka_brokers => $kafka_config['brokers']['string'],
-    }
-
-    include ::standard
-    include ::base::firewall
-
-    $loopbacks = [
-        # eqiad
-        '208.80.154.196/30',
-        '2620:0:861:ffff::/64',
-        # codfw
-        '208.80.153.192/29',
-        '2620:0:860:ffff::/64',
-        # esams
-        '91.198.174.244/30',
-        '2620:0:862:ffff::/64',
-        # ulsfo
-        '198.35.26.192/30',
-        '2620:0:863:ffff::/64',
-    ]
-
-    ferm::service { 'bgp':
-        proto  => 'tcp',
-        port   => '179',
-        desc   => 'BGP',
-        srange => inline_template('(<%= @loopbacks.join(" ") %>)'),
-    }
-
-    ferm::service { 'netflow':
-        proto  => 'udp',
-        port   => '2100',
-        desc   => 'NetFlow',
-        srange => inline_template('(<%= @loopbacks.join(" ") %>)'),
-    }
+    include ::profile::pmacct
 }
