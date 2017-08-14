c922087a37a52de68f38c5c15a8ac0d7eb0c7297
elukey <ltoscano@wikimedia.org>
2017-08-14 13:29:20 +0200

    pmacct: add the possibility to configure librdkafka
    First step to be able to eventually set the kafka API
    protocol version for librdkafka following this document:
    
    https://github.com/edenhill/librdkafka/wiki/Broker-version-compatibility
    
    rhenium is currently running stretch and librdkafka 0.11,
    meanwhile the analytics brokers are on kafka 0.9.0.1.
    
    Bug: T172681
    Change-Id: I015683aa99e06bab64ae7e8c7783d9170f92b7f0

--- modules/pmacct/manifests/init.pp
+++ modules/pmacct/manifests/init.pp
@@ -9,11 +9,15 @@
 #  include pmacct
 
 class pmacct(
-  $kafka_brokers = undef,
+  $kafka_brokers     = undef,
+  $librdkafka_config = undef,
 ) {
     package { 'pmacct':
         ensure => present,
     }
+
+    # Valid only when librdkafka_config is not undef
+    $kafka_config_file = '/etc/pmacct/librdkafka.conf'
 
     file { '/etc/pmacct/nfacctd.conf':
         ensure  => present,
@@ -24,6 +28,19 @@ class pmacct(
         require => Package['pmacct'],
         before  => Service['nfacctd'],
         notify  => Service['nfacctd'],
+    }
+
+    if $librdkafka_config != undef {
+        file { $kafka_config_file:
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0440',
+            content => template('pmacct/librdkafka.conf.erb'),
+            require => Package['pmacct'],
+            before  => Service['nfacctd'],
+            notify  => Service['nfacctd'],
+        }
     }
 
     service { 'nfacctd':
