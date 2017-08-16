416ddd7fe36cddae7e3a75015ed7ef16da01fbfc
elukey <ltoscano@wikimedia.org>
2017-08-16 12:52:28 +0200

    Introduce role::analytics_cluster::coordinator
    Bug: T167790
    Change-Id: I94480677b16821054aaa85ac7d0d7d345c0fec40

---
+++ modules/role/manifests/analytics_cluster/coordinator.pp
@@ -0,0 +1,51 @@
+# == Class role::analytics_cluster::coordinator
+#
+# This role includes Oozie and Hive servers, as well as a MySQL instance
+# that stores meta data associated with those and other
+# Analytics Cluster services.
+#
+# This role is a launch pad for various cron based Hadoop jobs.
+# Many ingestion jobs need a starting point.  Oozie is a great
+# Hadoop job scheduler, but it is not better than cron
+# for some jobs that need to be launched at regular time
+# intervals.  Cron is used for those.  These crons
+# do not use local resources, instead, they launch
+# Hadoop jobs that run throughout the cluster.
+#
+# This roles sets up a node responsible to coordinate and orchestrate
+# a Hadoop cluster equipped with tools like Camus, Hive, Oozie and
+# the Analytics Refinery.
+#
+class role::analytics_cluster::coordinator {
+    include ::role::analytics_cluster::client
+    include ::role::analytics_cluster::database::meta
+
+    # Back up analytics-meta MySQL instance
+    # to analytics1002. $dest is configured in
+    # hieradata/role/eqiad/analytics_cluster/database/meta/backup.yaml
+    include ::role::analytics_cluster::database::meta::backup
+    include ::role::analytics_cluster::hive::metastore::database
+    include ::role::analytics_cluster::oozie::server::database
+    include ::role::analytics_cluster::hive::metastore
+    include ::role::analytics_cluster::hive::server
+    include ::role::analytics_cluster::oozie::server
+
+    # Include a weekly cron job to run hdfs balancer.
+    include ::role::analytics_cluster::hadoop::balancer
+
+    # We need hive-site.xml in HDFS.  This can be included
+    # on any node with a Hive client, but we really only
+    # want to include it in one place.  analytics1003
+    # is a little special and standalone, so we do it here.
+    include ::role::analytics_cluster::hive::site_hdfs
+
+    # Camus crons import data into
+    # from Kafka into HDFS.
+    include ::role::analytics_cluster::refinery::job::camus
+
+    # Various crons that launch Hadoop jobs.
+    include ::role::analytics_cluster::refinery
+    include ::role::analytics_cluster::refinery::job::data_drop
+    include ::role::analytics_cluster::refinery::job::project_namespace_map
+    include ::role::analytics_cluster::refinery::job::sqoop_mediawiki
+}
