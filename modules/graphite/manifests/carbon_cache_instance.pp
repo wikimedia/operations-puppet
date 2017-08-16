3a5d7e7eeffc9d30a251383f04ae6683ae620b6c
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-08-16 14:57:57 +0200

    graphite: cleanup carbon-cache log files
    Bug: T173401
    Change-Id: I44490dce08e10c565b6786b1ab13e4c84bf523e0

--- modules/graphite/manifests/carbon_cache_instance.pp
+++ modules/graphite/manifests/carbon_cache_instance.pp
@@ -1,8 +1,18 @@
 define graphite::carbon_cache_instance {
-    service { "carbon-cache@${title}":
+    $service_name = "carbon-cache@${title}"
+    $log_dir = "/var/log/carbon/${service_name}"
+
+    service { $service_name:
         ensure   => 'running',
         provider => 'systemd',
         enable   => true,
         require  => File['/lib/systemd/system/carbon-cache@.service'],
+    }
+
+    cron { "${service_name}-cleanup":
+        command => "[ -d ${log_dir} ] && find ${log_dir} -type f -mtime +15 -iname '*.log.*' -delete",
+        user    => '_graphite',
+        hour    => fqdn_rand(24, $title),
+        minute  => fqdn_rand(60, $title),
     }
 }
