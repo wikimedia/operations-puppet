4cbe94da2354664af3dcd6500434c3c19ef69c80
Jaime Crespo <jcrespo@wikimedia.org>
2017-08-17 17:19:55 +0200

    mariadb: Allow individual instances to configure its innodb BPS
    Allow instances part of a mysql/mariadb multi-instance server to
    have differen per-instance configuration of innodb_buffer_pool_size,
    allowing some instances to get more memory (for example, enwiki may
    need more memory than x1).
    
    Also remove obsolete ferm rule for instance on port 3306 (no
    instance is running there).
    
    Bug: T169514
    Change-Id: I2f95691696742974cc9fe7715c771bb9932b71bc

--- modules/role/manifests/mariadb/dbstore_multiinstance.pp
+++ modules/role/manifests/mariadb/dbstore_multiinstance.pp
@@ -11,8 +11,6 @@ class role::mariadb::dbstore_multiinstance {
         port   => '3311:3320',
         srange => '$PRODUCTION_NETWORKS',
     }
-    # Temporary extra instance on port 3306:
-    include role::mariadb::ferm
 
     #TODO: define one group per shard
     class {'mariadb::groups':
@@ -49,29 +47,73 @@ disabled, use mariadb@<instance_name> instead'; exit 1\"",
         mode   => '0755',
     }
 
-    mariadb::instance {'s1': port => 3311, }
-    role::prometheus::mysqld_exporter_instance {'s1': port => 13311, }
-    mariadb::instance {'s2': port => 3312, }
-    role::prometheus::mysqld_exporter_instance {'s2': port => 13312, }
-    mariadb::instance {'s3': port => 3313, }
-    role::prometheus::mysqld_exporter_instance {'s3': port => 13313, }
-    mariadb::instance {'s4': port => 3314, }
-    role::prometheus::mysqld_exporter_instance {'s4': port => 13314, }
-    mariadb::instance {'x1': port => 3320, }
-    role::prometheus::mysqld_exporter_instance {'x1': port => 13320, }
-
-    if $::hostname != 'dbstore2002' {
-        $num_instances = 8
-        mariadb::instance {'s5': port => 3315, }
-        role::prometheus::mysqld_exporter_instance {'s5': port => 13315, }
-        mariadb::instance {'s6': port => 3316, }
-        role::prometheus::mysqld_exporter_instance {'s6': port => 13316, }
-        mariadb::instance {'s7': port => 3317, }
-        role::prometheus::mysqld_exporter_instance {'s7': port => 13317, }
-    } else {
-        $num_instances = 5
+    $s1 = hiera('role::mariadb::dbstore_multiinstance::s1', false)
+    if $s1 {
+        mariadb::instance { 's1':
+            port                    => 3311,
+            innodb_buffer_pool_size => $s1,
+        }
+        role::prometheus::mysqld_exporter_instance {'s1': port => 13311, }
+    }
+    $s2 = hiera('role::mariadb::dbstore_multiinstance::s2', false)
+    if $s2 {
+        mariadb::instance { 's2':
+            port                    => 3312,
+            innodb_buffer_pool_size => $s2,
+        }
+        role::prometheus::mysqld_exporter_instance { 's2': port => 13312, }
+    }
+    $s3 = hiera('role::mariadb::dbstore_multiinstance::s3', false)
+    if $s3 {
+        mariadb::instance { 's3':
+            port                    => 3313,
+            innodb_buffer_pool_size => $s2,
+        }
+        role::prometheus::mysqld_exporter_instance {'s3': port => 13313, }
+    }
+    $s4 = hiera('role::mariadb::dbstore_multiinstance::s4', false)
+    if $s4 {
+        mariadb::instance { 's4':
+            port                    => 3314,
+            innodb_buffer_pool_size => $s4,
+        }
+        role::prometheus::mysqld_exporter_instance { 's4': port => 13314, }
+    }
+    
+    $s5 = hiera('role::mariadb::dbstore_multiinstance::s5', false)
+    if $s5 {
+        mariadb::instance { 's5':
+            port => 3315,
+            innodb_buffer_pool_size => $s5,
+        }
+        role::prometheus::mysqld_exporter_instance { 's5': port => 13315, }
+    }
+    $s6 = hiera('role::mariadb::dbstore_multiinstance::s6', false)
+    if $s6 {
+        mariadb::instance { 's6':
+            port                    => 3316,
+            innodb_buffer_pool_size => $s6,
+        }
+        role::prometheus::mysqld_exporter_instance { 's6': port => 13316, }
+    }
+    $s7 = hiera('role::mariadb::dbstore_multiinstance::s7', false)
+    if $s7 {
+        mariadb::instance { 's7':
+            port                    => 3317,
+            innodb_buffer_pool_size => $s7,
+        }
+        role::prometheus::mysqld_exporter_instance { 's7': port => 13317, }
     }
 
+    $x1 = hiera('role::mariadb::dbstore_multiinstance::x1', false)
+    if $x1 {
+        mariadb::instance { 'x1':
+            port                    => 3320,
+            innodb_buffer_pool_size => '5G',
+        }
+        role::prometheus::mysqld_exporter_instance { 'x1': port => 13320, }
+    }
+    
     require_package ('mydumper')
 
     class { 'mariadb::monitor_disk':
@@ -79,6 +121,7 @@ disabled, use mariadb@<instance_name> instead'; exit 1\"",
         contact_group => 'admins',
     }
 
+    $num_instances = hiera('role::mariadb::dbstore_multiinstance::num_instances', 8)
     class { 'mariadb::monitor_process':
         process_count => $num_instances,
         is_critical   => false,
