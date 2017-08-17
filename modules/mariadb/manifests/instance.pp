675f0022c23008e5cbe4e6c35c26c6237e5150ff
Jaime Crespo <jcrespo@wikimedia.org>
2017-08-17 17:19:55 +0200

    mariadb: Allow individual instances to configure its innodb BPS
    Allow instances part of a mysql/mariadb multi-instance server to
    have differen per-instance configuration of innodb_buffer_pool_size,
    allowing some instances to get more memory (for example, enwiki may
    need more memory than x1).
    
    Also remove obsolete ferm rule for instance on port 3306 (no
    instance is running there).
    
    Bug: T169514
    Change-Id: I2f95691696742974cc9fe7715c771bb9932b71bc

--- modules/mariadb/manifests/instance.pp
+++ modules/mariadb/manifests/instance.pp
@@ -1,10 +1,20 @@
 # Setups additional instances for hosts that hosts more
 # than one instance
+# * port: Port where to run the instance (required)
+# * datadir: datadir mysql config, by default /srv/sqldata.title
+# * tmpdir: datadir mysql config, by default /srv/tmp.title
+# * socket: socket mysql config, by default /run/mysqld/mysqld.title.sock
+# * innodb_buffer_pool_size: config of the same name, it controls how much
+#   memory the instace uses. By default (or if it is configured as false,
+#   , it is unconfigured, and it will default to the one on the common
+#   config template (or the mysql default, if not configured there). When
+#   configured, it must be passed as a string, such as '11G' or '10000000'.
 define mariadb::instance(
     $port,
     $datadir = 'undefined',
     $tmpdir  = 'undefined',
     $socket  = 'undefined',
+    $innodb_buffer_pool_size = false,
 ) {
     if $datadir == 'undefined' {
         $datadir_instance = "/srv/sqldata.${title}"
