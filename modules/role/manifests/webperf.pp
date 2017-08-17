f359b679d040fed2e45f34e5b8e6174ee2094807
Timo Tijhof <krinklemail@gmail.com>
2017-08-17 16:40:56 -0700

    webperf: Convert navtiming.py to use KafkaConsumer
    Test plain:
    * Copy file to hafnium
    * Hot fix dispatch_stat:
      - `sock.sendto(stat.encode('utf-8'), addr)`
      + `print stat.encode('utf-8')`
    * Invoke as:
      $ python nav-test.py  --consumer-group testing \
        --brokers kafka1018.eqiad.wmnet:9092,kafka1022.eqiad.wmnet:9092
      ...
      frontend.navtiming.domInteractive.by_continent.Europe:807|ms
      mw.performance.save:905|ms
      mw.performance.save_by_version.1_30_0-wmf_14:905|ms
      ...
    
    Bug: T110903
    Change-Id: I39daa310f350ca5e166463ba835481fbe9b23d2c

--- modules/role/manifests/webperf.pp
+++ modules/role/manifests/webperf.pp
@@ -7,28 +7,42 @@ class role::webperf {
 
     include ::standard
     include ::base::firewall
-    include ::eventlogging
-    include ::webperf::statsv
 
-    $eventlogging_host = 'eventlog1001.eqiad.wmnet'
-    $statsd_host = 'statsd.eqiad.wmnet'
-    # Installed by eventlogging class using trebuchet
-    $eventlogging_path = '/srv/deployment/eventlogging/eventlogging'
+    $statsd = hiera('statsd')
+    $statsd_parts = split($statsd, ':')
+    $statsd_host = $statsd_parts[0]
+    $statsd_port = $statds_parts[1]
+
+    # Use brokers from this Kafka cluster to consume metrics.
+    $kafka_config  = kafka_config('analytics')
+    $kafka_brokers = $kafka_config['brokers']['string']
+
+    # Consume statsd metrics from Kafka and emit them to statsd.
+    class { '::webperf::statsv':
+        kafka_brokers => $kafka_brokers,
+        statsd        => $statsd,
+    }
 
     # Aggregate client-side latency measurements collected via the
     # NavigationTiming MediaWiki extension and send them to Graphite.
     # See <https://www.mediawiki.org/wiki/Extension:NavigationTiming>
     class { '::webperf::navtiming':
-        endpoint          => "tcp://${eventlogging_host}:8600",
-        eventlogging_path => $eventlogging_path,
-        statsd_host       => $statsd_host,
+        kafka_brokers => $kafka_brokers,
+        statsd_host   => $statsd_host,
+        statsd_port   => $statsd_port,
     }
 
+    # TODO: Remove eventlogging specific things once ve uses Kafka: T110903
+    include ::eventlogging
+    $eventlogging_host = 'eventlog1001.eqiad.wmnet'
+    # Installed by eventlogging class using trebuchet
+    $eventlogging_path = '/srv/deployment/eventlogging/eventlogging'
     # Report VisualEditor performance measurements to Graphite.
     # See <https://meta.wikimedia.org/wiki/Schema:TimingData>
     class { '::webperf::ve':
         endpoint          => "tcp://${eventlogging_host}:8600",
         eventlogging_path => $eventlogging_path,
         statsd_host       => $statsd_host,
+        statsd_port       => $statsd_port,
     }
 }
