f253f226d56f6b95c65adb72363c8d782d435c7f
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-08-17 13:13:10 +0200

    WIP: new prometheus instance 'services'
    Bug: T173490
    Change-Id: I88c11a302f0c430cebe38aef56ac2c160c180e74

---
+++ modules/prometheus/manifests/jmx_exporter_config.pp
@@ -0,0 +1,36 @@
+# == Define: prometheus::jmx_exporter_config
+#
+# Generate prometheus targets configuration for all nodes belonging to $class
+# and are defining prometheus::jmx_exporter_instance.
+# Data is gathered using the puppetdb querying functions
+
+# == Parameters
+# $dest:    The output file where to write the result.
+# $class:   Class name to search.
+# $site:    The site to use.
+# $labels:  Labels to attach to every host. 'Cluster' will be added automagically as well
+
+define prometheus::jmx_exporter_config(
+    $dest,
+    $class,
+    $site,
+    $labels = {},
+) {
+    validate_string($dest)
+    validate_string($site)
+    validate_hash($labels)
+
+    $resources = query_resources(
+                  "Class[\"$class\"]",
+                  'Define["prometheus::jmx_exporter_instance"]',
+                  true)
+    $site_clusters = get_clusters({'site' => $site})
+
+    file { $dest:
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('prometheus/jmx_exporter_config.erb'),
+    }
+}
---
+++ modules/role/manifests/prometheus/services.pp
@@ -0,0 +1,56 @@
+# Uses the prometheus module and generates the specific configuration
+# needed for WMF production
+#
+# filtertags: labs-project-monitoring
+class role::prometheus::services {
+    include ::standard
+    include ::base::firewall
+
+    $targets_path = '/srv/prometheus/services/targets'
+    $storage_retention = hiera('prometheus::server::storage_retention', '2190h0m0s')
+    $max_chunks_to_persist = hiera('prometheus::server::max_chunks_to_persist', '524288')
+    $memory_chunks = hiera('prometheus::server::memory_chunks', '1048576')
+
+    $config_extra = {
+        # All metrics will get an additional 'site' label when queried by
+        # external systems (e.g. via federation)
+        'external_labels' => {
+            'site' => $::site,
+        },
+    }
+
+    $jmx_exporter_jobs = [
+      {
+        'job_name'        => 'cassandra',
+        'scheme'          => 'http',
+        'file_sd_configs' => [
+          { 'files' => [ "${targets_path}/cassandra_*.yaml" ]}
+        ],
+      },
+    ]
+
+    prometheus::jmx_exporter_config{ "jmx_exporter_${::site}":
+        dest       => "${targets_path}/cassandra_restbase_dev_${::site}.yaml",
+        class      => 'role::restbase::dev_cluster',
+        site       => $::site,
+    }
+
+    prometheus::server { 'services':
+        storage_encoding      => '2',
+        listen_address        => '127.0.0.1:9903',
+        storage_retention     => $storage_retention,
+        max_chunks_to_persist => $max_chunks_to_persist,
+        memory_chunks         => $memory_chunks,
+        scrape_configs_extra  => $jmx_exporter_jobs,
+        global_config_extra   => $config_extra,
+    }
+
+    prometheus::web { 'services':
+        proxy_pass => 'http://localhost:9903/services',
+    }
+
+    prometheus::rule { 'rules_services.conf':
+        instance => 'services',
+        source   => 'puppet:///modules/role/prometheus/rules_services.conf',
+    }
+}
