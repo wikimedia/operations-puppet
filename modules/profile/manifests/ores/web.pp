dc98c98dda2d511c472c68f4c2b707189e8f2aad
Amir Sarabadani <ladsgroup@gmail.com>
2017-08-19 16:08:26 +0400

    Add hieradata for ores::celery::workers with default.
    Bug: T169246
    Change-Id: I100c36bea8c1083d0116e81e05c35525c7e86e96

--- modules/profile/manifests/ores/web.pp
+++ modules/profile/manifests/ores/web.pp
@@ -1,10 +1,12 @@
 class profile::ores::web(
     $redis_host = hiera('profile::ores::web::redis_host'),
     $redis_password = hiera('profile::ores::web::redis_password'),
+    $celery_workers = hiera('profile::ores::celery::workers', 45),
 ){
     class { '::ores::web':
         redis_password => $redis_password,
         redis_host     => $redis_host,
+        celery_workers => $celery_workers,
     }
 
     ferm::service { 'ores':
