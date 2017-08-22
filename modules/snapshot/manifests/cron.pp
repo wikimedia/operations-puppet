b5fbbf1da7129f43da9987365734053a51d2137e
Ariel T. Glenn <ariel@wikimedia.org>
2017-08-22 15:34:52 +0300

    central auth global blocks tabke dump fixes
    use aawiki everywhere; fix table name everywhere; make db user and
    pass retrieval functions a bit nicer
    
    Change-Id: I7013f7a5933479cf21b0fa6f2db90263f9ebae5f

--- modules/snapshot/manifests/cron.pp
+++ modules/snapshot/manifests/cron.pp
@@ -5,7 +5,7 @@ class snapshot::cron(
     class { '::snapshot::cron::pagetitles': user   => $user }
     class { '::snapshot::cron::cirrussearch': user   => $user }
     class { '::snapshot::cron::dumplists': user   => $user }
-    class { '::snapshot::cron::dump_global_block': user   => $user }
+    class { '::snapshot::cron::dump_global_blocks': user   => $user }
     class { '::snapshot::cron::wikidatadumps::json': user   => $user }
     class { '::snapshot::cron::wikidatadumps::rdf': user   => $user }
     class { '::snapshot::cron::contentxlation': user   => $user }
--- modules/snapshot/manifests/cron/dump_global_blocks.pp
+++ modules/snapshot/manifests/cron/dump_global_blocks.pp
@@ -1,31 +1,31 @@
-class snapshot::cron::dump_global_block(
+class snapshot::cron::dump_global_blocks(
     $user   = undef,
 ) {
     include ::snapshot::dumps::dirs
     $confsdir = $snapshot::dumps::dirs::confsdir
     $otherdir = "${snapshot::dumps::dirs::datadir}/public/other"
-    $globalblockdir = "${otherdir}/globalblock"
+    $globalblocksdir = "${otherdir}/globalblocks"
 
-    file { '/usr/local/bin/dump-global-block.sh':
+    file { '/usr/local/bin/dump-global-blocks.sh':
         mode   => '0755',
         owner  => 'root',
         group  => 'root',
-        source => 'puppet:///modules/snapshot/cron/dump-global-block.sh',
+        source => 'puppet:///modules/snapshot/cron/dump-global-blocks.sh',
     }
 
-    cron { 'global_block_cleanup':
+    cron { 'global_blocks_cleanup':
         ensure      => 'present',
         environment => 'MAILTO=ops-dumps@wikimedia.org',
         user        => $user,
-        command     => "find ${globalblockdir}/ -maxdepth 1 -type d -mtime +90 -exec rm -rf {} \\;",
+        command     => "find ${globalblocksdir}/ -maxdepth 1 -type d -mtime +90 -exec rm -rf {} \\;",
         minute      => '0',
         hour        => '8',
         weekday     => '6',
     }
 
-    cron { 'global_block_dump':
+    cron { 'global_blocks_dump':
         ensure      => 'present',
-        command     => "/usr/local/bin/dump-global-block.sh --config ${confsdir}/wikidump.conf",
+        command     => "/usr/local/bin/dump-global-blocks.sh --config ${confsdir}/wikidump.conf",
         environment => 'MAILTO=ops-dumps@wikimedia.org',
         user        => $user,
         minute      => '15',
