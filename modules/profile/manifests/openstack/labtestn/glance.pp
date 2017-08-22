c724be78434ebd92ed85c8a6aedc0a073f184d23
cpettet <rush@wikimedia.org>
2017-08-22 20:30:47 +0000

    openstack: glance as module/profile/role for deployments
    Bug: T171494
    Change-Id: Ie250ee4889cd144293c4339b42f01eb16f933ed6

---
+++ modules/profile/manifests/openstack/labtestn/glance.pp
@@ -0,0 +1,19 @@
+class profile::openstack::labtestn::glance(
+    $version = hiera('profile::openstack::labtestn::version'),
+    $nova_controller = hiera('profile::openstack::labtestn::nova_controller'),
+    $nova_controller_standby = hiera('profile::openstack::labtestn::nova_controller_standby'),
+    $db_pass = hiera('profile::openstack::labtestn::glance::db_pass'),
+    $db_host = hiera('profile::openstack::labtestn::glance::db_host'),
+    $ldap_user_pass = hiera('profile::openstack::labtestn::ldap_user_pass'),
+    ) {
+
+    require ::profile::openstack::labtestn::cloudrepo
+    class {'profile::openstack::base::glance':
+        version                 => $version,
+        nova_controller         => $nova_controller,
+        nova_controller_standby => $nova_controller_standby,
+        db_pass                 => $db_pass,
+        db_host                 => $db_host,
+        ldap_user_pass          => $ldap_user_pass,
+    }
+}
---
+++ modules/profile/manifests/openstack/main/glance.pp
@@ -0,0 +1,31 @@
+class profile::openstack::main::glance(
+    $version = hiera('profile::openstack::main::version'),
+    $nova_controller = hiera('profile::openstack::main::nova_controller'),
+    $nova_controller_standby = hiera('profile::openstack::main::nova_controller_standby'),
+    $db_pass = hiera('profile::openstack::main::glance::db_pass'),
+    $db_host = hiera('profile::openstack::main::glance::db_host'),
+    $ldap_user_pass = hiera('profile::openstack::main::ldap_user_pass'),
+    $glance_image_dir = hiera('profile::openstack::base::glance::image_dir'),
+    ) {
+
+    require ::profile::openstack::main::cloudrepo
+    class {'profile::openstack::base::glance':
+        version                 => $version,
+        nova_controller         => $nova_controller,
+        nova_controller_standby => $nova_controller_standby,
+        db_pass                 => $db_pass,
+        db_host                 => $db_host,
+        ldap_user_pass          => $ldap_user_pass,
+    }
+
+    class {'openstack2::glance::image_sync':
+        active                  => ($::fqdn == $nova_controller),
+        version                 => $version,
+        glance_image_dir        => $glance_image_dir,
+        nova_controller_standby => $nova_controller_standby,
+    }
+
+    class {'openstack2::glance::monitor':
+        active => ($::fqdn == $nova_controller),
+    }
+}
