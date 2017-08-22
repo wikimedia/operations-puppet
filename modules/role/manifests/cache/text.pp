c9389f1ba8d6d64c839ea94569b11806edffda58
Brandon Black <bblack@wikimedia.org>
2017-08-22 14:25:08 +0000

    Varnish: move errorpage/browsersec to common code
    Bug: T163251
    Change-Id: I6729b3678f6ed63b40cd3ca8e5bfc69508114199

--- modules/role/manifests/cache/text.pp
+++ modules/role/manifests/cache/text.pp
@@ -56,17 +56,6 @@ class role::cache::text(
 
     $text_storage_args = $::role::cache::base::file_storage_args
 
-    $errorpage = {
-        title       => 'Browser Connection Security Issues',
-        pagetitle   => "Your Browser's Connection Security is Outdated",
-        logo_link   => 'https://www.wikimedia.org',
-        logo_src    => 'https://www.wikimedia.org/static/images/wmf.png',
-        logo_srcset => 'https://www.wikimedia.org/static/images/wmf-2x.png 2x',
-        logo_alt    => 'Wikimedia',
-        content     => template('varnish/browsersec.body.html.erb'),
-    }
-    $error_browsersec_html = template('mediawiki/errorpage.html.erb')
-
     role::cache::instances { 'text':
         fe_jemalloc_conf  => 'lg_dirty_mult:8,lg_chunk:16',
         fe_runtime_params => $common_runtime_params,
@@ -75,7 +64,7 @@ class role::cache::text(
         app_def_be_opts   => hiera('cache::app_def_be_opts'),
         fe_vcl_config     => $fe_vcl_config,
         be_vcl_config     => $be_vcl_config,
-        fe_extra_vcl      => ['text-common', 'zero', 'normalize_path', 'geoip', 'browsersec'],
+        fe_extra_vcl      => ['text-common', 'zero', 'normalize_path', 'geoip'],
         be_extra_vcl      => ['text-common', 'normalize_path'],
         be_storage        => $text_storage_args,
         fe_cache_be_opts  => $fe_cache_be_opts,
---
+++ modules/varnish/manifests/common/browsersec.pp
@@ -0,0 +1,19 @@
+class varnish::common::browsersec {
+    $errorpage = {
+        title       => 'Browser Connection Security Issues',
+        pagetitle   => "Your Browser's Connection Security is Outdated",
+        logo_link   => 'https://www.wikimedia.org',
+        logo_src    => 'https://www.wikimedia.org/static/images/wmf.png',
+        logo_srcset => 'https://www.wikimedia.org/static/images/wmf-2x.png 2x',
+        logo_alt    => 'Wikimedia',
+        content     => template('varnish/browsersec.body.html.erb'),
+    }
+    $error_browsersec_html = template('mediawiki/errorpage.html.erb')
+
+    file { '/etc/varnish/browsersec.inc.vcl':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('varnish/browsersec.inc.vcl.erb'),
+    }
+}
