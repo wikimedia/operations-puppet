a03ffa2ad15d46e35b73d84d97ed936c021efa1d
Ariel T. Glenn <ariel@wikimedia.org>
2017-08-22 20:56:47 +0300

    start of setup of dumpsdata hosts
    These hosts provide a scratch area for dumps generation,
    via nfs mounts to the dumps snapshot hosts. They also will rsync
    this data over to the dataset server hosts and later to the
    replacements for the datasets.
    
    This changeset is the first step only: nfs service.
    
    Note that eventually the dataset server manifests will be converted
    to reuse dumpsnfs and other modules yet to be added.
    
    Bug: T169849
    Change-Id: I2cc59df6713ba8b479f73c7c0310c5604ea21381

---
+++ modules/profile/manifests/dumpsdata.pp
@@ -0,0 +1,62 @@
+class profile::dumpsdata(
+    $clients = hiera('dumps_clients_snapshots'),
+) {
+    $mountd_port     = '32767'
+    $statd_port      = '32765'
+    $statd_out       = '32766'
+    $portmapper_port = '111'
+    $lockd_udp       = '32768'
+    $lockd_tcp       = '32769'
+
+    class { '::dumpsnfs':
+        clients     => $clients,
+        statd_port  => $statd_port,
+        statd_out   => $statd_out,
+        lockd_udp   => $lockd_udp,
+        lockd_tcp   => $lockd_tcp,
+        mountd_port => $mountd_port,
+    }
+
+    class { '::base::firewall': }
+
+    ferm::service { 'nfs_rpc_mountd':
+        proto  => 'tcp',
+        port   => $mountd_port,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    ferm::service { 'nfs_rpc_statd':
+        proto  => 'tcp',
+        port   => $statd_port,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    ferm::service { 'nfs_portmapper_udp':
+        proto  => 'udp',
+        port   => $portmapper_port,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    ferm::service { 'nfs_portmapper_tcp':
+        proto  => 'tcp',
+        port   => $portmapper_port,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    ferm::service { 'nfs_lockd_udp':
+        proto  => 'udp',
+        port   => $lockd_udp,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    ferm::service { 'nfs_lockd_tcp':
+        proto  => 'tcp',
+        port   => $lockd_tcp,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    monitoring::service { 'nfs':
+        description   => 'NFS',
+        check_command => 'check_tcp!2049',
+    }
+}
