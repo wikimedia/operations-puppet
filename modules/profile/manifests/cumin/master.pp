2ad1b48627525809ff26b316b182780b2823aab4
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-08-24 16:23:26 +0200

    Cumin: update configuration file for v1.0.0
    * In version 1.0.0 of Cumin the 'backend' configuration property was
      removed and replaced with 'default_backend' one, to be used to parse
      with this backend each query first and fallback to the general grammar
      only on failure. Setting it to 'puppetdb' to make the transition
      transparent for the users.
    * Add aliases.yaml file with the first most obvious aliases. It will be
      improved later, also based on the needs for debdeploy.
    
    Change-Id: Ie3a975f1b45b4b6dd1dd1d4d623f396b2bc8f9c7

--- modules/profile/manifests/cumin/master.pp
+++ modules/profile/manifests/cumin/master.pp
@@ -1,6 +1,7 @@
 class profile::cumin::master (
     $puppetdb_host  = hiera('puppetmaster::puppetdb::master'),
     $cumin_log_path = '/var/log/cumin',
+    $datacenters    = hiera('datacenters'),
 ) {
     ::keyholder::agent { 'cumin_master':
         trusted_groups => ['root'],
@@ -28,6 +29,15 @@ class profile::cumin::master (
         group   => 'root',
         mode    => '0640',
         content => template('profile/cumin/config.yaml.erb'),
+        require => File['/etc/cumin'],
+    }
+
+    file { '/etc/cumin/aliases.yaml':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        content => template('profile/cumin/aliases.yaml.erb'),
         require => File['/etc/cumin'],
     }
 }
