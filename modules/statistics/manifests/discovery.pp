1164532b55479f821f6e35a669d896bd1d8f7211
Ottomata <aotto@wikimedia.org>
2017-08-24 17:00:13 -0400

    Include discovery-stats user in analytics_cluster::users
    This will allow discovery-stats to use Hadoop.
    
    Change-Id: Iba7f79f11240af8781a8c4c9b0aeab2dae0d6373

--- modules/statistics/manifests/discovery.pp
+++ modules/statistics/manifests/discovery.pp
@@ -1,35 +1,29 @@
 # = Class: statistics::discovery
+#
+# NOTE:  This class includes the statistics::discovery::user class, which ensures that
+# the discovery-stats user is in the analytics-privatedata-users group.  This means
+# that you CANNOT include statistics::discovery somewhere that does not have the
+# analytics-privatedata-users present.
+#
 class statistics::discovery {
     Class['::statistics'] -> Class['::statistics::discovery']
 
     include ::passwords::mysql::research
+    require ::statistics::discovery::user
 
-    $working_path = $::statistics::working_path
-    # Homedir for everything Wikimedia Discovery Analytics related
-    $dir = "${working_path}/discovery"
     # Path in which daily runs will log to
     $log_dir = "${dir}/log"
     # Path in which the R library will reside
     $rlib_dir = "${dir}/r-library"
 
-    $user = 'discovery-stats'
-    # Setting group to 'analytics-privatedata-users' so that Discovery's Analysts
-    # (as members of analytics-privatedata-users) have some privileges, and so
-    # the discovery-stats user can access private data in Hive.
-    $group ='analytics-privatedata-users'
 
-    user { $user:
-        ensure     => present,
-        home       => $dir,
-        shell      => '/bin/bash',
-        managehome => false,
-        system     => true,
-        gid        => $group,
-    }
+    $user = $::statistics::discovery::user::user
+    $group = $::statistics::discovery::user::group
+    $dir = $::statistics::discovery::user::dir
 
     # This file will render at
     # /etc/mysql/conf.d/discovery-stats-client.cnf.
-    ::mysql::config::client { 'discovery-stats':
+    ::mysql::config::client { $user:
         user  => $::passwords::mysql::research::user,
         pass  => $::passwords::mysql::research::pass,
         group => $group,
@@ -37,7 +31,6 @@ class statistics::discovery {
     }
 
     $directories = [
-        $dir,
         $log_dir,
         $rlib_dir
     ]
@@ -46,7 +39,9 @@ class statistics::discovery {
         ensure => 'directory',
         owner  => $user,
         group  => $group,
-        mode   => '0775', # so Discovery's Analysts (as members of wikidev group) can read, write, execute
+        # so Discovery's Analysts (as members of analytics-privatedata-users group) can read,
+        # write, execute
+        mode   => '0775',
     }
 
     git::clone { 'wikimedia/discovery/golden':
@@ -76,7 +71,7 @@ class statistics::discovery {
         require => [
             Class['::statistics::compute'],
             Git::Clone['wikimedia/discovery/golden'],
-            Mysql::Config::Client['discovery-stats']
+            Mysql::Config::Client[$user]
         ],
         user    => $user,
     }
