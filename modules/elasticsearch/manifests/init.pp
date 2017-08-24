bc1ec38777acee9ea4f5beeeeae14a1d43e97b3a
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-08-24 11:31:46 +0200

    elasticsearch - switch to using logrotate::rule
    Change-Id: I71e9602b84a6826b3899893a9f0616606c974574

--- modules/elasticsearch/manifests/init.pp
+++ modules/elasticsearch/manifests/init.pp
@@ -255,9 +255,14 @@ class elasticsearch(
         require => Package['elasticsearch'],
     }
 
-    logrotate::conf { 'elasticsearch':
-        ensure => present,
-        source => 'puppet:///modules/elasticsearch/logrotate',
+    logrotate::rule { 'elasticsearch':
+        ensure        => present,
+        frequency     => 'daily',
+        copy_truncate => true,
+        missing_ok    => true,
+        not_if_empty  => true,
+        rotate        => 7,
+        compress      => true,
     }
 
     file { $data_dir:
