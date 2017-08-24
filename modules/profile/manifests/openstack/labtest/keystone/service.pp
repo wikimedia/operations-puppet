1c2fc054f83241f91ac91baf3d5d4f1ef00fd711
cpettet <rush@wikimedia.org>
2017-08-24 17:24:30 +0000

    openstack: misc components to module/role/profile
    * spreadcheck to openstack2/monitor
    * env scripts to openstack2/util
    * use admin_scripts instead of virtscripts dir for
      consistency
    * remove file path specific headers for scripts
      as they are too brittle
    * make file resources consistent for ordering of params
    * keystone-public-uwsgi file cleanup
    
    Bug: T171494
    Change-Id: Ibc8e47e4ab670c242014807588a7bb871ee134e0

--- modules/profile/manifests/openstack/labtest/keystone/service.pp
+++ modules/profile/manifests/openstack/labtest/keystone/service.pp
@@ -5,6 +5,7 @@ class profile::openstack::labtest::keystone::service(
     $db_host = hiera('profile::openstack::labtest::keystone::db_host'),
     $token_driver = hiera('profile::openstack::labtest::keystone::token_driver'),
     $db_pass = hiera('profile::openstack::labtest::keystone::db_pass'),
+    $nova_db_pass = hiera('profile::openstack::labtest::nova::db_pass'),
     $ldap_hosts = hiera('profile::openstack::labtest::ldap_hosts'),
     $ldap_user_pass = hiera('profile::openstack::labtest::ldap_user_pass'),
     $wiki_status_consumer_token = hiera('profile::openstack::labtest::keystone::wiki_status_consumer_token'),
@@ -15,6 +16,9 @@ class profile::openstack::labtest::keystone::service(
     $wiki_consumer_secret = hiera('profile::openstack::labtest::keystone::wiki_consumer_secret'),
     $wiki_access_token = hiera('profile::openstack::labtest::keystone::wiki_access_token'),
     $wiki_access_secret = hiera('profile::openstack::labtest::keystone::wiki_access_secret'),
+    $wmflabsdotorg_admin = hiera('profile::openstack::base::designate::wmflabsdotorg_admin'),
+    $wmflabsdotorg_pass = hiera('profile::openstack::base::designate::wmflabsdotorg_pass'),
+    $wmflabsdotorg_project = hiera('profile::openstack::base::designate::wmflabsdotorg_project'),
     ) {
 
     package {'mysql-server':
@@ -29,6 +33,7 @@ class profile::openstack::labtest::keystone::service(
         db_host                     => $db_host,
         token_driver                => $token_driver,
         db_pass                     => $db_pass,
+        nova_db_pass                => $nova_db_pass,
         ldap_hosts                  => $ldap_hosts,
         ldap_user_pass              => $ldap_user_pass,
         wiki_status_consumer_token  => $wiki_status_consumer_token,
@@ -39,6 +44,9 @@ class profile::openstack::labtest::keystone::service(
         wiki_consumer_secret        => $wiki_consumer_secret,
         wiki_access_token           => $wiki_access_token,
         wiki_access_secret          => $wiki_access_secret,
+        wmflabsdotorg_admin         => $wmflabsdotorg_admin,
+        wmflabsdotorg_pass          => $wmflabsdotorg_pass,
+        wmflabsdotorg_project       => $wmflabsdotorg_project,
         require                     => Package['mysql-server'],
     }
 
