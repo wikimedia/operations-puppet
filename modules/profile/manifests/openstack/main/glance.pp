d9bf56aa42b98a5f7020946d92350ee4d82779a6
cpettet <rush@wikimedia.org>
2017-08-25 22:27:24 +0000

    openstack: nova components for module/profile/role
    Bug: T171494
    Change-Id: I90e2268d21d163ca3608f5a81243ad2ad0c9aaab

--- modules/profile/manifests/openstack/main/glance.pp
+++ modules/profile/manifests/openstack/main/glance.pp
@@ -6,6 +6,7 @@ class profile::openstack::main::glance(
     $db_host = hiera('profile::openstack::main::glance::db_host'),
     $ldap_user_pass = hiera('profile::openstack::main::ldap_user_pass'),
     $glance_image_dir = hiera('profile::openstack::base::glance::image_dir'),
+    $labs_hosts_range = hiera('profile::openstack::main::labs_hosts_range'),
     ) {
 
     require ::profile::openstack::main::cloudrepo
@@ -16,6 +17,7 @@ class profile::openstack::main::glance(
         db_pass                 => $db_pass,
         db_host                 => $db_host,
         ldap_user_pass          => $ldap_user_pass,
+        labs_hosts_range        => $labs_hosts_range,
     }
 
     class {'openstack2::glance::image_sync':
