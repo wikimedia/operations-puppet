2dec581cd13f02737a68c9fd6396dac3351636f7
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-08-28 17:50:11 +0200

    wmf-auto-reimage refactoring
    * Allow to specify a conftool state: T149230
    * Add step to reset puppet.service: T169555
    * Removed dependency on the old wmf-reimage script on the puppetmaster
      host: T148817
    * Remove all Salt dependencies and replaced them with Cumin or direct
      calls to ipmitool: T166300
    
    The work to migrate from Salt to Cumin and remove the wmf-reimage
    dependency required a major refactor in order to support multiple hosts
    in a reliable and independent way.
    In addition, it required to move it from the 'salt' module in the Puppet
    tree to the 'cumin' module and split it into a common library and two
    different scripts:
    * wmf-auto-reimage-host: takes care of reimaging a single host
    * wmf-auto-reimage: a wrapper around wmf-auto-reimage-host with
      subprocess to reimage a list of hosts either in parallel or in a
      sequence.
    
    Because of the above it was not possible to have a clean diff anyway, so
    I decided to add in the same CR the minor additional features that were
    pending.
    The scripts have been tested with production spare hosts and some new
    hosts.
    
    Bug: T148814
    Change-Id: If1e0aafcfbfa1ff429a8947bb409e4abee895559

---
+++ modules/profile/files/cumin/wmf_auto_reimage.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+"""Automated reimaging of a list of hosts."""
+
+import logging
+import os
+import socket
+import subprocess
+import sys
+import time
+
+from collections import defaultdict
+from datetime import datetime
+
+import wmf_auto_reimage_lib as lib
+
+
+LOG_PATTERN = '/var/log/wmf-auto-reimage/{start}_{user}_{pid}.log'
+
+logger = logging.getLogger('wmf-auto-reimage')
+
+
+def parse_args():
+    """Parse and return command line arguments, validate the hosts."""
+    parser = lib.get_base_parser('Automated reimaging of a list of hosts')
+    parser.add_argument(
+        '--sequential', action='store_true',
+        help=('run one reimage at a time, sequentially. By default the reimage for all the hosts '
+              'are run in parallel.'))
+    parser.add_argument(
+        '--sleep', action='store', type=int, default=0,
+        help=('amount of seconds to sleep between one reimage and the next when --sequential '
+              'is set. Has no effect if --sequential is not set. [default: 0]'))
+    parser.add_argument(
+        'hosts', metavar='HOST', nargs='+', action='store',
+        help='FQDN of the host(s) to be reimaged')
+
+    args = parser.parse_args()
+
+    # Perform a quick sanity check on the hosts
+    for host in args.hosts:
+        if '.' not in host or not lib.HOSTS_PATTERN.match(host):
+            raise ValueError("Expected FQDN of hosts, got '{host}'".format(
+                host=host))
+
+        if not lib.is_hostname_valid(host):
+            raise ValueError(
+                "Unable to resolve host '{host}'".format(host=host))
+
+    # Ensure there are no duplicates in the hosts list
+    duplicates = {host for host in args.hosts if args.hosts.count(host) > 1}
+    if len(duplicates) > 0:
+        raise ValueError("Duplicate hosts detected: {dup}".format(
+            dup=duplicates))
+
+    # Ensure Phab task is properly formatted
+    if (args.phab_task_id is not None and
+            lib.PHAB_TASK_PATTERN.search(args.phab_task_id) is None):
+        raise ValueError(("Invalid Phabricator task ID '{task}', expected in "
+                          "the form T12345").format(task=args.phab_task_id))
+
+    return args
+
+
+def setup_logging(user):
+    """Set up the logger instance and return the log file path.
+
+    Arguments:
+    user -- the real user to use in the logging formatter for auditing
+    """
+    log_path = LOG_PATTERN.format(
+        start=datetime.now().strftime('%Y%m%d%H%M'), user=user, pid=os.getpid())
+    lib.setup_logging(logger, user, log_path)
+
+    return log_path
+
+
+def reimage_host(host, mgmt, args):
+    """Run the reimage script for a single host in a subprocess.
+
+    Arguments:
+    host -- the FQDN of the host to be reimaged
+    mgmt -- the FQDN of the management interface of the host
+    args -- the parsed arguments to pass over to the reimage script
+    """
+    command = lib.get_reimage_host_command(host, mgmt, args)
+    return subprocess.Popen(command)
+
+
+def wait_for_childrens(procs):
+    """Wait for all the childrens.
+
+    Arguments:
+    procs -- a dictionary with host: subprocess with the subprocesses to wait for
+    """
+    retcodes = defaultdict(list)
+    while True:
+        if not procs:
+            break
+
+        for host, proc in procs.items():
+            ret = proc.poll()
+            if ret is None:
+                continue
+            else:
+                del procs[host]
+                retcodes[ret].append(host)
+
+        time.sleep(5)
+
+    return retcodes
+
+
+def run(args, user, log_path):
+    """Run the reimage for all the hosts in subproceesses."""
+    # Setup
+    phab_client = lib.get_phabricator_client()
+    lib.ensure_ipmi_password()
+    mgmts = lib.get_mgmts(args.hosts)
+
+    # Check that IPMI is working for all the hosts
+    for host in args.hosts:
+        status = lib.ipmitool_command(mgmts[host], ['chassis', 'power', 'status'])
+        if not status.startswith('Chassis Power is'):
+            raise RuntimeError(
+                "Unable to verify remote IPMI for mgmt '{mgmt}': {status}".format(
+                    mgmt=mgmts[host], status=status))
+
+    # Initialize data structures
+    procs = {}
+    retcodes = defaultdict(list)
+
+    # Validate hosts
+    if not args.new:
+        lib.validate_hosts(args.hosts, args.no_verify)
+
+    # Update the Phabricator task
+    if args.phab_task_id is not None:
+        lib.phabricator_task_update(
+            phab_client, args.phab_task_id, lib.PHAB_COMMENT_PRE.format(
+                user=user, hostname=socket.getfqdn(), hosts=args.hosts, log=log_path))
+
+    # Run the reimage for each host in a child process
+    try:
+        for host in args.hosts:
+            proc = reimage_host(host, mgmts[host], args)
+            if args.sequential:
+                retcodes[host] = proc.wait()
+                time.sleep(args.sleep)
+            else:
+                procs[host] = proc
+
+        if procs:
+            retcodes = wait_for_childrens(procs)
+    except KeyboardInterrupt:
+        # Terminate childrens
+        if procs:
+            for process in procs:
+                process.terminate()
+        else:
+            proc.terminate()
+
+        raise
+
+    # Comment on the Phabricator task
+    if args.phab_task_id is not None:
+        phabricator_message = lib.get_phabricator_post_message(retcodes)
+        lib.phabricator_task_update(phab_client, args.phab_task_id, phabricator_message)
+
+    if max(retcodes.keys()) > 0:
+        return 1
+
+    return 0
+
+
+def main():
+    """Automated reimaging of a list of hosts."""
+    # Setup
+    args = parse_args()
+    lib.ensure_shell_mode()
+    user = lib.get_running_user()
+    log_path = setup_logging(user)
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+    logger.info('wmf_auto_reimage called with args: {args}'.format(args=args))
+    lib.print_line('START. To monitor the full log:')
+    lib.print_line('sudo tail -F {log}'.format(log=log_path), skip_time=True)
+
+    try:
+        retcode = run(args, user, log_path)
+    except BaseException as e:
+        message = 'Unable to run wmf_auto_reimage'
+        lib.print_line('{message}: {error}'.format(message=message, error=e))
+        logger.exception(message)
+        retcode = 2
+    finally:
+        lib.print_line('END')
+
+    return retcode
+
+
+if __name__ == '__main__':
+    sys.exit(main())
--- modules/profile/manifests/cumin/master.pp
+++ modules/profile/manifests/cumin/master.pp
@@ -3,11 +3,17 @@ class profile::cumin::master (
     $cumin_log_path = '/var/log/cumin',
     $datacenters    = hiera('datacenters'),
 ) {
+    include passwords::phabricator
+
     ::keyholder::agent { 'cumin_master':
         trusted_groups => ['root'],
     }
 
-    require_package('cumin')
+    require_package([
+        'cumin',
+        'python-dnspython',
+        'python-phabricator',
+    ])
 
     file { $cumin_log_path:
         ensure => directory,
@@ -32,6 +38,15 @@ class profile::cumin::master (
         require => File['/etc/cumin'],
     }
 
+    file { '/etc/cumin/config-installer.yaml':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        content => template('profile/cumin/config-installer.yaml.erb'),
+        require => File['/etc/cumin'],
+    }
+
     file { '/etc/cumin/aliases.yaml':
         ensure  => present,
         owner   => 'root',
@@ -39,5 +54,48 @@ class profile::cumin::master (
         mode    => '0640',
         content => template('profile/cumin/aliases.yaml.erb'),
         require => File['/etc/cumin'],
+    }
+
+    # Auto reimage script
+    # Temporarily in Puppet, once the spinoff from Switchdc will be in production
+    # This will just become an available task in it
+
+    file { '/var/log/wmf-auto-reimage':
+        ensure => directory,
+        mode   => '0750',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    file { '/usr/local/lib/python2.7/dist-packages/wmf_auto_reimage_lib.py':
+        ensure => present,
+        source => 'puppet:///modules/profile/cumin/wmf_auto_reimage_lib.py',
+        mode   => '0644',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    # TODO: remove the -new suffix once the salt version has beed decommissioned
+    file { '/usr/local/sbin/wmf-auto-reimage-new':
+        ensure => present,
+        source => 'puppet:///modules/profile/cumin/wmf_auto_reimage.py',
+        mode   => '0544',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    file { '/usr/local/sbin/wmf-auto-reimage-host':
+        ensure => present,
+        source => 'puppet:///modules/profile/cumin/wmf_auto_reimage_host.py',
+        mode   => '0544',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    class { '::phabricator::bot':
+        username => 'ops-monitoring-bot',
+        token    => $passwords::phabricator::ops_monitoring_bot_token,
+        owner    => 'root',
+        group    => 'root',
     }
 }
