e5289340fbde968c1f35f36a82d08e958a315b80
Ariel T. Glenn <ariel@wikimedia.org>
2017-08-28 13:13:39 +0300

    add user and directory setup to dumpsdata hosts
    Putting them into separate modules means the relevant pieces
    such as the user can be re-used for the snapshot hosts.
    That's what we get when dealing with nfs-mounted filesystems.
    Sadness.
    
    Bug: T169849
    Change-Id: I7fc8fb00724909d656bb05a3100bee164f84ab1b

---
+++ modules/dumpsdirs/manifests/init.pp
@@ -0,0 +1,145 @@
+class dumpsdirs(
+    $user  = undef,
+    $group = undef,
+) {
+    # Directories where dumps of any type are generated
+    # This list is not for one-off directories, nor for
+    # directories with incoming rsyncs of datasets
+    $datadir                      = '/data/xmldatadumps'
+    $publicdir                    = '/data/xmldatadumps/public'
+    $otherdir                     = "${publicdir}/other"
+    $cirrussearchdir              = "${otherdir}/cirrussearch"
+    $xlationdir                   = "${otherdir}/contenttranslation"
+    $globalblocksdir              = "${otherdir}/globalblocks"
+    $medialistsdir                = "${otherdir}/imageinfo"
+    $incrsdir                     = "${otherdir}/incrs"
+    $mediatitlesdir               = "${otherdir}/mediatitles"
+    $othermiscdir                 = "${otherdir}/misc"
+    $pagetitlesdir                = "${otherdir}/pagetitles"
+    $othertestfilesdir            = "${otherdir}/testfiles"
+    $otherwikibasedir             = "${otherdir}/wikibase/"
+    $otherwikidatalegacydir       = "${otherdir}/wikidata"
+    $otherwikidatawikirelativedir  = 'other/wikibase/wikidatawiki'
+
+    file { $datadir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    file { $publicdir:
+        ensure => 'directory',
+        mode   => '0775',
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { $otherdir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { $cirrussearchdir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { $xlationdir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { $globalblocksdir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { $medialistsdir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { $incrsdir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { $mediatitlesdir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { $othermiscdir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { $pagetitlesdir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { $othertestfilesdir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    ########################
+    #
+    # wikidata-related items
+    #
+    ########################
+
+    file { $otherwikibasedir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    # Legacy
+    file { $otherwikidatalegacydir:
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    file { "${publicdir}/${otherwikidatawikirelativedir}":
+        ensure => 'directory',
+        mode   => '0755',
+        owner  => $user,
+        group  => $group,
+    }
+
+    # T72385
+    # needs to be relative so that it can be referenced on dumpsdata hosts
+    # via the path $publicdir/... and on the snapshot hosts via
+    # via NFS mount path /mnt$publicdir/...
+    file { "${publicdir}/wikidatawiki/entities":
+        ensure => 'link',
+        target => "../${otherwikidatawikirelativedir}",
+    }
+
+}
