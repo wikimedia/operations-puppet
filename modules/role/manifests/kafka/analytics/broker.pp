17335734c3242c6cce5bc0c22c10d714702c250b
Ottomata <aotto@wikimedia.org>
2017-08-29 15:17:14 -0400

    Synchronize message.max.bytes between all kafka clusters and producers
    This is causing mirror maker from main-eqiad -> analytics to fail,
    which is keeping EventStreams topics from being replicated!
    
    Change-Id: I3e5f125f362d350194a07e02147e3276ba528a2c

--- modules/role/manifests/kafka/analytics/broker.pp
+++ modules/role/manifests/kafka/analytics/broker.pp
@@ -111,6 +111,10 @@ class role::kafka::analytics::broker {
         replica_fetch_max_bytes         => 1048576,
         log_flush_interval_messages     => 10000,
         log_cleanup_policy              => 'delete',
+
+        # FIXME: this needs to be refactored when the role
+        # is moved to profiles.
+        message_max_bytes               => hiera('kafka_message_max_bytes', 1048576),
     }
 
     class { '::confluent::kafka::broker::jmxtrans':
