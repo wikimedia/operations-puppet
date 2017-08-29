4365d28b78428a9c45f42d35de90279147f791ac
Jaime Crespo <jcrespo@wikimedia.org>
2017-08-29 18:07:20 +0200

    mariadb: Implement regular logical backups using mydumper
    Bug: T169516
    Change-Id: I671fb54b35e92fadcc65e622381900443dca8729

---
+++ modules/role/files/mariadb/dump_shards.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+NUMTHREADS=${NUMTHREADS:-16}
+BACKUPDIR=${BACKUPDIR:-"/srv/backups"}
+SHARDS=${SHARDS:-"s2 s3 s5 s6 s7 x1"}
+SERVER=${SERVER:-"localhost"}
+ROWSMAX=${ROWSMAX:-20000000}
+
+mkdir -p "${BACKUPDIR}"
+# Make sure we got a very specific mode in our directory
+chmod 0700 "${BACKUPDIR}"
+
+# TODO: $BACKUPDIR sanitization
+find "${BACKUPDIR}" -mtime +14 -type f -delete
+find "${BACKUPDIR}" -mtime +14 -type d -exec rmdir '{}' ';'
+
+DATE=$(date "+%Y%m%d%H%M%S")
+# Overwrite/define USER since it may not be defined or may not be set correctly
+USER=$(whoami)
+
+# Sanitize IFS
+OLD_IFS=$IFS
+$IFS=' '
+# $USER will always contain the current euid
+for shard in $SHARDS; do
+    /usr/bin/mydumper \
+	--compress \
+	--events \
+	--host="${SERVER}" \
+	--logfile="${BACKUPDIR}/dump.${shard}.log" \
+	--outputdir="${BACKUPDIR}/${shard}.${DATE}" \
+	--routines \
+	--rows="${ROWSMAX}" \
+	--socket="/run/mysqld/mysqld.${shard}.sock" \
+	--threads="${NUMTHREADS}" \
+	--triggers \
+	--user="${USER}"
+done
+# Restore IFS
+IFS=$OLD_IFS
+
+# TODO: s3 packaging
