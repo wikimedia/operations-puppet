87f06eec5776bd1fa8d6d70cfc16b86e7d847e9b
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-08-30 15:12:38 +0300

    kubernetes: Refactor/add admission controllers
    Migrate away from the disjoint structure of an array for the admission
    controllers and separate parameters for their configuration to a more
    tightly coupled structure where admission controllers are specified as a
    Hash with keys the admission controller names and values their
    respective parameters. That allows for greater flexibility and decouples
    the definition of the class from the implementation details of the
    controllers and their parameters.
    
    Amend the roles for production, staging as well as labs to keep the
    status quo.
    
    There are two glaring differences in production/staging, that
    is we no longer enforce the registry. That is expected to be enforced
    later on if possible via the dynamic admission controllers, but even now
    it is not an immediate issue due to the networking policies we have in
    place. The second is that we add the ServiceAccount controller per the
    recommendedation of the kubernetes docs
    
    Bug: T170119
    Change-Id: Ie960a72da62b12beca2408e1b5028ea50ef3d5d5

--- modules/profile/manifests/kubernetes/master.pp
+++ modules/profile/manifests/kubernetes/master.pp
@@ -3,7 +3,6 @@ class profile::kubernetes::master(
     # List of hosts this is accessible to.
     # SPECIAL VALUE: use 'all' to have this port be open to the world
     $accessible_to=hiera('profile::kubernetes::master::accessible_to'),
-    $docker_registry=hiera('profile::kubernetes::master::docker_registry'),
     $service_cluster_ip_range=hiera('profile::kubernetes::master::service_cluster_ip_range'),
     $apiserver_count=hiera('profile::kubernetes::master::apiserver_count'),
     $admission_controllers=hiera('profile::kubernetes::master::admission_controllers'),
@@ -12,8 +11,6 @@ class profile::kubernetes::master(
     $ssl_cert_path=hiera('profile::kubernetes::master::ssl_cert_path'),
     $ssl_key_path=hiera('profile::kubernetes::master::ssl_cert_path'),
     $authz_mode=hiera('profile::kubernetes::master::authz_mode'),
-    $host_automounts=hiera('profile::kubernetes::master::host_automounts'),
-    $host_path_prefixes_allowed=hiera('profile::kubernetes::master::host_path_prefixes_allowed'),
 ){
     if $expose_puppet_certs {
         base::expose_puppet_certs { '/etc/kubernetes':
@@ -34,16 +31,13 @@ class profile::kubernetes::master(
 
     $etcd_servers = join($etcd_urls, ',')
     class { '::k8s::apiserver':
-        etcd_servers               => $etcd_servers,
-        docker_registry            => $docker_registry,
-        ssl_cert_path              => $ssl_cert_path,
-        ssl_key_path               => $ssl_key_path,
-        authz_mode                 => $authz_mode,
-        service_cluster_ip_range   => $service_cluster_ip_range,
-        apiserver_count            => $apiserver_count,
-        admission_controllers      => $admission_controllers,
-        host_path_prefixes_allowed => $host_path_prefixes_allowed,
-        host_automounts            => $host_automounts,
+        etcd_servers             => $etcd_servers,
+        ssl_cert_path            => $ssl_cert_path,
+        ssl_key_path             => $ssl_key_path,
+        authz_mode               => $authz_mode,
+        service_cluster_ip_range => $service_cluster_ip_range,
+        apiserver_count          => $apiserver_count,
+        admission_controllers    => $admission_controllers,
     }
 
     class { '::k8s::scheduler': }
