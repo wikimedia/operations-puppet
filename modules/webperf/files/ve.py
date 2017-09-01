5407fb7d38cf314ed5829367087a24b8e4fff385
Timo Tijhof <krinklemail@gmail.com>
2017-09-01 04:00:21 +0200

    webperf: Refactor ve.py and add unit tests
    Bug: T110903
    Change-Id: I96e81be8d4b1c3dcd9259ae7c4c9ef65b3002b64

--- modules/webperf/files/ve.py
+++ modules/webperf/files/ve.py
@@ -7,37 +7,66 @@ sys.setdefaultencoding("utf-8")
 
 import argparse
 import socket
+import unittest
+import yaml
 
 import eventlogging
 
 
-ap = argparse.ArgumentParser(description='PerfData StatsD module')
-ap.add_argument('endpoint', help='URI of EventLogging endpoint')
-ap.add_argument('--statsd-host', default='localhost',
-                type=socket.gethostbyname)
-ap.add_argument('--statsd-port', default=8125, type=int)
-args = ap.parse_args()
-
-addr = args.statsd_host, args.statsd_port
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-events = eventlogging.connect(args.endpoint)
-
-for meta in events.filter(schema='Edit'):
+def handle_edit(meta):
     event = meta['event']
     if event['editor'] == 'visualeditor':
+        action = event['action']
+        if action == 'saveSuccess':
+            metric = 'save'
+        elif action == 'ready':
+            metric = 'load'
+        else:
+            return
+        timing = int(event['action.%s.timing' % action])
+        # Log values between 0.1s an 100s (1.6min) only
+        if timing < 100 or timing > 100000:
+            return
+        return 'VisualEditor.%s:%s|ms' % (metric, timing)
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(description='PerfData StatsD module')
+    ap.add_argument('endpoint', help='URI of EventLogging endpoint')
+    ap.add_argument('--statsd-host', default='localhost',
+                    type=socket.gethostbyname)
+    ap.add_argument('--statsd-port', default=8125, type=int)
+    args = ap.parse_args()
+
+    addr = args.statsd_host, args.statsd_port
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    events = eventlogging.connect(args.endpoint)
+
+    for meta in events.filter(schema='Edit'):
         try:
-            action = event['action']
-            if action == 'saveSuccess':
-                metric = 'save'
-            elif action == 'ready':
-                metric = 'load'
-            else:
-                continue
-            timing = int(event['action.%s.timing' % action])
-            if timing < 100 or timing > 100000:
-                continue
-            stat = 'VisualEditor.%s:%s|ms' % (metric, timing)
-            sock.sendto(stat.encode('utf-8'), addr)
+            stat = handle_edit(meta)
+            if stat is not None:
+                sock.sendto(stat.encode('utf-8'), addr)
         except (ValueError, KeyError):
             continue
+
+
+# ##### Tests ######
+# To run:
+#   python -m unittest ve
+#
+class TestVePerfData(unittest.TestCase):
+    def test_handler(self):
+        with open('ve_fixture.yaml') as fixture_file:
+            fixture = yaml.safe_load(fixture_file)
+            actual = []
+            for meta in fixture:
+                stat = handle_edit(meta)
+                if stat is not None:
+                    actual.append(stat)
+            with open('ve_expected.txt') as expected_file:
+                self.assertItemsEqual(
+                    actual,
+                    expected_file.read().splitlines()
+                )
