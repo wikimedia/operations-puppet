7c1d146f84e958cb3944fe6d6128a4a5b739e9c7
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-09-04 12:41:42 +0200

    cassandra: reprovision restbase1010 with cassandra 3
    Bug: T169939
    Change-Id: I38ee0aff47f6de8ce769d1e133cec7d3e95fe772

--- manifests/site.pp
+++ manifests/site.pp
@@ -2077,7 +2077,7 @@ node /^restbase10(0[78]|1[012345678])\.eqiad\.wmnet$/ {
     role(restbase::production)
 }
 
-node /^restbase100[9]\.eqiad\.wmnet$/ {
+node /^restbase10(0[9]|10)\.eqiad\.wmnet$/ {
     role(restbase::production_ng)
 }
 
