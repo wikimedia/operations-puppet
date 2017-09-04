becdb3c1aa0ad9411850ed3ef51e066fe437b0ed
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-09-04 16:08:25 +0200

    elasticsearch - deploy plugins with debian package instead of trebuchet
    Bug: T158560
    Depends-On: Ibf1f12c775b0b59b59fd89cd0b09dd35c97c0190
    Change-Id: I69322724369e4574a1d048cc4c75eaffdbc2b260

--- modules/profile/manifests/elasticsearch.pp
+++ modules/profile/manifests/elasticsearch.pp
@@ -48,26 +48,13 @@ class profile::elasticsearch(
         srange  => "@resolve((${elastic_nodes_ferm}))",
     }
 
-    package { 'elasticsearch/plugins':
-        provider => 'trebuchet',
-    }
-    # Elasticsearch 5 doesn't allow setting the plugin path, we need
-    # to symlink it into place. The directory already exists as part of the
-    # debian package, so we need to force the creation of the symlink.
-    $plugins_dir = '/srv/deployment/elasticsearch/plugins'
-    file { '/usr/share/elasticsearch/plugins':
-        ensure  => 'link',
-        target  => $plugins_dir,
-        force   => true,
-        require => Package['elasticsearch/plugins'],
+    package {'wmf-elasticsearch-search-plugins':
+        ensure => present,
+        before => Service['elasticsearch'],
     }
 
     # Install
     class { '::elasticsearch':
-        require                            => [
-            Package['elasticsearch/plugins'],
-            File['/usr/share/elasticsearch/plugins'],
-        ],
         # Production elasticsearch needs these plugins to be loaded in order
         # to work properly.  This will keep elasticsearch from starting
         # if these plugins are  not available.
@@ -76,7 +63,6 @@ class profile::elasticsearch(
             'extra',
             'analysis-icu',
         ],
-        plugins_dir                        => $plugins_dir,
         # Let apifeatureusage create their indices
         auto_create_index                  => '+apifeatureusage-*,-*',
         # Production can get a lot of use out of the filter cache.
