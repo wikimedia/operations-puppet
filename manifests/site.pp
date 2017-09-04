1d730307de45b200bc4b434b731cb11eadfc4ae6
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-09-04 12:41:42 +0200

    cassandra: reprovision restbase2003 with cassandra 3
    Bug: T169939
    Change-Id: I7c4819e63675f453029351f8eeae323805c5b455

--- manifests/site.pp
+++ manifests/site.pp
@@ -2068,11 +2068,11 @@ node /^restbase10(0[789]|1[012345678])\.eqiad\.wmnet$/ {
 }
 
 # restbase codfw cluster
-node /^restbase20(0[23456789]|1[012])\.codfw\.wmnet$/ {
+node /^restbase20(0[2456789]|1[012])\.codfw\.wmnet$/ {
     role(restbase::production)
 }
 
-node /^restbase2001\.codfw\.wmnet$/ {
+node /^restbase200[13]\.codfw\.wmnet$/ {
     role(restbase::production_ng)
 }
 
