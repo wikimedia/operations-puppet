49e037c343a8d3e2b8439c8ad3d3828b27242bef
Emanuele Rocca <ema@wikimedia.org>
2017-09-06 13:20:34 +0200

    varnish: use varnish::wikimedia_vcl for all files
    Use varnish::wikimedia_vcl in varnish::instance instead of file
    resources for uniformity and to help in the transition to the future
    parser.
    
    Change-Id: Ifb457a9af9fcd1fe71c3108468fd0bb3fca8c0bd

--- modules/varnish/manifests/wikimedia_vcl.pp
+++ modules/varnish/manifests/wikimedia_vcl.pp
@@ -1,15 +1,27 @@
-define varnish::wikimedia_vcl($varnish_testing, $template_path) {
+define varnish::wikimedia_vcl(
+    $varnish_testing = false,
+    $template_path = '',
+) {
     if $varnish_testing  {
         $varnish_include_path = '/usr/share/varnish/tests/'
         $dynamic_backend_caches = false
         $netmapper_dir = $varnish_include_path
+    } else {
+        $varnish_include_path = ''
+        $dynamic_backend_caches = hiera('varnish::dynamic_backend_caches', true)
+        $netmapper_dir = '/var/netmapper'
     }
+
+    # Hieradata switch to shut users out of a DC/cluster. T129424
+    $traffic_shutdown = hiera('cache::traffic_shutdown', false)
 
     file { $title:
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
         content => template($template_path),
+        notify  => $notify,
+        require => $require,
     }
 }
 
