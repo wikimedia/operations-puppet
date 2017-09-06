36920960c8036351b8309e9a30ffcebd6d660469
cpettet <rush@wikimedia.org>
2017-09-06 13:07:35 -0500

    openstack: cease managing nova files via openstack/common.pp
    This leaves a few nova.conf files orphaned for a short
    while while in transition.
    
    * Horizon (does this even need it?)
    * wikitech (does this even need it?)
    
    Bug: T171494
    Change-Id: Ibb99909610cb536db5420290e55735824401e37e

--- modules/openstack/manifests/common.pp
+++ modules/openstack/manifests/common.pp
@@ -29,32 +29,32 @@ class openstack::common(
     #  This is redundant and makes log-rotate unhappy.
     # Not to mention, nova-manage.log is very low traffic and doesn't
     #  really need to be rotated anyway.
-    file { '/etc/logrotate.d/nova-manage':
-        ensure  => absent,
-        require => Package['nova-common'],
-    }
+    ## file { '/etc/logrotate.d/nova-manage':
+    ##     ensure  => absent,
+    ##    require => Package['nova-common'],
+    ##}
 
     # Allow unprivileged users to look at nova logs
-    file { '/var/log/nova':
-        ensure => directory,
-        owner  => 'nova',
-        group  => hiera('openstack::log_group', 'adm'),
-        mode   => '0750',
-    }
+    ## file { '/var/log/nova':
+    ##    ensure => directory,
+    ##    owner  => 'nova',
+    ##    group  => hiera('openstack::log_group', 'adm'),
+    ##    mode   => '0750',
+    ##}
 
-    file {
-        '/etc/nova/nova.conf':
-            content => template("openstack/${openstack_version}/nova/nova.conf.erb"),
-            owner   => 'nova',
-            group   => 'nogroup',
-            mode    => '0440',
-            require => Package['nova-common'];
-        '/etc/nova/api-paste.ini':
-            content => template("openstack/${openstack_version}/nova/api-paste.ini.erb"),
-            owner   => 'nova',
-            group   => 'nogroup',
-            mode    => '0440',
-            require => Package['nova-common'];
-    }
+    ## file {
+    ##    '/etc/nova/nova.conf':
+    ##        content => template("openstack/${openstack_version}/nova/nova.conf.erb"),
+    ##        owner   => 'nova',
+    ##        group   => 'nogroup',
+    ##        mode    => '0440',
+    ##        require => Package['nova-common'];
+    ##    '/etc/nova/api-paste.ini':
+    ##        content => template("openstack/${openstack_version}/nova/api-paste.ini.erb"),
+    ##        owner   => 'nova',
+    ##        group   => 'nogroup',
+    ##        mode    => '0440',
+    ##        require => Package['nova-common'];
+    ##}
 }
 
