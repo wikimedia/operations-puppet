15449b33262ec9688f45899c4bc9552c4beaa0ad
Nuria Ruiz <nuria@wikimedia.org>
2017-09-07 15:40:11 -0700

    Add cron to purge old mediawiki data snapshots
    Companion change needs to be merged: https://gerrit.wikimedia.org/r/#/c/355601/7
    
    Bug: T162034
    Change-Id: I8ae4104bfea7f99c3270527d63598b7d63a34a94

--- modules/role/manifests/analytics_cluster/refinery/job/data_drop.pp
+++ modules/role/manifests/analytics_cluster/refinery/job/data_drop.pp
@@ -5,11 +5,12 @@
 class role::analytics_cluster::refinery::job::data_drop {
     require ::role::analytics_cluster::refinery
 
-    $webrequest_log_file       = "${role::analytics_cluster::refinery::log_dir}/drop-webrequest-partitions.log"
-    $eventlogging_log_file     = "${role::analytics_cluster::refinery::log_dir}/drop-eventlogging-partitions.log"
-    $wdqs_extract_log_file     = "${role::analytics_cluster::refinery::log_dir}/drop-wdqs-extract-partitions.log"
-    $mediawiki_log_file        = "${role::analytics_cluster::refinery::log_dir}/drop-mediawiki-log-partitions.log"
-    $druid_webrequest_log_file = "${role::analytics_cluster::refinery::log_dir}/drop-druid-webrequest.log"
+    $webrequest_log_file        = "${role::analytics_cluster::refinery::log_dir}/drop-webrequest-partitions.log"
+    $eventlogging_log_file      = "${role::analytics_cluster::refinery::log_dir}/drop-eventlogging-partitions.log"
+    $wdqs_extract_log_file      = "${role::analytics_cluster::refinery::log_dir}/drop-wdqs-extract-partitions.log"
+    $mediawiki_log_file         = "${role::analytics_cluster::refinery::log_dir}/drop-mediawiki-log-partitions.log"
+    $druid_webrequest_log_file  = "${role::analytics_cluster::refinery::log_dir}/drop-druid-webrequest.log"
+    $mediawiki_history_log_file = "${role::analytics_cluster::refinery::log_dir}/drop-mediawiki-history.log"
 
     # Shortcut var to DRY up cron commands.
     $env = "export PYTHONPATH=\${PYTHONPATH}:${role::analytics_cluster::refinery::path}/python"
@@ -82,4 +83,14 @@ class role::analytics_cluster::refinery::job::data_drop {
         minute      => '15',
         hour        => '5'
     }
+
+    # keep this many mediawiki history snapshots, 6 minimum
+    # cron runs once a month
+    $keep_snapshots = 6
+    cron {'mediawiki-history-drop-snapshot':
+        command     => "${env} && ${role::analytics_cluster::refinery::path}/bin/refinery-drop-mediawiki-snapshots -s ${keep_snapshots} >> ${mediawiki_history_log_file}",
+        environment => "MAILTO=${mail_to}",
+        monthday    => '15'
+    }
+
 }
