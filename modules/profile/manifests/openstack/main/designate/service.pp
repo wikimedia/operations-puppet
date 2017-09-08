9e9519f7bf56ccd68fa809691a2021b1786d1ec9
cpettet <rush@wikimedia.org>
2017-09-08 22:22:46 +0000

    openstack: designate as module/profile/role
    Bug: T171494
    Change-Id: Ib66fdb449af65f8c3608a6d225ee80976f4acd7d

---
+++ modules/profile/manifests/openstack/main/designate/service.pp
@@ -0,0 +1,44 @@
+class profile::openstack::main::designate::service(
+    $version = hiera('profile::openstack::main::version'),
+    $designate_host = hiera('profile::openstack::main::designate_host'),
+    $nova_controller = hiera('profile::openstack::main::nova_controller'),
+    $puppetmaster_hostname = hiera('profile::openstack::main::puppetmaster_hostname'),
+    $db_pass = hiera('profile::openstack::main::designate::db_pass'),
+    $db_host = hiera('profile::openstack::main::designate::db_host'),
+    $domain_id_internal_forward = hiera('profile::openstack::main::designate::domain_id_internal_forward'),
+    $domain_id_internal_reverse = hiera('profile::openstack::main::designate::domain_id_internal_reverse'),
+    $ldap_user_pass = hiera('profile::openstack::main::ldap_user_pass'),
+    $pdns_db_pass = hiera('profile::openstack::main::designate::pdns_db_pass'),
+    $db_admin_pass = hiera('profile::openstack::main::designate::db_admin_pass'),
+    $primary_pdns = hiera('profile::openstack::main::pdns::host'),
+    $secondary_pdns = hiera('profile::openstack::main::pdns::host_secondary'),
+    $rabbit_pass = hiera('profile::openstack::main::nova::rabbit_pass'),
+    $osm_host = hiera('profile::openstack::main::osm_host'),
+    $horizon_host = hiera('profile::openstack::main::horizon_host'),
+    ) {
+
+    require ::profile::openstack::main::clientlib
+    class{'::profile::openstack::base::designate::service':
+        version                    => $version,
+        designate_host             => $designate_host,
+        db_pass                    => $db_pass,
+        db_host                    => $db_host,
+        domain_id_internal_forward => $domain_id_internal_forward,
+        domain_id_internal_reverse => $domain_id_internal_reverse,
+        puppetmaster_hostname      => $puppetmaster_hostname,
+        nova_controller            => $nova_controller,
+        ldap_user_pass             => $ldap_user_pass,
+        pdns_db_pass               => $pdns_db_pass,
+        db_admin_pass              => $db_admin_pass,
+        primary_pdns               => $primary_pdns,
+        secondary_pdns             => $secondary_pdns,
+        rabbit_pass                => $rabbit_pass,
+        osm_host                   => $osm_host,
+        horizon_host               => $horizon_host,
+    }
+
+
+    class {'::openstack2::designate::monitor':
+        active => ($::fqdn == $designate_host),
+    }
+}
--- modules/role/manifests/wmcs/openstack/main/services.pp
+++ modules/role/manifests/wmcs/openstack/main/services.pp
@@ -1,3 +1,4 @@
 class role::wmcs::openstack::main::services {
-    include profile::openstack::main::cloudrepo
+    include ::profile::openstack::main::cloudrepo
+    include ::profile::openstack::main::designate::service
 }
