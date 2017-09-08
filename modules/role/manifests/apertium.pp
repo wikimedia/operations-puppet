7809b251f3076b64aaf0947a588f787fef1b1b55
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-09-08 18:19:42 +0200

    apertium: move to role/profile
    Change-Id: I65f9890bad802dedee36296bf1b4bbfe309d59e1

--- modules/role/manifests/apertium.pp
+++ modules/role/manifests/apertium.pp
@@ -1,23 +1,9 @@
 # vim: set ts=4 et sw=4:
 #
 # filtertags: labs-project-deployment-prep
-class role::apertium(
-    $port = '2737',
-) {
+class role::apertium {
     system::role { 'apertium':
         description => 'Apertium APY server'
     }
-
-    include ::apertium
-
-    ferm::service { 'apertium_http':
-        proto => 'tcp',
-        port  => $port,
-    }
-
-    monitoring::service { 'apertium':
-        description   => 'apertium apy',
-        check_command => "check_http_hostheader_port_url!apertium.svc.${::site}.wmnet!${port}!/listPairs",
-    }
-
+    include ::profile::apertium
 }
