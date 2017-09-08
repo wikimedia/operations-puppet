a8e90738651a6ba8a17da79d675d92faa9633df9
cpettet <rush@wikimedia.org>
2017-09-08 22:22:46 +0000

    openstack: designate as module/profile/role
    Bug: T171494
    Change-Id: Ib66fdb449af65f8c3608a6d225ee80976f4acd7d

--- modules/role/manifests/wmcs/openstack/labtest/services.pp
+++ modules/role/manifests/wmcs/openstack/labtest/services.pp
@@ -1,3 +1,4 @@
 class role::wmcs::openstack::labtest::services {
-    include profile::openstack::labtest::cloudrepo
+    include ::profile::openstack::labtest::cloudrepo
+    include ::profile::openstack::labtest::designate::service
 }
