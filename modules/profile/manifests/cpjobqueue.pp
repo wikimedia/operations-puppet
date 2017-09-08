9dc81992fbc51960ec82f32dc9014f1102e7feb4
Marko Obrovac <mobrovac@wikimedia.org>
2017-09-08 13:51:31 +0200

    Add the cp-jobqueue profile
    Bug: T175281
    Change-Id: I78a09d0d99ae20a38d397da8f8a1faf9dd015fe3

---
+++ modules/profile/manifests/cpjobqueue.pp
@@ -0,0 +1,57 @@
+# == Class: profile::cpjobqueue
+# 
+# profile::cpjobqueue is the profile class for ChangeProp for JobQueue, a
+# special instance of ChangePropagation that is used to pick up job definitions
+# from the main Kafka cluster (EventBus) and sends them for execution on the
+# jobrunners.
+#
+# === Parameters
+#
+# [*redis_path*]
+#   The path to Redis' UNIX socket. Note that the password is taken directly
+#   from the private repository with secrets.
+#
+# [*jobrunner_host*]
+#   The address of the LVS end point for jobrunners.
+#
+# [*kafka_msg_max_bytes*]
+#   The maximum allowed message size in Kafka. This value *must* match the
+#   brokers' configuration of the same name.
+#
+class profile::cpjobqueue(
+    $redis_path          = hiera('profile::cpjobqueue::redis_path'),
+    $jobrunner_host      = hiera('profile::cpjobqueue::jobrunner_host'),
+    $kafka_msg_max_bytes = hiera('kafka_message_max_bytes'),
+) {
+
+    include ::passwords::redis
+    require ::changeprop::packages
+
+    $kafka_config = kafka_config('main')
+
+    system::role { 'cp-jobqueue':
+        description => 'ChangePropagation instance for JobQueue jobs',
+    }
+
+    service::node { 'cp-jobqueue':
+        repo              => 'cp-jobqueue',
+        port              => 7200,
+        healthcheck_url   => '',
+        has_spec          => true,
+        deployment        => 'scap3',
+        deployment_config => true,
+        deployment_vars   => {
+            broker_list     => $kafka_config['brokers']['string'],
+            site            => $::site,
+            redis_path      => $redis_path,
+            redis_pass      => $::passwords::redis::main_password,
+            jobrunner_uri   => "${jobrunner_host}/rpc/RunSingleJob.php",
+            kafka_max_bytes => $kafka_msg_max_bytes,
+        },
+        auto_refresh      => false,
+        init_restart      => false,
+        environment       => {
+            'UV_THREADPOOL_SIZE' => 128,
+        },
+    }
+}
