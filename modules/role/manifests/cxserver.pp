98174d31cf2e413761d13dfbc6c74d996604353a
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-09-08 17:59:50 +0200

    cxserver: convert to role/profile
    Change-Id: I04872168708db65d316c7fb32bfc4dd6f536f97b

--- modules/role/manifests/cxserver.pp
+++ modules/role/manifests/cxserver.pp
@@ -6,15 +6,5 @@ class role::cxserver {
     system::role { 'cxserver':
         description => 'content translation server'
     }
-
-    include ::passwords::cxserver
-    $yandex_api_key = $::passwords::cxserver::yandex_api_key
-    $youdao_api_key = $::passwords::cxserver::youdao_api_key
-    $jwt_secret = $::passwords::cxserver::jwt_secret
-
-    class { '::cxserver':
-        yandex_api_key => $yandex_api_key,
-        youdao_api_key => $youdao_api_key,
-        jwt_secret     => $jwt_secret,
-    }
+    include ::profile::cxserver
 }
