5e08bb23b41194780de125f9c42fad310e1292c5
cpettet <rush@wikimedia.org>
2017-09-08 22:22:46 +0000

    openstack: designate as module/profile/role
    Bug: T171494
    Change-Id: Ib66fdb449af65f8c3608a6d225ee80976f4acd7d

---
+++ modules/profile/manifests/openstack/labtestn/designate/service.pp
@@ -0,0 +1,36 @@
+class profile::openstack::labtestn::designate::service(
+    $version = hiera('profile::openstack::labtestn::version'),
+    $designate_host = hiera('profile::openstack::labtestn::designate_host'),
+    $nova_controller = hiera('profile::openstack::labtestn::nova_controller'),
+    $puppetmaster_hostname = hiera('profile::openstack::labtestn::puppetmaster_hostname'),
+    $db_pass = hiera('profile::openstack::labtestn::designate::db_pass'),
+    $db_host = hiera('profile::openstack::labtestn::designate::db_host'),
+    $domain_id_internal_forward = hiera('profile::openstack::labtestn::designate::domain_id_internal_forward'),
+    $domain_id_internal_reverse = hiera('profile::openstack::labtestn::designate::domain_id_internal_reverse'),
+    $ldap_user_pass = hiera('profile::openstack::labtestn::ldap_user_pass'),
+    $pdns_db_pass = hiera('profile::openstack::labtestn::designate::pdns_db_pass'),
+    $db_admin_pass = hiera('profile::openstack::labtestn::designate::db_admin_pass'),
+    $primary_pdns = hiera('profile::openstack::labtestn::pdns::host'),
+    $secondary_pdns = hiera('profile::openstack::labtestn::pdns::host_secondary'),
+    $rabbit_pass = hiera('profile::openstack::labtestn::nova::rabbit_pass'),
+    ) {
+
+    class{'::profile::openstack::base::designate::service':
+        version                    => $version,
+        designate_host             => $designate_host,
+        db_pass                    => $db_pass,
+        db_host                    => $db_host,
+        domain_id_internal_forward => $domain_id_internal_forward,
+        domain_id_internal_reverse => $domain_id_internal_reverse,
+        puppetmaster_hostname      => $puppetmaster_hostname,
+        nova_controller            => $nova_controller,
+        ldap_user_pass             => $ldap_user_pass,
+        pdns_db_pass               => $pdns_db_pass,
+        db_admin_pass              => $db_admin_pass,
+        primary_pdns               => $primary_pdns,
+        secondary_pdns             => $secondary_pdns,
+        rabbit_pass                => $rabbit_pass,
+        keystone_public_port       => $keystone_public_port,
+        keystone_auth_port       => $keystone_auth_port,
+    }
+}
