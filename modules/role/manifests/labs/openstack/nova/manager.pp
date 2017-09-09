c0733177e9375a1b25c5a31e147479bf3e6d435e
Bryan Davis <bd808@wikimedia.org>
2017-09-09 15:57:00 -0600

    wmcs: tweak wikitech-static test label
    Was: "<icinga-wm> PROBLEM - are wikitech and wt-static in sync on
    silver is CRITICAL: wikitech-static CRIT - wikitech and wikitech-static
    out of sync (241954s 200000s)".
    
    Now: "<icinga-wm> PROBLEM - Are wikitech and wt-static in sync? on
    silver is CRITICAL: wikitech-static CRIT - wikitech and wikitech-static
    out of sync (241954s 200000s)"
    
    Change-Id: I0f576f2c603bfb1e618d5024bcf39a592a8b07f8

--- modules/role/manifests/labs/openstack/nova/manager.pp
+++ modules/role/manifests/labs/openstack/nova/manager.pp
@@ -61,7 +61,7 @@ class role::labs::openstack::nova::manager {
 
     # T89323
     monitoring::service { 'wikitech-static-sync':
-        description    => 'are wikitech and wt-static in sync',
+        description    => 'Are wikitech and wt-static in sync?',
         check_command  => 'check_wikitech_static',
         check_interval => 120,
     }
