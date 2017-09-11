ee82c374acd0641562a9a5ec8ebada549bfcac72
Ottomata <aotto@wikimedia.org>
2017-09-11 17:37:46 -0400

    Fetch Hadoop NameNode fsimage backups daily and also save them in bacula
    Change-Id: I5d8b772c8501e914858cac16aaa6d3c9f3a3c028

---
+++ modules/role/manifests/analytics_cluster/hadoop/backup/namenode.pp
@@ -0,0 +1,43 @@
+# == Class role::analytics_cluster::hadoop::backup::namenode
+# Periodically runs hdfs dfsadmin -fetchImage
+# and ensures that bacula backs up Hadoop NameNode fsimages,
+# in the case we need to recover if both Hadoop NameNodes.
+#
+class role::analytics_cluster::hadoop::backup::namenode {
+    require ::profile::hadoop::client
+
+    include ::role::analytics_cluster::backup
+
+    $destination = '/srv/backup/hadoop/namenode'
+    file { [
+            '/srv/backup/hadoop',
+            $destination
+        ]:
+        ensure => 'directory',
+        owner  => 'hdfs',
+        group  => 'analytics-admins',
+        mode   => '0750',
+    }
+
+
+    cron { 'hadoop-namenode-backup-fetchimage':
+        command  => "/usr/bin/hdfs dfsadmin -fetchImage ${destination}",
+        user     => 'hdfs',
+        hour     => 0
+    }
+
+    $retention_days = 30
+    # Delete files older than $retention_days.
+    cron { 'hadoop-namenode-backup-prune':
+        command => "/usr/bin/find ${destination} -mtime +${retension_days} -delete",
+        user     => 'hdfs',
+        hour     => 1,
+    }
+
+    # Bacula will also back up this directory.
+    # See: bacula::director::fileset { 'hadoop-namenode-backup'
+    # in profile::backup::director
+    class { 'backup::host':
+       sets => ['hadoop-namenode-backup', ]
+    }
+}
