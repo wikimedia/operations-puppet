09a83e0e3ec4aec902fa2085413d932e7338ef9c
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-09-11 08:46:38 +0200

    eventstreams: convert to role/profile
    Change-Id: I94dad66103ce344a04b85a581203ecc5540052d0

---
+++ modules/profile/manifests/eventstreams.pp
@@ -0,0 +1,66 @@
+# == Class profile::eventstreams
+#
+# Profile that installs EventStreams HTTP service.
+# This class includes the ::eventstreams role, and configures
+# it to consume only specific topics from a specific Kafka cluster.
+#
+# NOTE: eventstreams is configured to use the 'analytics-eqiad' Kafka cluster
+# in both eqiad and codfw.  This means that codfw eventstreams will be doing
+# cross DC consumption.  Since we don't have a beefy non critical 'aggregate/analytics'
+# Kafka cluster in codfw (yet), we don't have a good Kafka cluster that can back
+# eventstreams in codfw.  We could back it with the main-codfw cluster, but there
+# are issues with doing that:
+#
+# 1. main Kafka clusters are for critical prod services, and we don't want some unknown
+#    bug/exploit taking down the main clusters.  This service 'exposes' Kafka to the internet.
+#
+# 2. change-prop requires that topics have only one partition, which means we can't spread
+#    consumer load for a given topic across multiple brokers.  change-prop uses the main
+#    Kafka clusters.  In the analytics/aggregate cluster, we can add partitions to the topics.
+#
+# In the future, we would like to rename analytics-eqiad to something more appropriate, perhaps
+# aggregate-eqiad, and also set up an aggregate-codfw Kafka cluster.
+#
+# == Parameters
+# [*kafka_cluster_name*]
+#   Name of the kafka cluster
+#
+# [*streams*]
+#   Streams to follow
+#
+# [*rdkafka_config*]
+#   Additional configuration for the kafka library
+#
+# filtertags: labs-project-deployment-prep
+class profile::eventstreams(
+    $kafka_cluster_name = hiera('profile::eventstreams::kafka_cluster_name')
+    $streams = hiera('profile::eventstreams::streams'),
+    $rdkafka_config = hiera('profile::eventstreams::rdkafka_config')
+) {
+    $kafka_config = kafka_config($kafka_cluster_name)
+    $broker_list = $kafka_config['brokers']['string']
+    service::packages { 'eventstreams':
+        pkgs     => ['librdkafka++1', 'librdkafka1'],
+    }
+
+    service::node { 'eventstreams':
+        enable            => true,
+        port              => 8092,
+        has_spec          => false, # TODO: figure out how to monitor stream with spec x-amples
+        deployment        => 'scap3',
+        deployment_config => true,
+        deployment_vars   => {
+            log_level      => 'info',
+            site           => $::site,
+            broker_list    => $broker_list,
+            rdkafka_config => $rdkafka_config,
+            streams        => $streams,
+        },
+        auto_refresh      => false,
+        init_restart      => false,
+        environment       => {
+            'UV_THREADPOOL_SIZE' => 128,
+        },
+        require           => Service::Packages['eventstreams'],
+    }
+}
