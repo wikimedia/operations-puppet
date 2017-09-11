d9fd75fce3a434eaa0aca7dab3f9ce12143efa0d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-09-11 15:01:48 +0200

    role::scb: only include profiles, not roles
    Change-Id: I456d3cb3bcec2c7ba7340aa93e8f3a70aaae35c5

--- modules/role/manifests/scb.pp
+++ modules/role/manifests/scb.pp
@@ -1,33 +1,37 @@
 # "Compendium" class for nodes supporting various *oid services
 # This class is an intermediate step to better design
 class role::scb {
+
+    $services = [
+        'ORES', 'changeprop', 'citoid', 'cpjobqueue', 'cxserver',
+        'eventstreams', 'graphoid', 'mathoid', 'mobileapps',
+        'pdfrender', 'trendingedits',
+    ]
+
+    system::role { 'scb':
+        description => inline_template('Service cluster B; includes:\n<%= @services.join "\n" -%>')
+    }
+
+    include ::standard
+    include ::base::firewall
+    include role::lvs::realserver
+
+    # Ores
     include ::profile::ores::worker
     include ::profile::ores::web
     include ::profile::nutcracker
 
+
     include ::profile::cpjobqueue
-    system::role { 'cpjobqueue':
-        description => 'ChangeProp instance for the JobQueue',
-    }
-
     include ::profile::recommendation_api
-
-    include role::mobileapps
-    include role::mathoid
-    include role::graphoid
-    include role::citoid
-    include role::cxserver
-    include role::changeprop
-    include role::apertium
-    include role::eventstreams
-    include role::pdfrender
-    include role::trendingedits
-
-    include ::standard
-    include ::base::firewall
-
-    if hiera('has_lvs', true) {
-        include role::lvs::realserver
-    }
-
+    include ::profile::mobileapps
+    include ::profile::mathoid
+    include ::profile::graphoid
+    include ::profile::citoid
+    include ::profile::cxserver
+    include ::profile::changeprop
+    include ::profile::apertium
+    include ::profile::eventstreams
+    include ::profile::pdfrender
+    include ::profile::trendingedits
 }
