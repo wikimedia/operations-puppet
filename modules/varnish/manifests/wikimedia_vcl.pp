0b82a00991dff2d14f1434bb4cbd73bb78cee698
Emanuele Rocca <ema@wikimedia.org>
2017-09-11 14:22:42 +0200

    varnish::instance: fix template attributes scope
    Change-Id: Ib2723ce0145335fd22b667d6c41090835c814198

--- modules/varnish/manifests/wikimedia_vcl.pp
+++ modules/varnish/manifests/wikimedia_vcl.pp
@@ -1,6 +1,13 @@
 define varnish::wikimedia_vcl(
     $varnish_testing = false,
     $template_path = '',
+    $vcl_config = {},
+    $backend_caches = {},
+    $vcl = '',
+    $inst = '',
+    $generate_extra_vcl = false,
+    $app_directors={},
+    $app_def_be_opts={},
 ) {
     if $varnish_testing  {
         $varnish_include_path = '/usr/share/varnish/tests/'
@@ -15,13 +22,29 @@ define varnish::wikimedia_vcl(
     # Hieradata switch to shut users out of a DC/cluster. T129424
     $traffic_shutdown = hiera('cache::traffic_shutdown', false)
 
-    file { $title:
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        content => template($template_path),
-        notify  => $notify,
-        require => $require,
+    $cache_route = $::role::cache::instances::cache_route
+
+    if $generate_extra_vcl {
+        $extra_vcl_name = regsubst($title, '^([^ ]+) .*$', '\1')
+        $extra_vcl_filename = "/etc/varnish/${extra_vcl_name}.inc.vcl"
+        if !defined(File[$extra_vcl_filename]) {
+            file { $extra_vcl_filename:
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0444',
+                content => template("varnish/${extra_vcl_name}.inc.vcl.erb"),
+            }
+        }
+    } else {
+        file { $title:
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            content => template($template_path),
+            notify  => $notify,
+            require => $require,
+            before  => $before,
+        }
     }
 }
 
