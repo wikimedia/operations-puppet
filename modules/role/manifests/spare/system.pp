aecb3e567b5ee1cdcfc016d9c84491f1d96a425b
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-09-11 09:10:24 +0200

    Update comment
    Change-Id: I9547710b8bd17b17dc898224e80fde6aeaf63b94

--- modules/role/manifests/spare/system.pp
+++ modules/role/manifests/spare/system.pp
@@ -1,7 +1,8 @@
 # This is a role for systems which are in the process of being reclaimed
 # or decommissioned. IOW. the host once had a feature role assigned, but
 # currently no has it. If such hosts are simply reset to "include ::standard"
-# in site.pp, they are no longer matched by role-based grains for debdeploy.
+# in site.pp, they are no longer matched by role-based server addressing
+# in debdeploy.
 #
 # This role is entirely transient. Once a system has been reclaimed to spares
 # or decomissioned, this role is removed from site.pp along with the host entry.
