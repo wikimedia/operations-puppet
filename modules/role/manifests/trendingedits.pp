2a980769117f96891d0af58fcad2022d08ca4184
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-09-11 08:08:09 +0200

    trendingedits: move to role/profile
    Change-Id: Ia0a2248c7282583a8c2fcc87e94d86fd749c9c33

--- modules/role/manifests/trendingedits.pp
+++ modules/role/manifests/trendingedits.pp
@@ -1,17 +1,8 @@
 # Role class for trendingedits
 class role::trendingedits {
-
-    $kafka_config = kafka_config('main')
-    $port = 6699
-
     system::role { 'trendingedits':
         description => 'computes the list of currently-trending articles',
     }
 
-    class { '::trendingedits':
-        port        => $port,
-        broker_list => $kafka_config['brokers']['string'],
-    }
-
+    include ::profile::trendingedits
 }
-
