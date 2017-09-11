103d2d4a0db7142234eb2d7c39593e8d6e58b208
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-09-11 09:25:40 +0200

    graphoid: convert to role/profile
    Change-Id: Ib15aaa3d86c825edb3d672331bcaf113852fb10c

--- modules/profile/manifests/graphoid.pp
+++ modules/profile/manifests/graphoid.pp
@@ -1,4 +1,4 @@
-# == Class: graphoid
+# == Class: profile::graphoid
 #
 # This class installs and configures graphoid, a node.js service that
 # converts a graph definition into a PNG image
@@ -8,31 +8,30 @@
 # [*allowed_domains*]
 #   The protocol-to-list-of-domains map. Default: {}
 #   The protocols include http, https, as well as some custom graph-specific protocols.
-#   See https://www.mediawiki.org/wiki/Extension:Graph?venotify=restored#External_data
-#
-# [*domain_map*]
-#   The domain-to-domain alias map. Default: {}
-#
-# [*timeout*]
-#   The timeout (in ms) for requests. Default: 5000
-#
+#   See https://www.mediawiki.org/wiki/Extension:Graph?venotify=restored#External_data#
 # [*headers*]
 #   A map of headers that will be sent with each reply. Could be used for caching, etc. Default: false
 #
 # [*error_headers*]
 #   A map of headers that will be sent with each reply in case of an error. If not set, above headers will be used. Default: false
 #
-class graphoid(
-    $allowed_domains = {},
-    $domain_map    = {},
-    $timeout       = 5000,
-    $headers       = false,
-    $error_headers = false,
+class profile::graphoid(
+    $allowed_domains = hiera('profile::graphoid::allowed_domains'),
+    $headers       = hiera('profile::graphoid::headers'),
+    $error_headers = hiera('profile::graphoid::error_headers'),
 ) {
-
-    require ::graphoid::packages
-
     requires_os('debian >= jessie')
+
+    $domain_map    = {}
+    $timeout       = 5000
+
+    require ::mediawiki::packages::fonts
+
+    service::packages { 'graphoid':
+        pkgs     => ['libcairo2', 'libgif4', 'libjpeg62-turbo', 'libpango1.0-0'],
+        dev_pkgs => ['libcairo2-dev', 'libgif-dev', 'libpango1.0-dev', 'libjpeg62-turbo-dev'],
+    }
+
 
     service::node { 'graphoid':
         port            => 19000,
