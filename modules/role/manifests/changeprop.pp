3d4363c0ebd94f3276e10ce30b3f0f7e7b8ce860
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-09-11 09:18:00 +0200

    changeprop: convert to role/profile
    Change-Id: I191a4f46aaffa13bca21bfbb3a00292ef10f46b1

--- modules/role/manifests/changeprop.pp
+++ modules/role/manifests/changeprop.pp
@@ -2,20 +2,9 @@
 #
 # filtertags: labs-project-deployment-prep
 class role::changeprop {
-
-    include ::passwords::redis
-    $kafka_config = kafka_config('main')
-
     system::role { 'changeprop':
         description => 'propagates events from the EventBus',
     }
 
-    class { '::changeprop':
-        broker_list         => $kafka_config['brokers']['string'],
-        redis_pass          => $::passwords::redis::main_password,
-        # FIXME: this needs to be refactored when the role
-        # is moved to profiles.
-        kafka_msg_max_bytes => hiera('kafka_message_max_bytes', 1048576),
-    }
-
+    include ::profile::changeprop
 }
