bc79e330bc0ec2a1a6ee189eeaebc91fc2f77b1b
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-09-11 08:58:05 +0200

    pdfrender: switch to role/profile
    In this case, since it's not incapsulating service::node and collects
    quite a few resources, we prefer not to remove the named class.
    
    Change-Id: I735701d4ea4069c307b328d8fcb5fab782aad165

--- modules/role/manifests/pdfrender.pp
+++ modules/role/manifests/pdfrender.pp
@@ -1,22 +1,7 @@
 class role::pdfrender {
-    $is_active = hiera('role::pdfrender::is_active', true)
-
     system::role { 'pdfrender':
         description => 'A PDF render service based on Electron',
     }
 
-    $port = 5252
-
-    class { '::pdfrender':
-        port        => $port,
-        no_browsers => 4,
-        running     => $is_active,
-    }
-
-    ferm::service { "pdfrender_http_${port}":
-        proto  => 'tcp',
-        port   => $port,
-        srange => '$DOMAIN_NETWORKS',
-    }
-
+    include ::profile::pdfrender
 }
