db5867cf77d9dd70ea578c23917191bd0420e0b3
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-09-11 17:59:04 +0200

    Remove Puppet code which is obsolete with Salt grain removal
    Salt is being removed from our infrastructure and replaced by Cumin.
    
    Change-Id: I8bcfec2b499b0f1597f71f215bf6e20db98e1250

--- modules/lvs/manifests/configuration.pp
+++ modules/lvs/manifests/configuration.pp
@@ -44,31 +44,6 @@ class lvs::configuration {
         },
     }
 
-    # NOTE: This is for informational purposes only. The actual configuration
-    # that decides primary/secondary is done at the BGP level on the routers.
-    $lvs_grain = $::hostname ? {
-        /^lvs100[123789]$/  => 'primary',
-        /^lvs200[123]$/     => 'primary',
-        /^lvs[34]00[1256]$/ => 'primary',
-        default => 'secondary'
-    }
-
-    # This is technically redundant information from $lvs_class_hosts, but
-    # transforming one into the other in puppet is a huge PITA.
-    $lvs_grain_class = $::hostname ? {
-        'lvs1007'          => 'high-traffic1',
-        'lvs1008'          => 'high-traffic2',
-        'lvs1009'          => 'low-traffic',
-        'lvs1010'          => 'secondary',
-        /^lvs[12]00[14]$/  => 'high-traffic1',
-        /^lvs[12]00[25]$/  => 'high-traffic2',
-        /^lvs[12]00[36]$/  => 'low-traffic',
-        /^lvs[34]00[135]$/ => 'high-traffic1',
-        /^lvs[34]00[246]$/ => 'high-traffic2',
-        /^lvs[34]007$/     => 'secondary',
-        default            => 'unknown',
-    }
-
     # NOTE! This hash is referenced in many other manifests
     $service_ips = hiera('lvs::configuration::lvs_service_ips')
     $lvs_services = hiera('lvs::configuration::lvs_services')
