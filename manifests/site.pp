fa330013a4a7cc33ca6e66c4ead65eb6b1fde2bb
Jaime Crespo <jcrespo@wikimedia.org>
2017-09-12 15:12:13 +0200

    Add new m1 host db2078, enable firewall on all misc services
    Bug: T175685
    Bug: T104699
    Change-Id: Ieb074118dd7e23ff5a154ba176e439a047b98b84

--- manifests/site.pp
+++ manifests/site.pp
@@ -555,21 +555,24 @@ node 'db1016.eqiad.wmnet' {
         shard  => 'm1',
         master => true,
     }
-    include ::base::firewall
 }
 
 node 'db1001.eqiad.wmnet' {
     class { '::role::mariadb::misc':
         shard  => 'm1',
     }
-    include ::base::firewall
 }
 
 node 'db2010.codfw.wmnet' {
     class { '::role::mariadb::misc':
         shard => 'm1',
     }
-    include ::base::firewall
+}
+
+node 'db2078.codfw.wmnet' {
+    class { '::role::mariadb::misc':
+        shard => 'm1',
+    }
 }
 
 ## m2 shard
@@ -592,27 +595,23 @@ node 'db1043.eqiad.wmnet' {
         shard  => 'm3',
         master => true,
     }
-    include ::base::firewall
 }
 
 node 'db1048.eqiad.wmnet' {
     class { '::role::mariadb::misc::phabricator':
         shard => 'm3',
     }
-    include ::base::firewall
 }
 node 'db1059.eqiad.wmnet' {
     class { '::role::mariadb::misc::phabricator':
         shard => 'm3',
     }
-    include ::base::firewall
 }
 
 node 'db2012.codfw.wmnet' {
     class { '::role::mariadb::misc::phabricator':
         shard => 'm3',
     }
-    include ::base::firewall
 }
 
 # m4 shard
@@ -621,7 +620,6 @@ node 'db1046.eqiad.wmnet' {
         shard  => 'm4',
         master => true,
     }
-    include ::base::firewall
 }
 node 'db1047.eqiad.wmnet' {
     # this slave has an m4 custom replication protocol
@@ -631,7 +629,6 @@ node 'db1047.eqiad.wmnet' {
         shard  => 'm4',
         master => false,
     }
-    include ::base::firewall
 }
 
 # m5 shard
@@ -646,7 +643,6 @@ node 'db2030.codfw.wmnet' {
     class { '::role::mariadb::misc':
         shard => 'm5',
     }
-    include ::base::firewall
 }
 
 # sanitarium
