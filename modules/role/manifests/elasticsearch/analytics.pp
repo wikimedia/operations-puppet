14699f37eeeb2d88af80ecd59d6a48ce0cab2a2f
Tyler Cipriani <tcipriani@wikimedia.org>
2017-09-13 16:46:59 -0600

    Deploy discovery-analytics with scap3
    Bug: T129149
    Depends-On: Icd80f28ee1a950cea72ec7b00e639526fe9bb846
    Change-Id: I2284aa559a6f5bddbd2c7277539eb7869acdd3f3

--- modules/role/manifests/elasticsearch/analytics.pp
+++ modules/role/manifests/elasticsearch/analytics.pp
@@ -1,7 +1,7 @@
 # Supports CirrusSearch usage on the analytics cluster
 class role::elasticsearch::analytics {
     # wikimedia/discovery/analytics will be deployed to this node
-    package { 'wikimedia/discovery/analytics':
-        provider => 'trebuchet',
+    scap::target { 'wikimedia/discovery/analytics':
+        deploy_user => 'deploy-service',
     }
 }
