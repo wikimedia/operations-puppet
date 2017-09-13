738f4bd5c7891eff6944931e3821843a9afb3fd0
Ariel T. Glenn <ariel@wikimedia.org>
2017-09-13 14:26:24 +0300

    restructure dumps webserver, zim manifests to module/role/profile
    We don't serve zim dumps via a special setup any more, so rename
    that stuff to reflect what it does, i.e. serve html dumps.
    
    Other dumps-related manifests will get moved into the dumps module
    and profile directories later.
    
    Bug: T175592
    Change-Id: I87da750a869061774156bde2997fce0f1908baf6

---
+++ modules/role/manifests/dumps/web/htmldumps.pp
@@ -0,0 +1,19 @@
+# dumps of revision content from restbase, in html format
+class role::dumps::web::htmldumps {
+
+    system::role { 'role::dumps::web::htmldumps': description => 'HTML format dumps' }
+
+    include ::standard
+    include ::profile::dumps::web::htmldumps
+    include ::base::firewall
+
+    ferm::service { 'html_dumps_http':
+        proto => 'tcp',
+        port  => '80',
+    }
+
+    ferm::service { 'html_dumps_https':
+        proto => 'tcp',
+        port  => '443',
+    }
+}
