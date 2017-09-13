f243fbe4e5af7f36829c85f5aacef1f4a7dce42e
Ariel T. Glenn <ariel@wikimedia.org>
2017-09-13 14:26:24 +0300

    restructure dumps webserver, zim manifests to module/role/profile
    We don't serve zim dumps via a special setup any more, so rename
    that stuff to reflect what it does, i.e. serve html dumps.
    
    Other dumps-related manifests will get moved into the dumps module
    and profile directories later.
    
    Bug: T175592
    Change-Id: I87da750a869061774156bde2997fce0f1908baf6

--- manifests/site.pp
+++ manifests/site.pp
@@ -328,7 +328,7 @@ node /^(diadem|dysprosium)\.wikimedia\.org$/ {
 
 node 'dataset1001.wikimedia.org' {
 
-    role(dataset::primary, dumps::server)
+    role(dataset::primary, dumps::web::xmldumps)
 
     interface::add_ip6_mapped { 'main': }
 }
@@ -944,9 +944,9 @@ node 'fermium.wikimedia.org' {
     interface::add_ip6_mapped { 'main': }
 }
 
-# ZIM dumps (https://en.wikipedia.org/wiki/ZIM_%28file_format%29)
+# HTML dumps from Restbase
 node 'francium.eqiad.wmnet' {
-    role(dumps::zim)
+    role(dumps::web::htmldumps)
 }
 
 # Virtualization hosts
@@ -1537,7 +1537,7 @@ node 'multatuli.wikimedia.org' {
 # also see dataset1001
 node 'ms1001.wikimedia.org' {
 
-    role(dataset::secondary, dumps::server)
+    role(dataset::secondary, dumps::web::xmldumps)
 
     interface::add_ip6_mapped { 'main': }
 }
