d601869eb6aa163ba32c1d6df78411a6bbe59ae3
Eric Evans <eevans@wikimedia.org>
2017-09-13 14:36:13 -0500

    Introduce a new `target_version` for dev-only builds
    Bug: T172384
    Change-Id: Iede18a8b810932775c4fa9c123a0a24a3b9d7231

--- modules/cassandra/manifests/init.pp
+++ modules/cassandra/manifests/init.pp
@@ -421,7 +421,8 @@ class cassandra(
     $package_version = $target_version ? {
         '2.1' => hiera('cassandra::version', '2.1.13'),
         '2.2' => hiera('cassandra::version', '2.2.6-wmf1'),
-        '3.x' => hiera('cassandra::version', '3.11.0-wmf4')
+        '3.x' => hiera('cassandra::version', '3.11.0-wmf3'),
+        'dev' => hiera('cassandra::version', '3.11.0-wmf4')
     }
     package { 'cassandra':
         ensure  => $package_version,
