ba290e18c721d49540a1903c9f98b1a43c6914c2
Ariel T. Glenn <ariel@wikimedia.org>
2017-09-13 14:26:24 +0300

    restructure dumps webserver, zim manifests to module/role/profile
    We don't serve zim dumps via a special setup any more, so rename
    that stuff to reflect what it does, i.e. serve html dumps.
    
    Other dumps-related manifests will get moved into the dumps module
    and profile directories later.
    
    Bug: T175592
    Change-Id: I87da750a869061774156bde2997fce0f1908baf6

---
+++ modules/profile/manifests/dumps/web/rsync_server.pp
@@ -0,0 +1,24 @@
+class profile::dumps::web::rsync_server(
+    $rsync_clients_ipv4 = hiera('profile::dumps::web::rsync_server::clients_ipv4'),
+    $rsync_clients_ipv6 = hiera('profile::dumps::web::rsync_server::clients_ipv6'),
+) {
+    # By default the resolve() function in ferm performs only an IPv4/A DNS
+    # lookup. It fails if a host only has an IPv6 address. Ferm also provides
+    # a AAAA lookup mode for IPv6 addresses, but this equally fails if only
+    # an IPv4 address is present.
+
+    $rsync_clients_ipv4_ferm = join($rsync_clients_ipv4, ' ')
+    $rsync_clients_ipv6_ferm = join($rsync_clients_ipv6, ' ')
+
+    ferm::service {'dumps_rsyncd_ipv4':
+        port   => '873',
+        proto  => 'tcp',
+        srange => "@resolve((${rsync_clients_ipv4_ferm}))",
+    }
+
+    ferm::service {'dumps_rsyncd_ipv6':
+        port   => '873',
+        proto  => 'tcp',
+        srange => "@resolve((${rsync_clients_ipv6_ferm}),AAAA)",
+    }
+}
