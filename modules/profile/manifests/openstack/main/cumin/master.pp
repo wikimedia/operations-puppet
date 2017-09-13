45991ff58034f9630410a062d1448c8232ad8232
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-09-13 11:52:51 +0200

    WMCS: install Cumin for WMCS admins
    * Install Cumin on labs puppetmaster frontend to act as masters
    * Add puppetmaster frontend hosts as Cumin masters for WMCS in
      constants.pp
    * Authorize Cumin in WMCS restricted bastions from the Cumin masters
    * Authorize Cumin in WMCS VM instances from the restricted bastions
    
    Cumin's SSH key will be created in the prod Puppet private repo and only
    its public key copied into the labs/private repo as is, to authorize
    Cumin's connections, while the private key will just be a stub one.
    
    Bug: T175712
    Change-Id: I352d7a7011f976c57b03e9ddf8ad381ddbb3e340

---
+++ modules/profile/manifests/openstack/main/cumin/master.pp
@@ -0,0 +1,47 @@
+class profile::openstack::main::cumin::master(
+    $openstack_protocol = hiera('profile::openstack::base::keystone::auth_protocol'),
+    $openstack_host = hiera('labs_keystone_host'),
+    $openstack_port = hiera('profile::openstack::base::keystone::public_port'),
+    $openstack_project = hiera('novaconfig::observer_project'),
+    $openstack_username = hiera('novaconfig::observer_user'),
+    $openstack_password = hiera('novaconfig::observer_password'),
+    ) {
+        ::keyholder::agent { 'cumin_openstack_master':
+            trusted_groups => ['root'],
+        }
+
+        require_package('cumin')
+
+        $cumin_log_path = '/var/log/cumin'  # Used also in config.yaml
+        file { $cumin_log_path:
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0750',
+        }
+
+        file { '/etc/cumin':
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0750',
+        }
+
+        file { '/etc/cumin/config.yaml':
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0640',
+            content => template('profile/openstack/main/cumin/config.yaml.erb'),
+            require => File['/etc/cumin'],
+        }
+
+        file { '/etc/cumin/aliases.yaml':
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0640',
+            content => template('profile/openstack/main/cumin/aliases.yaml.erb'),
+            require => File['/etc/cumin'],
+        }
+}
