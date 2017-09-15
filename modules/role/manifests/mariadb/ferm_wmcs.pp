c48e4f7ce9db2e01fd3fea7e3f95313a2802313d
andrewbogott <abogott@wikimedia.org>
2017-09-14 19:24:30 -0500

    add firewall exceptions for m5 and cloud services
    Several wmcs things run on public IPs so need special firewall
    rules.
    
    Change-Id: I0948903996861ef9b670bfb9775cc28d874db347

---
+++ modules/role/manifests/mariadb/ferm_wmcs.pp
@@ -0,0 +1,44 @@
+# Firewall rules for the misc db host used by wmcs.  We need special
+#  rules to allow access for openstack services (which typically
+#  run on hosts with public IPs)
+
+class role::mariadb::ferm_wmcs {
+    $nova_controller = hiera('profile::openstack::main::nova_controller'),
+    $nova_controller_standby = hiera('profile::openstack::main::nova_controller_standby'),
+    ferm::service{ 'nova_controller':
+        proto   => 'tcp',
+        port    => '3306',
+        notrack => true,
+        srange  => "${nova_controller} ${nova_controller_standby}",
+    }
+
+    $designate_host = hiera('profile::openstack::main::designate_host'),
+    $designate_host_standby = hiera('profile::openstack::main::designate_host_standby'),
+    ferm::service{ 'designate':
+        proto   => 'tcp',
+        port    => '3306',
+        notrack => true,
+        srange  => "${designate_host} ${designate_host_standby}",
+    }
+
+    ferm::service{ 'wmcs_puppetmasters':
+        proto   => 'tcp',
+        port    => '3306',
+        notrack => true,
+        srange  => '@resolve((labpuppetmaster1001.wikimedia.org)) @resolve((labpuppetmaster1002.wikimedia.org))',
+    }
+
+    ferm::service{ 'striker':
+        proto   => 'tcp',
+        port    => '3306',
+        notrack => true,
+        srange  => '@resolve((toolsadmin.wikimedia.org))',
+    }
+
+    ferm::service{ 'wikitech':
+        proto   => 'tcp',
+        port    => '3306',
+        notrack => true,
+        srange  => '@resolve((wikitech.wikimedia.org))',
+    }
+}
