cc08f4437ff1e560fdbf68f7184ba7a231a53c08
andrewbogott <abogott@wikimedia.org>
2017-09-16 06:57:17 -0500

    labtest: include salt profile on labtestcontrol
    I know we're about to kill off salt, but in the meantime
    lacking this on labcontrol breaks designate's cert-handling
    
    Change-Id: If07667d50f972d66afecf73f9a4246e60cf70a74

---
+++ modules/profile/manifests/openstack/labtest/salt.pp
@@ -0,0 +1,10 @@
+class profile::openstack::main::salt(
+    $instance_range = hiera('profile::openstack::labtest::nova::fixed_range'),
+    $designate_host = hiera('profile::openstack::labtest::designate_host'),
+    ) {
+
+    class {'::profile::openstack::base::salt':
+        instance_range => $instance_range,
+        designate_host => $designate_host,
+    }
+}
