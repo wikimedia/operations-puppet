f333f0201e85469852880c348a9cb39cee013b3a
cpettet <rush@wikimedia.org>
2017-09-18 21:24:49 +0000

    openstack: pdns auth module/role/profile
    Rename module from labs_dns to pdns_server
    
    Bug: T171494
    Change-Id: Ib021f565b5c528dce8d94209b70d424ad7735e00

--- manifests/site.pp
+++ manifests/site.pp
@@ -1027,7 +1027,6 @@ node 'labpuppetmaster1002.wikimedia.org' {
 # labservices1001 hosts openstack-designate, the labs DNS service.
 node 'labservices1001.wikimedia.org' {
     role(wmcs::openstack::main::services,
-          labs::dns,
           labs::dnsrecursor,
           labs::dns_floating_ip_updater)
     include ::standard
@@ -1037,7 +1036,6 @@ node 'labservices1001.wikimedia.org' {
 
 node 'labservices1002.wikimedia.org' {
     role(wmcs::openstack::main::services,
-          labs::dns,
           labs::dnsrecursor)
     include ::standard
     include ::base::firewall
@@ -1123,7 +1121,6 @@ node 'labtestpuppetmaster2001.wikimedia.org' {
 
 node 'labtestservices2001.wikimedia.org' {
     role(wmcs::openstack::labtest::services,
-          labs::dns,
           labs::dnsrecursor,
           openldap::labtest,
           labs::dns_floating_ip_updater)
