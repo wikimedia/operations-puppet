e4c8044d76ce7890ecd1a87318ff10680c841388
cpettet <rush@wikimedia.org>
2017-09-18 21:24:49 +0000

    openstack: pdns auth module/role/profile
    Rename module from labs_dns to pdns_server
    
    Bug: T171494
    Change-Id: Ib021f565b5c528dce8d94209b70d424ad7735e00

--- modules/role/manifests/wmcs/openstack/main/services.pp
+++ modules/role/manifests/wmcs/openstack/main/services.pp
@@ -1,4 +1,6 @@
 class role::wmcs::openstack::main::services {
     include ::profile::openstack::main::cloudrepo
+    include ::profile::openstack::main::pdns::auth::db
+    include ::profile::openstack::main::pdns::auth::service
     include ::profile::openstack::main::designate::service
 }
