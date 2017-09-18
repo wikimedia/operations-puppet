9a86898b11eadd946f967ec1a043adf4838012f9
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-09-18 16:49:41 +0200

    prometheus: add analytics instance
    Bug: T175922
    Change-Id: I6d9329c6c6d12c1f18305430aa5cd23a2daf327e

--- manifests/site.pp
+++ manifests/site.pp
@@ -2056,7 +2056,7 @@ node /poolcounter[12]00[12]\.(codfw|eqiad)\.wmnet/ {
 }
 
 node /^prometheus200[34]\.codfw\.wmnet$/ {
-    role(prometheus::ops, prometheus::global, prometheus::services)
+    role(prometheus::ops, prometheus::global, prometheus::services, prometheus::analytics)
 
     include ::base::firewall
     include ::standard
@@ -2072,7 +2072,7 @@ node 'puppetcompiler1001.eqiad.wmnet' {
 }
 
 node /^prometheus100[34]\.eqiad\.wmnet$/ {
-    role(prometheus::ops, prometheus::global, prometheus::services)
+    role(prometheus::ops, prometheus::global, prometheus::services, prometheus::analytics)
 
     include ::lvs::realserver
 
