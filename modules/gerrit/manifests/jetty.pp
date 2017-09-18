47e137fb79895ecef4d71bcd52880daebf207d1a
Paladox <thomasmulhall410@yahoo.com>
2017-09-18 19:11:23 +0000

    Gerrit: Use systemd::service for systemd
    This is a perquisite to removing the gerrit deb.
    
    Almost ready to remove the deb.
    
    Change-Id: I33951f9d204d4cad03903216cf86026b73eecf80

--- modules/gerrit/manifests/jetty.pp
+++ modules/gerrit/manifests/jetty.pp
@@ -12,6 +12,7 @@ class gerrit::jetty(
     $log_host = undef,
     $log_port = '4560',
     $config = 'gerrit.config.erb',
+    $git_open_files = 20000,
     ) {
 
     group { 'gerrit2':
@@ -249,16 +250,29 @@ class gerrit::jetty(
       require => [File['/var/lib/gerrit2/review_site'], Scap::Target['gerrit/gerrit']],
     }
 
-    service { 'gerrit':
-        ensure    => running,
-        enable    => true,
-        hasstatus => false,
-        status    => '/etc/init.d/gerrit check',
+    systemd::service { 'gerrit':
+        ensure         => present,
+        content        => systemd_template('gerrit'),
+        service_params => {
+            ensure     => 'running',
+            provider   => $::initsystem,
+            hasrestart => true,
+            hasstatus  => false,
+            status     => '/var/lib/gerrit2/review_site/bin/gerrit.sh check',
+        },
+    }
+
+    file { '/etc/default/gerrit':
+        content => template('gerrit/gerrit.default.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
     }
 
     file { '/etc/default/gerritcodereview':
-        ensure => 'link',
-        target => '/etc/default/gerrit',
+        ensure  => 'link',
+        target  => '/etc/default/gerrit',
+        require => File['/etc/default/gerrit'],
     }
 
     nrpe::monitor_service { 'gerrit':
