9f07e3ca039404c53731df50ae59ee4506833c66
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-09-18 15:12:02 +0200

    Remove salt grains used for trebuchet
    Also remove a bit related code currently using the grains.
    
    To be merged once 129290 is resolved
    
    Change-Id: I215bb0ae807ffa19342146295e67328e419d1cb7

--- modules/trebuchet/manifests/init.pp
+++ modules/trebuchet/manifests/init.pp
@@ -13,17 +13,4 @@ class trebuchet(
     }
 
     include ::trebuchet::packages
-
-    salt::grain { 'trebuchet_master':
-        value   => $trebuchet_master,
-        replace => true,
-    }
-
-    # Trebuchet needs salt-minion to query grains, and it needs the
-    # `trebuchet_master` grain set so that it knows where to fetch from.
-    # It also needs the dependencies from ::trebuchet::packages.
-    Service['salt-minion'] ->
-        Salt::Grain['trebuchet_master'] ->
-            Class['::trebuchet::packages'] ->
-                Package <| provider == 'trebuchet' |>
 }
