bf13394e16938c5df2aa81c726c4d9eaadc177f6
cpettet <rush@wikimedia.org>
2017-09-18 21:24:49 +0000

    openstack: pdns auth module/role/profile
    * Rename labs_dns pdns_server
    
    Bug: T171494
    Change-Id: Ib021f565b5c528dce8d94209b70d424ad7735e00

---
+++ modules/profile/manifests/openstack/base/pdns/auth/monitor/host_check.pp
@@ -0,0 +1,21 @@
+class profile::openstack::base::pdns::auth::monitor::host_check(
+    $auth_soa_name = hiera('profile::openstack::base::pdns::host'),
+    $target_fqdn = hiera('profile::openstack::base::pdns::monitor::target_fqdn'),
+    ) {
+
+    monitoring::host { $auth_soa_name:
+        ip_address => $::ipaddress,
+    }
+
+    monitoring::service { "${auth_soa_name} Auth DNS UDP":
+        host          => $auth_soa_name,
+        description   => 'Check for gridmaster host resolution UDP',
+        check_command => "check_dig!${auth_soa_name}!${target_fqdn}",
+    }
+
+    monitoring::service { "${auth_soa_name} Auth DNS TCP":
+        host          => $auth_soa_name,
+        description   => 'Check for gridmaster host resolution TCP',
+        check_command => "check_dig_tcp!${auth_soa_name}!${target_fqdn}",
+    }
+}
---
+++ modules/profile/manifests/openstack/base/pdns/auth/service.pp
@@ -0,0 +1,38 @@
+class profile::openstack::base::pdns::auth::service(
+    $host = hiera('profile::openstack::base::pdns::host'),
+    $db_host = hiera('profile::openstack::base::pdns::db_host'),
+    $db_pass = hiera('profile::openstack::base::pdns::db_pass'),
+    ) {
+
+    class { '::pdns_server':
+        dns_auth_ipaddress     => $facts['ipaddress'],
+        dns_auth_query_address => $facts['ipaddress'],
+        dns_auth_soa_name      => $host,
+        pdns_db_host           => $db_host,
+        pdns_db_password       => $db_pass,
+    }
+
+    ferm::service { 'udp_dns_rec':
+        proto => 'udp',
+        port  => '53',
+    }
+
+    ferm::service { 'tcp_dns_rec':
+        proto => 'tcp',
+        port  => '53',
+    }
+
+    ferm::rule { 'skip_dns_conntrack-out':
+        desc  => 'Skip DNS outgoing connection tracking',
+        table => 'raw',
+        chain => 'OUTPUT',
+        rule  => 'proto udp sport 53 NOTRACK;',
+    }
+
+    ferm::rule { 'skip_dns_conntrack-in':
+        desc  => 'Skip DNS incoming connection tracking',
+        table => 'raw',
+        chain => 'PREROUTING',
+        rule  => 'proto udp dport 53 NOTRACK;',
+    }
+}
