8299803c7a965da939facbe79a57387651c0fa12
cpettet <rush@wikimedia.org>
2017-09-18 21:24:49 +0000

    openstack: pdns auth module/role/profile
    Rename module from labs_dns to pdns_server
    
    Bug: T171494
    Change-Id: Ib021f565b5c528dce8d94209b70d424ad7735e00

--- modules/pdns_server/manifests/init.pp
+++ modules/pdns_server/manifests/init.pp
@@ -1,11 +1,13 @@
-# dns.pp
+# pdns_server/init.pp
 #
 # Parameters:
 # - $dns_auth_ipaddress:IP address PowerDNS will bind to and send packets from
 # - $dns_auth_soa_name:DNS SOA name of the server
-# - $dns_auth_master:Which DNS server to use as "master" to fetch zones from
+# - $pdns_db_host:Database server backing PDNS
+# - $pdns_db_password:PDNS user database password
+# - $dns_auth_query_address: The IP address to use as a source address for sending queries.
 
-class labs_dns(
+class pdns_server(
     $dns_auth_ipaddress,
     $dns_auth_soa_name,
     $pdns_db_host,
@@ -20,17 +22,17 @@ class labs_dns(
 
     file { '/etc/powerdns/pdns.conf':
         ensure  => 'present',
-        require => Package['pdns-server'],
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
-        content => template('labs_dns/pdns.conf.erb'),
+        content => template('pdns_server/pdns.conf.erb'),
+        require => Package['pdns-server'],
     }
 
     # Clean up any example configs that the pdns packages might have installed;
     #  We don't want them accidentally used or merged into our puppetized config.
     file { '/etc/powerdns/pdns.d/':
-        ensure  => directory,
+        ensure  => 'directory',
         purge   => true,
         recurse => true,
     }
