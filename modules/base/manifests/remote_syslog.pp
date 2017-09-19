ede871b9f2b572943020364896fd762edef78a49
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-09-19 15:58:03 +0200

    base: ability to send syslog over TLS
    Also enable syslog over TLS in codfw for prometheus::ops as a test.
    
    Bug: T136312
    Change-Id: I190c036d111593e625333a29eb8676c43fa4be7b

--- modules/base/manifests/remote_syslog.pp
+++ modules/base/manifests/remote_syslog.pp
@@ -11,14 +11,27 @@
 #   A list of host (and optional port) to forward syslog events to.
 #   (e.g. ["syslog.eqiad.wmnet"] or ["deployment-logstash2.deployment-prep.eqiad.wmflabs:10514"])
 #
+# [*central_hosts_tls*]
+#   A list of host:port (port is *required*) to forward syslog using TLS.
+#   (e.g. ["syslog.eqiad.wmnet:6154"])
+#
 #
 class base::remote_syslog (
     $enable,
     $central_hosts = [],
+    $central_hosts_tls = [],
 ) {
     if $enable {
-        if empty($central_hosts) {
-            fail('::base::remote_syslog::central_hosts required')
+        require_package('rsyslog-gnutls')
+
+        if empty($central_hosts) and empty($central_hosts_tls) {
+            fail('::base::remote_syslog::central_hosts central_hosts_tls required')
+        }
+
+        if ! empty($central_hosts_tls) {
+            ::base::expose_puppet_certs { '/etc/rsyslog':
+                provide_private => true,
+            }
         }
 
         rsyslog::conf { 'remote_syslog':
