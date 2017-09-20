f69fecae05bf16a0f47e75796f3aa904d4ca7c25
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-09-20 13:21:40 +0200

    WMCS: fine tuning Cumin configuration for proxies
    * The proxies need to be able to proxy to themselves because the
      hostname is a private name and the only public one is the proxy
      service. Merge the two lists in this case.
    * Change ssh LogLevel from QUIET to ERROR to be able to see fatals.
    * Add options to ignore the host key also in the proxy command for now.
      We should investigate a way to enforce it but this requires a
      mechanism that is able to automatically update that value in the cumin
      master whenever it changes.
    
    Bug: T175712
    Change-Id: I97f339ce4c4e8dac5320c5be8865af7e10cec500

--- modules/profile/manifests/openstack/main/cumin/target.pp
+++ modules/profile/manifests/openstack/main/cumin/target.pp
@@ -3,7 +3,17 @@ class profile::openstack::main::cumin::target(
 ) {
     require ::network::constants
 
-    $ssh_authorized_sources = join($::network::constants::special_hosts[$::realm][$cumin_auth_group], ',')
+    if $cumin_auth_group == 'cumin_masters' {
+        $ssh_authorized_sources_list = $::network::constants::special_hosts[$::realm][$cumin_auth_group]
+    } else {
+        # Authorize both the default cumin masters and the custom config, required for proxies.
+        $ssh_authorized_sources_list = concat(
+            $::network::constants::special_hosts[$::realm]['cumin_masters'],
+            $::network::constants::special_hosts[$::realm][$cumin_auth_group],
+        )
+    }
+
+    $ssh_authorized_sources = join($ssh_authorized_sources_list, ',')
     $cumin_master_pub_key = secret('keyholder/cumin_openstack_master.pub')
 
     ssh::userkey { 'root-cumin':
