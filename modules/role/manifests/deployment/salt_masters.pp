785acc21122d500ac6d368e01b5751be040c642e
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-09-20 10:55:25 +0200

    Remove role::deployment::config and the related Hiera data
    Only used by Trebuchet.
    
    Change-Id: I15ade6e9070adbb769e24a7da6e2d14570727ec6

--- modules/role/manifests/deployment/salt_masters.pp
+++ modules/role/manifests/deployment/salt_masters.pp
@@ -5,25 +5,4 @@
 class role::deployment::salt_masters(
     $deployment_server = hiera('deployment_server', 'tin.eqiad.wmnet'),
 ) {
-
-    $deployment_config = {
-        'parent_dir' => '/srv/deployment',
-        'servers'    => {
-            'eqiad'  => $deployment_server,
-            'codfw'  => $deployment_server,
-        },
-        'redis'      => {
-            'host'                   => $deployment_server,
-            'port'                   => '6379',
-            'db'                     => '0',
-            'socket_connect_timeout' => '5',
-        },
-    }
-
-    class { '::role::deployment::config': }
-
-    class { 'deployment::salt_master':
-        repo_config       => $role::deployment::config::repo_config,
-        deployment_config => $deployment_config,
-    }
 }
