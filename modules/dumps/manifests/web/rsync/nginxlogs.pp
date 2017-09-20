fc1fcd00c344077818a8ab837d573f27ccac7d2e
Ariel T. Glenn <ariel@wikimedia.org>
2017-09-20 13:00:23 +0300

    Move datasets nginx logs rsync to dumps web manifest where it belongs
    Only the dumps web server will ever have these logs, keep this rsync
    job separate from the generic "gran this dataset" or "rsync these dumps"
    jobs
    
    Bug: T175528
    Change-Id: I08b8c024c1d901670795757351bcead8b78c8ccf

--- modules/dumps/manifests/web/rsync/nginxlogs.pp
+++ modules/dumps/manifests/web/rsync/nginxlogs.pp
@@ -1,25 +1,17 @@
-class dataset::cron::rsync::nginxlogs (
-    $enable = true,
+class dumps::web::rsync::nginxlogs (
     $dest   = undef,
-    $user   = 'root',
 )
 {
-    include ::dataset::common
-
-    $ensure = $enable ? {
-        true    => 'present',
-        default => 'absent',
-    }
+    require_package('rsync')
 
     $rsync_args = '-rt --perms --chmod=go+r --bwlimit=50000'
     cron { 'rsync_nginxlogs':
-        ensure      => $ensure,
-        user        => $user,
+        ensure      => 'present',
+        user        => 'root',
         minute      => 55,
         hour        => 4,
         command     => "/usr/bin/rsync ${rsync_args} /var/log/nginx/*.gz ${dest}",
         environment => 'MAILTO=ops-dumps@wikimedia.org',
-        require     => User[$user],
     }
 }
 
