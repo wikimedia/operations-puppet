98ceda279f88aabe9e54352457c9079a8561bce5
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-09-21 10:12:47 +0200

    service: remove trebuchet references
    Change-Id: I4b661c1b86897073b17f7f222798d031aa73b5da

--- modules/service/spec/defines/service_node_spec.rb
+++ modules/service/spec/defines/service_node_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper'
 
 describe 'service::node', :type => :define do
+  let(:pre_condition) {
+    ['class passwords::etcd { $accounts = {}}',
+     'include ::passwords::etcd',
+     'class profile::base {
+      $notifications_enabled = "1"
+}
+include ::profile::base'
+    ]
+  }
   let(:title) { 'my_service_name' }
   let(:facts) { { :initsystem => 'systemd' } }
-  let(:node_params) { {'cluster' => 'test', 'site' => 'eqiad'} }
-  let(:pre_condition) { """
-class profile::base {
-      $notifications_enabled = '1'
-}
-include ::profile::base
-""" }
+  let(:node_params) { {'cluster' => 'test', 'site' => 'eqiad', 'realm' => 'production'} }
   context 'when only port is given' do
     let(:params) { { :port => 1234 } }
 
-    it 'create application config file' do
-      is_expected.to contain_file('/etc/my_service_name/config.yaml')
-    end
-    it 'contains a service named after the application' do
-      is_expected.to contain_file('/etc/my_service_name/config.yaml')
-                         .with_content(/name: my_service_name/)
+    it { is_expected.to compile }
+
+    it 'create the appropriate scap target' do
+      is_expected.to contain_scap__target('my_service_name/deploy')
+                       .with_service_name('my_service_name')
     end
   end
 end
