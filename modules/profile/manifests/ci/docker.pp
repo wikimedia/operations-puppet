99d3a0c746fc1596a190218f73bde5062e7d9767
Antoine Musso <hashar@free.fr>
2017-09-21 17:37:50 +0200

    contint: docker-ce on labs docker slaves
    The production slaves (contint1001/contint2001) already have docker-ce
    via role::ci::slave.
    
    The labs slaves (integration-slave-docker) were are stuck docker has
    packaged by the distribution which are installed by profile::ci::docker.
    
    Move the docker-ce bits from role::ci::slave (prod) to
    profile::ci::docker. That switch labs instance to use docker-ce.
    
    Bug: T176267
    Change-Id: I1d08e8961020feaf7c21914fd8046fd033a47798

--- modules/profile/manifests/ci/docker.pp
+++ modules/profile/manifests/ci/docker.pp
@@ -2,16 +2,25 @@
 #
 # Configures a host to be a docker-backed Jenkins agent
 #
-
 class profile::ci::docker {
-    include ::docker
-
+    apt::repository { 'thirdparty-ci':
+        uri        => 'http://apt.wikimedia.org/wikimedia',
+        dist       => "${::lsbdistcodename}-wikimedia",
+        components => 'thirdparty/ci',
+    }
+    package { 'docker-ce':
+        ensure  => present,
+        require => [
+          Apt::Repository['thirdparty-ci'],
+          Exec['apt-get update']
+        ],
+    }
     # Ensure jenkins-deploy membership in the docker group
     exec { 'jenkins-deploy docker membership':
         unless  => '/usr/bin/id -Gn jenkins-deploy | /bin/grep -qw "docker"',
         command => '/usr/sbin/usermod -aG docker jenkins-deploy',
         require => [
-            Class['::docker'],
+            Package['docker-ce'],
         ],
     }
 }
