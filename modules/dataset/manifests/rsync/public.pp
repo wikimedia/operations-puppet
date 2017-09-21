4b5b979cd3a0f5ec89bc41f73448c6fda0b52b1e
Reedy <reedy@wikimedia.org>
2017-09-21 12:12:02 +0000

    Template-ise rsync/public.pp hosts allow
    Change-Id: I4ac3ddde00afc8b921b5b8846a8e657c0f3cae23

--- modules/dataset/manifests/rsync/public.pp
+++ modules/dataset/manifests/rsync/public.pp
@@ -6,14 +6,16 @@ class dataset::rsync::public($enable=true) {
         $ensure = 'absent'
     }
 
+    $hosts_allow = 'sagres.c3sl.ufpr.br odysseus.fi.muni.cz odysseus.linux.cz odysseus.ip6.fi.muni.cz poincare.acc.umu.se wikimedia.bytemark.co.uk'
+
     include ::dataset::common
     include ::dataset::rsync::common
     file { '/etc/rsyncd.d/20-rsync-dumps_to_public.conf':
-        ensure => $ensure,
-        mode   => '0444',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/dataset/rsync/rsyncd.conf.dumps_to_public',
-        notify => Exec['update-rsyncd.conf'],
+        ensure  => $ensure,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => template('dataset/rsync/rsyncd.conf.dumps_to_public'),
+        notify  => Exec['update-rsyncd.conf'],
     }
 }
