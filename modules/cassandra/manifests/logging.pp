abf193914783ae9f19516febec194e8db66f78fa
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-09-21 09:30:13 +0200

    Remove obsolete deprecation code
    This is absented since over a year, I think it's safe to remove now..
    
    Change-Id: I93c5475433021da8691d8018e7063618580cb878

--- modules/cassandra/manifests/logging.pp
+++ modules/cassandra/manifests/logging.pp
@@ -38,13 +38,4 @@ class cassandra::logging(
         target  => '/srv/deployment/cassandra/logstash-logback-encoder/lib/jackson-databind-2.4.0.jar',
         require => Scap::Target['cassandra/logstash-logback-encoder'],
     }
-
-    # removes unnecessary dependencies, deprecates T128787; the following code
-    # can be removed once it's run on each node note! the source dependency
-    # jar files needs to be removed from somewhere (maybe trebuchet?? I don't
-    # know where)
-    $useless = ['/usr/share/cassandra/lib/commons-compiler-2.7.8.jar', '/usr/share/cassandra/lib/janino-2.7.8.jar']
-    file { $useless:
-        ensure  => 'absent',
-    }
 }
