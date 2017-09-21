3d8523843282af9f33646ca816c3cd770b1e2a57
elukey <ltoscano@wikimedia.org>
2017-09-21 15:47:52 +0200

    role::analytics_cluster::druid::worker: introduce tlsproxy for druid
    The druid TLS proxy is a basic nginx server doing HTTP auth with
    a htpasswd file, created on the fly by puppet using private hiera
    data (that needs to be configured before this code review is merged).
    
    The proxy will be useful when Druid will expose data to services like
    AQS, outside the analytics network.
    
    The code is heavily inspired by the etcd::tlsproxy, so in the future
    the two code paths should be merged to utilize a generic profile (DRY).
    
    Bug: T176223
    Change-Id: Id80140136a8c404a844292102579347af0b03ac1

---
+++ modules/profile/manifests/druid/tlsproxy.pp
@@ -0,0 +1,47 @@
+# == Class profile::druid::tlsproxy
+# Configure a nginx reverse proxy in front of Druid in order to
+# implement a basic authn scheme.
+#
+class profile::druid::tlsproxy(
+    $cert_name = hiera('profile::druid::tlsproxy::cert_name'),
+    $acls      = hiera('profile::druid::tlsproxy::accounts'),
+    $salt      = hiera('profile::druid::tlsproxy::salt'),
+){
+    require ::tlsproxy::instance
+    require ::passwords::druid
+
+    $accounts = $::passwords::druid::accounts
+
+    sslcert::certificate { $cert_name:
+        skip_private => false,
+        before       => Service['nginx'],
+    }
+
+    file { '/etc/nginx/auth/':
+        ensure  => directory,
+        mode    => '0550',
+        owner   => 'www-data',
+        require => Package['nginx-full'],
+        before  => Service['nginx']
+    }
+
+    file { '/etc/nginx/etcd-errors':
+        ensure  => directory,
+        mode    => '0550',
+        owner   => 'www-data',
+        require => Package['nginx-full'],
+        before  => Service['nginx']
+    }
+
+    file { '/etc/nginx/auth/druid.htpasswd':
+        content => template('profile/druid/htpasswd.erb'),
+        owner   => 'www-data',
+        group   => 'www-data',
+        mode    => '0444',
+    }
+
+    nginx::site { 'etcd_tls_proxy':
+        ensure  => present,
+        content => template('profile/druid/tls_proxy.conf.erb'),
+    }
+}
