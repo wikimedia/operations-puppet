96b243e3f448d12c2050e5cec10512f422cbe21b
milimetric <dandreescu@wikimedia.org>
2017-09-22 08:08:57 -0400

    Add druid options to AQS config
    Change-Id: I5c78ce68e8351eb0c379e6f9d0109c3388b757a7

--- modules/aqs/manifests/init.pp
+++ modules/aqs/manifests/init.pp
@@ -43,22 +43,40 @@
 # [*statsd_port*]
 #   statsd port. Default: 8125
 #
+# [*druid_scheme*]
+#   druid scheme. Default: http
+#
+# [*druid_host*]
+#   druid host. Default: druid1004.eqiad.wmnet
+#
+# [*druid_port*]
+#   druid broker port. Default: 8082
+#
+# [*druid_query_path*]
+#   druid broker query path. Default: /druid/v2/
+#
+include ::passwords::aqs::druid_http_auth
 class aqs(
     $cassandra_user = 'cassandra',
     $cassandra_password = 'cassandra',
     $seeds          = [$::ipaddress],
     $cassandra_default_consistency = 'localQuorum',
-    $cassandra_local_dc = 'datacenter1',
-    $cassandra_datacenters = [ 'datacenter1' ],
-    $port           = 7232,
-    $salt_key       = 'secretkey',
-    $page_size      = 250,
-    $logstash_host  = 'localhost',
-    $logstash_port  = 12201,
-    $logging_level  = 'warn',
-    $statsd_host    = 'localhost',
-    $statsd_port    = 8125,
+    $cassandra_local_dc            = 'datacenter1',
+    $cassandra_datacenters         = [ 'datacenter1' ],
+    $port                          = 7232,
+    $salt_key                      = 'secretkey',
+    $page_size                     = 250,
+    $logstash_host                 = 'localhost',
+    $logstash_port                 = 12201,
+    $logging_level                 = 'warn',
+    $statsd_host                   = 'localhost',
+    $statsd_port                   = 8125,
+    $druid_scheme                  = 'http',
+    $druid_host                    = 'druid1004.eqiad.wmnet',
+    $druid_port                    = 8082,
+    $druid_query_path              = '/druid/v2/',
 ) {
+    # NOTE: didn't know how to make the ::passwords::aqs values, they're in the private repo, right?
 
     service::node { 'aqs':
         port            => $port,
