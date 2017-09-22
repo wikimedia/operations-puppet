0b09f4ea4b6db0ad6c52f737134150d4471ed271
Brandon Black <bblack@wikimedia.org>
2017-09-22 14:54:34 +0000

    Global: Turn off ethernet flow for all interfaces
    Most switches do not send flow control pause frames anyways, but
    they can honor ones sent by the host and start buffering up some
    of the host's inbound traffic in the switch.  This contributes to
    bufferbloat.  It's better to drop packets quickly and let the
    higher-layer protocols (e.g. TCP congestion control algs)
    intellgently detect this for throttling.
    
    Change-Id: I43dfa167df05408c1d42e9d32c00ddc1cc2895f2

---
+++ modules/interface/manifests/noflow.pp
@@ -0,0 +1,28 @@
+# Definition: interface::noflow
+#
+# Disable ethernet flow control at boot time via up-commands, and also disable
+# at runtime when first adding to boot time up-commands.
+#
+# Parameters:
+# - $interface=$name:
+#   The network interface to operate on
+define interface::noflow($interface=$name) {
+    # Command will fail on some hosts, depending on kernel/driver revs and/or
+    # ethernet hardware capabilities, in which case we don't care, hence ||:
+    $cmd = "ethtool -A ${interface} autoneg off tx off rx off ||:"
+
+    # Add to ifup commands in /etc/network/interfaces
+    interface::up_command { "noflow-${interface}":
+        interface => $interface,
+        command   => $cmd,
+    }
+
+    # Exec immediately at runtime when first added to interfaces file
+    exec { "noflow-${interface}":
+        path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+        command     => $cmd,
+        require     => Package['ethtool'],
+        subscribe   => Augeas["${interface}_noflow-${interface}"],
+        refreshonly => true,
+    }
+}
