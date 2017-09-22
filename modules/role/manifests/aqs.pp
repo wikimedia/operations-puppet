68021b8cddf4eeff8a2e29c417dad4f52a58a036
milimetric <dandreescu@wikimedia.org>
2017-09-22 08:08:57 -0400

    role::aqs: add druid options to AQS config and move aqs to profile
    Change-Id: I5c78ce68e8351eb0c379e6f9d0109c3388b757a7

--- modules/role/manifests/aqs.pp
+++ modules/role/manifests/aqs.pp
@@ -10,38 +10,12 @@ class role::aqs {
         description => 'Analytics Query Service Node',
     }
 
-    include ::passwords::aqs
-
     include ::standard
     include ::base::firewall
 
-    #
-    # Set up Cassandra for AQS.
-    #
+    require ::passwords::aqs
     include ::profile::cassandra
-
-    #
-    # Set up AQS
-    #
-
-    include ::aqs
-    include ::aqs::monitoring
+    include ::profile::aqs
 
     include role::lvs::realserver
-
-    ferm::service {'aqs_web':
-        proto => 'tcp',
-        port  => $::aqs::port,
-    }
-
-    #TODO: add monitoring once we figure out what metrics we want
-    #monitoring::graphite_threshold { 'restbase_analytics_<<some-metric-name>>':
-        #description   => 'Analytics RESTBase req/s returning 5xx http://grafana.wikimedia.org/#/dashboard/db/restbase',
-        #metric        => '<<the metric and any transformations>>',
-        #from          => '10min',
-        #warning       => '<<warning threshold>>', # <<explain>>
-        #critical      => '<<critical threshold>>', # <<explain>>
-        #percentage    => '20',
-        #contact_group => 'aqs-admins',
-    #}
 }
