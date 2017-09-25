9c98196d451a5fe3008c2af161f6504d67401a9a
elukey <ltoscano@wikimedia.org>
2017-09-25 12:29:35 +0200

    mediawiki::web::modules: force dependency between apache confs
    The following error keeps popping up in the first mw puppet runs:
    
    Error: Could not set 'file' on ensure: No such file or directory
    @ dir_s_rmdir - /etc/apache2/mods-available/setenvif.conf20170923-9288-1sb5ir9.lock
    at 53:/etc/puppet/modules/mediawiki/manifests/web/modules.pp
    
    Change-Id: I8d698918a45acc34c9863cd9c62a18495d1698b9

--- modules/mediawiki/manifests/web/modules.pp
+++ modules/mediawiki/manifests/web/modules.pp
@@ -32,40 +32,46 @@ class mediawiki::web::modules {
     }
 
     file { '/etc/apache2/mods-available/expires.conf':
-        ensure => present,
-        source => 'puppet:///modules/mediawiki/apache/modules/expires.conf',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        before => Class['::apache::mod::expires'],
+        ensure  => present,
+        source  => 'puppet:///modules/mediawiki/apache/modules/expires.conf',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        before  => Class['::apache::mod::expires'],
+        require => Package['apache2'],
     }
 
     file { '/etc/apache2/mods-available/autoindex.conf':
-        ensure => present,
-        source => 'puppet:///modules/mediawiki/apache/modules/autoindex.conf',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        before => Class['::apache::mod::autoindex'],
+        ensure  => present,
+        source  => 'puppet:///modules/mediawiki/apache/modules/autoindex.conf',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        before  => Class['::apache::mod::autoindex'],
+        require => Package['apache2'],
     }
 
 
     file { '/etc/apache2/mods-available/setenvif.conf':
-        ensure => present,
-        source => 'puppet:///modules/mediawiki/apache/modules/setenvif.conf',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        notify => Service['apache2'],
+        ensure  => present,
+        source  => 'puppet:///modules/mediawiki/apache/modules/setenvif.conf',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['apache2'],
+        before  => Class['::apache::mod::setenvif'],
+        require => Package['apache2'],
     }
 
     file { '/etc/apache2/mods-available/mime.conf':
-        ensure => present,
-        source => 'puppet:///modules/mediawiki/apache/modules/mime.conf',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        notify => Service['apache2'],
+        ensure  => present,
+        source  => 'puppet:///modules/mediawiki/apache/modules/mime.conf',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        notify  => Service['apache2'],
+        before  => Class['::apache::mod::mime'],
+        require => Package['apache2'],
     }
 
     # TODO: remove this? It's not used anywhere AFAICT
