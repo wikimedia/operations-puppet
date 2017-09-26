fd9a5350078d0971066cdc66434eb416b48a5e49
Ottomata <aotto@wikimedia.org>
2017-09-26 14:08:23 -0400

    Set up separate druid public-eqiad cluster.
    NOTE: It is safe to rename the 'druid-eqiad' Zookeeper cluster
    to 'druid-analytics-eqiad', because it is only used in puppet.
    There is nothing in zookeeper or druid configurations that
    refer to the Zookeeper cluster name.
    
    This also sets up a new Zookeeper cluster colocated on the
    druid public-eqiad nodes called 'druid-public-eqiad'.
    
    Bug: T176223
    Change-Id: I8624fbc402105cc44818a2ee8d0db7dbab3526ee

--- modules/role/manifests/analytics_cluster/hadoop/ferm/resourcemanager.pp
+++ modules/role/manifests/analytics_cluster/hadoop/ferm/resourcemanager.pp
@@ -1,61 +1,60 @@
 # == Class role::analytics_cluster::hadoop::ferm::resourcemanager
 #
 class role::analytics_cluster::hadoop::ferm::resourcemanager {
+    $ferm_srange = '(($ANALYTICS_NETWORKS $DRUID_PUBLIC_HOSTS))'
 
     ferm::service{ 'hadoop-yarn-resourcemanager-scheduler':
         proto  => 'tcp',
         port   => '8030',
-        srange => '$ANALYTICS_NETWORKS',
+        srange => $ferm_srange,
     }
 
     ferm::service{ 'hadoop-yarn-resourcemanager-tracker':
         proto  => 'tcp',
         port   => '8031',
-        srange => '$ANALYTICS_NETWORKS',
+        srange => $ferm_srange,
     }
 
     ferm::service{ 'hadoop-yarn-resourcemanager':
         proto  => 'tcp',
         port   => '8032',
-        srange => '$ANALYTICS_NETWORKS',
+        srange => $ferm_srange,
     }
 
     ferm::service{ 'hadoop-yarn-resourcemanager-admin':
         proto  => 'tcp',
         port   => '8033',
-        srange => '$ANALYTICS_NETWORKS',
+        srange => $ferm_srange,
     }
 
     ferm::service{ 'hadoop-yarn-resourcemanager-http-ui':
         proto  => 'tcp',
         port   => '8088',
-        srange => '$ANALYTICS_NETWORKS',
+        srange => $ferm_srange,
     }
 
     ferm::service{ 'hadoop-mapreduce-historyserver':
         proto  => 'tcp',
         port   => '10020',
-        srange => '$ANALYTICS_NETWORKS',
+        srange => $ferm_srange,
     }
 
     ferm::service{ 'hadoop-mapreduce-historyserver-admin':
         proto  => 'tcp',
         port   => '10033',
-        srange => '$ANALYTICS_NETWORKS',
+        srange => $ferm_srange,
     }
 
     ferm::service{ 'hadoop-mapreduce-historyserver-http-ui':
         proto  => 'tcp',
         port   => '19888',
-        srange => '$ANALYTICS_NETWORKS',
+        srange => $ferm_srange,
     }
 
     ferm::service{ 'hadoop-yarn-resourcemanager-jmx':
         proto  => 'tcp',
         port   => '9983',
-        srange => '$ANALYTICS_NETWORKS',
+        srange => $ferm_srange,
     }
-
-
 }
 
