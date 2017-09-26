b5ffd48b69f4c83cb6fcd7687f884bc4b61899d9
Ottomata <aotto@wikimedia.org>
2017-09-26 14:01:10 -0400

    Improvements to druid profiles, move druid role out of analytics_cluster
    This refactor will allow us to include different druid worker roles on different
    clusters, while still using the same profile classes.  Configuration
    is done using role worker.yaml hiera.
    
    This also refactors the way the HDFS deep storage directory is created,
    so that we can create multiple druid deep storage directories in
    HDFS for different Druid clusters.
    
    Bug: T176223
    Change-Id: I0887d4c5b010785cbf7db6c298b4d6f256778215

---
+++ modules/profile/manifests/druid/overlord.pp
@@ -0,0 +1,31 @@
+# Class: profile::druid::overlord
+#
+class profile::druid::overlord(
+    $properties         = hiera('profile::druid::overlord::properties'),
+    $env                = hiera('profile::druid::overlord::env'),
+    $monitoring_enabled = hiera('profile::druid::monitoring_enabled'),
+    $daemon_autoreload  = hiera('profile::druid::daemons_autoreload'),
+    $ferm_srange        = hiera('profile::druid::ferm_srange'),
+) {
+
+    # Druid overlord Service
+    class { '::druid::overlord':
+        properties       => $properties,
+        env              => $env,
+        should_subscribe => $daemon_autoreload,
+    }
+
+    ferm::service { 'druid-overlord':
+        proto  => 'tcp',
+        port   => $::druid::overlord::runtime_properties['druid.port'],
+        srange => $ferm_srange,
+    }
+
+    if $monitoring_enabled {
+        nrpe::monitor_service { 'druid-overlord':
+            description  => 'Druid overlord',
+            nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -C java -a \'io.druid.cli.Main server overlord\'',
+            critical     => false,
+        }
+    }
+}
