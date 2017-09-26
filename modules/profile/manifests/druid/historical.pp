9e6c2471b85d9ca2b89fc1b2d0dcf98a67eba165
Ottomata <aotto@wikimedia.org>
2017-09-26 14:01:10 -0400

    Improvements to druid profiles, move druid role out of analytics_cluster
    This refactor will allow us to include different druid worker roles on different
    clusters, while still using the same profile classes.  Configuration
    is done using role worker.yaml hiera.
    
    This also refactors the way the HDFS deep storage directory is created,
    so that we can create multiple druid deep storage directories in
    HDFS for different Druid clusters.
    
    Bug: T176223
    Change-Id: I0887d4c5b010785cbf7db6c298b4d6f256778215

---
+++ modules/profile/manifests/druid/historical.pp
@@ -0,0 +1,31 @@
+# Class: profile::druid::historical
+#
+class profile::druid::historical(
+    $properties         = hiera('profile::druid::historical::properties'),
+    $env                = hiera('profile::druid::historical::env'),
+    $monitoring_enabled = hiera('profile::druid::monitoring_enabled'),
+    $daemon_autoreload  = hiera('profile::druid::daemons_autoreload'),
+    $ferm_srange        = hiera('profile::druid::ferm_srange'),
+) {
+
+    # Druid historical Service
+    class { '::druid::historical':
+        properties       => $properties,
+        env              => $env,
+        should_subscribe => $daemon_autoreload,
+    }
+
+    ferm::service { 'druid-historical':
+        proto  => 'tcp',
+        port   => $::druid::historical::runtime_properties['druid.port'],
+        srange => $ferm_srange,
+    }
+
+    if $monitoring_enabled {
+        nrpe::monitor_service { 'druid-historical':
+            description  => 'Druid historical',
+            nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -C java -a \'io.druid.cli.Main server historical\'',
+            critical     => false,
+        }
+    }
+}
