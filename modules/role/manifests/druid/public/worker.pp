2308ab77d7e9a824783aacefa3ecca445c209fba
Ottomata <aotto@wikimedia.org>
2017-09-26 14:08:23 -0400

    Set up separate druid public-eqiad cluster.
    NOTE: It is safe to rename the 'druid-eqiad' Zookeeper cluster
    to 'druid-analytics-eqiad', because it is only used in puppet.
    There is nothing in zookeeper or druid configurations that
    refer to the Zookeeper cluster name.
    
    This also sets up a new Zookeeper cluster colocated on the
    druid public-eqiad nodes called 'druid-public-eqiad'.
    
    Bug: T176223
    Change-Id: I8624fbc402105cc44818a2ee8d0db7dbab3526ee

--- modules/role/manifests/druid/public/worker.pp
+++ modules/role/manifests/druid/public/worker.pp
@@ -1,16 +1,14 @@
-# Class: role::druid::analytics::worker
-# Sets up the Druid analytics cluster for internal use.
-# This cluster may contain data not suitable for
-# use in public APIs.
+# Class: role::druid::public::worker
+# Sets up the Druid public cluster for use with AQS and wikistats 2.0.
 #
-class role::druid::analytics::worker {
+class role::druid::public::worker {
     include ::profile::druid::broker
     include ::profile::druid::coordinator
     include ::profile::druid::historical
     include ::profile::druid::middlemanager
     include ::profile::druid::overlord
 
-    # Zookeeper is co-located on some analytics druid hosts, but not all.
+    # Zookeeper is co-located on some public druid hosts, but not all.
     if $::fqdn in $::profile::druid::common::zookeeper_hosts {
         include profile::zookeeper::server
         include profile::zookeeper::firewall
