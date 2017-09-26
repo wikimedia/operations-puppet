86154a656359ffd4163979441a8801e6a84d729c
Ariel T. Glenn <ariel@wikimedia.org>
2017-09-26 13:53:18 +0300

    Move dataset nfs server manifests to dump module
    We now use one class for any nfs service, with different profiles
    depending on whether the filesystem is exported to the snapshot hosts,
    stats hosts, or all of them. Local path of filesystem is now hardcoded
    in the profile; it could turn into a hiera value later.
    
    Bug: T175528
    Change-Id: Ia2d61583134863679cc723266fe9cf86b0988c59

--- modules/role/manifests/dataset/primary.pp
+++ modules/role/manifests/dataset/primary.pp
@@ -4,12 +4,6 @@
 # of content may or may not be generated here (but should all be eventually)
 # mirrors to the public should not be provided from here via rsync
 class role::dataset::primary {
-    include role::dataset::common
-
-    system::role { 'dataset::primary':
-        description => 'dataset primary host',
-    }
-
     class {'dataset':}
 }
 
