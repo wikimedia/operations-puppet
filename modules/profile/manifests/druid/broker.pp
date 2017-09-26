994731016687a92a35bc703402924364f2c0660d
Ottomata <aotto@wikimedia.org>
2017-09-26 14:01:10 -0400

    Improvements to druid profiles, move druid role out of analytics_cluster
    This refactor will allow us to include different druid worker roles on different
    clusters, while still using the same profile classes.  Configuration
    is done using role worker.yaml hiera.
    
    This also refactors the way the HDFS deep storage directory is created,
    so that we can create multiple druid deep storage directories in
    HDFS for different Druid clusters.
    
    Bug: T176223
    Change-Id: I0887d4c5b010785cbf7db6c298b4d6f256778215

---
+++ modules/profile/manifests/druid/broker.pp
@@ -0,0 +1,30 @@
+# Class: profile::druid::broker
+#
+class profile::druid::broker(
+    $properties  = hiera('profile::druid::broker::properties'),
+    $env         = hiera('profile::druid::broker::env'),
+    $ferm_srange = hiera('profile::druid::broker::ferm_srange')
+) {
+    require ::profile::druid::common
+
+    # Druid Broker Service
+    class { '::druid::broker':
+        properties       => $properties,
+        env              => $env,
+        should_subscribe => $::profile::druid::common::daemon_autoreload,
+    }
+
+    ferm::service { 'druid-broker':
+        proto  => 'tcp',
+        port   => $::druid::broker::runtime_properties['druid.port'],
+        srange => $ferm_srange,
+    }
+
+    if $::profile::druid::common::monitoring_enabled {
+        nrpe::monitor_service { 'druid-broker':
+            description  => 'Druid broker',
+            nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -C java -a \'io.druid.cli.Main server broker\'',
+            critical     => false,
+        }
+    }
+}
