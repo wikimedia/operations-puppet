4bb8675f404069c02090f2ca10f8c4c914e2c0fd
Ariel T. Glenn <ariel@wikimedia.org>
2017-09-26 13:53:18 +0300

    Move daaset nfs server manifests to dump module
    We now use one class for any nfs service, with different profiles
    depending on whether the mount goes to the snapshot hosts, stats
    hosts. or all of them. Local path of filesystem is now hardcoded
    in the profile; it could turn into a hiera value later.
    
    Bug: T175528
    Change-Id: Ia2d61583134863679cc723266fe9cf86b0988c59

--- modules/dataset/manifests/init.pp
+++ modules/dataset/manifests/init.pp
@@ -1,17 +1,4 @@
-class dataset(
-    # args:
-    #    $nfs: true to share data with snapshot hosts via nfs
-    $nfs     = true,
-    ) {
-
+class dataset{
     include ::dataset::common
     require ::dataset::user
-
-    if ($nfs) {
-        $nfs_enable = true
-    }
-    else {
-        $nfs_enable = false
-    }
-    class { '::dataset::nfs': enable => $nfs_enable }
 }
