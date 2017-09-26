0d65cd9e48c8cf8311bf4e4e91ac1a099f40b27a
Ottomata <aotto@wikimedia.org>
2017-09-26 14:01:10 -0400

    Improvements to druid profiles, move druid role out of analytics_cluster
    This refactor will allow us to include different druid worker roles on different
    clusters, while still using the same profile classes.  Configuration
    is done using role worker.yaml hiera.
    
    This also refactors the way the HDFS deep storage directory is created,
    so that we can create multiple druid deep storage directories in
    HDFS for different Druid clusters.
    
    Bug: T176223
    Change-Id: I0887d4c5b010785cbf7db6c298b4d6f256778215

---
+++ modules/profile/manifests/druid/middlemanager.pp
@@ -0,0 +1,40 @@
+# Class: profile::druid::middlemanager
+#
+class profile::druid::middlemanager(
+    $properties  = hiera('profile::druid::middlemanager::properties'),
+    $env         = hiera('profile::druid::middlemanager::env'),
+) {
+    require ::profile::druid::common
+
+    # Druid middlemanager Service
+    class { '::druid::middlemanager':
+        properties       => $properties,
+        env              => $env,
+        should_subscribe => $::profile::druid::common::daemon_autoreload,
+    }
+
+    ferm::service { 'druid-middlemanager':
+        proto  => 'tcp',
+        port   => $::druid::middlemanager::runtime_properties['druid.port'],
+        srange => $::profile::druid::common::ferm_srange,
+    }
+
+    # Allow incoming connections to druid.indexer.runner.startPort + 900
+    $peon_start_port = $::druid::middlemanager::runtime_properties['druid.indexer.runner.startPort']
+    $peon_end_port   = $::druid::middlemanager::runtime_properties['druid.indexer.runner.startPort'] + 900
+    ferm::service { 'druid-middlemanager-indexer-task':
+        proto  => 'tcp',
+        port   => "${peon_start_port}:${peon_end_port}",
+        srange => $::profile::druid::common::ferm_srange,
+    }
+
+    if $::profile::druid::common::monitoring_enabled {
+        # Special case for the middlemanager daemon: its correspondent Java
+        # process name is 'middleManager'
+        nrpe::monitor_service { 'druid-middlemanager':
+            description  => 'Druid middlemanager',
+            nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -C java -a \'io.druid.cli.Main server middleManager\'',
+            critical     => false,
+        }
+    }
+}
