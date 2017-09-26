c95e4cb51c8b3a610394c7e10c790ceaf9563cd2
Ariel T. Glenn <ariel@wikimedia.org>
2017-09-26 13:53:18 +0300

    Move daaset nfs server manifests to dump module
    We now use one class for any nfs service, with different profiles
    depending on whether the mount goes to the snapshot hosts, stats
    hosts. or all of them. Local path of filesystem is now hardcoded
    in the profile; it could turn into a hiera value later.
    
    Bug: T175528
    Change-Id: Ia2d61583134863679cc723266fe9cf86b0988c59

--- modules/dataset/manifests/init.pp
+++ modules/dataset/manifests/init.pp
@@ -1,17 +1,4 @@
 class dataset(
-    # args:
-    #    $nfs: true to share data with snapshot hosts via nfs
-    $nfs     = true,
-    ) {
-
     include ::dataset::common
     require ::dataset::user
-
-    if ($nfs) {
-        $nfs_enable = true
-    }
-    else {
-        $nfs_enable = false
-    }
-    class { '::dataset::nfs': enable => $nfs_enable }
 }
--- modules/dumps/manifests/nfs.pp
+++ modules/dumps/manifests/nfs.pp
@@ -1,10 +1,11 @@
-class dumps::generation::server::nfs(
+class dumps::nfs(
     $clients = undef,
     $statd_port = undef,
     $statd_out = undef,
     $lockd_udp = undef,
     $lockd_tcp = undef,
     $mountd_port = undef,
+    $path = undef,
 ) {
     file { '/etc/exports':
         mode    => '0444',
@@ -43,5 +44,55 @@ class dumps::generation::server::nfs(
 
     kmod::options { 'lockd':
         options => "nlm_udpport=${lockd_udp} nlm_tcpport=${lockd_tcp}",
+    }
+
+    include ::base::firewall
+    include ::network::constants
+
+    ferm::service { 'dumps_nfs':
+        proto  => 'tcp',
+        port   => '2049',
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    ferm::service { 'nfs_rpc_mountd':
+        proto  => 'tcp',
+        port   => $mountd_port,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    ferm::service { 'nfs_rpc_statd':
+        proto  => 'tcp',
+        port   => $statd_port,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    ferm::service { 'nfs_portmapper_udp':
+        proto  => 'udp',
+        port   => $portmapper_port,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    ferm::service { 'nfs_portmapper_tcp':
+        proto  => 'tcp',
+        port   => $portmapper_port,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    ferm::service { 'nfs_lockd_udp':
+        proto  => 'udp',
+        port   => $lockd_udp,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    ferm::service { 'nfs_lockd_tcp':
+        proto  => 'tcp',
+        port   => $lockd_tcp,
+        srange => '$PRODUCTION_NETWORKS',
+    }
+
+    monitoring::service { 'nfs':
+        description   => 'NFS',
+        check_command => 'check_tcp!2049',
     }
 }
