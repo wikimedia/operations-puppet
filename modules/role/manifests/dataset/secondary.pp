a9a0f76565eb74a43a38cee1a70828166309f3ec
Ariel T. Glenn <ariel@wikimedia.org>
2017-09-26 13:53:18 +0300

    Move daaset nfs server manifests to dump module
    We now use one class for any nfs service, with different profiles
    depending on whether the mount goes to the snapshot hosts, stats
    hosts. or all of them. Local path of filesystem is now hardcoded
    in the profile; it could turn into a hiera value later.
    
    Bug: T175528
    Change-Id: Ia2d61583134863679cc723266fe9cf86b0988c59

--- modules/role/manifests/dataset/secondary.pp
+++ modules/role/manifests/dataset/secondary.pp
@@ -2,11 +2,5 @@
 # number of directories (but best is not at all)
 # mirrors to the public should be provided from here via rsync
 class role::dataset::secondary {
-    include role::dataset::common
-
-    system::role { 'dataset::secondary':
-        description => 'dataset secondary host',
-    }
-
     class {'dataset':}
 }
--- modules/role/manifests/dumps/web/xmldumps_active.pp
+++ modules/role/manifests/dumps/web/xmldumps_active.pp
@@ -5,9 +5,9 @@ class role::dumps::web::xmldumps_active {
     include ::standard
     include ::profile::dumps::web::xmldumps_active
     include ::profile::dumps::web::rsync_server
-    include ::profile::dumps::nfs_server
     include ::profile::dumps::rsyncer
     include ::profile::dumps::fetcher
+    include ::profile::dumps::nfs::all
 
     system::role { 'role::dumps::web::xmldumps': description => 'active web, nfs and rsync server of xml/sql dumps' }
 }
