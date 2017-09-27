68a2f8d1eee9289d057e940026fbaac3c990cc1b
Antoine Musso <hashar@free.fr>
2017-09-27 21:50:47 +0200

    prometheus: make ferm DNS record type configurable
    Prometheus exports have a ferm rule that resolves hosts using AAAA DNS
    records. Labs lacks IPv6 and the class is installed on the
    deploymemt-memc* hosts among others which causes ferm to fail loading.
    
    Make it possible to change the record type used by ferm when resolving.
    Default remains 'AAAA'.
    
    Set it to 'A' for labs via hiera:
    
        profile::prometheus::dns_record_type: 'A'
    
    Bug: T153468
    Bug: T176314
    Change-Id: Ib58ecbd3a91dbcc0b7e212b643515d7f91e643fe

--- modules/profile/manifests/prometheus/apache_exporter.pp
+++ modules/profile/manifests/prometheus/apache_exporter.pp
@@ -1,9 +1,10 @@
 class profile::prometheus::apache_exporter (
     $prometheus_nodes = hiera('prometheus_nodes'),
+    $prometheus_dns_record_type = hiera('profile::prometheus::dns_record_type', 'AAAA'),
 ) {
     prometheus::apache_exporter { 'default': }
     $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
-    $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
+    $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), ${prometheus_dns_record_type}))"
 
     ferm::service { 'prometheus-apache_exporter':
         proto  => 'tcp',
