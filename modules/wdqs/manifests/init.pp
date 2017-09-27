aca9803533892957e49c681f0a560e493ca1fe0e
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-09-27 15:36:29 +0200

    wdqs: reduce blazegraph heap size to 10GB
    GC logs indicate that heap is still oversized. Let's reduce it to 10GB. We
    will probably need to reduce it still further.
    
    Bug: T175919
    Change-Id: I2b8011902f92389f54a81b4fc149483d0637f4e3

--- modules/wdqs/manifests/init.pp
+++ modules/wdqs/manifests/init.pp
@@ -24,7 +24,7 @@ class wdqs(
     $log_dir = '/var/log/wdqs',
     $endpoint = '',
     $blazegraph_options = '',
-    $blazegraph_heap_size = '12g',
+    $blazegraph_heap_size = '10g',
     $blazegraph_config_file = 'RWStore.properties',
 ) {
 
