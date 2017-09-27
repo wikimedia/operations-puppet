f57aac7ac0c396028bc872d62b754379641ba43b
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-09-27 12:02:00 +0200

    cumin (WMCS): allow to setup cumin in a project
    * Refactor Cumin's resources for WMCS Cloud VPS to allow to set up a
      Cumin master also inside an OpenStack project, without access to
      per-project secrets, and without requiring a standalone puppetmaster
      inside the project. Everything can be set in hiera in Horizon.
    * Keyholder: accept a new optional parameter with an absolute path to a
      local SSH private key, to be used when it cannot be stored in secret()
    
    Bug: T176314
    Change-Id: I045984c1b87ae4f5a111d0de9f92b498da700275

--- modules/profile/manifests/openstack/main/cumin/target.pp
+++ modules/profile/manifests/openstack/main/cumin/target.pp
@@ -1,24 +1,51 @@
+# == profile::openstack::main::cumin::master
+#
+# Profile to allow a Cumin master for WMCS or a specific Cloud VPS project to
+# connect to this Cloud VPS instance.
+#
+# === Hiera Parameters required for a project-specific Cumin target
+#
+# [*profile::openstack::main::cumin::project_masters*]
+#   An array with the list of IPs of the Cumin master(s)
+#
+# [*profile::openstack::main::cumin::project_pub_key*]
+#   The SSH public key used by Cumin master
+#
 class profile::openstack::main::cumin::target(
-    $cumin_auth_group = hiera('profile::openstack::main::cumin_auth_group'),
+    $auth_group = hiera('profile::openstack::main::cumin::auth_group'),
+    $project_masters = hiera('profile::openstack::main::cumin::project_masters'),
+    $project_pub_key = hiera('profile::openstack::main::cumin::project_pub_key'),
 ) {
     require ::network::constants
 
-    if $cumin_auth_group == 'cumin_masters' {
-        $ssh_authorized_sources_list = $::network::constants::special_hosts[$::realm][$cumin_auth_group]
+    validate_array($project_masters)
+
+    if $auth_group == 'cumin_masters' {
+        $ssh_authorized_sources_list = $::network::constants::special_hosts[$::realm][$auth_group]
     } else {
         # Authorize both the default cumin masters and the custom config, required for proxies.
         $ssh_authorized_sources_list = concat(
             $::network::constants::special_hosts[$::realm]['cumin_masters'],
-            $::network::constants::special_hosts[$::realm][$cumin_auth_group])
+            $::network::constants::special_hosts[$::realm][$auth_group])
     }
 
     $ssh_authorized_sources = join($ssh_authorized_sources_list, ',')
-    $cumin_master_pub_key = secret('keyholder/cumin_openstack_master.pub')
+    $ssh_project_authorized_sources = join($project_masters, ',')
+    $ssh_project_ferm_sources = join($project_masters, ' ')
+    $pub_key = secret('keyholder/cumin_openstack_master.pub')
 
     ssh::userkey { 'root-cumin':
         ensure  => present,
         user    => 'root',
         skey    => 'cumin',
         content => template('profile/openstack/main/cumin/userkey.erb'),
+    }
+
+    if $ssh_project_ferm_sources != '' {
+        ::ferm::service { 'ssh-from-cumin-project-masters':
+            proto  => 'tcp',
+            port   => '22',
+            srange => "(${ssh_project_ferm_sources})",
+        }
     }
 }
