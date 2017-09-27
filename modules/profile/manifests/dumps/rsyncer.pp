116448fbb4045a27b59f37b9259def9cd2b5f34e
Sam Reed <reedy@wikimedia.org>
2017-09-27 16:32:34 +0300

    Template-ise rsync/public.pp hosts allow
    Any of the public mirrors for dumps is permitted to access any of the
    shares, they are all public after all.  We simply configure them for the
    convenience of the mirrors.
    
    This moves most of the hostnames out to a profile parameter. Still a bit
    more to do.
    
    Change-Id: I4ac3ddde00afc8b921b5b8846a8e657c0f3cae23

--- modules/profile/manifests/dumps/rsyncer.pp
+++ modules/profile/manifests/dumps/rsyncer.pp
@@ -1,9 +1,12 @@
-class profile::dumps::rsyncer {
+class profile::dumps::rsyncer(
+    $rsync_clients = hiera('dumps_web_rsync_server_clients')
+) {
     class {'::dumps::rsync::default':}
     class {'::dumps::rsync::media':}
     class {'::dumps::rsync::memfix':}
     class {'::dumps::rsync::pagecounts_ez':}
     class {'::dumps::rsync::peers':}
     class {'::dumps::rsync::phab_dump':}
-    class {'::dumps::rsync::public':}
+    $hosts_allow = flatten($rsync_clients)
+    class {'::dumps::rsync::public': 'hosts_allow' => $hosts_allow}
 }
