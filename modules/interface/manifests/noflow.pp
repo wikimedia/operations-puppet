111f056284b1bae1981093c9ebf04588a97e944f
Brandon Black <bblack@wikimedia.org>
2017-09-27 15:53:19 +0000

    Global: runtime disable ethernet flow on fresh install
    This adds runtime execution of the ethtool command to disable flow control when
    the interfaces file is first modified for it.  Assuming the previous change is
    applied fully on the fleet first, this should not cause runtime execution on
    existing hosts, but puts it in place for future fresh installs on their initial
    puppetization.
    
    Change-Id: I1dfc4ee6b9781bb32cae025a2d4773d9871ddde0

--- modules/interface/manifests/noflow.pp
+++ modules/interface/manifests/noflow.pp
@@ -1,6 +1,8 @@
 # Definition: interface::noflow
 #
-# Disable ethernet flow control at boot time via up-commands.
+# Disable ethernet flow control at boot time via up-commands, and also at
+# runtime when first adding to boot time up-commands, so that it's applied on
+# freshly-installed hosts without another reboot required.
 #
 # Parameters:
 # - $interface=$name:
@@ -14,5 +16,14 @@ define interface::noflow($interface=$name) {
     interface::up_command { "noflow-${interface}":
         interface => $interface,
         command   => $cmd,
+    }
+
+    # Exec immediately at runtime when first added to interfaces file
+    exec { "noflow-${interface}":
+        path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+        command     => $cmd,
+        require     => Package['ethtool'],
+        subscribe   => Augeas["${interface}_noflow-${interface}"],
+        refreshonly => true,
     }
 }
