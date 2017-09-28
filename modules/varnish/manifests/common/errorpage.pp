06840697550b62197093ccf5066034690d050170
Timo Tijhof <krinklemail@gmail.com>
2017-09-28 19:06:56 +0100

    errorpage: Migrate from back-compat wmf.png to wmf-logo.png
    See also 4e0c6d66a2489 in operations/mediawiki-config.
    
    Since the redesign of the Wikimedia Foundation logo, the standard
    rendering is now rectangular (given the mandatory title line)
    instead of square.
    
    wmf.png is a back-compat symlink to wmf-square.png which has added
    padding to keep its absolute dimensions 135x135px. However, given
    that this error page doesn't assume height (yet), we can migrate
    to the new variant without issue.
    
    In the next commit, I'll make the height explicit so that the
    browser doesn't need to reflow the text around the image after it
    loads.
    
    Change-Id: Ic18ee6fa7cab29ce8f3f9f164027f9306d68da8f

--- modules/varnish/manifests/common/errorpage.pp
+++ modules/varnish/manifests/common/errorpage.pp
@@ -3,8 +3,8 @@ class varnish::common::errorpage {
         title       => 'Wikimedia Error',
         pagetitle   => 'Error',
         logo_link   => 'https://www.wikimedia.org',
-        logo_src    => 'https://www.wikimedia.org/static/images/wmf.png',
-        logo_srcset => 'https://www.wikimedia.org/static/images/wmf-2x.png 2x',
+        logo_src    => 'https://www.wikimedia.org/static/images/wmf-logo.png',
+        logo_srcset => 'https://www.wikimedia.org/static/images/wmf-logo-2x.png 2x',
         logo_alt    => 'Wikimedia',
         content     => template('varnish/errorpage.body.html.erb'),
         # Placeholder "%error%" substituted at runtime in errorpage.inc.vcl
