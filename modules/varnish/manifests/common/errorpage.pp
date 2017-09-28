49435d9023efba0f8a52ce219b3222958997db46
Timo Tijhof <krinklemail@gmail.com>
2017-09-28 19:16:51 +0100

    errorpage: Set explicit height on logo image
    This prevents the browser from having to do a reflow for the text
    around the image after it loads the image.
    
    Change-Id: I81db36b4ed1cdb40be4b46dc0aa8d0ed78f6e223

--- modules/varnish/manifests/common/errorpage.pp
+++ modules/varnish/manifests/common/errorpage.pp
@@ -5,6 +5,8 @@ class varnish::common::errorpage {
         logo_link   => 'https://www.wikimedia.org',
         logo_src    => 'https://www.wikimedia.org/static/images/wmf-logo.png',
         logo_srcset => 'https://www.wikimedia.org/static/images/wmf-logo-2x.png 2x',
+        logo_width  => '135',
+        logo_height => '101',
         logo_alt    => 'Wikimedia',
         content     => template('varnish/errorpage.body.html.erb'),
         # Placeholder "%error%" substituted at runtime in errorpage.inc.vcl
