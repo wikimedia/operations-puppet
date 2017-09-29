19df645aa670cbcd68a531f64abd5fd8124b8b1b
Daniel Zahn <dzahn@wikimedia.org>
2017-09-29 09:02:34 -0700

    releases: add missing Jenkins proxy setup
    The releases class is using Jenkins but it was missing the
    setup to proxy jenkins behind Apache. Copying that (partially) from
    the existing contint class, but avoiding use of files from another
    module, so including it right here in the releases module.
    
    Bug: T164030
    Change-Id: Iea229458d73e0ccf32584c2adbb46480e423fe38

---
+++ modules/releases/manifests/proxy_jenkins.pp
@@ -0,0 +1,34 @@
+# == Class releases::proxy_jenkins
+#
+# A http proxy in front of Jenkins
+#
+# [*http_port*]
+# HTTP port for a Jenkins instance webservice. Example: 8080
+#
+# [*prefix*]
+# The HTTP path used to reach the Jenkins instance. Must have a leading slash.
+# Example: /ci
+class releases::proxy_jenkins (
+    $http_port,
+    $prefix,
+) {
+
+  include ::contint::proxy_common
+
+  # run jenkins behind Apache and have pretty URLs / proxy port 80
+  # https://wiki.jenkins-ci.org/display/JENKINS/Running+Jenkins+behind+Apache
+
+  file {
+    '/etc/apache2/conf.d/jenkins_proxy':
+      ensure => absent,
+  }
+
+  file {
+    '/etc/apache2/jenkins_proxy':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0444',
+      content => template('releases/proxy_jenkins.erb'),
+  }
+
+}
