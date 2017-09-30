02587d339252d5b0cb87f22391fd443623dbde05
Ariel T. Glenn <ariel@wikimedia.org>
2017-09-30 12:49:25 +0300

    move hardcoded refs to francium, dumps.wm.o out of dumps manifests to profiles
    Change-Id: Iad2ee2040297702649fdeeae5392d22152cd6449

--- modules/profile/manifests/dumps/web/xmldumps_active.pp
+++ modules/profile/manifests/dumps/web/xmldumps_active.pp
@@ -1,10 +1,12 @@
 class profile::dumps::web::xmldumps_active {
     class {'::dumps::web::xmldumps_active':
-        do_acme   => hiera('do_acme'),
-        datadir   => '/data/xmldatadumps',
-        publicdir => '/data/xmldatadumps/public',
-        otherdir  => '/data/xmldatadumps/public/other',
-        logs_dest => 'stat1005.eqiad.wmnet::srv/log/webrequest/archive/dumps.wikimedia.org/',
+        do_acme          => hiera('do_acme'),
+        datadir          => '/data/xmldatadumps',
+        publicdir        => '/data/xmldatadumps/public',
+        otherdir         => '/data/xmldatadumps/public/other',
+        logs_dest        => 'stat1005.eqiad.wmnet::srv/log/webrequest/archive/dumps.wikimedia.org/',
+        htmldumps_server => 'francium.eqiad.wmnet',
+        xmldumps_server  => 'dumps.wikimedia.org',
     }
     # copy dumps and other datasets to fallback host(s) and to labs
     class {'::dumps::copying::peers':}
