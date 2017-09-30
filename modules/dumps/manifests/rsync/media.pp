f6705687e8d42e734e25f8798c9fd01269e81143
Ariel T. Glenn <ariel@wikimedia.org>
2017-09-30 12:13:51 +0300

    move hardcoded references to stats hosts from dumps module to profiles
    Bug: T175528
    Change-Id: I21747a2ccf8d99ee675b949bd4525595dcc22892

--- modules/dumps/manifests/rsync/media.pp
+++ modules/dumps/manifests/rsync/media.pp
@@ -1,12 +1,14 @@
-class dumps::rsync::media {
+class dumps::rsync::media(
+    $hosts_allow = undef,
+) {
     include ::dumps::rsync::common
 
     file { '/etc/rsyncd.d/30-rsync-media.conf':
-        ensure => 'present',
-        mode   => '0444',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/dumps/rsync/rsyncd.conf.media',
-        notify => Exec['update-rsyncd.conf'],
+        ensure  => 'present',
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => template('dumps/rsync/rsyncd.conf.media'),
+        notify  => Exec['update-rsyncd.conf'],
     }
 }
