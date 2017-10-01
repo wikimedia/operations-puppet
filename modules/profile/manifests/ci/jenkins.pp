7c3a0c3ee88b05f39b40b0ca60b79f291f9a9091
Antoine Musso <hashar@free.fr>
2017-10-01 21:25:25 +0200

    contint: move jenkins from role to a profile
    Change-Id: Idc6b8dc97f5662e3c8ae9bc8fb5d0b6baef33327

---
+++ modules/profile/manifests/ci/jenkins.pp
@@ -0,0 +1,63 @@
+# [*jenkins_prefix*]
+# The HTTP path used to reach the Jenkins instance. Must have a leading slash.
+# Default: '/ci'.
+#
+class profile::ci::jenkins(
+    $prefix = hiera('profile::ci::jenkins::prefix'),
+    $service_ensure = hiera('profile::ci::jenkins::service_ensure'),
+    $service_enable = hiera('profile::ci::jenkins::service_enable'),
+    $service_monitor = hiera('profile::ci::jenkins::monitor'),
+) {
+    # Load the Jenkins module, that setup a Jenkins master
+    class { '::jenkins':
+        access_log     => true,
+        http_port      => '8080',
+        prefix         => $prefix,
+        umask          => '0002',
+        service_ensure => $service_ensure,
+        service_enable => $service_enable,
+    }
+    class { '::contint::proxy_jenkins':
+        http_port => '8080',
+        prefix    => $prefix,
+    }
+
+    # Nodepool spawns non ephemeral slaves which causes config-history plugin
+    # to fill up entries until it reaches the limit of 32k inodes. T126552
+    cron { 'tidy_jenkins_ephemeral_nodes_configs':
+        ensure      => present,
+        environment => 'MAILTO=jenkins-bot@wikimedia.org',
+        user        => 'jenkins',
+        command     => "/usr/bin/find /var/lib/jenkins/config-history/nodes -path '/var/lib/jenkins/config-history/nodes/ci-*' -mmin +60 -delete > /dev/null 2>&1",
+        minute      => '35',
+        hour        => '*',
+    }
+
+    # Templates for Jenkins plugin Email-ext.  The templates are hosted in
+    # the repository integration/jenkins.git, so link to there.
+    file { '/var/lib/jenkins/email-templates':
+        ensure => directory,
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
+    file { '/var/lib/jenkins/email-templates/wikimedia.template':
+        source  => 'puppet:///modules/contint/jenkins-email-template',
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        require => File['/var/lib/jenkins/email-templates'],
+    }
+
+    # TODO/FIXME: remove hiera condition once T150771 is resolved
+    # and jenkins service is running active/active in both DCs
+    # aware that there should not be permanent hiera lookups in role
+    # should also be converted to profile/role anyways (T162822)
+    if $service_monitor {
+        nrpe::monitor_service { 'jenkins_zmq_publisher':
+            description   => 'jenkins_zmq_publisher',
+            contact_group => 'contint',
+            nrpe_command  => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 8888 --timeout=2',
+        }
+    }
+}
--- modules/role/manifests/ci/master.pp
+++ modules/role/manifests/ci/master.pp
@@ -8,78 +8,21 @@
 #
 # CI test server as per T79623
 #
-# [*jenkins_prefix*]
-# The HTTP path used to reach the Jenkins instance. Must have a leading slash.
-# Default: '/ci'.
-#
 # filtertags: labs-project-ci-staging
-class role::ci::master(
-    $jenkins_prefix = '/ci'
-) {
+class role::ci::master {
 
     system::role { 'ci::master': description => 'CI Jenkins master' }
 
     include ::standard
     include ::contint::firewall
 
+    include ::profile::ci::jenkins
     include ::profile::ci::slave
     include ::profile::ci::website
     include ::profile::zuul::merger
-
-    # Load the Jenkins module, that setup a Jenkins master
-    class { '::jenkins':
-        access_log => true,
-        http_port  => '8080',
-        prefix     => $jenkins_prefix,
-        umask      => '0002',
-    }
-    class { '::contint::proxy_jenkins':
-        http_port => '8080',
-        prefix    => $jenkins_prefix,
-    }
 
     # Backups
     include ::profile::backup::host
     backup::set {'var-lib-jenkins-config': }
     backup::set { 'contint' : }
-
-    # Nodepool spawn non ephemeral slaves which causes config-history plugin to
-    # fill up entries until it reaches the limit of 32k inodes. T126552
-    cron { 'tidy_jenkins_ephemeral_nodes_configs':
-        ensure      => present,
-        environment => 'MAILTO=jenkins-bot@wikimedia.org',
-        user        => 'jenkins',
-        command     => "/usr/bin/find /var/lib/jenkins/config-history/nodes -path '/var/lib/jenkins/config-history/nodes/ci-*' -mmin +60 -delete > /dev/null 2>&1",
-        minute      => '35',
-        hour        => '*',
-    }
-
-    # TODO/FIXME: remove hiera condition once T150771 is resolved
-    # and jenkins service is running active/active in both DCs
-    # aware that there should not be permanent hiera lookups in role
-    # should also be converted to profile/role anyways (T162822)
-    if hiera('jenkins::service_monitor', true) {
-        nrpe::monitor_service { 'jenkins_zmq_publisher':
-            description   => 'jenkins_zmq_publisher',
-            contact_group => 'contint',
-            nrpe_command  => '/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p 8888 --timeout=2',
-        }
-    }
-
-    # Templates for Jenkins plugin Email-ext.  The templates are hosted in
-    # the repository integration/jenkins.git, so link to there.
-    file { '/var/lib/jenkins/email-templates':
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
-    }
-    file { '/var/lib/jenkins/email-templates/wikimedia.template':
-        source  => 'puppet:///modules/contint/jenkins-email-template',
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        require => File['/var/lib/jenkins/email-templates'],
-    }
-
 }
