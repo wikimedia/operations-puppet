ff98031a3df683b4fdc27adc41b65abee093b051
Ariel T. Glenn <ariel@wikimedia.org>
2017-10-02 16:49:07 +0300

    move hardcoded phab hostname out of rsync config for dataset hosts
    Bug: T175528
    Change-Id: I42644844a0add87f5bfa342336457e55ddea8bc6

--- modules/profile/manifests/dumps/rsyncer.pp
+++ modules/profile/manifests/dumps/rsyncer.pp
@@ -3,12 +3,13 @@ class profile::dumps::rsyncer(
 ) {
     $stats_hosts = 'stat1005.eqiad.wmnet stat1006.eqiad.wmnet'
     $peer_hosts = 'dataset1001.wikimedia.org ms1001.wikimedia.org'
+    $phab_hosts = 'phab1001.eqiad.wmnet'
     class {'::dumps::rsync::default':}
     class {'::dumps::rsync::media': hosts_allow => $stats_hosts}
     class {'::dumps::rsync::memfix':}
     class {'::dumps::rsync::pagecounts_ez': hosts_allow => $stats_hosts}
     class {'::dumps::rsync::peers': hosts_allow => $peer_hosts}
-    class {'::dumps::rsync::phab_dump':}
+    class {'::dumps::rsync::phab_dump': hosts_allow => $phab_hosts}
     $hosts_allow = join(concat($rsync_clients['ipv4']['external'], $rsync_clients['ipv6']['external']), ' ')
     class {'::dumps::rsync::public': hosts_allow => $hosts_allow,}
 }
