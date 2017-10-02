6a04c2b92efa783b305b178e8bf45c98fd49ad48
Ariel T. Glenn <ariel@wikimedia.org>
2017-10-02 15:03:38 +0300

    move hardcoded hostnames out of script for rsync of dumps to peers
    Change-Id: Id742ff4ff551db154c6de0802b2b00ccb1980654

--- modules/dumps/manifests/copying/peers.pp
+++ modules/dumps/manifests/copying/peers.pp
@@ -1,4 +1,6 @@
-class dumps::copying::peers {
+class dumps::copying::peers(
+    $serverinfo = undef,
+) {
     file { '/usr/local/bin/rsync-dumps.py':
         ensure => 'present',
         mode   => '0755',
@@ -11,7 +13,7 @@ class dumps::copying::peers {
     cron { 'rsync-dumps':
         ensure  => 'present',
         # filter out error messages about vanishing files, we don't want email for that
-        command => '/usr/bin/python /usr/local/bin/rsync-dumps.py 2>&1 | grep -v "vanished" ',
+        command => "/usr/bin/python /usr/local/bin/rsync-dumps.py --servers '${serverinfo}' 2>&1 | grep -v vanished",
         user    => 'root',
         minute  => '0',
         hour    => '*/2',
