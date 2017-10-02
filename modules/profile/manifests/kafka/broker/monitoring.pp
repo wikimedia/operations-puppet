e61625344ce4fb6ee07b7e7245f10d3e0172cd3a
Ottomata <aotto@wikimedia.org>
2017-10-02 10:55:35 -0400

    Quote Prometheus query label values in kafka broker monitoring
    Otherwise Prometheus query will fail parsing
    
    Bug: T381489
    Change-Id: I9c4619408af5055b31ff681b8697addd3fa14e1c

--- modules/profile/manifests/kafka/broker/monitoring.pp
+++ modules/profile/manifests/kafka/broker/monitoring.pp
@@ -59,7 +59,7 @@ class profile::kafka::broker::monitoring (
     }
 
     # Prometheus labels for this Kafka Broker instance
-    $prometheus_labels = "cluster=${cluster},instance=${::hostname}:${prometheus_jmx_exporter_port},job=jmx_kafka"
+    $prometheus_labels = "cluster=\"${cluster}\",instance=\"${::hostname}:${prometheus_jmx_exporter_port}\",job=\"jmx_kafka\""
 
     # Alert on the average number of under replicated partitions over the last 30 minutes.
     # https://grafana.wikimedia.org/dashboard/db/prometheus-kafka?panelId=29&fullscreen
@@ -68,7 +68,7 @@ class profile::kafka::broker::monitoring (
         query          => "scalar(avg_over_time(kafka_server_replicamanager_underreplicatedpartitions{${prometheus_labels}}[30m]))",
         warning        => 5,
         critical       => 10,
-        prometheus_url => "http://prometheus.svc.${::site}.wmnet/ops",
+        prometheus_url => "http://prometheus.svc.${::site}.wmnet/ops/",
     }
 
     # Alert on the average max replica lag over the last 30 minutes.
@@ -78,6 +78,6 @@ class profile::kafka::broker::monitoring (
         query          => "scalar(avg_over_time(kafka_server_replicafetchermanager_maxlag{${prometheus_labels}}[30m]))",
         warning        => $replica_maxlag_warning,
         critical       => $replica_maxlag_critical,
-        prometheus_url => "http://prometheus.svc.${::site}.wmnet/ops",
+        prometheus_url => "http://prometheus.svc.${::site}.wmnet/ops/",
     }
 }
