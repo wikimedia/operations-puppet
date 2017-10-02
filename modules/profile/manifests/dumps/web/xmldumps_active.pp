e7723d895720c7497d5729505aecb8a400a00d6c
Ariel T. Glenn <ariel@wikimedia.org>
2017-10-02 15:03:38 +0300

    move hardcoded hostnames out of script for rsync of dumps to peers
    Change-Id: Id742ff4ff551db154c6de0802b2b00ccb1980654

--- modules/profile/manifests/dumps/web/xmldumps_active.pp
+++ modules/profile/manifests/dumps/web/xmldumps_active.pp
@@ -8,7 +8,15 @@ class profile::dumps::web::xmldumps_active {
         htmldumps_server => 'francium.eqiad.wmnet',
         xmldumps_server  => 'dumps.wikimedia.org',
     }
-    # copy dumps and other datasets to fallback host(s) and to labs
-    class {'::dumps::copying::peers':}
+    # copy dumps and other datasets between host(s)
+    $primaryserver = 'dataset1001.wikimedia.org'
+    $secondaryserver = 'ms1001.wikimedia.org'
+    $publicsourceinfo = "source=public,server=${primaryserver},type=primary;source=public,server=${secondaryserver},type=secondary"
+    $othersourceinfo = "source=other,server=${primaryserver},type=primary;source=other,server=${secondaryserver},type=secondary"
+    class {'::dumps::copying::peers':
+      serverinfo => "${publicsourceinfo} ${othersourceinfo}"
+    }
+
+    # copy dumps and other datasets to labs
     class {'::dumps::copying::labs':}
 }
