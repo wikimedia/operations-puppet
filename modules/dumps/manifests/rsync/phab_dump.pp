81f231be2e86334359ed7a963d989a45245de00f
Ariel T. Glenn <ariel@wikimedia.org>
2017-10-02 16:49:07 +0300

    move hardcoded phab hostname out of rsync config for dataset hosts
    Bug: T175528
    Change-Id: I42644844a0add87f5bfa342336457e55ddea8bc6

--- modules/dumps/manifests/rsync/phab_dump.pp
+++ modules/dumps/manifests/rsync/phab_dump.pp
@@ -1,11 +1,13 @@
-class dumps::rsync::phab_dump {
+class dumps::rsync::phab_dump(
+    $hosts_allow = undef,
+) {
     include ::dumps::rsync::common
     file { '/etc/rsyncd.d/40-rsync-phab_dump.conf':
         ensure => 'present',
         mode   => '0444',
         owner  => 'root',
         group  => 'root',
-        source => 'puppet:///modules/dumps/rsync/rsyncd.conf.phab_dump',
+        content => template('dumps/rsync/rsyncd.conf.phab_dump.erb'),
         notify => Exec['update-rsyncd.conf'],
     }
 }
