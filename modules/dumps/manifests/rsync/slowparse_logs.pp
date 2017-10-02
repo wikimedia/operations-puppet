74b59818dd29baea60ba1a66ffc4d56cda7fc0ba
Ariel T. Glenn <ariel@wikimedia.org>
2017-10-02 18:13:47 +0300

    move hardcoded mwlog hostnames from dump rsync manifests to profile
    Bug: T175528
    Change-Id: I5a5495b06232f22da9a736e2f7d7c2647790ef08

---
+++ modules/dumps/manifests/rsync/slowparse_logs.pp
@@ -0,0 +1,13 @@
+class dumps::rsync::slowparse_logs(
+    $hosts_allow = undef,
+) {
+    include ::dumps::rsync::common
+    file { '/etc/rsyncd.d/10-rsync-slowparse-logs.conf':
+        ensure  => 'present',
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => template('dumps/rsync/rsyncd.conf.slowparse_logs.erb'),
+        notify  => Exec['update-rsyncd.conf'],
+    }
+}
