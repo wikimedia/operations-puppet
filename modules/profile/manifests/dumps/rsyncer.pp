b44ed705d9b4a519dd220502f10bc67e951b7d1a
Ariel T. Glenn <ariel@wikimedia.org>
2017-10-02 18:13:47 +0300

    move hardcoded mwlog hostnames from dump rsync manifests to profile
    Bug: T175528
    Change-Id: I5a5495b06232f22da9a736e2f7d7c2647790ef08

--- modules/profile/manifests/dumps/rsyncer.pp
+++ modules/profile/manifests/dumps/rsyncer.pp
@@ -4,6 +4,7 @@ class profile::dumps::rsyncer(
     $stats_hosts = 'stat1005.eqiad.wmnet stat1006.eqiad.wmnet'
     $peer_hosts = 'dataset1001.wikimedia.org ms1001.wikimedia.org'
     $phab_hosts = 'phab1001.eqiad.wmnet'
+    $mwlog_hosts = 'mwlog1001.eqiad.wmnet 2620:0:861:103:1618:77ff:fe33:4ac0 mwlog2001.codfw.wmnet 2620:0:860:103:1618:77ff:fe4e:3159'
     class {'::dumps::rsync::default':}
     class {'::dumps::rsync::media': hosts_allow => $stats_hosts}
     class {'::dumps::rsync::memfix':}
@@ -12,4 +13,5 @@ class profile::dumps::rsyncer(
     class {'::dumps::rsync::phab_dump': hosts_allow => $phab_hosts}
     $hosts_allow = join(concat($rsync_clients['ipv4']['external'], $rsync_clients['ipv6']['external']), ' ')
     class {'::dumps::rsync::public': hosts_allow => $hosts_allow,}
+    class {'::dumps::rsync::slowparse_logs': hosts_allow => $mwlog_hosts}
 }
