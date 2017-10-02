841470285d565c3dee5d420f24d064dbf583339d
Ariel T. Glenn <ariel@wikimedia.org>
2017-10-02 23:41:24 +0300

    move remaining hardcoded paths for base dir of misc dumps out to profile
    Bug: T175528
    Change-Id: I51d7a447d20d37ad3355355449d45fc26a016d90

--- modules/profile/manifests/dumps/fetcher.pp
+++ modules/profile/manifests/dumps/fetcher.pp
@@ -1,11 +1,16 @@
 class profile::dumps::fetcher {
+    $publicdir = '/data/xmldatadumps/public'
+    $otherdir = '/data/xmldatadumps/public/other'
+
     class {'dumps::web::fetches::kiwix':
-        user  => 'datasets',
-        group => 'datasets',
+        user      => 'datasets',
+        group     => 'datasets',
+        publicdir => $publicdir,
+        otherdir  => $otherdir,
     }
     class {'dumps::web::fetches::stats':
         src      => 'stat1005.eqiad.wmnet::hdfs-archive',
-        otherdir => '/data/xmldatadumps/public/other',
+        otherdir => $otherdir,
         user     => 'datasets',
     }
     class {'dumps::web::fetches::wikitech_dumps':
