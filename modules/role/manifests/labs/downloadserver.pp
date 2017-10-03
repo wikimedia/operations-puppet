46079810bacbc4a61be586e41abf919552f26360
andrewbogott <abogott@wikimedia.org>
2017-10-03 17:00:14 -0500

    Add a role for a trivial file download server
    Bug: T177293
    Change-Id: I42e593cb9048162826cc6c07ac77a69189996039

---
+++ modules/role/manifests/labs/downloadserver.pp
@@ -0,0 +1,21 @@
+# Simple file server for the 'download' project
+#
+# filtertags: labs-project-download
+class role::labs::downloadserver {
+    labs_lvm::volume { 'srv':
+        mountat => '/srv',
+    }
+
+    file { '/srv/public_files':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        require => Labs_lvm::Volume['srv'],
+    }
+
+    nginx::site { 'downloadserver':
+        source => 'puppet:///modules/role/labs/downloadserver.nginx',
+        require => Labs_lvm::Volume['srv'],
+    }
+}
