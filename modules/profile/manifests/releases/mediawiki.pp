1c9d206928a107b86dfac1559c4e58aa56471814
Daniel Zahn <dzahn@wikimedia.org>
2017-10-03 13:06:20 -0700

    releases: drop /ci/ suffix for jenkins-proxy, unify templates
    Don't have 2 separate config file snippets for Jenkins Proxy setup,
    put it all into one template.
    
    Drop the "/ci/" suffix, now that this has a separate virtual host
    we can use /. Also fixes monitoring alert for / being forbidden
    currently.
    
    Bug: T164030
    Change-Id: I5b829e5fc22bd8d94a65ba92a7395b7f88c1208a

--- modules/profile/manifests/releases/mediawiki.pp
+++ modules/profile/manifests/releases/mediawiki.pp
@@ -7,13 +7,13 @@ class profile::releases::mediawiki (
     class { '::jenkins':
         access_log => true,
         http_port  => '8080',
-        prefix     => '/ci',
+        prefix     => '/',
         umask      => '0002',
     }
 
     class { '::releases::proxy_jenkins':
         http_port => '8080',
-        prefix    => '/ci',
+        prefix    => '/',
     }
 
     class { '::releases':
