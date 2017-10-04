8d055fd9e359bce5cc964f58ac5020f8d866da68
andrewbogott <abogott@wikimedia.org>
2017-10-04 01:28:32 -0500

    diskspace.py: add some new flavors
    Change-Id: I4e8ddced82746eb68d2038153d6087e3f3c86546

--- modules/openstack2/files/liberty/admin_scripts/novastats/diskspace.py
+++ modules/openstack2/files/liberty/admin_scripts/novastats/diskspace.py
@@ -27,8 +27,8 @@ import os.path
 import collections
 
 
-ONEGIG = 1024*1024*1024
-ONEMEG = 1024*1024
+ONEGIG = 1024 * 1024 * 1024
+ONEMEG = 1024 * 1024
 
 clients = mwopenstackclients.clients()
 
@@ -44,8 +44,11 @@ flavordict['101'] = '40'
 flavordict['bb5bf060-cdbb-4448-b436-a015ae2d4aaf'] = '160'
 flavordict['8af1f1cc-d95f-4380-bf10-bcfa0321b10f'] = '60'
 flavordict['2d59cc0d-538c-4bbd-b975-8e696a4f7207'] = '80'
-flavordict['cc0f1723-38d7-42da-aa2c-cef28d5f4250'] = '300'
 flavordict['deea3460-069e-44c7-98ca-ae30bb0de772'] = '80'
+flavordict['cc0f1723-38d7-42da-aa2c-cef28d5f4250'] = '300'
+flavordict['7447b146-eb66-4ecd-b8c9-ecf480fc6fd1'] = '300'
+flavordict['6f43bc6c-c91e-4b4a-8981-dd1d06ec1bb7'] = '300'
+flavordict['21e9047d-a60f-499d-b7f5-51f83ddf3611'] = '300'
 
 
 def printstat(string, alert=False):
