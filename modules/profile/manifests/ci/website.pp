0bc3ed509c672274fbc7909a246486396c76c5f0
Antoine Musso <hashar@free.fr>
2017-10-04 11:20:35 +0200

    contint::website to a profile
    It was coupled with the apache module.
    Instantiate module classes instead of including them.
    Also reindent to use 4 spaces.
    
    Change-Id: Iade3d76f9e5ed00f400f7acf98701756299f0132

--- modules/profile/manifests/ci/website.pp
+++ modules/profile/manifests/ci/website.pp
@@ -5,5 +5,70 @@
 # http://integration.mediawiki.org/
 # http://integration.wikimedia.org/
 class profile::ci::website {
-    class { 'contint::website': }
+    # Need to send Vary: X-Forwarded-Proto since most sites are forced to HTTPS
+    # and behind a varnish cache. See also T62822
+    class { '::apache::mod::headers': }
+    class { '::apache::mod::rewrite': }
+
+    # Apache configuration for integration.wikimedia.org
+    apache::site { 'integration.wikimedia.org':
+        content => template('contint/apache/integration.wikimedia.org.erb'),
+    }
+
+    # Apache configuration for integration.mediawiki.org
+    apache::site { 'integration.mediawiki.org':
+        content => template('contint/apache/integration.mediawiki.org.erb'),
+    }
+
+    # Apache configuration for doc.wikimedia.org
+    apache::site { 'doc.wikimedia.org':
+        content => template('contint/apache/doc.wikimedia.org.erb'),
+    }
+
+    # Static files in these docroots are in integration/docroot.git
+
+    file { '/srv/org':
+        ensure => directory,
+        mode   => '0775',
+        owner  => 'jenkins-slave',
+        group  => 'jenkins-slave',
+    }
+
+    file { '/srv/org/wikimedia':
+        ensure => directory,
+        mode   => '0775',
+        owner  => 'jenkins-slave',
+        group  => 'jenkins-slave',
+    }
+    file { '/srv/org/wikimedia/integration':
+        ensure => directory,
+        mode   => '0775',
+        owner  => 'jenkins-slave',
+        group  => 'jenkins-slave',
+    }
+    # MediaWiki code coverage
+    file { '/srv/org/wikimedia/integration/coverage':
+        ensure => directory,
+        mode   => '0775',
+        owner  => 'jenkins-slave',
+        group  => 'jenkins-slave',
+    }
+
+    # Jenkins console logs
+    file { '/srv/org/wikimedia/integration/logs':
+        ensure => directory,
+        mode   => '0775',
+        owner  => 'jenkins-slave',
+        group  => 'jenkins-slave',
+    }
+
+    # Written to by jenkins for automatically generated
+    # documentations
+    file { '/srv/org/wikimedia/doc':
+        ensure => directory,
+        mode   => '0775',
+        owner  => 'jenkins-slave',
+        group  => 'jenkins-slave',
+    }
+
 }
