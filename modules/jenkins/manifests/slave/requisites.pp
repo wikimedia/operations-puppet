5d05fba19f3e5dc2f0bd09dc4c6c9aa3f7104be1
Antoine Musso <hashar@free.fr>
2017-10-04 21:12:12 +0200

    jenkins: switch to Java8
    The next Jenkins version requires Java 8 and we thus have to switch to
    it before upgrading.  We were stuck to Java 7 since we had slaves on
    Trusty which lacked Java 8 but they have all been decommissioned.
    
    master (::jenkins) and slaves (::jenkins::slave::requisites) are
    switched to java 8, be it on Jessie or Stretch. Thus remove the stretch
    conditional.
    
    The Jenkins master is driven by systemd which uses /usr/bin/java and the
    slaves use /usr/bin/java to start. Thus select the Java 8 alternative
    for /usr/bin/java
    
    Add a spec to ensure the catalog pass when an host has both master and
    slave classes.
    
    NOTE: for other tools, one would have to manually run:
        update-java-alternatives --set java-1.8.0-openjdk-amd64
    
    The reason it is done manually is that there is no good way to list all
    the alternatives and the version they point to.  That does not affect
    the service though, setting the alternative is merely a convenience for
    interactive use.
    
    Bug: T162828
    Change-Id: I7f761484f092e3c04ee6d8069ecbb653248cb85f

--- modules/jenkins/manifests/slave/requisites.pp
+++ modules/jenkins/manifests/slave/requisites.pp
@@ -3,14 +3,14 @@
 # Dependency for the Jenkins agent on slaves
 #
 class jenkins::slave::requisites() {
+    ensure_packages('openjdk-8-jre-headless')
 
-    if os_version('debian >= stretch') {
-        $jdk_version = '8'
-    } else {
-        $jdk_version = '7'
+    # When a slave happen to have another jre installed, make sure 8 is the
+    # default.  Might have been defined when the host is also a master.
+    if ! defined(Alternatives::Select['java']) {
+        alternatives::select { 'java':
+            path    => '/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java',
+            require => Package['openjdk-8-jre-headless'],
+        }
     }
-
-    $jdk_package = "openjdk-${jdk_version}-jre-headless"
-
-    ensure_packages($jdk_package)
 }
