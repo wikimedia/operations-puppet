d38a51970b4562cd3d0396809398e28e3a3df177
andrewbogott <abogott@wikimedia.org>
2017-10-04 09:15:02 -0500

    wikitech-static: convert yaml content to wikitext when dumping wikitech
    We don't have anything on wikitech-static that can read yaml.
    
    Bug: T177450
    Change-Id: I36aa5b6d29c9229cd7b60332cc9fcffc952fe1de

--- modules/openstack/files/mw-xml.sh
+++ modules/openstack/files/mw-xml.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /srv/mediawiki/php
-/usr/local/bin/mwscript maintenance/dumpBackup.php labswiki --current --uploads | nice -n 19 gzip -9 > /a/backup/public/labswiki-$(date '+%Y%m%d').xml.gz
+/usr/local/bin/mwscript maintenance/dumpBackup.php labswiki --current --uploads | sed 's/<model>yaml<\/model>/<model>wikitext<\/model>/g' | sed 's/<format>application\/yaml<\/format>/<format>text\/x-wiki<\/format>/g' | nice -n 19 gzip -9 > /a/backup/public/labswiki-$(date '+%Y%m%d').xml.gz
 cd -
