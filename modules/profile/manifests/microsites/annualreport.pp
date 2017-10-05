b3d66bc9a5af65efbdad630cfe77e35136cb9356
Daniel Zahn <dzahn@wikimedia.org>
2017-10-04 20:33:29 -0700

    annualreport: rm module, merge into profile, fix style
    Change-Id: I381a9714e2bcaf996072679d57de27dd1fec0404

--- modules/profile/manifests/microsites/annualreport.pp
+++ modules/profile/manifests/microsites/annualreport.pp
@@ -1,16 +1,30 @@
+# sets up the WMF annual report site
 # https://annual.wikimedia.org/
-# microsite for the WMF annual report
 # http://wikimediafoundation.org/wiki/Annual_Report
+# T599 - https://15.wikipedia.org (aka. annual report 2015)
 class profile::microsites::annualreport {
 
-    include ::annualreport
-
     include ::base::firewall
+    include ::apache
+    include ::apache::mod::headers
 
     ferm::service { 'annualreport_http':
         proto => 'tcp',
         port  => '80',
     }
 
+    apache::site { 'annual.wikimedia.org':
+        source => 'puppet:///modules/profile/annualreport/annual.wikimedia.org',
+    }
+
+    apache::site { '15.wikipedia.org':
+        source => 'puppet:///modules/profile/annualreport/15.wikipedia.org',
+    }
+
+    git::clone { 'wikimedia/annualreport':
+        ensure    => 'latest',
+        directory => '/srv/org/wikimedia/annualreport',
+        branch    => 'master',
+    }
 }
 
