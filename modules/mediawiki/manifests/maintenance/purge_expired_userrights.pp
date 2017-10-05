51e0a5551223bd440e511a938ced6a2016f91a43
Eddie Greiner-Petter <git@eddie-sh.de>
2017-10-06 01:36:09 +0200

    [DNM] Add cron job for expired userrights maintenance script
    ---
    DO NOT MERGE YET!
    The maintenance script for this will go out with the train 1.31wmf3,
    which means (if I understand it correctly) we can't expect the
    maintenance script to be present for all wikis before Fri, 13 Oct.
    Thus this should not be merged before Mon, 16 Oct.
    ---
    
    fdb4557 added a new maintenance script to mediawiki, which will move
    expired userrights from user_groups to user_former_groups table. This
    patch schedules a cronjob to run this maintenance script on 14th and 28th
    each month for all wikis.
    
    Bug: T176754
    Change-Id: I56a2d1771e0b87085eda53f58ed7b11023e987d1

---
+++ modules/mediawiki/manifests/maintenance/purge_expired_userrights.pp
@@ -0,0 +1,10 @@
+class mediawiki::maintenance::purge_expired_userrights( $ensure = present ) {
+    cron { 'purge_expired_userrights':
+        ensure   => $ensure,
+        command  => '/usr/local/bin/foreachwiki maintenance/purgeExpiredUserrights.php >/dev/null 2>&1',
+        user     => $::mediawiki::users::web,
+        monthday => [14, 28],
+        hour     => 6,
+        minute   => 42,
+    }
+}
