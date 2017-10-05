c6f9bcabd16a8ae60613db56d2839f2f813570f2
Daniel Zahn <dzahn@wikimedia.org>
2017-10-04 21:13:43 -0700

    start profile for wikiba.se web hosting
    Bug: T99531
    Change-Id: I3f9ca490788a52513ed4b89e17430706615a40f4

---
+++ modules/profile/manifests/microsites/wikibase.pp
@@ -0,0 +1,28 @@
+# https://wikiba.se (T99531)
+class profile::microsites::wikibase {
+
+    class { '::base::firewall': }
+ 
+    ferm::service { 'wikibase_http':
+        proto => 'tcp',
+        port  => '80',
+    }
+
+    class { '::apache': }
+    class { '::apache::mod::headers': }
+
+    apache::site { 'wikiba.se':
+        content => template('profile/wikibase/apache-wikibase.erb'),
+    }
+
+    ensure_resource('file', '/srv/org', {'ensure' => 'directory' })
+    ensure_resource('file', '/srv/org/wikibase', {'ensure' => 'directory' })
+
+    git::clone { 'wikibase/wikibase.se':
+        ensure    => 'latest', # TODO: talk about latest vs. present
+        directory => '/srv/org/wikibase',
+        branch    => 'master', # are we using master to deploy?
+    }
+
+}
+
