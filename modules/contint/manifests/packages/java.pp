de88fc6dbbe54c07c76df40e620911a94d3cf6a1
Paladox <thomasmulhall410@yahoo.com>
2017-10-05 20:34:43 +0000

    contint: Set java 8 as default
    Also removes java 7 as we doint use trusty any more in ci.
    
    Change-Id: Iacc0140f353ca498dd4d970912bcfbd10c5a2f87

--- modules/contint/manifests/packages/java.pp
+++ modules/contint/manifests/packages/java.pp
@@ -1,10 +1,11 @@
 class contint::packages::java {
 
-    if os_version('debian == jessie') {
-        require_package('openjdk-7-jdk')
-    }
-
     require_package('openjdk-8-jdk')
+
+    alternatives::select { 'java':
+        path    => '/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java',
+        require => Package['openjdk-8-jdk'],
+    }
 
     package { 'maven2':
         ensure => present,
