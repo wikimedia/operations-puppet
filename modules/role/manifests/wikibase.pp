4443fef0a2fd34fc76b60c024cf25f535aadc501
Daniel Zahn <dzahn@wikimedia.org>
2017-10-04 21:13:43 -0700

    start profile for wikiba.se web hosting
    Bug: T99531
    Change-Id: I3f9ca490788a52513ed4b89e17430706615a40f4

---
+++ modules/role/manifests/wikibase.pp
@@ -0,0 +1,11 @@
+# wikiba.se (T99531)
+class role::wikibase {
+
+    include ::standard
+
+    include ::profile::microsites::wikibase
+
+    system::role { 'wikibase':
+        description => 'https://wikiba.se'
+    }
+}
