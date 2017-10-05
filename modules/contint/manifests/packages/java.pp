34501982f474502ab1e7665218a7961fa21fffc6
Paladox <thomasmulhall410@yahoo.com>
2017-10-05 20:34:43 +0000

    contint: Set java 8 as default
    Change-Id: Iacc0140f353ca498dd4d970912bcfbd10c5a2f87

--- modules/contint/manifests/packages/java.pp
+++ modules/contint/manifests/packages/java.pp
@@ -1,10 +1,10 @@
 class contint::packages::java {
 
+    require_package('openjdk-8-jdk')
+
     if os_version('debian == jessie') {
         require_package('openjdk-7-jdk')
     }
-
-    require_package('openjdk-8-jdk')
 
     package { 'maven2':
         ensure => present,
