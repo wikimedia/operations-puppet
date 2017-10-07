d1213307ad8ad9f7995971f2920692488e50746b
Daniel Zahn <dzahn@wikimedia.org>
2017-10-06 20:35:52 -0700

    ganglia: delete the module
    We have stopped using Ganglia across the board.
    
    All the role classes are gone, uranium uses role(spare),
    aggregators are removed from install/bastion servers and
    the ganglia.wikimedia.org name has been deleted from DNS.
    
    Bug: T177225
    Change-Id: I72587e3fb90346f8171bddbd0304f0704d60bad5

--- modules/ganglia/manifests/monitor/packages.pp
+++
@@ -1,9 +0,0 @@
-class ganglia::monitor::packages {
-    require_package('ganglia-monitor')
-
-    file { ['/usr/lib/ganglia/python_modules',
-            '/etc/ganglia/conf.d']:
-        ensure  => directory,
-        require => Package['ganglia-monitor'],
-    }
-}
