690d973d6eccda567be8d9b7e22d26d4bf7f6da9
Daniel Zahn <dzahn@wikimedia.org>
2017-10-06 20:20:45 -0700

    hiera/wmflib: drop ganglia_clusters variable entirely?
    Bug: T177225
    Change-Id: I33a999acf54b598d33dcbe9ecb3340b0ffccae64

--- modules/wmflib/lib/puppet/parser/functions/get_clusters.rb
+++ modules/wmflib/lib/puppet/parser/functions/get_clusters.rb
@@ -24,7 +24,8 @@ module Puppet::Parser::Functions
   newfunction(:get_clusters, :type => :rvalue) do |args|
     all = {}
     # Ganglia config is the source of truth about clusters/site
-    cluster_config = function_hiera(['ganglia_clusters', {}])
+    # cluster_config = function_hiera(['ganglia_clusters', {}])
+    # FIXME: Add new source of truth?
 
     # Arguments are an hash of selectors
     selector ||= {}
