0d94d14c7d5907ab3e28be179ebff57fcda19281
Daniel Zahn <dzahn@wikimedia.org>
2017-10-06 19:19:49 -0700

    hhvm: remove ganglia monitoring
    Bug: T177225
    Change-Id: I4df1fab26fe661affe274640782c050d6b96b310

--- modules/hhvm/manifests/monitoring.pp
+++ modules/hhvm/manifests/monitoring.pp
@@ -1,6 +1,6 @@
 # == Class: hhvm::monitoring
 #
-# Provisions Ganglia metric-gathering modules for HHVM.
+# Provisions Diamond collector for HHVM.
 #
 class hhvm::monitoring {
     include ::standard
@@ -9,51 +9,5 @@ class hhvm::monitoring {
 
     diamond::collector { 'HhvmApc':
         source  => 'puppet:///modules/hhvm/monitoring/hhvm_apc.py',
-    }
-
-    if $::standard::has_ganglia {
-
-        include ::ganglia
-
-        file { '/usr/lib/ganglia/python_modules/hhvm_mem.py':
-            source  => 'puppet:///modules/hhvm/monitoring/hhvm_mem.py',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0555',
-            require => Package['ganglia-monitor'],
-        }
-
-        file { '/etc/ganglia/conf.d/hhvm_mem.pyconf':
-            source  => 'puppet:///modules/hhvm/monitoring/hhvm_mem.pyconf',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            require => File['/usr/lib/ganglia/python_modules/hhvm_mem.py'],
-            notify  => Service['ganglia-monitor'],
-        }
-
-    }
-
-    ## Health statistics
-
-    if $::standard::has_ganglia {
-
-        file { '/usr/lib/ganglia/python_modules/hhvm_health.py':
-            source  => 'puppet:///modules/hhvm/monitoring/hhvm_health.py',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0555',
-            require => Package['ganglia-monitor'],
-        }
-
-        file { '/etc/ganglia/conf.d/hhvm_health.pyconf':
-            source  => 'puppet:///modules/hhvm/monitoring/hhvm_health.pyconf',
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0444',
-            require => File['/usr/lib/ganglia/python_modules/hhvm_health.py'],
-            notify  => Service['ganglia-monitor'],
-        }
-
     }
 }
