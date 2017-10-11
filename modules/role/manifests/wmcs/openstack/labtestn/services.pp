e9067fa6acd5b6776896fd9807a380d1eb026b39
cpettet <rush@wikimedia.org>
2017-10-11 16:41:35 -0500

    openstack: remove pdns auth things for labtestservices200[23]
    Bug: T171494
    Change-Id: I2a7d460668c5a9582e1387b9cb16c63d6a417a86

--- modules/role/manifests/wmcs/openstack/labtestn/services.pp
+++ modules/role/manifests/wmcs/openstack/labtestn/services.pp
@@ -1,6 +1,3 @@
 class role::wmcs::openstack::labtestn::services {
     include ::profile::openstack::labtestn::cloudrepo
-    include ::profile::openstack::labtestn::pdns::auth::db
-    include ::profile::openstack::labtestn::pdns::auth::service
-    include ::profile::openstack::labtestn::designate::service
 }
