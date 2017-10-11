dc88452b9f710af3e46d30effdb6381efe5c2781
Emanuele Rocca <ema@wikimedia.org>
2017-10-11 16:42:44 +0200

    varnish: remove varnishlog.py
    Since we have ported all varnish python modules to
    cachestats.CacheStatsSender, we do not need varnishlog.py and
    varnishapi.py (its dependency) anymore.
    
    Change-Id: I903422f3ffe6082dc6bd0aac677fa6ec8974e58c

--- modules/varnish/manifests/logging/xcache.pp
+++ modules/varnish/manifests/logging/xcache.pp
@@ -27,19 +27,17 @@ define varnish::logging::xcache(
     include ::varnish::common
 
     file { '/usr/local/bin/varnishxcache':
-        source  => 'puppet:///modules/varnish/varnishxcache',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0555',
-        require => File['/usr/local/lib/python2.7/dist-packages/varnishlog.py'],
-        notify  => Service['varnishxcache'],
+        source => 'puppet:///modules/varnish/varnishxcache',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        notify => Service['varnishxcache'],
     }
 
     systemd::service { 'varnishxcache':
         ensure         => present,
         content        => systemd_template('varnishxcache'),
         require        => File['/usr/local/bin/varnishxcache'],
-        subscribe      => File['/usr/local/lib/python2.7/dist-packages/varnishlog.py'],
         service_params => {
             require => Service['varnish-frontend'],
             enable  => true,
