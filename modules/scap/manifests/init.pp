7e6402ff41f2ca5987f656ffd75d4b758b800e00
Tyler Cipriani <tcipriani@wikimedia.org>
2017-10-12 10:33:13 -0600

    scap: upgrade to 3.7.1-1
    Bug: T127762
    Change-Id: Ie839cdb380a17d3450e225acde6d6406de048447

--- modules/scap/manifests/init.pp
+++ modules/scap/manifests/init.pp
@@ -12,7 +12,7 @@
 class scap (
     $deployment_server = 'deployment',
     $wmflabs_master = 'deployment-tin.deployment-prep.eqiad.wmflabs',
-    $version = '3.7.0-1',
+    $version = '3.7.1-1',
 ) {
     package { 'scap':
         ensure => $version,
