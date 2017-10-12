4209ee6d7c789eea3d3595072234f13faa58ef72
cpettet <rush@wikimedia.org>
2017-10-12 19:26:39 +0000

    openstack: pdns recursor module/profile/role
    * This deprecates the needed per host values
      for labservices1002
    * use 'secondary' for primary/secondary active/active
      pairs and 'standby' for primary/standby active/standby
      pairs
    * couple recursor monitoring with recursor profile
    
    Bug: T171494
    Change-Id: I1055766185d5381a666bbd97976e80eb7a681837

---
+++ modules/role/manifests/wmcs/openstack/main/services_primary.pp
@@ -0,0 +1,8 @@
+class role::wmcs::openstack::main::services_primary {
+    system::role { $name: }
+    include ::profile::openstack::main::cloudrepo
+    include ::profile::openstack::main::pdns::auth::db
+    include ::profile::openstack::main::pdns::auth::service
+    include ::profile::openstack::main::pdns::recursor::primary
+    include ::profile::openstack::main::designate::service
+}
--- modules/role/manifests/wmcs/openstack/main/services_secondary.pp
+++ modules/role/manifests/wmcs/openstack/main/services_secondary.pp
@@ -1,6 +1,8 @@
-class role::wmcs::openstack::main::services {
+class role::wmcs::openstack::main::services_secondary {
+    system::role { $name: }
     include ::profile::openstack::main::cloudrepo
     include ::profile::openstack::main::pdns::auth::db
     include ::profile::openstack::main::pdns::auth::service
+    include ::profile::openstack::main::pdns::recursor::secondary
     include ::profile::openstack::main::designate::service
 }
