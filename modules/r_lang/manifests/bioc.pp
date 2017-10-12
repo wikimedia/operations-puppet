19b51bd95289d93ef6638936cc484bf11334e7b1
Mikhail Popov <mpopov@wikimedia.org>
2017-10-12 13:06:49 -0700

    Add profiles/roles for stats/ML on Wikimedia Cloud
    Makes the following roles available:
     - discovery::forecaster
         uses the discovery_computing::forecasting profile
     - discovery::bayes
         uses the discovery_computing::bayesian_statistics profile
     - discovery::learner
         uses the discovery_computing::machine_learning profile
     - discovery::deep_learner
         uses the discovery_computing::deep_learning profile
     - discovery::allstar_cruncher
         uses all 4 profiles mentioned above to create a
         multi-purpose statistical computing machine
    
    Also:
     - Fixes the timeout to install tidyverse
     - Adds a way to install R packages from Bioconductor
     - Changes ${title} to ${name} so R packages can be installed with names
       different than the resource name
        - Use case: say we want to add the H2O machine learning library with
          R and Python interfaces. We can install the Python interface 'h2o'
          as 'h2o-py' but previously we couldn't install the R interface and
          follow the same convention. Now we can install the 'h2o' R package
          but then call the resource 'h2o-r' in Puppet.
     - Fixes Shiny Server restart notification: now Puppet won't try notify
       the shiny-server service when that service doesn't actually exist
    
    Bug: T178096
    Change-Id: If5b67444d78773683da5892a2ecdf741cee606de

--- modules/r_lang/manifests/bioc.pp
+++ modules/r_lang/manifests/bioc.pp
@@ -1,8 +1,8 @@
-# = Define: r_lang::cran
+# = Define: r_lang::bioc
 #
-# Facilitates installation of R packages from Comprehensive R Archive Network.
+# Facilitates installation of R packages from bioconductor.org
 #
-# == Parameters
+# === Parameters
 #
 # [*timeout*]
 #     default 300 (seconds) but may need to be larger for R packages with a
@@ -12,20 +12,14 @@
 #     default 'present' but also accepts 'absent'
 #     hoping to support 'latest' eventually
 #
-# [*mirror*]
-#     The CRAN mirror to use, by default uses automatic redirection to servers
-#     worldwide (https://cloud.r-project.org) but can be overridden. Refer to
-#     https://cran.r-project.org/mirrors.html for the full list of mirrors.
-#
 # [*library*]
 #     default '/usr/local/lib/R/site-library', used
 #     for specifying the path of the library for
 #     installing the R package
 #
-define r_lang::cran (
+define r_lang::bioc (
     $timeout = 300,
-    $ensure  = 'present',
-    $mirror  = 'https://cloud.r-project.org',
+    $ensure = 'present',
     $library = '/usr/local/lib/R/site-library'
 ) {
     $pkg_path = "${library}/${title}"
@@ -47,11 +41,10 @@ define r_lang::cran (
         default: {
             exec { "package-${title}":
                 require => [
-                    Package['r-base'],
-                    Package['r-base-dev']
+                    File['/etc/R/biocLite.R']
                 ],
                 timeout => $timeout,
-                command => "/usr/bin/R -e \"install.packages('${title}', repos = c(CRAN = '${mirror}'), lib = '${library}')\"",
+                command => "/usr/bin/R -e \"source('/etc/R/biocLite.R'); biocLite('${title}', lib = '${library}', suppressUpdates = TRUE, ask = FALSE)\"",
                 creates => $pkg_path,
             }
         }
