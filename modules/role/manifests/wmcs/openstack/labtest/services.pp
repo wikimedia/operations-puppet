aa4947975f7b21df1f2c5640ccb034e935234a6e
cpettet <rush@wikimedia.org>
2017-10-12 19:26:39 +0000

    openstack: pdns recursor module/profile/role
    * This deprecates the needed per host values
      for labservices1002
    * use 'secondary' for primary/secondary active/active
      pairs and 'standby' for primary/standby active/standby
      pairs
    * couple recursor monitoring with recursor profile
    * labsdb zone entries to hiera (sorted)
    
    Bug: T171494
    Change-Id: I1055766185d5381a666bbd97976e80eb7a681837

--- modules/role/manifests/wmcs/openstack/labtest/services.pp
+++ modules/role/manifests/wmcs/openstack/labtest/services.pp
@@ -1,6 +1,8 @@
 class role::wmcs::openstack::labtest::services {
+    system::role { $name: }
     include ::profile::openstack::labtest::cloudrepo
     include ::profile::openstack::labtest::pdns::auth::db
     include ::profile::openstack::labtest::pdns::auth::service
+    include ::profile::openstack::labtest::pdns::recursor::service
     include ::profile::openstack::labtest::designate::service
 }
