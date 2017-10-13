ad7bfc532f799d2e1220f7eed9e6f8aac885dfea
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-10-13 11:51:43 +0300

    Purge /usr/lib/ganglia/ and /etc/ganglia
    None of these is strictly required, but let's use it to make sure we
    have purged ganglia fully from the fleet, handling human tests/mistakes
    as well in the process
    
    Bug: T177225
    Change-Id: Ied809661db3ec7ed65cc48187d9c1ca7d3581bb5

--- modules/ganglia/manifests/monitor/decommission.pp
+++ modules/ganglia/manifests/monitor/decommission.pp
@@ -2,4 +2,20 @@ class ganglia::monitor::decommission {
     package { 'ganglia-monitor':
         ensure => purged,
     }
+    # We normally would need only /usr/lib/ganglia/python_modules but let's be
+    # ultra cautious here and purge the parent directory
+    file { '/usr/lib/ganglia/':
+        ensure  => absent,
+        recurse => true,
+        force   => true,
+        require => Package['ganglia-monitor'],
+    }
+    # This is handled by the purging of ganglia-monitor, but let's be extra
+    # explicit so that this is not ever created by some human mistake
+    file { '/etc/ganglia/':
+        ensure  => absent,
+        recurse => true,
+        force   => true,
+        require => Package['ganglia-monitor'],
+    }
 }
