55c74b2896e62b3914ccfa1e09765ab957deef1f
cpettet <rush@wikimedia.org>
2017-10-13 15:21:33 +0000

    openstack: horizon to module/profile/role
    Bug: T171494
    Change-Id: Ia4c51b9610e12a4fdff52ff073bd95ea279e4d22

---
+++ modules/profile/manifests/openstack/labtest/horizon/dashboard.pp
@@ -0,0 +1,21 @@
+class profile::openstack::labtest::horizon::dashboard(
+    $version = hiera('profile::openstack::labtest::version'),
+    $nova_controller = hiera('profile::openstack::labtest::nova_controller'),
+    $wmflabsdotorg_admin = hiera('profile::openstack::labtest::designate::wmflabsdotorg_pass'),
+    $wmflabsdotorg_pass = hiera('profile::openstack::labtest::designate::wmflabsdotorg_pass'),
+    $dhcp_domain = hiera('profile::openstack::labtest::nova::dhcp_domain'),
+    $ldap_user_pass = hiera('profile::openstack::labtest::ldap_user_pass'),
+    $webserver_hostname = hiera('profile::openstack::labtest::horizon::webserver_hostname'),
+    ) {
+
+    require ::profile::openstack::labtest::clientlib
+    class {'::profile::openstack::base::horizon::dashboard':
+        version             => $version,
+        nova_controller     => $nova_controller,
+        wmflabsdotorg_admin => $wmflabsdotorg_admin,
+        wmflabsdotorg_pass  => $wmflabsdotorg_pass,
+        dhcp_domain         => $dhcp_domain,
+        ldap_user_pass      => $ldap_user_pass,
+        webserver_hostname  => $webserver_hostname,
+    }
+}
--- modules/role/manifests/wmcs/openstack/labtest/web.pp
+++ modules/role/manifests/wmcs/openstack/labtest/web.pp
@@ -2,4 +2,5 @@ class role::wmcs::openstack::labtest::web {
     include ::profile::openstack::labtest::cloudrepo
     include ::profile::openstack::labtest::clientlib
     include ::profile::openstack::labtest::observerenv
+    include ::profile::openstack::labtest::horizon::dashboard
 }
