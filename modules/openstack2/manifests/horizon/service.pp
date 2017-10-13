8b962e7ddf8814a64da431e8507a069da13c5050
cpettet <rush@wikimedia.org>
2017-10-13 15:21:33 +0000

    openstack: horizon to module/profile/role
    Bug: T171494
    Change-Id: Ia4c51b9610e12a4fdff52ff073bd95ea279e4d22

--- modules/openstack2/manifests/horizon/service.pp
+++ modules/openstack2/manifests/horizon/service.pp
@@ -1,23 +1,14 @@
 # The OpenStack Das2hboard Project
 # http://docs.openstack.org/developer/horizon/
-class openstack::horizon::service(
-    $novaconfig,
-    $designateconfig,
-    $openstack_version  = $::openstack::version,
+class openstack2::horizon::service(
+    $version,
+    $nova_controller,
+    $wmflabsdotorg_admin,
+    $wmflabsdotorg_pass,
+    $dhcp_domain,
+    $ldap_user_pass,
     $webserver_hostname = 'horizon.wikimedia.org'
 ) {
-
-    package { 'openstack-dashboard':
-        ensure  => present,
-        require => Class['::apache::mod::wsgi'];
-    }
-
-    require_package('python-keystoneclient')
-
-    package { [ 'python-openstack-auth',
-                'python-designate-dashboard' ]:
-        ensure  => present,
-    }
 
     include ::apache
     include ::apache::mod::ssl
@@ -25,6 +16,22 @@ class openstack::horizon::service(
     include ::apache::mod::rewrite
     include ::apache::mod::headers
     include ::memcached
+
+    package { 'openstack-dashboard':
+        ensure  => 'present',
+        require => Class['::apache::mod::wsgi'];
+    }
+
+    require_package([
+        'python-keystoneclient',
+        'python-keystone',
+        'python-mysqldb',
+    )
+
+    package { [ 'python-openstack-auth',
+                'python-designate-dashboard' ]:
+        ensure  => 'present',
+    }
 
     # Blank out these files so that the (broken) dashboard
     #  package doesn't fret.
@@ -36,32 +43,32 @@ class openstack::horizon::service(
     }
 
     file { '/etc/openstack-dashboard/local_settings.py':
-        content => template("openstack/${openstack_version}/horizon/local_settings.py.erb"),
+        content => template("openstack2/${version}/horizon/local_settings.py.erb"),
         owner   => 'horizon',
         group   => 'horizon',
-        notify  => [Service['apache2'], Exec['djangorefresh']],
-        require => Package['openstack-dashboard'],
         mode    => '0440',
+        require => Package['openstack-dashboard'],
+        notify  => [Service['apache2'], Exec['djangorefresh']],
     }
 
     # In the perfect future, Horizon policies will be the same
     #  files that the respective services use.  In the meantime, though
     #  it's useful to be able to disable not-yet-supported horizon features.
     file { '/etc/openstack-dashboard/nova_policy.json':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/nova_policy.json",
+        source  => "puppet:///modules/openstack2/${version}/horizon/nova_policy.json",
         owner   => 'horizon',
         group   => 'horizon',
-        notify  => Service['apache2'],
-        require => Package['openstack-dashboard'],
         mode    => '0440',
+        require => Package['openstack-dashboard'],
+        notify  => Service['apache2'],
     }
     file { '/etc/openstack-dashboard/glance_policy.json':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/glance_policy.json",
+        source  => "puppet:///modules/openstack2/${version}/horizon/glance_policy.json",
         owner   => 'horizon',
         group   => 'horizon',
-        notify  => Service['apache2'],
-        require => Package['openstack-dashboard'],
         mode    => '0440',
+        require => Package['openstack-dashboard'],
+        notify  => Service['apache2'],
     }
 
     # We need a horizon-specific keystone policy because horizon does weird/special
@@ -69,66 +76,69 @@ class openstack::horizon::service(
     #  some permissive policies here (e.g. "") cause Horizon to panic, not ask Keystone for permission,
     #  and log out the user.
     file { '/etc/openstack-dashboard/keystone_policy.json':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/keystone_policy.json",
+        source  => "puppet:///modules/openstack2/${version}/horizon/keystone_policy.json",
         owner   => 'horizon',
         group   => 'horizon',
-        notify  => Service['apache2'],
-        require => Package['openstack-dashboard'],
         mode    => '0440',
+        require => Package['openstack-dashboard'],
+        notify  => Service['apache2'],
     }
 
     file { '/etc/openstack-dashboard/designate_policy.json':
-        source  => "puppet:///modules/openstack/${openstack_version}/designate/policy.json",
+        source  => "puppet:///modules/openstack2/${version}/designate/policy.json",
         owner   => 'horizon',
         group   => 'horizon',
-        notify  => Service['apache2'],
-        require => Package['openstack-dashboard'],
         mode    => '0440',
+        require => Package['openstack-dashboard'],
+        notify  => Service['apache2'],
     }
 
     # This is a trivial policy file that forbids everything.  We'll use it
     #  for services that we don't support to prevent Horizon from
     #  displaying spurious panels.
     file { '/etc/openstack-dashboard/disabled_policy.json':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/disabled_policy.json",
+        source  => "puppet:///modules/openstack2/${version}/horizon/disabled_policy.json",
         owner   => 'horizon',
         group   => 'horizon',
-        notify  => Service['apache2'],
-        require => Package['openstack-dashboard'],
         mode    => '0440',
+        require => Package['openstack-dashboard'],
+        notify  => Service['apache2'],
     }
 
     file { '/usr/share/openstack-dashboard/openstack_dashboard/static/dashboard/img/logo.png':
-        source  => 'puppet:///modules/openstack/horizon/216px-Wikimedia_labs_dashboard_logo.png',
+        source  => 'puppet:///modules/openstack2/horizon/216px-Wikimedia_labs_dashboard_logo.png',
         owner   => 'horizon',
         group   => 'horizon',
-        require => Package['openstack-dashboard'],
         mode    => '0444',
+        require => Package['openstack-dashboard'],
     }
+
     file { '/usr/share/openstack-dashboard/openstack_dashboard/static/dashboard/img/logo-splash.png':
-        source  => 'puppet:///modules/openstack/horizon/180px-Wikimedia_labs_dashboard_splash.png',
+        source  => 'puppet:///modules/openstack2/horizon/180px-Wikimedia_labs_dashboard_splash.png',
         owner   => 'horizon',
         group   => 'horizon',
-        require => Package['openstack-dashboard'],
         mode    => '0444',
+        require => Package['openstack-dashboard'],
     }
+
     file { '/usr/share/openstack-dashboard/openstack_dashboard/static/dashboard/img/favicon.ico':
-        source  => 'puppet:///modules/openstack/horizon/Wikimedia_labs.ico',
+        source  => 'puppet:///modules/openstack2/horizon/Wikimedia_labs.ico',
         owner   => 'horizon',
         group   => 'horizon',
-        require => Package['openstack-dashboard'],
         mode    => '0444',
+        require => Package['openstack-dashboard'],
     }
 
     # Homemade totp plugin for keystoneclient
     file { '/usr/lib/python2.7/dist-packages/keystoneclient/auth/identity/v3/wmtotp.py':
-        source => "puppet:///modules/openstack/${openstack_version}/keystoneclient/wmtotp.py",
+        source => "puppet:///modules/openstack2/${version}/keystoneclient/wmtotp.py",
         owner  => 'root',
         group  => 'root',
         mode   => '0644',
     }
+
     file { '/usr/lib/python2.7/dist-packages/keystoneclient/auth/identity/v3/__init__.py':
-        source => "puppet:///modules/openstack/${openstack_version}/keystoneclient/__init__.py",
+        source => "puppet:///modules/openstack2/${version}/keystoneclient/__init__.py",
         owner  => 'root',
         group  => 'root',
         mode   => '0644',
@@ -136,31 +146,31 @@ class openstack::horizon::service(
 
     # Homemade totp plugin for openstack_auth
     file { '/usr/lib/python2.7/dist-packages/openstack_auth/plugin/wmtotp.py':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/openstack_auth/wmtotp.py",
+        source  => "puppet:///modules/openstack2/${version}/horizon/openstack_auth/wmtotp.py",
         owner   => 'root',
         group   => 'root',
-        require => Package['python-openstack-auth'],
         mode    => '0644',
+        require => Package['python-openstack-auth'],
     }
 
     # Replace the standard horizon login form to support 2fa
     file { '/usr/lib/python2.7/dist-packages/openstack_auth/forms.py':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/openstack_auth/forms.py",
+        source  => "puppet:///modules/openstack2/${version}/horizon/openstack_auth/forms.py",
         owner   => 'root',
         group   => 'root',
-        require => Package['python-openstack-auth'],
         mode    => '0644',
+        require => Package['python-openstack-auth'],
     }
 
     # Replace the standard login backend.py to fix
     #  https://bugs.launchpad.net/django-openstack-auth/+bug/1562452
     # and also to implement the 'rememberme' checkbox
     file { '/usr/lib/python2.7/dist-packages/openstack_auth/backend.py':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/openstack_auth/backend.py",
+        source  => "puppet:///modules/openstack2/${version}/horizon/openstack_auth/backend.py",
         owner   => 'root',
         group   => 'root',
-        require => Package['python-openstack-auth'],
         mode    => '0644',
+        require => Package['python-openstack-auth'],
     }
 
     # Install the designate dashboard
@@ -171,6 +181,7 @@ class openstack::horizon::service(
         mode    => '0644',
         require => Package['python-designate-dashboard', 'openstack-dashboard'],
     }
+
     file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled':
         ensure  => 'directory',
         owner   => 'root',
@@ -178,15 +189,17 @@ class openstack::horizon::service(
         mode    => '0644',
         require => Package['python-designate-dashboard', 'openstack-dashboard'],
     }
+
     file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled/_70_dns_add_group.py':
-        source  => "puppet:///modules/openstack/${openstack_version}/designate/dashboard/_70_dns_add_group.py",
+        source  => "puppet:///modules/openstack2/${version}/designate/dashboard/_70_dns_add_group.py",
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
         require => Package['python-designate-dashboard', 'openstack-dashboard'],
     }
+
     file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled/_71_dns_project.py':
-        source  => "puppet:///modules/openstack/${openstack_version}/designate/dashboard/_71_dns_project.py",
+        source  => "puppet:///modules/openstack2/${version}/designate/dashboard/_71_dns_project.py",
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
@@ -195,83 +208,86 @@ class openstack::horizon::service(
 
     # Proxy panel
     file { '/usr/lib/python2.7/dist-packages/wikimediaproxydashboard':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/proxy",
+        source  => "puppet:///modules/openstack2/${version}/horizon/proxy",
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        require => Package['python-designate-dashboard', 'openstack-dashboard'],
-        notify  => Exec['djangorefresh'],
         recurse => true,
+        require => Package['python-designate-dashboard', 'openstack-dashboard'],
+        notify  => Exec['djangorefresh'],
     }
+
     file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled/_1922_project_proxy_panel.py':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/proxy_enable.py",
+        source  => "puppet:///modules/openstack2/${version}/horizon/proxy_enable.py",
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        notify  => Exec['djangorefresh'],
         require => Package['python-designate-dashboard', 'openstack-dashboard'],
+        notify  => Exec['djangorefresh'],
     }
 
     # sudo dashboard
     file { '/usr/lib/python2.7/dist-packages/wikimediasudodashboard':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/sudo",
+        source  => "puppet:///modules/openstack2/${version}/horizon/sudo",
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        require => Package['python-designate-dashboard', 'openstack-dashboard'],
-        notify  => Exec['djangorefresh'],
         recurse => true,
-    }
-    file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled/_1926_project_sudo_panel.py':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/sudo_enable.py",
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        notify  => Exec['djangorefresh'],
         require => Package['python-designate-dashboard', 'openstack-dashboard'],
-    }
-    file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled/_72_sudoers_add_group.py':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/sudo_group_add.py",
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
         notify  => Exec['djangorefresh'],
-        require => Package['python-designate-dashboard', 'openstack-dashboard'],
     }
 
-    if $openstack_version != 'liberty' {
+    file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled/_1926_project_sudo_panel.py':
+        source  => "puppet:///modules/openstack2/${version}/horizon/sudo_enable.py",
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['python-designate-dashboard', 'openstack-dashboard'],
+        notify  => Exec['djangorefresh'],
+    }
+
+    file { '/usr/share/openstack-dashboard/openstack_dashboard/local/enabled/_72_sudoers_add_group.py':
+        source  => "puppet:///modules/openstack2/${version}/horizon/sudo_group_add.py",
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['python-designate-dashboard', 'openstack-dashboard'],
+        notify  => Exec['djangorefresh'],
+    }
+
+    if $version != 'liberty' {
         # Override some .js files to provide a simplified user experience.  Alas
         #  we can't do this via the overrides.py monkeypatch below
 
         file { '/usr/share/openstack-dashboard/openstack_dashboard/dashboards/project/static/dashboard/project/workflow/launch-instance/launch-instance-workflow.service.js':
-            source  => "puppet:///modules/openstack/${openstack_version}/horizon/jsoverrides/launch-instance-workflow.service.js",
+            source  => "puppet:///modules/openstack2/${version}/horizon/jsoverrides/launch-instance-workflow.service.js",
             owner   => 'root',
             group   => 'root',
+            mode    => '0644',
             require => Package['openstack-dashboard'],
             notify  => Exec['djangorefresh'],
-            mode    => '0644',
         }
     }
 
     # Monkeypatches for Horizon customization
     file { '/usr/lib/python2.7/dist-packages/horizon/overrides.py':
-        source  => "puppet:///modules/openstack/${openstack_version}/horizon/overrides.py",
+        source  => "puppet:///modules/openstack2/${version}/horizon/overrides.py",
         owner   => 'root',
         group   => 'root',
-        require => Package['python-openstack-auth'],
         mode    => '0644',
+        require => Package['python-openstack-auth'],
     }
 
     # Arbitrary handy script that needs to be on the horizon host because it only works with Liberty
     file { '/root/makedomain':
-        source => "puppet:///modules/openstack2/${openstack_version}/admin_scripts/makedomain",
+        source => "puppet:///modules/openstack2/${version}/admin_scripts/makedomain",
         owner  => 'root',
         group  => 'root',
         mode   => '0744',
     }
 
     apache::site { $webserver_hostname:
-        content => template("openstack/${openstack_version}/horizon/${webserver_hostname}.erb"),
+        content => template("openstack2/${version}/horizon/${webserver_hostname}.erb"),
         require => File['/etc/openstack-dashboard/local_settings.py'],
     }
 
