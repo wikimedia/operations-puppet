98db8d688424f48525ee10ae8342641f6c212696
Ottomata <aotto@wikimedia.org>
2017-10-16 15:30:26 -0400

    Set up Kafka MirrorMaker from main -> jumbo in eqiad
    This includes a new profile::kafka::mirror class.  This
    class can be used to set up a single Kafka MirrorMaker instance,
    and as such, cannot be used to set up multiple MirrorMaker
    instances running on a single node.  For now, this should
    be fine, as we don't have this use case.
    
    TODO:
    - TLS configuration
    
    Bug: T177216
    Change-Id: I020a9c1a5cb1e3c18a33f18c2ad91ea915a74ed7

---
+++ modules/role/manifests/kafka/simple/mirror.pp
@@ -0,0 +1,10 @@
+# == Class role::kafka::simple::mirror
+# This is useful for testing Kafka MirrorMaker in labs.
+#
+class role::kafka::simple::mirror {
+    system::role { 'role::kafka::simple::mirror':
+        description => 'Kafka simple MirrorMaker instance'
+    }
+
+    include profile::kafka::mirror
+}
