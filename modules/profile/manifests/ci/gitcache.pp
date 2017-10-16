911ecf1d55d3315ca7bc5c837f1b5af39c89296b
addshore <addshorewiki@gmail.com>
2017-10-16 19:01:41 +0100

    ci: add mediawiki core and vendor to gitcache for docker slaves
    Bug: T178076
    Change-Id: Ia509cbfc147d566ca73c18a37acdb6beb210d96b

--- modules/profile/manifests/ci/gitcache.pp
+++ modules/profile/manifests/ci/gitcache.pp
@@ -2,6 +2,7 @@ class profile::ci::gitcache {
     file { '/srv/git':
         ensure => directory,
     }
+
     file { '/srv/git/operations':
         ensure => directory,
     }
@@ -9,5 +10,21 @@ class profile::ci::gitcache {
         directory => '/srv/git/operations/puppet.git',
         bare      => true,
         require   => File['/srv/git/operations'],
+    }
+
+    file { '/srv/git/mediawiki':
+        ensure => directory,
+    }
+
+    git::clone { 'mediawiki/core':
+        directory => '/srv/git/mediawiki/core.git',
+        bare      => true,
+        require   => File['/srv/git/mediawiki'],
+    }
+
+    git::clone { 'mediawiki/vendor':
+        directory => '/srv/git/mediawiki/vendor.git',
+        bare      => true,
+        require   => File['/srv/git/mediawiki'],
     }
 }
