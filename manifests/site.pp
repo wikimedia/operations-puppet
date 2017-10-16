9bb455d15a1b1af5d26ffd1129e623612f46188c
Manuel Arostegui <marostegui@wikimedia.org>
2017-10-16 08:16:27 +0200

    mariadb: Add db2084 to s5 and later s8
    db2084 will be cloned from db2079 and will serve in s5 and later s8
    
    Bug: T170662
    Change-Id: Id20268b4e3821a9149591302454ef6b4f73a0e49

--- manifests/site.pp
+++ manifests/site.pp
@@ -458,7 +458,7 @@ node 'db2023.codfw.wmnet' {
     role(mariadb::core)
 }
 
-node /^db20(38|45|52|59|66|75|79|80|81|82|83)\.codfw\.wmnet/ {
+node /^db20(38|45|52|59|66|75|79|80|81|82|83|84)\.codfw\.wmnet/ {
     role(mariadb::core)
 }
 
