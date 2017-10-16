13827350c902639ef418f0ebffa4f6bab2813846
cpettet <rush@wikimedia.org>
2017-10-16 17:24:12 +0000

    openstack: nova manager/osm/wikitech to module/profile/role
    Bug: T171494
    Change-Id: I7e06a6e2f0cdc1e41462e4fdd7f5585f62e0575c

---
+++ modules/profile/manifests/openstack/labtest/wikitech/monitor.pp
@@ -0,0 +1,8 @@
+class profile::openstack::labtest::wikitech::monitor(
+    $osm_host = hiera('profile::openstack::labtest::osm_host'),
+    ) {
+
+    class {'::profile::openstack::base::wikitech::monitor':
+        osm_host => osm_host,
+    }
+}
