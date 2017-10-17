4755734096421f7104bf2b8e9778183668e388d9
Daniel Zahn <dzahn@wikimedia.org>
2017-10-17 14:50:06 -0700

    openstack2: no Icinga paging (SMS) if on labtest
    Instead of hardcoded "critical => true" for all of the
    process checks, make it flexible and a parameter of the class.
    
    Make it true by default.
    
    Then set it to false for "labtest" hosts in Hiera regex.
    
    This should avoid any SMS being sent if things are just on labtest
    hosts and not production hosts.
    
    Bug: T178008
    Change-Id: I61b50f51d83e517adb711a3d6371b0e910bd9290

--- modules/openstack2/manifests/designate/monitor.pp
+++ modules/openstack2/manifests/designate/monitor.pp
@@ -3,6 +3,7 @@
 
 class openstack2::designate::monitor (
     $active,
+    $critical = true,
     ) {
 
     # monitoring::service doesn't take a bool
@@ -13,41 +14,41 @@ class openstack2::designate::monitor (
         $ensure = 'absent'
     }
 
-    # Page if designate processes die.  We only have one of each of these,
+    # Page if designate processes die and 'critical' is true.  We only have one of each of these,
     #  and new instance creation will be very broken if services die.
     nrpe::monitor_service { 'check_designate_sink_process':
         ensure       => $ensure,
         description  => 'designate-sink process',
         nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/designate-sink'",
-        critical     => true,
+        critical     => $critical,
     }
 
     nrpe::monitor_service { 'check_designate_api_process':
         ensure       => $ensure,
         description  => 'designate-api process',
         nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/designate-api'",
-        critical     => true,
+        critical     => $critical,
     }
 
     nrpe::monitor_service { 'check_designate_central_process':
         ensure       => $ensure,
         description  => 'designate-central process',
         nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/designate-central'",
-        critical     => true,
+        critical     => $critical,
     }
 
     nrpe::monitor_service { 'check_designate_mdns':
         ensure       => $ensure,
         description  => 'designate-mdns process',
         nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/designate-mdns'",
-        critical     => true,
+        critical     => $critical,
     }
 
     nrpe::monitor_service { 'check_designate_pool-manager':
         ensure       => $ensure,
         description  => 'designate-pool-manager process',
         nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/designate-pool-manager'",
-        critical     => true,
+        critical     => $critical,
     }
 
     monitoring::service { 'designate-api-http':
