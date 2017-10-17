f64071bfba5b594aa75feb9009df453357492950
Jaime Crespo <jcrespo@wikimedia.org>
2017-10-17 14:52:53 +0200

    proxysql: Setup proxysql on terbium/wasat as a test
    Set up an instance to connect to a random enwiki host.
    It is easier to set them up with puppet than trying to set them
    up manually. We will not allow external conections from now.
    
    Update proxysql module to install wmf-mariadb101-client instead
    of the generic mysql client to ensure openssl compatibility.
    
    Bug: T175672
    Change-Id: I6eaa0955e690e34718d939bbc11ca8a47fcd858a

---
+++ modules/profile/manifests/proxysql.pp
@@ -0,0 +1,40 @@
+# role::labs::db::proxy
+# Manages proxysql for the replica and other sql services on
+# labs-support network
+
+class profile::proxysql {
+    include ::passwords::misc::scripts
+
+    $admin_user = 'root'
+    $admin_password = $::passwords::misc::scripts::mysql_root_pass
+
+    class { '::proxysql':
+        admin_user     => $admin_user,
+        admin_password => $admin_password,
+        admin_socket   => '/run/proxysql/proxysql_admin.sock',
+        mysql_socket   => '/run/proxysql/proxysql.sock',
+        mysql_port     => 3311,
+    }
+
+    # Let's not open the proxy port for now, only allow localhost connections
+    #ferm::service { 'proxysql_mysql':
+    #    proto   => 'tcp',
+    #    port    => '3311',
+    #    notrack => true,
+    #}
+
+    file {'/run/proxysql':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        require => Class['proxysql'],
+    }
+
+    nrpe::monitor_service { 'proxysql':
+        description   => 'proxysql processes',
+        nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 2:2 -C proxysql',
+        critical      => false,
+        contact_group => 'admins', # show on icinga/irc only
+    }
+}
