233543536a95ab20253a52e33c2065718aa8be4e
elukey <ltoscano@wikimedia.org>
2017-10-19 10:41:11 +0200

    prometheus::jmx_exporter_instance: use hostname rather than title
    After https://gerrit.wikimedia.org/r/#/c/384608 the new prometheus
    profile::prometheus::jmx_exporter define can be use multiple times
    on the same hosts; for example, on kafka nodes we have to instrument
    two JVMs (Mirror Maker and Kafka). This requires the use of more than
    one title that for uniqueness purposes can't be the hostname.
    For Cassandra we worked around the issue using the instance names, but
    for Kafka we don't have the same luxury. The current issue is in fact
    that on the Prometheus masters we have a config file like the following:
    
    - labels:
        cluster: kafka_jumbo
      targets:
        - "kafka_broker_kafka-jumbo1005:7800
    [..]
    
    This code change should force the use of the hostname retrieved from
    Puppetdb each time that a target is rendered, that should be fine also
    for the cassandra use case.
    
    Bug: T175923
    Change-Id: I89f2d95b144133f0c85d6085583e104d85694924

--- modules/prometheus/manifests/jmx_exporter_instance.pp
+++ modules/prometheus/manifests/jmx_exporter_instance.pp
@@ -1,5 +1,8 @@
-# Dummy define to instruct Prometheus to poll $title:$port for metrics exporter
+# == Define: prometheus::jmx_exporter_instance
+#
+# Dummy define to instruct Prometheus to poll $hostname:$port for metrics exporter
 # by jmx_exporter
+#
 define prometheus::jmx_exporter_instance (
     $address,
     $port
