647a202dd193d016167535cdb68b4856a02a865a
elukey <ltoscano@wikimedia.org>
2017-10-19 13:17:09 +0200

    [WIP] Introduce mariadb eventlogging profiles for master/replica
    Bug: T177405
    Change-Id: Ibf31d8f5976e96d7334f9db81bc3ad2f152000cc

--- manifests/site.pp
+++ manifests/site.pp
@@ -611,19 +611,18 @@ node 'db2012.codfw.wmnet' {
 
 # m4 shard
 node 'db1046.eqiad.wmnet' {
-    class { '::role::mariadb::misc::eventlogging':
-        shard  => 'm4',
-        master => true,
-    }
+    role(mariadb::analytics,
+        mariadb::misc::eventlogging::master,
+        mariadb::ferm,
+        mariadb::monitor::dba)
 }
 node 'db1047.eqiad.wmnet' {
     # this slave has an m4 custom replication protocol
     # this slave additionally replicates s1 and s2
-    role(mariadb::analytics, mariadb::analytics::custom_repl_slave)
-    class { '::role::mariadb::misc::eventlogging':
-        shard  => 'm4',
-        master => false,
-    }
+    role(mariadb::analytics,
+        mariadb::misc::eventlogging::replica,
+        mariadb::ferm,
+        mariadb::monitor::dba)
 }
 
 # m5 shard
@@ -672,7 +671,10 @@ node 'dbstore1001.eqiad.wmnet' {
 
 node 'dbstore1002.eqiad.wmnet' {
     # this slave has an m4 custom replication protocol
-    role(mariadb::dbstore, mariadb::analytics::custom_repl_slave)
+    role(mariadb::dbstore,
+        mariadb::misc::eventlogging::replica,
+        mariadb::ferm,
+        mariadb::monitor::dba)
 }
 
 node 'dbstore2001.codfw.wmnet' {
