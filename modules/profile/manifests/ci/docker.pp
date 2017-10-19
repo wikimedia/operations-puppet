defe9215cd46622ea7f52ebbbf8249f5963069f6
Antoine Musso <hashar@free.fr>
2017-10-19 18:40:42 +0200

    contint: move blubber from docker to pipeline profile
    profile::ci::docker is intended to install a Docker daemon. It is used
    both on the production machines and labs instances.
    
    blubber is for the pipeline project. So move it from profile::ci::docker
    to profile::ci::pipeline. Follow up  b94cafbce.
    
    That removes blubber from the labs Docker hosts.
    
    Change-Id: I458d0d6c27113c0291db791bbe209afe8b36d752

--- modules/profile/manifests/ci/docker.pp
+++ modules/profile/manifests/ci/docker.pp
@@ -5,9 +5,6 @@
 class profile::ci::docker(
     $jenkins_agent_username = hiera('jenkins_agent_username'),
 ) {
-    # We will need to build containers in production
-    require_package('blubber')
-
     apt::repository { 'thirdparty-ci':
         uri        => 'http://apt.wikimedia.org/wikimedia',
         dist       => "${::lsbdistcodename}-wikimedia",
