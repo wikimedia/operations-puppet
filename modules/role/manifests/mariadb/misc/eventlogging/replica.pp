d60d3dcd23074188d50dcbc724b4fa2971537e79
elukey <ltoscano@wikimedia.org>
2017-10-19 13:17:09 +0200

    Introduce mariadb eventlogging profiles for master/replica
    Bug: T177405
    Change-Id: Ibf31d8f5976e96d7334f9db81bc3ad2f152000cc

---
+++ modules/role/manifests/mariadb/misc/eventlogging/replica.pp
@@ -0,0 +1,4 @@
+class role::mariadb::misc::eventlogging::replica {
+    include ::profile::mariadb::misc::eventlogging::database
+    include ::profile::mariadb::misc::::eventlogging::replication
+}
