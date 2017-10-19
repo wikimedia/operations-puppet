885c1ccaf53a1079e1e0edab6096233372273bed
Daniel Zahn <dzahn@wikimedia.org>
2017-10-18 18:40:51 -0700

    cyberbot::db: set datadir, bind_dir, use mysql::server
    Change-Id: I3b0077e7c7a9f514558bc79fa16183a11d41b5b5

--- modules/profile/manifests/cyberbot/db.pp
+++ modules/profile/manifests/cyberbot/db.pp
@@ -1,4 +1,24 @@
 # sets up a dedicated DB server for cyberbot
 class profile::cyberbot::db{
-    require_package('mariadb-server')
+
+    file { '/srv/mysql':
+        ensure => 'directory',
+        owner  => 'mysql',
+        group  => 'mysql',
+    }
+
+    file { '/srv/mysql/data':
+        ensure => 'directory',
+        owner  => 'mysql',
+        group  => 'mysql',
+        require => File['/srv/mysql'],
+    }
+
+    class { '::mysql::server':
+        package_name => 'mariadb-server',
+        config_hash  => {
+            'datadir'      => '/srv/mysql/data',
+            'bind_address' => '0.0.0.0',
+        },
+    }
 }
