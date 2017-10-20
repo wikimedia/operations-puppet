11f3d656dc7b66a34dfaf6d517418ad2d0f8e1a4
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-10-20 14:59:53 +0200

    Add systemd::tmpfile to configure /etc/tmpfiles.d configuration files
    We already have quite a few of those handled via file resources, so
    introduce a common config wrapper to simplify the usage (which will
    also straighten out some inconsistencies, e.g. a few of the existing
    ones are installed to /usr/lib/tmpfiles.d/ which is reserved for
    configs shipped by Debian packages).
    
    Change-Id: I03e3ad7727f0d6070064f98e47ee51e8fcd08899

---
+++ modules/systemd/manifests/tmpfile.pp
@@ -0,0 +1,37 @@
+# == systemd::tmpfile ==
+#
+# This define creates a systemd tmpfiles.d configuration snippet
+#
+# === Parameters ===
+# The resource title is the basename of the resulting config file (the
+# .conf is automatically appended)
+#
+# [*content*]
+#   The content of the file. Required.
+# [*ensure*]
+#   The usual meta-parameter, defaults to present. Valid values are
+#   'absent' and 'present'
+# [*owner*]
+#   The owner of the file. Defaults to root, but can be overridden if
+#   the config file needs to be read by some daemon
+# [*group*]
+#   The group owner of the file. Defaults to root, but can be
+#   overridden if the config file needs to be read by some daemon
+#
+define systemd::tmpfile(
+    $content,
+    $ensure=present,
+    $owner='root',
+    $group='root',
+){
+    $safe_title = regsubst($title, '[\W_/]', '-', 'G')
+    $conf_path = "/etc/tmpfiles.d/${safe_title}.conf"
+
+    file { $conf_path:
+        ensure  => $ensure,
+        content => $content,
+        mode    => '0444',
+        owner   => $owner,
+        group   => $group,
+    }
+}
