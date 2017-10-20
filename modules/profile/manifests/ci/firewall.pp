8079fc68bd23b09029b1302e9bf0fa8fdd689e0e
Antoine Musso <hashar@free.fr>
2017-10-20 23:00:24 +0200

    Migrate contint::firewall to a profile
    Rename some hiera variables in the process.
    
    Resolved violations:
    
    modules/contint/manifests/firewall.pp
     Found hiera call in class 'contint::firewall'
     Found hiera call in class 'contint::firewall'
     includes base::firewall from another module
     includes network::constants from another module
    
    modules/profile/manifests/ci/firewall.pp
     profile 'profile::ci::firewall' includes non-profile class
     contint::firewall
    
    Change-Id: Ib2a4b057195c56a85640746b2f69646062f6e9d1

--- modules/profile/manifests/ci/firewall.pp
+++ modules/profile/manifests/ci/firewall.pp
@@ -1,3 +1,72 @@
-class profile::ci::firewall {
-    include ::contint::firewall
+# == class contint::firewall
+#
+# === Parameters
+#
+# Several bricks communicate with the Zuul Gearman server:
+#
+# [$nodepool_host] The Nodepool server
+# [$zuul_merger_hosts] List of zuul-mergers
+#
+class profile::ci::firewall (
+    $nodepool_host = hiera('profile::ci::firewall::nodepool_host'),
+    $zuul_merger_hosts = hiera('profile::ci::firewall::zuul_merger_hosts'),
+) {
+    class { '::base::firewall': }
+    include ::network::constants
+
+    # Restrict some services to be only reacheable from localhost over both
+    # IPv4 and IPv6 (to be safe)
+
+    # Jenkins on port 8080, reacheable via Apache proxying the requests
+    ferm::rule { 'jenkins_localhost_only':
+        rule => 'proto tcp dport 8080 { saddr (127.0.0.1 ::1) ACCEPT; }',
+    }
+    # Zuul status page on port 8001, reacheable via Apache proxying the requests
+    ferm::rule { 'zuul_localhost_only':
+        rule => 'proto tcp dport 8001 { saddr (127.0.0.1 ::1) ACCEPT; }',
+    }
+
+    # Gearman is used between Zuul and the Jenkin master, both on the same
+    # server and communicating over localhost.
+    # It is also used by Zuul merger daemons.
+    $zuul_merger_hosts_ferm = join($zuul_merger_hosts, ' ')
+
+    ferm::service { 'gearman_from_zuul_mergers':
+        proto  => 'tcp',
+        port   => '4730',
+        srange => "(${zuul_merger_hosts_ferm})",
+    }
+
+    # Nodepool related
+    ferm::service { 'gearman_from_nodepool':
+        proto  => 'tcp',
+        port   => '4730',
+        srange => $nodepool_host,
+    }
+    ferm::service { 'jenkins_zeromq_from_nodepool':
+        proto  => 'tcp',
+        port   => '8888',
+        srange => $nodepool_host,
+    }
+
+    ferm::service { 'jenkins_restapi_from_nodepool':
+        proto  => 'tcp',
+        port   => '443',
+        srange => $nodepool_host,
+    }
+
+    ferm::service { 'gerrit_ssh':
+        proto  => 'tcp',
+        port   => '29418',
+        srange => '@resolve((gerrit2001.wikimedia.org cobalt.wikimedia.org gerrit.wikimedia.org))',
+    }
+
+    # ALLOWS:
+
+    # web access
+    ferm::service { 'allow_http':
+        proto  => 'tcp',
+        port   => '80',
+        srange => '$PRODUCTION_NETWORKS',
+    }
 }
