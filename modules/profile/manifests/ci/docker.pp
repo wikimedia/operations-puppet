adfd61425570121f6cecb4661f15d8cb53cbb220
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-10-24 15:47:04 +0300

    ci: Use docker class, pass configuration
    Instead of installing the docker-ce package in a profile, which is a
    wrong thing to do, use the docker module class. Amend that class to
    allow specifying the package name and supply the correct version (i.e.
    docker-ce) for CI. Configure the docker daemon using the
    docker::configuration class, passing via hiera the iptables: false
    directive which forces docker to not manage the iptables rules. Ship the
    default docker iptables via ferm, thus resolving a race condition
    between ferm and docker that would end up with containers unable to
    access the network
    
    Change-Id: I591bedcc03e6759b6d761956ae09c2fdb73dd3a6

--- modules/profile/manifests/ci/docker.pp
+++ modules/profile/manifests/ci/docker.pp
@@ -4,6 +4,7 @@
 #
 class profile::ci::docker(
     $jenkins_agent_username = hiera('jenkins_agent_username'),
+    $settings = hiera('profile::ci::docker::settings'),
 ) {
     # We will need to build containers in production
     require_package('blubber')
@@ -13,12 +14,16 @@ class profile::ci::docker(
         dist       => "${::lsbdistcodename}-wikimedia",
         components => 'thirdparty/ci',
     }
-    package { 'docker-ce':
-        ensure  => present,
-        require => [
-          Apt::Repository['thirdparty-ci'],
-          Exec['apt-get update']
+    class { '::docker':
+        package_name => 'docker-ce',
+        version      => '17.06.2~ce-0~debian',
+        require      => [
+            Apt::Repository['thirdparty-ci'],
+            Exec['apt-get update']
         ],
+    }
+    class { '::docker::configuration':
+        settings => $settings,
     }
     # Ensure jenkins-deploy membership in the docker group
     exec { 'jenkins user docker membership':
@@ -27,5 +32,13 @@ class profile::ci::docker(
         require => [
             Package['docker-ce'],
         ],
+    }
+
+    # Ship the entire docker iptables configuration via ferm
+    # This is here to make sure docker and ferm play nice together.
+    ferm::conf { 'docker-ferm':
+        ensure => present,
+        prio   => 20,
+        source => 'puppet:///modules/profile/ci/docker-ferm',
     }
 }
