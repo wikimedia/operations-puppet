a2f9feff995a9ce61c1a5e55486e8d28340302a6
cpettet <rush@wikimedia.org>
2017-10-24 21:07:31 +0000

    openstack: refactor deployment specific puppetmaster code
    Bug: T171494
    Change-Id: Ia9f23bc853e296b53d4a0c41e86b6e3c99f789ac

---
+++ modules/profile/manifests/openstack/base/puppetmaster/backend.pp
@@ -0,0 +1,52 @@
+class profile::openstack::base::puppetmaster::backend(
+    $labs_instance_range = hiera('profile::openstack::base::nova::fixed_range'),
+    $horizon_host = hiera('profile::openstack::base::horizon_host'),
+    $designate_host = hiera('profile::openstack::base::designate_host'),
+    $puppetmaster_hostname = hiera('profile::openstack::base::puppetmaster_hostname'),
+    $puppetmaster_ca = hiera('profile::openstack::base::puppetmaster::ca'),
+    $puppetmasters = hiera('profile::openstack::base::puppetmaster::servers'),
+    $baremetal_servers = hiera('profile::openstack::base::puppetmaster::baremetal'),
+    $encapi_db_host = hiera('profile::openstack::base::puppetmaster::encapi::db_host'),
+    $encapi_db_name = hiera('profile::openstack::base::puppetmaster::encapi::db_name'),
+    $encapi_db_user = hiera('profile::openstack::base::puppetmaster::encapi::db_user'),
+    $encapi_db_pass = hiera('profile::openstack::base::puppetmaster::encapi::db_pass'),
+    $encapi_statsd_prefix = hiera('profile::openstack::base::puppetmaster::encapi::statsd_prefix'),
+    $statsd_host = hiera('profile::openstack::base::statsd_host'),
+    ) {
+
+    require ::profile::conftool::client
+    include ::network::constants
+    class {'profile::openstack::base::puppetmaster::common':
+        labs_instance_range   => $labs_instance_range,
+        horizon_host          => $horizon_host,
+        designate_host        => $designate_host,
+        puppetmaster_hostname => $puppetmaster_hostname,
+        puppetmasters         => $puppetmasters,
+        baremetal_servers     => $baremetal_servers,
+        encapi_db_host        => $encapi_db_host,
+        encapi_db_name        => $encapi_db_name,
+        encapi_db_user        => $encapi_db_user,
+        encapi_db_pass        => $encapi_db_pass,
+        encapi_statsd_prefix  => $encapi_statsd_prefix,
+        statsd_host           => $statsd_host,
+    }
+
+    # Only allow puppet access from the instances
+    $allow_from = flatten([$labs_instance_range, $baremetal_servers, '.wikimedia.org'])
+
+    $config = {
+        'node_terminus'     => 'exec',
+        'external_nodes'    => '/usr/local/bin/puppet-enc',
+        'thin_storeconfigs' => false,
+        'autosign'          => true,
+    }
+
+    class { '::profile::puppetmaster::backend':
+        config           => $config,
+        secure_private   => false,
+        allow_from       => $allow_from,
+        puppetmasters    => $puppetmasters,
+        ca_server        => $puppetmaster_ca,
+        extra_auth_rules => template('profile/openstack/base/puppetmaster/extra_auth_rules.conf.erb'),
+    }
+}
