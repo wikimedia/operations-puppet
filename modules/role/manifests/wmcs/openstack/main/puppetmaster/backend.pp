11cd11ca256c20bc552efa003ff21a29635a6086
cpettet <rush@wikimedia.org>
2017-10-24 21:07:31 +0000

    openstack: refactor deployment specific puppetmaster code
    Bug: T171494
    Change-Id: Ia9f23bc853e296b53d4a0c41e86b6e3c99f789ac

---
+++ modules/role/manifests/wmcs/openstack/main/puppetmaster/backend.pp
@@ -0,0 +1,6 @@
+class role::wmcs::openstack::main::puppetmaster::backend {
+    system::role { $name: }
+    include ::profile::openstack::main::clientlib
+    include ::profile::openstack::main::observerenv
+    include ::profile::openstack::main::puppetmaster::backend
+}
