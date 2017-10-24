4871ccd345efed51622a5a70d4f2284b07789bdc
Paladox <thomasmulhall410@yahoo.com>
2017-10-24 20:01:59 +0000

    ci::slave::labs: Drop host_aliases for gerrit.wm.org ssh
    Change-Id: I59b497a4cddc41043b5c3323634b4690160f354c

--- modules/role/manifests/ci/slave/labs.pp
+++ modules/role/manifests/ci/slave/labs.pp
@@ -60,7 +60,6 @@ class role::ci::slave::labs {
     sshkey { 'gerrit':
         ensure       => 'present',
         name         => 'gerrit.wikimedia.org',
-        host_aliases => ['208.80.154.81'],
         key          => 'AAAAB3NzaC1yc2EAAAADAQABAAAAgQCF8pwFLehzCXhbF1jfHWtd9d1LFq2NirplEBQYs7AOrGwQ/6ZZI0gvZFYiEiaw1o+F1CMfoHdny1VfWOJF3mJ1y9QMKAacc8/Z3tG39jBKRQCuxmYLO1SWymv7/Uvx9WQlkNRoTdTTa9OJFy6UqvLQEXKYaokfMIUHZ+oVFf1CgQ==',
         type         => 'ssh-rsa',
         notify       => File['/etc/ssh/ssh_known_hosts'],
