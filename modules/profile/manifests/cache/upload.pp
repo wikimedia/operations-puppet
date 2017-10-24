a6268ae287ac53fcf91e0d4dd5194fadc609fa4e
Emanuele Rocca <ema@wikimedia.org>
2017-10-24 16:17:38 +0200

    VCL: Exp cache admission policy for varnish-fe
    Introduce a hiera setting, profile::cache::base::admission_policy, to
    choose the desired frontend cache admission policy. The policies
    supported as of today are: "N-hit-wonder" (nhw) and "admission
    probability exponentially decreasing with size" (exp).
    
    Implement Exp policy.
    
    Bug: T144187
    Change-Id: I38bc15653743468c94bdef50c63200f351093615

--- modules/profile/manifests/cache/upload.pp
+++ modules/profile/manifests/cache/upload.pp
@@ -8,6 +8,7 @@ class profile::cache::upload(
     $statsd_server = hiera('statsd'),
     $cache_route_table = hiera('cache::route_table'),
     $backend_warming = hiera('cache::backend_warming', false),
+    $admission_policy = hiera('profile::cache::base::admission_policy', 'nhw'),
 ) {
     require ::profile::cache::base
 
@@ -55,7 +56,9 @@ class profile::cache::upload(
     })
 
     $fe_vcl_config = merge($common_vcl_config, {
-        'pass_random' => false,
+        'pass_random'      => false,
+        'admission_policy' => $admission_policy,
+        'fe_mem_gb'        => $::varnish::common::fe_mem_gb,
     })
 
     # See T145661 for storage binning rationale
