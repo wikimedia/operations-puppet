74050c6233c8b5ae291d3d7f5131a587941c50ac
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-10-24 13:43:17 +0300

    builder: Disable docker's iptables handling
    Ship the entire docker iptables configuration via ferm.
    This is to make sure docker and ferm play nice together.
    For now we only want this on builder hosts, which is why we don't put it in
    profile::docker::engine. This relies on settings iptables: false in docker
    
    Change-Id: I8db5ebfb637a3d7492c9ae5c554f5d7446dfb74f

--- modules/profile/manifests/docker/builder.pp
+++ modules/profile/manifests/docker/builder.pp
@@ -53,4 +53,14 @@ class profile::docker::builder(
         group  => 'root',
         mode   => '0500'
     }
+
+    # Ship the entire docker iptables configuration via ferm
+    # This is here to make sure docker and ferm play nice together.
+    # For now we only want this on builder hosts, which is why we don't put it in
+    # profile::docker::engine. This relies on settings iptables: false in docker
+    ferm::conf { 'docker-ferm':
+        ensure => present,
+        prio   => 20,
+        source => 'puppet:///modules/profile/docker/builder-docker-ferm',
+    }
 }
