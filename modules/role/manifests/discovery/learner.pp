44f9a9c6f99146bbd3367cb6310fa2af840bdb27
Mikhail Popov <mpopov@wikimedia.org>
2017-10-26 11:04:25 -0700

    role::discovery::learner: Remove deep learning profile
    Since we have the discovery::deep_learner role for that stuff :)
    
    Bug: T178096
    Change-Id: I2f30cadf989c340f30e0489ff9c46fbb9ec4dac8

--- modules/role/manifests/discovery/learner.pp
+++ modules/role/manifests/discovery/learner.pp
@@ -1,7 +1,6 @@
 # = Class: role::discovery::learner
 #
-# This class sets up R and Python packages for machine learning, including with
-# deep neural networks.
+# This class sets up R and Python packages for machine learning.
 #
 # Due to the outdated version of R on the currently available Ubuntu version
 # (Trusty), it is recommended to only apply this role to instances running on
@@ -12,7 +11,6 @@ class role::discovery::learner {
     # include ::standard
     # include ::base::firewall
     include ::profile::discovery_computing::machine_learning
-    include ::profile::discovery_computing::deep_learning
 
     system::role { 'role::discovery::learner':
         ensure      => 'present',
