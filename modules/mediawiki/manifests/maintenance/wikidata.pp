03dbfd7bc994b9de1220ac1ae540e3ff7ea6bafb
Marius Hoch <hoo@online.de>
2017-10-26 19:36:56 +0200

    Log Wikidata dispatchers on terbium
    Given we had problems with this several times in the past, let's
    log their output and exit code (at least for now).
    
    Bug: T179060
    Change-Id: I0a6318b610708f39b36affb1249754b5796d0418

--- modules/mediawiki/manifests/maintenance/wikidata.pp
+++ modules/mediawiki/manifests/maintenance/wikidata.pp
@@ -8,14 +8,24 @@ class mediawiki::maintenance::wikidata( $ensure = present ) {
         mode   => '0664',
     }
 
+    $dispatch_log_file = "/var/log/wikidata/dispatchChanges-wikidatawiki.log";
+    file { $dispatch_log_file:
+        ensure  => 'file',
+        owner   => $::mediawiki::users::web,
+        group   => $::mediawiki::users::web,
+        mode    => '0664',
+        require => File['/var/log/wikidata'],
+    }
+
     # Starts a dispatcher instance every 3 minutes
     # They will run for a maximum of 9 minutes, so we can only have 3 concurrent instances.
     # This handles inserting jobs into client job queue, which then process the changes
     cron { 'wikibase-dispatch-changes4':
         ensure  => $ensure,
-        command => '/usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dispatchChanges.php --wiki wikidatawiki --max-time 540 --batch-size 420 --dispatch-interval 25 --lock-grace-interval 200 >/dev/null 2>&1',
+        command => "echo \$\$: Starting dispatcher >> ${dispatch_log_file}; /usr/local/bin/mwscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dispatchChanges.php --wiki wikidatawiki --max-time 540 --batch-size 420 --dispatch-interval 25 --lock-grace-interval 200 >> ${dispatch_log_file} 2>&1; echo \$\$: Dispatcher exited with $? >> ${dispatch_log_file}",
         user    => $::mediawiki::users::web,
         minute  => '*/3',
+        require => File[$dispatch_log_file],
     }
 
     cron { 'wikibase-dispatch-changes-test':
