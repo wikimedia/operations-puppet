d20e36c51224a998024c9a3d4b81be4dd0e0ed07
Keith Herron <kherron@wikimedia.org>
2017-10-27 11:54:49 -0400

    puppet: update hiera function call in role::puppetmaster::puppetdb
    function_hiera breaks under puppet 4. update to call_function.
    
    Bug: T179171
    Change-Id: I2e8f7cc4b3c150cce847cc30fb205e7c543dbcb6

--- modules/role/manifests/puppetmaster/puppetdb.pp
+++ modules/role/manifests/puppetmaster/puppetdb.pp
@@ -37,7 +37,7 @@ class role::puppetmaster::puppetdb (
     }
 
     # Only the TLS-terminating nginx proxy will be exposed
-    $puppetmasters_ferm = inline_template('<%= scope.function_hiera([\'puppetmaster::servers\']).values.flatten(1).map { |p| p[\'worker\'] }.sort.join(\' \')%>')
+    $puppetmasters_ferm = inline_template('<%= scope.call_function(:hiera, [\'puppetmaster::servers\']).values.flatten(1).map { |p| p[\'worker\'] }.sort.join(\' \')%>')
     ferm::service { 'puppetdb':
         proto   => 'tcp',
         port    => 443,
