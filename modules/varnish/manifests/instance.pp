808338c970188ba1c4ca5214383d633daccc641f
Emanuele Rocca <ema@wikimedia.org>
2017-10-27 16:44:27 +0200

    varnish: monitor child process restarts
    Change-Id: I2e7ce67d0cf653d6507d0c12d152aeefb4831202

--- modules/varnish/manifests/instance.pp
+++ modules/varnish/manifests/instance.pp
@@ -44,6 +44,21 @@ define varnish::instance(
         $runtime_parameters = $::varnish::common::fe_runtime_params
     }
 
+    # Raise an icinga warning if the Varnish child process has been started
+    # more than once; that means it has died unexpectedly. Critical if it has
+    # been started more than 3 times.
+    $prometheus_labels = "instance=~'${::hostname}:.*',layer='${inst}'"
+    $grafana_dashboard = "https://grafana.wikimedia.org/dashboard/db/varnish-machine-stats?var-server=${::hostname}&var-datasource=${::site}%20prometheus%2Fops"
+
+    monitoring::check_prometheus { "varnish-${inst}-check-child-start":
+        description    => "Varnish Child Restarted ${grafana_dashboard}",
+        query          => "varnish_mgt_child_start{${prometheus_labels}}",
+        method         => 'gt',
+        warning        => 1,
+        critical       => 3,
+        prometheus_url => "http://prometheus.svc.${::site}.wmnet/ops",
+    }
+
     $runtime_params = join(prefix($runtime_parameters, '-p '), ' ')
 
     varnish::common::directors { $vcl:
