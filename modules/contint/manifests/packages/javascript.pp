8b1726dd8c54fc188521b07e8500deda9db8350b
Paladox <thomasmulhall410@yahoo.com>
2017-10-27 16:39:55 +0000

    javascript: Remove the npm package
    The nodejs package installs npm and also npm pin check, will install
    the version we want installed.
    
    This fixes using this class on stretch.
    
    Change-Id: Ia83dfbda46dd85d004fc1d9a42c1c542229d0401

--- modules/contint/manifests/packages/javascript.pp
+++ modules/contint/manifests/packages/javascript.pp
@@ -6,11 +6,6 @@ class contint::packages::javascript {
 
     requires_realm('labs')
 
-    package { 'npm':
-        ensure => present,
-        notify => Exec['pin npm'],
-    }
-
     # DO NOT CHANGE VERSION WITHOUT INVOLVING Krinkle OR hashar
     #
     # https://wikitech.wikimedia.org/wiki/Nova_Resource:Integration/Setup
@@ -22,14 +17,11 @@ class contint::packages::javascript {
     # called 'node'
     package { 'nodejs-legacy':
         ensure => present,
-        # Brings up 'nodejs' from upstream which we then override
-        before => Package['npm'],
     }
 
     exec { 'pin npm':
         command => "/usr/bin/npm install -g npm@${versions['npm']}",
         onlyif  => "/usr/bin/test \"`/usr/bin/npm --version`\" != \"${versions['npm']}\"",
-        require => Package['npm'],
     }
 
 }
