15967edc006496f201a71000b4426d346d493f58
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-10-30 13:23:44 +0100

    docker::builder: also install python3-wheel
    Change-Id: I5a21d9c8167f466e2e9f20d335a451dbbc8d3bc1

--- modules/docker/manifests/builder.pp
+++ modules/docker/manifests/builder.pp
@@ -2,7 +2,11 @@
 #
 # Installs all tools needed to build docker images for the WMF.
 class docker::builder () {
-    require_package(['python3', 'python3-virtualenv', 'virtualenv', 'python3-pip'])
+    require_package([
+        'python3', 'python3-virtualenv',
+        'virtualenv', 'python3-pip',
+        'python3-wheel',
+    ])
     # Scap target for docker-pkg
     file { '/srv/deployment':
         ensure => directory,
