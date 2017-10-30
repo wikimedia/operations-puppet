52f390a8130616b468013e5b5159f6d0fe789cd1
Ariel T. Glenn <ariel@wikimedia.org>
2017-10-30 11:28:58 +0200

    allow dumpsdata hosts to sync to/from ms1001 and dataset1001
    Change-Id: Ifb9faeb17d0038687d453effc232dcd0a0597708

--- modules/profile/manifests/dumps/rsyncer_peer.pp
+++ modules/profile/manifests/dumps/rsyncer_peer.pp
@@ -1,5 +1,5 @@
 class profile::dumps::rsyncer_peer {
-    $peer_hosts = 'dataset1001.wikimedia.org ms1001.wikimedia.org'
+    $peer_hosts = 'dataset1001.wikimedia.org ms1001.wikimedia.org dumpsdata1001.eqiad.wmnet dumpsdata1002.eqiad.wmnet'
     class {'::dumps::rsync::default':}
     class {'::dumps::rsync::memfix':}
     class {'::dumps::rsync::peers': hosts_allow => $peer_hosts}
