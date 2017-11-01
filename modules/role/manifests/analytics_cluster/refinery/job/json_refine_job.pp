143c20c4e2ec401aa0e623d3783d98c06234ea48
Ottomata <aotto@wikimedia.org>
2017-11-01 15:26:39 -0400

    Fix (again) refinery_job_jar var
    Change-Id: Ib7c05ea896c97bc6f088ec5f964c2eb08efd1582

--- modules/role/manifests/analytics_cluster/refinery/job/json_refine_job.pp
+++ modules/role/manifests/analytics_cluster/refinery/job/json_refine_job.pp
@@ -33,9 +33,9 @@ define role::analytics_cluster::refinery::job::json_refine_job (
     $refinery_path = $role::analytics_cluster::refinery::path
 
     # If $refinery_job_jar not given, use the symlink at artifacts/refinery-job.jar
-    $refinery_job_jar_path = $refinery_job_jar ? {
-        undef => $refinery_job_jar,
-        default => "${refinery_path}/artifacts/refinery-job.jar",
+    $_refinery_job_jar = $refinery_job_jar ? {
+        undef   => "${refinery_path}/artifacts/refinery-job.jar",
+        default => $refinery_job_jar,
     }
 
     $job_name = "json_refine_${title}"
@@ -54,11 +54,11 @@ define role::analytics_cluster::refinery::job::json_refine_job (
 
     # Only send email report if $email_to provided.
     $email_opts = $email_to ? {
-        undef => '',
+        undef   => '',
         default => "--send-email-report --to-emails ${email_to}"
     }
 
-    $command = "PYTHONPATH=${refinery_path}/python ${refinery_path}/bin/is-yarn-app-running || /usr/bin/spark-submit --master yarn --deploy-mode client --driver-memory ${spark_driver_memory} --conf spark.dynamicAllocation.maxExecutors=${spark_max_executors} --class org.wikimedia.analytics.refinery.job.JsonRefine --name ${job_name} ${refinery_job_jar_path} --parallelism ${parallelism} --since ${since} ${whitelist_blacklist_opt} ${email_opts} --input-base-path ${input_base_path} --input-regex '${input_regex}' --input-capture '${input_capture}' --output-base-path ${output_base_path} --output-database ${output_database} >> ${log_file} 2>&1"
+    $command = "PYTHONPATH=${refinery_path}/python ${refinery_path}/bin/is-yarn-app-running || /usr/bin/spark-submit --master yarn --deploy-mode client --driver-memory ${spark_driver_memory} --conf spark.dynamicAllocation.maxExecutors=${spark_max_executors} --class org.wikimedia.analytics.refinery.job.JsonRefine --name ${job_name} ${_refinery_job_jar} --parallelism ${parallelism} --since ${since} ${whitelist_blacklist_opt} ${email_opts} --input-base-path ${input_base_path} --input-regex '${input_regex}' --input-capture '${input_capture}' --output-base-path ${output_base_path} --output-database ${output_database} >> ${log_file} 2>&1"
 
     cron { $job_name:
         command  => $command,
