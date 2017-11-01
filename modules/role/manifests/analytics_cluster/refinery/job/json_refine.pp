a4629266da4db772c56fadd7c00415f4b288a5a6
Ottomata <aotto@wikimedia.org>
2017-11-01 14:10:20 -0400

    Refine Eventlogging analytics and eventbus data into Hive tables
    Bug: T162610
    Change-Id: Iba0da8144da8ac4085908cacacb0cfd77b41a916

---
+++ modules/role/manifests/analytics_cluster/refinery/job/json_refine.pp
@@ -0,0 +1,31 @@
+# == Class role::analytics_cluster::refinery::job::json_refine
+# Install cron jobs for Spark JsonRefine jobs.  These jobs
+# refine JSON data imported into Hadoop from Kafka using Camus into
+# Parquet backed Hive tables.
+#
+class role::analytics_cluster::refinery::job::json_refine {
+    require ::role::analytics_cluster::refinery
+
+    # Refine EventLogging Analytics (capsule based) data.
+    role::analytics_cluster::refinery::job::json_refine_job { 'eventlogging_analytics':
+        input_base_path  => '/wmf/data/raw/eventlogging',
+        input_regex      => 'eventlogging_(.+)/hourly/(\\d+)/(\\d+)/(\\d+)/(\\d+)',
+        input_capture    => 'table,year,month,day,hour',
+        output_base_path => '/wmf/data/event',
+        output_database  => 'event',
+        table_blacklist  => '^Edit|ChangesListHighlights$',
+        minute           => 20,
+    }
+
+    # Refine EventBus data.
+    role::analytics_cluster::refinery::job::json_refine_job { 'eventlogging_eventbus':
+        input_base_path  => '/wmf/data/raw/eventbus',
+        # 'datacenter' is extracted from the input path into a Hive table partition
+        input_regex      => '.*(eqiad|codfw)_(.+)/hourly/(\\d+)/(\\d+)/(\\d+)/(\\d+)',
+        input_capture    => 'datacenter,table,year,month,day,hour',
+        output_base_path => '/wmf/data/event',
+        output_database  => 'event',
+        table_blacklist  => '^mediawiki_page_properties_change$',
+        minute           => 10,
+    }
+}
