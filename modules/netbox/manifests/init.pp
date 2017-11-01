1290e063546de6c5073b207b995c5e28f8eddc4e
Arzhel Younsi <ayounsi@wikimedia.org>
2017-11-01 13:58:47 -0700

    [WIP] Puppetize Netbox
     - Setup apache with Let's encrypt
     - Installs Postgres
     - Installs configuration files (main+LDAP)
     - Add monitoring
     - Configures uwsgi
    
    Bug: T170144
    Change-Id: Ia3354fcf251952a27d58eba3487043d8d4bd26fe

---
+++ modules/netbox/manifests/init.pp
@@ -0,0 +1,68 @@
+# == Class: netbox::base
+#
+# Installs Netbox
+#
+class netbox(
+    $secret_key,
+    $ldap_password,
+    $db_password,
+    $debug=false,
+    $port=8001,
+    $admins=false,
+    $config_path = '/srv/deployment/netbox/deploy',
+    $venv_path = '/srv/deployment/netbox/venv',
+    $directory = '/srv/deployment/netbox/deploy/netbox',
+    $ensure='present',
+
+) {
+
+  require_package('virtualenv', 'python3-dev',
+                  'libldap2-dev',
+                  'build-essential', 'python3-pip',
+                  'libsasl2-dev', 'libssl-dev')
+
+
+  file { "${directory}/netbox/netbox/configuration.py":
+      ensure  => $ensure,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0555',
+      content => template('netbox/configuration.py.erb'),
+  }
+
+  file { "${directory}/netbox/netbox/ldap_config.py":
+      ensure  => $ensure,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0555',
+      content => template('netbox/ldap_config.py.erb'),
+  }
+
+  service::uwsgi { 'netbox':
+      port            => $port,
+      deployment_user => 'deploy-librenms',
+      config          => {
+          need-plugins => 'python3',
+          chdir        => $directory,
+          venv         => $venv_path,
+          wsgi         => "${directory}/netbox/netbox/wsgi.py",
+          vacuum       => true,
+          http-socket  => "127.0.0.1:${port}",
+          # T170189: make sure Python has a sane default encoding
+          env          => [
+              'LANG=C.UTF-8',
+              'PYTHONENCODING=utf-8',
+          ],
+      },
+      healthcheck_url => '/',
+      icinga_check    => false,
+      sudo_rules      => [
+          'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-netbox restart',
+          'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-netbox start',
+          'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-netbox status',
+          'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-netbox stop',
+      ],
+  }
+
+
+}
