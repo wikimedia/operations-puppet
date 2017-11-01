9cdc4948cc0fb0e3b86eedfaa82e36e3488abf3d
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-01 20:39:06 +0200

    make dumps snapshot host roles more role-like
    [WIP] at least shove some of the stuff into profiles, make it sorta kinda
    follow the coding guidelines.
    
    Bug: T175528
    Change-Id: If74ea6eded1a8a219c9210b4818b7eb1c0a82132

---
+++ modules/profile/manifests/dumps/generation/worker/common.pp
@@ -0,0 +1,18 @@
+class profile::dumps::generation::worker::common {
+    # mw packages and dependencies
+    require ::profile::mediawiki::scap_proxy
+    require ::profile::mediawiki::common
+    require ::profile::mediawiki::nutcracker
+
+    # dataset server nfs mount, config files,
+    # stages files, dblists, html templates
+    class { '::dumps::deprecated::user': }
+    class { '::snapshot::dumps': }
+
+    # scap3 deployment of dump scripts
+    scap::target { 'dumps/dumps':
+        deploy_user => 'datasets',
+        manage_user => false,
+        key_name    => 'dumpsdeploy',
+    }
+}
---
+++ modules/profile/manifests/dumps/generation/worker/monitor.pp
@@ -0,0 +1,7 @@
+class profile::dumps::generation::worker::monitor(
+    $do_monitor = hiera('snapshot::dumps::monitor'),
+) {
+    if $do_monitor {
+        class { '::snapshot::dumps::monitor': }
+    }
+}
