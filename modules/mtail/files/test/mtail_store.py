d5e092a635b861774a5e1538bb48a1a163a47bc8
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-11-03 16:55:37 +0100

    mtail: add test scaffolding
    Provide unittest-based tests for catching regressions in mtail rules.
    
    Bug: T179565
    Change-Id: I4f3f41125f41f95eee028feb7f08453bd5f9b452

---
+++ modules/mtail/files/test/mtail_store.py
@@ -0,0 +1,51 @@
+import json
+import subprocess
+
+
+class MtailMetricStore(object):
+    def __init__(self, progs, logs):
+        self._store = {}
+        self._progs = progs
+        self._logs = logs
+        self.parse_metric_store(self.run_mtail()[0])
+
+    def run_mtail(self):
+        stdout, stderr = subprocess.Popen(
+                ['mtail', '-one_shot', '-one_shot_metrics', '-logtostderr',
+                 '-progs', self._progs, '-logs', self._logs], stderr=subprocess.PIPE,
+                stdout=subprocess.PIPE).communicate()
+        return stdout, stderr
+
+    def parse_metric_store(self, output):
+        metrics_store = []
+
+        in_json = False
+        for line in output.splitlines():
+            if in_json:
+                metrics_store.append(line)
+            if line.startswith('Metrics store:{'):
+                in_json = True
+                metrics_store.append('{')
+
+        self._store = json.loads(''.join(metrics_store))
+
+    def get_metric(self, name):
+        if name not in self._store:
+            raise ValueError('metric %s not found in store', name)
+        return MtailMetric(self._store[name][0].get('Keys', []),
+                           self._store[name][0]['LabelValues'][0].get('Labels', []),
+                           self._store[name][0]['LabelValues'][0]['Value']['Value'])
+
+
+class MtailMetric(object):
+    def __init__(self, keys, labels, value):
+        self._keys = keys
+        self._labels = labels
+        self._value = value
+        self._labelpairs = self.get_labelpairs(keys, labels)
+
+    def get_labelpairs(self, keys, labels):
+        res = []
+        for k, v in zip(keys, labels):
+            res.append('%s=%s' % (k, v))
+        return res
