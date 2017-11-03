423a2c2037a7feebacf7c208ea2f09447b5dc200
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-03 17:19:49 +0200

    cleanup old dumps on dumps web servers, part one
    This commit sets up the needed directories and config files, as well
    as making sure we have the various lists of wikis according to size,
    since we keep more dump runs for small wikis than for large ones, for
    example.
    
    A script to remove oldest dumps as needed, and a cron job to run that
    script, will be added in a later commit.
    
    Bug: T178893
    Change-Id: I3a063418e84ba006b9cbb1b0dfc2ce7e50c69386

--- modules/dumps/manifests/web/xmldumps_active.pp
+++ modules/dumps/manifests/web/xmldumps_active.pp
@@ -1,11 +1,14 @@
 class dumps::web::xmldumps_active(
-    $do_acme = true,
-    $datadir = undef,
-    $publicdir = undef,
-    $otherdir = undef,
-    $logs_dest = undef,
+    $do_acme          = true,
+    $datadir          = undef,
+    $publicdir        = undef,
+    $otherdir         = undef,
+    $logs_dest        = undef,
     $htmldumps_server = undef,
-    $xmldumps_server = undef,
+    $xmldumps_server  = undef,
+    $wikilist_url     = undef,
+    $wikilist_dir     = undef,
+    $user             = undef,
 ) {
     # active web server
     class {'::dumps::web::xmldumps':
@@ -21,4 +24,14 @@ class dumps::web::xmldumps_active(
     class {'::dumps::web::rsync::nginxlogs':
         dest   => $logs_dest,
     }
+
+    # only the active web server needs to cleanup old files
+    # rsync between peers will take care of the other hosts
+    class {'::dumps::web::cleanups::xml_cleanup':
+        wikilist_url => $wikilist_url,
+        wikilist_dir => $wikilist_dir,
+        publicdir    => $publicdir,
+        user         => $user,
+    }
+
 }
