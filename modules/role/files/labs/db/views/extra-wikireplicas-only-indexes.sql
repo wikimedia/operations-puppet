a18d3804143f123290bf256c90e1696f5f301c94
Bryan Davis <bd808@wikimedia.org>
2017-11-03 13:33:02 -0600

    wikireplicas: Add partial index for page_props.pp_value
    Index the first 767 bytes of the page_props.pp_value BLOB column.
    
    Bug: T140609
    Change-Id: Idd071708c69b168083a1a87ad4142fbac426187a

--- modules/role/files/labs/db/views/extra-wikireplicas-only-indexes.sql
+++ modules/role/files/labs/db/views/extra-wikireplicas-only-indexes.sql
@@ -6,3 +6,14 @@
 -- mysql -A $db -e "ALTER TABLE $db.archive ADD KEY user_timestamp (ar_user,ar_timestamp);"; done
 
 ALTER TABLE archive ADD KEY `user_timestamp` (`ar_user`,`ar_timestamp`);
+
+-- Additional index T140609
+-- Deployed with:
+-- mysql -BN -A information_schema -e \
+-- "SELECT table_schema FROM tables WHERE table_name='page_props' and table_schema like '%wik%' and table_type='BASE TABLE'" \
+-- | while read db; do \
+-- s="ALTER TABLE $db.page_props ADD KEY pp_value_prefix (pp_value(767));"; \
+-- echo "$s"; \
+-- mysql -A $db -e "$s"; done
+
+ALTER TABLE page_props ADD KEY `pp_value_prefix` (`pp_value`(767));
