4a716ab063a348e35cc12dc6b7549c770a01d746
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-03 23:40:21 +0200

    enable rsyncs to dataset1001 from dumpsdata hosts
    Change-Id: I373fe0d3ad4db41e7af40aed243ca4efc4800e3b

--- modules/profile/manifests/dumps/rsyncer.pp
+++ modules/profile/manifests/dumps/rsyncer.pp
@@ -2,7 +2,7 @@ class profile::dumps::rsyncer(
     $rsync_clients = hiera('dumps_web_rsync_server_clients'),
 ) {
     $stats_hosts = 'stat1005.eqiad.wmnet stat1006.eqiad.wmnet'
-    $peer_hosts = 'dataset1001.wikimedia.org ms1001.wikimedia.org'
+    $peer_hosts = 'dataset1001.wikimedia.org ms1001.wikimedia.org dumpsdata1001.eqiad.wmnet dumpsdata1002.eqiad.wmnet'
     $phab_hosts = 'phab1001.eqiad.wmnet'
     $mwlog_hosts = 'mwlog1001.eqiad.wmnet 2620:0:861:103:1618:77ff:fe33:4ac0 mwlog2001.codfw.wmnet 2620:0:860:103:1618:77ff:fe4e:3159'
     class {'::dumps::rsync::default':}
