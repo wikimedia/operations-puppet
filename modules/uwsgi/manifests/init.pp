9d2911a06cb71b92f089b680475260615ba08657
Arzhel Younsi <ayounsi@wikimedia.org>
2017-11-03 17:19:32 -0700

    uwsgi: fix dependency for stretch
    Change-Id: Ie10a5977eab24c893f0a03f0c15429515f74fa2c

--- modules/uwsgi/manifests/init.pp
+++ modules/uwsgi/manifests/init.pp
@@ -8,8 +8,10 @@ class uwsgi {
     # There are 30+ uWSGI plug-ins, installable via the dependency package
     # 'uwsgi-plugins-all'. But I'm going to go out on a limb here and predict
     # that we won't use any except these two.  -- OL
-    if os_version('debian >= jessie') {
+    if os_version('debian == jessie') {
         $plugins = [ 'uwsgi-plugin-python', 'uwsgi-plugin-python3', 'uwsgi-plugin-rack-ruby2.1' ]
+    } elsif os_version('debian >= stretch') {
+        $plugins = [ 'uwsgi-plugin-python', 'uwsgi-plugin-python3', 'uwsgi-plugin-rack-ruby2.3' ]
     } else {
         $plugins = [ 'uwsgi-plugin-python', 'uwsgi-plugin-python3', 'uwsgi-plugin-rack-ruby1.9.1' ]
     }
