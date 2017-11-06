bc1617d56ac31866515c3ecd3ef8d20e2924e475
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-06 17:54:48 +0200

    fixups for the dumps generator rsync service
    including: actually deploy the script that does the rsync
    
    Change-Id: Icf057af58c156fb4a71d244294600e5c8c8719f3

--- modules/dumps/manifests/generation/server/rsyncer.pp
+++ modules/dumps/manifests/generation/server/rsyncer.pp
@@ -2,10 +2,18 @@ class dumps::generation::server::rsyncer(
     $dumpsdir = undef,
     $remotedirs = undef,
 )  {
-  base::service_unit { 'dumps-rsyncer':
-    ensure    => 'present',
-    systemd   => systemd_template('dumps-rsync-peers'),
-    upstart   => upstart_template('dumps-rsync-peers'),
-    subscribe => File['/usr/local/bin/rsync-to-peers.sh'],
-  }
+    file { '/usr/local/bin/rsync-to-peers.sh':
+        ensure => 'present',
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/dumps/generation/rsync-to-peers.sh',
+    }
+
+    base::service_unit { 'dumps-rsyncer':
+        ensure    => 'present',
+        systemd   => systemd_template('dumps-rsync-peers'),
+        upstart   => upstart_template('dumps-rsync-peers'),
+        subscribe => File['/usr/local/bin/rsync-to-peers.sh'],
+    }
 }
