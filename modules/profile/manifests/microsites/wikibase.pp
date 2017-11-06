48965685cfc3767817ef2ee537e0ab3d35949c08
Daniel Zahn <dzahn@wikimedia.org>
2017-11-06 03:21:47 -0800

    wikibase: restrict http access to cache_misc
    Change-Id: Id16894a072ea9049337e7a2458f5bc8d8d686f41

--- modules/profile/manifests/microsites/wikibase.pp
+++ modules/profile/manifests/microsites/wikibase.pp
@@ -7,8 +7,9 @@ class profile::microsites::wikibase(
     class { '::base::firewall': }
 
     ferm::service { 'wikibase_http':
-        proto => 'tcp',
-        port  => '80',
+        proto  => 'tcp',
+        port   => '80',
+        srange => '$CACHE_MISC',
     }
 
     class { '::apache': }
