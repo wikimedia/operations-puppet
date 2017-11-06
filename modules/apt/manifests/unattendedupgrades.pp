9e9c5e1c06c97a2400ebcdb8f572c22f7d1dda5d
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2017-11-06 15:00:08 +0100

    apt: unattended upgrades for wikimedia packages by default
    Include the required bits to get unattended upgrades for wikimedia packages
    if using the apt::unattendedupgrades class.
    
    We want wikimedia deb packages being upgraded by unattended-upgrades also  in
    labs machines, as it happens with the contint machines. To achieve this,
    factor our the config into the common class apt::unattendedupgrades.
    Note that the only users of this class right now are:
    
     % git grep ::apt::unattendedupgrades
     modules/contint/manifests/packages/apt.pp:    include ::apt::unattendedupgrades
     modules/profile/manifests/base/labs.pp:    include ::apt::unattendedupgrades
    
    In the future, if someone would like to use this class but don't get wikimedia
    deb packages upgraded, we could add a parameter for the class to disable this.
    
    Bug: T177920
    Change-Id: I9ed2dd731fd2149bc5ae163213f1160b9efca056
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/apt/manifests/unattendedupgrades.pp
+++ modules/apt/manifests/unattendedupgrades.pp
@@ -9,4 +9,13 @@ class apt::unattendedupgrades($ensure=present) {
         key      => 'APT::Periodic::Unattended-Upgrade',
         value    => '1',
     }
+
+    apt::conf { 'unattended-upgrades-wikimedia':
+        priority => '51',
+        # Key with trailing '::' to append to potentially existing entry
+        key      => 'Unattended-Upgrade::Allowed-Origins::',
+        # lint:ignore:single_quote_string_with_variables
+        value    => 'Wikimedia:${distro_codename}-wikimedia',
+        # lint:endignore
+    }
 }
