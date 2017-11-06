93c6eee3eac1b7c17cffd2d44b7ca175e22f2bfe
Paladox <thomasmulhall410@yahoo.com>
2017-11-06 14:46:58 +0000

    planet: Doin't set http_proxy or https_proxy if on labs
    Change-Id: I774fa21e272c9b5f532d73831c3e61e53070a0fb

--- modules/planet/manifests/cronjob.pp
+++ modules/planet/manifests/cronjob.pp
@@ -1,6 +1,12 @@
 # defined type: a cronjob for planet-venus or rawdog feed updates per language
 define planet::cronjob {
 
+    if $planet::planet_http_proxy
+      $use_proxy = "http_proxy=\"${planet::planet_http_proxy}\" https_proxy=\$http_proxy"
+    } else {
+      $use_proxy = ''
+    }
+
     if os_version('debian == stretch') {
         $planet_bin = '/usr/bin/rawdog'
         $planet_cmd = "-d /etc/rawdog/${title}/ -v -u -w"
@@ -16,7 +22,7 @@ define planet::cronjob {
 
     cron { "update-${title}-planet":
         ensure  => 'present',
-        command => "http_proxy=\"${planet::planet_http_proxy}\" https_proxy=\$http_proxy ${planet_bin} ${planet_cmd} > ${planet_logfile} 2>&1",
+        command => "${use_proxy} ${planet_bin} ${planet_cmd} > ${planet_logfile} 2>&1",
         user    => 'planet',
         minute  => $minute,
         require => [
