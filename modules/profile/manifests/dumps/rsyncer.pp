70ceb0d23961ff2448d63e0ff973f5c51c8d4d3c
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-07 18:48:56 +0200

    move references to datasets use from dumps module out to profile
    Bug: T179942
    Change-Id: I790b0f6cb48c56f46c1a68980b74228211e8516e

--- modules/profile/manifests/dumps/rsyncer.pp
+++ modules/profile/manifests/dumps/rsyncer.pp
@@ -1,14 +1,27 @@
 class profile::dumps::rsyncer(
     $rsync_clients = hiera('dumps_web_rsync_server_clients'),
 ) {
+    class {'::dumps::rsync::common':
+        user  => 'datasets',
+        group => 'datasets',
+    }
+
     $stats_hosts = 'stat1005.eqiad.wmnet stat1006.eqiad.wmnet'
     $peer_hosts = 'dataset1001.wikimedia.org ms1001.wikimedia.org dumpsdata1001.eqiad.wmnet dumpsdata1002.eqiad.wmnet'
     $phab_hosts = 'phab1001.eqiad.wmnet'
     $mwlog_hosts = 'mwlog1001.eqiad.wmnet 2620:0:861:103:1618:77ff:fe33:4ac0 mwlog2001.codfw.wmnet 2620:0:860:103:1618:77ff:fe4e:3159'
     class {'::dumps::rsync::default':}
-    class {'::dumps::rsync::media': hosts_allow => $stats_hosts}
+    class {'::dumps::rsync::media':
+        hosts_allow => $stats_hosts,
+        user        => 'datasets',
+        deploygroup => 'wikidev',
+    }
     class {'::dumps::rsync::memfix':}
-    class {'::dumps::rsync::pagecounts_ez': hosts_allow => $stats_hosts}
+    class {'::dumps::rsync::pagecounts_ez':
+        hosts_allow => $stats_hosts,
+        user        => 'datasets',
+        deploygroup => 'wikidev',
+    }
     class {'::dumps::rsync::peers': hosts_allow => $peer_hosts}
     class {'::dumps::rsync::phab_dump': hosts_allow => $phab_hosts}
     $hosts_allow = join(concat($rsync_clients['ipv4']['external'], $rsync_clients['ipv6']['external']), ' ')
@@ -17,5 +30,9 @@ class profile::dumps::rsyncer(
         hosts_allow => $hosts_allow,
         publicdir   => $publicdir,
     }
-    class {'::dumps::rsync::slowparse_logs': hosts_allow => $mwlog_hosts}
+    class {'::dumps::rsync::slowparse_logs':
+        hosts_allow => $mwlog_hosts,
+        user        => 'datasets',
+        group       => 'wikidev',
+    }
 }
