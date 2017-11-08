fcd4b3b1af08fb5676c90c1e44fd5ea393ed6b8c
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-09 00:27:19 +0200

    clean up dumps web server rsync to its fallback
    No need for a python script any more, just rsync the relevant
    directories.
    
    Move the destination host name out to a profile.
    
    Bug: T179942
    Change-Id: I48493292def0c0630d960aaa403d2f87ff59137b

--- modules/dumps/manifests/copying/peers.pp
+++ modules/dumps/manifests/copying/peers.pp
@@ -1,20 +1,22 @@
-class dumps::copying::peers {
-    file { '/usr/local/bin/rsync-dumps.py':
+class dumps::copying::peers(
+    $desthost = undef,
+) {
+    file { '/usr/local/bin/rsync_from_webserver.sh':
         ensure => 'present',
         mode   => '0755',
         owner  => 'root',
         group  => 'root',
-        path   => '/usr/local/bin/rsync-dumps.py',
-        source => 'puppet:///modules/dumps/copying/rsync-dumps.py',
+        path   => '/usr/local/bin/rsync_from_webserver.sh',
+        source => 'puppet:///modules/dumps/copying/rsync_from_webserver.sh'
     }
 
     cron { 'rsync-dumps':
         ensure  => 'present',
         # filter out error messages about vanishing files, we don't want email for that
-        command => '/usr/bin/python /usr/local/bin/rsync-dumps.py 2>&1 | grep -v "vanished" ',
+        command => '/bin/bash /usr/local/bin/rsync_from_webserver.sh --desthost $desthost 2>&1 | grep -v "vanished" ',
         user    => 'root',
         minute  => '0',
         hour    => '*/2',
-        require => File['/usr/local/bin/rsync-dumps.py'],
+        require => File['/usr/local/bin/rsync_from_webserver.sh'],
     }
 }
