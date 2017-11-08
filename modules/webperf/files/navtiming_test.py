5ddb050d6954207b377982966da92c199906edc4
Timo Tijhof <krinklemail@gmail.com>
2017-11-08 12:43:43 -0800

    webperf: Refactor tests to directly associate expected data with cases
    Previously we had a file with all input and a file with all output,
    which is pretty hard to review and basically only helps catch errors
    when the intent is to change nothing in the output.
    
    Whenever we do make a change in the output, the expected data was
    sufficiently mixed up that one basically ends up just recreating
    it with the new input with no feasible way to review.
    
    Hopefully this new structure will make it easier to review by
    having the input and expected output directly associated with
    each another, and in the same file.
    
    Change-Id: Ibb6c11476535e45490a5147294099d1965a3f970

--- modules/webperf/files/navtiming_test.py
+++ modules/webperf/files/navtiming_test.py
@@ -14,6 +14,10 @@ import navtiming
 #   python navtiming_test.py
 #
 class TestNavTiming(unittest.TestCase):
+    # When passing a 'msg' to assert methods, display that in
+    # addition to the value diff, not instead of the diff.
+    longMessage = True
+
     def test_parse_ua(self):
         data_path = os.path.join(os.path.dirname(__file__), 'navtiming_ua_data.yaml')
         with open(data_path) as data_file:
@@ -29,19 +33,27 @@ class TestNavTiming(unittest.TestCase):
 
     def test_handlers(self):
         fixture_path = os.path.join(os.path.dirname(__file__), 'navtiming_fixture.yaml')
-        expected_path = os.path.join(os.path.dirname(__file__), 'navtiming_expected.txt')
         with open(fixture_path) as fixture_file:
-            fixture = yaml.safe_load(fixture_file)
-            actual = []
-            for meta in fixture:
-                f = navtiming.handlers.get(meta['schema'])
-                assert f is not None
-                for stat in f(meta):
-                    actual.append(stat)
-            with open(expected_path) as expected_file:
+            cases = yaml.safe_load(fixture_file)
+            for key, case in cases.items():
+                if isinstance(case['input'], list):
+                    messages = case['input']
+                else:
+                    # Wrap in list if input is just one event
+                    messages = list([case['input']])
+                actual = []
+                # print "---", key # debug
+                for meta in messages:
+                    f = navtiming.handlers.get(meta['schema'])
+                    assert f is not None
+                    for stat in f(meta):
+                        # print stat # debug
+                        actual.append(stat)
+                # print "" # debug
                 self.assertItemsEqual(
                     actual,
-                    expected_file.read().splitlines()
+                    case['expect'],
+                    key
                 )
 
 
