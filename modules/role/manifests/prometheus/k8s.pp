538b22e59c6715c48974972cbfca60a6fb3d4a38
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-11-09 17:55:29 +0200

    Prometheus: add kubernetes node cadvisor job
    This is exactly the same as the k8s-node job with the added replacment
    of metrics path to /metrics/cadvisor unconditionally
    
    Change-Id: Ieea98078299adbd68fa4e546507f8c4a65e3e770

--- modules/role/manifests/prometheus/k8s.pp
+++ modules/role/manifests/prometheus/k8s.pp
@@ -85,6 +85,35 @@ class role::prometheus::k8s (
                 },
             ]
         },
+        {
+            'job_name'              => 'k8s-node-cadvisor',
+            'bearer_token_file'     => $bearer_token_file,
+            # Force read-only API for node servers. Unfortunately this is not
+            # encrypted, so force scheme HTTP
+            'scheme'                => 'http',
+            'metrics_path'          => '/metrics/cadvisor',
+            'kubernetes_sd_configs' => [
+                {
+                    'api_server'        => "https://${master_host}:6443",
+                    'bearer_token_file' => $bearer_token_file,
+                    'role'              => 'node',
+                },
+            ],
+            'relabel_configs'       => [
+                # Map kubernetes node labels to prometheus metric labels
+                {
+                    'action' => 'labelmap',
+                    'regex'  => '__meta_kubernetes_node_label_(.+)',
+                },
+                {
+                    'action'        => 'replace',  # Redundant but clearer
+                    'source_labels' => ['__address__'],
+                    'target_label'  => '__address__',
+                    'regex'         => '([\d\.]+):(\d+)',
+                    'replacement'   => "\${1}:10255",
+                },
+            ]
+        },
     ]
 
     prometheus::server { 'k8s':
