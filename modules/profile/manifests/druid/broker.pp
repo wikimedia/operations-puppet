1e00934fc39b6f16c91440187bc054c3e0b2b877
elukey <ltoscano@wikimedia.org>
2017-11-10 16:09:11 +0100

    profile::druid::broker: add prometheus jmx exporter config (jvm only)
    Bug: T177459
    Change-Id: Ied9513222bf5256c70b428228c8d9a196a09bc1c

--- modules/profile/manifests/druid/broker.pp
+++ modules/profile/manifests/druid/broker.pp
@@ -9,11 +9,29 @@ class profile::druid::broker(
     $properties         = hiera('profile::druid::broker::properties'),
     $env                = hiera('profile::druid::broker::env'),
     $ferm_srange        = hiera('profile::druid::broker::ferm_srange'),
-    $monitoring_enabled = hiera('profile::druid::monitoring_enabled'),
     $daemon_autoreload  = hiera('profile::druid::daemons_autoreload'),
+    $monitoring_enabled = hiera('profile::druid::broker::monitoring_enabled'),
 ) {
 
     require ::profile::druid::common
+
+    # If monitoring is enabled, then include the monitoring profile and set $java_opts
+    # for exposing the Prometheus JMX Exporter in the Kafka Broker process.
+    if $monitoring_enabled {
+        include ::profile::druid::broker::monitoring
+        $broker_extra_java_opts = ::profile::druid::broker::monitoring::broker_extra_java_opts
+
+        if $env['DRUID_EXTRA_JVM_OPTS'] {
+            $monitoring_env_vars = {
+                'DRUID_EXTRA_JVM_OPTS' => "${env['DRUID_EXTRA_JVM_OPTS']} ${broker_extra_java_opts}"
+            }
+        } else {
+            $monitoring_env_vars = {
+                'DRUID_EXTRA_JVM_OPTS' => $broker_extra_java_opts
+            }
+        }
+        $env = merge($env, $monitoring_env_vars)
+    }
 
     # Druid Broker Service
     class { '::druid::broker':
---
+++ modules/profile/manifests/druid/broker/monitoring.pp
@@ -0,0 +1,18 @@
+# Class: profile::druid::broker::monitoring
+#
+# Sets up Prometheus based monitoring (only jvm) for the druid broker.
+#
+class profile::druid::broker::monitoring (
+    $prometheus_nodes        = hiera('prometheus_nodes'),
+) {
+    $jmx_exporter_config_file = '/etc/druid/jvm_prometheus_jmx_exporter.yaml'
+    $prometheus_jmx_exporter_broker_port = 8182
+    $broker_extra_java_opts = "-javaagent:/usr/share/java/prometheus/jmx_prometheus_javaagent.jar=${::ipaddress}:${prometheus_jmx_exporter_broker_port}:${jmx_exporter_config_file}"
+    profile::prometheus::jmx_exporter { "druid_broker_${::hostname}":
+        hostname         => $::hostname,
+        port             => $prometheus_jmx_exporter_broker_port,
+        prometheus_nodes => $prometheus_nodes,
+        config_file      => $jmx_exporter_config_file,
+        source           => 'puppet:///modules/profile/druid/jvm_prometheus_jmx_exporter.yaml',
+    }
+}
