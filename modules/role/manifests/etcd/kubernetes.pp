7f6d9c7aa26bf7506ad2ca017d722aa5b8180d5b
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-11-10 15:52:51 +0200

    Add system::role to role::etcd::kubernetes
    It's missing one, add per our puppet guidelines
    
    Change-Id: I41e4b659f1687d69633969a07f57984d177e0421

--- modules/role/manifests/etcd/kubernetes.pp
+++ modules/role/manifests/etcd/kubernetes.pp
@@ -5,4 +5,8 @@ class role::etcd::kubernetes {
     include ::base::firewall
     include ::profile::etcd
     include ::profile::etcd::auth
+
+    system::role { 'role::etcd::kubernetes':
+        description => 'kubernetes etcd cluster member'
+    }
 }
