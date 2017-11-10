3bc54c052b2c70f4fd454a0228878d02b62afee5
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-11-10 15:38:10 +0200

    profile::etcd::auth: Rename hiera key
    To conform to our puppet coding guidelines. Seems like this was not
    really defined anywhere
    
    Change-Id: Ica666c66f8ba652242e43f9c3d5fa49eb7bdd4ee

--- modules/profile/manifests/etcd/auth.pp
+++ modules/profile/manifests/etcd/auth.pp
@@ -4,7 +4,7 @@
 # it is actually deprecated and profile::etcd::proxy should be used instead.
 class profile::etcd::auth(
     $enabled = hiera('profile::etcd::auth::enabled', false),
-    $root_password = hiera('etcd::auth::common::root_password', undef),
+    $root_password = hiera('profile::etcd::auth::root_password', undef),
 ){
     class { '::etcd::auth::common':
         root_password => $root_password,
