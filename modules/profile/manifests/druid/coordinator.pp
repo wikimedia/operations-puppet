5d0e9d62f4bc1fa95285efccfdc3d222e2d7def6
elukey <ltoscano@wikimedia.org>
2017-11-13 10:33:11 +0100

    profile::druid::*: add prometheus jvm monitoring via jmx exporter
    Bug: T177459
    Change-Id: Ic7d1f95ae8578582e254705b345258d0b87f8f56

--- modules/profile/manifests/druid/coordinator.pp
+++ modules/profile/manifests/druid/coordinator.pp
@@ -3,17 +3,36 @@
 class profile::druid::coordinator(
     $properties         = hiera('profile::druid::coordinator::properties'),
     $env                = hiera('profile::druid::coordinator::env'),
-    $monitoring_enabled = hiera('profile::druid::monitoring_enabled'),
     $daemon_autoreload  = hiera('profile::druid::daemons_autoreload'),
     $ferm_srange        = hiera('profile::druid::ferm_srange'),
+    $monitoring_enabled = hiera('profile::druid::coordinator::monitoring_enabled'),
 ) {
 
     require ::profile::druid::common
 
+    # If monitoring is enabled, then include the monitoring profile and set $java_opts
+    # for exposing the Prometheus JMX Exporter in the Druid Broker process.
+    if $monitoring_enabled {
+        include ::profile::druid::monitoring::coordinator
+        $java_opts = $::profile::druid::monitoring::coordinator::java_opts
+
+        if $env['DRUID_EXTRA_JVM_OPTS'] {
+            $monitoring_env_vars = {
+                'DRUID_EXTRA_JVM_OPTS' => "${env['DRUID_EXTRA_JVM_OPTS']} ${java_opts}"
+            }
+        } else {
+            $monitoring_env_vars = {
+                'DRUID_EXTRA_JVM_OPTS' => $java_opts
+            }
+        }
+    } else {
+        $monitoring_env_vars = {}
+    }
+
     # Druid coordinator Service
     class { '::druid::coordinator':
         properties       => $properties,
-        env              => $env,
+        env              => merge($env, $monitoring_env_vars),
         should_subscribe => $daemon_autoreload,
     }
 
--- modules/profile/manifests/druid/monitoring/broker.pp
+++ modules/profile/manifests/druid/monitoring/broker.pp
@@ -5,7 +5,7 @@
 class profile::druid::monitoring::broker(
     $prometheus_nodes        = hiera('prometheus_nodes'),
 ) {
-    $jmx_exporter_config_file = '/etc/druid/jvm_prometheus_jmx_exporter.yaml'
+    $jmx_exporter_config_file = '/etc/druid/jvm_prometheus_broker_jmx_exporter.yaml'
     $prometheus_jmx_exporter_broker_port = 8182
     $java_opts = "-javaagent:/usr/share/java/prometheus/jmx_prometheus_javaagent.jar=${::ipaddress}:${prometheus_jmx_exporter_broker_port}:${jmx_exporter_config_file}"
     profile::prometheus::jmx_exporter { "druid_broker_${::hostname}":
