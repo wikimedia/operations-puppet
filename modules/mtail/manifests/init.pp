a59ae4da2e30b6efe3afd7f4cef88cd9bd2c0768
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-11-13 13:20:02 +0100

    mtail: allow changing running group
    Also run as group 'adm' on mx servers to be able to read exim logs.
    
    Bug: T179565
    Change-Id: I13c53fb27799553e6cdfe1ccb501fcafc7deeb59

--- modules/mtail/manifests/init.pp
+++ modules/mtail/manifests/init.pp
@@ -22,6 +22,7 @@ class mtail (
   $graphite_hostport = 'graphite-in.eqiad.wmnet:2003',
   $graphite_prefix = "mtail.${::hostname}.",
   $enabled = '1',
+  $group = 'root',
 ) {
     validate_array($logs)
     validate_re($port, '^[0-9]+$')
