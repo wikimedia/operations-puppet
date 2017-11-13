472316b7534fa3f0d9e95193878e93c1821c2ba9
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-11-13 13:43:48 +0100

    Icinga: allow to set display_name
    * In order to add links to Grafana dashboards in the Icinga alerts based
      on Grafana and Prometheus thresholds, allow to set the Icinga
      display_name field as a union of the description and a dashboard_link.
    * The way naggen2 works, the new parameter should not be added to the
      generated Icinga configuration file unless it's set, avoiding to make
      the generated configuration larger.
    * The added dashboard_link parameter will be made mandatory once all the
      calls will have it.
    
    Bug: T170353
    Change-Id: I87c6daa29994f77b957ca04cd4af409121635f56

--- modules/monitoring/manifests/graphite_anomaly.pp
+++ modules/monitoring/manifests/graphite_anomaly.pp
@@ -23,17 +23,18 @@
 #   }
 #
 # == Parameters
-# $description          - Description of icinga alert
-# $metric               - graphite metric name
-# $warning              - alert warning datapoints
-# $critical             - alert critical datapoints
-# $check_window         - the number of datapoints on which the check
-#                         is performed. Defaults to 100.
-# $graphite_url         - URL of the graphite server.
-# $timeout              - Timeout for the http query to
-#                         graphite. Defaults to 10 seconds
-# over                  - check only for values above the limit
-# under                 - check only for values below the limit
+# $description    - Description of icinga alert
+# $metric         - graphite metric name
+# $warning        - alert warning datapoints
+# $critical       - alert critical datapoints
+# $check_window   - the number of datapoints on which the check
+#                   is performed. Defaults to 100.
+# $graphite_url   - URL of the graphite server.
+# $timeout        - Timeout for the http query to
+#                   graphite. Defaults to 10 seconds
+# $over           - check only for values above the limit
+# $under          - check only for values below the limit
+# $dashboard_link - Link to the Grafana dashboard for this alarm
 # $host
 # $retries
 # $group
@@ -49,21 +50,22 @@ define monitoring::graphite_anomaly(
     $metric,
     $warning,
     $critical,
-    $check_window          = 100,
-    $graphite_url          = 'https://graphite.wikimedia.org',
-    $timeout               = 10,
-    $over                  = false,
-    $under                 = false,
-    $host                  = $::hostname,
-    $retries               = 3,
-    $group                 = undef,
-    $ensure                = present,
-    $nagios_critical       = false,
-    $passive               = false,
-    $freshness             = 36000,
-    $check_interval        = 1,
-    $retry_interval        = 1,
-    $contact_group         = 'admins'
+    $check_window    = 100,
+    $graphite_url    = 'https://graphite.wikimedia.org',
+    $timeout         = 10,
+    $over            = false,
+    $under           = false,
+    $host            = $::hostname,
+    $retries         = 3,
+    $group           = undef,
+    $ensure          = present,
+    $nagios_critical = false,
+    $passive         = false,
+    $freshness       = 36000,
+    $check_interval  = 1,
+    $retry_interval  = 1,
+    $contact_group   = 'admins',
+    $dashboard_link  = undef,
 )
 {
 
@@ -79,6 +81,12 @@ define monitoring::graphite_anomaly(
 
     if $metric =~ /'/ {
         fail("single quotes will be stripped from graphite metric ${metric}, consider using double quotes")
+    }
+
+    if $dashboard_link {
+        $display_name = "${description} - ${dashboard_link}"
+    } else {
+        $display_name = undef
     }
 
     # checkcommands.cfg's check_graphite_anomaly command has
@@ -103,5 +111,6 @@ define monitoring::graphite_anomaly(
         check_interval => $check_interval,
         retry_interval => $retry_interval,
         contact_group  => $contact_group,
+        display_name   => $display_name,
     }
 }
