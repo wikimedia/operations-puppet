6a7753a14ac3cb66593eabbad30e8ac72e184751
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-11-13 20:13:51 +0100

    Revert "Deploy MjoLniR with new deploy repository"
    There is still a mismatch between MjoLniR and mjolnir, which makes scap
    fail to do init the repo on relforge. Let's revert and investigate further.
    
    This reverts commit bc8f26a35d07e39fa32f356be590624d6d738f00.
    
    Change-Id: I86cf0546dc9d4b367c1dcdd468466f6a31b1e9b3

--- modules/profile/manifests/mjolnir/kafka_daemon.pp
+++ modules/profile/manifests/mjolnir/kafka_daemon.pp
@@ -9,11 +9,18 @@ class profile::mjolnir::kafka_daemon(
     # it is named just 'eqiad'.
     $kafka_config = kafka_config('eqiad'),
 ) {
-    class { 'mjolnir': }
+    scap::target { 'relforge/mjolnir':
+      deploy_user => 'deploy-service',
+    }
+
+    # This is a limited subset of what the full mjolnir package requires because
+    # the daemon is a small part of the overall application. The daemon only needs
+    # to read/write kafka topics and send requests to localhost.
+    require_package('python-kafka', 'python-requests')
 
     systemd::service { 'mjolnir-kafka-daemon':
         content => template('profile/mjolnir/kafka-daemon.service.erb'),
-        require => Scap::Target['search/mjolnir/deploy'],
+        require => Scap::Target['relforge/mjolnir'],
     }
 
 }
--- modules/role/manifests/elasticsearch/analytics.pp
+++ modules/role/manifests/elasticsearch/analytics.pp
@@ -1,7 +1,5 @@
 # Supports CirrusSearch usage on the analytics cluster
 class role::elasticsearch::analytics {
-    class { 'mjolnir': }
-
     # wikimedia/discovery/analytics will be deployed to this node
     scap::target { 'wikimedia/discovery/analytics':
         deploy_user => 'deploy-service',
