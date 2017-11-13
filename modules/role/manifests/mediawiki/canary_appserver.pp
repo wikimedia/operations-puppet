2116078e94dbda0b543a00f85e189270be6080db
Marius Hoch <hoo@online.de>
2017-11-13 19:02:52 +0100

    Include php5 packages on canary hosts
    Similar to deployment/ maintenance hosts.
    
    Change-Id: Ia03a714bbe5b74859b4b5cda04e128d3996cdaed

--- modules/role/manifests/mediawiki/canary_appserver.pp
+++ modules/role/manifests/mediawiki/canary_appserver.pp
@@ -5,5 +5,9 @@ class role::mediawiki::canary_appserver {
     # include the deployment scripts because mwscript can occasionally be useful
     # here: T112174
     include scap::scripts
+
+    # maintenance/ dump scripts still run php5 often, so allow testing that,
+    # similar to the deployment hosts.
+    include ::mediawiki::packages::php5
 }
 
