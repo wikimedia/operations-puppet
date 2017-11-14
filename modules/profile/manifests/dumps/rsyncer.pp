af68b332bd3578b7c07906b9d88eae9324d0213c
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-14 18:31:36 +0200

    add dumps rsync user and path variables to hiera
    These vary depending on host; the labstore boxes will have a somewhat
    different configuration.
    
    Bug: T171541
    Change-Id: I54542e5bc2634ffb1f000fe44fefa8da81cfb5ab

--- modules/profile/manifests/dumps/rsyncer.pp
+++ modules/profile/manifests/dumps/rsyncer.pp
@@ -1,9 +1,11 @@
 class profile::dumps::rsyncer(
     $rsync_clients = hiera('dumps_web_rsync_server_clients'),
+    $rsyncer_settings = hiera('profile::dumps::rsyncer'),
 ) {
-    $user = 'datasets'
-    $group = 'datasets'
-    $deploygroup = 'wikidev'
+    $user = $rsyncer_settings['dumps_user']
+    $group = $rsyncer_settings['dumps_group']
+    $deploygroup = $rsyncer_settings['dumps_deploygroup']
+    $mntpoint = $rsyncer_settings['dumps_mntpoint']
 
     $stats_hosts = 'stat1005.eqiad.wmnet stat1006.eqiad.wmnet'
     $peer_hosts = 'dataset1001.wikimedia.org ms1001.wikimedia.org dumpsdata1001.eqiad.wmnet dumpsdata1002.eqiad.wmnet labstore1006.wikimedia.org'
@@ -12,7 +14,6 @@ class profile::dumps::rsyncer(
 
     $hosts_allow = join(concat($rsync_clients['ipv4']['external'], $rsync_clients['ipv6']['external']), ' ')
 
-    $mntpoint = '/data'
     $publicdir = "${mntpoint}/xmldatadumps/public"
     $otherdir = "${publicdir}/other"
 
--- modules/profile/manifests/dumps/rsyncer_peer.pp
+++ modules/profile/manifests/dumps/rsyncer_peer.pp
@@ -1,10 +1,11 @@
-class profile::dumps::rsyncer_peer {
-    $user = 'datasets'
-    $group = 'datasets'
+class profile::dumps::rsyncer_peer(
+    $rsyncer_peer_settings = hiera('profile::dumps::rsyncer_peer'),
+) {
+    $user = $rsyncer_peer_settings['dumps_user']
+    $group = $rsyncer_peer_settings['dumps_group']
+    $mntpoint = $rsyncer_peer_settings['dumps_mntpoint']
 
     $peer_hosts = 'dataset1001.wikimedia.org ms1001.wikimedia.org dumpsdata1001.eqiad.wmnet dumpsdata1002.eqiad.wmnet labstore1006.wikimedia.org'
-
-    $datapath = '/data'
 
     class {'::dumps::rsync::common':
         user  => $user,
@@ -14,6 +15,6 @@ class profile::dumps::rsyncer_peer {
     class {'::dumps::rsync::memfix':}
     class {'::dumps::rsync::peers':
         hosts_allow => $peer_hosts,
-        datapath    => $datapath,
+        datapath    => $mntpoint,
     }
 }
