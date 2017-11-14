5d835871a497ec1689cbe6eaa51d9106bded9f4b
Jaime Crespo <jcrespo@wikimedia.org>
2017-11-14 11:37:33 +0100

    Refactor evenlogging database hosts definition
    * Use only one role for the new hosts
    * Move common definitions to profiles (old classes are not removed
      yet, until we confirm they are unused, but that is the final goal)
    * Remove helper roles like monitoring or firewall
    * Fix firewall not being applied currently (could be a breaking change)
    * Apply standard/ferm on the main role
    * Create ::profile::base::firewall until the firewall mess is solved
      somehow
    
    Bug: T177405
    Change-Id: I746d24b68de1db2f1efa4dde1353b915cbad56a7

---
+++ modules/profile/manifests/mariadb/monitor.pp
@@ -0,0 +1,10 @@
+class profile::mariadb::monitor {
+
+    class { 'mariadb::monitor_disk':
+        contact_group => 'admins',
+    }
+
+    class { 'mariadb::monitor_process':
+        contact_group => 'admins',
+    }
+}
--- modules/role/manifests/mariadb/misc/eventlogging/replica.pp
+++ modules/role/manifests/mariadb/misc/eventlogging/replica.pp
@@ -1,5 +1,12 @@
 class role::mariadb::misc::eventlogging::replica {
+
+    include ::standard
+    include ::profile::base::firewall
+    include ::profile::mariadb::ferm
+    include ::profile::mariadb::monitor
+
     include ::profile::mariadb::misc::eventlogging::database
+    # custom manual replication setup
     include ::profile::mariadb::misc::eventlogging::replication
 
     system::role { 'role::mariadb::misc::eventlogging::replica':
