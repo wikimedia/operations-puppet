11887137596826191d294c85def4ba3e4733b4ca
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-14 15:05:15 +0200

    clean up the dumps rsyncer profiles
    Use vars instead of hardcoded strings everywhere.
    Group var declarations at the top.
    
    Change-Id: Id68278cbe65fe92064a8bc68688575d703c60be2

--- modules/profile/manifests/dumps/rsyncer.pp
+++ modules/profile/manifests/dumps/rsyncer.pp
@@ -1,48 +1,64 @@
 class profile::dumps::rsyncer(
     $rsync_clients = hiera('dumps_web_rsync_server_clients'),
 ) {
-    class {'::dumps::rsync::common':
-        user  => 'datasets',
-        group => 'datasets',
-    }
+    $user = 'datasets'
+    $group = 'datasets'
+    $deploygroup = 'wikidev'
 
     $stats_hosts = 'stat1005.eqiad.wmnet stat1006.eqiad.wmnet'
     $peer_hosts = 'dataset1001.wikimedia.org ms1001.wikimedia.org dumpsdata1001.eqiad.wmnet dumpsdata1002.eqiad.wmnet labstore1006.wikimedia.org'
     $phab_hosts = 'phab1001.eqiad.wmnet'
     $mwlog_hosts = 'mwlog1001.eqiad.wmnet 2620:0:861:103:1618:77ff:fe33:4ac0 mwlog2001.codfw.wmnet 2620:0:860:103:1618:77ff:fe4e:3159'
+
+    $hosts_allow = join(concat($rsync_clients['ipv4']['external'], $rsync_clients['ipv6']['external']), ' ')
+
+    $mntpoint = '/data'
+    $publicdir = "${mntpoint}/xmldatadumps/public"
+    $otherdir = "${publicdir}/other"
+
+    class {'::dumps::rsync::common':
+        user  => $user,
+        group => $group,
+    }
+
     class {'::dumps::rsync::default':}
+
     class {'::dumps::rsync::media':
         hosts_allow => $stats_hosts,
-        user        => 'datasets',
-        deploygroup => 'wikidev',
-        otherdir    => '/data/xmldatadumps/public/other',
+        user        => $user,
+        deploygroup => $deploygroup,
+        otherdir    => $otherdir,
     }
+
     class {'::dumps::rsync::memfix':}
+
     class {'::dumps::rsync::pagecounts_ez':
         hosts_allow => $stats_hosts,
-        user        => 'datasets',
-        deploygroup => 'wikidev',
-        otherdir    => '/data/xmldatadumps/public/other',
+        user        => $user,
+        deploygroup => $deploygroup,
+        otherdir    => $otherdir,
     }
+
     class {'::dumps::rsync::peers':
         hosts_allow => $peer_hosts,
-        datapath    => '/data',
+        datapath    => $mntpoint,
     }
+
     class {'::dumps::rsync::phab_dump':
         hosts_allow => $phab_hosts,
-        otherdir    => '/data/xmldatadumps/public/other',
+        otherdir    => $otherdir,
     }
-    $hosts_allow = join(concat($rsync_clients['ipv4']['external'], $rsync_clients['ipv6']['external']), ' ')
-    $publicdir = '/data/xmldatadumps/public'
+
     class {'::dumps::rsync::public':
         hosts_allow => $hosts_allow,
         publicdir   => $publicdir,
-        otherdir    => '/data/xmldatadumps/public/other',
+        otherdir    => $otherdir,
     }
+
     class {'::dumps::rsync::slowparse_logs':
         hosts_allow => $mwlog_hosts,
-        user        => 'datasets',
-        group       => 'datasets',
-        otherdir    => '/data/xmldatadumps/public/other',
+        user        => $user,
+        group       => $group,
+        otherdir    => $otherdir,
     }
 }
--- modules/profile/manifests/dumps/rsyncer_peer.pp
+++ modules/profile/manifests/dumps/rsyncer_peer.pp
@@ -1,13 +1,19 @@
 class profile::dumps::rsyncer_peer {
-    class {'::dumps::rsync::common':
-        user  => 'datasets',
-        group => 'datasets',
-    }
+    $user = 'datasets'
+    $group = 'datasets'
+
     $peer_hosts = 'dataset1001.wikimedia.org ms1001.wikimedia.org dumpsdata1001.eqiad.wmnet dumpsdata1002.eqiad.wmnet labstore1006.wikimedia.org'
+
+    $datapath = '/data'
+
+    class {'::dumps::rsync::common':
+        user  => $user,
+        group => $group,
+    }
     class {'::dumps::rsync::default':}
     class {'::dumps::rsync::memfix':}
     class {'::dumps::rsync::peers':
         hosts_allow => $peer_hosts,
-        datapath    => '/data',
+        datapath    => $datapath,
     }
 }
