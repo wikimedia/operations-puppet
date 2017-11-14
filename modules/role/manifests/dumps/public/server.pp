c26caedd6faba94153ffbb8a33322687cef54e2b
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-14 19:58:42 +0200

    enable dump rsyncs to/from labstore1006
    Prep for labstore1006 to become the public datasets
    webserver.
    
    While this will configure labstore1006 so that it may
    receive rsync requests (pulls or pushes) for various
    misc datasets, it does not enable them now.
    
    The only thing that will be rsynced to the host at this
    stage are the xml/sql dumps.
    
    Bug: T171541
    Change-Id: Iee2e5a70cd3181568dd3bf47507a45887b004fc5

--- modules/role/manifests/dumps/public/server.pp
+++ modules/role/manifests/dumps/public/server.pp
@@ -2,6 +2,7 @@ class role::dumps::public::server {
     system::role { 'dumps::public::server': description => 'labstore host in the public VLAN that serves Dumps to clients via NFS/Web/Rsync' }
 
     include ::standard
-
+    include ::profile::base::firewall
     include ::profile::dumps::public_server
+    include ::profile::dumps::rsyncer
 }
