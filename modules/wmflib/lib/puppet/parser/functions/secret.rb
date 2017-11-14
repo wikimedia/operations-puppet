dbc1af002973d524351c6a626dcda71dcfb43771
Ottomata <aotto@wikimedia.org>
2017-11-14 09:19:57 -0500

    Support looking up secrets in different modules
    e.g.
    
      content => secret('path/to/secret_key', 'certificates')
    
    Will get content of modules/certificates/secrets/path/to/secret_key
    
    Change-Id: I6eacad183402bffd4cc4346fd36949a7deeb1c06

--- modules/wmflib/lib/puppet/parser/functions/secret.rb
+++ modules/wmflib/lib/puppet/parser/functions/secret.rb
@@ -1,14 +1,27 @@
 require 'pathname'
 
+#
+# Usage in a file resource.
+#
+# Get content of 'modules/secret/secrets/path/to/secret_pw'
+#   content => secret('path/to/secret_pw')
+#
+# Get content of 'modules/certificates/secrets/path/to/secret_key'
+#   content => secret('path/to/secret_key', 'certificates')
+#
 module Puppet::Parser::Functions
   newfunction(:secret, :type => :rvalue) do |args|
     mod_name = 'secret'
     secs_subdir = '/secrets/'
 
-    if args.length != 1 || !args.first.is_a?(String)
-      fail(ArgumentError, 'secret(): exactly one string arg')
+    if args.length < 1 || args.length > 2 || !args.first.is_a?(String) !args[1].is_a?(String)
+      fail(ArgumentError, 'secret(): takes one or two string args')
     end
     in_path = args.first
+
+    if args.length == 2
+      mod_name = args[1]
+    end
 
     if mod = Puppet::Module.find(mod_name)
        mod_path = mod.path()
