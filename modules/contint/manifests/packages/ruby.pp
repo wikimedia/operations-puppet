0dc5f9866273c4f627b4fa36b36ee4e0110b68eb
Paladox <thomasmulhall410@yahoo.com>
2017-11-14 22:33:43 +0000

    contint: Fix support for stretch in ruby.pp
    Fix support for stretch
    
    Error: Could not retrieve catalog from remote server: Error 400 on
    SERVER: Duplicate declaration: Package[ruby2.1] is already declared in
    file /etc/puppet/modules/base/manifests/standard_packages.pp:119; cannot
    redeclare at /etc/puppet/modules/contint/manifests/packages/ruby.pp:26
    on node jenkins-slave-01.git.eqiad.wmflabs
    Warning: Not using cache on failed catalog
    
    Change-Id: I9ee4c6d39e4d94dedb79716599731e83ff488148

--- modules/contint/manifests/packages/ruby.pp
+++ modules/contint/manifests/packages/ruby.pp
@@ -15,26 +15,28 @@ class contint::packages::ruby {
         ensure => present,
     }
 
-    package { [
-        'ruby2.1',
-        'ruby2.1-dev',
-        'bundler',
-        # Used by PoolCounter tests (T152338)
-        'ruby-rspec',
-        'cucumber',
-        ]: ensure => present,
-    }
+    if os_version('debian == jessie') {
+      package { [
+          'ruby2.1',
+          'ruby2.1-dev',
+          'bundler',
+          # Used by PoolCounter tests (T152338)
+          'ruby-rspec',
+          'cucumber',
+          ]: ensure => present,
+      }
 
-    # JSDuck was built for Ubuntu ( T48236/ T82278 )
-    # It is a pain to rebuild for Jessie so give up (T95008), we will use
-    # bundler/rubygems instead
-    package { 'jsduck':
-        ensure   => present,
-        provider => 'gem',
-        require  => [
-            Package['ruby2.1-dev'],
-            Package['build-essential'],
-        ],
+      # JSDuck was built for Ubuntu ( T48236/ T82278 )
+      # It is a pain to rebuild for Jessie so give up (T95008), we will use
+      # bundler/rubygems instead
+      package { 'jsduck':
+          ensure   => present,
+          provider => 'gem',
+          require  => [
+              Package['ruby2.1-dev'],
+              Package['build-essential'],
+          ],
+      }
     }
 
 }
