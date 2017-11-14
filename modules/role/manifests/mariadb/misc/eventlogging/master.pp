95cdabe75712a11f7a04473948345e2b7d477116
Jaime Crespo <jcrespo@wikimedia.org>
2017-11-14 11:37:33 +0100

    Refactor evenlogging database hosts definition
    * Use only one role for the new hosts
    * Move common definitions to profiles (old classes are not removed
      yet, until we confirm they are unused, but that is the final goal)
    * Remove helper roles like monitoring or firewall
    * Fix firewall not being applied currently (could be a breaking change)
    * Apply standard/ferm on the main role
    
    Bug: T177405
    Change-Id: I746d24b68de1db2f1efa4dde1353b915cbad56a7

--- modules/role/manifests/mariadb/misc/eventlogging/master.pp
+++ modules/role/manifests/mariadb/misc/eventlogging/master.pp
@@ -1,4 +1,9 @@
 class role::mariadb::misc::eventlogging::master {
+    include ::standard
+    include ::base::firewall
+    include ::profile::mariadb::ferm
+    include ::profile::mariadb::monitor
+
     include ::profile::mariadb::misc::eventlogging::database
     system::role { 'role::mariadb::misc::eventlogging::master':
         description => 'Eventlogging Master datastore',
