775ae9af409caaced9a35bd6d8852bc276b0dd0b
Aaron Schulz <aschulz@wikimedia.org>
2017-11-14 20:01:04 -0800

    Add mcrouter module and mcrouter_wancache profile and enable on beta
    Change-Id: I8b881a036c121c010f1f2c8e42ddbf266630c1b2

---
+++ modules/mcrouter/manifests/monitoring.pp
@@ -0,0 +1,10 @@
+# == Class: mcrouter::monitoring
+#
+# Provisions Icinga alerts for mcrouter.
+#
+class mcrouter::monitoring {
+    nrpe::monitor_service { 'mcrouter':
+        description  => 'mcrouter process',
+        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u mcrouter -C mcrouter',
+    }
+}
