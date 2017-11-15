699ceb2415adbeed1e2c534d13fb15bc0b36a3f1
Aaron Schulz <aschulz@wikimedia.org>
2017-11-14 20:01:04 -0800

    Add mcrouter module and mcrouter_wancache profile and enable on beta
    Change-Id: I8b881a036c121c010f1f2c8e42ddbf266630c1b2

--- modules/role/manifests/mediawiki/common.pp
+++ modules/role/manifests/mediawiki/common.pp
@@ -3,4 +3,8 @@ class role::mediawiki::common {
     include ::profile::mediawiki::scap_proxy
     include ::profile::mediawiki::common
     include ::profile::mediawiki::nutcracker
+    # mcrouter testing (T151466)
+    if $::realm == 'labs' and os_version('debian == jessie') {
+        include ::profile::mediawiki::mcrouter_wancache
+    }
 }
