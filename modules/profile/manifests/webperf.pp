f2318e720941cfa0e4fd69d72ddcf31cc02687f7
Ottomata <aotto@wikimedia.org>
2017-11-15 16:41:08 -0500

    Move statsv varnishkafka and service to use main Kafka cluster(s)
    This gives statsv active/passive multi-DC support.
    
    After much IRC discussion, this allows for an active/passive statsv
    backed by the main Kafka clusters in eqiad and codfw.  varnishkafkas
    in all DCs produce statsv messages to the Kafka cluster name specified
    by profile::cache::kafka::statsv::kafka_cluster_name, which in production
    is set to 'main-eqiad'.
    
    statsv consumer instances will run in eqiad and codfw and consume
    from their local main Kafka clusters.  Since statsd is active/passive
    those statsv consumer instances will produce to the same active statsd
    instance, independent of which datacenter they run in.  I.e.
    if statsd is active in eqiad, both statsv in eqiad (consuming
    from main-eqiad) and statsv in codfw (consuming from main-codfw)
    will produce to statsd in eqiad.
    
    However, since all statsv varnishkafkas produce to the same
    Kafka cluster in an 'active' DC, only one statsv instance
    will have any messages to consume at any given time.
    
    If you plan to move the active statsd instance away from
    main-eqiad for an extended (permanent?) period of time, you should
    also change the value of profile::cache::kafka::statsv::kafka_cluster_name.
    
    Or, if you need to do maintenance on statsv for an extended period of time,
    you could route all varnishkafka produced statsv messages to e.g. main-codfw,
    and shut down the eqiad statsv consumers, and still get statsv messages
    in statsd.
    
    Bug: T179093
    Change-Id: I6c566c19fcdab004eec21384e6a5c136b3cf699c

--- modules/profile/manifests/webperf.pp
+++ modules/profile/manifests/webperf.pp
@@ -1,20 +1,29 @@
 class profile::webperf(
     $statsd = hiera('statsd')
 ){
-
     $statsd_parts = split($statsd, ':')
     $statsd_host = $statsd_parts[0]
     $statsd_port = $statsd_parts[1]
 
+    # statsv is on main kafka, not analytics or jumbo kafka.
+    # Note that at any given time, all statsv varnishkafka producers are
+    # configured to send to only one kafka cluster (usually main-eqiad).
+    # statsv in an inactive datacenter will not process any messages, as
+    # varnishkafka will not produce any messages to that DC's kafka cluster.
+    # This is configured by the value of the hiera param
+    # profile::cache::kafka::statsv::kafka_cluster_name when the statsv varnishkafka
+    # profile is included (as of this writing on text caches).
+    $kafka_main_config = kafka_config('main')
+    $kafka_main_brokers = $kafka_main_config['brokers']['string']
+    # Consume statsd metrics from Kafka and emit them to statsd.
+    class { '::webperf::statsv':
+        kafka_brokers => $kafka_main_brokers,
+        statsd        => $statsd,
+    }
+
     # Use brokers from this Kafka cluster to consume metrics.
     $kafka_config  = kafka_config('analytics')
     $kafka_brokers = $kafka_config['brokers']['string']
-
-    # Consume statsd metrics from Kafka and emit them to statsd.
-    class { '::webperf::statsv':
-        kafka_brokers => $kafka_brokers,
-        statsd        => $statsd,
-    }
 
     # Aggregate client-side latency measurements collected via the
     # NavigationTiming MediaWiki extension and send them to Graphite.
