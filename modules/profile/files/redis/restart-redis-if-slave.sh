e2657d4de014ca3666aca2abce998a843708e13d
elukey <ltoscano@wikimedia.org>
2017-11-16 12:52:00 +0100

    profile::redis::jobqueue: stagger redis slave restarts
    In T163337 a long investigation was made to figure out
    why the Redis Jobqueue shards get out of sync with their
    masters after some hours of work. We didn't find a permant
    fix for the issue, since it would have involved a major
    Redis upgrade in production and probably a review of all
    the Lua scripts that we are actually running, so we added
    daily restarts of all the Redis slaves at 1 AM.
    
    Configuration example for a Redis shard:
    
    rdb1001:shard1 --> master running in eqiad
    rdb1002:shard1 --> slave of rdb1001:shard1 running in eqiad
    rdb2001:shard1 --> slave of rdb1001:shard1 running in codfw
    rdb2002:shard1 --> slave of rdb2001:shard1 running in codfw
    
    At 1 AM all three slaves try to issue A SYNC to their masters,
    and it seems that this puts pressure on the eqiad masters.
    
    This patch forces eqiad slaves to be restarted at 1AM, meanwhile
    the codfw ones at 2AM. It also adds some sleep time between each
    redis shard restart, since restarting all the shards on slave in
    one go trigger multiple SYNC requests to the master host shards
    (that might hit the disk performances).
    
    Bug: T179684
    Change-Id: I58f1fb4b16f5947eecd0f89b075471e335e45de6

--- modules/profile/files/redis/restart-redis-if-slave.sh
+++ modules/profile/files/redis/restart-redis-if-slave.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Random sleep to stagger execution of this script
+sleep $(($RANDOM % 600))
+
 # Check if currently a slave
 for instance in "$@";
 do
@@ -8,5 +11,8 @@ do
     authpass=$(awk '{if ($1 == "requirepass") print $2}' "$_config")
     if redis-cli -h 127.0.0.1 -p "$instance" -a "$authpass" INFO replication | grep -q role:slave; then
         systemctl restart "redis-instance-tcp_${instance}.service"
+        # Avoid multiple SYNC requests to the master shards at the same time
+        # (that might hit disk performances and slow down the master host).
+        sleep 180
     fi
 done
