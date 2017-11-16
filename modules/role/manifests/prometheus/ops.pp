54351b82069195f53a3b40977953decdf8de2b3a
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-11-16 16:09:54 +0100

    prometheus: drop addr/alias redis_exporter labels
    Bug: T148637
    Change-Id: I667cb74ba41e8349377ae024beb1a47136653db6

--- modules/role/manifests/prometheus/ops.pp
+++ modules/role/manifests/prometheus/ops.pp
@@ -524,6 +524,16 @@ class role::prometheus::ops {
         'file_sd_configs' => [
           { 'files' => [ "${targets_path}/redis_*.yaml" ]}
         ],
+        # redis_exporter runs alongside each redis instance, thus drop the (uninteresting in this
+        # case) 'addr' and 'alias' labels
+        'metric_relabel_configs' => [
+          { 'source_labels' => ['addr'],
+            'action'        => 'labeldrop',
+          },
+          { 'source_labels' => ['alias'],
+            'action'        => 'labeldrop',
+          },
+        ],
       },
     ]
 
