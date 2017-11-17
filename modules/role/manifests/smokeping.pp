d909066c2ff87f0b54ba4586c0333fe57c3fb162
Daniel Zahn <dzahn@wikimedia.org>
2017-11-17 14:30:17 -0800

    smokeping: add $active_server parameter and use it
    Add a new parameter $active_server, like we already do for
    similar setups, to role class and actual smokeping class.
    
    Use the existing "netmon_server" Hiera value that is also
    used by other monitoring tools to determine the active server.
     (Alternative would be to have a separate smokeping_server setting).
    
    Use it to control whether the smokeping service is running or stopped.
    
    Also use it to determine which server should be the source of the rsync
    of RRD files. If the current FQDN equals active_server, do _not_ have
    an rsync-cron, otherwise (on one or multiple "passive" servers) do have
    an rsync cronjob and use $fqdn as destination and $active server as
    source. The passive server(s) pull from the active server.
    
    Bug: T180812
    Change-Id: Id1a5ec6d7a2dcf755eb9d82deb2e0e4b248c77be

--- modules/role/manifests/smokeping.pp
+++ modules/role/manifests/smokeping.pp
@@ -3,8 +3,21 @@ class role::smokeping {
 
     system::role { 'smokeping': description => 'smokeping server' }
 
-    include ::smokeping
-    include ::smokeping::web
+    $active_server = hiera('netmon_server')
+
+    if $active_server == $::fqdn {
+        $rsync_ensure = 'absent'
+    } else {
+        $rsync_ensure = 'present'
+    }
+
+    rsync::quickdatacopy { 'var-lib-smokeping':
+        ensure      => $rsync_ensure,
+        auto_sync   => true,
+        source_host => $active_server,
+        dest_host   => $::fqdn,
+        module_path => '/var/lib/smokeping',
+    }
 
     ferm::service { 'smokeping-http':
         proto  => 'tcp',
@@ -12,13 +25,11 @@ class role::smokeping {
         srange => '$CACHE_MISC',
     }
 
-    backup::set {'smokeping': }
+    backup::set { 'smokeping': }
 
-    rsync::quickdatacopy { 'var-lib-smokeping':
-        ensure      => present,
-        auto_sync   => true,
-        source_host => 'netmon2001.wikimedia.org',
-        dest_host   => 'netmon1002.wikimedia.org',
-        module_path => '/var/lib/smokeping',
+    class{ '::smokeping':
+        active_server => $active_server,
     }
+
+    class{ '::smokeping::web' }
 }
