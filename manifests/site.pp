4fb6060a4f8074030923468b565916efac43226d
Jaime Crespo <jcrespo@wikimedia.org>
2017-11-17 11:22:31 +0100

    wikireplicas: Repoint web and analytics back to the original servers
    Return dbproxyies used for wikireplicas back to the original state
    after labsdb1010 was successfuly cloned to labsdb1009.
    
    Bug: T179244
    Change-Id: I59a24ff46ff488b16bb165bc63b00144f6ecaff9

--- manifests/site.pp
+++ manifests/site.pp
@@ -714,10 +714,10 @@ node 'dbproxy1005.eqiad.wmnet' {
 node 'dbproxy1010.eqiad.wmnet' {
     class { '::role::mariadb::proxy::master':
         shard          => 'labsdb',
-        primary_name   => 'labsdb1011',
-        primary_addr   => '10.64.37.24',
-        secondary_name => 'labsdb1011',
-        secondary_addr => '10.64.37.24',
+        primary_name   => 'labsdb1009',
+        primary_addr   => '10.64.4.14',
+        secondary_name => 'labsdb1010',
+        secondary_addr => '10.64.37.23',
     }
 }
 
@@ -726,8 +726,8 @@ node 'dbproxy1011.eqiad.wmnet' {
         shard          => 'labsdb',
         primary_name   => 'labsdb1011',
         primary_addr   => '10.64.37.24',
-        secondary_name => 'labsdb1011',
-        secondary_addr => '10.64.37.24',
+        secondary_name => 'labsdb1010',
+        secondary_addr => '10.64.37.23',
     }
 }
 
