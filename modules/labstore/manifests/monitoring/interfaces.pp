17d3e5e3c1ddf63583189a8fcadcd6b520e1d31d
Riccardo Coccioli <rcoccioli@wikimedia.org>
2017-11-20 16:08:03 +0100

    Metric alarms: convert dashboad_link to array
    * Convert the dashboad_link parameter to dashboad_links, it must be an
      array.
    * Ensure that there is at least one item and that all items have a link
      to Grafana.
    * Populate the Icinga's notes_url parameter with all the URLs in
      dashboad_links, using the single-quoted syntax even when only one link
      is present, to force Icinga to use the 1-notes.gif image instead of
      the default notes.gif one.
    
    Bug: T170353
    Change-Id: I9d1f8b440844ad556281c0c30eac2c98422fe4ef

--- modules/labstore/manifests/monitoring/interfaces.pp
+++ modules/labstore/manifests/monitoring/interfaces.pp
@@ -16,43 +16,43 @@ class labstore::monitoring::interfaces(
     $int_throughput_crit = '106250000' # 850Mbps
 
     monitoring::graphite_threshold { 'network_out_saturated':
-        description    => 'Outgoing network saturation',
-        dashboard_link => 'https://grafana.wikimedia.org/dashboard/db/labs-monitoring',
-        metric         => "servers.${::hostname}.network.${monitor_iface}.tx_byte",
-        from           => '30min',
-        warning        => $int_throughput_warn,
-        critical       => $int_throughput_crit,
-        percentage     => '10',        # smooth over peaks
+        description     => 'Outgoing network saturation',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/labs-monitoring'],
+        metric          => "servers.${::hostname}.network.${monitor_iface}.tx_byte",
+        from            => '30min',
+        warning         => $int_throughput_warn,
+        critical        => $int_throughput_crit,
+        percentage      => '10',        # smooth over peaks
     }
 
     monitoring::graphite_threshold { 'network_in_saturated':
-        description    => 'Incoming network saturation',
-        dashboard_link => 'https://grafana.wikimedia.org/dashboard/db/labs-monitoring',
-        metric         => "servers.${::hostname}.network.${monitor_iface}.rx_byte",
-        from           => '30min',
-        warning        => $int_throughput_warn,
-        critical       => $int_throughput_crit,
-        percentage     => '10',        # smooth over peaks
+        description     => 'Incoming network saturation',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/labs-monitoring'],
+        metric          => "servers.${::hostname}.network.${monitor_iface}.rx_byte",
+        from            => '30min',
+        warning         => $int_throughput_warn,
+        critical        => $int_throughput_crit,
+        percentage      => '10',        # smooth over peaks
     }
 
     monitoring::graphite_threshold { 'high_iowait_stalling':
-        description    => 'Persistent high iowait',
-        dashboard_link => 'https://grafana.wikimedia.org/dashboard/db/labs-monitoring',
-        metric         => "servers.${::hostname}.cpu.total.iowait",
-        from           => '10min',
-        warning        => '40', # Based off looking at history of metric
-        critical       => '60',
-        percentage     => '50', # Ignore small spikes
+        description     => 'Persistent high iowait',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/labs-monitoring'],
+        metric          => "servers.${::hostname}.cpu.total.iowait",
+        from            => '10min',
+        warning         => '40', # Based off looking at history of metric
+        critical        => '60',
+        percentage      => '50', # Ignore small spikes
     }
 
     # Monitor for high load consistently, is a 'catchall'
     monitoring::graphite_threshold { 'high_load':
-        description    => 'High load average',
-        dashboard_link => 'https://grafana.wikimedia.org/dashboard/db/labs-monitoring',
-        metric         => "servers.${::hostname}.loadavg.01",
-        from           => '10min',
-        warning        => '16',
-        critical       => '24',
-        percentage     => '50', # Don't freak out on spikes
+        description     => 'High load average',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/labs-monitoring'],
+        metric          => "servers.${::hostname}.loadavg.01",
+        from            => '10min',
+        warning         => '16',
+        critical        => '24',
+        percentage      => '50', # Don't freak out on spikes
     }
 }
