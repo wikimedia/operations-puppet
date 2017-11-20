84889383465e594a192b35921d14ebdf3ce0ed5a
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2017-11-20 14:28:15 +0100

    apt: add --force-confold/--force-confdef dpkg option to apt calls
    This new configuration tells apt to call dpkg with '--force-confold' which will
    skip unnecesary prompts when upgrading packages and choose to keep old config
    files by default.
    
    The '--foce-confdef' option is also added for dpkg to don't prompt
    in cases where other prompts may arise (for example, a new conffile replacing
    an old conffile that we din't edit).
    
    In the systems using unattended-upgrades, we will see 2 new files:
    * /etc/apt/apt.conf.d/00dpkg-force-confdef, with content:
     Dpkg::Options:: "--force-confdef";
    * /etc/apt/apt.conf.d/00dpkg-force-confold, with content:
     Dpkg::Options:: "--force-confold";
    
    Bug: T180811
    Change-Id: Iab1a7219e8d129b1a39e172164e7a72430ea9552
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/apt/manifests/unattendedupgrades.pp
+++ modules/apt/manifests/unattendedupgrades.pp
@@ -3,6 +3,20 @@ class apt::unattendedupgrades($ensure=present) {
         ensure => $ensure,
     }
 
+    apt::conf { 'dpkg-force-confdef':
+        ensure   => present,
+        priority => '00',
+        key      => 'Dpkg::Options::',
+        value    => '--force-confdef',
+    }
+
+    apt::conf { 'dpkg-force-confold':
+        ensure   => present,
+        priority => '00',
+        key      => 'Dpkg::Options::',
+        value    => '--force-confold',
+    }
+
     apt::conf { 'auto-upgrades':
         ensure   => $ensure,
         priority => '20',
