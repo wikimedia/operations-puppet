44d03df48dc9bf354cdbb55135f080d3beca6915
Keith Herron <kherron@wikimedia.org>
2017-11-20 09:32:45 -0500

    icinga: add support for puppet 4 in backend puppetmaster https checks
    Bug: T180944
    Change-Id: Ifa0b210f2c8ecdeede121ea6ed962866f96a1113

--- modules/puppetmaster/manifests/monitoring.pp
+++ modules/puppetmaster/manifests/monitoring.pp
@@ -1,21 +1,29 @@
 # This class collects all alerts and metrics collection monitoring
 # for the puppemaster module.
 
-class puppetmaster::monitoring ($server_type = $::puppetmaster::server_type) {
+class puppetmaster::monitoring (
+    $server_type = $::puppetmaster::server_type,
+    $puppet_major_version = undef,
+){
 
     # monitor HTTPS on puppetmasters
     # Note that for frontends both 8140 and 8141 ports will be checked since
     # both will be used
+    $puppetmaster_check_uri = $puppet_major_version ? {
+        4       => '/puppet/v3',
+        default => '/',
+    }
+
     if $server_type == 'frontend' or $server_type == 'standalone' {
         monitoring::service { 'puppetmaster_https':
             description   => 'puppetmaster https',
-            check_command => 'check_https_port_status!8140!400',
+            check_command => "check_https_port_status!8140!400!${puppetmaster_check_uri}",
         }
     }
     if $server_type == 'frontend' or $server_type == 'backend' {
         monitoring::service { 'puppetmaster_backend_https':
             description   => 'puppetmaster backend https',
-            check_command => 'check_https_port_status!8141!400',
+            check_command => "check_https_port_status!8141!400!${puppetmaster_check_uri}",
         }
     }
 
