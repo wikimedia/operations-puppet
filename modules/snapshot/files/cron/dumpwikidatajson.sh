wikimedia/operations-puppet

commit 2edcf554ee8221958f9c2253df63564f4dfa47c3

snapshot: Use --no-cache for dumping Wikidata entities
This flag will work once 795350da2 is deployed, but
passing it earlier is fine (MW discards those).

Bug: T180048
Change-Id: Iac6fa3fb055834cebc1f10ba472d63671cb1322a

modules/snapshot/files/cron/dumpwikidatajson.sh
@@ -30,7 +30,8 @@ while true; do
 			set -o pipefail
 			errorLog=/var/log/wikidatadump/dumpwikidatajson-$filename-$i.log
 			# NOTE: We temporary set the shard size differently for each shard. T177486#3674942.
-			php5 $multiversionscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dumpJson.php --wiki wikidatawiki --shard $i --sharding-factor $shards --batch-size `expr $(expr $i + 1) \* 500` --snippet 2>> $errorLog | gzip -9 > $tempDir/wikidataJson.$i.gz
+			# Remove --no-cache once this runs on hhvm (or everything is back on Zend), see T180048.
+			php5 $multiversionscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dumpJson.php --wiki wikidatawiki --shard $i --sharding-factor $shards --batch-size `expr $(expr $i + 1) \* 500` --snippet 2 --no-cache >> $errorLog | gzip -9 > $tempDir/wikidataJson.$i.gz
 			exitCode=$?
 			if [ $exitCode -gt 0 ]; then
 				echo -e "\n\n(`date --iso-8601=minutes`) Process for shard $i failed with exit code $exitCode" >> $errorLog

modules/snapshot/files/cron/dumpwikidatardf.sh
@@ -56,7 +56,8 @@ while true; do
 		(
 			set -o pipefail
 			errorLog=/var/log/wikidatadump/dumpwikidata$dumpFormat-$filename-$i.log
-			php5 $multiversionscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dumpRdf.php --wiki wikidatawiki --shard $i --sharding-factor $shards --batch-size `expr $shards \* 500` --format $dumpFormat --flavor $dumpFlavor 2>> $errorLog | gzip -9 > $tempDir/wikidata$dumpFormat-$dumpName.$i.gz
+			# Remove --no-cache once this runs on hhvm (or everything is back on Zend), see T180048.
+			php5 $multiversionscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dumpRdf.php --wiki wikidatawiki --shard $i --sharding-factor $shards --batch-size `expr $shards \* 500` --format $dumpFormat --flavor $dumpFlavor --no-cache 2>> $errorLog | gzip -9 > $tempDir/wikidata$dumpFormat-$dumpName.$i.gz
 			exitCode=$?
 			if [ $exitCode -gt 0 ]; then
 				echo -e "\n\n(`date --iso-8601=minutes`) Process for shard $i failed with exit code $exitCode" >> $errorLog

commit 18dfe77b292288d4ccffaf1ec70c9a6b6548f4a3

fix missing redirect for wikidata json dumps
Fixes bug introduced by 2edcf554ee8221958f9c2253df63564f4dfa47c3

Bug: T180934
Change-Id: I859028695b66467156a9106a7f43a6de37617543

modules/snapshot/files/cron/dumpwikidatajson.sh
@@ -31,7 +31,7 @@ while true; do
 			errorLog=/var/log/wikidatadump/dumpwikidatajson-$filename-$i.log
 			# NOTE: We temporary set the shard size differently for each shard. T177486#3674942.
 			# Remove --no-cache once this runs on hhvm (or everything is back on Zend), see T180048.
-			php5 $multiversionscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dumpJson.php --wiki wikidatawiki --shard $i --sharding-factor $shards --batch-size `expr $(expr $i + 1) \* 500` --snippet 2 --no-cache >> $errorLog | gzip -9 > $tempDir/wikidataJson.$i.gz
+			php5 $multiversionscript extensions/Wikidata/extensions/Wikibase/repo/maintenance/dumpJson.php --wiki wikidatawiki --shard $i --sharding-factor $shards --batch-size `expr $(expr $i + 1) \* 500` --snippet 2 --no-cache 2>> $errorLog | gzip -9 > $tempDir/wikidataJson.$i.gz
 			exitCode=$?
 			if [ $exitCode -gt 0 ]; then
 				echo -e "\n\n(`date --iso-8601=minutes`) Process for shard $i failed with exit code $exitCode" >> $errorLog