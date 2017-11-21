803657cb3d715df879f77a8cecc3f33f972f5f9a
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-21 14:38:34 +0200

    rsync all dumps status files to web servers and unpack them periodically
    Make sure that the index.html files and various other dumps status files
    don't provide links to dumps files that haven't been copied over yet from
    internal servers.
    
    Bug: T179857
    Change-Id: I7fd30f713387da25d061a452786c71786f372a37

--- modules/dumps/files/generation/rsync-to-peers.sh
+++ modules/dumps/files/generation/rsync-to-peers.sh
@@ -78,10 +78,18 @@ IFS=$IFS_SAVE
 
 while [ 1 ]; do
 
+    # make xml/sql dumps tarball of all status files etc
+    tarballpath="${xmldumpsdir}/public/dumpstatusfiles.tar"
+    /usr/bin/find "${xmldumpsdir}/public/" -maxdepth 3 -regextype sed -regex '.*/*\.\(html\|json\|txt\)' | /usr/bin/xargs /bin/tar cvfp "$tarballpath"
+    /bin/gzip "$tarballpath"
+
     # rsync of xml/sql dumps for public wikis
     for dest in $xmlremotedirs_list; do
-	/usr/bin/rsync -a  --contimeout=600 --timeout=600 ${xmldumpsdir}/public/*html "$dest" > /dev/null 2>&1
-        /usr/bin/rsync -a  --contimeout=600 --timeout=600 --exclude='**bad/' --exclude='**save/' --exclude='**not/' --exclude='**temp/' --exclude='**tmp/' --exclude='*.inprog'  ${xmldumpsdir}/public/*wik* "$dest" > /dev/null 2>&1
+        /usr/bin/rsync -a  --contimeout=600 --timeout=600 --exclude='**bad/' --exclude='**save/' --exclude='**not/' --exclude='**temp/' --exclude='**tmp/' --exclude='*.inprog'  --exclude='*.html' --exclude='*.txt' --exclude-'*.json' ${xmldumpsdir}/public/*wik* "$dest" > /dev/null 2>&1
+	# send tarball over last, remote can unpack it when it notices the arrival
+	# this way, content of status and html files always reflects dump output already
+	# made available via rsync
+	/usr/bin/rsync -a  --contimeout=600 --timeout=600 "${tarballpath}.gz" "$dest" > /dev/null 2>&1
     done
 
     # rsync of misc dumps, not necessarily to/from the same tree as the public wikis
--- modules/role/manifests/dumps/web/xmldumps_fallback.pp
+++ modules/role/manifests/dumps/web/xmldumps_fallback.pp
@@ -6,6 +6,7 @@ class role::dumps::web::xmldumps_fallback {
     include ::profile::base::firewall
     include ::profile::dumps::web::xmldumps_fallback
     include ::profile::dumps::web::rsync_server
+    include ::profile::dumps::web::statusfiles_sync
     include ::profile::dumps::rsyncer_peer
     include ::profile::dumps::nfs::all
 
