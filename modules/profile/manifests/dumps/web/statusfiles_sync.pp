83ec6c9cbbaf00d0305c3fc8f0804ca5ab3fd86a
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-21 14:38:34 +0200

    rsync all dumps status files to web servers and unpack them periodically
    Make sure that the index.html files and various other dumps status files
    don't provide links to dumps files that haven't been copied over yet from
    internal servers.
    
    Bug: T179857
    Change-Id: I7fd30f713387da25d061a452786c71786f372a37

---
+++ modules/profile/manifests/dumps/web/statusfiles_sync.pp
@@ -0,0 +1,8 @@
+class profile::dumps::web::statusfiles_sync(
+    $rsyncer_peer_settings = hiera('profile::dumps::rsyncer_peer'),
+) {
+    $mntpoint = $rsyncer_peer_settings['dumps_mntpoint']
+    class {'::dumps::web::statusfiles':
+        $xmldumpsdir => "${mntpoint/xmldatadumps/public}",
+    }
+}
