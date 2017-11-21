394b6d81e41bd1df0104b50fbf5e8dd47501e2b4
Stanislav Malyshev <smalyshev@gmail.com>
2017-11-21 14:26:30 -0800

    Create script for automatic reload of categories
    Bug: T173772
    Change-Id: I96e0863aff73a3cece8c93c8f03ee4901759b7b1

--- modules/wdqs/manifests/gui.pp
+++ modules/wdqs/manifests/gui.pp
@@ -3,19 +3,25 @@
 # Provisions WDQS GUI
 #
 # == Parameters:
+# - $logstash_host: Where to send the logs for the service in syslog format
+# - $logstash_syslog_port: port on which to send logs in syslog format
 # - $package_dir:  Directory where the service is installed.
-# GUI files are expected to be under its gui/ directory.
+#   GUI files are expected to be under its gui/ directory.
 # - $data_dir: Where the data is installed.
-# - $logstash_host: Where to send the logs for the service in syslog format.
-#
+# - $log_dir: Directory where the logs go
+# - $username: Username owning the service
+# - $port: main GUI service port
+# - $additional_port: secondary port for internal requests
 class wdqs::gui(
     $logstash_host = undef,
     $logstash_syslog_port = 10514,
     $package_dir = $::wdqs::package_dir,
     $data_dir = $::wdqs::data_dir,
+    $log_dir = $::wdqs::log_dir,
     $username = $::wdqs::username,
     $port = 80,
     $additional_port = 8888,
+    $use_git_deploy = $::wdqs::use_git_deploy,
 ) {
 
     $alias_map = "${data_dir}/aliases.map"
@@ -25,7 +31,7 @@ class wdqs::gui(
         require => File[$alias_map],
     }
 
-    # List of namespace aliases in format: 
+    # List of namespace aliases in format:
     # ALIAS REAL_NAME
     # This map is generated manually or by category update script
     file { $alias_map:
@@ -44,4 +50,22 @@ class wdqs::gui(
         # Because nginx site creates /var/lib/nginx
         require => Nginx::Site['wdqs'],
     }
+
+    file { '/etc/wdqs/gui_vars.sh':
+        ensure  => present,
+        content => template('wdqs/cron/gui_vars.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+    }
+
+    file { '/usr/local/bin/reloadCategories.sh':
+        ensure  => present,
+        source  => 'puppet:///modules/wdqs/cron/reloadCategories.sh',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        require => File['/etc/default/gui_vars.sh'],
+    }
+
 }
