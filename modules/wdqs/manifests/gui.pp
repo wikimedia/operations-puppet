096b8549ef745f632689dcb9d0687b37d892e6a0
Stanislav Malyshev <smalyshev@gmail.com>
2017-11-20 16:20:16 -0800

    Enable configuration for aliasing namespaces
    For now, uses $data_dir/aliases.map as the map.
    Also, allows the admins to restart nginx since
    updating the map requires manual nginx restart.
    Unfortunately, I don't see any way to make nginx
    auto-load this file on change.
    
    Bug: T181016
    Change-Id: I1244b8c1da1dcdd13d38421e017f6c6d9a263cab

--- modules/wdqs/manifests/gui.pp
+++ modules/wdqs/manifests/gui.pp
@@ -5,25 +5,38 @@
 # == Parameters:
 # - $package_dir:  Directory where the service is installed.
 # GUI files are expected to be under its gui/ directory.
-# - $log_aggregator: Where to send the logs for the service in syslog format.
+# - $data_dir: Where the data is installed.
+# - $logstash_host: Where to send the logs for the service in syslog format.
 #
 class wdqs::gui(
     $logstash_host = undef,
     $logstash_syslog_port = 10514,
     $package_dir = $::wdqs::package_dir,
+    $data_dir = $::wdqs::data_dir,
+    $username = $::wdqs::username,
     $port = 80,
     $additional_port = 8888,
 ) {
+
+    $alias_map = "{$data_dir}/aliases.map"
+
     ::nginx::site { 'wdqs':
         content => template('wdqs/nginx.erb'),
+        require => File[$alias_map],
+    }
+
+    file { $alias_map:
+        ensure => present,
+        owner  => $username,
+        group  => 'wikidev',
+        mode   => '0664',
     }
 
     # The directory for operator-controlled nginx flags
     file { '/var/lib/nginx/wdqs/':
-        ensure  => directory,
-        owner   => 'root',
-        group   => 'wikidev',
-        mode    => '0775',
-        require => Nginx::Site['wdqs'],
+        ensure => directory,
+        owner  => 'root',
+        group  => 'wikidev',
+        mode   => '0775',
     }
 }
