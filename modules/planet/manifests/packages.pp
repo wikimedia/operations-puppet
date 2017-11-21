309c969add13169c437b69d9576c5ecdac70bcf8
Paladox <thomasmulhall410@yahoo.com>
2017-11-21 15:25:05 +0000

    planet: Add a xhtml archive plugin to rawdog
    This will generate the atom xtml like planet venus does.
    
    Change-Id: I7036c6e0f745da5256dea054847426d9ff45956d

--- modules/planet/manifests/packages.pp
+++ modules/planet/manifests/packages.pp
@@ -17,13 +17,16 @@ class planet::packages {
     # planet-venus does not exist anymore in stretch
     # rawdog is another RSS aggregator using Python and Feedparser
     # to produce a "planet"-like static site
-    if os_version('debian == stretch') {
+    if os_version('debian >= stretch') {
         # 'RSS Aggregator Without Delusions Of Grandeur'
         package { 'rawdog':
             ensure => 'present',
         }
+
         # PyTidyLib 0.2.1 or later (optional but strongly recommended)
-        package { 'python-tidylib':
+        # python-libxml2 is needed for the xml archive plugin we will use
+        # for rawdog.
+        package { ['python-tidylib', 'python-libxml2']:
             ensure => 'present',
         }
     }
