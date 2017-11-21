785796a4894f078de4acddf53d58bb08e497eb0b
Stanislav Malyshev <smalyshev@gmail.com>
2017-11-21 14:26:30 -0800

    Create script for automatic reload of categories
    Bug: T173772
    Change-Id: I96e0863aff73a3cece8c93c8f03ee4901759b7b1

---
+++ modules/wdqs/files/cron/reloadCategories.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# This script is reloading categories into a new namespace
+# NOTE: This should be run under user that has rights to
+# sudo systemctl reload nginx
+if [ -r /etc/wdqs/vars.sh ]; then
+  . /etc/wdqs/vars.sh
+fi
+
+if [ -r /etc/wdqs/gui_vars.sh ]; then
+  . /etc/wdqs/gui_vars.sh
+fi
+
+if [ -z "${DATA_DIR}" -o -z "LOG_DIR" -o -z "${DEPLOY_DIR}" ]; then
+	echo "Variables to set up right!"
+	exit 1
+fi
+
+today=$(date -u +'%Y%m%d')
+newNamespace="categories${today}"
+# Drop old dumps
+rm -f "${DATA_DIR}/*-categories.ttl.gz"
+cd $DEPLOY_DIR
+bash createNamespace.sh $newNamespace || exit 1
+# Load the data
+bash forAllCategoryWikis.sh loadCategoryDump.sh $newNamespace >> "${LOG_DIR}/${newNamespace}.log"
+# Get old namespace
+oldNamespace=$(cat $ALIAS_FILE | grep categories | cut -d' ' -f2)
+# Switch the map
+# NOTE: right now it overrides the map. If we reuse it for other purposes, this needs to be made smarter.
+echo "categories ${newNamespace}" > $ALIAS_FILE
+# Bump nginx to reload config
+sudo systemctl reload nginx
+if [ -n "${oldNamespace}" ]; then
+	# Drop old namespace
+	curl -s -X DELETE "http://localhost:9999/bigdata/namespace/${oldNamespace}"
+fi
