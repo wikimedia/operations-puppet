5263a4b2278fe9ce408005eebc62b4f2a5b95cd7
Paladox <thomasmulhall410@yahoo.com>
2017-11-22 22:46:45 +0000

    planet: Add support for http/2 on stretch
    This will only be enable on stretch+. So jessie is safe.
    
    Change-Id: I248af63cfda01813fb7f6d8c94fb4aacb3271ee8

--- modules/role/manifests/planet_server.pp
+++ modules/role/manifests/planet_server.pp
@@ -3,6 +3,15 @@ class role::planet_server {
 
     include ::standard
     include ::profile::base::firewall
+
+    class { '::apache': }
+    class { '::apache::mod::rewrite': }
+    # so we can vary on X-Forwarded-Proto when behind misc-web
+    class { '::apache::mod::headers': }
+    if os_version('debian >= stretch') {
+      class { '::apache::mod::http2': }
+    }
+
     include ::profile::planet::venus
 
     system::role { 'planet_server':
