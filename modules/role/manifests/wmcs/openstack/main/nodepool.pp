5b8bed3e3d03569ca8c2bd3d1d5f2461cb7ca87a
Daniel Zahn <dzahn@wikimedia.org>
2017-11-21 19:45:26 -0800

    labnodepool: move standard/firewall includes to role
    Change-Id: I8ac8c0e768be1512c8a6102a6a5cd14326fece51

--- modules/role/manifests/wmcs/openstack/main/nodepool.pp
+++ modules/role/manifests/wmcs/openstack/main/nodepool.pp
@@ -1,4 +1,6 @@
 class role::wmcs::openstack::main::nodepool {
     system::role { $name: }
+    include ::standard
+    include ::profile::base::firewall
     include ::profile::openstack::main::nodepool::service
 }
