2c5a3e1174325b7407e19aabffdb0e6e0c63ac1f
Antoine Musso <hashar@free.fr>
2017-11-22 22:02:35 +0100

    contint: libcurl4-gnutls-dev is now absent
    Has been ensure => absent for ages. No need to keep it around.
    
    Change-Id: Idbb5029775c29c2bd13bbe57f8a0d5c5ec6aaed6

--- modules/contint/manifests/packages/hhvm.pp
+++ modules/contint/manifests/packages/hhvm.pp
@@ -1,11 +1,5 @@
 class contint::packages::hhvm {
 
-    package { 'libcurl4-gnutls-dev':
-        # Conflict with HHVM build dependency libcurl4-openssl-dev.
-        # Was For pycurl which now build with openssl just fine.
-        ensure =>  absent,
-    }
-
     exec { '/usr/bin/apt-get -y build-dep hhvm':
         onlyif => '/usr/bin/apt-get -s build-dep hhvm | /bin/grep -Pq "will be (installed|upgraded)"',
     }
