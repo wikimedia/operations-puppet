bd75472d3d49e7e61d8e1c9bc337b92bdf24269a
Paladox <thomasmulhall410@yahoo.com>
2017-11-22 22:46:45 +0000

    planet: Enable http/2 mod for apache
    This will only be enable on stretch+. So jessie is safe.
    
    Change-Id: I248af63cfda01813fb7f6d8c94fb4aacb3271ee8

--- modules/planet/manifests/init.pp
+++ modules/planet/manifests/init.pp
@@ -29,6 +29,11 @@ class planet (
 
     # things done once for all planets
     include ::planet::webserver
+    class { '::apache': }
+    class { '::apache::mod::rewrite': }
+    # so we can vary on X-Forwarded-Proto when behind misc-web
+    class { '::apache::mod::headers': }
+    class { '::apache::mod::http2': }
     include ::planet::packages
     include ::planet::dirs
     include ::planet::user
