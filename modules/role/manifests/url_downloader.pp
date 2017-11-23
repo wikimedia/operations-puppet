55e5429c3a9e9404b92193bb61d0243990932838
Daniel Zahn <dzahn@wikimedia.org>
2017-11-22 16:36:06 -0800

    url_downloader: move firewall to role, use profile
    Change-Id: Ib64fa1d1cd091d2a4ff0b98a88e50572e469d837

--- modules/role/manifests/url_downloader.pp
+++ modules/role/manifests/url_downloader.pp
@@ -4,5 +4,6 @@ class role::url_downloader {
     system::role { 'url_downloader': description => 'Upload-by-URL proxy' }
 
     include ::standard
+    include ::profile::base::firewall
     include ::profile::url_downloader
 }
