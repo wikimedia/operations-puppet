db3aa4fc801ec7d988d4a5d2b89b52f36388f302
elukey <ltoscano@wikimedia.org>
2017-11-24 17:40:33 +0100

    role::analytics_cluster::database::meta::backup*: move to profiles
    Bug: T167790
    Change-Id: Ib0c195bde8471589d3c14afa5f1244ea202dbba9

--- modules/role/manifests/analytics_cluster/hadoop/standby.pp
+++ modules/role/manifests/analytics_cluster/hadoop/standby.pp
@@ -12,6 +12,16 @@ class role::analytics_cluster::hadoop::standby {
     include ::profile::hadoop::firewall::master
     include ::profile::hadoop::users
     include ::profile::hadoop::backup::namenode
+
+    # analytics1002 is usually inactive, and it has a
+    # decent amount of disk space.  We use it to
+    # store some backups, including fsimage snapshots
+    # of Hadoop NameNode metadata, and of the
+    # analytics_cluster::database::meta (MySQL analytics-meta) instance.
+    # If you move these, make sure /srv/backup has
+    # enough space to store backups.
+    include ::profile::analytics::database::meta::backup_dest
+
     include ::profile::base::firewall
     class { 'standard': }
 
