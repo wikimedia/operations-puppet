f0e1637ad57ad95f92e8dd2903c6c3080b23c5ec
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-26 19:28:57 +0200

    keep misc dumps dirs owner on generating host same as on web server for now
    As these jobs move from web server to generating host, we can
    change the owner, but not until then; otherwise jobs are unable
    to create the subdir needed for their output
    
    Change-Id: I16d84c2b5ea7e11d0019f084abab5c884f9b3f09

--- modules/profile/manifests/dumps/generation/server/fallback.pp
+++ modules/profile/manifests/dumps/generation/server/fallback.pp
@@ -4,5 +4,7 @@ class profile::dumps::generation::server::fallback {
     class { '::dumps::generation::server::dirs':
         user  => $dumpsuser::user,
         group => $dumpsuser::group,
+        deprecated_user  => 'datasets',
+        deprecated_group => 'datasets',
     }
 }
