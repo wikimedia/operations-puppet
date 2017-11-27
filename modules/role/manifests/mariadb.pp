0416776a724899515e55c5f34db4afc72d281053
elukey <ltoscano@wikimedia.org>
2017-11-27 15:41:58 +0100

    Delete role::mariadb::analytics
    Bug: T156844
    Change-Id: I9dc86f0a1f4f00b74a7d60571b8fa6f2faa61769

--- modules/role/manifests/mariadb.pp
+++ modules/role/manifests/mariadb.pp
@@ -156,16 +156,4 @@ class role::mariadb::groups(
     }
 }
 
-# MariaDB 10 Analytics all-shards slave, with scratch space and TokuDB
-# analytics slaves are already either dbstores or eventlogging slaves
-# so they just need the extra core monitoring
-class role::mariadb::analytics {
-    mariadb::monitor_replication { ['s1','s2']:
-        is_critical   => false,
-        contact_group => 'admins', # only show on nagios/irc
-        multisource   => true,
-        socket        => '/tmp/mysql.sock',
-    }
-}
-
 # lint:endignore
