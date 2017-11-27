389f92e9026363182f8d51bcf9d3a6fdfce8ab20
Ariel T. Glenn <ariel@wikimedia.org>
2017-11-27 10:23:38 +0200

    move last hiera calls out of snapshot modules into profile
    Change-Id: I19664ecd030a73d4ad1bd444ea29a8f0574fafc6

--- modules/profile/manifests/dumps/generation/worker/dumper.pp
+++ modules/profile/manifests/dumps/generation/worker/dumper.pp
@@ -1,9 +1,12 @@
 # this class is for snapshot hosts that run regular dumps
 # meaning sql/xml dumps every couple of weeks or so
 # and no other tasks
-class profile::dumps::generation::worker::dumper {
+class profile::dumps::generation::worker::dumper(
+    $runtype = hiera('snapshot::dumps::runtype'),
+) {
     class { 'snapshot::dumps::cron':
         user    => 'dumpsgen',
         maxjobs => '28',
+        runtype => $runtype,
     }
 }
