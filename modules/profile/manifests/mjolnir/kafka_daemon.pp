82ab2c849857203e76da844bcab18f56acf8c27e
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-11-28 22:30:32 -0800

    Revert "Revert "Deploy MjoLniR with new deploy repository""
    The problems with mjolnir vs MjoLniR have been resolved, it
    required adjustments to scap.cfg in the repo.
    
    This is a standard revert with one addition, we now create an
    empty directory /etc/mjolnir owned by deploy-service. This gives
    the deploy repo a sane place to install a configuration script
    to that can be auto-magicaly found by mjolnir.
    
    This reverts commit 6a7753a14ac3cb66593eabbad30e8ac72e184751.
    
    Change-Id: I599341bd16ecba0a2b8d8132fde6fe3d1443d754

--- modules/profile/manifests/mjolnir/kafka_daemon.pp
+++ modules/profile/manifests/mjolnir/kafka_daemon.pp
@@ -9,18 +9,11 @@ class profile::mjolnir::kafka_daemon(
     # it is named just 'eqiad'.
     $kafka_config = kafka_config('eqiad'),
 ) {
-    scap::target { 'relforge/mjolnir':
-      deploy_user => 'deploy-service',
-    }
-
-    # This is a limited subset of what the full mjolnir package requires because
-    # the daemon is a small part of the overall application. The daemon only needs
-    # to read/write kafka topics and send requests to localhost.
-    require_package('python-kafka', 'python-requests')
+    class { 'mjolnir': }
 
     systemd::service { 'mjolnir-kafka-daemon':
         content => template('profile/mjolnir/kafka-daemon.service.erb'),
-        require => Scap::Target['relforge/mjolnir'],
+        require => Class['mjolnir'],
     }
 
 }
