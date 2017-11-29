9434632c428def2a188c5cb8933a9ee0fd59f8ea
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-11-28 22:30:32 -0800

    Revert "Revert "Deploy MjoLniR with new deploy repository""
    The problems with mjolnir vs MjoLniR have been resolved, it
    required adjustments to scap.cfg in the repo.
    
    This is a standard revert with one addition, we now create an
    empty directory /etc/mjolnir owned by deploy-service. This gives
    the deploy repo a sane place to install a configuration script
    to that can be auto-magicaly found by mjolnir.
    
    This reverts commit 6a7753a14ac3cb66593eabbad30e8ac72e184751.
    
    Change-Id: I599341bd16ecba0a2b8d8132fde6fe3d1443d754

--- modules/role/manifests/elasticsearch/analytics.pp
+++ modules/role/manifests/elasticsearch/analytics.pp
@@ -1,5 +1,7 @@
 # Supports CirrusSearch usage on the analytics cluster
 class role::elasticsearch::analytics {
+    class { 'mjolnir': }
+
     # wikimedia/discovery/analytics will be deployed to this node
     scap::target { 'wikimedia/discovery/analytics':
         deploy_user => 'deploy-service',
