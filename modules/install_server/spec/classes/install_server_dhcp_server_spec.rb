9bcc9f48c687fa740ab9aa5175908e3f133aa396
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-11-30 10:55:20 +0100

    spec: make apt,authdns, install_server work with puppet 4
    Change-Id: I6d3a34e90e5294dea3c03e744b283c35d26c63b4

--- modules/install_server/spec/classes/install_server_dhcp_server_spec.rb
+++ modules/install_server/spec/classes/install_server_dhcp_server_spec.rb
@@ -1,18 +1,33 @@
 require 'spec_helper'
+test_on = {
+  supported_os: [
+    {
+      'operatingsystem'        => 'Debian',
+      'operatingsystemrelease' => ['8', '9'],
+    }
+  ]
+}
 
 describe 'install_server::dhcp_server', :type => :class do
-    it { should compile }
+  on_supported_os(test_on).each do |os, facts|
+    context "On #{os}" do
+      let(:facts){ facts }
+      it { is_expected.to compile }
 
-    it 'should have isc-dhcp-server' do
-        should contain_package('isc-dhcp-server').with_ensure('present')
-        should contain_service('isc-dhcp-server').with_ensure('running')
+      it 'should have isc-dhcp-server' do
+        is_expected.to contain_package('isc-dhcp-server').with_ensure('present')
+        is_expected.to contain_service('isc-dhcp-server').with_ensure('running')
 
-        should contain_file('/etc/dhcp/').with({
-            'ensure' => 'directory',
-            'mode'   => '0444',
-            'owner'  => 'root',
-            'group'  => 'root',
-            'recurse' => 'true',
-        })
+        is_expected.to contain_file('/etc/dhcp').with(
+                 {
+                   'ensure' => 'directory',
+                   'mode'   => '0444',
+                   'owner'  => 'root',
+                   'group'  => 'root',
+                   'recurse' => 'true',
+                 }
+               )
+      end
     end
+  end
 end
