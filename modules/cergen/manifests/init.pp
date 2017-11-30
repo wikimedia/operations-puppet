3cc6375f8ba389da8a0060bf299d90bf951f0139
Ottomata <aotto@wikimedia.org>
2017-11-30 10:54:31 -0500

    require python3-pkg-resources for cergen in jessie
    Bug: T166167
    Change-Id: Iac2721a7a81bb957beee5605db924e7b6dd45ad7

--- modules/cergen/manifests/init.pp
+++ modules/cergen/manifests/init.pp
@@ -3,22 +3,29 @@
 class cergen {
     # If Jessie, we need to manually specify versions of some dependencies from backports.
     if os_version('debian jessie') {
-        if !defined(Package['python3-pyasn1']) {
-            package { 'python3-pyasn1':
-                ensure => '0.1.9-1~bpo8+1',
+
+        if !defined(Package['python3-pkg-resources']) {
+            package { 'python3-pkg-resources':
+                ensure => '33.1.1-1~bpo8+1',
             }
         }
         if !defined(Package['python3-setuptools']) {
             package { 'python3-setuptools':
-                ensure => '33.1.1-1~bpo8+1',
+                ensure  => '33.1.1-1~bpo8+1',
+                require => Package['python3-pkg-resources'],
+            }
+        }
+        if !defined(Package['python3-pyasn1']) {
+            package { 'python3-pyasn1':
+                ensure => '0.1.9-1~bpo8+1',
             }
         }
         if !defined(Package['python3-cryptography']) {
             package { 'python3-cryptography':
                 ensure  => '1.7.1-3~bpo8+1',
                 require => [
+                    Package['python3-setuptools'],
                     Package['python3-pyasn1'],
-                    Package['python3-setuptools']
                 ],
             }
         }
