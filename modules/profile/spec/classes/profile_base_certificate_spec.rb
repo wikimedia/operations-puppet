4d2523ea59207735848e318e73f5241c68cf2fb9
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-11-30 10:56:57 +0100

    spec: make jenkins, zuul, monitoring, profile work with puppet 4
    Change-Id: Ie18130f6d833584d403783ae2071da4e14de7991

--- modules/profile/spec/classes/profile_base_certificate_spec.rb
+++ modules/profile/spec/classes/profile_base_certificate_spec.rb
@@ -3,9 +3,23 @@ require 'spec_helper'
 # Adding a test on the exposition of Puppet CA cert here to make it explicit
 # that clients of base::expose_puppet_certs most probably need this cert to be
 # exposed as well
+test_on = {
+  supported_os: [
+    {
+      'operatingsystem'        => 'Debian',
+      'operatingsystemrelease' => ['8', '9'],
+    }
+  ]
+}
+
 describe 'profile::base::certificates' do
-    it 'should exposes Puppet CA certificate' do
+  on_supported_os(test_on).each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+      it 'should expose Puppet CA certificate' do
         should contain_file('/usr/local/share/ca-certificates/Puppet_Internal_CA.crt')
-            .with({ 'ensure' => 'present' })
+                 .with({ 'ensure' => 'present' })
+      end
     end
+  end
 end
