42e8ba48df37040697f7a77c816b449a3e7d41b5
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-11-30 11:32:22 +0100

    spec: make network, puppetmaster, service, systemd compatible with puppet4
    Change-Id: If05f38643ca2977d73bbe34f4af156f6178efd2b

--- modules/puppetmaster/spec/classes/puppetmaster_rsync_spec.rb
+++ modules/puppetmaster/spec/classes/puppetmaster_rsync_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
+test_on = {
+  supported_os: [
+    {
+      'operatingsystem'        => 'Debian',
+      'operatingsystemrelease' => ['8', '9'],
+    }
+  ]
+}
 
 describe 'puppetmaster::rsync' do
-    let(:params) { {
-        :server => 'puppetmaster_host',
-    } }
-    let(:facts) { {
-        :realm => 'production',
-    } }
-    it { should compile }
+  on_supported_os(test_on).each do |os, facts|
+    context "On #{os}" do
+      let(:facts) { facts }
+      let(:params) { {
+                       :server => 'puppetmaster_host',
+                     } }
+      let(:node_params) { {
+                            'realm' => 'production',
+                    } }
+      it { should compile }
+    end
+  end
 end
