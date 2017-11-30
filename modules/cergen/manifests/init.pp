5499859ee17e4627e6c894db480ff0915d04aa96
Ottomata <aotto@wikimedia.org>
2017-11-30 11:09:13 -0500

    Use install_options instead of manually specifying cergen deps
    Bug: T166167
    Change-Id: I0e8d3a6212bc7cbdd8e32d0aef51d2917003846f

--- modules/cergen/manifests/init.pp
+++ modules/cergen/manifests/init.pp
@@ -1,46 +1,11 @@
 # == Class cergen
 # Installs cergen package.
+#
 class cergen {
-    # If Jessie, we need to manually specify versions of some dependencies from backports.
-    if os_version('debian jessie') {
-
-        if !defined(Package['python3-pkg-resources']) {
-            package { 'python3-pkg-resources':
-                ensure => '33.1.1-1~bpo8+1',
-            }
-        }
-        if !defined(Package['python3-setuptools']) {
-            package { 'python3-setuptools':
-                ensure  => '33.1.1-1~bpo8+1',
-                require => Package['python3-pkg-resources'],
-            }
-        }
-        if !defined(Package['python3-pyasn1']) {
-            package { 'python3-pyasn1':
-                ensure => '0.1.9-1~bpo8+1',
-            }
-        }
-        if !defined(Package['python3-cryptography']) {
-            package { 'python3-cryptography':
-                ensure  => '1.7.1-3~bpo8+1',
-                require => [
-                    Package['python3-setuptools'],
-                    Package['python3-pyasn1'],
-                ],
-            }
-        }
-        if !defined(Package['python3-openssl']) {
-            package { 'python3-openssl':
-                ensure  => '16.0.0-1~bpo8+1',
-                require => Package['python3-cryptography'],
-                before  => Package['cergen'],
-            }
-        }
-    }
-
-    # Not using require_package since it fails the whole
-    # class if the above dependencies are not first installed.
     package { 'cergen':
         ensure => 'present',
+        # If Jessie, we need to manually specify versions
+        # of some dependencies from backports.
+        install_options => ['-t', "${::lsbdistcodename}-backports"],
     }
 }
