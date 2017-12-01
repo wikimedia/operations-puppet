35df26463149688d9e7d107e700aa6fcc8c629fd
Daniel Zahn <dzahn@wikimedia.org>
2017-12-01 10:25:13 -0800

    wmcs: move standard includes from site to roles
    This should be no-op on everything but cleans up site.pp
    and gets us "wmf-style: total violations delta -7".
    
    Let me know if it's too much change at once but i can compile it too
    of course.
    
    Change-Id: I491de33e594c67b34b9e410f42ef8da1d519c79d

--- manifests/site.pp
+++ manifests/site.pp
@@ -1081,7 +1081,6 @@ node 'labtestservices2001.wikimedia.org' {
 node /labtestservices200[23]\.wikimedia\.org/ {
     role(wmcs::openstack::labtestn::services)
     include ::base::firewall
-    include ::standard
     interface::add_ip6_mapped { 'main': }
 }
 
@@ -1229,7 +1228,6 @@ node 'labcontrol1001.wikimedia.org' {
     role(wmcs::openstack::main::control)
 
     include ::base::firewall
-    include ::standard
     include ::ldap::role::client::labs
 }
 
@@ -1243,7 +1241,6 @@ node 'labcontrol1002.wikimedia.org' {
     role(wmcs::openstack::main::control)
 
     include ::base::firewall
-    include ::standard
     include ::ldap::role::client::labs
 }
 
@@ -1254,7 +1251,6 @@ node 'labtestweb2001.wikimedia.org' {
     role(wmcs::openstack::labtest::web,
           mariadb::wikitech)
     include ::base::firewall
-    include ::standard
     include ::ldap::role::client::labs
 
     interface::add_ip6_mapped { 'main': }
@@ -1276,7 +1272,6 @@ node 'labmon1002.eqiad.wmnet' {
 
 node 'labnet1001.eqiad.wmnet' {
     role(wmcs::openstack::main::net)
-    include ::standard
 }
 
 node /labnet100[34]\.eqiad\.wmnet/ {
@@ -1287,7 +1282,6 @@ node /labnet100[34]\.eqiad\.wmnet/ {
 
 node 'labnet1002.eqiad.wmnet' {
     role(wmcs::openstack::main::net_standby)
-    include ::standard
 }
 
 node 'labnodepool1001.eqiad.wmnet' {
