806df01c49e96c69d33ecb9d4cffbc0c20a92ff9
andrewbogott <abogott@wikimedia.org>
2017-12-01 01:54:48 -0600

    wmcs recursor:  allow labtest to override puppetmaster_hostname
    Change-Id: Ib56f58b24b72e4938140aa7b46e2e549fbfff153

--- modules/profile/manifests/openstack/labtest/pdns/recursor/service.pp
+++ modules/profile/manifests/openstack/labtest/pdns/recursor/service.pp
@@ -9,11 +9,12 @@ class profile::openstack::labtest::pdns::recursor::service(
     ) {
 
     class {'::profile::openstack::base::pdns::recursor::service':
-        nova_controller   => $nova_controller,
-        observer_password => $observer_password,
-        pdns_host         => $pdns_host,
-        pdns_recursor     => $pdns_recursor,
-        tld               => $tld,
-        private_reverse   => $private_reverse,
+        nova_controller       => $nova_controller,
+        observer_password     => $observer_password,
+        pdns_host             => $pdns_host,
+        pdns_recursor         => $pdns_recursor,
+        tld                   => $tld,
+        private_reverse       => $private_reverse,
+        puppetmaster_hostname => $puppetmaster_hostname
     }
 }
