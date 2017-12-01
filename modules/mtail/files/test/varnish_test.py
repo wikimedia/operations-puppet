eb75280fce40dfecf6a334e6036f3f5885b972b1
Emanuele Rocca <ema@wikimedia.org>
2017-12-01 17:08:49 +0100

    mtail: add varnishmtail tests
    Bug: T177199
    Change-Id: I7c33a3dc1754e62408b0c450a80018d71d516b34

---
+++ modules/mtail/files/test/varnish_test.py
@@ -0,0 +1,40 @@
+import mtail_store
+import unittest
+import os
+
+test_dir = os.path.join(os.path.dirname(__file__))
+
+
+class VarnishXcacheTest(unittest.TestCase):
+    def setUp(self):
+        self.store = mtail_store.MtailMetricStore(
+                os.path.join(test_dir, '../programs/varnishxcache.mtail'),
+                os.path.join(test_dir, 'logs/varnish.test'))
+
+    def testCacheStatus(self):
+        m = self.store.get_metric('varnish_x_cache')
+        self.assertEqual(2, m._value)
+        self.assertIn('x_cache=int-front', m._labelpairs)
+
+
+class VarnishRlsTest(unittest.TestCase):
+    def setUp(self):
+        self.store = mtail_store.MtailMetricStore(
+                os.path.join(test_dir, '../programs/varnishrls.mtail'),
+                os.path.join(test_dir, 'logs/varnish.test'))
+
+    def testIfNoneMatch(self):
+        m = self.store.get_metric('varnish_resourceloader_inm')
+        self.assertEquals(m._value, 1)
+
+
+class VarnishMediaTest(unittest.TestCase):
+    def setUp(self):
+        self.store = mtail_store.MtailMetricStore(
+                os.path.join(test_dir, '../programs/varnishmedia.mtail'),
+                os.path.join(test_dir, 'logs/varnish.test'))
+
+    def testThumbnails(self):
+        m = self.store.get_metric('varnish_thumbnails')
+        self.assertEquals(2, m._value)
+        self.assertIn('status=200', m._labelpairs)
