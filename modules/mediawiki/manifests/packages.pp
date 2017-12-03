047724632b055278dbe8e7d339835891e8d9e255
Ariel T. Glenn <ariel@wikimedia.org>
2017-12-03 18:18:23 +0200

    [WIP] php7 manifests for mediawiki on stretch
    First round of testing is for dumps (snapshot) hosts only.
    Use at your own risk.
    
    Change-Id: Ib7e60fcd98efca8bca749139836125216996a4b9

--- modules/mediawiki/manifests/packages.pp
+++ modules/mediawiki/manifests/packages.pp
@@ -1,14 +1,25 @@
 # === Class mediawiki::packages
 #
 # Packages needed for mediawiki
-class mediawiki::packages {
+class mediawiki::packages(
+  $php7 = false,
+) {
     if os_version('ubuntu == trusty') {
         include ::mediawiki::packages::php5
+    }
+    if os_version('debian == stretch') {
+        if ($php7 == true) {
+            apt::pin { 'php-luasandbox':
+                package  => 'php-luasandbox',
+                pin      => 'release a=stretch-backports',
+                priority => '1010',
+            }
+            include ::mediawiki::packages::php7
+        }
     }
 
     include ::mediawiki::packages::math
     include ::mediawiki::packages::tex
-    require ::apt
 
     include ::imagemagick::install
 
