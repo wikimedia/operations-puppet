64fc9cc35afa7801a2cfadcc195d3e63a1dfe55d
Ariel T. Glenn <ariel@wikimedia.org>
2017-12-03 18:18:23 +0200

    [WIP] php7 manifests for mediawiki on stretch
    First round of testing is for dumps (snapshot) hosts only.
    Use at your own risk.
    
    Change-Id: Ib7e60fcd98efca8bca749139836125216996a4b9

---
+++ modules/mediawiki/manifests/php7.pp
@@ -0,0 +1,34 @@
+# == Class: mediawiki::php7
+#
+# Packages and .ini files for PHP7 extensions.
+#
+class mediawiki::php7 {
+    $php_module_conf_dir = '/etc/php/7.0/mods-available'
+    mediawiki::php_enmod { ['mail']: }
+
+    # need to check if another package does this for us
+    file { '/etc/php7/apache2':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
+
+    file { '/etc/php7/apache2/php.ini':
+        source => 'puppet:///modules/mediawiki/php7/php.ini',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
+
+    file { '/etc/php7/cli/php.ini':
+        source => 'puppet:///modules/mediawiki/php/php.ini.cli',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
+
+    file { "${php_module_conf_dir}/mail.ini":
+        ensure => absent,
+    }
+}
