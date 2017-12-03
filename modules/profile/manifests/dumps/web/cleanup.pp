ba472556b698655f8b1dcb601a3423064f673318
Ariel T. Glenn <ariel@wikimedia.org>
2017-12-03 10:59:43 +0200

    cleanup xml/sql dumps on all hosts that have them
    On generator hosts, keep a small number for use for prefetch
    for the next run.
    On hosts serving the dumps, keep as many as
    we can make available to the public, given disk space.
    
    Bug: T181895
    Change-Id: I04fc060fa607e0bedc7971e2fd4b830aec1b66d2

--- modules/profile/manifests/dumps/web/cleanup.pp
+++ modules/profile/manifests/dumps/web/cleanup.pp
@@ -1,9 +1,12 @@
 class profile::dumps::web::cleanup(
     $isreplica = hiera('profile::dumps::cleanup::isreplica'),
     $miscdumpsdir = hiera('profile::dumps::miscdumpsdir'),
+    $publicdir = hiera('profile::dumps::xmldumpspublicdir'),
 ) {
     class {'::dumps::web::cleanup':
         isreplica    => $isreplica,
         miscdumpsdir => $miscdumpsdir,
+        publicdir    => $publicdir,
+        user         => 'dumpsgen',
     }
 }
