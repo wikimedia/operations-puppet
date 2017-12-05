4ca9350cdd41e0acceebf2ad3e68adcf5c700374
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2017-12-05 17:11:58 +0100

    profile: add redis_exporter to ores::redis
    The redis' "instances" list (i.e. the fact that the are two instances) is already split among the
    profile and ores::redis. For consistency include profile::redis_exporter within profile::ores::redis
    rather than inside ores::redis.
    
    Bug: T148637
    Change-Id: I4b32a61161ddada6b4733766ad78cb1c8a1e9b62

--- modules/profile/manifests/ores/redis.pp
+++ modules/profile/manifests/ores/redis.pp
@@ -4,19 +4,28 @@ class profile::ores::redis(
     $password = hiera('profile::ores::redis::password'),
     $redis_clients = hiera('profile::ores::redis::client_hosts'),
     $slaveof  = hiera('profile::ores::redis::slaveof', undef),
+    $prometheus_nodes = hiera('prometheus_nodes'),
 ){
     include ::standard
     include ::base::firewall
+
+    $instances = ['6379', '6380']
 
     class { '::ores::redis':
         password => $password,
         slaveof  => $slaveof,
     }
 
+    ::profile::prometheus::redis_exporter{ $instances:
+        password         => $password,
+        prometheus_nodes => $prometheus_nodes,
+    }
+
     $redis_hosts_ferm = join($redis_clients, ' ')
+    $redis_ports_ferm = join($instances, ' ')
     ferm::service { 'ores_redis':
         proto  => 'tcp',
-        port   => '(6379 6380)',
+        port   => "(${redis_ports_ferm})",
         srange => "@resolve((${redis_hosts_ferm}))",
     }
 }
