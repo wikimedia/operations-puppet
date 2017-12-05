da9b57e17b75a9866276986673a652792550f752
Emanuele Rocca <ema@wikimedia.org>
2017-12-05 18:41:18 +0100

    mtail: port varnishxcps
    Bug: T177199
    Change-Id: I79b5cefa202db3e6137904a2b9b411f50e3781e1

--- modules/mtail/files/test/varnish_test.py
+++ modules/mtail/files/test/varnish_test.py
@@ -38,3 +38,43 @@ class VarnishMediaTest(unittest.TestCase):
         m = self.store.get_metric('varnish_thumbnails')
         self.assertEquals(2, m._value)
         self.assertIn('status=200', m._labelpairs)
+
+
+class VarnishXcpsTest(unittest.TestCase):
+    def setUp(self):
+        self.store = mtail_store.MtailMetricStore(
+                os.path.join(test_dir, '../programs/varnishxcps.mtail'),
+                os.path.join(test_dir, 'logs/varnish.test'))
+
+    def testH2(self):
+        m = self.store.get_metric('tls_h2')
+        self.assertEqual(1, m._value)
+
+    def testReusedSessions(self):
+        m = self.store.get_metric('tls_sess_reused')
+        self.assertEqual(1, m._value)
+
+    def testTLSversion(self):
+        m = self.store.get_metric('tls_version')
+        self.assertEqual(1, m._value)
+        self.assertIn('version=TLSv1.2', m._labelpairs)
+
+    def testTLSKeyExchange(self):
+        m = self.store.get_metric('tls_key_exchange')
+        self.assertEqual(1, m._value)
+        self.assertIn('type=X25519', m._labelpairs)
+
+    def testTLSAuth(self):
+        m = self.store.get_metric('tls_auth')
+        self.assertEqual(1, m._value)
+        self.assertIn('type=ECDSA', m._labelpairs)
+
+    def testTLSCipher(self):
+        m = self.store.get_metric('tls_cipher')
+        self.assertEqual(1, m._value)
+        self.assertIn('name=CHACHA20-POLY1305', m._labelpairs)
+
+    def testTLSFullCipher(self):
+        m = self.store.get_metric('tls_full_cipher')
+        self.assertEqual(1, m._value)
+        self.assertIn('name=ECDHE-ECDSA-CHACHA20-POLY1305', m._labelpairs)
