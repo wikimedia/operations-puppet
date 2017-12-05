756e496ce4362e5c5c1ca44588f5843d4bc75613
Paladox <thomasmulhall410@yahoo.com>
2017-12-05 23:41:35 +0000

    planet: Add stretch check in rawdogplugin.pp
    follows up 105b1b1ab7239caa3bf70345374d08b304629514
    
    Change-Id: Iafa57318dc0a78cf65ab6f3629e2f851c3daaa8f

--- modules/planet/manifests/rawdogplugin.pp
+++ modules/planet/manifests/rawdogplugin.pp
@@ -1,19 +1,21 @@
 # defined type: dirs for (RSS) plugins for a planet-rawdog language
 define planet::rawdogplugin {
 
-    file { "/etc/rawdog/${title}/plugins":
-        ensure => directory,
-        owner  => 'planet',
-        group  => 'planet',
-        mode   => '0755',
-    }
+    if os_version('debian >= stretch') {
+      file { "/etc/rawdog/${title}/plugins":
+          ensure => directory,
+          owner  => 'planet',
+          group  => 'planet',
+          mode   => '0755',
+      }
 
-    file { "/etc/rawdog/${title}/plugins/rss.py":
-        ensure  => 'present',
-        owner   => 'planet',
-        group   => 'planet',
-        mode    => '0755',
-        content => template('planet/feeds_rawdog/plugins/rss.py.erb'),
+      file { "/etc/rawdog/${title}/plugins/rss.py":
+          ensure  => 'present',
+          owner   => 'planet',
+          group   => 'planet',
+          mode    => '0755',
+          content => template('planet/feeds_rawdog/plugins/rss.py.erb'),
+      }
     }
 
 }
