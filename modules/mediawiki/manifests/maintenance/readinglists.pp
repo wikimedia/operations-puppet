043cd564192c4ba73706ac911ac832713fe05792
Gergő Tisza <tgr.huwiki@gmail.com>
2017-12-05 22:40:45 -0800

    Add cron job for purging ReadingLists data
    Periodically hard-deletes expired soft-deleted data in the
    ReadingLists extension.
    
    Bug: T181107
    Depends-On: I083e3f25989fb3706f1eee4b7b1e609c6204ce32
    Change-Id: Idb3f9f730f97fcbf2ba3d312d68cd115ee37d3df

---
+++ modules/mediawiki/manifests/maintenance/readinglists.pp
@@ -0,0 +1,15 @@
+class mediawiki::maintenance::readinglists( $ensure = present ) {
+
+    require ::mediawiki
+
+    system::role { 'mediawiki::maintenance::readinglists': description => 'Mediawiki Maintenance Server: purge old deleted data from ReadingLists extension' }
+
+    cron { 'readinglists_purge':
+        ensure  => $ensure,
+        user    => $::mediawiki::users::web,
+        minute  => 42,
+        hour    => 2,
+        command => '/usr/local/bin/mwscript extensions/ReadingLists/maintenance/purge.php --wiki=metawiki > /var/log/mediawiki/readinglists_purge.log',
+    }
+}
+
