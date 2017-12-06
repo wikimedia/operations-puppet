bcf4a716b848348a567d47a11a05a06285772ffc
elukey <ltoscano@wikimedia.org>
2017-12-06 15:31:01 +0100

    profile::oozie::server: add Prometheus monitoring and tune jvm
    * Adding Prometheus monitoring support
    * Tuning JVM with Xms settings
    
    This change needs: https://gerrit.wikimedia.org/r/395740
    
    Bug: T177458
    Change-Id: Id5d12b49cbd8e66f0053bfcb353345822c5b5a6d

---
+++ modules/profile/manifests/oozie/monitoring/server.pp
@@ -0,0 +1,19 @@
+# Class: profile::oozie::monitoring::server
+#
+# Sets up Prometheus based monitoring for the Oozie Server.
+# This profile takes care of installing the Prometheus exporter and setting
+# up its configuration file, but it does not instruct the target JVM to use it.
+#
+class profile::oozie::monitoring::server(
+    $prometheus_nodes        = hiera('prometheus_nodes'),
+) {
+    $jmx_exporter_config_file = '/etc/hive/prometheus_oozie_server_jmx_exporter.yaml'
+    $prometheus_jmx_exporter_oozie_server_port = 12000
+    profile::prometheus::jmx_exporter { "oozie_server_${::hostname}":
+        hostname         => $::hostname,
+        port             => $prometheus_jmx_exporter_oozie_server_port,
+        prometheus_nodes => $prometheus_nodes,
+        config_file      => $jmx_exporter_config_file,
+        source           => 'puppet:///modules/profile/hive/prometheus_oozie_server_jmx_exporter.yaml',
+    }
+}
