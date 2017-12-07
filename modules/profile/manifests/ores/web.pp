544322c73ad155c77f219d192b76b8768b29ba93
halfak <aaron.halfaker@gmail.com>
2017-12-07 11:07:21 -0600

    Refactor web workers for ORES
    This change makes $web_workers an absolute value
    and sets a default that matches scb1*
    
    TODO:
    * Get rid of fallback values and specify defaults in a common file.
    
    Bug: T182249
    Change-Id: I4336c7522de8e3bc34dfbb122f4eb96b28ed8d37

--- modules/profile/manifests/ores/web.pp
+++ modules/profile/manifests/ores/web.pp
@@ -1,12 +1,14 @@
 class profile::ores::web(
     $redis_host = hiera('profile::ores::web::redis_host'),
     $redis_password = hiera('profile::ores::web::redis_password'),
+    $web_workers = hiera('profile::ores::web::workers', 32),
     $celery_workers = hiera('profile::ores::celery::workers', 45),
     $celery_queue_maxsize = hiera('profile::ores::celery::queue_maxsize', 100),
 ){
     class { '::ores::web':
         redis_password       => $redis_password,
         redis_host           => $redis_host,
+        web_workers          => $web_workers,
         celery_workers       => $celery_workers,
         celery_queue_maxsize => $celery_queue_maxsize
     }
