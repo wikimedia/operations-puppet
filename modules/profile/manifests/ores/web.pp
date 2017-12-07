91da20264f4b9f272474c9aea2a7ff2a6b3f8ed6
halfak <aaron.halfaker@gmail.com>
2017-12-07 11:07:21 -0600

    Bumps stresstest web workers_per_core from 2 to 6.
    Bug: T182249
    Change-Id: I4336c7522de8e3bc34dfbb122f4eb96b28ed8d37

--- modules/profile/manifests/ores/web.pp
+++ modules/profile/manifests/ores/web.pp
@@ -1,12 +1,14 @@
 class profile::ores::web(
     $redis_host = hiera('profile::ores::web::redis_host'),
     $redis_password = hiera('profile::ores::web::redis_password'),
+    $workers_per_core = hiera('profile::ores::web::workers_per_core', 2),
     $celery_workers = hiera('profile::ores::celery::workers', 45),
     $celery_queue_maxsize = hiera('profile::ores::celery::queue_maxsize', 100),
 ){
     class { '::ores::web':
         redis_password       => $redis_password,
         redis_host           => $redis_host,
+        workers_per_core     => $workers_per_core,
         celery_workers       => $celery_workers,
         celery_queue_maxsize => $celery_queue_maxsize
     }
