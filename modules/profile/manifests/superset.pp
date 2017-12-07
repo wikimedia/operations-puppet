5be3160efd3a87d76d3e3e763ef4e50e501fc363
Ottomata <aotto@wikimedia.org>
2017-12-07 15:19:45 -0500

    Configure LDAP proxy and authentication for Superset
    Bug: T166689
    Change-Id: I55aca23a9d34ef3496d0f7d837a9f606aeb6319e

--- modules/profile/manifests/superset.pp
+++ modules/profile/manifests/superset.pp
@@ -26,16 +26,24 @@
 #   passwords to external databases to be provided via configuration,
 #   rather than the web UI.
 #
+# [*ldap_proxy_enabled*]
+#   If true, an Apache HTTP proxy will be configured to authenticate users with WMF (labs) LDAP.
+#   Only users in the 'wmf' and 'nda' LDAP groups will be allowed to authenticate.
+#   This will configure superset with AUTH_TYPE = AUTH_REMOTE_USER, and the authenticated
+#   HTTP remote user will be used to log into superset.
+#
 # [*statsd*]
 #   statsd host:port
 #
 class profile::superset(
+    $workers           = hiera('profile::superset::workers', 1),
     $database_uri      = hiera('profile::superset::database_uri', 'sqlite:////var/lib/superset/superset.db'),
     $database_password = hiera('profile::superset::database_password', undef),
     $admin_user        = hiera('profile::superset::admin_user', 'admin'),
     $admin_password    = hiera('profile::superset::admin_password', 'admin'),
     $secret_key        = hiera('profile::superset::secret_key', 'not_really_a_secret_key'),
     $password_mapping  = hiera('profile::superset::password_mapping', undef),
+    $ldap_proxy_enabled = hiera('profile::superset::ldap_proxy_enabled', false),
     $statsd            = hiera('statsd', undef),
 ) {
     # If given $database_password, insert it into $database_uri.
@@ -44,11 +52,42 @@ class profile::superset(
         default => regsubst($database_uri, '(\w+)://(\w*)@(.*)', "\\1://\\2:${database_password}@\\3")
     }
 
+    if $ldap_proxy_enabled {
+        class { '::apache::mod::proxy_http': }
+        class { '::apache::mod::proxy': }
+        class { '::apache::mod::auth_basic': }
+        class { '::apache::mod::authnz_ldap': }
+        class { '::apache::mod::headers': }
+        class { '::passwords::ldap::production': }
+
+        $proxypass = $passwords::ldap::production::proxypass
+
+        # Set up the VirtualHost
+        apache::site { 'superset.wikimedia.org':
+            content => template('profile/superset/superset.wikimedia.org.erb'),
+        }
+
+        ferm::service { 'superset-http':
+            proto  => 'tcp',
+            port   => '80',
+            srange => '$CACHE_MISC',
+        }
+
+        # Use AUTH_REMOTE_USER if we are using
+        # LDAP authenticated HTTP proxy.
+        $auth_type = 'AUTH_REMOTE_USER'
+    }
+    else {
+        $auth_type = undef
+    }
+
     class { '::superset':
+        workers          => $workers,
         database_uri     => $full_database_uri,
         secret_key       => $secret_key,
         admin_user       => $admin_user,
         admin_password   => $admin_password,
+        auth_type        => $auth_type,
         password_mapping => $password_mapping,
         statsd           => $statsd,
     }
@@ -56,6 +95,7 @@ class profile::superset(
     monitoring::service { 'superset':
         description   => 'superset',
         check_command => "check_tcp!${::superset::port}",
-        require       => Systemd::Service['superset'],
+        require       => Class['::superset'],
     }
+
 }
