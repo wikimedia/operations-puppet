0cc9be00c2ad3cb43f875a6a82f1415ea6386576
Keith Herron <kherron@wikimedia.org>
2017-12-07 16:25:43 -0500

    puppetdb: remove (cleanup) ferm allow for puppetcompiler1001
    Bug: T177254
    Change-Id: I4c202ec23cc372a3a0a42d7e7eccbb585e03969d

--- modules/role/manifests/puppetmaster/puppetdb.pp
+++ modules/role/manifests/puppetmaster/puppetdb.pp
@@ -56,14 +56,6 @@ class role::puppetmaster::puppetdb (
         srange => '$CUMIN_MASTERS',
     }
 
-    # Temporarily allow puppetcompiler1001 to puppetdb nginx T177254
-    # Remove after upgrading to puppet 4
-    ferm::service { 'puppetdb-puppetcompiler1001':
-        proto  => 'tcp',
-        port   => 443,
-        srange => '(10.64.32.17 2620:0:861:103:10:64:32:17)',
-    }
-
     # Tuning
     file { "/etc/postgresql/${pgversion}/main/tuning.conf":
         ensure  => 'present',
