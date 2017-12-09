0ab345a588f085b19bff13b13319293a42a67436
Ariel T. Glenn <ariel@wikimedia.org>
2017-12-09 14:14:18 +0200

    move wikidata weekly dumps to new nfs server
    This includes removing the cleanup job in the wikidata dumps script;
    cleanups are handled by a job that takes care of all misc dumps.
    It also adds legacy directories used by wikidata weekly cron job.
    
    Additionally:
    Remove the last deprecated-user references from snapshot modules
    Add explicitly the 'temp' dir for dumps production to new nfs server
    Consolidate the declarations of dirs where xml/sql dumps, misc dumps
      and misc datasets live
    Remove a few extra spaces in some class declarations
    
    Bug: T179942
    Change-Id: I6d23ea51c9517ed7dbcd5d175ca8e8901315a7d4

--- modules/profile/manifests/dumps/generation/worker/cronrunner.pp
+++ modules/profile/manifests/dumps/generation/worker/cronrunner.pp
@@ -1,6 +1,5 @@
 class profile::dumps::generation::worker::cronrunner {
     class { '::snapshot::cron':
-        depr_user     => 'datasets',
         miscdumpsuser => 'dumpsgen',
         group         => 'www-data',
     }
