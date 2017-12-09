bf74d316e2b3a4c4b82973bd65d2b4f87ac7771a
Ariel T. Glenn <ariel@wikimedia.org>
2017-12-09 17:17:41 +0200

    clean up temp files created during xml/sql and other dumps generation
    Bug: T180102
    Change-Id: Ie54a25988ea56492e4215eadd8bac48c2179597f

--- modules/profile/manifests/dumps/web/cleanup.pp
+++ modules/profile/manifests/dumps/web/cleanup.pp
@@ -2,11 +2,13 @@ class profile::dumps::web::cleanup(
     $isreplica = hiera('profile::dumps::cleanup::isreplica'),
     $miscdumpsdir = hiera('profile::dumps::miscdumpsdir'),
     $publicdir = hiera('profile::dumps::xmldumpspublicdir'),
+    $dumpstempdir = hiera('profile::dumps::dumpstempdir'),
 ) {
     class {'::dumps::web::cleanup':
         isreplica    => $isreplica,
         miscdumpsdir => $miscdumpsdir,
         publicdir    => $publicdir,
+        dumpstempdir => $dumpstempdir,
         user         => 'dumpsgen',
     }
 }
