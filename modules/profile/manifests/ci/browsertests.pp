4ea3a6362e7e868fed18aea904356ac3c53998be
Paladox <thomasmulhall410@yahoo.com>
2017-12-11 19:10:35 +0000

    contint: Remove duplicate  Class[Contint::Browsers]
    This is defined in role::ci::slave::labs which is loaded by the main
    class modules/role/manifests/ci/slave/labs.pp
    
    so no need to do it in browsertests.pp.
    
    Duplicate declaration: Class[Contint::Browsers] is already declared;
    cannot redeclare at
    /etc/puppet/modules/profile/manifests/ci/browsertests.pp:9 at
    /etc/puppet/modules/profile/manifests/ci/browsertests.pp:9:5
    
    Change-Id: If1d89957203ad7199d9a1589b2f28c5f5bbe2c4e

--- modules/profile/manifests/ci/browsertests.pp
+++ modules/profile/manifests/ci/browsertests.pp
@@ -2,12 +2,6 @@
 #
 class profile::ci::browsertests {
 
-    class { '::contint::packages::ruby':
-    }
-
-    # Provides phantomjs, firefox and xvfb
-    require profile::ci::browsers
-
     # For Selenium jobs recording (T113520)
     package { 'libav-tools':
         ensure => present,
