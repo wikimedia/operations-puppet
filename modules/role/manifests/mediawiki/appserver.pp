ebd2d47b1623dc854142a0afabbcd7e852449a77
Daniel Zahn <dzahn@wikimedia.org>
2017-12-11 13:53:19 -0800

    mediawiki::appserver: move firewall from site to role
    Change-Id: I3ce1babecc3ab27bfd991c8ef5fbd9936e6d139e

--- modules/role/manifests/mediawiki/appserver.pp
+++ modules/role/manifests/mediawiki/appserver.pp
@@ -3,6 +3,7 @@ class role::mediawiki::appserver {
     system::role { 'mediawiki::appserver': }
 
     include ::role::mediawiki::webserver
+    include ::profile::base::firewall
     include ::profile::prometheus::apache_exporter
     include ::profile::prometheus::hhvm_exporter
 
