71e2cb84323f677c5c07d56a9c118933f68d8da6
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-12-12 15:59:02 +0100

    maps: extract style used by kartotherian / tilerator as parameter
    Bug: T162241
    Change-Id: I135c644fc312f1586156d0048f00074d6ec45a55

--- modules/profile/manifests/maps/sources_config.pp
+++ modules/profile/manifests/maps/sources_config.pp
@@ -5,18 +5,16 @@
 # Parameters:
 #   mode
 #       Use either kartotherian or tilerator mode (config is slightly different for each).
+#  style
+#       The style to use to render tiles
 define profile::maps::sources_config (
     $storage_id,
     $ensure = 'present',
     $mode   = 'kartotherian',
+    $style  = 'osm-bright-style',
 ) {
     if ! $mode in ['kartotherian', 'tilerator'] {
         fail("mode shoudl be either kartotherian or tilerator but was ${mode}")
-    }
-
-    $loader = $mode ? {
-        'tilerator' => '\'@kartotherian/osm-bright-style\'',
-        default     => 'osm-bright-style'
     }
 
     file { "/etc/${title}/sources.yaml":
