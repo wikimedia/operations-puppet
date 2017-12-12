63a91d01c70e423c0cc107cf557e528eef7c51c3
andrewbogott <abogott@wikimedia.org>
2017-12-12 00:55:40 -0600

    tools k8s workers:  add a mostly-permissive firewall
    Bug: T180055
    Change-Id: Icb9fc1a1f9d12708b8dc71282b4b4082e0652b8a

--- modules/role/manifests/toollabs/k8s/worker.pp
+++ modules/role/manifests/toollabs/k8s/worker.pp
@@ -4,11 +4,6 @@ class role::toollabs::k8s::worker {
 
     $flannel_etcd_url = join(prefix(suffix(hiera('flannel::etcd_hosts'), ':2379'), 'https://'), ',')
 
-    ferm::service { 'flannel-vxlan':
-        proto => udp,
-        port  => 8472,
-    }
-
     class { '::k8s::flannel':
         etcd_endpoints => $flannel_etcd_url,
     }
@@ -38,8 +33,44 @@ class role::toollabs::k8s::worker {
 
 
     class { '::profile::kubernetes::node':
-        use_cni   => false,
-        infra_pod => 'docker-registry.tools.wmflabs.org/pause:2.0',
-        require   => Class[::profile::docker::flannel],
+        use_cni        => false,
+        infra_pod      => 'docker-registry.tools.wmflabs.org/pause:2.0',
+        require        => Class[::profile::docker::flannel],
+        prod_firewalls => false,
     }
+
+    # Firewall!  Kubelet opens some scary ports to the outside world, 
+    #  so this class just closes those particular ports whilst leaving everything
+    #  else in the hands of the OpenStack security groups.
+    $master_hosts = hiera('k8s::master_hosts')
+    $master_hosts_ferm = join($master_hosts, ' ')
+
+    ferm::service { 'tools-kubelet-http':
+        proto  => 'tcp',
+        port   => '10250',
+        srange => "@resolve((${master_hosts_ferm}))",
+    }
+    ferm::service { 'tools-kubelet-http-readonly-prometheus':
+        proto  => 'tcp',
+        port   => '10255',
+        srange => "@resolve((${master_hosts_ferm}))",
+    }
+
+    ferm::service { 'flannel-vxlan':
+        proto => udp,
+        port  => 8472,
+    }
+
+    # We really only want to be this permissive for other tools hosts.
+    #  Fortunately there's a nova-network security rule overlaying this
+    #  one which limits this permissive policy to things within the tools
+    #  project.
+    #
+    # Ideally this will get winnowed down as time passes, but for the
+    #  moment I just really want to get the above things properly closed off
+    ferm::rule {'rest-of-everything':
+        rule => 'saddr 10.0.0.0/8 proto tcp dport (1:8472, 8473:10249, 10251:10254, 10256:65535) ACCEPT;'
+    }
+
+    include profile::base::firewall
 }
