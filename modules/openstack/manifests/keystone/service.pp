46e425044d2d6ccde446e1354258f8bb38cc4efe
cpettet <rush@wikimedia.org>
2017-12-12 08:58:04 -0600

    openstack: first install control node dependency issues
    Bug: T171494
    Change-Id: I8721ec4b59745b9926759249cb9af9e8eaafae0b

--- modules/openstack/manifests/keystone/service.pp
+++ modules/openstack/manifests/keystone/service.pp
@@ -35,13 +35,25 @@ class openstack::keystone::service(
     $labs_networks = $network::constants::labs_networks
 
     package { 'keystone':
-        ensure  => present,
+        ensure  => 'present',
     }
+
     package { 'python-oath':
-        ensure  => present,
+        ensure  => 'present',
     }
+
     package { 'python-mysql.connector':
-        ensure  => present,
+        ensure  => 'present',
+    }
+
+    group {'keystone':
+        ensure  => 'present',
+        require => Package['keystone'],
+    }
+
+    user {'keystone':
+        ensure  => 'present',
+        require => Package['keystone'],
     }
 
     if $token_driver == 'redis' {
@@ -52,37 +64,43 @@ class openstack::keystone::service(
 
     file {
         '/var/log/keystone':
-            ensure => directory,
-            owner  => 'keystone',
-            group  => 'www-data',
-            mode   => '0775';
+            ensure  => 'directory',
+            owner   => 'keystone',
+            group   => 'www-data',
+            mode    => '0775',
+            require => Package['keystone'];
         '/etc/keystone':
-            ensure => directory,
-            owner  => 'keystone',
-            group  => 'keystone',
-            mode   => '0755';
+            ensure  => 'directory',
+            owner   => 'keystone',
+            group   => 'keystone',
+            mode    => '0755',
+            require => Package['keystone'];
         '/etc/keystone/keystone.conf':
-            content => template("openstack/${version}/keystone/keystone.conf.erb"),
+            ensure  => 'present',
             owner   => 'keystone',
             group   => 'keystone',
             mode    => '0444',
+            content => template("openstack/${version}/keystone/keystone.conf.erb"),
             notify  => Service['keystone'],
             require => Package['keystone'];
         '/etc/keystone/keystone-paste.ini':
-            source  => "puppet:///modules/openstack/${version}/keystone/keystone-paste.ini",
+            ensure  => 'present',
             owner   => 'root',
             group   => 'root',
             mode    => '0644',
+            source  => "puppet:///modules/openstack/${version}/keystone/keystone-paste.ini",
             notify  => Service['keystone'],
             require => Package['keystone'];
         '/etc/keystone/policy.json':
-            source  => "puppet:///modules/openstack/${version}/keystone/policy.json",
+            ensure  => 'present',
             mode    => '0644',
             owner   => 'root',
             group   => 'root',
+            source  => "puppet:///modules/openstack/${version}/keystone/policy.json",
             notify  => Service['keystone'],
             require => Package['keystone'];
         '/etc/keystone/logging.conf':
+            ensure  => 'present',
             source  => "puppet:///modules/openstack/${version}/keystone/logging.conf",
             owner   => 'root',
             group   => 'root',
@@ -90,13 +108,15 @@ class openstack::keystone::service(
             notify  => Service['keystone'],
             require => Package['keystone'];
         '/usr/lib/python2.7/dist-packages/wmfkeystoneauth':
-            source  => "puppet:///modules/openstack/${version}/keystone/wmfkeystoneauth",
+            ensure  => 'present',
             owner   => 'root',
             group   => 'root',
             mode    => '0644',
+            source  => "puppet:///modules/openstack/${version}/keystone/wmfkeystoneauth",
             notify  => Service['keystone'],
             recurse => true;
         '/usr/lib/python2.7/dist-packages/wmfkeystoneauth.egg-info':
+            ensure  => 'present',
             source  => "puppet:///modules/openstack/${version}/keystone/wmfkeystoneauth.egg-info",
             owner   => 'root',
             group   => 'root',
