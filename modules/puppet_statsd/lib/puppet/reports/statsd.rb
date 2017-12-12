70954126050db9a4d88eb6e42ee3f3d615c58876
Faidon Liambotis <faidon@wikimedia.org>
2017-12-12 14:46:40 +0200

    puppet_statsd: fix three RuboCop offenses
    - Layout/SpaceAfterComma
    - Lint/UselessAssignment
    - Style/RedundantSelf
    
    Change-Id: Ic223244a5c8769e9c3d52527000017e02b99af83

--- modules/puppet_statsd/lib/puppet/reports/statsd.rb
+++ modules/puppet_statsd/lib/puppet/reports/statsd.rb
@@ -5,7 +5,9 @@ require 'yaml'
 require 'erb'
 
 Puppet::Reports.register_report(:statsd) do
-    desc = 'Send Puppet metrics to StatsD'
+    desc <<-DESC
+      Send Puppet metrics to StatsD
+    DESC
 
     def load_config
         config_file = File.join Puppet.settings[:confdir], 'statsd.yaml'
@@ -15,12 +17,12 @@ Puppet::Reports.register_report(:statsd) do
 
     def process
         config = load_config
-        hostname = self.host
-        Puppet.notice "Sending metrics for #{self.host} to "\
+        hostname = host
+        Puppet.notice "Sending metrics for #{host} to "\
             "#{config[:statsd_host]}:#{config[:statsd_port]}.."
 
         socket = UDPSocket.new
-        self.metrics['time'].values.each do |metric,description,value|
+        metrics['time'].values.each do |metric, description, value|
             value = (value * 1000).round  # Convert fractional seconds to whole miliseconds
             name = ERB.new(config[:metric_format]).result(binding)
             socket.send("#{name}:#{value}|ms", 0, config[:statsd_host], config[:statsd_port])
