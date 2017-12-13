af71a14f968e971d18dbedf73b7ca0906a77c83f
Jaime Crespo <jcrespo@wikimedia.org>
2017-12-13 09:05:12 +0100

    mariadb: Create profile::client for non-root mariadb clients
    ProxySQL requires extra tls configuration, which comes from the
    client roles/profiles. Create a new profile, similar to the old
    mariadb::client role, but with only the non-root bits (software
    and configuration, not passwords).
    
    Rename old mariadb::client role to wmf_root_client profile to
    avoid ambiguity.
    
    This is all required to enable tls towards mysql from terbium and
    other maintenance hosts.
    
    Bug: T175672
    Change-Id: I37d8d13d85370020e81cac386a8a5d1b667baf65

---
+++ modules/profile/manifests/mariadb/client.pp
@@ -0,0 +1,13 @@
+# hosts with client utilities to conect to remote servers
+# This role provides remote access to mysql server
+class profile::mariadb::client {
+    class { 'mariadb::packages_client': }
+
+    class { 'mariadb::config':
+        config   => 'profile/mariadb/mysqld_config/client.my.cnf.erb',
+        ssl      => 'puppet-cert',
+        ssl_ca   => '/etc/ssl/certs/Puppet_Internal_CA.pem',
+        ssl_cert => '/etc/mysql/ssl/cert.pem',
+        ssl_key  => '/etc/mysql/ssl/client.key',
+    }
+}
