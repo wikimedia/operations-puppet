5f44b101bec65a3012034c143b90c0fb33443551
andrewbogott <abogott@wikimedia.org>
2017-12-13 03:16:09 -0600

    labsaliaser: handle requests for the simple hostname 'puppet'
    This reverts commit ddec1f256693c9770076805fe934928e661e8e2d and
    solves the problem in a much simpler way, by just inserting
    the IP for the primary puppetmaster into extra_records.
    
    Bug: T181375
    Change-Id: I42755fecbaaf98ea0d82fbf6502624ed8c3cb490

--- modules/profile/manifests/openstack/labtest/pdns/recursor/service.pp
+++ modules/profile/manifests/openstack/labtest/pdns/recursor/service.pp
@@ -5,16 +5,14 @@ class profile::openstack::labtest::pdns::recursor::service(
     $pdns_recursor = hiera('profile::openstack::labtest::pdns::recursor'),
     $tld = hiera('profile::openstack::labtest::pdns::tld'),
     $private_reverse = hiera('profile::openstack::labtest::pdns::private_reverse'),
-    $puppetmaster_hostname = hiera('profile::openstack::labtest::puppetmaster_hostname'),
     ) {
 
     class {'::profile::openstack::base::pdns::recursor::service':
-        nova_controller       => $nova_controller,
-        observer_password     => $observer_password,
-        pdns_host             => $pdns_host,
-        pdns_recursor         => $pdns_recursor,
-        tld                   => $tld,
-        private_reverse       => $private_reverse,
-        puppetmaster_hostname => $puppetmaster_hostname
+        nova_controller   => $nova_controller,
+        observer_password => $observer_password,
+        pdns_host         => $pdns_host,
+        pdns_recursor     => $pdns_recursor,
+        tld               => $tld,
+        private_reverse   => $private_reverse,
     }
 }
