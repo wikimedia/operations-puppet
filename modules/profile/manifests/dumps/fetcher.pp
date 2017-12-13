fa5d61c851a4620c3a803b59f3d892e42e75d4d9
Ariel T. Glenn <ariel@wikimedia.org>
2017-12-13 13:45:04 +0200

    rename 'otherdir' in the dumps modules
    On some hosts the path for misc dumps and datasets ends in .../other/
    and on some hosts not, and it's a horrible name anyways. Other what?
    we leave the paths as they are, but change the var names everywhere
    to something with meaning.
    
    Also remove more hardcoded paths in the manifests.
    
    Change-Id: I5472a464c2db2335c6e692ff1053c754889ab18d

--- modules/profile/manifests/dumps/fetcher.pp
+++ modules/profile/manifests/dumps/fetcher.pp
@@ -1,19 +1,20 @@
 class profile::dumps::fetcher(
     $xmldumpsdir = hiera('profile::dumps::xmldumpspublicdir'),
-    $otherdir = hiera('profile::dumps::miscdumpsdir'),
+    $miscdatasetsdir = hiera('profile::dumps::miscdumpsdir'),
 ) {
     class {'dumps::web::fetches::kiwix':
-        user        => 'dumpsgen',
-        group       => 'dumpsgen',
-        xmldumpsdir => $xmldumpsdir,
-        otherdir    => $otherdir,
+        user            => 'dumpsgen',
+        group           => 'dumpsgen',
+        xmldumpsdir     => $xmldumpsdir,
+        miscdatasetsdir => $miscdatasetsdir,
     }
     class {'dumps::web::fetches::stats':
-        src      => 'stat1005.eqiad.wmnet::hdfs-archive',
-        otherdir => $otherdir,
-        user     => 'dumpsgen',
+        src             => 'stat1005.eqiad.wmnet::hdfs-archive',
+        miscdatasetsdir => $miscdatasetsdir,
+        user            => 'dumpsgen',
     }
     class {'dumps::web::fetches::wikitech_dumps':
-        url => 'https://wikitech.wikimedia.org/dumps/',
+        url             => 'https://wikitech.wikimedia.org/dumps/',
+        miscdatasetsdir => $miscdatasetsdir,
     }
 }
