5e78ddeb3658f1b61e89ec364f790e607b9f28a2
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-12-13 14:40:51 +0100

    Add Prometheus RabbitMQ exporter
    Bug: T181802
    Change-Id: I7f8569634218f06dfec448b99c663adba47d7513

---
+++ modules/profile/manifests/prometheus/rabbitmq_exporter.pp
@@ -0,0 +1,27 @@
+class profile::prometheus::rabbitmq_exporter (
+    $prometheus_nodes = hiera('profile::prometheus::rabbit_monitoring_host'),
+    $rabbit_monitor_username = hiera('profile::prometheus::rabbit_monitor_user'),
+    $rabbit_monitor_password = hiera('profile::prometheus::rabbit_monitor_pass'),
+    $rabbit_host = 'localhost:15672',
+) {
+
+    file { '/etc/prometheus/rabbitmq-exporter.yaml':
+        ensure  => 'present',
+        owner   => 'prometheus',
+        group   => 'prometheus',
+        mode    => '0440',
+        content => template('profile/prometheus/rabbitmq-exporter.conf.erb'),
+    }
+
+    service { 'prometheus-rabbitmq-exporter':
+        ensure  => running,
+        require => File['/etc/prometheus/rabbitmq-exporter.yaml'],
+    }
+
+    $ferm_srange = "(@resolve((${prometheus_nodes})) @resolve((${prometheus_nodes}), AAAA))"
+    ferm::service { 'prometheus-rabbitmq-exporter':
+        proto  => 'tcp',
+        port   => '9195',
+        srange => $ferm_srange,
+    }
+}
