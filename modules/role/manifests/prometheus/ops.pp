7e9b43f38c8ce11475d70d5aaddea7eeefbf8d9d
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-12-13 15:26:00 +0100

    elasticsearch: configure prometheus to collect metrics from elasticsearch
    Bug: T181627
    Change-Id: I7e3ab94a0c015823a88a79d48f7086dc79733403

--- modules/role/manifests/prometheus/ops.pp
+++ modules/role/manifests/prometheus/ops.pp
@@ -652,6 +652,22 @@ class role::prometheus::ops {
         port       => '9142',
     }
 
+    $elasticsearch_jobs = [
+        {
+            'job_name'        => 'elasticsearch',
+            'scheme'          => 'http',
+            'file_sd_configs' => [
+                { 'files' => [ "${targets_path}/elasticsearch_*.yaml" ]}
+            ],
+        },
+    ]
+    prometheus::class_config { "elasticsearch_${::site}":
+        dest       => "${targets_path}/elasticsearch_${::site}.yaml",
+        site       => $::site,
+        class_name => 'profile::elasticsearch::monitoring',
+        port       => '9108',
+    }
+
     prometheus::server { 'ops':
         storage_encoding      => '2',
         listen_address        => '127.0.0.1:9900',
@@ -662,7 +678,7 @@ class role::prometheus::ops {
             $mysql_jobs, $varnish_jobs, $memcached_jobs, $hhvm_jobs,
             $apache_jobs, $etcd_jobs, $etcdmirror_jobs, $pdu_jobs,
             $nginx_jobs, $pybal_jobs, $blackbox_jobs, $jmx_exporter_jobs,
-            $redis_jobs, $mtail_jobs, $ldap_jobs,
+            $redis_jobs, $mtail_jobs, $ldap_jobs, $elasticsearch_jobs,
         ),
         global_config_extra   => $config_extra,
     }
