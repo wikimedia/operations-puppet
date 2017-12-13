a1fcab19c9095fe428e3abce859047c8c2725a1a
cpettet <rush@wikimedia.org>
2017-12-13 12:25:01 -0600

    openstack: cloud repo  explicit apt-key update and apt-get update
    At the moment this throws errors for untrusted packages via
    the cloudrepo.  Theory is an apt-key update and following
    apt-get update inline will resolve.
    
    Bug: T171494
    Change-Id: I1461ff56d5907e76034be362094a8adecdc92897

--- modules/openstack/manifests/cloudrepo.pp
+++ modules/openstack/manifests/cloudrepo.pp
@@ -13,7 +13,17 @@ class openstack::cloudrepo(
             dist       => "trusty-updates/${version}",
             components => 'main',
             keyfile    => 'puppet:///modules/openstack/cloudrepo/ubuntu-cloud.key';
+            notify     => Exec['apt_key_and_update'],
         }
+
+        # First installs can trip without this
+        # seeing the mid-run repo as untrusted
+        exec {'apt_key_and_update':
+            command     => '/usr/bin/apt-key update && /usr/bin/apt-get update',
+            refreshonly => true,
+            logoutput   => true,
+        }
+
     } elsif os_version('debian jessie') and ($version != 'liberty') {
         fail("T169099: There is no plan for ${version} on Jessie")
     }
