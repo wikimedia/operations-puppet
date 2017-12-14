334e67b5748962647684ae7e65a64da6138b8e5a
Antoine Musso <hashar@free.fr>
2017-12-14 09:37:20 +0100

    Migrate contint::worker_localhost to a profile
    Resolved violation:
    
    modules/contint/manifests/worker_localhost.pp:16
    class 'contint::worker_localhost' includes apache::mod::rewrite from
    another module
    
    Change-Id: I3ae120b07a173cc35cc46eca64a5bc9a6e993523

--- modules/profile/manifests/ci/worker_localhost.pp
+++ modules/profile/manifests/ci/worker_localhost.pp
@@ -1,7 +1,33 @@
 # == Class profile::ci::worker_localhost
 #
-class profile::ci::worker_localhost() {
-    class { '::contint::worker_localhost':
-        owner => 'jenkins-deploy',
+# Apache virtual host for localhost testing (qunit/selenium)
+#
+# === Parameters
+#
+# [*owner*]
+# Unix user that runs the Jenkins job.
+#
+class profile::ci::worker_localhost(
+    $owner=hiera('jenkins_agent_username'),
+) {
+    file { '/srv/localhost-worker':
+        ensure => directory,
+        mode   => '0775',
+        owner  => 'jenkins-deploy',
+        group  => 'root',
     }
+
+    class { '::apache::mod::rewrite':
+    }
+
+    contint::localvhost { 'worker':
+        port       => 9412,
+        docroot    => '/srv/localhost-worker',
+        log_prefix => 'worker',
+        require    => [
+            File['/srv/localhost-worker'],
+            Class['::apache::mod::rewrite'],
+        ],
+    }
+
 }
