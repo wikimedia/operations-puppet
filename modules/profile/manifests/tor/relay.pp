f50f53f7c222d00bb0c1e20f7b548d877fa8ce9c
Faidon Liambotis <faidon@wikimedia.org>
2017-12-14 17:15:49 +0200

    tor: add an additional relay instance
    Tor is notoriously bad at being single threaded, and we currently max
    out 2½ of the CPUs on the relay box, which seems to be a bottleneck.
    Add an additional relay (but not more), given the machine is a single
    quad-core CPU. Be smart about it by using the new Puppet 4 each
    construct -- hopefully that works :)
    
    Change-Id: Idaf9c9ab1a78a6db412050787fa573b45db53f15

--- modules/profile/manifests/tor/relay.pp
+++ modules/profile/manifests/tor/relay.pp
@@ -4,39 +4,59 @@ class profile::tor::relay {
     include passwords::tor
     $controlpassword = $passwords::tor::hashed_control_password
 
+    # The default instance is special, listening on 80/443
     class { '::tor':
+        nickname        => 'wikimediaeqiad1',
         controlport     => '9051',
         controlpassword => $controlpassword,
         orport          => '443',
         dirport         => '80',
         address         => 'tor-eqiad-1.wikimedia.org',
-        nickname        => 'wikimediaeqiad1',
-        contact         => 'noc@wikimedia.org',
-        exit_policy     => 'reject *:*', # no exits allowed
-    }
-
-    ::tor::instance { 'wikimediaeqiad2':
-        controlport     => '9052',
-        controlpassword => $controlpassword,
-        orport          => '9002',
-        dirport         => '9032',
-        address         => 'tor-eqiad-1.wikimedia.org',
-        nickname        => 'wikimediaeqiad2',
         contact         => 'noc@wikimedia.org',
         exit_policy     => 'reject *:*', # no exits allowed
     }
 
     # actual Tor port where clients connect, public
-    ferm::service { 'tor_orport':
+    ferm::service { 'tor_orport_wikimediaeqiad1':
         desc  => 'port for the actual Tor client connections',
         proto => 'tcp',
-        port  => '(443 9002)',
+        port  => '443',
     }
 
     # for serving directory updates, public
-    ferm::service { 'tor_dirport':
+    ferm::service { 'tor_dirport_wikimediaeqiad1':
         desc  => 'port advertising the directory service',
         proto => 'tcp',
-        port  => '(80 9032)',
+        port  => '80',
+    }
+
+    # setup multiple additional instances, each with their own id and ports
+    ['2', '3'].each |$instance| {
+        $instance_name = "wikimediaeqiad${instance}"
+        $controlport = "905${instance}"
+        $orport = "900${instance}"
+        $dirport = "903${instance}"
+
+        ::tor::instance { $instance_name:
+            nickname        => $instance_name,
+            controlport     => $controlport,
+            controlpassword => $controlpassword,
+            orport          => $orport,
+            dirport         => $dirport,
+            address         => 'tor-eqiad-1.wikimedia.org',
+            contact         => 'noc@wikimedia.org',
+            exit_policy     => 'reject *:*', # no exits allowed
+        }
+
+        ferm::service { "tor_orport_${instance_name}":
+            desc  => 'port for the actual Tor client connections',
+            proto => 'tcp',
+            port  => $orport,
+        }
+        ferm::service { "tor_dirport_${instance_name}":
+            desc  => 'port advertising the directory service',
+            proto => 'tcp',
+            port  => $dirport,
+        }
     }
 }
