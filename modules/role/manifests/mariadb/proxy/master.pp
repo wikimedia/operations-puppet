cecc6a3c8ad45417ba09be7b657ec038e7fbe656
Jaime Crespo <jcrespo@wikimedia.org>
2017-12-15 13:13:00 +0100

    Update mariadb::proxy to the latest style and path locations
    Create profile::mariadb::proxy{,::master,::replicas}, for the base,
    master failover and replica load balancing roles. Replica class is
    unused, so probably untested and doesn't work.
    
    Make socker and pid configurable, default to /run location, but
    it can now be overriden by host by host migration from /tmp to
    /run.
    
    Bug: T148507
    Bug: T159412
    Change-Id: I71a888b516866ab22174565b00c426fafe55a7d9

--- modules/role/manifests/mariadb/proxy/master.pp
+++ modules/role/manifests/mariadb/proxy/master.pp
@@ -1,33 +1,8 @@
 # db master failover with a proxy
-class role::mariadb::proxy::master(
-    $shard,
-    $primary_name,
-    $primary_addr,
-    $secondary_name,
-    $secondary_addr,
-    ) {
+class role::mariadb::proxy::master {
 
     include role::mariadb::ferm
-
-    class { 'role::mariadb::proxy':
-        shard => $shard,
-    }
-    if os_version('debian >= stretch') {
-        $master_template = 'db-master-stretch.cfg'
-    } else {
-        $master_template = 'db-master.cfg'
-    }
-
-    file { '/etc/haproxy/conf.d/db-master.cfg':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        content => template("role/haproxy/${master_template}.erb"),
-    }
-
-    nrpe::monitor_service { 'haproxy_failover':
-        description  => 'haproxy failover',
-        nrpe_command => '/usr/lib/nagios/plugins/check_haproxy --check=failover',
-    }
+    include ::profile::mariadb::proxy
+    include ::profile::mariadb::proxy::master
 }
 
