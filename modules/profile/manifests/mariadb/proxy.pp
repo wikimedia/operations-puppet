eadaee5339405fff2833ce6b60163b9df5ca5770
Jaime Crespo <jcrespo@wikimedia.org>
2017-12-15 13:13:00 +0100

    Update mariadb::proxy to the latest style and path locations
    Create profile::mariadb::proxy{,::master,::replicas}, for the base,
    master failover and replica load balancing roles. Replica class is
    unused, so probably untested and doesn't work.
    
    Make socker and pid configurable, default to /run location, but
    it can now be overriden by host by host migration from /tmp to
    /run.
    
    Bug: T148507
    Bug: T159412
    Change-Id: I71a888b516866ab22174565b00c426fafe55a7d9

---
+++ modules/profile/manifests/mariadb/proxy.pp
@@ -0,0 +1,15 @@
+# base profile to have a manually-managed haproxy installation, pointing to
+# to nowere by default. Check ::profile::mariadb::proxy::{master,replica} for
+# it to do something useful (failover or load balancing)
+class profile::mariadb::proxy (
+    $pid    = hiera('profile::mariadb::proxy::pid', '/run/haproxy/haproxy.pid'),
+    $socket = hiera('profile::mariadb::proxy::socket', '/run/haproxy/haproxy.sock'),
+    ){
+
+    class { 'haproxy':
+        template => 'profile/mariadb/proxy/db.cfg.erb',
+        pid      => $pid,
+        socket   => $socket,
+    }
+
+}
