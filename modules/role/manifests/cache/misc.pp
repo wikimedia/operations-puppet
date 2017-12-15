7ff93af6e65882491162c31f17340bba34d3ecd3
elukey <ltoscano@wikimedia.org>
2017-12-15 14:50:08 +0100

    role::cache::misc: add new vk instance to test TLS traffic
    This change adds to all the cache misc hosts a new Varnishkafka
    instance that pushes webrequest data to the new Kafka Jumbo
    cluster via TLS. It has already been successfully deployed to
    cache::canary (cp1008).
    
    Change-Id: Ib6777fb17bd400aa8fab307885bcfddfb6886e7e

--- modules/role/manifests/cache/misc.pp
+++ modules/role/manifests/cache/misc.pp
@@ -6,4 +6,8 @@ class role::cache::misc {
     include ::profile::cache::base
     include ::profile::cache::ssl::unified
     include ::profile::cache::misc
+
+    # Temp. experiment to duplicate/mirror the webrequest data
+    # to the new Kafka Jumbo brokers.
+    include ::profile::cache::kafka::webrequest::jumbo
 }
