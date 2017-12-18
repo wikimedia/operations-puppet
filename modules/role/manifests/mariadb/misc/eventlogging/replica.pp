652dfd09ec972949b9b3611a4bd12c5655d93856
elukey <ltoscano@wikimedia.org>
2017-12-18 17:42:27 +0100

    profile::mariadb::misc::el::master: apply data sanitization policies
    Bug: T108850
    Change-Id: I1f4d273e1e1d537fdbd978cb69ea804ee9ea8802

--- modules/role/manifests/mariadb/misc/eventlogging/replica.pp
+++ modules/role/manifests/mariadb/misc/eventlogging/replica.pp
@@ -8,6 +8,9 @@ class role::mariadb::misc::eventlogging::replica {
     include ::profile::mariadb::misc::eventlogging::database
     # custom manual replication setup
     include ::profile::mariadb::misc::eventlogging::replication
+    # custom data sanitization setup to apply the Analytics
+    # data retention policies
+    include ::profile::mariadb::misc::eventlogging::sanitization
 
     system::role { 'role::mariadb::misc::eventlogging::replica':
         description => 'Eventlogging Datastore Custom Replica',
