8997b1b6ec36e64ca13670835c39e10587beaf3c
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-12-19 11:10:43 +0100

    Add PowerDNS exporter to labservices1001
    Change-Id: I10dce89572abb263ed2c187b406adb99ba6da722

---
+++ modules/profile/manifests/prometheus/pdns_exporter.pp
@@ -0,0 +1,15 @@
+class profile::prometheus::pdns_exporter (
+    $prometheus_nodes = 'labmon1001.eqiad.wmnet', # Should be properly defined via Hiera for WMCS
+) {
+    require_package('prometheus-pdns-exporter')
+
+    service { 'prometheus-pdns-exporter':
+        ensure  => running,
+    }
+
+    ferm::service { 'prometheus-pdns-exporter':
+        proto  => 'tcp',
+        port   => '9192',
+        srange => "@resolve((${prometheus_nodes}))",
+    }
+}
