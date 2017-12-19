d440d95989c20bb5375b716f00fcb102119d211e
Jaime Crespo <jcrespo@wikimedia.org>
2017-12-19 17:45:20 +0100

    mariadb: Fix bug causing too restrictive firewall on some proxies
    Fix bug by which proxies with firewall disabled (the most important
    ones, that we were supposed not to touch), actually enabled it with
    no rule exceptions.
    
    This is due to enabling by default base::firewall unconditionally,
    and not only on the ones where it was not disabled.
    
    Change-Id: Idefcb867b27371d69d189bafb8fc04faa928e2f6

--- modules/role/manifests/mariadb/proxy.pp
+++ modules/role/manifests/mariadb/proxy.pp
@@ -1,7 +1,6 @@
 # generic config for a database proxy using haproxy
 class role::mariadb::proxy {
     include ::standard
-    include ::profile::base::firewall
 
     system::role { 'mariadb::proxy':
         description => 'DB Proxy',
--- modules/role/manifests/mariadb/proxy/master.pp
+++ modules/role/manifests/mariadb/proxy/master.pp
@@ -1,7 +1,6 @@
 # db master failover with a proxy
 class role::mariadb::proxy::master {
     include ::standard
-    include ::profile::base::firewall
 
     system::role { 'mariadb::proxy::master':
         description => 'DB Proxy with failover',
