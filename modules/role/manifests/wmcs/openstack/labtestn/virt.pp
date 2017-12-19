3e9695e9f4cb1488fdd2445413f2c51c46fd0e2c
cpettet <rush@wikimedia.org>
2017-12-19 08:39:59 -0600

    openstack: labvirt role shuffle
    Change-Id: I068a32cf3cb49a3051baa956354b05fcefa3aa6d

--- modules/role/manifests/wmcs/openstack/labtestn/virt.pp
+++ modules/role/manifests/wmcs/openstack/labtestn/virt.pp
@@ -1,6 +1,5 @@
 class role::wmcs::openstack::labtestn::virt {
     system::role { $name: }
+    include ::standard
     include ::profile::openstack::labtestn::cloudrepo
-    include ::profile::openstack::labtestn::nova::common
-    include ::profile::openstack::labtestn::nova::compute::service
 }
