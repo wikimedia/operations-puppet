313761c81842c04f3f98c88cbb1dd92c431f4f5e
Reedy <reedy@wikimedia.org>
2017-12-20 17:17:04 +0000

    mediawiki: Update path to loadExitNodes.php
    Follows up Ibd6ee1dcbd387d5a208896b9f0059ad0c03e9eba
    
    Change-Id: I68f7e02ac9c038961c8e36a81fc8a1142eaed653

--- modules/mediawiki/manifests/maintenance/tor_exit_node.pp
+++ modules/mediawiki/manifests/maintenance/tor_exit_node.pp
@@ -1,7 +1,7 @@
 class mediawiki::maintenance::tor_exit_node( $ensure = present ) {
     cron { 'tor_exit_node_update':
         ensure  => $ensure,
-        command => '/usr/local/bin/mwscript extensions/TorBlock/loadExitNodes.php --wiki=aawiki --force > /dev/null',
+        command => '/usr/local/bin/mwscript extensions/TorBlock/maintenance/loadExitNodes.php --wiki=aawiki --force > /dev/null',
         user    => $::mediawiki::users::web,
         minute  => '*/20',
     }
