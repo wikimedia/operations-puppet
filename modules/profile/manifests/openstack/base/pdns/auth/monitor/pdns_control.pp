bef5745fdcae0df017489cbb84abb3acd944d655
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-12-20 12:47:01 +0100

    Adapt sudo config for Prometheus exporter
    Similar to what we did for the Prometheus exporter before.
    
    Change-Id: I29d6f8bc243cd7bab6b3dfe0e54b428b35c384fe

--- modules/profile/manifests/openstack/base/pdns/auth/monitor/pdns_control.pp
+++ modules/profile/manifests/openstack/base/pdns/auth/monitor/pdns_control.pp
@@ -5,6 +5,11 @@ class profile::openstack::base::pdns::auth::monitor::pdns_control {
         privileges => ['ALL=(root) NOPASSWD: /usr/bin/pdns_control list'],
     }
 
+    sudo::user { 'prometheus_sudo_for_pdns':
+        user       => 'prometheus',
+        privileges => ['ALL=(root) NOPASSWD: /usr/bin/pdns_control list'],
+    }
+
     # For the authoritative servers
     diamond::collector { 'PowerDNS':
         ensure   => present,
