3f675163908952ad93264a3fe620fbcc6e076d46
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2017-12-21 14:01:02 +0100

    Add support for selective automatic restarts of stateless services after library upgrades (WIP)
    Add a script which detects whether a given service requires a restart after
    an upgrade of a library or an interpreter. Also add a define to control
    automated daily restarts.
    
    Bug: T135991
    Change-Id: Ie73bb0e393e57b4d3626a959d13f6f400e0d8271

---
+++ modules/base/manifests/auto_restarts.pp
@@ -0,0 +1,15 @@
+# == Class: base::restarts
+#
+# This class automates service restarts for stateless services after
+# a library upgrade
+#
+class base::auto_restarts
+{
+    file { '/usr/local/sbin/wmf-auto-restart':
+        ensure => present,
+        source => 'puppet:///modules/base/wmf-auto-restart.py',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+}
