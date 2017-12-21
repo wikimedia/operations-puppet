2c933265b3d18962ce85a92cf98215bf7ba6640b
Daniel Zahn <dzahn@wikimedia.org>
2017-12-21 13:15:06 -0800

    site/logging/kafkatee: move includes from site to role
    Change-Id: Ib70d493d00e2844237173c68873e9e76c56c1e03

--- manifests/site.pp
+++ manifests/site.pp
@@ -1751,9 +1751,6 @@ node /^oresrdb200[12]\.codfw\.wmnet$/ {
 node 'oxygen.eqiad.wmnet'
 {
     role(logging::kafkatee::webrequest::ops)
-
-    include ::base::firewall
-    include ::standard
 }
 
 # parser cache databases
