0062afe9cf0cdce3461a8a0a7f8ca5e5ea1dd219
Stanislav Malyshev <smalyshev@gmail.com>
2017-12-22 13:46:13 -0800

    Add loading DCAT-AP data into dcatap namespace on WDQS
    Bug: T178978
    Change-Id: Ie1d7eb7567d1409fb1116a71b23b4d5160aa2c7b

---
+++ modules/wdqs/files/cron/reloadDCAT-AP.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script is reloading DCAT-AP data from Wikidata
+# NOTE: This should be run under user that has rights to
+# sudo systemctl reload nginx
+. /usr/local/bin/cronUtils.sh
+
+DCAT_SOURCE=${DCAT_SOURCE:-"https://dumps.wikimedia.org/wikidatawiki/entities/dcatap.rdf"}
+
+newNamespace="dcatap${today}"
+# Drop old dumps
+rm -f ${DATA_DIR}/dcatap-*.rdf
+cd $DEPLOY_DIR
+# Create new NS
+bash createNamespace.sh $newNamespace || exit 1
+# Load the data
+FILENAME=dcatap-${today}.rdf
+loadFileIntoBlazegraph $DCAT_SOURCE $FILENAME "${HOST}${NAMESPACE_URL}${newNamespace}/sparql"
+replaceNamespace dcatap $newNamespace
