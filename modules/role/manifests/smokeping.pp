2cfda3a2290f97c60b800c0c2dca277731f489e7
Daniel Zahn <dzahn@wikimedia.org>
2017-12-22 12:56:24 -0800

    smokeping: convert to profile
    Change-Id: Icdc94d36acd9a051bbfd720fb311dc74d0ba9f9b

--- modules/role/manifests/smokeping.pp
+++ modules/role/manifests/smokeping.pp
@@ -3,28 +3,5 @@ class role::smokeping {
 
     system::role { 'smokeping': description => 'smokeping server' }
 
-    $active_server = hiera('netmon_server')
-    $passive_server = hiera('netmon_server_failover')
-
-    rsync::quickdatacopy { 'var-lib-smokeping':
-        ensure      => present,
-        auto_sync   => true,
-        source_host => $active_server,
-        dest_host   => $passive_server,
-        module_path => '/var/lib/smokeping',
-    }
-
-    ferm::service { 'smokeping-http':
-        proto  => 'tcp',
-        port   => '80',
-        srange => '$CACHE_MISC',
-    }
-
-    backup::set { 'smokeping': }
-
-    class{ '::smokeping':
-        active_server => $active_server,
-    }
-
-    class{ '::smokeping::web': }
+    include ::profile::smokeping
 }
