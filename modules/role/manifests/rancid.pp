7fbd6ff2ac565c3cc23328f56499cb9befaacffa
Daniel Zahn <dzahn@wikimedia.org>
2017-12-22 15:06:33 -0800

    rancid: convert role to profile
    Change-Id: Id80efbae80bbabdf50715aba4b16277b417c7b17

--- modules/role/manifests/rancid.pp
+++ modules/role/manifests/rancid.pp
@@ -7,20 +7,6 @@ class role::rancid {
 
     include ::standard
     include ::profile::backup::host
-
-    backup::set { 'rancid': }
-
-    rsync::quickdatacopy { 'var-lib-rancid':
-      ensure      => present,
-      auto_sync   => false,
-      source_host => 'netmon2001.wikimedia.org',
-      dest_host   => 'netmon1002.wikimedia.org',
-      module_path => '/var/lib/rancid',
-    }
-
-    $active_server = hiera('netmon_server', 'netmon1002.wikimedia.org')
-
-    class { '::rancid':
-        active_server => $active_server,
-    }
+    # include ::profile::base::firewall
+    include ::profile::rancid
 }
