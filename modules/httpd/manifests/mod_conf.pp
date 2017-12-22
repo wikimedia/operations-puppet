3ecd45490551ffc2f2c157c0b9d5a48db90df5f8
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-12-22 13:05:52 +0100

    apache: add httpd module as a replacement
    The new httpd module is mostly a cherry-pick of the better parts of our
    apache module, with some tweaks:
    
    - Use puppet 4 constructs where appropriate.
    - Dependencies are not injected by including the base class
      everywhere. This allows us to make our manifests more compliant with
      the role/profile pattern, and to avoid implicit hiera lookups.
    - The use of classes to include for configuring apache modules is
      dropped. Instead, you're supposed to pass the list of
      configuration-less modules you want to declare to the httpd class,
      that you're supposed to declare once anyways.
    
    Also, add the first type aliases into wmflib, ready to use for general purposes.
    
    Change-Id: I7d8bc4c6ed8105021c6a8dbe45e288411e5d1d69

---
+++ modules/httpd/manifests/mod_conf.pp
@@ -0,0 +1,40 @@
+# == Define: httpd::mod_conf
+#
+# Custom resource for managing the configuration of Apache modules.
+#
+# === Parameters
+#
+# [*mod*]
+#   Module name. Defaults to the resource title.
+#
+# [*loadfile*]
+#   The .load config file that Puppet should manage.
+#   Defaults to the resource title plus a '.load' suffix.
+#
+define httpd::mod_conf(
+    Wmflib::Ensure $ensure   = present,
+    String $mod      = $title,
+    String $loadfile = "${title}.load",
+)
+{
+    require_package('apache2')
+
+    $flags = os_version('debian >= jessie') ? {
+        true    => '-f',
+        default => '',
+    }
+
+    if $ensure == present {
+        exec { "ensure_${ensure}_mod_${mod}":
+            command => "/usr/sbin/a2enmod ${mod}",
+            creates => "/etc/apache2/mods-enabled/${loadfile}",
+            notify  => Service['apache2'],
+        }
+    } else {
+        exec { "ensure_${ensure}_mod_${mod}":
+            command => "/usr/sbin/a2dismod ${flags} ${mod}",
+            onlyif  => "/usr/bin/test -L /etc/apache2/mods-enabled/${loadfile}",
+            notify  => Service['apache2'],
+        }
+    }
+}
