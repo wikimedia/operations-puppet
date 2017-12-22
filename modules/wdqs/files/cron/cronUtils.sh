7ce27fecde27c5672b3aedb80a692066779e8e13
Stanislav Malyshev <smalyshev@gmail.com>
2017-12-22 13:46:13 -0800

    Add loading DCAT-AP data into dcatap namespace on WDQS
    Bug: T178978
    Change-Id: Ie1d7eb7567d1409fb1116a71b23b4d5160aa2c7b

---
+++ modules/wdqs/files/cron/cronUtils.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Cron helper functions
+if [ -r /etc/wdqs/vars.sh ]; then
+  . /etc/wdqs/vars.sh
+fi
+
+if [ -r /etc/wdqs/gui_vars.sh ]; then
+  . /etc/wdqs/gui_vars.sh
+fi
+
+if [ -z "${DATA_DIR}" -o -z "${LOG_DIR}" -o -z "${DEPLOY_DIR}" ]; then
+	echo "Variables not set up right!"
+	exit 1
+fi
+
+HOST="http://localhost:9999"
+NAMESPACE_URL="/bigdata/namespace/"
+today=$(date -u +'%Y%m%d')
+
+function loadFileIntoBlazegraph {
+	# source URL
+	local URL=$1
+	# local filename (will be in DATA_DIR)
+	local fileName=$2
+	local sparqlEndpoint=$3
+	curl -s -f -XGET $URL -o ${DATA_DIR}/${fileName}
+	if [ ! -s ${DATA_DIR}/${fileName} ]; then
+		echo "Could not download $URL into ${fileName}"
+		exit 1
+	fi	
+	curl -s -XPOST --data-binary update="LOAD <file://$DATA_DIR/$FILENAME>" $sparqlEndpoint
+}
+
+# NOTE: This should be run under user that has rights to
+# sudo systemctl reload nginx
+function replaceNamespace {
+	local mainName=$1
+	local currentAlias=$2
+	local oldNamespace=$(cat $ALIAS_FILE | grep $mainName | cut -d' ' -f2 | cut -d ';' -f1)
+	if [ "${oldNamespace}" = ${currentAlias} ]; then
+		# nothing to do
+		return
+	fi
+	if [ -n "${oldNamespace}" ]; then
+		sed -i "/${mainName}/c ${mainName} ${currentAlias};" $ALIAS_FILE
+	else
+		echo "${mainName} ${currentAlias};" >> $ALIAS_FILE
+	fi
+	# Bump nginx to reload config
+	sudo systemctl reload nginx
+	if [ -n "${oldNamespace}" ]; then
+		# Drop old namespace
+		curl -s -X DELETE "${HOST}${NAMESPACE_URL}${oldNamespace}"
+	fi
+}
--- modules/wdqs/files/cron/reloadCategories.sh
+++ modules/wdqs/files/cron/reloadCategories.sh
@@ -2,35 +2,14 @@
 # This script is reloading categories into a new namespace
 # NOTE: This should be run under user that has rights to
 # sudo systemctl reload nginx
-if [ -r /etc/wdqs/vars.sh ]; then
-  . /etc/wdqs/vars.sh
-fi
+. /usr/local/bin/cronUtils.sh
 
-if [ -r /etc/wdqs/gui_vars.sh ]; then
-  . /etc/wdqs/gui_vars.sh
-fi
-
-if [ -z "${DATA_DIR}" -o -z "${LOG_DIR}" -o -z "${DEPLOY_DIR}" ]; then
-	echo "Variables not set up right!"
-	exit 1
-fi
-
-today=$(date -u +'%Y%m%d')
 newNamespace="categories${today}"
 # Drop old dumps
 rm -f ${DATA_DIR}/*-categories.ttl.gz
 cd $DEPLOY_DIR
+# Create new namespace
 bash createNamespace.sh $newNamespace || exit 1
 # Load the data
 bash forAllCategoryWikis.sh loadCategoryDump.sh $newNamespace >> "${LOG_DIR}/${newNamespace}.log"
-# Get old namespace
-oldNamespace=$(cat $ALIAS_FILE | grep categories | cut -d' ' -f2 | cut -d ';' -f1)
-# Switch the map
-# NOTE: right now it overrides the map. If we reuse it for other purposes, this needs to be made smarter.
-echo "categories ${newNamespace};" > $ALIAS_FILE
-# Bump nginx to reload config
-sudo systemctl reload nginx
-if [ -n "${oldNamespace}" ]; then
-	# Drop old namespace
-	curl -s -X DELETE "http://localhost:9999/bigdata/namespace/${oldNamespace}"
-fi
+replaceNamespace categories $newNamespace
