d0b04af7750868b1565532e29bde48f8267db343
Ariel T. Glenn <ariel@wikimedia.org>
2017-12-26 17:12:25 +0200

    manage directories locally on dumps workers if there is no nfs server
    Change-Id: I7949196fdaacb71633206019119ff734a5626cfb

--- modules/snapshot/manifests/dumps/nfsmount.pp
+++ modules/snapshot/manifests/dumps/nfsmount.pp
@@ -1,6 +1,9 @@
 define snapshot::dumps::nfsmount(
     $mountpoint = undef,
     $server = undef,
+    $managed_dumps_subdirs = [],
+    $user = undef,
+    $group = undef,
 ) {
     require_package('nfs-common')
 
@@ -17,6 +20,16 @@ define snapshot::dumps::nfsmount(
             options  => 'bg,hard,tcp,rsize=8192,wsize=8192,intr,nfsvers=3',
             require  => File[$mountpoint],
             remounts => false,
+        }
+    }
+    else {
+        # manage some directories that the nfs server
+        # server would otherwise take care of for us
+        file { $managed_dumps_subdirs:
+            ensure => 'directory',
+            mode   => '0755',
+            owner  => $user,
+            group  => $group,
         }
     }
 }
