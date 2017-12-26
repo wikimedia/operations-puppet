8d3a267751421ce6cdd4dda074b11706fa1c5266
Daniel Zahn <dzahn@wikimedia.org>
2017-12-26 13:17:06 -0800

    microsites::peopleweb: convert role to profile
    Change-Id: Id4f0641bd382682ff3c5ae8d9a22efbfed0834a4

---
+++ modules/profile/manifests/microsites/peopleweb.pp
@@ -0,0 +1,22 @@
+# let users publish their own HTML in their home dirs
+class profile::microsites::peopleweb {
+
+    class { '::publichtml':
+        sitename     => 'people.wikimedia.org',
+        server_admin => 'noc@wikimedia.org',
+    }
+
+    ferm::service { 'people-http':
+        proto  => 'tcp',
+        port   => '80',
+        srange => '$CACHE_MISC',
+    }
+
+    motd::script { 'people-motd':
+        ensure  => present,
+        content => "#!/bin/sh\necho '\nThis is people.wikimedia.org.\nFiles you put in 'public_html' in your home dir will be accessible on the web.\nMore info on https://wikitech.wikimedia.org/wiki/People.wikimedia.org.\n'",
+    }
+
+    backup::set {'home': }
+}
+
