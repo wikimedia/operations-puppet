592cdd7859c20df3bbf22ae84e4fe9338838507a
Madhuvishy <mviswanathan@wikimedia.org>
2017-12-31 19:47:10 +0000

    Revert "dumps: Turn off dumps auto-sync to labstore1006 for reimaging"
    This reverts commit 1c9a4581f7cac5014ab41200eb11f4697f2ce68b.
    
    Change-Id: I418472b7aa162c5806428b3faca31651659b3e8d

--- modules/profile/manifests/dumps/generation/server/primary.pp
+++ modules/profile/manifests/dumps/generation/server/primary.pp
@@ -3,8 +3,8 @@ class profile::dumps::generation::server::primary {
 
     class { '::dumps::generation::server::rsyncer':
         xmldumpsdir    => $profile::dumps::generation::server::common::xmldumpsdir,
-        xmlremotedirs  => 'dumpsdata1002.eqiad.wmnet::data/xmldatadumps/public/,dataset1001.wikimedia.org::data/xmldatadumps/public/',
+        xmlremotedirs  => 'dumpsdata1002.eqiad.wmnet::data/xmldatadumps/public/,dataset1001.wikimedia.org::data/xmldatadumps/public/,labstore1006.wikimedia.org::data/xmldatadumps/public/',
         miscdumpsdir   => $profile::dumps::generation::server::common::miscdatasetsdir,
-        miscremotedirs => 'dumpsdata1002.eqiad.wmnet::data/otherdumps/,dataset1001.wikimedia.org::data/xmldatadumps/public/other/',
+        miscremotedirs => 'dumpsdata1002.eqiad.wmnet::data/otherdumps/,dataset1001.wikimedia.org::data/xmldatadumps/public/other/,labstore1006.wikimedia.org::data/xmldatadumps/public/other/',
     }
 }
