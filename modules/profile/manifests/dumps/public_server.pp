32b152617695bc6ac0f387e166e7a6e64879eff1
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2017-12-31 19:06:43 -0800

    dumps_public: Cleanup the public_server profile
    Remove profile left over from previous clean up
    
    Change-Id: I9710121210b86ae11844c1ecc6edebb10b997c47

--- modules/profile/manifests/dumps/public_server.pp
+++
@@ -1,9 +0,0 @@
-# Profile for Dumps server in the Public VLAN,
-# that serves dumps to Cloud VPS/Stat boxes via NFS,
-# or via web or rsync to mirrors
-
-class profile::dumps::public_server {
-    class { '::dumpsuser': }
-    class {'::public_dumps::server':}
-
-}
