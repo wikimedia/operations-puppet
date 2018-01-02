7787db9e5c635adc82fecf507740e171e77154a8
cpettet <rush@wikimedia.org>
2018-01-02 08:35:16 -0600

    rabbitmq: drain_queue is defined dupe
    Remove from cleanup manifest and use require
    
    Change-Id: I10cd7a4836b7844aaf37e3e01ec0b6c94f2fce96

--- modules/rabbitmq/manifests/cleanup.pp
+++ modules/rabbitmq/manifests/cleanup.pp
@@ -6,19 +6,13 @@ class rabbitmq::cleanup(
     $enabled=false,
     ) {
 
+    require rabbitmq
+
     if ($enabled) {
         $ensure = 'present'
     }
     else {
         $ensure = 'absent'
-    }
-
-    file { '/usr/local/sbin/drain_queue':
-        ensure => 'present',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0655',
-        source => 'puppet:///modules/rabbitmq/drain_queue',
     }
 
     # These logfiles will be rotated by an already-existing wildcard logrotate rule for rabbit
