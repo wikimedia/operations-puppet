5d185c3776d1f970618c8dad9b779f7f07d5ee57
Daniel Zahn <dzahn@wikimedia.org>
2018-01-02 12:01:31 -0800

    microsites: create research.wikimedia.org static page
    Create a static HTML landing page for the research team.
    
    Bug: T183916
    Change-Id: I96d74f563d518a1b31832eead4b51cb39f2534dd

---
+++ modules/profile/manifests/microsites/research.pp
@@ -0,0 +1,23 @@
+# https://research.wikimedia.org (T183916)
+class profile::microsites::research(
+  $server_name = hiera('profile::microsites::research::server_name'),
+  $server_admin = hiera('profile::microsites::research::server_admin'),
+) {
+
+    httpd::site { 'research.wikimedia.org':
+        content => template('profile/research/apache-research.wikimedia.org.erb'),
+    }
+
+    ensure_resource('file', '/srv/org', {'ensure' => 'directory' })
+    ensure_resource('file', '/srv/org/wikimedia', {'ensure' => 'directory' })
+    ensure_resource('file', '/srv/org/wikimedia/research', {'ensure' => 'directory' })
+
+    git::clone { 'research/landing-page':
+        ensure    => 'present',
+        source    => 'gerrit',
+        directory => '/srv/org/wikimedia/research',
+        branch    => 'master',
+    }
+
+}
+
