9c94591316970f98e5474453aacac50cb770423f
Emanuele Rocca <ema@wikimedia.org>
2018-01-02 17:19:12 +0100

    mtail::program: allow to specify destination directory
    Change-Id: Ie5543848b249f9a1d74baa560ee31a95de5e7961

--- modules/mtail/manifests/program.pp
+++ modules/mtail/manifests/program.pp
@@ -15,21 +15,35 @@
 #   The content of the file provided as a puppet:/// file reference.
 #   Either this or 'content' must be specified.
 #
+# [*destination*]
+#   The directory where the mtail script will be installed provided as a
+#   string. Defaults to '/etc/mtail'.
+#
 define mtail::program(
-    $ensure   = present,
-    $content  = undef,
-    $source   = undef,
+    $ensure      = present,
+    $content     = undef,
+    $source      = undef,
+    $destination = '/etc/mtail',
 ) {
     validate_ensure($ensure)
+    validate_absolute_path($destination)
 
     include ::mtail
 
     $basename = regsubst($title, '\W', '-', 'G')
-    $filename = "/etc/mtail/${basename}.mtail"
+    $filename = "${destination}/${basename}.mtail"
+
+    file { $destination:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
 
     file { $filename:
         ensure  => $ensure,
         content => $content,
         source  => $source,
+        require => File[$destination],
     }
 }
