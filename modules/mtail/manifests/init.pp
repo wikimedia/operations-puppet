e2cea7f4e8f2adbabcdaa4a50ab0bc2b27a1f46e
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-01-02 15:09:15 +0100

    mtail: stop sending metrics to graphite
    Prometheus is running production now, also lessen the load on Graphite when emitting high
    cardinality metrics.
    
    Change-Id: I7021322cce9af95cee975b81af2a49de57df8000

--- modules/mtail/manifests/init.pp
+++ modules/mtail/manifests/init.pp
@@ -10,23 +10,17 @@
 # [*port*]
 #   TCP port to listen to for Prometheus-style metrics
 #
-# [*graphite_hostport*]
-#   Also send metrics via graphite line-oriented protocol to this host:port.
-#
 # [*ensure*]
 #   Whether mtail should be running or stopped.
 
 class mtail (
   $logs = ['/var/log/syslog'],
   $port = '3903',
-  $graphite_hostport = 'graphite-in.eqiad.wmnet:2003',
-  $graphite_prefix = "mtail.${::hostname}.",
   $ensure = 'running',
   $group = 'root',
 ) {
     validate_array($logs)
     validate_re($port, '^[0-9]+$')
-    validate_string($graphite_hostport)
     validate_re($ensure, '^(running|stopped)$')
 
     require_package('mtail')
