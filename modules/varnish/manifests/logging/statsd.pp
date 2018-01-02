3518ac60751abb14617abad21f7723413a14e251
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-01-02 17:25:35 +0100

    mtail: add program to count varnish backend metrics
    Skip ttfb handling for now, the "+=" operation with floats is broken in the mtail version
    we're deploying. See also upstream issue https://github.com/google/mtail/issues/106
    
    Bug: T177199
    Change-Id: Ia2fa39674be38ef340a7785e8f10722bd04373bf

--- modules/varnish/manifests/logging/statsd.pp
+++ modules/varnish/manifests/logging/statsd.pp
@@ -60,4 +60,9 @@ define varnish::logging::statsd(
         description  => 'Varnish traffic logger - varnishstatsd',
         nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -a "/usr/local/bin/varnishstatsd" -u root',
     }
+
+    mtail::program { 'varnishbackend':
+        source      => 'puppet:///modules/mtail/programs/varnishbackend.mtail',
+        destination => '/etc/varnishmtail-backend',
+    }
 }
