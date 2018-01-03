ba7ac00e9f7e5948d9e7e4338bed6b36204521a1
elukey <ltoscano@wikimedia.org>
2018-01-03 15:05:06 +0100

    role::icinga: add prometheus alerts profile
    Change-Id: Ic955b3295f5d10472d47dcb11ffae082232dd694

---
+++ modules/profile/manifests/prometheus/alerts.pp
@@ -0,0 +1,21 @@
+# == Class: profile::prometheus::alerts
+#
+# Install icinga alerts based on Prometheus metrics.
+# NOTE to be included only from one host, icinga will generate different alerts
+# for all hosts that include this class.
+#
+class profile::prometheus::alerts {
+
+    # Monitor Druid realtime ingestion event rate.
+    # Experimental, only alerting the Analytics alias.
+    monitoring::check_prometheus { 'druid_realtime_banner_activity':
+        description     => 'Number of banner_activity realtime events received by Druid over a 30 minutes period',
+        query           => 'sum_over_time(druid_realtime_ingest_events_processed_count{cluster="druid_analytics", instance=~"druid.*:8000", datasource=~"banner_activity_minutely"}[30m])',
+        prometheus_url  => 'http://prometheus.svc.eqiad.wmnet/analytics',
+        method          => 'le',
+        warning         => 10,
+        critical        => 0,
+        contact_group   => 'analytics',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/prometheus-druid?refresh=1m&panelId=41&fullscreen&orgId=1']
+    }
+}
