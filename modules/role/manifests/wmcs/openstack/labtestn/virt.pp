11d7fd7791495781aa99866c347b635bc526ef69
cpettet <rush@wikimedia.org>
2018-01-03 09:01:44 -0600

    nova: add compute profiles for labtestn virt role
    Change-Id: I1970761282ce32530528a013988eb6000b01503f

--- modules/role/manifests/wmcs/openstack/labtestn/virt.pp
+++ modules/role/manifests/wmcs/openstack/labtestn/virt.pp
@@ -2,4 +2,6 @@ class role::wmcs::openstack::labtestn::virt {
     system::role { $name: }
     include ::standard
     include ::profile::openstack::labtestn::cloudrepo
+    include ::profile::openstack::labtestn::nova::common
+    include ::profile::openstack::labtestn::nova::compute::service
 }
