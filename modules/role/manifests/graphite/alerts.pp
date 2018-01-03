00323399ef72a1557e6a8f7229e2c6c4f9a5e004
elukey <ltoscano@wikimedia.org>
2018-01-03 15:05:06 +0100

    role::graphite::alerts: add Druid realtime ingestion monitoring
    Change-Id: Ic955b3295f5d10472d47dcb11ffae082232dd694

--- modules/role/manifests/graphite/alerts.pp
+++ modules/role/manifests/graphite/alerts.pp
@@ -103,4 +103,17 @@ class role::graphite::alerts {
         from            => '10min',
         percentage      => 50,
     }
+
+    # Monitor Druid realtime ingestion event rate.
+    # Experimental, only alerting the Analytics alias.
+    monitoring::check_prometheus { 'druid_realtime_banner_activity':
+        description     => 'Number of banner_activity realtime events received by Druid over a 30 minutes period',
+        query           => 'sum_over_time(druid_realtime_ingest_events_processed_count{cluster="druid_analytics", instance=~"druid.*:8000", datasource=~"banner_activity_minutely"}[30m])',
+        prometheus_url  => "http://prometheus.svc.${::site}.wmnet/analytics",
+        method          => 'le'
+        warning         => 10,
+        critical        => 0,
+        contact_group   => 'analytics',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/prometheus-druid?refresh=1m&panelId=41&fullscreen&orgId=1']
+    }
 }
