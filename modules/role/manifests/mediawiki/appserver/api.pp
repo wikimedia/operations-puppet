2db4b19f6602948445a5ae6bae9a484a6492c459
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-01-03 11:49:41 +0100

    mediawiki::appserver::api: add load monitoring
    We've had quite a few cases of HHVM API appservers with high cpu usage
    causing noitceable latencies to users; the easiest way to detect such
    deadlocks is quite simply checking the machine CPU usage/load - at least
    that's what I do manually.
    
    This change won't solve the issue per-se, but it will make ops aware of
    what is going on proactively.
    
    Bug: T182568
    Bug: T184048
    Change-Id: I06af45cbf8f42ade5753dc7397c6e1aa2b32c4ea

--- modules/role/manifests/mediawiki/appserver/api.pp
+++ modules/role/manifests/mediawiki/appserver/api.pp
@@ -5,12 +5,5 @@ class role::mediawiki::appserver::api {
     include ::profile::base::firewall
     include ::profile::prometheus::apache_exporter
     include ::profile::prometheus::hhvm_exporter
-
-    # Using fastcgi we need more local ports
-    sysctl::parameters { 'raise_port_range':
-        values   => {
-            'net.ipv4.local_port_range' => '22500 65535',
-        },
-        priority => 90,
-    }
+    include ::profile::mediawiki::api
 }
