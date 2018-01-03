edb39eb84b4ccb3a9b8d61f8d640408bd153a098
Marko Obrovac <mobrovac@wikimedia.org>
2018-01-03 20:38:10 +0100

    RESTBase: Set up RESTBase for the production_ng role as well
    During the migration from Cassandra 2 to Cassandra 3 we chose not to set
    up RESTBase on the new nodes, but now the time has come to transition
    RESTBase to them as well.
    
    Bug: T184110
    Change-Id: Ie2484c2175e56ed40e5108b6ec41adbccfa19bfa

--- modules/role/manifests/restbase/production_ng.pp
+++ modules/role/manifests/restbase/production_ng.pp
@@ -2,9 +2,7 @@
 #
 # Configures the production cluster (next-gen)
 class role::restbase::production_ng {
-    include ::passwords::cassandra
-    include ::base::firewall
-    include ::standard
-    include ::profile::cassandra
-    system::role { 'restbase': description => 'Restbase (Cassandra 3.x-only)' }
+    include ::role::restbase::base
+    include ::role::lvs::realserver
+    system::role { 'restbase': description => 'Restbase (production with Cassandra 3.x)' }
 }
