e57ef02e5fe5449573deecaa03cf69aacdda2d7f
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-01-03 11:49:41 +0100

    mediawiki::appserver::api: add load monitoring
    We've had quite a few cases of HHVM API appservers with high cpu usage
    causing noitceable latencies to users; the easiest way to detect such
    deadlocks is quite simply checking the machine CPU usage/load - at least
    that's what I do manually.
    
    This change won't solve the issue per-se, but it will make ops aware of
    what is going on proactively.
    
    Bug: T182568, T184048
    Change-Id: I06af45cbf8f42ade5753dc7397c6e1aa2b32c4ea

--- modules/role/manifests/mediawiki/appserver/api.pp
+++ modules/role/manifests/mediawiki/appserver/api.pp
@@ -13,4 +13,15 @@ class role::mediawiki::appserver::api {
         },
         priority => 90,
     }
+
+    # Check the load to detect clearly hosts hanging (see T184048, T182568)
+    $nproc = $facts['processorcount']
+    $warning = join([ $nproc * 0.95, $nproc * 0.8, $nproc * 0.75], ',')
+    $critical = join([ $nproc * 1.5, $nproc * 1.1, $nproc * 1], ',')
+    # Since we're checking the load, that is already a moving average, we can
+    # alarm at the first occurrence
+    nrpe::monitor_service { 'cpu_load':
+        command => "check_load -w ${warning} -c ${critical}",
+        retries => 1,
+    }
 }
