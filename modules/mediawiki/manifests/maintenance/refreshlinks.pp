5b0c9895da94dc2f476db7203852cdb2b0f57454
Jaime Crespo <jcrespo@wikimedia.org>
2018-01-04 14:39:17 +0100

    mediawiki-maintenance: Run maintenance on new s8 replica set, too
    Are the days of the month ok?
    
    Bug: T184179
    Change-Id: I4bcabc7a72c406211ab7cbe11a0127aab7a1efb3

--- modules/mediawiki/manifests/maintenance/refreshlinks.pp
+++ modules/mediawiki/manifests/maintenance/refreshlinks.pp
@@ -11,5 +11,5 @@ class mediawiki::maintenance::refreshlinks( $ensure = present ) {
     }
 
     # add cron jobs - usage: <cluster>@<day of month> (these are just needed monthly)
-    mediawiki::maintenance::refreshlinks::cronjob { ['s1@1', 's2@2', 's3@3', 's4@4', 's5@5', 's6@6', 's7@7', 'silver@8']: }
+    mediawiki::maintenance::refreshlinks::cronjob { ['s1@1', 's2@2', 's3@3', 's4@4', 's5@5', 's6@6', 's7@7', 's8@8', 'silver@9']: }
 }
--- modules/mediawiki/manifests/maintenance/updatequerypages.pp
+++ modules/mediawiki/manifests/maintenance/updatequerypages.pp
@@ -9,6 +9,6 @@ class mediawiki::maintenance::updatequerypages( $ensure = present ) {
     }
 
     # add cron jobs - usage: <cluster>@<day of month> (monthday currently unused, only sets cronjob name)
-    mediawiki::maintenance::updatequerypages::cronjob { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 'silver@18']: }
+    mediawiki::maintenance::updatequerypages::cronjob { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18', 'silver@19']: }
     mediawiki::maintenance::updatequerypages::enwiki::cronjob { ['updatequerypages-enwiki-only']: }
 }
