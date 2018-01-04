daef6398708a7397138dcd3f798b014476b7c143
elukey <ltoscano@wikimedia.org>
2018-01-04 15:25:43 +0100

    profile::hadoop: set hiera defaults to ease labs deployments
    Bug: T167790
    Change-Id: Ifb5bbce5d6919ac220f9004fc7d24f52c4ef8912

--- modules/profile/manifests/hadoop/master.pp
+++ modules/profile/manifests/hadoop/master.pp
@@ -4,12 +4,15 @@
 #
 # == Parameters
 #
+#  [*hadoop_namenode_heapsize*]
+#    Current JVM heap size to use as threshold for monitoring.
+#
 #  [*monitoring_enabled*]
 #    If production monitoring needs to be enabled or not.
 #
 class profile::hadoop::master(
-    $monitoring_enabled       = hiera('profile::hadoop::master::monitoring_enabled'),
-    $hadoop_namenode_heapsize = hiera('profile::hadoop::master::namenode_heapsize'),
+    $monitoring_enabled       = hiera('profile::hadoop::master::monitoring_enabled', false),
+    $hadoop_namenode_heapsize = hiera('profile::hadoop::master::namenode_heapsize', 2048),
     $statsd                   = hiera('statsd'),
 ){
 
