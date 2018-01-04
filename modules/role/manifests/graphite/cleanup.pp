d0e52753adab8d000c8c77600a61d726cc69238b
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-01-04 10:17:06 +0100

    graphite: cleanup stale ORES metrics
    Also group cleanups into a separate class.
    
    Bug: T169969
    Change-Id: I01b37944e614a3e2e5a310094282381b67ade6dc

---
+++ modules/role/manifests/graphite/cleanup.pp
@@ -0,0 +1,41 @@
+class role::graphite::cleanup (
+  $storage_dir,
+) {
+    # Cleanup stale labs instances data - T143405
+    graphite::whisper_cleanup { 'graphite-labs-instances':
+        directory => "${storage_dir}/whisper/instances",
+    }
+
+    # Cleanup eventstreams rdkafka stale data - T160644
+    graphite::whisper_cleanup { 'graphite-eventstreams':
+        directory => "${storage_dir}/whisper/eventstreams/rdkafka",
+        keep_days => 5,
+    }
+
+    # Cleanup zuul data
+    graphite::whisper_cleanup { 'graphite-zuul':
+        directory => "${storage_dir}/whisper/zuul",
+    }
+    # Zuul also generates metrics related to Gerrit
+    graphite::whisper_cleanup { 'graphite-zuul-gerrit':
+        directory => "${storage_dir}/whisper/gerrit",
+    }
+
+    # Nodepool, which has several metrics for each of the Jenkins jobs
+    graphite::whisper_cleanup { 'graphite-nodepool':
+        directory => "${storage_dir}/whisper/nodepool",
+        keep_days => 15,
+    }
+
+    # Cassandra metrics - T179057
+    graphite::whisper_cleanup { 'graphite-cassandra':
+        directory => "${storage_dir}/whisper/cassandra",
+        keep_days => 182,
+    }
+
+    # ORES metrics - T169969
+    graphite::whisper_cleanup { 'graphite-ores':
+        directory => "${storage_dir}/whisper/ores",
+        keep_days => 30,
+    }
+}
--- modules/role/manifests/graphite/production.pp
+++ modules/role/manifests/graphite/production.pp
@@ -32,36 +32,8 @@ class role::graphite::production {
         }
     }
 
-    # Cleanup stale labs instances data - T143405
-    graphite::whisper_cleanup { 'graphite-labs-instances':
-        directory => "${storage_dir}/whisper/instances",
-    }
-
-    # Cleanup eventstreams rdkafka stale data - T160644
-    graphite::whisper_cleanup { 'graphite-eventstreams':
-        directory => "${storage_dir}/whisper/eventstreams/rdkafka",
-        keep_days => 5,
-    }
-
-    # Cleanup zuul data
-    graphite::whisper_cleanup { 'graphite-zuul':
-        directory => "${storage_dir}/whisper/zuul",
-    }
-    # Zuul also generates metrics related to Gerrit
-    graphite::whisper_cleanup { 'graphite-zuul-gerrit':
-        directory => "${storage_dir}/whisper/gerrit",
-    }
-
-    # Nodepool, which has several metrics for each of the Jenkins jobs
-    graphite::whisper_cleanup { 'graphite-nodepool':
-        directory => "${storage_dir}/whisper/nodepool",
-        keep_days => 15,
-    }
-
-    # Cassandra metrics - T179057
-    graphite::whisper_cleanup { 'graphite-cassandra':
-        directory => "${storage_dir}/whisper/cassandra",
-        keep_days => 182,
+    class { 'role::graphite::cleanup':
+        storage_dir => $storage_dir,
     }
 
     $graphite_hosts = [
