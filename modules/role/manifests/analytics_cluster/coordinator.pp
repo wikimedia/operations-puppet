a1582cf029356d2c94311972264103482ae96dbf
elukey <ltoscano@wikimedia.org>
2018-01-04 15:25:43 +0100

    profile::hadoop: set hiera defaults to ease labs deployments
    Bug: T167790
    Change-Id: Ifb5bbce5d6919ac220f9004fc7d24f52c4ef8912

--- modules/role/manifests/analytics_cluster/coordinator.pp
+++ modules/role/manifests/analytics_cluster/coordinator.pp
@@ -58,6 +58,6 @@ class role::analytics_cluster::coordinator {
     include ::role::analytics_cluster::refinery::job::sqoop_mediawiki
     include ::role::analytics_cluster::refinery::job::json_refine
 
-    class { '::standard': }
+    include ::standard
     include ::profile::base::firewall
 }
