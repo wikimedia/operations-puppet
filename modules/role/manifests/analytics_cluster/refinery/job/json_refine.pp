93ba85248a935cdc7b1677ace6d3a11f592e98f5
Ottomata <aotto@wikimedia.org>
2018-01-04 10:27:20 -0500

    Refine mediawiki job queue events into Hive event database
    Change-Id: I279aa9046d4a632183894d9d21893307962d4621

--- modules/role/manifests/analytics_cluster/refinery/job/json_refine.pp
+++ modules/role/manifests/analytics_cluster/refinery/job/json_refine.pp
@@ -30,4 +30,46 @@ class role::analytics_cluster::refinery::job::json_refine {
         table_blacklist  => '^mediawiki_page_properties_change|mediawiki_recentchange$',
         minute           => 20,
     }
+
+    # Refine Mediawiki job queue events (from EventBus).
+    # This could be combined into the same EventBus refine job above, but it is nice to
+    # have them separated, as the job queue schemas are legacy and can be problematic.
+
+    # $problematic_jobs will not be refined.
+    # These have inconsistent schemas that cause refinement to fail.
+    $problematic_jobs = [
+        'EchoNotificationJob',
+        'EchoNotificationDeleteJob',
+        'TranslationsUpdateJob',
+        'MessageGroupStatesUpdaterJob',
+        'InjectRCRecords',
+        'cirrusSearchDeleteArchive',
+        'enqueue',
+        'htmlCacheUpdate',
+        'LocalRenameUserJob',
+        'RecordLintJob',
+        'wikibase_addUsagesForPage',
+        'refreshLinks',
+        'cirrusSearchCheckerJob',
+        'MassMessageSubmitJob',
+        'refreshLinksPrioritized',
+        'TranslatablePageMoveJob',
+        'ORESFetchScoreJob',
+        'PublishStashedFile',
+        'CentralAuthCreateLocalAccountJob',
+        'gwtoolsetUploadMediafileJob',
+    ]
+    $table_blacklist = sprintf('.*(%s)$', join($problematic_jobs, '|'))
+
+    role::analytics_cluster::refinery::job::json_refine_job { 'eventlogging_eventbus_job_queue':
+        # This is imported by camus_job { 'mediawiki_job': }
+        input_base_path  => '/wmf/data/raw/mediawiki_job',
+        # 'datacenter' is extracted from the input path into a Hive table partition
+        input_regex      => '.*(eqiad|codfw)_(.+)/hourly/(\\d+)/(\\d+)/(\\d+)/(\\d+)',
+        input_capture    => 'datacenter,table,year,month,day,hour',
+        output_base_path => '/wmf/data/event',
+        output_database  => 'event',
+        table_blacklist  => $table_blacklist,
+        minute           => 25,
+    }
 }
