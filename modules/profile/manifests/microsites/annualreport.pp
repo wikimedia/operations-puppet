f636cb3a2a9d1924277f319fe51f3fc57d8cc0c1
Daniel Zahn <dzahn@wikimedia.org>
2018-01-04 19:41:58 -0800

    webserver_misc_static: switch apache to module httpd
    Now that we have the new module httpd we can convert
    this host with multiple Apache sites on a single node as well.
    
    Using one "base" profile for the httpd declaration and ferm rule
    to avoid duplicate declarations.
    
    Also we can save code lines by removing a ferm section in each
    class for each site.
    
    Change-Id: I0e48f233d1c5ca3e43d259120fc66bc57bbb9f9d

--- modules/profile/manifests/microsites/annualreport.pp
+++ modules/profile/manifests/microsites/annualreport.pp
@@ -4,20 +4,11 @@
 # T599 - https://15.wikipedia.org (aka. annual report 2015)
 class profile::microsites::annualreport {
 
-    include ::apache
-    include ::apache::mod::headers
-
-    ferm::service { 'annualreport_http':
-        proto  => 'tcp',
-        port   => '80',
-        srange => '$CACHE_MISC',
-    }
-
-    apache::site { 'annual.wikimedia.org':
+    httpd::site { 'annual.wikimedia.org':
         source => 'puppet:///modules/profile/annualreport/annual.wikimedia.org',
     }
 
-    apache::site { '15.wikipedia.org':
+    httpd::site { '15.wikipedia.org':
         source => 'puppet:///modules/profile/annualreport/15.wikipedia.org',
     }
 
--- modules/profile/manifests/microsites/static_bugzilla.pp
+++ modules/profile/manifests/microsites/static_bugzilla.pp
@@ -3,12 +3,6 @@ class profile::microsites::static_bugzilla {
 
     include ::bugzilla_static
 
-    ferm::service { 'bugzilla_static_http':
-        proto  => 'tcp',
-        port   => '80',
-        srange => '$CACHE_MISC',
-    }
-
     include ::profile::backup::host
     backup::set { 'bugzilla-static' : }
     backup::set { 'bugzilla-backup' : }
