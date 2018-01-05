d9388d6c8be9a1e879a3dfea232a87b7aada8508
Chad Horohoe <chadh@wikimedia.org>
2018-01-05 13:10:15 -0800

    Releases: Include all contint PHP packages
    Right now I'm missing php-mbstring, but continuing to
    install these one by one is just a game of whack-a-mole.
    
    Install all of them.
    
    Change-Id: Id777364fa468d8eec73320a539056fe3f834a49f

--- modules/profile/manifests/releases/mediawiki.pp
+++ modules/profile/manifests/releases/mediawiki.pp
@@ -24,6 +24,7 @@ class profile::releases::mediawiki (
     }
 
     class { '::contint::composer': }
+    class { '::contint::packages::php': }
 
     class { '::apache::mod::rewrite': }
     class { '::apache::mod::headers': }
