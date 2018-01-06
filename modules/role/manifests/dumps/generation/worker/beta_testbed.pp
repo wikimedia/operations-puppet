28048b3629fbfd29c2c3ffb6bbcdea019a5e7c1f
Ariel T. Glenn <ariel@wikimedia.org>
2018-01-06 13:31:07 +0200

    snapshot testbed role for deployment-prep
    One caveat: it requires application of a second role,
    role::beta::mediawiki, as do all instances in eployment-prep
    that have mediawiki scapped to them.
    
    Change-Id: I513f4edaa7decebe01702b4f587860952c2b1d81

---
+++ modules/role/manifests/dumps/generation/worker/beta_testbed.pp
@@ -0,0 +1,23 @@
+# == Class role::dumps::generation::worker::beta_testbed
+#
+# Set up a dumps snapshot instance as a testbed.
+#
+# Because this class uses mediawiki, the class
+# role::beta::mediawiki must be applied to the instance
+# as well.  The role can't be included in this class for
+# style reasons.
+#
+# You should be using at least a m1.medium instance,
+# giving some extra lvm space locally mounted on
+# which to write the dumps.
+#
+# filtertags: labs-project-deployment-prep
+class role::dumps::generation::worker::beta_testbed {
+    include standard
+
+    include profile::dumps::generation::worker::common
+
+    system::role { 'dumps::generation::worker::beta_testbed':
+        description => 'beta testbed for dumps of XML/SQL wiki content',
+    }
+}
