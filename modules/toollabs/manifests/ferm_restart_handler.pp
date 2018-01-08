459f801b1899283d25dfa02f7d4513f62b0353cf
cpettet <rush@wikimedia.org>
2018-01-08 16:54:15 -0600

    WIP: toolforge: ferm hook to restart components post updates
    * Ferm is not playing nice with other iptables tenants
    * Tested an /etc/ferm/conf.d/00_hooks to see it run external
      scripts in what seems like a totally post updates state.  This
      hopefully let's kube-proxy, flannel, and docker deal with
      Ferm stomping all around.
    
    This is a midterm fix where other options are being explored
    in the context of the task.  Right now any update to Ferm, even
    a definition MAC, results in an outage for k8s in Toolforge.
    
    Bug: T182722
    Change-Id: I5c700a2c8bce6050e8cb761450d3716a6b3f33c9

---
+++ modules/toollabs/manifests/ferm_restart_handler.pp
@@ -0,0 +1,20 @@
+# tldr; hook post ferm updates to let other interested
+#       parties resync their iptables state.
+# See: T182722
+class toollabs::ferm_restart_handler{
+
+    file {'/usr/local/sbin/ferm_restart_handler':
+        source => 'puppet:///modules/toollabs/ferm_restart_handler.sh',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+
+    # http://ferm.foo-projects.org/download/2.1/ferm.html#hooks
+    # https://phabricator.wikimedia.org/T182722
+    ferm::conf{'ferm_firewall_processing':
+        prio      => 00,
+        content   => '@hook post  "/usr/local/sbin/ferm_restart_handler"',
+        subscribe => File['/usr/local/sbin/ferm_restart_handler'], 
+    }
+}
