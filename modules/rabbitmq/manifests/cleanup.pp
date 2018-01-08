683ad5ab666816dbab2bc4fb16ddb70fe5331cb5
cpettet <rush@wikimedia.org>
2018-01-08 13:38:56 -0600

    rabbitmq: handling users and initial setup
    A few inconsistencies and issues across our stuff:
    
    * labtestcontrol2001 did not have the management
      plugin enabled which made the cleanup cron invalid
    * the queuecleanup script did not handle a queue
      not being present yet and they are created dynamically
      (this is not by necessarily an errant state)
    * queuecleanup script had hard coded credentials
    * There has been no way to manage rabbitmq users and
      this has largely been done adhoc and outside of
      Puppets purview resulting in inconsistencies
    * Rabbitmq can run on the non-primary control node
      and really needs to in order to manage rabbit
      internals as rabbit has a local user database
      and settings.
    * guest account had been left in use on installs
    * header docs for rabbit/init.pp were confusing
    * confirmed queue cleanup requires admin tag
    
    Change-Id: I1870faa07e49603bd7eff9f38cc1202519aeb467

--- modules/rabbitmq/manifests/cleanup.pp
+++ modules/rabbitmq/manifests/cleanup.pp
@@ -2,24 +2,48 @@
 # that are not being consumed and this becomes costly
 # over time.
 
+# Enabled is only for active cron, all other
+# components are setup if the class is included.
+
 class rabbitmq::cleanup(
+    $password,
     $enabled=false,
+    $username='drainqueue',
     ) {
 
     require rabbitmq
 
+    if ! ($password) {
+        fail("password must be set for ${name}")
+    }
+
+    file { '/usr/local/sbin/drain_queue':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0655',
+        source => 'puppet:///modules/rabbitmq/drain_queue',
+    }
+
+    # admin is needed for queue cleanup
+    rabbitmq::user{'drainqueue':
+        username      => $username,
+        password      => $password,
+        administrator => true,
+    }
+
     if ($enabled) {
-        $ensure = 'present'
+        $cron_ensure = 'present'
     }
     else {
-        $ensure = 'absent'
+        $cron_ensure = 'absent'
     }
 
     # These logfiles will be rotated by an already-existing wildcard logrotate rule for rabbit
     cron { 'drain and log rabbit notifications.error queue':
-            ensure  => $ensure,
+            ensure  => $cron_ensure,
             user    => 'root',
             minute  => '35',
-            command => '/usr/local/sbin/drain_queue notifications.error >> /var/log/rabbitmq/notifications_error.log 2>&1',
+            command => "/usr/local/sbin/drain_queue notifications.error --password ${password} >> /var/log/rabbitmq/notifications_error.log 2>&1",
     }
 }
