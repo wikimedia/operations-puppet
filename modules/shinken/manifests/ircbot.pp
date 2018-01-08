8fb7b4edff644ea37d6af9ec269933d2735b2e5d
andrewbogott <abogott@wikimedia.org>
2018-01-08 14:48:26 -0600

    shinken: remove references to 'wmt' project
    Because it doesn't exist anymore.
    
    Bug: T184449
    Change-Id: Idd5f83d3eba5933f57b3efd2e116449ca0dad157

--- modules/shinken/manifests/ircbot.pp
+++ modules/shinken/manifests/ircbot.pp
@@ -29,7 +29,6 @@ class shinken::ircbot(
         '/var/log/ircecho/irc-cloud.log'          => '#wikimedia-cloud',
         '/var/log/ircecho/irc-cloud-feed.log'     => '#wikimedia-cloud-feed',
         '/var/log/ircecho/irc-cvn.log'            => '#countervandalism',
-        '/var/log/ircecho/irc-wmt.log'            => '##wmt',
         '/var/log/ircecho/irc-ores.log'           => '#wikimedia-ai',
     }
 
