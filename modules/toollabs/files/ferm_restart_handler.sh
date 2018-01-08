0622691c1d5082180194e5408c6d705816869451
cpettet <rush@wikimedia.org>
2018-01-08 16:54:15 -0600

    toolforge: ferm hook to restart components post updates
    http://ferm.foo-projects.org/download/2.1/ferm.html#hooks
    
    * Ferm is not playing nice with other iptables tenants
    * Tested an /etc/ferm/conf.d/00_hooks to see it run external
      scripts in what seems like a totally post updates state.  This
      hopefully allows kube-proxy, flannel, and docker to deal with
      Ferm stomping all around.
    
    This is a midterm fix where other options are being explored
    in the context of the task.  Right now any update to Ferm, even
    a definition MAC, results in an outage for k8s in Toolforge.
    
    Note - the task includes restarting kubelet in the service handling
    post ferm update to mitigate.  This seems not to be necessary
    so is excluded to reduce to necessary scope.
    
    Bug: T182722
    Change-Id: I5c700a2c8bce6050e8cb761450d3716a6b3f33c9

---
+++ modules/toollabs/files/ferm_restart_handler.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+/usr/bin/logger -i -t ${0} "restart firewall components post ferm management"
+
+# Ferm expects to handle all firewall state
+# and that does not mesh well with dynamic chain management.
+# We tell the k8s stack here to restart
+#
+# This should be no more invasive than a rescheduling
+# of a POD to another worker.
+#
+# If we are living in an nftables world when you read
+# this, then this should be totally rethought.
+service docker restart
+service flannel restart
+service kube-proxy restart
