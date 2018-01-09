ed3283a076703f3f1affdccad20457b8821e9ecc
Marko Obrovac <mobrovac@wikimedia.org>
2018-01-09 11:12:53 +0100

    RESTBase: Do not manage Cassandra 2 in the legacy cluster
    The legacy cluster is going to be decommissioned soon, so we don't need
    to manage Cassandra on these nodes any longer. In contrast, we want to
    keep RESTBase running on them until each of the nodes is reimaged.
    Hence, remove profile::cassandra from the production role, but keep
    profile::restbase.
    
    Bug: T184100
    Change-Id: If295d2f3192ac57c31819b3bb077bb2799eb0d36

--- modules/role/manifests/restbase/production.pp
+++ modules/role/manifests/restbase/production.pp
@@ -3,7 +3,10 @@
 # Configures the restbase dev cluster
 class role::restbase::production {
     # Just includes base, no LVS etc.
-    include ::role::restbase::base
+    include ::passwords::cassandra
+    include ::base::firewall
+    include ::standard
+    include ::profile::restbase
     include ::role::lvs::realserver
-    system::role { 'restbase': description => 'Restbase (production)' }
+    system::role { 'restbase': description => 'Restbase (production, Cass 2.2.x)' }
 }
