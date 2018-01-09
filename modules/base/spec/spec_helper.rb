f0bd587b7015f42576b7ebf585d278792861d058
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-01-09 16:51:17 +0100

    base: fix spec tests for puppet 4
    Change-Id: I70175736de9cbf44ddb44b5c373b7467e6626fd6

--- modules/base/spec/spec_helper.rb
+++ modules/base/spec/spec_helper.rb
@@ -1,9 +1,12 @@
 require 'rspec-puppet'
+require 'rspec-puppet-facts'
 
+include RspecPuppetFacts
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
   c.module_path = File.expand_path("../..", File.dirname(__FILE__))
   c.manifest_dir = File.join(fixture_path, 'manifests')
   c.template_dir = File.join(fixture_path, 'templates')
+  c.hiera_config = File.join(fixture_path, 'hiera.yaml')
 end
