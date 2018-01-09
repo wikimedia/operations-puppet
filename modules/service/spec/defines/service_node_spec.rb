381672c140c0fcce7cdd77bcae59d2dbfde77d02
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-01-09 17:02:53 +0100

    service: properly fix specs for puppet 4
    Change-Id: I2f206c4719d94176eb2291917411bf8f14c9104b

--- modules/service/spec/defines/service_node_spec.rb
+++ modules/service/spec/defines/service_node_spec.rb
@@ -1,26 +1,39 @@
 require 'spec_helper'
+test_on = {
+  supported_os: [
+    {
+      'operatingsystem'        => 'Debian',
+      'operatingsystemrelease' => ['8', '9'],
+    }
+  ]
+}
 
 describe 'service::node', :type => :define do
-  let(:pre_condition) {
-    ['class passwords::etcd { $accounts = {}}',
-     'include ::passwords::etcd',
-     'class profile::base {
-      $notifications_enabled = "1"
-}
-include ::profile::base'
-    ]
-  }
-  let(:title) { 'my_service_name' }
-  let(:facts) { { :initsystem => 'systemd' } }
-  let(:node_params) { {'cluster' => 'test', 'site' => 'eqiad', 'realm' => 'production'} }
-  context 'when only port is given' do
-    let(:params) { { :port => 1234 } }
+  on_supported_os(test_on).each do |os, facts|
+    context "On #{os}" do
+      let(:facts) do
+        facts.merge({initsystem: 'systemd'})
+      end
 
-    it { is_expected.to compile }
+      let(:pre_condition) {
+        ['class passwords::etcd { $accounts = {}}',
+         'include ::passwords::etcd',
+         'class profile::base { $notifications_enabled = "1"}',
+         'include ::profile::base'
+        ]
+      }
+      let(:title) { 'my_service_name' }
+      let(:node_params) { {'cluster' => 'test', 'site' => 'eqiad', 'realm' => 'production'} }
+      context 'when only port is given' do
+        let(:params) { { :port => 1234 } }
 
-    it 'create the appropriate scap target' do
-      is_expected.to contain_scap__target('my_service_name/deploy')
-                       .with_service_name('my_service_name')
+        it { is_expected.to compile }
+
+        it 'create the appropriate scap target' do
+          is_expected.to contain_scap__target('my_service_name/deploy')
+                           .with_service_name('my_service_name')
+        end
+      end
     end
   end
 end
--- modules/service/spec/spec_helper.rb
+++ modules/service/spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-facts'
 
+include RspecPuppetFacts
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
