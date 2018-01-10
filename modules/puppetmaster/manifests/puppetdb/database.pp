41f5af4e499228c42146e80e604315a3603895ca
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-01-10 12:57:17 +0100

    puppetdb: refactor to role/profile
    * Split the content of the puppetdb role in two profiles: one for the
      puppetdb application, one for the corresponding postgresql database,
      that can be used separately
    * Rework puppetmaster::puppetdb::database, that was a mess.
    * Move monitoring to the profile for the database, move db tuning to the
      module class itself
    * Added type checking to the parameters of puppetmaster::puppetdb::database
    
    Change-Id: I93b02c19398e4fe16818910dfd3ed6f0653aa3a6

--- modules/puppetmaster/manifests/puppetdb/database.pp
+++ modules/puppetmaster/manifests/puppetdb/database.pp
@@ -1,21 +1,53 @@
-# Class puppetmaster::puppetdb::database
+# == Class puppetmaster::puppetdb::database
 #
 # Sets up the postgresql database
-class puppetmaster::puppetdb::database($master) {
-    $replication_pass = hiera('puppetdb::password::replication')
-    $puppetdb_pass = hiera('puppetdb::password::rw')
+#
+# === Parameters
+# [*master*] is the master server fqdn
+#
+# [*pgversion*] The postgresql version.
+#
+# [*shared_buffers*] The size of the postgresql shared buffer to use
+#
+# [*replication_pass*] The replication password
+#
+# [*puppetdb_pass*] Password for the puppetdb user,
+#
+# [*puppetdb_users*] Hash of users to create (if any), additionally to the local ones
+#
+class puppetmaster::puppetdb::database(
+    String $master,
+    Enum['9.4', '9.6'] $pgversion,
+    String $shared_buffers,
+    String $replication_pass,
+    String $puppetdb_pass,
+    Hash $puppetdb_users={},
+) {
+    # Tuning
+    file { "/etc/postgresql/${pgversion}/main/tuning.conf":
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('puppetmaster/puppetdb/tuning.conf.erb'),
+    }
 
-    if $master == $::fqdn {
-        # We do this for the require in postgres::db
-        $require_class = 'postgresql::master'
+    sysctl::parameters { 'postgres_shmem':
+        values => {
+            # That is derived after tuning postgresql, deriving automatically is
+            # not the safest idea yet.
+            'kernel.shmmax' => 8388608000,
+        },
+    }
+
+    $on_master = ($master == $::fqdn)
+    if $on_master {
         class { '::postgresql::master':
             includes => ['tuning.conf'],
             root_dir => '/srv/postgres',
             use_ssl  => true,
         }
-        $on_master = true
     } else {
-        $require_class = 'postgresql::slave'
         class { '::postgresql::slave':
             includes         => ['tuning.conf'],
             master_server    => $master,
@@ -23,20 +55,13 @@ class puppetmaster::puppetdb::database($master) {
             replication_pass => $replication_pass,
             use_ssl          => true,
         }
-        $on_master = false
-    }
-    class { 'prometheus::postgres_exporter': # lint:ignore:wmf_styleguide
-        require => Class[$require_class],
     }
     # Postgres replication and users
-    $postgres_users = hiera('puppetmaster::puppetdb::postgres_users', undef)
-    if $postgres_users {
-        $postgres_users_defaults = {
-            pgversion => 9.4,
-            master    => $on_master,
+    $puppetdb_users.each |$pg_name, $config| {
+        $actual_config = merge($config, {'master' => $on_master, 'pgversion' => $pgversion})
+        postgresql::user { $pg_name:
+            * => $config
         }
-        create_resources(postgresql::user, $postgres_users,
-            $postgres_users_defaults)
     }
     # Create the puppetdb user for localhost
     # This works on every server and is used for read-only db lookups
@@ -46,7 +71,7 @@ class puppetmaster::puppetdb::database($master) {
         database  => 'puppetdb',
         password  => $puppetdb_pass,
         cidr      => "${::ipaddress}/32",
-        pgversion => '9.4',
+        pgversion => $pgversion,
         master    => $on_master,
     }
 
@@ -60,7 +85,6 @@ class puppetmaster::puppetdb::database($master) {
     # Create the database
     postgresql::db { 'puppetdb':
         owner   => 'puppetdb',
-        require => Class[$require_class],
     }
 
     exec { 'create_tgrm_extension':
@@ -69,5 +93,4 @@ class puppetmaster::puppetdb::database($master) {
         user    => 'postgres',
         require => Postgresql::Db['puppetdb'],
     }
-
 }
