18c081c18d712e6f722d1a6ce6316cdab6057c1c
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-01-10 17:29:15 +0100

    base::resolving: remove useless "else" clause
    Change-Id: I3ca4358f02835e89864ab15b550e9346fdc3f0b5

--- modules/base/manifests/resolving.pp
+++ modules/base/manifests/resolving.pp
@@ -5,44 +5,43 @@ class base::resolving (
     if ! $::nameservers {
         fail('Variable $::nameservers is not defined!')
     }
-    else {
-        if $::realm == 'labs' {
-            $labs_tld = hiera('labs_tld')
-            # Thanks to dhcp, resolvconf is constantly messing with our resolv.conf.  Disable it.
-            file { '/sbin/resolvconf':
-                owner  => 'root',
-                group  => 'root',
-                mode   => '0555',
-                source => 'puppet:///modules/base/resolv/resolvconf.dummy',
-            }
 
-            file { '/etc/dhcp/dhclient-enter-hooks.d':
-                ensure => 'directory',
-            }
+    if $::realm == 'labs' {
+        $labs_tld = hiera('labs_tld')
+        # Thanks to dhcp, resolvconf is constantly messing with our resolv.conf.  Disable it.
+        file { '/sbin/resolvconf':
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            source => 'puppet:///modules/base/resolv/resolvconf.dummy',
+        }
 
-            # also stop dhclient from updating resolv.conf.
-            file { '/etc/dhcp/dhclient-enter-hooks.d/nodnsupdate':
-                owner   => 'root',
-                group   => 'root',
-                mode    => '0555',
-                source  => 'puppet:///modules/base/resolv/nodnsupdate',
-                require => File['/etc/dhcp/dhclient-enter-hooks.d'],
-            }
+        file { '/etc/dhcp/dhclient-enter-hooks.d':
+            ensure => 'directory',
+        }
 
-            # Now, finally, we can just puppetize the damn file
-            file { '/etc/resolv.conf':
-                owner   => 'root',
-                group   => 'root',
-                mode    => '0444',
-                content => template('base/resolv.conf.labs.erb'),
-            }
-        } else {
-            file { '/etc/resolv.conf':
-                owner   => 'root',
-                group   => 'root',
-                mode    => '0444',
-                content => template('base/resolv.conf.erb'),
-            }
+        # also stop dhclient from updating resolv.conf.
+        file { '/etc/dhcp/dhclient-enter-hooks.d/nodnsupdate':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0555',
+            source  => 'puppet:///modules/base/resolv/nodnsupdate',
+            require => File['/etc/dhcp/dhclient-enter-hooks.d'],
+        }
+
+        # Now, finally, we can just puppetize the damn file
+        file { '/etc/resolv.conf':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            content => template('base/resolv.conf.labs.erb'),
+        }
+    } else {
+        file { '/etc/resolv.conf':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            content => template('base/resolv.conf.erb'),
         }
     }
 }
