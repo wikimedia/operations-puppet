9cb945c00f32868ebb18bf5856deba6f2c7660bc
Emanuele Rocca <ema@wikimedia.org>
2018-01-10 11:38:56 +0100

    pybaltest: accept RAs even if forwarding is enabled
    With IP forwarding enabled, the Linux kernel ignores all Router
    Advertisements, breaking IPv6 SLAAC. Accept Router Advertisements even
    if forwarding is enabled.
    
    Change-Id: I98821528bbb123d11f736206758b05dcdf508628

--- modules/role/manifests/pybaltest.pp
+++ modules/role/manifests/pybaltest.pp
@@ -18,6 +18,15 @@ class role::pybaltest {
         srange => "(@resolve((${pybaltest_hosts_ferm})) @resolve((${pybaltest_hosts_ferm}), AAAA))",
     }
 
+    # If the host considers itself as a router (IP forwarding enabled), it will
+    # ignore all router advertisements, breaking IPv6 SLAAC. Accept Router
+    # Advertisements even if forwarding is enabled.
+    sysctl::parameters { 'accept-ra':
+        values => {
+            "net.ipv6.conf.${facts['interface_primary']}.accept_ra" => 2,
+        },
+    }
+
     # Install conftool-master for conftool testing
     class  { '::puppetmaster::base_repo':
         gitdir   => '/var/lib/git',
