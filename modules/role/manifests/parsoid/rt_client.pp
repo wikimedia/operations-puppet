052ec5b782450e9b8a05a4fc30525d6bd5def9b2
Arlo Breault <abreault@wikimedia.org>
2018-01-10 14:31:39 -0500

    Switch to YAML configuration for Parsoid on ruthenium
    This also enables cpu_workers with useWorker.
    
    The removed empty warnings are from cfff63a in the Parsoid repo.
    
    Change-Id: I0aab1a81f34e717ee1fade5f7b2765d49eb2773b

--- modules/role/manifests/parsoid/rt_client.pp
+++ modules/role/manifests/parsoid/rt_client.pp
@@ -2,14 +2,6 @@
 class role::parsoid::rt_client {
     include ::testreduce
 
-    file { '/srv/deployment/parsoid/deploy/src/tests/testreduce/parsoid-rt-client.rttest.localsettings.js':
-        content => template('testreduce/parsoid-rt-client.rttest.localsettings.js.erb'),
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        notify  => Service['parsoid-rt-client'],
-    }
-
     testreduce::client { 'parsoid-rt-client':
         instance_name => 'parsoid-rt-client',
         parsoid_port  => hiera('parsoid::testing::parsoid_port'),
