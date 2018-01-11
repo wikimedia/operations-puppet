cea475bbadae87e5e9340e8d1838ebfd7e96c206
Daniel Zahn <dzahn@wikimedia.org>
2018-01-11 11:24:48 -0800

    ci: replace apache with httpd for proxy/website
    Change-Id: I0856fa1aa168912f1220695e8d55a45883819810

--- modules/profile/manifests/ci/website.pp
+++ modules/profile/manifests/ci/website.pp
@@ -7,21 +7,21 @@
 class profile::ci::website {
     # Need to send Vary: X-Forwarded-Proto since most sites are forced to HTTPS
     # and behind a varnish cache. See also T62822
-    class { '::apache::mod::headers': }
-    class { '::apache::mod::rewrite': }
-
+    class { '::httpd':
+        modules => ['headers', 'rewrite'],
+    }
     # Apache configuration for integration.wikimedia.org
-    apache::site { 'integration.wikimedia.org':
+    httpd::site { 'integration.wikimedia.org':
         content => template('contint/apache/integration.wikimedia.org.erb'),
     }
 
     # Apache configuration for integration.mediawiki.org
-    apache::site { 'integration.mediawiki.org':
+    httpd::site { 'integration.mediawiki.org':
         content => template('contint/apache/integration.mediawiki.org.erb'),
     }
 
     # Apache configuration for doc.wikimedia.org
-    apache::site { 'doc.wikimedia.org':
+    httpd::site { 'doc.wikimedia.org':
         content => template('contint/apache/doc.wikimedia.org.erb'),
     }
 
