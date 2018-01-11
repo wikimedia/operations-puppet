f0183dcb2026ad6e8891655a43f93fdfd589bf9d
Daniel Zahn <dzahn@wikimedia.org>
2018-01-11 11:24:48 -0800

    ci: replace apache with httpd for proxy/website
    Change-Id: I0856fa1aa168912f1220695e8d55a45883819810

--- modules/profile/manifests/ci/proxy_common.pp
+++ modules/profile/manifests/ci/proxy_common.pp
@@ -1,9 +1,7 @@
 # Basic configuration of Apache as a proxy
 class profile::ci::proxy_common {
 
-  class { '::apache': }
-  class { '::apache::mod::php5': }
-  class { '::apache::mod::proxy': }
-  class { '::apache::mod::proxy_http': }
-
+    class { '::httpd':
+        modules => ['php5', 'proxy', 'proxy_http'],
+    }
 }
