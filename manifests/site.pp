9b87b3cd8f4f5cd3b5d2578362931a06a28dd1e4
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-01-15 10:33:34 +0100

    restbase: reprovision restbase1017
    This host has 4 ssds, thus also introduce the corresponding partman recipe for JBOD.
    
    Bug: T184100
    Change-Id: I1f7e0e0f3bf32d7255c24f8c1cb7ff4f1cf21d9d

--- manifests/site.pp
+++ manifests/site.pp
@@ -1834,11 +1834,11 @@ node /^relforge100[1-2]\.eqiad\.wmnet/ {
 }
 
 # restbase eqiad cluster
-node /^restbase101[35678]\.eqiad\.wmnet$/ {
+node /^restbase101[3568]\.eqiad\.wmnet$/ {
     role(spare::system)
 }
 
-node /^restbase10(0[789]|1[0124])\.eqiad\.wmnet$/ {
+node /^restbase10(0[789]|1[01247])\.eqiad\.wmnet$/ {
     role(restbase::production_ng)
 }
 
