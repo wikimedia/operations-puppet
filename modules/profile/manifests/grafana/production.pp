4b9ba0ee242f5288141c63aad72eb2a8d1ec41a3
Alexandros Kosiaris <akosiaris@wikimedia.org>
2018-01-15 18:48:49 +0200

    Simplify profile::grafana::production
    Instead of declaring profile::grafana, require it and let hiera work.
    Populate the corresponding hieradata under role::webserver_misc_apps
    This leaves us with only some grafana::dashboard rules that need some
    refactoring before the profile migration is fully done
    
    Bug: T170150
    Change-Id: Ie0092374f24f25a532b3443ed4c50fbf720f80a2

--- modules/profile/manifests/grafana/production.pp
+++ modules/profile/manifests/grafana/production.pp
@@ -4,21 +4,7 @@
 # It powers <https://grafana.wikimedia.org>.
 #
 class profile::grafana::production {
-    include ::passwords::grafana::production
-
-    class { '::profile::grafana':
-        readonly_domain         => 'grafana.wikimedia.org',
-        admin_domain            => 'grafana-admin.wikimedia.org',
-        secret_key              => $passwords::grafana::production::secret_key,
-        admin_password          => $passwords::grafana::production::admin_password,
-        ldap_editor_description => 'nda/ops/wmf/grafana-admin',
-        ldap_editor_groups      => [
-            'cn=ops,ou=groups,dc=wikimedia,dc=org',
-            'cn=nda,ou=groups,dc=wikimedia,dc=org',
-            'cn=wmf,ou=groups,dc=wikimedia,dc=org',
-            'cn=grafana-admin,ou=groups,dc=wikimedia,dc=org',
-        ]
-    }
+    require ::profile::grafana
 
     grafana::dashboard { 'varnish-http-errors':
         source => 'puppet:///modules/grafana/dashboards/varnish-http-errors',
