2c57a5be40ba25b442e689152bb69d5301085bd1
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2018-01-15 17:16:28 +0100

    wdqs: simplify logging of categories reload
    The reload of categories was logged to multiple files, including one that
    was timestamped. This generates unnecessary complexity.
    
    My guess is that this multiple logging was done to also log manual reload
    of categories, which was usefull during a testing phase where categories
    were reloaded manually.
    
    Change-Id: I58199e4c0d4397d2da8308187fb6b870fecf5f80

--- modules/wdqs/files/cron/reloadCategories.sh
+++ modules/wdqs/files/cron/reloadCategories.sh
@@ -4,12 +4,17 @@
 # sudo systemctl reload nginx
 . /usr/local/bin/cronUtils.sh
 
+echo "$(date --iso-8601=seconds) starting categories reload"
+
 newNamespace="categories${today}"
 # Drop old dumps
 rm -f ${DATA_DIR}/*-categories.ttl.gz
-cd $DEPLOY_DIR
+cd ${DEPLOY_DIR}
 # Create new namespace
-bash createNamespace.sh $newNamespace || exit 1
+bash createNamespace.sh ${newNamespace} || exit 1
 # Load the data
-bash forAllCategoryWikis.sh loadCategoryDump.sh $newNamespace >> "${LOG_DIR}/${newNamespace}.log"
-replaceNamespace categories $newNamespace
+echo "loading categories in ${newNamespace}"
+bash forAllCategoryWikis.sh loadCategoryDump.sh $newNamespace
+replaceNamespace categories ${newNamespace}
+
+echo "$(date --iso-8601=seconds) categories reload done"
