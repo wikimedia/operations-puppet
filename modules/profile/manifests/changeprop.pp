6e909546dc8d4c2c9ac4970afd0bbb3b5d02b6c4
Ottomata <aotto@wikimedia.org>
2018-01-16 16:28:34 -0500

    Ensure specific librdkafka version for changeprop and eventstreams
    Bug: T176126
    Bug: T185016
    Change-Id: I8dac12a8b8d49da97c7dc7dced47dd4f85fde8d5

--- modules/profile/manifests/changeprop.pp
+++ modules/profile/manifests/changeprop.pp
@@ -8,6 +8,9 @@ class profile::changeprop(
     $ores_uris  = hiera('profile::changeprop::ores_uris'),
     $kafka_msg_max_bytes = hiera('kafka_message_max_bytes', 1048576),
 ) {
+    # Pin librdkafka to specific version since node-rdkafka is built on it.
+    # See: https://phabricator.wikimedia.org/T185016
+    require ::profile::kafka::librdkafka::pin
 
     include ::passwords::redis
     include ::service::configuration
