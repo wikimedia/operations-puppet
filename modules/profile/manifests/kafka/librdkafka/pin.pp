fb185d9ef82820549268cf5abe3cba60b0900717
Ottomata <aotto@wikimedia.org>
2018-01-16 16:28:34 -0500

    Ensure specific librdkafka version for changeprop and eventstreams
    Bug: T176126
    Bug: T185016
    Change-Id: I8dac12a8b8d49da97c7dc7dced47dd4f85fde8d5

---
+++ modules/profile/manifests/kafka/librdkafka/pin.pp
@@ -0,0 +1,18 @@
+# == Class profile::kafka::librdkafka::pin
+#
+# TODO: hopefully we don't need this for long.
+# See: https://phabricator.wikimedia.org/T185016
+class profile::kafka::librdkafka::pin(
+    $version = hiera('profile::kafka::librdkafka::pin::version', '0.9.*'),
+) {
+    # https://phabricator.wikimedia.org/T185016
+    # Need to keep librddkafka from upgrading until
+    # node-rdkafka is rebuilt with later version,
+    # and we are sure that version is compatible with
+    # main kafka broker version (currently 0.9.0.1).
+    apt::pin { 'librdkafka':
+        package  => 'librdkafka*',
+        pin      => "version ${version}",
+        priority => '1002',
+    }
+}
