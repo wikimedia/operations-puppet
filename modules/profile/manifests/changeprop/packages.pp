c02904c933363f87e631d70d20fff54bdaf0a4f1
Ottomata <aotto@wikimedia.org>
2018-01-16 16:28:34 -0500

    Ensure specific librdkafka version for changeprop and eventstreams
    Bug: T176126
    Bug: T185016
    Change-Id: I8dac12a8b8d49da97c7dc7dced47dd4f85fde8d5

--- modules/profile/manifests/changeprop/packages.pp
+++ modules/profile/manifests/changeprop/packages.pp
@@ -1,9 +1,33 @@
 # Packages required by changeprop and cpjobqueue
 class profile::changeprop::packages() {
+    require ::service::configuration
 
-    service::packages { 'changeprop':
-        pkgs     => ['librdkafka++1', 'librdkafka1'],
-        dev_pkgs => ['librdkafka-dev'],
+    $librdkafka_version = $::lsbdistcodename ? {
+        'jessie'  => '0.9.4-1~jessie1',
+        'stretch' => '0.9.3-1',
     }
 
+    # We are only installing librdkafka packages here, so make all
+    # in scope package resources ensure the version.
+    # See: https://phabricator.wikimedia.org/T185016
+    Package {
+        ensure => $librdkafka_version
+    }
+    # Need to use package resource directly, so we can ensure version.
+    if !defined(Package['librdkafka1']) {
+        package { 'librdkafka1': }
+    }
+    if !defined(Package['librdkafka++1']) {
+        package { 'librdkafka++1': }
+    }
+    if $::service::configuration::use_dev_pkgs and !defined(Package['librdkafka-dev']) {
+        package { 'librdkafka-dev': }
+    }
+
+    # TODO: restore use of service::packages when we no longer need to
+    # ensure a specific librdkafka version.
+    # service::packages { 'changeprop':
+    #     pkgs     => ['librdkafka++1', 'librdkafka1'],
+    #     dev_pkgs => ['librdkafka-dev'],
+    # }
 }
--- modules/profile/manifests/eventstreams.pp
+++ modules/profile/manifests/eventstreams.pp
@@ -39,9 +39,32 @@ class profile::eventstreams(
 ) {
     $kafka_config = kafka_config($kafka_cluster_name)
     $broker_list = $kafka_config['brokers']['string']
-    service::packages { 'eventstreams':
-        pkgs     => ['librdkafka++1', 'librdkafka1'],
+
+
+    $librdkafka_version = $::lsbdistcodename ? {
+        'jessie'  => '0.9.4-1~jessie1',
+        'stretch' => '0.9.3-1',
     }
+
+    # We are only installing librdkafka packages here, so make all
+    # in scope package resources ensure the version.
+    # See: https://phabricator.wikimedia.org/T185016
+    Package {
+        ensure => $librdkafka_version
+    }
+    # Need to use package resource directly, so we can ensure version.
+    if !defined(Package['librdkafka1']) {
+        package { 'librdkafka1': }
+    }
+    if !defined(Package['librdkafka++1']) {
+        package { 'librdkafka++1': }
+    }
+
+    # TODO: restore use of service::packages when we no longer need to
+    # ensure a specific librdkafka version.
+    # service::packages { 'eventstreams':
+    #     pkgs     => ['librdkafka++1', 'librdkafka1'],
+    # }
 
     service::node { 'eventstreams':
         enable            => true,
