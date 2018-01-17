4ad74e30458e083665f6591f2e6cda48ebbb9310
Daniel Zahn <dzahn@wikimedia.org>
2018-01-17 12:15:44 -0800

    labtest: move firewall/standard includes to roles
    Change-Id: I95b3dfd8e37eabde3ba462c3a5646bda1d384262

--- modules/role/manifests/wmcs/openstack/labtest/net.pp
+++ modules/role/manifests/wmcs/openstack/labtest/net.pp
@@ -1,5 +1,6 @@
 class role::wmcs::openstack::labtest::net {
     system::role { $name: }
+    include ::standard
     include ::profile::openstack::labtest::cloudrepo
     include ::profile::openstack::labtest::nova::common
     include ::profile::openstack::labtest::nova::network::service
