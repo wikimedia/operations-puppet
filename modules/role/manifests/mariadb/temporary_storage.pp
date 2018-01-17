a606cdb1407425367d517b8e964b18dfcd011de9
Jaime Crespo <jcrespo@wikimedia.org>
2018-01-17 19:31:31 +0100

    mariadb: Add small tunings in preparation for es200[1234] reimage
    They are going to be upgraded to stretch, and that means some
    workarounds are no longer needed. Make sure, however, they (2-4) do
    not notify, as these are not user-facing services.
    
    Change-Id: I8b912b8be4f16a7423a911aa6fd1115e4ebe89c3

---
+++ modules/role/manifests/mariadb/temporary_storage.pp
@@ -0,0 +1,50 @@
+# Temporary storage class, very similar to core, but without
+# monitoring and other production-y things. This is mostly
+# for es2001-4 hosts, which keep a mysql install, but are
+# mostly used for temporary storage when disk space is needed
+class role::mariadb::temporary_storage {
+    if os_version('debian >= stretch') {
+        $default_package = 'wmf-mariadb101'
+    } else {
+        $default_package = 'wmf-mariadb10'
+    }
+    $package = hiera('mariadb::package', $default_package)
+    $basedir = hiera('mariadb::basedir',  "/opt/${package}")
+    $socket = hiera('mariadb::socket', '/run/mysqld/mysqld.sock')
+    $datadir = hiera('mariadb::datadir', '/srv/sqldata')
+    $tmpdir = hiera('mariadb::tmpdir', '/srv/tmp')
+    $mysql_role = 'standalone'
+    $semi_sync = 'off'
+    $ssl = 'puppet-cert'
+    $binlog_format = hiera('mariadb::binlog_format', 'ROW')
+
+    system::role { 'mariadb::temporary_storage':
+        description => 'MariaDB temporary storage',
+    }
+
+    include ::standard
+    include ::profile::base::firewall
+    include ::profile::mariadb::monitor
+    # mariadb port not open to the world (change for remote managing)
+    # include ::role::mariadb::ferm
+
+    class {'mariadb::packages_wmf':
+        package => $package,
+    }
+    class {'mariadb::service':
+        package  => $package,
+    }
+
+    class { 'mariadb::config':
+        config           => 'role/mariadb/mysqld_config/production.my.cnf.erb',
+        basedir          => $basedir,
+        datadir          => $datadir,
+        tmpdir           => $tmpdir,
+        socket           => $socket,
+        p_s              => 'on',
+        ssl              => $ssl,
+        binlog_format    => $binlog_format,
+        semi_sync        => $semi_sync,
+        replication_role => $mysql_role,
+    }
+}
