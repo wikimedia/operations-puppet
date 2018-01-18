0f79f70d0b0f087f0f3443ac284962b6716fffa0
Paladox <thomasmulhall410@yahoo.com>
2018-01-18 22:19:52 +0000

    lxc: Fix support for stretch
    Was failing with
    No matching entry for selector parameter with value 'stretch'
    at /etc/puppet/modules/lxc/manifests/init.pp:23
    
    Bug: T180377
    Change-Id: I7a6974f262d22475e76d5b6980b46f06254a9676

--- modules/lxc/manifests/init.pp
+++ modules/lxc/manifests/init.pp
@@ -17,6 +17,14 @@ class lxc(
         ensure => present,
     }
 
+    $package = [
+      'libapparmor1',
+      'liblxc1',
+      'libseccomp2',
+      'lxc',
+      'python3-lxc',
+    ]
+
     # T154294: Running a jessie image in the container requires newer versions
     # of LXC and it's dependencies than Trusty or Jessie shipped with.
     # Install the versions provided by backports instead.
@@ -30,22 +38,23 @@ class lxc(
             'lxc1',
             'python3-lxc',
         ],
-        jessie => [
-            'libapparmor1',
-            'liblxc1',
-            'libseccomp2',
-            'lxc',
-            'python3-lxc',
-        ],
+        jessie => $package,
+        default => '',
     }
 
-    apt::pin { $backports:
-      pin      => "release a=${::lsbdistcodename}-backports",
-      priority => 500,
-    }
-    package { $backports:
-      ensure  => present,
-      require => Apt::Pin[$backports],
+    if os_version('ubuntu == trusty || debian == jessie') {
+      apt::pin { $backports:
+        pin      => "release a=${::lsbdistcodename}-backports",
+        priority => 500,
+      }
+      package { $backports:
+        ensure  => present,
+        require => Apt::Pin[$backports],
+      }
+    } else {
+      package { $package:
+        ensure  => present,
+      }
     }
 
     if os_version('debian >= jessie') {
