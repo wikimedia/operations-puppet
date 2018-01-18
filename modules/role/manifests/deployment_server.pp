b325475d1809bc6e960280f64fdf04b430f65562
Chad Horohoe <chadh@wikimedia.org>
2018-01-17 16:18:10 -0800

    WIP: Sync security patches for MW from deployment to nightlies server
    Change-Id: I241438b471c07ca04f9f29b5bb8c131499f291f2

--- modules/role/manifests/deployment_server.pp
+++ modules/role/manifests/deployment_server.pp
@@ -5,6 +5,7 @@ class role::deployment_server {
     include ::profile::mediawiki::deployment::server
     include ::profile::backup::host
     include ::role::deployment::mediawiki
+    include ::profile::releases::mediawiki::security
     include ::profile::releases::upload
     backup::set {'home': }
 }
