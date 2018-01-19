c73742a28cde10369af93c9efc3a53bed11633ad
andrewbogott <abogott@wikimedia.org>
2018-01-19 15:19:34 -0600

    openstack horizon: rough in manifests for source deploy of Horizon 'ocata'
    At the moment I'm only trying to get a stock Horizon install; will add our
    customizations later (ideally via scap rather than puppet)
    
    Bug: T168470
    Change-Id: Ic4c41cc3ff7becd7b2759984f44f1cd72f1bab92

---
+++ modules/openstack/manifests/horizon/source_deploy.pp
@@ -0,0 +1,112 @@
+class openstack::horizon::source_deploy(
+    $version,
+    $nova_controller,
+    $wmflabsdotorg_admin,
+    $wmflabsdotorg_pass,
+    $dhcp_domain,
+    $ldap_user_pass,
+    $venv_dir      = '/srv/deployment/horizon/venv',
+    $webserver_hostname = 'newhorizon.wikimedia.org'
+) {
+    require_package(
+        'python-wheel',
+        'python-virtualenv',
+        'virtualenv',
+    )
+
+    file { '/etc/openstack-dashboard/local_settings.py':
+        content => template("openstack/${version}/horizon/local_settings.py.erb"),
+        owner   => 'horizon',
+        group   => 'horizon',
+        mode    => '0440',
+        notify  => Service['apache2'],
+    }
+
+    # In the perfect future, Horizon policies will be the same
+    #  files that the respective services use.  In the meantime, though
+    #  it's useful to be able to disable not-yet-supported horizon features.
+    file { '/etc/openstack-dashboard/nova_policy.json':
+        source => "puppet:///modules/openstack/${version}/horizon/nova_policy.json",
+        owner  => 'horizon',
+        group  => 'horizon',
+        mode   => '0440',
+        notify => Service['apache2'],
+    }
+    file { '/etc/openstack-dashboard/glance_policy.json':
+        source => "puppet:///modules/openstack/${version}/horizon/glance_policy.json",
+        owner  => 'horizon',
+        group  => 'horizon',
+        mode   => '0440',
+        notify => Service['apache2'],
+    }
+
+    # We need a horizon-specific keystone policy because horizon does weird/special
+    #  things for admin_required policies which I don't totally understand.  In particular,
+    #  some permissive policies here (e.g. "") cause Horizon to panic, not ask Keystone for permission,
+    #  and log out the user.
+    file { '/etc/openstack-dashboard/keystone_policy.json':
+        source => "puppet:///modules/openstack/${version}/horizon/keystone_policy.json",
+        owner  => 'horizon',
+        group  => 'horizon',
+        mode   => '0440',
+        notify => Service['apache2'],
+    }
+
+    file { '/etc/openstack-dashboard/designate_policy.json':
+        source => "puppet:///modules/openstack/${version}/designate/policy.json",
+        owner  => 'horizon',
+        group  => 'horizon',
+        mode   => '0440',
+        notify => Service['apache2'],
+    }
+
+    # A user and group to run this as
+    group { 'horizon':
+        ensure => present,
+        name   => 'horizon',
+        system => true,
+    }
+
+    user { 'horizon':
+        gid        => 'horizon',
+        system     => true,
+    }
+
+    # This is a trivial policy file that forbids everything.  We'll use it
+    #  for services that we don't support to prevent Horizon from
+    #  displaying spurious panels.
+    file { '/etc/openstack-dashboard/disabled_policy.json':
+        source => "puppet:///modules/openstack/${version}/horizon/disabled_policy.json",
+        owner  => 'horizon',
+        group  => 'horizon',
+        mode   => '0440',
+        notify => Service['apache2'],
+    }
+
+    scap::target { 'horizon/deploy':
+        deploy_user  => 'deploy-service',
+    }
+
+    httpd::site { $webserver_hostname:
+        content => template("openstack/${version}/horizon/${webserver_hostname}.erb"),
+        require => File['/etc/openstack-dashboard/local_settings.py'],
+    }
+
+    # We need to do some work that would otherwise by handled by the horizon
+    #  debian package
+    file { '/etc/openstack-dashboard':
+        ensure => 'directory',
+        owner  => 'root',
+    }
+
+    file { '/var/lib/openstack-dashboard':
+        ensure => 'directory',
+        owner  => 'horizon',
+    }
+
+    file { '/var/lib/openstack-dashboard/static':
+        ensure  => 'directory',
+        owner   => 'horizon',
+        require => File['/var/lib/openstack-dashboard'],
+    }
+}
