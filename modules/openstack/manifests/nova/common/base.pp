73aa8ab44a1f0e521c8aa17e8bf953e13b033f29
cpettet <rush@wikimedia.org>
2018-01-19 18:30:26 +0000

    openstack: nova-network and neutron nova::common split
    These two paths will not coexist in the same deployment,
    and will not exist side by side long term so I am splitting
    the configuration rather than mix them with template logic
    or other logic branching for the same manifests.
    
    Bug: T171494
    Change-Id: Iba0aecdfaa1e35c24dbc13a27d0459ce570abe3b

---
+++ modules/openstack/manifests/nova/common/base.pp
@@ -0,0 +1,31 @@
+class openstack::nova::common::base(
+    $version,
+    ) {
+
+    $packages = [
+        'unzip',
+        'bridge-utils',
+        'nova-common',
+    ]
+
+    package { $packages:
+        ensure => 'present',
+    }
+
+    # For some reason the Mitaka nova-common package installs
+    #  a logrotate rule for nova/*.log and also a nova/nova-manage.log.
+    #  This is redundant and makes log-rotate unhappy.
+    # Not to mention, nova-manage.log is very low traffic and doesn't
+    #  really need to be rotated anyway.
+    file { '/etc/logrotate.d/nova-manage':
+        ensure  => 'absent',
+        require => Package['nova-common'],
+    }
+
+    file { '/etc/nova/policy.json':
+        source => "puppet:///modules/openstack/${version}/nova/common/policy.json",
+        mode   => '0644',
+        owner  => 'root',
+        group  => 'root',
+    }
+}
