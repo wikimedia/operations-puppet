1818ed97e99a9e2880cd0ec7a0928ead37fd8e7a
Mikhail Popov <mpopov@wikimedia.org>
2018-01-19 10:30:31 -0800

    Make Shiny Server a separate profile
    Minor change that allows users to apply profile::shiny_server to
    VMs without also pulling in Discovery Dashboards stuff.
    
    Bug: T183984
    Bug: T185131
    Change-Id: I165b7f23915f4a9dddcb69e827800ae94687c816

---
+++ modules/profile/manifests/shiny_server.pp
@@ -0,0 +1,8 @@
+# == Class profile::shiny_server
+#
+# Install and configure Shiny Server.
+#
+# filtertags: labs-project-search labs-project-shiny-r labs-project-discovery-stats
+class profile::shiny_server {
+    include ::shiny_server
+}
