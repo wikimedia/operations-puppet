02189465aa2e91e563486d8707165880a0186014
andrewbogott <abogott@wikimedia.org>
2018-01-19 15:19:34 -0600

    openstack horizon: rough in manifests for source deploy of Horizon 'queens'
    At the moment I'm only trying to get a stock Horizon install; will add our
    customizations later (ideally via scap rather than puppet)
    
    'Queens' is the release currently in development so this is intended
    to work with the current git master branch.  Once queens is released
    the deploy module will need a branch.
    
    Bug: T168470
    Change-Id: Ic4c41cc3ff7becd7b2759984f44f1cd72f1bab92

---
+++ modules/role/manifests/wmcs/openstack/main/labweb.pp
@@ -0,0 +1,24 @@
+# All horizon/striker/wikitech profiles should fold into
+# role::wmcs::openstack::main::web when labweb* is finished
+class role::wmcs::openstack::main::labweb {
+    system::role { $name: }
+
+    require_package('libapache2-mod-wsgi-py3')
+    class { '::httpd':
+        modules => ['alias', 'ssl', 'rewrite', 'headers', 'wsgi'],
+    }
+
+    include ::profile::ldap::client::labs
+
+    # Wikitech:
+    #include ::role::mediawiki::webserver
+    #include ::profile::base::firewall
+    #include ::profile::prometheus::apache_exporter
+    #include ::profile::prometheus::hhvm_exporter
+
+    # Horizon:
+    include ::profile::openstack::base::horizon::dashboard_source_deploy
+
+    # Striker:
+    ## (this profile not yet written)
+}
