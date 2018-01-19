5189127c879adf49491f9163898f45205e8b10f4
Daniel Zahn <dzahn@wikimedia.org>
2018-01-18 17:03:06 -0800

    bast3002->bast3003 in DHCP,network constants,smokeping
    Bug: T184936
    Change-Id: I56e6afd6b303f78a11b24f1c9979a7fce4f9991e

--- modules/network/manifests/constants.pp
+++ modules/network/manifests/constants.pp
@@ -34,8 +34,6 @@ class network::constants {
                     '2620:0:861:2:208:80:154:149',      # bast1001.wikimedia.org
                     '208.80.153.5',                     # bast2001.wikimedia.org
                     '2620:0:860:1:208:80:153:5',        # bast2001.wikimedia.org
-                    '91.198.174.113',                   # bast3002.wikimedia.org
-                    '2620:0:862:1:91:198:174:113',      # bast3002.wikimedia.org
                     '198.35.26.5',                      # bast4001.wikimedia.org
                     '2620:0:863:1:198:35:26:5',         # bast4001.wikimedia.org
                     '198.35.26.6',                      # bast4002.wikimedia.org
