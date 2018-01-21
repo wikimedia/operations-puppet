165b376987b45313641db244fb0d27a6a6f0c2d6
Paladox <thomasmulhall410@yahoo.com>
2018-01-21 15:11:59 +0000

    ircecho: Support ssl when connecting to irc
    Put this under a config to let the user decide whether to connect
    through ssl.
    
    Change-Id: Ic2bf644d7922873d82548b69fb2ebf86c2b7b25c

--- modules/ircecho/files/ircecho.py
+++ modules/ircecho/files/ircecho.py
@@ -129,9 +129,16 @@ class EchoReader():
 
 
 class EchoBot(SingleServerIRCBot):
-    def __init__(self, chans, nickname, server):
+    def __init__(self, chans, nickname, server, port, ssl=False):
         print('Connecting to IRC server %s...' % server)
-        SingleServerIRCBot.__init__(self, [(server, 6667)], nickname, 'IRC echo bot')
+
+        if ssl:
+            import ssl
+            ssl_factory = irc.connection.Factory(wrapper=ssl.wrap_socket)
+            SingleServerIRCBot.__init__(self, [(server, port)], nickname, 'IRC echo bot',
+                                        connect_factory=ssl_factory)
+        else:
+            SingleServerIRCBot.__init__(self, [(server, port)], nickname, 'IRC echo bot')
         self.chans = chans
 
     def on_nicknameinuse(self, c, e):
@@ -180,15 +187,17 @@ class EventHandler(pyinotify.ProcessEvent):
 
 
 parser = OptionParser(conflict_handler='resolve')
-parser.set_usage('ircecho [--infile=<filename>] <channel> <nickname> <server>')
+parser.set_usage('ircecho [--infile=<filename>] <channel> <nickname> <server> [[+]port]')
 parser.add_option('--infile', dest='infile',
                   help='Read input from the specific file instead of from stdin')
 (options, args) = parser.parse_args()
 chans = args[0]
 nickname = args[1]
-server = args[2]
+server = args[2].split('+', 1)[0]
+ssl = args[2].split('+', 1)[-1].startswith('+')
+port = args[2].split('+', 1)[-1].strip('+')
 global bot
-bot = EchoBot(chans, nickname, server)
+bot = EchoBot(chans, nickname, server, port, ssl)
 global reader
 reader = EchoReader(options.infile)
 try:
