a82cce555ae07ac051795c640252602fa5bd6f33
Keith Herron <kherron@wikimedia.org>
2018-01-22 16:47:23 -0500

    puppetdb: add major version parameter and add data types
    Add parameter to support different versions of puppetdb
    
    Add data typs to existing puppetdb::app parameters
    
    Bug: T185501
    Change-Id: I0e19c279e295ca097d8ecc6d9396ec931221752a

--- modules/profile/manifests/puppetdb.pp
+++ modules/profile/manifests/puppetdb.pp
@@ -3,17 +3,20 @@ class profile::puppetdb(
     $puppetmasters = hiera('puppetmaster::servers'),
     $jvm_opts = hiera('profile::puppetdb::jvm_opts', '-Xmx4G'),
     $prometheus_nodes = hiera('prometheus_nodes'),
+    $puppetdb_major_version = hiera('puppetdb_major_version', undef),
 ) {
+
     # Prometheus JMX agent for the Puppetdb's JVM
     $jmx_exporter_config_file = '/etc/puppetdb/jvm_prometheus_puppetdb_jmx_exporter.yaml'
     $prometheus_jmx_exporter_port = 9400
     $prometheus_java_opts = "-javaagent:/usr/share/java/prometheus/jmx_prometheus_javaagent.jar=${::ipaddress}:${prometheus_jmx_exporter_port}:${jmx_exporter_config_file}"
+
     # The JVM heap size has been raised to 6G for T170740
     class { '::puppetmaster::puppetdb':
-        master   => $master,
-        jvm_opts => "${jvm_opts} ${prometheus_java_opts}",
+        master                   => $master,
+        jvm_opts                 => "${jvm_opts} ${prometheus_java_opts}",
+        puppetdb_major_version   => $puppetdb_major_version,
     }
-
 
     # Export JMX metrics to prometheus
     profile::prometheus::jmx_exporter { "puppetdb_${::hostname}":
@@ -23,7 +26,6 @@ class profile::puppetdb(
         config_file      => $jmx_exporter_config_file,
         source           => 'puppet:///modules/profile/puppetmaster/puppetdb/jvm_prometheus_puppetdb_jmx_exporter.yaml',
     }
-
 
     # Firewall rules
 
@@ -42,4 +44,5 @@ class profile::puppetdb(
         port   => 443,
         srange => '$CUMIN_MASTERS',
     }
+
 }
