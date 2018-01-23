5a5320200e434ee0040c79a42311fb271fafaa9d
Daniel Zahn <dzahn@wikimedia.org>
2018-01-23 15:29:37 -0800

    wmcs::puppetmaster: move standard/firewall include to roles
    Change-Id: Ifc7479dcccab94cc4fb0fea5a4b1f9f1ed8806ba

--- modules/role/manifests/wmcs/openstack/main/puppetmaster/frontend.pp
+++ modules/role/manifests/wmcs/openstack/main/puppetmaster/frontend.pp
@@ -1,5 +1,7 @@
 class role::wmcs::openstack::main::puppetmaster::frontend {
     system::role { $name: }
+    include ::standard
+    include ::profile::base::firewall
     include ::profile::openstack::main::clientlib
     include ::profile::openstack::main::observerenv
     include ::profile::openstack::main::puppetmaster::frontend
