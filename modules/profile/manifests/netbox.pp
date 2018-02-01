760a9e472bbf63443733b8de872916022aafbbf4
Alexandros Kosiaris <akosiaris@wikimedia.org>
2018-02-01 14:25:40 +0200

    netbox: Switch to 4 space indents
    Our puppet coding guidelines are 4 indents, not 2
    
    Change-Id: Idc660525929c84e810ba354f2ae50423d061ca6b

--- modules/profile/manifests/netbox.pp
+++ modules/profile/manifests/netbox.pp
@@ -15,46 +15,46 @@
 class profile::netbox ($active_server = hiera('profile::netbox::active_server', 'netmon1002.wikimedia.org')) {
 
 # lint:ignore:wmf_styleguide
-  include ::apache
-  include ::apache::mod::headers
-  include ::apache::mod::proxy_http
-  include ::apache::mod::proxy
-  include ::apache::mod::rewrite
-  include ::apache::mod::ssl
-  include ::apache::mod::wsgi
+    include ::apache
+    include ::apache::mod::headers
+    include ::apache::mod::proxy_http
+    include ::apache::mod::proxy
+    include ::apache::mod::rewrite
+    include ::apache::mod::ssl
+    include ::apache::mod::wsgi
 # lint:endignore
 
-  include passwords::netbox
-  $db_password = $passwords::netbox::db_password
-  $secret_key = $passwords::netbox::secret_key
-  $replication_pass = $passwords::netbox::replication_password
+    include passwords::netbox
+    $db_password = $passwords::netbox::db_password
+    $secret_key = $passwords::netbox::secret_key
+    $replication_pass = $passwords::netbox::replication_password
 
-  # Used for LDAP auth
-  include passwords::ldap::wmf_cluster
-  $proxypass = $passwords::ldap::wmf_cluster::proxypass
+    # Used for LDAP auth
+    include passwords::ldap::wmf_cluster
+    $proxypass = $passwords::ldap::wmf_cluster::proxypass
 
-  # Define master postgres server
-  $master = 'netmon1002.wikimedia.org'
+    # Define master postgres server
+    $master = 'netmon1002.wikimedia.org'
 
-  # Inspired by modules/puppetmaster/manifests/puppetdb/database.pp
-  if $master == $::fqdn {
-      # We do this for the require in postgres::db
-      $require_class = 'postgresql::master'
-      class { '::postgresql::master':
-          root_dir => '/srv/postgres',
-          use_ssl  => true,
-      }
-      $on_master = true
-  } else {
-      $require_class = 'postgresql::slave'
-      class { '::postgresql::slave':
-          master_server    => $master,
-          root_dir         => '/srv/postgres',
-          replication_pass => $replication_pass,
-          use_ssl          => true,
-      }
-      $on_master = false
-  }
+    # Inspired by modules/puppetmaster/manifests/puppetdb/database.pp
+    if $master == $::fqdn {
+        # We do this for the require in postgres::db
+        $require_class = 'postgresql::master'
+        class { '::postgresql::master':
+            root_dir => '/srv/postgres',
+            use_ssl  => true,
+        }
+        $on_master = true
+    } else {
+        $require_class = 'postgresql::slave'
+        class { '::postgresql::slave':
+            master_server    => $master,
+            root_dir         => '/srv/postgres',
+            replication_pass => $replication_pass,
+            use_ssl          => true,
+        }
+        $on_master = false
+    }
 
     postgresql::user { 'replication@netmon2001':
         ensure   => present,
@@ -74,62 +74,62 @@ class profile::netbox ($active_server = hiera('profile::netbox::active_server', 
         cidr     => '208.80.153.110/32',
         master   => $on_master,
     }
-  # Create the netbox user for localhost
-  # This works on every server and is used for read-only db lookups
-  postgresql::user { 'netbox@localhost':
-      ensure   => present,
-      user     => 'netbox',
-      database => 'netbox',
-      password => $db_password,
-      master   => $on_master,
-  }
+    # Create the netbox user for localhost
+    # This works on every server and is used for read-only db lookups
+    postgresql::user { 'netbox@localhost':
+        ensure   => present,
+        user     => 'netbox',
+        database => 'netbox',
+        password => $db_password,
+        master   => $on_master,
+    }
 
-  # Create the database
-  postgresql::db { 'netbox':
-      owner   => 'netbox',
-      require => Class[$require_class],
-  }
-  postgresql::user { 'prometheus@localhost':
-      user     => 'prometheus',
-      database => 'postgres',
-      type     => 'local',
-      method   => 'peer',
-  }
+    # Create the database
+    postgresql::db { 'netbox':
+        owner   => 'netbox',
+        require => Class[$require_class],
+    }
+    postgresql::user { 'prometheus@localhost':
+        user     => 'prometheus',
+        database => 'postgres',
+        type     => 'local',
+        method   => 'peer',
+    }
 
-  class { '::netbox':
-      directory     => '/srv/deployment/netbox/deploy/netbox',
-      db_password   => $db_password,
-      secret_key    => $secret_key,
-      ldap_password => $proxypass,
-      admins        => '("Ops Team", "ops@lists.wikimedia.org")',
-  }
-  $ssl_settings = ssl_ciphersuite('apache', 'mid', true)
+    class { '::netbox':
+        directory     => '/srv/deployment/netbox/deploy/netbox',
+        db_password   => $db_password,
+        secret_key    => $secret_key,
+        ldap_password => $proxypass,
+        admins        => '("Ops Team", "ops@lists.wikimedia.org")',
+    }
+    $ssl_settings = ssl_ciphersuite('apache', 'mid', true)
 
-  apache::site { 'netbox.wikimedia.org':
-      content => template('profile/netbox/netbox.wikimedia.org.erb'),
-  }
+    apache::site { 'netbox.wikimedia.org':
+        content => template('profile/netbox/netbox.wikimedia.org.erb'),
+    }
 
-  letsencrypt::cert::integrated { 'netbox':
-      subjects   => 'netbox.wikimedia.org',
-      puppet_svc => 'apache2',
-      system_svc => 'apache2',
-      require    => Class['apache::mod::ssl'],
-  }
-  if $active_server == $::fqdn {
-      $monitoring_ensure = 'present'
-  } else {
-      $monitoring_ensure = 'absent'
-  }
+    letsencrypt::cert::integrated { 'netbox':
+        subjects   => 'netbox.wikimedia.org',
+        puppet_svc => 'apache2',
+        system_svc => 'apache2',
+        require    => Class['apache::mod::ssl'],
+    }
+    if $active_server == $::fqdn {
+        $monitoring_ensure = 'present'
+    } else {
+        $monitoring_ensure = 'absent'
+    }
 
-  monitoring::service { 'netbox-ssl':
-      ensure        => $monitoring_ensure,
-      description   => 'Netbox SSL',
-      check_command => 'check_ssl_http_letsencrypt!netbox.wikimedia.org',
-  }
+    monitoring::service { 'netbox-ssl':
+        ensure        => $monitoring_ensure,
+        description   => 'Netbox SSL',
+        check_command => 'check_ssl_http_letsencrypt!netbox.wikimedia.org',
+    }
 
-  monitoring::service { 'netbox-https':
-      ensure        => $monitoring_ensure,
-      description   => 'Netbox HTTPS',
-      check_command => 'check_https_url!netbox.wikimedia.org!https://netbox.wikimedia.org',
-  }
+    monitoring::service { 'netbox-https':
+        ensure        => $monitoring_ensure,
+        description   => 'Netbox HTTPS',
+        check_command => 'check_https_url!netbox.wikimedia.org!https://netbox.wikimedia.org',
+    }
 }
