80c822a8adc940ed0458145c5b4517fc2341de68
andrewbogott <abogott@wikimedia.org>
2018-02-04 03:50:39 -0600

    openstack::common: Add version switching so this can be applied on Stretch
    Change-Id: I2a634c4700e14a20af45186a1d095f4784ff19ee

--- modules/openstack/manifests/common.pp
+++ modules/openstack/manifests/common.pp
@@ -1,10 +1,19 @@
 class openstack::common {
 
-    $packages = [
-        'ebtables',
-        'mysql-common',
-        'mysql-client-5.5',
-        'python-netaddr',
-    ]
+    if os_version('ubuntu == trusty') {
+        $packages = [
+            'ebtables',
+            'mysql-common',
+            'mysql-client-5.5',
+            'python-netaddr',
+        ]
+    } else {
+        $packages = [
+            'ebtables',
+            'mysql-common',
+            'mysql-client',
+            'python-netaddr',
+        ]
+    }
     require_package($packages)
 }
