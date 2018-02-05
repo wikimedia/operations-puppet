d1972e8d10eeef1040c89750fb5374e327404fcb
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-02-05 18:33:45 +0100

    aptly: add more attributes to published repos
    Add more attributes to published repos that were left empty.
    
    Bug: T186539
    Change-Id: I4197577c1972ba29f64793c5158c459cd1465cc7
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/aptly/manifests/repo.pp
+++ modules/aptly/manifests/repo.pp
@@ -15,7 +15,7 @@ define aptly::repo (
         # Pubish the repo directly, without snapshots
         # This isn't reccomended by aptly for production uses, but is perfect for labs :D
         exec { "publish-aptly-repo-${title}":
-            command => "/usr/bin/aptly -architectures=amd64,all publish -skip-signing repo ${title}",
+            command => "/usr/bin/aptly -architectures=amd64,all -skip-signing -origin=Wikimedia -label='${title}' -distribution='${title}'  -component='${component}' publish repo ${title}",
             unless  => "/usr/bin/aptly publish list | /bin/grep -F '[${title}]' > /dev/null",
             user    => 'root',
             require => Exec["create-aptly-repo-${title}"],
