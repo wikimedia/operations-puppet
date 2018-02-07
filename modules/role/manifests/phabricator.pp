53d3317546bb986fddc31822c9f39385ffb58b65
Daniel Zahn <dzahn@wikimedia.org>
2018-02-07 15:38:14 -0800

    phabricator: apache -> httpd module
    Change-Id: I5a56c1ca3cc3e848119c002f007e316d7a9aca40

--- modules/role/manifests/phabricator.pp
+++ modules/role/manifests/phabricator.pp
@@ -12,7 +12,17 @@ class role::phabricator {
     include ::profile::base::firewall
     include ::apache::mod::remoteip
     include ::profile::backup::host
-    include ::profile::phabricator::main
+    3include ::profile::phabricator::main
     include ::phabricator::monitoring
     include ::phabricator::mpm
+
+    if os_version('debian >= stretch') {
+        $php_module = "php7"
+    } else {
+        $php_module = "php5"
+    }
+
+    class { '::httpd':
+        modules => [status, $php_module],
+    }
 }
