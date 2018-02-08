f082f55e7ea7403dde3b1d20078d873ee60c73d5
Jaime Crespo <jcrespo@wikimedia.org>
2018-02-08 13:15:37 +0100

    mariadb: Redo mariadb::backup class into role/profile style
    Move backup_mydumper.pp into 2 profiles: bacula and mydumper.
    
    Bug: T184697
    Change-Id: I7ddfcde7e6bfd1dea092fa44110db6370391341b

--- modules/profile/manifests/mariadb/ferm.pp
+++ modules/profile/manifests/mariadb/ferm.pp
@@ -1,17 +1,27 @@
-# Common ferm class for database access. The actual databases are listening on 3306
-# and are initially limited to the internal network. More specialised sub classes
-# can grant additional access to other hosts
+# Common ferm resource for database access. The actual databases are listening on 3306
+# and are initially limited to the internal network. More specialised classes
+# could grant additional access to other hosts
 
-class profile::mariadb::ferm {
-    ferm::service{ 'mariadb_internal':
+define profile::mariadb::ferm (
+    $port = '3306',
+) {
+    if $port == '3306' {
+        $rule_name = 'mariadb_internal'
+    } else {
+        $rule_name = "${title}_mariadb_internal"
+    }
+    ferm::service{ $rule_name:
         proto   => 'tcp',
-        port    => '3306',
+        port    => $port,
         notrack => true,
         srange  => '$INTERNAL',
     }
 
-    # for DBA purposes
-    ferm::rule { 'mariadb_dba':
-        rule => 'saddr ($MYSQL_ROOT_CLIENTS) proto tcp dport (3307) ACCEPT;',
+    # auxiliary port. FIXME for non-standard ports
+    if $port == '3306' {
+        # for DBA purposes
+        ferm::rule { 'mariadb_dba':
+            rule => 'saddr ($MYSQL_ROOT_CLIENTS) proto tcp dport (3307) ACCEPT;',
+        }
     }
 }
