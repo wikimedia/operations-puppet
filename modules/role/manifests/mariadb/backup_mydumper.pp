24da3d33f6b89c6b9108a05d96f31c6eb02ed448
Jaime Crespo <jcrespo@wikimedia.org>
2018-02-08 13:15:37 +0100

    mariadb: Redo mariadb::backup class into role/profile style
    Move backup_mydumper.pp into 2 profiles: bacula and mydumper.
    
    Bug: T184697
    Change-Id: I7ddfcde7e6bfd1dea092fa44110db6370391341b

--- modules/role/manifests/mariadb/backup_mydumper.pp
+++ modules/role/manifests/mariadb/backup_mydumper.pp
@@ -3,64 +3,8 @@
 # Do that using a cron job on all backup hosts, all datacenters.
 # If we are on the active datacenter, also send the backups to the
 # long-term storage.
-#
-# FIXME: As an experiment, we will backup for now only one shard
 class role::mariadb::backup_mydumper {
     include ::profile::backup::host
-    include ::passwords::mysql::dump
-
-    group { 'dump':
-        ensure => present,
-        system => true,
-    }
-
-    user { 'dump':
-        ensure     => present,
-        gid        => 'dump',
-        shell      => '/bin/false',
-        home       => '/srv/backups',
-        system     => true,
-        managehome => false,
-    }
-
-    file { '/srv/backups':
-        ensure => directory,
-        owner  => 'dump',
-        group  => 'dump',
-        mode   => '0600', # implicitly 0700 for dirs
-    }
-
-    file { '/usr/local/bin/dump_shards':
-        ensure => present,
-        owner  => 'dump',
-        group  => 'dump',
-        mode   => '0555',
-        source => 'puppet:///modules/role/mariadb/dump_shards.sh',
-    }
-
-    file { '/etc/mysql/dump_shards.cnf':
-        ensure  => present,
-        owner   => 'dump',
-        group   => 'dump',
-        mode    => '0400',
-        content => "[client]\nuser=${passwords::mysql::dump::user}\npassword=${passwords::mysql::dump::pass}\n",
-    }
-
-    cron { 'dumps-shards':
-        minute  => 0,
-        hour    => 22,
-        weekday => 2,
-        user    => 'dump',
-        command => '/usr/local/bin/dump_shards >/dev/null 2>&1',
-        require => [File['/usr/local/bin/dump_shards'],
-                    File['/srv/backups'],
-        ],
-    }
-
-    # produce logical backups on all backups hosts, but only send them
-    # to the long term storage on one datacenter at a time
-    $backups_active = hiera('role::mariadb::backups_mydumper::active', false)
-    if $backups_active {
-        backup::set { 'mysql-srv-backups': }
-    }
+    include ::profile::mariadb::backup::mydumper
+    include ::profile::mariadb::backup::bacula
 }
