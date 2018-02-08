894b19605aafacc371547f2af4951a5fc7a77d38
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-02-08 16:30:52 +0100

    prometheus: add check prometheus metric script
    This is a replacement and enhancement of the current check_prometheus shell script, float thresholds
    are supported.
    
    Also vector results are supported, and metrics are grouped based on labels. This isn't ideal but can
    be improved upon later. Note that there are some inherent limitations with checking multiple metrics
    under a single check with icinga (e.g. it isn't possible to silence only one of the alarming
    metrics). This behaviour is similar to the "router port down" check, where status text reports which
    interface is down.
    
    For example to check multiple metrics:
    
    check_prometheus.py -c 0 -w 0 'up{job="node"} == 0'
    
    CRITICAL - 'up{job="node"} == 0': cluster={appserver,lvs}
    instance={lvs1008:9100,lvs1009:9100,mw1256:9100} job=node site=eqiad
    
    Bug: T181410
    Change-Id: Ic0b381e5d177e07ac9578aad8a1dd9b41dccc697

---
+++ modules/prometheus/files/usr/local/lib/nagios/plugins/check_prometheus_test.py
@@ -0,0 +1,62 @@
+import unittest
+import time
+
+from check_prometheus import PrometheusCheck, PREDICATE
+from check_prometheus import EX_OK, EX_CRITICAL, EX_WARNING, EX_UNKNOWN
+
+from prometheus_client.parser import text_string_to_metric_families
+
+
+class PrometheusCheckTest(unittest.TestCase):
+    def setUp(self):
+        self.c = PrometheusCheck('localhost')
+
+    def _check_scalar(self, value, warning, critical, predicate='ge', nan_ok=False):
+        status, text = self.c._check_scalar(
+                [time.time(), '{}'.format(value)],
+                PREDICATE.get(predicate), warning, critical, nan_ok)
+        return status
+
+    def _check_vector(self, metrics, warning, critical, predicate='ge', nan_ok=False):
+        status, text = self.c._check_vector(
+                [self._text_to_vector(x) for x in metrics],
+                PREDICATE.get(predicate), warning, critical, nan_ok)
+        return status
+
+    def _text_to_vector(self, text):
+        """Turn a metric from Prometheus text format (name{key="value"} number) into a metric
+        dictionary like what's returned by Prometheus API."""
+
+        result = {}
+        # assume a single metric in 'text' and a single sample for that metric
+        for metric in text_string_to_metric_families(text):
+            # metric.samples will be [('metric name', {labels..}, value)]
+            sample = metric.samples[0]
+            result['value'] = [time.time(), sample[2]]
+            result['metric'] = sample[1]
+            result['metric']['__name__'] = sample[0]
+        return result
+
+    def testScalarThreshold(self):
+        self.assertEqual(EX_OK, self._check_scalar(5, 10, 20))
+        self.assertEqual(EX_OK, self._check_scalar(9, 10, 20))
+        self.assertEqual(EX_WARNING, self._check_scalar(10, 10, 20))
+        self.assertEqual(EX_WARNING, self._check_scalar(15, 10, 20))
+        self.assertEqual(EX_CRITICAL, self._check_scalar(20, 10, 20))
+        self.assertEqual(EX_CRITICAL, self._check_scalar(21, 10, 20))
+        self.assertEqual(EX_UNKNOWN, self._check_scalar('NaN', 10, 20))
+
+    def testVectorThreshold(self):
+        self.assertEqual(EX_OK, self._check_vector(['up{foo="bar"} 0', 'up{foo="baz"} 1'], 10, 20))
+        self.assertEqual(EX_WARNING,
+                         self._check_vector(['up{foo="bar"} 11', 'up{foo="baz"} 1'], 10, 20))
+        self.assertEqual(EX_CRITICAL,
+                         self._check_vector(['up{foo="bar"} 21', 'up{foo="baz"} 1'], 10, 20))
+
+    def testGroupAllLabels(self):
+        self.assertEqual(['foo={bar,baz}', 'meh=zomg'],
+                         self.c._group_all_labels([{'foo': 'bar', 'meh': 'zomg'}, {'foo': 'baz'}]))
+
+
+if __name__ == '__main__':
+    unittest.main()
