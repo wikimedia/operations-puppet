096151e4b7d231b98fbc934f10f2f69969084f2f
Jaime Crespo <jcrespo@wikimedia.org>
2018-02-08 13:15:37 +0100

    mariadb: Redo mariadb::backup class into role/profile style
    Move backup_mydumper.pp into 2 profiles: bacula and mydumper.
    
    Bug: T184697
    Change-Id: I7ddfcde7e6bfd1dea092fa44110db6370391341b

--- modules/role/manifests/mariadb/core_multiinstance.pp
+++ modules/role/manifests/mariadb/core_multiinstance.pp
@@ -2,9 +2,8 @@ class role::mariadb::core_multiinstance {
     system::role { 'mariadb::core':
         description => 'Core multi-instance server',
     }
-    # lint:ignore:wmf_styleguide
-    include ::base::firewall
-    # lint:endignore:wmf_styleguide
+    include ::profile::base::firewall
     include ::standard
+
     include ::profile::mariadb::core::multiinstance
 }
