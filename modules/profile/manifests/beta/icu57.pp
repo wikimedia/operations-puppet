72390dd1693b0068e315026112a7c6a08180cc7d
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-02-08 14:17:29 +0100

    Add apt configuration to switch deployment-prep to the ICU57-enabled HHVM build
    The class will be enabled via the Horizon configuration for deployment-prep.
    
    Change-Id: If2c6e08bf6ba32ee5c740f2da7cc9430bed6e229

---
+++ modules/profile/manifests/beta/icu57.pp
@@ -0,0 +1,9 @@
+class profile::beta::icu57 {
+    if os_version('debian == jessie') {
+        apt::repository { 'hhvm-icu57':
+            uri        => 'http://apt.wikimedia.org/wikimedia',
+            dist       => "jessie-wikimedia",
+            components => 'component/icu57',
+        }
+    }
+}
