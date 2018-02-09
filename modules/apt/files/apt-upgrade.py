64761ec19866705c7e391c7cdf8284856aec5f2a
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-02-09 13:21:49 +0100

    apt: apt-upgrade: add switch for the node name output
    So we can decide either print it or not.
    
    While at it, instead of carrying booleans all over function calls, let's use
    2 global variables to control whether to print output or not.
    
    Bug: T181647
    Change-Id: I7f22c465f22e0f4e9f3680930329ce9c3ef10ff1
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/apt/files/apt-upgrade.py
+++ modules/apt/files/apt-upgrade.py
@@ -9,29 +9,36 @@ import socket
 
 # Usage:
 #
-#  % apt-upgrade [-u] upgrade <suite> [-yvh]
-#  % apt-upgrade [-u] report [<suite>] [-h]
-#  % apt-upgrade [-u] list [-h]
-#
+#  % apt-upgrade [-un] upgrade <suite> [-yvh]
+#  % apt-upgrade [-un] report [<suite>] [-h]
+#  % apt-upgrade [-un] list [-h]
 #
 # make sure you hold+pin beforehand those packages that should not be upgraded
 
+print_enabled = False
+print_hostname = False
 
-def print_verbose(verbose, msg):
-    """ print information if verbose
-    :param verbose: boolean
+
+def print_output(msg):
+    """ printing function. Reads above global variables
     :param msg: str
     """
-    if verbose:
-        print("{}: {}".format(socket.gethostname(), msg))
+    if not print_enabled:
+        return
+
+    if print_hostname:
+        hostname = "{}: ".format(socket.gethostname())
+    else:
+        hostname = ""
+
+    print("{}{}".format(hostname, msg))
 
 
-def print_verbose_pkg(verbose, pkg):
+def print_output_pkg(pkg):
     """ print information about a package
-    :param verbose: boolean
     :param pkg: Package
     """
-    if not verbose:
+    if not print_enabled:
         return
     archive = pkg.candidate.origins[0].archive
     if not archive:
@@ -45,12 +52,11 @@ def print_verbose_pkg(verbose, pkg):
         vdest = pkg.candidate.version
     else:
         vdest = "remove"
-    print_verbose(verbose, '{}: {} {} --> {}'.format(archive, name, vorig, vdest))
+    print_output('{}: {} {} --> {}'.format(archive, name, vorig, vdest))
 
 
-def pkg_upgrade(verbose, pkg):
+def pkg_upgrade(pkg):
     """ try to mark a package for upgrade
-    :param verbose: boolean
     :param pkg: Package
     """
     if not pkg.is_installed:
@@ -59,7 +65,7 @@ def pkg_upgrade(verbose, pkg):
         pkg.mark_upgrade()
         marked_upgrade = True
     except apt_pkg.Error as e:
-        print_verbose(verbose, '{} not for upgrade: {}'.format(pkg.name, str(e)))
+        print_output('{} not for upgrade: {}'.format(pkg.name, str(e)))
         pkg.mark_keep()
         marked_upgrade = False
 
@@ -107,32 +113,30 @@ def sort_pkgs_by_archive(pkg_list):
     return sorted(pkg_list, key=lambda pkg: pkg.candidate.origins[0].archive)
 
 
-def calculate_upgrades(cache, verbose):
+def calculate_upgrades(cache):
     """ calculate upgrades and print the changes
     :param cache: apt.Cache
-    :param verbose: boolean
     """
     for pkg_name in cache.keys():
-        pkg_upgrade(verbose, cache[pkg_name])
+        pkg_upgrade(cache[pkg_name])
 
     # report changes
     for pkg in sort_pkgs_by_archive(cache.get_changes()):
-        print_verbose_pkg(verbose, pkg)
+        print_output_pkg(pkg)
 
     return len(cache.get_changes())
 
 
-def run_upgrade(cache, src, confirm, verbose):
+def run_upgrade(cache, src, confirm):
     """ main upgrade routine: calculate upgrades and commit them
     :param cache: apt.Cache
     :param src: str
     :param confirm: boolean
-    :param verbose: boolean
     """
     cache.set_filter(AptFilterUpgradeableSrc(src))
 
-    if not calculate_upgrades(cache, verbose):
-        print_verbose(verbose, 'no packages found to upgrade from {}'.format(src))
+    if not calculate_upgrades(cache):
+        print_output('no packages found to upgrade from {}'.format(src))
         return
 
     if not confirm:
@@ -148,20 +152,24 @@ def run_report(cache, archive):
     :param cache: apt.Cache
     :param archive: str
     """
+    global print_enabled
     if archive:
         cache.set_filter(AptFilterUpgradeableSrc(archive))
     else:
         cache.set_filter(AptFilterUpgradeable())
 
-    return calculate_upgrades(cache, True)
+    print_enabled = True
+    return calculate_upgrades(cache)
 
 
 def run_list(cache):
     """ list available archives from which packages can be upgraded
     :param cache: apt.Cache
     """
+    global print_enabled
     cache.set_filter(AptFilterUpgradeable())
-    calculate_upgrades(cache, False)
+    print_enabled = False
+    calculate_upgrades(cache)
     archives = set()
     for pkg in cache.get_changes():
         archive = pkg.candidate.origins[0].archive
@@ -173,12 +181,16 @@ def run_list(cache):
     if len(archives) == 0:
         return
 
-    print_verbose(True, "{}".format(", ".join(a for a in sorted(archives))))
+    print_enabled = True
+    print_output("{}".format(", ".join(a for a in sorted(archives))))
 
 
 def main():
+    global print_enabled
+    global print_hostname
     parser = argparse.ArgumentParser(description="Utility to help with upgrades of packages")
     parser.add_argument("-u", action="store_true", help="don't run cache update")
+    parser.add_argument("-n", action="store_true", help="don't print the node name")
     subparser = parser.add_subparsers(help="possible operations (pass -h to know usage of each)",
                                       dest="operation")
     subparser.add_parser("list", help="list available sources of upgrades")
@@ -204,16 +216,19 @@ def main():
         parser.print_help()
         sys.exit(1)
 
+    print_hostname = not args.n
     cache = apt.cache.FilteredCache()
 
     if not args.u:
-        print_verbose(True, "updating apt cache ...")
+        print_enabled = True
+        print_output("updating apt cache ...")
         cache.update()
 
     cache.open(None)
 
     if args.operation == "upgrade":
-        run_upgrade(cache, args.archive, args.y, args.v)
+        print_enabled = args.v
+        run_upgrade(cache, args.archive, args.y)
     elif args.operation == "report":
         run_report(cache, args.archive)
     elif args.operation == "list":
