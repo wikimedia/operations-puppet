fc41cb753c1589e48c731030aba0d8d6d133fdbf
Daniel Zahn <dzahn@wikimedia.org>
2018-02-13 14:42:44 -0800

    mediawiki: reduce frequency of purge_abusefilter to weekly
    This script takes longer than 24 hours to run, so running it daily
    will cause failures since it won't be done before it's started the
    next time.
    
    The script needs to be optimized per linked ticket. Until then
    reduce frequencey so that i can finish once at least.
    
    Bug: T187078
    Change-Id: I3eb3618ea03a1878b6e89ca7df5a2f59270eda9d

--- modules/mediawiki/manifests/maintenance/purge_abusefilter.pp
+++ modules/mediawiki/manifests/maintenance/purge_abusefilter.pp
@@ -3,6 +3,7 @@ class mediawiki::maintenance::purge_abusefilter( $ensure = present ) {
         ensure  => $ensure,
         command => '/usr/local/bin/foreachwiki extensions/AbuseFilter/maintenance/purgeOldLogIPData.php >/var/log/mediawiki/purge_abusefilter.log 2>&1',
         user    => $::mediawiki::users::web,
+        weekday => 1,
         hour    => 1,
         minute  => 15,
     }
