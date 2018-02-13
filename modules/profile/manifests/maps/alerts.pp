68b790775b3806bb18ff385f1458f9761a7167d6
Guillaume Lederrey <glederrey@wikimedia.org>
2018-02-13 11:25:37 +0100

    maps: icinga alert if tiles are not being generated
    Bug: T175243
    Change-Id: Ie8caa18f4b4d95ba3c7a6a17a94561c8f0795fbb

--- modules/profile/manifests/maps/alerts.pp
+++ modules/profile/manifests/maps/alerts.pp
@@ -1,5 +1,18 @@
 # Define various checks for Maps
 class profile::maps::alerts {
+    monitoring::graphite_threshold { 'tilerator-tile-generation':
+        description     => 'Maps tiles generation',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/maps-performances?panelId=8&fullscreen&orgId=1'],
+        metric          => 'transformNull(sumSeries(tilerator.gen.*.*.*.done.sample_rate),0)',
+        # Tilerator should be generating tiles at least 2 hours per day
+        # Values need to be adjusted if synchronization frequency is changed
+        under           => true,
+        warning         => 10,
+        critical        => 5,
+        from            => '1day',
+        percentage      => 90,
+    }
+
     monitoring::check_prometheus { 'maps-osm-sync-lag-eqiad':
       description     => 'Maps - OSM synchronization lag - eqiad',
       dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/maps-performances?panelId=11&fullscreen&orgId=1'],
