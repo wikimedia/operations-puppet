219879c74db1bc02603f7182e818ab28fb914088
Guillaume Lederrey <glederrey@wikimedia.org>
2018-02-13 14:48:13 +0100

    maps: Icinga alert when OSM replication lags
    Bug: T167549
    Change-Id: I9d9ca824366f3883afa6098e9399ab75c8e0643f

--- modules/profile/manifests/maps/alerts.pp
+++ modules/profile/manifests/maps/alerts.pp
@@ -12,4 +12,30 @@ class profile::maps::alerts {
         from            => '1day',
         percentage      => 90,
     }
+
+    monitoring::check_prometheus { 'maps-osm-sync-lag-eqiad':
+      description     => 'Maps - OSM synchronization lag - eqiad',
+      dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/maps-performances?panelId=11&fullscreen&orgId=1'],
+      query           => 'scalar(max(time()-osm_sync_timestamp{cluster="maps"}))',
+      warning         => 25 * 3600, # 25 hours
+      critical        => 48 * 3600, # 48 hours
+      prometheus_url  => 'http://prometheus.svc.eqiad.wmnet/ops',
+    }
+    monitoring::check_prometheus { 'maps-osm-sync-lag-codf':
+        description     => 'Maps - OSM synchronization lag - codfw',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/maps-performances?panelId=12&fullscreen&orgId=1'],
+        query           => 'scalar(max(time()-osm_sync_timestamp{cluster="maps"}))',
+        warning         => 25 * 3600, # 25 hours
+        critical        => 48 * 3600, # 48 hours
+        prometheus_url  => 'http://prometheus.svc.codfw.wmnet/ops',
+    }
+    monitoring::check_prometheus { 'maps-test-osm-sync-lag-codfw':
+        description     => 'Maps (test) - OSM synchronization lag - codfw',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/maps-performances?panelId=12&fullscreen&orgId=1'],
+        query           => 'scalar(max(time()-osm_sync_timestamp{cluster="maps-test"}))',
+        warning         => 25 * 3600, # 25 hours
+        critical        => 48 * 3600, # 48 hours
+        prometheus_url  => 'http://prometheus.svc.codfw.wmnet/ops',
+    }
+
 }
