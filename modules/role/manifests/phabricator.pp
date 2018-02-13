8b9c68a7d968266a1f164bfe8d0d984e3d040912
Paladox <thomasmulhall410@yahoo.com>
2018-02-13 19:59:53 +0000

    Phabricator: Support php 7.2 under stretch
    Using thirdparty/php72 under stretch
    
    Bug: T182832
    Bug: T187127
    Change-Id: I47db9f0c4e6c3bad78a628ae0e58b24a513c3f9c

--- modules/role/manifests/phabricator.pp
+++ modules/role/manifests/phabricator.pp
@@ -17,12 +17,15 @@ class role::phabricator {
     include ::profile::prometheus::apache_exporter
 
     if os_version('debian >= stretch') {
-        $php_module = 'php7'
+        $php_module = 'php7.2'
     } else {
         $php_module = 'php5'
     }
 
+    $apache_lib = "libapache2-mod-${php_module}"
+
     class { '::httpd':
         modules => ['headers', 'rewrite', 'remoteip', $php_module],
+        require => Package[$apache_lib],
     }
 }
