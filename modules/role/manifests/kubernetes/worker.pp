ac67742a5f27f339d2875400933fa821142925f3
Alexandros Kosiaris <akosiaris@wikimedia.org>
2018-02-14 18:48:13 +0200

    Prepare kubernetes nodes for serving mathoid traffic
    Add the LVS role and IPs to the kubernetes nodes in both DCs, and reuse
    the scb conftool cluster since currently a service is bound to a cluster
    
    Bug: T184919
    Change-Id: I2f9594bce84418047ffe5323cc6dcea123e1a3b5

--- modules/role/manifests/kubernetes/worker.pp
+++ modules/role/manifests/kubernetes/worker.pp
@@ -5,6 +5,10 @@ class role::kubernetes::worker {
     # Sets up docker on the machine
     include ::profile::docker::storage
     include ::profile::docker::engine
+    # Setup kubernetes stuff
     include ::profile::kubernetes::node
+    # Setup calico
     include ::profile::calico::kubernetes
+    # Setup LVS
+    include ::role::lvs::realserver
 }
