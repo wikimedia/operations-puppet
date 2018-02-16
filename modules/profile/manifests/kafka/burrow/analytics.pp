6173f115d2194524c8f82fbe7c5941af17f32f7f
elukey <ltoscano@wikimedia.org>
2018-02-16 15:20:39 +0100

    profile::kafka::burrow: add prometheus monitoring
    Change-Id: I68c7c274a7e62a5e6a425cf82dd66c1a78d1018f

--- modules/profile/manifests/kafka/burrow/analytics.pp
+++ modules/profile/manifests/kafka/burrow/analytics.pp
@@ -3,13 +3,19 @@
 # Consumer offset lag monitoring tool for the Kafka Analytics cluster
 #
 class profile::kafka::burrow::analytics(
-    $consumer_groups = hiera('profile::kafka::burrow:analytics::consumer_groups'),
-    $to_emails       = hiera('profile::kafka::burrow:analytics::to_emails', ['analytics-alerts@wikimedia.org'])
+    $consumer_groups  = hiera('profile::kafka::burrow:analytics::consumer_groups'),
+    $to_emails        = hiera('profile::kafka::burrow:analytics::to_emails', ['analytics-alerts@wikimedia.org']),
+    $prometheus_nodes = hiera('prometheus_nodes'),
 ) {
 
     profile::kafka::burrow { 'analytics':
         consumer_groups => $consumer_groups,
         http_port       => 8000,
         to_emails       => $to_emails,
+    }
+
+    profile::prometheus::burrow_exporter { 'analytics':
+        burrow_addr => 'localhost:8000',
+        port        => 9000,
     }
 }
