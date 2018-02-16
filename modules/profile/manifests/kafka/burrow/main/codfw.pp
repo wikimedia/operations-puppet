6ffe54679eeca6254d0f9234c875af0bcf48864b
elukey <ltoscano@wikimedia.org>
2018-02-16 15:20:39 +0100

    profile::kafka::burrow: add prometheus monitoring
    Change-Id: I68c7c274a7e62a5e6a425cf82dd66c1a78d1018f

--- modules/profile/manifests/kafka/burrow/main/codfw.pp
+++ modules/profile/manifests/kafka/burrow/main/codfw.pp
@@ -2,9 +2,17 @@
 #
 # Consumer offset lag monitoring tool for the Kafka Main codfw cluster
 #
-class profile::kafka::burrow::main::codfw {
+class profile::kafka::burrow::main::codfw(
+    $prometheus_nodes = hiera('prometheus_nodes'),
+) {
 
     profile::kafka::burrow { 'main-codfw':
         http_port       => 8200,
+    }
+
+    profile::prometheus::burrow_exporter { 'main-codfw':
+        burrow_addr      => 'localhost:8200',
+        port             => 9200,
+        prometheus_nodes => $prometheus_nodes,
     }
 }
