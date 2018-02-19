5952c179736f864c38186a8cd6037e25e83ba5c3
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-02-19 12:35:16 +0100

    conftool::scripts: update scripts to work with conftool 1.0
    Since the --find option has been dropped from conftool 1.x, re-write the
    various scripts to be compatible with the new interface. Also, unify the
    scripts to use a single template, as they don't really need to be
    separated and new things copy/pasted around.
    
    Change-Id: Ie924be25cf56cdf9c2e310bbb04f36effad0282b

---
+++ modules/conftool/files/conftool-simple-command.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -u
+
+action=$(basename ${0})
+service="${1:-all services}"
+host=$(hostname -f)
+old_action=""
+msg=""
+
+# Get message to print on screen, and the old action
+case $action in
+    "pool")
+    old_action="set/pooled=yes"
+    msg="Pooling ${service} on ${host}"
+    ;;
+    "depool")
+    old_action="set/pooled=no"
+    msg="Depooling ${service} on ${host}"
+    ;;
+    "drain")
+    old_action="set/weight=0"
+    msg="Drain ${service} on ${host}"
+    ;;
+    "decommission")
+    old_action="set/pooled=inactive"
+    msg="Decommission ${service} on ${host}"
+    ;;
+    *)
+    echo "Invalid command: ${0}"
+    exit 2
+esac
+
+
+function do_action() {
+    if [ "${1}" == "all services" ]; then
+        confctl --quiet "${action}" 2> /dev/null
+    else
+        confctl --quiet "${action}" --service "${1}" 2> /dev/null
+    fi
+}
+
+function do_old_action() {
+    if [ "${1}" == "all services" ]; then
+        confctl --quiet --find --action $old_action "${host}"
+    else
+        confctl --quiet select "${1},name=${host}" $old_action
+    fi
+}
+
+echo $msg
+do_action "$service"
+# Compatibility with confctl < 1.0
+if [ $? -eq 2 ]; then
+    do_old_action "$service"
+fi
