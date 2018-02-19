f82dfe3fc45a0937b446a0acbb116dcc941142c3
andrewbogott <abogott@wikimedia.org>
2018-02-19 05:26:46 -0600

    bweb: include apache proxy_balancer mod for striker
    Eventually I will get all of these
    
    Change-Id: I6f553affcf71422fb705491a20d473bc97e03831

--- modules/role/manifests/wmcs/openstack/main/labweb.pp
+++ modules/role/manifests/wmcs/openstack/main/labweb.pp
@@ -5,7 +5,7 @@ class role::wmcs::openstack::main::labweb {
 
     require_package('libapache2-mod-wsgi-py3')
     class { '::httpd':
-        modules => ['alias', 'ssl', 'rewrite', 'headers', 'wsgi', 'proxy', 'expires', 'proxy_http'],
+        modules => ['alias', 'ssl', 'rewrite', 'headers', 'wsgi', 'proxy', 'expires', 'proxy_http', 'proxy_balancer'],
     }
 
     include ::profile::ldap::client::labs
