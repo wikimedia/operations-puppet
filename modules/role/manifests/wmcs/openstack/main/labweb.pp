4d68cc1d3ca686c5036eb1844ec22ab78a5eee4f
andrewbogott <abogott@wikimedia.org>
2018-02-19 05:15:08 -0600

    labweb: include apache proxy_http mod for striker
    Change-Id: I82b5f580dadbb45bb4e68d9d9dbff153d259f39f

--- modules/role/manifests/wmcs/openstack/main/labweb.pp
+++ modules/role/manifests/wmcs/openstack/main/labweb.pp
@@ -5,7 +5,7 @@ class role::wmcs::openstack::main::labweb {
 
     require_package('libapache2-mod-wsgi-py3')
     class { '::httpd':
-        modules => ['alias', 'ssl', 'rewrite', 'headers', 'wsgi', 'proxy', 'expires'],
+        modules => ['alias', 'ssl', 'rewrite', 'headers', 'wsgi', 'proxy', 'expires', 'proxy_http'],
     }
 
     include ::profile::ldap::client::labs
