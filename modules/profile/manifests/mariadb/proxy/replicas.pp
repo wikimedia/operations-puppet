be8c335c2fa15509bd0c287c756bab1e74e3cea0
Jaime Crespo <jcrespo@wikimedia.org>
2018-02-19 16:17:15 +0100

    dbproxy: Fix load balancing syntax and apply it to labsdb1010
    dbproxy1010 serves the analytics hosts. We used to run it with
    a failover model- all queries went to labsdb1009, except if it
    failed, in which case all were sent to labsdb1010.
    
    Due to the large load that labsdb1009 is suffering, we will change
    the model to round-robing, so each server receives approximately
    50% of the queries, and get depooled if it fails.
    
    This required fixes on the templates- in particular, aside from
    stretch haproxy version changes, we changed the repool time to
    1000 seconds to avoid flapping. The right value may need future
    tuning.
    
    The -strech versions are temporary patches, when all proxies
    are on stretch or higher, those will become the default and only
    configuration. Small changes on the master template name, too.
    
    Change-Id: I1ca3015ac3417037d622a271e43f0eeee16098a6

--- modules/profile/manifests/mariadb/proxy/replicas.pp
+++ modules/profile/manifests/mariadb/proxy/replicas.pp
@@ -3,10 +3,21 @@ class profile::mariadb::proxy::replicas(
     $servers = hiera('profile::mariadb::proxy::replicas::servers'),
     ) {
 
+    # patch until all haproxies have been upgraded to 1.7
+    if os_version('debian >= stretch') {
+        $replicas_template = 'db-replicas-stretch.cfg.erb'
+    } else {
+        $replicas_template = 'db-replicas.cfg.erb'
+    }
     file { '/etc/haproxy/conf.d/db-replicas.cfg':
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
-        content => template('profile/mariadb/proxy/db-replicas.cfg.erb'),
+        content => template("profile/mariadb/proxy/${replicas_template}"),
+    }
+
+    nrpe::monitor_service { 'haproxy_failover':
+        description  => 'haproxy failover',
+        nrpe_command => '/usr/lib/nagios/plugins/check_haproxy --check=failover',
     }
 }
