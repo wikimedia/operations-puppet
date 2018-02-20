c2baa7bbe3ae879022564516991ba85983537890
Ottomata <aotto@wikimedia.org>
2018-02-20 16:56:18 -0500

    Add kafkatee back in as a regular puppet module (not git submodule)
    Change-Id: I205ce3eab769b2ec8821841623ea568cdc8c3a94

---
+++ modules/kafkatee/manifests/output.pp
@@ -0,0 +1,25 @@
+# == Define kafkatee::output
+# Configures a kafkatee output.
+#
+# == Parameters
+# $destination      - Where this output will be sent.  If $type is
+#                     'file', then this should be a file path.  Otherwise
+#                     it should be a process that receives input from stdin.
+# $type             - Type of kafkatee output.  Either 'file' or 'pipe'.
+#                     Default: file
+# $sample           - The sample rate denominator (1/$sample).
+#                     e.g. 1 means 100%, 1/10 means 10%, etc.
+#
+define kafkatee::output(
+    $destination,
+    $type           = 'file',
+    $sample         = 1,
+    $ensure         = 'present',
+)
+{
+    file { "/etc/kafkatee.d/output.${title}.conf":
+        ensure  => $ensure,
+        content => template('kafkatee/output.conf.erb'),
+        notify  => Service['kafkatee'],
+    }
+}
