e553f302c3e8aa34bb0bc9bc671ce319f6961507
Jaime Crespo <jcrespo@wikimedia.org>
2018-02-20 20:58:26 +0100

    mariadb: Move db2044 from codfw-core-s4 to codfw-misc-s2
    Also mark db1111 and db1111 as the separate core test role.
    
    db2044 will replace db2011.
    
    Bug: T183470
    Change-Id: I1fc601fee199d144e032bffe97e0bf43916404a7

---
+++ modules/role/manifests/mariadb/core_test.pp
@@ -0,0 +1,96 @@
+class role::mariadb::core_test {
+    if os_version('debian >= stretch') {
+        $default_package = 'wmf-mariadb101'
+    } else {
+        $default_package = 'wmf-mariadb10'
+    }
+    $package = hiera('mariadb::package', $default_package)
+    $basedir = hiera('mariadb::basedir',  "/opt/${package}")
+    $socket = hiera('mariadb::socket', '/run/mysqld/mysqld.sock')
+    $datadir = hiera('mariadb::datadir', '/srv/sqldata')
+    $tmpdir = hiera('mariadb::tmpdir', '/srv/tmp')
+    $shard = hiera('mariadb::shard', undef)
+    $mysql_role = hiera('mariadb::mysql_role', 'slave')
+    $ssl = hiera('mariadb::ssl', 'puppet-cert')
+    $binlog_format = hiera('mariadb::binlog_format', 'ROW')
+
+    system::role { 'mariadb::core':
+        description => "Core Test DB Server ${shard}",
+    }
+
+    include ::standard
+    include ::base::firewall
+    include ::profile::mariadb::monitor
+    include ::passwords::misc::scripts
+    include ::role::mariadb::ferm
+
+    # Semi-sync replication
+    # off: for shard(s) of a single machine, with no slaves
+    # slave: for all slaves
+    # both: for masters (they are slaves and masters at the same time)
+    if ($mysql_role == 'standalone') {
+        $semi_sync = 'off'
+    } elsif $mysql_role == 'master' {
+        $semi_sync = 'master'
+    } else {
+        $semi_sync = 'slave'
+    }
+
+    class { 'profile::mariadb::monitor::prometheus':
+        mysql_group => 'core',
+        mysql_shard => $shard,
+        mysql_role  => $mysql_role,
+        socket      => $socket,
+    }
+
+    class {'mariadb::packages_wmf':
+        package => $package,
+    }
+    class {'mariadb::service':
+        package  => $package,
+        # override not needed, default configuration changed on package
+        # override => "[Service]\nLimitNOFILE=200000",
+    }
+
+    # Read only forced on also for the masters of the primary datacenter
+    class { 'mariadb::config':
+        config           => 'role/mariadb/mysqld_config/production.my.cnf.erb',
+        basedir          => $basedir,
+        datadir          => $datadir,
+        tmpdir           => $tmpdir,
+        socket           => $socket,
+        p_s              => 'on',
+        ssl              => $ssl,
+        binlog_format    => $binlog_format,
+        semi_sync        => $semi_sync,
+        replication_role => $mysql_role,
+    }
+
+    class { 'profile::mariadb::grants::core': }
+    class { 'profile::mariadb::grants::production':
+        shard    => 'core',
+        prompt   => "PRODUCTION ${shard} ${mysql_role}",
+        password => $passwords::misc::scripts::mysql_root_pass,
+    }
+
+    $replication_is_critical = ($::mw_primary == $::site)
+    $contact_group = $replication_is_critical ? {
+        true  => 'dba',
+        false => 'admins',
+    }
+
+    mariadb::monitor_replication { [ $shard ]:
+        multisource   => false,
+        is_critical   => $replication_is_critical,
+        contact_group => $contact_group,
+        socket        => $socket,
+    }
+
+    $heartbeat_enabled = $mysql_role == 'master'
+    class { 'mariadb::heartbeat':
+        shard      => $shard,
+        datacenter => $::site,
+        enabled    => $heartbeat_enabled,
+        socket     => $socket,
+    }
+}
