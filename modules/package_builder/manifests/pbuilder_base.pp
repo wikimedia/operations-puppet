d10a3a4b717939ecfa90d2abee3392f571dceec1
Alexandros Kosiaris <akosiaris@wikimedia.org>
2018-02-20 15:21:41 +0000

    Revert "Use security mirrors in cowbuilder apt config"
    This also requires an http proxy to be set up as in our environment. Reverting to restore functionality while investigating use of --http-proxy
    This reverts commit 5436bf34c5e3c5bb6fe3e680c970fd706e768d23.
    
    Change-Id: I0b99771b450502c2a4c0a2be89cc5536ea57b313

--- modules/package_builder/manifests/pbuilder_base.pp
+++ modules/package_builder/manifests/pbuilder_base.pp
@@ -50,14 +50,6 @@ define package_builder::pbuilder_base(
 
     $cowdir = "${basepath}/base-${distribution}-${architecture}.cow"
 
-    # We need to be explicit with specifying only released distros since the
-    # other chroots supported (like unstable) are not present on security.debian.org
-    if ($distribution == 'stretch' or $distribution == 'jessie' or $distribution == 'buster') {
-        $security_apt = "--othermirror \"deb http://security.debian.org/ ${distribution}/updates ${components}\""
-    } else {
-        $security_apt = ''
-    }
-
     $command = "/usr/sbin/cowbuilder --create \
                         --mirror ${mirror} \
                         --distribution ${distribution} \
@@ -65,16 +57,15 @@ define package_builder::pbuilder_base(
                         --architecture ${architecture} \
                         --basepath \"${cowdir}\" \
                         --debootstrapopts --variant=buildd \
-                        ${security_apt} ${arg}"
+                        ${arg}"
 
     exec { "cowbuilder_init_${distribution}-${architecture}":
         command => $command,
         creates => $cowdir,
     }
 
-    $update_command = "/usr/sbin/cowbuilder --update --override-config \
+    $update_command = "/usr/sbin/cowbuilder --update \
                     --basepath \"${cowdir}\" \
-                    ${security_apt} \
                     >/dev/null 2>&1"
 
     cron { "cowbuilder_update_${distribution}-${architecture}":
