1f350ffd2b6f74e6268cacb5ac6cf13c4e42ba41
cpettet <rush@wikimedia.org>
2018-02-20 16:17:37 -0600

    openstack: eth1.2120 for labtestneutron200[12]
    Bug: T184209
    Change-Id: I466cfa866d2fde2113676c8da04b8191dd2e9d2a

--- modules/profile/manifests/openstack/labtestn/neutron/l3_agent.pp
+++ modules/profile/manifests/openstack/labtestn/neutron/l3_agent.pp
@@ -9,4 +9,12 @@ class profile::openstack::labtestn::neutron::l3_agent(
         network_flat_interface_vlan => $network_flat_interface_vlan,
     }
     contain '::profile::openstack::base::neutron::l3_agent'
+
+    interface::tagged { 'eth1.2120':
+        base_interface => 'eth1',
+        vlan_id        => '2120',
+        method         => 'manual',
+        up             => 'ip link set $IFACE up',
+        down           => 'ip link set $IFACE down',
+    }
 }
