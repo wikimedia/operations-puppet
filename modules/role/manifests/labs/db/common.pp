e2df6a7baa0df430ea076b0590c2c809eb25a72c
Bryan Davis <bd808@wikimedia.org>
2018-02-20 22:07:42 -0700

    labsdb: Remove obsolete mediawiki-config submodule
    The docroot/wikimedia.org/WikipediaMobileFirefoxOS submodule has been
    removed from the upstream git repo, but live git checkouts do not deal
    well with submodule removal. This has left the files locally on the
    labsdb hosts which is blocking further cleanups in the
    operations/mediawiki-config.git repo. This change attempts to have
    Puppet clean up both the working copy and the git index to match what
    a fresh clone would produce.
    
    Once this has been run across the cluster it can be reverted.
    
    Bug: T187850
    Change-Id: Ieb1a25f182efbb2b0801bc4c19a75caabd754009

--- modules/role/manifests/labs/db/common.pp
+++ modules/role/manifests/labs/db/common.pp
@@ -5,9 +5,26 @@ class role::labs::db::common {
         'python3-pymysql',
     )
 
+    $mwconfig = '/usr/local/lib/mediawiki-config'
     git::clone { 'operations/mediawiki-config':
         ensure             => 'latest',
-        directory          => '/usr/local/lib/mediawiki-config',
+        directory          => $mwconfig,
         recurse_submodules => true,
     }
+
+    # T187850: clean up old submodule junk
+    # TODO: remove this after it has run everywhere
+    $firefoxos = 'docroot/wikimedia.org/WikipediaMobileFirefoxOS'
+    file { [
+        "${mwconfig}/${firefoxos}",
+        "${mwconfig}/.git/modules/${firefoxos}",
+    ]:
+        ensure  => 'absent',
+        recurse => true,
+        purge   => true,
+        force   => true,
+        backup  => false,
+        require => Git::Clone['operations/mediawiki-config'],
+    }
+
 }
