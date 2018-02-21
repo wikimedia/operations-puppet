a04fae8c0bb99b14ad0d843dff33144f43448a84
Riccardo Coccioli <rcoccioli@wikimedia.org>
2018-02-22 00:06:58 +0100

    Icinga: add sync check for MW config on etcd
    * Add a systemd unit on the Icinga role that every 30s check and updates
      (if needed) a local file with the maximum modifiedIndex as reported by
      etcd for the mediawiki-config directory.
    * Add an Icinga check to verify that the maximum modifiedIndex of a
      given MediaWiki host, as exposed by siteinfo, is in sync with etcd.
    
    Bug: T182597
    Change-Id: I8b790669901c08fcd98337ce4c31044e1f11ffe9

---
+++ modules/icinga/manifests/monitor/etcd_mw_config.pp
@@ -0,0 +1,20 @@
+# == Class: icinga::monitor::mw_etcd_config
+#
+# Monitor the last modified intex in Etcd for MediaWiki config
+#
+class icinga::monitor::etcd_mw_config {
+    file { '/usr/local/sbin/update-etcd-mw-config-lastindex':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0640',
+        source => template('icinga/update-etcd-mw-config-lastindex.erb'),
+    }
+
+    systemd::unit { 'update-etcd-mw-config-lastindex':
+        ensure  => present,
+        restart => true,
+        source  => 'puppet:///modules/icinga/update-etcd-mw-config-lastindex.service',
+        require => File['/usr/local/sbin/update-etcd-mw-config-lastindex']
+    }
+}
