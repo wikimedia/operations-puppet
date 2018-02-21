e7e9cff876828c5aa0f17f610df4ae8e0c47136f
Ottomata <aotto@wikimedia.org>
2018-02-21 09:58:01 -0500

    Refactor kafkatee module to support multi instance
    Tested in Cloud VPS.  This will require manually removing single
    instance configs on oxygen and rhenium.
    
    Bug: T187890
    Change-Id: I41d46cc9dca00f5b7c682623b2b87f35de668e3b

---
+++ modules/role/manifests/logging/webrequest/ops.pp
@@ -0,0 +1,8 @@
+# == Class role::logging::kafkatee::webrequest::ops
+# Includes output filters useful for operational debugging.
+#
+class role::logging::webrequest::ops {
+    include ::standard
+    include ::profile::base::firewall
+    include ::profile::kafkatee::webrequest::ops
+}
