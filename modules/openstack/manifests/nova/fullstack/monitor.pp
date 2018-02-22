2a4e6f28edf80fae07f58e38dc8ac518cb4d3da2
cpettet <rush@wikimedia.org>
2018-02-22 16:00:01 -0600

    openstack: nova-fullstack alert wmcs-team
    Bug: T178405
    Change-Id: I34559c62e32cdcda73a1f106fa442ad0fdec2848

--- modules/openstack/manifests/nova/fullstack/monitor.pp
+++ modules/openstack/manifests/nova/fullstack/monitor.pp
@@ -1,6 +1,8 @@
 class openstack::nova::fullstack::monitor {
+
     nrpe::monitor_service { 'nova-fullstack':
         description  => 'nova instance creation test',
         nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1: -C python -a nova-fullstack',
+        contact_group => 'wmcs-team,admins',
     }
 }
