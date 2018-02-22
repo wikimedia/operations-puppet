1c4ed40c72f1441f7da60011bd569416a9b35c0e
Manuel Arostegui <marostegui@wikimedia.org>
2018-02-22 07:43:36 +0100

    tendril.pp: Fix typo
    Bug: T184704
    Change-Id: Iaa776c91899f64daef5743d28570b782a0a475df

--- modules/role/manifests/mariadb/misc/tendril.pp
+++ modules/role/manifests/mariadb/misc/tendril.pp
@@ -6,7 +6,7 @@ class role::mariadb::misc::tendril {
     }
 
     include ::standard
-    include ::profile::base::firewal
+    include ::profile::base::firewall
     include ::profile::mariadb::ferm
 
     include ::profile::mariadb::misc::tendril
