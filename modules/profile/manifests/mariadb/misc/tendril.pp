0699168ce0cd2651e43eb89a98c208d6054cbe63
Jaime Crespo <jcrespo@wikimedia.org>
2018-02-22 15:18:02 +0100

    tendril: MariaDB fixes and tunings
    * Increase buffer size for increased performance
    * Explicit plugin dependencies
    * Fix basedir path
    * Install libodbc1
    
    Bug: T184704
    Change-Id: I466ee79e769f8113bc9a485e9f63f5e2b96df0b0

--- modules/profile/manifests/mariadb/misc/tendril.pp
+++ modules/profile/manifests/mariadb/misc/tendril.pp
@@ -3,6 +3,8 @@ class profile::mariadb::misc::tendril {
 
     include mariadb::packages_wmf
 
+    require_package('libodbc1') # hack to fix CONNECT dependency
+
     include ::profile::mariadb::monitor::dba
     include passwords::misc::scripts
 
@@ -15,6 +17,7 @@ class profile::mariadb::misc::tendril {
     class { 'mariadb::config':
         config  => 'profile/mariadb/mysqld_config/tendril.my.cnf.erb',
         datadir => '/srv/sqldata',
+        basedir => '/opt/wmf-mariadb101',
         tmpdir  => '/srv/tmp',
         ssl     => 'puppet-cert',
     }
