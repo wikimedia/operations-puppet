97e324ca354c14364afe1e8f49d3b2a9217106a4
andrewbogott <abogott@wikimedia.org>
2018-02-22 10:10:48 -0600

    labweb nutcracker: further attempt to pass in memcached_pools correctly
    Bug: T187506
    Change-Id: I1b611c80e83a269f93cd6c72880dce6246b000bc

--- modules/profile/manifests/openstack/base/nutcracker.pp
+++ modules/profile/manifests/openstack/base/nutcracker.pp
@@ -6,10 +6,23 @@ class profile::openstack::base::nutcracker(
     $labweb_hosts = hiera('profile::openstack::base::labweb_hosts'),
 ) {
     $labweb_ips = $labweb_hosts.map |$host| { ipresolve($host, 4) }
-    $memcached_pools = $labweb_ips.map |$ip| { "${ip}:11000:1" }
+    $memcached_servers = $labweb_ips.map |$ip| { "${ip}:11000:1" }
+
+    $memcached_pools = {
+        auto_eject_hosts     => true,
+        distribution         => 'ketama',
+        hash                 => 'md5',
+        listen               => '127.0.0.1:11212',
+        preconnect           => true,
+        server_connections   => 1,
+        server_failure_limit => 3,
+        server_retry_timeout => to_milliseconds('30s'),
+        timeout              => 250,
+        servers              => $memcached_servers,
+    }
 
     class {'::profile::nutcracker':
-        redis_pools     => [],
+        redis_pools     => "",
         memcached_pools => $memcached_pools,
         monitor_port    => 0,
     }
