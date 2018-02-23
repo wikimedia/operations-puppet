772c27f576c1b9d1466c780dba3849d9fab59f00
cpettet <rush@wikimedia.org>
2018-02-23 13:15:08 -0600

    openstack: monitor nova-scheduler as critical
    No instance scheduling works without this.
    
    Bug: T178405
    Change-Id: Ic39e2923ee5e4391052d30631abbc501fc98e08c

--- modules/openstack/manifests/nova/scheduler/monitor.pp
+++ modules/openstack/manifests/nova/scheduler/monitor.pp
@@ -2,6 +2,8 @@
 # particular instance should run
 class openstack::nova::scheduler::monitor(
     $active,
+    $critical=false,
+    $contact_groups='admins',
 ){
 
     # monitoring::service doesn't take a bool
@@ -14,7 +16,9 @@ class openstack::nova::scheduler::monitor(
 
     nrpe::monitor_service { 'check_nova_scheduler_process':
         ensure       => $ensure,
+        critical     => $critical,
         description  => 'nova-scheduler process',
         nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/nova-scheduler'",
+        contact_group => $contact_groups,
     }
 }
