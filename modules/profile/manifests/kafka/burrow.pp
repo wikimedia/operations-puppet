f95357e3034d2553201dcab7ef8db06d7a96c968
elukey <ltoscano@wikimedia.org>
2018-02-23 15:14:04 +0100

    role::webserver_misc_apps: refactor kafka Burrow configuration
    Change-Id: I019c7088205215ac19f25a2be91acad41db2aa26

--- modules/profile/manifests/kafka/burrow.pp
+++ modules/profile/manifests/kafka/burrow.pp
@@ -3,13 +3,18 @@
 # Consumer offset lag monitoring tool template for a generic Kafka cluster
 #
 define profile::kafka::burrow(
-    $http_port,
-    $consumer_groups = undef,
+    $prometheus_nodes,
+    $monitoring_config,
     $to_emails = [],
 ) {
     $config = kafka_config($title)
     $smtp_server = 'mx1001.wikimedia.org'
     $kafka_cluster_name = $config['name']
+
+    $consumer_groups = monitoring_config[$title]['consumer_groups']
+    $burrow_http_port = monitoring_config[$title]['burrow_port']
+    $prometheus_burrow_http_port = monitoring_config[$title]['burrow_exporter_port']
+    $to_emails = monitoring_config[$title]['to_emails']
 
     burrow { $kafka_cluster_name:
         zookeeper_hosts    => $config['zookeeper']['hosts'],
@@ -20,14 +25,20 @@ define profile::kafka::burrow(
         from_email         => "burrow@${::fqdn}",
         to_emails          => $to_emails,
         lagcheck_intervals => 100,
-        httpserver_port    => $http_port,
+        httpserver_port    => $burrow_http_port,
         consumer_groups    => $consumer_groups,
+    }
+
+    profile::prometheus::burrow_exporter { $kafka_cluster_name:
+        burrow_addr      => "localhost:${burrow_http_port}",
+        port             => $prometheus_burrow_http_port,
+        prometheus_nodes => $prometheus_nodes,
     }
 
     # Burrow offers a HTTP REST API
     ferm::service { "burrow-${kafka_cluster_name}":
         proto  => 'tcp',
-        port   => $http_port,
+        port   => $burrow_http_port,
         srange => '$DOMAIN_NETWORKS',
     }
 }
