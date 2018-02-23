5c5610301aca9e32f95d631f25161f51bb487999
andrewbogott <abogott@wikimedia.org>
2018-02-23 00:48:55 -0600

    ferm_wmcs: fix copy/paste error
    Change-Id: I98cc76a058527a36ad01e7d547b9cad21c2a2c15

--- modules/profile/manifests/mariadb/ferm_wmcs.pp
+++ modules/profile/manifests/mariadb/ferm_wmcs.pp
@@ -8,7 +8,7 @@ class profile::mariadb::ferm_wmcs(
     $designate_host = hiera('profile::openstack::main::designate_host'),
     $designate_host_standby = hiera('profile::openstack::main::designate_host_standby'),
     $horizon_host = hiera('profile::openstack::main::horizon_host'),
-    $labweb_hosts = hiera('profile::openstack::labtestn::labweb_hosts'),
+    $labweb_hosts = hiera('profile::openstack::main::labweb_hosts'),
     $osm_host = hiera('profile::openstack::main::osm_host'),
     ) {
 
