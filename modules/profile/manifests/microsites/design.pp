b53b674e556fe2efcd99083b6bbc82371db27706
Daniel Zahn <dzahn@wikimedia.org>
2018-02-23 17:54:30 -0800

    microsites: profile to setup design.wikimedia.org
    Modeled after the existing setup for research.wikimedia.org,
    mostly same thing for design.wikimedia.org now as well.
    
    Will have a follow-up to add second repo and more Apache config
    but this is the basics to start with.
    
    Bug: T185282
    Change-Id: I4d5070e9faf9b2e04cef5807ecc8eefebec7813e

---
+++ modules/profile/manifests/microsites/design.pp
@@ -0,0 +1,23 @@
+# https://design.wikimedia.org (T185282)
+class profile::microsites::design(
+  $server_name = hiera('profile::microsites::design::server_name'),
+  $server_admin = hiera('profile::microsites::design::server_admin'),
+) {
+
+    httpd::site { 'design.wikimedia.org':
+        content => template('profile/design/apache-design.wikimedia.org.erb'),
+    }
+
+    ensure_resource('file', '/srv/org', {'ensure' => 'directory' })
+    ensure_resource('file', '/srv/org/wikimedia', {'ensure' => 'directory' })
+    ensure_resource('file', '/srv/org/wikimedia/design', {'ensure' => 'directory' })
+
+    # git::clone { 'FIXME':
+    #   ensure    => 'latest',
+    #   source    => 'gerrit',
+    #   directory => '/srv/org/wikimedia/design',
+    #   branch    => 'master',
+    # }
+
+}
+
