faab5e0479d54c8dd16ab65b8605dc802abed19e
andrewbogott <abogott@wikimedia.org>
2018-02-25 00:10:49 -0600

    labweb: include mediawiki profiles
    Bug: T168470
    Change-Id: I0982866aa7a56e6e546f19a234d2775b6aca9497

---
+++ modules/profile/manifests/openstack/base/wikitech/web.pp
@@ -0,0 +1,97 @@
+class profile::openstack::base::wikitech::web(
+    $osm_host = hiera('profile::openstack::base::wikitech::web::osm_host'),
+    $nova_controller = hiera('profile::openstack::base::nova_controller'),
+    $webserver_hostname_aliases = hiera('profile::openstack::base::wikitech::webserver_hostname_aliases'),
+    $wikidb = hiera('profile::openstack::base::wikitech::db_name'),
+    $wikitech_nova_ldap_proxyagent_pass = hiera('profile::openstack::base::ldap_proxyuser_pass'),
+    $wikitech_nova_ldap_user_pass = hiera('profile::openstack::base::ldap_user_pass'),
+    ) {
+
+    # What follows is a modified copy/paste of ::mediawiki.
+    #  modified to avoid pulling in the webserver bits which conflict
+    #  with our httpd-using horizon and striker web conf.
+
+    include ::mediawiki::cgroup
+    include ::mediawiki::packages
+    include ::mediawiki::scap
+    include ::mediawiki::users
+    #include ::mediawiki::syslog
+    #include ::mediawiki::php
+    #include ::mediawiki::mwrepl
+
+    #include ::mediawiki::hhvm
+
+    # This profile is used to contain the convert command of imagemagick using
+    # firejail Profiles specific to the image/video scalers are handled via
+    # mediawiki::firejail
+    file { '/etc/firejail/mediawiki-imagemagick.profile':
+        source  => 'puppet:///modules/mediawiki/mediawiki-imagemagick.profile',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['firejail'],
+    }
+
+    file { '/usr/local/bin/mediawiki-firejail-convert':
+        source => 'puppet:///modules/mediawiki/mediawiki-firejail-convert',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+
+    file { '/etc/firejail/mediawiki-converters.profile':
+        source => 'puppet:///modules/mediawiki/mediawiki-converters.profile',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+    }
+
+    file { '/usr/local/bin/mediawiki-firejail-ghostscript':
+        source => 'puppet:///modules/mediawiki/mediawiki-firejail-ghostscript',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+
+    # /var/log/mediawiki contains log files for the MediaWiki jobrunner
+    # and for various periodic jobs that are managed by cron.
+    file { '/var/log/mediawiki':
+        ensure => directory,
+        owner  => $::mediawiki::users::web,
+        group  => 'wikidev',
+        mode   => '0644',
+    }
+    # -- end copy/paste of ::mediawiki --
+
+
+    class {'::mediawiki::multimedia':}
+    class {'::profile::backup::host':}
+
+    # For Math extensions file (T126628)
+    file { '/srv/math-images':
+        ensure => 'directory',
+        owner  => 'www-data',
+        group  => 'www-data',
+        mode   => '0755',
+    }
+
+    class { '::openstack::wikitech::web':
+        webserver_hostname                 => $osm_host,
+        webserver_hostname_aliases         => $webserver_hostname_aliases,
+        wikidb                             => $wikidb,
+        wikitech_nova_ldap_proxyagent_pass => $wikitech_nova_ldap_proxyagent_pass,
+        wikitech_nova_ldap_user_pass       => $wikitech_nova_ldap_user_pass,
+    }
+
+    ferm::service { 'wikitech_http':
+        proto  => 'tcp',
+        port   => '80',
+        srange => '$CACHE_MISC',
+    }
+
+    ferm::service { 'deployment-ssh':
+        proto  => 'tcp',
+        port   => '22',
+        srange => '$DEPLOYMENT_HOSTS',
+    }
+}
---
+++ modules/profile/manifests/openstack/main/wikitech/web.pp
@@ -0,0 +1,18 @@
+class profile::openstack::main::wikitech::web(
+    $osm_host = hiera('profile::openstack::main::wikitech::web::osm_host'),
+    $nova_controller = hiera('profile::openstack::main::nova_controller'),
+    $webserver_hostname_aliases = hiera('profile::openstack::main::wikitech::webserver_hostname_aliases'),
+    $wikidb = hiera('profile::openstack::main::wikitech::db_name'),
+    $wikitech_nova_ldap_proxyagent_pass = hiera('profile::openstack::base::ldap_proxyuser_pass'),
+    $wikitech_nova_ldap_user_pass = hiera('profile::openstack::main::ldap_user_pass'),
+    ) {
+
+    class {'profile::openstack::base::wikitech::web':
+        osm_host                           => $osm_host,
+        nova_controller                    => $nova_controller,
+        webserver_hostname_aliases         => $webserver_hostname_aliases,
+        wikidb                             => $wikidb,
+        wikitech_nova_ldap_proxyagent_pass => $wikitech_nova_ldap_proxyagent_pass,
+        wikitech_nova_ldap_user_pass       => $wikitech_nova_ldap_user_pass,
+    }
+}
