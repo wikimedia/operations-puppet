29cffeb03b3b59961e057371e721ba941ae0139e
Brandon Black <bblack@wikimedia.org>
2018-02-26 14:27:21 +0000

    interface::rps: change IRQ count without reboot
    Change-Id: I5c09af2d19cbab2d5578cf10ae42a0e712908832

--- modules/interface/manifests/rps/modparams.pp
+++ modules/interface/manifests/rps/modparams.pp
@@ -1,6 +1,8 @@
 class interface::rps::modparams {
     include initramfs
 
+    $iface = $facts['interface_primary']
+
     if $::numa_networking != 'off' {
         # note this assumes if bnx2x queue counts matter at all, that the
         # primary interface is bnx2x.  This is true for current cases, but may
@@ -9,14 +11,28 @@ class interface::rps::modparams {
         # There's no avoiding the fact that this setting is global to the
         # driver, and therefore can't handle differing IRQ counts for different
         # bnx2x interfaces.  Again, not presently an issue...
-        $num_queues = size($facts['numa']['device_to_htset'][$facts['interface_primary']])
+        $num_queues = size($facts['numa']['device_to_htset'][$iface])
     }
     else {
         $num_queues = $facts['physicalcorecount']
     }
 
+    # This sets it at boot time for future boots via driver param
     file { '/etc/modprobe.d/rps.conf':
         content => template("${module_name}/rps.conf.erb"),
         notify  => Exec['update-initramfs']
+    }
+
+    # This sets it at runtime if it's incorrect (first run or change of
+    # numa_networking setting, before the above can take effect on next boot).
+    # Changing at runtime *will* blip the interface.  This shouldn't be an
+    # issue for first-run scenarios, but might require a depool when changing
+    # $numa_networking on live production hosts.
+    exec { "ethtool_rss_combined_channels":
+        path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+        command => "ethtool -L ${iface} combined ${num_queues}",
+        unless  => "test $(ethtool -l ${iface} | tail -4 | awk '/Combined:/ { print \$2 }') = '${num_queues}'",
+        require => Package['ethtool'],
+        before  => Exec["rps-${iface}"],
     }
 }
