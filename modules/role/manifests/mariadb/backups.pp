fac4c22503cc162d16b92cc477e83f462a065dc3
Jaime Crespo <jcrespo@wikimedia.org>
2018-02-27 16:34:15 +0100

    mariadb: Set up es2001 as the temporary backup target
    dbstore2001 will be degraded back to only being a multi-source
    host, source of such backups.
    
    Configure the backup options on the template:
    profile/mariadb/backup-{$site}.cnf.erb
    
    Remove redundant roles.
    
    Bug: T184696
    Change-Id: I2b160d16d7e7766e17bcc18d5096aeda9df3574a

--- modules/role/manifests/mariadb/backups.pp
+++ modules/role/manifests/mariadb/backups.pp
@@ -1,9 +1,9 @@
-# Create mydumper logical backups using the dump_shards.sh hosts
+# Create mydumper logical backups using the dump_sections.py script
 # of all production core (s*, x*) and misc (m*) hosts.
 # Do that using a cron job on all backup hosts, all datacenters.
 # If we are on the active datacenter, also send the backups to the
 # long-term storage.
-class role::mariadb::backup_mydumper {
+class role::mariadb::backups {
     include ::profile::backup::host
     include ::profile::mariadb::backup::mydumper
     include ::profile::mariadb::backup::bacula
