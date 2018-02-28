5bb6cde51ebce648980b2de5c4edc89d899f2757
cpettet <rush@wikimedia.org>
2018-02-28 07:27:48 -0600

    rabbitmq: move management to plugins manifest
    Jessie expects 'home' in env, and use regular
    unless with a service notify to force plugin
    to take effect.
    
    Bug: T188266
    Change-Id: I9f010d8b42c420c6ecfc9c0bbb9e7542ece2ad5b

---
+++ modules/rabbitmq/manifests/plugins.pp
@@ -0,0 +1,14 @@
+class rabbitmq::plugins {
+
+    include rabbitmq
+    # https://www.rabbitmq.com/management.html
+    # Needed for https://www.rabbitmq.com/management-cli.html
+    # rabbitmq-plugins -E list | egrep '\[E\*]\srabbitmq_management
+    exec { 'enable_management_plugin':
+        environment => 'HOME=/var/lib/rabbitmq/',
+        command   => '/usr/sbin/rabbitmq-plugins enable rabbitmq_management',
+        unless    => '/usr/sbin/rabbitmq-plugins -E list | grep rabbitmq_management',
+        logoutput => true,
+        notify    => Service['rabbitmq-server'],
+    }
+}
