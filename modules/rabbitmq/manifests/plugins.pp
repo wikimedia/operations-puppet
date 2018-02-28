c95e809c82f0d39fbcfabd7216556f680ad81371
cpettet <rush@wikimedia.org>
2018-02-28 07:27:48 -0600

    rabbitmq: move management to plugins manifest
    Needs to follow complete setup.
    
    Change-Id: I9f010d8b42c420c6ecfc9c0bbb9e7542ece2ad5b

---
+++ modules/rabbitmq/manifests/plugins.pp
@@ -0,0 +1,14 @@
+class rabbitmq::plugins {
+
+    require rabbitmq
+
+    # this appears to be idempotent
+    # https://www.rabbitmq.com/management.html
+    # Needed for https://www.rabbitmq.com/management-cli.html
+    # rabbitmq-plugins -E list | egrep '\[E\*]\srabbitmq_management
+    exec { 'enable_management_plugin':
+        command   => '/usr/sbin/rabbitmq-plugins enable rabbitmq_management',
+        unless    => '/usr/sbin/rabbitmq-plugins -E list | egrep "\[E\*]\srabbitmq_management"',
+        logoutput => true,
+    }
+}
