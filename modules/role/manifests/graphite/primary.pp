a8207c71e80da82b06ebe12b822a86391277ede6
Ian Marlier <imarlier@wikimedia.org>
2018-02-27 22:57:57 -0500

    coal: Process from Kafka instead of from ZMQ
    Pulls in NavTiming events from Kafka instead of from ZMQ.  Tries
    to improve on the prior handling by, eg, not disposing of everything
    that was in the queue prior to the current 5 minute window.
    
    Bug: T110903
    Change-Id: Icdb0e7c31c0c012ea523fea7ceabc103017a868a

--- modules/role/manifests/graphite/primary.pp
+++ modules/role/manifests/graphite/primary.pp
@@ -1,5 +1,6 @@
 # graphite production server with performance web site
 class role::graphite::primary {
     include ::role::graphite::production
+    include ::role::performance::coal
     include ::role::performance::site
 }
---
+++ modules/role/manifests/performance/coal.pp
@@ -0,0 +1,22 @@
+# This role installs coal, which is a utility built/maintained by
+# the performance team in order to collect decent median values
+# for incoming RUM performance data.
+#
+#   Contact: performance-team@wikimedia.org
+#
+
+class role::performance::coal {
+
+    # Where coal whisper files are located
+    $coal_whisper_dir = hiera('performance::coal::coal_whisper_dir')
+
+    # Consumes from eventlogging, on the analytics kafka cluster
+    $kafka_config  = kafka_config('jumbo-eqiad')
+    $kafka_brokers = $kafka_config['brokers']['string']
+
+    # Additional vars have defaults set in modules/coal/init.pp
+    class { '::coal':
+        kafka_brokers => $kafka_brokers,
+        whisper_dir   => $coal_whisper_dir
+    }
+}
