cf8999b84d3b631168376f7c0378c4e1ad1358ad
andrewbogott <abogott@wikimedia.org>
2018-03-01 01:14:25 -0600

    labweb: include standard vhost snippets from mediawiki::web::sites
    Change-Id: I170ef306482ffd9deade80421db5942d509002d8

--- modules/profile/manifests/openstack/base/wikitech/web.pp
+++ modules/profile/manifests/openstack/base/wikitech/web.pp
@@ -11,6 +11,39 @@ class profile::openstack::base::wikitech::web(
     class {'::mediawiki::multimedia':}
     class {'::profile::backup::host':}
 
+
+    # common code snippets that are included in the virtualhosts.
+    # from ::mediawiki::web::sites
+    file { '/etc/apache2/sites-enabled/wikimedia-common.incl':
+        ensure  => present,
+        content => template('mediawiki/apache/sites/wikimedia-common.incl.erb'),
+        before  => Service['apache2'],
+    }
+
+    file { '/etc/apache2/sites-enabled/wikimedia-legacy.incl':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/apache/sites/wikimedia-legacy.incl',
+        before => Service['apache2'],
+    }
+
+    file { '/etc/apache2/sites-enabled/public-wiki-rewrites.incl':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/apache/sites/public-wiki-rewrites.incl',
+        before => Service['apache2'],
+    }
+
+    file { '/etc/apache2/sites-enabled/api-rewrites.incl':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/apache/sites/api-rewrites.incl',
+        before => Service['apache2'],
+    }
+
+    file { '/etc/apache2/sites-enabled/wikidata-uris.incl':
+        ensure => present,
+        source => 'puppet:///modules/mediawiki/apache/sites/wikidata-uris.incl',
+        before => Service['apache2'],
+    }
+
     # For Math extensions file (T126628)
     file { '/srv/math-images':
         ensure => 'directory',
