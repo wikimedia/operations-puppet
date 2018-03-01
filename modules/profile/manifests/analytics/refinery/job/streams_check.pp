b68ccf72dfa884dfa9556a33f47552a777553700
Ottomata <aotto@wikimedia.org>
2018-03-01 14:44:47 -0500

    Parameterize kafka_cluster_name for streams_check job
    This will allow us to switch BannerImpression Spark streaming job to kafka jumbo on Tuesday.
    
    Bug: T185136
    Change-Id: Ib865ffa8920042ee4ba76d793e736e1380b32801

--- modules/profile/manifests/analytics/refinery/job/streams_check.pp
+++ modules/profile/manifests/analytics/refinery/job/streams_check.pp
@@ -5,9 +5,13 @@
 # take care of alarming, that needs to be done separately.
 #
 
-class profile::analytics::refinery::job::streams_check {
+class profile::analytics::refinery::job::streams_check(
+    $kafka_cluster_name = hiera('profile::analytics::refinery:job::streams_check', 'analytics')
+) {
     require ::profile::analytics::refinery
 
+    $kafka_config = kafka_config($kafka_cluster_name)
+    $kafka_brokers_string = $kafka_config['brokers']['string']
     # Shortcut var to DRY up cron commands.
     $refinery_path = $profile::analytics::refinery::path
 
@@ -27,7 +31,7 @@ class profile::analytics::refinery::job::streams_check {
     $job_name = 'BannerImpressionsStream'
 
     # No log needed as job runs in cluster mode
-    $command = "PYTHONPATH=${refinery_path}/python ${refinery_path}/bin/is-yarn-app-running ${job_name} || /usr/bin/spark2-submit --master yarn --deploy-mode cluster --queue production --conf spark.dynamicAllocation.enabled=false --driver-memory ${spark_driver_memory} --executor-memory ${spark_executor_memory} --executor-cores ${spark_executor_cores} --num-executors ${spark_num_executors} --class org.wikimedia.analytics.refinery.job.druid.BannerImpressionsStream --name ${job_name} ${refinery_job_jar} --druid-indexing-segment-granularity ${druid_segment_gran} --druid-indexing-window-period ${tranq_window_period} --batch-duration-seconds ${batch_duration_secs} > /dev/null 2>&1"
+    $command = "PYTHONPATH=${refinery_path}/python ${refinery_path}/bin/is-yarn-app-running ${job_name} || /usr/bin/spark2-submit --master yarn --deploy-mode cluster --queue production --conf spark.dynamicAllocation.enabled=false --driver-memory ${spark_driver_memory} --executor-memory ${spark_executor_memory} --executor-cores ${spark_executor_cores} --num-executors ${spark_num_executors} --class org.wikimedia.analytics.refinery.job.druid.BannerImpressionsStream --name ${job_name} ${refinery_job_jar} --kafka-brokers ${kafka_brokers_string} --druid-indexing-segment-granularity ${druid_segment_gran} --druid-indexing-window-period ${tranq_window_period} --batch-duration-seconds ${batch_duration_secs} > /dev/null 2>&1"
 
     # This checks for banner streaming job running in Yarn, and relaunches it if needed.
     cron { 'refinery-relaunch-banner-streaming':
