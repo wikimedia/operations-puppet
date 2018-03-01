a9b00ba80abd290fdc4c648ba5a7885d9eb8f6eb
Daniel Zahn <dzahn@wikimedia.org>
2018-03-01 14:55:04 -0800

    openstack:labtest:web: add some php7/stretch support
    Change-Id: I15479e8ddb0fa7dc3a49d2580590f746c1fcd2c7

--- modules/role/manifests/wmcs/openstack/labtest/web.pp
+++ modules/role/manifests/wmcs/openstack/labtest/web.pp
@@ -9,7 +9,13 @@ class role::wmcs::openstack::labtest::web {
     include ::profile::openstack::labtest::horizon::dashboard
     include ::profile::ldap::client::labs
 
+    if os_version('debian >= stretch') {
+        $php_module = 'php7.0'
+    } else {
+        $php_module = 'php5'
+    }
+
     class { '::httpd':
-        modules => ['alias', 'ssl', 'php5', 'rewrite', 'headers'],
+        modules => ['alias', 'ssl', $php_module, 'rewrite', 'headers'],
     }
 }
