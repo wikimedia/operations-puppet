dbbf570bf160246e96eff0d07f738b647d6e8f03
Aaron Schulz <aschulz@wikimedia.org>
2018-03-01 13:06:37 -0800

    [WIP] Add dynomite module and dynomite_wancache profile
    Change-Id: I6580add1492c5255ee232240433fb0bcc12fb92c

--- modules/role/manifests/mediawiki/common.pp
+++ modules/role/manifests/mediawiki/common.pp
@@ -3,4 +3,8 @@ class role::mediawiki::common {
     include ::profile::mediawiki::scap_proxy
     include ::profile::mediawiki::common
     include ::profile::mediawiki::nutcracker
+    # dynomite testing (T97562)
+    if $::realm == 'labs' {
+        include ::profile::mediawiki::dynomite_wancache
+    }
 }
