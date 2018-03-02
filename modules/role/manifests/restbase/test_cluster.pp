9a1345376ca978c7bd3e9c199ed69453a34a6b6b
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-03-02 11:27:12 +0100

    Decom restbase-test cluster and role
    Bug: T186755
    Bug: T187446
    Change-Id: I703711240af0faffd6afadb986918dcc657ba6f4

--- modules/role/manifests/restbase/test_cluster.pp
+++
@@ -1,8 +0,0 @@
-# == Class role::restbase::test_cluster
-#
-# Configures the restbase test cluster
-class role::restbase::test_cluster {
-    # Just includes base, no LVS etc.
-    include ::role::restbase::base
-    system::role { 'restbase': description => "Restbase-test (${::realm})" }
-}
