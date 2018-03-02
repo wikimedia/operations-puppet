733063f1ca7182c61852de67547bfcade5c077d8
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2018-03-02 17:52:31 +0100

    wdqs: remove diamond collectors which have been replaced by prometheus
    Change-Id: Ia92da6dead97cd7c3047d166ba82ac63a4dac932

--- modules/wdqs/manifests/monitor/blazegraph.pp
+++ modules/wdqs/manifests/monitor/blazegraph.pp
@@ -5,35 +5,8 @@
 class wdqs::monitor::blazegraph {
     require ::wdqs::service
 
-    require_package('python-dateutil')
-
     diamond::collector { 'Blazegraph':
-        settings => {
-            counters => [
-                '"/Query Engine/queryStartCount"',
-                '"/Query Engine/queryDoneCount"',
-                '"/Query Engine/queryErrorCount"',
-                '"/Query Engine/queriesPerSecond"',
-                '"/Query Engine/GeoSpatial/geoSpatialSearchRequests"',
-                '"/Journal/bytesReadPerSec"',
-                '"/Journal/bytesWrittenPerSec"',
-                '"/Journal/extent"',
-                '"/Journal/commitCount"',
-                '"/Journal/commit/totalCommitSecs"',
-                '"/Journal/commit/flushWriteSetSecs"',
-                '"/JVM/Memory/DirectBufferPool/default/bytesUsed"',
-                '"/JVM/Memory/Runtime Free Memory"',
-                '"/JVM/Memory/Runtime Max Memory"',
-                '"/JVM/Memory/Runtime Total Memory"',
-                '"/JVM/Memory/Garbage Collectors/G1 Old Generation/Collection Count"',
-                '"/JVM/Memory/Garbage Collectors/G1 Old Generation/Cumulative Collection Time"',
-                '"/JVM/Memory/Garbage Collectors/G1 Young Generation/Collection Count"',
-                '"/JVM/Memory/Garbage Collectors/G1 Young Generation/Cumulative Collection Time"',
-            ],
-        },
-        source   => 'puppet:///modules/wdqs/monitor/blazegraph.py',
-        require  => Package['python-dateutil'],
+        ensure => absent,
     }
 
-    # TODO: add monitoring of the http and https endpoints, and of the service
 }
