d686715ecdfb53f6c48a3060547cb9eba8b7c914
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2018-03-05 12:19:42 -0800

    dumps: Refactor profiles and hierakeys in web/
    This patch attempts to cleanup profile/dumps/web
    which currently has both generation and distribution
    related profiles, and split the profiles and hiera keys
    up. It leaves in place any profiles that will be deprecated
    after the migration to labstore1006|7 for all dumps
    distribution needs
    
    * Moves cleanup and datasets related profiles applied in
    distribution servers to distribution/datasets
    * Move all distribution related hierakeys under distribution/
    * Replace labstore1006|7 host specific hiera config with common
    profile level config
    * Split cleanup and dumpstatussync profiles for generation and
    distribution and apply to appropriate profiles
    
    Bug: T168486
    Change-Id: I71d9bb9653239873ebd641f59a7b8685ac41cf49

--- modules/profile/manifests/dumps/web/dumpsuser.pp
+++ modules/profile/manifests/dumps/web/dumpsuser.pp
@@ -1,3 +1,5 @@
+# Only used in labstore1003, can be deprecated once that
+# server stops serving dumps post migrating to labstore1006|7
 class profile::dumps::web::dumpsuser {
     class { '::dumpsuser': }
 }
