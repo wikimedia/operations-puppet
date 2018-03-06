c222582edb23527c5b21b36261e08a546e171258
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-03-06 12:54:15 +0100

    wmflib: port role and nuyaml to hiera3
    Introduce separate backends for hiera3, this makes it easy to have jessie and stretch puppet masters
    cohexist. Once all puppet masters are on stretch, we can drop the "3" suffix.
    
    This change incorporates/obsoletes I547d4ffe5 and I9866b2ae00
    
    Bug: T188623
    Change-Id: I05195420b987779b3e4f89fff76fb03a99dd5026

---
+++ modules/wmflib/lib/hiera/backend/nuyaml3_backend.rb
@@ -0,0 +1,223 @@
+# Nuyaml Hiera backend - the yaml backend with some sugar on top
+#
+# Based on the original yaml_backend from hiera distribution, any
+# modification/addition:
+# Author: Giuseppe Lavagetto <glavagetto@wikimedia.org>
+# Copyright  (c) 2014 Wikimedia Foundation
+#
+#
+# This backend allows some more flexibility over the vanilla yaml
+# backend, as path expansion in the lookup.
+#
+# == Private path
+#
+# If you define a 'private' data source in hiera, we will look up
+# in a data.yaml file in the data dir we've specified as the datadir
+# for a 'private' backend, or in the default datadir as a fallback.
+#
+# == Path expansion
+#
+# Any hierarchy named in the backend-configuration section
+# :expand_path be expanded when looking the file up on disk. This
+# allows both to have a more granular set of files, but also to avoid
+# unnecessary cache evictions for cached data.
+# === Example
+#
+# Say your hiera.yaml has defined
+#
+# :nuyaml:
+#   :expand_path:
+#     - module_data
+#
+# :hierarchy:
+#   - common
+#   - module_data
+#
+# then when searching hiera for say passwords::mysql::s1, hiera will
+# first load the #{datadir}/common.yaml file and search for
+# passwords::mysql::s1, then if not found, it will search for 's1'
+# inside the file #{datadir}/module_data/passwords/mysql.yaml
+#
+# Unless very small, all files should be split up like this.
+#
+# == Regexp matching
+#
+# As multiple hosts may correspond to the same rules/configs in a
+# large cluster, we allow to define a self-contained "regex.yaml" file
+# in your datadir, where each different class of servers may be
+# represented by a label and a corresponding regexp.
+#
+# === Example
+# Say you have a lookup for "cluster", and you have
+# "regex/%{hostname}" in your hierarchy; also, let's say that your
+# scope contains hostname = "web1001.local". So if your regex.yaml
+# file contains:
+#
+# databases:
+#   __regex: !ruby/regex '/db.*\.local/'
+#   cluster: db
+#
+# webservices:
+#   __regex: !ruby/regex '/^web.*\.local$/'
+#   cluster: www
+#
+# This will make it so that "cluster" will assume the value "www"
+# given the regex matches the "webservices" stanza
+#
+class Hiera
+  module Backend
+    # This naming is required by puppet.
+    class Nuyaml3_backend
+      def initialize(cache = nil)
+        require 'yaml'
+        @cache = cache || Filecache.new
+        config = Config[:nuyaml]
+        @expand_path = config[:expand_path] || []
+      end
+
+      def get_path(key, scope, source, context)
+        config_section = :nuyaml
+        # Special case: regex
+        if %r{^regex/}.match(source)
+          Hiera.debug("Regex match going on - using regex.yaml")
+          return Backend.datafile(config_section, scope, 'regex', "yaml")
+        end
+
+        # Special case: 'private' repository.
+        # We use a different datadir in this case.
+        # Example: private/common will search in the common source
+        # within the private datadir
+        if %r{private/(.*)} =~ source
+          config_section = :private
+          source = Regexp.last_match(1)
+        end
+
+        # Special case: 'secret' repository. This is practically labs only
+        # We use a different datadir in this case.
+        # Example: private/common will search in the common source
+        # within the private datadir
+        if %r{secret/(.*)} =~ source
+          config_section = :secret
+          source = Regexp.last_match(1)
+        end
+
+        Hiera.debug("The source is: #{source}")
+        # If the source is in the expand_path list, perform path
+        # expansion. This is thought to allow large codebases to live
+        # with fairly small yaml files as opposed to a very large one.
+        # Example:
+        # $apache::mpm::worker will be in common/apache/mpm.yaml
+        paths = @expand_path.map{ |x| Backend.parse_string(x, scope, {}, context) }
+        if paths.include? source
+          namespaces = key.gsub(/^::/, '').split('::')
+          namespaces.pop
+
+          unless namespaces.empty?
+            source += "/".concat(namespaces.join('/'))
+          end
+        end
+
+        Backend.datafile(config_section, scope, source, "yaml")
+      end
+
+      def plain_lookup(key, data, scope, context)
+          return nil unless data.include?(key)
+          Backend.parse_answer(data[key], scope, {}, context)
+      end
+
+      def regex_lookup(key, matchon, data, scope, context)
+        data.each do |label, datahash|
+          r = datahash["__regex"]
+          Hiera.debug("Scanning label #{label} for matches to '#{r}' in '#{matchon}' ")
+          next unless r.match(matchon)
+          Hiera.debug("Label #{label} matches; searching within it")
+          next unless datahash.include?(key)
+          return Backend.parse_answer(datahash[key], scope, {}, context)
+        end
+        return nil
+      rescue => detail
+        Hiera.debug(detail)
+        return nil
+      end
+
+      # Hiera 3 supports "segmented lookup" by splitting keys on dots, to allow looking up values
+      # inside nested structures. In this case reconstruct the segmented key into its original form
+      # and perform a lookup.
+      def lookup_with_segments(segments, scope, order_override, resolution_type, context)
+        Hiera.debug("Got a segmented key #{segments}")
+
+        lookup(segments.join('.'), scope, order_override, resolution_type, context)
+      end
+
+      def lookup(key, scope, order_override, resolution_type, context)
+        answer = nil
+
+        Hiera.debug("Looking up #{key}")
+
+        Backend.datasources(scope, order_override) do |source|
+          Hiera.debug("Loading info from #{source} for #{key}")
+
+          yamlfile = get_path(key, scope, source, context)
+
+          Hiera.debug("Searching for #{key} in #{yamlfile}")
+
+          next if yamlfile.nil?
+
+          Hiera.debug("Loading file #{yamlfile}")
+
+          next unless File.exist?(yamlfile)
+
+          data = @cache.read(yamlfile, Hash) do |content|
+            YAML.load(content)
+          end
+
+          next if data.nil?
+
+          if %r{regex/(.*)$} =~ source
+            matchto = Regexp.last_match(1)
+            new_answer = regex_lookup(key, matchto, data, scope, context)
+          else
+            new_answer = plain_lookup(key, data, scope, context)
+          end
+
+          next if new_answer.nil?
+          # Extra logging that we found the key. This can be outputted
+          # multiple times if the resolution type is array or hash but that
+          # should be expected as the logging will then tell the user ALL the
+          # places where the key is found.
+          Hiera.debug("Found #{key} in #{source}")
+
+          # for array resolution we just append to the array whatever
+          # we find, we then go onto the next file and keep adding to
+          # the array
+          #
+          # for priority searches we break after the first found data
+          # item
+
+          case resolution_type
+          when :array
+            raise Exception, "Hiera type mismatch: expected Array and got #{new_answer.class}" unless new_answer.kind_of?(Array) || new_answer.kind_of?(String)
+            answer ||= []
+            answer << new_answer
+          when :hash, Hash
+            raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
+            answer ||= {}
+            rt = resolution_type
+            # If we get :hash as a resolution_type, we will use the defaults.
+            rt = nil if resolution_type == :hash
+
+            answer = Backend.merge_answer(new_answer, answer, rt)
+          else
+            answer = new_answer
+            break
+          end
+        end
+        if answer.nil?
+          Hiera.debug("No answer! #{key}")
+          throw(:no_such_key)
+        end
+        answer
+      end
+    end
+  end
+end
---
+++ modules/wmflib/lib/hiera/backend/role3_backend.rb
@@ -0,0 +1,201 @@
+# Role-based Hiera backend
+#
+# Author: Giuseppe Lavagetto <glavagetto@wikimedia.org>
+# Copyright  (c) 2014 Wikimedia Foundation
+#
+#
+# This backend allows to group definitions based on the roles defined
+# at the node level using the 'role' keyword.
+# It allows searching in hierarchies that are based on the role
+# currently applied; this is very handy if you have group of hosts
+# whose configuration is dependent on their role rather than on other
+# facts like hostnames.
+#
+# == How this works
+#
+# Whenever you use the 'role' keyword in a node:
+#
+#   role cache::text
+#
+# Two things happen:
+# - the class role::cache::text gets included in the current node
+# - this gets registered in a global registry
+#
+# Whenever a hiera lookup is performed and the role backend is used,
+# the key is searched in all the hierarchies defined in the
+# :role_hierarchy.
+#
+# A typical hierarchy would be:
+# - "%{::environment}"
+# - common
+#
+# The path in which the files must be is as follows:
+# role/${hierarchy}/<path>.yaml
+# where <path> corresponds to the argument passed to the 'role'
+# keyword.
+#
+# === Example
+#
+# In site.pp:
+#
+# node /pinkunicorn.wikimedia.org/ {
+#      role foo::bar, fizzbuzz
+#      notice(hiera('admin::groups'))
+# }
+#
+# Give the hierarchy from before, the final hiera lookup will result
+# in searching admin::groups in two distinct hierarchies:
+#
+# role/production/foo/bar.yaml
+# role/common/foo/bar.yaml
+#
+# role/production/fizzbuzz.yaml
+# role/common/fizzbuzz.yaml
+#
+# The research will be performed for *both* hierarchies as a normal
+# hiera lookup; if a plain lookup is performed, the results of the two
+# searches will be compared; if they differ, an exception will be
+# thrown, so that conflicting directives will need manual resolution.
+#
+# === Things to pay attention to
+#
+# One big caveat: if you do use multiple times the role keyword, any
+# class included by the first role keyword is declared  would be
+# evaluated just after the first "role" has been called, thus
+# not having the full scope of both to search from; this could result
+# in unexpected behaviour so I advice _against_ using multiple role
+# keywords in a single node, if both roles include conflicting
+# classes.
+require 'yaml'
+class Hiera
+  module Backend
+    # This naming is required by puppet.
+    class Role3_backend
+      def initialize(cache = nil)
+        @cache = cache || Filecache.new
+      end
+
+      def get_path(_key, role, source, scope)
+        config_section = :role
+
+        # Special case: 'private' repository.
+        # We use a different datadir in this case.
+        # Example: private/common will search in the role/common source
+        # within the private datadir
+        if %r{private/(.*)} =~ source
+          config_section = :private
+          source = Regexp.last_match(1)
+        end
+
+        # Variables for role::foo::bar will be searched in:
+        # role/foo/bar.yaml
+        # role/$::site/foo/bar.yaml
+        # etc, depending on your hierarchy
+        path = role.split('::').join('/')
+        src = "role/#{source}/#{path}"
+
+        # Use the datadir for the 'role' section of the config
+        Backend.datafile(config_section, scope, src, "yaml")
+      end
+
+      def merge_answer(new_answer, answer, resolution_type)
+        case resolution_type
+        when :array
+          raise Exception, "Hiera type mismatch: expected Array and got #{new_answer.class}" unless new_answer.kind_of?(Array) || new_answer.kind_of?(String)
+          answer ||= []
+          answer << new_answer
+        when :hash, Hash
+          raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
+          rt = resolution_type
+          # If we get :hash as a resolution_type, we will use the defaults.
+          rt = nil if resolution_type == :hash
+
+          answer ||= {}
+          answer = Backend.merge_answer(new_answer, answer, rt)
+        else
+          answer = new_answer
+          return true, answer
+        end
+
+        [false, answer]
+      end
+
+      # Hiera 3 supports "segmented lookup" by splitting keys on dots, to allow looking up values
+      # inside nested structures. In this case reconstruct the segmented key into its original form
+      # and perform a lookup.
+      def lookup_with_segments(segments, scope, order_override, resolution_type, context)
+        Hiera.debug("Got a segmented key #{segments}")
+
+        lookup(segments.join('.'), scope, order_override, resolution_type, context)
+      end
+
+      def lookup(key, scope, order_override, resolution_type, context)
+        Hiera.debug("Looking up #{key}")
+        topscope_var = '_roles'
+        resultset = nil
+        throw(:no_such_key) unless scope.include?topscope_var
+        roles = scope[topscope_var]
+        throw(:no_such_key) if roles.nil?
+        if Config.include?(:role_hierarchy)
+          hierarchy = Config[:role_hierarchy]
+        else
+          hierarchy = nil
+        end
+
+        roles.keys.each do |role|
+          Hiera.debug("Looking in hierarchy for role #{role}")
+          answer = nil
+          Backend.datasources(scope, order_override, hierarchy) do |source|
+            yamlfile = get_path(key, role, source, scope)
+            next if yamlfile.nil?
+            Hiera.debug("Searching in file #{yamlfile} for #{key}")
+            next unless File.exist?(yamlfile)
+
+            data = @cache.read(yamlfile, Hash) do |content|
+              YAML.load(content)
+            end
+
+            next if data.nil? || data.empty?
+
+            next unless data.include? key
+
+            new_answer = Backend.parse_answer(data[key], scope, context)
+            Hiera.debug("Found: #{key} =>  #{new_answer}")
+            is_done, answer = merge_answer(new_answer, answer,
+                                           resolution_type)
+            break if is_done
+          end
+          # skip parsing if no answer was found.
+          next if answer.nil?
+
+          # Now we got one answer for this role, we can merge it with
+          # what we got earlier.
+          case resolution_type
+          when :array
+            resultset ||= []
+            answer.each { |el| resultset.push(el) }
+          when :hash, Hash
+            rt = resolution_type
+            # If we get :hash as a resolution_type, we will use the defaults.
+            rt = nil if resolution_type == :hash
+
+            resultset ||= {}
+            resultset = Backend.merge_answer(answer, resultset, rt)
+          else
+            # We raise an exception if we have received conflicting results
+            if resultset && answer != resultset
+              raise Exception, "Conflicting value for #{key} found in role #{role}"
+            else
+              resultset = answer
+            end
+          end
+        end
+        if resultset.nil? || resultset.empty?
+          Hiera.debug("No answer for #{key}!")
+          throw(:no_such_key)
+        end
+        resultset
+      end
+    end
+  end
+end
