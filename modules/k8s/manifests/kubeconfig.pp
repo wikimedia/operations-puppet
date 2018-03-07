a322c7504b5ce258bc732e217337f9c4c4acb071
Alexandros Kosiaris <akosiaris@wikimedia.org>
2018-03-07 16:35:53 +0200

    Populate kubeconfigs on deployment server
    Bug: T184919
    Change-Id: Iadc095e2c52af11a0e3a021b842658f1dfb4e167

--- modules/k8s/manifests/kubeconfig.pp
+++ modules/k8s/manifests/kubeconfig.pp
@@ -2,13 +2,16 @@ define k8s::kubeconfig(
     $master_host,
     $username,
     $token,
+    $mode='0400',
+    $owner='root',
+    $group='root',
 ) {
     file { $title:
         ensure  => present,
         content => template('k8s/kubeconfig-client.yaml.erb'),
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0400',
+        owner   => $owner,
+        group   => $group,
+        mode    => $mode,
         require => File['/etc/kubernetes'],
     }
 }
---
+++ modules/profile/manifests/kubernetes/deployment_server.pp
@@ -0,0 +1,52 @@
+# Class makes sure we got a deployment server ready
+class profile::kubernetes::deployment_server(
+    $services=hiera('profile::kubernetes::deployment_server::services', {}),
+    $tokens=hiera('profile::kubernetes::deployment_server::tokens', {}),
+){
+    package { [
+        'helm',
+        'kubernetes-client',
+        ]:
+        ensure => installed,
+    }
+
+    # The deployment script
+    file { '/usr/local/bin/scap-helm':
+        ensure => file,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/profile/kubernetes/scap-helm.sh',
+    }
+
+    file { '/etc/kubernetes':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
+
+    $envs = {
+        'eqiad' => 'kubemaster.svc.eqiad.wmnet',
+        'codfw' => 'kubemaster.svc.codfw.wmnet',
+        'staging' => 'neon.eqiad.wmnet',
+    }
+
+    $real_services = deep_merge($services, $tokens)
+
+    # Now populate the /etc/kubernetes/ kubeconfig resources
+    $envs.each |String $env, String $master_host| {
+        $real_services.each |String $service, Hash $data| {
+            # lint:ignore:variable_scope
+            k8s::kubeconfig { "/etc/kubernetes/${service}-${env}.config":
+                master_host => $master_host,
+                username    => $data['username'],
+                token       => $data['token'],
+                group       => $data['group'],
+                mode        => $data['mode'],
+            }
+            # lint:endignore
+        }
+    }
+
+}
