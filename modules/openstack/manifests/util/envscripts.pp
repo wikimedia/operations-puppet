4d34ee63454ef739df03bf155035fd069da6952c
Bryan Davis <bd808@wikimedia.org>
2018-03-07 15:03:23 -0700

    openstack: Refactor /root/novaenv.sh
    Refactor the /root/novaenv.sh to consume a new /etc/novaadmin.yaml data
    file which makes using the credentials from python script easier. The
    new YAML file will work with our mwopenstackclients library.
    
    A follow up could be done to make the generation of novaenv.sh,
    observerenv.sh, and wmflabsorg-domainadminenv.sh all use the same
    pattern and shared code. This would probably entail creating a Define to
    generate the YAML data file and the shell wrapper script and using it
    for all three files.
    
    Change-Id: I1bc015e3fc0afbf1172559b54ce745f479c26c88

--- modules/openstack/manifests/util/envscripts.pp
+++ modules/openstack/manifests/util/envscripts.pp
@@ -9,15 +9,25 @@ class openstack::util::envscripts(
     $wmflabsdotorg_project,
     ) {
 
+    # Keystone credentials for novaadmin
+    file { '/etc/novaadmin.yaml':
+        content => template('openstack/util/novaadmin.yaml.erb'),
+        mode    => '0440',
+        owner   => 'root',
+        group   => 'root',
+    }
+
     # Handy script to set up environment for commandline nova magic
     file { '/root/novaenv.sh':
         owner   => 'root',
         group   => 'root',
         mode    => '0755',
-        content => template('openstack/util/novaenv.sh.erb'),
+        source  => 'puppet:///modules/openstack/util/novaenv.sh',
+        require => File['/etc/novaadmin.yaml'],
     }
 
     # Handy script to set up environment for commandline glance magic
+    # TODO: convert to yaml + wrapper script
     file { '/root/wmflabsorg-domainadminenv.sh':
         owner   => 'root',
         group   => 'root',
