5f815f412dfeddbdccf2656cff246553420e398e
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2018-03-07 00:02:40 -0800

    dumps: Move rsync ferm rules to base rsync config
    Bug: T188727
    Change-Id: I1a1b1b975c21032f1aaecf4cbef1b08277f897b3

--- modules/profile/manifests/dumps/distribution/rsync.pp
+++ modules/profile/manifests/dumps/distribution/rsync.pp
@@ -1,6 +1,7 @@
 # Set up rsync server and base config
 class profile::dumps::distribution::rsync(
     $rsyncer_settings = hiera('profile::dumps::rsyncer'),
+    $rsync_clients = hiera('dumps_web_rsync_server_clients'),
 ) {
     $user = $rsyncer_settings['dumps_user']
     $group = $rsyncer_settings['dumps_group']
@@ -16,4 +17,24 @@ class profile::dumps::distribution::rsync(
 
     class {'::vm::higher_min_free_kbytes':}
 
+    # Setup ferm rules for internal and external clients -
+    # By default the resolve() function in ferm performs only an IPv4/A DNS
+    # lookup. It fails if a host only has an IPv6 address. Ferm also provides
+    # a AAAA lookup mode for IPv6 addresses, but this equally fails if only
+    # an IPv4 address is present.
+
+    $rsync_clients_ipv4_ferm = join(concat($rsync_clients['ipv4']['internal'], $rsync_clients['ipv4']['external']), ' ')
+    $rsync_clients_ipv6_ferm = join(concat($rsync_clients['ipv6']['internal'], $rsync_clients['ipv6']['external']), ' ')
+
+    ferm::service {'dumps_rsyncd_ipv4':
+        port   => '873',
+        proto  => 'tcp',
+        srange => "@resolve((${rsync_clients_ipv4_ferm}))",
+    }
+
+    ferm::service {'dumps_rsyncd_ipv6':
+        port   => '873',
+        proto  => 'tcp',
+        srange => "@resolve((${rsync_clients_ipv6_ferm}),AAAA)",
+    }
 }
