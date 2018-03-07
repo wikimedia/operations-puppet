2a718c05cb6649052ab9c6429a64df59300518f0
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2018-03-07 08:49:34 -0800

    dumps: Refactor fetcher profile
    - Move profile to distribution/datasets
    - Get dumps user and group from hiera
    
    Bug: T188727
    Change-Id: Id55e9e5ed9d3dd2d08c2be9edfeb5be473e9db77

--- modules/profile/manifests/dumps/distribution/datasets/fetcher.pp
+++ modules/profile/manifests/dumps/distribution/datasets/fetcher.pp
@@ -1,17 +1,22 @@
-class profile::dumps::fetcher(
+class profile::dumps::distribution::datasets::fetcher(
     $xmldumpsdir = hiera('profile::dumps::distribution::xmldumpspublicdir'),
     $miscdatasetsdir = hiera('profile::dumps::distribution::miscdumpsdir'),
+    $rsyncer_settings = hiera('profile::dumps::distribution::rsync_config'),
 ) {
+
+    $user = $rsyncer_settings['dumps_user']
+    $group = $rsyncer_settings['dumps_group']
+
     class {'dumps::web::fetches::kiwix':
-        user            => 'dumpsgen',
-        group           => 'dumpsgen',
+        user            => $user,
+        group           => $group,
         xmldumpsdir     => $xmldumpsdir,
         miscdatasetsdir => $miscdatasetsdir,
     }
     class {'dumps::web::fetches::stats':
         src             => 'stat1005.eqiad.wmnet::hdfs-archive',
         miscdatasetsdir => $miscdatasetsdir,
-        user            => 'dumpsgen',
+        user            => $user,
     }
     class {'dumps::web::fetches::wikitech_dumps':
         url             => 'https://wikitech.wikimedia.org/dumps/',
