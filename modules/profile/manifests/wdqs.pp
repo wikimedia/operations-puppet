a25056916e401d67213e1967f834f9c55c7e48cc
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2018-03-07 11:20:41 +0100

    wdqs: enable LDF server is now configurable
    LDF server should be disable on the new WDQS internal cluster, so having
    the option to disable it with puppet is required.
    
    Bug: T187766
    Change-Id: I22ecbe9166fa3464b7ecf24e833ad6650ef49ece

--- modules/profile/manifests/wdqs.pp
+++ modules/profile/manifests/wdqs.pp
@@ -12,6 +12,7 @@ class profile::wdqs (
     $use_kafka_for_updates = hiera('profile::wdqs::use_kafka_for_updates'),
     $cluster_names = hiera('profile::wdqs::cluster_names'),
     $rc_options = hiera('profile::wdqs::rc_updater_options'),
+    $enable_ldf = hiera('profile::wdqs::enable_ldf'),
     $prometheus_nodes = hiera('prometheus_nodes'),
     $contact_groups = hiera('contactgroups', 'admins'),
 ) {
@@ -67,6 +68,7 @@ class profile::wdqs (
     # Service Web proxy
     class { '::wdqs::gui':
         logstash_host => $logstash_host,
+        enable_ldf    => $enable_ldf,
     }
 
     # Firewall
