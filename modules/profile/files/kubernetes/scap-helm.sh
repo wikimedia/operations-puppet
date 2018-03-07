d73f0c00783c7cfbdd36efd6aaf8edddd9adb4e9
Alexandros Kosiaris <akosiaris@wikimedia.org>
2018-03-07 16:35:53 +0200

    Populate kubeconfigs on deployment server
    Bug: T184919
    Change-Id: Iadc095e2c52af11a0e3a021b842658f1dfb4e167

---
+++ modules/profile/files/kubernetes/scap-helm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -x
+set -e
+
+SERVICE=$1
+NAMESPACE=$SERVICE
+
+for i in ${SERVICE}-eqiad ${SERVICE}-codfw; do
+	KUBECONFIG="/etc/kubernetes/${i}.config" helm install --tiller-namespace=${NAMESPACE} ${SERVICE}
+done
