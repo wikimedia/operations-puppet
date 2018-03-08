b53985dfb2b5607952bcfc5575437383e0acbd6d
cpettet <rush@wikimedia.org>
2018-03-08 16:04:31 -0600

    openstack: nova-compute jessie mitaka setup
    Bug: T188266
    Change-Id: Iad2b888c2d93d188c0463918971d409e5d72c30e

--- modules/openstack/manifests/nova/compute/audit.pp
+++ modules/openstack/manifests/nova/compute/audit.pp
@@ -5,13 +5,19 @@
 # 3.19 series kernels, instance clocks die after resuming from suspension
 # Virtio has shown to be non-determinstic on certain host:client kernel
 # version matchups (IO freezing)
+
+# 4.4.0-109-generic = Trusty known good post Meltdown
+# 4.9.0-0.bpo.6-amd64 = Jessie (known good?) post Meltdown
 class openstack::nova::compute::audit(
-    $whitelist_kernels=['4.4.0-109-generic'],
+    $whitelist_kernels=['4.4.0-109-generic', '4.9.0-0.bpo.6-amd64'],
     ) {
 
-    if os_version('ubuntu >= trusty') {
-        if ! ($::kernelrelease in $whitelist_kernels) {
-            fail("nova-compute is only valid for ${whitelist_kernels} and not ${::kernelrelease}")
-        }
-    }
+    notify{"nova-compute is only valid for ${whitelist_kernels} and not ${::kernelrelease}":}
+
+    # XXX: Need to update facts to have this be useful
+    # if os_version('ubuntu >= trusty') {
+    #     if ! ($::kernelrelease in $whitelist_kernels) {
+    #         fail("nova-compute is only valid for ${whitelist_kernels} and not ${::kernelrelease}")
+    #    }
+    #}
 }
