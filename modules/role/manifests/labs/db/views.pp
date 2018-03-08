5bf2728d0c4fbc23dbeb47c548e6f6afeb3632a1
bstorm <bstorm@wikimedia.org>
2018-03-08 13:04:08 -0700

    wiki replicas: script index creation for easier maintenance
    This may or may not work, depending on where these must be made.
    In order to reduce error and ensure easier maintenance, this is an effort
    to script maintenance of the wiki replica-only indexes that are not in
    production.  This is accepting that at least the user/password referenced
    is probably wrong.  This is also assuming the same hosts are needed for
    all of these indexes.
    
    Bug: T181650
    Change-Id: I71e286a9fcc05437c1cbaeb4bd7423e25a275c3a

--- modules/role/manifests/labs/db/views.pp
+++ modules/role/manifests/labs/db/views.pp
@@ -5,6 +5,7 @@ class role::labs::db::views {
         'python-pymysql',
         'python-requests',
         'python-simplejson',
+        'python3-psutil'
     ]:
         ensure => present,
     }
@@ -27,6 +28,25 @@ class role::labs::db::views {
         group   => 'root',
         mode    => '0655',
         require => [Package['python3-yaml', 'python3-pymysql'],
+                    Git::Clone['operations/mediawiki-config'],
+        ],
+    }
+
+    file { '/etc/index-conf.yaml':
+        ensure  => file,
+        content => template('role/labs/db/views/index-conf.yaml'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+
+    file { '/usr/local/sbin/maintain_replica_indexes.py':
+        ensure  => file,
+        source  => 'puppet:///modules/role/labs/db/views/maintain_replica_indexes.py',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0655',
+        require => [Package['python3-yaml', 'python3-pymysql', 'python3-psutil'],
                     Git::Clone['operations/mediawiki-config'],
         ],
     }
