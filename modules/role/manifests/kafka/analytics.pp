b52852f5996bb602832af3532188cf46c3e374c4
Ottomata <aotto@wikimedia.org>
2018-03-08 15:32:51 -0500

    Conditionally include MirrorMaker instances
    Older MirrorMaker is having problems replicating main-eqiad -> jumbo-eqiad.
    It seems that kafka1012 is always being assigned all partitions for all
    instances.  This at least will force another node to do some work
    for kafka jumbo.
    
    Change-Id: I8e5c7ebc11a51a8362e75c3b30fb9cbb6ce61f88

--- modules/role/manifests/kafka/analytics.pp
+++ modules/role/manifests/kafka/analytics.pp
@@ -8,16 +8,21 @@ class role::kafka::analytics {
 
     include ::role::kafka::analytics::broker
     # Mirror all other Kafka cluster data into the analytics Kafka cluster.
-    include ::role::kafka::analytics::mirror
+    if $::hostname =~ /kafka10(12|13|14)/ {
+        include ::role::kafka::analytics::mirror
+    }
+
     # Mirror main Kafka cluster data to Jumbo Kafka cluster.
     # NOTE:  this is only running on the analytics Kafka brokers because
     # of a 0.11 client compatibility issue.  Ideally this role would
     # be included on the jumbo brokers instead.  But, since we need to consume
     # from a 0.9 cluster (main), we need to use a non 0.11 MirrorMaker version,
     # which is not available on the Kafka jumbo brokers, since they are 0.11.
-    include ::role::kafka::jumbo::mirror
-    include ::role::ipsec
+    if $::hostname =~ /kafka10(20|22|23)/ {
+        include ::role::kafka::jumbo::mirror
+    }
 
+    include ::role::ipsec
     include ::standard
     include ::base::firewall
 }
