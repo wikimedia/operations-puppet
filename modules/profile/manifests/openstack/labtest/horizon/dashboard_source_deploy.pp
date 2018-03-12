5de509ca8f6e3df1177025b6a74c40a2255ecbaf
andrewbogott <abogott@wikimedia.org>
2018-03-12 15:30:53 -0500

    labtestweb: refactor to more closely resemble the labweb* deploy
    Bug: T168470
    Change-Id: I67b5a52c577f836e77f6c1683258578a5b268175

---
+++ modules/profile/manifests/openstack/labtest/horizon/dashboard_source_deploy.pp
@@ -0,0 +1,21 @@
+class profile::openstack::labtest::horizon::dashboard_source_deploy(
+    $version = hiera('profile::openstack::labtest::version'),
+    $nova_controller = hiera('profile::openstack::labtest::nova_controller'),
+    $wmflabsdotorg_admin = hiera('profile::openstack::labtest::designate::wmflabsdotorg_admin'),
+    $wmflabsdotorg_pass = hiera('profile::openstack::labtest::designate::wmflabsdotorg_pass'),
+    $dhcp_domain = hiera('profile::openstack::labtest::nova::dhcp_domain'),
+    $ldap_user_pass = hiera('profile::openstack::labtest::ldap_user_pass'),
+    $webserver_hostname = hiera('profile::openstack::labtest::horizon::webserver_hostname'),
+    ) {
+
+    require ::profile::openstack::labtest::clientlib
+    class {'::profile::openstack::base::horizon::dashboard_source_deploy':
+        version             => $version,
+        nova_controller     => $nova_controller,
+        wmflabsdotorg_admin => $wmflabsdotorg_admin,
+        wmflabsdotorg_pass  => $wmflabsdotorg_pass,
+        dhcp_domain         => $dhcp_domain,
+        ldap_user_pass      => $ldap_user_pass,
+        webserver_hostname  => $webserver_hostname,
+    }
+}
