9468a3970b08d3aa4beca6d91e00cd07abac4e7e
Ottomata <aotto@wikimedia.org>
2018-03-12 13:37:59 -0400

    Use proper kafka protocol version for varnishkafka webrequest
    Also switch canary to Kafka jumbo
    
    Change-Id: I71217c78dfcaea4c0520faf2601f6127de04328b

--- modules/profile/manifests/cache/kafka/webrequest.pp
+++ modules/profile/manifests/cache/kafka/webrequest.pp
@@ -115,13 +115,6 @@ class profile::cache::kafka::webrequest(
         $ssl_cipher_suites        = undef
     }
 
-    # TEMPORARY HACK to aid in migration to kafka jumbo.
-    # this will be removed once all varnishkafka webrequest instances use jumbo.
-    $force_protocol_version  = $kafka_cluster_name ? {
-        'jumbo' => undef,
-        default => '0.9.0.1',
-    }
-
     varnishkafka::instance { 'webrequest':
         brokers                      => $kafka_brokers,
         topic                        => $topic,
@@ -160,7 +153,6 @@ class profile::cache::kafka::webrequest(
         ssl_key_location             => $ssl_key_location,
         ssl_certificate_location     => $ssl_certificate_location,
         ssl_cipher_suites            => $ssl_cipher_suites,
-        force_protocol_version       => $force_protocol_version,
     }
 
     if $monitoring_enabled {
