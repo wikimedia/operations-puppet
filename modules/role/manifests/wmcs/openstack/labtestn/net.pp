7337aa4c2161d825d413d3dea17c9c6de9ca2af1
cpettet <rush@wikimedia.org>
2018-03-12 16:22:23 -0500

    openstack: labtestn/net role uncomment config
    Bug: T188266
    Change-Id: I9b8e7f78f547d1c8da098d68df149e287327004f

--- modules/role/manifests/wmcs/openstack/labtestn/net.pp
+++ modules/role/manifests/wmcs/openstack/labtestn/net.pp
@@ -2,7 +2,7 @@ class role::wmcs::openstack::labtestn::net {
     system::role { $name: }
     # Do not add base firewall
     include ::standard
-    #include ::profile::openstack::labtestn::clientlib
-    #include ::profile::openstack::labtestn::observerenv
-    #include ::profile::openstack::labtestn::neutron::l3_agent
+    include ::profile::openstack::labtestn::clientlib
+    include ::profile::openstack::labtestn::observerenv
+    include ::profile::openstack::labtestn::neutron::l3_agent
 }
