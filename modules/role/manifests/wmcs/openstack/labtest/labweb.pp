7d6e667633a65cc46a68d56df67a4565cb62ac1b
andrewbogott <abogott@wikimedia.org>
2018-03-12 15:30:53 -0500

    labtestweb: refactor to more closely resemble the labweb* deploy
    Bug: T168470
    Change-Id: I67b5a52c577f836e77f6c1683258578a5b268175

---
+++ modules/role/manifests/wmcs/openstack/labtest/labweb.pp
@@ -0,0 +1,26 @@
+# All horizon/striker/wikitech profiles should fold into
+# role::wmcs::openstack::labtest::web when labweb* is finished
+class role::wmcs::openstack::labtest::labweb {
+    system::role { $name: }
+
+    require_package('libapache2-mod-wsgi-py3')
+    class { '::httpd':
+        modules => ['alias', 'ssl', 'rewrite', 'headers', 'wsgi',
+                    'proxy', 'expires', 'proxy_http', 'proxy_balancer',
+                    'lbmethod_byrequests', 'proxy_fcgi'],
+    }
+
+    include ::profile::ldap::client::labs
+    include ::profile::base::firewall
+    include ::profile::openstack::labtest::nutcracker
+
+    # Wikitech:
+    include ::profile::openstack::labtest::wikitech::web
+    include ::profile::openstack::labtest::wikitech::monitor
+
+    # Horizon:
+    include ::profile::openstack::labtest::horizon::dashboard_source_deploy
+
+    # Striker:
+    include ::profile::openstack::base::striker::web
+}
