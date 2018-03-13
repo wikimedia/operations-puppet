3b6e97b78e496b79100df23b753b551f0c0a5205
cpettet <rush@wikimedia.org>
2018-03-13 10:14:42 -0500

    openstack: labtestn initial neutron framework
    Bug: T188266
    Change-Id: Ie6f74c70ebfa86d9ded33f731e10b2ee4c9b9b36

--- modules/profile/manifests/openstack/base/neutron/l3_agent.pp
+++ modules/profile/manifests/openstack/base/neutron/l3_agent.pp
@@ -1,4 +1,5 @@
 class profile::openstack::base::neutron::l3_agent(
+    $version = hiera('profile::openstack::base::version),
     $network_flat_interface = hiera('profile::openstack::base::neutron::network_flat_interface'),
     $network_flat_tagged_base_interface = hiera('profile::openstack::base::neutron::network_flat_tagged_base_interface'),
     $network_flat_interface_vlan = hiera('profile::openstack::base::neutron::network_flat_interface_vlan'),
@@ -12,6 +13,8 @@ class profile::openstack::base::neutron::l3_agent(
         down           => 'ip link set $IFACE down',
     }
 
-    class {'::openstack::neutron::l3_agent':}
+    class {'::openstack::neutron::l3_agent':
+        version => $version,
+    }
     contain '::openstack::neutron::l3_agent'
 }
