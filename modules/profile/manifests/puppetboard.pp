4bf97d7d4dfa8e70f8edab51bd34c728d47cb076
Riccardo Coccioli <rcoccioli@wikimedia.org>
2018-03-13 19:24:53 +0100

    Puppetboard: add role, profile and configuration
    Bug: T184563
    Change-Id: I4d644d014bec87fcc260370972aecc44fa0b32d4

---
+++ modules/profile/manifests/puppetboard.pp
@@ -0,0 +1,67 @@
+# Class: profile::puppetboard
+#
+# This profile installs all the Puppetboard related parts as WMF requires it
+#
+# Actions:
+#       Deploy Puppetboard
+#       Install apache, uwsgi, configure reverse proxy to uwsgi
+#
+# Sample Usage:
+#       include ::profile::puppetboard
+#
+class profile::puppetboard (
+    String $puppetdb_host = hiera('puppetdb_host'),
+    String $flask_secret_key = hiera('profile::puppetboard::flask_secret_key'),
+) {
+    include passwords::ldap::production
+
+    $ldap_password = $passwords::ldap::production::proxypass
+    $port = 8001
+    $base_path = '/srv/deployment/puppetboard'
+    $config_path = "${base_path}/deploy"
+    $venv_path = "${base_path}/venv"
+    $directory = "${venv_path}/lib/python3.5/site-packages/puppetboard"
+
+    require_package('virtualenv', 'python3-pip')
+
+    file { "${base_path}/settings.py":
+        ensure  => present,
+        owner   => 'deploy-puppetboard',
+        group   => 'www-data',
+        mode    => '0440',
+        content => template('profile/puppetboard/settings.py.erb'),
+        before  => Uwsgi::App['puppetboard'],
+    }
+
+    service::uwsgi { 'puppetboard':
+        port            => $port,
+        no_workers      => 4,
+        deployment_user => 'deploy-puppetboard',
+        config          => {
+            need-plugins => 'python3',
+            chdir        => $directory,
+            venv         => $venv_path,
+            wsgi         => 'puppetboard.wsgi',
+            vacuum       => true,
+            http-socket  => "127.0.0.1:${port}",
+            # T164034: make sure Python has a sane default encoding
+            env          => [
+                'LANG=C.UTF-8',
+                'LC_ALL=C.UTF-8',
+                'PYTHONENCODING=utf-8',
+            ],
+        },
+        healthcheck_url => '/',
+        icinga_check    => false,
+        sudo_rules      => [
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-puppetboard restart',
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-puppetboard start',
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-puppetboard status',
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-puppetboard stop',
+        ],
+    }
+
+    httpd::site { 'puppetboard.wikimedia.org':
+        content => template('profile/puppetboard/puppetboard.wikimedia.org.erb'),
+    }
+}
