eb6760618f0d225b779343bb9045bde3facf4d0a
Daniel Zahn <dzahn@wikimedia.org>
2018-03-13 11:44:58 -0700

    restbase: allow to skip monitoring, disable on dev hosts
    Add support to the restbase::monitoring class to allow disabling
    it for select hosts.
    
    Disable it on restbase-dev hosts via regex on hostname in Hiera.
    
    Bug: T189050
    Change-Id: I9259dd53999e77cfb932d6c5282c99a770e03c5e

--- modules/restbase/manifests/monitoring.pp
+++ modules/restbase/manifests/monitoring.pp
@@ -2,10 +2,18 @@
 #
 # For now, a rudimentary check for restbase
 class restbase::monitoring(
+    $monitor_restbase = hiera('monitor_restbase', true),
 ) {
     require ::restbase
 
+    $ensure_monitor_restbase = $monitor_restbase ? {
+        true    => present,
+        false   => absent,
+        default => present,
+    }
+
     monitoring::service { 'restbase_http_root':
+        ensure        => $ensure_monitor_restbase,
         description   => 'Restbase root url',
         check_command => "check_http_port_url!${::restbase::port}!/",
         contact_group => 'admins,team-services',
