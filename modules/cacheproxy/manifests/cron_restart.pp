1db5af9e32eb23a1528de8ff112ce0fd4d8a716b
Brandon Black <bblack@wikimedia.org>
2018-03-13 00:51:59 +0000

    varnish: remove weekly restart cron entries
    Do not deploy this until 3.5 days (84 hours) after the previous
    commit with the semiweekly entries goes live, to avoid the edge
    case where a varnishd might be left alive for ~10 days total
    during the transition.
    
    Bug: T181315
    Bug: T174932
    Change-Id: Ib45e9180e2785262e2fc7622258c11fc65671a14

--- modules/cacheproxy/manifests/cron_restart.pp
+++ modules/cacheproxy/manifests/cron_restart.pp
@@ -11,18 +11,7 @@ class cacheproxy::cron_restart ($nodes, $cache_cluster) {
     #TODO: maybe use the list of datacenters to do this?
     $all_nodes = array_concat($nodes['eqiad'], $nodes['esams'], $nodes['ulsfo'], $nodes['codfw'], $nodes['eqsin'])
 
-    # Previous weekly restarts.  Should be maintained for 3.5 days after
-    # starting the new semiweekly schedule below, to avoid edge cases where the
-    # shifting times could leave a node's varnish up for ~10 days during the
-    # transition time.  Will cause a total of 3 restarts per week until
-    # removed, and we'll need to double-check their timing manually to make
-    # sure this one isn't dangerously close to one of the ones below.
-    $times_w = cron_splay($all_nodes, 'weekly', "${cache_cluster}-backend-restarts")
-    $be_restart_h = $times_w['hour']
-    $be_restart_m = $times_w['minute']
-    $be_restart_d = $times_w['weekday']
-
-    # New semiweekly cron entries for restarts every 3.5 days
+    # Semiweekly cron entries for restarts every 3.5 days
     $times = cron_splay($all_nodes, 'semiweekly', "${cache_cluster}-backend-restarts")
     $be_restart_a_h = $times['hour-a']
     $be_restart_a_m = $times['minute-a']
