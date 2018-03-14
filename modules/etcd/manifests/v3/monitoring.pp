d23afa8c1a7de4b66ad7e25e77eeadcf266c24e0
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-03-14 16:05:48 +0100

    etcd::v3: add basic monitoring
    Change-Id: Ie1527a7319bf1f3cabac28a9423b2d94aafb0751

---
+++ modules/etcd/manifests/v3/monitoring.pp
@@ -0,0 +1,21 @@
+class etcd::v3::monitoring(Stdlib::HTTPSurl $endpoint) {
+    # Check the daemon is running
+    nrpe::monitor_systemd_unit_state { 'etcd':
+        require => Service['etcd'],
+    }
+
+    # Just check the local health
+    file { '/usr/local/bin/check_etcd_health':
+        ensure  => present,
+        source  => 'puppet:///modules/etcd/v3/check_etcd_health.sh',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        require => Service['etcd'],
+    }
+
+    nrpe::monitor_service{ 'etcd_cluster_health':
+        description  => 'Etcd cluster health',
+        nrpe_command => "/usr/local/bin/check_etcd_health ${::fqdn}",
+    }
+}
