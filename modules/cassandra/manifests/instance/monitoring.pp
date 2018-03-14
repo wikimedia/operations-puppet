cb6566850bcece0ba786c4d480a2e638bb951258
Daniel Zahn <dzahn@wikimedia.org>
2018-03-13 18:59:05 -0700

    cassandra/icinga: make monitoring configurable, skip on dev
    Add support to cassandra module/profile to disable Icinga
    monitoring checks specific to Cassandra.
    
    Disable on dev machines via Hiera.
    
    Bug: T189050
    Change-Id: Id84d8491a1420edc84be504d9693cb77ffff6358

--- modules/cassandra/manifests/instance/monitoring.pp
+++ modules/cassandra/manifests/instance/monitoring.pp
@@ -11,6 +11,7 @@ define cassandra::instance::monitoring (
     $instances        = $::cassandra::instances,
     $contact_group    = 'admins,team-services',
     $tls_cluster_name = $::cassandra::tls_cluster_name,
+    $monitor_enabled  = $::cassandra::monitor_enabled,
 ) {
     $instance_name  = $title
     $this_instance  = $instances[$instance_name]
@@ -25,12 +26,20 @@ define cassandra::instance::monitoring (
         default   => "cassandra-${instance_name}"
     }
 
+    $ensure_monitor = $monitor_enabled ?
+        true    => present,
+        false   => absent,
+        default => present,
+    }
+
     nrpe::monitor_systemd_unit_state { $service_name:
+        ensure  => $ensure_monitor,
         require => Service[$service_name],
     }
 
     # CQL query interface monitoring (T93886)
     monitoring::service { "${service_name}-cql":
+        ensure        => $ensure_monitor,
         description   => "${service_name} CQL ${listen_address}:9042",
         check_command => "check_tcp_ip!${listen_address}!9042",
         contact_group => $contact_group,
@@ -39,6 +48,7 @@ define cassandra::instance::monitoring (
     # SSL cert expiration monitoring (T120662)
     if !empty($tls_cluster_name) {
         monitoring::service { "${service_name}-ssl":
+            ensure        => $ensure_monitor,
             description   => "${service_name} SSL ${listen_address}:7001",
             check_command => "check_ssl_on_host_port!${::hostname}-${instance_name}!${listen_address}!7001",
             contact_group => $contact_group,
