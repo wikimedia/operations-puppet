2d0dd944119764e45d22c1a6d52ded42f28d0c0f
Jaime Crespo <jcrespo@wikimedia.org>
2018-03-14 12:00:08 +0100

    dbproxy: Reenable firewall on proxies for m1 and m2 with holes
    The following services have been found running on public ips that
    use database services:
    
    * m1: 208.80.154.5 netmon1002.wikimedia.org (librenms)
    * m1: 208.80.154.19 ununpentium.wikimedia.org (rt)
    * m2: 208.80.154.81 cobalt.wikimedia.org (gerrit)
    
    Setup rules allowing all of those on m1 and m2-related proxies.
    
    Bug: T189655
    Bug: T189656
    Change-Id: I6c34b181d4b135ad9343295c220c587bd2b9fbe5

---
+++ modules/profile/manifests/mariadb/ferm_misc.pp
@@ -0,0 +1,25 @@
+# Firewall rules for the misc db host used by internet-facing websites.
+# We need special rules to allow access for some services which
+# run on hosts with public IPs.
+class profile::mariadb::ferm_misc(
+    ferm::service{ 'librenms':
+        proto   => 'tcp',
+        port    => '3306',
+        notrack => true,
+        srange  => "@resolve(netmon1002.wikimedia.org)",
+    }
+
+    ferm::service{ 'rt':
+        proto   => 'tcp',
+        port    => '3306',
+        notrack => true,
+        srange  => "@resolve(ununpentium.wikimedia.org)",
+    }
+
+    ferm::service{ 'gerrit':
+        proto   => 'tcp',
+        port    => '3306',
+        notrack => true,
+        srange  => "@resolve(cobalt.wikimedia.org)",
+    }
+}
