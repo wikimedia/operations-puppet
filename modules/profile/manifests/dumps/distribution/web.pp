08bfa9abe4c242f9e4d63eab6ceb108408dbd636
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2018-03-14 13:29:43 -0700

    dumps: Setup web server config in distribution hosts
    Bug: T188641
    Change-Id: Id27dc2d7406ba7d920b59e6df580015107d3240c

---
+++ modules/profile/manifests/dumps/distribution/web.pp
@@ -0,0 +1,23 @@
+class profile::dumps::distribution::web (
+    $do_acme = hiera('do_acme'),
+    $datadir = hiera('profile::dumps::distribution::basedatadir'),
+    $xmldumpsdir = hiera('profile::dumps::distribution::xmldumpspublicdir'),
+    $miscdatasetsdir = hiera('profile::dumps::distribution::miscdumpsdir'),
+){
+    # includes module for bandwidth limits
+    class { '::nginx':
+        variant => 'extras',
+    }
+
+    class {'::dumps::web::xmldumps':
+        do_acme          => $do_acme,
+        datadir          => $datadir,
+        xmldumpsdir      => $xmldumpsdir,
+        miscdatasetsdir  => $miscdatasetsdir,
+        htmldumps_server => 'francium.eqiad.wmnet',
+        xmldumps_server  => 'dumps.wikimedia.org',
+        webuser          => 'dumpsgen',
+        webgroup         => 'dumpsgen',
+    }
+
+}
