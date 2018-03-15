f580f72fdd8ffb9b519daa65cebad9ac0b708d2f
cpettet <rush@wikimedia.org>
2018-03-15 14:31:10 -0500

    openstack: neutron change for agent registration
    * make the bootstrap class
    * dhcp agent perms and service
    * l3 agent perms and service
    * metadata agent perms and service
    * linuxbridge perms, paths (the package paths
      for config files etc are bad) and service
    * neutron-server service
    * plugin names must be the terse version
    
    Bug: T188266
    Change-Id: I15b10b2da6d0dafd0d4717655a368a1c3ac960c7

---
+++ modules/openstack/manifests/nova/compute/kmod.pp
@@ -0,0 +1,10 @@
+class openstack::nova::compute::kmod {
+
+    # Starting with 3.18 (34666d467cbf1e2e3c7bb15a63eccfb582cdd71f) the netfilter code
+    # was split from the bridge kernel module into a separate module (br_netfilter)
+    if (versioncmp($::kernelversion, '3.18') >= 0) {
+        kmod::module { 'br_netfilter':
+            ensure => 'present',
+        }
+    }
+}
