fd80dfbffbea359ab89a0ab934b79f19b46404a0
elukey <ltoscano@wikimedia.org>
2018-03-15 10:23:02 +0100

    profile::hadoop: refactor depencencies to reduce confusion
    After the last refactory some redundant classes and explicit
    ordering arrows were used:
    1) profile::cdh::apt_pin, that has been coupled directly with
       profile::cdh::apt.
    2) profile::hadoop::common already contains an explicit ordering
       for cdh-apt -> exec apt-get update -> cdh::common, so simply
       adding a require for profile::hadoop::common should be enough.
    3) added an explicit dependency on profile::hadoop::common for
       the resource manager monitoring, that was missed during the last
       code change.
    
    Bug: T188294
    Change-Id: I35a28bc9110133d6550a0542f44ee660fcb208b0

--- modules/profile/manifests/cdh/apt.pp
+++ modules/profile/manifests/cdh/apt.pp
@@ -1,11 +1,21 @@
 # == Class profile::cdh::apt
 #
 # Set Cloudera's apt repository to the host.
+# Pins thirdparty/cloudera packages in our apt repo
+# to a higher priority than others.  This mainly exists
+# because both Debian and CDH have versions of zookeeper
+# that conflict.  Where this class is included, the
+# CDH version of zookeeper (and any other conflicting packages)
+# will be prefered.
 #
 class profile::cdh::apt {
     apt::repository { 'thirdparty-cloudera':
         uri        => 'http://apt.wikimedia.org/wikimedia',
         dist       => "${::lsbdistcodename}-wikimedia",
         components => 'thirdparty/cloudera',
+    }
+    apt::pin { 'thirdparty-cloudera':
+        pin      => 'release c=thirdparty/cloudera',
+        priority => '1002',
     }
 }
