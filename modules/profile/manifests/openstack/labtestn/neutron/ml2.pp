c966ee7c4f133c875bc2a54c92ec3de1e363a75b
cpettet <rush@wikimedia.org>
2018-03-15 11:20:12 -0500

    openstack: pass network_flat_interface to ml2 setup
    (also put ml2 config on control node)
    Bug: T188266
    
    Change-Id: Id5693c6575b757daafcfbe3301d83b94db26c06c

--- modules/profile/manifests/openstack/labtestn/neutron/ml2.pp
+++ modules/profile/manifests/openstack/labtestn/neutron/ml2.pp
@@ -1,11 +1,13 @@
 class profile::openstack::labtestn::neutron::ml2(
     $version = hiera('profile::openstack::labtestn::version'),
+    $network_flat_interface = hiera('profile::openstack::labtestn::neutron::network_flat_interface'),
     ) {
 
     require ::profile::openstack::labtestn::clientlib
     require ::profile::openstack::labtestn::neutron::common
     class {'::profile::openstack::base::neutron::ml2':
-        version => $version,
+        version                => $version,
+        network_flat_interface => $network_flat_interface,
     }
     contain '::profile::openstack::base::neutron::ml2'
 }
