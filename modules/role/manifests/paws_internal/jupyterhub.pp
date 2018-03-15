b6302cbc4f160461620a107f97bd5c3edf1ff6c5
Ottomata <aotto@wikimedia.org>
2018-03-15 00:05:17 -0400

    Puppetization for newer SWAP (JupyterHub)
    Clones from analytics/jupterhub/deploy and builds a virtualenv.
    
    Bug: T183145
    Change-Id: I408aeea670b1aae000ee04a7351ec10cd6e4f012

--- modules/role/manifests/paws_internal/jupyterhub.pp
+++ modules/role/manifests/paws_internal/jupyterhub.pp
@@ -2,17 +2,18 @@
 # Role for setting up PAWS Internal - Jupyterhub service running on analytics cluster
 #
 # See https://wikitech.wikimedia.org/wiki/PAWS/Internal for more info
+# TODO: This class will be removed as part of https://phabricator.wikimedia.org/T183145
 class role::paws_internal::jupyterhub {
 
     include ::base::firewall
     include ::statistics::packages
 
-    class { '::jupyterhub':
+    class { '::jupyterhub_old':
         base_path   => '/srv/paws-internal',
         wheels_repo => 'operations/wheels/paws-internal',
     }
 
-    class { '::jupyterhub::static':
+    class { '::jupyterhub_old::static':
         sitename    => 'paws-internal.wikimedia.org',
         static_path => '/srv/paws-internal/static',
         url_prefix  => '/public',
