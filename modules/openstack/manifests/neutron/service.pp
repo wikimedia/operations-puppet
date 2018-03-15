8303aaa320fa51364d50d1809bd645ebc567c67b
cpettet <rush@wikimedia.org>
2018-03-15 11:20:12 -0500

    openstack: pass network_flat_interface to ml2 setup
    (also put ml2 config on control node)
    
    Bug: T188266
    Change-Id: Id5693c6575b757daafcfbe3301d83b94db26c06c

--- modules/openstack/manifests/neutron/service.pp
+++ modules/openstack/manifests/neutron/service.pp
@@ -5,4 +5,13 @@ class openstack::neutron::service(
     package {'neutron-server':
         ensure => 'present',
     }
+
+    # Needed for setup and schema changes via neutron-db-manage
+    file { '/etc/neutron/plugins/ml2/ml2_conf.ini':
+        content => template("openstack/${version}/neutron/plugins/ml2/ml2_conf.ini.erb"),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0744',
+        require => Package['neutron-linuxbridge-agent'];
+    }
 }
--- modules/profile/manifests/openstack/base/neutron/ml2.pp
+++ modules/profile/manifests/openstack/base/neutron/ml2.pp
@@ -1,9 +1,11 @@
 class profile::openstack::base::neutron::ml2(
     $version = hiera('profile::openstack::base::version'),
+    $network_flat_interface = hiera('profile::openstack::base::neutron::network_flat_interface'),
     ) {
 
     class {'::openstack::neutron::ml2':
-        version => $version,
+        version                => $version,
+        network_flat_interface => $network_flat_interface,
     }
     contain '::openstack::neutron::ml2'
 }
