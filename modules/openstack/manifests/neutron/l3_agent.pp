288de80f7c8e194875b9b7e672425b7552303765
cpettet <rush@wikimedia.org>
2018-03-16 08:04:08 -0500

    openstack: neutron l3-agent sysctl settings
    Bug: T188266
    Change-Id: Ic83d063cf5e53434245b317abface1aac84f5fcb

--- modules/openstack/manifests/neutron/l3_agent.pp
+++ modules/openstack/manifests/neutron/l3_agent.pp
@@ -18,4 +18,25 @@ class openstack::neutron::l3_agent(
         ensure  => 'running',
         require => Package['neutron-l3-agent'],
     }
+
+    sysctl::parameters { 'openstack':
+        values   => {
+            # Turn off IP filter
+            'net.ipv4.conf.default.rp_filter' => 0,
+            'net.ipv4.conf.all.rp_filter'     => 0,
+
+            # Enable IP forwarding
+            'net.ipv4.ip_forward'             => 1,
+            'net.ipv6.conf.all.forwarding'    => 1,
+
+            # Disable RA
+            'net.ipv6.conf.all.accept_ra'     => 0,
+
+            # Increase connection tracking size
+            # and bucket since all of labs is
+            # tracked on the network host
+            'net.netfilter.nf_conntrack_max'  => 262144,
+        },
+        priority => 50,
+    }
 }
