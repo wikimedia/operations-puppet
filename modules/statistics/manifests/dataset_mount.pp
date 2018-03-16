4613e1e97e08be76df1f6dbd77648b11118c6ed1
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2018-03-16 10:53:59 -0700

    [WIP] statistics: Migrate dumps mount to labstore1006|7
    Don't merge, putting this up in advance of migrating
    dumps distribution servers for NFS on April from dataset1001
    to labstore1006
    
    Bug: T188644
    Change-Id: I7657b8be03f0e571eedd582b69a4f7f0905180aa

--- modules/statistics/manifests/dataset_mount.pp
+++ modules/statistics/manifests/dataset_mount.pp
@@ -10,16 +10,50 @@ class statistics::dataset_mount {
     # need this for NFS mounts.
     require_package('nfs-common')
 
-    file { '/mnt/data':
+    file {'/mnt/nfs':
         ensure => 'directory',
     }
 
-    mount { '/mnt/data':
-        ensure  => 'mounted',
-        device  => '208.80.154.11:/data',
-        fstype  => 'nfs',
-        options => 'ro,bg,tcp,rsize=8192,wsize=8192,timeo=14,intr,addr=208.80.154.11',
-        atboot  => true,
-        require => File['/mnt/data'],
+    file { '/mnt/nfs/README':
+        ensure  => 'present',
+        source  => 'puppet:///modules/statistics/dumps-nfsmount-readme.txt',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => File['/mnt/nfs'],
     }
+
+    file { '/mnt/nfs/labstore1006-dumps':
+        ensure => 'directory',
+    }
+
+    file { '/mnt/nfs/labstore1007-dumps':
+        ensure => 'directory',
+    }
+
+    mount { '/mnt/nfs/labstore1006-dumps':
+        ensure  => 'mounted',
+        device  => 'labstore1006.wikimedia.org:/dumps',
+        fstype  => 'nfs',
+        options => 'ro,bg,tcp,rsize=8192,wsize=8192,timeo=14,intr',
+        atboot  => true,
+        require => File['/mnt/nfs/labstore1006-dumps'],
+    }
+
+    mount { '/mnt/nfs/labstore1007-dumps':
+        ensure  => 'mounted',
+        device  => 'labstore1007.wikimedia.org:/dumps',
+        fstype  => 'nfs',
+        options => 'ro,bg,tcp,rsize=8192,wsize=8192,timeo=14,intr',
+        atboot  => true,
+        require => File['/mnt/nfs/labstore1007-dumps'],
+    }
+
+
+    file { '/mnt/data/':
+        ensure  => 'link',
+        target  => '/mnt/nfs/labstore1006-dumps',
+        require => Mount['/mnt/nfs/labstore1006-dumps'],
+    }
+
 }
