f1b25b5ef19229b78edfe57aee0681fb4f899b85
cpettet <rush@wikimedia.org>
2018-03-16 13:09:21 -0500

    openstack: neutron-server ferm rule for default port
    Bug: T187954
    Change-Id: I5f843992b5f2adbb5749ca781777584aa6ad7cd4

--- modules/profile/manifests/openstack/base/neutron/service.pp
+++ modules/profile/manifests/openstack/base/neutron/service.pp
@@ -1,8 +1,17 @@
 class profile::openstack::base::neutron::service(
     $version = hiera('profile::openstack::base::version'),
     ) {
+
+    include ::network::constants
+    $prod_networks = join($::network::constants::production_networks, ' ')
+
     class {'::openstack::neutron::service':
         version => $version,
     }
     contain '::openstack::neutron::service'
+
+    ferm::rule{'neutron-server-api':
+        ensure => 'present',
+        rule   => "saddr (${prod_networks}) proto tcp dport (9696) ACCEPT;",
+    }
 }
