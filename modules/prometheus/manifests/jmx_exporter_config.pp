ceb3d8a98592991bfe24667c50832d27503bd965
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-03-21 09:05:32 +0100

    prometheus: unbreak all jmx-originated metrics collection
    Since I65b1aeac097be7f24, we're using a new version of the puppetdbquery
    module.
    
    We need to do the same for all instances of query_resources, and not
    just the few fixed in that commit.
    
    Let's start with the jmx metrics, which are urgent.
    
    Bug: T187259
    Change-Id: I965d1bb52a326471c88c38eadc37ca8f3e8f8073

--- modules/prometheus/manifests/jmx_exporter_config.pp
+++ modules/prometheus/manifests/jmx_exporter_config.pp
@@ -22,7 +22,7 @@ define prometheus::jmx_exporter_config(
 
     $resources = query_resources(
                   "Class[\"${class_name}\"]",
-                  'Prometheus::Jmx_exporter_instance',
+                  'Prometheus::Jmx_exporter_instance[~".*"]',
                   true)
     $site_clusters = get_clusters({'site' => $site})
 
