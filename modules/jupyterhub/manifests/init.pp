95d978882c464a75057fc9be6e527ad9ed1de342
Ottomata <aotto@wikimedia.org>
2018-03-22 10:54:16 -0400

    Install python3 packages; configure user venvs from requirements
    Bug: T183145
    Change-Id: I60486478a8579362a2c1706300bbf06ed6f5dc52

--- modules/jupyterhub/manifests/init.pp
+++ modules/jupyterhub/manifests/init.pp
@@ -38,11 +38,14 @@ class jupyterhub (
         'nodejs-legacy', # For embedded configurable-http-proxy
         'virtualenv',
         'python3-venv',
+        'python3-wheel',
         # Packages for PDF exports
         'pandoc',
         'texlive-xetex',
         'texlive-fonts-recommended',
         'texlive-generic-recommended',
+        # For pyhive
+        'libsasl2-dev',
     )
 
     $deploy_repository = 'analytics/jupyterhub/deploy'
@@ -108,7 +111,7 @@ class jupyterhub (
         content   => systemd_template('jupyterhub'),
         restart   => true,
         subscribe => [
-            File['/etc/jupyterhub/jupyterhub_config.py'],
+            File["${config_path}/jupyterhub_config.py"],
             Exec['jupyterhub_generate_cookie_secret']
         ],
         require   => [
