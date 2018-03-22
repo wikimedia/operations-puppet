bbb46acea4fa8ae01eb9a56051cde61b46717e2b
andrewbogott <abogott@wikimedia.org>
2018-03-22 09:10:31 -0500

    labweb: remove obsolete wikitech code
    Change-Id: I83823e2b1b7c210eaad1c2ad8077a0e86de691f0

--- modules/openstack/files/wikitech/mw-files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-tar -czvf /a/backup/labswiki-$(date '+%Y%m%d')-files.tar.gz -C /srv mediawiki
-tar -czvf /a/backup/public/labswiki-$(date '+%Y%m%d')-images.tar.gz -C /srv/org/wikimedia/controller/wikis images --exclude images/deleted
