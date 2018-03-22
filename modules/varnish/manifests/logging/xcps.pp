e9cc8e1fe900da51dd41fc085c77da5318e6ce38
Emanuele Rocca <ema@wikimedia.org>
2018-03-22 17:39:52 +0100

    varnishxcps: remove python daemon
    The metrics extracted by varnishxcps are available in Prometheus, and
    all dashboards depending on the statsd data have been ported/removed.
    
    Bug: T184942
    Change-Id: I5452123b11471448ab401ab9a0600ebf44343b00

--- modules/varnish/manifests/logging/xcps.pp
+++ modules/varnish/manifests/logging/xcps.pp
@@ -1,48 +1,29 @@
 # == Define: varnish::logging::xcps
 #
-# Accumulates client connection statistics from the SSL terminators
-# by parsing X-Connection-Properties headers. Reports to StatsD.
+# Mtail program parsing X-Connection-Properties header from the SSL terminators
+# and making metrics available to Prometheus.
 #
 # === Parameters
-#
-# [*statsd_server*]
-#   StatsD server address, in "host:port" format.
-#   Defaults to localhost:8125.
 #
 # === Examples
 #
 #  varnish::logging::xcps {
-#    statsd_server => 'statsd.eqiad.wmnet:8125'
 #  }
 #
-define varnish::logging::xcps( $statsd_server = 'statsd' ) {
+define varnish::logging::xcps {
     include ::varnish::common
 
     file { '/usr/local/bin/varnishxcps':
-        source  => 'puppet:///modules/varnish/varnishxcps',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0555',
-        require => File['/usr/local/lib/python2.7/dist-packages/cachestats.py'],
-        notify  => Service['varnishxcps'],
+        ensure => absent,
+        notify => Service['varnishxcps'],
     }
 
     systemd::service { 'varnishxcps':
-        ensure         => present,
-        content        => systemd_template('varnishxcps'),
-        restart        => true,
-        require        => File['/usr/local/bin/varnishxcps'],
-        subscribe      => File['/usr/local/lib/python2.7/dist-packages/cachestats.py'],
-        service_params => {
-            require => Service['varnish-frontend'],
-            enable  => true,
-        },
+        ensure => stopped,
     }
 
     nrpe::monitor_service { 'varnishxcps':
-        ensure       => present,
-        description  => 'Varnish traffic logger - varnishxcps',
-        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -a "/usr/local/bin/varnishxcps" -u root',
+        ensure => absent,
     }
 
     mtail::program { 'varnishxcps':
