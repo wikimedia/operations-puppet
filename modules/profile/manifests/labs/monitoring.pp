2edea5469fe22bf5dd706c7e4b57bdb15b01617e
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-03-23 14:31:34 +0100

    lab: monitoring: include apache mod rewrite
    Required in labmon configuration.
    
    Bug: T190515
    Change-Id: If22e45045d53f5aeb86342a3a8e7249c8dd8b4e0
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/labs/monitoring.pp
+++ modules/profile/manifests/labs/monitoring.pp
@@ -1,6 +1,9 @@
 # profile class for labs monitoring specific stuff
 
 class profile::labs::monitoring {
+
+    include ::apache::mod::rewrite
+
     $packages = [
         'python-keystoneauth1',
         'python-keystoneclient',
