e69fa4b8ddf7a7a54f36dd3cd5e5b2493b950c19
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-03-23 12:49:51 +0100

    Don't include mediawiki fonts list in toollabs::exec_environ
    This class is used to setup an excution node for Toolforge. It doesn't
    run mediawiki itself, but includes mediawiki::packages::fonts to
    establish a base line of fonts.
    
    Instead of including mediawiki::packages::fonts include a copy of the
    fonts here, the mediawiki modules will undergo heavy refactoring and
    we need to disentangle this. (A former refactoring already broke
    puppet runs on trusty-based toolforge instances).
    
    Bug: T190135
    Change-Id: I7784c120f82c41501099f30f8b14a6ce7eaa5bb0

--- modules/toollabs/manifests/exec_environ.pp
+++ modules/toollabs/manifests/exec_environ.pp
@@ -10,7 +10,113 @@ class toollabs::exec_environ {
     include ::profile::locales::extended
     include ::identd
     include ::redis::client::python
-    include ::mediawiki::packages::fonts
+
+    require_package(
+        'fonts-arabeyes',
+        'fonts-arphic-ukai',
+        'fonts-arphic-uming',
+        'fonts-farsiweb',
+        'fonts-kacst',
+        'fonts-khmeros',
+        'fonts-lao',
+        'fonts-liberation',
+        'fonts-linuxlibertine',
+        'fonts-manchufont',
+        'fonts-nafees',
+        'fonts-sil-abyssinica',
+        'fonts-sil-ezra',
+        'fonts-sil-padauk',
+        'fonts-sil-scheherazade',
+        'fonts-takao-gothic',
+        'fonts-takao-mincho',
+        'fonts-thai-tlwg',
+        'fonts-tibetan-machine',
+        'fonts-unfonts-core',
+        'fonts-unfonts-extra',
+        'texlive-fonts-recommended',
+        'ttf-alee',
+        'ttf-ubuntu-font-family',    # Not in Debian. T32288, T103325
+        'ttf-wqy-zenhei',
+        'xfonts-100dpi',
+        'xfonts-75dpi',
+        'xfonts-base',
+        'xfonts-mplus',
+        'xfonts-scalable',
+        'fonts-sil-nuosusil',        # T83288
+        'culmus',                    # T40946
+        'culmus-fancy',              # T40946
+        'fonts-lklug-sinhala',       # T57462
+        'fonts-vlgothic',            # T66002
+        'fonts-dejavu-core',         # T65206
+        'fonts-dejavu-extra',        # T65206
+        'fonts-lyx',                 # T40299
+        'fonts-crosextra-carlito',   # T84842
+        'fonts-crosextra-caladea',   # T84842
+        'fonts-smc',                 # T33950
+        'fonts-hosny-amiri',         # T135347
+        'fonts-taml-tscu',           # T117919
+    )
+
+    if os_version('ubuntu >= trusty') {
+        require_package(
+            'ttf-bengali-fonts',
+            'ttf-devanagari-fonts',
+            'ttf-gujarati-fonts',
+            'ttf-kannada-fonts',
+            'ttf-oriya-fonts',
+            'ttf-punjabi-fonts',
+            'ttf-tamil-fonts',
+            'ttf-telugu-fonts',
+            'ttf-kochi-gothic',
+            'ttf-kochi-mincho',
+            'fonts-mgopen'
+        )
+    }
+
+    if os_version('debian == jessie') {
+        require_package(
+            'fonts-noto' # T184664
+        )
+    }
+
+    if os_version('debian > jessie') {
+        require_package(
+            'fonts-noto-hinted',  # T184664
+            'fonts-noto-unhinted' # T184664
+        )
+    }
+
+    if os_version('debian >= jessie') {
+        require_package(
+            'fonts-beng',
+            'fonts-deva',
+            'fonts-gujr',
+            'fonts-knda',
+            'fonts-mlym',
+            'fonts-orya',
+            'fonts-guru',
+            'fonts-taml',
+            'fonts-telu',
+            'fonts-gujr-extra',
+            'fonts-noto-cjk',
+            'fonts-sil-lateef',
+            'fonts-ipafont-gothic',
+            'fonts-ipafont-mincho',
+        )
+    }
+
+    # In older releases (up to the version present in trusty), fontconfig-config provided
+    # a config file which forced antialiasing. This is no longer present in the versions
+    # in jessie and later, so provide it manually since otherwise some fonts look distorted
+    # in smaller resolutions: T139543
+    if os_version('debian >= jessie') {
+        file { '/etc/fonts/conf.d/10-antialias.conf':
+            source => 'puppet:///modules/mediawiki/fontconfig-antialias.conf',
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0644',
+        }
+    }
 
     # T65000
     include ::imagemagick::install
