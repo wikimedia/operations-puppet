def9ab7328309a31a7dc547588987fe4a9d48d0c
Ottomata <aotto@wikimedia.org>
2018-03-26 14:28:19 -0400

    Install python protobuf package on stat and notebook hosts for python facets
    Change-Id: I4fa86794a71f06ec38425d312ac46e01e315c1a7

--- modules/statistics/manifests/packages.pp
+++ modules/statistics/manifests/packages.pp
@@ -95,6 +95,7 @@ class statistics::packages {
         'python-pymysql',           'python3-pymysql',
         'python-virtualenv',        'python3-virtualenv', # T84378
         'python-dev',               'python3-dev',        # T83316
+        'python-protobuf',          'python3-protobuf',
     ])
 
     if os_version('debian >= jessie') {
