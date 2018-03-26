0080eae9a5da7530c1373d2c5fe87a81d162b2b8
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-03-26 13:07:54 +0200

    profile::etcd::v3: fix monitoring declaration, and monitoring script
    Change-Id: I2f4662d0f31b43155fb82a2bb51c2ac796299b01

--- modules/etcd/files/v3/check_etcd_health.sh
+++ modules/etcd/files/v3/check_etcd_health.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-_host=${1:-localhost}
+_endpoint=${1:-http://localhost:2379}
 # Check the status of the local etcd server
-HEALTH=$(/usr/bin/curl -s -L "https://${_host}:2379/health" | /usr/bin/jq '.health == "true"')
+HEALTH=$(/usr/bin/curl -s -L "${_endpoint}/health" | /usr/bin/jq '.health == "true"')
 
 if [ "${HEALTH}" == "true" ]; then
     echo "The etcd server is healthy"
