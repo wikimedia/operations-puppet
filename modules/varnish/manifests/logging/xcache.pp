23c3d30654ab928765bea3b3507a961ff4f51265
Valentin Gutierrez <vgutierrez@wikimedia.org>
2018-03-26 17:48:06 +0200

    varnish: Remove varnishxcache python daemon
    varnishxcache metrics are available in Prometheus and all
    dashboards depending on the statsd/graphite data have been ported.
    
    Bug: T184942
    Change-Id: I41b749fb511f23ea99f1bb6eef34da901ffe664b

--- modules/varnish/manifests/logging/xcache.pp
+++ modules/varnish/manifests/logging/xcache.pp
@@ -1,51 +1,30 @@
 # == Define varnish::logging::xcache:
-# Logs X-Cache hit-related stats to statsd. Expose counters to prometheus.
+# Mtail program parsing X-Cache header and
+# making hit-related stats available to Prometheus.
 #
 # === Parameters
-#
-# [*statsd_server*]
-#   StatsD server address, in "host:port" format.
-#   Defaults to localhost:8125.
-#
-# [*key_prefix*]
-#   Prefix of the stats that will be sent to statsd.
-#   Within this prefix, it will create counters named:
-#   hit-front, hit-local, hit-remote, int-front, int-local, int-remote, misspass
-#   Default 'test.varnish.xcache'
 #
 # === Examples
 #
 #  varnish::logging::xcache {
-#    statsd_server => 'statsd.eqiad.wmnet:8125',
-#    key_prefix => "varnish.${::site}.${::cluster}.xcache"
 #  }
 #
 define varnish::logging::xcache(
-    $statsd_server = 'localhost:8125',
-    $key_prefix = 'test.varnish.xcache',
 ) {
     include ::varnish::common
 
     file { '/usr/local/bin/varnishxcache':
-        source => 'puppet:///modules/varnish/varnishxcache',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
+        ensure => absent,
         notify => Service['varnishxcache'],
     }
 
     systemd::service { 'varnishxcache':
-        ensure         => present,
-        content        => systemd_template('varnishxcache'),
-        require        => File['/usr/local/bin/varnishxcache'],
-        service_params => {
-            require => Service['varnish-frontend'],
-            enable  => true,
-        },
+        ensure  => absent,
+        content => '',
     }
 
     nrpe::monitor_service { 'varnishxcache':
-        ensure       => present,
+        ensure       => absent,
         description  => 'Varnish traffic logger - varnishxcache',
         nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -a "/usr/local/bin/varnishxcache" -u root',
     }
