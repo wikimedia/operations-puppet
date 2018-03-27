915cc59d571c636ffd2e26384583a0c97bf76bed
Ottomata <aotto@wikimedia.org>
2018-03-27 15:40:28 -0400

    Fix path to check_kafka_consumer_lag nrpe check
    Bug: T189611
    Change-Id: Ic21321ce5a26b97defdb4e136131ca5f73cbf4c2

--- modules/burrow/manifests/check_consumer_lag.pp
+++ modules/burrow/manifests/check_consumer_lag.pp
@@ -38,7 +38,7 @@ define burrow::check_consumer_lag(
 
     nrpe::monitor_service { "${kafka_cluster_name}_${consumer_group}_consumer_lag":
         description   => "Kafka ${kafka_cluster_name} consumer group lag for ${consumer_group}",
-        nrpe_command  => "/usr/lib/nagios/plugins/check_kafka_consumer_lag --base-url ${burrow_uri} --kafka-cluster ${kafka_cluster_name} --consumer-group ${consumer_group} --critical-lag ${lag_threshold}",
+        nrpe_command  => "/usr/local/lib/nagios/plugins/check_kafka_consumer_lag --base-url ${burrow_uri} --kafka-cluster ${kafka_cluster_name} --consumer-group ${consumer_group} --critical-lag ${lag_threshold}",
         contact_group => $contact_group,
         critical      => $critical,
     }
