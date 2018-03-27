c465a336402d59d589cbcf534ff9d165a33e2f4f
elukey <ltoscano@wikimedia.org>
2018-03-27 14:18:55 +0200

    eventlogging: move alarms from graphite to prometheus
    Varnishkafka::eventlogging has been configured to push data
    on Kafka Jumbo, so now all the kafka related metrics are
    in Prometheus and not in graphite anymore. The check_prometheus
    class is the only one currently available in puppet to set alarm
    based on Prometheus metrics, and it is as simple as checking
    a scalar value for a certain threshold. I tried to leverage the
    quantile function to set reasonable alarms for the eventlogging
    topics that we care, but a more broader discussion might be needed
    within the Analytics team to figure out what's best.
    
    Bug: T114199
    Change-Id: Ie030bbd6da9f5052ed97f034f4b8b7c79c3aa3a4

--- modules/profile/manifests/prometheus/alerts.pp
+++ modules/profile/manifests/prometheus/alerts.pp
@@ -24,4 +24,48 @@ class profile::prometheus::alerts {
         #  For now, alert analytics admins, until alerts are more stable.
         contact_group => 'analytics',
     }
+    # Eventlogging
+
+    # Warn if p50 of overall error event throughput goes above 20 events/s
+    # in a 15 minute period.
+    # The EventError topic counted here includes both events that do not
+    # validate and events that can not be processed for other reasons
+    monitoring::check_prometheus { 'eventlogging_EventError_throughput':
+        description     => 'Throughput of EventLogging EventError events',
+        query           => 'scalar(quantile(0.50,rate(kafka_server_BrokerTopicMetrics_MessagesIn_total{cluster="kafka_jumbo",topic="eventlogging_EventError"}[15m])))',
+        prometheus_url  => 'http://prometheus.svc.eqiad.wmnet/analytics',
+        warning         => 20,
+        critical        => 30,
+        method          => 'ge',
+        contact_group   => 'analytics',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/eventlogging?panelId=13&fullscreen&orgId=1'],
+    }
+
+    # Alarms if p50 of Navigation Timing event throughput goes under 1 req/sec
+    # in a 15 min period
+    # https://meta.wikimedia.org/wiki/Schema:NavigationTiming
+    monitoring::check_prometheus { 'eventlogging_NavigationTiming_throughput':
+        description     => 'Throughput of EventLogging NavigationTiming events',
+        query           => 'scalar(quantile(0.50,rate(kafka_server_BrokerTopicMetrics_MessagesIn_total{cluster="kafka_jumbo",topic="eventlogging_NavigationTiming"}[15m])))',
+        prometheus_url  => 'http://prometheus.svc.eqiad.wmnet/analytics',
+        warning         => 1,
+        critical        => 0,
+        method          => 'le',
+        contact_group   => 'analytics',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/eventlogging?panelId=6&fullscreen&orgId=1'],
+    }
+
+    # Warn if p50 of overall event throughput goes beyond 1500 events/s
+    # in a 15 min period.
+    # These thresholds are somewhat arbtirary.
+    monitoring::check_prometheus { 'eventlogging_throughput':
+        description     => 'Throughput of EventLogging events',
+        query           => 'scalar(quantile(0.50, rate(kafka_server_BrokerTopicMetrics_MessagesIn_total{cluster="kafka_jumbo",topic="eventlogging-client-side"}[15m])))',
+        prometheus_url  => 'http://prometheus.svc.eqiad.wmnet/analytics',
+        warning         => 1500,
+        critical        => 5000,
+        method          => 'ge',
+        contact_group   => 'analytics',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/eventlogging?panelId=6&fullscreen&orgId=1'],
+    }
 }
