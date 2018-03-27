9b93e3690b1e0b2b22dfb0352d3bf51d101cd95a
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-03-27 13:57:43 +0200

    wmcs: monitoring: rsync whisper files between mon servers
    We aim to have 2 monitoring servers, master and slave (standby).
    
    The slave has a cronjob with rsync to fetch data (whisper files) from the
    master.
    
    This is following a similar approach as found at:
     modules/openstack/manifests/glance/image_sync.pp
    
    Bug: T190512
    Depends-On: I404766bf9f3e87dc403c383aa2c6c6b74f95aa09
    Change-Id: Id00140870337450fda33890fab721d3dba13ff4c
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/labs/monitoring.pp
+++ modules/profile/manifests/labs/monitoring.pp
@@ -1,14 +1,64 @@
 # profile class for labs monitoring specific stuff
 
-class profile::labs::monitoring {
+class profile::labs::monitoring (
+    $monitoring_master = hiera('wmcs::monitoring::master'),
+    $monitoring_slave = hiera('wmcs::monitoring::slave'),
+    $whisper_dir = hiera('wmcs::monitoring::whisper_dir'),
+) {
     $packages = [
         'python-keystoneauth1',
         'python-keystoneclient',
         'python-novaclient',
         'libapache2-mod-uwsgi',
+        'rsync',
     ]
 
     package { $packages:
         ensure => 'present',
+    }
+
+    #
+    # hourly cron to rsync whisper files
+    #
+    ssh::userkey { '_graphite-sshkey':
+        ensure  => 'present',
+        content => secret('ssh/wmcs/monitoring/wmcs_monitoring_rsync.pub'),
+    }
+
+    file { '/var/lib/graphite/.ssh':
+        ensure => directory,
+        owner  => '_graphite',
+        group  => '_graphite',
+        mode   => '0700',
+    }
+
+    file { '/var/lib/graphite/.ssh/id_rsa':
+        content   => secret('ssh/wmcs/monitoring/wmcs_monitoring_rsync.priv'),
+        owner     => '_graphite',
+        group     => '_graphite',
+        mode      => '0600',
+        require   => File['/var/lib/graphite/.ssh'],
+        show_diff => false,
+    }
+
+    # master / slave specific bits
+    if $facts['fqdn'] == $monitoring_master {
+        $cron_ensure = 'absent'
+
+        ferm::service { 'wmcs_monitoring_rsync_ferm':
+            proto  => 'tcp',
+            port   => '22',
+            srange => '@resolve($monitoring_slave)',
+        }
+    } else {
+        $cron_ensure = 'present'
+    }
+
+    cron { 'wmcs_monitoring_rsync_cronjob':
+        ensure  => $cron_ensure,
+        command => "/usr/bin/rsync --delete --delete-after -aSO ${whisper_dir} ${monitoring_master}:${whisper_dir}/",
+        minute  => 00,
+        user    => '_graphite',
+        require => Package['rsync'],
     }
 }
