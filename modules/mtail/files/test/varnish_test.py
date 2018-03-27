6c5cef187a5ccf15e6a7fe631e778c6c9190073d
Valentin Gutierrez <vgutierrez@wikimedia.org>
2018-03-27 16:34:59 +0200

    mtail: Provide ttfb histogram for varnishbackend
    Now that https://github.com/google/mtail/issues/106 is solved in
    mtail 3.0-rc5 and it is available in apt.wikimedia.org,
    we can implement the ttfb histogram
    
    Bug: T184942
    Change-Id: I14002e0d641650170498649ac53e6e995b8c626a

--- modules/mtail/files/test/varnish_test.py
+++ modules/mtail/files/test/varnish_test.py
@@ -90,7 +90,23 @@ class VarnishBackendTest(unittest.TestCase):
 
     def testRespStatus(self):
         s = self.store.get_samples('varnish_backend_requests_seconds_count')
-        self.assertIn(('status=200,method=GET,backend=be_wdqs_svc_eqiad_wmnet', 12), s)
-        self.assertIn(('status=204,method=GET,backend=be_bohrium_eqiad_wmnet', 2), s)
-        self.assertIn(('status=200,method=POST,backend=be_bohrium_eqiad_wmnet', 1), s)
-        self.assertIn(('status=301,method=GET,backend=be_cp1065_eqiad_wmnet', 1), s)
+        self.assertIn(('method=GET,backend=be_wdqs_svc_eqiad_wmnet', 12), s)
+        self.assertIn(('method=GET,backend=be_bohrium_eqiad_wmnet', 2), s)
+        self.assertIn(('method=POST,backend=be_bohrium_eqiad_wmnet', 1), s)
+        self.assertIn(('method=GET,backend=be_cp1065_eqiad_wmnet', 1), s)
+
+        bucket_samples = self.store.get_samples('varnish_backend_requests_seconds_bucket')
+        self.assertIn(('le=0.01,method=GET,backend=be_cp1065_eqiad_wmnet', 1),
+                      bucket_samples)
+        self.assertIn(('le=0.05,method=GET,backend=be_cp1065_eqiad_wmnet', 1),
+                      bucket_samples)
+        self.assertIn(('le=0.1,method=GET,backend=be_cp1065_eqiad_wmnet', 1),
+                      bucket_samples)
+        self.assertIn(('le=0.5,method=GET,backend=be_cp1065_eqiad_wmnet', 1),
+                      bucket_samples)
+        self.assertIn(('le=1.0,method=GET,backend=be_cp1065_eqiad_wmnet', 1),
+                      bucket_samples)
+        self.assertIn(('le=5.0,method=GET,backend=be_cp1065_eqiad_wmnet', 1),
+                      bucket_samples)
+        self.assertIn(('le=+Inf,method=GET,backend=be_cp1065_eqiad_wmnet', 1),
+                      bucket_samples)
