40f208445736ab676a8dc8dd0b873066f49e0174
Ottomata <aotto@wikimedia.org>
2018-03-27 10:51:23 -0400

    Install nrpe check for Kafka consumer lag by checking burrow
    Bug: T189611
    Change-Id: Ifd7e388687a9c069055035d011a277a059b1a363

---
+++ modules/burrow/manifests/check_consumer_lag.pp
@@ -0,0 +1,45 @@
+# == Define burrow::check_consumer_lag
+#
+# Installs an nrpe check for Kafka consumer lag on a single consumer group
+# in a Kafka cluster.
+#
+# == Parameters
+#
+#   [*title*]
+#       Kafka consumer group name to check
+#
+#   [*burrow_uri*]
+#       Default: http://localhost:8100
+#
+#   [*lag_threshold*]
+#       Critical threshold for consumer group lag.  Default 1000
+#
+#   [*contact_group*]
+#       Default: admins
+#
+#   [*critical*]
+#       Default: false
+#
+define burrow::check_consumer_lag(
+    $kafka_cluster_name,
+    $burrow_uri             = 'http://localhost:8100',
+    $lag_threshold          = 1000,
+    $contact_group          = 'admins',
+    $critical               = false,
+) {
+    $consumer_group = $title
+
+    if !defined(File['/usr/local/lib/nagios/plugins/check_kafka_consumer_lag']) {
+        file { '/usr/local/lib/nagios/plugins/check_kafka_consumer_lag':
+            source => 'puppet:///modules/burrow/check_kafka_consumer_lag.py',
+            mode   => '0555',
+        }
+    }
+
+    nrpe::monitor_service { "${kafka_cluster_name}_${consumer_group}_consumer_lag":
+        description   => "Kafka ${kafka_cluster_name} consumer group lag for ${consumer_group}",
+        nrpe_command  => "/usr/lib/nagios/plugins/check_kafka_consumer_lag --base-url ${burrow_uri} --kafka-cluster ${kafka_cluster_name} --consumer-group ${consumer_group} --critical-lag ${lag_threshold}",
+        contact_group => $contact_group,
+        critical      => $critical,
+    }
+}
