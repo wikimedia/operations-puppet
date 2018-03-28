640c152cc4b41c5283195eff535cfd7174be6dd7
cpettet <rush@wikimedia.org>
2018-03-28 16:04:40 -0500

    openstack: add manual bridges for linux bridge agent
    Bug: T188266
    Change-Id: I849be8217b96e91a83f374efe4d96e39a616b89f

---
+++ modules/openstack/manifests/neutron/bridge.pp
@@ -0,0 +1,24 @@
+define openstack::neutron::bridge(
+    $brname,
+    $interface='',
+    $ensure='present',
+    ) {
+
+    if ($ensure == 'present') {
+
+        exec {"create-${brname}-bridge":
+            command => "/sbin/brctl ${brname}",
+            unless  => "/sbin/brctl show | /bin/grep ${brname}",
+            notify  => Exec["create-${brname}-bridge-${interface}"],
+        }
+
+        # this is hokey but solves the simple case for neutron at the moment
+        if ($interface) {
+            exec {"create-${brname}-bridge-${interface}":
+                command     => "/sbin/brctl addif ${brname} ${interface}"
+                unless      => "/sbin/brctl show ${brname} | /bin/grep ${interface}",
+                refreshonly => true,
+            }
+        }
+    }
+}
