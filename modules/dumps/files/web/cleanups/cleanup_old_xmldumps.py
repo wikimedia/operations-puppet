a219c7a0c62babbeeddb05b9e441e2a3de01d07c
Ariel T. Glenn <ariel@wikimedia.org>
2018-03-29 10:43:53 +0300

    clean up dumps 'latest' links that are too old
    These can't be removed nicely via rsync, so we do it server-side
    
    Bug: T189527
    Change-Id: I5c28ed97360113adaaa065e89941f7ba0eb5420c

--- modules/dumps/files/web/cleanups/cleanup_old_xmldumps.py
+++ modules/dumps/files/web/cleanups/cleanup_old_xmldumps.py
@@ -45,7 +45,8 @@ Options:
   --keep       (-k):  path to config file describing how many
                       dumps per wiki we keep, for each file
                       containing a list of wikis
-  --wiki       (-w):  wiki for which to dump config settings
+  --wikilists  (-w):  directory of files with lists of wikis
+                      for which to do cleanup
   --subdirs    (-s):  directories in treepath must
                       match this expression in order to
                       be examined and cleaned up.
@@ -127,7 +128,7 @@ def get_keeps(keeps_conffile):
 
 class DumpsCleaner(object):
     """
-    methods for finding and cleaning up old wiki dump dirs
+    methods for finding and cleaning up old wiki dump dirs and 'latest' links
     """
     def __init__(self, keeps_conffile, wikilists_dir, dumpsdir, wikipattern, dryrun):
         self.keeps_per_list = get_keeps(keeps_conffile)
@@ -146,6 +147,42 @@ class DumpsCleaner(object):
             return []
         dirs = os.listdir(path)
         return sorted([dirname for dirname in dirs if dirname.isdigit() and len(dirname) == 8])
+
+    def get_latestlinks(self, wiki):
+        """
+        get list of 'latest' links from dumpsdir/wiki/latest
+        """
+        path = os.path.join(self.dumpsdir, wiki, 'latest')
+        if not os.path.exists(path):
+            return []
+        links = os.listdir(path)
+        return sorted(links)
+
+    def get_dates_from_filenames(self, wiki, filenames):
+        '''
+        given a list of links to filenames with date string YYYYMMDD in
+        the path, return a list of these date strings, sorted, with
+        dups filtered out. if such a string occurs more than once in
+        a filename, we take the first one.
+        return also a dict of the filenames by their datestrings.
+        filenames with no such string will be silently skipped.
+        '''
+        files_by_date = {}
+        dates = []
+        path = os.path.join(self.dumpsdir, wiki, 'latest')
+        for fname in filenames:
+            fname_path = os.path.join(path, fname)
+            if not os.path.islink(fname_path):
+                continue
+            real_file = os.readlink(fname_path)
+            fields = real_file.split('/')
+            for field in fields:
+                if len(field) == 8 and field.isdigit():
+                    files_by_date[fname] = field
+                    dates.append(field)
+                    # fail this breaks twice fixme
+                    break
+        return files_by_date, sorted(list(set(dates)))
 
     def get_keep_for_wiki(self, wiki):
         """
@@ -173,6 +210,35 @@ class DumpsCleaner(object):
             else:
                 shutil.rmtree("%s" % to_remove)
 
+    def cleanup_wiki_latestfiles(self, wiki):
+        """
+        remove links to 'latest' files for the wiki that are older
+        than two runs ago. these can't be cleaned up by rsync so we
+        do it here.
+        """
+        latestlinks = self.get_latestlinks(wiki)
+        links_by_date, dates = self.get_dates_from_filenames(wiki, latestlinks)
+        if len(dates) <= 2:
+            # nothing to do
+            return
+
+        cutoff = dates[-2]
+        path = os.path.join(self.dumpsdir, wiki, 'latest')
+        # FIXME this cleanup of rss paths hardcoded in here sucks. a lot.
+        for filename, filedate in links_by_date.iteritems():
+            if filedate < cutoff:
+                filepath = os.path.join(path, filename)
+                if self.dryrun:
+                    print "would remove link, file", filepath, filepath + '-rss.xml'
+                else:
+                    try:
+                        os.unlink(filepath)
+                        os.unlink(filepath + '-rss.xml')
+                    except Exception:
+                        # we want to hear about it but we want to continue our cleanup too
+                        print "failed to remove", filepath, filepath + '-rss.xml'
+                        continue
+
     def cleanup_wiki(self, keeps, wiki):
         """
         remove oldest dumps if we have more than the number to keep
@@ -183,12 +249,15 @@ class DumpsCleaner(object):
 
     def clean(self):
         """
-        remove oldest dumps from each wiki directory in dumpsdir
+        remove links to 'latest' files for wiki that are older than
+        two runs ago
+        also remove oldest dumps from each wiki directory in dumpsdir
         if we are keeping too many, as determined by the conffile
         describing how many we keep for wikis in each list in the
         wikilists_dir, with optional default keep value.
         """
         for wiki in self.wikistoclean:
+            self.cleanup_wiki_latestfiles(wiki)
             tokeep = self.get_keep_for_wiki(wiki)
             if tokeep is None:
                 continue
