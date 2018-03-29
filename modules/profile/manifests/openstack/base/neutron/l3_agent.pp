c592c329be580d18d453d59559078a939b0688c1
cpettet <rush@wikimedia.org>
2018-03-29 14:58:22 -0500

    openstack: neutron router l3-agent HA
    Bug: T188266
    Change-Id: I4a4621c099ba21c3e51d78d8eb9c40a3ad0d476f

--- modules/profile/manifests/openstack/base/neutron/l3_agent.pp
+++ modules/profile/manifests/openstack/base/neutron/l3_agent.pp
@@ -1,23 +1,16 @@
 class profile::openstack::base::neutron::l3_agent(
     $version = hiera('profile::openstack::base::version'),
-    $network_flat_interface = hiera('profile::openstack::base::neutron::network_flat_interface'),
-    $network_flat_tagged_base_interface = hiera('profile::openstack::base::neutron::network_flat_tagged_base_interface'),
-    $network_flat_interface_vlan = hiera('profile::openstack::base::neutron::network_flat_interface_vlan'),
     $dmz_cidr = hiera('profile::openstack::base::neutron::dmz_cidr'),
+    $external_bridge = hiera('profile::openstack::labtestn::neutron::external_bridge'),
+    $network_flat_interface_external = hiera('profile::openstack::base::neutron::network_flat_interface_external'),
     $network_public_ip = hiera('profile::openstack::base::neutron::network_public_ip'),
     ) {
-
-    interface::tagged { $network_flat_interface:
-        base_interface => $network_flat_tagged_base_interface,
-        vlan_id        => $network_flat_interface_vlan,
-        method         => 'manual',
-        up             => 'ip link set $IFACE up',
-        down           => 'ip link set $IFACE down',
-    }
 
     class {'::openstack::neutron::l3_agent':
         version           => $version,
         dmz_cidr          => $dmz_cidr,
+        bridge            => $external_bridge,
+        bridge_addif      => $network_flat_interface_external,
         network_public_ip => $network_public_ip,
     }
     contain '::openstack::neutron::l3_agent'
