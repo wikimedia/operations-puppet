86fa4aa5dffefeb81c23d9abe7731fa8b6dbceaf
cpettet <rush@wikimedia.org>
2018-04-03 08:31:52 -0500

    openstack: manage bridges and bridge mappings for l3 and compute
    Bug: T188266
    Change-Id: I1fbe874236222df82c288e38514070a4d3d73fb0

--- modules/profile/manifests/openstack/base/neutron/l3_agent.pp
+++ modules/profile/manifests/openstack/base/neutron/l3_agent.pp
@@ -1,19 +1,8 @@
 class profile::openstack::base::neutron::l3_agent(
     $version = hiera('profile::openstack::base::version'),
-    $network_flat_interface = hiera('profile::openstack::base::neutron::network_flat_interface'),
-    $network_flat_tagged_base_interface = hiera('profile::openstack::base::neutron::network_flat_tagged_base_interface'),
-    $network_flat_interface_vlan = hiera('profile::openstack::base::neutron::network_flat_interface_vlan'),
     $dmz_cidr = hiera('profile::openstack::base::neutron::dmz_cidr'),
     $network_public_ip = hiera('profile::openstack::base::neutron::network_public_ip'),
     ) {
-
-    interface::tagged { $network_flat_interface:
-        base_interface => $network_flat_tagged_base_interface,
-        vlan_id        => $network_flat_interface_vlan,
-        method         => 'manual',
-        up             => 'ip link set $IFACE up',
-        down           => 'ip link set $IFACE down',
-    }
 
     class {'::openstack::neutron::l3_agent':
         version           => $version,
