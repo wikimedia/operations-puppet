83a3f2fa7b15bf103ec56a8047c45b48731055b0
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-04-04 12:59:15 +0200

    Disable PrivateTmp via systemd override for stretch-based mediawiki setups
    The apache2 systemd unit in stretch enables PrivateTmp by default
    This makes "systemctl reload apache" fail with error code 226/EXIT_NAMESPACE
    (which is a failure to setup a mount namespace). This is specific to our
     mediawiki setup:
    
    Normally, with PrivateTmp enabled, /tmp would appear as
    /tmp/systemd-private-$ID-apache2.service-$RANDOM and /var/tmp would appear as
    /var/tmp/systemd-private-$ID-apache2.service-$RANDOM. That works fine for
    /var/tmp, but fails for /tmp (so the reload only exposes the issue)
    
    This is most definitely caused by HHVM in some way (although I have been
    unable to pinpoint where exactly). Per systemd unit ordering both start up
    in parallel and lsof -a +L1 /tmp/ shows e.g. references to deleted file
    handles owned by HHVM processed running under www-data.
    
    As such disable PrivateTmp on stretch, it causes disruptions for stretch-based setups
    and we can revisit this when phasing out HHVM, with firejail being used for
    command execution from mediawiki we're isolated those executions to a private
    file namespace anyway.
    
    To do that ship a custom systemd override when running on stretch; we have
    a cleaner mechanism to pass an override via systemd::unit, but that would require
    extensive changes and since the mediawiki classes are up for major refactoring
    soon, add this via simple file references for now
    
    Bug: T185195
    Change-Id: I007b45ae474c019f3c673f0a1f9b96fe214a4702

--- modules/mediawiki/manifests/web.pp
+++ modules/mediawiki/manifests/web.pp
@@ -55,6 +55,52 @@ class mediawiki::web {
         }
     }
 
+    # The apache2 systemd unit in stretch enables PrivateTmp by default
+    # This makes "systemctl reload apache" fail with error code 226/EXIT_NAMESPACE
+    # (which is a failure to setup a mount namespace). This is specific to our
+    # mediawiki setup:
+    # Normally, with PrivateTmp enabled, /tmp would appear as
+    # /tmp/systemd-private-$ID-apache2.service-$RANDOM and /var/tmp would appear as
+    # /var/tmp/systemd-private-$ID-apache2.service-$RANDOM. That works fine for
+    # /var/tmp, but fails for /tmp (so the reload only exposes the issue)
+    #
+    # This is most definitely caused by HHVM in some way (although I have been
+    # unable to pinpoint where exactly). Per systemd unit ordering both start up
+    # in parallel and lsof -a +L1 /tmp/ shows e.g. references to deleted file
+    # handles owned by HHVM processed running under www-data.
+    #
+    # Disable PrivateTmp on stretch, it causes disruptions for stretch-based setups
+    # and we can revisit this when phasing out HHVM, with firejail being used for
+    # command execution from mediawiki we're isolated those executions to a private
+    # file namespace anyway.
+    #
+    # To disable, ship a custom systemd override when running on stretch; we have
+    # a cleaner mechanism to pass an override via systemd::unit, but that would require
+    # extensive changes and since the mediawiki classes are up for major refactoring
+    # soon, add this via simple file references for now
+    if os_version('debian >= stretch') {
+        file { '/etc/systemd/system/apache2.service.d':
+            ensure => directory,
+            mode   => '0755',
+            owner  => 'root',
+            group  => 'root',
+        }
+
+        file { '/etc/systemd/system/apache2.service.d/override.conf':
+            ensure  => present,
+            mode    => '0755',
+            owner   => 'root',
+            group   => 'root',
+            content => "[Service]\nPrivateTmp=false\n",
+            notify  => Exec['systemctl-daemon-reload'],
+        }
+
+        exec { 'systemctl-daemon-reload':
+            command     => '/bin/systemctl daemon-reload',
+            refreshonly => true,
+        }
+    }
+
     file { '/var/lock/apache2':
         ensure => directory,
         owner  => $::mediawiki::users::web,
