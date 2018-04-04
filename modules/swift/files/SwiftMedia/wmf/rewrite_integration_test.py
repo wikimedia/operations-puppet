7edeaa4b478e63fc1afed863e3a830411ae03dbd
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-04-04 10:24:14 +0200

    swift: deprecate webob usage in rewrite.py
    Switch webob usage to Swift's internal version 'swob' since we're running
    inside Swift.
    
    Capture only 404 responses for thumbs from Swift, returning all other responses
    as-is. Also add integration tests for 416 responses on range not satisfiable.
    
    Bug: T183902
    Change-Id: I8d02f36990f1695197b90379325a65568759e5bc

--- modules/swift/files/SwiftMedia/wmf/rewrite_integration_test.py
+++ modules/swift/files/SwiftMedia/wmf/rewrite_integration_test.py
@@ -27,7 +27,7 @@ class WMFRewriteTest(unittest.TestCase):
     thumb_path_swift = (
         '/v1/AUTH_mw/wikipedia-commons-local-thumb.02/0/02/'
         'Berlin_2014_077.JPG/666px-Berlin_2014_077.JPG')
-    thumb_size = 70195
+    thumb_size = 87819
 
     def test_remapping_originals(self):
         r = requests.get('http://localhost' + self.original_path, headers=self.headers)
@@ -65,6 +65,13 @@ class WMFRewriteTest(unittest.TestCase):
             'http://localhost/wikipedia/commons/thumb/non_existant',
             headers=self.headers)
         self.assertEquals(r.status_code, 404)
+
+    # https://phabricator.wikimedia.org/T183902
+    def test_invalid_range(self):
+        headers = self.headers.copy()
+        headers.update({'Range': 'bytes=%d-' % self.thumb_size})
+        r = requests.get('http://localhost' + self.thumb_path, headers=headers)
+        self.assertEquals(r.status_code, 416)
 
 
 if __name__ == '__main__':
