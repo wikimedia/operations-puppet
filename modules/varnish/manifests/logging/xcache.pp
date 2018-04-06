4b9769146d87a12dc72ec043ad9277c9b78893e5
Valentin Gutierrez <vgutierrez@wikimedia.org>
2018-04-06 16:48:52 +0200

    varnish: varnishxcache post-removal cleanup
    Merge this one after https://gerrit.wikimedia.org/r/#/c/421925/ is
    merged and applied by puppet to finish the removal.
    
    Bug: T184942
    Change-Id: If77f24be392efff7818a77a8e962726684fff9f6

--- modules/varnish/manifests/logging/xcache.pp
+++ modules/varnish/manifests/logging/xcache.pp
@@ -13,22 +13,6 @@ define varnish::logging::xcache(
 ) {
     include ::varnish::common
 
-    file { '/usr/local/bin/varnishxcache':
-        ensure => absent,
-        notify => Service['varnishxcache'],
-    }
-
-    systemd::service { 'varnishxcache':
-        ensure  => absent,
-        content => '',
-    }
-
-    nrpe::monitor_service { 'varnishxcache':
-        ensure       => absent,
-        description  => 'Varnish traffic logger - varnishxcache',
-        nrpe_command => '/bin/true',
-    }
-
     mtail::program { 'varnishxcache':
         source => 'puppet:///modules/mtail/programs/varnishxcache.mtail',
         notify => Service['varnishmtail'],
