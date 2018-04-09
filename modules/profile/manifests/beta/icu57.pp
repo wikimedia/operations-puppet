09d80d9f117fd6b1bf5794a5f182380717cf4f45
Eddie Greiner-Petter <git@eddie-sh.de>
2018-04-09 11:47:43 +0000

    Remove profile::beta::icu57
    This declaration is now part of the main hhvm module. The module was
    already removed from deployment-prep in horizon (as it caused a failure
    with the package being applied multiple times) and is thus unused.
    
    Change-Id: I7305e7a98707ba7a02fb1edc0ced135ce4eeaadc

--- modules/profile/manifests/beta/icu57.pp
+++
@@ -1,9 +0,0 @@
-class profile::beta::icu57 {
-    if os_version('debian == jessie') {
-        apt::repository { 'hhvm-icu57':
-            uri        => 'http://apt.wikimedia.org/wikimedia',
-            dist       => 'jessie-wikimedia',
-            components => 'component/icu57',
-        }
-    }
-}
