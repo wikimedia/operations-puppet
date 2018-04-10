c07fce0686786255387ae370069e96769482ffd4
Francisco Dans <fdans@wikimedia.org>
2018-04-10 13:00:07 +0200

    Puppetize cron job archiving old MaxMind files to stat1005 and HDFS
    So far they were getting saved by a manual cron job with a script in
    @Milimetric's home directory in stat1005.
    
    This change adds a cron job that will check for changes in the maxmind
    database, and each time there's a difference it will take a copy of its
    current state, timestamp it and archive it in /srv/geoip/archive. It will
    also copy it to HDFS.
    
    Tested locally the script, the cron job and the mail alert.
    
    Change-Id: I0a3b0e85ccd0abd797192166ec62e6a721552bb7

---
+++ modules/geoip/files/archive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+MAXMIND_DB_SOURCE_DIR=${1:"/usr/lib/GeoIP"}
+MAXMIND_DB_ARCHIVE_DIR=${2:"$MAXMIND_DB_SOURCE_DIR/archive"}
+HDFS_ARCHIVE_DIR=${3:"/wmf/data/archive/geoip"}
+
+CURRENT_DATE=$(date +'%Y-%m-%d')
+
+# hardlinking to avoid duplicating too much data
+cp -rl "$HDFS_ARCHIVE_DIR/." "$MAXMIND_DB_ARCHIVE_DIR/$CURRENT_DATE"
+
+hdfs dfs -mkdir -p $HDFS_ARCHIVE_DIR
+hdfs dfs -put -f "$MAXMIND_DB_ARCHIVE_DIR/$CURRENT_DATE" "$HDFS_ARCHIVE_DIR"
---
+++ modules/geoip/manifests/data/archive.pp
@@ -0,0 +1,40 @@
+# == Class geoip::data::archive
+#
+
+# Sets up a cron job that grabs the latest version of the MaxMind database, puts it
+# in a timestamped directory in /srv/geoip/archive, and pushes its contents to hdfs.
+
+class geoip::data::archive(
+    $maxmind_db_source_dir = '/usr/share/GeoIP',
+    $hdfs_archive_dir = '/wmf/data/archive/geoip'
+) {
+    $archive_dir = "${maxmind_db_source_dir}/archive"
+    # Puppet assigns 755 permissions to files and dirs, so the script can be ran
+    # manually without sudo.
+    file { $archive_dir:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'wikidev',
+    }
+
+    $archive_script = '/usr/local/bin/geoip_archive.sh'
+
+    file { $archive_script:
+        ensure  => file,
+        owner   => 'root',
+        group   => 'wikidev',
+        content => file('geoip/archive.sh')
+    }
+
+    $archive_command = "/bin/sh ${archive_script} ${maxmind_db_source_dir} ${archive_dir} ${hdfs_archive_dir} > /dev/null"
+
+    cron { 'archive-maxmind-geoip-database':
+        ensure      => present,
+        command     => $archive_command,
+        environment => 'MAILTO=analytics-alerts@wikimedia.org',
+        user        => root,
+        weekday     => 3,
+        hour        => 5,
+        minute      => 30
+    }
+}
