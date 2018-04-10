1bb6d599bca98cbec709ee684674f38b0e99ce7f
Francisco Dans <fdans@wikimedia.org>
2018-04-10 13:00:07 +0200

    Puppetize cron job archiving old MaxMind databases
    So far they were getting saved by a manual cron job with a script in
    @Milimetric's home directory in stat1005.
    
    This change moves the cron job to puppet. Makes sure that the directory
    tree exists, that the repo will have been created and that it will send
    an email to analytics-alerts@wikimedia.org if anything fails.
    
    Tested locally the script, the cron job and the mail alert.
    
    Bug: T136732
    Change-Id: I5a9b0e29a09d8549957e53f4ed8599898c6baa79

---
+++ modules/geoip/manifests/data/archive.pp
@@ -0,0 +1,45 @@
+# == Class geoip::data::archive
+#
+
+# Sets up a cron to archive every version of the MaxMind GeoIP database in a
+# local git repository.
+
+class geoip::data::archive(
+    $maxmind_db_source_dir = '/usr/share/GeoIP',
+    $working_dir = '/srv/GeoIP'
+) {
+    $archive_dir = "${working_dir}/archive"
+    $repo_dir = "${archive_dir}/MaxMind"
+  
+    if ! defined(File[$repo_dir]) {
+        file { $working_dir:
+            ensure => directory,
+        }
+        file { $archive_dir:
+            ensure => directory,
+        }
+        file { $repo_dir:
+            ensure => directory,
+        }
+    }
+  
+    $maxmind_dir = '/usr/share/GeoIP'
+    file { "${archive_dir}/archive.sh":
+        ensure  => file,
+        owner   => 'root',
+        group   => 'wikidev',
+        mode    => '0700',
+        content => template('geoip/archive.sh.erb')
+    }
+  
+    $archive_command = "${archive_dir}/archive.sh"
+  
+    cron { 'archive-maxmind-geoip-database':
+        ensure      => present,
+        command     => $archive_command,
+        environment => 'MAILTO=analytics-alerts@wikimedia.org',
+        user        => root,
+        hour        => 5,
+        minute      => 30
+    }
+}
