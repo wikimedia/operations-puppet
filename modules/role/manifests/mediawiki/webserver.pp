81de0041c10b7fa2374e612690c9611afd3b7e95
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-04-11 12:38:24 +0200

    Disable PrivateTmp via systemd override for stretch-based app servers
    The apache2 systemd unit in stretch enables PrivateTmp by default
    This makes "systemctl reload apache" fail with error code 226/EXIT_NAMESPACE
    (which is a failure to setup a mount namespace). This is specific to our
     mediawiki setup:
    
    Normally, with PrivateTmp enabled, /tmp would appear as
    /tmp/systemd-private-$ID-apache2.service-$RANDOM and /var/tmp would appear as
    /var/tmp/systemd-private-$ID-apache2.service-$RANDOM. That works fine for
    /var/tmp, but fails for /tmp (so the reload only exposes the issue)
    
    As such disable PrivateTmp on stretch, it causes disruptions for stretch-based setups
    and we can revisit this when phasing out HHVM, with firejail being used for
    command execution from mediawiki we've isolated those executions to a private
    file namespace anyway.
    
    To do that ship a custom systemd override when running on stretch; we have
    a cleaner mechanism to pass an override via systemd::unit, but that would require
    extensive changes and since the mediawiki classes are up for major refactoring
    soon, add this via simple file references for now
    
    Bug: T185195
    Change-Id: I90f8d211799b885e7aa210e8cea4e4ac4763898b

--- modules/role/manifests/mediawiki/webserver.pp
+++ modules/role/manifests/mediawiki/webserver.pp
@@ -48,6 +48,43 @@ class role::mediawiki::webserver {
         srange  => '$DOMAIN_NETWORKS',
     }
 
+    # The apache2 systemd unit in stretch enables PrivateTmp by default
+    # This makes "systemctl reload apache" fail with error code 226/EXIT_NAMESPACE
+    # (which is a failure to setup a mount namespace). This is specific to our
+    # mediawiki setup: Normally, with PrivateTmp enabled, /tmp would appear as
+    # /tmp/systemd-private-$ID-apache2.service-$RANDOM and /var/tmp would appear as
+    # /var/tmp/systemd-private-$ID-apache2.service-$RANDOM. That works fine for
+    # /var/tmp, but fails for /tmp (so the reload only exposes the issue)
+    #
+    # Disable PrivateTmp on stretch for now; we can revisit this when phasing out HHVM.
+    #
+    # To disable, ship a custom systemd override when running on stretch; we have
+    # a cleaner mechanism to pass an override via systemd::unit, but that would require
+    # extensive changes and since the mediawiki classes are up for major refactoring
+    # soon, add this via simple file references for now
+    if os_version('debian >= stretch') {
+        file { '/etc/systemd/system/apache2.service.d':
+            ensure => directory,
+            mode   => '0755',
+            owner  => 'root',
+            group  => 'root',
+        }
+
+        file { '/etc/systemd/system/apache2.service.d/override.conf':
+            ensure  => present,
+            mode    => '0755',
+            owner   => 'root',
+            group   => 'root',
+            content => "[Service]\nPrivateTmp=false\n",
+            notify  => Exec['systemctl-daemon-reload'],
+        }
+
+        exec { 'systemctl-daemon-reload':
+            command     => '/bin/systemctl daemon-reload',
+            refreshonly => true,
+        }
+    }
+
     # If a service check happens to run while we are performing a
     # graceful restart of Apache, we want to try again before declaring
     # defeat. See T103008.
