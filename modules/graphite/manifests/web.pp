73e0b8546292dfe4eaa41ac75f97d3628518fbaa
Alexandros Kosiaris <akosiaris@wikimedia.org>
2018-04-13 15:09:53 +0300

    graphite::web: Stop using --autoload in uwsgi
    Relying on autoload can have adverse affects to the application like
    loading the python2 plugin instead of the python3. Applications should
    be explicit about the plugins they use and not rely on autoloading
    
    Bug: T192102
    Change-Id: I6cfd983c06ecf91b3bcc23a5c6a2ce125116fc7c

--- modules/graphite/manifests/web.pp
+++ modules/graphite/manifests/web.pp
@@ -118,8 +118,7 @@ class graphite::web(
     }
 
     uwsgi::app { 'graphite-web':
-        service_settings => '--die-on-term --autoload',
-        settings         => {
+        settings => {
             uwsgi => {
                 'plugins'   => 'python',
                 'socket'    => '/run/uwsgi/graphite-web.sock',
@@ -129,7 +128,7 @@ class graphite::web(
                 'processes' => $uwsgi_processes,
             },
         },
-        require          => File['/var/log/graphite-web'],
+        require  => File['/var/log/graphite-web'],
     }
 
     file { '/usr/local/sbin/graphite-index':
