60592416880a9b6aebd2dc8b80d087153e4ce0ab
Riccardo Coccioli <rcoccioli@wikimedia.org>
2018-04-13 12:36:18 +0200

    debmonitor: add server side puppettization
    * Install and configure the server part of DebMonitor with uWSGI
    * Install and congigure Nginx to listen on port 80 for the traffic that
      will come from Varnish and on port 443 for the internal endpoint.
    
    Bug: T191299
    Change-Id: I80d342a27b0033a08b2fcdc5f582516376d96512

---
+++ modules/role/manifests/debmonitor/server.pp
@@ -0,0 +1,18 @@
+# Class: role::debmonitor::server
+#
+# This role installs all the Debmonitor server related parts as WMF requires it
+#
+# Actions:
+#       Deploy Debmonitor server
+#
+# Sample Usage:
+#       role(debmonitor::server)
+#
+
+class role::debmonitor::server {
+    system::role { 'debmonitor::server': description => 'Debmonitor server' }
+
+    include ::profile::base::firewall
+    include ::standard
+    include ::profile::debmonitor::server
+}
