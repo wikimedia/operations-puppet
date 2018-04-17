b766064243cc37a8ecf15d01bd3fda1fb4d251bd
Ottomata <aotto@wikimedia.org>
2018-04-17 12:48:54 -0400

    Don't need to pin backports for R on jessie anymore
    Bug: T192348
    Change-Id: I6e69a4ac63ea161d7fbf74691f0db0e4babfd4e2

--- modules/profile/manifests/hadoop/worker.pp
+++ modules/profile/manifests/hadoop/worker.pp
@@ -46,6 +46,11 @@ class profile::hadoop::worker(
     # Install MaxMind databases for geocoding UDFs
     class { '::geoip': }
 
+    # Need R for Spark2R.
+    class { '::r_lang': }
+    # Note: RMariaDB (https://github.com/rstats-db/RMariaDB) will replace RMySQL, but is currently not on CRAN
+    require_package('r-cran-rmysql')
+
     # Install packages that are useful for distributed
     # computation in Hadoop, and thus should be available on
     # any Hadoop nodes.
@@ -91,33 +96,6 @@ class profile::hadoop::worker(
             ensure  => 'installed',
             require => Package['python3-numpy'],
         }
-    }
-
-    # Need R for Spark2R.
-    # Use R from Jessie Backports on jessie boxes.
-    if os_version('debian == jessie') {
-        $r_packages = [
-            'r-base',
-            'r-base-dev',      # Needed for R packages that have to compile C++ code; see T147682
-            'r-cran-rmysql',
-            'r-recommended'    # CRAN-recommended packages (e.g. MASS, Matrix, boot)
-        ]
-
-        # Explicitly list package dependencies, instead of using require_package,
-        # to avoid dependency issues with the 'before' clause in apt::pin.
-        package { $r_packages:
-            ensure => present,
-        }
-
-        apt::pin { $r_packages:
-            pin      => 'release a=jessie-backports',
-            priority => '1001',
-            before   => Package[$r_packages],
-        }
-    }
-    else {
-        class { '::r_lang': }
-        require_package('r-cran-rmysql') # Note: RMariaDB (https://github.com/rstats-db/RMariaDB) will replace RMySQL, but is currently not on CRAN
     }
 
 
