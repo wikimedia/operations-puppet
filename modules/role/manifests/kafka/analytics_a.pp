976f586694dc3280521b26a685463426b32c5a7f
Ottomata <aotto@wikimedia.org>
2018-04-18 11:05:10 -0400

    Split role::kafka::analytics into different roles so we can apply different hiera
    We currently colocate 2 different instances of MirrorMaker on the analytics Kafka
    brokers.  main -> analytics and main -> jumbo.  This split allows us to apply
    the same profile::kafka::mirror class on the broker nodes, but select
    and apply different configurations from hiera.
    
    This should be a no-op as is, but a future patch will switch the main -> analytics
    instances (role::kafka::analytics_a) to use profile::kafka::mirror.
    
    Bug: T192387
    Change-Id: I88998e26cb0f331004defc8b144b5d9a5fa3050d

---
+++ modules/role/manifests/kafka/analytics_a.pp
@@ -0,0 +1,22 @@
+# Compound role for analytics kafka brokers.
+#
+# This 'analytics_b' role is temporary, and will be removed after
+# The Kafka analytics cluster is decomissioned.
+# It exists so we can selectively apply hiera differently
+# to profile::kafka::mirror to mirror from main -> jumbo.
+#
+class role::kafka::analytics_a {
+    system::role { 'kafka_analytics': }
+    # Kafka brokers are routed via IPv6 so that
+    # other DCs can address without public IPv4
+    # addresses.
+    interface::add_ip6_mapped { 'main': }
+
+    include ::role::kafka::analytics::broker
+    # TODO: use profile::kafka::broker as part of https://phabricator.wikimedia.org/T192387
+    include ::role::kafka::analytics::mirror
+
+    include ::role::ipsec
+    include ::standard
+    include ::base::firewall
+}
---
+++ modules/role/manifests/kafka/analytics_b.pp
@@ -0,0 +1,21 @@
+# Compound role for analytics kafka brokers.
+#
+# This 'analytics_b' role is temporary, and will be removed after
+# The Kafka analytics cluster is decomissioned.
+# It exists so we can selectively apply hiera differently
+# to profile::kafka::mirror to mirror from main -> jumbo.
+#
+class role::kafka::analytics_b {
+    system::role { 'kafka_analytics': }
+    # Kafka brokers are routed via IPv6 so that
+    # other DCs can address without public IPv4
+    # addresses.
+    interface::add_ip6_mapped { 'main': }
+
+    include ::role::kafka::analytics::broker
+    include ::profile::kafka::mirror
+
+    include ::role::ipsec
+    include ::standard
+    include ::base::firewall
+}
