7fd91f6e9d7ccecb228d18a698d4530d1175b6fb
Daniel Zahn <dzahn@wikimedia.org>
2018-04-18 14:46:31 -0700

    releases: add directory for parsoid archive
    Bug: T150672
    Change-Id: I0415184adf3f67d3117f906f19aeb80d0446e550

---
+++ modules/profile/manifests/releases/parsoid.pp
@@ -0,0 +1,11 @@
+# server hosting (an archive of) Parsoid releases
+# https://releases.wikimedia.org/parsoid/
+class profile::releases::parsoid {
+
+    file { '/srv/org/wikimedia/releases/parsoid':
+        ensure => 'directory',
+        owner  => 'releasers-parsoid',
+        group  => 'releasers-parsoid', 
+    }
+
+}
