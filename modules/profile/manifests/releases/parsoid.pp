b6b60a69e8631076b9e70f411308456210d4692b
Daniel Zahn <dzahn@wikimedia.org>
2018-04-19 07:36:16 -0700

    releases-parsoid: setup rsync between releases servers
    Bug: T150672
    Change-Id: I7c52adfa9a56a08ecb2fb9b90969158559d898e0

--- modules/profile/manifests/releases/parsoid.pp
+++ modules/profile/manifests/releases/parsoid.pp
@@ -1,12 +1,21 @@
 # server hosting (an archive of) Parsoid releases
 # https://releases.wikimedia.org/parsoid/
-class profile::releases::parsoid {
-
+class profile::releases::parsoid (
+    $active_server = hiera('releases_server'),
+    $passive_server = hiera('releases_server_failover'),
+){
     file { '/srv/org/wikimedia/releases/parsoid':
         ensure => 'directory',
         owner  => 'root',
         group  => 'releasers-parsoid',
         mode   => '2775',
+    }
+
+    rsync::quickdatacopy { 'srv-org-wikimedia-releases-parsoid':
+      ensure      => present,
+      source_host => $active_server,
+      dest_host   => $passive_server,
+      module_path => '/srv/org/wikimedia/releases/parsoid',
     }
 
 }
