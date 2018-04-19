1a377570ea9c11274fee26ded6965ab4e3bbc87a
Ariel T. Glenn <ariel@wikimedia.org>
2018-04-19 10:31:50 +0300

    set actimeo=0 on snapshot1006 nfs mount for the next dump run
    Bug: T191177
    Change-Id: I0dd2202968f2b98ddc6775082d83440739d19938

--- modules/snapshot/manifests/dumps/datamount.pp
+++ modules/snapshot/manifests/dumps/datamount.pp
@@ -3,6 +3,7 @@ define snapshot::dumps::datamount(
     $mount_type = undef,
     $server = undef,
     $managed_subdirs = [],
+    $extra_mountopts = undef,
     $user = undef,
     $group = undef,
 ) {
@@ -17,6 +18,12 @@ define snapshot::dumps::datamount(
         }
     }
 
+    $standard_mountopts = 'bg,hard,tcp,rsize=8192,wsize=8192,intr,nfsvers=3'
+    if ($extra_mountopts == '') {
+        $mountopts = "${standard_mountopts},${extra_mountopts}"
+    } else {
+        $mountopts = $standard_mountopts
+    }
     if ($mount_type == 'nfs') {
         require_package('nfs-common')
 
@@ -25,7 +32,7 @@ define snapshot::dumps::datamount(
             device   => "${server}:/data",
             fstype   => 'nfs',
             name     => $mountpoint,
-            options  => 'bg,hard,tcp,rsize=8192,wsize=8192,intr,nfsvers=3',
+            options  => $mountopts,
             require  => File[$mountpoint],
             remounts => false,
         }
