4d25ed80007aa901295efa29fdc28a4582a5d01c
Daniel Zahn <dzahn@wikimedia.org>
2018-04-19 07:05:55 -0700

    releases-parsoid: add directory and fix permissions
    Bug: T150672
    Change-Id: Ibad570a5bbe8fb8070d385bf80f72392dd75572e

---
+++ modules/profile/manifests/releases/parsoid.pp
@@ -0,0 +1,12 @@
+# server hosting (an archive of) Parsoid releases
+# https://releases.wikimedia.org/parsoid/
+class profile::releases::parsoid {
+
+    file { '/srv/org/wikimedia/releases/parsoid':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'releasers-parsoid',
+        mode   => '2775',
+    }
+
+}
