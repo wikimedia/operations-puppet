75b815dc055aaa0db8bec0d40445bbfa4c813d90
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-04-23 12:09:08 +0200

    Remove obsolete fontconfig/imagemagick code from mediawiki::multimedia
    With the removal of the image scaler code, mediawiki::multimedia is only
    used by the video scalers:
    
    - The fontconfig snippet was used to reject the use of fixed size
      fonts by rsvg-convert on the image scalers (T139543)
    - The magick-tmp related puppet code was used for image format
      conversion done via imagemagick
    
    Since the functionality of both is now fully assumed by Thumbor and isn't
    relevant to the video scalers, remove it.
    
    Change-Id: I757e39b1ed0b401665d4e656d2e58c0991bb680c

--- modules/mediawiki/manifests/multimedia.pp
+++ modules/mediawiki/manifests/multimedia.pp
@@ -8,27 +8,4 @@ class mediawiki::multimedia {
     include ::mediawiki::packages::fonts
     include ::mediawiki::users
     include ::mediawiki::firejail
-
-    file { '/etc/fonts/conf.d/70-no-bitmaps.conf':
-        source  => 'puppet:///modules/mediawiki/fontconfig-no-bitmaps.conf',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        require => Package['fontconfig-config'],
-    }
-
-    file { '/tmp/magick-tmp':
-        ensure => directory,
-        owner  => $::mediawiki::users::web,
-        group  => 'root',
-        mode   => '0755',
-    }
-
-    tidy { [ '/tmp', '/tmp/magick-tmp' ]:
-        matches => [ '*.png', 'EasyTimeline.*', 'gs_*', 'localcopy_*', 'magick-*', 'transform_*', 'vips-*.v' ],
-        age     => '2h',
-        type    => 'ctime',
-        backup  => false,
-        recurse => 1,
-    }
 }
