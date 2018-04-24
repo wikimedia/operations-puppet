1ceef6c3bbf463833b8fb6b25c73fe347a171f8b
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-04-24 19:04:22 +0200

    labs_bootstrapvz: add labtest-jessie specific firstboot.sh script
    We may need some specific bits for the labtest deployment.
    
    Bug: T181523
    Change-Id: Ia7073075dac89aeb74a8a7ec67ec12b99e60d46e
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/labs_bootstrapvz/files/firstboot.sh
+++ modules/labs_bootstrapvz/files/firstboot.sh
@@ -82,6 +82,13 @@ do
     /sbin/dhclient -1 eth0
 done
 
+# at this point, if no hostname, try to fetch it from openstack itself
+if [ "$(hostname)" == "localhost" ] ; then
+    echo "Trying to fetch hostname from Openstack"
+    hostname=$(curl -s http://169.254.169.254/openstack/latest/meta_data.json/ | sed -r 's/^.*name\": \"//'  | sed -r 's/\".*$//g')
+    hostname $hostname
+fi
+
 project=`curl http://169.254.169.254/openstack/latest/meta_data.json/ | sed -r 's/^.*project_id\": \"//'  | sed -r 's/\".*$//g'`
 ip=`curl http://169.254.169.254/1.0/meta-data/local-ipv4 2> /dev/null`
 
@@ -100,10 +107,66 @@ fi
 
 
 fqdn=${hostname}.${project}.${domain}
-master="puppet"
+if [ "${domain}" == "eqiad.wmflabs" ]
+then
+	master="labs-puppetmaster.wikimedia.org"
+	ldaphosts="ldap://ldap-labs.eqiad.wikimedia.org:389 ldap://ldap-labs.codfw.wikimedia.org:389"
+fi
+if [ "${domain}" == "codfw.wmflabs" ]
+then
+	master="labs-puppetmaster.wikimedia.org"
+	ldaphosts="ldap://ldap-labs.codfw.wikimedia.org:389 ldap://ldap-labs.eqiad.wikimedia.org:389"
+fi
+if [ "${domain}" == "codfw.labtest" ]
+then
+	master="labtest-puppetmaster.wikimedia.org"
+	ldaphosts="ldap://labtestservices2001.wikimedia.org:389"
+
+	# The labtest ldap password is the prod password prepended with lt-
+	sed -i "s%^bindpw\s*%bindpw          lt-%g" /etc/ldap.conf
+	sed -i "s%^bindpw\s*%BINDPW          lt-%g" /etc/ldap/ldap.conf
+	sed -i "s%^bindpw\s*%bindpw lt-%g" /etc/nslcd.conf
+fi
+
+# Finish LDAP configuration
 sed -i "s/_PROJECT_/${project}/g" /etc/security/access.conf
+sed -i "s/_PROJECT_/${project}/g" /etc/ldap/ldap.conf
+sed -i "s/_PROJECT_/${project}/g" /etc/sudo-ldap.conf
+sed -i "s/_PROJECT_/${project}/g" /etc/nslcd.conf
+sed -i "s%^uri.*%uri ${ldaphosts}%g" /etc/nslcd.conf
 sed -i "s/_FQDN_/${fqdn}/g" /etc/puppet/puppet.conf
 sed -i "s/_MASTER_/${master}/g" /etc/puppet/puppet.conf
+sed -i "s%^uri.*%uri             ${ldaphosts}%g" /etc/ldap.conf
+sed -i "s%^URI.*%URI             ${ldaphosts}%g" /etc/ldap/ldap.conf
+
+# Set resolv.conf and stop anyone else from messing with it.
+echo "" > /sbin/resolvconf
+mkdir /etc/dhcp/dhclient-enter-hooks.d
+cat > /etc/dhcp/dhclient-enter-hooks.d/nodnsupdate <<EOF
+#!/bin/sh
+make_resolv_conf() {
+        :
+}
+EOF
+
+if [ "${domain}" == "codfw.labtest" ]
+then
+	# Just hardcode the IP for labtest-recursor0, in case we
+        #  can't dig yet
+	nameserver='208.80.153.51'
+else
+	nameserver=`/usr/bin/dig +short labs-recursor0.wikimedia.org`
+fi
+
+cat > /etc/resolv.conf <<EOF
+domain ${project}.${domain}
+search ${project}.${domain} ${domain}
+nameserver ${nameserver}
+options timeout:5 ndots:2
+EOF
+
+echo "$ip	$fqdn $hostname" >> /etc/hosts
+echo $hostname > /etc/hostname
 
 # This is only needed when running bootstrap-vz on
 # a puppetmaster::self instance, and even then
@@ -119,23 +182,14 @@ nscd -i hosts
 # set mailname
 echo $fqdn > /etc/mailname
 
-if [[ $domain = *"labtest"* ]]; then
-# On labtest and labtestn need to use a proxy for apt
-    echo 'Acquire::http::Proxy "http://208.80.153.75:5001";' > /etc/apt/apt.conf.d/01proxy
-    echo 'Acquire::https::Proxy "https://208.80.153.75:5001";' >> /etc/apt/apt.conf.d/01proxy
-fi
-
 apt-get update
 
-# Make sure nothing has leaked in certwise
-rm -rf /var/lib/puppet/ssl/*
-
+rm -rf /var/lib/puppet/ssl
 puppet agent --enable
 # Run puppet, twice.  The second time is just to pick up packages
 #  that may have been unavailable in apt before the first puppet run
 #  updated sources.list
 puppet agent --onetime --verbose --no-daemonize --no-splay --show_diff --waitforcert=10 --certname=${fqdn} --server=${master}
-
 apt-get update
 puppet agent -t
 
