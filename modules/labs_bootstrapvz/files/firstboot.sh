9f77de29a576914c1a0a7d443a72ddc8b7ca3687
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-04-24 19:04:22 +0200

    labs_bootstrapvz: address labtest issues
    Remove the whole /var/lib/puppet/ssl directory in firstboot.sh script,
    also, use 'labtest-puppetmaster.wikimedia.org' as puppetmaster, otherwise
    we may see an invalid certificate.
    
    Bug: T181523
    Change-Id: Ia7073075dac89aeb74a8a7ec67ec12b99e60d46e
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/labs_bootstrapvz/files/firstboot.sh
+++ modules/labs_bootstrapvz/files/firstboot.sh
@@ -100,7 +100,11 @@ fi
 
 
 fqdn=${hostname}.${project}.${domain}
-master="puppet"
+if [ "${domain}" == "codfw.labtest" ] ; then
+	master="labtest-puppetmaster.wikimedia.org"
+else
+	master="puppet"
+fi
 sed -i "s/_PROJECT_/${project}/g" /etc/security/access.conf
 sed -i "s/_FQDN_/${fqdn}/g" /etc/puppet/puppet.conf
 sed -i "s/_MASTER_/${master}/g" /etc/puppet/puppet.conf
@@ -128,7 +132,7 @@ fi
 apt-get update
 
 # Make sure nothing has leaked in certwise
-rm -rf /var/lib/puppet/ssl/*
+rm -rf /var/lib/puppet/ssl
 
 puppet agent --enable
 # Run puppet, twice.  The second time is just to pick up packages
