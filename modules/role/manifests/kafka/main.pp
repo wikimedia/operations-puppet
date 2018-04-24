72db43c1d4e4f22ba66469f3c1883932339683e9
Ottomata <aotto@wikimedia.org>
2018-04-24 14:50:25 -0400

    Use profile::kafka::broker in main-eqiad
    Bug: T192831
    Change-Id: I4e6ff6387c072ede1651ca62cc3af1807f16d7d5

--- modules/role/manifests/kafka/main.pp
+++ modules/role/manifests/kafka/main.pp
@@ -1,19 +1,15 @@
 # Compound role for the Kafka "main" cluster
 class role::kafka::main {
-    if $::site == 'eqiad' {
-        include ::role::kafka::main::broker
-    }
-    else {
-        system::role { 'kafka::main':
-            description => "Kafka Broker in the main-${::site} Kafka cluster",
-        }
-        include ::profile::kafka::broker
+
+    include ::profile::kafka::broker
+    system::role { 'kafka::main':
+        description => "Kafka Broker in the main-${::site} Kafka cluster",
     }
 
+    include ::role::eventbus::eventbus
     system::role { 'eventbus':
         description => 'EventBus HTTP Service node',
     }
-    include ::role::eventbus::eventbus
 
     if $::realm == 'production' {
         # Mirror eqiad.* topics from main-eqiad into main-codfw,
