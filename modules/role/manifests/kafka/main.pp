302dc4418d31ede6e5e21210b9f260065a9cbe97
Ottomata <aotto@wikimedia.org>
2018-04-24 10:51:48 -0400

    Use profile::kafka::broker for main-codfw
    Also add system::role info into role::kafka::main class
    
    Bug: T192831
    Change-Id: I19bfc44eecda9eaf8b48271bc71896e64f538be4

--- modules/role/manifests/kafka/main.pp
+++ modules/role/manifests/kafka/main.pp
@@ -1,11 +1,26 @@
 # Compound role for the Kafka "main" cluster
 class role::kafka::main {
-    include ::role::kafka::main::broker
+    if $::site == 'eqiad' {
+        include ::role::kafka::main::broker
+    }
+    else {
+        system::role { 'kafka::main':
+            description => "Kafka Broker in the main-${::site} Kafka cluster",
+        }
+        include ::profile::kafka::broker
+    }
+
+    system::role { 'eventbus':
+        description => 'EventBus HTTP Service node',
+    }
     include ::role::eventbus::eventbus
 
     if $::realm == 'production' {
         # Mirror eqiad.* topics from main-eqiad into main-codfw,
         # or mirror codfw.* topics from main-codfw into main-eqiad.
+        system::role { 'kafka::mirror':
+            description => 'main Kafka cluster cross-DC MirrorMaker node',
+        }
         include ::profile::kafka::mirror
     }
 
