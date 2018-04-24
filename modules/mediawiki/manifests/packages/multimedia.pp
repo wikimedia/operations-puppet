59f2ecdfb6b44c009395e199226fab1e435d815d
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-04-24 15:33:00 +0200

    Remove obsolete mediawiki multimedia packages
    Originally the class was shared by image scalers and video scalers, but
    with the image scalers replaced by Thumbor several of those are obsolete:
    
    - fontconfig-config was used to ship font settings relevant for SVG scaling
    - libimage-exiftool-perl ships exiftool, used as $wgExiftool for lossless
      ICC profile swapping
    - libjpeg-turbo-progs ships jpegtran, used as $wgJpegTran
    - libogg0 wasn't needed anyway; libvorbis0a depends on libogg, and libavcodec57
      (one of the core libraries of ffmpeg) depends on libogg. It's generally
      better to not hardcode such library names anyway, they will change if the
      SONAME of the libs change and ffmpeg will always pull it whatever is needed
      and however it's called
    - Same for libvorbisenc2, also pulled in via libavcodec57/ffmpeg
    - libvips-tools ships vips, used by $wgVipsCommand
    - netpbm ships pnmtojpeg, used by $wgDjvuPostProcessor
    
    Change-Id: I4e9353f0a10ab689b33dc5b9c09ef7189373b810

--- modules/mediawiki/manifests/packages/multimedia.pp
+++ modules/mediawiki/manifests/packages/multimedia.pp
@@ -5,13 +5,6 @@
 class mediawiki::packages::multimedia {
     package { [
         'ffmpeg',
-        'fontconfig-config',
-        'libimage-exiftool-perl',
-        'libjpeg-turbo-progs',
-        'libogg0',
-        'libvips-tools',
-        'libvorbisenc2',
-        'netpbm',
         'oggvideotools',
     ]:
         ensure => present,
