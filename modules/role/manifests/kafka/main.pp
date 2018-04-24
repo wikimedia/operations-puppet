695154ac9ac49562bd805d4087a971bb4a4d8d84
Ottomata <aotto@wikimedia.org>
2018-04-24 10:15:31 -0400

    role::kafka::main - only include MirrorMaker in prod
    We don't have a remote Cloud VPS DC to mirror Kafka Main from.
    
    Bug: T192831
    Change-Id: Iecd11d814d9b31432dd96eaefd0fa081020622dd

--- modules/role/manifests/kafka/main.pp
+++ modules/role/manifests/kafka/main.pp
@@ -1,12 +1,13 @@
 # Compound role for the Kafka "main" cluster
 class role::kafka::main {
     include ::role::kafka::main::broker
-
     include ::role::eventbus::eventbus
 
-    # Mirror eqiad.* topics from main-eqiad into main-codfw,
-    # or mirror codfw.* topics from main-codfw into main-eqiad.
-    include ::profile::kafka::mirror
+    if $::realm == 'production' {
+        # Mirror eqiad.* topics from main-eqiad into main-codfw,
+        # or mirror codfw.* topics from main-codfw into main-eqiad.
+        include ::profile::kafka::mirror
+    }
 
     include ::standard
 }
