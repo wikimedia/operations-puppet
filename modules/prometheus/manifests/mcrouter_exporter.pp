e2b44f74a8c7ae858f3ff75b9e41a2e029a3cd8f
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-04-25 15:01:25 +0200

    Add puppetization for mcrouter_exporter
    Bug: T192763
    Change-Id: Ic3fd9f50460ad9b8d2db7692e4f1c8e01ae15f8f

---
+++ modules/prometheus/manifests/mcrouter_exporter.pp
@@ -0,0 +1,28 @@
+# == Define: prometheus::mcrouter_exporter
+#
+# Prometheus exporter for mcrouter server metrics.
+#
+# = Parameters
+#
+# [*arguments*]
+#   Additional command line arguments for prometheus-mcrouter-exporter.
+
+define prometheus::mcrouter_exporter (
+    $arguments = '',
+) {
+    require_package('prometheus-mcrouter-exporter')
+
+    file { '/etc/default/prometheus-mcrouter-exporter':
+        ensure  => present,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => "ARGS=\"${arguments}\"",
+        notify  => Service['prometheus-mcrouter-exporter'],
+    }
+
+    service { 'prometheus-mcrouter-exporter':
+        ensure  => running,
+        require => Package['prometheus-mcrouter-exporter'],
+    }
+}
