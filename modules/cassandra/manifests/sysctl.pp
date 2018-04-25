69a79633b431080e97ff518010b62ecf01e39e21
Eric Evans <eevans@wikimedia.org>
2018-04-25 15:50:07 -0500

    cassandra: increase `vm.max_map_count` to 1048575
    This addresses several JVM crashes that occurred in the RESTBase
    cluster during a period of decommissions/bootstraps (lots of
    additional file IO).
    
    Bug: T193083
    Change-Id: I311eafc651e2b196d419d88df1901e494f6932b6

--- modules/cassandra/manifests/sysctl.pp
+++ modules/cassandra/manifests/sysctl.pp
@@ -11,18 +11,27 @@
 # [*vm_dirty_background_bytes*]
 #   The `vm.dirty_background_bytes' kernel parameter
 #   Default: 0
+# [*vm_max_map_count*]
+#   The `vm.max_map_count` kernel parameter. The maximum number of memory map
+#   areas a process may have.
+#   Default: 1048575
 
 class cassandra::sysctl(
     $vm_dirty_background_bytes = 0,
+    $vm_max_map_count = 1048575,
 ){
     if (!is_integer($vm_dirty_background_bytes)) {
         fail('vm_dirty_background_bytes must be a number')
+    }
+    if (!is_integer($vm_max_map_count)) {
+        fail('vm_max_map_count must be a number')
     }
 
     # 05-cassandra.conf
     sysctl::parameters { 'cassandra':
         values   => {
             'vm.dirty_background_bytes' => $vm_dirty_background_bytes,
+            'vm.max_map_count'          => $vm_max_map_count,
         },
         priority => 5,
     }
