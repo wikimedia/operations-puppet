c4d74ab1bf1d998020124304c050a28c97d04fdd
Ian Marlier <imarlier@wikimedia.org>
2018-04-26 13:29:06 -0400

    Make webperf role install coal things
    * Refactor the coal module so that the web portion and the
      kafka consumer portion are set up seperately.
    * Add the kafka consumer portion to the webperf profile,
      which will result in the webperf machines having an appropriate
      scap target, and an appropriate service
    * Add the web portion to the performance profile, which is
      actually used by graphite (just somewhat poorly named)
    * Remove the 'coal' user, which isn't needed any longer, since
      graphite is writing the whisper files.  Run coal as 'nobody'
    * Stop creating a symlink from /var/lib/carbon/whisper/coal to
      /var/lib/coal.  This will _not_ remove the directory or anything.
      Cleanup will be needed: stop coal; remove the symlink; create the
      target dir as a real directory; move the files there.
    
    Confirmation that coal user isn't needed:
        imarlier@graphite1001:~$ find / -user coal -or -group coal -ls 2> /dev/null
        imarlier@graphite1001:~$
    
    Bug: T186774
    Change-Id: Id5dd66855523a5ac5209a136afbac420ddad1882

---
+++ modules/coal/manifests/processor.pp
@@ -0,0 +1,80 @@
+# == Class: coal::processor
+#
+# Configure and run the processor side of the coal service.  Reads from Kafka,
+# writes to graphite
+#
+# === Parameters
+#
+# [*kafka_brokers*]
+#   List of kafka brokers to use for bootstrapping
+#
+# [*kafka_consumer_group*]
+#   Name of the consumer group to use for Kafka
+#
+# [*el_schemas*]
+#   Event Logging schemas that should be read from Kafka.  Topic names are
+#   derived from these values (eventlogging_$schema)
+#
+# [*log_dir*]
+#   Directory where coal's logs should be written by rsyslog
+#
+# [*graphite_host*]
+#   Hostname to which graphite metrics are sent.  
+#   Defaults to graphite-in.eqiad.wmnet
+#
+# [*graphite_port*]
+#   Port for graphite metrics. Defaults to 2003 (standard plain-text port)
+#
+# [*graphite_prefix*]
+#   Beginning of the graphite metric path.  Defaults to "coal", which results
+#   in metrics like "coal.domInteractive"
+#
+class coal::processor(
+    $kafka_brokers,
+    $kafka_consumer_group = "coal_${::site}",
+    $el_schemas = ['NavigationTiming', 'SaveTiming'],
+    $log_dir = '/var/log/coal',
+    $graphite_host = 'graphite-in.eqiad.wmnet',
+    $graphite_port = 2003,
+    $graphite_prefix = 'coal'
+) {
+    # Include common elements
+    include ::coal::common
+
+    require_package('python-kafka')
+    require_package('python-dateutil')
+    require_package('python-etcd')
+
+    file { $log_dir:
+        ensure => directory,
+        owner  => 'nobody',
+        group  => 'nogroup',
+        mode   => '0755',
+    }
+
+    logrotate::rule { 'coal':
+        ensure       => present,
+        file_glob    => "${log_dir}/*.log",
+        not_if_empty => true,
+        max_age      => 30,
+        rotate       => 7,
+        date_ext     => true,
+        compress     => true,
+        missing_ok   => true,
+    }
+
+    rsyslog::conf { 'coal':
+        content  => template('coal/rsyslog.conf.erb'),
+        priority => 80,
+    }
+
+    systemd::service { 'coal':
+        ensure  => present,
+        content => systemd_template('coal'),
+        restart => true,
+        require => [
+            File[$log_dir]
+        ],
+    }
+
+}
