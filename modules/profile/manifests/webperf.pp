1e744ce30288a06fae27b200adcdd98c555712fb
Ian Marlier <imarlier@wikimedia.org>
2018-04-26 13:29:06 -0400

    Make webperf role install coal things
    * Refactor the coal module so that the web portion and the
      kafka consumer portion are set up seperately.
    * Add the kafka consumer portion to the webperf profile,
      which will result in the webperf machines having an appropriate
      scap target, and an appropriate service
    * Add the web portion to the performance profile, which is
      actually used by graphite (just somewhat poorly named)
    * Remove the 'coal' user, which isn't needed any longer, since
      graphite is writing the whisper files.  Run coal as 'nobody'
    * Stop creating a symlink from /var/lib/carbon/whisper/coal to
      /var/lib/coal.  This will _not_ remove the directory or anything.
      Cleanup will be needed: stop coal; remove the symlink; create the
      target dir as a real directory; move the files there.
    
    Bug: 186774
    Change-Id: Id5dd66855523a5ac5209a136afbac420ddad1882

--- modules/profile/manifests/webperf.pp
+++ modules/profile/manifests/webperf.pp
@@ -33,4 +33,11 @@ class profile::webperf(
         statsd_host   => $statsd_host,
         statsd_port   => $statsd_port,
     }
+
+    # Make a valid target for coal, and set up what's needed for the consumer
+    # Consumes from the jumbo-eqiad cluster, just like navtiming
+    class { '::coal' }
+    class { '::coal::consumer':
+        kafka_brokers => $kafka_brokers
+    }
 }
