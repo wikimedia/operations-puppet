b28987e08e7a9a8dbfda47e8647d34ea60311cfe
Ian Marlier <imarlier@wikimedia.org>
2018-04-26 13:29:06 -0400

    Make webperf role install coal things
    * Refactor the coal module so that the web portion and the
      kafka consumer portion are set up seperately.
    * Add the kafka consumer portion to the webperf profile,
      which will result in the webperf machines having an appropriate
      scap target, and an appropriate service
    * Add the web portion to the performance profile, which is
      actually used by graphite (just somewhat poorly named)
    * Remove the 'coal' user, which isn't needed any longer, since
      graphite is writing the whisper files.  Run coal as 'nobody'
    * Stop creating a symlink from /var/lib/carbon/whisper/coal to
      /var/lib/coal.  This will _not_ remove the directory or anything.
      Cleanup will be needed: stop coal; remove the symlink; create the
      target dir as a real directory; move the files there.
    
    Change-Id: Id5dd66855523a5ac5209a136afbac420ddad1882

---
+++ modules/coal/manifests/web.pp
@@ -0,0 +1,24 @@
+# == Class: coal::web
+#
+# Run the web portion of the coal service (provides metrics for
+# performance.wikimedia.org)
+#
+class coal::web() {
+    require_package('python-flask')
+    require_package('python-numpy')
+    require_package('python-whisper')
+
+    uwsgi::app { 'coal':
+        service_settings => '--die-on-term',
+        settings         => {
+            uwsgi => {
+                'plugins'   => 'python',
+                'socket'    => '/run/uwsgi/coal.sock',
+                'wsgi-file' => '/srv/deployment/performance/coal/coal/coal_web.py',
+                'callable'  => 'app',
+                'master'    => true,
+                'processes' => 8,
+            },
+        },
+    }
+}
