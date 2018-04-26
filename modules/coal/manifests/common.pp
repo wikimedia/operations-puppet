8ab9647fbbac284ae0d7ed0df68a96b63e162b93
Ian Marlier <imarlier@wikimedia.org>
2018-04-26 13:29:06 -0400

    Make webperf role install coal things
    * Refactor the coal module so that the web portion and the
      kafka consumer portion are set up seperately.
    * Add the kafka consumer portion to the webperf profile,
      which will result in the webperf machines having an appropriate
      scap target, and an appropriate service
    * Add the web portion to the performance profile, which is
      actually used by graphite (just somewhat poorly named)
    * Remove the 'coal' user, which isn't needed any longer, since
      graphite is writing the whisper files.  Run coal as 'nobody'
    * Stop creating a symlink from /var/lib/carbon/whisper/coal to
      /var/lib/coal.  This will _not_ remove the directory or anything.
      Cleanup will be needed: stop coal; remove the symlink; create the
      target dir as a real directory; move the files there.
    
    Confirmation that coal user isn't needed:
        imarlier@graphite1001:~$ find / -user coal -or -group coal -ls 2> /dev/null
        imarlier@graphite1001:~$
    
    Bug: T186774
    Change-Id: Id5dd66855523a5ac5209a136afbac420ddad1882

---
+++ modules/coal/manifests/common.pp
@@ -0,0 +1,39 @@
+# == Class: coal::common
+#
+# Coal 
+#   1) processes and stores NavTiming metrics (coal::processor)
+#   2) Delivers metrics to performance.wikimedia.org (coal::web)
+#
+# This file contains directives that are common to both parts
+#
+class coal::common {
+    # Clean up some things that used to make sense but don't any more
+    user { 'coal':
+        ensure => absent,
+    }
+
+    group { 'coal':
+        ensure => absent,
+    }
+
+    file { '/usr/local/bin/coal':
+        ensure => absent,
+    }
+
+    file { '/usr/local/bin/coal-web':
+        ensure => absent,
+    }
+
+    # Make sure that scap target is there for all types.
+    scap::target { 'performance/coal':
+        service_name => 'coal',
+        deploy_user  => 'deploy-service',
+        sudo_rules   => [
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-coal start',
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-coal stop',
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-coal restart',
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-coal reload',
+            'ALL=(root) NOPASSWD: /usr/sbin/service uwsgi-coal status'
+        ]
+    }
+}
