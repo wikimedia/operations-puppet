79906029fd0f8b2f252290587cddefc359bdf62b
Ariel T. Glenn <ariel@wikimedia.org>
2018-04-26 16:59:00 +0300

    pull phabricator dumps from phab server to dumps web server
    Bug: T188726
    Bug: T188149
    Change-Id: Ifb3495b2ad493a95d481a85b77061eabf5714b19

--- modules/profile/manifests/phabricator/main.pp
+++ modules/profile/manifests/phabricator/main.pp
@@ -33,13 +33,13 @@ class profile::phabricator::main (
     # dumps are only enabled on the active server set in Hiera
     $phabricator_active_server = hiera('phabricator_active_server')
     if $::hostname == $phabricator_active_server {
-        $dump_rsync_ensure = 'present'
         $dump_enabled = true
+        $rsync_cfg_enabled = true
         $ferm_ensure = 'present'
         $aphlict_ensure = 'present'
     } else {
-        $dump_rsync_ensure ='absent'
         $dump_enabled = false
+        $rsync_cfg_enabled = false
         $ferm_ensure = 'absent'
         $aphlict_ensure = 'absent'
     }
@@ -221,12 +221,25 @@ class profile::phabricator::main (
         require         => Package[$deploy_target]
     }
 
-    cron { 'phab_dump':
-        ensure  => $dump_rsync_ensure,
-        command => 'rsync -zpt --bwlimit=40000 -4 /srv/dumps/phabricator_public.dump dataset1001.wikimedia.org::other_misc/ >/dev/null 2>&1',
-        user    => 'root',
-        minute  => '10',
-        hour    => '4',
+    if $rsync_cfg_enabled {
+        class { '::rsync::server': }
+
+        $rsync_client = 'dumps.wikimedia.org'
+        rsync::server::module { 'srvdumps':
+            path        => '/srv/dumps',
+            read_only   => 'yes',
+            hosts_allow => $rsync_client,
+        }
+        ferm::service {'phabdumps_rsyncd_ipv4':
+            port   => '873',
+            proto  => 'tcp',
+            srange => "@resolve((${rsync_client}))",
+        }
+        ferm::service {'phabdumps_rsyncd_ipv6':
+            port   => '873',
+            proto  => 'tcp',
+            srange => "@resolve((${rsync_client}),AAAA)",
+        }
     }
 
     # restart apache periodically to free up deadlocked workers. See T187790
