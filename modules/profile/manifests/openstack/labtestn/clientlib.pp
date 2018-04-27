13da4fd13f4b8898b2245bd075d7d5e218d70ced
andrewbogott <abogott@wikimedia.org>
2018-04-27 13:59:33 -0500

    openstack: refactor 'cloudrepo' setup
    Now we have two main things we can include: 'clientrepo' and 'cloudrepo'.
    On Debian, 'clientrepo' will only pin the exact client packages that
    we want.  in 'cloudrepo' we'll just prefer backports for everything.
    
    Bug: T192162
    Change-Id: I1de022ef9c62ace0b0ed7965f8cebe51b5b7d99d

--- modules/profile/manifests/openstack/labtestn/clientlib.pp
+++ modules/profile/manifests/openstack/labtestn/clientlib.pp
@@ -1,8 +1,6 @@
 class profile::openstack::labtestn::clientlib(
     $version = hiera('profile::openstack::labtestn::version'),
     ) {
-
-    require ::profile::openstack::base::backports
     class {'profile::openstack::base::clientlib':
         version => $version
     }
