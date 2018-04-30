9fffe9a0d8e4319eb5eda5d926703007c08ab2bc
Ariel T. Glenn <ariel@wikimedia.org>
2018-04-30 10:31:03 +0300

    stop rsync of web logs from dataset1001
    Bug: T182540
    Change-Id: Ia2c0ed5d6f41d8f03b4a0373a30e1c62b5b18c9b

--- modules/profile/manifests/dumps/web/xmldumps_active.pp
+++ modules/profile/manifests/dumps/web/xmldumps_active.pp
@@ -2,9 +2,4 @@
 # Can be deprecated after migrating to labstore1006|7
 class profile::dumps::web::xmldumps_active {
     require profile::dumps::web::xmldumps_common
-
-    # copy dumps web server logs to stat host
-    class {'::dumps::web::rsync::nginxlogs':
-        dest => 'stat1005.eqiad.wmnet::srv/log/webrequest/archive/dumps.wikimedia.org/',
-    }
 }
