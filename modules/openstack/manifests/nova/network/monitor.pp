ea261468fb9971b979bc0924426b6bd6f65c385c
cpettet <rush@wikimedia.org>
2018-05-01 14:12:04 -0500

    openstack: nova-network service stopped on net secondary
    Add this setup so that all it takes is conversion
    to primary to start up relevant services.
    
    Change-Id: Ib6e1b909ce8be3decde225f5f149ecbc204ddde3

--- modules/openstack/manifests/nova/network/monitor.pp
+++ modules/openstack/manifests/nova/network/monitor.pp
@@ -1,16 +1,7 @@
 class openstack::nova::network::monitor(
-    $active,
     $critical=false,
     $contact_groups='wmcs-bots,admins',
     ) {
-
-    # monitoring::service doesn't take a bool
-    if $active {
-        $ensure = 'present'
-    }
-    else {
-        $ensure = 'absent'
-    }
 
     file { '/usr/lib/nagios/plugins/check_conntrack':
         source => 'puppet:///modules/base/firewall/check_conntrack.py',
@@ -18,7 +9,7 @@ class openstack::nova::network::monitor(
     }
 
     nrpe::monitor_service { 'check_nova_network_process':
-        ensure        => $ensure,
+        ensure        => 'present',
         critical      => $critical,
         description   => 'nova-network process',
         nrpe_command  => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array '^/usr/bin/python /usr/bin/nova-network'",
@@ -26,7 +17,7 @@ class openstack::nova::network::monitor(
     }
 
     nrpe::monitor_service { 'conntrack_table_size':
-        ensure        => $ensure,
+        ensure        => 'present',
         critical      => $critical,
         description   => 'Check size of conntrack table',
         nrpe_command  => '/usr/lib/nagios/plugins/check_conntrack 80 90',
