f4ce1792c75ff7d2d1e07c7b16df766e20f77666
Francisco Dans <fdans@wikimedia.org>
2018-05-01 15:50:29 +0200

    Small improvements to the geoip archive script
    This change adds echoes to the script to illustrate better what it's
    doing, as well as adding 744 permissions to it so that it can be exectuted
    by users.
    
    Bug: T136732
    Change-Id: I3aa56d45a419bfda209b2a94cd11e7e6b09b8a82

--- modules/geoip/files/archive.sh
+++ modules/geoip/files/archive.sh
@@ -6,8 +6,11 @@ HDFS_ARCHIVE_DIR=${3:-"/wmf/data/archive/geoip"}
 
 CURRENT_DATE=$(date +'%Y-%m-%d')
 
-# hardlinking to avoid duplicating too much data
+echo "Hardlink copying $MAXMIND_DB_SOURCE_DIR to $MAXMIND_DB_ARCHIVE_DIR/$CURRENT_DATE"
+
 cp -rl "$MAXMIND_DB_SOURCE_DIR/." "$MAXMIND_DB_ARCHIVE_DIR/$CURRENT_DATE"
+
+echo "Copying $MAXMIND_DB_ARCHIVE_DIR/$CURRENT_DATE into HDFS at $HDFS_ARCHIVE_DIR"
 
 hdfs dfs -mkdir -p $HDFS_ARCHIVE_DIR
 hdfs dfs -put -f "$MAXMIND_DB_ARCHIVE_DIR/$CURRENT_DATE" "$HDFS_ARCHIVE_DIR"
