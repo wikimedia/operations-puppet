887694cc4caf0b013b64a91c1cf50e679694e976
Ian Marlier <imarlier@wikimedia.org>
2018-05-02 13:46:12 -0400

    coal: require requests module; deploy to webperf
    Coal-web has been modified to use the graphite API instead of
    reading directly from whisper files.  This adds the new required
    module (requests), and enables the coal-web service on webperf.
    
    Also adds the performance website config to webperfX001, since
    using the graphite API removes the dependency on that machine.
    
    Bug: T159354
    Change-Id: I0677c2c610a6897ef3f0cf0b9a9094f2f7349ca1

--- modules/profile/manifests/performance/site.pp
+++ modules/profile/manifests/performance/site.pp
@@ -7,6 +7,8 @@ class profile::performance::site {
 
     require ::profile::performance::coal
 
+    include ::apache
+
     file { '/srv/org':
         ensure => directory,
         owner  => 'root',
@@ -25,7 +27,8 @@ class profile::performance::site {
         owner     => 'www-data',
         group     => 'www-data',
         directory => '/srv/org/wikimedia/performance',
-        notify    => Service['apache2']
+        notify    => Service['apache2'],
+        require   => Package['apache2']
     }
 
     httpd::site { 'performance.wikimedia.org':
