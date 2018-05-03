5168aa15ca823ab24b6688fe0755250bc88a7b93
Dzahn <dzahn@wikimedia.org>
2018-05-03 21:23:42 +0000

    Revert "Revert "mwmaint1001: add mediawiki-maintenance role""
    This reverts commit 9615b3b0a6e2edbee34e053bafcf7e224c17ef7c.
    
    Change-Id: I7294d5d4c813ab3cd96eab8ab775922b2a2e76ca

--- manifests/site.pp
+++ manifests/site.pp
@@ -1439,7 +1439,7 @@ node /^mw12(2[1-9]|3[0-5])\.eqiad\.wmnet$/ {
 
 # Former imagescaler temp. used as terbium stand-in (T192185)
 node 'mwmaint1001.eqiad.wmnet' {
-    # role(mediawiki_maintenance)
+    role(mediawiki_maintenance)
     # lint:ignore:wmf_styleguide
     interface::add_ip6_mapped { 'main': }
     # lint:endignore
