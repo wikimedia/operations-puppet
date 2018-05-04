8c5cfac65f36204de703165223647d9a48e02ca6
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-05-04 16:12:15 +0200

    Remove support for trusty in mediawiki classes
    All snapshot hosts are now running stretch.
    
    Change-Id: Ifb61ab4c6ee10bb94bb543acf06187d21930d543

--- modules/mediawiki/manifests/packages/math.pp
+++ modules/mediawiki/manifests/packages/math.pp
@@ -5,7 +5,7 @@
 class mediawiki::packages::math {
     require_package('dvipng', 'gsfonts', 'make', 'ocaml', 'ploticus')
 
-    if os_version('ubuntu == trusty') or os_version('debian == jessie') {
+    if os_version('debian == jessie') {
         require_package('mediawiki-math-texvc')
     } else {
         require_package('texvc')
