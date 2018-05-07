a3f6c1fe54539c2a7e0cb48d9789ad7405d6d511
Ariel T. Glenn <ariel@wikimedia.org>
2018-05-07 13:46:50 +0300

    remove a bunch of manifests only used on dataset1001, ms1001
    Bug: T182540
    Change-Id: I4e3bf8e0b7c86a4aaa6ea75710b23601c916ea5c

--- modules/profile/manifests/dumps/web/xmldumps_active.pp
+++
@@ -1,5 +0,0 @@
-# Profile applied to the current dataset serving hosts
-# Can be deprecated after migrating to labstore1006|7
-class profile::dumps::web::xmldumps_active {
-    require profile::dumps::web::xmldumps_common
-}
