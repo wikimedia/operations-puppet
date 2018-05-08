e3dc401cb84d5c90449f5e40d6be01c36e5a2efe
Keith Herron <kherron@wikimedia.org>
2018-05-08 15:41:16 -0400

    logstash: add tcp tls input for syslogs
    Add a logstash TLS listener for syslogs on port 16514 and monitor the
    new service with icinga check_ssl.
    
    Bug: T193766
    Change-Id: If169c5bb97d5f68bb017adfe05b5232ebcca9ac1

--- modules/logstash/manifests/input/tcp.pp
+++ modules/logstash/manifests/input/tcp.pp
@@ -9,6 +9,9 @@
 # - $port: Port to listen for udp2log input on. Default '5229'.
 # - $codec: Codec to decode tcp stream input. Default 'plain'.
 # - $priority: Configuration loading priority. Default '10'.
+# - $ssl_enable: Enable/disable ssl support in logstash input. Default false.
+# - $ssl_cert: Path to ssl certificate file. Requred when ssl is enabled.
+# - $ssl_key: Path to ssl key file. Required when ssl is enabled.
 #
 # == Sample usage:
 #
@@ -18,11 +21,22 @@
 #   }
 #
 define logstash::input::tcp(
-    $ensure   = present,
-    $port     = 5229,
-    $codec    = 'plain',
-    $priority = 10,
+    Wmflib::Ensure   $ensure     = present,
+    String           $type       = 'tcp',
+    String           $codec      = 'plain',
+    Integer          $port       = 5229,
+    Integer          $priority   = 10,
+    Boolean          $ssl_enable = false,
+    Optional[String] $ssl_cert   = undef,
+    Optional[String] $ssl_key    = undef,
 ) {
+
+    # Ensure cert/key params look like filesystem paths when ssl is enabled.
+    if $ssl_enable == true {
+      validate_absolute_path($ssl_cert)
+      validate_absolute_path($ssl_key)
+    }
+
     logstash::conf { "input-tcp-${title}":
         ensure   => $ensure,
         content  => template('logstash/input/tcp.erb'),
