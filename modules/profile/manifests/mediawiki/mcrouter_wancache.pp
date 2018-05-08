c07be748be2516171d1cedc17adfb7a427b5c720
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-05-08 13:53:41 +0200

    profile::mediawiki::mcrouter_wancache: add ssl, proxy support
    Bug: T192370
    Change-Id: I9abf9b6a8d5cf6e2fb22b26426542b6804c72545

--- modules/profile/manifests/mediawiki/mcrouter_wancache.pp
+++ modules/profile/manifests/mediawiki/mcrouter_wancache.pp
@@ -1,33 +1,43 @@
 # Class profile::mcrouter_wancache
 #
 # Configures a mcrouter instance for multi-datacenter caching
+# TODO: fix notrack defs
+# TODO: use a non-root user
 class profile::mediawiki::mcrouter_wancache(
     Hash $servers_by_datacenter_category = hiera('mcrouter::shards'),
-    Integer $port = hiera('profile::mediawiki::mcrouter_wancache::port')
+    Integer $port = hiera('profile::mediawiki::mcrouter_wancache::port'),
+    Boolean $has_ssl = hiera('profile::mediawiki::mcrouter_wancache::has_ssl')
 ) {
     $servers_by_datacenter = $servers_by_datacenter_category['wancache']
+    $proxies_by_datacenter = pick($servers_by_datacenter_category['proxies'], {})
 
     $pool_configs = $servers_by_datacenter.map |$region, $servers| {
-        $servers_ = $servers; # https://github.com/rodjek/puppet-lint/issues/464
-        {
-            $region => {
-                'servers' => $servers_.map |$shard_slot, $address| {
-                    "${address['host']}:${address['port']}"
-                }
-            }
+        # We need to get the servers from the current datacenter, and the proxies from the others
+        if $region == $::site {
+            profile::mcrouter_pools($region, $servers)
+        }
+        else {
+            profile::mcrouter_pools($region, $proxies_by_datacenter[$region])
         }
     }
     $pools = $pool_configs.reduce |$memo, $value| {
         $memo + $value
     }
 
+    $route_config_all_regions = {
+        'type'     => 'AllFastestRoute',
+        'children' => keys($servers_by_datacenter).map |$region| { "PoolRoute|${region}" }
+    }
+
     $routes = union(
+        # local cache for each region
         $servers_by_datacenter.map |$region, $servers| {
             {
                 'aliases' => [ "/${region}/mw/" ],
                 'route'   => "PoolRoute|${region}"
             }
         },
+        # WAN cache: read locally, set/delete everywhere
         $servers_by_datacenter.map |$region, $servers| {
             {
                 'aliases' => [ "/${region}/mw-wan/" ],
@@ -35,13 +45,54 @@ class profile::mediawiki::mcrouter_wancache(
                     'type'           => 'OperationSelectorRoute',
                     'default_policy' => "PoolRoute|${region}",
                     'operation_policies' => {
-                        'set'    => "AllFastestRoute|Pool|${region}",
-                        'delete' => "AllFastestRoute|Pool|${region}"
+                        'set'    => $route_config_all_regions,
+                        'delete' => $route_config_all_regions,
                     }
                 }
             }
         }
     )
+    if $has_ssl {
+        file { '/etc/mcrouter/ssl':
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0750',
+        }
+        file { '/etc/mcrouter/ssl/ca.pem':
+            ensure => present,
+            source => 'puppet:///modules/profile/mcrouter/ssl/ca.pem',
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+        }
+
+        file { '/etc/mcrouter/ssl/cert.pem':
+            ensure => present,
+            source => secret("mcrouter/ssl/${::ipaddress}.cert.pem"),
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+        }
+
+        file { '/etc/mcrouter/ssl/key.pem':
+            ensure => present,
+            source => secret("mcrouter/ssl/${::ipaddress}.key.pem"),
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0400',
+        }
+
+        $ssl_options = {
+            'port' => ($port + 1),
+            'ca_cert' => '/etc/mcrouter/ssl/ca.pem',
+            'cert' => '/etc/mcrouter/ssl/cert.pem',
+            'key' => '/etc/mcrouter/ssl/key.pem',
+        }
+    }
+    else {
+        $ssl_options = undef
+    }
 
     class { '::mcrouter':
         pools                    => $pools,
@@ -50,7 +101,8 @@ class profile::mediawiki::mcrouter_wancache(
         cluster                  => 'mw',
         cross_region_timeout_ms  => 250,
         cross_cluster_timeout_ms => 1000,
-        port                     => $port
+        port                     => $port,
+        ssl_options              => $ssl_options,
     }
 
     class { '::mcrouter::monitoring': }
