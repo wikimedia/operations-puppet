3df2e88a9e62921222cd6f19a73aee754d41092a
Ottomata <aotto@wikimedia.org>
2018-05-10 16:57:02 -0400

    Puppetize Turnilo (Pivot replacement)
    This patch should include everything needed to deploy
    Turnilo from analytics/turnilo/deploy, except for
    the varnish directory configuration, and DNS changes.
    
    Once this is verified to work, and we decide we want to
    we can enable turnilo.wikimedia.org via varnish and DNS.
    
    Bug: T194427
    Change-Id: Iabf31250e8d8aa4ddad188e5fdbbb6615072c4c1

---
+++ modules/profile/manifests/druid/turnilo.pp
@@ -0,0 +1,34 @@
+# Class: profile::druid::turnilo
+#
+# Install and configure the Druid's Turnilo nodejs UI
+#
+# [*druid_clusters*]
+#
+# [*port*]
+#   The port used by Turnilo to accept HTTP connections.
+#   Default: 9091
+#
+# [*monitoring_enabled*]
+#   Enable monitoring for the Pivot service.
+#   Default: false
+#
+# [*contact_group*]
+#   Monitoring's contact grup.
+#   Default: 'analytics'
+#
+class profile::druid::turnilo(
+    $druid_clusters     = hiera('profile::druid::turnilo::druid_clusters'),
+    $port               = hiera('profile::druid::turnilo::port', 9091),
+    $monitoring_enabled = hiera('profile::druid::turnilo::monitoring_enabled', false),
+    $contact_group      = hiera('profile::druid::turnilo::contact_group', 'analytics'),
+) {
+    class { 'turnilo':
+        druid_clusters => $druid_clusters,
+    }
+
+    monitoring::service { 'turnilo':
+        description   => 'turnilo',
+        check_command => "check_tcp!${port}",
+        contact_group => $contact_group,
+    }
+}
