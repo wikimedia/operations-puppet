76ef1b9d07c5963ac77d9c30d9699680b0db5a0f
cpettet <rush@wikimedia.org>
2018-05-10 13:45:46 -0700

    openstack: resolve in puppet for labtest keystone ferm
    Change-Id: I22620a938aa4bac745a3725837298e34939b442f

--- modules/profile/manifests/openstack/labtest/keystone/service.pp
+++ modules/profile/manifests/openstack/labtest/keystone/service.pp
@@ -91,10 +91,12 @@ class profile::openstack::labtest::keystone::service(
 
     # Since the DB for keystone is local and we want keystone
     # in the other codfw deployment to access we add this rule
+
     ferm::rule{'keystone_for_cross_region':
         ensure => 'present',
-        rule   => "saddr (${labtestn_nova_controller} ${labtestn_nova_controller_standby}
-                             ) proto tcp dport (3306) ACCEPT;",
+        rule   => "saddr (@resolve(${labtestn_nova_controller})
+                          @resolve(${labtestn_nova_controller_standby})
+                          ) proto tcp dport (3306) ACCEPT;",
     }
 }
 
