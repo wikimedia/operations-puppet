c7ae59c32cf5182251e2a7537eef9644ec303c95
Ariel T. Glenn <ariel@wikimedia.org>
2018-05-10 13:44:54 +0300

    role for new misc dumps cron host
    In our current setup, some xml/sql dumps run on the misc dumps
    cron host; in the new setup, it will be dedicated for misc dumps
    cron jobs only.
    
    Bug: T181936
    Change-Id: I55f93e02ff2495b0a18663dee5f78ea9faf62899

---
+++ modules/role/manifests/dumps/generation/worker/dumper_misc_crons_only.pp
@@ -0,0 +1,12 @@
+class role::dumps::generation::worker::dumper_misc_crons_only {
+    include standard
+    include ::profile::base::firewall
+
+    include profile::dumps::generation::worker::common
+    include profile::dumps::generation::worker::cronrunner
+    include profile::dumps::generation::worker::monitor
+
+    system::role { 'snapshot::dumper_misc':
+        description => 'dumper of XML/SQL wiki content, misc dumps, monitor',
+    }
+}
