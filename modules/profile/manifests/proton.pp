338c63f82cae780e4459d4219fee5a1f747290a0
Marko Obrovac <mobrovac@wikimedia.org>
2018-05-10 10:09:56 +0200

    Proton: Add role and profile
    Proton is a Puppeteer- and Chromium-based PDF rendering service that
    will replace the current Electron rendering service.
    
    Bug: T186748
    Change-Id: Id227c1e597f999097093c1f17e97e02eee7b17df

---
+++ modules/profile/manifests/proton.pp
@@ -0,0 +1,21 @@
+# == Class: profile::proton
+#
+# This class installs and configures the Chromium-based PDF renderer service.
+#
+class profile::proton(
+) {
+
+    require_package('chromium', 'fonts-liberation')
+
+    service::node { 'proton':
+        port              => 24766,
+        has_spec          => true,
+        healthcheck_url   => '',
+        deployment        => 'scap3',
+        deployment_config => true,
+        environment       => {
+            'CHROME_BIN' => '/usr/bin/chromium'
+        },
+    }
+
+}
