c699765d83016fff5d163fc11a43bb91012245e5
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-05-11 11:00:34 +0200

    prometheus: rabbitmq_exporter: add missing directory
    This directory is required previous to adding config files to it.
    
    Change-Id: I51bb3f2df202cd8b3cee03670ce9f347ffb90e78
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/prometheus/rabbitmq_exporter.pp
+++ modules/profile/manifests/prometheus/rabbitmq_exporter.pp
@@ -3,14 +3,21 @@ class profile::prometheus::rabbitmq_exporter (
     $rabbit_monitor_username = hiera('profile::prometheus::rabbit_monitor_user'),
     $rabbit_monitor_password = hiera('profile::prometheus::rabbit_monitor_pass'),
 ) {
-
     $rabbit_host = 'localhost:15672'
+
+    file { '/etc/prometheus/':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+    }
+
     file { '/etc/prometheus/rabbitmq-exporter.yaml':
         ensure  => 'present',
         owner   => 'prometheus',
         group   => 'prometheus',
         mode    => '0440',
         content => template('profile/prometheus/rabbitmq-exporter.conf.erb'),
+        require => File['/etc/prometheus/'],
     }
 
     require_package('prometheus-rabbitmq-exporter')
