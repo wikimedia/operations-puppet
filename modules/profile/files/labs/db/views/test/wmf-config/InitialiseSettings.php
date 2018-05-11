14c313799680788bf31865cae2cacf8b40729c75
Merlijn van Deen <valhallasw@arctus.nl>
2018-05-11 23:13:13 +0200

    labs/db: create basic integration test for maintain-meta_p
    This checks whether the correct entries are inserted for a range of
    different wikis. The goal of this test is not to be embedded in automatic
    CI, as the output currently depends on the API output of live wikis.
    However, this test can be used to refactor more safely, as long as the
    output of the test is modified before starting refactoring.
    
    Change-Id: I1c1833e8099e142f822622ee90871967b8668e9c

---
+++ modules/profile/files/labs/db/views/test/wmf-config/InitialiseSettings.php
@@ -0,0 +1,25 @@
+<?php
+$wgConf->settings = [
+// ...
+'wgCanonicalServer' => [
+	// Projects
+	'wikipedia' => 'https://$lang.wikipedia.org',
+	'wikibooks' => 'https://$lang.wikibooks.org',
+	'wikimedia' => 'https://$lang.wikimedia.org',
+	'wikinews' => 'https://$lang.wikinews.org',
+	'wikiquote' => 'https://$lang.wikiquote.org',
+	'wikisource' => 'https://$lang.wikisource.org',
+	'wikiversity' => 'https://$lang.wikiversity.org',
+	'wikivoyage' => 'https://$lang.wikivoyage.org',
+	'wiktionary' => 'https://$lang.wiktionary.org',
+
+	// Individual wikis
+	'advisorswiki' => 'https://advisors.wikimedia.org',
+	'amwikimedia' => 'https://am.wikimedia.org',
+	'commonswiki' => 'https://commons.wikimedia.org',
+	'testwikidatawiki' => 'https://test.wikidata.org',
+	'wikidatawiki' => 'https://www.wikidata.org',
+	'wikimania2005wiki' => 'https://wikimania2005.wikimedia.org',
+],
+// ...
+]
