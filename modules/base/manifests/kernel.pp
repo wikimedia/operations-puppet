f413583c2d434b07c8d9b817aba5c5b42df4f676
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-05-15 16:20:37 +0200

    Allow enabling microcode updates gradually
    We have two clusters which need updated microcode to provide support
    for the new IBPB instruction needed to secure KVM instances against
    Spectre.
    
    In addition to that keeping the microcode updated it also recommended
    by Intel since microcode updates can also address functional CPU anomalies.
    
    Plus, it's not entirely unlikely that future CPU vulnerabilities might
    need microcode updates for the non-virtualisation part as well.
    
    Deploying microcode updates was attempted in the past but led to CPU
    frequency problems. A lot of time has passed and we're almost
    universally on Linux 4.9, so let's revisit it now.
    
    Updated microcode is only picked up during reboots. This patch allows
    to enable it on a per host or cluster basis. The general idea is that
    if a cluster is e.g. comprised of three server generations we test one
    each and if tests are successful on all generations we enable it per
    role in Hiera. This way we can also test now and postpone reboots until
    the next round of fleetwide kernel security updates.
    
    Finally, when we're at some point confident that no further regressions
    are expected, we can enable it unconditionally by default.
    
    This only covers Intel microcode, our last server with an AMD CPU
    (stat1001) was decommissioned in December 2016.
    
    Bug: T127825
    Change-Id: I7c5fa2723643d1e14e7dcdb39d1065ddf2be06e3

--- modules/base/manifests/kernel.pp
+++ modules/base/manifests/kernel.pp
@@ -92,4 +92,9 @@ class base::kernel(
             },
         }
     }
+
+    $enable_microcode = hiera('base::microcode', false) # lint:ignore:wmf_styleguide
+    if $enable_microcode and $facts['is_virtual'] == false and $::processor0 !~ /AMD/ {
+        require_package('intel-microcode')
+    }
 }
