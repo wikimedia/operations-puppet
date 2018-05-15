130e23ef82d68de74b3694fe5331b1ab25559402
elukey <ltoscano@wikimedia.org>
2018-05-15 18:27:49 +0200

    role::configcluster: decom zookeeper on conf1001
    Bug: T182924
    Change-Id: If4e61493a620260145ff2106cbc2f48b18778b71

--- modules/role/manifests/configcluster.pp
+++ modules/role/manifests/configcluster.pp
@@ -1,6 +1,12 @@
 class role::configcluster {
     include ::standard
     include ::base::firewall
+
+     # Temporary to ease the migration process
+    if $::hostname =~ /conf100[23]/ {
+        include ::profile::zookeeper::server
+        include ::profile::zookeeper::firewall
+    }
     include ::profile::zookeeper::server
     include ::profile::zookeeper::firewall
     include ::profile::etcd
