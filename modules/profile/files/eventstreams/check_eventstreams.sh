0df6b97a34b94a366008fccdc15a6cd89e6305a3
Ottomata <aotto@wikimedia.org>
2018-05-15 10:57:36 -0400

    Alert if EventStreams recentchange endpoint has no messages
    This check runs on every host serving EventStreams.
    
    Bug: T174493
    Change-Id: Id82310b83b9c0b958623e0bdd588f58ab0c829a2

---
+++ modules/profile/files/eventstreams/check_eventstreams.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Attempts to consume from the given EventStreams endpoint and looks
+# for the presense of a line that begins with 'data:'.  This indicates
+# a message.  Use this script to monitor an active EventStreams endpoing
+# to alert if messages stop flowing through.
+#
+# Usage:
+# ./check_eventstreams [<eventstreams-url>] [<timeout-seconds>]
+#
+# Default eventstreams-url is http://localhost:8092/v2/stream/recentchange
+# Default timeout-seconds is 10
+#
+
+url=${1:-http://localhost:8092/v2/stream/recentchange}
+timeout=${2:-10}
+
+curl -s --max-time $timeout $url | head -n 5 | grep -qE '^data:'
+exitval=$?
+
+if [ $exitval -ne 0 ]; then
+    echo "CRITICAL: No EventStreams message was consumed from $url within $timeout seconds."
+    exit 1
+else
+    echo "OK: An EventStreams message was consumed from $url within $timeout seconds."
+    exit 0
+fi
