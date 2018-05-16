89da1a1385467578b88bc295550a4f12b40fa596
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2018-05-16 12:33:52 +0200

    wdqs: configure new wdqs test cluster
    also renamed role::wdqs_internal to role::wdqs::internal for coherence.
    
    Bug: T194184
    Change-Id: Ib8b1b1432e9f5fbeadd1dd2f1880d398f90e2be4

--- modules/role/manifests/wdqs/internal.pp
+++ modules/role/manifests/wdqs/internal.pp
@@ -1,13 +1,13 @@
-# = Class: role::wdqs
+# = Class: role::wdqs::internal
 #
 # This class sets up Wikidata Query Service
-class role::wdqs_internal {
+class role::wdqs::internal {
     include ::standard
     include ::role::lvs::realserver
     include ::profile::base::firewall
     include ::profile::wdqs
 
-    system::role { 'wdqs-internal':
+    system::role { 'wdqs::internal':
         ensure      => 'present',
         description => 'Wikidata Query Service - internally available service',
     }
