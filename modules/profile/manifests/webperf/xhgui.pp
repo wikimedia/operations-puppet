e51def3cd9e814579ab75df2fa1c328adb52d3a1
Daniel Zahn <dzahn@wikimedia.org>
2018-05-17 08:26:47 -0700

    webperf: add ferm rules and basic role/profile for xhgui
    Bug: T194390
    Change-Id: If00de924fafeff52f3ff13fcf10e8976f7cde959

---
+++ modules/profile/manifests/webperf/xhgui.pp
@@ -0,0 +1,17 @@
+# xhgui is a graphical interface for XHProf data
+# built on MongoDB and used by the performance team
+class profile::webperf::xhgui {
+
+    ferm::service { 'webperf-xhgui-http':
+        proto  => 'tcp',
+        port   => '80',
+        srange => '$INTERNAL',
+    }
+
+    ferm::service { 'webperf-xhgui-mongo':
+        proto  => 'tcp',
+        port   => '27017',
+        srange => '$INTERNAL',
+    }
+
+}
---
+++ modules/role/manifests/webperf/profiling_tools.pp
@@ -0,0 +1,22 @@
+# == Class: role::webperf::profiling_tools
+#
+# This role provisions a set of profiling tools for
+# the performance team. (T194390)
+#
+class role::webperf::profiling_tools {
+
+    system::role { 'webperf::profiling_tools':
+        description => 'profiling tools for the performance team'
+    }
+
+    interface::add_ip6_mapped { 'main': } # lint:ignore:wmf_styleguide
+
+    include ::standard
+    include ::profile::base::firewall
+
+    include ::profile::webperf::xhgui
+
+    class { '::httpd':
+        modules => ['authnz_ldap', 'php7.0', 'rewrite'],
+    }
+}
