b8c181335e2529386271a3d5ec00a74c1a4f753b
Daniel Zahn <dzahn@wikimedia.org>
2018-05-17 12:59:56 -0700

    network/tcpircbot/kubernetes: add deploy2001 to allowed hosts
    Bug: T193916
    Change-Id: Icc036d1c570db36685543cdd0bd3d4e5947153d5

--- modules/profile/manifests/tcpircbot.pp
+++ modules/profile/manifests/tcpircbot.pp
@@ -15,6 +15,8 @@ class profile::tcpircbot(
             '2620:0:861:101:10:64:0:196/128',   # deployment eqiad v6: tin
             '::ffff:10.64.32.16/128',           # deployment eqiad v4: deploy1001
             '2620:0:861:103:10:64:32:16/128',   # deployment eqiad v6: deploy1001
+            '::ffff:10.192.32.24/128',          # deployment codfw v4: deploy2001
+            '2620:0:860:103:10:192:32:24/128',  # deployment codfw v6: deploy2001
             '::ffff:10.192.32.22/128',          # deployment codfw v4: naos
             '2620:0:860:103:10:192:32:22/128',  # deployment codfw v6: naos
             '::ffff:10.64.32.13/128',           # maintenance eqiad v4: terbium
@@ -43,6 +45,7 @@ class profile::tcpircbot(
     $allowed_hosts = [
         'tin.eqiad.wmnet',              # deployment eqiad
         'deploy1001.eqiad.wmnet',       # deployment eqiad
+        'deploy2001.codfw.wmnet',       # deployment codfw
         'naos.codfw.wmnet',             # deployment codfw
         'puppetmaster1001.eqiad.wmnet', # puppet eqiad
         'puppetmaster2001.codfw.wmnet', # puppet codfw
