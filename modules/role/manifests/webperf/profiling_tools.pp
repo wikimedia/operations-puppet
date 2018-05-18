f4cdfb7c3f31db950e71a58a5dd86aef81dab5e2
Ian Marlier <imarlier@wikimedia.org>
2018-05-18 14:27:26 +0200

    webperf: separate permissions from specific apps
    base xhgui profile doesn't have a way to give perf team admin permissions.
    This is an attempt to address that, but separating the permissions application
    from the actual application being installed.
    
    Note: I'm not actually sure that I'm doing this right...
    
    Bug: T158837
    Change-Id: I31396aec9df10ff7ed6eee30194a6022594360bd

--- modules/role/manifests/webperf/profiling_tools.pp
+++ modules/role/manifests/webperf/profiling_tools.pp
@@ -5,12 +5,12 @@
 #
 class role::webperf::profiling_tools {
 
+    include ::standard
+    include ::profile::base::firewall
+
     system::role { 'webperf::profiling_tools':
         description => 'profiling tools for the performance team'
     }
-
-    include ::standard
-    include ::profile::base::firewall
 
     include ::profile::webperf::xhgui
 
