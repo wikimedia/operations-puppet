76b5008f20bc6ff2a4464765f93788c257a5f532
Reedy <reedy@wikimedia.org>
2018-05-18 20:34:52 +0100

    Load Tor Exit Nodes on labswiki
    Change-Id: I909b245b7306e1778eaccf25950209aec8d0098f

--- modules/mediawiki/manifests/maintenance/tor_exit_node.pp
+++ modules/mediawiki/manifests/maintenance/tor_exit_node.pp
@@ -1,7 +1,7 @@
-class mediawiki::maintenance::tor_exit_node( $ensure = present ) {
+class mediawiki::maintenance::tor_exit_node( $ensure = present, $wiki = 'aawiki' ) {
     cron { 'tor_exit_node_update':
         ensure  => $ensure,
-        command => '/usr/local/bin/mwscript extensions/TorBlock/maintenance/loadExitNodes.php --wiki=aawiki --force > /dev/null',
+        command => '/usr/local/bin/mwscript extensions/TorBlock/maintenance/loadExitNodes.php --wiki=${wiki} --force > /dev/null',
         user    => $::mediawiki::users::web,
         minute  => '*/20',
     }
