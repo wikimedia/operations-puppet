138b82fe972e722a5a47bf7006761a0f1b246ee9
Petr Pchelko <ppchelko@wikimedia.org>
2018-05-18 18:53:22 +0200

    Provide EventBus URI to change-prop profile
    Change-Id: I9aced440b209194364bb0693377c9ca565afa8be

--- modules/profile/manifests/changeprop.pp
+++ modules/profile/manifests/changeprop.pp
@@ -6,6 +6,7 @@ class profile::changeprop(
     $purge_host  = hiera('profile::changeprop::purge_host'),
     $purge_port  = hiera('profile::changeprop::purge_port'),
     $ores_uris  = hiera('profile::changeprop::ores_uris'),
+    $eventbus_uri = hiera('profile::changeprop::eventbus_uri'),
     $kafka_msg_max_bytes = hiera('kafka_message_max_bytes', 1048576),
 ) {
 
@@ -30,6 +31,7 @@ class profile::changeprop(
             mwapi_uri       => $::service::configuration::mwapi_uri,
             restbase_uri    => $restbase_uri,
             ores_uris       => $ores_uris,
+            eventbus_uri    => $eventbus_uri,
             purge_host      => $purge_host,
             purge_port      => $purge_port,
             site            => $::site,
