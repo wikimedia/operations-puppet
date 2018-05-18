25155a5e4ad1a65427445458b90a49ea77a1fd7b
Daniel Zahn <dzahn@wikimedia.org>
2018-05-17 17:46:18 -0700

    dumps: replace base::service_unit with systemd::service
    Bug: T194724
    Change-Id: I0d0c7d27dd5947119e6bafe3b8ea7fa8b75fc482

--- modules/dumps/manifests/generation/server/rsyncer.pp
+++ modules/dumps/manifests/generation/server/rsyncer.pp
@@ -12,10 +12,9 @@ class dumps::generation::server::rsyncer(
         source => 'puppet:///modules/dumps/generation/rsync-to-peers.sh',
     }
 
-    base::service_unit { 'dumps-rsyncer':
+    systemd::service { 'dumps-rsyncer':
         ensure    => 'present',
-        systemd   => systemd_template('dumps-rsync-peers'),
-        upstart   => upstart_template('dumps-rsync-peers'),
+        content   => systemd_template('dumps-rsync-peers'),
         subscribe => File['/usr/local/bin/rsync-to-peers.sh'],
     }
 }
