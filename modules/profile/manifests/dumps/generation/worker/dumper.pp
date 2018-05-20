ea5962101bfd59264ef623f68fe5c8f3f4af4045
Ariel T. Glenn <ariel@wikimedia.org>
2018-05-20 14:00:57 +0300

    move last dumps generator hiera settings into the profile
    This lets us get rid of a profile too.
    
    Change-Id: I5295b4a11cdaabd3ca1618b6e65a765d3879e14b

--- modules/profile/manifests/dumps/generation/worker/dumper.pp
+++ modules/profile/manifests/dumps/generation/worker/dumper.pp
@@ -1,12 +1,12 @@
 # this class is for snapshot hosts that run regular dumps
 # meaning sql/xml dumps every couple of weeks or so
-# and no other tasks
 class profile::dumps::generation::worker::dumper(
-    $runtype = hiera('snapshot::dumps::runtype'),
+    $runtype = hiera('profile::dumps::generation::worker::dumper::runtype'),
+    $maxjobs = hiera('profile::dumps::generation::worker::dumper::maxjobs'),
 ) {
     class { 'snapshot::dumps::cron':
         user    => 'dumpsgen',
-        maxjobs => '28',
+        maxjobs => $maxjobs,
         runtype => $runtype,
     }
 }
