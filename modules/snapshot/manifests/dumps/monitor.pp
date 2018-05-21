cb3595315632364dc1be915dff65b81e947800a7
Daniel Zahn <dzahn@wikimedia.org>
2018-05-21 16:46:27 -0700

    snapshot/dumps-monitor: replace base:service_unit
    Bug: T194724
    Change-Id: I45261924cb9f9c0c4631f0db9e1854c968954122

--- modules/snapshot/manifests/dumps/monitor.pp
+++ modules/snapshot/manifests/dumps/monitor.pp
@@ -5,10 +5,9 @@ class snapshot::dumps::monitor(
   $repodir = $snapshot::dumps::dirs::repodir
   $confsdir = $snapshot::dumps::dirs::confsdir
 
-  base::service_unit { 'dumps-monitor':
+  systemd::service { 'dumps-monitor':
     ensure    => 'present',
-    systemd   => systemd_template('dumps-monitor'),
-    upstart   => upstart_template('dumps-monitor'),
+    content   => systemd_template('dumps-monitor'),
     subscribe => File["${confsdir}/wikidump.conf.dumps"],
   }
 }
