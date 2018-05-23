c821bd717d1634ab144c1e2692e4a9a4f05345f9
Jaime Crespo <jcrespo@wikimedia.org>
2018-05-23 10:12:30 +0200

    mariadb: Setup db1117 as misc multiinstance
    It will be a single host with 4 misc instances (m1, m2, m3 and m5).
    
    It includes a custom config and extra files for phabricator.
    
    Notifications will be disabled at the start to avoid alert
    spamming (the host has not been yet setup).
    
    Bug: T192979
    Change-Id: I1d7da75a7522d340c80241526b03cecbfb876077

---
+++ modules/role/manifests/mariadb/misc/multiinstance.pp
@@ -0,0 +1,15 @@
+# miscellaneous services clusters
+class role::mariadb::misc::multiinstance {
+
+    system::role { 'mariadb::misc':
+        description => 'Misc Services Multiinstance Databases',
+    }
+
+    include ::standard
+    include ::profile::mariadb::monitor
+    include ::passwords::misc::scripts
+    include ::profile::base::firewall
+
+    include ::profile::mariadb::misc::multiinstance
+}
+
