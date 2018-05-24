16bdc1241aecc6cece34046ef1ebe43f4f6b8d8f
Riccardo Coccioli <rcoccioli@wikimedia.org>
2018-05-24 11:54:13 +0200

    wmf-auto-reimage: improve donwtime of reimaged host
    * When (re)imaging a host, there are a lot of race conditions that don't
      allow to properly downtime it on Icinga, leading to some alert spam in
      the IRC channel.
    * While a proper solution might require a lot of effort, this approach
      tries to improve the current situation, hopefully drastically reducing
      the alert spam.
      To achieve this the Icinga downtime is triggered before the first
      Puppet run, but with a delay of 1 minute, to allow Puppet to compile
      the catalog and send it to PuppetDB. Of course is not a perfect
      solution, but should work in most cases.
    * Add a new wmf-downtime-host script that perform the following tasks:
      - optionally sleep before executing
      - run Puppet on the Icinga host
      - downtime the given host on Icinga
    * Refactor the wmf-auto-reimage-host script to execute the
      wmf-downtime-host with a sleep of 1 minute in a subprocess right
      before the first Puppet run. The exit code of which is checked after
      the first Puppet run and a message is printed in case of failure.
    
    Bug: T195423
    Change-Id: Ia2fb547be664959e0b91820995ff4b060a45d48d

--- modules/profile/files/cumin/wmf_auto_reimage_host.py
+++ modules/profile/files/cumin/wmf_auto_reimage_host.py
@@ -5,6 +5,7 @@ import argparse
 import logging
 import os
 import socket
+import subprocess
 import sys
 import time
 
@@ -166,10 +167,17 @@ def run(args, user, log_path):
         if args.mask:  # Mask systemd services
             lib.mask_systemd_services(args.host, args.mask)
 
+        # Downtime the host on Icinga with delay to give time to compile the Puppet catalog
+        # and export its resources
+        downtime = subprocess.Popen(
+            ['wmf-downtime-host', '-s', '60', '-p', str(args.phab_task_id), args.host])
+
         lib.puppet_first_run(args.host)
-        # Ensure the host is in Icinga
-        lib.run_puppet([lib.resolve_dns(lib.ICINGA_DOMAIN, 'CNAME')], no_raise=True)
-        lib.icinga_downtime(args.host, user, args.phab_task_id)
+
+        downtime_ret = downtime.poll()
+        if downtime_ret != 0:
+            lib.print_line(('WARNING: failed to downtime host on Icinga, wmf-downtime-host '
+                            'returned {ret}').format(ret=downtime_ret))
 
     lib.check_bios_bootparams(args.host, args.mgmt)
 
