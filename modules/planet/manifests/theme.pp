b5a631bee2debd5e27eeaf4f7b1c0ccea4375cb0
Paladox <thomasmulhall410@yahoo.com>
2018-05-26 00:12:51 +0000

    Planet: Redesign UI for rawdog, add theme files
    Uses Planet KDE theme from https://cgit.kde.org/websites/planet-kde-org.git/tree/
    
    * Updates the look with a modern ui that is optimised for mobiles
      (theme from kde).
    
    Bug: T180498
    Change-Id: I5913befa1ec18a81978ee9c835e7addf53b85980

--- modules/planet/manifests/theme.pp
+++ modules/planet/manifests/theme.pp
@@ -18,21 +18,52 @@ define planet::theme {
         file { "/etc/rawdog/theme/wikimedia/${title}":
             ensure  => 'directory',
         }
-        file { "/var/www/planet/${title}/style.css":
+        file { "/var/www/planet/${title}/main.css":
             ensure => 'present',
-            source => 'puppet:///modules/planet/theme/rawdog_style.css';
+            source => 'puppet:///modules/planet/theme/rawdog/rawdog_style.css';
+        }
+        file { "/var/www/planet/${title}/bootstrap.css":
+            ensure => 'present',
+            source => 'puppet:///modules/planet/theme/rawdog/bootstrap.css';
+        }
+        file { "/var/www/planet/${title}/bootstrap.min.js":
+            ensure => 'present',
+            source => 'puppet:///modules/planet/theme/rawdog/bootstrap.min.js';
+        }
+        file { "/var/www/planet/${title}/jquery.min.js":
+            ensure => 'present',
+            source => 'puppet:///modules/planet/theme/rawdog/jquery.min.js';
         }
         file { "/var/www/planet/${title}/Wikimedia_Community_Logo.svg":
             ensure => 'present',
-            source => 'puppet:///modules/planet/theme/Wikimedia_Community_Logo.svg';
+            source => 'puppet:///modules/planet/theme/rawdog/Wikimedia_Community_Logo.svg';
         }
         file { "/etc/rawdog/theme/wikimedia/${title}/rd_page.tmpl":
             ensure  => 'present',
-            content => template('planet/html/rd_page.html.tmpl.erb');
+            content => template('planet/html/rawdog/rd_page.html.tmpl.erb');
         }
         file { "/etc/rawdog/theme/wikimedia/${title}/rd_item.tmpl":
             ensure  => 'present',
-            content => template('planet/html/rd_item.html.tmpl.erb');
+            content => template('planet/html/rawdog/rd_item.html.tmpl.erb');
+        }
+        file { "/var/www/planet/${title}/fonts":
+            ensure  => 'directory',
+        }
+        file { "/var/www/planet/${title}/fonts/icomoon.eot":
+            ensure => 'present',
+            source => 'puppet:///modules/planet/theme/rawdog/icomoon.eot';
+        }
+        file { "/var/www/planet/${title}/fonts/icomoon.svg":
+            ensure => 'present',
+            source => 'puppet:///modules/planet/theme/rawdog/icomoon.svg';
+        }
+        file { "/var/www/planet/${title}/fonts/icomoon.ttf":
+            ensure => 'present',
+            source => 'puppet:///modules/planet/theme/rawdog/icomoon.ttf';
+        }
+        file { "/var/www/planet/${title}/fonts/icomoon.woff":
+            ensure => 'present',
+            source => 'puppet:///modules/planet/theme/rawdog/icomoon.woff';
         }
     } else {
         $theme_path = '/usr/share/planet-venus/theme/wikimedia'
