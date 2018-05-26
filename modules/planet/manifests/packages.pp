9d3ae3493f53f78cef1bada34ff7ea5257815e3f
Paladox <thomasmulhall410@yahoo.com>
2018-05-26 00:12:51 +0000

    WIP: Planet: Redesgn UI for rawdog
    Uses Planet KDE theme from https://cgit.kde.org/websites/planet-kde-org.git/tree/
    
    * Updates the look with a modern ui that is optimised for mobiles
      (theme from kde).
    
    Change-Id: I5913befa1ec18a81978ee9c835e7addf53b85980

--- modules/planet/manifests/packages.pp
+++ modules/planet/manifests/packages.pp
@@ -23,6 +23,12 @@ class planet::packages {
             ensure => 'present',
         }
 
+        file { '/usr/lib/python2.7/dist-packages/rawdoglib/rawdog.py':
+            ensure  => 'present',
+            source  => 'puppet:///modules/planet/rawdog/rawdog.py';
+            require => Package['rawdog'],
+        }
+
         # PyTidyLib 0.2.1 or later (optional but strongly recommended)
         # python-libxml2 is needed for the xml archive plugin we will use
         # for rawdog.
