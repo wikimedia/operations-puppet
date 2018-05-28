9b9f67f216887f39e1903560c3b5c159219fb8af
Emanuele Rocca <ema@wikimedia.org>
2018-05-28 11:37:00 +0200

    varnishrls: post-removal cleanup
    After having removed varnishrls in 187ad48, we can now eliminate the
    nrpe check and other leftovers.
    
    Bug: T184942
    Change-Id: Ieefc363532e2df29dc3ba311cbed5584d2472140

--- modules/varnish/manifests/logging/rls.pp
+++ modules/varnish/manifests/logging/rls.pp
@@ -6,22 +6,6 @@
 define varnish::logging::rls {
     include ::varnish::common
 
-    file { '/usr/local/bin/varnishrls':
-        ensure => absent,
-        notify => Service['varnishrls'],
-    }
-
-    systemd::service { 'varnishrls':
-        ensure  => absent,
-        content => '',
-    }
-
-    nrpe::monitor_service { 'varnishrls':
-        ensure       => absent,
-        description  => 'Varnish traffic logger - varnishrls',
-        nrpe_command => '/bin/true',
-    }
-
     mtail::program { 'varnishrls':
         source => 'puppet:///modules/mtail/programs/varnishrls.mtail',
         notify => Service['varnishmtail'],
