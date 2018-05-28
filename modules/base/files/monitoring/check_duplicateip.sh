78f9dc3d60dc651509b7238a15238c63b6d59869
Arzhel Younsi <ayounsi@wikimedia.org>
2018-05-28 17:03:29 +0200

    Use arping to detect duplicated IPs
    Send a arp request for the main IP from the main network interface,
    If any reply, which means the IP is live on another host in the same
    subnet, raise a critical.
    
    - Install the package "arping"
    - Add a NRPE check
    - Allow the NRPE script to be run as root
    
    Bug: T189522
    Change-Id: I5dbead9ac4a67667fa6af8527bcf0c953109ba4a

---
+++ modules/base/files/monitoring/check_duplicateip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+arping_result=$(/usr/sbin/arping -i "${1}" -c 1 -C 1 "${2}" -S 0.0.0.0 -r)
+
+if [[ "${?}" -eq "0" ]]; then # We got a reply
+  echo "ARP reply from MAC $arping_result"
+  exit 2
+elif [[ "${?}" -eq "1" ]]; then # No reply
+  echo "No ARP reply for primary IP"
+  exit 0
+else
+  echo "Unknown result"
+  exit 3
+fi
