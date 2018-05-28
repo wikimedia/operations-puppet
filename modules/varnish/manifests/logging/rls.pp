f5e859edb8f73e38f4583ba15e15fa773dc12737
Emanuele Rocca <ema@wikimedia.org>
2018-05-28 10:53:20 +0200

    varnishrls: remove python daemon
    Bug: T184942
    Change-Id: Ifb278f7d92da63198aa5468d406fed79a91459b9

--- modules/varnish/manifests/logging/rls.pp
+++ modules/varnish/manifests/logging/rls.pp
@@ -1,48 +1,25 @@
 # == Define: varnish::logging::rls
 #
 #  Accumulate browser cache hit ratio and total request volume statistics for
-#  ResourceLoader requests (/w/load.php) and report to StatsD. Expose metrics
-#  to prometheus.
+#  ResourceLoader requests (/w/load.php) and expose metrics to prometheus.
 #
-# === Parameters
-#
-# [*statsd_server*]
-#   StatsD server address, in "host:port" format.
-#   Defaults to localhost:8125.
-#
-# === Examples
-#
-#  varnish::logging::rls {
-#    statsd_server => 'statsd.eqiad.wmnet:8125
-#  }
-#
-define varnish::logging::rls( $statsd_server = 'statsd' ) {
+define varnish::logging::rls {
     include ::varnish::common
 
     file { '/usr/local/bin/varnishrls':
-        source  => 'puppet:///modules/varnish/varnishrls',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0555',
-        require => File['/usr/local/lib/python2.7/dist-packages/cachestats.py'],
-        notify  => Service['varnishrls'],
+        ensure => absent,
+        notify => Service['varnishrls'],
     }
 
     systemd::service { 'varnishrls':
-        ensure         => present,
-        content        => systemd_template('varnishrls'),
-        restart        => true,
-        require        => File['/usr/local/bin/varnishrls'],
-        subscribe      => File['/usr/local/lib/python2.7/dist-packages/cachestats.py'],
-        service_params => {
-            enable => true,
-        },
+        ensure  => absent,
+        content => '',
     }
 
     nrpe::monitor_service { 'varnishrls':
-        ensure       => present,
+        ensure       => absent,
         description  => 'Varnish traffic logger - varnishrls',
-        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -a "/usr/local/bin/varnishrls" -u root',
+        nrpe_command => '/bin/true',
     }
 
     mtail::program { 'varnishrls':
