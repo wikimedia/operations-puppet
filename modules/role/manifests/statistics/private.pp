7f4cf6c15f55b4e9ec57c0b7b0461dc06326acf0
Ottomata <aotto@wikimedia.org>
2018-05-29 10:11:36 -0400

    Create profile::analytics::cluster::packages class
    This helps to unify the installation of common other packages
    for Hadoop and local computation. Tried also a first pass of
    merging the statistics packages list into this new profile.
    Removed also some unused checks.
    
    Change-Id: Id9e007975cb676860e9b51942754349837bc4e22

--- modules/role/manifests/statistics/private.pp
+++ modules/role/manifests/statistics/private.pp
@@ -3,6 +3,7 @@ class role::statistics::private {
         description => 'Statistics private data host and general compute node'
     }
 
+    include ::standard
     include ::profile::statistics::private
 
     # Run Hadoop/Hive reportupdater jobs here.
