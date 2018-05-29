d727c32fab3337e612ecde00bb5f4b538dc21e0e
Ottomata <aotto@wikimedia.org>
2018-05-29 10:11:36 -0400

    [WIP] Create profile::analytics::cluster::packages::* classes
    This helps to unify the installation of common other packages
    for Hadoop and local computation. Tried also a first pass of
    merging the statistics packages list into this new profile.
    Removed also some unused checks.
    
    Change-Id: Id9e007975cb676860e9b51942754349837bc4e22

--- modules/role/manifests/statistics/cruncher.pp
+++ modules/role/manifests/statistics/cruncher.pp
@@ -3,15 +3,15 @@ class role::statistics::cruncher {
         description => 'Statistics general compute node (non private data)'
     }
 
+    include ::standard
+    include ::profile::base::firewall
+
     include ::profile::statistics::cruncher
 
-    # will be removed as part of T152712
-    if $::hostname == 'stat1006' {
-        # Geowiki jobs that get data from MySQL analytics slave and save the data
-        # in a private locally hosted git repository.
-        include ::profile::geowiki
+    # Geowiki jobs that get data from MySQL analytics slave and save the data
+    # in a private locally hosted git repository.
+    include ::profile::geowiki
 
-        # Reportupdater jobs that get data from MySQL analytics slaves
-        include ::profile::reportupdater::jobs::mysql
-    }
+    # Reportupdater jobs that get data from MySQL analytics slaves
+    include ::profile::reportupdater::jobs::mysql
 }
