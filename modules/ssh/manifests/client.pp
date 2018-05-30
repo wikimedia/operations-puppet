afc4ee716caaaf36f701956b10d592859a5c1576
Alex Monk <krenair@gmail.com>
2018-05-30 18:41:32 +0100

    Delete sshknowngen
    Per Alexandros on I8cfba01d PS4
    
    Change-Id: Ibbf7c547ce92628b5a955c0b94097e336599881f

--- modules/ssh/manifests/client.pp
+++ modules/ssh/manifests/client.pp
@@ -11,14 +11,5 @@ class ssh::client {
             group   => 'root',
             mode    => '0644',
         }
-    } elsif $::realm == 'production' {
-        # Effectively unused at the moment as production has use_puppetdb
-        file { '/etc/ssh/ssh_known_hosts':
-            content => generate('/usr/local/bin/sshknowngen'),
-            backup  => false,
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0644',
-        }
     }
 }
