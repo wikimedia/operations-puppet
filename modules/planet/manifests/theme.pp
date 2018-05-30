2a6084b4c15a539f188e7c2ba618d30f5732e1f2
Paladox <thomasmulhall410@yahoo.com>
2018-05-30 15:33:34 +0000

    WIP: Planet: Update libs and some ui tweeks for rawdog
    * Switches to bootstrap minified leading to the browser downloading less.
    
    * Updates bootstrap to 4.1.1 (i manually removed the map reference from
      the css and js as it dosen't look like it's needed).
    
    * More ui tweeks, updated navbar.
    
    * Update jQuery to 3.x. (drops support for ie 9, ie 8 and old browser
      versions)
    
    Change-Id: Ic695b47f76b4e67123d8b23d93b6449e6ce4dbf0

--- modules/planet/manifests/theme.pp
+++ modules/planet/manifests/theme.pp
@@ -24,7 +24,7 @@ define planet::theme {
         }
         file { "/var/www/planet/${title}/bootstrap.css":
             ensure => 'present',
-            source => 'puppet:///modules/planet/theme/rawdog/bootstrap.css';
+            source => 'puppet:///modules/planet/theme/rawdog/bootstrap.min.css';
         }
         file { "/var/www/planet/${title}/bootstrap.min.js":
             ensure => 'present',
@@ -33,6 +33,10 @@ define planet::theme {
         file { "/var/www/planet/${title}/jquery.min.js":
             ensure => 'present',
             source => 'puppet:///modules/planet/theme/rawdog/jquery.min.js';
+        }
+        file { "/var/www/planet/${title}/popper.min.js":
+            ensure => 'present',
+            source => 'puppet:///modules/planet/theme/rawdog/popper.min.js';
         }
         file { "/var/www/planet/${title}/Wikimedia_Community_Logo.svg":
             ensure => 'present',
