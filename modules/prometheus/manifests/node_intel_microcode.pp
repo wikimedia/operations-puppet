04a75e4535eca4cce94d828961d1a96866114185
Emanuele Rocca <ema@wikimedia.org>
2018-05-31 16:29:22 +0200

    prometheus: export intel-microcode information via node_exporter
    Add prometheus::node_intel_microcode to periodically export
    intel_microcode version information via node-exporter.
    
    Enable collection if base::microcode is set to true in hiera.
    
    Bug: T127825
    Change-Id: I13d5cd36ca32f92ba2a5a9e35780dbe4e4391545

---
+++ modules/prometheus/manifests/node_intel_microcode.pp
@@ -0,0 +1,30 @@
+# = Class: prometheus::node_intel_microcode
+#
+# Periodically export intel_microcode version information via node-exporter
+# textfile collector.
+
+class prometheus::node_intel_microcode (
+    $ensure = 'present',
+    $outfile = '/var/lib/prometheus/node.d/intel_microcode.prom',
+) {
+    validate_re($outfile, '\.prom$')
+    validate_ensure($ensure)
+
+    require_package("iucode-tool")
+
+    file { '/usr/local/bin/prometheus-intel-microcode':
+        ensure => file,
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/prometheus/usr/local/bin/prometheus-intel-microcode',
+    }
+
+    # Collect every hour
+    cron { 'prometheus_intel_microcode':
+        ensure  => $ensure,
+        user    => 'root',
+        minute  => '42',
+        command => "/usr/local/bin/prometheus-intel-microcode ${outfile}",
+    }
+}
