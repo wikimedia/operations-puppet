d63d862189dde4c8a686ad7865ac361fa07c59aa
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-06-01 12:08:13 +0200

    role::deployment_server: add mcrouter
    Change-Id: I916750526c5c3fbe17d42fd22f7243ed0b770733

--- modules/role/manifests/deployment_server.pp
+++ modules/role/manifests/deployment_server.pp
@@ -3,6 +3,8 @@ class role::deployment_server {
     include ::standard
     include ::base::firewall
     include ::profile::mediawiki::deployment::server
+    include ::profile::mediawiki::mcrouter_wancache
+    include ::profile::prometheus::mcrouter_exporter
     include ::profile::backup::host
     include ::role::deployment::mediawiki
     include ::profile::releases::mediawiki::security
