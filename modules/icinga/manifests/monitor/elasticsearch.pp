22328c0e9a90c6330455ae6023d9eee8821e9d65
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2018-06-04 17:31:07 +0200

    elasticsearch: raise unassigned shard alerting threshold
    Since we had a false positive during cluster restart, let's raise this
    a bit. It seems that the cluster has become slightly more unbalanced
    than usual...
    
    Change-Id: Icd2313af9b6e44da73c26e57af007839e2a84001

--- modules/icinga/manifests/monitor/elasticsearch.pp
+++ modules/icinga/manifests/monitor/elasticsearch.pp
@@ -1,9 +1,11 @@
 # monitor the different elasticsearch clusters
 class icinga::monitor::elasticsearch {
 
+    $threshold = '>=0.15'
+
     monitoring::service { 'elasticsearch shards - eqiad':
         host          => 'search.svc.eqiad.wmnet',
-        check_command => 'check_elasticsearch_shards',
+        check_command => "check_elasticsearch_shards_threshold!${threshold}",
         description   => 'ElasticSearch health check for shards',
         critical      => true,
         contact_group => 'admins,team-discovery',
@@ -11,7 +13,7 @@ class icinga::monitor::elasticsearch {
 
     monitoring::service { 'elasticsearch shards - codfw':
         host          => 'search.svc.codfw.wmnet',
-        check_command => 'check_elasticsearch_shards',
+        check_command => "check_elasticsearch_shards_threshold!${threshold}",
         description   => 'ElasticSearch health check for shards',
         critical      => true,
         contact_group => 'admins,team-discovery',
