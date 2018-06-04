ce59fdb2c836fa1a29c0099ba6b0924dc60d42e7
elukey <ltoscano@wikimedia.org>
2018-06-04 09:09:08 +0200

    geowiki::job::limn: add MAILTO env variable to avoid root@ spam
    Change-Id: I25bff075854785ffdff8718575f530191ab6d2fe

--- modules/geowiki/manifests/job/limn.pp
+++ modules/geowiki/manifests/job/limn.pp
@@ -8,11 +8,12 @@ class geowiki::job::limn {
     # cron job to do the actual fetching from the database, computation of
     # the limn files, and pushing the limn files to the data repositories
     cron { 'geowiki-process-db-to-limn':
-        minute  => 0,
-        hour    => 15,
-        user    => $::geowiki::user,
-        command => "${::geowiki::scripts_path}/scripts/make_and_push_limn_files.sh --cron-mode --basedir_private=${::geowiki::private_data_path} --source_sql_cnf=${::geowiki::mysql_conf::conf_file}",
-        require => [
+        minute      => 0,
+        hour        => 15,
+        user        => $::geowiki::user,
+        environment => 'MAILTO=analytics-alerts@wikimedia.org',
+        command     => "${::geowiki::scripts_path}/scripts/make_and_push_limn_files.sh --cron-mode --basedir_private=${::geowiki::private_data_path} --source_sql_cnf=${::geowiki::mysql_conf::conf_file}",
+        require     => [
             Git::Clone['geowiki-scripts'],
             Git::Clone['geowiki-data-private'],
             File[$::geowiki::mysql_conf::conf_file],
