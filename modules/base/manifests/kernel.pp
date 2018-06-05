26ce4117e4372da4c096eca59a21b2fcda5767f0
Emanuele Rocca <ema@wikimedia.org>
2018-06-05 14:46:21 +0200

    Move microcode hiera call to profile::base
    Puppet modules should not include hiera calls. Move hiera call to check
    whether intel-microcode should be enabled from base::kernel to
    profile::base.
    
    Bug: T127825
    Change-Id: Ib9a77e6b27e281435d17b26ddf2f4d6e120781b0

--- modules/base/manifests/kernel.pp
+++ modules/base/manifests/kernel.pp
@@ -5,9 +5,13 @@
 #
 # [*overlayfs*]
 #  bool for whether overlay module is needed
+#
+# [*enable_microcode*]
+#  bool for whether the package intel-microcode should be installed or not
 
 class base::kernel(
     $overlayfs,
+    $enable_microcode,
     ) {
     if os_version('ubuntu == trusty') {
         # This directory is shipped by systemd, but trusty's upstart job for
@@ -93,8 +97,7 @@ class base::kernel(
         }
     }
 
-    $enable_microcode = hiera('base::microcode', false) # lint:ignore:wmf_styleguide
-    if $enable_microcode and $facts['is_virtual'] == false and $::processor0 !~ /AMD/ {
+    if $enable_microcode {
         require_package('intel-microcode')
     }
 }
