1623a25f3f1518c63a33559ad4c5b20052eb8e8e
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-06-08 12:15:32 +0200

    openstack: eqiad1: enable more components for labcontrol boxes (keystone)
    Let's enable keystone.
    
    Bug: T196633
    Change-Id: I0caf8fe35d46781ad947cac38599539e17ee2614
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/role/manifests/wmcs/openstack/eqiad1/control.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/control.pp
@@ -4,7 +4,7 @@ class role::wmcs::openstack::eqiad1::control {
     include ::profile::base::firewall
     include ::profile::openstack::eqiad1::observerenv
     include ::profile::openstack::eqiad1::rabbitmq
-    # include ::profile::openstack::eqiad1::keystone::service
+    include ::profile::openstack::eqiad1::keystone::service
     # include ::profile::openstack::eqiad1::glance
     # include ::profile::openstack::eqiad1::nova::common
     # include ::profile::openstack::eqiad1::nova::conductor::service
