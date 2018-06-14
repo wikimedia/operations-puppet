7e8ce72261a2bf4dfe8bb4b7a1a832105ef45c83
andrewbogott <abogott@wikimedia.org>
2018-06-14 14:57:45 -0500

    nova-api: allow access to port 8774 for api access
    On the nova-network node (which basically has no firewall) this
    port was already open.  In new Neutron deploys nova-api
    will be running on a controller node which needs this
    port explicitly opened.
    
    Change-Id: Ibfc14a48a407afbd2d34f7b1da74e6d7e0d97b9e

--- modules/profile/manifests/openstack/base/nova/api/service.pp
+++ modules/profile/manifests/openstack/base/nova/api/service.pp
@@ -2,8 +2,17 @@ class profile::openstack::base::nova::api::service(
     $nova_api_host = hiera('profile::openstack::base::nova_api_host'),
     ) {
 
+    $prod_networks = join($::network::constants::production_networks, ' ')
+    $labs_networks = join($::network::constants::labs_networks, ' ')
+
     class {'::openstack::nova::api::service':
         active => ($::fqdn == $nova_api_host),
     }
     contain '::openstack::nova::api::service'
+
+    ferm::rule{'nova_api_public':
+        ensure => 'present',
+        rule   => "saddr (${prod_networks} ${labs_networks}
+                             ) proto tcp dport (8774) ACCEPT;",
+    }
 }
