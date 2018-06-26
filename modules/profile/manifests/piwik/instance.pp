9e73518a6447dc5da35e26cc9c327c29e53f4f22
elukey <ltoscano@wikimedia.org>
2018-06-26 14:48:31 +0200

    profile::piwik::instance: allow hiera settings for trusted hosts
    Change-Id: I52e4edc84e8e387d65c8ab3889c500ec9915d266

--- modules/profile/manifests/piwik/instance.pp
+++ modules/profile/manifests/piwik/instance.pp
@@ -7,7 +7,8 @@ class profile::piwik::instance (
     $admin_username    = hiera('profile::piwik::admin_username'),
     $admin_password    = hiera('profile::piwik::admin_password'),
     $password_salt     = hiera('profile::piwik::password_salt'),
-    $trusted_hosts     = ['piwik.wikimedia.org', 'wikimediafoundation.org'],
+    $trusted_hosts     = hiera('profile::piwik::trusted_hosts',
+        ['piwik.wikimedia.org', 'wikimediafoundation.org']),
 ) {
     class { 'piwik':
         database_password  => $database_password,
