38e30d63395150c0c62f8d85d68583ec17ac3401
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-06-28 12:02:21 +0200

    openstack: eqiad1: enable glance in control boxes
    Control boxes need to run glance from jessie-backports.
    
    Bug: T196633
    Change-Id: Ic77ee7ca7d0c802933f49ac97ba86ab9f0fae1a0
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

---
+++ modules/profile/manifests/openstack/eqiad1/glance.pp
@@ -0,0 +1,23 @@
+class profile::openstack::eqiad1::glance (
+    $version = hiera('profile::openstack::eqiad1::version'),
+    $nova_controller = hiera('profile::openstack::eqiad1::nova_controller'),
+    $nova_controller_standby = hiera('profile::openstack::eqiad1::nova_controller_standby'),
+    $db_pass = hiera('profile::openstack::eqiad1::glance::db_pass'),
+    $db_host = hiera('profile::openstack::eqiad1::glance::db_host'),
+    $ldap_user_pass = hiera('profile::openstack::eqiad1::ldap_user_pass'),
+    $labs_hosts_range = hiera('profile::openstack::eqiad1::labs_hosts_range'),
+    ) {
+
+    require ::profile::openstack::eqiad1::clientlib
+    class {'::profile::openstack::base::glance':
+        version                 => $version,
+        nova_controller         => $nova_controller,
+        nova_controller_standby => $nova_controller_standby,
+        keystone_host           => $nova_controller,
+        db_pass                 => $db_pass,
+        db_host                 => $db_host,
+        ldap_user_pass          => $ldap_user_pass,
+        labs_hosts_range        => $labs_hosts_range,
+    }
+    contain '::profile::openstack::base::glance'
+}
--- modules/role/manifests/wmcs/openstack/eqiad1/control.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/control.pp
@@ -7,7 +7,7 @@ class role::wmcs::openstack::eqiad1::control {
     include ::profile::openstack::eqiad1::keystone::service
     include ::profile::openstack::eqiad1::neutron::common
     include ::profile::openstack::eqiad1::neutron::service
-    # include ::profile::openstack::eqiad1::glance
+    include ::profile::openstack::eqiad1::glance
     # include ::profile::openstack::eqiad1::nova::common
     # include ::profile::openstack::eqiad1::nova::conductor::service
     # include ::profile::openstack::eqiad1::nova::scheduler::service
