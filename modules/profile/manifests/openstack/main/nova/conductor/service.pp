2f1fed561c891b178954ccdeab16b8f2773b8c9e
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-07-04 13:16:38 +0200

    openstack: pass version hiera key down to nova conductor service class
    The base class gained a new parameter (version) but all callers were not
    accordingly updated, thus causing puppet catalog errors:
    
    aborrero@labcontrol1001:~ 1 $ sudo puppet agent -t -v
    Info: Using configured environment 'production'
    Info: Retrieving pluginfacts
    Info: Retrieving plugin
    Info: Loading facts
    Error: Could not retrieve catalog from remote server:
     Error 500 on SERVER:
      Server Error:
       Evaluation Error:
        Error while evaluating a Function Call,
         Could not find data item profile::openstack::base::version in any Hiera
         data file and no default supplied at
         [.]modules/profile/manifests/openstack/base/nova/conductor/service.pp:2:16
         on node labcontrol1001.wikimedia.org
    
    Change-Id: I0e84cfbf2168c80f6e17e7c48dd41a22a9128ccf
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/openstack/main/nova/conductor/service.pp
+++ modules/profile/manifests/openstack/main/nova/conductor/service.pp
@@ -1,9 +1,11 @@
 class profile::openstack::main::nova::conductor::service(
+    $version = hiera('profile::openstack::main::version'),
     $nova_controller = hiera('profile::openstack::main::nova_controller'),
     ) {
 
     require ::profile::openstack::main::nova::common
     class {'::profile::openstack::base::nova::conductor::service':
+        version         => $version,
         nova_controller => $nova_controller,
     }
 
