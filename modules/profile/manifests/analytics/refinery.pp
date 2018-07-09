1ffb0d8a05e29d6ab34f2a5bd28dbc0f0290180f
elukey <ltoscano@wikimedia.org>
2018-07-09 16:21:03 +0200

    profile::analytics::refinery::repository: remove logging config
    Logging config seems something required where the repository is
    actively used, like stat/analytics hosts, meanwhile it can be
    problematic in hosts like the databases where we only need
    the repository deployed (and no hdfs/analytics-admins/etc..)
    users/groups deployed.
    
    Bug: T198766
    Change-Id: I5481c2acb57035811f3c7912a86b4844acc9f5f4

--- modules/profile/manifests/analytics/refinery.pp
+++ modules/profile/manifests/analytics/refinery.pp
@@ -17,7 +17,26 @@ class profile::analytics::refinery {
     # Required by a lot of profiles dependent on this one
     # to find the correct path for scripts etc..
     $path = $::profile::analytics::refinery::repository::path
-    $log_dir = $::profile::analytics::refinery::repository::log_dir
+
+    # Create directory in /var/log for general purpose Refinery job logging.
+    $log_dir = '/var/log/refinery'
+    $log_dir_group = $::realm ? {
+        'production' => 'analytics-admins',
+        'labs'       => "project-${::labsproject}",
+    }
+    file { $log_dir:
+        ensure => 'directory',
+        owner  => 'hdfs',
+        group  => $log_dir_group,
+        # setgid bit here to make refinery log files writeable
+        # by users in the $log_dir_group group.
+        mode   => '2775',
+    }
+
+    logrotate::conf { 'refinery':
+        source  => 'puppet:///modules/profile/analytics/refinery-logrotate.conf',
+        require => File[$log_dir],
+    }
 
     # Clone mediawiki/event-schemas so refinery can use them.
     class { '::eventschemas': }
--- modules/profile/manifests/analytics/refinery/repository.pp
+++ modules/profile/manifests/analytics/refinery/repository.pp
@@ -30,24 +30,4 @@ class profile::analytics::refinery::repository {
     file { '/etc/profile.d/refinery.sh':
         content => "export PYTHONPATH=\${PYTHONPATH}:${path}/python",
     }
-
-    # Create directory in /var/log for general purpose Refinery job logging.
-    $log_dir = '/var/log/refinery'
-    $log_dir_group = $::realm ? {
-        'production' => 'analytics-admins',
-        'labs'       => "project-${::labsproject}",
-    }
-    file { $log_dir:
-        ensure => 'directory',
-        owner  => 'hdfs',
-        group  => $log_dir_group,
-        # setgid bit here to make refinery log files writeable
-        # by users in the $$log_dir_group group.
-        mode   => '2775',
-    }
-
-    logrotate::conf { 'refinery':
-        source  => 'puppet:///modules/profile/analytics/refinery-logrotate.conf',
-        require => File[$log_dir],
-    }
 }
