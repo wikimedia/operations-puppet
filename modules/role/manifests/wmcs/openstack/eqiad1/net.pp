760ec11ca5dd59b08d764c2e174c35e25c50d8bc
cpettet <rush@wikimedia.org>
2018-07-10 16:15:34 -0500

    openstack: eqiad1 deployment net role notes
    Change-Id: I79bfef50f2c1d60bf8a4ce1b7fcb89b5a2603d98

--- modules/role/manifests/wmcs/openstack/eqiad1/net.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/net.pp
@@ -5,8 +5,9 @@ class role::wmcs::openstack::eqiad1::net {
     include ::profile::openstack::eqiad1::clientlib
     include ::profile::openstack::eqiad1::observerenv
     include ::profile::openstack::eqiad1::neutron::common
-    # still partial
+    # TODO: partial config?
     include ::profile::openstack::eqiad1::neutron::l3_agent
     include ::profile::openstack::eqiad1::neutron::dhcp_agent
+    # TODO: needs appropriate pinning for jessie mitaka
     include ::profile::openstack::eqiad1::neutron::metadata_agent
 }
