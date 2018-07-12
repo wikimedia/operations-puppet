0a2dbaa635757291ae9e3e5917a5455503bfaf5b
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-07-12 12:08:11 +0200

    deploy-apache-change: stop asking for confirmation
    That is handled by cumin already; let's be informative about what people
    are consenting to instead.
    
    Also, fix the puppet selector.
    
    Change-Id: Ib298921c068f6c7a1537d20c68a40d2d3ead17a1

--- modules/profile/files/mediawiki/web_testing/deploy_apache_change.sh
+++ modules/profile/files/mediawiki/web_testing/deploy_apache_change.sh
@@ -32,38 +32,30 @@ environment=${2:-prod}
 canary=${CANARY:-"mwdebug1001.eqiad.wmnet"}
 control=${CONTROL:-"mw1266.eqiad.wmnet"}
 puppetmaster=$(sudo puppet config print --section agent ca_server)
-targets="R:Class ~ '(?i)mediawiki(_exp)?::web::${environment}_sites"
-
-
-function confirm {
-    echo "$1"
-    read -r -p '> ' -a answer
-    if [ "$answer" == "y" ] || [ "$answer" == "Y" ]; then
-        return
-    else
-        echo "Bailing out"
-        exit 1
-    fi
-}
+targets="R:Class ~ '(?i)mediawiki(_exp)?::web::${environment}_sites'"
 
 
 # Check the canary and control servers are in good health
 apache-fast-test "$test_file" "$canary" "$control"
 # Disable puppet everywhere
+echo "Now we will disable puppet on all servers with the shared apache configuration."
 sudo cumin "$targets" "disable-puppet 'apache change ongoing --${USER}'"
 # Merge the puppet change you already submitted
-confirm "Should we merge the puppet change?"
+echo "Puppet is disabled everywhere. Should we merge the puppet change?"
 sudo cumin "$puppetmaster" "TERM=$TERM puppet merge -y"
 # Run puppet on the canary
+echo "Ok to run puppet on the canary host to apply the apache change?"
 sudo cumin "$canary" "run-puppet-agent -e 'apache change ongoing --${USER}'"
 # Re-check the sites
+echo "Checking sites again"
 apache-fast-test "$test_file" "$canary" "$control"
+echo "Things seem OK from our test."
 if [ "$apply" == "yes" ]; then
     # Run puppet everywhere, 5 hosts at a time, which is very conservative.
-    confirm "Should we run apply the puppet change on all servers?"
+    echo "Should we run apply the puppet change on all servers?"
     sudo cumin -b 5 -p 99 "$targets" "run-puppet-agent -e 'apache change ongoing --${USER}'"
 else
     # Enable puppet everywhere when you feel comfortable
-    confirm  "Should puppet be reenabled on all servers?"
+    echo  "Should puppet be reenabled on all servers?"
     sudo cumin "$targets" "enable-puppet 'apache change ongoing --${USER}'"
 fi
