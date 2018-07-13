192064b0fd012b01f7665f8b20f51fbde8b7dfdd
andrewbogott <abogott@wikimedia.org>
2018-07-13 04:44:54 -0500

    labtestn: install designate on labtestservices2002
    Change-Id: I5f7825119ee9cdee3f2e440821d41d4e7ef9d83d

--- modules/role/manifests/wmcs/openstack/labtestn/services.pp
+++ modules/role/manifests/wmcs/openstack/labtestn/services.pp
@@ -2,6 +2,13 @@ class role::wmcs::openstack::labtestn::services {
     system::role { $name: }
     include ::standard
     include ::profile::base::firewall
-    # include ::profile::openstack::labtestn::clientlib
-    # include ::profile::openstack::labtestn::observerenv
+
+    # We need a separate designate to listen to events in labtestn.
+    # For now, though, just use the same DNS setup as labtest.  Eventually
+    #  we'll have to figure out how to migrate over to a new dns server here.
+
+    # This designate uses the same designate DB as the labtest designate;
+    # we want DNS to know about the state of both regions in a single place.
+    include ::profile::openstack::labtestn::cloudrepo
+    include ::profile::openstack::labtestn::designate::service
 }
