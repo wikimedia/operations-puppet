97de784d08cbabe4f47c4ba46a84ddda87c5cfb2
Daniel Zahn <dzahn@wikimedia.org>
2018-07-25 20:07:47 +0200

    postgresql: add class to create db backups
    Add a new class to create backups for postgresql
    databases.
    
    Bug: T190184
    Change-Id: I7744581c9c12ab5e8c8f182446b8e31b294cf216

---
+++ modules/postgresql/manifests/backup.pp
@@ -0,0 +1,47 @@
+#
+# Class: postgresql::backup
+#
+# Provides a way to add backups for postgresql.
+# Uses pg_dumpall to dump all databases and meta information.
+#
+# Parameters:
+# $path
+# Full path to a directory to dump to. No ending slash. default: /srv/postgres-backup
+#
+# $rotate_days
+# Number of days after which old backups are deleted. default: 7
+#
+# Requires:
+# postgresql-client, pg_dumpall, gzip, find
+#
+# Sample Usage:
+# postgresql::backup { 'pg-backup-netbox': }
+#
+class postgresql::backup(
+    String $path = '/srv/postgres-backup',
+    Integer $rotate_days = 7,
+    ) {
+
+    file { $path:
+        ensure => 'directory',
+        owner  => 'postgres',
+        group  => 'postgres',
+        mode   => '0750',
+    }
+
+    cron { 'postgres-dump':
+        ensure  => 'present',
+        command => "pq_dumpall | gzip > ${path}/psql-all-dbs-`date \"+%Y%m%d\"`.sql.gz",
+        user    => 'postgres',
+        hour    => fqdn_rand(23, $title),
+        minute  => fqdn_rand(59, $title),
+    }
+
+    cron { 'postgres-rotate-dump':
+        ensure  => 'present',
+        command => "find ${path} -type -f -name '*.sql.gz' -mtime +$(rotate_days} -delete",
+        user    => 'postgres',
+        hour    => fqdn_rand(23, $title),
+        minute  => fqdn_rand(59, $title),
+    }
+}
