ed3f0c40b520b81cf9c78b0442be5fb9fb860dac
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-07-25 12:41:31 +0200

    grafana: introduce dashboard json validator
    Bug: T200317
    Change-Id: I2b0c3722be9c9f938dbd72d83abc80dae182b37d

---
+++ modules/grafana/files/test_dashboards.py
@@ -0,0 +1,24 @@
+import unittest
+import json
+import os
+
+test_dir = os.path.join(os.path.dirname(__file__))
+dashboards_dir = os.path.join(test_dir, 'dashboards')
+
+
+class ValidJSONDashboardTest(unittest.TestCase):
+    """Make sure dashboards are effectively valid JSON."""
+
+    def load_json(self, path):
+        with open(path) as f:
+            json.load(f)
+        return True
+
+    def testHomeDashboard(self):
+        self.load_json(os.path.join(test_dir, 'home.json'))
+
+    def testCustomDashboards(self):
+        for name in os.listdir(dashboards_dir):
+            dashboard_path = os.path.join(dashboards_dir, name)
+            if os.path.isfile(dashboard_path):
+                self.load_json(dashboard_path)
