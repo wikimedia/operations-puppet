ac44d2d2e21fbc35fcab73fe89ec5b7d21bb6b0c
Daniel Zahn <dzahn@wikimedia.org>
2018-07-26 02:16:25 +0200

    maps: use ::profile::base::firewall
    Change-Id: I2b3b4eb3eb900adbaed7b80b072664167bac9a9c

--- modules/role/manifests/maps/master.pp
+++ modules/role/manifests/maps/master.pp
@@ -1,7 +1,7 @@
 # Sets up a maps server master
 class role::maps::master {
     include ::standard
-    include ::base::firewall
+    include ::profile::base::firewall
     include ::role::lvs::realserver
 
     include ::profile::maps::apps
