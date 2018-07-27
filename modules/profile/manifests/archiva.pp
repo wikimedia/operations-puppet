e9a759555d3a5d5a3cadacccb5107f8b0ee69aca
Luca Toscano <ltoscano@wikimedia.org>
2018-07-27 17:22:16 +0200

    role::archiva: move to profile
    Change-Id: I46c46237c05cd3122cb0cc2aef22e41208aaf2be

---
+++ modules/profile/manifests/archiva.pp
@@ -0,0 +1,47 @@
+# Class: profile::archiva
+#
+# Installs Apache Archiva and sets up a cron job to symlink .jar files to
+# a git-fat store.
+#
+class profile::archiva(
+    $enable_backup = hiera('profile::archiva::enable_backup', false),
+) {
+    require_package('openjdk-7-jdk')
+
+    class { '::archiva':
+        require => Package['openjdk-7-jdk'],
+    }
+
+    # Set up a reverse proxy for the archiva service.
+    class { '::archiva::proxy': }
+
+    class { '::archiva::gitfat': }
+
+    # Bacula backups for /var/lib/archiva.
+    if $enable_backup {
+        include ::profile::backup::host
+        backup::set { 'var-lib-archiva':
+            require => Class['::archiva']
+        }
+    }
+
+    ferm::service { 'archiva_rsync':
+        proto => 'tcp',
+        port  => '873',
+    }
+
+    ferm::service { 'archiva_https':
+        proto => 'tcp',
+        port  => 443,
+    }
+
+    ferm::service { 'archiva_http':
+        proto => 'tcp',
+        port  => 80,
+    }
+
+    monitoring::service { 'https_archiva':
+        description   => 'HTTPS',
+        check_command => 'check_ssl_http_letsencrypt!archiva.wikimedia.org',
+    }
+}
