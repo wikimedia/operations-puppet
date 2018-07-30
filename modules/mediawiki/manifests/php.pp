8eb8343c0171b292d954f96695b35fc4f017ab42
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-07-30 17:06:36 +0200

    mediawiki::php: Remove support for PHP 5
    With terbium gone, we're no longer using PHP5 in Mediawiki anywhere.
    
    Change-Id: Ib8348530971bd754b0ef9841ecb0df7e28364f7e

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -1,43 +1,34 @@
 # == Class: mediawiki::php
 #
-# Packages and .ini files for PHP5 or PHP7 extensions.
+# Packages and .ini files for PHP extensions
 #
 class mediawiki::php {
-    if os_version('debian >= stretch') {
-        include ::mediawiki::packages::php7
-    }
+    include ::mediawiki::packages::php7
 
     mediawiki::php_enmod { ['mail']: }
 
-    if os_version('debian >= stretch') {
-        file { '/etc/php/7.0/fpm':
-            ensure => directory,
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0444',
-        }
+    file { '/etc/php/7.0/fpm':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
 
-        file { '/etc/php/7.0/fpm/php.ini':
-            source => 'puppet:///modules/mediawiki/php/php7.ini',
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0444',
-        }
+    file { '/etc/php/7.0/fpm/php.ini':
+        source => 'puppet:///modules/mediawiki/php/php7.ini',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
 
-        file { '/etc/php/7.0/cli/php.ini':
-            source => 'puppet:///modules/mediawiki/php/php7.ini.cli',
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0444',
-        }
+    file { '/etc/php/7.0/cli/php.ini':
+        source => 'puppet:///modules/mediawiki/php/php7.ini.cli',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+    }
 
-        file { '/etc/php/7.0/mods-available/mail.ini':
-            ensure => absent,
-        }
-    } else {
-        file { '/etc/php5/mods-available/mail.ini':
-            ensure  => absent,
-            require => Package['php-mail'],
-        }
+    file { '/etc/php/7.0/mods-available/mail.ini':
+        ensure => absent,
     }
 }
