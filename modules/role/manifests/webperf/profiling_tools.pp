a876d2a3c61f7724c81235d41bfabb103134ded6
Timo Tijhof <krinklemail@gmail.com>
2018-07-31 21:08:45 +0100

    webperf: Set mpm=prefork explicitly for profiling_tools' httpd
    Bug: T180761
    Change-Id: I044443985f1aff86bc0ca56dd9ca3032fd539e90

--- modules/role/manifests/webperf/profiling_tools.pp
+++ modules/role/manifests/webperf/profiling_tools.pp
@@ -14,6 +14,14 @@ class role::webperf::profiling_tools {
 
     include ::profile::webperf::xhgui
 
+    # class httpd installs mpm_event by default, and once installed,
+    # it cannot easily be uninstalled. The xhgui profile installs
+    # libapache2-mod-php7.0 which in turn installs mpm_prefork, which
+    # fails if mpm_event is present. (T180761)
+    class { '::httpd::mpm':
+        mpm => 'prefork'
+    }
+
     class { '::httpd':
         modules => ['authnz_ldap', 'php7.0', 'rewrite'],
     }
