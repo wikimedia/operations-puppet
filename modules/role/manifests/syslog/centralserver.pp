badaba00b64a132db01106ee129f10a8ce8abd0c
Daniel Zahn <dzahn@wikimedia.org>
2018-08-01 00:25:29 +0200

    syslog::centralserver: use ::profile::base::firewall
    Change-Id: I5518527d110c791ee258c5e9cccb0dbb93b32a45

--- modules/role/manifests/syslog/centralserver.pp
+++ modules/role/manifests/syslog/centralserver.pp
@@ -6,7 +6,7 @@ class role::syslog::centralserver (
     $prometheus_nodes = hiera('prometheus_nodes', []), # lint:ignore:wmf_styleguide
 ) {
     include ::standard
-    include ::base::firewall
+    include ::profile::base::firewall
     include ::profile::backup::host
 
     system::role { 'syslog::centralserver':
