3136b159f80f7f3cc9d0f7654a35be80b22a9e0d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-08-01 15:39:44 +0200

    mediawiki: convert remnant.conf to use one file per vhost
    Bug: T196968
    Change-Id: I5904d75c23369d05c1da6f5d858d04d29c949067

--- modules/mediawiki/manifests/web/prod_sites.pp
+++ modules/mediawiki/manifests/web/prod_sites.pp
@@ -11,31 +11,37 @@ class mediawiki::web::prod_sites {
         priority => 3,
     }
 
+    # Other wikis
     apache::site { 'remnant':
         source   => 'puppet:///modules/mediawiki/apache/sites/remnant.conf',
         priority => 4,
     }
 
+    # Search vhost
     apache::site { 'search.wikimedia':
         source   => 'puppet:///modules/mediawiki/apache/sites/search.wikimedia.conf',
         priority => 5,
     }
 
+    # Old secure redirects
     apache::site { 'secure.wikimedia':
         source   => 'puppet:///modules/mediawiki/apache/sites/secure.wikimedia.conf',
         priority => 6,
     }
 
+    # Wikimania sites, plus one wiki for wikimaniateam
     apache::site { 'wikimania':
         source   => 'puppet:///modules/mediawiki/apache/sites/wikimania.conf',
         priority => 7,
     }
 
+    # wikimediafoundation wiki, already a single wiki
     apache::site { 'foundation':
         source   => 'puppet:///modules/mediawiki/apache/sites/foundation.conf',
         priority => 8,
     }
 
+    # Some other wikis, plus loginwiki, and www.wikimedia.org
     apache::site { 'wikimedia':
         source   => 'puppet:///modules/mediawiki/apache/sites/wikimedia.conf',
         priority => 9,
@@ -51,6 +57,40 @@ class mediawiki::web::prod_sites {
         before => Apache::Site['main']
     }
 
+    # Remnant related wikis
+    $remnant_conf_sites = [
+        'meta.wikimedia.org',
+        '_wikisource.org',
+        'commons.wikimedia.org',
+        'incubator.wikimedia.org',
+        'species.wikimedia.org',
+        'usability.wikimedia.org',
+        'strategy.wikimedia.org',
+        'advisory.wikimedia.org',
+        'quality.wikimedia.org',
+        'outreach.wikimedia.org'
+    ]
+    mediawiki::web::site { $remnant_conf_sites:
+        before => Apache::Site['remnant']
+    }
+
+    # private wikis in remnant.conf; they all change just by ServerName
+    $small_private_wikis = [
+        'internal.wikimedia.org', 'grants.wikimedia.org', 'fdc.wikimedia.org',
+        'board.wikimedia.org', 'boardgovcom.wikimedia.org', 'spcom.wikimedia.org',
+        'affcom.wikimedia.org', 'searchcom.wikimedia.org',
+        'office.wikimedia.org', 'chair.wikimedia.org',
+        'auditcom.wikimedia.org', 'otrs-wiki.wikimedia.org',
+        'exec.wikimedia.org', 'collab.wikimedia.org',
+        'movementroles.wikimedia.org', 'checkuser.wikimedia.org',
+        'steward.wikimedia.org', 'ombudsmen.wikimedia.org',
+        'projectcom.wikimedia.org', 'techconduct.wikimedia.org',
+        'electcom.wikimedia.org', 'advisors.wikimedia.org'
+    ]
+    mediawiki::web::site { $small_private_wikis:
+        template_name => 'private-https',
+        before        => Apache::Site['remnant'],
+    }
 
     mediawiki::web::site { 'wikimaniateam.wikimedia.org':
         before => Apache::Site['wikimania']
