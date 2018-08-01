324b3d841713e67b7ad0a88399da90c12ee71271
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-08-01 10:13:18 +0200

    Remove Jessie-specific Puppet code from Mediawiki math class
    We only use Stretch at this point.
    
    Change-Id: I95c1af322bd564e825376ea849866b0182f053a0

--- modules/mediawiki/manifests/packages/math.pp
+++ modules/mediawiki/manifests/packages/math.pp
@@ -3,11 +3,5 @@
 # Provisions packages used by MediaWiki for math rendering.
 #
 class mediawiki::packages::math {
-    require_package('dvipng', 'gsfonts', 'make', 'ocaml', 'ploticus')
-
-    if os_version('debian == jessie') {
-        require_package('mediawiki-math-texvc')
-    } else {
-        require_package('texvc')
-    }
+    require_package('dvipng', 'gsfonts', 'make', 'ocaml', 'ploticus', 'texvc')
 }
