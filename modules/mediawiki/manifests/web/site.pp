9c3ffcd4e7a628d7f260a12e4efc9e4bdab44dea
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-08-01 08:01:24 +0200

    mediawiki::web::site: backport changes from mediawiki_exp
    Change-Id: Ia00375bf29fc98e75a004af8be407cff13efb8dd

--- modules/mediawiki/manifests/web/site.pp
+++ modules/mediawiki/manifests/web/site.pp
@@ -1,13 +1,15 @@
-# == define mediawiki::site
+# == define mediawiki::web::site
 #
 # Defines a simple file that can be included in the mediawiki
 # apache configuration. This will allow us to convert our sites to using mediawiki::vhost one by one
 # if needed (while allowing us to see a diff in the puppet compiler).
 #
 # This define is transitional and should NOT be used outside of the mediawiki::web hierarchy
-define mediawiki::web::site() {
+define mediawiki::web::site(
+    String $template_name = $title,
+) {
     file { "/etc/apache2/sites-available/${title}.conf":
-        content => template("mediawiki/apache/sites/included/${title}.conf.erb"),
+        content => template("mediawiki/apache/sites/included/${template_name}.conf.erb"),
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
