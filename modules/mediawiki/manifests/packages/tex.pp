af4759b7552adca29f9f3cc3335193cbed828694
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-08-01 10:19:55 +0200

    Remove jessie-specific code from mediawiki::packages::tex, cleanups
    We're only using stretch at this point, remove obsolete conditionals.
    While we're at it, also switch to require_package() for all packages.
    
    Change-Id: If2b1a2570dedcd580ae08c6d984bf2a8e8f2fe3b

--- modules/mediawiki/manifests/packages/tex.pp
+++ modules/mediawiki/manifests/packages/tex.pp
@@ -3,24 +3,7 @@
 # Provisions packages used by MediaWiki for TeX rendering.
 #
 class mediawiki::packages::tex {
-    package { [
-        'texlive',
-        'texlive-bibtex-extra',
-        'texlive-font-utils',
-        'texlive-fonts-extra',
-        'texlive-generic-extra',
-        'texlive-lang-all',
-        'texlive-latex-extra',
-        'texlive-pictures',
-        'texlive-pstricks',
-        'texlive-publishers',
-    ]:
-        ensure => present,
-    }
-
-    if os_version('debian >= stretch') {
-        require_package('texlive-science')
-    } else {
-        require_package('texlive-math-extra')
-    }
+    require_package('texlive', 'texlive-bibtex-extra', 'texlive-science', 'texlive-font-utils')
+    require_package('texlive-fonts-extra', 'texlive-generic-extra', 'texlive-lang-all', 'texlive-pictures')
+    require_package('texlive-latex-extra', 'texlive-pstricks', 'texlive-publishers')
 }
