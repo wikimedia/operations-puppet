b78632b7b0a1ed53420f27002610ca64ec3f28c9
Ayounsi <ayounsi@wikimedia.org>
2018-08-01 15:37:10 +0000

    Revert "Extend cp to cp ipsec MTU 1450 to codfw"
    This reverts commit f68db57a1eb0ca400a22c916ddeef28cfc7d93fa.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I86b5fc70d2d5815952c72493243e1e1508352f81

--- modules/role/manifests/ipsec.pp
+++ modules/role/manifests/ipsec.pp
@@ -43,7 +43,6 @@ class role::ipsec ($hosts = undef) {
     # progressively push to more sites
     $mtu_value = $::site ? {
       ulsfo => 1450,
-      codfw => 1450,
       default => undef,
     }
     class { '::strongswan':
