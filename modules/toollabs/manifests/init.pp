06e4457944b6ae62584bd8968f94becf8bcba4f4
Brooke Storm <bstorm@wikimedia.org>
2018-08-06 11:46:06 -0600

    gridengine: remove variables that were for deleted template
    Bug: T139190
    Change-Id: I9ae355177308d231617c191bbed8a905f226d770

--- modules/toollabs/manifests/init.pp
+++ modules/toollabs/manifests/init.pp
@@ -158,9 +158,6 @@ class toollabs (
         content => $active_proxy,
     }
 
-    $active_redis = hiera('active_redis')
-    $active_redis_ip = ipresolve($active_redis, 4, $::nameservers[0])
-
     # Silence e-mails sent when regular users try to sudo (T95882)
     file { '/etc/sudoers.d/40-tools-sudoers-no-warning':
         ensure => file,
