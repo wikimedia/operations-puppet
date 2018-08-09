0f9379682754578fcaa24c216c1a2c195a798e05
Jaime Crespo <jcrespo@wikimedia.org>
2018-08-09 15:45:23 +0200

    wikireplicas: Add SUPER privileges to cloud admin accounts
    So they can continue writing even in read-write mode.
    
    If this doesn't work, then strip SUPER from most users, and then
    set wikireplicas in read-write.
    
    Bug: T172489
    Change-Id: Ieb0e8a7e808f82f8558fd9d840f0f2152d7126c7

--- modules/role/templates/mariadb/grants/wiki-replicas.sql
+++ modules/role/templates/mariadb/grants/wiki-replicas.sql
@@ -1,34 +1,39 @@
--- Initial grants and grants added at T178128
-GRANT labsdbuser TO 'labsdbadmin'@'10.64.37.19' WITH ADMIN OPTION
-GRANT SELECT, INSERT, UPDATE ON `mysql`.* TO 'labsdbadmin'@'10.64.37.19'
-GRANT SELECT, SHOW VIEW ON `%\\_p`.* TO 'labsdbadmin'@'10.64.37.19' WITH GRANT OPTION
-GRANT SELECT, SHOW VIEW ON `%wik%`.* TO 'labsdbadmin'@'10.64.37.19'
+-- These grants are wrong, as they lack passwords- do not use directly
 
-GRANT labsdbuser TO 'labsdbadmin'@'10.64.37.20' WITH ADMIN OPTION
-GRANT SELECT, INSERT, UPDATE ON `mysql`.* TO 'labsdbadmin'@'10.64.37.20'
-GRANT SELECT, SHOW VIEW ON `%wik%`.* TO 'labsdbadmin'@'10.64.37.20'
-GRANT SELECT, SHOW VIEW ON `%\\_p`.* TO 'labsdbadmin'@'10.64.37.20' WITH GRANT OPTION
+-- Initial grants and grants added at T178128
+GRANT labsdbuser TO 'labsdbadmin'@'10.64.37.19' WITH ADMIN OPTION;
+GRANT SUPER ON *.* TO 'labsdbadmin'@'10.64.37.19';
+GRANT SELECT, INSERT, UPDATE ON `mysql`.* TO 'labsdbadmin'@'10.64.37.19';
+GRANT SELECT, SHOW VIEW ON `%\\_p`.* TO 'labsdbadmin'@'10.64.37.19' WITH GRANT OPTION;
+GRANT SELECT, SHOW VIEW ON `%wik%`.* TO 'labsdbadmin'@'10.64.37.19';
+
+GRANT labsdbuser TO 'labsdbadmin'@'10.64.37.20' WITH ADMIN OPTION;
+GRANT SUPER ON *.* TO 'labsdbadmin'@'10.64.37.20';
+GRANT SELECT, INSERT, UPDATE ON `mysql`.* TO 'labsdbadmin'@'10.64.37.20';
+GRANT SELECT, SHOW VIEW ON `%wik%`.* TO 'labsdbadmin'@'10.64.37.20';
+GRANT SELECT, SHOW VIEW ON `%\\_p`.* TO 'labsdbadmin'@'10.64.37.20' WITH GRANT OPTION;
 
 -- Labsdbuser is a role with privileges for all views like
 -- GRANT SELECT, SHOW VIEW ON `rowikiquote\_p`.* TO 'labsdbuser'
-GRANT USAGE ON *.* TO 'labsdbuser'
+GRANT USAGE ON *.* TO 'labsdbuser';
 
 -- maintainviews user used by cloud services team
-GRANT ALL PRIVILEGES ON `heartbeat\\_p`.* TO 'maintainviews'@'localhost'
-GRANT ALL PRIVILEGES ON `meta\\_p`.* TO 'maintainviews'@'localhost'
-GRANT ALL PRIVILEGES ON `centralauth\\_p`.* TO 'maintainviews'@'localhost'
-GRANT SELECT ON `centralauth`.* TO 'maintainviews'@'localhost'
-GRANT SELECT ON `heartbeat`.* TO 'maintainviews'@'localhost'
-GRANT ALL PRIVILEGES ON `%wik%\\_p`.* TO 'maintainviews'@'localhost'
-GRANT ALL PRIVILEGES ON `%\\_p`.* TO 'maintainviews'@'localhost'
-GRANT SELECT, DROP, CREATE VIEW ON `%wik%`.* TO 'maintainviews'@'localhost'
-GRANT SELECT (user, host) ON `mysql`.`user` TO 'maintainviews'@'localhost'
+GRANT ALL PRIVILEGES ON `heartbeat\\_p`.* TO 'maintainviews'@'localhost';
+GRANT ALL PRIVILEGES ON `meta\\_p`.* TO 'maintainviews'@'localhost';
+GRANT ALL PRIVILEGES ON `centralauth\\_p`.* TO 'maintainviews'@'localhost';
+GRANT SELECT ON `centralauth`.* TO 'maintainviews'@'localhost';
+GRANT SELECT ON `heartbeat`.* TO 'maintainviews'@'localhost';
+GRANT ALL PRIVILEGES ON `%wik%\\_p`.* TO 'maintainviews'@'localhost';
+GRANT ALL PRIVILEGES ON `%\\_p`.* TO 'maintainviews'@'localhost';
+GRANT SELECT, DROP, CREATE VIEW ON `%wik%`.* TO 'maintainviews'@'localhost';
+GRANT SELECT (user, host) ON `mysql`.`user` TO 'maintainviews'@'localhost';
 
 -- maintainindexes user, used by cloud services team
 GRANT SELECT, INDEX, ALTER ON `%wik%`.* TO 'maintainindexes'@'localhost';
+GRANT SUPER ON *.* TO 'maintainindexes'@'localhost';
 
 -- viewmaster user
-GRANT SELECT ON *.* TO 'viewmaster'@'%'
+GRANT SELECT ON *.* TO 'viewmaster'@'%';
 
 -- quarry user granted 48 connections #T180141
 GRANT USAGE ON *.* TO 's52788'@'%' WITH MAX_USER_CONNECTIONS 48;
