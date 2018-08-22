49b66e2ce8243d9c3473b9912371422f569d2aad
Daniel Zahn <dzahn@wikimedia.org>
2018-08-22 15:58:46 -0700

    quarry::redis: convert to profile
    Change-Id: I7c89983a16b916c8d0ce8e677837be4c2ea21d75

--- modules/profile/manifests/quarry/redis.pp
+++ modules/profile/manifests/quarry/redis.pp
@@ -1,9 +1,9 @@
-# = Class: quarry::redis
+# = Class: profile::quarry::redis
 #
 # Sets up a redis instance for use as caching and session storage
 # by the Quarry frontends and also as working queue & results
 # backend by the query runners.
-class quarry::redis {
+class profile::quarry::redis {
     redis::instance { '6379':
         settings => {
             bind      => '0.0.0.0',
--- modules/role/manifests/labs/quarry/redis.pp
+++ modules/role/manifests/labs/quarry/redis.pp
@@ -1,7 +1,5 @@
 # filtertags: labs-project-quarry
 class role::labs::quarry::redis {
     require ::profile::labs::lvm::srv
-
-    class { '::quarry::redis':
-    }
+    include ::profile::quarry::redis
 }
