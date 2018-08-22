9d39a5eb7bdf12ebe94506c85c07057164e1747e
Luca Toscano <ltoscano@wikimedia.org>
2018-08-22 11:35:51 +0200

    profile::archiva::proxy: protect first run with firewall rules
    The first run of Archiva after its installation allows to create
    a Admin account, that will have unlimited power on Archiva. To protect
    this use case when the archiva host runs on untrusted networks (like
    when it is exposed to Internet), a new option to limit port 80/443's
    ferm srange is introduced.
    
    This should ease the deployment of archiva to the new host (archiva1001)
    without risking that somebody external to WMF can compromise Archiva's
    security.
    
    Bug: T192639
    Change-Id: I2e317d5a43040dfa05bce3c8fb3fdc7aa26ffb42

--- modules/profile/manifests/archiva/proxy.pp
+++ modules/profile/manifests/archiva/proxy.pp
@@ -10,23 +10,38 @@
 #    Enable TLS settings for archiva.wikimedia.org and deploy
 #    related certificates.
 #
+#  [*only_localhost*]
+#    Right after the installation step, achiva will ask to the user
+#    to create an Admin account with related password. If the host is exposed
+#    to untrusted networks (like the public Internet), it will have no
+#    protection against any attacker. This option restricts the firewall rules
+#    to allow only localhost TCP connections.
+#
 class profile::archiva::proxy(
-    $ssl_enabled = hiera('profile::archiva::proxy::ssl_enabled', false),
+    $ssl_enabled    = hiera('profile::archiva::proxy::ssl_enabled', false),
+    $only_localhost = hiera('profile::archiva::proxy::only_localhost', false),
 ) {
 
     class { '::archiva::proxy':
         ssl_enabled => $ssl_enabled,
     }
 
+    $ferm_srange = $only_localhost ? {
+        true  => '127.0.0.1 localhost',
+        false => undef,
+    }
+
     ferm::service { 'archiva_http':
-        proto => 'tcp',
-        port  => 80,
+        proto  => 'tcp',
+        port   => 80,
+        srange => $ferm_srange,
     }
 
     if $ssl_enabled {
         ferm::service { 'archiva_https':
-            proto => 'tcp',
-            port  => 443,
+            proto  => 'tcp',
+            port   => 443,
+            srange => $ferm_srange,
         }
 
         monitoring::service { 'https_archiva':
