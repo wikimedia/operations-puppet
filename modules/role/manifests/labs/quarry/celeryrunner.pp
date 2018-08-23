f9ed1439fa06991d7218a29d560748de79a96776
Daniel Zahn <dzahn@wikimedia.org>
2018-08-23 12:40:00 -0700

    quarry: fix changed class name in celeryrunner
    Change-Id: I41b0d619d9c2a042ca13dd05f1653042b36505af

--- modules/role/manifests/labs/quarry/celeryrunner.pp
+++ modules/role/manifests/labs/quarry/celeryrunner.pp
@@ -1,9 +1,6 @@
 # filtertags: labs-project-quarry
 class role::labs::quarry::celeryrunner {
     require ::profile::labs::lvm::srv
-    include ::labs_debrepo
-
-    class { '::quarry::celeryrunner':
-        require => [Class['::labs_debrepo']],
-    }
+    require ::labs_debrepo
+    include ::profile::quarry::celeryrunner
 }
