d15f9a2c5ebbba2c148f3e31ac8385eee9467230
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-08-23 14:06:48 +0200

    Recognise archiva-deployers LDAP group in offboarding script
    This new group will be used to manage access to deployment rights in
    Archiva (previously a shared user was used).
    
    Bug: T200454
    Change-Id: I98fdc2b06d2dafd7d1d6a1c39deb7b13daa87d50

--- modules/openldap/files/offboard-user.py
+++ modules/openldap/files/offboard-user.py
@@ -68,6 +68,7 @@ member: {user_dn}
                          'cn=grafana-admin,ou=groups,dc=wikimedia,dc=org',
                          'cn=ciadmin,ou=groups,dc=wikimedia,dc=org',
                          'cn=releng,ou=groups,dc=wikimedia,dc=org',
+                         'cn=archiva-deployers,ou=groups,dc=wikimedia,dc=org',
                          'cn=tools.admin,ou=servicegroups,dc=wikimedia,dc=org']
     ldapdata = ldap_conn.search_s(
         base_dn,
