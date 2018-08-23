e6f5527c2335155b71129bd649296ff1c17e46b2
Kartik Mistry <kartik.mistry@gmail.com>
2018-08-23 11:01:29 +0530

    Add LingoCloud MT config
    Bug: T202604
    Change-Id: I3235288d1f256203b45f31b36a3bfb6e50a69350

--- modules/profile/manifests/cxserver.pp
+++ modules/profile/manifests/cxserver.pp
@@ -6,10 +6,11 @@ class profile::cxserver(
     $apertium_uri=hiera('profile::cxserver::apertium_uri')
 ) {
     include ::passwords::cxserver
-    $matxin_api_key = $::passwords::cxserver::matxin_api_key
-    $yandex_api_key = $::passwords::cxserver::yandex_api_key
-    $youdao_api_key = $::passwords::cxserver::youdao_api_key
-    $jwt_secret = $::passwords::cxserver::jwt_secret
+    $lingocloud_api_key = $::passwords::cxserver::lingocloud_api_key
+    $matxin_api_key     = $::passwords::cxserver::matxin_api_key
+    $yandex_api_key     = $::passwords::cxserver::yandex_api_key
+    $youdao_api_key     = $::passwords::cxserver::youdao_api_key
+    $jwt_secret         = $::passwords::cxserver::jwt_secret
 
     service::node { 'cxserver':
         port              => 8080,
@@ -18,11 +19,13 @@ class profile::cxserver(
         deployment        => 'scap3',
         deployment_config => true,
         deployment_vars   => {
-            jwt_token    => $jwt_secret,
-            apertium_uri => $apertium_uri,
-            matxin_key   => $matxin_api_key,
-            yandex_key   => $yandex_api_key,
-            youdao_key   => $youdao_api_key,
+            jwt_token          => $jwt_secret,
+            apertium_uri       => $apertium_uri,
+            lingocloud_key     => $lingocloud_api_key,
+            lingocloud_account => 'wikimedia',
+            matxin_key         => $matxin_api_key,
+            yandex_key         => $yandex_api_key,
+            youdao_key         => $youdao_api_key,
         },
     }
 }
