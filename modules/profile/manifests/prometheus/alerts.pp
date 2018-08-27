728d1cca5c70ee2a540798facced315a36918906
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-08-27 16:39:41 +0200

    prometheus: alert on unusual day-over-day logstash ingestion rate change
    The idea is to spot cases where Logstash is receiving an "unsual" amount of logs, compared to
    yesterday.
    
    Bug: T202307
    Change-Id: Ifce8680690244f77cdc932ad233d0b19ef940654

--- modules/profile/manifests/prometheus/alerts.pp
+++ modules/profile/manifests/prometheus/alerts.pp
@@ -125,4 +125,20 @@ class profile::prometheus::alerts {
     monitoring::alerts::traffic_drop{'traffic_drop_esams': site => 'esams'}
     monitoring::alerts::traffic_drop{'traffic_drop_ulsfo': site => 'ulsfo'}
     monitoring::alerts::traffic_drop{'traffic_drop_eqsin': site => 'eqsin'}
+
+    # Alert on unusual day-over-day logstash ingestion rate change - T202307
+    monitoring::check_prometheus { 'logstash_ingestion_spike':
+        description     => 'Logstash rate of ingestion percent change compared to yesterday',
+        # Divide rate of input now vs yesterday, multiplied by 100
+        query           => '100 * (sum (rate(logstash_node_plugin_events_out_total{plugin_id=~"input/.*"}[5m])) / sum (rate(logstash_node_plugin_events_out_total{plugin_id=~"input/.*"}[5m] offset 1d)))',
+        prometheus_url  => 'http://prometheus.svc.eqiad.wmnet/ops',
+        warning         => 110,
+        critical        => 130,
+        method          => 'ge',
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/logstash?orgId=1&panelId=2&fullscreen'],
+        # Check every 120 minutes, once in breach check every 10 minutes up to 5 times
+        check_interval  => 120,
+        retry_interval  => 10,
+        retries         => 5,
+    }
 }
