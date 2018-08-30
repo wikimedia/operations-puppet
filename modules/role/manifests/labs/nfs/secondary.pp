ff3508e203134a3e639f8b5de16fc138b93ce897
Brooke Storm <bstorm@wikimedia.org>
2018-08-29 20:46:15 -0400

    labstore: move the load numbers for our problem children up temporarily
    Change-Id: I0d89b59b628e45174f91a05b3aff520795225441

--- modules/role/manifests/labs/nfs/secondary.pp
+++ modules/role/manifests/labs/nfs/secondary.pp
@@ -116,8 +116,8 @@ class role::labs::nfs::secondary(
     include labstore::monitoring::nfsd
     class { 'labstore::monitoring::interfaces':
         monitor_iface => $monitor_iface,
-        load_warn     => '24',
-        load_crit     => '36',
+        load_warn     => '36',
+        load_crit     => '48',
     }
 
     class { 'labstore::monitoring::secondary':
