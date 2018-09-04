8c037eabf28be1f70bfb8cdaa2558dc996d70841
Balazs Pocze <bpocze@wikimedia.org>
2018-09-04 11:42:19 +0200

    Labs: Add support for socket path and/or port to redact_sanitarium.sh
    On multi-instance MariaDB hosts we should be able to do the
    redact sanitarium process on every instance and we need to connect to
    those instances through socket.
    The possible list of sanitarium hosts were also updated.
    
    Bug: T203394
    Change-Id: I7b2f205e454fc77661c37aa3c5f8dac330b5ffa6

--- modules/role/files/mariadb/redact_sanitarium.sh
+++ modules/role/files/mariadb/redact_sanitarium.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-db=$1
-host=${2:-localhost}
-port=${3:-3306}
+host=localhost                   # default host localhost
+port=3306                        # default port 3306
+socket="/run/mysqld/mysqld.sock" # default socket to connect. If host & port are unset this parameter will used as dsn
 
-if [ -z "$db" ]; then
-    echo "usage: <script> <db> [<host> [<port>]]" 1>&2
+while getopts "d:S:h:P:" flag; do
+case ${flag} in
+    d) db=${OPTARG};;
+    S) socket=${OPTARG};;
+    h) host=${OPTARG};;
+    P) port=${OPTARG};;
+esac
+done
+
+if [ -z "${db}" ]; then
+    echo "usage: <script> <-d db> [<-S socket> | <-h host> [<-P port>]]" 1>&2
     exit 1
 fi
 
-if [[ ! "$host" =~ ^(db1(102|124|125)(\..+)?|localhost)$ ]]; then
-    echo "unexpected sanitarium host! $host" 2>&1
+if [[ ! "${host}" =~ ^(db(1124|1125|2094|2095)(\..+)?|localhost)$ ]]; then
+    echo "unexpected sanitarium host! ${host}" 2>&1
     exit 1
 fi
 
-if [[ ! "$port" =~ ^33(06|1[12345678])$ ]]; then
-    echo "unexpected sanitarium port! $port" 2>&1
+if [[ ! "${port}" =~ ^33(06|1[12345678])$ ]]; then
+    echo "unexpected sanitarium port! ${port}" 2>&1
     exit 1
 fi
 
-if [[ "$host" == "localhost" && "$port" == "3306" ]]; then
-    dsn=""
+if [[ "${host}" == "localhost" && "${port}" == "3306" ]]; then
+    dsn="-S ${socket}"
 else
-    dsn="-h $host -P $port"
+    dsn="-h ${host} -P ${port}"
 fi
 
-query="mysql --skip-ssl --skip-column-names $dsn -e "
+query="mysql --skip-ssl --skip-column-names ${dsn} -e "
 
 for tbl in $(egrep ',F' /etc/mysql/filtered_tables.txt | awk -F ',' '{print $1}' | uniq); do
 
@@ -37,51 +46,51 @@ for tbl in $(egrep ',F' /etc/mysql/filtered_tables.txt | awk -F ',' '{print $1}'
 
     for col in $(egrep "${tbl},.*,F" /etc/mysql/filtered_tables.txt | awk -F ',' '{print $2}'); do
 
-        datatype=$($query "select data_type from columns where table_schema = '${db}' and table_name = '${tbl}' and column_name = '${col}'" information_schema)
+        datatype=$(${query} "select data_type from columns where table_schema = '${db}' and table_name = '${tbl}' and column_name = '${col}'" information_schema)
 
-        if [ -n "$datatype" ]; then
+        if [ -n "${datatype}" ]; then
 
-            echo "-- $col found"
+            echo "-- ${col} found"
 
-            if [[ "$datatype" =~ int ]]; then
-                insert="$insert NEW.${col} = 0,"
-                update="$update NEW.${col} = 0,"
-                remove="$remove ${col} = 0,"
+            if [[ "${datatype}" =~ int ]]; then
+                insert="${insert} NEW.${col} = 0,"
+                update="${update} NEW.${col} = 0,"
+                remove="${remove} ${col} = 0,"
             else
-                insert="$insert NEW.${col} = '',"
-                update="$update NEW.${col} = '',"
-                remove="$remove ${col} = '',"
+                insert="${insert} NEW.${col} = '',"
+                update="${update} NEW.${col} = '',"
+                remove="${remove} ${col} = '',"
             fi
 
         else
-            echo "-- $col MISSING"
+            echo "-- ${col} MISSING"
         fi
 
     done
 
-    if [[ "$insert" =~ ^(.*),$ ]]; then
+    if [[ "${insert}" =~ ^(.*),$ ]]; then
         insert="${BASH_REMATCH[1]}"
     fi
 
-    if [[ "$update" =~ ^(.*),$ ]]; then
+    if [[ "${update}" =~ ^(.*),$ ]]; then
         update="${BASH_REMATCH[1]}"
     fi
 
-    if [[ "$remove" =~ ^(.*),$ ]]; then
+    if [[ "${remove}" =~ ^(.*),$ ]]; then
         remove="${BASH_REMATCH[1]}"
     fi
 
     echo "DROP TRIGGER IF EXISTS ${db}.${tbl}_insert;"
     echo "DROP TRIGGER IF EXISTS ${db}.${tbl}_update;"
 
-    if [[ ! "$insert" =~ SET$ ]]; then
+    if [[ ! "${insert}" =~ SET$ ]]; then
 
-        echo "-- $insert"
-        echo "$insert;"
-        echo "-- $update"
-        echo "$update;"
-        echo "-- $remove"
-        echo "set session binlog_format = 'STATEMENT'; $remove;"
+        echo "-- ${insert}"
+        echo "${insert};"
+        echo "-- ${update}"
+        echo "${update};"
+        echo "-- ${remove}"
+        echo "set session binlog_format = 'STATEMENT'; ${remove};"
 
     fi
 
