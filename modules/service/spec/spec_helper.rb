ffd26796e550f9bb626adc0eb137539dddcd8e67
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-09-06 14:45:40 +0200

    service: fix spec for debian 9+
    We're basically reproducing via monkey-patching the behaviour that the
    debian package uses - it defaults to using systemd as a provider of the
    service type in debian.
    
    Bug: T203645
    Change-Id: I5bc3082b539283053ebcdcf2c8f1d095683386a6

--- modules/service/spec/spec_helper.rb
+++ modules/service/spec/spec_helper.rb
@@ -9,3 +9,8 @@ RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
 end
+# Force usage of the systemd provider by overriding the stupid defaults
+# set by puppetlabs. This is the same thing that the debian package does,
+# please see https://salsa.debian.org/puppet-team/puppet/commit/428f6e560dea3cab2f0be39d51806c321bbf6e61
+service_type = Puppet::Type.type(:service)
+service_type.provider_hash[:systemd].defaultfor :operatingsystem => :debian
