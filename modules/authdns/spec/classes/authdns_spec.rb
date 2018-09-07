c129125ee08238fe04a77143a450a1e249059fe6
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-09-07 11:10:42 +0200

    authdns: fix spec tests
    * Add systemd service provider monkey-patching
    * Add missing dependencies in fixtures
    * Add more pre-conditions to the authdns class test
    * Remove munging of facts
    * Correctly split the tests in the two files present.
    
    Change-Id: I3a09d82046465ce6dbead6a9e8c8d6be9a93693b

--- modules/authdns/spec/classes/authdns_spec.rb
+++ modules/authdns/spec/classes/authdns_spec.rb
@@ -14,30 +14,22 @@ describe 'authdns' do
 
   on_supported_os(test_on).each do |os, facts|
     context "On #{os}" do
-      facts[:initsystem] = 'systemd'
       let(:facts) { facts }
       let(:params) { {
                        :lvs_services => {},
                        :discovery_services => {},
                      } }
       let(:pre_condition) { [
+                              '$site = "test"',
                               'define git::clone($directory, $origin, $branch,$owner,$group) {}',
                               'define monitoring::service($description,$check_command) {}',
                               'define ssh::userkey($content) {}',
                               'define sudo::user($privileges) {}',
                               'class confd($prefix) {}',
                               'package{ "git": }',
+                              'include ::apt'
                             ] }
       it { is_expected.to compile.with_all_deps  }
-    end
-  end
-end
-
-describe 'authdns::lint' do
-  on_supported_os(test_on).each do |os, facts|
-    let(:facts) { facts }
-    context "On #{os}" do
-      it { should compile }
     end
   end
 end
--- modules/authdns/spec/spec_helper.rb
+++ modules/authdns/spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rspec-puppet'
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
+require_relative '../../../rake_modules/fix_service_provider'
 
 include RspecPuppetFacts
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
