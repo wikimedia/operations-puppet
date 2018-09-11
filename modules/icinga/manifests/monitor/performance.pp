6fb1d2ac6a34373a06c7eea80dd62079b2671757
Daniel Zahn <dzahn@wikimedia.org>
2018-09-11 15:48:48 -0700

    icinga::performance: remind users to ignore checks using notes_url
    Use the notes_url parameter (added to grafana_alerts in I9da86d026550)
    to link to T203485 in all performance/grafana checks to remind
    SRE users to ignore un-ACKed alerts per ticket discussion.
    
    Also fixing lint warnigns about unaligned arrows.
    
    T197873#4572835 shows how this looks in the web UI.
    
    Bug: T203485
    Change-Id: Ie72a5a5efcdf46815ffc05252ac31c632800b758

--- modules/icinga/manifests/monitor/performance.pp
+++ modules/icinga/manifests/monitor/performance.pp
@@ -3,26 +3,32 @@
 # Monitor Performance
 class icinga::monitor::performance {
     monitoring::grafana_alert { 'db/webpagetest-alerts':
-        contact_group   => 'team-performance',
+        contact_group => 'team-performance',
+        notes_url     => 'https://phabricator.wikimedia.org/T203485',
     }
 
     monitoring::grafana_alert { 'db/navigation-timing-alerts':
-        contact_group   => 'team-performance',
+        contact_group => 'team-performance',
+        notes_url     => 'https://phabricator.wikimedia.org/T203485',
     }
 
     monitoring::grafana_alert { 'db/save-timing-alerts':
-        contact_group   => 'team-performance',
+        contact_group => 'team-performance',
+        notes_url     => 'https://phabricator.wikimedia.org/T203485',
     }
 
     monitoring::grafana_alert { 'db/resourceloader-alerts':
-        contact_group   => 'team-performance',
+        contact_group => 'team-performance',
+        notes_url     => 'https://phabricator.wikimedia.org/T203485',
     }
 
     monitoring::grafana_alert { 'db/webpagereplay-desktop-alerts':
-        contact_group   => 'team-performance',
+        contact_group => 'team-performance',
+        notes_url     => 'https://phabricator.wikimedia.org/T203485',
     }
 
     monitoring::grafana_alert { 'db/webpagereplay-mobile-alerts':
-        contact_group   => 'team-performance',
+        contact_group => 'team-performance',
+        notes_url     => 'https://phabricator.wikimedia.org/T203485',
     }
 }
