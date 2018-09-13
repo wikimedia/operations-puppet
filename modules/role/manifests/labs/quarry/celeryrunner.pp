5fe627ec6b0633fe40e5d093242693d7f0f950e9
zhuyifei1999 <zhuyifei1999@gmail.com>
2018-09-13 16:53:31 -0500

    quarry: Get rid of ::labs_debrepo
    It was used for some self-built deb packages. We no longer use them.
    
    Role web also includes a redundant dependency on profile base. The
    dependency is already declared in profile web.
    
    Bug: T153615
    Change-Id: I0795863972e9f28cc883d5a39610cdd06f77d2b2

--- modules/role/manifests/labs/quarry/celeryrunner.pp
+++ modules/role/manifests/labs/quarry/celeryrunner.pp
@@ -1,6 +1,5 @@
 # filtertags: labs-project-quarry
 class role::labs::quarry::celeryrunner {
     require ::profile::labs::lvm::srv
-    require ::labs_debrepo
     include ::profile::quarry::celeryrunner
 }
--- modules/role/manifests/labs/quarry/web.pp
+++ modules/role/manifests/labs/quarry/web.pp
@@ -1,8 +1,5 @@
 # filtertags: labs-project-quarry
 class role::labs::quarry::web {
     require ::profile::labs::lvm::srv
-    require ::labs_debrepo
-
-    require ::profile::quarry::base
     include ::profile::quarry::web
 }
