765a2c2bdb4fc8e12fd96c59d9b0ca7e27841cd6
andrewbogott <abogott@wikimedia.org>
2018-09-19 00:32:48 -0500

    keystone hooks: remove port 5666 from default security groups
    I think this must be leftover from a previous monitoring regime;
    I can't find any evidence that it's used now.
    
    Change-Id: I97829e9c33ead520e97f16fb9d4903f6f4278ac2

--- modules/openstack/files/mitaka/keystone/wmfkeystonehooks/wmfkeystonehooks.py
+++ modules/openstack/files/mitaka/keystone/wmfkeystonehooks/wmfkeystonehooks.py
@@ -220,22 +220,6 @@ class KeystoneHooks(notifier.Driver):
             try:
                 client.security_group_rules.create(groupid,
                                                    ip_protocol='tcp',
-                                                   from_port='5666',
-                                                   to_port='5666',
-                                                   cidr='10.0.0.0/8')
-            except (exceptions.ClientException):
-                LOG.warning("Port 5666 security rule already exists.")
-            try:
-                client.security_group_rules.create(groupid,
-                                                   ip_protocol='tcp',
-                                                   from_port='5666',
-                                                   to_port='5666',
-                                                   cidr='172.16.0.0/21')
-            except (exceptions.ClientException):
-                LOG.warning("Port 5666 security rule already exists.")
-            try:
-                client.security_group_rules.create(groupid,
-                                                   ip_protocol='tcp',
                                                    from_port='1',
                                                    to_port='65535',
                                                    cidr='',
