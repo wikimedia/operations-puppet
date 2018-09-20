1dd0075c117bfc73809f11fc3a6b62cc7ff1667c
Arzhel Younsi <ayounsi@wikimedia.org>
2018-09-19 17:35:56 -0700

    SNMP: set snmp-mibs-downloader BASEDIR to Debian 9 standard
    Debian 8 BASEDIR is /var/lib/mibs which is not recognized by snimpy
    This sets snmp-mibs-downloader's BASEDIR to /var/lib/snmp/mibs which is
    the default in Debian 9.
    
    See current situation on einsteinium (8) vs. icinga1001 (9)
    with /usr/lib/nagios/plugins/check_bfd.py
    
    Bug: T83992
    Change-Id: Ic9597ac4739d28c186757b0b65c94e2be073698b

--- modules/snmp/manifests/mibs.pp
+++ modules/snmp/manifests/mibs.pp
@@ -13,6 +13,14 @@ class snmp::mibs {
         ensure => present,
     }
 
+    file { '/etc/snmp-mibs-downloader/snmp-mibs-downloader.conf':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        source  => 'puppet:///modules/snmp/snmp-mibs-downloader.conf',
+        require => Package['snmp-mibs-downloader'],
+    }
+
     snmp::mibs::source { 'juniper':
         source  => 'https://www.juniper.net/documentation/software/junos/junos182/juniper-mibs-18.2R1.9.tgz',
         list    => 'puppet:///modules/snmp/juniper-mibs',
