1c6a225691c1df5fe7f4967c5399baf5e5097580
Michael Holloway <mholloway@wikimedia.org>
2018-09-26 16:01:43 +0500

    Fix: Regenerate map tiles up to zoom level 9 with notify-tilerator-regen
    In modules/tilerator/manifests/regen.pp, a cron job is defined to send a
    command to tilerator to regenerate map tiles for lower zoom levels.  It
    intends to regenerate zoom levels 0-9, but is sending '9' as the
    "beforeZoom" parameter.  "beforeZoom" is an exclusive upper bound, so
    currently only zoom levels 0-8 are being regenerated. This updates the
    command so that zoom level 9 is also regenerated.
    
    Bug: T202201
    Change-Id: Ib99cceca2e6e82052ef10d8682ccd3d33b0171de

--- modules/tilerator/manifests/regen.pp
+++ modules/tilerator/manifests/regen.pp
@@ -1,11 +1,40 @@
 # Class: tilerator::regen
+#
+# Provides for periodically regenerating low-zoom map tiles
+#
+# [*osmosis_dir*]
+#   directory in which osmosis keeps its state
+#
+# [*generator_id*]
+#   source to copy tiles from ("gen" will only produce non-empty tiles)
+#
+# [*storage_id*]
+#   source to copy tiles to
+#
+# [*delete_empty*]
+#   if tile is empty, make sure we don't store it, if it was there before
+#
+# [*expire_dir*]
+#   expire directory location
+#
+# [*statefile_dir*]
+#   directory containing the state file from OSM
+#
+# [*from_zoom*]
+#   new jobs will be created from this zoom
+#
+# [*before_zoom*]
+#   and until (but not including) this zoom
+#
 class tilerator::regen (
-    $osmosis_dir    = '/srv/osmosis',
-    $generator_id   = 'gen',
-    $storage_id     = 'v3',
-    $delete_empty   = true,
-    $expire_dir     = '/srv/osm_expire/',
-    $statefile_dir  = '/var/run/tileratorui',
+    Stdlib::Absolutepath $osmosis_dir   = '/srv/osmosis',
+    String $generator_id                = 'gen',
+    String $storage_id                  = 'v3',
+    Boolean $delete_empty               = true,
+    Stdlib::Absolutepath $expire_dir    = '/srv/osm_expire/',
+    Stdlib::Absolutepath $statefile_dir = '/var/run/tileratorui',
+    Integer[0, 19] $from_zoom           = 0,
+    Integer[0, 19] $before_zoom         = 10,
 ) {
 
     $tilerator_log_dir = '/var/log/tilerator/'
@@ -40,7 +69,7 @@ class tilerator::regen (
     }
 
     # Notify tilerator to regenerate zoom levels 0-9 monthly
-    $regen_options = "${osmosis_dir} 0 9 ${generator_id} ${storage_id} ${delete_empty} ${expire_dir} ${statefile}"
+    $regen_options = "${osmosis_dir} ${from_zoom} ${before_zoom} ${generator_id} ${storage_id} ${delete_empty} ${expire_dir} ${statefile}"
     cron { "regen-zoom-level-${title}":
         ensure   => present,
         command  => "/usr/local/bin/notify-tilerator-regen ${regen_options} >> ${tilerator_log_dir}/regen-zoom-level.log 2>&1",
