77439a73b11deb515997e4f8755a90a575f353aa
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-09-28 15:15:51 +0200

    prometheus-openstack-exporter: fix subvars usage
    Not the right syntax, use proper hiera keys for each.
    
    Bug: T203177
    Change-Id: I1f464c3b6059799ce9f37951b5f2a58d86893c80
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/openstack/eqiad1/metrics.pp
+++ modules/profile/manifests/openstack/eqiad1/metrics.pp
@@ -5,7 +5,9 @@ class profile::openstack::eqiad1::metrics(
     $listen_port = hiera('profile::openstack::eqiad1::metrics::prometheus_listen_port'),
     $cache_refresh_interval = hiera('profile::openstack::eqiad1::metrics::cache_refresh_interval'),
     $cache_file = hiera('profile::openstack::eqiad1::metrics::cache_file'),
-    $schedulable_instance_size = hiera('profile::openstack::eqiad1::metrics::schedulable_instance_size'),
+    $sched_ram_mbs = hiera('profile::openstack::eqiad1::metrics::sched_ram_mbs'),
+    $sched_vcpu = hiera('profile::openstack::eqiad1::metrics::sched_vcpu'),
+    $sched_disk_gbs = hiera('profile::openstack::eqiad1::metrics::sched_disk_gbs'),
     $region = hiera('profile::openstack::eqiad1::region'),
     $keystone_host = hiera('profile::openstack::eqiad1::keystone_host'),
     $observer_password = hiera('profile::openstack::eqiad1::observer_password'),
@@ -13,16 +15,18 @@ class profile::openstack::eqiad1::metrics(
 
     require ::profile::openstack::eqiad1::observerenv
     class {'::profile::openstack::base::metrics':
-        cpu_allocation_ratio      => $cpu_allocation_ratio,
-        ram_allocation_ratio      => $ram_allocation_ratio,
-        disk_allocation_ratio     => $disk_allocation_ratio,
-        listen_port               => $listen_port,
-        cache_refresh_interval    => $cache_refresh_interval,
-        cache_file                => $cache_file,
-        schedulable_instance_size => $schedulable_instance_size,
-        region                    => $region,
-        keystone_host             => $keystone_host,
-        observer_password         => $observer_password,
+        cpu_allocation_ratio   => $cpu_allocation_ratio,
+        ram_allocation_ratio   => $ram_allocation_ratio,
+        disk_allocation_ratio  => $disk_allocation_ratio,
+        listen_port            => $listen_port,
+        cache_refresh_interval => $cache_refresh_interval,
+        cache_file             => $cache_file,
+        sched_ram_mbs          => $sched_ram_mbs,
+        sched_vcpu             => $sched_vcpu,
+        sched_disk_gbs         => $sched_disk_gbs,
+        region                 => $region,
+        keystone_host          => $keystone_host,
+        observer_password      => $observer_password,
     }
     contain '::profile::openstack::base::metrics'
 }
