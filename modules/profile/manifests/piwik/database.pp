7a5da726233a1eec9a470d989b2ab381d4a0f5b2
Luca Toscano <ltoscano@wikimedia.org>
2018-10-01 17:16:59 +0200

    profile::piwik: move to mariadb
    Bug: T202962
    Change-Id: I960120dbedd95227f99bb181bf1931e2af5e1c6b

--- modules/profile/manifests/piwik/database.pp
+++ modules/profile/manifests/piwik/database.pp
@@ -7,15 +7,45 @@
 #
 class profile::piwik::database {
 
-    require_package('mysql-server')
+    # This is a hacky temporary measure
+    # to allow to configure a proper mariadb instance
+    # on the new Matomo/Piwik host keeping the other
+    # unchanged.
+    # Bug: T202962
+    if os_version('debian == jessie') {
+        require_package('mysql-server')
 
-    file { '/etc/mysql/my.cnf':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        source  => 'puppet:///modules/profile/piwik/my.cnf',
-        require => Package['mysql-server'],
+        file { '/etc/mysql/my.cnf':
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            source  => 'puppet:///modules/profile/piwik/my.cnf',
+            require => Package['mysql-server'],
+        }
+    } else {
+
+        package { 'mysql-server':
+            ensure => absent,
+        }
+
+        class { '::mariadb::packages_wmf': }
+
+        class { '::mariadb::config':
+            config    => 'profile/piwik/my.cnf.erb',
+            socket    => '/run/mysqld/mysqld.sock',
+            port      => 3306,
+            datadir   => '/var/lib/mysql',
+            basedir   => '/opt/wmf-mariadb101',
+            read_only => false,
+            require   => Class['mariadb::packages_wmf'],
+        }
+
+        class { '::mariadb::service':
+            ensure  => 'running',
+            manage  => true,
+            enable  => true,
+            require => Class['mariadb::config'],
+        }
     }
-
 }
