e26579ff3c4abedd2beb9d329d48b79f755d6406
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-10-01 09:54:40 +0200

    parsoid: fixup for the https uri
    Change-Id: Id1237eb827b7890e31477ac40b02efe595db4855

--- modules/profile/manifests/parsoid.pp
+++ modules/profile/manifests/parsoid.pp
@@ -7,9 +7,10 @@ class profile::parsoid(
 
     class { '::service::configuration': }
 
+    $mwapi_server = "${::service::configuration::mwapi_host}/w/api.php"
     class { '::parsoid':
         port         => 8000,
-        mwapi_server => $::service::configuration::mwapi_uri,
+        mwapi_server => $mwapi_server,
         mwapi_proxy  => ''
     }
 
