6921e044c9ea2cd8b4443264e966b5b0473cbd59
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-10-08 15:35:34 +0200

    site: enable logging Kafka on Logstash nodes
    Bug: T206454
    Change-Id: I45628f96e5c7647be9bf86fb0a20d3a7b3e4fdea

--- modules/role/manifests/logstash/elasticsearch.pp
+++ modules/role/manifests/logstash/elasticsearch.pp
@@ -7,4 +7,8 @@ class role::logstash::elasticsearch {
     include ::standard
     include ::profile::base::firewall
     include ::profile::elasticsearch::logstash
+
+    # Co-locate kafka with elasticsearch until dedicated hardware is online
+    # See also T206454
+    include ::role::kafka::logging
 }
