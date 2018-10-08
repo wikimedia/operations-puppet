7f5dfa0f3d3f501590a21cecaadcfd4c0958f4b6
joal <joal@wikimedia.org>
2018-10-08 09:57:59 +0200

    Add http_proxy to profile::analytics::refinery::job::project_namespace_map
    Change-Id: Ie9c012b462103f3e8fb7aee5e15031946fb6de06

--- modules/profile/manifests/analytics/refinery/job/project_namespace_map.pp
+++ modules/profile/manifests/analytics/refinery/job/project_namespace_map.pp
@@ -3,7 +3,9 @@
 # namespace map file so that other refinery jobs know about what wiki projects
 # exist.
 #
-class profile::analytics::refinery::job::project_namespace_map {
+class profile::analytics::refinery::job::project_namespace_map(
+    $http_proxy = hiera('profile::analytics::refinery::job::project_namespace_map::http_proxy', undef)
+  ) {
     require ::profile::analytics::refinery
 
     # Shortcut var to DRY up cron commands.
@@ -12,9 +14,15 @@ class profile::analytics::refinery::job::project_namespace_map {
     $output_directory = '/wmf/data/raw/mediawiki/project_namespace_map'
     $log_file         = "${::profile::analytics::refinery::log_dir}/download-project-namespace-map.log"
 
+    if $http_proxy {
+        $http_proxy_option = "-p ${http_proxy}"
+    } else {
+        $http_proxy_option = ''
+    }
+
     # This downloads the project namespace map for a 'labsdb' public import.
     cron { 'refinery-download-project-namespace-map':
-        command  => "${env} && ${profile::analytics::refinery::path}/bin/download-project-namespace-map -x ${output_directory} -s \$(/bin/date --date=\"$(/bin/date +\\%Y-\\%m-15) -1 month\" +'\\%Y-\\%m') >> ${log_file} 2>&1 ",
+        command  => "${env} && ${profile::analytics::refinery::path}/bin/download-project-namespace-map -x ${output_directory} -s \$(/bin/date --date=\"$(/bin/date +\\%Y-\\%m-15) -1 month\" +'\\%Y-\\%m') ${http_proxy_option} >> ${log_file} 2>&1 ",
         user     => 'hdfs',
         minute   => '0',
         hour     => '0',
