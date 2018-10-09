007ee1bb808650304d97cc7a7d7b2b8bd6acd904
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-10-09 14:16:55 +0200

    scap: define MW canaries dynamically
    While for other dsh groups it's ok to just extract data from conftool,
    in the case of canaries we only want to check the ones in the active
    datacenter, as they're the only ones that get real traffic and can
    give us information about the error rates.
    
    Change-Id: I33e8330d371eaa0c86db4e1a93d75bde6547975a

--- modules/profile/manifests/scap/dsh.pp
+++ modules/profile/manifests/scap/dsh.pp
@@ -17,4 +17,25 @@ class profile::scap::dsh(
         scap_proxies => $proxies,
         scap_masters => $masters,
     }
+
+    # Special-case file for the MediaWiki canaries
+    # These need to change according to the MediaWiki active datacenter.
+    # We also want the servers from each cluster in their own canary list.
+    $canary_dcs = ['eqiad', 'codfw']
+    $canary_clusters = ['appserver', 'api_appserver', 'jobrunner', 'parsoid']
+
+    $canary_clusters.each |$cl| {
+        # Cosmetic fix to get the same filenames as before
+        $dsh_name = $cl ? {
+            'api_appserver' => 'api',
+            default => $cl
+        }
+        # We also need mediawiki-config to get the active DC.
+        $keys = $canary_dcs.map |$dc| { "/pool/${dc}/${cl}/canary" } + '/mediawiki-config'
+        confd::file { "/etc/dsh/group/mediawiki-${dsh_name}-canaries":
+            ensure     => present,
+            content    => template('profile/scap/dsh-mediawiki-canaries.tpl.erb'),
+            watch_keys => $keys,
+        }
+    }
 }
