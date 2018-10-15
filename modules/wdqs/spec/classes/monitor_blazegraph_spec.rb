ec2bf6a048c6d80981d35f4dc0ae891418fb4786
Mathew Onipe <monipe@wikimedia.org>
2018-10-15 16:20:45 +0100

    wdqs: cleanup rspec test
    Bug: T204240
    Change-Id: I777896d968a4f5564b7a1f8fc8a8fb6f4a190462

--- modules/wdqs/spec/classes/monitor_blazegraph_spec.rb
+++ modules/wdqs/spec/classes/monitor_blazegraph_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
-describe 'wdqs::monitor::blazegraph', :type => :class do
+describe 'icinga::monitor::wdqs', :type => :class do
   let(:facts) { { :lsbdistrelease => 'debian',
                   :lsbdistid      => 'jessie',
                   :initsystem     => 'systemd',
-                  :fqdn           => 'my.example.net',
   } }
 
-  it { is_expected.to contain_monitoring__graphite_threshold('wdqs-response-time')
-                          .with_metric('varnish.eqiad.backends.be_my_example_net.GET.p99')
+  it { is_expected.to contain_monitoring__graphite_threshold('wdqs-response-time-eqiad')
+                          .with_metric('varnish.eqiad.backends.be_wdqs_svc_eqiad_wmnet.GET.p99')
   }
 end
--- modules/wdqs/spec/classes/wdqs_service_spec.rb
+++ modules/wdqs/spec/classes/wdqs_service_spec.rb
@@ -1,19 +1,32 @@
 require 'spec_helper'
 
 describe 'wdqs::service', :type => :class do
-  context 'with systemd' do
-    let(:facts) { { :initsystem => 'systemd' } }
+   before(:each) do
+        Puppet::Parser::Functions.newfunction(:secret, :type => :rvalue) { |_|
+            'fake_secret'
+        }
+   end
+
+   let(:params) { {
+        :deploy_user => 'deploy-service',
+        :deploy_mode => 'scap3',
+        :package_dir => '/srv/deployment/wdqs/wdqs',
+        :username => 'blazegraph',
+        :config_file => 'RWStore.properties',
+        :logstash_host => 'localhost',
+        :logstash_json_port => 115_14,
+        }
+   }
+
+   context 'with systemd' do
+    let(:facts) { {
+        :initsystem => 'systemd',
+        :lsbdistrelease => '8.7',
+        :lsbdistid => 'Debian',
+    } }
 
     it { is_expected.to contain_file('/lib/systemd/system/wdqs-blazegraph.service')
       .with_content(/runBlazegraph.sh -f RWStore.properties/)
-    }
-  end
-
-  context 'with upstart' do
-    let(:facts) { { :initsystem => 'upstart' } }
-
-    it { is_expected.to contain_file('/etc/init/wdqs-blazegraph.conf')
-                            .with_content(/runBlazegraph.sh -f RWStore.properties/)
     }
   end
 end
