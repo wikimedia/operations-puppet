cf2c527840b720a8b788ae085f8c79329ea2247c
Luca Toscano <ltoscano@wikimedia.org>
2018-10-15 12:09:18 +0200

    profile::analytics::refinery::job::data_purge: move crons to timers
    Bug: T172532
    Change-Id: I8528b190774922165971aee9a1fbc0a3e0fdc953

--- modules/profile/manifests/analytics/refinery/job/data_purge.pp
+++ modules/profile/manifests/analytics/refinery/job/data_purge.pp
@@ -61,43 +61,89 @@ class profile::analytics::refinery::job::data_purge (
         # Keep this many days of refined webrequest data.
         $refined_retention_days = 90
         cron { 'refinery-drop-webrequest-refined-partitions':
+            ensure  => absent,
             command => "${env} && ${refinery_path}/bin/refinery-drop-webrequest-partitions -d ${refined_retention_days} -D wmf -l /wmf/data/wmf/webrequest -w refined >> ${webrequest_log_file} 2>&1",
             user    => 'hdfs',
             minute  => '45',
             hour    => '*/4',
         }
 
+        profile::analytics::systemd_timer { 'refinery-drop-webrequest-refined-partitions':
+            description => 'Drop Webrequest refined data imported on HDFS following data retention policies.',
+            command     => "${refinery_path}/bin/refinery-drop-webrequest-partitions -d ${refined_retention_days} -D wmf -l /wmf/data/wmf/webrequest -w refined",
+            interval    => '*-*-* 00/4:45:00',
+            environment => $systemd_env,
+            user        => 'hdfs',
+        }
+
         # Keep this many days of eventlogging data.
         $eventlogging_retention_days = 90
         cron {'refinery-drop-eventlogging-partitions':
+            ensure  => absent,
             command => "${env} && ${refinery_path}/bin/refinery-drop-eventlogging-partitions -d ${eventlogging_retention_days} -l /wmf/data/raw/eventlogging >> ${eventlogging_log_file} 2>&1",
             user    => 'hdfs',
             minute  => '15',
             hour    => '*/4',
         }
 
+        profile::analytics::systemd_timer { 'refinery-drop-eventlogging-partitions':
+            description => 'Drop Eventlogging data imported on HDFS following data retention policies.',
+            command     => "${refinery_path}/bin/refinery-drop-eventlogging-partitions -d ${eventlogging_retention_days} -l /wmf/data/raw/eventlogging",
+            interval    => '*-*-* 00/4:15:00',
+            environment => $systemd_env,
+            user        => 'hdfs',
+        }
+
         # keep this many days of wdqs_extract data
         $wdqs_extract_retention_days = 90
         cron {'refinery-drop-wdqs-extract-partitions':
+            ensure  => absent,
             command => "${env} && ${refinery_path}/bin/refinery-drop-hourly-partitions -d ${wdqs_extract_retention_days} -p hive -D wmf -t wdqs_extract -l /wmf/data/wmf/wdqs_extract >> ${wdqs_extract_log_file} 2>&1",
             user    => 'hdfs',
             minute  => '0',
             hour    => '1',
         }
 
+        profile::analytics::systemd_timer { 'refinery-drop-wdqs-extract-partitions':
+            description => 'Drop WDQS data imported on HDFS following data retention policies.',
+            command     => "${refinery_path}/bin/refinery-drop-hourly-partitions -d ${wdqs_extract_retention_days} -p hive -D wmf -t wdqs_extract -l /wmf/data/wmf/wdqs_extract",
+            interval    => '*-*-* 01:00:00',
+            environment => $systemd_env,
+            user        => 'hdfs',
+        }
+
         # keep this many days of mediawiki application logs
         $mediawiki_log_retention_days = 90
         cron {'refinery-drop-apiaction-partitions':
+            ensure  => absent,
             command => "export PYTHONPATH=\${PYTHONPATH}:${refinery_path}/python && ${refinery_path}/bin/refinery-drop-hourly-partitions -d ${mediawiki_log_retention_days} -D wmf_raw -t apiaction -p camus -l /wmf/data/raw/mediawiki/mediawiki_ApiAction/hourly >> ${mediawiki_log_file} 2>&1",
             user    => 'hdfs',
             minute  => '15',
             hour    => '*/4',
         }
+
+        profile::analytics::systemd_timer { 'refinery-drop-apiaction-partitions':
+            description => 'Drop API action data imported on HDFS following data retention policies.',
+            command     => "${refinery_path}/bin/refinery-drop-hourly-partitions -d ${mediawiki_log_retention_days} -D wmf_raw -t apiaction -p camus -l /wmf/data/raw/mediawiki/mediawiki_ApiAction/hourly",
+            interval    => '*-*-* 00/4:15:00',
+            environment => $systemd_env,
+            user        => 'hdfs',
+        }
+
         cron {'refinery-drop-cirrussearchrequestset-partitions':
+            ensure  => absent,
             command => "export PYTHONPATH=\${PYTHONPATH}:${refinery_path}/python && ${refinery_path}/bin/refinery-drop-hourly-partitions -d ${mediawiki_log_retention_days} -D wmf_raw -t cirrussearchrequestset -p camus -l /wmf/data/raw/mediawiki/mediawiki_CirrusSearchRequestSet/hourly >> ${mediawiki_log_file} 2>&1",
             user    => 'hdfs',
             minute  => '25',
             hour    => '*/4',
+        }
+
+        profile::analytics::systemd_timer { 'refinery-drop-cirrussearchrequestset-partitions':
+            description => 'Drop CirrusSearch request data imported on HDFS following data retention policies.',
+            command     => "${refinery_path}/bin/refinery-drop-hourly-partitions -d ${mediawiki_log_retention_days} -D wmf_raw -t cirrussearchrequestset -p camus -l /wmf/data/raw/mediawiki/mediawiki_CirrusSearchRequestSet/hourly",
+            interval    => '*-*-* 00/4:25:00',
+            environment => $systemd_env,
+            user        => 'hdfs',
         }
 
         # keep this many days of druid webrequest sampled
@@ -113,7 +159,7 @@ class profile::analytics::refinery::job::data_purge (
         }
 
         profile::analytics::systemd_timer { 'refinery-drop-webrequest-sampled-druid':
-            description => 'Drop Druid Webrequest sampled data from deep storage',
+            description => 'Drop Druid Webrequest sampled data from deep storage following data retention policies.',
             command     => "${refinery_path}/bin/refinery-drop-druid-deep-storage-data -d ${druid_webrequest_sampled_retention_days} webrequest_sampled_128",
             interval    => '*-*-* 05:15:00',
             environment => $systemd_env,
@@ -127,6 +173,7 @@ class profile::analytics::refinery::job::data_purge (
             $druid_public_keep_snapshots = 4
             $mediawiki_history_reduced_basename = 'mediawiki_history_reduced'
             cron {'refinery-druid-drop-public-snapshots':
+                ensure      => absent,
                 command     => "${env} && ${refinery_path}/bin/refinery-drop-druid-snapshots -d ${mediawiki_history_reduced_basename} -t ${public_druid_host} -s ${druid_public_keep_snapshots} -f ${public_druid_snapshots_log_file}",
                 environment => "MAILTO=${mail_to}",
                 user        => 'hdfs',
@@ -134,12 +181,21 @@ class profile::analytics::refinery::job::data_purge (
                 hour        => '7',
                 monthday    => '15'
             }
+
+            profile::analytics::systemd_timer { 'refinery-druid-drop-public-snapshots':
+                description => 'Drop Druid Public snapshots from deep storage following data retention policies.',
+                command     => "${refinery_path}/bin/refinery-drop-druid-snapshots -d ${mediawiki_history_reduced_basename} -t ${public_druid_host} -s ${druid_public_keep_snapshots} -f ${public_druid_snapshots_log_file}",
+                environment => $systemd_env,
+                interval    => '*-*-15 07:00:00',
+                user        => 'hdfs',
+            }
         }
 
         # keep this many mediawiki history snapshots, 6 minimum
         # cron runs once a month
         $keep_snapshots = 6
         cron {'mediawiki-history-drop-snapshot':
+            ensure      => absent,
             command     => "${env} && ${refinery_path}/bin/refinery-drop-mediawiki-snapshots -s ${keep_snapshots} >> ${mediawiki_history_log_file}",
             environment => "MAILTO=${mail_to}",
             user        => 'hdfs',
@@ -148,10 +204,19 @@ class profile::analytics::refinery::job::data_purge (
             monthday    => '15'
         }
 
+        profile::analytics::systemd_timer { 'mediawiki-history-drop-snapshot':
+            description => 'Drop Druid Mediawiki History snapshots from deep storage following data retention policies.',
+            command     => "${refinery_path}/bin/refinery-drop-mediawiki-snapshots -s ${keep_snapshots}",
+            environment => $systemd_env,
+            interval    => '*-*-15 06:15:00',
+            user        => 'hdfs',
+        }
+
         # keep this many days of banner activity success files
         # cron runs once a day
         $banner_activity_retention_days = 90
         cron {'refinery-drop-banner-activity':
+            ensure      => absent,
             command     => "${env} && ${refinery_path}/bin/refinery-drop-banner-activity-partitions -d ${banner_activity_retention_days} -l /wmf/data/wmf/banner_activity >> ${banner_activity_log_file}",
             environment => "MAILTO=${mail_to}",
             user        => 'hdfs',
@@ -159,16 +224,33 @@ class profile::analytics::refinery::job::data_purge (
             hour        => '2',
         }
 
+        profile::analytics::systemd_timer { 'refinery-drop-banner-activity':
+            description => 'Drop Druid Banner Activity from deep storage following data retention policies.',
+            command     => "${refinery_path}/bin/refinery-drop-banner-activity-partitions -d ${banner_activity_retention_days} -l /wmf/data/wmf/banner_activity",
+            environment => $systemd_env,
+            interval    => '*-*-* 02:00:00',
+            user        => 'hdfs',
+        }
+
         # create and rotate cryptographic salts for EventLogging sanitization
         # cron runs once a day, at midnight, to coincide with salt rotation time
         # given that hdfs stores modified dates without milliseconds
         # 1 minute margin is given to avoid timestamp comparison problems
         cron {'refinery-eventlogging-saltrotate':
+            ensure      => absent,
             command     => "${env} && ${refinery_path}/bin/saltrotate --verbose -p '3 months' -b '14 days' /user/hdfs/eventlogging-sanitization-salt.txt >> ${$el_saltrotate_log_file}",
             environment => "MAILTO=${mail_to}",
             user        => 'hdfs',
             minute      => '1',
             hour        => '0',
+        }
+
+        profile::analytics::systemd_timer { 'refinery-eventlogging-saltrotate':
+            description => 'Create and rotate cryptographic salts for EventLogging sanitization.',
+            command     => "${refinery_path}/bin/saltrotate --verbose -p '3 months' -b '14 days' /user/hdfs/eventlogging-sanitization-salt.txt",
+            environment => $systemd_env,
+            interval    => '*-*-* 01:00:00',
+            user        => 'hdfs',
         }
 
         # Sanitize event database into event_sanitized.
@@ -199,6 +281,7 @@ class profile::analytics::refinery::job::data_purge (
         # cron runs once a month
         $geoeditors_private_retention_days = 80
         cron {'mediawiki-raw-cu-changes-drop-month':
+            ensure      => absent,
             command     => "${env} && ${refinery_path}/bin/refinery-drop-hive-partitions -d ${geoeditors_private_retention_days} -D wmf_raw -t mediawiki_private_cu_changes -l 1 -f ${mediawiki_private_log_file}",
             environment => "MAILTO=${mail_to}",
             user        => 'hdfs',
@@ -207,15 +290,32 @@ class profile::analytics::refinery::job::data_purge (
             monthday    => '16'
         }
 
+        profile::analytics::systemd_timer { 'mediawiki-raw-cu-changes-drop-month':
+            description => 'Drop raw Mediawiki cu_changes from Hive/HDFS following data retention policies.',
+            command     => "${refinery_path}/bin/refinery-drop-hive-partitions -d ${geoeditors_private_retention_days} -D wmf_raw -t mediawiki_private_cu_changes -l 1 -f ${mediawiki_private_log_file}",
+            environment => $systemd_env,
+            interval    => '*-*-16 05:00:00',
+            user        => 'hdfs',
+        }
+
         # drop data older than 2 months from geoeditors_daily table
         # cron runs once a month
         cron {'mediawiki-geoeditors-drop-month':
+            ensure      => absent,
             command     => "${env} && ${refinery_path}/bin/refinery-drop-hive-partitions -d ${geoeditors_private_retention_days} -D wmf -t geoeditors_daily -l 1 -f ${geoeditors_log_file}",
             environment => "MAILTO=${mail_to}",
             user        => 'hdfs',
             minute      => '0',
             hour        => '6',
             monthday    => '16'
+        }
+
+        profile::analytics::systemd_timer { 'mediawiki-geoeditors-drop-month':
+            description => 'Drop Geo-editors data from Hive/HDFS following data retention policies.',
+            command     => "${refinery_path}/bin/refinery-drop-hive-partitions -d ${geoeditors_private_retention_days} -D wmf -t geoeditors_daily -l 1 -f ${geoeditors_log_file}",
+            environment => $systemd_env,
+            interval    => '*-*-16 06:00:00',
+            user        => 'hdfs',
         }
 
         # keep this many days of search query click files
