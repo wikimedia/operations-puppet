cdc07f447d26939f64d48ab8e9a12127d3b2512c
Luca Toscano <ltoscano@wikimedia.org>
2018-10-16 10:03:21 +0200

    profile::memcached::instance: set the -R arg via hiera
    Bug: T203786
    Change-Id: I438f935dae209df6c9b326328a9bbe9825986e2b

--- modules/profile/manifests/memcached/instance.pp
+++ modules/profile/manifests/memcached/instance.pp
@@ -20,24 +20,39 @@
 # [*size*]
 #   Memcached max memory allocated size.
 #
+# [*max_seq_reqs*]
+#   Maximum number of sequential requests (over the same TCP conn)
+#   that memcached will process before yielding to another connection
+#   (to avoid starving clients). Sets the '-R' option in memcached.
+#   Default: undef (memcached's default is 20)
+#
 class profile::memcached::instance (
     $growth_factor    = hiera('profile::memcached::growth_factor'),
     $extended_options = hiera_array('profile::memcached::extended_options'),
     $version          = hiera('profile::memcached::version'),
     $port             = hiera('profile::memcached::port'),
     $size             = hiera('profile::memcached::size'),
+    $max_seq_reqs     = hiera('profile::memcached::max_seq_reqs', undef),
 ) {
     include ::profile::prometheus::memcached_exporter
+
+    $base_extra_options = {
+        '-o' => join($extended_options, ','),
+        '-D' => ':',
+    }
+
+    if $max_seq_reqs {
+        $extra_options = merge($base_extra_options, {'-R' => $max_seq_reqs})
+    } else {
+        $extra_options = $base_extra_options
+    }
 
     class { '::memcached':
         size          => $size,
         port          => $port,
         version       => $version,
         growth_factor => $growth_factor,
-        extra_options => {
-            '-o' => join($extended_options, ','),
-            '-D' => ':',
-        }
+        extra_options => $extra_options,
     }
 
     ferm::service { 'memcached':
