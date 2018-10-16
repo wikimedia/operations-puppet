997c0874cc0af39ea0a9a169f64644f6a6df92c5
Marko Obrovac <mobrovac@wikimedia.org>
2018-10-16 11:13:57 +0100

    Recommendation API: Add MySQL connection config
    The Recommendation API service has been set up with its own MySQL DB on
    m2. In order to access it, we need to propagate the connection info to
    its config on the SCB nodes.
    
    Bug: T205452
    Change-Id: Ie2052129a05b2b3324d8fa78d5a45a078582246f

--- modules/profile/manifests/recommendation_api.pp
+++ modules/profile/manifests/recommendation_api.pp
@@ -1,5 +1,13 @@
 # Profile class for recommendation_api
-class profile::recommendation_api($wdqs_uri=hiera('profile::recommendation_api::wdqs_uri')) {
+class profile::recommendation_api(
+    String $wdqs_uri = hiera('profile::recommendation_api::wdqs_uri'),
+    String $dbhost = hiera('profile::recommendation_api::dbhost'),
+    String $dbname = hiera('profile::recommendation_api::dbname'),
+    String $dbuser = hiera('profile::recommendation_api::dbuser'),
+) {
+
+    include passwords::recommendationapi::mysql
+    $pwd = $::passwords::recommendationapi::mysql::recommendationapiservice_pass
 
     service::node { 'recommendation_api':
         port              => 9632,
@@ -10,6 +18,10 @@ class profile::recommendation_api($wdqs_uri=hiera('profile::recommendation_api::
         deployment_config => true,
         deployment_vars   => {
             wdqs_uri => $wdqs_uri,
+            dbhost   => $dbhost,
+            dbname   => $dbname,
+            dbuser   => $dbuser,
+            dbpass   => $pwd,
         },
     }
 }
