02549d4160c53636b3440b926a231ca619cae26e
bstorm <bstorm@wikimedia.org>
2018-10-23 10:06:23 -0500

    sonofgridengine: move the filetype definition outside the provider def
    Bug: T200557
    Change-Id: If13f7e3630ab9473bd643c14969b71972d7da6da

--- modules/sonofgridengine/lib/puppet/provider/gridengine_complex/parsed.rb
+++ modules/sonofgridengine/lib/puppet/provider/gridengine_complex/parsed.rb
@@ -16,29 +16,28 @@ require 'puppet/provider/parsedfile'
 require 'puppet/util/execution'
 require 'puppet/util/filetype'
 
+Puppet::Util::FileType.newfiletype(:gridengine_complex) do
+  # TODO: target/default_target should be used to point to a
+  #       specific gridengine instance and default to, well, the
+  #       default.
+
+  def read
+    `qconf -sc`
+  end
+
+  def write(text)
+    Tempfile.open('gridengine_complex') do |tmpfile|
+      tmpfile.write(text)
+      tmpfile.close
+      Puppet::Util::Execution.execute(['qconf', '-Mc', tmpfile.path])
+    end
+  end
+end
+
 Puppet::Type.type(:gridengine_complex).provide(:parsed, :parent => Puppet::Provider::ParsedFile, :default_target => 'default') do
   desc 'Provider for gridengine_complex.'
 
   commands :qconf => 'qconf'
-
-  # Handle gridengine configuration for complex values.
-  Puppet::Util::FileType.newfiletype(:gridengine_complex) do
-    # TODO: target/default_target should be used to point to a
-    #       specific gridengine instance and default to, well, the
-    #       default.
-
-    def read
-      `qconf -sc`
-    end
-
-    def write(text)
-      Tempfile.open('gridengine_complex') do |tmpfile|
-        tmpfile.write(text)
-        tmpfile.close
-        Puppet::Util::Execution.execute(['qconf', '-Mc', tmpfile.path])
-      end
-    end
-  end
 
   def self.filetype
     Puppet::Util::FileType.filetype(:gridengine_complex)
