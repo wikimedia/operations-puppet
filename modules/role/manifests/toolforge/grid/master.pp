b854cdcedcc935034fca71f7bee4305b9b83ba32
bstorm <bstorm@wikimedia.org>
2018-10-25 12:22:30 -0500

    sonofgridengine: clean up the old roles after moving under wmcs
    Change-Id: I1c52ccc80995afee82f6849f29867d3fc040135d

--- modules/role/manifests/toolforge/grid/master.pp
+++
@@ -1,6 +0,0 @@
-class role::toolforge::grid::master {
-    system::role { 'toolforge::grid::master': description => 'Toolforge gridengine master' }
-
-    include profile::toolforge::grid::base
-    include profile::toolforge::grid::master
-}
