6b54f229148695208a5d1bb9876fbef66dd01411
Marko Obrovac <mobrovac@wikimedia.org>
2018-10-25 15:24:20 -0700

    service::node: Set config-vars.yaml's mode to 0440
    Config file fragments distributed by Puppet may contain passwords, so
    make sure they are not world-readable. Also set show_diff => false for
    the same reason.
    
    Bug: T207143
    Change-Id: I665f6a4536dac056cae20d1b0effb0f0f8bd1d3e

--- modules/service/manifests/node/config/scap3.pp
+++ modules/service/manifests/node/config/scap3.pp
@@ -25,6 +25,11 @@ define service::node::config::scap3 (
     # the local log file name
     $local_logdir = "${service::configuration::log_dir}/${title}"
     $local_logfile = "${local_logdir}/main.log"
+    # file mode for config-vars.yaml
+    $mode = $deployment_user ? {
+        'root'  => '0444',
+        default => '0440',
+    }
     # We need to ensure that the full config gets deployed when we change the
     # puppet controlled part. If auto_refresh is true, this will also restart
     # the service.
@@ -36,14 +41,14 @@ define service::node::config::scap3 (
         mode    => '0755',
     }
 
-
     file { "/etc/${title}/config-vars.yaml":
-        ensure  => present,
-        content => template('service/node/config-vars.yaml.erb'),
-        owner   => $deployment_user,
-        group   => $deployment_user,
-        mode    => '0444',
-        tag     => "${title}::config",
+        ensure    => present,
+        content   => template('service/node/config-vars.yaml.erb'),
+        owner     => $deployment_user,
+        group     => $deployment_user,
+        mode      => $mode,
+        tag       => "${title}::config",
+        show_diff => false,
     }
     exec { "${title} config deploy":
         command     => "/usr/local/bin/apply-config-${title}",
