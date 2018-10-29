ecdee5e7d5ac93af0a86810a9d5b307d368b651c
bstorm <bstorm@wikimedia.org>
2018-10-29 10:16:23 -0500

    sonofgridengine: try using the gridengine_queue puppet type
    It is entirely possible this will fail.  The only sure way to tell is to
    run it in toolsbeta.
    
    Bug: T200557
    Change-Id: Ibc079e8a1581fa36ed369a73afc4b966cbbbbcd3

--- modules/profile/manifests/toolforge/grid/node/compute/general.pp
+++ modules/profile/manifests/toolforge/grid/node/compute/general.pp
@@ -22,6 +22,22 @@ class profile::toolforge::grid::node::compute::general {
 
     $hostlist = '@general'
 
+    sonofgridengine::gridengine_queue { 'continuous':
+        hostlist  => $hostlist,
+        seq_no    => 1,
+        priority  => 10,
+        qtype     => 'BATCH',
+        ckpt_list => 'continuous',
+        rerun     => 'TRUE',
+    }
+
+    sonofgridengine::gridengine_queue { 'task':
+        hostlist  => $hostlist,
+        seq_no    => 0,
+        qtype     => 'BATCH INTERACTIVE',
+        ckpt_list => 'NONE',
+    }
+
     sonofgridengine::queue { 'continuous':
         config => 'profile/toolforge/grid/queue-continuous.erb',
     }
