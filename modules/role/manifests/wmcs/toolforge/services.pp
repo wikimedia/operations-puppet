119a3d2985028ce8a624038874cf29062d79434e
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-10-29 15:07:21 +0100

    toolforge: add missing grid base profile to services role
    In the previous patch I61557b8de2db33f4de1957cc2cefb2e71459615f we skipped
    this grid base profile, and we need it.
    
    Bug: T207591
    Change-Id: I1317116fa71ccd0fc765e98d4e06361734b58c53
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/role/manifests/wmcs/toolforge/services.pp
+++ modules/role/manifests/wmcs/toolforge/services.pp
@@ -3,6 +3,7 @@ class role::wmcs::toolforge::services {
     # not sure about the next two
     #include ::standard
     #include ::profile::base::firewall
+    include ::profile::toolforge::grid::base
     include ::profile::toolforge::services::basic
     include ::profile::toolforge::services::aptly
     include ::profile::toolforge::services::bigbrother
