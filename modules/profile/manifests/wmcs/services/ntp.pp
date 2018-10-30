5941504bade3c2c3f07e86578b78ec8c4c036cb4
andrewbogott <abogott@wikimedia.org>
2018-10-30 09:27:23 -0500

    Add role/profile for a set of in-cloud ntp servers
    This is an extremely simplified (and sightly hiera-enabled)
    copy of profile::ntp.
    
    Bug: T208244
    Change-Id: I88e5f2c7ddffef71b9f5b211294910b3316a7d1e

---
+++ modules/profile/manifests/wmcs/services/ntp.pp
@@ -0,0 +1,28 @@
+# == Class profile::wmcs::services::ntp
+#
+# Ntp server profile
+class profile::wmcs::services::ntp(
+    $server_peers = hiera('profile::wmcs::services::server_peers'),
+    $networks_acl = hiera('profile::wmcs::services::wmcs_networks_acl',
+        [ '172.16.0.0/21 mask 255.255.248.0' ]),
+) {
+
+    $server_upstream_pools = ['0.us.pool.ntp.org']
+
+    # Keep syncing even if our peer doesn't respond
+    $extra_config = 'tos orphan 12'
+
+    $query_acl = []
+    $server_upstreams = []
+
+    ntp::daemon { 'server':
+        servers      => $server_upstreams,
+        pools        => $server_upstream_pools,
+        peers        => $server_peers,
+        time_acl     => $networks_acl,
+        extra_config => $extra_config,
+        query_acl    => $query_acl,
+    }
+
+    # FIXME: add monitoring once we decide on a wmcs/services monitoring system
+}
---
+++ modules/role/manifests/wmcs/services/ntp.pp
@@ -0,0 +1,10 @@
+# == Class role::wmcs::services::ntp
+#
+# Ntp server role, to be applied to a cloud instance
+class role::wmcs::services::ntp {
+
+    system::role { 'ntp': description => 'NTP server for WMCS' }
+
+    include ::standard::ntp
+    include ::profile::wmcs::services::ntp
+}
