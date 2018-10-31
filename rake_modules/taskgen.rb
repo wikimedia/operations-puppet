e8ce67404da654b81c62fc4ed4e042ef79c0f52a
Alex Monk <krenair@gmail.com>
2018-10-31 17:12:37 +0000

    tests: Also check templates/*.erb
    See parent commit
    
    Change-Id: Ifc905bc01faff836e43066ac4fb85d6c7111397e

--- rake_modules/taskgen.rb
+++ rake_modules/taskgen.rb
@@ -224,7 +224,7 @@ class TaskGen < ::Rake::TaskLib
     end
     # Set up filelists
     PuppetSyntax.manifests_paths = puppet_changed_files
-    PuppetSyntax.templates_paths = filter_files_by("**/templates/**/*.erb", "**/templates/**/*.epp")
+    PuppetSyntax.templates_paths = filter_files_by("**/templates/**.erb", "**/templates/**.epp")
     PuppetSyntax.hieradata_paths = filter_files_by("hieradata/**.yaml", "conftool-data/**.yaml")
     tasks = []
     unless PuppetSyntax.manifests_paths.empty?
