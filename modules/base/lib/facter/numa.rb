280212a072f22371d59855223eb256a63a13dc9e
Brandon Black <bblack@wikimedia.org>
2018-10-31 11:21:49 +0000

    remove numa device_to_cpumask_invert fact
    Nothing uses this.  Some earlier experiments that did not go very
    well tried to use it, and thinking at this point is anything that
    would logically need this data is probably a fundamentally bad
    idea to automate/puppetize, so may as well remove the complexity.
    
    Change-Id: I6f9c1e5d529c227df1ecdae5733e7f774eeed85d

--- modules/base/lib/facter/numa.rb
+++ modules/base/lib/facter/numa.rb
@@ -12,13 +12,6 @@
 # attached to a single specific node is assigned to all nodes in the output
 # here, as shown for "lo" in the first example below.
 #
-# device_to_cpumask_invert gives a sysfs "cpumask" form which specifies all
-# CPUs which are not part of the NUMA node of the adapter.  In the case of
-# non-NUMA adapaters like "lo", it will be all-cpus instead of no-cpus, to
-# avoid common silly failure modes when applying this data in non-NUMA cases.
-# The intent is to use this cpumask to move low-priority (or at least,
-# non-network) work away from the NUMA node of the adapter.
-#
 # Example with 2x numa nodes as 2x cpu dies.  Each die has 4 physical (8 logical
 # HT) cpu cores, system would show "16 cpus" in the simple view.  eth0 and eth1
 # are each attached to a distinct node:
@@ -36,11 +29,6 @@
 #        "lo"   => [[0, 8], [1, 9], [2, 10], [3, 11],
 #                   [4, 12], [5, 13], [6, 14], [7, 15]],
 #    }
-#    device_to_cpumask_invert => {
-#        "eth0" => "aaaa", # node1's cpus
-#        "eth1" => "5555", # node0's cpus
-#        "lo"   => "ffff", # all cpus
-#    }
 # }
 #
 # Minimal example, how things might look on a small or virtual node with 1
@@ -55,10 +43,6 @@
 #    "device_to_htset" => {
 #        "eth0" => [[0]],
 #        "lo"   => [[0]],
-#    }
-#    device_to_cpumask_invert => {
-#        "eth0" => "1",
-#        "lo"   => "1",
 #    }
 # }
 #
@@ -84,20 +68,12 @@ def nodes_to_htsets(nodes)
   node_to_htset
 end
 
-def get_cpumask(nodes)
-  nodes.map do |n|
-    File.open("/sys/devices/system/node/node#{n}/cpumap")
-        .read.tr(',', '').to_i(16)
-  end.reduce(:|).to_s(16).reverse.scan(/.{8}|.+/).join(',').reverse
-end
-
 Facter.add('numa') do
   setcode do
     nodes = nodes_list
     node_to_htset = nodes_to_htsets(nodes)
     device_to_node = {}
     device_to_htset = {}
-    device_to_cpumask_invert = {}
     Pathname.glob('/sys/class/net/*').sort.each do |d|
       dev = d.to_s.split('/')[4]
       nodefile = "#{d}/device/numa_node"
@@ -105,18 +81,15 @@ Facter.add('numa') do
       if raw_node >= 0 && nodes.length > 1
         device_to_node[dev] = [raw_node]
         device_to_htset[dev] = node_to_htset[raw_node]
-        device_to_cpumask_invert[dev] = get_cpumask(nodes - [raw_node])
       else
         device_to_node[dev] = nodes
         device_to_htset[dev] = node_to_htset.values.flatten(1).sort
-        device_to_cpumask_invert[dev] = get_cpumask(nodes)
       end
     end
     {
       'nodes' => nodes,
       'device_to_node' => device_to_node,
       'device_to_htset' => device_to_htset,
-      'device_to_cpumask_invert' => device_to_cpumask_invert,
     }
   end
 end
