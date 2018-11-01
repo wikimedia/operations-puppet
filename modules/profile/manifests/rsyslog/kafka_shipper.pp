e0d909c31ae4acf88bfc7719fa67fb58948a1358
Keith Herron <kherron@wikimedia.org>
2018-11-01 16:50:59 -0400

    profile::rsyslog::kafka_shipper: generalized log pipeline/elk shipper
    create a generalized log shipper within rsyslog that utilizes the
    logging pipeline of rsyslog -> kafka -> logstash
    
    this adds a profile to be included by the base profile which enables general
    purpose rsyslog kafka output support.
    
    later, individual log sources may "opt in" to the kafka output by updating
    the "output lookup table" to specify kafka as a desired output destination
    
    Bug: T206454
    Bug: T206633
    Change-Id: Id751995020c4d505aa917ad13a2f66f5b668078c

---
+++ modules/profile/manifests/rsyslog/kafka_shipper.pp
@@ -0,0 +1,53 @@
+
+# Class profile::rsyslog::kafka_shipper - generalized rsyslog configuration to ship logs into logging pipeline
+#                                         comprised of rsyslog -> kafka -> logstash
+#
+#              @@@@@@@@@
+#                    __\_\__
+#         ___________|_____|___________
+#          \ logs logs logs logs logs /
+#           \  O  O  O  O  O  O  O  /
+#^^^^^^^^^^^^\_____________________/^^^^^^^^^^^
+#
+#
+# $logging_kafka_brokers - array of kafka broker used in rsyslog omkafka config
+#
+
+class profile::rsyslog::kafka_shipper (
+    Array   $logging_kafka_brokers = hiera('profile::rsyslog::kafka_shipper::kafka_brokers'),
+) {
+
+    require_package('rsyslog-kafka')
+
+    file { '/etc/rsyslog.lookup.d':
+        ensure => directory,
+    }
+
+    file { '/etc/rsyslog.lookup.d/lookup_table_output.json':
+        ensure  => present,
+        source  => 'puppet:///modules/profile/rsyslog/lookup_table_output.json',
+        require => File['/etc/rsyslog.lookup.d'],
+    }
+
+    rsyslog::conf { 'lookup_output':
+        content  => template('profile/rsyslog/lookup_output.conf.erb'),
+        priority => 10,
+        require  => File['/etc/rsyslog.lookup.d/lookup_table_output.json'],
+    }
+
+    rsyslog::conf { 'template_syslog_json':
+        source   => 'puppet:///modules/profile/rsyslog/template_syslog_json.conf',
+        priority => 10,
+    }
+
+    rsyslog::conf { 'output_kafka':
+        content  => template('profile/rsyslog/output_kafka.conf.erb'),
+        priority => 30,
+    }
+
+    rsyslog::conf { 'output_local':
+        content  => template('profile/rsyslog/output_local.conf.erb'),
+        priority => 95,
+    }
+
+}
