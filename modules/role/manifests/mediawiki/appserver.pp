4c857b9f05abced22ce5ea6850e80842dbe9bdf4
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-11-02 11:20:35 +0100

    mediawiki::appserver: use newer class structure
    Change-Id: I5e3fd42243c42bade57a68c09b2cf1eef2e78585

--- modules/role/manifests/mediawiki/appserver.pp
+++ modules/role/manifests/mediawiki/appserver.pp
@@ -4,14 +4,9 @@ class role::mediawiki::appserver {
     include standard
     include ::role::mediawiki::common
 
-    # Temporary to test the new profile
-    if $::hostname =~ /^mwdebug\d+/ or $::realm == 'labs' {
-        include ::profile::mediawiki::webserver
-    }
-    else {
-        include ::role::mediawiki::webserver
-    }
     include ::profile::base::firewall
     include ::profile::prometheus::apache_exporter
     include ::profile::prometheus::hhvm_exporter
+
+    include ::profile::mediawiki::webserver
 }
--- modules/role/manifests/mediawiki/appserver/api.pp
+++ modules/role/manifests/mediawiki/appserver/api.pp
@@ -1,9 +1,12 @@
 class role::mediawiki::appserver::api {
     system::role { 'mediawiki::appserver::api': }
+    include standard
+    include ::role::mediawiki::common
 
-    include ::role::mediawiki::webserver
     include ::profile::base::firewall
     include ::profile::prometheus::apache_exporter
     include ::profile::prometheus::hhvm_exporter
+
+    include ::profile::mediawiki::webserver
     include ::profile::mediawiki::api
 }
