4733326b665ccb04f26ad62a317160543ab0c27a
Alex Monk <krenair@gmail.com>
2018-11-06 18:39:40 +0000

    network::constants labs: add missing redundant project-proxy host
    Dunno why this was left off the list, my understanding is that generally
    such redundant hosts sit on access control lists so they are ready to become
    active.
    
    Change-Id: I9bcf202e2010dbfd71b3b8d712c8a5a3608b122a

--- modules/network/manifests/constants.pp
+++ modules/network/manifests/constants.pp
@@ -444,6 +444,7 @@ class network::constants {
                 ],
             'caches' => [ # So that roles with CACHES in ferm rules can be used in labs
                     '10.68.21.68', # novaproxy-01.project-proxy.eqiad.wmflabs
+                    '10.68.21.69', # novaproxy-02.project-proxy.eqiad.wmflabs
                 ],
             }
     }
