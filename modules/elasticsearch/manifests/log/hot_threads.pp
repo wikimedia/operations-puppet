65b2178089fc92cc16fc45ee88837a2cac79ce1d
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2018-11-08 14:09:31 +0100

    elasticsearch: hotthread logger needs python3-yaml
    The main script was upgraded to python3 but not the dependencies.
    
    Change-Id: I25505a0c5ef8bcb8e5def4d18845283c8ce2ab78

--- modules/elasticsearch/manifests/log/hot_threads.pp
+++ modules/elasticsearch/manifests/log/hot_threads.pp
@@ -3,7 +3,7 @@
 # Install a cron job to log the hot threads.
 #
 class elasticsearch::log::hot_threads {
-    require_package('python-yaml')
+    require_package('python3-yaml')
 
     file { '/etc/elasticsearch_hot_threads.d':
         ensure => directory,
