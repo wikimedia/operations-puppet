c3e00c101e466c60a3c49de59bb5edfaa8e82ac6
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-11-09 15:49:18 +0100

    mtail: more verbose test output on failure
    Report mtail stderr on logging, to be captured by tox, so help the user understand why mtail's
    metric store cannot be parsed. This is likely due to mtail program failed to compile.
    
    Change-Id: Ifba6a5c48cb8ac859a62a6ea22c714b9e19ec8b3

--- modules/mtail/files/test/mtail_store.py
+++ modules/mtail/files/test/mtail_store.py
@@ -1,5 +1,9 @@
 import json
 import subprocess
+import logging
+
+
+log = logging.getLogger(__name__)
 
 
 class MtailMetricStore(object):
@@ -7,34 +11,39 @@ class MtailMetricStore(object):
         self._store = {}
         self._progs = progs
         self._logs = logs
-        self.parse_metric_store(self.run_mtail()[0])
+        self.parse_metric_store(*self.run_mtail())
 
     def run_mtail(self):
-        stdout, stderr = subprocess.Popen(
-                ['mtail', '-one_shot', '-one_shot_metrics', '-logtostderr',
-                 '-progs', self._progs, '-logs', self._logs], stderr=subprocess.PIPE,
-                stdout=subprocess.PIPE).communicate()
+        mtail_cmd = ['mtail', '-one_shot', '-one_shot_metrics', '-logtostderr',
+                     '-progs', self._progs, '-logs', self._logs]
+        process = subprocess.Popen(mtail_cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        stdout, stderr = process.communicate()
         return stdout, stderr
 
-    def parse_metric_store(self, output):
+    def parse_metric_store(self, stdout, stderr):
         metrics_store = []
 
         in_json = False
-        for line in output.splitlines():
+        for line in stdout.splitlines():
             if in_json:
                 metrics_store.append(line)
             if line.startswith('Metrics store:{'):
                 in_json = True
                 metrics_store.append('{')
 
-        self._store = json.loads(''.join(metrics_store))
+        try:
+            self._store = json.loads(''.join(metrics_store))
+        except ValueError as e:
+            log.warn(subprocess.check_output(['dpkg-query', '-W', 'mtail']))
+            log.warn(stderr)
+            raise ValueError('Error parsing metric store: %r' % e)
 
     def get_samples(self, name):
         """Return all samples for metric name as a list of samples.
            Each sample is in this form: ("k1=v1,k2=v2", value)"""
         samples = []
         if name not in self._store:
-            raise ValueError('metric %s not found in store', name)
+            raise ValueError('metric %s not found in store' % name)
         for metric in self._store[name][0]['LabelValues']:
             label_names = self._store[name][0].get('Keys', [])
             label_values = metric.get('Labels', [])
