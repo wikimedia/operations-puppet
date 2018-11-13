127bc31f79447f58f776d5f4753278a79e9c0492
Elukey <ltoscano@wikimedia.org>
2018-11-13 07:38:04 +0000

    Revert "toolforge: refactor mail server"
    This reverts commit 8c0bf727268df9f5302872d0855366464c5caa98.
    
    Change-Id: I91dd1333284c3ecae30cb6a6489b8a810f23290e

--- modules/role/manifests/wmcs/toolforge/mailrelay.pp
+++
@@ -1,7 +0,0 @@
-class role::wmcs::toolforge::mailrelay {
-    system::role { $name: }
-
-    include profile::toolforge::base
-    include profile::toolforge::infrastructure
-    include profile::toolforge::mailrelay
-}
