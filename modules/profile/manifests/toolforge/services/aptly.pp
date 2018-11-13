af06bc519332e06479c22d9365eb2d460548e209
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2018-11-13 12:26:43 +0100

    toolforge: aptly: create stretch-toolsbeta repo
    Temporal, while building both grids in toolforge/toolsbeta.
    We need to serve .deb packages from the services node in toolforge to servers
    in toolsbeta.
    
    Bug: T207970
    Change-Id: I65017f052a9689d824b27a26eed35fbcd4be5bfd
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/toolforge/services/aptly.pp
+++ modules/profile/manifests/toolforge/services/aptly.pp
@@ -1,4 +1,9 @@
 class profile::toolforge::services::aptly() {
     # generic aptly server role
     include profile::aptly::server
+
+    # while working with the dual grid in tools/toolsbeta
+    aptly::repo { 'stretch-toolsbeta':
+        publish      => true,
+    }
 }
