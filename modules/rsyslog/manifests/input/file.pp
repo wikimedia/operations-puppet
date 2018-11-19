ba714ac78103e71da14c110cacf2dfe4f3109f79
Keith Herron <kherron@wikimedia.org>
2018-11-19 14:08:38 -0500

    rsyslog::input::file add rsyslog imfile wrapper for file ingestion
    Bug: T205852
    Bug: T209860
    Change-Id: Ia2bbe76de134d4de154ad09123e9a111cf1bc0f2

---
+++ modules/rsyslog/manifests/input/file.pp
@@ -0,0 +1,33 @@
+#
+# rsyslog::input::file - ingest a log file with rsyslog imfile for processing/shipping
+#
+# see https://www.rsyslog.com/doc/v8-stable/configuration/modules/imfile.html for full imfile documentation
+#
+# $path - Input file path glob. Wildcards supported in both file and directories
+# $syslog_tag_prefix - Prefix to be applied to syslog tag field (defaults to 'imfile')
+# $syslog_tag - Desired syslog tag (useful for example with filtering/shipping)
+# $priority - Rsyslog conf file priority number (used in filename prefix)
+# $reopen_on_truncate - Reopen input file when it was truncated (inode unchanged but file size on disk is less than current offset in memory).
+#
+define rsyslog::input::file(
+    String  $path,
+    Enum['on','off'] $reopen_on_truncate         = 'on',
+    Pattern[/[a-zA-Z0-9-_]+/] $syslog_tag_prefix = 'input-file',
+    Pattern[/[a-zA-Z0-9-_]+/] $syslog_tag        = $title,
+    Integer $priority                            = 10,
+) {
+
+    rsyslog::conf { "${syslog_tag_prefix}-${syslog_tag}":
+        content  => template('rsyslog/input/file.erb'),
+        priority => $priority,
+        require  => Rsyslog::Conf['imfile'],
+    }
+
+    if ! defined(Rsyslog::Conf['imfile']) {
+        rsyslog::conf { 'imfile':
+            content  => 'module(load="imfile")',
+            priority => 00,
+        }
+    }
+
+}
