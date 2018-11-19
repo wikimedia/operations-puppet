84ccc5582c95460c5b0c4a8c6c4eb2902a5e2b2e
bstorm <bstorm@wikimedia.org>
2018-11-19 13:41:43 -0700

    sonofgridengine: observer env and the openstack client libs for SGE master
    Bug: T200557
    Change-Id: Ie197cfdf90ca1c0663b5e5da0aaac7d725514495

--- modules/profile/manifests/toolforge/grid/master.pp
+++ modules/profile/manifests/toolforge/grid/master.pp
@@ -3,6 +3,8 @@
 class profile::toolforge::grid::master (
     $etcdir = hiera('profile::toolforge::etcdir'),
 ){
+    include profile::openstack::main::clientlib
+    include profile::openstack::main::observerenv
     include profile::toolforge::infrastructure
 
     $hostlist = '@general'
