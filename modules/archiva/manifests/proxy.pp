5ac0a959724077e0565824564d3aaf7cbb986064
Valentin Gutierrez <vgutierrez@wikimedia.org>
2018-11-26 15:47:07 +0100

    archiva: Use certcentral managed TLS certificate
    To be merged after I8bd814a1afd381765979ab4994ff6c8a96cd21cf
    
    Bug: T207050
    Change-Id: I3da9a3c3a2ebe6c902b840be570d1a363816197b

--- modules/archiva/manifests/proxy.pp
+++ modules/archiva/manifests/proxy.pp
@@ -53,8 +53,8 @@ class archiva::proxy(
             # regsubst is needed due to letsencrypt::cert::integrated's naming
             # conventions.
             $safe_cert_name = regsubst($certificate_name, '\W', '_', 'G')
-            $ssl_certificate_chained = "/etc/acme/cert/${safe_cert_name}.chained.crt"
-            $ssl_certificate_key = "/etc/acme/key/${safe_cert_name}.key"
+            $ssl_certificate_chained = "/etc/centralcerts/${safe_cert_name}.rsa-2048.chained.crt"
+            $ssl_certificate_key = "/etc/centralcerts/${safe_cert_name}.rsa-2048.key"
 
             $tls_server_properties = [
                 "ssl_certificate     ${ssl_certificate_chained};",
