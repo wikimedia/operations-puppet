aedd72d86bb301f97034d15748b6a301e0003348
Daniel Zahn <dzahn@wikimedia.org>
2018-11-26 13:56:27 -0800

    icinga/NSCA (passive checks): remove jessie support hacks
    Bug: T202782
    Change-Id: I4a9677642c14cbfd69f2e74bd5d07b1f5ab84e04

--- modules/icinga/manifests/nsca/daemon.pp
+++ modules/icinga/manifests/nsca/daemon.pp
@@ -14,29 +14,14 @@ class icinga::nsca::daemon (
     include ::passwords::icinga
     $nsca_decrypt_password = $::passwords::icinga::nsca_decrypt_password
 
-
-    if os_version('debian >= stretch') {
-
-        systemd::service { 'nsca':
-            ensure  => 'present',
-            content => systemd_template('nsca'),
-            require => File['/etc/nsca.cfg'],
-        }
-
-        $nsca_chroot = '/var/lib/icinga'
-        $command_file = '/rw/icinga.cmd'
-
-    # FIXME: remove after we are off jessie
-    } else {
-
-        service { 'nsca':
-            ensure  => running,
-            require => File['/etc/nsca.cfg'],
-        }
-
-        $nsca_chroot = '/var/lib/nagios'
-        $command_file = '/rw/nagios.cmd'
+    systemd::service { 'nsca':
+        ensure  => 'present',
+        content => systemd_template('nsca'),
+        require => File['/etc/nsca.cfg'],
     }
+
+    $nsca_chroot = '/var/lib/icinga'
+    $command_file = '/rw/icinga.cmd'
 
     file { '/etc/nsca.cfg':
         content => template('icinga/nsca.cfg.erb'),
