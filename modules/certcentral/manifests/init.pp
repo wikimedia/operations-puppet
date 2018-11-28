edb78da0d7c66294889c2c10a24d043e2bff9c5c
Valentin Gutierrez <vgutierrez@wikimedia.org>
2018-11-28 09:13:25 +0100

    certcentral: check for already declared LE Intermediate certs
    This is only required to allow coexisting letsencrypt and certcentral
    puppet modules. After the migration is completed this can be reverted
    
    Bug: T207050
    Change-Id: I26aeba71edd4cf19d964c9526d47404244a88397

--- modules/certcentral/manifests/init.pp
+++ modules/certcentral/manifests/init.pp
@@ -3,12 +3,16 @@
 
 class certcentral {
     # LE Intermediate: current since ~2016-03-26
-    sslcert::ca { 'Lets_Encrypt_Authority_X3':
-        source  => 'puppet:///modules/certcentral/lets-encrypt-x3-cross-signed.pem'
+    if !defined(Sslcert::Ca['Lets_Encrypt_Authority_X3']) {
+        sslcert::ca { 'Lets_Encrypt_Authority_X3':
+            source  => 'puppet:///modules/certcentral/lets-encrypt-x3-cross-signed.pem'
+        }
     }
 
     # LE Intermediate: disaster recovery fallback since ~2016-03-26
-    sslcert::ca { 'Lets_Encrypt_Authority_X4':
-        source  => 'puppet:///modules/certcentral/lets-encrypt-x4-cross-signed.pem'
+    if !defined(Sslcert::Ca['Lets_Encrypt_Authority_X4']) {
+        sslcert::ca { 'Lets_Encrypt_Authority_X4':
+            source  => 'puppet:///modules/certcentral/lets-encrypt-x4-cross-signed.pem'
+        }
     }
 }
