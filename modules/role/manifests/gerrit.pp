b8d0a9764c9465d853d9aec37d9383da31b83b28
Herron <kherron@wikimedia.org>
2018-11-29 18:25:32 +0000

    rsyslog: input::file add multiline handling & ship gerrit logs to ELK
     * Add parameter to parse multi-line log events with rsyslog imfile
    startmsg.regex and...
    
     * Add logstash filter to replace escaped newlines for pretty output
    in kibana and...
    
     * Ship gerrit logs to ELK using rsyslog -> kafka -> logstash pipeline
    
    Bug: T141324
    Bug: T205852
    Change-Id: Ibfed6d087966fe4d60f2ad642278a345c2d1a440

--- modules/role/manifests/gerrit.pp
+++ modules/role/manifests/gerrit.pp
@@ -9,6 +9,7 @@ class role::gerrit {
     include ::profile::backup::host
     include ::profile::base::firewall
     include ::profile::gerrit::server
+    include ::profile::rsyslog::kafka_shipper
 
     class { '::httpd':
         modules => ['rewrite', 'headers', 'proxy', 'proxy_http', 'remoteip', 'ssl'],
