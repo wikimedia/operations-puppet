1e02a5bd4b183d15b0144e6a6a83df41dd680ea1
Emanuele Rocca <ema@wikimedia.org>
2018-12-03 13:39:43 +0100

    ATS: set cache_responses_to_cookies to 1
    Setting the value to 0, as previously done in 24ce8dd, makes all
    responses to requests with a Cookie header uncacheable. Set it to 1
    instead. Make sure we add CC:private to Set-Cookie responses *before*
    deciding if they are cacheable (i.e. in TS_LUA_HOOK_READ_RESPONSE_HDR).
    
    Bug: T209021
    Change-Id: I1bbc8f58264345fcb4c942ca70cc6af9a869434d

--- modules/profile/files/trafficserver/cookies-private.lua
+++ modules/profile/files/trafficserver/cookies-private.lua
@@ -1,10 +1,11 @@
 function set_cc_private()
-    if ts.client_response.header['Set-Cookie'] then
-        ts.client_response.header['Cache-Control'] = 'private, max-age=0, s-maxage=0'
+    if ts.server_response.header['Set-Cookie'] then
+        ts.server_response.header['Cache-Control'] = 'private, max-age=0, s-maxage=0'
+        ts.error("Setting CC:private on response with Set-Cookie for uri " ..  ts.client_request.get_uri())
     end
 end
 
 function do_remap()
-    ts.hook(TS_LUA_HOOK_SEND_RESPONSE_HDR, set_cc_private)
+    ts.hook(TS_LUA_HOOK_READ_RESPONSE_HDR, set_cc_private)
     return 0
 end
--- modules/profile/files/trafficserver/cookies-private_test.lua
+++ modules/profile/files/trafficserver/cookies-private_test.lua
@@ -1,24 +1,28 @@
 _G.ts = {
-  client_response = {
+  server_response = {
     header = {}
-  }
+  },
+  client_request = {},
 }
 
 describe("Busted unit testing framework", function()
   describe("script for ATS Lua Plugin", function()
 
     it("test - set_cc_private", function()
+      ts.client_request.get_uri = function() return "/" end
+      stub(ts, "error")
+
       require("cookies-private")
 
       set_cc_private()
 
-      assert.are.equals(nil, _G.ts.client_response.header['Cache-Control'])
+      assert.are.equals(nil, _G.ts.server_response.header['Cache-Control'])
 
-      _G.ts.client_response.header['Set-Cookie'] = 'banana potato na'
+      _G.ts.server_response.header['Set-Cookie'] = 'banana potato na'
 
       set_cc_private()
 
-      assert.are.equals("private, max-age=0, s-maxage=0", _G.ts.client_response.header['Cache-Control'])
+      assert.are.equals("private, max-age=0, s-maxage=0", _G.ts.server_response.header['Cache-Control'])
     end)
 
   end)
