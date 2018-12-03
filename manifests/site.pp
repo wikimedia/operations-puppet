8a62aa7159fe6e88918af57e54b0445e81ace4f9
Chris Danis <cdanis@wikimedia.org>
2018-12-03 10:45:30 -0500

    Add DHCP and autoinstall options for grafana1001
    Bug: T210416
    Change-Id: I60fc12b8bd483a8ba2eca3f98213b4ef0215f390

--- manifests/site.pp
+++ manifests/site.pp
@@ -981,6 +981,11 @@ node /^ganeti[12]00[0-9]\.(codfw|eqiad)\.wmnet$/ {
     role(ganeti)
 }
 
+# Virtual machine being turned up to run Grafana (T210416)
+node 'grafana1001.eqiad.wmnet' {
+    role(spare::system)
+}
+
 # debug_proxy hosts; Varnish backend for X-Wikimedia-Debug reqs
 node /^(hassaleh|hassium)\.(codfw|eqiad)\.wmnet$/ {
     role(debug_proxy)
