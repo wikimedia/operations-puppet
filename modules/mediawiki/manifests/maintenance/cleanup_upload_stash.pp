c75b314b1f3bcb7edb51c330d745fc165a65fb4b
thifranc <thifranc42@gmail.com>
2018-12-07 10:28:04 +0100

    puppet:Reduce cronspam from modules/mediawiki/
    Redirect stdout to /var/log/cleanup_upload_stash.log
    Redirect stderr to /var/log/cleanup_upload_stash.err
    Per compliance, no logrotate so we will keep only the latest execution
    
    Bug: T150375
    Change-Id: I22eb77ed5e728d25d52b8157092abd8b6e3949d6

--- modules/mediawiki/manifests/maintenance/cleanup_upload_stash.pp
+++ modules/mediawiki/manifests/maintenance/cleanup_upload_stash.pp
@@ -1,7 +1,7 @@
 class mediawiki::maintenance::cleanup_upload_stash( $ensure = present ) {
     cron { 'cleanup_upload_stash':
         ensure  => $ensure,
-        command => '/usr/local/bin/foreachwiki maintenance/cleanupUploadStash.php > /dev/null',
+        command => '/usr/local/bin/foreachwiki maintenance/cleanupUploadStash.php > /var/log/mediawiki/cleanup_upload_stash.log 2>/var/log/mediawiki/cleanup_upload_stash.err',
         user    => $::mediawiki::users::web,
         hour    => 1,
         minute  => 0,
