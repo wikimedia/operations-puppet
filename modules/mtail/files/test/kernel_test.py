4e9ca613f3852b58d494ae9d0aa6853060895b59
Chris Danis <cdanis@wikimedia.org>
2018-12-07 12:00:04 -0500

    kernel.mtail needs hostnames
    Right now the only place that kernel.mtail runs is on the
    syslog::centralserver hosts.  Because we don't parse originating
    hostname out of the logs, they don't presently make it to Prometheus,
    which means that we wind up with a single timeseries per metric per
    centralserver, which is not really useful.  For instance, here is the
    complete list of cpu_throttled timeseries we have in eqiad:
    
    cpu_throttled{cluster="misc",instance="centrallog1001:3903",job="mtail",
                  prog="kernel.mtail",site="eqiad"}
    
    cpu_throttled{cluster="misc",instance="lithium:3903",job="mtail",
                  prog="kernel.mtail",site="eqiad"}
    
    With no way to tie it back to originating machine, these are not all
    that useful.
    
    This change adds hostnames from syslog for all kernel.mtail metrics
    and adds tests for metrics that lacked them.
    
    Bug: T209863
    Change-Id: I4c01342d41e0c6fa9989c5f8503589e213b569ac

--- modules/mtail/files/test/kernel_test.py
+++ modules/mtail/files/test/kernel_test.py
@@ -13,10 +13,26 @@ class KernelTest(unittest.TestCase):
 
     def testOOM(self):
         s = self.store.get_samples('oom_kill')
-        self.assertIn(('binary=thumbor', 2), s)
-        self.assertIn(('binary=Chrome_IOThread', 1), s)
+        self.assertIn(('binary=thumbor,hostname=thumbor1003', 1), s)
+        self.assertIn(('binary=thumbor,hostname=thumbor1001', 1), s)
+        self.assertIn(('binary=Chrome_IOThread,hostname=scb1003', 1), s)
 
     def testSegfault(self):
         s = self.store.get_samples('segfault')
-        self.assertIn(('binary=rsvg-convert', 3), s)
-        self.assertIn(('binary=HTML5 Parser', 1), s)
+        self.assertIn(('binary=rsvg-convert,hostname=thumbor1003', 2), s)
+        self.assertIn(('binary=rsvg-convert,hostname=thumbor1002', 1), s)
+        self.assertIn(('binary=HTML5 Parser,hostname=sca2004', 1), s)
+
+    def testThrottle(self):
+        s = self.store.get_samples('cpu_throttled')
+        self.assertIn(('hostname=mw1227', 1), s)
+        self.assertIn(('hostname=mw1225', 1), s)
+        self.assertIn(('hostname=mw1253', 2), s)
+
+    def testEdac(self):
+        s = self.store.get_samples('edac_events')
+        self.assertIn(('hostname=mw1239', 1), s)
+
+    def testRespawn(self):
+        s = self.store.get_samples('upstart_respawn')
+        self.assertIn(('service=nova-spiceproxy,hostname=labcontrol1002', 1), s)
