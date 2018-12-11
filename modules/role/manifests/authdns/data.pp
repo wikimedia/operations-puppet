9a555b797a710e431acc29520a137b921dded713
Brandon Black <bblack@wikimedia.org>
2018-12-11 14:21:01 +0000

    Remove pointless ns[012] IPv6 in ns_addrs
    Change-Id: Ie51b31d24e32b83570c1866022ee45c5a41c8b0f

--- modules/role/manifests/authdns/data.pp
+++ modules/role/manifests/authdns/data.pp
@@ -4,13 +4,9 @@ class role::authdns::data {
     $gitrepo = 'https://gerrit.wikimedia.org/r/p/operations/dns.git'
 
     # These are all of the public addresses the Internet finds us at
-    # (Note the IPv6 ones aren't published yet, not in "real" use)
     $ns_addrs = {
         ns0-v4 => { address => '208.80.154.238',     prefixlen => '32'  },
         ns1-v4 => { address => '208.80.153.231',     prefixlen => '32'  },
         ns2-v4 => { address => '91.198.174.239',     prefixlen => '32'  },
-        ns0-v6 => { address => '2620:0:861:ed1a::e', prefixlen => '128' },
-        ns1-v6 => { address => '2620:0:860:ed1a::e', prefixlen => '128' },
-        ns2-v6 => { address => '2620:0:862:ed1a::e', prefixlen => '128' },
     }
 }
