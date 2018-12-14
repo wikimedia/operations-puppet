1023c5f6c735337e373f5809db4cbc0a2a923a44
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-12-14 15:28:22 +0100

    Enable Kerberos support for hdfs-balancer systemd timer
    Change-Id: Ibb49ca4b35bd218cf61b50aee261de08e8cb80fc

--- modules/profile/manifests/hadoop/balancer.pp
+++ modules/profile/manifests/hadoop/balancer.pp
@@ -2,8 +2,16 @@
 #
 # Runs hdfs balancer periodically to keep data balanced across all DataNodes
 #
-class profile::hadoop::balancer {
+class profile::hadoop::balancer(
+    $use_kerberos       = hiera('profile::hadoop::balancer::use_kerberos', false),
+) {
     require ::profile::hadoop::common
+
+    if $use_kerberos {
+        $wrapper = '/usr/local/bin/kerberos-puppet-wrapper hdfs '
+    } else {
+        $wrapper = ''
+    }
 
     file { '/usr/local/bin/hdfs-balancer':
         source => 'puppet:///modules/profile/hadoop/hdfs-balancer',
@@ -30,7 +38,7 @@ class profile::hadoop::balancer {
 
     profile::analytics::systemd_timer { 'hdfs-balancer':
         description     => 'Run the HDFS balancer script to keep HDFS blocks replicated in the most redundant and efficient way.',
-        command         => '/usr/local/bin/hdfs-balancer',
+        command         => "${wrapper}/usr/local/bin/hdfs-balancer",
         interval        => '*-*-* 06:00:00',
         logfile_name    => 'balancer.log',
         logfile_basedir => '/var/log/hadoop-hdfs',
