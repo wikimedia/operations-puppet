503978fba5746bb7fe90773dedca3469df591100
Amir Sarabadani <Ladsgroup@gmail.com>
2018-12-14 16:58:50 +0100

    ores: Allow cross origin requests if 'X-Wikimedia-Debug' header is sent
    Bug: T211511
    Change-Id: I989bfcba1f831028c40c6226cfc8a6c52e772433

--- modules/ores/manifests/web.pp
+++ modules/ores/manifests/web.pp
@@ -47,7 +47,7 @@ class ores::web(
             venv          => $ores::base::venv_path,
             logformat     => '[pid: %(pid)] %(addr) (%(user)) {%(vars) vars in %(pktsize) bytes} [%(ctime)] %(method) %(uri) => generated %(rsize) bytes in %(msecs) msecs (%(proto) %(status)) %(headers) headers in %(hsize) bytes (%(switches) switches on core %(core)) user agent "%(uagent)"',
             processes     => $web_workers,
-            add-header    => [ 'Access-Control-Allow-Origin: *', "Server: ${::fqdn}" ],
+            add-header    => [ 'Access-Control-Allow-Origin: *', "Server: ${::fqdn}", 'Access-Control-Allow-Headers: X-Wikimedia-Debug' ],
             max-requests  => 200,
             stats-push    => "statsd:${statsd_server}:8125,ores.${::hostname}.uwsgi",
             memory-report => true,
