fa208e7d0f0ad7ceca91e9615fe659fc5ddfaf09
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2018-12-14 15:29:06 +0100

    Remove unused cron and logrotate config, replaced by systemd timer
    Change-Id: If96c48b99812614df57328f65fac4fd0fea1b3a6

--- modules/profile/manifests/hadoop/balancer.pp
+++ modules/profile/manifests/hadoop/balancer.pp
@@ -20,22 +20,6 @@ class profile::hadoop::balancer(
         group  => 'hdfs',
     }
 
-    # logrotate HDFS balancer's log files
-    logrotate::conf { 'hdfs_balancer':
-        ensure => absent,
-        source => 'puppet:///modules/profile/hadoop/hdfs_balancer.logrotate',
-    }
-
-    cron { 'hdfs-balancer':
-        ensure  => absent,
-        command => '/usr/local/bin/hdfs-balancer >> /var/log/hadoop-hdfs/balancer.log 2>&1',
-        user    => 'hdfs',
-        # Every day at 6am UTC.
-        minute  => 0,
-        hour    => 6,
-        require => File['/usr/local/bin/hdfs-balancer'],
-    }
-
     profile::analytics::systemd_timer { 'hdfs-balancer':
         description     => 'Run the HDFS balancer script to keep HDFS blocks replicated in the most redundant and efficient way.',
         command         => "${wrapper}/usr/local/bin/hdfs-balancer",
