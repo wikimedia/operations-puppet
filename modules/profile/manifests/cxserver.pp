0207b15de9fbfa85303f9010f009370ae2506fc4
Kartik Mistry <kartik.mistry@gmail.com>
2018-12-18 18:44:50 +0530

    Configure cxserver ratelimiter
    * Open up udp ferm rules for cxserver hosts
    * Set up the kademlia ratelimit port
    * Set in hiera the port to be 3051
    * Specify per DC hiera for the hosts
    * Pass the variable to the deployment vars
    
    Change-Id: I47b4c46beefd21be20721f62c6379f86b4f87072

--- modules/profile/manifests/cxserver.pp
+++ modules/profile/manifests/cxserver.pp
@@ -3,15 +3,21 @@
 # filtertags: labs-project-deployment-prep
 
 class profile::cxserver(
-    $apertium_uri=hiera('profile::cxserver::apertium_uri')
+    $apertium_uri=hiera('profile::cxserver::apertium_uri'),
+    $ratelimit_hosts=hiera('profile::cxserver::ratelimit_hosts'),
+    $ratelimit_port=hiera('profile::cxserver::ratelimit_port'),
 ) {
     include ::passwords::cxserver
+
     $lingocloud_api_key = $::passwords::cxserver::lingocloud_api_key
     $matxin_api_key     = $::passwords::cxserver::matxin_api_key
     $yandex_api_key     = $::passwords::cxserver::yandex_api_key
     $youdao_app_key     = $::passwords::cxserver::youdao_app_key
     $youdao_app_secret  = $::passwords::cxserver::youdao_app_secret
     $jwt_secret         = $::passwords::cxserver::jwt_secret
+
+    # Kick out our own IP and fqdn from the list
+    $rl_hosts = $ratelimit_hosts.filter |$x| { $x != $::fqdn and $x != $::ipaddress }
 
     service::node { 'cxserver':
         port              => 8080,
@@ -28,6 +34,17 @@ class profile::cxserver(
             yandex_key         => $yandex_api_key,
             youdao_app_key     => $youdao_app_key,
             youdao_app_secret  => $youdao_app_secret,
+            ratelimit_port     => $ratelimit_port,
+            ratelimit_hosts    => $rl_hosts,
         },
+    }
+
+    # cxserver rate limiting rule
+    $cx_hosts_ferm = join($ratelimit_hosts, ' ')
+    ferm::service { 'cxserver-ratelimit':
+        desc   => "kademlia cxserver ratelimiting - ${ratelimit_port}",
+        proto  => 'udp',
+        port   => $ratelimit_port,
+        srange => "@resolve((${cx_hosts_ferm}))",
     }
 }
