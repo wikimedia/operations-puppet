a8641d3638674a4ac354936c299048d3710d6f59
Daniel Zahn <dzahn@wikimedia.org>
2018-12-17 17:57:44 -0800

    doc: dependency issue using require_package, fix path worker.conf
    Change-Id: I0dc76b405ef42431d12701e88af1d419c0751285

--- modules/profile/manifests/doc.pp
+++ modules/profile/manifests/doc.pp
@@ -7,21 +7,19 @@ class profile::doc {
     # Use php7.2 from Ondrej Sury's repository.
     if $php_version == '7.2' {
 
-        exec {'apt_update_php':
-            command     => '/usr/bin/apt-get update',
-            refreshonly => true,
-            logoutput   => true,
-        }
-
         apt::repository { 'wikimedia-php72':
             uri        => 'http://apt.wikimedia.org/wikimedia',
             dist       => "${::lsbdistcodename}-wikimedia",
             components => 'thirdparty/php72',
             notify     => Exec['apt_update_php'],
         }
-    }
 
-    require_package($php_package)
+        package { '$php_package':
+            ensure  => installed,
+            require => [ Apt::Repository['wikimedia-php72'],
+                        Exec['apt-get update']],
+        }
+    }
 
     class { '::httpd':
         modules => ['headers',
@@ -32,7 +30,7 @@ class profile::doc {
 
     class { '::httpd::mpm':
         mpm    => 'worker',
-        source => 'puppet:///modules/profile/files/doc/httpd_worker.conf'
+        source => 'puppet:///modules/profile/doc/httpd_worker.conf'
     }
 
     ferm::service { 'doc-http':
