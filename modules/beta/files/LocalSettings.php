2e254a1c5dce7a8c623d7a26b57a8f85c3979fa1
Tim Starling <tstarling@wikimedia.org>
2018-12-19 16:01:52 +1100

    When running scripts from staging, use the CommonSettings.php from staging
    Scap attempts to run eval.php from staging as a sanity check prior to
    deployment to /srv/mediawiki. The LocalSettings.php in staging requires
    the CommonSettings.php from /srv/mediawiki, but with $IP set to staging,
    so you end up with a mix of configuration files from both versions. This
    made I799c0884922a603b3cb69ee0f9d18cfa4dc4db9e  undeployable.
    
    So, in LocalSettings.php, get the CommonSettings.php using a relative
    path.
    
    Change-Id: I8df901c21fc5f807fdb9832e2d2ca8d6b35aea16

--- modules/beta/files/LocalSettings.php
+++ modules/beta/files/LocalSettings.php
@@ -2,4 +2,4 @@
 # Managed by puppet. (modules/beta/files/LocalSettings.php)
 # WARNING: This file is publically viewable on the web. Do not put private
 # data here.
-require_once '/srv/mediawiki/wmf-config/CommonSettings.php';
+require __DIR__ . '/../wmf-config/CommonSettings.php';
