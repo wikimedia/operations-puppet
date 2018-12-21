0151dc03679a79d2c6df174bdf04b4d16f8ad874
Luca Toscano <ltoscano@wikimedia.org>
2018-12-21 07:34:25 +0100

    Fix grafana's dashboard links using the .json suffix
    It seems that the varnish-aggregate-client-status-codes dashboard
    link does not need anymore the '.json' suffix, same thing for
    the swift dashboard.
    
    Change-Id: I58948c9e10f3c72a27b4f931d2bb3404ea09e05f

--- modules/role/manifests/graphite/alerts/reqstats.pp
+++ modules/role/manifests/graphite/alerts/reqstats.pp
@@ -7,7 +7,7 @@ class role::graphite::alerts::reqstats {
     # sites aggregates
     monitoring::graphite_threshold { 'reqstats-5xx-eqiad':
         description     => 'Eqiad HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes.json?panelId=3&fullscreen&orgId=1&var-site=eqiad&var-cache_type=All&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=eqiad&var-cache_type=All&var-status_type=5'],
         metric          => 'sumSeries(varnish.eqiad.*.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -17,7 +17,7 @@ class role::graphite::alerts::reqstats {
 
     monitoring::graphite_threshold { 'reqstats-5xx-esams':
         description     => 'Esams HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes.json?panelId=3&fullscreen&orgId=1&var-site=esams&var-cache_type=All&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=esams&var-cache_type=All&var-status_type=5'],
         metric          => 'sumSeries(varnish.esams.*.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -27,7 +27,7 @@ class role::graphite::alerts::reqstats {
 
     monitoring::graphite_threshold { 'reqstats-5xx-codfw':
         description     => 'Codfw HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes.json?panelId=3&fullscreen&orgId=1&var-site=codfw&var-cache_type=All&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=codfw&var-cache_type=All&var-status_type=5'],
         metric          => 'sumSeries(varnish.codfw.*.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -37,7 +37,7 @@ class role::graphite::alerts::reqstats {
 
     monitoring::graphite_threshold { 'reqstats-5xx-ulsfo':
         description     => 'Ulsfo HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes.json?panelId=3&fullscreen&orgId=1&var-site=ulsfo&var-cache_type=All&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=ulsfo&var-cache_type=All&var-status_type=5'],
         metric          => 'sumSeries(varnish.ulsfo.*.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -47,7 +47,7 @@ class role::graphite::alerts::reqstats {
 
     monitoring::graphite_threshold { 'reqstats-5xx-eqsin':
         description     => 'Eqsin HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes.json?panelId=3&fullscreen&orgId=1&var-site=eqsin&var-cache_type=All&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=eqsin&var-cache_type=All&var-status_type=5'],
         metric          => 'sumSeries(varnish.eqsin.*.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -58,7 +58,7 @@ class role::graphite::alerts::reqstats {
     # per-cache aggregates
     monitoring::graphite_threshold { 'reqstats-5xx-text':
         description     => 'Text HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes.json?panelId=3&fullscreen&orgId=1&var-site=All&var-cache_type=text&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=All&var-cache_type=text&var-status_type=5'],
         metric          => 'sumSeries(varnish.*.text.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -68,7 +68,7 @@ class role::graphite::alerts::reqstats {
 
     monitoring::graphite_threshold { 'reqstats-5xx-upload':
         description     => 'Upload HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes.json?panelId=3&fullscreen&orgId=1&var-site=All&var-cache_type=upload&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=All&var-cache_type=upload&var-status_type=5'],
         metric          => 'sumSeries(varnish.*.upload.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
--- modules/swift/manifests/monitoring/graphite_alerts.pp
+++ modules/swift/manifests/monitoring/graphite_alerts.pp
@@ -3,7 +3,7 @@ define swift::monitoring::graphite_alerts (
 ) {
     monitoring::graphite_threshold { "swift_${cluster}_dispersion_object":
         description     => "swift ${cluster} object availability",
-        dashboard_links => ["https://grafana.wikimedia.org/dashboard/file/swift.json?panelId=8&fullscreen&orgId=1&var-DC=${cluster}"],
+        dashboard_links => ["https://grafana.wikimedia.org/dashboard/file/swift?panelId=8&fullscreen&orgId=1&var-DC=${cluster}"],
         metric          => "keepLastValue(swift.${cluster}.dispersion.object.pct_found)",
         from            => '1hours',
         warning         => 95,
@@ -14,7 +14,7 @@ define swift::monitoring::graphite_alerts (
 
     monitoring::graphite_threshold { "swift_${cluster}_dispersion_container}":
         description     => "swift ${cluster} container availability",
-        dashboard_links => ["https://grafana.wikimedia.org/dashboard/file/swift.json?panelId=8&fullscreen&orgId=1&var-DC=${cluster}"],
+        dashboard_links => ["https://grafana.wikimedia.org/dashboard/file/swift?panelId=8&fullscreen&orgId=1&var-DC=${cluster}"],
         metric          => "keepLastValue(swift.${cluster}.dispersion.container.pct_found)",
         from            => '30min',
         warning         => 92,
@@ -25,7 +25,7 @@ define swift::monitoring::graphite_alerts (
 
     monitoring::graphite_threshold { "mediawiki_${cluster}_media_uploads":
         description     => "mediawiki originals uploads (hourly) for ${cluster}",
-        dashboard_links => ["https://grafana.wikimedia.org/dashboard/file/swift.json?panelId=9&fullscreen&orgId=1&var-DC=${cluster}"],
+        dashboard_links => ["https://grafana.wikimedia.org/dashboard/file/swift?panelId=9&fullscreen&orgId=1&var-DC=${cluster}"],
         metric          => "summarize(nonNegativeDerivative(keepLastValue(swift.${cluster}.containers.mw-media.originals.objects)), \"1h\")",
         from            => '5h',
         warning         => 2000,
