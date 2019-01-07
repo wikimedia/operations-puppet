087683878422b245bc085c0f67de9045f20d80ac
Marcel Ruiz Forns <mforns@wikimedia.org>
2019-01-07 23:24:03 +0100

    Bump up refinery_version in refine.pp to v0.0.83
    Change-Id: I3bb880203d3e39d00c91476274537b3edf4c0019

--- modules/profile/manifests/analytics/refinery/job/refine.pp
+++ modules/profile/manifests/analytics/refinery/job/refine.pp
@@ -15,7 +15,7 @@ class profile::analytics::refinery::job::refine (
 
     # Update this when you want to change the version of the refinery job jar
     # being used for the refine job.
-    $refinery_version = '0.0.82'
+    $refinery_version = '0.0.83'
 
     # Use this value by default
     Profile::Analytics::Refinery::Job::Refine_job {
