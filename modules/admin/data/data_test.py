23b2cd73165970ad0cb7f5664a3cfbf9f0156335
Antoine Musso <hashar@free.fr>
2019-01-07 11:23:01 +0100

    admin: test for absent users
    We recently marked 'ladsgroup' as being absent via ensure: absent but
    the user had not been added as a member of the 'absent' group.
    
    Add a test that ensures that members of the group and users marked
    absent match.
    Slightly refactor the test to open the data.yaml file only once for the
    test class.
    
    Ran test via: tox -e admin
    
    Change-Id: I8f17b2786ec034456ef8ea898afefc8294079f9a

--- modules/admin/data/data_test.py
+++ modules/admin/data/data_test.py
@@ -10,16 +10,35 @@ import yaml
 
 
 class DataTest(unittest.TestCase):
-    def testDataDotYaml(self):
 
+    admins = None
+
+    @classmethod
+    def setUpClass(cls):
         with open(os.path.join(os.path.dirname(__file__), 'data.yaml')) as f:
-            admins = yaml.safe_load(f)
+            cls.admins = yaml.safe_load(f)
 
+    def testGroupGIDSAreUniques(self):
         # ensure no two groups uses the same gid
         gids = filter(None, [
-            v.get('gid', None) for k, v in admins['groups'].iteritems()])
+            v.get('gid', None) for k, v in self.admins['groups'].iteritems()])
         dupes = [k for k, v in Counter(gids).items() if v > 1]
         self.assertEqual([], dupes, 'Duplicate group GIDs: %r' % dupes)
+
+    def testAbsentMembers(self):
+        absent_members = set(self.admins['groups']['absent']['members'])
+        absentees = set([
+            username
+            for username, val in self.admins['users'].items()
+            if val['ensure'] == 'absent'
+            ])
+        self.maxDiff = None
+        self.longMessage = True
+        self.assertSetEqual(
+            absent_members,
+            absentees,
+            'Absent users are both in "absent" group (first set)'
+            'and in marked "ensure: absent" (second set)')
 
 
 if __name__ == '__main__':
