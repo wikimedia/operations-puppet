1c3635215e2decbaf6cdd393364806f6553ca185
Luca Toscano <ltoscano@wikimedia.org>
2019-01-08 16:37:39 +0100

    profile::hive::site_hdfs: move exec to cdh::exec
    Change-Id: I950aa1547f797ba2c94d2ddc38c357d61f7f128f

--- modules/profile/manifests/hive/site_hdfs.pp
+++ modules/profile/manifests/hive/site_hdfs.pp
@@ -7,16 +7,19 @@
 # filesystem.  If we figure that out, we can replace this class and also
 # the analytics_cluster::mysql_password class.
 #
-class profile::hive::site_hdfs {
+class profile::hive::site_hdfs (
+    $use_kerberos = hiera('profile::hive::site_hdfs::use_kerberos', false),
+){
     Class['cdh::hive'] -> Class['profile::hive::site_hdfs']
 
     $hdfs_path = '/user/hive/hive-site.xml'
     # Put /etc/hive/conf/hive-site.xml in HDFS whenever puppet
     # notices that it has changed.
-    exec { 'put-hive-site-in-hdfs':
-        command     => "/usr/bin/hdfs dfs -put -f ${cdh::hive::config_directory}/hive-site.xml ${hdfs_path} && /usr/bin/hdfs dfs -chmod 644 ${hdfs_path} && /usr/bin/hdfs dfs -chown hdfs:hdfs ${hdfs_path}",
-        user        => 'hdfs',
-        refreshonly => true,
-        subscribe   => File["${cdh::hive::config_directory}/hive-site.xml"],
+    cdh::exec { 'put-hive-site-in-hdfs':
+        command      => "/usr/bin/hdfs dfs -put -f ${cdh::hive::config_directory}/hive-site.xml ${hdfs_path} && /usr/bin/hdfs dfs -chmod 644 ${hdfs_path} && /usr/bin/hdfs dfs -chown hdfs:hdfs ${hdfs_path}",
+        user         => 'hdfs',
+        refreshonly  => true,
+        subscribe    => File["${cdh::hive::config_directory}/hive-site.xml"],
+        use_kerberos => $use_kerberos,
     }
 }
