5614cce1b9d13c62cf4d13b6906bea239f1b6520
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-01-09 11:12:51 +0100

    openstack: virt: dont install libssl1.0.0 in mitaka/stretch
    Make sure we don't install libssl1.0.0 from jessie-backports.
    
    Change-Id: I214328a07b4d0bb9737c7541ca2f246aaff1d9ba
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/openstack/manifests/serverpackages/mitaka/stretch.pp
+++ modules/openstack/manifests/serverpackages/mitaka/stretch.pp
@@ -15,4 +15,22 @@ class openstack::serverpackages::mitaka::stretch(
         dist       => 'jessie',
         components => 'main',
     }
+
+    # make sure we don't have libssl1.0.0 installed, and exclude
+    # packages that depend on it
+    package { 'libssl1.0.0':
+        ensure => 'absent',
+    }
+
+    apt::pin { 'mitaka_stretch_libpq5_nojessiebpo':
+        package  => 'libpq5',
+        pin      => 'version 9.6.6-0*',
+        priority => '-1',
+    }
+
+    apt::pin { 'mitaka_stretch_libisns0_nojessiebpo':
+        package  => 'libisns0',
+        pin      => 'version 0.97-1*',
+        priority => '-1',
+    }
 }
