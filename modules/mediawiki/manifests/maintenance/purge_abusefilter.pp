f1f126a888ed67a11e2e971e895113419ddf14ed
MarcoAurelio <maurelio@tools.wmflabs.org>
2019-01-11 23:07:10 +0000

    mediawiki: Stop logging each run of purge_abusefilter.pp
    After the script was fixed from its broken status for some years, it
    seemed wise to log each run afterwards to detect issues with
    the maintenance script (cfr. I3544d91a). After some time without
    apparent issues, let's just use the standard configuration we use for
    all cronned maintenance scripts, that is, don't save its logs.
    
    Hosts: mwmaint1002.eqiad.wmnet, mwmaint2001.codfw.wmnet
    
    Bug: T213591
    Change-Id: Iab855e35b54907a354ef357f352c230fe3042864

--- modules/mediawiki/manifests/maintenance/purge_abusefilter.pp
+++ modules/mediawiki/manifests/maintenance/purge_abusefilter.pp
@@ -1,7 +1,7 @@
 class mediawiki::maintenance::purge_abusefilter( $ensure = present ) {
     cron { 'purge_abusefilteripdata':
         ensure  => $ensure,
-        command => '/usr/local/bin/foreachwiki extensions/AbuseFilter/maintenance/purgeOldLogIPData.php >/var/log/mediawiki/purge_abusefilter.log 2>&1',
+        command => '/usr/local/bin/foreachwiki extensions/AbuseFilter/maintenance/purgeOldLogIPData.php >/dev/null 2>&1',
         user    => $::mediawiki::users::web,
         hour    => 1,
         minute  => 15,
