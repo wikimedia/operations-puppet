7eb541e584b7d8b52f63bb936678e63f87991bb4
Mathew Onipe <monipe@wikimedia.org>
2019-01-14 16:36:45 +0100

    elasticsearch: mask default exporter service
    Mask prometheus-elasticsearch-exporter which is the default
    elasticsearch exporter. This will prevent postinstl
    from restarting this unit when doing upgrades.
    This should also fix T210597
    
    Bug: T210592
    Change-Id: I9748c94ad598854ebdd6c7c40a60a9648ba02389

--- modules/prometheus/manifests/elasticsearch_exporter/common.pp
+++ modules/prometheus/manifests/elasticsearch_exporter/common.pp
@@ -1,9 +1,11 @@
 class prometheus::elasticsearch_exporter::common {
     ensure_packages('prometheus-elasticsearch-exporter')
     # We will install per-cluster systemd units instead
+    # mask the default service to make sure its not restarted on package upgrades
     service { 'prometheus-elasticsearch-exporter':
         ensure  => 'stopped',
         require => Package['prometheus-elasticsearch-exporter'],
+        enable  => 'mask',
     }
     # Remove so there is no confusion about if it's referenced
     file { '/etc/default/prometheus-elasticsearch-exporter':
