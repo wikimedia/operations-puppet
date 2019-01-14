017e84218fc8ccd32197fadeb6df580ab18a7c4c
Daniel Zahn <dzahn@wikimedia.org>
2019-01-14 15:43:10 -0800

    contint: delete unused doc.wikimedia.org site config
    This has been replaced by doc1001 using the puppet class
    modules/profile/templates/doc/httpd-doc.wikimedia.org.erb.
    
    This version of it is installed on contint servers but not serving
    any traffic anymore since varnish/trafficserver has switched backends.
    
    Delete the duplicate unused code.
    Remove legacy directories that are no more used.
    
    Hosts: contint1001.wikimedia.org
    
    Bug: T137890
    Change-Id: Ic8ef2dff5f512873dd16fcf99878f2bc3d83453a

--- modules/profile/manifests/ci/website.pp
+++ modules/profile/manifests/ci/website.pp
@@ -1,7 +1,5 @@
 # Website for Continuous integration
 #
-# http://doc.mediawiki.org/
-# http://doc.wikimedia.org/
 # http://integration.mediawiki.org/
 # http://integration.wikimedia.org/
 class profile::ci::website {
@@ -14,12 +12,6 @@ class profile::ci::website {
     # Apache configuration for integration.mediawiki.org
     httpd::site { 'integration.mediawiki.org':
         content => template('contint/apache/integration.mediawiki.org.erb'),
-    }
-
-    # Apache configuration for doc.wikimedia.org
-    # Will be superseeded by profile::doc
-    httpd::site { 'doc.wikimedia.org':
-        content => template('contint/apache/doc.wikimedia.org.erb'),
     }
 
     # Static files in these docroots are in integration/docroot.git
@@ -38,30 +30,6 @@ class profile::ci::website {
         group  => 'jenkins-slave',
     }
     file { '/srv/org/wikimedia/integration':
-        ensure => directory,
-        mode   => '0775',
-        owner  => 'jenkins-slave',
-        group  => 'jenkins-slave',
-    }
-    # MediaWiki code coverage
-    file { '/srv/org/wikimedia/integration/coverage':
-        ensure => directory,
-        mode   => '0775',
-        owner  => 'jenkins-slave',
-        group  => 'jenkins-slave',
-    }
-
-    # Jenkins console logs
-    file { '/srv/org/wikimedia/integration/logs':
-        ensure => directory,
-        mode   => '0775',
-        owner  => 'jenkins-slave',
-        group  => 'jenkins-slave',
-    }
-
-    # Written to by jenkins for automatically generated
-    # documentations
-    file { '/srv/org/wikimedia/doc':
         ensure => directory,
         mode   => '0775',
         owner  => 'jenkins-slave',
