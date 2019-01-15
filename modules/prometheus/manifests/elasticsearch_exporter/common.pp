caa1176173c4398905ae40c9607b83cc2f570821
Mathew Onipe <monipe@wikimedia.org>
2019-01-15 10:44:03 +0100

    elasticsearch: mask default exporter
    Bug: T210592
    Change-Id: I0e00b7358575fb61cfe8eeef8f2ca96f07bab48a

--- modules/prometheus/manifests/elasticsearch_exporter/common.pp
+++ modules/prometheus/manifests/elasticsearch_exporter/common.pp
@@ -9,4 +9,10 @@ class prometheus::elasticsearch_exporter::common {
     file { '/etc/default/prometheus-elasticsearch-exporter':
         ensure => absent,
     }
+    # mask the default service to make sure its not restarted on package upgrades
+    # Also clean up this and use the mask param for service when logstash servers have been upgraded to stretch
+    exec { 'mask_default_prometheus_elasticsearch_exporter':
+        command => '/bin/systemctl mask prometheus-elasticsearch-exporter.service',
+        creates => '/etc/systemd/system/prometheus-elasticsearch-exporter.service',
+    }
 }
