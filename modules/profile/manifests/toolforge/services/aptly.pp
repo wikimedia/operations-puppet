cfa170d39ed87d7dd3e8c70f5460d1a848347ea4
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-01-16 14:21:15 +0100

    toolforge: services: sync aptly repo
    We usually build the services nodes in pairs, using a cold-standby approach.
    But we don't sync the aptly data between them, we currenly only do backups to
    NFS. Let's improve a bit the situation with a rsync-based sync of the aptly
    data.
    
    Bug: T213917
    Change-Id: Ibc9a53e07956cc1d49d3443c92b927be7b5bd7e6
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/toolforge/services/aptly.pp
+++ modules/profile/manifests/toolforge/services/aptly.pp
@@ -1,9 +1,20 @@
 class profile::toolforge::services::aptly(
+    $active_node = lookup('profile::toolforge::services::active_node'),
+    $standby_node = lookup('profile::toolforge::services::standby_node'),
 ) {
     aptly::repo { 'stretch-tools':
         publish      => true,
     }
     aptly::repo { 'jessie-tools':
         publish      => true,
+    }
+
+    # make sure we have a backup server ready to take over
+    rsync::quickdatacopy { 'aptly-sync':
+        ensure      => present,
+        auto_sync   => true,
+        source_host => $active_node,
+        dest_host   => $standby_node,
+        module_path => '/srv/packages',
     }
 }
