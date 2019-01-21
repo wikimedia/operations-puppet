720ef6113bb94d68e979bd5351e161c112dfd529
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2019-01-21 12:52:21 +0100

    prometheus: install wmcs_scripts dependencies from jessie-backports
    Bug: T214058
    Change-Id: I21d6e21d58f3eda2b67f44fb6ae759bfd6c6830f

--- modules/prometheus/manifests/wmcs_scripts.pp
+++ modules/prometheus/manifests/wmcs_scripts.pp
@@ -4,7 +4,18 @@
 # for Prometheus to pick up.
 
 class prometheus::wmcs_scripts {
-    require_package(['python3-yaml', 'python3-novaclient', 'python3-keystoneclient'])
+    require_package('python3-yaml')
+
+    if os_version('debian == jessie') {
+        $install_options = ['-t', 'jessie-backports']
+    } else {
+        $install_options = ''
+    }
+
+    package { ['python3-keystoneclient', 'python3-novaclient']:
+        ensure          => 'present',
+        install_options => $install_options,
+    }
 
     # output all nova instances for the current labs project as prometheus
     # 'targets'
