657a5d3fd2cd24b680438758f933a2114c78ce1e
Daniel Zahn <dzahn@wikimedia.org>
2019-01-22 11:26:40 -0800

    wikistats: cron job minutes are integers, not strings now
    follow-up to I6bbb7946d9b7e885e941
    
    The minutes when cron jobs run are now integers and not strings.
    
    Change-Id: I8a328546e8e70ed074118753f09f126b2467831d

--- modules/wikistats/manifests/updates.pp
+++ modules/wikistats/manifests/updates.pp
@@ -61,34 +61,34 @@ class wikistats::updates (
 
     # dump xml data: usage: <project prefix>@<hour>
     wikistats::cronjob::xmldump {
-        'wp' : db_pass => $db_pass, table => 'wikipedias',   minute => '3';
-        'wt' : db_pass => $db_pass, table => 'wiktionaries', minute => '5';
-        'wq' : db_pass => $db_pass, table => 'wikiquotes', minute => '7';
-        'wb' : db_pass => $db_pass, table => 'wikibooks', minute => '9';
-        'wn' : db_pass => $db_pass, table => 'wikinews', minute => '11';
-        'ws' : db_pass => $db_pass, table => 'wikisources', minute => '13';
-        'wy' : db_pass => $db_pass, table => 'wikivoyage', minute => '15';
-        'wx' : db_pass => $db_pass, table => 'wmspecials', minute => '17';
-        'et' : db_pass => $db_pass, table => 'editthis', minute => '23';
-        'wr' : db_pass => $db_pass, table => 'wikitravel', minute => '25';
-        'mw' : db_pass => $db_pass, table => 'mediawikis', minute => '32';
-        'mt' : db_pass => $db_pass, table => 'metapedias', minute => '37';
-        'sc' : db_pass => $db_pass, table => 'scoutwiki', minute => '39';
-        'os' : db_pass => $db_pass, table => 'opensuse', minute => '41';
-        'un' : db_pass => $db_pass, table => 'uncyclomedia', minute => '43';
-        'wf' : db_pass => $db_pass, table => 'wikifur', minute => '45';
-        'an' : db_pass => $db_pass, table => 'anarchopedias', minute => '47';
-        'si' : db_pass => $db_pass, table => 'wikisite', minute => '51';
-        'ne' : db_pass => $db_pass, table => 'neoseeker', minute => '53';
-        'wv' : db_pass => $db_pass, table => 'wikiversity', minute => '34';
-        're' : db_pass => $db_pass, table => 'referata', minute => '57';
-        'ro' : db_pass => $db_pass, table => 'rodovid', minute => '1';
-        'lx' : db_pass => $db_pass, table => 'lxde', minute => '59';
-        'sw' : db_pass => $db_pass, table => 'shoutwiki', minute => '36';
-        'w3' : db_pass => $db_pass, table => 'w3cwikis', minute => '27';
-        'ga' : db_pass => $db_pass, table => 'gamepedias', minute => '29';
-        'sf' : db_pass => $db_pass, table => 'sourceforge', minute => '24';
-        'mh' : db_pass => $db_pass, table => 'miraheze', minute => '6';
+        'wp' : db_pass => $db_pass, table => 'wikipedias',   minute => 3;
+        'wt' : db_pass => $db_pass, table => 'wiktionaries', minute => 5;
+        'wq' : db_pass => $db_pass, table => 'wikiquotes', minute => 7;
+        'wb' : db_pass => $db_pass, table => 'wikibooks', minute => 9;
+        'wn' : db_pass => $db_pass, table => 'wikinews', minute => 11;
+        'ws' : db_pass => $db_pass, table => 'wikisources', minute => 13;
+        'wy' : db_pass => $db_pass, table => 'wikivoyage', minute => 15;
+        'wx' : db_pass => $db_pass, table => 'wmspecials', minute => 1;
+        'et' : db_pass => $db_pass, table => 'editthis', minute => 23;
+        'wr' : db_pass => $db_pass, table => 'wikitravel', minute => 25;
+        'mw' : db_pass => $db_pass, table => 'mediawikis', minute => 32;
+        'mt' : db_pass => $db_pass, table => 'metapedias', minute => 37;
+        'sc' : db_pass => $db_pass, table => 'scoutwiki', minute => 39;
+        'os' : db_pass => $db_pass, table => 'opensuse', minute => 41;
+        'un' : db_pass => $db_pass, table => 'uncyclomedia', minute => 43;
+        'wf' : db_pass => $db_pass, table => 'wikifur', minute => 45;
+        'an' : db_pass => $db_pass, table => 'anarchopedias', minute => 47;
+        'si' : db_pass => $db_pass, table => 'wikisite', minute => 51;
+        'ne' : db_pass => $db_pass, table => 'neoseeker', minute => 53;
+        'wv' : db_pass => $db_pass, table => 'wikiversity', minute => 34;
+        're' : db_pass => $db_pass, table => 'referata', minute => 57;
+        'ro' : db_pass => $db_pass, table => 'rodovid', minute => 1;
+        'lx' : db_pass => $db_pass, table => 'lxde', minute => 59;
+        'sw' : db_pass => $db_pass, table => 'shoutwiki', minute => 36;
+        'w3' : db_pass => $db_pass, table => 'w3cwikis', minute => 27;
+        'ga' : db_pass => $db_pass, table => 'gamepedias', minute => 29;
+        'sf' : db_pass => $db_pass, table => 'sourceforge', minute => 24;
+        'mh' : db_pass => $db_pass, table => 'miraheze', minute => 6;
     }
 
     # imports (fetching lists of wikis itself) usage: <project name>@<weekday>
