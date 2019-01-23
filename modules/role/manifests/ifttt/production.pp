02940c86768c02f4189fba77b5aed813c3c0495c
Daniel Zahn <dzahn@wikimedia.org>
2019-01-23 10:31:36 -0800

    delete ifttt roles and module
    These roles and the module they use do not appear anywhere
    in production and neither show up in:
    
    https://tools.wmflabs.org/openstack-browser/puppetclass/
    
    That should mean it's not used. (unless there is a private puppetmaster
    somwhere?) and we want to avoid maintaining unused code.
    
    Change-Id: I60a3403ff6fbb971e6195d948f2e23346d7da5b0

--- modules/role/manifests/ifttt/production.pp
+++
@@ -1,9 +0,0 @@
-# == Class role::ifttt::production
-# This is the production specific ifttt role
-class role::ifttt::production {
-    include ::ifttt::base
-
-    class { '::ifttt::web':
-        workers_per_core => 4,
-    }
-}
--- modules/role/manifests/ifttt/staging.pp
+++
@@ -1,9 +0,0 @@
-# == Class role::ifttt::staging
-# This is the staging specific ifttt role
-class role::ifttt::staging {
-    include ::ifttt::base
-
-    class { '::ifttt::web':
-        workers_per_core => 1,
-    }
-}
