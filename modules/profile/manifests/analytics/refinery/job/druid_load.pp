7753d9c55adf8e6c232a5547d84cd65c7c117a10
Marcel Ruiz Forns <mforns@wikimedia.org>
2019-01-23 19:28:58 +0100

    Normalize field names in eventlogging_to_druid_job calls
    It seems some field names are not recognized any more.
    I believe they are named differently by Refine process.
    By normalizing them here, the Druid ingestion works again.
    
    Bug: T214136
    Change-Id: I3f99953ca7166be8935faf943f0dc74657543466

--- modules/profile/manifests/analytics/refinery/job/druid_load.pp
+++ modules/profile/manifests/analytics/refinery/job/druid_load.pp
@@ -17,7 +17,7 @@ class profile::analytics::refinery::job::druid_load {
     # Load event.NavigationTiming
     profile::analytics::refinery::job::eventlogging_to_druid_job { 'navigationtiming':
         job_config => {
-            dimensions    => 'event.action,event.isAnon,event.isOversample,event.mediaWikiVersion,event.mobileMode,event.namespaceId,event.netinfoEffectiveConnectionType,event.originCountry,recvFrom,revision,userAgent.browser_family,userAgent.browser_major,userAgent.device_family,userAgent.is_bot,userAgent.os_family,userAgent.os_major,wiki',
+            dimensions    => 'event.action,event.isAnon,event.isOversample,event.mediaWikiVersion,event.mobileMode,event.namespaceId,event.netinfoEffectiveConnectionType,event.originCountry,recvFrom,revision,useragent.browser_family,useragent.browser_major,useragent.device_family,useragent.is_bot,useragent.os_family,useragent.os_major,wiki',
             time_measures => 'event.connectEnd,event.connectStart,event.dnsLookup,event.domComplete,event.domInteractive,event.fetchStart,event.firstPaint,event.loadEventEnd,event.loadEventStart,event.redirecting,event.requestStart,event.responseEnd,event.responseStart,event.secureConnectionStart,event.unload,event.gaps,event.mediaWikiLoadEnd,event.RSI',
         },
     }
@@ -25,7 +25,7 @@ class profile::analytics::refinery::job::druid_load {
     # Load event.ReadingDepth
     profile::analytics::refinery::job::eventlogging_to_druid_job { 'readingdepth':
         job_config => {
-            dimensions    => 'event.action,event.default_sample,event.isAnon,event.namespaceId,event.skin,revision,userAgent.browser_family,userAgent.browser_major,userAgent.browser_minor,userAgent.device_family,userAgent.is_bot,userAgent.os_family,userAgent.os_major,userAgent.os_minor,wiki,event.page-issues-a_sample,event.page-issues-b_sample',
+            dimensions    => 'event.action,event.default_sample,event.isAnon,event.namespaceId,event.skin,revision,useragent.browser_family,useragent.browser_major,useragent.browser_minor,useragent.device_family,useragent.is_bot,useragent.os_family,useragent.os_major,useragent.os_minor,wiki,event.page_issues_a_sample,event.page_issues_b_sample',
             time_measures => 'event.domInteractiveTime,event.firstPaintTime,event.totalLength,event.visibleLength',
         },
     }
@@ -35,7 +35,7 @@ class profile::analytics::refinery::job::druid_load {
     profile::analytics::refinery::job::eventlogging_to_druid_job { 'pageissues':
         ensure     => 'absent',
         job_config => {
-            dimensions => 'event.action,event.editCountBucket,event.isAnon,event.issuesSeverity,event.issuesVersion,event.namespaceId,event.sectionNumbers,revision,wiki,userAgent.browser_family,userAgent.browser_major,userAgent.browser_minor,userAgent.device_family,userAgent.is_bot,userAgent.os_family,userAgent.os_major,userAgent.os_minor',
+            dimensions => 'event.action,event.editCountBucket,event.isAnon,event.issuesSeverity,event.issuesVersion,event.namespaceId,event.sectionNumbers,revision,wiki,useragent.browser_family,useragent.browser_major,useragent.browser_minor,useragent.device_family,useragent.is_bot,useragent.os_family,useragent.os_major,useragent.os_minor',
         },
     }
 }
