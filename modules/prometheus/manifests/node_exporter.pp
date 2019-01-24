f814bac67cf80bf3c454835b5f7bfdb659ec283c
Cole White <cwhite@wikimedia.org>
2019-01-23 17:02:31 -0700

    prometheus: upgrade to node-exporter 0.17
    sets up a flag to perform the node-exporter upgrade.  should also be able to
    gracefully rollback
    
    Bug: T213708
    Change-Id: Ib1468d77c742dfd8ea798c99d6124c635783fad9

--- modules/prometheus/manifests/node_exporter.pp
+++ modules/prometheus/manifests/node_exporter.pp
@@ -17,33 +17,87 @@
 # [*$ignored_mount_points*]
 #  Regular expression to exclude mount points from being reported
 #
+# [*$netstat_fields*]
+#  Regular expression of netstat fields to include
+#
+# [*vmstat_fields*]
+#  Regular expression of vmstat fields to include
+#
 # [*$collectors_extra*]
 #  List of extra collectors to be enabled.
 #
 # [*$web_listen_address*]
 #  IP:Port combination to listen on
 #
-#  Available collectors: (from "prometheus-node-exporter -collectors.print")
-#  bonding diskstats filefd filesystem gmond interrupts ipvs lastlogin loadavg
-#  mdadm megacli meminfo netdev netstat ntp runit sockstat stat supervisord
-#  tcpstat textfile time uname
+# [*upgrade_one_seven*]
+#  A feature flag to run the prometheus-node-exporter upgrade to 0.17.
+#
+#  Available collectors: https://github.com/prometheus/node_exporter/tree/v0.17.0#collectors
 
 
 class prometheus::node_exporter (
     String $ignored_devices  = '^(ram|loop|fd|(h|s|v|xv)d[a-z]|nvme\\\\d+n\\\\d+p)\\\\d+$',
     String $ignored_fs_types  = '^(overlay|autofs|binfmt_misc|cgroup|configfs|debugfs|devpts|devtmpfs|fusectl|hugetlbfs|mqueue|nsfs|proc|procfs|pstore|rpc_pipefs|securityfs|sysfs|tracefs)$',
     String $ignored_mount_points  = '^/(sys|proc|dev|var/lib/docker|var/lib/kubelet)($|/)',
+    String $netstat_fields = '^(.*)',
+    String $vmstat_fields = '^(.*)',
     Array[String] $collectors_extra = [],
     String $collector_ntp_server = '127.0.0.1',
     Pattern[/:\d+$/] $web_listen_address = ':9100',
+    Boolean $upgrade_one_seven = false,
 ) {
-    require_package('prometheus-node-exporter')
-
     $collectors_default = ['buddyinfo', 'conntrack', 'diskstats', 'entropy', 'edac', 'filefd', 'filesystem', 'hwmon',
         'loadavg', 'mdadm', 'meminfo', 'netdev', 'netstat', 'sockstat', 'stat', 'tcpstat',
         'textfile', 'time', 'uname', 'vmstat']
     $textfile_directory = '/var/lib/prometheus/node.d'
-    $collectors_enabled = join(sort(concat($collectors_default, $collectors_extra)), ',')
+
+    if ($upgrade_one_seven or os_version('debian == buster')) {
+        $collectors_enabled = concat($collectors_default, $collectors_extra)
+        if (os_version('debian <= stretch')) {
+            apt::repository { 'component-prometheus-node-exporter':
+                uri        => 'http://apt.wikimedia.org/wikimedia',
+                dist       => "${::lsbdistcodename}-wikimedia",
+                components => 'component/prometheus-node-exporter',
+                before     => Package['prometheus-node-exporter'],
+            }
+        }
+
+        package { 'prometheus-node-exporter':
+            ensure  => '0.17.0+ds-2',
+            require => Exec['apt-get update'],
+        }
+
+        file { '/etc/default/prometheus-node-exporter':
+            ensure  => present,
+            mode    => '0444',
+            owner   => 'root',
+            group   => 'root',
+            content => template('prometheus/etc/default/prometheus-node-exporter-0.17.erb'),
+            notify  => Service['prometheus-node-exporter'],
+        }
+    } else {
+        $collectors_enabled = join(sort(concat($collectors_default, $collectors_extra)), ',')
+
+        package { 'prometheus-node-exporter':
+            ensure => '0.14.0~git20170523-1'
+        }
+
+        apt::repository { 'component-prometheus-node-exporter':
+            ensure     => 'absent',
+            uri        => '',
+            dist       => '',
+            components => '',
+        }
+
+        file { '/etc/default/prometheus-node-exporter':
+            ensure  => present,
+            mode    => '0444',
+            owner   => 'root',
+            group   => 'root',
+            content => template('prometheus/etc/default/prometheus-node-exporter.erb'),
+            notify  => Service['prometheus-node-exporter'],
+        }
+    }
 
     # members of this group are able to publish metrics
     # via the 'textfile' collector by writing files to $textfile_directory.
@@ -59,15 +113,6 @@ class prometheus::node_exporter (
         group   => 'prometheus-node-exporter',
         require => [Package['prometheus-node-exporter'],
                     Group['prometheus-node-exporter']],
-    }
-
-    file { '/etc/default/prometheus-node-exporter':
-        ensure  => present,
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        content => template('prometheus/etc/default/prometheus-node-exporter.erb'),
-        notify  => Service['prometheus-node-exporter'],
     }
 
     base::service_unit { 'prometheus-node-exporter':
