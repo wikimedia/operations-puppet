403e13f993663174e79ccb9225726c93a7547c2e
John Bond <jbond@wikimedia.org>
2019-01-24 15:08:51 +0000

    Add apt pinning for buster
    With the introduction of debian buster we will move to using puppet 5.10
    as provided in the buster main repo.  however we are still relient on
    facter 2.4.  This patch updates the manifest to ensure facter and puppet
    are pinned correctly.
    
    This change will change the behaviour of pinning on stretch as currently
    we pin to release = $facts['lsbdistcodename'] which is redundent
    
    Also remove `puppet_major_version` from the puppet files.  the variable
    is now only used in puppet master related modules, profiles and roles,
    including standalon and openstack componets.
    
    Change-Id: I8f65b5ce8f8617bfab20d05f935170ae0f83575b

--- modules/base/manifests/puppet.pp
+++ modules/base/manifests/puppet.pp
@@ -3,7 +3,6 @@ class base::puppet(
     $certname=undef,
     $dns_alt_names=undef,
     $environment=undef,
-    $puppet_major_version=undef,
 ) {
     include ::passwords::puppet::database
     include ::base::puppet::params
@@ -14,9 +13,7 @@ class base::puppet(
     $ca_server = hiera('puppetmaster::ca_server', '')
 
 
-    if $puppet_major_version == 4 {
-        include base::puppet::puppet4
-    }
+    include base::puppet::pins
 
     package { [ 'puppet', 'facter' ]:
         ensure => present,
