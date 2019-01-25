02f9919db634cc77063be8042492169cc2cfd992
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2019-01-25 10:08:47 +0100

    maps: re-enable OSM lag check
    Now that maps master has been fully switched to maps1004, we can re-enable
    this check.
    
    Bug: T198622
    Change-Id: I7827226dd22b0ef7ec0ac933fa3fe36d44ec5a16

--- modules/profile/manifests/maps/alerts.pp
+++ modules/profile/manifests/maps/alerts.pp
@@ -13,11 +13,10 @@ class profile::maps::alerts {
         percentage      => 90,
     }
 
-    # Temporarily restrict this check to only maps1001, untill data is fully imported on maps1004 (T205462)
     monitoring::check_prometheus { 'maps-osm-sync-lag-eqiad':
       description     => 'Maps - OSM synchronization lag - eqiad',
       dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/maps-performances?panelId=11&fullscreen&orgId=1'],
-      query           => 'scalar(max(time()-osm_sync_timestamp{cluster="maps", instance="maps1001:9100"}))',
+      query           => 'scalar(max(time()-osm_sync_timestamp{cluster="maps"}))',
       warning         => 49 * 3600, # 49 hours
       critical        => 3 * 24 * 3600, # 3 days
       prometheus_url  => 'http://prometheus.svc.eqiad.wmnet/ops',
