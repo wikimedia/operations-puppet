93bf1a4c25962e239f798a814f317118fb1cb698
Luca Toscano <ltoscano@wikimedia.org>
2019-01-31 15:29:13 -0800

    Fix ports for wmcs/labs' Prometheus Memcached exporters
    We are currently not collecting any metrics from these memcached
    clusters since the default port of the memcached class is 11000,
    meanwhile the prometheus exporter has 11211.
    
    Change-Id: I6ff50ae74987340c0e48d26c7ec6feedf717bdbe

--- modules/profile/manifests/prometheus/memcached_exporter.pp
+++ modules/profile/manifests/prometheus/memcached_exporter.pp
@@ -1,7 +1,10 @@
 class profile::prometheus::memcached_exporter (
     $prometheus_nodes = hiera('prometheus_nodes'),
+    $arguments = hiera('profile::prometheus::memcached_exporter', ''),
 ) {
-    prometheus::memcached_exporter { 'default': }
+    prometheus::memcached_exporter { 'default':
+        arguments => $arguments,
+    }
     $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
     $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
 
