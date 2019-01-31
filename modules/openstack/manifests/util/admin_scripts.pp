b3c17f4c3b9d98d0e8dc7b06353ede69f4853b70
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-01-31 17:35:35 +0100

    wmcs: prefix all of our customs scripts with 'wmcs-'
    When doing operations, a way to quickly identify available commands is by using
    the tab auto-completion feature of the shell. If we prefix all of our custom
    scripts with the same string, a couple of keystrokes will show all of our
    available options, which may increase a bit our overview of common operations
    on a server or a cluster. Also, having a consistent namespace for commands
    seems a good idea itself.
    
    Also, we inconsistently deployed scripts in several different filesystem paths,
    while there is usually a specific place for this: /usr/local/[s]bin/.
    So, while at the renaming, reallocate scripts. Also add .py extension to python
    scripts to trigger CI linters.
    
    I may understand people don't want some of the changes proposed by this patch,
    because it may change some already established workflows. I agree with that
    and I will have no problem if we abandon/revert/withdraw this change.
    
    Example of changes:
     * in puppet:
    	modules/openstack/files/mitaka/admin_scripts/cold-migrate --->
    	modules/openstack/files/mitaka/admin_scripts/wmcs-cold-migrate.py
     * in the resulting filesystem:
    	/root/cold-migrate --->
    	/usr/local/sbin/wmcs-cold-migrate
    
    This change requires a strong review of our documentation at wikitech. I will
    do once this commit is merged.
    
    Change-Id: Ibdc41abc37576a7d9e70f19b94cbdc742fbb3948
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/openstack/manifests/util/admin_scripts.pp
+++ modules/openstack/manifests/util/admin_scripts.pp
@@ -1,4 +1,4 @@
-# helper scripts for Labs openstack administration
+# helper scripts for CloudVPS openstack administration
 class openstack::util::admin_scripts(
     $version,
     ) {
@@ -11,141 +11,136 @@ class openstack::util::admin_scripts(
     }
 
     # Script to cold-migrate instances between compute nodes
-    file { '/root/cold-nova-migrate':
+    file { '/usr/local/sbin/wmcs-cold-nova-migrate':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/cold-nova-migrate",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/wmcs-cold-nova-migrate.py",
     }
 
     # Script to migrate from nova-network region to neutron region
     #  (hopefully this will only be needed transitionally)
-    file { '/root/region-migrate':
+    file { '/usr/local/sbin/wmcs-region-migrate':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/region-migrate",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/wmcs-region-migrate.py",
     }
-    file { '/root/region-migrate-security-groups':
+    file { '/usr/local/sbin/wmcs-region-migrate-security-groups':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/region-migrate-security-groups",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/wmcs-region-migrate-security-groups.py",
     }
 
     # Script to migrate (with suspension) instances between compute nodes
-    file { '/root/live-migrate':
+    file { '/usr/local/sbin/wmcs-live-migrate':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/live-migrate",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/wmcs-live-migrate.py",
     }
 
-    file { '/root/nova-quota-sync':
+    file { '/root/wmcs-nova-quota-sync':
         ensure => 'directory',
         owner  => 'root',
     }
 
     # Script to check and/or fix quotas.  With luck this won't be
     #  needed in Pike or later.
-    file { '/root/nova-quota-sync/nova-quota-sync':
+    file { '/usr/local/sbin/wmcs-nova-quota-sync':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/nova-quota-sync/nova-quota-sync",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/wmcs-nova-quota-sync/wmcs-nova-quota-sync.py",
     }
 
-    file { '/root/nova-quota-sync/readme.md':
+    file { '/root/wmcs-nova-quota-sync/readme.md':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0555',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/nova-quota-sync/readme.md",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/wmcs-nova-quota-sync/readme.md",
     }
 
     # Set up keystone services (example script)
-    file { '/root/prod-example.sh':
+    file { '/root/wmcs-prod-example.sh':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/prod.sh",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/wmcs-prod-example.sh",
     }
 
-    file { '/root/novastats':
-        ensure => 'directory',
-        owner  => 'root',
-    }
-
-    file { '/root/novastats/imagestats.py':
+    file { '/usr/local/sbin/wmcs-novastats-imagestats':
         ensure  => 'present',
         owner   => 'root',
         group   => 'root',
         mode    => '0755',
-        source  => "puppet:///modules/openstack/${version}/admin_scripts/novastats/imagestats.py",
-        require => File['/root/novastats'],
+        source  => "puppet:///modules/openstack/${version}/admin_scripts/novastats/wmcs-novastats-imagestats.py",
+        require => File['/usr/local/sbin/wmcs-novastats'],
     }
 
-    file { '/root/novastats/capacity.py':
+    file { '/usr/local/sbin/wmcs-novastats-capacity':
         ensure  => 'present',
         owner   => 'root',
         group   => 'root',
         mode    => '0755',
-        source  => "puppet:///modules/openstack/${version}/admin_scripts/novastats/capacity.py",
-        require => File['/root/novastats'],
+        source  => "puppet:///modules/openstack/${version}/admin_scripts/novastats/wmcs-novastats-capacity.py",
+        require => File['/usr/local/sbin/wmcs-novastats'],
     }
 
-    file { '/root/novastats/dnsleaks.py':
+    file { '/usr/local/sbin/wmcs-novastats-dnsleaks':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/novastats/dnsleaks.py",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/novastats/wmcs-novastats-dnsleaks.py",
     }
 
-    file { '/root/novastats/proxyleaks.py':
+    file { '/usr/local/sbin/wmcs-novastats-proxyleaks':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/novastats/proxyleaks.py",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/novastats/wmcs-novastats-proxyleaks.py",
     }
 
-    file { '/root/novastats/puppetleaks.py':
+    file { '/usr/local/sbin/wmcs-novastats-puppetleaks':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/novastats/puppetleaks.py",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/novastats/wmcs-novastats-puppetleaks.py",
     }
 
-    file { '/root/novastats/flavorreport.py':
+    file { '/usr/local/sbin/wmcs-novastats-flavorreport':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/novastats/flavorreport.py",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/novastats/wmcs-novastats-flavorreport.py",
     }
 
-    file { '/root/novastats/alltrusty.py':
+    file { '/usr/local/sbin/wmcs-novastats-alltrusty':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/novastats/alltrusty.py",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/novastats/wmcs-novastats-alltrusty.py",
     }
 
-    file { '/usr/local/sbin/wikitech-grep':
+    file { '/usr/local/sbin/wmcs-wikitech-grep':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/wikitech-grep.py",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/wmcs-wikitech-grep.py",
     }
 
     # XXX: per deployment?
@@ -158,12 +153,12 @@ class openstack::util::admin_scripts(
     # Script to rsync shutoff instances between compute nodes.
     #  This ignores most nova facilities so is a good last resort
     #  when nova is misbehaving.
-    file { '/root/cold-migrate':
+    file { '/usr/local/sbin/wmcs-cold-migrate':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => "puppet:///modules/openstack/${version}/admin_scripts/cold-migrate",
+        source => "puppet:///modules/openstack/${version}/admin_scripts/wmcs-cold-migrate.py",
     }
 
     # Script and config to maintain DNS records for *.db.svc.eqiad.wmflabs
@@ -177,16 +172,16 @@ class openstack::util::admin_scripts(
         source => 'puppet:///modules/openstack/util/wikireplica_dns.yaml',
     }
 
-    file { '/usr/local/sbin/wikireplica_dns':
+    file { '/usr/local/sbin/wmcs-wikireplica-dns':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => 'puppet:///modules/openstack/util/wikireplica_dns.py',
+        source => 'puppet:///modules/openstack/util/wmcs-wikireplica-dns.py',
     }
 
-    file { '/root/makedomain':
-        source => "puppet:///modules/openstack/${version}/admin_scripts/makedomain",
+    file { '/usr/local/sbin/wmcs-makedomain':
+        source => "puppet:///modules/openstack/${version}/admin_scripts/wmcs-makedomain.py",
         owner  => 'root',
         group  => 'root',
         mode   => '0744',
@@ -194,27 +189,58 @@ class openstack::util::admin_scripts(
 
     # Script to list, add, and delete dynamicproxy entries. Also updates
     # Designate managed DNS entries for the proxied hostname.
-    file { '/usr/local/sbin/webproxy':
+    file { '/usr/local/sbin/wmcs-webproxy':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => 'puppet:///modules/openstack/util/webproxy.py',
+        source => 'puppet:///modules/openstack/util/wmcs-webproxy.py',
     }
 
     # Script to reassign VPS proxies to use a different proxy IP
-    file { '/root/updateproxies':
+    file { '/usr/local/sbin/wmcs-updateproxies':
         ensure => 'present',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        source => 'puppet:///modules/openstack/util/updateproxies.py',
+        source => 'puppet:///modules/openstack/util/wmcs-updateproxies.py',
     }
 
-    file { '/usr/local/sbin/openstack-cvps':
-        source => 'puppet:///modules/openstack/util/openstack-cvps.sh',
+    file { '/usr/local/sbin/wmcs-openstack':
+        source => 'puppet:///modules/openstack/util/wmcs-openstack.sh',
         mode   => '0555',
         owner  => 'root',
         group  => 'root',
+    }
+
+    # rename cleanup
+    $files = [
+        '/root/cold-nova-migrate',
+        '/root/region-migrate',
+        '/root/region-migrate-security-groups',
+        '/root/live-migrate',
+        '/root/nova-quota-sync',
+        '/root/nova-quota-sync/nova-quota-sync',
+        '/root/nova-quota-sync/readme.md',
+        '/root/prod-example.sh',
+        '/root/novastats',
+        '/root/novastats/imagestats.py',
+        '/root/novastats/capacity.py',
+        '/root/novastats/dnsleaks.py',
+        '/root/novastats/proxyleaks.py',
+        '/root/novastats/puppetleaks.py',
+        '/root/novastats/flavorreport.py',
+        '/root/novastats/alltrusty.py',
+        '/usr/local/sbin/wikitech-grep',
+        '/root/cold-migrate',
+        '/usr/local/sbin/wikireplica_dns',
+        '/root/makedomain',
+        '/usr/local/sbin/webproxy',
+        '/root/updateproxies',
+        '/usr/local/sbin/openstack-cvps',
+    ]
+
+    file { $files:
+        ensure => 'absent',
     }
 }
