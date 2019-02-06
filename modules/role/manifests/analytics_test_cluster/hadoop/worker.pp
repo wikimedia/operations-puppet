a6f0d3f5cd6f78ca19400800536c32c9e5841b0b
Luca Toscano <ltoscano@wikimedia.org>
2019-02-06 13:10:53 +0100

    role::analytics_test_cluster::hadoop::worker: add hadoop users
    Bug: T212256
    Change-Id: I82e80fd912f42199d867d3f08fe904145cab2b2a

--- modules/role/manifests/analytics_test_cluster/hadoop/worker.pp
+++ modules/role/manifests/analytics_test_cluster/hadoop/worker.pp
@@ -6,6 +6,7 @@ class role::analytics_test_cluster::hadoop::worker {
         description => 'Hadoop Worker (DataNode & NodeManager)',
     }
     include ::profile::hadoop::worker
+    include ::profile::hadoop::users
     include ::profile::hadoop::logstash
     include ::profile::base::firewall
     include standard
