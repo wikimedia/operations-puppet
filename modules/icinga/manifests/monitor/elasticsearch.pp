4d40f067a82e8c3f84711898313b765b533cd37a
Gehel <guillaume.lederrey@wikimedia.org>
2019-02-06 15:11:04 +0000

    Revert "icinga: enable check for psi and omega clusters"
    _cluster/health endpoint is only available over HTTPS on the
    new psi and omega clusters
    
    This reverts commit a43f444456431a34151aeb0db228d43f23cacdb6.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I1ff96c9722fd25264e8de3bbf4973ec695e6dd43

--- modules/icinga/manifests/monitor/elasticsearch.pp
+++ modules/icinga/manifests/monitor/elasticsearch.pp
@@ -1,54 +1,87 @@
 # monitor the different elasticsearch clusters
 class icinga::monitor::elasticsearch {
 
-    $clusters = ['eqiad', 'codfw']
-    $instances = [
-        {'name' => 'search', 'http_port' => '9200'},
-        {'name' => 'search-omega', 'http_port' => '9400'},
-        {'name' => 'search-psi', 'http_port' => '9600'},
-    ]
+    $threshold = '>=0.15'
+    $http_port = 9200
 
-    $instances.each |$instance| {
-        $clusters.each |$cluster| {
-            monitoring::service { "elasticsearch shards - ${cluster}(${instance['name']}":
-                host          => "search.svc.${cluster}.wmnet",
-                check_command => "check_elasticsearch_shards_threshold!${instance['http_port']}!>=0.15",
-                description   => 'ElasticSearch health check for shards',
-                critical      => false,
-                contact_group => 'admins,team-discovery',
-            }
-
-            monitoring::service { "elasticsearch / cirrus frozen writes - ${cluster}(${instance['name']})":
-                host          => "search.svc.${cluster}.wmnet",
-                check_command => "check_cirrus_frozen_writes!${instance['http_port']}",
-                description   => 'ElasticSearch health check for frozen writes',
-                critical      => true,
-                contact_group => 'admins,team-discovery',
-            }
-
-            monitoring::service { "elasticsearch / shard size check - ${cluster}(${instance['name']})":
-                host           => "search.svc.${cluster}.wmnet",
-                check_command  => "check_elasticsearch_shard_size!${instance['http_port']}",
-                description    => 'ElasticSearch shard size check',
-                critical       => false,
-                check_interval => 1440, # 24h
-                retry_interval => 180, # 3h
-                retries        => 3,
-                contact_group  => 'admins,team-discovery',
-            }
-
-            monitoring::service { "elasticsearch / unassigned shard check - ${cluster}(${instance['name']})":
-                host           => "search.svc.${cluster}.wmnet",
-                check_command  => "check_elasticsearch_unassigned_shards!${instance['http_port']}",
-                description    => 'ElasticSearch unassigned shard size check',
-                critical       => false,
-                check_interval => 720, # 12h
-                retry_interval => 120, # 2h
-                retries        => 1,
-                notes_url      => "https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?panelId=64&fullscreen&orgId=1&var-cluster=${cluster}&var-exported_cluster=production-${instance['name']}-${cluster}'",
-                contact_group  => 'admins,team-discovery',
-            }
-        }
+    monitoring::service { 'elasticsearch shards - eqiad':
+        host          => 'search.svc.eqiad.wmnet',
+        check_command => "check_elasticsearch_shards_threshold!${http_port}!${threshold}",
+        description   => 'ElasticSearch health check for shards',
+        critical      => false,
+        contact_group => 'admins,team-discovery',
     }
+
+    monitoring::service { 'elasticsearch shards - codfw':
+        host          => 'search.svc.codfw.wmnet',
+        check_command => "check_elasticsearch_shards_threshold!${http_port}!${threshold}",
+        description   => 'ElasticSearch health check for shards',
+        critical      => false,
+        contact_group => 'admins,team-discovery',
+    }
+
+    monitoring::service { 'elasticsearch / cirrus frozen writes - eqiad':
+        host          => 'search.svc.eqiad.wmnet',
+        check_command => "check_cirrus_frozen_writes!${http_port}",
+        description   => 'ElasticSearch health check for frozen writes',
+        critical      => true,
+        contact_group => 'admins,team-discovery',
+    }
+
+    monitoring::service { 'elasticsearch / cirrus frozen writes - codfw':
+        host          => 'search.svc.codfw.wmnet',
+        check_command => "check_cirrus_frozen_writes!${http_port}",
+        description   => 'ElasticSearch health check for frozen writes',
+        critical      => true,
+        contact_group => 'admins,team-discovery',
+    }
+
+    # this check is throttled to reduce the noise from segment merges.
+    monitoring::service { 'elasticsearch / shard size check - eqiad':
+        host           => 'search.svc.eqiad.wmnet',
+        check_command  => "check_elasticsearch_shard_size!${http_port}",
+        description    => 'ElasticSearch shard size check',
+        critical       => false,
+        check_interval => 1440, # 24h
+        retry_interval => 180, # 3h
+        retries        => 3,
+        contact_group  => 'admins,team-discovery',
+    }
+
+    monitoring::service { 'elasticsearch / shard size check - codfw':
+        host           => 'search.svc.codfw.wmnet',
+        check_command  => "check_elasticsearch_shard_size!${http_port}",
+        description    => 'ElasticSearch shard size check',
+        critical       => false,
+        check_interval => 1440, # 24h
+        retry_interval => 180, # 3h
+        retries        => 3,
+        contact_group  => 'admins,team-discovery',
+    }
+
+    monitoring::service { 'elasticsearch / unassigned shard check - eqiad':
+        host           => 'search.svc.eqiad.wmnet',
+        check_command  => "check_elasticsearch_unassigned_shards!${http_port}",
+        description    => 'ElasticSearch unassigned shard size check',
+        critical       => false,
+        check_interval => 720, # 12h
+        retry_interval => 120, # 2h
+        retries        => 1,
+        notes_url      => 'https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?panelId=64&fullscreen&orgId=1&var-cluster=eqiad',
+        contact_group  => 'admins,team-discovery',
+    }
+
+    monitoring::service { 'elasticsearch / unassigned shard size check - codfw':
+        host           => 'search.svc.codfw.wmnet',
+        check_command  => "check_elasticsearch_unassigned_shards!${http_port}",
+        description    => 'ElasticSearch unassigned shard size check',
+        critical       => false,
+        check_interval => 720, # 12h
+        retry_interval => 120, # 2h
+        retries        => 1,
+        notes_url      => 'https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?panelId=64&fullscreen&orgId=1&var-cluster=codfw',
+        contact_group  => 'admins,team-discovery',
+    }
+
 
 }
