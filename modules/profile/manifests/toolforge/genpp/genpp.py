014bf59f1d867e12a68ae7b4ecba023f241a3b1b
bstorm <bstorm@wikimedia.org>
2019-02-05 16:25:53 -0800

    toolforge: apply black formatter before adding more python packages
    Just applying "black" formatting changes before moving python packages
    out of dev/exec environ and into the genpp framework for toolforge.
    
    This is purely to keep the formatting patch away from material changes.
    
    Bug: T210116
    Change-Id: I974be2c915a01f6b0e888fd8ede99c24a5e2fad5

--- modules/profile/manifests/toolforge/genpp/genpp.py
+++ modules/profile/manifests/toolforge/genpp/genpp.py
@@ -10,20 +10,20 @@ import logging
 
 from jinja2 import Environment, FileSystemLoader
 
-logger = logging.getLogger('genpp')
+logger = logging.getLogger("genpp")
 
 basedir = os.path.dirname(__file__)
 moduledir = basedir.rsplit("modules", 1)[0] + "modules"
 cachedir = os.path.join(basedir, ".cache")
 
 env = Environment(
-    loader=FileSystemLoader(os.path.join(basedir, 'jinja')),
-    keep_trailing_newline=True
+    loader=FileSystemLoader(os.path.join(basedir, "jinja")), keep_trailing_newline=True
 )
 
-env.filters['pline'] = lambda x: repr(x) + "," + (" " * (20 - len(x)))
-env.filters['pydictsort'] = lambda x: \
-    sorted(x.items(), key=lambda y: (y[0].split('-', 1)[1].lower(), y))
+env.filters["pline"] = lambda x: repr(x) + "," + (" " * (20 - len(x)))
+env.filters["pydictsort"] = lambda x: sorted(
+    x.items(), key=lambda y: (y[0].split("-", 1)[1].lower(), y)
+)
 
 
 try:
@@ -31,11 +31,13 @@ try:
 except OSError:
     pass
 
-releases = collections.OrderedDict([
-    ('trusty', 'http://packages.ubuntu.com/trusty'),
-    ('jessie', 'https://packages.debian.org/jessie'),
-    ('stretch', 'https://packages.debian.org/stretch')
-])
+releases = collections.OrderedDict(
+    [
+        ("trusty", "http://packages.ubuntu.com/trusty"),
+        ("jessie", "https://packages.debian.org/jessie"),
+        ("stretch", "https://packages.debian.org/stretch"),
+    ]
+)
 
 
 @functools.lru_cache()
@@ -46,14 +48,14 @@ def load_release(release):
         url = releases[release] + "/allpackages?format=txt.gz"
         subprocess.check_output(["wget", url, "-O", fn])
 
-    with gzip.open(fn, mode='rt', encoding='utf-8') as f:
+    with gzip.open(fn, mode="rt", encoding="utf-8") as f:
         # skip header
         for i in range(6):
             next(f)
         packages = {}
         for line in f:
             name, version, desc = line.strip().split(" ", 2)
-            version = version[1:].split('-')[0].split('+')[0]
+            version = version[1:].split("-")[0].split("+")[0]
             packages[name] = version
 
         return packages
@@ -76,26 +78,26 @@ def write_pp(module, release, environment, packages):
         module=module, env=environment, release=release
     )
     path = pp_path(class_)
-    template = env.get_template('packageclass.pp.jinja')
-    stream = template.stream({
-        'class': class_,
-        'command': " ".join(sys.argv),
-        'module': module,
-        'release': release,
-        'environment': environment,
-        'packages': packages,
-    })
-    stream.dump(open(path, 'w', encoding='utf-8'))
+    template = env.get_template("packageclass.pp.jinja")
+    stream = template.stream(
+        {
+            "class": class_,
+            "command": " ".join(sys.argv),
+            "module": module,
+            "release": release,
+            "environment": environment,
+            "packages": packages,
+        }
+    )
+    stream.dump(open(path, "w", encoding="utf-8"))
     logger.info("Wrote class {} to {}".format(class_, path))
 
 
 def write_report(module, environments):
     path = os.path.join(basedir, "report-{}.html".format(module))
-    template = env.get_template('report.html')
-    stream = template.stream({
-        'module': module,
-        'releases': releases,
-        'envs': environments
-    })
-    stream.dump(open(path, 'w', encoding='utf-8'))
+    template = env.get_template("report.html")
+    stream = template.stream(
+        {"module": module, "releases": releases, "envs": environments}
+    )
+    stream.dump(open(path, "w", encoding="utf-8"))
     logger.info("Wrote report to {}".format(path))
--- modules/profile/manifests/toolforge/genpp/python.py
+++ modules/profile/manifests/toolforge/genpp/python.py
@@ -8,76 +8,71 @@ import genpp
 import logging
 
 environ_packages = {
-    'dev': [
-        'cffi',
-        'coverage',
-        'dev',
-        'stdeb',
-    ],
-    'exec': [
-        'babel',                # T60220
-        'beautifulsoup',        # For valhallasw.
-        'bottle',               # T58995
-        'celery',
-        'cffi',                 # T204422
-        'egenix-mxdatetime',
-        'egenix-mxtools',
-        'enum34',               # T111602
-        'flask',
-        'flask-login',
-        'flickrapi',            # T86015
-        'flup',
-        'gdal',
-        'gdbm',
-        'genshi',               # T50863.
-        'genshi-doc',           # T50863.
-        'geoip',                # T64649
-        'gevent',
-        'gi',
-        'greenlet',
-        'httplib2',
-        'imaging',
-        'ipaddr',               # T86015.
-        'irclib',
-        'keyring',
-        'launchpadlib',
-        'lxml',                 # T61083.
-        'magic',                # T62211.
-        'matplotlib',           # T63445.
-        'mysql.connector',
-        'mysqldb',
-        'newt',
-        'numpy',                # T131675
-        'nose',
-        'opencv',
-        'requests-oauthlib',    # T130529
-        'pandas',               # T131675
-        'pil',                  # T108210
-        'problem-report',
-        'psycopg2',
-        'pycountry',            # T86015
-        'pydot',                # T86015
-        'pyexiv2',              # T61122.
-        'pygments',             # T71050
-        'pyicu',                # T102165
-        'pyinotify',            # T59003
-        'requests',
-        'rsvg',                 # T58996
-        'scipy',                # T103136
-        'sqlalchemy',
-        'svn',                  # T58996
-        'tk',                   # T182562
-        'twisted',
-        'twitter',
-        'unicodecsv',           # T86015
-        'unittest2',            # T86015
-        'venv',
-        'virtualenv',
-        'wadllib',
-        'webpy',
-        'werkzeug',
-        'zbar',                 # T58996
-        'zmq',
+    "dev": ["cffi", "coverage", "dev", "stdeb"],
+    "exec": [
+        "babel",  # T60220
+        "beautifulsoup",  # For valhallasw.
+        "bottle",  # T58995
+        "celery",
+        "cffi",  # T204422
+        "egenix-mxdatetime",
+        "egenix-mxtools",
+        "enum34",  # T111602
+        "flask",
+        "flask-login",
+        "flickrapi",  # T86015
+        "flup",
+        "gdal",
+        "gdbm",
+        "genshi",  # T50863.
+        "genshi-doc",  # T50863.
+        "geoip",  # T64649
+        "gevent",
+        "gi",
+        "greenlet",
+        "httplib2",
+        "imaging",
+        "ipaddr",  # T86015.
+        "irclib",
+        "keyring",
+        "launchpadlib",
+        "lxml",  # T61083.
+        "magic",  # T62211.
+        "matplotlib",  # T63445.
+        "mysql.connector",
+        "mysqldb",
+        "newt",
+        "numpy",  # T131675
+        "nose",
+        "opencv",
+        "requests-oauthlib",  # T130529
+        "pandas",  # T131675
+        "pil",  # T108210
+        "problem-report",
+        "psycopg2",
+        "pycountry",  # T86015
+        "pydot",  # T86015
+        "pyexiv2",  # T61122.
+        "pygments",  # T71050
+        "pyicu",  # T102165
+        "pyinotify",  # T59003
+        "requests",
+        "rsvg",  # T58996
+        "scipy",  # T103136
+        "sqlalchemy",
+        "svn",  # T58996
+        "tk",  # T182562
+        "twisted",
+        "twitter",
+        "unicodecsv",  # T86015
+        "unittest2",  # T86015
+        "venv",
+        "virtualenv",
+        "wadllib",
+        "webpy",
+        "werkzeug",
+        "zbar",  # T58996
+        "zmq",
     ],
 }
 
@@ -88,7 +83,7 @@ environ_processed = {}
 for environ, packages_post in environ_packages.items():
     package_list = {}
     for package_post in packages_post:
-        for prefix in ['python-', 'python3-']:
+        for prefix in ["python-", "python3-"]:
             package_name = prefix + package_post
             package_list[package_name] = {
                 release: genpp.get_version(package_name, release)
@@ -96,16 +91,8 @@ for environ, packages_post in environ_packages.items():
             }
 
     for release in genpp.releases:
-        genpp.write_pp(
-            'python',
-            release,
-            environ,
-            package_list
-        )
+        genpp.write_pp("python", release, environ, package_list)
 
     environ_processed[environ] = package_list
 
-genpp.write_report(
-    'python',
-    environ_processed
-)
+genpp.write_report("python", environ_processed)
