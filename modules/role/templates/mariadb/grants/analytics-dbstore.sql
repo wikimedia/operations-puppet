2281ec5bfea72eaee7da0671e8dcaa50e742aa09
Manuel Arostegui <marostegui@wikimedia.org>
2019-02-06 10:20:20 +0100

    dbstore-grants: Add research user and fixing styling
    analytics-dbstore.sql: Add the research user (which will be deprecated
    at some point). Also fix the styling a little bit.
    
    analytics-grants.sql: Make sure it is known that this file is and will
    be deprecated and deleted once dbstore1002 is decommissioned
    
    Bug: T214469
    Change-Id: Iecbb32c616c9270e4a5a13f06eb03d2103e21b75

--- modules/role/templates/mariadb/grants/analytics-dbstore.sql
+++ modules/role/templates/mariadb/grants/analytics-dbstore.sql
@@ -1,15 +1,19 @@
-# Role for the research user on the dbstore1003-1005 for analytics
-# Creating the role:
-# CREATE ROLE research_role;
-# This role needs to be granted to each user before setting the default role:
-# GRANT research_role TO '$user'@'10.%';
-# Assigning this default role to all the users by default is required:
-# SET DEFAULT ROLE research_role for '$user'@'10.%';
+-- Role for the research user on the dbstore1003-1005 for analytics
+-- Creating the role:
+CREATE ROLE research_role;
 
-# Role GRANTS
+-- Role GRANTS
 GRANT USAGE ON *.* TO research_role;
 GRANT SELECT ON `wikishared`.* TO research_role;
 GRANT SELECT ON `flowdb`.* TO research_role;
 GRANT SELECT ON `centralauth`.* TO research_role;
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON `staging`.* TO research_role;
 GRANT SELECT ON `%wik%`.* TO research_role;
+
+-- Research user (to be deprecated - T214469)
+GRANT USAGE ON *.* TO 'research'@'%' IDENTIFIED BY <%= @research_pass %> WITH MAX_USER_CONNECTIONS 200;
+
+-- This role needs to be granted to each user before setting the default role:
+GRANT research_role TO 'research'@'10.%';
+-- Assigning this default role to research user by default is required:
+SET DEFAULT ROLE research_role for 'research'@'10.%';
