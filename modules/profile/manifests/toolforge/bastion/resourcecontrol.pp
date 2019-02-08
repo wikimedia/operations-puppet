da5dd42affba6552a6a512afa07043fb7518a2e8
Luca Toscano <ltoscano@wikimedia.org>
2019-02-08 17:28:46 +0100

    profile::toolforge::bastion::resourcecontrol: fix class definition
    Change-Id: I679798b066672c92dae2abb5e269a13b744394de

--- modules/profile/manifests/toolforge/bastion/resourcecontrol.pp
+++ modules/profile/manifests/toolforge/bastion/resourcecontrol.pp
@@ -5,7 +5,7 @@
 class profile::toolforge::bastion::resourcecontrol(
     $nproc = hiera('profile::toolforge::bastion::nproc',30),
 ){
-    systemd::slice::all_users { 'bastion-systemd-user-slice':
+    class { 'systemd::slice::all_users':
         all_users_slice_config  => file('profile/toolforge/bastion-user-resource-control.conf'),
     }
 
