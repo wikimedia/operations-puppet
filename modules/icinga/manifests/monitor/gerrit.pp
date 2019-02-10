62ef16e9c697c5dce6631489bc4cc55fbbeaf0c1
Paladox <thomasmulhall410@yahoo.com>
2019-02-10 14:33:20 +0000

    Gerrit: Add icinga check to use healthcheck endpoint
    Add an Icinga check to test the special URL provided
    by the Gerrit HealthCheck plugin. It will turn non-200
    if an issue is reported.
    
    Linking to the output in notes_url field so the user can
    check quickly what the details are.
    
    Bug: T215457
    Change-Id: I26b2b1870852c74192fdd3ecac8405fc5d454175

--- modules/icinga/manifests/monitor/gerrit.pp
+++ modules/icinga/manifests/monitor/gerrit.pp
@@ -7,6 +7,14 @@ class icinga::monitor::gerrit {
         host_fqdn => 'gerrit.wikimedia.org',
     }
 
+    monitoring::service { 'gerrit-healthcheck':
+        description   => 'Gerrit Health Check',
+        check_command => 'check_https_url!gerrit.wikimedia.org!"/r/config/server/healthcheck~status',
+        contact_group => 'admins,gerrit',
+        host          => 'gerrit.wikimedia.org',
+        notes_url     => 'https://gerrit.wikimedia.org/r/config/server/healthcheck~status',
+    }
+
     monitoring::service { 'gerrit-json':
         description   => 'Gerrit JSON',
         check_command => 'check_https_url_at_address_for_minsize!gerrit.wikimedia.org!"/r/changes/?n=25&O=81"!10000',
