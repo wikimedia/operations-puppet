b93c18d96efe35a01f8ffdb5c12c39d9be9e3005
Luca Toscano <ltoscano@wikimedia.org>
2019-02-11 17:02:43 +0100

    profile::analytics::cluster::repo::statistics: fix directory (again)
    Bug: T212386
    Change-Id: I6cf04bc57ebd230ace96844077ad53657df891f8

--- modules/profile/manifests/analytics/cluster/repositories/statistics.pp
+++ modules/profile/manifests/analytics/cluster/repositories/statistics.pp
@@ -10,19 +10,9 @@ class profile::analytics::cluster::repositories::statistics {
     # The information is in turn used to get the mapping between
     # wiki name and dbstore host on which the correspondent database
     # is deployed.
-    if !defined(File['/srv/mediawiki']) {
-        file { '/srv/mediawiki':
-            ensure => directory,
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0755',
-        }
-    }
-
     git::clone { 'operations/mediawiki-config':
         ensure             => 'latest',
-        directory          => '/srv/mediawiki/mediawiki-config',
+        directory          => '/srv/mediawiki-config',
         recurse_submodules => true,
-        require            => File['/srv/mediawiki']
     }
 }
