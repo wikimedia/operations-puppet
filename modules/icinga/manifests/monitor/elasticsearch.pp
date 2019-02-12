d81078c9f9868e2491320fe677255b0bfd19749c
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2019-02-12 11:42:45 +0100

    elasticsearch: cleanup typo
    Change-Id: I5081acb239acfbfd4f2c0f0b064f2a3d4a718d8e

--- modules/icinga/manifests/monitor/elasticsearch.pp
+++ modules/icinga/manifests/monitor/elasticsearch.pp
@@ -40,7 +40,7 @@ class icinga::monitor::elasticsearch {
             monitoring::service { "elasticsearch / unassigned shard check - ${cluster}(${instance['name']})":
                 host           => "search.svc.${cluster}.wmnet",
                 check_command  => "check_elasticsearch_unassigned_shards_via_https!${instance['https_port']}",
-                description    => "ElasticSearch unassigned shard size check - ${cluster}(${instance['name']})",
+                description    => "ElasticSearch unassigned shard check - ${cluster}(${instance['name']})",
                 critical       => false,
                 check_interval => 720, # 12h
                 retry_interval => 120, # 2h
