42aeefdf2583126b638cf9f3da5f097cc148226a
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2019-02-14 17:31:58 +0100

    elasticsearch: both relforge clusters are accessible from mwmaint
    The access to 'relforge-eqiad-small-alpha' was not added when that
    second cluster was created.
    
    Change-Id: I28bb834706814656000595cff3d26785f94633b5

--- modules/role/manifests/elasticsearch/relforge.pp
+++ modules/role/manifests/elasticsearch/relforge.pp
@@ -18,11 +18,19 @@ class role::elasticsearch::relforge {
     # production, except from mwmaint hosts to import production indices.
     $maintenance_hosts = join($network::constants::special_hosts['production']['maintenance_hosts'], ' ')
 
-    ::ferm::service { 'elastic-https-mwmaint':
-      ensure => present,
-      proto  => 'tcp',
-      port   => '9243',
-      srange => "(${maintenance_hosts})",
+    ::ferm::service {
+        default:
+            ensure => present,
+            proto  => 'tcp',
+            port   => '9243',
+            srange => "(${maintenance_hosts})",
+        ;
+        'elastic-main-https-mwmaint-9243':
+            port   => '9243',
+        ;
+        'elastic-small-alpha-https-mwmaint-9443':
+            port   => '9443',
+        ;
     }
 
 }
