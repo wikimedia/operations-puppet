21fe5f3073424c9d5f8e15637e7ffd66e1b73fff
Alexandros Kosiaris <akosiaris@wikimedia.org>
2019-02-14 16:47:01 +0200

    HTTP availability alerts: Decrease retries to 1
    Decrease from 2 to 1 mins before alerting on an http_avaialibity alert.
    This will decrease a bit the window of time between the event and the
    alert
    
    https://wikitech.wikimedia.org/wiki/Incident_documentation/20190211-varnish
    
    Retries > 1 on a prometheus alerts are probably anyways rather useless
    as the statistical nature of the alert accounts for that already.
    
    Change-Id: I402a364c4a9fd7270fc80b63b6c08a48cd7c28d5

--- modules/monitoring/manifests/alerts/http_availability.pp
+++ modules/monitoring/manifests/alerts/http_availability.pp
@@ -10,7 +10,7 @@ define monitoring::alerts::http_availability(
         query           => "100 * (1 - site_job:varnish_requests:avail5m{job=~\"varnish-(text|upload)\",site=\"${site}\"})",
         prometheus_url  => 'http://prometheus.svc.eqiad.wmnet/global',
         method          => 'le',
-        retries         => 2,
+        retries         => 1,
         warning         => $warning,
         critical        => $critical,
         dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/frontend-traffic?panelId=3&fullscreen&refresh=1m&orgId=1']
@@ -22,7 +22,7 @@ define monitoring::alerts::http_availability(
         query           => "100 * (1 - site_cluster:nginx_requests:avail5m{cluster=~\"cache_(text|upload)\",site=\"${site}\"})",
         prometheus_url  => 'http://prometheus.svc.eqiad.wmnet/global',
         method          => 'le',
-        retries         => 2,
+        retries         => 1,
         warning         => $warning,
         critical        => $critical,
         dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/frontend-traffic?panelId=4&fullscreen&refresh=1m&orgId=1']
