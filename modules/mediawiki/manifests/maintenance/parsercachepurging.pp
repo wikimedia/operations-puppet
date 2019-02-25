08348fd45578501574633222c4abe797c5a5a5dc
Manuel Arostegui <marostegui@wikimedia.org>
2019-02-25 07:34:24 +0100

    parsercachepurging.pp: Increase TTL from 24 days to 30
    Initially the keys had a TTL of 30 days, but
    due to disk space issues (T167784) it had to
    be reduced to 22 days.
    
    We have re-freshed the parsercache hosts (T208383),
    so we are ready to go back to 30 days TTL
    
    Bug: T210992
    Change-Id: I3d398222073f1d04a5169c187f8e85e735d3e71c

--- modules/mediawiki/manifests/maintenance/parsercachepurging.pp
+++ modules/mediawiki/manifests/maintenance/parsercachepurging.pp
@@ -8,8 +8,8 @@ class mediawiki::maintenance::parsercachepurging( $ensure = present ) {
         minute  => 0,
         hour    => 1,
         weekday => '*',
-        # Every day, Purge entries older than 24d * 86400s/d = 2073600s
-        command => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --age=2073600 --msleep 500 >/dev/null 2>&1',
+        # Every day, Purge entries older than 30d * 86400s/d = 2592000s
+        command => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --age=2592000 --msleep 500 >/dev/null 2>&1',
     }
 }
 
