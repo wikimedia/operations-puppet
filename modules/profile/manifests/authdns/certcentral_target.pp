fd3aa54c99a0d43e7a9fc1e3b153ecbac5f9f7a6
Valentin Gutierrez <vgutierrez@wikimedia.org>
2019-02-25 16:53:22 +0100

    authdns: Drop certcentral hosts access (1/2)
    Bug: T207389
    Change-Id: Ia1c3d00b56b08055aa5a60f5a25b050e94bc83a0

--- modules/profile/manifests/authdns/certcentral_target.pp
+++ modules/profile/manifests/authdns/certcentral_target.pp
@@ -2,17 +2,19 @@ class profile::authdns::certcentral_target(
     $certcentral_hosts=hiera('profile::authdns::certcentral_target::certcentral_hosts'),
 ) {
     user { 'certcentral':
-        ensure => present,
+        ensure => absent,
         system => true,
         home   => '/nonexistent',
         shell  => '/bin/bash',
     }
 
     ssh::userkey { 'certcentral':
+        ensure  => absent,
         content => secret('keyholder/authdns_certcentral.pub'),
     }
 
     sudo::user { 'certcentral':
+        ensure     => absent,
         privileges => [
             'ALL = (gdnsd) NOPASSWD: /usr/bin/gdnsdctl -- acme-dns-01 *',
         ],
@@ -20,6 +22,7 @@ class profile::authdns::certcentral_target(
 
     $hosts = join($certcentral_hosts, ' ')
     ferm::service { 'certcentral_dns_ssh':
+        ensure => absent,
         proto  => 'tcp',
         port   => '22',
         srange => "(@resolve((${hosts})) @resolve((${hosts}), AAAA))",
