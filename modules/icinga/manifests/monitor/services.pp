750c61d7a0eb581147d43d47bd631c0a3d3ddcb5
Daniel Zahn <dzahn@wikimedia.org>
2019-03-05 05:29:51 -0800

    icinga/restbase/eventbus: add notes URLs
    We want to make the notes_url a required parameter so that
    all Icinga checks have a link to some kind of runbook or background
    info for a user who happens to see a triggered alert.
    
    Ideally that URL contains info how to react to the alert.
    
    Are these ok for now or are there better ones? Maybe Phabricator
    workboards are an option as well to tell users where to create tickets.
    
    Bug: T197873
    Change-Id: I387de73a9ead590e108caa534d38f150a33e9fb2

--- modules/icinga/manifests/monitor/services.pp
+++ modules/icinga/manifests/monitor/services.pp
@@ -5,15 +5,18 @@ class icinga::monitor::services {
     monitoring::grafana_alert { 'restbase':
         dashboard_uid => '000000068',
         contact_group => 'team-services',
+        notes_url     => 'https://wikitech.wikimedia.org/wiki/RESTBase#What_to_check_after_a_deploy',
     }
 
     monitoring::grafana_alert { 'eventbus':
         dashboard_uid => '000000201',
         contact_group => 'analytics,team-services',
+        notes_url     => 'https://wikitech.wikimedia.org/wiki/EventBus',
     }
 
     monitoring::grafana_alert { 'jobqueue-eventbus':
       dashboard_uid => '000000400',
       contact_group => 'team-services',
+      notes_url     => 'https://wikitech.wikimedia.org/wiki/EventBus',
     }
 }
