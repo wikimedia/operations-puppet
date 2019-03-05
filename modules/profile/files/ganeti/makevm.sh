2ea176664dbd6998a5ca2009ad5f67858a727c8e
Luca Toscano <ltoscano@wikimedia.org>
2019-03-05 11:25:27 +0100

    ganeti: add the Analytics VLAN use case to makevm
    Bug: T217640
    Change-Id: Ic3a90bcdf94c17cb1a75b1389110c6c3ae33ac62

--- modules/profile/files/ganeti/makevm.sh
+++ modules/profile/files/ganeti/makevm.sh
@@ -11,6 +11,7 @@ echo -e "This is an interactive script to make it easier to
 create a Ganeti VM.\nPlease see https://wikitech.wikimedia.org/wiki/Ganeti#Create_a_VM for more details.\n"
 
 read -n1 -r -p "$(echo -e 'Are you going to need a public IP? (y/n)\n\b')" PUBLICIP
+read -n1 -r -p "$(echo -e '\n\nIf you need a private IP, do you need it to be inside the Analytics VLAN? (y/n)\n\b')" ANALYTICSIP
 read -n1 -r -p "$(echo -e '\n\nPlease enter the correct row. (A, B or C - 'gnt-group list' to show)\n\b')" ROW
 read -p "$(echo -e '\n\nHow many vCPUs do you need?\n\b')" VCPUS
 read -p "$(echo -e '\nHow much RAM do you need? (Gigabytes)\n\b')" MEMORY
@@ -19,6 +20,8 @@ read -p "$(echo -e '\nHow do you want to call the instance? (FQDN)\n\b')" FQDN
 
 if [[ $PUBLICIP =~ ^[Yy]$ ]]; then
     LINK="public"
+elif [[ $ANALYTICSIP =~ ^[Yy]$ ]]; then
+    LINK="analytics"
 else
     LINK="private"
 fi
