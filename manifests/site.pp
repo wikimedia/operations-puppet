1aea91b032c5a3b7bbae0164f9332a9a67a2c8dd
RobH <rob@wikimedia.org>
2019-03-05 15:15:36 -0800

    decom cp10[58-68] repo entries
    decommission of cp systems that were in a5-eqiad that are no longer in
    use
    
    Bug: T208584
    Change-Id: If110b0d2495fb1651a62aef6d26cd7e76c083ee7

--- manifests/site.pp
+++ manifests/site.pp
@@ -289,18 +289,6 @@ node 'cp1008.wikimedia.org' {
     interface::add_ip6_mapped { 'main': }
 }
 
-# ex cp-misc_eqiad
-node /^cp10(|58|61)\.eqiad\.wmnet$/ {
-    role(spare::system)
-}
-
-# 18 total systems here for decom (old eqiad caches not in use):
-# cp104[6789], cp1050, cp105[2345], cp1059, cp1060, cp106[2345678]
-# 4          + 1     + 4          + 1     + 1     + 7 = 18
-node /^cp10(59|6[02345678])\.eqiad\.wmnet$/ {
-    role(spare::system)
-}
-
 # ATS Test Cluster
 node /^cp107[1-4]\.eqiad\.wmnet$/ {
     interface::add_ip6_mapped { 'main': }
