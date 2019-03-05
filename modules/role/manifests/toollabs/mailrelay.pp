f6e882b17c5be52daa1957c4b28b4ed2b3b8d490
Bryan Davis <bd808@wikimedia.org>
2019-03-05 09:45:57 -0700

    toolforge: remove obsolete mailrelay manifests
    Remove the legacy role::toollabs::mailrelay manifest and the manifests
    and support files that it was built from. This is a follow up to
    8c0bf72726 which introduced role::toolforge::mailrelay and a parallel
    manifest structure. The legacy role is no longer used in Toolforge's
    deployed instances.
    
    Bug: T208843
    Change-Id: If43065f1b22ef1cc2c823fba5de9677b77b7af73

--- modules/role/manifests/toollabs/mailrelay.pp
+++
@@ -1,6 +0,0 @@
-# filtertags: labs-project-tools
-class role::toollabs::mailrelay {
-    system::role { 'toollabs::mailrelay': description => 'Tool Labs mail relay' }
-
-    include ::toollabs::mailrelay
-}
