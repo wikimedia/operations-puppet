5f24a105fc799dbc1a9039da076093a3c401ad4e
Daniel Zahn <dzahn@wikimedia.org>
2019-03-07 01:32:30 -0800

    doc.wikimedia.org: switch from PHP 7.2 back to standard stretch/7.0
    Change-Id: I41c55d60be533a57514bd59db0ee0863c38756bb

--- modules/profile/manifests/doc.pp
+++ modules/profile/manifests/doc.pp
@@ -1,24 +1,7 @@
 # web server hosting https://docs.wikimedia.org
 class profile::doc {
 
-    $php_version='7.2'
-    $php_packages=["php${php_version}-fpm", "php${php_version}-xml"]
-
-    # Use php7.2 from Ondrej Sury's repository.
-    if $php_version == '7.2' {
-
-        apt::repository { 'wikimedia-php72':
-            uri        => 'http://apt.wikimedia.org/wikimedia',
-            dist       => "${::lsbdistcodename}-wikimedia",
-            components => 'thirdparty/php72',
-        }
-
-        package { $php_packages:
-            ensure  => installed,
-            require => [ Apt::Repository['wikimedia-php72'],
-                        Exec['apt-get update']],
-        }
-    }
+    require_package(['php-fpm', 'php-xml'])
 
     class { '::httpd':
         modules => ['headers',
