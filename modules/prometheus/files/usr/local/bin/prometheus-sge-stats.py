9eec75648027f4be3e3bfeb46eee9af304dc75f1
bstorm <bstorm@wikimedia.org>
2019-03-07 17:01:40 -0700

    toolforge: reformatting promethus grid engine collector
    Reformatting with black.
    
    Change-Id: Ic541ec1f6fd02c47e524bf5ead661ae176956895

--- modules/prometheus/files/usr/local/bin/prometheus-sge-stats.py
+++ modules/prometheus/files/usr/local/bin/prometheus-sge-stats.py
@@ -28,39 +28,34 @@ from prometheus_client.exposition import generate_latest
 
 
 logger = logging.getLogger(__name__)
-SGE_ROOT = '/data/project/.system_sge/gridengine'
-RE_JOBLINE = re.compile(r'\s+\d')
+SGE_ROOT = "/data/project/.system_sge/gridengine"
+RE_JOBLINE = re.compile(r"\s+\d")
 
 
 def get_job_count():
     """Get sequential all time job count"""
-    with open('{}/spool/qmaster/jobseqnum'.format(SGE_ROOT), 'r') as f:
+    with open("{}/spool/qmaster/jobseqnum".format(SGE_ROOT), "r") as f:
         return int(f.read().strip())
 
 
 def grid_cmd(cmd):
-    logger.debug('Running %s', cmd)
+    logger.debug("Running %s", cmd)
     try:
         return subprocess.check_output(
-            cmd,
-            env={
-                'SGE_ROOT': SGE_ROOT,
-            },
-            universal_newlines=True)
+            cmd, env={"SGE_ROOT": SGE_ROOT}, universal_newlines=True
+        )
     except subprocess.CalledProcessError as e:
-        logger.warning(
-            'Output from failed shell command %s: %s', cmd, e.output)
+        logger.warning("Output from failed shell command %s: %s", cmd, e.output)
         raise e
 
 
 def get_jobs(queue):
     """Retrieve all users job output for a queue."""
-    return grid_cmd(
-        ['/usr/bin/qstat', '-q', queue, '-u', '*']).splitlines()[1:]
+    return grid_cmd(["/usr/bin/qstat", "-q", queue, "-u", "*"]).splitlines()[1:]
 
 
 def get_queues():
-    queues = grid_cmd(['/usr/bin/qconf', '-sql'])
+    queues = grid_cmd(["/usr/bin/qconf", "-sql"])
     return [q for q in queues.splitlines()]
 
 
@@ -75,27 +70,31 @@ def job_state_stats(jobs):
 
 
 def get_exec_hosts():
-    return grid_cmd(['/usr/bin/qconf', '-sel']).splitlines()
+    return grid_cmd(["/usr/bin/qconf", "-sel"]).splitlines()
 
 
 def get_jobs_by_host(host):
-    output = grid_cmd(['/usr/bin/qhost', '-j', '-h', host])
+    output = grid_cmd(["/usr/bin/qhost", "-j", "-h", host])
     return [line for line in output.splitlines() if RE_JOBLINE.match(line)]
 
 
 def collect_sge_stats(registry):
     # This rolls over at 10million
     jobseqnum = Counter(
-        'jobseqnum', 'Job sequence number',
-        namespace='sge', registry=registry)
+        "jobseqnum", "Job sequence number", namespace="sge", registry=registry
+    )
     jobseqnum.inc(get_job_count())
 
     # Active jobs per (queue, state)
     queuejobs = Gauge(
-        'queuejobs', 'Concurrent jobs per queue', ('queue', 'state'),
-        namespace='sge', registry=registry)
+        "queuejobs",
+        "Concurrent jobs per queue",
+        ("queue", "state"),
+        namespace="sge",
+        registry=registry,
+    )
     for q in get_queues():
-        logger.debug('Examining queue %s', q)
+        logger.debug("Examining queue %s", q)
         jobs = get_jobs(q)
         states = job_state_stats(jobs)
         for state, scount in states.items():
@@ -103,8 +102,12 @@ def collect_sge_stats(registry):
 
     # Active jobs per host
     hostjobs = Gauge(
-        'hostjobs', 'Concurrent jobs per host', ('host',),
-        namespace='sge', registry=registry)
+        "hostjobs",
+        "Concurrent jobs per host",
+        ("host",),
+        namespace="sge",
+        registry=registry,
+    )
     for line in get_exec_hosts():
         host = line.strip()
         jcount = len(get_jobs_by_host(host))
@@ -113,19 +116,22 @@ def collect_sge_stats(registry):
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--outfile", metavar="FILE.prom", help="Output file (stdout)")
     parser.add_argument(
-        '--outfile', metavar='FILE.prom',
-        help='Output file (stdout)')
-    parser.add_argument(
-        '-d', '--debug', dest='log_level',
-        action='store_const', const=logging.DEBUG, default=logging.WARNING,
-        help='Enable debug logging (false)')
+        "-d",
+        "--debug",
+        dest="log_level",
+        action="store_const",
+        const=logging.DEBUG,
+        default=logging.WARNING,
+        help="Enable debug logging (false)",
+    )
     args = parser.parse_args()
 
     logging.basicConfig(level=args.log_level)
 
-    if args.outfile and not args.outfile.endswith('.prom'):
-        parser.error('Output file does not end with .prom')
+    if args.outfile and not args.outfile.endswith(".prom"):
+        parser.error("Output file does not end with .prom")
 
     registry = CollectorRegistry()
     collect_sge_stats(registry)
@@ -133,8 +139,8 @@ def main():
     if args.outfile:
         write_to_textfile(args.outfile, registry)
     else:
-        sys.stdout.write(generate_latest(registry).decode('utf-8'))
+        sys.stdout.write(generate_latest(registry).decode("utf-8"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
