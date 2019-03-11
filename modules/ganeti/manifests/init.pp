eff518716f3906ffb740e1190e2d19024b3fdecf
Cas Rusnov <crusnov@wikimedia.org>
2019-03-11 10:13:16 -0700

    Add certs for RAPI and adjust config to use them
    Change-Id: I56f7d2fe79bc6d67251b9ccfcc5597b75a8831b3

--- modules/ganeti/manifests/init.pp
+++ modules/ganeti/manifests/init.pp
@@ -59,12 +59,20 @@ class ganeti(
         source => 'puppet:///modules/ganeti/lvm.conf',
     }
 
+    # Deploy defaults (for now, configuring RAPI) and the certificates for RAPI.
+    # Potential fixme: We don't restart the daemon here since it's not independent
+    # and this file configures other aspects of Ganeti. Manually restart ganeti
+    # on the target hosts after changes are merged.
+    $certname = "ganeti01.svc.${::site}.wmnet"
     file { '/etc/default/ganeti':
-        ensure => present,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0644',
-        source => 'puppet:///modules/ganeti/etc_default_ganeti'
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('ganeti/etc_default_ganeti.erb')
     }
-
+    sslcert::certificate { $certname:
+        ensure => present,
+        group  => 'gnt-admin',
+    }
 }
