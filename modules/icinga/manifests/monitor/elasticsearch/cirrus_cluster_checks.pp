4433f97e1c64520453361f657508cd353ec51b3f
Mathew Onipe <monipe@wikimedia.org>
2019-03-11 15:09:30 +0100

    elasticsearch: mjolnir bulk update lag
    Alert on mjolnir daemons failure and kafka consumer lag.
    
    Bug: T214494
    Change-Id: I255114e2c7e0802f8d043651720458a559724330

--- modules/icinga/manifests/monitor/elasticsearch/cirrus_cluster_checks.pp
+++ modules/icinga/manifests/monitor/elasticsearch/cirrus_cluster_checks.pp
@@ -1,10 +1,11 @@
 # == Class icinga::monitor::elasticsearch::cirrus_cluster_checks
 class icinga::monitor::elasticsearch::cirrus_cluster_checks{
     $ports = [9243, 9443, 9643]
-    $hosts = ['search.svc.eqiad.wmnet', 'search.svc.codfw.wmnet']
+    $sites = ['eqiad', 'codfw']
     $scheme = 'https'
 
-    $hosts.each |$host| {
+    $sites.each |$site| {
+        $host = "search.svc.${site}.wmnet"
         icinga::monitor::elasticsearch::base_checks { $host:
             host   => $host,
             scheme => $scheme,
@@ -15,6 +16,34 @@ class icinga::monitor::elasticsearch::cirrus_cluster_checks{
             host   => $host,
             scheme => $scheme,
             ports  => $ports,
+        }
+
+        # Alert on mjolnir daemons - T214494
+        monitoring::check_prometheus { "mjolnir_bulk_update_failure_${site}":
+            description     => "Mjolnir bulk update failure check - ${site}",
+            dashboard_links => ['https://grafana.wikimedia.org/d/000000591/elasticsearch-mjolnir-bulk-updates?orgId=1&from=now-7d&to=now&panelId=1&fullscreen'],
+            query           => 'scalar(sum(increase(mjolnir_bulk_action_total{result="failed"}[4d])))',
+            prometheus_url  => "http://prometheus.svc.${site}.wmnet/ops",
+            method          => 'gt',
+            critical        => 1,
+            warning         => 1,
+            check_interval  => 1440, # 24h
+            retries         => 1,
+            contact_group   => 'admins,team-discovery',
+        }
+
+        # ensure kafka queue is empty at least once in a week
+        monitoring::check_prometheus { "cirrus_update_lag_${site}":
+            description     => "Cirrus Update lag check - ${site}",
+            dashboard_links => ['https://grafana.wikimedia.org/d/000000484/kafka-consumer-lag?orgId=1&from=now-7d&to=now'],
+            query           => "scalar(sum(min_over_time(kafka_burrow_partition_lag{exported_cluster=~\"main-${site}\", topic=\"${site}.cirrussearch.page-index-update\", group=~\"cirrussearch_updates_${site}\"}[7d])))",
+            prometheus_url  => "http://prometheus.svc.${site}.wmnet/ops",
+            method          => 'gt',
+            critical        => 1,
+            warning         => 1,
+            check_interval  => 1440, # 24h
+            retries         => 1,
+            contact_group   => 'admins,team-discovery',
         }
     }
 }
