f54bde73e623ee471e9b8f8e1eb42cdf21713345
John Bond <jbond@wikimedia.org>
2019-03-13 11:59:14 +0000

    Add openjpeg2 libraries to config file
    Have also moved the config file to be a plain file as it doesn't make use
    of ERB so there is no need to use the template function.
    
    Change-Id: I558dd06d64778b659222191144ec5a75b4df52c0

--- modules/profile/manifests/debdeploy.pp
+++ modules/profile/manifests/debdeploy.pp
@@ -4,9 +4,9 @@ class profile::debdeploy {
     require_package('debdeploy-server')
 
     file { '/etc/debdeploy.conf':
-            mode    => '0444',
-            owner   => 'root',
-            group   => 'root',
-            content => template('profile/debdeploy/debdeploy.erb');
+        mode   => '0444',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/profile/debdeploy/debdeploy.conf',
     }
 }
