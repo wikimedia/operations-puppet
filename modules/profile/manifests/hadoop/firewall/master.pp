caffaf21e0542b9fe36665c592e028800b8b17f9
Luca Toscano <ltoscano@wikimedia.org>
2019-03-14 13:54:05 +0100

    profile::hadoop::firewall::master: break down ssl config
    Selectively enable/disable TLS ports for various services
    instead of having a single configuration parameter. This allows
    us to tune our TLS configuration based on our needs.
    
    Add also the TLS configuration to the Hadoop test cluster.
    
    Bug: T217412
    Change-Id: I20e5241e6b1e7c2299055473bbe9dcde1338e95b

--- modules/profile/manifests/hadoop/firewall/master.pp
+++ modules/profile/manifests/hadoop/firewall/master.pp
@@ -5,23 +5,31 @@
 class profile::hadoop::firewall::master(
     $analytics_srange       = hiera('profile::hadoop::firewall::master::analytics_srange', '$DOMAIN_NETWORKS'),
     $analytics_druid_srange = hiera('profile::hadoop::firewall::master::analytics_druid_srange', '$DOMAIN_NETWORKS'),
-    $ssl_enabled            = hiera('profile::hadoop::firewall::master::ssl_enabled', false),
+    $hdfs_ssl_enabled       = hiera('profile::hadoop::firewall::master::hdfs::ssl_enabled', false),
+    $yarn_ssl_enabled       = hiera('profile::hadoop::firewall::master::yarn::ssl_enabled', false),
+    $mapred_ssl_enabled     = hiera('profile::hadoop::firewall::master::mapred::ssl_enabled', false),
 ) {
-    if ($ssl_enabled) {
-        # This port is also used by the HDFS Checkpoint
-        # workflow, as described in:
-        # https://blog.cloudera.com/blog/2014/03/a-guide-to-checkpointing-in-hadoop/
-        # If blocked it can lead to longer restarts for
-        # the active NameNode (that needs to reply all the edit log
-        # from its last old fsimage) and connect timeouts on the standby Namenode logs
-        # (since it periodically tries to establish HTTPS connections).
-        $hadoop_hdfs_namenode_http_port = 50470
-        $hadoop_yarn_resourcemanager_http_port = 8090
-        $hadoop_mapreduce_historyserver_http_port = 19890
-    } else {
-        $hadoop_hdfs_namenode_http_port = 50070
-        $hadoop_yarn_resourcemanager_http_port = 8088
-        $hadoop_mapreduce_historyserver_http_port = 19888
+
+    # This port is also used by the HDFS Checkpoint
+    # workflow, as described in:
+    # https://blog.cloudera.com/blog/2014/03/a-guide-to-checkpointing-in-hadoop/
+    # If blocked it can lead to longer restarts for
+    # the active NameNode (that needs to reply all the edit log
+    # from its last old fsimage) and connect timeouts on the standby Namenode logs
+    # (since it periodically tries to establish HTTPS connections).
+    $hadoop_hdfs_namenode_http_port = $hdfs_ssl_enabled ? {
+        true    => 50470,
+        default => 50070,
+    }
+
+    $hadoop_yarn_resourcemanager_http_port = $yarn_ssl_enabled ? {
+        true    => 8090,
+        default => 8088,
+    }
+
+    $hadoop_mapreduce_historyserver_http_port = $mapred_ssl_enabled ? {
+        true    => 19890,
+        default => 19888,
     }
 
     ferm::service{ 'hadoop-hdfs-namenode':
