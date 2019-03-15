d40b3b9ce8a8bc29c02eac47271173fab5bd0d80
Alex Monk <krenair@gmail.com>
2019-03-15 22:34:20 +0000

    Re-apply "openstack::clientpackages::common: include python3 packages"
    This reverts commit 40e38d28cd9a80101e5a19050df0d2d107625804.
    
    Take 2! Don't merge until we've sorted out the python3-cliff dependency on jessie.
    
    Bug: T218423
    Change-Id: If2db633cf77b970d8818c859f876140b7560986f

--- modules/openstack/manifests/clientpackages/newton/stretch.pp
+++ modules/openstack/manifests/clientpackages/newton/stretch.pp
@@ -2,20 +2,6 @@ class openstack::clientpackages::newton::stretch(
 ) {
     # no special repo is configured, yet?
 
-    $python3packages = [
-        'python3-keystoneauth1',
-        'python3-keystoneclient',
-        'python3-novaclient',
-        'python3-glanceclient',
-        'python3-openstackclient',
-        'python3-designateclient',
-        'python3-neutronclient',
-    ]
-
-    package{ $python3packages:
-        ensure => 'present',
-    }
-
     package{ 'mariadb-client-10.1':
         ensure => 'present',
     }
