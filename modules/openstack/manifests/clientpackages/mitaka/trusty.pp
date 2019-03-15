40e38d28cd9a80101e5a19050df0d2d107625804
Bstorm <bstorm@wikimedia.org>
2019-03-15 21:18:05 +0000

    Revert "openstack::clientpackages::common: include python3 packages"
    This reverts commit 3f48eecf82766f6ea13bc886e6e45caa00c20402.
    
    Reason for revert: Broken on the bare-metal servers.
    
    Change-Id: Icd761878d6c10f92bd26632d9c2e0cfae0cc1c13

--- modules/openstack/manifests/clientpackages/mitaka/trusty.pp
+++ modules/openstack/manifests/clientpackages/mitaka/trusty.pp
@@ -3,7 +3,16 @@ class openstack::clientpackages::mitaka::trusty(
     # repo is same as for server packages!
     include ::openstack::serverpackages::mitaka::trusty
 
-    # TODO: What is this doing here?
+    $python3packages = [
+        'python3-keystoneclient',
+        'python3-novaclient',
+        'python3-glanceclient',
+    ]
+
+    package{ $python3packages:
+        ensure => 'present',
+    }
+
     $mainpackages = [
         'mysql-client-5.5',
         'mysql-common',
@@ -11,5 +20,13 @@ class openstack::clientpackages::mitaka::trusty(
 
     package { $mainpackages:
         ensure => 'present',
+    }
+
+    file { '/usr/lib/python3/dist-packages/mwopenstackclients.py':
+        ensure => 'present',
+        source => 'puppet:///modules/openstack/clientpackages/mwopenstackclients.py',
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
     }
 }
