2fdd2880a24ef068ece043266a8d895e7f3224b7
Daniel Zahn <dzahn@wikimedia.org>
2019-03-19 08:09:17 -0700

    dumps: add Icinga notes URL
    Change-Id: I0094b110577e369e2cd3512ddf34d50816048664

--- modules/dumps/manifests/monitoring.pp
+++ modules/dumps/manifests/monitoring.pp
@@ -7,5 +7,6 @@ class dumps::monitoring {
         nrpe_command  => '/usr/lib/nagios/plugins/check_procs -w 85 -c 95 -m CPU -C nfsd',
         contact_group => 'admins',
         retries       => 3,
+        notes_url     => 'https://wikitech.wikimedia.org/wiki/Dumps/XML-SQL_Dumps#A_dumpsdata_host_dies',
     }
 }
