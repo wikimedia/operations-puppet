bd21d3f470d02406f21406761c0a2784b1d16a42
Alex Monk <krenair@gmail.com>
2019-03-24 05:18:57 +0000

    network: Allow customisation of cumin list on a per-project basis
    So I can add deployment-cumin.
    
    Change-Id: I67447c326e5e84f650ebe8e61b38593187df4ab5

--- modules/network/manifests/constants.pp
+++ modules/network/manifests/constants.pp
@@ -386,12 +386,12 @@ class network::constants {
                 ],
             'deployment_hosts' => hiera('network::allow_deployment_from_ips', []), # lint:ignore:wmf_styleguide
             'maintenance_hosts' => hiera('network::allow_maintenance_from_ips', []), # lint:ignore:wmf_styleguide
-            'cumin_masters' => [  # As seen by labs instances
+            'cumin_masters' => concat([  # As seen by labs instances
                     '10.68.18.66',  # bastion-restricted-01.eqiad.wmflabs
                     '10.68.18.68',  # bastion-restricted-02.eqiad.wmflabs
                     '172.16.1.211', # bastion-restricted-eqiad1-02.eqiad.wmflabs
                     '172.16.1.135', # bastion-restricted-eqiad1-01.eqiad.wmflabs
-                ],
+                ], hiera('network::allow_cumin_from_ips', [])), # lint:ignore:wmf_styleguide
             'cumin_real_masters' => [  # Where Cumin can be run
                     '208.80.154.158',               # labpuppetmaster1001.wikimedia.org
                     '2620:0:861:2:208:80:154:158',  # labpuppetmaster1001.wikimedia.org
