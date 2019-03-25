3045a11a61d894318c1801809f18b85c3ce43369
Daniel Zahn <dzahn@wikimedia.org>
2019-03-25 06:14:33 -0700

    mediawiki::maintenance: run tor_exit cron with PHP 7.2
    Bug: T219135
    Change-Id: Id152d494b483dc8869753e2b27d212e7a5016a2a

--- modules/mediawiki/manifests/maintenance/tor_exit_node.pp
+++ modules/mediawiki/manifests/maintenance/tor_exit_node.pp
@@ -1,7 +1,7 @@
 class mediawiki::maintenance::tor_exit_node( $ensure = present, $wiki = 'aawiki' ) {
     cron { 'tor_exit_node_update':
         ensure  => $ensure,
-        command => "/usr/local/bin/mwscript extensions/TorBlock/maintenance/loadExitNodes.php --wiki=${wiki} --force > /dev/null",
+        command => "PHP=php7.2 /usr/local/bin/mwscript extensions/TorBlock/maintenance/loadExitNodes.php --wiki=${wiki} --force > /dev/null",
         user    => $::mediawiki::users::web,
         minute  => '*/20',
     }
