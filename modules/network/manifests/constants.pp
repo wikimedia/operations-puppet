d7e81312fb39961ecd367bea7ce15a7fb1034a95
Daniel Zahn <dzahn@wikimedia.org>
2019-03-27 02:20:22 -0700

    network::constants: add bast2002
    Bug: T196665
    Change-Id: I7f22e950347bbb981c0515d132a3bda1c6cf8153

--- modules/network/manifests/constants.pp
+++ modules/network/manifests/constants.pp
@@ -31,6 +31,8 @@ class network::constants {
                     '2620:0:861:3:208:80:154:86',       # bast1002.wikimedia.org
                     '208.80.153.5',                     # bast2001.wikimedia.org
                     '2620:0:860:1:208:80:153:5',        # bast2001.wikimedia.org
+                    '208.80.153.54',                    # bast2002.wikimedia.org
+                    '2620:0:860:2:208:80:153:54',       # bast2002.wikimedia.org
                     '91.198.174.113',                   # bast3002.wikimedia.org
                     '2620:0:862:1:91:198:174:113',      # bast3002.wikimedia.org
                     '198.35.26.6',                      # bast4002.wikimedia.org
