7953af612daffde381dba34ddb0c3f0d4b069747
Cole White <cwhite@wikimedia.org>
2019-03-27 16:09:37 -0600

    prometheus: clean up node exporter transition code
    Now that the upgrade to 0.17 is rolled out to all datacenters, clean up the transition code to
    prior behavior except for Ubuntu Trusty. This CS retains the exception for Ubuntu hosts.
    
    Bug: T213708
    Change-Id: I24723194b0d4ddf9a900bde8b6bb22bcdfeaa76a

--- modules/prometheus/manifests/node_exporter.pp
+++ modules/prometheus/manifests/node_exporter.pp
@@ -29,9 +29,6 @@
 # [*$web_listen_address*]
 #  IP:Port combination to listen on
 #
-# [*upgrade_one_seven*]
-#  A feature flag to run the prometheus-node-exporter upgrade to 0.17.
-#
 #  Available collectors: https://github.com/prometheus/node_exporter/tree/v0.17.0#collectors
 
 
@@ -44,20 +41,20 @@ class prometheus::node_exporter (
     Array[String] $collectors_extra = [],
     String $collector_ntp_server = '127.0.0.1',
     Pattern[/:\d+$/] $web_listen_address = ':9100',
-    Boolean $upgrade_one_seven = hiera('prometheus_upgrade_one_seven', false),
 ) {
     $collectors_default = ['buddyinfo', 'conntrack', 'diskstats', 'entropy', 'edac', 'filefd', 'filesystem', 'hwmon',
         'loadavg', 'mdadm', 'meminfo', 'netdev', 'netstat', 'sockstat', 'stat', 'tcpstat',
         'textfile', 'time', 'uname', 'vmstat']
     $textfile_directory = '/var/lib/prometheus/node.d'
 
-    # Replicate old behavior for 14.04 Trusty
+    package { 'prometheus-node-exporter':
+      ensure => 'present'
+    }
+
+    # Configuration compatibile less than 0.16
+    # TODO: remove after Trusty is gone.
     if ($::lsbdistcodename == 'trusty') {
         $collectors_enabled = join(sort(concat($collectors_default, $collectors_extra)), ',')
-
-        package { 'prometheus-node-exporter':
-          ensure => 'present'
-        }
 
         file { '/etc/default/prometheus-node-exporter':
             ensure  => present,
@@ -67,83 +64,37 @@ class prometheus::node_exporter (
             content => template('prometheus/etc/default/prometheus-node-exporter.erb'),
             notify  => Service['prometheus-node-exporter'],
         }
-    } else {
-        if ($upgrade_one_seven or $::lsbdistcodename == 'buster') {
-            $collectors_enabled = concat($collectors_default, $collectors_extra)
+    }
+    # New configuration style (0.16+)
+    else {
+        $collectors_enabled = concat($collectors_default, $collectors_extra)
 
-            if (os_version('debian <= stretch')) {
-                apt::repository { 'component-prometheus-node-exporter':
-                    uri        => 'http://apt.wikimedia.org/wikimedia',
-                    dist       => "${::lsbdistcodename}-wikimedia",
-                    components => 'component/prometheus-node-exporter',
-                    before     => Package['prometheus-node-exporter'],
-                    notify     => Exec['post-repo-install apt update'],
-                }
+        file { '/etc/default/prometheus-node-exporter':
+              ensure  => present,
+              mode    => '0444',
+              owner   => 'root',
+              group   => 'root',
+              content => template('prometheus/etc/default/prometheus-node-exporter-0.17.erb'),
+              notify  => Service['prometheus-node-exporter'],
+        }
 
-                exec { 'post-repo-install apt update':
-                    command     => '/usr/bin/apt update',
-                    refreshonly => true,
-                }
+        # Disabled because broken (https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=922803)
+        service { 'prometheus-node-exporter-ipmitool-sensor.timer':
+            ensure   => 'stopped',
+            provider => 'systemd',
+            enable   => 'mask',
+            require  => Package['prometheus-node-exporter'],
+        }
 
-                package { 'prometheus-node-exporter':
-                    ensure  => '0.17.0+ds-3',
-                    require => [Exec['apt-get update'], Exec['post-repo-install apt update']],
-                }
-            } elsif ($::lsbdistcodename == 'buster') {
-                package { 'prometheus-node-exporter':
-                    ensure  => installed,
-                }
-            }
-
-            file { '/etc/default/prometheus-node-exporter':
-                  ensure  => present,
-                  mode    => '0444',
-                  owner   => 'root',
-                  group   => 'root',
-                  content => template('prometheus/etc/default/prometheus-node-exporter-0.17.erb'),
-                  notify  => Service['prometheus-node-exporter'],
-            }
-
-            # Disabled because broken (https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=922803)
-            service { 'prometheus-node-exporter-ipmitool-sensor.timer':
-                ensure   => 'stopped',
-                provider => 'systemd',
-                enable   => 'mask',
-                require  => Package['prometheus-node-exporter'],
-            }
-
-            # Disabled in favor of internal smart module (smart-data-dump.py)
-            service { 'prometheus-node-exporter-smartmon.timer':
-                ensure   => 'stopped',
-                provider => 'systemd',
-                enable   => 'mask',
-                require  => Package['prometheus-node-exporter'],
-            }
-
-        } else {
-            $collectors_enabled = join(sort(concat($collectors_default, $collectors_extra)), ',')
-
-            package { 'prometheus-node-exporter':
-                ensure => '0.14.0~git20170523-1'
-            }
-
-            apt::repository { 'component-prometheus-node-exporter':
-                ensure     => 'absent',
-                uri        => '',
-                dist       => '',
-                components => '',
-            }
-
-            file { '/etc/default/prometheus-node-exporter':
-                ensure  => present,
-                mode    => '0444',
-                owner   => 'root',
-                group   => 'root',
-                content => template('prometheus/etc/default/prometheus-node-exporter.erb'),
-                notify  => Service['prometheus-node-exporter'],
-            }
+        # Disabled in favor of internal smart module (smart-data-dump.py)
+        service { 'prometheus-node-exporter-smartmon.timer':
+            ensure   => 'stopped',
+            provider => 'systemd',
+            enable   => 'mask',
+            require  => Package['prometheus-node-exporter'],
         }
     }
+
     # members of this group are able to publish metrics
     # via the 'textfile' collector by writing files to $textfile_directory.
     # prometheus-node-exporter will export all files matching *.prom
