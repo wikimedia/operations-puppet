a6b24f73ba5c6832e755d011513be55006ae4f29
John Bond <jbond@wikimedia.org>
2019-03-28 11:59:24 +0000

    jessie-backports: remove pins for packages in openstack-mitaka-jessie
    Bug: T219333
    Change-Id: I9dbcb5e72f4d9c4c750bcbebbe63dc6645ecfbda

--- modules/profile/manifests/openstack/main/cumin/master.pp
+++ modules/profile/manifests/openstack/main/cumin/master.pp
@@ -40,15 +40,6 @@ class profile::openstack::main::cumin::master(
             }
         }
 
-        if os_version('debian == jessie') {
-            apt::pin { 'cumin-openstack-deps':
-                package  => 'python3-keystoneauth1 python3-keystoneclient python3-novaclient python3-debtcollector python3-keyring python3-oslo.config python3-oslo.i18n python3-oslo.serialization python3-oslo.utils python3-pbr python3-positional python3-urllib3 python3-requests python3-requests-kerberos python3-stevedore',
-                pin      => 'release a=jessie-backports',
-                priority => '1001',
-                before   => [Package['python3-keystoneauth1'], Package['python3-keystoneclient'], Package['python3-novaclient']],
-            }
-        }
-
         # Explicitely require cumin's suggested packages to enable OpenStack backend,
         # --install-suggests would recursively install many more unwanted dependencies.
         # Install clustershell as it provides nodeset CLI that is useful to mangle host lists.
