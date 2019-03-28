725e7f4eeb37a3742591a3f7357b6862e3b4c361
Valentin Gutierrez <vgutierrez@wikimedia.org>
2019-03-28 12:10:48 +0100

    acme_chief: Provide OCSP stapling support
    Bug: T213705
    Change-Id: I437b91c177d97b863a4356ca30b388561f5b9d3c

--- modules/acme_chief/manifests/cert.pp
+++ modules/acme_chief/manifests/cert.pp
@@ -7,9 +7,14 @@
 #           ec-prime256v1.key
 #           [rsa-2048,ec-prime256v1].[chain,chained].crt
 #           [rsa-2048,ec-prime256v1].crt
+#           [rsa-2048,ec-prime256v1].client.ocsp --> OCSP stapling response prefetched by the client
+#           [rsa-2048,ec-prime256v1].ocsp --> OCSP stapling response prefetched by acme-chief server: TBI
 define acme_chief::cert (
+    $ensure = present,
     Variant[String, Undef] $puppet_svc = undef,
     String $key_group = 'root',
+    Boolean $ocsp = false,
+    Variant[String, Undef] $proxy = undef,
 ) {
     require ::acme_chief
 
@@ -24,7 +29,7 @@ define acme_chief::cert (
 
     # lint:ignore:puppet_url_without_modules
     file { "/etc/acmecerts/${title}":
-        ensure    => directory,
+        ensure    => ensure_directory($ensure),
         owner     => 'root',
         group     => $key_group,
         mode      => '0640',
@@ -34,4 +39,38 @@ define acme_chief::cert (
         notify    => Service[$puppet_svc],
     }
     # lint:endignore
+
+    if $ocsp {
+        # This will dissapear as soon as acme-chief performs OCSP stapling centrally
+        require sslcert::ocsp::init # lint:ignore:wmf_styleguide
+
+        ['ec-prime256v1', 'rsa-2048'].each |String $type| {
+            ['live', 'new'].each |String $version| {
+                $config = "/etc/update-ocsp.d/${title}-${version}-${type}.conf"
+                $output = "/etc/acmecerts/${title}/${version}/${type}.client.ocsp"
+                $cert_path = "/etc/acmecerts/${title}/${version}/${type}.crt"
+                file { $config:
+                    ensure  => $ensure,
+                    owner   => 'root',
+                    group   => 'root',
+                    mode    => '0444',
+                    content => template('acme_chief/update-ocsp.erb'),
+                    require => File["/etc/acmecerts/${title}"],
+                }
+
+                if $ensure == 'present' {
+                    # initial creation on the first puppet run
+                    exec { "${title}-${version}-${type}-create-ocsp":
+                        command => "/usr/local/sbin/update-ocsp --config ${config}",
+                        creates => $output,
+                        require => File[$config],
+                    }
+                } else {
+                    file { $output:
+                        ensure => absent,
+                    }
+                }
+            }
+        }
+    }
 }
