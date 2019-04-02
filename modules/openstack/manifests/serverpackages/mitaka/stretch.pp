078793837b475718c176f0da8aeacaac4c859919
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-04-02 13:25:54 +0200

    openstack: serverpackages: stretch: factorize negative apt pinning
    Instead of one for each package, have a single one with all of them.
    
    While at it, include the 'sphinx-common' pkg, which is part of the sphinx
    dependecy chain that should be avoided from openstack-mitaka-jessie due to apt
    trying to downgrade them (thus leading to puppet agent error).
    
    Introduce cleanup file declaration for previous apt pinnings.
    
    Hosts: cloudvirt1030.eqiad.wmnet, cloudnet1003.eqiad.wmnet, cloudcontrol2001-dev.wikimedia.org
    
    Bug: T215407
    Change-Id: I6df33cfeb5ce11a3b8c4c3ddae887eda696c5bfc
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/openstack/manifests/serverpackages/mitaka/stretch.pp
+++ modules/openstack/manifests/serverpackages/mitaka/stretch.pp
@@ -11,37 +11,66 @@ class openstack::serverpackages::mitaka::stretch(
         ensure => 'absent',
     }
 
+    # in stretch, these packages are included in both our custom repo component
+    # and in the stretch stable repo. Avoid conflicts with apt by avoiding
+    # installing the version from our custom repo.
+    $avoid_packages = [
+        'python-cryptography',
+        'libpq5',
+        'libisns0',
+        'libjs-sphinxdoc',
+        'python-sphinx',
+        'sphinx-common',
+        'sqlite3',
+    ]
+
+    $avoid_packages_list = join($avoid_packages, ' ')
+    apt::pin { 'mitaka_stretch_nojessiebpo':
+        package  => $avoid_packages_list,
+        pin      => 'release c=openstack-mitaka-jessie',
+        priority => '-1',
+    }
+
+    #
+    # puppet cleanup, remove after a couple of cycles
+    #
     apt::pin { 'mitaka_stretch_python-cryptography_nojessiebpo':
+        ensure   => 'absent',
         package  => 'python-cryptography',
         pin      => 'version 1.7.1-3~bpo9*',
         priority => '-1',
     }
 
     apt::pin { 'mitaka_stretch_libpq5_nojessiebpo':
+        ensure   => 'absent',
         package  => 'libpq5',
         pin      => 'version 9.6.6-0*',
         priority => '-1',
     }
 
     apt::pin { 'mitaka_stretch_libisns0_nojessiebpo':
+        ensure   => 'absent',
         package  => 'libisns0',
         pin      => 'version 0.97-1*',
         priority => '-1',
     }
 
     apt::pin { 'mitaka_stretch_libjs-sphinxdoc_nojessiebpo':
+        ensure   => 'absent',
         package  => 'libjs-sphinxdoc',
         pin      => 'version 1.4.9-2~bpo8+1',
         priority => '-1',
     }
 
     apt::pin { 'mitaka_stretch_python-sphinx_nojessiebpo':
+        ensure   => 'absent',
         package  => 'python-sphinx',
         pin      => 'version 1.4.9-2~bpo8+1',
         priority => '-1',
     }
 
     apt::pin { 'mitaka_stretch_sqlite3_nojessiebpo':
+        ensure   => 'absent',
         package  => 'sqlite3',
         pin      => 'version 3.16.2-3~bpo8+1',
         priority => '-1',
