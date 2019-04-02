44b912fb8e67be5a39cfd0678452690815b0d7de
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-04-02 12:37:22 +0200

    openstack: neutron-common: fix relationship with sqlite3
    There is no longer need for this explicit relationship, since we don't have
    explicit pinning for this package (see commit 3681724f4f).
    
    Change-Id: I2836be3c3be6de02284614c248780dfc86b3b7f1
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/openstack/manifests/neutron/common/mitaka/stretch.pp
+++ modules/openstack/manifests/neutron/common/mitaka/stretch.pp
@@ -5,7 +5,6 @@ class openstack::neutron::common::mitaka::stretch(
     # packages will be installed from openstack-mitaka-jessie component from
     # the jessie-wikimedia repo, since that has higher apt pinning by default
     package { 'neutron-common':
-        ensure  => 'present',
-        require => Package['sqlite3'],
+        ensure => 'present',
     }
 }
