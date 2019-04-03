b50268fddd7a4217e02318f14fc3c91c1bf59b5b
Jaime Crespo <jcrespo@wikimedia.org>
2019-04-03 11:15:35 +0200

    tendril: Open firewall only from tendril web to tendril db backend
    This used to be too open. Now that it is restricted, allow only
    communication from dbmonitor[12]001.wikimedia.org to db1115 and
    db2093, which contains the backend of the web.
    
    Change-Id: I91f00a9d8575b3bcea03d04bb7169d7ebd017160

--- modules/profile/manifests/mariadb/misc/tendril.pp
+++ modules/profile/manifests/mariadb/misc/tendril.pp
@@ -22,4 +22,13 @@ class profile::mariadb::misc::tendril {
         binlog_format => 'ROW',
         ssl           => 'puppet-cert',
     }
+
+    # Firewall rules for the tendril db hosts so they can be accessed
+    # by tendril and dbtree web server (on a public ip)
+    ferm::service { 'tendril-backend':
+        proto   => 'tcp',
+        port    => '3306',
+        notrack => true,
+        srange  => '@resolve((dbmonitor1001.wikimedia.org dbmonitor2001.wikimedia.org))',
+    }
 }
