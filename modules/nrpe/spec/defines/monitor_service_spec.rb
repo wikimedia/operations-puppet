9d0898e2d9cd175dc1be264c495c43c5b7730c0d
Alexandros Kosiaris <akosiaris@wikimedia.org>
2019-04-05 09:43:06 +0300

    nrpe: Add more rspec tests
    Add a few more tests in preparation for I24833450ab
    
    Change-Id: I526c7faa3d814077657492c83e8ae98f9dfc8b2b

--- modules/nrpe/spec/defines/monitor_service_spec.rb
+++ modules/nrpe/spec/defines/monitor_service_spec.rb
@@ -2,29 +2,84 @@ require 'spec_helper'
 
 describe 'nrpe::monitor_service', :type => :define do
     let(:title) { 'something' }
-    let(:params) do {
-        :description   => 'this is a description',
-        :contact_group => 'none',
-        :nrpe_command  => '/usr/local/bin/mycommand -i this -o that',
-        :critical      => 'false',
-        :timeout       => 42,
-        }
+
+    context 'with ensure present' do
+      let(:params) do {
+          :description   => 'this is a description',
+          :contact_group => 'none',
+          :nrpe_command  => '/usr/local/bin/mycommand -i this -o that',
+          :critical      => 'false',
+          :timeout       => 42,
+          }
+      end
+
+      it 'contains normal resources' do
+          should contain_nrpe__check('check_something').with(
+              :command       => '/usr/local/bin/mycommand -i this -o that',
+              :ensure        => 'present'
+          )
+          should contain_monitoring__service('something').with(
+              :description   => 'this is a description',
+              :contact_group => 'none',
+              :retries       => 3,
+              :ensure        => 'present',
+              :check_command => 'nrpe_check!check_something!42',
+              :critical      => 'false'
+          )
+      end
     end
 
-    it do
-        should contain_nrpe__check('check_something').with(
-            :command       => '/usr/local/bin/mycommand -i this -o that',
-            :ensure        => 'present'
-        )
+    context 'with ensure present, description missing' do
+      let(:params) do {
+          :nrpe_command  => '/usr/local/bin/mycommand -i this -o that',
+          :contact_group => 'none',
+          :critical      => 'false',
+          :timeout       => 42,
+          }
+      end
+      it 'does not compile' do
+        should_not compile
+      end
     end
-    it do
-        should contain_monitoring__service('something').with(
-            :description   => 'this is a description',
-            :contact_group => 'none',
-            :retries       => 3,
-            :ensure        => 'present',
-            :check_command => 'nrpe_check!check_something!42',
-            :critical      => 'false'
-        )
+
+    context 'with ensure present, nrpe_command missing' do
+      let(:params) do {
+          :description   => 'none',
+          :contact_group => 'none',
+          :critical      => 'false',
+          :timeout       => 42,
+          }
+      end
+      it 'does not compile' do
+        should_not compile
+      end
+    end
+
+    context 'with ensure absent, nrpe_command missing' do
+      let(:params) do {
+          :description   => 'none',
+          :contact_group => 'none',
+          :critical      => 'false',
+          :timeout       => 42,
+          :ensure        => 'absent',
+          }
+      end
+      it 'does not compile' do
+        should_not compile
+      end
+    end
+
+    context 'with ensure absent, description missing' do
+      let(:params) do {
+          :nrpe_command  => '/usr/local/bin/mycommand -i this -o that',
+          :contact_group => 'none',
+          :critical      => 'false',
+          :timeout       => 42,
+          :ensure        => 'absent',
+          }
+      end
+      it 'does not compile' do
+        should_not compile
+      end
     end
 end
