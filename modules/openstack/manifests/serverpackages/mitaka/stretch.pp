3c4cb0d06e2112e11aff94bedeb4f9eeff7cddd4
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-04-08 14:00:49 +0200

    openstack: serverpackages: mitaka: stretch: drop puppet cleanup code
    No longer required.
    
    Change-Id: Ia7ebdb7128ea063c8acc9bfc13644d09a15828ff
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/openstack/manifests/serverpackages/mitaka/stretch.pp
+++ modules/openstack/manifests/serverpackages/mitaka/stretch.pp
@@ -31,49 +31,4 @@ class openstack::serverpackages::mitaka::stretch(
         pin      => 'release c=openstack-mitaka-jessie',
         priority => '-1',
     }
-
-    #
-    # puppet cleanup, remove after a couple of cycles
-    #
-    apt::pin { 'mitaka_stretch_python-cryptography_nojessiebpo':
-        ensure   => 'absent',
-        package  => 'python-cryptography',
-        pin      => 'version 1.7.1-3~bpo9*',
-        priority => '-1',
-    }
-
-    apt::pin { 'mitaka_stretch_libpq5_nojessiebpo':
-        ensure   => 'absent',
-        package  => 'libpq5',
-        pin      => 'version 9.6.6-0*',
-        priority => '-1',
-    }
-
-    apt::pin { 'mitaka_stretch_libisns0_nojessiebpo':
-        ensure   => 'absent',
-        package  => 'libisns0',
-        pin      => 'version 0.97-1*',
-        priority => '-1',
-    }
-
-    apt::pin { 'mitaka_stretch_libjs-sphinxdoc_nojessiebpo':
-        ensure   => 'absent',
-        package  => 'libjs-sphinxdoc',
-        pin      => 'version 1.4.9-2~bpo8+1',
-        priority => '-1',
-    }
-
-    apt::pin { 'mitaka_stretch_python-sphinx_nojessiebpo':
-        ensure   => 'absent',
-        package  => 'python-sphinx',
-        pin      => 'version 1.4.9-2~bpo8+1',
-        priority => '-1',
-    }
-
-    apt::pin { 'mitaka_stretch_sqlite3_nojessiebpo':
-        ensure   => 'absent',
-        package  => 'sqlite3',
-        pin      => 'version 3.16.2-3~bpo8+1',
-        priority => '-1',
-    }
 }
