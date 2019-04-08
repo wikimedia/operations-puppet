60fc4b7b3601dd2f25692763754ef5f24a1871e3
Antoine Musso <hashar@free.fr>
2019-04-08 14:32:00 +0200

    zuul: stop pinning python-pbr
    The zuul version comes from the Debian package version. It was
    compatible not compatible with python-pbr 1.10 and we had to pin it to
    the verison that comes from Jessie.
    
    Eventually Zuul 2.5.1-wmf6 gets the version from Distribute instead of
    relying on python-pbr.  We thus no more need the package pinning.
    
    Bug: T218559
    Change-Id: I21895ddd66f03520682c867f40142427c646fed0

--- modules/zuul/manifests/init.pp
+++ modules/zuul/manifests/init.pp
@@ -11,21 +11,6 @@ class zuul ( ){
 
     include zuul::user
 
-    # Pin python-pbr to an old version for Zuul
-
-    # Zuul has a version string that isn't SemVer compliant.
-    # Ideally we should rebuild Zuul with a new version string,
-    # but for now this will do.
-    if os_version('debian == jessie') {
-        include ::apt
-        apt::pin { 'python-pbr':
-            pin      => 'release o=debian',
-            priority => '1002',
-            before   => Package['zuul'],
-        }
-    }
-
-
     package { 'zuul':
         ensure => present,
     }
