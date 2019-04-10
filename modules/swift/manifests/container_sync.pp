fa5821510dd73c7d1b26c5e90b5ff83584bf7b8d
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2019-04-10 12:28:07 +0200

    swift: don't show container-sync-realms diff
    Noticed while working on T214289, this file can have private data in it.
    
    Change-Id: I20a08858f17a34fd0a9b85fec9696cc1c6446186

--- modules/swift/manifests/container_sync.pp
+++ modules/swift/manifests/container_sync.pp
@@ -3,10 +3,11 @@ class swift::container_sync (
     $replication_keys     = $::swift::params::replication_keys,
 ) {
     file { '/etc/swift/container-sync-realms.conf':
-        ensure  => present,
-        mode    => '0440',
-        owner   => 'swift',
-        group   => 'swift',
-        content => template('swift/container-sync-realms.conf.erb'),
+        ensure    => present,
+        mode      => '0440',
+        owner     => 'swift',
+        group     => 'swift',
+        content   => template('swift/container-sync-realms.conf.erb'),
+        show_diff => false,
     }
 }
