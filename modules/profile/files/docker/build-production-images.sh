777f62df56ba53e2123550db657eada8d0925497
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-04-10 17:13:12 +0200

    profile::docker::builder: fix shell script for docker-pkg 2.0.0
    Change-Id: I11940cbeb2c069e1d14c17370fe035a1c0bab7cb

--- modules/profile/files/docker/build-production-images.sh
+++ modules/profile/files/docker/build-production-images.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /srv/images/production-images \
-   && /srv/deployment/docker-pkg/venv/bin/docker-pkg -c /etc/production-images/config.yaml images
+   && /srv/deployment/docker-pkg/venv/bin/docker-pkg -c /etc/production-images/config.yaml build images
