d5c2f1763f1b53a4b1eef98bd6e149953201890b
John Bond <jbond@wikimedia.org>
2019-04-11 17:36:24 +0100

    debdeploy: add zsh autocompletion script
    Change-Id: I2415db29ee22a4048576a62e340e02add7fba492

--- modules/profile/manifests/debdeploy.pp
+++ modules/profile/manifests/debdeploy.pp
@@ -9,4 +9,10 @@ class profile::debdeploy {
         group  => 'root',
         source => 'puppet:///modules/profile/debdeploy/debdeploy.conf',
     }
+    file { '/usr/local/share/zsh/site-functions/_debdeploy':
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/profile/debdeploy/_debdeploy',
+    }
 }
