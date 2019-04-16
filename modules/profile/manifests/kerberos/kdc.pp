db27747f3eb1c0fc414f6a870a82a7c4c52e40e1
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2019-04-16 16:58:04 +0200

    Fix data type for krb_kdc_servers
    We'll support more than one KDC server.
    
    Change-Id: Ibf4029062b1627c57fd9cf00be19d4a3c6696c20

--- modules/profile/manifests/kerberos/kdc.pp
+++ modules/profile/manifests/kerberos/kdc.pp
@@ -1,6 +1,6 @@
 class profile::kerberos::kdc (
     Stdlib::Fqdn $krb_realm_name = lookup('kerberos_realm_name'),
-    Stdlib::Fqdn $krb_kdc_servers = lookup('kerberos_kdc_servers'),
+    Array[Stdlib::Fqdn] $krb_kdc_servers = lookup('kerberos_kdc_servers'),
 ) {
     package { 'krb5-kdc':
         ensure => present,
