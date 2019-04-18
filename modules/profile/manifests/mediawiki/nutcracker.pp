d3c9700d7287c1aab35d212eca26184ea40359bd
Luca Toscano <ltoscano@wikimedia.org>
2019-04-18 09:48:56 +0200

    profile::mediawiki::nutcracker: make memcached configuration optional
    Mcrouter has replaced nutcracker for memcached traffic a long time ago,
    so this change makes the memcached config optional unblocking its config
    removal from mediawiki servers.
    
    Bug: T214275
    Change-Id: Iacedc6a0b2e01a5b253d89e60f682bdf4704ca51

--- modules/profile/manifests/mediawiki/nutcracker.pp
+++ modules/profile/manifests/mediawiki/nutcracker.pp
@@ -2,55 +2,62 @@
 #
 # Configures nutcracker for mediawiki
 class profile::mediawiki::nutcracker(
-    $memcached_servers = hiera('mediawiki_memcached_servers'),
     $redis_shards      = hiera('redis::shards'),
     $datacenters       = hiera('datacenters'),
+    $memcached_servers = hiera('mediawiki_memcached_servers', []),
 ) {
     $redis_servers = $redis_shards['sessions']
     include ::passwords::redis
     include ::profile::prometheus::nutcracker_exporter
 
-    $pools = {
-        'memcached'     => {
-            auto_eject_hosts     => true,
-            distribution         => 'ketama',
-            hash                 => 'md5',
-            listen               => '127.0.0.1:11212',
-            preconnect           => true,
-            server_connections   => 1,
-            server_failure_limit => 3,
-            server_retry_timeout => to_milliseconds('30s'),
-            timeout              => 250,
-            servers              => $memcached_servers,
-        },
+    $redis_eqiad_pool = {
+        auto_eject_hosts     => true,
+        distribution         => 'ketama',
+        redis                => true,
+        redis_auth           => $passwords::redis::main_password,
+        hash                 => 'md5',
+        listen               => '/var/run/nutcracker/redis_eqiad.sock 0666',
+        server_connections   => 1,
+        server_failure_limit => 3,
+        server_retry_timeout => to_milliseconds('30s'),
+        timeout              => 1000,
+        server_map           => $redis_servers['eqiad'],
+    }
 
-        'mc-unix'       => {
-            auto_eject_hosts     => true,
-            distribution         => 'ketama',
-            hash                 => 'md5',
-            listen               => '/var/run/nutcracker/nutcracker.sock 0666',
-            preconnect           => true,
-            server_connections   => 1,
-            server_failure_limit => 3,
-            server_retry_timeout => to_milliseconds('30s'),
-            timeout              => 250,
-            servers              => $memcached_servers,
-        },
+    if $memcached_servers == [] {
+        $pools = {
+            'redis_eqiad' => $redis_eqiad_pool,
+        }
+    } else {
+        $pools = {
+            'memcached'     => {
+                auto_eject_hosts     => true,
+                distribution         => 'ketama',
+                hash                 => 'md5',
+                listen               => '127.0.0.1:11212',
+                preconnect           => true,
+                server_connections   => 1,
+                server_failure_limit => 3,
+                server_retry_timeout => to_milliseconds('30s'),
+                timeout              => 250,
+                servers              => $memcached_servers,
+            },
 
-        'redis_eqiad'           =>  {
-            auto_eject_hosts     => true,
-            distribution         => 'ketama',
-            redis                => true,
-            redis_auth           => $passwords::redis::main_password,
-            hash                 => 'md5',
-            listen               => '/var/run/nutcracker/redis_eqiad.sock 0666',
-            server_connections   => 1,
-            server_failure_limit => 3,
-            server_retry_timeout => to_milliseconds('30s'),
-            timeout              => 1000,
-            server_map           => $redis_servers['eqiad'],
-        },
+            'mc-unix'       => {
+                auto_eject_hosts     => true,
+                distribution         => 'ketama',
+                hash                 => 'md5',
+                listen               => '/var/run/nutcracker/nutcracker.sock 0666',
+                preconnect           => true,
+                server_connections   => 1,
+                server_failure_limit => 3,
+                server_retry_timeout => to_milliseconds('30s'),
+                timeout              => 250,
+                servers              => $memcached_servers,
+            },
 
+            'redis_eqiad'   =>  $redis_eqiad_pool,
+        }
     }
 
     if member($datacenters, 'codfw') {
