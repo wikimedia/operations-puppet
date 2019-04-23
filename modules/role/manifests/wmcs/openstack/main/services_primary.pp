d0edf58c6dbd387a499bdd2ac1ab406238d74901
andrewbogott <abogott@wikimedia.org>
2019-04-23 06:45:27 -0500

    Temporary hack: turn off designate on labservices1001
    I need DNS here for a bit longer, no need to mess with designate
    
    Bug: T221183
    Change-Id: I65b8c4182d2704895bca6b636af1b0a5b9f2568b

--- modules/role/manifests/wmcs/openstack/main/services_primary.pp
+++ modules/role/manifests/wmcs/openstack/main/services_primary.pp
@@ -5,8 +5,8 @@ class role::wmcs::openstack::main::services_primary {
     include ::profile::openstack::main::pdns::auth::db
     include ::profile::openstack::main::pdns::auth::service
     include ::profile::openstack::main::pdns::recursor::primary
-    include ::profile::openstack::main::designate::service
-    include ::profile::prometheus::pdns_exporter
-    include ::profile::prometheus::pdns_rec_exporter_wmcs
+    #include ::profile::openstack::main::designate::service
+    #include ::profile::prometheus::pdns_exporter
+    #include ::profile::prometheus::pdns_rec_exporter_wmcs
     include ::profile::ldap::client::labs
 }
