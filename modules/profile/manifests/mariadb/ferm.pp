23ca96b31824e680f73c5ddfe8f54198a85430e8
Jaime Crespo <jcrespo@wikimedia.org>
2019-04-24 12:49:48 +0200

    mariadb-ferm: Update comment to clarify intention of generic define
    Update comment explaining the ideas and future of the mariadb::ferm
    define.
    
    Change-Id: I60496c7bad1aae4689c2f95158aca65f4bdea38a

--- modules/profile/manifests/mariadb/ferm.pp
+++ modules/profile/manifests/mariadb/ferm.pp
@@ -1,6 +1,13 @@
-# Common ferm resource for database access. The actual databases are listening on 3306
-# and are initially limited to the internal network. More specialised classes
-# could grant additional access to other hosts
+# ferm define for generic production database access.
+# Two ports are opened here:
+# The main, production use port, which by default is 3306, but
+# could be any other, usually in the 3310-3330 range.
+# The extra port, which resides on 3307 by default, and on
+# production port + 20 on the others (e.g. 3317 has its extra port on 3337).
+# Production port access is, for now, limited to the internal network.
+# Extra port is limited to mysql production root hosts (cumin, tendril).
+# More specialised classes could grant additional access to other hosts or
+# subnetworks.
 
 define profile::mariadb::ferm (
     $port = '3306',
