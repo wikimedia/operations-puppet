9507e29a707e146657c33a93425b0ceedebc2404
fsero <fsero@wikimedia.org>
2019-04-24 17:09:42 +0200

    registryha: registry health check was querying wrong port
    Bug: T221759
    Change-Id: I7ac40ba4274bfcce089bb81aa29a5f48f336a519

--- modules/profile/manifests/docker_registry_ha/registry.pp
+++ modules/profile/manifests/docker_registry_ha/registry.pp
@@ -124,10 +124,10 @@ class profile::docker_registry_ha::registry(
         notes_url     => 'https://wikitech.wikimedia.org/wiki/Docker',
     }
 
-    # This will test both nginx and the docker registry application
+    # This will query /debug/health registry endpoint on 5001 debug server
     monitoring::service { 'check_docker_registry_health':
         description   => 'Docker registry health',
-        check_command => "check_https_url_for_string!${::fqdn}:5001!/debug/health!{}",
+        check_command => "check_http_url_for_regexp_on_port!${::fqdn}:5001!5001!/debug/health!{}",
         notes_url     => 'https://wikitech.wikimedia.org/wiki/Docker',
     }
 
