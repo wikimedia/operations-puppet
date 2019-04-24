1b7fa4d5cdff96fd6fadae216fa60b86045a7d7c
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2019-04-24 09:45:29 +0200

    grub: Remove fallback code for augeas < 1.2
    Per Puppetboard the augeasversion fact is at least 1.2.0 across the
    fleet.
    
    Change-Id: Id9d8e0ac436f75a0d4e72e54dbab768bf9e6008f

--- modules/grub/manifests/defaults.pp
+++ modules/grub/manifests/defaults.pp
@@ -14,40 +14,18 @@
 class grub::defaults {
     include ::grub
 
-    # The augeas Shellvars_list lens can't handle backticks for versions < 1.2
-    # We fallback to the legacy grep/sed method in that case.
-    if versioncmp($::augeasversion, '1.2.0') >= 0 {
-        augeas { 'grub2':
-            incl    => '/etc/default/grub',
-            lens    => 'Shellvars_list.lns',
-            changes => [
-                # set terminal; default usually is just serial
-                'set GRUB_TERMINAL/quote \'"\'',
-                'set GRUB_TERMINAL/value[1] console',
-                'set GRUB_TERMINAL/value[2] serial',
-                # removes quiet, splash from default kopts
-                'rm GRUB_CMDLINE_LINUX_DEFAULT/value[. = "quiet"]',
-                'rm GRUB_CMDLINE_LINUX_DEFAULT/value[. = "splash"]',
-            ],
-            notify  => Exec['update-grub'],
-        }
-    } else {
-        # Disable the 'quiet' kernel command line option so console messages
-        # will be printed.
-        exec { 'grub2 remove quiet':
-            path    => '/bin:/usr/bin',
-            command => "sed -r -i '/^GRUB_CMDLINE_LINUX_DEFAULT/s/quiet( splash)?//' /etc/default/grub",
-            onlyif  => "grep -E -q '^GRUB_CMDLINE_LINUX_DEFAULT=.*quiet( splash)?' /etc/default/grub",
-            notify  => Exec['update-grub'],
-        }
-
-        # show the GRUB menu on both console & serial (default is serial)
-        exec { 'grub2 terminal':
-            path    => '/bin:/usr/bin',
-            command => "sed -i '/^GRUB_TERMINAL/s/=.*/=\"console serial\"/' /etc/default/grub",
-            unless  => "grep -q '^GRUB_TERMINAL=.*console serial' /etc/default/grub",
-            onlyif  => 'test -f /etc/default/grub',
-            notify  => Exec['update-grub'],
-        }
+    augeas { 'grub2':
+        incl    => '/etc/default/grub',
+        lens    => 'Shellvars_list.lns',
+        changes => [
+                    # set terminal; default usually is just serial
+                    'set GRUB_TERMINAL/quote \'"\'',
+                    'set GRUB_TERMINAL/value[1] console',
+                    'set GRUB_TERMINAL/value[2] serial',
+                    # removes quiet, splash from default kopts
+                    'rm GRUB_CMDLINE_LINUX_DEFAULT/value[. = "quiet"]',
+                    'rm GRUB_CMDLINE_LINUX_DEFAULT/value[. = "splash"]',
+                    ],
+        notify  => Exec['update-grub'],
     }
 }
