f74259c0817a67fa130c3b802d3ccaed99d3f931
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-04-25 12:57:05 +0200

    openstack: neutron: l3_agent: increase conntrack table size
    Previous to this patch we were using 80% of the table. Increase it
    to gain more room.
    
    We have 25GB free RAM in cloudnet servers, so this should be safe.
    
    Bug: T221760
    Change-Id: Ia364bc813219afa759a80f82036fc59be53be9cf
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/openstack/manifests/neutron/l3_agent.pp
+++ modules/openstack/manifests/neutron/l3_agent.pp
@@ -25,20 +25,23 @@ class openstack::neutron::l3_agent(
     sysctl::parameters { 'openstack':
         values   => {
             # Turn off IP filter
-            'net.ipv4.conf.default.rp_filter' => 0,
-            'net.ipv4.conf.all.rp_filter'     => 0,
+            'net.ipv4.conf.default.rp_filter'    => 0,
+            'net.ipv4.conf.all.rp_filter'        => 0,
 
             # Enable IP forwarding
-            'net.ipv4.ip_forward'             => 1,
-            'net.ipv6.conf.all.forwarding'    => 1,
+            'net.ipv4.ip_forward'                => 1,
+            'net.ipv6.conf.all.forwarding'       => 1,
 
             # Disable RA
-            'net.ipv6.conf.all.accept_ra'     => 0,
+            'net.ipv6.conf.all.accept_ra'        => 0,
 
             # Increase connection tracking size
-            # and bucket since all of labs is
-            # tracked on the network host
-            'net.netfilter.nf_conntrack_max'  => 262144,
+            # and bucket since all of CloudVPS VM instances ingress/egress
+            # are flowing through cloudnet servers
+            # default buckets is 65536. Let's use x4; 65536 * 4 = 262144
+            # default max is buckets x4; 262144 * 4 = 1048576
+            'net.netfilter.nf_conntrack_buckets' => 262144,
+            'net.netfilter.nf_conntrack_max'     => 1048576,
         },
         priority => 50,
     }
