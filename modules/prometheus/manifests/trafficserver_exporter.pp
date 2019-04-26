2e279c67ef53adfa9b3a2f4e985e6c19b1c144ff
Valentin Gutierrez <vgutierrez@wikimedia.org>
2019-04-26 14:24:49 +0200

    prometheus: Support several instances of the trafficserver exporter
    Bug: T221217
    Change-Id: I804ca8f2fabd375f8615b97d07b6660da4d1cbd8

--- modules/prometheus/manifests/trafficserver_exporter.pp
+++ modules/prometheus/manifests/trafficserver_exporter.pp
@@ -2,6 +2,9 @@
 
 # === Parameters
 #
+# [*instance_name*]
+#  Traffic server instance name (default: backend)
+#
 # [*$endpoint*]
 #  The stats_over_http Traffic Server URL
 #
@@ -9,22 +12,25 @@
 #  The TCP port to listen on
 
 define prometheus::trafficserver_exporter (
+    String $instance_name = 'backend',
     Stdlib::HTTPUrl $endpoint  = 'http://127.0.0.1/_stats',
     Wmflib::UserIpPort $listen_port = 9122,
 ) {
     require_package('prometheus-trafficserver-exporter')
 
-    systemd::service { 'prometheus-trafficserver-exporter':
+    $service_name = "prometheus-trafficserver-${instance_name}-exporter"
+
+    systemd::service { $service_name:
         ensure  => present,
         restart => true,
         content => systemd_template('prometheus-trafficserver-exporter@'),
     }
 
-    monitoring::service { 'trafficserver_exporter_check_http':
+    monitoring::service { "trafficserver_${instance_name}_exporter_check_http":
         description   => "Ensure traffic_exporter binds on port ${listen_port} and responds to HTTP requests",
         check_command => "check_http_port_url!${listen_port}!/",
         notes_url     => 'https://wikitech.wikimedia.org/wiki/Apache_Traffic_Server',
     }
 
-    base::service_auto_restart { 'prometheus-trafficserver-exporter': }
+    base::service_auto_restart { $service_name: }
 }
