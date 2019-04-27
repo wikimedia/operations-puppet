72e50df4ee34f506a98d250b97c88999712708ed
Alex Monk <krenair@gmail.com>
2019-04-27 17:11:21 +0100

    Puppet CAs: Make it easy to swap CAs by hiera change
    The idea being to change this at the same time as the 'puppetmaster' hiera key.
    The last run against the old master can change this CA as well as the puppet
    config, meaning it can trust the new puppetmaster without local root
    intervention.
    
    Bug: T220268
    Change-Id: I891b97e10a58dee572f92cd62fc504cd29aecbc6

--- modules/profile/manifests/base/certificates.pp
+++ modules/profile/manifests/base/certificates.pp
@@ -1,4 +1,11 @@
-class profile::base::certificates {
+# The puppet_ca_content variable here is intended to be used temporarily to assist with a puppetmaster
+# switch. You can set it at the same time as changing an agent's puppetmaster, obviously using the CA of
+# the new puppetmaster, and on the agent's last run with the old puppetmaster, it will replace the CA file
+# with the new one. This will make it able to talk to the new puppetmaster on its next run.
+# A puppetmaster's CA cert can be found at /var/lib/puppet/server/ssl/certs/ca.pem
+class profile::base::certificates (
+    $puppet_ca_content = hiera('profile::base::certificates::puppet_ca_content', undef),
+) {
     include ::sslcert
 
     sslcert::ca { 'wmf_ca_2017_2020':
@@ -17,10 +24,15 @@ class profile::base::certificates {
         source  => 'puppet:///modules/base/ca/GlobalSign_Organization_Validation_CA_-_SHA256_-_G2.crt',
     }
 
-    $puppet_ssl_dir = puppet_ssldir()
-
-    sslcert::ca { 'Puppet_Internal_CA':
-        source => "${puppet_ssl_dir}/certs/ca.pem",
+    if $puppet_ca_content {
+        sslcert::ca { 'Puppet_Internal_CA':
+            content => $puppet_ca_content,
+        }
+    } else {
+        $puppet_ssl_dir = puppet_ssldir()
+        sslcert::ca { 'Puppet_Internal_CA':
+            source => "${puppet_ssl_dir}/certs/ca.pem",
+        }
     }
 
     # install all CAs before generating certificates
