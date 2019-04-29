fdf5711c869a8d4c36e946f3939f3c3721b23a6f
bstorm <bstorm@wikimedia.org>
2019-04-29 13:10:12 -0600

    cloudstore: edit ferm rules a bit more
    Bug: T209527
    Change-Id: I8227374649a34f2c8d469622ec6a918d12212f64

--- modules/profile/manifests/wmcs/nfs/rsync/ferm.pp
+++ modules/profile/manifests/wmcs/nfs/rsync/ferm.pp
@@ -1,7 +1,8 @@
 # Setup ferm rules for rsync - currently this is only for backup
 class profile::wmcs::nfs::rsync::ferm(
-    $rsync_hosts = hiera('secondary_nfs_servers'),
+    Array[Stdlib::Host] $secondary_nfs_servers = lookup('secondary_nfs_servers'),
 ) {
+    $rsync_hosts = join($secondary_nfs_servers, ' ')
     ferm::service {'secondary_rsyncd':
         port   => '873',
         proto  => 'tcp',
