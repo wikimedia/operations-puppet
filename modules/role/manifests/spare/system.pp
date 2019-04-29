fb6c416d4988197fc984aaea39ae7a349d71af68
Alex Monk <krenair@gmail.com>
2019-04-29 18:05:26 +0100

    role::spare::system: replace standard with profile::standard
    See various parent commits
    
    Change-Id: Ifc7d829045bc1d471471218e154fb9ba54a646a5

--- modules/role/manifests/spare/system.pp
+++ modules/role/manifests/spare/system.pp
@@ -1,15 +1,15 @@
 # This is a role for systems which are in the process of being reclaimed
 # or decommissioned. IOW. the host once had a feature role assigned, but
-# currently no has it. If such hosts are simply reset to "include ::standard"
-# in site.pp, they are no longer matched by role-based server addressing
-# in debdeploy.
+# currently no has it. If such hosts are simply reset to "include
+# ::profile::standard" in site.pp, they are no longer matched by role-based
+# server addressing in debdeploy.
 #
 # This role is entirely transient. Once a system has been reclaimed to spares
 # or decomissioned, this role is removed from site.pp along with the host entry.
 #
 # filtertags: labs-project-puppet
 class role::spare::system {
-    include ::standard
+    include ::profile::standard
     include ::profile::base::firewall
 
     system::role { 'spare::system': description => 'Unused spare system' }
