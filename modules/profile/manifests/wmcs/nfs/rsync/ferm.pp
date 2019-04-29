04e6eb712f35f931de00c6de0220e479fbe80a6b
bstorm <bstorm@wikimedia.org>
2019-04-29 12:07:04 -0600

    cloudstore: correct problems in ferm rules
    Don't be stupid when copy-pasting
    
    Bug: T209527
    Change-Id: I333123d2d2720732470edad76f10d0efc11399e5

--- modules/profile/manifests/wmcs/nfs/rsync/ferm.pp
+++ modules/profile/manifests/wmcs/nfs/rsync/ferm.pp
@@ -1,16 +1,10 @@
 # Setup ferm rules for rsync - currently this is only for backup
 class profile::wmcs::nfs::rsync::ferm(
-    $rsync_clients = hiera('secondary_nfs_servers'),
+    $rsync_hosts = hiera('secondary_nfs_servers'),
 ) {
-    ferm::service {'dumps_rsyncd_ipv4':
+    ferm::service {'secondary_rsyncd':
         port   => '873',
         proto  => 'tcp',
-        srange => "@resolve((${rsync_clients}))",
-    }
-
-    ferm::service {'dumps_rsyncd_ipv6':
-        port   => '873',
-        proto  => 'tcp',
-        srange => "@resolve((${rsync_clients}),AAAA)",
+        srange => "(@resolve((${rsync_hosts})) @resolve((${rsync_hosts}), AAAA))",
     }
 }
