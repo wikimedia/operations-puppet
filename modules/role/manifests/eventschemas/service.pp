00763ce000b695d319c1c51c70577afff89acab7
Ottomata <aotto@wikimedia.org>
2019-04-30 11:54:25 -0400

    Include profile::standard and base::firewall in role::eventschemas::service
    Bug: T219556
    Change-Id: Id35c39698910b49ed1bbc59c9689355cb9df1f87

--- modules/role/manifests/eventschemas/service.pp
+++ modules/role/manifests/eventschemas/service.pp
@@ -4,6 +4,8 @@ class role::eventschemas::service {
     system::role { 'eventschemas::service':
         description => 'HTTP Service for event schemas'
     }
+    include ::profile::standard
+    include ::profile::base::firewall
 
     include ::profile::eventschemas::service
     include ::profile::lvs::realserver
