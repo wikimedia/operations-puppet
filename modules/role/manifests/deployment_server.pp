2537631fbd42fb6209bb4454fbae0472fdc64a3b
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-05-07 15:55:14 +0200

    role::deployment_server: fold in the base class
    Change-Id: I019266359ea8b4cbd2881ca1f2d0508c781c10ca

--- modules/role/manifests/deployment_server.pp
+++ modules/role/manifests/deployment_server.pp
@@ -1,13 +1,30 @@
 # Mediawiki Deployment Server (prod)
 class role::deployment_server {
-    require role::deployment_server::base
-    # All needed classes for deploying mediawiki
+    include ::profile::standard
+    include ::profile::base::firewall
+
+    # Install the scap server components.
+    include ::profile::mediawiki::common
     include ::profile::mediawiki::mcrouter_wancache
     include ::profile::prometheus::mcrouter_exporter
+    include ::profile::mediawiki::nutcracker
+    include ::profile::mediawiki::deployment::server
+    include ::profile::scap::dsh
+    # Install the keyholder agents
+    include ::profile::keyholder::server
+    # Install conftool
+    include ::profile::conftool::client
+
+    # All needed classes for deploying mediawiki
     include ::profile::releases::mediawiki::security
     include ::profile::releases::upload
+
+    # Kubernetes deployments
     include ::profile::kubernetes::deployment_server
+
+    # apache-fast-test and co.
     include ::profile::mediawiki::web_testing
+
     include ::profile::backup::host
     backup::set {'home': }
     # proxy for connection to other servers
