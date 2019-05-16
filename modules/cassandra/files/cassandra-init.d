e8fd2d816d82b3a7a95811b2079c9bb4a9a64244
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2019-05-16 10:19:37 +0200

    cassandra: add init.d 'stop' action
    Puppet will attempt to set cassandra service as stopped, and effectively
    do nothing, thus leaving the default cassandra instance running on first puppet run.
    
    Add only 'stop' action that DTRT.
    
    Bug: T214166
    Bug: T219404
    Change-Id: Idc1835ce8d7fee88b0f9dfac6e22e4c8647dd230

--- modules/cassandra/files/cassandra-init.d
+++ modules/cassandra/files/cassandra-init.d
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-# No (https://phabricator.wikimedia.org/T127365).
+# Replace stock Cassandra init (https://phabricator.wikimedia.org/T127365).
+
+. /lib/lsb/init-functions
+
+case "$1" in
+  stop)
+    if [ -s /var/run/cassandra/cassandra.pid ]; then
+      log_daemon_msg "Stopping Cassandra"
+      kill $(cat /var/run/cassandra/cassandra.pid)
+      log_end_msg 0
+    fi
+    ;;
+esac
 
 exit 0
