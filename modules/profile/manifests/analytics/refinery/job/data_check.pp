3aa641aad2b51c803aa0a5f2cd3e8fc2863515eb
Luca Toscano <ltoscano@wikimedia.org>
2019-05-17 12:26:13 +0200

    profile::analytics::refinery::job::data_check: run checks as analytics
    Change-Id: I6cb1af7fc98ffb3bc46b91c4bf36a2957cbef65f

--- modules/profile/manifests/analytics/refinery/job/data_check.pp
+++ modules/profile/manifests/analytics/refinery/job/data_check.pp
@@ -21,11 +21,13 @@ class profile::analytics::refinery::job::data_check {
         description => 'Check HDFS Webrequest partitions',
         command     => "${::profile::analytics::refinery::path}/bin/refinery-dump-status-webrequest-partitions --hdfs-mount ${hdfs_mount_point} --datasets webrequest,raw_webrequest --quiet --percent-lost",
         interval    => '*-*-* 10:00:00',
+        user        => 'analytics',
     }
 
     profile::analytics::systemd_timer { 'check_pageviews_partitions':
         description => 'Check HDFS Pageviews partitions',
         command     => "${::profile::analytics::refinery::path}/bin/refinery-dump-status-webrequest-partitions --hdfs-mount ${hdfs_mount_point} --datasets pageview,projectview --quiet",
         interval    => '*-*-* 10:10:00',
+        user        => 'analytics',
     }
 }
