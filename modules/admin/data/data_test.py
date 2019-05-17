abbecbf611c0a6fa27784695533976191a3913dd
John Bond <jbond@wikimedia.org>
2019-05-17 13:21:31 +0100

    admin module: improve CI
    Add additional checks to ensure members of all groups exist as a real users
    
    The first patch set will add the none existent users to bobbybobbobob to
    trigger a CI failure
    
    Note: an earlier patch added a test to ensure users have at least
    one group however this is a valid configuration.  User will be dynamically
    added to the `all-users` group which is used by the bastion servers and
    people.wikimedia.org
    
    *I have also updated some flake8 violations
    
    Change-Id: Ifa29d5fe30754671ad18287bb361af58819da10e

--- modules/admin/data/data_test.py
+++ modules/admin/data/data_test.py
@@ -3,10 +3,22 @@
 # 2014 Chase Pettet
 # Tests to perform basic validation on data.yaml
 
-from collections import Counter
 import os
 import unittest
+from collections import Counter, Iterable
+
 import yaml
+
+
+def flatten(not_flat):
+    '''flatten a complex list of lists'''
+    # https://stackoverflow.com/a/2158532/3075306
+    for element in not_flat:
+        if isinstance(element, Iterable) and not isinstance(element, basestring):
+            for sub_list in flatten(element):
+                yield sub_list
+        else:
+            yield element
 
 
 class DataTest(unittest.TestCase):
@@ -18,20 +30,21 @@ class DataTest(unittest.TestCase):
         with open(os.path.join(os.path.dirname(__file__), 'data.yaml')) as f:
             cls.admins = yaml.safe_load(f)
 
-    def testGroupGIDSAreUniques(self):
-        # ensure no two groups uses the same gid
+    def test_group_gids_are_uniques(self):
+        """Ensure no two groups uses the same gid"""
         gids = filter(None, [
             v.get('gid', None) for k, v in self.admins['groups'].iteritems()])
         dupes = [k for k, v in Counter(gids).items() if v > 1]
         self.assertEqual([], dupes, 'Duplicate group GIDs: %r' % dupes)
 
-    def testAbsentMembers(self):
+    def test_absent_members(self):
+        """Ensure absent users in the absent group and have ensure => absent"""
         absent_members = set(self.admins['groups']['absent']['members'])
-        absentees = set([
+        absentees = set(
             username
             for username, val in self.admins['users'].items()
             if val['ensure'] == 'absent'
-            ])
+            )
         self.maxDiff = None
         self.longMessage = True
         self.assertSetEqual(
@@ -39,6 +52,23 @@ class DataTest(unittest.TestCase):
             absentees,
             'Absent users are both in "absent" group (first set)'
             'and in marked "ensure: absent" (second set)')
+
+    def test_group_members(self):
+        """Ensure group members are real users"""
+        present_users = set(
+            username
+            for username, val in self.admins['users'].items()
+            if val['ensure'] == 'present'
+            )
+        present_group_members = set(
+            user for user in flatten(
+                value['members'] for group, value in self.admins['groups'].items()
+                if group not in ['absent', 'absent_ldap']))
+        missing_users = present_group_members - present_users
+        self.assertEqual(
+            missing_users, set(),
+            "The following users are members of a group but don't exist: {}".format(
+                ','.join(missing_users)))
 
 
 if __name__ == '__main__':
