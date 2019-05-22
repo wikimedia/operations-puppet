ad25d2b6f47c3b7acb259c2012292065a9080843
andrewbogott <abogott@wikimedia.org>
2019-05-21 23:47:02 -0500

    clouddb2001-dev: adjust firewall rules to allow access to both cloudcontrols.
    Bug: T223905
    Change-Id: I81f631f6af44a3ede7f70c1c126d07c196690e88

--- modules/profile/manifests/openstack/codfw1dev/db.pp
+++ modules/profile/manifests/openstack/codfw1dev/db.pp
@@ -1,7 +1,8 @@
 # at some point this class should inherit all the code currently present
 # in profile::openstack::base::keystone::db
 class profile::openstack::codfw1dev::db(
-    Stdlib::Fqdn        $cloudcontrol_fqdn = lookup('profile::openstack::codfw1dev::keystone_host'),
+    Stdlib::Fqdn        $cloudcontrol_fqdn = lookup('profile::openstack::codfw1dev::nova_controller'),
+    Stdlib::Fqdn        $cloudcontrol_standby_fqdn = lookup('profile::openstack::codfw1dev::nova_controller_standby'),
     Array[Stdlib::Fqdn] $prometheus_nodes  = lookup('prometheus_nodes'),
 ) {
     include ::profile::standard
@@ -32,6 +33,6 @@ class profile::openstack::codfw1dev::db(
 
     ferm::rule { 'cloudcontrol_mysql':
         ensure => 'present',
-        rule   => "saddr (@resolve(${cloudcontrol_fqdn}) @resolve(${cloudcontrol_fqdn}, AAAA)) proto tcp dport (3306) ACCEPT;",
+        rule   => "saddr (@resolve(${cloudcontrol_fqdn}) @resolve(${cloudcontrol_fqdn}, AAAA) @resolve(${cloudcontrol_standby_fqdn}) @resolve(${cloudcontrol_standby_fqdn}, AAAA) ) proto tcp dport (3306) ACCEPT;",
     }
 }
