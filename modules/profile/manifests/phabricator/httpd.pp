a9c58d56fb4154043817e9e28a3f541f5aabf04b
Daniel Zahn <dzahn@wikimedia.org>
2019-05-22 12:13:42 -0700

    phabricator: add forensic apache logging and enable on phab1003
    I copied Ori's change doing the same for appservers at
    Ia95469658195a7793b and adjusted it minimally for phabricator.
    
    Following up to Mukunda's comment on Ori's change how this would
    be useful to debug Phabricator as well.
    
    Change-Id: If8715f9e02433c8f66fd492b870d7b9445e3c3eb

--- modules/profile/manifests/phabricator/httpd.pp
+++ modules/profile/manifests/phabricator/httpd.pp
@@ -2,6 +2,7 @@
 #
 class profile::phabricator::httpd (
     Boolean $enable_php_fpm = hiera('phabricator_enable_php_fpm', false),
+    Boolean $enable_forensic_log = hiera('profile::phabricator::httpd::enable_forensic_log', false),
 ) {
 
     $httpd_base_modules = [ 'headers', 'rewrite', 'remoteip' ]
@@ -41,5 +42,18 @@ class profile::phabricator::httpd (
     class { '::httpd::mpm':
         mpm    => $mpm,
         source => $mpm_source,
+    }
+
+    # Forensic logging (logs requests at both beginning and end of request processing)
+    if $enable_forensic_log {
+        ::httpd::mod_conf { 'log_forensic':
+            ensure  => present,
+        }
+
+        ::httpd::conf { 'log_forensic':
+            ensure  => present,
+            source  => 'puppet:///modules/phabricator/apache/log_forensic.conf',
+            require => Httpd::Mod_conf['log_forensic'],
+        }
     }
 }
