67f1ed2ce87601ace433a1d776ec9edec61ebad9
Dzahn <dzahn@wikimedia.org>
2019-05-24 22:15:13 +0000

    Revert "webserver_misc_apps: add PHP7.2 APT repository on stretch"
    This reverts commit 844071dea9dcb96301eac2b45833378a2dd14ce4.
    
    Reason for revert: https://phabricator.wikimedia.org/T224247#5209664
    
    Change-Id: I6e203f8e1f9928cae1c805cbef474e465284dbb0

--- modules/profile/manifests/misc_apps/httpd.pp
+++ modules/profile/manifests/misc_apps/httpd.pp
@@ -6,14 +6,6 @@ class profile::misc_apps::httpd {
     if os_version('debian == stretch') {
         require_package('libapache2-mod-php7.0')
         $apache_modules = concat($apache_modules_common, 'php7.0')
-
-        apt::repository { 'wikimedia-php72':
-            uri        => 'http://apt.wikimedia.org/wikimedia',
-            dist       => "${::lsbdistcodename}-wikimedia",
-            components => 'component/php72',
-            notify     => Exec['apt_update_php'],
-        }
-
     } else {
         $apache_modules = concat($apache_modules_common, 'php5')
     }
