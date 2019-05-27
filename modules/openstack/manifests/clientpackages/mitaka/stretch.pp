883f7b97b2f314ecbf1abb852d0f0d12955edf4a
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-05-27 12:46:57 +0200

    openstack: clientpackages: mitaka: stretch: special case for py3 client libs
    In Debian stretch systems we can't install python3 openstack packages from the
    custom jessie-backports/openstack-mitaka-jessie repo, because they aren't
    installable due to python3 dependencies clash.
    
    At first glance, I didn't detect any strong dependency on the openstack client
    libs version, so we can't just delete the apt pinnign on jessie-backports and
    let apt install whichever candidate is present (perhaps openstack newton).
    This is not elegant, because it means that we aren't installing openstack mitaka
    python3 packages in stretch systems, but a newer version, but I can't think of
    a better solution right now.
    
    Bug: T224345
    Change-Id: Ia28454bff423eb1e840abe40ebd57116fbee17aa
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/openstack/manifests/clientpackages/mitaka/stretch.pp
+++ modules/openstack/manifests/clientpackages/mitaka/stretch.pp
@@ -25,6 +25,34 @@ class openstack::clientpackages::mitaka::stretch(
         ensure => 'present',
     }
 
+    # in stretch, these packages are included in both our custom repo component
+    # and in the stretch stable repo. Avoid conflicts with apt by avoiding
+    # installing the version from our custom repo.
+    # for dependency version reasons, we don't want those to be pulled from jessie-bpo
+    # which means these no longer are mitaka when installed, which is not elegant
+    # because this class is named mitaka, but hey..
+    $avoid_packages = [
+        'python3-keystoneauth1',
+        'python3-keystoneclient',
+        'python3-novaclient',
+        'python3-glanceclient',
+        'python3-openstackclient',
+        'python3-designateclient',
+        'python3-neutronclient',
+        'python3-cffi-backend',
+        'python3-stevedore',
+        'python3-oslo.config',
+        'python3-oslo.utils',
+        'python3-osc-lib',
+    ]
+
+    $avoid_packages_list = join($avoid_packages, ' ')
+    apt::pin { 'mitaka_stretch_nojessiebpo_clientpackages':
+        package  => $avoid_packages_list,
+        pin      => 'release c=openstack-mitaka-jessie',
+        priority => '-1',
+    }
+
     # Wrapper python class to easily query openstack clients
     file { '/usr/lib/python2.7/dist-packages/mwopenstackclients.py':
         ensure => 'present',
