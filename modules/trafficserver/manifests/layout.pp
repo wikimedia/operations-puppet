5f144ac6eef540c130d7150cd37a31cc411a4b1b
Valentin Gutierrez <vgutierrez@wikimedia.org>
2019-05-28 09:37:40 +0200

    ATS: Avoid using traffic_layout
    As described in T224428 traffic_layout truncates the .so files
    when the system-wide libdir/libexecdir are used in a layout.
    This commit avoids using traffic_layout to provide a valid runroot
    directory.
    
    Bug: T224428
    Change-Id: I87255a9e88fe501b3da899b3966fd24aa6df5d3b

--- modules/trafficserver/manifests/layout.pp
+++ modules/trafficserver/manifests/layout.pp
@@ -1,5 +1,12 @@
 define trafficserver::layout(
     Trafficserver::Paths $paths,
+    Array[Stdlib::Absolutepath] $bins = [
+                                            '/usr/bin/traffic_crashlog', '/usr/bin/traffic_ctl', '/usr/bin/traffic_layout',
+                                            '/usr/bin/traffic_logcat', '/usr/bin/traffic_logstats',
+                                            '/usr/bin/traffic_manager','/usr/bin/traffic_server', '/usr/bin/traffic_top',
+                                            '/usr/bin/traffic_via', '/usr/bin/tspush'
+                                        ],
+    Array[Stdlib::Absolutepath] $sbins = [],
 ) {
     if !defined(File[$paths['base_path']]) {
         file { $paths['base_path']:
@@ -17,37 +24,94 @@ define trafficserver::layout(
         require => Package['trafficserver'],
     }
 
-    exec { "bootstrap-${title}-ats-runroot":
-        command => "/usr/bin/traffic_layout init --path ${paths['prefix']} --layout /etc/trafficserver/${title}-layout.yaml -a --copy-style=soft",
-        creates => "${paths['prefix']}/runroot.yaml",
-        require => File["/etc/trafficserver/${title}-layout.yaml"],
+    file { $paths['prefix']:
+        ensure  => directory,
+        owner   => 'trafficserver',
+        mode    => '0555',
+        require => File[$paths['base_path']],
+    }
+
+    file { "${paths['prefix']}/runroot.yaml":
+        ensure  => file,
+        owner   => $trafficserver::user,
+        mode    => '0400',
+        content => template('trafficserver/layout.yaml.erb'),
+        require => File[$paths['prefix']],
+    }
+
+    file { $paths['bindir']:
+        ensure  => directory,
+        owner   => $trafficserver::user,
+        mode    => '0555',
+        require => File[$paths['prefix']],
+    }
+
+    # populate bindir with symlinks to $bins
+    $bins.each |Stdlib::Absolutepath $bin| {
+        $filename = basename($bin)
+        file { "${paths['bindir']}/${filename}":
+            ensure  => link,
+            target  => $bin,
+            require => File[$paths['bindir']],
+        }
+    }
+
+    file { $paths['sbindir']:
+        ensure  => directory,
+        owner   => $trafficserver::user,
+        mode    => '0555',
+        require => File[$paths['prefix']],
+    }
+
+    # populate sbindir with symlinks to $sbins
+    $sbins.each |Stdlib::Absolutepath $sbin| {
+        $filename = basename($sbin)
+        file { "${paths['sbindir']}/${filename}":
+            ensure  => link,
+            target  => $sbin,
+            require => File[$paths['sbindir']],
+        }
+    }
+
+    file { $paths['sysconfdir']:
+        ensure  => directory,
+        owner   => $trafficserver::user,
+        mode    => '0555',
+        require => File[$paths['prefix']],
+    }
+
+    file { $paths['includedir']:
+        ensure  => directory,
+        owner   => $trafficserver::user,
+        mode    => '0555',
+        require => File[$paths['prefix']],
     }
 
     file { $paths['runtimedir']:
         ensure  => directory,
         owner   => $trafficserver::user,
         mode    => '0755',
-        require => Exec["bootstrap-${title}-ats-runroot"],
+        require => File[$paths['localstatedir']],
     }
 
     file { $paths['logdir']:
         ensure  => directory,
         owner   => $trafficserver::user,
         mode    => '0755',
-        require => Exec["bootstrap-${title}-ats-runroot"],
+        require => File[$paths['localstatedir']],
     }
 
     file { $paths['localstatedir']:
         ensure  => directory,
         owner   => $trafficserver::user,
         mode    => '0755',
-        require => Exec["bootstrap-${title}-ats-runroot"],
+        require => File[$paths['prefix']],
     }
 
     file { $paths['cachedir']:
         ensure  => directory,
         owner   => $trafficserver::user,
         mode    => '0755',
-        require => Exec["bootstrap-${title}-ats-runroot"],
+        require => File[$paths['localstatedir']],
     }
 }
