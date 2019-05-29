d9f12c34c8e5bc61a15dff00a0713dd18e499566
bstorm <bstorm@wikimedia.org>
2019-05-29 06:49:26 -0700

    nfs-exportd: apply black formatting
    Change-Id: I1896ee013927cf12aa5d46eb83c4726570ba61ac

--- modules/labstore/files/nfs-exportd.py
+++ modules/labstore/files/nfs-exportd.py
@@ -51,23 +51,26 @@ class Project:
         no_root_squash -- allow root in project instances
                           to be treated as root on mount
     """
-    EXPORTS_TEMPLATE = '/exp/project/{name} ' + \
-        '-rw,nohide,fsid=00000000000000000-{gid}-0000000000' + \
-        ',no_subtree_check,async,no_root_squash ' + \
-        '{instance_ips}'
+
+    EXPORTS_TEMPLATE = (
+        "/exp/project/{name} "
+        + "-rw,nohide,fsid=00000000000000000-{gid}-0000000000"
+        + ",no_subtree_check,async,no_root_squash "
+        + "{instance_ips}"
+    )
 
     def __init__(self, name, gid, instance_ips, volumes):
         self.name = name
         self.instance_ips = instance_ips
         self.volumes = volumes
         self.gid = gid
-        self.path = os.path.join('/exp/project/', name)
+        self.path = os.path.join("/exp/project/", name)
 
     def get_exports(self):
         return Project.EXPORTS_TEMPLATE.format(
             name=self.name,
             gid=self.gid,
-            instance_ips=' '.join(self.instance_ips)
+            instance_ips=" ".join(self.instance_ips),
         )
 
 
@@ -77,41 +80,47 @@ def get_instance_ips(project, observer_pass, regions):
 
     This uses the Nova API to fetch this data
     """
-    session = KeystoneSession(auth=KeystonePassword(
-        auth_url="http://cloudcontrol1004.wikimedia.org:5000/v3",
-        username="novaobserver",
-        password=observer_pass,
-        project_name=project,
-        user_domain_name='default',
-        project_domain_name='default'
-    ))
+    session = KeystoneSession(
+        auth=KeystonePassword(
+            auth_url="http://cloudcontrol1004.wikimedia.org:5000/v3",
+            username="novaobserver",
+            password=observer_pass,
+            project_name=project,
+            user_domain_name="default",
+            project_domain_name="default",
+        )
+    )
 
     ips = []
     for region in regions:
         try:
             client = novaclient.Client(
-                "2.0",
-                session=session,
-                region_name=region,
+                "2.0", session=session, region_name=region
             )
             for instance in client.servers.list():
                 # Only provide internal IPs!
-                if 'public' in instance.addresses:
+                if "public" in instance.addresses:
                     # This is a nova-network instance
-                    for ip in instance.addresses['public']:
-                        if ip['OS-EXT-IPS:type'] == 'fixed' and is_valid_ipv4(ip['addr']):
-                            ips.append(str(ip['addr']))
+                    for ip in instance.addresses["public"]:
+                        if ip["OS-EXT-IPS:type"] == "fixed" and is_valid_ipv4(
+                            ip["addr"]
+                        ):
+                            ips.append(str(ip["addr"]))
                 else:
                     # This is probably a neutron instance.  Export all fixed
                     #  addresses (probably there's only one)
                     for value in instance.addresses.values():
                         for ip in value:
-                            if ip['OS-EXT-IPS:type'] == 'fixed' and is_valid_ipv4(ip['addr']):
-                                ips.append(str(ip['addr']))
+                            if ip[
+                                "OS-EXT-IPS:type"
+                            ] == "fixed" and is_valid_ipv4(ip["addr"]):
+                                ips.append(str(ip["addr"]))
 
         except keystoneauth1.exceptions.http.Unauthorized:
-            logging.error("Failed to get server list for project %s."
-                          "  Maybe the project was deleted." % project)
+            logging.error(
+                "Failed to get server list for project %s."
+                "  Maybe the project was deleted." % project
+            )
             raise
 
     return ips
@@ -126,42 +135,48 @@ def get_projects_with_nfs(mounts_config, observer_pass):
     projects = []
 
     # Special one-off session just to grab the list of regions
-    session = KeystoneSession(auth=KeystonePassword(
-        auth_url="http://cloudcontrol1004.wikimedia.org:5000/v3",
-        username="novaobserver",
-        password=observer_pass,
-        project_name='observer',
-        user_domain_name='default',
-        project_domain_name='default'
-    ))
-    keystoneclient = keystone_client.Client(session=session, interface='public')
+    session = KeystoneSession(
+        auth=KeystonePassword(
+            auth_url="http://cloudcontrol1004.wikimedia.org:5000/v3",
+            username="novaobserver",
+            password=observer_pass,
+            project_name="observer",
+            user_domain_name="default",
+            project_domain_name="default",
+        )
+    )
+    keystoneclient = keystone_client.Client(session=session, interface="public")
     region_recs = keystoneclient.regions.list()
     regions = [region.id for region in region_recs]
 
-    server_vols = mounts_config['volumes_served']
-    for name, config in mounts_config['private'].items():
-        if 'mounts' in config:
-            mounts = [k for k, v in config['mounts'].items()
-                      if k in server_vols and v]
+    server_vols = mounts_config["volumes_served"]
+    for name, config in mounts_config["private"].items():
+        if "mounts" in config:
+            mounts = [
+                k for k, v in config["mounts"].items() if k in server_vols and v
+            ]
             if len(mounts) == 0:
                 # Skip project if it has no private mounts
-                logging.debug('skipping exports for %s, no private mounts', name)
+                logging.debug(
+                    "skipping exports for %s, no private mounts", name
+                )
                 continue
         else:
             continue
         ips = get_instance_ips(name, observer_pass, regions)
         if ips:
-            project = Project(name, config['gid'], ips, mounts)
+            project = Project(name, config["gid"], ips, mounts)
             projects.append(project)
-            logging.debug('project %s has %s instances',
-                          name, len(project.instance_ips))
+            logging.debug(
+                "project %s has %s instances", name, len(project.instance_ips)
+            )
         else:
-            logging.warning('project %s has no instances; skipping.', name)
+            logging.warning("project %s has no instances; skipping.", name)
 
     # Validate that there are no duplicate gids
     gids = [p.gid for p in projects]
     if len(set(gids)) != len(gids):
-        logging.error('duplicate GIDs found in project config, aborting')
+        logging.error("duplicate GIDs found in project config, aborting")
         sys.exit(1)
 
     logging.warning("found %s projects requiring private mounts", len(projects))
@@ -172,13 +187,9 @@ def exportfs():
     """ translate on disk definitions into active NFS exports
     :warn: this can fail with 0 exit code
     """
-    exportfs = [
-        '/usr/bin/sudo',
-        '/usr/sbin/exportfs',
-        '-ra'
-    ]
+    exportfs = ["/usr/bin/sudo", "/usr/sbin/exportfs", "-ra"]
 
-    logging.warning(' '.join(exportfs))
+    logging.warning(" ".join(exportfs))
     subprocess.check_call(exportfs)
 
 
@@ -188,9 +199,9 @@ def write_public_exports(public_exports, exports_d_path):
     """
     public_paths = []
     for name, content in public_exports.items():
-        logging.debug('writing exports file for public export %s', name)
-        path = os.path.join(exports_d_path, 'public_%s.exports' % name)
-        with open(path, 'w') as f:
+        logging.debug("writing exports file for public export %s", name)
+        path = os.path.join(exports_d_path, "public_%s.exports" % name)
+        with open(path, "w") as f:
             f.write(content)
         public_paths.append(path)
     logging.warning("found %s public NFS exports" % (len(public_paths)))
@@ -204,9 +215,9 @@ def write_project_exports(mounts_config, exports_d_path, observer_pass):
     project_paths = []
     projects = get_projects_with_nfs(mounts_config, observer_pass)
     for project in projects:
-        logging.debug('writing exports file for %s', project.name)
-        path = os.path.join(exports_d_path, '%s.exports' % project.name)
-        with open(path, 'w') as f:
+        logging.debug("writing exports file for %s", project.name)
+        path = os.path.join(exports_d_path, "%s.exports" % project.name)
+        with open(path, "w") as f:
             f.write(project.get_exports())
         project_paths.append(path)
     return project_paths
@@ -245,7 +256,11 @@ def do_fix_and_export(existing_wo_all):
             #  because of one bad one
             try:
                 with open(unmanaged_export) as f:
-                    logging.warning('deleting %s with contents: %s', unmanaged_export, f.read())
+                    logging.warning(
+                        "deleting %s with contents: %s",
+                        unmanaged_export,
+                        f.read(),
+                    )
                 os.remove(unmanaged_export)
             except OSError:
                 logging.exception("failed to delete export")
@@ -257,50 +272,49 @@ def main():
     argparser = argparse.ArgumentParser()
 
     argparser.add_argument(
-        '--exports-d-path',
-        default='/etc/exports.d/',
-        help='Dir to write exports files to',
+        "--exports-d-path",
+        default="/etc/exports.d/",
+        help="Dir to write exports files to",
     )
 
     argparser.add_argument(
-        '--config-path',
-        default='/etc/nfs-mounts.yaml',
-        help='Path to YAML file containing config of which exports to maintain',
+        "--config-path",
+        default="/etc/nfs-mounts.yaml",
+        help="Path to YAML file containing config of which exports to maintain",
     )
 
     argparser.add_argument(
-        '--observer-pass',
+        "--observer-pass",
         required=True,
-        help='Password for the OpenStack observer account',
+        help="Password for the OpenStack observer account",
     )
 
     argparser.add_argument(
-        '--interval',
+        "--interval",
         type=int,
         default=0,
-        help='Set interval to rerun at.  Default is 0 which means run once.',
+        help="Set interval to rerun at.  Default is 0 which means run once.",
     )
 
     argparser.add_argument(
-        '--debug',
-        help='Turn on debug logging',
-        action='store_true'
+        "--debug", help="Turn on debug logging", action="store_true"
     )
 
     argparser.add_argument(
-        '--no-exports-is-ok',
-        help='Having no NFS exports is OK. Otherwise, refuse to do anything if that happens',
-        action='store_true'
+        "--no-exports-is-ok",
+        help="Having no NFS exports is OK. Otherwise, refuse to do anything if that happens",
+        action="store_true",
     )
 
     args = argparser.parse_args()
 
     logging.basicConfig(
-        format='%(asctime)s %(levelname)s %(message)s',
-        level=logging.DEBUG if args.debug else logging.WARNING)
+        format="%(asctime)s %(levelname)s %(message)s",
+        level=logging.DEBUG if args.debug else logging.WARNING,
+    )
 
     if os.getuid() == 0:
-        logging.error('Daemon started as root, exiting')
+        logging.error("Daemon started as root, exiting")
         sys.exit(1)
 
     while True:
@@ -309,17 +323,22 @@ def main():
             with open(args.config_path) as f:
                 config = yaml.safe_load(f)
         except Exception:
-            logging.exception('Could not load projects config file from %s', args.config_path)
+            logging.exception(
+                "Could not load projects config file from %s", args.config_path
+            )
             sys.exit(1)
 
         exports_d_path = args.exports_d_path
 
         existing_exports = [
             os.path.join(exports_d_path, filename)
-            for filename in os.listdir(exports_d_path)]
+            for filename in os.listdir(exports_d_path)
+        ]
 
-        public_paths = write_public_exports(config['public'], exports_d_path)
-        project_paths = write_project_exports(config, exports_d_path, args.observer_pass)
+        public_paths = write_public_exports(config["public"], exports_d_path)
+        project_paths = write_project_exports(
+            config, exports_d_path, args.observer_pass
+        )
 
         # compile list of entries in export_d path that are not defined in current config
         existing_wo_public = list(set(existing_exports) - set(public_paths))
