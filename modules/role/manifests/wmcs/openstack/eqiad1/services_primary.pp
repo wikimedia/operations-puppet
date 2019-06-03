3fc500b96a204683ac441ed7ab9a7589cc5d4b9e
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-06-03 13:32:21 +0200

    cloudservices: use unified prometheus_pdns_rec_exporter
    We no longer need a separate profile. It was previously required because
    labmon1001/1002 didn't have AAAA records.
    
    Bug: T224743
    Change-Id: If0bcc04155e759fbbb609e7a0c94910883f28246
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/role/manifests/wmcs/openstack/eqiad1/services_primary.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/services_primary.pp
@@ -11,6 +11,6 @@ class role::wmcs::openstack::eqiad1::services_primary {
     include ::profile::openstack::eqiad1::pdns::recursor::primary
     include ::profile::openstack::eqiad1::designate::service
     include ::profile::prometheus::pdns_exporter
-    include ::profile::prometheus::pdns_rec_exporter_wmcs
+    include ::profile::prometheus::pdns_rec_exporter
     include ::profile::ldap::client::labs
 }
