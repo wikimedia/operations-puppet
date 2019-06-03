88fab13a06ae153e70ff6d6678bed579cd4274ac
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-06-03 13:38:33 +0200

    cloudservices: use prometheus pdns exporters in all roles
    So if we failover the services, we keep having metrics.
    
    Bug: T224743
    Change-Id: I31bbad2647a71bf5fc5ce21ba0cec74bc63cafcb
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/role/manifests/wmcs/openstack/codfw1dev/services.pp
+++ modules/role/manifests/wmcs/openstack/codfw1dev/services.pp
@@ -9,5 +9,7 @@ class role::wmcs::openstack::codfw1dev::services {
     include ::profile::openstack::codfw1dev::pdns::auth::service
     include ::profile::openstack::codfw1dev::pdns::recursor::service
     include ::profile::openstack::codfw1dev::designate::service
+    include ::profile::prometheus::pdns_exporter
+    include ::profile::prometheus::pdns_rec_exporter
     include ::profile::openldap
 }
--- modules/role/manifests/wmcs/openstack/eqiad1/services_secondary.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/services_secondary.pp
@@ -10,5 +10,7 @@ class role::wmcs::openstack::eqiad1::services_secondary {
     include ::profile::openstack::eqiad1::pdns::auth::service
     include ::profile::openstack::eqiad1::pdns::recursor::secondary
     include ::profile::openstack::eqiad1::designate::service
+    include ::profile::prometheus::pdns_exporter
+    include ::profile::prometheus::pdns_rec_exporter
     include ::profile::ldap::client::labs
 }
