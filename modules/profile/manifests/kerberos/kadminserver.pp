1fb45bc6581c88c823352e725621f05e46e7b5a2
Luca Toscano <ltoscano@wikimedia.org>
2019-06-05 11:09:46 +0200

    profile::kerberos::kadminserver: allow puppetmaster to rsync keytabs
    The idea is to manually generate, for the moment, Kerberos keytabs on
    the kadmind master/active, and then allow puppetmaster1001 to rsync
    them in order to be able to add them to the puppet private repository.
    
    In this way the puppet master will not need to be a kerberos client.
    
    As extra layer of protection, the rsync module will require user/pass
    to pull data from puppetmaster1001.
    
    Bug: T212257
    Change-Id: Icb9b255df18014872852e42f7e610f8d801baeee

--- modules/profile/manifests/kerberos/kadminserver.pp
+++ modules/profile/manifests/kerberos/kadminserver.pp
@@ -1,6 +1,7 @@
 class profile::kerberos::kadminserver (
     Stdlib::Fqdn $krb_realm_name = lookup('kerberos_realm_name'),
     Stdlib::Fqdn $krb_kadmin_primary = lookup('kerberos_kadmin_server_primary'),
+    Stdlib::Fqdn $krb_kadmin_keytabs_repo = lookup('kerberos_kadmin_keytabs_repo'),
 ) {
     package { 'krb5-admin-server':
         ensure => present,
@@ -10,5 +11,53 @@ class profile::kerberos::kadminserver (
         service { 'krb5-admin-server':
             ensure    => stopped,
         }
+    }
+
+    # Keytabs will be generated manually, via a script that uses kadmin.local,
+    # under /srv/kerberos/keytabs
+    file{ '/srv/kerberos':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    file{ '/srv/kerberos/keytabs':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0750',
+    }
+
+    # Add the rsync server configuration only to the
+    # active kerberos host.
+    if $trusted['certname'] == $krb_kadmin_primary {
+        $ensure_rsync = true
+    } else {
+        $ensure_rsync = false
+    }
+
+    class { 'rsync::server': }
+
+    $rsync_secrets_file = '/srv/kerberos/rsync_secrets_file'
+    file { $rsync_secrets_file:
+        ensure    => 'present',
+        owner     => 'root',
+        group     => 'root',
+        mode      => '0400',
+        content   => secret('kerberos/rsync_secrets_file'),
+        show_diff => false,
+        require   => File['/srv/kerberos']
+    }
+
+    rsync::server::module { 'srv-keytabs':
+        ensure         => $ensure_rsync,
+        path           => '/srv/kerberos/keytabs',
+        read_only      => 'yes',
+        hosts_allow    => [$krb_kadmin_keytabs_repo],
+        auto_ferm      => true,
+        auto_ferm_ipv6 => true,
+        secrets_file   => $rsync_secrets_file,
+        require        => File[$rsync_secrets_file],
     }
 }
