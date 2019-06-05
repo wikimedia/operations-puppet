cb5c068574622da693b3d50d24a69bfe6693a2fb
Marcel Ruiz Forns <mforns@wikimedia.org>
2019-06-05 23:20:35 +0200

    analytics::refinery::job::refine Bump up refinery_jar_version
    Change-Id: I11fde11c7424e0a4979f081eae561afad9dd5977

--- modules/profile/manifests/analytics/refinery/job/refine.pp
+++ modules/profile/manifests/analytics/refinery/job/refine.pp
@@ -9,7 +9,7 @@ class profile::analytics::refinery::job::refine {
 
     # Update this when you want to change the version of the refinery job jar
     # being used for the refine job.
-    $refinery_version = '0.0.90'
+    $refinery_version = '0.0.91'
 
     # Use this value by default
     Profile::Analytics::Refinery::Job::Refine_job {
@@ -38,7 +38,7 @@ class profile::analytics::refinery::job::refine {
             input_path_regex_capture_groups => 'table,year,month,day,hour',
             table_blacklist_regex           => '^Edit|ChangesListHighlights|InputDeviceDynamics$',
             # Deduplicate basd on uuid field and geocode ip in EventLogging analytics data.
-            transform_functions             => 'org.wikimedia.analytics.refinery.job.refine.deduplicate_eventlogging,org.wikimedia.analytics.refinery.job.refine.geocode_ip',
+            transform_functions             => 'org.wikimedia.analytics.refinery.job.refine.filter_out_non_wiki_hostname,org.wikimedia.analytics.refinery.job.refine.deduplicate_eventlogging,org.wikimedia.analytics.refinery.job.refine.geocode_ip',
             # Get EventLogging JSONSchemas from meta.wikimedia.org.
             schema_base_uri                 => 'eventlogging',
         }),
