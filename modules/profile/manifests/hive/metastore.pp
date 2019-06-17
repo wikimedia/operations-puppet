56f9b6273a0f0463068ccae8d74901d882e9ac43
Luca Toscano <ltoscano@wikimedia.org>
2019-06-17 15:30:10 +0200

    profile::hive::*: add jvm heap usage alarms
    Bug: T222895
    Change-Id: I18116ffac017084728408b6f58e3af40c7895530

--- modules/profile/manifests/hive/metastore.pp
+++ modules/profile/manifests/hive/metastore.pp
@@ -29,5 +29,15 @@ class profile::hive::metastore(
             require       => Class['cdh::hive::metastore'],
             notes_url     => 'https://wikitech.wikimedia.org/wiki/Analytics/Systems/Cluster/Hive',
         }
+
+        monitoring::check_prometheus { 'hive-metastore-heap-usage':
+            description     => 'Hive Metastore JVM Heap usage',
+            dashboard_links => ['https://grafana.wikimedia.org/d/000000379/hive?panelId=2&fullscreen&orgId=1'],
+            query           => "scalar(avg_over_time(jvm_memory_bytes_used{instance=\"${::hostname}:9183\",area=\"heap\"}[60m])/avg_over_time(jvm_memory_bytes_max{instance=\"${::hostname}:9183\",area=\"heap\"}[60m]))",
+            warning         => 0.8,
+            critical        => 0.9,
+            contact_group   => 'analytics',
+            prometheus_url  => "http://prometheus.svc.${::site}.wmnet/analytics",
+        }
     }
 }
--- modules/profile/manifests/hive/server.pp
+++ modules/profile/manifests/hive/server.pp
@@ -31,5 +31,15 @@ class profile::hive::server(
             require       => Class['cdh::hive::server'],
             notes_url     => 'https://wikitech.wikimedia.org/wiki/Analytics/Systems/Cluster/Hive',
         }
+
+        monitoring::check_prometheus { 'hive-server-heap-usage':
+            description     => 'Hive Server JVM Heap usage',
+            dashboard_links => ['https://grafana.wikimedia.org/d/000000379/hive?panelId=7&fullscreen&orgId=1'],
+            query           => "scalar(avg_over_time(jvm_memory_bytes_used{instance=\"${::hostname}:10100\",area=\"heap\"}[60m])/avg_over_time(jvm_memory_bytes_max{instance=\"${::hostname}:10100\",area=\"heap\"}[60m]))",
+            warning         => 0.8,
+            critical        => 0.9,
+            contact_group   => 'analytics',
+            prometheus_url  => "http://prometheus.svc.${::site}.wmnet/analytics",
+        }
     }
 }
