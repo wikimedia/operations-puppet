2465efef78b06e839951635e3cb62e84d638faa0
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-06-19 14:09:13 +0200

    toolforge: k8s: etcd: don't wrap profile::etcd, and use base etcd v3 directly
    Use the etcd v3 base class directly instead of wrapping profile::etcd.
    In Toolforge, we don't need many of the things from profile::etcd,
    and also this should help simplify the relationship between WMCS/Prod in this
    puppet code.
    
    Before this patch we were using an approach like this:
     * toolforge k8s etcd profile --> prod etcd profile --> etcd base class
    
    After this patch, this is the appraoch instead:
     * toolforge k8s etcd profile --> etcd v3 base class
    
    We can drop several hiera keys that we no longer require in this profile.
    
    Bug: T226098
    Change-Id: I8d07adb18166fdcf5e80d7493e3326184cfd3829
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/toolforge/k8s/etcd.pp
+++ modules/profile/manifests/toolforge/k8s/etcd.pp
@@ -2,36 +2,48 @@ class profile::toolforge::k8s::etcd(
     Array[Stdlib::Fqdn] $peer_hosts    = lookup('profile::k8s::etcd_hosts',              {default_value => ['localhost']}),
     Array[Stdlib::Fqdn] $checker_hosts = lookup('profile::toolforge::checker_hosts',     {default_value => ['tools-checker-03.tools.eqiad.wmflabs']}),
     Array[Stdlib::Fqdn] $k8s_hosts     = lookup('profile::toolforge::k8s_masters_hosts', {default_value => ['localhost']}),
-    String              $cluster_name  = lookup('profile::etcd::cluster_name',           {default_value => 'toolforge-k8s-etcd'}),
     Boolean             $bootstrap     = lookup('profile::etcd::cluster_bootstrap',      {default_value => false}),
-    String              $discovery     = lookup('profile::etcd::discovery',              {default_value => 'no'}),
-    Boolean             $client_certs  = lookup('profile::etcd::use_client_certs',       {default_value => false}),
-    Boolean             $use_proxy     = lookup('profile::etcd::use_proxy',              {default_value => false}),
-    Boolean             $do_backup     = lookup('profile::etcd::do_backup',              {default_value => false}),
-    String              $allow_from    = lookup('profile::etcd::allow_from',             {default_value => 'localhost'}),
 ) {
-
-    # TODO: this is an effort to provide some sane defaults for our environment
-    # which could probably be improved
-    if $allow_from == 'localhost' {
-        $firewall_peers    = join(($peer_hosts), ' ')
-        $firewall_checker  = join(($checker_hosts), ' ')
-        $firewall_k8s      = join(($k8s_hosts), ' ')
-        $actual_allow_from = "@resolve((${firewall_peers} ${firewall_checker} ${firewall_k8s}))"
+    if $bootstrap {
+        $cluster_state = 'new'
     } else {
-        $actual_allow_from = $allow_from
+        $cluster_state = 'existing'
     }
 
-    class { '::profile::etcd':
-        cluster_name      => $cluster_name,
-        cluster_bootstrap => $bootstrap,
-        discovery         => $discovery,
-        use_client_certs  => $client_certs,
-        use_proxy         => $use_proxy,
-        allow_from        => $actual_allow_from,
-        do_backup         => $do_backup,
+    # for $peers_list we need a string like this:
+    # node1=https://1.1.1.1:2380,node2=https://2.2.2.2:2380,node3=https://3.3.3.3:2380
+    $protocol    = 'https://'
+    $port        = ':2380'
+    $peers_list_array = map($peer_hosts) |$element| {
+        $addr  =  ipresolve($element, 4)
+        $value = "${element}=${protocol}${addr}${port}"
     }
-    contain '::profile::etcd'
+    $peers_list = join(($peers_list_array), ',')
+
+    class { '::etcd::v3':
+        cluster_state => $cluster_state,
+        peers_list    => $peers_list,
+        trusted_ca    => '/var/lib/puppet/ssl/certs/ca.pem',
+        peer_cert     => "/var/lib/puppet/ssl/certs/${::fqdn}.pem",
+        peer_key      => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem",
+    }
+
+    $checker_hosts_string = join(($checker_hosts), ' ')
+    $k8s_hosts_string     = join(($k8s_hosts), ' ')
+    $firewall_clients     = "@resolve((${checker_hosts_string} ${k8s_hosts_string}))"
+    ferm::service { 'etcd_clients':
+        proto  => 'tcp',
+        port   => 2379,
+        srange => $firewall_clients,
+    }
+
+    $peer_hosts_string = join(($peer_hosts), ' ')
+    $firewall_peers    = "@resolve((${peer_hosts_string}))"
+    ferm::service { 'etcd_peers':
+        proto  => 'tcp',
+        port   => 2380,
+        srange => $firewall_peers,
+    }
 
     # from role::toollabs::etcd::expose_metrics
     $exposed_port = '9051'
