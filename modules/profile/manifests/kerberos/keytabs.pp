d74b8035e41d120d63cc8ab16a2989d78740cdde
Luca Toscano <ltoscano@wikimedia.org>
2019-06-20 10:43:07 +0200

    profile::kerberos::keytabs: add parent_dir_grp option
    Also set it up for some Hadoop testing roles.
    
    Bug: T212257
    Change-Id: Iee3b158499a764b1d1bdc204d98c257364e0be52

--- modules/profile/manifests/kerberos/keytabs.pp
+++ modules/profile/manifests/kerberos/keytabs.pp
@@ -5,11 +5,12 @@
 # == Parameters
 #  [*keytabs_metadata*]
 #    Array of hash tables having the following format:
-#    [{'role' => 'hadoop', 'owner': 'hdfs', group: 'hdfs', filename: 'hdfs.keytab'} ..]
+#    [{'role' => 'hadoop', 'owner': 'hdfs', group: 'hdfs', filename: 'hdfs.keytab', parent_dir_grp: 'hadoop'} ..]
 #    Keytabs needs to be present in the private puppet repository.
 #    The user indicated as 'owner' of the keytab needs to be present in the catalog
-#    (since it is required to properly create the keytab). The related group is not
+#    (since it is required to properly create the keytab). The related groups are not
 #    explicitly required, as it is assumed to be present.
+#    The 'parent_dir_grp is optional, if not set it defaults to the value of 'group'.
 #
 class profile::kerberos::keytabs (
     Array $keytabs_metadata = lookup('profile::kerberos::keytabs::keytabs_metadata', { 'default_value' => [] })
@@ -18,10 +19,14 @@ class profile::kerberos::keytabs (
     $keytabs_metadata.each |Hash $keytab_metadata| {
 
         if !defined(File["/etc/security/keytabs/${keytab_metadata['role']}"]) {
+            $parent_dir_grp = $keytab_metadata['parent_dir_grp'] ? {
+                undef   => $keytab_metadata['group'],
+                default => $keytab_metadata['parent_dir_grp'],
+            }
             file { "/etc/security/keytabs/${keytab_metadata['role']}":
                 ensure  => 'directory',
                 owner   => $keytab_metadata['owner'],
-                group   => $keytab_metadata['group'],
+                group   => $parent_dir_grp,
                 mode    => '0550',
                 require => File['/etc/security/keytabs']
             }
