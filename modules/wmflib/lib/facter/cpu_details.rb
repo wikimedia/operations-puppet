6f27eb22cbf2f5f786c687320c14cdc7a08a8705
John Bond <jbond@wikimedia.org>
2019-06-21 16:08:19 +0100

    facter: add confine to cpu_details fact
    This fact requires the /sys/devices/system/cpu/vulnerabilities directory
    which is only avalible with specific kernel versions
    
    Change-Id: I925ac6c2dbaaefc6b589462add0bd642e1d9f045

--- modules/wmflib/lib/facter/cpu_details.rb
+++ modules/wmflib/lib/facter/cpu_details.rb
@@ -2,14 +2,19 @@ require 'facter'
 
 Facter.add(:cpu_details) do
   confine :kernel => 'Linux'
-  cpu_details = {
-    'vulnerabilities' => {},
-  }
-  Dir.foreach('/sys/devices/system/cpu/vulnerabilities') do |vuln|
-    next if %w{. ..}.include? vuln
-    cpu_details['vulnerabilities'][vuln] = File.read(
-      "/sys/devices/system/cpu/vulnerabilities/#{vuln}"
-    )
+  confine do
+    File.directory?('/sys/devices/system/cpu/vulnerabilities')
   end
-  setcode { cpu_details }
+  setcode do
+    cpu_details = {
+      'vulnerabilities' => {},
+    }
+    Dir.foreach('/sys/devices/system/cpu/vulnerabilities') do |vuln|
+      next if %w{. ..}.include? vuln
+      cpu_details['vulnerabilities'][vuln] = File.read(
+        "/sys/devices/system/cpu/vulnerabilities/#{vuln}"
+      )
+    end
+    cpu_details
+  end
 end
