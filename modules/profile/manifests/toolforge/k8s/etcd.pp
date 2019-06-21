1610fdecc2cc1cd6a9f9eb2d76429ba769db2ee2
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-06-21 12:22:49 +0200

    toolforge: k8s: etcd: refresh server with certs changes
    In WMCS switching puppetmasters is an operation that is more common than in
    the prod realm. The etcd service is using puppet certs, and we had issues in
    the past when we switched puppetmasters and there was a big mess of nothing
    working in toolforge k8s/etcd.
    
    One approach to prevent this mess is restart the etcd daemon if the
    certificate changes, but puppet agent certs are not in the catalog, so let's do
    this trick:
    
     * copy the puppet cert to the etcd ssl dir, therefore creating a File resource
     in the catalog
     * let's use this copied cert for the etcd config, now that we can notify on
     changes of this new File resource
    
    Using this trick, we no longer need the etcd user to belong to the puppet
    group.
    
    Bug: T169287
    Bug: T169287
    Change-Id: Ie34ea262bce95ab483957385fda13e5bc17a1506
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/toolforge/k8s/etcd.pp
+++ modules/profile/manifests/toolforge/k8s/etcd.pp
@@ -19,22 +19,56 @@ class profile::toolforge::k8s::etcd(
     }
     $peers_list = join(($peers_list_array), ',')
 
+    # the certificate trick
+    $etcd_cert_pub    = "/etc/etcd/ssl/${::fqdn}.pem"
+    $etcd_cert_priv   = "/etc/etcd/ssl/${::fqdn}.priv"
+    $etcd_cert_ca     = '/etc/etcd/ssl/ca.pem'
+    $puppet_cert_pub  = "/var/lib/puppet/ssl/certs/${::fqdn}.pem"
+    $puppet_cert_priv = "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem"
+    $puppet_cert_ca   = '/var/lib/puppet/ssl/certs/ca.pem'
+
+    file { '/etc/etcd/ssl/':
+        ensure => directory,
+    }
+
+    file { $etcd_cert_pub:
+        ensure => present,
+        source => "file://${puppet_cert_pub}",
+        owner  => 'etcd',
+        group  => 'etcd',
+    }
+
+    file { $etcd_cert_priv:
+        ensure    => present,
+        source    => "file://${puppet_cert_priv}",
+        owner     => 'etcd',
+        group     => 'etcd',
+        mode      => '0640',
+        show_diff => false,
+    }
+
+    file { $etcd_cert_ca:
+        ensure => present,
+        source => "file://${puppet_cert_ca}",
+        owner  => 'etcd',
+        group  => 'etcd',
+    }
+
     class { '::etcd::v3':
         member_name   => $::fqdn,
         cluster_state => $cluster_state,
         peers_list    => $peers_list,
-        client_cert   => "/var/lib/puppet/ssl/certs/${::fqdn}.pem",
-        client_key    => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem",
-        trusted_ca    => '/var/lib/puppet/ssl/certs/ca.pem',
-        peer_cert     => "/var/lib/puppet/ssl/certs/${::fqdn}.pem",
-        peer_key      => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem",
+        client_cert   => $etcd_cert_pub,
+        client_key    => $etcd_cert_priv,
+        trusted_ca    => $etcd_cert_ca,
+        peer_cert     => $etcd_cert_pub,
+        peer_key      => $etcd_cert_priv,
     }
 
-    # add the etcd user to the puppet group so it can read puppet certs
-    user { 'etcd':
-        ensure => 'present',
-        groups => 'puppet',
-    }
+    # restart the service if a cert file changes
+    File[$etcd_cert_pub]  ~> Service[etcd]
+    File[$etcd_cert_priv] ~> Service[etcd]
+    File[$etcd_cert_ca]   ~> Service[etcd]
 
     $checker_hosts_string = join(($checker_hosts), ' ')
     $k8s_hosts_string     = join(($k8s_hosts), ' ')
