73d53e673e3b3605bec2a35742bb1c870e331317
bstorm <bstorm@wikimedia.org>
2019-06-25 12:21:51 -0600

    toolforge: remove junk from k8s::apilb
    The nrpe monitor won't work right now in this profile, so removing.
    
    Bug: T215531
    Change-Id: I591bf2c33fa3114026c8634c693d393b13c068cb

--- modules/profile/manifests/toolforge/k8s/apilb.pp
+++ modules/profile/manifests/toolforge/k8s/apilb.pp
@@ -1,14 +1,7 @@
 class profile::toolforge::k8s::apilb (
         $servers = hiera('profile::toolforge::k8s::api_servers'),
     ) {
-
     class { 'haproxy':
         template => 'profile/toolforge/k8s/apilb/haproxy.cfg.erb',
-    }
-
-    nrpe::monitor_service { 'haproxy_failover':
-        description  => 'haproxy failover',
-        nrpe_command => '/usr/lib/nagios/plugins/check_haproxy --check=failover',
-        notes_url    => 'https://phabricator.wikimedia.org/tag/toolforge/',
     }
 }
