348c2ac81f9e99043b2e19d2eb98ccc0439d34da
Emanuele Rocca <ema@wikimedia.org>
2019-06-25 10:52:34 +0200

    cache: reimage cp5001 as upload_ats
    Bug: T226477
    Change-Id: I66d2d147ab8cf96a99e3b19c08bf5e32f9213222

--- manifests/site.pp
+++ manifests/site.pp
@@ -350,10 +350,15 @@ node /^cp40(2[789]|3[012])\.ulsfo\.wmnet$/ {
 }
 
 #
-# eqsin varnishes
+# eqsin caches
 #
 
-node /^cp500[1-6]\.eqsin\.wmnet$/ {
+node 'cp5001.eqsin.wmnet' {
+    interface::add_ip6_mapped { 'main': }
+    role(cache::upload_ats)
+}
+
+node /^cp500[2-6]\.eqsin\.wmnet$/ {
     interface::add_ip6_mapped { 'main': }
     role(cache::upload)
 }
