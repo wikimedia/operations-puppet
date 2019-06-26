76a453767960bfc9ebea33601ce348b2af16776a
bstorm <bstorm@wikimedia.org>
2019-06-25 18:27:13 -0600

    haproxy: make monitoring code optional
    To use this in the WMCS environment, haproxy won't be accessible to
    icinga, so it would be good if we could turn off the monitoring code.
    
    Bug: T215531
    Change-Id: Ief65c401d0f25f18b61a2569e7e86f99b70fbd05

--- modules/haproxy/manifests/init.pp
+++ modules/haproxy/manifests/init.pp
@@ -7,11 +7,17 @@
 #   It will work only if 'log /dev/log local0 info' is set. This implementation
 #   will simply direct *all* haproxy logs.
 #
+# [*monitor*]
+#   If set to false, monitoring will not be set up for icinga. Defaults to true.
+#   Useful for places where monitoring is not appropriate or impossible via icinga
+#   such as cloud or perhaps a PoC system
+#
 
 class haproxy(
     $template = 'haproxy/haproxy.cfg.erb',
     $socket   = '/run/haproxy/haproxy.sock',
     $pid      = '/run/haproxy/haproxy.pid',
+    $monitor  = true,
     $logging  = false,
 ) {
 
@@ -53,13 +59,6 @@ class haproxy(
         refreshonly => true,
     }
 
-    file { '/usr/lib/nagios/plugins/check_haproxy':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0755',
-        content => template('haproxy/check_haproxy.erb'),
-    }
-
     # defaults file cannot be dynamic anymore on systemd
     # pregenerate them on systemd start/reload
     file { '/usr/local/bin/generate_haproxy_default.sh':
@@ -86,16 +85,25 @@ class haproxy(
         refreshonly => true,
     }
 
-    nrpe::monitor_service { 'haproxy':
-        description  => 'haproxy process',
-        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1: -C haproxy',
-        notes_url    => 'https://wikitech.wikimedia.org/wiki/HAProxy',
-    }
+    if $monitor {
+        file { '/usr/lib/nagios/plugins/check_haproxy':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755',
+            content => template('haproxy/check_haproxy.erb'),
+        }
 
-    nrpe::monitor_service { 'haproxy_alive':
-        description  => 'haproxy alive',
-        nrpe_command => '/usr/lib/nagios/plugins/check_haproxy --check=alive',
-        notes_url    => 'https://wikitech.wikimedia.org/wiki/HAProxy',
+        nrpe::monitor_service { 'haproxy':
+            description  => 'haproxy process',
+            nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1: -C haproxy',
+            notes_url    => 'https://wikitech.wikimedia.org/wiki/HAProxy',
+        }
+
+        nrpe::monitor_service { 'haproxy_alive':
+            description  => 'haproxy alive',
+            nrpe_command => '/usr/lib/nagios/plugins/check_haproxy --check=alive',
+            notes_url    => 'https://wikitech.wikimedia.org/wiki/HAProxy',
+        }
     }
 
     if $logging {
