c5358d031ad636af87b751f089c44fac7d6688ee
Emanuele Rocca <ema@wikimedia.org>
2019-06-26 15:38:31 +0200

    cache: reimage cp5006 as upload_ats
    This completes the conversion of upload@eqsin to ATS.
    
    Bug: T226477
    Change-Id: I360813379edc312a76e3331eaf531cd534bb6b15

--- manifests/site.pp
+++ manifests/site.pp
@@ -349,14 +349,9 @@ node /^cp40(2[789]|3[012])\.ulsfo\.wmnet$/ {
 # eqsin caches
 #
 
-node /^cp500[1-5]\.eqsin\.wmnet$/ {
+node /^cp500[1-6]\.eqsin\.wmnet$/ {
     interface::add_ip6_mapped { 'main': }
     role(cache::upload_ats)
-}
-
-node 'cp5006.eqsin.wmnet' {
-    interface::add_ip6_mapped { 'main': }
-    role(cache::upload)
 }
 
 node /^cp50(0[789]|1[012])\.eqsin\.wmnet$/ {
