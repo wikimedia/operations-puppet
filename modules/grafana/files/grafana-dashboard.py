34cd466b4042e90390b5e156957d7b00e626c149
Cole White <cwhite@wikimedia.org>
2019-06-28 10:10:13 -0600

    grafana: fix and update to grafana-dashboard script
    script appears to have been broken by a recent grafana update. adapt the script to fetch dashboards
    by uid and update to python3
    
    Change-Id: I4113238027b2f829ff2dfcec39aacc093eb6a990

--- modules/grafana/files/grafana-dashboard.py
+++ modules/grafana/files/grafana-dashboard.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # export grafana dashboard JSON description for revision control
 # pretty printing and sorting the JSON representation should minimise
@@ -7,30 +7,22 @@
 import argparse
 import json
 import logging
-import re
 import sys
+from urllib.parse import urlparse, urlunparse
 
 import requests
-
-DEFAULT_URL = 'https://grafana.wikimedia.org/api/dashboards/db/%s'
 
 log = logging.getLogger(__name__)
 
 
 def dump_dashboard(url, tags=None):
-    if '/' not in url:
-        url = DEFAULT_URL % url
-    # non-API url, convert to API
-    elif '/dashboard/' in url:
-        url = re.sub('/dashboard/', '/api/dashboards/', url)
+    parsed = urlparse(url)
+    name, uid = get_name_and_uid(parsed)
+    api_url = urlunparse(
+        (parsed.scheme, parsed.netloc, '/api/dashboards/uid/{}'.format(uid), '', '', '')
+    )
 
-    name = url.split('/')[-1]
-
-    # when saved to disk we might add .json extension, strip it
-    if name.endswith('.json'):
-        name = re.sub(r'\.json$', '', name)
-
-    req = requests.get(url)
+    req = requests.get(api_url, headers={'Content-Type': 'application/json'})
     req.raise_for_status()
 
     dashboard = req.json()['dashboard']
@@ -46,10 +38,24 @@ def dump_dashboard(url, tags=None):
                   separators=(',', ': '), indent=2)
 
 
+def get_name_and_uid(parsed):
+    uid, name = parsed.path.split('/')[-2:]
+    if not uid or not len(uid) == 9 or not name:
+        raise ValueError('Provided url is missing either uid or name.  '
+                         'Expected form: https://grafana.wikimedia.org/d/000000001/dashboard-name')
+    # when saved to disk we might add .json extension, strip it
+    if name.endswith('.json'):
+        name = name[:-5]
+    return name, uid
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('dashboard', help='URL or dashboard name to act on',
-                        action='append', nargs='+')
+    parser.add_argument('dashboard',
+                        help='Dashboard url '
+                             '(e.g. https://grafana.wikimedia.org/d/000000001/dashboard-name)',
+                        action='append',
+                        nargs='+')
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
