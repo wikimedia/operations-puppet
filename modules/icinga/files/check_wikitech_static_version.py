5e481e450ea889b1fcd2e096b5340d39804b3eb8
John Bond <jbond@wikimedia.org>
2019-07-02 19:24:13 +0100

    python3/icinga check: refactor check to python3
    Change-Id: Iae3a31c4e1c84218520dc5b21c80207fd129562f

--- modules/icinga/files/check_wikitech_static_version.py
+++ modules/icinga/files/check_wikitech_static_version.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # Check the MediaWiki version of wikitech-static.wikimedia.org against the
@@ -18,8 +18,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 # http://www.gnu.org/copyleft/gpl.html
-
-from __future__ import print_function
+"""Icinga check to ensure wikietech static is running the correct version"""
 
 import os
 import sys
@@ -58,39 +57,46 @@ def mwapi(server, **kwargs):
     query.update(kwargs)
     headers = requests.utils.default_headers()
     headers['User-agent'] = 'wmf-icinga/{} (root@wikimedia.org)'.format(os.path.basename(__file__))
-    req = requests.get(url, params=query, headers=headers)
-    req.raise_for_status()
+    try:
+        req = requests.get(url, params=query, headers=headers)
+        req.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        msg = 'Failed to fetch json from {}: {}'.format(server, error)
+        icinga_exit(UNKNOWN, msg)
     return req.json()
 
 
-# Fetch the latest stable version number from mediawiki.org's
-# [[Template:MW stable release number]] in a format that will match siteinfo
-# output.
-try:
-    stable = mwapi(
+def main():
+    """Main entry point"""
+    # Fetch the latest stable version number from mediawiki.org's
+    # [[Template:MW stable release number]] in a format that will match siteinfo
+    # output.
+    mediawiki_data = mwapi(
         'www.mediawiki.org',
         action='expandtemplates', prop='wikitext',
         text='MediaWiki {{MW stable release number}}',
-    )['expandtemplates']['wikitext']
-except Exception:
-    icinga_exit(
-        UNKNOWN,
-        'Failed to fetch latest MediaWiki version from mediawiki.org')
+    )
+    stable_version = mediawiki_data.get('expandtemplates', {}).get('wikitext', None)
+    target_data = mwapi(TARGET, action='query', meta='siteinfo', siprop='general')
+    target_version = target_data.get('query', {}).get('general', {}).get('generator', None)
 
-# Fetch the current version from the target wiki.
-try:
-    version = mwapi(
-        TARGET,
-        action='query', meta='siteinfo', siprop='general',
-    )['query']['general']['generator']
-except Exception:
-    icinga_exit(
-        UNKNOWN, 'Failed to fetch MediaWiki version for {}', TARGET)
+    if stable_version is None:
+        icinga_exit(UNKNOWN, 'Unable to fetch version number from www.mediawiki.org')
+    if target_version is None:
+        icinga_exit(UNKNOWN, 'Unable to fetch version number from {}'.format(TARGET))
 
-if version == stable:
-    icinga_exit(OK, '{} is running {}', TARGET, stable)
-else:
-    icinga_exit(
-        WARNING, '{} is running {}, latest is {} '
-                 'Consult https://wikitech.wikimedia.org/wiki/Wikitech-static '
-                 'for details.', TARGET, version, stable)
+    if target_version == stable_version:
+        icinga_exit(OK, '{} is running {}', TARGET, stable_version)
+    else:
+        icinga_exit(
+            WARNING,
+            '{} is running {}, latest is {} '
+            'Consult https://wikitech.wikimedia.org/wiki/Wikitech-static '
+            'for details.', TARGET, target_version, stable_version)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as error:
+        icinga_exit(UNKNOWN, 'An unknown error occurred: {}'.format(error))
