e72bbdd7a8b61330aa520c7f99d5bfa0588cb96d
Ariel T. Glenn <ariel@wikimedia.org>
2019-07-02 12:17:13 +0300

    set dump cron start dates and times back to normal
    Change-Id: Ie29e383bfa62f095e337a706dc51c779b6555f92

--- modules/snapshot/manifests/dumps/cron.pp
+++ modules/snapshot/manifests/dumps/cron.pp
@@ -36,7 +36,7 @@ class snapshot::dumps::cron(
         user        => $user,
         command     => "/usr/local/bin/fulldumps.sh 01 14 ${runtype} full ${maxjobs} > /dev/null",
         minute      => '05',
-        hour        => [8, 22],
+        hour        => [8, 20],
         monthday    => '01-14',
     }
 
