aea387e398a9bdbef30b16da15f758a85c57bc88
bstorm <bstorm@wikimedia.org>
2019-07-03 15:38:20 -0400

    toolforge: set up cert distribution for additional control plane nodes
    This obviously requires that the certs for the initial node be added to
    the local puppetmaster as secrets.  That seems to be a sensible way to
    join new nodes. This only includes the safe ones. kubeadm should generate
    the certs, using these, for the new apiserver modules.
    
    This is triggers via hiera because we have to have some manual way of
    saying the certs are there and should be used.  As long as a single control plane
    node is in the cluster, we should always be able to tear down and rebuild/join
    control plane nodes via this method.
    
    Bug: T215531
    Change-Id: I3b9c3da7a609e41d7afd9b02500176275abb3466

--- modules/profile/manifests/toolforge/k8s/kubeadm/master.pp
+++ modules/profile/manifests/toolforge/k8s/kubeadm/master.pp
@@ -1,7 +1,8 @@
 class profile::toolforge::k8s::kubeadm::master(
-    Array[Stdlib::Fqdn] $etcd_hosts = lookup('profile::toolforge::k8s::etcd_hosts'),
-    Stdlib::Fqdn        $apiserver  = lookup('profile::toolforge::k8s::apiserver'),
-    String              $node_token = lookup('profile::toolforge::k8s::node_token'),
+    Array[Stdlib::Fqdn] $etcd_hosts     = lookup('profile::toolforge::k8s::etcd_hosts'),
+    Stdlib::Fqdn        $apiserver      = lookup('profile::toolforge::k8s::apiserver'),
+    String              $node_token     = lookup('profile::toolforge::k8s::node_token'),
+    Boolean             $existing_certs = lookup('profile::toolforge::k8s::existing_certs', { default_value => false})
 ) {
     require profile::toolforge::k8s::kubeadm::preflight_checks
 
@@ -22,5 +23,86 @@ class profile::toolforge::k8s::kubeadm::master(
 
     class { 'toolforge::k8s::kubeadm_calico':
         pod_subnet     => $pod_subnet,
+    }
+
+    # If there is an existing, bootstrapped control plane node, distribute certs from it
+    if $existing_certs {
+        file { '/etc/kubernetes/pki':
+            ensure => directory,
+            mode   => '0755',
+            owner  => 'root',
+            group  => 'root',
+        }
+
+        file { '/etc/kubernetes/pki/ca.crt':
+            content   => secret('toolforge/k8s/ca.crt'),
+            require   => File['/etc/kubernetes/pki'],
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0444',
+            show_diff => false,
+        }
+        file { '/etc/kubernetes/pki/ca.key':
+            content   => secret('toolforge/k8s/ca.key'),
+            require   => File['/etc/kubernetes/pki'],
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0400',
+            show_diff => false,
+        }
+
+        file { '/etc/kubernetes/pki/sa.pub':
+            content   => secret('toolforge/k8s/sa.pub'),
+            require   => File['/etc/kubernetes/pki'],
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0400',
+            show_diff => false,
+        }
+
+        file { '/etc/kubernetes/pki/sa.key':
+            content   => secret('toolforge/k8s/sa.key'),
+            require   => File['/etc/kubernetes/pki'],
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0400',
+            show_diff => false,
+        }
+
+        file { '/etc/kubernetes/pki/front-proxy-ca.crt':
+            content   => secret('toolforge/k8s/front-proxy-ca.crt'),
+            require   => File['/etc/kubernetes/pki'],
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0444',
+            show_diff => false,
+        }
+
+        file { '/etc/kubernetes/pki/front-proxy-ca.key':
+            content   => secret('toolforge/k8s/front-proxy-ca.key'),
+            require   => File['/etc/kubernetes/pki'],
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0400',
+            show_diff => false,
+        }
+
+        file { '/etc/kubernetes/pki/etcd/ca.crt':
+            content   => secret('toolforge/k8s/etcd-ca.crt'),
+            require   => File['/etc/kubernetes/pki'],
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0444',
+            show_diff => false,
+        }
+
+        file { '/etc/kubernetes/pki/etcd/ca.key':
+            content   => secret('toolforge/k8s/etcd-ca.key'),
+            require   => File['/etc/kubernetes/pki'],
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0400',
+            show_diff => false,
+        }
     }
 }
