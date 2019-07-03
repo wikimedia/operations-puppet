c369eefef6b3af1d5ec2e7a07206e933d8a6fe02
bstorm <bstorm@wikimedia.org>
2019-07-03 08:59:26 -0400

    toolforge: correct the values for bootstrapping a cluster
    We needed to add information about the load balancer in here as well as
    remove PodSecurityPolicy for now.
    
    Bug: T215531
    Change-Id: I285e149b3f55cf3b051cc3451a6724575a34f113

--- modules/profile/manifests/toolforge/k8s/kubeadm/master.pp
+++ modules/profile/manifests/toolforge/k8s/kubeadm/master.pp
@@ -1,18 +1,18 @@
 class profile::toolforge::k8s::kubeadm::master(
     Array[Stdlib::Fqdn] $etcd_hosts = lookup('profile::toolforge::k8s::etcd_hosts'),
     Stdlib::Fqdn        $apiserver  = lookup('profile::toolforge::k8s::apiserver'),
-    String              $dns_domain = lookup('profile::toolforge::k8s::dns_domain'),
     String              $node_token = lookup('profile::toolforge::k8s::node_token'),
 ) {
     require profile::toolforge::k8s::kubeadm::preflight_checks
 
     class { 'toolforge::k8s::kubeadm': }
+
+    $pod_subnet = '192.168.0.0/16'
     class { 'toolforge::k8s::kubeadm_init':
-        etcd_hosts     => $etcd_hosts,
-        apiserver      => $apiserver,
-        pod_subnet     => '192.168.0.0/24', # TODO: review this
-        service_subnet => '192.168.1.0/24', # TODO: review this
-        node_token     => $node_token,
+        etcd_hosts => $etcd_hosts,
+        apiserver  => $apiserver,
+        pod_subnet => $pod_subnet,
+        node_token => $node_token,
     }
 
     class { 'toolforge::k8s::kubeadm_join':
@@ -21,6 +21,6 @@ class profile::toolforge::k8s::kubeadm::master(
     }
 
     class { 'toolforge::k8s::kubeadm_calico':
-        pod_subnet     => '192.168.0.0/24', # TODO: make this a parameter
+        pod_subnet     => $pod_subnet,
     }
 }
--- modules/toolforge/manifests/k8s/kubeadm_init.pp
+++ modules/toolforge/manifests/k8s/kubeadm_init.pp
@@ -1,12 +1,10 @@
 class toolforge::k8s::kubeadm_init(
     Stdlib::Fqdn        $apiserver,
     String              $pod_subnet,
-    String              $service_subnet,
     Array[Stdlib::Fqdn] $etcd_hosts = [],
     String              $kubernetes_version = '1.15.0',
     String              $node_token = undef,
 ) {
-    $apiserver_ip = ipresolve($apiserver, 4)
     file { '/etc/kubernetes/kubeadm-init.yaml':
         ensure  => present,
         content => template('toolforge/k8s/kubeadm-init.yaml.erb'),
