de7fad83013d3984a4a89acefddc510bc154c070
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-07-03 11:45:04 +0200

    toolforge: k8s: fix half-done code factorization for kubadm_join
    In a previous patch we factorized the code, but this block which is meant to be
    shared was left behind (thus, duplicated). Fix this now by simply dropping it.
    
    Users of toolforge::k8s::kubeadm_init now have to use the class
    toolforge::k8s::kubeadm_init as well. That is the case of the user
    profile::toolforge::k8s::kubeadm::master.
    
    Bug: T215531
    Change-Id: Idc26caf89269c17fea77f558bea48bc012ab2614
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/toolforge/manifests/k8s/kubeadm_init.pp
+++ modules/toolforge/manifests/k8s/kubeadm_init.pp
@@ -15,13 +15,4 @@ class toolforge::k8s::kubeadm_init(
         mode    => '0400',
         require => File['/etc/kubernetes'],
     }
-
-    file { '/etc/kubernetes/kubeadm-join.yaml':
-        ensure  => present,
-        content => template('toolforge/k8s/kubeadm-join.yaml.erb'),
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0400',
-        require => File['/etc/kubernetes'],
-    }
 }
