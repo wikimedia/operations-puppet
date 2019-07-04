02ce7e1768b045ebdc08e7b0f30038b18a4b31a5
Emanuele Rocca <ema@wikimedia.org>
2019-07-04 14:32:17 +0200

    cache: reimage cp1080 as upload_ats
    Bug: T226638
    Change-Id: I54eb002ebd9892b1a405a547d804f8bc3d25d0fa

--- manifests/site.pp
+++ manifests/site.pp
@@ -302,12 +302,12 @@ node /^cp10(7[579]|8[13579])\.eqiad\.wmnet$/ {
     role(cache::text)
 }
 
-node /^cp10(8[02468]|90)\.eqiad\.wmnet$/ {
+node /^cp10(8[2468]|90)\.eqiad\.wmnet$/ {
     interface::add_ip6_mapped { 'main': }
     role(cache::upload)
 }
 
-node /^cp107[68]\.eqiad\.wmnet$/ {
+node /^cp10(7[68]|80)\.eqiad\.wmnet$/ {
     interface::add_ip6_mapped { 'main': }
     role(cache::upload_ats)
 }
