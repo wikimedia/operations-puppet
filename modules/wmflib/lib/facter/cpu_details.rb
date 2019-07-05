3b6f65ad13e15753777bb3b69fcad52d71a7c996
John Bond <jbond@wikimedia.org>
2019-07-05 14:45:33 +0100

    facter - cpu_details: add governor and scaling_driver facts
    e.g.
     $ sudo facter -p cpu_details.scaling_governor
     powersave
     $ sudo facter -p cpu_scaling_driver
     intel_pstate
     $ sudo facter -p cpu_scaling_governor
     powersave
     $ sudo facter -p cpu_details.cpus.cpu0
     {
       governor => "powersave",
       scaling_driver => "intel_pstate"
     }
    
    Bug: T225713
    Change-Id: Id9b18e75c2085d31918573de0f718dcd22fdd727

--- modules/wmflib/lib/facter/cpu_details.rb
+++ modules/wmflib/lib/facter/cpu_details.rb
@@ -1,20 +1,47 @@
 require 'facter'
 
+cpu_details = {
+  'vulnerabilities' => {},
+  'scaling_governor' => false,
+  'scaling_driver' => false,
+  'cpus' => {}
+}
 Facter.add(:cpu_details) do
   confine :kernel => 'Linux'
   confine do
     File.directory?('/sys/devices/system/cpu/vulnerabilities')
   end
   setcode do
-    cpu_details = {
-      'vulnerabilities' => {},
-    }
     Dir.foreach('/sys/devices/system/cpu/vulnerabilities') do |vuln|
       next if %w{. ..}.include? vuln
       cpu_details['vulnerabilities'][vuln] = File.read(
         "/sys/devices/system/cpu/vulnerabilities/#{vuln}"
-      )
+      ).strip
+    end
+    Dir.glob('/sys/devices/system/cpu/cpu*').each do |cpu_dir|
+      cpu_id = File.basename(cpu_dir)
+      governor_file = File.join(cpu_dir, 'cpufreq/scaling_governor')
+      driver_file = File.join(cpu_dir, 'cpufreq/scaling_driver')
+      cpu_details['cpus'][cpu_id] = {
+        'scaling_governor' => false,
+        'scaling_driver' => false,
+      }
+      if File.file?(governor_file)
+        governor_content = File.read(governor_file).strip
+        cpu_details['cpus'][cpu_id]['scaling_governor'] = governor_content
+        cpu_details['scaling_governor'] = governor_content unless cpu_details['scaling_governor']
+      end
+      next unless File.file?(driver_file)
+      driver_content = File.read(driver_file).strip
+      cpu_details['cpus'][cpu_id]['scaling_driver'] = driver_content
+      cpu_details['scaling_driver'] = driver_content unless cpu_details['scaling_driver']
     end
     cpu_details
   end
+end
+Facter.add(:cpu_scaling_governor) do
+  setcode { cpu_details['scaling_governor'] }
+end
+Facter.add(:cpu_scaling_driver) do
+  setcode { cpu_details['scaling_driver'] }
 end
