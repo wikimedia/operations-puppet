a4b863e0f53a1f27e5420653a498fce2a7692374
Emanuele Rocca <ema@wikimedia.org>
2019-07-09 10:23:31 +0200

    monitoring: update icinga links to varnish-aggregate-client-status-codes
    We have moved from the statsd version of the "Varnish: Aggregate Client
    Status Codes" dashboard to the prometheus one in 756561b. However, the
    alerts sent by Icinga still carry links relevant to the previous version
    of the dashboard. This patch updates them.
    
    Bug: T184942
    Change-Id: Ide02a277c4ae1fa25a9b7d6461674f091f5d7f4d

--- modules/profile/manifests/graphite/alerts/reqstats.pp
+++ modules/profile/manifests/graphite/alerts/reqstats.pp
@@ -10,7 +10,7 @@ class profile::graphite::alerts::reqstats($graphite_url = hiera('graphite_url'))
     # sites aggregates
     monitoring::graphite_threshold { 'reqstats-5xx-eqiad':
         description     => 'Eqiad HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=eqiad&var-cache_type=All&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/d/myRmf1Pik/varnish-aggregate-client-status-codes?var-site=eqiad&var-status_type=5'],
         metric          => 'sumSeries(varnish.eqiad.*.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -21,7 +21,7 @@ class profile::graphite::alerts::reqstats($graphite_url = hiera('graphite_url'))
 
     monitoring::graphite_threshold { 'reqstats-5xx-esams':
         description     => 'Esams HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=esams&var-cache_type=All&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/d/myRmf1Pik/varnish-aggregate-client-status-codes?var-site=esams&var-status_type=5'],
         metric          => 'sumSeries(varnish.esams.*.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -32,7 +32,7 @@ class profile::graphite::alerts::reqstats($graphite_url = hiera('graphite_url'))
 
     monitoring::graphite_threshold { 'reqstats-5xx-codfw':
         description     => 'Codfw HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=codfw&var-cache_type=All&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/d/myRmf1Pik/varnish-aggregate-client-status-codes?var-site=codfw&var-status_type=5'],
         metric          => 'sumSeries(varnish.codfw.*.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -43,7 +43,7 @@ class profile::graphite::alerts::reqstats($graphite_url = hiera('graphite_url'))
 
     monitoring::graphite_threshold { 'reqstats-5xx-ulsfo':
         description     => 'Ulsfo HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=ulsfo&var-cache_type=All&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/d/myRmf1Pik/varnish-aggregate-client-status-codes?var-site=ulsfo&var-status_type=5'],
         metric          => 'sumSeries(varnish.ulsfo.*.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -54,7 +54,7 @@ class profile::graphite::alerts::reqstats($graphite_url = hiera('graphite_url'))
 
     monitoring::graphite_threshold { 'reqstats-5xx-eqsin':
         description     => 'Eqsin HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=eqsin&var-cache_type=All&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/d/myRmf1Pik/varnish-aggregate-client-status-codes?var-site=eqsin&var-status_type=5'],
         metric          => 'sumSeries(varnish.eqsin.*.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -66,7 +66,7 @@ class profile::graphite::alerts::reqstats($graphite_url = hiera('graphite_url'))
     # per-cache aggregates
     monitoring::graphite_threshold { 'reqstats-5xx-text':
         description     => 'Text HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=All&var-cache_type=text&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/d/myRmf1Pik/varnish-aggregate-client-status-codes?var-cache_type=varnish-text&var-status_type=5'],
         metric          => 'sumSeries(varnish.*.text.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
@@ -77,7 +77,7 @@ class profile::graphite::alerts::reqstats($graphite_url = hiera('graphite_url'))
 
     monitoring::graphite_threshold { 'reqstats-5xx-upload':
         description     => 'Upload HTTP 5xx reqs/min',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/file/varnish-aggregate-client-status-codes?panelId=3&fullscreen&orgId=1&var-site=All&var-cache_type=upload&var-status_type=5'],
+        dashboard_links => ['https://grafana.wikimedia.org/d/myRmf1Pik/varnish-aggregate-client-status-codes?var-cache_type=varnish-upload&var-status_type=5'],
         metric          => 'sumSeries(varnish.*.upload.frontend.request.client.status.5xx.sum)',
         warning         => $settings['warning'],
         critical        => $settings['critical'],
