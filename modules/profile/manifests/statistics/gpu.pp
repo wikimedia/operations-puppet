fc5e5cb0e9bb800c06311bfbaa65a35c4dd613b8
Luca Toscano <ltoscano@wikimedia.org>
2019-07-09 12:36:36 +0200

    profile::statistics::gpu: add packages from thirdparty/rocm
    This change to the profile is a test to see if all the packages
    that we need from AMD ROCm have been imported successfully.
    
    Bug: T224723
    Change-Id: I093d9ea618ef7683e6e6f3619a4ef475a8065890

--- modules/profile/manifests/statistics/gpu.pp
+++ modules/profile/manifests/statistics/gpu.pp
@@ -7,6 +7,49 @@ class profile::statistics::gpu {
         ensure => present,
     }
 
+    if os_version('debian >= buster'){
+        apt::repository { 'amd-rocm':
+            uri        => 'http://apt.wikimedia.org/wikimedia',
+            dist       => "${::lsbdistcodename}-wikimedia",
+            components => 'thirdparty/amd-rocm',
+            notify     => Exec['apt_update_rocm'],
+        }
+
+        exec {'apt_update_rocm':
+            command     => '/usr/bin/apt-get update',
+            refreshonly => true,
+        }
+
+        $packages = [
+            'cxlactivitylogger',
+            'hcc',
+            'hsa-rocr-dev',
+            'hsakmt-roct',
+            'miopen-hip',
+            'miopen-opencl',
+            'mivisionx',
+            'radeontop',
+            'rocblas',
+            'rocfft',
+            'rocm-cmake',
+            'rocm-dev',
+            'rocm-device-libs',
+            'rocm-opencl',
+            'rocm-opencl-dev',
+            'rocm-utilsv',
+            'rocrand',
+        ]
+
+        package { $packages:
+            ensure  => 'latest',
+            require => [
+                Exec['apt_update_rocm'],
+                Apt::Repository['amd-rocm'],
+            ],
+        }
+
+    }
+
     # Wide range of packages that we deploy across all the stat nodes.
     include ::profile::analytics::cluster::packages::statistics
 }
