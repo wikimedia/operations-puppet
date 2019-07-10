1b0ef6583948403b31b422ac4bc2376067de95b9
Emanuele Rocca <ema@wikimedia.org>
2019-07-10 12:53:39 +0200

    cache: refresh text VTC tests
    Make cache_text VTC tests work locally with the recently added
    Vagrant-based infrastructure. Remove 16-normalize-path.vtc, which has
    never worked, and align to current reality tests like
    22-cache_misc-vcl-switch.vtc.
    
    Change-Id: I1a115b54f1116d3f5dc0bfb766c45ff845658417

--- modules/varnish/files/tests/run.sh
+++ modules/varnish/files/tests/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: $0 hostname change_id"
+    echo "Usage: $0 change_id hostname"
     exit 1
 fi
 
-vagrant ssh -c "export JENKINS_USERNAME=$JENKINS_USERNAME ; export JENKINS_API_TOKEN=$JENKINS_API_TOKEN ; cd /vagrant/ ; ./run.py $1 $2 /utils/pcc"
+vagrant ssh -c "export JENKINS_USERNAME=$JENKINS_USERNAME ; export JENKINS_API_TOKEN=$JENKINS_API_TOKEN ; cd /vagrant/ ; ./run.py $2 $1 /utils/pcc"
