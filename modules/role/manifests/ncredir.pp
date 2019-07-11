8f97d5369f7a00566a1038b0af5e09744468e1e3
Valentin Gutierrez <vgutierrez@wikimedia.org>
2019-07-11 17:33:28 +0700

    lvs: Add ncredir service to high-traffic1
    This must be merged after I737b6abab129b05e626820ba3838d63a026da98e
    
    Bug: T133548
    Change-Id: If0ba921a3c43b92ba62854f31f3db09fee1d10e7

--- modules/role/manifests/ncredir.pp
+++ modules/role/manifests/ncredir.pp
@@ -2,5 +2,7 @@ class role::ncredir {
     system::role { 'ncredir': description => 'Non canonical domains redirection service' }
     include ::profile::standard
     include ::profile::base::firewall
+    # TODO: use ::role::lvs::realserver instead
+    include ::lvs::realserver  # lint:ignore:wmf_styleguide
     include ::profile::ncredir
 }
