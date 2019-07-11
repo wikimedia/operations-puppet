c1795d1bf57870e12755c0d53a11b7f3e568a22e
John Bond <jbond@wikimedia.org>
2019-07-11 14:56:30 +0100

    hiera backends: update hiera.yaml file to work with puppet 4.9
    newer version of puppet require all top level keys i.e. backends to be defined
    in the backend array[1].  Currently we abuse the fact that there is a
    lack of checking currently so that we can change the `datadir` based on
    whether an element in the hierarchy is prefixed with `private` or
    `secret`
    
    This update fixes the config so that is compatible with newer versions
    of puppet and also creates empty backend objects for secret and private.
    
    [1]https://github.com/puppetlabs/puppet/blob/4.9.0/lib/puppet/pops/lookup/hiera_config.rb#L301-L307
    
    Bug: T227779
    Change-Id: I5b2ab74c722f107db18fb44d3ad3d91cf8a4a11b

---
+++ modules/wmflib/lib/hiera/backend/private_backend.rb
@@ -0,0 +1,13 @@
+class Hiera
+  module Backend
+    # This naming is required by puppet.
+    class Private_backend
+      # This is not a real backend.  We abuse the backend functionality so we
+      # can dynamicly change 'datadirs' in the nuyaml3 backend based on the
+      # directory prefix 'private/'
+      def lookup(_key, _scope, _order_override, _resolution_type, _context)
+        throw(:no_such_key)
+      end
+    end
+  end
+end
---
+++ modules/wmflib/lib/hiera/backend/secret_backend.rb
@@ -0,0 +1,13 @@
+class Hiera
+  module Backend
+    # This naming is required by puppet.
+    class Secret_backend
+      # This is not a real backend.  We abuse the backend functionality so we
+      # can dynamicly change 'datadirs' in the nuyaml3 backend based on the
+      # directory prefix 'secret/'
+      def lookup(_key, _scope, _order_override, _resolution_type, _context)
+        throw(:no_such_key)
+      end
+    end
+  end
+end
