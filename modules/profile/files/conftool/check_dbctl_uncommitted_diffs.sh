be918e3821e6c0fbe066eee8945064a66b784c9f
Chris Danis <cdanis@wikimedia.org>
2019-07-14 21:52:42 -0400

    dbctl: monitor for uncommitted changes
    This monitors for changes made to instance/sections objects that have not
    yet been pushed live using 'dbctl config commit'.  It is similar in concept
    to the monitoring::icinga::git_merge alerts used on the puppetmasters for
    puppet repo and on the deployment servers for mediawiki-config.
    
    Bug: T197126
    Change-Id: I6295b2151e5eea7bc3d7b80fc13c1b035a825425

---
+++ modules/profile/files/conftool/check_dbctl_uncommitted_diffs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Nagios/Icinga check command for dbctl, MediaWiki database config in etcd.
+# See also https://wikitech.wikimedia.org/wiki/Dbctl
+
+# Returns CRITICAL (2) on any uncommitted dbctl diffs, or on any error.
+# Returns OK (0) otherwise.
+
+set -uf
+
+dbctl config diff --quiet
+case "$?" in
+    0)
+        echo "OK - no diffs"
+        exit 0
+    ;;
+    1)
+        echo "CRITICAL - Uncommitted dbctl configuration changes, check dbctl config diff"
+        exit 2
+    ;;
+    *)
+        echo "CRITICAL - Unknown error executing dbctl config diff"
+        exit 2
+    ;;
+esac
--- modules/profile/manifests/conftool/dbctl_client.pp
+++ modules/profile/manifests/conftool/dbctl_client.pp
@@ -2,4 +2,26 @@ class profile::conftool::dbctl_client() {
     require ::profile::conftool::client
 
     require_package('python3-conftool-dbctl', 'etcd-client')
+
+    # Set up a check for uncommitted diffs
+    $filename = '/usr/local/lib/nagios/plugins/check_dbctl_uncommitted_diffs'
+
+    file { $filename:
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+        source => 'puppet:///modules/profile/conftool/check_dbctl_uncommitted_diffs.sh'
+    }
+
+    nrpe::monitor_service { 'dbctl_uncommitted_diffs':
+        ensure         => present,
+        description    => 'Uncommitted dbctl configuration changes, check dbctl config diff',
+        nrpe_command   => $filename,
+        critical       => false,
+        check_interval => 5,
+        retry_interval => 5,
+        retries        => 3,
+        notes_url      => 'https://wikitech.wikimedia.org/wiki/Dbctl#Uncommitted_dbctl_diffs'
+    }
 }
