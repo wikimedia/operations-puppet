090694ade2a2b4189c8a6f9e992f21a64479c0ba
Antoine Musso <hashar@free.fr>
2019-07-15 14:22:10 +0200

    contint: remove php packages
    We no more need PHP on the Jenkins instances. Should be a noop for
    production hosts.
    
    Hosts: contint1001.wikimedia.org
    
    Bug: T225735
    Change-Id: I18cf365743bb2be53351a40a756b140f05dd8d8c

--- modules/contint/manifests/packages/apt.pp
+++ modules/contint/manifests/packages/apt.pp
@@ -4,17 +4,4 @@
 #
 class contint::packages::apt {
     include ::apt::unattendedupgrades
-
-    if os_version('debian == jessie') {
-        # Sanity check: only enable in labs
-        requires_realm('labs')
-        # Enable deb.sury.org PHP packages for jessie only
-        apt::repository { 'sury-php':
-            uri        => 'https://packages.sury.org/php/',
-            dist       => $::lsbdistcodename,
-            components => 'main',
-            source     => false,
-            keyfile    => 'puppet:///modules/contint/sury-php.gpg',
-        }
-    }
 }
--- modules/contint/manifests/packages/labs.pp
+++ modules/contint/manifests/packages/labs.pp
@@ -5,14 +5,7 @@ class contint::packages::labs {
 
     require ::contint::packages::apt
 
-    # We're no longer installing PHP on app servers starting with
-    # jessie, but we still need it for CI
-    if os_version('debian == jessie') {
-        include ::contint::packages::php5
-    }
-
     include ::contint::packages::javascript
-    include ::contint::packages::php
     include ::contint::packages::ruby
 
     # Database related
