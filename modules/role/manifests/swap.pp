0d0a4eebca7e738ba7480bd4988a437682deb74e
Luca Toscano <ltoscano@wikimedia.org>
2019-07-15 09:07:16 +0200

    role::swap: add profile::base::firewall
    Bug: T170826
    Change-Id: Ibe694f560ce73d0923510914f9d81583bd42459d

--- modules/role/manifests/swap.pp
+++ modules/role/manifests/swap.pp
@@ -6,6 +6,7 @@
 class role::swap {
     system::role { 'SWAP (Jupyter Notebook)': }
     include ::profile::standard
+    include ::profile::base::firewall
     include ::profile::analytics::cluster::gitconfig
     include ::profile::swap
     include ::profile::analytics::cluster::packages::hadoop
