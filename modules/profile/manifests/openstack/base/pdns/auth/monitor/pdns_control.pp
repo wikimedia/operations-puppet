c3d199c4d97c1d4901b83b23db0a58aab6143dbd
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2019-07-15 09:25:29 +0200

    Remove sudo users formerly used by Diamond collectors
    Diamond is removed from production and these can go away, the existing
    sudoers file entries will be removed with Cumin.
    
    Change-Id: Ia17620994c807dabbb5fd118a948223845f7fe03

--- modules/profile/manifests/openstack/base/pdns/auth/monitor/pdns_control.pp
+++ modules/profile/manifests/openstack/base/pdns/auth/monitor/pdns_control.pp
@@ -1,10 +1,5 @@
 class profile::openstack::base::pdns::auth::monitor::pdns_control {
 
-    sudo::user { 'diamond_sudo_for_pdns':
-        user       => 'diamond',
-        privileges => ['ALL=(root) NOPASSWD: /usr/bin/pdns_control list'],
-    }
-
     sudo::user { 'prometheus_sudo_for_pdns':
         user       => 'prometheus',
         privileges => ['ALL=(root) NOPASSWD: /usr/bin/pdns_control list'],
--- modules/profile/manifests/openstack/base/pdns/recursor/monitor/rec_control.pp
+++ modules/profile/manifests/openstack/base/pdns/recursor/monitor/rec_control.pp
@@ -1,10 +1,5 @@
 class profile::openstack::base::pdns::recursor::monitor::rec_control {
 
-    sudo::user { 'diamond_sudo_for_pdns_recursor':
-        user       => 'diamond',
-        privileges => ['ALL=(root) NOPASSWD: /usr/bin/rec_control get-all'],
-    }
-
     sudo::user { 'prometheus_sudo_for_pdns_recursor':
         user       => 'prometheus',
         privileges => ['ALL=(root) NOPASSWD: /usr/bin/rec_control get-all'],
