78896253f2906290de3c57ea3a9c820d4a99b7d6
John Bond <jbond@wikimedia.org>
2019-07-16 16:47:20 +0100

    hiera: use hiera v1 backend in labs
    Bug: T228174
    Change-Id: I9629266e7450a436f097053cd7d2e1a59a2ba848

--- modules/wmflib/lib/hiera/backend/private3_backend.rb
+++ modules/wmflib/lib/hiera/backend/private3_backend.rb
@@ -1,11 +1,11 @@
 class Hiera
   module Backend
     # This naming is required by puppet.
-    class Secret_backend
+    class Private_backend
       # This is not a real backend.  We abuse the backend functionality so we
       # can dynamicly change 'datadirs' in the nuyaml3 backend based on the
-      # directory prefix 'secret/'
-      def lookup(_key, _scope, _order_override, _resolution_type)
+      # directory prefix 'private/'
+      def lookup(_key, _scope, _order_override, _resolution_type, _context)
         throw(:no_such_key)
       end
     end
--- modules/wmflib/lib/hiera/backend/private_backend.rb
+++ modules/wmflib/lib/hiera/backend/private_backend.rb
@@ -5,7 +5,7 @@ class Hiera
       # This is not a real backend.  We abuse the backend functionality so we
       # can dynamicly change 'datadirs' in the nuyaml3 backend based on the
       # directory prefix 'private/'
-      def lookup(_key, _scope, _order_override, _resolution_type, _context)
+      def lookup(_key, _scope, _order_override, _resolution_type)
         throw(:no_such_key)
       end
     end
