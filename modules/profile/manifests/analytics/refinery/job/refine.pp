19b1df58553b5dfda1464e9c7e77f3b74782c740
Nuria Ruiz <nuria@wikimedia.org>
2019-07-18 15:51:38 -0700

    Correcting package name in transform function
    Bug: T227150
    Change-Id: Iac3c63c3f384144a5ac242b2fa767bce15f00816

--- modules/profile/manifests/analytics/refinery/job/refine.pp
+++ modules/profile/manifests/analytics/refinery/job/refine.pp
@@ -38,7 +38,7 @@ class profile::analytics::refinery::job::refine {
             input_path_regex_capture_groups => 'table,year,month,day,hour',
             table_blacklist_regex           => '^Edit|ChangesListHighlights|InputDeviceDynamics|PageIssues$',
             # Deduplicate basd on uuid field and geocode ip in EventLogging analytics data.
-            transform_functions             => 'org.wikimedia.analytics.refinery.job.refine.deduplicate_eventlogging,org.wikimedia.analytics.refinery.job.refine.geocode_ip,org.wikimedia.analytics.refinery.job.eventlogging_filter_is_allowed_hostname',
+            transform_functions             => 'org.wikimedia.analytics.refinery.job.refine.deduplicate_eventlogging,org.wikimedia.analytics.refinery.job.refine.geocode_ip,org.wikimedia.analytics.refinery.job.refine.eventlogging_filter_is_allowed_hostname',
             # Get EventLogging JSONSchemas from meta.wikimedia.org.
             schema_base_uri                 => 'eventlogging',
         }),
