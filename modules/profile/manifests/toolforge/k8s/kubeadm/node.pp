93e9e20e334df405c8c82002440ceca788ab2dc6
bstorm <bstorm@wikimedia.org>
2019-07-18 14:14:40 -0500

    toolforge: remove class redeclaration
    Because it was being forgotten in the master class, but it is essential
    for kubeadm to function correctly without warnings in a supported fashion,
    I made the docker service class a simple include at the module level.
    
    Because it is not a profile, I think it is an antipattern to split it out
    like this, when this profile is ultimately very simple.  The calico
    workaround I think is good to keep separate because it will likely be
    unnecessary in the future sometime.
    
    I didn't expect it to be declared again at the node level, so I have to
    remove it here for it to not error out.
    
    Bug: T215531
    Change-Id: I0de83e31e2702578c6b1d35fa96304808c37b7b2

--- modules/profile/manifests/toolforge/k8s/kubeadm/node.pp
+++ modules/profile/manifests/toolforge/k8s/kubeadm/node.pp
@@ -3,6 +3,5 @@ class profile::toolforge::k8s::kubeadm::node(
     require profile::toolforge::k8s::kubeadm::preflight_checks
 
     class { '::toolforge::k8s::kubeadm': }
-    class { '::toolforge::k8s::kubeadm_docker_service': }
     class { '::toolforge::k8s::kubeadm_calico_workaround': }
 }
