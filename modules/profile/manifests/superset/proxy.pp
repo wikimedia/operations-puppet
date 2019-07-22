a6a54a38ebd71ea902ebfe0eac4b852022e06525
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2019-07-22 13:08:11 +0200

    Superset: Read LDAP servers from Hiera and switch to read-only replicas
    Bug: T227650
    Change-Id: Ic1447016bfb0aca7ce9492b09a3d9470bd9c85f1

--- modules/profile/manifests/superset/proxy.pp
+++ modules/profile/manifests/superset/proxy.pp
@@ -2,7 +2,9 @@
 #
 # Sets up a WMF HTTP LDAP auth proxy.
 #
-class profile::superset::proxy {
+class profile::superset::proxy (
+    Hash $ldap_config        = lookup('ldap', Hash, hash, {}),
+) {
 
     require ::profile::analytics::httpd::utils
 
@@ -17,6 +19,8 @@ class profile::superset::proxy {
     class { '::passwords::ldap::production': }
 
     $proxypass = $passwords::ldap::production::proxypass
+    $ldap_server_primary = $ldap_config['ro-server']
+    $ldap_server_fallback = $ldap_config['ro-server-fallback']
 
     # Set up the VirtualHost
     httpd::site { 'superset.wikimedia.org':
