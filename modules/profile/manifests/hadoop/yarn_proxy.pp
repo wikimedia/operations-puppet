f94695b7ee658d2af44464130141e9aa28c88984
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2019-07-22 12:28:11 +0200

    yarn: Read LDAP servers from Hiera and switch to read-only replicas
    Bug: T227650
    Change-Id: Ia7e20d8af5550cd580c2154204f40267a180a0eb

--- modules/profile/manifests/hadoop/yarn_proxy.pp
+++ modules/profile/manifests/hadoop/yarn_proxy.pp
@@ -2,12 +2,16 @@
 #
 # Sets up a yarn ldap auth http proxy to the Hadoop ResourceManager web interface.
 #
-class profile::hadoop::yarn_proxy {
+class profile::hadoop::yarn_proxy (
+    Hash $ldap_config        = lookup('ldap', Hash, hash, {}),
+) {
     require profile::hadoop::common
     require profile::hadoop::httpd
     require ::profile::analytics::httpd::utils
     include ::passwords::ldap::production
 
+    $ldap_server_primary = $ldap_config['ro-server']
+    $ldap_server_fallback = $ldap_config['ro-server-fallback']
     $proxypass = $passwords::ldap::production::proxypass
 
     $resourcemanager_primary_host = $profile::hadoop::common::resourcemanager_hosts[0]
