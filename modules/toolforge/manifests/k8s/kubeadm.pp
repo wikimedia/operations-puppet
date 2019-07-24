9e47415cd0f6099683e809479a5365341cef9dda
bstorm <bstorm@wikimedia.org>
2019-07-24 15:23:56 -0600

    toolforge: add internal pause container to all the other kubelets
    Kubelet extra args do not propogate via kubeadm join.  This is very annoying,
    but the devs are not looking to change this for now. This file is sourced
    by the extra file created by kubeadm for systemd as the semi-supported workaround.
    The alternative is to use a join config which may or may not support it.
    
    Bug: T228887
    Bug: T215531
    Change-Id: I085a8bf386d4b90c0186511a9a5c7059dc78357e

--- modules/toolforge/manifests/k8s/kubeadm.pp
+++ modules/toolforge/manifests/k8s/kubeadm.pp
@@ -22,4 +22,10 @@ class toolforge::k8s::kubeadm(
     }
 
     include ::toolforge::k8s::kubeadm_docker_service
+
+    file { '/etc/default/kubelet':
+        ensure  => 'present',
+        mode    => '0444',
+        content => 'KUBELET_EXTRA_ARGS="--pod-infra-container-image=docker-registry.tools.wmflabs.org/pause:3.1"\n'
+    }
 }
