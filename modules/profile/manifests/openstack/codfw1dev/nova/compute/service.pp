c01799d308a176c19d8e3f78adca80bf9756cb11
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2019-07-25 09:45:15 +0200

    Fix name for kmod::options, it needs to match the kernel module name
    Change-Id: Ib4506adfd8005f29b192a13815a8d1d388224b2b

--- modules/profile/manifests/openstack/codfw1dev/nova/compute/service.pp
+++ modules/profile/manifests/openstack/codfw1dev/nova/compute/service.pp
@@ -28,7 +28,7 @@ class profile::openstack::codfw1dev::nova::compute::service(
     # For OpenStack configure unconditional flushes of the L1 cache during VMENTER
     # https://www.kernel.org/doc/html/latest/admin-guide/hw-vuln/l1tf.html
     if os_version('debian == stretch') and (versioncmp($::kernelrelease, '4.9.0-9-amd64') >= 0) {
-        kmod::options { 'kvm-vmentry-always':
+        kmod::options { 'kvm_intel':
             options => 'kvm-intel.vmentry_l1d_flush=always',
         }
     }
