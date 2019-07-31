3bf0609d0e1d975f118b245ffa1d6e6e2fdbd3f4
Luca Toscano <ltoscano@wikimedia.org>
2019-07-31 09:46:00 +0200

    profile::cache::kafka::alerts: move alarms to prometheus
    Bug: T229357
    Change-Id: Id02864056648403e473edec015f3ed7e9058b336

--- modules/profile/manifests/cache/kafka/alerts.pp
+++ modules/profile/manifests/cache/kafka/alerts.pp
@@ -1,76 +1,34 @@
-# == Class: profile::analytics::alerts
+# == Class: profile::cache::kafka::alerts
 #
-# Monitoring Analytics graphite aggregated alarms.
+# Monitoring Varnishkafka prometheus aggregate metrics.
 #
-class profile::cache::kafka::alerts($graphite_url = hiera('graphite_url')) {
+class profile::cache::kafka::alerts {
 
     # Generate an alert if too many delivery report errors per second
-    # (logster only reports once a minute, but we can use perSecond() in graphite
-    # over a period of time of X minutes).
+    #
     # Currently monitored:
     # varnishkafka-webrequest in text/upload
     # varnishkafka-eventlogging in text
-    # varnishkafka-statsd in text
-    #
-    # These alarms will be probably removed after https://phabricator.wikimedia.org/T196066
-    # in favor of Prometheus based ones.
-    Monitoring::Graphite_threshold {
-        graphite_url => $graphite_url
+    # varnishkafka-statsv in text
+
+    profile::cache::kafka::varnishkafka_delivery_alert { 'varnishkafka-alert-webrequest-text':
+        cache_segment => 'text',
+        instance      => 'webrequest',
     }
 
-    monitoring::graphite_threshold { 'varnishkafka-webrequest-text-kafka_drerr':
-        ensure          => 'present',
-        description     => 'cache_text: Varnishkafka Webrequest Delivery Errors per second',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/varnishkafka?panelId=20&fullscreen&orgId=1&var-instance=webrequest&var-host=All'],
-        metric          => 'sumSeries(perSecond(varnishkafka.*.webrequest.text.varnishkafka.kafka_drerr))',
-        warning         => 1,
-        critical        => 5,
-        from            => '10min',
-        retry_interval  => 1,
-        retries         => 3,
-        contact_group   => 'admins,analytics',
-        notes_link      => 'https://wikitech.wikimedia.org/wiki/Analytics/Systems/Varnishkafka',
+    profile::cache::kafka::varnishkafka_delivery_alert { 'varnishkafka-alert-eventlogging-text':
+        cache_segment => 'text',
+        instance      => 'eventlogging',
     }
 
-    monitoring::graphite_threshold { 'varnishkafka-eventlogging-text-kafka_drerr':
-        ensure          => 'present',
-        description     => 'Varnishkafka Eventlogging Delivery Errors per second',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/varnishkafka?panelId=20&fullscreen&orgId=1&var-instance=eventlogging&var-host=All'],
-        metric          => 'sumSeries(perSecond(varnishkafka.*.eventlogging.text.varnishkafka.kafka_drerr))',
-        warning         => 1,
-        critical        => 5,
-        from            => '10min',
-        retry_interval  => 1,
-        retries         => 3,
-        contact_group   => 'admins,analytics',
-        notes_link      => 'https://wikitech.wikimedia.org/wiki/Analytics/Systems/Varnishkafka',
+    profile::cache::kafka::varnishkafka_delivery_alert { 'varnishkafka-alert-statsv-text':
+        cache_segment => 'text',
+        instance      => 'statsv',
     }
 
-    monitoring::graphite_threshold { 'varnishkafka-statsv-text-kafka_drerr':
-        ensure          => 'present',
-        description     => 'Varnishkafka Statsv Delivery Errors per second',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/varnishkafka?panelId=20&fullscreen&orgId=1&var-instance=statsv&var-host=All'],
-        metric          => 'sumSeries(perSecond(varnishkafka.*.statsv.text.varnishkafka.kafka_drerr))',
-        warning         => 1,
-        critical        => 5,
-        from            => '10min',
-        retry_interval  => 1,
-        retries         => 3,
-        contact_group   => 'admins,analytics',
-        notes_link      => 'https://wikitech.wikimedia.org/wiki/Analytics/Systems/Varnishkafka',
-    }
-
-    monitoring::graphite_threshold { 'varnishkafka-webrequest-upload-kafka_drerr':
-        ensure          => 'present',
-        description     => 'cache_upload: Varnishkafka Webrequest Delivery Errors per second',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/varnishkafka?panelId=20&fullscreen&orgId=1&var-instance=webrequest&var-host=All'],
-        metric          => 'sumSeries(perSecond(varnishkafka.*.webrequest.upload.varnishkafka.kafka_drerr))',
-        warning         => 1,
-        critical        => 5,
-        from            => '10min',
-        retry_interval  => 1,
-        retries         => 3,
-        contact_group   => 'admins,analytics',
-        notes_link      => 'https://wikitech.wikimedia.org/wiki/Analytics/Systems/Varnishkafka',
+    # Varnishkafka - Cache upload
+    profile::cache::kafka::varnishkafka_delivery_alert { 'varnishkafka-alert-webrequest-upload':
+        cache_segment => 'upload',
+        instance      => 'webrequest',
     }
 }
