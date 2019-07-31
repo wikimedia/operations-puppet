666748d55ed06f2d6e6967936710330e730fe1de
Arzhel Younsi <ayounsi@wikimedia.org>
2019-07-31 17:09:20 -0600

    check_anycast_healthchecker, add sudo bird rights
    This check needs to run as user bird
    
    Change-Id: I45066bd0aefab05d2df3847e02198667c56202d8

--- modules/profile/manifests/bird/anycast_healthchecker_monitoring.pp
+++ modules/profile/manifests/bird/anycast_healthchecker_monitoring.pp
@@ -2,17 +2,24 @@
 # Original file from https://github.com/unixsurfer/anycast_healthchecker/blob/master/contrib/nagios/check_anycast_healthchecker.py
 class profile::bird::anycast_healthchecker_monitoring{
 
-  file { 'check_anycast_healthchecker':
-      path   => '/usr/lib/nagios/plugins/check_anycast_healthchecker.py',
-      mode   => '0555',
-      owner  => 'bird',
-      group  => 'bird',
-      source => 'puppet:///modules/profile/bird/check_anycast_healthchecker.py',
-  }
+    file { 'check_anycast_healthchecker':
+        path   => '/usr/lib/nagios/plugins/check_anycast_healthchecker.py',
+        mode   => '0555',
+        owner  => 'bird',
+        group  => 'bird',
+        source => 'puppet:///modules/profile/bird/check_anycast_healthchecker.py',
+    }
+
+    sudo::user { 'nagios_check_anycast_healthchecker':
+        user       => 'nagios',
+        privileges => [ 'ALL = (bird) NOPASSWD: /usr/lib/nagios/plugins/check_anycast_healthchecker.py' ],
+        require    => File['check_anycast_healthchecker'],
+    }
+
     nrpe::monitor_service { 'anycast_healthchecker':
         description  => 'Check if anycast-healthchecker and all configured threads are running',
-        nrpe_command => 'sudo -u bird /usr/bin/python3 /usr/lib/nagios/plugins/check_anycast_healthchecker.py',
-        require      => File['check_anycast_healthchecker'],
-        notes_url    => 'https://wikitech.wikimedia.org/wiki/Anycast_recursive_DNS#Anycast_healthchecker_not_running',
+        nrpe_command => '/usr/bin/sudo -u bird /usr/lib/nagios/plugins/check_anycast_healthchecker.py',
+        require      => Sudo::User['nagios_check_anycast_healthchecker'],
+        notes_url    => 'https://wikitech.wikimedia.org/wiki/Anycast#Anycast_healthchecker_not_running',
     }
 }
