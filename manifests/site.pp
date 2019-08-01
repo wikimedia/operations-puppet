f7c4b1c0068fb93e4427391deb703065a20a7911
Brandon Black <bblack@wikimedia.org>
2019-08-01 19:25:23 +0000

    cloudelastic: add mapped ipv6
    Change-Id: I234f63d0dfb6119704fa0fa75856c6d111cf2b27

--- manifests/site.pp
+++ manifests/site.pp
@@ -1322,6 +1322,7 @@ node /^cloudcontrol100[3-4].wikimedia.org$/ {
 # New systems to be placed into service by cloud team via T194186
 node /^cloudelastic100[1-4].wikimedia.org$/ {
     role(elasticsearch::cloudelastic)
+    interface::add_ip6_mapped { 'main': } # lint:ignore:wmf_styleguide
 }
 
 node /^cloudnet100[3-4].eqiad.wmnet$/ {
