41d0333e7d794c52eff3b3d6534521f64c6ee790
Brandon Black <bblack@wikimedia.org>
2019-08-01 18:06:02 +0000

    Attempt to fix cloudelastic LVS IPs
    The puppetization for this stuff is in a pretty bad state, I have
    no idea if this is actually a fix or improvement, yet!
    
    (Round 2!)
    
    Change-Id: I92f63b76e9f5c2181c5d1d167b358e0f03d2057a

--- modules/role/manifests/elasticsearch/cloudelastic.pp
+++ modules/role/manifests/elasticsearch/cloudelastic.pp
@@ -5,9 +5,16 @@
 class role::elasticsearch::cloudelastic {
     include ::profile::standard
     include ::profile::base::firewall
-    include ::role::lvs::realserver
     include ::profile::elasticsearch::cirrus
     include ::profile::elasticsearch::monitor::base_checks
+
+    # This clearly isn't the intended "right" way to use the latest factorings
+    # of LVS realserver config, but I can't figure out how to make them work
+    # for this case, either :P
+    include ::lvs::configuration # lint:ignore:wmf_styleguide
+    class { '::lvs::realserver':
+        realserver_ips => $lvs::configuration::service_ips['cloudelastic'][$::site],
+    }
 
     # To be enabled after elasticsearch is setup as kafka topic has not been created
     #include ::profile::mjolnir::kafka_bulk_daemon
