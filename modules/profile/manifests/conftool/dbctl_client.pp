d1c1cee96bd97841096cfaef78bcaae2ef2583ee
Chris Danis <cdanis@wikimedia.org>
2019-08-01 18:50:09 -0400

    Revert "dbctl: diff PHP vs dbctl configs"
    This reverts commit 2c8128ece79904f9b175d590d13db91251e02631.
    
    This check was just needed during the duration of the dbctl phased rollout,
    when both configuration methods were in use.
    
    Bug: T229070
    Change-Id: Ie407f61f6cb09deb9311d0d5cb4b18e0aca5eacf

--- modules/profile/manifests/conftool/dbctl_client.pp
+++ modules/profile/manifests/conftool/dbctl_client.pp
@@ -24,27 +24,4 @@ class profile::conftool::dbctl_client() {
         retries        => 3,
         notes_url      => 'https://wikitech.wikimedia.org/wiki/Dbctl#Uncommitted_dbctl_diffs'
     }
-
-    # Also set up a temporary check for diffs between db-{codfw,eqiad}.php and dbctl.
-    $prod_vs_dbctl_filename = '/usr/local/lib/nagios/plugins/check_dbctl_deltas_from_mwconfig'
-
-    file { $prod_vs_dbctl_filename:
-        ensure => present,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
-        source => 'puppet:///modules/profile/conftool/check_dbctl_deltas_from_mwconfig.py'
-    }
-
-    nrpe::monitor_service { 'dbctl_deltas_from_mwconfig':
-        ensure         => present,
-        description    => "dbctl differs from mediawiki-config in ${::site}, did you forget to update both?",
-        nrpe_command   => "${prod_vs_dbctl_filename} -d ${::site}",
-        critical       => false,
-        check_interval => 5,
-        retry_interval => 5,
-        retries        => 3,
-        notes_url      => 'https://wikitech.wikimedia.org/wiki/Dbctl#Configuration_deltas_vs_PHP'
-    }
-
 }
