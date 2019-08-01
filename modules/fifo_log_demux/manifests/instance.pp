0fcfed7582dcedd80c9df15d283ad754c1aefa99
Valentin Gutierrez <vgutierrez@wikimedia.org>
2019-08-01 18:06:52 +0700

    fifo_log_demux: Remove socket activation
    To be merged after I458199b8ce9f12f7c4676a7187eebef001b71aca
    
    Change-Id: Ibb0f1e9a084e81d0da7cb70dc8faf6c14eba175f

--- modules/fifo_log_demux/manifests/instance.pp
+++ modules/fifo_log_demux/manifests/instance.pp
@@ -2,7 +2,6 @@ define fifo_log_demux::instance(
     String $user = 'root',
     Stdlib::Absolutepath $fifo = '/var/run/fifo.pipe',
     Stdlib::Absolutepath $socket = '/var/run/log.socket',
-    Boolean $socket_activation = false,
     Optional[String] $wanted_by = undef,
     Optional[String] $required_by = undef,
     Boolean $create_fifo = false,
@@ -31,12 +30,5 @@ define fifo_log_demux::instance(
         ensure  => present,
         restart => true,
         content => systemd_template('fifo-log-demux@'),
-    }
-
-    if $socket_activation {
-        systemd::unit { "fifo-log-demux@${title}.socket":
-            ensure  => present,
-            content => systemd_template('fifo-log-demux@.socket'),
-        }
     }
 }
