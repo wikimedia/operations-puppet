c22b77d1a2bb00eef081d276e9e1161d0466b457
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2019-08-06 10:26:27 +0200

    toil: rsyslog_tls_remedy log to journald
    Bug: T199406
    Change-Id: I15e562c8d2ed6d7afff8dfaba55b19c56a620e46

--- modules/toil/manifests/rsyslog_tls_remedy.pp
+++ modules/toil/manifests/rsyslog_tls_remedy.pp
@@ -6,11 +6,13 @@ class toil::rsyslog_tls_remedy (
 ) {
 
   systemd::timer::job { 'rsyslog-tls-remedy':
-    ensure      => $ensure,
-    user        => 'root',
-    description => 'Restart rsyslog when TLS listener is not responding T199406',
-    command     => '/bin/sh -c "timeout 10s openssl s_client -connect localhost:6514 -quiet </dev/null || systemctl restart rsyslog"',
-    interval    => {
+    ensure          => $ensure,
+    # Don't log to file, use journald
+    logging_enabled => false,
+    user            => 'root',
+    description     => 'Restart rsyslog when TLS listener is not responding T199406',
+    command         => '/bin/sh -c "timeout 10s openssl s_client -connect localhost:6514 -quiet </dev/null || systemctl restart rsyslog"',
+    interval        => {
       'start'    => 'OnCalendar',
       'interval' => '*-*-* *:00/30:00', # every 30 min
     },
