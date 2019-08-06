8ebe5d0a98a697c9da196e0d3d87e3ea446be2d7
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2019-08-06 12:27:59 +0200

    swift: 'nobarrier' for xfs has been removed in linux 4.19
    Bug: T229911
    Change-Id: I451d3396f3ba94d094850a213d2ebe624ae32a4b

--- modules/swift/manifests/mount_filesystem.pp
+++ modules/swift/manifests/mount_filesystem.pp
@@ -13,7 +13,13 @@ define swift::mount_filesystem (
         mode   => '0750',
     }
 
-    $mount_options = 'nofail,noatime,nodiratime,nobarrier,logbufs=8'
+    # nobarrier for xfs has been removed in buster / linux 4.19
+    # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=1c02d502c20809a2a5f71ec16a930a61ed779b81
+    if os_version('debian <= stretch') {
+      $mount_options = 'nofail,noatime,nodiratime,nobarrier,logbufs=8'
+    } else {
+      $mount_options = 'nofail,noatime,nodiratime,logbufs=8'
+    }
 
     mount { $mount_point:
         # XXX this won't mount the disks the first time they are added to
