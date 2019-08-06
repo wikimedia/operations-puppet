ae88ef51d8d2abfcdccd847821fad159923d6ce8
Chris Danis <cdanis@wikimedia.org>
2019-08-06 13:21:01 -0400

    noc: fetch dbconfig from etcd to local disk
    * Script and associated systemd timer to fetch dbconfig
      data from etcd and store it locally.
    * Expose those files via noc.wikimedia.org for a replacement
      of db-{eqiad,codfw}.php as served from there.
    
    Followup work: make db.php read from this JSON.
    
    Bug: T229631
    Change-Id: Id2c3d544c0cca7ab7e6bcbcfd625f84cface1e9d

---
+++ modules/noc/files/fetch_dbconfig.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# For each datacenter, fetches dbconfig JSON from etcd and places it under
+# e.g. /srv/dbconfig/eqiad.json.
+#
+# Runs via a systemd timer.
+
+set -eu
+set -o pipefail
+
+OUTPUT_PATH=/srv/dbconfig
+
+# Given a service name (e.g. etcd) and optional protocol (e.g. tcp),
+# resolves a DNS SRV record into a host:port.
+function resolve-srv () {
+    SERVICE=$1
+    [ -z "$1" ] && return 1
+    PROTO=${2:-tcp}
+    dig _${SERVICE}._${PROTO} srv +short +ndots=2 +search | grep -ve '^$' | shuf -n1 | awk '{print $4 ":" $3}'
+}
+
+ETCD=$(resolve-srv etcd)
+
+for DC in eqiad codfw; do
+    ETCD_KEY="conftool/v1/mediawiki-config/${DC}/dbconfig"
+    # Extract and unescape the value from the response of etcd's JSON API.
+    # That yields a dbconfig JSON dict with val: <data we want>; extract that
+    # and write it out.
+    curl -s -m15 "https://${ETCD}/v2/keys/${ETCD_KEY}" | jq -er .node.value | jq -er .val > ${OUTPUT_PATH}/.${DC}.json.tmp
+    [ $? -eq 0 ] && mv $OUTPUT_PATH/.${DC}.json.tmp $OUTPUT_PATH/${DC}.json
+done
--- modules/noc/manifests/init.pp
+++ modules/noc/manifests/init.pp
@@ -17,6 +17,40 @@ class noc {
         content => template('noc/noc.wikimedia.org.erb'),
     }
 
+    $fetch_dbconfig_user = 'mwdeploy'
+    file { '/srv/dbconfig':
+        ensure => directory,
+        owner  => $fetch_dbconfig_user,
+        group  => $fetch_dbconfig_user,
+        mode   => '0755',
+    }
+    file { '/srv/dbconfig/README':
+        ensure  => present,
+        content => join(
+            [
+                'Database configs mirrored from etcd.',
+                'This directory is publicly viewable on the web.'],
+            '\n'),
+        require => File['/srv/dbconfig'],
+    }
+
+    $fetch_dbconfig_path = '/usr/local/sbin/fetch_dbconfig'
+
+    file { $fetch_dbconfig_path:
+        source => 'puppet:///modules/noc/fetch_dbconfig.sh',
+    }
+
+    systemd::timer::job { 'fetch_dbconfig':
+        description => 'Fetch the dbconfig from etcd and store it locally',
+        command     => $fetch_dbconfig_path,
+        interval    => {
+            'start'    => 'OnUnitInactiveSec',
+            'interval' => '60s',
+        },
+        user        => $fetch_dbconfig_user,
+        require     => [File[$fetch_dbconfig_path], File['/srv/dbconfig']],
+    }
+
     # Monitoring
     monitoring::service { 'http-noc':
         description   => 'HTTP-noc',
