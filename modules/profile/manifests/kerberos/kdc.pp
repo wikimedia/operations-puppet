9e668c89ac8a561c353d2794bd757cf42507b0f8
Luca Toscano <ltoscano@wikimedia.org>
2019-08-07 12:51:12 +0200

    profile::kerberos::kdc: add daily backup for the KDC database
    Bug: T226089
    Change-Id: If384ae1900db212f525f1422ac3aefccd4a31775

--- modules/profile/manifests/kerberos/kdc.pp
+++ modules/profile/manifests/kerberos/kdc.pp
@@ -1,6 +1,7 @@
 class profile::kerberos::kdc (
     Stdlib::Fqdn $krb_realm_name = lookup('kerberos_realm_name'),
     Array[Stdlib::Fqdn] $krb_kdc_servers = lookup('kerberos_kdc_servers'),
+    Optional[Boolean] $monitoring_enabled = lookup('profile::kerberos::kdc::monitoring_enabled', { 'default_value' => false }),
 ) {
     package { 'krb5-kdc':
         ensure => present,
@@ -42,5 +43,51 @@ class profile::kerberos::kdc (
         proto  => 'udp',
         port   => '88',
         srange => '$DOMAIN_NETWORKS',
+    }
+
+    file { '/srv/backup':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0750',
+    }
+
+    file { '/usr/local/sbin/dump_kdc_database':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0550',
+        content => 'puppet:///modules/profile/kdc/dump_kdc_database',
+    }
+
+    systemd::timer::job { 'backup-kdc-database':
+        description        => 'Daily dump of the KDC database',
+        command            => '/usr/local/sbin/dump_kdc_database',
+        interval           => {
+            'start'    => 'OnCalendar',
+            'interval' => '*-*-* 00:00:00'
+        },
+        user               => 'root',
+        monitoring_enabled => $monitoring_enabled,
+        logging_enabled    => false,
+        require            => [
+            File['/usr/local/sbin/dump_kdc_database'],
+            File['/srv/backup'],
+        ],
+    }
+
+    systemd::timer::job { 'delete-old-backups-kdc-database':
+        description        => 'Daily clean up of old backups of the KDC database',
+        command            => 'find /srv/backup -name "kdc_database_.*" -mtime +30 -delete',
+        interval           => {
+            'start'    => 'OnCalendar',
+            'interval' => '*-*-* 01:00:00'
+        },
+        user               => 'root',
+        monitoring_enabled => false,
+        logging_enabled    => false,
+        require            => [
+            File['/srv/backup'],
+        ],
     }
 }
