b05be0da7bd0747ce907cf5f8e8a391c6f811f5b
John Bond <jbond@wikimedia.org>
2019-08-08 13:43:18 +0100

    puppetdb - filter_job_id: add some debug
    Change-Id: I5a0d43108344802326222153db6a14b091e6b7b8

--- modules/puppetmaster/files/filter_job_id.lua
+++ modules/puppetmaster/files/filter_job_id.lua
@@ -1,11 +1,13 @@
 if ngx.req.get_method() == "POST" then
   local args, err = ngx.req.get_uri_args()
-  if args['command'] == 'replace_catalog' then
+  if args['command'] == 'store_report' then
     ngx.req.read_body()
     body = ngx.req.get_body_data()
-    if string.find(body, '"job_id":null,') then
+    if body and string.find(body, '"job_id":null,') then
+      ngx.log(ngx.ERR, body)
       body = string.gsub(ngx.req.get_body_data(), '"job_id":null,','')
+      ngx.log(ngx.ERR, body)
       ngx.req.set_body_data(body)
-    end
-  end
+    end 
+  end 
 end
