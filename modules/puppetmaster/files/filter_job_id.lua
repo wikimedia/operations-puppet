cd3f28591145b6a64b162d353b36a3b05953ff47
John Bond <jbond@wikimedia.org>
2019-08-08 13:20:24 +0100

    puppetdb - filter_job_id: only munge data we need to
    Change-Id: I6a2659dc81b6c847f0972f4cf6c104ef511e3cbd

--- modules/puppetmaster/files/filter_job_id.lua
+++ modules/puppetmaster/files/filter_job_id.lua
@@ -1,6 +1,11 @@
 if ngx.req.get_method() == "POST" then
-  ngx.req.read_body()
-  local body = ngx.req.get_body_data()
-  body = string.gsub(body, '"job_id":null,','')
-  ngx.req.set_body_data(body)
+  local args, err = ngx.req.get_uri_args()
+  if args['command'] == 'replace_catalog' then
+    ngx.req.read_body()
+    body = ngx.req.get_body_data()
+    if string.find(body, '"job_id":null,') then
+      body = string.gsub(ngx.req.get_body_data(), '"job_id":null,','')
+      ngx.req.set_body_data(body)
+    end
+  end
 end
