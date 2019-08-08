eeedf115b76ff8d8bf113e9d7e470c6942ffad33
John Bond <jbond@wikimedia.org>
2019-08-08 14:20:09 +0100

    puppetmaster1003: add canary hosts back and remove debug logging
    Bug: T228657
    Change-Id: I1f3c0aef3533f8b5d1298eddaff4356301cb2058

--- modules/puppetmaster/files/filter_job_id.lua
+++ modules/puppetmaster/files/filter_job_id.lua
@@ -4,9 +4,7 @@ if ngx.req.get_method() == "POST" then
     ngx.req.read_body()
     body = ngx.req.get_body_data()
     if body and string.find(body, '"job_id":null,') then
-      ngx.log(ngx.ERR, body)
       body = string.gsub(ngx.req.get_body_data(), '"job_id":null,','')
-      ngx.log(ngx.ERR, body)
       ngx.req.set_body_data(body)
     end
   end
