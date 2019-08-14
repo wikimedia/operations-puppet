af5bbedf4b84097b0458e0116b47cd0e21f25914
John Bond <jbond@wikimedia.org>
2019-08-14 14:29:03 +0100

    idp: add crypto key support with spec tests
    Change-Id: Ib71b707c1fdb6ff694d4aa282ed6873d69a0fd62

--- modules/apereo_cas/spec/classes/apereo_cas_spec.rb
+++ modules/apereo_cas/spec/classes/apereo_cas_spec.rb
@@ -16,73 +16,115 @@ describe 'apereo_cas' do
     context "on #{os}" do
       let(:facts) { facts }
       let(:params) { { keystore_source: 'puppet:///modules/apereo_cas/thekeystore' } }
-      it { is_expected.to compile.with_all_deps }
-      ['/srv', '/srv/cas', '/srv/cas/overlay-template',
-       '/etc/cas', '/etc/cas/services', '/etc/cas/config'].each do |dir|
+      describe 'test with default settings' do
+        it { is_expected.to compile.with_all_deps }
+        ['/srv', '/srv/cas', '/srv/cas/overlay-template',
+         '/etc/cas', '/etc/cas/services', '/etc/cas/config'].each do |dir|
+          it do
+            is_expected.to contain_file(dir).with_ensure('directory')
+          end
+        end
         it do
-          is_expected.to contain_file(dir).with_ensure('directory')
+          is_expected.to contain_git__clone('operations/software/cas-overlay-template').with(
+            directory: '/srv/cas/overlay-template'
+          )
+          is_expected.to contain_file('/etc/cas/config/cas.properties').with(
+            owner: 'root',
+            group: 'root',
+            mode: '0400'
+          ).with_content(
+            %r{^cas.server.name=https://foo.example.com:8443$}
+          ).with_content(
+            %r{^cas.server.prefix=https://foo.example.com:8443/cas$}
+          ).with_content(
+            %r{^cas.serviceRegistry.json.location=file:/etc/cas/services$}
+          ).with_content(
+            /^cas.authn.ldap\[0\].principalAttributeList=cn,memberOf,mail$/
+          ).with_content(
+            /^cas.authn.ldap\[0\].type=AUTHENTICATED$/
+          ).with_content(
+            /^cas.authn.ldap\[0\].connectionStrategy=ACTIVE_PASSIVE$/
+          ).with_content(
+            /^cas.authn.ldap\[0\].ldapurl=$/
+          ).with_content(
+            /^cas.authn.ldap\[0\].useStartTLS=true$/
+          ).with_content(
+            /^cas.authn.ldap\[0\].basedn=dc=example,dc=org/
+          ).with_content(
+            /^cas.authn.ldap\[0\].searchFilter=cn={user}$/
+          ).with_content(
+            /^cas.authn.ldap\[0\].binddn=cn=user,dc=example,dc=org$/
+          ).with_content(
+            /^cas.authn.ldap\[0\].bindcredential=changeme$/
+          ).with_content(
+            /^cas.authn.accept.users=$/
+          ).with_content(
+            /^logging.level.org.apereo=WARN$/
+          ).without_content(
+            /cas.(tgc|webflow).crypto.(signing|encryption).key/
+          )
+          is_expected.to contain_file('/etc/cas/config/log4j2.xml').with(
+            owner: 'root',
+            group: 'root',
+            mode: '0400'
+          )
+          is_expected.to contain_file('/etc/cas/thekeystore').with(
+            owner: 'root',
+            group: 'root',
+            mode: '0400'
+          )
+          is_expected.to contain_exec('build cas war').with(
+            command: '/srv/cas/overlay-template/build.sh package',
+            creates: '/srv/cas/overlay-template/build/libs/cas.war',
+            cwd: '/srv/cas/overlay-template'
+          )
+          is_expected.to contain_exec('update cas war').with(
+            command: '/srv/cas/overlay-template/build.sh update',
+            cwd: '/srv/cas/overlay-template',
+            refreshonly: true
+          )
+          is_expected.to contain_systemd__service('cas').with_content(
+            %r{ExecStart=/usr/bin/java -jar /srv/cas/overlay-template/build/libs/cas.war}
+          )
         end
       end
-      it do
-        is_expected.to contain_git__clone('operations/software/cas-overlay-template').with(
-          directory: '/srv/cas/overlay-template'
-        )
-        is_expected.to contain_file('/etc/cas/config/cas.properties').with(
-          owner: 'root',
-          group: 'root',
-          mode: '0400'
-        ).with_content(
-          %r{^cas.server.name=https://foo.example.com:8443$}
-        ).with_content(
-          %r{^cas.server.prefix=https://foo.example.com:8443/cas$}
-        ).with_content(
-          %r{^cas.serviceRegistry.json.location=file:/etc/cas/services$}
-        ).with_content(
-          /^cas.authn.ldap\[0\].principalAttributeList=cn,memberOf,mail$/
-        ).with_content(
-          /^cas.authn.ldap\[0\].type=AUTHENTICATED$/
-        ).with_content(
-          /^cas.authn.ldap\[0\].connectionStrategy=ACTIVE_PASSIVE$/
-        ).with_content(
-          /^cas.authn.ldap\[0\].ldapurl=$/
-        ).with_content(
-          /^cas.authn.ldap\[0\].useStartTLS=true$/
-        ).with_content(
-          /^cas.authn.ldap\[0\].basedn=dc=example,dc=org/
-        ).with_content(
-          /^cas.authn.ldap\[0\].searchFilter=cn={user}$/
-        ).with_content(
-          /^cas.authn.ldap\[0\].binddn=cn=user,dc=example,dc=org$/
-        ).with_content(
-          /^cas.authn.ldap\[0\].bindcredential=changeme$/
-        ).with_content(
-          /^cas.authn.accept.users=$/
-        ).with_content(
-          /^logging.level.org.apereo=WARN$/
-        )
-        is_expected.to contain_file('/etc/cas/config/log4j2.xml').with(
-          owner: 'root',
-          group: 'root',
-          mode: '0400'
-        )
-        is_expected.to contain_file('/etc/cas/thekeystore').with(
-          owner: 'root',
-          group: 'root',
-          mode: '0400'
-        )
-        is_expected.to contain_exec('build cas war').with(
-          command: '/srv/cas/overlay-template/build.sh package',
-          creates: '/srv/cas/overlay-template/build/libs/cas.war',
-          cwd: '/srv/cas/overlay-template'
-        )
-        is_expected.to contain_exec('update cas war').with(
-          command: '/srv/cas/overlay-template/build.sh update',
-          cwd: '/srv/cas/overlay-template',
-          refreshonly: true
-        )
-        is_expected.to contain_systemd__service('cas').with_content(
-          %r{ExecStart=/usr/bin/java -jar /srv/cas/overlay-template/build/libs/cas.war}
-        )
+      describe 'Change Defaults' do
+        context 'tgc_signing_key' do
+          before(:each) { params.merge!(tgc_signing_key: 'foobar') }
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_file('/etc/cas/config/cas.properties').with_content(
+              /cas.tgc.crypto.signing.key=foobar/
+            )
+          end
+        end
+        context 'tgc_encryption_key' do
+          before(:each) { params.merge!(tgc_encryption_key: 'foobar') }
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_file('/etc/cas/config/cas.properties').with_content(
+              /cas.tgc.crypto.encryption.key=foobar/
+            )
+          end
+        end
+        context 'webflow_signing_key' do
+          before(:each) { params.merge!(webflow_signing_key: 'foobar') }
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_file('/etc/cas/config/cas.properties').with_content(
+              /cas.webflow.crypto.signing.key=foobar/
+            )
+          end
+        end
+        context 'webflow_encryption_key' do
+          before(:each) { params.merge!(webflow_encryption_key: 'foobar') }
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_file('/etc/cas/config/cas.properties').with_content(
+              /cas.webflow.crypto.encryption.key=foobar/
+            )
+          end
+        end
       end
     end
   end
--- modules/profile/manifests/idp.pp
+++ modules/profile/manifests/idp.pp
@@ -1,23 +1,31 @@
 class profile::idp(
-    Hash   $ldap_config       = lookup('ldap', Hash, hash, {}),
-    String $keystore_password = lookup('profile::idp::keystore_password'),
-    String $key_password      = lookup('profile::idp::key_password'),
+    Hash   $ldap_config            = lookup('ldap', Hash, hash, {}),
+    String $keystore_password      = lookup('profile::idp::keystore_password'),
+    String $key_password           = lookup('profile::idp::key_password'),
+    String $tgc_signing_key        = lookup('profile::idp::tgc_signing_key'),
+    String $tgc_encryption_key     = lookup('profile::idp::tgc_encryption_key'),
+    String $webflow_signing_key    = lookup('profile::idp::webflow_signing_key'),
+    String $webflow_encryption_key = lookup('profile::idp::webflow_encryption_key'),
 ){
 
     include passwords::ldap::production
 
     class { 'apereo_cas':
-        server_name       => 'https://idp.wikimedia.org:8443',
-        server_prefix     => 'https://idp.wikimedia.org:8443/cas',
-        keystore_content  => secret('casserver/thekeystore'),
-        keystore_password => $keystore_password,
-        key_password      => $key_password,
-        ldap_start_tls    => false,
-        ldap_uris         => ["ldaps://${ldap_config[ro-server]}:636",
-                              "ldaps://${ldap_config[ro-server-fallback]}:636",],
-        ldap_base_dn      => $ldap_config['base-dn'],
-        log_level         => 'DEBUG',
-        ldap_bind_pass    => $passwords::ldap::production::proxypass,
-        ldap_bind_dn      => "cn=proxyagent,ou=profile,${ldap_config['base-dn']}",
+        server_name            => 'https://idp.wikimedia.org:8443',
+        server_prefix          => 'https://idp.wikimedia.org:8443/cas',
+        keystore_content       => secret('casserver/thekeystore'),
+        keystore_password      => $keystore_password,
+        key_password           => $key_password,
+        tgc_signing_key        => $tgc_signing_key,
+        tgc_encryption_key     => $tgc_encryption_key,
+        webflow_signing_key    => $webflow_signing_key,
+        webflow_encryption_key => $webflow_encryption_key,
+        ldap_start_tls         => false,
+        ldap_uris              => ["ldaps://${ldap_config[ro-server]}:636",
+                                    "ldaps://${ldap_config[ro-server-fallback]}:636",],
+        ldap_base_dn           => $ldap_config['base-dn'],
+        log_level              => 'DEBUG',
+        ldap_bind_pass         => $passwords::ldap::production::proxypass,
+        ldap_bind_dn           => "cn=proxyagent,ou=profile,${ldap_config['base-dn']}",
     }
 }
