d4aaaa91be00d8ed5ec161287ad0947f85de013c
Valentin Gutierrez <vgutierrez@wikimedia.org>
2019-08-19 21:26:52 +0800

    ATS: Fix traffic_server --run-root parameter value in check_procs check
    Even if we start traffic_manager with --run-root=${paths['prefix']},
    traffic_server gets started with
    --run-root=${paths['prefix']}/runroot.yaml
    
    Bug: T221594
    Change-Id: I5fd11c6084cc0940857374e08c4fe14815ffa395

--- modules/profile/manifests/trafficserver/monitoring.pp
+++ modules/profile/manifests/trafficserver/monitoring.pp
@@ -21,8 +21,8 @@ define profile::trafficserver::monitoring(
         $traffic_server_nrpe_command = "/usr/lib/nagios/plugins/check_procs -c 1:1 -a '/usr/bin/traffic_server -M --httpport ${port}'"
         $check_trafficserver_config_status_args = $paths['records']
     } else {
-        $traffic_manager_nrpe_command = "/usr/lib/nagios/plugins/check_procs -c 1:1 -a '/usr/bin/traffic_manager --run-root=/srv/trafficserver/${instance_name} --nosyslog'"
-        $traffic_server_nrpe_command = "/usr/lib/nagios/plugins/check_procs -c 1:1 -a '/usr/bin/traffic_server --run-root=/srv/trafficserver/${instance_name} -M --httpport ${port}'"
+        $traffic_manager_nrpe_command = "/usr/lib/nagios/plugins/check_procs -c 1:1 -a '/usr/bin/traffic_manager --run-root=${paths['prefix']} --nosyslog'"
+        $traffic_server_nrpe_command = "/usr/lib/nagios/plugins/check_procs -c 1:1 -a '/usr/bin/traffic_server --run-root=${paths['prefix']}/runroot.yaml -M --httpport ${port}'"
         $check_trafficserver_config_status_args = "${paths['records']} ${paths['prefix']}"
     }
 
