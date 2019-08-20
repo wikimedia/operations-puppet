b24ccfb6e64e43c63571425837e5f7a15ea13593
John Bond <jbond@wikimedia.org>
2019-08-20 11:54:29 +0100

    IPv6: add static IPv6 addresses to spare hosts
    This change also disables the style wmf_style checks for the spare role.
    I personally think this is ok as per the comments in the change but happy
    to abandon
    
    Change-Id: I2ca29abffa941fbdcb9e1aab4ea5d2fe5f792621

--- modules/role/manifests/spare/system.pp
+++ modules/role/manifests/spare/system.pp
@@ -1,3 +1,4 @@
+# lint:ignore:wmf_styleguide
 # This is a role for systems which are in the process of being reclaimed
 # or decommissioned. IOW. the host once had a feature role assigned, but
 # currently no has it. If such hosts are simply reset to "include
@@ -11,6 +12,12 @@
 class role::spare::system {
     include ::profile::standard
     include ::profile::base::firewall
+    # This is a style validation however it makes senses in this instance as
+    # a) people will likely forget to add this parameter when moving hosts to
+    # the spare role
+    # b) We will hopefully move this to profile::standard shortly
+    interface::add_ip6_mapped { 'main': }
 
     system::role { 'spare::system': description => 'Unused spare system' }
 }
+# lint:endignore
