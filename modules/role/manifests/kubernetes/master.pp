420b42ce6b5372fdbf88ba14225f8c41aa05c4a2
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-08-27 08:44:46 +0200

    k8s::master: switch to profile::lvs::realserver
    Change-Id: I15b9da9d48f75f5bdcfc57b1e5aa4d594e448f76

--- modules/role/manifests/kubernetes/master.pp
+++ modules/role/manifests/kubernetes/master.pp
@@ -5,7 +5,6 @@ class role::kubernetes::master {
     # Sets up docker on the machine
     include ::profile::kubernetes::master
     if hiera('has_lvs', true) {
-        # TODO: This needs to become a profile
-        include role::lvs::realserver
+        include profile::lvs::realserver
     }
 }
