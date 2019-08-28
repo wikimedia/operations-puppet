ebf183d6b158d0c22eb0d19ae950b382df43ea09
Daniel Zahn <dzahn@wikimedia.org>
2019-08-28 12:52:18 +0200

    remove wikiba.se microsite puppetization
    wikiba.se now stays with WMDE and is using https there since
    T155359. Remove the puppetization on the WMF side that is now
    not needed anymore.
    
    Bug: T99531
    Change-Id: I7ba075d6bfe31cab2a9d1591bd751658d4bdaa97

--- modules/role/manifests/webserver_misc_static.pp
+++ modules/role/manifests/webserver_misc_static.pp
@@ -14,7 +14,6 @@ class role::webserver_misc_static {
     include ::profile::microsites::static_bugzilla # https://static-bugzilla.wikimedia.org
     include ::profile::microsites::static_rt       # https://static-rt.wikimedia.org
     include ::profile::microsites::transparency    # https://transparency.wikimedia.org
-    include ::profile::microsites::wikibase        # https://wikiba.se
     include ::profile::microsites::research        # https://research.wikimedia.org (T183916)
     include ::profile::microsites::design          # https://design.wikimedia.org (T185282)
     include ::profile::microsites::sitemaps        # https://sitemaps.wikimedia.org
