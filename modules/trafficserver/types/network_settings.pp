7985a190d5396c211656845a584109c34f98e252
Valentin Gutierrez <vgutierrez@wikimedia.org>
2019-08-28 13:53:44 +0800

    ATS: Fix indentation on Trafficserver::Network_settings
    Change-Id: I0b7b4eeab39895e0a8774bcafa3f2eabd3ed3f2c

--- modules/trafficserver/types/network_settings.pp
+++ modules/trafficserver/types/network_settings.pp
@@ -2,20 +2,20 @@
 # https://docs.trafficserver.apache.org/en/8.0.x/admin-guide/files/records.config.en.html#network
 #
 # [*connections_throttle*]
-# The total number of client and origin server connections that the server can handle simultaneously.
-# This is in fact the max number of file descriptors that the traffic_server process can have open at any given time.
-# Roughly 10% of these connections are reserved for origin server connections.
-# If this is set to 0, the throttling logic is disabled.
+#   The total number of client and origin server connections that the server can handle simultaneously.
+#   This is in fact the max number of file descriptors that the traffic_server process can have open at any given time.
+#   Roughly 10% of these connections are reserved for origin server connections.
+#   If this is set to 0, the throttling logic is disabled.
 #
 # [*sock_option_flag_in*]
-# Turns different options “on” for the socket handling client connections:
-#   Socket options for incoming connections. The provided value is a bitmask:
-#     TCP_NODELAY  = 0x1
-#     SO_KEEPALIVE = 0x2
-#     SO_LINGER    = 0x4
-#     TCP_FASTOPEN = 0x8
+#   Turns different options "on" for the socket handling client connections:
+#       Socket options for incoming connections. The provided value is a bitmask:
+#           TCP_NODELAY  = 0x1
+#           SO_KEEPALIVE = 0x2
+#           SO_LINGER    = 0x4
+#           TCP_FASTOPEN = 0x8
 #
 type Trafficserver::Network_settings = Struct[{
-  'connections_throttle' => Integer[0],
-  'sock_option_flag_in'  => Integer[0, 0xF],
+    'connections_throttle' => Integer[0],
+    'sock_option_flag_in'  => Integer[0, 0xF],
 }]
