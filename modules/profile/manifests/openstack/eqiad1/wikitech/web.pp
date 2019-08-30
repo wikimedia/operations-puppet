404a23b6b433961f9a463a67849be0fa66bd7ce9
Timo Tijhof <krinklemail@gmail.com>
2019-08-30 19:35:01 +0100

    mediawiki: Remove loadExitNodes.php maintenance script
    Follows-up 6e58361da3d594fd6.
    
    Bug: T229736
    Change-Id: I31f7d68dd1207c461161f68f9275df8edc8ca076

--- modules/profile/manifests/openstack/eqiad1/wikitech/web.pp
+++ modules/profile/manifests/openstack/eqiad1/wikitech/web.pp
@@ -15,11 +15,4 @@ class profile::openstack::eqiad1::wikitech::web(
     }
 
     class {'::openstack::wikitech::wikitech_static_sync': }
-
-    cron { 'tor_exit_node_update':
-        ensure  => absent,
-        command => '/usr/local/bin/mwscript extensions/TorBlock/maintenance/loadExitNodes.php --wiki=labswiki --force > /dev/null',
-        user    => 'www-data',
-        minute  => '*/20',
-    }
 }
