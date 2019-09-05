8deff9d04d11e84549864c7e28a85822fa24c7d6
Daniel Zahn <dzahn@wikimedia.org>
2019-09-05 12:11:00 +0200

    webperf: add envoy for TLS termination
    Bug: T210411
    Change-Id: I1e55a14e7248400aa0642ba2f41cd5ec95daaad7

--- modules/role/manifests/webperf/processors_and_site.pp
+++ modules/role/manifests/webperf/processors_and_site.pp
@@ -6,6 +6,7 @@ class role::webperf::processors_and_site {
     include ::profile::webperf::processors
     include ::profile::webperf::coal_web
     include ::profile::webperf::site
+    include ::profile::tlsproxy::envoy # TLS termination
 
     system::role { 'webperf::processors_and_site':
         description => 'performance team data processor and performance.wikimedia.org server'
