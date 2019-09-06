1eccf977bf4ff8161ff74cead3dbb55bfb9f3258
Jason Hedden <jhedden@wikimedia.org>
2019-09-06 10:37:40 -0500

    openstack: add haproxy health check path support
    The glance services use a different health check path `/healthcheck` than the
    other services '/'. This commit adds support to the haproxy defined resource and
    template to define different healthcheck paths per service.
    
    Bug: T223907
    Change-Id: I490ab67e3c969c20bd57c0eaf483e8bf50feb7ac

--- modules/profile/manifests/openstack/codfw1dev/haproxy.pp
+++ modules/profile/manifests/openstack/codfw1dev/haproxy.pp
@@ -8,32 +8,37 @@ class profile::openstack::codfw1dev::haproxy(
     Stdlib::Port $nova_osapi_compute_listen_port = lookup('profile::openstack::codfw1dev::nova::osapi_compute_listen_port'),
 ) {
     profile::openstack::base::haproxy::site { 'glance_api':
-        servers       => [$nova_controller, $nova_controller_standby],
-        port_frontend => 9292,
-        port_backend  => $glance_api_bind_port,
+        servers          => [$nova_controller, $nova_controller_standby],
+        healthcheck_path => '/healthcheck',
+        port_frontend    => 9292,
+        port_backend     => $glance_api_bind_port,
     }
 
     profile::openstack::base::haproxy::site { 'glance_registry':
-        servers       => [$nova_controller, $nova_controller_standby],
-        port_frontend => 9191,
-        port_backend  => $glance_registry_bind_port,
+        servers          => [$nova_controller, $nova_controller_standby],
+        healthcheck_path => '/healthcheck',
+        port_frontend    => 9191,
+        port_backend     => $glance_registry_bind_port,
     }
 
     profile::openstack::base::haproxy::site { 'neutron':
-        servers       => [$nova_controller, $nova_controller_standby],
-        port_frontend => 9696,
-        port_backend  => $neutron_bind_port,
+        servers          => [$nova_controller, $nova_controller_standby],
+        healthcheck_path => '/',
+        port_frontend    => 9696,
+        port_backend     => $neutron_bind_port,
     }
 
     profile::openstack::base::haproxy::site { 'nova_api':
-        servers       => [$nova_controller, $nova_controller_standby],
-        port_frontend => 8774,
-        port_backend  => $nova_osapi_compute_listen_port,
+        servers          => [$nova_controller, $nova_controller_standby],
+        healthcheck_path => '/',
+        port_frontend    => 8774,
+        port_backend     => $nova_osapi_compute_listen_port,
     }
 
     profile::openstack::base::haproxy::site { 'nova_metadata':
-        servers       => [$nova_controller, $nova_controller_standby],
-        port_frontend => 8775,
-        port_backend  => $nova_metadata_listen_port,
+        servers          => [$nova_controller, $nova_controller_standby],
+        healthcheck_path => '/',
+        port_frontend    => 8775,
+        port_backend     => $nova_metadata_listen_port,
     }
 }
