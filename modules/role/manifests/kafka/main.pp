3f711a5c4e55d803ef97ae12d8b6d95715033915
Ottomata <aotto@wikimedia.org>
2019-09-09 15:12:41 -0400

    Decomission eventlogging-service-eventbus in beta / deployment-prep
    Bug: T232122
    Change-Id: Ifa3536ce7499edd022546150f6455c8711c29e52

--- modules/role/manifests/kafka/main.pp
+++ modules/role/manifests/kafka/main.pp
@@ -7,12 +7,16 @@ class role::kafka::main {
         description => "Kafka Broker in the main-${::site} Kafka cluster",
     }
 
-    include ::profile::eventbus
-    system::role { 'eventbus':
-        description => 'EventBus HTTP Service node',
-    }
-
     if $::realm == 'production' {
+        # eventlogging-service-eventbus is being commissioned.
+        # It has been removed in beta; this is here temporarily until
+        # we remove in production as well.
+        # https://phabricator.wikimedia.org/T232122
+        include ::profile::eventbus
+        system::role { 'eventbus':
+            description => 'EventBus HTTP Service node',
+        }
+
         # Mirror eqiad.* topics from main-eqiad into main-codfw,
         # or mirror codfw.* topics from main-codfw into main-eqiad.
         system::role { 'kafka::mirror':
