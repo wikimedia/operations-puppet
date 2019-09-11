5a027daf789556a11559ede794f4c3255c732e34
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-09-11 15:35:33 +0200

    envoyproxy: fixup for hot restarted
    Change-Id: I27e4d12bd753bc609a5d87c4261e5c022f82af02

--- modules/envoyproxy/files/hot_restarter/start-envoy.sh
+++ modules/envoyproxy/files/hot_restarter/start-envoy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ENVOY_CONFIG=${ENVOY_CONFIG:-/etc/envoy/envoy.yaml}
-exec /usr/sbin/envoy -c "$ENVOY_CONFIG" --restart-epoch "$RESTART_EPOCH"
+exec /usr/bin/envoy -c "$ENVOY_CONFIG" --restart-epoch "$RESTART_EPOCH"
