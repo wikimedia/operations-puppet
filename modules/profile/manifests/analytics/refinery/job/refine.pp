98042fcb742228af93a28950f9bccd2647d348a0
Nuria Ruiz <nuria@wikimedia.org>
2019-09-11 11:47:44 -0700

    Bumping up jar version to correct typo
    Third party data should not be refined and was being so
    
    Change-Id: Ic9896008e5e50a9bfb97d21d105c8191ab823a6b

--- modules/profile/manifests/analytics/refinery/job/refine.pp
+++ modules/profile/manifests/analytics/refinery/job/refine.pp
@@ -11,7 +11,7 @@ class profile::analytics::refinery::job::refine(
 
     # Update this when you want to change the version of the refinery job jar
     # being used for the refine job.
-    $refinery_version = '0.0.94'
+    $refinery_version = '0.0.99'
 
     # Use this value by default
     Profile::Analytics::Refinery::Job::Refine_job {
