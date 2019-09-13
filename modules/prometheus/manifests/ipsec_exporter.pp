e17b8789ede6e40e01234b9f92079f76f10745b4
Keith Herron <kherron@wikimedia.org>
2019-09-13 16:00:46 -0400

    prometheus-ipsec-exporter: subscribe service to /etc/ipsec.conf
    restart prometheus-ipsec-exporter to pick up config file changes
    
    Bug: T230236
    Change-Id: I25c2cea397ac8c675016400ab056d38127d4c681

--- modules/prometheus/manifests/ipsec_exporter.pp
+++ modules/prometheus/manifests/ipsec_exporter.pp
@@ -2,7 +2,8 @@ class prometheus::ipsec_exporter{
   ensure_packages('prometheus-ipsec-exporter')
 
   service { 'prometheus-ipsec-exporter':
-    ensure  => 'running',
-    require => Package['prometheus-ipsec-exporter'],
+    ensure    => 'running',
+    subscribe => File['/etc/ipsec.conf'],
+    require   => Package['prometheus-ipsec-exporter'],
   }
 }
