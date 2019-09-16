8b043242c7a8300ca54fb4a6a53242a6bcecb6e1
Marko Obrovac <mobrovac@wikimedia.org>
2019-09-16 11:13:56 +0200

    Parsoid: Change the beta port to 8001 to avoid conflict with PHP7
    Bug: T231569
    Change-Id: I61473cd62d4f563d5b0de2f449b7fa6a3feea63f

--- modules/profile/manifests/parsoid.pp
+++ modules/profile/manifests/parsoid.pp
@@ -1,5 +1,6 @@
 class profile::parsoid(
     Boolean $has_lvs = hiera('has_lvs', true),
+    Integer[1025, 65535] $port = lookup('profile::parsoid::port', {'default_value' => 8000}),
 ) {
     if $has_lvs {
         require ::profile::lvs::realserver
@@ -9,7 +10,7 @@ class profile::parsoid(
 
     $mwapi_server = "${::service::configuration::mwapi_host}/w/api.php"
     class { '::parsoid':
-        port         => 8000,
+        port         => $port,
         mwapi_server => $mwapi_server,
         mwapi_proxy  => ''
     }
