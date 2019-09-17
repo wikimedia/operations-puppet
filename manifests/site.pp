cef2896a8dc90a7aebead87a96072e1eb4e0bd98
Keith Herron <kherron@wikimedia.org>
2019-09-17 13:21:44 -0400

    kafka-main: move kafka1003 to role spare system
    Bug: T225005
    Change-Id: I914c39c854dd152900a56f22c46e02217a51f64e

--- manifests/site.pp
+++ manifests/site.pp
@@ -1293,11 +1293,7 @@ node 'iron.wikimedia.org' {
 }
 
 # Kafka Brokers - main-eqiad and main-codfw Kafka clusters.
-node /kafka1003\.eqiad\.wmnet/ {
-    role(kafka::main)
-}
-
-node /kafka100[12]\.eqiad\.wmnet/ {
+node /kafka100[123]\.eqiad\.wmnet/ {
     role(spare::system)
 }
 
