4bb8bc928da9cd045c0c17ab0363497e968252ad
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-09-18 08:29:30 +0200

    envoy: fix typo in restarter script
    Change-Id: Ic8767e2abaab9721fb1544a6c5b05851901940b6

--- modules/envoyproxy/files/hot_restarter/start-envoy.sh
+++ modules/envoyproxy/files/hot_restarter/start-envoy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ENVOY_CONFIG=${ENVOY_CONFIG:-/etc/envoy/envoy.yaml}
-exec /usr/bin/envoy -c "$ENVOY_CONFIG" --restart-epoch "$RESTART_EPOCH" --service-zone "$SERVICE_ZONE" --service-cluster "$SERVICE_CULSTER" --service-node "$SERVICE_NODE"
+exec /usr/bin/envoy -c "$ENVOY_CONFIG" --restart-epoch "$RESTART_EPOCH" --service-zone "$SERVICE_ZONE" --service-cluster "$SERVICE_CLUSTER" --service-node "$SERVICE_NODE"
