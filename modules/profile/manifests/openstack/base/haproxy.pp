a16fed16a199b6c7e58b89cce614c8f0f1969cea
Jason Hedden <jhedden@wikimedia.org>
2019-09-20 10:35:48 -0500

    openstack: add haproxy prometheus exporter
    HAProxy is used to load balance the OpenStack APIs across the controllers. This
    commit adds the prometheus haproxy exporter to the controllers running HAProxy.
    
    Change-Id: I8f13804557d532704ad2f20132a315694eb80ba9

--- modules/profile/manifests/openstack/base/haproxy.pp
+++ modules/profile/manifests/openstack/base/haproxy.pp
@@ -1,6 +1,8 @@
 class profile::openstack::base::haproxy(
     Boolean $logging = lookup('profile::openstack::base::haproxy::logging'),
 ) {
+    include ::profile::prometheus::haproxy_exporter
+
     class { 'haproxy':
         logging  => $logging,
         template => 'profile/openstack/base/haproxy/haproxy.cfg.erb',
