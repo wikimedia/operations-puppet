9f6c3a1ed97bf72121a45f7b8b86bd816e24e872
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2019-09-20 11:30:08 +0200

    netops: add ripe atlas tools class
    Useful to run ad-hoc checks when troubleshooting problems
    
    Bug: T232711
    Change-Id: I7ecb4a574eb25660d5982254372002990977481c

---
+++ modules/netops/manifests/ripeatlas/cli.pp
@@ -0,0 +1,39 @@
+class netops::ripeatlas::cli {
+    require_package('ripe-atlas-tools')
+
+    include ::passwords::netops # lint:ignore:wmf_styleguide
+    $api_key = $::passwords::netops::ripeatlas_cli_api_key
+    $utils = ['adig', 'ahttp', 'antp', 'aping', 'asslcert', 'atraceroute']
+
+    $home = '/var/lib/atlas'
+
+    user { 'atlas':
+        ensure => present,
+        system => true,
+        home   => $home,
+        shell  => '/bin/bash',
+    }
+
+    file { ["${home}/.config", "${home}/.config/ripe-atlas-tools"]:
+        ensure => directory,
+        owner  => 'atlas',
+        group  => 'root',
+        mode   => '0500',
+    }
+
+    file { "${home}/.config/ripe-atlas-tools/rc":
+        ensure  => present,
+        content => template('netops/ripeatlas-cli-config.erb'),
+        owner   => 'atlas',
+        group   => 'root',
+        mode    => '0400',
+    }
+
+    file { '/etc/ripeatlas.alias':
+        ensure  => present,
+        content => template('netops/ripeatlas-alias.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+}
---
+++ modules/profile/manifests/netops/ripeatlas/cli.pp
@@ -0,0 +1,4 @@
+# Interact with ripe atlas API from command line
+class profile::netops::ripeatlas::cli {
+    class { 'netops::ripeatlas::cli': }
+}
