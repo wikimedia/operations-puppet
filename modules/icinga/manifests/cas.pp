e73733643d70f8922a4e192e4d10bfcdf56b6351
John Bond <jbond@wikimedia.org>
2019-09-23 13:26:57 +0200

    icinga::cas apply correct type checking
    Change-Id: I3b235923ae4c6659e78ed5b19232367d9037e0c2

--- modules/icinga/manifests/cas.pp
+++ modules/icinga/manifests/cas.pp
@@ -1,16 +1,16 @@
 class icinga::cas (
-    Stdlib::Host        $virtual_host     = 'icinga.example.com',
-    Stdlib::Unixpath    $cookie_path      = '/var/cache/apache2/mod_auth_cas/',
-    Stdlib::Unixpath    $certificate_path = '/etc/ssl/certs/',
-    Stdlib::HTTPUrl     $login_url        = 'https://idp.example.org/cas/login',
-    Stdlib::HTTPUrl     $validate_url     = 'https://idp.example.org/cas/samlValidate',
-    String[1]           $authn_header     = 'CAS-User',
-    String[1]           $attribute_prefix = 'X-CAS-',
-    Boolean             $debug            = false,
-    Boolean             $validate_saml    = true,
-    String[1]           $apache_owner     = 'www-data',
-    String[1]           $apache_group     = 'www-data',
-    Optional[String[1]] $required_groups  = [],
+    Stdlib::Host               $virtual_host     = 'icinga.example.com',
+    Stdlib::Unixpath           $cookie_path      = '/var/cache/apache2/mod_auth_cas/',
+    Stdlib::Unixpath           $certificate_path = '/etc/ssl/certs/',
+    Stdlib::HTTPUrl            $login_url        = 'https://idp.example.org/cas/login',
+    Stdlib::HTTPUrl            $validate_url     = 'https://idp.example.org/cas/samlValidate',
+    String[1]                  $authn_header     = 'CAS-User',
+    String[1]                  $attribute_prefix = 'X-CAS-',
+    Boolean                    $debug            = false,
+    Boolean                    $validate_saml    = true,
+    String[1]                  $apache_owner     = 'www-data',
+    String[1]                  $apache_group     = 'www-data',
+    Optional[Array[String[1]]] $required_groups  = [],
 ) {
     ensure_packages(['libapache2-mod-auth-cas'])
     httpd::mod_conf{'auth_cas':}
