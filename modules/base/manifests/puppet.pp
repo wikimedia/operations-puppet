f1e7280d55ea596d6c1d264b792553ed1d44e783
John Bond <jbond@wikimedia.org>
2019-09-23 19:37:27 +0200

    base::puppet: add preferred_serialization_format = pson
    the 4.8.2 puppet masters do not understand json serialisation so force
    pson while the migration completes.
    
    Bug: T233643
    Change-Id: I968a2c19949ab892f42507d017e67a857c4dc70d

--- modules/base/manifests/puppet.pp
+++ modules/base/manifests/puppet.pp
@@ -1,13 +1,14 @@
 class base::puppet(
-    String                 $ca_server                   = '',
-    Stdlib::Host           $server                      = 'puppet',
-    Optional[String]       $certname                    = undef,
-    Optional[String]       $dns_alt_names               = undef,
-    Optional[String]       $environment                 = undef,
-    Integer                $interval                    = 30,
-    Boolean                $auto_puppetmaster_switching = false,
-    Integer[4,5]           $puppet_major_version        = 5,
-    Integer[2,3]           $facter_major_version        = 3,
+    String                          $ca_server                   = '',
+    Stdlib::Host                    $server                      = 'puppet',
+    Optional[String]                $certname                    = undef,
+    Optional[String]                $dns_alt_names               = undef,
+    Optional[String]                $environment                 = undef,
+    Integer                         $interval                    = 30,
+    Boolean                         $auto_puppetmaster_switching = false,
+    Integer[4,5]                    $puppet_major_version        = 5,
+    Integer[2,3]                    $facter_major_version        = 3,
+    Enum['pson', 'json', 'msgpack'] $serialization_format        = 'pson',
 ) {
     include ::passwords::puppet::database # lint:ignore:wmf_styleguide
 
