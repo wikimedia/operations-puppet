6ea2b87dcd9e0ae264fc6dca71c597472c43c891
Daniel Zahn <dzahn@wikimedia.org>
2019-09-25 20:53:32 +0200

    parsoid: introduce parameter to use parsoid/PHP
    Add a Hiera key and parameter "use_php" to let us switch
    an existing wtp Parsoid server to support Parsoid/PHP
    in addition to Parsoid/JS.
    
    This means adding mediawiki php appserver-like roles
    and profiles similar to the parsoid test server scandium.
    
    Bug: T233654
    Change-Id: I5ef55276d06df1627da3acaabb6c845476c9fd32

--- modules/profile/manifests/parsoid.pp
+++ modules/profile/manifests/parsoid.pp
@@ -1,9 +1,26 @@
 class profile::parsoid(
     Boolean $has_lvs = hiera('has_lvs', true),
     Integer[1025, 65535] $port = lookup('profile::parsoid::port', {'default_value' => 8000}),
+    Boolean $use_php = lookup('profile::parsoid::use_php', {'default_value' => false }),
 ) {
     if $has_lvs {
         require ::profile::lvs::realserver
+    }
+
+    if $use_php {
+
+        require ::profile::mediawiki::scap_proxy
+        require ::profile::mediawiki::common
+        require ::profile::mediawiki::nutcracker
+        require ::profile::mediawiki::mcrouter_wancache
+        require ::profile::prometheus::mcrouter_exporter
+
+        # proxy for connection to other servers
+        require ::profile::services_proxy
+
+        require ::profile::mediawiki::php
+        require ::profile::mediawiki::php::monitoring
+        require ::profile::mediawiki::webserver
     }
 
     class { '::service::configuration': }
@@ -12,6 +29,6 @@ class profile::parsoid(
     class { '::parsoid':
         port         => $port,
         mwapi_server => $mwapi_server,
-        mwapi_proxy  => ''
+        mwapi_proxy  => '',
     }
 }
