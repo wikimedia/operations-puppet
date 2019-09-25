fd26642bf5df8b58be48958002a54cb76c15c625
Mukunda Modell <mmodell@wikimedia.org>
2019-09-25 14:43:03 -0500

    Script to upgrade phatality
    Turns out that in order to deploy a new version of a kibana plugin
    you must first remove the old one. This adjusts the sudoers privileges
    and provides a script to run `kibana-plugin remove` followed by
    `kibana-plugin install`
    
    Change-Id: Ie24996b7ec8cce5f604ca4251f4ee2a3196f5537

---
+++ modules/kibana/files/upgrade-phatality.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+KIBANA_VERSION=$(jq -r ".version" /usr/share/kibana/package.json)
+PHATALITY_PKG="/srv/deployment/releng/phatality/deploy/phatality-$KIBANA_VERSION.zip"
+/usr/bin/sudo /usr/share/kibana/bin/kibana-plugin remove phatality
+/usr/bin/sudo /usr/share/kibana/bin/kibana-plugin install "file://$PHATALITY_PKG"
--- modules/kibana/manifests/phatality.pp
+++ modules/kibana/manifests/phatality.pp
@@ -9,13 +9,21 @@ class kibana::phatality () {
         ],
     }
 
-    $installcmd = '/usr/share/kibana/bin/kibana-plugin install'
-    $deploydir = "/srv/deployment/${target}"
+    $upgradecmd = '/usr/share/kibana/bin/upgrade-phatality.sh'
+    $plugincmd = '/usr/share/kibana/bin/kibana-plugin'
+    $deploydir = '/srv/deployment/releng/phatality/deploy/'
 
-    sudo::user { 'kibana-deploy-plugin':
+    sudo::user { 'kibana-deploy-phatality':
         user       => 'deploy-service',
         privileges => [
-            "ALL = (kibana) NOPASSWD: ${installcmd} file\\://${deploydir}/*",
+            "ALL = (kibana) NOPASSWD: ${plugincmd} file\\://${deploydir}/*",
+            "ALL = (kibana) NOPASSWD: ${plugincmd} remove *",
         ]
+    }
+
+    file { $upgradecmd:
+        ensure => 'file',
+        mode   => '0555',
+        source => 'puppet:///modules/kibana/upgrade-phatality.sh',
     }
 }
