944dad8016cfe0048073ec60f43f913107e6ee26
Marcel Ruiz Forns <mforns@wikimedia.org>
2019-09-26 19:21:18 +0200

    dumps::manifests::web::fetches::stats: correct path for mediawiki history
    Bug: T208612
    Change-Id: If4e7c9f442929e5066924ead646ade38e7faa874

--- modules/dumps/manifests/web/fetches/stats.pp
+++ modules/dumps/manifests/web/fetches/stats.pp
@@ -46,7 +46,7 @@ class dumps::web::fetches::stats(
 
     # Copies over mediawiki history dumps from an rsyncable location.
     dumps::web::fetches::job { 'mediawiki_history_dumps':
-        source      => "${src}/mediawiki/history/dumps",
+        source      => "${src}/mediawiki/history",
         destination => "${miscdatasetsdir}/mediawiki_history",
         hour        => '5',
         user        => $user,
