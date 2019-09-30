72eed76a48e8047be9d715630c9c2d536bad7406
Daniel Zahn <dzahn@wikimedia.org>
2019-09-30 13:57:53 -0700

    wikistats (cloud): support buster with PHP7.3, httpd module
    Change-Id: I7beca553c99d849dd130c594da25bd2601a7f46f

--- modules/role/manifests/wikistats/instance.pp
+++ modules/role/manifests/wikistats/instance.pp
@@ -8,7 +8,11 @@ class role::wikistats::instance {
 
     system::role { 'wikistats': description => 'wikistats instance' }
 
-    $php_module = 'php7.0'
+    if os_version('debian == buster') {
+        $php_module = 'php7.3'
+    } else {
+        $php_module = 'php7.0'
+    }
 
     class { '::httpd':
         modules => [$php_module, 'rewrite'],
