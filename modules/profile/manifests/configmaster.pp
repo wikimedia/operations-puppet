a70bfe1f12d8d5e0fda98e36e568af060b43255b
Chris Danis <cdanis@wikimedia.org>
2019-10-01 10:33:00 -0400

    config-master: ensure perms on puppet-merge sha files
    Bug: T234332
    Change-Id: I198e0748c06e04dd1f610b388fbb04030696f30e

--- modules/profile/manifests/configmaster.pp
+++ modules/profile/manifests/configmaster.pp
@@ -20,6 +20,24 @@ class profile::configmaster(
         mode   => '0755',
     }
 
+    # The contents of these files are managed by puppet-merge, but user
+    # gitpuppet can't/shouldn't be able to create files under $root_dir.
+    # So puppet makes sure the file at least exists, and then puppet-merge
+    # can write.
+    file { "${root_dir}/puppet-sha1.txt":
+        ensure => present,
+        owner  => 'gitpuppet',
+        group  => 'gitpuppet',
+        mode   => '0644',
+    }
+
+    file { "${root_dir}/labsprivate-sha1.txt":
+        ensure => present,
+        owner  => 'gitpuppet',
+        group  => 'gitpuppet',
+        mode   => '0644',
+    }
+
     # Write pybal pools
     class { '::pybal::web':
         ensure   => present,
