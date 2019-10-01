f532605483f55e9bf8a0d48380da1068a2165cda
bstorm <bstorm@wikimedia.org>
2019-10-01 11:02:25 -0700

    dologmsg: ensure the directory exists before trying to add the man page
    Bug: T222244
    Change-Id: Ia6d1aa5e2638efc1e4793ac985698e593a2bad2e

--- modules/profile/manifests/wmcs/dologmsg.pp
+++ modules/profile/manifests/wmcs/dologmsg.pp
@@ -11,11 +11,20 @@ class profile::wmcs::dologmsg(
         content => template('profile/wmcs/dologmsg.erb'),
     }
 
+    file {'/usr/local/share/man/man1/':
+        ensure  => 'directory',
+        recurse => true,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+    }
+
     file { '/usr/local/share/man/man1/dologmsg.1':
-        ensure => file,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => 'puppet:///modules/profile/manpages/man/dologmsg.1',
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        source  => 'puppet:///modules/profile/manpages/man/dologmsg.1',
+        require => File['/usr/local/share/man/man1/'],
     }
 }
