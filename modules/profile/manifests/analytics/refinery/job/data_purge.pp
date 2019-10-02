257fdf68c386bcfe8e0f698821300d7bb28204f4
Marcel Ruiz Forns <mforns@wikimedia.org>
2019-10-02 17:06:10 +0200

    analytics:refinery:job:data_purge: Re-enable deletion of geoeditors
    Bug: T234238
    Change-Id: Ibb4256ebe681c03ccb0dd5cfc1aeac0a501bae12

--- modules/profile/manifests/analytics/refinery/job/data_purge.pp
+++ modules/profile/manifests/analytics/refinery/job/data_purge.pp
@@ -241,7 +241,6 @@ class profile::analytics::refinery::job::data_purge (
     # runs once a day (but only will delete data on the needed date)
     # Temporary stopped to prevent data to be dropped.
     kerberos::systemd_timer { 'mediawiki-geoeditors-drop-month':
-        ensure       => absent,
         description  => 'Drop Geo-editors data from Hive/HDFS following data retention policies.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --database='wmf' --tables='geoeditors_daily' --base-path='/wmf/data/wmf/mediawiki_private/geoeditors_daily' --path-format='month=(?P<year>[0-9]+)-(?P<month>[0-9]+)' --older-than='${geoeditors_private_retention_days}' --skip-trash --execute='5faa519abce3840e7718cfbde8779078'",
         environment  => $systemd_env,
