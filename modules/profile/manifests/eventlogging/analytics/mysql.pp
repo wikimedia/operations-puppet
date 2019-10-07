072825dcbd50865919962d5c5a793988b25be952
Ottomata <aotto@wikimedia.org>
2019-10-07 15:42:09 -0400

    Ensure eventlogging-consumer mysql is absent on eventlog1002
    Bug: T223414
    Change-Id: Iea9f8cbfd27d615703ba20406e755008c9426387

--- modules/profile/manifests/eventlogging/analytics/mysql.pp
+++ modules/profile/manifests/eventlogging/analytics/mysql.pp
@@ -6,6 +6,7 @@ class profile::eventlogging::analytics::mysql(
     $mysql_consumers = hiera('profile::eventlogging::analytics::mysql::consumers'),
     $mysql_db        = hiera('profile::eventlogging::analytics::mysql::db', '127.0.0.1/log'),
     $statsd_host     = hiera('profile::eventlogging::analytics::mysql::statsd_host', 'statsd.eqiad.wmnet'),
+    $ensure          = hiera('profile::eventlogging::analytics::mysql::ensure', 'present'),
 ) {
 
     include profile::eventlogging::analytics::server
@@ -37,6 +38,7 @@ class profile::eventlogging::analytics::mysql(
 
     # Kafka consumer group for this consumer is mysql-m4-master
     eventlogging::service::consumer { $mysql_consumers:
+        ensure => $ensure,
         # auto commit offsets to kafka more often for mysql consumer
         input  => "${map_scheme}${kafka_consumer_uri}&auto_commit_interval_ms=1000${map_function}",
         output => "mysql://${mysql_user}:${mysql_pass}@${mysql_db}?charset=utf8&statsd_host=${statsd_host}&replace=True",
