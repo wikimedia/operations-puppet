d24db14a110821b5496280e5c79fc3104f1dc3df
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-10-08 08:26:15 +0200

    echostore: add LVS configuration stanzas
    Why:
      We're setting up this new service
    
    What:
      Add the LVS configuration stanza (with critical false as the service
      is still not set up)
    
    Bug: T234464
    Change-Id: Ie61c99f1147cd90f173e88ab04e2951f80c71764

--- modules/lvs/manifests/monitor_services.pp
+++ modules/lvs/manifests/monitor_services.pp
@@ -102,6 +102,13 @@ class lvs::monitor_services(
                 check_command => "check_wmf_service!http://wikifeeds.svc.${dc}.wmnet:8889!15!",
                 notes_url     => 'https://wikitech.wikimedia.org/wiki/Wikifeeds',
                 ;
+            "check_echostore_${dc}":
+                host          => "sessionstore.svc.${dc}.wmnet",
+                description   => "Sessionstore ${dc}",
+                check_command => "check_wmf_service_url!https1://sessionstore.svc.${dc}.wmnet:8081!15!/openapi",
+                notes_url     => 'https://www.mediawiki.org/wiki/Kask',
+                ;
+
         }
     }
 
