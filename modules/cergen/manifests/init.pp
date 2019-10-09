1273587c898fb16d55ef5b824008b6bc8a0dcf24
John Bond <jbond@wikimedia.org>
2019-10-09 14:23:35 +0200

    certgen: only use install options on jessie
    Bug: T235060
    Change-Id: I5bf034c957be888aa940608f77a9f37008338e35

--- modules/cergen/manifests/init.pp
+++ modules/cergen/manifests/init.pp
@@ -3,17 +3,12 @@
 #
 class cergen {
     package { 'cergen':
-        ensure          => 'present',
-        # If Jessie, we need to manually specify versions
-        # of some dependencies from backports.
-        install_options => ['-t', "${::lsbdistcodename}-backports"],
+        ensure => 'present',
     }
-
     file { '/etc/cergen':
         ensure => directory,
         owner  => 'root',
         group  => 'root',
         mode   => '0750',
     }
-
 }
