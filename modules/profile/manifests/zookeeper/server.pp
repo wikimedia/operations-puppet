5f92ec21b071c058ffd0af00254ba2e680246c32
Luca Toscano <ltoscano@wikimedia.org>
2019-10-14 08:44:16 +0200

    role::analytics_cluster::zookeeper: enable monitoring
    Remove old Analytics monitor and allow the possibility
    to set the monitoring's contact group.
    
    Bug: T217057
    Change-Id: Ia0fe1461d23b559814bfaa98d055ed43c0d027a7

--- modules/profile/manifests/zookeeper/server.pp
+++ modules/profile/manifests/zookeeper/server.pp
@@ -5,14 +5,15 @@
 #
 # filtertags: labs-project-deployment-prep labs-project-analytics
 class profile::zookeeper::server (
-    $clusters               = hiera('zookeeper_clusters'),
-    $cluster_name           = hiera('profile::zookeeper::cluster_name'),
-    $version                = hiera('profile::zookeeper::zookeeper_version'),
-    $max_client_connections = hiera('profile::zookeeper::max_client_connections', 1024),
-    $sync_limit             = hiera('profile::zookeeper::sync_limit', 8),
-    $monitoring_enabled     = hiera('profile::zookeeper::monitoring_enabled', false),
-    $is_critical            = hiera('profile::zookeeper::is_critical', false),
-    $prometheus_instance    = hiera('profile::zookeeper::prometheus_instance', 'ops'),
+    $clusters                 = hiera('zookeeper_clusters'),
+    $cluster_name             = hiera('profile::zookeeper::cluster_name'),
+    $version                  = hiera('profile::zookeeper::zookeeper_version'),
+    $max_client_connections   = hiera('profile::zookeeper::max_client_connections', 1024),
+    $sync_limit               = hiera('profile::zookeeper::sync_limit', 8),
+    $monitoring_enabled       = hiera('profile::zookeeper::monitoring_enabled', false),
+    $monitoring_contact_group = hiera('profile::zookeeper::monitoring_contact_group', 'admins'),
+    $is_critical              = hiera('profile::zookeeper::is_critical', false),
+    $prometheus_instance      = hiera('profile::zookeeper::prometheus_instance', 'ops'),
 ) {
 
     require_package('default-jdk')
@@ -61,10 +62,11 @@ class profile::zookeeper::server (
     if $monitoring_enabled {
         # Alert if Zookeeper Server is not running.
         nrpe::monitor_service { 'zookeeper':
-            description  => 'Zookeeper Server',
-            nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -C java -a "org.apache.zookeeper.server.quorum.QuorumPeerMain /etc/zookeeper/conf/zoo.cfg"',
-            critical     => $is_critical,
-            notes_url    => 'https://wikitech.wikimedia.org/wiki/Zookeeper',
+            description   => 'Zookeeper Server',
+            nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 1:1 -C java -a "org.apache.zookeeper.server.quorum.QuorumPeerMain /etc/zookeeper/conf/zoo.cfg"',
+            critical      => $is_critical,
+            contact_group => $monitoring_contact_group,
+            notes_url     => 'https://wikitech.wikimedia.org/wiki/Zookeeper',
         }
 
         monitoring::check_prometheus { 'zookeeper_client_conns':
@@ -74,21 +76,8 @@ class profile::zookeeper::server (
             warning         => $max_client_connections / 2,
             critical        => $max_client_connections,
             method          => 'ge',
+            contact_group   => $monitoring_contact_group,
             dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/zookeeper?refresh=5m&orgId=1&panelId=6&fullscreen'],
-            notes_link      => 'https://wikitech.wikimedia.org/wiki/Zookeeper',
-        }
-
-        # Experimental Analytics alarms on JVM usage
-        # These alarms are not really generic and the thresholds are based
-        # on a fixed Max Heap size of 1G.
-        monitoring::check_prometheus { 'zookeeper-server-heap-usage':
-            description     => 'Zookeeper node JVM Heap usage',
-            dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/zookeeper?refresh=5m&orgId=1&panelId=40&fullscreen'],
-            query           => "scalar(avg_over_time(jvm_memory_bytes_used{instance=\"${::hostname}:12181\",area=\"heap\"}[60m])/avg_over_time(jvm_memory_bytes_max{instance=\"${::hostname}:12181\",area=\"heap\"}[60m]))",
-            warning         => 0.9,
-            critical        => 0.95,
-            contact_group   => 'analytics',
-            prometheus_url  => "http://prometheus.svc.${::site}.wmnet/${prometheus_instance}",
             notes_link      => 'https://wikitech.wikimedia.org/wiki/Zookeeper',
         }
     }
