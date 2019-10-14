e99f4c4dee2557d07b6219ab45bfe57a33965434
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2019-10-14 16:53:37 +0200

    Add component/cergen to cergen class on buster
    Change-Id: I5e81a3a5c1c7a826638c77180304f7d7ed323ea8

--- modules/cergen/manifests/init.pp
+++ modules/cergen/manifests/init.pp
@@ -2,9 +2,33 @@
 # Installs cergen package.
 #
 class cergen {
-    package { 'cergen':
-        ensure => 'present',
+
+    if os_version('debian == buster') {
+        package { 'cergen':
+            ensure  => 'present',
+            require => [
+                        Exec['apt_update_cergen'],
+                        Apt::Repository['buster-cergen'],
+                        ],
+        }
+
+        apt::repository { 'buster-cergen':
+            uri        => 'http://apt.wikimedia.org/wikimedia',
+            dist       => 'buster-wikimedia',
+            components => 'component/cergen',
+            notify     => Exec['apt_update_cergen'],
+        }
+
+        exec {'apt_update_cergen':
+            command     => '/usr/bin/apt-get update',
+            refreshonly => true,
+        }
+    } else {
+        package { 'cergen':
+            ensure => 'present',
+        }
     }
+
     file { '/etc/cergen':
         ensure => directory,
         owner  => 'root',
