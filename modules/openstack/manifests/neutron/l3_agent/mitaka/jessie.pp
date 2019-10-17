17cbc16d1ec5a32b2c86b22d2dc09b6fa4d27e02
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-10-17 10:59:07 +0200

    openstack: drop jessie code
    We no longer have any server using Debian Jessie.
    
    Bug: T212302
    Change-Id: I89d514d5e808eacb33d256796232730eaa857955
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/openstack/manifests/neutron/l3_agent/mitaka/jessie.pp
+++
@@ -1,9 +0,0 @@
-class openstack::neutron::l3_agent::mitaka::jessie(
-) {
-    require openstack::serverpackages::mitaka::jessie
-
-    package { 'neutron-l3-agent':
-        ensure          => 'present',
-        install_options => ['-t', 'jessie-backports'],
-    }
-}
--- modules/openstack/manifests/neutron/metadata_agent/mitaka/jessie.pp
+++
@@ -1,9 +0,0 @@
-class openstack::neutron::metadata_agent::mitaka::jessie(
-) {
-    require ::openstack::serverpackages::mitaka::jessie
-
-    package {'neutron-metadata-agent':
-        ensure          => 'present',
-        install_options => ['-t', 'jessie-backports'],
-    }
-}
