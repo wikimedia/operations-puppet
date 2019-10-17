e1664a7cc3ef495a84700ea315253a855ddaa26a
andrewbogott <abogott@wikimedia.org>
2019-10-17 14:18:36 -0500

    codfw1dev: correct lookup for the nova proxy ldap password
    Change-Id: I484120dfb5c1758103ba23b1e39dbce2a4a3874a

--- modules/profile/manifests/openstack/codfw1dev/wikitech/web.pp
+++ modules/profile/manifests/openstack/codfw1dev/wikitech/web.pp
@@ -2,7 +2,7 @@ class profile::openstack::codfw1dev::wikitech::web(
     $osm_host = hiera('profile::openstack::codfw1dev::wikitech::web::osm_host'),
     $webserver_hostname_aliases = hiera('profile::openstack::codfw1dev::wikitech::webserver_hostname_aliases'),
     $wikidb = hiera('profile::openstack::codfw1dev::wikitech::db_name'),
-    $wikitech_nova_ldap_proxyagent_pass = hiera('profile::openstack::base::ldap_proxyuser_pass'),
+    $wikitech_nova_ldap_proxyagent_pass = hiera('profile::openstack::codfw1dev::ldap_proxyuser_pass'),
     $wikitech_nova_ldap_user_pass = hiera('profile::openstack::codfw1dev::ldap_user_pass'),
     ) {
 
