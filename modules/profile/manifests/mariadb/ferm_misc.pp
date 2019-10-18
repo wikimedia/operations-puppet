c61aa1642a3790b69faa9e3bdf8f3fe3b319887c
Daniel Zahn <dzahn@wikimedia.org>
2019-10-17 17:18:02 -0700

    mariadb/ferm_misc: allow moscovium to connect to rt database
    Bug: T180641
    Change-Id: I00fe6eb748c2ca4a96ede020219ac24bc1b8b0dc

--- modules/profile/manifests/mariadb/ferm_misc.pp
+++ modules/profile/manifests/mariadb/ferm_misc.pp
@@ -18,7 +18,7 @@ class profile::mariadb::ferm_misc {
         proto   => 'tcp',
         port    => '3306',
         notrack => true,
-        srange  => '@resolve(ununpentium.wikimedia.org)',
+        srange  => '@resolve((ununpentium.wikimedia.org moscovium.eqiad.wmnet))',
     }
 
     ferm::service { 'gerrit':
