d8359725a1530ea9e453032349391d1a14f09e3b
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2019-10-21 16:50:10 +0200

    Update microcode check
    1. The CPU flag for L1TF isn't visible on KVM hosts, the mitigation fully
       takes effect on the virtualisation server itself
    2. All CPUs which were fixed by Intel for SSBD also shipped mitigations
       for L1TF (for some models it wasn't available in the first release, which
       caused some confusion. As such rename the black lists.
    3. Update the black lists based on initial tests with Cumin
    
    Bug: T235250
    Change-Id: I37451c2c5a4f3d1319388bdb6b6c3c070ef25045

--- modules/base/files/check-microcode.py
+++ modules/base/files/check-microcode.py
@@ -3,6 +3,7 @@
 
 import os
 import re
+import subprocess
 import sys
 
 import apt_pkg
@@ -39,27 +40,39 @@ def main():
     expected_cpu_flags = set()
     available_cpu_flags = set()
 
+    try:
+        systemd_detect_virt = subprocess.check_output('/usr/bin/systemd-detect-virt; exit 0',
+                                                      shell=True, universal_newlines=True).strip()
+    except subprocess.CalledProcessError as error:
+        unknown('Could not determine host virtualisation status: {}'.format(str(error.returncode)))
+
+    virtual_host = False
+    if systemd_detect_virt == 'kvm':
+        virtual_host = True
+
     # CPUs which were not fixed for SSBD (which was the first) are also not
     # fixed for L1TF/MDS
-    blacklist_ssbd = ['bast3002', 'dbproxy1006', 'dbproxy1007', 'dbproxy1008']
-    blacklist_l1tf_mds = ['es2001', 'es2002', 'es2003', 'es2004'] + blacklist_ssbd
+    blacklist_ssbd_l1tf = ['dbproxy1001', 'dbproxy1002', 'dbproxy1007', 'dbproxy1008',
+                           'es2001', 'es2002', 'es2003', 'es2004']
+    blacklist_mds = ['bast3002', 'maerlant', 'nescio', 'multatuli', 'cp1008'] + blacklist_ssbd_l1tf
 
     if apt_pkg.version_compare(current_kernelpackage_version, '4.9.107-1') > 0:
         expected_cpu_flags.add('ssbd')
     else:
         ok('No CPU flags are expected with the {} kernel'.format(current_kernelpackage_version))
 
-    if apt_pkg.version_compare(current_kernelpackage_version, '4.9.110-3+deb9u3') > 0:
-        expected_cpu_flags.add('flush_l1d')
+    if not virtual_host:
+        if apt_pkg.version_compare(current_kernelpackage_version, '4.9.110-3+deb9u3') > 0:
+            expected_cpu_flags.add('flush_l1d')
 
     if apt_pkg.version_compare(current_kernelpackage_version, '4.9.168-1+deb9u1') > 0:
         expected_cpu_flags.add('md_clear')
 
-    if 'ssbd' in expected_cpu_flags and hostname in blacklist_ssbd:
+    if 'ssbd' in expected_cpu_flags and hostname in blacklist_ssbd_l1tf:
         expected_cpu_flags.remove('ssbd')
 
     for flag in ['flush_l1d', 'md_clear']:
-        if flag in expected_cpu_flags and hostname in blacklist_l1tf_mds:
+        if flag in expected_cpu_flags and hostname in blacklist_mds:
             expected_cpu_flags.remove(flag)
 
     if not expected_cpu_flags:
