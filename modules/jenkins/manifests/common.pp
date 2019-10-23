e2db098cb5a05d10166f65eaf6d06015d953a4a7
Jbond <jbond@wikimedia.org>
2019-10-23 15:50:08 +0000

    Revert "jenkins: use correct java version on buster"
    This reverts commit 8be447cb4baf2f33ddb8dda113a971bdc753e2cc.
    
    Reason for revert: facts['os'] is different on facter v2 vs v3
    
    Change-Id: I76ac0fe9a8cd4aac58e7d6acb35cbdccdf902e87

--- modules/jenkins/manifests/common.pp
+++ modules/jenkins/manifests/common.pp
@@ -3,14 +3,10 @@
 # Dependency common to master and slaves
 #
 class jenkins::common {
-    $java_version = $facts['os']['lsb']['distcodename'] ? {
-        'buster' => '11',
-        default  => '8',
-    }
-    ensure_packages("openjdk-${java_version}-jre-headless")
+    ensure_packages('openjdk-8-jre-headless')
 
     alternatives::select { 'java':
-        path    => "/usr/lib/jvm/java-${java_version}-openjdk-amd64/jre/bin/java",
-        require => Package["openjdk-${java_version}-jre-headless"],
+        path    => '/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java',
+        require => Package['openjdk-8-jre-headless'],
     }
 }
