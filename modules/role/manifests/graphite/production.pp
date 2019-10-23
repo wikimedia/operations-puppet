9c0f32db5272ae9ba633268d025c0aef59c8c743
Emanuele Rocca <ema@wikimedia.org>
2019-10-23 10:10:17 +0200

    graphite: add TLS termination with envoy
    Bug: T210411
    Change-Id: I277753cf2dfabdb6351c552e2ea4d83593030f88

--- modules/role/manifests/graphite/production.pp
+++ modules/role/manifests/graphite/production.pp
@@ -4,4 +4,5 @@ class role::graphite::production {
     include ::profile::backup::host
     include ::profile::statsd # all graphite hosts also include statsd
     include ::profile::graphite::production
+    include ::profile::tlsproxy::envoy # TLS termination
 }
