a11e55201239c20e57c031128f42bde1648bbc9f
John Bond <jbond@wikimedia.org>
2019-10-24 12:16:27 +0200

    jenkins: use correct java version on buster
    PCC: https://integration.wikimedia.org/ci/job/operations-puppet-catalog-compiler/19041
    
    Change-Id: Iea8c730dbc8731b1555a0e8b95106380a29a4bf1

--- modules/jenkins/manifests/common.pp
+++ modules/jenkins/manifests/common.pp
@@ -3,10 +3,14 @@
 # Dependency common to master and slaves
 #
 class jenkins::common {
-    ensure_packages('openjdk-8-jre-headless')
+    $java_version = $facts['os']['release']['major'] ? {
+        /10/    => '11',
+        default => '8',
+    }
+    ensure_packages("openjdk-${java_version}-jre-headless")
 
     alternatives::select { 'java':
-        path    => '/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java',
-        require => Package['openjdk-8-jre-headless'],
+        path    => "/usr/lib/jvm/java-${java_version}-openjdk-amd64/jre/bin/java",
+        require => Package["openjdk-${java_version}-jre-headless"],
     }
 }
