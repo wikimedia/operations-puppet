372db8a87c9200db89cde36892a0462694b363e2
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-10-24 15:25:21 +0200

    conftool::scripts: add a helper script to initialize a node
    Since we removed the services definitions, all nodes get created in
    conftool as uninitialized, meaning they're in status pooled=inactive and
    weight set to 0.
    
    In some clusters, being able to predefine weights in an automated way is
    desirable; this script automates the procedure.
    
    Change-Id: I3f9a48cee486b787262ce0202a39c2b83f54a07a

---
+++ modules/conftool/manifests/scripts/initialize.pp
@@ -0,0 +1,13 @@
+# conftool::scripts::initialize: simple script to
+# set up a node with a default weight and pooled state.
+#
+# This is a class as it should be only initialized once per node.
+class conftool::scripts::initialize(Hash[String, Integer] $services) {
+    file { '/usr/local/sbin/initialize':
+        ensure  => present,
+        content => template('conftool/scripts/initialize.sh.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555'
+    }
+}
