1d1d031368e7af03c4dd09603dbdf0ae9a25964c
Alex Monk <krenair@gmail.com>
2019-10-31 00:37:31 +0000

    toolforge: Remove direct TLS termination support from static-server
    Moving behind project-proxy
    
    Bug: T236952
    Change-Id: I57816f475e1cf75fc727d1527dfffef7f5059e72

--- modules/profile/manifests/toolforge/static.pp
+++ modules/profile/manifests/toolforge/static.pp
@@ -2,10 +2,8 @@ class profile::toolforge::static (
     Stdlib::Fqdn $web_domain = lookup('profile::toolforge::web_domain', {default_value => 'tools.wmflabs.org'}),
 ) {
     $resolver             = join($::nameservers, ' ')
-    $ssl_settings         = ssl_ciphersuite('nginx', 'compat')
-    $ssl_certificate_name = 'star.wmflabs.org'
 
-    sslcert::certificate { $ssl_certificate_name: }
+    sslcert::certificate { 'star.tools.wmflabs.org': ensure => absent }
     nginx::site { 'static-server':
         content => template('profile/toolforge/static-server.conf.erb'),
     }
