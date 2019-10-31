9014159513f62432f3815107086a0ca39aebc2fc
Alex Monk <krenair@gmail.com>
2019-10-31 00:38:42 +0000

    toolforge: Remove old absented star.wmflabs.org certificate
    Bug: T236952
    Change-Id: I4413cb9cead5db8fa7db0c09b39d2214fc534120

--- modules/profile/manifests/toolforge/static.pp
+++ modules/profile/manifests/toolforge/static.pp
@@ -2,8 +2,6 @@ class profile::toolforge::static (
     Stdlib::Fqdn $web_domain = lookup('profile::toolforge::web_domain', {default_value => 'tools.wmflabs.org'}),
 ) {
     $resolver             = join($::nameservers, ' ')
-
-    sslcert::certificate { 'star.wmflabs.org': ensure => absent }
     nginx::site { 'static-server':
         content => template('profile/toolforge/static-server.conf.erb'),
     }
