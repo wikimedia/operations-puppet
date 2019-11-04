320404c4bd540a866af7ceacbb63896effad1056
Luca Toscano <ltoscano@wikimedia.org>
2019-11-04 08:18:07 +0100

    role::analytics_test_cluster::client: add git proxy configuration
    Change-Id: Ie8bec402f24a525b1fccb965cf61944b2a6b1524

--- modules/role/manifests/analytics_test_cluster/client.pp
+++ modules/role/manifests/analytics_test_cluster/client.pp
@@ -16,6 +16,8 @@ class role::analytics_test_cluster::client {
     include ::profile::kerberos::client
     include ::profile::kerberos::keytabs
 
+    include ::profile::analytics::cluster::gitconfig
+
     include ::profile::swap
 
     # Need refinery to test Refine jobs
