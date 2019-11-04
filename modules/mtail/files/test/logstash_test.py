4bd3678da0d72e9546f770328ead8295b7cbff4f
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2019-11-04 15:18:22 +0100

    mtail: add logstash program
    The idea is to inspect Logstash's own logs and extract metrics on
    failures/general Logstash health.
    
    Bug: T236343
    Change-Id: I3a5f5665659bc018bd22f9ea41f051cb253e25de

---
+++ modules/mtail/files/test/logstash_test.py
@@ -0,0 +1,22 @@
+import mtail_store
+import unittest
+import os
+
+test_dir = os.path.join(os.path.dirname(__file__))
+
+
+class LogstashTest(unittest.TestCase):
+    def setUp(self):
+        self.store = mtail_store.MtailMetricStore(
+                os.path.join(test_dir, '../programs/logstash.mtail'),
+                os.path.join(test_dir, 'logs/logstash.test'))
+
+    def testIndexFailure(self):
+        s = self.store.get_samples('logstash_elasticsearch_index_failure_total')
+        self.assertIn(('', 8), s)
+
+    def testErrors(self):
+        s = self.store.get_samples('logstash_log_level_total')
+        self.assertIn(('level=INFO', 7), s)
+        self.assertIn(('level=WARN', 8), s)
+        self.assertIn(('level=ERROR', 1), s)
