3609745f0a0af4b0d3c9ffaabf8efc7e6e2c4ceb
Ottomata <aotto@wikimedia.org>
2019-11-06 10:48:40 -0500

    HDFSCleaner - change command args
    Bug: T235200
    Change-Id: I24a8e57e0a3eabaddde20536c7c198186df0ef14

--- modules/profile/manifests/analytics/refinery/job/hdfs_cleaner.pp
+++ modules/profile/manifests/analytics/refinery/job/hdfs_cleaner.pp
@@ -10,12 +10,8 @@ class profile::analytics::refinery::job::hdfs_cleaner(
     # Ensure hadoop client is installed (refinery conditionally includes this)
     Class['::profile::hadoop::common'] -> Class['::profile::analytics::refinery::job::hdfs_cleaner']
 
-    # HDFSCleaner available since refinery 0.0.104. Just use latest installed version.
-    # https://phabricator.wikimedia.org/T235200
-    $refinery_job_jar = "${::profile::analytics::refinery::path}/artifacts/refinery-job.jar"
     $older_than_threshold = 2678400 # seconds in 31 days
-
-    $command = "/usr/bin/java -cp ${refinery_job_jar}:$(/usr/bin/hadoop classpath) org.wikimedia.analytics.refinery.job.HDFSCleaner /tmp ${older_than_threshold}"
+    $command = "${::profile::analytics::refinery::path}/bin/hdfs-cleaner --path=/tmp --older_than_seconds=${older_than_threshold}"
     kerberos::systemd_timer { 'hdfs-cleaner':
         description     => 'Run the HDFSCleaner job to keep HDFS /tmp dir clean of old files.',
         command         => $command,
