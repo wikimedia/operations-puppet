234bedf31583a5316fdb98bc5b335800372c6a5e
John Bond <jbond@wikimedia.org>
2019-11-06 15:36:12 +0100

    wmflib - puppet_config: add master section config
    Change-Id: I9daeb6a9620287cc5aa245d33b652ba7e1b09fd9

--- modules/wmflib/lib/facter/puppet_config.rb
+++ modules/wmflib/lib/facter/puppet_config.rb
@@ -1,19 +1,30 @@
-require 'facter/util/puppet_settings'  # provided by puppetlabs-stdlib
-
-settings = [
-  :localcacert,
-  :ssldir,
-  :hostpubkey,
-  :hostprivkey,
-  :hostcert,
-]
+desired_settings = {
+  :master => [
+    :localcacert,
+    :ssldir,
+  ],
+  :main => [
+    :localcacert,
+    :ssldir,
+    :hostpubkey,
+    :hostprivkey,
+    :hostcert,
+  ]
+}
 Facter.add(:puppet_config) do
   confine :kernel => 'Linux'
   puppet_config = {}
   setcode do
-    Facter::Util::PuppetSettings.with_puppet do
+    desired_settings.each_pair do |section, settings|
       settings.each do |setting|
-        puppet_config[setting.to_s] = Puppet[setting]
+        if section == :main
+          puppet_config[setting] = Puppet[setting]
+        else
+          puppet_config[sections] = {} unless puppet_config.key?(sections)
+          puppet_config[sections][settings] = Puppet.settings.values(
+            Puppet[:environment].to_sym, section
+          ).print(setting)
+        end
       end
     end
     puppet_config
