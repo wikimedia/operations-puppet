c227e74ee5e2462c7d675f7654da2c00f6b4b2d8
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-11-06 13:30:37 +0100

    toolforge: toolviews: add default hiera values
    Required for toolsbeta. We don't want toolviews in there.
    
    Bug: T237443
    Change-Id: I36aceb0a6c220b01bae805e1fe466bba64a1fe61
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/toolforge/toolviews.pp
+++ modules/profile/manifests/toolforge/toolviews.pp
@@ -2,10 +2,10 @@
 #
 # filtertags: labs-project-tools
 class profile::toolforge::toolviews (
-    $mysql_host     = lookup('profile::toolforge::toolviews::mysql_host'),
-    $mysql_db       = lookup('profile::toolforge::toolviews::mysql_db'),
-    $mysql_user     = lookup('profile::toolforge::toolviews::mysql_user'),
-    $mysql_password = lookup('profile::toolforge::toolviews::mysql_password'),
+    $mysql_host     = lookup('profile::toolforge::toolviews::mysql_host',     {default_value => 'localhost'}),
+    $mysql_db       = lookup('profile::toolforge::toolviews::mysql_db',       {default_value => 'example_db'}),
+    $mysql_user     = lookup('profile::toolforge::toolviews::mysql_user',     {default_value => 'example_user'}),
+    $mysql_password = lookup('profile::toolforge::toolviews::mysql_password', {default_value => 'example_passwd'}),
 ){
     class { '::toollabs::toolviews':
         mysql_host     => $mysql_host,
