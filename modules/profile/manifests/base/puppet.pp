392c73947148d768b084a7a5af7e45db16735c08
John Bond <jbond@wikimedia.org>
2019-11-07 12:15:22 +0100

    profile::base::puppet: move the puppetca to a global variable
    The Ca file is used by the base::puppet profile and the
    puppetmaster::frontend profile.  Its also possible that it is useful
    else where in the puppet try as such we expose it at the global level
    
    The change to Stdlib::Filesource is taken from upstream
    
    Bug: T234332
    Change-Id: I5ec6357694501dc3f9d1f60a23955bdff7aa5fad

--- modules/profile/manifests/base/puppet.pp
+++ modules/profile/manifests/base/puppet.pp
@@ -2,13 +2,13 @@
 class profile::base::puppet(
   String             $puppetmaster         = lookup('puppetmaster'),
   String             $ca_server            = lookup('puppet_ca_server'),
+  Stdlib::Filesource $ca_source            = lookup('puppet_ca_source'),
+  Boolean            $manage_ca_file       = lookup('manage_puppet_ca_file'),
   Integer[1,59]      $interval             = lookup('profile::base::puppet::interval'),
   String             $environment          = lookup('profile::base::puppet::environment'),
   Integer[2,3]       $facter_major_version = lookup('profile::base::puppet::facter_major_version'),
   Integer[4,5]       $puppet_major_version = lookup('profile::base::puppet::puppet_major_version'),
   String             $serialization_format = lookup('profile::base::puppet::serialization_format'),
-  Stdlib::Filesource $ca_source            = lookup('profile::base::puppet::ca_source'),
-  Boolean            $manage_ca_file       = lookup('profile::base::puppet::manage_ca_file'),
   # Looks like we need hiera version 5 to pass undef via hiera
   Optional[String] $dns_alt_names          = lookup('profile::base::puppet::dns_alt_names',
                                                     {'default_value' => undef})
