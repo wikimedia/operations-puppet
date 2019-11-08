5f37042220df86f2207cc7e9ce8d173d55d20163
Alexandros Kosiaris <akosiaris@wikimedia.org>
2019-11-08 15:20:06 +0200

    md: Globally set lower sync limits
    Why:
    kubernetes hosts were bitten by the same issue that etcd hosts have been
    bit in T162013, namely that the RAID resync speeds are high enough to
    cause IO latencies issues and thus facter failing. Since this has served
    us fine up to now, there is good reason to generalize it across the
    fleet. Note that one of the extra goodies coming with it is that the
    mdadm cron is being moved to only be executed during a weekday,
    minimizing the chances an issue will show up during a weekend.
    
    What:
    Move the code from the etcd profile to the raid/ module to enable it
    globally
    
    Bug: T237197
    Change-Id: Id1d25cb8c59856fde540e64a3b8efb4edf917cf3

--- modules/raid/manifests/md.pp
+++ modules/raid/manifests/md.pp
@@ -2,6 +2,22 @@
 class raid::md {
   include raid
 
+  # T162013 - reduce raid resync speeds on clustered etcd noes with software RAID
+  # in order to mitigate the risk of losing consensus due to I/O latencies
+  sysctl::parameters { 'raid_resync_speed':
+      ensure => present,
+      values => { 'dev.raid.speed_limit_max' => '20000' },
+  }
+  # Only run on a weekday of our choice, and vary it between servers
+  $dow = fqdn_rand(5, 'md_checkarray') + 1
+  # Replace the default mdadm script from upstream with our own
+  file { '/etc/cron.d/mdadm':
+      content => template('raid/mdadm-cron.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0444',
+  }
+
   nrpe::monitor_service { 'raid_md':
     description   => 'MD RAID',
     nrpe_command  => "${raid::check_raid} md",
