2907741f3e5e3fa4375d14a0a889691855b73230
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-11-08 18:40:47 +0100

    role::debug_proxy: remove old/unused aliases, depool mwdebug1002
    Also remove the logic to set the PHP_ENGINE cookie
    
    Change-Id: Ib78c5dc3c495e8d38166a31906aca3d06cdc13fc

--- modules/role/manifests/debug_proxy.pp
+++ modules/role/manifests/debug_proxy.pp
@@ -15,16 +15,12 @@ class role::debug_proxy {
     # Backward compatibility
     $aliases = {
         '1'                  => 'mwdebug1001.eqiad.wmnet',
-        'mw1017.eqiad.wmnet' => 'mwdebug1001.eqiad.wmnet',
-        'mw1099.eqiad.wmnet' => 'mwdebug1002.eqiad.wmnet',
-        'mw2017.codfw.wmnet' => 'mwdebug2001.codfw.wmnet',
-        'mw2099.codfw.wmnet' => 'mwdebug2002.codfw.wmnet',
     }
 
     # - Allow X-Wikimedia-Debug to select mwdebug hosts
     # - For back-compat, pass 'X-Wikimedia-Debug: 1' requests to mwdebug1001.
     class { '::debug_proxy':
-        backend_regexp  => '^((mwdebug1001|mwdebug1002)\.eqiad\.wmnet|(mwdebug2001|mwdebug2002)\.codfw\.wmnet)',
+        backend_regexp  => '^(mwdebug1001\.eqiad\.wmnet|(mwdebug2001|mwdebug2002)\.codfw\.wmnet)',
         backend_aliases => $aliases,
         resolver        => join($::nameservers, ' '),
     }
