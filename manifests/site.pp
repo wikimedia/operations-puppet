79133a9ba1554b5ef68bd7bc505a003bd7834b92
Jason Hedden <jhedden@wikimedia.org>
2019-11-11 11:10:45 -0600

    ceph: add spare::system role to ceph mon and osd
    These systems will be used for Ceph based block storage on the cloud services
    CloudVPS platform.
    
    Bug: T228102
    Bug: T224188
    Change-Id: I7c57f0690ac0ed899c3e1151d8072b40f1f01c94

--- manifests/site.pp
+++ manifests/site.pp
@@ -1209,6 +1209,16 @@ node /^cloudcontrol100[3-4]\.wikimedia\.org$/ {
     role(wmcs::openstack::eqiad1::control)
 }
 
+# new systems provisioning in process T228102
+node /^cloudcephmon100[1-3]\.wikimedia\.org$/ {
+    role(spare::system)
+}
+
+# new systems provisioning in process T224188
+node /^cloudcephosd100[1-3]\.wikimedia\.org$/ {
+    role(spare::system)
+}
+
 # New systems to be placed into service by cloud team via T194186
 node /^cloudelastic100[1-4]\.wikimedia\.org$/ {
     role(elasticsearch::cloudelastic)
