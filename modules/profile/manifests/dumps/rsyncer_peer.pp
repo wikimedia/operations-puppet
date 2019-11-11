aecc4d2226ab092619d8610e16e3aa585545d7ad
Ariel T. Glenn <ariel@wikimedia.org>
2019-11-11 20:52:08 +0200

    add dumpsdata1003 to peer hosts for rsync, part 2
    Change-Id: Ic249d109c78f0c8cdd04240d73529dee9c6f1363

--- modules/profile/manifests/dumps/rsyncer_peer.pp
+++ modules/profile/manifests/dumps/rsyncer_peer.pp
@@ -5,7 +5,7 @@ class profile::dumps::rsyncer_peer(
     $group = $rsyncer_settings['dumps_group']
     $mntpoint = $rsyncer_settings['dumps_mntpoint']
 
-    $peer_hosts = 'dumpsdata1001.eqiad.wmnet dumpsdata1002.eqiad.wmnet labstore1006.wikimedia.org labstore1007.wikimedia.org'
+    $peer_hosts = 'dumpsdata1001.eqiad.wmnet dumpsdata1002.eqiad.wmnet dumpsdata1003.eqiad.wmnet labstore1006.wikimedia.org labstore1007.wikimedia.org'
 
     class {'::dumps::rsync::common':
         user  => $user,
