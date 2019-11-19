1d6dfcb051c19a304c3990fa03c6bab53cb2c0cf
Luca Toscano <ltoscano@wikimedia.org>
2019-11-19 12:08:22 +0100

    kerberos: add syslog logging to kerberos-run-command.py
    This change should add syslog logging for users that want
    to execute kerberos-run-command like the following:
    
    sudo -u analytics kerberos-run-command analytics ls -l
    
    Nov 19 11:04:00 stat1004 kerberos-run-command:
      User elukey executes as user analytics the command ['ls', '-l']
    
    The get_username() function was shamelessly copied by
    spicerack's codebase.
    
    Bug: T238306
    Change-Id: Ieb24cdba2ed11bdad941ef1c767fb98dd0fc90ad

--- modules/kerberos/files/kerberos_run_command.py
+++ modules/kerberos/files/kerberos_run_command.py
@@ -3,7 +3,16 @@
 import sys
 import socket
 import subprocess
+import logging
+import logging.handlers
 import os
+
+
+logger = logging.getLogger('kerberos-run-command')
+logger.setLevel(logging.INFO)
+handler = logging.handlers.SysLogHandler('/dev/log')
+formatter = logging.Formatter('%(name)s - %(message)s')
+logger.addHandler(handler)
 
 # Keytabs with special paths
 keytabs = {
@@ -12,37 +21,62 @@ keytabs = {
     'oozie': '/etc/security/keytabs/oozie/HTTP-oozie.keytab',
 }
 
-realm_name = ""
-with open('/etc/krb5.conf', 'r') as krbconf:
-    for line in krbconf.readlines():
-        if line.strip().startswith("default_realm"):
-            realm_name = line.strip().split("=")[1].strip()
 
-if not realm_name:
-    print("Could not detect realm name, aborting...")
-    sys.exit(1)
+def get_username():
+    """Detect and return the name of the effective running user even if run as root.
+    Returns:
+        The name of the effective running user or ``-`` if unable to detect it.
+    """
+    user = os.getenv('USER')
+    sudo_user = os.getenv('SUDO_USER')
+    if sudo_user is not None and sudo_user != 'root':
+        return sudo_user
+    if user is not None:
+        return user
+    return '-'
 
 
-if(len(sys.argv) < 3):
-    print("Expected format: kerberos-run-command user command")
-    sys.exit(1)
+def main():
+    realm_name = ""
+    with open('/etc/krb5.conf', 'r') as krbconf:
+        for line in krbconf.readlines():
+            if line.strip().startswith("default_realm"):
+                realm_name = line.strip().split("=")[1].strip()
 
-user = sys.argv[1]
-cmd = sys.argv[2:]
-fqdn = socket.getfqdn()
+    if not realm_name:
+        print("Could not detect realm name, aborting...")
+        sys.exit(1)
 
-if not keytabs.get(user, None):
-    keytab_path = "/etc/security/keytabs/{}/{}.keytab".format(user, user)
-else:
-    keytab_path = keytabs[user]
+    if(len(sys.argv) < 3):
+        print("Expected format: kerberos-run-command user command")
+        sys.exit(1)
 
-if not os.path.isfile(keytab_path):
-    print("The user keytab that you are trying to use "
-          "({}) doesn't exist or it isn't readable from your "
-          "user, aborting...".format(keytab_path))
-    sys.exit(1)
+    running_user = get_username()
 
-principal = "%s/%s@%s" % (user, fqdn, realm_name)
+    run_as_user = sys.argv[1]
+    cmd = sys.argv[2:]
+    fqdn = socket.getfqdn()
 
-subprocess.call(["/usr/bin/kinit", principal, "-k", "-t", keytab_path])
-subprocess.call(cmd)
+    if not keytabs.get(run_as_user, None):
+        keytab_path = "/etc/security/keytabs/{}/{}.keytab".format(run_as_user, run_as_user)
+    else:
+        keytab_path = keytabs[run_as_user]
+
+    if not os.path.isfile(keytab_path):
+        print("The user keytab that you are trying to use "
+              "({}) doesn't exist or it isn't readable from your "
+              "user, aborting...".format(keytab_path))
+        sys.exit(1)
+
+    principal = "%s/%s@%s" % (run_as_user, fqdn, realm_name)
+
+    logger.info(
+        "kerberos-run-command: User {} executes as user {} the command {}"
+        .format(running_user, run_as_user, cmd))
+
+    subprocess.call(["/usr/bin/kinit", principal, "-k", "-t", keytab_path])
+    subprocess.call(cmd)
+
+
+if __name__ == '__main__':
+    main()
