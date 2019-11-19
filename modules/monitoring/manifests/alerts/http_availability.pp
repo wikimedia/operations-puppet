27f379d0d835c306394d672f612a577bc8ffafea
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2019-11-19 11:31:13 +0100

    monitoring: remove nginx availability alerts
    Bug: T231627
    Change-Id: I27a4342f508c6be474d7c8d66868aabd4df05660

--- modules/monitoring/manifests/alerts/http_availability.pp
+++ modules/monitoring/manifests/alerts/http_availability.pp
@@ -17,19 +17,6 @@ define monitoring::alerts::http_availability(
         notes_link      => 'https://wikitech.wikimedia.org/wiki/Varnish#Diagnosing_Varnish_alerts',
     }
 
-    # Nginx (on Varnish hosts) HTTP availability as seen by looking at status codes
-    monitoring::check_prometheus { "nginx_${title}":
-        description     => 'Nginx has reduced HTTP availability',
-        query           => '100 * (1 - job:nginx_requests:avail2m)',
-        prometheus_url  => 'http://prometheus.svc.eqiad.wmnet/global',
-        method          => 'le',
-        retries         => 2,
-        warning         => $warning,
-        critical        => $critical,
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/frontend-traffic?panelId=4&fullscreen&refresh=1m&orgId=1'],
-        notes_link      => 'https://wikitech.wikimedia.org/wiki/Cache_TLS_termination',
-    }
-
     # ATS (on Varnish hosts) HTTP availability as seen by looking at status codes
     monitoring::check_prometheus { "ats_${title}":
         description     => 'ATS TLS has reduced HTTP availability',
