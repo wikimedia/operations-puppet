09d3b3fac86f7fe2be2fc18b3a1612734244cc54
Daniel Zahn <dzahn@wikimedia.org>
2019-11-18 20:13:00 -0500

    site/xhgui: add xhgui eqiad and codfw nodes as spares
    Bug: T238098
    Change-Id: I5fbc3e8878e37af578281cc58144be6d6e0237c2

--- manifests/site.pp
+++ manifests/site.pp
@@ -2058,6 +2058,10 @@ node /^wtp20(0[1-9]|1[0-9]|2[0-4])\.codfw\.wmnet$/ {
     role(parsoid)
 }
 
+node 'xhgui1001.eqiad.wmnet', 'xhgui2001.codfw.wmnet' {
+    role(spare::system)
+}
+
 node default {
     if $::realm == 'production' {
         include ::profile::standard
