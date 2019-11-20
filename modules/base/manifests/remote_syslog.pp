8eb8bbb639d9a775af2d1d1a33164a0e8f9d2cbb
Alex Monk <krenair@gmail.com>
2019-11-20 00:34:34 +0000

    deployment-prep: Migrate to new logstash host
    Bug: T238707
    Change-Id: I4700b3154312239e5bec212a4885f59d67557270

--- modules/base/manifests/remote_syslog.pp
+++ modules/base/manifests/remote_syslog.pp
@@ -9,7 +9,7 @@
 #
 # [*central_hosts*]
 #   A list of host (and optional port) to forward syslog events to.
-#   (e.g. ["centrallog1001.eqiad.wmnet"] or ["deployment-logstash2.deployment-prep.eqiad.wmflabs:10514"])
+#   (e.g. ["centrallog1001.eqiad.wmnet"] or ["deployment-logstash03.deployment-prep.eqiad.wmflabs:10514"])
 #
 # [*central_hosts_tls*]
 #   A list of host:port (port is *required*) to forward syslog using TLS.
--- modules/role/manifests/beta/puppetmaster.pp
+++ modules/role/manifests/beta/puppetmaster.pp
@@ -4,7 +4,7 @@
 # filtertags: labs-project-deployment-prep
 class role::beta::puppetmaster {
     class { 'puppetmaster::logstash':
-        logstash_host => 'deployment-logstash2.deployment-prep.eqiad.wmflabs',
+        logstash_host => 'deployment-logstash03.deployment-prep.eqiad.wmflabs',
         logstash_port => 5229,
     }
 }
