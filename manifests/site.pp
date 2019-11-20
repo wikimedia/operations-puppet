aac73a3bb2fd6992749ed8da7e8cb35e5ca8acd4
Emanuele Rocca <ema@wikimedia.org>
2019-11-20 12:19:08 +0100

    cache: reimage cp2012 as text_ats
    Bug: T227432
    Change-Id: Ifc51c926ca4f28d337ada824fd545e700108c0a5

--- manifests/site.pp
+++ manifests/site.pp
@@ -276,11 +276,11 @@ node /^cp10(7[68]|8[02468]|90)\.eqiad\.wmnet$/ {
     role(cache::upload)
 }
 
-node /^cp20(1[2369]|23)\.codfw\.wmnet$/ {
+node /^cp20(1[369]|23)\.codfw\.wmnet$/ {
     role(cache::text)
 }
 
-node /^cp20(0[1467]|10)\.codfw\.wmnet$/ {
+node /^cp20(0[1467]|1[02])\.codfw\.wmnet$/ {
     role(cache::text_ats)
 }
 
