c1624afb6419b23eb37b864d8ef862dd68b39c9f
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-11-20 15:36:55 +0100

    profile::mediawiki::maintenance::wikidata: use systemd logging
    Why:
      mediawiki::periodic_job handles logging properly
    
    What:
      Remove output redirection from the command.
    
    Change-Id: I12eaa275b5fdda13636aac310eeef1c197610acc

--- modules/profile/manifests/mediawiki/maintenance/wikidata.pp
+++ modules/profile/manifests/mediawiki/maintenance/wikidata.pp
@@ -78,8 +78,9 @@ class profile::mediawiki::maintenance::wikidata {
     # Update the cached query service maxlag value every minute
     # We don't need to ensure present/absent as the wrapper will ensure nothing
     # is run unless we're in the master dc
+    # Logs are saved to /var/log/mediawiki/mediawiki_job_wikidata-updateQueryServiceLag/syslog.log and properly rotated.
     profile::mediawiki::periodic_job { 'wikidata-updateQueryServiceLag':
-        command  => '/usr/local/bin/mwscript extensions/Wikidata.org/maintenance/updateQueryServiceLag.php --wiki wikidatawiki --cluster wdqs --prometheus prometheus.svc.eqiad.wmnet --prometheus prometheus.svc.codfw.wmnet >> /var/log/wikidata/updateQueryServiceLag.log 2>&1',
+        command  => '/usr/local/bin/mwscript extensions/Wikidata.org/maintenance/updateQueryServiceLag.php --wiki wikidatawiki --cluster wdqs --prometheus prometheus.svc.eqiad.wmnet --prometheus prometheus.svc.codfw.wmnet',
         interval => '*-*-* *:*:00'
     }
 }
