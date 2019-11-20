21a4c01c3c2663906048d93f216ed84214ef0220
Timo Tijhof <krinklemail@gmail.com>
2019-11-20 16:23:18 -0500

    webperf: Remove xhgui profile from webperf::profiling_tools role
    The xhgui profile is still an incomplete work in progress, and not
    applied in production. This was added here in the past for testing
    in Beta Cluster ahead of prod.
    
    We've made some progress since then but it is still incomplete.
    
    Meanwhile, the plan for prod has changed. XHGui will not be installed
    on the webperf1002 (profiling_tools) host, but instead on a new
    xhgui1001 host.
    
    In order for beta's webperf13 to be like webperf1002 in prod,
    remove the xhgui profile from it (again).
    
    Bug: T180761
    Bug: T238788
    Change-Id: Idccfdf0968f5b28192c77fb90da0d6eef5903054

--- modules/role/manifests/webperf/profiling_tools.pp
+++ modules/role/manifests/webperf/profiling_tools.pp
@@ -13,17 +13,14 @@ class role::webperf::profiling_tools {
     include ::profile::base::firewall
     include ::profile::backup::host
     include ::profile::webperf::arclamp
-    include ::profile::webperf::xhgui
 
     # class httpd installs mpm_event by default, and once installed,
-    # it cannot easily be uninstalled. The xhgui profile installs
-    # libapache2-mod-php7.0 which in turn installs mpm_prefork, which
-    # fails if mpm_event is present. (T180761)
+    # it cannot easily be uninstalled.
     class { '::httpd::mpm':
         mpm => 'prefork'
     }
 
-    # Web server (for xhgui, and arclamp)
+    # Web server (for arclamp)
     class { '::httpd':
         modules => ['authnz_ldap', 'php7.0', 'rewrite', 'mime', 'proxy', 'proxy_http'],
     }
