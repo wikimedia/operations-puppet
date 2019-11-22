c1a336b414b58eb791220ed8bab903f95c0d3070
John Bond <jbond@wikimedia.org>
2019-11-22 15:32:30 +0100

    profile::idp::client::httpd: refactor
    Change-Id: I94dc57b50d21c0442c1f35cb55f309013af043dc

--- modules/profile/manifests/idp/client/httpd.pp
+++ modules/profile/manifests/idp/client/httpd.pp
@@ -2,6 +2,7 @@ class profile::idp::client::httpd (
     String[1]                     $acme_chief_cert  = lookup('profile::idp::client::httpd::acme_chief_cert'),
     String[1]                     $vhost_content    = lookup('profile::idp::client::httpd::vhost_content'),
     Stdlib::Host                  $virtual_host     = lookup('profile::idp::client::httpd::virtual_host'),
+    Stdlib::Unixpath              $document_root    = lookup('profile::idp::client::httpd::document_root'),
     Stdlib::Unixpath              $cookie_path      = lookup('profile::idp::client::httpd::cookie_path'),
     Stdlib::Unixpath              $certificate_path = lookup('profile::idp::client::httpd::certificate_path'),
     Hash[String, Stdlib::HTTPUrl] $apereo_cas       = lookup('profile::idp::client::httpd::apereo_cas'),
@@ -14,7 +15,29 @@ class profile::idp::client::httpd (
 ) {
     ensure_packages(['libapache2-mod-auth-cas'])
 
+    $_cookie_path = $cookie_path[-1] ? {
+        '/'     => $cookie_path,
+        default => "${cookie_path}/",
+    }
     $ssl_settings = ssl_ciphersuite('apache', 'strong', true)
+    $cas_settings = {
+        'CASVersion'         => 2,
+        'CASCertificatePath' => $certificate_path,
+        'CASCookiePath'      => $_cookie_path,
+        'CASLoginURL'        => $apereo_cas['login_url'],
+        'CASValidateURL'     => $apereo_cas['validate_url'],
+        'CASAttributePrefix' => $attribute_prefix,
+        'CASDebug'           => $debug ? { true => 'On', default => 'Off' },
+    }
+    $cas_base_auth = {
+        'AuthType'       => 'CAS',
+        'CASAuthNHeader' => $authn_header,
+    }
+    $cas_auth_require = $required_groups.empty? {
+        true    => ['valid-user' ],
+        default => $required_groups.map |$group| { "cas-attribute memberOf:${group}" },
+    }
+    $cas_auth_settings = merge($cas_base_auth, {'Require' => $cas_auth_require})
 
     acme_chief::cert { $acme_chief_cert:
         puppet_svc => 'apache2',
