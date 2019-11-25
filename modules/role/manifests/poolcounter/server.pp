e351a3879a98528f4cc69919ed37cd644468c813
Reuven Lazarus <rlazarus@wikimedia.org>
2019-11-25 17:43:59 -0500

    poolcounter: Refactor: In role::poolcounter::server, use profile::poolcounter.
    Previously this pulled in ::poolcounter directly, and duplicated the
    rest of what's in the profile.
    
    Change-Id: Ie2b127241a7261680cd70ebfcc52bfcf00882606

--- modules/role/manifests/poolcounter/server.pp
+++ modules/role/manifests/poolcounter/server.pp
@@ -1,32 +1,10 @@
 # filtertags: labs-project-deployment-prep
 class role::poolcounter::server {
     include ::profile::standard
-    include ::poolcounter
+    include ::profile::poolcounter
     include ::profile::base::firewall
 
     system::role { 'poolcounter':
         description => 'PoolCounter server',
-    }
-
-    # Process running
-    nrpe::monitor_service { 'poolcounterd':
-        description  => 'poolcounter',
-        nrpe_command => '/usr/lib/nagios/plugins/check_procs -w 1:1 -c 1:3 -C poolcounterd',
-        notes_url    => 'https://www.mediawiki.org/wiki/PoolCounter',
-    }
-
-    # TCP port 7531 reacheable
-    monitoring::service { 'poolcounterd_port_7531':
-        description   => 'Poolcounter connection',
-        check_command => 'check_tcp!7531',
-        notes_url     => 'https://www.mediawiki.org/wiki/PoolCounter',
-    }
-
-    # firewalling
-    ferm::service { 'poolcounterd':
-        proto   => 'tcp',
-        port    => '7531',
-        srange  => '$DOMAIN_NETWORKS',
-        notrack => true,
     }
 }
