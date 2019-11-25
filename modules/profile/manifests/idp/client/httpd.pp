8629c61f5d2b402c105182c009e99c261ff4aa76
John Bond <jbond@wikimedia.org>
2019-11-25 17:37:15 +0100

    puppetboard: add proxied_as parameter
    puppet board lives behind envoy as such we need to add the proxied_as
    parameter.
    
    PCC: https://puppet-compiler.wmflabs.org/compiler1003/19594/
    
    Bug: T238924
    Change-Id: I3d85d56cedbe29633d8f29d930745dad8486869e

--- modules/profile/manifests/idp/client/httpd.pp
+++ modules/profile/manifests/idp/client/httpd.pp
@@ -1,3 +1,17 @@
+# @summary configure an apache web site using mod_auth_cas
+# @param vhost_content an erb template file to use for the apache vhost configuration
+# @param virtual_host the virtual host to use in the apache vhost
+# @param document_root the document root to configure for the apache vhost
+# @param cookie_path The location where cas stores information relating to authentication cookies issued
+# @param certificate_path the SSL certificate path used for validation
+# @param apereo_cas hash holding the login and validation
+# @param authn_header The prefix to use when adding CAS or SAML attributes to the HTTP headers
+# @param debug Enable cas debug
+# @param apache_owner The user apache runs as
+# @param apache_group The group apache runs as
+# @param required_groups An array of LDAP groups allowed to access this resource
+# @param acme_cheif_cert the name of the acme chief certificate to use
+# @param proxied_as This URL represents the URL that end users may see in the event that Apache server is proxied
 class profile::idp::client::httpd (
     String[1]                     $vhost_content    = lookup('profile::idp::client::httpd::vhost_content'),
     Stdlib::Host                  $virtual_host     = lookup('profile::idp::client::httpd::virtual_host'),
@@ -12,6 +26,8 @@ class profile::idp::client::httpd (
     String[1]                     $apache_group     = lookup('profile::idp::client::httpd::apache_group'),
     Optional[Array[String[1]]]    $required_groups  = lookup('profile::idp::client::httpd::required_groups'),
     Optional[String[1]]           $acme_chief_cert  = lookup('profile::idp::client::httpd::acme_chief_cert',
+                                                            {'default_value' => undef}),
+    Optional[String[1]]           $proxied_as       = lookup('profile::idp::client::httpd::proxied_as',
                                                             {'default_value' => undef}),
 ) {
     ensure_packages(['libapache2-mod-auth-cas'])
@@ -29,6 +45,7 @@ class profile::idp::client::httpd (
         'CASValidateURL'     => $apereo_cas['validate_url'],
         'CASAttributePrefix' => $attribute_prefix,
         'CASDebug'           => $debug ? { true => 'On', default => 'Off' },
+        'CASRootProxiedAs'   => $proxied_as,
     }
     $cas_base_auth = {
         'AuthType'       => 'CAS',
