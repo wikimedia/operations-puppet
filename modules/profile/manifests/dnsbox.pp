7d3d4bcd4a96c1af7d1208b606b61d9e9b733ef5
Brandon Black <bblack@wikimedia.org>
2019-11-26 20:26:25 +0000

    Draft fully-blended role
    Change-Id: Ib29521e4a868962a12d4af6b2dfed35f0b6080c0

--- modules/profile/manifests/dnsbox.pp
+++ modules/profile/manifests/dnsbox.pp
@@ -1,8 +1,16 @@
 # Combo profile for configuring production dnsN00x machines in a combined role
-# using both profile::dns::recursor and profile::ntp (and in the future,
-# profile::dns::auth as well, plus a little bit of inter-service glue)
-class profile::dnsbox {
+# for recursive DNS, authoritative DNS, and NTP.
+class profile::dnsbox(
+    Boolean $include_auth = lookup('profile::dnsbox::include_auth', {default_value => false}),
+) {
     include ::profile::standard
     include ::profile::dns::recursor
     include ::profile::ntp
+
+    # The $include_auth mechanism is temporary as we progressively test and
+    # roll out this setup to the dnsbox fleet.
+    if $include_auth {
+        include ::profile::dns::auth
+        # XXX also need to put some minimal glue here, in the form of puppet and systemd dependencies, to ensure that the recdns service requires the gdnsd service to happen first.  At the systemd layer this will probably be with add-in fragments defining the runtime relationship...
+    }
 }
