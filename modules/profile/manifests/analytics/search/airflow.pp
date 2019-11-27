e4af2d62b8a884a1a23c8d1b0a806e9909a171a2
Daniel Zahn <dzahn@wikimedia.org>
2019-11-27 13:11:45 -0500

    airflow: move parameters, use lookup, style changes
    Bug: T236180
    Change-Id: I019ee94572326cc383c2e9a51bcdf0647ba8b92b

--- modules/profile/manifests/analytics/search/airflow.pp
+++ modules/profile/manifests/analytics/search/airflow.pp
@@ -4,9 +4,18 @@
 # in the analytics cluster.
 #
 class profile::analytics::search::airflow(
-    $mysql_host = hiera('profile::analytics::search::airflow::mysql_host'),
-    $webserver_port = hiera('profile::analytics::search::airflow::webserver_port', 8778),
-    $db = hiera('profile::analytics::search::airflow::db', 'search_airflow')
+    Stdlib::Port $webserver_port  = lookup('profile::analytics::search::airflow::webserver_port'),
+    Stdlib::Fqdn $mysql_host      = lookup('profile::analytics::search::airflow::mysql_host'),
+    String $db_name               = lookup('profile::analytics::search::airflow::db_name'),
+    String $deploy_target         = lookup('profile::analytics::search::airflow::deploy_target'),
+    String $deploy_target_plugins = lookup('profile::analytics::search::airflow::deploy_target_plugins'),
+    String $deploy_user           = lookup('profile::analytics::search::airflow::deploy_user'),
+    Stdlib::Unixpath $deploy_dir  = lookup('profile::analytics::search::airflow::deploy_dir'),
+    Stdlib::Unixpath $airflow_dir = lookup('profile::analytics::search::airflow::airflow_dir'),
+    Stdlib::Unixpath $log_dir     = lookup('profile::analytics::search::airflow::log_dir'),
+    Stdlib::Unixpath $pid_dir     = lookup('profile::analytics::search::airflow::pid_dir'),
+    Stdlib::Unixpath $conf_dir    = lookup('profile::analytics::search::airflow::conf_dir'),
+    Stdlib::Unixpath $conf_file   = lookup('profile::analytics::search::airflow::conf_file'),
 ) {
     include ::passwords::mysql::airflow::search
 
@@ -32,48 +41,40 @@ class profile::analytics::search::airflow(
         require    => Group['airflow'],
     }
 
-    $target = 'search/airflow'
-    $deploydir = "/srv/deployment/${target}"
-    $airflowdir = '/srv/deployment/wikimedia/search/analytics/airflow'
-    $logdir = '/var/log/airflow'
-    $piddir = '/var/run/airflow'
 
     # Deploy upstream airflow code + dependencies
-    scap::target { $target:
-        deploy_user => 'deploy-service',
+    scap::target { $deploy_target:
+        deploy_user => $deploy_user,
     }
 
     # Deploy dags + plugins
-    scap::target { 'wikimedia/discovery/analytics':
-        deploy_user => 'deploy-service',
+    scap::target { $deploy_target_plugins:
+        deploy_user => $deploy_user,
     }
 
-    file { '/etc/airflow':
+    file { $conf_dir:
         ensure => 'directory',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
     }
 
-    $sql_user = $::passwords::mysql::airflow::search::user # lint:ignore:wmf_styleguide
-    $sql_pass = $::passwords::mysql::airflow::search::password # lint:ignore:wmf_styleguide
-    $sql_alchemy_conn = "mysql://${sql_user}:${sql_pass}@${mysql_host}/${db}"
+    $sql_user = $::passwords::mysql::airflow::search::user
+    $sql_pass = $::passwords::mysql::airflow::search::password
+    $sql_alchemy_conn = "mysql://${sql_user}:${sql_pass}@${mysql_host}/${db_name}"
 
-    file { '/etc/airflow/airflow.cfg':
+    file { "${conf_dir}/${conf_file}":
         ensure  => present,
         # Since this stores passwords limit read access
         owner   => 'root',
         group   => 'airflow',
         mode    => '0440',
         content => template('profile/analytics/search/airflow/airflow.cfg.erb'),
-        require => [
-            File['/etc/airflow'],
-            Group['airflow'],
-        ]
+        require => Group['airflow'],
     }
 
-    # Ensure places the daemons will write are available
-    file { [$logdir, $piddir]:
+    # Ensure places the daemons will write to are available.
+    file { [$log_dir, $pid_dir]:
         ensure => 'directory',
         owner  => 'airflow',
         group  => 'airflow',
@@ -82,13 +83,15 @@ class profile::analytics::search::airflow(
 
     systemd::service { 'airflow-webserver':
         content => template('profile/analytics/search/airflow/webserver.service.erb'),
-        require => File[$logdir, $piddir, '/etc/airflow/airflow.cfg']
+        require => File[$log_dir, $pid_dir, "${conf_dir}/${conf_file}"],
     }
+
     base::service_auto_restart { 'airflow-webserver': }
 
     systemd::service { 'airflow-scheduler':
         content => template('profile/analytics/search/airflow/scheduler.service.erb'),
-        require => File[$logdir, $piddir, '/etc/airflow/airflow.cfg']
+        require => File[$log_dir, $pid_dir, "${conf_dir}/${conf_file}"],
     }
+
     base::service_auto_restart { 'airflow-scheduler': }
 }
