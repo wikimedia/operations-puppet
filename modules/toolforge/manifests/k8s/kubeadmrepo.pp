abba1a023e3bd808ef30f0e77d24a199a1079867
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-12-02 15:32:30 +0100

    toolforge: new k8s: introduce apt pinning configurations
    We need to make sure relevant packages for the k8s deployment stay in the
    version we want.
    
    Bug: T239409
    Change-Id: I18c70e758b39f701526e3c60bb732d690a45e77d
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/toolforge/manifests/k8s/kubeadmrepo.pp
+++ modules/toolforge/manifests/k8s/kubeadmrepo.pp
@@ -1,5 +1,37 @@
 class toolforge::k8s::kubeadmrepo(
 ) {
+
+    # concrete package versions present in the repo are defined in
+    # modules/aptrepo/files/updates (search for thirdparty/kubeadm-k8s)
+    # we may keep different versions in the repo for upgrading/testing
+    # purposes, so instruct apt to stick to the versions we want (cannot use
+    # the trick of blindly installing what is in the repo)
+    apt::pin { 'toolforge-k8s-kubeadmrepo-core':
+        package  => 'kubeadm kubelet kubectl',
+        pin      => 'version 1.15.6-00',
+        priority => '1001',
+    }
+    apt::pin { 'toolforge-k8s-kubeadmrepo-kubernetes-cni':
+        package  => 'kubernetes-cni',
+        pin      => 'version 0.7.5-00',
+        priority => '1001',
+    }
+    apt::pin { 'toolforge-k8s-kubeadmrepo-cri-tools':
+        package  => 'cri-tools',
+        pin      => 'version 1.13.0-00',
+        priority => '1001',
+    }
+    apt::pin { 'toolforge-k8s-kubeadmrepo-docker':
+        package  => 'docker-ce docker-ce-cli',
+        pin      => 'version 5:19.03.5~3-0~debian-stretch',
+        priority => '1001',
+    }
+    apt::pin { 'toolforge-k8s-kubeadmrepo-containerd':
+        package  => 'containerd.io',
+        pin      => 'version 1.2.10-3',
+        priority => '1001',
+    }
+
     apt::repository { 'toolforge-k8s-kubeadmrepo':
         uri        => 'http://apt.wikimedia.org/wikimedia/',
         dist       => 'buster-wikimedia',
