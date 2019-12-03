f544ce19fcb3590eada9576e46523e50e90ecfcf
Jaime Crespo <jcrespo@wikimedia.org>
2019-12-03 14:35:56 +0100

    database-backups: Use a different pool for database backups
    Until now, database backups (only active on dbprov2001 and
    dbprov1001 for now), were sent to the production pool alongside
    all other production backups.
    
    At Ibb35ef34e19be4948, a new pool was configured to allow a
    dedicated database backups pool. Use it for database backups
    by configuring a specific job defaults.
    
    Wednesday was chosen for full backups because dumps happen on
    Tuesday, preventing dumps and bacula jobs to happen at the
    same time for load reasons, but running them shortly after,
    to send them to long term storage (although none of this is
    really required).
    
    Bug: T238048
    Change-Id: I3ab3a223d0ed39e7fc454c4e1ff0328e5aca900c

--- modules/profile/manifests/mariadb/backup/bacula.pp
+++ modules/profile/manifests/mariadb/backup/bacula.pp
@@ -4,6 +4,8 @@ class profile::mariadb::backup::bacula (
     $active = hiera('profile::mariadb::backup::bacula::active'),
 ) {
     if $active {
-        backup::set { 'mysql-srv-backups-dumps-latest': }
+        backup::set { 'mysql-srv-backups-dumps-latest':
+            jobdefaults => 'Monthly-1st-Wed-databases',
+        }
     }
 }
