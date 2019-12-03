abbc219e1322f76193af53a9d1051b840e6f8d2c
Manuel Arostegui <marostegui@wikimedia.org>
2019-12-03 06:59:35 +0100

    mariadb: Set db2065 to spare
    db2065 will be decommissioned
    
    Bug: T239046
    Change-Id: I67fc344e75638ca3f82c8c8fdff5845ee428b385

--- manifests/site.pp
+++ manifests/site.pp
@@ -609,9 +609,9 @@ node 'db1128.eqiad.wmnet' {
 
 
 # codfw
-# db2065 will be decommissioned T228258
+# db2065 will be decommissioned T239046
 node 'db2065.codfw.wmnet' {
-    role(mariadb::misc::phabricator)
+    role(spare::system)
 }
 
 node 'db2134.codfw.wmnet' {
