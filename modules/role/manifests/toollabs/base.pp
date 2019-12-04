a245b0dd9531356498bdacfe6a018561ff080e82
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-12-04 14:43:21 +0100

    role: toollabs: drop unused role classes
    No longer in use, deprecated. No VM uses this role any longer.
    
    The following roles are dropped:
    
    * role::toollabs::k8s::bastion
    * role::toollabs::master
    * role::toollabs::base
    
    Change-Id: Iada5a34fec5a24acdace1f89071fc9e996653b2a
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/role/manifests/toollabs/base.pp
+++
@@ -1,7 +0,0 @@
-class role::toollabs::base {
-    include ::toollabs::base
-
-    system::role { 'toollabs::base':
-        description => 'server part of the Toolforge cluster'
-    }
-}
--- modules/role/manifests/toollabs/master.pp
+++
@@ -1,6 +0,0 @@
-# filtertags: labs-project-tools
-class role::toollabs::master {
-    include ::toollabs::master
-
-    system::role { 'toollabs::master': description => 'Toolforge gridengine master' }
-}
