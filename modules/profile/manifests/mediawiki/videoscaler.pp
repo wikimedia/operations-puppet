f32f8261b172dfb94c5334f18fb4baef33b70411
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2019-12-05 10:31:16 +0100

    Extend pinning for ffmpeg with libav libraries
    Change-Id: I886a0fd777ab589f7f8a425c2683bef6ee9374e7

--- modules/profile/manifests/mediawiki/videoscaler.pp
+++ modules/profile/manifests/mediawiki/videoscaler.pp
@@ -10,10 +10,17 @@ class profile::mediawiki::videoscaler()
         components => 'component/vp9',
     }
 
+    # ffmpeg has a dependency on the base version of the release, so e.g. (>= 7:3.2.14)
+    # This isn't sufficient it make apt pull in our patched version, so explicitly add
+    # pinning for the various ffmpeg libraries as well
+
+    $ffmpeg_packages = ['ffmpeg', 'libavcodec57', 'libavdevice57', 'libavfilter6', 'libavformat57',
+                        'libavresample3', 'libpostproc54', 'libswresample2', 'libavutil55', 'libswscale4']
+
     apt::pin { 'ffmpeg-vp9-stretch':
         pin      => 'release c=component/vp9',
         priority => '1001',
-        before   => Package['ffmpeg'],
+        before   => Package[$ffmpeg_packages],
     }
 
     package { 'ffmpeg':
