8d7b758b1e22cac984feee8dbc9387dabd2e7d64
Brandon Black <bblack@wikimedia.org>
2019-12-09 23:06:20 +0000

    dnsbox: replace glue with bind_service
    Bug: T240285
    Change-Id: I1d79bdf4c39fd566a076cd417c1f68bde207e9e9

--- modules/profile/manifests/dnsbox.pp
+++ modules/profile/manifests/dnsbox.pp
@@ -2,29 +2,12 @@
 # for recursive DNS, authoritative DNS, and NTP.
 class profile::dnsbox() {
     include ::profile::ntp
-    include ::profile::dns::recursor
     include ::profile::dns::auth
-
+    class { '::profile::dns::recursor':
+        # This glues pdns-recursor to gdnsd at the systemd level
+        bind_service => 'gdnsd.service',
+    }
     # This is the puppet-level glue, to ensure that it operates on these
     # services in the appropriate order to avoid unnecessary mayhem.
     Service['gdnsd'] -> Service['pdns-recursor']
-
-    # This is the systemd-level glue to ensure pdns-recursor cannot be
-    # running unless gdnsd is already running
-    $sysd_glue = '/etc/systemd/system/pdns-recursor.service.d/rec-needs-auth.conf'
-    file { $sysd_glue:
-        ensure => present,
-        mode   => '0444',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/profile/dnsbox/rec-needs-auth.conf',
-    }
-
-    exec { 'systemd reload for rec-needs-auth glue':
-        refreshonly => true,
-        command     => '/bin/systemctl daemon-reload',
-        subscribe   => File[$sysd_glue],
-        before      => Service['pdns-recursor'],
-        require     => Service['gdnsd'],
-    }
 }
