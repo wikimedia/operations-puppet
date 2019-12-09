0f8a962c650ef0fd78f344b61d3f1a5aacb5b11b
Effie Mouzeli <effie@wikimedia.org>
2019-12-09 16:29:42 +0200

    mediawiki: Check APCu fragmentation in php-check-and-restart.sh
    We were able to verify that, if a MediaWiki server has 100%
    APCu fragmentation, a potential deployment (eg scap-sync),
    can severely impact performance.
    
    One way to mitigate this is to periodically check the APCu
    fragmentation percentage and free apcu cache when it reaches
    95%
    
    Scap is using the same script to check opcache status, so it
    makes sense to bundle the APCu checks here as well
    
    Bug: T240205
    Change-Id: I41af9703b6205dda9a9c303e969203cff4b059fc

--- modules/profile/files/mediawiki/php/php-check-and-restart.sh
+++ modules/profile/files/mediawiki/php/php-check-and-restart.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 set -euo pipefail
 # Daily cronjob to restart php-fpm if the free opcache is below a certain level.
+# This script is also run by scap during runs as it is specified in
+# modules/scap/templates/scap.cfg.erb
+
+# Due to observations where 100% APCu fragmentation can cause perf degradation
+# (T240205), we can make an additional check about its status. If APCu
+# fragmenation is over 95%, we will issue a /apcu-free command
+
 # Service name
 SERVICE="$1"
 # Minimum opcache free MB.
-MIN_FREE="$2"
+MIN_OPCACHE_FREE="$2"
+OPCACHE_FREE=$(php7adm /opcache-info | jq .memory_usage.free_memory/1024/1024 2>&1)
+OPCACHE_FREE_MB=${OPCACHE_FREE%.*}
 
-FREE=$(php7adm /opcache-info | jq .memory_usage.free_memory/1024/1024 2>&1)
-FREE_MB=${FREE%.*}
-if [ "$MIN_FREE" -ge "$FREE_MB" ]; then
-    echo "Restarting ${SERVICE}: free opcache $FREE_MB MB"
-   "/usr/local/sbin/restart-$SERVICE"
+if [ "$MIN_OPCACHE_FREE" -ge "$OPCACHE_FREE_MB" ]; then
+    echo "Restarting ${SERVICE}: free opcache $OPCACHE_FREE_MB MB"
+    "/usr/local/sbin/restart-$SERVICE"
+    exit 0
 else
-    echo "NOT restarting ${SERVICE}: free opcache $FREE_MB MB"
+    echo "NOT restarting ${SERVICE}: free opcache $OPCACHE_FREE_MB MB"
+fi
+
+APCU_FRAGMENTATION_LIMIT='95' # percentage
+APCU_FRAGMENTATION=$(php7adm /apcu-frag |jq .fragmentation 2>&1)
+
+if [ "$APCU_FRAGMENTATION" -ge "$APCU_FRAGMENTATION_LIMIT" ]; then
+    echo "Fragmentation is at ${$APCU_FRAGMENTATION}%, freeing APCu cache"
+    "php7adm /apcu-free"
+else
+    echo "Fragmentation is at ${$APCU_FRAGMENTATION}%, nothing to do here"
 fi
