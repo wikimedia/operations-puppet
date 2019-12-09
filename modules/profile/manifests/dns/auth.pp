2017d4084a3d78429911c0abd158420dc819998b
Brandon Black <bblack@wikimedia.org>
2019-12-09 21:56:49 +0000

    authdns: remove host:53 monitor listener
    We're doing icinga monitoring via port 5353 now, as this makes it
    compatible with blended roles.
    
    Bug: T240285
    Change-Id: Ie19daaa59b189fd58f262613e2fac9f02513cb95

--- modules/profile/manifests/dns/auth.pp
+++ modules/profile/manifests/dns/auth.pp
@@ -4,7 +4,6 @@ class profile::dns::auth (
     Hash[String, Hash[String, String]] $authdns_addrs = lookup('authdns_addrs'),
     Hash[Stdlib::Fqdn, Stdlib::IP::Address::Nosubnet] $authdns_servers = lookup('authdns_servers'),
     Stdlib::HTTPSUrl $gitrepo = lookup('profile::dns::auth::gitrepo'),
-    Boolean $mon_host53 = lookup('profile::dns::auth::mon_host53', {default_value => false}),
     $conftool_prefix = lookup('conftool_prefix'),
 ) {
     include ::profile::dns::auth::acmechief_target
@@ -36,13 +35,7 @@ class profile::dns::auth (
 
     $service_listeners = $authdns_addrs.map |$aspec| { $aspec[1]['address'] }
 
-    # Port 53 monitoring on the host-level IPs, for legacy for now
-    $host53_array = $mon_host53 ? {
-        true    => [$::ipaddress, $::ipaddress6],
-        default => [],
-    }
-
-    $monitor_listeners = $host53_array + [
+    $monitor_listeners = [
         # Any-address, both protocols, port 5353, for blended-role monitoring
         '0.0.0.0:5353',
         '[::]:5353',
