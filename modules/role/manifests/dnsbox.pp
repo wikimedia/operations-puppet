bb43c97c15ae6dd90b9107a7f011eb1d0ebfe0dd
Brandon Black <bblack@wikimedia.org>
2019-12-09 23:09:02 +0000

    dnsbox: eliminate extra profile layer
    Bug: T240285
    Change-Id: I2dde1135f6aefc6d4542b657f06166774ee033e6

--- modules/role/manifests/dnsbox.pp
+++ modules/role/manifests/dnsbox.pp
@@ -1,6 +1,16 @@
+# Combo role for configuring production dnsN00x machines with recursive DNS,
+# authoritative DNS, and NTP.
 class role::dnsbox {
     system::role { 'dnsbox': description => 'DNS/NTP Site Infra Server' }
 
     include ::profile::standard
-    include ::profile::dnsbox
+    include ::profile::ntp
+    include ::profile::dns::auth
+    class { '::profile::dns::recursor':
+        # This glues pdns-recursor to gdnsd at the systemd level
+        bind_service => 'gdnsd.service',
+    }
+    # This is the puppet-level glue, to ensure that it operates on these
+    # services in the appropriate order to avoid unnecessary mayhem.
+    Service['gdnsd'] -> Service['pdns-recursor']
 }
