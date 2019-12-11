7ca6231c1d50e7fdb701ec27e01e1e7933b99689
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2019-12-11 14:06:09 +0100

    toolforge: k8s: metrics: include some hints and comments
    Include some hints and comments about the required steps to make this stuff
    work. This will be also documented in wikitech anyway.
    
    Bug: T237643
    Bug: T240402
    Change-Id: Id4ccf4ad209d74773b5ac51f86866f0b86201f9e
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/toolforge/manifests/k8s/prometheus_metrics_yaml.pp
+++ modules/toolforge/manifests/k8s/prometheus_metrics_yaml.pp
@@ -2,12 +2,24 @@ class toolforge::k8s::prometheus_metrics_yaml(
 ) {
     require ::toolforge::k8s::kubeadm # because /etc/kubernetes
 
+    # for this to work you need to generate a x509 cert using the admin script
+    # the generated certs should be then moved as secrets to the private repo
+    # in a control node:
+    #   root:~# wmcs-k8s-get-cert prometheus
+    #
+    # See also: profile::toolforge::prometheus
+    # See also: https://wikitech.wikimedia.org/wiki/Portal:Toolforge/Admin/Deploying_k8s
     file { '/etc/kubernetes/prometheus_metrics.yaml':
         ensure  => present,
         source  => 'puppet:///modules/toolforge/k8s/prometheus_metrics.yaml',
         require => File['/etc/kubernetes'],
     }
 
+    # for this to work you need to generate a x509 cert and create a k8s secret
+    # in a control node:
+    #   root:~# wmcs-k8s-secret-for-cert -n kube-system -s metrics-server-certs -a metrics-server
+    #
+    # See also: https://wikitech.wikimedia.org/wiki/Portal:Toolforge/Admin/Deploying_k8s
     file { '/etc/kubernetes/metrics-server.yaml':
         ensure  => present,
         source  => 'puppet:///modules/toolforge/k8s/metrics-server.yaml',
