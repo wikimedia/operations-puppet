c8fb81c7bab0c8c0c477804dc2d2cb6c0cc8867b
bstorm <bstorm@wikimedia.org>
2019-12-12 12:38:32 -0700

    toolforge-k8s: harden the kubelet a bit
    Add cipher suite settings, redundantly and explicitly close the read-only
    port and protect kernel defaults the kubelet expects.
    
    Bug: T240009
    Change-Id: I7e7d45704e11225b866da5301767ab8ce3d0ec93

--- modules/toolforge/manifests/k8s/kubeadm.pp
+++ modules/toolforge/manifests/k8s/kubeadm.pp
@@ -26,6 +26,16 @@ class toolforge::k8s::kubeadm(
     file { '/etc/default/kubelet':
         ensure  => 'present',
         mode    => '0444',
-        content => 'KUBELET_EXTRA_ARGS="--pod-infra-container-image=docker-registry.tools.wmflabs.org/pause:3.1"'
+        content => @(ARGS/L),
+        KUBELET_EXTRA_ARGS="--read-only-port=0 --protect-kernel-defaults=true\
+        --tls-cipher-suites=TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE\
+        _RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,\
+        TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,\
+        TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,\
+        TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,\
+        TLS_RSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_128_GCM_SHA256 \
+        --pod-infra-container-image=docker-registry.tools.wmflabs.org/pause:3.1\
+        "
+        |-ARGS
     }
 }
