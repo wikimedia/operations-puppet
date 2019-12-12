69864892ea07964c6fa75330adaa8f55bcfdbfad
bstorm <bstorm@wikimedia.org>
2019-12-12 11:53:30 -0700

    toolforge-k8s: harden the init config a bit
    This enables event rate limiting, node restrictions, saner garbage
    collection for our servers and disables profiling all around. This
    should preserve good performance and reduce risk of a few (un)intentional
    DoS options in the cluster. Also specify a set of cipher suites.
    
    This is the live configuration on toolsbeta-test-k8s-control-1
    
    Bug: T240009
    Change-Id: Ic655fb8530f566953fa5b69e69cdcdfbf6820349

--- modules/toolforge/manifests/k8s/kubeadm_init_yaml.pp
+++ modules/toolforge/manifests/k8s/kubeadm_init_yaml.pp
@@ -43,4 +43,29 @@ class toolforge::k8s::kubeadm_init_yaml(
         mode    => '0400',
         require => File['/etc/kubernetes'],
     }
+
+    file { '/etc/kubernetes/admission':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        require => File['/etc/kubernetes'],
+    }
+
+    file { '/etc/kubernetes/admission/admission.yaml':
+        ensure  => present,
+        source  => 'puppet:///modules/toolforge/k8s/admission.yaml',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        require => File['/etc/kubernetes/admission'],
+    }
+
+    file { '/etc/kubernetes/admission/eventconfig.yaml':
+        ensure  => present,
+        source  => 'puppet:///modules/toolforge/k8s/eventconfig.yaml',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        require => File['/etc/kubernetes/admission'],
+    }
 }
