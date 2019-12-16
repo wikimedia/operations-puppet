2aef2b0207b2c235af2dd33fbb1a664bb515e495
Bryan Davis <bd808@wikimedia.org>
2019-12-16 10:22:14 -0700

    cloud: add ::apparmor dependency to ::profile::wmcs::mediawiki_vagrant
    LXC needs apparmour to work as expected. Something has changed either in
    our Cloud VPS base image build or in the packages we are applying via
    Puppet that caused this dependency to be missed. Making the dependency
    explicit in Puppet should correct the issues that folks are seeing when
    applying ::role::labs::mediawiki_vagrant.
    
    Change also updates the coding style of profile::wmcs::mediawiki_vagrant
    to current standards.
    
    Bug: T240875
    Change-Id: I2faba681c6a458cc279e9a16fc04a8c43bd99a63

--- modules/profile/manifests/wmcs/mediawiki_vagrant.pp
+++ modules/profile/manifests/wmcs/mediawiki_vagrant.pp
@@ -1,6 +1,7 @@
 class profile::wmcs::mediawiki_vagrant(
 ) {
-    include ::vagrant
-    include ::vagrant::lxc
-    include ::vagrant::mediawiki
+    class { '::apparmor': }
+    class { '::vagrant': }
+    class { '::vagrant::lxc': }
+    class { '::vagrant::mediawiki': }
 }
