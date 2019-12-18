22839c3521fc6194a617d26f330fed037824db85
Jason Hedden <jhedden@wikimedia.org>
2019-12-18 09:51:15 -0600

    lvs ceph: add cloudceph service and cluster
    The cloudceph-stats service will serve as an endpoint for the prometheus
    exporter running in active/standby across the cloudcephmon100[1-3] servers.
    
    Bug: T240715
    Change-Id: I9c3208308da5d855b4f76127fabcc8d565284c9d

--- modules/role/manifests/wmcs/ceph/mon.pp
+++ modules/role/manifests/wmcs/ceph/mon.pp
@@ -2,5 +2,6 @@ class role::wmcs::ceph::mon {
     system::role { $name: }
     include ::profile::standard
     include ::profile::base::firewall
+    include ::profile::lvs::realserver
     include ::profile::ceph::mon
 }
