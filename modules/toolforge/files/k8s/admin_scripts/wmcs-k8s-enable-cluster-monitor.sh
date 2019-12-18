4299425647d507d6d051e526b8d2b2d64f420d86
bstorm <bstorm@wikimedia.org>
2019-12-18 14:37:50 -0700

    toolforge-k8s: add a script to grant "observer" access to a tool
    This grants a particular tool/namespace the usage of a service account
    that can view just about every non-secret resource in the cluster outside
    of RBAC and security policy. That should be plenty sufficient for
    monitoring and information-gathering tools intended to inform Toolforge
    users and admins.
    
    Bug: T233372
    Change-Id: I6ef66d470bbb2b64e9757f5c9a5b7d90d2504391

---
+++ modules/toolforge/files/k8s/admin_scripts/wmcs-k8s-enable-cluster-monitor.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# Run this script with your root/cluster admin account as appropriate.
+
+set -Eeuo pipefail
+
+function usage {
+        echo -e "Usage (must be cluster-admin or similar):\n"
+        echo "wmcs-enable-cluster-monitor.sh <tool-name>"
+        echo ""
+        echo "Example: wmcs-enable-cluster-monitor.sh observer-tool"
+}
+if [ $# -eq 0 ] || [ $# -gt 1 ]; then
+    usage
+    exit 1
+fi
+ARGL=$(echo $1 | wc -c)
+if [ $ARGL -gt 58 ]; then
+    echo "Name too long to be a real tool"
+    usage
+    exit 22
+fi
+
+namespace="tool-${1}"
+tool=$1
+
+if ! kubectl get ns $namespace; then
+    echo "********************"
+    echo "Namespace doesn't exist -- the tool name must be incorrect"
+    usage
+    exit 1
+fi
+
+
+echo "Creating the service account..."
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: ${tool}-obs
+  namespace: $namespace
+  labels:
+    tool.toolforge.org/name: tool-observer
+EOF
+
+echo "Enabling read-only access to the cluster..."
+
+cat <<EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: ${tool}-obs
+  labels:
+    tool.toolforge.org/name: tool-observer
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: view
+subjects:
+  - kind: ServiceAccount
+    name: ${tool}-obs
+    namespace: $namespace
+EOF
+
+cat <<EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: ${tool}-obs-psp
+  namespace: $namespace
+  labels:
+    tool.toolforge.org/name: tool-observer
+subjects:
+  - kind: ServiceAccount
+    name: ${tool}-obs
+    namespace: $namespace
+roleRef:
+  kind: Role
+  name: tool-${tool}-psp
+  apiGroup: rbac.authorization.k8s.io
+EOF
+
+kubectl get serviceaccounts ${tool}-obs -n $namespace
+echo "*********************"
+echo "Done!"
--- modules/toolforge/manifests/k8s/admin_scripts.pp
+++ modules/toolforge/manifests/k8s/admin_scripts.pp
@@ -15,4 +15,12 @@ class toolforge::k8s::admin_scripts (
         mode   => '0755',
         source => 'puppet:///modules/toolforge/k8s/admin_scripts/wmcs-k8s-secret-for-cert.sh',
     }
+
+    file { '/usr/local/sbin/wmcs-k8s-enable-cluster-monitor':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        source => 'puppet:///modules/toolforge/k8s/admin_scripts/wmcs-k8s-enable-cluster-monitor.sh',
+    }
 }
