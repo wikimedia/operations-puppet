62f60155b3bdef12dbde57aeefbd7529f34fc2d8
Mathew Onipe <monipe@wikimedia.org>
2019-12-19 12:08:43 +0100

    maps: Increase replication frequency
    Osmosis still failed after initially reducing maxInterval.
    Let's reduce it further and run replicate-osm twice a day
    to make up for the current lag.
    
    Bug: T239728
    Change-Id: I29834779abf4432665ee3c68e447a8c64a88b05a

--- modules/profile/manifests/maps/osm_master.pp
+++ modules/profile/manifests/maps/osm_master.pp
@@ -1,7 +1,7 @@
 class profile::maps::osm_master (
     $planet_sync_period       = hiera('profile::maps::osm_master::planet_sync_period', 'day'),
     $planet_sync_day          = hiera('profile::maps::osm_master::planet_sync_day', '*/2'),
-    $planet_sync_hour         = hiera('profile::maps::osm_master::planet_sync_hour', '1'),
+    $planet_sync_hour         = hiera('profile::maps::osm_master::planet_sync_hour', '1,13'),
     $planet_sync_minute       = hiera('profile::maps::osm_master::planet_sync_minute', '27'),
     $maps_hosts               = hiera('profile::maps::hosts'),
     $kartotherian_pass        = hiera('profile::maps::osm_master::kartotherian_pass'),
