f063b7d58ddd9957b0b49d3e62c3bbdff80c24d7
Timo Tijhof <krinklemail@gmail.com>
2019-12-19 00:56:59 +0000

    mediawiki: Capture shutdown/destruct backtrace in php7-fatal-error.php
    Also documented two examples. I'm not sure if there is a deeper
    source that we might be able to influence to make this better in
    the first place, but seems easy enough to handle for now.
    
    The previous regular expression this code was using does not appear
    to be matching against anything I've seen from PHP 7.2 in our current
    setup.
    
    Bug: T241097
    Change-Id: Id9fe711b5d60deace5babd767b4592611bbd472b

--- modules/profile/files/mediawiki/php/php7-fatal-error.php
+++ modules/profile/files/mediawiki/php/php7-fatal-error.php
@@ -2,8 +2,76 @@
 # This file is managed by Puppet.
 
 $err = error_get_last();
-$message = $err['message'];
-$message = preg_replace( "/^.*?exception '.*?' with message '(.*?)'.*$/im", '\1', $message );
+
+// What can $err look like?
+//
+// == Fatal during main stack ==
+//
+// Last confirmed: December 2019 (PHP 7.2.24).
+//
+// When a fatal exception happens during the main stack,
+// then $err is populated with the properties of the Exception
+// object. $err has no trace field, but the stack is still intact
+// in this scenario, so we can get it the normal way.
+//
+// file: /srv/mediawiki/php-.../vendor/guzzlehttp/psr7/src/Stream.php
+// line: 73
+// message: Maximum execution time of 180 seconds exceeded
+// --
+// debug_backtrace:
+//     #1 /srv/mediawiki/php-/extensions/.../CheckConstraintsJob.php(74): CheckConstraintsJob->checkConstraints()
+//     #2 /srv/mediawiki/php-/JobExecutor.php(70): CheckConstraintsJob->run()
+//     #3 /srv/mediawiki/rpc/RunSingleJob.php(76): JobExecutor->execute()
+//
+// == Fatal during shutdown ==
+//
+// Last confirmed: December 2019 (PHP 7.2.24).
+//
+// When a fatal exception happens during request process shutdown.
+// This is post-send and after the main call stack has unwound.
+// This new stack stack will originate from one of the __destruct() methods,
+// or from a register_shutdown_function callback.
+//
+// For this scenario, PHP does not expose its new stack through normal means,
+// ie. debug_backtrace, or '(new Exception)->getTrace()'. Instead, it is exposed
+// through the $err['message'] field.
+//
+// file: /srv/mediawiki/wmf-config/set-time-limit.php
+// line: 39
+// message: |
+//     PHP Fatal error:  Uncaught WMFTimeoutException: the execution time limit of 60 seconds was exceeded in /srv/mediawiki/wmf-config/set-time-limit.php:39
+//     Stack trace:
+//     #0 /srv/mediawiki/php-/includes/parser/Parser.php(414): {closure}(1)
+//     #1 [internal function]: Parser->__destruct()
+//     #2 {main}
+//       thrown in /srv/mediawiki/wmf-config/set-time-limit.php on line 39
+//
+$messageParts = explode( "\nStack trace:\n", $err['message'], 2 );
+$message = $messageParts[0];
+$trace = $messageParts[1] ?? '';
+unset( $messageParts );
+
+// Obtain a trace
+if ( $trace === '' ) {
+	$traceData = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, 500 );
+	// Based on mediawiki-core: MWExceptionHandler::prettyPrintTrace()
+	foreach ( $traceData as $level => $frame ) {
+		if ( isset( $frame['file'] ) && isset( $frame['line'] ) ) {
+			$trace .= "#{$level} {$frame['file']}({$frame['line']}): ";
+		} else {
+			$trace .= "#{$level} [internal function]: ";
+		}
+		if ( isset( $frame['class'] ) && isset( $frame['type'] ) && isset( $frame['function'] ) ) {
+			$trace .= $frame['class'] . $frame['type'] . $frame['function'];
+		} elseif ( isset( $frame['function'] ) ) {
+			$trace .= $frame['function'];
+		} else {
+			$trace .= 'NO_FUNCTION_GIVEN';
+		}
+		$trace .= "()\n";
+	}
+	unset( $traceData, $level, $frame );
+}
 
 // If the output has already been flushed, it may be unsafe to append an error message.
 if ( !headers_sent() ) {
@@ -54,27 +122,6 @@ if ( $statsd_host && $statsd_port ) {
 	@socket_sendto( $sock, $stat, strlen( $stat ), 0, $statsd_host, $statsd_port );
 }
 
-// Obtain a trace
-// Based on mediawiki-core: MWExceptionHandler::prettyPrintTrace()
-$traceData = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, 500 );
-$trace = '';
-foreach ( $traceData as $level => $frame ) {
-	if ( isset( $frame['file'] ) && isset( $frame['line'] ) ) {
-		$trace .= "#{$level} {$frame['file']}({$frame['line']}): ";
-	} else {
-		$trace .= "#{$level} [internal function]: ";
-	}
-	if ( isset( $frame['class'] ) && isset( $frame['type'] ) && isset( $frame['function'] ) ) {
-		$trace .= $frame['class'] . $frame['type'] . $frame['function'];
-	} elseif ( isset( $frame['function'] ) ) {
-		$trace .= $frame['function'];
-	} else {
-		$trace .= 'NO_FUNCTION_GIVEN';
-	}
-	$trace .= "()\n";
-}
-unset( $traceData, $level, $frame );
-
 // Should match the structure of exceptions logged by MediaWiki core.
 // so that it blends in with its log channel and the Logstash dashboards
 // written for it.
@@ -85,7 +132,7 @@ $info = [
 	'exception' => [
 		'message' => $message,
 		'file' => "{$err['file']}:{$err['line']}",
-		'trace' => $trace,
+		'trace' => $trace ?: '{unknown}',
 	],
 	'caught_by' => '/etc/php/php7-fatal-error.php (via wmerrors)',
 	// Match wmf-config: logging.php
