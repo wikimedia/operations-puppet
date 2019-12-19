7acec4756c510357a7258876c1d2aa3629438a28
Brennen Bearnes <bbearnes@wikimedia.org>
2019-12-18 17:18:47 -0700

    logspam.pl: Shorten paths and include fatals
    Based on feedback from deployers using this tool:
    
      - Condense file paths like "includes/foo/bar.php" -> "i/f/bar.php".
    
      - Drop exception namespaces, and remove trailing "Exception".
    
      - extract version strings for MediaWiki and Parsoid deploys.
    
      - Include fatal.log output, and condense extremely common "Allowed
        memory size" and "Maximum execution time" errors to a single line
        item.  This should let people keep an eye on overall volume of
        those errors while still separately highlighting any other PHP
        fatals.
    
      - Add some documentation with usage examples.
    
    Bug: T242252
    Change-Id: Ia7f0017fda721796c6f614fb49fc1be6d944cf16

--- modules/role/files/logging/logspam.pl
+++ modules/role/files/logging/logspam.pl
@@ -1,16 +1,44 @@
 #!/usr/bin/env perl
 
-# This deduplicates exceptions from exception.log and error.log and prints them
-# with a leading count of their occurrence and a snippet of the stacktrace.
-#
-# In practice, you may want the logspam-watch wrapper.
-#
-# This is hacky and contains many assumptions, but has proven useful when
-# watching for breakage during deploys.
+=pod
+
+=head1 NAME
+
+logspam - summarize exceptions from production logs
+
+=head1 USAGE
+
+    # Just get a list of recent exceptions:
+    logspam
+
+    # Monitor logspam output every 10 seconds, sorted:
+    watch -n 10 sh -c "./bin/logspam | sort -nr"
+
+    # Like the above, but filter out Parsoid spam:
+    watch -n 10 sh -c "./bin/logspam | grep -v parsoid | sort -nr"
+
+    # A quick shorthand for invoking watch(1):
+    logspam-watch
+
+=head1 DESCRIPTION
+
+This deduplicates exceptions from exception.log, error.log, and fatal.log and
+prints them with a leading count of their occurrence and a snippet of the
+stacktrace.  In the interests of concision, it shortens exception names by
+removing any trailing "Exception" and condenses file paths.
+
+In practice, you may want the logspam-watch wrapper.
+
+This is hacky and contains many assumptions, but has proven useful when
+watching for breakage during deploys.
+
+=cut
 
 use warnings;
 use strict;
 use 5.10.0;
+use utf8;
+use open qw(:std :utf8);
 
 use List::Util qw(min max);
 
@@ -24,19 +52,29 @@ if (defined $ENV{MW_LOG_DIRECTORY}) {
   $mw_log_dir = $ENV{MW_LOG_DIRECTORY};
 }
 
-my $loglines = tail("${mw_log_dir}/exception.log")
-             . tail("${mw_log_dir}/error.log");
+# Be lazy and shell out to tail:
+my $log_path_str = join ' ', (
+  "${mw_log_dir}/exception.log",
+  "${mw_log_dir}/error.log",
+  "${mw_log_dir}/fatal.log",
+);
+my $loglines =  `tail -n 3000 -q $log_path_str`;
 
 # Treat the datestamped log line as a separator between records - we're
 # interested in how often the stack trace appears
 my @errors = split m{
 
-  ^           # start of datestamped log line
-    [\d-]{10} # datestamp
-    .*        # remainder of line
+  ^           # Start of datestamped log line
+    [\d-]{10} # Datestamp
+    .*        # Remainder of line
   $           # EOL
 
 }mx, $loglines;
+
+my %consolidate_patterns = (
+  '[mem size]' => qr/Allowed memory size of/,
+  '[max time]' => qr/Maximum execution time of 180 seconds exceeded/,
+);
 
 # A pattern for extracting exception names and the invariant stack traces from
 # errors looking like so:
@@ -45,24 +83,40 @@ my @errors = split m{
 #   #1 /srv/mediawiki/php-1.35.0-wmf.4/includes/parser/Parser.php(3125): Preprocessor_Hash->preprocessToObj(string, integer)
 #   ...
 my $exception_pat = qr{
+
   ^                         # Start of error
     \[Exception [ ] (.*?)\] # Yank out the class of our exception
     (.*?)                   # Stack trace
   $                         # End of error
+
 }mx;
 
 # Count times each stacktrace appears:
 my %unique_errors;
 my $max_exception_len = 0;
 foreach (@errors) {
-  if ( $_ =~ $exception_pat ) {
-    # Retain this for display use:
-    $max_exception_len = max(length $1, $max_exception_len);
+  next unless $_ =~ $exception_pat;
+  my $exception_class = $1;
+  my $stack_trace = shorten($2);
 
-    my $unique_error_msg = "$1\t$2";
-    $unique_error_msg =~ s/^ \s+ | \s+ $//gx; # Strip whitespace.
-    $unique_errors{$unique_error_msg}++;
+  # Condense some common errors:
+  for my $key (keys %consolidate_patterns) {
+    if ($stack_trace =~ $consolidate_patterns{$key}) {
+      $exception_class = $key;
+      $stack_trace = $consolidate_patterns{$key};
+    }
   }
+
+  # Drop the namespace of the exception class and chop off the typical
+  # trailing "Exception":
+  $exception_class =~ s{[a-z] [a-z]+ \\}{}xgi;
+  $exception_class =~ s{Exception$}{};
+
+  # Retain this for display use:
+  $max_exception_len = max(length $exception_class, $max_exception_len);
+
+  my $unique_error_msg = "$exception_class\t$stack_trace";
+  $unique_errors{$unique_error_msg}++;
 }
 
 my $display_width = $terminal_width - ($max_exception_len + 16);
@@ -77,8 +131,59 @@ foreach (keys %unique_errors) {
   say join "\t", ($count, $display_exception, $display_trace);
 }
 
-# I'm being incredibly lazy here and shelling out to tail:
-sub tail {
-  my ($file) = @_;
-  return `tail -3000 $file`;
+=head1 SUBROUTINES
+
+=over
+
+=item shorten($stack_trace)
+
+Condense and format traces matching some common patterns.
+
+=cut
+
+sub shorten {
+  my ($stack_trace) = @_;
+
+  my $mw_root = '/srv/mediawiki/';
+  my $parsoid_root = '/srv/deployment/parsoid/deploy-cache/revs/';
+
+  # Strip trailing / leading whitespace:
+  $stack_trace =~ s/^ \s+ | \s+ $//gx;
+
+  if ( $stack_trace =~ m{^ \( $mw_root php-.*(wmf[.].*?)/ (.*?) \) (.*) $}x ) {
+    my ($version, $path, $trace) = ($1, $2, $3);
+    $path = condense_path($path);
+    $stack_trace = "$version $path $trace";
+  }
+
+  if ( $stack_trace =~ m{^ \( $parsoid_root ([a-z0-9]{6}) .*? / (.*?) \) (.*) $}x) {
+    my ($rev, $path, $trace) = ($1, $2, $3);
+    $path = condense_path($path);
+    $stack_trace = "parsoid:$rev $path $trace";
+  }
+
+  return $stack_trace;
 }
+
+=item condense_path($path)
+
+Condense a typical file path by reducing to the first letter of each leading
+particle, like:
+
+    includes/foo/bar/baz.php
+
+Becomes:
+
+    i/f/b/baz.php
+
+=cut
+
+sub condense_path {
+  my ($path) = @_;
+  $path =~ s!([a-z]) [a-z.]+ /!$1/!xgi;
+  return $path;
+}
+
+=back
+
+=cut
