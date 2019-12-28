ad9bac292d481f61b36b67b1e1ba5bd86e5e23a1
andrewbogott <abogott@wikimedia.org>
2019-12-28 05:48:09 -0600

    keystone: add service manifests for openstack Pike
    Bug: T241347
    Change-Id: I55c2da1d7b7302c8574df069ab51b713895b3ed0

---
+++ modules/openstack/manifests/keystone/service/pike.pp
@@ -0,0 +1,109 @@
+class openstack::keystone::service::pike(
+    $keystone_host,
+    $osm_host,
+    $db_name,
+    $db_user,
+    $db_pass,
+    $db_host,
+    $db_max_pool_size,
+    $public_workers,
+    $admin_workers,
+    $ldap_hosts,
+    $ldap_base_dn,
+    $ldap_user_id_attribute,
+    $ldap_user_name_attribute,
+    $ldap_user_dn,
+    $ldap_user_pass,
+    $auth_protocol,
+    $auth_port,
+    $wiki_status_page_prefix,
+    $wiki_status_consumer_token,
+    $wiki_status_consumer_secret,
+    $wiki_status_access_token,
+    $wiki_status_access_secret,
+    $wiki_consumer_token,
+    $wiki_consumer_secret,
+    $wiki_access_token,
+    $wiki_access_secret,
+    String $wsgi_server,
+) {
+    class { "openstack::keystone::service::pike::${::lsbdistcodename}": }
+
+    include ::network::constants
+    $prod_networks = $network::constants::production_networks
+    $labs_networks = $network::constants::labs_networks
+
+    file {
+        '/etc/logrotate.d/keystone':
+            ensure  => 'present',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            source  => 'puppet:///modules/openstack/pike/keystone/keystone_logrotate',
+            require => Package['keystone'];
+        '/etc/keystone/keystone.conf':
+            ensure    => 'present',
+            owner     => 'keystone',
+            group     => 'keystone',
+            mode      => '0444',
+            show_diff => false,
+            content   => template('openstack/pike/keystone/keystone.conf.erb'),
+            notify    => Service[$wsgi_server],
+            require   => Package['keystone'];
+        '/etc/keystone/keystone-paste.ini':
+            ensure  => 'present',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            source  => 'puppet:///modules/openstack/pike/keystone/keystone-paste.ini',
+            notify  => Service[$wsgi_server],
+            require => Package['keystone'];
+        '/etc/keystone/policy.json':
+            ensure  => 'present',
+            mode    => '0644',
+            owner   => 'root',
+            group   => 'root',
+            source  => 'puppet:///modules/openstack/pike/keystone/policy.json',
+            notify  => Service[$wsgi_server],
+            require => Package['keystone'];
+        '/etc/keystone/logging.conf':
+            ensure  => 'present',
+            source  => 'puppet:///modules/openstack/pike/keystone/logging.conf',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            notify  => Service[$wsgi_server],
+            require => Package['keystone'];
+        '/etc/keystone/keystone.my.cnf':
+            ensure    => 'present',
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0400',
+            show_diff => false,
+            content   => template('openstack/pike/keystone/keystone.my.cnf.erb');
+        '/usr/lib/python2.7/dist-packages/wmfkeystoneauth':
+            ensure  => 'present',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            source  => 'puppet:///modules/openstack/pike/keystone/wmfkeystoneauth',
+            notify  => Service[$wsgi_server],
+            recurse => true;
+        '/usr/lib/python2.7/dist-packages/wmfkeystoneauth.egg-info':
+            ensure  => 'present',
+            source  => 'puppet:///modules/openstack/pike/keystone/wmfkeystoneauth.egg-info',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            notify  => Service[$wsgi_server],
+            recurse => true;
+        '/usr/bin/keystone-wsgi-admin.py':
+            ensure  => 'present',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755',
+            source  => 'puppet:///modules/openstack/pike/keystone/keystone-wsgi-admin.py',
+            notify  => Service[$wsgi_server],
+            require => Package['keystone'];
+    }
+}
---
+++ modules/openstack/manifests/keystone/service/pike/stretch.pp
@@ -0,0 +1,73 @@
+class openstack::keystone::service::pike::stretch(
+) {
+    require ::openstack::serverpackages::pike::stretch
+
+    apt::repository { 'stretch-wikimedia-thirdparty-oath':
+        uri        => 'http://apt.wikimedia.org/wikimedia',
+        dist       => stretch-wikimedia,
+        components => 'thirdparty/oath',
+        source     => false,
+        before     => Package[python-oath],
+    }
+
+    package { 'python-oath':
+        ensure  => present,
+    }
+
+    # pull in python-ldap version 3+ from the buster repo.
+    #  older versions don't handle unicode properly.
+    #  T229227
+    apt::repository { 'stretch-wikimedia-component-python-ldap-bpo':
+        uri        => 'http://apt.wikimedia.org/wikimedia',
+        dist       => stretch-wikimedia,
+        components => 'component/python-ldap-bpo',
+        before     => Package['keystone'],
+        notify     => Exec['apt_update_python_ldap'],
+    }
+    exec {'apt_update_python_ldap':
+        command     => '/usr/bin/apt-get update',
+        refreshonly => true,
+    }
+
+    $packages = [
+        'keystone',
+        'alembic',
+        'python-alembic',
+        'python-amqp',
+        'python-castellan',
+        'python-cliff',
+        'python-cmd2',
+        'python-concurrent.futures',
+        'python-cryptography',
+        'python-dogpile.cache',
+        'python-eventlet',
+        'python-funcsigs',
+        'python-futurist',
+        'python-jinja2',
+        'python-jsonschema',
+        'python-kombu',
+        'python-mysql.connector',
+        'python-memcache',
+        'python-migrate',
+        'python-openssl',
+        'python-pyasn1',
+        'python-pycadf',
+        'python-pyinotify',
+        'python-pymysql',
+        'python-pyparsing',
+        'python-routes',
+        'python-sqlalchemy',
+        'python-unicodecsv',
+        'python-warlock',
+        'ldapvi',
+        'python-ldap',
+        'python-ldappool',
+        'python3-ldap3',
+        'ruby-ldap',
+        'python-mwclient',
+    ]
+
+    package { $packages:
+        ensure  => 'present',
+    }
+}
