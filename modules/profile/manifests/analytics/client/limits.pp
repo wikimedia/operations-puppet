66874bd269e1434e639011cee27fd80a8015284b
Luca Toscano <ltoscano@wikimedia.org>
2020-01-02 17:37:18 +0100

    profile::analytics::client::limits: add cpu limits to Analytics clients
    What:
    Add CPU limits for Analytics client nodes (stat/notebook).
    
    Why:
    This is a follow up of memory limits added a couple of months ago.
    The idea is to limit a single systemd unit to 80% of CPU usage across
    all the cores of a host.
    
    Bug: T240440
    Change-Id: I8d995bc246d3651452345fa8afacce9560103d29

--- modules/profile/manifests/analytics/client/limits.pp
+++ modules/profile/manifests/analytics/client/limits.pp
@@ -7,7 +7,12 @@
 #
 class profile::analytics::client::limits {
 
+    # Allow a single unit to use maximum 80% of the CPU resources
+    # From the systemd docs for CPUQuota:
+    # 'Use values > 100% for allotting CPU time on more than one CPU'
+    $cpu_quota = floor($facts['processors']['count'] * 0.8) * 100
+
     class { 'systemd::slice::all_users':
-        all_users_slice_config  => file('profile/analytics/client/limits/user-resource-control.conf'),
+        all_users_slice_config  => template('profile/analytics/client/limits/user-resource-control.conf.erb'),
     }
 }
