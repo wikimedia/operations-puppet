50c94fddc4c7be46501880bd399cfea33d205781
Ottomata <aotto@wikimedia.org>
2020-01-02 15:27:08 -0500

    Clone new primary and secondary event schemas repos for eventschemas site
    Bug: T206789
    Change-Id: Ib998a417243d33b857f62a5d487f3aabb1a32c3d

--- modules/profile/manifests/eventschemas/repositories.pp
+++ modules/profile/manifests/eventschemas/repositories.pp
@@ -7,10 +7,19 @@
 # [*repositories*]
 #   Hash of repository $name -> git origin.
 #   Each of these will be cloned at /srv/schema/repositories/$name
-#   Default: { mediawiki => mediawiki/eventschemas }
+#   Default: {
+#       'primary'   => 'schemas/event/primary',
+#       'secondary' => 'schemas/event/secondary',
+#       'mediawiki' => 'mediawiki/event-schemas'
+#   }
 #
 class profile::eventschemas::repositories(
-    $repositories = hiera('profile::eventschemas::repositories', {'mediawiki' => 'mediawiki/event-schemas'})
+    $repositories = hiera('profile::eventschemas::repositories', {
+        'primary'   => 'schemas/event/primary',
+        'secondary' => 'schemas/event/secondary',
+        # mediawiki/event-schemas is being deprecated in favor of schemas/event/primary
+        'mediawiki' => 'mediawiki/event-schemas',
+    })
 ) {
     keys($repositories).each |String $name| {
         eventschemas::repository { $name:
