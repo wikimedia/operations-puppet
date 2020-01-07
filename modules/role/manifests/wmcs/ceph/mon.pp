559bbb3b52c625efcbc302d0d7564a9973325c9c
Jason Hedden <jhedden@wikimedia.org>
2020-01-07 12:22:50 -0600

    ceph: Update ceph role desc and lvs pool map
    Add description for the WMCS Ceph roles
    
    Add the correct LVS configuration for the cloudceph LVS pool mapping
    
    Bug: T240715
    Change-Id: Icd97b4ef47ff3374837c3ac1e23275cdec257b60

--- modules/role/manifests/wmcs/ceph/mon.pp
+++ modules/role/manifests/wmcs/ceph/mon.pp
@@ -1,5 +1,5 @@
 class role::wmcs::ceph::mon {
-    system::role { $name: }
+    system::role { $name: description => 'Ceph Monitor / Manager server.' }
     include ::profile::standard
     include ::profile::base::firewall
     include ::profile::lvs::realserver
--- modules/role/manifests/wmcs/ceph/osd.pp
+++ modules/role/manifests/wmcs/ceph/osd.pp
@@ -1,5 +1,5 @@
 class role::wmcs::ceph::osd {
-    system::role { $name: }
+    system::role { $name: description => 'Ceph Object Storage Daemon server.' }
     include ::profile::standard
     include ::profile::base::firewall
     include ::profile::ceph::osd
