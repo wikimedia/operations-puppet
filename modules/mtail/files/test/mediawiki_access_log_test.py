1f6af36d358aabf654ae48c653ba5457e2b737b5
Effie Mouzeli <effie@wikimedia.org>
2020-01-09 18:23:10 +0200

    mtail: Remove hhvm handler
    Change-Id: Ic0e1992097ff09b572019a8916c7939c924ca8f2

--- modules/mtail/files/test/mediawiki_access_log_test.py
+++ modules/mtail/files/test/mediawiki_access_log_test.py
@@ -9,7 +9,6 @@ class MediaWikiAccessLogTest(unittest.TestCase):
     def setUp(self):
         self._handler_codes = {
             'php7': 'proxy:unix:/run/php/fpm-www.sock|fcgi://localhost',
-            'hhvm': 'proxy:fcgi://127.0.0.1:9000',
             'static': '-'
         }
         self.store = mtail_store.MtailMetricStore(
@@ -27,7 +26,6 @@ class MediaWikiAccessLogTest(unittest.TestCase):
         """Test requests are correctly divided by handler."""
         s = self.store.get_samples('mediawiki_http_requests_duration_count')
         self.assertIn((self.id_str('php7'), 5), s)
-        self.assertIn((self.id_str('hhvm'), 3), s)
         self.assertIn((self.id_str('static', code=301), 1), s)
 
     def testByBucket(self):
