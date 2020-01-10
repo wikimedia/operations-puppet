ec3964fc3c339db3edf9ce872ef250afb9442baf
Jbond <jbond@wikimedia.org>
2020-01-10 10:29:33 +0000

    apt:::pin: allow callers to override the notify resource
    It appears that puppet 4.8 requires the selector statment to be
    encapsulated in brackets ().  confirmed using the following tests
    manifests.  I also found PUP-8067[1] which lists this bug as fixed in puppet 5.4
    
      # Without brackets
      jbond@af-puppetmaster02:~$ puppet apply --version
      4.8.2
      jbond@af-puppetmaster02:~$ cat test.pp
      $foo = 'foo'
      $bar = $foo =~ /thisdosntmatter/ ? {
              true => 'yes',
              default => 'no',
      }
      notify {"${bar}":}
    
      jbond@af-puppetmaster02:~$ puppet apply test.pp
      Notice: Compiled catalog for af-puppetmaster02.automation-framework.eqiad.wmflabs
      in environment production in 0.10 seconds
      Notice: false
      Notice: /Stage[main]/Main/Notify[false]/message: defined 'message' as 'false'
      Notice: Applied catalog in 0.01 seconds
    
      # With braces
      jbond@af-puppetmaster02:~$ cat test.pp
      $foo = 'foo'
      $bar = ($foo =~ /thisdosntmatter/) ? {
              true => 'yes',
              default => 'no',
      }
      notify {"${bar}":}
    
      jbond@af-puppetmaster02:~$ puppet apply test.pp
      Notice: Compiled catalog for af-puppetmaster02.automation-framework.eqiad.wmflabs
      in environment production in 0.09 seconds
      Notice: no
      Notice: /Stage[main]/Main/Notify[no]/message: defined 'message' as 'no'
      Notice: Applied catalog in 0.02 seconds
    
    [1]https://tickets.puppetlabs.com/browse/PUP-8067
    
    Bug: T242383
    Change-Id: Ie2528da709bdc98639a6ac3ff77267145d402560

--- modules/apt/manifests/pin.pp
+++ modules/apt/manifests/pin.pp
@@ -4,19 +4,23 @@ define apt::pin (
     $package=$name,
     $ensure=present,
 ) {
-    # Validate that $name does not already have a ".pref" suffix.
-    if $name =~ /\.pref$/ {
-        fail('$name must not have a ".pref" suffix.')
+    # Braces required on puppet < 5.4 PUP-8067
+    $filename = ($name =~ /\.pref$/) ? {
+        true    => $name.regsubst('[^\w\.]', '_', 'G'),
+        default => "${name.regsubst('\W', '_', 'G')}.pref",
     }
 
-    $filename = regsubst( $name, '\W', '_', 'G' )
+    $_notify = defined('$notify') ? {
+        true => $notify,
+        default => Exec['apt-get update'],
+    }
 
-    file { "/etc/apt/preferences.d/${filename}.pref":
+    file { "/etc/apt/preferences.d/${filename}":
         ensure  => $ensure,
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
         content => "Package: ${package}\nPin: ${pin}\nPin-Priority: ${priority}\n",
-        notify  => Exec['apt-get update'],
+        notify  => $_notify,
     }
 }
--- modules/apt/spec/defines/apt_pin_spec.rb
+++ modules/apt/spec/defines/apt_pin_spec.rb
@@ -1,28 +1,65 @@
 require 'spec_helper'
 
-describe 'apt::pin' do
-    let(:pre_condition) {
-        'exec { "apt-get update": path => "/usr/bin" }'
+test_on = {
+  supported_os: [
+    {
+      'operatingsystem'        => 'Debian',
+      'operatingsystemrelease' => ['8', '9', '10'],
     }
-    let(:params) { {
-        :pin => 'release o=Wikimedia',
-        :priority => '1042',
-    } }
+  ]
+}
 
-    context do
-        let(:title) { 'mypackage' }
-        it { should compile }
+describe 'apt::pin' do
+  let(:pre_condition) {
+    'exec { "apt-get update": path => "/usr/bin" }
+     exec { "foobar": command => "/bin/true" }'
+  }
+  let(:params) { {
+    :pin => 'release o=Wikimedia',
+    :priority => '1042',
+  } }
+  on_supported_os(test_on).each do |os, _facts|
+    context "#{os}" do
+      let(:title) { 'mypackage' }
+      it { should compile }
+      it "have file with correct content and dependencies" do
+        is_expected.to contain_file('/etc/apt/preferences.d/mypackage.pref').with(
+          ensure: 'present',
+          owner: 'root',
+          group: 'root',
+          mode: '0444',
+          notify: "Exec[apt-get update]"
+        ).with_content(
+          /Package:\smypackage\n
+          Pin:\srelease\so=Wikimedia\n
+          Pin-Priority:\s1042\n/x
+        )
+      end
     end
 
+    context "override parameters" do
+      let(:title) { 'mypackage' }
+      before(:each) { params.merge!(notify: ref('Exec', 'foobar')) }
+      it { should compile }
+      it "has no file notify" do
+        is_expected.to contain_file(
+          '/etc/apt/preferences.d/mypackage.pref'
+        ).with_notify('Exec[foobar]')
+      end
+    end
     context "when title has spaces" do
-        let(:title) { 'pin package' }
-        it "convert spaces to underscores" do
-            is_expected.to contain_file('/etc/apt/preferences.d/pin_package.pref')
-        end
+      let(:title) { 'pin package' }
+      it "convert spaces to underscores" do
+        is_expected.to contain_file('/etc/apt/preferences.d/pin_package.pref')
+      end
     end
 
     context "when title already has '.pref'" do
-        let(:title) { 'mypackage.pref' }
-        it { should compile.and_raise_error(/must not have a "\.pref" suffix/) }
+      let(:title) { 'mypackage.pref' }
+      it { should compile }
+      it "accept name with pref" do
+        is_expected.to contain_file('/etc/apt/preferences.d/mypackage.pref')
+      end
     end
+  end
 end
