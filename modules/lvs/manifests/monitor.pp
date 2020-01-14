7f9ef2ac5efe093ae344674c0acf2f2f2aa35d1a
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2020-01-14 11:25:40 +0100

    lvs::monitor: fix most technical debt
    Lvs::monitor has long been one pain point, as it was plagued by the
    following problems:
    
    * Data structures were mangled in templates we'd then load as yaml. A
    legacy of puppet 3.x
    * lvs::configuration had a lot of special cases and non-uniform data
    structures, making it hard to code around
    * Most importantly, it was impossible for two service stanzas to
    declare separately two check commands for the same hostname - so you
    could either declare both http and https checks with some magic in
    check_http_https or declare checks for either of them
    
    This patch removes all the roadblocks above by using modern puppet
    data mangling primitives and the removal of most special cases.
    
    What gets declared and how should now be easier to reason about, and
    with less unseen side-effects
    
    Change-Id: I7fcc7db7661ff663041939e0579c12de2987f8ad

--- modules/lvs/manifests/monitor.pp
+++ modules/lvs/manifests/monitor.pp
@@ -1,14 +1,90 @@
 # lvs/monitor.pp
 
-class lvs::monitor {
-    include ::lvs::configuration
+class lvs::monitor() {
+    # Services in "monitoring_setup" state will be configured but won't page.
+    # Services in "production" state will be fully configured.
+    $enabled_states = ['monitoring_setup', 'production']
+    $monitored_services = wmflib::service::fetch().filter |$n, $data| {
+        ('monitoring' in $data and $data['state'] in $enabled_states)
+    }
+    # First let's declare all the hosts.
+    $hosts = $monitored_services.map |$n, $data | {
+        # TODO: use get() in puppet 6.x
+        $contact_group = pick($data['monitoring']['contact_group'], 'admins')
 
-    $ip = $lvs::configuration::service_ips
-    $lvs_services = $lvs::configuration::lvs_services
+        $data['monitoring']['sites'].map |$sitename, $host| {
+            $hostname = $host['hostname']
+            $data['ip'][$sitename].map |$k, $ip| {
+                # TODO: only make critical hosts in production state.
+                $host_params = {'ip_address' => $ip, 'contact_group' => $contact_group, 'critical' => true}
+                if $ip =~ Stdlib::Ipv4 {
+                    { $hostname => $host_params}
+                }
+                else {
+                    {"${hostname}_ipv6" => $host_params}
+                }
+            }
+        }
+    }
+    .flatten()
+    .reduce({})|$memo,$el| {$memo.merge($el)} # TODO: improve this. Right now it can coalesce values erratically between different services.
 
-    # This is a hack. Use a template to get a yaml structure of a ruby hash,
-    # then use parseyaml from puppetlabs/stdlib to get a puppet hash back
-    $yaml_tmp_var = template('lvs/monitor_lvs.erb')
-    $monitors = parseyaml($yaml_tmp_var)
-    create_resources(lvs::monitor_service_http_https, $monitors)
+    $hosts.each |$hostname, $params| {
+        @monitoring::host { $hostname:
+            ip_address    => $params['ip_address'],
+            contact_group => $params['contact_group'],
+            group         => 'lvs',
+            critical      => $params['critical']
+        }
+    }
+
+    # Now that all monitoring hosts have been declared, let's declare the services.
+    $monitored_services.each |$n, $data| {
+        $monitoring = $data['monitoring']
+        $critical = $data['state'] ? {
+            'monitoring_setup' => false,
+            default            => $monitoring['critical']
+        }
+        $monitoring['sites'].each |$sitename, $host| {
+            $hostname = $host['hostname']
+            # The condition  will be removed in a followup patch. 
+            # It's just for reproducing the status-quo of the magic of 
+            # lvs::monitor::http_https as much as possible.
+            # Our final solution will be using the name of the pool in the title anyways.
+            if $data['encryption'] {
+                $service_title = "${hostname}_https"
+                $description = 'LVS HTTPS'
+                $service_title_v6 = "${hostname}_ipv6_https"
+            }
+            else {
+                $service_title = $hostname
+                $description = 'LVS HTTP'
+                $service_title_v6 = "${hostname}_ipv6"
+            }
+            # Add ipv4 monitoring if present
+            if $hostname in $hosts {
+                @monitoring::service { $service_title:
+                    host          => $hostname,
+                    group         => 'lvs',
+                    description   => "${description} IPv4",
+                    check_command => $monitoring['check_command'],
+                    critical      => $critical,
+                    contact_group => $monitoring['contact_group'],
+                    notes_url     => 'https://wikitech.wikimedia.org/wiki/LVS#Diagnosing_problems',
+                }
+            }
+            # Add ipv6 monitoring if present
+            if "${hostname}_ipv6" in $hosts {
+                @monitoring::service { $service_title_v6:
+                    host          => "${hostname}_ipv6",
+                    group         => 'lvs',
+                    description   => "${description} IPv6",
+                    check_command => $monitoring['check_command'],
+                    critical      => $monitoring['critical'],
+                    contact_group => $monitoring['contact_group'],
+                    notes_url     => 'https://wikitech.wikimedia.org/wiki/LVS#Diagnosing_problems',
+                }
+            }
+        }
+    }
 }
--- modules/wmflib/functions/service/fetch.pp
+++ modules/wmflib/functions/service/fetch.pp
@@ -2,6 +2,10 @@
 # In the future, this function should include all validation that is not doable
 # with basic puppet typing.
 function wmflib::service::fetch() >> Hash[String, Wmflib::Service] {
+    # Use when doing local development
+    #$yaml = loadyaml('hieradata/common/service.yaml')
+    #$catalog = $yaml['service::catalog']
+
     $catalog = lookup('service::catalog', {'default_value' => {}})
     wmflib::service::validate($catalog)
     return $catalog
