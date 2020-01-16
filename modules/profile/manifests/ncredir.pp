5a0d65e433a1132d536d2ae5cf01f80b94943c3c
Valentin Gutierrez <vgutierrez@wikimedia.org>
2020-01-16 16:23:29 +0000

    ncredir: Remove TLSv1.0 && TLSv1.1 support
    Bug: T238518
    Change-Id: If3944cf65012286a24800f9ddbf82aed17ab5898

--- modules/profile/manifests/ncredir.pp
+++ modules/profile/manifests/ncredir.pp
@@ -25,7 +25,7 @@ class profile::ncredir(
     }
 
     class { '::ncredir':
-        ssl_settings           => ssl_ciphersuite('nginx', 'compat'),
+        ssl_settings           => ssl_ciphersuite('nginx', 'mid'),
         redirection_maps       => compile_redirects('puppet:///modules/ncredir/nc_redirects.dat', 'nginx'),
         acme_certificates      => $shared_acme_certificates,
         acme_chief_cert_prefix => $acme_chief_cert_prefix,
