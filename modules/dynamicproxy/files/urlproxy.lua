cdfc4dafa5b7a3913e07c2f564442deba376a718
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-01-17 13:50:17 +0100

    dynamicproxy: urlproxy: introduce support for domain-based routing
    Modify urlproxy LUA code to evaluate first the subdomain of the request. We are
    interested in the left-most subdomain, it might indicate which tool the user
    is interested in. If we find a match in redis, then proceed selecting a
    backend as normal.
    If we don't find a backend using the subdomain, then evaluate the rest of the
    URL as we were doing before (another redis lookup).
    
    Doing it this way, we give support for the 2 addressing schemes:
     * $toolname.toolforge.org
     * tools.wmflabs.org/$toolname
    
    This patch is specifically about giving support for the legacy gridengine
    webservice setup + the legacy kubernetes cluster. Mind that webservices running
    in the new kubernetes cluster don't register in redis at all.
    Once all webservices are happy with the new addressing scheme, we could
    consider dropping the code for the old scheme.
    
    I'm aware this is kind of mixing urlproxy and domainproxy functionalities.
    
    While at it, prevent nginx from hijacking request to simple / as redirections
    to the admin tool. That is something that the fourohfour tool will need to
    handle.
    
    Bug: T234617
    Change-Id: I1f48e239a394dbb36e2c93f69bc19b2e06ad4b5e
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/dynamicproxy/files/urlproxy.lua
+++ modules/dynamicproxy/files/urlproxy.lua
@@ -17,36 +17,49 @@
 local redis = require 'resty.redis'
 local red = redis:new()
 red:set_timeout(1000)
-
 red:connect('127.0.0.1', 6379)
 
-local captures = ngx.re.match(ngx.var.uri, "^/([^/]*)(/.*)?$")
+function route_backend_and_exit_if_ok(toolname, url)
+    local routes_arr = nil
+    local route = nil
 
-local prefix = captures[1]
-local rest = captures[2] or "/"
-local routes_arr = nil
-local route = nil
+    if not toolname then
+        return
+    end
 
-routes_arr = red:hgetall('prefix:' .. prefix)
+    routes_arr = red:hgetall('prefix:' .. toolname)
+    if not routes_arr then
+        return
+    end
 
-if routes_arr then
     local routes = red:array_to_hash(routes_arr)
     for pattern, backend in pairs(routes) do
-        if ngx.re.match(rest, pattern) ~= nil then
+        if ngx.re.match(url, pattern) ~= nil then
             route = backend
             break
         end
     end
-end
 
-if not route then
-    -- No routes defined for this URI, hope nginx can handle this! (new k8s cluster?)
+    if not route then
+        return
+    end
+
+    -- Use a connection pool of 256 connections with a 32s idle timeout
+    -- This also closes the current redis connection.
+    red:set_keepalive(1000 * 32, 256)
+    ngx.var.backend = route
     ngx.exit(ngx.OK)
 end
 
--- Use a connection pool of 256 connections with a 32s idle timeout
--- This also closes the current redis connection.
-red:set_keepalive(1000 * 32, 256)
+-- try to match first in the subdomain-based routing scheme
+local subdomain = string.match(ngx.var.http_host, "^[^.]+")
+route_backend_and_exit_if_ok(subdomain, "/")
 
-ngx.var.backend = route
+-- if no subdomain-based routing was found, then use the legacy routing scheme
+local captures = ngx.re.match(ngx.var.uri, "^/([^/]*)(/.*)?$")
+local prefix = captures[1]
+local rest = captures[2] or "/"
+route_backend_and_exit_if_ok(prefix, rest)
+
+-- No routes defined for this URI, hope nginx can handle this! (new k8s cluster?)
 ngx.exit(ngx.OK)
