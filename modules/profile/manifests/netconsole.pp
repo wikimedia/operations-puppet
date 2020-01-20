1e2c4e5be53057e9527c7cb8f7c6377443cb7270
Emanuele Rocca <ema@wikimedia.org>
2020-01-20 15:29:25 +0100

    profile::netconsole: use ipaddress and interface_primary
    Instead of expecting dev_name and local_ip to be set in hiera, simply
    use $::interface_primary and $::ipaddress.
    
    Bug: T242579
    Change-Id: I457eae6bb3d61112e26ee5eb244eb87ac62697a2

--- modules/profile/manifests/netconsole.pp
+++ modules/profile/manifests/netconsole.pp
@@ -7,15 +7,13 @@
 #
 class profile::netconsole (
     Wmflib::Ensure $ensure = lookup('profile::netconsole::ensure'),
-    Optional[String] $dev_name = lookup('profile::netconsole::dev_name', {default_value => undef}),
-    Optional[Stdlib::Ipv4] $local_ip = lookup('profile::netconsole::local_ip', {default_value => undef}),
     Optional[Stdlib::Ipv4] $remote_ip = lookup('profile::netconsole::remote_ip', {default_value => undef}),
     Optional[Stdlib::MAC] $remote_mac = lookup('profile::netconsole::remote_mac', {default_value => undef}),
 ) {
     class { '::netconsole':
       ensure     => $ensure,
-      dev_name   => $dev_name,
-      local_ip   => $local_ip,
+      dev_name   => $::interface_primary,
+      local_ip   => $::ipaddress,
       remote_ip  => $remote_ip,
       remote_mac => $remote_mac,
     }
