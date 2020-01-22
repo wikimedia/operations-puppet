720042b14e7e7c13733ec4d9aa2548d0d9529726
Manuel Arostegui <marostegui@wikimedia.org>
2020-01-22 10:49:59 +0100

    mariadb: Productionize es2024 as es5 codfw slave
    es2024 will be an es5 slave replicating from es2023
    
    Bug: T243052
    Change-Id: I48521464b836833f889310bb417af2a5ecea557b

--- manifests/site.pp
+++ manifests/site.pp
@@ -334,7 +334,7 @@ node /^es10(20|21|22|23|24|25)\.eqiad\.wmnet/ {
 }
 
 # Spare external store codfw hosts to be provisioned # T241336
-node /^es20(24|25)\.codfw\.wmnet/ {
+node /^es20(25)\.codfw\.wmnet/ {
     role(spare::system)
 }
 
@@ -907,6 +907,9 @@ node 'es2023.codfw.wmnet' {
     role(mariadb::core)
 }
 
+node /^es202[4]\.codfw\.wmnet/ {
+    role(mariadb::core)
+}
 
 # Disaster recovery hosts for external storage
 # These nodes are in process of being decommissioned
