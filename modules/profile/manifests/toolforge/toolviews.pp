da1f7d78bbc6741c6ab53b51b9e0c7a6c30f0c72
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-01-22 12:42:25 +0100

    toolforge: toolviews: don't run it if not in the tools CloudVPS project
    We are not interested in running toolviews if not in the tools CloudVPS
    project.
    
    This happens specifically in toolsbeta, where we have proxy servers, they have
    this manifest deployed but credentials to connect to the DB are wrong, thus
    producing cronspam. Right now we are not interested in data from toolsbeta
    anyway, only actual tools running in Toolforge.
    
    Change-Id: Iee4b223c0d140f975059099e5520b6607da10405
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/toolforge/toolviews.pp
+++ modules/profile/manifests/toolforge/toolviews.pp
@@ -7,10 +7,15 @@ class profile::toolforge::toolviews (
     $mysql_user     = lookup('profile::toolforge::toolviews::mysql_user',     {default_value => 'example_user'}),
     $mysql_password = lookup('profile::toolforge::toolviews::mysql_password', {default_value => 'example_passwd'}),
 ){
-    class { '::toolforge::toolviews':
-        mysql_host     => $mysql_host,
-        mysql_db       => $mysql_db,
-        mysql_user     => $mysql_user,
-        mysql_password => $mysql_password,
+    # due to wrong or missing DB credentials, toolviews will produce cronspam
+    # if not running in the tools project. If you want to run this in toolsbeta
+    # make sure you provide relevant hiera keys and update the following if:
+    if $::labsproject == 'tools' {
+        class { '::toolforge::toolviews':
+            mysql_host     => $mysql_host,
+            mysql_db       => $mysql_db,
+            mysql_user     => $mysql_user,
+            mysql_password => $mysql_password,
+        }
     }
 }
