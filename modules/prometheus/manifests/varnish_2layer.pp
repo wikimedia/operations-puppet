df4aa4b577b694e10a3548cc1840b032924793df
Valentin Gutierrez <vgutierrez@wikimedia.org>
2020-01-22 18:06:25 +0000

    prometheus: Clean up varnish-backend cluster config
    To be merged after I0867afa06ed6fb05db7243dc90cee219fdcf160e is merged
    and puppet runs on prometheus::ops hosts
    
    Bug: T241239
    Depends-On: I0867afa06ed6fb05db7243dc90cee219fdcf160e
    Change-Id: I0d0c874cdffef7fb290840939ee0c11d6b7b90f2

--- modules/prometheus/manifests/varnish_2layer.pp
+++ modules/prometheus/manifests/varnish_2layer.pp
@@ -5,17 +5,6 @@ define prometheus::varnish_2layer(
     $targets_path,
     $cache_name,
 ) {
-    prometheus::cluster_config{ "${cache_name}_backend":
-        ensure  => absent,
-        dest    => "${targets_path}/varnish-${cache_name}_${::site}_backend.yaml",
-        site    => $::site,
-        cluster => "cache_${cache_name}",
-        port    => 9131,
-        labels  => {
-          'layer' => 'backend',
-        },
-    }
-
     prometheus::cluster_config{ "${cache_name}_frontend":
         dest    => "${targets_path}/varnish-${cache_name}_${::site}_frontend.yaml",
         site    => $::site,
