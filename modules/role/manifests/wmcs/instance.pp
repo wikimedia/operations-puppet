0629ad2fdc8e878b014f6e72bdfb6dd684a88d4c
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-01-23 14:57:53 +0100

    wmcs: instance: introduce per-deployment openstack profiles
    We have different configs in each openstack deployment, include the
    deployment-specific profiles.
    
    This explicitly gives support to VMs running in our our codfw1dev deployment,
    which were previously loading eqiad1 profiles. This patch introduces only the
    observerenv selection. Follow-up patches may introduce additional
    deployment-specific selection as required.
    
    Bug: T242607
    Change-Id: I1fd4bc06f5bf1c1d72bb47098380a1d224394ec0
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/role/manifests/wmcs/instance.pp
+++ modules/role/manifests/wmcs/instance.pp
@@ -2,7 +2,8 @@
 class role::wmcs::instance {
     include ::profile::standard
     include ::profile::base::labs
-    include ::profile::openstack::eqiad1::observerenv
+    include "::profile::openstack::${::wmcs_deployment}::observerenv"
+    # TODO: if required, introduce per-deployment profiles for the next 2:
     include ::profile::openstack::eqiad1::clientpackages::vms
     include ::profile::openstack::eqiad1::cumin::target
     include ::profile::wmcs::instance
