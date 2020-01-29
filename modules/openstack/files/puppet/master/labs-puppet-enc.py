0b2e6cb6dd16dfde1be07e94f487794cf44c317c
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-01-29 14:36:19 +0100

    openstack: puppet-enc: allow hostnames from the new domain
    This script is rejecting valid hostnames in the new domain.
    Give it proper support for FQDNs in the new domain.
    
    Support for FQDN combos ends like this:
    
     * <host>.(eqiad|codfw).wmflabs
     * <host>.<project>.(eqiad|codfw).wmflabs
     * <host>.(eqiad1|codfw1dev).wikimedia.cloud
     * <host>.<project>.(eqiad1|codfw1dev).wikimedia.cloud
    
    While at it, provide additional info via print. A later patch can introduce
    proper logger support.
    
    Bug: T243556
    Change-Id: I0736cb65d806d40806cecd462cf101725ebfa695
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/openstack/files/puppet/master/labs-puppet-enc.py
+++ modules/openstack/files/puppet/master/labs-puppet-enc.py
@@ -6,29 +6,48 @@ from urllib.request import urlopen
 
 def _is_valid_hostname(name):
     """
-    Check that hostname is of the form <host>.(eqiad|codfw).wmflabs or
-    <host>.<project>.(eqiad|codfw).wmflabs
+    Check that hostname is of the form:
+     * <host>.(eqiad|codfw).wmflabs
+     * <host>.<project>.(eqiad|codfw).wmflabs
+     * <host>.(eqiad1|codfw1dev).wikimedia.cloud
+     * <host>.<project>.(eqiad1|codfw1dev).wikimedia.cloud
 
     where host and project are alphanumeric with '-' and '_' allowed
     """
     host_parts = name.split('.')[::-1]
 
-    if len(host_parts) > 4 or len(host_parts) < 3:
+    # this must be either 'wmflabs' or 'cloud'
+    tld = host_parts.pop(0)
+    if tld != 'wmflabs' and tld != 'cloud':
+        print('Invalid hostname ({}) Unknown TLD.'.format(name))
         return False
 
-    domain = host_parts.pop(0)
-    realm = host_parts.pop(0)
+    if tld == 'wmflabs':
+        # current / legacy FQDN case
+        # this must be either eqiad or codfw
+        realm = host_parts.pop(0)
+        if realm != 'eqiad' and realm != 'codfw':
+            print('Invalid hostname ({}) Unknown realm.'.format(name))
+            return False
 
-    if domain != 'wmflabs' and domain != 'labtest' and domain != 'cloud':
-        return False
+    if tld == 'cloud':
+        # new domain case
+        # must be wikimedia.cloud
+        wikimedia = host_parts.pop(0)
+        if wikimedia != 'wikimedia':
+            print('Invalid hostname ({}) Unknown domain.'.format(name))
+            return False
 
-    if realm != 'codfw' and realm != 'eqiad' and realm != 'codfw1dev' and realm != 'wikimedia':
-        return False
+        # must be deployment name, either eqiad1 or codfw1dev
+        deployment = host_parts.pop(0)
+        if deployment != 'eqiad1' and deployment != 'codfw1dev':
+            print('Invalid hostname ({}) Unknown deployment (outdated script?).'.format(name))
+            return False
 
     hostname = [x.replace('-', '').replace('_', '') for x in host_parts]
-
     # list of fqdn parts that are not alphanumeric should be empty
     if len([s for s in hostname if not s.isalnum()]) > 0:
+        print('Invalid hostname ({}) Invalid characters found.'.format(name))
         return False
 
     return True
@@ -41,7 +60,6 @@ if __name__ == '__main__':
     # check to make sure ec2id_name is an actual ec2id based hostname, to
     # prevent ldap injection attacks
     if not _is_valid_hostname(hostname):
-        print('Invalid hostname', hostname)
         sys.exit(-1)
 
     with open('/etc/puppet-enc.yaml', encoding='utf-8') as f:
