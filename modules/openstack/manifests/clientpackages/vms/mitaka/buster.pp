98bf72a4564a69f9aaedf813f0827800121bbc4d
andrewbogott <abogott@wikimedia.org>
2020-02-04 09:06:36 -0600

    Buster/Mitaka vms: include python3 versions of openstack clients
    This is a duplicate of
    
    https://gerrit.wikimedia.org/r/#/c/operations/puppet/+/569084/
    
    But for Mitaka, which seems to be what many VMs are still set to.
    
    Change-Id: Ibb04d144a7c15142b69c41a6287292047d5c7aa0

--- modules/openstack/manifests/clientpackages/vms/mitaka/buster.pp
+++ modules/openstack/manifests/clientpackages/vms/mitaka/buster.pp
@@ -6,4 +6,18 @@
 class openstack::clientpackages::vms::mitaka::buster(
 ) {
     requires_realm('labs')
+
+    $python3packages = [
+        'python3-keystoneauth1',
+        'python3-keystoneclient',
+        'python3-novaclient',
+        'python3-glanceclient',
+        'python3-openstackclient',
+        'python3-designateclient',
+        'python3-neutronclient',
+    ]
+
+    package{ $python3packages:
+        ensure => 'present',
+    }
 }
