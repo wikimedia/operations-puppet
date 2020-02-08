a0f79578efad59194f6ecf2f1ca058ceb20d47fc
Daniel Zahn <dzahn@wikimedia.org>
2020-02-07 19:05:13 -0500

    introduce new role to install nginx and APT repo without DHCP/TFTP
    Bug: T224576
    Bug: T242602
    Change-Id: Ic2aee8074c5b3b01c412fee25f023f6919ab424b

---
+++ modules/role/manifests/apt_repo.pp
@@ -0,0 +1,11 @@
+# Sets up a webserver (nginx) and an APT repository.
+class role::apt_repo {
+    system::role { 'webserver-and-APT-repository': }
+
+    include ::profile::standard
+    include ::profile::base::firewall
+    include ::profile::backup::host
+
+    include ::profile::installserver::http
+    include ::profile::aptrepo::wikimedia
+}
