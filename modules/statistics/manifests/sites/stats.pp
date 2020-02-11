22c1467175d57a1629581025658d7257efdbeec1
Ottomata <aotto@wikimedia.org>
2020-02-11 09:54:35 -0500

    Symlink wikistats v2 at stats.wikimedia.org/index.html
    Bug: T237752
    Change-Id: Ia10c02b8f6a3d0501e4d5fdd18aaf594dccac25f

--- modules/statistics/manifests/sites/stats.pp
+++ modules/statistics/manifests/sites/stats.pp
@@ -1,14 +1,16 @@
 # == Class statistics::sites::stats
 #
-# stats.wikimedia.org's httpd configuration
+# stats.wikimedia.org's httpd configuration.
+#
+# stats.wikimedia.org was originally Erik Zachte's wikistats files.
+# We call this wikistats v1.  wikistats v2 has superseded wikistats v1,
+# but we still want to keep the wikistats v1 files and URLs around, as
+# they are used and referenced by the Wikimedia community.
 #
 class statistics::sites::stats {
     require ::statistics::web
 
     $wikistats_web_directory       = '/srv/stats.wikimedia.org'
-    $wikistats_v2_link             = "${wikistats_web_directory}/htdocs/v2"
-    $source_directory              = '/srv/src'
-    $wikistats_source_directory    = '/srv/src/wikistats-v2'
     $geowiki_private_directory     = "${wikistats_web_directory}/htdocs/geowiki-private"
     $geowiki_private_htpasswd_file = '/etc/apache2/htpasswd.stats-geowiki'
 
@@ -32,27 +34,16 @@ class statistics::sites::stats {
         show_diff => false,
     }
 
-    httpd::site { 'stats.wikimedia.org':
-        content => template('statistics/stats.wikimedia.org.erb'),
-    }
-
-    file { $source_directory:
+    # stats.wikimedia.org (Wikistats 2.0) setup:
+    $wikistats_source_directory    = '/srv/src/wikistats-v2'
+    file { ['/srv/src', $wikistats_source_directory]:
         ensure => directory,
         owner  => 'root',
         group  => 'www-data',
         mode   => '0755',
     }
 
-    file { $wikistats_source_directory:
-        ensure  => directory,
-        owner   => 'root',
-        group   => 'www-data',
-        mode    => '0755',
-        require => File[$source_directory],
-    }
-
-    # stats.wikimedia.org/v2 (Wikistats 2.0) setup:
-    # 1) wikistats v2 is cloned and has a built version available in /dist
+    # wikistats-v2 is cloned and has a built version available in /dist
     git::clone { 'wikistats-v2':
         ensure    => 'latest',
         directory => $wikistats_source_directory,
@@ -64,10 +55,30 @@ class statistics::sites::stats {
         require   => File[$wikistats_source_directory],
     }
 
-    # 2) it is then linked to serve at http://stats.wikimedia.org/v2
-    file { $wikistats_v2_link:
+    # It is then linked to serve at http://stats.wikimedia.org/v2
+    # TO BE REMOVED once redirect from /v2 -> / is set up.
+    file { "${wikistats_web_directory}/htdocs/v2":
         ensure  => 'link',
         target  => "${wikistats_source_directory}/dist",
         require => File[$wikistats_source_directory],
+    }
+
+    # We want to serve wikistats 2 from the root stats.wikimedia.org domain.
+    # wikistats 2 has only 2 entry URLs, index.html and assets-v2.  Symlink them
+    # from the docroot.
+    file { "${wikistats_web_directory}/htdocs/index.html":
+        ensure  => 'link',
+        target  => "${wikistats_source_directory}/dist/index.html",
+        require => File[$wikistats_source_directory],
+    }
+    file { "${wikistats_web_directory}/htdocs/assets-v2":
+        ensure  => 'link',
+        target  => "${wikistats_source_directory}/dist/assets-v2",
+        require => File[$wikistats_source_directory],
+    }
+
+    # Apache site for stats.wikimedia.org
+    httpd::site { 'stats.wikimedia.org':
+        content => template('statistics/stats.wikimedia.org.erb'),
     }
 }
