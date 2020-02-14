0be10504269e4f535207e21811f242b1cab7dc3a
Daniel Zahn <dzahn@wikimedia.org>
2020-02-14 15:47:38 -0500

    noc::site: close port 80 for caching servers
    The backends for noc.wikimedia.org, mwmaint servers,
    are nowadays using envoy for TLS termination.
    
    Therefore caching servers don't need to talk to port 80
    anymore. They talk https to port 443 instead.
    
    Removing these firewall rules is the last step for
    each service on the epic ticket T210411.
    
    Cumin masters were allowed and should be allowed after
    and not be affected by this change. Assuming that
    cumin masters need to keep using http/port 80.
    
    Change-Id: I7598e3ee1088e5b32f686924af0f6d3a3389cc48

--- modules/profile/manifests/noc.pp
+++ modules/profile/manifests/noc.pp
@@ -3,11 +3,18 @@ class profile::noc {
 
     include profile::mediawiki::common
 
-    ferm::service { 'noc-http':
+    # http from envoy to httpd on the backend itself
+    ferm::service { 'noc-http-envoy':
         proto  => 'tcp',
         port   => 'http',
-        srange => '($CACHES $CUMIN_MASTERS)',
+        srange => "(${::ipaddress} ${::ipaddress6}",
     }
 
+    # http from cumin masters
+    ferm::service { 'noc-http-cumin':
+        proto  => 'tcp',
+        port   => 'http',
+        srange => '$CUMIN_MASTERS',
+    }
     class { '::noc': }
 }
