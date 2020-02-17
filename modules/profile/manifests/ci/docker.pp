4b0981fa6bf647b5c721591e673d699171dd9262
John Bond <jbond@wikimedia.org>
2020-02-17 16:53:08 +0100

    profile::ci::docker: manage all group membership in data module
    This manifest attempts to manage the docker group permissions however
    this group is already managed by the admin module as such it means that
    on each puppet run the admin module removes `jenkins-slave` from the
    docker group and the exec adds it back.
    
    This CR adds `jenkins-slave` to the system_members for the docker group
    ensuring all permissions are managed in one place
    
    Bug: T242910
    Change-Id: I508229fbea028ff61947ff5467f67571a150cec3

--- modules/profile/manifests/ci/docker.pp
+++ modules/profile/manifests/ci/docker.pp
@@ -3,8 +3,7 @@
 # Configures a host to be a docker-backed Jenkins agent
 #
 class profile::ci::docker(
-    $jenkins_agent_username = hiera('jenkins_agent_username'),
-    $settings = hiera('profile::ci::docker::settings'),
+    $settings = lookup('profile::ci::docker::settings'),
 ) {
     # Having blubber on all docker nodes will give us a broad pool for all
     # jobs that require similar image building workloads
@@ -32,15 +31,6 @@ class profile::ci::docker(
             Exec['apt-get update']
         ],
     }
-    # Ensure jenkins-deploy membership in the docker group
-    exec { 'jenkins user docker membership':
-        unless  => "/usr/bin/id -Gn '${jenkins_agent_username}' | /bin/grep -qw 'docker'",
-        command => "/usr/sbin/usermod -aG docker '${jenkins_agent_username}'",
-        require => [
-            Package['docker-ce'],
-        ],
-    }
-
     # Ship the entire docker iptables configuration via ferm
     # This is here to make sure docker and ferm play nice together.
     ferm::conf { 'docker-ferm':
