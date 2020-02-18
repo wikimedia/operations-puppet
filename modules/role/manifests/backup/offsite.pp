2a00d10fb577afeec251ac958d914c38a4e49208
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-02-18 17:39:12 +0100

    Add system::role for role::backup::offsite
    The new OS upgrades tracking script spotted that role::backup::offsite
    doesn't set a system::role.
    
    Change-Id: I887bd8208f470697f5bec557be25ff29c66b2dd8

--- modules/role/manifests/backup/offsite.pp
+++ modules/role/manifests/backup/offsite.pp
@@ -1,4 +1,8 @@
 class role::backup::offsite {
     include ::profile::standard
     include ::profile::backup::storage
+
+    system::role { 'backup::offsite':
+        description => 'Backup server (offsite storage)',
+    }
 }
