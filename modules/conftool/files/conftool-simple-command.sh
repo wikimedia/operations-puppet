48e993a3cc3a8098a0a8f6017494cafb99900a17
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2020-02-19 14:58:44 +0100

    conftool::scripts: refuse to pool a server if the weight is 0
    That configuration is not supported by pybal
    
    Bug: T245594
    Change-Id: I0cd646f9a27843662b0bd4d939c6e4c0dd0f8ef2

--- modules/conftool/files/conftool-simple-command.sh
+++ modules/conftool/files/conftool-simple-command.sh
@@ -16,9 +16,24 @@ fi
 
 host=$(hostname -f)
 
+function check_weight {
+    # Check that none of the selected entries has weight 0
+    host=$1
+    service=$2
+    selector="name=$host"
+    if [ "$service" != "all services" ]; then
+        selector="${selector},service=${service}"
+    fi
+    if confctl select "$selector" get | jq -e ".[\"$host\"] | select(.pooled == \"yes\") | select(.weight == 0)" > /dev/null; then
+        printf "\033[0;31mYou cannot pool a node where weight is equal to 0\033[0m\n"
+        exit 2
+    fi
+}
+
 # Get message to print on screen.
 case $action in
     "pool")
+    check_weight "$host" "$service"
     echo "Pooling ${service} on ${host}"
     ;;
     "depool")
