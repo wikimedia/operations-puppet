99aca2ab83171a342dec540e0c9df9814ce27cde
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-02-19 10:26:32 +0100

    Add system::role for role::logging::webrequest::ops
    Change-Id: Idc200039a6b000ede61ef9f719ec1ec74056ae54

--- modules/role/manifests/logging/webrequest/ops.pp
+++ modules/role/manifests/logging/webrequest/ops.pp
@@ -5,4 +5,8 @@ class role::logging::webrequest::ops {
     include ::profile::standard
     include ::profile::base::firewall
     include ::profile::kafkatee::webrequest::ops
+
+    system::role { 'logging:webrequest::ops':
+        description => 'Host with various filters for debugging web requests',
+    }
 }
