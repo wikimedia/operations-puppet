3ef111dba9f7d5aea3fa8639320e4d31a07af0df
Manuel Arostegui <marostegui@wikimedia.org>
2020-02-20 09:18:33 +0100

    mariadb: Productionize es1024
    es1024 will be a slave in es5
    
    Bug: T243052
    Change-Id: I1a85a8b2c86d4c0be6b0055be66fe71c9c71e1e6

--- manifests/site.pp
+++ manifests/site.pp
@@ -329,7 +329,7 @@ node /^cumin[12]001\.(eqiad|codfw)\.wmnet$/ {
 # MariaDB 10
 
 # Spare external store eqiad hosts to be provisioned #T235659
-node /^es10(24|25)\.eqiad\.wmnet/ {
+node /^es10(25)\.eqiad\.wmnet/ {
     role(spare::system)
 }
 
@@ -912,6 +912,11 @@ node /^es202[12]\.codfw\.wmnet/ {
 ## eqiad servers
 # master
 node 'es1023.eqiad.wmnet' {
+    role(mariadb::core)
+}
+
+# slaves
+node 'es1024.eqiad.wmnet' {
     role(mariadb::core)
 }
 
