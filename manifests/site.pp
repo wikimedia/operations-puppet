daad47de7899c7cb9f8b23a9c4bd74529fe60274
Daniel Zahn <dzahn@wikimedia.org>
2020-02-20 17:20:13 -0500

    site: add mw2317-24 in rack A3 as API appservers
    Bug: T241852
    Change-Id: Ie4af05ddb35dbc36d64ec7e9dcd392f87ef852ad

--- manifests/site.pp
+++ manifests/site.pp
@@ -1650,14 +1650,14 @@ node /^mw22(6[8-9]|70)\.codfw\.wmnet$/ {
     role(mediawiki::appserver)
 }
 
-#mw2317-24 are in rack B3
-node /^mw23(1[7-9]|2[0-4])\.codfw\.wmnet$/ {
-    role(spare::system)
-}
-
 #mw2310-2316 are in rack B3
 node /^mw23(1[0-6])\.codfw\.wmnet$/ {
     role(mediawiki::appserver)
+}
+
+#mw2317-24 are in rack B3
+node /^mw23(1[7-9]|2[0-4])\.codfw\.wmnet$/ {
+    role(mediawiki::appserver::api)
 }
 
 #mw2325-34 are in rack B6
