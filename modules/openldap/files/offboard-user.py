315229b358705eb5a74f7f735f96e6d63e3eb190
John Bond <jbond@wikimedia.org>
2020-02-21 14:42:17 +0100

    offboard-user: add acl*security to list of protected phab groups
    Bug: T245771
    Change-Id: I9db5b93adc8214e2a3131fb886042306315b93af

--- modules/openldap/files/offboard-user.py
+++ modules/openldap/files/offboard-user.py
@@ -383,7 +383,7 @@ def offboard_phabricator(username, remove_all_groups, dry_run, turn_volunteer):
                            'acl*communityliaison_policy_admins', 'acl*procurement-review',
                            'acl*annual_report_policy_admins', 'acl*security_team',
                            'acl*research_collaborations_policy_admins', 'WMF-SIEM',
-                           'acl*support_and_safety_policy_admins']
+                           'acl*support_and_safety_policy_admins', 'acl*security']
 
     if len(user_query) == 0:
         print("Phabricator user", username, "not found")
