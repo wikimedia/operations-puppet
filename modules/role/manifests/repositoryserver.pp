e97678a079233b8d217d63dca2b98ea566908134
Daniel Zahn <dzahn@wikimedia.org>
2020-02-21 17:00:56 -0500

    delete role::repositoryserver, duplicate of role::apt_repo
    This role is basically identical to the role::apt_repo.
    
    Change-Id: I1aa3c56537c24c759c3104ea81e1faab85c675a5

--- modules/role/manifests/repositoryserver.pp
+++
@@ -1,9 +0,0 @@
-class role::repositoryserver {
-    system::role { 'repositoryserver': }
-
-    include ::profile::standard
-    include ::profile::base::firewall
-    include ::profile::backup::host
-
-    include ::profile::aptrepo::wikimedia
-}
