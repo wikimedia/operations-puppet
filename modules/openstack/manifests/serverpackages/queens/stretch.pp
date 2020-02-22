33bce800ff1f9bd0c6fb2d888f7570c76431d1ca
andrewbogott <abogott@wikimedia.org>
2020-02-22 15:12:33 -0600

    Openstack: add apt definitions for version 'queens'
    This is a temporary reference to http://osbpo.debian.net/debian
    for test/dev purposes.  Ulimately we'll host these packages
    ourselves.
    
    Bug: T246287
    Change-Id: If8aafd9f84ac95a93ec2a5e0d7fb18ac39b6aa45

---
+++ modules/openstack/manifests/serverpackages/queens/stretch.pp
@@ -0,0 +1,22 @@
+class openstack::serverpackages::queens::stretch(
+){
+    apt::repository { 'openstack-queens-stretch':
+        uri        => 'http://osbpo.debian.net/debian',
+        dist       => 'stretch-queens-backports',
+        components => 'main',
+        source     => false,
+        notify     => Exec['openstack-queens-stretch-apt-upgrade'],
+    }
+
+    # ensure apt can see the repo before any further Package[] declaration
+    # so this proper repo/pinning configuration applies in the same puppet
+    # agent run
+    exec { 'openstack-queens-stretch-apt-upgrade':
+        command     => '/usr/bin/apt-get update',
+        require     => Apt::Repository['openstack-queens-stretch'],
+        subscribe   => Apt::Repository['openstack-queens-stretch'],
+        refreshonly => true,
+        logoutput   => true,
+    }
+    Exec['openstack-queens-stretch-apt-upgrade'] -> Package <| |>
+}
