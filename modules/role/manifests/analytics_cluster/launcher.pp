05c34edbfd299aca6dcb69e07841d24c86daba18
Luca Toscano <ltoscano@wikimedia.org>
2020-02-24 09:19:57 +0100

    role::analytics_cluster::launcher: add git proxy config for Analytics vlan
    Bug: T243934
    Change-Id: I896ebaac20a59fdfe43c5bc6b6af74d10764c483

--- modules/role/manifests/analytics_cluster/launcher.pp
+++ modules/role/manifests/analytics_cluster/launcher.pp
@@ -15,6 +15,8 @@ class role::analytics_cluster::launcher {
 
     include ::profile::hive::site_hdfs
 
+    include ::profile::analytics::cluster::gitconfig
+
     # Include analytics/refinery deployment target.
     include ::profile::analytics::refinery
 
