435408c119ab9c51636c92e1ab9b21ab4f49f6b0
Bryan Davis <bd808@wikimedia.org>
2020-02-24 11:31:53 -0700

    codesearch: Prevent ferm from deleting Docker iptables rules
    Ferm and Docker can fight over iptables rules. This has been rediscovered
    over and over again in various projects. CI handles this by configuring
    Docker not to create its iptables chains at all and instead shipping
    a Ferm config to create them (adfd614). This patch instead tries
    a technique pioneered by JHedden while testing Ceph via rook.io of
    configuring Ferm to preserve the DOCKER* chains.
    
    A "better" fix would probably involve refactoring the code used for CI
    into an actual reusable profile for managing Docker in the presence of
    ferm and moving all of ::codesearch to ::profile::codesearch, but I am
    too lazy to do that.
    
    Bug: T246017
    Change-Id: I3f8251d67e46a25a86d6860cd2f98abff7fb96eb

--- modules/codesearch/manifests/init.pp
+++ modules/codesearch/manifests/init.pp
@@ -115,6 +115,12 @@ class codesearch(
         ]
     }
 
+    ferm::conf { 'docker-preserve':
+        ensure => present,
+        prio   => 20,
+        source => 'puppet:///modules/codesearch/ferm/docker-preserve.conf',
+    }
+
     ferm::service { 'codesearch':
         proto => 'tcp',
         port  => '3002',
