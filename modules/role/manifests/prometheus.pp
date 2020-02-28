8e5a08639b49c4604333e556914e8b60a00b7c2d
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-02-28 11:48:07 +0100

    Remove role::prometheus::k8s in favour of including the profile
    The system::role in role::prometheus::k8s triggers a false positive
    in the OS status script. profile::prometheus::k8s already exists, so
    simply add it to role::prometheus rightaway and drop role::prometheus::k8s
    
    Change-Id: I18c19c68c1f9b78a976b8718e4b01377c40cca83

--- modules/role/manifests/prometheus.pp
+++ modules/role/manifests/prometheus.pp
@@ -4,8 +4,13 @@ class role::prometheus {
     include ::role::prometheus::global
     include ::role::prometheus::services
     include ::role::prometheus::analytics
-    include ::role::prometheus::k8s
+    include ::profile::prometheus::k8s
     include ::conftool::scripts # lint:ignore:wmf_styleguide
+
+    # We only have a staging cluster in eqiad, don't poll it from both DCs
+    if $::site == 'eqiad' {
+        include ::profile::prometheus::k8s::staging
+    }
 
     include ::profile::standard
     # TODO: use profile::lvs::realserver instead
