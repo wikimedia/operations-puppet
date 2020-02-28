c47259438ed4fdbb3f35de9d01c528218f26806a
Luca Toscano <ltoscano@wikimedia.org>
2020-02-28 18:12:27 +0100

    profile::analytics::refinery::job::refine: fix el blacklist
    What:
    Blacklist MobileWebMainMenuClickTracking for eventlogging refine.
    
    Why:
    The Eventlogging table MobileWebMainMenuClickTracking is not refining
    correctly, and we decided some weeks ago to blacklist it, but it
    happened in the wrong place.
    
    Change-Id: I33b6447e263804af2c44007337dd453c1b62a9a8

--- modules/profile/manifests/analytics/refinery/job/refine.pp
+++ modules/profile/manifests/analytics/refinery/job/refine.pp
@@ -38,7 +38,7 @@ class profile::analytics::refinery::job::refine(
             input_path                      => '/wmf/data/raw/eventlogging',
             input_path_regex                => 'eventlogging_(.+)/hourly/(\\d+)/(\\d+)/(\\d+)/(\\d+)',
             input_path_regex_capture_groups => 'table,year,month,day,hour',
-            table_blacklist_regex           => '^Edit|InputDeviceDynamics|PageIssues$',
+            table_blacklist_regex           => '^Edit|InputDeviceDynamics|PageIssues|MobileWebMainMenuClickTracking$',
             # Deduplicate basd on uuid field and geocode ip in EventLogging analytics data.
             transform_functions             => 'org.wikimedia.analytics.refinery.job.refine.deduplicate_eventlogging,org.wikimedia.analytics.refinery.job.refine.geocode_ip,org.wikimedia.analytics.refinery.job.refine.eventlogging_filter_is_allowed_hostname',
             # Get EventLogging JSONSchemas from meta.wikimedia.org.
@@ -123,7 +123,6 @@ class profile::analytics::refinery::job::refine(
         'gwtoolsetUploadMetadataJob',
         'MessageGroupStatsRebuildJob',
         'fetchGoogleCloudVisionAnnotations',
-        'MobileWebMainMenuClickTracking',
     ]
     $job_table_blacklist = sprintf('.*(%s)$', join($problematic_jobs, '|'))
 
