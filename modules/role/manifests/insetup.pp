d9c11fdcb65a88b6b9ba4f8c97dc9a2641281719
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-02-28 11:13:33 +0100

    Create roles for the initial setup of a server
    Currently often servers get racked/setup without being added to site.pp,
    which causes various issues (these get e.g. flagged in the OS migration
    tracking tool as not having any role and also cause side affects in
    other tracking like Netbox). Also, they miss base::firewall that way.
    
    Sometimes systems get installed as role::spare which is also a bit
    confusing.
    
    This adds two roles to be used for the initial setup (before the actual
    production role gets applied by the service owner). There's also a
    variant without base::firewall, which e.g. needs to be applied if the
    system being racked will end up as a service without Ferm (like caches
    or OpenStack Nova).
    
    Change-Id: Ic93c5a6ce2c4ab85070df229df922929573745c9

---
+++ modules/role/manifests/insetup.pp
@@ -0,0 +1,14 @@
+# == Class: role::insetup
+#
+# Role to be applied for a server during initial setup, before it's passed
+# to the server owner for the actual application of the production role
+class role::insetup {
+
+    system::role { 'insetup':
+        ensure      => 'present',
+        description => 'Host being setup for later application of a role',
+    }
+
+    include ::profile::standard
+    include ::profile::base::firewall
+}
---
+++ modules/role/manifests/insetup_noferm.pp
@@ -0,0 +1,15 @@
+# == Class: role::insetup_noferm
+#
+# Role to be applied for a server during initial setup, before it's passed
+# to the server owner for the actual application of the production role
+# This is a variant for the setup of a service which will not use base::firewall
+# when moved to full production
+class role::insetup_noferm {
+
+    system::role { 'insetup_noferm':
+        ensure      => 'present',
+        description => 'Host being setup for later application of a role (no ferm)',
+    }
+
+    include ::profile::standard
+}
