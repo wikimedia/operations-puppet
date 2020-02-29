4fd669ec6b97b543a3301443daa402426ad7fbb7
Alex Monk <krenair@gmail.com>
2020-02-29 22:08:26 +0000

    profile::mariadb::cloudinfra: Allow overriding of hostnames
    Bug: T242607
    Change-Id: Ia596fdcdbc085b7515f3264a7c38f5cfa9b70bf0

--- modules/profile/manifests/mariadb/cloudinfra.pp
+++ modules/profile/manifests/mariadb/cloudinfra.pp
@@ -1,23 +1,27 @@
 class profile::mariadb::cloudinfra (
     Boolean $master = hiera('profile::mariadb::cloudinfra::master'),
+    Array[Stdlib::Fqdn] $puppetmasters = lookup('profile::mariadb::cloudinfra::puppetmasters'),
+    Array[Stdlib::Fqdn] $cloudinfra_dbs = lookup('profile::mariadb::cloudinfra::cloudinfra_dbs'),
 ) {
     $read_only = $master ? {
         true  => 0,
         false => 1,
     }
 
+    $puppetmasters_joined = join($puppetmasters.map |$puppetmaster| { "@resolve(${$puppetmaster})" }, ' ')
     ferm::service { 'wmcs_puppetmasters':
         proto   => 'tcp',
         port    => 3306,
         notrack => true,
-        srange  => '(@resolve(cloud-puppetmaster-01.cloudinfra.eqiad.wmflabs) @resolve(cloud-puppetmaster-02.cloudinfra.eqiad.wmflabs) @resolve(cloud-puppetmaster-03.cloudinfra.eqiad.wmflabs) @resolve(cloud-puppetmaster-04.cloudinfra.eqiad.wmflabs))',
+        srange  => "(${$puppetmasters_joined})",
     }
 
+    $cloudinfra_dbs_joined = join($cloudinfra_dbs.map |$db| { "@resolve(${$db})" }, ' ')
     ferm::service { 'mariadb_replication':
         proto   => 'tcp',
         port    => 3306,
         notrack => true,
-        srange  => '(@resolve(cloudinfra-db01.cloudinfra.eqiad.wmflabs) @resolve(cloudinfra-db02.cloudinfra.eqiad.wmflabs))',
+        srange  => "(${$cloudinfra_dbs_joined})",
     }
 
     class { 'mariadb::packages_wmf': }
