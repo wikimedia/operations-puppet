661cdc2138f48c4a627a446a67fdea34133c09a6
andrewbogott <abogott@wikimedia.org>
2020-03-02 15:49:41 -0600

    wmcs-novastats-dnsleaks: run through 2to3 and black
    Bug: T246551
    Change-Id: I90de89290cd1815cd2fc8864178f154bf11e2292

--- modules/openstack/files/pike/admin_scripts/wmcs-novastats/wmcs-novastats-dnsleaks.py
+++ modules/openstack/files/pike/admin_scripts/wmcs-novastats/wmcs-novastats-dnsleaks.py
@@ -41,12 +41,12 @@ clients = mwopenstackclients.clients()
 def designate_endpoint_and_token():
     services = clients.keystoneclient().services.list()
     for service in services:
-        if service.type == 'dns':
+        if service.type == "dns":
             serviceid = service.id
             break
     endpoints = clients.keystoneclient().endpoints.list(serviceid)
     for endpoint in endpoints:
-        if endpoint.interface == 'public':
+        if endpoint.interface == "public":
             url = endpoint.url
 
     session = clients.session()
@@ -56,41 +56,42 @@ def designate_endpoint_and_token():
 
 
 def delete_recordset(endpoint, token, zoneid, recordsetid):
-    headers = {'X-Auth-Token': token,
-               'X-Auth-Sudo-Tenant-ID': 'noauth-project',
-               'X-Designate-Edit-Managed-Records': 'true'}
+    headers = {
+        "X-Auth-Token": token,
+        "X-Auth-Sudo-Tenant-ID": "noauth-project",
+        "X-Designate-Edit-Managed-Records": "true",
+    }
     recordseturl = "%s/v2/zones/%s/recordsets/%s" % (endpoint, zoneid, recordsetid)
-    print("Deleting %s with %s" % (recordsetid, recordseturl))
-    req = requests.delete(recordseturl,
-                          headers=headers, verify=False)
+    print(("Deleting %s with %s" % (recordsetid, recordseturl)))
+    req = requests.delete(recordseturl, headers=headers, verify=False)
     req.raise_for_status()
     time.sleep(1)
 
 
 def edit_recordset(endpoint, token, zoneid, recordset, newrecords):
-    headers = {'X-Auth-Token': token,
-               'X-Auth-Sudo-Tenant-ID': 'noauth-project',
-               'X-Designate-Edit-Managed-Records': 'true'}
+    headers = {
+        "X-Auth-Token": token,
+        "X-Auth-Sudo-Tenant-ID": "noauth-project",
+        "X-Designate-Edit-Managed-Records": "true",
+    }
 
     patch = {"records": newrecords}
 
-    print("Updating %s with %s" % (recordset['id'], newrecords))
-    recordseturl = "%s/v2/zones/%s/recordsets/%s" % (endpoint, zoneid, recordset['id'])
+    print(("Updating %s with %s" % (recordset["id"], newrecords)))
+    recordseturl = "%s/v2/zones/%s/recordsets/%s" % (endpoint, zoneid, recordset["id"])
 
-    req = requests.put(recordseturl,
-                       headers=headers, verify=False,
-                       json=patch)
+    req = requests.put(recordseturl, headers=headers, verify=False, json=patch)
     req.raise_for_status()
 
 
 def recordset_is_service(recordset):
-    if recordset['type'] == 'A':
-        if recordset['name'].lower().endswith(".svc.eqiad.wmflabs."):
+    if recordset["type"] == "A":
+        if recordset["name"].lower().endswith(".svc.eqiad.wmflabs."):
             return True
         return False
 
-    if recordset['type'] == 'PTR':
-        for record in recordset['records']:
+    if recordset["type"] == "PTR":
+        for record in recordset["records"]:
             if record.lower().endswith(".svc.eqiad.wmflabs."):
                 return True
     return False
@@ -99,77 +100,106 @@ def recordset_is_service(recordset):
 def purge_duplicates(delete=False):
     (endpoint, token) = designate_endpoint_and_token()
 
-    headers = {'X-Auth-Token': token, 'X-Auth-Sudo-Tenant-ID': 'noauth-project'}
+    headers = {"X-Auth-Token": token, "X-Auth-Sudo-Tenant-ID": "noauth-project"}
     req = requests.get("%s/v2/zones" % (endpoint), headers=headers, verify=False)
     req.raise_for_status()
-    zones = yaml.safe_load(req.text)['zones']
+    zones = yaml.safe_load(req.text)["zones"]
 
     for zone in zones:
-        req = requests.get("%s/v2/zones/%s/recordsets" % (endpoint, zone['id']),
-                           headers=headers, verify=False)
+        req = requests.get(
+            "%s/v2/zones/%s/recordsets" % (endpoint, zone["id"]),
+            headers=headers,
+            verify=False,
+        )
         req.raise_for_status()
-        recordsets = yaml.safe_load(req.text)['recordsets']
+        recordsets = yaml.safe_load(req.text)["recordsets"]
 
         # we need a fresh copy of all instances so we don't accidentally
         #  delete things that have been created since we last checked.
         instances = clients.allinstances(allregions=True)
-        all_nova_instances_legacy = ["%s.%s.eqiad.wmflabs." % (instance.name.lower(),
-                                                               instance.tenant_id)
-                                     for instance in instances]
-        all_nova_instances = ["%s.%s.eqiad1.wikimedia.cloud." % (instance.name.lower(),
-                                                                 instance.tenant_id)
-                              for instance in instances]
-        all_nova_shortname_instances = ["%s.eqiad.wmflabs." % (instance.name.lower())
-                                        for instance in instances]
+        all_nova_instances_legacy = [
+            "%s.%s.eqiad.wmflabs." % (instance.name.lower(), instance.tenant_id)
+            for instance in instances
+        ]
+        all_nova_instances = [
+            "%s.%s.eqiad1.wikimedia.cloud."
+            % (instance.name.lower(), instance.tenant_id)
+            for instance in instances
+        ]
+        all_nova_shortname_instances = [
+            "%s.eqiad.wmflabs." % (instance.name.lower()) for instance in instances
+        ]
 
         for recordset in recordsets:
-            name = recordset['name'].lower()
+            name = recordset["name"].lower()
             if recordset_is_service(recordset):
                 # These are service records and shouldn't point to instances.
                 #  Leave them be.
                 continue
-            recordsetid = recordset['id']
-            if recordset['type'] == 'A':
+            recordsetid = recordset["id"]
+            if recordset["type"] == "A":
                 # For an A record, we can just delete the whole recordset
                 #  if it's for a missing instance.
-                if (name not in all_nova_instances and
-                        name not in all_nova_shortname_instances and
-                        name not in all_nova_instances_legacy):
-                    print "%s is linked to missing instance %s" % (recordsetid, name)
+                if (
+                    name not in all_nova_instances
+                    and name not in all_nova_shortname_instances
+                    and name not in all_nova_instances_legacy
+                ):
+                    print("%s is linked to missing instance %s" % (recordsetid, name))
                     if delete:
-                        delete_recordset(endpoint, token, zone['id'], recordsetid)
+                        delete_recordset(endpoint, token, zone["id"], recordsetid)
                 # If the instance exists, check to see that it doesn't have multiple IPs.
-                if len(recordset['records']) > 1:
-                    print "A record for %s has multiple IPs: %s" % (name, recordset['records'])
-                    print "This needs cleanup but that isn't implemented and almost never happens."
-            elif recordset['type'] == 'PTR':
+                if len(recordset["records"]) > 1:
+                    print(
+                        "A record for %s has multiple IPs: %s"
+                        % (name, recordset["records"])
+                    )
+                    print(
+                        "This needs cleanup but that isn't implemented and almost never happens."
+                    )
+            elif recordset["type"] == "PTR":
                 # Check each record in this set and verify that instances still exist.
-                originalrecords = recordset['records']
+                originalrecords = recordset["records"]
                 goodrecords = []
                 for record in originalrecords:
-                    if (record.lower() in all_nova_instances or
-                            record.lower() in all_nova_shortname_instances or
-                            record.lower() in all_nova_instances_legacy):
+                    if (
+                        record.lower() in all_nova_instances
+                        or record.lower() in all_nova_shortname_instances
+                        or record.lower() in all_nova_instances_legacy
+                    ):
                         goodrecords += [record]
                     else:
-                        print "PTR %s is linked to missing instance %s" % (recordsetid, record)
+                        print(
+                            "PTR %s is linked to missing instance %s"
+                            % (recordsetid, record)
+                        )
                 if not goodrecords:
                     if delete:
-                        print "Deleting the whole recordset."
-                        delete_recordset(endpoint, token, zone['id'], recordsetid)
+                        print("Deleting the whole recordset.")
+                        delete_recordset(endpoint, token, zone["id"], recordsetid)
                 else:
                     if len(goodrecords) != len(originalrecords):
                         if delete:
-                            print("Deleting partial recordset: %s vs %s" %
-                                  (goodrecords, originalrecords))
-                            edit_recordset(endpoint, token, zone['id'], recordset, goodrecords)
+                            print(
+                                (
+                                    "Deleting partial recordset: %s vs %s"
+                                    % (goodrecords, originalrecords)
+                                )
+                            )
+                            edit_recordset(
+                                endpoint, token, zone["id"], recordset, goodrecords
+                            )
 
 
-parser = argparse.ArgumentParser(description='Find (and, optionally, remove) leaked dns records.')
-parser.add_argument('--delete',
-                    dest='delete',
-                    help='Actually delete leaked records',
-                    action='store_true')
+parser = argparse.ArgumentParser(
+    description="Find (and, optionally, remove) leaked dns records."
+)
+parser.add_argument(
+    "--delete",
+    dest="delete",
+    help="Actually delete leaked records",
+    action="store_true",
+)
 args = parser.parse_args()
 
 purge_duplicates(args.delete)
