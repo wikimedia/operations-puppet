4f507d33e5a454b6af551341088542592237f158
andrewbogott <abogott@wikimedia.org>
2020-03-02 15:06:06 -0600

    codfw1dev: include clientpackages on keystone service host
    The equivalent is already present in eqiad1; not sure why it isn't here.
    
    Change-Id: I50410982ee75cd22040bdbf545c9e72aae5780bd

--- modules/profile/manifests/openstack/codfw1dev/neutron/service.pp
+++ modules/profile/manifests/openstack/codfw1dev/neutron/service.pp
@@ -2,6 +2,7 @@ class profile::openstack::codfw1dev::neutron::service(
     $version = hiera('profile::openstack::codfw1dev::version'),
     ) {
 
+    require ::profile::openstack::codfw1dev::clientpackages
     require ::profile::openstack::codfw1dev::neutron::common
     class {'::profile::openstack::base::neutron::service':
         version         => $version,
