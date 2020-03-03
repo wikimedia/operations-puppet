af82050ac80a7de55beed9e6b4a624e5e2430e8f
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-03-03 10:46:20 +0100

    openstack: serverpackages: queens: stretch: add nochange repository
    This repository contains also valuable packages for installing openstack queens
    in stretch.
    
    Bug: T246287
    Change-Id: I7e7bdfb25ac518800c5f024c6d2d873ae33b4781
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/openstack/manifests/serverpackages/queens/stretch.pp
+++ modules/openstack/manifests/serverpackages/queens/stretch.pp
@@ -8,13 +8,23 @@ class openstack::serverpackages::queens::stretch(
         notify     => Exec['openstack-queens-stretch-apt-upgrade'],
     }
 
+    apt::repository { 'openstack-queens-stretch-nochange':
+        uri        => 'http://mirrors.wikimedia.org/osbpo',
+        dist       => 'stretch-queens-backports-nochange',
+        components => 'main',
+        source     => false,
+        notify     => Exec['openstack-queens-stretch-apt-upgrade'],
+    }
+
     # ensure apt can see the repo before any further Package[] declaration
     # so this proper repo/pinning configuration applies in the same puppet
     # agent run
     exec { 'openstack-queens-stretch-apt-upgrade':
         command     => '/usr/bin/apt-get update',
-        require     => Apt::Repository['openstack-queens-stretch'],
-        subscribe   => Apt::Repository['openstack-queens-stretch'],
+        require     => [Apt::Repository['openstack-queens-stretch'],
+                        Apt::Repository['openstack-queens-stretch-nochange']],
+        subscribe   => [Apt::Repository['openstack-queens-stretch'],
+                        Apt::Repository['openstack-queens-stretch-nochange']],
         refreshonly => true,
         logoutput   => true,
     }
