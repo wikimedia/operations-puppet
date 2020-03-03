4999503ed1eb1e6b31a4ac4ba7f5fd055c8252c6
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-03-03 11:27:50 +0100

    openstack: serverpackages: introduce repo configuration for rocky/stretch
    Introduce this repo configuration for the rocky/stretch combo. Right now,
    mostly copied from queens/stretch until we have time to dig further into what
    are the actual requirements for rocky/stretch.
    
    Bug: T246287
    Change-Id: Icf515d73aef87828c090c6c10e3a89333ae5f031
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

---
+++ modules/openstack/manifests/serverpackages/rocky/stretch.pp
@@ -0,0 +1,49 @@
+class openstack::serverpackages::rocky::stretch(
+){
+    $stretch_bpo_packages = [
+      'librados2',
+      'librgw2',
+      'librbd1',
+      'python-rados',
+      'python-rbd',
+      'ceph-common',
+      'python-cephfs',
+      'libradosstriper1',
+    ]
+
+    apt::pin { 'openstack-rocky-stretch-bpo':
+        package  => join($stretch_bpo_packages, ' '),
+        pin      => 'release n=stretch-backports',
+        priority => '1002',
+    }
+
+    apt::repository { 'openstack-rocky-stretch':
+        uri        => 'http://mirrors.wikimedia.org/osbpo',
+        dist       => 'stretch-rocky-backports',
+        components => 'main',
+        source     => false,
+        notify     => Exec['openstack-rocky-stretch-apt-upgrade'],
+    }
+
+    apt::repository { 'openstack-rocky-stretch-nochange':
+        uri        => 'http://mirrors.wikimedia.org/osbpo',
+        dist       => 'stretch-rocky-backports-nochange',
+        components => 'main',
+        source     => false,
+        notify     => Exec['openstack-rocky-stretch-apt-upgrade'],
+    }
+
+    # ensure apt can see the repo before any further Package[] declaration
+    # so this proper repo/pinning configuration applies in the same puppet
+    # agent run
+    exec { 'openstack-rocky-stretch-apt-upgrade':
+        command     => '/usr/bin/apt-get update',
+        require     => [Apt::Repository['openstack-rocky-stretch'],
+                        Apt::Repository['openstack-rocky-stretch-nochange']],
+        subscribe   => [Apt::Repository['openstack-rocky-stretch'],
+                        Apt::Repository['openstack-rocky-stretch-nochange']],
+        refreshonly => true,
+        logoutput   => true,
+    }
+    Exec['openstack-rocky-stretch-apt-upgrade'] -> Package <| |>
+}
