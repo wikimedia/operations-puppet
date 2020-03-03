fa826b00a280d60a70b90856f45835d76da58986
bstorm <bstorm@wikimedia.org>
2020-03-03 15:54:48 -0700

    toolforge: remove special configuration for kubernetes on proxy servers
    At this point, we are moved off the old cluster. That means we no longer
    need flannel, kubernetes node software or any special config on the
    proxy servers to serve Toolforge.
    
    This patch is to clean up the mess. After this, we can remove the profile.
    
    Bug: T214513
    Bug: T246689
    Change-Id: I89cf46a558b003fb9ab93d1d1cd93bf2af17f35a

--- modules/profile/manifests/toolforge/k8s/webproxy.pp
+++ modules/profile/manifests/toolforge/k8s/webproxy.pp
@@ -1,8 +1,4 @@
 class profile::toolforge::k8s::webproxy (
-    $k8s_infrastructure_users   = lookup('k8s_infrastructure_users'), # complex datatype?
-    Stdlib::Fqdn  $master_host  = lookup('k8s_master'),
-    Array[String] $etcd_hosts   = lookup('flannel::etcd_hosts'),
-    String        $active_proxy = lookup('profile::toolforge::active_proxy_host', {default_value => 'tools-proxy-03'})
 ) {
     # workaround kube-proxy not playing well with iptables-nft
     if os_version('debian == buster') {
@@ -11,36 +7,26 @@ class profile::toolforge::k8s::webproxy (
         }
     }
 
-    $etcd_url = join(prefix(suffix($etcd_hosts, ':2379'), 'https://'), ',')
-
-    ferm::service { 'flannel-vxlan':
-        proto => udp,
-        port  => 8472,
+    package { [
+        'flannel',
+        'kubernetes-node',
+        'python3-pip',
+        'python3-redis',
+        'python3-requests',
+    ]:
+        ensure => absent,
     }
 
-    class { '::k8s::flannel':
-        etcd_endpoints => $etcd_url,
+    systemd::service { 'kube2proxy':
+        ensure  => absent,
+        content => '',
     }
 
-    class { '::toolforge::kube2proxy':
-        k8s_infrastructure_users => $k8s_infrastructure_users,
-        master_host              => $master_host,
-        active_proxy_host        => $active_proxy,
+    file { '/usr/local/sbin/kube2proxy':
+        ensure => absent,
     }
 
-    class { '::k8s::infrastructure_config':
-        master_host => $master_host,
-    }
-
-    class { '::k8s::proxy':
-        master_host          => $master_host,
-        metrics_bind_address => undef,
-    }
-
-    # The kubelet service is installed automatically as part of the kubernetes-node
-    # deb, we don't want it to be running on tools-proxy - so we are explicitly ensuring
-    # it's stopped
-    service { 'kubelet':
-        ensure => stopped,
+    file {'/etc/kube2proxy.yaml':
+        ensure => absent,
     }
 }
