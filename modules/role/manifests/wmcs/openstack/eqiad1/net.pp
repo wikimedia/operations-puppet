50882a2fcc8685e119551d7e870aab7988ec553f
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-03-04 12:04:45 +0100

    role: openstack: eqiad1: net: cleanup old comments
    These comments are old, clean them up.
    
    Change-Id: I6f1e08198e933ea54e581f9f7b7a202596e837b0
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/role/manifests/wmcs/openstack/eqiad1/net.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/net.pp
@@ -5,10 +5,8 @@ class role::wmcs::openstack::eqiad1::net {
     include ::profile::openstack::eqiad1::clientpackages
     include ::profile::openstack::eqiad1::observerenv
     include ::profile::openstack::eqiad1::neutron::common
-    # TODO: partial config?
     include ::profile::openstack::eqiad1::neutron::l3_agent
     include ::profile::openstack::eqiad1::neutron::dhcp_agent
-    # TODO: needs appropriate pinning for jessie mitaka
     include ::profile::openstack::eqiad1::neutron::metadata_agent
     include ::profile::wmcs::services::oidentd::proxy
 }
