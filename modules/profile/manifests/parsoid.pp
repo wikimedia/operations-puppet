f9283f11c20f875aa88fe27dcbd7aad1b5e77ff8
C. Scott Ananian <cscott@cscott.net>
2020-03-04 23:07:04 -0500

    parsoid: All profiles use_php=true, remove parameter
    All parsoid machines in production and the cloud VPS
    instance use use_php=true so we can remove this parameter
    that was made for the migration from parsoid-js to parsoid-php.
    
    Bug: T233654
    Change-Id: If42c5db9d40b2cb24c3d00b256f5c70a1d557cd2

--- modules/profile/manifests/parsoid.pp
+++ modules/profile/manifests/parsoid.pp
@@ -1,31 +1,25 @@
 class profile::parsoid(
     Boolean $has_lvs = lookup('has_lvs', {'default_value' => true }),
     Integer[1025, 65535] $port = lookup('profile::parsoid::port', {'default_value' => 8000}),
-    Boolean $use_php = lookup('profile::parsoid::use_php', {'default_value' => false }),
 ) {
     if $has_lvs {
         require ::profile::lvs::realserver
     }
 
-    if $use_php {
-        require ::profile::mediawiki::php
-        require ::profile::mediawiki::php::monitoring
-        include ::profile::mediawiki::php::restarts
-        require ::profile::mediawiki::webserver
-    }
+    require ::profile::mediawiki::php
+    require ::profile::mediawiki::php::monitoring
+    include ::profile::mediawiki::php::restarts
+    require ::profile::mediawiki::webserver
 
-    # On beta $use_php is not set but we still need sudo for restarts (T236275)
-    if $use_php or $::realm == 'labs' {
-        # Temporarily allow scap3 to restart php-fpm.
-        # This is going away, see T236275
-        # Also yes, this is all hardcoded as it's going away soon (TM)
-        sudo::user { 'scap3_restart_php':
-            user       => 'deploy-service',
-            privileges => [
-                'ALL = (root) NOPASSWD: /usr/local/sbin/restart-php7.2-fpm',
-                'ALL = (root) NOPASSWD: /bin/systemctl restart php7.2-fpm',
-            ],
-        }
+    # Temporarily allow scap3 to restart php-fpm.
+    # This is going away, see T236275
+    # Also yes, this is all hardcoded as it's going away soon (TM)
+    sudo::user { 'scap3_restart_php':
+        user       => 'deploy-service',
+        privileges => [
+            'ALL = (root) NOPASSWD: /usr/local/sbin/restart-php7.2-fpm',
+            'ALL = (root) NOPASSWD: /bin/systemctl restart php7.2-fpm',
+        ],
     }
 
     class { '::service::configuration': }
