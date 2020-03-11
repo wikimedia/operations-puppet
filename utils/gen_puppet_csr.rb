5709e7728f696cd68bb65f79d02257f0de8ede3d
John Bond <jbond@wikimedia.org>
2020-03-11 17:48:16 +0100

    utils: add puppet csr generation script
    This ruby script can be used to generate a CSR for the puppet CA
    ensuring that it has all the correct extensions enabled
    
    Change-Id: If49de5d3f03fbbbf746d6a298c85009faa2858ff

---
+++ utils/gen_puppet_csr.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# This script can be used to generate a a new CSR for the puppet master.
+# It should be run from one of the puppet frontend masters
+require 'openssl'
+
+cert = '/var/lib/puppet/server/ssl/ca/ca_crt.pem'
+key = '/var/lib/puppet/server/ssl/ca/ca_key.pem'
+
+root_cert = OpenSSL::X509::Certificate.new(File.read(cert))
+ca_key = OpenSSL::PKey::RSA.new(File.read(key))
+csr = OpenSSL::X509::Request.new
+
+csr.version = 0
+csr.subject = root_cert.subject
+csr.public_key = root_cert.public_key
+
+ef = OpenSSL::X509::ExtensionFactory.new
+
+extensions = [ef.create_extension("keyUsage", "cRLSign, keyCertSign", true),
+              ef.create_extension("basicConstraints", "CA:TRUE", true)]
+
+subject_key_identifier = root_cert.extensions.select {|ext| ext.oid == 'subjectKeyIdentifier' }
+
+if subject_key_identifier.any?
+  extensions << ef.create_extension("subjectKeyIdentifier", subject_key_identifier[0].value)
+end
+
+ext_req = OpenSSL::X509::Attribute.new("extReq",
+                                       OpenSSL::ASN1::Set([OpenSSL::ASN1::Sequence(extensions)]))
+
+csr.add_attribute(ext_req)
+csr.sign(ca_key, OpenSSL::Digest::SHA256.new)
+
+File.open("req.pem", "w") do |f|
+  f.write(csr.to_s)
+end
+puts csr.to_s
