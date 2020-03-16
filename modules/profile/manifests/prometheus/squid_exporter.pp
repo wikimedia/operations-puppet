b0c77a894fda10e5af6453a3e59990ddc545a660
Arzhel Younsi <ayounsi@wikimedia.org>
2020-03-16 12:12:09 +0100

    Prometheus Squid exporter, specify proxy port
    Bug: T245176
    Change-Id: Ib6de5c7b7a44944b19957d9761bb6aaeadb5dbb2

--- modules/profile/manifests/prometheus/squid_exporter.pp
+++ modules/profile/manifests/prometheus/squid_exporter.pp
@@ -1,7 +1,8 @@
 # Installs prometheus-squid-exporter and open matching ACLs
 
 class profile::prometheus::squid_exporter (
-    $prometheus_nodes = hiera('prometheus_nodes'),
+    Array[Stdlib::Fqdn] $prometheus_nodes = lookup('prometheus_nodes'),
+    Stdlib::HTTPUrl $http_proxy = lookup('http_proxy', {'default_value' => undef}),
 ) {
     $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
     $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
@@ -13,6 +14,18 @@ class profile::prometheus::squid_exporter (
         ensure  => running,
         require => Service['squid'], # Squid and not Squid3 again on buster
     }
+
+    if $http_proxy{
+        $proxy_port = split($http_proxy, ':')[2]
+        file { '/etc/default/prometheus-squid-exporter':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            content => template('profile/prometheus/squid-exporter.conf.erb'),
+            notify  => Service['prometheus-squid-exporter'],
+        }
+    }
+
 
     base::service_auto_restart { 'prometheus-squid-exporter': }
 
