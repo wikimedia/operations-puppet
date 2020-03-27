1c92706f421080546ef99084130667d18eed16d8
Jason Hedden <jhedden@wikimedia.org>
2020-03-27 12:26:21 -0500

    openstack: add ceph common profile to control and virt nodes
    Starting in Queens the ceph-common package is a required dependency for
    nova-common. This patch adds the ceph profile which ensures the user and group
    installed by ceph-common align with the enforced UID and GID restrictions.
    
    This does not configure any hosts to use Ceph based storage.
    
    Bug: T248610
    Change-Id: I82f41478e5047f9aadb86cdde502d9e695b191f4

--- modules/role/manifests/wmcs/openstack/eqiad1/control.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/control.pp
@@ -12,6 +12,9 @@ class role::wmcs::openstack::eqiad1::control {
     include ::profile::openstack::eqiad1::neutron::common
     include ::profile::openstack::eqiad1::neutron::service
     include ::profile::openstack::eqiad1::glance
+    # NOTE: ceph is not enabled in this role. Starting in Queens
+    #       ceph-common is a dependency for the nova-common package
+    include ::profile::ceph::common
     include ::profile::openstack::eqiad1::nova::common
     include ::profile::openstack::eqiad1::nova::conductor::service
     include ::profile::openstack::eqiad1::nova::scheduler::service
--- modules/role/manifests/wmcs/openstack/eqiad1/virt.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/virt.pp
@@ -2,6 +2,9 @@ class role::wmcs::openstack::eqiad1::virt {
     system::role { $name: }
     include ::profile::standard
     # include ::profile::base::firewall
+    # NOTE: ceph is not enabled in this role. Starting in Queens
+    #       ceph-common is a dependency for the nova-common package
+    include ::profile::ceph::common
     include ::profile::openstack::eqiad1::clientpackages
     include ::profile::openstack::eqiad1::observerenv
     include ::profile::openstack::eqiad1::nova::common
