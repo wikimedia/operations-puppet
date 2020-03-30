10e7d97297934b1cf20dbd292c86e2d97b94a53c
Valentin Gutierrez <vgutierrez@wikimedia.org>
2020-03-30 12:20:18 +0000

    site,install_server: Decommission cp2004
    Bug: T248824
    Change-Id: I873d8c9cf614a3ed568141e6f240e0c0e5d402aa

--- manifests/site.pp
+++ manifests/site.pp
@@ -270,7 +270,7 @@ node /^cp10(7[68]|8[02468]|90)\.eqiad\.wmnet$/ {
     role(cache::upload)
 }
 
-node /^cp20(0[467]|1[02369]|2[379])\.codfw\.wmnet$/ {
+node /^cp20(0[67]|1[02369]|2[379])\.codfw\.wmnet$/ {
     role(cache::text)
 }
 
