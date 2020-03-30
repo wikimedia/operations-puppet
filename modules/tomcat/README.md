9448ff26a8faab9197d98597083f7b2301c5181a
John Bond <jbond@wikimedia.org>
2020-03-30 18:50:23 +0200

    tomcat: create new tomcat module intended for use with apereo cas
    Bug: T233950
    Change-Id: I58591cb548148920c584c91a75087b31cc848a2f

---
+++ modules/tomcat/README.md
@@ -0,0 +1,5 @@
+# Tomcat module
+
+Acceptance testing can be performed with the following command
+
+`BEAKER_set="debian10" bundle exec rake beaker`
---
+++ modules/tomcat/manifests/init.pp
@@ -0,0 +1,59 @@
+# @summary module to install and configure tomcat
+class tomcat (
+    Stdlib::Host           $hostname                 = 'localhost',
+    String[1]              $user                     = 'tomcat',
+    String[1]              $group                    = 'tomcat',
+    String                 $app_base                 = 'webapps',
+    Boolean                $unpack_wars              = true,
+    Boolean                $auto_deploy              = true,
+    Stdlib::Port           $connector_port           = 8080,
+    Integer                $connector_timeout        = 20000,
+    Stdlib::Unixpath       $config_basedir           = '/etc/tomcat9',
+    Boolean                $versionlogger_listener   = true,
+    Boolean                $security_listener        = false,
+    Boolean                $apr_listener             = false,
+    Boolean                $apr_sslengine            = false,
+    Boolean                $userdatabase_realm       = false,
+    String[1]              $userdatabase_file        = 'tomcat-users.xml',
+    String[1]              $logs_dir                 = 'logs',
+    String[1]              $log_pattern              = '%h %l %u %t &quot;%r&quot; %s %b',
+    Integer[1]             $default_session_timeout  = 30,
+    Boolean                $default_servlet_debug    = false,
+    Boolean                $default_servlet_listings = false,
+    Boolean                $default_servlet_readonly = true,
+    Array[String[1]]       $default_servlet_mappings = ['/'],
+    Boolean                $jsp_servlet_fork         = false,
+    Boolean                $jsp_servlet_xpoweredby   = false,
+    Array[String[1]]       $jsp_servlet_mappings     = ['*.jsp', '*.jspx'],
+    Array[String[1]]       $welcome_files            = ['index.html', 'index.htm', 'index.jsp'],
+    Stdlib::Host           $shutdown_address         = 'localhost',
+    String                 $shutdown_string          = 'SHUTDOWN',
+    Array[String[1]]       $watched_resources        = ['WEB-INF/web.xml',
+                                                        'WEB-INF/tomcat-web.xml',
+    # lint:ignore:single_quote_string_with_variables
+                                                        '${catalina.base}/conf/web.xml'],
+    # lint:endignore
+    Optional[Stdlib::Port] $shutdown_port           = undef,
+){
+    ensure_packages(['tomcat9'])
+    file{
+        default:
+            ensure => file,
+            owner  => 'root',
+            mode   => '0640',
+            group  => $group,
+            notify => Service['tomcat9'];
+        $config_basedir:
+            ensure => directory;
+        "${config_basedir}/server.xml":
+            content => template('tomcat/server.xml.erb');
+        "${config_basedir}/context.xml":
+            content => template('tomcat/context.xml.erb');
+        "${config_basedir}/web.xml":
+            content => template('tomcat/web.xml.erb');
+    }
+    service{'tomcat9':
+        ensure => 'running',
+        enable => true,
+    }
+}
