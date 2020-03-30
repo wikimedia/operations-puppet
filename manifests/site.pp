a85d36df3f438f10ae2ad791f4d77eea847ad93f
Valentin Gutierrez <vgutierrez@wikimedia.org>
2020-03-30 12:50:05 +0000

    site,install_server: Decommission cp2005
    Bug: T248848
    Change-Id: I796b9cdcde14f87871c43914be82da372fed046d

--- manifests/site.pp
+++ manifests/site.pp
@@ -274,7 +274,7 @@ node /^cp20(0[67]|1[02369]|2[379]|31)\.codfw\.wmnet$/ {
     role(cache::text)
 }
 
-node /^cp20(0[58]|1[1478]|2[024568]|30)\.codfw\.wmnet$/ {
+node /^cp20(08|1[1478]|2[024568]|30)\.codfw\.wmnet$/ {
     role(cache::upload)
 }
 
