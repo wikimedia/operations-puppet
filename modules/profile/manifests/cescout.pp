c53fa90b699ba17fbea965188a523a31570b5956
Sukhbir Singh <ssingh@wikimedia.org>
2020-03-31 13:37:04 -0400

    cescout: add HTTPS proxy support
    Bug: T247273
    Change-Id: Ibb25392b1b722bcbd92b47566ad0766a280e4459

--- modules/profile/manifests/cescout.pp
+++ modules/profile/manifests/cescout.pp
@@ -1,3 +1,12 @@
 class profile::cescout {
     require_package('cescout')
+
+    # enable system-wide proxy for cescout
+    file { '/etc/profile.d/cescout.sh':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('cescout/cescout.sh.erb'),
+    }
 }
