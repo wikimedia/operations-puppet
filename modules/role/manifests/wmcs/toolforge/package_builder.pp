3cc56d1ec141bc761072ff52833d5a3a3a1c2a05
bstorm <bstorm@wikimedia.org>
2020-03-31 15:31:39 -0700

    toolforge: update the package_builder role so it can be neatly used
    Right now, the puppet prefix uses a set of roles and one profile directly.
    That's weird, so this will be able to assume things nicely. This also finishes
    what was started in Ic71ded4884751560dc2d16e5cadbde16b7fadc23
    
    I never completed or applied that to the builder host.
    
    Change-Id: Id2ad846d757cbca38e310b46fb6823809de8aae5

--- modules/role/manifests/wmcs/toolforge/package_builder.pp
+++ modules/role/manifests/wmcs/toolforge/package_builder.pp
@@ -1,7 +1,9 @@
 class role::wmcs::toolforge::package_builder {
-    include ::profile::toolforge::package_builder
-
     system::role { $name:
         description => 'Debian package builder'
     }
+
+    include ::profile::toolforge::apt_pinning
+    include ::profile::labs::lvm::srv
+    include ::profile::toolforge::package_builder
 }
