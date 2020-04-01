62ad45a8497e396f0679afe7edc636625d9ba825
Valentin Gutierrez <vgutierrez@wikimedia.org>
2020-04-01 12:40:26 +0000

    site: Reimage cp2041 as cache::text
    Bug: T248816
    Change-Id: Ib86f383c14a542448bbb594c2af50dc208a35ca6

--- manifests/site.pp
+++ manifests/site.pp
@@ -265,7 +265,7 @@ node /^cp10(7[68]|8[02468]|90)\.eqiad\.wmnet$/ {
     role(cache::upload)
 }
 
-node /^cp20(1[69]|2[379]|3[13579])\.codfw\.wmnet$/ {
+node /^cp20(1[69]|2[379]|3[13579]|41)\.codfw\.wmnet$/ {
     role(cache::text)
 }
 
@@ -280,7 +280,7 @@ node /^cp20(0[39]|15|21)\.codfw\.wmnet$/ {
 }
 
 # new cp nodes (T247340)
-node /^cp20(4[1-2])\.codfw\.wmnet$/ {
+node 'cp2042.codfw.wmnet' {
     role(insetup_noferm)
 }
 
