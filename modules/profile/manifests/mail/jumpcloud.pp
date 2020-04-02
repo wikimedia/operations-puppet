bd1fab9be09d4781389b26e887f7290043586bfa
John Bond <jbond@wikimedia.org>
2020-04-02 16:12:50 +0200

    profile::mail::jumpcloud: add new class to manage jumpcloud aliases
    This profile installs a script which will query the jump cloud api and
    write out a file containing all the users who should be configured for
    mail delivery on the MX servers.
    
    When writing the file if an aliases existed but has been removed from
    jumpcloud instead of removing the email address entirely we alias it to
    a special email address $removed_alias@$managed_domain.  This is
    intended as a stop gap to make sure we don't accidentally remove users
    however it
    
    Host: O:mail::mx
    
    Bug: T244792
    Change-Id: I3dcfc08b365117b209787e3cb9eab8f279aaaf6f

---
+++ modules/profile/manifests/mail/jumpcloud.pp
@@ -0,0 +1,44 @@
+# @summary a class to install the jumpcloud scraping script and create the correct exim files
+# @param api_uri the jumpcloud api uri endpoint
+# @param api_key The jumpcloud API key
+# @managed_domain The domain name of the jumpcloud managed domain
+class profile::mail::jumpcloud (
+    Stdlib::Host     $api_uri        = lookup('profile::mail::jumpcloud::api_uri'),
+    String[40,40]    $api_key        = lookup('profile::mail::jumpcloud::api_key'),
+    Stdlib::Host     $managed_domain = lookup('profile::mail::jumpcloud::managed_domain'),
+) {
+    # TODO aliases dir is currently hardcoded in exim, profile::mail::mx and here
+    # we should update the code so it is configured in profile::mx and passed to
+    # this class and exim
+    $aliases_dir = '/etc/exim4/aliases'
+    $config = @(CONFIG)
+    [DEFAULT]
+    api_uri: ${api_uri}
+    api_key: ${api_key}
+    aliases_directory: ${aliases_dir}
+    managed_domain: ${managed_domain}
+    | CONFIG
+    file {
+        default:
+            ensure => file,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0550';
+        '/usr/local/sbin/jumpcloud_aliases':
+            content => file('profile/mail/jumpcloud_aliases.py');
+        '/etc/jumpcloud.ini':
+            mode    => '0440',
+            content => $config;
+    }
+    systemd::timer::job {'generate jumpcloud aliases':
+        ensure             => present,
+        description        => 'Generte local mailparts from jumpcloud API',
+        command            => '/usr/local/sbin/jumpcloud_aliases',
+        interval           => {
+            'start'    => 'OnCalendar',
+            'interval' => '*-*-* *:15:00', # Every hour at minute 15
+        },
+        monitoring_enabled => true,
+        user               => 'root',
+    }
+}
