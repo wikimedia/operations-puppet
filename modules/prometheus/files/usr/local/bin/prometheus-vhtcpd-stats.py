1318ad09c143a7004ebc2d5b62e124243fe5e7b9
Chris Danis <cdanis@wikimedia.org>
2020-04-05 12:11:58 -0400

    update vhtcpd exporter to match reality
    * Rename metrics to match Prometheus naming best practices
    * Update to output format in use since Oct 2017
    * Add per-purger-worker stats
    * Remove some redundant metrics
    * Clarify some metric docstrings
    * Use python3
    
    Bug: T249346
    Bug: T249325
    Change-Id: Iadf788f9e8b1ceb2a9d504e1541c6483faa7466c

--- modules/prometheus/files/usr/local/bin/prometheus-vhtcpd-stats.py
+++ modules/prometheus/files/usr/local/bin/prometheus-vhtcpd-stats.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # Copyright 2016 Filippo Giunchedi
 #                Wikimedia Foundation
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,47 +24,89 @@ log = logging.getLogger(__name__)
 
 
 def collect_vhtcp_stats(infile, registry):
+    # Metric documentation: https://phabricator.wikimedia.org/T249346#6026839
+    #
+    # Many of these are more properly counters than gauges, but the Prometheus
+    # server is typeless & doesn't care -- Counter vs Gauge only affects the
+    # client-side APIs (and Counter doesn't make .set() available).
     metrics = {
         'start': Gauge(
-            'start', 'Start timestamp', namespace='vhtcpd', registry=registry),
+            'start', 'Start timestamp epoch seconds',
+            namespace='vhtcpd', registry=registry),
         'uptime': Gauge(
-            'uptime', 'Daemon uptime', namespace='vhtcpd', registry=registry),
-        'inpkts_recvd': Gauge(
-            'inpkts_recvd', 'Packets received', namespace='vhtcpd',
-            registry=registry),
-        'inpkts_sane': Gauge(
-            'inpkts_sane', 'Packets received and correct', namespace='vhtcpd',
-            registry=registry),
-        'inpkts_enqueued': Gauge(
-            'inpkts_enqueued',
-            'Packets received and made it to the send queue',
+            'uptime', 'Daemon uptime in seconds',
             namespace='vhtcpd', registry=registry),
-        'inpkts_dequeued': Gauge(
-            'inpkts_dequeued',
-            'Packets sent as TCP PURGE to all applicable varnish instances',
+        'input_packets_total': Gauge(
+            'input_packets_total', 'Packets received by the shared listener',
             namespace='vhtcpd', registry=registry),
-        'queue_overflows': Gauge(
-            'queue_overflows',
-            'How many times the queue was overflowed', namespace='vhtcpd',
-            registry=registry),
-        'queue_size': Gauge(
-            'queue_size',
-            'Number of packets in the queue',
-            namespace='vhtcpd', registry=registry),
-        'queue_max_size': Gauge(
-            'queue_max_size',
-            'Maximum number packets in the queue since start/overflow',
-            namespace='vhtcpd', registry=registry),
+        'input_packets_bad_total': Gauge(
+            'input_packets_bad_total',
+            'Packets received by the shared listener that were somehow faulty',
+            namespace='vhtcpd', registry=registry
+        ),
+        'input_packets_filtered_total': Gauge(
+            'input_packets_filtered_total',
+            'Packets received by the listener but filtered by configuration',
+            namespace='vhtcpd', registry=registry
+        ),
+        'worker_input_packets_total': Gauge(
+            'worker_input_packets_total',
+            'Total packets enqueued on a given purger worker',
+            ['worker'], namespace='vhtcpd', registry=registry
+        ),
+        'worker_input_packets_failed_total': Gauge(
+            'worker_input_packets_failed_total',
+            'Packets a given purger worker failed to deliver',
+            ['worker'], namespace='vhtcpd', registry=registry
+        ),
+        'worker_queue_length': Gauge(
+            'worker_queue_length',
+            'Instantaneous queue size of each worker',
+            ['worker'], namespace='vhtcpd', registry=registry
+        ),
+        'worker_queue_size_bytes': Gauge(
+            'worker_queue_size_bytes',
+            'Number of memory bytes consumed by each worker\'s queue',
+            ['worker'], namespace='vhtcpd', registry=registry
+        ),
     }
 
-    stats = infile.readlines()[0].split(' ')
-    for stat in stats:
-        k, v = stat.split(':', 1)
-        if k in metrics:
-            try:
-                metrics[k].set(int(v))
-            except ValueError as e:
-                log.warn('Error parsing %r: %r', stat, e)
+    # Maps vhtcpd output abbreviations to keys in the metrics dict.
+    vhtcpd_output_to_metric = {
+        # shared listener ('start:....') metrics
+        'start': 'start',
+        'uptime': 'uptime',
+        'recvd': 'input_packets_total',
+        'bad': 'input_packets_bad_total',
+        'filtered': 'input_packets_filtered_total',
+        # per-worker metrics
+        'input': 'worker_input_packets_total',
+        'failed': 'worker_input_packets_failed_total',
+        'q_size': 'worker_queue_length',
+        'q_mem': 'worker_queue_size_bytes',
+    }
+
+    def parse_metrics(stats, worker=None):
+        for stat in stats:
+            k, v = stat.split(':', 1)
+            if k in vhtcpd_output_to_metric:
+                m = vhtcpd_output_to_metric[k]
+                try:
+                    if worker:
+                        metrics[m].labels(worker=worker).set(int(v))
+                    else:
+                        metrics[m].set(int(v))
+                except ValueError as e:
+                    log.warn('Error parsing %r: %r', stat, e)
+
+    for line in infile.readlines():
+        if line.startswith('start:'):
+            stats = line.split(' ')
+            parse_metrics(stats)
+        else:
+            worker, rest = line.split(': ', 1)
+            stats = rest.split(' ')
+            parse_metrics(stats, worker=worker)
 
 
 def main():
@@ -98,7 +140,7 @@ def main():
     if args.outfile:
         write_to_textfile(args.outfile, registry)
     else:
-        sys.stdout.write(generate_latest(registry))
+        sys.stdout.buffer.write(generate_latest(registry))
 
 
 if __name__ == "__main__":
