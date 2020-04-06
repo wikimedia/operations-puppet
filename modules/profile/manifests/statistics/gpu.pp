7a36511a7788e0a0c83d2b8d49807aba2a1c59c9
Luca Toscano <ltoscano@wikimedia.org>
2020-04-06 13:17:21 +0200

    profile::statistics::gpu: upgrade stat1008 to ROCm 3.3
    Bug: T247082
    Change-Id: I17468fc147946e7d9252919086dc85222e6ae5a4

--- modules/profile/manifests/statistics/gpu.pp
+++ modules/profile/manifests/statistics/gpu.pp
@@ -5,10 +5,17 @@ class profile::statistics::gpu {
     $hosts_with_gpu = ['stat1005', 'stat1008']
 
     if $::hostname in $hosts_with_gpu {
-        class { 'amd_rocm':
-            version => '271',
+
+        # Testing new version - T247082
+        if $::hostname == 'stat1008' {
+            $rocm_version = '33'
+        } else {
+            $rocm_version = '271'
         }
 
+        class { 'amd_rocm':
+            version => $rocm_version,
+        }
         class { 'prometheus::node_amd_rocm': }
     }
 }
