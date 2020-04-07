17ca4de27275160e853379391d103f0492e9544c
Timo Tijhof <krinklemail@gmail.com>
2020-04-07 16:57:27 +0100

    mediawiki/maintenance: add startupregistrystats for test.wp.o and mw.o
    Ensure we have telemetry from group0 wikis.
    
    Bug: T233678
    Change-Id: Ic3191025366b306c31f9be921b2d057664ae9926

--- modules/mediawiki/manifests/maintenance/startupregistrystats.pp
+++ modules/mediawiki/manifests/maintenance/startupregistrystats.pp
@@ -1,6 +1,23 @@
 class mediawiki::maintenance::startupregistrystats(
     $ensure = present
 ) {
+    # group0: test.wikipedia.org
+    cron { 'startupregistrystats-testwiki':
+        ensure  => $ensure,
+        user    => $::mediawiki::users::web,
+        minute  => 10,
+        command => '/usr/local/bin/mwscript extensions/WikimediaMaintenance/blameStartupRegistry.php --wiki testwiki --record-stats > /dev/null 2>&1',
+    }
+
+    # group0: mediawiki.org
+    cron { 'startupregistrystats-mediawikiwiki':
+        ensure  => $ensure,
+        user    => $::mediawiki::users::web,
+        minute  => 15,
+        command => '/usr/local/bin/mwscript extensions/WikimediaMaintenance/blameStartupRegistry.php --wiki mediawikiwiki --record-stats > /dev/null 2>&1',
+    }
+
+    # large wikis (inludes several group1 and group2 wikis)
     cron { 'startupregistrystats':
         ensure  => $ensure,
         user    => $::mediawiki::users::web,
