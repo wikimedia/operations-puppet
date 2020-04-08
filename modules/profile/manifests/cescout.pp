2c3cd4b79bfb69c9d67060cd5347dbb94f904541
Sukhbir Singh <ssingh@wikimedia.org>
2020-04-08 10:34:21 -0400

    cescout: add OONI's metadb sync scripts
    Add the script that sets up the OONI metadb sync. OONI's metadb database
    has all current and historic censorship measurements from ooni.org and
    this data is useful in understanding censorship events.
    
    Bug: T247273
    Change-Id: I869ad6bd7a73b6ba10280b0a8653a0bba8653404

--- modules/profile/manifests/cescout.pp
+++ modules/profile/manifests/cescout.pp
@@ -1,6 +1,9 @@
 class profile::cescout {
     require_package('cescout')
 
+    # required by metadb_s3_tarx
+    require_package('make', 'bc')
+
     # enable system-wide proxy for cescout
     file { '/etc/profile.d/cescout.sh':
         ensure  => present,
@@ -8,5 +11,24 @@ class profile::cescout {
         group   => 'root',
         mode    => '0444',
         content => template('cescout/cescout.sh.erb'),
+    }
+
+    # directory for saving metadb data. the OONI scripts use (and expect)
+    # /mnt/metadb as they mount an EBS volume on EC2; since we don't do that,
+    # we can use any directory and later point the metadb_s3_tarx to it.
+    file { '/var/lib/metadb/':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    # copy the metadb_s3_tarx file, the script that sets up the metadb sync.
+    file { '/usr/local/sbin/metadb_s3_tarx':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0544',
+        content => template('cescout/metadb_s3_tarx.erb'),
     }
 }
