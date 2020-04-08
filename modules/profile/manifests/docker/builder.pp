06712c9974e5ca8d9311205cf3ebae0bf49b1821
Alexandros Kosiaris <akosiaris@wikimedia.org>
2020-04-08 17:36:49 +0300

    profile::docker::builder: Add buster, drop jessie
    Why:
    We want to have the tool build our base images and we want to drop
    jessie image support from our infrastructure
    
    What:
    Add buster, drop jessie
    
    Bug: T249724
    Change-Id: I4a175f9abc9192452207ca0357b54e81304dd77b

--- modules/profile/manifests/docker/builder.pp
+++ modules/profile/manifests/docker/builder.pp
@@ -37,7 +37,7 @@ class profile::docker::builder(
         docker_registry => $registry,
         proxy_address   => $proxy_address,
         proxy_port      => $proxy_port,
-        distributions   => ['jessie', 'stretch', 'alpine'],
+        distributions   => ['buster', 'stretch', 'alpine'],
     }
 
     require_package('python3-virtualenv', 'virtualenv')
