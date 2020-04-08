a15c1bcde9724d8236df1fabb5213c0a9e4842a9
Daniel Zahn <dzahn@wikimedia.org>
2020-04-08 10:12:45 -0400

    ci: allow rsyncing of data dirs for server migrations
    Bug: T224591
    Change-Id: I75696bf11e522e4d19c9b9624ed00b63ef34729d

---
+++ modules/profile/manifests/ci/data_rsync.pp
@@ -0,0 +1,29 @@
+# allow rsyncing data between CI servers during server migrations
+class profile::ci::data_rsync (
+    Stdlib::Fqdn $src_host = lookup(profile::ci::migration::rsync_src_host),
+    Array[Stdlib::Fqdn] $dst_hosts = lookup(profile::ci::migration::rsync_dst_hosts),
+    Array[Stdlib::Unixpath] $data_dirs = lookup(profile::ci::migration::rsync_data_dirs),
+) {
+
+    if $::fqdn in $dst_hosts {
+
+        ferm::service { 'ci-migration-rsync':
+            proto  => 'tcp',
+            port   => '873',
+            srange => "(@resolve((${src_host})) @resolve((${src_host}), AAAA))",
+        }
+
+        class { '::rsync::server': }
+
+        $data_dirs.each |String $data_dir| {
+
+            $module_name = regsubst($data_dir, '\/', '-', 'G')
+
+            rsync::server::module { "ci-${module_name}":
+                path        => $data_dir,
+                read_only   => 'no',
+                hosts_allow => $src_host,
+            }
+        }
+    }
+}
