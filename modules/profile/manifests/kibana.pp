aa0eb5c55bfcc750aa3c8c5e739352e4abf397af
Luca Toscano <ltoscano@wikimedia.org>
2020-04-08 08:45:50 +0200

    profile::kibana: add the package_name parameter
    What:
    Add the 'package_name' parameter to profile::kibana.
    
    Why:
    Instead of setting the kibana class variable in hiera, it seems
    cleaner to pass through the profile hiera lookup (easier to find in
    puppet etc..).
    
    Bug: T246961
    Change-Id: I9911771dcbbb985229fc366d6f4e866384e4a681

--- modules/profile/manifests/kibana.pp
+++ modules/profile/manifests/kibana.pp
@@ -1,8 +1,10 @@
 # == Class: profile::kibana
 class profile::kibana (
   Boolean $enable_phatality = lookup('profile::kibana::enable_phatality', {'default_value' => true}),
+  String $package_name      = lookup('profile::kibana::package_name', {'default_value' => 'kibana'})
   ) {
     class { 'kibana':
       enable_phatality => $enable_phatality,
+      kibana_package   => $package_name,
     }
 }
