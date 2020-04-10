b4626ad372fbd56fb78f1a49b4d6d3b7723dbca5
Luca Toscano <ltoscano@wikimedia.org>
2020-04-10 11:36:07 +0200

    role::analytics_cluster::coordinator: absent data_purge
    Bug: T249593
    Change-Id: Ice901987e272432a43d845425914e7c03cd328e4

--- modules/profile/manifests/analytics/refinery/job/data_purge.pp
+++ modules/profile/manifests/analytics/refinery/job/data_purge.pp
@@ -10,6 +10,7 @@
 class profile::analytics::refinery::job::data_purge (
     $public_druid_host = lookup('profile::analytics::refinery::job::data_purge::public_druid_host', { 'default_value' => undef }),
     $use_kerberos = lookup('profile::analytics::refinery::job::data_purge::use_kerberos', { 'default_value' => false }),
+    $ensure_timers = lookup('profile::analytics::refinery::job::data_purge::ensure_timers', { 'default_value' => 'present' }),
 ) {
     require ::profile::analytics::refinery
 
@@ -33,6 +34,7 @@ class profile::analytics::refinery::job::data_purge (
     # Keep this many days of raw webrequest data.
     $raw_retention_days = 31
     kerberos::systemd_timer { 'refinery-drop-webrequest-raw-partitions':
+        ensure       => $ensure_timers,
         description  => 'Drop Webrequest raw data imported on HDFS following data retention policies.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --database='wmf_raw' --tables='webrequest' --base-path='/wmf/data/raw/webrequest' --path-format='.+/hourly/(?P<year>[0-9]+)(/(?P<month>[0-9]+)(/(?P<day>[0-9]+)(/(?P<hour>[0-9]+))?)?)?' --older-than='${raw_retention_days}' --skip-trash --execute='96726ec893174544fc9bd7c7fa0083ea'",
         interval     => '*-*-* 00/4:15:00',
@@ -59,6 +61,7 @@ class profile::analytics::refinery::job::data_purge (
     # Keep this many days of raw event data (all data that comes via EventGate instances).
     $event_raw_retention_days = 90
     kerberos::systemd_timer { 'refinery-drop-raw-event':
+        ensure       => $ensure_timers,
         description  => 'Drop raw event (/wmf/data/raw/event) data imported on HDFS following data retention policies.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --base-path='/wmf/data/raw/event' --path-format='.+/hourly/(?P<year>[0-9]+)(/(?P<month>[0-9]+)(/(?P<day>[0-9]+)(/(?P<hour>[0-9]+))?)?)?' --older-than='${event_raw_retention_days}' --skip-trash --execute='209837413aff8d4332104e7dc454a27d'",
         interval     => '*-*-* 00/4:20:00',
@@ -98,6 +101,7 @@ class profile::analytics::refinery::job::data_purge (
     # \w to get the proper checksum.
     $event_refined_to_drop.each |String $dataset, String $checksum| {
         kerberos::systemd_timer { "refinery-drop-refined-event.${dataset}":
+            ensure       => $ensure_timers,
             description  => "Drop refined event.${dataset} data imported on Hive/HDFS following data retention policies.",
             command      => "${refinery_path}/bin/refinery-drop-older-than --database='event' --tables='^${dataset}$$' --base-path='/wmf/data/event/${dataset}' --path-format='datacenter=\\\\w+(/year=(?P<year>[0-9]+)(/month=(?P<month>[0-9]+)(/day=(?P<day>[0-9]+)(/hour=(?P<hour>[0-9]+))?)?)?)?' --older-than='90' --skip-trash --execute='${checksum}'",
             interval     => '*-*-* 00:00:00',
@@ -110,6 +114,7 @@ class profile::analytics::refinery::job::data_purge (
     # Keep this many days of raw eventlogging data.
     $eventlogging_retention_days = 90
     kerberos::systemd_timer { 'refinery-drop-eventlogging-partitions':
+        ensure       => $ensure_timers,
         description  => 'Drop Eventlogging data imported on HDFS following data retention policies.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --base-path='/wmf/data/raw/eventlogging' --path-format='.+/hourly/(?P<year>[0-9]+)(/(?P<month>[0-9]+)(/(?P<day>[0-9]+)(/(?P<hour>[0-9]+))?)?)?' --older-than='${eventlogging_retention_days}' --skip-trash --execute='bb7022b36bcf0d75bdd03b6f836f09e6'",
         interval     => '*-*-* 00/4:15:00',
@@ -119,6 +124,7 @@ class profile::analytics::refinery::job::data_purge (
     }
 
     kerberos::systemd_timer { 'refinery-drop-eventlogging-client-side-partitions':
+        ensure       => $ensure_timers,
         description  => 'Drop Eventlogging Client Side data imported on HDFS following data retention policies.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --base-path='/wmf/data/raw/eventlogging_client_side' --path-format='eventlogging-client-side/hourly/(?P<year>[0-9]+)(/(?P<month>[0-9]+)(/(?P<day>[0-9]+)(/(?P<hour>[0-9]+))?)?)?' --older-than='${eventlogging_retention_days}' --skip-trash --execute='1fcad629ec569645ff864686e523029d'",
         interval     => '*-*-* 00/4:30:00',
@@ -131,6 +137,7 @@ class profile::analytics::refinery::job::data_purge (
     # Currently being tested as systemd timer, see below
     $druid_webrequest_sampled_retention_days = 60
     kerberos::systemd_timer { 'refinery-drop-webrequest-sampled-druid':
+        ensure       => $ensure_timers,
         description  => 'Drop Druid Webrequest sampled data from deep storage following data retention policies.',
         command      => "${refinery_path}/bin/refinery-drop-druid-deep-storage-data -d ${druid_webrequest_sampled_retention_days} webrequest_sampled_128",
         interval     => '*-*-* 05:15:00',
@@ -145,6 +152,7 @@ class profile::analytics::refinery::job::data_purge (
         $druid_public_keep_snapshots = 4
         $mediawiki_history_reduced_basename = 'mediawiki_history_reduced'
         kerberos::systemd_timer { 'refinery-druid-drop-public-snapshots':
+            ensure       => $ensure_timers,
             description  => 'Drop Druid Public snapshots from deep storage following data retention policies.',
             command      => "${refinery_path}/bin/refinery-drop-druid-snapshots -d ${mediawiki_history_reduced_basename} -t ${public_druid_host} -s ${druid_public_keep_snapshots} -f ${public_druid_snapshots_log_file}",
             environment  => $systemd_env,
@@ -158,6 +166,7 @@ class profile::analytics::refinery::job::data_purge (
     # runs once a month
     $keep_snapshots = 6
     kerberos::systemd_timer { 'mediawiki-history-drop-snapshot':
+        ensure       => $ensure_timers,
         description  => 'Drop snapshots from multiple raw and refined mediawiki datasets, configured in the refinery-drop script.',
         command      => "${refinery_path}/bin/refinery-drop-mediawiki-snapshots -s ${keep_snapshots}",
         environment  => $systemd_env,
@@ -170,6 +179,7 @@ class profile::analytics::refinery::job::data_purge (
     # Runs on the first day of each month. This way it frees up space for the new snapshot.
     $mediawiki_history_dumps_retention_days = 180
     kerberos::systemd_timer { 'refinery-drop-mediawiki-history-dumps':
+        ensure       => $ensure_timers,
         description  => 'Drop mediawiki history dump versions older than 6 months.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --base-path='/wmf/data/archive/mediawiki/history' --path-format='(?P<year>[0-9]+)-(?P<month>[0-9]+)' --older-than='${mediawiki_history_dumps_retention_days}' --skip-trash --execute='40f24e7bbccb397671dfa3266c5ebd2f'",
         interval     => '*-*-01 00:00:00',
@@ -182,6 +192,7 @@ class profile::analytics::refinery::job::data_purge (
     # runs once a day
     $banner_activity_retention_days = 90
     kerberos::systemd_timer { 'refinery-drop-banner-activity':
+        ensure       => $ensure_timers,
         description  => 'Clean old Banner Activity _SUCCESS flags from HDFS.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --base-path='/wmf/data/wmf/banner_activity' --path-format='daily/year=(?P<year>[0-9]+)(/month=(?P<month>[0-9]+)(/day=(?P<day>[0-9]+))?)?' --older-than='${banner_activity_retention_days}' --skip-trash --execute='39b7f84330a54c2128f6ade41feba28b'",
         environment  => $systemd_env,
@@ -199,6 +210,7 @@ class profile::analytics::refinery::job::data_purge (
     }
 
     file { '/usr/local/bin/refinery-eventlogging-saltrotate':
+        ensure  => $ensure_timers,
         content => template('profile/analytics/refinery/job/refinery-eventlogging-saltrotate.erb'),
         mode    => '0550',
         owner   => 'analytics',
@@ -207,6 +219,7 @@ class profile::analytics::refinery::job::data_purge (
 
     # Timer runs at midnight (salt rotation time):
     kerberos::systemd_timer { 'refinery-eventlogging-saltrotate':
+        ensure       => $ensure_timers,
         description  => 'Create, rotate and delete cryptographic salts for EventLogging sanitization.',
         command      => '/usr/local/bin/refinery-eventlogging-saltrotate',
         environment  => $systemd_env,
@@ -231,6 +244,7 @@ class profile::analytics::refinery::job::data_purge (
     # Execute 1st sanitization pass, right after data collection. Runs once per hour.
     # Job starts a couple minutes after the hour, to leave time for the salt files to be updated.
     profile::analytics::refinery::job::refine_job { 'sanitize_eventlogging_analytics_immediate':
+        ensure              => $ensure_timers,
         job_class           => 'org.wikimedia.analytics.refinery.job.refine.EventLoggingSanitization',
         monitor_class       => 'org.wikimedia.analytics.refinery.job.refine.EventLoggingSanitizationMonitor',
         job_config          => $eventlogging_sanitization_job_config,
@@ -243,6 +257,7 @@ class profile::analytics::refinery::job::data_purge (
     # Execute 2nd sanitization pass, after 45 days of collection.
     # Runs once per day at a less busy time.
     profile::analytics::refinery::job::refine_job { 'sanitize_eventlogging_analytics_delayed':
+        ensure              => $ensure_timers,
         job_class           => 'org.wikimedia.analytics.refinery.job.refine.EventLoggingSanitization',
         monitor_class       => 'org.wikimedia.analytics.refinery.job.refine.EventLoggingSanitizationMonitor',
         job_config          => $eventlogging_sanitization_job_config.merge({
@@ -266,6 +281,7 @@ class profile::analytics::refinery::job::data_purge (
     # So, if you want to make changes to this job, make sure to execute all tests (DRY-RUN) with just 1
     # single $ sign, to get the correct checksum. And then add the double $$ sign here.
     kerberos::systemd_timer { 'drop-el-unsanitized-events':
+        ensure       => $ensure_timers,
         description  => 'Drop unsanitized EventLogging data from the event database after retention period.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --database='event' --tables='^(?!wmdebanner)[A-Za-z0-9]+$$' --base-path='/wmf/data/event' --path-format='(?!WMDEBanner)[A-Za-z0-9]+/year=(?P<year>[0-9]+)(/month=(?P<month>[0-9]+)(/day=(?P<day>[0-9]+)(/hour=(?P<hour>[0-9]+))?)?)?' --older-than='90' --skip-trash --execute='dc3b5e020579ae5516b7f372081d1fac' --log-file='${el_unsanitized_log_file}'",
         interval     => '*-*-* 00:00:00',
@@ -279,6 +295,7 @@ class profile::analytics::refinery::job::data_purge (
     # So, if you want to make changes to this job, make sure to execute all tests (DRY-RUN) with just 1
     # single $ sign, to get the correct checksum. And then add the double $$ sign here.
     kerberos::systemd_timer { 'drop-el-helppanel-events':
+        ensure       => $ensure_timers,
         description  => 'Drop HelpPanel data from the event_sanitized database after 270 days.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --database='event_sanitized' --tables='^helppanel$$' --base-path='/wmf/data/event_sanitized/HelpPanel' --path-format='year=(?P<year>[0-9]+)(/month=(?P<month>[0-9]+)(/day=(?P<day>[0-9]+)(/hour=(?P<hour>[0-9]+))?)?)?' --older-than='270' --skip-trash --execute='e1447ccd3a6f808d038e2f4656e5a016'",
         interval     => '*-*-* 04:40:00',
@@ -287,6 +304,7 @@ class profile::analytics::refinery::job::data_purge (
         use_kerberos => $use_kerberos,
     }
     kerberos::systemd_timer { 'drop-el-homepagevisit-events':
+        ensure       => $ensure_timers,
         description  => 'Drop HomepageVisit data from the event_sanitized database after 270 days.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --database='event_sanitized' --tables='^homepagevisit$$' --base-path='/wmf/data/event_sanitized/HomepageVisit' --path-format='year=(?P<year>[0-9]+)(/month=(?P<month>[0-9]+)(/day=(?P<day>[0-9]+)(/hour=(?P<hour>[0-9]+))?)?)?' --older-than='270' --skip-trash --execute='22e0daca11d3085a43149b3bf963b392'",
         interval     => '*-*-* 04:45:00',
@@ -295,6 +313,7 @@ class profile::analytics::refinery::job::data_purge (
         use_kerberos => $use_kerberos,
     }
     kerberos::systemd_timer { 'drop-el-homepagemodule-events':
+        ensure       => $ensure_timers,
         description  => 'Drop HomepageModule data from the event_sanitized database after 270 days.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --database='event_sanitized' --tables='^homepagemodule$$' --base-path='/wmf/data/event_sanitized/HomepageModule' --path-format='year=(?P<year>[0-9]+)(/month=(?P<month>[0-9]+)(/day=(?P<day>[0-9]+)(/hour=(?P<hour>[0-9]+))?)?)?' --older-than='270' --skip-trash --execute='98a07ca21191bdaf1e6ce8c59093377a'",
         interval     => '*-*-* 04:50:00',
@@ -307,6 +326,7 @@ class profile::analytics::refinery::job::data_purge (
     # runs once a day (but only will delete data on the needed date)
     $geoeditors_private_retention_days = 60
     kerberos::systemd_timer { 'mediawiki-raw-cu-changes-drop-month':
+        ensure       => $ensure_timers,
         description  => 'Drop raw MediaWiki cu_changes from Hive/HDFS following data retention policies.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --database='wmf_raw' --tables='mediawiki_private_cu_changes' --base-path='/wmf/data/raw/mediawiki_private/tables/cu_changes' --path-format='month=(?P<year>[0-9]+)-(?P<month>[0-9]+)' --older-than='${geoeditors_private_retention_days}' --skip-trash --execute='9d9f8adf2eb7de69c9c3634e45e1f7d9'",
         environment  => $systemd_env,
@@ -319,6 +339,7 @@ class profile::analytics::refinery::job::data_purge (
     # runs once a day (but only will delete data on the needed date)
     # Temporary stopped to prevent data to be dropped.
     kerberos::systemd_timer { 'mediawiki-geoeditors-drop-month':
+        ensure       => $ensure_timers,
         description  => 'Drop Geo-editors data from Hive/HDFS following data retention policies.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --database='wmf' --tables='geoeditors_daily' --base-path='/wmf/data/wmf/mediawiki_private/geoeditors_daily' --path-format='month=(?P<year>[0-9]+)-(?P<month>[0-9]+)' --older-than='${geoeditors_private_retention_days}' --skip-trash --execute='5faa519abce3840e7718cfbde8779078'",
         environment  => $systemd_env,
@@ -331,6 +352,7 @@ class profile::analytics::refinery::job::data_purge (
     # runs once a month
     $dumps_retention_days = 80
     kerberos::systemd_timer { 'drop-mediawiki-pages_meta_history-dumps':
+        ensure       => $ensure_timers,
         description  => 'Drop pages_meta_history dumps data from HDFS after 80 days.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --base-path /wmf/data/raw/mediawiki/dumps/pages_meta_history --path-format '(?P<year>[0-9]{4})(?P<month>[0-9]{2})01' --older-than ${dumps_retention_days} --log-file ${mediawiki_dumps_log_file} --skip-trash --execute 88d1df6aee62b8562ab3d31964ba6b49",
         environment  => $systemd_env,
@@ -342,6 +364,7 @@ class profile::analytics::refinery::job::data_purge (
     # drop monthly pages_meta_current dumps data after 80 days (last day of month as reference)
     # runs once a month
     kerberos::systemd_timer { 'drop-mediawiki-pages_meta_current-dumps':
+        ensure       => $ensure_timers,
         description  => 'Drop pages_meta_current dumps data from HDFS after 80 days.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --base-path /wmf/data/raw/mediawiki/dumps/pages_meta_current --path-format '(?P<year>[0-9]{4})(?P<month>[0-9]{2})01' --older-than ${dumps_retention_days} --log-file ${mediawiki_dumps_log_file} --skip-trash --execute 7fd55f34e12cb3a6c586a29043ae5402",
         environment  => $systemd_env,
@@ -353,6 +376,7 @@ class profile::analytics::refinery::job::data_purge (
     # drop monthly siteinfo_namespaces dumps data after 80 days (last day of month as reference)
     # runs once a month
     kerberos::systemd_timer { 'drop-mediawiki-siteinfo_namespaces-dumps':
+        ensure       => $ensure_timers,
         description  => 'Drop pages_meta_current dumps data from HDFS after 80 days.',
         command      => "${refinery_path}/bin/refinery-drop-older-than --base-path /wmf/data/raw/mediawiki/dumps/siteinfo_namespaces --path-format '(?P<year>[0-9]{4})(?P<month>[0-9]{2})01' --older-than ${dumps_retention_days} --log-file ${mediawiki_dumps_log_file} --skip-trash --execute b5ced2ce9e4be85f144a2228ade9125d",
         environment  => $systemd_env,
