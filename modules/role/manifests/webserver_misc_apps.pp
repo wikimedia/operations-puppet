ebd5535f1552205c3e23e70d99e0649e297a7710
Daniel Zahn <dzahn@wikimedia.org>
2020-04-10 08:38:54 -0400

    merge microsites into webserver_misc_apps
    Traditionally we had 2 separate pairs of VMs for misc sites,
    one for apps and one for static sites.
    
    The suggestion here is to merge them into a single set of VMs,
    add all static site profiles to webserver_misc_apps
    and then eventually remove VMs bromine and vega which are on stretch.
    
    The miscweb* have recently been upgraded to buster.
    
    Bug: T247650
    Change-Id: If1e9229d46e7ddcf5d4314f06ee43d7f2f26ff80

--- modules/role/manifests/webserver_misc_apps.pp
+++ modules/role/manifests/webserver_misc_apps.pp
@@ -9,8 +9,18 @@ class role::webserver_misc_apps {
     include ::profile::standard
     include ::profile::base::firewall
     include ::profile::misc_apps::httpd       # common webserver setup
-    include ::profile::wikimania_scholarships # https://scholarships.wikimedia.org
-    include ::profile::iegreview              # https://iegreview.wikimedia.org
-    include ::profile::racktables             # https://racktables.wikimedia.org
     include ::profile::tlsproxy::envoy        # TLS termination
+
+    include ::profile::wikimania_scholarships      # https://scholarships.wikimedia.org
+    include ::profile::iegreview                   # https://iegreview.wikimedia.org
+    include ::profile::racktables                  # https://racktables.wikimedia.org
+    include ::profile::microsites::annualreport    # https://annual.wikimedia.org
+    include ::profile::microsites::static_bugzilla # https://static-bugzilla.wikimedia.org
+    include ::profile::microsites::static_rt       # https://static-rt.wikimedia.org
+    include ::profile::microsites::transparency    # https://transparency.wikimedia.org
+    include ::profile::microsites::research        # https://research.wikimedia.org (T183916)
+    include ::profile::microsites::design          # https://design.wikimedia.org (T185282)
+    include ::profile::microsites::sitemaps        # https://sitemaps.wikimedia.org
+    include ::profile::microsites::bienvenida      # https://bienvenida.wikimedia.org (T207816)
+    include ::profile::microsites::wikiworkshop    # https://wikiworkshop.org (T242374)
 }
