d563bcc77fff11cef83619bee8551783e0ba7e25
Luca Toscano <ltoscano@wikimedia.org>
2020-04-11 13:56:47 +0200

    kafkatee::instance: add types to parameters
    Change-Id: I552a6ac3c878bf8ddd6c503c25f1e55454f1f157

---
+++ modules/kafkatee/types/input.pp
@@ -0,0 +1,6 @@
+type Kafkatee::Input = Struct[{
+    topic      => String[1],
+    partitions => Pattern[/\d+(-\d+)?/],
+    options    => Hash[String[1], String[1]],
+    offset     => String[1],
+}]
