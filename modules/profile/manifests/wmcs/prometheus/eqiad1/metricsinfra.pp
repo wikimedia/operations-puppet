392005ed101c4b1574571c81a5e31be881a4ee6e
Jason Hedden <jhedden@wikimedia.org>
2020-04-16 10:23:23 -0500

    cloudvps: update project service discovery prometheus config
    Use hieradata to define which projects will be monitored by the metricsinfra
    prometheus server.
    
    Bug: T250206
    Change-Id: I623751e3b865b6bb0a7ccd3f1d9068f3be9c15ed

--- modules/profile/manifests/wmcs/prometheus/eqiad1/metricsinfra.pp
+++ modules/profile/manifests/wmcs/prometheus/eqiad1/metricsinfra.pp
@@ -1,12 +1,13 @@
 class profile::wmcs::prometheus::eqiad1::metricsinfra(
-    Stdlib::Fqdn $keystone_host     = lookup('profile::openstack::eqiad1::keystone_host'),
-    String       $observer_password = lookup('profile::openstack::eqiad1::observer_password'),
-    String       $observer_user     = lookup('profile::openstack::base::observer_user'),
-    String       $region            = lookup('profile::openstack::eqiad1::region'),
+    Array[String] $projects          = lookup('profile::wmcs::prometheus::eqiad1::metricsinfra::projects'),
+    Stdlib::Fqdn  $keystone_host     = lookup('profile::openstack::eqiad1::keystone_host'),
+    String        $observer_password = lookup('profile::openstack::eqiad1::observer_password'),
+    String        $observer_user     = lookup('profile::openstack::base::observer_user'),
+    String        $region            = lookup('profile::openstack::eqiad1::region'),
 ) {
-    $openstack_sd_configs = [
+    $project_configs = $projects.map |String $project| {
         {
-            'job_name'             => 'tools_project',
+            'job_name'             => "${project}_node",
             'openstack_sd_configs' => [
                 {
                     'role'              => 'instance',
@@ -15,7 +16,7 @@ class profile::wmcs::prometheus::eqiad1::metricsinfra(
                     'username'          => $observer_user,
                     'password'          => $observer_password,
                     'domain_name'       => 'default',
-                    'project_name'      => 'tools',
+                    'project_name'      => $project,
                     'all_tenants'       => false,
                     'refresh_interval'  => '5m',
                     'port'              => 9100,
@@ -32,41 +33,12 @@ class profile::wmcs::prometheus::eqiad1::metricsinfra(
                     'regex'         => 'ACTIVE',
                 }
             ]
-
-        },
-        {
-            'job_name'             => 'cloudinfra_project',
-            'openstack_sd_configs' => [
-                {
-                    'role'              => 'instance',
-                    'region'            => $region,
-                    'identity_endpoint' => "http://${keystone_host}:5000/v3",
-                    'username'          => $observer_user,
-                    'password'          => $observer_password,
-                    'domain_name'       => 'default',
-                    'project_name'      => 'cloudinfra',
-                    'all_tenants'       => false,
-                    'refresh_interval'  => '5m',
-                    'port'              => 9100,
-                }
-            ],
-            'relabel_configs'     => [
-                {
-                    'source_labels' => ['__meta_openstack_instance_name'],
-                    'target_label'  => 'instance',
-                },
-                {
-                    'source_labels' => ['__meta_openstack_instance_status'],
-                    'action'        => 'keep',
-                    'regex'         => 'ACTIVE',
-                }
-            ]
         }
-    ]
+    }
 
     prometheus::server { 'cloud':
         listen_address       => '127.0.0.1:9900',
-        scrape_configs_extra => $openstack_sd_configs,
+        scrape_configs_extra => $project_configs,
     }
 
     class { '::httpd':
