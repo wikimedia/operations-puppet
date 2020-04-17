c4ac33197026d5f864893ad0214420c44680642c
Luca Toscano <ltoscano@wikimedia.org>
2020-04-17 10:04:09 +0200

    statistics::rsync::mediawiki: reduce retention and improve security
    What:
    Allow only analyitics-privatedata-users to read mw-logs, and retain
    only 60 days of logs.
    
    Why:
    The MW api logs contain PII data and should be restricted to a more
    security-focused group. The logs are currently 1.3TB in size, reducing
    to 60 days will give us ~400GB back of storage.
    
    Bug: T249754
    Change-Id: I1ed4eb4dbd6fe118685c53d25ba9365fb832f640

--- modules/statistics/manifests/rsync/mediawiki.pp
+++ modules/statistics/manifests/rsync/mediawiki.pp
@@ -8,22 +8,16 @@ class statistics::rsync::mediawiki {
 
     # Any logs older than this will be pruned by
     # the rsync_job define.
-    $retention_days = 90
+    $retention_days = 60
 
     $mw_log_dir = '/srv/log/mw-log'
 
-    file { [$mw_log_dir, "${mw_log_dir}/archive", "${mw_log_dir}/archive/api", "${mw_log_dir}/archive/wmde"]:
-        ensure => 'directory',
-        owner  => 'stats',
-        group  => 'wikidev',
-        mode   => '0775',
-    }
-
-    # WMDE temporary debug logs - T171958
-    statistics::rsync_job { 'mw-wmde-temp':
-        source         => 'mwlog1001.eqiad.wmnet::udp2log/archive/WMDE.log-*.gz',
-        destination    => "${$mw_log_dir}/archive/wmde",
-        retention_days => 90,
+    file { [$mw_log_dir, "${mw_log_dir}/archive", "${mw_log_dir}/archive/api"]:
+        ensure  => 'directory',
+        owner   => 'root',
+        group   => 'analytics-privatedata-users',
+        mode    => '0750',
+        require => Group['analytics-privatedata-users'],
     }
 
     # MediaWiki API logs
@@ -32,5 +26,6 @@ class statistics::rsync::mediawiki {
         destination    => "${$mw_log_dir}/archive/api",
         # Retention of 30 days to save disk space
         retention_days => 30,
+        cron_user      => 'root',
     }
 }
