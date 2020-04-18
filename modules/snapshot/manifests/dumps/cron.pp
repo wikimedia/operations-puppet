545ac8b8818ced5061daee4fb7ca998ef77cab3f
Ariel T. Glenn <ariel@wikimedia.org>
2020-04-18 20:44:29 +0300

    Re-enable the second dumps run of the month.
    It was disabled last month due to vslow load, which is now back to normal.
    
    Change-Id: I896197097aa875933335630a52a1e0a638bfa6ac

--- modules/snapshot/manifests/dumps/cron.pp
+++ modules/snapshot/manifests/dumps/cron.pp
@@ -41,7 +41,7 @@ class snapshot::dumps::cron(
     }
 
     cron { 'partialdumps_rest':
-        ensure      => 'absent',
+        ensure      => 'present',
         environment => 'MAILTO=ops-dumps@wikimedia.org',
         user        => $user,
         command     => "/usr/local/bin/fulldumps.sh 20 25 ${runtype} partial ${maxjobs} > /dev/null",
