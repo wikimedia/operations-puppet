e3aaafae2d246a2a773d17e13f388247608bf7fc
Andrew Bogott <abogott@wikimedia.org>
2020-04-19 23:23:26 -0500

    mariadb wmcs ferm: remove references to osm_host and cloudweb_dev_hosts
    osm_host is redundan (the same as labweb_hosts).  the cloudweb_dev_hosts
    have their own database now so don't need access on m5 anymore.
    
    Change-Id: I110e404c1c1bf70ab686426cb33e2aaa401bab16

--- modules/profile/manifests/mariadb/ferm_wmcs.pp
+++ modules/profile/manifests/mariadb/ferm_wmcs.pp
@@ -6,8 +6,6 @@ class profile::mariadb::ferm_wmcs(
     Array[Stdlib::Fqdn] $eqiad1_openstack_controllers = lookup('profile::openstack::eqiad1::openstack_controllers'),
     Array[Stdlib::Fqdn] $designate_hosts = lookup('profile::openstack::eqiad1::designate_hosts'),
     Array[Stdlib::Fqdn] $labweb_hosts = lookup('profile::openstack::eqiad1::labweb_hosts'),
-    Array[Stdlib::Fqdn] $cloudweb_dev_hosts = lookup('profile::openstack::codfw1dev::labweb_hosts'),
-    Stdlib::Fqdn $osm_host = lookup('profile::openstack::eqiad1::osm_host'),
     ) {
     $port = '3306'
 
@@ -25,26 +23,11 @@ class profile::mariadb::ferm_wmcs(
         srange  => "(@resolve((${join($designate_hosts,' ')})))",
     }
 
-    ferm::service{ 'wikitech':
-        proto   => 'tcp',
-        port    => $port,
-        notrack => true,
-        srange  => "@resolve(${osm_host})",
-    }
-
-    # Soon, 'labweb' will replace horizon, striker, and wikitech
     $labweb_ips = inline_template("@resolve((<%= @labweb_hosts.join(' ') %>))")
     ferm::service{ 'labweb':
         proto   => 'tcp',
         port    => $port,
         notrack => true,
         srange  => $labweb_ips,
-    }
-    $cloudweb_dev_ips = inline_template("@resolve((<%= @cloudweb_dev_hosts.join(' ') %>))")
-    ferm::service{ 'cloudweb_dev':
-        proto   => 'tcp',
-        port    => $port,
-        notrack => true,
-        srange  => $cloudweb_dev_ips,
     }
 }
