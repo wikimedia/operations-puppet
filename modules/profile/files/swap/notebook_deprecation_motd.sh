575329d180ef2b33e351d98d9a57cf3b71930134
Luca Toscano <ltoscano@wikimedia.org>
2020-04-21 16:16:52 +0200

    role::swap: add deprecation notice motd on notebook100[3,4]
    Bug: T249752
    Change-Id: I59aa7cbf159739b354034280ce1e78bb2b143807

---
+++ modules/profile/files/swap/notebook_deprecation_motd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cat <<'MOTD'
+
+The notebook100[3,4] hosts are on a deprecation path, see https://phabricator.wikimedia.org/T249752
+
+If you are a regular user of Jupyter, please consider to migrate your home directory
+to one of the stat100x hosts following these guides:
+
+https://wikitech.wikimedia.org/wiki/Analytics/Systems/Clients#Jupyterhub_usage
+https://wikitech.wikimedia.org/wiki/Analytics/Systems/Clients#Rsync_between_clients
+
+For any question/doubt/etc.. please follow up with the Analytics team in the aforementioned task
+or via IRC/email.
+
+MOTD
