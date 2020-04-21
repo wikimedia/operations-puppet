cb82327823be438995923f43169033ecbac30be6
Andrew Bogott <abogott@wikimedia.org>
2020-04-21 13:48:48 -0500

    Openstack metrics profiles: tidy up params
    - Use lookup() instead of hiera()
    - Specify param types
    - Align indentations
    
    Change-Id: Ib9f6130f86e24d411d9caa880eecf09b6d9269eb

--- modules/profile/manifests/openstack/eqiad1/metrics.pp
+++ modules/profile/manifests/openstack/eqiad1/metrics.pp
@@ -1,16 +1,16 @@
 class profile::openstack::eqiad1::metrics(
-    $cpu_allocation_ratio = hiera('profile::openstack::eqiad1::metrics::cpu_allocation_ratio'),
-    $ram_allocation_ratio = hiera('profile::openstack::eqiad1::metrics::ram_allocation_ratio'),
-    $disk_allocation_ratio = hiera('profile::openstack::eqiad1::metrics::disck_allocation_ratio'),
-    $listen_port = hiera('profile::openstack::eqiad1::metrics::prometheus_listen_port'),
-    $cache_refresh_interval = hiera('profile::openstack::eqiad1::metrics::cache_refresh_interval'),
-    $cache_file = hiera('profile::openstack::eqiad1::metrics::cache_file'),
-    $sched_ram_mbs = hiera('profile::openstack::eqiad1::metrics::sched_ram_mbs'),
-    $sched_vcpu = hiera('profile::openstack::eqiad1::metrics::sched_vcpu'),
-    $sched_disk_gbs = hiera('profile::openstack::eqiad1::metrics::sched_disk_gbs'),
-    $region = hiera('profile::openstack::eqiad1::region'),
-    $keystone_host = hiera('profile::openstack::eqiad1::keystone_host'),
-    $observer_password = hiera('profile::openstack::eqiad1::observer_password'),
+    Float                $cpu_allocation_ratio   = lookup('profile::openstack::eqiad1::metrics::cpu_allocation_ratio'),
+    Float                $ram_allocation_ratio   = lookup('profile::openstack::eqiad1::metrics::ram_allocation_ratio'),
+    Float                $disk_allocation_ratio  = lookup('profile::openstack::eqiad1::metrics::disck_allocation_ratio'),
+    Stdlib::Port         $listen_port            = lookup('profile::openstack::eqiad1::metrics::prometheus_listen_port'),
+    Integer              $cache_refresh_interval = lookup('profile::openstack::eqiad1::metrics::cache_refresh_interval'),
+    Stdlib::Absolutepath $cache_file             = lookup('profile::openstack::eqiad1::metrics::cache_file'),
+    Integer              $sched_ram_mbs          = lookup('profile::openstack::eqiad1::metrics::sched_ram_mbs'),
+    Integer              $sched_vcpu             = lookup('profile::openstack::eqiad1::metrics::sched_vcpu'),
+    Integer              $sched_disk_gbs         = lookup('profile::openstack::eqiad1::metrics::sched_disk_gbs'),
+    String               $region                 = lookup('profile::openstack::eqiad1::region'),
+    Stdlib::Fqdn         $keystone_host          = lookup('profile::openstack::eqiad1::keystone_host'),
+    String               $observer_password      = lookup('profile::openstack::eqiad1::observer_password'),
   ) {
 
     class {'::profile::openstack::base::metrics':
