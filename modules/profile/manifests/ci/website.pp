636e1544f59f829992b71f0694d42786f5a3683e
Daniel Zahn <dzahn@wikimedia.org>
2020-04-21 10:23:00 -0400

    ci: remove integration.mediawiki.org
    This is pointing to the application servers and is being
    rewritten to integration.wikimedia.org so it should
    not be needed at all on contint servers.
    
    Change-Id: I7ca2e20c1f1c31dd006a364dd03efd53d1f19cb8

--- modules/profile/manifests/ci/website.pp
+++ modules/profile/manifests/ci/website.pp
@@ -1,17 +1,11 @@
 # Website for Continuous integration
 #
-# http://integration.mediawiki.org/
 # http://integration.wikimedia.org/
 class profile::ci::website {
 
     # Apache configuration for integration.wikimedia.org
     httpd::site { 'integration.wikimedia.org':
         content => template('contint/apache/integration.wikimedia.org.erb'),
-    }
-
-    # Apache configuration for integration.mediawiki.org
-    httpd::site { 'integration.mediawiki.org':
-        content => template('contint/apache/integration.mediawiki.org.erb'),
     }
 
     # Static files in these docroots are in integration/docroot.git
