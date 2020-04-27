c9ab35ffb498053c0efad2e2c57e99e8a671ab95
Daniel Zahn <dzahn@wikimedia.org>
2020-04-27 08:59:16 -0400

    create simplelamp2, replace mysql->mariadb, apache->httpd
    A modernized version of the old "simplelamp" role used
    across cloud VPS that replaces the mysql module with
    mariab and the apache module with httpd.
    
    Both old modules we want to get rid of and this should let users
    of the old role have something to replace it with and unblock
    us from deleting them.
    
    Bug: T162070
    Change-Id: I4a9265b43eeb119e9c9cf25d22df2f40ba9520b9

---
+++ modules/role/manifests/simplelamp2.pp
@@ -0,0 +1,37 @@
+# = class: role::simplelamp2
+#
+# Sets up a simple LAMP server for use by arbitrary php applications
+#
+# httpd ("apache"), memcached, PHP, MariaDB
+#
+# As opposed to the original simplelamp role it uses
+# MariaDB instead of MySQL and the httpd instead of the apache module.
+#
+# filtertags: labs-common
+class role::simplelamp2 {
+
+    system::role { 'simplelamp2':
+        ensure      => 'present',
+        description => 'httpd, memcached, PHP, mariadb',
+    }
+
+    $apache_modules_common = ['rewrite', 'headers']
+
+    require_package('libapache2-mod-php')
+
+    if os_version('debian == buster') {
+        $apache_php_module = 'php7.3'
+    } else {
+        $apache_php_module = 'php7.0'
+    }
+
+    $apache_modules = concat($apache_modules_common, $apache_php_module)
+
+    class { '::httpd':
+        modules => $apache_modules,
+    }
+
+    class { '::memcached': }
+
+    include ::profile::mariadb::generic_server
+}
