0d0fb2c2af386f835542c620a920eafb6cad1d07
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-04-28 16:59:38 +0200

    Enable idp-test1001 as second IDP staging server
    Bug: T233930
    Change-Id: I60a4d0745586a6f09bb772c9a4992aa17a991368

--- manifests/site.pp
+++ manifests/site.pp
@@ -1126,12 +1126,8 @@ node /^idp[12]001\.wikimedia\.org$/ {
     role(idp)
 }
 
-node 'idp-test1001.wikimedia.org' {
-    role(insetup)
-}
-
-# IDP staging server
-node 'idp-test2001.wikimedia.org' {
+# IDP staging servers
+node /^idp-test[12]001\.wikimedia\.org$/ {
     role(idp_test)
 }
 
