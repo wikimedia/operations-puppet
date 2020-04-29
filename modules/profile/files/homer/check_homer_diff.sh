36ef13706df09a5a20361856d376f22e50debd79
Riccardo Coccioli <rcoccioli@wikimedia.org>
2020-04-29 12:23:26 +0200

    homer: improve check diff email message
    Bug: T249224
    Change-Id: I88ea20a5b1e1f8c82b4b3c2711b3b66f0b8e66e0

--- modules/profile/files/homer/check_homer_diff.sh
+++ modules/profile/files/homer/check_homer_diff.sh
@@ -3,6 +3,7 @@
 set -e
 
 ADDRESS="rancid-core@wikimedia.org"
+INFO="To see the actual diff for a given set of devices, from a cumin host run: homer DEVICES diff"
 
 set +e
 DIFF="$(/usr/local/bin/homer --quiet '*' diff --omit-diff 2>&1)"
@@ -15,6 +16,6 @@ if [[ "${EXIT}" -eq "0" ]]; then
 fi
 
 echo "Found diff for some devices, sending email to ${ADDRESS}"
-echo "${DIFF}" | mail -s "[Homer] Device live config differs from committed one" "${ADDRESS}"
+echo "${DIFF}\n\n${INFO}" | mail -s "[Homer] Device live config differs from committed one" "${ADDRESS}"
 
 # Do not make the systemd timer fail even if the email was sent
