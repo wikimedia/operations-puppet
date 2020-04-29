8d1c24fca8d2450df5b4c67fedee8c246ce24442
Daniel Zahn <dzahn@wikimedia.org>
2020-04-29 09:46:24 -0400

    puppetize meet-accountmanager, step 1
    Bug: T251034
    Change-Id: Ib3d4cf844b3a805c794b42badfe62bd49021aeaa

---
+++ modules/profile/manifests/meet/accountmanager.pp
@@ -0,0 +1,42 @@
+# Sets up the account manager for Wikimedia Meet (T251034)
+class profile::meet::accountmanager(
+    $clone_path = lookup('profile::meet::accountmanager::clone_path'),
+) {
+
+    group { 'meet-auth':
+        ensure => present,
+        name   => 'meet-auth',
+        system => true,
+    }
+
+    user { 'meet-auth':
+        home       => $clone_path,
+        groups     => 'meet-auth',
+        managehome => true,
+        system     => true,
+    }
+
+    git::clone { 'wikimedia/meet-accountmanager':
+        ensure    => present,
+        directory => $clone_path,
+        branch    => 'master',
+        owner     => 'meet-auth',
+        group     => 'meet-auth',
+        require   => [User['meet-auth'], Group['meet-auth']]
+    }
+
+    uwsgi::app { 'meet-accountmanager':
+        settings         => {
+            uwsgi => {
+                'plugins'   => 'python',
+                'socket'    => '/run/uwsgi/meet-accountmanager.sock',
+                'wsgi-file' => '?',
+                'callable'  => 'app',
+                'master'    => true,
+                'processes' => 2,
+            },
+        },
+    }
+
+    base::service_auto_restart { 'meet-accountmanager': }
+}
---
+++ modules/role/manifests/meet/accountmanager.pp
@@ -0,0 +1,13 @@
+# account manager for Wikimedia Meet
+# https://phabricator.wikimedia.org/tag/wikimedia_meet/
+class role::meet::accountmanager {
+
+    system::role { 'meet::accountmanager':
+        description => 'account manager for Wikimedia Meet'
+    }
+
+    include ::profile::standard
+    include ::profile::backup::host
+    include ::profile::base::firewall
+    include ::profile::meet::accountmanager
+}
