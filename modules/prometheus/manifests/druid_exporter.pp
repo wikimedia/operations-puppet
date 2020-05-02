b78d319830a9bad5b1b79f092d045dc273ddf3e7
Luca Toscano <ltoscano@wikimedia.org>
2020-05-02 10:34:29 +0200

    prometheus::druid_exporter: upgrade to 0.8
    What:
    Upgrade puppet configuration to support the new version of the exporter.
    
    Why:
    The Prometheus Druid exporter now supports to load the list of metrics
    to plot via JSON config file.
    
    Change-Id: I4d5b04628360a957cec726994d2f5524336a9f13

--- modules/prometheus/manifests/druid_exporter.pp
+++ modules/prometheus/manifests/druid_exporter.pp
@@ -6,11 +6,49 @@
 #
 # [*arguments*]
 #   Additional command line arguments for prometheus-druid-exporter.
-
+#   The path to the metrics config file is already provided by default.
+#   Default: ''
+#
+# [*druid_version*]
+#   From version 0.8, the exporter supports the metric definition
+#   via external json file. To simplify the overall configuration,
+#   we define a list of supported/suggested metrics for each version
+#   of Druid. In the future, if more granularity is needed, we may
+#   add metric definition support via Hiera.
+#   Default: '0.12.3'
+#
 define prometheus::druid_exporter (
-    $arguments = '',
+    String $arguments = '',
+    String $druid_version = '0.12.3',
 ) {
-    require_package('prometheus-druid-exporter')
+
+    # The exporter works in a weird way with the Stretch's
+    # package version, 0.0.18, that is from late 2016.
+    # Rather than keeping compatibility with old code,
+    # force deps to something newer (stretch-backports offers 0.5.0).
+    if os_version('debian == stretch') {
+        apt::pin { 'python3-prometheus-client-package':
+            pin      => 'release a=stretch-backports',
+            package  => 'python3-prometheus-client',
+            priority => '1001',
+            before   => Package['python3-prometheus-client'],
+        }
+    }
+
+    package { 'prometheus-druid-exporter':
+        ensure => present,
+    }
+
+    $metrics_config_file = '/etc/prometheus/druid-exporter_metrics'
+    $metrics_config_version = regsubst($druid_version, '\.', '_', 'G')
+    file { $metrics_config_file:
+        ensure => present,
+        mode   => '0444',
+        owner  => 'root',
+        group  => 'root',
+        source => "puppet:///modules/prometheus/druid_exporter/metrics_config_${metrics_config_version}.json",
+        notify => Service['prometheus-druid-exporter'],
+    }
 
     file { '/etc/default/prometheus-druid-exporter':
         ensure  => present,
@@ -23,7 +61,10 @@ define prometheus::druid_exporter (
 
     service { 'prometheus-druid-exporter':
         ensure  => running,
-        require => Package['prometheus-druid-exporter'],
+        require => [
+            Package['prometheus-druid-exporter'],
+            File['/etc/default/prometheus-druid-exporter'],
+            File[$metrics_config_file]]
     }
 
     base::service_auto_restart { 'prometheus-druid-exporter': }
