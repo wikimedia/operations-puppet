7e3bff294d01c43c35a81ced7291c6d682bd3db8
Andrew Bogott <abogott@wikimedia.org>
2020-05-04 14:10:26 -0500

    Openstack/Buster: remove explicit import of python-ldap from Buster
    Since this manifest is specifically for buster we no longer need this
    override.
    
    Bug: T251294
    Change-Id: I3ef426b5c47ea12dfbeb943fad00186772b5d872

--- modules/openstack/manifests/keystone/service/rocky/buster.pp
+++ modules/openstack/manifests/keystone/service/rocky/buster.pp
@@ -2,21 +2,6 @@ class openstack::keystone::service::rocky::buster(
 ) {
     require ::openstack::serverpackages::rocky::buster
 
-    # pull in python-ldap version 3+ from the buster repo.
-    #  older versions don't handle unicode properly.
-    #  T229227
-    apt::repository { 'buster-wikimedia-component-python-ldap-bpo':
-        uri        => 'http://apt.wikimedia.org/wikimedia',
-        dist       => buster-wikimedia,
-        components => 'component/python-ldap-bpo',
-        before     => Package['keystone'],
-        notify     => Exec['apt_update_python_ldap'],
-    }
-    exec {'apt_update_python_ldap':
-        command     => '/usr/bin/apt-get update',
-        refreshonly => true,
-    }
-
     $packages = [
         'keystone',
         'alembic',
