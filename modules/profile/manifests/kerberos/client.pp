89ca48645925d6fea51982d5bdeb0e1f81667294
Luca Toscano <ltoscano@wikimedia.org>
2020-05-06 16:32:51 +0200

    profile::kerberos::client: set KRB5CCNAME with default_ccache_name
    What:
    Automatically set KRB5CCNAME when default_ccache_name changes from
    the default.
    
    Why:
    The openjdk seems not able to pick up default_ccache_name from
    /etc/krb.conf because the value is hardcoded:
    
    https://github.com/openjdk/jdk/blob/master/src/java.security.jgss/share/classes/sun/security/krb5/internal/ccache/FileCredentialsCache.java#L448-L456
    
    A workaround is to set KRB5CCNAME for all the users by default.
    
    Change-Id: I6cb29cf227aeadfea590dd2dd2fd03829f745d85

--- modules/profile/manifests/kerberos/client.pp
+++ modules/profile/manifests/kerberos/client.pp
@@ -2,12 +2,23 @@ class profile::kerberos::client (
     Stdlib::Fqdn $krb_realm_name = lookup('kerberos_realm_name'),
     Array[Stdlib::Fqdn] $krb_kdc_servers = lookup('kerberos_kdc_servers'),
     Stdlib::Fqdn $krb_kadmin_primary = lookup('kerberos_kadmin_server_primary'),
-    Optional[String] $default_ccache_name = lookup('profile::kerberos::client::default_ccache_name', { 'default_value' => undef}),
+    Optional[Boolean] $use_new_ccache = lookup('profile::kerberos::client::use_new_ccache', { 'default_value' => false}),
 ) {
 
     class { 'kerberos::wrapper': }
 
     $run_command_script = $::kerberos::wrapper::kerberos_run_command_script
+
+    # Java doesn't support a different default_ccache_name value
+    # from the default one, since it is hardcoded in its code
+    # (see Openjdk's FileCredentialsCache.java#L448-L456).
+    # It does support the KRB5CCNAME env variable override.
+    if $use_new_ccache {
+        $default_ccache_name = '/user/run/%{uid}/krb_cred'
+        file { '/etc/profile.d/java_KRB5CCNAME.sh':
+            content => 'export KRB5CCNAME=/user/run/$(uid)/krb_cred',
+        }
+    }
 
     file { '/etc/krb5.conf':
         owner   => 'root',
