wikimedia/operations-puppet

commit 63addad7b79d8dff7bbf80584016a2798e18544e

redis::instance: Deduplicate base_settings
Why:
Just make it a bit more DRY

What:
Move the aof to aof_settings dedicated Hash and deduplicate
base_settings

Change-Id: If8e05030601bc4fc10d9882bd803b439ef5acbf8

modules/profile/manifests/redis/instance.pp
@@ -9,21 +9,21 @@ define profile::redis::instance(
         default => undef
     }
 
+    $base_settings = {
+        dbfilename => "${::hostname}-${title}.rdb",
+        slaveof    => $slaveof_actual,
+    }
+
     if $aof {
-        $base_settings = {
+        $aof_settings = {
             appendfilename => "${::hostname}-${title}.aof",
-            dbfilename     => "${::hostname}-${title}.rdb",
-            slaveof        => $slaveof_actual,
         }
     } else {
-        $base_settings = {
-            dbfilename => "${::hostname}-${title}.rdb",
-            slaveof    => $slaveof_actual,
-        }
+        $aof_settings = {}
     }
 
     ::redis::instance { $title:
-        settings => merge($base_settings, $settings)
+        settings => merge($base_settings, $aof_settings, $settings)
     }
 
 }

commit 94d35177c8bc7942635918838378afda9c2760e7

redis: Followup for 63addad7b79d8dff7bbf
Why:
63addad7b79d8dff7bbf80584 broke redis replication because aof was
ovewritten by $settings

What:
Move $aof_settings last in the merge call

Change-Id: Ib7012731544bf8cfabad334fb557c2febcf22f3a

modules/profile/manifests/redis/instance.pp
@@ -23,7 +23,7 @@ define profile::redis::instance(
     }
 
     ::redis::instance { $title:
-        settings => merge($base_settings, $aof_settings, $settings)
+        settings => merge($base_settings, $settings, $aof_settings)
     }
 
 }