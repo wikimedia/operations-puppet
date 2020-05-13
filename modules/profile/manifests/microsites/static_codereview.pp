fecd12f29047496a1c5fbdf1d7159f44c5744cb8
Daniel Zahn <dzahn@wikimedia.org>
2020-05-13 08:04:17 -0400

    static-codereview: activate Icinga monitoring
    Also adjust file name from r1.html to just 1.html.
    
    Bug: T243056
    Change-Id: I2a7637c8f57bd15a482009876928b49701175471

--- modules/profile/manifests/microsites/static_codereview.pp
+++ modules/profile/manifests/microsites/static_codereview.pp
@@ -3,11 +3,11 @@ class profile::microsites::static_codereview {
 
     backup::set { 'static-codereview' : }
 
-    #monitoring::service { 'static-codereview-http':
-    #    description   => 'Static CodeReview archive HTTP',
-    #    check_command => 'check_http_url!static-codereview.wikimedia.org!/MediaWiki/r1.html',
-    #    notes_url     => 'https://wikitech.wikimedia.org/wiki/Static-codereview.wikimedia.org',
-    #}
+    monitoring::service { 'static-codereview-http':
+        description   => 'Static CodeReview archive HTTP',
+        check_command => 'check_http_url!static-codereview.wikimedia.org!/MediaWiki/1.html',
+        notes_url     => 'https://wikitech.wikimedia.org/wiki/Static-codereview.wikimedia.org',
+    }
 
     ensure_resource('file', '/srv/org', {'ensure' => 'directory' })
     ensure_resource('file', '/srv/org/wikimedia', {'ensure' => 'directory' })
