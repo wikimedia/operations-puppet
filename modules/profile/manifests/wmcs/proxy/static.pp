c13ef854767ee476298f77e3fe6ab02d776d31eb
Andrew Bogott <abogott@wikimedia.org>
2020-05-13 22:54:06 -0500

    Add profile::wmcs::proxy::static
    This sets up a group of static nginx web proxies based on a hiera hash.
    
    For now it just uses the existing wmflabs wildcard certs for ssl. FQDNs
    that aren't of the form <name>.wmflabs.org won't match the cert.
    
    Bug: T233995
    Change-Id: Idc9c2b7fc341126954d7c1cb13495af322f489c5

---
+++ modules/profile/manifests/wmcs/proxy/static.pp
@@ -0,0 +1,23 @@
+# Create a list of static web-proxy mappings, using key, value pairs in $proxy_mappings
+#
+# By default we block two user agents:
+#
+#   TweetmemeBot T73120
+#   OruxMaps     T97841
+#
+#
+class profile::wmcs::proxy::static(
+    Hash   $proxy_mappings           = lookup('profile::wmcs::proxy::static::proxy_mappings'),
+    Array  $banned_ips               = lookup('profile::wmcs::proxy::static::banned_ips',          {default_value => []}),
+    String $blocked_user_agent_regex = lookup('profile::wmcs::proxy::static::blocked_user_agents', {default_value => '(TweetmemeBot|OruxMaps.*)'}),
+    String $blocked_referer_regex    = lookup('profile::wmcs::proxy::static::blocked_referers',    {default_value => ''}),
+) {
+    $ssl_settings  = ssl_ciphersuite('nginx', 'compat')
+
+    class { '::nginx':
+        variant => 'extras',
+    }
+    nginx::site { 'proxies':
+        content => template('profile/wmcs/proxy/static.conf.erb'),
+    }
+}
