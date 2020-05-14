c25cc45980e69accbce7a7f0f47fd4e7464fa94a
Daniel Zahn <dzahn@wikimedia.org>
2020-05-14 06:59:48 -0400

    maintenance: fix hostname of miscweb server in codfw
    Change-Id: Ie2cd76da6e08d2cc41df04eece1291970b0669c0

--- modules/profile/manifests/mediawiki/maintenance.pp
+++ modules/profile/manifests/mediawiki/maintenance.pp
@@ -86,7 +86,7 @@ class profile::mediawiki::maintenance {
     rsync::quickdatacopy { 'home-mwmaint':
         ensure      => present,
         auto_sync   => false,
-        source_host => 'mwmaint2001.codfw.wmnet',
+        source_host => 'mwmaint2002.codfw.wmnet',
         dest_host   => 'mwmaint1002.eqiad.wmnet',
         module_path => '/home',
     }
