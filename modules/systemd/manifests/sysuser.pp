a1f22e1f58aa953699c2cccf24a608e71391ebfd
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-05-19 14:25:35 +0200

    Add a define for creating a system user using systemd-sysusers
    This adds a new define to create system users (and groups)
    with systemd-sysusers.
    
    The main benefit of systemd-sysusers is that it properly declares
    system users in a central place (or rather two; /usr/lib/sysusers.d/
    for anything shipped by distro packages) and /etc/sysusers.d/*.conf
    for anything locally deployed (as in our Puppet), while system users
    created via adduser are much harder to track down (some postinst,
    Puppet or wherever).
    
    systemd-sysusers is also distro-agnostic (while adduser is a
    Debian-specific tool), so if e.g. a Debian package that we use ships
    /usr/lib/sysusers.d/daemon.conf and we need a WMF-specific customisation
    for the home path or similar, our deployed /etc/sysusers.d/daemon.conf
    takes precedence.
    
    Change-Id: I9c8031f9dee8e896da91c2d3da924bf4546f8778

---
+++ modules/systemd/manifests/sysuser.pp
@@ -0,0 +1,39 @@
+# == systemd::sysuser ==
+#
+# This define creates a system user using systemd-sysusers.
+#
+# Example:
+# systemd::sysuser { 'food':
+#    content => 'u  food   - "foo daemon"  -  -'
+# }
+#
+# This allocates the next available UID and creates a "foo" system
+# user with the home set to /root and nologin as the shell.
+# See the sysusers.d manpage for the full syntax.
+#
+define systemd::sysuser(
+    Array[Systemd::Sysuser::Config] $content,
+    Wmflib::Ensure $ensure=present,
+){
+    $safe_title = regsubst($title, '[\W_/]', '-', 'G')
+    $conf_path = "/etc/sysusers.d/${safe_title}.conf"
+
+    $_content = $content.reduce('') |$memo, $v| {
+        "${memo}${v['usertype']}\t${v['name']}\t${v['id']}\t${v['gecos']}\t${v['home_dir']}\t${v['shell']}\n"
+    }
+
+    file { $conf_path:
+        ensure  => $ensure,
+        content => $_content,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+    }
+
+    exec { 'Refresh sysusers':
+        command     => '/usr/bin/systemd-sysusers',
+        user        => 'root',
+        refreshonly => true,
+        subscribe   => File[$conf_path],
+    }
+}
