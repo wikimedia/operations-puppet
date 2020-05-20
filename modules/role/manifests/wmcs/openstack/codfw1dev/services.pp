b92efae6481e69160a1bf1b0afd3bdd02a9545a5
Andrew Bogott <abogott@wikimedia.org>
2020-05-20 15:26:10 -0500

    role::wmcs::openstack::codfw1dev::services: added a comment re: ldap
    Change-Id: I3972ead3ef0ac6bf9a8748202686f14b0b300839

--- modules/role/manifests/wmcs/openstack/codfw1dev/services.pp
+++ modules/role/manifests/wmcs/openstack/codfw1dev/services.pp
@@ -8,5 +8,9 @@ class role::wmcs::openstack::codfw1dev::services {
     include ::profile::openstack::codfw1dev::designate::service
     include ::profile::prometheus::pdns_exporter
     include ::profile::prometheus::pdns_rec_exporter
+
+    # For testing purposes, these boxes host a non-production ldap cluster
+    #  with test accounts/groups/etc.  eqiad1 services boxes do not
+    #  host ldap; it's just crammed on here because it's convenient.
     include ::profile::openldap_clouddev
 }
