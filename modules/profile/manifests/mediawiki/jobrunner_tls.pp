a108d093eeca8c34770d94522781ef73fe5f7fa6
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2020-05-20 13:06:57 +0200

    jobrunner: add code to switch to envoy, switch codfw
    Bug: T247389
    Change-Id: I45a6aa794b8dc3ba966fc828ef5e5b3c2ebd270e

--- modules/profile/manifests/mediawiki/jobrunner_tls.pp
+++ modules/profile/manifests/mediawiki/jobrunner_tls.pp
@@ -3,27 +3,30 @@
 # Sets up the TLS proxy to the jobrunner rpc endpoints
 #
 class profile::mediawiki::jobrunner_tls (
-    String $ocsp_proxy = hiera('http_proxy', ''),
+    Boolean $use_envoy = lookup('profile::mediawiki::jobrunner_tls::use_envoy'),
+    String $ocsp_proxy = lookup('http_proxy'),
 ) {
     require ::profile::mediawiki::jobrunner
-    require ::profile::tlsproxy::instance
-
-    each({'jobrunner' => 1200, 'videoscaler' => 86400}) |$sitename, $timeout| {
-        $certname = "${sitename}.svc.${::site}.wmnet"
-        tlsproxy::localssl { $sitename:
-            server_name    => $certname,
-            server_aliases => ["${sitename}.discovery.wmnet"],
-            certs          => [$certname],
-            certs_active   => [$certname],
-            default_server => ($sitename == 'jobrunner'),
-            do_ocsp        => false,
-            upstream_ports => [$::profile::mediawiki::jobrunner::local_only_port],
-            access_log     => false,
-            read_timeout   => $timeout,
-            ocsp_proxy     => $ocsp_proxy,
+    if $use_envoy {
+        require ::profile::tlsproxy::envoy
+    } else {
+        require ::profile::tlsproxy::instance
+        each({'jobrunner' => 1200, 'videoscaler' => 86400}) |$sitename, $timeout| {
+            $certname = "${sitename}.svc.${::site}.wmnet"
+            tlsproxy::localssl { $sitename:
+                server_name    => $certname,
+                server_aliases => ["${sitename}.discovery.wmnet"],
+                certs          => [$certname],
+                certs_active   => [$certname],
+                default_server => ($sitename == 'jobrunner'),
+                do_ocsp        => false,
+                upstream_ports => [$::profile::mediawiki::jobrunner::local_only_port],
+                access_log     => false,
+                read_timeout   => $timeout,
+                ocsp_proxy     => $ocsp_proxy,
+            }
         }
     }
-
     ::ferm::service { 'mediawiki-jobrunner-https':
         proto   => 'tcp',
         port    => 'https',
