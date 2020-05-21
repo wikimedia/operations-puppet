23a42e41bb18795942112db3b4f96604246ef96c
Luca Toscano <ltoscano@wikimedia.org>
2020-05-21 09:07:19 +0200

    role::druid::analytics::worker: add autoconfig for historical
    What:
    Set the number of threads and max direct memory size automatically
    in the Historical settings if not provided via hiera.
    
    Why:
    We are running Druid on different hw, and upstream suggests to set
    the number of processing threads for Historical equal to the number
    of available cores on the host. As solution, this patch adds autoconfig
    for processing threads if not provided in hiera. It also sets max direct
    memory size for the JVM accordingly, since a wrong parameter will lead
    to the historical refusing to start.
    
    This patch also move the historicals to G1GC, as suggested by upstream.
    
    Bug: T252771
    Change-Id: I7d44afd835d3c6ac128e8853431a211986b3e51c

--- modules/profile/manifests/druid/historical.pp
+++ modules/profile/manifests/druid/historical.pp
@@ -14,25 +14,45 @@ class profile::druid::historical(
     # for exposing the Prometheus JMX Exporter in the Druid Broker process.
     if $monitoring_enabled {
         require ::profile::druid::monitoring::historical
-        $java_opts = $::profile::druid::monitoring::historical::java_opts
+        $monitoring_java_opts = $::profile::druid::monitoring::historical::java_opts
+    } else {
+        $monitoring_java_opts = undef
+    }
 
+    # The suggestion for the Historical daemon is to set the
+    # druid.processing.numThreads to ncores.
+    # https://druid.apache.org/docs/latest/operations/basic-cluster-tuning.html#process-specific-guidelines
+    # This is useful since we have historicals running on different hw.
+    # As consequence, the MaxDirectMemorySize java opt needs to be set according to a formula, otherwise
+    # the Historical will refuse to start.
+    if !has_key($properties, 'druid.processing.numThreads') {
+        $extra_properties = {
+            'druid.processing.numThreads' => $facts['processors']['count']
+        }
+        # max direct memory = druid.processing.buffer.sizeBytes[268,435,456] * (druid.processing.numMergeBuffers[10] + druid.processing.numThreads[64] + 1)
+        $max_direct_memory = Integer($properties['druid.processing.buffer.sizeBytes']) * (Integer($properties['druid.processing.numMergeBuffers']) + Integer($facts['processors']['count']) + 1)
+        $max_direct_memory_java_opt = "-XX:MaxDirectMemorySize=${max_direct_memory}"
+    } else {
+        $extra_properties = {}
+        $max_direct_memory = undef
+    }
+
+    if $monitoring_java_opts or $max_direct_memory_java_opt {
         if $env['DRUID_EXTRA_JVM_OPTS'] {
-            $monitoring_env_vars = {
-                'DRUID_EXTRA_JVM_OPTS' => "${env['DRUID_EXTRA_JVM_OPTS']} ${java_opts}"
+            $druid_extra_env_var = {
+                'DRUID_EXTRA_JVM_OPTS' => "${env['DRUID_EXTRA_JVM_OPTS']} ${max_direct_memory_java_opt} ${monitoring_java_opts}"
             }
         } else {
-            $monitoring_env_vars = {
-                'DRUID_EXTRA_JVM_OPTS' => $java_opts
+            $druid_extra_env_var = {
+                'DRUID_EXTRA_JVM_OPTS' => "${max_direct_memory} ${monitoring_java_opts}"
             }
         }
-    } else {
-        $monitoring_env_vars = {}
     }
 
     # Druid historical Service
     class { '::druid::historical':
-        properties       => $properties,
-        env              => merge($env, $monitoring_env_vars),
+        properties       => merge($properties, $extra_properties),
+        env              => merge($env, $druid_extra_env_var),
         should_subscribe => $daemon_autoreload,
     }
 
