5f9935768f6e48f56c05edcc56422be6ef653196
Daniel Zahn <dzahn@wikimedia.org>
2020-05-21 03:40:28 -0400

    ci::slave: add /srv/deployment dir to fix dependency problem
    After merging 37b6e2c5956ab7596 puppet was broken on contint* with:
    
    "Could not find resource 'File[/srv/deployment]' in parameter 'require'"
    
    Reverting it would be downvoted by jenkins-bot because:
    
    profile 'profile::ci::slave' includes non-profile class contint::slave_scripts
    
    There was previously a 'require ::contint::deployment_dir'
    which then does an 'include ::service::deploy::common' which then only creates
    this directory and nothing else.
    
    Add the same code directly here instead of one require and one include from
    another module which causes these kinds of issues.
    
    Bug: T252955
    Change-Id: Ib23cca6da0a08972fcc814e6011b511f8c909c3a

--- modules/profile/manifests/ci/slave.pp
+++ modules/profile/manifests/ci/slave.pp
@@ -4,15 +4,23 @@
 # directly on the Jenkins master for example to publish publish content under
 # https://doc.wikimedia.org/
 #
-# You should look instaed at role::ci::labs::slave
+# You should look instead at role::ci::labs::slave
 #
 
 class profile::ci::slave {
+
     class { 'jenkins::slave':
         # Master connect to itself via the fqdn / primary IP ipaddress
         ssh_key => "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4QGc1Zs/S4s7znEYw7RifTuZ4y4iYvXl5jp5tJA9kGUGzzfL0dc4ZEEhpu+4C/TixZJXqv0N6yke67cM8hfdXnLOVJc4n/Z02uYHQpRDeLAJUAlGlbGZNvzsOLw39dGF0u3YmwDm6rj85RSvGqz8ExbvrneCVJSaYlIRvOEKw0e0FYs8Yc7aqFRV60M6fGzWVaC3lQjSnEFMNGdSiLp3Vl/GB4GgvRJpbNENRrTS3Te9BPtPAGhJVPliTflVYvULCjYVtPEbvabkW+vZznlcVHAZJVTTgmqpDZEHqp4bzyO8rBNhMc7BjUVyNVNC5FCk+D2LagmIriYxjirXDNrWlw== jenkins@gallium from=\"${::ipaddress}\"",
         user    => 'jenkins-slave',
         workdir => '/srv/jenkins-slave',
+    }
+
+    file { '/srv/deployment':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
     }
 
     file { '/var/lib/jenkins-slave':
