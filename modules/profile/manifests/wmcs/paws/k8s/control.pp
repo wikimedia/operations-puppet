590ae7a3445d5d33b439f5a62dc60009ec8a7a41
Brooke Storm <bstorm@wikimedia.org>
2020-05-21 10:33:45 -0600

    paws-k8s: Adding a checkout of the paws repo
    Since paws admins will have full rights on the repo to merge PRs, but
    they won't be able to merge puppet, this allows manifests and helm to
    be reasoned about in that repo for Kubernetes.
    
    Bug: T211096
    Bug: T188912
    Change-Id: Ia2b3fe7ace76f9b1be473fc2b81634c6851df79e

--- modules/profile/manifests/wmcs/paws/k8s/control.pp
+++ modules/profile/manifests/wmcs/paws/k8s/control.pp
@@ -4,4 +4,13 @@ class profile::wmcs::paws::k8s::control (
 
     class { '::profile::wmcs::kubeadm::control': }
     contain '::profile::wmcs::kubeadm::control'
+
+    # To facilitate deploying manifests directly from the repo to k8s.
+    # This would allow paws admins more flexibility for k8s-controlled elements
+    git::clone { 'paws-git':
+        ensure    => 'latest',
+        directory => '/srv/git/paws',
+        branch    => 'master',
+        origin    => 'https://github.com/toolforge/paws.git'
+    }
 }
