5a8f361e87d2b05ff43e594283c2df288d0f2e1e
Bryan Davis <bd808@wikimedia.org>
2020-05-22 10:37:59 -0600

    toolforge: remove elasticsearch5 role and profile manifests
    Remove ::role:wmcs:toolforge:elastic and related profiles and support
    file which were not reused in the new ::role::wmcs::toolforge::elastic7.
    
    Bug: T236606
    Change-Id: I3115e251516479ec9bba4cb00a2881fb5a44b558

--- modules/role/manifests/wmcs/toolforge/elastic.pp
+++
@@ -1,8 +0,0 @@
-class role::wmcs::toolforge::elastic {
-    system::role { $name: }
-    include ::profile::base::firewall
-    include ::profile::toolforge::base
-    include ::profile::toolforge::apt_pinning
-    include ::profile::elasticsearch::toolforge
-    include ::profile::toolforge::elasticsearch::nginx
-}
