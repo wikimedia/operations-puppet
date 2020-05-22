6fc826e82c13c4c3c206519df23a0734e493d5e4
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2020-05-22 12:16:46 +0200

    profile: remove unused profile::etcd::auth
    Native etcdv2 auth was ditched a long time ago for performance
    reasons.
    
    The only leftover reference was in role::etcd::kubernetes, but the
    class itself was never enabled.
    
    Change-Id: I5433a204993ae777478a9649b091a7206c3f1773

--- modules/role/manifests/etcd/kubernetes.pp
+++ modules/role/manifests/etcd/kubernetes.pp
@@ -4,7 +4,6 @@ class role::etcd::kubernetes {
     include ::profile::standard
     include ::profile::base::firewall
     include ::profile::etcd
-    include ::profile::etcd::auth
 
     system::role { 'role::etcd::kubernetes':
         description => 'kubernetes etcd cluster member'
