31b7ab51441ee620f9b9c70f61d3f88270c72d6a
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-05-27 12:47:09 +0200

    Drop colordiff from gerrit role, now part of standard packages
    Change-Id: I465fb4660128594e0ac9eca9bef2b21d46b2b4dd

--- modules/profile/manifests/gerrit/server.pp
+++ modules/profile/manifests/gerrit/server.pp
@@ -117,9 +117,4 @@ class profile::gerrit::server(
     rsyslog::input::file { 'gerrit-apache2-access':
         path => '/var/log/apache2/*access*.log',
     }
-
-    # For admins and assist with T236114
-    package { 'colordiff':
-        ensure => present,
-    }
 }
