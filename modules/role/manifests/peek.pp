68b895315a4ccf2adca866154b1a0a280bdc14ed
cpettet <rush@wikimedia.org>
2020-05-27 13:40:27 -0500

    peek: add profile to role
    Bug: T251784
    Change-Id: I4a66efcec522263db6d53d3233591ff68b5f3ae5

--- modules/role/manifests/peek.pp
+++ modules/role/manifests/peek.pp
@@ -2,4 +2,5 @@ class role::peek {
     system::role { 'peek': description => 'Security Team Tooling' }
     include ::profile::standard
     include ::profile::base::firewall
+    include ::profile::peek
 }
