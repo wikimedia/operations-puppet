bcaf9f95a54d02073a92b015ad6dc69929c6ba6c
Emanuele Rocca <ema@wikimedia.org>
2020-05-27 13:53:21 +0200

    prometheus: filter interesting rdkafka metrics in purged job
    Similarly to what we have done in c14eaebd4 for atskafka, limit the
    rdkafka metrics imported by Prometheus to a few interesting ones when it
    comes to purged too.
    
    Change-Id: I046816fa0c7542efc6381140c29be04df8465b8c

--- modules/profile/manifests/prometheus/ops.pp
+++ modules/profile/manifests/prometheus/ops.pp
@@ -342,6 +342,13 @@ class profile::prometheus::ops (
         'job_name'        => 'purged',
         'file_sd_configs' => [
           { 'files' => [ "${targets_path}/purged_*.yaml"] },
+        ],
+        # rdkafka produces lots of metrics, keep only those we are interested in for purged
+        'metric_relabel_configs' => [
+          { 'source_labels' => ['__name__'],
+            'regex'         => '^(purged_|go_|process_|rdkafka_consumer_(topics_partitions_msgs|msg_cnt|replyq|msg_size|brokers_txbytes|brokers_req_timeouts|brokers_txerrs|brokers_txretries|brokers_rxbytes|brokers_rxerrs|brokers_rtt_min|brokers_rtt_avg|brokers_rtt_max)).*$',
+            'action'        => 'keep'
+          },
         ]
       },
     ]
