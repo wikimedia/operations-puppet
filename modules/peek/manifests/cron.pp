c0a3e8f06620307095ac54851cff3ea18a1df73e
cpettet <rush@wikimedia.org>
2020-05-28 15:21:21 -0500

    peek: fix paths to conf files for cron
    Bug: T253901
    Change-Id: I94a23be277dbc18688177531337df363d39b52fc

--- modules/peek/manifests/cron.pp
+++ modules/peek/manifests/cron.pp
@@ -1,13 +1,13 @@
 class peek::cron {
 
     cron { 'peek_monthly':
-        command  => '/var/lib/peek/git/peek.py -c /etc/peek/config/base.yml,/etc/peek/config/monthly.yml -s 2>&1',
+        command  => '/var/lib/peek/git/peek.py -c /etc/peek/config/base.conf,/etc/peek/config/monthly.conf -s 2>&1',
         user     => 'peek',
         monthday => 1,
     }
 
     cron { 'peek_weekly':
-        command => '/var/lib/peek/git/peek.py -c /etc/peek/config/base.yml,/etc/peek/config/weekly.yml -s 2>&1',
+        command => '/var/lib/peek/git/peek.py -c /etc/peek/config/base.conf,/etc/peek/config/weekly.conf -s 2>&1',
         user    => 'peek',
         weekday => 1,
     }
