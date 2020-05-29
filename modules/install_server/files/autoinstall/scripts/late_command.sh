wikimedia/operations-puppet

commit 0e72d16f8aea6664aaf266b642b6ba4d95818282

Remove late-install d-i hack for Puppet 5 / Facter 3
Change-Id: Ic30745e98f4888538d9be51de3bfd4451e3b9496

modules/install_server/files/autoinstall/scripts/late_command.sh
@@ -12,20 +12,6 @@ chmod go-rwx /target/root/.ssh/authorized_keys
 apt-install lsb-release
 LSB_RELEASE=$(chroot /target /usr/bin/lsb_release --codename --short)
 
-# We need to pin add the puppet and facter components before installing puppet
-# until WMCS also uses puppet 5 / facter 3 (then we can simply import the new
-# versions to the main component
-# https://phabricator.wikimedia.org/T219803
-if printf $LSB_RELEASE | grep -qv buster
-then
-  BASE_REPO="http://apt.wikimedia.org/wikimedia ${LSB_RELEASE}-wikimedia component"
-  printf 'deb %s/puppet5\n' "$BASE_REPO" > /target/etc/apt/sources.list.d/component-puppet5.list
-  printf 'deb %s/facter3\n' "$BASE_REPO" > /target/etc/apt/sources.list.d/component-facter3.list
-  # we dont use this service, also the reimage script assumes it is the first to run puppet
-  in-target systemctl mask puppet.service
-  in-target apt-get update
-fi
-
 # openssh-server: to make the machine accessible
 # puppet: because we'll need it soon anyway
 # lldpd: announce the machine on the network

commit 1ad72adbc976aba93121a41ff7e5d14c34447ddc

Mask puppet in d-i on pre Buster
Fixes a regression in stretch/jessie installs introduced in
0e72d16f8aea6664aaf266b642b6ba4d95818282

Change-Id: Ia2466f7207e151f8f34fbe9be0550986794647a8

modules/install_server/files/autoinstall/scripts/late_command.sh
@@ -12,6 +12,12 @@ chmod go-rwx /target/root/.ssh/authorized_keys
 apt-install lsb-release
 LSB_RELEASE=$(chroot /target /usr/bin/lsb_release --codename --short)
 
+if printf $LSB_RELEASE | grep -qv buster
+then
+  # we dont use this service, also the reimage script assumes it is the first to run puppet
+  in-target systemctl mask puppet.service
+fi
+
 # openssh-server: to make the machine accessible
 # puppet: because we'll need it soon anyway
 # lldpd: announce the machine on the network