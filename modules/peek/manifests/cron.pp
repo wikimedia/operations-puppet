5f6dc924d5a372f8e84d0be151f056a16a8777c9
Jcrespo <jcrespo@wikimedia.org>
2020-06-01 06:53:13 +0000

    peek: Reenable cron with correct params
    First of each week cron: 0 0 * * MON
    First of each month cron: 0 0 1 * *
    
    Note to self, cron anatomy params not
    specified default to '*' and not 0.
    
    Change redirect to send stdout to /dev/null
    with stderror respecting the .forward
    specification of security-team@ to not
    flood root@ on issue.
    
    Created a task to convert to systemd timer which
    will take a bit more figuring than I have time
    for now, and I want to talk to the security team
    about monitoring strategy, etc.
    
    Bug: T254127
    Bug: T251784
    Bug: T254368
    Change-Id: Ibb004376aebbd2a72f3d31a0230a8cb5d0f98396

--- modules/peek/manifests/cron.pp
+++ modules/peek/manifests/cron.pp
@@ -1,16 +1,20 @@
 class peek::cron {
 
     cron { 'peek_monthly':
-        ensure   => absent,
-        command  => '. $HOME/.profile; /var/lib/peek/git/peek.py -c /etc/peek/config/base.conf,/etc/peek/config/monthly.conf -s 2>&1',
-        user     => 'peek',
-        monthday => 1,
+        command     => '. $HOME/.profile; /var/lib/peek/git/peek.py -c /etc/peek/config/base.conf,/etc/peek/config/monthly.conf -s > /dev/null',
+        environment => 'MAILTO=security-team@wikimedia.org',
+        user        => 'peek',
+        minute      => 0,
+        hour        => 0,
+        monthday    => 1,
     }
 
     cron { 'peek_weekly':
-        ensure  => absent,
-        command => '. $HOME/.profile; /var/lib/peek/git/peek.py -c /etc/peek/config/base.conf,/etc/peek/config/weekly.conf -s 2>&1',
-        user    => 'peek',
-        weekday => 1,
+        command     => '. $HOME/.profile; /var/lib/peek/git/peek.py -c /etc/peek/config/base.conf,/etc/peek/config/weekly.conf -s > /dev/null',
+        environment => 'MAILTO=security-team@wikimedia.org',
+        user        => 'peek',
+        minute      => 0,
+        hour        => 0,
+        weekday     => 1,
     }
 }
