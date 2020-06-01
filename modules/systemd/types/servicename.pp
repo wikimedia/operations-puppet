41f7496bf05a1bb44d7a67d5c9d344677444ae47
Chris Danis <cdanis@wikimedia.org>
2020-06-01 18:46:06 -0400

    Systemd::Servicename: make it reflect reality e.g. php7.2-fpm
    Change-Id: I2c146df4eefb8b7b017cd28db5d14a7d1683c12d

--- modules/systemd/types/servicename.pp
+++ modules/systemd/types/servicename.pp
@@ -1,1 +1,6 @@
-type Systemd::Servicename = Pattern[/^[a-z-]+\.service$/]
+# Per systemd.unit(5):
+# Valid unit names consist of a "name prefix" and a dot and a suffix specifying the unit type.
+# The "unit prefix" must consist of one or more valid characters (ASCII letters, digits, ":", "-",
+# "_", ".", and "\"). The total length of the unit name including the suffix must not exceed
+# 256 characters.
+type Systemd::Servicename = Pattern[/^[a-zA-Z0-9:_.\\-]{1,248}\.service$/]
