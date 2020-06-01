c876c4b78f7f55dcbb0ce1977b16a9869ced03f3
Jaime Crespo <jcrespo@wikimedia.org>
2020-06-01 08:29:29 +0200

    peek: Disable cron execution
    peek is configured to run every minute the first of the month.
    
    Disable it temporarilly until it is correctly configured.
    
    Bug: T254127
    Change-Id: I76de69c4cfa9f8813ed6ee0903a5db3a48a4fc34

--- modules/peek/manifests/cron.pp
+++ modules/peek/manifests/cron.pp
@@ -1,12 +1,14 @@
 class peek::cron {
 
     cron { 'peek_monthly':
+        ensure   => absent,
         command  => '. $HOME/.profile; /var/lib/peek/git/peek.py -c /etc/peek/config/base.conf,/etc/peek/config/monthly.conf -s 2>&1',
         user     => 'peek',
         monthday => 1,
     }
 
     cron { 'peek_weekly':
+        ensure  => absent,
         command => '. $HOME/.profile; /var/lib/peek/git/peek.py -c /etc/peek/config/base.conf,/etc/peek/config/weekly.conf -s 2>&1',
         user    => 'peek',
         weekday => 1,
