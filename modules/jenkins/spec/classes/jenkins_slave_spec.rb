575b71ab69aaec2aaeed315c4569da2dc3b2d236
Antoine Musso <hashar@free.fr>
2020-06-03 17:10:09 +0200

    jenkins: reindent spec to two spaces
    The spec files are indenteded with four spaces, a follow up change adds
    two extra level of indentation which would result in overlong lines.
    Reindent the spec definitions to use two spaces instead of four.
    
    Change-Id: I1a20850b40947f398754ff6d809362a782963cf0

--- modules/jenkins/spec/classes/jenkins_slave_spec.rb
+++ modules/jenkins/spec/classes/jenkins_slave_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe 'jenkins::slave' do
-    let(:pre_condition) do
-        """
-        User {
-            provider => 'useradd',
-        }
-        """
-    end
-    let(:params) { {
-        :ssh_key => 'abc id-rsa',
-    } }
-    it { should compile }
+  let(:pre_condition) do
+    """
+    User {
+      provider => 'useradd',
+    }
+    """
+  end
+  let(:params) { {
+    :ssh_key => 'abc id-rsa',
+  } }
+  it { should compile }
 end
--- modules/jenkins/spec/hosts/master_and_slave_spec.rb
+++ modules/jenkins/spec/hosts/master_and_slave_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 describe 'Host being both a Jenkins master and a slave' do
   let(:node_params) { {'cluster' => 'ci', 'site' => 'eqiad'} }
   let(:pre_condition) {
-        """
-        class profile::base {
-           $notifications_enabled = '1'
-        }
-        include ::profile::base
-        class { 'jenkins':
-            prefix => '/jenkins',
-        }
-        class { 'jenkins::slave':
-            ssh_key => 'fake ssh key',
-        }
-        """
+    """
+    class profile::base {
+      $notifications_enabled = '1'
+    }
+    include ::profile::base
+    class { 'jenkins':
+      prefix => '/jenkins',
+    }
+    class { 'jenkins::slave':
+      ssh_key => 'fake ssh key',
+    }
+    """
   }
   it { should compile }
 end
