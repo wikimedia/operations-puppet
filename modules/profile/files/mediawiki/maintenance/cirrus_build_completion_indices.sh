44462d932d8d97093ccae5df0df8d3f7cbe56d3c
David Causse <dcausse@wikimedia.org>
2020-06-03 09:58:52 +0200

    [cirrus] prepend the wiki id in comp suggest logs
    with 4 wikis running in parallel it is not obvious to determine
    which wiki is responsible for a particular log line.
    
    Change-Id: I7b4f49974be5cf7dc014153f78ac0f0b0ec66dc0

--- modules/profile/files/mediawiki/maintenance/cirrus_build_completion_indices.sh
+++ modules/profile/files/mediawiki/maintenance/cirrus_build_completion_indices.sh
@@ -2,4 +2,4 @@
 
 ## Input: $1 is cluster to update (either eqiad or codfw)
 
-/usr/local/bin/expanddblist all | xargs -I{} -P 4 sh -c "/usr/local/bin/mwscript extensions/CirrusSearch/maintenance/UpdateSuggesterIndex.php --wiki={} --masterTimeout=10m --replicationTimeout=5400 --indexChunkSize 3000 --cluster=$1 --optimize"
+/usr/local/bin/expanddblist all | xargs -I{} -P 4 sh -c "/usr/local/bin/mwscript extensions/CirrusSearch/maintenance/UpdateSuggesterIndex.php --wiki={} --masterTimeout=10m --replicationTimeout=5400 --indexChunkSize 3000 --cluster=$1 --optimize 2>&1 | ts '{}'"
