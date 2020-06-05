2b155ec708a749904e2131cf175def0791f770c0
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2020-06-05 11:19:15 +0200

    Revert "thanos: add alerts for Thanos components"
    Info: Loading facts
    Info: Caching catalog for icinga1001.wikimedia.org
    Error: /Stage[main]/Icinga/Resources[nagios_service]: Failed to generate additional resources using 'generate': Could not parse configuration for nagios_service: undefined method `sum=' for #<Nagios::Base::Service:0x0055e5152796d8>
    Info: Applying configuration version '(98b8f7b328) Filippo Giunchedi - thanos: add alerts for Thanos components'
    Error: Failed to apply catalog: Could not parse configuration for nagios_service: undefined method `sum=' for #<Nagios::Base::Service:0x0055e5161f2330>
    
    Also this:
    
    "Error: Invalid service object directive 'sum'  and "in file '/etc/nagios/nagios_service.cfg' on line 22229"
    
    Because of newlines not collapsed:
    
            check_command                  check_prometheus!http://thanos-query.discovery.wmnet!(
      sum by (job) (rate(thanos_compact_group_compactions_failures_total{job=~"thanos-compact.*"}[5m]))
      /
      sum by (job) (rate(thanos_compact_group_compactions_total{job=~"thanos-compact.*"}[5m]))
    ) * 100
    !2!5!thanos_compact_failures!ge
    
    This reverts commit 98b8f7b3287c450a0a43dac440c0aa7469fc199f.
    
    Change-Id: I218bf2c7ec6c2b65c644c34df0c8e0641a52f7fe

--- modules/profile/manifests/icinga.pp
+++ modules/profile/manifests/icinga.pp
@@ -93,7 +93,6 @@ class profile::icinga(
 
     class { 'profile::bird::anycast_monitoring': }
     class { 'profile::prometheus::alerts': }
-    class { 'profile::thanos::alerts': }
     class { 'profile::maps::alerts': }
     class { 'profile::cache::kafka::alerts': }
     class { 'profile::mediawiki::alerts': }
