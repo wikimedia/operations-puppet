97df6e3bbc2d8d6f9a3475d8226dbe34176b149b
Cole White <cwhite@wikimedia.org>
2020-06-05 10:12:36 -0600

    profile: add loki_event filter script
    this is in preparation to update logstash-filter-verifier CI job to incorporate
    this directory in tests
    
    Bug: T222826
    Change-Id: I4d571730e012ea05e37bae4f760ac65d477bd2c3

---
+++ modules/profile/files/logstash/filter_scripts/loki_event.rb
@@ -0,0 +1,32 @@
+# Custom filter that populates a 'streams' top-level key in the logstash event with
+# the format Loki expects.
+# https://github.com/grafana/loki/blob/master/docs/api.md#post-lokiapiv1push
+
+require 'json'
+
+def register(params)
+  @event_type = params["type"]
+end
+
+def filter(event)
+  # event timestamp to nanoseconds
+  ts = (event.get('@timestamp').to_i * 1e9).to_s.split('.')[0]
+
+  case @event_type
+  when 'alert'
+    stream = { :type => 'alert', :host => event.get('host')}
+    values = [[ts, "#{event.get('icinga_state')} -- #{event.get('icinga_check_descr')}: #{event.get('icinga_message')}"]]
+  when 'sal'
+    stream = { :type => 'sal', :project => event['project'] }
+    values = [[ts, "#{event.get('nick')}: #{event.get('message')}"]]
+  when 'deploy'
+    stream = { :type => 'deploy' }
+    values = [[ts, "#{event.get('user')}: #{event.get('message')}"]]
+  else
+    # do nothing
+    return [event]
+  end
+
+  event.set('streams', [{ :stream => stream, :values => values }])
+  [event]
+end
