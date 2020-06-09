d09b6ccae58a36453f80046e9a73b9c2f0950ffc
John Bond <jbond@wikimedia.org>
2020-06-09 16:56:26 +0200

    profile::mail::jumpmcloud: remove jumpcloud
    We will instead us gsuite so this script is no longer needed
    
    Hosts: P:mail::mx
    
    Bug: T244792
    Change-Id: I7bf30eb2ac1ad19e6fa0abafc8f90a280e91d220

--- modules/profile/manifests/mail/jumpcloud.pp
+++ modules/profile/manifests/mail/jumpcloud.pp
@@ -4,39 +4,12 @@
 # @param managed_domain The domain name of the jumpcloud managed domain
 # @param aliases_dir location of aliases directory
 class profile::mail::jumpcloud (
-    Stdlib::Host     $api_uri        = lookup('profile::mail::jumpcloud::api_uri'),
-    String[40,40]    $api_key        = lookup('profile::mail::jumpcloud::api_key'),
-    Stdlib::Host     $managed_domain = lookup('profile::mail::jumpcloud::managed_domain'),
-    Stdlib::Unixpath $aliases_dir    = lookup('profile::mail::jumpcloud::aliases_dir')
 ) {
-    $config = @(CONFIG)
-    [DEFAULT]
-    api_uri: ${api_uri}
-    api_key: ${api_key}
-    aliases_directory: ${aliases_dir}
-    managed_domain: ${managed_domain}
-    | CONFIG
-    file {
-        default:
-            ensure => file,
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0550';
-        '/usr/local/sbin/jumpcloud_aliases':
-            content => file('profile/mail/jumpcloud_aliases.py');
-        '/etc/jumpcloud.ini':
-            mode    => '0440',
-            content => $config;
+    file{ ['/usr/local/sbin/jumpcloud_aliases',
+            '/etc/jumpcloud.ini']:
+        ensure => absent,
     }
     systemd::timer::job {'generate jumpcloud aliases':
-        ensure             => present,
-        description        => 'Generte local mailparts from jumpcloud API',
-        command            => '/usr/local/sbin/jumpcloud_aliases',
-        interval           => {
-            'start'    => 'OnCalendar',
-            'interval' => '*-*-* *:15:00', # Every hour at minute 15
-        },
-        monitoring_enabled => true,
-        user               => 'root',
+        ensure => absent,
     }
 }
