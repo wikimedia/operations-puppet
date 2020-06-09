794ded180fc2eb8fe3155ddf0caf17d3f348be1f
Luca Toscano <ltoscano@wikimedia.org>
2020-06-09 17:57:16 +0200

    archiva: move archiva-gitfat-link to systemd timer
    Bug: T252767
    Change-Id: I2c12c71ae94091ca6d88f813908100bb9d4fe1d8

--- modules/profile/manifests/archiva.pp
+++ modules/profile/manifests/archiva.pp
@@ -4,14 +4,55 @@
 # a git-fat store.
 #
 class profile::archiva(
-    $enable_backup = hiera('profile::archiva::enable_backup', false),
+    $enable_backup  = lookup('profile::archiva::enable_backup', { 'default_value' => false }),
+    $contact_groups = lookup('profile::archiva::contact_groups', { 'default_value' => 'analytics' }),
 ) {
     # needed by ssl_ciphersuite() used in ::archiva::proxy
     class { '::sslcert::dhparam': }
 
     class { '::archiva': }
 
-    class { '::archiva::gitfat': }
+    # The rsync daemon module will chroot to this directory
+    $archiva_path            = '/var/lib/archiva'
+    # git-fat symlinks will be created here.
+    $archiva_gitfat_path     = "${archiva_path}/git-fat"
+
+    # We want symlinks to be created with relative paths
+    # so that the rsync daemon module's chroot will work
+    # properly with symlinks.   All symlinks and targets
+    # must be relative and within the rsync module for
+    # this to work.  This path is relative to the
+    # directory in which git-fat links are created
+    # ($archiva_git_fat_path).
+    $archiva_repository_path = '../repositories'
+
+    file { $archiva_gitfat_path:
+        ensure => 'directory',
+        owner  => 'archiva',
+        group  => 'archiva',
+    }
+
+    # install script to symlink archiva .jars into a git-fat store
+    file { '/usr/local/bin/archiva-gitfat-link':
+        source => 'puppet:///modules/archiva/archiva-gitfat-link',
+        mode   => '0555',
+    }
+
+    $link_command = "cd ${archiva_gitfat_path} && /usr/local/bin/archiva-gitfat-link ${archiva_repository_path}"
+
+    systemd::timer::job { 'archiva-gitfat-link':
+        description               => 'Archiva tool to create jar symlinks using their sha1 checksum as filename.',
+        command                   => "/bin/bash -c '${link_command}'",
+        interval                  => {
+            'start'    => 'OnCalendar',
+            'interval' => '*-*-* *:00/5:00',
+        },
+        logfile_basedir           => '/var/log/archiva',
+        logfile_name              => 'archiva-gitfat-link.log',
+        syslog_identifier         => 'archiva-gitfat-link',
+        user                      => 'archiva',
+        monitoring_contact_groups => $contact_groups,
+    }
 
     # This uses modules/rsync to set up an rsync daemon service.
     # An empty address field will allow rsync to bind to IPv6/4
@@ -19,8 +60,6 @@ class profile::archiva(
     class { '::rsync::server':
         address => '',
     }
-
-    $archiva_path = $::archiva::gitfat::archiva_path
 
     # Set up an rsync module so that anybody
     # can rsync read from $gitfat_archiva_path.
