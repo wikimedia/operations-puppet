01361c936e55a00f124fe242ba1ab3bee6739767
Christian Aistleitner <christian@quelltextlich.at>
2020-06-09 16:24:44 +0200

    Gerrit: Fix log retention period in comment
    The log retention got bumped from 7 to 30 days in commit
    1066d685b4b46680567a7b97a0e2092e147f7f38
    
    Change-Id: If36525d9c6b8ced6b4887df5bc0859a80c322ebf

--- modules/gerrit/manifests/crons.pp
+++ modules/gerrit/manifests/crons.pp
@@ -10,7 +10,7 @@ class gerrit::crons() {
 
     cron { 'clear_gerrit_logs':
         # Gerrit rotates their own logs, but doesn't clean them out
-        # Delete logs older than a week
+        # Delete logs older than 30 days
         command => 'find /var/log/gerrit/ -name "*.gz" -mtime +30 -delete',
         user    => 'root',
         hour    => 1,
