3592083f2029b187e4a5b025538414106d89484d
John Bond <jbond@wikimedia.org>
2020-06-10 15:13:25 +0200

    facter: cpu_details add flags, model, bugs and family
    add new entries to the cpu_details fact based on /proc/cpuinfo for:
      flags
      bugs
      model
      cpu family
    
    also add legacy facts for cpu_flags and cpu_model so we can query via
    cumin
    
    Change-Id: I06ae4af7ebb745d47b6056fd902b8d4ddbbda9d7

--- modules/wmflib/lib/facter/cpu_details.rb
+++ modules/wmflib/lib/facter/cpu_details.rb
@@ -4,14 +4,29 @@ cpu_details = {
   'vulnerabilities' => {},
   'scaling_governor' => false,
   'scaling_driver' => false,
+  'model' => nil,
+  'family' => nil,
+  'flags' => [],
+  'bugs' => [],
   'cpus' => {}
 }
 Facter.add(:cpu_details) do
   confine :kernel => 'Linux'
   confine do
-    File.directory?('/sys/devices/system/cpu/vulnerabilities')
+    # File.directory?('/sys/devices/system/cpu/vulnerabilities')
+    File.file?('/proc/cpuinfo')
   end
   setcode do
+    File.foreach('/proc/cpuinfo') do |line|
+      if /^(:?cpu\s+)?(?<key>model|family)\s+:\s+(?<value>\d+)/ =~ line
+        cpu_details[key] = key == 'model' ? value.to_i.to_s(16) : value
+      end
+      if /^(?<key>flags|bugs)\s+:\s+(?<value>[\w\s]+)/ =~ line
+        cpu_details[key] = value.split(' ')
+      end
+      break if cpu_details['model'] && cpu_details['family'] && !cpu_details['flags'].empty? && !cpu_details['bugs'].empty?
+    end
+
     Dir.foreach('/sys/devices/system/cpu/vulnerabilities') do |vuln|
       next if %w{. ..}.include? vuln
       cpu_details['vulnerabilities'][vuln] = File.read(
@@ -44,4 +59,10 @@ Facter.add(:cpu_scaling_governor) do
 end
 Facter.add(:cpu_scaling_driver) do
   setcode { cpu_details['scaling_driver'] }
+end
+Facter.add(:cpu_flags) do
+  setcode { cpu_details['flags'] }
+end
+Facter.add(:cpu_model) do
+  setcode { cpu_details['model'] }
 end
---
+++ modules/wmflib/spec/unit/facter/cpu_details_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+cpu_details = {
+  'vulnerabilities' => {
+    'spectre_v2' => "Mitigation: Full generic retpoline, IBPB: conditional, IBRS_FW, STIBP: disabled, RSB filling",
+    'itlb_multihit' => "KVM: Vulnerable",
+    'mds' => "Mitigation: Clear CPU buffers; SMT Host state unknown",
+    'l1tf' => "Mitigation: PTE Inversion",
+    'spec_store_bypass' => "Mitigation: Speculative Store Bypass disabled via prctl and seccomp",
+    'tsx_async_abort' => "Not affected",
+    'spectre_v1' => "Mitigation: usercopy/swapgs barriers and __user pointer sanitization",
+    'meltdown' => "Mitigation: PTI"
+  },
+  'scaling_governor' => false,
+  'scaling_driver' => false,
+  'model' => "3a",
+  'family' => "6",
+  'flags' => ["fpu", "vme", "de", "pse", "tsc", "msr", "pae", "mce", "cx8", "apic", "sep", "mtrr",
+              "pge", "mca", "cmov", "pat", "pse36", "clflush", "mmx", "fxsr", "sse", "sse2",
+              "syscall", "nx", "rdtscp", "lm", "constant_tsc", "rep_good", "nopl", "xtopology",
+              "cpuid", "tsc_known_freq", "pni", "pclmulqdq", "ssse3", "cx16", "pcid", "sse4_1",
+              "sse4_2", "x2apic", "popcnt", "tsc_deadline_timer", "aes", "xsave", "avx", "f16c",
+              "rdrand", "hypervisor", "lahf_lm", "pti", "ssbd", "ibrs", "ibpb", "fsgsbase", "smep",
+              "erms", "xsaveopt", "arat", "md_clear"],
+  'bugs' => ["cpu_meltdown", "spectre_v1", "spectre_v2", "spec_store_bypass",
+             "l1tf", "mds", "swapgs", "itlb_multihit"],
+  'cpus' => {
+    'cpu0' => {
+      'scaling_governor' => false,
+      'scaling_driver' => false
+    }
+  }
+}
+
+describe Facter::Util::Fact.to_s do
+  before { Facter.clear }
+  context 'cpu_details' do
+    before :each do
+      File.stubs(:foreach).with('/proc/cpuinfo').multiple_yields(
+        ["model           : 58"],
+        ["cpu family      : 6"],
+        # rubocop:disable Metrics/LineLength
+        ["flags           : fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush mmx fxsr sse sse2 syscall nx rdtscp lm constant_tsc rep_good nopl xtopology cpuid tsc_known_freq pni pclmulqdq ssse3 cx16 pcid sse4_1 sse4_2 x2apic popcnt tsc_deadline_timer aes xsave avx f16c rdrand hypervisor lahf_lm pti ssbd ibrs ibpb fsgsbase smep erms xsaveopt arat md_clear",],
+        # rubocop:enable Metrics/LineLength
+        ["bugs            : cpu_meltdown spectre_v1 spectre_v2 spec_store_bypass l1tf mds swapgs itlb_multihit"]
+      )
+    end
+    it { expect(Facter.fact(:cpu_details).value['model']).to eq cpu_details['model'] }
+    it { expect(Facter.fact(:cpu_details).value['family']).to eq cpu_details['family'] }
+    it { expect(Facter.fact(:cpu_details).value['bugs']).to eq cpu_details['bugs'] }
+    it { expect(Facter.fact(:cpu_details).value['flags']).to eq cpu_details['flags'] }
+  end
+end
