33a7afa0da7e41e4d07d98bb33030735d8df8d88
Emanuele Rocca <ema@wikimedia.org>
2020-06-11 12:35:11 +0200

    varnish: use Host:varnishcheck.wm.org for icinga checks
    This change is needed by Ib24649153bc546839e450767f4343438e5fe37e5.
    
    Bug: T255015
    Change-Id: I34aedd7f250157c99f246715f6886042478ff195

--- modules/varnish/manifests/monitoring/instance.pp
+++ modules/varnish/manifests/monitoring/instance.pp
@@ -2,7 +2,7 @@ define varnish::monitoring::instance($instance) {
     $port = $title
     monitoring::service { "varnish http ${instance} - port ${port}":
         description    => "Varnish HTTP ${instance} - port ${port}",
-        check_command  => "check_http_varnish!varnishcheck!${port}",
+        check_command  => "check_http_varnish!varnishcheck.wikimedia.org!${port}",
         check_interval => 3,
         notes_url      => 'https://wikitech.wikimedia.org/wiki/Varnish',
     }
