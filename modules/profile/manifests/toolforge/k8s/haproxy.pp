fac8f5e864ca7cfa97c5e9be9aa663e41f77f347
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-06-11 13:18:52 +0200

    kubeadm: rename hiera key for ingress nodes
    We are moving towards a model in which not all worker nodes are serving the ingress.
    Instead of worker_nodes, use ingress_nodes, which is a more accurate name.
    
    Bug: T250172
    Bug: T195217
    Change-Id: Ia6e1a4ce3e4ce1483be5f2dd22c8198368728e85
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/toolforge/k8s/haproxy.pp
+++ modules/profile/manifests/toolforge/k8s/haproxy.pp
@@ -1,11 +1,11 @@
 class profile::toolforge::k8s::haproxy (
-    Array[Stdlib::Fqdn] $worker_nodes  = lookup('profile::toolforge::k8s::worker_nodes',   {default_value => ['localhost']}),
+    Array[Stdlib::Fqdn] $ingress_nodes = lookup('profile::toolforge::k8s::ingress_nodes',  {default_value => ['localhost']}),
     Stdlib::Port        $ingress_port  = lookup('profile::toolforge::k8s::ingress_port',   {default_value => 30000}),
     Array[Stdlib::Fqdn] $control_nodes = lookup('profile::toolforge::k8s::control_nodes',  {default_value => ['localhost']}),
     Stdlib::Port        $api_port      = lookup('profile::toolforge::k8s::apiserver_port', {default_value => 6443}),
 ) {
     class { '::profile::wmcs::kubeadm::haproxy':
-        worker_nodes  => $worker_nodes,
+        ingress_nodes => $ingress_nodes,
         ingress_port  => $ingress_port,
         control_nodes => $control_nodes,
         api_port      => $api_port,
