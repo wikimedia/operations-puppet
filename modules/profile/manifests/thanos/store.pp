2417f354d1eccfa549c9c07e120ad4f17f5b3690
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2020-06-16 17:23:42 +0200

    thanos: pass min / max time to store
    Allows both for gradual rollout (i.e. "hit object storage only for metrics older than X") and
    sharding of Thanos store according to time ranges.
    
    Bug: T252186
    Change-Id: I2b8c219279cc033903d225d00888d1c2bb4c986d

--- modules/profile/manifests/thanos/store.pp
+++ modules/profile/manifests/thanos/store.pp
@@ -3,6 +3,8 @@ class profile::thanos::store (
     Hash[String, String] $objstore_account = lookup('profile::thanos::objstore_account'),
     String $objstore_password = lookup('profile::thanos::objstore_password'),
     Array $query_hosts = lookup('profile::thanos::frontends'),
+    Optional[String] $max_time = lookup('profile::thanos::store::max_time', { 'default_value' => undef }),
+    Optional[String] $min_time = lookup('profile::thanos::store::min_time', { 'default_value' => undef }),
 ) {
     $http_port = 11902
     $grpc_port = 11901
@@ -12,6 +14,8 @@ class profile::thanos::store (
         objstore_password => $objstore_password,
         http_port         => $http_port,
         grpc_port         => $grpc_port,
+        max_time          => $max_time,
+        min_time          => $min_time,
     }
 
     # Allow access from query hosts
