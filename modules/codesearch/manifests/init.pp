fe6ada061d9d5e726e65a32a5b3328d81b6f49ed
Daniel Zahn <dzahn@wikimedia.org>
2020-06-19 13:03:43 -0400

    codesearch: move ferm rules from module to profile
    Bug: T114209
    Change-Id: I94548569f82039a8c25d416f4a8fc4342afdeb95

--- modules/codesearch/manifests/init.pp
+++ modules/codesearch/manifests/init.pp
@@ -115,17 +115,6 @@ class codesearch(
         ]
     }
 
-    ferm::conf { 'docker-preserve':
-        ensure => present,
-        prio   => 20,
-        source => 'puppet:///modules/codesearch/ferm/docker-preserve.conf',
-    }
-
-    ferm::service { 'codesearch':
-        proto => 'tcp',
-        port  => '3002',
-    }
-
     file { '/etc/codesearch_ports.json':
         ensure  => present,
         content => ordered_json($ports),
--- modules/profile/manifests/codesearch.pp
+++ modules/profile/manifests/codesearch.pp
@@ -3,6 +3,17 @@ class profile::codesearch (
     Hash[String, Integer] $ports = lookup('profile::codesearch::ports'),
 ) {
 
+    ferm::conf { 'docker-preserve':
+        ensure => present,
+        prio   => 20,
+        source => 'puppet:///modules/codesearch/ferm/docker-preserve.conf',
+    }
+
+    ferm::service { 'codesearch':
+        proto => 'tcp',
+        port  => '3002',
+    }
+
     class { '::codesearch':
         base_dir => $base_dir,
         ports    => $ports,
