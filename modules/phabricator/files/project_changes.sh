bf3686c72e9c90d6cb2904fbe3d377ef8a02d9e3
Andre Klapper <a9016009@gmx.de>
2020-06-23 09:16:34 +0200

    phabricator weekly changes email: List projects' color/icon violations
    https://www.mediawiki.org/wiki/Phabricator/Project_management#Types_of_Projects
    defines which colors can be combined with which icons. This is not
    always followed, hence automate getting a list of these projects.
    
    Bug: T249806
    Change-Id: Id3486c5dd91b466a10bbb5bd412b232f7e572647

--- modules/phabricator/files/project_changes.sh
+++ modules/phabricator/files/project_changes.sh
@@ -117,6 +117,23 @@ SELECT CONCAT("https://phabricator.wikimedia.org/project/profile/", p.id) AS url
 END
 )
 
+# echo "result_problematic_color_icon_combos"
+# see https://phabricator.wikimedia.org/T249806
+result_problematic_color_icon_combos=$(MYSQL_PWD=${sql_pass} /usr/bin/mysql -h $sql_host -P $sql_port -u$sql_user $sql_name << END
+SELECT CONCAT("https://phabricator.wikimedia.org/project/profile/", id) AS url, name, color, icon
+    FROM phabricator_project.project
+    WHERE status != "100"
+    AND ((color = "violet" AND icon != "group")
+        OR (color = "yellow" AND icon != "tag")
+        OR (color = "orange" AND (icon != "goal" AND icon != "release"))
+        OR (color = "checkered" AND icon != "account")
+        OR (color = "green" AND icon != "timeline")
+        OR color = "indigo"
+        OR (color = "grey" AND icon != "meta" AND name != "Trash"));
+
+END
+)
+
 # echo "result_inactive_users_assigned_tasks"
 # see https://phabricator.wikimedia.org/T157740
 result_inactive_users_assigned_tasks=$(MYSQL_PWD=${sql_pass} /usr/bin/mysql -h $sql_host -P $sql_port -u$sql_user $sql_name << END
@@ -289,6 +306,10 @@ ARCHIVED PROJECTS WITH OPEN TASKS WITH NO OTHER ACTIVE PROJECTS, EXCLUDING PATCH
 ${result_archived_projects_open_tasks}
 
 
+PROJECTS WITH COLOR/ICON COMBINATIONS WHICH MIGHT VIOLATE OUR GUIDELINES:
+${result_problematic_color_icon_combos}
+
+
 DISABLED USER ACCOUNTS WITH OPEN TASKS ASSIGNED:
 ${result_inactive_users_assigned_tasks}
 
@@ -296,11 +317,14 @@ ${result_inactive_users_assigned_tasks}
 USER ACCOUNTS WHO BECAME AN ASSIGNEE LAST WEEK AND HAD LESS THAN 5 TASKS EVER ASSIGNED:
 ${result_new_user_assignees}
 
+
 USER PROFILES WITH A URL IN THEIR DESC WHICH CHANGED WITHIN THE LAST 1 WEEK (to spam check):
 ${result_user_profile_urls}
 
+
 OPEN TASKS WITH A DUE DATE MORE THAN 1 MONTH AGO:
 ${result_past_due_dates}
+
 
 PROJECTS WHICH HAVE AN EMPTY PROJECT DESCRIPTION:
 ${result_parent_projects_without_desc}
@@ -315,6 +339,7 @@ ${result_herald_rules}
 OPEN TASKS THAT HAVE BEEN ASSIGNED TO THE SAME USER FOR MORE THAN FOUR YEARS:
 ${result_cookie_licked_tasks}
 Note: Tasks might have been un- and re-assigned to same user in the meantime.
+
 
 Yours sincerely,
 Fab Rick Aytor
