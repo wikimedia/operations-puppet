911958ea3dabd3599b808113fb2394f552daf9bf
Chris Danis <cdanis@wikimedia.org>
2020-06-22 23:12:17 -0400

    MW PHP-FPM worker saturation: make it page
    No noise from this alert so far and this seems like a good signal that
    something is wrong in a user-visible way.
    
    TODO: Instead of having this page, page instead for 75%ile latency above
    a reasonable threshold (1 second?)
    
    Bug: T252605
    Change-Id: I850441699bcf45723c9841ad0da0a0fdfce576ab

--- modules/profile/manifests/mediawiki/alerts.pp
+++ modules/profile/manifests/mediawiki/alerts.pp
@@ -41,6 +41,7 @@ class profile::mediawiki::alerts {
         critical        => 0.3,
         notes_link      => 'https://docs.google.com/document/d/1SeXdegjsfL94R6XYB1I4Uv8yjCPH1tVXeL0taJF0NNs/preview#heading=h.qe04i0ld9cvl',
         dashboard_links => ["https://grafana.wikimedia.org/d/RIA1lzDZk/application-servers-red-dashboard?panelId=54&fullscreen&orgId=1&from=now-3h&to=now&var-datasource=${site} prometheus/ops&var-cluster=${cluster}"],
+        nagios_critical => true,
       }
     }
   }
