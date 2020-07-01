969b0fa3d9a6ddd4d8db4695e3d917ecda1830dd
John Bond <jbond@wikimedia.org>
2020-07-01 10:37:08 +0200

    profile::waf::apache::administrative: remove waf config
    This change absents all the WAF resources we should merge this and wait
    30 minutes before applying the follow up patches in the chain
    
    Hosts: P:waf::apache::administrative
    
    Bug: T253632
    Change-Id: I742987e813fff5d06e3543ce1c324f8636d9bda9

--- modules/profile/manifests/waf/apache2/administrative.pp
+++ modules/profile/manifests/waf/apache2/administrative.pp
@@ -4,49 +4,28 @@ class profile::waf::apache2::administrative {
     # Not using require_package so apt::pin may be applied
     # before attempting to install package.
     package { 'libapache2-mod-security2':
-        ensure => present,
+        ensure => absent,
     }
 
     # Ensure that the CRS modsecurity ruleset is not used.
     file { '/etc/apache2/mods-available/security2.conf':
-        ensure  => present,
-        source  => 'puppet:///modules/profile/waf/apache2/security2.conf',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        require => Package['libapache2-mod-security2'],
+        ensure  => absent,
     }
 
     httpd::conf { 'modsecurity_admin':
-        priority => 00,
-        content  => secret('waf/modsecurity_admin.conf'),
+        ensure   => absent,
     }
 
     file { '/etc/apache2/admin1':
-        ensure  => present,
-        mode    => '0644',
-        owner   => 'root',
-        group   => 'root',
-        content => secret('waf/admin1'),
-        notify  => Service['apache2'],
+        ensure  => absent,
     }
 
     file { '/etc/apache2/admin2':
-        ensure  => present,
-        mode    => '0644',
-        owner   => 'root',
-        group   => 'root',
-        content => secret('waf/admin2'),
-        notify  => Service['apache2'],
+        ensure  => absent,
     }
 
     file { '/etc/apache2/admin3':
-        ensure  => present,
-        mode    => '0644',
-        owner   => 'root',
-        group   => 'root',
-        content => secret('waf/admin3'),
-        notify  => Service['apache2'],
+        ensure  => absent,
     }
 
     # TBD: add link from /etc/apache/modsec_rules
@@ -57,6 +36,7 @@ class profile::waf::apache2::administrative {
     #     target  => '/srv/deployment/apache2modsec',
     # }
     scap::target { 'apache2modsec/apache2modsec':
+        ensure      => absent,
         deploy_user => 'apache2modsec',
         key_name    => 'apache2modsec',
     }
