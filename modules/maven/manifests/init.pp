38ef998496879cc226c4676677cc3845f59ae4be
Luca Toscano <ltoscano@wikimedia.org>
2020-07-01 15:19:53 +0200

    maven: remove main /etc/maven/settings.xml
    The mirrored repository is not available anymore after
    the migration of Archiva to Buster (and archiva1002),
    and having only the releases mirror doesn't seem to be
    needed anymore.
    
    Bug: T252767
    Change-Id: Icc7ba0b0eee7bfe1dc0573f945fb3cb7df13f423

--- modules/maven/manifests/init.pp
+++ modules/maven/manifests/init.pp
@@ -8,15 +8,6 @@ class maven {
         }
     }
 
-    file { '/etc/maven/settings.xml':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        source  => 'puppet:///modules/maven/settings.xml',
-        require => Package['maven'],
-    }
-
     # Install ivysettings uses archiva.wikimedia.org repositories to resolve dependencies.
     # This can be used with spark jobs to resolve dependencies at runtime.
     # While perhaps 'ivy' things don't belong in a WMF maven model, this is the easiest
