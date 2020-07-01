da76d75f35ce07a4f4188577552d23e4e29919fc
John Bond <jbond@wikimedia.org>
2020-07-01 14:47:09 +0200

    apero_cas: add abbility to configure per service properties
    This should allow us to override various options such as the
    X-Frame Header
    
    Bug: T251513
    Change-Id: I7efda9b730c7ba3658bd5ea18a9c3b77d97d8bfb

--- modules/apereo_cas/manifests/service.pp
+++ modules/apereo_cas/manifests/service.pp
@@ -1,18 +1,23 @@
 define apereo_cas::service (
-    String  $class,
-    Integer $id,
-    String  $service_id,
-    Hash    $attribute_release_policy,
-    Hash    $access_strategy
+    String         $class,
+    Integer        $id,
+    String         $service_id,
+    Hash           $attribute_release_policy,
+    Hash           $access_strategy,
+    Optional[Hash] $properties = {}
 ) {
     include apereo_cas
-    $data = {
+    $base_data = {
         '@class'                 => $class,
         'name'                   => $title,
         'serviceId'              => $service_id,
         'attributeReleasePolicy' => $attribute_release_policy,
         'id'                     => $id,
         'accessStrategy'         => $access_strategy,
+    }
+    $data = $properties.empty ? {
+        true    => $base_data,
+        default => $base_data + $properties,
     }
     file {"${apereo_cas::services_dir}/${title}-${id}.json":
         ensure  => file,
