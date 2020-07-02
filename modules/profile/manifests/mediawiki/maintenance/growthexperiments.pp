47aa3edfa02f322140356265e3aa3465a490fa64
Gergő Tisza <tgr.huwiki@gmail.com>
2020-07-02 21:33:24 +0200

    GrowthExperiments: shorten welcome survey retention window
    Bug: T252575
    Change-Id: I922dd75bf93caaebf2f15ca6cfa7b150d4f47607

--- modules/profile/manifests/mediawiki/maintenance/growthexperiments.pp
+++ modules/profile/manifests/mediawiki/maintenance/growthexperiments.pp
@@ -1,10 +1,10 @@
 class profile::mediawiki::maintenance::growthexperiments {
     # Purge old welcome survey data (personal data used in user options,
-    # with a 360-day retention exception) that's within 30 days of expiry,
-    # twice a month. See T208369. Logs are saved to
+    # with a 270-day retention exception) that's within 30 days of expiry,
+    # twice a month. See T208369 and T252575. Logs are saved to
     # /var/log/mediawiki/mediawiki_job_growthexperiments-deleteOldSurveys/syslog.log
     profile::mediawiki::periodic_job { 'growthexperiments-deleteOldSurveys':
-        command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/growthexperiments.dblist extensions/GrowthExperiments/maintenance/deleteOldSurveys.php --cutoff 335',
+        command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/growthexperiments.dblist extensions/GrowthExperiments/maintenance/deleteOldSurveys.php --cutoff 240',
         interval => '*-*-01,15 03:15:00',
     }
 }
