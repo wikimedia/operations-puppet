0d558b72d9f7956342eaeb877cde4915802b3bd8
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2020-07-07 12:37:00 +0200

    restbase-dev: add the service proxy
    Change-Id: I78b6c5530ad9241362f78cfb76ebd5e8aa8dda81

--- modules/role/manifests/restbase/dev_cluster.pp
+++ modules/role/manifests/restbase/dev_cluster.pp
@@ -5,7 +5,7 @@
 class role::restbase::dev_cluster {
     # Just includes base, no LVS etc.
     include ::role::restbase::base
-
+    include ::profile::services_proxy::envoy  # service-to-service proxy
     system::role { 'restbase::dev_cluster':
         description => "Restbase-dev (${::realm})",
     }
