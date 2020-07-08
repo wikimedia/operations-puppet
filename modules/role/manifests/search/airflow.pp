ea8994f6002b3f1da30b7b13a7ecbe44132867cb
Erik Bernhardson <ebernhardson@wikimedia.org>
2020-07-08 08:31:50 -0700

    Deploy analytics refinery to airflow instances
    Airflow is a task scheduling platform. Deploying the analytics
    refinery to airflow allows for jobs that create data to also
    clean that data up, and defining it all in a central place.
    
    Change-Id: I20c0904121a5eb9871ecdb573e9eef0338f45705

--- modules/role/manifests/search/airflow.pp
+++ modules/role/manifests/search/airflow.pp
@@ -12,6 +12,7 @@ class role::search::airflow {
     include ::profile::analytics::cluster::users
     include ::profile::analytics::cluster::client
     include ::profile::analytics::cluster::repositories::statistics
+    include ::profile::analytics::refinery::repository
     include ::profile::analytics::search::airflow
 
     include ::profile::kerberos::client
