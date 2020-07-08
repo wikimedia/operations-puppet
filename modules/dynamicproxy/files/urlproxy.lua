876b77991164e11afc8b0cd55f5de9055f1f5ed5
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-07-08 13:07:58 +0200

    toolforge: urlproxy: drop support for the legacy routing scheme
    The toolforge front proxy no longer receives traffic to the tools.wmflabs.org domain, and therefore
    we no longer support the legacy routing scheme.
    
    We can simplify the setup by assuming everything is using the toolforge.org domain and, if not, the
    tool-fourohfour webservice running in k8s will handle it.
    
    Bug: T234617
    Change-Id: I9de66b9e88977095085ebbac934cb6ae5dda7c4c
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/dynamicproxy/files/urlproxy.lua
+++ modules/dynamicproxy/files/urlproxy.lua
@@ -21,14 +21,6 @@ local red = redis:new()
 red:set_timeout(1000)
 red:connect('127.0.0.1', 6379)
 
--- Does str end with ending?
--- @param str String to check
--- @param ending Substring to look for at end of str
--- From http://lua-users.org/wiki/StringRecipes
-function ends_with(str, ending)
-    return ending == "" or str:sub(-#ending) == ending
-end
-
 --- Look up a backend for a given tool
 -- @param toolname Name of the tool
 -- @param path url URL
@@ -64,86 +56,8 @@ function route_backend_and_exit_if_ok(toolname, url)
     ngx.exit(ngx.OK)
 end
 
---- Generate the redirect URL by checking first for nginx vars for safety
--- @param toolname Name of the tool
--- @param path Path fragment of URL
-function compute_redirect_url(toolname, path)
-    -- toolname and path were checked in the previous function
+local subdomain = string.match(ngx.var.http_host, "^[^.]+")
+route_backend_and_exit_if_ok(subdomain, "/")
 
-    if not ngx.var.canonical_scheme then
-        ngx.log(ngx.STDERR, 'ERROR: no $canonical_scheme var defined in nginx conf. This is a Toolforge outage!')
-        ngx.log(ngx.STDERR, 'ERROR: the LUA code expects the $canonical_scheme var to be set to "https://"')
-        ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-    end
-
-    if not ngx.var.canonical_domain then
-        ngx.log(ngx.STDERR, 'ERROR: no $canonical_domain var defined in nginx conf. This is a Toolforge outage!')
-        ngx.log(ngx.STDERR, 'ERROR: the LUA code expects the $canonical_domain var to be set to "toolforge.org"')
-        ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-    end
-
-    local is_args = ''
-    if ngx.var.is_args then
-        is_args = ngx.var.is_args
-    end
-    local args = ''
-    if ngx.var.args then
-        args = ngx.var.args
-    end
-
-    return ngx.var.canonical_scheme .. toolname .. '.' .. ngx.var.canonical_domain .. path .. is_args .. args
-end
-
---- Look up a canonical redirect target for a given tool
--- @param toolname Name of the tool
--- @param path Path fragment of the URL
-function try_redirect_and_exit_if_ok(toolname, path)
-    if not toolname or not path then
-        return
-    end
-
-    -- check to see if we have a redirect key for this tool, that's all we need
-    -- to generate the redirect
-    local redirect = red:hgetall('redirect:' .. toolname)
-    if not redirect or next(redirect) == nil then
-        return
-    end
-
-    -- Use a connection pool of 256 connections with a 32s idle timeout
-    -- This also closes the current redis connection. At this point we don't
-    -- expect any more calls to redis for this end user request.
-    red:set_keepalive(1000 * 32, 256)
-
-    -- We are redirecting the following:
-    --- from  tools.wmflabs.org/$tool/index.php?param=foo
-    --- to    $tool.toolforge.org/index.php?param=foo
-    -- redirect happens right here; nothing else is evaluated in the nginx conf
-    return ngx.redirect(compute_redirect_url(toolname,path), 307)
-end
-
-if ends_with(ngx.var.http_host, ngx.var.canonical_domain) then
-    -- T253816: The hostname used in the request is *.toolforge.org
-    -- Either there is a grid backend, or we should pass on to k8s
-
-    -- case 1: webservices running in the grid using $tool.toolforge.org
-    local subdomain = string.match(ngx.var.http_host, "^[^.]+")
-    route_backend_and_exit_if_ok(subdomain, "/")
-
-    -- case 2: webservices running in k8s using $tool.toolforge.org
-    ngx.exit(ngx.OK)
-end
-
--- if no subdomain-based routing was found, then use the legacy routing scheme
-local captures = ngx.re.match(ngx.var.uri, "^/([^/]*)(/.*)?$")
-local prefix = captures[1]
-local rest = captures[2] or "/"
--- case 3: webservices running in the grid using tools.wmflabs.org/$tool
--- case 3: the webservice used --canonical and wants a redirect to toolforge.org
--- case 3: eventually, next request will be for case 1
-try_redirect_and_exit_if_ok(prefix, rest)
--- case 4: webservices running in the grid using tools.wmflabs.org/$tool
--- case 4: the webservice didn't use --canonical
-route_backend_and_exit_if_ok(prefix, rest)
-
--- case 5: anything else, the tool-fourohfour webservice will handle it!
+-- anything else, send it to k8s
 ngx.exit(ngx.OK)
