07ed360f3efdc890399fbff78bd4b432670f2bb9
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-07-09 10:25:45 +0200

    No longer install osm2pgsql from stretch-backports
    Now that Stretch is an LTS release, stretch-backports will vanish soon.
    I've imported osm2pgsql into the "main" component of the repository,
    so that will continue to be available. This also simplifies the setup
    since we now can use require_package() as for the rest of the packages.
    
    Bug: T256877
    Change-Id: I47ee138a95d91000891d606a37fbfd4fcc54dc57

--- modules/osm/manifests/init.pp
+++ modules/osm/manifests/init.pp
@@ -1,24 +1,7 @@
 #
 
 class osm {
-
-    # Use backports versions of osm2pgsql for improved memory handling and other updates
-    if os_version('debian == stretch') {
-        apt::pin { 'osm2pgsql':
-            pin      => 'release a=stretch-backports',
-            priority => '1001',
-            before   => Package['osm2pgsql'],
-        }
-    }
-
-    # require_package creates a dynamic intermediate class that makes declaring
-    # dependencies a bit strange. Let's use package directly here.
-    if !defined(Package['osm2pgsql']) {
-        package { 'osm2pgsql':
-            ensure => 'present',
-        }
-    }
-
+    require_package('osm2pgsql')
     require_package('osmosis')
     require_package('osmium-tool')
     require_package('osmborder')
