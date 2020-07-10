0df73b81b58d87f9fa636922f5ced1d02fc19e2d
Cwhite <cwhite@wikimedia.org>
2020-07-10 15:57:59 +0000

    Revert "ci: switch integration.wikimedia.org to scap DocumentRoot"
    This reverts commit 1112f8237565170df6294eddfa2910756f985bf0.
    
    Reason for revert: appears to have broken integration.wm.o
    
    Change-Id: I401261e670e7c80b860c804aa0844cf7c5a272a6

--- modules/profile/manifests/ci/website.pp
+++ modules/profile/manifests/ci/website.pp
@@ -12,4 +12,26 @@ class profile::ci::website {
         content => template('contint/apache/integration.wikimedia.org.erb'),
     }
 
+    # Static files in these docroots are in integration/docroot.git
+
+    file { '/srv/org':
+        ensure => directory,
+        mode   => '0775',
+        owner  => 'jenkins-slave',
+        group  => 'jenkins-slave',
+    }
+
+    file { '/srv/org/wikimedia':
+        ensure => directory,
+        mode   => '0775',
+        owner  => 'jenkins-slave',
+        group  => 'jenkins-slave',
+    }
+    file { '/srv/org/wikimedia/integration':
+        ensure => directory,
+        mode   => '0775',
+        owner  => 'jenkins-slave',
+        group  => 'jenkins-slave',
+    }
+
 }
