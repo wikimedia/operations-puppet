e70728582a31a644e6ebf56e125f0d4baf7760ba
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-07-13 10:08:01 +0200

    Switch yarn.wikimedia.org to CAS
    Bug: T159584
    Change-Id: I17c315bb99ab71e30c67976ad28dcd3b394d5075

--- modules/profile/manifests/hadoop/yarn_proxy.pp
+++ modules/profile/manifests/hadoop/yarn_proxy.pp
@@ -3,26 +3,18 @@
 # Sets up a yarn ldap auth http proxy to the Hadoop ResourceManager web interface.
 #
 class profile::hadoop::yarn_proxy (
-    Hash $ldap_config        = lookup('ldap', Hash, hash, {}),
 ) {
     require profile::hadoop::httpd
     require ::profile::analytics::httpd::utils
-    include ::passwords::ldap::production
-
-    $ldap_server_primary = $ldap_config['ro-server']
-    $ldap_server_fallback = $ldap_config['ro-server-fallback']
-    $proxypass = $passwords::ldap::production::proxypass
-
-    $resourcemanager_primary_host = $profile::hadoop::common::resourcemanager_hosts[0]
 
     $server_name = $::realm ? {
         'production' => 'yarn.wikimedia.org',
         'labs'       => "yarn-${::labsproject}.${::site}.wmnet",
     }
 
-    # Set up the VirtualHost
-    httpd::site { 'yarn.wikimedia.org':
-        content => template('profile/hadoop/yarn.vhost.erb'),
-        require => File['/var/www/health_check'],
+    $resourcemanager_primary_host = $profile::hadoop::common::resourcemanager_hosts[0]
+
+    class {'profile::idp::client::httpd':
+        vhost_settings => { 'res_manager' => $resourcemanager_primary_host },
     }
 }
