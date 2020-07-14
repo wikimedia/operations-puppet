b00e59c6f24b45b80a73c9537408f50eec017d98
John Bond <jbond@wikimedia.org>
2020-07-14 10:36:43 +0200

    prometheus::memcached_exporter: fix arguments hiera call
    Currently the hiera call points to a variable which dosen't exists.  the
    variables defined in hiera are correct and located via automatic
    parameter lookup
    
    Also add type checking and convert from hiera to lookup call
    
    Hosts: P:prometheus::mcrouter_exporter
    
    Change-Id: I46c8f5c66270255653e7f9e3e4c07e56197afde8

--- modules/profile/manifests/prometheus/memcached_exporter.pp
+++ modules/profile/manifests/prometheus/memcached_exporter.pp
@@ -1,6 +1,6 @@
 class profile::prometheus::memcached_exporter (
-    $prometheus_nodes = hiera('prometheus_nodes'),
-    $arguments = hiera('profile::prometheus::memcached_exporter', ''),
+    Array[Stdlib::Host] $prometheus_nodes = lookup('prometheus_nodes'),
+    String              $arguments        = lookup('profile::prometheus::memcached_exporter::arguments'),
 ) {
     prometheus::memcached_exporter { 'default':
         arguments => $arguments,
