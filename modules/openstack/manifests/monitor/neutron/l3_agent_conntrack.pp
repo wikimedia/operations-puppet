2201ac27f5977fa817da00f3c370fc70acbedb04
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-07-14 13:14:28 +0200

    openstack: monitor: neutron: nf_conntrack: run the NRPE check as root using sudo
    The check script requires root, given reading netns is a privileged operation.
    
    Bug: T257552
    Change-Id: I235dda1699fdf6b7c90da944d38c3d2a972c87b8
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/openstack/manifests/monitor/neutron/l3_agent_conntrack.pp
+++ modules/openstack/manifests/monitor/neutron/l3_agent_conntrack.pp
@@ -7,9 +7,14 @@ class openstack::monitor::neutron::l3_agent_conntrack (
         source => 'puppet:///modules/openstack/monitor/neutron/nrpe-neutron-conntrack.py',
     }
 
+    sudo::user { 'nagios_neutron_l3_agent_conntrack':
+        user       => 'nagios',
+        privileges => ["ALL = NOPASSWD: ${script}"],
+    }
+
     nrpe::monitor_service { 'check-neutron-conntrack':
         ensure        => 'present',
-        nrpe_command  => $script,
+        nrpe_command  => "/usr/bin/sudo ${script}",
         description   => 'Check nf_conntrack usage in neutron netns',
         require       => File[$script],
         contact_group => 'wmcs-team,admins',
