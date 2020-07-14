d8280f4dc2f47eef39b720d7c77ba79fe8dab276
John Bond <jbond@wikimedia.org>
2020-07-14 10:46:33 +0200

    role: noop style change
    Change-Id: I6b69092871e4753547f00425278be2c7d2648f2c

--- modules/role/manifests/deployment_server.pp
+++ modules/role/manifests/deployment_server.pp
@@ -1,39 +1,39 @@
 # MediaWiki Deployment Server (prod)
 class role::deployment_server {
-    include ::profile::standard
-    include ::profile::base::firewall
+    include profile::standard
+    include profile::base::firewall
 
     # Install the scap server components.
-    include ::profile::mediawiki::common
-    include ::profile::mediawiki::mcrouter_wancache
-    include ::profile::prometheus::mcrouter_exporter
-    include ::profile::mediawiki::nutcracker
-    include ::profile::mediawiki::deployment::server
-    include ::profile::scap::dsh
+    include profile::mediawiki::common
+    include profile::mediawiki::mcrouter_wancache
+    include profile::prometheus::mcrouter_exporter
+    include profile::mediawiki::nutcracker
+    include profile::mediawiki::deployment::server
+    include profile::scap::dsh
     # Install the keyholder agents
-    include ::profile::keyholder::server
+    include profile::keyholder::server
     # Install conftool
-    include ::profile::conftool::client
+    include profile::conftool::client
 
     # All needed classes for deploying mediawiki
-    include ::profile::releases::mediawiki::security
-    include ::profile::releases::upload
+    include profile::releases::mediawiki::security
+    include profile::releases::upload
 
     # Kubernetes deployments
-    include ::profile::kubernetes::deployment_server
+    include profile::kubernetes::deployment_server
 
     # apache-fast-test and co.
-    include ::profile::mediawiki::web_testing
-    include ::profile::httpbb
+    include profile::mediawiki::web_testing
+    include profile::httpbb
 
-    include ::profile::backup::host
+    include profile::backup::host
     backup::set {'home': }
     # proxy for connection to other servers
-    include ::profile::services_proxy::envoy
+    include profile::services_proxy::envoy
 
     # in cloud mount a second disk as /srv
     if $::realm == 'labs' {
-        require ::profile::labs::lvm::srv
+        require profile::labs::lvm::srv
     }
 
     system::role { 'deployment_server':
--- modules/role/manifests/mediawiki/common.pp
+++ modules/role/manifests/mediawiki/common.pp
@@ -1,12 +1,12 @@
 class role::mediawiki::common {
-    include ::profile::standard
-    include ::profile::mediawiki::scap_proxy
-    include ::profile::mediawiki::common
-    include ::profile::mediawiki::nutcracker
+    include profile::standard
+    include profile::mediawiki::scap_proxy
+    include profile::mediawiki::common
+    include profile::mediawiki::nutcracker
 
-    include ::profile::mediawiki::mcrouter_wancache
-    include ::profile::prometheus::mcrouter_exporter
+    include profile::mediawiki::mcrouter_wancache
+    include profile::prometheus::mcrouter_exporter
 
     # proxy for connection to other servers
-    include ::profile::services_proxy::envoy
+    include profile::services_proxy::envoy
 }
