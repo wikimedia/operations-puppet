ae283a2cd2d561bafb05244c995e7a1a53cc0933
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-07-16 15:46:32 +0200

    Add separate role for Yarn
    Bug: T258152
    Change-Id: I667d0b35f61a6c9be705133dd38edc5221825716

---
+++ modules/role/manifests/analytics_cluster/hadoop/yarn.pp
@@ -0,0 +1,19 @@
+# Class: role::analytics_cluster::hadoop::yarn
+#
+# Hadoop Yarn UI
+#
+class role::analytics_cluster::hadoop::yarn {
+    system::role { 'analytics_cluster::hadoop::yarn':
+        description => 'Hadoop Yarn ResourceManager web interface'
+    }
+
+    include ::profile::java
+    include ::profile::tlsproxy::service
+    include ::profile::hadoop::yarn_proxy
+
+    include ::profile::kerberos::client
+    include ::profile::kerberos::keytabs
+
+    include ::profile::base::firewall
+    include ::profile::standard
+}
