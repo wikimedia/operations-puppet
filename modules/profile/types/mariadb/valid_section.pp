4c87f632015415cade34115e9f06c4b249a84184
Stephen Shirley <sshirley@wikimedia.org>
2020-07-20 11:09:28 +0200

    mariadb: Comment out sections that do not appear in puppet.
    Bug: T258376
    Change-Id: If8ab16e4946f5a160c1fef058c09eb92e64376fd

--- modules/profile/types/mariadb/valid_section.pp
+++ modules/profile/types/mariadb/valid_section.pp
@@ -1,10 +1,10 @@
 type Profile::Mariadb::Valid_section = Enum[
     # Mediawiki metadata
     's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8',
-    # Wikitech
-    's10',
-    # labtestwiki
-    's11',
+    # Wikitech - hosted in m5, does not appear in puppet T258376
+    #'s10',
+    # labtestwiki - hosted on clouddb2001-dev, does not appear in puppet T258376
+    #'s11',
     # Mediawiki extension data
     'x1',
     # Mediawiki page content (External Storage)
