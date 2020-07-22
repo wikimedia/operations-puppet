bec5199dc0d5f616d622a33c61b0b53bfea53a9b
Jbond <jbond@wikimedia.org>
2020-07-22 09:00:02 +0000

    labs - hiera: migrate to hiera version5
    This change updates the codfw1dev hiera config file to use hiera version 5 format.
    It also re-writes the httpyaml custom backend to use the modern API
    
    Hosts: re:.*codfw1dev
    
    Change-Id: I29b557774ef16c176510597d91f0a9b4dc4494ea

---
+++ modules/cloudlib/lib/puppet/functions/cloudlib/httpyaml.rb
@@ -0,0 +1,36 @@
+Puppet::Functions.create_function(:'cloudlib::httpyaml') do
+  require 'net/http'
+  require 'uri'
+  require 'yaml'
+  dispatch :data_hash do
+    param 'Hash', :options
+    param 'Puppet::LookupContext', :context
+  end
+  def data_hash(options, context)
+    uri = URI.parse(options['uri'])
+    path = URI.escape(context.interpolate(uri.request_uri))
+    if context.cache_has_key(path)
+      context.explain { "Returning cached value for #{path}" }
+      value = context.cached_value(path)
+      return value
+    else
+      context.explain { "Querying #{uri}" }
+      if context.cache_has_key('__http_handler')
+        http_handler = context.cached_value('__http_handler')
+      else
+        http_handler = Net::HTTP.new(uri.host, uri.port)
+        context.cache('__http_handler', http_handler)
+      end
+      request = Net::HTTP::Get.new(path)
+      begin
+        response = http_handler.request(request)
+        body = YAML.safe_load(response.body)
+        value = body.fetch('hiera', {})
+        context.cache(path, value)
+        return value
+      rescue StandardError => e
+        raise Puppet::DataBinding::LookupError, "cloudlib::httpyaml failed #{e.message}"
+      end
+    end
+  end
+end
