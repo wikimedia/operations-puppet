7e2e623878cb14ddae75da3efeaf855d163ff0d9
Daniel Zahn <dzahn@wikimedia.org>
2020-07-23 14:51:36 -0700

    aphlict: add scap deploy target and missing parameters
    In order to get the aphlict files on the new dedicated VM for
    it we need to deploy Phabricator to it.
    
    Or at least that's the easiest way forward to add the
    scap::target for Phabricator as opposed to separating
    the deploy repos as well.
    
    Add the scap::target, symlink from root dir to deployment dir
    and missing parameters. Copied and adjusted from the main
    phabricator module.
    
    Bug: T238593
    Change-Id: Ic5603ee3ef7cab4763fa3c4c80582a78b31b3fa6

--- modules/profile/manifests/phabricator/aphlict.pp
+++ modules/profile/manifests/phabricator/aphlict.pp
@@ -1,15 +1,17 @@
 # aphlict for phabricator
 #
 class profile::phabricator::aphlict (
-    Boolean                     $aphlict_ssl        = lookup('phabricator_aphlict_enable_ssl',
-                                                      { 'default_value' => false }),
-    Optional[Stdlib::Unixpath]  $aphlict_cert       = lookup('phabricator_aphlict_cert',
-                                                      { 'default_value' => undef }),
-    Optional[Stdlib::Unixpath]  $aphlict_key        = lookup('phabricator_aphlict_key',
-                                                      { 'default_value' => undef }),
-    Optional[Stdlib::Unixpath]  $aphlict_chain      = lookup('phabricator_aphlict_chain',
-                                                      { 'default_value' => undef }),
+    Stdlib::Unixpath $base_dir = lookup('aphlict_base_dir', { 'default_value' => '/srv/aphlict' }),
+    Boolean $aphlict_ssl = lookup('phabricator_aphlict_enable_ssl', { 'default_value' => false }),
+    Optional[Stdlib::Unixpath]  $aphlict_cert  = lookup('phabricator_aphlict_cert', { 'default_value' => undef }),
+    Optional[Stdlib::Unixpath]  $aphlict_key   = lookup('phabricator_aphlict_key', { 'default_value' => undef }),
+    Optional[Stdlib::Unixpath]  $aphlict_chain = lookup('phabricator_aphlict_chain', { 'default_value' => undef }),
+    String $deploy_target = lookup('phabricator_deploy_target', { 'default_value' => 'phabricator/deployment'}),
+    Optional[String] $deploy_user   = lookup('phabricator_deploy_user', { 'default_value' => 'phab-deploy' }),
+    Boolean $manage_scap_user = lookup('profile::phabricator::main::manage_scap_user', { 'default_value' => true }),
 ) {
+
+    $deploy_root = "/srv/deployment/${deploy_target}"
 
     class { '::phabricator::aphlict':
         ensure     => present,
@@ -23,5 +25,23 @@ class profile::phabricator::aphlict (
         ensure => present,
         proto  => 'tcp',
         port   => '22280',
+    }
+
+    scap::target { $deploy_target:
+        deploy_user => $deploy_user,
+        key_name    => 'phabricator',
+        manage_user => $manage_scap_user,
+        require     => File['/usr/local/sbin/phab_deploy_finalize'],
+        sudo_rules  => [
+            'ALL=(root) NOPASSWD: /usr/local/sbin/phab_deploy_promote',
+            'ALL=(root) NOPASSWD: /usr/local/sbin/phab_deploy_rollback',
+            'ALL=(root) NOPASSWD: /usr/local/sbin/phab_deploy_finalize',
+        ],
+    }
+
+    file { $base_dir:
+        ensure  => 'link',
+        target  => $deploy_root,
+        require => Package[$deploy_target],
     }
 }
