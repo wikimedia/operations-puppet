1f0a77c6ca193666418637ee5069c7d3c33de02c
Andrew Bogott <abogott@wikimedia.org>
2020-07-24 10:52:13 -0500

    Make cloudcephosd1005.eqiad.wmnet a ceph node
    It definitely won't be able to talk to the existing
    OSDs, but I want to see how puppet manages the new vlans.
    
    Bug: T251619
    Change-Id: I26355b7d145b03a75913a9bff69d3179c1b1da8e

--- manifests/site.pp
+++ manifests/site.pp
@@ -1290,12 +1290,12 @@ node /^cloudcephmon100[1-3]\.wikimedia\.org$/ {
 }
 
 # new systems deployment in process T225320
-node /^cloudcephosd100[1-3]\.wikimedia\.org$/ {
+node /^cloudcephosd100[1-4]\.wikimedia\.org$/ {
     role(wmcs::ceph::osd)
 }
 
 # new servers  waiting for deployment  T251619
-node /^cloudcephosd10(0[4-9]|1[0-5])\.eqiad\.wmnet$/ {
+node /^cloudcephosd10(0[5-9]|1[0-5])\.eqiad\.wmnet$/ {
     role(insetup)
 }
 
