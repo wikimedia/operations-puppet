8f76032183803ea3c0d36ea7db682bd9cd525e80
John Bond <jbond@wikimedia.org>
2020-07-28 13:27:17 +0200

    profile::redis::master: drop legacy validate_ functions
    Hosts: P:redis::master
    
    Bug: T259013
    Change-Id: Ia93c655f251315c6982f5ff93df087d30430b35a

--- modules/profile/manifests/redis/master.pp
+++ modules/profile/manifests/redis/master.pp
@@ -1,11 +1,14 @@
 class profile::redis::master(
-    $instances = lookup('profile::redis::master::instances'),
-    $settings = lookup('profile::redis::master::settings'),
-    $instance_overrides = lookup('profile::redis::master::instance_overrides', {'default_value' => {}}),
-    $password = lookup('profile::redis::master::password'),
-    $aof = lookup('profile::redis::master::aof', {'default_value' => false}),
-    $clients = lookup('profile::redis::master::clients', {'default_value' => []}),
-    $prometheus_nodes = lookup('prometheus_nodes'),
+    Array[String]       $instances          = lookup('profile::redis::master::instances'),
+    Hash                $settings           = lookup('profile::redis::master::settings'),
+    Hash                $instance_overrides = lookup('profile::redis::master::instance_overrides',
+                                                    {'default_value' => {}}),
+    String              $password           = lookup('profile::redis::master::password'),
+    Boolean             $aof                = lookup('profile::redis::master::aof',
+                                                    {'default_value' => false}),
+    Array[String]       $clients            = lookup('profile::redis::master::clients',
+                                                    {'default_value' => []}),
+    Array[Stdlib::Host] $prometheus_nodes   = lookup('prometheus_nodes'),
 ){
     $uris = apply_format("localhost:%s/${password}", $instances)
     $redis_ports = join($instances, ' ')
@@ -15,12 +18,9 @@ class profile::redis::master(
         'requirepass' => $password,
     }
 
-    validate_array($clients)
-
-    if $clients == [] {
-        $srange = undef
-    } else {
-        $srange = inline_template("@resolve((<%= @clients.join(' ') %>))")
+    $srange = $clients.empty? {
+        true    => undef,
+        default => inline_template("@resolve((<%= @clients.join(' ') %>))"),
     }
 
     $instances.each |String $instance| {
@@ -29,21 +29,21 @@ class profile::redis::master(
         } else {
             $override = {}
         }
-        ::profile::redis::instance { $instance:
+        profile::redis::instance { $instance:
             settings => merge($settings, $auth_settings, $override),
             aof      => $aof,
         }
     }
 
     # Add monitoring, using nrpe and not remote checks anymore
-    ::redis::monitoring::nrpe_instance { $instances: }
+    redis::monitoring::nrpe_instance { $instances: }
 
-    ::profile::prometheus::redis_exporter{ $instances:
+    profile::prometheus::redis_exporter{ $instances:
         password         => $password,
         prometheus_nodes => $prometheus_nodes,
     }
 
-    ::ferm::service { 'redis_master_role':
+    ferm::service { 'redis_master_role':
         proto   => 'tcp',
         notrack => true,
         port    => inline_template('(<%= @redis_ports %>)'),
