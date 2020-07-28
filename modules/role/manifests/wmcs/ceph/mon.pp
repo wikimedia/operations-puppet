4e6194fd726c41f4a480375a40b59bf19a603dde
Andrew Bogott <abogott@wikimedia.org>
2020-07-28 10:27:08 -0500

    Remove lvs from cloudcephmon nodes
    Followup to https://gerrit.wikimedia.org/r/c/operations/puppet/+/616818
    
    Bug: T258826
    Change-Id: Ieb65ff380a5204ef3817ad7d32ee02287e14de33

--- modules/role/manifests/wmcs/ceph/mon.pp
+++ modules/role/manifests/wmcs/ceph/mon.pp
@@ -2,6 +2,5 @@ class role::wmcs::ceph::mon {
     system::role { $name: description => 'Ceph Monitor / Manager server.' }
     include ::profile::standard
     include ::profile::base::firewall
-    include ::profile::lvs::realserver
     include ::profile::ceph::mon
 }
