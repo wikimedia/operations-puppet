5070e6b968cbe88dc7b0d3bcde0242e87713b899
John Bond <jbond@wikimedia.org>
2020-07-28 11:48:23 +0200

    sudo: create new command to create safe wildcard sudo commands
    sudo commands using a wildcard in the path expantion are suceptible to
    wild card expantion e.g. with the following:
      'ALL = NOPASSWD: /bin/rmdir -p /srv/*'
    A user is able run the following
      rmdir /srv/../
      rmdir /srv/foo /
    
    This command takes the command and path argument and returns a command
    spec which is a bit more resistent to this misconfiguration.  taking the
    above example the function :
      '/bin/rmdir'.sudo::safe_wildcard_path('/srv/*')
    The following string is returned
      /bin/rmdir /tmp/*, !/bin/rmdir /tmp/* *, !/bin/rmdir /tmp/*..*
    
    Bug: T258943
    Change-Id: Ifec9c02df1c511797fd2d20e9683c579451dbc30

---
+++ modules/sudo/functions/safe_wildcard_cmd.pp
@@ -0,0 +1,15 @@
+# @summary this function takes a command and a path containing a wildcard and
+#         returns a sudo command spec with additional negations to avoid path traversal
+# @param $cmd the sudo command to run
+# @param $wildpath a unixpath containing a wildcard to expand
+# @return an sudo safe command spec of the form
+#         "${cmd} ${wildpath}, !${cmd} ${wildpath} *, !${cmd} ${wildpath}..*
+function sudo::safe_wildcard_cmd(Stdlib::Unixpath $cmd, Stdlib::Unixpath $wildpath) {
+    if $wildpath !~ /\*$/ {
+        "${cmd} ${wildpath}"
+    } else {
+        [' *', '..*'].reduce("${cmd} ${wildpath}") |$memo, $value| {
+            "${memo}, !${cmd} ${wildpath}${value}"
+        }
+    }
+}
---
+++ modules/sudo/spec/functions/sudo_safe_wildcard_cmd_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../../../../rake_modules/spec_helper'
+
+describe 'sudo::safe_wildcard_cmd' do
+  it 'return command as normal if no wild card' do
+    is_expected.to run.with_params('/bin/mkdir', '/tmp').and_return('/bin/mkdir /tmp')
+  end
+  it 'expand to a safe path' do
+    is_expected.to run.with_params('/bin/mkdir', '/tmp*').and_return(
+      '/bin/mkdir /tmp*, !/bin/mkdir /tmp* *, !/bin/mkdir /tmp*..*'
+    )
+  end
+end
