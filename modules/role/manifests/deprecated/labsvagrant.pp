05a479f22c3b191ba237fedf0d458b94ff611c57
Bryan Davis <bd808@wikimedia.org>
2020-07-28 11:41:12 -0600

    Remove ::role::deprecated::labsvagrant
    Remove this role which was superseded by ::role::labs::mediawiki_vagrant
    in late 2015. There are no Cloud VPS instances still using it.
    
    Bug: T258943
    Change-Id: I4eec9fec843e337344110f35ebcfda2fc75be0ae

--- modules/role/manifests/deprecated/labsvagrant.pp
+++
@@ -1,5 +0,0 @@
-# DEPRECATED, DO NOT USE!
-# Install MediaWiki-Vagrant puppet repo, manage manually from CLI
-class role::deprecated::labsvagrant {
-    class { '::labs_vagrant':}
-}
--- modules/role/manifests/labs/mediawiki_vagrant.pp
+++ modules/role/manifests/labs/mediawiki_vagrant.pp
@@ -3,17 +3,10 @@
 # Install Linux Containers (LXC) and Vagrant along with helper programs and
 # provision a MediaWiki-Vagrant clone for shared use.
 #
-# Conflicts with role::labs_vagrant.
-#
 # filtertags: labs-common
 class role::labs::mediawiki_vagrant {
     include ::profile::wmcs::mediawiki_vagrant
 
     # Ensure that secondary disks are mounted first if they are being used.
     Labs_lvm::Volume <| |> -> Class['role::labs::mediawiki_vagrant']
-
-    # Ensure that role::labs_vagrant is not applied
-    if defined(Class['role::deprecated::labsvagrant']) {
-        fail('role::labs::mediawiki_vagrant and role::deprecated::labsvagrant  conflict.')
-    }
 }
