44cce2a57a633f2f71cab7c714385ef9e0c23046
John Bond <jbond@wikimedia.org>
2020-07-29 13:40:38 +0200

    role::logstash7: refactor role to conform to coding guide
    Hosts: O:logstash7
    Bug: T259013
    Change-Id: I9669695889debd65f0df23fa39c77048b9d54abd

--- modules/role/manifests/logstash7.pp
+++ modules/role/manifests/logstash7.pp
@@ -3,10 +3,14 @@ class role::logstash7 {
         description => 'ELK7 Logstash/Kibana host',
     }
 
-    include ::role::logstash::collector7
-    include ::profile::kibana
-    include ::profile::kibana::httpd_proxy
-    include ::profile::prometheus::logstash_exporter
-    include ::profile::tlsproxy::envoy # TLS termination
-    include ::profile::lvs::realserver
+    include profile::standard
+    include profile::base::firewall
+    include profile::logstash::collector7
+    include profile::elasticsearch::logstash
+    include profile::elasticsearch::monitor::base_checks
+    include profile::kibana
+    include profile::kibana::httpd_proxy
+    include profile::prometheus::logstash_exporter
+    include profile::tlsproxy::envoy # TLS termination
+    include profile::lvs::realserver
 }
