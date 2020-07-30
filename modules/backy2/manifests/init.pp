3794cd89b8d16448f4332604e63ab0eecda55136
Andrew Bogott <abogott@wikimedia.org>
2020-07-30 16:31:43 -0500

    Add Backy2 module and profile
    This setup assumes that backy2 will be used to back up
    ceph rbd devices; the profile must be installed alongside
    profile::ceph::client::rbd
    
    Bug: T259192
    Change-Id: Iffd179befa3858a05a5569fd2e9680a728169467

---
+++ modules/backy2/manifests/init.pp
@@ -0,0 +1,69 @@
+# Class backy2
+#
+# Install backy2 and configure for backing up ceph rbd volumes to local storage
+#
+# This class expects to find a ceph config in /etc/ceph/ceph.conf, which
+#  is typically provided by profile::ceph::client::rbd
+#
+# On an initial install, the database can be created with
+#
+# $ sudo backy2 initdb
+#
+# Parameters:
+#
+#  cluster_name: ceph cluster name
+#  rados_name: ceph client username
+#
+# Requires:
+#
+# Sample Usage
+#   include backy2
+class backy2(
+    String       $cluster_name,
+    Stdlib::Fqdn $db_host,
+    String       $db_name,
+    String       $db_user,
+    String       $db_pass,
+    String       $rados_name = 'client.admin',
+    ) {
+
+    # The upstream backy2 deb is available from
+    #    https://github.com/wamdam/backy2/releases
+    #
+    # It targets ubuntu but installs just fine on Debian Buster.
+    #
+    # The dependencies are a bit incomplete, so rather than take any
+    #  chances I'm enumerating them here.
+    $packages = [
+        'python3-alembic',
+        'python3-dateutil',
+        'python3-fusepy',
+        'python3-prettytable',
+        'python3-setproctitle',
+        'python3-shortuuid',
+        'python3-sqlalchemy',
+        'python3-lz4',
+        'python3-pycrypto',
+        'python3-pycryptodome',
+        'python3-alembic',
+    ]
+    package { $packages:
+        ensure => 'present',
+        before => Package['backy2'],
+    }
+    package { 'backy2':
+        ensure => 'present',
+    }
+
+    file {
+        '/srv/backy2/data':
+            ensure    => 'directory';
+        '/etc/backy.cfg':
+            content   => template('backy2/backy.cfg.erb'),
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0440',
+            show_diff => false,
+            require   => Package['backy2'];
+    }
+}
---
+++ modules/profile/manifests/wmcs/backy2.pp
@@ -0,0 +1,15 @@
+class profile::wmcs::backy2(
+    String       $cluster_name = lookup('profile::wmcs::backy2::cluster_name'),
+    Stdlib::Fqdn $db_host      = lookup('profile::wmcs::backy2::db_host'),
+    String       $db_name      = lookup('profile::wmcs::backy2::db_name'),
+    String       $db_user      = lookup('profile::wmcs::backy2::db_user'),
+    String       $db_pass      = lookup('profile::wmcs::backy2::db_pass'),
+) {
+    class {'::backy2':
+        cluster_name => $cluster_name,
+        db_host      => $db_host,
+        db_name      => $db_host,
+        db_user      => $db_host,
+        db_pass      => $db_host,
+    }
+}
