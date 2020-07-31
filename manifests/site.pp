aad7a29dffbb6022372766aa7ed6a17a907bd441
Andrew Bogott <abogott@wikimedia.org>
2020-07-31 20:03:23 +0000

    Revert "Move cloudvirt1031 from virt_ceph to virt"
    this taught me nothing!
    
    This reverts commit 763ad9f737822de33c3136a20da3a4fbd6b90d46.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I11a7b23e061129bdbf013b671fc1df74f8ff15a1

--- manifests/site.pp
+++ manifests/site.pp
@@ -2315,10 +2315,7 @@ node 'cloudvirt1030.eqiad.wmnet' {
 node /^cloudvirt100[4,6]\.eqiad\.wmnet$/ {
     role(wmcs::openstack::eqiad1::virt_ceph)
 }
-node 'cloudvirt1031.eqiad.wmnet' {
-    role(wmcs::openstack::eqiad1::virt)
-}
-node 'cloudvirt1032.eqiad.wmnet' {
+node /^cloudvirt103[1-2]\.eqiad\.wmnet$/ {
     role(wmcs::openstack::eqiad1::virt_ceph)
 }
 node /^cloudvirt103[3-9]\.eqiad\.wmnet$/ {
