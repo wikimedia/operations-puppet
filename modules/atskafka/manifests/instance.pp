e3961f4675f67428a2ae8b45ed03519a8fbd821f
Emanuele Rocca <ema@wikimedia.org>
2020-07-31 13:30:02 +0200

    atskafka: librdkafka settings tuning
    Add the following settings to atskafka::instance:
    
    - buffering_max_messages
    - batch_num_messages
    - send_max_retries
    - send_buffer_bytes
    - request_required_acks
    - request_timeout_ms
    - message_timeout_ms
    
    Specify the same values currently passed to the equivalent varnishkafka
    settings in the webrequest profile.
    
    Hosts: cp3050.esams.wmnet
    
    Bug: T254317
    Change-Id: Ife88973dcb97f76e34a1edcd781cf4058ed66888

--- modules/atskafka/manifests/instance.pp
+++ modules/atskafka/manifests/instance.pp
@@ -8,18 +8,54 @@
 # [*brokers*]
 #   Array of Kafka broker host:ports.
 #
-# [*stats_interval_ms*]
-#   Flush rdkafka statistics every stats_interval_ms.
-#
-# [*buffering_ms*]
-#   How long to wait, in milliseconds, for messages in the producer queue to
-#   accumulate before constructing MessageSets to transmit to brokers. A higher
-#   value allows larger and more effective (less overhead, improved
-#   compression) batches of messages to accumulate at the expense of increased
-#   message delivery latency.
-#
 # [*topic*]
 #   Kafka topic to which all logs must be written.
+#
+# [*stats_interval_ms*]
+#   statistics.interval.ms in librdkafka: statistics emit interval in
+#   milliseconds. A value of 0 disables statistics. (Default: 0)
+#
+# [*buffering_ms*]
+#   queue.buffering.max.ms in librdkafka. How long to wait, in milliseconds,
+#   for messages in the producer queue to accumulate before constructing
+#   MessageSets to transmit to brokers. A higher value allows larger and more
+#   effective (less overhead, improved compression) batches of messages to
+#   accumulate at the expense of increased message delivery latency.
+#   (Default: 5)
+#
+# [*buffering_max_messages*]
+#   queue.buffering.max.messages in librdkafka. Maximum number of messages
+#   allowed on the producer queue. The queue is shared by all topics and
+#   partitions. (Default: 100000)
+#
+# [*batch_num_messages*]
+#   batch.num.messages in librdkafka. Maximum number of messages batched in one
+#   MessageSet. (Default: 10000)
+#
+# [*send_max_retries*]
+#   message.send.max.retries in librdkafka. How many times to retry sending a
+#   failing Message. (Default: 2)
+#
+# [*send_buffer_bytes*]
+#   socket.send.buffer.bytes in librdkafka. Broker socket send buffer size.
+#   System default is used if 0. (Default: 0)
+#
+# [*request_required_acks*]
+#   request.required.acks in librdkafka. The number of acknowledgements the
+#   leader broker must receive from ISR brokers before responding to the
+#   request: 0=Broker does not send any response/ack to client, -1=Broker will
+#   block until message is committed by all in sync replicas (ISRs). If there
+#   are less than min.insync.replicas (broker configuration) in the ISR set the
+#   produce request will fail. (Default: -1)
+#
+# [*request_timeout_ms*]
+#   request.timeout.ms in librdkafka. The ack timeout of the producer request
+#   in milliseconds. (Default: 5000)
+#
+# [*message_timeout_ms*]
+#   message.timeout.ms in librdkafka. The maximum time librdkafka may use to
+#   deliver a message (including retries). Delivery error occurs when either
+#   the retry count or the message timeout are exceeded. (Default: 300000)
 #
 # [*numeric_fields*]
 #   Array of fields to be considered as numeric. All fields not specified in
@@ -51,9 +87,16 @@
 #
 define atskafka::instance(
     Array[String] $brokers                            = ['localhost:9092'],
-    Integer $stats_interval_ms                        = 60000,
-    Integer $buffering_ms                             = 200,
     String $topic                                     = 'atskafka_test',
+    Integer $stats_interval_ms                        = 0,
+    Integer $buffering_ms                             = 5,
+    Integer $buffering_max_messages                   = 100000,
+    Integer $batch_num_messages                       = 10000,
+    Integer $send_max_retries                         = 2,
+    Integer $send_buffer_bytes                        = 0,
+    Integer $request_required_acks                    = -1,
+    Integer $request_timeout_ms                       = 5000,
+    Integer $message_timeout_ms                       = 300000,
     Array[String] $numeric_fields                     = ['time_firstbyte', 'response_size'],
     Stdlib::Absolutepath $socket                      = '/var/run/log.socket',
     Stdlib::Absolutepath $conf_file                   = "/etc/atskafka-${name}.conf",
