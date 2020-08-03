ad5c5836b352ebcfe6d2113c7ff190eec2686eac
Michael Große <mic.grosse@googlemail.com>
2020-08-03 16:56:09 +0200

    Create dispatch lag alerts for test.wikidata.org
    Having this alert for test.wikidata.org as well could have prevented a
    production incident where dispatch stopped entirely. See T258062
    
    This only adds the alert for extreme lag as the other one is overly
    sensitive and has its notifications already disabled for that reason.
    See 673194d and 56e665e.
    
    Bug: T258374
    Co-Authored-by: gtzatchkova <guergana.tzatchkova@wikimedia.de>
    Co-Authored-by: Tobias Andersson <tobias.andersson@wikimedia.de>
    Change-Id: Icc08c22ecf4e21118d2ae295ef4d96a7db71b0b8

--- modules/icinga/manifests/monitor/wikidata.pp
+++ modules/icinga/manifests/monitor/wikidata.pp
@@ -26,6 +26,20 @@ class icinga::monitor::wikidata {
         notes_url      => 'https://phabricator.wikimedia.org/project/view/71/',
     }
 
+    @monitoring::host { 'test.wikidata.org':
+        host_fqdn => 'test.wikidata.org',
+    }
+
+    monitoring::service { 'test.wikidata.org dispatch extreme lag':
+        description    => 'test.wikidata.org dispatch lag is REALLY high (>=4000s)',
+        check_command  => 'check_wikidata_crit',
+        host           => 'test.wikidata.org',
+        check_interval => 5,
+        retry_interval => 1,
+        contact_group  => 'admins,wikidata',
+        notes_url      => 'https://phabricator.wikimedia.org/project/view/71/',
+    }
+
     monitoring::grafana_alert { 'wikidata-alerts':
         dashboard_uid => 'TUJ0V-0Zk',
         contact_group => 'wikidata',
