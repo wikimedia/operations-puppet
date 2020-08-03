b3fcb9ef1ba3f71b2fc3d58863b6e370f0788e8b
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-08-03 08:59:34 +0200

    toolforge: Remove jessie conditionals
    AFAICT toolforge no longer uses jessie.
    
    Change-Id: Ibdb2c45ee2fa0f74d972bc422fd2bb4276f676a3

--- modules/profile/manifests/toolforge/apt_pinning.pp
+++ modules/profile/manifests/toolforge/apt_pinning.pp
@@ -7,12 +7,10 @@ class profile::toolforge::apt_pinning {
     # virtual meta-package, they usually have 3 levels of indirection:
     # linux-meta -> linux-meta-4.9 -> linux-image-4.9-xx
     $linux_meta_pkg = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'linux-meta*',
         'stretch' => 'linux-image-amd64',
         'buster'  => 'linux-image-amd64',
     }
     $linux_meta_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 1.16',
         'stretch' => 'version 4.9+80+deb9u3',
         'buster'  => 'version 4.19+105+deb10u1',
     }
@@ -24,12 +22,10 @@ class profile::toolforge::apt_pinning {
     # actual kernel package. Pinning only this is not enough, given that the meta-package
     # could be upgraded pointing to another version and then you would have a pending reboot
     $linux_pkg = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'linux-image-4.9.0-0.bpo.5-amd64',
         'stretch' => 'linux-image-4.9.0-5-amd64',
         'buster'  => 'linux-image-4.19.0-5-amd64',
     }
     $linux_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 4.9.65-3+deb9u1~bpo8+2',
         'stretch' => 'version 4.9.65-3+deb9u2',
         'buster'  => 'version 4.19.37-5+deb10u2',
     }
@@ -45,7 +41,6 @@ class profile::toolforge::apt_pinning {
     # dpkg -l | grep ^ii | egrep libnss\|ldap\|nscd\|nslcd\|pam | awk -F' ' '{print $3" "$2}' | sort -n
     # libpam-runtime libpam-modules* libpam0g
     $libpam_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 1.1.8-3.1+deb8u1*',
         'stretch' => 'version 1.1.8-3.6',
         'buster'  => 'version 1.3.1-5',
     }
@@ -56,7 +51,6 @@ class profile::toolforge::apt_pinning {
     }
     # libpam-ldapd nslcd* libnss-ldapd
     $libpam_ldapd_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 0.9.4-3+deb8u1',
         'stretch' => 'version 0.9.7-2',
         'buster'  => 'version 0.9.10-2',
     }
@@ -67,7 +61,6 @@ class profile::toolforge::apt_pinning {
     }
     # ldapvi
     $ldapvi_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 1.7-9',
         'stretch' => 'version 1.7-10*',
         'buster'  => 'version 1.7-10*',
     }
@@ -78,7 +71,6 @@ class profile::toolforge::apt_pinning {
     }
     # sudo-ldap
     $sudo_ldap_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 1.8.10p3-1+deb8u5',
         'stretch' => 'version 1.8.19p1-2.1',
         'buster'  => 'version 1.8.27-1',
     }
@@ -89,7 +81,6 @@ class profile::toolforge::apt_pinning {
     }
     # nscd
     $nscd_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 2.19-18+deb8u10',
         'stretch' => 'version 2.24-11+deb9u1',
         'buster'  => 'version 2.28-10',
     }
@@ -100,7 +91,6 @@ class profile::toolforge::apt_pinning {
     }
     # libnss-db
     $libnss_db_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 2.2.3pre1-5+b3',
         'stretch' => 'version 2.2.3pre1-6+b1',
         'buster'  => 'version 2.2.3pre1-6+b6',
     }
@@ -111,7 +101,6 @@ class profile::toolforge::apt_pinning {
     }
     # python-ldap
     $python_ldap_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 2.4.10-1',
         'stretch' => 'version 2.4.28-0.1',
         'buster'  => 'version 3.1.0-2',
     }
@@ -122,7 +111,6 @@ class profile::toolforge::apt_pinning {
     }
     # ldap-utils libldap*
     $ldap_utils_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 2.4.41+dfsg-1+wmf1',
         'stretch' => 'version 2.4.44+dfsg-5+deb9u1',
         'buster'  => 'version 2.4.47+dfsg-3+deb10u1',
     }
@@ -133,7 +121,6 @@ class profile::toolforge::apt_pinning {
     }
     # libnss3*
     $libnss3_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 2:3.26-1+debu8u3',
         'stretch' => 'version xxx',
         'buster'  => 'version 2:3.42.1-1+deb10u1',
     }
@@ -151,27 +138,6 @@ class profile::toolforge::apt_pinning {
         }
     }
 
-    #
-    # kubernetes stuff
-    #
-    # main k8s
-    if os_version('debian == jessie') {
-        apt::pin { 'toolforge-kubernetes-node-pinning':
-            package  => 'kubernetes-node',
-            pin      => 'version 1.4.6-6',
-            priority => '2000',
-        }
-        apt::pin { 'toolforge-kubernetes-master-pinning':
-            package  => 'kubernetes-master',
-            pin      => 'version 1.4.6-6',
-            priority => '2000',
-        }
-        apt::pin { 'toolforge-kubernetes-client-pinning':
-            package  => 'kubernetes-client',
-            pin      => 'version 1.4.6-3',
-            priority => '2000',
-        }
-    }
     # paws
     if os_version('debian == stretch') {
         apt::pin { 'toolforge-kubeadm-pinning':
@@ -197,43 +163,10 @@ class profile::toolforge::apt_pinning {
     }
 
     #
-    # nginx stuff
-    #
-    if os_version('debian == jessie') {
-        apt::pin { 'toolforge-libnginx-mod-pinning':
-            package  => 'libnginx-mod*',
-            pin      => 'version 1.13.6-2+wmf1~jessie1',
-            priority => '1001',
-        }
-    }
-    if os_version('debian == jessie') {
-        $nginx_pkg_version = $facts['lsbdistcodename'] ? {
-            'jessie'  => 'version 1.13.6-2+wmf1~jessie1',
-        }
-        apt::pin { 'toolforge-nginx-pinning':
-            package  => 'nginx-*',
-            pin      => $nginx_pkg_version,
-            priority => '1001',
-        }
-    }
-
-    #
-    # apache stuff
-    #
-    if os_version('debian == jessie') {
-        apt::pin { 'toolforge-apache2-pinning':
-            package  => 'apache2',
-            pin      => 'version 2.4.10-10+deb8u12+wmf1',
-            priority => '1001',
-        }
-    }
-
-    #
     # NFS libs and related packages
     #
     # nfs-common
     $nfs_common_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 1:1.2.8-9',
         'stretch' => 'version 1:1.3.4-2.1',
         'buster'  => 'version 1:1.3.4-2.5',
     }
@@ -244,7 +177,6 @@ class profile::toolforge::apt_pinning {
     }
     # libnfsidmap2
     $libnfsidmap2_pkg_version = $facts['lsbdistcodename'] ? {
-        'jessie'  => 'version 0.25-5',
         'stretch' => 'version 0.25-5.1',
         'buster'  => 'version 0.25-5.1',
     }
