f86e62de9fcc253467ec4dca8bbad606fb7fd0dd
Reuven Lazarus <rlazarus@wikimedia.org>
2020-08-05 12:32:54 -0400

    httpbb: Move test files into subdirectories by host type.
    Bug: T259665
    Change-Id: If2fcd50b4a323fb64937e6a327964f07831fa4ee

--- modules/profile/manifests/httpbb.pp
+++ modules/profile/manifests/httpbb.pp
@@ -2,29 +2,44 @@
 class profile::httpbb {
     class {'::httpbb':}
 
-    httpbb::test_suite {'baseurls.yaml':
-        source => 'puppet:///modules/profile/httpbb/baseurls.yaml'
+    httpbb::test_suite {'appserver/baseurls.yaml':
+        source => 'puppet:///modules/profile/httpbb/appserver/baseurls.yaml'
     }
-    httpbb::test_suite {'test_foundation.yaml':
-        source => 'puppet:///modules/profile/httpbb/test_foundation.yaml'
+    httpbb::test_suite {'appserver/test_foundation.yaml':
+        source => 'puppet:///modules/profile/httpbb/appserver/test_foundation.yaml'
     }
-    httpbb::test_suite {'test_main.yaml':
-        source => 'puppet:///modules/profile/httpbb/test_main.yaml'
+    httpbb::test_suite {'appserver/test_main.yaml':
+        source => 'puppet:///modules/profile/httpbb/appserver/test_main.yaml'
     }
-    httpbb::test_suite {'test_miscweb.yaml':
-        source => 'puppet:///modules/profile/httpbb/test_miscweb.yaml'
+    httpbb::test_suite {'appserver/test_remnant.yaml':
+        source => 'puppet:///modules/profile/httpbb/appserver/test_remnant.yaml'
     }
-    httpbb::test_suite {'test_remnant.yaml':
-        source => 'puppet:///modules/profile/httpbb/test_remnant.yaml'
+    httpbb::test_suite {'appserver/test_search.yaml':
+        source => 'puppet:///modules/profile/httpbb/appserver/test_search.yaml'
     }
-    httpbb::test_suite {'test_search.yaml':
-        source => 'puppet:///modules/profile/httpbb/test_search.yaml'
+    httpbb::test_suite {'appserver/test_secure.yaml':
+        source => 'puppet:///modules/profile/httpbb/appserver/test_secure.yaml'
     }
-    httpbb::test_suite {'test_secure.yaml':
-        source => 'puppet:///modules/profile/httpbb/test_secure.yaml'
+    httpbb::test_suite {'appserver/test_wikimania_wikimedia.yaml':
+        source => 'puppet:///modules/profile/httpbb/appserver/test_wikimania_wikimedia.yaml'
     }
-    httpbb::test_suite {'test_wikimania_wikimedia.yaml':
-        source => 'puppet:///modules/profile/httpbb/test_wikimania_wikimedia.yaml'
+    httpbb::test_suite {'miscweb/test_miscweb.yaml':
+        source => 'puppet:///modules/profile/httpbb/miscweb/test_miscweb.yaml'
+    }
+
+    # Temporary: Moving test files into subdirectories. (T259665)
+    file {
+        [
+            '/srv/deployment/httpbb-tests/baseurls.yaml',
+            '/srv/deployment/httpbb-tests/test_foundation.yaml',
+            '/srv/deployment/httpbb-tests/test_main.yaml',
+            '/srv/deployment/httpbb-tests/test_remnant.yaml',
+            '/srv/deployment/httpbb-tests/test_search.yaml',
+            '/srv/deployment/httpbb-tests/test_secure.yaml',
+            '/srv/deployment/httpbb-tests/test_wikimania_wikimedia.yaml',
+            '/srv/deployment/httpbb-tests/test_miscweb.yaml',
+        ]:
+            ensure => 'absent'
     }
 
     systemd::timer::job { 'git_pull_httpbb':
