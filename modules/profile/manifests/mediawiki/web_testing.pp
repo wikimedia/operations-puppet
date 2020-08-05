8d694616423e4e291514aeb710927d3daebd0d8d
Reuven Lazarus <rlazarus@wikimedia.org>
2020-08-05 16:00:21 -0400

    web_testing: Remove the apache-fast-test placeholder
    Change-Id: Ia31841b4fc7de23ec40bb2a6a40111c5d76ac028

--- modules/profile/manifests/mediawiki/web_testing.pp
+++ modules/profile/manifests/mediawiki/web_testing.pp
@@ -1,10 +1,7 @@
 class profile::mediawiki::web_testing {
-    # Perl helper script for apache changes
+    # Formerly the Perl helper script for apache changes
     file  { '/usr/local/bin/apache-fast-test':
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
-        source => 'puppet:///modules/profile/mediawiki/web_testing/apache-fast-test',
+        ensure => absent,
     }
 
     # Formerly the predefined test files for apache-fast-test
