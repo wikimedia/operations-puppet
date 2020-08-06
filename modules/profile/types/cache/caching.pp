7bee088cbff257c449515255faa146a46dc074c8
Emanuele Rocca <ema@wikimedia.org>
2020-08-06 15:23:47 +0200

    cache: add type Profile::Cache::Sites
    Introduce a new type describing the format of cache::req_handling and
    cache::alternate_domains. Use the new type in both
    profile::cache::varnish::frontend and profile::trafficserver::backend.
    
    Hosts: cp3050.esams.wmnet, cp3051.esams.wmnet
    
    Change-Id: I8fbeb642e19e5b6aa435b2c0dc39db79e908f13b

---
+++ modules/profile/types/cache/caching.pp
@@ -0,0 +1,2 @@
+# Valid values for the 'caching' attribute of Profile::Cache::Sites
+type Profile::Cache::Caching = Enum['normal', 'pass', 'pipe', 'websockets']
---
+++ modules/profile/types/cache/sites.pp
@@ -0,0 +1,9 @@
+# Describes the websites served by Wikimedia's CDN. The hash key is the FQDN of
+# the site as sent in the Host request header.
+type Profile::Cache::Sites = Hash[String, Struct[{
+    'caching'   => Profile::Cache::Caching,
+    'regex_key' => Optional[Boolean],
+    'subpaths'  => Optional[Hash[String, Struct[{
+        'caching'   => Profile::Cache::Caching,
+    }]]],
+}]]
