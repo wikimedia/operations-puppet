1cca59c60166021902ff47f924a82d144e7f3297
Riccardo Coccioli <rcoccioli@wikimedia.org>
2020-08-06 16:56:48 +0200

    interface::alias: add optional is_service_ip param
    * So far the interface::alias has always set all addresses with a /32 or
      /128 netmask as if they were service IPs (like VIPs). In some cases
      though they are real addresses like for example in the cassandra usage
      of it.
    * Add an optional parameter is_service_ip that defaults to true that if
      set to false makes the code pick the actual netmask of the primary
      interface.
    * Add also types to all parameters.
    
    Change-Id: I5f5e09faca819c0586ec34e02d826b24b271d9d1

--- modules/interface/manifests/alias.pp
+++ modules/interface/manifests/alias.pp
@@ -2,23 +2,32 @@
 # Simplified version of interface::ip, only used for adding secondary IPs to hosts
 #
 define interface::alias(
-  $interface=$facts['interface_primary'],
-  $ipv4=undef,
-  $ipv6=undef,
+  String $interface=$facts['interface_primary'],
+  Optional[Stdlib::IP::Address::V4::Nosubnet] $ipv4=undef,
+  Optional[Stdlib::IP::Address::V6::Nosubnet] $ipv6=undef,
+  Boolean $is_service_ip=true,
 ) {
     if $ipv4 != undef {
+        $prefixlen_v4 = $is_service_ip ? {
+            true  => 32,
+            false => netmask_to_cidr($facts['networking']['interfaces'][$interface]['netmask']),
+        }
         interface::ip { "${title} ipv4":
             interface => $interface,
             address   => $ipv4,
-            prefixlen => 32,
+            prefixlen => $prefixlen_v4,
         }
     }
 
     if $ipv6 != undef {
+        $prefixlen_v6 = $is_service_ip ? {
+            true  => 128,
+            false => netmask_to_cidr($facts['networking']['interfaces'][$interface]['netmask6']),
+        }
         interface::ip { "${title} ipv6":
             interface => $interface,
             address   => $ipv6,
-            prefixlen => 128,
+            prefixlen => $prefixlen_v6,
             # mark as deprecated = never pick this address unless explicitly asked
             options   => 'preferred_lft 0',
         }
