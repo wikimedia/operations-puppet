0c5fa79ea0bffeac43afd5588b0ce51f25f44dcf
Andrew Bogott <abogott@wikimedia.org>
2020-08-07 11:42:02 -0500

    wmcs/backy2/ceph: add admin keyring so backy can access things
    Bug: T259192
    Change-Id: I8b013b8f3585687586022faaafbf601525b59a4c

--- modules/profile/manifests/wmcs/backy2.pp
+++ modules/profile/manifests/wmcs/backy2.pp
@@ -1,9 +1,12 @@
 class profile::wmcs::backy2(
-    String       $cluster_name = lookup('profile::wmcs::backy2::cluster_name'),
-    Stdlib::Fqdn $db_host      = lookup('profile::wmcs::backy2::db_host'),
-    String       $db_name      = lookup('profile::wmcs::backy2::db_name'),
-    String       $db_user      = lookup('profile::wmcs::backy2::db_user'),
-    String       $db_pass      = lookup('profile::wmcs::backy2::db_pass'),
+    String               $cluster_name  = lookup('profile::wmcs::backy2::cluster_name'),
+    Stdlib::Fqdn         $db_host       = lookup('profile::wmcs::backy2::db_host'),
+    String               $db_name       = lookup('profile::wmcs::backy2::db_name'),
+    String               $db_user       = lookup('profile::wmcs::backy2::db_user'),
+    String               $db_pass       = lookup('profile::wmcs::backy2::db_pass'),
+    Stdlib::Unixpath     $data_dir      = lookup('profile::ceph::data_dir'),
+    Stdlib::AbsolutePath $admin_keyring = lookup('profile::ceph::admin_keyring'),
+    String               $admin_keydata = lookup('profile::ceph::admin_keydata'),
 ) {
     class {'::backy2':
         cluster_name => $cluster_name,
@@ -11,5 +14,10 @@ class profile::wmcs::backy2(
         db_name      => $db_name,
         db_user      => $db_user,
         db_pass      => $db_pass,
+    }
+
+    ceph::keyring { 'client.admin':
+        keydata => $admin_keydata,
+        keyring => $admin_keyring,
     }
 }
