2c7532776ae5835a3b9a60e43ce6856bd3ca4550
Stephen Shirley <sshirley@wikimedia.org>
2020-08-10 14:44:54 +0200

    mariadb: Give better name to sustained replication lag alert
    Also remove obsolete 'dc' dashboard variable.
    
    Change-Id: Ica8c34e42132007a9162ad38903ef6fcf3b9a887

--- modules/profile/manifests/mariadb/replication_lag.pp
+++ modules/profile/manifests/mariadb/replication_lag.pp
@@ -8,8 +8,8 @@ define profile::mariadb::replication_lag(
     # Don't monitor replication lag for 'standalone' hosts, or section masters in the primary DC
     if ($role == 'master' and !$is_on_primary_dc) or $role == 'slave' {
         monitoring::check_prometheus { "mariadb-prolonged-lag-${title}":
-            description     => '5-minute average replication lag is over 2s',
-            dashboard_links => ["https://grafana.wikimedia.org/d/000000273/mysql?orgId=1&var-server=${::hostname}&var-port=${prom_port}&var-dc=${::site} prometheus/ops"],
+            description     => 'MariaDB sustained replica lag',
+            dashboard_links => ["https://grafana.wikimedia.org/d/000000273/mysql?orgId=1&var-server=${::hostname}&var-port=${prom_port}"],
             query           => "scalar(avg_over_time(mysql_slave_status_seconds_behind_master{instance=\"${::hostname}:${prom_port}\"}[5m]))",
             prometheus_url  => "http://prometheus.svc.${::site}.wmnet/ops",
             warning         => 1,
