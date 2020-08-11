94f4a6e764dd511d29e048c52b9a72f404b0af94
Daniel Zahn <dzahn@wikimedia.org>
2020-08-11 15:28:15 -0700

    mailman: replace fermium with lists1001 in rsync scripts
    fermium has been replaced with lists1001
    
    Bug: T224586
    Change-Id: I41ec49282f7e07869bf77293fe4032b6a8721e27

--- modules/mailman/files/scripts/rsync_exim.sh
+++ modules/mailman/files/scripts/rsync_exim.sh
@@ -2,7 +2,7 @@
 # rsync exim queue - T110138
 
 SPOOL_DIR="/var/spool/exim4"
-RSYNC_TARGET="fermium.wikimedia.org/exim"
+RSYNC_TARGET="lists1001.wikimedia.org/exim"
 
 /usr/bin/rsync -avp ${SPOOL_DIR}/ \
 --exclude="db" \
--- modules/mailman/files/scripts/rsync_lists.sh
+++ modules/mailman/files/scripts/rsync_lists.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # rsync lists - T108071, T109399#1561586
 
-/usr/bin/rsync -avz --delete /var/lib/mailman/lists/ rsync://fermium.wikimedia.org/lists
+/usr/bin/rsync -avz --delete /var/lib/mailman/lists/ rsync://lists1001.wikimedia.org/lists
 
-/usr/bin/rsync -avz --delete /var/lib/mailman/archives/ rsync://fermium.wikimedia.org/archives
+/usr/bin/rsync -avz --delete /var/lib/mailman/archives/ rsync://lists1001.wikimedia.org/archives
 
-/usr/bin/rsync -avz --delete /var/lib/mailman/qfiles/ rsync://fermium.wikimedia.org/qfiles
+/usr/bin/rsync -avz --delete /var/lib/mailman/qfiles/ rsync://lists1001.wikimedia.org/qfiles
 
 /usr/bin/rsync -avz --delete /var/lib/mailman/data/ \
 --include="heldmsg-*" \
 --exclude="*.pw" \
 --exclude="bounce-*" \
 --exclude="sitelist.cfg" \
-rsync://fermium.wikimedia.org/data
+rsync://lists1001.wikimedia.org/data
 
