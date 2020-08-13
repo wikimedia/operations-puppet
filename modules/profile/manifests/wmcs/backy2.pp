58e1b29fcf684f7524f2fb7f22d31a8e75c06bba
Andrew Bogott <abogott@wikimedia.org>
2020-08-13 13:59:00 -0500

    wmcs/ceph/backy: add timer to run a backup job and a cleanup job once per day
    Bug: T259192
    Change-Id: Ib97e81d575ad67162952fcc18a52eca259a94e13

--- modules/profile/manifests/wmcs/backy2.pp
+++ modules/profile/manifests/wmcs/backy2.pp
@@ -48,4 +48,30 @@ class profile::wmcs::backy2(
         mode   => '0755',
         source => 'puppet:///modules/profile/wmcs/backy2/wmcs-backup-instances.py';
     }
+
+    systemd::timer::job { 'backup_vms':
+        ensure                    => present,
+        description               => 'snapshot and backup vms in specified projects',
+        command                   => '/usr/local/sbin/wmcs-backup-instances',
+        interval                  => {
+        'start'    => 'OnCalendar',
+        'interval' => '*-*-* 2:00:00', # daily at 02:00
+        },
+        logging_enabled           => true,
+        monitoring_enabled        => true,
+        monitoring_contact_groups => 'wmcs-email',
+    }
+
+    systemd::timer::job { 'purge_vm_backup':
+        ensure                    => present,
+        description               => 'purge old VM backups; allow backy2 to decide what is too old',
+        command                   => 'for version in `/usr/bin/backy2 -ms ls -e -f uid`; do /usr/bin/backy2 rm $version; done && /usr/bin/backy2 cleanup',
+        interval                  => {
+        'start'    => 'OnCalendar',
+        'interval' => '*-*-* 00:05:00', # daily at five past midnight
+        },
+        logging_enabled           => true,
+        monitoring_enabled        => true,
+        monitoring_contact_groups => 'wmcs-email',
+    }
 }
