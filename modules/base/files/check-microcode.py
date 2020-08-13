6af7490e666abb9851fd687b4fcbcbfd701b7b65
Daniel Zahn <dzahn@wikimedia.org>
2020-08-13 15:36:38 -0700

    base: remove tungsten from check-microcode.py
    Bug: T260395
    Change-Id: Ibc7ba55eb96a6ff85651e3b60a6f3071bae266e8

--- modules/base/files/check-microcode.py
+++ modules/base/files/check-microcode.py
@@ -52,7 +52,7 @@ def main():
 
     # CPUs which were not fixed for SSBD (which was the first) are also not
     # fixed for L1TF/MDS
-    blacklist_mds = ['helium', 'tungsten']
+    blacklist_mds = ['helium']
 
     if apt_pkg.version_compare(current_kernelpackage_version, '4.9.107-1') > 0:
         expected_cpu_flags.add('ssbd')
