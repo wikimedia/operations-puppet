a9138326e668b42562e430969d881aa361210f87
Andrew Bogott <abogott@wikimedia.org>
2020-08-13 11:29:04 -0500

    role::wmcs::ceph::backup: include observerenv
    Change-Id: I76aca600ea3da9d71638a56775c20ea0d96fa8e7

--- modules/role/manifests/wmcs/ceph/backup.pp
+++ modules/role/manifests/wmcs/ceph/backup.pp
@@ -4,6 +4,7 @@ class role::wmcs::ceph::backup {
     include ::profile::base::firewall
 
     include ::profile::openstack::eqiad1::clientpackages
+    include ::profile::openstack::eqiad1::observerenv
     include profile::wmcs::backy2
     include profile::ceph::client::rbd
 }
