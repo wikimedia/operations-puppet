a5a3438371d8503497085299192af4d71c615855
Daniel Zahn <dzahn@wikimedia.org>
2020-08-13 11:52:02 -0700

    releases: rsync needs to be on all servers incl the primary
    The rsync::quickdatacopy code needs to be applied on ALL
    releases servers, not only all secondary servers.
    
    Inside it there is code to compare source and dest server
    name against FQDN and change behavior based on that.
    
    We need it to setup the rsyncd source config to be able to pull
    from the primary server.
    
    Bug: T247652
    Change-Id: I576032a16912603fcee5e031fed4073a3be91a5c

--- modules/profile/manifests/releases/common.pp
+++ modules/profile/manifests/releases/common.pp
@@ -2,7 +2,7 @@
 class profile::releases::common(
     Stdlib::Fqdn $sitename = lookup('profile::releases::mediawiki::sitename'),
     Stdlib::Host $deployment_server = lookup('deployment_server'),
-    Stdlib::Fqdn $active_server = lookup('releases_server'),
+    Stdlib::Fqdn $primary_server = lookup('releases_server'),
     Array[Stdlib::Fqdn] $secondary_servers = lookup('releases_servers_failover'),
     String $server_admin = lookup('profile::releases::mediawiki::server_admin'),
 ){
@@ -21,25 +21,29 @@ class profile::releases::common(
 
     base::service_auto_restart { 'rsync': }
 
-    $secondary_servers.each |String $secondary_server| {
+    $all_secondary_servers = join($secondary_servers, ' ')
+    $all_releases_servers = "${primary_server} ${all_secondary_servers}"
+    $all_releases_servers_array = split($all_releases_servers, ' ')
+
+    $all_releases_servers_array.each |String $releases_server| {
         # automatically sync relases files to all secondary
         # servers and ensure they are real mirrors of each other
-        rsync::quickdatacopy { "srv-org-wikimedia-releases-${secondary_server}":
+        rsync::quickdatacopy { "srv-org-wikimedia-releases-${releases_server}":
           ensure      => present,
           auto_sync   => true,
           delete      => true,
-          source_host => $active_server,
-          dest_host   => $secondary_server,
+          source_host => $primary_server,
+          dest_host   => $releases_server,
           module_path => '/srv/org/wikimedia/releases',
         }
         # allow syncing jenkins data between servers for migrations
         # but do not automatically do it
-        rsync::quickdatacopy { "var-lib-jenkins-${secondary_server}":
+        rsync::quickdatacopy { "var-lib-jenkins-${releases_server}":
           ensure      => present,
           auto_sync   => false,
           delete      => true,
-          source_host => $active_server,
-          dest_host   => $secondary_server,
+          source_host => $primary_server,
+          dest_host   => $releases_server,
           module_path => '/var/lib/jenkins',
         }
     }
