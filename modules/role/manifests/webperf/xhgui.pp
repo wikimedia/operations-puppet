27d37a19224b07bf240cca19d58468158a5fbce4
Daniel Zahn <dzahn@wikimedia.org>
2020-08-13 15:35:09 -0700

    delete role::xhgui::app
    Bug: T260395
    Bug: T158837
    Change-Id: Ifdaff1758a0e4202909f868cfdbd1869b8663a81

--- modules/role/manifests/webperf/xhgui.pp
+++ modules/role/manifests/webperf/xhgui.pp
@@ -5,9 +5,8 @@
 # role::webperf::processors_and_site includes a reverse proxy server which
 # exposes this web server as https://performance.wikimedia.org/xhgui.
 #
-# This replaces role::xhgui::app, which will be removed as soon as the new,
-# MariaDB-backed version of XHGui is confirmed to be equivalent to the old,
-# MongoDB-backed version.
+# This is MySQL-based and replaced the previous role::xhgui::app
+# which was the old MongoDB-backed version.
 #
 class role::webperf::xhgui {
     include ::profile::standard
