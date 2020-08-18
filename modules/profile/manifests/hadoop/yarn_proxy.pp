8c08562d73f53b2e4bee5bc4f7740dd50cd0756d
John Bond <jbond@wikimedia.org>
2020-08-18 16:33:22 +0200

    yarn: move ui to new mod_auth_cas define
    Hosts: O:analytics_cluster::hadoop::yarn
    
    Bug: T260677
    Change-Id: Iae324a1789de5f85203b22d056fe6829b70a038d

--- modules/profile/manifests/hadoop/yarn_proxy.pp
+++ modules/profile/manifests/hadoop/yarn_proxy.pp
@@ -5,8 +5,8 @@
 class profile::hadoop::yarn_proxy (
 ) {
     require profile::hadoop::httpd
-    require ::profile::analytics::httpd::utils
-    require ::profile::hadoop::common
+    require profile::analytics::httpd::utils
+    require profile::hadoop::common
 
     $server_name = $::realm ? {
         'production' => 'yarn.wikimedia.org',
@@ -15,7 +15,14 @@ class profile::hadoop::yarn_proxy (
 
     $resourcemanager_primary_host = $profile::hadoop::common::resourcemanager_hosts[0]
 
-    class {'profile::idp::client::httpd_legacy':
-        vhost_settings => { 'res_manager' => $resourcemanager_primary_host },
+    profile::idp::client::httpd::site{ 'yarn.wikimedia.org':
+        vhost_content    => 'profile/idp/client/httpd-yarn.erb',
+        proxied_as_https => true,
+        vhost_settings   => { 'res_manager' => $resourcemanager_primary_host },
+        required_groups  => [
+            'cn=ops,ou=groups,dc=wikimedia,dc=org',
+            'cn=wmf,ou=groups,dc=wikimedia,dc=org',
+            'cn=nda,ou=groups,dc=wikimedia,dc=org',
+        ],
     }
 }
