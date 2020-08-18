7d6064f5005d146826cb257a363d15c5a71fd920
Jaime Crespo <jcrespo@wikimedia.org>
2020-08-18 11:33:28 +0200

    mariadb: core::multiinstance - generate sections.
    By having the list of instances in a hash, we can loop over it and
    setup the different properties of the different instances.
    
    Bug: T257033
    Change-Id: I5666baf2cd7004fc3ac96e3b9e1259ff232b9b1d

--- modules/profile/manifests/mariadb/core/multiinstance.pp
+++ modules/profile/manifests/mariadb/core/multiinstance.pp
@@ -1,15 +1,6 @@
-# TODO: use a data structure for the shards
 class profile::mariadb::core::multiinstance(
-    $num_instances = hiera('profile::mariadb::core::multiinstance::num_instances', 2),
-    $s1 = hiera('profile::mariadb::core::multiinstance::s1', false),
-    $s2 = hiera('profile::mariadb::core::multiinstance::s2', false),
-    $s3 = hiera('profile::mariadb::core::multiinstance::s3', false),
-    $s4 = hiera('profile::mariadb::core::multiinstance::s4', false),
-    $s5 = hiera('profile::mariadb::core::multiinstance::s5', false),
-    $s6 = hiera('profile::mariadb::core::multiinstance::s6', false),
-    $s7 = hiera('profile::mariadb::core::multiinstance::s7', false),
-    $s8 = hiera('profile::mariadb::core::multiinstance::s8', false),
-    $x1 = hiera('profile::mariadb::core::multiinstance::x1', false),
+    Hash[String, Pattern[/^\d+[KMG]?$/]] $instances = lookup('profile::mariadb::core::multiinstance::instances'),
+    Hash[String, Stdlib::Port] $section_ports = lookup('profile::mariadb::section_ports'),
 ) {
     require profile::mariadb::packages_wmf
     class { 'mariadb::service':
@@ -40,113 +31,21 @@ disabled, use mariadb@<instance_name> instead'; exit 1\"",
         mode   => '0755',
     }
 
-
-    if $s1 {
-        mariadb::instance { 's1':
-            port                    => 3311,
-            innodb_buffer_pool_size => $s1,
+    $instances.each |$section, $buffer_pool| {
+        $port = $section_ports[$section]
+        if (!$port) {
+            fail("'${section}' is not a valid section.")
+        }
+        $prom_port = Integer("1${port}")
+        mariadb::instance { $section:
+            port                    => $port,
+            innodb_buffer_pool_size => $buffer_pool,
             is_critical             => $is_critical,
         }
-        profile::mariadb::section { 's1': }
-        profile::mariadb::ferm { 's1': port => 3311, }
-        profile::prometheus::mysqld_exporter_instance {'s1': port => 13311, }
-        profile::mariadb::replication_lag { 's1': prom_port => 13311, }
-    }
-
-    if $s2 {
-        mariadb::instance { 's2':
-            port                    => 3312,
-            innodb_buffer_pool_size => $s2,
-            is_critical             => $is_critical,
-        }
-        profile::mariadb::section { 's2': }
-        profile::mariadb::ferm { 's2': port => 3312, }
-        profile::prometheus::mysqld_exporter_instance { 's2': port => 13312, }
-        profile::mariadb::replication_lag { 's2': prom_port => 13312, }
-    }
-
-    if $s3 {
-        mariadb::instance { 's3':
-            port                    => 3313,
-            innodb_buffer_pool_size => $s3,
-            is_critical             => $is_critical,
-        }
-        profile::mariadb::section { 's3': }
-        profile::mariadb::ferm { 's3': port => 3313, }
-        profile::prometheus::mysqld_exporter_instance {'s3': port => 13313, }
-        profile::mariadb::replication_lag { 's3': prom_port => 13313, }
-    }
-
-    if $s4 {
-        mariadb::instance { 's4':
-            port                    => 3314,
-            innodb_buffer_pool_size => $s4,
-            is_critical             => $is_critical,
-        }
-        profile::mariadb::section { 's4': }
-        profile::mariadb::ferm { 's4': port => 3314, }
-        profile::prometheus::mysqld_exporter_instance { 's4': port => 13314, }
-        profile::mariadb::replication_lag { 's4': prom_port => 13314, }
-    }
-
-    if $s5 {
-        mariadb::instance { 's5':
-            port                    => 3315,
-            innodb_buffer_pool_size => $s5,
-            is_critical             => $is_critical,
-        }
-        profile::mariadb::section { 's5': }
-        profile::mariadb::ferm { 's5': port => 3315, }
-        profile::prometheus::mysqld_exporter_instance { 's5': port => 13315, }
-        profile::mariadb::replication_lag { 's5': prom_port => 13315, }
-    }
-
-    if $s6 {
-        mariadb::instance { 's6':
-            port                    => 3316,
-            innodb_buffer_pool_size => $s6,
-            is_critical             => $is_critical,
-        }
-        profile::mariadb::section { 's6': }
-        profile::mariadb::ferm { 's6': port => 3316, }
-        profile::prometheus::mysqld_exporter_instance { 's6': port => 13316, }
-        profile::mariadb::replication_lag { 's6': prom_port => 13316, }
-    }
-
-    if $s7 {
-        mariadb::instance { 's7':
-            port                    => 3317,
-            innodb_buffer_pool_size => $s7,
-            is_critical             => $is_critical,
-        }
-        profile::mariadb::section { 's7': }
-        profile::mariadb::ferm { 's7': port => 3317, }
-        profile::prometheus::mysqld_exporter_instance { 's7': port => 13317, }
-        profile::mariadb::replication_lag { 's7': prom_port => 13317, }
-    }
-
-    if $s8 {
-        mariadb::instance { 's8':
-            port                    => 3318,
-            innodb_buffer_pool_size => $s8,
-            is_critical             => $is_critical,
-        }
-        profile::mariadb::section { 's8': }
-        profile::mariadb::ferm { 's8': port => 3318, }
-        profile::prometheus::mysqld_exporter_instance { 's8': port => 13318, }
-        profile::mariadb::replication_lag { 's8': prom_port => 13318, }
-    }
-
-    if $x1 {
-        mariadb::instance { 'x1':
-            port                    => 3320,
-            innodb_buffer_pool_size => $x1,
-            is_critical             => $is_critical,
-        }
-        profile::mariadb::section { 'x1': }
-        profile::mariadb::ferm { 'x1': port => 3320, }
-        profile::prometheus::mysqld_exporter_instance { 'x1': port => 13320, }
-        profile::mariadb::replication_lag { 'x1': prom_port => 13320, }
+        profile::mariadb::section { $section: }
+        profile::mariadb::ferm { $section: port => $port }
+        profile::prometheus::mysqld_exporter_instance { $section: port => $prom_port }
+        profile::mariadb::replication_lag { $section: prom_port => $prom_port }
     }
 
     class { 'mariadb::monitor_disk':
@@ -155,7 +54,7 @@ disabled, use mariadb@<instance_name> instead'; exit 1\"",
     }
 
     class { 'mariadb::monitor_process':
-        process_count => $num_instances,
+        process_count => length($instances),
         is_critical   => $is_critical,
         contact_group => $contact_group,
     }
