f2f2e968a7285cb933f1f11b687c8ffc6a46d217
Andrew Bogott <abogott@wikimedia.org>
2020-08-18 11:19:02 -0500

    wmcs/ceph/backy: move backup engine to cloudstore1009
    Bug: T260692
    Change-Id: Id2ec45efcd74d4a32632b076474b10ca91bedd5d

--- modules/role/manifests/wmcs/nfs/secondary.pp
+++ modules/role/manifests/wmcs/nfs/secondary.pp
@@ -1,6 +1,6 @@
 class role::wmcs::nfs::secondary {
     system::role { $name:
-        description => 'NFS secondary share cluster',
+        description => 'NFS secondary share cluster & ceph VM backup store',
     }
 
     include ::profile::standard
@@ -10,4 +10,11 @@ class role::wmcs::nfs::secondary {
     include ::profile::wmcs::nfs::rsync
     include ::profile::ldap::client::labs
     include ::profile::wmcs::nfs::secondary
+
+
+    # For ceph backups:
+    include ::profile::openstack::eqiad1::clientpackages
+    include ::profile::openstack::eqiad1::observerenv
+    include profile::wmcs::backy2
+    include profile::ceph::client::rbd
 }
