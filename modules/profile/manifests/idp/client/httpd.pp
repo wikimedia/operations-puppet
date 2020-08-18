d19b57d527e1034e0e9cbeabee63d42a4a40f66e
John Bond <jbond@wikimedia.org>
2020-08-18 14:33:41 +0200

    profile::idp::client::httpd: create new httpd class
    This new class supports multiple vhosts
    
    Hosts: P:idp::client::httpd
    
    Bug: T260677
    Change-Id: I977202ff332996199827f3dcb6561967a58c8a54

---
+++ modules/profile/manifests/idp/client/httpd.pp
@@ -0,0 +1,31 @@
+# @summary configure mod_auth_cas authentication
+# @param cookie_path The location where cas stores information relating to authentication cookies issued
+# @param certificate_path the SSL certificate path used for validation
+# @param apereo_cas hash holding the login and validation
+# @param apache_owner The user apache runs as
+# @param apache_group The group apache runs as
+# @param sites A hash of sites to be used with profile::httpd::client::idp::site
+class profile::idp::client::httpd (
+    Apereo_cas::Urls $apereo_cas       = lookup('apereo_cas', Apereo_cas::Urls, 'deep'),
+    Stdlib::Unixpath $certificate_path = lookup('profile::idp::client::httpd::certificate_path'),
+    Stdlib::Unixpath $cookie_path      = lookup('profile::idp::client::httpd::cookie_path'),
+    String[1]        $apache_owner     = lookup('profile::idp::client::httpd::apache_owner'),
+    String[1]        $apache_group     = lookup('profile::idp::client::httpd::apache_group'),
+    Hash             $sites            = lookup('profile::idp::client::httpd::sites')
+) {
+    ensure_packages(['libapache2-mod-auth-cas'])
+
+    httpd::mod_conf{'auth_cas':}
+    file{$cookie_path:
+        ensure => directory,
+        owner  => $apache_owner,
+        group  => $apache_group,
+    }
+    $sites.each |Stdlib::Host $vhost, Hash $config| {
+        profile::idp::client::httpd::site {$vhost:
+            * => $config,
+        }
+    }
+}
+
+
---
+++ modules/profile/manifests/idp/client/httpd/site.pp
@@ -0,0 +1,96 @@
+# @summary private define used to configure an apache vhost using mod_auth_cas
+# @param vhost_content an erb template file to use for the apache vhost configuration
+# @param server_aliases an array of additional server aliases
+# @param document_root the document root to configure for the apache vhost
+# @param cookie_path The location where cas stores information relating to authentication cookies issued
+# @param certificate_path the SSL certificate path used for validation
+# @param apereo_cas hash holding the login and validation
+# @param authn_header The prefix to use when adding CAS or SAML attributes to the HTTP headers
+# @param debug Enable cas debug
+# @param priority the priority of the vhost site.  default: 99
+# @param validate_saml if true set CASValidateSAML On
+# @param enable_monitor if true an icinga check to make sure the site correctly redirects
+# @param protected_uri The protected URI endpoint which is validated if "enable_monitor" is set.  default: '/'
+# @param required_groups An array of LDAP groups allowed to access this resource
+# @param acme_chief_cert the name of the acme chief certificate to use
+# @param vhost_settings Allows to pass settings to the vhost config which are unrelated to the IDP setup
+# @param proxied_as_https if true set the proxied_as address to https://${vhost}/
+define profile::idp::client::httpd::site (
+    String[1]                     $vhost_content,
+    Stdlib::Unixpath              $document_root,
+    Array[Stdlib::Host]           $server_aliases   = [],
+    String[1]                     $authn_header     = 'CAS-User',
+    String[1]                     $attribute_prefix = 'X-CAS-',
+    Boolean                       $debug            = false,
+    Integer[1,99]                 $priority         = 50,
+    Boolean                       $validate_saml    = false,
+    Boolean                       $enable_monitor   = true,
+    String[1]                     $protected_uri    = '/',
+    String[1]                     $cookie_scope     = $protected_uri,
+    Boolean                       $proxied_as_https = false,
+    Enum['staging', 'production'] $environment      = 'production',
+    Optional[Hash[String,Any]]    $vhost_settings   = {},
+    Optional[Array[String[1]]]    $required_groups  = [],
+    Optional[String[1]]           $acme_chief_cert  = undef,
+) {
+    assert_private()
+    require profile::idp::client::httpd
+    $apereo_cas        = $profile::idp::client::httpd::apereo_cas
+    $apache_owner      = $profile::idp::client::httpd::apache_owner
+    $apache_group      = $profile::idp::client::httpd::apache_group
+    $certificate_path  = $profile::idp::client::httpd::certificate_path
+    $cookie_path       = "${profile::idp::client::httpd::cookie_path}/${title}/"
+    $ssl_settings      = ssl_ciphersuite('apache', 'strong', true)
+    $cas_base_settings = {
+        'CASVersion'         => 2,
+        'CASCertificatePath' => $certificate_path,
+        'CASCookiePath'      => $cookie_path,
+        'CASAttributePrefix' => $attribute_prefix,
+        'CASValidateSAML'    => $validate_saml ? { true => 'On', default => 'Off' },
+    }
+
+    $cas_base_auth = {
+        'AuthType'       => 'CAS',
+        'CASAuthNHeader' => $authn_header,
+        'CASScope'       => $cookie_scope,
+    }
+    $cas_auth_require = $required_groups.empty? {
+        true    => ['valid-user' ],
+        default => $required_groups.map |$group| { "cas-attribute memberOf:${group}" },
+    }
+    $cas_auth_settings = merge($cas_base_auth, {'Require' => $cas_auth_require})
+    file{$cookie_path:
+        ensure => directory,
+        owner  => $apache_owner,
+        group  => $apache_group,
+    }
+
+    if $acme_chief_cert and !defined(Acme_chief::Cert[$acme_chief_cert]) {
+        acme_chief::cert { $acme_chief_cert:
+            puppet_svc => 'apache2',
+        }
+    }
+
+    $proxied_as = $proxied_as_https ? {
+        true    => "https://${title}",
+        default => undef,
+    }
+    $cas_settings = merge({
+        'CASLoginURL'        => $apereo_cas[$environment]['login_url'],
+        'CASValidateURL'     => $apereo_cas[$environment]['validate_url'],
+        'CASDebug'           => $debug,
+        'CASRootProxiedAs'   => $proxied_as,
+    }, $cas_base_settings)
+    httpd::site {$title:
+        content  => template($vhost_content),
+        priority => $priority,
+    }
+
+    if $enable_monitor {
+        monitoring::service {"https-${title}-unauthorized":
+            description   => "${title} requires authentication",
+            check_command => "check_https_sso_redirect!${title}!${protected_uri}",
+            notes_url     => 'https://wikitech.wikimedia.org/wiki/CAS-SSO/Administration',
+        }
+    }
+}
