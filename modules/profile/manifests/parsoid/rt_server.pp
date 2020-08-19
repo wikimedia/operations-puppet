c66b86f57d827c512cef0c38e0afe9e2ab147625
Daniel Zahn <dzahn@wikimedia.org>
2020-08-19 11:57:19 -0700

    parsoid/testreduce: add parameter to control parsoid-rt service
    We need to be able to easily stop parsoid-rt on the new instance
    until it is removed from the previous host scandium.
    
    Do the same that was recently done for the parsoid-vd service
    and add parameters and hiera keys for it.
    
    Bug: T257906
    Change-Id: I358cd3ec730f6f2bfb678635e2641c9fbd6d3e3e

--- modules/profile/manifests/parsoid/rt_server.pp
+++ modules/profile/manifests/parsoid/rt_server.pp
@@ -1,16 +1,19 @@
 # Parsoid RT testing services
 
 # This instantiates testreduce::server
-class profile::parsoid::rt_server {
+class profile::parsoid::rt_server (
+    Variant[Enum['running', 'stopped'], Boolean] $service_ensure = lookup('profile::parsoid::rt_server::service_ensure'),
+){
     include ::testreduce
     include ::passwords::testreduce::mysql
 
     testreduce::server { 'parsoid-rt':
-        instance_name => 'parsoid-rt',
-        db_host       => 'm5-master.eqiad.wmnet',
-        db_name       => 'testreduce',
-        db_user       => 'testreduce',
-        db_pass       => $passwords::testreduce::mysql::db_pass,
+        instance_name  => 'parsoid-rt',
+        db_host        => 'm5-master.eqiad.wmnet',
+        db_name        => 'testreduce',
+        db_user        => 'testreduce',
+        db_pass        => $passwords::testreduce::mysql::db_pass,
+        service_ensure => $service_ensure,
     }
 
     base::service_auto_restart { 'parsoid-rt': }
