072d02040c6b78d54262f7d327ba2fe94ce0e5df
Andrew Bogott <abogott@wikimedia.org>
2020-08-25 15:54:59 -0500

    cloudvirts: add ceph config to non-ceph-enabled cloudvirts
    This will allow us to copy things off of old-school cloudvirts directly
    into ceph without converting the whole host.
    
    Bug: T261252
    Change-Id: I97cb2b9ee65017bc0df5e25df739086641831a82

--- modules/role/manifests/wmcs/openstack/eqiad1/virt.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/virt.pp
@@ -4,10 +4,10 @@ class role::wmcs::openstack::eqiad1::virt {
     # include ::profile::base::firewall
     # NOTE: ceph is not enabled in this role. Starting in Queens
     #       ceph-common is a dependency for the nova-common package
-    include ::profile::ceph::common
     include ::profile::openstack::eqiad1::clientpackages
     include ::profile::openstack::eqiad1::observerenv
     include ::profile::openstack::eqiad1::nova::common
     include ::profile::openstack::eqiad1::nova::compute::service
     include ::profile::openstack::eqiad1::envscripts
+    include ::profile::ceph::client::rbd
 }
