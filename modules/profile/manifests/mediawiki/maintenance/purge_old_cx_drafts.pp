988edd7f51293719b49da7a08c01eb87009b311e
Kartik Mistry <kartik.mistry@gmail.com>
2020-08-26 14:03:07 +0530

    Add --notify-age-in-days option to notify users before draft purge
    Bug: T261189
    Change-Id: If1c1409c9e07dc423c4ee976f137f4fe9faac376

--- modules/profile/manifests/mediawiki/maintenance/purge_old_cx_drafts.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_old_cx_drafts.pp
@@ -1,8 +1,9 @@
 # This cron will purge unpublished articles from ContentTranslation older than
-# 455 days (--age-in-days). See: T183890.
+# 455 days (--age-in-days) and notify users 30 days prior (--notify-age-in-days)
+# See: T183890, T261189.
 class profile::mediawiki::maintenance::purge_old_cx_drafts {
     profile::mediawiki::periodic_job { 'purge_old_cx_drafts':
-        command  => '/usr/local/bin/foreachwikiindblist "/srv/mediawiki/dblists/wikipedia.dblist - /srv/mediawiki/dblists/special.dblist - /srv/mediawiki/dblists/closed.dblist" extensions/ContentTranslation/scripts/purge-unpublished-drafts.php --age-in-days=455 --really',
+        command  => '/usr/local/bin/foreachwikiindblist "/srv/mediawiki/dblists/wikipedia.dblist - /srv/mediawiki/dblists/special.dblist - /srv/mediawiki/dblists/closed.dblist" extensions/ContentTranslation/scripts/purge-unpublished-drafts.php --age-in-days=455 --notify-age-in-days=425 --really',
         interval => '*-3,18 10:30'
     }
 }
