284848cd6ea05c67e5889b1755c4b9661301b12b
John Bond <jbond@wikimedia.org>
2020-08-31 14:19:03 +0200

    spec_helper: fix hiera definition in shared spec helper
    Change-Id: I8f45d4e57d856be14d03860ecd64d7d0162ad7b6

--- modules/standard/manifests/init.pp
+++ modules/standard/manifests/init.pp
@@ -5,28 +5,28 @@ class standard(
     $has_default_mail_relay = true,
     Array[String] $monitoring_hosts = [],
     ) {
-    include ::profile::base
-    include ::standard::ntp
+    include profile::base
+    include standard::ntp
 
     if $::realm == 'production' {
         # Include this first so we create all the required groups before
         # something else creates a system group with one of our GID's
         # e.g. ::profile::debmonitor::client
-        contain ::admin
-        include ::profile::cumin::target
-        include ::profile::debmonitor::client  # lint:ignore:wmf_styleguide
+        contain admin
+        include profile::cumin::target
+        include profile::debmonitor::client  # lint:ignore:wmf_styleguide
     }
 
-    unless $::fqdn in $::ntp_peers[$::site] {
-        include ::standard::ntp::timesyncd
+    unless $facts['fqdn'] in $::ntp_peers[$::site] {
+        include standard::ntp::timesyncd
     }
 
-    include ::standard::prometheus
+    include standard::prometheus
 
     # Some instances have their own exim definition that
     # will conflict with this
     if $has_default_mail_relay {
-        include ::standard::mail::sender
+        include standard::mail::sender
     }
 
     # For historical reasons, users in modules/admin/data/data.yaml
