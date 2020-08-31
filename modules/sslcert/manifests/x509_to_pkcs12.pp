30c848f99a2a1d24e29c390e1c7c30c79891db78
John Bond <jbond@wikimedia.org>
2020-08-31 15:01:41 +0200

    sslcert::x509_to_pkcs12: add define for creating p12 files
    Bug: T253957
    Change-Id: I6fb1d50ea503ecec83535b681254bc56dea92613

---
+++ modules/sslcert/manifests/x509_to_pkcs12.pp
@@ -0,0 +1,30 @@
+# @summary create pkcs12 file from a x509 public/private key pair
+# @param public_key the location of the public key
+# @param private_key the location of the private key
+# @param outfile location to store the pkcs12 file
+# @param certfile a certificate bundle to add to the exported file
+define sslcert::x509_to_pkcs12 (
+    Stdlib::Unixpath            $public_key  = "/etc/ssl/localcerts/${title}.crt",
+    Stdlib::Unixpath            $private_key = "/etc/ssl/private/${title}.key",
+    Stdlib::Unixpath            $outfile     = "/etc/ssl/localcerts/${title}.p12",
+    String                      $password    = '',
+    Optional[Stdlib::Unixpath]  $certfile    = undef,
+) {
+    ensure_packages(['openssl'])
+    $_certfile = $certfile ? {
+        undef   => '',
+        default => "-certfile ${certfile}",
+    }
+    $command = @("COMMAND"/L)
+        /usr/bin/openssl pkcs12 -export ${_certfile} \
+        -in ${public_key} \
+        -inkey ${private_key} \
+        -out ${outfile} \
+        -password 'pass:${password}'
+        | COMMAND
+    exec {"sslcert generate ${title}.p12":
+        command => $command,
+        creates => $outfile,
+        require => Package['openssl'],
+    }
+}
