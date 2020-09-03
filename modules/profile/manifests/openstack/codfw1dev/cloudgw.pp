cd3baec17c874d67a58ddec6ee7c9a7eb4640602
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-09-03 11:43:43 +0200

    cloudgw: fix hiera variable name
    Typo.
    
    Change-Id: Icb3e7b88b12865bc8127d525c26e7b95288ca0a3
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/openstack/codfw1dev/cloudgw.pp
+++ modules/profile/manifests/openstack/codfw1dev/cloudgw.pp
@@ -1,7 +1,7 @@
 class profile::openstack::codfw1dev::cloudgw (
     Array[String]                 $dmz_cidr       = lookup('profile::openstack::codfw1dev::cloudgw::dmz_cidr',         {default_value => ['0.0.0.0/0 . 0.0.0.0/0']}),
     Stdlib::IP::Address           $routing_source = lookup('profile::openstack::codfw1dev::cloudgw::routing_source_ip',{default_value => '185.15.57.1'}),
-    Stdlib::IP::Address::V4::CIDR $vir_subnet     = lookup('profile::openstack::codfw1dev::cloudgw::virt_subnet_cidr', {default_value => '172.16.128.0/24'}),
+    Stdlib::IP::Address::V4::CIDR $virt_subnet    = lookup('profile::openstack::codfw1dev::cloudgw::virt_subnet_cidr', {default_value => '172.16.128.0/24'}),
     String                        $nic_host       = lookup('profile::openstack::codfw1dev::cloudgw::nic_host',         {default_value => 'bond0.2118'}),
     String                        $nic_virt       = lookup('profile::openstack::codfw1dev::cloudgw::nic_virt',         {default_value => 'bond0.2120'}),
     String                        $nic_wan        = lookup('profile::openstack::codfw1dev::cloudgw::nic_wan',          {default_value => 'bond0.21xx'}),
