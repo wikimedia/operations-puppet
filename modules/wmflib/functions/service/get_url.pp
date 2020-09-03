e736a9be0d285c69207c411fe68b95ba98c96a27
Ottomata <aotto@wikimedia.org>
2020-09-03 10:50:04 -0400

    Add $site parameter to wmflib::service:get_url
    For monitoring purposes, we want to be able to address a service
    in a specific site.
    
    Analytics will use this to produce canary events into streams to
    be able to differentiate between topics that have no data and broken
    production pipelines.
    
    Bug: T251609
    Change-Id: I1f1f4ca7d0274e0224cdd027d6223e4ab26814e0

--- modules/wmflib/functions/service/get_url.pp
+++ modules/wmflib/functions/service/get_url.pp
@@ -1,11 +1,30 @@
 # This function returns the url for a service, supporting the use of the services proxy too.
 # Parameters:
-# [*svc_name*] The service name, as found in the service::catalog hiera variable or in the listeners list
-# [*url*] The url path relative to the server name. Defaults to the empty string
-# [*listeners*] An optional array of service proxy listeners. If provided, the url will be
-#     pointed to the service proxy instead than directly to the load balancer.
-
-function wmflib::service::get_url(String $svc_name, String $url = '', Optional[Array[Hash]] $listeners = undef) >> String {
+#
+# [*svc_name*]
+#   The service name, as found in the service::catalog hiera variable or in the listeners list
+#
+# [*url*]
+#   The url path relative to the server name. Defaults to the empty string
+#
+# [*listeners*]
+#   An optional array of service proxy listeners. If provided, the url will be
+#   pointed to the service proxy instead than directly to the load balancer.
+#
+# [*site*]
+#   If set to 'discovery', the discovery service URL will be returned,
+#   e.g. $svc_name.discovery.wmnet. Else, a site (datacenter) specific url will
+#   be returned, e.g. $svc_name.svc.$site.wmnet.  The site specific URL is
+#   obatined from the service catalog entry's monitoring configuration.
+#   This parameter is ignored if $listeners is set.
+#   Default: 'discovery'.
+#
+function wmflib::service::get_url(
+    String $svc_name,
+    String $url = '',
+    Optional[Array[Hash]] $listeners = undef,
+    String $site = 'discovery',
+) >> String {
     # We are using the service directly, no proxy
     if $listeners == undef {
         # service::catalog should contain $svc_name.
@@ -13,11 +32,28 @@ function wmflib::service::get_url(String $svc_name, String $url = '', Optional[A
         if $service == undef {
             fail("Service ${svc_name} not found in the catalog.")
         }
-        if $service['discovery'] == undef {
-            fail("Service ${svc_name} doesn't have a discovery record!")
+
+        if $site == 'discovery' {
+            if $service['discovery'] == undef {
+                fail("Service ${svc_name} doesn't have a discovery record!")
+            }
+            $dns_label = $service['discovery'][0]['dnsdisc']
+            $host = "${dns_label}.discovery.wmnet"
+        } else {
+            if !($site in $service['sites']) {
+                fail("Service ${svc_name} is not present in site ${site}.")
+            }
+            if (
+                $service['monitoring'] == undef or
+                $service['monitoring']['sites'] == undef or
+                $service['monitoring']['sites'][$site] == undef or
+                $service['monitoring']['sites'][$site]['hostname'] == undef
+            ) {
+                fail("Service ${svc_name} does not have monitoring for site ${site} configured; cannot determine site specific hostname.")
+            }
+            $host = $service['monitoring']['sites'][$site]['hostname']
         }
-        $dns_label = $service['discovery'][0]['dnsdisc']
-        $host = "${dns_label}.discovery.wmnet"
+
         $port = $service['port']
         $scheme = $service['encryption'] ? {
             true    => 'https',
