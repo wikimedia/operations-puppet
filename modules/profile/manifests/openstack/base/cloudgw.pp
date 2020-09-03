a2aa6f22956598a0ee81c241be97c390594c338b
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2020-09-03 12:05:39 +0200

    cloudgw: use newer nftables and kernel
    For using concatenated ranges (like we use for the dmz_cidr rule) we need a newer nftables and
    linux kernel version, which both are in buster-backports.
    
    While at it, don't use keywords in the basefirewall policy, so potential users won't need the
    bpo version.
    
    Bug: T261724
    Change-Id: I9602b687b987913d51067131fe069961c98af76e
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/openstack/base/cloudgw.pp
+++ modules/profile/manifests/openstack/base/cloudgw.pp
@@ -1,5 +1,19 @@
 class profile::openstack::base::cloudgw (
 ) {
+    # need nft >= 0.9.6 and kernel >= 5.6 to use some of the concatenated rules
+    apt::pin { 'nft-from-buster-bpo':
+        package  => 'nftables libnftables1 libnftnl11 linux-image-amd64',
+        pin      => 'release n=buster-backports',
+        priority => 1001,
+        before   => Class['::nftables'],
+        notify   => Exec['apt-get-update'],
+    }
+
+    exec { 'apt-get-update':
+        command     => '/usr/bin/apt-get update',
+        refreshonly => true,
+    }
+
     class { '::nftables':
         ensure_service => 'present',
     }
