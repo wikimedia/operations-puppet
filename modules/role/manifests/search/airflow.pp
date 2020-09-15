78b1bd5a391111463281df38ed2f5e25e845d4c2
Luca Toscano <ltoscano@wikimedia.org>
2020-09-15 14:13:47 +0200

    role::search::airflow: add profile::analytics::cluster::gitconfig
    The profile deploys settings to allow git to pull from gerrin within
    the Analytics VLAN.
    
    Change-Id: Ie097ea210ac2cf0ce52306e227858d4371703d25

--- modules/role/manifests/search/airflow.pp
+++ modules/role/manifests/search/airflow.pp
@@ -11,6 +11,7 @@ class role::search::airflow {
     include ::profile::java
     include ::profile::analytics::cluster::users
     include ::profile::analytics::cluster::client
+    include ::profile::analytics::cluster::gitconfig
     include ::profile::analytics::cluster::repositories::statistics
     include ::profile::analytics::refinery
     include ::profile::analytics::search::airflow
