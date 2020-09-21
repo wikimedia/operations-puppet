53bf7125c820f529f93a31ae0c31c5ba0e29ba73
Kartik Mistry <kartik.mistry@gmail.com>
2020-09-21 14:30:31 +0530

    Exclude testwikis and private wikis from CX draft purge script run
    Bug: T263417
    Change-Id: I76b70bb5dd7251399770a5a756b46966d49dad99

--- modules/profile/manifests/mediawiki/maintenance/purge_old_cx_drafts.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_old_cx_drafts.pp
@@ -3,7 +3,7 @@
 # See: T183890, T261189.
 class profile::mediawiki::maintenance::purge_old_cx_drafts {
     profile::mediawiki::periodic_job { 'purge_old_cx_drafts':
-        command  => '/usr/local/bin/foreachwikiindblist "/srv/mediawiki/dblists/wikipedia.dblist - /srv/mediawiki/dblists/special.dblist - /srv/mediawiki/dblists/closed.dblist" extensions/ContentTranslation/scripts/purge-unpublished-drafts.php --age-in-days=455 --notify-age-in-days=425 --really',
+        command  => '/usr/local/bin/foreachwikiindblist "/srv/mediawiki/dblists/wikipedia.dblist - /srv/mediawiki/dblists/special.dblist - /srv/mediawiki/dblists/closed.dblist - /srv/mediawiki/dblists/testwikis.dblist - /srv/mediawiki/dblists/private.dblist" extensions/ContentTranslation/scripts/purge-unpublished-drafts.php --age-in-days=455 --notify-age-in-days=425 --really',
         interval => '*-3,18 10:30'
     }
 }
