02a1bf9fa269538d9ea4756d1c4d055c52cefd43
Daniel Zahn <dzahn@wikimedia.org>
2020-09-21 17:27:33 -0700

    syslog::centralserver: convert role to profile, fix lint issues
    - a role class should not have hiera lookups
    - convert to profile and use it in the role
    - replace hiera() with lookup()
    - fix lint-ignored lint issues
    - add data types
    
    Change-Id: I2fc84621151aea3904c996493ae4ed4da51f00c6

---
+++ modules/profile/manifests/syslog/centralserver.pp
@@ -0,0 +1,62 @@
+# Setup rsyslog as a receiver of cluster wide syslog messages.
+#
+class profile::syslog::centralserver (
+    Array[Stdlib::Host] $prometheus_nodes = lookup ('prometheus_nodes', {'default_value' => []}),
+){
+
+    ferm::service { 'rsyslog-receiver_udp':
+        proto   => 'udp',
+        port    => 514,
+        notrack => true,
+        srange  => '($DOMAIN_NETWORKS $MGMT_NETWORKS)',
+    }
+
+    ferm::service { 'rsyslog-netdev_kafka_relay_udp':
+        proto   => 'udp',
+        port    => 10514,
+        notrack => true,
+        srange  => '($DOMAIN_NETWORKS $MGMT_NETWORKS $NETWORK_INFRA)',
+    }
+
+    ferm::service { 'rsyslog-receiver_tcp':
+        proto   => 'tcp',
+        port    => 6514,
+        notrack => true,
+        srange  => '($DOMAIN_NETWORKS $MGMT_NETWORKS)',
+    }
+
+    class { 'rsyslog::receiver': }
+
+    class { 'profile::rsyslog::netdev_kafka_relay': }
+
+    monitoring::service { "syslog::centralserver ${::hostname} syslog-tls":
+        description   => 'rsyslog TLS listener on port 6514',
+        check_command => "check_ssl_on_host_port!${::fqdn}!${::fqdn}!6514",
+        notes_url     => 'https://wikitech.wikimedia.org/wiki/Logs',
+    }
+
+    mtail::program { 'kernel':
+        ensure => present,
+        source => 'puppet:///modules/mtail/programs/kernel.mtail',
+        notify => Service['mtail'],
+    }
+
+    mtail::program { 'systemd':
+        ensure => present,
+        source => 'puppet:///modules/mtail/programs/systemd.mtail',
+        notify => Service['mtail'],
+    }
+
+    mtail::program { 'ulogd':
+        ensure => present,
+        source => 'puppet:///modules/mtail/programs/ulogd.mtail',
+        notify => Service['mtail'],
+    }
+
+    $prometheus_nodes_ferm = join($prometheus_nodes, ' ')
+    ferm::service { 'mtail':
+        proto  => 'tcp',
+        port   => '3903',
+        srange => "(@resolve((${prometheus_nodes_ferm})) @resolve((${prometheus_nodes_ferm}), AAAA))",
+    }
+}
--- modules/role/manifests/syslog/centralserver.pp
+++ modules/role/manifests/syslog/centralserver.pp
@@ -2,76 +2,18 @@
 #
 # Setup rsyslog as a receiver of cluster wide syslog messages.
 #
-class role::syslog::centralserver (
-    $prometheus_nodes = hiera('prometheus_nodes', []), # lint:ignore:wmf_styleguide
-) {
-    include ::profile::standard
-    include ::profile::base::firewall
-    include ::profile::backup::host
+class role::syslog::centralserver {
 
     system::role { 'syslog::centralserver':
         description => 'Central syslog server and web requests debugging'
     }
 
+    include ::profile::standard
+    include ::profile::base::firewall
+    include ::profile::backup::host
+    include ::profile::syslog::centralserver
     include ::profile::bird::anycast
-
     include ::profile::kafkatee::webrequest::ops
-
-    ferm::service { 'rsyslog-receiver_udp':
-        proto   => 'udp',
-        port    => 514,
-        notrack => true,
-        srange  => '($DOMAIN_NETWORKS $MGMT_NETWORKS)',
-    }
-
-    ferm::service { 'rsyslog-netdev_kafka_relay_udp':
-        proto   => 'udp',
-        port    => 10514,
-        notrack => true,
-        srange  => '($DOMAIN_NETWORKS $MGMT_NETWORKS $NETWORK_INFRA)',
-    }
-
-    ferm::service { 'rsyslog-receiver_tcp':
-        proto   => 'tcp',
-        port    => 6514,
-        notrack => true,
-        srange  => '($DOMAIN_NETWORKS $MGMT_NETWORKS)',
-    }
-
-    class { 'rsyslog::receiver': }
-
-    class { 'profile::rsyslog::netdev_kafka_relay': }
-
-    monitoring::service { "syslog::centralserver ${::hostname} syslog-tls":
-        description   => 'rsyslog TLS listener on port 6514',
-        check_command => "check_ssl_on_host_port!${::fqdn}!${::fqdn}!6514",
-        notes_url     => 'https://wikitech.wikimedia.org/wiki/Logs',
-    }
-
-    mtail::program { 'kernel':
-        ensure => present,
-        source => 'puppet:///modules/mtail/programs/kernel.mtail',
-        notify => Service['mtail'],
-    }
-
-    mtail::program { 'systemd':
-        ensure => present,
-        source => 'puppet:///modules/mtail/programs/systemd.mtail',
-        notify => Service['mtail'],
-    }
-
-    mtail::program { 'ulogd':
-        ensure => present,
-        source => 'puppet:///modules/mtail/programs/ulogd.mtail',
-        notify => Service['mtail'],
-    }
-
-    $prometheus_nodes_ferm = join($prometheus_nodes, ' ')
-    ferm::service { 'mtail':
-        proto  => 'tcp',
-        port   => '3903',
-        srange => "(@resolve((${prometheus_nodes_ferm})) @resolve((${prometheus_nodes_ferm}), AAAA))",
-    }
 
     # https://phabricator.wikimedia.org/T199406
     include ::toil::rsyslog_tls_remedy # lint:ignore:wmf_styleguide
