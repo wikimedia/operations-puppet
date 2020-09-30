b79e37b1f4314bbdfa9ea31683dc7bf5b1022b6f
John Bond <jbond@wikimedia.org>
2020-09-30 11:59:07 +0200

    profile::swift::stats_reporter: pass statsd host and port via hiera
    Hosts: P:swift::stats_reporter
    Change-Id: I660795fe79b617b5d96d5551845e3fc05fcadafa

--- modules/profile/manifests/swift/stats_reporter.pp
+++ modules/profile/manifests/swift/stats_reporter.pp
@@ -1,19 +1,26 @@
 # filtertags: labs-project-deployment-prep
 class profile::swift::stats_reporter (
-    Wmflib::Ensure $ensure = present, # lint:ignore:wmf_styleguide
-    String $swift_cluster = lookup('profile::swift::cluster'),
-    Hash[String, Hash] $accounts = lookup('profile::swift::accounts'),
-    Hash[String, String] $credentials = lookup('profile::swift::accounts_keys'),
-) {
-    class { '::swift::stats::dispersion':
+    Wmflib::Ensure         $ensure        = lookup('profile::swift::stats_reporter::ensure'),
+    String                 $swift_cluster = lookup('profile::swift::cluster'),
+    Hash[String, Hash]     $accounts      = lookup('profile::swift::accounts'),
+    Hash[String, String]   $credentials   = lookup('profile::swift::accounts_keys'),
+    Optional[Stdlib::Host] $statsd_host   = lookup('profile::swift::stats_reporter::statsd_host'),
+    Optional[Stdlib::Port] $statsd_port   = lookup('profile::swift::stats_reporter::statsd_port'),
+){
+
+    class { 'swift::stats::dispersion':
         ensure        => $ensure,
         swift_cluster => $swift_cluster,
+        statsd_host   => $statsd_host,
+        statsd_port   => $statsd_port,
     }
 
-    class { '::swift::stats::accounts':
+    class { 'swift::stats::accounts':
         ensure        => $ensure,
         swift_cluster => $swift_cluster,
         accounts      => $accounts,
         credentials   => $credentials,
+        statsd_host   => $statsd_host,
+        statsd_port   => $statsd_port,
     }
 }
