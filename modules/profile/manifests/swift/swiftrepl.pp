dfa730cb20de197a96d7ae78b4422c5448698cd3
John Bond <jbond@wikimedia.org>
2020-09-30 11:49:18 +0200

    swift::swiftrepl: convert role to profile
    Hosts: O:swift::swiftrepl
    Change-Id: Ie386b609fdf37dee428b896217b38c31a6a3457c

---
+++ modules/profile/manifests/swift/swiftrepl.pp
@@ -0,0 +1,22 @@
+class profile::swift::swiftrepl(
+    $ensure = lookup('profile::swift::swiftrepl::ensure', { 'default_value' => 'present' }),
+){
+
+    $source_site = $::site
+
+    case $source_site {
+        'eqiad': {
+            $destination_site = 'codfw'
+        }
+        'codfw': {
+            $destination_site = 'eqiad'
+        }
+        default: { fail("Unsupported source site ${::site}") }
+    }
+
+    class { '::swift::swiftrepl':
+        ensure           => $ensure,
+        destination_site => $destination_site,
+        source_site      => $source_site,
+    }
+}
--- modules/role/manifests/swift/swiftrepl.pp
+++ modules/role/manifests/swift/swiftrepl.pp
@@ -1,26 +1,8 @@
-# lint:ignore:wmf_styleguide
-# This should be converted to a profile
+class role::swift::swiftrepl {
 
-class role::swift::swiftrepl (
-  $ensure = lookup('role::swift::swiftrepl::ensure', { 'default_value' => 'present' }), # lint:ignore:wmf_styleguide
-) {
-    include ::profile::standard
-
-    $source_site = $::site
-    case $source_site {
-        'eqiad': {
-            $destination_site = 'codfw'
-        }
-        'codfw': {
-            $destination_site = 'eqiad'
-        }
-        default: { fail("Unsupported source site ${::site}") }
+    system::role { 'swift::swiftrepl':
+        description => 'swift replication',
     }
-
-    class { '::swift::swiftrepl':
-        ensure           => $ensure,
-        destination_site => $destination_site,
-        source_site      => $source_site,
-    }
+    include profile::standard
+    include profile::swift::swiftrepl
 }
-# lint:endignore
