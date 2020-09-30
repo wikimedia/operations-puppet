a3cce558cb3da961c1b47a36adc8ba8772a1e26b
Daniel Zahn <dzahn@wikimedia.org>
2020-09-30 14:17:02 -0700

    prometheus: convert mysqld_exporter from role to profile
    - convert role to profile and remove intermediate class
    - replace hiera() with lookup()
    - move lookup from body to parameters
    - add one data type
    
    Bug: T159412
    Change-Id: I039a87b9b9ccc9a98717610897aec52992e86020

--- modules/profile/manifests/mariadb/monitor/prometheus.pp
+++ modules/profile/manifests/mariadb/monitor/prometheus.pp
@@ -1,8 +1,20 @@
 class profile::mariadb::monitor::prometheus(
     $socket = '/run/mysqld/mysqld.sock',
-    ) {
+    Array[Stdlib::Host] $prometheus_nodes = lookup('prometheus_nodes'),
+){
 
-    class { 'role::prometheus::mysqld_exporter':
-        socket => $socket,
+    include passwords::prometheus
+
+    prometheus::mysqld_exporter { 'default':
+        client_password => $passwords::prometheus::db_pass,
+        client_socket   => $socket,
+    }
+
+    $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
+
+    ferm::service { 'prometheus-mysqld-exporter':
+        proto  => 'tcp',
+        port   => '9104',
+        srange => "@resolve((${prometheus_ferm_nodes}))",
     }
 }
