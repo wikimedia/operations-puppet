68a10bba01116aeeedf9a6cbb15d2aa8debbed7a
Daniel Zahn <dzahn@wikimedia.org>
2020-10-02 17:34:16 -0700

    openstack: turn bash scripts without bashisms into sh scripts
    The tool 'checkbashisms' identified these as scripts that have
    a /bin/bash shebang but don't actually use any bashisms so they
    can be sh scripts.
    
    Bug: T95064
    Change-Id: Icea498f99bdca1db44322506c3181cf8ca360354

--- modules/openstack/files/wikitech/mw-xml.sh
+++ modules/openstack/files/wikitech/mw-xml.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 cd /srv/mediawiki/php
 /usr/local/bin/mwscript maintenance/dumpBackup.php labswiki --current --uploads | sed 's/<model>yaml<\/model>/<model>wikitext<\/model>/g' | sed 's/<format>application\/yaml<\/format>/<format>text\/x-wiki<\/format>/g' | nice -n 19 gzip -9 > /srv/backup/public/labswiki-$(date '+%Y%m%d').xml.gz
