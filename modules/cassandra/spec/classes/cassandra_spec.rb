4b180e9d742e08e347c2a18e211b4ffc384eece7
John Bond <jbond@wikimedia.org>
2020-10-06 13:03:00 +0200

    casandra -rspec: update rspec jobs
    Change-Id: I9bb93921278cfa5a88510b98017ca79e1991c77a

--- modules/cassandra/spec/classes/cassandra_spec.rb
+++ modules/cassandra/spec/classes/cassandra_spec.rb
@@ -1,23 +1,17 @@
-require 'spec_helper'
+require_relative '../../../../rake_modules/spec_helper'
 
 describe 'cassandra', :type => :class do
     let(:pre_condition) { 'class { "::apt": }' }
+    on_supported_os(WMFConfig.test_on).each do |os, facts|
+        context "on #{os}" do
+          let(:params) {  {target_version: '2.2'} }
+          let(:facts) { facts }
 
-    let(:params) { {
-        :target_version => '2.2',
-    } }
+          # check that there are no dependency cycles
+          it { is_expected.to compile }
 
-    let(:facts) { {
-         :ipaddress => '10.0.0.1',
-         :initsystem => 'systemd',
-         :lsbdistrelease => '9.7',
-         :lsbdistid => 'Debian',
-         :operatingsystem => 'Debian',
-    } }
-
-    # check that there are no dependency cycles
-    it { is_expected.to compile }
-
-    it { is_expected.to contain_apt__repository('wikimedia-cassandra22').that_comes_before('Package[cassandra]') }
-    it { is_expected.to contain_exec('apt-get update').that_comes_before('Package[cassandra]') }
+          it { is_expected.to contain_apt__repository('wikimedia-cassandra22').that_comes_before('Package[cassandra]') }
+          it { is_expected.to contain_exec('apt-get update').that_comes_before('Package[cassandra]') }
+        end
+    end
 end
---
+++ modules/cassandra/spec/defines/instance_spec.rb
@@ -0,0 +1,37 @@
+require_relative '../../../../rake_modules/spec_helper'
+
+describe 'cassandra::instance' do
+    let(:title) {'foobar'}
+    let(:pre_condition) do
+      'include cassandra
+      include apt'
+    end
+
+    on_supported_os(WMFConfig.test_on).each do |os, facts|
+        context "on #{os}" do
+            let(:facts) { facts }
+            let(:params) do
+              {
+                cluster_name: 'Test Cluster',
+                memory_allocator: 'foobar',
+                listen_address: '192.0.2.1',
+                native_transport_port: 9043,
+                target_version: '2.1',
+                seeds: ['192.0.2.1'],
+                dc: 'datacenter1',
+                rack: 'rack1',
+                additional_jvm_opts: [],
+                extra_classpath: [],
+                logstash_host: 'localhost',
+                logstash_port: 11_514,
+                start_rpc: true,
+                super_username: 'cassandra',
+                super_password: 'cassandra',
+              }
+            end
+            describe 'test with default settings' do
+               it { is_expected.to compile.with_all_deps }
+            end
+        end
+    end
+end
