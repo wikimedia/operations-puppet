6fee44b7a14daec673dfd6698d0588b3fac161b9
Luca Toscano <ltoscano@wikimedia.org>
2020-10-12 08:30:52 +0200

    role::analytics_test_cluster::coordiantor: avoid db backups
    Bug: T255139
    Change-Id: If7c9dd7ccd3000ff38454ba02ef3845b1910410c

--- modules/role/manifests/analytics_test_cluster/coordinator.pp
+++ modules/role/manifests/analytics_test_cluster/coordinator.pp
@@ -33,10 +33,6 @@ class role::analytics_test_cluster::coordinator {
     # for interacting with HDFS.
     include ::profile::analytics::cluster::users
 
-    # Back up analytics-meta MySQL instance
-    # to an-master1002.
-    include ::profile::analytics::database::meta::backup
-
     # SQL-like queries to data stored in HDFS
     include ::profile::hive::metastore
     include ::profile::hive::server
