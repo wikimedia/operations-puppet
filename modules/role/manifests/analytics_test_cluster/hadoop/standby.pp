470b161df000cf857bb49234eb689e00d3c0a2e8
Luca Toscano <ltoscano@wikimedia.org>
2020-10-12 13:06:09 +0200

    Remove lvmbackups of Analytics meta from the Hadoop Standby master
    Historically the Analytics team have copied lvm backups of the Meta db instance
    to the Hadoop standby master as a way to preserve data. Since then a more
    efficient backup infrastructure has been deployed, so this one is not needed
    anymore. The space used by the lvm backups are also reducing the available space
    for namenode image backups (every image has grown in size over time).
    
    Bug: T257412
    Change-Id: I9f98b65b52879b52f5b45657e5dbe0b11fa1f1ce

--- modules/role/manifests/analytics_test_cluster/hadoop/standby.pp
+++ modules/role/manifests/analytics_test_cluster/hadoop/standby.pp
@@ -13,19 +13,8 @@ class role::analytics_test_cluster::hadoop::standby {
     include ::profile::hadoop::firewall::master
     include ::profile::analytics::cluster::users
     include ::profile::hadoop::backup::namenode
-
-    # an-master1002 is usually inactive, and it has a
-    # decent amount of disk space.  We use it to
-    # store some backups, including fsimage snapshots
-    # of Hadoop NameNode metadata, and of the
-    # analytics_cluster::database::meta (MySQL analytics-meta) instance.
-    # If you move these, make sure /srv/backup has
-    # enough space to store backups.
-    include ::profile::analytics::database::meta::backup_dest
-
     include ::profile::kerberos::client
     include ::profile::kerberos::keytabs
-
     include ::profile::base::firewall
     include ::profile::standard
 
