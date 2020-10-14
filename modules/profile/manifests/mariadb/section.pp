88f58f7cde79c5028670e77ae74df2850a86f7e7
Stevie Shirley <sshirley@wikimedia.org>
2020-10-14 11:28:00 +0200

    mariadb: Add db sections to motd.
    This helps compensate for system::role not containing the section(s).
    
    Change-Id: Ib4a97bd252b38434dbe92085fcfd9d80ad450e79

--- modules/profile/manifests/mariadb/section.pp
+++ modules/profile/manifests/mariadb/section.pp
@@ -1,4 +1,10 @@
 # Title should be the section name
 define profile::mariadb::section(
     Profile::Mariadb::Valid_section $section = $title,
-) {}
+) {
+    # Add the db section to the motd, directly after the system::role line.
+    motd::script { "db-section-${title}":
+        priority => 6,
+        content  => "#!/bin/sh\necho 'DB section ${title}'\n",
+    }
+}
