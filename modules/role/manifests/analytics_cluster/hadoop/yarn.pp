47e412e2a3285790d51b29d75dbc657ad8032b1a
Razzi Abuissa <rabuissa@wikimedia.org>
2020-10-15 10:48:43 -0700

    yarn: replace nginx with envoy for tls
    Bug: T240439
    Change-Id: I4b5ef809e128d3bf53835a0bcb8bd40a46224e06

--- modules/role/manifests/analytics_cluster/hadoop/yarn.pp
+++ modules/role/manifests/analytics_cluster/hadoop/yarn.pp
@@ -9,7 +9,6 @@ class role::analytics_cluster::hadoop::yarn {
 
     include ::profile::java
 
-    include ::profile::tlsproxy::service
     include ::profile::tlsproxy::envoy
 
     include ::profile::hadoop::yarn_proxy
