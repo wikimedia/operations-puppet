2fcee0afa9fe46e0933009d00f43b08a2db92862
Razzi <rabuissa@wikimedia.org>
2020-10-21 20:48:11 +0000

    Revert "geoip: move archive timer from stat1007 to an-launcher1002"
    This reverts commit ef93495a45a1ffd1126439d69372c9c6516b94de.
    
    Reason for revert: Permission denied error prevented script from running on new host
    
    Change-Id: I6b401f35c4053268e516841297f2225b276fa459

--- modules/geoip/manifests/data/archive.pp
+++ modules/geoip/manifests/data/archive.pp
@@ -1,27 +1,33 @@
 # == Class geoip::data::archive
 #
 #
-# Sets up a cron job that grabs the latest version of the MaxMind database
-# and pushes its contents to hdfs.
+# Sets up a cron job that grabs the latest version of the MaxMind database, puts it
+# in a timestamped directory in $archive_dir, and pushes its contents to hdfs.
+#
 class geoip::data::archive(
     Stdlib::Unixpath $maxmind_db_source_dir = '/usr/share/GeoIP',
     Stdlib::Unixpath $hdfs_archive_dir = '/wmf/data/archive/geoip',
+    Stdlib::Unixpath $archive_dir = "${maxmind_db_source_dir}/archive",
     Boolean $use_kerberos = false,
-    Wmflib::Ensure $ensure = 'present',
+    Boolean $enable_timer = false,
 ){
 
-    # This comment is here as a reminder to cleanup the local archive directory on stat1007
-    # at /usr/share/GeoIP/archive once T264152 is all done.
+    # Puppet assigns 755 permissions to files and dirs, so the script can be ran
+    # manually without sudo.
+    file { $archive_dir:
+        ensure  => directory,
+        owner   => 'analytics-privatedata',
+        group   => 'analytics-privatedata-users',
+        require => [
+            User['analytics-privatedata'],
+            Group['analytics-privatedata-users']
+        ],
+    }
 
     $archive_script = '/usr/local/bin/geoip_archive.sh'
 
-    $archive_script_ensure = $ensure ? {
-        'present' => file,
-        default   => absent,
-    }
-
     file { $archive_script:
-        ensure  => $archive_script_ensure,
+        ensure  => file,
         owner   => 'root',
         group   => 'wikidev',
         mode    => '0555',
@@ -31,11 +37,10 @@ class geoip::data::archive(
     $archive_command = "${archive_script} ${maxmind_db_source_dir} ${hdfs_archive_dir}"
 
     kerberos::systemd_timer { 'archive-maxmind-geoip-database':
-        ensure                    => $ensure,
         description               => 'Archives Maxmind GeoIP files',
         command                   => $archive_command,
         interval                  => 'Tue *-*-* 05:30:00',
-        user                      => 'analytics',
+        user                      => 'analytics-privatedata',
         monitoring_contact_groups => 'analytics',
         use_kerberos              => $use_kerberos,
         require                   => [File[$archive_script], User['analytics']],
