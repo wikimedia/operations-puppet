3cccd5de46000a4410bc2967ccdd370536507918
Daniel Zahn <dzahn@wikimedia.org>
2020-10-23 13:40:26 -0700

    gerrit: remove 'list_mediawiki_extensions' cron job
    Bug: T266024
    Change-Id: Ib5b27c204e1b29f6e3e9a4b618c998e1b621ebe9

--- modules/gerrit/manifests/crons.pp
+++ modules/gerrit/manifests/crons.pp
@@ -1,13 +1,6 @@
 # sets up cron jobs for Gerrit
 class gerrit::crons() {
     require gerrit::jetty
-    cron { 'list_mediawiki_extensions':
-    # Gerrit is missing a public list of projects.
-    # This hack list MediaWiki extensions repositories
-        command => "/bin/ls -1d ${gerrit::jetty::git_dir}/mediawiki/extensions/*.git | sed 's#.*/##' | sed 's/\\.git//' > /var/www/mediawiki-extensions.txt",
-        user    => 'root',
-        minute  => [0, 15, 30, 45],
-    }
 
     cron { 'clear_gerrit_logs':
         # Gerrit rotates their own logs, but doesn't clean them out
