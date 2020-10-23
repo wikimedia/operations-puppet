bdb7ea36b99da628c35f9e40c5a008f0b86e9200
Stevie Shirley <sshirley@wikimedia.org>
2020-10-23 14:21:15 +0200

    orchestrator: Add basic config
    This should allow orchestrator to start and connect to its backend. No
    discovery is included at this stage.
    
    Hosts: dborch1001.eqiad.wmnet
    Bug: T265990
    Change-Id: I1294ce615cce5992a7a46d8c11819d3721f5496c

--- modules/orchestrator/manifests/server.pp
+++ modules/orchestrator/manifests/server.pp
@@ -1,6 +1,38 @@
-class orchestrator::server {
+class orchestrator::server (
+    Stdlib::Host $db_backend_host,
+    String[1] $db_backend_password,
+    Stdlib::Port $db_backend_port = 3306,
+    String[1] $db_backend_username = 'orchestrator_srv',
+    String[1] $db_backend_database = 'orchestrator',
+) {
     apt::package_from_component { 'thirdparty-orchestrator-server':
         component => 'thirdparty/orchestrator',
         packages  => ['orchestrator', 'orchestrator-cli']
+    }
+
+    file { '/etc/orchestrator.conf.json':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('orchestrator/orchestrator.conf.json.erb'),
+        notify  => Service['orchestrator'],
+    }
+
+    file { '/etc/mysql/orchestrator_srv.cnf':
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0400',
+        content => template('orchestrator/orchestrator_srv.cnf.erb'),
+        notify  => Service['orchestrator'],
+    }
+
+    service { 'orchestrator':
+        ensure  => 'running',
+        enable  => true,
+        require => [
+            Package['orchestrator'],
+        ],
     }
 }
--- modules/profile/manifests/orchestrator/server.pp
+++ modules/profile/manifests/orchestrator/server.pp
@@ -1,4 +1,9 @@
-class profile::orchestrator::server {
-    class { 'orchestrator::server': }
+class profile::orchestrator::server (
+    String[1] $db_backend_password = lookup('profile::orchestrator::server::db_backend_password'),
+) {
+    class { 'orchestrator::server':
+        db_backend_host     => 'db2093.codfw.wmnet',
+        db_backend_password => $db_backend_password,
+    }
     class { 'orchestrator::client': }
 }
