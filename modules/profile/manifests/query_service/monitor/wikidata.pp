0ab53276df9e9e96c510965df7a594b831271d95
David Causse <dcausse@wikimedia.org>
2020-10-26 15:48:18 +0100

    [wdqs] add support for streaming updater lag metric
    The lag is now known by the updater process and is no longer relying
    on blazegraph to hold this state.
    
    Overall I think that this metric should disappear in favor of two
    separate metrics based on kafka consumer offsets:
    
    - alert on lag on all the streaming-updater-producer input topics (changeprop)
    - alert on lag on streaming-updater-consumer input topic
    
    Bug: T255399
    Change-Id: Iae7772203d01f5bb4ffa845376aa542c9a249e57

--- modules/profile/manifests/query_service/monitor/wikidata.pp
+++ modules/profile/manifests/query_service/monitor/wikidata.pp
@@ -4,6 +4,7 @@ class profile::query_service::monitor::wikidata (
     String $contact_groups = lookup('contactgroups', {'default_value' => 'admins'}),
     Integer[0] $lag_warning = lookup('profile::query_service::lag_warning', {'default_value' => 1200}),
     Integer[0] $lag_critical = lookup('profile::query_service::lag_critical', {'default_value' => 3600}),
+    Enum['regular', 'streaming'] $updater_type = lookup('profile::query_service::updater_type', {'default_value' => 'regular'})
 ) {
     nrpe::monitor_service { 'Query_Service_Internal_HTTP_endpoint':
         description  => 'Query Service HTTP Port',
@@ -17,14 +18,31 @@ class profile::query_service::monitor::wikidata (
         notes_url     => 'https://wikitech.wikimedia.org/wiki/Wikidata_query_service/Runbook',
     }
 
-    monitoring::check_prometheus { 'WDQS_Lag':
-        description     => 'WDQS high update lag',
-        dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/wikidata-query-service?orgId=1&panelId=8&fullscreen'],
-        query           => "scalar(time() - blazegraph_lastupdated{instance=\"${::hostname}:9193\"})",
-        prometheus_url  => "http://prometheus.svc.${::site}.wmnet/ops",
-        warning         => $lag_warning,
-        critical        => $lag_critical,
-        contact_group   => $contact_groups,
-        notes_link      => 'https://wikitech.wikimedia.org/wiki/Wikidata_query_service/Runbook#Update_lag',
+    case $updater_type {
+        'regular': {
+            monitoring::check_prometheus { 'WDQS_Lag':
+                description     => 'WDQS high update lag',
+                dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/wikidata-query-service?orgId=1&panelId=8&fullscreen'],
+                query           => "scalar(time() - blazegraph_lastupdated{instance=\"${::hostname}:9193\"})",
+                prometheus_url  => "http://prometheus.svc.${::site}.wmnet/ops",
+                warning         => $lag_warning,
+                critical        => $lag_critical,
+                contact_group   => $contact_groups,
+                notes_link      => 'https://wikitech.wikimedia.org/wiki/Wikidata_query_service/Runbook#Update_lag',
+            }
+        }
+        'streaming': {
+            monitoring::check_prometheus { 'WDQS_Lag_Streaming':
+                description     => 'WDQS high update lag',
+                dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/wikidata-query-service?orgId=1&panelId=8&fullscreen'],
+                query           => "scalar(wdqs_streaming_updater_kafka_stream_consumer_lag{instance=\"${::hostname}:9101\"})",
+                prometheus_url  => "http://prometheus.svc.${::site}.wmnet/ops",
+                warning         => $lag_warning,
+                critical        => $lag_critical,
+                contact_group   => $contact_groups,
+                notes_link      => 'https://wikitech.wikimedia.org/wiki/Wikidata_query_service/Runbook#Update_lag',
+            }
+        }
+        default: { fail("Unsupported updater_type: ${updater_type}") }
     }
 }
