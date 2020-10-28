d9399a088fe5bf10db6ca59e5788e9e648b57cd8
John Bond <jbond@wikimedia.org>
2020-10-28 16:25:09 +0100

    base: fix spec test
    Change-Id: I8177eec63c700696e42bd4fc9ba54faf5576f9d6

--- modules/base/spec/defines/expose_puppet_certs_spec.rb
+++ modules/base/spec/defines/expose_puppet_certs_spec.rb
@@ -1,45 +1,49 @@
 require_relative '../../../../rake_modules/spec_helper'
 
 describe 'base::expose_puppet_certs', :type => :define do
-  let(:title) { '/my/ssl/dir' }
-  let(:facts) { { :fqdn => 'host.example.net'} }
-  let(:pre_condition) { "class {'base::puppet': ca_source => 'puppet:///files/puppet/ca.production.pem'}" }
+  on_supported_os(WMFConfig.test_on).each do |os, facts|
+    context "on #{os}" do
+      let(:title) { '/my/ssl/dir' }
+      let(:facts) { facts.merge(fqdn: 'host.example.net') }
+      let(:pre_condition) { "class {'base::puppet': ca_source => 'puppet:///files/puppet/ca.production.pem'}" }
 
-  describe 'directory structure is created' do
-    it { should contain_file('/my/ssl/dir/ssl').with({ 'ensure' => 'directory', 'mode' => '0555' }) }
-  end
+      describe 'directory structure is created' do
+        it { should contain_file('/my/ssl/dir/ssl').with({ 'ensure' => 'directory', 'mode' => '0555' }) }
+      end
 
-  describe 'host certificate is exposed' do
-    it { should contain_file('/my/ssl/dir/ssl/cert.pem')
-                    .with({
-                              'ensure' => 'present',
-                              'mode' => '0444',
-                              'source' => '/var/lib/puppet/ssl/certs/host.example.net.pem',
-                          })
-    }
-  end
+      describe 'host certificate is exposed' do
+        it { should contain_file('/my/ssl/dir/ssl/cert.pem')
+          .with({
+          'ensure' => 'present',
+          'mode' => '0444',
+          'source' => '/var/lib/puppet/ssl/certs/host.example.net.pem',
+        })
+        }
+      end
 
-  describe 'private key is not exposed by default' do
-    it { should contain_file('/my/ssl/dir/ssl/server.key').with({ 'ensure' => 'absent' }) }
-  end
+      describe 'private key is not exposed by default' do
+        it { should contain_file('/my/ssl/dir/ssl/server.key').with({ 'ensure' => 'absent' }) }
+      end
 
-  describe 'private key is exposed if required' do
-    let(:params) { { :provide_private => true } }
+      describe 'private key is exposed if required' do
+        let(:params) { { :provide_private => true } }
 
-    it { should contain_file('/my/ssl/dir/ssl/server.key')
-                    .with({
-                              'ensure' => 'present',
-                              'mode' => '0400',
-                              'source' => '/var/lib/puppet/ssl/private_keys/host.example.net.pem',
-                          })
-    }
-  end
+        it { should contain_file('/my/ssl/dir/ssl/server.key')
+          .with({
+          'ensure' => 'present',
+          'mode' => '0400',
+          'source' => '/var/lib/puppet/ssl/private_keys/host.example.net.pem',
+        })
+        }
+      end
 
-  describe 'all files are removed when ensure => absent' do
-    let(:params) { { :ensure => 'absent' } }
+      describe 'all files are removed when ensure => absent' do
+        let(:params) { { :ensure => 'absent' } }
 
-    it { should contain_file('/my/ssl/dir/ssl').with({ 'ensure' => 'absent' }) }
-    it { should contain_file('/my/ssl/dir/ssl/cert.pem').with({ 'ensure' => 'absent' }) }
-    it { should contain_file('/my/ssl/dir/ssl/server.key').with({ 'ensure' => 'absent' }) }
+        it { should contain_file('/my/ssl/dir/ssl').with({ 'ensure' => 'absent' }) }
+        it { should contain_file('/my/ssl/dir/ssl/cert.pem').with({ 'ensure' => 'absent' }) }
+        it { should contain_file('/my/ssl/dir/ssl/server.key').with({ 'ensure' => 'absent' }) }
+      end
+    end
   end
 end
