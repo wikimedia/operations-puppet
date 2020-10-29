770e5fd49f9cb0fb35bd69453b39999f4cbf20e4
Daniel Zahn <dzahn@wikimedia.org>
2020-10-29 11:55:15 -0700

    create microsite for WDQS UI
    This will create a httpd site on the miscweb* VMs that are
    backends for other microsites and git clone the contents
    of the deploy repo listed on the ticket.
    
    Another change will be to point the traffic layer to this.
    This first step we can merge anytime before that.
    
    Bug: T266702
    Change-Id: I6e097837b8102bc0e198a1c231116f8781389258

---
+++ modules/profile/manifests/microsites/wdqs.pp
@@ -0,0 +1,18 @@
+# Wikidata Query Service UI (T266702)
+class profile::microsites::wdqs {
+
+    httpd::site { 'query.wikidata.org':
+        content => template('profile/wdqs/httpd-query.wikidata.org.erb'),
+    }
+
+    ensure_resource('file', '/srv/org', {'ensure' => 'directory' })
+    ensure_resource('file', '/srv/org/wikidata', {'ensure' => 'directory' })
+    ensure_resource('file', '/srv/org/wikidata/query', {'ensure' => 'directory' })
+
+    git::clone { 'wikidata/query/gui-deploy':
+        ensure    => 'latest',
+        source    => 'gerrit',
+        directory => '/srv/org/wikidata/query',
+        branch    => 'master',
+    }
+}
