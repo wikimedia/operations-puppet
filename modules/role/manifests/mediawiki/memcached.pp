83ab0c9bd0ab24cd780578af472e339fcdaf4e92
Daniel Zahn <dzahn@wikimedia.org>
2020-10-30 10:39:50 -0700

    mediawiki/memcached: stop using role inheritance
    This is the only role I see globally that uses inheritance
    from another role.
    
    Instead just include the same profiles as in the memcached
    role plus the additional stuff.
    
    Should be noop.
    
    Change-Id: Ia0d75fb4a55e803df50962802a597b978e305e8f

--- modules/role/manifests/mediawiki/memcached.pp
+++ modules/role/manifests/mediawiki/memcached.pp
@@ -1,11 +1,18 @@
 # Role for the MediaWiki memcached+redis-sessions role for production.
-class role::mediawiki::memcached inherits role::memcached {
+class role::mediawiki::memcached{
+
+    system::role { 'mediawiki::memcached':
+        description => 'memcached+redis sessions',
+    }
+
+    include ::profile::standard
+    include ::profile::base::firewall
+    include profile::memcached::instance
+    include profile::memcached::memkeys
+    include profile::memcached::performance
 
     # Trying out buster on one shard (T252391)
     if debian::codename::eq('jessie') {
         include profile::redis::multidc
-    }
-    system::role { 'mediawiki::memcached':
-        description => 'memcached+redis sessions',
     }
 }
