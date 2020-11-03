40c81c4f661d835925b357407cf5becf48fd68d2
John Bond <jbond@wikimedia.org>
2020-11-03 17:37:36 +0100

    php: migrate to shared spec_test
    also drop test_on variable as it will be picked up from metadata.json
    
    Change-Id: Ia0ddcbbe47fa13d8ffb03871e6461daafcb072ed

--- modules/php/spec/classes/php_default_extensions_spec.rb
+++ modules/php/spec/classes/php_default_extensions_spec.rb
@@ -1,35 +1,29 @@
-require 'spec_helper'
-test_on = {
-  supported_os: [
-    {
-      'operatingsystem'        => 'Debian',
-      'operatingsystemrelease' => ['9'],
-    }
-  ]
-}
+require_relative '../../../../rake_modules/spec_helper'
 
 describe 'php::default_extensions' do
-  on_supported_os(test_on).each do |_os, facts|
-    let(:facts) {facts}
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) {facts}
 
-    context 'when called alone' do
-      it { is_expected.to compile.and_raise_error(/php::default_extensions is a private class/)}
-    end
+      context 'when called alone' do
+        it { is_expected.to compile.and_raise_error(/php::default_extensions is a private class/)}
+      end
 
-    context 'when called from the php class' do
-      let(:pre_condition) { 'include php' }
-      it { is_expected.to compile }
+      context 'when called from the php class' do
+        let(:pre_condition) { 'include php' }
+        it { is_expected.to compile }
 
-      it { is_expected.to contain_php__extension('tokenizer')
-                            .with_package_name('')
-                            .with_priority(20)
-      }
+        it { is_expected.to contain_php__extension('tokenizer')
+          .with_package_name('')
+          .with_priority(20)
+        }
 
-      it { is_expected.to contain_php__extension('opcache')
-                            .with_package_name('')
-                            .with_priority(10)
-                            .with_config({'zend_extension' => 'opcache.so'})
-      }
+        it { is_expected.to contain_php__extension('opcache')
+          .with_package_name('')
+          .with_priority(10)
+          .with_config({'zend_extension' => 'opcache.so'})
+        }
+      end
     end
   end
 end
