57e92789b2d044a64ed27e6b3e8be2d3b1594c17
John Bond <jbond@wikimedia.org>
2020-11-03 16:13:55 +0100

    elasticsearch: migrate to shared spec helper
    Change-Id: Ie245df9c7b0af9a69d84a82dfe74129ef9cf5aec

--- modules/elasticsearch/spec/classes/elasticsearch_rspec.rb
+++ modules/elasticsearch/spec/classes/elasticsearch_rspec.rb
@@ -1,25 +1,26 @@
-require 'spec_helper'
+require_relative '../../../../rake_modules/spec_helper'
 
 describe 'elasticsearch', :type => :class do
-  let(:facts) { { :lsbdistrelease => 'debian',
-                  :lsbdistid      => 'jessie',
-                  :initsystem     => 'systemd',
-  } }
+  on_supported_os(WMFConfig.test_on).each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
 
-  describe 'when GC logging is enabled' do
-    let(:params) { {
-        :default_instance_params => {
+      describe 'when GC logging is enabled' do
+        let(:params) { {
+          :default_instance_params => {
             :cluster_name => 'my_cluster_name',
             :short_cluster_name => 'the_short_cluster_name',
             :gc_log => true,
             :send_logs_to_logstash => false,
             :publish_host => '127.0.0.1',
-        },
-    } }
-    it {
-        is_expected.to contain_file('/etc/elasticsearch/my_cluster_name/jvm.options')
+          },
+        } }
+        it {
+          is_expected.to contain_file('/etc/elasticsearch/my_cluster_name/jvm.options')
             .with_content(/-XX:\+PrintGCDetails$/)
             .with_content(/-XX:\+PrintGCDateStamps$/)
-    }
+        }
+      end
+    end
   end
 end
--- modules/elasticsearch/spec/classes/ship_logs_to_logstash_rspec.rb
+++ modules/elasticsearch/spec/classes/ship_logs_to_logstash_rspec.rb
@@ -1,38 +1,35 @@
-require 'spec_helper'
+require_relative '../../../../rake_modules/spec_helper'
 
 describe 'elasticsearch', :type => :class do
-  describe 'when NOT sending logs to logstash' do
-    let(:params) { {
-        :default_instance_params => {
+  on_supported_os(WMFConfig.test_on).each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+      describe 'when NOT sending logs to logstash' do
+        let(:params) { {
+          :default_instance_params => {
             :cluster_name          => 'my_cluster_name',
             :short_cluster_name    => 'the_short_cluster_name',
             :send_logs_to_logstash => false,
             :publish_host          => '127.0.0.1',
-        }
-    } }
-    let(:facts) { { :lsbdistrelease => 'debian',
-                    :lsbdistid      => 'jessie',
-                    :initsystem     => 'systemd',
-    } }
+          }
+        } }
 
-    it { should_not contain_package('liblogstash-gelf-java') }
-  end
+        it { should_not contain_package('liblogstash-gelf-java') }
+      end
 
-  describe 'when sending logs to logstash' do
-    let(:params) { {
-        :logstash_host => 'logstash.example.net',
-        :default_instance_params => {
+      describe 'when sending logs to logstash' do
+        let(:params) { {
+          :logstash_host => 'logstash.example.net',
+          :default_instance_params => {
             :cluster_name          => 'my_cluster_name',
             :short_cluster_name    => 'the_short_cluster_name',
             :send_logs_to_logstash => true,
             :publish_host          => '127.0.0.1',
-        }
-    } }
-    let(:facts) { { :lsbdistrelease => 'debian',
-                    :lsbdistid      => 'jessie',
-                    :initsystem     => 'systemd',
-    } }
+          }
+        } }
 
-    it { should contain_package('liblogstash-gelf-java').with({ :ensure => 'present' }) }
+        it { should contain_package('liblogstash-gelf-java').with({ :ensure => 'present' }) }
+      end
+    end
   end
 end
