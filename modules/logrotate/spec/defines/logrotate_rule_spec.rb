ca32c32f714f4a45241186d36556554d9745a8f6
John Bond <jbond@wikimedia.org>
2020-11-03 14:23:57 +0100

    logrotate: migrate spec tests to shared helper
    Change-Id: I149437ecfa5b4a917ab565cc26fe934b7b94161d

--- modules/logrotate/spec/defines/logrotate_rule_spec.rb
+++ modules/logrotate/spec/defines/logrotate_rule_spec.rb
@@ -1,46 +1,51 @@
-require 'spec_helper'
+require_relative '../../../../rake_modules/spec_helper'
 
 describe 'logrotate::rule', :type => :define do
+  on_supported_os(WMFConfig.test_on).each do |os, facts|
+  context "on #{os}" do
+    let(:facts) { facts }
     let(:title) { 'some_rule' }
 
     context 'undef frequency is allowed' do
-        let(:params) { {
-            :frequency => :undef,
-            :file_glob => '/var/log/some.log',
-        } }
-        it { should contain_file('/etc/logrotate.d/some_rule') }
+      let(:params) { {
+        :frequency => :undef,
+        :file_glob => '/var/log/some.log',
+      } }
+      it { should contain_file('/etc/logrotate.d/some_rule') }
     end
 
     context 'undef frequency and size' do
-        let(:params) { {
-            :frequency => :undef,
-            :file_glob => '/var/log/some.log',
-            :size => '10M',
-        } }
-        it { should contain_file('/etc/logrotate.d/some_rule')
-                        .with_content(/\ssize 10M/)
-        }
+      let(:params) { {
+        :frequency => :undef,
+        :file_glob => '/var/log/some.log',
+        :size => '10M',
+      } }
+      it { should contain_file('/etc/logrotate.d/some_rule')
+        .with_content(/\ssize 10M/)
+      }
     end
 
     context 'defined frequency and size' do
-        let(:params) { {
-            :frequency => 'daily',
-            :file_glob => '/var/log/some.log',
-            :size => '10M',
-        } }
-        it { should contain_file('/etc/logrotate.d/some_rule')
-                        .with_content(/maxsize 10M/)
-        }
+      let(:params) { {
+        :frequency => 'daily',
+        :file_glob => '/var/log/some.log',
+        :size => '10M',
+      } }
+      it { should contain_file('/etc/logrotate.d/some_rule')
+        .with_content(/maxsize 10M/)
+      }
     end
 
     context 'defined frequency and no size' do
-        let(:params) { {
-            :frequency => 'daily',
-            :file_glob => '/var/log/some.log',
-            :size => :undef,
-        } }
-        it { should contain_file('/etc/logrotate.d/some_rule')
-                        .without_content(/size/)
-        }
+      let(:params) { {
+        :frequency => 'daily',
+        :file_glob => '/var/log/some.log',
+        :size => :undef,
+      } }
+      it { should contain_file('/etc/logrotate.d/some_rule')
+        .without_content(/size/)
+      }
     end
+  end
+  end
 end
