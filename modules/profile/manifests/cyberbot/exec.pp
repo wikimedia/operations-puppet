a6f87b88ffa3849443e6736e05f76c92bd922a39
John Bond <jbond@wikimedia.org>
2020-11-06 18:19:14 +0100

    P:cyberbot::exec:  migrate to debian::codename and ensure_packages
    Bug: T266479
    Bug: T267396
    Change-Id: Ifa73babda0fa17b5c6f95bfcb9d645a578ec0783

--- modules/profile/manifests/cyberbot/exec.pp
+++ modules/profile/manifests/cyberbot/exec.pp
@@ -1,16 +1,15 @@
 # sets up a an exec node for cyberbot
 class profile::cyberbot::exec{
 
-    if os_version('debian >= stretch') {
-        $php_version = '7.2'
-    } else {
-        $php_version = '5'
+    # TODO: im not sure if this is correct for buster
+    # Also wonder if we should create a php_version fact
+    $php_version = debian::codename::ge('stretch') ? {
+        true    => '7.2',
+        default => '5',
     }
 
-    require_package("php${php_version}-mysql")
-    require_package("php${php_version}-mysqlnd")
-    require_package("php${php_version}-cli")
-    require_package("php${php_version}-intl")
-    require_package("php${php_version}-json")
-    require_package("php${php_version}-curl")
+    ensure_packages([
+        "php${php_version}-mysql", "php${php_version}-mysqlnd", "php${php_version}-cli",
+        "php${php_version}-intl", "php${php_version}-json", "php${php_version}-curl"
+    ])
 }
