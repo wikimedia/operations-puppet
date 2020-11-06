b553edae01d0624b0aeee232a405964e00367900
John Bond <jbond@wikimedia.org>
2020-11-06 19:06:35 +0100

    P:phabricator: migrate to debian::codename and ensure_packages
    Bug: T266479
    Bug: T267396
    Change-Id: I0aa327421c83b5f9502a5768d30682403f329bc7

--- modules/profile/manifests/phabricator/httpd.pp
+++ modules/profile/manifests/phabricator/httpd.pp
@@ -6,34 +6,34 @@ class profile::phabricator::httpd (
 
     $httpd_base_modules = [ 'headers', 'rewrite', 'remoteip' ]
 
-    if os_version('debian == buster') {
-        $php_lib = 'php7.3-fpm'
-    } else {
-        $php_lib = 'php7.2-fpm'
+    # TODO: php_fact use case
+    $php_lib = debian::codename::eq('buster') ? {
+        true    => 'php7.3-fpm',
+        default => 'php7.2-fpm',
     }
     $httpd_extra_modules = [ 'proxy', 'proxy_fcgi' ]
 
     $httpd_modules = concat($httpd_base_modules, $httpd_extra_modules)
 
-    class { '::httpd':
+    class { 'httpd':
         modules => $httpd_modules,
         require => Package[$php_lib],
     }
 
     # MPM tweaks for high load systems
     # More performance specific tweaks to follow here
-    class { '::httpd::mpm':
+    class { 'httpd::mpm':
         mpm    => 'worker',
         source => 'puppet:///modules/phabricator/apache/worker.conf',
     }
 
     # Forensic logging (logs requests at both beginning and end of request processing)
     if $enable_forensic_log {
-        ::httpd::mod_conf { 'log_forensic':
+        httpd::mod_conf { 'log_forensic':
             ensure  => present,
         }
 
-        ::httpd::conf { 'log_forensic':
+        httpd::conf { 'log_forensic':
             ensure  => present,
             source  => 'puppet:///modules/phabricator/apache/log_forensic.conf',
             require => Httpd::Mod_conf['log_forensic'],
