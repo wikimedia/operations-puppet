7c5949790eba37246333692d202158e3eafbf574
John Bond <jbond@wikimedia.org>
2020-11-06 21:00:01 +0100

    striker: migrate to debian::codename and ensure_packages
    Bug: T266479
    Bug: T267396
    Hosts: O:striker
    Change-Id: Ied0ac548f4595ba41ccf366783a2c6713f914af8

--- modules/role/manifests/striker/web.pp
+++ modules/role/manifests/striker/web.pp
@@ -6,19 +6,19 @@
 # filtertags: labs-project-striker
 class role::striker::web {
 
-    if os_version('debian >= stretch') {
-        require_package('libapache2-mod-wsgi-py3')
-        class { '::httpd':
+    if debian::codname::ge('stretch') {
+        ensure_packages('libapache2-mod-wsgi-py3')
+        class { 'httpd':
             modules => ['alias', 'ssl', 'rewrite', 'headers', 'wsgi',
                         'proxy', 'expires', 'proxy_http', 'proxy_balancer',
                         'lbmethod_byrequests'],
         }
     }
 
-    include ::profile::standard
-    include ::memcached
-    include ::striker::apache
-    include ::striker::uwsgi
-    require ::passwords::striker
+    include profile::standard
+    include memcached
+    include striker::apache
+    include striker::uwsgi
+    require passwords::striker
 }
 # vim:sw=4:ts=4:sts=4:ft=puppet:
