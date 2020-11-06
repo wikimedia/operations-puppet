b5501a3dc40afbc0e60dc3f504504be9f4279e00
John Bond <jbond@wikimedia.org>
2020-11-06 20:42:29 +0100

    O:alerting_host: migrate to debian::codename and ensure_packages
    Bug: T266479
    Bug: T267396
    Hosts: O:alerting_host
    Change-Id: I7d30a4c7a71db7489a601c3aa6e248060f3a1540

--- modules/role/manifests/alerting_host.pp
+++ modules/role/manifests/alerting_host.pp
@@ -11,33 +11,33 @@ class role::alerting_host {
         description => 'central host for health checking and alerting'
     }
 
-    include ::profile::standard
-    include ::profile::base::firewall
+    include profile::standard
+    include profile::base::firewall
 
-    include ::profile::icinga
-    include ::profile::icinga::logmsgbot
-    include ::profile::certspotter
-    include ::profile::scap::dsh
+    include profile::icinga
+    include profile::icinga::logmsgbot
+    include profile::certspotter
+    include profile::scap::dsh
 
-    include ::profile::dns::auth::monitoring::global
+    include profile::dns::auth::monitoring::global
     # backup checks directly to db to avoid spof
-    include ::profile::mariadb::wmfmariadbpy
-    include ::profile::mariadb::backup::check
+    include profile::mariadb::wmfmariadbpy
+    include profile::mariadb::backup::check
 
     # Temporary until all hosts are on Buster
     # https://phabricator.wikimedia.org/T247966
-    if os_version('debian >= buster') {
-        include ::profile::alertmanager
-        include ::profile::alertmanager::irc
-        include ::profile::alertmanager::web
-        include ::profile::alertmanager::ack
+    if debian::codename::ge('buster') {
+        include profile::alertmanager
+        include profile::alertmanager::irc
+        include profile::alertmanager::web
+        include profile::alertmanager::ack
     }
 
-    class { '::httpd::mpm':
+    class { 'httpd::mpm':
         mpm => 'prefork'
     }
 
-    class { '::httpd':
+    class { 'httpd':
         modules => ['headers', 'rewrite', 'authnz_ldap', 'authn_file', 'cgi',
                     'ssl', 'proxy_http', 'allowmethods'],
     }
