d1ba54a7f19df0047de5b029357586a7f1de409e
Cole White <cwhite@wikimedia.org>
2020-11-06 10:36:40 -0700

    hiera: add parameter to enable access from hosts and add grafana
    opens up the firewall for grafana to talk to alertmanager
    
    Bug: T267017
    Change-Id: I1b60253e873d6c752897f54ac6a0e1fd99ea287e

--- modules/profile/manifests/alertmanager.pp
+++ modules/profile/manifests/alertmanager.pp
@@ -1,6 +1,7 @@
 class profile::alertmanager (
     Stdlib::Host        $active_host = lookup('profile::alertmanager::active_host'),
     Array[Stdlib::Host] $partners    = lookup('profile::alertmanager::partners'),
+    Array[Stdlib::Host] $allow_from  = lookup('profile::alertmanager::allow_from', { 'default_value' => [] }),
     String              $irc_channel = lookup('profile::alertmanager::irc::channel'),
     Optional[String]    $victorops_api_key = lookup('profile::alertmanager::victorops_api_key'),
     Array $prometheus_all_nodes = lookup('prometheus_all_nodes'),
@@ -13,7 +14,7 @@ class profile::alertmanager (
     }
 
     # All Prometheus servers need access to Alertmanager to send alerts
-    $prometheus_nodes_ferm = join($prometheus_all_nodes, ' ')
+    $prometheus_nodes_ferm = join($prometheus_all_nodes + $allow_from, ' ')
     ferm::service { 'alertmanager-prometheus':
         proto  => 'tcp',
         port   => '9093',
