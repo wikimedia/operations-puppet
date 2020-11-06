82427166da9d71c116a36739cc4dcf5b74863918
John Bond <jbond@wikimedia.org>
2020-11-06 20:49:26 +0100

    mediawaki::memcached: migrate to debian::codename and ensure_packages
    Bug: T266479
    Bug: T267396
    Hosts: O:mediawaki::memcached
    Change-Id: I4bca9bbf36c9c4b2cf1ccf2ed2f22579f1fd5cdf

--- modules/role/manifests/mediawiki/memcached.pp
+++ modules/role/manifests/mediawiki/memcached.pp
@@ -2,8 +2,8 @@
 class role::mediawiki::memcached inherits role::memcached {
 
     # Trying out buster on one shard (T252391)
-    if os_version('debian == jessie') {
-        include ::profile::redis::multidc
+    if debian::codename::eq('jessie') {
+        include profile::redis::multidc
     }
     system::role { 'mediawiki::memcached':
         description => 'memcached+redis sessions',
