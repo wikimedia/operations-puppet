31af4b6e44ab9ade9f4662848ff47355b1a5d456
John Bond <jbond@wikimedia.org>
2020-11-06 12:19:08 +0100

    base:standard_packages: migrate to debian::codename and ensure_packages
    Also do a bit of refactoring
    
    Hosts: conf2001.codfw.wmnet,cumin1001.eqiad.wmnet,neon.eqiad.wmnet
    Bug: T266479
    Bug: T267396
    Change-Id: I2142e8a328138a685c8ebc8f90368c65c35842f4

--- modules/base/manifests/standard_packages.pp
+++ modules/base/manifests/standard_packages.pp
@@ -1,87 +1,43 @@
 class base::standard_packages {
 
-    require_package ([
-        'acct',
-        'apt-transport-https',
-        'byobu',
-        'colordiff',
-        'curl',
-        'debian-goodies',
-        'dnsutils',
-        'dstat',
-        'ethtool',
-        'gdb',
-        'gdisk',
-        'git-fat',
-        'git',
-        'htop',
-        'httpry',
-        'iotop',
-        'iperf',
-        'jq',
-        'libtemplate-perl', # Suggested by vim-scripts
-        'lldpd',
-        'lshw',
-        'molly-guard',
-        'moreutils',
-        'net-tools',
-        'numactl',
-        'ncdu',
-        'ngrep',
-        'pigz',
-        'psmisc',
-        'pv',
-        'python3',
-        'quickstack',
-        'screen',
-        'strace',
-        'sysstat',
-        'tcpdump',
-        'tmux',
-        'tree',
-        'vim',
-        'vim-addon-manager',
-        'vim-scripts',
-        'wipe',
-        'xfsprogs',
-        'zsh',
+    ensure_packages ([
+        'acct', 'apt-transport-https', 'byobu', 'colordiff', 'curl', 'debian-goodies', 'dnsutils', 'dstat',
+        'ethtool', 'gdb', 'gdisk', 'git-fat', 'git', 'htop', 'httpry', 'iotop', 'iperf', 'jq',
+        'libtemplate-perl', 'lldpd', 'lshw', 'molly-guard', 'moreutils', 'net-tools', 'numactl', 'ncdu',
+        'ngrep', 'pigz', 'psmisc', 'pv', 'python3', 'quickstack', 'screen', 'strace', 'sysstat', 'tcpdump',
+        'tmux', 'tree', 'vim', 'vim-addon-manager', 'vim-scripts', 'wipe', 'xfsprogs', 'zsh',
     ])
+    package { 'tzdata': ensure => latest }
+
+    # Pulled in via tshark below, defaults to "no"
+    debconf::seen { 'wireshark-common/install-setuid': }
+    package { 'tshark': ensure => present }
+
+    # ack-grep was renamed to ack
+    if debian::codename::ge('stretch') {
+        ensure_packages('ack')
+    } else {
+        ensure_packages('ack-grep')
+    }
 
     # These packages exists only from stretch onwards, once we are free of
     # jessie, remove the if and move them to the array above
-    if os_version('debian > jessie') {
-        require_package('icdiff')
-        require_package('linux-perf')
+    if debian::codename::gt('jessie') {
+        ensure_packages('icdiff')
+        ensure_packages('linux-perf')
     }
-
-    package { 'tzdata': ensure => latest }
 
     # pxz was removed in buster. In xz >= 5.2 (so stretch and later), xz has
     # builtin threading support using the -T option, so pxz was removed
-    if os_version('debian <= stretch') {
-        require_package('pxz')
-    }
-
-    # ack-grep was renamed to ack
-    if os_version('debian >= stretch') {
-        require_package('ack')
-    } else {
-        require_package('ack-grep')
+    if debian::codename::lt('buster') {
+        ensure_packages('pxz')
     }
 
     # uninstall these packages
     package { [
-            'apport',
-            'apt-listchanges',
-            'command-not-found',
-            'command-not-found-data',
-            'ecryptfs-utils',
-            'mlocate',
-            'os-prober',
-            'python3-apport',
-            'wpasupplicant',
-        ]:
-        ensure => absent,
+        'apport', 'apt-listchanges', 'command-not-found', 'command-not-found-data',
+        'ecryptfs-utils', 'mlocate', 'os-prober', 'python3-apport', 'wpasupplicant']:
+            ensure => absent,
     }
 
     # purge these packages
@@ -92,15 +48,15 @@ class base::standard_packages {
     }
 
     # real-hardware specific
-    if $facts['is_virtual'] == false {
+    unless $facts['is_virtual'] {
         # As of September 2015, mcelog still does not support newer AMD processors.
         # See <https://www.mcelog.org/faq.html#18>.
         if $::processor0 !~ /AMD/ {
-            if os_version('debian <= stretch') {
-                if versioncmp($::kernelversion, '4.12') < 0 {
-                    $mcelog_ensure = 'present'
-                } else {
-                    $mcelog_ensure = 'absent'
+            ensure_packages('intel-microcode')
+            if debian::codename::le('stretch') {
+                $mcelog_ensure = versioncmp($::kernelversion, '4.12') ? {
+                    -1      => 'present',
+                    default => 'absent',
                 }
                 package { 'mcelog':
                     ensure => $mcelog_ensure,
@@ -109,75 +65,52 @@ class base::standard_packages {
                     ensure => $mcelog_ensure,
                 }
             }
-            require_package('intel-microcode')
         }
         # rasdaemon replaces mcelog on buster
-        if os_version('debian == buster') {
-            require_package('rasdaemon')
+        if debian::codename::eq('buster') {
+            ensure_packages('rasdaemon')
             base::service_auto_restart { 'rasdaemon': }
         }
-    }
 
-    # for HP servers only - install the backplane health service and CLI
-    # As of February 2018, we are using a version of Facter where manufacturer
-    # is a current fact.  In a future upgrade, it will be a legacy fact and
-    # should be replaced with a parse of the dmi fact (which will be a map not
-    # a string).
-    if $facts['is_virtual'] == false and $facts['manufacturer'] == 'HP' {
-        require_package('hp-health')
-    }
-
-    # Pulled in via tshark below, defaults to "no"
-    debconf::seen { 'wireshark-common/install-setuid': }
-    package { 'tshark': ensure => present }
-
-    # An upgrade from jessie to stretch leaves some old binary
-    # packages around, remove those
-    if os_version('debian == stretch') {
-        package { [
-                  'libapt-inst1.5',
-                  'libapt-pkg4.12',
-                  'libdns-export100',
-                  'libirs-export91',
-                  'libisc-export95',
-                  'libisccfg-export90',
-                  'liblwres90',
-                  'libgnutls-deb0-28',
-                  'libhogweed2',
-                  'libjasper1',
-                  'libnettle4',
-                  'libruby2.1',
-                  'ruby2.1',
-                  'libpsl0',
-                  'libwiretap4',
-                  'libwsutil4',
-                  'libbind9-90',
-                  'libdns100',
-                  'libisc95',
-                  'libisccc90',
-                  'libisccfg90',
-                  'python-reportbug',
-                  'libpng12-0'
-            ]:
-            ensure => absent,
+        # for HP servers only - install the backplane health service and CLI
+        # As of February 2018, we are using a version of Facter where manufacturer
+        # is a current fact.  In a future upgrade, it will be a legacy fact and
+        # should be replaced with a parse of the dmi fact (which will be a map not
+        # a string).
+        if $facts['manufacturer'] == 'HP' {
+            ensure_packages('hp-health')
         }
     }
 
-    # An upgrade from stretch to buster leaves some old binary packages around, remove those
-    if os_version('debian == buster') {
-        package {['libbind9-140', 'libdns162', 'libevent-2.0-5', 'libisc160',
-                  'libisccc140', 'libisccfg140', 'liblwres141', 'libonig4',
-                  'libdns-export162', 'libhunspell-1.4-0', 'libisc-export160',
-                  'libgdbm3', 'libyaml-cpp0.5v5',
-                  'libperl5.24', 'ruby2.3', 'libruby2.3', 'libunbound2', 'git-core']:
-            ensure => absent,
+    case debian::codename() {
+        'stretch': {
+            # An upgrade from jessie to stretch leaves some old binary packages around, remove those
+            $absent_packages = [
+                'libapt-inst1.5', 'libapt-pkg4.12', 'libdns-export100', 'libirs-export91',
+                'libisc-export95', 'libisccfg-export90', 'liblwres90', 'libgnutls-deb0-28',
+                'libhogweed2', 'libjasper1', 'libnettle4', 'libruby2.1', 'ruby2.1', 'libpsl0',
+                'libwiretap4', 'libwsutil4', 'libbind9-90', 'libdns100', 'libisc95', 'libisccc90',
+                'libisccfg90', 'python-reportbug', 'libpng12-0'
+            ]
+            $purged_packages = []
         }
-
-        # mcelog is broken with the Linux kernel used in buster
-        package {['mcelog']:
-            ensure => purged,
+        'buster': {
+            # An upgrade from stretch to buster leaves some old binary packages around, remove those
+            $absent_packages = [
+                'libbind9-140', 'libdns162', 'libevent-2.0-5', 'libisc160', 'libisccc140', 'libisccfg140',
+                'liblwres141', 'libonig4', 'libdns-export162', 'libhunspell-1.4-0', 'libisc-export160',
+                'libgdbm3', 'libyaml-cpp0.5v5', 'libperl5.24', 'ruby2.3', 'libruby2.3', 'libunbound2', 'git-core'
+            ]
+            # mcelog is broken with the Linux kernel used in buster
+            $purged_packages = ['mcelog']
+        }
+        default: {
+            $absent_packages = []
+            $purged_packages = []
         }
     }
+    package {$absent_packages: ensure => 'absent'}
+    package {$purged_packages: ensure => 'purged'}
 
     base::service_auto_restart { 'lldpd': }
     base::service_auto_restart { 'cron': }
@@ -191,7 +124,7 @@ class base::standard_packages {
     # passed. This is now used by journald to ensure that the
     # various sockets connected to all the system's stdout/stderr
     # are not lost when journald is restarted.
-    if os_version('debian >= stretch') {
+    if debian::codename::ge('stretch') {
         base::service_auto_restart { 'systemd-journald': }
     }
 }
---
+++ modules/base/spec/classes/standard_packages_spec.rb
@@ -0,0 +1,42 @@
+require_relative '../../../../rake_modules/spec_helper'
+
+describe 'base::standard_packages' do
+  on_supported_os(WMFConfig.test_on).each do |os, facts|
+    context "On #{os}" do
+      let(:facts) { facts}
+      case facts[:os]['distro']['codename']
+      when 'buster'
+        let(:installed_packages) { ['ack'] }
+        let(:absent_packages) { ['libbind9-140'] }
+        let(:purged_packages) { ['mcelog'] }
+      when 'stretch'
+        let(:installed_packages) { ['ack'] }
+        let(:absent_packages) { ['libapt-inst1.5'] }
+        let(:purged_packages) { [] }
+      else
+        let(:installed_packages) { ['ack-grep'] }
+        let(:absent_packages) { [] }
+        let(:purged_packages) { [] }
+      end
+      it { is_expected.to compile }
+      # tests a some random packages
+      it { is_expected.to contain_package('vim').with_ensure('present') }
+      it { is_expected.to contain_package('tzdata').with_ensure('latest') }
+      it 'test debian specific present package' do
+        installed_packages.each do |package|
+          is_expected.to contain_package(package).with_ensure('present')
+        end
+      end
+      it 'test debian specific absent package' do
+        absent_packages.each do |package|
+          is_expected.to contain_package(package).with_ensure('absent')
+        end
+      end
+      it 'test debian specific purged package' do
+        purged_packages.each do |package|
+          is_expected.to contain_package(package).with_ensure('purged')
+        end
+      end
+    end
+  end
+end
