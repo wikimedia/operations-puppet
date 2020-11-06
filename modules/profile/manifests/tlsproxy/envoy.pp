96561ca4c4c2f8bbbd934fe2ba0605eac2ec3295
John Bond <jbond@wikimedia.org>
2020-11-06 19:21:07 +0100

    P:tlsproxy::envoy: migrate to debian::codename and ensure_packages
    Bug: T266479
    Bug: T267396
    Hosts: P:tlsproxy::envoy
    Change-Id: I456cba7939d1b82f2230c82bf5e7d4989f411cbc

--- modules/profile/manifests/tlsproxy/envoy.pp
+++ modules/profile/manifests/tlsproxy/envoy.pp
@@ -76,7 +76,7 @@ class profile::tlsproxy::envoy(
 ) {
     require profile::envoy
     $ensure = $profile::envoy::ensure
-    if os_version('debian jessie') and $tls_port !~ Stdlib::Port::Unprivileged {
+    if debian::codename::eq('jessie') and $tls_port !~ Stdlib::Port::Unprivileged {
             fail('Envoy can only work with unprivileged ports under jessie.')
     }
     if $global_cert_name and $acme_cert_name {
