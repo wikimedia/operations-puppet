b42b04dd031f8d741bc6b4a1abea14b14c2ebd30
Amir Sarabadani <Ladsgroup@gmail.com>
2020-11-16 22:26:50 +0100

    Revert "icinga: Drop ores.wmflabs.org monitoring"
    This reverts commit 580f983bd79f69da73b884702fd0063bf25ab32d
    
    But turning them absent and then removing them
    
    Change-Id: I541696def47cbbee34731d19a996e13178af2b97

---
+++ modules/icinga/manifests/monitor/ores_labs_web_node.pp
@@ -0,0 +1,11 @@
+# monitor an ores labs web node
+define icinga::monitor::ores_labs_web_node () {
+    monitoring::service { "ores_web_node_labs_${title}":
+        ensure        => absent,
+        description   => "ORES web node labs ${title}",
+        check_command => "check_ores_workers!oresweb/node/${title}",
+        host          => 'ores.wmflabs.org',
+        contact_group => 'team-scoring',
+        notes_url     => 'https://wikitech.wikimedia.org/wiki/ORES',
+    }
+}
