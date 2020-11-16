a180132456333570373a0b300a2f3ff93c22c059
Dzahn <dzahn@wikimedia.org>
2020-11-16 23:20:13 +0000

    Revert "ores: absent icinga monitoring of labs nodes"
    This reverts commit a017ea05eebc4148e7f2ca7f5a5fb57cd8b73d15.
    
    Reason for revert: still not removed from alert1001 and testing
    
    Change-Id: I7d7b02ac6d14aa99e643d69003decf36eb99ae7a

--- modules/icinga/manifests/monitor/ores_labs_web_node.pp
+++ modules/icinga/manifests/monitor/ores_labs_web_node.pp
@@ -1,7 +1,7 @@
 # monitor an ores labs web node
 define icinga::monitor::ores_labs_web_node () {
     monitoring::service { "ores_web_node_labs_${title}":
-        ensure        => 'absent',
+        ensure        => 'present',
         description   => "ORES web node labs ${title}",
         check_command => "check_ores_workers!oresweb/node/${title}",
         host          => 'ores.wmflabs.org',
