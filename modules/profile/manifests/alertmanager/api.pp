e4bfe1c5e1a5897779b43309165a724f5704863b
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2020-11-17 11:27:21 +0100

    alertmanager: vary apache access config based on type
    Bug: T266017
    Change-Id: I5f864e39f859408bb891269f4e00ae6c316ed756

--- modules/profile/manifests/alertmanager/api.pp
+++ modules/profile/manifests/alertmanager/api.pp
@@ -4,9 +4,15 @@ class profile::alertmanager::api (
     # lint:ignore:wmf_styleguide - T260574
     String $vhost  = lookup('profile::alertmanager::api::vhost', {'default_value' => "alertmanager.${facts['domain']}"}),
     # lint:endignore
-    Array[Stdlib::Host] $ro_hosts = lookup('profile::alertmanager::api::ro'),
-    Array[Stdlib::Host] $rw_hosts = lookup('profile::alertmanager::api::rw'),
+    Array[Stdlib::Host] $ro = lookup('profile::alertmanager::api::ro'),
+    Array[Stdlib::Host] $rw = lookup('profile::alertmanager::api::rw'),
 ) {
+
+    $ro_hosts = $ro.filter |$el| { $el =~ Stdlib::Host }
+    $ro_ips = $ro.filter |$el|   { ! $el =~ Stdlib::Host }
+    $rw_hosts = $rw.filter |$el| { $el =~ Stdlib::Host }
+    $rw_ips = $rw.filter |$el|   { ! $el =~ Stdlib::Host }
+
     httpd::site { $vhost:
         content => template('profile/alertmanager/api.apache.erb'),
     }
