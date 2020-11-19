3016d4d0b6e65270efd0f3f45fc432074f30874d
Ryan Kemper <ryankemper.cs@gmail.com>
2020-11-18 16:20:25 -0800

    cirrus: re-enable saneitizer
    This reverts commit 9a705d222c697c32b71543066ea76582a13c60a4.
    
    Following an increase of the concurrency factor from 20 to 30, we are
    now able to keep up with the saneitizer's workload.
    
    See below for where concurrency was increased:
    
    https://gerrit.wikimedia.org/r/c/operations/deployment-charts/+/641251
    
    Bug: T266762
    Change-Id: I93c1f0796a250f834c97e9d66a1debe9330177c5

--- modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
+++ modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
@@ -22,7 +22,6 @@ class profile::mediawiki::maintenance::cirrussearch {
     }
 
     profile::mediawiki::periodic_job { 'cirrus_sanitize_jobs':
-        ensure   => absent,
         command  => '/usr/local/bin/foreachwiki extensions/CirrusSearch/maintenance/SaneitizeJobs.php --push --refresh-freq=7200',
         interval => '0/2:10',
     }
