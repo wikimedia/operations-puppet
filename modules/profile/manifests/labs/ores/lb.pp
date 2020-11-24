9aeea5980e233aaf2eea05d824c457b398708407
Daniel Zahn <dzahn@wikimedia.org>
2020-11-23 17:53:54 -0800

    ores: move LB setup for cloud from role to profile
    - replace hiera() with lookup()
    - all the code should be a profile and not a role
      so turn it into one
    - Still need to check the part about dropping str2bool
      and what Hiera values are actually set in Horizon (WIP)
    
    Change-Id: Ic6469c35800bd4f36131c06af7e1a00259c4e90a

---
+++ modules/profile/manifests/labs/ores/lb.pp
@@ -0,0 +1,29 @@
+# Simple nginx HTTP load balancer for ores
+#
+# filtertags: labs-project-ores
+class profile::labs::ores::lb (
+    Array $realservers = lookup('role::labs::ores::lb::realservers'),
+    Optional[Boolean] $cache = str2bool(lookup('role::labs::ores::lb::cache',{'default_value' => ''})),
+){
+
+    labs_lvm::volume { 'srv':
+        mountat => '/srv',
+    }
+
+    if $cache {
+        file { [
+            '/srv/nginx/',
+            '/srv/nginx/cache',
+            '/srv/nginx/tmp',
+        ]:
+            ensure => directory,
+            owner  => 'www-data',
+            group  => 'www-data',
+            mode   => '0775',
+        }
+    }
+    nginx::site { 'ores-lb':
+        content => template('role/ores/lb.nginx.erb'),
+        require => Labs_lvm::Volume['srv'],
+    }
+}
--- modules/role/manifests/labs/ores/lb.pp
+++ modules/role/manifests/labs/ores/lb.pp
@@ -2,26 +2,5 @@
 #
 # filtertags: labs-project-ores
 class role::labs::ores::lb {
-    labs_lvm::volume { 'srv':
-        mountat => '/srv',
-    }
-    $realservers = hiera('role::labs::ores::lb::realservers')
-    $cache = str2bool(hiera('role::labs::ores::lb::cache', ''))
-
-    if $cache {
-        file { [
-            '/srv/nginx/',
-            '/srv/nginx/cache',
-            '/srv/nginx/tmp',
-        ]:
-            ensure => directory,
-            owner  => 'www-data',
-            group  => 'www-data',
-            mode   => '0775',
-        }
-    }
-    nginx::site { 'ores-lb':
-        content => template('role/ores/lb.nginx.erb'),
-        require => Labs_lvm::Volume['srv'],
-    }
+    include profile::labs::ores::lb
 }
