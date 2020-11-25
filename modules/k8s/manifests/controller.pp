4d02dd4f07ce56303aaefd407e80c725def7f22c
Alexandros Kosiaris <akosiaris@wikimedia.org>
2020-11-25 12:51:33 +0200

    k8s::controller: Define --logtostderr=true and --v=0
    Why:
    We decided to have the configuration solely generated by puppet to
    remove confusion.
    
    What:
    Define --logtostderr=true, --v=0 and --leader-elect=true as those are
    the settings that were derived got from the old /etc/kubernetes/config.
    
    While at it, add an optional, defaulting to undef, $kubeconfig variable
    that will allow us to switch to using that instead of relying on local
    master privileged port
    
    Change-Id: Ia662dddfb9eba9fa67d19dd3936cabee8cb9c928

--- modules/k8s/manifests/controller.pp
+++ modules/k8s/manifests/controller.pp
@@ -1,6 +1,15 @@
+# Class that sets up and configures kube-controller-manager
 class k8s::controller(
+    Boolean $use_service_account_credentials=false,
+    Boolean $logtostderr=true,
+    Integer $v_log_level=0,
     Optional[String] $service_account_private_key_file=undef,
+    Optional[String] $kubeconfig=undef,
 ){
+
+    if $use_service_account_credentials and !$service_account_private_key_file {
+        fail('Need service_account_private_key_file set if use_service_account_credentials is to be used')
+    }
 
     require_package('kubernetes-master')
 
