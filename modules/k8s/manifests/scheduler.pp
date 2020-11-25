2e8b6744a05c0fc5fce3ef43f02c8b3a0a0c7521
Alexandros Kosiaris <akosiaris@wikimedia.org>
2020-11-25 11:50:42 +0200

    k8s::scheduler: Define --logtostderr=true and --v=0
    Why:
    We decided to have the configuration solely generated by puppet to
    remove confusion.
    
    What:
    Define --logtostderr=true, --v=0 and --leader-elect=true as those are
    the settings that were derived got from the old /etc/kubernetes/config.
    
    While at it, add an optional, defaulting to undef, $kubeconfig variable
    that will allow us to switch to using that instead of relying on local
    master privileged port
    
    Change-Id: I9be52ccdedbabc1bcc37127a8493a2cc2da9fec6

--- modules/k8s/manifests/scheduler.pp
+++ modules/k8s/manifests/scheduler.pp
@@ -1,4 +1,9 @@
-class k8s::scheduler {
+#  Class that sets up and configures kube-scheduler
+class k8s::scheduler(
+    Boolean $logtostderr = true,
+    Integer $v_log_level = 0,
+    Optional[String] $kubeconfig = undef,
+) {
 
     require_package('kubernetes-master')
 
