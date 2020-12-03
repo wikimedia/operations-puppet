ca8a2547693c1b2239652a4507e743cefa5f48d2
Janis Meybohm <jmeybohm@wikimedia.org>
2020-12-03 15:11:45 +0100

    Remove calico::builder
    We no longer build calico this way so calico::builder profile and role can be
    removed now.
    
    Bug: T266893
    Change-Id: I1f324ddf03fe677979f680e5e1abfa7f6483bfc8

--- modules/role/manifests/calico/builder.pp
+++
@@ -1,7 +0,0 @@
-# filtertags: labs-project-packaging
-class role::calico::builder {
-    include ::profile::standard
-    include ::profile::docker::storage::loopback
-    include ::profile::docker::engine
-    include ::profile::calico::builder
-}
