2fca3fb6914b07de7db5ad77418e842ca766eb79
Andrew Bogott <abogott@wikimedia.org>
2020-12-09 12:00:42 -0600

    Cinder: install ceph client keyring
    Bug: T269511
    Change-Id: I569f90ae7f95c6ef415d5faf02cb4518492f4301

--- modules/profile/manifests/openstack/codfw1dev/cinder.pp
+++ modules/profile/manifests/openstack/codfw1dev/cinder.pp
@@ -8,6 +8,7 @@ class profile::openstack::codfw1dev::cinder(
     String $ceph_pool = lookup('profile::openstack::codfw1dev::cinder::ceph_pool'),
     String $rabbit_pass = lookup('profile::openstack::codfw1dev::nova::rabbit_pass'),
     String $ldap_user_pass = lookup('profile::openstack::codfw1dev::cinder::ldap_user_pass'),
+    String $ceph_client_keydata = lookup('profile::ceph::client::rbd::cinder_client_keydata'),
     ) {
 
     class {'::profile::openstack::base::cinder':
@@ -21,5 +22,17 @@ class profile::openstack::codfw1dev::cinder(
         ldap_user_pass        => $ldap_user_pass,
         rabbit_pass           => $rabbit_pass,
         active                => true,
+    }
+
+    # The keydata used in this step is pre-created on one of the ceph mon hosts
+    # typically with the 'ceph auth get-or-create' command
+    file { '/etc/ceph/ceph.client.codfw1dev-cinder.keyring':
+        ensure    => present,
+        mode      => '0440',
+        owner     => cinder,
+        group     => cinder,
+        content   => "[client.codfw1dev-cinder]\n        key = ${ceph_client_keydata}\n",
+        show_diff => false,
+        require   => Package['ceph-common'],
     }
 }
--- modules/profile/manifests/openstack/eqiad1/cinder.pp
+++ modules/profile/manifests/openstack/eqiad1/cinder.pp
@@ -8,6 +8,7 @@ class profile::openstack::eqiad1::cinder(
     String $ceph_pool = lookup('profile::openstack::eqiad1::cinder::ceph_pool'),
     String $rabbit_pass = lookup('profile::openstack::eqiad1::nova::rabbit_pass'),
     String $ldap_user_pass = lookup('profile::openstack::eqiad1::cinder::ldap_user_pass'),
+    String $ceph_client_keydata = lookup('profile::ceph::client::rbd::cinder_client_keydata'),
     ) {
 
     class {'::profile::openstack::base::cinder':
@@ -21,5 +22,17 @@ class profile::openstack::eqiad1::cinder(
         ldap_user_pass        => $ldap_user_pass,
         rabbit_pass           => $rabbit_pass,
         active                => true,
+    }
+
+    # The keydata used in this step is pre-created on one of the ceph mon hosts
+    # typically with the 'ceph auth get-or-create' command
+    file { '/etc/ceph/ceph.client.eqiad1-cinder.keyring':
+        ensure    => present,
+        mode      => '0440',
+        owner     => cinder,
+        group     => cinder,
+        content   => "[client.eqiad1-cinder]\n        key = ${ceph_client_keydata}\n",
+        show_diff => false,
+        require   => Package['ceph-common'],
     }
 }
