6f7e503457a516719558c25b3ea454055d0ae636
Cas Rusnov <crusnov@wikimedia.org>
2020-12-09 13:42:29 -0800

    icinga/raid.pp: Add Python3 requirements for raid_handler.py
    As required to test (and in the future to deploy) the Python3 version
    of raid_handler, this adds the python3-phabricator package as a requirement.
    
    Bug: T247364
    Change-Id: I53735168f297a124fff1aae50d55da070ae1797f

--- modules/icinga/manifests/event_handlers/raid.pp
+++ modules/icinga/manifests/event_handlers/raid.pp
@@ -14,9 +14,7 @@ class icinga::event_handlers::raid (
         group    => $icinga_group,
     }
 
-    package { 'python-phabricator':
-        ensure => 'present',
-    }
+    ensure_packages(['python-phabricator', 'python3-phabricator'])
 
     file { '/usr/lib/nagios/plugins/eventhandlers/raid_handler':
         source  => 'puppet:///modules/icinga/raid_handler.py',
