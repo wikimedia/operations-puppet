8a2d38bc28fc4408227d42cc6e86464f2d37b32b
Daniel Zahn <dzahn@wikimedia.org>
2020-12-09 16:39:09 -0800

    wikistats: redirect output of mysqldump command properly
    follow-up to I44ac35f66ca3ac and Iffd53199d15
    
    stdout must be redirected properly
    
    Change-Id: I17c4b5647914fdb8cf3e82c9a7dad03ff79c2e80

--- modules/wikistats/files/dbdump.sh
+++ modules/wikistats/files/dbdump.sh
@@ -2,5 +2,5 @@
 # create a DB backup with mysqldump and compress it
 source /etc/wikistats/dbdump.cfg
 DATESTRING=$(date +%Y%m%d)
-${MYSQLDUMP} -u root ${DBNAME} ${BACKUPDIR}/${DUMPNAME}_${DATESTRING}.sql
+${MYSQLDUMP} -u root ${DBNAME} > ${BACKUPDIR}/${DUMPNAME}_${DATESTRING}.sql
 /bin/gzip ${BACKUPDIR}/${DUMPNAME}_${DATESTRING}.sql
