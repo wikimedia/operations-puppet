1ac7c5b327f3e785d6f6b4019cb90932e0aec433
John Bond <jbond@wikimedia.org>
2020-12-14 14:51:44 +0100

    cfssl::ocsp: use correct flag for responses file
    Change-Id: I51f608af8e30108054fd044599f8aa2c0e92dd8c

--- modules/cfssl/manifests/ocsp.pp
+++ modules/cfssl/manifests/ocsp.pp
@@ -65,7 +65,7 @@ define cfssl::ocsp (
     $refresh_command = @("CMD"/L)
         /usr/local/sbin/cfssl-ocsprefresh \
         --responder-cert ${cert_path} --responder-key ${key_path} \
-        --ca-file ${_ca_file} --responses_file ${_responses_file} \
+        --ca-file ${_ca_file} --responses-file ${_responses_file} \
         --restart-service '${serve_service}' ${safe_title} \
         | CMD
 
