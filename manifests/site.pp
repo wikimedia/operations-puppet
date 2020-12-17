f3f90d88f4c9c3400d3e0dc00c8548084cf41daa
Luca Toscano <ltoscano@wikimedia.org>
2020-12-17 14:20:13 +0100

    Restore puppet roles for the Hadoop test cluster after reimage
    Bug: T269919
    Change-Id: I6507b55c1ac52736a40ba6c406688f8b51c1b5dc

--- manifests/site.pp
+++ manifests/site.pp
@@ -50,20 +50,20 @@ node 'an-launcher1002.eqiad.wmnet' {
 
 # Analytics Hadoop test cluster
 node 'an-test-master1001.eqiad.wmnet' {
-    role(insetup)
+    role(analytics_test_cluster::hadoop::master)
 }
 
 node 'an-test-master1002.eqiad.wmnet' {
-    role(insetup)
+    role(analytics_test_cluster::hadoop::standby)
 }
 
 node /^an-test-worker100[1-3]\.eqiad\.wmnet$/ {
-    role(insetup)
+    role(analytics_test_cluster::hadoop::worker)
 }
 
 # new an-test-coord1001  T255518
 node 'an-test-coord1001.eqiad.wmnet' {
-    role(insetup)
+    role(analytics_test_cluster::coordinator)
 }
 
 node 'an-test-client1001.eqiad.wmnet' {
