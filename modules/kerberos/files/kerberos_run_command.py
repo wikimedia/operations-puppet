0218f1abc90fc822a7e9a32e06023370b5175032
Luca Toscano <ltoscano@wikimedia.org>
2020-12-18 14:55:34 +0100

    kerberos: rollback settings for ccache in /run/user/$uid
    Rollback the test on stat1004 (leaving it on for the hadoop test cluster)
    until we find a better solution.
    
    Bug: T255262
    Change-Id: I318f67e5acd346ade596d899ea6aadf350a175e5

--- modules/kerberos/files/kerberos_run_command.py
+++ modules/kerberos/files/kerberos_run_command.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import pwd
 import sys
 import socket
 import subprocess
@@ -70,13 +69,6 @@ def main():
         sys.exit(1)
 
     principal = "%s/%s@%s" % (run_as_user, fqdn, realm_name)
-
-    # Explicitly set KRB5CCNAME since it is a variable preserved by sudo.
-    # If the user that issues the sudo command has KRB5CCNAME set,
-    # then its value gets carried over and the sudoed user is forced to reuse
-    # it.
-    user_id = str(pwd.getpwnam(run_as_user).pw_uid)
-    os.environ['KRB5CCNAME'] = "/tmp/krb5cc_" + user_id
 
     logger.info(
         "kerberos-run-command: User {} executes as user {} the command {}"
