b6f7f1b39c031c30723c301b6d66e63f70c9df85
Daniel Zahn <dzahn@wikimedia.org>
2020-12-18 16:55:20 -0800

    pybaltest: convert to role/profile, hiera->lookup
    Roles should not have hiera lookups and hiera should
    be replaced with lookup.
    
    Convert to proper role/profile structure and move
    it into a profile parameter.
    
    Bug: T209953
    Change-Id: Id642064e588e1f7718aa05e34e5736cb1120d0a3

--- modules/role/manifests/pybaltest.pp
+++ modules/role/manifests/pybaltest.pp
@@ -5,35 +5,6 @@ class role::pybaltest {
 
     include ::profile::standard
     include ::profile::base::firewall
-    $pybaltest_hosts_ferm = join(hiera('pybaltest::hosts'), ' ')
-
-    ferm::service { 'pybaltest-http':
-        proto  => 'tcp',
-        port   => '80',
-        srange => "(@resolve((${pybaltest_hosts_ferm})) @resolve((${pybaltest_hosts_ferm}), AAAA))",
-    }
-
-    ferm::service { 'pybaltest-bgp':
-        proto  => 'tcp',
-        port   => '179',
-        srange => "(@resolve((${pybaltest_hosts_ferm})) @resolve((${pybaltest_hosts_ferm}), AAAA))",
-    }
-
-    # If the host considers itself as a router (IP forwarding enabled), it will
-    # ignore all router advertisements, breaking IPv6 SLAAC. Accept Router
-    # Advertisements even if forwarding is enabled.
-    sysctl::parameters { 'accept-ra':
-        values => {
-            "net.ipv6.conf.${facts['interface_primary']}.accept_ra" => 2,
-        },
-    }
-
-    # Install conftool-master for conftool testing
-    class  { '::puppetmaster::base_repo':
-        gitdir   => '/var/lib/git',
-        gitowner => 'root',
-    }
-
-
+    include ::profile::pybaltest
     include ::profile::conftool::master
 }
