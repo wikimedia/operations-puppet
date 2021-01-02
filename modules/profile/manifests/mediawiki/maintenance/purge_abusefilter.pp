90cf35159332763d4d78cf5e5b3d32d0c8c1a405
Daimona Eaytoy <daimona.wiki@gmail.com>
2021-01-02 20:59:05 +0100

    mediawiki: Temporarily add alternative path for AbuseFilter script
    Renamed in Ifcc2bff9e400fde.
    
    Change-Id: Iaf3c218abd5578d604a89634c38d9156bb19a0d2

--- modules/profile/manifests/mediawiki/maintenance/purge_abusefilter.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_abusefilter.pp
@@ -1,6 +1,7 @@
 class profile::mediawiki::maintenance::purge_abusefilter {
     profile::mediawiki::periodic_job { 'purge_abusefilteripdata':
-        command  => '/usr/local/bin/foreachwiki extensions/AbuseFilter/maintenance/purgeOldLogIPData.php',
+        # Remove old name once Ifcc2bff9e40 is deployed everywhere
+        command  => '[ -f extensions/AbuseFilter/maintenance/purgeOldLogIPData.php ] && /usr/local/bin/foreachwiki extensions/AbuseFilter/maintenance/purgeOldLogIPData.php || /usr/local/bin/foreachwiki extensions/AbuseFilter/maintenance/PurgeOldLogIPData.php',
         interval => '01:15'
     }
 }
