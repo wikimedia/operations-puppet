fe076046623b1ba43ea74cc1495d50c3d0c000f3
Legoktm <legoktm@member.fsf.org>
2021-01-05 01:36:44 +0000

    Revert "mediawiki: Temporarily add alternative path for AbuseFilter script"
    This reverts commit 90cf35159332763d4d78cf5e5b3d32d0c8c1a405.
    
    Reason for revert: Not working
    
    Change-Id: I444fa06e4d67b31ba7b041078bbfde0f8d6fe3c3

--- modules/profile/manifests/mediawiki/maintenance/purge_abusefilter.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_abusefilter.pp
@@ -1,7 +1,6 @@
 class profile::mediawiki::maintenance::purge_abusefilter {
     profile::mediawiki::periodic_job { 'purge_abusefilteripdata':
-        # Remove old name once Ifcc2bff9e40 is deployed everywhere
-        command  => '[ -f extensions/AbuseFilter/maintenance/purgeOldLogIPData.php ] && /usr/local/bin/foreachwiki extensions/AbuseFilter/maintenance/purgeOldLogIPData.php || /usr/local/bin/foreachwiki extensions/AbuseFilter/maintenance/PurgeOldLogIPData.php',
+        command  => '/usr/local/bin/foreachwiki extensions/AbuseFilter/maintenance/purgeOldLogIPData.php',
         interval => '01:15'
     }
 }
