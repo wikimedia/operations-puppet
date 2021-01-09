71e23dd49c4d61d70bc0b2f179fe1c342420a065
Kunal Mehta <legoktm@member.fsf.org>
2021-01-08 18:06:23 -0800

    zuul: Switch require_package -> ensure_packages
    Bug: T266479
    Change-Id: Icd673573c0066ad31e754a82e8060ee4ba58984c

--- modules/zuul/manifests/init.pp
+++ modules/zuul/manifests/init.pp
@@ -11,7 +11,7 @@ class zuul ( ){
 
     include zuul::user
 
-    require_package('virtualenv', 'make')
+    ensure_packages(['virtualenv', 'make'])
 
     # Both merger and server require the zuul class
     if !defined(Scap::Target['zuul/deploy']) {
