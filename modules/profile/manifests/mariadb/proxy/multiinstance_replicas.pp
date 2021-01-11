52c9702ffd48011c8e7f7ddc922c81bc16895505
Brooke Storm <bstorm@wikimedia.org>
2021-01-11 12:59:41 -0700

    wikireplicas: enable standby behavior on multiinstance proxies
    This is for the physical server (production realm) proxies. The intent
    is to generate the hash for the other "replica_type" with an additional
    hash key of "standby" to generate appropriate config from.
    
    Standby servers would still be removed by the inclusion of the server in
    the overrides hiera with a "depooled" key.
    
    Since the config doesn't interfere with the legacy systems on the proxy
    servers, we should be pretty safe to play with this and the overrides
    unless puppet just fails.
    
    Bug: T271476
    Change-Id: I617d85c07198ee7f7e00e0e2daa876f23d50abc0

--- modules/profile/manifests/mariadb/proxy/multiinstance_replicas.pp
+++ modules/profile/manifests/mariadb/proxy/multiinstance_replicas.pp
@@ -18,18 +18,41 @@ class profile::mariadb::proxy::multiinstance_replicas(
                     ['fqdn', 'ipaddress']
                 )}
     }
+
+    # Generate a hash of default standby servers for each section, which is the
+    # "other" replica type.
+    $standby_type = $replica_type ? {
+        'web'       => 'analytics',
+        'analytics' => 'web',
+    }
+    $base_standby_servers = $replica_sections.reduce({}) |$memo, $section|{
+        $memo + {$section =>  query_facts(
+                    "Class['role::wmcs::db::wikireplicas::${standby_type}_multiinstance'] and Profile::Mariadb::Section[${section}]",
+                    ['fqdn', 'ipaddress']
+                )}
+    }
+    # Inserting a hash key into an existing nested hash in puppet is interesting
+    $default_standby_servers = $base_standby_servers.reduce({}) |$memo, $section|{
+        $memo + {$section[0] => $section[1].reduce({}) |$x, $config| {
+            $x + {$config[0] => $config[1] + {'standby' => true}}
+            }
+        }
+    }
     # This next part is mostly necessary so that the puppet catalog compiler
     # works as expected. Without it, it either errors or does odd things because
     # it lacks puppetdb.
     $scrubbed_servers = $default_backend_servers.filter |$section|{
         !$section[1].empty
     }
+    $scrubbed_standbys = $default_standby_servers.filter |$section|{
+        !$section[1].empty
+    }
 
     # Merge $section_overrides to provide weights and depoolings
     if $section_overrides {
-        $section_servers = $scrubbed_servers + $section_overrides
+        $section_servers = $scrubbed_servers + $scrubbed_standbys + $section_overrides
     } else {
-        $section_servers = $scrubbed_servers
+        $section_servers = $scrubbed_servers + $scrubbed_standbys
     }
     file { '/etc/haproxy/conf.d/multi-db-replicas.cfg':
         owner   => 'root',
