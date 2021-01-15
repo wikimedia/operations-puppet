a06133284116aef30dab9577a5c9ace89dfe4430
joal <joal@wikimedia.org>
2021-01-15 10:52:09 +0100

    profile::analytics::refinery::job::hdfs_cleaner Update
    Add new HDFSCleaner timer instances for
    /wmf/tmp/druid and /wmf/tmp/analytics
    Make the already existing one absent and create a new one
    for detailed naming.
    Also update hieradata/role/common/analytics_cluster/launcher.yaml
    setting hdfs_cleaner::ensure_timer: present.
    
    Bug: T271560
    Change-Id: If72dc0ca6f40392cebb7c339249994d3420afafe

--- modules/profile/manifests/analytics/refinery/job/hdfs_cleaner.pp
+++ modules/profile/manifests/analytics/refinery/job/hdfs_cleaner.pp
@@ -1,6 +1,9 @@
-# == Class profile::hadoop::balancer
+# == Class profile::analytics::refinery::job::hdfs_cleaner
 #
-# Deletes files and empty directories older than 31 days from HDFS /tmp dir.
+# Deletes files and empty directories older than 31 days from HDFS dirs:
+#  - /tmp
+#  - /wmf/tmp/analytics
+#  - /wmf/tmp/druid
 #
 class profile::analytics::refinery::job::hdfs_cleaner(
     Wmflib::Ensure $ensure_timer  = lookup('profile::analytics::refinery::job::hdfs_cleaner::ensure_timer', { 'default_value' => 'present' }),
@@ -11,9 +14,44 @@ class profile::analytics::refinery::job::hdfs_cleaner(
     Class['::profile::hadoop::common'] -> Class['::profile::analytics::refinery::job::hdfs_cleaner']
 
     $older_than_threshold = 2678400 # seconds in 31 days
+
+    $command_tmp = "${::profile::analytics::refinery::path}/bin/hdfs-cleaner --path=/tmp --older_than_seconds=${older_than_threshold}"
+    kerberos::systemd_timer { 'hdfs-cleaner-tmp':
+        ensure          => $ensure_timer,
+        description     => 'Run the HDFSCleaner job to keep HDFS /tmp dir clean of old files.',
+        command         => $command_tmp,
+        interval        => '*-*-* 23:00:00',
+        logfile_name    => 'hdfs-cleaner-tmp.log',
+        logfile_basedir => '/var/log/hadoop-hdfs',
+        user            => 'hdfs',
+    }
+
+    $command_tmp_analytics = "${::profile::analytics::refinery::path}/bin/hdfs-cleaner --path=/wmf/tmp/analytics --older_than_seconds=${older_than_threshold}"
+    kerberos::systemd_timer { 'hdfs-cleaner-tmp-analytics':
+        ensure          => $ensure_timer,
+        description     => 'Run the HDFSCleaner job to keep HDFS /wmf/tmp/analytics dir clean of old files.',
+        command         => $command_tmp_analytics,
+        interval        => '*-*-* 23:15:00',
+        logfile_name    => 'hdfs-cleaner-tmp-analytics.log',
+        logfile_basedir => '/var/log/hadoop-hdfs',
+        user            => 'hdfs',
+    }
+
+    $command_tmp_druid = "${::profile::analytics::refinery::path}/bin/hdfs-cleaner --path=/wmf/tmp/druid --older_than_seconds=${older_than_threshold}"
+    kerberos::systemd_timer { 'hdfs-cleaner-tmp-druid':
+        ensure          => $ensure_timer,
+        description     => 'Run the HDFSCleaner job to keep HDFS /wmf/tmp/druid dir clean of old files.',
+        command         => $command_tmp_druid,
+        interval        => '*-*-* 23:30:00',
+        logfile_name    => 'hdfs-cleaner-tmp-druid.log',
+        logfile_basedir => '/var/log/hadoop-hdfs',
+        user            => 'hdfs',
+    }
+
+    # Ensuring absent the old existing time to faciliate puppet cleaning
     $command = "${::profile::analytics::refinery::path}/bin/hdfs-cleaner --path=/tmp --older_than_seconds=${older_than_threshold}"
     kerberos::systemd_timer { 'hdfs-cleaner':
-        ensure          => $ensure_timer,
+        ensure          => 'absent',
         description     => 'Run the HDFSCleaner job to keep HDFS /tmp dir clean of old files.',
         command         => $command,
         interval        => '*-*-* 23:00:00',
