b31cf73e7ad3a56289195f5c2d7b9e031f672cfc
Amir Sarabadani <Ladsgroup@gmail.com>
2021-01-16 03:33:36 +0100

    query_service: Migrate hiera() to lookup() in gui
    Part of upgrading to puppet 6
    https://puppet.com/docs/puppet/5.5/hiera_migrate.html#updated-classic-hiera-function-calls
    
    Bug: T209953
    Change-Id: Iac9b7baeaacad5d09825deb4f85bc23c3527c930

--- modules/profile/manifests/query_service/gui.pp
+++ modules/profile/manifests/query_service/gui.pp
@@ -1,13 +1,13 @@
 class profile::query_service::gui (
     String $username = lookup('profile::query_service::username'),
-    Stdlib::Unixpath $package_dir = hiera('profile::query_service::package_dir'),
-    Stdlib::Unixpath $data_dir = hiera('profile::query_service::data_dir'),
-    Stdlib::Unixpath $log_dir = hiera('profile::query_service::log_dir'),
-    Query_service::DeployMode $deploy_mode = hiera('profile::query_service::deploy_mode'),
-    String $deploy_name = hiera('profile::query_service::deploy_name'),
-    Boolean $enable_ldf = hiera('profile::query_service::enable_ldf', false),
-    Integer $max_query_time_millis = hiera('profile::query_service::max_query_time_millis', 60000),
-    Boolean $high_query_time_port = hiera('profile::query_service::high_query_time_port', false),
+    Stdlib::Unixpath $package_dir = lookup('profile::query_service::package_dir'),
+    Stdlib::Unixpath $data_dir = lookup('profile::query_service::data_dir'),
+    Stdlib::Unixpath $log_dir = lookup('profile::query_service::log_dir'),
+    Query_service::DeployMode $deploy_mode = lookup('profile::query_service::deploy_mode'),
+    String $deploy_name = lookup('profile::query_service::deploy_name'),
+    Boolean $enable_ldf = lookup('profile::query_service::enable_ldf', {default_value => false}),
+    Integer $max_query_time_millis = lookup('profile::query_service::max_query_time_millis', {default_value => 60000}),
+    Boolean $high_query_time_port = lookup('profile::query_service::high_query_time_port', {default_value => false}),
     String $blazegraph_main_ns = lookup('profile::query_service::blazegraph_main_ns'),
     Boolean $oauth = lookup('profile::query_service::oauth')
 ) {
