b2f7986ffbfc3f81b0dc53d6a773a72f26d8e0fe
Stevie Shirley <sshirley@wikimedia.org>
2021-01-19 15:05:29 +0100

    orchestrator: Add sslcert:dhparam
    Bug: T266106
    Change-Id: I071a788633186f292255e2b80faf80839d55a99f

--- modules/profile/manifests/orchestrator/web.pp
+++ modules/profile/manifests/orchestrator/web.pp
@@ -2,6 +2,7 @@ class profile::orchestrator::web {
     class { '::httpd':
         modules => ['headers', 'proxy', 'proxy_http', 'rewrite', 'ssl']
     }
+    class { 'sslcert::dhparam': }
 
     $ssl_settings = ssl_ciphersuite('apache', 'strong', true)
     include profile::idp::client::httpd
