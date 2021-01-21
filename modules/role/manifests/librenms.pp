d2848969b81a360e9010bdd845171f6117dbb5c1
Arzhel Younsi <ayounsi@wikimedia.org>
2021-01-21 12:30:40 +0100

    Remove unused roles librenms and rancid
    Bug: T272559
    Change-Id: I140a4852b33b3a046ed41cb92a8910b6be65dbe9

--- modules/role/manifests/librenms.pp
+++
@@ -1,6 +0,0 @@
-# https://www.librenms.org/ | https://github.com/librenms/librenms
-class role::librenms {
-    system::role { 'librenms': description => 'LibreNMS' }
-
-    include ::profile::librenms
-}
