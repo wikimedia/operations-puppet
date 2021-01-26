e29ec2ac3052ba876a544c91a6c624b17f9074df
Andrew Bogott <abogott@wikimedia.org>
2021-01-26 12:59:25 -0600

    Add manifests for Keystone version Train
    Bug: T261135
    Change-Id: I04b29fe8a452cb5d1f9f46726214ca58b25370fe

---
+++ modules/openstack/manifests/keystone/service/train.pp
@@ -0,0 +1,126 @@
+class openstack::keystone::service::train(
+    $controller_hosts,
+    $osm_host,
+    $db_name,
+    $db_user,
+    $db_pass,
+    $db_host,
+    $db_max_pool_size,
+    $public_workers,
+    $admin_workers,
+    $ldap_hosts,
+    $ldap_base_dn,
+    $ldap_user_id_attribute,
+    $ldap_user_name_attribute,
+    $ldap_user_dn,
+    $ldap_user_pass,
+    $region,
+    $auth_protocol,
+    $auth_port,
+    $wiki_status_page_prefix,
+    $wiki_status_consumer_token,
+    $wiki_status_consumer_secret,
+    $wiki_status_access_token,
+    $wiki_status_access_secret,
+    $wiki_consumer_token,
+    $wiki_consumer_secret,
+    $wiki_access_token,
+    $wiki_access_secret,
+    String $wsgi_server,
+    Stdlib::IP::Address::V4::CIDR $instance_ip_range,
+    String $wmcloud_domain_owner,
+    String $bastion_project_id,
+) {
+    class { "openstack::keystone::service::train::${::lsbdistcodename}": }
+
+    include ::network::constants
+    $prod_networks = $network::constants::production_networks
+    $labs_networks = $network::constants::labs_networks
+
+    # Fernet key count.  We rotate once per day on each host.  That means that
+    #  for our keys to live a week, we need at least 7*(number of hosts) keys
+    #  at any one time.  Using 9 here instead because it costs us nothing
+    #  and provides ample slack.
+    $max_active_keys = $controller_hosts.length * 9
+
+    file {
+        '/etc/logrotate.d/keystone':
+            ensure  => 'present',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            source  => 'puppet:///modules/openstack/train/keystone/keystone_logrotate',
+            require => Package['keystone'];
+        '/etc/keystone/keystone.conf':
+            ensure    => 'present',
+            owner     => 'keystone',
+            group     => 'keystone',
+            mode      => '0444',
+            show_diff => false,
+            content   => template('openstack/train/keystone/keystone.conf.erb'),
+            notify    => Service[$wsgi_server],
+            require   => Package['keystone'];
+        '/etc/keystone/keystone-paste.ini':
+            ensure  => 'present',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            source  => 'puppet:///modules/openstack/train/keystone/keystone-paste.ini',
+            notify  => Service[$wsgi_server],
+            require => Package['keystone'];
+        '/etc/keystone/policy.json':
+            ensure  => 'absent';
+        '/etc/keystone/policy.yaml':
+            ensure  => 'present',
+            mode    => '0644',
+            owner   => 'root',
+            group   => 'root',
+            source  => 'puppet:///modules/openstack/train/keystone/policy.yaml',
+            notify  => Service[$wsgi_server],
+            require => Package['keystone'];
+        '/etc/keystone/logging.conf':
+            ensure  => 'present',
+            source  => 'puppet:///modules/openstack/train/keystone/logging.conf',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            notify  => Service[$wsgi_server],
+            require => Package['keystone'];
+        '/etc/keystone/keystone.my.cnf':
+            ensure    => 'present',
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0400',
+            show_diff => false,
+            content   => template('openstack/train/keystone/keystone.my.cnf.erb');
+        '/usr/lib/python3/dist-packages/wmfkeystoneauth':
+            ensure  => 'present',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            source  => 'puppet:///modules/openstack/train/keystone/wmfkeystoneauth',
+            notify  => Service[$wsgi_server],
+            recurse => true;
+        '/usr/lib/python3/dist-packages/wmfkeystoneauth.egg-info':
+            ensure  => 'present',
+            source  => 'puppet:///modules/openstack/train/keystone/wmfkeystoneauth.egg-info',
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            notify  => Service[$wsgi_server],
+            recurse => true;
+    }
+
+
+    # Keystone is managed via apache/wsgi so we don't
+    #  want the systemd unit running.
+    exec { 'mask_keystone_service':
+        command => '/bin/systemctl mask keystone.service',
+        creates => '/etc/systemd/system/keystone.service',
+        require => Package['keystone'];
+    }
+    service {'keystone':
+        ensure  => 'stopped',
+        require => Package['keystone'];
+    }
+}
