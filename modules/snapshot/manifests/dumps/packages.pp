61d3bbd5d999fde4d44173a342d189d4c1a04db1
Ariel T. Glenn <ariel@wikimedia.org>
2021-01-27 14:01:16 +0200

    include correct mysql client package for dumps on buster
    Change-Id: I83a3cd841f424d9aab4fdc408f392dba27a2e3c9

--- modules/snapshot/manifests/dumps/packages.pp
+++ modules/snapshot/manifests/dumps/packages.pp
@@ -1,7 +1,12 @@
 class snapshot::dumps::packages {
+    $mysql_client_pkg = case $::lsbdistcodename {
+        'buster': { 'default-mysql-client' }
+        'stretch': { 'mysql-client' }
+        default: { fail("Unsupported distro ${::lsbdistcodename}") }
+    }
     ensure_packages(['mwbzutils',
-                    'mysql-client',
                     'p7zip-full',
+                    $mysql_client_pkg,
                     'lbzip2',
                     'python3-yaml'])
 }
