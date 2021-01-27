218e8c5d2c986ef9a508753b217f451ec1872196
Daniel Zahn <dzahn@wikimedia.org>
2021-01-27 12:02:53 -0800

    tlsproxy::prometheus: hiera->lookup
    Bug: T209953
    Change-Id: I08e792b8885da4ffadc63d04483dd094465cd23b

--- modules/tlsproxy/manifests/prometheus.pp
+++ modules/tlsproxy/manifests/prometheus.pp
@@ -1,7 +1,7 @@
 # == Class: tlsproxy::prometheus
 
 class tlsproxy::prometheus {
-    $prometheus_nodes = hiera('prometheus_nodes', [])
+    $prometheus_nodes = lookup('prometheus_nodes', {'default_value' => []})
 
     nginx::site { 'tlsproxy-prometheus':
         content => template('tlsproxy/prometheus-nginx.conf.erb'),
