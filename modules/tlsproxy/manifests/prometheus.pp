5df429b71c643d2e5d84862ca1eff6e3a956b2d5
Daniel Zahn <dzahn@wikimedia.org>
2021-01-28 10:55:05 -0800

    delete class tlsproxy::prometheus and nginx template
    This class has been created in 2017 by Ema but nowadays
    nothing seems to use or include it.
    
    Can we remove it?
    
    Bug: T272559
    Change-Id: I0aeed2a4b9cdfeff491ae9f0a7d49300032d5f1d

--- modules/tlsproxy/manifests/prometheus.pp
+++
@@ -1,9 +0,0 @@
-# == Class: tlsproxy::prometheus
-
-class tlsproxy::prometheus {
-    $prometheus_nodes = lookup('prometheus_nodes', {'default_value' => []})
-
-    nginx::site { 'tlsproxy-prometheus':
-        content => template('tlsproxy/prometheus-nginx.conf.erb'),
-    }
-}
