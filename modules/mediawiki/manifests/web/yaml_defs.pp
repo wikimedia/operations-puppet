8c1c2d294bfead2656b9c437527a62aaf2bf45f0
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2021-01-29 17:09:44 +0100

    kubernetes::deployment_server: add yaml to configure MediaWiki sites
    Bug: T272305
    Change-Id: I831ee85bd6fd1c20f0006dc1018771ebbdb075f6

---
+++ modules/mediawiki/manifests/web/yaml_defs.pp
@@ -0,0 +1,39 @@
+class mediawiki::web::yaml_defs(
+    Stdlib::Unixpath $path,
+    Array[Mediawiki::SiteCollection] $siteconfigs,
+    String $domain_suffix,
+    String $fcgi_proxy,
+) {
+    $all_defs = $siteconfigs.map |$siteconfig| {
+        # the eventual disk path is used as a key, so that
+        # the resulting data structure fits well as a configmap.
+        if 'vhosts' in $siteconfig {
+            # If we have a vhost, we have everything in the yaml.
+            # just copy it over.
+            $siteconfig
+        } elsif $siteconfig['source'] {
+            # Get the contents of the source file
+            $source_url = "puppet:///modules/${siteconfig['source']}"
+            $sc = {
+                'name' => $siteconfig['name'],
+                'priority' => $siteconfig['priority'],
+                'content' => inline_template("<%= Puppet::FileServing::Content.indirection.find(@source_url).content.force_encoding('utf-8') %>")
+            }
+            $sc
+        } elsif $siteconfig['template'] {
+            {
+                'name' => $siteconfig['name'],
+                'priority' => $siteconfig['priority'],
+                'content' => template($siteconfig['template'])
+            }
+        }
+    }
+
+    file { $path:
+        ensure  => present,
+        content => to_yaml({'mediawiki' => {'sites' => $all_defs}}),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+    }
+}
