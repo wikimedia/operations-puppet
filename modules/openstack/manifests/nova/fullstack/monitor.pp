546e095fa3ebbecd483039c69eef3ccf8865e650
Andrew Bogott <abogott@wikimedia.org>
2021-01-31 00:40:42 -0600

    Update the nova-fullstack monitoring to expect python3
    Bug: T272587
    Change-Id: Ib5d266945beb842bb84972c7fa6b4cfce69dd551

--- modules/openstack/manifests/nova/fullstack/monitor.pp
+++ modules/openstack/manifests/nova/fullstack/monitor.pp
@@ -2,7 +2,7 @@ class openstack::nova::fullstack::monitor {
 
     nrpe::monitor_service { 'nova-fullstack':
         description   => 'nova instance creation test',
-        nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 1: -C python -a nova-fullstack',
+        nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 1: -C python3 -a nova-fullstack',
         contact_group => 'wmcs-team,admins',
         retries       => 2,
         notes_url     => 'https://wikitech.wikimedia.org/wiki/Portal:Cloud_VPS/Admin/Troubleshooting',
