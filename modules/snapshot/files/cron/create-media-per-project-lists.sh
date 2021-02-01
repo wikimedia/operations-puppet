53fc6b39b09193dc489cbe6c0d35b60885e31574
Ariel T. Glenn <ariel@wikimedia.org>
2021-02-01 12:48:19 +0200

    Make media lists dump easily runnable in deployment-prep
    Bug: T269377
    Change-Id: I9bbb0c4f7b1a2e7e662ae8ba0f5829c3903f10a2

--- modules/snapshot/files/cron/create-media-per-project-lists.sh
+++ modules/snapshot/files/cron/create-media-per-project-lists.sh
@@ -7,17 +7,45 @@
 
 source /usr/local/etc/set_dump_dirs.sh
 
+usage() {
+    echo "Usage: $0 [--config <pathtofile>] [--dryrun]" >& 2
+    echo >& 2
+    echo "  --config   path to configuration file for dump generation" >& 2
+    echo "             (default value: ${confsdir}/wikidump.conf.dumps:media" >& 2
+    echo "  --dryrun   don't run dump, show what would have been done" >& 2
+    exit 1
+}
+
+configfile="${confsdir}/wikidump.conf.dumps:media"
+dryrun="false"
+
+while [ $# -gt 0 ]; do
+    if [ $1 == "--config" ]; then
+        configfile="$2"
+        shift; shift
+    elif [ $1 == "--dryrun" ]; then
+        dryrun="true"
+        shift
+    else
+        echo "$0: Unknown option $1" >& 2
+        usage
+    fi
+done
+
 DATE=`/bin/date '+%Y%m%d'`
 outputdir="${cronsdir}/imageinfo/$DATE"
-configfile="${confsdir}/wikidump.conf.dumps:media"
 errors=0
 
 cd "$repodir"
 
-python3 "${repodir}/onallwikis.py" --outdir "$outputdir" \
-       --config "$configfile" --nooverwrite \
-       --query "'select img_name, img_timestamp from image;'" \
-       --filenameformat "{w}-{d}-local-wikiqueries.gz"
+if [ "$dryrun" == "true" ]; then
+    echo python3 "${repodir}/onallwikis.py" --outdir "$outputdir" --config "$configfile" --nooverwrite \
+	 --query "'select img_name, img_timestamp from image;'" --filenameformat "{w}-{d}-local-wikiqueries.gz"
+else
+    python3 "${repodir}/onallwikis.py" --outdir "$outputdir" --config "$configfile" --nooverwrite \
+       --query "'select img_name, img_timestamp from image;'" --filenameformat "{w}-{d}-local-wikiqueries.gz"
+fi
+
 if [ $? -ne 0 ]; then
     echo "failed sql dump of image tables"
     errors=1
@@ -27,11 +55,13 @@ fi
 globalusagelist="${dblistsdir}/globalusage.dblist"
 basewiki=`cat "$globalusagelist"`
 
-python3 "${repodir}/onallwikis.py" --outdir "$outputdir" \
-       --base "$basewiki" \
-       --config "$configfile" --nooverwrite \
-       --query "'select gil_to from globalimagelinks where gil_wiki= \"{w}\";'" \
-       --filenameformat "{w}-{d}-remote-wikiqueries.gz"
+if [ "$dryrun" == "true" ]; then
+    echo python3 "${repodir}/onallwikis.py" --outdir "$outputdir" --base "$basewiki" --config "$configfile" \
+       --nooverwrite --query "'select gil_to from globalimagelinks where gil_wiki= \"{w}\";'" --filenameformat "{w}-{d}-remote-wikiqueries.gz"
+else
+    python3 "${repodir}/onallwikis.py" --outdir "$outputdir" --base "$basewiki" --config "$configfile" \
+       --nooverwrite --query "'select gil_to from globalimagelinks where gil_wiki= \"{w}\";'" --filenameformat "{w}-{d}-remote-wikiqueries.gz"
+fi
 
 if [ $? -ne 0 ]; then
     echo "failed sql dump of globalimagelink tables"
