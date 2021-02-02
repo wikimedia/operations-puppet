3273a4473425c040b719d6ca909b315c51fc7b5f
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2021-02-02 09:57:45 +0100

    swift: apply interface::rps to i40e NICs
    interface::rps works fine in the i40e case as well as bnx NICs
    
    Bug: T271415
    Change-Id: I9f5af0b8363d425b5bed7f5fa09681f68c3a1d3e

--- modules/profile/manifests/swift/performance.pp
+++ modules/profile/manifests/swift/performance.pp
@@ -3,7 +3,7 @@ class profile::swift::performance {
     $iface_primary = $facts['interface_primary']
 
     # RPS/RSS to spread network i/o evenly (for 10Gbps primary interface only)
-    if $facts['net_driver'][$iface_primary]['driver'] in ['bnxt_en', 'bnx2x'] {
+    if $facts['net_driver'][$iface_primary]['driver'] in ['bnxt_en', 'bnx2x', 'i40e'] {
         interface::rps { 'primary':
             interface => $iface_primary,
         }
