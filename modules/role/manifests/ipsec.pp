5bb8e26245a88861da6e5c7e7eeffeaf4f44b1a4
Amir Sarabadani <Ladsgroup@gmail.com>
2021-02-02 11:21:56 +0100

    ipsec: Clean up the parts related to cache cluster
    The cache cluster doesn't use ipsec anymore.
    
    Bug: T241239
    Change-Id: Ic1dca39ed02e41e59e39b13e2a84809b01477a03

--- modules/role/manifests/ipsec.pp
+++ modules/role/manifests/ipsec.pp
@@ -10,51 +10,10 @@ class role::ipsec ($hosts = undef) {
 
     if $hosts != undef {
         $targets = $hosts
-    } else {
-        # The cache-cluster ipsec associations are still manually-defined, so
-        # any changes to cache cluster routing schemes beyond our present
-        # plans (which only have codfw or eqiad backing other caches) must
-        # make changes here to secure the traffic.
-        # The current ipsec association scheme below is basically:
-        #    eqiad <=> codfw
-        #    eqiad+codfw <=> esams+ulsfo+eqsin
-
-        if $::hostname =~ /^cp/ {
-            $ipsec_cluster = hiera('cache::cluster')
-            $cluster_nodes = hiera('cache::nodes')[$ipsec_cluster]
-
-            if $::site == 'esams' or $::site == 'ulsfo' or $::site == 'eqsin' {
-                $targets = array_concat(
-                    $cluster_nodes['eqiad'],
-                    $cluster_nodes['codfw']
-                )
-            } elsif $::site == 'codfw' {
-                $targets = array_concat(
-                    $cluster_nodes['esams'],
-                    $cluster_nodes['ulsfo'],
-                    $cluster_nodes['eqsin'],
-                    $cluster_nodes['eqiad']
-                )
-            } elsif $::site == 'eqiad' {
-                $targets = array_concat(
-                    $cluster_nodes['esams'],
-                    $cluster_nodes['ulsfo'],
-                    $cluster_nodes['eqsin'],
-                    $cluster_nodes['codfw']
-                )
-            }
-        }
-    }
-    # Change the MTU for all cp* servers, ignore the rest (mc*, etc)
-    if $::hostname =~ /^cp/ {
-        $mtu_value = 1450
-    } else {
-        $mtu_value = undef
     }
 
     class { '::strongswan':
         puppet_certname => $puppet_certname,
         hosts           => $targets.filter |$target| { $target != '' },
-        mtu_hosts       => $mtu_value,
     }
 }
