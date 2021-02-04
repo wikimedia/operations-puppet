502e2f3f1c771791fe8045be315821921c2f4c7c
John Bond <jbond@wikimedia.org>
2021-02-04 10:44:48 +0100

    wmflib: Email type
    Add email type inspired by HTML5 specs[1].  The unit tests where taken
    from a random on line[2] list however they are currently failing.
    Hopping hashar can point me to a good set of test candidates
    
    [1]https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
    [2]https://gist.github.com/cjaoude/fd9910626629b53c4d25
    
    Bug: T273743
    Change-Id: I37f771cbf908dd91357472c7dffb3acb79664612

---
+++ modules/wmflib/types/email.pp
@@ -0,0 +1,2 @@
+# https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+type Wmflib::Email = Pattern[/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/]
