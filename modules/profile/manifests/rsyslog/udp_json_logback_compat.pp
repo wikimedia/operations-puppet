7b024157e9d003b5ffc966d53203f59f09beecc8
Daniel Zahn <dzahn@wikimedia.org>
2021-02-05 16:37:03 -0800

    profile::rsyslog::udp_json_logback_compat: hiera -> lookup
    Bug: T209953
    Change-Id: I7a0679139e932addcd7931846e1487f980fc0d6a

--- modules/profile/manifests/rsyslog/udp_json_logback_compat.pp
+++ modules/profile/manifests/rsyslog/udp_json_logback_compat.pp
@@ -5,8 +5,8 @@
 #
 
 class profile::rsyslog::udp_json_logback_compat(
-    Array   $logging_kafka_brokers = hiera('profile::rsyslog::kafka_shipper::kafka_brokers'),
-    Integer $port = hiera('profile::rsyslog::udp_json_logback_compat_port', 11514),
+    Array $logging_kafka_brokers = lookup('profile::rsyslog::kafka_shipper::kafka_brokers'),
+    Integer $port = lookup('profile::rsyslog::udp_json_logback_compat_port', {'default_value' => 11514}),
     Array[String] $queue_enabled_sites = lookup('profile::rsyslog::kafka_queue_enabled_sites',
                                                 {'default_value' => []}),
 ) {
