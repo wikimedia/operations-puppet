d780ff9681c6a2113004357df074245d207590b1
John Bond <jbond@wikimedia.org>
2021-02-08 13:45:51 +0100

    interfaces: remove ethtool configueration
    The interfaces-rps script should now be able to perform this function as
    such remove it from the puppet define.  Further if i read the following
    comment correctly i think we can also drop interfaces::rps::modprobe
    
    Bug: T236208
    Bug: T263578
    Hosts: cumin:R:interface::rps
    Change-Id: If3772c440f4060b35401c3d650efdbe6d0931f2d

--- modules/interface/manifests/rps.pp
+++ modules/interface/manifests/rps.pp
@@ -1,18 +1,15 @@
-# Definition: interface::rps
+# @summary Automagically sets RPS/RSS/XPS for an interface
 #
-# Automagically sets RPS/RSS/XPS for an interface
-#
-# Parameters:
-# - $interface:
-#   The network interface to operate on
-# - $rss_pattern:
-#   Optional RSS IRQ name pattern (normally auto-detected)
-# - $qdisc
-#   Options qdisc string to populate mq subqueues, e.g.:
-#   "fq flow_limit 422"
-# - $avoid_cpu0:
-#   If true, don't use CPU 0, to avoid contention issues (T236208)
-define interface::rps($interface=$name, $rss_pattern='', $qdisc='', $avoid_cpu0='') {
+# @param interface The network interface to operate on
+# @param rss_pattern Optional RSS IRQ name pattern (normally auto-detected)
+# @param qdisc Options qdisc string to populate mq subqueues, e.g.: "fq flow_limit 422"
+# @param avoid_cpu0 If true, don't use CPU 0, to avoid contention issues (T236208)
+define interface::rps(
+  String           $interface   = $name,
+  Optional[String] $rss_pattern = undef,
+  Optional[String] $qdisc       = '',
+  Boolean          $avoid_cpu0  = false,
+) {
     require interface::rpstools
     require interface::rps::modparams
 
@@ -42,23 +39,6 @@ define interface::rps($interface=$name, $rss_pattern='', $qdisc='', $avoid_cpu0=
         ],
     }
 
-    # This sets the queue count at runtime if it's incorrect (first run
-    # or after a reboot).  Changing at runtime *will* blip the interface.  This
-    # shouldn't be an issue for first-run scenarios, but might require a depool
-    # on live production hosts that can't handle short network blips.
-    if $facts['net_driver'][$interface]['driver'] =~ /^bnx(2x|t_en)/ {
-        # Limit to known cases bnx2x and bnxt_en which generally allow tons of
-        # queues, so that we can generally configure them for all non-HT cores
-        # on the interface's NUMA node.
-        $num_queues = size($facts['numa']['device_to_htset'][$interface])
-        exec { "ethtool_rss_combined_channels_${interface}":
-            path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-            command => "ethtool -L ${interface} combined ${num_queues}",
-            unless  => "test $(ethtool -l ${interface} | tail -4 | awk '/Combined:/ { print \$2 }') = '${num_queues}'",
-            require => Package['ethtool'],
-            before  => Exec["rps-${interface}"],
-        }
-    }
     # Note: tg3 (Broadcom Tigon3): driver defaults to 4 RX queues and 1 TX
     # queue, and supports raising the TX queues to 4 optionally as well, but
     # there's a comment in the tg3.c source code that says this is a bad idea,
