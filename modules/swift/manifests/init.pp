85c2a01e78d30a3593f9e667424f9739984f3455
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-02-09 10:36:43 +0100

    Remove obsolete netfilter sysctl setting
    When spelunking in our Puppet repo for sysctl settings I noticed that
    the Swift class is trying to set an obsolete sysctl setting for
    the conntrack table size. It has been replaced by net.netfilter.nf_conntrack_max
    for a long time (which gets applied centrally in the base::firewall class
    already)
    
    Change-Id: I16189c877afd4e7fb2e29e2a535ba3c7c00717fb

--- modules/swift/manifests/init.pp
+++ modules/swift/manifests/init.pp
@@ -5,30 +5,29 @@ class swift (
     # Recommendations from Swift -- see <http://tinyurl.com/swift-sysctl>.
     sysctl::parameters { 'swift_performance':
         values => {
-            'net.ipv4.tcp_syncookies'             => '0',
+            'net.ipv4.tcp_syncookies'      => '0',
             # Disable TIME_WAIT
-            'net.ipv4.tcp_tw_recycle'             => '1',
-            'net.ipv4.tcp_tw_reuse'               => '1',
-            'net.ipv4.netfilter.ip_conntrack_max' => '262144',
+            'net.ipv4.tcp_tw_recycle'      => '1',
+            'net.ipv4.tcp_tw_reuse'        => '1',
 
             # Other general network/TCP tuning
 
             # Increase the number of ephemeral ports
-            'net.ipv4.ip_local_port_range'        =>  [ 1024, 65535 ],
+            'net.ipv4.ip_local_port_range' => [ 1024, 65535 ],
 
             # Recommended to increase this for 1000 BT or higher
-            'net.core.netdev_max_backlog'         =>  30000,
+            'net.core.netdev_max_backlog'  => 30000,
 
             # Increase the queue size of new TCP connections
-            'net.core.somaxconn'                  => 4096,
-            'net.ipv4.tcp_max_syn_backlog'        => 262144,
-            'net.ipv4.tcp_max_tw_buckets'         => 360000,
+            'net.core.somaxconn'           => 4096,
+            'net.ipv4.tcp_max_syn_backlog' => 262144,
+            'net.ipv4.tcp_max_tw_buckets'  => 360000,
 
             # Decrease FD usage
-            'net.ipv4.tcp_fin_timeout'            => 3,
-            'net.ipv4.tcp_max_orphans'            => 262144,
-            'net.ipv4.tcp_synack_retries'         => 2,
-            'net.ipv4.tcp_syn_retries'            => 2,
+            'net.ipv4.tcp_fin_timeout'     => 3,
+            'net.ipv4.tcp_max_orphans'     => 262144,
+            'net.ipv4.tcp_synack_retries'  => 2,
+            'net.ipv4.tcp_syn_retries'     => 2,
         },
     }
 
