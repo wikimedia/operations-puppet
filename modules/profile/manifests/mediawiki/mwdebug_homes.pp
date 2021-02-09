947ad87302b025c72d8af0290ebfeee1fa4164ac
Daniel Zahn <dzahn@wikimedia.org>
2021-02-08 16:47:08 -0800

    mwdebug: allow rsyncing home dirs from any mwdebug* to a backup host
    Bug: T274023
    Change-Id: I66cb5c98989ddb3fea6d4bba21cce91b96420739

---
+++ modules/profile/manifests/mediawiki/mwdebug_homes.pp
@@ -0,0 +1,11 @@
+class profile::mediawiki::mwdebug_homes(
+    Stdlib::Fqdn $backup_mwdebug_host = lookup('profile::mediawiki::mwdebug_homes::backup_mwdebug_host'),
+){
+    rsync::quickdatacopy { 'mwdebug-home':
+        ensure      => present,
+        auto_sync   => true,
+        source_host => $::fqdn,
+        dest_host   => $backup_mwdebug_host,
+        module_path => '/srv/userhomes',
+    }
+}
