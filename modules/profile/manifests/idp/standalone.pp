9024b7b3f38851004326945c86e91abfd33e9119
John Bond <jbond@wikimedia.org>
2021-02-11 13:37:55 +0100

    profile::idp::standalone: create a profile for managing idp on cloud
    Change-Id: Ifa4e5b0f758135b5973a9670a0e1470a9ba209d2

---
+++ modules/profile/manifests/idp/standalone.pp
@@ -0,0 +1,28 @@
+# @summary Standalone IDP class for creating an instance in WM cloud
+class profile::idp::standalone {
+    # Standard stuff
+    include profile::standard
+    include profile::base::firewall
+
+    # configure database
+    include profile::mariadb::packages_wmf
+    class {'mariadb::service': }
+    class {'mariadb::config':
+      basedir => '/usr',
+      config  => 'role/mariadb/mysqld_config/misc.my.cnf.erbrole/mariadb/mysqld_config/misc.my.cnf.erb',
+      datadir => '/srv/sqldata',
+    }
+    # TODO: configure openldap
+    #  https://wikitech.wikimedia.org/wiki/Standalone-slapd
+
+    # configure IDP
+    include profile::idp
+    include profile::java
+    # Set up test web application
+    class {'httpd': modules => ['proxy_http', 'proxy']}
+    include profile::idp::client::httpd
+    ferm::service {'http-idp-test-login':
+      proto => 'tcp',
+      port  => 80,
+    }
+}
