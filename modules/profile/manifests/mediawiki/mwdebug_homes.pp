34441e23e72d2a51e3d075d7ad2c26385f906dfe
Daniel Zahn <dzahn@wikimedia.org>
2021-02-11 10:29:25 -0800

    mwdebug: do not automatically sync home files, just manual
    Change-Id: I0241f39c9a58c4bf686a6de36cd28791c5cf6320

--- modules/profile/manifests/mediawiki/mwdebug_homes.pp
+++ modules/profile/manifests/mediawiki/mwdebug_homes.pp
@@ -3,7 +3,7 @@ class profile::mediawiki::mwdebug_homes(
 ){
     rsync::quickdatacopy { 'mwdebug-home':
         ensure      => present,
-        auto_sync   => true,
+        auto_sync   => false,
         source_host => $::fqdn,
         dest_host   => $backup_mwdebug_host,
         module_path => '/srv/userhomes',
