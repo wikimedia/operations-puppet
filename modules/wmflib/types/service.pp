ac83a5df281e08ebcee724dedde9b3e81da3f24e
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2021-02-17 15:38:37 +0100

    wmflib: add role/public_endpoint to wmflib::service
    These entries are used in Pontoon to facilitate setting up new public services (i.e. services in the
    form of <name>.<public_domain>). The keys are used as following:
    
    * 'role' is used to indicate which role's hosts are backing the service
    * 'public_endpoint' is the service's name as made available publically (i.e. as an entry under
      $public_domain)
    
    Change-Id: I88ddf0f3f84507a6d8121b31868202c12e8c7cc6

--- modules/wmflib/types/service.pp
+++ modules/wmflib/types/service.pp
@@ -25,18 +25,30 @@
 #     - monitoring_setup means we want to introduce monitoring, but we still don't
 #     want to page.
 #     - finally production means the setup is complete and live in production.
-# @param Optional[Array] Array of discovery records related to the current service, if any
+# @param Optional[Array] discovery
+#     Array of discovery records related to the current service, if any
+# @param Optional[String] role
+#     The role used to run the service. The parameter is optional and used to
+#     associate the service to a list of hosts expected to run the service on $port.
+# @param Optional[String] public_endpoint
+#     The service's public name available under $public_domain. For example
+#     'public_endpoint: logstash' in production will be available at
+#     logstash.wikimedia.org. The parameter is optional for services not
+#     publicly available (e.g. internal only or fronted by restbase)
+#
 #
 type Wmflib::Service = Struct[
     {
-    'description' => String,
-    'sites'       => Array[String],
-    'ip'                => Hash[String, Wmflib::Service::Ipblock],
-    'port'        => Stdlib::Port, # This used to be optional in lvs config
-    'encryption'  => Boolean, # Wether we need TLS to connect
-    'lvs'         => Optional[Wmflib::Service::Lvs],
-    'monitoring'  => Optional[Wmflib::Service::Monitoring],
-    'state'       => Enum['service_setup', 'lvs_setup', 'monitoring_setup', 'production'],
-    'discovery'   => Optional[Wmflib::Service::Discovery],
+    'description'     => String,
+    'sites'           => Array[String],
+    'ip'              => Hash[String, Wmflib::Service::Ipblock],
+    'port'            => Stdlib::Port, # This used to be optional in lvs config
+    'encryption'      => Boolean, # Wether we need TLS to connect
+    'lvs'             => Optional[Wmflib::Service::Lvs],
+    'monitoring'      => Optional[Wmflib::Service::Monitoring],
+    'state'           => Enum['service_setup', 'lvs_setup', 'monitoring_setup', 'production'],
+    'discovery'       => Optional[Wmflib::Service::Discovery],
+    'role'            => Optional[String],
+    'public_endpoint' => Optional[String],
     }
 ]
