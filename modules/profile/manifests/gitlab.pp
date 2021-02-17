da78a92139b75d38867db625d9c85100e407eeae
Daniel Zahn <dzahn@wikimedia.org>
2021-02-17 12:26:31 -0800

    create placeholder role/profile for gitlab VMs
    Even if these VMs are not going to be puppetized, we still need
    to ensure at least these basics:
    
    - a role class we can use to add the admin group to
    - have base::firewall
    - have standard tools
    
    Bug: T274458
    Change-Id: I9cd875e93aec6e7f1f34342595a70c61d161824e

---
+++ modules/profile/manifests/gitlab.pp
@@ -0,0 +1,4 @@
+# a placeholder profile for a manual gitlab setup by
+# https://phabricator.wikimedia.org/T274458
+class profile::gitlab {
+}
---
+++ modules/role/manifests/gitlab.pp
@@ -0,0 +1,12 @@
+# a placeholder role for a manual gitlab setup by
+# https://phabricator.wikimedia.org/T274458
+class role::gitlab {
+
+    system::role { 'gitlab':
+        description => 'A virtual machine running gitlab.',
+    }
+
+    include ::profile::standard
+    include ::profile::base::firewall
+    include ::profile::gitlab
+}
