8d5fcd5f6e923b24c0ed5a57268c9f103b411e4f
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-02-17 13:39:18 +0100

    profile::base::cuminunpriv: Allow SSH access from unprivileged Cumin masters
    Bug: T244840
    Change-Id: I9506e442f34ebcd42a4ae84842aba40d9257fe97

--- modules/profile/manifests/base/cuminunpriv.pp
+++ modules/profile/manifests/base/cuminunpriv.pp
@@ -2,9 +2,18 @@
 #
 # - Install the Kerberos client tools
 # - Deploy the host keytab needed for kerberised SSH
+# - Make the SSH port accessible to the unpriv. Cumin masters in Ferm
 
 class profile::base::cuminunpriv(
+    Array[Stdlib::IP::Address] $unpriv_cumin_masters = lookup('unpriv_cumin_masters', {default_value => []}),
 ) {
     include profile::kerberos::client
     include profile::kerberos::keytabs
+
+    $cumin_hosts_ferm = join($unpriv_cumin_masters, ' ')
+    ferm::service { 'ssh-from-unprivcumin-masters':
+        proto  => 'tcp',
+        port   => '22',
+        srange => "(${cumin_hosts_ferm})",
+    }
 }
