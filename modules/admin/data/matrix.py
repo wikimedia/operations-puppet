f9fe23a1210a8a24daa88d654c98dcbfb388094c
John Bond <jbond@wikimedia.org>
2021-02-18 14:44:00 +0100

    admin: update matrix.py
    Change-Id: Idc70e6890759260ab64c352f0de35a75cf425e95

--- modules/admin/data/matrix.py
+++ modules/admin/data/matrix.py
@@ -1,100 +1,129 @@
 #!/usr/bin/env python3
-#
-# Given a list of users, generate a table of groups they belong to
-# Idea by Greg Grossmeier on T135187
-#
-# Example:
-#
-#   $ python matrix.py hashar thcipriani|column -t
-#   groups/user          hashar  thcipriani
-#   bastiononly          -       OK
-#   contint-admins       OK      OK
-#   contint-roots        OK      -
-#   deploy-phabricator   -       OK
-#   deploy-service       -       OK
-#   deployment           OK      OK
-#   gerrit-admin         OK      OK
-#   releasers-mediawiki  OK      -
-#   $
-#
-# Antoine Musso <hashar@free.fr> - 2016
-# Wikimedia Foundation Inc - 2016
-
-import argparse
-import sys
+from argparse import (
+    ArgumentParser,
+    ArgumentDefaultsHelpFormatter,
+    RawDescriptionHelpFormatter,
+)
+from pathlib import Path
 
 import yaml
 
+__doc__ = """
+Given a list of users, generate a table of groups they belong to
+Idea by Greg Grossmeier on T135187
 
-def flatten(memberlist, a=None):
-    '''
+Example:
+
+  $ python3 matrix.py hashar thcipriani|column -t
+  groups/user          hashar  thcipriani
+  bastiononly          -       OK
+  contint-admins       OK      OK
+  contint-roots        OK      -
+  deploy-phabricator   -       OK
+  deploy-service       -       OK
+  deployment           OK      OK
+  gerrit-admin         OK      OK
+  releasers-mediawiki  OK      -
+
+Antoine Musso <hashar@free.fr> - 2016
+Wikimedia Foundation Inc - 2016
+"""
+
+
+class MatrixArgumentFormatter(
+    ArgumentDefaultsHelpFormatter, RawDescriptionHelpFormatter
+):
+    """Custom argument formatter"""
+
+
+def flatten(memberlist, start=None):
+    """
     Flatten a list recursively. Make sure to only flatten list elements, which
-    is a problem with itertools.chain which also flattens strings. a defaults
+    is a problem with itertools.chain which also flattens strings. start defaults
     to None instead of the empty list to avoid issues with Copy by reference
     which is the default in python
-    '''
+    """
 
-    if a is None:
-        a = []
+    if start is None:
+        start = []
 
-    for i in memberlist:
-        if isinstance(i, list):
-            flatten(i, a)
+    for member in memberlist:
+        if isinstance(member, list):
+            flatten(member, start)
         else:
-            a.append(i)
-    return a
+            start.append(member)
+    return start
 
 
-parser = argparse.ArgumentParser(
-    description="Utility to generate a matrix of production users and their groups",
-)
-parser.add_argument('--wikitext',
-                    help="Whether to output wikitext or not.",
-                    action="store_true")
-parser.add_argument('user',
-                    help="User to display.",
-                    nargs='+')
+def get_args():
+    """Get arguments."""
+    parser = ArgumentParser(
+        description=__doc__, formatter_class=MatrixArgumentFormatter
+    )
+    parser.add_argument(
+        '--wikitext', help="Whether to output wikitext or not.", action="store_true"
+    )
+    parser.add_argument(
+        '--admin-file',
+        help="path to admin module data.yaml file",
+        type=Path,
+        default=Path(__file__).parents[0] / 'data.yaml',
+    )
+    parser.add_argument('user', help="User to display.", nargs='+')
 
-args = parser.parse_args()
-users = args.user
+    return parser.parse_args()
 
-TOP_LEFT = 'groups/users'
-if args.wikitext:
-    HEADER_SEPARATOR = '\n! '
-    TOP_LEFT = '! ' + TOP_LEFT
-    ROW_SEPARATOR = '\n|'
-    GROUP_BEGIN = '|-\n|'
-    POSITIVE = 'style="background-color:lightgreen" |OK'
-else:
-    HEADER_SEPARATOR = '\t'
-    ROW_SEPARATOR = '\t'
-    GROUP_BEGIN = ''
-    POSITIVE = 'OK'
 
-with open('data.yaml', 'r', encoding='utf-8') as f:
-    admins = yaml.safe_load(f)
+def main():
+    """Main function."""
+    args = get_args()
+    users = args.user
 
-all_users = list(admins.get('users'))
-unknown = set(users) - set(all_users)
-if unknown:
-    print('Unknown user(s):', ', '.join(unknown))
-    sys.exit(1)
+    top_left = 'groups/users'
+    if args.wikitext:
+        header_separator = '\n! '
+        top_left = '! ' + top_left
+        row_separator = '\n|'
+        group_begin = '|-\n|'
+        positive = 'style="background-color:lightgreen" |OK'
+    else:
+        header_separator = '\t'
+        row_separator = '\t'
+        group_begin = ''
+        positive = 'OK'
 
-if args.wikitext:
-    print('\n{| class="wikitable"')
-print(HEADER_SEPARATOR.join([TOP_LEFT] + users))
+    admins = yaml.safe_load(args.admin_file.read_text())
 
-groups = admins.get('groups', {})
-for group_name in sorted(groups.keys()):
-    group = groups[group_name]
+    all_users = list(admins.get('users'))
+    unknown = set(users) - set(all_users)
+    if unknown:
+        print('Unknown user(s):', ', '.join(unknown))
+        return 1
 
-    group_members = set(flatten(group['members']))
-    if set(users).isdisjoint(group_members):
-        continue
+    if args.wikitext:
+        print('\n{| class="wikitable"')
+    print(header_separator.join([top_left] + users))
 
-    members = set(users) & set(group_members)
-    print(GROUP_BEGIN + ROW_SEPARATOR.join(
-        [group_name] + [POSITIVE if u in members else ' ' for u in users]))
+    groups = admins.get('groups', {})
+    for group_name in sorted(groups.keys()):
+        group = groups[group_name]
 
-if args.wikitext:
-    print('|}')
+        group_members = set(flatten(group['members']))
+        if set(users).isdisjoint(group_members):
+            continue
+
+        members = set(users) & set(group_members)
+        print(
+            group_begin
+            + row_separator.join(
+                [group_name] + [positive if u in members else ' ' for u in users]
+            )
+        )
+
+    if args.wikitext:
+        print('|}')
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
