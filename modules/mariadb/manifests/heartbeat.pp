84eb1c3edb72473189d0fc4affe5af530d5e587c
Stevie Shirley <sshirley@wikimedia.org>
2021-02-19 12:40:17 +0100

    mariadb: Convert pt-heartbeat to a systemd service.
    This fixes the race-condition of puppet trying to start heartbeat before
    mariadb is running, and also simplifies stopping/starting heartbeat from
    scripts.
    
    Bug: T252528
    Change-Id: Ief91528a3ed0b7d51501593e77f9291bbbf9b2ad

--- modules/mariadb/manifests/heartbeat.pp
+++ modules/mariadb/manifests/heartbeat.pp
@@ -17,29 +17,8 @@ class mariadb::heartbeat (
         source => 'puppet:///modules/mariadb/pt-heartbeat-wikimedia',
     }
 
-    # TODO: This should be moved to systemd::service
-    if $enabled {
-        exec { 'pt-heartbeat':
-            command => "/usr/bin/perl \
-            /usr/local/bin/pt-heartbeat-wikimedia \
-            --defaults-file=/dev/null \
-            --user=root --host=localhost -D heartbeat \
-            --shard=${shard} --datacenter=${datacenter} \
-            --update --replace --interval=${interval} \
-            --set-vars=\"binlog_format=${mariadb::config::binlog_format}\" \
-            -S ${socket} --daemonize \
-            --pid /var/run/pt-heartbeat.pid",
-            unless  => '/bin/ps --pid $(cat /var/run/pt-heartbeat.pid) \
-            > /dev/null 2>&1',
-            user    => 'root',
-            require => File['/usr/local/bin/pt-heartbeat-wikimedia'],
-        }
-    } else {
-        exec { 'pt-heartbeat-kill':
-            command => '/bin/kill -TERM $(cat /var/run/pt-heartbeat.pid)',
-            onlyif  => '/bin/ps --pid $(cat /var/run/pt-heartbeat.pid) \
-            > /dev/null 2>&1',
-            user    => 'root',
-        }
+    $binlog_format = $mariadb::config::binlog_format
+    systemd::unit { 'pt-heartbeat-wikimedia':
+        content        => template('mariadb/pt-heartbeat-wikimedia.service.erb'),
     }
 }
