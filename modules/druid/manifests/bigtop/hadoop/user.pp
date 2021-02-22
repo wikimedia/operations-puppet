a72b4cbbe5776546f866711be6538ef52958a48e
Luca <ltoscano@wikimedia.org>
2021-02-22 15:38:48 +0100

    druid::bigtop::hadoop::user: add fixed uid/gid from Buster onward
    Bug: T231067
    Change-Id: If70120de4d2cd1c1427643b6c89e67dd72a95463

--- modules/druid/manifests/bigtop/hadoop/user.pp
+++ modules/druid/manifests/bigtop/hadoop/user.pp
@@ -4,12 +4,22 @@
 #
 class druid::bigtop::hadoop::user {
 
+    if debian::codename::ge('buster') {
+        $druid_uid = 907
+        $druid_gid = 907
+    } else {
+        $druid_uid = undef
+        $druid_gid = undef
+    }
+
     group { 'druid':
         ensure => 'present',
         system => true,
+        gid    => $druid_gid,
     }
     user { 'druid':
         ensure     => 'present',
+        uid        => $druid_uid,
         gid        => 'druid',
         shell      => '/bin/false',
         home       => '/nonexistent',
