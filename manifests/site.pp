7ccba76df75f87a8ec84aba3b4b18d1307393375
pt1979 <ptshibamba@wikimedia.org>
2021-02-23 19:45:40 -0600

    Fix role insetup for db21[45-52]
    Bug: T273568
    Change-Id: I5b88945f067b2dd1d792fda4870d8ffe164bf231

--- manifests/site.pp
+++ manifests/site.pp
@@ -795,7 +795,7 @@ node 'db2141.codfw.wmnet' {
     role(mariadb::dbstore_multiinstance)
 }
 # Codfw new database nodes T273568
-node /^db21[45|46|47|48|49|50|51|52]\.codfw\.wmnet/ {
+node /^db21(45|46|47|48|49|50|51|52)\.codfw\.wmnet$/ {
     role(insetup)
 }
 
