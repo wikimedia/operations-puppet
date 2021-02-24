1fead1a90ccc49000ccddf2870971cde78b8c9c9
Brooke Storm <bstorm@wikimedia.org>
2021-02-23 17:34:02 -0700

    kubeadm: remove swap from preflight_checks
    We no longer include swap by default in our images, and the scsi driver
    change changes the name of the devices.
    
    We should probably just retire this bit of code.
    
    Bug: T275585
    Change-Id: I96168f192337292daf9ee0a274e3dee18c503ad0

--- modules/profile/manifests/wmcs/kubeadm/preflight_checks.pp
+++ modules/profile/manifests/wmcs/kubeadm/preflight_checks.pp
@@ -6,20 +6,4 @@ class profile::wmcs::kubeadm::preflight_checks (
     if $facts['processorcount'] < 2 {
         fail('Please apply this profile into a VM with nCPU >= 2')
     }
-    # disable swap: kubelet doesn't want it
-    if $swap {
-        fail('Please set the swap_partition hiera key to false for this VM')
-    }
-    exec { 'kubeadm_disable_swap_swapoff':
-        command => '/sbin/swapoff -a',
-        onlyif  => '/usr/bin/test $(swapon -s | wc -l) -gt 0',
-    }
-    exec { 'kubeadm_disable_swap_fstab':
-        command => '/bin/sed -i /none.*swap/d /etc/fstab',
-        onlyif  => '/usr/bin/test $(grep swap /etc/fstab | wc -l) -gt 0',
-    }
-    exec { 'kubeadm_disable_swap_partition':
-        command => '/sbin/parted /dev/vda rm 2',
-        onlyif  => '/usr/bin/test $(parted /dev/vda print | grep swap | grep ^[[:space:]]2 | wc -l) -gt 0',
-    }
 }
