607ad2574f1f3c617f64cc1ad89a45b354518425
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-02-24 12:53:37 +0100

    Initial Cumin parts for the unprivileged Cumin master profile
    These will need more fine-tuning/testing, but eventually we should
    be able to move common parts to a new profile::cumin::common class.
    
    Bug: T244840
    Change-Id: Ia386ea42ee2bbe1b8507285ed3e3234b82e8b452

--- modules/profile/manifests/cumin/unprivmaster.pp
+++ modules/profile/manifests/cumin/unprivmaster.pp
@@ -1,4 +1,46 @@
 class profile::cumin::unprivmaster (
+    Stdlib::Host  $puppetdb_host        = lookup('puppetdb_host'),
+    Array[String] $datacenters          = lookup('datacenters'),
+    Stdlib::Host  $kerberos_kadmin_host = lookup('kerberos_kadmin_server_primary')
 ) {
     include profile::kerberos::client
+
+    $cumin_log_path = '/var/log/cumin'
+    $ssh_config_path = '/etc/cumin/ssh_config'
+
+    ensure_packages([
+        'clustershell',  # Installs nodeset CLI that is useful to mangle host lists.
+        'cumin',
+    ])
+
+    file { $cumin_log_path:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0750',
+    }
+
+    file { '/etc/cumin':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    file { '/etc/cumin/config.yaml':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('profile/cumin/config-unpriv.yaml.erb'),
+        require => File['/etc/cumin'],
+    }
+
+    file { $ssh_config_path:
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => 'puppet:///modules/profile/cumin/ssh_config-unpriv',
+    }
 }
