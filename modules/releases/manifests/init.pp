4d88cf2352430fa19b0674bd82559fdf26095adc
Kunal Mehta <legoktm@member.fsf.org>
2021-02-24 09:21:28 -0800

    releases: Move documentation to correct role
    Change-Id: I8f46e9cd7e182d400ac1d67e22c66a7694a9eab3

--- modules/releases/manifests/init.pp
+++ modules/releases/manifests/init.pp
@@ -1,27 +1,4 @@
 # Release server module for Wikimedia
-#
-# this module sets up a simple web server
-# that will serve static files
-#
-# production: https://releases.wikimedia.org
-# jenkins:    https://releases-jenkins.wikimedia.org
-# requirements:
-#
-# - initial content must be manually copied into
-#   /srv/org/wikimedia/releases
-# - ownership/perms of subdirs must be initially
-#   be set appropriately for users to add content
-#
-# this sets up:
-#
-# - the apache site config
-# - the /srv/org/wikimedia/ subdir docroot
-# - a Jenkins instance for automated MW releases
-# - another separate apache site for jenkins UI
-#
-# Because this service is intended to live behind a
-# caching cluster which would handle ssl/tls, it does not
-# install certs or configure apache for ssl/tls
 
 class releases (
     Optional[String] $sitename = undef,
--- modules/role/manifests/releases.pp
+++ modules/role/manifests/releases.pp
@@ -1,6 +1,28 @@
 # == Class: role::releases
 #
-# Sets up a machine to generate and host releases of software
+# this role sets up a simple web server
+# that will serve static files
+#
+# production: https://releases.wikimedia.org
+# jenkins:    https://releases-jenkins.wikimedia.org
+# requirements:
+#
+# - initial content must be manually copied into
+#   /srv/org/wikimedia/releases
+# - ownership/perms of subdirs must be initially
+#   be set appropriately for users to add content
+#
+# this sets up:
+#
+# - the apache site config
+# - the /srv/org/wikimedia/ subdir docroot
+# - a Jenkins instance for automated MW releases
+# - another separate apache site for jenkins UI
+#
+# Because this service is intended to live behind a
+# caching cluster which would handle ssl/tls, it does not
+# install certs or configure apache for ssl/tls
+
 class role::releases {
 
     system::role { 'releases':
