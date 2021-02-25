578fc0c56173a9a33e6d64f4e66dce5569608e3c
Daniel Zahn <dzahn@wikimedia.org>
2021-02-25 13:36:26 -0800

    role::deployment_server: re-order includes, add comments, clean up
    Adding and editing comments.
    
    Changing order and grouping of profile includes.
    
    Removing include of mediawiki::common because that is already
    included in profile::mediawiki::deployment::server.
    
    Change-Id: Idb68480480145cb3dd9eeeea06e98b163c650801

--- modules/role/manifests/deployment_server.pp
+++ modules/role/manifests/deployment_server.pp
@@ -1,39 +1,43 @@
 # MediaWiki Deployment Server (prod)
 class role::deployment_server {
+
+    system::role { 'deployment_server':
+        description => 'Deployment server for MediaWiki and related code',
+    }
+
+    # standards
     include profile::standard
     include profile::base::firewall
+    include profile::backup::host
+    backup::set {'home': }
 
-    # Install the scap server components.
-    include profile::mediawiki::common
-    include profile::mediawiki::mcrouter_wancache
-    include profile::mediawiki::nutcracker
+    # webserver, scap deployment tool with SSH agent, rsync
     include profile::mediawiki::deployment::server
     include profile::scap::dsh
-    # Install the keyholder agents
     include profile::keyholder::server
-    # Install conftool
+
+    # memcached-related 
+    include profile::mediawiki::mcrouter_wancache
+    include profile::mediawiki::nutcracker
+
+    # client to fetch configuration data
     include profile::conftool::client
 
-    # All needed classes for deploying mediawiki
+    # MediaWiki release uploads to releases servers
     include profile::releases::mediawiki::security
     include profile::releases::upload
 
     # Kubernetes deployments
     include profile::kubernetes::deployment_server
 
+    # tool to test webserver config changes
     include profile::httpbb
 
-    include profile::backup::host
-    backup::set {'home': }
     # proxy for connection to other servers
     include profile::services_proxy::envoy
 
     # in cloud mount a second disk as /srv
     if $::realm == 'labs' {
         require profile::labs::lvm::srv
-    }
-
-    system::role { 'deployment_server':
-        description => 'Deployment server for MediaWiki and related code',
     }
 }
