0b8954058346f55139c1074dd05023213a82c818
Daniel Zahn <dzahn@wikimedia.org>
2021-02-25 14:56:49 -0800

    deployment::rsync:: also sync patches directory
    It's important that the patches directory also stays intact
    and gets synced to other deployment servers.
    
    So use the same mechanism that keeps /srv/deployment in sync;
    all passive servers pull from the one active server with --delete.
    
    Change-Id: I9d42f21242a37f7cc25cd168bf69e35e2db926a8

--- modules/deployment/manifests/rsync.pp
+++ modules/deployment/manifests/rsync.pp
@@ -7,6 +7,7 @@ class deployment::rsync(
     Wmflib::Ensure $job_ensure = 'absent',
     Array[Stdlib::Host] $deployment_hosts = [],
     Stdlib::Unixpath $deployment_path = '/srv/deployment',
+    Stdlib::Unixpath $patches_path = '/srv/patches',
 ){
 
     include ::rsync::server
@@ -31,6 +32,30 @@ class deployment::rsync(
             },{
             'start'    => 'OnCalendar',
             'interval' => '*-*-* *:00:00', # then hourly on the hour
+            },
+        ],
+    }
+
+    rsync::server::module { 'patches_module':
+        path        => $patches_path,
+        read_only   => 'yes',
+        hosts_allow => $deployment_hosts,
+    }
+
+    $sync_patches_command = "/usr/bin/rsync -avz --delete ${deployment_server}::patches_module ${patches_path}"
+
+    systemd::timer::job { 'sync_patches_dir':
+        ensure      => $job_ensure,
+        user        => 'root',
+        description => "rsync the deployment server patches directory ${patches_path}",
+        command     => $sync_patches_command,
+        interval    => [
+            {
+            'start'    => 'OnBootSec', # initially start the unit
+            'interval' => '15sec',
+            },{
+            'start'    => 'OnCalendar',
+            'interval' => '*-*-* *:30:00', # then hourly on the half hour
             },
         ],
     }
