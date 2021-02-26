6404e10af380707a20df57080f31b4314266e1ed
John Bond <jbond@wikimedia.org>
2021-02-26 10:19:40 +0100

    sudo: add validate_cmd for sudoers file
    Change-Id: Id7b11873b896a92f8c9b04f5335e1cb6724971dc

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -4,11 +4,12 @@ class sudo {
     }
 
     file { '/etc/sudoers':
-        ensure  => present,
-        mode    => '0440',
-        owner   => 'root',
-        group   => 'root',
-        source  => 'puppet:///modules/sudo/sudoers',
-        require => Package[sudo],
+        ensure       => present,
+        mode         => '0440',
+        owner        => 'root',
+        group        => 'root',
+        source       => 'puppet:///modules/sudo/sudoers',
+        require      => Package[sudo],
+        validate_cmd => '/usr/sbin/visudo -c -f %'
     }
 }
