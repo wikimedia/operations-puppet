6c5459319395c51a3be96d265064fd8ac9f5b59c
Daniel Zahn <dzahn@wikimedia.org>
2021-03-01 15:37:39 -0800

    gitlab: open port 80 for traffic from caching servers
    This opens a firewall hole on the gitlab* VMs to
    allow incoming traffic on port 80 coming from the caching
    layer (ATS/Varnish).
    
    Similarly to Ic4eb6a1040036ff the question here is also
    whether we open 80, 443 or both.
    
    Bug: T276144
    Change-Id: I5f465986c0adafe3ee102dd88e9128666fdfd533

--- modules/profile/manifests/gitlab.pp
+++ modules/profile/manifests/gitlab.pp
@@ -1,4 +1,10 @@
 # a placeholder profile for a manual gitlab setup by
 # https://phabricator.wikimedia.org/T274458
 class profile::gitlab {
+
+    ferm::service { 'gitlab-http-caches':
+        proto  => 'tcp',
+        port   => '80',
+        srange => '$CACHES',
+    }
 }
