8c6acf73676eaf30dd1bdab3742efedb23cbcffc
Luca <ltoscano@wikimedia.org>
2021-03-01 15:20:17 +0100

    profile::python38: fix component name
    Change-Id: I3ccf11095b4f5dc6f0bb55cff1ac6fafadd03b62

--- modules/profile/manifests/python38.pp
+++ modules/profile/manifests/python38.pp
@@ -7,7 +7,7 @@ class profile::python38 {
     if debian::codename::eq('buster') {
         if ! defined( Apt::Package_from_component['component-pyall'] ) {
             apt::package_from_component { 'component-pyall':
-                component => 'component/pyall',
+                component => 'thirdparty/pyall',
                 packages  => ['python3.8', 'libpython3.8']
             }
         }
