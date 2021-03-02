ae9afdb33312b3fb1029f93c610f3685ce2a6335
Cole White <cwhite@wikimedia.org>
2021-03-01 17:37:19 -0700

    icinga: add grafana dashboard alert for client errors
    Bug: T264665
    Change-Id: Ib25327c4dee5a3dbaa373fb2a785d7f06335ebb9

--- modules/icinga/manifests/monitor/reading_web.pp
+++ modules/icinga/manifests/monitor/reading_web.pp
@@ -9,4 +9,10 @@ class icinga::monitor::reading_web {
         contact_group => 'team-reading-web',
         notes_url     => 'https://phabricator.wikimedia.org/rOPUP8e70f242a7888527d6af8ff8d823f82fe0202cd0',
     }
+
+    monitoring::grafana_alert { 'reading-web-client-errors':
+        dashboard_uid => '000000566',
+        contact_group => 'team-reading-web,admins',
+        notes_url     => 'https://logstash.wikimedia.org/app/kibana#/dashboard/AXDBY8Qhh3Uj6x1zCF56'
+    }
 }
