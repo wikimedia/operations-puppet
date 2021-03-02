dfdea940919abecf3aea5bb884a59b13cbcba56a
John Bond <jbond@wikimedia.org>
2021-03-02 18:31:03 +0100

    P:profile::client::httpd: add param override the attribute delimiter
    We default this to ',' as this is the upstream default.  however for use
    it makes much more sense to use anything but ',' as the important
    attribute to use is the memberof attribute which is a ldapdn and this
    contains commers naturarly.
    
    However some set ups already use this mapping (librenms at the very
    least) we would need to investigate to see there are any other instances
    and what other local modiufications may be required
    
    Hosts: cumin:R:profile::idp::client::httpd::site
    Change-Id: I63031fc296e907a388f9e5c804d72a50e9258e39

--- modules/profile/manifests/idp/client/httpd/site.pp
+++ modules/profile/manifests/idp/client/httpd/site.pp
@@ -15,24 +15,27 @@
 # @param acme_chief_cert the name of the acme chief certificate to use
 # @param vhost_settings Allows to pass settings to the vhost config which are unrelated to the IDP setup
 # @param proxied_as_https if true set the proxied_as address to https://${vhost}/
+# @attribute_delimiter mod_auth_cas will set the value of the attribute header (as described in CASAttributePrefix)
+#   to <attrvalue><CASAttributeDelimiter><attrvalue> in the case of multiple attribute values.
 define profile::idp::client::httpd::site (
     String[1]                     $vhost_content,
-    Stdlib::Host                  $virtual_host      = $title,
-    Stdlib::Unixpath              $document_root    = '/var/www',
-    Array[Stdlib::Host]           $server_aliases   = [],
-    String[1]                     $authn_header     = 'CAS-User',
-    String[1]                     $attribute_prefix = 'X-CAS-',
-    Boolean                       $debug            = false,
-    Integer[1,99]                 $priority         = 50,
-    Boolean                       $validate_saml    = false,
-    Boolean                       $enable_monitor   = true,
-    String[1]                     $protected_uri    = '/',
-    String[1]                     $cookie_scope     = $protected_uri,
-    Boolean                       $proxied_as_https = false,
-    Enum['staging', 'production'] $environment      = 'production',
-    Optional[Hash[String,Any]]    $vhost_settings   = {},
-    Optional[Array[String[1]]]    $required_groups  = [],
-    Optional[String[1]]           $acme_chief_cert  = undef,
+    Stdlib::Host                  $virtual_host         = $title,
+    Stdlib::Unixpath              $document_root       = '/var/www',
+    Array[Stdlib::Host]           $server_aliases      = [],
+    String[1]                     $authn_header        = 'CAS-User',
+    String[1]                     $attribute_prefix    = 'X-CAS-',
+    Boolean                       $debug               = false,
+    Integer[1,99]                 $priority            = 50,
+    Boolean                       $validate_saml       = false,
+    Boolean                       $enable_monitor      = true,
+    String[1]                     $protected_uri       = '/',
+    String[1]                     $cookie_scope        = $protected_uri,
+    Boolean                       $proxied_as_https    = false,
+    String[1,1]                   $attribute_delimiter = ',',
+    Enum['staging', 'production'] $environment         = 'production',
+    Optional[Hash[String,Any]]    $vhost_settings      = {},
+    Optional[Array[String[1]]]    $required_groups     = [],
+    Optional[String[1]]           $acme_chief_cert     = undef,
 ) {
     include profile::idp::client::httpd
     $apereo_cas        = $profile::idp::client::httpd::apereo_cas
@@ -46,15 +49,16 @@ define profile::idp::client::httpd::site (
         default => undef,
     }
     $cas_settings = {
-        'CASLoginURL'        => $apereo_cas[$environment]['login_url'],
-        'CASValidateURL'     => $apereo_cas[$environment]['validate_url'],
-        'CASDebug'           => $debug ? { true => 'On', default => 'Off' },
-        'CASRootProxiedAs'   => $proxied_as,
-        'CASVersion'         => 2,
-        'CASCertificatePath' => $certificate_path,
-        'CASCookiePath'      => $cookie_path,
-        'CASAttributePrefix' => $attribute_prefix,
-        'CASValidateSAML'    => $validate_saml ? { true => 'On', default => 'Off' },
+        'CASLoginURL'           => $apereo_cas[$environment]['login_url'],
+        'CASValidateURL'        => $apereo_cas[$environment]['validate_url'],
+        'CASDebug'              => $debug ? { true => 'On', default => 'Off' },
+        'CASRootProxiedAs'      => $proxied_as,
+        'CASVersion'            => 2,
+        'CASCertificatePath'    => $certificate_path,
+        'CASCookiePath'         => $cookie_path,
+        'CASAttributePrefix'    => $attribute_prefix,
+        'CASAttributeDelimiter' => $attribute_delimiter,
+        'CASValidateSAML'       => $validate_saml ? { true => 'On', default => 'Off' },
     }
 
     $cas_auth_require = $required_groups.empty? {
