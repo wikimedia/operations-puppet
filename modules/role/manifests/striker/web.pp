17f815612a73afb8252b5139eebfd95736f4f377
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-03-03 10:45:20 +0100

    striker: Remove support for jessie
    Change-Id: I54ee3bd5aef54300686886e13dd970a509335013

--- modules/role/manifests/striker/web.pp
+++ modules/role/manifests/striker/web.pp
@@ -6,13 +6,11 @@
 # filtertags: labs-project-striker
 class role::striker::web {
 
-    if debian::codename::ge('stretch') {
-        ensure_packages('libapache2-mod-wsgi-py3')
-        class { 'httpd':
-            modules => ['alias', 'ssl', 'rewrite', 'headers', 'wsgi',
-                        'proxy', 'expires', 'proxy_http', 'proxy_balancer',
-                        'lbmethod_byrequests'],
-        }
+    ensure_packages('libapache2-mod-wsgi-py3')
+    class { 'httpd':
+        modules => ['alias', 'ssl', 'rewrite', 'headers', 'wsgi',
+                    'proxy', 'expires', 'proxy_http', 'proxy_balancer',
+                    'lbmethod_byrequests'],
     }
 
     include profile::standard
