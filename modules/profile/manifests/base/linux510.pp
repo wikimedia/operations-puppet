2b1f9b36e41b8a560531f5f8073bb7a487d81f8f
Luca <ltoscano@wikimedia.org>
2021-03-03 10:21:53 +0100

    Add profile to deploy the 5.10 Linux kernel on Buster hosts
    Change-Id: I1a3bbdaa8f0224d37a29af31af02bfc94460bbdd

---
+++ modules/profile/manifests/base/linux510.pp
@@ -0,0 +1,23 @@
+# == Class profile::base::linux510
+#
+# Setup Kernel 5.10 on Buster hosts. Used for special use cases:
+# if you want to use it in prod, please sync up with Infrastructure Foundation's
+# SREs first for visibility."
+# Some use cases:
+# - Hosts with GPU (AMD ROCm drivers are published to the kernel, the more recent the better).
+#   This includes Machine Learning and Analytics.
+# - cloudgw specific NAT settings used by the Cloud team.
+# - bnx2x NICs firmware issues (cloudnet servers, see T271058)
+#
+class profile::base::linux510(
+    Boolean $enable = lookup('profile::base::linux510::enable', { 'default_value' => false }),
+) {
+    # only for Buster
+    if $enable and debian::codename::eq('buster') {
+        apt::pin { 'linux-image-apt-pin':
+            pin      => 'release a=buster-backports',
+            package  => 'linux-image-amd64',
+            priority => '1001',
+        }
+    }
+}
