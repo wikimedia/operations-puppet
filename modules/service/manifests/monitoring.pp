02228f31d79432cdcb0c76b565496ca6c062fd87
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-03-03 10:46:50 +0100

    service::monitoring: Remove support for jessie
    With the removal of scb, these are all on stretch or Buster.
    
    Change-Id: Ia47c4a746ebf517e338376da97b2dd0e57e897ad

--- modules/service/manifests/monitoring.pp
+++ modules/service/manifests/monitoring.pp
@@ -4,10 +4,5 @@
 # services defined via service::node
 
 class service::monitoring {
-    # On stretch+, we use a newer version of service-checker that is python3-only
-    if debian::codename::ge('stretch') {
-        ensure_packages('python3-service-checker')
-    } else {
-        ensure_packages(['python-yaml', 'python-urllib3', 'python-service-checker'])
-    }
+    ensure_packages('python3-service-checker')
 }
