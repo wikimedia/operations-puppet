84e4a21b84140efa6eadbf20798be0390cd3d4ae
Janis Meybohm <jmeybohm@wikimedia.org>
2021-03-05 11:24:49 +0100

    kubernetes/helmfile_log_sal: Allow to suppress logging to SAL
    This will make deployments and tests on the inactive staging cluster
    less verbose and less confusing for deployers/developers as they don't
    see their services being deployed to "staging" over and over again.
    
    Change-Id: I3b003330b1002a035088f9e93b9c99a36bcb1a87

--- modules/profile/files/kubernetes/helmfile_log_sal.sh
+++ modules/profile/files/kubernetes/helmfile_log_sal.sh
@@ -4,6 +4,9 @@
 TCPIRCBOT_HOST=${TCPIRCBOT_HOST:-icinga.wikimedia.org}
 TCPIRCBOT_PORT=${TCPIRCBOT_PORT:-9200}
 
+# Allow to explicitely suppress logging to SAL
+SUPPRESS_SAL=${SUPPRESS_SAL:-false}
+
 function sal_log {
 	if [ -n "${SUDO_USER}" ]; then
 		LOG_USER=${SUDO_USER}
@@ -17,7 +20,7 @@ function sal_log {
 
 COMMAND=$1
 shift
-if [[ ! -z "${COMMAND}" ]]; then
+if [[ ! -z "${COMMAND}" && "${SUPPRESS_SAL}" == "false" ]]; then
 	if [[ "${COMMAND}" == "apply" || "${COMMAND}" == "sync" ]]; then
 		sal_log "$@"
 	fi
