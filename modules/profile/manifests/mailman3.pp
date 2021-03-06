692dbd1053b13e8ef78e9366c56e21fc8d8a3b30
Amir Sarabadani <Ladsgroup@gmail.com>
2021-03-06 22:48:35 +0100

    mailman3: Add exim4 configuration
    This is combination of exim4 config of the old mailman and documentation
    of mailman3 [1]. Mostly copied the config to keep it decoupled from the
    old mailman (making disabling the old one easier once the migration is
    done) and also avoiding touching production at the moment
    
    [1] https://mailman.readthedocs.io/en/latest/src/mailman/docs/mta.html
    
    Bug: T256536
    Change-Id: I0747801d748320ca1ca52a3fc1ee1b83911e4086

--- modules/profile/manifests/mailman3.pp
+++ modules/profile/manifests/mailman3.pp
@@ -1,12 +1,16 @@
 class profile::mailman3 (
-    String $host            = lookup('profile::mailman3::host'),
-    String $db_host         = lookup('profile::mailman3::db_host'),
-    String $db_password     = lookup('profile::mailman3::db_password'),
+    String $host = lookup('profile::mailman3::host'),
+    String $db_host = lookup('profile::mailman3::db_host'),
+    String $db_password = lookup('profile::mailman3::db_password'),
     String $db_password_web = lookup('profile::mailman3::web::db_password'),
-    String $api_password    = lookup('profile::mailman3::api_password'),
-    String $web_secret      = lookup('profile::mailman3::web::secret'),
-    String $archiver_key    = lookup('profile::mailman3::archiver_key'),
+    String $api_password = lookup('profile::mailman3::api_password'),
+    String $web_secret = lookup('profile::mailman3::web::secret'),
+    String $archiver_key = lookup('profile::mailman3::archiver_key'),
+    Optional[Stdlib::IP::Address] $lists_ipv4 = lookup('profile::mailman3::ipv4', {'default_value' => undef}),
+    Optional[Stdlib::IP::Address] $lists_ipv6 = lookup('profile::mailman3::ipv6', {'default_value' => undef}),
 ) {
+    include network::constants
+
     class { '::mailman3':
         host            => $host,
         db_host         => $db_host,
@@ -22,5 +26,42 @@ class profile::mailman3 (
     }
     httpd::site { $host:
         content => template('mailman3/apache.conf.erb'),
+    }
+
+    # This will be a noop if $lists_ipv[46] are undef
+    interface::alias { $host:
+        ipv4 => $lists_ipv4,
+        ipv6 => $lists_ipv6,
+    }
+
+    $list_outbound_ips = [
+        pick($lists_ipv4, $facts['ipaddress']),
+        pick($lists_ipv6, $facts['ipaddress6']),
+    ]
+
+    if $::realm == 'labs' {
+        $trusted_networks = ['172.16.0.0/12']
+    }
+    if $::realm == 'production' {
+        $trusted_networks = $network::constants::aggregate_networks.filter |$x| {
+            $x !~ /127.0.0.0|::1/
+        }
+    }
+
+    class { 'spamassassin':
+        required_score   => '4.0',
+        use_bayes        => '0',
+        bayes_auto_learn => '0',
+        trusted_networks => $trusted_networks,
+    }
+
+    class { 'exim4':
+        variant => 'heavy',
+        config  => template('profile/exim/exim4.conf.mailman3.erb'),
+        filter  => template('profile/exim/system_filter.conf.mailman3.erb'),
+        require => [
+            Class['spamassassin'],
+            Interface::Alias[$host],
+        ],
     }
 }
