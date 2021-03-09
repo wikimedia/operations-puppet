wikimedia/operations-puppet

commit 7a9998235a0be0b3ed9017b26b722de36e858fa7

check-microcode.py: Remove dbproxy1008
dbproxy1008 has been decommissioned.

Bug: T255406
Change-Id: I7b5d0fdf265480796c799d3e3594b5018fd68640

modules/base/files/check-microcode.py
@@ -52,8 +52,7 @@ def main():
 
     # CPUs which were not fixed for SSBD (which was the first) are also not
     # fixed for L1TF/MDS
-    blacklist_ssbd_l1tf = ['dbproxy1008']
-    blacklist_mds = ['helium', 'tungsten', 'dbproxy1003'] + blacklist_ssbd_l1tf
+    blacklist_mds = ['helium', 'tungsten', 'dbproxy1003']
 
     if apt_pkg.version_compare(current_kernelpackage_version, '4.9.107-1') > 0:
         expected_cpu_flags.add('ssbd')
@@ -67,7 +66,7 @@ def main():
     if apt_pkg.version_compare(current_kernelpackage_version, '4.9.168-1+deb9u1') > 0:
         expected_cpu_flags.add('md_clear')
 
-    if 'ssbd' in expected_cpu_flags and hostname in blacklist_ssbd_l1tf:
+    if 'ssbd' in expected_cpu_flags:
         expected_cpu_flags.remove('ssbd')
 
     for flag in ['flush_l1d', 'md_clear']:
@@ -80,9 +79,9 @@ def main():
     # Reading the flags from lscpu is not supported in jessie
     try:
         with open('/proc/cpuinfo', 'r') as proc_file:
-            for l in proc_file.readlines():
-                if l.startswith("flags"):
-                    available_cpu_flags.update(l.split(":")[1].split())
+            for line in proc_file.readlines():
+                if line.startswith("flags"):
+                    available_cpu_flags.update(line.split(":")[1].split())
                     break
     except IOError:
         unknown('Failed to read CPU flags from proc')

commit 6c42eb586a1d03a34dbe7570a4345a017cad0394

base: Fix bug introduced on check microcode by 7a99982 & 4a436bd
Both Manuel and I introduced logical errors to the way the microcode
filterlists work, as we didn't understand the context of why those
were there. Instead, filter lists comparisons should have been
completely removed.

Otherwise, in the current logic, all of the flag comparisons
stop working, and cause hosts such as backup2003 (with the latest
kernel) to complain about "hardware too old".

Eliminate the filter comparison completely to fix the errors.

Change-Id: I48d3c349c21ab74b886feb71fd81caec4f0ba7da

modules/base/files/check-microcode.py
@@ -61,13 +61,6 @@ def main():
     if apt_pkg.version_compare(current_kernelpackage_version, '4.9.168-1+deb9u1') > 0:
         expected_cpu_flags.add('md_clear')
 
-    if 'ssbd' in expected_cpu_flags:
-        expected_cpu_flags.remove('ssbd')
-
-    for flag in ['flush_l1d', 'md_clear']:
-        if flag in expected_cpu_flags:
-            expected_cpu_flags.remove(flag)
-
     if not expected_cpu_flags:
         ok('Hardware is too old')
 