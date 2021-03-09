b1dc5a04a8df877b71c79c28541e365379e7a5f5
Daniel Zahn <dzahn@wikimedia.org>
2021-03-09 12:53:30 -0800

    builder/docker: break out docker ferm rules into own profile
    This aims to be a complete noop on docker builder host (deneb).
    
    It just moves the ferm rules for docker builder hosts into a
    separate profile and then includes it.
    
    This allows for reusing the rules without including the entire
    package builder setup.
    
    Bug: T276869
    Change-Id: Ic4ea15098d355b3bb7c39ce500beed6731eca3b9

---
+++ modules/profile/manifests/docker/ferm.pp
@@ -0,0 +1,13 @@
+# ferm firewall rules for docker
+class profile::docker::ferm {
+
+    # Ship the entire docker iptables configuration via ferm
+    # This is here to make sure docker and ferm play nice together.
+    # For now we only want this on a subset of hosts, which is why we don't put it in
+    # profile::docker::engine. This relies on settings iptables: false in docker
+    ferm::conf { 'docker-ferm':
+        ensure => present,
+        prio   => 20,
+        source => 'puppet:///modules/profile/docker/docker-ferm',
+    }
+}
