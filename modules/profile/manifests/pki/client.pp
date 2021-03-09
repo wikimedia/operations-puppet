26f3cdc695f4130cf5e28a0131f20ce789e0874e
John Bond <jbond@wikimedia.org>
2021-03-09 18:53:08 +0100

    pki::client: allow user to override remote ca
    This should error on foobar-client, fixed in the follow up
    
    Change-Id: Ie8f5451b35f620171e8f30459a7fabc77fcecd18

--- modules/profile/manifests/pki/client.pp
+++ modules/profile/manifests/pki/client.pp
@@ -5,20 +5,33 @@
 # @param use_stunnel use an stunnel encrypt
 # @param auth_key the cfssl sha256 hmax key
 class profile::pki::client (
-    Wmflib::Ensure             $ensure                 = lookup('profile::pki::client::ensure'),
-    Stdlib::Host               $signer_host            = lookup('profile::pki::client::signer_host'),
-    Stdlib::Port               $signer_port            = lookup('profile::pki::client::signer_port'),
-    Sensitive[String[1]]       $auth_key               = lookup('profile::pki::client::auth_key'),
-    Boolean                    $enable_proxy           = lookup('profile::pki::client::enable_proxy'),
-    Stdlib::IP::Address        $listen_addr            = lookup('profile::pki::client::listen_addr'),
-    Stdlib::Port               $listen_port            = lookup('profile::pki::client::listen_port'),
-    Optional[Stdlib::Unixpath] $mutual_tls_client_cert = lookup('profile::pki::client::mutual_tls_client_cert'),
-    Optional[Stdlib::Unixpath] $mutual_tls_client_key  = lookup('profile::pki::client::mutual_tls_client_key'),
-    Optional[Stdlib::Unixpath] $tls_remote_ca          = lookup('profile::pki::client::tls_remote_ca'),
-    Hash                       $certs                  = lookup('profile::pki::client::certs'),
+    Wmflib::Ensure               $ensure                 = lookup('profile::pki::client::ensure'),
+    Stdlib::Host                 $signer_host            = lookup('profile::pki::client::signer_host'),
+    Stdlib::Port                 $signer_port            = lookup('profile::pki::client::signer_port'),
+    Sensitive[String[1]]         $auth_key               = lookup('profile::pki::client::auth_key'),
+    Boolean                      $enable_proxy           = lookup('profile::pki::client::enable_proxy'),
+    Stdlib::IP::Address          $listen_addr            = lookup('profile::pki::client::listen_addr'),
+    Stdlib::Port                 $listen_port            = lookup('profile::pki::client::listen_port'),
+    Optional[Stdlib::Unixpath]   $mutual_tls_client_cert = lookup('profile::pki::client::mutual_tls_client_cert'),
+    Optional[Stdlib::Unixpath]   $mutual_tls_client_key  = lookup('profile::pki::client::mutual_tls_client_key'),
+    Optional[Stdlib::Unixpath]   $tls_remote_ca          = lookup('profile::pki::client::tls_remote_ca'),
+    Optional[Stdlib::Filesource] $tls_remote_ca_source   = lookup('profile::pki::client::tls_remote_source'),
+    Hash                         $certs                  = lookup('profile::pki::client::certs'),
 ) {
     $signer = "https://${signer_host}:${signer_port}"
     $bundles_source = "http://${signer_host}/bundles"
+    if $tls_remote_ca_source {
+        if $tls_remote_ca == $facts['puppet_config']['localcacert'] {
+            fail('When setting \$tls_remote_ca_source you must change \$tls_remote_ca')
+        }
+        file{$tls_remote_ca:
+            ensure => file,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0440',
+            source => $tls_remote_ca_source,
+        }
+    }
     class {'cfssl::client':
         ensure                 => $ensure,
         signer                 => $signer,
