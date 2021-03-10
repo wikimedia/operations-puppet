16cbd2c805c5eec07f5b7dedef1768a783b49eec
Daniel Zahn <dzahn@wikimedia.org>
2021-03-09 17:31:47 -0800

    gitlab: open firewall for 80,443. use drange to limit to service IP
    Since we now have a second IP to run public services, https and
    ssh, on, we should open the firewall on ports 80 and 443
    but only for traffic to the service IP, not for the main
    server IP.
    
    So we are using drange for that which isn't used a lot
    but makes sense in this situation.
    
    That way SSH to the the server IP gitlab1001 is still limited
    to come from bastion hosts.
    
    per T276144#6890920 we can wait or merge now
    
    Bug: T276144
    Change-Id: I3f4159d6a3f6aa415d21a7bfaab54da885eff103

--- modules/profile/manifests/gitlab.pp
+++ modules/profile/manifests/gitlab.pp
@@ -5,9 +5,25 @@ class profile::gitlab(
     Stdlib::IP::Address::V6 $service_ip_v6 = lookup('profile::gitlab::service_ip_v6'),
 ){
 
-    # add secondary (service) IP to NIC
-    interface::alias { 'gitlab service IP': # T276148
+    # add a service IP to the NIC - T276148
+    interface::alias { 'gitlab service IP':
         ipv4 => $service_ip_v4,
         ipv6 => $service_ip_v6,
+    }
+
+    # open ports in firewall - T276144
+
+    # world -> service IP, HTTP
+    ferm::service { 'gitlab-http-public':
+        proto  => 'tcp',
+        port   => 80,
+        drange => "${service_ip_v4} ${service_ip_v6}",
+    }
+
+    # world -> service IP, HTTPS
+    ferm::service { 'gitlab-https-public':
+        proto  => 'tcp',
+        port   => 443,
+        drange => "${service_ip_v4} ${service_ip_v6}",
     }
 }
