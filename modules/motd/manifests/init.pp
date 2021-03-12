84e58b94e981da43375fe1188eb5866fa5e275d3
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-03-12 11:05:07 +0100

    motd: Remove support for jessie
    Change-Id: I08c5252e193b31e77f383a9f3105d05eb060562f

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -3,21 +3,9 @@
 # Module for customizing MOTD (Message of the Day) banners.
 #
 class motd {
-    # This was incomplete & buggy in jessie, as detailed in
-    # https://lists.debian.org/debian-devel/2014/12/msg00368.html
-    #
-    # This has been fixed since in stretch onwards.
-    if debian::codename::eq('jessie') {
-        file { '/etc/motd':
-            ensure => link,
-            target => '/var/run/motd',
-            force  => true,
-        }
-    } else {
-        # Kill Debian's default copyright/warranty banner
-        file { '/etc/motd':
-            ensure => absent,
-        }
+    # Kill Debian's default copyright/warranty banner
+    file { '/etc/motd':
+        ensure => absent,
     }
 
     file { '/etc/update-motd.d':
