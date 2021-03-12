1646fc35c287adf0c59337975351003f34555bba
Andrew Bogott <abogott@wikimedia.org>
2021-03-12 15:59:36 -0600

    Add cinderutils::swap
    This will look for an appropriate block device and convert
    it to a swap volume.
    
    This is in 'cinderutils' and will indeed work with a cinder volume.
    The idea of using a cinder volume for swap is silly, though -- I
    expect this to mostly be used with VMs build from flavors that
    have a ready-made (ephemeral) swap partition.
    
    It should work to apply this class to VMs with lvm-created
    swap partitions since the first thing this class does is
    check if there's already a block device formatted for swap.
    
    Bug: T272114
    Change-Id: Ie1ba35c8c259bfbfed3c9fa18c03b4d905f946c9

---
+++ modules/cinderutils/manifests/swap.pp
@@ -0,0 +1,68 @@
+# = define: cinderutils::swap
+# Detects an available volume and creates it as swap space
+#
+# == Parameters
+#
+# [*min_gb*]
+#   When detecting a volume to use as swap, only use volumes at least this size
+# [*max_gb*]
+#   When detecting a volume to use as swap, only use volumes no larger than this
+#
+define cinderutils::swap(
+    $min_gb,
+    $max_gb,
+) {
+    $swap_volume = $facts['block_devices'].reduce(undef) |$memo, $volume| {
+        if $volume['fstype'] == 'swap' {
+            # There's already a swap partition! Let's grab the biggest one
+            $mount_requires = []
+            if $memo != undef and $memo['size'] > $volume['size'] {
+                $memo
+            } else {
+                $volume
+            }
+        } elsif (!$memo and
+                  !$volume['fstype'] and
+                  $volume['dev'] != 'vda' and
+                  $volume['dev'] != 'sda' and
+                  $volume['type'] == 'disk' and
+                  $volume['size'] >= $min_gb * 1024 * 1024 * 1024 and
+                  $volume['size'] <= $max_gb * 1024 * 1024 * 1024) {
+            # This is a candidate to make into swap if we don't already
+            #  have something formatted as swap.
+            $mount_requires = [Exec["format-${name}"]]
+            $volume
+        } else {
+            $memo
+        }
+    }
+
+    if $swap_volume == undef {
+        fail("Unable to locate a swap volume.  Please attach a cinder volume or use a flavor with a swap partition.\nThe swap volume or partition must be at least ${min_gb}GB and no larger than ${max_gb}GB\n")
+    }
+
+    # Support legacy dev names from lvm
+    $swap_dev_path = regsubst($swap_volume['dev'],'^vd-','vd/')
+
+    if !$swap_volume['fstype'] {
+        exec { "format-${name}":
+            command     => "/sbin/mkswap /dev/${swap_dev_path}",
+        }
+    }
+
+    mount { "mount-swap-${name}":
+        ensure  => present,
+        name    => '-',
+        atboot  => true,
+        device  => "/dev/${swap_dev_path}",
+        options => 'defaults',
+        fstype  => 'swap',
+        require => $mount_requires,
+        notify  => Exec["swapon-${name}"],
+    }
+
+    exec { "swapon-${name}":
+        refreshonly => true,
+        command     => "/sbin/swapon /dev/${swap_dev_path}",
+    }
+}
