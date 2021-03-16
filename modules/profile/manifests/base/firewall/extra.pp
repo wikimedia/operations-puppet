c1f01404101de7f765a5cc4aa59ae3b02dd06206
John Bond <jbond@wikimedia.org>
2021-03-16 14:47:59 +0100

    P:base::firewall::extra: profile to set ferm rules via hiera
    This allows useres to spcify ferm rules in heira which can be quite
    usefull when testing things on cloud environments.
    
    E.g structure:
    profile::base::firewall::extra::services:
      'open http for cloud network':
        proto: 'tcp'
        port: 80
        srange: '172.16.0.0/12'
    
    Change-Id: I19e844ddb63789c00a94ef5e0a6dbbfb8ada038b

---
+++ modules/profile/manifests/base/firewall/extra.pp
@@ -0,0 +1,11 @@
+# @summary a profile to allow one to create firewall rules via hiera.  usefull for cloud hosts
+# @param rules a hash of rules passed to ferm::rule
+class profile::base::firewall::extra (
+  Hash $services = lookup('profile::base::firewall::extra::services')
+) {
+  $services.each |$service, $config| {
+    ferm::service {$service:
+      * => $config
+    }
+  }
+}
