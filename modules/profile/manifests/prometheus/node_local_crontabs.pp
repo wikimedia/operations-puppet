aa891e19974d4f76e4bbff0c54f2953964d207de
Brooke Storm <bstorm@wikimedia.org>
2021-03-18 11:43:07 -0700

    prometheus: re-order sudo access for the cron
    Since we are now on a version of puppet where order is meaningful,
    this may stop some spam from new cloud instances.
    
    Change-Id: I6bc24a4461a52ed1b50d880bda9748f62126f32d

--- modules/profile/manifests/prometheus/node_local_crontabs.pp
+++ modules/profile/manifests/prometheus/node_local_crontabs.pp
@@ -5,14 +5,6 @@
 class profile::prometheus::node_local_crontabs {
     class { 'prometheus::node_local_crontabs': }
 
-    # Collect every 5 minutes
-    cron { 'prometheus_local_crontabs':
-        ensure  => 'present',
-        user    => 'prometheus',
-        minute  => '*/5',
-        command => '/usr/local/bin/prometheus-local-crontabs',
-    }
-
     sudo::user { 'prometheus_sudo_for_local_crontab':
         ensure     => 'present',
         user       => 'prometheus',
@@ -20,6 +12,12 @@ class profile::prometheus::node_local_crontabs {
             'ALL=(root) NOPASSWD: /bin/ls -1 /var/spool/cron/crontabs/',
         ],
     }
+
+    # Collect every 5 minutes
+    cron { 'prometheus_local_crontabs':
+        ensure  => 'present',
+        user    => 'prometheus',
+        minute  => '*/5',
+        command => '/usr/local/bin/prometheus-local-crontabs',
+    }
 }
-
-
