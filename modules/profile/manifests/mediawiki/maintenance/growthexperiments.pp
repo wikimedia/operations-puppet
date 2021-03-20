855e882559dd2cd0e7cf9fbf53ecdcdd0b12fa53
Gergő Tisza <tgr.huwiki@gmail.com>
2021-03-20 01:40:17 +0100

    Update GrowthExperiments cronjob parameters
    * reduce WelcomeSurvey retention period from 270 to 90 days
    * use verbose logging for the link recommendation refresh script
      while it's being developed
    
    Bug: T275171
    Bug: T261408
    Change-Id: Ie152d96725247e97490b7255a0c06fbfcb6d0721

--- modules/profile/manifests/mediawiki/maintenance/growthexperiments.pp
+++ modules/profile/manifests/mediawiki/maintenance/growthexperiments.pp
@@ -1,16 +1,16 @@
 class profile::mediawiki::maintenance::growthexperiments {
     # Purge old welcome survey data (personal data used in user options,
-    # with a 270-day retention exception) that's within 30 days of expiry,
-    # twice a month. See T208369 and T252575. Logs are saved to
+    # with 90-day retention) that's within 30 days of expiry, twice a month.
+    # See T208369 and T252575. Logs are saved to
     # /var/log/mediawiki/mediawiki_job_growthexperiments-deleteOldSurveys/syslog.log
     profile::mediawiki::periodic_job { 'growthexperiments-deleteOldSurveys':
-        command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/growthexperiments.dblist extensions/GrowthExperiments/maintenance/deleteOldSurveys.php --cutoff 240',
+        command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/growthexperiments.dblist extensions/GrowthExperiments/maintenance/deleteOldSurveys.php --cutoff 60',
         interval => '*-*-01,15 03:15:00',
     }
 
     # Ensure that a sufficiently large pool of link recommendations is available.
     profile::mediawiki::periodic_job { 'growthexperiments-refreshLinkRecommendations':
-        command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/growthexperiments.dblist extensions/GrowthExperiments/maintenance/refreshLinkRecommendations.php',
+        command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/growthexperiments.dblist extensions/GrowthExperiments/maintenance/refreshLinkRecommendations.php --verbose',
         interval => '*-*-* *:27:00',
     }
 }
