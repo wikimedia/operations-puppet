108bb6db2c53abdc8f4c1a4354a020193f64a743
David Caro <dcaro@wikimedia.org>
2021-03-22 15:58:12 +0100

    wmcs.ceph.codfw: Upgrade to latest 5.X kernel
    Bug: T274565
    Change-Id: Ie1f172fdc3ed16e071bb2bad7626a9b31aad75fa
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- modules/role/manifests/wmcs/ceph/osd.pp
+++ modules/role/manifests/wmcs/ceph/osd.pp
@@ -2,5 +2,7 @@ class role::wmcs::ceph::osd {
     system::role { $name: description => 'Ceph Object Storage Daemon server.' }
     include ::profile::standard
     include ::profile::base::firewall
+    # This does not really install it by default, controlled through hiera
+    include ::profile::base::linux510
     include ::profile::ceph::osd
 }
