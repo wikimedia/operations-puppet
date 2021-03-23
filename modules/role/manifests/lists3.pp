ebee6596dc1e01f7d361c9fd8eae06a907b86ad2
Kunal Mehta <legoktm@member.fsf.org>
2021-03-23 16:54:54 -0700

    role::lists3: Include profile::standard and profile::base::firewall
    Accidentally missed in the initial puppetization.
    
    Change-Id: I43e67124e198a110419269bc5447e0bebc41de8f

--- modules/role/manifests/lists3.pp
+++ modules/role/manifests/lists3.pp
@@ -1,7 +1,9 @@
 class role::lists3 {
 
     system::role { 'lists3': description => 'Mailing list server (mailman3)', }
-    include profile::mailman3
 
+    include profile::standard
+    include profile::base::firewall
+    include profile::mailman3
 }
 
