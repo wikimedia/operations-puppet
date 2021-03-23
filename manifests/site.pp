dcada7e105424ccf17848b5c3cd8988395d7fc23
Effie Mouzeli <effie@wikimedia.org>
2021-03-23 13:48:54 +0200

    hiera/nodes: put mc1038 into our session redis and memcached cluster
    mc1038 will replace the dead mc1024 in shard06. This will cause
    a resharding in our memcached cluster since we are re-introducting
    shard06 to mcrouter.  Regarding redis, there will be the replacement
    of the current server in shard06.
    
    Puppet will be disabled while the host is running puppet, and then
    we will let it take its course.
    
    Bug: T278225
    Change-Id: Ib6a0c63f35a6b769462eeb67ac06327bfb28f47c

--- manifests/site.pp
+++ manifests/site.pp
@@ -1644,11 +1644,7 @@ node /^mc10(19|2[0-3]|2[5-9]|3[0-7])\.eqiad\.wmnet/ {
     role(mediawiki::memcached)
 }
 
-node 'mc1038.eqiad.wmnet' {
-    role(insetup)
-}
-
-node /^mc20(19|2[0-7]|29|3[0-7])\.codfw\.wmnet/ {
+node /^mc20(19|2[0-7]|29|3[0-8])\.codfw\.wmnet/ {
     role(mediawiki::memcached)
 }
 
