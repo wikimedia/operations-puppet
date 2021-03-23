34a7066966a8415eda60d5a2968e6579aecae071
Daniel Zahn <dzahn@wikimedia.org>
2021-03-23 15:23:00 -0700

    gitlab: add gitlab.wm.org service IP, with lookup from Hiera
    Previously gitlab.wikimedia.org was a CNAME for gitlab1001.wikimedia.org
    but we dropped that in Ia75a707508c and then a new service IP was
    assigned in netbox and then added to DNS via cookbook.
    
    This is to add it to the actual interface on the VM gitlab1001.
    
    Add proper parameters for v4 and v6 service IP to the profile class
    and lookup values from Hiera. A little step towards doing it right
    from the start without making it too complicated either.
    
    Bug: T276148
    Change-Id: I41f6900651bdadbd76537f59e30ae9c4aef9890c

--- modules/profile/manifests/gitlab.pp
+++ modules/profile/manifests/gitlab.pp
@@ -1,4 +1,13 @@
 # a placeholder profile for a manual gitlab setup by
 # https://phabricator.wikimedia.org/T274458
-class profile::gitlab {
+class profile::gitlab(
+    Stdlib::IP::Address::V4 $service_ip_v4 = lookup('profile::gitlab::service_ip_v4'),
+    Stdlib::IP::Address::V6 $service_ip_v6 = lookup('profile::gitlab::service_ip_v6'),
+){
+
+    # add secondary (service) IP to NIC
+    interface::alias { 'gitlab service IP': # T276148
+        ipv4 => $service_ip_v4,
+        ipv6 => $service_ip_v6,
+    }
 }
