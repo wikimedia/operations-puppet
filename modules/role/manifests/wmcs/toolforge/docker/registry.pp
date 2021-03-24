ff9457210914075ae3dd10ef0afaca2cdd6a89b6
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2021-03-24 13:23:04 +0100

    toolforge: docker registry: don't use labs_lvm on buster
    We are now using a cinder volume in the Debian Buster iteration.
    
    Bug: T278303
    Change-Id: Id1bf4bafb0d894a29e7b3e653b4721c58fc8165b
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/role/manifests/wmcs/toolforge/docker/registry.pp
+++ modules/role/manifests/wmcs/toolforge/docker/registry.pp
@@ -2,7 +2,10 @@ class role::wmcs::toolforge::docker::registry(
 ) {
     system::role { $name: }
 
-    include profile::labs::lvm::srv
+    # we don't want this in buster, now using a cinder volume
+    if debian::codename::eq('stretch') {
+        include profile::labs::lvm::srv
+    }
     include profile::toolforge::base
     include profile::toolforge::apt_pinning
     include profile::toolforge::infrastructure
