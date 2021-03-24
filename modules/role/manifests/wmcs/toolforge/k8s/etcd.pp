8caf874beb90cbc200833d545a8363b233443add
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-03-24 08:56:15 +0100

    toolforge:k8s:etcd: Remove support for jessie
    The entire toollabs module has been removed already and jessie is gone.
    
    Change-Id: If9185763c442016726c95ca511d7fd6fc06b752c

--- modules/role/manifests/wmcs/toolforge/k8s/etcd.pp
+++ modules/role/manifests/wmcs/toolforge/k8s/etcd.pp
@@ -1,22 +1,9 @@
 class role::wmcs::toolforge::k8s::etcd {
     system::role { $name: }
 
-    # this is for the old Debian Jessie VMs running the etcd cluster for the
-    # old Toolforge kubernetes cluster. Once the old k8s is gone, this switch
-    # can be removed
-    case debian::codename() {
-        'jessie': {
-            include role::toollabs::etcd::k8s
-        }
-        'buster': {
-            include profile::base::firewall
-            include profile::toolforge::base
-            include profile::toolforge::infrastructure
-            include profile::toolforge::k8s::etcd
-            include profile::toolforge::prometheus_fixup
-        }
-        default: {
-            fail("${debian::codename()}: not supported")
-        }
-    }
+    include profile::base::firewall
+    include profile::toolforge::base
+    include profile::toolforge::infrastructure
+    include profile::toolforge::k8s::etcd
+    include profile::toolforge::prometheus_fixup
 }
