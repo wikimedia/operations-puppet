b59674b6fe7dbe4ca65683bdcce06273f589d660
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2021-03-24 16:05:58 +0100

    cinderutils: refresh helper script
    The script lacks permissions to be used with sudo, use 0554 instead.
    
    Also, rename the script to use a more standard 'wmcs' prefix, replacing '_'
    with '-' as is common in the linux world.
    
    A compatibility symlink is left behind to don't break user workflows until all
    documentation is updated.
    
    Change-Id: Ib91b9e8b21468b939eb7c1d105f5f62b5417cff2
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/cinderutils/manifests/init.pp
+++ modules/cinderutils/manifests/init.pp
@@ -1,9 +1,20 @@
 class cinderutils {
+    # remove this after a few puppet runs
     file { '/usr/sbin/prepare_cinder_volume':
+        ensure => absent,
+    }
+
+    file { '/usr/local/sbin/wmcs-prepare-cinder-volume':
         ensure => present,
-        source => 'puppet:///modules/cinderutils/prepare_cinder_volume.py',
+        source => 'puppet:///modules/cinderutils/wmcs-prepare-cinder-volume.py',
         owner  => 'root',
         group  => 'root',
-        mode   => '0544',
+        mode   => '0554',
+    }
+
+    # compat
+    file { '/usr/local/sbin/prepare_cinder_volume':
+        ensure => link,
+        target => '/usr/local/sbin/wmcs-prepare-cinder-volume',
     }
 }
