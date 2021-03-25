dbd23c06e401fec050fe2872abd5f53c2d5e1411
Brooke Storm <bstorm@wikimedia.org>
2021-03-25 15:16:30 -0700

    quarry: remove the querykiller profile
    This has been removed from the host since it is completely broken at this
    point.
    
    Bug: T264254
    Change-Id: If702297f1bb1c7556320146451c1f32f669fb7fc

--- modules/role/manifests/labs/quarry/killer.pp
+++
@@ -1,7 +0,0 @@
-# Should be included on an instance that already has
-# a Quarry install (celery or web) setup
-#
-# filtertags: labs-project-quarry
-class role::labs::quarry::killer {
-    include ::profile::quarry::querykiller
-}
