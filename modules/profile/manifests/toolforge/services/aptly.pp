b4cc4d516cdabcf8f4258387afac81513033dd48
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2021-03-25 14:07:19 +0100

    toolforge: services: drop code to support cold-standby approach
    We are storing the data in a cinder volume instead.
    
    Bug: T278354
    Change-Id: I50265af8401848cc30815d4c0707f0f45c11dc17
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/toolforge/services/aptly.pp
+++ modules/profile/manifests/toolforge/services/aptly.pp
@@ -1,6 +1,4 @@
 class profile::toolforge::services::aptly(
-    $active_node = lookup('profile::toolforge::services::active_node'),
-    $standby_node = lookup('profile::toolforge::services::standby_node'),
 ) {
     aptly::repo { 'buster-tools':
         publish      => true,
@@ -15,14 +13,5 @@ class profile::toolforge::services::aptly(
     }
     aptly::repo { 'buster-toolsbeta':
         publish      => true,
-    }
-
-    # make sure we have a backup server ready to take over
-    rsync::quickdatacopy { 'aptly-sync':
-        ensure      => present,
-        auto_sync   => true,
-        source_host => $active_node,
-        dest_host   => $standby_node,
-        module_path => '/srv/packages',
     }
 }
