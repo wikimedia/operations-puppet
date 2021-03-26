6ba245fee971641fd88331298102e5edc19e437d
John Bond <jbond@wikimedia.org>
2021-03-26 15:29:28 +0100

    C:ssh::server: make authorized_keys_file an Arrauy[Stdlib::Unixpath]
    This change updates the authorized_keys param to take an array, this
    allows us to preform a bit more data validation on the elements.  this
    cr should be a noop in production however pcc will show changes due to
    the change in parameters
    
    I have also checked instance-puppet and we will also need to make a
    change to the gitlab project
    
    I have also included a bit of whitespace cleanup
    
    Hosts: O:gerrit
    Change-Id: I008374375b76c90818d02c2c2b582c9be176ec9b

--- modules/ssh/manifests/server.pp
+++ modules/ssh/manifests/server.pp
@@ -13,20 +13,19 @@
 # @param max_sessions allow users to override the maximum number ops sessions
 # @param max_startups allow users to override the maximum number ops startups
 class ssh::server (
-    Stdlib::Port           $listen_port              = 22,
-    Optional[Stdlib::Host] $listen_address           = undef,
-    Boolean                $permit_root              = true,
-    # TODO convert to Array[Stdlib::Unuxpath]
-    String                 $authorized_keys_file     = '/etc/ssh/userkeys/%u /etc/ssh/userkeys/%u.d/cumin',
-    Stdlib::Unixpath       $authorized_keys_command  = '/usr/sbin/ssh-key-ldap-lookup',
-    Boolean                $disable_nist_kex         = true,
-    Boolean                $explicit_macs            = true,
-    Boolean                $enable_hba               = false,
-    Boolean                $enable_kerberos          = false,
-    Boolean                $disable_agent_forwarding = true,
-    Boolean                $challenge_response_auth  = true,
-    Optional[Integer]      $max_sessions             = undef,
-    Optional[Integer]      $max_startups             = undef,
+    Stdlib::Port            $listen_port              = 22,
+    Optional[Stdlib::Host]  $listen_address           = undef,
+    Boolean                 $permit_root              = true,
+    Array[Stdlib::Unixpath] $authorized_keys_file     = ['/etc/ssh/userkeys/%u', '/etc/ssh/userkeys/%u.d/cumin'],
+    Stdlib::Unixpath        $authorized_keys_command  = '/usr/sbin/ssh-key-ldap-lookup',
+    Boolean                 $disable_nist_kex         = true,
+    Boolean                 $explicit_macs            = true,
+    Boolean                 $enable_hba               = false,
+    Boolean                 $enable_kerberos          = false,
+    Boolean                 $disable_agent_forwarding = true,
+    Boolean                 $challenge_response_auth  = true,
+    Optional[Integer]       $max_sessions             = undef,
+    Optional[Integer]       $max_startups             = undef,
 ) {
     package { 'openssh-server':
         ensure => present,
