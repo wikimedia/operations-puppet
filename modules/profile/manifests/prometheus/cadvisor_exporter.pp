9094904e32a8c4e89aa261e778c17a7e2da5616b
Alexandros Kosiaris <akosiaris@wikimedia.org>
2021-03-26 13:13:45 +0200

    cadvisor_exporter: Switch to 4 spaces tab
    Why:
    Cause https://wikitech.wikimedia.org/wiki/Puppet_coding#Coding_Style
    says so
    
    Change-Id: I3b019d33b521a8572c74f9d0fd5fb43ec47305c2

--- modules/profile/manifests/prometheus/cadvisor_exporter.pp
+++ modules/profile/manifests/prometheus/cadvisor_exporter.pp
@@ -1,18 +1,18 @@
 class profile::prometheus::cadvisor_exporter (
-  Stdlib::Port        $port             = lookup('profile::prometheus::cadvisor_exporter::port'),
-  Array[Stdlib::Host] $prometheus_nodes = lookup('prometheus_nodes'),
+    Stdlib::Port        $port             = lookup('profile::prometheus::cadvisor_exporter::port'),
+    Array[Stdlib::Host] $prometheus_nodes = lookup('prometheus_nodes'),
 ){
 
-  class { 'prometheus::cadvisor_exporter':
-    port   => $port,
-    ensure => 'present',
-  }
+    class { 'prometheus::cadvisor_exporter':
+      port   => $port,
+      ensure => 'present',
+    }
 
-  $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
-  $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
-  ferm::service { 'prometheus-cadvisor-exporter':
-    proto  => 'tcp',
-    port   => $port,
-    srange => $ferm_srange,
-  }
+    $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
+    $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
+    ferm::service { 'prometheus-cadvisor-exporter':
+      proto  => 'tcp',
+      port   => $port,
+      srange => $ferm_srange,
+    }
 }
