83c566c4f7db7019a228fa457007f5c41eaba263
Alexandros Kosiaris <akosiaris@wikimedia.org>
2021-03-26 13:18:52 +0200

    prometheus::cadvisor_exporter: Support only buster and later
    Why:
    There is no incentive to support stretch
    
    Bug: T278220
    Change-Id: Iaec6406af4f8bbd798728bbd1ede8c0abfa41e67

--- modules/profile/manifests/prometheus/cadvisor_exporter.pp
+++ modules/profile/manifests/prometheus/cadvisor_exporter.pp
@@ -3,16 +3,20 @@ class profile::prometheus::cadvisor_exporter (
     Array[Stdlib::Host] $prometheus_nodes = lookup('prometheus_nodes'),
 ){
 
-    class { 'prometheus::cadvisor_exporter':
-      port   => $port,
-      ensure => 'present',
-    }
+    # We only support buster and above cause we have no incentive to support
+    # stretch and below
+    if debian::codename::ge('buster'){
+        class { 'prometheus::cadvisor_exporter':
+          port   => $port,
+          ensure => 'present',
+        }
 
-    $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
-    $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
-    ferm::service { 'prometheus-cadvisor-exporter':
-      proto  => 'tcp',
-      port   => $port,
-      srange => $ferm_srange,
+        $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
+        $ferm_srange = "(@resolve((${prometheus_ferm_nodes})) @resolve((${prometheus_ferm_nodes}), AAAA))"
+        ferm::service { 'prometheus-cadvisor-exporter':
+          proto  => 'tcp',
+          port   => $port,
+          srange => $ferm_srange,
+        }
     }
 }
