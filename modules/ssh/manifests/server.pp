ef67021133798595ca7382a646326f0512d65ff4
John Bond <jbond@wikimedia.org>
2021-03-26 14:49:12 +0100

    C:ssh::server: refactor
    small clean up, this change is stylistic and should be a noop
    
    Hosts: C:ssh::server
    Change-Id: Ib049e9b298ad4fdccd0b0c7607181fcefd998987

--- modules/ssh/manifests/server.pp
+++ modules/ssh/manifests/server.pp
@@ -1,17 +1,33 @@
+# @summary manage the ssh server daemon and config
+# @param listen_port the port to listen on
+# @param listen_address the address to listen on
+# @param permit_root if true allow root logins
+# @param authorized_keys_file space seperated list of authorized keys files
+# @param authorized_keys_command command to run for authorized keys
+# @param disable_nist_kex Allow uses to temporarily opt out of nist kex disabling
+# @param explicit_macs Allow users to opt out of more secure MACs
+# @param enable_hba enable host based authentication
+# @param enable_kerberos enable kerberos
+# @param disable_agent_forwarding disable agent forwarding
+# @param challenge_response_auth Disable all password auth
+# @param max_sessions allow users to override the maximum number ops sessions
+# @param max_startups allow users to override the maximum number ops startups
 class ssh::server (
-    $listen_port = '22',
-    $listen_address = undef,
-    $permit_root = true,
-    $authorized_keys_file = undef,
-    $authorized_keys_command = '/usr/sbin/ssh-key-ldap-lookup',
-    $disable_nist_kex = true, # Allow labs projects to temporarily opt out of nist kex disabling
-    $explicit_macs = true, # Allow labs projects to temporarily opt out of more secure MACs
-    $enable_hba = false,
-    Boolean $enable_kerberos = false,
-    $disable_agent_forwarding = true,
-    $challenge_response_auth = true,  # Disable all password auth in labs, we don't use 2fa there
-    $max_sessions = undef,  # Allow Cloud VPS restricted bastions to override it for Cumin
-    $max_startups = undef,  # Allow Cloud VPS restricted bastions to override it for Cumin
+    # TODO convert to Stdlib::Port
+    String                     $listen_port              = '22',
+    Optional[Stdlib::Host]     $listen_address           = undef,
+    Boolean                    $permit_root              = true,
+    # TODO convert to Array[Stdlib::Unuxpath]
+    Optional[String]           $authorized_keys_file     = undef,
+    Stdlib::Unixpath           $authorized_keys_command  = '/usr/sbin/ssh-key-ldap-lookup',
+    Boolean                    $disable_nist_kex         = true,
+    Boolean                    $explicit_macs            = true,
+    Boolean                    $enable_hba               = false,
+    Boolean                    $enable_kerberos          = false,
+    Boolean                    $disable_agent_forwarding = true,
+    Boolean                    $challenge_response_auth  = true,
+    Optional[Integer]          $max_sessions             = undef,
+    Optional[Integer]          $max_startups             = undef,
 ) {
     package { 'openssh-server':
         ensure => present,
@@ -23,12 +39,8 @@ class ssh::server (
     }
 
     base::service_auto_restart { 'ssh': }
-
-    if $authorized_keys_file {
-        $ssh_authorized_keys_file = $authorized_keys_file
-    } else {
-        $ssh_authorized_keys_file ='/etc/ssh/userkeys/%u /etc/ssh/userkeys/%u.d/cumin'
-    }
+    # TODO just make this the param default
+    $_authorized_keys_file = pick($authorized_keys_file, '/etc/ssh/userkeys/%u /etc/ssh/userkeys/%u.d/cumin')
 
     file { '/etc/ssh/userkeys':
         ensure  => directory,
@@ -48,36 +60,16 @@ class ssh::server (
         require => Package['openssh-server'],
     }
 
-    # publish this hosts's host key; prefer ECDSA -> RSA (no DSA)
-    #
-    # Puppet sshkey hardcodes acceptable types in its code, and ed25519 is not
-    # a valid type in trusty's version (3.4.3). It is in jessie's version
-    # (3.7.3), though. So this is waiting until trusty is gone, or until we
-    # backport a newer version of puppet to trusty.
+    $aliases = [
+        $facts['networking']['hostname'],
+        $facts['networking']['ip'],
+        $facts['networking']['ip6'],
+    ].filter |$x| { $x =~ NotUndef }
 
-    if $::sshecdsakey {
-        # facter bug: one key regardless of ECDSA keytype;
-        # no type exported as a separate variable
-        $key  = $::sshecdsakey
-        $type = 'ecdsa-sha2-nistp256'
-    } elsif $::sshrsakey {
-        $key  = $::sshrsakey
-        $type = 'ssh-rsa'
-    } else {
-        err("No valid SSH host key found for ${::fqdn}")
-    }
-
-    if $::ipaddress6 == undef {
-        $aliases = [ $::hostname, $::ipaddress ]
-    } else {
-        $aliases = [ $::hostname, $::ipaddress, $::ipaddress6 ]
-    }
-
-    debug("Storing ${type} SSH hostkey for ${::fqdn}")
-    @@sshkey { $::fqdn:
+    @@sshkey { $facts['networking']['fqdn']:
         ensure       => present,
-        type         => $type,
-        key          => $key,
+        type         => 'ecdsa-sha2-nistp256',
+        key          => $facts['ssh']['ecdsa']['key'],
         host_aliases => $aliases,
     }
 }
