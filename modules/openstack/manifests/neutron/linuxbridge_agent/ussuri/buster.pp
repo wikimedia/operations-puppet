983780e7993e5b59b6006b455b47313b7981dbfe
Andrew Bogott <abogott@wikimedia.org>
2021-04-01 15:31:20 -0500

    Neutron: Remove ip_lib.py hack for Ussuri
    This was inserted for a one-line change which is now present
    in the upstream file.
    
    Bug: T261136
    Change-Id: Ibcf11486871704843da0a1b9fc170790139e1ae5

--- modules/openstack/manifests/neutron/linuxbridge_agent/ussuri/buster.pp
+++ modules/openstack/manifests/neutron/linuxbridge_agent/ussuri/buster.pp
@@ -19,21 +19,4 @@ class openstack::neutron::linuxbridge_agent::ussuri::buster(
     alternatives::select { 'ebtables':
         path    => '/usr/sbin/ebtables-legacy',
     }
-
-    # Hack to fix pyroute2
-    #
-    # Upstream bug: https://bugs.launchpad.net/neutron/+bug/1899141
-    #
-    # This fix was backported to train but isn't present in our packages.
-    # We'll need to check to see if it's present in U and V.
-    #
-    file { '/usr/lib/python3/dist-packages/neutron/agent/linux/ip_lib.py':
-        ensure  => 'present',
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        require => Package['neutron-linuxbridge-agent'],
-        notify  => Service['neutron-linuxbridge-agent'],
-        source  => 'puppet:///modules/openstack/ussuri/neutron/hacks/ip_lib.py';
-    }
 }
