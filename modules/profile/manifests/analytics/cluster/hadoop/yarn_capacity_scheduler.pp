0bb39bf9a37616330c8061aa5109f0f29bff8552
Luca <ltoscano@wikimedia.org>
2021-04-06 10:04:01 +0200

    hadoop: refactor and simplify the Yarn Capacity scheduler's settings
    Bug: T277062
    Change-Id: I7e9182fc4be9d329a7a8145331ad8ed49dc5e1be

--- modules/profile/manifests/analytics/cluster/hadoop/yarn_capacity_scheduler.pp
+++ modules/profile/manifests/analytics/cluster/hadoop/yarn_capacity_scheduler.pp
@@ -48,32 +48,20 @@ class profile::analytics::cluster::hadoop::yarn_capacity_scheduler (
         # Queue definitions
         # Sum of capacity (not max) needs to be 100 at any level/branch of the tree
         # First layer
-        'yarn.scheduler.capacity.root.queues' => 'users,production',
-        'yarn.scheduler.capacity.root.production.capacity' => 60,
+        'yarn.scheduler.capacity.root.queues' => 'fifo,default,production,essential',
+        'yarn.scheduler.capacity.root.fifo.capacity' => 5,
+        'yarn.scheduler.capacity.root.fifo.maximum-capacity' => -1,
+        'yarn.scheduler.capacity.root.default.capacity' => 35,
+        'yarn.scheduler.capacity.root.default.maximum-capacity' => -1,
+        'yarn.scheduler.capacity.root.production.capacity' => 50,
         'yarn.scheduler.capacity.root.production.maximum-capacity' => -1,
-        'yarn.scheduler.capacity.root.users.capacity' => 40,
-        'yarn.scheduler.capacity.root.users.maximum-capacity' => -1,
-        # Second layer (users)
-        'yarn.scheduler.capacity.root.users.queues' => 'default, fifo',
-        'yarn.scheduler.capacity.root.users.default.capacity' => 80,
-        'yarn.scheduler.capacity.root.users.default.maximum-capacity' => -1,
-        'yarn.scheduler.capacity.root.users.fifo.capacity' => 20,
-        'yarn.scheduler.capacity.root.users.fifo.maximum-capacity' => -1,
-        # Second layer (production)
-        'yarn.scheduler.capacity.root.production.queues' => 'analytics,search,product,ingest',
-        'yarn.scheduler.capacity.root.production.analytics.capacity' => 40,
-        'yarn.scheduler.capacity.root.production.analytics.maximum-capacity' => -1,
-        'yarn.scheduler.capacity.root.production.search.capacity' => 30,
-        'yarn.scheduler.capacity.root.production.search.maximum-capacity' => -1,
-        'yarn.scheduler.capacity.root.production.product.capacity' => 10,
-        'yarn.scheduler.capacity.root.production.product.maximum-capacity' => -1,
-        'yarn.scheduler.capacity.root.production.ingest.capacity' => 20,
-        'yarn.scheduler.capacity.root.production.ingest.maximum-capacity' => -1,
+        'yarn.scheduler.capacity.root.essential.capacity' => 10,
+        'yarn.scheduler.capacity.root.essential.maximum-capacity' => -1,
 
         # Default mappings
         # PLEASE NOTE: use only the leaf queue names, not full path.
         # Example: root.production.analytics BAD, analytics GOOD
-        'yarn.scheduler.capacity.queue-mappings' => 'u:druid:analytics,u:analytics:analytics,u:analytics-search:search,u:analytics-product:product,g:analytics-privatedata-users:default',
+        'yarn.scheduler.capacity.queue-mappings' => 'u:druid:production,u:analytics:production,u:analytics-search:production,u:analytics-product:production,g:analytics-privatedata-users:default',
 
         # Limits
         # https://docs.cloudera.com/HDPDocuments/HDP2/HDP-2.6.4/bk_yarn-resource-management/content/setting_user_limits.html
@@ -81,28 +69,29 @@ class profile::analytics::cluster::hadoop::yarn_capacity_scheduler (
         # The user limit factor is a multiplier used to allow users of a specific queue to take up to X
         # times the resource allocated (as min value) for the queue. It is needed to allow/control elasticity,
         # so users can overcome Yarn default limits in case there are free resources.
-        'yarn.scheduler.capacity.root.users.default.user-limit-factor' => 2,
-        'yarn.scheduler.capacity.root.production.analytics.user-limit-factor' => 5,
-        'yarn.scheduler.capacity.root.production.search.user-limit-factor' => 5,
-        'yarn.scheduler.capacity.root.production.product.user-limit-factor' => 10,
-        'yarn.scheduler.capacity.root.production.ingest.user-limit-factor' => 10,
+        'yarn.scheduler.capacity.root.fifo.user-limit-factor' => 5,
+        'yarn.scheduler.capacity.root.default.user-limit-factor' => 2,
+        'yarn.scheduler.capacity.root.production.user-limit-factor' => 2,
+        'yarn.scheduler.capacity.root.essential.user-limit-factor' => 10,
         # The user limit percent is different from the factor, since it is about how many users can run jobs on a queue
         # at any given time. For example, if we set:
         # 'yarn.scheduler.capacity.root.production.analytics.minimum-user-limit-percent' => 50,
         # we want to allow up to two users concurrently in the queue (druid and analytics), leaving the others waiting.
         # If we use '25', we'll allow a max of 4 different users, etc..
-        'yarn.scheduler.capacity.root.production.analytics.minimum-user-limit-percent' => 50,
-        'yarn.scheduler.capacity.root.production.search.minimum-user-limit-percent' => 100,
-        'yarn.scheduler.capacity.root.production.product.minimum-user-limit-percent' => 100,
-        'yarn.scheduler.capacity.root.users.default.minimum-user-limit-percent' => 10,
-        'yarn.scheduler.capacity.root.users.default.maximum-application-lifetime' => 604800, # 1 week in seconds
+        'yarn.scheduler.capacity.root.fifo.minimum-user-limit-percent' => 100,
+        'yarn.scheduler.capacity.root.default.minimum-user-limit-percent' => 10,
+        'yarn.scheduler.capacity.root.production.minimum-user-limit-percent' => 5,
+        'yarn.scheduler.capacity.root.essential.minimum-user-limit-percent' => 50,
+
+        # Max lifetime for a Yarn application
+        'yarn.scheduler.capacity.root.default.maximum-application-lifetime' => 604800, # 1 week in seconds
+        'yarn.scheduler.capacity.root.fifo.maximum-application-lifetime' => 604800, # 1 week in seconds
 
         # Ordering policy
-        'yarn.scheduler.capacity.root.production.analytics.ordering-policy' => 'fair',
-        'yarn.scheduler.capacity.root.production.search.ordering-policy' => 'fair',
-        'yarn.scheduler.capacity.root.production.product.ordering-policy' => 'fair',
-        'yarn.scheduler.capacity.root.users.default.ordering-policy' => 'fair',
-        'yarn.scheduler.capacity.root.users.fifo.ordering-policy' => 'fifo',
+        'yarn.scheduler.capacity.root.fifo.ordering-policy' => 'fifo',
+        'yarn.scheduler.capacity.root.default.ordering-policy' => 'fair',
+        'yarn.scheduler.capacity.root.production.ordering-policy' => 'fair',
+        'yarn.scheduler.capacity.root.essential.ordering-policy' => 'fair',
 
         # Labels
         # https://hadoop.apache.org/docs/r2.10.0/hadoop-yarn/hadoop-yarn-site/NodeLabel.html
@@ -112,42 +101,25 @@ class profile::analytics::cluster::hadoop::yarn_capacity_scheduler (
         # use the GPU label, so we concentrate all the capacity to it.
         'yarn.scheduler.capacity.root.accessible-node-labels' => 'GPU',
         'yarn.scheduler.capacity.root.accessible-node-labels.GPU.capacity' => '100',
-        'yarn.scheduler.capacity.root.users.accessible-node-labels' => 'GPU',
-        'yarn.scheduler.capacity.root.users.accessible-node-labels.GPU.capacity' => '100',
-        'yarn.scheduler.capacity.root.production.accessible-node-labels' => ' ',
-        'yarn.scheduler.capacity.root.users.fifo.accessible-node-labels.GPU.capacity' => '100',
-        'yarn.scheduler.capacity.root.users.fifo.accessible-node-labels.GPU.max-capacity' => '100',
-        'yarn.scheduler.capacity.root.users.default.accessible-node-labels.GPU.capacity' => '0',
-        'yarn.scheduler.capacity.root.users.default.accessible-node-labels.GPU.max-capacity' => '0',
-        'yarn.scheduler.capacity.root.users.fifo.default-node-label-expression' => 'GPU',
+        'yarn.scheduler.capacity.root.fifo.accessible-node-labels' => 'GPU',
+        'yarn.scheduler.capacity.root.fifo.accessible-node-labels.GPU.capacity' => '100',
 
         # ACLs
         # Permissions cannot be reduced on the lower layer of the tree once set for a specific
         # queue, they can only be incremented.
         'yarn.scheduler.capacity.root.acl_submit_applications' => ' ',
         'yarn.scheduler.capacity.root.acl_administer_queue' => ' ',
-        'yarn.scheduler.capacity.root.production.analytics.acl_submit_applications' => 'analytics,druid',
-        'yarn.scheduler.capacity.root.production.analytics.acl_administer_queue' => 'analytics-admins',
-        'yarn.scheduler.capacity.root.production.search.acl_submit_applications' => 'analytics-search',
-        # Note: the ' ' at the beginning is important, since the format is 'user1,user2,.. group1,group2,..'
-        'yarn.scheduler.capacity.root.production.search.acl_administer_queue' => ' analytics-search-users,analytics-admins',
-        'yarn.scheduler.capacity.root.production.product.acl_submit_applications' => 'analytics-product',
-        # Note: the ' ' at the beginning is important, since the format is 'user1,user2,.. group1,group2,..'
-        'yarn.scheduler.capacity.root.production.product.acl_administer_queue' => ' analytics-product-users,analytics-admins',
-        'yarn.scheduler.capacity.root.production.ingest.acl_submit_applications' => 'analytics',
-        # Note: the ' ' at the beginning is important, since the format is 'user1,user2,.. group1,group2,..'
-        'yarn.scheduler.capacity.root.production.ingest.acl_administer_queue' => ' analytics-admins',
-        # Note: the ' ' at the beginning is important, since the format is 'user1,user2,.. group1,group2,..'
-        'yarn.scheduler.capacity.root.users.default.acl_submit_applications' => ' analytics-privatedata-users',
-        # Note: the ' ' at the beginning is important, since the format is 'user1,user2,.. group1,group2,..'
-        'yarn.scheduler.capacity.root.users.default.acl_administer_queue' => ' analytics-privatedata-users,analytics-admins',
-        # Note: the ' ' at the beginning is important, since the format is 'user1,user2,.. group1,group2,..'
-        'yarn.scheduler.capacity.root.users.fifo.acl_submit_applications' => ' analytics-privatedata-users',
-        # Note: the ' ' at the beginning is important, since the format is 'user1,user2,.. group1,group2,..'
-        'yarn.scheduler.capacity.root.users.fifo.acl_administer_queue' => ' analytics-privatedata-users,analytics-admins',
+        'yarn.scheduler.capacity.root.fifo.acl_submit_applications' => ' analytics-privatedata-users',
+        'yarn.scheduler.capacity.root.fifo.acl_administer_queue' => ' analytics-privatedata-users',
+        'yarn.scheduler.capacity.root.default.acl_submit_applications' => ' analytics-privatedata-users',
+        'yarn.scheduler.capacity.root.default.acl_administer_queue' => ' analytics-privatedata-users',
+        'yarn.scheduler.capacity.root.production.acl_submit_applications' => 'analytics,druid,analytics-search,analytics-product',
+        'yarn.scheduler.capacity.root.production.acl_administer_queue' => ' analytics-admins,analytics-search-users,analytics-product-users',
+        'yarn.scheduler.capacity.root.essential.acl_submit_applications' => 'analytics,druid',
+        'yarn.scheduler.capacity.root.essential.acl_administer_queue' => ' analytics-admins',
 
         # Preemption
-        'yarn.scheduler.capacity.root.production.ingest.disable_preemption' => true,
+        'yarn.scheduler.capacity.root.essential.disable_preemption' => true,
     }
 
     $scheduler_settings = $base_settings + $extra_settings
