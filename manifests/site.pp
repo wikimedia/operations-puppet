8dbffcfb1d56902c11b14ce05221e6f5ed945c60
Manuel Arostegui <marostegui@wikimedia.org>
2021-04-06 09:19:50 +0200

    mariadb: Productionize db1184
    db1184 will go to s1
    Will be cloned from db1169
    
    Bug: T275633
    Change-Id: Icbcf33d1d3c7fb4b0fee19c3db749e201b2898fe

--- manifests/site.pp
+++ manifests/site.pp
@@ -402,7 +402,7 @@ node 'db1083.eqiad.wmnet' {
 }
 # eqiad replicas
 # See also db1099 and db1105 below
-node /^db1(106|118|119|134|135|163|164|169)\.eqiad\.wmnet/ {
+node /^db1(106|118|119|134|135|163|164|169|184)\.eqiad\.wmnet/ {
     role(mariadb::core)
 }
 
@@ -1129,7 +1129,7 @@ node /^failoid[12]001\.(eqiad|codfw)\.wmnet$/ {
 # 9 expansion hosts T273566
 # 2 hosts (db1154 and db1155) will be used to temporary test sanitarium upgrades to 10.4 T268742
 # Set them to spare individually as it will take take to transfer the data
-node /^db11(56|58|67|76|77|78|79|80|82|83|84)\.eqiad\.wmnet$/ {
+node /^db11(56|58|67|76|77|78|79|80|82|83)\.eqiad\.wmnet$/ {
     role(insetup)
 }
 
