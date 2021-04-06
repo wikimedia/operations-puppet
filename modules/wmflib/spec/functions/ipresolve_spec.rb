93a4210ff59f5c1599a67b4b66f5140afe61ef22
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-04-06 11:55:07 +0200

    wmflib: Switch spec test to bast1003
    bast1002 will be decommissioned soon.
    
    Bug: T276399
    Change-Id: I8e90311b7e96e5421edf2c1cd2642d6ff4d2038b

--- modules/wmflib/spec/functions/ipresolve_spec.rb
+++ modules/wmflib/spec/functions/ipresolve_spec.rb
@@ -2,19 +2,19 @@ require_relative '../../../../rake_modules/spec_helper'
 
 describe 'ipresolve' do
   it "should resolve ipv4 addresses by default" do
-    should run.with_params('bast1002.wikimedia.org').and_return('208.80.154.86')
+    should run.with_params('bast1003.wikimedia.org').and_return('208.80.155.110')
   end
   it "should resolve ipv4 addresses when explicitly asked to" do
-    should run.with_params('bast1002.wikimedia.org', '4').and_return('208.80.154.86')
+    should run.with_params('bast1003.wikimedia.org', '4').and_return('208.80.155.110')
   end
 
   it "should resolve ipv6 addresses" do
-    should run.with_params('bast1002.wikimedia.org', '6').and_return('2620::861:3:208:80:154:86')
+    should run.with_params('bast1003.wikimedia.org', '6').and_return('2620::861:4:208:80:155:110')
   end
 
   it "should be able to perform a reverse DNS lookup" do
-    should run.with_params('2620:0:861:3:208:80:154:86', 'ptr').and_return('bast1002.wikimedia.org')
-    should run.with_params('208.80.154.86', 'ptr').and_return('bast1002.wikimedia.org')
+    should run.with_params('2620:0:861:4:208:80:155:110', 'ptr').and_return('bast1003.wikimedia.org')
+    should run.with_params('208.80.155.110', 'ptr').and_return('bast1003.wikimedia.org')
   end
 
   it "fails when resolving an inexistent name" do
