354a5b72e4b3be12e6e558a7505eebb0741be162
Amir Sarabadani <Ladsgroup@gmail.com>
2021-04-06 10:58:58 +0200

    tendril: Remove absented crons
    Bug: T273673
    Change-Id: I2de6a124bf14499c9e129fe4276f3c0b3c81b66f

--- modules/tendril/manifests/maintenance.pp
+++ modules/tendril/manifests/maintenance.pp
@@ -1,4 +1,4 @@
-# This class sets up the cron jobs needed for the correct execution
+# This class sets up the regular jobs needed for the correct execution
 # of the tendril web interface and database
 # It does not require being run on the same server than the web
 # or the database, but it requires having mysql access
@@ -50,35 +50,11 @@ class tendril::maintenance (
         source => 'puppet:///modules/tendril/tendril-cron-5m.pl',
     }
 
-    file { '/var/log/tendril-cron-5m.log':
-        owner => 'tendril',
-        group => 'tendril',
-        mode  => '0640',
-    }
-
-    file { '/var/log/tendril-cron-5m.err':
-        owner => 'tendril',
-        group => 'tendril',
-        mode  => '0640',
-    }
-
     file { '/usr/local/bin/tendril-queries.pl':
         owner  => 'tendril',
         group  => 'tendril',
         mode   => '0750',
         source => 'puppet:///modules/tendril/tendril-queries.pl',
-    }
-
-    file { '/var/log/tendril-queries.log':
-        owner => 'tendril',
-        group => 'tendril',
-        mode  => '0640',
-    }
-
-    file { '/var/log/tendril-queries.err':
-        owner => 'tendril',
-        group => 'tendril',
-        mode  => '0640',
     }
 
     file { '/etc/mysql/tendril.cnf':
@@ -88,17 +64,6 @@ class tendril::maintenance (
         content => template('tendril/tendril.cnf.erb'),
     }
 
-    cron { 'tendril-cron-5m':
-        ensure  => absent,
-        user    => 'tendril',
-        command => '/usr/local/bin/tendril-cron-5m.pl /etc/mysql/tendril.cnf > /var/log/tendril-cron-5m.log 2> /var/log/tendril-cron-5m.err',
-        minute  => '*/5',
-        require => [
-            File['/usr/local/bin/tendril-cron-5m.pl'],
-            File['/var/log/tendril-cron-5m.log'],
-            File['/var/log/tendril-cron-5m.err'],
-        ]
-    }
     systemd::timer::job { 'tendril-5m':
         ensure      => $ensure,
         description => 'Regular jobs to refresh some statistics about tendril',
@@ -111,18 +76,6 @@ class tendril::maintenance (
         interval    => {'start' => 'OnCalendar', 'interval' => '*-*-* *:0/5:0'},
     }
 
-    cron { 'tendril-queries':
-        ensure  => absent,
-        user    => 'tendril',
-        command => '/usr/local/bin/tendril-queries.pl /etc/mysql/tendril.cnf > /var/log/tendril-queries.log 2> /var/log/tendril-queries.err',
-        minute  => '*/5',
-        require => [
-            File['/usr/local/bin/tendril-queries.pl'],
-            File['/var/log/tendril-queries.log'],
-            File['/var/log/tendril-queries.err'],
-            File['/etc/mysql/tendril.cnf'],
-        ]
-    }
     systemd::timer::job { 'tendril-queries':
         ensure      => $ensure,
         description => 'Regular jobs to refresh some statistics about tendril',
