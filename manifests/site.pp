2fad2ff6840dc2309e7593aa96c4c5ea38805659
Manuel Arostegui <marostegui@wikimedia.org>
2021-04-07 15:12:45 +0200

    mariadb: Add db1180 to s6
    Extra host for s6.
    Cloned from db1173
    
    Bug: T275633
    Change-Id: Id2c64e237efcdccfe271a1816ac3d2b647647e17

--- manifests/site.pp
+++ manifests/site.pp
@@ -535,7 +535,7 @@ node 'db1131.eqiad.wmnet' {
 
 # eqiad replicas
 # See also db1096, db1098 and db1113 below
-node /^db1(085|165|168|173)\.eqiad\.wmnet/ {
+node /^db1(085|165|168|173|180)\.eqiad\.wmnet/ {
     role(mariadb::core)
 }
 
@@ -1133,7 +1133,7 @@ node /^failoid[12]001\.(eqiad|codfw)\.wmnet$/ {
 # 9 expansion hosts T273566
 # 2 hosts (db1154 and db1155) will be used to temporary test sanitarium upgrades to 10.4 T268742
 # Set them to spare individually as it will take take to transfer the data
-node /^db11(56|58|67|76|77|78|79|80|82|83)\.eqiad\.wmnet$/ {
+node /^db11(56|58|67|76|77|78|79|82|83)\.eqiad\.wmnet$/ {
     role(insetup)
 }
 
