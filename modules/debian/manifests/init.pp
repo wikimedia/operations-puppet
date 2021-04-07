217ba6427d3440573ddf3ab45c4b3fac6803a695
John Bond <jbond@wikimedia.org>
2021-04-07 11:07:50 +0200

    debian: Fail if the release values are not numberes
    While a debian release is still in testing the value in
    /etc/debian_version contains a string simlar buster/sid, however facter
    expects an number like 11.0.  This CR updates the debian module so that
    it will fail early with a more descriptive error message i this
    situation
    
    Change-Id: Idd87f11bc7490edca26412ae14c247cbd16e0ea4

--- modules/debian/manifests/init.pp
+++ modules/debian/manifests/init.pp
@@ -15,4 +15,12 @@ class debian {
     unless $facts['os']['name'] in $supported {
         fail("invalid Derivative (${$facts['os']}). supported derivatives: ${supported.keys.join(', ')}")
     }
+    # Before a debian release is stable /etc/debian_version, which is what
+    # facter uses to calculate the release values, is equal to $codename/sid
+    # instead of the expected point release value e.g. 11.0.  This causes this
+    # module to fail as it expects theses values to be numbers
+    unless $facts['os']['release']['major'] =~ /\d+/ {
+        fail("unsupported: facts['os']['release'['major'] (${facts['os']['release']['major']}) is not a number")
+    }
+
 }
