46b12bd3d0cdd6c2d3b998ebd96b9fe7c3d2d220
Amir Sarabadani <Ladsgroup@gmail.com>
2021-04-10 21:58:03 +0200

    snapshot: Migrate cronjobs in pagetitles to systemd timer
    First of many patches to move snapshot crons to systemd timers.
    
    This is part of the work needed to upgrade puppet to version six as
    crons are not supported in core anymore.
    
    Hosts: snapshot1008.eqiad.wmnet
    Bug: T273673
    Change-Id: I52a28d680bc9f0dbc7225f29e67e9bca48cf7697

--- modules/snapshot/manifests/cron/pagetitles.pp
+++ modules/snapshot/manifests/cron/pagetitles.pp
@@ -8,21 +8,43 @@ class snapshot::cron::pagetitles(
 
     if !$filesonly {
         cron { 'pagetitles-ns0':
-            ensure      => 'present',
+            ensure      => absent,
             environment => 'MAILTO=ops-dumps@wikimedia.org',
             user        => $user,
             command     => "cd ${repodir}; python3 onallwikis.py --configfile ${confsdir}/wikidump.conf.dumps:monitor  --filenameformat '{w}-{d}-all-titles-in-ns-0.gz' --outdir '${cronsdir}/pagetitles/{d}' --query \"'select page_title from page where page_namespace=0;'\"",
             minute      => '10',
             hour        => '8',
         }
+        systemd::timer::job { 'pagetitles-ns0':
+            ensure             => present,
+            description        => 'Regular jobs to build snapshot of page titles of main namespace',
+            user               => $user,
+            monitoring_enabled => false,
+            send_mail          => true,
+            environment        => {'MAILTO' => 'ops-dumps@wikimedia.org'},
+            working_directory  => $repodir,
+            command            => "/usr/bin/python3 onallwikis.py --configfile ${confsdir}/wikidump.conf.dumps:monitor  --filenameformat '{w}-{d}-all-titles-in-ns-0.gz' --outdir '${cronsdir}/pagetitles/{d}' --query \"'select page_title from page where page_namespace=0;'\"",
+            interval           => {'start' => 'OnCalendar', 'interval' => '*-*-* 8:10:0'},
+        }
 
         cron { 'pagetitles-ns6':
-            ensure      => 'present',
+            ensure      => absent,
             environment => 'MAILTO=ops-dumps@wikimedia.org',
             user        => $user,
             command     => "cd ${repodir}; python3 onallwikis.py --configfile ${confsdir}/wikidump.conf.dumps:monitor  --filenameformat '{w}-{d}-all-media-titles.gz' --outdir '${cronsdir}/mediatitles/{d}' --query \"'select page_title from page where page_namespace=6;'\"",
             minute      => '50',
             hour        => '8',
+        }
+        systemd::timer::job { 'pagetitles-ns6':
+            ensure             => present,
+            description        => 'Regular jobs to build snapshot of page titles of file namespace',
+            user               => $user,
+            monitoring_enabled => false,
+            send_mail          => true,
+            environment        => {'MAILTO' => 'ops-dumps@wikimedia.org'},
+            working_directory  => $repodir,
+            command            => "/usr/bin/python3 onallwikis.py --configfile ${confsdir}/wikidump.conf.dumps:monitor  --filenameformat '{w}-{d}-all-media-titles.gz' --outdir '${cronsdir}/mediatitles/{d}' --query \"'select page_title from page where page_namespace=6;'\"",
+            interval           => {'start' => 'OnCalendar', 'interval' => '*-*-* 8:50:0'},
         }
     }
 }
