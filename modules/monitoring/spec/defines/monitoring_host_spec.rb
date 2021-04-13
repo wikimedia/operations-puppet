6f813489ba71d58287c1487587177ed5f6b7ff0c
John Bond <jbond@wikimedia.org>
2021-04-13 17:14:26 +0200

    monitoring: fix spec tests for monitoring class
    Change-Id: I5ec777d55fd5f944a1b1243b7a5536215424d080

--- modules/monitoring/spec/defines/monitoring_host_spec.rb
+++ modules/monitoring/spec/defines/monitoring_host_spec.rb
@@ -1,224 +1,172 @@
 require_relative '../../../../rake_modules/spec_helper'
 
 describe 'monitoring::host' do
-  before(:each) do
-    Puppet::Parser::Functions.newfunction(:secret, :type => :rvalue) { |_|
-      'fake_secret'
-    }
-  end
-
-  context 'with a standard physical host' do
-    let(:pre_condition){
-      """
-    class passwords::nagios::mysql {
-      $mysql_check_pass='foo'
-    }
-    class {'passwords::nagios::mysql': }
-    """
-    }
-    let(:node_params) { {'cluster' => 'ci', 'site' => 'eqiad'} }
-
-    let(:facts) {
-      {
-        :hostname        => 'ahost',
-        :operatingsystem => 'Debian',
-        :ipaddress       => '1.2.3.4',
-        :is_virtual      => false,
-        :lldp_parent     => 'ahosts_parent',
-        :has_ipmi        => true,
-        :ipmi_lan        => { :ipaddress => '2.2.2.2', },
-        :lsbdistrelease  => '9.3',
-        :lsbdistid       => 'Debian',
-        :initsystem      => 'systemd',
-      }
-    }
-    let(:title) { 'ahost' }
-    it { should compile }
-
-    describe 'with no parameters' do
-      subject { exported_resources }
-      it do
-        should contain_nagios_host('ahost').with(
-          'host_name'  => 'ahost',
-          'parents'    => 'ahosts_parent',
-          'icon_image' => 'vendors/debian.png',
-          'address'    => '1.2.3.4'
-        )
-        should contain_nagios_host('ahost.mgmt').with(
-          'host_name'  => 'ahost.mgmt',
-          'address'    => '2.2.2.2'
+  on_supported_os(WMFConfig.test_on).each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge!(
+          ipaddress: '192.0.2.42',
+          hostname: 'ahost',
+          lldp_parent: 'ahosts_parent',
+          has_ipmi: true,
+          ipmi_lan: {'ipaddress' => '198.51.100.42' }
         )
       end
-    end
+      let(:node_params) { {'cluster' => 'ci', 'site' => 'eqiad'} }
+      let(:title) { 'ahost' }
 
-    describe 'with a parents parameters' do
-      let(:params) {
-        {
-          :parents => 'aparent',
-        }
-      }
+      context 'with a standard physical host' do
+        let(:facts) { super().merge(is_virtual: false) }
 
-      subject { exported_resources }
-      it do
-        should contain_nagios_host('ahost').with(
-          'host_name'  => 'ahost',
-          'parents'    => 'aparent',
-          'icon_image' => 'vendors/debian.png',
-          'address'    => '1.2.3.4'
-        )
-        should contain_nagios_host('ahost.mgmt').with(
-          'host_name'  => 'ahost.mgmt',
-          'address'    => '2.2.2.2'
-        )
+        it { should compile }
+
+        describe 'with no parameters' do
+          subject { exported_resources }
+          it do
+            should contain_nagios_host('ahost').with(
+              'host_name'  => 'ahost',
+              'parents'    => 'ahosts_parent',
+              'icon_image' => 'vendors/debian.png',
+              'address'    => '192.0.2.42'
+            )
+            should contain_nagios_host('ahost.mgmt').with(
+              'host_name'  => 'ahost.mgmt',
+              'address'    => '198.51.100.42'
+            )
+          end
+        end
+
+        describe 'with a parents parameters' do
+          let(:params) {
+            {
+              :parents => 'aparent',
+            }
+          }
+
+          subject { exported_resources }
+          it do
+            should contain_nagios_host('ahost').with(
+              'host_name'  => 'ahost',
+              'parents'    => 'aparent',
+              'icon_image' => 'vendors/debian.png',
+              'address'    => '192.0.2.42'
+            )
+            should contain_nagios_host('ahost.mgmt').with(
+              'host_name'  => 'ahost.mgmt',
+              'address'    => '198.51.100.42'
+            )
+          end
+        end
       end
-    end
-  end
 
-  context 'with a standard virtual host' do
-    let(:pre_condition){
-      """
-    class passwords::nagios::mysql {
-      $mysql_check_pass='foo'
-    }
-    class {'passwords::nagios::mysql': }
-    """
-    }
-    let(:node_params) { {'cluster' => 'ci', 'site' => 'eqiad'} }
+      context 'with a standard virtual host' do
+        let(:facts) { super().merge(is_virtual: true, has_ipmi: false) }
 
-    let(:facts) {
-      {
-        :hostname        => 'ahost',
-        :operatingsystem => 'Debian',
-        :ipaddress       => '1.2.3.4',
-        :is_virtual      => true,
-        :lldp_parent     => 'ahosts_parent',
-        :has_ipmi        => false,
-        :lsbdistrelease  => '9.3',
-        :lsbdistid       => 'Debian',
-        :initsystem      => 'systemd',
-      }
-    }
-    let(:title) { 'ahost' }
-
-    it { should compile }
-    describe 'with no parameters' do
-      subject { exported_resources }
-      it do
-        should contain_nagios_host('ahost').with(
-          'host_name'  => 'ahost',
-          'parents'    => nil,
-          'icon_image' => 'vendors/debian.png',
-          'address'    => '1.2.3.4'
-        )
-        should_not contain_nagios_host('ahost.mgmt')
+        it { should compile }
+        describe 'with no parameters' do
+          subject { exported_resources }
+          it do
+            should contain_nagios_host('ahost').with(
+              'host_name'  => 'ahost',
+              'parents'    => nil,
+              'icon_image' => 'vendors/debian.png',
+              'address'    => '192.0.2.42'
+            )
+            should_not contain_nagios_host('ahost.mgmt')
+          end
+        end
+        describe 'with a parents parameters' do
+          let(:params) {
+            {
+              :parents => 'aparent',
+            }
+          }
+          subject { exported_resources }
+          it do
+            should contain_nagios_host('ahost').with(
+              'host_name'  => 'ahost',
+              'parents'    => 'aparent',
+              'icon_image' => 'vendors/debian.png',
+              'address'    => '192.0.2.42'
+            )
+            should_not contain_nagios_host('ahost.mgmt')
+          end
+        end
       end
-    end
-    describe 'with a parents parameters' do
-      let(:params) {
-        {
-          :parents => 'aparent',
-        }
-      }
-      subject { exported_resources }
-      it do
-        should contain_nagios_host('ahost').with(
-          'host_name'  => 'ahost',
-          'parents'    => 'aparent',
-          'icon_image' => 'vendors/debian.png',
-          'address'    => '1.2.3.4'
-        )
-        should_not contain_nagios_host('ahost.mgmt')
-      end
-    end
-  end
 
-  context 'with an icinga host' do
-    let(:pre_condition){
-      """
+      context 'with an icinga host' do
+        let(:pre_condition){
+          """
     class profile::base { $notifications_enabled = '1' }
-    class passwords::nagios::mysql {
-      $mysql_check_pass='foo'
-    }
     include ::profile::base
     class { 'icinga': icinga_user => 'icinga', icinga_group => 'icinga' }
-    """
-    }
-    let(:node_params) { {'cluster' => 'ci', 'site' => 'eqiad'} }
-
-    let(:facts) {
-      {
-        :hostname        => 'icingahost',
-        :operatingsystem => 'Debian',
-        :ipaddress       => '1.2.3.4',
-        :is_virtual      => false,
-        :lldp_parent     => 'ahosts_parent',
-        :has_ipmi        => true,
-        :ipmi_lan        => { :ipaddress => '2.2.2.2', },
-        :lsbdistrelease  => '9.3',
-        :lsbdistid       => 'Debian',
-        :initsystem      => 'systemd',
-      }
-    }
-
-    describe 'monitoring itself' do
-      let(:title) { 'icingahost' }
-      it do
-        should contain_nagios_host('icingahost').with(
-          'host_name'  => 'icingahost',
-          'parents'    => 'ahosts_parent',
-          'icon_image' => 'vendors/debian.png',
-          'address'    => '1.2.3.4'
-        )
-        should contain_nagios_host('icingahost.mgmt').with(
-          'host_name'  => 'icingahost.mgmt',
-          'address'    => '2.2.2.2'
-        )
-      end
-    end
-
-    describe 'monitoring a service, no params' do
-      let(:title) { 'service.svc.wmnet' }
-      it do
-        should contain_nagios_host('service.svc.wmnet').with(
-          'host_name'  => 'service.svc.wmnet',
-          'parents'    => nil,
-          'icon_image' => nil,
-          'address'    => '1.2.3.4'
-        )
-        should_not contain_nagios_host('service.svc.wmnet.mgmt')
-      end
-    end
-    describe 'monitoring a service, with ip_address,parents' do
-      let(:title) { 'service.svc.wmnet' }
-      let(:params) {
-        { :ip_address => '4.3.2.1',
-          :parents    => 'service_parent',
+          """
         }
-      }
-      it do
-        should contain_nagios_host('service.svc.wmnet').with(
-          'host_name'  => 'service.svc.wmnet',
-          'parents'    => 'service_parent',
-          'icon_image' => nil,
-          'address'    => '4.3.2.1'
-        )
-        should_not contain_nagios_host('service.svc.wmnet.mgmt')
-      end
-    end
-    describe 'monitoring a service, with fqdn' do
-      let(:title) { 'service.svc.wmnet' }
-      let(:params) {
-        { :host_fqdn => 'blah.foo.bar',
-        }
-      }
-      it do
-        should contain_nagios_host('service.svc.wmnet').with(
-          'host_name'  => 'service.svc.wmnet',
-          'parents'    => nil,
-          'icon_image' => nil,
-          'address'    => 'blah.foo.bar'
-        )
-        should_not contain_nagios_host('service.svc.wmnet.mgmt')
+        let(:node_params) { {'cluster' => 'ci', 'site' => 'eqiad'} }
+
+        describe 'monitoring itself' do
+          let(:title) { 'icingahost' }
+          let(:facts) { super().merge(is_virtual: false, hostname: 'icingahost') }
+
+          it do
+            should contain_nagios_host('icingahost').with(
+              'host_name'  => 'icingahost',
+              'parents'    => 'ahosts_parent',
+              'icon_image' => 'vendors/debian.png',
+              'address'    => '192.0.2.42'
+            )
+            should contain_nagios_host('icingahost.mgmt').with(
+              'host_name'  => 'icingahost.mgmt',
+              'address'    => '198.51.100.42'
+            )
+          end
+        end
+
+        describe 'monitoring a service, no params' do
+          let(:title) { 'service.svc.wmnet' }
+          it do
+            should contain_nagios_host('service.svc.wmnet').with(
+              'host_name'  => 'service.svc.wmnet',
+              'parents'    => nil,
+              'icon_image' => nil,
+              'address'    => '192.0.2.42'
+            )
+            should_not contain_nagios_host('service.svc.wmnet.mgmt')
+          end
+        end
+        describe 'monitoring a service, with ip_address,parents' do
+          let(:title) { 'service.svc.wmnet' }
+          let(:params) {
+            { :ip_address => '4.3.2.1',
+              :parents    => 'service_parent',
+          }
+          }
+          it do
+            should contain_nagios_host('service.svc.wmnet').with(
+              'host_name'  => 'service.svc.wmnet',
+              'parents'    => 'service_parent',
+              'icon_image' => nil,
+              'address'    => '4.3.2.1'
+            )
+            should_not contain_nagios_host('service.svc.wmnet.mgmt')
+          end
+        end
+        describe 'monitoring a service, with fqdn' do
+          let(:title) { 'service.svc.wmnet' }
+          let(:params) {
+            { :host_fqdn => 'blah.foo.bar',
+          }
+          }
+          it do
+            should contain_nagios_host('service.svc.wmnet').with(
+              'host_name'  => 'service.svc.wmnet',
+              'parents'    => nil,
+              'icon_image' => nil,
+              'address'    => 'blah.foo.bar'
+            )
+            should_not contain_nagios_host('service.svc.wmnet.mgmt')
+          end
+        end
       end
     end
   end
