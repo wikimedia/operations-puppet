1994b503a5493930889eab1ea35f084848766456
Emanuele Rocca <ema@wikimedia.org>
2021-04-13 10:12:29 +0200

    cache: explicitly list object sizes in exp_policy.py
    Make object size distribution explicit so that it's easier to test
    specific values.
    
    Bug: T275809
    Change-Id: Iec0446d18fd1fe7be1c289c28cc9d63ce47ca626

--- modules/varnish/files/exp_policy.py
+++ modules/varnish/files/exp_policy.py
@@ -11,8 +11,22 @@ BASE = -20.3
 
 ADM_PARAM = (MEMORY ** RATE) / (2.0 ** BASE)
 
-for i in range(10, 23):
-    size = 2 ** i
+objsize = (
+    4096,
+    32768,
+    65536,
+    131072,
+    262144,
+    524288,
+    1048576,
+    2097152,
+    3145728,
+    4194304,
+    5242880,
+    8388608,
+)
+
+for size in objsize:
     clen_neg = -1.0 * size
     prob = math.exp(clen_neg / ADM_PARAM)
     print("{: 7} KB {:.3}%".format(size / 1024, 100 * prob))
