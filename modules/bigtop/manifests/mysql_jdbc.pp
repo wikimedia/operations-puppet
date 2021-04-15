a356f8f755bd92043301c97917b8227bd406b1f9
Luca <ltoscano@wikimedia.org>
2021-04-15 14:16:01 +0200

    bigtop::mysql_jdbc: add general use case for jar_path
    Now that we have only libmysql-java, we can use a general $jar_path
    variable.
    
    Change-Id: I0f5ca2020f5bd558645a2f8be32a5aa4f1e28a34

--- modules/bigtop/manifests/mysql_jdbc.pp
+++ modules/bigtop/manifests/mysql_jdbc.pp
@@ -7,12 +7,13 @@ define bigtop::mysql_jdbc (
     String $link_path,
     Optional[Boolean] $use_mysql_jar = false,
 ) {
+    $jar_path = $use_mysql_jar ? {
+        true    => '/usr/share/java/mysql.jar',
+        default => '/usr/share/java/mysql-connector-java.jar',
+    }
+
     if debian::codename::le('stretch') {
         $package_name = 'libmysql-java'
-        $jar_path = $use_mysql_jar ? {
-            true    => '/usr/share/java/mysql.jar',
-            default => '/usr/share/java/mysql-connector-java.jar',
-        }
     } elsif debian::codename::eq('buster') {
         if !defined(Apt::Package_from_component['libmysql-java-component']) {
             apt::package_from_component { 'libmysql-java-component':
