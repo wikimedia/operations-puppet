602cd8257bf83ce2de2994f0a0b79eec9ee753de
Razzi Abuissa <rabuissa@wikimedia.org>
2021-04-15 15:30:07 -0700

    superset: Enable superset http check
    Forgot to enable this after fixing it.
    
    Bug: T277729
    Change-Id: I1c36811a63aeaf6141ec327f9e255cb21c919a02

--- modules/profile/manifests/superset.pp
+++ modules/profile/manifests/superset.pp
@@ -142,7 +142,6 @@ class profile::superset(
     }
 
     nrpe::monitor_service { 'check_superset_http':
-        ensure        => 'absent',
         nrpe_command  => "/usr/local/bin/check_superset_http ${user_header}",
         description   => 'Check that superset http server is responding ok',
         require       => File['/usr/local/bin/check_superset_http'],
