a810f0ce106b4bd967437b99b4dcc446d7a22c98
Amir Sarabadani <Ladsgroup@gmail.com>
2021-04-20 15:06:12 +0200

    snapshot: Migrate cronjobs in dump_global_blocks to systemd timer
    This is part of the work needed to upgrade puppet to version six as
    crons are not supported in core anymore.
    
    Hosts: snapshot1008.eqiad.wmnet
    Bug: T273673
    Change-Id: I56411d41ee51bad5a3f36955e0a3858f934bcba3

--- modules/snapshot/manifests/cron/dump_global_blocks.pp
+++ modules/snapshot/manifests/cron/dump_global_blocks.pp
@@ -13,13 +13,23 @@ class snapshot::cron::dump_global_blocks(
 
     if !$filesonly {
         cron { 'global_blocks_dump':
-            ensure      => 'present',
+            ensure      => absent,
             command     => "/usr/local/bin/dump-global-blocks.sh --config ${confsdir}/wikidump.conf.other",
             environment => 'MAILTO=ops-dumps@wikimedia.org',
             user        => $user,
             minute      => '15',
             hour        => '8',
             weekday     => '6',
+        }
+        systemd::timer::job { 'global_blocks_dump':
+            ensure             => present,
+            description        => 'Regular jobs to build snapshot of globally blocked users',
+            user               => $user,
+            monitoring_enabled => false,
+            send_mail          => true,
+            environment        => {'MAILTO' => 'ops-dumps@wikimedia.org'},
+            command            => "/usr/local/bin/dump-global-blocks.sh --config ${confsdir}/wikidump.conf.other",
+            interval           => {'start' => 'OnCalendar', 'interval' => 'Sat *-*-* 8:15:0'},
         }
     }
 }
