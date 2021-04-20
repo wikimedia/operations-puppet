cfdfb9e76c848e048e1235f0f0618c61944c6945
Jcrespo <jcrespo@wikimedia.org>
2021-04-20 11:25:09 +0200

    dbbackups: Add enabled parameter backups on the cluster::management role
    Because we can sometimes have more than one host with the cluster::
    management role per datacenter at a time, have a way to disable its
    scheduling of new backups.
    
    A new parameter "enabled" has been added to the dbbackups::transfer
    profile, that defaults to true, but allows now disabling of the
    active systemd timers.
    
    While nothing horribly broke if there was multiple scheduling backup
    host per datacenter (either the missing configuration would make
    puppet complain, or one could add an empty configuration and the
    backups would run as a noop), it is nice to have a way to enable
    and disable the functionality on specific hosts/roles/datacenters.
    
    Bug: T138562
    Change-Id: I4991b2ed0b93f6ee39a75d10b79bf9f167b2f73c

--- modules/profile/manifests/dbbackups/transfer.pp
+++ modules/profile/manifests/dbbackups/transfer.pp
@@ -1,6 +1,20 @@
 # Create remote xtrabackup/mariabackup backups
 # By using transfer.py
-class profile::dbbackups::transfer {
+# This class will setup the configuration and software
+# needed to trigger new raw backups. If enabled, it
+# will set up a systemd timer to schedule it.
+# It requires the previous setup of a config at:
+# modules/profile/templates/dbbackups/<hostname>.cnf.erb
+# If the host has it enabled but the file hasn't been
+# setup, puppet run will fail. Either add its custom
+# backup configuration (see wmfbackups documentation) or
+# disable scheduled backup taking.
+# * enabled: Defaults to true. If false, it setups all
+#            dependencies, but does not retrieve a schedule
+#            configuration nor sets up a timer.
+class profile::dbbackups::transfer (
+    Boolean $enabled = lookup('profile::dbbackups::transfer::enabled', {'default_value' => true})
+) {
     require ::profile::mariadb::wmfmariadbpy
     ensure_packages([
         'wmfbackups-remote',  # will install also wmfmariadbpy-remote and transferpy
@@ -15,34 +29,47 @@ class profile::dbbackups::transfer {
         source => 'puppet:///modules/profile/dbbackups/transferpy.conf',
     }
 
-    include passwords::mysql::dump
-    $stats_user = $passwords::mysql::dump::stats_user
-    $stats_password = $passwords::mysql::dump::stats_pass
-    # Configuration file where the daily backup routine (source hosts,
-    # destination, statistics db is configured
-    # Can contain private data like db passwords
-    file { '/etc/wmfbackups/remote_backups.cnf':
-        ensure    => present,
-        owner     => 'root',
-        group     => 'root',
-        mode      => '0400',
-        show_diff => false,
-        content   => template("profile/dbbackups/${::hostname}.cnf.erb"),
-        require   => Package['wmfbackups-remote'],
-    }
+    if $enabled {
+        include passwords::mysql::dump
+        $stats_user = $passwords::mysql::dump::stats_user
+        $stats_password = $passwords::mysql::dump::stats_pass
+        # Configuration file where the daily backup routine (source hosts,
+        # destination, statistics db is configured
+        # Can contain private data like db passwords
+        file { '/etc/wmfbackups/remote_backups.cnf':
+            ensure    => present,
+            owner     => 'root',
+            group     => 'root',
+            mode      => '0400',
+            show_diff => false,
+            content   => template("profile/dbbackups/${::hostname}.cnf.erb"),
+            require   => Package['wmfbackups-remote'],
+        }
 
-    systemd::timer::job { 'database-backups-snapshots':
-        ensure      => 'present',
-        user        => 'root',
-        description => 'Generate mysql snapshot backup batch',
-        command     => '/usr/bin/remote-backup-mariadb',
-        interval    => {
-            'start'    => 'OnCalendar',
-            'interval' => 'Sun,Tue,Wed,Fri *-*-* 19:00:00',
-        },
-        require     => [
-            File['/etc/wmfbackups/remote_backups.cnf'],
-            Package['wmfbackups-remote'],
-        ]
+        systemd::timer::job { 'database-backups-snapshots':
+            ensure      => 'present',
+            user        => 'root',
+            description => 'Generate mysql snapshot backup batch',
+            command     => '/usr/bin/remote-backup-mariadb',
+            interval    => {
+                'start'    => 'OnCalendar',
+                'interval' => 'Sun,Tue,Wed,Fri *-*-* 19:00:00',
+            },
+            require     => [
+                File['/etc/wmfbackups/remote_backups.cnf'],
+                Package['wmfbackups-remote'],
+            ]
+        }
+    } else {
+        # remove existing leftovers without dependencies
+        file { '/etc/wmfbackups/remote_backups.cnf':
+            ensure    => absent,
+            show_diff => false,
+        }
+        systemd::timer::job { 'database-backups-snapshots':
+            ensure  => absent,
+            user    => 'root',
+            command => '/usr/bin/remote-backup-mariadb',
+        }
     }
 }
