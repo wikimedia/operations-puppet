1d157d4a3d67b774d01bbee15dff03e11a9b7ad6
Amir Sarabadani <Ladsgroup@gmail.com>
2021-04-22 20:52:22 +0200

    snapshot: Migrate cronjobs in shorturl to systemd timer
    This is part of the work needed to upgrade puppet to version six as
    crons are not supported in core anymore.
    
    Hosts: snapshot1008.eqiad.wmnet
    Bug: T273673
    Change-Id: I518b5d9dc4f9befe8218d3ce47e2c263189fe404

--- modules/snapshot/manifests/cron/shorturls.pp
+++ modules/snapshot/manifests/cron/shorturls.pp
@@ -8,13 +8,24 @@ class snapshot::cron::shorturls(
 
     if !$filesonly {
         cron { 'shorturls':
-            ensure      => 'present',
+            ensure      => absent,
             environment => 'MAILTO=ops-dumps@wikimedia.org',
             user        => $user,
             command     => "cd ${repodir}; python3 onallwikis.py --wiki metawiki --configfile ${confsdir}/wikidump.conf.dumps:monitor  --filenameformat 'shorturls-{d}.gz' --outdir '${cronsdir}/shorturls' --script extensions/UrlShortener/maintenance/dumpURLs.php 'compress.zlib://{DIR}'",
             minute      => '5',
             hour        => '8',
             weekday     => '1',
+        }
+        systemd::timer::job { 'shorturls':
+            ensure             => present,
+            description        => 'Regular jobs to build snapshot of short urls and their targets',
+            user               => $user,
+            monitoring_enabled => false,
+            send_mail          => true,
+            environment        => {'MAILTO' => 'ops-dumps@wikimedia.org'},
+            working_directory  => $repodir,
+            command            => "/usr/bin/python3 onallwikis.py --wiki metawiki --configfile ${confsdir}/wikidump.conf.dumps:monitor  --filenameformat 'shorturls-{d}.gz' --outdir '${cronsdir}/shorturls' --script extensions/UrlShortener/maintenance/dumpURLs.php 'compress.zlib://{DIR}'",
+            interval           => {'start' => 'OnCalendar', 'interval' => 'Mon *-*-* 8:5:0'},
         }
     }
 }
