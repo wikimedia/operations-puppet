e2054d2643a23ad99cd07cc42916634fbb76204e
Andrew Bogott <abogott@wikimedia.org>
2021-04-25 23:22:56 -0500

    radosgw: try generating a per-host keyring
    I tried storing the keydata in puppet but ceph doesn't
    seem to like sharing it across multiple nodes.  It seems
    to work fine to regenerate as needed.
    
    Change-Id: I40e6f154d06f880085380aafa55c1130ade6285e

--- modules/profile/manifests/openstack/base/radosgw.pp
+++ modules/profile/manifests/openstack/base/radosgw.pp
@@ -2,7 +2,6 @@
 #  and defined in profile::openstack::base::rbd_cloudcontrol
 class profile::openstack::base::radosgw(
     String $version = lookup('profile::openstack::base::version'),
-    String $ceph_client_keydata = lookup('profile::openstack::base::radosgw::client_keydata'),
     ) {
 
     class { '::openstack::radosgw::service':
@@ -19,15 +18,11 @@ class profile::openstack::base::radosgw(
                              ) proto tcp dport (8080) ACCEPT;",
     }
 
-    # The keydata used in this step is pre-created on one of the ceph mon hosts
-    # typically with the 'ceph auth get-or-create' command
-    file { '/etc/ceph/ceph.client.radosgw.keyring':
-        ensure    => present,
-        mode      => '0440',
-        owner     => ceph,
-        group     => ceph,
-        content   => "[client.rgw.${::hostname}]\n        key = ${ceph_client_keydata}\n",
-        show_diff => false,
-        require   => Package['ceph-common'],
+    ceph::keyring { "client.rgw.${::hostname}":
+        ensure  => 'present',
+        cap_mon => 'allow rw',
+        cap_osd => 'allow rwx',
+        mode    => '0440',
+        keyring => "/etc/ceph/ceph.client.radosgw-${::hostname}.keyring",
     }
 }
