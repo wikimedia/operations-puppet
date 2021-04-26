1cacbd42f9a340bac775d126d6b9bfe1264668cc
Janis Meybohm <jmeybohm@wikimedia.org>
2021-04-26 16:05:14 +0200

    Swap zookeeper from conf2002 to conf2005
    1) Downtime and disable puppet on conf200[1-6]
    2) Stop zookeeper on conf2002
    3) Merge this
    4) Run puppet on conf2005 + echo stats | nc localhost 2181
    5) Run puppet on conf200[3,4] + restart zookeeper
    6) Verify the status of the cluster (1 master, two followers)
       with echo stats | nc localhost 2181 on each node.
    
    Bug: T271573
    Change-Id: I1800c715a86cf3b97e3c46a7db6a90802e0a1db5

--- modules/role/manifests/configcluster_stretch.pp
+++ modules/role/manifests/configcluster_stretch.pp
@@ -6,7 +6,7 @@ class role::configcluster_stretch {
     include ::profile::base::firewall
 
     # Temporary to ease the migration process; T271573
-    if $::hostname !~ /conf200[56]/ {
+    if $::hostname !~ /conf2006/ {
         include ::profile::zookeeper::server
         include ::profile::zookeeper::firewall
     }
