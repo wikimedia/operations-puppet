159a46a31d0560f6daf4736c82a177cd3dced5e5
Janis Meybohm <jmeybohm@wikimedia.org>
2021-04-26 16:24:46 +0200

    configcluster: No longer include zookeeper in old configcluster role
    All zookeeper nodes/clusters migrated to the role configcluster_stretch
    now. So remove the zookeeper profiles from here.
    
    With merging this, also purge zookerper from 200[123].
    
    Bug: T271573
    Change-Id: I4b0404c95cc02916cdc3e78d5b77bfc28a760d83

--- modules/role/manifests/configcluster.pp
+++ modules/role/manifests/configcluster.pp
@@ -2,11 +2,6 @@ class role::configcluster {
     include ::profile::standard
     include ::profile::base::firewall
 
-    # Temporary to ease the migration process
-    if $::hostname !~ /conf100[123]/ {
-        include ::profile::zookeeper::server
-        include ::profile::zookeeper::firewall
-    }
     include ::profile::etcd
     include ::profile::etcd::tlsproxy
     include ::profile::etcd::replication
