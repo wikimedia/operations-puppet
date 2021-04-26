a5da46708c28463a39cffa448d4acf9b89174d91
Andrew Bogott <abogott@wikimedia.org>
2021-04-25 23:38:29 -0500

    radosgw profiles: remove keydata param
    Change-Id: I3f468b827bcda28b03b82244718c36a7a832310b

--- modules/profile/manifests/openstack/codfw1dev/radosgw.pp
+++ modules/profile/manifests/openstack/codfw1dev/radosgw.pp
@@ -1,10 +1,8 @@
 class profile::openstack::codfw1dev::radosgw(
     String $version = lookup('profile::openstack::codfw1dev::version'),
-    String $ceph_client_keydata = lookup('profile::openstack::codfw1dev::radosgw::client_keydata'),
     ) {
 
     class { '::profile::openstack::base::radosgw':
         version             => $version,
-        ceph_client_keydata => $ceph_client_keydata,
     }
 }
--- modules/profile/manifests/openstack/eqiad1/radosgw.pp
+++ modules/profile/manifests/openstack/eqiad1/radosgw.pp
@@ -1,10 +1,8 @@
 class profile::openstack::eqiad1::radosgw(
     String $version = lookup('profile::openstack::eqiad1::version'),
-    String $ceph_client_keydata = lookup('profile::openstack::eqiad1::radosgw::client_keydata'),
     ) {
 
     class { '::profile::openstack::base::radosgw':
         version             => $version,
-        ceph_client_keydata => $ceph_client_keydata,
     }
 }
