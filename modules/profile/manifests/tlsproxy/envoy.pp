08a1e498fc119d3907eb725382f4e9e4a4b14824
John Bond <jbond@wikimedia.org>
2021-04-28 14:18:38 +0200

    P:tlsproxy::envoy: Add support for cfssl ssl provider
    Hosts: P:tlsproxy::envoy
    Change-Id: Ie5d9429eee84c9001e76297769d99311fc2b0c5c

--- modules/profile/manifests/tlsproxy/envoy.pp
+++ modules/profile/manifests/tlsproxy/envoy.pp
@@ -52,28 +52,27 @@
 # @param capitalize_headers Whether to capitalize headers when responding to HTTP/1.1 requests
 # @param idle_timeout If indicated, that's how long an idle connection to the service is left open before closing it.
 #                     It should match the idle timeout of the upstream service.
+# @param cfssl_label if using cfssl this parameter is mandatory and should specify the CA label sign CSR's
 class profile::tlsproxy::envoy(
-    Profile::Tlsproxy::Envoy::Sni $sni_support               = lookup('profile::tlsproxy::envoy::sni_support'),
-    Stdlib::Port                  $tls_port                  = lookup('profile::tlsproxy::envoy::tls_port'),
-    Boolean                       $websockets                = lookup('profile::tlsproxy::envoy::websockets'),
-    Float                         $upstream_response_timeout = lookup('profile::tlsproxy::envoy::upstream_response_timeout'),
-    Boolean                       $retries                   = lookup('profile::tlsproxy::envoy::retries'),
-    Boolean                       $use_remote_address        = lookup('profile::tlsproxy::envoy::use_remote_address'),
-    Boolean                       $access_log                = lookup('profile::tlsproxy::envoy::access_log'),
-    Boolean                       $capitalize_headers        = lookup('profile::tlsproxy::envoy::capitalize_headers'),
-    Boolean                       $listen_ipv6               = lookup('profile::tlsproxy::envoy::listen_ipv6'),
-    Boolean                       $floc_opt_out              = lookup('profile::tlsproxy::envoy::floc_opt_out'),
-    Enum['sslcert', 'acme']       $ssl_provider              = lookup('profile::tlsproxy::envoy::ssl_provider'),
-    Array[Profile::Tlsproxy::Envoy::Service] $services = lookup('profile::tlsproxy::envoy::services'),
-    Optional[Stdlib::Host]        $upstream_addr    = lookup('profile::tlsproxy::envoy::upstream_addr'),
-    Optional[String]              $global_cert_name = lookup('profile::tlsproxy::envoy::global_cert_name',
-                                                      {'default_value' => undef}),
-    Optional[Float]               $idle_timeout = lookup('profile::tlsproxy::envoy::idle_timeout',
-                                                      {'default_value' => undef}),
-    Optional[String]              $ferm_srange  = lookup('profile::tlsproxy::envoy::ferm_srange',
-                                                      {'default_value' => undef}),
-    Optional[Integer]             $max_requests = lookup('profile::tlsproxy::envoy::max_requests',
-                                                      {'default_value' => undef}),
+    Profile::Tlsproxy::Envoy::Sni    $sni_support               = lookup('profile::tlsproxy::envoy::sni_support'),
+    Stdlib::Port                     $tls_port                  = lookup('profile::tlsproxy::envoy::tls_port'),
+    Boolean                          $websockets                = lookup('profile::tlsproxy::envoy::websockets'),
+    Float                            $upstream_response_timeout = lookup('profile::tlsproxy::envoy::upstream_response_timeout'),
+    Boolean                          $retries                   = lookup('profile::tlsproxy::envoy::retries'),
+    Boolean                          $use_remote_address        = lookup('profile::tlsproxy::envoy::use_remote_address'),
+    Boolean                          $access_log                = lookup('profile::tlsproxy::envoy::access_log'),
+    Boolean                          $capitalize_headers        = lookup('profile::tlsproxy::envoy::capitalize_headers'),
+    Boolean                          $listen_ipv6               = lookup('profile::tlsproxy::envoy::listen_ipv6'),
+    Boolean                          $floc_opt_out              = lookup('profile::tlsproxy::envoy::floc_opt_out'),
+    Enum['sslcert', 'acme', 'cfssl'] $ssl_provider              = lookup('profile::tlsproxy::envoy::ssl_provider'),
+    Hash                             $cfssl_options             = lookup('profile::tlsproxy::envoy::cfssl_options'),
+    Array[Profile::Tlsproxy::Envoy::Service] $services          = lookup('profile::tlsproxy::envoy::services'),
+    Optional[Stdlib::Host]           $upstream_addr             = lookup('profile::tlsproxy::envoy::upstream_addr'),
+    Optional[String]                 $global_cert_name          = lookup('profile::tlsproxy::envoy::global_cert_name'),
+    Optional[Float]                  $idle_timeout              = lookup('profile::tlsproxy::envoy::idle_timeout'),
+    Optional[String]                 $ferm_srange               = lookup('profile::tlsproxy::envoy::ferm_srange'),
+    Optional[Integer]                $max_requests              = lookup('profile::tlsproxy::envoy::max_requests'),
+    Optional[String]                 $cfssl_label               = lookup('profile::tlsproxy::envoy::cfssl_label'),
 ) {
     require profile::envoy
     $ensure = $profile::envoy::ensure
@@ -84,26 +83,51 @@ class profile::tlsproxy::envoy(
     unless $upstream_addr in $valid_upstream_addr {
         fail("upstream_addr must be one of: ${valid_upstream_addr.join(', ')}")
     }
+    if $ssl_provider == 'cfssl' and !$cfssl_label {
+        fail('must specify \$cfssl_label when using ssl_provider: cfssl')
+    }
 
+    # By default use the server profile
+    $base_cfssl_options = {'profile' => 'server'}
     $upstreams = $services.map |$service| {
         if $service['cert_name'] and $sni_support != 'no' {
             # ensure all the needed certs are present. Given these are internal services,
             # we want certs declared with the traditional sslcert for now.
-            if $ssl_provider == 'sslcert' {
-                sslcert::certificate { $service['cert_name']:
-                    ensure => $ensure,
-                    group  => 'envoy',
-                    notify => Service['envoyproxy.service'],
+            case $ssl_provider {
+                'sslcert': {
+                    sslcert::certificate { $service['cert_name']:
+                        ensure => $ensure,
+                        group  => 'envoy',
+                        notify => Service['envoyproxy.service'],
+                    }
+                    $cert = "/etc/ssl/localcerts/${service['cert_name']}.crt"
+                    $key = "/etc/ssl/private/${service['cert_name']}.key"
                 }
-                $cert = "/etc/ssl/localcerts/${service['cert_name']}.crt"
-                $key = "/etc/ssl/private/${service['cert_name']}.key"
-            } elsif $ssl_provider == 'acme' {
-                acme_chief::cert { $service['cert_name']:
-                    puppet_svc => 'envoyproxy.service',
-                    key_group  => 'envoy',
+                'acme': {
+                    acme_chief::cert { $service['cert_name']:
+                        puppet_svc => 'envoyproxy.service',
+                        key_group  => 'envoy',
+                    }
+                    $cert = "/etc/acmecerts/${service['cert_name']}/live/ec-prime256v1.chained.crt"
+                    $key = "/etc/acmecerts/${service['cert_name']}/live/ec-prime256v1.key"
                 }
-                $cert = "/etc/acmecerts/${service['cert_name']}/live/ec-prime256v1.chained.crt"
-                $key = "/etc/acmecerts/${service['cert_name']}/live/ec-prime256v1.key"
+                'cfssl': {
+                    $_cfssl_options = $service['cfssl_options'] ? {
+                        undef   => $base_cfssl_options,
+                        default => $base_cfssl_options + $service['cfssl_options'],
+                    }
+                    $_cfssl_label = $service['cfssl_label'] ? {
+                        undef   => $cfssl_label,
+                        default => $service['cert_options'],
+                    }
+                    $ssl_paths = profile::pki::get_cert($_cfssl_label, $service['cert_name'], $_cfssl_options)
+                    $cert = $ssl_paths['cert']
+                    $key = $ssl_paths['key']
+                }
+                default: {
+                    # shouldn't ever reach here
+                    fail("ssl_provider (${ssl_provider}) unsupported")
+                }
             }
         } else {
             $cert = undef
@@ -128,21 +152,35 @@ class profile::tlsproxy::envoy(
             fail(['If you want non-sni TLS to be supported, you need to define ',
                   'profile::tlsproxy::envoy::global_cert_name or '].join(' '))
         }
-        if $ssl_provider == 'sslcert' {
-            sslcert::certificate { $global_cert_name:
-                ensure => $ensure,
-                group  => 'envoy',
-                notify => Service['envoyproxy.service'],
+        case $ssl_provider {
+            'sslcert': {
+                sslcert::certificate { $global_cert_name:
+                    ensure => $ensure,
+                    group  => 'envoy',
+                    notify => Service['envoyproxy.service'],
+                }
+                $global_cert_path = "/etc/ssl/localcerts/${global_cert_name}.crt"
+                $global_key_path = "/etc/ssl/private/${global_cert_name}.key"
             }
-            $global_cert_path = "/etc/ssl/localcerts/${global_cert_name}.crt"
-            $global_key_path = "/etc/ssl/private/${global_cert_name}.key"
-        } elsif $ssl_provider == 'acme' {
-            acme_chief::cert {$global_cert_name:
-                puppet_svc => 'envoyproxy.service',
-                key_group  => 'envoy',
+            'acme': {
+                acme_chief::cert {$global_cert_name:
+                    puppet_svc => 'envoyproxy.service',
+                    key_group  => 'envoy',
+                }
+                $global_cert_path = "/etc/acmecerts/${global_cert_name}/live/ec-prime256v1.chained.crt"
+                $global_key_path = "/etc/acmecerts/${global_cert_name}/live/ec-prime256v1.key"
             }
-            $global_cert_path = "/etc/acmecerts/${global_cert_name}/live/ec-prime256v1.chained.crt"
-            $global_key_path = "/etc/acmecerts/${global_cert_name}/live/ec-prime256v1.key"
+            'cfssl': {
+                $ssl_paths = profile::pki::get_cert(
+                    $cfssl_label, $global_cert_name, $base_cfssl_options + $cfssl_options
+                )
+                $global_cert_path = $ssl_paths['cert']
+                $global_key_path = $ssl_paths['key']
+            }
+            default: {
+                # shouldn't ever reach here
+                fail("ssl_provider (${ssl_provider}) unsupported")
+            }
         }
     }
 
