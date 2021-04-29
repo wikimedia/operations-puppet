4f3c0834ca0166bf8f4f1374cd64ca02b04dedbb
Kunal Mehta <legoktm@debian.org>
2021-04-28 22:08:51 -0700

    mailman3: Properly import templates for admins
    The import process only imports the templates into mailman3, so they
    exist on disk and in config, but are entirely invisible to listadmins.
    
    We really need to import them into postorius, but upstream doesn't have
    that functionality yet. So we do it!
    
    * Identify templates on disk
    * Directly insert them into mailman3web.postorius_emailtemplate
    * Use REST API to point mailman3 at the postorius templates
    * Delete templates off disk
    
    For already imported lists, we can do something like:
    
    >>> from migrate_to_mailman3 import fix_templates
    >>> fix_templates(listname)
    
    Bug: T281425
    Change-Id: I9d94caed930dcae5f9f40fc9baf30757254b70b8

--- modules/mailman3/files/scripts/migrate_to_mailman3.py
+++ modules/mailman3/files/scripts/migrate_to_mailman3.py
@@ -17,14 +17,25 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import argparse
+import datetime
+import importlib.machinery
+import shutil
 import socket
 import subprocess
 import sys
 import tempfile
 import textwrap
 import time
+import types
 
 from pathlib import Path
+
+import pymysql
+import wmflib.config
+
+from mailmanclient import Client
+
+DOMAIN = "lists.wikimedia.org"
 
 
 def parse_args():
@@ -45,6 +56,102 @@ def send_email(to: str, subject: str, body: str):
     print(f"Sent email to {to}")
 
 
+def get_client() -> Client:
+    cfg = wmflib.config.load_ini_config("/etc/mailman3/mailman.cfg")
+    return Client(
+        "http://localhost:8001/3.1",
+        cfg["webservice"]["admin_user"],
+        cfg["webservice"]["admin_pass"],
+    )
+
+
+def get_webdb() -> pymysql.Connection:
+    loader = importlib.machinery.SourceFileLoader(
+        "mailman_web", "/etc/mailman3/mailman-web.py"
+    )
+    mod = types.ModuleType(loader.name)
+    loader.exec_module(mod)
+    cfg = mod.DATABASES["default"]
+    return pymysql.connect(
+        host=cfg["HOST"],
+        user=cfg["USER"],
+        password=cfg["PASSWORD"],
+        database=cfg["NAME"],
+        charset=cfg["OPTIONS"]["charset"],
+    )
+
+
+def fix_templates(listname: str):
+    """
+    Scan for templates on disk, import them into postorius,
+    update mailman3 to get them from postorius, then
+    delete them off disk.
+    """
+    templ_dir = Path(f"/var/lib/mailman3/templates/lists/{listname}.{DOMAIN}")
+    if not templ_dir.is_dir():
+        return
+    lang_dirs = list(templ_dir.iterdir())
+    if not lang_dirs:
+        return
+    lang_dir = lang_dirs[0]
+    # Postorius only allows setting templates for one language
+    if len(lang_dirs) > 1:
+        skipped = ", ".join(lang.name for lang in lang_dirs[1:])
+        print(
+            f"WARNING: Only templates for {lang_dir.name} will be imported, skipping: {skipped}."
+        )
+        print("WARNING: Please delete the skipped templates after import")
+    rows = []
+    for template in lang_dir.glob("*.txt"):
+        print(f"Importing {template.name}")
+        rows.append(
+            (
+                # name
+                template.name[:-4],  # Strip .txt suffix
+                # data
+                template.read_text(),
+                # language
+                "",
+                # created_at
+                datetime.datetime.utcnow(),
+                # modified_at
+                datetime.datetime.utcnow(),
+                # context
+                "list",
+                # identifier
+                f"{listname}.{DOMAIN}",
+            )
+        )
+    if not rows:
+        # No templates?
+        return
+    connection = get_webdb()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO postorius_emailtemplate (name, data, language, created_at, modified_at, context, identifier) VALUES (%s, %s, %s, %s, %s, %s, %s)"  # noqa
+            cursor.executemany(sql, rows)
+        connection.commit()
+        print(f"Inserted {len(rows)} templates into `postorius_emailtemplate`.")
+    # Use REST API to update mailman3 to point to postorius
+    client = get_client()
+    mlist = client.get_list(f"{listname}@{DOMAIN}")
+    for row in rows:
+        mlist.set_template(
+            template_name=row[0],
+            uri=f"https://{DOMAIN}/postorius/api/templates/list/{listname}.{DOMAIN}/{row[0]}",
+            # For some reason mailman3 itself has username as NULL and password as empty string
+            username=None,
+            password="",
+        )
+    print("Updated mailman3.template table to point to postorius")
+    shutil.rmtree(lang_dir)
+    print(f"Deleted {lang_dir}")
+    if not list(templ_dir.iterdir()):
+        # Directory is empty, get rid of it too
+        shutil.rmtree(templ_dir)
+        print(f"Deleted {templ_dir}")
+
+
 def check_call(args):
     """Wrapper to print the command being run"""
     print("$ " + " ".join(args))
@@ -54,8 +161,8 @@ def check_call(args):
 def main() -> int:
     args = parse_args()
     listname = args.listname
-    listaddr = f"{listname}@lists.wikimedia.org"
-    if Path(f"/var/lib/mailman3/lists/{listname}.lists.wikimedia.org").exists():
+    listaddr = f"{listname}@{DOMAIN}"
+    if Path(f"/var/lib/mailman3/lists/{listname}.{DOMAIN}").exists():
         print("Already done")
         return 0
     # FIXME don't do this
@@ -64,7 +171,7 @@ def main() -> int:
         return 0
     print(f"Migrating {listname} to Mailman3")
     send_email(
-        to=f"{listname}-owner@lists.wikimedia.org",
+        to=f"{listname}-owner@{DOMAIN}",
         subject=f"{listname} mailing list is being migrated to Mailman3",
         body=textwrap.dedent(
             f"""\
@@ -96,18 +203,19 @@ def main() -> int:
         ]
     )
     check_call(["mailman-web", "update_index_one_list", listaddr])
+    fix_templates(listname)
     check_call(["/usr/local/sbin/disable_list", listname])
     send_email(
-        to=f"{listname}-owner@lists.wikimedia.org",
+        to=f"{listname}-owner@{DOMAIN}",
         subject=f"{listname} mailing list migration complete",
         body=textwrap.dedent(
             f"""\
         Dear list administrator,
 
         Your mailing list, {listname}, has been migrated to Mailman3. You can
-        access it at <https://lists.wikimedia.org/postorius/lists/{listname}.lists.wikimedia.org/>.
+        access it at <https://{DOMAIN}/postorius/lists/{listname}.lists.wikimedia.org/>.
 
-        Please create an account at <https://lists.wikimedia.org/accounts/signup/>.
+        Please create an account at <https://{DOMAIN}/accounts/signup/>.
 
         Finally, review <https://meta.wikimedia.org/wiki/Mailing_lists/Mailman3_migration>, which
         has some items to check.
