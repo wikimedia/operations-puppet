feaf22b32db8e2c00b644b6e5a9d696f28f2f243
Ottomata <aotto@wikimedia.org>
2021-05-03 14:16:50 -0400

    refine - Remove webproxy for eventlogging_analytics job
    This can be done now now that Refine uses Wikimedia-event-utilities.
    
    Also bump refinery version to 0.1.9 to get an alert formatting fix
    
    Bug: T247510
    Change-Id: I1373ba58b884a6de341e32e2deb20b62578970c3

--- modules/profile/manifests/analytics/refinery/job/refine.pp
+++ modules/profile/manifests/analytics/refinery/job/refine.pp
@@ -40,7 +40,7 @@ class profile::analytics::refinery::job::refine(
 
     # Update this when you want to change the version of the refinery job jar
     # being used for the refine job.
-    $refinery_version = '0.1.5'
+    $refinery_version = '0.1.9'
 
     # Use this value by default
     Profile::Analytics::Refinery::Job::Refine_job {
@@ -243,8 +243,6 @@ class profile::analytics::refinery::job::refine(
             # Get EventLogging JSONSchemas from meta.wikimedia.org.
             schema_base_uris                => 'eventlogging',
         }),
-        # Use webproxy so that this job can access meta.wikimedia.org to retrive JSONSchemas.
-        spark_extra_opts => '--driver-java-options=\'-Dhttp.proxyHost=webproxy.eqiad.wmnet -Dhttp.proxyPort=8080 -Dhttps.proxyHost=webproxy.eqiad.wmnet -Dhttps.proxyPort=8080\'',
         interval         => '*-*-* *:30:00',
         monitor_interval => '*-*-* 00:15:00',
         use_keytab       => $use_kerberos_keytab,
