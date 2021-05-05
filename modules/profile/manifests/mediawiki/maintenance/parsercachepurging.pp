72962313028ccf057f157542cf5df1bbc502a379
Manuel Arostegui <marostegui@wikimedia.org>
2021-05-05 06:56:35 +0200

    parsercachepurging.pp: Reduce parsercache retention to 21 days
    Due to disk space issues, we want to decrease retention from 30 days to
    21 days.
    
    Bug: T280605
    Change-Id: Ia2c78b60f39611db1ac2afbfa8d38e737b05f85d

--- modules/profile/manifests/mediawiki/maintenance/parsercachepurging.pp
+++ modules/profile/manifests/mediawiki/maintenance/parsercachepurging.pp
@@ -3,8 +3,8 @@ class profile::mediawiki::maintenance::parsercachepurging {
     system::role { 'mediawiki::maintenance::parsercachepurging': description => 'MediaWiki Maintenance Server: parser cache purging' }
 
     profile::mediawiki::periodic_job { 'parser_cache_purging':
-        # Every day, Purge entries older than 30d * 86400s/d = 2592000s
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --age=2592000 --msleep 500',
+        # Every day, Purge entries older than 21d * 86400s/d = 1814400s
+        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --age=1814400 --msleep 500',
         interval => '01:00',
     }
 }
