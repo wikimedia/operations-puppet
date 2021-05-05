352ffdacce9dcd4bfaf8b39bde3f076f2f5c05bd
Daniel Zahn <dzahn@wikimedia.org>
2021-05-05 10:42:32 -0700

    httpbb: fix path to tests for people.wm.org
    Change-Id: Icdf9f311e1e64018f967e5e3397b9c77f71b1091

--- modules/profile/manifests/httpbb.pp
+++ modules/profile/manifests/httpbb.pp
@@ -45,7 +45,7 @@ class profile::httpbb {
         source => 'puppet:///modules/profile/httpbb/miscweb/test_miscweb.yaml'
     }
     httpbb::test_suite {'miscweb/test_people.yaml':
-        source => 'puppet:///modules/profile/httpbb/miscweb/test_people.yaml'
+        source => 'puppet:///modules/profile/httpbb/people/test_people.yaml'
     }
     httpbb::test_suite {'releases/test_releases.yaml':
         source => 'puppet:///modules/profile/httpbb/releases/test_releases.yaml'
