a357b5c75767b2c666c8a1bf9b448a7eaa3b66a0
Valentin Gutierrez <vgutierrez@wikimedia.org>
2021-05-05 17:25:02 +0200

    trafficserver: Fix cacert_(dirpath|filename) usage
    It looks like we inherited a bug from the ATS documentation
    where it's said that proxy.config.ssl.client.CA.cert.path
    is used as the base path for proxy.config.ssl.client.CA.cert.filename
    when actually both are feeded to SSL_CTX_load_verify_locations()
    as CAfile and CApath where CAfile is the absolute path to a single PEM file
    and CApath is the absolute path to a directory containing CA
    certificates in PEM format. More details available on
    https://www.openssl.org/docs/man1.1.0/man3/SSL_CTX_load_verify_locations.html
    
    Bug: T281673
    Change-Id: I12044d6efeeb51ea24d023a140cbaf84a0ab2ee0

--- modules/trafficserver/types/outbound_tls_settings.pp
+++ modules/trafficserver/types/outbound_tls_settings.pp
@@ -4,16 +4,18 @@
 #   If true, validate origin server certificate.
 #
 # [*cacert_dirpath*]
-#   Absolute path to the directory containing the file specified in
-#   cacert_filename.
+#   If specified, absolute path to the directory containing the certificates
+#   that will be used for origin server certificate validation.
 #
 # [*cacert_filename*]
-#   If specified, the filename of the CA to trust for origin server certificate
-#   validation.
+#   If specified, absolute path of the filename of the CA to trust for
+#   origin server certificate validation.
 
 type Trafficserver::Outbound_TLS_settings = Struct[{
     'common'          => Trafficserver::TLS_settings,
     'verify_origin'   => Boolean,
-    'cacert_dirpath'  => Stdlib::Absolutepath,
+    'cacert_dirpath'  => Optional[Stdlib::Absolutepath],
+    # TODO: Replace String with Stdlib::Absolutepath after updating
+    # the config cluster wide.
     'cacert_filename' => Optional[String],
 }]
