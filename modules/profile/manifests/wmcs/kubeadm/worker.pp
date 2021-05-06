0f903de34a3eac0a7fd946b35fc07f02c4e1b7dd
Brooke Storm <bstorm@wikimedia.org>
2021-05-06 14:36:33 -0700

    toolforge kubernetes: change class for the new cinder environment
    This will work with the g3.cores8.ram16.disk20.ephem140 (not actually
    looking to eat up cinder with k8s nodes). That flavor mirrors the existing
    xlarge nodes we were making before cinder's introduction.
    
    Additionally, make the separate docker vol optional becuase ingress nodes
    honestly should be able to opt out. They run a single container that is
    pretty big, but it also under 2GB.
    
    I believe this supports existing nodes.
    
    Bug: T282087
    Change-Id: I339d3b5f02b7167fc51760d52e03efa1ebbda061

--- modules/profile/manifests/wmcs/kubeadm/worker.pp
+++ modules/profile/manifests/wmcs/kubeadm/worker.pp
@@ -1,12 +1,18 @@
 class profile::wmcs::kubeadm::worker (
     String $component = lookup('profile::wmcs::kubeadm::component', {default_value => 'thirdparty/kubeadm-k8s-1-17'}),
+    Boolean $docker_vol = lookup('profile::wmcs::kubeadm::docker_vol', {default_value => true}),
 ) {
     require profile::wmcs::kubeadm::preflight_checks
 
-    labs_lvm::volume { 'docker':
-        mountat   => '/var/lib/docker',
-        mountmode => '711',
-        before    => Service['docker'],
+    if $docker_vol {
+        cinderutils::ensure { 'separate-docker':
+            min_gb        => 40,
+            max_gb        => 160,
+            mount_point   => '/var/lib/docker',
+            mount_mode    => '711',
+            mount_options => 'discard,defaults',
+            before        => Service['docker'],
+        }
     }
 
     class { '::kubeadm::repo':
