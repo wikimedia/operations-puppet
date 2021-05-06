ff3d5a54277dae16ccef90f48e8c8df5087f17fd
Taavi Väänänen <hi@taavi.wtf>
2021-05-06 10:24:28 +0300

    toolforge: Add ingress-nginx Helm files
    Install Helm on the control nodes.
    
    Add a values file to be generated by Puppet to deploy the nginx ingress
    via Helm.
    
    Bug: T264221
    Change-Id: I45ffadaf6538ce712e1bfd024e7186366a94fd0f

--- modules/toolforge/manifests/k8s/nginx_ingress_yaml.pp
+++ modules/toolforge/manifests/k8s/nginx_ingress_yaml.pp
@@ -3,12 +3,21 @@ class toolforge::k8s::nginx_ingress_yaml (
     Stdlib::Port $jobs_port        = 30001,
     Stdlib::Fqdn $jobs_fqdn        = 'jobs.toolforge.org',
 ) {
+    # Helm 3 from component/kubeadm-*
+    require_package('helm')
+
     # make sure you declare ::kubeadm::core somewhere in the calling profile
     # because /etc/kubernetes
 
     file { '/etc/kubernetes/psp/nginx-ingress-psp.yaml':
         ensure  => present,
         source  => 'puppet:///modules/toolforge/k8s/nginx-ingress-psp.yaml',
+        require => File['/etc/kubernetes'],
+    }
+
+    file { '/etc/kubernetes/nginx-ingress-helm-values.yaml':
+        ensure  => present,
+        content => template('toolforge/k8s/nginx-ingress-helm-values.yaml.erb'),
         require => File['/etc/kubernetes'],
     }
 
