c8fe981161166671d2ea3a7d568b83100020d6cd
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2021-05-06 14:49:05 +0200

    icinga: switch to LibreNMS AlertManager paging
    Bug: T281095
    Change-Id: I173b97bdb40095bf452e3de9df6dcccf9f78d681

--- modules/icinga/manifests/monitor/librenms.pp
+++ modules/icinga/manifests/monitor/librenms.pp
@@ -3,6 +3,7 @@
 # Included on the Icinga hosts to poll LibreNMS API and generate Icinga criticals.
 class icinga::monitor::librenms {
     monitoring::service { 'librenms_alerts':
+        ensure        => absent,
         check_command => 'check_librenms',
         description   => 'LibreNMS has a critical alert',
         notes_url     => 'https://bit.ly/wmf-librenms',
