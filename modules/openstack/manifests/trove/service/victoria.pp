f2fd774ba63c90170bf068fb83a7adf76d298768
Andrew Bogott <abogott@wikimedia.org>
2021-05-11 13:57:09 -0500

    Trove api-paste.ini: remove trailing space
    Change-Id: I23668daa4b1be8e903164a8e93a37fece2ee823b

--- modules/openstack/manifests/trove/service/victoria.pp
+++ modules/openstack/manifests/trove/service/victoria.pp
@@ -48,7 +48,7 @@ class openstack::trove::service::victoria(
             notify  => Service['trove-api', 'trove-taskmanager', 'trove-conductor'],
             require => Package['trove-api'];
         '/etc/trove/api-paste.ini':
-            source  => 'puppet:///modules/openstack/victoria/trove/api-paste.ini ',
+            source  => 'puppet:///modules/openstack/victoria/trove/api-paste.ini',
             owner   => 'trove',
             group   => 'trove',
             mode    => '0644',
