533c72ea867f75add18fd7fad45c62e66d61e441
Marcel Ruiz Forns <mforns@wikimedia.org>
2021-05-12 16:53:08 +0200

    analytics:refinery:job:data_purge: improve drop-el-unsanitized-events
    This change does 3 things:
    1. Removes the --skip-trash flag, so that from now on all deleted
    event unsanitized data goes to the Trash folder temporarily instead
    of being deleted right away.
    2. Changes the start of the path_format regex so that from now on:
        a. WMDEBanner* tables are also purged
        b. Tables with underscores in their names are also purged
        c. Directories with datacenter partitions are also purged
    3. Modifies the argument checksum accordingly.
    
    Bug: T273789
    Change-Id: I1b6fcda95cd823fb8fde6a61b8ad380d0d631be5

--- modules/profile/manifests/analytics/refinery/job/data_purge.pp
+++ modules/profile/manifests/analytics/refinery/job/data_purge.pp
@@ -263,7 +263,7 @@ class profile::analytics::refinery::job::data_purge (
     kerberos::systemd_timer { 'drop-el-unsanitized-events':
         ensure      => $ensure_timers,
         description => 'Drop unsanitized EventLogging data from the event database after retention period.',
-        command     => "${refinery_path}/bin/refinery-drop-older-than --database='event' --tables='^(?!wmdebanner)[A-Za-z0-9]+$$' --base-path='/wmf/data/event' --path-format='(?!WMDEBanner)[A-Za-z0-9]+/year=(?P<year>[0-9]+)(/month=(?P<month>[0-9]+)(/day=(?P<day>[0-9]+)(/hour=(?P<hour>[0-9]+))?)?)?' --older-than='90' --skip-trash --execute='dc3b5e020579ae5516b7f372081d1fac' --log-file='${el_unsanitized_log_file}'",
+        command     => "${refinery_path}/bin/refinery-drop-older-than --database='event' --tables='.*' --base-path='/wmf/data/event' --path-format='[^/]+(/datacenter=[^/]+)?/year=(?P<year>[0-9]+)(/month=(?P<month>[0-9]+)(/day=(?P<day>[0-9]+)(/hour=(?P<hour>[0-9]+))?)?)?' --older-than='90' --execute='ea43326f56fd374d895bb931dc0ce3d4' --log-file='${el_unsanitized_log_file}'",
         interval    => '*-*-* 00:00:00',
         environment => $systemd_env,
         user        => 'analytics',
