4d544cc3db9e981a24f9da01a45adc347f467f78
Herron <kherron@wikimedia.org>
2021-05-13 14:32:20 +0000

    Revert "logstash: remove kibana and elasticsearch from role::logstash"
    This reverts commit 1b8937c6014b9eca5ac1dfc91208aa403f1d0fd4.
    
    Reason for revert: caused lvs alert for kibana, going to split logstash and kibana removals
    into separate patches
    
    Change-Id: Ie91db270484908a53c9a2a8d98da2534a0c32f71

--- modules/role/manifests/logstash.pp
+++ modules/role/manifests/logstash.pp
@@ -6,6 +6,10 @@ class role::logstash {
     include profile::base::firewall
     include profile::logstash::collector
     include profile::logstash::apifeatureusage
+    include profile::elasticsearch::logstash
+    include profile::elasticsearch::monitor::base_checks
+    include profile::kibana
+    include profile::kibana::httpd_proxy
     include profile::prometheus::logstash_exporter
     include profile::tlsproxy::envoy # TLS termination
 }
