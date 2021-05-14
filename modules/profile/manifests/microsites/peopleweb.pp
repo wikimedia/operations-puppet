263441122482f0f1f4c51b72f52e128d56758c23
Daniel Zahn <dzahn@wikimedia.org>
2021-05-13 17:08:54 -0700

    microsites::peopleweb: add more comments
    Change-Id: I26d1ce87ff18574f96c4da3a1816e4648741ac75

--- modules/profile/manifests/microsites/peopleweb.pp
+++ modules/profile/manifests/microsites/peopleweb.pp
@@ -1,4 +1,6 @@
-# let users publish their own HTML in their home dirs
+# https://people.wikimedia.org
+# lets shells users publish their own files (apache2, mod_userdir)
+# e.g. https://people.wikimedia.org/~username/
 class profile::microsites::peopleweb (
     Stdlib::Host     $deployment_server = lookup('deployment_server'),
     Stdlib::Host     $sitename          = lookup('profile::microsites::peopleweb::sitename'),
@@ -7,18 +9,21 @@ class profile::microsites::peopleweb (
     Stdlib::Host     $rsync_dst_host    = lookup('profile::microsites::peopleweb::rsync_dst_host'),
 ){
 
+    # firewall: allow caching layer to talk to http backend
     ferm::service { 'people-http':
         proto  => 'tcp',
         port   => '80',
         srange => '$CACHES',
     }
 
+    # firewall: allow http from deployment servers for testing
     ferm::service { 'people-http-deployment':
         proto  => 'tcp',
         port   => '80',
         srange => "(@resolve((${deployment_server})) @resolve((${deployment_server}), AAAA))"
     }
 
+    # httpd (apache2)
     class { '::httpd':
         modules => ['userdir', 'rewrite', 'headers'],
     }
@@ -29,22 +34,33 @@ class profile::microsites::peopleweb (
 
     base::service_auto_restart { 'apache2': }
 
+    rsyslog::input::file { 'apache2-error':
+        path => '/var/log/apache2/*error*.log',
+    }
+
     wmflib::dir::mkdir_p($docroot)
 
+    # the index page shown at https://people.wikimedia.org/
     file { "${docroot}/index.html":
         content => template('profile/microsites/peopleweb/index.html.erb'),
         mode    => '0444',
         owner   => 'root',
         group   => 'root',
     }
+
+    # Wikimedia single sign-on portal (idp.wikimedia.org)
+    # allows users to password protect files
     include profile::idp::client::httpd
 
+    # Icinga alerting, crit but not paging
+    # TODO: only do this ONCE and not for each node using this
     monitoring::service { 'https-peopleweb':
         description   => 'HTTPS-peopleweb',
         check_command => "check_https_url!${sitename}!https://${sitename}",
         notes_url     => 'https://wikitech.wikimedia.org/wiki/People.wikimedia.org',
     }
 
+    # warn users on servers that are NOT the active backend and source of rsync
     if $::fqdn == $rsync_src_host {
         $motd_content = "#!/bin/sh\necho '\nThis is people.wikimedia.org.\nFiles you put in 'public_html' in your home dir will be accessible on the web.\nMore info on https://wikitech.wikimedia.org/wiki/People.wikimedia.org.\n'"
     } else {
@@ -56,11 +72,9 @@ class profile::microsites::peopleweb (
         content => $motd_content,
     }
 
+    # people's entire home dirs (not just public_html) are backed up in Bacula
     backup::set {'home': }
 
-    rsyslog::input::file { 'apache2-error':
-        path => '/var/log/apache2/*error*.log',
-    }
 
     # allow copying /home from one server to another for migrations
     ensure_packages(['rsync'])
