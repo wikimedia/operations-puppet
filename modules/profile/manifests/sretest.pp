dc26bca022ae72d82ee041c80b977a58940cfaaf
John Bond <jbond@wikimedia.org>
2021-05-17 12:28:30 +0200

    sretest: test puppetdb function
    This CR should be reverted after the test
    
    Hosts: sretest1001.eqiad.wmnet
    Change-Id: I09ccf8c160650ed71e28d4b835f38b0e3d1c1789

--- modules/profile/manifests/sretest.pp
+++ modules/profile/manifests/sretest.pp
@@ -7,5 +7,32 @@ class profile::sretest {
     profile::contact { $title:
         contacts => ['jbond', 'jmm']
     }
-
+    $cache_nodes = lookup('cache::nodes')  # lint:ignore:wmf_styleguide
+    file {'/var/tmp/testing':
+        ensure => directory,
+    }
+    file {'/var/tmp/testing/cache.nodes.eqsin.pdb':
+        ensure  => file,
+        content => wmflib::role_hosts('cache::upload', 'eqsin').to_yaml,
+    }
+    file {'/var/tmp/testing/cache.nodes.eqsin.hiera':
+        ensure  => file,
+        content => $cache_nodes['upload']['eqsin'].to_yaml
+    }
+    file {'/var/tmp/testing/cache.nodes.eq.pdb':
+        ensure  => file,
+        content => wmflib::role_hosts('cache::upload', ['eqsin', 'eqiad']).to_yaml,
+    }
+    file {'/var/tmp/testing/cache.nodes.eq.hiera':
+        ensure  => file,
+        content => ($cache_nodes['upload']['eqsin'] + $cache_nodes['upload']['eqiad']).to_yaml
+    }
+    file {'/var/tmp/testing/cache.nodes.pdb':
+        ensure  => file,
+        content => wmflib::role_hosts('cache::upload').to_json,
+    }
+    file {'/var/tmp/testing/cache.nodes.hiera':
+        ensure  => file,
+        content => $cache_nodes['upload'].to_yaml
+    }
 }
