24ef25932f7f066a41010e98fbcaf4d2e3c662c6
John Bond <jbond@wikimedia.org>
2021-05-20 14:11:35 +0200

    P:logoutd: create a logout.d profile for managing logout scripts
    Change-Id: I786c2ff7b6bd9499decd7da1c47e9dc3218d9dce

---
+++ modules/profile/manifests/logoutd.pp
@@ -0,0 +1,24 @@
+# @summary this class provides scaffolding for logoutd
+#   A cli based API for deleting user sessions
+# @param owner the owner of the scripts
+# @param group the group of the scripts
+# @param scripts A hash of profile::logoutd::scripts to install
+class profile::logoutd (
+    String $owner   = lookup('profile::logoutd::owner'),
+    String $group   = lookup('profile::logoutd::group'),
+    Hash   $scripts = lookup('profile::logoutd::scripts'),
+) {
+    $base_dir = '/etc/wikimedia/logout.d'
+    wmflib::dir::mkdir_p($base_dir, {
+        ensure => directory,
+        owner  => $owner,
+        group  => $group,
+        mode   => '0550',
+        source => 'puppet:///modules/profile/logout.d',
+    })
+    $scripts.each |$res_title, $params| {
+        profile::logoutd::script { $res_title:
+            * => $params,
+        }
+    }
+}
---
+++ modules/profile/manifests/logoutd/script.pp
@@ -0,0 +1,27 @@
+# @summary resource used to install logout.d scripts into the logout.d folder.
+#   All scripts must be CLI programs which implement a specific API.  See T283242
+#   for more details
+# @param source a puppet Source to the script file
+# @param content String content of the script
+define profile::logoutd::script (
+    Integer[1,99]                $priority = 50,
+    Optional[Stdlib::Filesource] $source   = undef,
+    Optional[String]             $content  = undef,
+) {
+    include profile::logoutd
+    unless $source or $content {
+        fail('must provide either $source or $content')
+    }
+    if $source and $content {
+        fail('must provide only one of $source or $content')
+    }
+    $file_name = sprintf('%s/%02d-%s', $profile::logoutd::base_dir, $priority, $title)
+    file { $file_name:
+        ensure  => file,
+        owner   => $profile::logoutd::owner,
+        group   => $profile::logoutd::group,
+        mode    => '0550',
+        source  => $source,
+        content => $content,
+    }
+}
