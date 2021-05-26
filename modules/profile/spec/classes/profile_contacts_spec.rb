43e3b12bf584d6387f8a70da38becc444af2fe1b
John Bond <jbond@wikimedia.org>
2021-05-26 12:51:10 +0200

    profile::contacts: add a profile and define for adding contact metadata
    Bug: T216088
    Change-Id: Ie7b399f608db08c5a726a6d36c2c0ac6a395cb59

---
+++ modules/profile/spec/classes/profile_contacts_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../../../../rake_modules/spec_helper'
+
+describe 'profile::contacts' do
+  on_supported_os(WMFConfig.test_on).each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts  }
+      let(:params) { { } }
+      let(:node_params) {{ '_role' => 'sretest' }}
+
+      context "Defaults" do
+        it { is_expected.to compile.with_all_deps }
+      end
+      context "add contacts" do
+        let(:params) { super().merge(role_contacts: ['foobar']) }
+        it { is_expected.to compile.with_all_deps }
+      end
+    end
+  end
+end
