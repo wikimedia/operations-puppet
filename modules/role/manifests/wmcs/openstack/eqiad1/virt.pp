915272e7f8be6c37ab1b887c0b55c5c7e708ba55
David Caro <dcaro@wikimedia.org>
2021-05-27 17:42:42 +0200

    role.wmcs.virt*: sorted and add extra comments
    To ease up tracking where to do and look for changes.
    
    Change-Id: Id04827f222860d161bed272cd88ef84f95869086
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- modules/role/manifests/wmcs/openstack/eqiad1/virt.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/virt.pp
@@ -1,13 +1,14 @@
+# Nova hypervisors with local storage for instance storage (not ceph)
+# Most of the differences (if not all) will come from hiera parameters
+# see role/eqiad/wmcs/openstack/eqiad1/virt.yaml
 class role::wmcs::openstack::eqiad1::virt {
     system::role { $name: }
     include ::profile::standard
     # include ::profile::base::firewall
-    # NOTE: ceph is not enabled in this role. Starting in Queens
-    #       ceph-common is a dependency for the nova-common package
+    include ::profile::ceph::client::rbd_libvirt
     include ::profile::openstack::eqiad1::clientpackages
-    include ::profile::openstack::eqiad1::observerenv
+    include ::profile::openstack::eqiad1::envscripts
     include ::profile::openstack::eqiad1::nova::common
     include ::profile::openstack::eqiad1::nova::compute::service
-    include ::profile::openstack::eqiad1::envscripts
-    include ::profile::ceph::client::rbd_libvirt
+    include ::profile::openstack::eqiad1::observerenv
 }
--- modules/role/manifests/wmcs/openstack/eqiad1/virt_ceph.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/virt_ceph.pp
@@ -1,13 +1,14 @@
 # Nova hypervisors with ceph for instance storage
-#
+# Most of the differences (if not all) will come from hiera parameters
+# see role/eqiad/wmcs/openstack/eqiad1/virt_ceph.yaml
 class role::wmcs::openstack::eqiad1::virt_ceph {
     system::role { $name: }
     include ::profile::standard
     # include ::profile::base::firewall
     include ::profile::ceph::client::rbd_libvirt
     include ::profile::openstack::eqiad1::clientpackages
-    include ::profile::openstack::eqiad1::observerenv
+    include ::profile::openstack::eqiad1::envscripts
     include ::profile::openstack::eqiad1::nova::common
     include ::profile::openstack::eqiad1::nova::compute::service
-    include ::profile::openstack::eqiad1::envscripts
+    include ::profile::openstack::eqiad1::observerenv
 }
