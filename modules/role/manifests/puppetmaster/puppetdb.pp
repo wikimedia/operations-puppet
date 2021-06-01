d8b37e355f5844087830d7dbb20e569dfadaf973
John Bond <jbond@wikimedia.org>
2021-06-01 18:31:41 +0200

    O:puppetmaster::puppetdb: add nginx profile to puppetdb
    This should be a NO-OP as puppet db already installs nginx with the
    default values
    
    Bug: T164456
    Change-Id: Ib45534c85c7a97975b3e05c3bd77b05d7d67e95b

--- modules/role/manifests/puppetmaster/puppetdb.pp
+++ modules/role/manifests/puppetmaster/puppetdb.pp
@@ -1,10 +1,11 @@
 # filtertags: labs-project-deployment-prep labs-project-automation-framework labs-project-toolsbeta
 class role::puppetmaster::puppetdb {
-    include ::profile::standard
-    include ::profile::base::firewall
-    include ::profile::puppetdb::database
-    include ::profile::puppetdb
-    include ::profile::prometheus::postgres_exporter
+    include profile::standard
+    include profile::base::firewall
+    include profile::nginx
+    include profile::puppetdb::database
+    include profile::puppetdb
+    include profile::prometheus::postgres_exporter
 
     system::role { "puppetmaster::puppetdb (postgres ${::profile::puppetdb::database::role})":
         ensure      => 'present',
