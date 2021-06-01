12f44ed79724f6bfa5ac8955fcd9ac1ce3d59e6d
Amir Sarabadani <Ladsgroup@gmail.com>
2021-06-01 19:31:59 +0200

    mailman: Absent configuration files of mailman2 and make package absent
    Bug: T282303
    Change-Id: I03003b7ac38d12b37aacc19ba3118d9b64b0c9a9

--- modules/mailman/manifests/listserve.pp
+++ modules/mailman/manifests/listserve.pp
@@ -3,31 +3,15 @@ class mailman::listserve (
 ) {
 
     package { 'mailman':
-        ensure => present,
+        ensure => absent,
     }
 
     file { '/etc/mailman/mm_cfg.py':
+        ensure => absent,
         owner  => 'root',
         group  => 'root',
         mode   => '0444',
         source => 'puppet:///modules/mailman/mm_cfg.py',
-    }
-
-    debconf::set { 'mailman/site_languages':
-        value  => 'sk, gl, fa, ast, ar, ca, cs, da, de, en, es, et, eu, fi, fr, he, hr, hu, ia, it, ja, ko, lt, nl, no, pl, pt, pt_BR, ro, ru, sl, sr, sv, tr, uk, vi, zh_CN, zh_TW',
-        notify => Exec['dpkg-reconfigure mailman'],
-    }
-
-    debconf::set { 'mailman/default_server_language':
-        value  => 'en',
-        notify => Exec['dpkg-reconfigure mailman'],
-    }
-
-    exec { 'dpkg-reconfigure mailman':
-        command     => '/usr/sbin/dpkg-reconfigure -fnoninteractive mailman',
-        refreshonly => true,
-        require     => Class['::profile::locales::extended'],
-        before      => Service['mailman'],
     }
 
     service { 'mailman':
