d7dbe6ccaeee7221656b612db531375d7c2add66
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-06-01 18:40:43 +0200

    Disable peek crons
    Hosts: peek2001.codfw.wmnet
    Bug: T284090
    Change-Id: I86db946710f3e672784d6b325b9d519473014408

--- modules/peek/manifests/cron.pp
+++ modules/peek/manifests/cron.pp
@@ -1,6 +1,7 @@
 class peek::cron {
 
     cron { 'peek_monthly':
+        ensure      => absent,
         command     => '. $HOME/.profile; /var/lib/peek/git/peek.py -c /etc/peek/config/base.conf,/etc/peek/config/monthly.conf -s > /dev/null',
         environment => 'MAILTO=security-team@wikimedia.org',
         user        => 'peek',
@@ -10,6 +11,7 @@ class peek::cron {
     }
 
     cron { 'peek_weekly':
+        ensure      => absent,
         command     => '. $HOME/.profile; /var/lib/peek/git/peek.py -c /etc/peek/config/base.conf,/etc/peek/config/weekly.conf -s > /dev/null',
         environment => 'MAILTO=security-team@wikimedia.org',
         user        => 'peek',
