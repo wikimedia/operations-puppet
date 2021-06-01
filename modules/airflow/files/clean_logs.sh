492b1f5acf2d8891ccd82b633dae4b2d3749dca5
Ottomata <aotto@wikimedia.org>
2021-06-01 14:57:43 -0400

    airflow - add clean_logs.sh script
    Should have been included in last commit
    
    Bug: T272973
    Change-Id: If1749ac325648427fd61aa966a18f32a814cb69d

---
+++ modules/airflow/files/clean_logs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# NOTE: This file is managed by Puppet.
+
+# Deletes old files and directories in log_folder older than specified days.
+usage="$0 <log_folder> <older_than_days>"
+
+
+log_folder="${1}"
+
+
+
+if [ -e "${log_folder}" ]; then
+    echo "Error: log_folder '${log_folder}' does not exist."
+    echo "${usage}"
+    exit 1
+fi
+
+older_than_days="${2}"
+
+if [ -z "${older_than_days}" ]; then
+    echo "Error: Must specify <older_than_days>"
+    echo "${usage}"
+    exit 1
+fi
+
+echo "Deleting files and directories in ${log_folder} older than ${older_than_days}..."
+/usr/bin/find "${logs_folder}" -type f -mtime +"${older_than_days}" -delete && \
+/usr/bin/find "${logs_folder}" -type d -mtime +"${older_than_days}" -empty -delete
