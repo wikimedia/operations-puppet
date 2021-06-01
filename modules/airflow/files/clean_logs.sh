33b8551f50efc7584ebc1c40eae6704eab21526e
Ottomata <aotto@wikimedia.org>
2021-06-01 15:03:14 -0400

    airflow - clean_logs bug fix
    Change-Id: Ib80c64ff8057eb4a11c19d695e30cbe7e2199f6a

--- modules/airflow/files/clean_logs.sh
+++ modules/airflow/files/clean_logs.sh
@@ -5,25 +5,20 @@
 # Deletes old files and directories in log_folder older than specified days.
 usage="$0 <log_folder> <older_than_days>"
 
-
-log_folder="${1}"
-
-
-
-if [ -e "${log_folder}" ]; then
-    echo "Error: log_folder '${log_folder}' does not exist."
+logs_folder="${1}"
+if [ ! -e "${logs_folder}" ]; then
+    echo "Error: log_folder '${logs_folder}' does not exist."
     echo "${usage}"
     exit 1
 fi
 
 older_than_days="${2}"
-
 if [ -z "${older_than_days}" ]; then
     echo "Error: Must specify <older_than_days>"
     echo "${usage}"
     exit 1
 fi
 
-echo "Deleting files and directories in ${log_folder} older than ${older_than_days}..."
+echo "Deleting files and directories in ${logs_folder} older than ${older_than_days}..."
 /usr/bin/find "${logs_folder}" -type f -mtime +"${older_than_days}" -delete && \
 /usr/bin/find "${logs_folder}" -type d -mtime +"${older_than_days}" -empty -delete
