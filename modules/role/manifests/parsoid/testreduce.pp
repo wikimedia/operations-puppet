64cc973aa4a4807ee4f3126c4fca37c5e1428ffd
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-06-03 17:02:05 +0200

    Switch testreduce to profile::nginx
    To enable a later switch to nginx-light.
    
    Hosts: testreduce1001.eqiad.wmnet
    Bug: T164456
    Change-Id: I9701a4837e80e69c50fcf9521c3bdc12d8373bf8

--- modules/role/manifests/parsoid/testreduce.pp
+++ modules/role/manifests/parsoid/testreduce.pp
@@ -5,21 +5,22 @@ class role::parsoid::testreduce {
         description => 'Parsoid visual diffing tests'
     }
 
-    include ::profile::standard
-    include ::profile::base::firewall
+    include profile::standard
+    include profile::base::firewall
 
-    include ::profile::parsoid::testing
-    include ::profile::parsoid::testreduce
+    include profile::nginx
+    include profile::parsoid::testing
+    include profile::parsoid::testreduce
 
-    include ::profile::parsoid::vd_client
-    include ::profile::parsoid::vd_server
+    include profile::parsoid::vd_client
+    include profile::parsoid::vd_server
 
-    include ::profile::parsoid::rt_client
-    include ::profile::parsoid::rt_server
+    include profile::parsoid::rt_client
+    include profile::parsoid::rt_server
 
-    include ::profile::tlsproxy::envoy # TLS termination
+    include profile::tlsproxy::envoy # TLS termination
 
-    class { '::profile::mariadb::generic_server':
+    class { 'profile::mariadb::generic_server':
         datadir => '/srv/data/mysql',
     }
 }
