89c213bd90f247cc041541a70e985025c5e4a8ef
Erik Bernhardson <ebernhardson@wikimedia.org>
2021-06-03 12:56:14 -0700

    Undeploy mjolnir profile from analytics
    After I410c1dcea9 the analytics use case for mjolnir no longer needs
    this deployment, rather usage from airflow installs mjolnir as a
    python package along with the other python dependencies the airflow
    install manages. The profile itself is still needed to configure and
    install mjolnir daemons on search loader instances.
    
    This will not do any automated cleanup, the /etc/mjolnir directory
    along with the git repo in /srv/deployment need to be cleaned
    up.separately.
    
    Bug: T265547
    Change-Id: I9569595cf6406b45e5da0fcde897fa2ae29ba86d

--- modules/profile/manifests/analytics/cluster/elasticsearch.pp
+++ modules/profile/manifests/analytics/cluster/elasticsearch.pp
@@ -1,7 +1,5 @@
 # Supports CirrusSearch usage on the analytics cluster
 class profile::analytics::cluster::elasticsearch {
-    include profile::mjolnir
-
     # wikimedia/discovery/analytics will be deployed to this node
     scap::target { 'wikimedia/discovery/analytics':
         deploy_user => 'deploy-service',
