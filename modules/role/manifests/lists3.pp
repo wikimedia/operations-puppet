2e34a404bdd9a7b1e21049f20015a1bc35d46200
Amir Sarabadani <Ladsgroup@gmail.com>
2021-06-05 18:38:48 +0200

    mailman: Drop lists3 role
    It doesn't exist anymore. All are in lists role now.
    
    Bug: T282303
    Change-Id: I8e35b94b9e1d794bfebe949b2b5637d35a90f071

--- modules/role/manifests/lists3.pp
+++
@@ -1,9 +0,0 @@
-class role::lists3 {
-
-    system::role { 'lists3': description => 'Mailing list server (mailman3)', }
-
-    include profile::standard
-    include profile::base::firewall
-    include profile::mailman3
-}
-
