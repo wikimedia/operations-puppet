739291a595eb2972e03c8cd1a2d21127e7cb04a1
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2021-06-08 11:22:03 +0200

    icinga: update reading web Grafana alerts
    Bug: T281359
    Change-Id: I7818480bf1649afa07fb77823a2e9f162cc8e6b1

--- modules/icinga/manifests/monitor/reading_web.pp
+++ modules/icinga/manifests/monitor/reading_web.pp
@@ -1,15 +1,7 @@
 # == Class: icinga::monitor::reading_web
 #
-# Monitor the following Reading Web Grafana dashboards:
-#
-# * Page Previews Dashboard <grafana.wikimedia.org/dashboard/db/reading-web-page-previews>
+# Monitor Reading Web Grafana dashboards:
 class icinga::monitor::reading_web {
-    monitoring::grafana_alert { 'reading-web-page-previews':
-        dashboard_uid => '000000340',
-        contact_group => 'team-reading-web',
-        notes_url     => 'https://phabricator.wikimedia.org/rOPUP8e70f242a7888527d6af8ff8d823f82fe0202cd0',
-    }
-
     monitoring::grafana_alert { 'wikimedia-client-errors-alerts':
         dashboard_uid => '000000566',
         contact_group => 'team-reading-web,admins',
