4a2627b8e9de1c920083ba706fab4d08b48deeb5
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-06-08 16:49:09 +0200

    role::docker_registry_ha::registry: Switch to profile::nginx
    profile::nginx is our new standard profile to manage Nginx (initially
    to select the flavour). Switch the Docker registry to use it.
    
    Hosts: registry1003.eqiad.wmnet
    Bug: T164456
    Change-Id: Icaa0044e0d54d87768dfa743eb2a1685faafac7a

--- modules/role/manifests/docker_registry_ha/registry.pp
+++ modules/role/manifests/docker_registry_ha/registry.pp
@@ -1,9 +1,10 @@
 class role::docker_registry_ha::registry {
-    include ::profile::standard
-    include ::profile::base::firewall
-    include ::profile::docker_registry_ha::registry
+    include profile::standard
+    include profile::base::firewall
+    include profile::nginx
+    include profile::docker_registry_ha::registry
     system::role { 'docker_registry_ha':
         description => 'Docker registry HA',
     }
-    include ::profile::lvs::realserver
+    include profile::lvs::realserver
 }
