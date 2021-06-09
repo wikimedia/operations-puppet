fadcfe8755062cba7ec685ee0dc3fa68ae935549
John Bond <jbond@wikimedia.org>
2021-06-09 12:11:02 +0200

    apereo_cas: add base_url to type signiture
    Change-Id: Idab22b0d6a2d00fb1a92044e287aa49462912437

--- modules/apereo_cas/types/urls.pp
+++ modules/apereo_cas/types/urls.pp
@@ -1,9 +1,11 @@
 type Apereo_cas::Urls = Struct[{
     production => Struct[{
+        base_url => Stdlib::HTTPUrl,
         login_url => Stdlib::HTTPUrl,
         validate_url => Stdlib::HTTPUrl,
     }],
     staging => Struct[{
+        base_url => Stdlib::HTTPUrl,
         login_url => Stdlib::HTTPUrl,
         validate_url => Stdlib::HTTPUrl,
     }],
