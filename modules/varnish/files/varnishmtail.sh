f7f0373f95e9b3fae3c8de0bf84257b0284d3814
Emanuele Rocca <ema@wikimedia.org>
2021-06-10 16:29:02 +0200

    varnish: add timing data to varnishmtail
    Bug: T284576
    Hosts: cp3050.esams.wmnet
    Change-Id: I715cd83ac5e65ba40f995218aa0eb1826901a9b6

--- modules/varnish/files/varnishmtail.sh
+++ modules/varnish/files/varnishmtail.sh
@@ -15,7 +15,28 @@ fmt_inm='inm %{If-None-Match}i'
 fmt_ttfb='ttfb %{Varnish:time_firstbyte}x'
 fmt_cache_int='cache_int %{X-Cache-Int}o'
 
-FMT="${fmt_side}\t${fmt_url}\t${fmt_cache_status}\t${fmt_http_status}\t${fmt_http_method}\t${fmt_cache_control}\t${fmt_inm}\t${fmt_ttfb}\t${fmt_cache_int}\t"
+# Request handling timestamps
+fmt_timestamp_start='tstart %{VSL:Timestamp:Start[3]}x'
+fmt_timestamp_req='treq %{VSL:Timestamp:Req[3]}x'
+fmt_timestamp_reqbody='treqbody %{VSL:Timestamp:ReqBody[3]}x'
+fmt_timestamp_waitinglist='twaitinglist %{VSL:Timestamp:WaitingList[3]}x'
+fmt_timestamp_fetch='tfetch %{VSL:Timestamp:Fetch[3]}x'
+fmt_timestamp_process='tprocess %{VSL:Timestamp:Process[3]}x'
+fmt_timestamp_resp='tresp %{VSL:Timestamp:Resp[3]}x'
+fmt_timestamp_restart='trestart %{VSL:Timestamp:Restart[3]}x'
+
+# Pipe handling timestamps
+fmt_timestamp_pipe='tpipe %{VSL:Timestamp:Pipe[3]}x'
+fmt_timestamp_pipesess='tpipesess %{VSL:Timestamp:PipeSess[3]}x'
+
+# Backend fetch timestamps
+fmt_timestamp_bereq='tbereq %{VSL:Timestamp:Bereq[3]}x'
+fmt_timestamp_beresp='tberesp %{VSL:Timestamp:Beresp[3]}x'
+fmt_timestamp_berespbody='tberespbody %{VSL:Timestamp:BerespBody[3]}x'
+fmt_timestamp_retry='tretry %{VSL:Timestamp:Retry[3]}x'
+fmt_timestamp_error='terror %{VSL:Timestamp:Error[3]}x'
+
+FMT="${fmt_side}\t${fmt_url}\t${fmt_cache_status}\t${fmt_http_status}\t${fmt_http_method}\t${fmt_cache_control}\t${fmt_inm}\t${fmt_ttfb}\t${fmt_cache_int}\t${fmt_timestamp_start}\t${fmt_timestamp_req}\t${fmt_timestamp_reqbody}\t${fmt_timestamp_waitinglist}\t${fmt_timestamp_fetch}\t${fmt_timestamp_process}\t${fmt_timestamp_resp}\t${fmt_timestamp_restart}\t${fmt_timestamp_pipe}\t${fmt_timestamp_pipesess}\t${fmt_timestamp_bereq}\t${fmt_timestamp_beresp}\t${fmt_timestamp_berespbody}\t${fmt_timestamp_retry}\t${fmt_timestamp_error}\t"
 
 # Pass -c and -b to log requests from clients (tls terminators) and to backends (origin)
 /usr/bin/varnishncsa -P /run/varnishncsa-mtail.pid -n frontend -c -b -F "${FMT}" | mtail -progs "${PROGS}" -logs /dev/stdin -disable_fsnotify $MTAIL_ARGS &
