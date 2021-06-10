3e91acc48f7813240db90d1b75173009c9a13412
Jbond <jbond@wikimedia.org>
2021-06-10 09:16:25 +0000

    Revert "P:cumin::master: Use sudo to run the check command"
    This reverts commit f5f0ed3247e69394a92cb3798a7b7de9eac4db95.
    
    Reason for revert: Strange error which looks unrelated but revert to rule out
    
    Change-Id: I910ad7bbb62b49501ecb4eaad9988a9adaa941ee
    Error: Could not retrieve catalog from remote server: Error 500 on SERVER: Server Error: Evaluation Error: Error while evaluating a Function Call, Lookup of key 'profile::standard::has_default_mail_relay' failed: Unable to parse (/etc/puppet/private/hieradata/role/common/cluster/management.yaml): mapping values are not allowed in this context at line 3 column 17 (file: /etc/puppet/modules/role/manifests/cluster/management.pp, line: 12, column: 5) on node cumin1001.eqiad.wmnet

--- modules/profile/manifests/cumin/monitoring_agentrun.pp
+++ modules/profile/manifests/cumin/monitoring_agentrun.pp
@@ -12,17 +12,14 @@ class profile::cumin::monitoring_agentrun (
         content => file('profile/cumin/check_puppet_run_changes.py'),
         require => Package['python3-pypuppetdb'],
     }
+
     $nrpe_command = @("COMMAND"/L)
-    sudo /usr/lib/nagios/plugins/check_puppet_run_changes \
+    /usr/lib/nagios/plugins/check_puppet_run_changes \
     -w ${warn} -c ${crit} \
     --ssl-key ${facts['puppet_config']['hostprivkey']} \
     --ssl-cert ${facts['puppet_config']['hostcert']} \
     --ssl-ca ${facts['puppet_config']['localcacert']}
     | COMMAND
-    sudo::user {'check_puppet_run command':
-        user       => 'nagios',
-        privileges => ["ALL = NOPASSWD: ${nrpe_command}"],
-    }
     nrpe::monitor_service{'puppet_run_changes':
         description    => 'Ensure hosts are not performing a change on every puppet run',
         nrpe_command   => $nrpe_command,
