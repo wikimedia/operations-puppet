d844adec507601d4af396f79a6da9c5565111f59
Erik Bernhardson <ebernhardson@wikimedia.org>
2021-06-14 15:21:51 -0700

    mjolnir: Provide prioritized topics to bulk daemon
    Moves the recently added prioritized topics into a new
    cli argument that flags them specifically as such.
    
    Bug: T261407
    Change-Id: Id25fb0bde18c93e3f11edfc1f88846d51441377e

--- modules/profile/manifests/mjolnir/kafka_bulk_daemon.pp
+++ modules/profile/manifests/mjolnir/kafka_bulk_daemon.pp
@@ -8,16 +8,22 @@
 # $kafka_cluster - Name of kafka cluster to consume from
 # $group_id - Kafka consumer group to join.
 # $topics - Name of one or more kafka topics to consume from
+# $priority_topics - Name of one of more kafka topics to consume from and give priority to.
 
 class profile::mjolnir::kafka_bulk_daemon(
     String $kafka_cluster = lookup('profile::mjolnir::kafka_bulk_daemon::kafka_cluster'),
     String $group_id = lookup('profile::mjolnir::kafka_bulk_daemon::group_id'),
     String $es_cluster_endpoint = lookup('profile::mjolnir::kafka_bulk_daemon::es_cluster_endpoint', { 'default_value' => 'localhost:9200' }),
     Array[String] $topics = lookup('profile::mjolnir::kafka_bulk_daemon::topics'),
+    Array[String] $priority_topics = lookup('profile::mjolnir::kafka_bulk_daemon::priority_topics'),
     Array[String] $prometheus_nodes = lookup('prometheus_nodes', { 'default_value' => [] }),
     Wmflib::Ensure $ensure = lookup('profile::mjolnir::kafka_bulk_daemon::ensure', { 'default_value' => 'present' }),
 ) {
     require ::profile::mjolnir
+
+    if empty($topics) and empty($priority_topics) {
+        fail('you must provide either "topics" or "priority_topics"')
+    }
 
     $prometheus_port = 9170
     $kafka_config = kafka_config($kafka_cluster)
