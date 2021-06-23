21ff27573c38790eda2e45861cc7b6bbebc6836c
David Caro <dcaro@wikimedia.org>
2021-06-23 11:40:04 +0200

    toolforge.exec_environ: add tests
    Simple dummy tests to check if there's any typos/big issues with the
    class.
    
    Bug: T277653
    Bug: T275865
    Hosts: re:^tools.*(wmnet|cloud)$
    Change-Id: I6d2711a2d0a7e9c3a8d5fcc3fcae69068a1f7369
    Signed-off-by: David Caro <dcaro@wikimedia.org>

---
+++ modules/profile/spec/classes/profile_toolforge_grid_exec_environ.rb
@@ -0,0 +1,10 @@
+require_relative '../../../../rake_modules/spec_helper'
+describe 'profile::toolforge::grid::exec_environ' do
+  on_supported_os(WMFConfig.test_on).each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) { { } }
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end
