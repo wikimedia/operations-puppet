8392c6e043948603227035cd6c337f19dbff0212
David Caro <dcaro@wikimedia.org>
2021-06-24 18:28:27 +0200

    cinderutils.ensure: use 11MB as filesystem space lost
    It turns out that it's a bit more than 10MB, so bumping to 11.
    
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: I770d97fc846267d35a0b9119f69a3b0c8ae10c41

--- modules/cinderutils/manifests/ensure.pp
+++ modules/cinderutils/manifests/ensure.pp
@@ -39,8 +39,8 @@ define cinderutils::ensure(
     Integer          $max_gb        = 1000,
 ){
     if has_key($facts['mountpoints'], $mount_point) {
-        # account for 10MB needed for the partitioning and formatting
-        $min_size_bytes = ($min_gb * 1024 - 10) * 1024 * 1024
+        # account for 11MB needed for the partitioning and formatting
+        $min_size_bytes = ($min_gb * 1024 - 11) * 1024 * 1024
         if $facts['mountpoints'][$mount_point]['size_bytes'] < $min_size_bytes {
             fail("Volume at ${mount_point} is smaller than required size of ${min_gb}GB")
         }
