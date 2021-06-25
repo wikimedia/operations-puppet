63279cb267faf5eb254f29a7aac7b0426c323192
John Bond <jbond@wikimedia.org>
2021-06-25 15:04:13 +0200

    debian::autostart: function to prevent services autostarting on install
    On bullseye use systemd presets, for older distros (where presets don't
    work), use update-rc.d to prevent a service from automatically starting
    after installation.
    
    This works for both services with native systemd units and sysvinit scripts.
    
    See follow up patch for usecase
    https://www.debian.org/doc/debian-policy/ch-opersys.html#managing-the-links
    https://freedesktop.org/wiki/Software/systemd/Preset/
    
    Change-Id: Id7a8f4de98a1ca6035224899b6311786318a9091

---
+++ modules/debian/functions/autostart.pp
@@ -0,0 +1,37 @@
+# @summary create a function to prevent services from autostarting on installation
+# @param service the name of the service
+# @param enabled boolean indicating if the service autostart should be enabled.
+function debian::autostart (
+    String  $service,
+    Boolean $enabled,
+) {
+    # For Debian Bullseye and later we use the systemd-native preset feature.
+    # This doesn't work in Buster and older, so on those distros we fall back
+    # to update-rc.d
+    if debian::codename::ge('bullseye') {
+
+        file { "/etc/systemd/system-preset/${service}.preset":
+            ensure  => stdlib::ensure($enabled, 'file'),
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            content => "disable ${service}.service",
+        }
+
+    } else {
+
+        if $enabled {
+            $action  = 'enable'
+            $creates = "/etc/rc3.d/S01${service}"
+            $unless  = undef
+        } else {
+            $action  = 'disabled'
+            $creates = undef
+            $unless  = "/usr/bin/test -L /etc/rc3.d/S01${service}"
+        }
+        exec {"/usr/sbin/update-rc.d ${service} ${action}":
+            creates => $creates,
+            unless  => $unless,
+        }
+    }
+}
