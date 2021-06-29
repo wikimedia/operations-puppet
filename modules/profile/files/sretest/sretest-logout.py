4d56b5dad5317c0eaa6ed7e2db44ac01b846f79b
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-06-29 15:51:37 +0200

    Convert sretest-logout.py to wmflib.idm
    Change-Id: I8b744c35d692461330a71c3cf7448bfaa5d3fdd3

--- modules/profile/files/sretest/sretest-logout.py
+++ modules/profile/files/sretest/sretest-logout.py
@@ -1,91 +1,56 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import argparse
 import json
-import os
 import subprocess
-import sys
-
-SERVICE = 'sretest'
+from wmflib.idm import LogoutdBase
 
 
-# Return codes follow the logout.d semantics, see T283242
-def logout(uid, verbose):
+class SystemdLogind(LogoutdBase):
+    """sretest"""
 
-    try:
-        output = subprocess.check_output(["/usr/bin/loginctl", "terminate-user", uid],
-                                         universal_newlines=True).strip()
-    except subprocess.CalledProcessError as error:
-        print('Failed to logout user {}: {}'.format(uid, error.returncode))
-        return 1
+    user_identifier: str = 'uid'
 
-    if verbose:
-        print(output)
-    return 0
+    # Return codes follow the logout.d semantics, see T283242
+    def logout_user(self, user):
+        try:
+            output = subprocess.check_output(["/usr/bin/loginctl", "terminate-user", user],
+                                             universal_newlines=True).strip()
+        except subprocess.CalledProcessError as error:
+            print('Failed to logout user {}: {}'.format(user, error.returncode))
+            return 1
 
-
-# Return codes follow the logout.d semantics, see T283242
-def query(uid):
-    output = ""
-
-    res = {'id': uid}
-
-    try:
-        output = subprocess.check_output(["/usr/bin/loginctl", "--no-pager", "user-status", uid],
-                                         universal_newlines=True).strip()
-    except subprocess.CalledProcessError as error:
-        res['active'] = 'unknown'
-        res['verbose'] = error.output
-        print(json.dumps(res))
+        if self._args.verbose:
+            print(output)
         return 0
 
-    if output:
-        res['active'] = "active"
-    else:
-        res['active'] = "inactive"
+    def list(self):
+        pass
 
-    res['verbose'] = output
-    print(json.dumps(res))
-    return 1
+    # Return codes follow the logout.d semantics, see T283242
+    def query_user(self, user):
+        output = ""
+        print(user)
+        res = {'id': user}
 
+        try:
+            output = subprocess.check_output(["/usr/bin/loginctl", "--no-pager", "user-status",
+                                              user], universal_newlines=True).strip()
+        except subprocess.CalledProcessError as error:
+            res['active'] = 'unknown'
+            res['verbose'] = error.output
+            print(json.dumps(res))
+            return 0
 
-def main():
-    if os.geteuid() != 0:
-        print("Logout script needs to be run as root")
-        sys.exit(1)
+        if output:
+            res['active'] = "active"
+        else:
+            res['active'] = "inactive"
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--verbose", action="store_true", dest="verbose",
-                        help="Enable verbose output")
-
-    subp = parser.add_subparsers(title='Command', description='Valid commands', dest='command')
-    subp.required = True
-
-    parser_logout = subp.add_parser('logout', help='Logout a user')
-    parser_logout.add_argument('--uid',
-                               help="Forcibly logout LDAP UID for service {}".
-                               format(SERVICE), required=True)
-    parser_logout.add_argument('--cn',
-                               help="Forcibly logout wikitech name/CN for service {}".
-                               format(SERVICE))
-
-    parser_query = subp.add_parser('query', help='Query the login state of a user')
-    parser_query.add_argument('--uid',
-                              help="Query login state for LDAP UID at service {}".
-                              format(SERVICE), required=True)
-    parser_query.add_argument('--cn',
-                              help="Query login state for wikitech name/CN at service {}".
-                              format(SERVICE))
-
-    args = parser.parse_args()
-    if args.command == "logout":
-        sys.exit(logout(args.uid, args.verbose))
-
-    elif args.command == "query":
-        sys.exit(query(args.uid))
+        res['verbose'] = output
+        print(json.dumps(res))
+        return 1
 
 
-if __name__ == '__main__':
-    main()
+systemdlogoutd = SystemdLogind()
+raise SystemExit(systemdlogoutd.run())  # This includes the parsing of command line arguments.
