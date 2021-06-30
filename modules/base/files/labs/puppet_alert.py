6665c9970bef47be04d671f2cf144344d9c03033
David Caro <dcaro@wikimedia.org>
2021-06-30 12:04:43 +0200

    wmcs.puppet_alert: Add more info and differentiate cases
    We were always just checking the last successful run time, not caring
    if it's still running or what errors happened.
    
    This adds some extra info, like the last run summary and adds a line
    to differentiate between:
    * puppet not running but having passed the last time
    * puppet running but failing
    * puppet not running and having failed the last time
    
    Bug: T285839
    Change-Id: I241243295ce5d2567f7aee3f4c6ed86b140f5a38
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- modules/base/files/labs/puppet_alert.py
+++ modules/base/files/labs/puppet_alert.py
@@ -24,65 +24,114 @@ import socket
 import sys
 import time
 
+import yaml
 from notify_maintainers import email_admins
 
 sys.path.append("/usr/local/sbin/")
 
 # Nag if it's been 24 hours since the last puppet run
 NAG_INTERVAL = 60 * 60 * 24
+PUPPET_STATE_FILE = "/var/lib/puppet/state/last_run_summary.yaml"
+# last_run_summary.yaml reports the last run but it updates the stamp even
+# on failed runs. Instead, check to see the last time puppet actually did
+# something.
+PUPPET_SUCCESS_TIMESTAMP_FILE = "/var/lib/puppet/state/classes.txt"
 
 logger = logging.getLogger(__name__)
 
 
-def lastrun():
-    # last_run_summary.yaml reports the last run but it updates the stamp even
-    # on failed runs. Instead, check to see the last time puppet actually did
-    # something.
-    try:
-        return os.path.getmtime("/var/lib/puppet/state/classes.txt")
-    except os.error:
-        logger.warning(
-            "Unable to determine last puppet run; classes.txt missing."
-        )
-        # Returning 0 should imply that puppet has been broken since day one,
-        # which is likely the case here!
-        return 0
+def get_last_success_time():
+    return os.path.getmtime(PUPPET_SUCCESS_TIMESTAMP_FILE)
+
+
+def get_last_run_summary():
+    return yaml.load(open(PUPPET_STATE_FILE))
 
 
 def main():
-    elapsed = calendar.timegm(time.gmtime()) - lastrun()
-    if elapsed > NAG_INTERVAL:
+    exception_msg = ""
+    first_line = ""
 
-        try:
-            with open("/etc/wmflabs-project") as f:
-                project_name = f.read().strip()
+    try:
+        last_success_elapsed = (
+            calendar.timegm(time.gmtime()) - get_last_success_time()
+        )
+        too_old = last_success_elapsed < NAG_INTERVAL
+    except os.error as error:
+        logging.warning("Unable to check puppet last success time: %s", error)
+        exception_msg += (
+            "\nUnable to check puppet last success time: {}".format(error)
+        )
+        last_success_elapsed = -1
+        too_old = True
 
-        except Exception as error:
-            logger.warning(
-                "Unable to determine the current vm project: %s", error
+    try:
+        last_run_summary = get_last_run_summary()
+        has_errors = last_run_summary["events"]["failure"] > 0
+    except Exception as error:
+        logging.warning("Unable to read puppet last run summary: %s", error)
+        exception_msg += "\nUnable to read puppet last run summary: {}".format(
+            error
+        )
+        last_run_summary = {}
+        has_errors = True
+
+    if too_old and has_errors:
+        first_line = (
+            "Puppet did not run in the last {} seconds, and the last run was "
+            "a failure.".format(
+                last_success_elapsed
             )
-            project_name = "no_project"
-
-        fqdn = socket.getfqdn()
-        ip = socket.gethostbyname(socket.gethostname())
-
-        # No f-strings until after Stretch goes away! Instead we have this ugly
-        # stuff.
-        subject = "[Cloud VPS alert][{}] Puppet failure on {} ({})".format(
-            project_name, fqdn, ip
         )
 
-        logger.info(
-            (
-                "It has been %s seconds since last Puppet run. Sending nag "
-                "emails."
-            ),
-            NAG_INTERVAL,
+    elif too_old:
+        first_line = (
+            "Puppet did not run in the last {}, though the last run was a "
+            "success.".format(
+                last_success_elapsed
+            )
         )
 
-        body = """
-Puppet is failing to run on the "{fqdn} ({ip})" instance in project
+    elif has_errors:
+        first_line = "Puppet is running with failures."
+
+    if not too_old and not has_errors:
+        logging.info("Puppet is running correctly, not notifying anyone.")
+        return
+
+    try:
+        with open("/etc/wmflabs-project") as f:
+            project_name = f.read().strip()
+
+    except Exception as error:
+        logger.warning("Unable to determine the current vm project: %s", error)
+        exception_msg += (
+            "\nUnable to determine the current vm project: {}".format(error)
+        )
+        project_name = "no_project"
+
+    fqdn = socket.getfqdn()
+    ip = socket.gethostbyname(socket.gethostname())
+
+    logger.info(
+        (
+            "It has been more than %s seconds since last Puppet run or it "
+            "failed. Sending nag emails."
+        ),
+        NAG_INTERVAL,
+    )
+
+    # No f-strings until after Stretch goes away! Instead we have this ugly
+    # stuff.
+    subject = "[Cloud VPS alert][{}] Puppet failure on {} ({})".format(
+        project_name, fqdn, ip
+    )
+
+    body = """
+Puppet is having issues on the "{fqdn} ({ip})" instance in project
 {project_name} in Wikimedia Cloud VPS.
+
+{first_line}
 
 Working Puppet runs are needed to maintain instance security and logins.
 As long as Puppet continues to fail, this system is in danger of becoming
@@ -97,9 +146,20 @@ You might find some help here:
 
 For further support, visit #wikimedia-cloud on libera.chat or
 <https://wikitech.wikimedia.org>
-""".format(fqdn=fqdn, ip=ip, project_name=project_name)
-        email_admins(subject, body)
+
+Some extra info follows:
+---- Last run summary:
+{last_run_summary}
+
+---- Exceptions that happened if any:
+{exception_msg}
+    """.format(
+        fqdn=fqdn, ip=ip, first_line=first_line, project_name=project_name,
+        last_run_summary=yaml.dump(last_run_summary), exception_msg=exception_msg
+    )
+    email_admins(subject, body)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     main()
