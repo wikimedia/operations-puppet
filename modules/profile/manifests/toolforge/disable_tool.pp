c5b1765ef64d4d31eb19b56d894e838087c6ee86
Andrew Bogott <abogott@wikimedia.org>
2021-07-01 14:30:23 -0500

    profile::toolforge::disable_tool: standardize on the singular 'disable_tool'
    Bug: T170355
    Change-Id: I066ec9baa9baf893c17f6b8f7bce1340c5b91083

--- modules/profile/manifests/toolforge/disable_tool.pp
+++ modules/profile/manifests/toolforge/disable_tool.pp
@@ -2,16 +2,16 @@ class profile::toolforge::disable_tool (
     String $novaadmin_bind_dn = lookup('profile::openstack::eqiad1::ldap_user_pass'),
     String $novaadmin_bind_pass = lookup('profile::openstack::base::ldap_user_dn')
 ) {
-    file { '/etc/disable_tools.conf':
+    file { '/etc/disable_tool.conf':
         ensure  => file,
         owner   => 'root',
         group   => 'root',
         mode    => '0500',
-        content => template('profile/toolforge/disable_tools.conf.erb'),
+        content => template('profile/toolforge/disable_tool.conf.erb'),
     }
 
-    git::clone { 'cloud/toolforge/disable-tools':
+    git::clone { 'cloud/toolforge/disable-tool':
         ensure    => latest,
-        directory => '/srv/disable-tools',
+        directory => '/srv/disable-tool',
     }
 }
