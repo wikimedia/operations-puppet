35217f0285dd1429771b2151244a85b07ccbabfd
Amir Sarabadani <Ladsgroup@gmail.com>
2021-07-06 19:38:23 +0200

    dumps: Migrate kiwix update cron to systemd timer
    This is part of the work needed to upgrade puppet to version six as
    crons are not supported in core anymore.
    
    Bug: T273673
    Change-Id: I812bb6c242797c058a1a564877115616c9c16cb3

--- modules/dumps/manifests/web/fetches/kiwix.pp
+++ modules/dumps/manifests/web/fetches/kiwix.pp
@@ -22,12 +22,23 @@ class dumps::web::fetches::kiwix(
     }
 
     cron { 'kiwix-mirror-update':
-        ensure      => 'present',
+        ensure      => absent,
         environment => 'MAILTO=ops-dumps@wikimedia.org',
         command     => "/bin/bash /usr/local/bin/kiwix-rsync-cron.sh ${miscdatasetsdir}",
         user        => $user,
         minute      => '15',
         hour        => '*/2',
         require     => File['/usr/local/bin/kiwix-rsync-cron.sh'],
+    }
+    systemd::timer::job { 'kiwix-mirror-update':
+        ensure             => present,
+        description        => 'Regular jobs to update kiwix mirror',
+        user               => $user,
+        monitoring_enabled => false,
+        send_mail          => true,
+        environment        => {'MAILTO' => 'ops-dumps@wikimedia.org'},
+        command            => "/bin/bash /usr/local/bin/kiwix-rsync-cron.sh ${miscdatasetsdir}",
+        interval           => {'start' => 'OnCalendar', 'interval' => '*-*-* 0/2:15:0'},
+        require            => File['/usr/local/bin/kiwix-rsync-cron.sh'],
     }
 }
