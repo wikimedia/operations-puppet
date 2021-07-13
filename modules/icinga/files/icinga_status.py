3d7eee4007821b0359c8b866c2ba4efd4cc1652e
Reuven Lazarus <rlazarus@wikimedia.org>
2021-07-13 16:38:52 -0400

    icinga: Performance improvements to icinga-status
    This change cuts out >80% of the clock time to run `icinga-status
    cumin1001` as measured at alert1001 -- from 2.839s down to 0.502s. That
    makes it more feasible to use icinga-status for quick checks (like "is
    this hostname known to icinga").
    
    Two changes are involved (and they're fairly inseparable; sorry for the
    large diff):
    
    1. We've always had a list of hosts we're interested in, given on the
       command line. Now, we take those hostnames and pass them through to
       _parse_status(). As soon as we find out a block of the status file
       concerns a host that we're interested in, we skip to the next block.
       That way we can skip parsing most of the file, which we were never
       going to read.
    
    2. In order to do that efficiently, we don't loop through the whole file
       line by line -- if we did, we'd still have to scan through those
       blocks we don't care about, in order to look for the close bracket.
       Instead, we start by splitting the whole file into blocks, then parse
       each of *those* line by line. That way, "skip to the next block" is
       fast.
    
    I made a couple of other optimizations, e.g. avoiding most calls to
    split() and strip(), and using string slicing instead because it's
    faster -- normally I wouldn't bother because it's a little less
    readable, but at the center of this hot loop the time savings turned out
    to be meaningful.
    
    Bug: T285803
    Change-Id: Id924461e6ea3d9a772be6de3862a6c4ee79e573f

--- modules/icinga/files/icinga_status.py
+++ modules/icinga/files/icinga_status.py
@@ -18,6 +18,7 @@ import logging
 
 from argparse import ArgumentParser
 from json import dumps as json_dumps
+from pathlib import Path
 from time import sleep
 
 from ClusterShell.NodeSet import NodeSet, RESOLVER_NOGROUP
@@ -155,24 +156,24 @@ class Host:
 class IcingaStatus:
     """Object to represent an icinga status.dat file"""
 
-    def __init__(self, filename):
+    def __init__(self, status_path, target_hostnames):
         try:
-            with open(filename) as status_file:
-                self._lines = status_file.readlines()
+            status_text = status_path.read_text()
         except OSError as error:
             raise IcingaStatusParseError('corrupt status.dat: Failed to open file: {}'.format(
                 error))
 
         self.hosts = {}
-        self._parse_status()
+        self._target_hostnames = target_hostnames
+        self._parse_status(status_text)
 
     def get_host(self, name):
         """Return a Host object matching `name`"""
         return self.hosts[name]
 
-    def get_hosts(self, names):
-        """Return a dict of Hosts matching `names`"""
-        return {name: self.hosts.get(name, False) for name in names}
+    def get_hosts(self):
+        """Return a dict of Hosts matching `target_hostnames` given in __init__"""
+        return {name: self.hosts.get(name, False) for name in self._target_hostnames}
 
     def get_downtimed_hosts(self):
         """Return a dict of the current hosts that have scheduled downtime"""
@@ -186,51 +187,62 @@ class IcingaStatus:
         """Return all Host objects with a Service matching `name`"""
         return [host for host in self.hosts.values() if host.has_service(name)]
 
-    def _add_block(self, name, data):
-        if name == 'hoststatus':
-            host = Host(data)
-            self.hosts[host.name] = host
-        elif name == 'servicestatus':
-            service = Service(data)
-            self.hosts[service.host].add_service(service)
-
-    def _parse_status(self):
-        block_name = None
-        block = {}
-
-        for line in self._lines:
-            if line[0] in ('\n', '#'):  # Skip empty lines and comments
-                continue
-            elif line[0].isalpha():  # New block found
-                block_name = line.split()[0]
-                continue
-
-            sline = line.strip()
-            if sline == '}':
-                self._add_block(block_name, block)
-                block_name = None
-                block = {}
-                continue
-            # Theses objects appear last in the file so we can exit the function
-            if block_name in ('hostdowntime', 'servicedowntime'):
+    def _parse_status(self, status_text):
+        # The status file is several million lines long, consisting of blocks that start with
+        # "BLOCK_NAME {", contain "\tKEY=VALUE" lines, and end with "\t}". For performance reasons,
+        # rather than iterating on the status file line-by-line, we instead start by splitting it
+        # into those blocks so that we can parse each of them individually. (Some blocks will also
+        # have leading blank lines, and/or comments.)
+        #
+        # The reason that's so much faster is that most blocks describe hosts (or services on hosts)
+        # that weren't listed on the command line, so we're never going to read them. Splitting this
+        # way means that we can immediately bail out of those blocks and skip to the next.
+        blocks = status_text.split('\n\t}\n')
+        for block in blocks:
+            name, data = self._parse_block(block)
+            # These objects appear last in the file so we can exit the function
+            if name in {'hostdowntime', 'servicedowntime'}:
                 return
-
-            # Small optimization
-            if block_name not in ('hoststatus', 'servicestatus'):
+            if not data:
                 continue
+            if name == 'hoststatus':
+                host = Host(data)
+                self.hosts[host.name] = host
+            elif name == 'servicestatus':
+                service = Service(data)
+                self.hosts[service.host].add_service(service)
+        # If we get to this point we have found no downtime and likely read a corrupt file
+        raise IcingaStatusParseError('corrupt status.dat: Failed to find downtime object')
 
-            # Block key=value entry
-            parts = sline.split('=', 1)
-            block[parts[0]] = parts[1]
-        # If we get to this point we have found no downtime and likley read a corrupt file
-        raise IcingaStatusParseError('corrupt status.dat: Failed to found downtime object')
+    def _parse_block(self, block):
+        name = None
+        data = {}
+        for line in block.splitlines():
+            if not line or line[0] == '#':  # Skip empty lines and comments.
+                continue
+            if line[0] != '\t':  # Found the start of the block.
+                name = line[:-2]  # Strip off the trailing " {"
+                if name not in {'hoststatus', 'servicestatus'}:
+                    # Not a block type we're interested in, skip the whole thing.
+                    return name, None
+                continue
+            sline = line[1:]  # Strip off the leading tab.
+            key, value = sline.split('=', 1)
+            if key == 'host_name' and value not in self._target_hostnames:
+                # Not a host we're interested in, skip it.
+                return name, None
+            data[key] = value
+        # The split() function in _parse_status removed the trailing "}", so as soon as we run out
+        # of lines, we've finished the block.
+        return name, data
 
 
 def get_args():
     """Argument handler"""
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('hosts', help="Hosts selection query")
-    parser.add_argument('-s', '--status-file', default='/var/icinga-tmpfs/status.dat')
+    parser.add_argument('-s', '--status-file', type=Path,
+                        default=Path('/var/icinga-tmpfs/status.dat'))
     parser.add_argument('-v', '--verbose', action='count')
     parser.add_argument('-p', '--pretty-print', action='store_true',
                         help='pretty print json output.  Implies `--json`')
@@ -259,20 +271,20 @@ def main():
     log_level = get_log_level(args.verbose)
     logging.basicConfig(level=log_level)
 
+    hosts_nodeset = NodeSet(args.hosts, resolver=RESOLVER_NOGROUP)
+    if args.verbatim_hosts:
+        hosts_set = set(hosts_nodeset)
+    else:
+        hosts_set = {host.split('.')[0] for host in hosts_nodeset}
+
     try:
-        status = IcingaStatus(args.status_file)
+        status = IcingaStatus(args.status_file, hosts_set)
     except IcingaStatusParseError as error:
         logging.error('Failed to read status.dat (retrying): %s', error)
         sleep(0.5)
-        status = IcingaStatus(args.status_file)
+        status = IcingaStatus(args.status_file, hosts_set)
 
-    hosts_nodeset = NodeSet(args.hosts, resolver=RESOLVER_NOGROUP)
-    if args.verbatim_hosts:
-        hosts_list = list(hosts_nodeset)
-    else:
-        hosts_list = [host.split('.')[0] for host in hosts_nodeset]
-
-    hosts = status.get_hosts(hosts_list)
+    hosts = status.get_hosts()
 
     for host, status in hosts.items():
         if status is False:
