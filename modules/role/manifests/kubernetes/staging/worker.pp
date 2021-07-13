c6c0350f8076d0dde6fb27a9ff34efef252e5676
Janis Meybohm <jmeybohm@wikimedia.org>
2021-07-13 14:26:12 +0200

    kubernetes::*::worker: include dragonfly dfdaemon
    Include profile::dragonfly::dfdaemon to kubernetes worker roles (staging
    and production) but only enable it in codfw staging for initial
    verification of the dragonfly patches.
    
    Bug: T286054
    Change-Id: I1bb76278cf1145278b5497f0558c744e58d35ef1

--- modules/role/manifests/kubernetes/staging/worker.pp
+++ modules/role/manifests/kubernetes/staging/worker.pp
@@ -1,15 +1,17 @@
 class role::kubernetes::staging::worker {
-    include ::profile::standard
-    include ::profile::base::firewall
-    include ::profile::base::linux419
+    include profile::standard
+    include profile::base::firewall
+    include profile::base::linux419
 
     # Sets up docker on the machine
-    include ::profile::docker::storage
-    include ::profile::docker::engine
+    include profile::docker::storage
+    include profile::docker::engine
+    # Setup dfdaemon and configure docker to use it
+    include profile::dragonfly::dfdaemon
     # Setup kubernetes stuff
-    include ::profile::kubernetes::node
+    include profile::kubernetes::node
     # Setup calico
-    include ::profile::calico::kubernetes
+    include profile::calico::kubernetes
 
     system::role { 'kubernetes::staging::worker':
         description => 'Kubernetes worker node (staging setup)',
--- modules/role/manifests/kubernetes/worker.pp
+++ modules/role/manifests/kubernetes/worker.pp
@@ -1,17 +1,19 @@
 class role::kubernetes::worker {
-    include ::profile::standard
-    include ::profile::base::firewall
-    include ::profile::base::linux419
+    include profile::standard
+    include profile::base::firewall
+    include profile::base::linux419
 
     # Sets up docker on the machine
-    include ::profile::docker::storage
-    include ::profile::docker::engine
+    include profile::docker::storage
+    include profile::docker::engine
+    # Setup dfdaemon and configure docker to use it
+    include profile::dragonfly::dfdaemon
     # Setup kubernetes stuff
-    include ::profile::kubernetes::node
+    include profile::kubernetes::node
     # Setup calico
-    include ::profile::calico::kubernetes
+    include profile::calico::kubernetes
     # Setup LVS
-    include ::profile::lvs::realserver
+    include profile::lvs::realserver
 
     system::role { 'kubernetes::worker':
         description => 'Kubernetes worker node',
