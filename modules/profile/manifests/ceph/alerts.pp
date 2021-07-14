7461eedc481265b017c8877bcc5b286ae6f44eb1
David Caro <dcaro@wikimedia.org>
2021-07-14 16:25:47 +0200

    ceph: Update health alert url to runbook
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: Id070ba733295b57499f8123bf11c88e40c0b273d

--- modules/profile/manifests/ceph/alerts.pp
+++ modules/profile/manifests/ceph/alerts.pp
@@ -11,7 +11,7 @@ class profile::ceph::alerts(
         critical        => 2,
         contact_group   => 'wmcs-team',
         dashboard_links => ['https://grafana.wikimedia.org/dashboard/db/cloudvps-ceph-cluster'],
-        notes_link      => 'https://wikitech.wikimedia.org/wiki/Portal:Cloud_VPS/Admin/Alerts#Ceph_Cluster_Health',
+        notes_link      => 'https://wikitech.wikimedia.org/wiki/Portal:Cloud_VPS/Admin/Runbooks/Ceph_Cluster_Health',
     }
 
     # Check the global Ceph mon quorum count
