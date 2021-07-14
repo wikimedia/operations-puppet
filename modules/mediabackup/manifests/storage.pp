519e17fb93c83a9a734e3b13198da0ad25c89082
Jcrespo <jcrespo@wikimedia.org>
2021-07-14 13:04:11 +0200

    mediabackup: Update config dir behaviour, add root passwords
    Config dir parameter is now deprecated. Keep using it for the
    the default TLS cert deployment, but generate the soft links
    for the certs on the right location:
    
    /srv/mediabackups/.minio/certs
    
    Delete the old certs subdir.
    
    Also setup a secure user and password, this last one, different
    on each datacenter. Setup the regio to be that of the site
    (datacenter) where the backup is happening.
    
    Bug: T276442
    Change-Id: I23c6923a6b0d120427b0db3abacc48dc286ba24d

--- modules/mediabackup/manifests/storage.pp
+++ modules/mediabackup/manifests/storage.pp
@@ -4,11 +4,16 @@
 # * storage_path: the absolute path for the directory that will be
 #                 used for storage of files, as configured by minio
 # * port: the TCP port number where minio will be listening from
-# * config_dir: The location of the main config directory and where
-#               TLS certs will be located
+# * root_user: the string containing the admin user name
+# * root_password: the string containing the admin authentication
+#                  string.
+# * config_dir: (deprecated) The location of the main config directory.
+#               Minio now always stores config in a storage_path subdir.
 class mediabackup::storage (
     Stdlib::Unixpath $storage_path,
     Stdlib::Port $port,
+    String $root_user,
+    String $root_password,
     Stdlib::Unixpath $config_dir = '/etc/minio',
 ) {
     ensure_packages(['minio', ])
@@ -35,7 +40,8 @@ class mediabackup::storage (
         group   => 'minio-user',
         require => [ User['minio-user'], Group['minio-user'] ],
     }
-
+    # Please note that config dir option is deprecated:
+    # https://docs.min.io/docs/minio-server-configuration-guide.html
     File { $config_dir:
         ensure  => directory,
         mode    => '0440',
@@ -52,22 +58,42 @@ class mediabackup::storage (
         group           => 'minio-user',
         require         => [ File[$config_dir], User['minio-user'], Group['minio-user'] ],
     }
-    File { "${config_dir}/certs":
+    File { "${storage_path}/.minio":
         ensure  => directory,
-        mode    => '0440',
+        mode    => '0700',
         owner   => 'minio-user',
         group   => 'minio-user',
-        require => File[$config_dir],
+        require => File[$storage_path],
     }
-    # file names are hardcoded, only cert-dir can be configured
+    File { "${storage_path}/.minio/certs":
+        ensure  => directory,
+        mode    => '0600',
+        owner   => 'minio-user',
+        group   => 'minio-user',
+        require => File["${storage_path}/.minio"],
+    }
+
+    # TODO: Remove the next 3 resources, as they will be linked elsewere
     file { "${config_dir}/certs/private.key":
+        ensure  => absent,
+    }
+    file { "${config_dir}/certs/public.crt":
+        ensure  => absent,
+    }
+    File { "${config_dir}/certs":
+        ensure  => absent,
+        require => [ File["${config_dir}/certs/private.key"], File["${config_dir}/certs/public.crt"], ],
+    }
+
+    # file names are hardcoded, so using symlinks to expose them
+    file { "${storage_path}/.minio/certs/private.key":
         ensure  => 'link',
         target  => '${config_dir}/ssl/server.key',
         owner   => 'minio-user',
         group   => 'minio-user',
         require => Base::Expose_puppet_certs[$config_dir],
     }
-    file { "${config_dir}/certs/public.crt":
+    file { "${storage_path}/.minio/certs/public.crt":
         ensure  => 'link',
         target  => '${config_dir}/ssl/cert.pem',
         owner   => 'minio-user',
--- modules/profile/manifests/mediabackup/storage.pp
+++ modules/profile/manifests/mediabackup/storage.pp
@@ -4,8 +4,10 @@ class profile::mediabackup::storage (
     Hash $mediabackup_config = lookup('mediabackup', Hash, 'hash'),
 ){
     class { 'mediabackup::storage':
-        storage_path => $mediabackup_config['storage_path'],
-        port         => $mediabackup_config['storage_port'],
+        storage_path  => $mediabackup_config['storage_path'],
+        port          => $mediabackup_config['storage_port'],
+        root_user     => $mediabackup_config['storage_root_user'],
+        root_password => $mediabackup_config['storage_root_password'],
     }
 
     # Do not open the firewall to everyone if there are no available storage hosts
