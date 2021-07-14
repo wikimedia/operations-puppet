d6c7e4c4b2f8cbd5dc9d10aa980df60dde5556f9
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2021-07-14 21:30:09 +0200

    pontoon: add sd module
    This is the default implementation for Service Discovery in Pontoon. The
    main goal is to resemble Production as much as possible from the client
    and server perspective. In other words, we shouldn't need to change
    names/configurations between Pontoon and Production.
    
    Change-Id: Ie131c64b53895fa65062e50d659674e4b38c15e2

---
+++ modules/pontoon/manifests/sd.pp
@@ -0,0 +1,50 @@
+# Service Discovery for Pontoon
+
+# The module takes care of mapping service names to a single load balancer address.
+
+# The implementation uses a local dnsmasq instance configured with:
+# - upstream resolvers, $nameservers
+# - an hosts file, with service records generated from $services_config and
+#   pointing to $lb_address
+
+# dnsmasq will listen on 127.0.0.53 for DNS queries only, service records are synthetized
+# as if services are deployed in all domains. Queries for unknown records will be sent to the
+# upstream resolvers instead.
+
+class pontoon::sd (
+    Stdlib::IP::Address $lb_address,
+    Array[Stdlib::IP::Address] $nameservers,
+    Hash[String, Wmflib::Service] $services_config,
+) {
+    require_package('dnsmasq')
+
+    $services = pontoon::service_names($services_config)
+
+    file { '/etc/pontoon-sd':
+        ensure => directory,
+    }
+
+    file { '/etc/pontoon-sd/resolv':
+        content => template('pontoon/dnsmasq.resolv'),
+        notify  => Exec['dnsmasq-reload'],
+    }
+
+    file { '/etc/pontoon-sd/hosts':
+        content => template('pontoon/dnsmasq.hosts'),
+        notify  => Exec['dnsmasq-reload'],
+    }
+
+    file { '/etc/dnsmasq.d/pontoon-sd.conf':
+        content => template('pontoon/dnsmasq.conf'),
+        notify  => Service['dnsmasq'],
+    }
+
+    service { 'dnsmasq':
+        ensure => running,
+    }
+
+    exec { 'dnsmasq-reload':
+        command     => '/bin/systemctl reload dnsmasq',
+        refreshonly => true,
+    }
+}
---
+++ modules/profile/manifests/pontoon/sd.pp
@@ -0,0 +1,33 @@
+# Pontoon Service Discovery
+#
+# The default implementation of sd: all routable services will be pointed to the first host
+# running the pontoon::lb role. Other DNS queries will be sent to DNS resolvers listed in
+# sd_nameservers.
+class profile::pontoon::sd (
+    Array[Stdlib::IP::Address] $sd_nameservers = lookup('profile::pontoon::sd_nameservers'),
+    Array[Stdlib::IP::Address] $local_nameservers = lookup('profile::base::nameservers'),
+    Array[Stdlib::Fqdn] $lbs = pontoon::hosts_for_role('pontoon::lb'), # lint:ignore:wmf_styleguide
+) {
+    unless length($local_nameservers) == 1 and $local_nameservers[0] == '127.0.0.53' {
+        fail("Local nameservers ${local_nameservers} misconfigured")
+    }
+
+    if empty($lbs) {
+        fail('No LBs configured')
+    }
+
+    if empty($sd_nameservers) {
+        fail('No upstream nameservers configured')
+    }
+
+    # Announce all services with 'role' keyword. The LB will take care of routing to backend hosts.
+    $role_services = wmflib::service::fetch().filter |$name, $config| {
+        ('role' in $config)
+    }
+
+    class { 'pontoon::sd':
+        lb_address      => ipresolve($lbs[0], 4),
+        nameservers     => $sd_nameservers,
+        services_config => $role_services,
+    }
+}
