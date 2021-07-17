097a3c1283104fcee50e538db9e7e1c8642110b0
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2021-07-17 17:18:31 +0200

    pontoon: add service_names
    This function is used to map a service's name (its key in
    service::catalog) with a list of its 'names'. In this context a name for
    a service is any FQDN the service can be reached at on the network. It
    forms the basis for Service Discovery.
    
    Change-Id: If14890c69b9a4bbdafc1dfa78f79c42ed9886b59

---
+++ modules/pontoon/functions/service_names.pp
@@ -0,0 +1,32 @@
+# @summary Map each service to its 'names'. Names include the service name itself and any
+#          configured discovery names.
+# @param services_config The Wmflib::Service configuration to inspect
+# @param domains The domains to map services to
+# @param tld     The network's TLD
+function pontoon::service_names(
+    Hash[String, Wmflib::Service] $services_config,
+    Array[String] $domains = ['discovery', 'svc.eqiad', 'svc.codfw'],
+    String $tld = 'wmnet',
+) >> Hash[String, Array[String]] {
+    $t = $services_config.map |$service_name, $config| {
+        $disc_names = ('discovery' in $config) ? {
+          true  => $config['discovery'].map |$el| { $el['dnsdisc'] },
+          false => [],
+        }
+        $all_names = unique($disc_names + $service_name)
+
+        $svc_names = $domains.map |$d| {
+            $all_names.map |$n| {
+                [ "${n}.${d}.${tld}" ]
+            }
+        }
+
+        [
+            $service_name,
+            $svc_names.flatten().sort(),
+        ]
+    }
+
+    Hash($t)
+}
+
