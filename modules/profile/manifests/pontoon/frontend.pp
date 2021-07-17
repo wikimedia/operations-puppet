de2dfcfddcaa70e5f958d87f8074c774561520e0
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2021-07-17 19:08:09 +0200

    pontoon: fix lookup of public_domain in frontend
    Change-Id: I07f2e6124d1ae4c0e724a3da732320459c9de763

--- modules/profile/manifests/pontoon/frontend.pp
+++ modules/profile/manifests/pontoon/frontend.pp
@@ -1,11 +1,13 @@
-class profile::pontoon::frontend {
+class profile::pontoon::frontend (
+  $public_domain = lookup('public_domain'),
+) {
     $public_services = wmflib::service::fetch().filter |$name, $config| {
         ('public_endpoint' in $config and 'role' in $config)
     }
 
     class { 'pontoon::public_lb':
         services_config => $public_services,
-        public_domain   => $::public_domain,
+        public_domain   => $public_domain,
     }
 
     class { '::httpd':
@@ -14,6 +16,6 @@ class profile::pontoon::frontend {
 
     class { 'pontoon::public_certs':
         services_config => $public_services,
-        public_domain   => $::public_domain,
+        public_domain   => $public_domain,
     }
 }
