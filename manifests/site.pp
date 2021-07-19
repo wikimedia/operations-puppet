3124329393aa1bdafe9f72f2da57e2ee4138d64c
Ariel T. Glenn <ariel@wikimedia.org>
2021-07-19 17:21:54 +0300

    swap roles of dumpsdata1001, 1003 in prep for T286065
    Change-Id: I71cb514a7b46eb6148dcc77d8dd2a76952fd93f9

--- manifests/site.pp
+++ manifests/site.pp
@@ -954,7 +954,7 @@ node /^druid100[4-8]\.eqiad\.wmnet$/ {
 # nfs server for xml dumps generation, also rsyncs xml dumps
 # data to fallback nfs server(s)
 node /^dumpsdata1003\.eqiad\.wmnet$/ {
-    role(dumps::generation::server::xmldumps)
+    role(dumps::generation::server::xmlfallback)
 }
 
 # nfs server for misc dumps generation, also rsyncs misc dumps
@@ -965,7 +965,7 @@ node /^dumpsdata1002\.eqiad\.wmnet$/ {
 # fallback nfs server for dumps generation, also
 # will rsync data to web servers
 node /^dumpsdata1001\.eqiad\.wmnet$/ {
-    role(dumps::generation::server::xmlfallback)
+    role(dumps::generation::server::xmldumps)
 }
 
 node /^elastic103[2-9]\.eqiad\.wmnet/ {
