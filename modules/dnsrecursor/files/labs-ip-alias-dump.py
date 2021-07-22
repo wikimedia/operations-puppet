042049e9546270138953dc17c0cd67e8fb1422b9
Brooke Storm <bstorm@wikimedia.org>
2021-07-22 15:06:04 -0700

    cloud dns: alias all attached floating_ips, not just server ones
    This captures IPs that are used as vips for keepalived as well as ones
    associated just with servers. It also seems to run faster and actually
    correctly checks for equality of the json instead of randomly ordering
    and working most of the time.
    
    Also moving to the openstacksdk.
    
    Bug: T287107
    Bug: T287201
    Change-Id: I96a9f3e2a782283730e2c7dc6e30234b9169ca96

--- modules/dnsrecursor/files/labs-ip-alias-dump.py
+++ modules/dnsrecursor/files/labs-ip-alias-dump.py
@@ -4,114 +4,89 @@ import os
 import sys
 import yaml
 import argparse
-import itertools
 
 from keystoneauth1.identity.v3 import Password as KeystonePassword
 from keystoneauth1 import session as keystone_session
 
-from keystoneclient.v3 import client as keystone_client
-
-from novaclient import client as novaclient
+from openstack import connection
 
 
 def new_session(project):
     auth = KeystonePassword(
-        auth_url=config['nova_api_url'],
-        username=config['username'],
-        password=config['password'],
-        user_domain_name='Default',
-        project_domain_name='Default',
-        project_name=project)
+        auth_url=config["nova_api_url"],
+        username=config["username"],
+        password=config["password"],
+        user_domain_name="Default",
+        project_domain_name="Default",
+        project_name=project,
+    )
 
     return keystone_session.Session(auth=auth)
 
 
 argparser = argparse.ArgumentParser(
-    description='Generate a lua script for public->private IP mappings')
-argparser.add_argument(
-    '--config-file',
-    help='Path to config file',
-    default='/etc/labs-dns-alias.yaml',
-    type=argparse.FileType('r')
+    description="Generate a lua script for public->private IP mappings"
 )
 argparser.add_argument(
-    '--check-changes-only',
-    help='Exit 0 if there are no changes and 1 otherwise. Do not write to file',
-    action='store_true'
+    "--config-file",
+    help="Path to config file",
+    default="/etc/labs-dns-alias.yaml",
+    type=argparse.FileType("r"),
+)
+argparser.add_argument(
+    "--check-changes-only",
+    help="Exit 0 if there are no changes and 1 otherwise. Do not write to file",
+    action="store_true",
 )
 args = argparser.parse_args()
 config = yaml.safe_load(args.config_file)
-session = new_session(config['observer_project_name'])
-keystoneClient = keystone_client.Client(
-    session=session, interface='public', connect_retries=5)
+session = new_session(config["observer_project_name"])
+conn = connection.Connection(session=session)
 
-region_recs = keystoneClient.regions.list()
+
+region_recs = conn.identity.regions()
 regions = [region.id for region in region_recs]
 
 projects = []
-for tenant in keystoneClient.projects.list():
+for tenant in conn.identity.projects():
     projects.append(tenant.name)
 
-aliases = {}
+# The output template
+output_d = {"aliasmapping": {}, "extra_records": {}}
+
 for project in projects:
     # There's nothing useful in 'admin,' and
     #  the novaobserver isn't a member.
-    if project == 'admin':
+    if project == "admin":
         continue
 
     project_session = new_session(project)
     for region in regions:
-        client = novaclient.Client('2', session=project_session,
-                                   connect_retries=5, region_name=region)
+        r_conn = connection.Connection(
+            "2", session=project_session, connect_retries=5, region_name=region
+        )
 
-        for server in client.servers.list():
-            for network_name, addresses in list(server.addresses.items()):
-                try:
-                    private = [
-                        str(ip['addr']) for ip in addresses
-                        if ip['OS-EXT-IPS:type'] == 'fixed'
-                    ]
-                    public = [
-                        str(ip['addr']) for ip in addresses
-                        if ip['OS-EXT-IPS:type'] == 'floating'
-                    ]
-                    if public:
-                        # Match all possible public IPs to all possible private ones
-                        # Technically there can be more than one floating IP and more
-                        # than one private IP Although this is never practically the
-                        # case...
-                        aliases[server.name] = list(itertools.product(public, private))
-                except KeyError:
-                    # This can happen if a server doesn't (yet) have any addresses,
-                    # while it's being constructed.  In which case we simply
-                    # harmlessly ignore it.
-                    pass
+        for floating_ip in r_conn.list_floating_ips():
+            if floating_ip.attached:
+                output_d["aliasmapping"][
+                    floating_ip.floating_ip_address
+                ] = floating_ip.fixed_ip_address
 
-output_d = {'aliasmapping': {}, 'extra_records': {}}
-# Sort to prevent flapping around due to random ordering
-for name in sorted(aliases.keys()):
-    ips = aliases[name]
-    for public, private in ips:
-        output_d['aliasmapping'][public] = private
 
-if 'extra_records' in config:
-    extra_records = config['extra_records']
+if "extra_records" in config:
+    output_d["extra_records"] = config["extra_records"]
 
-    for q in sorted(extra_records.keys()):
-        output_d['extra_records'][q] = extra_records[q]
-
-output = json.dumps(output_d)
-if os.path.exists(config['output_path']):
-    with open(config['output_path']) as f:
-        current_contents = f.read()
+if os.path.exists(config["output_path"]):
+    with open(config["output_path"]) as f:
+        current_contents = json.load(f)
 else:
-    current_contents = ""
+    current_contents = {}
 
 exit_status = 0
-if output != current_contents:
+if output_d != current_contents:
     if not args.check_changes_only:
-        with open(config['output_path'], 'w') as f:
-            f.write(output)
+        with open(config["output_path"], "w") as f:
+            f.write(json.dumps(output_d))
     exit_status = 1
 if args.check_changes_only:
     sys.exit(exit_status)
