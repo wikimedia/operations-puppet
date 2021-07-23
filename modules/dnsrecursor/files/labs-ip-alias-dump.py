db53a238bc6323c3cbf42a2a6853380e075689cc
Brooke Storm <bstorm@wikimedia.org>
2021-07-23 11:00:23 -0700

    cloud dns: tidy up the labs-ip-alias-dump script
    This is mostly stylistic changes. I haven't added a retry yet because
    this script doesn't seem to fail often (from a scan of the logs), and
    we are using different libraries now that would be interesting to
    compare the performance of. Previously, we used novaclient to get info
    from neutron, which regularly generates token errors for some reason.
    
    One quirk is that changes (corrects?) the behavior when --check-changes-only
    is not passed. There is no reason to return exit-status of 1 when this
    succeeds in updating the DNS config! In fact, we may have been paged for that.
    
    openstacksdk switches to neutron in the backend directly, so this is
    more direct. This may resolve some of the problems in the original ticket
    while retaining some of the better practices from I6291ea0b1e62560b7dda80ed9e2
    
    Bug: T285537
    Change-Id: I6a3c7b77459bacac7190cc1dfb5f68cbd6134988

--- modules/dnsrecursor/files/labs-ip-alias-dump.py
+++ modules/dnsrecursor/files/labs-ip-alias-dump.py
@@ -11,7 +11,7 @@ from keystoneauth1 import session as keystone_session
 from openstack import connection
 
 
-def new_session(project):
+def new_session(project, config):
     auth = KeystonePassword(
         auth_url=config["nova_api_url"],
         username=config["username"],
@@ -21,72 +21,77 @@ def new_session(project):
         project_name=project,
     )
 
-    return keystone_session.Session(auth=auth)
+    return keystone_session.Session(auth=auth, connect_retries=3)
 
 
-argparser = argparse.ArgumentParser(
-    description="Generate a lua script for public->private IP mappings"
-)
-argparser.add_argument(
-    "--config-file",
-    help="Path to config file",
-    default="/etc/labs-dns-alias.yaml",
-    type=argparse.FileType("r"),
-)
-argparser.add_argument(
-    "--check-changes-only",
-    help="Exit 0 if there are no changes and 1 otherwise. Do not write to file",
-    action="store_true",
-)
-args = argparser.parse_args()
-config = yaml.safe_load(args.config_file)
-session = new_session(config["observer_project_name"])
-conn = connection.Connection(session=session)
+def main():
+    argparser = argparse.ArgumentParser(
+        description="Generate a lua script for public->private IP mappings"
+    )
+    argparser.add_argument(
+        "--config-file",
+        help="Path to config file",
+        default="/etc/labs-dns-alias.yaml",
+        type=argparse.FileType("r"),
+    )
+    argparser.add_argument(
+        "--check-changes-only",
+        help="Exit 0 if there are no changes and 1 otherwise. Do not write to file",
+        action="store_true",
+    )
+    args = argparser.parse_args()
+    config = yaml.safe_load(args.config_file)
+    session = new_session(config["observer_project_name"], config)
+    conn = connection.Connection(session=session)
+
+    region_recs = conn.identity.regions()
+    regions = [region.id for region in region_recs]
+
+    projects = []
+    for tenant in conn.identity.projects():
+        projects.append(tenant.name)
+
+    # The output template
+    output_d = {"aliasmapping": {}, "extra_records": {}}
+
+    for project in projects:
+        # There's nothing useful in 'admin,' and
+        #  the novaobserver isn't a member.
+        if project == "admin":
+            continue
+
+        project_session = new_session(project, config)
+        for region in regions:
+            r_conn = connection.Connection(
+                "2", session=project_session, connect_retries=5, region_name=region
+            )
+
+            for floating_ip in r_conn.list_floating_ips():
+                if floating_ip.attached:
+                    output_d["aliasmapping"][
+                        floating_ip.floating_ip_address
+                    ] = floating_ip.fixed_ip_address
+
+    if "extra_records" in config:
+        output_d["extra_records"] = config["extra_records"]
+
+    if os.path.exists(config["output_path"]):
+        with open(config["output_path"]) as f:
+            current_contents = json.load(f)
+    else:
+        current_contents = {}
+
+    exit_status = 0
+    if output_d != current_contents:
+        if not args.check_changes_only:
+            with open(config["output_path"], "w") as f:
+                f.write(json.dumps(output_d))
+        exit_status = 1
+    if args.check_changes_only:
+        return exit_status
+
+    return 0
 
 
-region_recs = conn.identity.regions()
-regions = [region.id for region in region_recs]
-
-projects = []
-for tenant in conn.identity.projects():
-    projects.append(tenant.name)
-
-# The output template
-output_d = {"aliasmapping": {}, "extra_records": {}}
-
-for project in projects:
-    # There's nothing useful in 'admin,' and
-    #  the novaobserver isn't a member.
-    if project == "admin":
-        continue
-
-    project_session = new_session(project)
-    for region in regions:
-        r_conn = connection.Connection(
-            "2", session=project_session, connect_retries=5, region_name=region
-        )
-
-        for floating_ip in r_conn.list_floating_ips():
-            if floating_ip.attached:
-                output_d["aliasmapping"][
-                    floating_ip.floating_ip_address
-                ] = floating_ip.fixed_ip_address
-
-
-if "extra_records" in config:
-    output_d["extra_records"] = config["extra_records"]
-
-if os.path.exists(config["output_path"]):
-    with open(config["output_path"]) as f:
-        current_contents = json.load(f)
-else:
-    current_contents = {}
-
-exit_status = 0
-if output_d != current_contents:
-    if not args.check_changes_only:
-        with open(config["output_path"], "w") as f:
-            f.write(json.dumps(output_d))
-    exit_status = 1
-if args.check_changes_only:
-    sys.exit(exit_status)
+if __name__ == "__main__":
+    sys.exit(main())
