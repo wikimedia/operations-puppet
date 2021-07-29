bac1c13f0f3b271263d7008007d79138c49fa871
Ariel T. Glenn <ariel@wikimedia.org>
2021-07-29 11:40:04 +0300

    Swap dumpsdata rols back now that network maintenance is complete
    Bug: T286065
    Change-Id: Ia7cff63ec5d7c4ecb0e113dbc0e5dbf2374a5c50

--- manifests/site.pp
+++ manifests/site.pp
@@ -959,7 +959,7 @@ node /^druid100[4-8]\.eqiad\.wmnet$/ {
 # nfs server for xml dumps generation, also rsyncs xml dumps
 # data to fallback nfs server(s)
 node /^dumpsdata1003\.eqiad\.wmnet$/ {
-    role(dumps::generation::server::xmlfallback)
+    role(dumps::generation::server::xmldumps)
 }
 
 # nfs server for misc dumps generation, also rsyncs misc dumps
@@ -970,7 +970,7 @@ node /^dumpsdata1002\.eqiad\.wmnet$/ {
 # fallback nfs server for dumps generation, also
 # will rsync data to web servers
 node /^dumpsdata1001\.eqiad\.wmnet$/ {
-    role(dumps::generation::server::xmldumps)
+    role(dumps::generation::server::xmlfallback)
 }
 
 node /^elastic103[2-9]\.eqiad\.wmnet/ {
