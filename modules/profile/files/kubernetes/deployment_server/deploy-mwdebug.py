3d8fa7269cd0dbd6cc6d25d33f5ca671f2fdeb2e
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2021-07-29 15:42:22 +0200

    profile::kubernetes::deployment_server: add automation for mw on k8s
    Currently, updating the image version for mediawiki on kubernetes
    follows the standard gitops procedure we follow for every other service,
    so we need to post and merge a patch to deployment-charts. This is
    inconvenient given we already have a deployment procedure for mediawiki
    outside of kubernetes.
    
    As a very first solution, a simple script is introduced that does the
    following:
    * Check the latest released tag for the mediawiki-multiversion image
    * update a special values file if a newer image is found
    * if the file is updated, perform helmfile deploys to eqiad and codfw
    
    This is of course an oversimplified model of release, and is not
    expected to persist for long. It is, however, our way out of not being
    able to allow browsing freely mwdebug on kubernetes for external clients.
    
    Bug: T287570
    Change-Id: Ie877156fbd62c9cf21f0a663b52f640fd9fd7fef

---
+++ modules/profile/files/kubernetes/deployment_server/deploy-mwdebug.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+"""
+Temporary script to keep the mwdebug deployment up to date with
+the scap-released version. Given its temporary nature, the script
+will hardcode most values.
+
+Copyright (c) 2021 Giuseppe Lavagetto <joe@wikimedia.org>
+"""
+import fcntl
+import logging
+import pathlib
+import re
+import subprocess
+import sys
+
+import yaml
+from docker_report.registry import operations
+
+logger = logging.getLogger()
+
+IMAGE = "restricted/mediawiki-multiversion"
+WEB_IMAGE = "restricted/mediawiki-webserver"
+REGISTRY = "docker-registry.discovery.wmnet"
+VALUES_FILE = pathlib.Path("/etc/helmfile-defaults/mediawiki/releases.yaml")
+DEPLOY_DIR = "/srv/deployment-charts/helmfile.d/services/mwdebug"
+
+
+def find_last_tag(registry: operations.RegistryOperations) -> str:
+    """Finds the last standard tag present on the registry"""
+    maxtag = "0"
+    good_tag_regex = re.compile(r"\d{4}-\d{2}-\d{2}-d{6}-publish")
+    for tag in registry.get_tags_for_image(IMAGE):
+        m = good_tag_regex.match(tag)
+        # This is a non-standard tag like "latest", skip it
+        if m is None:
+            continue
+        ts = m.group()
+        if ts > maxtag:
+            maxtag = tag
+    if maxtag == "0":
+        raise RuntimeError("No release tags found")
+    logger.info(f"Found the most recent release, {maxtag}")
+    return maxtag
+
+
+def values_file_update(maxtag: str) -> bool:
+    """Updates the values file, if necessary. Returns true in that case."""
+    values = yaml.safe_dump(
+        {
+            "main_app": {"image": f"{IMAGE}/{maxtag}"},
+            "mw": {"httpd": {"image_tag": f"{WEB_IMAGE}/{maxtag}"}},
+        }
+    )
+    orig_values = VALUES_FILE.read_text()
+    if values == orig_values:
+        return False
+    logger.info("Updating the values file")
+    with VALUES_FILE.open("w") as fh:
+        yaml.safe_dump(values, fh)
+    return True
+
+
+def deployment():
+    """Deploy to production"""
+    logger.info("Deploying to eqiad")
+    subprocess.run(["helmfile", "-e", "eqiad", "apply"], cwd=DEPLOY_DIR, check=True)
+    logger.info("Deploying to codfw")
+    subprocess.run(["helmfile", "-e", "codfw", "apply"], cwd=DEPLOY_DIR, check=True)
+
+
+def main():
+    try:
+        fd = open("/tmp/deploy-mwdebug-flock", "w+")
+        fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        logging.basicConfig(level=logging.INFO)
+        ops = operations.RegistryOperations(REGISTRY, logger=logger)
+        maxtag = find_last_tag(ops)
+        if values_file_update(maxtag):
+            deployment()
+        else:
+            logger.info("Nothing to deploy")
+    except BlockingIOError:
+        logger.info("Unable to acquire the lock, not running")
+        sys.exit(1)
+    finally:
+        fcntl.flock(fd, fcntl.LOCK_UN)
+        fd.close()
+
+
+if __name__ == "__main__":
+    main()
--- modules/profile/manifests/kubernetes/deployment_server/mediawiki.pp
+++ modules/profile/manifests/kubernetes/deployment_server/mediawiki.pp
@@ -11,8 +11,8 @@ class profile::kubernetes::deployment_server::mediawiki(
     Stdlib::Unixpath $general_dir                       = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
     Hash  $servers_by_datacenter_category               = lookup('profile::mediawiki::mcrouter_wancache::shards'),
     Hash  $redis_shards                                 = lookup('redis::shards'),
-
-
+    String $docker_password                             = lookup('kubernetes_docker_password'),
+    Stdlib::Fqdn $docker_registry                       = lookup('docker::registry'),
 ){
     file { "${general_dir}/mediawiki":
         ensure => directory,
@@ -37,5 +37,48 @@ class profile::kubernetes::deployment_server::mediawiki(
     class { 'mediawiki::nutcracker::yaml_defs':
         path         => "${general_dir}/mediawiki/nutcracker_pools.yaml",
         redis_shards => $redis_shards,
+    }
+
+    # Install docker-report in order to be able to list tags remotely
+    package { 'python3-docker-report':
+        ensure => present,
+    }
+
+    # Now install the credentials for the kubernetes docker user so that we can list tags
+    # in the restricted namespace. This is ok since the credentials will be guarded by
+    # being ran as root, and that is more restrictive than access to the stuff contained
+    # in the restricted images.
+    # Registry credentials require push privileges
+    # uses strict_encode64 since encode64 adds newlines?!
+    $docker_auth = inline_template("<%= require 'base64'; Base64.strict_encode64('kubernetes:${docker_password}') -%>")
+
+    $docker_config = {
+        'auths' => {
+            "${docker_registry}" => {
+                'auth' => $docker_auth,
+            },
+        },
+    }
+
+    file { '/root/.docker':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0550',
+    }
+
+    file { '/root/.docker/config.json':
+        content => ordered_json($docker_config),
+        owner   => 'root',
+        group   => 'docker',
+        mode    => '0440',
+    }
+
+    # Add a script that updates the mediawiki images.
+    file { '/usr/local/sbin/deploy-mwdebug':
+        source => 'puppet:///modules/profile/kubernetes/deployment_server/deploy-mwdebug.py',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0440',
     }
 }
