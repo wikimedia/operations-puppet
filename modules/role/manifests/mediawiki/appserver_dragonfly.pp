962573ed5d65f20dd8f0cb443eff4cbeea064be5
Janis Meybohm <jmeybohm@wikimedia.org>
2021-08-06 09:36:37 +0200

    appserver_dragonfly: Remove experimental stuff from appservers
    This will potentially need some more cleanup that I will follow up on
    manually.
    
    Bug: T286054
    Change-Id: I7bd070c81230bba8fbb5fc76a17a607f5458ad31

--- modules/role/manifests/mediawiki/appserver_dragonfly.pp
+++ modules/role/manifests/mediawiki/appserver_dragonfly.pp
@@ -8,13 +8,9 @@ class role::mediawiki::appserver_dragonfly {
     include role::mediawiki::appserver
 
     # Install docker and set up credentials
-    ensure_packages(['docker.io'], {'ensure' => present})
-    docker::credentials { '/root/.docker/config.json':
-        owner             => 'root',
-        group             => 'root',
-        registry          => 'docker-registry.discovery.wmnet',
-        registry_username => 'kubernetes',
-        registry_password => lookup('kubernetes_docker_password', {default_value => undef}),
+    ensure_packages(['docker.io'], {'ensure' => 'purged'})
+    file { '/root/.docker/config.json':
+        ensure => absent,
     }
 
     # Setup dfdaemon and configure docker to use it
