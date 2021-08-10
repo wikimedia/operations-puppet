8a39b7523a083ddc7f3ebdfa75f19a9c5234e31c
Jcrespo <jcrespo@wikimedia.org>
2021-08-10 17:31:50 +0200

    mediabackup: Puppetize the media backup workers
    Setup the configuration and paths needed to run the backups on each
    backup "worker", the hosts where the backup logic runs and downloads
    the necesary metadata and files and then send them to the final
    storage location on the media backup storage hosts.
    
    This requires access to multitude of hiera configuration endpoints,
    some common, some per-datacenter, as well as access secrets for
    the storage and the databases.
    
    For now, we will hardcode just a backup of a single wiki. In the
    future we will move some of the configuration to the database, due
    to it being quite dynamic (e.g. list of wikis to backup)- keeping
    it in sync automatically with production configuration.
    
    Bug: T276442
    Change-Id: I17695bbd272b27144534faf5606f9c8648096ab9

--- modules/mediabackup/manifests/worker.pp
+++ modules/mediabackup/manifests/worker.pp
@@ -1,8 +1,16 @@
 # Media backups worker: Install required packages and configures
 # them.
-# * db_host: fqdn of the database used for the media backups metadata backend
+# * mw_db_host: fqdn pf the mediawiki core database used to read the image metadata
+# * mw_db_port: port where the previous db is publicly listening to
+# * mw_db_user: user used to authenticate to the mediawiki database (neads only
+#               SELECT grants on the image, oldimage and filearchive tables)
+# * mw_db_password: password used to authenticate to the mediawiki database
+# * wiki: name of the mediawiki database that will be read
+# * batchsize: maximum number of rows (files) of metadata to be read into memory and
+#              processed for both mw metadata and backups metadata
+# * db_host: fqdn of the database used to write the media backups metadata backend
 # * db_port: port of such database
-# * db_user: user used to authenticate to the database
+# * db_user: user used to authenticate to the database (needs read/write grants)
 # * db_password: password used to authenticate to the database
 # * db_schema: name of the database inside the server where the data is read
 #              from and written to
@@ -11,14 +19,90 @@
 # * storage_port: Port where all storage nodes will be listening to (it may
 #                 change in the future to a per-host configuration (socket)
 #                 to allow for multiplexing with multiple services per host
+# * access_key: identifier to authenticate on the s3-compatible api to store
+#               the backup files
+# * secret_key: password to authenticate on the s3-compatible api
 class mediabackup::worker (
+    Stdlib::Fqdn        $mw_db_host,
+    Stdlib::Port        $mw_db_port,
+    String              $mw_db_user,
+    String              $mw_db_password,
+    String              $wiki,
+    Integer[1]          $batchsize,
     Stdlib::Fqdn        $db_host,
     Stdlib::Port        $db_port,
     String              $db_user,
     String              $db_password,
     Array[Stdlib::Fqdn] $storage_hosts,
     Stdlib::Port        $storage_port,
+    String              $access_key,
+    String              $secret_key,
     String              $db_schema = 'mediabackups',
 ) {
     ensure_packages(['python3', ])  # placeholder until we have a package
+
+    # user and group so we don't run anything as a privileged user
+    group { 'mediabackup':
+        ensure => present,
+        system => true,
+    }
+    user { 'mediabackup':
+        ensure     => present,
+        gid        => 'mediabackup',
+        shell      => '/bin/false',
+        home       => '/srv/mediabackup',
+        system     => true,
+        managehome => false,
+        require    => Group['mediabackup'],
+    }
+
+    # location of temporary storage to download and hash files before
+    # sending it to its final location
+    File { '/srv/mediabackup':
+        ensure  => directory,
+        mode    => '0750',
+        owner   => 'mediabackup',
+        group   => 'mediabackup',
+        require => [ User['mediabackup'], Group['mediabackup'] ],
+    }
+
+    # backup execution configuration dir (including secrets)
+    File { '/etc/mediabackup':
+        ensure  => directory,
+        mode    => '0400',
+        owner   => 'mediabackup',
+        group   => 'mediabackup',
+        require => [ User['mediabackup'], Group['mediabackup'] ],
+    }
+
+    # access to a backup source db to read mediawiki image metadata
+    File { '/etc/mediabackup/mw_db.conf':
+        ensure  => present,
+        mode    => '0400',
+        owner   => 'mediabackup',
+        group   => 'mediabackup',
+        content => template('mediabackup/mw_db.conf.erb'),
+        require => File['/etc/mediabackup'],
+    }
+
+    # access to a rw db to write and coordinate backup metadata
+    File { '/etc/mediabackup/mediabackups_db.conf':
+        ensure  => present,
+        mode    => '0400',
+        owner   => 'mediabackup',
+        group   => 'mediabackup',
+        content => template('mediabackup/mediabackups_db.conf.erb'),
+        require => File['/etc/mediabackup'],
+    }
+
+    # access to final storage (S3-compatible cluster on the same dc)
+    $tmpdir = '/srv/mediabackup'
+    File { '/etc/mediabackup/mediabackups_storage.conf':
+        ensure  => present,
+        mode    => '0400',
+        owner   => 'mediabackup',
+        group   => 'mediabackup',
+        content => template('mediabackup/mediabackups_storage.conf.erb'),
+        require => [ File['/etc/mediabackup'], File['/srv/mediabackup'], ]
+    }
 }
--- modules/profile/manifests/mediabackup/worker.pp
+++ modules/profile/manifests/mediabackup/worker.pp
@@ -4,13 +4,25 @@
 class profile::mediabackup::worker (
     Hash $mediabackup_config = lookup('mediabackup', Hash, 'hash'),
 ){
+    # Setup the media backups worker in production.
+    # Some of the static configuration used here should probably
+    # be moved later to the db to allow for more dynamic
+    # configuration.
     class { 'mediabackup::worker':
-        db_host       => $mediabackup_config['db_host'],
-        db_port       => $mediabackup_config['db_port'],
-        db_user       => $mediabackup_config['db_user'],
-        db_password   => $mediabackup_config['db_password'],
-        db_schema     => $mediabackup_config['db_schema'],
-        storage_hosts => $mediabackup_config['storage_hosts'],
-        storage_port  => $mediabackup_config['storage_port'],
+        mw_db_host     => $mediabackup_config['mw_db_host'],
+        mw_db_port     => $mediabackup_config['mw_db_port'],
+        mw_db_user     => $mediabackup_config['mw_db_user'],
+        mw_db_password => $mediabackup_config['mw_db_password'],
+        wiki           => $mediabackup_config['wiki'],
+        batchsize      => $mediabackup_config['batchsize'],
+        db_host        => $mediabackup_config['db_host'],
+        db_port        => $mediabackup_config['db_port'],
+        db_user        => $mediabackup_config['db_user'],
+        db_password    => $mediabackup_config['db_password'],
+        db_schema      => $mediabackup_config['db_schema'],
+        storage_hosts  => $mediabackup_config['storage_hosts'],
+        storage_port   => $mediabackup_config['storage_port'],
+        access_key     => $mediabackup_config['access_key'],
+        secret_key     => $mediabackup_config['secret_key'],
     }
 }
