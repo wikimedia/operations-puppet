50088f91c30fc5091b86baf2491ff6c1bab4f5e1
Valentin Gutierrez <vgutierrez@wikimedia.org>
2021-08-16 13:14:33 +0200

    envoyproxy: Support TLS min/max version config
    Allow configuring minimum and maximum TLS version
    allowed on downstream connections
    
    Bug: T271421
    Change-Id: I202148b1a3fdf73145ff23a84ff5a5699afb991b

--- modules/envoyproxy/types/tlsparams.pp
+++ modules/envoyproxy/types/tlsparams.pp
@@ -1,4 +1,6 @@
 type Envoyproxy::Tlsparams = Struct[{
-    'cipher_suites' => Optional[Array[String]],
-    'ecdh_curves'   => Optional[Array[String]],
+    'cipher_suites'   => Optional[Array[String]],
+    'ecdh_curves'     => Optional[Array[String]],
+    'tls_min_version' => Optional[Enum['TLSv1_0', 'TLSv1_1', 'TLSv1_2', 'TLSv1_3']],
+    'tls_max_version' => Optional[Enum['TLSv1_0', 'TLSv1_1', 'TLSv1_2', 'TLSv1_3']],
 }]
