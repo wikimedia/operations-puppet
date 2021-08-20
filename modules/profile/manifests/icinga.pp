24ccd2a9970b153fd4f247998d041cfe374ad0d8
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2021-08-20 17:51:28 +0200

    icinga: remove reading-web Grafana checks
    The alerts have been moved over to AlertManager:
    https://alerts.wikimedia.org
    
    Bug: T281359
    Change-Id: I044671817846efc74d04e3c04be853c14a11a31d

--- modules/profile/manifests/icinga.pp
+++ modules/profile/manifests/icinga.pp
@@ -84,7 +84,6 @@ class profile::icinga(
     class { 'icinga::monitor::cloudelastic': }
 
     class { 'icinga::monitor::wdqs': }
-    class { 'icinga::monitor::reading_web': }
     class { 'icinga::monitor::gerrit': }
     class { 'icinga::monitor::librenms': }
     class { 'icinga::monitor::debmonitor': }
