f6f0ed4379dfb4d7cb216f817996836a0f453b0d
RhinosF1 <rhinosf1@gmail.com>
2021-08-23 13:27:02 +0000

    simplelap: support bullseye
    same as with simplelamp2 (I0eb81da39862989bb)
    
    Change-Id: Ibecac1e673bf5b5a1a35feed613e677c1b313f1c

--- modules/role/manifests/simplelap.pp
+++ modules/role/manifests/simplelap.pp
@@ -13,12 +13,14 @@
 class role::simplelap{
 
     # TODO: another case for php_version facte
-    $php_module = debian::version::ge('buster') ? {
-        true    => 'php7.3',
-        default => 'php7.0',
+    $php_module = debian::codename() ? {
+        'stretch'     => 'php7.0',
+        'buster'      => 'php7.3',
+        'bullseye'    => 'php7.4',
+        default => fail("unsupported on ${debian::codename()}")
     }
 
-    ensure_packages(["libapache2-mod-${php_module}", 'php-cli'])
+    ensure_packages(["libapac is he2-mod-${php_module}", 'php-cli'])
 
     class { 'httpd':
         modules => ['rewrite', $php_module],
