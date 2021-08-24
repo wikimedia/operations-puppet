0614e3a1ed368baba34d99285618726000793ebe
John Bond <jbond@wikimedia.org>
2021-08-24 15:26:06 +0200

    utils/run_ci_locally: add ability to run the ci image interactively
    Change-Id: Idb70afc046eae8257527d8f1ab01c738ac2b95bb

--- utils/run_ci_locally.sh
+++ utils/run_ci_locally.sh
@@ -8,13 +8,17 @@ usage() {
 $0 - run tests on your puppet working directory.
 
 USAGE:
-[IMG_VERSION=X.Y.Z] $0 [-h|RAKE_ARGS]
+[INTERACTIVE=yes] [IMG_VERSION=X.Y.Z] $0 [-h|RAKE_ARGS]
 
     -h Prints this help message
     RAKE_ARGS are (optional) arguments that get passed directly to "rake" in the container.
 
 You can override the image version to use with the environment variable
 IMG_VERSION.
+
+yuo can have the image spin up and drop yu to a bash terminal by setting
+INTERACTIVE=yes.
+
 
 EXAMPLES:
 # Run all tests CI would run
@@ -43,6 +47,7 @@ if ! (docker info > /dev/null); then
     exit 1
 fi
 
+INTERACTIVE=${INTERACTIVE:-"no"}
 IMG_VERSION=${IMG_VERSION:-"latest"}
 IMG_NAME=docker-registry.wikimedia.org/releng/operations-puppet:$IMG_VERSION
 CONT_NAME=puppet-tests-${IMG_VERSION}
@@ -55,5 +60,15 @@ then
 fi
 
 pushd "${SCRIPT_DIR}/.."
-docker run --rm --env ZUUL_REF="" --env RAKE_TARGET="$*" --name "$CONT_NAME" --volume /"$(pwd)"://src "$IMG_NAME"
+if [ "${INTERACTIVE}" == "yes" ]
+then
+  echo "starting docker in interactive mode."
+  echo "you will most likely want to run the following steps"
+  echo "bundle update"
+  echo "run your custom rspec debug steps e.g."
+  echo "cd modules/wmflib && bundle exec rake spec"
+  docker run --rm -it --user root --entrypoint bash --workdir /src --name "$CONT_NAME" --volume /"$(pwd)"://src "$IMG_NAME"
+else
+  docker run --rm --env ZUUL_REF="" --env RAKE_TARGET="$*" --name "$CONT_NAME" --volume /"$(pwd)"://src "$IMG_NAME"
+fi
 popd
