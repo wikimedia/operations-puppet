034c01272984df1a53597faecabdf85f3d337a1c
Erik Bernhardson <ebernhardson@wikimedia.org>
2021-08-24 10:11:03 -0700

    query_service: support multiple variants of wdqs microsite
    The wdqs microsite is being generalized as the wikibase query
    service, to support deploying commons query service with the same
    deployment as the existing wdqs.
    
    * Renames the related components to make it clear they are not
      specific to wdqs
    * Adds a per-site define to declare the domain name served and where
      to find domain specific assets.
    * Add mod_write to the query_service microsite. Rewrite requests to
      not found top-level assets into the per-site assets. This allows
      sites to add additional files to the domain root, but not override
      anything provided by the build.
    
    Bug: T280247
    Change-Id: I609ee42c2b163f852686503b748e5292405f4cde

--- modules/profile/manifests/microsites/query_service.pp
+++ modules/profile/manifests/microsites/query_service.pp
@@ -1,12 +1,13 @@
-# Wikidata Query Service UI (T266702)
-class profile::microsites::wdqs {
+# Wikibase Query Service UI (T266702)
+class profile::microsites::query_service {
 
-    httpd::site { 'query.wikidata.org':
-        content => template('profile/wdqs/httpd-query.wikidata.org.erb'),
+    profile::microsites::query_service::site { 'wdqs':
+        domain_name => 'query.wikidata.org',
     }
 
-    httpd::site { 'query-preview.wikidata.org':
-        content => template('profile/wdqs/httpd-query-preview.wikidata.org.erb'),
+    profile::microsites::query_service::site { 'wdqs-preview':
+        domain_name => 'query-preview.wikidata.org',
+        deploy_name => 'wdqs',
     }
 
     ensure_resource('file', '/srv/org', {'ensure' => 'directory' })
---
+++ modules/profile/manifests/microsites/query_service/site.pp
@@ -0,0 +1,12 @@
+# A single site served by Wikibase Query Service GUI
+#
+# $domain_name - The domain name to respond to
+# $deploy_name - Name used to find site specific assets in deployment.
+define profile::microsites::query_service::site(
+    String $domain_name,
+    String $deploy_name = $title
+) {
+    httpd::site { $domain_name:
+        content => template('profile/query_service/httpd.erb')
+    }
+}
