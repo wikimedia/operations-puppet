f21b24e85c12d8cd74f815e9b6fd02b983e72d0a
John Bond <jbond@wikimedia.org>
2021-08-25 13:56:50 +0200

    wmflib::role_hosts: fix typos
    Change-Id: I685a9a70593db60d90dfb1267ece28e7a0a98e9c

--- modules/wmflib/functions/role_hosts.pp
+++ modules/wmflib/functions/role_hosts.pp
@@ -1,8 +1,8 @@
 # @summary function to return a list of hosts running a specific role
 #  This function relies on data being present in puppetdb.  This means that new nodes will
-#  be retuned after their first successful puppet run using the specified role.  It also means
+#  be returned after their first successful puppet run using the specified role.  It also means
 #  that nodes will be removed from the results once they have been purged from puppetdb.  This currently
-#  happens when a server has failed to tun puppet for 14 days
+#  happens when a server has failed to run puppet for 14 days
 # @param role the role to search for
 # @param optional list of sites if present filter to output based on the sites passed
 function wmflib::role_hosts (
