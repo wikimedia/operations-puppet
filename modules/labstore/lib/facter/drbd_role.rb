d6374862142e0115d278dc4f2a1f510c8e12e561
John Bond <jbond@wikimedia.org>
2021-08-25 15:21:48 +0200

    labstore::drdb_role fact: update facter implementation to ignore stderr
    Currently when running facter where drbd is avalible we get the
    following message from facter
    
      NOTE: drbd-overview will be deprecated soon.
      Please consider using drbdtop.
    
    Update the fact implementation to use Facter::Util::Resolution.exec
    instead of backticks to ensure stderr is automatically thrown out.  also
    Guard the fact with a confine instead of an if statement
    
    Bug: T289679
    Change-Id: I218a03be904d822930df8875fb5246f999d97590

--- modules/labstore/lib/facter/drbd_role.rb
+++ modules/labstore/lib/facter/drbd_role.rb
@@ -1,16 +1,17 @@
 # Gather the DRBD role for each volume and report it
-if FileTest.exists?('/usr/sbin/drbd-overview')
-  Facter.add('drbd_role') do
-      confine :kernel => :linux
-      setcode do
-        drbd_info = `/usr/sbin/drbd-overview`
-        drbd_role = {}
-        drbd_info.split(/\n/).each do |line|
-          next unless line =~ /^\s+\d.*/
-          sections = line.split(' ')
-          drbd_role[sections[0].sub(%r{^\d+:(\w+)/.*$}, '\1')] = sections[2].split("/")[0].downcase
-        end
-        drbd_role
-      end
+Facter.add('drbd_role') do
+  confine :kernel => :linux
+  confine do
+    FileTest.exists?('/usr/sbin/drbd-overview')
+  end
+  setcode do
+    drbd_info = Facter::Util::Resolution.exec('/usr/sbin/drbd-overview')
+    drbd_role = {}
+    drbd_info.split(/\n/).each do |line|
+      next unless line =~ /^\s+\d.*/
+      sections = line.split(' ')
+      drbd_role[sections[0].sub(%r{^\d+:(\w+)/.*$}, '\1')] = sections[2].split("/")[0].downcase
+    end
+    drbd_role
   end
 end
