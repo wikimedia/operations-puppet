fcf2787cf04629b823c5e975ff5300281de4c6d6
Riccardo Coccioli <rcoccioli@wikimedia.org>
2021-08-26 17:16:07 +0200

    mariadb: add section to alert name
    * In order to prevent duplicated entried in Icinga config.
    
    Change-Id: I2f25d3c1361cd8b83d4249f8087ecf407a3e76ca

--- modules/profile/manifests/mariadb/replication_lag.pp
+++ modules/profile/manifests/mariadb/replication_lag.pp
@@ -2,7 +2,7 @@ define profile::mariadb::replication_lag(
     Integer $prom_port = 9104,
 ){
     monitoring::check_prometheus { "mariadb-prolonged-lag-${title}":
-        description     => 'MariaDB sustained replica lag',
+        description     => "MariaDB sustained replica lag on ${title}",
         dashboard_links => ["https://grafana.wikimedia.org/d/000000273/mysql?orgId=1&var-server=${::hostname}&var-port=${prom_port}"],
         query           => "scalar(avg_over_time(mysql_slave_status_seconds_behind_master{instance=\"${::hostname}:${prom_port}\"}[5m]))",
         prometheus_url  => "http://prometheus.svc.${::site}.wmnet/ops",
