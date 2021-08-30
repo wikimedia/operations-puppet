701941c257420460efdbb75dd06a7b571aec11cb
Marcel Ruiz Forns <mforns@wikimedia.org>
2021-08-30 09:04:14 +0200

    Fix --until for monitor_refine_event_sanitized_analytics_delayed
    Change-Id: Iacec4792bb9c9dfd7f432f7fdda17683931073fb

--- modules/profile/manifests/analytics/refinery/job/refine_sanitize.pp
+++ modules/profile/manifests/analytics/refinery/job/refine_sanitize.pp
@@ -107,7 +107,7 @@ class profile::analytics::refinery::job::refine_sanitize(
         interval      => '*-*-* 05:00:00',
         # delayed job should monitor around the day it is scheduled for.
         monitor_since => $delayed_since + 24,
-        monitor_until => $delayed_until - 24,
+        monitor_until => $delayed_until,
         job_config    => $event_sanitized_main_job_config.merge({
             'since' => $delayed_since,
             'until' => $delayed_until,
