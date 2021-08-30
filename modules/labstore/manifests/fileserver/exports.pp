60d00e2ec72096f0ab43c2aaffe1663260b1b455
Alexander Vorwerk <alexander.vorwerk@stud.uni-goettingen.de>
2021-08-30 22:11:54 +0200

    labstore: remove absented /etc/exports.d/public_root.exports file
    Change-Id: Ie0e3e05410b1e70060cc82b1849d39e01520fd53

--- modules/labstore/manifests/fileserver/exports.pp
+++ modules/labstore/manifests/fileserver/exports.pp
@@ -76,11 +76,6 @@ class labstore::fileserver::exports(
         require     => File['/etc/exports.bak'],
     }
 
-    # TODO: Remove after initial runs. This just cleans up the old setup.
-    file { '/etc/exports.d/public_root.exports':
-        ensure => absent,
-    }
-
     file { '/usr/local/sbin/archive-project-volumes':
         owner  => 'root',
         group  => 'root',
