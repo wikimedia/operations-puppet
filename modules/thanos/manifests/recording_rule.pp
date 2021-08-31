7d96628049c995853845f4eb015b164056c493cc
herron <kherron@wikimedia.org>
2021-08-31 15:26:49 -0400

    thanos: add thanos::recording_rule
    Create thanos::recording_rule to deploy thanos rule recording rules
    in a way similar to prometheus::rule.
    
    Bug: T287142
    Change-Id: I651bdb7401778d65eaa3acf0bfcacb117670317b

---
+++ modules/thanos/manifests/recording_rule.pp
@@ -0,0 +1,29 @@
+# Manage Thanos recording rules
+#
+
+define thanos::recording_rule (
+    Wmflib::Ensure   $ensure    = 'present',
+    Optional[String] $source    = undef,
+    Optional[String] $content   = undef,
+    Stdlib::Unixpath $rules_dir = '/etc/thanos-rule/rules',
+) {
+
+    # The thanos config includes glob $rules_dir/*.yaml, so require a .yaml file extension
+    if $title !~ '.yaml$' {
+        fail("Title(${title}): thanos recording rules must have a .yaml file extention")
+    }
+
+    # Perform a rule validity check before deploying
+    $validate_cmd = '/usr/bin/thanos tools rules-check --rules %'
+
+    file { "${rules_dir}/${title}":
+        ensure       => file,
+        mode         => '0444',
+        owner        => 'root',
+        source       => $source,
+        content      => $content,
+        validate_cmd => $validate_cmd,
+        notify       => Exec['reload thanos-rule'],
+    }
+
+}
