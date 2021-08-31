7ac83a4b1df2550c3dd42853f6399e4def362053
Taavi Väänänen <hi@taavi.wtf>
2021-08-31 10:51:59 +0300

    toolforge: drop legacy webservice endpoints on bullseye
    Change-Id: I6fc0c0bebd51ac4f03c2ed79a8f7334dd5fe9d76

--- modules/profile/manifests/toolforge/k8s/client.pp
+++ modules/profile/manifests/toolforge/k8s/client.pp
@@ -7,16 +7,18 @@ class profile::toolforge::k8s::client (
         ensure => latest,
     }
 
-    # Legacy locations for the entry point script from toollabs-webservice
-    # that are probably still hardcoded in some Tools.
-    file { [
-        '/usr/local/bin/webservice2',
-        '/usr/local/bin/webservice',
-    ]:
-        ensure => link,
-        target => '/usr/bin/webservice',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
+    if debian::codename::le('buster') {
+        # Legacy locations for the entry point script from toollabs-webservice
+        # that are probably still hardcoded in some Tools.
+        file { [
+            '/usr/local/bin/webservice2',
+            '/usr/local/bin/webservice',
+        ]:
+            ensure => link,
+            target => '/usr/bin/webservice',
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+        }
     }
 }
