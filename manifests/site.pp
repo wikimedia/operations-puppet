5d4237bec6b0d174926270bcee4890478a061dcc
Manuel Arostegui <marostegui@wikimedia.org>
2021-09-02 10:08:07 +0200

    mariadb: Promote db2110 to s4 master
    db2090 will become the candidate master although eventually will be
    replaced by db2140.
    
    Bug: T289650
    Change-Id: I2d2f185b2d6e688f94280a112e36add68786d6f0

--- manifests/site.pp
+++ manifests/site.pp
@@ -526,7 +526,7 @@ node 'db1125.eqiad.wmnet' {
 
 # s4 (commons) core production dbs on codfw
 # codfw master
-node 'db2090.codfw.wmnet' {
+node 'db2110.codfw.wmnet' {
     role(mariadb::core)
 }
 
@@ -537,7 +537,7 @@ node 'db2140.codfw.wmnet' {
 
 # codfw replicas
 # See also db2137 and db2138 below
-node /^db2(106|110|119|136|147)\.codfw\.wmnet/ {
+node /^db2(090|106|119|136|147)\.codfw\.wmnet/ {
     role(mariadb::core)
 }
 
