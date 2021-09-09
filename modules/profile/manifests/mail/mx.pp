9df7bca34218954894cc109f57a5f66d9ed7c968
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-09-09 12:42:27 +0200

    Disable new config validation on Bullseye
    Bug: T286911
    Hosts: mx1001.wikimedia.org, mx2002.wikimedia.org, otrs1001.eqiad.wmnet
    Change-Id: I1583c3435b8bdd81722e04658e0d754b7e5dc4b9

--- modules/profile/manifests/mail/mx.pp
+++ modules/profile/manifests/mail/mx.pp
@@ -40,6 +40,12 @@ class profile::mail::mx (
     $otrs_mysql_password = $passwords::exim::otrs_mysql_password
     $smtp_ldap_password  = $passwords::exim::smtp_ldap_password
 
+    # Exim 4.94 introduces a new tainting mechanism. We're not adapting our configs
+    # to his new scheme since the mail servers will be switched to Postfix anyway
+    if debian::codename::eq('bullseye') {
+        $disable_taint_check = true
+    }
+
     class { 'exim4':
         variant => 'heavy',
         config  => template('role/exim/exim4.conf.mx.erb'),
