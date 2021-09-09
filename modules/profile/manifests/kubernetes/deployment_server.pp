2d5673020a9994176147a5e2cda446b3f6abe1dc
Luca <ltoscano@wikimedia.org>
2021-09-09 17:10:05 +0200

    kubernetes: add revscoring-editquality in the services configs
    The ML team would like to deploy the first helmfile-based service
    definition on the deployment nodes.
    There is already a draft related to how the helmfile config will
    look like in https://gerrit.wikimedia.org/r/c/operations/deployment-charts/+/719128
    
    Most of the work on which we'll focus during the next months
    is related to add revscoring models, namely what we have on ORES.
    
    Part of this change is to split tokens and secrets between "main"
    clusters and "ml-serve" clusters, so that there is a clean separation
    between the two.
    
    For secrets, the idea is to create new directories on deploy1002 named:
    - /etc/helmfile-defaults/private/ml-serve_services
    - /etc/helmfile-defaults/private/main_services
    - /etc/helmfile-defaults/private/admin_ng_services
    The current /etc/helmfile/private's content is not absented, so any
    deployment that may happen between merging this change and changing
    helmfile configs in deployment-charts will not be disrupted. We will
    eventually clean up the leftovers manually.
    
    The admin token will be copied over from main to ml-serve for the moment
    (so all clusters will keep sharing it) and a subsequent patch will change
    it for ML clusters (configuring kube-api as well).
    
    Before merging this change some changes to the puppet private repo
    are of course needed.
    
    Bug: T286791
    Change-Id: I6cbdab0f868f62adb5fdeda2a632befca6db458f

--- modules/profile/manifests/kubernetes/deployment_server.pp
+++ modules/profile/manifests/kubernetes/deployment_server.pp
@@ -1,10 +1,10 @@
 # Class makes sure we got a deployment server ready
 class profile::kubernetes::deployment_server(
-    Hash[String, Any] $services = lookup('profile::kubernetes::deployment_server::services', {default_value => {}}),
-    Hash[String, Any] $tokens   = lookup('profile::kubernetes::deployment_server::tokens', {default_value => {}}),
-    String $git_owner           = lookup('profile::kubernetes::deployment_server::git_owner'),
-    String $git_group           = lookup('profile::kubernetes::deployment_server::git_group'),
-    Boolean $packages_from_future = lookup('profile::kubernetes::deployment_server::packages_from_future', {default_value => false}),
+    Hash[String, Any] $services    = lookup('profile::kubernetes::deployment_server::services', {default_value => {}}),
+    Hash[String, Any] $tokens      = lookup('profile::kubernetes::deployment_server::tokens', {default_value => {}}),
+    String $git_owner              = lookup('profile::kubernetes::deployment_server::git_owner'),
+    String $git_group              = lookup('profile::kubernetes::deployment_server::git_group'),
+    Boolean $packages_from_future  = lookup('profile::kubernetes::deployment_server::packages_from_future', {default_value => false}),
 
 ){
     include profile::kubernetes::deployment_server::helmfile
@@ -16,22 +16,31 @@ class profile::kubernetes::deployment_server(
 
     ensure_packages('istioctl')
 
-    $real_services = deep_merge($services, $tokens)
-    $ml_services = filter($real_services) |String $service, Hash $data| {
-        $service == 'admin'
+    $kube_clusters = {
+        'main' => {
+            'eqiad'         => 'kubemaster.svc.eqiad.wmnet',
+            'codfw'         => 'kubemaster.svc.codfw.wmnet',
+            'staging-eqiad' => 'kubestagemaster.svc.eqiad.wmnet',
+            'staging-codfw' => 'kubestagemaster.svc.codfw.wmnet',
+            # This represents the active staging cluster currently used by deployment tools (helmfile)
+            'staging'       => 'kubestagemaster.svc.eqiad.wmnet',
+        },
+        'ml-serve' => {
+            'ml-serve-eqiad' => 'ml-ctrl.svc.eqiad.wmnet',
+            'ml-serve-codfw' => 'ml-ctrl.svc.codfw.wmnet',
+        },
     }
 
-    $envs = {
-        'eqiad'          => { 'master' => 'kubemaster.svc.eqiad.wmnet', 'services' => $real_services },
-        'codfw'          => { 'master' => 'kubemaster.svc.codfw.wmnet', 'services' => $real_services },
-        'staging-eqiad'  => { 'master' => 'kubestagemaster.svc.eqiad.wmnet', 'services' => $real_services },
-        'staging-codfw'  => { 'master' => 'kubestagemaster.svc.codfw.wmnet', 'services' => $real_services },
-        # This represents the active staging cluster currently used by deployment tools (helmfile)
-        'staging'        => { 'master' => 'kubestagemaster.svc.eqiad.wmnet', 'services' => $real_services },
-        # ML clusters
-        'ml-serve-eqiad' => { 'master' => 'ml-ctrl.svc.eqiad.wmnet', 'services' => $ml_services },
-        'ml-serve-codfw' => { 'master' => 'ml-ctrl.svc.codfw.wmnet', 'services' => $ml_services },
-    }
+    # For each cluster, define a map between its codename and:
+    # - Its kube-api endpoint fqdn.
+    # - The data structure containing k8s tokens for its cluster group
+    #   (all clusters in a group shares the same set of tokens).
+    $envs = $kube_clusters.map |$cluster_group, $clusters| {
+        $services = deep_merge($services[$cluster_group], $tokens[$cluster_group])
+        $clusters.map |$cluster, $master| {
+            { $cluster => {'master' => $master, 'services' => $services } }
+        }.reduce({}) |$m, $v| { $m.merge($v) }
+    }.reduce({}) |$mem, $val| { $mem.merge($val)}
 
     # Now populate the /etc/kubernetes/ kubeconfig resources
     $envs.each |String $env, Hash $env_data| {
@@ -50,6 +59,12 @@ class profile::kubernetes::deployment_server(
             # lint:endignore
         }
     }
+
+    # Used to support the kube-env.sh script. A list of service names is useful
+    # to create an auto-complete feature for kube_env.
+    $all_service_names = $services.map |$group, $srvs| {
+        keys($srvs) }.reduce([]) |$mem, $val| { $mem + $val }
+
     # Add a script to profile.d with functions to set the configuration for kubernetes.
     file { '/etc/profile.d/kube-env.sh':
         ensure  => present,
--- modules/profile/manifests/kubernetes/deployment_server/helmfile.pp
+++ modules/profile/manifests/kubernetes/deployment_server/helmfile.pp
@@ -6,7 +6,6 @@ class profile::kubernetes::deployment_server::helmfile(
     Hash[String, Any] $services=lookup('profile::kubernetes::deployment_server::services', {'default_value' => {}}),
     Hash[String, Any] $services_secrets=lookup('profile::kubernetes::deployment_server_secrets::services', {'default_value' => {}}),
     Hash[String, Any] $default_secrets=lookup('profile::kubernetes::deployment_server_secrets::defaults', {'default_value' => {}}),
-
 ){
     # Add the global configuration for all deployments.
     require ::profile::kubernetes::deployment_server::global_config
@@ -18,44 +17,46 @@ class profile::kubernetes::deployment_server::helmfile(
         srcdir     => '/srv/deployment-charts'
     }
 
-
     # Install the private values for each deployment.
     $general_private_dir = "${::profile::kubernetes::deployment_server::global_config::general_dir}/private"
 
-
-    $merged_services = deep_merge($services, $services_secrets).filter|String $svcname, Hash $data| {
-        $svcname != 'admin'
-    }
-
-    # New-style private directories are one per service, not per cluster too.
-    $merged_services.each |String $svcname, Hash $data| {
-        file { "${general_private_dir}/${svcname}":
-            ensure => directory,
-            owner  => $data['owner'],
-            group  => $data['group'],
-            mode   => '0750',
+    # "service_group" holds a value for each k8s cluster group that we run (main, ml-serve, etc..).
+    $services.each |String $service_group, Hash $service_data| {
+        $merged_services = deep_merge($service_data, $services_secrets[$service_group]).filter|String $svcname, Hash $data| {
+            $svcname != 'admin'
         }
-    }
+        $private_dir = "${general_private_dir}/${service_group}_services"
 
-    $clusters.each |String $environment, String $dc| {
-        $merged_services.map |String $svcname, Hash $data| {
-            $raw_data = deep_merge($default_secrets[$environment], $data[$environment])
-            # write private section only if there is any secret defined.
-            unless $raw_data.empty {
-                # Substitute the value of any key in the form <somekey>: secret__<somevalue>
-                # with <somekey>: secret(<somevalue>)
-                # This allows to avoid having to copy/paste certs inside of yaml files directly,
-                # for example.
-                $secret_data = wmflib::inject_secret($raw_data)
-
-                file { "${general_private_dir}/${svcname}/${environment}.yaml":
-                    owner   => $data['owner'],
-                    group   => $data['group'],
-                    mode    => $data['mode'],
-                    content => ordered_yaml($secret_data),
-                    require => "File[${general_private_dir}/${svcname}]"
-                }
+        # New-style private directories are one per service, not per cluster too.
+        $merged_services.each |String $svcname, Hash $data| {
+            file { "${private_dir}/${svcname}":
+                ensure => directory,
+                owner  => $data['owner'],
+                group  => $data['group'],
+                mode   => '0750',
             }
         }
-    } # end clusters
+
+        $clusters.each |String $environment, String $dc| {
+            $merged_services.map |String $svcname, Hash $data| {
+                $raw_data = deep_merge($default_secrets[$environment], $data[$environment])
+                # write private section only if there is any secret defined.
+                unless $raw_data.empty {
+                    # Substitute the value of any key in the form <somekey>: secret__<somevalue>
+                    # with <somekey>: secret(<somevalue>)
+                    # This allows to avoid having to copy/paste certs inside of yaml files directly,
+                    # for example.
+                    $secret_data = wmflib::inject_secret($raw_data)
+
+                    file { "${private_dir}/${svcname}/${environment}.yaml":
+                        owner   => $data['owner'],
+                        group   => $data['group'],
+                        mode    => $data['mode'],
+                        content => ordered_yaml($secret_data),
+                        require => "File[${private_dir}/${svcname}]"
+                    }
+                }
+            }
+        } # end clusters
+    } # end cluster_directories
 }
