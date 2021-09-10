3ae0e6cb644ebcbed65ca769c329616cbafa7d93
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2021-09-10 11:33:35 +0200

    toolforge: package_builder: don't assume we have 2 disks starting with bullseye
    I don't think we need 2 disks in the package builder VMs.
    
    Let's assume we don't have that starting with bullseye.
    
    Change-Id: I7e96eaf37277d7f1f6fd67b368d41c779f9cca5e
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/role/manifests/wmcs/toolforge/package_builder.pp
+++ modules/role/manifests/wmcs/toolforge/package_builder.pp
@@ -4,6 +4,10 @@ class role::wmcs::toolforge::package_builder {
     }
 
     include ::profile::toolforge::apt_pinning
-    include ::profile::labs::lvm::srv
     include ::profile::toolforge::package_builder
+
+    # arturo thinks we don't need this, trying in bullseye without it
+    if debian::codename::lt('bullseye') {
+        include ::profile::labs::lvm::srv
+    }
 }
