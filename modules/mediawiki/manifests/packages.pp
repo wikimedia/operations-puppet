1c366977a2949157c953d87b0df70813b9f031e0
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2021-09-14 14:41:54 +0200

    mediawiki::packages: remove libvips
    It is no longer used.
    
    Bug: T290759
    Change-Id: Ice5c9ef1ed93b56cf26ab11e330a01b65c9d5cc4

--- modules/mediawiki/manifests/packages.pp
+++ modules/mediawiki/manifests/packages.pp
@@ -6,8 +6,7 @@ class mediawiki::packages {
 
     ensure_packages([
         'fluidsynth', 'fluid-soundfont-gs', 'fluid-soundfont-gm', 'firejail',
-        # vips is needed for (rare) non-Thumbor scaling of TIFF/PNG uploads (T199938)
-        'tidy', 'libvips-tools',
+        'tidy',
         # PDF and DjVu
         'ghostscript', 'djvulibre-bin', 'librsvg2-bin', 'libtiff-tools', 'poppler-utils',
         # ploticus for EasyTimeline extension (T237304)
