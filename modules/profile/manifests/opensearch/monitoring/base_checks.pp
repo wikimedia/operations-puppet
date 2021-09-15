63d3c2fd13507511cf4a3569e4246874fe88308a
Cole White <cwhite@wikimedia.org>
2021-09-15 13:59:17 -0600

    profile: fork elasticsearch base_checks for opensearch
    Better view of the differences can be seen with:
      `diff modules/profile/manifests/elasticsearch/monitor/base_checks.pp \
    modules/profile/manifests/opensearch/monitoring/base_checks.pp`
    
    Bug: T288618
    Change-Id: I2054c2e53ce79653b8e4a2749767b41080567d06

---
+++ modules/profile/manifests/opensearch/monitoring/base_checks.pp
@@ -0,0 +1,22 @@
+class profile::opensearch::monitoring::base_checks(
+    Integer $shard_size_warning  = lookup('profile::opensearch::monitoring::shard_size_warning',  { 'default_value' => 110 }),
+    Integer $shard_size_critical = lookup('profile::opensearch::monitoring::shard_size_critical', { 'default_value' => 140 }),
+    String $threshold            = lookup('profile::opensearch::monitoring::threshold',           { 'default_value' => '>=0.15' }),
+    Integer $timeout             = lookup('profile::opensearch::monitoring::timeout',             { 'default_value' => 4 }),
+) {
+    require ::profile::opensearch::server
+
+    $configured_ports = $::profile::opensearch::server::filtered_instances.reduce([]) |$ports, $instance_params| {
+        $ports + [$instance_params[1]['http_port']]
+    }
+
+    # For monitoring of eqiad/codfw cirrus clusters, see icinga::monitor::elasticsearch::cirrus_cluster_checks
+    icinga::monitor::opensearch::base_checks { $::hostname:
+        ports               => $configured_ports,
+        shard_size_warning  => $shard_size_warning,
+        shard_size_critical => $shard_size_critical,
+        timeout             => $timeout,
+        threshold           => $threshold,
+        use_nrpe            => true,
+    }
+}
