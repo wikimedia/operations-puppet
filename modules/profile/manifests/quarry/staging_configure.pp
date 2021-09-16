e8af9a044f6e7af62d1644d3f3484a56a8fdcd80
Michael DiPietro <mdipietro@wikimedia.org>
2021-09-16 12:36:29 -0400

    create role to deploy staging instance for quarry
    To productionize a bit this role is being introduced. It
    will allow for a new quarry dev system to be brought online
    with code. Used as a staging system to test features and
    bug fixes before moving the code to public quarry nodes.
    
    Bug: T291204
    Change-Id: Iac2a1e0cfbc1dd1e42fcba2468c7c33570ba4abb

---
+++ modules/profile/manifests/quarry/staging_configure.pp
@@ -0,0 +1,48 @@
+# = Class: profile::quarry::staging_configure
+#
+# Sets up a mysql database and configs for use by the Quarry
+# staging node.
+class profile::quarry::staging_configure (
+    String       $dbpass = lookup('profile::quarry::staging_configure::dbpass', {default_value => 'notarealpassword'}),
+    String       $oauth_secret_token = lookup('profile::quarry::staging_configure::oauth_secret_token', {default_value => 'blank'}),
+    String       $oauth_consumer_token = lookup('profile::quarry::staging_configure::oauth_consumer_token', {default_value => 'blank'}),
+    String       $replicapass = lookup('profile::quarry::staging_configure::replica_password', {default_value => 'blank'}),
+    String       $replicauser = lookup('profile::quarry::staging_configure::replica_user', {default_value => 'blank'}),
+){
+    file { ['/data', '/data/project']:
+        ensure => directory,
+        owner  => 'root',
+        before => 'File[/data/project/quarry]'
+    }
+
+    file { '/srv/results':
+        ensure => 'directory',
+        owner  => 'quarry',
+        group  => 'quarry',
+        mode   => '0755',
+    }
+
+    exec {'import mysql':
+        path    => '/usr/bin:/usr/sbin',
+        command => 'mysql -u root < /srv/quarry/schema.sql',
+        require => 'Exec[git_clone_analytics/quarry/web]',
+    }
+
+    file { '/srv/quarry/quarry/config.yaml':
+        ensure  => file,
+        content => template('quarry/staging_config.yaml.erb'),
+        require => 'Exec[git_clone_analytics/quarry/web]',
+    }
+
+    file { '/root/staging_user.sql':
+        ensure  => file,
+        content => template('quarry/staging_user.sql.erb'),
+        notify  => 'Exec[add quarry user]',
+    }
+
+    exec {'add quarry user':
+        path        => '/usr/bin',
+        command     => 'mysql -u root < /root/staging_user.sql',
+        refreshonly => true,
+    }
+}
---
+++ modules/role/manifests/wmcs/quarry/staging.pp
@@ -0,0 +1,13 @@
+# filtertags: labs-project-quarry
+class role::wmcs::quarry::staging {
+    system::role { $name:
+        description => 'staging instance deployment for quarry'
+    }
+
+    include ::profile::quarry::database
+    include ::profile::quarry::base
+    include ::profile::quarry::staging_configure
+    include ::profile::quarry::web
+    include ::profile::quarry::celeryrunner
+    include ::profile::quarry::redis
+}
