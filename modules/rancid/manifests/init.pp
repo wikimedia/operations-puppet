53d325e889d850471db8dfddbb2cca1db0b2f955
Daniel Zahn <dzahn@wikimedia.org>
2021-09-17 08:11:30 -0700

    rancid: convert crons to systemd timers
    Bug: T273673
    Change-Id: Ie4be2f2307c3df41d25d4a0c0ee42e8f08a9cc01

--- modules/rancid/manifests/init.pp
+++ modules/rancid/manifests/init.pp
@@ -94,24 +94,45 @@ class rancid (
     }
 
     if $active_server == $::fqdn {
-        $cron_ensure = 'present'
+        $job_ensure = 'present'
     } else {
-        $cron_ensure = 'absent'
+        $job_ensure = 'absent'
     }
 
     cron { 'rancid_differ':
-        ensure  => $cron_ensure,
+        ensure  => 'absent',
         command => 'SSH_AUTH_SOCK=/run/keyholder/proxy.sock /usr/lib/rancid/bin/rancid-run',
         user    => 'rancid',
         minute  => '1',
     }
 
+    systemd::timer::job { 'rancid-differ':
+        ensure             => $job_ensure,
+        user               => 'rancid',
+        description        => 'run rancid-run',
+        environment        => { 'SSH_AUTH_SOCK' => '/run/keyholder/proxy.sock' },
+        command            => '/usr/lib/rancid/bin/rancid-run',
+        interval           => {'start' => 'OnUnitInactiveSec', 'interval' => '1h'},
+        monitoring_enabled => false,
+        logging_enabled    => false,
+    }
+
     cron { 'rancid_clean_logs':
-        ensure  => $cron_ensure,
+        ensure  => 'absent',
         command => '/usr/bin/find /var/log/rancid -type f -mtime +2 -exec rm {} \;',
         user    => 'rancid',
         minute  => '50',
         hour    => '23',
+    }
+
+    systemd::timer::job { 'rancid-clean-logs':
+        ensure             => $job_ensure,
+        user               => 'rancid',
+        description        => 'clean rancid logs',
+        command            => '/usr/bin/find /var/log/rancid -type f -mtime +2 -exec rm {} \;',
+        interval           => {'start' => 'OnCalendar', 'interval' => '*-*-* 23:50:0'},
+        monitoring_enabled => false,
+        logging_enabled    => false,
     }
 
     file { '/var/log/rancid':
