95c738813b5b0624644e1d9802a8dfd6ceb53293
Daniel Zahn <dzahn@wikimedia.org>
2021-09-20 14:43:55 -0700

    swift: remove absented cron for dispersion stats
    follow-up to I28dbede367296be
    
    Bug: T273673
    Bug: T288806
    Change-Id: Ib0265bc0958a465eabc2de42df1d4e83ac48cb0e

--- modules/swift/manifests/stats/dispersion.pp
+++ modules/swift/manifests/stats/dispersion.pp
@@ -22,15 +22,6 @@ class swift::stats::dispersion(
     }
 
     # XXX swift-dispersion-populate is not ran/initialized
-    cron { 'swift-dispersion-stats':
-        ensure  => 'absent',
-        command => "/usr/local/bin/swift-dispersion-stats --prefix ${statsd_prefix} --statsd-host ${statsd_host} --statsd-port ${statsd_port} >/dev/null 2>&1",
-        user    => 'root',
-        hour    => '*',
-        minute  => '*/15',
-        require => File['/usr/local/bin/swift-dispersion-stats'],
-    }
-
     systemd::timer::job { 'swift_dispersion_stats':
         ensure             => $ensure,
         user               => 'root',
