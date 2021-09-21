40b8f51f6ca7ace15d27e9d2e8daea552ed8caeb
Brooke Storm <bstorm@wikimedia.org>
2021-09-21 11:03:13 -0700

    toolforge harbor: install clients for redis and postgres
    Setup, troubleshooting and maintenance are likely to require these.
    An initial test just puts this all in containers, but that's a little
    too disposable, especially for postgres.
    
    Bug: T267616
    Change-Id: I9d4eadc68fcd25143da8abdcaf86e9e47bedec00

--- modules/profile/manifests/toolforge/harbor.pp
+++ modules/profile/manifests/toolforge/harbor.pp
@@ -1,5 +1,7 @@
 class profile::toolforge::harbor (
 ) {
     require profile::wmcs::kubeadm::client
+
+    ensure_packages(['postgresql-client', 'redis-tools'])
     acme_chief::cert { 'toolforge': }
 }
