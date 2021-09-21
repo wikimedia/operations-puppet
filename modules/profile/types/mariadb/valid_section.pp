0d41c9da6983a657d4f30653cbcd711534f80551
Manuel Arostegui <marostegui@wikimedia.org>
2021-09-21 08:39:49 +0200

    valid_section.pp: Remove reference to s10
    s10 is no longer. Wikitech now lives in s6.
    
    Bug: T167973
    Change-Id: I587ce91fa97820248740383c1d33b2e2efd6ebd1

--- modules/profile/types/mariadb/valid_section.pp
+++ modules/profile/types/mariadb/valid_section.pp
@@ -1,8 +1,6 @@
 type Profile::Mariadb::Valid_section = Enum[
     # Mediawiki metadata
     's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8',
-    # Wikitech - hosted in m5, does not appear in puppet T258376
-    #'s10',
     # labtestwiki - hosted on clouddb2001-dev, does not appear in puppet T258376
     #'s11',
     # Mediawiki extension data
