27112ab8c03c2347c7fc7df35659f39ea2da3ebc
Daniel Zahn <dzahn@wikimedia.org>
2021-09-22 16:51:00 -0700

    mail::mx: add host name and fix quoting in mail-exim-aliases
    follow-up to Icd312ee242e6907
    
    We want the host name in the subject like it was before and
    quoting needed fixing to correctly set the subject.
    
    Change-Id: Ib9b3504ae4e0d2bbc5c44da634d259ab18012d15

--- modules/profile/files/mail/mx/mail-exim-aliases.sh
+++ modules/profile/files/mail/mx/mail-exim-aliases.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /etc/mail-exim-aliases
-/usr/bin/mail -s '${SUBJECT}' ${RECIPIENT} < ${ALIAS_FILE}
+/usr/bin/mail -s "${SUBJECT} from $(hostname -s)" ${RECIPIENT} < ${ALIAS_FILE}
