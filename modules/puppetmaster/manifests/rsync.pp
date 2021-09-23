bb41e9c3c87c2192fece9b025876dd901a3a1441
Daniel Zahn <dzahn@wikimedia.org>
2021-09-23 14:01:51 -0700

    puppetmaster::rsync: replace data sync crons with timers/jobs
    Bug: T273673
    Change-Id: I25308153361438c3eeb9883cd271c328ff2ac334

--- modules/puppetmaster/manifests/rsync.pp
+++ modules/puppetmaster/manifests/rsync.pp
@@ -20,14 +20,34 @@ class puppetmaster::rsync(
     }
 
     cron { 'sync_volatile':
-        ensure  => $cron_ensure,
+        ensure  => absent,
         command => "/usr/bin/rsync -avz --delete ${server}::puppet_volatile /var/lib/puppet/volatile > /dev/null 2>&1",
         minute  => '*/15',
     }
 
+    systemd::timer::job { 'sync-puppet-volatile':
+        ensure             => $cron_ensure,
+        user               => 'root',
+        description        => 'rsync puppet volatile data to another server',
+        command            => "/usr/bin/rsync -avz --delete ${server}::puppet_volatile /var/lib/puppet/volatile",
+        interval           => {'start' => 'OnUnitInactiveSec', 'interval' => '15m'},
+        monitoring_enabled => false,
+        logging_enabled    => false,
+    }
+
     cron { 'sync_ca':
-        ensure  => $cron_ensure,
+        ensure  => absent,
         command => "/usr/bin/rsync -avz --delete ${server}::puppet_ca /var/lib/puppet/server/ssl/ca > /dev/null 2>&1",
         hour    => '4',
+    }
+
+    systemd::timer::job { 'sync-puppet-ca':
+        ensure             => $cron_ensure,
+        user               => 'root',
+        description        => 'rsync puppet CA data to another server',
+        command            => "/usr/bin/rsync -avz --delete ${server}::puppet_volatile /var/lib/puppet/server/ssl/ca",
+        interval           => {'start' => 'OnUnitInactiveSec', 'interval' => 'daily'},
+        monitoring_enabled => false,
+        logging_enabled    => false,
     }
 }
