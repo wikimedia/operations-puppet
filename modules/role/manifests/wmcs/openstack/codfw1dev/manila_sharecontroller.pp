df3c32a224000df673f9e23b53b56243a4afbf05
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2021-09-24 15:07:18 +0200

    openstack: manila_sharecontroller: require Debian Bullseye
    So far this only works on Debian Bullseye.
    
    Bug: T291257
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: Ica1f641b94ee19c61c159bbbfc447c7d775a7c2d

--- modules/role/manifests/wmcs/openstack/codfw1dev/manila_sharecontroller.pp
+++ modules/role/manifests/wmcs/openstack/codfw1dev/manila_sharecontroller.pp
@@ -4,5 +4,9 @@ class role::wmcs::openstack::codfw1dev::manila_sharecontroller {
     # this is meant to be deployed on a virtual machine inside CloudVPS
     requires_realm('labs')
 
+    # so far we only have the manila-share package available for bullseye,
+    # adding it for buster should be trivial, but ... worth it?
+    debian::codename::require::min('bullseye')
+
     include profile::openstack::coddfw1dev::manila::sharecontroller
 }
