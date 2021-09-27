533bd7a95b1fdf0e6c31535a2fd0f134b44d73f4
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-09-27 16:25:40 +0200

    Setup systemd timer to sync OS reports
    Hosts: miscweb1002.eqiad.wmnet
    Change-Id: Id8d071d843c84249526b164e1cca82de26d1a232

--- modules/profile/manifests/microsites/os_reports.pp
+++ modules/profile/manifests/microsites/os_reports.pp
@@ -1,8 +1,22 @@
-class profile::microsites::os_reports {
+class profile::microsites::os_reports (
+    Stdlib::Host $os_reports_host = lookup('profile::sre::os_reports::host'),
+) {
     $docroot  = '/srv/org/wikimedia/os-reports'
     wmflib::dir::mkdir_p($docroot)
 
     httpd::site { 'os-reports.wikimedia.org':
         content => template('role/apache/sites/os-reports.wikimedia.org.erb'),
+    }
+
+    ensure_packages('rsync')
+
+    systemd::timer::job { 'sync_os_reports':
+        ensure          => present,
+        description     => 'Sync OS migration reports/overview',
+        user            => 'root',
+        logging_enabled => false,
+        send_mail       => false,
+        command         => "/usr/bin/rsync rsync://${os_reports_host}/osreports/* ${docroot}",
+        interval        => {'start' => 'OnCalendar', 'interval' => '*-*-* 03:00:00'},
     }
 }
