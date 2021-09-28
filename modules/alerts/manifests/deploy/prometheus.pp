eb88b340c51f9f0e5e69553049615c2898bf88e5
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2021-09-28 11:18:28 +0200

    prometheus: deploy instance-specific alerts
    Extend alerts to take into account also instance-specific alert files.
    
    Bug: T289662
    Change-Id: I134031c22686b40a3da3cfbf62666bbeab6527b8

--- modules/alerts/manifests/deploy/prometheus.pp
+++ modules/alerts/manifests/deploy/prometheus.pp
@@ -3,6 +3,7 @@
 class alerts::deploy::prometheus(
   Stdlib::Unixpath $deploy_dir = '/srv/alerts',
   Stdlib::Unixpath $git_dir = '/srv/alerts.git',
+  Array[String] $instances = [],
 ) {
     require ::alerts
 
@@ -25,6 +26,23 @@ class alerts::deploy::prometheus(
         user        => 'alerts-deploy',
         refreshonly => true,
         notify      => Exec['reload all prometheus instances'],
+    }
+
+    # Deploy instance-specific alerts
+    $instances.each |$instance| {
+        file { "${deploy_dir}/${instance}":
+            ensure => directory,
+            owner  => 'alerts-deploy',
+            group  => 'alerts-deploy',
+            mode   => '0755',
+        }
+
+        exec { "deploy alerts for instance ${instance}":
+            command     => "/usr/local/bin/alerts-deploy --cleanup --alerts-dir ${git_dir} ${deploy_dir}/${instance} --deploy-tag ${instance}",
+            user        => 'alerts-deploy',
+            refreshonly => true,
+            notify      => Exec['reload all prometheus instances'],
+        }
     }
 
     exec { 'reload all prometheus instances':
--- modules/profile/manifests/alerts/deploy/prometheus.pp
+++ modules/profile/manifests/alerts/deploy/prometheus.pp
@@ -1,4 +1,7 @@
 # Satisfy the WMF style guide
 class profile::alerts::deploy::prometheus {
-    class { 'alerts::deploy::prometheus': }
+    class { 'alerts::deploy::prometheus':
+        instances => ['analytics', 'ext', 'k8s', 'k8s-staging',
+                      'k8s-mlserve', 'ops', 'services']
+    }
 }
