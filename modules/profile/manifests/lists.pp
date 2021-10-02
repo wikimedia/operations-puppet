d26358305fbc2e8fa6979b582b03367c6fd8564d
Amir Sarabadani <ladsgroup@gmail.com>
2021-10-02 21:42:41 +0200

    mailman: Drop mailman module and move them to profile::lists
    They are apache2 config which shouldn't be in mailman module in the
    first place.
    
    Hosts: lists1001.wikimedia.org
    Bug: T282303
    Change-Id: I992422e0f994e8a3d56d0e316356189871939016

--- modules/profile/manifests/lists.pp
+++ modules/profile/manifests/lists.pp
@@ -43,10 +43,37 @@ class profile::lists (
         memcached      => $memcached,
     }
 
-    class { 'mailman':
-        lists_servername => $lists_servername,
-        acme_chief_cert  => $acme_chief_cert,
-        renamed_lists    => $renamed_lists,
+    $ssl_settings = ssl_ciphersuite('apache', 'mid', true)
+
+    httpd::site { $lists_servername:
+        content => template('profile/lists/apache.conf.erb'),
+    }
+
+    # Add files in /var/www (docroot)
+    file { '/var/www':
+        source  => 'puppet:///modules/profile/lists/docroot/',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        recurse => 'remote',
+    }
+
+    # Not using require_package so apt::pin may be applied
+    # before attempting to install package.
+    package { 'libapache2-mod-security2':
+        ensure => present,
+    }
+
+    # Ensure that the CRS modsecurity ruleset is not used. it has not
+    # yet been tested for compatibility with our mailman instance and may
+    # cause breakage.
+    file { '/etc/apache2/mods-available/security2.conf':
+        ensure  => present,
+        source  => 'puppet:///modules/profile/lists/modsecurity/security2.conf',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['libapache2-mod-security2'],
     }
 
     mailalias { 'root': recipient => 'root@wikimedia.org' }
