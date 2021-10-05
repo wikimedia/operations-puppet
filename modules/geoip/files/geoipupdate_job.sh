312b284a75a3e85df8cdd303303dd155b4705ed7
Daniel Zahn <dzahn@wikimedia.org>
2021-10-05 16:40:48 -0700

    geoip: also limit pulling of legacy databases to CA server
    Just like I54733ddd267d489f but for the legacy database downloads.
    
    Also addresses the issue originally described in Idd9998abb63.
    
    Additionally fixes date format for logs of new job.
    
    Bug: T288844
    Change-Id: I23a376346d153d58c81eadc5a4ebcd31ec5c53ff

--- modules/geoip/files/geoipupdate_job.sh
+++ modules/geoip/files/geoipupdate_job.sh
@@ -7,5 +7,5 @@ if [[ $(hostname -f) == ${puppet_ca_server} ]]; then
     /usr/bin/printf "$(/bin/date -Iseconds): geoipupdate attempting to download MaxMind .dat files into \"${data_directory}\"" >> ${geoipupdate_log} && \
     /usr/bin/geoipupdate -f "${config_file}" -d "${data_directory}" &>> ${geoipupdate_log}
 else
-    /usr/bin/printf "$(/bin/date): geoipupdate skipping download of MaxMind .dat files into \"${data_directory}\". This is not the CA server and we sync the entire volatile dir from there separately." >> ${geoipupdate_log}
+    /usr/bin/printf "$(/bin/date -Iseconds): geoipupdate skipping download of MaxMind .dat files into \"${data_directory}\". This is not the CA server and we sync the entire volatile dir from there separately." >> ${geoipupdate_log}
 fi
--- modules/geoip/files/geoipupdate_job_legacy.sh
+++ modules/geoip/files/geoipupdate_job_legacy.sh
@@ -2,5 +2,9 @@
 set -u
 set -e
 source /etc/geoipupdate_job_legacy
-/usr/bin/printf "$(/bin/date -Iseconds): geoipupdate attempting to download MaxMind .dat files into \"${data_directory}\"" >> ${geoipupdate_log} && \
-/usr/bin/geoipupdate -f "${config_file}" -d "${data_directory}" &>> ${geoipupdate_log}
+if [[ $(hostname -f) == ${puppet_ca_server} ]]; then
+    /usr/bin/printf "$(/bin/date -Iseconds): geoipupdate attempting to download legacy MaxMind .dat files into \"${data_directory}\"" >> ${geoipupdate_log} && \
+    /usr/bin/geoipupdate -f "${config_file}" -d "${data_directory}" &>> ${geoipupdate_log}
+else
+    /usr/bin/printf "$(/bin/date -Iseconds): geoipupdate skipping download of legacy MaxMind .dat files into \"${data_directory}\". This is not the CA server and we sync the entire volatile dir from there separately." >> ${geoipupdate_log}
+fi
