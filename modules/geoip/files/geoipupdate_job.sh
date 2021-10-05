1fda28e430878ec61b9f1aa553d5fe191766f491
Daniel Zahn <dzahn@wikimedia.org>
2021-10-05 15:55:11 -0700

    geoip: use ISO-8601 date format in logs and fix log shell redirection
    - use a reasonable date format (2021-10-05T22:56:26+00:00) in log files
      -Iseconds = ISO-8601 format to second accuracy. Relatively undocumented.
    
    - fix shell redirection so that we get stdout and stderr of the update
      command but append and not overwrite the log
    
    Bug: T288844
    Change-Id: Ic0671e1b2503dddd9ebb68ce3ede8dc9b72185e1

--- modules/geoip/files/geoipupdate_job.sh
+++ modules/geoip/files/geoipupdate_job.sh
@@ -4,8 +4,8 @@ set -e
 source /etc/geoipupdate_job
 
 if [[ $(hostname -f) == ${puppet_ca_server} ]]; then
-    /usr/bin/printf "$(/bin/date): geoipupdate downloading MaxMind .dat files into \"${data_directory}\"" >> ${geoipupdate_log} && \
-    /usr/bin/geoipupdate -f "${config_file}" -d "${data_directory}" > ${geoipupdate_log}
+    /usr/bin/printf "$(/bin/date -Iseconds): geoipupdate attempting to download MaxMind .dat files into \"${data_directory}\"" >> ${geoipupdate_log} && \
+    /usr/bin/geoipupdate -f "${config_file}" -d "${data_directory}" &>> ${geoipupdate_log}
 else
     /usr/bin/printf "$(/bin/date): geoipupdate skipping download of MaxMind .dat files into \"${data_directory}\". This is not the CA server and we sync the entire volatile dir from there separately." >> ${geoipupdate_log}
 fi
