a7430a8e09bcc9eec4286c745584c53307464221
Daniel Zahn <dzahn@wikimedia.org>
2021-10-05 15:22:51 -0700

    geoip: skip pulling of maxmind files if not on the puppet CA server
    made possible by I54733ddd267d489f
    
    Now we can skip the pulling of files if we are not on the puppet
    CA server.
    
    Bug: T288844
    Change-Id: Id61893222e56ce320822e9499711b9f24e5c4d6e

--- modules/geoip/files/geoipupdate_job.sh
+++ modules/geoip/files/geoipupdate_job.sh
@@ -2,5 +2,10 @@
 set -u
 set -e
 source /etc/geoipupdate_job
-/usr/bin/printf "$(/bin/date): geoipupdate downloading MaxMind .dat files into \"${data_directory}\"" >> ${geoipupdate_log} && \
-/usr/bin/geoipupdate -f "${config_file}" -d "${data_directory}" > ${geoipupdate_log}
+
+if [[ $(hostname -f) == ${puppet_ca_server} ]]; then
+    /usr/bin/printf "$(/bin/date): geoipupdate downloading MaxMind .dat files into \"${data_directory}\"" >> ${geoipupdate_log} && \
+    /usr/bin/geoipupdate -f "${config_file}" -d "${data_directory}" > ${geoipupdate_log}
+else
+    /usr/bin/printf "$(/bin/date): geoipupdate skipping download of MaxMind .dat files into \"${data_directory}\". This is not the CA server and we sync the entire volatile dir from there separately." >> ${geoipupdate_log}
+fi
