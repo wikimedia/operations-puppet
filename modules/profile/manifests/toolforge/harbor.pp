886e0c2831225c98eaebb5717eca679b1902c87d
Brooke Storm <bstorm@wikimedia.org>
2021-10-07 17:19:26 -0700

    toolforge harbor: puppetize the install/compose config file and such
    This should help keep configuration in source control for the docker
    setup used to launch this docker registry.
    
    The docker-compose is set up with restart: always, so unless you stop
    services with a `docker-compose down`, services should start at boot
    and all is well. It's a different way of managing things, but it is
    well-documented upstream and inline with several local dev environments.
    
    Bug: T267616
    Change-Id: Ib7a4b81eef500e3fa41ffd148c4eaf953d8fbf56

--- modules/profile/manifests/toolforge/harbor.pp
+++ modules/profile/manifests/toolforge/harbor.pp
@@ -1,7 +1,37 @@
 class profile::toolforge::harbor (
+    Stdlib::Unixpath $data_volume = lookup('profile::toolforge::harbor::data_volume', {default_value => '/srv/harbor/data'}),
+    Stdlib::Unixpath $tlscert = lookup('profile::toolforge::harbor::tlscert', {default_value => '/etc/acmecerts/toolforge/live/ec-prime256v1.chained.crt'}),
+    Stdlib::Unixpath $tlskey = lookup('profile::toolforge::harbor::tlskey', {default_value => '/etc/acmecerts/toolforge/live/ec-prime256v1.key'}),
+    Boolean $cinder_attached = lookup('profile::toolforge::harbor::cinder_attached', {default_value => false}),
+    String $harbor_init_pwd = lookup('profile::toolforge::harbor::init_pwd', {default_value => 'insecurityrules'}),
+    String $harbor_db_pwd = lookup('profile::toolforge::harbor::db::harbor_pwd'),
+    Stdlib::Host $harbor_db_host = lookup('profile::toolforge::harbor::db::primary'),
+    Stdlib::Fqdn $harbor_url = lookup('profile::toolforge::harbor::url'),
 ) {
+    # Easy way to get docker and such from our repos.
     require profile::wmcs::kubeadm::client
 
+    # Useful for troubleshooting, etc.
     ensure_packages(['postgresql-client', 'redis-tools'])
+
     acme_chief::cert { 'toolforge': }
+
+    # There must be some kind of puppet fact for this?
+    if $cinder_attached {
+        file { '/srv/ops':
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0640',
+        } -> file { '/srv/ops/harbor':
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0640',
+        } -> file { '/srv/ops/harbor/harbor.yml':
+            ensure  => present,
+            mode    => '0600',
+            content => template('profile/toolforge/harbor/harbor-docker.yaml.erb'),
+        }
+    }
 }
