e4167a326baa65e8cda8ea50905c06bbdf83505d
Andrew Bogott <abogott@wikimedia.org>
2021-10-12 16:47:02 -0500

    mark_tool.py: fix password/dn swap
    Change-Id: I96c922c74291d800c4b61feeee71b21b3e0006c8

--- modules/profile/manifests/toolforge/mark_tool.pp
+++ modules/profile/manifests/toolforge/mark_tool.pp
@@ -4,8 +4,8 @@
 #  password.
 #
 class profile::toolforge::mark_tool (
-    String $novaadmin_bind_dn = lookup('profile::openstack::eqiad1::ldap_user_pass'),
-    String $novaadmin_bind_pass = lookup('profile::openstack::base::ldap_user_dn')
+    String $novaadmin_bind_dn = lookup('profile::openstack::base::ldap_user_dn'),
+    String $novaadmin_bind_pass = lookup('profile::openstack::eqiad1::ldap_user_pass')
 ) {
     file { '/etc/mark_tool.conf':
         ensure  => file,
