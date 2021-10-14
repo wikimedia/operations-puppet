e8ec1664fa9911b07f648b49080aa2e0b23e256c
Daniel Zahn <dzahn@wikimedia.org>
2021-10-14 10:29:16 -0700

    builder/systemtap: convert role::systemtap::devserver to profile
    We want to avoid roles including other roles. This is one more
    example of the few we have left by now.
    
    Nothing else includes this role and it itself does nothing besides
    including the module directly.
    
    Change-Id: Ifb15c74ba03ca02431a27226541ddf4115ecb849

---
+++ modules/profile/manifests/systemtap/devserver.pp
@@ -0,0 +1,8 @@
+# == Class: profile::systemtap::devserver
+#
+# configure a SystemTap development server
+#
+class profile::systemtap::devserver {
+
+    class {'systemtap::devserver': }
+}
--- modules/role/manifests/builder.pp
+++ modules/role/manifests/builder.pp
@@ -1,14 +1,15 @@
 # filtertags: labs-project-packaging
 class role::builder {
-    include ::profile::standard
-    include ::profile::base::firewall
-    include ::profile::package_builder
-    include ::profile::docker::storage::loopback
-    include ::profile::docker::engine
-    include ::profile::docker::builder
-    include ::profile::docker::ferm
-    include ::profile::docker::reporter
-    include role::systemtap::devserver
+
+    include profile::standard
+    include profile::base::firewall
+    include profile::package_builder
+    include profile::docker::storage::loopback
+    include profile::docker::engine
+    include profile::docker::builder
+    include profile::docker::ferm
+    include profile::docker::reporter
+    include profile::systemtap::devserver
 
     system::role { 'builder':
         description => 'Docker images builder',
