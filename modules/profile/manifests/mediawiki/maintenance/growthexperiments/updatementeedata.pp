9728ba4ad23714837ef225bea82b36fa46432c3b
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-10-16 00:42:31 +0200

    growthexperiments/updatementeedata: Run updates every three hours
    Hosts: mwmaint1002.eqiad.wmnet
    Bug: T293447
    Change-Id: I3520c21bc5605c1941bedc3a43155748144fc0d7

--- modules/profile/manifests/mediawiki/maintenance/growthexperiments/updatementeedata.pp
+++ modules/profile/manifests/mediawiki/maintenance/growthexperiments/updatementeedata.pp
@@ -3,6 +3,6 @@ define profile::mediawiki::maintenance::growthexperiments::updatementeedata() {
     # name is the DB cluster
     profile::mediawiki::periodic_job { "growthexperiments-updateMenteeData-${name}":
         command  => "/usr/local/bin/foreachwikiindblist 'growthexperiments & ${name}' extensions/GrowthExperiments/maintenance/updateMenteeData.php --statsd --dbshard ${name}",
-        interval => '*-*-* 04:15:00',
+        interval => '*-*-* 00,03,06,09,12,15,18,21:15:00',
     }
 }
