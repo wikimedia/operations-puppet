85acce5d56aa65292f35927d5bc12863fc50f5e0
Arnold Okoth <aokoth@wikimedia.org>
2021-10-15 20:03:41 +0300

    gitlab: redirect out to logfile in restore script
    Bug: T285867
    Change-Id: I94001e020807ab076f0e87da147eae4ac7f38cca

--- modules/gitlab/files/gitlab-restore.sh
+++ modules/gitlab/files/gitlab-restore.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
-echo "Running Pre-requisites..."
+LOGFILE=/var/log/gitlab-restore-backup.log
+
+echo "Running Pre-requisites..." >> $LOGFILE
 
 # Create Backup Configuration Files
 CONFIG_FILE=/etc/gitlab/gitlab.rb
 SECRETS_FILE=/etc/gitlab/gitlab-secrets.json
 
 if [ -f "$CONFIG_FILE" ] && [ -f "$SECRETS_FILE" ]; then
-    echo "Creating backup of $CONFIG_FILE and $SECRETS_FILE"
-    cp $CONFIG_FILE $CONFIG_FILE.bak
-    cp $SECRETS_FILE $SECRETS_FILE.bak
+    /usr/bin/echo "Creating backup of $CONFIG_FILE and $SECRETS_FILE" >> $LOGFILE
+    /usr/bin/cp $CONFIG_FILE $CONFIG_FILE.bak
+    /usr/bin/cp $SECRETS_FILE $SECRETS_FILE.bak
 else
-    echo "Configuration File: $CONFIG_FILE Not Found"
+    /usr/bin/echo "Configuration File: $CONFIG_FILE Not Found" >> $LOGFILE
     exit 1
 fi
 
 OLD_BACKUP_FILE=/srv/gitlab-backup/latest/latest.tar
 NEW_BACKUP_FILE=/srv/gitlab-backup/latest_gitlab_backup.tar
 if [ -f "$OLD_BACKUP_FILE" ]; then
-    echo "Moving $OLD_BACKUP_FILE to $NEW_BACKUP_FILE"
-    cp $OLD_BACKUP_FILE $NEW_BACKUP_FILE
+    echo "Moving $OLD_BACKUP_FILE to $NEW_BACKUP_FILE"  >> $LOGFILE
+    /usr/bin/cp $OLD_BACKUP_FILE $NEW_BACKUP_FILE  >> $LOGFILE
 else
-    echo "Backup File $OLD_BACKUP_FILE Not Found"
+    echo "Backup File $OLD_BACKUP_FILE Not Found"  >> $LOGFILE
     exit 1
 fi
 
@@ -29,91 +31,96 @@ fi
 CONFIG_BACKUP=/etc/gitlab/config_backup/latest/latest.tar
 
 # Change Permissions
-chmod 600 $NEW_BACKUP_FILE $CONFIG_BACKUP
+echo "changing permissions - chmod 600"  >> $LOGFILE
+/usr/bin/chmod 600 $NEW_BACKUP_FILE $CONFIG_BACKUP
 
 
 # Extract Configuration Backup
-tar -xvf $CONFIG_BACKUP --strip-components=2 -C /etc/gitlab/
+/usr/bin/tar -xvf $CONFIG_BACKUP --strip-components=2 -C /etc/gitlab/
 if [ -f $CONFIG_FILE.bak ] && [ -f "$SECRETS_FILE" ]; then
-    echo "Reverting configuration files to those of the replica..."
-    cp $CONFIG_FILE.bak $CONFIG_FILE
-    cp $SECRETS_FILE.bak $SECRETS_FILE
+    echo "Reverting configuration files to those of the replica..." >> $LOGFILE
+    /usr/bin/cp $CONFIG_FILE.bak $CONFIG_FILE
+    /usr/bin/cp $SECRETS_FILE.bak $SECRETS_FILE
 else
-    echo "Configuration backup files $CONFIG_FILE.bak $SECRETS_FILE.bak not found"
+    echo "Configuration backup files $CONFIG_FILE.bak $SECRETS_FILE.bak not found" >> $LOGFILE
     exit
 fi
 
-/usr/bin/gitlab-ctl reconfigure
+echo "running gitlab-ctl reconfigure" >> $LOGFILE
+/usr/bin/gitlab-ctl reconfigure >> $LOGFILE
 /usr/bin/gitlab-ctl status
 
 /usr/bin/systemctl stop ssh-gitlab
 if [[ $? -ne 0 ]]; then
-    echo "something went wrong stopping ssh-gitlab"
+    echo "something went wrong stopping ssh-gitlab" >> $LOGFILE
     exit 1
 fi
 
 # just a sanity check to see if the service is not running
 SSH_GITLAB_STATUS=$(/usr/bin/systemctl show -p SubState --value ssh-gitlab)
 if [ "${SSH_GITLAB_STATUS}" = "running" ]; then
-    echo "ssh-gitlab service still running. please kill it before proceeding"
+    echo "ssh-gitlab service still running. please kill it before proceeding" >> $LOGFILE
     exit 1
 fi
 
-
 if /usr/bin/gitlab-ctl stop puma && /usr/bin/gitlab-ctl stop sidekiq; then
-    echo "stopped puma & sidekiq service"
+    echo "stopped puma & sidekiq service" >> $LOGFILE
 else
-    echo "something went wrong stopping /usr/bin/gitlab-ctl services"
+    echo "something went wrong stopping /usr/bin/gitlab-ctl services" >> $LOGFILE
 fi
 
 
 /usr/bin/gitlab-ctl status | grep "down: puma" &> /dev/null
 if [ $? == 0 ]; then
-   echo "puma service stopped"
+   echo "puma service stopped" >> $LOGFILE
 else
-    echo "puma service still running"
+    echo "puma service still running" >> $LOGFILE
     exit 1
 fi
 
 /usr/bin/gitlab-ctl status | grep "down: sidekiq" &> /dev/null
 if [ $? == 0 ]; then
-   echo "sidekiq service stopped"
+   echo "sidekiq service stopped" >> $LOGFILE
 else
-    echo "sidekiq service still running"
+    echo "sidekiq service still running" >> $LOGFILE
     exit 1
 fi
 
+echo "running gitlab-backup restore"
 BACKUP=latest
-/usr/bin/gitlab-backup restore GITLAB_ASSUME_YES=1 BACKUP=$BACKUP &> /dev/null
+/usr/bin/gitlab-backup restore GITLAB_ASSUME_YES=1 BACKUP=$BACKUP >> $LOGFILE
 if [ $? == 0 ]; then
-   echo "Successfully Restored Backup: $BACKUP"
+   echo "Successfully Restored Backup: $BACKUP" >> $LOGFILE
 else
-   echo "Something Went Wrong Restoring Backup: $BACKUP"
+   echo "Something Went Wrong Restoring Backup: $BACKUP" >> $LOGFILE
    exit 1
 fi
 
+echo "running gitlab-ctl reconfigure"
 /usr/bin/gitlab-ctl reconfigure
+
+echo "running gitlab-ctl restart"
 /usr/bin/gitlab-ctl restart
 
 /usr/bin/gitlab-ctl status | grep "run: puma" &> /dev/null
 if [ $? == 0 ]; then
-   echo "puma service running"
+   echo "puma service running" >> $LOGFILE
 else
-    echo "puma service not running"
+    echo "puma service not running" >> $LOGFILE
     exit 1
 fi
 
 /usr/bin/gitlab-ctl status | grep "run: sidekiq" &> /dev/null
 if [ $? == 0 ]; then
-   echo "sidekiq service running"
+   echo "sidekiq service running" >> $LOGFILE
 else
-    echo "sidekiq service not running"
+    echo "sidekiq service not running" >> $LOGFILE
     exit 1
 fi
 
-/usr/bin/gitlab-rake gitlab:check SANITIZE=true
-/usr/bin/gitlab-rake gitlab:doctor:secrets
+/usr/bin/gitlab-rake gitlab:check SANITIZE=true >> $LOGFILE
+/usr/bin/gitlab-rake gitlab:doctor:secrets >> $LOGFILE
 
-echo "ApplicationSetting.last.update(home_page_url: 'https://gitlab-replica.wikimedia.org/explore')" | /usr/bin/gitlab-rails console
+echo "ApplicationSetting.last.update(home_page_url: 'https://gitlab-replica.wikimedia.org/explore')" | /usr/bin/gitlab-rails console >> $LOGFILE
 
 /usr/bin/systemctl restart ssh-gitlab
