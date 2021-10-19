27f4e6df3b047b85d7bcacfc93802cbeb9187bd5
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-10-19 14:25:37 +0200

    Remove obsolete pki role
    Replaced by role::pki::multirootca and role::pki::root
    
    Change-Id: I57c4d01259fd762ad298e40170f56cdd133cd911

--- modules/role/manifests/pki.pp
+++
@@ -1,8 +0,0 @@
-# @summary Role to install a PKI server
-class role::pki {
-    system::role { 'pki': description => 'PKI Server' }
-    include profile::standard
-    include profile::base::firewall
-    include profile::pki::server
-    include profile::pki::client
-}
