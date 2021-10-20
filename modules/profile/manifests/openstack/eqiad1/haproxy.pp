62fdbc3cb02129bbfd6f60c862c9e53ad0d26b59
Andrew Bogott <abogott@wikimedia.org>
2021-10-20 13:12:38 -0500

    openStack:haproxy add tls termination for openstack in eqiad1
    Bug: T267194
    Hosts: cloudcontrol1003.wikimedia.org
    Change-Id: I33030c1b114dc136b6151b42626f0ba9527e43ea

--- modules/profile/manifests/openstack/eqiad1/haproxy.pp
+++ modules/profile/manifests/openstack/eqiad1/haproxy.pp
@@ -1,4 +1,5 @@
 class profile::openstack::eqiad1::haproxy(
+    Optional[String] $acme_chief_cert_name = lookup('profile::openstack::eqiad1::haproxy::acme_chief_cert_name', {default_value => undef}),
     Array[Stdlib::Fqdn] $openstack_controllers = lookup('profile::openstack::eqiad1::openstack_controllers'),
     Array[Stdlib::Fqdn] $designate_hosts = lookup('profile::openstack::eqiad1::designate_hosts'),
     Stdlib::Port $glance_api_bind_port = lookup('profile::openstack::eqiad1::glance::api_bind_port'),
@@ -13,77 +14,100 @@ class profile::openstack::eqiad1::haproxy(
     Stdlib::Fqdn $galera_primary_host = lookup('profile::openstack::eqiad1::galera::primary_host'),
     Stdlib::Port $nova_osapi_compute_listen_port = lookup('profile::openstack::eqiad1::nova::osapi_compute_listen_port'),
 ) {
+    if $acme_chief_cert_name != undef {
+        acme_chief::cert { $acme_chief_cert_name:
+            puppet_svc => 'haproxy',
+        }
+    }
 
     profile::openstack::base::haproxy::site { 'designate':
-        servers            => $designate_hosts,
-        healthcheck_method => 'HEAD',
-        healthcheck_path   => '/',
-        port_frontend      => 9001,
-        port_backend       => 9001,
+        servers                => $designate_hosts,
+        healthcheck_method     => 'HEAD',
+        healthcheck_path       => '/',
+        port_frontend          => 9001,
+        port_backend           => 9001,
+        frontend_tls_cert_name => $acme_chief_cert_name,
+        port_frontend_tls      => 29001,
     }
 
     profile::openstack::base::haproxy::site { 'keystone_admin':
-        servers            => $openstack_controllers,
-        healthcheck_method => 'GET',
-        healthcheck_path   => '/',
-        port_frontend      => 35357,
-        port_backend       => $keystone_admin_bind_port,
+        servers                => $openstack_controllers,
+        healthcheck_method     => 'GET',
+        healthcheck_path       => '/',
+        port_frontend          => 35357,
+        port_backend           => $keystone_admin_bind_port,
+        frontend_tls_cert_name => $acme_chief_cert_name,
+        port_frontend_tls      => 25357,
     }
 
     profile::openstack::base::haproxy::site { 'keystone_public':
-        servers            => $openstack_controllers,
-        healthcheck_method => 'GET',
-        healthcheck_path   => '/',
-        port_frontend      => 5000,
-        port_backend       => $keystone_public_bind_port,
+        servers                => $openstack_controllers,
+        healthcheck_method     => 'GET',
+        healthcheck_path       => '/',
+        port_frontend          => 5000,
+        port_backend           => $keystone_public_bind_port,
+        frontend_tls_cert_name => $acme_chief_cert_name,
+        port_frontend_tls      => 25000,
     }
 
     profile::openstack::base::haproxy::site { 'glance_api':
-        servers            => $openstack_controllers,
-        healthcheck_method => 'GET',
-        healthcheck_path   => '/',
-        port_frontend      => 9292,
-        port_backend       => $glance_api_bind_port,
+        servers                => $openstack_controllers,
+        healthcheck_method     => 'GET',
+        healthcheck_path       => '/',
+        port_frontend          => 9292,
+        port_backend           => $glance_api_bind_port,
+        frontend_tls_cert_name => $acme_chief_cert_name,
+        port_frontend_tls      => 29292,
     }
 
     profile::openstack::base::haproxy::site { 'cinder_api':
-        servers            => $openstack_controllers,
-        healthcheck_method => 'GET',
-        healthcheck_path   => '/',
-        port_frontend      => 8776,
-        port_backend       => $cinder_api_bind_port,
+        servers                => $openstack_controllers,
+        healthcheck_method     => 'GET',
+        healthcheck_path       => '/',
+        port_frontend          => 8776,
+        port_backend           => $cinder_api_bind_port,
+        frontend_tls_cert_name => $acme_chief_cert_name,
+        port_frontend_tls      => 28776,
     }
 
     profile::openstack::base::haproxy::site { 'trove_api':
-        servers            => $openstack_controllers,
-        healthcheck_method => 'GET',
-        healthcheck_path   => '/',
-        port_frontend      => 8779,
-        port_backend       => $trove_api_bind_port,
+        servers                => $openstack_controllers,
+        healthcheck_method     => 'GET',
+        healthcheck_path       => '/',
+        port_frontend          => 8779,
+        port_backend           => $trove_api_bind_port,
+        frontend_tls_cert_name => $acme_chief_cert_name,
+        port_frontend_tls      => 28779,
     }
 
     profile::openstack::base::haproxy::site { 'neutron':
-        servers            => $openstack_controllers,
-        healthcheck_method => 'GET',
-        healthcheck_path   => '/',
-        port_frontend      => 9696,
-        port_backend       => $neutron_bind_port,
+        servers                => $openstack_controllers,
+        healthcheck_method     => 'GET',
+        healthcheck_path       => '/',
+        port_frontend          => 9696,
+        port_backend           => $neutron_bind_port,
+        frontend_tls_cert_name => $acme_chief_cert_name,
+        port_frontend_tls      => 29696,
     }
 
     profile::openstack::base::haproxy::site { 'nova_api':
-        servers            => $openstack_controllers,
-        healthcheck_method => 'HEAD',
-        healthcheck_path   => '/',
-        port_frontend      => 8774,
-        port_backend       => $nova_osapi_compute_listen_port,
+        servers                => $openstack_controllers,
+        healthcheck_method     => 'HEAD',
+        healthcheck_path       => '/',
+        port_frontend          => 8774,
+        port_backend           => $nova_osapi_compute_listen_port,
+        frontend_tls_cert_name => $acme_chief_cert_name,
+        port_frontend_tls      => 28774,
     }
 
     profile::openstack::base::haproxy::site { 'placement_api':
-        servers            => $openstack_controllers,
-        healthcheck_method => 'GET',
-        healthcheck_path   => '/',
-        port_frontend      => 8778,
-        port_backend       => $placement_api_bind_port,
+        servers                => $openstack_controllers,
+        healthcheck_method     => 'GET',
+        healthcheck_path       => '/',
+        port_frontend          => 8778,
+        port_backend           => $placement_api_bind_port,
+        frontend_tls_cert_name => $acme_chief_cert_name,
+        port_frontend_tls      => 28778,
     }
 
     profile::openstack::base::haproxy::site { 'nova_metadata':
