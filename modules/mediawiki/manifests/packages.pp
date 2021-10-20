d7356078deab6ee859a0cc61db8a8c0f3ef2471a
Kunal Mehta <legoktm@debian.org>
2021-10-20 10:25:22 -0700

    mediawiki: Remove tidy binary
    AFAIK this hasn't been used since the early HHVM days. We already
    no longer load the PHP tidy extension (php-tidy).
    
    Change-Id: I9c5d0ea2957def3e31ffea9950f25790add4091f

--- modules/mediawiki/manifests/packages.pp
+++ modules/mediawiki/manifests/packages.pp
@@ -7,12 +7,16 @@ class mediawiki::packages {
     ensure_packages([
         'fluidsynth', 'fluid-soundfont-gs', 'fluid-soundfont-gm', 'firejail',
         # vips is needed for (rare) non-Thumbor scaling of TIFF/PNG uploads (T199938)
-        'tidy', 'libvips-tools',
+        'libvips-tools',
         # PDF and DjVu
         'ghostscript', 'djvulibre-bin', 'librsvg2-bin', 'libtiff-tools', 'poppler-utils',
         # SecurePoll is incompatible with gpg2 (T209802)
         'gnupg1',
     ])
+
+    package {'tidy':
+        ensure => absent
+    }
 
     # Used by captcha.py from ConfirmEdit extension (used to generate captchas)
     ensure_packages(['python-pil', 'python3-pil'])
