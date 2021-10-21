af88a796ad85595d831c607527abb2c90e8cb893
Cole White <cwhite@wikimedia.org>
2021-10-21 16:39:23 -0600

    profile: add enable_relay flag to statsd exporter profile
    StatsD Exporter for MediaWiki will not need relay functionality.  Add a
    flag that allows us to turn off the relay without undefining the statsd
    global hiera key.
    
    Bug: T240685
    Change-Id: I62e3453e67e6acfa507d768607ea1dc2b1e80a1f

--- modules/profile/manifests/prometheus/statsd_exporter.pp
+++ modules/profile/manifests/prometheus/statsd_exporter.pp
@@ -1,12 +1,19 @@
 class profile::prometheus::statsd_exporter (
     Array[Stdlib::Host] $prometheus_nodes = lookup('prometheus_nodes'),
     Array[Hash]         $mappings         = lookup('profile::prometheus::statsd_exporter::mappings'),
+    Boolean             $enable_relay     = lookup('profile::prometheus::statsd_exporter::enable_relay', { 'default_value' => true }),
     String              $relay_address    = lookup('statsd'),
 ){
 
+    if $enable_relay {
+        $relay_addr = $relay_address
+    } else {
+        $relay_addr = ''
+    }
+
     class { '::prometheus::statsd_exporter':
         mappings      => $mappings,
-        relay_address => $relay_address,
+        relay_address => $relay_addr,
     }
 
     $prometheus_ferm_nodes = join($prometheus_nodes, ' ')
