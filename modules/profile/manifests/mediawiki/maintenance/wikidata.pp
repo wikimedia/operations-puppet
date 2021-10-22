9d2e4d7e5eb2d69153afe9da215b97c12ba265b7
Michael Große <michael.grosse@wikimedia.de>
2021-10-22 15:28:45 +0200

    Regularly resubmit changes that might be stuck in wb_changes
    Changes getting stuck in wb_changes should happen only rarely, but it
    _can_ happen, for example if the JobQueue refuses to accept new jobs for
    an amount of time for any reason.
    
    It only runs every hour and only resubmits jobs older than 6 hours to
    prevent jobs from being resubmitted too often again.
    
    Bug: T294031
    Change-Id: I12e3d8bdc22bf0e654e30a3309e3f4e4cb6cb478

--- modules/profile/manifests/mediawiki/maintenance/wikidata.pp
+++ modules/profile/manifests/mediawiki/maintenance/wikidata.pp
@@ -1,6 +1,12 @@
 class profile::mediawiki::maintenance::wikidata {
     require profile::mediawiki::common
 
+    # Resubmit changes in wb_changes that are older than 6 hours
+    profile::mediawiki::periodic_job { 'wikidata_resubmit_changes_for_dispatch':
+        command  => '/usr/local/bin/mwscript extensions/Wikibase/repo/maintenance/ResubmitChanges.php --wiki wikidatawiki --minimum-age 21600',
+        interval => '*-*-* *:39:00',
+    }
+
     # Update the cached query service maxlag value every minute
     # We don't need to ensure present/absent as the wrapper will ensure nothing
     # is run unless we're in the master dc
