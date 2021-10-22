eab1b26ed30c4b1ba5c785e623b744438e449224
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-10-22 09:44:44 +0200

    Set ganeti2025/2026 to insetup
    Bug: T282603
    Change-Id: I16b9f236a3dca92afda459f8b6c81b2f8cbbd29c

--- manifests/site.pp
+++ manifests/site.pp
@@ -1315,9 +1315,9 @@ node /^ganeti20(0[7-9]|1[0-9]|2[0-4])\.codfw\.wmnet$/ {
     role(ganeti)
 }
 
-# Temporary test cluster until T284954 arrives
+# To be added to the codfw cluster once Buster upgrade completed
 node /^(ganeti2025|ganeti2026)\.codfw\.wmnet$/ {
-    role(ganeti_test)
+    role(insetup)
 }
 
 # New Test cluster for codfw T286484
