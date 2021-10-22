1d3ac15b37e708b77cf96c7a6629faaf280bf6b8
Antoine Musso <hashar@free.fr>
2021-10-22 17:48:06 +0200

    zuul: double git-daemon max connections 48 -> 96
    There are still some "Too many children, dropping connection" messages
    on the Zuul merger git-daemon.
    
    Bug: T222661
    Change-Id: I76852814ed55227bc1f59ab466c5130057f6bf31

--- modules/profile/manifests/zuul/merger.pp
+++ modules/profile/manifests/zuul/merger.pp
@@ -36,7 +36,7 @@ class profile::zuul::merger(
     # Serves Zuul git repositories
     class { 'contint::zuul::git_daemon':
         zuul_git_dir    => $conf_merger['git_dir'],
-        max_connections => 48,
+        max_connections => 96,
     }
 
     # We run a git-daemon process to expose the zuul-merger git repositories.
