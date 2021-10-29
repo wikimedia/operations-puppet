f74fc1f25fb8b257a4d72d41e860ac0e2c6a1e6a
jbond <jbond@wikimedia.org>
2021-10-29 11:52:50 +0100

    P:docker::storage: Add defaults for physical_volumes and vg_to_remove
    Hosts: P:docker::storage
    Bug: T294517
    Change-Id: I072475d2f8e2b433111aa2f3ec56b8ab2911afce

--- modules/profile/manifests/docker/storage.pp
+++ modules/profile/manifests/docker/storage.pp
@@ -5,15 +5,17 @@
 #
 class profile::docker::storage(
     # list of physical volumes to use.
-    $physical_volumes = lookup('profile::docker::storage::physical_volumes'),
+    Optional[String] $physical_volumes = lookup('profile::docker::storage::physical_volumes'),
     # Volume group to substitute.
-    $vg_to_remove = lookup('profile::docker::storage::vg_to_remove'),
+    Optional[String] $vg_to_remove     = lookup('profile::docker::storage::vg_to_remove'),
 ) {
     # Size of the thin pool and the metadata pool.
     $extents = '95%VG'
     $metadata_size = '5%VG'
 
-    Class['::profile::docker::storage'] -> Service['docker']
+    if defined(Service['docker']) {
+        Class['profile::docker::storage'] -> Service['docker']
+    }
 
     if ($vg_to_remove  and ! empty($vg_to_remove)) {
         volume_group { $vg_to_remove:
@@ -39,17 +41,19 @@ class profile::docker::storage(
         'metadata' => $metadata_lv_params,
     }
 
-    $volume_group = {
-        docker => {
-            ensure           => present,
-            physical_volumes => $physical_volumes,
-            logical_volumes  => $logical_volumes,
-        },
-    }
+    if $physical_volumes {
+        $volume_group = {
+            docker => {
+                ensure           => present,
+                physical_volumes => $physical_volumes,
+                logical_volumes  => $logical_volumes,
+            },
+        }
 
-    class { '::lvm':
-        manage_pkg    => true,
-        volume_groups => $volume_group,
+        class { 'lvm':
+            manage_pkg    => true,
+            volume_groups => $volume_group,
+        }
     }
 
     # This will be used in profile::docker::engine
