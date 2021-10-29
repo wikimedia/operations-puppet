c170d7ee429b1e7dc018d2df1b15a2bf79daff02
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2021-10-29 12:59:16 +0200

    sslcert: introduce ca_deselect_dstx3
    Introduce this class to blacklist the DST_Root_CA_X3 CA certificate.
    Interested users can declare this class in their individual profiles.
    
    Based on https://gerrit.wikimedia.org/r/c/operations/puppet/+/725331/ by Brandon Black.
    
    Bug: T292291
    Bug: T292289
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: I6be89bb3e00a1500bd3caf1637fef5f92bf2efd3

---
+++ modules/sslcert/manifests/ca_deselect_dstx3.pp
@@ -0,0 +1,17 @@
+# Envoy's BoringSSL gets confused by the Let's Encrypt root cross-signing
+# hack around the expired DST Root CA X3, and the easy fix is to deselect
+# the expired cert from the ca-certificates configuration.
+#
+class sslcert::ca_deselect_dstx3 {
+    include sslcert
+
+    file_line { 'deselect_dst_root_ca_x3':
+        path               => '/etc/ca-certificates.conf',
+        match              => '^!?mozilla/DST_Root_CA_X3\.crt$',
+        line               => '!mozilla/DST_Root_CA_X3.crt',
+        append_on_no_match => false,
+        # These are in the sslcert init.pp:
+        notify             => Exec['update-ca-certificates'],
+        require            => Package['ca-certificates'],
+    }
+}
