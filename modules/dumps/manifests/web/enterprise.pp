78d246bf3cde87e444890734c3c619c36bb3b374
Ariel T. Glenn <ariel@wikimedia.org>
2021-11-03 13:08:48 +0200

    fix up name of enterprise dumps downloader script
    Bug: T273585
    Change-Id: Ia77c0821b68252f73f13e72f9fec9714e7daf7e3

--- modules/dumps/manifests/web/enterprise.pp
+++ modules/dumps/manifests/web/enterprise.pp
@@ -1,11 +1,11 @@
 class dumps::web::enterprise {
-    file { '/usr/local/bin/wm_enterprise_html.py':
+    file { '/usr/local/bin/wm_enterprise_downloader.py':
         ensure => 'present',
-        path   => '/usr/local/bin/wm_enterprise_html.py',
+        path   => '/usr/local/bin/wm_enterprise_downloader.py',
         mode   => '0644',
         owner  => 'root',
         group  => 'root',
-        source => 'puppet:///modules/dumps/web/wm_enterprise_html.py',
+        source => 'puppet:///modules/dumps/web/wm_enterprise_downloader.py',
     }
 
     # the systemd timer to run the content pull will go here later.
