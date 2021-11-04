380ba697ce37c9e2eef302ae8c35e21b49df00eb
Luca Toscano <ltoscano@wikimedia.org>
2021-11-04 14:20:43 +0100

    Add sslcert::trusted_root_ca
    This is a little helper to deploy the chained certificate
    of the WMF's interal root CAs (PKI and Puppet).
    The define by default deploys the .pem file in a defined location,
    and it optionally create a .p12 truststore file containing it
    (to be used for various use cases, for example JVM-based TLS
    deployments).
    The .jks truststore is not provided yet since most of the Java
    systems that we run accept .p12 files. It is also nice to provide
    a dependency to openssl only, rather than openjdk (to have
    the keytool command).
    
    Bug: T291905
    Change-Id: I2639ed2015643a66c54d3c1d863f430a4fe2a682

---
+++ modules/sslcert/manifests/trusted_ca.pp
@@ -0,0 +1,60 @@
+# @summary Deploy a .pem file containing the WMF's internal Root CA crts.
+#          Create a .p12 truststore if needed.
+# @param trusted_root_ca_source the puppet source location of the .pem file
+# @param trusted_ca_path the location on the fs where to deploy the .pem file
+# @param p12_truststore_path location on the fs where to create the .p12 truststore
+# @param owner user set as owner of the files to be created
+# @param group group set as group-owner of the files to be created
+define sslcert::trusted_ca (
+    Wmflib::Ensure             $ensure                 = 'present',
+    String                     $trusted_root_ca_source = 'puppet:///modules/sslcert/trusted_root_ca.pem',
+    Stdlib::Unixpath           $trusted_ca_path        = '/etc/ssl/localcerts/trusted_root_ca.pem',
+    String                     $truststore_password    = '',
+    String                     $owner                  = 'root',
+    String                     $group                  = 'root',
+    Optional[Stdlib::Unixpath] $p12_truststore_path    = undef,
+) {
+    ensure_packages(['openssl'])
+
+    # Deploy the WMF internal/trusted CAs as bundle .pem file.
+    file { $trusted_ca_path:
+        ensure => $ensure,
+        owner  => $owner,
+        group  => $group,
+        mode   => '0444',
+        source => $trusted_root_ca_source,
+    }
+
+    # If a truststore is needed (for example, for JVM-related TLS settings)
+    # create a .p12 file from the above .pem file.
+    if $p12_truststore_path {
+        file { $p12_truststore_path:
+            ensure => $ensure,
+            owner  => $owner,
+            group  => $group,
+            mode   => '0444',
+        }
+        $create_pkcs12_command = @("CREATE_PKCS12_COMMAND"/L)
+            /usr/bin/openssl pkcs12 -export \
+            -nokeys -in ${trusted_ca_path} -out ${p12_truststore_path} \
+            -password 'pass:${truststore_password}'
+            |- CREATE_PKCS12_COMMAND
+
+        $check_certificates_match = @("CHECK_CERTIFICATES_MATCH_COMMAND"/L)
+            /usr/bin/test \
+                "$(/usr/bin/openssl x509 -in ${trusted_ca_path})" == \
+                "$(/usr/bin/openssl pkcs12 \
+                  -password 'pass:${truststore_password}' \
+                  -in ${trusted_ca_path} -nokeys | openssl x509)"
+            |- CHECK_CERTIFICATES_MATCH_COMMAND
+
+        if $ensure == 'present' {
+            exec {"sslcert generate ${p12_truststore_path}":
+                command => $create_pkcs12_command,
+                unless  => $check_certificates_match,
+                require => Package['openssl'],
+                before  => File[$p12_truststore_path],
+            }
+        }
+    }
+}
