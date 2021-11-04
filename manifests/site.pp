41c8b9df8662a0859c2a17d459cde898df0fd482
Taavi Väänänen <hi@taavi.wtf>
2021-11-04 13:17:06 +0200

    Re-assign role to cloudcephosd1020
    Change-Id: I84a6e453e33cff17a925360ec30a1e5db0e9bb03

--- manifests/site.pp
+++ manifests/site.pp
@@ -1581,7 +1581,7 @@ node /^cloudcephmon100[1-3]\.eqiad\.wmnet$/ {
 }
 
 # ceph storage nodes
-node /^cloudcephosd10(0[1-9]|1[0-9]|2[1-4])\.eqiad\.wmnet$/ {
+node /^cloudcephosd10(0[1-9]|1[0-9]|2[0-4])\.eqiad\.wmnet$/ {
     role(wmcs::ceph::osd)
 }
 
