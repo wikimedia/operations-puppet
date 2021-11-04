64e1f82caea93c0dea67beef4ee16c2195aac396
Keith Herron <kherron@wikimedia.org>
2021-11-04 14:02:37 -0400

    role::elasticsearch::cirrus: ship ES logs via gelf_relay
    Bug: T288620
    Change-Id: If1a42c7341e7b51029ad4b4692c287b73c5e5d71
    Hosts: re:elastic.*.wmnet

--- modules/role/manifests/elasticsearch/cirrus.pp
+++ modules/role/manifests/elasticsearch/cirrus.pp
@@ -7,11 +7,7 @@ class role::elasticsearch::cirrus {
     include ::profile::base::firewall
     include ::profile::lvs::realserver
     include ::profile::elasticsearch::cirrus
-
-    # limit initial rollout of gelf_relay to elastic1049
-    if $::hostname == 'elastic1049' {
-        include ::profile::logstash::gelf_relay
-    }
+    include ::profile::logstash::gelf_relay
 
     system::role { 'elasticsearch::cirrus':
         ensure      => 'present',
