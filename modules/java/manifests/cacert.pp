49eff9621eb50ad0cdbb72118c7444d65220b8f2
Luca Toscano <ltoscano@wikimedia.org>
2021-11-05 12:31:44 +0100

    sslcert::trusted_ca: add jks truststore
    It seems that Kafka doesn't like .p12 truststores,
    so this change adds the option to create a .jks as well.
    
    To avoid duplicating code, the java::cacert define is
    modified to allow to specify a truststore to create
    (rather than using the default system one).
    
    Change-Id: Ib67f81424ef4d0b631a979e1526c490d3db0243f

--- modules/java/manifests/cacert.pp
+++ modules/java/manifests/cacert.pp
@@ -1,19 +1,30 @@
-# @summary a define to add a certificate to the java truststore
+# @summary a define to add a certificate to the system java truststore
+# or to a custom one.
 # @param path the location of the CA pem file to add to the truststore
 # @param storepass the keystore password
+# @param keystore_path optional, the keystore to create (instead of using
+# the system one).
 define java::cacert (
-    Stdlib::Unixpath $path,
-    Wmflib::Ensure   $ensure    = 'present',
-    String           $storepass = 'changeit',
+    Stdlib::Unixpath           $path,
+    Wmflib::Ensure             $ensure        = 'present',
+    String                     $storepass     = 'changeit',
+    Optional[Stdlib::Unixpath] $keystore_path = undef,
 ) {
     include java
-    $keystore = $java::default_java_package['version'] ? {
-        '7'     => '-keystore /etc/ssl/certs/java/cacerts',
-        '8'     => '-keystore /etc/ssl/certs/java/cacerts',
-        default => '-cacerts',
+
+    if $keystore_path != undef {
+        $keystore = $keystore_path
+        $trust_cacert = ''
+    } else {
+        $keystore = $java::default_java_package['version'] ? {
+            '7'     => '-keystore /etc/ssl/certs/java/cacerts',
+            '8'     => '-keystore /etc/ssl/certs/java/cacerts',
+            default => '-cacerts',
+        }
+        $trust_cacert = '-trustcacerts'
     }
     $import_cmd = @("IMPORT"/L)
-        /usr/bin/keytool -import -trustcacerts -noprompt ${keystore} \
+        /usr/bin/keytool -import ${trust_cacert} -noprompt ${keystore} \
             -file ${path} -storepass ${storepass} -alias ${title}
         | IMPORT
     $delete_cmd = "/usr/bin/keytool -delete ${keystore} -noprompt -storepass ${storepass} -alias ${title}"
--- modules/sslcert/manifests/trusted_ca.pp
+++ modules/sslcert/manifests/trusted_ca.pp
@@ -3,6 +3,7 @@
 # @param trusted_root_ca_source the puppet source location of the .pem file
 # @param trusted_ca_path the location on the fs where to deploy the .pem file
 # @param p12_truststore_path location on the fs where to create the .p12 truststore
+# @param jks_truststore_path location on the fs where to create the .jks truststore
 # @param owner user set as owner of the files to be created
 # @param group group set as group-owner of the files to be created
 define sslcert::trusted_ca (
@@ -13,9 +14,8 @@ define sslcert::trusted_ca (
     String                     $owner                  = 'root',
     String                     $group                  = 'root',
     Optional[Stdlib::Unixpath] $p12_truststore_path    = undef,
+    Optional[Stdlib::Unixpath] $jks_truststore_path    = undef,
 ) {
-    ensure_packages(['openssl'])
-
     # Deploy the WMF internal/trusted CAs as bundle .pem file.
     file { $trusted_ca_path:
         ensure => $ensure,
@@ -25,9 +25,9 @@ define sslcert::trusted_ca (
         source => $trusted_root_ca_source,
     }
 
-    # If a truststore is needed (for example, for JVM-related TLS settings)
-    # create a .p12 file from the above .pem file.
+    # If a PKCS12 truststore is needed, create a .p12 file from the above .pem file.
     if $p12_truststore_path {
+        ensure_packages(['openssl'])
         file { $p12_truststore_path:
             ensure => $ensure,
             owner  => $owner,
@@ -55,6 +55,23 @@ define sslcert::trusted_ca (
                 require => Package['openssl'],
                 before  => File[$p12_truststore_path],
             }
+        }
+    }
+
+    # Same thing if a jks truststore is needed. In this case, since the keytool
+    # command is needed, Java dependencies will be deployed as well.
+    if $jks_truststore_path {
+        file { $jks_truststore_path:
+            ensure => $ensure,
+            owner  => $owner,
+            group  => $group,
+            mode   => '0444',
+        }
+        java::cacert { $title:
+            ensure        => $ensure,
+            path          => $trusted_ca_path,
+            storepass     => $truststore_password,
+            keystore_path => $jks_truststore_path,
         }
     }
 }
