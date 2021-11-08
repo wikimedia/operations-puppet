f5ab39ddfb2fa2583022a816d599e388dda7f857
David Caro <dcaro@wikimedia.org>
2021-11-08 11:36:33 +0100

    Add codfw cloudvirts ceph::auth::deploy profile
    Bug: T293752
    Change-Id: I4ec9124a28fb385db5c94610c2ee32395a6340e1
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- modules/role/manifests/wmcs/openstack/codfw1dev/virt_ceph.pp
+++ modules/role/manifests/wmcs/openstack/codfw1dev/virt_ceph.pp
@@ -2,12 +2,13 @@
 #
 class role::wmcs::openstack::codfw1dev::virt_ceph {
     system::role { $name: }
-    include ::profile::base::production
-    # include ::profile::base::firewall
-    include ::profile::ceph::client::rbd_libvirt
-    include ::profile::openstack::codfw1dev::clientpackages
-    include ::profile::openstack::codfw1dev::observerenv
-    include ::profile::openstack::codfw1dev::nova::common
-    include ::profile::openstack::codfw1dev::nova::compute::service
-    include ::profile::openstack::codfw1dev::envscripts
+    include profile::base::production
+    # include profile::base::firewall
+    include profile::ceph::client::rbd_libvirt
+    include profile::openstack::codfw1dev::clientpackages
+    include profile::openstack::codfw1dev::observerenv
+    include profile::openstack::codfw1dev::nova::common
+    include profile::openstack::codfw1dev::nova::compute::service
+    include profile::openstack::codfw1dev::envscripts
+    include profile::ceph::auth::deploy
 }
