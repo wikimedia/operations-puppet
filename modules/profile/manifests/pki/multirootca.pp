b40b5d23dcb908128a34b13ecaf61fa0ffcee1e8
jbond <jbond@wikimedia.org>
2021-11-15 13:42:02 +0100

    P:multirootca: dont include the root CA
    Change-Id: I824f5d8d7381065945bfe3ac0860fd0e4000090b

--- modules/profile/manifests/pki/multirootca.pp
+++ modules/profile/manifests/pki/multirootca.pp
@@ -146,7 +146,7 @@ class profile::pki::multirootca (
             owner   => 'root',
             group   => 'root',
             mode    => '0444',
-            content => "${root_ca_content}${int_ca_content}",
+            content => $int_ca_content,
         }
         $memo + {
             $safe_title => {
