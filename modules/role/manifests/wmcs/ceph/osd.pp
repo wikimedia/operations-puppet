3ce8299b2247b5759b6975d923fdb86cfacfba9b
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2021-11-15 19:20:23 +0100

    cloud: ceph: reorder osd/auth profile declaration
    So the osd code sees it in the catalog when is evaluated.
    
    Bug: T293752
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: Ib6f577c627c1927fad195d1b743ce3aa0f3a5853

--- modules/role/manifests/wmcs/ceph/osd.pp
+++ modules/role/manifests/wmcs/ceph/osd.pp
@@ -2,6 +2,6 @@ class role::wmcs::ceph::osd {
     system::role { $name: description => 'Ceph Object Storage Daemon server.' }
     include ::profile::base::production
     include ::profile::base::firewall
-    include ::profile::ceph::osd
     include profile::ceph::auth::deploy
+    include profile::ceph::osd
 }
