bd259ae2fca4d01097acbbfcb73006aa5515f7fd
Andrew Bogott <abogott@wikimedia.org>
2021-11-16 13:06:40 -0600

    disable_tool: add ldap uri to the config file
    This will need a corresponding change in the disable_tool repo
    to make use of this entry.
    
    Bug: T170355
    Change-Id: If30c92a1c034f743a61d80d2e1b9abf5a67450f6

--- modules/profile/manifests/toolforge/disable_tool.pp
+++ modules/profile/manifests/toolforge/disable_tool.pp
@@ -1,7 +1,10 @@
 class profile::toolforge::disable_tool (
+    Hash   $ldap_config = lookup('ldap', Hash, hash, {}),
     String $novaadmin_bind_dn = lookup('profile::openstack::base::ldap_user_dn'),
     String $novaadmin_bind_pass = lookup('profile::openstack::eqiad1::ldap_user_pass')
+
 ) {
+    $ldap_uri = "ldap://${ldap_config['rw-server']}:389"
     file { '/etc/disable_tool.conf':
         ensure  => file,
         owner   => 'root',
