9d3ebd42e69e96f9863eb73367416bf79fc996c2
Taavi Väänänen <hi@taavi.wtf>
2021-11-23 20:19:46 +0200

    Delete roles for bare metal WMCS puppet masters
    These are unused.
    
    Change-Id: I91872c4e7e1ab77a0806000d47f102e9e4229c24

--- modules/role/manifests/wmcs/openstack/codfw1dev/puppetmaster/frontend.pp
+++
@@ -1,8 +0,0 @@
-class role::wmcs::openstack::codfw1dev::puppetmaster::frontend {
-    system::role { $name: }
-    include ::profile::base::production
-    include ::profile::openstack::codfw1dev::clientpackages
-    include ::profile::openstack::codfw1dev::observerenv
-    include ::profile::openstack::codfw1dev::puppetmaster::frontend
-    include ::profile::openstack::base::optional_firewall
-}
--- modules/role/manifests/wmcs/openstack/eqiad1/puppetmaster/frontend.pp
+++
@@ -1,8 +0,0 @@
-class role::wmcs::openstack::eqiad1::puppetmaster::frontend {
-    system::role { $name: }
-    include ::profile::base::production
-    include ::profile::openstack::eqiad1::clientpackages
-    include ::profile::openstack::eqiad1::observerenv
-    include ::profile::openstack::eqiad1::puppetmaster::frontend
-    include ::profile::openstack::base::optional_firewall
-}
