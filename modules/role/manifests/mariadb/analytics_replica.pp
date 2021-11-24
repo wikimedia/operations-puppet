78390293441361f1e0869c6e2db35202dca1a538
Jcrespo <jcrespo@wikimedia.org>
2021-11-24 16:23:29 +0100

    mariadb: Remove :: from profile setup on 2 mariadb roles
    As per Moritz suggestion.
    
    Bug: T296285
    Change-Id: I1d3e69e7555e9c3795107ef3d617cc816364adb2

--- modules/role/manifests/mariadb/analytics_replica.pp
+++ modules/role/manifests/mariadb/analytics_replica.pp
@@ -3,8 +3,8 @@ class role::mariadb::analytics_replica {
         description => 'MariaDB server containing replicas of mediawiki databases for analytics & research usage',
     }
 
-    include ::profile::base::production
-    include ::profile::base::firewall
+    include profile::base::production
+    include profile::base::firewall
 
-    include ::profile::mariadb::dbstore_multiinstance
+    include profile::mariadb::dbstore_multiinstance
 }
--- modules/role/manifests/mariadb/backup_source.pp
+++ modules/role/manifests/mariadb/backup_source.pp
@@ -3,8 +3,8 @@ class role::mariadb::backup_source {
         description => 'MariaDB server containing replicas of mediawiki databases used to generate backups',
     }
 
-    include ::profile::base::production
-    include ::profile::base::firewall
+    include profile::base::production
+    include profile::base::firewall
 
-    include ::profile::mariadb::dbstore_multiinstance
+    include profile::mariadb::dbstore_multiinstance
 }
