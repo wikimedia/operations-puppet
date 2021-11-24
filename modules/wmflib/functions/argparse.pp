26f2254a8895d5ab200c768e9be59f21c64f4937
jbond <jbond@wikimedia.org>
2021-11-24 11:51:54 +0100

    wmflib::argpasre: parse args to command string
      @example
       wmflib::argparse({'hostname' => 'foo.example.org', 'port' => 8080, 'ssl' => true}) =>
        '--hostname foo.example.org --port 8080 --ssl'
    
    Change-Id: Id7dd2e095746339e35848d5d3893c47fa36eec29

---
+++ modules/wmflib/functions/argparse.pp
@@ -0,0 +1,18 @@
+# @summary take a hash of key value parse and return an argument string
+# @example
+#  wmflib::argparse({'hostname' => 'foo.example.org', 'port' => 8080, 'ssl' => true}) =>
+#   '--hostname foo.example.org --port 8080 --ssl'
+#
+function wmflib::argparse (
+    Hash   $args,
+    String $prefix = '',
+) >> String {
+    $args.reduce($prefix) |$memo, $value| {
+        $args_str = $value[1] ? {
+            Boolean => $value[1].bool2str(" --${value[0]}", ''),
+            Array   => " --${value[0]} ${value[1].join(',')}",
+            default => " --${value[0]} ${value[1]}",
+        }
+        "${memo}${args_str}".strip
+    }
+}
