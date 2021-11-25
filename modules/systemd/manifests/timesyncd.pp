ce5ba87692bb0dde7b4189e01a1e54eae5a3a31a
David Caro <dcaro@wikimedia.org>
2021-11-25 10:51:56 +0100

    timesyncd: handle bullseye ntp hosts
    systemd-timesyncd became a package of it's own (split from systemd),
    so handling of the associated service has to be linked to that package
    when that package is there, otherwise continues the same way.
    This is not a problem until you want timesyncd disabled (ex. ntp
    hosts).
    
    Bug: T296456
    Change-Id: I07f761e2a8c2abeac222c7047a0986b568b1979c
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- modules/systemd/manifests/timesyncd.pp
+++ modules/systemd/manifests/timesyncd.pp
@@ -8,6 +8,12 @@ class systemd::timesyncd (
     # only purge ntp if we are ensuring timesyncd
     if $ensure == 'present' {
         ensure_packages(['ntp'], {'ensure' => 'purged'})
+        # On systemd >247.3-6~bpo10+1 at least this service is handled by the systemd-timesyncd package
+        # before it was installed by systemd
+        if (debian::codename::ge('bullseye')) {
+            ensure_packages(['systemd-timesyncd'])
+            Package['systemd-timesyncd'] -> File['/etc/systemd/timesyncd.conf']
+        }
     }
 
     file { '/etc/systemd/timesyncd.conf':
@@ -16,11 +22,17 @@ class systemd::timesyncd (
         owner   => 'root',
         group   => 'root',
         content => template('systemd/timesyncd.conf.erb'),
-        notify  => Service['systemd-timesyncd'],
     }
 
-    service { 'systemd-timesyncd':
-        ensure => stdlib::ensure($ensure, 'service'),
-        enable => true,
+    # Ideally we could just rely on stdlib::ensure below. However this causes an error when:
+    # * ensure => 'absent'
+    # * debian::codename::ge bullseye
+    # As in this case there is no systemd-timesync service available to manage.
+    if (!defined(Package['systemd-timesyncd']) or $ensure == 'present') {
+        service { 'systemd-timesyncd':
+            ensure => stdlib::ensure($ensure, 'service'),
+            enable => true,
+        }
+        File['/etc/systemd/timesyncd.conf'] ~> Service['systemd-timesyncd']
     }
 }
