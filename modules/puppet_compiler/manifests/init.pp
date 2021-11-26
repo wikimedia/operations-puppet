9caebc4e435c007663bbe494c3a1aa890be576f8
jbond <jbond@wikimedia.org>
2021-11-26 15:22:11 +0100

    puppet_compiler: mkdir_p workdir not vardir
    Change-Id: I4a50c51082089e85508c4ccc84b30c12cafdfaec

--- modules/puppet_compiler/manifests/init.pp
+++ modules/puppet_compiler/manifests/init.pp
@@ -22,103 +22,103 @@ class puppet_compiler(
         }
 
 
-        wmflib::dir::mkdir_p($libdir, {
+    wmflib::dir::mkdir_p($libdir, {
+        ensure => stdlib::ensure($ensure, 'directory'),
+        owner  => $user,
+        mode   => '0644',
+        recurse => true,
+    })
+    wmflib::dir::mkdir_p($workdir, {
+        ensure => stdlib::ensure($ensure, 'directory'),
+        owner  => $user,
+        mode   => '0644',
+    })
+    file{
+        default:
             ensure => stdlib::ensure($ensure, 'directory'),
             owner  => $user,
-            mode   => '0644',
-            recurse => true,
-            })
-            wmflib::dir::mkdir_p($vardir, {
-                ensure => stdlib::ensure($ensure, 'directory'),
-                owner  => $user,
-                mode   => '0644',
-                })
-                file{
-                    default:
-                        ensure => stdlib::ensure($ensure, 'directory'),
-                        owner  => $user,
-                        mode   => '0644';
-                    $workdir: ;
-                $yamldir:
-                    recurse => true;
-                }
+            mode   => '0644';
+        $vardir: ;
+    $yamldir:
+        recurse => true;
+    }
 
-                if $ensure == 'present' {
-                    class { 'puppet_compiler::setup':
-                        user    => $user,
-                        vardir  => $vardir,
-                        homedir => $homedir,
-                    }
-                }
+    if $ensure == 'present' {
+        class { 'puppet_compiler::setup':
+            user    => $user,
+            vardir  => $vardir,
+            homedir => $homedir,
+        }
+    }
 
-                file { '/usr/local/bin/sshknowngen':
-                    ensure => absent,
-                }
-                # We don't really need some generators from puppet master, link them to
-                # /bin/true
-                file { '/usr/local/bin/naggen2':
-                    ensure => stdlib::ensure($ensure, 'link'),
-                    target => '/bin/true',
-                }
+    file { '/usr/local/bin/sshknowngen':
+        ensure => absent,
+    }
+    # We don't really need some generators from puppet master, link them to
+    # /bin/true
+    file { '/usr/local/bin/naggen2':
+        ensure => stdlib::ensure($ensure, 'link'),
+        target => '/bin/true',
+    }
 
-                class {'puppet_compiler::web':
-                    ensure => $enable_web.bool2str('present', 'absent')
-                }
+    class {'puppet_compiler::web':
+        ensure => $enable_web.bool2str('present', 'absent')
+    }
 
-                ## Git cloning
+    ## Git cloning
 
-                # Git clone of the puppet repo
-                git::clone { 'operations/puppet':
-                    ensure    => $ensure,
-                    directory => "${libdir}/production",
-                    owner     => $user,
-                    mode      => '0755',
-                    require   => File[$libdir],
-                }
+    # Git clone of the puppet repo
+    git::clone { 'operations/puppet':
+        ensure    => $ensure,
+        directory => "${libdir}/production",
+        owner     => $user,
+        mode      => '0755',
+        require   => File[$libdir],
+    }
 
-                # Git clone labs/private
-                git::clone { 'labs/private':
-                    ensure    => $ensure,
-                    directory => "${libdir}/private",
-                    owner     => $user,
-                    mode      => '0755',
-                    require   => File[$libdir],
-                }
+    # Git clone labs/private
+    git::clone { 'labs/private':
+        ensure    => $ensure,
+        directory => "${libdir}/private",
+        owner     => $user,
+        mode      => '0755',
+        require   => File[$libdir],
+    }
 
-                $compiler_dir = "${libdir}/compiler"
-                # Git clone the puppet compiler, install it
-                git::install { 'operations/software/puppet-compiler':
-                    ensure    => $ensure,
-                    git_tag   => $version,
-                    directory => $compiler_dir,
-                    owner     => $user,
-                    notify    => Exec['install compiler'],
-                }
+    $compiler_dir = "${libdir}/compiler"
+    # Git clone the puppet compiler, install it
+    git::install { 'operations/software/puppet-compiler':
+        ensure    => $ensure,
+        git_tag   => $version,
+        directory => $compiler_dir,
+        owner     => $user,
+        notify    => Exec['install compiler'],
+    }
 
-                # Install the compiler
-                exec { 'install compiler':
-                    command     => '/usr/bin/python3 setup.py install',
-                    user        => 'root',
-                    cwd         => $compiler_dir,
-                    refreshonly => true,
-                }
+    # Install the compiler
+    exec { 'install compiler':
+        command     => '/usr/bin/python3 setup.py install',
+        user        => 'root',
+        cwd         => $compiler_dir,
+        refreshonly => true,
+    }
 
-                # configuration file
-                file { '/etc/puppet-compiler.conf':
-                    ensure  => $ensure,
-                    owner   => $user,
-                    content => template('puppet_compiler/puppet-compiler.conf.erb'),
-                }
+    # configuration file
+    file { '/etc/puppet-compiler.conf':
+        ensure  => $ensure,
+        owner   => $user,
+        content => template('puppet_compiler/puppet-compiler.conf.erb'),
+    }
 
-                # A new, better approach is to just use confd independently. Here we
-                # fake it with a file on disk
-                file { '/etc/conftool-state':
-                    ensure => directory,
-                    mode   => '0755'
-                }
-                file { '/etc/conftool-state/mediawiki.yaml':
-                    ensure => present,
-                    mode   => '0444',
-                    source => 'puppet:///modules/puppet_compiler/mediawiki.yaml'
-                }
+    # A new, better approach is to just use confd independently. Here we
+    # fake it with a file on disk
+    file { '/etc/conftool-state':
+        ensure => directory,
+        mode   => '0755'
+    }
+    file { '/etc/conftool-state/mediawiki.yaml':
+        ensure => present,
+        mode   => '0444',
+        source => 'puppet:///modules/puppet_compiler/mediawiki.yaml'
+    }
 }
