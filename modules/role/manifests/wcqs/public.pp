495d8d40c4ba869c5dc4c827cc0f2201155d5d27
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-11-30 15:34:03 +0100

    Add profile::base::linux419 to the WCQS role
    The data import led to system hangs with Linux 4.9
    
    Bug: T294961
    Change-Id: I448d9d46181c6cfc593e176380e169dfdc6f98e6

--- modules/role/manifests/wcqs/public.pp
+++ modules/role/manifests/wcqs/public.pp
@@ -4,14 +4,16 @@
 # Data on Commons dataset to service public queries from prod infra
 class role::wcqs::public {
     # Standard for all roles
-    include ::profile::base::production
-    include ::profile::base::firewall
+    include profile::base::production
+    include profile::base::firewall
+    # The data import led to system hangs with Linux 4.9, see T294961
+    include profile::base::linux419
     # Standard wcqs installation
-    require ::profile::query_service::wcqs
+    require profile::query_service::wcqs
     # Public endpoint specific profiles
     include profile::tlsproxy::envoy # TLS termination
     # Production specific profiles
-    include ::profile::lvs::realserver
+    include profile::lvs::realserver
 
     system::role { 'wcqs::public':
         ensure      => 'present',
