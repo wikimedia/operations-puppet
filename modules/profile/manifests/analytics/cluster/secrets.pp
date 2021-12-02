fb8b57842d52a08af0e364eaf04f55c9de448679
Ottomata <aotto@wikimedia.org>
2021-12-02 11:34:14 -0500

    Deploy research_poc thanos swift auth env file to hadoop
    Also fix ST_AUTH url for analytics user
    
    Bug: T296945
    Change-Id: I9ec780f47ff9e1dcbb42b75d276a288520d83e19

--- modules/profile/manifests/analytics/cluster/secrets.pp
+++ modules/profile/manifests/analytics/cluster/secrets.pp
@@ -16,51 +16,86 @@
 #
 # [*swift_accounts_keys*]
 #   The accounts keys map to use for swift.
+#
+# [*swift_thanos_accounts*]
+#   The accounts map to use for the thanos swift cluster.
+#
+# [*swift_thanos_accounts_keys*]
+#   The accounts keys map to use for the thanos swift cluster.
+#
 class profile::analytics::cluster::secrets(
     String $swift_group = lookup('profile::analytics::cluster::secrets::swift_group', {'default_value' => 'analytics-privatedata-users'}),
     Hash[String, Hash[String, String]] $swift_accounts = lookup('profile::swift::accounts'),
     Hash[String, String] $swift_account_keys = lookup('profile::swift::accounts_keys'),
+    Hash[String, Hash[String, String]] $swift_thanos_accounts = lookup('profile::thanos::swift::accounts'),
+    Hash[String, String] $swift_thanos_account_keys = lookup('profile::thanos::swift::accounts_keys'),
 ) {
     require ::profile::hadoop::common
 
-    $secrets_user = 'analytics'
-    $secrets_group = 'analytics'
+    $analytics_user = 'analytics'
+    $analytics_group = 'analytics'
 
-    # Make sure something has declared the $secrets_user
-    User[$secrets_user] -> Class['profile::analytics::cluster::secrets']
+    # Make sure something has declared the $analytics_user
+    User[$analytics_user] -> Class['profile::analytics::cluster::secrets']
 
     # mysql research user creds
     include ::passwords::mysql::research
     $research_user = $::passwords::mysql::research::user
     $research_pass = $::passwords::mysql::research::pass
-    $research_path = "/user/${secrets_user}/mysql-analytics-research-client-pw.txt"
+    $research_path = "/user/${analytics_user}/mysql-analytics-research-client-pw.txt"
 
     kerberos::exec { 'hdfs_put_mysql-analytics-research-client-pw.txt':
-        command => "/bin/echo -n '${research_pass}' | /usr/bin/hdfs dfs -put - ${research_path} && /usr/bin/hdfs dfs -chmod 600 ${research_path} && /usr/bin/hdfs dfs -chown ${secrets_user}:${secrets_group} ${research_path}",
+        command => "/bin/echo -n '${research_pass}' | /usr/bin/hdfs dfs -put - ${research_path} && /usr/bin/hdfs dfs -chmod 600 ${research_path} && /usr/bin/hdfs dfs -chown ${analytics_user}:${analytics_group} ${research_path}",
         unless  => "/usr/bin/hdfs dfs -test -e ${research_path}",
-        user    => $secrets_user,
+        user    => $analytics_user,
     }
 
     # mysql clouddb1021 analytics user creds
     include ::passwords::mysql::analytics_labsdb
     $labsdb_user = $::passwords::mysql::analytics_labsdb::user
     $labsdb_pass = $::passwords::mysql::analytics_labsdb::pass
-    $labsdb_path = "/user/${secrets_user}/mysql-analytics-labsdb-client-pw.txt"
+    $labsdb_path = "/user/${analytics_user}/mysql-analytics-labsdb-client-pw.txt"
     kerberos::exec { 'hdfs_put_mysql-analytics-labsdb-client-pw.txt':
-        command => "/bin/echo -n '${labsdb_pass}' | /usr/bin/hdfs dfs -put - ${labsdb_path} && /usr/bin/hdfs dfs -chmod 600 ${labsdb_path} && /usr/bin/hdfs dfs -chown ${secrets_user}:${secrets_group} ${labsdb_path}",
+        command => "/bin/echo -n '${labsdb_pass}' | /usr/bin/hdfs dfs -put - ${labsdb_path} && /usr/bin/hdfs dfs -chmod 600 ${labsdb_path} && /usr/bin/hdfs dfs -chown ${analytics_user}:${analytics_group} ${labsdb_path}",
         unless  => "/usr/bin/hdfs dfs -test -e ${labsdb_path}",
-        user    => $secrets_user,
+        user    => $analytics_user,
     }
 
+
+    # Render the analytics_admin swift account Auth v1 env file for use by the analytics posix user.
+    # https://phabricator.wikimedia.org/T294380
+    # https://phabricator.wikimedia.org/T296945
+    # See: https://docs.openstack.org/python-swiftclient/latest/cli/index.html
     $swift_analytics_admin_auth_url = "${swift_accounts['analytics_admin']['auth']}/auth/v1.0"
     $swift_analytics_admin_user     = $swift_accounts['analytics_admin']['user']
     $swift_analytics_admin_key      = $swift_account_keys['analytics_admin']
-    $swift_analytics_admin_auth_env_content = "export ST_AUTH=${swift_analytics_admin_auth_url}/auth/v1.0\nexport ST_USER=${swift_analytics_admin_user}\nexport ST_KEY=${swift_analytics_admin_key}\n"
-    $swift_analytics_admin_auth_env_path    = "/user/${secrets_user}/swift_auth_analytics_admin.env"
+    $swift_analytics_admin_auth_env_content = "export ST_AUTH=${swift_analytics_admin_auth_url}\nexport ST_USER=${swift_analytics_admin_user}\nexport ST_KEY=${swift_analytics_admin_key}\n"
+    $swift_analytics_admin_auth_env_path    = "/user/${analytics_user}/swift_auth_analytics_admin.env"
     kerberos::exec { 'hdfs_put_swift_auth_analytics_admin.env':
-        command => "/bin/echo -n '${swift_analytics_admin_auth_env_content}' | /usr/bin/hdfs dfs -put - ${swift_analytics_admin_auth_env_path} && /usr/bin/hdfs dfs -chmod 640 ${swift_analytics_admin_auth_env_path} && /usr/bin/hdfs dfs -chown ${secrets_user}:${swift_group} ${swift_analytics_admin_auth_env_path}",
+        command => "/bin/echo -n '${swift_analytics_admin_auth_env_content}' | /usr/bin/hdfs dfs -put - ${swift_analytics_admin_auth_env_path} && /usr/bin/hdfs dfs -chmod 640 ${swift_analytics_admin_auth_env_path} && /usr/bin/hdfs dfs -chown ${analytics_user}:${swift_group} ${swift_analytics_admin_auth_env_path}",
         unless  => "/usr/bin/hdfs dfs -test -e ${swift_analytics_admin_auth_env_path}",
-        user    => $secrets_user,
+        user    => $analytics_user,
+    }
+
+    # Render the research_poc thanos swift account Auth v1 env file for use by the analytics-research posix user.
+    # https://phabricator.wikimedia.org/T294380
+    # https://phabricator.wikimedia.org/T296945
+    # See: https://docs.openstack.org/python-swiftclient/latest/cli/index.html
+
+    # This user must have an HDFS account and have an HDFS /user home directory.
+    # This can be done by making sure it is in one of the profile::hadoop::master::hadoop_user_groups.
+    $analytics_research_user  = 'analytics-research'
+    $analytics_research_group = 'analytics-research'
+
+    $swift_research_poc_auth_url = "${swift_thanos_accounts['research_poc']['auth']}/auth/v1.0"
+    $swift_research_poc_user     = $swift_thanos_accounts['research_poc']['user']
+    $swift_research_poc_key      = $swift_thanos_account_keys['research_poc']
+    $swift_research_poc_auth_env_content = "export ST_AUTH=${swift_research_poc_auth_url}\nexport ST_USER=${swift_research_poc_user}\nexport ST_KEY=${swift_research_poc_key}\n"
+    $swift_research_poc_auth_env_path    = "/user/${analytics_research_user}/swift_auth_analytics_admin.env"
+    kerberos::exec { 'hdfs_put_swift_auth_research_poc.env':
+        command => "/bin/echo -n '${swift_research_poc_auth_env_content}' | /usr/bin/hdfs dfs -put - ${swift_research_poc_auth_env_path} && /usr/bin/hdfs dfs -chmod 440 ${swift_research_poc_auth_env_path} && /usr/bin/hdfs dfs -chown ${analytics_research_user}:${analytics_research_group} ${swift_analytics_admin_auth_env_path}",
+        unless  => "/usr/bin/hdfs dfs -test -e ${swift_analytics_admin_auth_env_path}",
+        user    => $analytics_research_user,
     }
 
 }
