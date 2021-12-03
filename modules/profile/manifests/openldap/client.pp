7648001e2855f070fed5f5f96cc1a28dadeaa25d
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-12-03 10:04:24 +0100

    Write out a separate /etc/ldap/wmf-ldap.conf for tools
    In some cases we don't want the whole server (or rather any tool which reads the
    default ldap.conf) to use the r/w servers, but only a specific tool (e.g. the
    offboarding script or the modify-mfa tool).
    
    Provide a separate /etc/ldap/wmf-ldap.conf file where such tools can read from.
    
    Change-Id: I584ae1ba8a11f9f2c174be15f603eaba177c4dbb

--- modules/profile/manifests/openldap/client.pp
+++ modules/profile/manifests/openldap/client.pp
@@ -4,7 +4,11 @@
 # and populates /etc/ldap/ldap.conf as needed
 #
 # By default the readonly replicas are configured in ldap.conf, this can be changed
-# with profile::openldap::client::read_write
+# with profile::openldap::client::read_write. This will change _all_ LDAP operations
+# by tools which read /etc/ldap/ldap.conf to use the r/w servers.
+#
+# If only a few select tools need r/w access, it's better read the server from
+# /etc/ldap/wmf-ldap.conf instead
 #
 class profile::openldap::client(
     Hash    $ldap_config    = lookup('ldap', Hash, hash, {}),
@@ -27,5 +31,12 @@ class profile::openldap::client(
 
     class { 'ldap::client::openldap':
         ldapconfig   => $ldapconfig,
+    }
+
+    file { '/etc/ldap/wmf-ldap.conf':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('ldap/wmf-ldap.erb'),
     }
 }
