c5eca62534c52e7902671f094365ac294b5c23cf
jbond <jbond@wikimedia.org>
2021-12-03 12:44:01 +0100

    P:openldap::client: Add ldap::client::utils
    Hosts: mwmaint1002.eqiad.wmnet
    Change-Id: Ice26a0d936646f97e7fd9b473f4f8dff40a71a61

--- modules/profile/manifests/openldap/client.pp
+++ modules/profile/manifests/openldap/client.pp
@@ -11,26 +11,26 @@
 # /etc/ldap/wmf-ldap.conf instead
 #
 class profile::openldap::client(
-    Hash    $ldap_config    = lookup('ldap', Hash, hash, {}),
-    Hash    $private_config = lookup('labsldapconfig', {'merge' => hash}),
-    Boolean $read_write     = lookup('profile::openldap::client::read_write', {default_value => false}),
+    Hash $ldap_config = lookup('ldap', Hash, hash, {}),
 ){
-    if $read_write {
-        $servernames = [ $ldap_config['rw-server'], $ldap_config['rw-server-fallback'] ]
-    } else {
-        $servernames = [ $ldap_config['ro-server'], $ldap_config['ro-server-fallback'] ]
-    }
 
     $ldapconfig = {
-        'servernames'          => servernames,
-        'basedn'               => $ldap_config['base-dn'],
-        'proxyagent'           => $ldap_config['proxyagent'],
-        'proxypass'            => $private_config['proxypass'],
-        'ca'                   => 'ca-certificates.crt',
+        'servernames'      => [$ldap_config['ro-server'], $ldap_config['ro-server-fallback']],
+        'servernames_rw'   => [$ldap_config['rw-server'], $ldap_config['rw-server-fallback']],
+        'basedn'           => $ldap_config['base-dn'],
+        'proxyagent'       => $ldap_config['proxyagent'],
+        'proxypass'        => $ldap_config['proxypass'],
+        'script_user_dn'   => $ldap_config['script_user_dn'],
+        'script_user_pass' => $ldap_config['script_user_pass'],
+        'ca'               => 'ca-certificates.crt',
+    }
+
+    class { 'ldap::client::utils':
+        ldapconfig => $ldapconfig,
     }
 
     class { 'ldap::client::openldap':
-        ldapconfig   => $ldapconfig,
+        ldapconfig => $ldapconfig,
     }
 
     file { '/etc/ldap/wmf-ldap.conf':
