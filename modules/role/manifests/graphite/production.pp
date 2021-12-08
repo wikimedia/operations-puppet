cc71f5afc7c01ed0b5364d2f42841ea2ba42ed86
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2021-12-08 10:57:51 +0100

    graphite: enable netconsole client
    Bug: T297265
    Change-Id: Iacbe13c4dcf2015372088a02e1a1d9858df8bdee

--- modules/role/manifests/graphite/production.pp
+++ modules/role/manifests/graphite/production.pp
@@ -5,6 +5,7 @@ class role::graphite::production {
     include ::profile::statsd # all graphite hosts also include statsd
     include ::profile::graphite::production
     include ::profile::tlsproxy::envoy # TLS termination
+    include ::profile::netconsole::client
 
     system::role { 'graphite::production':
         description => 'Real-time metrics processor',
