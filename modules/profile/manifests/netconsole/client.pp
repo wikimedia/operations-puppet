53ec3ffc98cda5b2c141677c06fd43ab0682b27b
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2021-12-09 10:49:18 +0100

    netconsole: move to anycast syslog
    With netconsole deployed on syslog centralserver we can simplify
    configuration and default to infra-wide values for any production
    client.
    
    Change-Id: I95d0766ded059366ffd64dc33ba9308e8f69e77d

--- modules/profile/manifests/netconsole/client.pp
+++ modules/profile/manifests/netconsole/client.pp
@@ -1,14 +1,17 @@
 # Class: profile::netconsole::client
 #
 # This profile configures netconsole client on the host.
+# Note the broadcast MAC is used to allow for cross-row communication.
 #
 # Sample Usage:
-#       include profile::netconsole::client
+#       * Add 'include profile::netconsole::client' in your role
+#       * Set 'profile::netconsole::client::ensure: present' where needed in hieradata/
+#
 #
 class profile::netconsole::client (
     Wmflib::Ensure $ensure = lookup('profile::netconsole::client::ensure'),
     Optional[Stdlib::IP::Address::V4] $remote_ip = lookup('profile::netconsole::client::remote_ip', {default_value => undef}),
-    Optional[Stdlib::MAC] $remote_mac = lookup('profile::netconsole::client::remote_mac', {default_value => undef}),
+    Optional[Stdlib::MAC] $remote_mac = lookup('profile::netconsole::client::remote_mac', {default_value => 'ff:ff:ff:ff:ff:ff'}),
 ) {
     class { '::netconsole::client':
       ensure     => $ensure,
