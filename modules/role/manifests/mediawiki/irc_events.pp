f7de535829c3869e1c573ca15fef17d671a9613a
Kunal Mehta <legoktm@debian.org>
2021-12-15 16:02:30 -0800

    Delete unused role::mediawiki::irc_events
    Replaced by role::mw_rc_irc.
    
    Bug: T272559
    Change-Id: I2cd03d93610f7ed85378f833dcd64615fb19374f

--- modules/role/manifests/mediawiki/irc_events.pp
+++
@@ -1,7 +0,0 @@
-# MediaWiki events to IRC (irc.wikimedia.org)
-#
-class role::mediawiki::irc_events {
-    system::role { 'mediawiki::irc_events': }
-    include ::profile::base::production
-    include ::profile::base::firewall
-}
