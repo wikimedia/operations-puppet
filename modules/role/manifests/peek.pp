79631dce9893e960184c103cace4453d3d2d9d7e
David Caro <dcaro@wikimedia.org>
2022-01-03 17:27:37 +0100

    {role:,profile:,}peek: remove unused classes
    The peek project was disabled (T284090). And the machines
    decommissioned (on I9c5fb68d0c2c10a1134ec60349d25090fb0d8b38).
    
    Bug: T272559
    Change-Id: I5967a4fae3d49c2c2f409d4a233e8f24afb5c064
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- modules/role/manifests/peek.pp
+++
@@ -1,6 +0,0 @@
-class role::peek {
-    system::role { 'peek': description => 'Security Team Tooling' }
-    include ::profile::base::production
-    include ::profile::base::firewall
-    include ::profile::peek
-}
