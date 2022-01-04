8f88483af2d39ca9655c4b6e578d2bb06ae51f58
David Caro <dcaro@wikimedia.org>
2022-01-04 17:19:57 +0100

    r:wmcs:paws:k8s:etcd: remove unused role
    This was added but never used, in:
    Ice72d9723b9deb9a83e4aab36ccc4529ecbfe976
    
    Probably because the etcd cluster is embedded in the control nodes.
    
    Bug: T188912
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: I603f26bebb233ffef63b9b12d063253dd451b8b9

--- modules/role/manifests/wmcs/paws/k8s/etcd.pp
+++
@@ -1,7 +0,0 @@
-class role::wmcs::paws::k8s::etcd {
-    system::role { $name: }
-
-    include ::profile::base::firewall
-    include ::profile::wmcs::paws::common
-    include ::profile::wmcs::kubeadm::etcd
-}
