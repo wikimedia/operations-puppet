5106429ded3d0c8e0bcaa9f632cf9aa1d09ff584
David Caro <dcaro@wikimedia.org>
2022-01-04 17:03:48 +0100

    r:wmcs:openstack:eqiad1:cumin_controller: remove unused role
    This was deprecated with Ib7a3cbdc0bfa2bb62f8f1ae733549ac162d66578
    
    Bug: T234462
    Bug: T272559
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: Ic72b0c5baef082f96b39a60d3672858076e7f89d

--- modules/role/manifests/wmcs/openstack/eqiad1/cumin_controller.pp
+++
@@ -1,8 +0,0 @@
-class role::wmcs::openstack::eqiad1::cumin_controller {
-    system::role { $name: }
-    include ::profile::base::production
-    include ::profile::openstack::eqiad1::clientpackages
-    include ::profile::openstack::eqiad1::observerenv
-    include ::profile::openstack::base::optional_firewall
-    include ::profile::openstack::eqiad1::cumin::master
-}
