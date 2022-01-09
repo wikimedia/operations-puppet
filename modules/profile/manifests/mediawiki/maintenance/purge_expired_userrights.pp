fd2fa3de8781523a91126cd50fc3e073c230baf3
Taavi Väänänen <hi@taavi.wtf>
2022-01-09 16:44:29 +0200

    P:mw::maintenance: add centralauth group purge job
    This is a new maintenance script in CentralAuth that needs to be ran
    regularly.
    
    Bug: T153815
    Hosts: mwmaint1002.eqiad.wmnet
    Depends-On: Icd059902d30dcd9b404f8ac2cf9205e7a81442d4
    Change-Id: I7016c02a5afed1d35777bc57c3ec20490467098e

--- modules/profile/manifests/mediawiki/maintenance/purge_expired_userrights.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_expired_userrights.pp
@@ -3,4 +3,11 @@ class profile::mediawiki::maintenance::purge_expired_userrights {
         command  => '/usr/local/bin/foreachwiki maintenance/purgeExpiredUserrights.php',
         interval => '*-14,28 06:42'
     }
+
+    # CentralAuth tables are global, we only need to run this on one wiki.
+    # I picked meta since that's where all on-wiki CentralAuth actions are done.
+    profile::mediawiki::periodic_job { 'purge_expired_global_rights':
+        command  => '/usr/local/bin/mwscript extensions/CentralAuth/maintenance/purgeExpiredGlobalRights.php --wiki metawiki',
+        interval => '*-3,17 13:23',
+    }
 }
