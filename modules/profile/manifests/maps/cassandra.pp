88da58f557b0e4e30440e19b03b984e8a3b71289
Hugh Nowlan <hnowlan@wikimedia.org>
2022-01-11 15:04:00 +0000

    maps: add cassandra toggle, disable cassandra on maps hosts
    As part of the decommissioning of maps 1.x services, we need to
    disable cassandra for a period to ensure that we can later remove it.
    
    Bug: T298246
    Change-Id: I035a9f40cc30a3012bc499fca94f74b82277717f

--- modules/profile/manifests/maps/cassandra.pp
+++ modules/profile/manifests/maps/cassandra.pp
@@ -1,15 +1,18 @@
 class profile::maps::cassandra(
+    Boolean $cassandra_enable           = lookup('profile::maps::cassandra::enable', { 'default_value' => true }),
     String $cassandra_kartotherian_pass = lookup('profile::maps::cassandra::kartotherian_pass'),
     String $cassandra_tilerator_pass    = lookup('profile::maps::cassandra::tilerator_pass'),
     String $cassandra_tileratorui_pass  = lookup('profile::maps::cassandra::tileratorui_pass'),
 ){
 
-    require profile::cassandra::single_instance
+    if $cassandra_enable {
+        require profile::cassandra::single_instance
 
-    file { '/usr/local/bin/maps-grants.cql':
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0400',
-        content => template('profile/maps/grants.cql.erb'),
+        file { '/usr/local/bin/maps-grants.cql':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0400',
+            content => template('profile/maps/grants.cql.erb'),
+        }
     }
 }
