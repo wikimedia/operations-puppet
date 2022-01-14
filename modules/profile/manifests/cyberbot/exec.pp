998528390f1f512352bd32d3a765e002ee2468c3
Taavi Väänänen <hi@taavi.wtf>
2022-01-14 20:55:23 +0200

    P:cyberbot::exec: support newer debian versions
    Change-Id: I077e433aa0ee34ba0b91e0e82ce70e3db41d2932

--- modules/profile/manifests/cyberbot/exec.pp
+++ modules/profile/manifests/cyberbot/exec.pp
@@ -2,7 +2,12 @@
 class profile::cyberbot::exec{
 
     # switch to a fact once T271196 is resolved
-    $php_version = '7.2'
+    $php_version = debian::codename() ? {
+        'bullseye' => '7.4',
+        'buster'   => '7.3',
+        'stretch'  => '7.2',
+        default    => fail("profile::cyberbot::exec currently unsupported on debian ${debian::codename()}"),
+    }
 
     ensure_packages([
         "php${php_version}-mysql", "php${php_version}-mysqlnd", "php${php_version}-cli",
