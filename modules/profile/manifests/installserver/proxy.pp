ef742e4366e38816ef2b81c31125858d17aeda82
jbond <jbond@wikimedia.org>
2022-01-17 15:54:11 +0100

    P:installserver::proxy: switch access logs to syslog
    Logging via syslog enables:
      * central logging of this information
      * sending logs into central log
    
    Hosts: P:installserver::proxy
    Bug: T298087
    Change-Id: If1fbce7ab9300767026d8ed1f38ce5838a2eaad5

--- modules/profile/manifests/installserver/proxy.pp
+++ modules/profile/manifests/installserver/proxy.pp
@@ -1,8 +1,11 @@
-# Installs a proxy server for the install server
-class profile::installserver::proxy(
+# @summary Installs a proxy server for the install server
+# @param ensure ensurable parameter
+class profile::installserver::proxy (
     Wmflib::Ensure $ensure = lookup('profile::installserver::proxy::ensure'),
-){
+) {
 
+    $syslog_facility = 'local0'
+    $syslog_priority = 'info'
     class { 'squid':
         ensure         => $ensure,
         config_content => template('role/caching-proxy/squid.conf.erb'),
@@ -10,6 +13,18 @@ class profile::installserver::proxy(
 
     profile::auto_restarts::service { 'squid': }
 
+    $rsyslog_content = @("CONF"/L)
+    # Send squid access logs
+    if \$programname startswith 'squid' \
+        and  \$syslogfacility-text == ${syslog_facility} \
+        and \$syslogpriority-text == ${syslog_priority} \
+        then /var/log/squid/access.log
+    &~
+    | CONF
+
+    rsyslog::conf { 'squid-access':
+        content => $rsyslog_content,
+    }
     systemd::timer::job { 'squid-logrotate':
         ensure      => $ensure,
         user        => 'root',
