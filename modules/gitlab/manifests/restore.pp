3c987e9c98cf46962fa1f9fc421fac73d6c0888c
Arnold Okoth <aokoth@wikimedia.org>
2022-01-21 20:35:48 +0300

    gitlab: change restore manifest formatting
    Change-Id: I27329826b4fd4e2dc7037acd1cffc7d9751b0b7f

--- modules/gitlab/manifests/restore.pp
+++ modules/gitlab/manifests/restore.pp
@@ -1,32 +1,31 @@
+# install restore script on secondary gitlab host
 class gitlab::restore(
-  Wmflib::Ensure   $restore_ensure          = 'present',
-  Wmflib::Ensure   $restore_ensure_timer    = 'present',
-  Stdlib::Unixpath $restore_dir_data        = '/srv/gitlab-backup',
+    Wmflib::Ensure   $restore_ensure          = 'present',
+    Wmflib::Ensure   $restore_ensure_timer    = 'present',
+    Stdlib::Unixpath $restore_dir_data        = '/srv/gitlab-backup',
 ){
 
-  # install restore script
-  file {"${restore_dir_data}/gitlab-restore.sh":
-      ensure => $restore_ensure,
-      mode   => '0744' ,
-      owner  => 'root',
-      group  => 'root',
-      source => 'puppet:///modules/gitlab/gitlab-restore.sh';
-  }
-
-  file {"${restore_dir_data}/gitlab-restore-v2.sh":
-      ensure => $restore_ensure,
-      mode   => '0744' ,
-      owner  => 'root',
-      group  => 'root',
-      source => 'puppet:///modules/gitlab/gitlab-restore-v2.sh';
+    file {"${restore_dir_data}/gitlab-restore.sh":
+        ensure => $restore_ensure,
+        mode   => '0744' ,
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/gitlab/gitlab-restore.sh';
     }
 
-  # systemd timer for backup restore
-  systemd::timer::job { 'backup-restore':
-      ensure      => $restore_ensure_timer,
-      user        => 'root',
-      description => 'GitLab Backup Restore',
-      command     => "${restore_dir_data}/gitlab-restore.sh",
-      interval    => {'start' => 'OnCalendar', 'interval' => '*-*-* 01:30:00'},
-  }
+    file {"${restore_dir_data}/gitlab-restore-v2.sh":
+        ensure => $restore_ensure,
+        mode   => '0744' ,
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/gitlab/gitlab-restore-v2.sh';
+    }
+
+    systemd::timer::job { 'backup-restore':
+        ensure      => $restore_ensure_timer,
+        user        => 'root',
+        description => 'GitLab Backup Restore',
+        command     => "${restore_dir_data}/gitlab-restore.sh",
+        interval    => {'start' => 'OnCalendar', 'interval' => '*-*-* 01:30:00'},
+    }
 }
