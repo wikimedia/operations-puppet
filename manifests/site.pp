a8cc39e577c0907d7dfd6199f4acc31326382069
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-01-21 14:17:39 +0100

    Make ganeti1026 a Ganeti node
    Change-Id: I08e9d5fc2b700a8b2ecfb2598b6e5a143599f371

--- manifests/site.pp
+++ manifests/site.pp
@@ -1410,12 +1410,12 @@ node 'lists1001.wikimedia.org' {
 }
 
 # New ganeti hosts for eqiad T283036 and T293909
-node /^ganeti102[678]\.eqiad\.wmnet/ {
+node /^ganeti102[78]\.eqiad\.wmnet/ {
     role(insetup)
 }
 
 # Ganeti virtualization hosts - eqiad
-node /^ganeti10(0[5-9]|1[0-9]|2[0-5])\.eqiad\.wmnet$/ {
+node /^ganeti10(0[5-9]|1[0-9]|2[0-6])\.eqiad\.wmnet$/ {
     role(ganeti)
 }
 # Ganeti virtualization hosts - codfw
