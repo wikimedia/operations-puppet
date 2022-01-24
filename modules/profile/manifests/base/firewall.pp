d32605928a55114684752f57cff3c622e8ac1ae5
jbond <jbond@wikimedia.org>
2022-01-24 11:30:53 +0100

    P:base::firewall: Add proemethous hosts to catch all ferm rule
    This CR adds the promethus hosts to the allow all rule currently in
    place for icinga hosts
    
    Bug: T291946
    Change-Id: Iabadbb93a8b728e1dac9b3d226d2c6369fe00779

--- modules/profile/manifests/base/firewall.pp
+++ modules/profile/manifests/base/firewall.pp
@@ -2,6 +2,8 @@
 class profile::base::firewall (
     Array[Stdlib::IP::Address] $monitoring_hosts        = lookup('monitoring_hosts',
                                                                 {default_value => []}),
+    Array[Stdlib::IP::Address] $prometheus_nodes        = lookup('prometheus_nodes',
+                                                                {default_value => []}),
     Array[Stdlib::IP::Address] $cumin_masters           = lookup('cumin_masters',
                                                                 {default_value => []}),
     Array[Stdlib::IP::Address] $bastion_hosts           = lookup('bastion_hosts',
@@ -31,7 +33,7 @@ class profile::base::firewall (
     Boolean                    $default_reject   = lookup('profile::base::firewall::default_reject'),
 ) {
     class { '::base::firewall':
-        monitoring_hosts        => $monitoring_hosts,
+        monitoring_hosts        => $monitoring_hosts + $prometheus_nodes,
         cumin_masters           => $cumin_masters,
         bastion_hosts           => $bastion_hosts,
         cache_hosts             => $cache_hosts,
