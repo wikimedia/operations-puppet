97e520b52c27d173ae6be9098ff8471871479e8e
Janis Meybohm <jmeybohm@wikimedia.org>
2022-01-26 14:26:15 +0100

    Upgrade codfw kubernetes masters to tainted full nodes
    This will make the master basically a normal node + kubernetes control
    plain. Effectively allowing pods to be scheduled (if they tolerate a
    certain taint) making it possible to run calico there as well.
    
    Bug: T290967
    Change-Id: Ic97e6d7ddd97acd66d9549c6acc426f0a9683498

--- modules/role/manifests/kubernetes/master.pp
+++ modules/role/manifests/kubernetes/master.pp
@@ -9,6 +9,19 @@ class role::kubernetes::master {
     # reasons
     include ::profile::kubernetes::client
 
+    if ($::site == 'codfw') {
+        # See T290967 for context
+        # Initial testing will be done in staging-codfw only.
+
+        # ::profile::docker::storage is not included which means the docker
+        # default (overlayfs for >=buster) will be used. While this differs
+        # from the setup of the worker nodes, but we will migrate them in
+        # the future and may gain some experience already.
+        include ::profile::docker::engine
+        include ::profile::kubernetes::node
+        include ::profile::calico::kubernetes
+    }
+
     # Kubernetes masters are LVS backend servers
     include ::profile::lvs::realserver
 
