61e21a10f1c8d6b6c43405a53faf07b1af868fd0
Gehel <guillaume.lederrey@wikimedia.org>
2022-01-31 08:14:15 +0000

    Revert "cirrussearch: Reenable saneitizer"
    This reverts commit 20552760e74cfe3f4010de31c829c92b79947bfa.
    
    Reason for revert: Saneitizer is generating lag on Search indexation.
    
    Change-Id: Ia325c21b6574b8c3a43a8869264ff4faf052753e

--- modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
+++ modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
@@ -21,7 +21,9 @@ class profile::mediawiki::maintenance::cirrussearch {
         interval => '02:30',
     }
 
+    # T295705 2021-11-22 Temporarily disabling saneitizer for at least the next several days
     profile::mediawiki::periodic_job { 'cirrus_saneitize_jobs':
+        ensure   => absent,
         command  => '/usr/local/bin/foreachwiki extensions/CirrusSearch/maintenance/SaneitizeJobs.php --push --refresh-freq=7200',
         interval => '0/2:10',
     }
