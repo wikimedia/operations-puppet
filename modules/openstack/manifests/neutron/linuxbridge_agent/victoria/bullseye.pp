6af2f93e378e9c248c147db7e6d29ee0139943fa
Andrew Bogott <abogott@wikimedia.org>
2022-02-01 10:28:00 -0600

    Revert "Neutron/Victoria/Bullseye: try removing iptables entirely"
    This reverts commit 061a47e4989898cf218579e0f4a9cf0c508481aa.
    
    I was trying to troubleshoot a problem that's unrelated to iptables
    
    Change-Id: I86e00a48b3e3558385b648ce484dc19e564b277f

--- modules/openstack/manifests/neutron/linuxbridge_agent/victoria/bullseye.pp
+++ modules/openstack/manifests/neutron/linuxbridge_agent/victoria/bullseye.pp
@@ -9,4 +9,19 @@ class openstack::neutron::linuxbridge_agent::victoria::bullseye(
     package { 'neutron-linuxbridge-agent':
         ensure => 'present',
     }
+
+    # Not installed by default, but still available on Bullseye
+    package { 'iptables':
+        ensure => 'present',
+    }
+
+    alternatives::select { 'iptables':
+        path => '/usr/sbin/iptables-legacy',
+    }
+    alternatives::select { 'ip6tables':
+        path => '/usr/sbin/ip6tables-legacy',
+    }
+    alternatives::select { 'ebtables':
+        path    => '/usr/sbin/ebtables-legacy',
+    }
 }
