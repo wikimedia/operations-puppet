19e7faebbdc394d1360c6c72ebb038a971d73eb2
Andrew Bogott <abogott@wikimedia.org>
2022-02-05 14:27:52 -0600

    cloudvirt/libvirtd: another attempt to fix startup ordering
    Again, referring to https://github.com/openshift/installer/issues/3604
    
    Change-Id: I5c08a7733d621a1073dba67222c25804b7bad749

--- modules/openstack/manifests/nova/compute/service/victoria/bullseye.pp
+++ modules/openstack/manifests/nova/compute/service/victoria/bullseye.pp
@@ -29,6 +29,16 @@ class openstack::nova::compute::service::victoria::bullseye() {
         require => Package['busybox'],
     }
 
+    # The only reliable order to get this working is:
+    #  - stop libvirtd
+    #  - start libvirtd-tls.socket
+    #  - start libvirtd
+    exec {'stop-libvirtd-so-we-can-start-it':
+        command     => 'systemctl stop libvirtd',
+        refreshonly => true,
+        require     => Package[libvirt-daemon-system],
+    }
+
     service { 'libvirtd':
         ensure  => 'running',
         enable  => true,
@@ -38,7 +48,7 @@ class openstack::nova::compute::service::victoria::bullseye() {
     service { 'libvirtd-tls.socket':
         ensure  => 'running',
         enable  => true,
-        require => Package[libvirt-daemon-system],
+        require => [Package[libvirt-daemon-system], Exec['stop-libvirtd-so-we-can-start-it']],
         before  => Service['libvirtd'],
     }
 
