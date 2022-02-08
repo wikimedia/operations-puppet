8bda244c6d6aad37f075b3035167bbc7b592b889
Daniel Zahn <dzahn@wikimedia.org>
2022-02-08 14:29:37 -0800

    microsites: remove 15.wikipedia.org
    Bug: T300171
    Change-Id: I898a80e6953d2f381917e941da631724bd031f96

--- modules/profile/manifests/microsites/annualreport.pp
+++ modules/profile/manifests/microsites/annualreport.pp
@@ -2,15 +2,10 @@
 # sets up the WMF annual report site
 # https://annual.wikimedia.org/
 # https://foundation.wikimedia.org/wiki/Annual_Report
-# T599 - https://15.wikipedia.org (aka. annual report 2015)
 class profile::microsites::annualreport {
 
     httpd::site { 'annual.wikimedia.org':
         source => 'puppet:///modules/profile/annualreport/annual.wikimedia.org',
-    }
-
-    httpd::site { '15.wikipedia.org':
-        source => 'puppet:///modules/profile/annualreport/15.wikipedia.org',
     }
 
     git::clone { 'wikimedia/annualreport':
