742568954b424ee0cceeb46f0f9bd2cc07f2e0e7
Luca Toscano <ltoscano@wikimedia.org>
2022-02-10 10:38:47 +0100

    Add ml-serve2006 to the ml-serve-codfw k8s cluster
    This is the first node running Bullseye and Overlay FS.
    
    Bug: T300744
    Change-Id: I7b305f3ff2af28c15dfc470eb840974536397ef6

--- manifests/site.pp
+++ manifests/site.pp
@@ -1973,12 +1973,12 @@ node /^ml-serve-ctrl200[12]\.codfw\.wmnet/ {
     role(ml_k8s::master)
 }
 
-node /^ml-serve200[12345]\.codfw\.wmnet/ {
+node /^ml-serve200[123456]\.codfw\.wmnet/ {
     role(ml_k8s::worker)
 }
 
 # New ml-serve nodes T294945
-node /^ml-serve200[678]\.codfw\.wmnet/ {
+node /^ml-serve200[78]\.codfw\.wmnet/ {
     role(insetup)
 }
 
