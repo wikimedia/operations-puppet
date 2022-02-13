1ca6fdbacc1195533a02d73cab99ccf853bec99f
Taavi Väänänen <hi@taavi.wtf>
2022-02-13 13:29:49 +0200

    P:mariadb::grants::cloudinfra: read grant hosts from hiera
    Change-Id: I7e82c993d73b25f1627996907b87b440e438bb59

--- modules/profile/manifests/mariadb/grants/cloudinfra.pp
+++ modules/profile/manifests/mariadb/grants/cloudinfra.pp
@@ -1,7 +1,17 @@
 class profile::mariadb::grants::cloudinfra (
-    String $labspuppet_pass = lookup('profile::mariadb::grants::cloudinfra::labspuppet_pass'),
-    String $repl_pass       = lookup('profile::mariadb::grants::cloudinfra::repl_pass'),
+    Array[Stdlib::Fqdn] $cloudinfra_dbs  = lookup('profile::mariadb::cloudinfra::cloudinfra_dbs'),
+    Array[Stdlib::Fqdn] $puppetmasters   = lookup('profile::mariadb::cloudinfra::puppetmasters'),
+    String              $labspuppet_pass = lookup('profile::mariadb::grants::cloudinfra::labspuppet_pass'),
+    String              $repl_pass       = lookup('profile::mariadb::grants::cloudinfra::repl_pass'),
 ) {
+    $repl_ips = $cloudinfra_dbs.map |Stdlib::Fqdn $fqdn| {
+        ipresolve($fqdn, 4)
+    }
+
+    $labspuppet_client_ips = $puppetmasters.map |Stdlib::Fqdn $fqdn| {
+        ipresolve($fqdn, 4)
+    }
+
     file { '/etc/mysql/cloudinfra-grants.sql':
         ensure  => present,
         owner   => 'root',
