a69fffca945eb6587aa61853245a46aac35f1dc8
Arnold Okoth <aokoth@wikimedia.org>
2022-02-15 18:56:30 +0300

    vrts: rename profile variables
    Bug: T293942
    Change-Id: I56c5bc90f06a1c54a9c3875642c28d2a02ea360b

--- modules/profile/manifests/vrts.pp
+++ modules/profile/manifests/vrts.pp
@@ -2,11 +2,11 @@
 # sets up an instance of the 'Volunteer Response Team System'
 # https://wikitech.wikimedia.org/wiki/VRT_System
 class profile::vrts(
-    Stdlib::Fqdn $otrs_database_host = lookup('profile::vrts::database_host'),
-    String $otrs_database_name       = lookup('profile::vrts::database_name'),
-    String $otrs_database_user       = lookup('profile::vrts::database_user'),
-    String $otrs_database_pw         = lookup('profile::vrts::database_pass'),
-    Boolean $otrs_daemon             = lookup('profile::vrts::daemon'),
+    Stdlib::Fqdn $vrts_database_host = lookup('profile::vrts::database_host'),
+    String $vrts_database_name       = lookup('profile::vrts::database_name'),
+    String $vrts_database_user       = lookup('profile::vrts::database_user'),
+    String $vrts_database_pw         = lookup('profile::vrts::database_pass'),
+    Boolean $vrts_daemon             = lookup('profile::vrts::daemon'),
     String $exim_database_name       = lookup('profile::vrts::exim_database_name'),
     String $exim_database_user       = lookup('profile::vrts::exim_database_user'),
     String $exim_database_pass       = lookup('profile::vrts::exim_database_pass'),
@@ -19,11 +19,11 @@ class profile::vrts(
     }
 
     class { '::otrs':
-        otrs_database_host => $otrs_database_host,
-        otrs_database_name => $otrs_database_name,
-        otrs_database_user => $otrs_database_user,
-        otrs_database_pw   => $otrs_database_pw,
-        otrs_daemon        => $otrs_daemon,
+        otrs_database_host => $vrts_database_host,
+        otrs_database_name => $vrts_database_name,
+        otrs_database_user => $vrts_database_user,
+        otrs_database_pw   => $vrts_database_pw,
+        otrs_daemon        => $vrts_daemon,
         exim_database_name => $exim_database_name,
         exim_database_user => $exim_database_user,
         exim_database_pass => $exim_database_pass,
