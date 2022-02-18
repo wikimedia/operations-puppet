54baaa85799f1f3640ecb88385577052978b4bd9
Cole White <cwhite@wikimedia.org>
2022-02-18 20:09:17 +0000

    search: update grafana dashboard links
    Bug: T211982
    Change-Id: I84fb0c2bd0f2269bce4055a343ec46b38e46f3ac

--- modules/role/manifests/elasticsearch/alerts.pp
+++ modules/role/manifests/elasticsearch/alerts.pp
@@ -26,7 +26,7 @@ class role::elasticsearch::alerts {
     # warning level is ~1% of peak traffic failing
     monitoring::graphite_threshold { 'search_backend_failure_count':
         description     => 'Number of backend failures per minute from CirrusSearch',
-        dashboard_links => ['https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?orgId=1&var-cluster=eqiad&var-smoothing=1&panelId=9&fullscreen'],
+        dashboard_links => ['https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?orgId=1&var-cluster=eqiad&var-smoothing=1&viewPanel=9'],
         metric          => 'transformNull(MediaWiki.CirrusSearch.eqiad.backend_failure.failed.count, 0)',
         from            => '10min',
         warning         => 300,
