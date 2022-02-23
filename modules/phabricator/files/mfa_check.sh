a57b88b55a4e8e08a7eca9b0908bd29a6bd526d6
Andre Klapper <a9016009@gmx.de>
2022-02-23 12:34:55 +0100

    MFA Phab accounts email: Fix incorrect SQL query; misc improvements
    * Query on project DB instead of user DB
    ** Cover also materialized membership edge types
    * Add convenience links to members list of projects
    * Alias column headers (looks prettier)
    * Fix a typo (s/security/stewards/)
    * Add convenience link to query people
    
    Bug: T302385
    Change-Id: I80628e6ceb049644f785bcb7eb5f36f6c8d92c34

--- modules/phabricator/files/mfa_check.sh
+++ modules/phabricator/files/mfa_check.sh
@@ -7,12 +7,12 @@ source /etc/phab_mfa_check.conf
 
 #echo "result_admins_and_security"
 result_admins_and_security=$(MYSQL_PWD=${sql_pass} /usr/bin/mysql -h $sql_host -P $sql_port -u $sql_user $sql_name << END
-SELECT CONCAT("https://phabricator.wikimedia.org/p/", u.userName)
+SELECT DISTINCT(CONCAT("https://phabricator.wikimedia.org/p/", u.userName)) AS user
   FROM phabricator_user.user u
-  WHERE (u.phid IN
-    (SELECT e.src
-      FROM phabricator_user.edge e
-      WHERE (e.dst = "PHID-PROJ-koo4qqdng27q7r65x3cw" AND e.type=14))
+  JOIN phabricator_project.edge e
+  ON u.phid = e.dst
+  WHERE ((e.src = "PHID-PROJ-koo4qqdng27q7r65x3cw"
+    AND (e.type=14 OR e.type=60))
     OR u.isAdmin = 1)
   AND u.isDisabled = 0
   AND u.phid NOT IN
@@ -22,12 +22,12 @@ END
 
 #echo "result_acl_stewards"
 result_acl_stewards=$(MYSQL_PWD=${sql_pass} /usr/bin/mysql -h $sql_host -P $sql_port -u $sql_user $sql_name << END
-SELECT CONCAT("https://phabricator.wikimedia.org/p/", u.userName)
+SELECT DISTINCT(CONCAT("https://phabricator.wikimedia.org/p/", u.userName)) AS user
   FROM phabricator_user.user u
-  WHERE (u.phid IN
-    (SELECT e.src
-      FROM phabricator_user.edge e
-      WHERE (e.dst = "PHID-PROJ-py76uxfk5h7eem3wljmf" AND e.type=14)))
+  JOIN phabricator_project.edge e
+  ON u.phid = e.dst
+  WHERE (e.src = "PHID-PROJ-py76uxfk5h7eem3wljmf"
+    AND (e.type=14 OR e.type=60))
   AND u.isDisabled = 0
   AND u.phid NOT IN
     (SELECT mfa.userPHID FROM phabricator_auth.auth_factorconfig mfa);
@@ -36,12 +36,12 @@ END
 
 #echo "result_wmf_nda"
 result_wmf_nda=$(MYSQL_PWD=${sql_pass} /usr/bin/mysql -h $sql_host -P $sql_port -u $sql_user $sql_name << END
-SELECT CONCAT("https://phabricator.wikimedia.org/p/", u.userName)
+SELECT DISTINCT(CONCAT("https://phabricator.wikimedia.org/p/", u.userName)) AS user
   FROM phabricator_user.user u
-  WHERE (u.phid IN
-    (SELECT e.src
-      FROM phabricator_user.edge e
-      WHERE (e.dst = "PHID-PROJ-ibxm3v6ithf3jpqpqhl7" AND e.type=14)))
+  JOIN phabricator_project.edge e
+  ON u.phid = e.dst
+  WHERE (e.src = "PHID-PROJ-ibxm3v6ithf3jpqpqhl7"
+    AND (e.type=14 OR e.type=60))
   AND u.isDisabled = 0
   AND u.phid NOT IN
     (SELECT mfa.userPHID FROM phabricator_auth.auth_factorconfig mfa);
@@ -54,14 +54,19 @@ Hi Phabricator admin,
 
 This is your automatic weekly Phabricator mail
 listing elevated accounts and their 2FA status.
+  * https://phabricator.wikimedia.org/people/query/advanced/
 
 PHAB ADMINS AND SECURITY:
+  * Security: https://phabricator.wikimedia.org/project/members/30/
+  * Admins: https://phabricator.wikimedia.org/people/query/WETApwetxsAM/#R
 ${result_admins_and_security}
 
-ACL*SECURITY MEMBERS:
+ACL*STEWARDS MEMBERS:
+  * https://phabricator.wikimedia.org/project/members/2849/
 ${result_acl_stewards}
 
 WMF-NDA MEMBERS:
+  * https://phabricator.wikimedia.org/project/members/61/
 ${result_wmf_nda}
 
 Yours sincerely,
