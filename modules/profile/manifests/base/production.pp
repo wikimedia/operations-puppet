21fa33b455bfd956f45525b7bb3c22c77ff3787d
jbond <jbond@wikimedia.org>
2022-02-23 13:34:07 +0100

    P:base::production: move system::role to profile::base::production
    The plan for this would replace system::role
    
    Hosts: sretest1001.eqiad.wmnet,pki1001.eqiad.wmnet
    Change-Id: I687beb7c0618774dc524cdd422a4cca44336d93e

--- modules/profile/manifests/base/production.pp
+++ modules/profile/manifests/base/production.pp
@@ -3,9 +3,11 @@
 # @param enable weather to enable or disabl this profile.  This is most often used to disable this profile
 #   in cloud environments which directly include a role:: class
 # @param enable_ip6_mapped if ipv6 mapped addresses should be enabled
+# @param role_description A role description to add to the motd
 class profile::base::production (
-    Boolean $enable            = lookup('profile::base::production::enable'),
-    Boolean $enable_ip6_mapped = lookup('profile::base::production::enable_ip6_mapped'),
+    Boolean $enable                       = lookup('profile::base::production::enable'),
+    Boolean $enable_ip6_mapped            = lookup('profile::base::production::enable_ip6_mapped'),
+    Optional[String[1]] $role_description = lookup('profile::base::production::role_description')
 ) {
     if $enable {
         # include this early so we can use the data elsewhere
@@ -46,6 +48,15 @@ class profile::base::production (
 
         if $facts['is_virtual'] {
             class { 'toil::ganeti_ifupdown': }
+        }
+        $role_str = $::_role.regsubst('\/', '::', 'G') # lint:ignore:top_scope_facts
+        $message = $role_description ? {
+            undef   => "${facts['networking']['hostname']} is ${role_str}",
+            default => "${facts['networking']['hostname']} is a ${role_description} (${role_str})",
+        }
+        motd::message { $role_str:
+            priority => 5,
+            message  => $message,
         }
     }
 }
