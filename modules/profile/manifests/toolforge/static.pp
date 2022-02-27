c144e8cf3577727a8d5eaa6d4ed6487c43365a9b
Taavi Väänänen <hi@taavi.wtf>
2022-02-27 12:57:39 +0200

    P:toolforge::static: publish SSH fingerprints under /admin
    This is to make it possible to enable tab completion and proper SSH
    fingerprint validation like config-master.wikimedia.org allows for
    production hosts.
    
    Change-Id: Ia7e55e0288ebaedcad6d9e523fa439ba6203e475

--- modules/profile/manifests/toolforge/static.pp
+++ modules/profile/manifests/toolforge/static.pp
@@ -1,8 +1,16 @@
 class profile::toolforge::static (
     Stdlib::Fqdn $web_domain = lookup('profile::toolforge::web_domain', {default_value => 'toolforge.org'}),
 ) {
-    $resolver             = join($::nameservers, ' ')
+    $resolver = join($::nameservers, ' ')
+    $fingerprints_dir = '/var/www/fingerprints'
+
+    wmflib::dir::mkdir_p($fingerprints_dir)
+
     nginx::site { 'static-server':
         content => template('profile/toolforge/static-server.conf.erb'),
+    }
+
+    class { 'ssh::publish_fingerprints':
+        document_root => $fingerprints_dir,
     }
 }
