af39127c7a22c0f9a7d6f950593a4470a13a83da
jbond <jbond@wikimedia.org>
2022-03-02 12:42:37 +0100

    C:geoip::data::maxmind: update systemd timers
    I noticed a number of things which could be updated with theses scripts:
      * the output didn;t include carrage returns
      * redirecting to a dedicated log file instead of making use of systemd
        logging
      * Additional dedicated scripts not needed
      * exec resource run even when system is not active
    
    Bug: T302864
    Hosts: puppetmaster1001.eqiad.wmnet
    Hosts: puppetmaster1002.eqiad.wmnet
    Hosts: puppetmaster2001.codfw.wmnet
    Change-Id: I6b0cbd7e328ac9b0d2d52c7cb7fa2e8264e4e789

--- modules/geoip/manifests/data/maxmind.pp
+++ modules/geoip/manifests/data/maxmind.pp
@@ -1,17 +1,15 @@
-# == Class geoip::data::maxmind
-# Installs Maxmind GeoIP database files by downloading
+# @summary Installs Maxmind GeoIP database files by downloading
 # them from Maxmind with the geoipupdate command.
 # This also installs a timer job to do this weekly.
 #
-# == Parameters
-# $data_directory - Where the data files should live.
-# $license_key    - MaxMind license key.
-# $user_id        - MaxMind user id.
-# $product_ids    - Array of MaxMind product ids to specify which data files
+# @param data_directory Where the data files should live.
+# @param license_key    MaxMind license key.
+# @param user_id        MaxMind user id.
+# @param product_ids    Array of MaxMind product ids to specify which data files
 #                   to download.  default: [506] (GeoLite Country)
-# $proxy          - Proxy server to use to fetch files.
-# == Example
-# You can use this class on your puppetmaster to stick the GeoIP .dat files
+# @param ca_server      Proxy server to use to fetch files.
+# @param proxy          Active CA server
+# @example You can use this class on your puppetmaster to stick the GeoIP .dat files
 # into a fileserver module.  Once the files are there, you can use the
 # default geoip::data::puppet class to sync the files from your puppetmaster,
 # instead of downloading them from maxmind on all your nodes.
@@ -31,7 +29,7 @@ class geoip::data::maxmind(
   String $user_id                  = '999999',
   String $license_key              = '000000000000',
   Array $product_ids               = [506],
-  Stdlib::Host $ca_server          = $facts['fqdn'],
+  Stdlib::Host $ca_server          = $facts['networking']['fqdn'],
   Optional[Stdlib::Httpurl] $proxy = undef,
 ) {
   # Version 3 on buster has different config keys to version 2
@@ -41,6 +39,7 @@ class geoip::data::maxmind(
 
   ensure_resource('file', $data_directory, {'ensure' => 'directory'})
 
+  $is_active = $facts['networking']['fqdn'] == $ca_server
   $config_file = '/etc/GeoIP.conf'
 
   # Install GeoIP.conf with Maxmind user_id, licence_key, and product_ids.
@@ -49,62 +48,49 @@ class geoip::data::maxmind(
   }
 
   # command to run to update the GeoIP database files
-  $geoipupdate_command = "/usr/bin/geoipupdate -f ${config_file} -d ${data_directory}"
 
-  # Go ahead and exec geoipupdate now, so that we can be sure we have these
-  # files if this is the first time puppetmaster is running this class.
-  exec { 'geoipupdate':
-    command     => $geoipupdate_command,
-    refreshonly => true,
-    subscribe   => File[$config_file],
-    require     => [
-        Package['geoipupdate'],
-        File[$config_file],
-        File[$data_directory]
-    ],
+  if $is_active {
+    # Go ahead and exec geoipupdate now, so that we can be sure we have these
+    # files if this is the first time puppetmaster is running this class.
+    $geoipupdate_command = "/usr/bin/geoipupdate -f ${config_file} -d ${data_directory}"
+    exec { 'geoipupdate':
+        command     => $geoipupdate_command,
+        refreshonly => true,
+        subscribe   => File[$config_file],
+        require     => [
+            Package['geoipupdate'],
+            File[$data_directory]
+        ],
+    }
+  } else {
+    $geoipupdate_command = "/usr/bin/printf 'this job only runs on the active ca server: ${ca_server}\\n'"
   }
-
-  $geoipupdate_log = '/var/log/geoipupdate.log'
 
   # Set up a timer to run geoipupdate daily. This will download .dat files for
   # the specified MaxMind Product IDs.  We expect new data to generally arrive
   # weekly on Tuesdays, but there is no guarantee as to the precise timing in
   # the long term.
-
-  file { '/usr/local/bin/geoipupdate_job_legacy':
-      ensure => present,
-      mode   => '0555',
-      source => 'puppet:///modules/geoip/geoipupdate_job_legacy.sh',
-  }
-
-  file { '/etc/geoipupdate_job_legacy':
-      ensure  => present,
-      mode    => '0555',
-      content => template('geoip/geoipupdate_job_legacy.erb'),
-  }
-
   systemd::timer::job { 'geoip_update_legacy':
       ensure             => 'present',
       user               => 'root',
       description        => 'download geoip database from MaxMind',
-      command            => '/usr/local/bin/geoipupdate_job_legacy',
+      command            => $geoipupdate_command,
       interval           => {'start' => 'OnCalendar', 'interval' => '*-*-* 3:30:0'},
       monitoring_enabled => true,
       logging_enabled    => true,
       require            => [
         Package['geoipupdate'],
-        File[$config_file],
-        File[$data_directory]
+        File[$config_file, $data_directory]
       ],
   }
 
-  # logrotate for geoipupdate.log
+  # Clean up old resources
   logrotate::rule { 'geoipupdate_log_legacy':
-    ensure       => present,
-    file_glob    => $geoipupdate_log,
-    size         => '1M',
-    rotate       => 1,
-    missing_ok   => true,
-    not_if_empty => true,
+    ensure    => absent,
+    file_glob => 'foobar', # required parameter for absent
   }
+  file { ['/usr/local/bin/geoipupdate_job_legacy', '/etc/geoipupdate_job_legacy']:
+      ensure => absent,
+  }
+
 }
--- modules/geoip/manifests/data/maxmind/ipinfo.pp
+++ modules/geoip/manifests/data/maxmind/ipinfo.pp
@@ -1,4 +1,4 @@
-# == Class geoip::data::maxmind::ipinfo
+# @summary
 # Installs Maxmind GeoIP database files for the IP Info extension
 # by downloading them from Maxmind with the geoipupdate command.
 # This also installs a timer/job to do this weekly.
@@ -7,14 +7,14 @@
 # and different database product IDs.
 # This is a transitional class for T288844.
 #
-# == Parameters
-# $data_directory - Where the data files should live.
-# $license_key    - MaxMind license key.
-# $user_id        - MaxMind user id.
-# $product_ids    - Array of MaxMind product ids to specify which data files
+# @param data_directory Where the data files should live.
+# @param license_key MaxMind license key.
+# @param user_id MaxMind user id.
+# @param product_ids Array of MaxMind product ids to specify which data files
 #                   to download.  default: [506] (GeoLite Country)
-# $proxy          - Proxy server to use to fetch files.
-# == Example
+# @param proxy Proxy server to use to fetch files.
+# @param ca_server The active CA server
+# @example Example
 # You can use this class on your puppetmaster to stick the GeoIP .dat files
 # into a fileserver module.  Once the files are there, you can use the
 # default geoip::data::puppet class to sync the files from your puppetmaster,
@@ -35,16 +35,16 @@ class geoip::data::maxmind::ipinfo(
   String $user_id                  = '999999',
   String $license_key              = '000000000000',
   Array $product_ids               = [506],
-  Stdlib::Host $ca_server          = $facts['fqdn'],
+  Stdlib::Host $ca_server          = $facts['networking']['fqdn'],
   Optional[Stdlib::Httpurl] $proxy = undef,
 ) {
   # Version 3 on buster has different config keys to version 2
   $legacy_format = debian::codename::lt('buster')
 
   ensure_packages(['geoipupdate'])
-
   ensure_resource('file', $data_directory, {'ensure' => 'directory'})
 
+  $is_active = $facts['networking']['fqdn'] == $ca_server
   $config_file = '/etc/GeoIPInfo.conf'
 
   # Install GeoIP.conf with Maxmind user_id, licence_key, and product_ids.
@@ -53,58 +53,48 @@ class geoip::data::maxmind::ipinfo(
     content => template('geoip/GeoIP.conf.erb'),
   }
 
-  # command to run to update the GeoIP database files
-  $geoipupdate_command = "/usr/bin/geoipupdate -f ${config_file} -d ${data_directory}"
 
-  # Go ahead and exec geoipupdate now, so that we can be sure we have these
-  # files if this is the first time puppetmaster is running this class.
-  exec { 'geoipupdate-ipinfo':
-    command     => $geoipupdate_command,
-    refreshonly => true,
-    require     => [
-        Package['geoipupdate'],
-        File[$config_file],
-        File[$data_directory]
-    ],
+  if $is_active {
+    $geoipupdate_command = "/usr/bin/geoipupdate -f ${config_file} -d ${data_directory}"
+    # Go ahead and exec geoipupdate now, so that we can be sure we have these
+    # files if this is the first time puppetmaster is running this class.
+    exec { 'geoipupdate-ipinfo':
+        command     => $geoipupdate_command,
+        refreshonly => true,
+        require     => [
+            Package['geoipupdate'],
+            File[$config_file, $data_directory]
+        ],
+    }
+  } else {
+    # Emit a log entry if not on the active server
+    $geoipupdate_command = "/usr/bin/printf 'this job only runs on the active ca server: ${ca_server}\\n'"
   }
-
-  $geoipupdate_log = '/var/log/geoipupdate-ipinfo.log'
 
   # Set up a timer to run geoipupdate daily. This will download .dat files for
   # the specified MaxMind Product IDs.  We expect new data to generally arrive
   # weekly on Tuesdays, but there is no gaurantee as to the precise timing in
   # the long term.
-  file { '/usr/local/bin/geoipupdate_job':
-      ensure => present,
-      mode   => '0555',
-      source => 'puppet:///modules/geoip/geoipupdate_job.sh',
-  }
-  file { '/etc/geoipupdate_job':
-      ensure  => present,
-      mode    => '0555',
-      content => template('geoip/geoipupdate_job.erb'),
-  }
   systemd::timer::job { 'geoip_update':
       ensure             => 'present',
       user               => 'root',
       description        => 'download geoip database for IP Info from MaxMind',
-      command            => '/usr/local/bin/geoipupdate_job',
+      command            => $geoipupdate_command,
       interval           => {'start' => 'OnCalendar', 'interval' => '*-*-* 4:30:0'},
       monitoring_enabled => false,
       logging_enabled    => true,
       require            => [
-        Package['geoipupdate'],
-        File[$config_file],
-        File[$data_directory]
+          Package['geoipupdate'],
+          File[$config_file, $data_directory]
       ],
   }
 
+  # clean up old resources
   logrotate::rule { 'geoipupdate_log':
-    ensure       => present,
-    file_glob    => $geoipupdate_log,
-    size         => '1M',
-    rotate       => 1,
-    missing_ok   => true,
-    not_if_empty => true,
+    ensure    => absent,
+    file_glob => 'foobar', # required parameter for absent
+  }
+  file { ['/etc/geoipupdate_job', '/usr/local/bin/geoipupdate_job']:
+      ensure => absent,
   }
 }
