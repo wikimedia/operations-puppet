a98f53b3540ea59341aa8fb9485979bf8fdd2708
Ben Tullis <btullis@wikimedia.org>
2022-03-07 14:31:13 +0000

    Add a profile specific to datahubsearch servers
    At the moment all this does is configure the inbound firewall for
    opensearch to allow access to port 9200, but there will likely be more
    configuration here in the near future.
    
    Bug: T301382
    Change-Id: Icc13089bbeb52fb09512504f4637cac65403172c

---
+++ modules/profile/manifests/opensearch/datahubsearch.pp
@@ -0,0 +1,15 @@
+# vim:sw=4 ts=4 sts=4 et:
+# == Class: profile::opensearch::datahubsearch
+#
+# Provisions OpenSearch backend node for a DataHubsearch cluster.
+#
+class profile::opensearch::datahubsearch {
+
+    include profile::opensearch::server
+
+    ferm::service {'opensearch-query':
+        proto  => 'tcp',
+        port   => '9200',
+        srange => '$DOMAIN_NETWORKS',
+    }
+}
--- modules/role/manifests/analytics_cluster/datahub/opensearch.pp
+++ modules/role/manifests/analytics_cluster/datahub/opensearch.pp
@@ -5,7 +5,7 @@ class role::analytics_cluster::datahub::opensearch {
 
     include ::profile::base::production
     include ::profile::base::firewall
-    include ::profile::opensearch::server
+    include ::profile::opensearch::datahubsearch
     include ::profile::rsyslog::udp_json_logback_compat
     include ::profile::opensearch::monitoring::base_checks
 }
