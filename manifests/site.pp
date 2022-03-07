4ea51dd45653c38efdec9da100bc3d737d002a79
Valentin Gutierrez <vgutierrez@wikimedia.org>
2022-03-07 17:07:14 +0100

    site: Reimage cp3058 as cache::text_haproxy
    Bug: T290005
    Change-Id: Iccaec485f16c45a826966dd92345526888b93424

--- manifests/site.pp
+++ manifests/site.pp
@@ -405,12 +405,12 @@ node /^cp20(3[68]|4[02])\.codfw\.wmnet$/ {
 # esams caches
 #
 
-node /^cp305[02468]\.esams\.wmnet$/ {
+node /^cp305[0246]\.esams\.wmnet$/ {
     role(cache::text)
 }
 
 # HAProxy test - T290005
-node /^cp306[024]\.esams\.wmnet$/ {
+node /^cp30(58|6[024])\.esams\.wmnet$/ {
     role(cache::text_haproxy)
 }
 
