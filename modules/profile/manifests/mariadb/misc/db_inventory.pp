24cfa58329d2d797542ba8b7f04e636b5d295d8a
Manuel Arostegui <marostegui@wikimedia.org>
2022-03-10 08:02:51 +0100

    db_inventory.pp: Remove binlog override
    Tendril is gone so this should follow the normal standard.
    STATEMENT if master, ROW if slave
    
    Bug: T303496
    Change-Id: Ia10941b60a24a00a23f86b0ee6d0560969ca31b6

--- modules/profile/manifests/mariadb/misc/db_inventory.pp
+++ modules/profile/manifests/mariadb/misc/db_inventory.pp
@@ -67,12 +67,9 @@ class profile::mariadb::misc::db_inventory(
     }
 
     class { 'mariadb::heartbeat':
-        shard                  => $id,
-        datacenter             => $::site,
-        enabled                => $mysql_role == 'master',
-        # As tendril requires READ-COMMITTED, heartbeat needs to use ROW.
-        # https://phabricator.wikimedia.org/T283228#7100571
-        override_binlog_format => 'ROW',
+        shard      => $id,
+        datacenter => $::site,
+        enabled    => $mysql_role == 'master',
     }
 
     class { 'mariadb::monitor_memory': }
