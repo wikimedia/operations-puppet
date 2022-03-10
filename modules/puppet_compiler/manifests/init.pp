03c8985ec79481885083d20c7d0ea7c61d67ed9d
jbond <jbond@wikimedia.org>
2022-03-10 17:25:22 +0100

    puppet_compiler: fix netbox checkout path
    Change-Id: I99c601d5c7380960fc084eceb55bbf17d08d64ec

--- modules/puppet_compiler/manifests/init.pp
+++ modules/puppet_compiler/manifests/init.pp
@@ -87,7 +87,7 @@ class puppet_compiler(
     git::clone { 'netbox-hiera':
         ensure    => $ensure,
         origin    => 'https://netbox-exports.wikimedia.org/netbox-hiera',
-        directory => "${libdir}/private",
+        directory => "${libdir}/netbox-hiera",
         owner     => $user,
         mode      => '0755',
         require   => File[$libdir],
