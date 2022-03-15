970a15b6d21d4cd8fcb028845d284d6174d923f0
jbond <jbond@wikimedia.org>
2022-03-15 12:15:34 +0100

    O:kafka::logging: ensure that all base classes are initiated first
    Hosts: O:kafka::logging
    Change-Id: I60857ba1a1defd722c4ebdf94cf1796786478c93

--- modules/role/manifests/kafka/logging.pp
+++ modules/role/manifests/kafka/logging.pp
@@ -1,10 +1,10 @@
 class role::kafka::logging {
 
-    include ::profile::kafka::broker
     system::role { 'kafka::logging':
         description => "Kafka Broker in the logging-${::site} Kafka cluster",
     }
 
-    include ::profile::base::firewall
-    include ::profile::base::production
+    include profile::base::firewall
+    include profile::base::production
+    include profile::kafka::broker
 }
