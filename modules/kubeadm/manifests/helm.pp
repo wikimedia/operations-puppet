5221531b9d61a203667f9b91004bd8e544138a5c
Taavi Väänänen <hi@taavi.wtf>
2022-03-16 19:36:32 +0200

    kubeadm::helm: use systemd::environment
    Depends-On: I02bc984b4a598639981eedf18c397e6fa7c3ef9f
    Change-Id: I638d78d07c3eaca0056e17c36a44d2e1a2a4555a

--- modules/kubeadm/manifests/helm.pp
+++ modules/kubeadm/manifests/helm.pp
@@ -8,9 +8,17 @@ class kubeadm::helm (
         tag    => 'kubeadm-k8s',
     }
 
+    systemd::environment { 'helm':
+        variables => {
+            # Let's not set full HELM_HOME so that full root isn't needed to run
+            # Helm (per-user caches and repositories), but still load plugins
+            # installed from Apt and (as a side effect) block non-roots from
+            # manually installing plugins.
+            'HELM_PLUGINS' => '/etc/helm/plugins',
+        },
+    }
+
     file { '/etc/profile.d/helm-config.sh':
-        ensure => present,
-        mode   => '0555',
-        source => 'puppet:///modules/kubeadm/helm-config.sh',
+        ensure => absent,
     }
 }
