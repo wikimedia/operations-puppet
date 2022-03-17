a358ee32081716df26721d4437fc6e1c7eac08fc
jbond <jbond@wikimedia.org>
2022-03-17 12:19:59 +0100

    P:environment: Add support for environment.d to zsh and bash
    This CR adds support for sourcing the
    30-systemd-environment-d-generator from zsh and bash login shells,
    allowing us to inject environment variables set via systemd
    envirnment.d into login shells
    
    Hosts: sretest1001.eqiad.wmnet
    Bug: T278315
    Change-Id: I98dfc9615234e0df773fa92acb965f6272e2f280

--- modules/profile/manifests/environment.pp
+++ modules/profile/manifests/environment.pp
@@ -3,15 +3,17 @@
 # Sets up the base environment for all hosts (profile, editor, sysctl, etc)
 #
 # === Parameters
-# @ls_aliases if true, will setup the ll/la aliases
-# @custom_bashrc when set, will replace the system bashrc with the given template (as a path)
-# @custom_skel_bashrc when set, will replace the system skel bashrc with the given template (as a path)
-# @editor choose the default editor, if 'use_default' will use the system's default
-# @profile_scripts list of script names to be added to /etc/profile.d and their source
-# @core_dump_pattern pattern to use when generating core dumps
+# @param ls_aliases if true, will setup the ll/la aliases
+# @param export_systemd_env if true, add login scripts to ensure systemd environment.d variables are exported
+# @param custom_bashrc when set, will replace the system bashrc with the given template (as a path)
+# @param custom_skel_bashrc when set, will replace the system skel bashrc with the given template (as a path)
+# @param custom_skel_zshrc when set, will replace the system skel zshrc with the given template (as a path)
+# @param editor choose the default editor, if 'use_default' will use the system's default
+# @param profile_scripts list of script names to be added to /etc/profile.d and their source
 #
 class profile::environment (
     Boolean                    $ls_aliases         = lookup('profile::environment::ls_aliases'),
+    Boolean                    $export_systemd_env = lookup('profile::environment::export_systemd_env'),
     Optional[String[1]]        $custom_skel_bashrc = lookup('profile::environment::custom_skel_bashrc'),
     Optional[String[1]]        $custom_skel_zshrc  = lookup('profile::environment::custom_skel_zshrc'),
     Optional[String[1]]        $custom_bashrc      = lookup('profile::environment::custom_bashrc'),
@@ -61,15 +63,29 @@ class profile::environment (
         }
     }
 
-    $profile_scripts.each |$name, $source| {
+    $profile_scripts.each |$script, $source| {
         file {
-            "/etc/profile.d/${name}":
-                ensure => present,
+            "/etc/profile.d/${script}":
+                ensure => file,
                 owner  => 'root',
                 group  => 'root',
                 mode   => '0444',
                 source => $source,
         }
+    }
+    $export_command = 'export $(/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)'
+    file_line { 'zsh_export_systemd_env':
+        ensure => $export_systemd_env.bool2str('present', 'absent'),
+        path   => '/etc/zsh/zshenv',
+        line   => $export_command,
+    }
+
+    file { '/etc/profile.d/systemd-environment.sh':
+        ensure  => stdlib::ensure($export_systemd_env, 'file'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => $export_command,
     }
 
     ### Settings commons to all realms
