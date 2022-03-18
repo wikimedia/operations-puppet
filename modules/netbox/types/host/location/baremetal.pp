e4faa4ea7d5de961f1b47af3e798d812a65738c8
jbond <jbond@wikimedia.org>
2022-03-18 14:56:02 +0100

    P:netbox::host: relx type checking as we have racks like \d.\d.\d
    Change-Id: I6a91a2b20fbca23df04dc0a25ba2837bf0c5607e

--- modules/netbox/types/host/location/baremetal.pp
+++ modules/netbox/types/host/location/baremetal.pp
@@ -1,6 +1,6 @@
 type Netbox::Host::Location::BareMetal = Struct[{
     # should at some point have a Wmflib::Site
     site    => String[5,5],
-    row     => String[11,11],
-    rack    => String[2,2],
+    row     => String[2],
+    rack    => String[2],
 }]
