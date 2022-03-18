96a522dd62db6c38968880e49fe31cfca65d85fd
Valentin Gutierrez <vgutierrez@wikimedia.org>
2022-03-18 12:16:43 +0100

    ncredir: Use profile::lvs::realserver
    Change-Id: Iaa8bd6fdb6ffacc0bb4cc34cb6d6973968129b3d

--- modules/role/manifests/ncredir.pp
+++ modules/role/manifests/ncredir.pp
@@ -2,8 +2,7 @@ class role::ncredir {
     system::role { 'ncredir': description => 'Non canonical domains redirection service' }
     include profile::base::production
     include profile::base::firewall
-    # TODO: use ::profile::lvs::realserver instead
-    include lvs::realserver  # lint:ignore:wmf_styleguide
+    include profile::lvs::realserver
     include profile::nginx
     include profile::ncredir
 }
