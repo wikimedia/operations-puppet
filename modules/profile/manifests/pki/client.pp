e900b3b6cd9afd11f6a7a89487370b06076fb3a2
jbond <jbond@wikimedia.org>
2022-03-18 12:14:42 +0100

    P:pki::client: add documentation
    Hosts: sretest1001.eqiad.wmnet
    Change-Id: I6d8f7b03a008b9bb18e292aff7fe0903048c0426

--- modules/profile/manifests/pki/client.pp
+++ modules/profile/manifests/pki/client.pp
@@ -2,8 +2,17 @@
 # @param ensure whether to ensure the resource
 # @param signer_host The signer host
 # @param signer_port The signer port
-# @param use_stunnel use an stunnel encrypt
 # @param auth_key the cfssl sha256 hmax key
+# @param enable_proxy if tru start the proxy service
+# @param listen_addr for the proxy service
+# @param listen_port for the proxy service
+# @param root_ca_cn cn of the root ca
+# @param root_ca_source puppet source location of root ca
+# @param mutual_tls_client_cert location of client auth tls cert
+# @param mutual_tls_client_key location of client auth tls key
+# @param tls_remote_ca location of ca bundle for pki service
+# @param tls_remote_ca_source puppet source for tls_remote_ca
+# @param certs a hash of certs to create
 class profile::pki::client (
     Wmflib::Ensure               $ensure                 = lookup('profile::pki::client::ensure'),
     Stdlib::Host                 $signer_host            = lookup('profile::pki::client::signer_host'),
@@ -12,18 +21,18 @@ class profile::pki::client (
     Boolean                      $enable_proxy           = lookup('profile::pki::client::enable_proxy'),
     Stdlib::IP::Address          $listen_addr            = lookup('profile::pki::client::listen_addr'),
     Stdlib::Port                 $listen_port            = lookup('profile::pki::client::listen_port'),
-    String                       $root_ca                = lookup('profile::pki::client::root_ca_cn'),
+    String                       $root_ca_cn             = lookup('profile::pki::client::root_ca_cn'),
+    Optional[Stdlib::Filesource] $root_ca_source         = lookup('profile::pki::client::root_ca_source'),
     Optional[Stdlib::Unixpath]   $mutual_tls_client_cert = lookup('profile::pki::client::mutual_tls_client_cert'),
     Optional[Stdlib::Unixpath]   $mutual_tls_client_key  = lookup('profile::pki::client::mutual_tls_client_key'),
     Optional[Stdlib::Unixpath]   $tls_remote_ca          = lookup('profile::pki::client::tls_remote_ca'),
     Optional[Stdlib::Filesource] $tls_remote_ca_source   = lookup('profile::pki::client::tls_remote_ca_source'),
-    Optional[Stdlib::Filesource] $root_ca_source         = lookup('profile::pki::client::root_ca_source'),
     Hash                         $certs                  = lookup('profile::pki::client::certs'),
 ) {
     $signer = "https://${signer_host}:${signer_port}"
     $bundles_source = 'puppet:///modules/profile/pki/intermediates'
     if $root_ca_source {
-        file { "/etc/ssl/certs/${root_ca}.pem":
+        file { "/etc/ssl/certs/${root_ca_cn}.pem":
             ensure => file,
             owner  => 'root',
             group  => 'root',
