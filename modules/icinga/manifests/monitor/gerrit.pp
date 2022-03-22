4a39db169ba70a963d2b70607e4a559e5102034f
Riccardo Coccioli <rcoccioli@wikimedia.org>
2022-03-22 11:29:44 +0100

    icinga: avoid double quoting for the URL
    * The URL parameter is now quoted in the command itself.
    
    Bug: T304323
    Change-Id: Ie82b69245e5c15afee4248da38a0d699ed33bf8e

--- modules/icinga/manifests/monitor/gerrit.pp
+++ modules/icinga/manifests/monitor/gerrit.pp
@@ -17,7 +17,7 @@ class icinga::monitor::gerrit {
 
     monitoring::service { 'gerrit-json':
         description   => 'Gerrit JSON',
-        check_command => 'check_https_url_at_address_for_minsize!gerrit.wikimedia.org!"/r/changes/?n=25&O=81"!10000',
+        check_command => 'check_https_url_at_address_for_minsize!gerrit.wikimedia.org!/r/changes/?n=25&O=81!10000',
         contact_group => 'admins,gerrit',
         host          => 'gerrit.wikimedia.org',
         notes_url     => 'https://wikitech.wikimedia.org/wiki/Gerrit#Monitoring',
