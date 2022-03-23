4ab857f95953b952d355fec7ead3d1ea76c1b493
John Bond <jbond@wikimedia.org>
2022-03-23 13:58:06 +0100

    C:icinga::commons: Add ssl expiry checks for commons
    Hosts: alerts1001.wikimedia.org
    Bug: T304321
    Change-Id: I4435e50deae24d693bd29b180064eb9dc647d776

--- modules/icinga/manifests/monitor/commons.pp
+++ modules/icinga/manifests/monitor/commons.pp
@@ -5,13 +5,18 @@ class icinga::monitor::commons {
         host_fqdn => 'commons.wikimedia.org',
     }
 
-    monitoring::service { 'commons_content':
-        description   => 'wiki content on commons',
-        check_command => 'check_https_url_for_string!commons.wikimedia.org!/wiki/Main_Page!Picture of the day',
-        host          => 'commons.wikimedia.org',
-        contact_group => 'admins',
-        critical      => true,
-        notes_url     => 'https://phabricator.wikimedia.org/project/view/1118/',
+    monitoring::service {
+        default:
+            host          => 'commons.wikimedia.org',
+            contact_group => 'admins',
+            critical      => true,
+            notes_url     => 'https://phabricator.wikimedia.org/project/view/1118/';
+        'commons_content':
+            description   => 'wiki content on commons',
+            check_command => 'check_https_url_for_string!commons.wikimedia.org!/wiki/Main_Page!Picture of the day';
+        'commons_content_expiry':
+            description   => 'wiki content on commons expiry',
+            check_command => 'check_https_expiry!commons.wikimedia.org!443';
     }
 
 }
