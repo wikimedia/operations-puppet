ad369649534e5b78cf8e9472ab884e5453cfaa9f
Riccardo Coccioli <rcoccioli@wikimedia.org>
2022-03-23 11:41:03 +0100

    cluster::management: backup also /home
    * In the last host refresh/reimage it was asked to keep the /home
      directory because it contains important data.
    * The current sizes are:
      * cumin1001:
    
        $ sudo du -csh /home/* | grep G
        4.8G    /home/marostegui
        5.7G    total
    
      * cumin2002:
    
        $ sudo du -csh /home/* | grep G
        326M       total
    
    Change-Id: I0bdd97b903b85110079d8b987a8da886b2d18a00

--- modules/profile/manifests/cluster/management/backup.pp
+++ modules/profile/manifests/cluster/management/backup.pp
@@ -2,4 +2,5 @@
 # so data for infrastructure deplotments and pwstore are not lost
 class profile::cluster::management::backup () {
     backup::set { 'srv-deployment': }  # this backs up all of /srv
+    backup::set { 'home': }
 }
