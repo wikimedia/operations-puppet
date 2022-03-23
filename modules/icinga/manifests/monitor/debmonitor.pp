6bf048b04d25309c506cba9ed1cd85f69260688d
John Bond <jbond@wikimedia.org>
2022-03-23 14:02:15 +0100

    C:icinga::commons: Add ssl expiry checks for debmonitor
    Hosts: alerts1001.wikimedia.org
    Bug: T304321
    Change-Id: I2013b55eebdd8ef0867557a2d677e3f39d4102d3

--- modules/icinga/manifests/monitor/debmonitor.pp
+++ modules/icinga/manifests/monitor/debmonitor.pp
@@ -7,12 +7,17 @@ class icinga::monitor::debmonitor {
         host_fqdn => 'debmonitor.wikimedia.org',
     }
 
-    monitoring::service { 'debmonitor-healthcheck':
-        description   => 'Debmonitor Health Check',
-        check_command => 'check_https_redirect!443!debmonitor.wikimedia.org!/!302!https://idp.wikimedia.org/',
-        contact_group => 'admins',
-        host          => 'debmonitor.wikimedia.org',
-        notes_url     => 'https://wikitech.wikimedia.org/wiki/Debmonitor',
+    monitoring::service {
+        default:
+            contact_group => 'admins',
+            host          => 'debmonitor.wikimedia.org',
+            notes_url     => 'https://wikitech.wikimedia.org/wiki/Debmonitor';
+        'debmonitor-healthcheck':
+            description   => 'Debmonitor Health Check',
+            check_command => 'check_https_redirect!443!debmonitor.wikimedia.org!/!302!https://idp.wikimedia.org/';
+        'debmonitor-healthcheck-expiry':
+            description   => 'Debmonitor Health Check Expiry',
+            check_command => 'check_https_expiry!debmonitor.wikimedia.org!/!443';
     }
 
 }
