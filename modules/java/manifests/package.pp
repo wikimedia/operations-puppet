c00885b446a9f4a74b9b5047c92041e2b6306f1f
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-03-24 12:36:32 +0100

    profile::java: Also add component/jdk on bullseye
    Bullseye also doesn't include Java 8, so it needs to be installed from
    the component.
    
    Change-Id: I0e8d5e3116948a5886f5c5ac03f30fec2dfc91bf
    Hosts: P:java

--- modules/java/manifests/package.pp
+++ modules/java/manifests/package.pp
@@ -25,7 +25,7 @@ define java::package(
 
     $package_name = "openjdk-${package_info['version']}-${package_info['variant']}"
 
-    if $package_info['version'] == '8' and debian::codename::eq('buster') {
+    if $package_info['version'] == '8' and debian::codename::ge('buster') {
         apt::package_from_component { $package_name:
             component => 'component/jdk8',
             packages  => [$package_name],
