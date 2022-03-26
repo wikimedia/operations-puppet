53df30d65402a976e35589f58508045d5ef7733f
Taavi Väänänen <hi@taavi.wtf>
2022-03-26 14:11:56 +0200

    P:toolforge: use puppetdb for grid hba data
    Bug: T153163
    Change-Id: Ib5acf15fc590b2bd6e3f9d7e8abc91b60885c8a2

--- modules/profile/manifests/toolforge/grid/hba.pp
+++ modules/profile/manifests/toolforge/grid/hba.pp
@@ -1,48 +1,27 @@
 # Establish the ability to do Host Based Auth from bastions to execs/webgrid
 
 class profile::toolforge::grid::hba {
-
-    $sysstore = $profile::toolforge::grid::base::store
-
-    file { '/usr/local/sbin/project-make-shosts':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0755',
-        content => template('profile/toolforge/project-make-shosts.erb'),
-    }
-
-    exec { 'make-shosts':
-        command => '/usr/local/sbin/project-make-shosts >/etc/ssh/shosts.equiv~',
-        onlyif  => "/usr/bin/test -n \"\$(/usr/bin/find ${sysstore} -maxdepth 1 \\( -type d -or -type f -name submithost-\\* \\) -newer /etc/ssh/shosts.equiv~)\" -o ! -s /etc/ssh/shosts.equiv~",
-        require => File['/usr/local/sbin/project-make-shosts'],
-    }
+    $bastions = wmflib::class::hosts('profile::toolforge::bastion')
+    $bastion_ips = $bastions.map |Stdlib::Fqdn $host| { ipresolve($host, 4) }
 
     file { '/etc/ssh/shosts.equiv':
         ensure  => file,
+        content => template('profile/toolforge/grid/hba/shosts.equiv.erb'),
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
-        source  => '/etc/ssh/shosts.equiv~',
-        require => Exec['make-shosts'],
-    }
-
-    file { '/usr/local/sbin/project-make-access':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0755',
-        content => template('profile/toolforge/project-make-access.erb'),
-    }
-
-    exec { 'make-access':
-        command => '/usr/local/sbin/project-make-access >/etc/project.access',
-        onlyif  => "/usr/bin/test -n \"\$(/usr/bin/find ${sysstore} -maxdepth 1 \\( -type d -or -type f -name submithost-\\* \\) -newer /etc/project.access)\" -o ! -s /etc/project.access",
-        require => File['/usr/local/sbin/project-make-access'],
     }
 
     security::access::config { 'toolforge-hba':
-        source  => '/etc/project.access',
-        require => Exec['make-access'],
+        content => template('profile/toolforge/grid/hba/security.conf.erb'),
+    }
+
+    file { [
+        '/usr/local/sbin/project-make-access',
+        '/usr/local/sbin/project-make-shosts',
+        '/etc/project.access',
+        '/etc/ssh/shosts.equiv~'
+    ]:
+        ensure => absent,
     }
 }
