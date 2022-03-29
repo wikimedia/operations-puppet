2951adc203aac885bd4087cffb4342e783d9f169
Luca Toscano <ltoscano@wikimedia.org>
2022-03-29 16:17:33 +0200

    k8s::kubeconfig: add ensure parameter
    This is useful when we need to auto-clean up kubeconfig files.
    
    Change-Id: I17b3c2e4f812f2e88d4bf09af271b8c4d855badd

--- modules/k8s/manifests/kubeconfig.pp
+++ modules/k8s/manifests/kubeconfig.pp
@@ -2,14 +2,15 @@ define k8s::kubeconfig(
     String $master_host,
     String $username,
     String $token,
-    Optional[String] $namespace=undef,
-    Stdlib::Filemode $mode='0400',
     String $owner='root',
     String $group='root',
+    Stdlib::Filemode $mode='0400',
+    Optional[String] $namespace=undef,
+    Wmflib::Ensure $ensure = present,
 ) {
     require k8s::base_dirs
     file { $title:
-        ensure  => present,
+        ensure  => $ensure,
         content => template('k8s/kubeconfig-client.yaml.erb'),
         owner   => $owner,
         group   => $group,
