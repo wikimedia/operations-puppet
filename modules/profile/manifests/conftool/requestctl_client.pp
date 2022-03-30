5de16f91dcb2a49c57cff36894701a161d0e9273
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2022-03-30 16:13:31 +0200

    requestctl::client: install preview files for actions
    To build confidence in a new action we're adding to requestctl,
    we install test files under /var/lib/requestctl on the requestctl clients,
    with the peculiarity that all action rules will be printed out, including
    the ones that are not enabled globally / in the current site.
    
    Change-Id: Id92fb18ed2f4747556938981b5c25daf5d6ed5cf

--- modules/profile/manifests/conftool/requestctl_client.pp
+++ modules/profile/manifests/conftool/requestctl_client.pp
@@ -1,5 +1,22 @@
-class profile::conftool::requestctl_client() {
+class profile::conftool::requestctl_client(
+        String $conftool_prefix = lookup('conftool_prefix'),
+) {
     require profile::conftool::client
     ensure_packages(['python3-conftool-requestctl'])
+    # Create the test directory
+    file { ['/var/lib/requestctl', '/var/lib/requestct/tests']:
+        ensure => directory,
+    }
+
+    # Install the per-cluster test files.
+    ['text', 'cache'].each |$cache_cluster| {
+        $is_test = true
+        confd::file { "/var/lib/requestctl/${cache_cluster}-actions.inc.vcl":
+            ensure     => 'present',
+            watch_keys => ['/request-patterns', "/request-actions/cache-${cache_cluster}"],
+            content    => template('profile/cache/blocked-nets.inc.vcl.tpl.erb'),
+            prefix     => $conftool_prefix
+        }
+    }
     # TODO: add an alert if there are uncommitted changes
 }
