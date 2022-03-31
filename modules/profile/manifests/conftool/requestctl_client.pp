631726d62ea7a4266483e2df07a4efdfac0be9e1
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2022-03-31 08:38:19 +0200

    profile::requestctl_client: brown paper bag fix
    Change-Id: Iccc85f2a3e549f72008a11812bcd4511669d1577

--- modules/profile/manifests/conftool/requestctl_client.pp
+++ modules/profile/manifests/conftool/requestctl_client.pp
@@ -4,7 +4,7 @@ class profile::conftool::requestctl_client(
     require profile::conftool::client
     ensure_packages(['python3-conftool-requestctl'])
     # Create the test directory
-    file { ['/var/lib/requestctl', '/var/lib/requestct/tests']:
+    file { ['/var/lib/requestctl', '/var/lib/requestctl/tests']:
         ensure => directory,
     }
 
