f7f2b3499ee610be31389bde8dc561dd746a4a49
Marc Mandere <mmandere@wikimedia.org>
2022-04-05 11:49:38 +0300

    site: Reimage cp4035 as cache::text_haproxy
    Bug: T290005
    Change-Id: I060501a15fa317196a7dbdcc240752df4e9f19f9

--- manifests/site.pp
+++ manifests/site.pp
@@ -426,12 +426,12 @@ node /^cp40(2[2-6]|34)\.ulsfo\.wmnet$/ {
     role(cache::upload_haproxy)
 }
 
-node /^cp40(27|35)\.ulsfo\.wmnet$/ {
+node /^cp4027\.ulsfo\.wmnet$/ {
     role(cache::text)
 }
 
 # HAProxy test - T290005
-node /^cp40(2[89]|3[026])\.ulsfo.wmnet$/ {
+node /^cp40(2[89]|3[0256])\.ulsfo.wmnet$/ {
     role(cache::text_haproxy)
 }
 
