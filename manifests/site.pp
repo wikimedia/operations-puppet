2fe24f1f6ce34564360c4c00d71660ee52074533
Marc Mandere <mmandere@wikimedia.org>
2022-04-07 17:42:54 +0300

    site: Reimage cp6010 as cache::text_haproxy
    Bug: T290005
    Change-Id: Ib1fc3b45373759bbf6e1297884852ec62b45bf34

--- manifests/site.pp
+++ manifests/site.pp
@@ -448,12 +448,12 @@ node /^cp600[3-8]\.drmrs\.wmnet$/ {
     role(cache::upload_haproxy)
 }
 
-node /^cp60(09|10)\.drmrs\.wmnet$/ {
+node 'cp6009.drmrs.wmnet' {
     role(cache::text)
 }
 
 # HAProxy test T290005
-node /^cp601[1-6]\.drmrs\.wmnet$/ {
+node /^cp601[0-6]\.drmrs\.wmnet$/ {
     role(cache::text_haproxy)
 }
 
