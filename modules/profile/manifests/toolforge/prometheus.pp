95a22bcf5588b66fb55e98082792c15252b45556
Taavi Väänänen <hi@taavi.wtf>
2022-04-10 19:21:52 +0300

    P:toolforge::prometheus: remove paws jobs
    PAWS now has its own Prometheus server.
    
    Bug: T304716
    Change-Id: I591a896d922b8e67dfcca1fc3408ec0be3d0c1eb

--- modules/profile/manifests/toolforge/prometheus.pp
+++ modules/profile/manifests/toolforge/prometheus.pp
@@ -4,9 +4,7 @@
 
 class profile::toolforge::prometheus (
     Stdlib::Fqdn               $new_k8s_apiserver_fqdn = lookup('profile::toolforge::k8s::apiserver_fqdn', {default_value => 'k8s.tools.eqiad1.wikimedia.cloud'}),
-    Stdlib::Fqdn               $paws_apiserver_fqdn    = lookup('profile::wmcs::paws::k8s::apiserver_fqdn', {default_value => 'k8s.svc.paws.eqiad1.wikimedia.cloud'}),
     Stdlib::Port               $new_k8s_apiserver_port = lookup('profile::toolforge::k8s::apiserver_port', {default_value => 6443}),
-    Stdlib::Port               $paws_apiserver_port    = lookup('profile::wmcs::paws::apiserver_port',     {default_value => 6443}),
     Stdlib::Fqdn               $keystone_api_fqdn      = lookup('profile::openstack::eqiad1::keystone_api_fqdn'),
     String                     $observer_password      = lookup('profile::openstack::eqiad1::observer_password'),
     String                     $observer_user          = lookup('profile::openstack::base::observer_user'),
@@ -37,15 +35,9 @@ class profile::toolforge::prometheus (
         require => Package['prometheus'], # group is defined by the package?
         notify  => Service['prometheus@tools'],
     }
-    $paws_certname  = 'paws-k8s-prometheus'
-    $paws_cert_pub  = "/etc/ssl/localcerts/${paws_certname}.crt"
-    $paws_cert_priv = "/etc/ssl/private/${paws_certname}.key"
-    sslcert::certificate { $paws_certname:
-        ensure  => present,
-        chain   => false,
-        group   => 'prometheus',
-        require => Package['prometheus'], # group is defined by the package?
-        notify  => Service['prometheus@tools'],
+
+    sslcert::certificate { 'paws-k8s-prometheus':
+        ensure  => absent,
     }
 
     prometheus::server { 'tools':
@@ -331,261 +323,6 @@ class profile::toolforge::prometheus (
                 ]
             },
             {
-                'job_name'             => 'paws-node',
-                'openstack_sd_configs' => [
-                    {
-                        'role'              => 'instance',
-                        'region'            => 'eqiad1-r',
-                        'identity_endpoint' => "https://${keystone_api_fqdn}:25000/v3",
-                        'username'          => $observer_user,
-                        'password'          => $observer_password,
-                        'domain_name'       => 'default',
-                        'project_name'      => 'paws',
-                        'all_tenants'       => false,
-                        'refresh_interval'  => '5m',
-                        'port'              => 9100,
-                    }
-                ],
-                'relabel_configs'      => [
-                    {
-                        'source_labels' => ['__meta_openstack_project_id'],
-                        'replacement'   => 'paws',
-                        'target_label'  => 'project',
-                    },
-                    {
-                        'source_labels' => ['job'],
-                        'replacement'   => 'node',
-                        'target_label'  => 'job',
-                    },
-                    {
-                        'source_labels' => ['__meta_openstack_instance_name'],
-                        'target_label'  => 'instance',
-                    },
-                    {
-                        'source_labels' => ['__meta_openstack_instance_status'],
-                        'action'        => 'keep',
-                        'regex'         => 'ACTIVE',
-                    },
-                ]
-            },
-            {
-                'job_name'              => 'paws-k8s-nodes',
-                'scheme'                => 'https',
-                'tls_config'            => {
-                    'insecure_skip_verify' => true,
-                    'cert_file'            => $paws_cert_pub,
-                    'key_file'             => $paws_cert_priv,
-                },
-                'kubernetes_sd_configs' => [
-                    {
-                        'api_server' => "https://${paws_apiserver_fqdn}:${paws_apiserver_port}",
-                        'role'       => 'node',
-                        'tls_config' => {
-                            'insecure_skip_verify' => true,
-                            'cert_file'            => $paws_cert_pub,
-                            'key_file'             => $paws_cert_priv,
-                        },
-                    },
-                ],
-                'relabel_configs'       => [
-                    {
-                        'action' => 'labelmap',
-                        'regex'  => '__meta_kubernetes_node_label_(.+)',
-                    },
-                    {
-                        'target_label' => '__address__',
-                        'replacement'  => "${paws_apiserver_fqdn}:${paws_apiserver_port}",
-                    },
-                    {
-                        'source_labels' => ['__meta_kubernetes_node_name'],
-                        'regex'         => '(.+)',
-                        'target_label'  => '__metrics_path__',
-                        # lint:ignore:single_quote_string_with_variables
-                        'replacement'   => '/api/v1/nodes/${1}/proxy/metrics',
-                        # lint:endignore
-                    },
-                ]
-            },
-            {
-                'job_name'              => 'paws-ingress-nginx',
-                'scheme'                => 'https',
-                'tls_config'            => {
-                    'insecure_skip_verify' => true,
-                    'cert_file'            => $paws_cert_pub,
-                    'key_file'             => $paws_cert_priv,
-                },
-                'kubernetes_sd_configs' => [
-                    {
-                        'api_server' => "https://${paws_apiserver_fqdn}:${paws_apiserver_port}",
-                        'role'       => 'pod',
-                        'tls_config' => {
-                            'insecure_skip_verify' => true,
-                            'cert_file'            => $paws_cert_pub,
-                            'key_file'             => $paws_cert_priv,
-                        },
-                    },
-                ],
-                'relabel_configs'       => [
-                    {
-                        'action'        => 'keep',
-                        'regex'         => 'ingress-nginx',
-                        'source_labels' => ['__meta_kubernetes_pod_label_app_kubernetes_io_name'],
-                    },
-                    {
-                        'action' => 'labelmap',
-                        'regex'  => '__meta_kubernetes_pod_label_(.+)',
-                    },
-                    {
-                        'target_label' => '__address__',
-                        'replacement'  => "${paws_apiserver_fqdn}:${paws_apiserver_port}",
-                    },
-                    {
-                        'source_labels' => ['__meta_kubernetes_pod_name'],
-                        'regex'         => '(ingress-nginx-gen2-controller-[a-zA-Z0-9]+-[a-zA-Z0-9]+)',
-                        'target_label'  => '__metrics_path__',
-                        # lint:ignore:single_quote_string_with_variables
-                        # PORT is not arbitrary! the pod is listening on that one
-                        'replacement'   => '/api/v1/namespaces/ingress-nginx-gen2/pods/${1}:10254/proxy/metrics',
-                        # lint:endignore
-                    },
-                ]
-            },
-            {
-                'job_name'              => 'paws-api',
-                'scheme'                => 'https',
-                'tls_config'            => {
-                    'insecure_skip_verify' => true,
-                    'cert_file'            => $paws_cert_pub,
-                    'key_file'             => $paws_cert_priv,
-                },
-                'kubernetes_sd_configs' => [
-                    {
-                        'api_server' => "https://${paws_apiserver_fqdn}:${paws_apiserver_port}",
-                        'role'       => 'endpoints',
-                        'tls_config' => {
-                            'insecure_skip_verify' => true,
-                            'cert_file'            => $paws_cert_pub,
-                            'key_file'             => $paws_cert_priv,
-                        },
-                    },
-                ],
-                'relabel_configs'       => [
-                    {
-                        'action' => 'labelmap',
-                        'regex'  => '__meta_kubernetes_node_label_(.+)',
-                    },
-                    {
-                        'target_label' => '__address__',
-                        'replacement'  => "${paws_apiserver_fqdn}:${paws_apiserver_port}",
-                    },
-                    {
-                        'source_labels' => ['__meta_kubernetes_namespace',
-                                            '__meta_kubernetes_service_name',
-                                            '__meta_kubernetes_endpoint_port_name'],
-                        'regex'         => 'default;kubernetes;https',
-                        'action'        => 'keep',
-                    },
-                ]
-            },
-            {
-                'job_name'       => 'paws-haproxy',
-                'scheme'         => 'http',
-                'static_configs' => [
-                    {
-                        'targets' => ["${paws_apiserver_fqdn}:9901"],
-                    },
-                ],
-            },
-            {
-                'job_name'              => 'paws-kube-state-metrics',
-                'scheme'                => 'https',
-                'tls_config'            => {
-                    'insecure_skip_verify' => true,
-                    'cert_file'            => $paws_cert_pub,
-                    'key_file'             => $paws_cert_priv,
-                },
-                'kubernetes_sd_configs' => [
-                    {
-                        'api_server' => "https://${paws_apiserver_fqdn}:${paws_apiserver_port}",
-                        'role'       => 'pod',
-                        'tls_config' => {
-                            'insecure_skip_verify' => true,
-                            'cert_file'            => $paws_cert_pub,
-                            'key_file'             => $paws_cert_priv,
-                        },
-                    },
-                ],
-                'relabel_configs'       => [
-                    {
-                        'action' => 'labelmap',
-                        'regex'  => '__meta_kubernetes_node_label_(.+)',
-                    },
-                    {
-                        'target_label' => '__address__',
-                        'replacement'  => "${paws_apiserver_fqdn}:${paws_apiserver_port}",
-                    },
-                    {
-                        'target_label' => '__metrics_path__',
-                        # this service is not an arbitrary name; it was created
-                        # inside the k8s cluster with that specific name
-                        'replacement'  => '/api/v1/namespaces/metrics/services/kube-state-metrics/proxy/metrics',
-                    },
-                ]
-            },
-            {
-                'job_name'              => 'paws-cadvisor',
-                'scheme'                => 'https',
-                'tls_config'            => {
-                    'insecure_skip_verify' => true,
-                    'cert_file'            => $paws_cert_pub,
-                    'key_file'             => $paws_cert_priv,
-                },
-                'kubernetes_sd_configs' => [
-                    {
-                        'api_server' => "https://${paws_apiserver_fqdn}:${paws_apiserver_port}",
-                        'role'       => 'pod',
-                        'tls_config' => {
-                            'insecure_skip_verify' => true,
-                            'cert_file'            => $paws_cert_pub,
-                            'key_file'             => $paws_cert_priv,
-                        },
-                    },
-                ],
-                'relabel_configs'       => [
-                    {
-                        'action'        => 'keep',
-                        'regex'         => 'cadvisor',
-                        'source_labels' => ['__meta_kubernetes_pod_label_app'],
-                    },
-                    {
-                        'action' => 'labelmap',
-                        'regex'  => '__meta_kubernetes_pod_label_(.+)',
-                    },
-                    {
-                        'target_label' => '__address__',
-                        'replacement'  => "${paws_apiserver_fqdn}:${paws_apiserver_port}",
-                    },
-                    {
-                        'source_labels' => ['__meta_kubernetes_pod_name'],
-                        'regex'         => '(cadvisor-[a-zA-Z0-9]+)',
-                        'target_label'  => '__metrics_path__',
-                        # lint:ignore:single_quote_string_with_variables
-                        'replacement'   => '/api/v1/namespaces/metrics/pods/${1}/proxy/metrics',
-                        # lint:endignore
-                    },
-                ]
-            },
-            {
-                'job_name'       => 'paws-jupyterhub',
-                'scheme'         => 'https',
-                'metrics_path'   => '/hub/metrics',
-                'static_configs' => [
-                    {
-                        'targets' => ['hub.paws.wmcloud.org'],
-                    },
-                ],
-            },
-            {
                 'job_name'       => 'tools-email',
                 'scheme'         => 'http',
                 'static_configs' => [
@@ -627,11 +364,8 @@ class profile::toolforge::prometheus (
     }
 
     cron { 'prometheus_paws_k8s_etcd_targets':
-        ensure  => present,
-        command => "/usr/local/bin/prometheus-labs-targets --project paws --port 2381 --prefix paws-k8s-control- > ${targets_path}/etcd_paws.$$ && mv ${targets_path}/etcd_paws.$$ ${targets_path}/etcd_paws.yml",
-        minute  => '*/10',
-        hour    => '*',
-        user    => 'prometheus',
+        ensure => absent,
+        user   => 'prometheus',
     }
 
     cron { 'prometheus_tools_project_ssh_targets':
