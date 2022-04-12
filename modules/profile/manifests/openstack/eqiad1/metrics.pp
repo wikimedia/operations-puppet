3230e41faa32953332a6ca65705b21b0f2499c7f
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2022-04-12 18:59:29 +0200

    prometheus-openstack-exporter: only run it on the primary server
    Only run the prometheus-openstack-exporter on the primary server, otherwise
    we see a lot of unneccesary API requests.
    
    Bug: T302178
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: I8f25f3e3702864cc63ea0888ffe5c043ba695e54

--- modules/profile/manifests/openstack/eqiad1/metrics.pp
+++ modules/profile/manifests/openstack/eqiad1/metrics.pp
@@ -1,8 +1,18 @@
 class profile::openstack::eqiad1::metrics (
+    Stdlib::Fqdn $controller = lookup('profile::openstack::eqiad1::keystone_api_fqdn'),
 ) {
-    class { '::profile::prometheus::openstack_exporter':
-        listen_port => 12345,
-        cloud       => 'eqiad1',
+    $this_ip = ipresolve($::fqdn, 4)
+    $controller_ip = ipresolve($controller, 4)
+
+    if $this_ip == $controller_ip {
+        class { '::profile::prometheus::openstack_exporter':
+            listen_port => 12345,
+            cloud       => 'eqiad1',
+        }
+        contain '::profile::prometheus::openstack_exporter'
+    } else {
+        class { '::profile::prometheus::openstack_exporter':
+            ensure => absent,
+        }
     }
-    contain '::profile::prometheus::openstack_exporter'
 }
--- modules/profile/manifests/prometheus/openstack_exporter.pp
+++ modules/profile/manifests/prometheus/openstack_exporter.pp
@@ -1,6 +1,7 @@
 class profile::prometheus::openstack_exporter (
     Stdlib::Port $listen_port = lookup('profile::prometheus::openstack_exporter::listen_port', {default_value => 12345}),
     String[1]    $cloud       = lookup('profile::prometheus::openstack_exporter::cloud',       {default_value => 'eqiad1'}),
+    Enum['present', 'absent'] $ensure = lookup('profile::prometheus::openstack_exporter::ensure', {default_value => 'present'}),
 ){
     # package only available on bullseye
     debian::codename::require::min('bullseye')
@@ -13,7 +14,7 @@ class profile::prometheus::openstack_exporter (
     $config_file = '/etc/prometheus-openstack-exporter.yaml'
 
     file { $config_file:
-        ensure  => 'present',
+        ensure  => $ensure,
         owner   => 'prometheus',
         group   => 'prometheus',
         mode    => '0440',
@@ -21,12 +22,12 @@ class profile::prometheus::openstack_exporter (
     }
 
     file { '/usr/local/sbin/prometheus-openstack-exporter-wrapper':
-        ensure => 'present',
+        ensure => $ensure,
         source => 'puppet:///modules/profile/prometheus/prometheus-openstack-exporter-wrapper.sh',
     }
 
     systemd::service { 'prometheus-openstack-exporter':
-        ensure         => present,
+        ensure         => $ensure,
         content        => systemd_template('prometheus-openstack-exporter'),
         restart        => true,
         override       => false,
