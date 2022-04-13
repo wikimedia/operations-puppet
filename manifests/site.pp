0dad7d9d0c7f7a22f2ade30c049110bc8ae5d4a9
Hugh Nowlan <hnowlan@wikimedia.org>
2022-04-13 13:12:23 +0100

    Set production role and add config for restbase2027
    This is a new host that needs to be added to the cluster
    
    Change-Id: Ie99d5bfb998e062061a95b5e7277845a54355e4e

--- manifests/site.pp
+++ manifests/site.pp
@@ -2713,13 +2713,8 @@ node /^restbase10(1[6-9]|2[0-9]|30)\.eqiad\.wmnet$/ {
 }
 
 # restbase codfw cluster
-node /^restbase20(09|1[0-9]|2[0-6])\.codfw\.wmnet$/ {
+node /^restbase20(09|1[0-9]|2[0-7])\.codfw\.wmnet$/ {
     role(restbase::production)
-}
-
-# codfw new restbase host T301399
-node 'restbase2027.codfw.wmnet' {
-    role(insetup)
 }
 
 # cassandra/restbase dev cluster
