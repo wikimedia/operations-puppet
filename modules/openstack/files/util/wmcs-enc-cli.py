886129517f888da97f5714528eedf291b13edcbe
Taavi Väänänen <hi@taavi.wtf>
2022-04-13 18:44:51 +0300

    openstack: make enc-cli authenticate via keystone
    Bug: T274666
    Change-Id: Ic99492a7819742242ae648a3e70db3ef316ab86f

--- modules/openstack/files/util/wmcs-enc-cli.py
+++ modules/openstack/files/util/wmcs-enc-cli.py
@@ -4,7 +4,19 @@ import sys
 import yaml
 from argparse import ArgumentParser, Namespace
 
+import mwopenstackclients
 import requests
+
+
+def get_url(clients, project):
+    keystone = clients.keystoneclient()
+    proxy = keystone.services.list(type="puppet-enc")[0]
+    endpoint = keystone.endpoints.list(
+        service=proxy.id, interface="public", enabled=True)[0]
+    enc_api_url = endpoint.url.replace("$(project_id)s", project)
+    session = clients.session(project)
+
+    return enc_api_url, session
 
 
 class EncError(Exception):
@@ -12,8 +24,8 @@ class EncError(Exception):
 
 
 class EncConnection:
-    def __init__(self, enc_url, openstack_project):
-        self.enc_url = enc_url
+    def __init__(self, clients, openstack_project):
+        self.enc_url, self.session = get_url(clients, openstack_project)
         self.openstack_project = openstack_project
 
     def get_project_hiera(self) -> requests.Response:
@@ -22,12 +34,12 @@ class EncConnection:
         return self.get_prefix_hiera(prefix=" ")
 
     def get_prefix_hiera(self, prefix: str) -> requests.Response:
-        response = requests.get(
-            "{0}/{1}/prefix/{2}/hiera".format(
+        response = self.session.get(
+            "{0}/prefix/{1}/hiera".format(
                 self.enc_url,
-                self.openstack_project,
                 prefix,
-            )
+            ),
+            raise_exc=False
         )
         if not response.ok:
             raise EncError(
@@ -41,14 +53,14 @@ class EncConnection:
         return response
 
     def set_prefix_hiera(self, prefix: str, data: str) -> requests.Response:
-        response = requests.post(
-            "{0}/{1}/prefix/{2}/hiera".format(
+        response = self.session.post(
+            "{0}/prefix/{1}/hiera".format(
                 self.enc_url,
-                self.openstack_project,
                 prefix,
             ),
             # make sure it's pretty formatted
             yaml.dump(yaml.safe_load(data)),
+            raise_exc=False
         )
         if not response.ok:
             raise EncError(
@@ -63,14 +75,14 @@ class EncConnection:
         return response
 
     def set_prefix_roles(self, prefix: str, data: str) -> requests.Response:
-        response = requests.post(
-            "{0}/{1}/prefix/{2}/roles".format(
+        response = self.session.post(
+            "{0}/prefix/{1}/roles".format(
                 self.enc_url,
-                self.openstack_project,
                 prefix,
             ),
             # make sure it's pretty formatted
             yaml.dump(yaml.safe_load(data)),
+            raise_exc=False
         )
         if not response.ok:
             raise EncError(
@@ -89,12 +101,12 @@ class EncConnection:
         This gives the results of applying all the openstack_project + prefix
         + node configs, ready to be used by puppet.
         """
-        response = requests.get(
-            "{0}/{1}/node/{2}".format(
+        response = self.session.get(
+            "{0}/node/{1}".format(
                 self.enc_url,
-                self.openstack_project,
                 fqdn,
-            )
+            ),
+            raise_exc=False
         )
         if not response.ok:
             raise EncError(
@@ -113,12 +125,12 @@ class EncConnection:
         the ones for the prefix and openstack_project.
         """
         # Yep, we treat the hostname as a prefix itself
-        response = requests.get(
-            "{0}/{1}/prefix/{2}".format(
+        response = self.session.get(
+            "{0}/prefix/{1}".format(
                 self.enc_url,
-                self.openstack_project,
                 fqdn,
-            )
+            ),
+            raise_exc=False
         )
         if not response.ok:
             raise EncError(
@@ -134,9 +146,10 @@ class EncConnection:
 
 def main(args: Namespace) -> int:
     enc_connection = EncConnection(
-        enc_url=args.enc_url,
-        openstack_project=args.openstack_project,
+        mwopenstackclients.Clients(envfile=args.envfile),
+        args.openstack_project
     )
+
     fn = getattr(enc_connection, args.action)
     res = fn(*args.params)
     res.raise_for_status()
@@ -150,12 +163,9 @@ if __name__ == "__main__":
         action for action in dir(EncConnection) if not action.startswith("_")
     ]
     parser.add_argument(
-        "--enc-url",
-        default="http://puppet-enc.cloudinfra.wmcloud.org:8101/v1",
-        help=(
-            "Full url to the enc, for example "
-            "http://puppet-enc.cloudinfra.wmcloud.org:8101/v1"
-        ),
+        "--envfile",
+        default="/etc/novaadmin.yaml",
+        help="Path to OpenStack authentication YAML file"
     )
     parser.add_argument("--openstack-project")
     parser.add_argument("action", choices=available_actions)
