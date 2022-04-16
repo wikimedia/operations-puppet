e1adf923a9e5deecb5e9a8cd68fd4af8681a4f44
Taavi Väänänen <hi@taavi.wtf>
2022-04-16 21:20:47 +0300

    kubeadm: remove metrics files
    These are now deployed from a separate repository[0] and deployed using
    the wmcs.toolforge.k8s.component.deploy cookbook.
    
    [0]: https://gitlab.wikimedia.org/repos/cloud/toolforge/kubernetes-metrics
    
    Change-Id: Ie9fafafd2884410fd7cf648fad5e455cb213856f

--- modules/kubeadm/manifests/metrics_yaml.pp
+++ modules/kubeadm/manifests/metrics_yaml.pp
@@ -1,45 +1,11 @@
-class kubeadm::metrics_yaml (
-) {
-    require ::kubeadm::core # because /etc/kubernetes
+class kubeadm::metrics_yaml () {
+    # Now served from a separate gitlab repository and deployed using the
+    # wmcs.toolforge.k8s.component.deploy cookbook.
+    # https://gitlab.wikimedia.org/repos/cloud/toolforge/kubernetes-metrics
 
     file { '/etc/kubernetes/metrics/':
-        ensure  => directory,
-        require => File['/etc/kubernetes'],
-    }
-
-    # for this to work you need to generate a x509 cert using the admin script
-    # the generated certs should be then moved as secrets to the private repo
-    # in a control node:
-    #   root:~# wmcs-k8s-get-cert prometheus
-    #
-    # See also: profile::toolforge::prometheus
-    # See also: https://wikitech.wikimedia.org/wiki/Portal:Toolforge/Admin/Deploying_k8s
-    file { '/etc/kubernetes/metrics/prometheus_metrics.yaml':
-        ensure  => present,
-        source  => 'puppet:///modules/kubeadm/metrics/prometheus_metrics.yaml',
-        require => File['/etc/kubernetes/metrics'],
-    }
-
-    # for this to work you need to generate a x509 cert and create a k8s secret
-    # in a control node:
-    #   root:~# wmcs-k8s-secret-for-cert -n kube-system -s metrics-server-certs -a metrics-server
-    #
-    # See also: https://wikitech.wikimedia.org/wiki/Portal:Toolforge/Admin/Deploying_k8s
-    file { '/etc/kubernetes/metrics/metrics-server.yaml':
-        ensure  => present,
-        source  => 'puppet:///modules/kubeadm/metrics/metrics-server.yaml',
-        require => File['/etc/kubernetes/metrics'],
-    }
-
-    file { '/etc/kubernetes/metrics/kube-state-metrics.yaml':
-        ensure  => present,
-        source  => 'puppet:///modules/kubeadm/metrics/kube-state-metrics.yaml',
-        require => File['/etc/kubernetes/metrics'],
-    }
-
-    file { '/etc/kubernetes/metrics/cadvisor.yaml':
-        ensure  => present,
-        source  => 'puppet:///modules/kubeadm/metrics/cadvisor.yaml',
-        require => File['/etc/kubernetes/metrics'],
+        ensure  => absent,
+        recurse => true,
+        force   => true,
     }
 }
