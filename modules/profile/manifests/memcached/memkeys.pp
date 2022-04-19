1f5903793b8279171c9bc8c4e110b33ebdd17ee3
Alexander Vorwerk <zabe@avorwerk.net>
2022-04-19 23:03:25 +0200

    memcached: migrate memkeys cron to systemd timer job
    Bug: T273673
    Change-Id: Ifb4cc1f72002a9d84a9f65eecb05a8aca4e60873

--- modules/profile/manifests/memcached/memkeys.pp
+++ modules/profile/manifests/memcached/memkeys.pp
@@ -2,23 +2,20 @@
 #
 # This class installs and configures memkeys, a tool to inspect memcache keys
 # usage in real time.
-# In addition to making it available for interactive use, it configures
-# a cronjob to run once a day and log 20 seconds' worth of memcached usage stats
+# In addition to making it available for interactive use, it configures a systemd
+# timer job to run once a day and log 20 seconds' worth of memcached usage stats
 # to a CSV file. That way, if there is a spike in memcached usage,
 # it is easier to diff the logs and see which keys are suspect.
 #
 class profile::memcached::memkeys {
 
-    package { 'memkeys':
-        ensure => present,
-        before => Cron['memkeys'],    }
+    ensure_packages(['memkeys'])
 
     file { '/var/log/memkeys':
         ensure => directory,
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
-        before => Cron['memkeys'],
     }
 
     file { '/etc/logrotate.d/memkeys':
@@ -26,7 +23,6 @@ class profile::memcached::memkeys {
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
-        before  => Cron['memkeys'],
         require => File['/var/log/memkeys'],
     }
 
@@ -35,15 +31,21 @@ class profile::memcached::memkeys {
         owner  => 'root',
         group  => 'root',
         mode   => '0555',
-        before => Cron['memkeys'],
+    }
+
+    $hour = fqdn_rand(24, 'hour-memkeys')
+    $minute = fqdn_rand(60, 'minute-memkeys')
+
+    systemd::timer::job { 'memkeys-snapshot':
+        ensure      => present,
+        description => 'Regular jobs to log memcached usage stats',
+        user        => 'root',
+        command     => '/usr/local/sbin/memkeys-snapshot',
+        interval    => {'start' => 'OnCalendar', 'interval' => "*-*-* ${hour}:${minute}:00"},
     }
 
     cron { 'memkeys':
-        ensure  => present,
-        command => '/usr/local/sbin/memkeys-snapshot',
-        user    => 'root',
-        hour    => fqdn_rand(24, 'hour-memkeys'),
-        minute  => fqdn_rand(60, 'minute-memkeys'),
+        ensure => absent,
     }
 
     rsyslog::conf { 'memkeys':
