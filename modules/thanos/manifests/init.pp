0402e9d50c734cd580b01efa9bcde32a70eba4c1
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2022-04-20 10:07:49 +0200

    thanos: reload rule via http endpoint
    Temporary until Thanos has been upgraded.
    
    Bug: T288726
    Bug: T303154
    Change-Id: I19ff3318aefb1f4d4f9472c81acccd97fbdc2300

--- modules/thanos/manifests/init.pp
+++ modules/thanos/manifests/init.pp
@@ -1,6 +1,9 @@
 class thanos {
     exec { 'reload thanos-rule':
-        command     => '/bin/systemctl reload thanos-rule',
+        # Workaround for SIGHUP not working in thanos 0.21.1
+        # https://github.com/thanos-io/thanos/issues/4432
+        # https://phabricator.wikimedia.org/T303154
+        command     => '/usr/bin/curl -X POST localhost:17902/-/reload',
         refreshonly => true,
     }
 }
