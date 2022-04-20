6565ad7c02eef821de86da5cd5809ae5c4aabce5
Valentin Gutierrez <vgutierrez@wikimedia.org>
2022-04-20 14:45:29 +0200

    cache: Fix varnish-frontend puppetization on non-etcd environments
    Change-Id: Id2fee3ea854936722c6eab6ea6e1008260cd7fe0

--- modules/profile/manifests/cache/varnish/frontend.pp
+++ modules/profile/manifests/cache/varnish/frontend.pp
@@ -113,29 +113,30 @@ class profile::cache::varnish::frontend (
 
     $directors_keyspaces = [ "${conftool_prefix}/pools/${::site}/cache_${cache_cluster}/ats-be" ]
     $req_keyspaces = ['/request-patterns', "/request-actions/cache-${cache_cluster}"]
-    confd::file {
-        default:
-            ensure => $etcd_backends.bool2str('present', 'absent'),
-            reload => "/usr/local/bin/confd-reload-vcl varnish-frontend ${reload_vcl_opts}",
-            before => Service['varnish-frontend'],;
-        # Backend caches used by this Frontend from Etcd
-        '/etc/varnish/directors.frontend.vcl':
-            watch_keys => $directors_keyspaces,
-            content    => template('profile/cache/varnish-frontend.directors.vcl.tpl.erb'),;
-        # Abuse networks
-        '/etc/varnish/blocked-nets.inc.vcl':
-            watch_keys => ['/request-ipblocks/abuse'],
-            content    => template('profile/cache/blocked-nets.inc.vcl.tpl.erb'),
-            prefix     => $conftool_prefix,;
-        # Request filter actions based on the content of the
-        # /request-actions tree in conftool.
-        # Only enabled if $use_etcd_req_filters is true.
-        '/etc/varnish/dynamic.actions.inc.vcl':
-            watch_keys => $req_keyspaces,
-            content    => template('profile/cache/varnish-frontend-dynamic-actions.vcl.tpl.erb'),
-            prefix     => $conftool_prefix,;
-    }
-    unless $etcd_backends {
+    if $etcd_backends {
+        confd::file {
+            default:
+                ensure => $etcd_backends.bool2str('present', 'absent'),
+                reload => "/usr/local/bin/confd-reload-vcl varnish-frontend ${reload_vcl_opts}",
+                before => Service['varnish-frontend'],;
+            # Backend caches used by this Frontend from Etcd
+            '/etc/varnish/directors.frontend.vcl':
+                watch_keys => $directors_keyspaces,
+                content    => template('profile/cache/varnish-frontend.directors.vcl.tpl.erb'),;
+            # Abuse networks
+            '/etc/varnish/blocked-nets.inc.vcl':
+                watch_keys => ['/request-ipblocks/abuse'],
+                content    => template('profile/cache/blocked-nets.inc.vcl.tpl.erb'),
+                prefix     => $conftool_prefix,;
+            # Request filter actions based on the content of the
+            # /request-actions tree in conftool.
+            # Only enabled if $use_etcd_req_filters is true.
+            '/etc/varnish/dynamic.actions.inc.vcl':
+                watch_keys => $req_keyspaces,
+                content    => template('profile/cache/varnish-frontend-dynamic-actions.vcl.tpl.erb'),
+                prefix     => $conftool_prefix,;
+        }
+    } else {
         # deployment-prep still uses the old template.
         $abuse_networks = network::parse_abuse_nets('varnish')
         file { '/etc/varnish/blocked-nets.inc.vcl':
@@ -144,6 +145,10 @@ class profile::cache::varnish::frontend (
             owner   => 'root',
             group   => 'root',
             mode    => '0444',
+        }
+
+        file { [ '/etc/varnish/directors.frontend.vcl', '/etc/varnish/dynamic.actions.inc.vcl' ]:
+            ensure => absent,
         }
     }
 
