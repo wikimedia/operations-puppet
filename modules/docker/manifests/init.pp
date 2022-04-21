5e58e340eb3545fcafc4330eed4c9acc154744b3
Daniel Zahn <dzahn@wikimedia.org>
2022-04-21 15:10:13 -0700

    docker: ensure apparmor package is installed if on bullseye
    On bullseye apparmor is now installed by default as opposed to buster.
    This is the "libapparmor1" package.
    
    The userspace utilities package "apparmor" is not installed by default
    though.
    
    Both come from the 'apparmor' source package but are not pulling in each
    other.
    
    This leads to Debian bug #989781 and you can't run containers.
    
    The docker-gc and docker-resource-monitor services will also fail.
    
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=989781
    
    After installing the apparmor package this is resolved.
    
    This happened to me when upgrading gitlab-runners to bullseye.
    
    For now I did the fix in the gitlab-runner class but it seems
    like this should be in the docker class in general.
    
    Change-Id: I2c914347d31233154842d1a41513eac9759fa779

--- modules/docker/manifests/init.pp
+++ modules/docker/manifests/init.pp
@@ -12,6 +12,13 @@
 # [*package_name*] Docker is going through various transitions changing package
 # names multiple times already. Support that so we can choose which one we want.
 # Defaults to docker-engine on < Buster, docker.io >= Buster (but this is subject to change).
+#
+# On Debian bullseye the 'libapparmor1' package is now installed by default but the
+# userspace uilities package 'apparmor' is not.
+# This leads to Debian bug #989781 (https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=989781).
+# Therefore we need to explicitly install it if on bullseye.
+# The good part: actually working apparmor for docker as opposed to previous distro versions.
+#
 class docker(
     Optional[String] $version = undef,
     Optional[String] $package_name = undef,
@@ -19,7 +26,7 @@ class docker(
     require ::docker::configuration
 
 
-        # If not set, pick a smart default value for docker packagename.
+    # If not set, pick a smart default value for docker packagename.
     if $package_name == undef {
         if debian::codename::lt('buster') {
             $_package_name = 'docker-engine'
@@ -36,6 +43,6 @@ class docker(
             ensure => $version,
         }
     } else {
-        ensure_packages($_package_name)
+        ensure_packages([$_package_name, 'apparmor'])
     }
 }
