f517d8026c5c942e8e7f884b24a45212a4864698
Sukhbir Singh <ssingh@wikimedia.org>
2022-04-21 13:52:14 -0400

    P:wikidough: add a check to ensure service has been restarted
    This commits adds an nrpe::monitor_service check to make sure that if a
    configuration file was changed, that the related service was restarted
    within a given time interval.
    
    For a more practical example (and the reason for this commit): some
    services such as dnsdist and pdns-recursor (both used by Wikidough)
    don't support reloading of the configuration and so their systemd
    service has to be restarted whenever a configuration file changes. But
    restarting also means clearing out the cache, so ideally we would like
    to manage such restarts ourselves versus letting Puppet do that and
    because of that, any change to a configuration file should not result in
    an automatic restart.
    
    The purpose of this script is to check if a configuration file change
    (mtime) is more recent than the subsequent service restart
    (ActiveEnterTimestamp), to remind us to restart the service (WARNING).
    If the time delta between the mtime and ActiveEnterTimestamp exceeds 24
    hours (configurable), then it raises the alert from WARNING to CRITICAL.
    
    Hosts: O:wikidough
    Change-Id: Ibe9390bcee4b68115f68ffa45e8feb73a5351025

---
+++ modules/profile/files/wikidough/servicerestartcheck.py
@@ -0,0 +1,103 @@
+#! /usr/bin/env python3
+
+"""servicerestartcheck.py: Really, you should restart that service!
+
+This script checks to make sure that if a configuration file was changed, the
+related systemd service was also restarted so that the service can pick up and
+apply the new changes.
+
+For a more practical example: some services such as dnsdist and pdns-recursor
+don't support reloading of the configuration and so their systemd service has
+to be restarted whenever a configuration file changes. But restarting also
+means clearing out the cache, so ideally we would like to manage such restarts
+ourselves versus letting Puppet do that and because of that, any change to a
+configuration file should not result in an automatic restart.
+
+The purpose of this script is to check if a configuration file change (mtime)
+is more recent than the subsequent service restart (ActiveEnterTimestamp), to
+remind us to restart the service (WARNING). If the time delta between the mtime
+and ActiveEnterTimestamp exceeds 24 hours (configurable), then it raises the
+alert from WARNING to CRITICAL.
+"""
+
+import argparse
+import enum
+import os
+import sys
+
+import pystemd
+
+
+class StatusCode(enum.IntEnum):
+    OK = 0
+    WARNING = 1
+    CRITICAL = 2
+
+
+def write_msg(status_code, msg):
+    print(msg)
+    sys.exit(status_code.value)
+
+
+def unit_active_time(service):
+    unit = pystemd.systemd1.Unit(service.encode())
+    unit.load()
+
+    # Check if the service is active and if not, we should report that.
+    if unit.Unit.ActiveState == b'active':
+        # From https://www.freedesktop.org/wiki/Software/systemd/dbus/,
+        # ActiveEnterTimestamp is in microseconds.
+        active_time = unit.Unit.ActiveEnterTimestamp
+        return active_time / 1000000
+    else:
+        write_msg(StatusCode.CRITICAL,
+                  f"CRITICAL: Service {service} is not active.")
+
+
+def file_last_modified(file_path):
+    try:
+        modified_time = os.path.getmtime(file_path)
+    except FileNotFoundError:
+        write_msg(StatusCode.CRITICAL,
+                  f"CRITICAL: Configuration file {file_path} not found.")
+
+    return modified_time
+
+
+def parse_args():
+    desc = ("Alerts if a configuration file change is more recent than that "
+            "of a systemd unit restart.")
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("-s", "--service",
+                        required=True,
+                        help="name of the service to monitor")
+    parser.add_argument("-f", "--file",
+                        required=True,
+                        help="path to the configuration file")
+    parser.add_argument("--critical",
+                        type=int, default=24,
+                        help="time (h) after which to raise alert to CRITICAL")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    unit_time = unit_active_time(args.service)
+    file_time = file_last_modified(args.file)
+    diff = file_time - unit_time
+
+    if file_time > unit_time:
+        alert = (StatusCode.CRITICAL if (diff > args.critical * 3600)
+                 else StatusCode.WARNING)
+        msg = (f"{alert.name}: Service {args.service} has not been restarted "
+               f"after {args.file} was changed (stale by {diff:.2f}s).")
+        write_msg(alert, msg)
+    else:
+        msg = (f"OK: {args.service} was restarted after {args.file} "
+               f"was changed (time delta of {diff * -1:.2f}s).")
+        write_msg(StatusCode.OK, msg)
+
+
+if __name__ == "__main__":
+    main()
--- modules/profile/manifests/wikidough.pp
+++ modules/profile/manifests/wikidough.pp
@@ -8,6 +8,8 @@ class profile::wikidough (
     Dnsdist::Http_headers     $custom_headers   = lookup('profile::wikidough::dnsdist::custom_headers'),
 ) {
 
+    ensure_packages(['python3-pystemd'])
+
     include network::constants
     include passwords::wikidough::dnsdist
 
@@ -94,6 +96,42 @@ class profile::wikidough (
         description   => 'Wikidough DoT Check (IPv6)',
         check_command => "check_tcp_ssl!${facts['ipaddress6']}!853",
         notes_url     => 'https://wikitech.wikimedia.org/wiki/Wikidough',
+    }
+
+    $service_check_file = '/usr/lib/nagios/plugins/service_restart_check'
+    file { $service_check_file:
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0555',
+        content => file('profile/wikidough/servicerestartcheck.py'),
+        require => [
+            Package['python3-pystemd'],
+        ],
+    }
+
+    sudo::user { 'nagios_service_restart_check':
+        user       => 'nagios',
+        privileges => [ "ALL = NOPASSWD: ${service_check_file}" ],
+        require    => File[$service_check_file],
+    }
+
+    $service_to_check = {
+        'dnsdist.service'       => '/etc/dnsdist/dnsdist.conf',
+        'pdns-recursor.service' => '/etc/powerdns/recursor.conf',
+    }
+    $service_to_check.each |$service, $conf_file| {
+      nrpe::monitor_service { "check_service_restart_${service}":
+          description    => "Check if ${service} has been restarted after ${conf_file} was changed",
+          nrpe_command   => "/usr/bin/sudo ${service_check_file} --service ${service} --file ${conf_file}",
+          check_interval => 360,  # 6h
+          retry_interval => 60,   # 1h
+          notes_url      => 'https://wikitech.wikimedia.org/wiki/Wikidough',
+          require        => [
+              File[$service_check_file],
+              Sudo::User['nagios_service_restart_check'],
+          ],
+      }
     }
 
     class { 'auditd':
