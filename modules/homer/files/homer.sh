5de3d5c605dab7673e437c0f782eaa59c7eb1216
Riccardo Coccioli <rcoccioli@wikimedia.org>
2022-04-26 23:51:37 +0200

    homer: suppress cryptography deprecation warning
    * The latest release of cryptography with the current version of
      paramiko included in homer does raise a CryptographyDeprecationWarning
      for the Blowfish algorigthm that has been deprecated.
    * In order to suppress this warning, set PYTHONWARNINGS. Not setting a
      specific message because from testing the filtering doesn't seem to
      match it but set the module to limit the filter that way.
    * See also: https://phabricator.wikimedia.org/P26600
    
    Change-Id: Ib21f65829c719eaaf462d1afa291211f71497295

--- modules/homer/files/homer.sh
+++ modules/homer/files/homer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-SSH_AUTH_SOCK=/run/keyholder/proxy.sock PATH="/srv/deployment/homer/venv/bin:${PATH}" exec homer "${@}"
+SSH_AUTH_SOCK=/run/keyholder/proxy.sock PATH="/srv/deployment/homer/venv/bin:${PATH}" PYTHONWARNINGS="ignore::UserWarning:paramiko.transport" exec homer "${@}"
