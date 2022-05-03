27dee62d39397fcf74fed6e4b2d3983f833aa245
Brian King <bking@wikimedia.org>
2022-05-03 17:14:24 -0500

    elastic: enable/disable ssl_ecdhe_curve based on OS version
    The version of nginx that ships with Bullseye does not support the
    $ssl_ecdhe_curve directive.
    
    Hosts: elastic1051.eqiad.wmnet,elastic2059.codfw.wmnet
    Hosts: cloudelastic1001.wikimedia.org,relforge1003.eqiad.wmnet
    Bug: T307510
    Change-Id: I175c8cf3fe832cac3f07a3845f89cb2fc8f7f4f7

--- modules/elasticsearch/manifests/tlsproxy.pp
+++ modules/elasticsearch/manifests/tlsproxy.pp
@@ -15,38 +15,56 @@
 #  port to expose tls on
 #
 define elasticsearch::tlsproxy (
-    Stdlib::Port $upstream_port,
-    Stdlib::Port $tls_port,
-    Array[String] $certificate_names = [],
-    Array[Stdlib::Host] $server_aliases = [],
-    Boolean $acme_chief = false,
-    Optional[String] $acme_certname = undef,
-    String $server_name = $::fqdn,
-    Boolean $read_only = false,
-    Boolean $enable_http2 = false,
-){
-    tlsproxy::localssl { $title:
-        certs             => $certificate_names,
-        server_name       => $server_name,
-        server_aliases    => $server_aliases,
-        default_server    => true,
-        acme_chief        => $acme_chief,
-        acme_certname     => $acme_certname,
-        upstream_ports    => [$upstream_port],
-        tls_port          => $tls_port,
-        only_get_requests => $read_only,
-        enable_http2      => $enable_http2,
-    }
+  Stdlib::Port $upstream_port,
+  Stdlib::Port $tls_port,
+  Array[String] $certificate_names    = [],
+  Array[Stdlib::Host] $server_aliases = [],
+  Boolean $acme_chief                 = false,
+  Optional[String] $acme_certname     = undef,
+  String $server_name                 = $::fqdn,
+  Boolean $read_only                  = false,
+  Boolean $enable_http2               = false,
+) {
 
-    $check_command = $acme_chief ? {
-        true    => 'check_ssl_on_port_letsencrypt',
-        default => 'check_ssl_on_port',
-    }
+  # on stretch (Debian 9) we used a custom patched version of nginx, which added the
+  # ssl_ecdhe_curve directive.  Starting with buster we simply use the default nginx
+  # from Debian , so this directive is does not work/is not needed.
 
-    monitoring::service { "elasticsearch-https-${title}":
-        ensure        => present,
-        description   => "Elasticsearch HTTPS for ${title}",
-        check_command => "${check_command}!${server_name}!${tls_port}",
-        notes_url     => 'https://wikitech.wikimedia.org/wiki/Search',
+  case $facts['os']['release']['major'] {
+    '9': {
+      $ssl_ecdhe_curve = true
     }
+    /(10|11)/: {
+      $ssl_ecdhe_curve = false
+    }
+    default: { fail("Major OS release detected as (${facts['os']['release']['major']}) , should be one of: 9, 10, 11") }
+  }
+
+  tlsproxy::localssl { $title:
+    certs             => $certificate_names,
+    server_name       => $server_name,
+    server_aliases    => $server_aliases,
+    default_server    => true,
+    acme_chief        => $acme_chief,
+    acme_certname     => $acme_certname,
+    upstream_ports    => [$upstream_port],
+    tls_port          => $tls_port,
+    only_get_requests => $read_only,
+    enable_http2      => $enable_http2,
+    ssl_ecdhe_curve   => $ssl_ecdhe_curve,
+  }
+
+
+
+  $check_command = $acme_chief ? {
+    true    => 'check_ssl_on_port_letsencrypt',
+    default => 'check_ssl_on_port',
+  }
+
+  monitoring::service { "elasticsearch-https-${title}":
+    ensure        => present,
+    description   => "Elasticsearch HTTPS for ${title}",
+    check_command => "${check_command}!${server_name}!${tls_port}",
+    notes_url     => 'https://wikitech.wikimedia.org/wiki/Search',
+  }
 }
