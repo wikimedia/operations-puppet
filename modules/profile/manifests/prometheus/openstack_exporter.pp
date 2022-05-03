86b8c37c31ac79dfc2c76b730554f0b76a2fbd28
David Caro <dcaro@wikimedia.org>
2022-05-03 12:41:37 +0200

    openstack_exporter: don't use ensure twice for service
    This also includes using better typing and adding tests, that would
    have caught the error added in:
    I8f25f3e3702864cc63ea0888ffe5c043ba695e54
    
    Before it reached production.
    
    Bug: T302178
    Change-Id: Id030041d4bf7bdd03576228fa4b93feb696a42a7
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- modules/profile/manifests/prometheus/openstack_exporter.pp
+++ modules/profile/manifests/prometheus/openstack_exporter.pp
@@ -1,7 +1,7 @@
 class profile::prometheus::openstack_exporter (
-    Stdlib::Port $listen_port = lookup('profile::prometheus::openstack_exporter::listen_port', {default_value => 12345}),
-    String[1]    $cloud       = lookup('profile::prometheus::openstack_exporter::cloud',       {default_value => 'eqiad1'}),
-    Enum['present', 'absent'] $ensure = lookup('profile::prometheus::openstack_exporter::ensure', {default_value => 'present'}),
+    Stdlib::Port   $listen_port = lookup('profile::prometheus::openstack_exporter::listen_port', {default_value => 12345}),
+    String[1]      $cloud       = lookup('profile::prometheus::openstack_exporter::cloud', {default_value => 'eqiad1'}),
+    Wmflib::Ensure $ensure      = lookup('profile::prometheus::openstack_exporter::ensure', {default_value => 'present'}),
 ){
     # package only available on bullseye
     debian::codename::require::min('bullseye')
@@ -14,7 +14,7 @@ class profile::prometheus::openstack_exporter (
     $config_file = '/etc/prometheus-openstack-exporter.yaml'
 
     file { $config_file:
-        ensure  => $ensure,
+        ensure  => stdlib::ensure($ensure, 'file'),
         owner   => 'prometheus',
         group   => 'prometheus',
         mode    => '0440',
@@ -22,20 +22,17 @@ class profile::prometheus::openstack_exporter (
     }
 
     file { '/usr/local/sbin/prometheus-openstack-exporter-wrapper':
-        ensure => $ensure,
+        ensure => stdlib::ensure($ensure, 'file'),
         source => 'puppet:///modules/profile/prometheus/prometheus-openstack-exporter-wrapper.sh',
     }
 
     systemd::service { 'prometheus-openstack-exporter':
-        ensure         => $ensure,
-        content        => systemd_template('prometheus-openstack-exporter'),
-        restart        => true,
-        override       => false,
-        require        => File[$config_file],
-        service_params => {
-            ensure     => 'running',
-        },
-        subscribe      => [
+        ensure    => $ensure,
+        content   => systemd_template('prometheus-openstack-exporter'),
+        restart   => true,
+        override  => false,
+        require   => File[$config_file],
+        subscribe => [
             File[$config_file],
         ],
     }
---
+++ modules/profile/spec/classes/profile_prometheus_openstack_exporter_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../../../../rake_modules/spec_helper'
+describe 'profile::prometheus::openstack_exporter' do
+  let(:pre_condition) { }
+  on_supported_os(WMFConfig.test_on(11)).each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) { {
+        'listen_port' => 4242,
+        'cloud'       => 'eqiad1',
+      } }
+      context "compiles without errors" do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_service('prometheus-openstack-exporter').with_ensure('running') }
+        it { is_expected.to contain_file('/etc/prometheus-openstack-exporter.yaml').with_ensure('file') }
+        it { is_expected.to contain_file('/usr/local/sbin/prometheus-openstack-exporter-wrapper').with_ensure('file') }
+      end
+
+      context "removes everything when ensure absent" do
+        let(:params) {
+          super().merge(ensure: 'absent')
+        }
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_service('prometheus-openstack-exporter').with_ensure('stopped') }
+        it { is_expected.to contain_file('/etc/prometheus-openstack-exporter.yaml').with_ensure('absent') }
+        it { is_expected.to contain_file('/usr/local/sbin/prometheus-openstack-exporter-wrapper').with_ensure('absent') }
+      end
+    end
+  end
+end
