fe6402da281f00b2bb413fa59854bdf5923f2d05
Taavi Väänänen <hi@taavi.wtf>
2022-05-05 16:36:52 +0300

    P:wmcs: toolsdb: support mariadb 10.4 on bullseye
    Bug: T301949
    Change-Id: I558da8af8940338a8ac69c15637f5931d6953f99

--- modules/profile/manifests/wmcs/services/toolsdb_apt_pinning.pp
+++ modules/profile/manifests/wmcs/services/toolsdb_apt_pinning.pp
@@ -1,23 +1,36 @@
 class profile::wmcs::services::toolsdb_apt_pinning (
 ) {
-    # if you are hitting this assert, you likely need to refresh package pins
-    debian::codename::require('stretch')
-
-    apt::pin { 'toolsdb_fixed_mariadb_version':
-        package  => 'wmf-mariadb101',
-        pin      => 'version 10.1.39-1',
-        priority => 1002,
-    }
-
     apt::pin { 'toolsdb_no_debian_package':
         package  => 'mariadb-server',
         pin      => 'version *',
         priority => -1,
     }
 
-    apt::pin { 'toolsdb_no_mariadb_103':
-        package  => 'wmf-mariadb103',
-        pin      => 'version *',
-        priority => -1,
+    if debian::codename::eq('stretch') {
+        apt::pin { 'toolsdb_fixed_mariadb_version':
+            package  => 'wmf-mariadb101',
+            pin      => 'version 10.1.39-1',
+            priority => 1002,
+        }
+
+        apt::pin { 'toolsdb_no_mariadb_103':
+            package  => 'wmf-mariadb103',
+            pin      => 'version *',
+            priority => -1,
+        }
+    } elsif debian::codename::eq('bullseye') {
+        apt::pin { 'toolsdb_fixed_mariadb_version':
+            package  => 'wmf-mariadb104',
+            pin      => 'version 10.4.25+deb11u1',
+            priority => 1002,
+        }
+
+        apt::pin { 'toolsdb_no_mariadb_106':
+            package  => 'wmf-mariadb106',
+            pin      => 'version *',
+            priority => -1,
+        }
+    } else {
+        fail("${debian::codename()}: not supported by profile::wmcs::services::toolsdb_apt_pinning")
     }
 }
