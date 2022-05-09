2e8006fe28f3883713d4642cacb498d7fd1fa7b9
Jbond <jbond@wikimedia.org>
2022-05-09 15:36:17 +0000

    Revert "P:mariadb::proxy::multiinstance_replicas: drop use of query_facts"
    This reverts commit 17772e583b7ff76d0d5e07477efd274087bb28d9.
    
    Reason for revert: removed backups
    
    Change-Id: I06cfaf8dc2721a4a920229e65160994e1c787a42

--- modules/profile/manifests/mariadb/proxy/multiinstance_replicas.pp
+++ modules/profile/manifests/mariadb/proxy/multiinstance_replicas.pp
@@ -12,35 +12,44 @@ class profile::mariadb::proxy::multiinstance_replicas(
     # The intent here is to define instances *on the db servers only*
     # because defining them all by hand for both haproxy servers seems like toil
     $replica_sections = ['s1','s2','s3','s4','s5','s6','s7','s8']
+    $default_backend_servers = $replica_sections.reduce({}) |$memo, $section|{
+        $memo + {$section =>  query_facts(
+                    "Class['role::wmcs::db::wikireplicas::${replica_type}_multiinstance'] and Profile::Mariadb::Section[${section}]",
+                    ['fqdn', 'ipaddress']
+                )}
+    }
+
     # Generate a hash of default standby servers for each section, which is the
     # "other" replica type.
     $standby_type = $replica_type ? {
         'web'       => 'analytics',
         'analytics' => 'web',
     }
-    $base_section_servers = $replica_sections.reduce({}) |$memo, $section|{
-        $backend_pql = @("PQL")
-        facts[certname, value] {
-            name = 'ipaddress' and
-            resources { type = 'Class' and title = 'Role::Wmcs::Db::Wikireplicas::${replica_type.capitalize}_multiinstance' } and
-            resources { type = 'Profile::Mariadb::Section' and title = '${section}' }
+    $base_standby_servers = $replica_sections.reduce({}) |$memo, $section|{
+        $memo + {$section =>  query_facts(
+                    "Class['role::wmcs::db::wikireplicas::${standby_type}_multiinstance'] and Profile::Mariadb::Section[${section}]",
+                    ['fqdn', 'ipaddress']
+                )}
+    }
+
+    # Keep Puppet Catalog Compiler happy. No puppetdb
+    $scrubbed_standbys = $base_standby_servers.filter |$section|{
+        !$section[1].empty
+    }
+    # Inserting a hash key into an existing nested hash in puppet is interesting
+    $standby_servers = $scrubbed_standbys.reduce({}) |$memo, $section|{
+        $memo + {$section[0] => $section[1].reduce({}) |$x, $config| {
+            $x + {$config[0] => $config[1] + {'standby' => true}}
+            }
         }
-        | PQL
-        $standby_pql = @("PQL")
-        facts[certname, value] {
-            name = 'ipaddress' and
-            resources { type = 'Class' and title = 'Role::Wmcs::Db::Wikireplicas::${standby_type.capitalize}_Multiinstance' } and
-            resources { type = 'Profile::Mariadb::Section' and title = '${section}' }
-        }
-        | PQL
-        $backend_servers = Hash(puppetdb_query($backend_pql).map |$value| {
-            [$value['certname'], { 'ipaddress' => $value['value'] }]
-        })
-        $standby_servers = Hash(puppetdb_query($standby_pql).map |$value| {
-            [$value['certname'], { 'ipaddress' => $value['value'], 'standby' => true }]
-        })
-        $memo + { $section => deep_merge($backend_servers, $standby_servers) }
-    }.filter |$section| { !$section[1].empty }
+    }
+
+    # Keep Puppet Catalog Compiler happy. No puppetdb
+    $scrubbed_servers = $default_backend_servers.filter |$section|{
+        !$section[1].empty
+    }
+
+    $base_section_servers = deep_merge($scrubbed_servers, $standby_servers)
 
     # Merge $section_overrides to provide weights and depoolings
     # NOTE: Overrides an entire section config intentionally, not server by server
