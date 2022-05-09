dfcff4bf917495790e9a84bb5bde2473937c63b8
Andrew Bogott <abogott@wikimedia.org>
2022-05-08 23:15:23 -0500

    cinder-backup: apply a hack to prevent repeated backup failures
    This is upstream patch:
    
    https://review.opendev.org/c/openstack/cinder/+/838009
    
    It looks for a successful backup to use as the base for
    any future incremental backups.
    
    Change-Id: I3e1d06d9cad3593c9a922f81f015785424a2413d

--- modules/openstack/manifests/cinder/backup.pp
+++ modules/openstack/manifests/cinder/backup.pp
@@ -23,6 +23,21 @@ class openstack::cinder::backup (
         }
     }
 
+    $api_file_to_patch = '/usr/lib/python3/dist-packages/cinder/backup/api.py'
+    $api_patch_file = "${api_file_to_patch}.patch"
+    file {$api_patch_file:
+        source => "puppet:///modules/openstack/${version}/cinder/hacks/backup/api.py.patch",
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+    }
+    exec { "apply ${api_patch_file}":
+        command => "/usr/bin/patch --forward ${api_file_to_patch} ${api_patch_file}",
+        unless  => "/usr/bin/patch --reverse --dry-run -f ${api_file_to_patch} ${api_patch_file}",
+        require => [File[$api_patch_file], Package['cinder-backup']],
+        notify  => Service['cinder-backup'],
+    }
+
     service { 'cinder-backup':
         ensure    => $active,
         subscribe => Class["openstack::cinder::config::${version}"],
