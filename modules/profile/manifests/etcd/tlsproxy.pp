129a89dd851d56e0035cb97d13f38b5190d5f1dc
jbond <jbond@wikimedia.org>
2022-05-10 13:24:30 +0200

    P:etcd::tlsproxy: add documentation and fix minor lint issues
    Bug: T307383
    Hosts: P:etcd::tlsproxy
    Change-Id: I58c81cbbf721979731b5c334d899d43b4b05fd63

--- modules/profile/manifests/etcd/tlsproxy.pp
+++ modules/profile/manifests/etcd/tlsproxy.pp
@@ -1,18 +1,27 @@
+# @summary configure tlsproxy for etcd service
+# @param cert_name the certificate cn
+# @param acls Hash of paths and the users allowed to access them
+# @param salt salt used for htpasswd
+# @param read_only indicate if the node is readonly
+# @param listen_port The port to listen on
+# @param upstream_port The upstream port of etcd
+# @param tls_upstream The tls port to listen on
+# @param pool_pwd_seed seed used for autogenrated passwords
 class profile::etcd::tlsproxy(
-    Stdlib::Fqdn $cert_name = lookup('profile::etcd::tlsproxy::cert_name'),
-    Hash[Stdlib::Unixpath, Array[String]] $acls = lookup('profile::etcd::tlsproxy::acls'),
-    String $salt = lookup('profile::etcd::tlsproxy::salt'),
-    Boolean $read_only = lookup('profile::etcd::tlsproxy::read_only'),
-    Stdlib::Port $listen_port = lookup('profile::etcd::tlsproxy::listen_port'),
-    Stdlib::Port $upstream_port = lookup('profile::etcd::tlsproxy::upstream_port'),
-    Boolean $tls_upstream = lookup('profile::etcd::tlsproxy::tls_upstream'),
-    String $pool_pwd_seed = lookup('etcd::autogen_pwd_seed')
+    Stdlib::Fqdn                          $cert_name     = lookup('profile::etcd::tlsproxy::cert_name'),
+    Hash[Stdlib::Unixpath, Array[String]] $acls          = lookup('profile::etcd::tlsproxy::acls'),
+    String                                $salt          = lookup('profile::etcd::tlsproxy::salt'),
+    Boolean                               $read_only     = lookup('profile::etcd::tlsproxy::read_only'),
+    Stdlib::Port                          $listen_port   = lookup('profile::etcd::tlsproxy::listen_port'),
+    Stdlib::Port                          $upstream_port = lookup('profile::etcd::tlsproxy::upstream_port'),
+    Boolean                               $tls_upstream  = lookup('profile::etcd::tlsproxy::tls_upstream'),
+    String                                $pool_pwd_seed = lookup('etcd::autogen_pwd_seed')
 ) {
-    require ::profile::tlsproxy::instance
-    require ::passwords::etcd
+    require profile::tlsproxy::instance
+    require passwords::etcd
 
     # this is a hash of user => password
-    $accounts = $::passwords::etcd::accounts
+    $accounts = $passwords::etcd::accounts
 
     # The testserver cluster is a bit peculiar: it is in conftool but doesn't have a load-balancer.
     # So we need to add it to the list manually.
@@ -39,8 +48,8 @@ class profile::etcd::tlsproxy(
     }
 
     $upstream_host = $tls_upstream ? {
-        true    => $::fqdn,
-        default => '127.0.0.1'
+        true    => $facts['networking']['fqdn'],
+        default => '127.0.0.1',
     }
     sslcert::certificate { $cert_name:
         skip_private => false,
@@ -50,32 +59,32 @@ class profile::etcd::tlsproxy(
     monitoring::service { 'etcd-tlsproxy-ssl':
         description   => "etcd tlsproxy SSL ${upstream_host}:${listen_port}",
         check_command => "check_ssl_on_host_port!${upstream_host}!${upstream_host}!${listen_port}",
-        notes_url     => 'https://wikitech.wikimedia.org/wiki/Cergen'
+        notes_url     => 'https://wikitech.wikimedia.org/wiki/Cergen',
     }
 
     file { '/etc/nginx/auth/':
         ensure => directory,
         mode   => '0550',
         owner  => 'www-data',
-        before => Service['nginx']
+        before => Service['nginx'],
     }
 
     file { '/etc/nginx/etcd-errors':
         ensure => directory,
         mode   => '0550',
         owner  => 'www-data',
-        before => Service['nginx']
+        before => Service['nginx'],
     }
 
     # Simulate the etcd auth error
     file { '/etc/nginx/etcd-errors/401.json':
-        ensure  => present,
+        ensure  => file,
         mode    => '0444',
         content => '{"errorCode":110,"message":"The request requires user authentication","cause":"Insufficient credentials","index":0}',
     }
 
     file { '/etc/nginx/etcd-errors/readonly.json':
-        ensure  => present,
+        ensure  => file,
         mode    => '0444',
         content => '{"errorCode":107,"message":"This cluster is in read-only mode","cause":"Cluster configured to be read-only","index":0}',
     }
