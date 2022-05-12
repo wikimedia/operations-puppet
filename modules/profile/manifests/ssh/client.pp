41c4b6519008b453157ae95fbefc3da24feca332
jbond <jbond@wikimedia.org>
2022-05-12 17:20:22 +0200

    C:ssh:client: Add ability to manage ssh_config file
    Hosts: deployment-mediawiki12.deployment-prep.eqiad1.wikimedia.cloud
    Hosts: sretest1001.eqiad.wmnet
    Bug: T307565
    Change-Id: I125c96e1f6f4f9f9953cb0b7ebef430ec53ec1ec

--- modules/profile/manifests/ssh/client.pp
+++ modules/profile/manifests/ssh/client.pp
@@ -1,7 +1,16 @@
 # @summary class to managed ssh client config
 # @param manage_ssh_keys indicate if we should manage the known_hosts file
+# @param manage_ssh_config if true manage the /etc/ssh/ssh_config file,
+#   most other parameters are only valid if this is true
+# @param hash_known_hosts HashKnownHosts value
+# @param gss_api_authentication GSSAPIAuthentication value
+# @param send_env list of environment variables to send
 class profile::ssh::client (
-    Boolean $manage_ssh_keys = lookup('profile::ssh::client::manage_ssh_keys'),
+    Boolean          $manage_ssh_keys        = lookup('profile::ssh::client::manage_ssh_keys'),
+    Boolean          $manage_ssh_config      = lookup('profile::ssh::client::manage_ssh_config'),
+    Boolean          $hash_known_hosts       = lookup('profile::ssh::client::hash_known_hosts'),
+    Boolean          $gss_api_authentication = lookup('profile::ssh::client::gss_api_authentication'),
+    Array[String[1]] $send_env               = lookup('profile::ssh::client::send_env'),
 ) {
     class { 'ssh::client':
         * => wmflib::dump_params(),
--- modules/ssh/manifests/client.pp
+++ modules/ssh/manifests/client.pp
@@ -1,19 +1,36 @@
 # @summary class to managed ssh client config
 # @param manage_ssh_keys indicate if we should manage the known_hosts file
+# @param manage_ssh_config if true manage the /etc/ssh/ssh_config file,
+#   most other parameters are only valid if this is true
+# @param hash_known_hosts HashKnownHosts value
+# @param gss_api_authentication GSSAPIAuthentication value
+# @param send_env list of environment variables to send
 class ssh::client (
-    Boolean $manage_ssh_keys = true,
+    Boolean          $manage_ssh_keys        = true,
+    Boolean          $manage_ssh_config      = true,
+    Boolean          $hash_known_hosts       = true,
+    Boolean          $gss_api_authentication = true,
+    Array[String[1]] $send_env               = ['LANG', 'LC_*'],
 ) {
-    package { 'openssh-client':
-        ensure => present,
-    }
+    ensure_packages('openssh-client')
 
     if $manage_ssh_keys and $::use_puppetdb {
         file { '/etc/ssh/ssh_known_hosts':
+            ensure  => file,
             content => template('ssh/known_hosts.erb'),
             backup  => false,
             owner   => 'root',
             group   => 'root',
             mode    => '0644',
+        }
+    }
+    if $manage_ssh_config {
+        file { '/etc/ssh/ssh_config':
+            ensure  => file,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            content => template('ssh/ssh_config.erb'),
         }
     }
 }
