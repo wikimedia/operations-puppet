0152c296ab2b55096c7e436d64a930df25c561f8
Simon Lyngshede <slyngshede@wikimedia.org>
2022-05-12 15:21:14 +0200

    Move rabbitmq to systemd timer
    Bug: T273673
    Change-Id: Ic1bc8df12e8a438475683781dd62de0175526fe4

--- modules/rabbitmq/manifests/cleanup.pp
+++ modules/rabbitmq/manifests/cleanup.pp
@@ -33,17 +33,25 @@ class rabbitmq::cleanup(
     }
 
     if ($enabled) {
-        $cron_ensure = 'present'
+        $ensure = 'present'
     }
     else {
-        $cron_ensure = 'absent'
+        $ensure = 'absent'
     }
 
     # These logfiles will be rotated by an already-existing wildcard logrotate rule for rabbit
+    systemd::timer::job { 'drain_rabbitmq_notification_error':
+        ensure          => $ensure,
+        description     => 'Drain and log RabbitMQ notifications.error queue',
+        user            => 'root',
+        command         => "/usr/local/sbin/drain_queue notifications.error --password ${password}",
+        logfile_basedir => '/var/log/rabbitmq/',
+        logfile_name    => 'notifications_error.log',
+        interval        => {'start' => 'OnCalendar', 'interval' => '*:0/35:00'}
+    }
+
     cron { 'drain and log rabbit notifications.error queue':
-            ensure  => $cron_ensure,
-            user    => 'root',
-            minute  => '35',
-            command => "/usr/local/sbin/drain_queue notifications.error --password ${password} >> /var/log/rabbitmq/notifications_error.log 2>&1",
+            ensure => 'absent',
+            user   => 'root',
     }
 }
