c25e37028ba00927104f9be35a1039782d0b7cab
Simon Lyngshede <slyngshede@wikimedia.org>
2022-05-16 09:30:17 +0200

    query service: port cronjobs to systemd timers
    Hosts: C:query_service::crontasks
    Bug: T273673
    Change-Id: Ib244e594e3016d0a19f31e185ad69f5df723f0b1

--- modules/query_service/manifests/crontasks.pp
+++ modules/query_service/manifests/crontasks.pp
@@ -74,8 +74,7 @@ class query_service::crontasks(
     }
 
     $reload_categories_log = "${log_dir}/reloadCategories.log"
-    $reload_dcatap_log = "${log_dir}/reloadDCATAP.log"
-    $reload_wcqs_data_log = "${log_dir}/reloadWCQS.log"
+
     # the reload-categories cron needs to reload nginx once the categories are up to date
     sudo::user { "${username}-reload-nginx":
       ensure     => present,
@@ -96,32 +95,51 @@ class query_service::crontasks(
     # Category dumps start on Sat 20:00. By Mon, they should be done.
     # We want random time so that hosts don't reboot at the same time, but we
     # do not want them to be too far from one another.
-    cron { 'reload-categories':
-        ensure  => $ensure_reload_categories,
-        command => "/usr/local/bin/reloadCategories.sh ${deploy_name} >> ${reload_categories_log} 2>&1",
-        user    => $username,
-        weekday => 1,
-        minute  => fqdn_rand(60),
-        hour    => fqdn_rand(2),
+    systemd::timer::job { 'reload-categories':
+        ensure          => $ensure_reload_categories,
+        description     => 'Query service category dump (weekly)',
+        command         => "/usr/local/bin/reloadCategories.sh ${deploy_name}",
+        user            => $username,
+        logfile_basedir => $log_dir,
+        logfile_name    => 'reloadCategories.log',
+        interval        => {'start' => 'OnCalendar', 'interval' => "Mon *-*-* ${fqdn_rand(2)}:${fqdn_rand(60)}:00"},
     }
 
     # Categories daily dump starts at 5:00. Currently it is done by 5:05, but just in case
     # it ever takes longer, start at 7:00.
+    systemd::timer::job { 'load-catgories-daily':
+        ensure          => $ensure_daily_categories,
+        description     => 'Query service category dump (daily)',
+        command         => "/usr/local/bin/loadCategoriesDaily.sh ${deploy_name}",
+        user            => $username,
+        logfile_basedir => $log_dir,
+        logfile_name    => 'reloadCategories.log',
+        interval        => {'start' => 'OnCalendar', 'interval' => "Mon *-*-* 07:${fqdn_rand(60)}:00"},
+    }
+
+    systemd::timer::job{ 'load-dcatap-weekly':
+        ensure          => $ensure_daily_categories,
+        description     => 'Reload DCAT-AP',
+        command         => "/usr/local/bin/reloadDCAT-AP.sh ${deploy_name}",
+        user            => $username,
+        logfile_basedir => $log_dir,
+        logfile_name    => 'reloadDCATAP.log',
+        interval        => {'start' => 'OnCalendar', 'interval' => "Fri *-*-* 07:${fqdn_rand(60)}:00"},
+    }
+
+    cron { 'reload-categories':
+        ensure => 'absent',
+        user   => $username,
+    }
+
     cron { 'load-categories-daily':
-        ensure  => $ensure_daily_categories,
-        command => "/usr/local/bin/loadCategoriesDaily.sh ${deploy_name} >> ${reload_categories_log} 2>&1",
-        user    => $username,
-        minute  => fqdn_rand(60),
-        hour    => 7
+        ensure => 'absent',
+        user   => $username,
     }
 
     cron { 'load-dcatap-weekly':
-        ensure  => $ensure_daily_categories,
-        command => "/usr/local/bin/reloadDCAT-AP.sh ${deploy_name} >> ${reload_dcatap_log} 2>&1",
-        user    => $username,
-        minute  => fqdn_rand(60),
-        hour    => 7,
-        weekday => 5,
+        ensure => 'absent',
+        user   => $username,
     }
 
     $ensure_tests = $run_tests ? {
@@ -134,21 +152,35 @@ class query_service::crontasks(
         default => absent,
     }
 
+    # Run test queries
+    systemd::timer::job { 'run-query-service-test-queries':
+        ensure       => $ensure_tests,
+        description  => 'Run test queries for query service',
+        command      => "${package_dir}/queries/test.sh",
+        user         => $username,
+        send_mail_to => 'wdqs-admin@wikimedia.org',
+        send_mail    => true,
+        interval     => {'start' => 'OnCalendar', 'interval' => '*:0/30:00' }
+    }
+
+    systemd::timer::job { 'wcqs-data-reload-weekly':
+        ensure          => $ensure_reload_wcqs_data,
+        description     => 'WCQS data reload',
+        command         => "${package_dir}/wcqs-data-reload.sh",
+        user            => 'root', # we need to restart blazegraph, so we need sudo priviliges
+        logfile_basedir => $log_dir,
+        logfile_name    => 'reloadWCQS.log',
+        interval        => {'start' => 'OnCalendar', 'interval' => "Tue *-*-* 07:${fqdn_rand(60)}:00"},
+    }
+
     cron { 'run-query-service-test-queries':
-        ensure      => $ensure_tests,
-        environment => 'MAILTO=wdqs-admins',
-        command     => "${package_dir}/queries/test.sh > /dev/null",
-        user        => $username,
-        minute      => '*/30',
+        ensure => 'absent',
+        user   => $username,
     }
 
     cron { 'wcqs-data-reload-weekly':
-      ensure  => $ensure_reload_wcqs_data,
-      command => "${package_dir}/wcqs-data-reload.sh >> ${reload_wcqs_data_log} 2>&1",
-      user    => 'root',        #we need to restart blazegraph, so we need sudo priviliges
-      minute  => fqdn_rand(60),
-      hour    => 7,
-      weekday => 2,
+        ensure => 'absent',
+        user   => 'root',
     }
 
     logrotate::rule { 'query-service-reload-categories':
