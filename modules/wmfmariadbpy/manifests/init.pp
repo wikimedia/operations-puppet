5f0c2d23a0a945c1aa2a78e38ad1c1c285205662
Alexander Vorwerk <zabe@avorwerk.net>
2022-05-17 14:56:17 +0200

    wmfmariadbpy: Add SPDX headers
    Bug: T308013
    Change-Id: Ie377b494ee686ce6022a1503929baee810937e26

--- modules/wmfmariadbpy/manifests/init.pp
+++ modules/wmfmariadbpy/manifests/init.pp
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0
 # Profile to setup and configure wmfmariadbpy libraries and
 # utilities.
 class wmfmariadbpy (
--- modules/wmfmariadbpy/types/role.pp
+++ modules/wmfmariadbpy/types/role.pp
@@ -1,1 +1,2 @@
+# SPDX-License-Identifier: Apache-2.0
 type Wmfmariadbpy::Role = Enum['admin', 'db', 'library']
