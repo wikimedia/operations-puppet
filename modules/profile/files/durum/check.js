b8eed35d458d0821520f43509227b2005415d67e
Sukhbir Singh <ssingh@wikimedia.org>
2022-05-17 13:23:56 -0400

    durum: display the DC the user is connected to in the frontend
    For the durum frontend (check.wikimedia-dns.org), display which data
    center the user is connected to. This is useful information for the user
    for a number of reasons: transparency, which country (or state in the
    US) there queries are being forwarded to, debugging, etc.
    
    This builds on commit d7e3c98b in which we added the JSON response that
    we now format and display in the frontend.
    
    Hosts: O:durum
    Change-Id: Ib42c3b836860e9c6c70c2bdc7557a2c52828e2a7

--- modules/profile/files/durum/check.js
+++ modules/profile/files/durum/check.js
@@ -2,6 +2,8 @@
   const testUUID = uuidv4();
   const testTime = new Date().toLocaleTimeString();
   const checkURL = `https://${testUUID}.check.wikimedia-dns.org/check/`;
+  const dcToLoc = {"eqiad": "(US) Virginia", "codfw": "(US) Texas", "esams": "The Netherlands",
+                   "ulsfo": "(US) California", "eqsin": "Singapore", "drmrs": "France"}
 
   fetch(checkURL)
   .then((response) => response.json())
@@ -16,7 +18,7 @@
       document.getElementById("check-heading").innerHTML = "&#9745;";
       document.getElementById("check-result").textContent = "Congratulations! You are using Wikidough.";
       document.getElementById("check-result").style.color = 'green';
-      document.getElementById("check-service").textContent = `Connected over ${description}`;
+      document.getElementById("check-service").textContent = `Connected over ${description} in ${dcToLoc[check.site]}`;
     } else {
       document.getElementById("check-heading").innerHTML = "&#9746;";
       document.getElementById("check-result").textContent = "Sorry, you are not using Wikidough.";
