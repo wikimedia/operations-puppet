aa5c0209277f19e6cf0cbdec2e46af426176526b
Taavi Väänänen <hi@taavi.wtf>
2022-05-18 13:09:02 +0300

    gdnsd: use nrpe::plugin
    Bug: T308601
    Change-Id: Ic77d252d8a6963942e6d68569ade396d7cac611c

--- modules/gdnsd/manifests/monitor_conf.pp
+++ modules/gdnsd/manifests/monitor_conf.pp
@@ -4,18 +4,13 @@ class gdnsd::monitor_conf {
     # puppet-deploying an invalid configuration, which might otherwise only
     # cause a single failed puppet run (until someone tries to deploy DNS
     # changes and gets blocked).
-    file { '/usr/local/lib/nagios/plugins/check_gdnsd_checkconf':
-        ensure => present,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
+    nrpe::plugin { 'check_gdnsd_checkconf':
         source => 'puppet:///modules/gdnsd/check_gdnsd_checkconf',
     }
 
     nrpe::monitor_service { 'gdnsd_checkconf':
         description  => 'gdnsd checkconf',
         nrpe_command => '/usr/local/lib/nagios/plugins/check_gdnsd_checkconf',
-        require      => File['/usr/local/lib/nagios/plugins/check_gdnsd_checkconf'],
         notes_url    => 'https://wikitech.wikimedia.org/wiki/gdnsd',
     }
 }
