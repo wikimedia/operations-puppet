43c425dba8686cf8955ee9215cd35cc99ea5ada5
John Bond <jbond@wikimedia.org>
2022-05-18 21:42:51 +0200

    C:redis::multidc::ipsec: migrate legacy redis_shard_hosts to puppet code
    Hosts: C:redis::multidc::ipsec
    Bug: T308639
    Change-Id: Ib6c4842454ab20906e8a5e431496ab84036e6774

--- modules/redis/manifests/multidc/ipsec.pp
+++ modules/redis/manifests/multidc/ipsec.pp
@@ -4,9 +4,17 @@ class redis::multidc::ipsec($shards) {
     # Set up IPsec between hosts (for encrypting the redis replica) #
     #################################################################
 
-    # This is actually more reliable than $::ipaddress for our uses
-    $my_ip = ipresolve($::fqdn, 4)
-    $ipsec_host_list = redis_shard_hosts($my_ip, $shards)
+    $my_ip = $facts['networking']['ip']
+    $myshards = $shards.reduce([]) |$memo, $values| {
+        $memo + $values[1].filter |$key, $value| { $value['host'] == $my_ip }.keys
+    }
+    $ipsec_host_list = $shards.reduce([]) |$memo, $values| {
+        $memo + $values[1].filter |$key, $value| {
+            $key in $myshards
+        }.map |$key, $value| {
+            ipresolve($value['host'], 'ptr')
+        }
+    }.sort.unique
 
     # No reason to define IPsec if the host doesn't need replication.
     if size($ipsec_host_list) > 0 {
