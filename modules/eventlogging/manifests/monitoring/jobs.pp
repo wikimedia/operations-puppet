33ac630b485c48cd9ff6484dea37dae33a30ce1e
Taavi Väänänen <hi@taavi.wtf>
2022-05-18 12:16:19 +0300

    eventlogging: use nrpe::plugin
    Bug: T308601
    Change-Id: Id81f20e17a379fa0498fad210c42caf5d511b71f

--- modules/eventlogging/manifests/monitoring/jobs.pp
+++ modules/eventlogging/manifests/monitoring/jobs.pp
@@ -4,17 +4,19 @@
 # eventlogging services are running.
 #
 class eventlogging::monitoring::jobs($ensure = 'present') {
-
     file { '/usr/lib/nagios/plugins/check_eventlogging_jobs':
+        ensure => absent,
+    }
+
+    nrpe::plugin { 'check_eventlogging_jobs':
+        ensure => $ensure,
         source => 'puppet:///modules/eventlogging/check_eventlogging_jobs.systemd',
-        mode   => '0755',
     }
 
     nrpe::monitor_service { 'eventlogging-jobs':
         ensure        => $ensure,
         description   => 'Check status of defined EventLogging jobs',
-        nrpe_command  => '/usr/lib/nagios/plugins/check_eventlogging_jobs',
-        require       => File['/usr/lib/nagios/plugins/check_eventlogging_jobs'],
+        nrpe_command  => '/usr/local/lib/nagios/plugins/check_eventlogging_jobs',
         contact_group => 'admins,analytics',
         notes_url     => 'https://wikitech.wikimedia.org/wiki/Analytics/Systems/EventLogging',
     }
