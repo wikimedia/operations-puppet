0ddf2317aae1dba5f0ab2308109ef72321a80db2
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2022-05-19 10:53:00 +0200

    mediawiki::system_users: add mwpresync
    This user will be used for the presync process for mediawiki.
    
    Bug: T303857
    Change-Id: I21a61cd788a94d97ac7eceddcd7f3f78bd4f83d0

--- modules/profile/manifests/mediawiki/system_users.pp
+++ modules/profile/manifests/mediawiki/system_users.pp
@@ -15,4 +15,19 @@ class profile::mediawiki::system_users(Wmflib::Ensure $ensure = lookup('profile:
         managehome => true,
         system     => true,
     }
+    # Create a second user that is used during the presync process.
+    # This user will have the ability to prepare the mediawiki sources for train presync, and to
+    # sudo to mwdeploy to distribute the code to the appservers.
+    # Soon it will also be able to pre-pull images on the kubernetes nodes.
+    # Please note we're using the "mwbuilder" group as its primary group, so that we group these system users
+    # in the same primary group.
+    user { 'mwpresync':
+        ensure     => present,
+        gid        => 'mwbuilder',
+        shell      => '/bin/false',
+        comment    => '',
+        home       => '/srv/mwpresync',
+        managehome => true,
+        system     => true,
+    }
 }
