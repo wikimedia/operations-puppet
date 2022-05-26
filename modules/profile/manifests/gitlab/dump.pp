b167704c66b52ae9c8f2e92ee4c3740709ceaabe
Daniel Zahn <dzahn@wikimedia.org>
2022-05-26 16:32:00 -0700

    gitlab::dump: ensure /srv/gitlab-backup exists so that rsync starts
    follow-up to I6f1a56bda16e9c
    
    Bug: T274463
    Change-Id: I9b5a9be76bc1db89c76b1ee001e4432fad10a3d6

--- modules/profile/manifests/gitlab/dump.pp
+++ modules/profile/manifests/gitlab/dump.pp
@@ -1,12 +1,17 @@
 # SPDX-License-Identifier: Apache-2.0
 # A place to temp. dump gitlab backups until T274463 is resolved
-class profile::gitlab::dump {
+class profile::gitlab::dump(
+    Stdlib::Unixpath $backup_path = lookup('profile::gitlab::dump::backup_path',
+                { 'default_value' => '/srv/gitlab-backup'}),
+){
+
+        wmflib::dir::mkdir_p($backup_path)
 
         rsync::quickdatacopy { 'gitlab-backups':
             source_host         => 'gitlab1001.wikimedia.org',
             dest_host           => 'gitlab1004.wikimedia.org',
             auto_sync           => false,
-            module_path         => '/srv/gitlab-backup',
+            module_path         => $backup_path,
             server_uses_stunnel => true,
         }
 }
