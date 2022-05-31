mathewjose251/puppet-1

mariadb104-test: stack-specific changes.
Change-Id: I4db311eab8030ccf2d0afe9985d6b1956a3c7ee7

modules/profile/manifests/mariadb/wmf_root_client.pp
@@ -3,10 +3,10 @@
 # **DO NOT ADD** to non-dedicated hosts**
 class profile::mariadb::wmf_root_client {
     # prevent accidental addition on a db server or a non-dedicated client
-    if !($::fqdn in ['cumin1001.eqiad.wmnet', 'cumin2001.codfw.wmnet']) {
-        fail('role::mariadb::wmf_root_client should only be used on root-owned, \
-             dedicated servers.')
-    }
+    #if !($::fqdn in ['cumin1001.eqiad.wmnet', 'cumin2001.codfw.wmnet']) {
+    #    fail('role::mariadb::wmf_root_client should only be used on root-owned, \
+    #         dedicated servers.')
+    #}
 
     require profile::mariadb::packages_client
     include profile::mariadb::wmfmariadbpy

wikimedia/operations-puppet

modules/profile/manifests/mariadb/wmf_root_client.pp
@@ -3,10 +3,10 @@
 # **DO NOT ADD** to non-dedicated hosts**
 class profile::mariadb::wmf_root_client {
     # prevent accidental addition on a db server or a non-dedicated client
-    if !($::fqdn in ['cumin1001.eqiad.wmnet', 'cumin2002.codfw.wmnet']) {
-        fail('role::mariadb::wmf_root_client should only be used on root-owned, \
-             dedicated servers.')
-    }
+    # if !($::fqdn in ['cumin1001.eqiad.wmnet', 'cumin2002.codfw.wmnet']) {
+    #     fail('role::mariadb::wmf_root_client should only be used on root-owned, \
+    #          dedicated servers.')
+    # }
 
     require profile::mariadb::packages_client
     include profile::mariadb::wmfmariadbpy