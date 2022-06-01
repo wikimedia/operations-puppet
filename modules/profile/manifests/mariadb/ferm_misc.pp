5db732b7a894cc5fd2136fc2b7c6c1c055eafb70
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-06-01 13:27:12 +0200

    Remove old buster idp-test hosts, add new idp/bullseye ones from Ferm rules
    - idp-test1001/idp-test2001 have been replaced by idp-test1002/idp-test2002
    - idp1002/idp2002 will replace idp1001/idp2001
    
    Bug: T308214
    Change-Id: I5c72905bf40f67efb1372bfa0a7d6727ab1d3cb8

--- modules/profile/manifests/mariadb/ferm_misc.pp
+++ modules/profile/manifests/mariadb/ferm_misc.pp
@@ -32,13 +32,13 @@ class profile::mariadb::ferm_misc {
         proto   => 'tcp',
         port    => '3306',
         notrack => true,
-        srange  => '@resolve((idp-test1001.wikimedia.org idp-test1002.wikimedia.org idp-test2001.wikimedia.org idp-test2002.wikimedia.org))',
+        srange  => '@resolve((idp-test1002.wikimedia.org idp-test2002.wikimedia.org))',
     }
 
     ferm::service { 'idp':
         proto   => 'tcp',
         port    => '3306',
         notrack => true,
-        srange  => '@resolve((idp1001.wikimedia.org idp2001.wikimedia.org))',
+        srange  => '@resolve((idp1001.wikimedia.org idp1002.wikimedia.org idp2001.wikimedia.org idp2002.wikimedia.org))',
     }
 }
