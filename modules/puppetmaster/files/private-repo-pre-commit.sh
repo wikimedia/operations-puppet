152facea5288ee9f76da57e9fd34a04be09337d2
jbond <jbond@wikimedia.org>
2022-06-06 19:17:40 +0200

    C:puppetmaster: Add requestctl validate to the private repo pre-commit
    Hosts: C:puppetmaster
    Change-Id: I58dfdd645b305156b4473bfea78704cd70cdf65f

--- modules/puppetmaster/files/private-repo-pre-commit.sh
+++ modules/puppetmaster/files/private-repo-pre-commit.sh
@@ -2,9 +2,14 @@
 set -euf -o pipefail
 PATH=/usr/bin
 EXIT=0
+CHECK_REQUESTCTL=0
 for staged in $(git diff-index --cached --name-only --diff-filter=AM HEAD)
 do
   # Only check yaml files in the hieradata directory
+  if [ "${staged%%/*}" == "requestctl" ]
+  then
+    CHECK_REQUESTCTL=1
+  fi
   if [ "${staged%%/*}" != "hieradata" ]
   then
     continue
@@ -20,4 +25,10 @@ do
   # use git show to ensure we check the staged file instead of the on disk file
   git show :"${staged}" | yamllint -c /etc/puppet/yamllint.yaml - || EXIT=1
 done
+if [ ${CHECK_REQUESTCTL} -eq 1 ]
+then
+  # TODO: with this we check the files on disk and not the ones staged which could in theory be different
+  # however i believe requestctl always uses the files on disk so this is probably the right thing to do?
+  requestctl validate /srv/private/requestctl || EXIT=1
+fi
 exit $EXIT
