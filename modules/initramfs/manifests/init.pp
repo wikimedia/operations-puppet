6cafef274f1c3fe5cbfcb669ccef28d7da9257a0
Alexander Vorwerk <zabe@avorwerk.net>
2022-06-07 18:29:21 +0200

    initramfs: Add SPDX headers
    Bug: T308013
    Change-Id: I4ac51439d1cebeef662e258d351c16937963e819

--- modules/initramfs/manifests/init.pp
+++ modules/initramfs/manifests/init.pp
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0
 class initramfs {
     package { 'initramfs-tools':
         ensure => installed,
--- modules/initramfs/manifests/script.pp
+++ modules/initramfs/manifests/script.pp
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0
 define initramfs::script($boot_stage='', $content='') {
     include initramfs
 
