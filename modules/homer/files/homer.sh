492e1900671f263801da14026162db73dd3a6875
Arzhel Younsi <ayounsi@wikimedia.org>
2022-06-08 08:40:50 +0200

    Homer: add REQUESTS_CA_BUNDLE for new Netbox endpoint
    Tested manually.
    
    Bug: T296452
    Change-Id: I6f035e4977003c073147d0534cd427acab38dfbb

--- modules/homer/files/homer.sh
+++ modules/homer/files/homer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # SPDX-License-Identifier: Apache-2.0
 
-SSH_AUTH_SOCK=/run/keyholder/proxy.sock PATH="/srv/deployment/homer/venv/bin:${PATH}" PYTHONWARNINGS="ignore::UserWarning:paramiko.transport" exec homer "${@}"
+REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt SSH_AUTH_SOCK=/run/keyholder/proxy.sock PATH="/srv/deployment/homer/venv/bin:${PATH}" PYTHONWARNINGS="ignore::UserWarning:paramiko.transport" exec homer "${@}"
