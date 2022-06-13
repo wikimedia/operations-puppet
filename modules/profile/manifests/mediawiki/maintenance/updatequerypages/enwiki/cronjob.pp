37bd63375c807e5a2c85deb872322fe28db5aef6
Kunal Mehta <legoktm@debian.org>
2022-06-13 00:47:01 -0400

    mediawiki: Remove absented mostlinkedcategories job
    Follow up for I5b61d127ad96.
    
    Change-Id: If4e41f268dce30b8aa1c6a660bc2400e946e7e9b

--- modules/profile/manifests/mediawiki/maintenance/updatequerypages/enwiki/cronjob.pp
+++ modules/profile/manifests/mediawiki/maintenance/updatequerypages/enwiki/cronjob.pp
@@ -11,12 +11,6 @@ class profile::mediawiki::maintenance::updatequerypages::enwiki::cronjob {
         interval => '*-16 01:00',
     }
 
-    profile::mediawiki::periodic_job { 'updatequerypages_mostlinkedcategories_s1':
-        ensure   => absent,
-        command  => '/usr/local/bin/mwscriptwikiset updateSpecialPages.php s1.dblist --override --only=Mostlinkedcategories',
-        interval => '*-17 01:00',
-    }
-
     profile::mediawiki::periodic_job { 'updatequerypages_mostlinkedtemplates_s1':
         command  => '/usr/local/bin/mwscriptwikiset updateSpecialPages.php s1.dblist --override --only=Mostlinkedtemplates',
         interval => '*-18 01:00',
