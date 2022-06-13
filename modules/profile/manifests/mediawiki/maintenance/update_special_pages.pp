3e7c25563287048d3a709466055a0ca718a1d53e
Kunal Mehta <legoktm@debian.org>
2022-06-12 20:39:28 -0400

    mediawiki: Split updateSpecialPages.php job to be per-shard
    Ocassionally the job takes too long and misses the next run (after 3
    days), so editors have to wait even longer for page updates. The special
    pages are all just querying the local wiki, so it should be safe to
    parallelize.
    
    flock hasn't been needed since the switch to systemd timers, which
    prevents it from running concurrently already.
    
    Bug: T307314
    Change-Id: Ic4522867adfb8b6d753d4f3e1c4b1de8dba11d20

--- modules/profile/manifests/mediawiki/maintenance/update_special_pages.pp
+++ modules/profile/manifests/mediawiki/maintenance/update_special_pages.pp
@@ -1,5 +1,11 @@
 class profile::mediawiki::maintenance::update_special_pages {
+    profile::mediawiki::sharded_periodic_job { 'update_special_pages':
+        command  => 'updateSpecialPages.php',
+        interval => '*-1/3 05:00',
+    }
+
     profile::mediawiki::periodic_job { 'update_special_pages':
+        ensure   => absent,
         # TODO: Instead of flock, make this unit run every N time units after it's finished.
         command  => 'flock -n /var/lock/update-special-pages /usr/local/bin/foreachwiki updateSpecialPages.php',
         interval => '*-1/3 05:00'
