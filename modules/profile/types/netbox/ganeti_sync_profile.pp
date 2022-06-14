ca9558cb897787025fb10e24cffc3d5f185d2454
Riccardo Coccioli <rcoccioli@wikimedia.org>
2022-06-14 12:23:41 +0200

    Netbox: adapt ganeti-sync config file
    * Update the configuration file for the Ganeti -> Netbox sync script to
      adapt to the refactored script.
    
    Bug: T262446
    Change-Id: I2ee99ab6458e0b7cca4e7546e24a1e9b358064af
    Depends-On: I90f1d54c6982496c1e10115aa0809f82dbe43ca7

--- modules/profile/types/netbox/ganeti_sync_profile.pp
+++ modules/profile/types/netbox/ganeti_sync_profile.pp
@@ -1,5 +1,6 @@
 type Profile::Netbox::Ganeti_sync_profile = Struct[{
-    profile => String[1],
-    url     => Stdlib::Fqdn,
-    port    => Stdlib::Port,
+    profile        => String[1],
+    url            => Stdlib::Fqdn,
+    site           => Wmflib::Sites,
+    Optional[port] => Stdlib::Port,
 }]
