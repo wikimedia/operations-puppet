180f6dfa812d761b2ed40d363d2d4879cbbcb113
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2022-06-14 11:13:32 +0200

    pontoon: reload haproxy post-certbot
    Pick up the new certs.
    
    Change-Id: I87bfcf2d693eaf18695d1289598e15b8ff6b76d0

--- modules/pontoon/files/certbot-deploy.sh
+++ modules/pontoon/files/certbot-deploy.sh
@@ -10,3 +10,5 @@ pushd $RENEWED_LINEAGE
 # haproxy < 2.2 supports only combined cert+key
 cat fullchain.pem privkey.pem > combined.pem
 chmod 0600 combined.pem
+
+systemctl try-reload-or-restart haproxy
