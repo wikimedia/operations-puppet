4b06532800ca8e362d30d4baadd6fd514a057962
Dzahn <dzahn@wikimedia.org>
2022-06-16 20:29:38 +0000

    Revert "Revert "Provide buildkitd to GitLab runners""
    This reverts commit 64a3739adbaac89ea861af60af3154952ca932fb.
    
    Reason for revert: trying again after Ahmon could not reproduce in devtools
    
    Change-Id: I2be2d11f1ae2df0cdd6f15f8776765d40873cb46

--- modules/gitlab_runner/manifests/config.pp
+++ modules/gitlab_runner/manifests/config.pp
@@ -2,6 +2,8 @@
 class gitlab_runner::config (
     Integer             $concurrent              = 3,
     String              $docker_image            = 'docker-registry.wikimedia.org/buster:latest',
+    String              $docker_network          = 'gitlab-runner',
+    Wmflib::Ensure      $ensure_buildkitd        = 'present',
     Stdlib::HTTPSUrl    $gitlab_url              = 'https://gitlab.wikimedia.org/',
     String              $runner_name             = 'GitLab Runner',
     Boolean             $enable_exporter         = false,
@@ -10,7 +12,6 @@ class gitlab_runner::config (
     Integer             $check_interval          = 3,
     Integer             $session_timeout         = 1800,
     String              $gitlab_runner_user      = 'gitlab-runner',
-
 ) {
 
     # Setup config template which is used while registering new runners
