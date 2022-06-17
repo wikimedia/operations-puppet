e3553bd9e4142b8141c4c6f215801aa6f14f8e54
jbond <jbond@wikimedia.org>
2022-06-17 16:18:36 +0200

    wmflib: update kernel_details to also include kernel.unprivileged_userns_clone
    Ass a fact for kernel.unprivileged_userns_clone to make it easier to
    report on and target machines with this parameter set.  also update the
    fact for net.ipv4.tcp_min_snd_mss to be an int instead of just true
    indicateing the file exists.
    
    Change-Id: Ibdc231172364d5f859472f3cb82b8a2883835955

--- modules/wmflib/lib/facter/kernel_details.rb
+++ modules/wmflib/lib/facter/kernel_details.rb
@@ -2,20 +2,39 @@ require 'facter'
 
 # this is a list of sysctl_settings we care about
 # we can add more as needed
-sysctl_settings = [
+sysctl_int_settings = [
   'net.ipv4.tcp_min_snd_mss',
 ]
+sysctl_bool_settings = [
+  'kernel.unprivileged_userns_clone',
+]
+kernel_details = {}
 Facter.add(:kernel_details) do
   confine :kernel => 'Linux'
   confine do
     File.exist?('/bin/uname')
   end
-  kernel_details = {}
   kernel_details['release'] =  Facter::Util::Resolution.exec('/bin/uname -r')
   kernel_details['version'] =  Facter::Util::Resolution.exec('/bin/uname -v')
   kernel_details['sysctl_settings'] = {}
-  sysctl_settings.each do |sysctl_setting|
-    kernel_details['sysctl_settings'][sysctl_setting] = File.file?("/proc/sys/#{sysctl_setting.gsub('.', '/')}")
+  sysctl_int_settings.each do |sysctl_setting|
+    proc_file = "/proc/sys/#{sysctl_setting.gsub('.', '/')}"
+    if File.file?(proc_file)
+      kernel_details['sysctl_settings'][sysctl_setting] = File.read(proc_file).strip.to_i
+    end
+  end
+  sysctl_bool_settings.each do |sysctl_setting|
+    proc_file = "/proc/sys/#{sysctl_setting.gsub('.', '/')}"
+    if File.file?(proc_file)
+      kernel_details['sysctl_settings'][sysctl_setting] = !File.read(proc_file).strip.to_i.zero?
+    end
   end
   setcode { kernel_details }
+end
+# Also add this as a legacy fact so its avalible to cumin
+Facter.add(:kernel_unprivileged_userns_clone) do
+  confine do
+    kernel_details.fetch('sysctl_settings', {}).key?('kernel.unprivileged_userns_clone')
+  end
+  setcode { kernel_details['sysctl_settings']['kernel.unprivileged_userns_clone'] }
 end
