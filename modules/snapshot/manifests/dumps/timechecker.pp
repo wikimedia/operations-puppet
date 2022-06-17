702d70879109eff78c5424d2a5a31d0b5001675d
Simon Lyngshede <slyngshede@wikimedia.org>
2022-06-17 09:53:11 +0200

    C:snapshot::dumps::timechecker convert cron to timer.
    Bug: T273673
    Change-Id: I1a87546cbbd80d3c7d38eae31333747b99c016ec

--- modules/snapshot/manifests/dumps/timechecker.pp
+++ modules/snapshot/manifests/dumps/timechecker.pp
@@ -12,8 +12,32 @@ class snapshot::dumps::timechecker(
     $apachedir = $snapshot::dumps::dirs::apachedir
     $dblist = "${apachedir}/dblists/all.dblist"
 
+    systemd::timer::job { 'dumps-timecheck-wikilist':
+        ensure                  => present,
+        description             => 'Show runtimes for dumps (wiki list)',
+        user                    => $xmldumpsuser,
+        send_mail               => true,
+        send_mail_to            => 'ops-dumps@wikimedia.org',
+        send_mail_only_on_error => false,
+        command                 => "/usr/bin/python3 show_runtimes.py -d ${dumpsbasedir} -W ${wikis_list}",
+        working_directory       => $repodir,
+        interval                => {'start' => 'OnCalendar', 'interval' => '*-*-1,20 01:10:00'}
+    }
+
+    systemd::timer::job { 'dumps-timecheck-dblist':
+        ensure                  => present,
+        description             => 'Show runtimes for dumps (dblist)',
+        user                    => $xmldumpsuser,
+        send_mail               => true,
+        send_mail_to            => 'ops-dumps@wikimedia.org',
+        send_mail_only_on_error => false,
+        command                 => "/usr/bin/python3 show_runtimes.py -d ${dumpsbasedir} -j meta-history-bz2 -s 40 -w ${dblist}",
+        working_directory       => $repodir,
+        interval                => {'start' => 'OnCalendar', 'interval' => '*-*-1,20 02:10:00'}
+    }
+
     cron { 'dumps-timechecker':
-        ensure      => 'present',
+        ensure      => absent,
         environment => 'MAILTO=ops-dumps@wikimedia.org',
         command     => "cd ${repodir}; python3 show_runtimes.py -d ${dumpsbasedir} -W ${wikis_list}; python3 show_runtimes.py -d ${dumpsbasedir} -j meta-history-bz2 -s 40 -w ${dblist}",
         user        => $xmldumpsuser,
