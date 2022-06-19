eed3c0a5fe7a2362aa04fc7e46c6014adad433d0
Taavi Väänänen <hi@taavi.wtf>
2022-06-19 19:15:37 +0300

    P:metricsinfra::haproxy: add thanos routing
    Route all Prometheus queries into Thanos, which will then load balance
    those queries to the Prometheus instances.
    
    Bug: T286301
    Change-Id: I92803b6ddbc0006df31551f400562dde51226bac

--- modules/profile/manifests/wmcs/metricsinfra/haproxy.pp
+++ modules/profile/manifests/wmcs/metricsinfra/haproxy.pp
@@ -1,7 +1,7 @@
 class profile::wmcs::metricsinfra::haproxy (
-    Array[Stdlib::Fqdn] $prometheus_hosts = lookup('profile::wmcs::metricsinfra::prometheus_hosts'),
     Array[Stdlib::Fqdn] $prometheus_alertmanager_hosts = lookup('profile::wmcs::metricsinfra::prometheus_alertmanager_hosts'),
     Stdlib::Fqdn        $alertmanager_active_host = lookup('profile::wmcs::metricsinfra::alertmanager_active_host'),
+    Array[Stdlib::Fqdn] $thanos_fe_hosts = lookup('profile::wmcs::metricsinfra::thanos_fe_hosts'),
     Array[Stdlib::Fqdn] $config_manager_hosts = lookup('profile::wmcs::metricsinfra::config_manager_hosts'),
 ) {
     class { 'haproxy::cloud::base': }
