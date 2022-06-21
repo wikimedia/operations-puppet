1e5427045f780f5e10523673272cbe1f8d148178
Simon Lyngshede <slyngshede@wikimedia.org>
2022-06-21 09:58:04 +0200

    C:dumps::web::dumpstatusfiles, convert to systemd timer.
    Bug: T273673
    Change-Id: I15713d1b1afab8e1bb448dd4b1736c4a744e6d65

--- modules/dumps/manifests/web/dumpstatusfiles.pp
+++ modules/dumps/manifests/web/dumpstatusfiles.pp
@@ -12,13 +12,22 @@ class dumps::web::dumpstatusfiles(
     # interval passed to script should match interval cron job runs;
     # check every interval minutes for a new tarball of status files
     # and unpack them
+    systemd::timer::job { 'update-dump-statusfiles':
+        ensure       => present,
+        description  => 'Check for a new tarball of status files, and unpack',
+        user         => 'root',
+        send_mail    => true,
+        send_mail_to => 'ops-dumps@wikimedia.org',
+        command      => "/bin/bash /usr/local/bin/unpack-dumpstatusfiles.sh --xmldumpsdir ${xmldumpsdir} --newer 5",
+        interval     => {'start' =>  'OnCalendar', 'interval' => '*-*-* 00:0/5:00',}
+    }
+
     cron { 'update-dump-statusfiles':
-        ensure      => 'present',
+        ensure      => absent,
         environment => 'MAILTO=ops-dumps@wikimedia.org',
         command     => "/bin/bash /usr/local/bin/unpack-dumpstatusfiles.sh --xmldumpsdir ${xmldumpsdir} --newer 5",
         user        => root,
         minute      => '*/5',
         require     => File['/usr/local/bin/unpack-dumpstatusfiles.sh'],
     }
-
 }
