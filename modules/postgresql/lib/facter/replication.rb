e59a43576af227c45f10761fe927b97f9dcf35a9
jbond <jbond@wikimedia.org>
2022-06-22 16:03:09 +0200

    C:postgresql: grab the data directory from postgresql
    The data directory is not always the one hardcoded in this fact as such
    get it dynamically.  This means this fact is reliant on psql running, it
    may be better to parse the config file?
    
    Hosts: C:postgresql
    Bug: T311156
    Change-Id: Iede3d0263c0c8abe5000dc16f96781d58406c1b5

--- modules/postgresql/lib/facter/replication.rb
+++ modules/postgresql/lib/facter/replication.rb
@@ -3,33 +3,21 @@
 require 'facter'
 
 Facter.add('postgres_version') do
+  confine do
+    Facter::Util::Resolution.which('psql')
+  end
   setcode do
-    version = nil
-    if Facter::Util::Resolution.which('psql')
-      Facter::Util::Resolution.exec('psql --version').lines.each do |line|
-        version_split = line.split(" ")
-        version = version_split[2]
-      end
-    end
-    version
+    Facter::Util::Resolution.exec('psql --version').strip.split[2]
   end
 end
 
 Facter.add('postgres_replica_initialised') do
+  confine do
+    Facter::Util::Resolution.which('psql')
+    Facter::Util::Resolution.which('sudo')
+  end
   setcode do
-    initialised = nil
-    pg_version_fact = Facter.fact(:postgres_version)
-    unless pg_version_fact.value.nil?
-      pg_version_match = pg_version_fact.value.match /^([\d\.]+)\.(\d)+$/
-      pg_base_version = pg_version_match[1]
-      unless pg_base_version.nil?
-        if File.exists?("/srv/postgresql/#{pg_base_version}/main/backup_label.old")
-          initialised = true
-        else
-          initialised = false
-        end
-      end
-    end
-    initialised
+    data_dir = Facter::Util::Resolution.exec("sudo -u postgres psql -tc 'SHOW data_directory;'").strip
+    File.exists?("#{data_dir}/backup_label.old")
   end
 end
