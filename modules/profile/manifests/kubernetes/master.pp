333d7e7d1f5dce83e33478a507877d43b36dd2ea
Janis Meybohm <jmeybohm@wikimedia.org>
2022-06-23 17:23:23 +0200

    kubernetes::master: Double the apiserver latency thresholds
    Periodic LIST requests for numerous large objects (helm release secrets
    in this case), lead to a significant bump in average LIST latency.
    Especially ofc. when there are very few LIST request selecting only a
    few (small) objects.
    
    Bug: T310714
    Change-Id: I275804e710b0ee3effcf247710b09b42b654e4d9

--- modules/profile/manifests/kubernetes/master.pp
+++ modules/profile/manifests/kubernetes/master.pp
@@ -120,8 +120,8 @@ class profile::kubernetes::master(
         query           => "instance_verb:apiserver_request_latencies_summary:avg5m{verb\\!~\"(CONNECT|WATCH|WATCHLIST)\",instance=\"${::ipaddress}:6443\"}",
         prometheus_url  => $prometheus_url,
         nan_ok          => true,
-        warning         => 50000,
-        critical        => 100000,
+        warning         => 100000,
+        critical        => 200000,
         dashboard_links => ['https://grafana.wikimedia.org/d/000000435/kubernetes-api?orgId=1&viewPanel=27'],
         notes_link      => 'https://wikitech.wikimedia.org/wiki/Kubernetes',
     }
