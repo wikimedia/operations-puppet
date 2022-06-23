66324f7da62f66a8ccb9c6e7ae66e197544ad514
jbond <jbond@wikimedia.org>
2022-06-23 16:34:23 +0200

    netbox: fix type definitions
    Change-Id: Icb7ed03d633f21bdcc141df8180d2b6edcf4aaa0

--- modules/netbox/types/host/location/virtual.pp
+++ modules/netbox/types/host/location/virtual.pp
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 type Netbox::Host::Location::Virtual = Struct[{
     # should at some point have a Wmflib::Site
-    site    => String[5,5],
-    cluster => Stdlib::Fqdn,
+    site           => String[5,5],
+    ganeti_cluster => Stdlib::Fqdn,
 }]
