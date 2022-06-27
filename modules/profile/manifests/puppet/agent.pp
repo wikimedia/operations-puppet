86affed988a5d153e823941f2217aa8384a5d754
jbond <jbond@wikimedia.org>
2022-06-27 17:05:49 +0200

    P:puppet::agent: move WMF customisations to profile
    Update the puppet agent profile and module to ensure WMF specific
    customisations are in the profile
    
    Hosts: sretest1001.eqiad.wmnet
    Change-Id: I3dd18b9f34b8868d180610e8a32807040b41c008

--- modules/profile/manifests/puppet/agent.pp
+++ modules/profile/manifests/puppet/agent.pp
@@ -10,31 +10,96 @@
 # @param dns_alt_names a list of dns alt names
 # @param certificate_revocation The level of certificate revocation to perform
 class profile::puppet::agent(
-  String                          $puppetmaster           = lookup('puppetmaster'),
-  Optional[String[1]]             $ca_server              = lookup('puppet_ca_server'),
-  Stdlib::Filesource              $ca_source              = lookup('puppet_ca_source'),
-  Boolean                         $manage_ca_file         = lookup('manage_puppet_ca_file'),
-  Integer[1,59]                   $interval               = lookup('profile::puppet::agent::interval'),
-  Optional[String[1]]             $environment            = lookup('profile::puppet::agent::environment'),
-  Enum['pson', 'json', 'msgpack'] $serialization_format   = lookup('profile::puppet::agent::serialization_format'),
-  Array[Stdlib::Fqdn]             $dns_alt_names          = lookup('profile::puppet::agent::dns_alt_names'),
-  Optional[Enum['chain', 'leaf']] $certificate_revocation = lookup('profile::puppet::agent::certificate_revocation'),
+    String                          $puppetmaster           = lookup('puppetmaster'),
+    Optional[String[1]]             $ca_server              = lookup('puppet_ca_server'),
+    Stdlib::Filesource              $ca_source              = lookup('puppet_ca_source'),
+    Boolean                         $manage_ca_file         = lookup('manage_puppet_ca_file'),
+    Integer[1,59]                   $interval               = lookup('profile::puppet::agent::interval'),
+    Optional[String[1]]             $environment            = lookup('profile::puppet::agent::environment'),
+    Enum['pson', 'json', 'msgpack'] $serialization_format   = lookup('profile::puppet::agent::serialization_format'),
+    Array[Stdlib::Fqdn]             $dns_alt_names          = lookup('profile::puppet::agent::dns_alt_names'),
+    Optional[Enum['chain', 'leaf']] $certificate_revocation = lookup('profile::puppet::agent::certificate_revocation'),
 ) {
 
-  class { 'puppet::agent':
-      ca_source              => $ca_source,
-      manage_ca_file         => $manage_ca_file,
-      server                 => $puppetmaster,
-      ca_server              => $ca_server,
-      dns_alt_names          => $dns_alt_names,
-      environment            => $environment,
-      interval               => $interval,
-      certificate_revocation => $certificate_revocation,
-  }
-  class { 'puppet_statsd':
-      statsd_host   => 'statsd.eqiad.wmnet',
-      metric_format => 'puppet.<%= metric %>',
-  }
-  class { 'prometheus::node_puppet_agent': }
-  include profile::puppet::client_bucket
+    class { 'puppet::agent':
+        ca_source              => $ca_source,
+        manage_ca_file         => $manage_ca_file,
+        server                 => $puppetmaster,
+        ca_server              => $ca_server,
+        dns_alt_names          => $dns_alt_names,
+        environment            => $environment,
+        certificate_revocation => $certificate_revocation,
+    }
+    class { 'puppet_statsd':
+        statsd_host   => 'statsd.eqiad.wmnet',
+        metric_format => 'puppet.<%= metric %>',
+    }
+    class { 'prometheus::node_puppet_agent': }
+    include profile::puppet::client_bucket
+
+    # Mode 0751 to make sure non-root users can access
+    # /var/lib/puppet/state/agent_disabled.lock to check if puppet is enabled
+    file { '/var/lib/puppet':
+        ensure => directory,
+        owner  => 'puppet',
+        group  => 'puppet',
+        mode   => '0751',
+    }
+    # WMF helper scripts
+    file {
+        default:
+            ensure => file,
+            mode   => '0555',
+            owner  => 'root',
+            group  => 'root';
+        '/usr/local/share/bash/puppet-common.sh':
+            source => 'puppet:///modules/base/puppet/puppet-common.sh';
+        '/usr/local/sbin/puppet-run':
+            content => template('base/puppet-run.erb');
+        '/usr/local/bin/puppet-enabled':
+            source => 'puppet:///modules/base/puppet/puppet-enabled';
+        '/usr/local/sbin/disable-puppet':
+            mode   => '0550',
+            source => 'puppet:///modules/base/puppet/disable-puppet';
+        '/usr/local/sbin/enable-puppet':
+            mode   => '0550',
+            source => 'puppet:///modules/base/puppet/enable-puppet';
+        '/usr/local/sbin/run-puppet-agent':
+            mode   => '0550',
+            source => 'puppet:///modules/base/puppet/run-puppet-agent';
+        '/usr/local/sbin/run-no-puppet':
+            mode   => '0550',
+            source => 'puppet:///modules/base/puppet/run-no-puppet';
+    }
+    $timer_interval = "*:${interval.fqdn_rand}/${interval}:00"
+
+    systemd::timer::job { 'puppet-agent-timer':
+        ensure        => present,
+        description   => "Run Puppet agent every ${interval} minutes",
+        user          => 'root',
+        ignore_errors => true,
+        command       => '/usr/local/sbin/puppet-run',
+        interval      => {'start' => 'OnCalendar', 'interval' => $timer_interval},
+    }
+
+    logrotate::rule { 'puppet':
+        ensure       => present,
+        file_glob    => '/var/log/puppet /var/log/puppet.log',
+        frequency    => 'daily',
+        compress     => true,
+        missing_ok   => true,
+        not_if_empty => true,
+        rotate       => 7,
+    }
+
+    rsyslog::conf { 'puppet-agent':
+        source   => 'puppet:///modules/base/rsyslog.d/puppet-agent.conf',
+        priority => 10,
+        require  => File['/etc/logrotate.d/puppet'],
+    }
+    motd::script { 'last-puppet-run':
+        ensure   => present,
+        priority => 97,
+        source   => 'puppet:///modules/base/puppet/97-last-puppet-run',
+    }
 }
--- modules/puppet/manifests/agent.pp
+++ modules/puppet/manifests/agent.pp
@@ -6,7 +6,6 @@
 # @param certname the agent certname
 # @param dns_alt_names a list of dns alt names
 # @param environment the agent environment
-# @param interval the, in minutes, interval to perform puppet runs
 # @param serialization_format the serilasation format of catalogs
 # @param ca_source to source of the CA file
 # @param certificate_revocation The level of certificate revocation to perform
@@ -18,13 +17,10 @@ class puppet::agent(
     Optional[String[1]]             $certname               = undef,
     Array[Stdlib::Fqdn]             $dns_alt_names          = [],
     Optional[String[1]]             $environment            = undef,
-    Integer                         $interval               = 30,
     Enum['pson', 'json', 'msgpack'] $serialization_format   = 'json',
     Optional[Stdlib::Filesource]    $ca_source              = undef,
     Optional[Enum['chain', 'leaf']] $certificate_revocation = undef,
 ) {
-    $crontime          = fqdn_rand(60, 'puppet-params-crontime')
-
     # augparse is required to resolve the augeasversion in facter3
     # facter needs virt-what for proper "virtual"/"is_virtual" resolution
     package { [ 'facter', 'puppet', 'augeas-tools', 'virt-what' ]:
@@ -92,106 +88,4 @@ class puppet::agent(
         enable => false,
     }
 
-    file { '/usr/local/share/bash/puppet-common.sh':
-        mode   => '0555',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/base/puppet/puppet-common.sh',
-    }
-
-    file { '/usr/local/sbin/puppet-run':
-        mode    => '0555',
-        owner   => 'root',
-        group   => 'root',
-        content => template('base/puppet-run.erb'),
-    }
-
-    file { '/usr/local/sbin/disable-puppet':
-        ensure => file,
-        mode   => '0550',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/base/puppet/disable-puppet',
-    }
-
-    file { '/usr/local/sbin/enable-puppet':
-        ensure => file,
-        mode   => '0550',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/base/puppet/enable-puppet',
-    }
-
-    file { '/usr/local/sbin/run-puppet-agent':
-        ensure => file,
-        mode   => '0550',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/base/puppet/run-puppet-agent',
-    }
-
-    file { '/usr/local/sbin/run-no-puppet':
-        mode   => '0550',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/base/puppet/run-no-puppet',
-    }
-
-    file { '/etc/cron.d/puppet':
-        ensure  => absent,
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        content => template('base/puppet.cron.erb'),
-        require => File['/usr/local/sbin/puppet-run'],
-    }
-
-    $timer_interval = "*:${interval.fqdn_rand}/${interval}:00"
-
-    systemd::timer::job { 'puppet-agent-timer':
-        ensure        => present,
-        description   => "Run Puppet agent every ${interval} minutes",
-        user          => 'root',
-        ignore_errors => true,
-        command       => '/usr/local/sbin/puppet-run',
-        interval      => {'start' => 'OnCalendar', 'interval' => $timer_interval}
-    }
-
-    logrotate::rule { 'puppet':
-        ensure       => present,
-        file_glob    => '/var/log/puppet /var/log/puppet.log',
-        frequency    => 'daily',
-        compress     => true,
-        missing_ok   => true,
-        not_if_empty => true,
-        rotate       => 7,
-    }
-
-    rsyslog::conf { 'puppet-agent':
-        source   => 'puppet:///modules/base/rsyslog.d/puppet-agent.conf',
-        priority => 10,
-        require  => File['/etc/logrotate.d/puppet'],
-    }
-
-    # Mode 0751 to make sure non-root users can access
-    # /var/lib/puppet/state/agent_disabled.lock to check if puppet is enabled
-    file { '/var/lib/puppet':
-        ensure => directory,
-        owner  => 'puppet',
-        group  => 'puppet',
-        mode   => '0751',
-    }
-
-    file { '/usr/local/bin/puppet-enabled':
-        mode   => '0555',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/base/puppet/puppet-enabled',
-    }
-
-    motd::script { 'last-puppet-run':
-        ensure   => present,
-        priority => 97,
-        source   => 'puppet:///modules/base/puppet/97-last-puppet-run',
-    }
 }
