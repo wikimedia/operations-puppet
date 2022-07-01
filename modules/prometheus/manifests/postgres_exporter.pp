cd0fccfae7666005ba79617c8448937c451d7d05
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-07-01 13:21:18 +0200

    prometheus::postgres_exporter: Remove support for stretch
    All postgres installations are on Buster or Bullseye.
    
    Hosts: C:prometheus::postgres_exporter
    Change-Id: I8cd32159d35ad6a4ff07a51ba89a96da3134f914

--- modules/prometheus/manifests/postgres_exporter.pp
+++ modules/prometheus/manifests/postgres_exporter.pp
@@ -10,10 +10,6 @@ class prometheus::postgres_exporter(
         ensure => $ensure,
     }
 
-    if debian::codename::eq('stretch') {
-        $postgres_exporter_extra_args = '-extend.query-path /etc/postgres-prometheus-exporter-queries.yaml'
-    }
-
     file { '/etc/default/prometheus-postgres-exporter':
         ensure  => $ensure,
         owner   => 'root',
