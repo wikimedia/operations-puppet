7c371b745bd8a40f391c7e02ea276a357ed1cda6
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-07-01 13:24:50 +0200

    snapshot: Remove support for stretch
    All dumps hosts are on buster (that class is only used in a single place
    and could just as well be inlined)
    
    Change-Id: Ibebe1f7ba9a0136e24875fe2a2c7efa12b072747

--- modules/snapshot/manifests/dumps/packages.pp
+++ modules/snapshot/manifests/dumps/packages.pp
@@ -1,12 +1,7 @@
 class snapshot::dumps::packages {
-    $mysql_client_pkg = case $::lsbdistcodename {
-        'buster': { 'default-mysql-client' }
-        'stretch': { 'mysql-client' }
-        default: { fail("Unsupported distro ${::lsbdistcodename}") }
-    }
     ensure_packages(['mwbzutils',
                     'p7zip-full',
-                    $mysql_client_pkg,
+                    'default-mysql-client',
                     'lbzip2',
                     'python3-yaml'])
 }
