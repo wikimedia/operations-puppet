760a5fc5bc4a70ff3f3cb4388f87d4be03abd5b1
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-07-08 12:50:05 +0200

    druid: Fixed UID/GIDs are universally in use now
    All hosts are on Buster.
    
    Hosts: C:druid::bigtop::hadoop::user
    Change-Id: I7d3e4c25e2f7cb8c502d700ab9e92401b2e8a035

--- modules/druid/manifests/bigtop/hadoop/user.pp
+++ modules/druid/manifests/bigtop/hadoop/user.pp
@@ -4,26 +4,18 @@
 #
 class druid::bigtop::hadoop::user {
 
-    # From Buster onward, we want to have fixed uid/gids for daemons.
     # We manage service system users in puppet classes, but declare
     # commented placeholders for them in the admin module's data.yaml file
     # to ensure that people don't accidentally add uid/gid conflicts.
-    if debian::codename::ge('buster') {
-        $druid_uid = 907
-        $druid_gid = 907
-    } else {
-        $druid_uid = undef
-        $druid_gid = undef
-    }
 
     group { 'druid':
         ensure => 'present',
         system => true,
-        gid    => $druid_gid,
+        gid    => 907,
     }
     user { 'druid':
         ensure     => 'present',
-        uid        => $druid_uid,
+        uid        => 907,
         gid        => 'druid',
         shell      => '/bin/false',
         home       => '/nonexistent',
