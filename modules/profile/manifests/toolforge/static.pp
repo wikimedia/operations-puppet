e40e320cbfa0c050ae5756d8bb95e48e7233d1df
David Caro <dcaro@wikimedia.org>
2022-07-08 08:25:31 +0000

    Revert "P:toolforge::static: add blackbox monitoring"
    This reverts commit 7feb7c28f14f4a30f3ab5f3724d367099c39ac28.
    
    Reason for revert: Not ready until T311911 is finished
    
    Change-Id: I6cbce9117c24533dca449c83ef845936404b8368

--- modules/profile/manifests/toolforge/static.pp
+++ modules/profile/manifests/toolforge/static.pp
@@ -1,6 +1,5 @@
 class profile::toolforge::static (
-    Stdlib::Fqdn $static_domain = lookup('profile::toolforge::static::static_domain', {default_value => 'tools-static.wmflabs.org'}),
-    Stdlib::Fqdn $web_domain    = lookup('profile::toolforge::web_domain', {default_value => 'toolforge.org'}),
+    Stdlib::Fqdn $web_domain = lookup('profile::toolforge::web_domain', {default_value => 'toolforge.org'}),
 ) {
     $resolver = join($::nameservers, ' ')
     $fingerprints_dir = '/var/www/fingerprints'
@@ -13,15 +12,5 @@ class profile::toolforge::static (
 
     class { 'ssh::publish_fingerprints':
         document_root => $fingerprints_dir,
-    }
-
-    prometheus::blackbox::check::http { $static_domain:
-        port                => 80,
-        # this should always exist
-        path                => '/admin/fingerprints/',
-        ip_families         => ['ip4'],
-        prometheus_instance => 'tools',
-        team                => 'wmcs',
-        severity            => 'warning',
     }
 }
