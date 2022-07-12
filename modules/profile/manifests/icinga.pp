40225c164b124bfa21a1ae224a35290485990bce
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2022-07-12 12:27:58 +0200

    icinga: fix ip addresses for commons.wikimedia.org probe
    Bug: T305847
    Change-Id: I2b8db5b98f086f20d6b3d88bc8c6400f8f0be76a

--- modules/profile/manifests/icinga.pp
+++ modules/profile/manifests/icinga.pp
@@ -77,6 +77,9 @@ class profile::icinga(
     # monitoring of content on commons (T124812)
     prometheus::blackbox::check::http { 'commons.wikimedia.org':
         fqdn               => 'commons.wikimedia.org',
+        # Not ideal but good enough, see task for more context https://phabricator.wikimedia.org/T312840
+        ip4                => ipresolve("text-lb.${::site}.wikimedia.org", 4),
+        ip6                => ipresolve("text-lb.${::site}.wikimedia.org", 6),
         path               => '/wiki/Main_Page',
         body_regex_matches => ['Picture of the day'],
         severity           => 'page',
