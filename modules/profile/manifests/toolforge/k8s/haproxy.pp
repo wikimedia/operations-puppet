c69583a1b09d6cd7c367277ab4d4756ca6566128
Taavi Väänänen <hi@taavi.wtf>
2022-07-13 18:45:55 +0300

    P:toolforge:k8s:haproxy: add k8s-ingress monitoring
    Monitors that the entire haproxy->ingress-nginx->webservice flow works
    properly by monitoring that the 'admin' tool is alive and that a
    non-existent webservice returns the tool-fourohfour error view.
    
    Change-Id: I5f879f67e596b427f8406a533174560409f4d22c

--- modules/profile/manifests/toolforge/k8s/haproxy.pp
+++ modules/profile/manifests/toolforge/k8s/haproxy.pp
@@ -16,6 +16,7 @@ class profile::toolforge::k8s::haproxy (
     Array[Stdlib::Fqdn] $keepalived_vips     = lookup('profile::toolforge::k8s::haproxy::keepalived_vips',     {default_value => []}),
     Array[Stdlib::Fqdn] $keepalived_peers    = lookup('profile::toolforge::k8s::haproxy::keepalived_peers',    {default_value => ['localhost']}),
     String              $keepalived_password = lookup('profile::toolforge::k8s::haproxy::keepalived_password', {default_value => 'notarealpassword'}),
+    Stdlib::Fqdn        $web_domain          = lookup('profile::toolforge::web_domain',                        {default_value => 'toolforge.org'}),
 ) {
     class { 'haproxy::cloud::base': }
 
@@ -50,5 +51,25 @@ class profile::toolforge::k8s::haproxy (
             peers     => delete($keepalived_peers, $::fqdn),
             vips      => $keepalived_vips.map |$host| { ipresolve($host, 4) },
         }
+    }
+
+    prometheus::blackbox::check::http {
+        default:
+            port                => $ingress_port,
+            ip_families         => ['ip4'],
+            prometheus_instance => 'tools',
+            team                => 'wmcs',
+            severity            => 'warning';
+
+        # well-known-to-exist web service
+        "admin.${web_domain}":
+            path               => '/healthz',
+            body_regex_matches => ['OK'];
+
+        # monitor the 404 handler
+        # creation on this tool has been blocked by the title blacklist
+        "this-tool-does-not-exist.${web_domain}":
+            body_regex_matches => ['The URL you have requested'],
+            status_matches     => [404];
     }
 }
