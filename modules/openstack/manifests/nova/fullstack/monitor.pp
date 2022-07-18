137d00bf77bb491b76624e9cd6502ee93e85f009
David Caro <dcaro@wikimedia.org>
2022-07-18 14:47:09 +0200

    wmcs.novafullstack: Remove nrpe check and cleanup absent resources
    We don't need that check anymore, also clean up old absented
    resources.
    
    Bug: T316919
    Change-Id: I3a22d88510b058864f362285b3de54626516196f
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- modules/openstack/manifests/nova/fullstack/monitor.pp
+++ modules/openstack/manifests/nova/fullstack/monitor.pp
@@ -1,28 +1,15 @@
 class openstack::nova::fullstack::monitor {
 
+    # Remove once puppet runs a couple times
     nrpe::monitor_service { 'nova-fullstack':
-        description   => 'nova instance creation test',
-        nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 1: -C python3 -a nova-fullstack',
-        contact_group => 'wmcs-team,admins',
-        retries       => 2,
-        notes_url     => 'https://wikitech.wikimedia.org/wiki/Portal:Cloud_VPS/Admin/Troubleshooting',
-    }
-
-    # replaced by prometheus stats
-    file { '/usr/local/bin/check_nova_fullstack_leaks.py':
-        ensure => 'absent',
-    }
-    nrpe::monitor_service { 'check-fullstack-failures':
-        ensure => 'absent',
-    }
-
-    # Script to make sure that every flavor is assigned to a host aggregate
-    nrpe::plugin { 'check_flavor_properties':
-        source => 'puppet:///modules/openstack/monitor/nova/check_flavor_properties.py',
+        ensure => absent,
     }
 
     # Make sure every flavor is assigned to an aggregate, to avoid
-    #  things like T259542
+    # things like T259542
+    nrpe::plugin { 'check_flavor_properties':
+        source => 'puppet:///modules/openstack/monitor/nova/check_flavor_properties.py',
+    }
     nrpe::monitor_service { 'check-flavor_aggregates':
         ensure         => 'present',
         nrpe_command   => '/usr/local/lib/nagios/plugins/check_flavor_properties',
