cd1a580441b4681c3fc96b68baaafbdad261a828
David Caro <dcaro@wikimedia.org>
2022-07-18 09:31:45 +0200

    kiwix: create dest dir before rsyncing if it does not exist
    We were not creating it, and in new hosts (like cloudumps*) it was
    making the cron fail continuously.
    
    Change-Id: I988496fb67349396929025b7b92293b6d7e23535
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- modules/dumps/files/fetches/kiwix-rsync-cron.sh
+++ modules/dumps/files/fetches/kiwix-rsync-cron.sh
@@ -14,30 +14,36 @@ do_rsync(){
     local srcpath=$1
     local destpath=$2
     local destroot=$3
+    local destdir="${destroot}/${destpath}"
 
     local running=$(
-        /usr/bin/pgrep -f -x "/usr/bin/rsync -rlptq $BWLIMIT ${SOURCEHOST}::${srcpath} ${destroot}/${destpath}"
+        /usr/bin/pgrep -f -x "/usr/bin/rsync -rlptq $BWLIMIT ${SOURCEHOST}::${srcpath} ${destdir}"
     )
-    if [[ "$running" == "" ]]; then
-        # filter out messages of the type
-        #   file has vanished: "/zim/wikipedia/.wikipedia_tg_all_nopic_2016-05.zim.TQH5Zv" (in download.kiwix.org)
-        #   rsync warning: some files vanished before they could be transferred (code 24) at main.c(1655) [generator=3.1.1]
-        # cat makes the pipeline command never fail
-        /usr/bin/rsync \
-            -rlptq \
-            --delete \
-            "$BWLIMIT" \
-            "${SOURCEHOST}::${srcpath}" \
-            "${destroot}/${destpath}" \
-        2>&1 \
-        | grep -v 'vanished' \
-        | cat
-        # now we check the return code of rsync
-        local rsync_rc=${PIPESTATUS[0]}
-        if [[ "$rsync_rc" != "0" ]]; then
-            echo "Error while running rsync, check the logs..."
-            return $rsync_rc
-        fi
+    if [[ "$running" != "" ]]; then
+        echo "Already running, skipping: $running"
+        return 0
+    fi
+
+    [[ -e "$destdir" ]] || mkdir -p "$destdir"
+
+    # filter out messages of the type
+    #   file has vanished: "/zim/wikipedia/.wikipedia_tg_all_nopic_2016-05.zim.TQH5Zv" (in download.kiwix.org)
+    #   rsync warning: some files vanished before they could be transferred (code 24) at main.c(1655) [generator=3.1.1]
+    # cat makes the pipeline command never fail
+    /usr/bin/rsync \
+        -rlptq \
+        --delete \
+        "$BWLIMIT" \
+        "${SOURCEHOST}::${srcpath}" \
+        "${destroot}/${destpath}" \
+    2>&1 \
+    | grep -v 'vanished' \
+    | cat
+    # now we check the return code of rsync
+    local rsync_rc=${PIPESTATUS[0]}
+    if [[ "$rsync_rc" != "0" ]]; then
+        echo "Error while running rsync, check the logs..."
+        return $rsync_rc
     fi
     return 0
 }
