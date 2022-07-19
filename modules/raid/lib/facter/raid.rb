7475cde50bbea5272dac9091682f1e90e8c0eeb3
jbond <jbond@wikimedia.org>
2022-07-19 15:16:48 +0200

    raid_fact: Add new refactored raid fact
    This is now solely based on PCI IDs (except for software RAID), which
    allows for more reliable detection of controllers compared to the
    device probing logic used on the old raid fact.
    
    Bug: T313312
    Change-Id: I70345ce350a6354e734b32bccf22e40b33ac739f

--- modules/raid/lib/facter/raid.rb
+++ modules/raid/lib/facter/raid.rb
@@ -1,6 +1,30 @@
 # SPDX-License-Identifier: Apache-2.0
 require 'facter'
 
+Facter.add('raid_mgmt_tools') do
+  # ref: http://pci-ids.ucw.cz/v2.2/pci.ids
+  # Run sudo lspci -nn to add missing PCI IDs, the combination of vendor ID and device ID is printed in []
+  pci_ids = {
+    '9005028f' => 'ssacli',   # Smart Storage PQI 12G SAS/PCIe 3
+    '100010e2' => 'perccli',  # Perc 750
+    '103c3239' => 'hpssacli', # Hewlett-Packard Company Smart Array Gen9 Controllers
+    '1000005d' => 'megaraid', # LSI Logic / Symbios Logic MegaRAID SAS-3 3108
+    '100010e2' => 'megaraid', # Broadcom / LSI MegaRAID 12GSAS/PCIe Secure SAS39x
+  }
+  setcode do
+    raids = []
+
+    File.open('/proc/bus/pci/devices').each do |line|
+      words = line.split
+      raids.push(pci_ids[words[1]]) if pci_ids.key?(words[1])
+    end
+
+    if File.exists?('/proc/mdstat') && File.open('/proc/mdstat').grep(/md\d+\s+:\s+active/)
+      raids.push('mdadm')
+    end
+  end
+end
+
 Facter.add('raid') do
   confine :kernel => :linux
   # ref: http://pci-ids.ucw.cz/v2.2/pci.ids
