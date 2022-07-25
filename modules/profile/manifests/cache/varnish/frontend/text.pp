2792c622f025fc43c6a378acc986f02c59445c2e
Brandon Black <bblack@wikimedia.org>
2022-07-25 15:23:12 +0000

    cache::text - include esitest service
    Bug: T308799
    Change-Id: I0e3446dd684fa824f506fe1e5af29928fad588d4

--- modules/profile/manifests/cache/varnish/frontend/text.pp
+++ modules/profile/manifests/cache/varnish/frontend/text.pp
@@ -2,4 +2,9 @@ class profile::cache::varnish::frontend::text {
     # for VCL compilation using libGeoIP
     class { '::geoip': }
     class { '::geoip::dev': }
+
+    # Include ESI testing backend service in all text nodes
+    class { '::esitest':
+        numa_iface => $facts['interface_primary']
+    }
 }
