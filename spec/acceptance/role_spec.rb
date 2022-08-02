063cc77c37db2ef3180107296993be2c6443b060
jbond <jbond@wikimedia.org>
2022-08-02 14:11:24 +0200

    beaker: fail if no changes preformed in first puppet run
    Change-Id: I1e59125db97e3c407d82946df9dece57cc7a7578

--- spec/acceptance/role_spec.rb
+++ spec/acceptance/role_spec.rb
@@ -7,7 +7,7 @@ describe 'sretest role' do
   describe 'running puppet code' do
     it 'work with no errors' do
       pp = PP_HEADER
-      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, expect_changes: true)
       apply_manifest(pp, catch_failures: true)
       expect(apply_manifest(pp, catch_failures: true).exit_code).to eq 0
     end
