03ba9431693800e7d7333f0ef628375a5f266140
Reuven Lazarus <rlazarus@wikimedia.org>
2022-08-02 11:05:42 -0700

    site: Add mc2038 as mediawiki::memcached
    Bug: T293012
    Change-Id: I9ce60125005914b88b27b2fbdcc36a7dacd04692

--- manifests/site.pp
+++ manifests/site.pp
@@ -1971,12 +1971,12 @@ node /^mc10(3[7-9]|4[0-9]|5[0-4])\.eqiad\.wmnet/ {
     role(mediawiki::memcached)
 }
 
-node /^mc20(19|2[0-7]|29|3[0-7])\.codfw\.wmnet/ {
+node /^mc20(19|2[0-7]|29|3[0-8])\.codfw\.wmnet/ {
     role(mediawiki::memcached)
 }
 
 # New mc nodes T294962
-node /^mc20(3[8-9]|4[0-9]|5[0-5])\.codfw\.wmnet/ {
+node /^mc20(39|4[0-9]|5[0-5])\.codfw\.wmnet/ {
     role(insetup)
 }
 
