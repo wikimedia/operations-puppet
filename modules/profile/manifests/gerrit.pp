63248579559c7cf3b56a040334382691307d90dc
jbond <jbond@wikimedia.org>
2022-08-02 11:26:02 +0200

    P:gerrit: add ipaddress to host_aliases
    Hosts: P:gerrit
    Bug: T303857
    Bug: T310395
    Change-Id: Ic50ef35183243be9c8468df1302ea900e73f8fcd

--- modules/profile/manifests/gerrit.pp
+++ modules/profile/manifests/gerrit.pp
@@ -107,9 +107,10 @@ class profile::gerrit(
     $sshkey = 'AAAAB3NzaC1yc2EAAAADAQABAAAAgQCF8pwFLehzCXhbF1jfHWtd9d1LFq2NirplEBQYs7AOrGwQ/6ZZI0gvZFYiEiaw1o+F1CMfoHdny1VfWOJF3mJ1y9QMKAacc8/Z3tG39jBKRQCuxmYLO1SWymv7/Uvx9WQlkNRoTdTTa9OJFy6UqvLQEXKYaokfMIUHZ+oVFf1CgQ=='
 
     @@sshkey { 'gerrit.wikimedia.org':
-        ensure => 'present',
-        key    => $sshkey,
-        type   => 'ssh-rsa',
+        ensure       => 'present',
+        key          => $sshkey,
+        type         => 'ssh-rsa',
+        host_aliases => [ipresolve('gerrit.wikimedia.org'), ipresolve('gerrit.wikimedia.org', 6)],
     }
 
     # Ship Gerrit built-in logs to ELK
