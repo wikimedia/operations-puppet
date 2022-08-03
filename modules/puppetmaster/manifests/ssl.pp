82089b215d520e878ae7856f5dae9f84c3e13b78
jbond <jbond@wikimedia.org>
2022-08-03 16:09:22 +0200

    C:puppetmaster::ssl: ensure we generate the ssl certificate
    Hosts: C:puppetmaster::ssl
    Bug: T314136
    Change-Id: Ib550ec2793a7a7c92c7a6f89f8183fec815d795c

--- modules/puppetmaster/manifests/ssl.pp
+++ modules/puppetmaster/manifests/ssl.pp
@@ -1,7 +1,9 @@
-
+# @summary configure puppet ssl
+# @param server_name the puppet server name
+# @param ssldir tyhe ssl directory to use
 class puppetmaster::ssl(
-    Stdlib::Fqdn $server_name='puppet',
-    Stdlib::Unixpath $ssldir = '/var/lib/puppet/server/ssl'
+    Stdlib::Fqdn     $server_name = 'puppet',
+    Stdlib::Unixpath $ssldir      = '/var/lib/puppet/server/ssl'
 ){
 
     # TODO: Hack to make class pass tests
@@ -26,11 +28,15 @@ class puppetmaster::ssl(
             "${ssldir}/certificate_requests",
             "${ssldir}/certs",
             "${ssldir}/private",
-            "${ssldir}/private_keys",
-            "${ssldir}/public_keys",
             "${ssldir}/crl",
         ]:
             ensure => directory;
+        [
+            "${ssldir}/private_keys",
+            "${ssldir}/public_keys",
+        ]:
+            ensure => directory,
+            mode   => '0750',;
     }
 
     exec { 'setup crl dir':
@@ -39,4 +45,12 @@ class puppetmaster::ssl(
         command => "ln -s ${ssldir}/ca/ca_crl.pem ${ssldir}/crl/$(openssl crl -in ${ssldir}/ca/ca_crl.pem -hash -noout).r0",
         onlyif  => "test ! -L ${ssldir}/crl/$(openssl crl -in ${ssldir}/ca/ca_crl.pem -hash -noout).r0",
     }
+    # required so passanger app can start
+    exec { 'generate puppet private key':
+        command => '/usr/bin/puppet master',
+        creates => "${ssldir}/private_keys/${server_name}.pem",
+        require => File["${ssldir}/private_keys"],
+        before  => Service['apache2'],
+    }
+
 }
--- modules/puppetmaster/spec/classes/puppetmaster_ssl_spec.rb
+++ modules/puppetmaster/spec/classes/puppetmaster_ssl_spec.rb
@@ -1,5 +1,12 @@
 require_relative '../../../../rake_modules/spec_helper'
 
 describe 'puppetmaster::ssl' do
-    it { should compile }
+  on_supported_os(WMFConfig.test_on).each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+      let(:pre_condition) { 'service{"apache2": }' }
+
+      it { is_expected.to compile }
+    end
+  end
 end
