c7319d3a97a07531703be1a63b7fbdcfea053000
Ben Tullis <btullis@wikimedia.org>
2022-08-04 11:52:02 +0100

    Bootstrap etcd on the dse_k8s_etcd cluster
    We are bootstrapping a new etcd cluster in support of the dse_k8s
    kubernetes cluster. The hosts and roles exist already, but this commit
    addds the configuration required to bootstrap the etcd cluster.
    
    DNS SRV records already exist for this new cluster. The certificates
    will be automatically generated due to the use of the `use_pki_certs`
    parameter.
    
    In addition we are defining two new clusters in the wikimedia_clusters
    hiera structure, which will be used to refer to the dse_k8s and
    dse_k8s_etcd clusters.
    
    Hosts: O:etcd::v3::dse_k8s_etcd
    Bug: T313129
    Change-Id: Ieaef9761fba925c173322c8c030b7df6b97beca5

--- modules/profile/manifests/etcd/v3.pp
+++ modules/profile/manifests/etcd/v3.pp
@@ -87,7 +87,7 @@ class profile::etcd::v3(
     # TODO: consider allowing the use of other intermediate CAs, other than 'discovery'
     else {
         $trusted_ca  = '/etc/ssl/certs/wmf-ca-certificates.crt'
-        $ssl_paths = profile::pki::get_cert('discovery', $certname, { hosts => $certname,} )
+        $ssl_paths = profile::pki::get_cert('discovery', $certname, { hosts => [$facts['networking']['fqdn']],} )
     }
 
     # Service
--- modules/role/manifests/etcd/v3/dse_k8s_etcd.pp
+++ modules/role/manifests/etcd/v3/dse_k8s_etcd.pp
@@ -4,7 +4,7 @@
 class role::etcd::v3::dse_k8s_etcd {
     include ::profile::base::production
     include ::profile::base::firewall
-    #include ::profile::etcd::v3
+    include ::profile::etcd::v3
 
     system::role { 'role::etcd::v3::dse_k8s_etcd':
         description => 'dse_k8s_etcd etcd cluster member'
