e9298a27801841616e00676c022a8106d694ff07
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-08-05 10:11:24 +0200

    sbuild: Remove support for stretch
    Hosts: C:profile::toolforge::package_builder
    Change-Id: Idb256dd53a73d676d80a8dd7ba8166726af0b6e6

--- modules/sbuild/manifests/chroot.pp
+++ modules/sbuild/manifests/chroot.pp
@@ -4,11 +4,7 @@ define sbuild::chroot(
 ) {
     require ::sbuild
 
-    if debian::codename::ge('buster'){
-        $sbuild_cmd    = '/usr/bin/sbuild-createchroot --include=eatmydata,ccache'
-    } else {
-        $sbuild_cmd    = '/usr/sbin/sbuild-createchroot --include=eatmydata,ccache'
-    }
+    $sbuild_cmd    = '/usr/bin/sbuild-createchroot --include=eatmydata,ccache'
     $sbuild_mirror = 'http://127.0.0.1:3142/deb.debian.org/debian'
 
     $chroot_name = "${distribution}-amd64-sbuild"
