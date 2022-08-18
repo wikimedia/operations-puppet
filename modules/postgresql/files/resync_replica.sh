039554214fd1862a1ef1c75da2c4f3300cf5f2b1
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2022-08-18 11:11:28 +0200

    postgresql: resync_replica improvements and fixes
    When setting up Dispatch database replicas I came across resync_replica
    and when trying it I discovered the following:
    
    * /srv/postgresql is hardcoded (netbox and dispatch use /srv/postgres)
    * PG version can be autodetected
    
    While I was at it I took the chance to (IMHO) improve the script,
    namely:
    
    * Bail when reading unset variables
    * Run pg_basebackup as user postgres so the resulting files will have
      the correct ownership
    
    Bug: T313229
    Change-Id: I10488ce60986637ed19a611f5a44bf5f7adf70d2

--- modules/postgresql/files/resync_replica.sh
+++ modules/postgresql/files/resync_replica.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 set -e
+set -u
 
-POSTGRES_VERSION=9.6
+POSTGRES_VERSION=$(/usr/share/postgresql-common/supported-versions | tail -1)
 
 function show_help() {
     echo "Resync the local replica to the configured Postgres primary. Removes all local data"
@@ -10,14 +11,14 @@ function show_help() {
 }
 
 function parse_args() {
-    if [ -n "$1" ]; then
+    if [ -n "${1:-}" ]; then
         POSTGRES_VERSION="$1"
         echo "Setting postgres version to $POSTGRES_VERSION"
     fi
 }
 
 parse_args "$@"
-POSTGRES_PATH="/srv/postgresql/$POSTGRES_VERSION/main"
+POSTGRES_PATH=$(pg_conftool "$POSTGRES_VERSION" main show data_directory -s)
 
 if [ ! -d "$POSTGRES_PATH" ]; then
     echo "Postgres data dir not found at $POSTGRES_PATH"
@@ -28,23 +29,20 @@ echo "Stopping postgres"
 service postgresql stop
 
 rm -R "$POSTGRES_PATH"
-mkdir -p "$POSTGRES_PATH"
+install --directory --mode 700 --owner postgres --group postgres "$POSTGRES_PATH"
 
-# pg_basebackup will use PGPASSFILE for credentials
-# shellcheck disable=SC2034
-export PGPASSFILE="/etc/postgresql/${POSTGRES_VERSION}/main/.pgpass"
-export PG_HOST=$(cat $PGPASSFILE | cut -f1 -d:)
+PGPASSFILE="/etc/postgresql/${POSTGRES_VERSION}/main/.pgpass"
+PG_HOST=$(cut -f1 -d: < "$PGPASSFILE")
 
 # -R will cause pg_basebackup to write out a new recovery.conf -
 # without this file, postgres will start up as a non-replica and the
 # host will need to be re-synced. Puppet will overwrite this file on a
 # later run but it will more or less contain the same information
-echo "Starting backup from primary - this will take a while"
-/usr/bin/pg_basebackup -R -X stream -D "$POSTGRES_PATH" -U replication -w -h "$PG_HOST"
-echo "Backup complete"
+BACKUP_COMMAND="PGPASSFILE=\"$PGPASSFILE\" pg_basebackup -R -X stream -D \"$POSTGRES_PATH\" -U replication -w -h \"$PG_HOST\""
 
-chown -R postgres:postgres "$POSTGRES_PATH"
-chmod 700 "$POSTGRES_PATH"
+echo "Starting backup from primary - this will take a while"
+su postgres --login --command "$BACKUP_COMMAND"
+echo "Backup complete"
 
 echo "Resync complete - restarting postgres service"
 
