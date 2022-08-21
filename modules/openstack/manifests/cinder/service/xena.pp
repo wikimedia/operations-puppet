a872e729870e3f3b109d33d2f996a6241431b5ad
Andrew Bogott <abogott@wikimedia.org>
2022-08-20 22:44:16 -0500

    Add manifests for Openstack Cinder Xena
    Bug: T296561
    Change-Id: Iae3a9f151da1a61f159955a5dddfdc5686a6e4d3

---
+++ modules/openstack/manifests/cinder/service/xena.pp
@@ -0,0 +1,45 @@
+# SPDX-License-Identifier: Apache-2.0
+
+class openstack::cinder::service::xena(
+    Stdlib::Port $api_bind_port,
+) {
+    require "openstack::serverpackages::xena::${::lsbdistcodename}"
+    require 'openstack::cinder::user'
+    # config should have been declared via a profile, with proper hiera, and is
+    # here only for ordering/dependency purposes:
+    require 'openstack::cinder::config::xena'
+
+    package { 'cinder-api':
+        ensure => 'present',
+    }
+    package { 'cinder-scheduler':
+        ensure => 'present',
+    }
+    package { 'cinder-volume':
+        ensure => 'present',
+    }
+
+    file {
+        '/etc/cinder/policy.json':
+            ensure  => 'absent';
+        '/etc/cinder/policy.yaml':
+            source  => 'puppet:///modules/openstack/xena/cinder/policy.yaml',
+            owner   => 'cinder',
+            group   => 'cinder',
+            mode    => '0644',
+            require => Package['cinder-api'];
+        '/etc/cinder/resource_filters.json':
+            source  => 'puppet:///modules/openstack/xena/cinder/resource_filters.json',
+            owner   => 'cinder',
+            group   => 'cinder',
+            mode    => '0644',
+            require => Package['cinder-api'];
+        '/etc/init.d/cinder-api':
+            content => template('openstack/xena/cinder/cinder-api'),
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755',
+            notify  => Service['cinder-api'],
+            require => Package['cinder-api'];
+    }
+}
