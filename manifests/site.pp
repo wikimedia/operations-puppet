55b99d39a082c859af15c403a97ed1eabdb5190e
Manuel Arostegui <marostegui@wikimedia.org>
2022-08-22 12:10:56 +0200

    site.pp: Remove insetup from db1195
    This host is now in m1
    
    Bug: T313569
    Change-Id: I199181c2dcec284c5d5be457cb2f90e5e85bb71c

--- manifests/site.pp
+++ manifests/site.pp
@@ -516,7 +516,7 @@ node /^datahubsearch100[1-3]\.eqiad\.wmnet$/ {
 
 # MariaDB 10
 # new db servers T306928
-node /^db11(8[5-9]|9[0-5]).eqiad.wmnet$/ {
+node /^db11(8[5-9]|9[0-4]).eqiad.wmnet$/ {
     role(insetup)
 }
 
