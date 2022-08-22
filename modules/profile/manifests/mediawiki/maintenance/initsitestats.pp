aaf8d7254339294924a52074afea38139f13d6c9
Alexander Vorwerk <zabe@avorwerk.net>
2022-08-23 00:54:23 +0200

    Run the initsitestats period job on a daily basis
    After new wikis are created and their content is imported by Incubator,
    it currently takes up to two weeks for the statistics to be updated on
    Special:Statistics.
    
    Running the update script does not seem to be very expensive, so it
    seems appropriate to run the script daily to fix potential drifts
    instead of letting them persist for up to two weeks.
    
    Bug: T315121
    Change-Id: Ie98bf696620a3c8a73efac2509dbc9af4f062a62

--- modules/profile/manifests/mediawiki/maintenance/initsitestats.pp
+++ modules/profile/manifests/mediawiki/maintenance/initsitestats.pp
@@ -1,6 +1,6 @@
 class profile::mediawiki::maintenance::initsitestats {
     profile::mediawiki::periodic_job { 'initsitestats':
         command  => '/usr/local/bin/foreachwiki initSiteStats.php --update',
-        interval => '*-1,15 05:39',
+        interval => '*-*-* 05:39:00',
     }
 }
