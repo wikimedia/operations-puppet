893b87645e49295f8c2c1fdcc57cdbbc3ae24442
Andrew Bogott <abogott@wikimedia.org>
2022-08-22 12:16:57 -0500

    Keystone: expand the password safelist to specify restricted domains
    We want to limit API access for users doing default domain actions,
    but service domains (e.g. Heat and Magnum) need to allow access
    from VMs and service users.
    
    Change-Id: I4b3aba027386288f9fe833d31baf2819a5625637

--- modules/openstack/files/xena/keystone/wmfkeystoneauth/password_safelist.py
+++ modules/openstack/files/xena/keystone/wmfkeystoneauth/password_safelist.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
-
+#
 # Copyright 2016 Andrew Bogott for the Wikimedia Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -35,22 +35,34 @@ safelist_ops = [
                     help='user:ip range permitted to use password auth.'
                          'also supports a simple one-character * wildcard'
                          'for user.'),
+    cfg.ListOpt('password_restricted_domains',
+                default=['default'],
+                help='list of domain ids on which to restrict password '
+                     'access. Typically we restrict password access on '
+                     'the default domain but permit access in service '
+                     'domains (e.g. Heat) for VM-managed orchestration.'),
 ]
 
 CONF = cfg.CONF
 CONF.register_opts(safelist_ops, group='auth')
 
 
-def check_safelist(user_id, remote_addr):
+def check_safelist(user_id, remote_addr, domain_id):
     """Return True if the user_id/remote_addr combination is in our safelist.
        Otherwise, return raise Unauthorized"""
-    LOG.debug("Auth request for user %s from %s" % (user_id,
-                                                    remote_addr))
+    LOG.debug("Auth request for user %s from %s in domain %s" % (user_id,
+                                                                 remote_addr,
+                                                                 domain_id))
+
+    if domain_id not in CONF.auth.password_restricted_domains:
+        LOG.debug('Password auth permitted on unrestricted domain %s' % domain_id)
+        return True
 
     for entry in CONF.auth.password_safelist:
         user, subnet = entry.split(':', 1)
         if user == "*" or user_id == user:
             if IPAddress(remote_addr) in IPNetwork(subnet):
+                LOG.debug('Password auth found in safelist.')
                 return True
 
     LOG.warn('Password auth not allowed for %s from %s' % (user_id,
@@ -68,6 +80,7 @@ class PasswordSafelist(password.Password):
 
         user_info = auth_plugins.UserAuthInfo.create(auth_payload, METHOD_NAME)
         check_safelist(user_info.user_id,
-                       flask.request.environ.get('REMOTE_ADDR'))
+                       flask.request.environ.get('REMOTE_ADDR'),
+                       user_info.domain_id)
 
         return super(PasswordSafelist, self).authenticate(auth_payload)
