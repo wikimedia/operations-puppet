b93a26cb626f3bc5e1ac6b66c0e70ab9b2164f76
Daniel Zahn <dzahn@wikimedia.org>
2022-08-24 09:50:27 -0700

    mediwiki/initsitestats: change time of day to run initsitestats
    It was requested to move this timer as close as possible
    to another timer that runs in cloud and creates stats tables.
    
    It took ~ 2h 25m to run when I looked at it today.
    
    So about 3 hours before midnight because the other timer runs
    at midnight UTC.
    
    Bug: T315121
    Change-Id: I4d219c03433593bc719e98ba3f20a7a489860b29

--- modules/profile/manifests/mediawiki/maintenance/initsitestats.pp
+++ modules/profile/manifests/mediawiki/maintenance/initsitestats.pp
@@ -1,6 +1,6 @@
 class profile::mediawiki::maintenance::initsitestats {
     profile::mediawiki::periodic_job { 'initsitestats':
         command  => '/usr/local/bin/foreachwiki initSiteStats.php --update',
-        interval => '*-*-* 05:39:00',
+        interval => '*-*-* 21:00:00',
     }
 }
