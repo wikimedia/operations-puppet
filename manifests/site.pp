c10a9f46b2068f8e39a049bf50d7b494f6831f8d
Manuel Arostegui <marostegui@wikimedia.org>
2022-08-25 11:23:03 +0200

    site.pp: Remove db1193 from insetup
    Bug: T313569
    Change-Id: Iced13cb902e0d61333daf490f9a249951613c4d9

--- manifests/site.pp
+++ manifests/site.pp
@@ -516,7 +516,7 @@ node /^datahubsearch100[1-3]\.eqiad\.wmnet$/ {
 
 # MariaDB 10
 # new db servers T306928
-node /^db11(93|94).eqiad.wmnet$/ {
+node /^db11(94).eqiad.wmnet$/ {
     role(insetup)
 }
 
