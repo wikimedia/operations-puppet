cdff9c20ae11cea5afce1165dbdab61df26a600b
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2022-08-29 09:05:52 +0200

    memcached: adapt values to bullseye
    Specifically, we remove temporarily the profiled::memcached::memkeys
    while we decide on an alternative.
    
    Change-Id: Iff737ac9c13321a2a6dfb9bed691079ec8ea5f17

--- modules/role/manifests/memcached.pp
+++ modules/role/manifests/memcached.pp
@@ -8,6 +8,6 @@ class role::memcached{
     include ::profile::base::production
     include ::profile::base::firewall
     include profile::memcached::instance
-    include profile::memcached::memkeys
+    #include profile::memcached::memkeys
     include profile::memcached::performance
 }
