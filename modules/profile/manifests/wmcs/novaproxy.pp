ad0c22da1cc4050a705ddd89f2cbcb9efd427548
Taavi Väänänen <hi@taavi.wtf>
2022-09-03 17:53:03 +0300

    P:wmcs::novaproxy: enable keepalived for HA
    Bug: T316982
    Change-Id: I8521d59940fb2d1a5387e68f6ec9e7e55482c51c

--- modules/profile/manifests/wmcs/novaproxy.pp
+++ modules/profile/manifests/wmcs/novaproxy.pp
@@ -1,29 +1,31 @@
-class profile::wmcs::novaproxy(
-    Array[Stdlib::Fqdn]   $all_proxies                 = lookup('profile::wmcs::novaproxy::all_proxies',    {default_value => ['localhost']}),
-    Stdlib::Fqdn          $active_proxy                = lookup('profile::wmcs::novaproxy::active_proxy',   {default_value => 'localhost'}),
-    Optional[String]      $acme_certname               = lookup('profile::wmcs::novaproxy::acme_certname',  {default_value => undef}),
-    String                $block_ua_re                 = lookup('profile::wmcs::novaproxy::block_ua_re',    {default_value => ''}),
-    String                $block_ref_re                = lookup('profile::wmcs::novaproxy::block_ref_re',   {default_value => ''}),
-    Array[Stdlib::Fqdn]   $xff_fqdns                   = lookup('profile::wmcs::novaproxy::xff_fqdns',      {default_value => []}),
-    Array[Stdlib::IP::Address::V4] $banned_ips         = lookup('profile::wmcs::novaproxy::banned_ips',     {default_value => []}),
-    Boolean               $api_readonly                = lookup('profile::wmcs::novaproxy::api_readonly',   {default_value => false}),
-    Stdlib::IP::Address::V4::Nosubnet  $proxy_dns_ipv4 = lookup('profile::wmcs::novaproxy::proxy_dns_ipv4', {default_value => '198.51.100.1'}),
-    Hash[String, Dynamicproxy::Zone] $supported_zones  = lookup('profile::wmcs::novaproxy::supported_zones'),
-    Integer               $rate_limit_requests         = lookup('profile::wmcs::novaproxy::rate_limit_requests', {default_value => 100}),
-    Enum['http', 'https'] $keystone_api_protocol       = lookup('profile::openstack::base::keystone::auth_protocol'),
-    Stdlib::Port          $keystone_api_port           = lookup('profile::openstack::base::keystone::public_port'),
+class profile::wmcs::novaproxy (
+    Array[Stdlib::Fqdn]               $all_proxies              = lookup('profile::wmcs::novaproxy::all_proxies',    {default_value => ['localhost']}),
+    Stdlib::Fqdn                      $active_proxy             = lookup('profile::wmcs::novaproxy::active_proxy',   {default_value => 'localhost'}),
+    Optional[String]                  $acme_certname            = lookup('profile::wmcs::novaproxy::acme_certname',  {default_value => undef}),
+    String                            $block_ua_re              = lookup('profile::wmcs::novaproxy::block_ua_re',    {default_value => ''}),
+    String                            $block_ref_re             = lookup('profile::wmcs::novaproxy::block_ref_re',   {default_value => ''}),
+    Array[Stdlib::Fqdn]               $xff_fqdns                = lookup('profile::wmcs::novaproxy::xff_fqdns',      {default_value => []}),
+    Array[Stdlib::IP::Address::V4]    $banned_ips               = lookup('profile::wmcs::novaproxy::banned_ips',     {default_value => []}),
+    Boolean                           $api_readonly             = lookup('profile::wmcs::novaproxy::api_readonly',   {default_value => false}),
+    Stdlib::IP::Address::V4::Nosubnet $proxy_dns_ipv4 = lookup('profile::wmcs::novaproxy::proxy_dns_ipv4', {default_value => '198.51.100.1'}),
+    Hash[String, Dynamicproxy::Zone]  $supported_zones          = lookup('profile::wmcs::novaproxy::supported_zones'),
+    Integer                           $rate_limit_requests      = lookup('profile::wmcs::novaproxy::rate_limit_requests', {default_value => 100}),
+    Enum['http', 'https']             $keystone_api_protocol    = lookup('profile::openstack::base::keystone::auth_protocol'),
+    Stdlib::Port                      $keystone_api_port        = lookup('profile::openstack::base::keystone::public_port'),
     # I don't want to add per-deployment profiles, so this is duplicated instead of using profile::openstack::$DEPLOYMENT::keystone_api_fqdn
-    Stdlib::Fqdn          $keystone_api_fqdn           = lookup('profile::wmcs::novaproxy::keystone_api_fqdn'),
-    String                $dns_updater_username        = lookup('profile::wmcs::novaproxy::dns_updater_username'),
-    String                $dns_updater_project         = lookup('profile::wmcs::novaproxy::dns_updater_project'),
-    String                $dns_updater_password        = lookup('profile::wmcs::novaproxy::dns_updater_password'),
-    String                $token_validator_username    = lookup('profile::wmcs::novaproxy::token_validator_username'),
-    String                $token_validator_project     = lookup('profile::wmcs::novaproxy::token_validator_project'),
-    String                $token_validator_password    = lookup('profile::wmcs::novaproxy::token_validator_password'),
-    Stdlib::Host          $mariadb_host                = lookup('profile::wmcs::novaproxy::mariadb_host'),
-    String[1]             $mariadb_db                  = lookup('profile::wmcs::novaproxy::mariadb_db'),
-    String[1]             $mariadb_username            = lookup('profile::wmcs::novaproxy::mariadb_username'),
-    String[1]             $mariadb_password            = lookup('profile::wmcs::novaproxy::mariadb_password'),
+    Stdlib::Fqdn                      $keystone_api_fqdn        = lookup('profile::wmcs::novaproxy::keystone_api_fqdn'),
+    String                            $dns_updater_username     = lookup('profile::wmcs::novaproxy::dns_updater_username'),
+    String                            $dns_updater_project      = lookup('profile::wmcs::novaproxy::dns_updater_project'),
+    String                            $dns_updater_password     = lookup('profile::wmcs::novaproxy::dns_updater_password'),
+    String                            $token_validator_username = lookup('profile::wmcs::novaproxy::token_validator_username'),
+    String                            $token_validator_project  = lookup('profile::wmcs::novaproxy::token_validator_project'),
+    String                            $token_validator_password = lookup('profile::wmcs::novaproxy::token_validator_password'),
+    Stdlib::Host                      $mariadb_host             = lookup('profile::wmcs::novaproxy::mariadb_host'),
+    String[1]                         $mariadb_db               = lookup('profile::wmcs::novaproxy::mariadb_db'),
+    String[1]                         $mariadb_username         = lookup('profile::wmcs::novaproxy::mariadb_username'),
+    String[1]                         $mariadb_password         = lookup('profile::wmcs::novaproxy::mariadb_password'),
+    Array[Stdlib::IP::Address]        $keepalived_vips          = lookup('profile::wmcs::novaproxy::keepalived_vips',     {default_value => []}),
+    String[1]                         $keepalived_password      = lookup('profile::wmcs::novaproxy::keepalived_password', {default_value => 'notarealpassword'}),
 ) {
     $proxy_nodes = join($all_proxies, ' ')
     # Open up redis to all proxies!
@@ -124,5 +126,26 @@ class profile::wmcs::novaproxy(
     }
 
     class { 'prometheus::nginx_exporter': }
+
+    if !$keepalived_vips.empty() {
+        $is_primary = $::facts['hostname'] == $active_proxy
+        # Ensure the primary server (where we would prefer to get API writes)
+        # gets priority when it is online
+        $priority_modifier = $is_primary ? {
+            true    => 100,
+            default => 0,
+        }
+
+        class { 'keepalived':
+            auth_pass => $keepalived_password,
+            peers     => $all_proxies - $::facts['networking']['fqdn'],
+            vips      => $keepalived_vips,
+            priority  => fqdn_rand(100) + $priority_modifier,
+        }
+
+        ferm::rule { 'keepalived-vrrp':
+            rule   => "proto vrrp saddr ${ferm::join_hosts($all_proxies)} ACCEPT;",
+        }
+    }
 }
 
