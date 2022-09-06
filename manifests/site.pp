e5bf6fd411296c797f5881b0d345012b9d5ddd7f
Francesco Negri <fnegri@wikimedia.org>
2022-09-06 12:30:25 +0200

    Add cloudcephosd103[1-4] to the Ceph pool
    Bug: T314870
    Change-Id: Ibc07dd2d427214b41f60f1b76c431824da8de9ad

--- manifests/site.pp
+++ manifests/site.pp
@@ -1622,13 +1622,8 @@ node /^cloudcephmon100[1-3]\.eqiad\.wmnet$/ {
 }
 
 # ceph storage nodes
-node /^cloudcephosd10(0[1-9]|1[0-9]|2[0-9]|30)\.eqiad\.wmnet$/ {
+node /^cloudcephosd10(0[1-9]|1[0-9]|2[0-9]|3[0-4])\.eqiad\.wmnet$/ {
     role(wmcs::ceph::osd)
-}
-
-# ceph storage nodes
-node /^cloudcephosd103[1-4]\.eqiad\.wmnet$/ {
-    role(insetup)
 }
 
 # New systems placed into service by cloud team via T194186 and T249062
