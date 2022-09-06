1d625659bed5eacbc25080b7002817113ec934c2
Manuel Arostegui <marostegui@wikimedia.org>
2022-09-06 15:46:06 +0200

    mariadb: Productionize db1201
    Will be serving in s6
    
    Bug: T316342
    Change-Id: I427f8a04e03a191934b28282e2c2f9b3c6020f95

--- manifests/site.pp
+++ manifests/site.pp
@@ -640,7 +640,7 @@ node 'db2128.codfw.wmnet' {
 
 # s6 (frwiki, jawiki, ruwiki) core production dbs on eqiad
 # See also db1096, db1098 and db1113 below
-node /^db1(131|168|173|180|187)\.eqiad\.wmnet/ {
+node /^db1(131|168|173|180|187|201)\.eqiad\.wmnet/ {
     role(mariadb::core)
 }
 
@@ -1039,7 +1039,7 @@ node /^dumpsdata100[67]\.eqiad\.wmnet$/ {
 
 # MariaDB 10
 # new db servers T306848
-node /^db1(201|202|203).eqiad.wmnet$/ {
+node /^db1(202|203).eqiad.wmnet$/ {
     role(insetup)
 }
 
