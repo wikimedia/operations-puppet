e18e217121b472f0223632b4f41505e540817013
Andrew Bogott <abogott@wikimedia.org>
2022-09-13 17:38:29 -0500

    toolviews.py: run through black in advance of some changes
    Change-Id: If2bbb2f03b0475245c08978821daa531e8542836

--- modules/toolforge/files/toolviews.py
+++ modules/toolforge/files/toolviews.py
@@ -36,22 +36,22 @@ import pymysql
 import yaml
 
 
-logger = logging.getLogger('toolviews')
+logger = logging.getLogger("toolviews")
 
 
 class ToolViews(object):
     RE_LINE = re.compile(
-        r'(?P<vhost>[^ ]+) '
-        r'(?P<ipaddr>[^ ]+) '
-        r'(?P<ident>[^ ]+) '
-        r'(?P<userid>[^ ]+) '
-        r'\[(?P<datetime>[^\]]+) \+0000\] '
+        r"(?P<vhost>[^ ]+) "
+        r"(?P<ipaddr>[^ ]+) "
+        r"(?P<ident>[^ ]+) "
+        r"(?P<userid>[^ ]+) "
+        r"\[(?P<datetime>[^\]]+) \+0000\] "
         r'"(?P<verb>[^ ]+) /(?P<tool>[^ /?]+)(?P<path>[^ ]*) HTTP/[^"]+" '
-        r'(?P<status>\d+) '
-        r'(?P<bytes>\d+) '
+        r"(?P<status>\d+) "
+        r"(?P<bytes>\d+) "
         r'"(?P<referer>[^"]*)" '
         r'"(?P<ua>[^"]*)"'
-        r'(?P<extra>.*)'
+        r"(?P<extra>.*)"
     )
 
     UPSERT_SQL = (
@@ -66,11 +66,11 @@ class ToolViews(object):
         self.tools = self.get_tools()
 
     def get_tools(self):
-        dn = 'ou=servicegroups,{}'.format(self.config['ldap']['basedn'])
+        dn = "ou=servicegroups,{}".format(self.config["ldap"]["basedn"])
         conn = ldap3.Connection(
-            self.config['ldap']['servers'],
-            user=self.config['ldap']['user'],
-            password=self.config['ldap']['password'],
+            self.config["ldap"]["servers"],
+            user=self.config["ldap"]["user"],
+            password=self.config["ldap"]["password"],
             auto_bind=True,
             auto_range=True,
             read_only=True,
@@ -78,18 +78,15 @@ class ToolViews(object):
         try:
             groups = conn.extend.standard.paged_search(
                 dn,
-                '(&(objectclass=groupofnames)(cn=tools.*))',
-                attributes=['cn'],
+                "(&(objectclass=groupofnames)(cn=tools.*))",
+                attributes=["cn"],
                 time_limit=5,
                 paged_size=256,
                 generator=True,
             )
-            return [
-                group['attributes']['cn'][0].split('.')[1]
-                for group in groups
-            ]
+            return [group["attributes"]["cn"][0].split(".")[1] for group in groups]
         except Exception:
-            logger.exception('Exception getting LDAP data for %s', dn)
+            logger.exception("Exception getting LDAP data for %s", dn)
         return []
 
     @staticmethod
@@ -119,8 +116,7 @@ class ToolViews(object):
         Returns:
             ISO 8601 formatted date (YYYY-MM-DD)
         """
-        return datetime.datetime.strptime(
-            d, '%d/%b/%Y:%H:%M:%S').strftime('%Y-%m-%d')
+        return datetime.datetime.strptime(d, "%d/%b/%Y:%H:%M:%S").strftime("%Y-%m-%d")
 
     @staticmethod
     def parse_log(lines, logpat):
@@ -134,8 +130,8 @@ class ToolViews(object):
         """
         groups = (logpat.match(line) for line in lines)
         tuples = (g.groupdict() for g in groups if g)
-        log = ToolViews.field_map(tuples, 'datetime', ToolViews.format_date)
-        log = ToolViews.field_map(log, 'status', int)
+        log = ToolViews.field_map(tuples, "datetime", ToolViews.format_date)
+        log = ToolViews.field_map(log, "status", int)
         return log
 
     @staticmethod
@@ -147,10 +143,8 @@ class ToolViews(object):
     def run(self, files):
         # Count rows per tool per day
         stats = collections.defaultdict(lambda: collections.defaultdict(int))
-        for r in ToolViews.parse_log(
-            fileinput.input(files=files), self.RE_LINE
-        ):
-            if 200 <= r['status'] < 300:
+        for r in ToolViews.parse_log(fileinput.input(files=files), self.RE_LINE):
+            if 200 <= r["status"] < 300:
                 # Status codes of 200 to 299 are 'success' codes.
                 # We don't care about client or server errors or redirects
 
@@ -163,26 +157,26 @@ class ToolViews(object):
                     tool = r["vhost"].split(".")[0]
 
                 if tool not in self.tools:
-                    if tool not in ('.well-known', 'index.php', 'robots.txt'):
+                    if tool not in (".well-known", "index.php", "robots.txt"):
                         logger.info('Unknown tool "%s"', tool)
                     # fourohfour is the default route handler
-                    tool = 'fourohfour'
-                stats[tool][r['datetime']] += 1
+                    tool = "fourohfour"
+                stats[tool][r["datetime"]] += 1
 
         rows = []
         for tool, days in stats.items():
             if self.dry_run:
-                print('{}: {}'.format(tool, sum(days.values())))
+                print("{}: {}".format(tool, sum(days.values())))
             for day, hits in days.items():
                 rows.append((tool, day, hits))
 
         if not self.dry_run:
             dbh = pymysql.connect(
-                host=self.config['mysql_host'],
-                user=self.config['mysql_user'],
-                password=self.config['mysql_password'],
-                database=self.config['mysql_db'],
-                charset='utf8',
+                host=self.config["mysql_host"],
+                user=self.config["mysql_user"],
+                password=self.config["mysql_password"],
+                database=self.config["mysql_db"],
+                charset="utf8",
                 cursorclass=pymysql.cursors.DictCursor,
                 autocommit=False,
             )
@@ -195,7 +189,7 @@ class ToolViews(object):
                             cursor.executemany(self.UPSERT_SQL, chunk)
                             dbh.commit()
                         except pymysql.MySQLError:
-                            logger.exeception('Failed to insert rows')
+                            logger.exeception("Failed to insert rows")
                             dbh.rollback()
             finally:
                 dbh.close()
@@ -203,42 +197,52 @@ class ToolViews(object):
 
 def main():
     # T237080: Verify that the version of ldap3 is new enough
-    if StrictVersion(ldap3.__version__) < StrictVersion('1.2.2'):
+    if StrictVersion(ldap3.__version__) < StrictVersion("1.2.2"):
         raise AssertionError(
-            'toolviews needs ldap3>=1.2.2, found {}'.format(ldap3.__version__)
+            "toolviews needs ldap3>=1.2.2, found {}".format(ldap3.__version__)
         )
 
-    parser = argparse.ArgumentParser(
-        description='Load tool analytics into database')
+    parser = argparse.ArgumentParser(description="Load tool analytics into database")
     parser.add_argument(
-        '-v', '--verbose', action='count', default=0, dest='loglevel',
-        help='Increase logging verbosity')
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        dest="loglevel",
+        help="Increase logging verbosity",
+    )
     parser.add_argument(
-        '--config', default='/etc/toolviews.yaml',
-        help='Path to YAML config file')
+        "--config", default="/etc/toolviews.yaml", help="Path to YAML config file"
+    )
     parser.add_argument(
-        '--dry-run', action='store_true',
-        help='Parse logs but output to screen instead of database')
+        "--dry-run",
+        action="store_true",
+        help="Parse logs but output to screen instead of database",
+    )
     parser.add_argument(
-        'files', metavar='FILE', nargs='*',
-        help='Access logs to read. If empty, stdin is used')
+        "files",
+        metavar="FILE",
+        nargs="*",
+        help="Access logs to read. If empty, stdin is used",
+    )
     args = parser.parse_args()
 
     logging.basicConfig(
         level=max(logging.DEBUG, logging.WARNING - (10 * args.loglevel)),
-        format='%(asctime)s %(name)-12s %(levelname)-8s: %(message)s',
-        datefmt='%Y-%m-%dT%H:%M:%SZ')
+        format="%(asctime)s %(name)-12s %(levelname)-8s: %(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%SZ",
+    )
     logging.captureWarnings(True)
 
     with open(args.config) as f:
         config = yaml.safe_load(f)
 
-    with open(config.get('ldap_config', '/etc/ldap.yaml')) as f:
-        config['ldap'] = yaml.safe_load(f)
+    with open(config.get("ldap_config", "/etc/ldap.yaml")) as f:
+        config["ldap"] = yaml.safe_load(f)
 
     stats = ToolViews(config, args.dry_run)
     stats.run(args.files)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
