d541c5240e7fa102003b3f7444540166403d8c78
Antoine Musso <hashar@free.fr>
2022-09-13 19:12:50 +0200

    gerrit: ignore lint error in role
    The gerrit role includes passwords::gerrit which is neither a role nor a
    profile.
    
    Change-Id: I412b7b36b02118226c00fe661569e3b5d437a7b5

--- modules/role/manifests/gerrit.pp
+++ modules/role/manifests/gerrit.pp
@@ -8,7 +8,9 @@ class role::gerrit {
     include ::profile::base::production
     include ::profile::backup::host
     include ::profile::base::firewall
+    # lint:ignore:wmf_styleguide - It is neither a role nor a profile
     include ::passwords::gerrit
+    # lint:endignore
     include ::profile::gerrit
     include ::profile::gerrit::migration
     include ::profile::prometheus::apache_exporter
