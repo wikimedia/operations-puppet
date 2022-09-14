4e593f0e9749e7e841707eabd5281bce9b123b4d
Elukey <ltoscano@wikimedia.org>
2022-09-14 09:32:32 +0000

    Revert "wikilabels::web: use the master branch for git repos"
    This reverts commit 4a9a5af0c03c8840843bbff17763b1ef96e0fa1a.
    
    Reason for revert: Doesn't work as intended.
    
    Change-Id: I7b7e7b95e5f0fc2fe2583a9fe1826f5fe05e10a3

--- modules/wikilabels/manifests/web.pp
+++ modules/wikilabels/manifests/web.pp
@@ -3,7 +3,7 @@
 # Sets up a uwsgi based web server for Wikilabels running python3
 #
 class wikilabels::web (
-    $branch = 'master',
+    $branch = 'deploy',
 ) {
 
     $venv_path = '/srv/wikilabels/venv'
