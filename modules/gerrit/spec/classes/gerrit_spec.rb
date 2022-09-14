74514cb2a0d6879db57d4dbbe587a5ef50257e3c
John Bond <jbond@wikimedia.org>
2022-09-14 15:18:03 +0200

    gerrit: modernize spec
    Use newer ruby keys when definining params.
    
    Replace `should` by `is_expected.to`.
    
    Change-Id: Ie0b0ca96e2bcef0541e0a2da0bf46694309bb8a1

--- modules/gerrit/spec/classes/gerrit_spec.rb
+++ modules/gerrit/spec/classes/gerrit_spec.rb
@@ -7,16 +7,16 @@ describe 'gerrit' do
       let(:facts) { facts }
       let(:params) {
         {
-          :host          => 'gerrit.example.org',
-          :ipv4          => '192.0.2.42',
-          :ipv6          => '2001:db8::1',
-          :java_home     => '/path/to/java_home',
-          :ldap_config   => {
-            :'ro-server' => 'ldapro.example.org',
-            :'base-dn'   => 'dc=example,dc=org',
+          host:        'gerrit.example.org',
+          ipv4:        '192.0.2.42',
+          ipv6:        '2001:db8::1',
+          java_home:   '/path/to/java_home',
+          ldap_config: {
+            'ro-server': 'ldapro.example.org',
+            'base-dn':   'dc=example,dc=org',
           },
-          :scap_user     => 'gerrit-deployer',
-          :scap_key_name => 'gerrit-ssh-key',
+          scap_user:     'gerrit-deployer',
+          scap_key_name: 'gerrit-ssh-key',
         }
       }
       let(:pre_condition) do
@@ -25,7 +25,7 @@ describe 'gerrit' do
         Group{"gerrit-deployer":}
         '''
       end
-      it { should compile.with_all_deps }
+      it { is_expected.to compile.with_all_deps }
     end
   end
 end
