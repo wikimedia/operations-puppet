c2072fc72ede9bdcae72c477bd9c3033dc04dfdc
Daniel Zahn <dzahn@wikimedia.org>
2022-09-14 15:21:11 -0700

    phabricator: remove absented code for icinga phd superiving procs
    follow-up to Iab3aa58eb2287c60 after puppet ran on all phab
    hosts followed by alert* hosts.
    
    removing Icinga NRPE checks (exported resources)
    
    Bug: T315962
    Change-Id: I40efb4a01aaf882166f5a36a87c057abadba3e68

--- modules/profile/manifests/phabricator/monitoring.pp
+++ modules/profile/manifests/phabricator/monitoring.pp
@@ -10,14 +10,6 @@ class profile::phabricator::monitoring (
 
         $phab_contact_groups = 'admins,phabricator'
 
-        nrpe::monitor_service { 'check_phab_taskmaster':
-            ensure        => absent,
-            description   => 'PHD should be supervising processes',
-            nrpe_command  => '/usr/lib/nagios/plugins/check_procs -c 3:150 -u phd',
-            contact_group => $phab_contact_groups,
-            notes_url     => 'https://wikitech.wikimedia.org/wiki/Phabricator',
-        }
-
         nrpe::monitor_service { 'check_phab_phd':
             description   => 'PHD should be running',
             nrpe_command  => "/usr/lib/nagios/plugins/check_procs -c 1: --ereg-argument-array  'php ./phd-daemon' -u phd",
