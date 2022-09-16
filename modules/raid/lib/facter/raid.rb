2076cd2d03caa4c7b0444aeeb4271c8c4b1b2791
jbond <jbond@wikimedia.org>
2022-09-16 12:47:02 +0200

    raid: add Broadcom / LSI MegaRAID SAS-3 3324 [Intruder] (rev 01)
    Bug: T317924
    Change-Id: I3774e5a64b7d7fa2e086594881b77fed9ded5a1b

--- modules/raid/lib/facter/raid.rb
+++ modules/raid/lib/facter/raid.rb
@@ -10,7 +10,9 @@ Facter.add('raid_mgmt_tools') do
     '103c3239' => 'hpsa',     # Hewlett-Packard Company Smart Array Gen9 Controllers
     '1000005d' => 'megaraid', # LSI Logic / Symbios Logic MegaRAID SAS-3 3108, also shows up as
                               # Broadcom / LSI MegaRAID SAS-3 3108 [Invader]
+    '100000cf' => 'megaraid', # Broadcom / LSI MegaRAID SAS-3 3324 [Intruder] (rev 01)
     '10000016' => 'megaraid', # Broadcom / LSI MegaRAID Tri-Mode SAS3508
+
   }
   setcode do
     raids = []
