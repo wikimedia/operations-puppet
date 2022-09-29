3391edb8d1860a14c629770f6b580e6f60f28e4c
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-09-29 10:16:15 +0200

    keyholder: Switch shebang to /usr/bin/python3
    No need for env since we only ever want to use the distro-provided Python
    and not using env makes keyholder show up as the process name in lsof
    (which is what we parse to detect necessary service restarts after
    library updates), otherwise it simply shows as as python3.
    
    Also axe the full Apache license statement, since we now have a proper
    SPDX header.
    
    Change-Id: Ifde09fda747bf38aa328ebd9bffd2b56db7af53c

--- modules/keyholder/files/ssh-agent-proxy.py
+++ modules/keyholder/files/ssh-agent-proxy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # SPDX-License-Identifier: Apache-2.0
 # -*- coding: utf-8 -*-
 """
@@ -15,18 +15,6 @@
   Copyright 2015 Ori Livneh <ori@wikimedia.org>
   Copyright 2015 Tyler Cipriani <thcipriani@wikimedia.org>
   Copyright 2018 Faidon Liambotis <faidon@wikimedia.org>
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY CODE, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
 
 """
 import argparse
