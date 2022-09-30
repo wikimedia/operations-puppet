6819e4f42488b9f8daac910e65248b63722f90cb
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2022-09-30 07:09:05 +0200

    profile::mediawiki::php::absented_version: also remove systemd unit
    Change-Id: I6d4023e0dab6c4b4a5c2adf0d3d28f0b957d8329

--- modules/profile/manifests/mediawiki/php/absented_version.pp
+++ modules/profile/manifests/mediawiki/php/absented_version.pp
@@ -1,16 +1,20 @@
 # SPDX-License-Identifier: Apache-2.0
 # This define can be used to remove as mediawiki php installation from production.
 define profile::mediawiki::php::absented_version() {
-    $fpm_programname = php::fpm::programname($title)
-
     # Remove the check-restart timer
-    systemd::timer{ "${fpm_programname}_check_restart":
+    $fpm_programname = php::fpm::programname($title)
+    $restart = "${fpm_programname}_check_restart"
+    systemd::timer{ $restart:
         ensure          => 'absent',
         timer_intervals => [
             { 'start'    => 'OnCalendar',
               'interval' => '*-*-* 00:00:00',
             }
         ],
+    }
+    systemd::unit{ "${restart}.service":
+        ensure  => absent,
+        content => '',
     }
     # Remove all packages related to php$version-common
     # Also remove all their config files, which include /etc/php/$version
