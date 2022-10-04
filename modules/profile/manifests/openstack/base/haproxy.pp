9bb2fdb0551db5a54a2acad006c0038044513005
Andrew Bogott <abogott@wikimedia.org>
2022-10-04 15:03:18 -0500

    wmcs haproxy: prepare for IP and user-agent blocking
    Bug: T319313
    Change-Id: I5a8ce27198fa7232fa57d2730d86c2ea3f01b60f

--- modules/profile/manifests/openstack/base/haproxy.pp
+++ modules/profile/manifests/openstack/base/haproxy.pp
@@ -6,4 +6,20 @@ class profile::openstack::base::haproxy(
         logging  => $logging,
         template => 'profile/openstack/base/haproxy/haproxy.cfg.erb',
     }
+
+    file { '/etc/haproxy/ipblocklist.txt':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/openstack/haproxy/ipblocklist.txt',
+    }
+
+    file { '/etc/haproxy/agentblocklist.txt':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/openstack/haproxy/agentblocklist.txt',
+    }
 }
