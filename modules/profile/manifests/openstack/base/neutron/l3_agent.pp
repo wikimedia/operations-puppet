2ca29f5b11b60667c002f8048d6911ce8510886e
jbond <jbond@wikimedia.org>
2022-10-05 12:21:37 +0200

    P:openstack::base::neutron: dont use the legacy naming
    Use the shorter naming for vlans
    
    Hosts: cloudnet1005.eqiad.wmnet
    Hosts: cloudnet1003.eqiad.wmnet
    Hosts: P:openstack::eqiad1::neutron
    Bug: T319300
    Change-Id: I3d39e8a2a39dfa4b2fb707041b50fea6c0a02d52

--- modules/profile/manifests/openstack/base/neutron/l3_agent.pp
+++ modules/profile/manifests/openstack/base/neutron/l3_agent.pp
@@ -4,22 +4,25 @@ class profile::openstack::base::neutron::l3_agent(
     $base_interface = lookup('profile::openstack::base::neutron::base_interface'),
     $network_flat_interface_vlan_external = lookup('profile::openstack::base::neutron::network_flat_interface_vlan_external'),
     $network_flat_interface_vlan = lookup('profile::openstack::base::neutron::network_flat_interface_vlan'),
+    $legacy_vlan_naming = lookup('profile::openstack::base::neutron::legacy_vlan_naming'),
     ) {
 
     interface::tagged { "${base_interface}.${$network_flat_interface_vlan_external}":
-        base_interface => $base_interface,
-        vlan_id        => $network_flat_interface_vlan_external,
-        method         => 'manual',
-        up             => 'ip link set $IFACE up',
-        down           => 'ip link set $IFACE down',
+        base_interface     => $base_interface,
+        vlan_id            => $network_flat_interface_vlan_external,
+        method             => 'manual',
+        up                 => 'ip link set $IFACE up',
+        down               => 'ip link set $IFACE down',
+        legacy_vlan_naming => $legacy_vlan_naming,
     }
 
     interface::tagged { "${base_interface}.${$network_flat_interface_vlan}":
-        base_interface => $base_interface,
-        vlan_id        => $network_flat_interface_vlan,
-        method         => 'manual',
-        up             => 'ip link set $IFACE up',
-        down           => 'ip link set $IFACE down',
+        base_interface     => $base_interface,
+        vlan_id            => $network_flat_interface_vlan,
+        method             => 'manual',
+        up                 => 'ip link set $IFACE up',
+        down               => 'ip link set $IFACE down',
+        legacy_vlan_naming => $legacy_vlan_naming,
     }
 
     class {'::openstack::neutron::l3_agent':
