421f5130b5ccdfd68950edcdc07093613f97fef3
Andrew Bogott <abogott@wikimedia.org>
2022-10-05 15:58:53 -0500

    Openstack Designate: Expose the Designate public API
    Bug: T319312
    Change-Id: Ibaaf84641fd3ee75544efaecfadda567c421fa09

--- modules/profile/manifests/openstack/base/designate/firewall/api.pp
+++ modules/profile/manifests/openstack/base/designate/firewall/api.pp
@@ -2,21 +2,8 @@ class profile::openstack::base::designate::firewall::api(
     Array[Stdlib::Fqdn] $labweb_hosts = lookup('profile::openstack::base::labweb_hosts'),
     Array[Stdlib::Fqdn] $openstack_controllers = lookup('profile::openstack::base::openstack_controllers'),
 ) {
-    # Open designate API to WMCS web UIs and the commandline on control servers
     ferm::service { 'designate-tls-api':
-        proto  => 'tcp',
-        port   => '29001',
-        srange => "(@resolve((${labweb_hosts.join(' ')} ${openstack_controllers.join(' ')})))",
-    }
-
-    # Allow labs instances to hit the designate api.
-    # This is not as permissive as it looks; The wmfkeystoneauth
-    # plugin (via the password whitelist) only allows 'novaobserver'
-    # to authenticate from within labs, and the novaobserver is
-    # limited by the designate policy.json to read-only queries.
-    ferm::service { 'designate-tls-api-for-labs':
-        proto  => 'tcp',
-        port   => '29001',
-        srange => '$LABS_NETWORKS',
+        proto => 'tcp',
+        port  => '29001',
     }
 }
