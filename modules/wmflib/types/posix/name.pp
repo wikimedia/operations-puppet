9b0c301446bbd241a21966dd22abaa1f481d6af9
Ahmon Dancy <adancy@wikimedia.org>
2022-10-07 12:23:12 -0700

    Add type Wmflib::POSIX::Name
    Implemented `Wmflib::POSIX::Name` and `Wmflib::POSIX::Variables` types
    that seemed like a good general thing to have for cases where a resource
    accepts arbitrary environment variables and would like some
    assurances/safety over format.
    
    Change-Id: I73636db4bd95774c25fea180553ea04a84832997

---
+++ modules/wmflib/types/posix/name.pp
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: Apache-2.0
+# https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap03.html#tag_03_235
+type Wmflib::POSIX::Name = Pattern[/\A[a-zA-Z_]\w*\z/]
---
+++ modules/wmflib/types/posix/variables.pp
@@ -0,0 +1,2 @@
+# SPDX-License-Identifier: Apache-2.0
+type Wmflib::POSIX::Variables = Hash[Wmflib::POSIX::Name, String]
