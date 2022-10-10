428d3fe6b4f80c42facc5367e55c4f13c71679f9
Hoo man <hoo@online.de>
2022-10-10 14:14:25 +0000

    maintenance::wikidata: Update cron with lb and lb-pool
    This reverts commit 40bedcb0f64c00267f77387cd881c995e5fc7b7a,
    and thus reinstates commit 2b4befc3a47c813d89677c565aeb9670e17c2113.
    
    Reason for revert: Get this in again, once the maintenance
    script accepts the new parameters.
    
    Change-Id: Idc7f1ddfc63e805ab307b457fab602c64023793e
    Depends-On: I0df74fe4ba326839c0a2c010fdb8909475efa0bf

--- modules/profile/manifests/mediawiki/maintenance/wikidata.pp
+++ modules/profile/manifests/mediawiki/maintenance/wikidata.pp
@@ -1,4 +1,9 @@
-class profile::mediawiki::maintenance::wikidata {
+class profile::mediawiki::maintenance::wikidata(
+    Hash[Wmflib::Sites, Hash[
+        Profile::Lvs::Classes,
+        Profile::Lvs::Class_hosts
+    ]] $lb_by_class = lookup('profile::lvs::configuration::all_class_hosts'),
+    ) {
     require profile::mediawiki::common
 
     # Resubmit changes in wb_changes that are older than 6 hours
@@ -11,8 +16,19 @@ class profile::mediawiki::maintenance::wikidata {
     # We don't need to ensure present/absent as the wrapper will ensure nothing
     # is run unless we're in the master dc
     # Logs are saved to /var/log/mediawiki/mediawiki_job_wikidata-updateQueryServiceLag/syslog.log and properly rotated.
+    # When calculating maxlag, we want to only query WDQS servers that are currently pooled. See T238751
+    $service_name = 'wdqs'
+    $svc = wmflib::service::fetch(true)[$service_name]
+    $svc_lbl = "${service_name}_${svc['port']}"
+    # Needed to find the LVS servers we need to check.
+    $my_lvs_class = $svc['lvs']['class']
+    # Select only the hosts in our class
+    $lbs_my_class = $lb_by_class.values.filter |$val| { $my_lvs_class in $val}.map |$lbs| {$lbs[$my_lvs_class].values}.flatten.unique
+    $lb = $lbs_my_class.map |$host| { "--lb ${host}:9090"}.join(' ')
+
+    $additional_args = "--lb-pool ${svc_lbl} ${lb}"
     profile::mediawiki::periodic_job { 'wikidata-updateQueryServiceLag':
-        command  => '/usr/local/bin/mwscript extensions/Wikidata.org/maintenance/updateQueryServiceLag.php --wiki wikidatawiki --cluster wdqs --prometheus prometheus.svc.eqiad.wmnet',
+        command  => "/usr/local/bin/mwscript extensions/Wikidata.org/maintenance/updateQueryServiceLag.php --wiki wikidatawiki --cluster wdqs --prometheus prometheus.svc.eqiad.wmnet ${additional_args}",
         interval => '*-*-* *:*:00'
     }
 }
