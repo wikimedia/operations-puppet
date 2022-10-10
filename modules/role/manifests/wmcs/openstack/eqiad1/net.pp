cf246cad8333d78ed75c49a26ba3ea2c93602377
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-10-10 10:53:20 +0200

    cloudnet: Stop including profile::base::linux510
    profile::base::linux510 is only needed on Buster (it provides the 5.10
    kernel for Buster hosts), but cloudnet* are running Bullseye by now.
    
    Change-Id: Ic5dd4e23bff7ad3a48fbce02d54f2ed2fb70b076

--- modules/role/manifests/wmcs/openstack/eqiad1/net.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/net.pp
@@ -1,13 +1,12 @@
 class role::wmcs::openstack::eqiad1::net {
     system::role { $name: }
     # Do not add base firewall
-    include ::profile::base::production
-    include ::profile::openstack::eqiad1::clientpackages
-    include ::profile::openstack::eqiad1::observerenv
-    include ::profile::openstack::eqiad1::neutron::common
-    include ::profile::openstack::eqiad1::neutron::l3_agent
-    include ::profile::openstack::eqiad1::neutron::dhcp_agent
-    include ::profile::openstack::eqiad1::neutron::metadata_agent
-    include ::profile::wmcs::services::oidentd::proxy
-    include profile::base::linux510
+    include profile::base::production
+    include profile::openstack::eqiad1::clientpackages
+    include profile::openstack::eqiad1::observerenv
+    include profile::openstack::eqiad1::neutron::common
+    include profile::openstack::eqiad1::neutron::l3_agent
+    include profile::openstack::eqiad1::neutron::dhcp_agent
+    include profile::openstack::eqiad1::neutron::metadata_agent
+    include profile::wmcs::services::oidentd::proxy
 }
