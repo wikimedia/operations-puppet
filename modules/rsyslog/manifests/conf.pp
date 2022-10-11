1f23f5a76258265422475cc17df888f10137c320
Jesse Hathaway <jhathaway@wikimedia.org>
2022-10-11 14:51:37 -0500

    rsyslog::conf remove trailing newline logic
    This logic is currently broken, because the regexp always matches
    the first line of the content, regardless of whether it has a trailing
    newline. The `$` anchor matches the end of a *line* without the
    newline, so a `\n` is always inserted.
    
    Since rsyslogd doesn't mind if files don't have trailing newlines, just
    drop the logic rather than reworking it.
    
    Bug: T320569
    Hosts: C:rsyslog
    Change-Id: I631d13f9cb47a9c80050b0924db59d26cdab6c2c

--- modules/rsyslog/manifests/conf.pp
+++ modules/rsyslog/manifests/conf.pp
@@ -39,15 +39,9 @@ define rsyslog::conf (
     $basename = regsubst($title, '[\W_]', '-', 'G')
     $filename = sprintf('/etc/rsyslog.d/%02d-%s.conf', $priority, $basename)
 
-    # append a trailing newline if omitted
-    $content_formatted = $content ? {
-        undef   => undef,
-        default => regsubst($content, "\n?$", "\n")
-    }
-
     file { $filename:
         ensure  => $ensure,
-        content => $content_formatted,
+        content => $content,
         source  => $source,
         owner   => 'root',
         group   => 'root',
