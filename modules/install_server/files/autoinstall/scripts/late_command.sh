65cf2a5aa2163673f24c19a199b86f6aeab127ed
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-10-12 16:50:02 +0200

    Install 5.10 in late_setup.sh for next Gen PowerEdges
    Bug: T319067
    Change-Id: I4436cf869b82ef5c269b6b481bceaa69f1665963

--- modules/install_server/files/autoinstall/scripts/late_command.sh
+++ modules/install_server/files/autoinstall/scripts/late_command.sh
@@ -44,6 +44,14 @@ case $(hostname) in
     ;;
 esac
 
+# On Dell PowerEdge 450 on Buster we need 5.10 before rebooting after early install, T319067
+case $(hostname) in
+    cp4045)
+	apt-install linux-image-5.10-amd64
+    ;;
+esac
+
+
 # Temporarily pre-provision swift user at a fixed UID on new installs.
 # Once T123918 is resolved and swift is the same uid/gid everywhere, the
 # 'admin' puppet module can take over.
