0bd113cc1d8fc8d26e3a7374c60a9154e64ca072
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2022-10-25 13:35:46 +0200

    kubeadm: wmcs-k8s-node-upgrade.py: refresh licence
    Relicense this code to Apache 2.0.
    
    Bug: T308013
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: I706411e6a94082a8fb28b20c7ca94f10413b74c0

--- modules/kubeadm/files/wmcs-k8s-node-upgrade.py
+++ modules/kubeadm/files/wmcs-k8s-node-upgrade.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-# SPDX-License-Identifier: GPL-3.0
-
-# (C) 2020 by Arturo Borrero Gonzalez <aborrero@wikimedia.org>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
+# SPDX-License-Identifier: Apache-2.0
 
 # Read the docs here:
 # TODO: https://wikitech.wikimedia.org/wiki/Portal:Toolforge/Admin/Kubernetes/Upgrading_Kubernetes
