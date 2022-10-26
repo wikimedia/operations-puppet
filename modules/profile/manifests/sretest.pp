3cbef0189cf3a28ad494313d4d8eccfbabdff4e9
jbond <jbond@wikimedia.org>
2022-10-26 11:09:10 +0200

    P:sretest: create a test to see if the escap code actully makes through
    Change-Id: I9eb67f511db71b85d5e4c1c790b2761d1b8b6cb2

--- modules/profile/manifests/sretest.pp
+++ modules/profile/manifests/sretest.pp
@@ -27,4 +27,7 @@ class profile::sretest {
         'tag'     => 'foo::bar::merge',
     })
     wmflib::resource::import('file', undef, { tag => 'foo::bar::merge' }, true)
+    motd::message { 'netbox status':
+        message => wmflib::ansi::fg('yellow Status', 'yellow').wmflib::ansi::attr('bold'),
+    }
 }
