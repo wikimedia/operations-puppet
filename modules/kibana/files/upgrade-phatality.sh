948724777b528bfe10b1e7c64374c50d469dc7f8
Antoine Musso <hashar@free.fr>
2022-10-26 19:11:14 +0200

    opensearch: make upgrade-phatality.sh stricter
    Both versions of our script would continue installing Phatality even if
    the removal failed. Doing a deployment I have encountered:
    
     Unable to remove plugin because of error:
     "ENOTEMPTY: directory not empty,
      rmdir '/usr/share/opensearch-dashboards/plugins/phatality'"
    
    The script ignores the error and process with the install which fails
    due to the plugin already existing:
    
     Attempting to transfer from
       file:///srv/deployment/releng/phatality/deploy/phatality-2.2.0.zip
     Transferring 13336 bytes....................
     Transfer complete
     Retrieving metadata from plugin archive
     Extracting plugin archive
     Extraction complete
     Plugin phatality already exists, please remove before installing a new version
    
    Only attempt to remove the Phatality plugin when it is installed. This
    is done by looking it up in the output of `kibana-plugin install`.
    
    Instruct bash to abort on script errors (-e), unset variable (-u) and
    also when a command fails in a pipe chain (which is not used currently
    but might later on and is a good practice to make bash stricter).
    
    Also double quote a variable to please shellcheck.
    
    Bug: T304440
    Change-Id: I25a956c3165be8f3ae156f49d3ceae2fb178f892

--- modules/kibana/files/upgrade-phatality.sh
+++ modules/kibana/files/upgrade-phatality.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: Apache-2.0
+
+set -eu -o pipefail
+
 KIBANA_VERSION=$(jq -r ".version" /usr/share/kibana/package.json)
 PHATALITY_PKG="/srv/deployment/releng/phatality/deploy/phatality-$KIBANA_VERSION.zip"
-/usr/bin/sudo -u kibana /usr/share/kibana/bin/kibana-plugin remove phatality
-/usr/bin/sudo -u kibana /usr/share/kibana/bin/kibana-plugin install file://$PHATALITY_PKG
+
+mapfile -t plugins < <(/usr/bin/sudo -u kibana /usr/share/kibana/bin/kibana-plugin list phatality)
+
+for plugin_line in "${plugins[@]}"
+do
+    if [[ $plugin_line =~ ^phatality ]]; then
+        echo "Removing existing phatality plugin before installation"
+        /usr/bin/sudo -u kibana /usr/share/kibana/bin/kibana-plugin remove phatality
+    fi
+done
+
+/usr/bin/sudo -u kibana /usr/share/kibana/bin/kibana-plugin install "file://$PHATALITY_PKG"
--- modules/opensearch_dashboards/files/upgrade-phatality.sh
+++ modules/opensearch_dashboards/files/upgrade-phatality.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: Apache-2.0
+
+set -eu -o pipefail
+
 DASHBOARDS_VERSION=$(jq -r ".version" /usr/share/opensearch-dashboards/package.json)
 PHATALITY_PKG="/srv/deployment/releng/phatality/deploy/phatality-$DASHBOARDS_VERSION.zip"
-/usr/bin/sudo -u opensearch-dashboards /usr/share/opensearch-dashboards/bin/opensearch-dashboards-plugin remove phatality
-/usr/bin/sudo -u opensearch-dashboards /usr/share/opensearch-dashboards/bin/opensearch-dashboards-plugin install file://$PHATALITY_PKG
+
+mapfile -t plugins < <(/usr/bin/sudo -u opensearch-dashboards /usr/share/opensearch-dashboards/bin/opensearch-dashboards-plugin list phatality)
+
+for plugin_line in "${plugins[@]}"
+do
+    if [[ $plugin_line =~ ^phatality ]]; then
+        echo "Removing existing phatality plugin before installation"
+        /usr/bin/sudo -u opensearch-dashboards /usr/share/opensearch-dashboards/bin/opensearch-dashboards-plugin remove phatality
+    fi
+done
+
+/usr/bin/sudo -u opensearch-dashboards /usr/share/opensearch-dashboards/bin/opensearch-dashboards-plugin install "file://$PHATALITY_PKG"
