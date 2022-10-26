332103ea7903214a8f1f9c580b274fc9ebd33fe7
Ssingh <ssingh@wikimedia.org>
2022-10-26 23:30:59 +0000

    Revert "cp4048: update site.pp and related configs for cp role"
    This reverts commit be0c3b20aa7f3373c6a0c381d6be37ee52e50c7e.
    
    Reason for revert: host failing to boot up, this one is for later as well
    
    Change-Id: Ib2a817030efe76cf17f9f2b93250a939be1f6ed1

--- manifests/site.pp
+++ manifests/site.pp
@@ -464,7 +464,7 @@ node /^cp30(5[13579]|6[135])\.esams\.wmnet$/ {
 #
 
 # HAProxy test
-node /^cp40(4[56789])\.ulsfo\.wmnet$/ {
+node /^cp40(4[5679])\.ulsfo\.wmnet$/ {
     role(cache::upload_haproxy)
 }
 
