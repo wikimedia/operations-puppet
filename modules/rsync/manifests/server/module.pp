527bb1d22fa34cef903fe4afad7626247a804032
jbond <jbond@wikimedia.org>
2022-10-27 15:47:56 +0200

    R:rsync::manifests::server::module: add type validation
    Hosts: cumin:R:rsync::manifests::server::module
    Change-Id: I2ec8b52548b8f070874f2563364889cd75be57ee

--- modules/rsync/manifests/server/module.pp
+++ modules/rsync/manifests/server/module.pp
@@ -1,62 +1,80 @@
-# Definition: rsync::server::module
-#
-# sets up a rsync server
+# @summary sets up a rsync server
 #
 # Parameters:
-#   $path            - path to data
-#   $comment         - rsync comment
-#   $read_only       - yes||no, defaults to yes
-#   $write_only      - yes||no, defaults to no
-#   $list            - yes||no, defaults to yes
-#   $uid             - uid of rsync server, defaults to 0
-#   $gid             - gid of rsync server, defaults to 0
-#   $incoming_chmod  - incoming file mode, defaults to undef
-#   $outgoing_chmod  - outgoing file mode, defaults to undef
-#   $max_connections - maximum number of simultaneous connections allowed, defaults to 0
-#   $lock_file       - file used to support the max connections parameter, defaults to /var/run/rsyncd.lock
-#    only needed if max_connections > 0
-#   $secrets_file    - path to the file that contains the username:password pairs used for authenticating this module
-#   $auth_users      - list of usernames that will be allowed to connect to this module (must be undef or an array)
-#   $hosts_allow     - list of patterns allowed to connect to this module (man 5 rsyncd.conf for details, must be undef or an array)
-#   $hosts_deny      - list of patterns allowed to connect to this module (man 5 rsyncd.conf for details, must be undef or an array)
-#   $chroot          - chroot to the destination before starting the rsync.  enabled by default.
-#   $auto_ferm       - If enabled and if $hosts_allow is set, generate a ferm service which restricts access to the allowed hosts
-#   $auto_ferm_ipv6  - If auto_ferm is used and this option is enabled, ferm rules are also generated for ipv6
+# @param path
+#   path to data
+# @param ensure
+#   ensurable parameter
+# @param comment
+#   rsync comment
+# @param read_only
+#   yes||no, defaults to yes
+# @param write_only
+#   yes||no, defaults to no
+# @param list
+#   yes||no, defaults to yes
+# @param uid
+#   uid of rsync server, defaults to 0
+# @param gid
+#   gid of rsync server, defaults to 0
+# @param incoming_chmod
+#   incoming file mode, defaults to undef
+# @param outgoing_chmod
+#   outgoing file mode, defaults to undef
+# @param max_connections
+#   maximum number of simultaneous connections allowed, defaults to 0
+# @param lock_file
+#   file used to support the max connections parameter, defaults to
+#   /var/run/rsyncd.lock only needed if max_connections > 0
+# @param secrets_file
+#   path to the file that contains the username:password pairs used for
+#   authenticating this module
+# @param auth_users
+#   list of usernames that will be allowed to connect to this module (must be
+#   undef or an array)
+# @param hosts_allow
+#   list of patterns allowed to connect to this module (man 5 rsyncd.conf for
+#   details, must be undef or an array)
+# @param hosts_deny
+#   list of patterns allowed to connect to this module (man 5
+#   rsyncd.conf for details, must be undef or an array)
+# @param chroot
+#   chroot to the destination before starting the rsync.  enabled by default.
+# @param auto_ferm
+#   If enabled and if $hosts_allow is set, generate a ferm service which restricts
+#   access to the allowed hosts
+# @param auto_ferm_ipv6
+#   If auto_ferm is used and this option is enabled, ferm rules are also generated
+#   for ipv6
 #
-#   sets up an rsync server
-#
-# Requires:
-#   $path must be set
-#
-# Sample Usage:
-#   # setup default rsync repository
+# @example
 #   rsync::server::module { 'repo':
 #     path    => $base,
 #     require => File[$base],
 #   }
 #
 define rsync::server::module (
-  $path,
-  $ensure                                              = present,
-  $comment                                             = undef,
-  $read_only                                           = 'yes',
-  $write_only                                          = 'no',
-  $list                                                = 'yes',
-  $uid                                                 = '0',
-  $gid                                                 = '0',
-  $incoming_chmod                                      = undef,
-  $outgoing_chmod                                      = undef,
-  $max_connections                                     = '0',
-  $lock_file                                           = '/var/run/rsyncd.lock',
-  $chroot                                              = true,
-  $auto_ferm                                           = false,
-  $auto_ferm_ipv6                                      = false,
-  $secrets_file                                        = undef,
-  $auth_users                                          = undef,
-  Optional[Variant[String,Array[String]]] $hosts_allow = undef,
-  Optional[Variant[String,Array[String]]] $hosts_deny  = undef,
+  Stdlib::Unixpath                        $path,
+  Wmflib::Ensure                          $ensure          = present,
+  Stdlib::Yes_no                          $read_only       = 'yes',
+  Stdlib::Yes_no                          $write_only      = 'no',
+  Stdlib::Yes_no                          $list            = 'yes',
+  String[1]                               $uid             = '0',
+  String[1]                               $gid             = '0',
+  Variant[Integer, String[1]]             $max_connections = '0',
+  Stdlib::Unixpath                        $lock_file       = '/var/run/rsyncd.lock',
+  Boolean                                 $chroot          = true,
+  Boolean                                 $auto_ferm       = false,
+  Boolean                                 $auto_ferm_ipv6  = false,
+  Optional[Stdlib::Unixpath]              $secrets_file    = undef,
+  Optional[String[1]]                     $comment         = undef,
+  Optional[String[4]]                     $incoming_chmod  = undef,
+  Optional[String[4]]                     $outgoing_chmod  = undef,
+  Optional[Array[String]]                 $auth_users      = undef,
+  Optional[Variant[String,Array[String]]] $hosts_allow     = undef,
+  Optional[Variant[String,Array[String]]] $hosts_deny      = undef,
 ){
-  include ::rsync::server
+  include rsync::server
 
   if $hosts_allow {
     $hosts_allow_as_array = $hosts_allow ? {
