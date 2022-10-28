1c4c5aa36fa512edbb7cf048f096be069db0ec26
Matthias Mullie <git@mullie.eu>
2022-10-28 12:12:41 +0200

    Schedule image suggestions for ca, no, fi & huwiki
    Meanwhile also lowering the min-edit-count threshold
    for ptwiki from 500 to 300.
    
    Bug: T300064
    Bug: T321715
    Change-Id: If85edf8881d59a9322cd9dd2af071deaebd39f7b

--- modules/profile/manifests/mediawiki/maintenance/image_suggestions.pp
+++ modules/profile/manifests/mediawiki/maintenance/image_suggestions.pp
@@ -1,7 +1,17 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::mediawiki::maintenance::image_suggestions {
+  profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_CA':
+    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=cawiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --quiet',
+    interval => 'Wed 0:00',
+  }
+
+  profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_NO':
+    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=nowiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --quiet',
+    interval => 'Wed 1:00',
+  }
+
   profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_PT':
-    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=ptwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --quiet',
+    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=ptwiki --min-edit-count=300 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --quiet',
     interval => 'Wed 2:00',
   }
 
@@ -13,5 +23,15 @@ class profile::mediawiki::maintenance::image_suggestions {
   profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_ID':
     command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=idwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --quiet',
     interval => 'Wed 4:00',
+  }
+
+  profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_FI':
+    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=fiwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --quiet',
+    interval => 'Wed 5:00',
+  }
+
+  profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_HU':
+    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=huwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --quiet',
+    interval => 'Wed 6:00',
   }
 }
