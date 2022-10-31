bf6b2a7346ede561c08da3471048148875ceff90
Andrew Bogott <abogott@wikimedia.org>
2022-10-31 13:47:08 -0500

    Add openstack::clientpackages::vms::yoga classes
    Bug: T305828
    Change-Id: I287f06371781f9c2489fe365a8c448788ec60ca9

---
+++ modules/openstack/manifests/clientpackages/yoga/bullseye.pp
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: Apache-2.0
+
+class openstack::clientpackages::yoga::bullseye(
+) {
+    $py3packages = [
+        'python3-novaclient',
+        'python3-glanceclient',
+        'python3-keystoneauth1',
+        'python3-keystoneclient',
+        'python3-openstackclient',
+        'python3-troveclient',
+        'python3-designateclient',
+        'python3-neutronclient',
+        'python3-osc-placement',
+    ]
+
+    package{ $py3packages:
+        ensure => 'present',
+    }
+
+    $otherpackages = [
+        'ebtables',
+    ]
+
+    package { $otherpackages:
+        ensure => 'present',
+    }
+
+    file { '/usr/lib/python3/dist-packages/mwopenstackclients.py':
+        ensure => 'present',
+        source => 'puppet:///modules/openstack/clientpackages/mwopenstackclients3.py',
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
+}
---
+++ modules/openstack/manifests/clientpackages/yoga/buster.pp
@@ -0,0 +1,58 @@
+# SPDX-License-Identifier: Apache-2.0
+
+class openstack::clientpackages::yoga::buster(
+) {
+    $py2packages = [
+        'python-novaclient',
+        'python-glanceclient',
+        'python-keystoneclient',
+        'python-openstackclient',
+        'python-designateclient',
+        'python-neutronclient',
+    ]
+
+    package{ $py2packages:
+        ensure => 'present',
+    }
+
+    $py3packages = [
+        'python3-novaclient',
+        'python3-glanceclient',
+        'python3-keystoneauth1',
+        'python3-keystoneclient',
+        'python3-openstackclient',
+        'python3-troveclient',
+        'python3-designateclient',
+        'python3-neutronclient',
+        'python3-osc-placement',
+    ]
+
+    package{ $py3packages:
+        ensure => 'present',
+    }
+
+    $otherpackages = [
+        'ebtables',
+        'python-netaddr',
+    ]
+
+    package { $otherpackages:
+        ensure => 'present',
+    }
+
+    # Wrapper python class to easily query openstack clients
+    file { '/usr/lib/python2.7/dist-packages/mwopenstackclients.py':
+        ensure => 'present',
+        source => 'puppet:///modules/openstack/clientpackages/mwopenstackclients.py',
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
+    file { '/usr/lib/python3/dist-packages/mwopenstackclients.py':
+        ensure => 'present',
+        source => 'puppet:///modules/openstack/clientpackages/mwopenstackclients3.py',
+        mode   => '0755',
+        owner  => 'root',
+        group  => 'root',
+    }
+}
