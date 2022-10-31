2269e1a07999ba28aa413e162467a6f36489e7fb
Brett Cornwall <bcornwall@wikimedia.org>
2022-10-31 14:57:17 -0700

    prometheus: Rename ats_ metrics to trafficserver_
    The metric names weren't consistent with existing ones. All the previous
    metrics are named using the prefix trafficserver so the new metrics
    should follow suit.
    
    Bug: T292815
    Change-Id: I06388a6a8731a884a703f56802cbb650b1b2db2b

---
+++ modules/prometheus/files/usr/local/bin/prometheus-trafficserver-config.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Usage: prometheus-trafficserver-config [OUTFILE]
+#
+# Parse and export select ATS configurations for Prometheus to consume. Useful
+# for enabling monitoring to compare current values with maximums.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+OUTFILE="${1:-/var/lib/prometheus/node.d/trafficserver_config.prom}"
+
+max_connections_in="$(
+    traffic_ctl config get proxy.config.net.max_connections_in \
+        | awk '{print $2}'
+)"
+
+max_requests_in="$(
+    traffic_ctl config get proxy.config.net.max_requests_in \
+        | awk '{print $2}'
+)"
+
+cat <<EOF > "${OUTFILE}.$$"
+# HELP trafficserver_proxy_config_net_max_connections_in Total number of client requests that Traffic Server can handle simultaneously
+# TYPE trafficserver_proxy_config_net_max_connections_in gauge
+trafficserver_proxy_config_net_max_connections_in $max_connections_in
+# HELP trafficserver_proxy_config_net_max_requests_in Total number of concurrent requests or active client connections that the Traffic Server can handle simultaneously
+# TYPE trafficserver_proxy_config_net_max_requests_in gauge
+trafficserver_proxy_config_net_max_requests_in $max_requests_in
+EOF
+mv "${OUTFILE}.$$" "${OUTFILE}"
---
+++ modules/prometheus/manifests/node_trafficserver_config.pp
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# = Class: prometheus::node_trafficserver_config
+#
+# Periodically parse the ATS configuration file text and export e.g. the max
+# number of connnections set via a node_exporter textfile collector.
+#
+# = Parameters
+#
+# [*records config*]
+#   Path to read in ATS' records configuration
+#
+# [*outfile*]
+#   Path to write the finished textfile-exporter-format file.
+
+class prometheus::node_trafficserver_config (
+    Wmflib::Ensure $ensure = 'present',
+    Pattern[/\.prom$/] $outfile = '/var/lib/prometheus/node.d/trafficserver_config.prom',
+) {
+    $exec = '/usr/local/bin/prometheus-trafficserver-config'
+    file { $exec:
+        ensure => $ensure,
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/prometheus/usr/local/bin/prometheus-trafficserver-config.sh',
+    }
+
+    # Collect every 10 minutes
+    systemd::timer::job { 'prometheus-trafficserver-config':
+        ensure          => $ensure,
+        description     => 'Export select ATS configuration paramaters to node_exporter',
+        command         => $exec,
+        user            => 'root',
+        logging_enabled => false,
+        require         => [File[$exec]],
+        interval        => {
+            'start'    => 'OnUnitInactiveSec',
+            'interval' => '10m',
+        },
+    }
+}
