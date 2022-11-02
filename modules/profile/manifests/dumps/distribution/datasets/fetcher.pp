bcce5325dbfa663bfb4cd270540e677c7b4d497a
Daniel Zahn <dzahn@wikimedia.org>
2022-11-02 11:00:03 -0700

    dumps/distribution: move hardcoded host names to parameters
    The goal is to get the hard-coded host names out of the puppet
    code and into parameters and Hiera.
    
    Add a parameter for the stat dump host and one for the phab dump host.
    
    Also partially follow-up to T322221 though unrelated to the bug there.
    
    And in preparation for the upcoming Phabricator server switch
    where we will have to update the phab1001 host name soon.
    
    Bug: T280597
    Bug: T322221
    Change-Id: I570768bb03df32f954aced32943a8af123e0b441

--- modules/profile/manifests/dumps/distribution/datasets/fetcher.pp
+++ modules/profile/manifests/dumps/distribution/datasets/fetcher.pp
@@ -3,6 +3,8 @@ class profile::dumps::distribution::datasets::fetcher(
     Stdlib::Unixpath $xmldumpsdir = lookup('profile::dumps::distribution::xmldumpspublicdir'),
     Stdlib::Unixpath $miscdatasetsdir = lookup('profile::dumps::distribution::miscdumpsdir'),
     Hash $rsyncer_settings = lookup('profile::dumps::distribution::rsync_config'),
+    Stdlib::Host $stat_dump_host = lookup('profile::dumps::distribution::stat_dump_host'),
+    Stdlib::Host $phab_dump_host = lookup('profile::dumps::distribution::phab_dump_host'),
 ) {
 
     $user = $rsyncer_settings['dumps_user']
@@ -22,7 +24,7 @@ class profile::dumps::distribution::datasets::fetcher(
     }
 
     class {'dumps::web::fetches::stat_dumps':
-        src             => 'stat1007.eqiad.wmnet::srv/dumps',
+        src             => "${stat_dump_host}::srv/dumps",
         miscdatasetsdir => $miscdatasetsdir,
         user            => $user,
     }
@@ -33,7 +35,7 @@ class profile::dumps::distribution::datasets::fetcher(
     }
 
     class {'dumps::web::fetches::phab':
-        src             => 'phab1001.eqiad.wmnet::srv-dumps',
+        src             => "${phab_dump_host}::srv-dumps",
         miscdatasetsdir => $miscdatasetsdir,
         user            => root,
     }
