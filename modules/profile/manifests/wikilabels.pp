684f545fde79106bcebfdf561debe2d1504bfa8a
Tobias Klausmann <tklausmann@wikimedia.org>
2022-11-03 12:34:20 +0100

    wikilabels: Cleanup old DB proxy information
    Bug: T307389
    Change-Id: Iaeb0504350abd5bab35e94789b3a2e3ee62258b1

--- modules/profile/manifests/wikilabels.pp
+++ modules/profile/manifests/wikilabels.pp
@@ -1,12 +1,7 @@
 class profile::wikilabels (
     $branch = undefined,
 ){
-
     class { 'wikilabels::web':
         branch => $branch,
-    }
-
-    class { '::wikilabels::db_proxy':
-        server => 'wikilabels.db.svc.eqiad.wmflabs',
     }
 }
