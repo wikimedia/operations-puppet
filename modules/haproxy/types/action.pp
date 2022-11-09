6d9216c5e04abd4d541331903726f51bac12f527
Chris Danis <cdanis@wikimedia.org>
2022-11-09 16:28:36 -0500

    haproxy: concurrency tracking as discussed
    Bug: T306580
    Change-Id: I72192f5c7958c94aed8ef02935060300a82616bd

--- modules/haproxy/types/action.pp
+++ modules/haproxy/types/action.pp
@@ -3,4 +3,5 @@ type Haproxy::Action = Struct[{
     'context'   => Enum['tcp-request connection', 'http-request', 'http-response'],
     'verb'      => String[1],
     'condition' => Optional[String],  # e.g. "if cache_miss or cache_pass"
+    'comment'   => Optional[String],
 }]
