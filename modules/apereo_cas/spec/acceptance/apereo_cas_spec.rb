6eceb764ab8557ee8426f9d0b9f42fd4729700fb
John Bond <jbond@wikimedia.org>
2022-11-14 20:45:28 +0000

    apereo_cas: update beaker tests
    Change-Id: I83338d51f9791e644914967b67969e3329a36f27

--- modules/apereo_cas/spec/acceptance/apereo_cas_spec.rb
+++ modules/apereo_cas/spec/acceptance/apereo_cas_spec.rb
@@ -6,20 +6,25 @@ require 'spec_helper_acceptance'
 describe 'apereo_cas class' do
   describe 'running puppet code' do
     it 'work with no errors' do
-      pp = "class {'apereo_cas': keystore_source => 'puppet:///modules/apereo_cas/thekeystore' }"
-      apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_failures: true)
+      pp = <<~PP
+      class {'apereo_cas':
+        idp_nodes => ['idp.example.org'],
+        keystore_source => 'puppet:///modules/apereo_cas/thekeystore',
+      }
+      PP
+      apply_manifest(pp, catch_failures: false)
+      apply_manifest(pp, catch_failures: false)
       expect(apply_manifest(pp, catch_failures: true).exit_code).to eq 0
     end
-    describe service('cas') do
+    describe service('tomcat9') do
       it { is_expected.to be_enabled }
       it { is_expected.to be_running.under('systemd') }
     end
-    describe port(8443) do
-      let(:pre_command) { 'sleep 30' }
+    describe port(8080) do
+      # let(:pre_command) { 'sleep 30' }
       it { is_expected.to be_listening }
     end
-    describe command('curl -k https://localhost:8443/cas/login') do
+    describe command('curl -km1 http://localhost:8080/cas/login') do
       its(:stdout) { is_expected.to match(/Login - CAS &#8211; Central Authentication Service/) }
     end
   end
--- modules/apereo_cas/spec/spec_helper_acceptance.rb
+++ modules/apereo_cas/spec/spec_helper_acceptance.rb
@@ -7,28 +7,14 @@ require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 require 'beaker-rspec'
 
-modules = [
-  'git',
-  'wmflib',
-  'stdlib',
-  'systemd'
-]
-def install_modules(host, modules)
-  module_root = File.expand_path(File.join(__dir__, '..'))
-  install_dev_puppet_module_on(
-    host, source: module_root, module_name: File.basename(module_root))
-  modules.each do |m|
-    source = File.expand_path(File.join(module_root, '..', m))
-    install_dev_puppet_module_on(host, source: source, module_name: m)
-  end
-end
+def install_modules(host, modules, vendor_modules); end
+module_root = File.expand_path(File.join(__dir__, '..'))
 hosts.each do |host|
-  # install puppet from debian repos
-  step "install puppet on #{host}"
-  host.install_package('puppet')
-  step "Enable IPv6 on #{host}"
-  on(host, 'sysctl net.ipv6.conf.all.disable_ipv6=0')
-  install_modules(host, modules)
+  on(host, 'git -C /etc/puppet/code/environments/production fetch')
+  on(host, 'git -C /etc/puppet/code/environments/production reset --hard origin/production')
+  install_dev_puppet_module_on(
+    host, source: module_root, module_name: File.basename(module_root)
+  )
 end
 RSpec.configure do |c|
   c.formatter = :documentation
