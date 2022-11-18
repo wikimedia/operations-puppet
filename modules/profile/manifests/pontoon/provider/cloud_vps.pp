766aa5ecc093b065961a3b438ccd8c948074037b
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2022-11-18 08:43:36 +0100

    pontoon: don't double-define ferm rules for metricsinfra prometheus
    Partial revert of I5b6f858f35, see discussion there too
    
    Change-Id: I29b76f22d61d792f12bf9c7af017baa9464c5671

--- modules/profile/manifests/pontoon/provider/cloud_vps.pp
+++ modules/profile/manifests/pontoon/provider/cloud_vps.pp
@@ -1,11 +1,4 @@
-# @summary Cloud VPS eqiad1 specific tweaks to the base Pontoon setup
+# @summary Cloud VPS specific tweaks to the base Pontoon setup
 # SPDX-License-Identifier: Apache-2.0
-class profile::pontoon::provider::cloud_vps (
-  Array[Stdlib::Fqdn] $metricsinfra_prometheus_nodes = lookup('metricsinfra_prometheus_nodes', {default_value => []}),
-) {
-  if !empty($metricsinfra_prometheus_nodes) {
-    ferm::rule { 'metricsinfra-prometheus-all':
-      rule => "saddr @resolve((${metricsinfra_prometheus_nodes.join(' ')})) ACCEPT;"
-    }
-  }
+class profile::pontoon::provider::cloud_vps () {
 }
