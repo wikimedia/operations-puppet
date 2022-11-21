d895ffdacce616e303a02b94e9c2c9f3cc247da8
cgoubert <cgoubert@wikimedia.org>
2022-11-21 12:46:50 +0100

    P:mediawiki::maintenance::wikidata labs exception
    There are no wdqs and no LVS on labs, skip this setup in labs because it
    breaks the maintenance server in the beta cluster.
    
    Hosts: mwmaint1002.eqiad.wmnet
    Change-Id: I2ca76a23eec86a8feb2c5c1c6b3906a48a766e23

--- modules/profile/manifests/mediawiki/maintenance/wikidata.pp
+++ modules/profile/manifests/mediawiki/maintenance/wikidata.pp
@@ -7,22 +7,24 @@ class profile::mediawiki::maintenance::wikidata() {
         interval => '*-*-* *:39:00',
     }
 
-    # Update the cached query service maxlag value every minute
-    # We don't need to ensure present/absent as the wrapper will ensure nothing
-    # is run unless we're in the master dc
-    # Logs are saved to /var/log/mediawiki/mediawiki_job_wikidata-updateQueryServiceLag/syslog.log and properly rotated.
-    # When calculating maxlag, we want to only query WDQS servers that are currently pooled. See T238751
-    $service_name = 'wdqs'
-    $svc = wmflib::service::fetch(true)[$service_name]
-    $svc_lbl = "${service_name}_${svc['port']}"
-    # Needed to find the LVS servers we need to check.
-    $my_lvs_class = $svc['lvs']['class']
-    # Select only the hosts in our class
-    $lb = $profile::lvs::configuration::lvs_classes.filter |$h, $val| { $my_lvs_class == $val}.map |$host, $_| { "--lb ${host}:9090"}.join(' ')
+    if $::realm != 'labs' {
+        # Update the cached query service maxlag value every minute
+        # We don't need to ensure present/absent as the wrapper will ensure nothing
+        # is run unless we're in the master dc
+        # Logs are saved to /var/log/mediawiki/mediawiki_job_wikidata-updateQueryServiceLag/syslog.log and properly rotated.
+        # When calculating maxlag, we want to only query WDQS servers that are currently pooled. See T238751
+        $service_name = 'wdqs'
+        $svc = wmflib::service::fetch(true)[$service_name]
+        $svc_lbl = "${service_name}_${svc['port']}"
+        # Needed to find the LVS servers we need to check.
+        $my_lvs_class = $svc['lvs']['class']
+        # Select only the hosts in our class
+        $lb = $profile::lvs::configuration::lvs_classes.filter |$h, $val| { $my_lvs_class == $val}.map |$host, $_| { "--lb ${host}:9090"}.join(' ')
 
-    $additional_args = "--lb-pool ${svc_lbl} ${lb}"
-    profile::mediawiki::periodic_job { 'wikidata-updateQueryServiceLag':
-        command  => "/usr/local/bin/mwscript extensions/Wikidata.org/maintenance/updateQueryServiceLag.php --wiki wikidatawiki --cluster wdqs --prometheus prometheus.svc.eqiad.wmnet ${additional_args}",
-        interval => '*-*-* *:*:00'
+        $additional_args = "--lb-pool ${svc_lbl} ${lb}"
+        profile::mediawiki::periodic_job { 'wikidata-updateQueryServiceLag':
+            command  => "/usr/local/bin/mwscript extensions/Wikidata.org/maintenance/updateQueryServiceLag.php --wiki wikidatawiki --cluster wdqs --prometheus prometheus.svc.eqiad.wmnet ${additional_args}",
+            interval => '*-*-* *:*:00'
+        }
     }
 }
