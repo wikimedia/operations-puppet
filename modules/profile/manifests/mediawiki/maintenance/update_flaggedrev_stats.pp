2e5cb49055b1401c623d20fe9af2359f37746b8c
Amir Sarabadani <ladsgroup@gmail.com>
2022-11-22 18:43:16 +0100

    mediawiki: Reduce the frequency of flaggedrevs updates
    Now flaggedrevs_statistics has millions of rows for even really small
    wikis due to overly aggressive updates adding lots of rows every two
    hours. Reducing this to once a day reduces its growth by an order of
    magnitude.
    
    Bug: T323495
    Change-Id: I589bca670b85b1e2d00e1e1abf760291381bedfa

--- modules/profile/manifests/mediawiki/maintenance/update_flaggedrev_stats.pp
+++ modules/profile/manifests/mediawiki/maintenance/update_flaggedrev_stats.pp
@@ -2,6 +2,6 @@ class profile::mediawiki::maintenance::update_flaggedrev_stats {
 
     profile::mediawiki::periodic_job { 'update_flaggedrev_stats':
         command  => '/usr/local/bin/mwscriptwikiset extensions/FlaggedRevs/maintenance/updateStats.php flaggedrevs.dblist',
-        interval => '0/2:00'
+        interval => '00:08'
     }
 }
