fda9167faea9262ab04a2bea6517f0bf6b38cfb8
John Bond <jbond@wikimedia.org>
2022-11-23 22:33:12 +0100

    install_server: fix config for ms-be dynamic partition
    Change-Id: I668a6a597d6b8645e263480390822673842e7a3f

--- modules/install_server/files/autoinstall/scripts/partman_early_command.sh
+++ modules/install_server/files/autoinstall/scripts/partman_early_command.sh
@@ -5,45 +5,26 @@ configure_swift_disks() {
   devices=""
   for disk in /sys/block/*/queue/rotational
   do
-    grep -q 0 "${disk}" && devices="${devices} $(echo "${disk}" | cut -d/ -f3 -)"
+    if grep -q 0 "${disk}"
+    then
+      device="$(printf "%s" "${disk}" | cut -d/ -f4 -)"
+      expr "${device}" : "sd.$" && devices="${devices## } /dev/${device}"
+    fi
   done
-  devices_hash_sep=$(echo "${devices}" | tr ' ' '#')
+  root_parts=$(printf "%s1#%s1" "${devices% *}" "${devices#* }")
+  swap_parts=$(printf "%s" "${root_parts}" | tr '1' '2')
 cat > /tmp/dynamic_disc.cfg <<EOF
-d-i partman-auto/method     string  raid
-d-i partman-md/device_remove_md boolean true
-d-i partman-lvm/device_remove_lvm   boolean true
-d-i partman-auto/disk   string ${devices}
 d-i grub-installer/bootdev  string  ${devices}
-# this workarounds LP #1012629 / Debian #666974
-# it makes grub-installer to jump to step 2, where it uses bootdev
-d-i grub-installer/only_debian  boolean false
-# Define physical partitions
-d-i partman-auto/expert_recipe  string  \
-        multiraid ::    \
-            60000   8000    60000   raid        \
-                \$primary{ } method{ raid }  \
-            .                   \
-            1000    1000    1000    raid        \
-                \$primary{ } method{ raid }  \
-            .                   \
-            200000  500 240000  xfs     \
-                \$primary{ } method{ format }    \
-                format{ } use_filesystem{ } \
-                filesystem{ xfs }       \
-            . \
-            30000   10000   -1  xfs     \
-                \$primary{ } method{ format }    \
-                format{ } use_filesystem{ } \
-                filesystem{ xfs }       \
+d-i partman-auto/disk   string ${devices}
 # Parameters are:
-# <raidtype> <devcount> <sparecount> <fstype> <mountpoint> \
+# <raidtype> <devcount> <sparecount> <fstype> <mountpoint> \\
 #   <devices> <sparedevices>
-d-i partman-auto-raid/recipe    string      \
-        1   2   0   ext4    /   \
-            ${devices_hash_sep}     \
-        .                   \
-        1   2   0   swap    -   \
-            ${devices_hash_sep}     \
+d-i partman-auto-raid/recipe    string      \\
+        1   2   0   ext4    /   \\
+            ${root_parts}     \\
+        .                   \\
+        1   2   0   swap    -   \\
+            ${swap_parts}     \\
         .
 EOF
 debconf-set-selections /tmp/dynamic_disc.cfg
