fb1805309b815d410728ca2dfc9655b66b4dd84a
John Bond <jbond@wikimedia.org>
2022-11-28 18:15:16 +0100

    wmflib: update xfs partitions to 4/5 after conversion to GPT
    Change-Id: I0b9f5454cace87480dcfeacd945448fbac2666d2

--- modules/wmflib/lib/facter/swift_disks.rb
+++ modules/wmflib/lib/facter/swift_disks.rb
@@ -5,12 +5,12 @@ Facter.add('swift_disks') do
   setcode do
     swift_disks = { }
     devices = Dir["/dev/disk/by-path/*"].map {|f| f.split('/')[-1]  }
-    swift_disks[:accounts] = devices.select {|d| d.end_with?('-part6') }.sort
-    swift_disks[:container] = devices.select {|d| d.end_with?('-part7') }.sort
+    swift_disks[:accounts] = devices.select {|d| d.end_with?('-part4') }.sort
+    swift_disks[:container] = devices.select {|d| d.end_with?('-part5') }.sort
     ssds = swift_disks[:accounts].map { |d| d[0...-6] }
     # disks may not be paritioned so we just get the route disk for this
     # This may be confusing need to check
-    swift_disks[:objects] = devices.reject { |d| d.start_with?(*ssds) || d =~ (/-part[1-7]$/) }.sort
+    swift_disks[:objects] = devices.reject { |d| d.start_with?(*ssds) || d =~ (/-part[1-9]$/) }.sort
     swift_disks
   end
 end
