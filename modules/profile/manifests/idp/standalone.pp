dd23db58dbbfe8693e6a0b728302a27797b1e7c3
John Bond <jbond@wikimedia.org>
2022-11-29 19:06:14 +0100

    P:idp::standalone: fix minor formating issues
    Change-Id: Id74728bfed66ccd99c0b91870e2b3fdf645cb9dc

--- modules/profile/manifests/idp/standalone.pp
+++ modules/profile/manifests/idp/standalone.pp
@@ -8,8 +8,8 @@ class profile::idp::standalone {
 
   # configure database
   include profile::mariadb::packages_wmf
-  class {'mariadb::service': }
-  class {'mariadb::config':
+  class { 'mariadb::service': }
+  class { 'mariadb::config':
     basedir => '/usr',
     config  => 'role/mariadb/mysqld_config/misc.my.cnf.erb',
     datadir => '/srv/sqldata',
@@ -33,14 +33,14 @@ class profile::idp::standalone {
 
   # BUG: need to use dirname() vs dirname
   # https://github.com/rodjek/puppet-lint/issues/937
-  file {$wsgi_file.dirname():
+  file { $wsgi_file.dirname():
     ensure => directory,
   }
-  file {$wsgi_file:
+  file { $wsgi_file:
     ensure  => file,
     content => $simple_flask_debug_app,
   }
-  uwsgi::app{'idp-test':
+  uwsgi::app { 'idp-test':
     settings => {
       uwsgi => {
         'plugins'     => 'python3',
@@ -48,13 +48,13 @@ class profile::idp::standalone {
         'http-socket' => '127.0.0.1:8081',
         'wsgi-file'   => $wsgi_file,
         'die-on-term' => true,
-      }
-    }
+      },
+    },
   }
 
-  class {'httpd': modules => ['proxy_http', 'proxy']}
+  class { 'httpd': modules => ['proxy_http', 'proxy'] }
   include profile::idp::client::httpd
-  ferm::service {'http-idp-test-login':
+  ferm::service { 'http-idp-test-login':
     proto => 'tcp',
     port  => 80,
   }
