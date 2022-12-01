f2adc88193ee71f108dc3e5b2b0ae7afab1040c4
John Bond <jbond@wikimedia.org>
2022-12-01 18:22:06 +0100

    apereo_cas: add OidcRegisteredService service support
    Bug: T311999
    Hosts: auto
    Change-Id: I0554467b16abc4a3549149946d0b2c9e40494def

--- modules/apereo_cas/manifests/service.pp
+++ modules/apereo_cas/manifests/service.pp
@@ -7,6 +7,8 @@
 # @param access_strategy the access strategy to use
 # @param required_groups a list of required ldap groups for the services
 # @param properties a list of addtional properties for the services
+# @param client_id the client_id used for OIDC
+# @param client_secret the client_secret used for OIDC
 define apereo_cas::service (
     Integer                              $id,
     String                               $service_id,
@@ -15,7 +17,12 @@ define apereo_cas::service (
     Apereo_cas::Service::Access_strategy $access_strategy = 'DefaultRegisteredServiceAccessStrategy',
     Array[String]                        $required_groups = [],
     Hash                                 $properties      = {},
+    Optional[String[1]]                  $client_id       = undef,
+    Optional[String[1]]                  $client_secret   = undef,
 ) {
+    if $service_class == 'OidcRegisteredService' and (!$client_id or !$client_secret) {
+        fail('$client_id and $client_secret required when using OidcRegisteredService')
+    }
     include apereo_cas
     $ldap_root = "${apereo_cas::ldap_group_cn},${apereo_cas::ldap_base_dn}"
     if $required_groups.empty() {
@@ -40,7 +47,9 @@ define apereo_cas::service (
         'attributeReleasePolicy' => { '@class' => "org.apereo.cas.services.${release_policy}" },
         'id'                     => $id,
         'accessStrategy'         => $_access_strategy,
-    }
+        'clientId'               => $client_id,
+        'clientSecret'           => $client_secret,
+    }.filter |$x| { $x[1] =~ NotUndef }
     $data = $properties.empty ? {
         true    => $base_data,
         default => $base_data + { 'properties' => $properties },
