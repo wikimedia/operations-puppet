eac387ff38cb4b290079c3397528eb17449c2e16
John Bond <jbond@wikimedia.org>
2022-12-02 14:10:25 +0100

    apereo_cas::services: drop mfa-u2f support
    mfa-u2f is no longer used so drop it
    
    Also fix up documentation and add fix minor formatting errors
    
    Bug: T311999
    Bug: T280691
    Hosts: auto
    Change-Id: Ie8eff727e25cad8c0d52d8a56648a0563cbef8d9

--- modules/apereo_cas/manifests/service.pp
+++ modules/apereo_cas/manifests/service.pp
@@ -1,60 +1,52 @@
 # SPDX-License-Identifier: Apache-2.0
+# @summary define a apereo_cas services
+# @param id the numerical id
+# @param service_id the id of the services i.e. the url pattern
+# @param service_class The services class to use
+# @param release_policy the release policy to use
+# @param access_strategy the access strategy to use
+# @param required_groups a list of required ldap groups for the services
+# @param properties a list of addtional properties for the services
 define apereo_cas::service (
-  Integer                              $id,
-  String                               $service_id,
-  Apereo_cas::Service::Class           $service_class   = 'RegexRegisteredService',
-  Apereo_cas::Service::Release_policy  $release_policy  = 'ReturnAllAttributeReleasePolicy',
-  Apereo_cas::Service::Access_strategy $access_strategy = 'DefaultRegisteredServiceAccessStrategy',
-  Boolean                              $require_u2f     = false,
-  Array[String]                        $required_groups = [],
-  Hash                                 $properties      = {},
+    Integer                              $id,
+    String                               $service_id,
+    Apereo_cas::Service::Class           $service_class   = 'RegexRegisteredService',
+    Apereo_cas::Service::Release_policy  $release_policy  = 'ReturnAllAttributeReleasePolicy',
+    Apereo_cas::Service::Access_strategy $access_strategy = 'DefaultRegisteredServiceAccessStrategy',
+    Array[String]                        $required_groups = [],
+    Hash                                 $properties      = {},
 ) {
-  include apereo_cas
-  $ldap_root = "${apereo_cas::ldap_group_cn},${apereo_cas::ldap_base_dn}"
-  if $required_groups.empty() {
-    $_access_strategy = {'@class' => "org.apereo.cas.services.${access_strategy}"}
-  } else {
-    $ldap_groups = $required_groups.map |$group| { "cn=${group},${ldap_root}" }
-    $_access_strategy = {
-      '@class'             => "org.apereo.cas.services.${access_strategy}",
-      'requiredAttributes' => {
-        '@class'   => 'java.util.HashMap',
-        'memberOf' => [
-          'java.util.HashSet',
-          $ldap_groups,
-        ]
-      }
+    include apereo_cas
+    $ldap_root = "${apereo_cas::ldap_group_cn},${apereo_cas::ldap_base_dn}"
+    if $required_groups.empty() {
+        $_access_strategy = { '@class' => "org.apereo.cas.services.${access_strategy}" }
+    } else {
+        $ldap_groups = $required_groups.map |$group| { "cn=${group},${ldap_root}" }
+        $_access_strategy = {
+            '@class'             => "org.apereo.cas.services.${access_strategy}",
+            'requiredAttributes' => {
+                '@class'   => 'java.util.HashMap',
+                'memberOf' => [
+                    'java.util.HashSet',
+                    $ldap_groups,
+                ],
+            },
+        }
     }
-  }
-  # We could make this a bit more flexible with failureMode
-  # but for now we will just block set CLOSED
-  # https://apereo.github.io/cas/6.1.x/mfa/Configuring-Multifactor-Authentication.html#failure-modes
-  $multifactor_policy = $require_u2f ? {
-      false   => {},
-      default => {
-          'multifactorPolicy' => {
-              '@class'       => 'org.apereo.cas.services.DefaultRegisteredServiceMultifactorPolicy',
-              'failureMode'  => 'CLOSED',
-              'multifactorAuthenticationProviders' => [ 'java.util.LinkedHashSet', [ 'mfa-u2f' ]],
-          }
-      }
-  }
-
-  $base_data = {
-    '@class'                 => "org.apereo.cas.services.${service_class}",
-    'name'                   => $title,
-    'serviceId'              => $service_id,
-    'attributeReleasePolicy' => {'@class' => "org.apereo.cas.services.${release_policy}"},
-    'id'                     => $id,
-    'accessStrategy'         => $_access_strategy,
-  } + $multifactor_policy
-
-  $data = $properties.empty ? {
-    true    => $base_data,
-    default => $base_data + {'properties' => $properties},
-  }
-  file {"${apereo_cas::services_dir}/${title}-${id}.json":
-    ensure  => file,
-    content => $data.to_json()
-  }
+    $base_data = {
+        '@class'                 => "org.apereo.cas.services.${service_class}",
+        'name'                   => $title,
+        'serviceId'              => $service_id,
+        'attributeReleasePolicy' => { '@class' => "org.apereo.cas.services.${release_policy}" },
+        'id'                     => $id,
+        'accessStrategy'         => $_access_strategy,
+    }
+    $data = $properties.empty ? {
+        true    => $base_data,
+        default => $base_data + { 'properties' => $properties },
+    }
+    file { "${apereo_cas::services_dir}/${title}-${id}.json":
+        ensure  => file,
+        content => $data.to_json(),
+    }
 }
