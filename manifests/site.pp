a7ab0d02262209fcfbc7ee1e0ac3c296741d4c7f
Manuel Arostegui <marostegui@wikimedia.org>
2022-12-02 08:47:47 +0100

    site.pp: db1132 is no longer special
    We have more 10.6 hosts.
    
    Change-Id: I1408a0d8279f6f7feb0f839e72ccf3b82a52fc64

--- manifests/site.pp
+++ manifests/site.pp
@@ -504,7 +504,6 @@ node /^datahubsearch100[1-3]\.eqiad\.wmnet$/ {
 
 # s1 (enwiki) core production dbs on eqiad
 # See also db1099 and db1105 below
-# db1132 is a testing mariadb 10.6 host T303395
 node /^db1(107|118|119|128|132|134|135|163|169|184|186|196|206)\.eqiad\.wmnet/ {
     role(mariadb::core)
 }
