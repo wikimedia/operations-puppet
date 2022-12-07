51e3d58e69d4160f2331f7b2a5b372476dba8bfe
Daniel Zahn <dzahn@wikimedia.org>
2022-12-06 16:10:33 -0800

    phabricator: rm code from before system user was created with systemd
    Bug: T280597
    Change-Id: I6dc257fe479c843079bec4e16d567732039af5c5

--- modules/phabricator/manifests/phd.pp
+++ modules/phabricator/manifests/phd.pp
@@ -9,7 +9,6 @@ class phabricator::phd (
     Stdlib::Unixpath $phd_home    = '/var/run/phd',
     Integer $phd_uid              = 920,
     Stdlib::Unixpath $basedir     = '/',
-    Boolean $use_systemd_sysuser  = false,
 ) {
 
     # PHD user needs perms to drop root perms on start
@@ -18,7 +17,7 @@ class phabricator::phd (
         recurse => true,
     }
 
-    # Managing repo's as the PHD user
+    # Managing repos as the PHD user
     file { "${basedir}/phabricator/scripts/repository/":
         owner   => $phd_user,
         recurse => true,
@@ -36,28 +35,11 @@ class phabricator::phd (
         group  => $phd_user,
     }
 
-    # TODO: remove if/else and parameter after migration to new servers
-    if $use_systemd_sysuser {
-        systemd::sysuser { $phd_user:
-            ensure      => present,
-            id          => "${phd_uid}:${phd_uid}",
-            description => 'Phabricator daemon user',
-            home_dir    => $phd_home,
-        }
-
-    } else {
-
-        group { $phd_user:
-            ensure => present,
-            system => true,
-        }
-
-        user { $phd_user:
-        gid    => $phd_user,
-        shell  => '/bin/false',
-        home   => $phd_home,
-        system => true,
-        }
+    systemd::sysuser { $phd_user:
+        ensure      => present,
+        id          => "${phd_uid}:${phd_uid}",
+        description => 'Phabricator daemon user',
+        home_dir    => $phd_home,
     }
 
     logrotate::conf { 'phd':
