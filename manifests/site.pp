3ae09ca1617bf7d19b0e3bea73b4f70fcfc2bfed
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-12-09 12:57:06 +0100

    Make ganeti5007 a Ganeti node
    Bug: T324610
    Change-Id: Ifc64c199af10b57bb34c0519aef58b2121c24274

--- manifests/site.pp
+++ manifests/site.pp
@@ -1308,12 +1308,8 @@ node /^ganeti400[568]\.ulsfo\.wmnet$/ {
     role(ganeti)
 }
 
-node /^ganeti500[3456]\.eqsin\.wmnet$/ {
+node /^ganeti500[34567]\.eqsin\.wmnet$/ {
     role(ganeti)
-}
-
-node /^ganeti500[7]\.eqsin\.wmnet$/ {
-    role(insetup::infrastructure_foundations)
 }
 
 node /^ganeti600[1234]\.drmrs\.wmnet$/ {
