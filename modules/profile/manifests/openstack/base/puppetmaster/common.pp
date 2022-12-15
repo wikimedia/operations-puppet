6d4fe685e5f840987fdf2a036cb9959e96b24556
Andrew Bogott <abogott@wikimedia.org>
2022-12-15 12:03:55 -0600

    cloud-vps puppet: allow multiple users to access our puppet git checkout
    Bug: T325280
    Change-Id: Ic40288536498a91f45cffc59f439aac2cbc53da4

--- modules/profile/manifests/openstack/base/puppetmaster/common.pp
+++ modules/profile/manifests/openstack/base/puppetmaster/common.pp
@@ -11,4 +11,23 @@ class profile::openstack::base::puppetmaster::common () {
         port   => '8141',
         srange => '$LABS_NETWORKS',
     }
+
+    # Git defaults to preventing multiple users from messing with the
+    # a directory, but for us that's a feature more than a security risk.
+    #
+    # T325128, T325280
+    git::systemconfig { 'allow multiple local git users in labs/private':
+        settings => {
+            'safe' => {
+                'directory' => '/var/lib/git/labs/private/',
+            }
+        }
+    }
+    git::systemconfig { 'allow multiple local git users in operations/puppet':
+        settings => {
+            'safe' => {
+                'directory' => '/var/lib/git/operations/puppet/',
+            }
+        }
+    }
 }
