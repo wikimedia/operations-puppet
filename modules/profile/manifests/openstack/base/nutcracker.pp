bb60b66ac1ffa09b6c234c04ce48afec24d971a0
Andrew Bogott <abogott@wikimedia.org>
2022-12-15 15:54:56 -0600

    profile::openstack::base::nutcracker: merge in profile::mediawiki::nutcracker
    profile::mediawiki::nutcracker is unused elsewhere, so merge in its
    functionality here so that profile can be removed.
    
    Bug: T325244
    Change-Id: Id04db392ae1db6dfeef8e2ce66d8305074e6126e

--- modules/profile/manifests/openstack/base/nutcracker.pp
+++ modules/profile/manifests/openstack/base/nutcracker.pp
@@ -10,17 +10,93 @@ class profile::openstack::base::nutcracker(
     $labweb_ips = $labweb_hosts.map |$host| { ipresolve($host, 4) }
     $memcached_servers = $labweb_ips.map |$ip| { "${ip}:11000:1" }
 
-    class {'::profile::mediawiki::nutcracker':
-        memcached_servers => $memcached_servers,
-        redis_shards      => $redis_shards,
-        datacenters       => [],
+    file { '/var/run/nutcracker':
+        ensure => 'directory',
+        owner  => 'nutcracker',
+        group  => 'nutcracker',
     }
 
-    # Memcached is now getting installed by the cloudweb_mcrouter class
-    # class { '::memcached':
-    #     size => $memcached_size,
-    # }
-    # class { '::profile::prometheus::memcached_exporter': }
+    $redis_servers = $redis_shards['sessions']
+    include ::passwords::redis
+    include ::profile::prometheus::nutcracker_exporter
+
+    $redis_eqiad_pool = {
+        auto_eject_hosts     => true,
+        distribution         => 'ketama',
+        redis                => true,
+        redis_auth           => $passwords::redis::main_password,
+        hash                 => 'md5',
+        listen               => '/var/run/nutcracker/redis_eqiad.sock 0666',
+        server_connections   => 1,
+        server_failure_limit => 3,
+        server_retry_timeout => 30000,  # milliseconds
+        timeout              => 1000,   # milliseconds
+        server_map           => $redis_servers['eqiad'],
+    }
+
+    if $memcached_servers == [] {
+        $nutcracker_pools = {
+            'redis_eqiad' => $redis_eqiad_pool,
+        }
+    } else {
+        $nutcracker_pools = {
+            'memcached'     => {
+                auto_eject_hosts     => true,
+                distribution         => 'ketama',
+                hash                 => 'md5',
+                listen               => '127.0.0.1:11212',
+                preconnect           => true,
+                server_connections   => 1,
+                server_failure_limit => 3,
+                server_retry_timeout => 30000,  # milliseconds
+                timeout              => 250,    # milliseconds
+                servers              => $memcached_servers,
+            },
+
+            'mc-unix'       => {
+                auto_eject_hosts     => true,
+                distribution         => 'ketama',
+                hash                 => 'md5',
+                listen               => '/var/run/nutcracker/nutcracker.sock 0666',
+                preconnect           => true,
+                server_connections   => 1,
+                server_failure_limit => 3,
+                server_retry_timeout => 30000,  # milliseconds
+                timeout              => 250,    # milliseconds
+                servers              => $memcached_servers,
+            },
+
+            'redis_eqiad'   =>  $redis_eqiad_pool,
+        }
+    }
+
+    # Ship a tmpfiles.d configuration to create /run/nutcracker
+    systemd::tmpfile { 'nutcracker':
+        content => 'd /run/nutcracker 0755 nutcracker nutcracker - -'
+    }
+
+    class { '::nutcracker':
+        mbuf_size => '64k',
+        pools     => $nutcracker_pools,
+    }
+
+    systemd::unit{ 'nutcracker':
+        content  => "[Service]\nCPUAccounting=yes\n",
+        override => true,
+    }
+
+    # monitor memcached if present, redis otherwise.
+    if $memcached_servers != [] {
+        class { '::nutcracker::monitoring':
+            port => 11212
+        }
+    }
+    else {
+        class { '::nutcracker::monitoring':
+            port   => 0,
+            socket => "/var/run/nutcracker/redis_${::site}.sock",
+        }
+    }
 
     $labweb_ips_ferm = inline_template("(@resolve((<%= @labweb_hosts.join(' ') %>)) @resolve((<%= @labweb_hosts.join(' ') %>), AAAA))")
     ferm::service { 'horizon_memcached':
@@ -29,10 +105,18 @@ class profile::openstack::base::nutcracker(
         srange => $labweb_ips_ferm,
     }
 
-    # Why doesn't profile::mediawiki::nutcracker handle this?
-    file { '/var/run/nutcracker':
-        ensure => 'directory',
-        owner  => 'nutcracker',
-        group  => 'nutcracker',
+
+    ferm::rule { 'skip_nutcracker_conntrack_out':
+        desc  => 'Skip outgoing connection tracking for Nutcracker',
+        table => 'raw',
+        chain => 'OUTPUT',
+        rule  => 'proto tcp sport (6378:6382 11212) NOTRACK;',
+    }
+
+    ferm::rule { 'skip_nutcracker_conntrack_in':
+        desc  => 'Skip incoming connection tracking for Nutcracker',
+        table => 'raw',
+        chain => 'PREROUTING',
+        rule  => 'proto tcp dport (6378:6382 11212) NOTRACK;',
     }
 }
