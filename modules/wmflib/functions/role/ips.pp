19fc545703ded9e1a36ea4ce4328ba8f988eb600
John Bond <jbond@wikimedia.org>
2022-12-16 13:14:25 +0100

    wmflib: function to get the ips for all hosts in a specific resource
    This is similar to the {role,resource,class}::hosts function except it
    returns a list of ip addresses instead of a list of hostnames
    
    Change-Id: I036b4e0ac63e607305c7dbd4cf6e49861db123d1

---
+++ modules/wmflib/functions/role/ips.pp
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: Apache-2.0
+# @summary function to return a list of ips running a specific role
+#  This function relies on data being present in puppetdb.  This means that new nodes will
+#  be returned after their first successful puppet run using the specified role.  It also means
+#  that nodes will be removed from the results once they have been purged from puppetdb.  This currently
+#  happens when a server has failed to run puppet for 14 days
+# @param role the role to search for
+# @param optional list of sites if present filter to output based on the sites passed
+function wmflib::role::ips (
+    Pattern[/\A\w+(::\w+)*\z/]                   $role,
+    Variant[Wmflib::Sites, Array[Wmflib::Sites]] $location = [],
+) >> Array[Stdlib::Host] {
+
+    $_role = $role.capitalize.stdlib::start_with('Role::') ? {
+        true    => $role,
+        default => "Role::${role}",
+    }
+    wmflib::class::ips($_role, $location)
+}
