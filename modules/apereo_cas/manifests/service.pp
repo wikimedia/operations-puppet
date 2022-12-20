920c281ccb1dabaf1167a2d7cd2e1d05f854f023
John Bond <jbond@wikimedia.org>
2022-12-20 16:29:51 +0100

    idp01: fix allowed_delage logic
    Hosts: auto
    Change-Id: I017446a1123375c6ca718a33b6dd026bae5abfdc

--- modules/apereo_cas/manifests/service.pp
+++ modules/apereo_cas/manifests/service.pp
@@ -7,16 +7,18 @@
 # @param access_strategy the access strategy to use
 # @param required_groups a list of required ldap groups for the services
 # @param properties a list of addtional properties for the services
+# @param allowed_delegate add an allowed delegated authentication provider
 # @param client_secret the client_secret used for OIDC
 define apereo_cas::service (
     Integer                              $id,
     String                               $service_id,
-    Apereo_cas::Service::Class           $service_class   = 'RegexRegisteredService',
-    Apereo_cas::Service::Release_policy  $release_policy  = 'ReturnAllAttributeReleasePolicy',
-    Apereo_cas::Service::Access_strategy $access_strategy = 'DefaultRegisteredServiceAccessStrategy',
-    Array[String]                        $required_groups = [],
-    Hash                                 $properties      = {},
-    Optional[String[1]]                  $client_secret   = undef,
+    Apereo_cas::Service::Class           $service_class    = 'RegexRegisteredService',
+    Apereo_cas::Service::Release_policy  $release_policy   = 'ReturnAllAttributeReleasePolicy',
+    Apereo_cas::Service::Access_strategy $access_strategy  = 'DefaultRegisteredServiceAccessStrategy',
+    Array[String]                        $required_groups  = [],
+    Hash                                 $properties       = {},
+    Optional[String[1]]                  $allowed_delegate = undef,
+    Optional[String[1]]                  $client_secret    = undef,
 ) {
     if $service_class == 'OidcRegisteredService' and !$client_secret {
         fail('$client_secret required when using OidcRegisteredService')
@@ -26,6 +28,15 @@ define apereo_cas::service (
         default                 => undef,
     }
     include apereo_cas
+    $delegate = $allowed_delegate ? {
+        undef   => {},
+        default => {
+            'delegatedAuthenticationPolicy' => {
+                '@class'           => 'org.apereo.cas.services.DefaultRegisteredServiceDelegatedAuthenticationPolicy',
+                'allowedProviders' => [ 'java.util.ArrayList', [ $allowed_delegate ]],
+            }
+        }
+    }
     $ldap_root = "${apereo_cas::ldap_group_cn},${apereo_cas::ldap_base_dn}"
     if $required_groups.empty() {
         $_access_strategy = { '@class' => "org.apereo.cas.services.${access_strategy}" }
@@ -48,7 +59,7 @@ define apereo_cas::service (
         'serviceId'              => $service_id,
         'attributeReleasePolicy' => { '@class' => "org.apereo.cas.services.${release_policy}" },
         'id'                     => $id,
-        'accessStrategy'         => $_access_strategy,
+        'accessStrategy'         => $_access_strategy + $delegate,
         'clientId'               => $client_id,
         'clientSecret'           => $client_secret,
     }.filter |$x| { $x[1] =~ NotUndef }
