6c2796bc7c7d8100829dd8923bd3ab2c5205380c
Andrew Bogott <abogott@wikimedia.org>
2023-01-03 12:10:43 -0600

    Add manifests for OpenStack Neutron version 'zed'
    Bug: T323086
    Change-Id: I70ae44b2f96cbce65919484c9613a4d6f6e793f3

---
+++ modules/openstack/manifests/neutron/common/zed.pp
@@ -0,0 +1,51 @@
+# SPDX-License-Identifier: Apache-2.0
+
+class openstack::neutron::common::zed(
+    Array[Stdlib::Fqdn] $openstack_controllers,
+    Array[Stdlib::Fqdn] $rabbitmq_nodes,
+    Stdlib::Fqdn $keystone_fqdn,
+    $db_pass,
+    $db_user,
+    $db_host,
+    $region,
+    $dhcp_domain,
+    $ldap_user_pass,
+    $rabbit_user,
+    $rabbit_pass,
+    $log_agent_heartbeats,
+    $agent_down_time,
+    Stdlib::Port $bind_port,
+    ) {
+
+    class { "openstack::neutron::common::zed::${::lsbdistcodename}": }
+
+    # Subtemplates of neutron.conf are going to want to know what
+    #  version this is
+    $version = inline_template("<%= @title.split(':')[-1] -%>")
+    $keystone_auth_username = 'novaadmin'
+    $keystone_auth_project = 'admin'
+    file { '/etc/neutron/neutron.conf':
+            owner     => 'neutron',
+            group     => 'neutron',
+            mode      => '0660',
+            show_diff => false,
+            content   => template('openstack/zed/neutron/neutron.conf.erb'),
+            require   => Package['neutron-common'];
+    }
+
+    file { '/etc/neutron/policy.yaml':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            source  => 'puppet:///modules/openstack/zed/neutron/policy.yaml',
+            require => Package['neutron-common'];
+    }
+
+    file { '/etc/neutron/plugins/ml2/ml2_conf.ini':
+        owner   => 'neutron',
+        group   => 'neutron',
+        mode    => '0744',
+        content => template('openstack/zed/neutron/plugins/ml2/ml2_conf.ini.erb'),
+        require => Package['neutron-common'];
+    }
+}
