3167750f627f75beaca941ab32dbcded651a459a
Antoine Musso <hashar@free.fr>
2023-01-04 10:21:06 +0100

    phabricator: create phd home directory on service start
    `/var/run/phd` is used as the `phd` user home directory which does not
    exist when the system boots. As a result `phd.service` fails with:
    
     systemd[1]: Starting phabricator-phd...
     mkdir[888]: /bin/mkdir: cannot create directory ‘/var/run/phd/’: Permission denied
     chown[965]: /bin/chown: cannot access '/var/run/phd/': No such file or directory
     systemd[1]: phd.service: Control process exited, code=exited, status=1/FAILURE
     systemd[1]: phd.service: Failed with result 'exit-code'.
     systemd[1]: Failed to start phabricator-phd.
    
    The permission denied happens because the service runs as `phd` while
    `/var/run` is owned by `root`.
    
    The `mkdir` and `chown` were added six years ago by 7dff957ad528 but
    never worked until Puppet ran to create the directory for us.
    
    Let systemd create `/var/run/phd` replacing the faulty `mkdir` / `chown`.
    
    Notes:
    - `/var/run/phd` is apparently unused. It is empty on
      `phab1004.eqiad.wmnet` and `lsof` yields nothing opened in it for the
      `phd` user.  The directory is only used as the home directory for the
      `phd` user so changing it should have no impact.
    - We used to have a pid file as `/var/run/phd/pid` but it is no more
      needed since 34c0f5b5bb
    - We had a `~/.ssh/known_hosts` file but that is no more needed anymore.
    
    Bug: T326146
    Hosts: O:phabricator
    Change-Id: Id3fdcb7456bb0d97c31e672b75582b9c1ee001fa

--- modules/phabricator/manifests/phd/user.pp
+++ modules/phabricator/manifests/phd/user.pp
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 class phabricator::phd::user(
   String $user_name = 'phd',
-  Stdlib::Unixpath $home_dir = "/var/run/${user_name}",
 ) {
 
     # global UID reserved in modules/admin/data/data.yaml
@@ -9,16 +8,11 @@ class phabricator::phd::user(
     # in our setup normally GID always equals UID
     $gid = assert_type(Admin::UID::System::Global, $uid)
 
-    file { $home_dir:
-        ensure => directory,
-        owner  => $user_name,
-        group  => $user_name,
-    }
-
     systemd::sysuser { $user_name:
         ensure      => present,
         id          => "${uid}:${gid}",
         description => 'Phabricator daemon user',
-        home_dir    => $home_dir,
+        # Created by systemd when starting the service
+        home_dir    => '/var/run/phd',
     }
 }
