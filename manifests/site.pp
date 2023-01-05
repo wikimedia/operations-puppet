c9216e8d95e30fbebc477383f8586b7dfb27084c
Manuel Arostegui <marostegui@wikimedia.org>
2023-01-05 07:41:15 +0100

    mariadb: Productionize db1176 into s1
    db1176 is going to be placed in s1 to test MariaDB 11
    
    Bug: T326211
    Change-Id: Ibffe64329d2e905cd29c96192e6a33ae71040bc0

--- manifests/site.pp
+++ manifests/site.pp
@@ -504,7 +504,7 @@ node /^db1(207|208|209|210|211|212|213|214|215|216|217|218|219|220|221|222|223|2
 
 # s1 (enwiki) core production dbs on eqiad
 # See also db1099 and db1105 below
-node /^db1(107|118|119|128|132|134|135|163|169|184|186|196|206)\.eqiad\.wmnet/ {
+node /^db1(107|118|119|128|132|134|135|163|169|176|184|186|196|206)\.eqiad\.wmnet/ {
     role(mariadb::core)
 }
 
@@ -884,11 +884,6 @@ node 'db1133.eqiad.wmnet' {
 
 node 'db2102.codfw.wmnet' {
     role(mariadb::core_test)
-}
-
-# spare database hosts
-node 'db1176.eqiad.wmnet' {
-    role(insetup::data_persistence)
 }
 
 # Analytics production replicas
