9ac4733d4d9dc8c10b8ae8fb9e8daf3cfd5efaf3
Ben Tullis <btullis@wikimedia.org>
2023-01-06 17:35:16 +0000

    Detect the correct disks for the O/S on the cephosd servers
    Since it is no longer possible to rely on predictable device names for
    block storage devices, we have to use another mechanism to ascertain
    which devices should be used for the O/S when preseeding.
    
    The new cephosd servers require this, so this commit adds a case for
    their hostnames to the partman-early-command.sh file and a suitable
    function to configure the debian-installer with the correct device names.
    
    In this particular case we examine all of the block devices and select
    only SSDs with a capacity of less than 3 TB. The two drives were are
    using for the operating system are 480 GB SSDs.
    
    Three RAID1 mirrors are created on these disks; for boot, swap, and an
    LVM physical volume.
    
    I have checked that the blockdev and wc and egrep commands referenced in
    the function are available in the d-i environment for bullseye.
    
    Bug: T324670
    Change-Id: I1bc67686f7f9dc254629a932469b430696a9c83a

--- modules/install_server/files/autoinstall/scripts/partman_early_command.sh
+++ modules/install_server/files/autoinstall/scripts/partman_early_command.sh
@@ -25,9 +25,59 @@ d-i partman-auto-raid/recipe    string      \\
 EOF
 debconf-set-selections /tmp/dynamic_disc.cfg
 }
+
+configure_cephosd_disks() {
+  devices=""
+  for disk in /sys/block/sd*/queue/rotational
+  do
+    # We are checking for two SSDs that are less than 3TB in size.
+    # These device names will be configured for the RAID array and grub boot devices.
+    if grep -q 0 "${disk}"
+    then
+      device="$(printf "%s" "${disk}" | cut -d/ -f4 -)"
+      if [ $(/sbin/blockdev --getsize64 /dev/"${device}") -lt 3000000000000 ]
+        then
+          devices="${devices## } /dev/${device}"
+        fi
+    fi
+  done
+  # Double checking that we have exactly two SCSI devices
+  num_devices=$(echo ${devices} | egrep -o '\/dev\/sd[a-z]'|wc -l)
+  if [ ${num_devices} -ne 2 ]
+  then
+    echo "We expected to find two boot devices, but instead found ${num_devices}".
+    exit 1
+  fi
+  boot_parts=$(printf "%s1#%s1" "${devices% *}" "${devices#* }")
+  swap_parts=$(printf "%s2#%s2" "${devices% *}" "${devices#* }")
+  root_parts=$(printf "%s3#%s3" "${devices% *}" "${devices#* }")
+
+cat > /tmp/dynamic_disc.cfg <<EOF
+d-i partman-auto/disk   string ${devices}
+d-i grub-installer/bootdev  string  ${devices}
+# Parameters are:
+# <raidtype> <devcount> <sparecount> <fstype> <mountpoint> \\
+#   <devices> <sparedevices>
+d-i partman-auto-raid/recipe string  \
+        1    2    0    ext4    /boot \
+            ${boot_parts}      \
+        .                            \
+        1    2    0    swap    -     \
+            ${swap_parts}      \
+        .                            \
+        1    2    0    lvm    -      \
+            ${root_parts}      \
+        .
+EOF
+debconf-set-selections /tmp/dynamic_disc.cfg
+}
+
 case $(hostname) in
   ms-be2050)
     configure_swift_disks
+    ;;
+  cephosd*)
+    configure_cephosd_disks
     ;;
 esac
 
