baf9ba1a2135cbeb3bb9fe4f62498c5f74a896e9
Brett Cornwall <bcornwall@wikimedia.org>
2023-01-09 09:03:58 -0800

    errorpage: Send a comment on browsersec errors
    A comment set to the top of the returned page is useful for API queries
    with inadequate security settings. As the HTTP code returned for
    /sec-warning is limited to 200 (see the Bug page) this is the most
    convenient way to inform API users that they need to review security
    settings.
    
    Bug: T240794
    Change-Id: I662a4678e226846211d0917f63854cb051f7a803

--- modules/varnish/manifests/common/browsersec.pp
+++ modules/varnish/manifests/common/browsersec.pp
@@ -1,14 +1,15 @@
 class varnish::common::browsersec {
     $errorpage = {
-        title       => 'Browser Connection Security Issues',
-        pagetitle   => "Your Browser's Connection Security is Outdated",
-        logo_link   => 'https://www.wikimedia.org',
-        logo_src    => 'https://www.wikimedia.org/static/images/wmf-logo.png',
-        logo_srcset => 'https://www.wikimedia.org/static/images/wmf-logo-2x.png 2x',
-        logo_width  => '135',
-        logo_height => '101',
-        logo_alt    => 'Wikimedia',
-        content     => template('varnish/browsersec.body.html.erb'),
+        title              => 'Browser Connection Security Issues',
+        pagetitle          => "Your Browser's Connection Security is Outdated",
+        logo_link          => 'https://www.wikimedia.org',
+        logo_src           => 'https://www.wikimedia.org/static/images/wmf-logo.png',
+        logo_srcset        => 'https://www.wikimedia.org/static/images/wmf-logo-2x.png 2x',
+        logo_width         => '135',
+        logo_height        => '101',
+        logo_alt           => 'Wikimedia',
+        content            => template('varnish/browsersec.body.html.erb'),
+        browsersec_comment => true,
     }
     $error_browsersec_html = template('mediawiki/errorpage.html.erb')
 
