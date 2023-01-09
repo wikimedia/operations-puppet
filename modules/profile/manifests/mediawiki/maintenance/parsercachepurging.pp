78285d7a0c2552cc9b8351cbf046789c409cee4b
Manuel Arostegui <marostegui@wikimedia.org>
2023-01-09 17:51:27 +0100

    parsercachepurging.pp: Increase retention back to 30 days
    30 days was the original retention policy, we changed it months ago to
       21 days months ago to address some space disk issues.
    
    Bug: T280604
    Change-Id: I1b63dd02698ea34650b0e133fe20034ba5359676

--- modules/profile/manifests/mediawiki/maintenance/parsercachepurging.pp
+++ modules/profile/manifests/mediawiki/maintenance/parsercachepurging.pp
@@ -1,24 +1,24 @@
 class profile::mediawiki::maintenance::parsercachepurging {
 
-    # Every day, Purge entries older than 21d * 86400s/d = 1814400s
+    # Every day, Purge entries older than 30d * 86400s/d = 2592000s
     #
     # WARNING: Increasing msleep may cause exponential growth. Deletes must outpace other writes! (T282761)
     #
     profile::mediawiki::periodic_job { 'purge_parsercache_pc1':
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc1 --age=1814400 --msleep 200',
+        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc1 --age=2592000 --msleep 200',
         interval => '01:00',
     }
     profile::mediawiki::periodic_job { 'purge_parsercache_pc2':
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc2 --age=1814400 --msleep 200',
+        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc2 --age=2592000 --msleep 200',
         interval => '01:00',
     }
     profile::mediawiki::periodic_job { 'purge_parsercache_pc3':
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc3 --age=1814400 --msleep 200',
+        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc3 --age=2592000 --msleep 200',
         interval => '01:00',
     }
 
     profile::mediawiki::periodic_job { 'purge_parsercache_pc4':
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc4 --age=1814400 --msleep 200',
+        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc4 --age=2592000 --msleep 200',
         interval => '01:00',
     }
 }
