58b09d1431e581bd63421504150e37b0cf0171fe
Daniel Zahn <dzahn@wikimedia.org>
2023-01-09 15:22:14 -0800

    phabricator: use specific data type for UIDs
    Change-Id: If72ae5270f85a3d4727ed887c56a5db6436ea9d2

--- modules/phabricator/manifests/phd/user.pp
+++ modules/phabricator/manifests/phd/user.pp
@@ -2,8 +2,8 @@
 class phabricator::phd::user(
   String $user_name = 'phd',
   Stdlib::Unixpath $home_dir = "/var/run/${user_name}",
-  Integer $uid = 920, # reserved in modules/admin/data/data.yaml
-  Integer $gid = $uid, # in our setup normally GID always equals UID
+  Admin::UID::System::Global $uid = 920, # reserved in modules/admin/data/data.yaml
+  Admin::UID::System::Global $gid = $uid, # in our setup normally GID always equals UID
 ) {
 
     file { $home_dir:
