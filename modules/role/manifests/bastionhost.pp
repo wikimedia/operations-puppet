cfd70d73a281fc7cd776a95f322e8ffc13f544ed
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-01-16 12:55:20 +0100

    Cleanup bastion role
    Inline bastionhost::packages since it's just installing a few packages
    and switch to ensure_packages.
    
    Change-Id: Ib3fb30efdf8d16616a5566a9ed2dcfb6ccc0ab50

--- modules/role/manifests/bastionhost.pp
+++ modules/role/manifests/bastionhost.pp
@@ -4,11 +4,11 @@ class role::bastionhost{
         description => 'Bastion host for all shell users',
     }
 
-    include ::profile::base::production
-    include ::profile::base::firewall
-    include ::profile::backup::host
+    include profile::base::production
+    include profile::base::firewall
+    include profile::backup::host
 
-    include ::profile::bastionhost::packages
+    ensure_packages(['mtr-tiny', 'traceroute', 'mosh'])
 
     # Used by parsoid deployers
     include ::profile::scap::dsh
