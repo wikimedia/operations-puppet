eb2cc42e33381435aa23c6959335c959ea53e118
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-01-18 13:36:21 +0100

    Enable profile::auto_restarts::service for blkmapd
    Automatically restart blkmapd if underlying runtime library dependencies
    have been upgraded (e.g. openssl). The restart is splayed across
    each day (on Mo-Fr).
    
    Bug: T135991
    Hosts: clouddumps1001.wikimedia.org
    Change-Id: I0c9f0e26d7e26e886ae512595de01766358dfb9e

--- modules/profile/manifests/dumps/distribution/nfs.pp
+++ modules/profile/manifests/dumps/distribution/nfs.pp
@@ -59,4 +59,5 @@ class profile::dumps::distribution::nfs (
 
     profile::auto_restarts::service { 'rpcbind':}
     profile::auto_restarts::service { 'nfs-idmapd':}
+    profile::auto_restarts::service { 'nfs-blkmap':}
 }
