6e98b063db1aa02dc2e3f8e87e76bf45f4ac807f
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-01-20 16:03:13 +0100

    openstack::cinder::user: Switch to systemd::sysuser
    The system group is created alongside by systemd::sysuser, the rest
    of previosly covered options is set by defaults.
    
    The main benefit of systemd-sysusers is that it properly declares
    system users in a central place (or rather two; /usr/lib/sysusers.d/
    for anything shipped by distro packages) and /etc/sysusers.d/*.conf
    for anything locally deployed (as in our Puppet), while system users
    reated via adduser are much harder to track down (some postinst,
    Puppet or wherever).
    
    Hosts: C:openstack::cinder::user
    Change-Id: I883913b4c743b7d29cf3e7f34dd5dad689346271

--- modules/openstack/manifests/cinder/user.pp
+++ modules/openstack/manifests/cinder/user.pp
@@ -2,18 +2,8 @@ class openstack::cinder::user (
 ) {
     # The cinder packages create this user, but with a weird, non-system ID.
     # Instead, create the user ahead of time with a proper uid.
-    group { 'cinder':
-        ensure => 'present',
-        name   => 'cinder',
-        system => true,
-    }
 
-    user { 'cinder':
-        ensure     => 'present',
-        name       => 'cinder',
-        comment    => 'cinder system user',
-        gid        => 'cinder',
-        managehome => true,
-        system     => true,
+    systemd::sysuser { 'cinder':
+        description => 'cinder system user',
     }
 }
