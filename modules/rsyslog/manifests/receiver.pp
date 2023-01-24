a07384921b390999186b18859f1f9e551bf0f5f9
Andrea Denisse Gómez-Martínez <adenisse@wikimedia.org>
2023-01-23 19:08:46 -0600

    centrallog: Sync centrallog1001 to centrallog1002
    Bug: T318778
    Change-Id: I42ffb2e1997a159a433cc021ec4cb5a85fdab28b

--- modules/rsyslog/manifests/receiver.pp
+++ modules/rsyslog/manifests/receiver.pp
@@ -144,12 +144,11 @@ class rsyslog::receiver (
     # inactive (ensure => absent, auto_sync => false)  but kept here to be
     # quickly enabled when needed.
     rsync::quickdatacopy { 'centrallog':
-        ensure              => absent,
-        source_host         => 'centrallog2001.codfw.wmnet',
-        dest_host           => 'centrallog2002.codfw.wmnet',
+        ensure              => present,
+        source_host         => 'centrallog1001.eqiad.wmnet',
+        dest_host           => 'centrallog1002.eqiad.wmnet',
         auto_sync           => false,
         module_path         => '/srv',
         server_uses_stunnel => true,
     }
-
 }
