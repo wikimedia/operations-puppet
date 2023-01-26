87dbbdb6dd8406a33948b9edc0678dc7cb0c2f7a
Ryan Kemper <rkemper@wikimedia.org>
2023-01-26 12:05:44 -0800

    django_oidc: fix formatting
    FIXES THESE LINT ERRORS:
    pipeline.py:10:27: E203 whitespace before ','
    pipeline.py:12:1: E302 expected 2 blank lines, found 1
    
    Change-Id: Idc550b7828a6374483222c61631b3efce24b1843

--- modules/profile/files/idp/standalone/django_oidc/common/pipeline.py
+++ modules/profile/files/idp/standalone/django_oidc/common/pipeline.py
@@ -7,7 +7,8 @@ logger = logging.getLogger('__name__')
 
 
 def save_profile(backend, user, response, *args, **kwargs):
-    logger.error("HERE %s" , response)
+    logger.error("HERE %s", response)
+
 
 @partial
 def require_email(strategy, details, user=None, is_new=False, *args, **kwargs):
