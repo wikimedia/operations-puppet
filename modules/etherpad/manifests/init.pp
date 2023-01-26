45b5ae8b8979eadf38a6d90e307ee69d58d1b1a8
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-01-26 15:04:42 +0100

    Enable profile::auto_restarts::service for etherpad-lite
    Automatically restart etherpad-lite if underlying runtime library dependencies
    have been upgraded (e.g. openssl). The restart is splayed across
    each day (on Mo-Fr).
    
    While at it, also modernise the Puppet code a bit, switch to ensure_packages
    and drop the obsolete Requires: on the package.
    
    Tested on etherpad1003 following the latest nodejs security update.
    
    Bug: T135991
    Hosts: etherpad1003.eqiad.wmnet
    Change-Id: Ia7fb07e665e69c5f33a3e3e28b2610eb34a0f602

--- modules/etherpad/manifests/init.pp
+++ modules/etherpad/manifests/init.pp
@@ -9,29 +9,25 @@ class etherpad(
     $etherpad_ip = '0.0.0.0',
     $etherpad_port = 9001,
 ){
-
-    package { 'etherpad-lite':
-        ensure => present,
-    }
+    ensure_packages('etherpad-lite')
 
     service { 'etherpad-lite':
         ensure    => running,
         enable    => true,
-        require   => Package['etherpad-lite'],
         subscribe => File['/etc/etherpad-lite/settings.json'],
     }
 
+    profile::auto_restarts::service { 'etherpad-lite':}
+
     file { '/etc/etherpad-lite/settings.json':
-        require => Package['etherpad-lite'],
         content => template('etherpad/settings.json.erb'),
     }
 
     file { '/usr/share/etherpad-lite/src/static/robots.txt':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        source  => 'puppet:///modules/etherpad/etherpad-robots.txt',
-        require => Package['etherpad-lite'],
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/etherpad/etherpad-robots.txt',
     }
 }
