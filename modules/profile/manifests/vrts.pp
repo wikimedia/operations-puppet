cb7144f40b57ec00c190664b12aecc3425161d39
Arnold Okoth <aokoth@wikimedia.org>
2023-02-06 18:51:18 +0300

    vrts: enable/disable daemon depending on active host
    Bug: T323515
    Change-Id: Idd59392cb6ae5febca93f6ba4dbd82252b48ca20

--- modules/profile/manifests/vrts.pp
+++ modules/profile/manifests/vrts.pp
@@ -3,12 +3,12 @@
 # sets up an instance of the 'Volunteer Response Team System'
 # https://wikitech.wikimedia.org/wiki/VRT_System
 class profile::vrts(
+    Stdlib::Fqdn $active_host        = lookup('profile::vrts::active_host'),
     Stdlib::Fqdn $vrts_database_host = lookup('profile::vrts::database_host'),
     String $vrts_database_name       = lookup('profile::vrts::database_name'),
     String $vrts_database_user       = lookup('profile::vrts::database_user'),
     String $vrts_database_pw         = lookup('profile::vrts::database_pass'),
     String $vrts_database_port       = lookup('profile::vrts::database_port'),
-    Boolean $vrts_daemon             = lookup('profile::vrts::daemon'),
     String $exim_database_name       = lookup('profile::vrts::exim_database_name'),
     String $exim_database_user       = lookup('profile::vrts::exim_database_user'),
     String $exim_database_pass       = lookup('profile::vrts::exim_database_pass'),
@@ -28,13 +28,15 @@ class profile::vrts(
         $x !~ /127.0.0.0|::1/
     }
 
+    $enable_service = $active_host == $facts['fqdn']
+
     class { '::vrts':
         vrts_database_host => $vrts_database_host,
         vrts_database_name => $vrts_database_name,
         vrts_database_user => $vrts_database_user,
         vrts_database_pw   => $vrts_database_pw,
         vrts_database_port => $vrts_database_port,
-        vrts_daemon        => $vrts_daemon,
+        vrts_daemon        => $enable_service,
         exim_database_name => $exim_database_name,
         exim_database_user => $exim_database_user,
         exim_database_pass => $exim_database_pass,
