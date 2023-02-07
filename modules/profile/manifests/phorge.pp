e85c91cd7ce253dcae4a0391419639d25c88c54a
Daniel Zahn <dzahn@wikimedia.org>
2023-02-07 12:54:35 -0800

    phorge: add httpd Directory snippet, git clone arcanist
    - httpd needs the <Directory> snippet with Require all granted
      or it won't allow access to the index.php in docroot
    
    "You don't have permission to access this resource."
    
    - arcanist is a hard requirement for phorge to run and needs to be
      cloned as well, from github phacility upstream
    
    "Unable to load the "Arcanist" library.
    Put "arcanist/" next to "phabricator/" on disk."
    
    Bug: T328595
    Change-Id: If965f4135f00ab03a89e552eb7a883a2f1a36775

--- modules/profile/manifests/phorge.pp
+++ modules/profile/manifests/phorge.pp
@@ -3,14 +3,22 @@
 class profile::phorge(
     String $server_name = lookup('profile::phorge::server_name'),
     Stdlib::Unixpath $install_path = lookup('profile::phorge::install_path'),
-    Stdlib::HTTPSUrl $git_origin = lookup('profile::phorge::git_origin'),
+    Stdlib::HTTPSUrl $git_origin_arcanist = lookup('profile::phorge::git_origin_arcanist'),
+    Stdlib::HTTPSUrl $git_origin_phorge = lookup('profile::phorge::git_origin_phorge'),
 ){
 
     wmflib::dir::mkdir_p($install_path)
 
+    git::clone { 'arcanist':
+        ensure    => 'present',
+        origin    => $git_origin_arcanist,
+        directory => $install_path,
+        branch    => 'master',
+    }
+
     git::clone { 'phorge':
         ensure    => 'present',
-        origin    => $git_origin,
+        origin    => $git_origin_phorge,
         directory => $install_path,
         branch    => 'master',
     }
