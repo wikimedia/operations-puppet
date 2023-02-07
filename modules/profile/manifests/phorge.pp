b10c03fd5d43f2c2a915c65ea4fc137576ed5f91
Daniel Zahn <dzahn@wikimedia.org>
2023-02-07 12:20:55 -0800

    phorge: add minimal apache site, add class parameters for docroot et al
    Add minimal apache virtual site config pointing to docroot and enabling
    rewrite rule as per install docs.
    
    Turn server_name, install_path and git clone URL into class parameters
    and move values to Hiera, on cloud project-level.
    
    Derive document_root from install_path.
    
    Bug: T328595
    Change-Id: I08b2e8dc8918f35c291279700ab2ff8703383f7a

--- modules/profile/manifests/phorge.pp
+++ modules/profile/manifests/phorge.pp
@@ -1,13 +1,24 @@
 # SPDX-License-Identifier: Apache-2.0
 # https://we.phorge.it - fork of Phabricator
-class profile::phorge {
+class profile::phorge(
+    String $server_name = lookup('profile::phorge::server_name'),
+    Stdlib::Unixpath $install_path = lookup('profile::phorge::install_path'),
+    Stdlib::HTTPSUrl $git_origin = lookup('profile::phorge::git_origin'),
+){
 
-    wmflib::dir::mkdir_p('/srv/phorge')
+    wmflib::dir::mkdir_p($install_path)
 
     git::clone { 'phorge':
         ensure    => 'present',
-        origin    => 'https://we.phorge.it/source/phorge.git',
-        directory => '/srv/phorge',
+        origin    => $git_origin,
+        directory => $install_path,
         branch    => 'master',
     }
+
+    $document_root = "${install_path}/webroot"
+
+    httpd::site { 'phorge':
+        content => template('profile/phorge/apache.conf.erb'),
+    }
+
 }
