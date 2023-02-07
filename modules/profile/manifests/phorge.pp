1a694b9767de81dfe65fd627ace45f5188094f1b
Daniel Zahn <dzahn@wikimedia.org>
2023-02-07 15:44:28 -0800

    phorge: separate install pathes for phorge and arcanist
    Puppet won't git clone 2 things into the same install path.
    
    Upstream install docs say to put them "next to each other".
    
    Bug: T328595
    Change-Id: I67708b44e94e44bf85aa92f1c5df84718319ca66

--- modules/profile/manifests/phorge.pp
+++ modules/profile/manifests/phorge.pp
@@ -2,28 +2,29 @@
 # https://we.phorge.it - fork of Phabricator
 class profile::phorge(
     String $server_name = lookup('profile::phorge::server_name'),
-    Stdlib::Unixpath $install_path = lookup('profile::phorge::install_path'),
+    Stdlib::Unixpath $install_path_arcanist = lookup('profile::phorge::install_path_arcanist'),
     Stdlib::HTTPSUrl $git_origin_arcanist = lookup('profile::phorge::git_origin_arcanist'),
+    Stdlib::Unixpath $install_path_phorge = lookup('profile::phorge::install_path_phorge'),
     Stdlib::HTTPSUrl $git_origin_phorge = lookup('profile::phorge::git_origin_phorge'),
 ){
 
-    wmflib::dir::mkdir_p($install_path)
+    wmflib::dir::mkdir_p([$install_path_arcanist, $install_path_phorge])
 
     git::clone { 'arcanist':
         ensure    => 'present',
         origin    => $git_origin_arcanist,
-        directory => $install_path,
+        directory => $install_path_arcanist,
         branch    => 'master',
     }
 
     git::clone { 'phorge':
         ensure    => 'present',
         origin    => $git_origin_phorge,
-        directory => $install_path,
+        directory => $install_path_phorge,
         branch    => 'master',
     }
 
-    $document_root = "${install_path}/webroot"
+    $document_root = "${install_path_phorge}/webroot"
 
     httpd::site { 'phorge':
         content => template('profile/phorge/apache.conf.erb'),
