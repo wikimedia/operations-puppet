d989b057696003b9f8512dde012f897e2e1a3696
Ndibe Raymond Olisaemeka <rolisaemeka-ctr@wikimedia.org>
2023-02-07 18:05:29 +0100

    puppet: modify role::wmcs::nfs::primary for replica_cnf api
    Add ::profile::wmcs::services::toolsdb_replica_cnf
    to role::wmcs::nfs::primary
    
    Bug: T303663
    Change-Id: I03454cdd7a4e6da4fd711d821da81b682cb1eb90

--- modules/role/manifests/wmcs/nfs/primary.pp
+++ modules/role/manifests/wmcs/nfs/primary.pp
@@ -10,4 +10,5 @@ class role::wmcs::nfs::primary {
     include ::profile::wmcs::nfs::ferm
     include ::profile::wmcs::nfs::primary
     include ::profile::wmcs::nfs::maintain_dbusers
+    include ::profile::wmcs::services::toolsdb_replica_cnf
 }
