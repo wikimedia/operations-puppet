14fdbf786c0e522fa27da5ded1dbe8d4bd37e613
Manuel Arostegui <marostegui@wikimedia.org>
2023-02-13 07:20:31 +0100

    mariadb: Promote db1164 to m1 master
    Bug: T329259
    Change-Id: Id3e3914fef7c707f231abdfdb379534f0dbef445

--- manifests/site.pp
+++ manifests/site.pp
@@ -713,12 +713,12 @@ node /^db21(42|43|44)\.codfw\.wmnet$/ {
 ## m1 shard
 # See also multiinstance misc hosts db1117, db2160 below
 
-# m1 master
+# old m1 master
 node 'db1176.eqiad.wmnet' {
     role(mariadb::misc)
 }
 
-# m1 future master
+# m1 master
 node 'db1164.eqiad.wmnet' {
     role(mariadb::misc)
 }
