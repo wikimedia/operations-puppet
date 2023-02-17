7669f8bb53ac62e902b2f808850238ca6046d0a1
pt1979 <ptshibamba@wikimedia.org>
2023-02-17 08:19:39 -0600

    Update model for ps1-a8-codfw
    Bug: T327404
    Change-Id: I9aa0138c13029be2a3573c1e95bda5fccf9ea046

--- modules/facilities/manifests/init.pp
+++ modules/facilities/manifests/init.pp
@@ -368,9 +368,10 @@ class facilities (
         model => 'sentry4',
     }
     facilities::monitor_pdu_3phase { 'ps1-a8-codfw':
-        ip   => '10.193.0.32',
-        row  => 'a',
-        site => 'codfw',
+        ip    => '10.193.0.32',
+        row   => 'a',
+        site  => 'codfw',
+        model => 'sentry4',
     }
     facilities::monitor_pdu_3phase { 'ps1-b1-codfw':
         ip    => '10.193.0.33',
