baa0836c8405f3ad110935655e9039b27dd12de7
jbond <jbond@wikimedia.org>
2023-02-22 16:19:42 +0100

    sre:check_user: make namley_api_key optional
    Hosts: auto
    Change-Id: Ia4ca0dbbf7df3e88f0fa5f822f117c682865268c

--- modules/profile/manifests/sre/check_user.pp
+++ modules/profile/manifests/sre/check_user.pp
@@ -3,32 +3,37 @@
 # @param super_admin the support admin account to impersonate
 # @param service_file_source the location in the secret module of the service account json file
 # @param proxy_server the proxy server to use
+# @param namely_api_key the namely api key
 class profile::sre::check_user (
-    String               $super_admin         = lookup('profile::sre::check_user::super_admin'),
-    String               $service_file_source = lookup('profile::sre::check_user::service_file'),
-    Sensitive[String[1]] $namely_api_key      = lookup('profile::sre::check_user::namely_api_key'),
-    Stdlib::HTTPUrl      $proxy_server        = lookup('profile::sre::check_user::proxy_server'),
+    String                         $super_admin         = lookup('profile::sre::check_user::super_admin'),
+    String                         $service_file_source = lookup('profile::sre::check_user::service_file'),
+    Stdlib::HTTPUrl                $proxy_server        = lookup('profile::sre::check_user::proxy_server'),
+    Sensitive[Optional[String[1]]] $namely_api_key      = lookup('profile::sre::check_user::namely_api_key'),
 ) {
     # python3-google-auth-httplib2 is also required
     # https://github.com/googleapis/google-auth-library-python/issues/190#issuecomment-322837328
     $packages = ['python3-googleapi', 'python3-google-auth', 'python3-google-auth-httplib2']
     # need at least version 1.6.0
     # https://github.com/googleapis/google-auth-library-python/issues/190#issuecomment-322640637
-    apt::pin {'python3-googleapi':
+    apt::pin { 'python3-googleapi':
         pin      => 'release a=buster-backports',
         priority => 1001,
     }
     ensure_packages($packages)
 
+    $namley_config = $namely_api_key.unwrap.empty.bool2str(
+        '',
+        "namely_api_key: ${namely_api_key.unwrap}"
+    )
     $service_file_path = '/etc/ssl/private/gsuite_service.json'
     $config = @("CONFIG")
     [DEFAULT]
     impersonate: ${super_admin}
     key_file: ${service_file_path}
     proxy_host: ${proxy_server}
-    namely_api_key: ${namely_api_key.unwrap}
+    ${namley_config}
     | CONFIG
-    file{
+    file {
         default:
             ensure => file,
             owner  => 'root',
