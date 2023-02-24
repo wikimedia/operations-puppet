01e7bdea78f6c7279b8ab708aa39929fc107195c
Taavi Väänänen <hi@taavi.wtf>
2023-02-24 15:19:19 +0200

    P:toolforge::k8s::haproxy: allow simplifying the backend array
    By adding a separate ingress_backend_port variable. Due to historical
    reasons the ingress-nginx NodePort is on port 30002 but the frontend is
    on 30000.
    
    Change-Id: I92b23f66057842ff74c04a927f2cc62772586e5f

--- modules/profile/manifests/toolforge/k8s/haproxy.pp
+++ modules/profile/manifests/toolforge/k8s/haproxy.pp
@@ -8,15 +8,16 @@ class profile::toolforge::k8s::haproxy (
                 jobs_port => Optional[Stdlib::Port],
             }]
         ]
-    ]                   $ingress_nodes       = lookup('profile::toolforge::k8s::ingress_nodes',                {default_value => ['localhost']}),
-    Stdlib::Port        $ingress_port        = lookup('profile::toolforge::k8s::ingress_port',                 {default_value => 30000}),
-    Array[Stdlib::Fqdn] $control_nodes       = lookup('profile::toolforge::k8s::control_nodes',                {default_value => ['localhost']}),
-    Stdlib::Port        $api_port            = lookup('profile::toolforge::k8s::apiserver_port',               {default_value => 6443}),
-    Stdlib::Port        $jobs_port           = lookup('profile::toolforge::jobs_api_port',                     {default_value => 30001}),
-    Array[Stdlib::Fqdn] $keepalived_vips     = lookup('profile::toolforge::k8s::haproxy::keepalived_vips',     {default_value => []}),
-    Array[Stdlib::Fqdn] $keepalived_peers    = lookup('profile::toolforge::k8s::haproxy::keepalived_peers',    {default_value => ['localhost']}),
-    String              $keepalived_password = lookup('profile::toolforge::k8s::haproxy::keepalived_password', {default_value => 'notarealpassword'}),
-    Stdlib::Fqdn        $web_domain          = lookup('profile::toolforge::web_domain',                        {default_value => 'toolforge.org'}),
+    ]                   $ingress_nodes        = lookup('profile::toolforge::k8s::ingress_nodes',                {default_value => ['localhost']}),
+    Stdlib::Port        $ingress_port         = lookup('profile::toolforge::k8s::ingress_port',                 {default_value => 30000}),
+    Stdlib::Port        $ingress_backend_port = lookup('profile::toolforge::k8s::ingress_backend_port',         {default_value => 30002}),
+    Array[Stdlib::Fqdn] $control_nodes        = lookup('profile::toolforge::k8s::control_nodes',                {default_value => ['localhost']}),
+    Stdlib::Port        $api_port             = lookup('profile::toolforge::k8s::apiserver_port',               {default_value => 6443}),
+    Stdlib::Port        $jobs_port            = lookup('profile::toolforge::jobs_api_port',                     {default_value => 30001}),
+    Array[Stdlib::Fqdn] $keepalived_vips      = lookup('profile::toolforge::k8s::haproxy::keepalived_vips',     {default_value => []}),
+    Array[Stdlib::Fqdn] $keepalived_peers     = lookup('profile::toolforge::k8s::haproxy::keepalived_peers',    {default_value => ['localhost']}),
+    String              $keepalived_password  = lookup('profile::toolforge::k8s::haproxy::keepalived_password', {default_value => 'notarealpassword'}),
+    Stdlib::Fqdn        $web_domain           = lookup('profile::toolforge::web_domain',                        {default_value => 'toolforge.org'}),
 ) {
     class { 'haproxy::cloud::base': }
 
