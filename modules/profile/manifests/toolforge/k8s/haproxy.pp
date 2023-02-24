a87dc7dc44198fb3a3e734ea1c63b511294f7f3f
Taavi Väänänen <hi@taavi.wtf>
2023-02-24 15:21:30 +0200

    P:toolforge::k8s::haproxy: remove support for hash node list
    Removes complexity after I92b23f66057842ff74c04a927f2cc62772586e5f made
    that possible.
    
    Change-Id: I0b8cabbc099ef59cf4bf6a21ac05071a5a0be1da

--- modules/profile/manifests/toolforge/k8s/haproxy.pp
+++ modules/profile/manifests/toolforge/k8s/haproxy.pp
@@ -1,14 +1,5 @@
 class profile::toolforge::k8s::haproxy (
-    Array[
-        Variant[
-            Stdlib::Fqdn,
-            Struct[{
-                host      => Stdlib::Fqdn,
-                port      => Optional[Stdlib::Port],
-                jobs_port => Optional[Stdlib::Port],
-            }]
-        ]
-    ]                   $ingress_nodes        = lookup('profile::toolforge::k8s::ingress_nodes',                {default_value => ['localhost']}),
+    Array[Stdlib::Fqdn] $ingress_nodes        = lookup('profile::toolforge::k8s::ingress_nodes',                {default_value => ['localhost']}),
     Stdlib::Port        $ingress_port         = lookup('profile::toolforge::k8s::ingress_port',                 {default_value => 30000}),
     Stdlib::Port        $ingress_backend_port = lookup('profile::toolforge::k8s::ingress_backend_port',         {default_value => 30002}),
     Array[Stdlib::Fqdn] $control_nodes        = lookup('profile::toolforge::k8s::control_nodes',                {default_value => ['localhost']}),
