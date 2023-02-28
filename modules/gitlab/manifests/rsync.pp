8f2a58850a56707b4c4350d63ddd5b0db08aa19d
John Bond <jbond@wikimedia.org>
2023-02-28 13:09:38 +0100

    gitlab::rsync: ensure we remove old jobs when promoting a server to active
    Currently when we change the active server the old systemd job which
    pushed from the old active server to the new active server is simply
    remove from the catalog instead of beaing set to absent.  As such it
    never gets cleaned up.
    
    This Cr updats the logic to ensure all old jobs are removed.
    
    Bug: T330744
    Hosts: gitlab1004.wikimedia.org
    Hosts: gitlab2002.wikimedia.org
    Change-Id: Id86f3538be3e1819e9ca070720a4b06d7d0d558a

--- modules/gitlab/manifests/rsync.pp
+++ modules/gitlab/manifests/rsync.pp
@@ -11,45 +11,39 @@ class gitlab::rsync (
     Stdlib::Unixpath         $backup_dir_config = '/etc/gitlab/config_backup',
 ){
     # only activate rsync/firewall hole on the server that is NOT active
-    if $ensure != 'present' {
-        $ensure_job = 'absent'
-        $ensure_sync = 'absent'
-    }
-    elsif $facts['fqdn'] == $active_host {
-        $ensure_job = 'present'
-        $ensure_sync = 'absent'
-    } else {
-        $ensure_job = 'absent'
-        $ensure_sync = 'present'
-    }
+    $ensure_sync = ($facts['networking']['fqdn'] != $active_host).bool2str($ensure, 'absent')
 
     # On the replica, only one folder is used for config and data
     # backup due to restrictions in writing to /etc/. So only one
     # rsync server module is needed.
+    $all_hosts = $passive_hosts + $active_host
     rsync::server::module { 'data-backup':
         ensure         => $ensure_sync,
         path           => $backup_dir_data,
         read_only      => 'no',
-        hosts_allow    => $passive_hosts + $active_host,
+        hosts_allow    => $all_hosts,
         auto_ferm      => true,
         auto_ferm_ipv6 => true,
     }
 
-    $passive_hosts.each | Stdlib::Fqdn $passive_host | {
+    $all_hosts.each | Stdlib::Fqdn $host | {
+        # We need to ensure that systemd timeres are only active on the active host
+        # and that any jobs on the old active pulling from the new active are also cleaned up
+        $ensure_job = ($active_host == $facts['networking']['fqdn'] and $active_host != $host).bool2str($ensure, 'absent')
         # rsync data backup and exclude Shell scripts and config backup from sync
-        systemd::timer::job { "rsync-data-backup-${passive_host}":
+        systemd::timer::job { "rsync-data-backup-${host}":
             ensure      => $ensure_job,
             user        => 'root',
             description => 'rsync GitLab data backup primary to a secondary server',
-            command     => "/usr/bin/rsync -avp --delete --exclude='*.sh' --exclude='gitlab_config_*.tar' ${backup_dir_data}/ rsync://${passive_host}/data-backup",
+            command     => "/usr/bin/rsync -avp --delete --exclude='*.sh' --exclude='gitlab_config_*.tar' ${backup_dir_data}/ rsync://${host}/data-backup",
             interval    => $rsync_interval,
         }
         # rsync config backup and exclude Shell scripts and data backup from sync
-        systemd::timer::job { "rsync-config-backup-${passive_host}":
+        systemd::timer::job { "rsync-config-backup-${host}":
             ensure      => $ensure_job,
             user        => 'root',
             description => 'rsync GitLab config backup primary to a secondary server',
-            command     => "/usr/bin/rsync -avp --delete --exclude='*.sh' --exclude='*_gitlab_backup.tar' ${backup_dir_config}/ rsync://${passive_host}/data-backup",
+            command     => "/usr/bin/rsync -avp --delete --exclude='*.sh' --exclude='*_gitlab_backup.tar' ${backup_dir_config}/ rsync://${host}/data-backup",
             interval    => $rsync_interval,
         }
     }
