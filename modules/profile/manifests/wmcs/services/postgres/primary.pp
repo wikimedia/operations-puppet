7e615071b245741ec51af8705f95e2e93b7bdb3a
Taavi Väänänen <hi@taavi.wtf>
2023-02-28 11:42:51 +0200

    P:wmcs: remove osmdb classes
    Removed all osmdb classes except P::wmcs::services::postgres::primary,
    which seems to also be in use by the wikilabels project. Merged ::common
    into ::primary.
    
    Bug: T323159
    Change-Id: I96c85c635530eb76c9cca1e0d2ee87a55a838f9b

--- modules/profile/manifests/wmcs/services/postgres/primary.pp
+++ modules/profile/manifests/wmcs/services/postgres/primary.pp
@@ -1,31 +1,33 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::wmcs::services::postgres::primary (
-    Optional[Stdlib::Host] $postgres_secondary = lookup('profile::wmcs::services::postgres::secondary', {default_value => undef}),
-    Optional[String] $replication_pass = lookup('profile::wmcs::services::postgres::replication_pass', {default_value => undef}),
     Stdlib::Unixpath $root_dir = lookup('profile::wmcs::services::postgres::root_dir', {default_value => '/srv/postgres'}),
-){
-    include profile::wmcs::services::postgres::common
+) {
+    $pgversion = debian::codename() ? {
+        'buster' => 11,
+    }
+
+    file { "/etc/postgresql/${pgversion}/main/tuning.conf":
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0444',
+        source => 'puppet:///modules/profile/wmcs/db/postgres/tuning.conf',
+    }
+
+    sysctl::parameters { 'postgres_shmem':
+        values => {
+            # That is derived after tuning postgresql, deriving automatically is
+            # not the safest idea yet.
+            'kernel.shmmax' => 8388608000,
+        },
+    }
+
     class {'::postgresql::postgis': }
     include ::profile::prometheus::postgres_exporter
 
     class { 'postgresql::master':
-        includes => ['tuning.conf'],
-        root_dir => $root_dir,
-    }
-
-    if $postgres_secondary {
-        $postgres_secondary_v4 = ipresolve($postgres_secondary, 4)
-        if $postgres_secondary_v4 {
-            postgresql::user { "replication@${postgres_secondary}-v4":
-                ensure   => 'present',
-                user     => 'replication',
-                password => $replication_pass,
-                cidr     => "${postgres_secondary_v4}/32",
-                type     => 'host',
-                method   => 'md5',
-                attrs    => 'REPLICATION',
-                database => 'all',
-            }
-        }
+        pgversion => $pgversion,
+        includes  => ['tuning.conf'],
+        root_dir  => $root_dir,
     }
 }
