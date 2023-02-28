7572dde834a24121ea4a15a69992ecd1f6314e59
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2023-02-28 11:55:09 +0100

    prometheus: add pint support
    The basic scaffolding will run 'pint', watch /srv/alerts and report
    basic problems. In a followup change also runtime problems will be added
    via prometheus::pint::source.
    
    Bug: T309182
    Change-Id: Ie763eeddfcf19a8ca70d21318d791e742b7e95ab

---
+++ modules/prometheus/manifests/pint.pp
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# pint (https://github.com/cloudflare/pint) is used to check Prometheus
+# alerting and recording rules for problems. This class takes care of
+# deploying the base pint scaffolding and start watching alerts.
+#
+# Additional 'live' checks can be performed (e.g. if metrics still exist)
+# by adding prometheus::pint::source to an existing prometheus instance.
+
+class prometheus::pint (
+    Stdlib::Port $listen_port = 9999,
+    Array[Stdlib::Unixpath] $watch_paths = ['/srv/alerts', '/srv/alerts-thanos'],
+) {
+    ensure_packages('pint')
+
+    require prometheus::assemble_config
+
+    file { '/etc/prometheus/pint.hcl.d':
+        ensure  => directory,
+        mode    => '0555',
+        owner   => 'root',
+        group   => 'root',
+        recurse => true,
+        purge   => true,
+    }
+
+    exec { 'assemble pint.hcl':
+        onlyif  => 'prometheus-assemble-config --onlyif pint',
+        command => 'prometheus-assemble-config pint',
+        notify  => Service['pint'],
+        path    => '/usr/local/bin',
+    }
+
+    systemd::service { 'pint':
+        ensure   => present,
+        content  => init_template('pint', 'systemd_override'),
+        override => true,
+        restart  => true,
+    }
+
+    profile::auto_restarts::service { 'pint': }
+}
---
+++ modules/prometheus/manifests/pint/source.pp
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# Add a pint Prometheus source to enable runtime checking of alerting
+# and recording rules.
+define prometheus::pint::source (
+    Stdlib::Port $port,
+    Wmflib::Ensure $ensure = 'present',
+    String $instance = $title,
+) {
+    include prometheus::pint
+
+    file { "/etc/prometheus/pint.hcl.d/${title}.hcl":
+        ensure  => $ensure,
+        content => template('prometheus/pint/source.hcl.erb'),
+        mode    => '0444',
+        notify  => Exec['assemble pint.hcl'],
+    }
+}
