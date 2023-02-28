d7e6e4b349e64ef46060478a8b2f5cede04eef8d
John Bond <jbond@wikimedia.org>
2023-02-28 17:40:45 +0100

    check_puppet_run_changes: refactor and switch to pql
    Change-Id: I08358edfbc3d56d2acbeab73997bccf46b8e700b

--- modules/profile/files/cumin/check_puppet_run_changes.py
+++ modules/profile/files/cumin/check_puppet_run_changes.py
@@ -6,18 +6,18 @@ every puppet agent run
 """
 import logging
 import os
+import re
 
 from argparse import ArgumentParser, Namespace
 from collections import defaultdict
 from datetime import datetime, timedelta
 from re import search
-from typing import Set
+from typing import Dict, Optional, Set
 
 from cumin import Config
 from cumin.transports import clustershell
 from pypuppetdb import connect
 from pypuppetdb.api import API
-from pypuppetdb.QueryBuilder import ExtractOperator, FunctionOperator, GreaterOperator
 from requests.packages.urllib3 import disable_warnings
 from requests.packages.urllib3.exceptions import SubjectAltNameWarning
 from spicerack import Spicerack
@@ -105,6 +105,114 @@ def get_inactive_alert_hosts(pdb: API) -> Set:
     return hosts
 
 
+def get_node_status(pdb: API, max_age: datetime) -> Dict:
+    """Return a list of inactive alerting hosts
+
+    Arguments:
+        pql: an pypuppetdb api object
+        max_age: filter reports no older this this date
+
+    Returns:
+        dict: a dict of hosts with keys representing counts for each status
+
+    """
+    pql = f"""
+    reports[count(status), certname, status] {{
+        receive_time > "{max_age.isoformat()}"
+        group by status, certname
+    }}
+    """
+    nodes = defaultdict(dict)
+    reports = pdb.pql(pql)
+
+    for report in reports:
+        nodes[report['certname']][report['status']] = report['count']
+
+    return nodes
+
+
+def filter_failed_nodes(nodes: Dict, skip_regex: Optional[re.Pattern] = None) -> Set:
+    """Filter the nodes dict to produce a set of constantly changing hosts.
+
+    Arguments:
+        nodes: A dict of hosts with keys representing counts for each status
+        skip_regex: A regex of hosts to skip
+
+    """
+    failed_nodes = set()
+    for fqdn, node in nodes.items():
+        # skip hosts with no unchanged reports:
+        if node.get('unchanged', 0):
+            continue
+        if (
+            skip_regex is not None
+            and search(skip_regex, fqdn.split('.')[0]) is not None
+        ):
+            logger.debug('%s: Skipping staging host', fqdn)
+            continue
+        failed_nodes.add(fqdn)
+    return failed_nodes
+
+
+def filter_icinga_nodes(nodes: Set, skip_icinga: Set, verbose: bool) -> Set:
+    """Check icinga and filter out any hosts that are downtimed or not active.
+
+    Arguments:
+        nodes: A set of hosts to filter
+        skip_icinga: a set of hosts to exclude when checking icinga
+        verbose_level: boolean value passed to spicerack verbose paramter
+
+    """
+    # TODO: Remove temporary workaround once Cumin has full support to suppress output (T212783)
+    stdout = clustershell.sys.stdout
+    stderr = clustershell.sys.stderr
+    try:
+        with open(os.devnull, 'w') as discard_output:
+            clustershell.sys.stdout = discard_output
+            clustershell.sys.stderr = discard_output
+            icinga = Spicerack(verbose=verbose, dry_run=False).icinga_hosts(
+                nodes - skip_icinga
+            )
+            icinga_status = icinga.get_status()
+    finally:
+        clustershell.sys.stdout = stdout
+        clustershell.sys.stderr = stderr
+
+    return nodes - {
+        node.name
+        for node in icinga_status.values()
+        if node.downtimed and not node.notifications_enabled
+    }
+
+
+def icinga_exit(nodes: Set, critical: int, warning: int) -> int:
+    """Check the nodes agans the critical and warning values and pick the correct exit status.
+
+    Arguments:
+        nodes: the set of nodes to check
+        critical: Exit with critical if the nodes are greater then this value
+        warning: Exit with warning if the nodes are greater then this value
+
+    Return:
+        int: the exit code to use
+    """
+    if len(nodes) < warning:
+        print('OK: all nodes running as expected')
+        return 0
+
+    if len(nodes) >= critical:
+        status = 'CRITICAL'
+        exit_code = 2
+    else:
+        status = 'WARNING'
+        exit_code = 1
+    print(
+        f"{status}: the following ({len(nodes)}) node(s) "
+        f"change every puppet run: {', '.join(sorted(nodes))}"
+    )
+    return exit_code
+
+
 def main() -> int:
     """main entry point
 
@@ -131,87 +239,23 @@ def main() -> int:
         pdb_config['ssl_verify'] = args.ssl_ca
 
     pdb = connect(**pdb_config)
-    nodes = defaultdict(dict)
 
     max_age = datetime.utcnow() - timedelta(hours=args.max_age)
 
-    inactive_alert_hosts = get_inactive_alert_hosts(pdb)
-    extract = ExtractOperator()
-    extract.add_field(['certname', FunctionOperator('count'), 'status'])
-    extract.add_group_by(['certname', 'status'])
-    extract.add_query(GreaterOperator('receive_time', max_age.isoformat()))
-
-    # pypuppetdb does have a `reports` method which wraps `_query`.  however it yields
-    # results of type pypuppetdb.Report which expects a number of parameters e.g. hash
-    # to be present in the result payload.  however we don't extract theses values and
-    # therefore have to resort to the more powerful private method
-    reports = pdb._query('reports', query=extract)  # pylint: disable=protected-access
-
-    for report in reports:
-        nodes[report['certname']][report['status']] = report['count']
-
-    if args.dev:
-        failed_nodes = {
-            fqdn for fqdn, node in nodes.items() if not node.get('unchanged', 0)
-        }
-    else:
-        for fqdn, node in nodes.items():
-            # skip hosts with no unchanged reports:
-            if node.get('unchanged', 0):
-                continue
-            # skip staging servers:
-            # - hostname starting labstest*
-            # - hostname ending dev or dev\d{4}
-            # - hostname ending test or test\d{4}
-            if (
-                fqdn.startswith('labtest')
-                or search(r'(:?dev|test)(:?\d{4})?$', fqdn.split('.')[0]) is not None
-            ):
-                logger.debug('%s: Skipping staging host', fqdn)
-                continue
-            failed_nodes.add(fqdn)
+    nodes = get_node_status(pdb, max_age)
+    # skip staging servers:
+    # - hostname starting labstest*
+    # - hostname ending dev or dev\d{4}
+    # - hostname ending test or test\d{4}
+    skip_regex = (
+        re.compile(r'(:?^labtest|(:?dev|test)(:?\d{4})?$)') if args.dev else None
+    )
+    failed_nodes = filter_failed_nodes(nodes, skip_regex)
 
     if failed_nodes:
-        # only run spicerack in verbose if using debug
-        verbose = False if args.verbose is None else args.verbose > 2
-
-        # TODO: Remove temporary workaround once Cumin has full support to suppress output (T212783)
-        stdout = clustershell.sys.stdout
-        stderr = clustershell.sys.stderr
-        try:
-            with open(os.devnull, 'w') as discard_output:
-                clustershell.sys.stdout = discard_output
-                clustershell.sys.stderr = discard_output
-                icinga = Spicerack(verbose=verbose, dry_run=False).icinga_hosts(
-                    failed_nodes - inactive_alert_hosts
-                )
-                icinga_status = icinga.get_status()
-        finally:
-            clustershell.sys.stdout = stdout
-            clustershell.sys.stderr = stderr
-        failed_nodes -= {
-            node.name
-            for node in icinga_status.values()
-            if node.downtimed and not node.notifications_enabled
-        }
-
-    if len(failed_nodes) >= args.critical:
-        print(
-            'CRITICAL: the following ({}) node(s) change every puppet run: {}'.format(
-                len(failed_nodes), ', '.join(sorted(failed_nodes))
-            )
-        )
-        return 2
-    if len(failed_nodes) >= args.warning:
-        print(
-            'WARNING: the following ({}) node(s) change every puppet run: {}'.format(
-                len(failed_nodes), ', '.join(sorted(failed_nodes))
-            )
-        )
-        return 1
-
-    print('OK: all nodes running as expected')
-    return 0
+        inactive_alert_hosts = get_inactive_alert_hosts(pdb)
+        failed_nodes = filter_icinga_nodes(failed_nodes, inactive_alert_hosts, args.verbose > 2)
+    return icinga_exit(failed_nodes, args.critical, args.warning)
 
 
 if __name__ == '__main__':
