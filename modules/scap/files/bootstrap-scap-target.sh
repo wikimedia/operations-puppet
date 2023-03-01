b01ff55f2685f2e92ee939f1ea8ad0ca32bc17f4
jnuche <jnuche@wikimedia.org>
2023-03-01 13:51:59 +0100

    scap bootstrap: use new installation mechanism
    Update the scap targets bootstrap script to use wheels
    
    Bug: T329622
    Change-Id: I02b016b0c00a52ea350ba8a5968772d46d3cc4ce

--- modules/scap/files/bootstrap-scap-target.sh
+++ modules/scap/files/bootstrap-scap-target.sh
@@ -15,19 +15,17 @@ if (($# < 2)); then
 fi
 
 DEPLOYMENT_SERVER="$1"
-DESTINATION_DIR="$2"
+DESTINATION_DIR="${2}/"
+INSTALL_USER="${USER:-scap}"
 
 # "scap-install-staging" is an rsync module defined in class scap::master
 /usr/bin/rsync --archive \
                --delay-updates --delete --delete-delay \
                --compress --new-compress \
                --timeout=5 --contimeout=2 \
-               --exclude=*.swp --exclude=**/__pycache__ \
-               "$DEPLOYMENT_SERVER::scap-install-staging/scap/" "$DESTINATION_DIR/scap/"
+               "$DEPLOYMENT_SERVER::scap-install-staging/scap-wheels/$(lsb_release -cs)" \
+               "$DEPLOYMENT_SERVER::scap-install-staging/scap/bin/install_local_version.sh" \
+               "$DESTINATION_DIR"
 
-# Use symlink to make sure interpreter adds libs to sys.path
 cd "$DESTINATION_DIR"
-PYTHON_VERSION=$(scap/bin/python3 --version | cut -d' ' -f2 | cut -d. -f1-2)
-if [ "$(ls -d scap/lib/python*)" != "scap/lib/python$PYTHON_VERSION" ]; then
-    ln -s "$DESTINATION_DIR"/scap/lib/python* "scap/lib/python$PYTHON_VERSION"
-fi
+scap/bin/install_local_version.sh -u "$INSTALL_USER"
