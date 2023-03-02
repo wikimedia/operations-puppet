23dedc2893749fedaa6d0764c842f49850a8d15c
John Bond <jbond@wikimedia.org>
2023-03-02 10:15:18 +0100

    aptrepo::rsync: refactor to fix issues
    Seems there where a number of issues with this module probably from been
    refactored over time.
     * it included its self
     * it included rsync::server (which is not needed as
       rsync::server::module includes this
     * rsync::server::module was only in the else block, meaning it would
       never get made absent
     * the systemd timers never remove the old jobs to push to a previous
       active after a failover
    
    Hosts: apt1001.wikimedia.org
    Hosts: apt2001.wikimedia.org
    Change-Id: I8f657e839a3c6c2ac3bdaeb80e24bc235c2209bd

--- modules/aptrepo/manifests/rsync.pp
+++ modules/aptrepo/manifests/rsync.pp
@@ -1,51 +1,40 @@
 # SPDX-License-Identifier: Apache-2.0
-# sets up rsync of APT repos between 2 servers
-# activates rsync for push from the primary to secondary
+# @summary sets up rsync of APT repos between 2 servers
+#          activates rsync for push from the primary to secondary
+# @param primary_server the active server data will be pusshed from this server to the secondaries
+# @param secondary_servers the passive servers. Firewall rules and rsync will
+#        be configured to receive data
 class aptrepo::rsync (
-    $primary_server,
-    $secondary_servers,
+    Stdlib::Fqdn        $primary_server,
+    Array[Stdlib::Fqdn] $secondary_servers,
 ){
-    # only activate rsync/firewall hole on the server that is NOT active
-    if $facts['fqdn'] == $primary_server {
+    $ensure_sync = ($facts['fqdn'] == $primary_server).bool2str('absent', 'present')
 
-        $ensure_ferm = 'absent'
-        $ensure_job = 'present'
-        $ensure_sync = 'absent'
-
-    } else {
-
-        $ensure_ferm = 'present'
-        $ensure_job = 'absent'
-        $ensure_sync = 'present'
-
-        include rsync::server
-        include aptrepo::rsync
-
-        rsync::server::module { 'install-srv':
-            ensure         => $aptrepo::rsync::ensure,
-            path           => '/srv',
-            read_only      => 'no',
-            hosts_allow    => [$primary_server],
-            auto_ferm      => true,
-            auto_ferm_ipv6 => true,
-        }
-
-        rsync::server::module { 'install-home':
-            ensure         => $aptrepo::rsync::ensure,
-            path           => '/home',
-            read_only      => 'no',
-            hosts_allow    => [$primary_server],
-            auto_ferm      => true,
-            auto_ferm_ipv6 => true,
-        }
+    rsync::server::module { 'install-srv':
+        ensure         => $ensure_sync,
+        path           => '/srv',
+        read_only      => 'no',
+        hosts_allow    => [$primary_server],
+        auto_ferm      => true,
+        auto_ferm_ipv6 => true,
     }
 
-    $secondary_servers.each |String $secondary_server| {
-        systemd::timer::job { "rsync-aptrepo-${secondary_server}":
+    rsync::server::module { 'install-home':
+        ensure         => $ensure_sync,
+        path           => '/home',
+        read_only      => 'no',
+        hosts_allow    => [$primary_server],
+        auto_ferm      => true,
+        auto_ferm_ipv6 => true,
+    }
+
+    ($secondary_servers + $primary_server).each |String $server| {
+        $ensure_job = ($primary_server == $facts['networking']['fqdn'] and $primary_server != $server).bool2str('present', 'absent')
+        systemd::timer::job { "rsync-aptrepo-${server}":
             ensure      => $ensure_job,
             user        => 'root',
             description => 'rsync APT repo data from the primary to a secondary server',
-            command     => "/usr/bin/rsync -avp --delete /srv/ rsync://${secondary_server}/install-srv",
+            command     => "/usr/bin/rsync -avp --delete /srv/ rsync://${server}/install-srv",
             interval    => {'start' => 'OnUnitInactiveSec', 'interval' => '6h'},
         }
     }
