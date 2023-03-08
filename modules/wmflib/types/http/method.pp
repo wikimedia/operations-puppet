adcac9d9a6201ea784d0c01fefa1fb53d645dd4a
jbond <jbond@wikimedia.org>
2023-03-08 14:19:21 +0100

    wmflib: Add post to http methods
    Change-Id: Ib5fc89f5ec956a167cdc3f1a287206ffde9136fe

--- modules/wmflib/types/http/method.pp
+++ modules/wmflib/types/http/method.pp
@@ -1,2 +1,2 @@
 # https://github.com/puppetlabs/puppetlabs-stdlib/pull/1192
-type Wmflib::HTTP::Method = Enum['GET', 'HEAD', 'OPTIONS', 'PUT', 'DELETE']
+type Wmflib::HTTP::Method = Enum['GET', 'HEAD', 'OPTIONS', 'PUT', 'DELETE', 'POST']
