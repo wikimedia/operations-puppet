a07d4ddbcfca3cdbc5d575e0999ed9bb75f367cc
Daniel Zahn <dzahn@wikimedia.org>
2023-03-15 15:52:34 -0700

    miscweb: limit http monitoring for design.wm.org to IPv4
    follow-up to Icfbf667ff3e629d
    
    Just like in I605e48955ffdc6dac we
    also get "Connection refused" when trying to connect
    to port 443 from external.
    
    While apache on 80 listens on both protocols,
    envoy which does TLS termination on 443, only listens on IPv4.
    
    Because by default prometheus will do both protocols
    this leads to alerts on alerts.wikimedia.org.
    
    Bug: T327976
    Bug: T331863
    Change-Id: I6e01339b0140a603c9002c9de2d9d9f35313bcf0

--- modules/profile/manifests/microsites/design.pp
+++ modules/profile/manifests/microsites/design.pp
@@ -52,6 +52,7 @@ class profile::microsites::design(
         severity           => 'task',
         path               => '/',
         force_tls          => true,
+        ip_families        => [ip4],
         body_regex_matches => ['Foundation Design'],
     }
 
