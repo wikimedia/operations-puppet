a47fdb78b4f3c85de8ec9714c98e777cb01baa51
Tim Starling <tstarling@wikimedia.org>
2023-03-16 11:20:15 +1100

    Unprovision the "swift" dashboard
    Disabling the web UI makes it more difficult to update dashboards and
    to make them useful for incident response.
    
    My updated dashboard is at
    https://grafana-rw.wikimedia.org/d/R8et-PaVk/swift-updated-unprovisioned
    
    Bug: T328872
    Change-Id: I6bb19d438a67a11162c005e5404bb072c7a989be

--- modules/profile/manifests/grafana/production.pp
+++ modules/profile/manifests/grafana/production.pp
@@ -45,8 +45,4 @@ class profile::grafana::production (
     grafana::dashboard { 'varnish-aggregate-client-status-codes':
         source => 'puppet:///modules/grafana/dashboards/varnish-aggregate-client-status-codes',
     }
-
-    grafana::dashboard { 'swift':
-        source => 'puppet:///modules/grafana/dashboards/swift',
-    }
 }
