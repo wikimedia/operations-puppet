3abf1524552175de31da7d355795f9ab4a1b5239
Valentin Gutierrez <vgutierrez@wikimedia.org>
2023-03-20 16:27:37 +0100

    haproxy: Allow specifying maxconn per backend
    Allow specifying a max amount of connections per backend
    
    Bug: T310609
    Change-Id: I3308eefa8710ef2718cd6fcca2e48bad6ce15102

--- modules/haproxy/types/backend.pp
+++ modules/haproxy/types/backend.pp
@@ -2,4 +2,5 @@ type Haproxy::Backend = Struct[{
     prefix  => Enum['unix', 'ipv4', 'ipv6'],
     address => Variant[Stdlib::Unixpath, Stdlib::Host],
     port    => Optional[Stdlib::Port],
+    maxconn => Optional[Integer[1]],
 }]
