29cf2c55e031c680e7c2b539a4796c744673b9fe
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2023-03-22 11:05:28 +0100

    monitoring: write node-exporter dpkg_success metric
    The metric will be used for alerting purposes
    
    Bug: T332764
    Change-Id: Ia0631db36177a5d5675f1b5ff0bc6a56789ebda9

--- modules/profile/files/monitoring/check_dpkg.sh
+++ modules/profile/files/monitoring/check_dpkg.sh
@@ -4,6 +4,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
 PROGNAME=$(basename "$0")
+METRIC_FILE="/var/lib/prometheus/node.d/dpkg.prom"
 
 print_usage() {
   echo "Usage: $PROGNAME"
@@ -18,6 +19,15 @@ print_help() {
   echo ""
   support
   exit 0
+}
+
+write_dpkg_success() {
+  status="$1"
+  cat > "$METRIC_FILE" <<EOF
+# HELP node_dpkg_success Indicate whether dpkg is functioning
+# TYPE node_dpkg_success gauge
+node_dpkg_success $status
+EOF
 }
 
 case "$1" in
@@ -40,6 +50,7 @@ case "$1" in
   *)
     if fuser -s /var/lib/dpkg/lock ; then
       echo "dpkg is running, not checking broken packages"
+      write_dpkg_success 1
       exit 0
     fi
     packagedata=$(dpkg -l | grep '^[uirph]' | grep -Ev '^(ii|rc)')
@@ -51,12 +62,15 @@ case "$1" in
       echo "$packagedata" | grep -qv '^hi'
       if test $? -eq 1; then
         echo DPKG WARNING dpkg reports held packages
+        write_dpkg_success 0
         exit 1
       fi
       echo DPKG CRITICAL dpkg reports broken packages
+      write_dpkg_success 0
       exit 2
     fi
     echo All packages OK
+    write_dpkg_success 1
     exit 0
     ;;
 esac
