8475a103cc23fa4eb352ca6767a7dd5dd35f66e0
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2023-03-23 18:25:36 +0100

    mediawiki::tlsproxy::yaml_defs: add error page to envoy
    This way, errors coming directly from envoy will have their error page
    populated. We might want to expand on the amount of information we return
    in the future.
    
    Bug: T287983
    Change-Id: Ia9e1413418abaea5e0587e5f2171940bcde7c991

--- modules/mediawiki/manifests/tlsproxy/yaml_defs.pp
+++ modules/mediawiki/manifests/tlsproxy/yaml_defs.pp
@@ -2,11 +2,16 @@ class mediawiki::tlsproxy::yaml_defs (
   Stdlib::Unixpath $path,
   Optional[Array[String]] $listeners,
 ) {
+  $errorpage = mediawiki::errorpage_content({
+    'footer'     => '<p>Original error: %LOCAL_REPLY_BODY% </p>',
+    'margin'     => '7vw auto 0 auto', # Envoy can't accept % signs in its string formats AFAICS
+    'margin_top' => '14vh'
+  })
   file { $path:
     ensure  => present,
     owner   => 'root',
     group   => 'root',
-    content => to_yaml({'discovery' => {'listeners' => $listeners}}),
+    content => to_yaml({'discovery' => {'listeners' => $listeners}, 'mesh' => {'error_page' => $errorpage}}),
     mode    => '0444',
   }
 }
