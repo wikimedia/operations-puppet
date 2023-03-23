21943129537031f1cbcc5780a4e16db6f367c824
Daniel Zahn <dzahn@wikimedia.org>
2023-03-22 18:39:36 -0700

    miscweb: add monitor for wikiworkshop.org
    Bug: T329587
    Change-Id: I989d61d173e0ee3e70e6defa4476ca673787e40c

--- modules/profile/manifests/microsites/wikiworkshop.pp
+++ modules/profile/manifests/microsites/wikiworkshop.pp
@@ -6,6 +6,16 @@ class profile::microsites::wikiworkshop {
         content => template('profile/wikiworkshop/apache-wikiworkshop.org.erb'),
     }
 
+    prometheus::blackbox::check::http { 'wikiworkshop.org':
+        team               => 'serviceops-collab',
+        severity           => 'task',
+        path               => '/',
+        ip_families        => ['ip4'],
+        force_tls          => true,
+        status_matches     => [200],
+        body_regex_matches => ['Wiki Workshop'],
+    }
+
     wmflib::dir::mkdir_p('/srv/org/wikimedia/wikiworkshop')
 
     git::clone { 'research/wikiworkshop':
