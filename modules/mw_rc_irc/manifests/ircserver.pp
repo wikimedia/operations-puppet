wikimedia/operations-puppet

commit d0e5bd791317ed1af9d98cf322a67e966307df74

C:mw_rc_irc::ircserver: Refresh ircd services on config changes
Send correct signals when config files changes based on wikitech
comments:
  https://wikitech-static.wikimedia.org/wiki/IRCD#Reloading_config

Also switch to systemd::service to make sure daemon-reload is run

Bug: T284052
Hosts: C:mw_rc_irc::ircserver
Change-Id: I78592c62a39e237920158be7c03c9ed5443e361b

modules/mw_rc_irc/manifests/ircserver.pp
@@ -6,50 +6,35 @@ class mw_rc_irc::ircserver {
     ensure_packages(['ircd-ratbox', 'irssi'])
 
     # public part of the ircd config
-    file { '/usr/etc/ircd.conf':
-        mode    => '0444',
-        owner   => 'irc',
-        group   => 'irc',
-        content => template('mw_rc_irc/ircd.conf.erb');
+    file {
+        default:
+            mode  => '0444',
+            owner => 'irc',
+            group => 'irc';
+        '/usr/etc/ircd.conf':
+            notify  => Service['ircd'],
+            content => template('mw_rc_irc/ircd.conf.erb');
+        '/usr/etc/auth.conf':
+            notify    => Service['ircd'],
+            show_diff => false,
+            content   => secret('mw_rc_irc/auth.conf');
+        '/usr/etc/operator.conf':
+            notify    => Service['ircd'],
+            show_diff => false,
+            content   => secret('mw_rc_irc/operator.conf');
+        '/usr/etc/ircd.motd':
+            notify  => Exec['reload ircd-motd'],
+            content => template('mw_rc_irc/motd.erb');
     }
 
-    # private config block for auth/allowed users
-    file { '/usr/etc/auth.conf':
-        mode      => '0444',
-        owner     => 'irc',
-        group     => 'irc',
-        content   => secret('mw_rc_irc/auth.conf'),
-        show_diff => false,
+    exec {'reload ircd-motd':
+        command     => '/usr/bin/systemctl kill --signalt=SIGUSR1 ircd',
+        refreshonly => true,
     }
 
-    # private config block for operators and their passwords
-    file { '/usr/etc/operator.conf':
-        mode      => '0444',
-        owner     => 'irc',
-        group     => 'irc',
-        content   => secret('mw_rc_irc/operator.conf'),
-        show_diff => false,
-    }
-
-    # message of the day / connect banner
-    file { '/usr/etc/ircd.motd':
-        mode    => '0444',
-        owner   => 'irc',
-        group   => 'irc',
-        content => template('mw_rc_irc/motd.erb');
-    }
-
-    file { '/etc/systemd/system/ircd.service':
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => 'puppet:///modules/mw_rc_irc/systemd/ircd.service',
-    }
-
-    service { 'ircd':
-        ensure   => running,
-        provider => 'systemd',
-        require  => File['/etc/systemd/system/ircd.service'],
+    systemd::service { 'ircd':
+        content        => file('mw_rc_irc/systemd/ircd.service'),
+        service_params => {'restart' => '/usr/bin/systemctl reload ircd'},
     }
 
     monitoring::service { 'ircd':

modules/mw_rc_irc/files/systemd/ircd.service
@@ -6,5 +6,6 @@ After=network.target
 User=irc
 Group=irc
 ExecStart=/usr/bin/ircd -foreground
+ExecReload=/usr/bin/kill -HUP $MAINPID
 [Install]
 WantedBy=multi-user.target

commit 554e7dff9dcb85812d9750510199b2b17dea8dfa

Fix typo in systemctl command
d0e5bd791317ed1af9d98cf322a67e966307df74 introduced a typo, the option
to be passed to systemctl kill should be  --signal

Bug: T331702
Change-Id: Ib429d2533add16741b572f9c39e859bdd62bdd98

modules/mw_rc_irc/manifests/ircserver.pp
@@ -28,7 +28,7 @@ class mw_rc_irc::ircserver {
     }
 
     exec {'reload ircd-motd':
-        command     => '/usr/bin/systemctl kill --signalt=SIGUSR1 ircd',
+        command     => '/usr/bin/systemctl kill --signal=SIGUSR1 ircd',
         refreshonly => true,
     }
 