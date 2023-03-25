10c80703aa36451d573c0be2a3af7124fef85032
Taavi Väänänen <hi@taavi.wtf>
2023-03-25 09:36:06 +0200

    dumps: properly absent enterprise timers
    For example clouddumps1001 has an older version of the rsync job which
    should not be there.
    
    Change-Id: I140ccd17347f41b9e968285c3a265f7c8988eebd

--- modules/dumps/manifests/web/enterprise.pp
+++ modules/dumps/manifests/web/enterprise.pp
@@ -39,35 +39,31 @@ class dumps::web::enterprise(
         group  => $group,
     }
 
-    if $is_primary_server {
-        $download_command = "/usr/bin/python3 ${script_path} --creds ${creds_path} --settings ${settings_path} --retries 2"
-        systemd::timer::job { 'download_enterprise_htmldumps':
-            ensure                  => present,
-            description             => 'Twice monthly download of Wikimedia Enterprise HTML dumps',
-            user                    => $user,
-            monitoring_enabled      => false,
-            send_mail               => true,
-            send_mail_only_on_error => false,
-            environment             => {'MAILTO' => 'ops-dumps@wikimedia.org'},
-            command                 => $download_command,
-            interval                => {'start' => 'OnCalendar', 'interval' => '*-*-1,20 8:30:0'},
-            require                 => [ File[$script_path], File[$creds_path], File[$settings_path] ],
-        }
+    $download_command = "/usr/bin/python3 ${script_path} --creds ${creds_path} --settings ${settings_path} --retries 2"
+    systemd::timer::job { 'download_enterprise_htmldumps':
+        ensure                  => $is_primary_server.bool2str('present', 'absent'),
+        description             => 'Twice monthly download of Wikimedia Enterprise HTML dumps',
+        user                    => $user,
+        monitoring_enabled      => false,
+        send_mail               => true,
+        send_mail_only_on_error => false,
+        environment             => {'MAILTO' => 'ops-dumps@wikimedia.org'},
+        command                 => $download_command,
+        interval                => {'start' => 'OnCalendar', 'interval' => '*-*-1,20 8:30:0'},
+        require                 => [ File[$script_path], File[$creds_path], File[$settings_path] ],
     }
 
-    if ($is_primary_server == false) {
-        # rsync the downloaded files to secondary host, allowing the rsync to take a full day
-        $rsync_command = "/usr/bin/rsync -a --bwlimit=160000 ${dumps_web_server}::data/xmldatadumps/public/other/enterprise_html/runs ${miscdumpsdir}/enterprise_html/"
-        systemd::timer::job { 'rsync_enterprise_htmldumps':
-            ensure             => present,
-            description        => 'Twice monthly rsync after download of Wikimedia Enterprise HTML dumps',
-            user               => root,
-            monitoring_enabled => false,
-            send_mail          => true,
-            environment        => {'MAILTO' => 'ops-dumps@wikimedia.org'},
-            command            => $rsync_command,
-            interval           => {'start' => 'OnCalendar', 'interval' => '*-*-2,21 8:30:0'},
-            require            => [ File[$script_path], File[$creds_path], File[$settings_path] ],
-        }
+    # rsync the downloaded files to secondary host, allowing the rsync to take a full day
+    $rsync_command = "/usr/bin/rsync -a --bwlimit=160000 ${dumps_web_server}::data/xmldatadumps/public/other/enterprise_html/runs ${miscdumpsdir}/enterprise_html/"
+    systemd::timer::job { 'rsync_enterprise_htmldumps':
+        ensure             => $is_primary_server.bool2str('absent', 'present'),
+        description        => 'Twice monthly rsync after download of Wikimedia Enterprise HTML dumps',
+        user               => root,
+        monitoring_enabled => false,
+        send_mail          => true,
+        environment        => {'MAILTO' => 'ops-dumps@wikimedia.org'},
+        command            => $rsync_command,
+        interval           => {'start' => 'OnCalendar', 'interval' => '*-*-2,21 8:30:0'},
+        require            => [ File[$script_path], File[$creds_path], File[$settings_path] ],
     }
 }
