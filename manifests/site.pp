94f896ea376521c9470661e7dca4033d96f1d833
Manuel Arostegui <marostegui@wikimedia.org>
2023-04-05 13:15:03 +0200

    mariadb: Add db1220 to x1
    Bug: T326669
    Change-Id: I06839d55da88a86ae9e721f1a70a2e2d82302e42

--- manifests/site.pp
+++ manifests/site.pp
@@ -490,7 +490,7 @@ node /^datahubsearch100[1-3]\.eqiad\.wmnet$/ {
 }
 
 # Hosts to be set up T325209
-node /^db1(208|209|210|211|212|213|214|215|216|217|218|219|220|221|222|223|224|225)\.eqiad\.wmnet$/ {
+node /^db1(208|209|210|211|212|213|214|215|216|217|218|219|221|222|223|224|225)\.eqiad\.wmnet$/ {
     role(insetup::data_persistence)
 }
 
@@ -692,7 +692,7 @@ node /^db2(137|138|167|168|169|170|171)\.codfw\.wmnet$/ {
 
 ## x1 shard
 # eqiad
-node /^db1(103|120|137|179)\.eqiad\.wmnet$/ {
+node /^db1(103|120|137|179|220)\.eqiad\.wmnet$/ {
     role(mariadb::core)
 }
 
