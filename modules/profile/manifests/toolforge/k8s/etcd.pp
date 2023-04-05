a385eb128be2805335deda575895351a30955be9
Taavi Väänänen <hi@taavi.wtf>
2023-04-05 10:49:15 +0300

    P:toolforge::k8s::etcd: load list of control nodes from PuppetDB
    Instead of relying on a static list of nodes in hiera, make the etcd
    profile query that from PuppetDB instead. This means that the only
    etcd-related thing the new 'add control node' cookbook has to do is to
    run Puppet on all of the etcd nodes.
    
    Unlike the HAProxy profile, where we want to be able to manually
    control which nodes are receiving traffic, here using PuppetDB works
    great since it includes nodes that are still being set up and nodes that
    are being removed, as both of those do still need etcd connectivity.
    
    Bug: T274499
    Change-Id: If873d6c8f90a7a3979b6209a735b9974cead1983

--- modules/profile/manifests/toolforge/k8s/etcd.pp
+++ modules/profile/manifests/toolforge/k8s/etcd.pp
@@ -1,12 +1,11 @@
 class profile::toolforge::k8s::etcd (
     Array[Stdlib::Fqdn] $peer_hosts    = lookup('profile::toolforge::k8s::etcd_nodes',   {default_value => ['localhost']}),
     Array[Stdlib::Fqdn] $checker_hosts = lookup('profile::toolforge::checker_hosts',     {default_value => ['tools-checker-03.tools.eqiad.wmflabs']}),
-    Array[Stdlib::Fqdn] $control_nodes = lookup('profile::toolforge::k8s::control_nodes',{default_value => ['localhost']}),
     Boolean             $bootstrap     = lookup('profile::etcd::cluster_bootstrap',      {default_value => false}),
 ) {
     class { '::profile::wmcs::kubeadm::etcd':
         peer_hosts    => $peer_hosts,
-        control_nodes => $control_nodes,
+        control_nodes => wmflib::role::hosts('wmcs::toolforge::k8s::control'),
         bootstrap     => $bootstrap,
     }
     contain '::profile::wmcs::kubeadm::etcd'
