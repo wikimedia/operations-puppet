e7ef30b9bb1590118d6d59543a2466d232830ffc
Taavi Väänänen <hi@taavi.wtf>
2023-04-09 11:52:46 +0300

    O:wmcs::nfs: remove primary_backup::misc and related classes
    Bug: T301280
    Change-Id: I3e500c3105b5d0edd96d3eee2d97a3921153d097

--- modules/role/manifests/wmcs/nfs/primary_backup/misc.pp
+++
@@ -1,9 +0,0 @@
-class role::wmcs::nfs::primary_backup::misc {
-    system::role { $name:
-        description => 'NFS shares primary backup (misc)',
-    }
-    include profile::base::production
-    include profile::wmcs::nfs::backup::primary::base
-    include profile::base::cloud_production
-    include profile::wmcs::nfs::backup::primary::misc
-}
