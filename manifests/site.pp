3b2a0acc4177b015d3a2135e80fe13cec62dcf2c
Daniel Zahn <dzahn@wikimedia.org>
2023-04-10 11:07:41 -0700

    miscweb: remove iegreview profile from role/hiera/tests
    Removes the iegreview profile from the miscweb role to decom
    the service from current miscweb backends.
    
    Along with it remove Hiera DB settings and the tests mapped
    to miscweb* in httpbb.
    
    Bug: T334415
    Change-Id: I1a8c197d1b3306562775358457b57314ab210311

--- manifests/site.pp
+++ manifests/site.pp
@@ -1533,7 +1533,6 @@ node /^karapace1001\.eqiad\.wmnet$/ {
 # virtual machines for misc. applications and static sites
 # replaced miscweb1001/2001 in T247648 and bromine/vega in T247650
 #
-# profile::iegreview                   # https://iegreview.wikimedia.org
 # profile::microsites::annualreport    # https://annual.wikimedia.org
 # profile::microsites::static_rt       # https://static-rt.wikimedia.org
 # profile::microsites::transparency    # https://transparency.wikimedia.org
