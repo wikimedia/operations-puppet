48b11ba0b0c1ba544260c8b4434d2e9f917e93c1
Eevans <eevans@wikimedia.org>
2023-04-10 21:46:50 +0000

    Revert "sessionstore: assign values to net.ipv4.conf.all.arp_{ignore,announce}"
    This reverts commit ae6ec291d5211969df5a3efa3ee4b4974f684e8c.
    
    Reason for revert: Tests complete; Restoring production
    settings.
    
    Change-Id: I713a095068a9e3a8e071c1c692bd33da12519e93

--- modules/profile/manifests/sessionstore.pp
+++ modules/profile/manifests/sessionstore.pp
@@ -1,18 +1,6 @@
 # sets up a service for session storage (T206016)
-class profile::sessionstore (
-    Integer   $arp_ignore   = lookup('profile::sessionstore::arp_ignore', {'default_value' => 0}),
-    Integer   $arp_announce = lookup('profile::sessionstore::arp_announce', {'default_value' => 0}),
-) {
+class profile::sessionstore {
 
     # needed for T219560
     class {'passwords::cassandra': }
-
-    # Experimental (to be removed after completion of T327954)
-    sysctl::parameters { 'sessionstore-test':
-        values   => {
-            'net.ipv4.conf.all.arp_ignore'   => $arp_ignore,
-            'net.ipv4.conf.all.arp_announce' => $arp_announce,
-        },
-        priority => 5,
-    }
 }
