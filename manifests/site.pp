b3654a5c869eb2bf8586bcaa03a94e0ea7498520
Manuel Arostegui <marostegui@wikimedia.org>
2023-04-11 09:31:36 +0200

    mariadb: Decommission db1103
    Bug: T332293
    Change-Id: I9907c8ab7a784cb07e7fb1179a76a6c33ecf6511

--- manifests/site.pp
+++ manifests/site.pp
@@ -692,7 +692,7 @@ node /^db2(137|138|167|168|169|170|171)\.codfw\.wmnet$/ {
 
 ## x1 shard
 # eqiad
-node /^db1(103|120|137|179|220)\.eqiad\.wmnet$/ {
+node /^db1(120|137|179|220)\.eqiad\.wmnet$/ {
     role(mariadb::core)
 }
 
