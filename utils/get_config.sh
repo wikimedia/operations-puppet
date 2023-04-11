6b4eb363fd6cd932bdfcd613c89c18e2616671ab
jbond <jbond@wikimedia.org>
2023-04-11 23:18:36 +0200

    environment: add environment.conf file and remove environments dir
    This is on preperation for the puppet 7 upgrade where we should specify
    the module path per environments, even if we don't use them.
    
    We keep the environments dir as thats currently used in prod to enable
    the get_config script im gussing something that you may only be able to
    set from environments,
    
        need to check that the environmnts.conf file is not going to causes
        issues production.  i think it should be ignored but ned ti check
    
    Change-Id: I583afcf0e7706a1e10ebaa9a9643390165f502e6

---
+++ utils/get_config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# SPDX-License-Identifier: Apache-2.0
+# We dont actully need the environment
+# environment=$1
+set -ue
+PATH=/usr/bin
+
+script_dir=$(dirname "$(realpath "$0")")
+repo_dir=$(realpath "${script_dir}/../../.git")
+git --git-dir "${repo_dir}" log -1 --pretty='(%h) %cn - %s'
