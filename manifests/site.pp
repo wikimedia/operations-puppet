6093eea8bfb71f24c862f8b768c4f78ee622cae3
Manuel Arostegui <marostegui@wikimedia.org>
2023-04-11 10:35:25 +0200

    mariadb: Productionize db1218
    db1218 will go to s1
    
    Bug: T326669
    Change-Id: I1ca44efd7b1a276fd38c09fdea324fa95d572581

--- manifests/site.pp
+++ manifests/site.pp
@@ -490,7 +490,7 @@ node /^datahubsearch100[1-3]\.eqiad\.wmnet$/ {
 }
 
 # Hosts to be set up T325209
-node /^db1(208|210|212|213|214|215|216|217|218|219|221|222|223|225)\.eqiad\.wmnet$/ {
+node /^db1(208|210|212|213|214|215|216|217|219|221|222|223|225)\.eqiad\.wmnet$/ {
     role(insetup::data_persistence)
 }
 
@@ -500,7 +500,7 @@ node /^dbproxy10(22|23|24|25|26|27)\.eqiad\.wmnet$/ {
 }
 
 # s1 (enwiki) core production dbs on eqiad
-node /^db1(106|107|118|119|128|132|134|135|163|169|184|186|206|207)\.eqiad\.wmnet$/ {
+node /^db1(106|107|118|119|128|132|134|135|163|169|184|186|206|207|218)\.eqiad\.wmnet$/ {
     role(mariadb::core)
 }
 
