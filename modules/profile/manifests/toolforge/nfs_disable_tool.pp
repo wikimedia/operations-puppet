24bd5a0e27467fcf26f1c7bafef6c89b208733b0
Andrew Bogott <abogott@wikimedia.org>
2023-04-12 14:12:20 -0500

    Add profile::toolforge::nfs_disable_tool
    This code was moved over from profile::wmcs::nfs::primary;
    that profile is no longer in use as we've moved tools nfs to
    a VM-based nfs server.
    
    Change-Id: I9c3b20cbdc5cd64de871953e78ad2215e17a4c63

---
+++ modules/profile/manifests/toolforge/nfs_disable_tool.pp
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# This profile should be applied to the primary nfs server.
+#  It detects tools scheduled for delete and archives their
+#  files.
+class profile::toolforge::nfs_disable_tool() {
+    require profile::toolforge::disable_tool
+
+    systemd::timer::job { 'disable-tool':
+        ensure          => 'present',
+        logging_enabled => false,
+        user            => 'root',
+        description     => 'Archive home dir of deleted or disabled+expired tools',
+        command         => '/srv/disable-tool/disable_tool.py archive',
+        interval        => {
+        'start'    => 'OnCalendar',
+        'interval' => '*:0/2', # every 2 minutes
+        },
+        require         => Class['::profile::toolforge::disable_tool'],
+    }
+}
