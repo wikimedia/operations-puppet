6c2f5305d8116fe10391910fd591b5eb255c79c8
Manuel Arostegui <marostegui@wikimedia.org>
2023-04-14 08:00:22 +0200

    mariadb: Remove db1107 from puppet
    Host being decommissioned.
    
    Bug: T334447
    Change-Id: I60084afc3142dab7a6149e033a921633d1bd468f

--- manifests/site.pp
+++ manifests/site.pp
@@ -499,7 +499,7 @@ node /^dbproxy10(22|23|24|25|26|27)\.eqiad\.wmnet$/ {
 }
 
 # s1 (enwiki) core production dbs on eqiad
-node /^db1(106|107|118|119|128|132|134|135|163|169|184|186|206|207|218)\.eqiad\.wmnet$/ {
+node /^db1(106|118|119|128|132|134|135|163|169|184|186|206|207|218)\.eqiad\.wmnet$/ {
     role(mariadb::core)
 }
 
