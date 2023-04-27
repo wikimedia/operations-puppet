509d94e535cb4a328fa61c4508da380a56f0c5fb
Luca Toscano <ltoscano@wikimedia.org>
2023-04-27 14:12:08 +0200

    cassandra: update 'dev' version to 3.11.14
    The current package version in the dev component seems to be
    3.11.14, and puppet refused to install cassandra on ml-cache1001
    (first bullseye node with dev component, now 3.x).
    
    Bug: T331712
    Change-Id: If269d43b04d36b9acb5ca8a6d32cb46ebe946a9e

--- modules/cassandra/manifests/init.pp
+++ modules/cassandra/manifests/init.pp
@@ -149,7 +149,7 @@ class cassandra (
     $package_version = $target_version ? {
         '2.2' => pick($version, '2.2.6-wmf5'),
         '3.x' => pick($version, '3.11.13'),
-        'dev' => pick($version, '3.11.13')
+        'dev' => pick($version, '3.11.14')
     }
 
     $component = $target_version  ? {
