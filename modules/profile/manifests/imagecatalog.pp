b4f0938516cd172dc8432068fc2c9b42caf80254
Janis Meybohm <jmeybohm@wikimedia.org>
2023-04-27 14:18:47 +0200

    profile::imagecatalog migrate from user token to client cert
    Replace the imagecatalog token with a client certificate
    
    Hosts: auto
    Bug: T325268
    Change-Id: Id2e98118d1065701e13e2c4c6c935922c35e2f3e

--- modules/profile/manifests/imagecatalog.pp
+++ modules/profile/manifests/imagecatalog.pp
@@ -1,24 +1,28 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::imagecatalog (
-    Hash[String, Hash[String, Hash]] $tokens = lookup('profile::kubernetes::infrastructure_users', { default_value => {} }),
-    Stdlib::Fqdn $deployment_server          = lookup('deployment_server'),
+    Stdlib::Fqdn $deployment_server = lookup('deployment_server'),
 ) {
     # Fetch clusters without aliases
     $kubernetes_clusters = k8s::fetch_clusters(false).map | String $name, K8s::ClusterConfig $config | {
-        $_tokens = $tokens[$config['cluster_group']]
-        $token = $_tokens['imagecatalog']
-        if ($token) {
-            if ($config['imagecatalog']) {
-                $kubeconfig_path = "/etc/kubernetes/imagecatalog-${name}.config"
-                k8s::kubeconfig { $kubeconfig_path:
-                    master_host => $config['master'],
-                    username    => 'imagecatalog',
-                    token       => $token['token'],
-                    owner       => 'imagecatalog',
-                    group       => 'imagecatalog',
-                }
-                [$name, $kubeconfig_path]
+        if ($config['imagecatalog']) {
+            $username = 'imagecatalog'
+
+            $auth_cert = profile::pki::get_cert($config['pki_intermediate_base'], $username, {
+                'renew_seconds'  => $config['pki_renew_seconds'],
+                'outdir'         => '/etc/kubernetes/pki',
+                # imagecatalog user does not have any organisation attributes (e.g. groups)
+                # attached as it is being granted specific (limited) rights via RBAC.
+            })
+
+            $kubeconfig_path = "/etc/kubernetes/imagecatalog-${name}.config"
+            k8s::kubeconfig { $kubeconfig_path:
+                master_host => $config['master'],
+                username    => 'imagecatalog',
+                auth_cert   => $auth_cert,
+                owner       => 'imagecatalog',
+                group       => 'imagecatalog',
             }
+            [$name, $kubeconfig_path]
         }
     }
     .filter |$v| { $v =~ NotUndef }       # and remove the undef entries for clusters where imagecatalog isn't enabled.
