28042645d3ca77c16ca04cdbf83787b61f62af97
Jesse Hathaway <jhathaway@wikimedia.org>
2023-05-02 15:50:37 -0500

    ssh: clamp lifetime_remaining_seconds to a value JRuby can accept
    Clamp lifetime_remaining_seconds to a value that Puppet on JRuby
    can support.
    
    Bug: T268344
    Change-Id: Ib0089d03c2cdbfdef4a44fb88cf7c32c6fbdaa7c

--- modules/ssh/lib/facter/ssh_ca_host_certificate.rb
+++ modules/ssh/lib/facter/ssh_ca_host_certificate.rb
@@ -16,9 +16,17 @@ Facter.add(:ssh_ca_host_certificate) do
     Dir.glob('/etc/ssh/*-cert.pub').each do |file_path|
       ssh_data = Net::SSH::KeyFactory.load_public_key(file_path)
 
+      # The OpenSSH certificate format interprets a valid_before time of 2^64-1
+      # (max for unsigned long) to mean "forever", but Puppet's max integer
+      # value is 9,223,372,036,854,775,807. Ensure valid_before_seconds is
+      # equal or less than Puppet's max value. Otherwise puppetserver's JRuby
+      # throws an error when accessing the facts hash:
+      #    https://github.com/net-ssh/net-ssh/pull/746
+      #    https://www.puppet.com/docs/puppet/7/lang_data_number.html#lang_data_number_integer_type
+      valid_before_seconds = [ssh_data.valid_before.to_i, 9_223_372_036_854_775_807].min
       hash[file_path] = {
         principals: ssh_data.valid_principals,
-        lifetime_remaining_seconds: ssh_data.valid_before.to_i - Time.now.to_i,
+        lifetime_remaining_seconds: valid_before_seconds - Time.now.to_i,
       }
     end
 
