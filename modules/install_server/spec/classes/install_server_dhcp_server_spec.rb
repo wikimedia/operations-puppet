25c9412a6df8082bace826f82fd73b11cc0c5a61
jbond <jbond@wikimedia.org>
2023-05-03 19:47:15 +0200

    install_server: improve readability of netmask logic
    Hosts: C:install_server::dhcp_server
    Change-Id: Ife2ace7dc4a50983059e056f38a90924220ae137

--- modules/install_server/spec/classes/install_server_dhcp_server_spec.rb
+++ modules/install_server/spec/classes/install_server_dhcp_server_spec.rb
@@ -19,6 +19,30 @@ describe 'install_server::dhcp_server', :type => :class do
             'recurse' => 'true',
           }
         )
+        is_expected.to contain_file('/etc/dhcp/automation.conf')
+          .without_content(/subnet/)
+      end
+      describe 'with managment networks' do
+        let(:params) { { mgmt_networks: {'eqiad' => ['10.0.0.0/22'], 'codfw' => ['10.0.4.0/23'] } } }
+        it do
+          is_expected.to contain_file('/etc/dhcp/automation.conf')
+            .with_content(%r{
+              subnet\s10.0.0.0\snetmask\s255.255.252.0\s\{\s+
+                authoratative;\s+
+                option\ssubnet-mask\s255.255.252.0;\s+
+                option\srouters\s10.0.0.1;\s+
+                option\sdomain-name\s"mgmt\.eqiad\.wmnet";\s+
+                include\s"/etc/dhcp/automation/proxies/mgmt-eqiad.conf";\n\}
+            }x)
+            .with_content(%r{
+              subnet\s10.0.4.0\snetmask\s255.255.254.0\s\{\s+
+                authoratative;\s+
+                option\ssubnet-mask\s255.255.254.0;\s+
+                option\srouters\s10.0.4.1;\s+
+                option\sdomain-name\s"mgmt\.codfw\.wmnet";\s+
+                include\s"/etc/dhcp/automation/proxies/mgmt-codfw.conf";\n\}
+            }x)
+        end
       end
     end
   end
