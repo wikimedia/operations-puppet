8c609e744529276f7f14999a2c37101c0bf3c718
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2023-05-04 17:16:13 +0200

    pontoon: install dhcp client
    Change-Id: I831ca3b35e02e529e158f6a26d6f163b597a7abb

--- modules/profile/manifests/pontoon/provider/cloud_vps.pp
+++ modules/profile/manifests/pontoon/provider/cloud_vps.pp
@@ -1,4 +1,9 @@
 # @summary Cloud VPS specific tweaks to the base Pontoon setup
 # SPDX-License-Identifier: Apache-2.0
 class profile::pontoon::provider::cloud_vps () {
+    # In production we're explicitly purging isc-dhcp-client (8d58ccdbc8be6)
+    # via 'profile::base::additional_purged_packages' variable.
+    # To avoid fully overriding the variable, install a *different* dhcp client
+    # here since it is required in Cloud VPS.
+    ensure_packages(['dhcpcd5'])
 }
