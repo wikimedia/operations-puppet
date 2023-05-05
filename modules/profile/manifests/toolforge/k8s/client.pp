ad19d1213ccac260f8b1d923d09ccd416396b602
Taavi Väänänen <hi@taavi.wtf>
2023-05-05 14:48:55 +0300

    P:toolforge::k8s::client: remove toolforge-webservice link
    This is provisioned by the package.
    
    Change-Id: Ib984c51c009e4ceadda2f669b2ed1424a847411f

--- modules/profile/manifests/toolforge/k8s/client.pp
+++ modules/profile/manifests/toolforge/k8s/client.pp
@@ -8,24 +8,23 @@ class profile::toolforge::k8s::client (
         ensure => latest,
     }
 
-    $extra_links = ['/usr/local/bin/toolforge-webservice']
-
     # Legacy locations for the entry point script from webservice
     # that are probably still hardcoded in some Tools.
-    $legacy_links = debian::codename::le('buster') ? {
-        true =>  [
+    if debian::codename::le('buster') {
+        file { [
             '/usr/local/bin/webservice2',
             '/usr/local/bin/webservice',
-        ],
-        false => [],
+        ]:
+            ensure => link,
+            target => '/usr/bin/webservice',
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+        }
     }
 
-    file { $extra_links + $legacy_links:
-        ensure => link,
-        target => '/usr/bin/webservice',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
+    file { '/usr/local/bin/toolforge-webservice':
+        ensure => absent,
     }
 
     file { '/etc/toolforge':
