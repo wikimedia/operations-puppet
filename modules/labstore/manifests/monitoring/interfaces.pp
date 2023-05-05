a9838b363131acc5c2d708415241332920aac2b7
David Caro <dcaro@wikimedia.org>
2023-05-05 09:55:36 +0200

    labstore: avoid paging for iowait/throughput alerts
    Given that it still lives in nagios, I found no easier way to disable
    the pages for us than avoiding the checks from going critical on
    nagios by raising the critical threshold.
    
    Took the chance too to update the links.
    
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: I88a0e14a747d0251a6de3c34713e00a1e9709c83

--- modules/labstore/manifests/monitoring/interfaces.pp
+++ modules/labstore/manifests/monitoring/interfaces.pp
@@ -12,49 +12,51 @@ class labstore::monitoring::interfaces(
     String $monitor_iface = 'eth0',
     String $contact_groups='wmcs-team,admins',
     Integer $int_throughput_warn = 937500000,  # 7.5Gbps
-    Integer $int_throughput_crit = 1062500000, # 8.5Gbps
 ){
 
     # In minutes, how long icinga will wait before considering HARD state, see also T188624
     $retries = 10
+    # Absurdly big number, to avoid pages ()
+    $int_throughput_crit = 106250000000 # 850Gbps
 
     monitoring::check_prometheus { 'network_out_saturated':
         description     => 'Outgoing network saturation',
-        dashboard_links => ['https://grafana.wikimedia.org/d/000000568/labstore1004-1005'],
+        dashboard_links => ['https://grafana.wikimedia.org/d/000000568/wmcs-dumps-general-view'],
         query           => "sum(irate(node_network_transmit_bytes_total{instance=\"${::hostname}:9100\",device=\"${monitor_iface}\"}[5m]))",
         warning         => $int_throughput_warn,
         critical        => $int_throughput_crit,
         retries         => $retries,
         method          => 'ge',
         contact_group   => $contact_groups,
-        notes_link      => 'https://wikitech.wikimedia.org/wiki/Portal:Data_Services/Admin/Labstore',
+        notes_link      => 'https://wikitech.wikimedia.org/wiki/Portal:Data_Services/Admin/Shared_storage#Dumps',
         prometheus_url  => "http://prometheus.svc.${::site}.wmnet/ops",
     }
 
     monitoring::check_prometheus { 'network_in_saturated':
         description     => 'Incoming network saturation',
-        dashboard_links => ['https://grafana.wikimedia.org/d/000000568/labstore1004-1005'],
+        dashboard_links => ['https://grafana.wikimedia.org/d/000000568/wmcs-dumps-general-view'],
         query           => "sum(irate(node_network_receive_bytes_total{instance=\"${::hostname}:9100\",device=\"${monitor_iface}\"}[5m]))",
         warning         => $int_throughput_warn,
         critical        => $int_throughput_crit,
         retries         => $retries,
         method          => 'ge',
         contact_group   => $contact_groups,
-        notes_link      => 'https://wikitech.wikimedia.org/wiki/Portal:Data_Services/Admin/Labstore',
+        notes_link      => 'https://wikitech.wikimedia.org/wiki/Portal:Data_Services/Admin/Shared_storage#Dumps',
         prometheus_url  => "http://prometheus.svc.${::site}.wmnet/ops",
     }
 
     monitoring::check_prometheus { 'high_iowait_stalling':
         description     => 'Persistent high iowait',
-        dashboard_links => ['https://grafana.wikimedia.org/d/000000568/labstore1004-1005'],
+        dashboard_links => ['https://grafana.wikimedia.org/d/000000568/wmcs-dumps-general-view'],
         # iowait % across all CPUs
         query           => "100 * sum(irate(node_cpu_seconds_total{instance=\"${::hostname}:9100\",mode=\"iowait\"}[5m])) / scalar(count(node_cpu_seconds_total{mode=\"idle\",instance=\"${::hostname}:9100\"}))",
         warning         => 5,
-        critical        => 10,
+        # Big number to avoid pages
+        critical        => 10000,
         retries         => $retries,
         method          => 'ge',
         contact_group   => $contact_groups,
-        notes_link      => 'https://wikitech.wikimedia.org/wiki/Portal:Data_Services/Admin/Labstore',
+        notes_link      => 'https://wikitech.wikimedia.org/wiki/Portal:Data_Services/Admin/Shared_storage#Dumps',
         prometheus_url  => "http://prometheus.svc.${::site}.wmnet/ops",
     }
 }
