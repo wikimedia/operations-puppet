2041849df2f43a0089e64d583a035a5e103f55c3
Taavi Väänänen <hi@taavi.wtf>
2023-05-05 14:46:12 +0300

    P:toolforge: fix toolforge-cli config file location
    Change-Id: Ib88c84187fdfbd84c00d9cfd1ce491766b7765ee

--- modules/profile/manifests/toolforge/bastion/toolforge_cli.pp
+++ modules/profile/manifests/toolforge/bastion/toolforge_cli.pp
@@ -1,18 +1,22 @@
 # SPDX-License-Identifier: Apache-2.0
-class profile::toolforge::bastion::toolforge_cli() {
+class profile::toolforge::bastion::toolforge_cli () {
+  $cli_config = {
+    'build' => {
+      'dest_repository' => "${::wmcs_project}-harbor.wmcloud.org",
+      'builder_image'   => "${::wmcs_project}-harbor.wmcloud.org/toolforge/heroku-builder-classic:22",
+    },
+  }
 
-    $cli_config = {
-        'build' => {
-            'dest_repository' => "${::wmcs_project}-harbor.wmcloud.org",
-            'builder_image' => "${::wmcs_project}-harbor.wmcloud.org/toolforge/heroku-builder-classic:22",
-          },
-    }
+  file { '/etc/toolforge.yaml':
+    ensure => absent,
+  }
 
-    # toolforge cli configuration file
-    file {'/etc/toolforge.yaml':
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0444',
-      content => $cli_config.to_yaml,
-    }
+  # toolforge cli configuration file
+  file { '/etc/toolforge-cli.yaml':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    content => $cli_config.to_yaml,
+  }
 }
