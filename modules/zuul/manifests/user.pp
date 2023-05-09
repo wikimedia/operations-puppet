167509ea96dfed9f9aa8f4866721548749c8d7ad
Antoine Musso <hashar@free.fr>
2023-05-09 17:57:20 +0200

    zuul: switch to fixed uid/gid 923
    The Zuul user is created by Puppet on three hosts:
    
    - contint1002.wikimedia.org spare (running zuul-merger)
    - contint2001.wikimedia.org primary
    - contint2002.wikimedia.org replacement for primary
    
    They currently all have the same uid/gid:
    
    uid=497(zuul) gid=498(zuul)
    
    They are provisionned by Puppet but the order might well change leading
    to uid/gid inconsistency. I have reserved uid/gid 923 for Zuul with
    I746c75973706668330a7a22d7362cf047bd2a271.
    
    The deployment would cause Zuul to be unavailable and thus entirely
    bring down CI which should be announced ahead of time.
    
    Stop the two affected services:
    
      sudo systemctl stop zuul-merger zuul-server
    
    Migrate the files owned by uid 497 to the new uid 923 in the
    directories:
    
      sudo chown -R 923:923 /srv/zuul/git /var/lib/zuul
      sudo chown root:root /var/lib/zuul/{.gitconfig,git-template*}
    
    Run Puppet:
    
      sudo puppet agent -tv
    
    Start the services:
    
    On contint2001:
    
      sudo systemctl start zuul-merger zuul-server
    
    On contint1002:
      sudo systemctl start zuul-merger
    
    The deployment can be done progressively by disabling Puppet on all
    three hosts. Then doing in order:
    
     contint2002.wikimedia.org (all services masked)
     contint1002.wikimedia.org (only zuul-merger used)
     contint2001.wikimedia.org the primary which has both zuul-merger and
    zuul-server
    
    Bug: T324659
    Hosts: contint2001.wikimedia.org
    Hosts: contint1002.wikimedia.org
    Hosts: contint2002.wikimedia.org
    Change-Id: Ia117f1985f305cbe1c153325a378e81162c5a2f7

--- modules/zuul/manifests/user.pp
+++ modules/zuul/manifests/user.pp
@@ -2,11 +2,13 @@ class zuul::user {
 
     group { 'zuul':
         ensure => present,
+        gid    => 923,
         name   => 'zuul',
         system => true,
     }
 
     user { 'zuul':
+        uid        => 923,
         home       => '/var/lib/zuul',
         shell      => '/bin/bash',
         managehome => true,
