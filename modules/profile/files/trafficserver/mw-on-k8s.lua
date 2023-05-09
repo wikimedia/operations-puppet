5b7c79faf1f1fdfda91bff502d70d5b0ae660cbe
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2023-05-09 10:57:10 +0200

    trafficserver: make mw-on-k8s use a config file
    Much like we did for multi-dc, make the mw-on-k8s module read a
    configuration file at runtime so that we avoid having to reload trafficserver
    every time we want to change something there.
    
    Also, refactor tests so that we can bypass loading the configuration file
    like we did in multi-dc.lua.
    
    Finally, bump tlslua to use python3 so that it can work on modern computers.
    
    Bug: T336037
    Change-Id: I3d51952aa005a4b1a06a5fe813610760f3145dd8

--- modules/profile/files/trafficserver/mw-on-k8s.lua
+++ modules/profile/files/trafficserver/mw-on-k8s.lua
@@ -6,6 +6,12 @@
 -- The JIT compiler is causing severe performance issues:
 -- https://phabricator.wikimedia.org/T265625
 jit.off(true, true)
+
+local config_read_time = nil
+-- Wikis on mw-on-k8s. They will be read from mw-on-k8s.lua.conf
+local wikis_on_k8s = {}
+
+
 -- We're mapping the results from multi-dc.lua as we want to process if we're going to k8s *after*
 -- multi-dc has been determined
 local onprem_to_k8s_cluster_map = {
@@ -14,12 +20,38 @@ local onprem_to_k8s_cluster_map = {
   ['api-ro.discovery.wmnet'] = {host = 'mw-api-ext-ro.discovery.wmnet', port = 4447},
   ['api-rw.discovery.wmnet'] = {host = 'mw-api-ext.discovery.wmnet', port = 4447}
 }
--- TODO: add a config file like for multi-dc at a later point in time if
--- we want to change this often
-local wikis_on_k8s = {
-  ['test2.wikipedia.org'] = true, -- T290536#843499
-  ['test.wikidata.org'] = true -- T331268
-}
+
+-- Read the configuration file and return the resulting table
+local function read_config()
+  local configfile = ts.get_config_dir() .. "/lua/mw-on-k8s.lua.conf"
+  local conf = dofile(configfile)
+  if (type(conf) ~= "table") then
+      ts.error("mw-on-k8s.lua: invalid config file")
+      return {}
+  end
+  return conf
+end
+
+-- Reload the config every 10 seconds.
+--
+-- In ATS 8, Lua modules are never reloaded, you have to restart the server.
+-- In ATS 10, there is documentation to the effect that Lua modules may be
+-- reloaded if remap.config was touched. Maybe it just means if the plugin
+-- parameters were changed.
+--
+-- Note that with 256 states, read_config() will receive an average of 25.6
+-- calls per second. But it takes <1ms for a small file.
+local function reload_config()
+  local now = ts.now()
+  if config_read_time == nil or now - config_read_time > 10 then
+      config_read_time = now
+      -- only reload the configuration if it's valid
+      local conf = read_config()
+      if conf ~= {} then
+        wikis_on_k8s = conf
+      end
+  end
+end
 
 local function use_k8s()
   -- For now, we just check by wiki
@@ -28,7 +60,7 @@ local function use_k8s()
     return false
   end
   local host = host_raw:lower()
-
+  reload_config()
   local do_use_k8s = wikis_on_k8s[host]
 
   if do_use_k8s ~= nil then
--- modules/profile/files/trafficserver/mw-on-k8s_test.lua
+++ modules/profile/files/trafficserver/mw-on-k8s_test.lua
@@ -1,47 +1,98 @@
 -- SPDX-License-Identifier: Apache-2.0
-_G.ts = {
+local file_name = debug.getinfo(1, "S").source:sub(1)
+local base_dir = (file_name:reverse():match("/([^@]*)") or ""):reverse()
+local mw_on_k8s_file = loadfile(base_dir .. "/mw-on-k8s.lua")
+
+local function make_ts(request)
+  ts = {
     client_request = {
-      header = {},
-      get_url_host = function() return "api-ro.discovery.wmnet" end
+      get_url_host = function() return request.backend end
     },
-    http = {}
+    http = {},
+    now = function() return os.clock() end,
+    get_config_dir = function() return base_dir end,
+    error = function(msg) error(msg) end
+  }
+  if request.host ~= nil then
+    ts.client_request.header = {Host = request.host}
+  else
+    ts.client_request.header = {}
+  end
+  ts.client_request.set_url_host = function(host) ts.client_request.mapped_host = host end
+  ts.client_request.set_url_port = function(port) ts.client_request.mapped_port = port end
+  return ts
+end
+
+local function run(request, config)
+  local result = {}
+  _G.ts = make_ts(request)
+  _G.dofile = function ()
+    return config
+  end
+
+  mw_on_k8s_file()
+  result.remap_value = do_remap()
+  result.host = _G.ts.client_request.mapped_host
+  result.port = _G.ts.client_request.mapped_port
+  return result
+end
+
+local default_config = {
+  ["test2.wikipedia.org"] = true,
+  ["test.wikidata.org"] = true
 }
+
 describe("Busted unit testing framework", function()
   describe("script for ATS Lua Plugin", function()
     it("test - do_remap without a host header", function()
-      require("mw-on-k8s")
+      result = run({
+          host = nil,
+          backend = "api-ro.discovery.wmnet",
+        },
+        default_config
+      )
 
-      assert.are.equals(TS_LUA_REMAP_NO_REMAP, do_remap())
+      assert.are.equals(TS_LUA_REMAP_NO_REMAP, result.remap_value)
+      assert.are.equals(result.host, nil)
+      assert.are.equals(result.port, nil)
     end)
 
+
     it("test - api RO request with a matching host header", function()
-      stub(ts.client_request, "set_url_host")
-      stub(ts.client_request, "set_url_port")
-      _G.ts.client_request.header = {Host = "test2.wikipedia.org"}
-      _G.ts.client_request.get_url_host = function() return "api-ro.discovery.wmnet" end
-      assert.are.equals(TS_LUA_REMAP_DID_REMAP, do_remap())
-      assert.stub(ts.client_request.set_url_host).was.called_with("mw-api-ext-ro.discovery.wmnet")
-      assert.stub(ts.client_request.set_url_port).was.called_with(4447)
+      result = run({
+          host = 'test2.wikipedia.org',
+          backend = 'api-ro.discovery.wmnet'
+        },
+        default_config
+      )
+      assert.are.equals(TS_LUA_REMAP_DID_REMAP, result.remap_value)
+      assert.are.equals(result.host, "mw-api-ext-ro.discovery.wmnet")
+      assert.are.equals(result.port, 4447)
     end)
 
     it("test - appserver RW with a matching host header", function()
-      stub(ts.client_request, "set_url_host")
-      stub(ts.client_request, "set_url_port")
-      _G.ts.client_request.header = {Host = "test.wikidata.org"}
-      _G.ts.client_request.get_url_host = function() return "appservers-rw.discovery.wmnet" end
-      assert.are.equals(TS_LUA_REMAP_DID_REMAP, do_remap())
-      assert.stub(ts.client_request.set_url_host).was.called_with("mw-web.discovery.wmnet")
-      assert.stub(ts.client_request.set_url_port).was.called_with(4450)
+      result = run({
+          host = 'test.wikidata.org',
+          backend = 'appservers-rw.discovery.wmnet'
+        },
+        default_config
+      )
+      assert.are.equals(TS_LUA_REMAP_DID_REMAP, result.remap_value)
+      assert.are.equals(result.host, "mw-web.discovery.wmnet")
+      assert.are.equals(result.port, 4450)
     end)
 
     it("test - appserver RO with a non-matching host header", function()
-      stub(ts.client_request, "set_url_host")
-      stub(ts.client_request, "set_url_port")
-      _G.ts.client_request.header = {Host = "fr.wikipedia.org"}
-      _G.ts.client_request.get_url_host = function() return "appservers-ro.discovery.wmnet" end
-      assert.are.equals(TS_LUA_REMAP_DID_REMAP, do_remap())
-      assert.stub(ts.client_request.set_url_host).was_not.called()
-      assert.stub(ts.client_request.set_url_port).was_not.called()
+      result = run({
+            host = 'fr.wikipedia.org',
+            backend = 'appservers-rw.discovery.wmnet'
+          },
+          default_config
+      )
+
+      assert.are.equals(TS_LUA_REMAP_NO_REMAP, result.remap_value)
+      assert.are.equals(result.host, nil)
+      assert.are.equals(result.port, nil)
     end)
   end)
 end)
