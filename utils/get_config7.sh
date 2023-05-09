933061dec2b9336b44e0b699833dd09c3265b48e
jbond <jbond@wikimedia.org>
2023-05-09 13:41:35 +0200

    utils: terminate flags to prevent ambiguity
    Change-Id: Id5a527b6b6b3a7480d32908a724acbcbb452e574

--- utils/get_config7.sh
+++ utils/get_config7.sh
@@ -5,4 +5,4 @@ PATH=/usr/bin
 
 environment=$1
 git_dir=/srv/git/operations/puppet/.git
-git --git-dir="${git_dir}" log -1 --pretty='(%h) %cn - %s' --first-parent "${environment}"
+git --git-dir="${git_dir}" log -1 --pretty='(%h) %cn - %s' --first-parent "${environment}" --
