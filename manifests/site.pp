833874c7a656616b34f34755341776bc6264369f
Amir Sarabadani <ladsgroup@gmail.com>
2023-05-10 12:05:54 +0200

    Remove db1113 from puppet
    Being decommissioned
    
    Bug: T336029
    Change-Id: I67980d1a0a91b2f180ab1592fdab0d8634314d0f

--- manifests/site.pp
+++ manifests/site.pp
@@ -609,7 +609,7 @@ node 'db2155.codfw.wmnet' {
 }
 
 # s5 (default, dewiki and others) core production dbs on eqiad
-# See also db1113, db1213 and db1144 below
+# See also db1213 and db1144 below
 node /^db1(130|183|185|200|210)\.eqiad\.wmnet$/ {
     role(mariadb::core)
 }
@@ -631,7 +631,7 @@ node 'db2128.codfw.wmnet' {
 }
 
 # s6 (frwiki, jawiki, ruwiki) core production dbs on eqiad
-# See also db1113, db1213 below
+# See also db1213 below
 node /^db1(131|168|173|180|187|201|224)\.eqiad\.wmnet$/ {
     role(mariadb::core)
 }
@@ -696,7 +696,7 @@ node 'db2164.codfw.wmnet' {
 }
 
 # multi-instance hosts with multiple shards
-node /^db1(113|144|146|170|213)\.eqiad\.wmnet$/ {
+node /^db1(144|146|170|213)\.eqiad\.wmnet$/ {
     role(mariadb::core_multiinstance)
 }
 node /^db2(137|138|167|168|169|170|171)\.codfw\.wmnet$/ {
