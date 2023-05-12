5d20bedfd0c252d142317f326242f37960c83f18
Ssingh <ssingh@wikimedia.org>
2023-05-12 15:44:26 +0000

    Revert "O:traffic: Add lvs::kernel_config during insetup to allow reimages"
    This reverts commit d044d1a0ecded87bc2c9e5bf7c7517fbd8baf898.
    
    Reason for revert: lvs2012 is in a weird state because of the failed
    lvs::balancer reimaging, so let's revert it to a clean insetup state for
    now so that we can try to reimage it again
    
    Change-Id: I255ee8d82987ff7496c802d5f7389344e5b409b1

--- modules/role/manifests/insetup/traffic.pp
+++ modules/role/manifests/insetup/traffic.pp
@@ -7,9 +7,4 @@ class role::insetup::traffic {
 
     include profile::base::production
     include profile::base::firewall
-    # If we are an LVS host the install the appropriate sysctls so we can
-    # complete reimaging. See T336428
-    if $facts['networking']['fqdn'] =~ /^lvs[\d]{4}/ {
-        include lvs::kernel_config  # lint:ignore:wmf_styleguide
-    }
 }
