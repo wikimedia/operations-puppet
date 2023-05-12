3791b473d965473e941a110834d76347a8e0e1a0
Daniel Zahn <dzahn@wikimedia.org>
2023-05-12 13:28:55 -0700

    site: remove gerrit1001 from gerrit role, rm hiera host data
    Move the server back to the "insetup" role at the end of
    its lifecycle.
    
    This will remove gerrit specific configs and also
    shell access for gerrit admins.
    
    Data from /srv/gerrit and /var/lib/gerrit2/ has been copied
    to gerrit1003 and added to Bacula as well.
    
    All the hiera data is cleaned up too.
    
    But it does NOT yet kill the file system or shut the server
    down.
    
    Bug: T336427
    Change-Id: I142b87ea128946f7c2276c6f6c3cc3d930e7afff

--- manifests/site.pp
+++ manifests/site.pp
@@ -381,8 +381,13 @@ node /^clouddumps100[12]\.wikimedia\.org$/ {
 }
 
 # All gerrit servers (swap master status in hiera)
-node 'gerrit1001.wikimedia.org', 'gerrit1003.wikimedia.org', 'gerrit2002.wikimedia.org' {
+node 'gerrit1003.wikimedia.org', 'gerrit2002.wikimedia.org' {
     role(gerrit)
+}
+
+# old gerrit server, about to be decom'ed forever
+node 'gerrit1001..wikimedia.org' {
+    role(insetup::serviceops_collab)
 }
 
 # Zookeeper and Etcd discovery service nodes
