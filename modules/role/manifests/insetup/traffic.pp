3886ad09fa68ad5115b5f75248727e27e37f71b5
Brandon Black <bblack@wikimedia.org>
2023-05-12 15:57:10 +0000

    insetup::traffic: properly handle LVS case
    Basically, if we're going to get this process to be race-free and
    support re-roling straight from insetup -> balancer roles, we have
    to both blacklist iptables /and/ prevent applying the base ferm
    puppetization, in the case that insetup::traffic is running on a
    future LVS host.
    
    Bug: T336428
    Change-Id: I917c482a60bb9520fab6557d62a5f6330e5f7006

--- modules/role/manifests/insetup/traffic.pp
+++ modules/role/manifests/insetup/traffic.pp
@@ -6,5 +6,16 @@ class role::insetup::traffic {
     }
 
     include profile::base::production
-    include profile::base::firewall
+
+    if $facts['networking']['fqdn'] =~ /^lvs[\d]{4}/ {
+        # If we are an LVS host, bring in the LVS kernel config to get iptables
+        # blacklisted in modprobe, and use the no_firwall base, so that it's
+        # compatible with re-roling straight into lvs::balancer.  See also
+        # T336428.
+        include ::profile::base::no_firewall
+        include lvs::kernel_config  # lint:ignore:wmf_styleguide
+    } else {
+        # Otherwise, do it the normal way
+        include profile::base::firewall
+    }
 }
