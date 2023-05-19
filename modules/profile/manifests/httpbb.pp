f977f8479e2ff1e95c32193e7ec8cfc298abcbc2
Daniel Zahn <dzahn@wikimedia.org>
2023-05-19 09:00:59 -0700

    httpbb: add tests for CI, https://integration.wikimedia.org
    Test if:
    
    - http://integration.wikimedia.org does protocol redirect
    - /zuul/ is a 200 and contains "Zuul Status"
    - /ci/ is a 200 and contains "Jenkins"
    - /coverage/ redirects to doc.wikimedia.org
    
    Bug: T326891
    Change-Id: I8b7723d23abfad8c1684145e40136a4297389bc1

--- modules/profile/manifests/httpbb.pp
+++ modules/profile/manifests/httpbb.pp
@@ -48,6 +48,7 @@ class profile::httpbb (
             '/srv/deployment/httpbb-tests/releases',
             '/srv/deployment/httpbb-tests/gitlab',
             '/srv/deployment/httpbb-tests/vrts',
+            '/srv/deployment/httpbb-tests/contint',
             '/srv/deployment/httpbb-tests/gerrit',
             '/srv/deployment/httpbb-tests/noc',
             '/srv/deployment/httpbb-tests/doc',
@@ -102,6 +103,9 @@ class profile::httpbb (
     }
     httpbb::test_suite {'vrts/test_vrts.yaml':
         source => 'puppet:///modules/profile/httpbb/vrts/test_vrts.yaml'
+    }
+    httpbb::test_suite {'contint/test_integration.yaml':
+        source => 'puppet:///modules/profile/httpbb/contint/test_integration.yaml'
     }
     httpbb::test_suite {'gerrit/test_gerrit.yaml':
         source => 'puppet:///modules/profile/httpbb/gerrit/test_gerrit.yaml'
