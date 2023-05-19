71b7cd5115eca44086971b75fe3de16d68fb8da8
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-05-19 10:23:59 +0200

    Drop Boost packages from legacy package removal list for Bookworm
    Both Bullseye and Bookworm have 1.74, so this isn't necessary as
    upgrades will simply replace the existing packages.
    
    Bug: T330495
    Change-Id: Ie6706faec5d280597e1d48840fb4587198a554ac

--- modules/base/manifests/standard_packages.pp
+++ modules/base/manifests/standard_packages.pp
@@ -167,8 +167,7 @@ class base::standard_packages (
             # A dist upgrade to bookworm leaves some old binary packages around, remove those
             # Starting with bookworm inetutils-telnet provides telnet and "telnet" is a transition package
             $absent_packages = [
-                'libboost-filesystem1.74.0', 'libboost-locale1.74.0', 'libboost-log1.74.0', 'libboost-nowide1.74.0',
-                'libboost-program-options1.74.0', 'libboost-thread1.74.0', 'libicu67', 'libwsutil12', 'libwireshark14',
+                'libicu67', 'libwsutil12', 'libwireshark14',
                 'libwiretap11', 'ruby2.7', 'python3.9-minimal', 'python3.9', 'perl-modules-5.32', 'libpython3.9',
                 'libperl5.32', 'libpython3.9-minimal', 'libpython3.9-stdlib', 'libidn11', 'libldap-2.4-2',
                 'liburing1', 'libwebp6', 'libcbor0', 'libusb-0.1-4', 'telnet',
