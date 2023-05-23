3e7452aabb4da4929c0a44152aeb33f9d3907d0e
Ben Tullis <btullis@wikimedia.org>
2023-05-23 18:40:24 +0100

    Fix the script to install the spark3 yarn shuffler jar symlink
    The previous version of the script didn't work correctly because it left
    both versions of the symlink present. Also, we have not been including
    the jar since version 2 so we no longer need to move it to the backup
    directory, only remove the symlink.
    
    Bug: T332765
    Change-Id: I7db69e34bb929ab418728d21b5d58d7730da595b

--- modules/profile/files/hadoop/spark3/spark3_yarn_shuffle_jar_install.sh
+++ modules/profile/files/hadoop/spark3/spark3_yarn_shuffle_jar_install.sh
@@ -2,12 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 #
-# Removes all Spark 1 and 2 spark-*yarn-shuffle.jar from /usr/lib/hadoop-yarn/lib and then
-# symlinks the one from /usr/lib/spark3/jars into /usr/lib/hadoop-yarn/lib
+# Removes the spark2-yarn-shuffle.jar symlink from /usr/lib/hadoop-yarn/lib and then
+# creates another symlink to the version in /usr/lib/spark3/jars
 #
 
 yarn_lib_dir='/usr/lib/hadoop-yarn/lib'
-yarn_lib_removed_dir='/usr/lib/hadoop-yarn/lib.removed'
 
 spark3_yarn_shuffle_jar=$(find /usr/lib/spark3/yarn -name 'spark*yarn-shuffle.jar' | head -n 1)
 
@@ -16,22 +15,17 @@ if [ ! -f $spark3_yarn_shuffle_jar ]; then
     exit 1
 fi
 
+# If the symlink to the spark2-yarn-shuffler jar exists, delete it.
+if [ -L $yarn_lib_dir/spark2-yarn-shuffle.jar ]; then
+    rm $yarn_lib_dir/spark2-yarn-shuffle.jar
+fi
+
 # If the version spark3-yarn-shuffle.jar already points at the currently installed version
 # of Spark 3's spark yarn shuffle jar, then we don't need to do anything else.
 if [ $spark3_yarn_shuffle_jar -ef $yarn_lib_dir/spark3-yarn-shuffle.jar ]; then
     echo "No need to install spark3-yarn-shuffle.jar symlink it already points at $spark3_yarn_shuffle_jar."
     exit 0
 fi
-
-# Create a  README file and directory in which to store removed jar files.
-if [ ! -f $yarn_lib_removed_dir/README ]; then
-    mkdir -p $yarn_lib_removed_dir
-    echo "Files here were originally in $yarn_lib_dir, but have been moved so they no longer are loaded by YARN" > $yarn_lib_removed_dir/README
-fi
-
-
-echo "Moving YARN Spark 2 spark yarn shuffle jar(s) out of $yarn_lib_dir"
-mv -v $yarn_lib_dir/spark-*yarn-shuffle.jar $yarn_lib_removed_dir/
 
 echo "Symlinking $spark3_yarn_shuffle_jar to $yarn_lib_dir/spark3-yarn-shuffle.jar"
 ln -sfv $spark3_yarn_shuffle_jar $yarn_lib_dir/spark3-yarn-shuffle.jar
