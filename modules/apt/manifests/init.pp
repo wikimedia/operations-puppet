3c523978bc0f824fb03db0980fd153ffd36f82c9
Jesse Hathaway <jhathaway@wikimedia.org>
2023-05-23 16:18:06 -0500

    bookworm: Change to deb822 format for sources.list
    Beginning with Debian bookworm the default format for sources.list
    is deb822^[1]. This patch adds support for the deb822 format and
    makes a couple of notable changes:
    
    1. Uses deb822 as the default format for bookworm and newer
    2. Uses the apt::repository define type to configure debian.sources on
       bookworm
    3. drops the `comment_old` option, I could not determine why this
       was ever needed.
    
    I would love to validate our templated out deb822 file, but I didn't
    find a validation method in stock debian.
    
    [1]: https://www.mail-archive.com/debian-devel@lists.debian.org/msg370529.html
    
    Hosts: C:apt
    Bug: T330495
    Change-Id: I3c0ffba3d8d7cf2ed696bd8922bae1c0fa931cf7

--- modules/apt/manifests/init.pp
+++ modules/apt/manifests/init.pp
@@ -42,30 +42,65 @@ class apt(
     }
 
     if $manage_apt_source {
+        if debian::codename::ge('bookworm') {
+            # Starting with bookworm there's a new non-free-firmware component
+            # and deb822 is the default format, sources.list(5)
+            file { '/etc/apt/sources.list':
+                ensure  => absent,
+            }
+            $debian_sources='/etc/apt/sources.list.d/debian.sources'
+            concat { $debian_sources:
+                mode    => '0444',
+                owner   => 'root',
+                group   => 'root',
+                require => Apt::Repository['wikimedia'],
+            }
+            apt::repository { 'debian':
+                uri           => 'http://mirrors.wikimedia.org/debian',
+                dist          => $facts['os']['distro']['codename'],
+                components    => 'main contrib non-free non-free-firmware',
+                concat_target => $debian_sources,
+                keyfile_path  => '/usr/share/keyrings/debian-archive-keyring.gpg',
+                notify        => Exec['apt-get update'],
+            }
+            apt::repository { 'debian-security':
+                uri           => 'http://security.debian.org/debian-security',
+                dist          => "${facts['os']['distro']['codename']}-security",
+                components    => 'main contrib non-free non-free-firmware',
+                concat_target => $debian_sources,
+                keyfile_path  => '/usr/share/keyrings/debian-archive-keyring.gpg',
+                notify        => Exec['apt-get update'],
+            }
+            apt::repository { 'debian-updates':
+                uri           => 'http://mirrors.wikimedia.org/debian',
+                dist          => "${facts['os']['distro']['codename']}-updates",
+                components    => 'main contrib non-free non-free-firmware',
+                keyfile_path  => '/usr/share/keyrings/debian-archive-keyring.gpg',
+                concat_target => $debian_sources,
+                notify        => Exec['apt-get update'],
+            }
+        } else {
+            # Starting with bullseye, the security suite moved from
+            #   foo/updates to foo-security (since the former was confusingly
+            #   similar to foo-updates (what was called volatile.debian.org
+            #   in the past)
+            # Stretch has been removed, so the apt config only ships stub entries
+            if debian::codename::eq('bullseye') {
+                $apt_template    = 'apt/base-apt-conf-bullseye.erb'
+            } elsif debian::codename::eq('stretch') {
+                $apt_template    = 'apt/base-apt-conf-stretch.erb'
+            } elsif debian::codename::eq('buster') {
+                $apt_template    = 'apt/base-apt-conf-buster.erb'
+            }
 
-        # Starting with bookworm there's a new non-free-firmware component
-        # Starting with bullseye, the security suite moved from
-        #   foo/updates to foo-security (since the former was confusingly
-        #   similar to foo-updates (what was called volatile.debian.org
-        #   in the past)
-        # Stretch has been removed, so the apt config only ships stub entries
-        if debian::codename::eq('bullseye') {
-            $apt_template    = 'apt/base-apt-conf-bullseye.erb'
-        } elsif debian::codename::eq('bookworm') {
-            $apt_template    = 'apt/base-apt-conf-bookworm.erb'
-        } elsif debian::codename::eq('stretch') {
-            $apt_template    = 'apt/base-apt-conf-stretch.erb'
-        } elsif debian::codename::eq('buster') {
-            $apt_template    = 'apt/base-apt-conf-buster.erb'
-        }
-
-        file { '/etc/apt/sources.list':
-            ensure  => file,
-            mode    => '0555',
-            owner   => 'root',
-            group   => 'root',
-            content => template($apt_template),
-            require => Apt::Repository['wikimedia'],
+            file { '/etc/apt/sources.list':
+                ensure  => file,
+                mode    => '0555',
+                owner   => 'root',
+                group   => 'root',
+                content => template($apt_template),
+                require => Apt::Repository['wikimedia'],
+            }
         }
     }
 
@@ -124,11 +159,10 @@ class apt(
     }
 
     apt::repository { 'wikimedia':
-        uri         => 'http://apt.wikimedia.org/wikimedia',
-        dist        => "${::lsbdistcodename}-wikimedia",
-        components  => $components,
-        comment_old => true,
-        keyfile     => $wikimedia_apt_keyfile,
+        uri        => 'http://apt.wikimedia.org/wikimedia',
+        dist       => "${::lsbdistcodename}-wikimedia",
+        components => $components,
+        keyfile    => $wikimedia_apt_keyfile,
     }
 
     if debian::codename::ge('bullseye') and $use_private_repo and !$facts['is_virtual']{
@@ -147,10 +181,9 @@ class apt(
 
     if debian::codename::ge('buster'){
         apt::repository { 'debian-backports':
-            uri         => 'http://mirrors.wikimedia.org/debian/',
-            dist        => "${::lsbdistcodename}-backports",
-            components  => 'main contrib non-free',
-            comment_old => true,
+            uri        => 'http://mirrors.wikimedia.org/debian/',
+            dist       => "${::lsbdistcodename}-backports",
+            components => 'main contrib non-free',
         }
     }
 
--- modules/apt/manifests/repository.pp
+++ modules/apt/manifests/repository.pp
@@ -1,24 +1,31 @@
 define apt::repository(
-    Optional[Stdlib::HTTPUrl]          $uri         = undef,
-    Optional[String]                   $dist        = undef,
-    Optional[String]                   $components  = undef,
-    Boolean                            $bin         = true,
-    Boolean                            $source      = true,
-    Boolean                            $comment_old = false,
-    Optional[Pattern[/\.(asc|gpg)\z/]] $keyfile     = undef,
-    Wmflib::Ensure                     $ensure      = present,
-    Boolean                            $trust_repo  = false,
+    Optional[Stdlib::HTTPUrl]          $uri           = undef,
+    Optional[String]                   $dist          = undef,
+    Optional[String]                   $components    = undef,
+    Boolean                            $bin           = true,
+    Boolean                            $source        = true,
+    Optional[Pattern[/\.(asc|gpg)\z/]] $keyfile       = undef,
+    Optional[Stdlib::Unixpath]         $keyfile_path  = undef,
+    Wmflib::Ensure                     $ensure        = present,
+    Boolean                            $trust_repo    = false,
+    Optional[Stdlib::Unixpath]         $concat_target = undef,
 ) {
     if $ensure == 'present' and ! ($uri and $dist and $components) {
       fail('uri, dist and component are all required if ensure =>  present')
     }
+
+    if $keyfile and $keyfile_path {
+        fail('Only one of keyfile and keyfile_path may be specified')
+    }
+
     if $trust_repo {
+        $kpath = undef
         $trustedline = '[trusted=yes] '
     } elsif $keyfile {
-        $repo_key = $keyfile.basename
+        $kpath = "/etc/apt/keyrings/${$keyfile.basename}"
 
-        if !defined(File["/etc/apt/keyrings/${repo_key}"]) {
-            file { "/etc/apt/keyrings/${repo_key}":
+        if !defined(File[$kpath]) {
+            file { $kpath:
                 ensure => stdlib::ensure($ensure, 'file'),
                 owner  => 'root',
                 group  => 'root',
@@ -28,38 +35,65 @@ define apt::repository(
             }
         }
 
-        $trustedline = "[signed-by=/etc/apt/keyrings/${repo_key}] "
+        $trustedline = "[signed-by=${kpath}] "
+    } elsif $keyfile_path {
+        $kpath = $keyfile_path
+        $trustedline = "[signed-by=${kpath}] "
     } else {
+        $kpath = undef
         $trustedline = ''
     }
 
-    $binline = $bin ? {
-        true    => "deb ${trustedline}${uri} ${dist} ${components}\n",
-        default => '',
-    }
-    $srcline = $source ? {
-        true    => "deb-src ${trustedline}${uri} ${dist} ${components}\n",
-        default => '',
+    if $source {
+        $types = ['deb', 'deb-src']
+    } else {
+        $types = ['deb']
     }
 
-    file { "/etc/apt/sources.list.d/${name}.list":
-        ensure  => stdlib::ensure($ensure, 'file'),
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0444',
-        content => "${binline}${srcline}",
-        notify  => Exec['apt-get update'],
-    }
+    if debian::codename::ge('bookworm') {
+        $deb_sources = epp('apt/sources-deb822.epp', {
+            components => $components,
+            keyfile    => $kpath,
+            suites     => $dist,
+            trust_repo => $trust_repo,
+            types      => $types,
+            url        => $uri,
+        })
 
-    if $comment_old {
-        $escuri = regsubst(regsubst($uri, '/', '\/', 'G'), '\.', '\.', 'G')
-        $binre = "deb(-src)?\s+${escuri}\s+${dist}\s"
-
-        # comment out the old entries in /etc/apt/sources.list
-        exec { "apt-${name}-sources":
-            command => "/bin/sed -ri '/${binre}/s/^deb/#deb/' /etc/apt/sources.list",
-            creates => "/etc/apt/sources.list.d/${name}.list",
-            before  => File["/etc/apt/sources.list.d/${name}.list"],
+        if $concat_target {
+            concat::fragment { $name:
+                target  => $concat_target,
+                content => "${deb_sources}\n",
+            }
+        } else {
+            file { "/etc/apt/sources.list.d/${name}.sources":
+                ensure  => stdlib::ensure($ensure, 'file'),
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0444',
+                content => $deb_sources,
+                notify  => Exec['apt-get update'],
+            }
+        }
+    } else {
+        if $concat_target {
+            fail('concat_target only supported on bookworm and newer.')
+        }
+        $binline = $bin ? {
+            true    => "deb ${trustedline}${uri} ${dist} ${components}\n",
+            default => '',
+        }
+        $srcline = $source ? {
+            true    => "deb-src ${trustedline}${uri} ${dist} ${components}\n",
+            default => '',
+        }
+        file { "/etc/apt/sources.list.d/${name}.list":
+            ensure  => stdlib::ensure($ensure, 'file'),
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0444',
+            content => "${binline}${srcline}",
+            notify  => Exec['apt-get update'],
         }
     }
 }
