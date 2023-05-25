e9141f89faae7dcf854e9d493fa8f5c32d6acc4b
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2023-05-25 12:39:09 +0200

    cloudcontrol2005-dev: move to the new network setup
    This makes a cloudcontrol2005-dev a backend of cloudlb.
    
    The FQDN cloudcontrol2005-dev.wikimedia.org no longer exists, but we gain 2 new:
     * cloudcontrol2005-dev.codfw.wmnet
     * cloudcontrol2005-dev.private.codfw.wikimedia.cloud
    
    Bug: T336564
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: I51273f47f354c275db4537e6c6749bdbf3140164

--- modules/role/manifests/wmcs/openstack/codfw1dev/control.pp
+++ modules/role/manifests/wmcs/openstack/codfw1dev/control.pp
@@ -3,8 +3,8 @@ class role::wmcs::openstack::codfw1dev::control {
     include profile::base::production
     include profile::base::firewall
     include profile::base::cloud_production
-    if $facts['hostname'] == 'cloudcontrol2001-dev' {
-        # cloudlb: this host is already behind cloudlb
+    if $facts['hostname'] != 'cloudcontrol2004-dev' {
+        # cloudlb: the only host not behind cloudlb is cloudcontrol2004-dev
         include profile::wmcs::cloud_private_subnet
     }
     include profile::openstack::codfw1dev::observerenv
@@ -35,8 +35,8 @@ class role::wmcs::openstack::codfw1dev::control {
     include profile::openstack::codfw1dev::neutron::common
     include profile::openstack::codfw1dev::neutron::service
 
-    if $facts['hostname'] != 'cloudcontrol2001-dev' {
-        # cloudlb: the other cloudcontrols are in the old setup
+    if $facts['hostname'] == 'cloudcontrol2004-dev' {
+        # cloudlb: the only host in the old setup is cloudcontrol2004-dev
         include profile::openstack::codfw1dev::haproxy
         include profile::prometheus::haproxy_exporter
     }
