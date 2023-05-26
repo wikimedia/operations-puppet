b5d0e1f538e966e85d25de39249092980e37f7f5
Jesse Hathaway <jhathaway@wikimedia.org>
2023-05-26 16:30:09 -0500

    dev env: get_config support for dev
    In the dev environment we may have a dirty tree, so for now always
    return dev.
    
    Bug: T337972
    Hosts: basic
    Change-Id: Ie1e8c9ca11d0eca37a5c91dd29b8eaf94fd35f1b

--- utils/get_config7.sh
+++ utils/get_config7.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 # SPDX-License-Identifier: Apache-2.0
 set -ue
 PATH=/usr/bin
 
 environment=$1
-git_dir=/srv/git/operations/puppet/.git
-git --git-dir="${git_dir}" log -1 --pretty='(%h) %cn - %s' --first-parent "${environment}" --
+
+if [[ $environment != 'dev' ]]; then
+	git_dir=/srv/git/operations/puppet/.git
+	git --git-dir="${git_dir}" log -1 --pretty='(%h) %cn - %s' --first-parent "${environment}" --
+else
+	# In the dev environment we may have a dirty tree, so for now always
+	# return dev
+	printf 'dev\n'
+fi
