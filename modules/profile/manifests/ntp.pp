e883c30ef4f7e8833bfc7b755a2d41ad3e0b9b53
Brandon Black <bblack@wikimedia.org>
2023-06-02 15:36:18 +0000

    ntp: revamp and refresh our "tos" options a bit
    * In the past, we have twice forgotten to add 2 to the maxclock
      number, (when eqsin was provisioned, and when the authdns
      machines became dnsbox machines) making it a total of 4 less
      than it should be at the core sites prior to this commit (!!!).
      This commit switches to a calculated number that's currently
      correct and will automatically account for changes to the peer
      counts in the future.
    
    * minsane can be bumped to 3 at the core sites now, given we have
      more local and other-core-dc peers at our disposal.
    
    Change-Id: I0e212ef2dc21e2c5dad8cb6e7dbe545b83db01fd

--- modules/profile/manifests/ntp.pp
+++ modules/profile/manifests/ntp.pp
@@ -41,31 +41,46 @@ class profile::ntp (
     $wmf_server_upstream_pools = ["0.${pool_zone}.pool.ntp.org"]
     $wmf_server_upstreams = []
 
-    # Extra config only for our servers (not clients):
-    # minsane 2 requires 2 (default: 1) sane upstreams/peers present to be in sync
-    # maxclock 14 is to better support our large-ish "peer" lists at cores
-    #        (if we set it this high non-core, it results in way too many pool peers)
-    #        (as we add more edge DCs, we'll need to bump this value by 2 each time)
-    #        (could abstract this as $count_ntp_peers + 4)
+    ### Extra "tos" config for our servers:
+
+    # minsane <N> - the number of acceptably-working pool-servers + peers we
+    # must be syncing with to consider *ourselves* to be a reliable source for
+    # others. These numbers can be bikeshedded a bit, but the default of 1 is
+    # lower than we'd like.  Setting it too high can break time sync in some
+    # otherwise-survivable scenarios.  The cores have more local peers between
+    # them and greater reliability in general, so they can tolerate a slightly
+    # higher number than the edges.
+    $minsane = $::site ? {
+        eqiad   => 3,
+        codfw   => 3,
+        default => 2,
+    }
+
     # orphan <stratum> - if no internet servers are reachable, our servers will
     #     operate as an orphaned peer island and maintain some kind of stable
     #     sync with each other.  Without this, if all of our global servers
     #     lost their upstreams, within a few minutes we'd have no time syncing
-    #     happening at all ("peer" only protects you from some servers losing
+    #     happening at all ("peer" only protects you from *some* servers losing
     #     upstreams, not all).  A plausible scenario here would be some global
     #     screwup of pool.ntp.org DNS ops.  So set cores to do the orphan job.
-    $extra_config = $::site ? {
-        eqiad   => 'tos maxclock 16 minsane 2 orphan 12',
-        codfw   => 'tos maxclock 16 minsane 2 orphan 12',
-        default => 'tos minsane 2 orphan 13',
+    $orphan = $::site ? {
+        eqiad   => 12,
+        codfw   => 12,
+        default => 13,
     }
+
+    # maxclock - This needs to be the sum of:
+    #     * The count of servers in wmf_server_peers for this host
+    #     * The number (4) we want to use from the "pool" DNS lookup
+    #     * One extra to account for the dummy "0.X.pool.ntp.org" entry
+    $maxclock = length($wmf_server_peers) + 4 + 1
 
     ntp::daemon { 'server':
         servers      => $wmf_server_upstreams,
         pools        => $wmf_server_upstream_pools,
         peers        => $wmf_server_peers,
         time_acl     => $our_networks_acl,
-        extra_config => $extra_config,
+        extra_config => "tos minsane ${minsane} orphan ${orphan} maxclock ${maxclock}",
         query_acl    => $monitoring_hosts,
     }
 
