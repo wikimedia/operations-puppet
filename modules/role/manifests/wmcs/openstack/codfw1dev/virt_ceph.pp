c5cb22131895efa37f59d300cbb2c71d202be74b
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2023-06-05 11:53:48 +0200

    cloudvirt: codfw1dev: add cloud_private_subnet
    Connect and setup the cloudvirt hypervisors for the cloud_private subnet.
    
    Bug: T338125
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: I9ac68657578c2c75a7f484015c95952e5f0675f3

--- modules/role/manifests/wmcs/openstack/codfw1dev/virt_ceph.pp
+++ modules/role/manifests/wmcs/openstack/codfw1dev/virt_ceph.pp
@@ -5,6 +5,7 @@ class role::wmcs::openstack::codfw1dev::virt_ceph {
     include profile::base::production
     # include profile::firewall
     include profile::base::cloud_production
+    include profile::wmcs::cloud_private_subnet
     include profile::cloudceph::client::rbd_libvirt
     include profile::openstack::codfw1dev::clientpackages
     include profile::openstack::codfw1dev::observerenv
