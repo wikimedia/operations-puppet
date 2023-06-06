879eb9b9c062b602768df46b5aeabe728187aed3
Andrew Bogott <abogott@wikimedia.org>
2023-06-06 15:57:19 -0500

    cloud-vps VMs: don't install py2 mwopenstackclients on py3 distros
    Bug: T338188
    Change-Id: I8053fc09618c9279f0b182a44d20536f3f0ea8c2

--- modules/openstack/manifests/clientpackages/vms/common.pp
+++ modules/openstack/manifests/clientpackages/vms/common.pp
@@ -16,6 +16,15 @@ class openstack::clientpackages::vms::common(
             'python-netaddr',
         ]
         ensure_packages($py2packages)
+
+        # Wrapper python class to easily query openstack clients
+        file { '/usr/lib/python2.7/dist-packages/mwopenstackclients.py':
+            ensure => 'present',
+            source => 'puppet:///modules/openstack/clientpackages/py2/mwopenstackclients.py',
+            mode   => '0755',
+            owner  => 'root',
+            group  => 'root',
+        }
     }
 
     $py3packages = [
@@ -37,14 +46,6 @@ class openstack::clientpackages::vms::common(
     ]
     ensure_packages($otherpackages)
 
-    # Wrapper python class to easily query openstack clients
-    file { '/usr/lib/python2.7/dist-packages/mwopenstackclients.py':
-        ensure => 'present',
-        source => 'puppet:///modules/openstack/clientpackages/py2/mwopenstackclients.py',
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
-    }
     file { '/usr/lib/python3/dist-packages/mwopenstackclients.py':
         ensure => 'present',
         source => 'puppet:///modules/openstack/clientpackages/mwopenstackclients.py',
