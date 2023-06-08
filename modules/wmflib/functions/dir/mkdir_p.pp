3bf457802d931fa8d98ef3a17cd56b1e54ce80e7
Jesse Hathaway <jhathaway@wikimedia.org>
2023-06-08 14:05:38 -0500

    wmflib::dir::mkdir_p: exclude FHS dirs
    FHS dirs are known to exist, so no need to manage them.
    
    Hosts: basic
    Change-Id: Ibf4e17fe8cf838eb633d7c79e779704da4d558cd

--- modules/wmflib/functions/dir/mkdir_p.pp
+++ modules/wmflib/functions/dir/mkdir_p.pp
@@ -18,11 +18,56 @@ function wmflib::dir::mkdir_p(
     Variant[Stdlib::Unixpath, Array[Stdlib::Unixpath]] $dirs,
     Hash                                               $params = {},
 ) {
+    # FHS dirs generated via:
+    # $ MANWIDTH=100 man file-hierarchy |
+    #   sed -e 's/\/, /\n       /g' -e 's/arch-id/x86_64-linux-gnu/' |
+    #   grep '^       /.*/$' |
+    #   sed -Ee "s/ *(.*)\/$/        '\1',/g" |
+    #   sort -u
+    $fhs_dirs = [
+        '/boot',
+        '/dev',
+        '/dev/shm',
+        '/efi',
+        '/etc',
+        '/home',
+        '/lib',
+        '/lib64',
+        '/lib/x86_64-linux-gnu',
+        '/proc',
+        '/proc/sys',
+        '/root',
+        '/run',
+        '/run/log',
+        '/run/user',
+        '/srv',
+        '/sys',
+        '/sys/fs/cgroup',
+        '/tmp',
+        '/usr',
+        '/usr/bin',
+        '/usr/include',
+        '/usr/lib',
+        '/usr/sbin',
+        '/usr/share',
+        '/usr/share/doc',
+        '/usr/share/factory/etc',
+        '/usr/share/factory/var',
+        '/var',
+        '/var/cache',
+        '/var/lib',
+        '/var/log',
+        '/var/run',
+        '/var/spool',
+        '/var/tmp',
+    ]
+
     $_dirs = wmflib::dir::normalise($dirs)
-    $parents = wmflib::dir::split($_dirs) - $_dirs
+    # Exclude FHS dirs as they are known to exist
+    $parents = wmflib::dir::split($_dirs) - $_dirs - $fhs_dirs
     # ensure all parent directories exist
     ensure_resource('file', $parents, {'ensure' => 'directory'})
     # Apply params only to the actual directories
-    ensure_resource('file', $dirs, {'ensure' => 'directory'} + $params)
+    ensure_resource('file', $dirs, {'ensure'    => 'directory'} + $params)
 }
 
--- modules/wmflib/spec/functions/wmflib_dir_mkdir_p_spec.rb
+++ modules/wmflib/spec/functions/wmflib_dir_mkdir_p_spec.rb
@@ -5,38 +5,45 @@ describe 'wmflib::dir::mkdir_p' do
     is_expected.to run.with_params('/')
     expect(catalogue).to contain_file('/').with_ensure('directory')
   end
-  it "with '/etc'" do
-    is_expected.to run.with_params('/etc')
-    expect(catalogue).not_to contain_file('/').with_ensure('directory')
-    expect(catalogue).to contain_file('/etc').with_ensure('directory')
-  end
+  # check whether we rmove FHS dirs
   it "with '/etc/foo'" do
     is_expected.to run.with_params('/etc/foo')
     expect(catalogue).not_to contain_file('/').with_ensure('directory')
-    expect(catalogue).to contain_file('/etc').with_ensure('directory')
+    expect(catalogue).not_to contain_file('/etc').with_ensure('directory')
     expect(catalogue).to contain_file('/etc/foo').with_ensure('directory')
   end
-  it "with '/etc/foo/'" do
-    is_expected.to run.with_params('/etc/foo/', {'owner' => 'foo'})
+  it "with '/baz'" do
+    is_expected.to run.with_params('/baz')
     expect(catalogue).not_to contain_file('/').with_ensure('directory')
-    expect(catalogue).to contain_file('/etc').with_ensure('directory').without_owner
-    expect(catalogue).to contain_file('/etc/foo').with_ensure('directory').with_owner('foo')
+    expect(catalogue).to contain_file('/baz').with_ensure('directory')
   end
-  it "with '/etc/foo', '/foo'" do
-    is_expected.to run.with_params(['/etc/foo', '/foo'])
+  it "with '/baz/foo'" do
+    is_expected.to run.with_params('/baz/foo')
     expect(catalogue).not_to contain_file('/').with_ensure('directory')
-    expect(catalogue).to contain_file('/etc').with_ensure('directory')
-    expect(catalogue).to contain_file('/etc/foo').with_ensure('directory')
+    expect(catalogue).to contain_file('/baz').with_ensure('directory')
+    expect(catalogue).to contain_file('/baz/foo').with_ensure('directory')
+  end
+  it "with '/baz/foo/'" do
+    is_expected.to run.with_params('/baz/foo/', {'owner' => 'foo'})
+    expect(catalogue).not_to contain_file('/').with_ensure('directory')
+    expect(catalogue).to contain_file('/baz').with_ensure('directory').without_owner
+    expect(catalogue).to contain_file('/baz/foo').with_ensure('directory').with_owner('foo')
+  end
+  it "with '/baz/foo', '/foo'" do
+    is_expected.to run.with_params(['/baz/foo', '/foo'])
+    expect(catalogue).not_to contain_file('/').with_ensure('directory')
+    expect(catalogue).to contain_file('/baz').with_ensure('directory')
+    expect(catalogue).to contain_file('/baz/foo').with_ensure('directory')
     expect(catalogue).to contain_file('/foo').with_ensure('directory')
   end
-  it "with '/etc/foo', '/foo' and custom params" do
-    is_expected.to run.with_params(['/etc/foo', '/foo'], {'owner' => 'foo', 'mode' => '0500'})
+  it "with '/baz/foo', '/foo' and custom params" do
+    is_expected.to run.with_params(['/baz/foo', '/foo'], {'owner' => 'foo', 'mode' => '0500'})
     expect(catalogue).not_to contain_file('/').with_ensure('directory')
-    expect(catalogue).to contain_file('/etc')
+    expect(catalogue).to contain_file('/baz')
       .with_ensure('directory')
       .without_owner
       .without_mode
-    expect(catalogue).to contain_file('/etc/foo')
+    expect(catalogue).to contain_file('/baz/foo')
       .with_ensure('directory')
       .with_owner('foo')
       .with_mode('0500')
@@ -45,35 +52,35 @@ describe 'wmflib::dir::mkdir_p' do
       .with_owner('foo')
       .with_mode('0500')
   end
-  it "with '/etc/foo', '/etc/bar/' and custom params" do
-    is_expected.to run.with_params(['/etc/foo', '/etc/bar/'], {'owner' => 'foo', 'mode' => '0500'})
+  it "with '/baz/foo', '/baz/bar/' and custom params" do
+    is_expected.to run.with_params(['/baz/foo', '/baz/bar/'], {'owner' => 'foo', 'mode' => '0500'})
     expect(catalogue).not_to contain_file('/').with_ensure('directory')
-    expect(catalogue).to contain_file('/etc')
+    expect(catalogue).to contain_file('/baz')
       .with_ensure('directory')
       .without_owner
       .without_mode
-    expect(catalogue).to contain_file('/etc/foo')
+    expect(catalogue).to contain_file('/baz/foo')
       .with_ensure('directory')
       .with_owner('foo')
       .with_mode('0500')
-    expect(catalogue).to contain_file('/etc/bar')
+    expect(catalogue).to contain_file('/baz/bar')
       .with_ensure('directory')
       .with_owner('foo')
       .with_mode('0500')
   end
-  it "with '/etc/foo', '//etc/bar' and custom params" do
-    is_expected.to run.with_params('/etc/foo', {'owner' => 'foo', 'mode' => '0500'})
-    is_expected.to run.with_params('/etc/bar', {'owner' => 'foo', 'mode' => '0500'})
+  it "with '/baz/foo', '//baz/bar' and custom params" do
+    is_expected.to run.with_params('/baz/foo', {'owner' => 'foo', 'mode' => '0500'})
+    is_expected.to run.with_params('/baz/bar', {'owner' => 'foo', 'mode' => '0500'})
     expect(catalogue).not_to contain_file('/').with_ensure('directory')
-    expect(catalogue).to contain_file('/etc')
+    expect(catalogue).to contain_file('/baz')
       .with_ensure('directory')
       .without_owner
       .without_mode
-    expect(catalogue).to contain_file('/etc/foo')
+    expect(catalogue).to contain_file('/baz/foo')
       .with_ensure('directory')
       .with_owner('foo')
       .with_mode('0500')
-    expect(catalogue).to contain_file('/etc/bar')
+    expect(catalogue).to contain_file('/baz/bar')
       .with_ensure('directory')
       .with_owner('foo')
       .with_mode('0500')
