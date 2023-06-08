cf8cd96a67917a90851d0de95f599772065df296
Abijeet Patro <apatro@wikimedia.org>
2023-06-08 05:42:58 +0000

    TranslationNotifications: Run UnsubscribeInactiveUsers periodically
    Run UnsubscribeInactiveUsers once a quarter for different wikis
    
    Bug: T323192
    Change-Id: Ifd4cda34d7199b5d1c2c917acff47aa50e53782b

--- modules/profile/manifests/mediawiki/maintenance/translationnotifications.pp
+++ modules/profile/manifests/mediawiki/maintenance/translationnotifications.pp
@@ -3,13 +3,43 @@ class profile::mediawiki::maintenance::translationnotifications {
     # or just one which runs the scripts which iterates over
     # selected set of wikis?
 
+    # MetaWiki
     profile::mediawiki::periodic_job { 'translationnotifications-metawiki':
         command  => '/usr/local/bin/mwscript extensions/TranslationNotifications/scripts/DigestEmailer.php --wiki metawiki',
         interval => 'Mon 10:00',
     }
 
+    profile::mediawiki::periodic_job { 'translationnotifications-unsubscribeinactiveusers-metawiki':
+        command  => '/usr/local/bin/mwscript extensions/TranslationNotifications/maintenance/UnsubscribeInactiveUsers.php --wiki metawiki --days 365 --really',
+        interval => '* *-01,04,07,10-02 02:00:00',
+    }
+
+    # Mediawiki
     profile::mediawiki::periodic_job { 'translationnotifications-mediawikiwiki':
         command  => '/usr/local/bin/mwscript extensions/TranslationNotifications/scripts/DigestEmailer.php --wiki mediawikiwiki',
         interval => 'Mon 10:05',
+    }
+
+    profile::mediawiki::periodic_job { 'translationnotifications-unsubscribeinactiveusers-mediawikiwiki':
+        command  => '/usr/local/bin/mwscript extensions/TranslationNotifications/maintenance/UnsubscribeInactiveUsers.php --wiki mediawikiwiki --days 365 --really',
+        interval => '* *-01,04,07,10-02 03:30:00',
+    }
+
+    # Incubator
+    profile::mediawiki::periodic_job { 'translationnotifications-unsubscribeinactiveusers-incubator':
+        command  => '/usr/local/bin/mwscript extensions/TranslationNotifications/maintenance/UnsubscribeInactiveUsers.php --wiki incubatorwiki --days 365 --really',
+        interval => '* *-01,04,07,10-02 04:15:00',
+    }
+
+    # Wikimania
+    profile::mediawiki::periodic_job { 'translationnotifications-unsubscribeinactiveusers-wikimania':
+        command  => '/usr/local/bin/mwscript extensions/TranslationNotifications/maintenance/UnsubscribeInactiveUsers.php --wiki wikimaniawiki --days 365 --really',
+        interval => '* *-01,04,07,10-02 05:00:00',
+    }
+
+    # Commons
+    profile::mediawiki::periodic_job { 'translationnotifications-unsubscribeinactiveusers-commons':
+        command  => '/usr/local/bin/mwscript extensions/TranslationNotifications/maintenance/UnsubscribeInactiveUsers.php --wiki commonswiki --days 365 --really',
+        interval => '* *-01,04,07,10-02 05:30:00',
     }
 }
