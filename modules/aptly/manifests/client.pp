5d7a9bea823ccd10c6e2c88507882b5cc90360b2
Taavi Väänänen <hi@taavi.wtf>
2023-06-11 17:19:11 +0300

    aptly::client: configure unattended-upgrades
    Configure the aptly client class, used by the Toolforge and
    deployment-prep[0] WMCS projects only, to create unattended-upgrades
    configuration for the repositories it configures. This ensures that
    packages from the project apt repositories get automatically updated
    during the daily unattended-upgrades run, which I believe is a better
    option than leaving the packages outdated if the person deploying the
    new package forgot to upgrade some nodes.
    
    [0]: I believe the repo in deployment-prep is now unused as scap is no
         longer distributed as a .deb, so in practice this only affects
         Toolforge.
    
    Change-Id: I0b9924b0435c3d06c370084fe84907080b8204ba

--- modules/aptly/manifests/client.pp
+++ modules/aptly/manifests/client.pp
@@ -1,16 +1,16 @@
 # SPDX-License-Identifier: Apache-2.0
-class aptly::client(
-    $servername="${::wmcs_project}-packages.${::wmcs_project}.${::site}.wmflabs",
-    $source=false,
-    $components='main',
-    $protocol='http',
+class aptly::client (
+    Stdlib::Fqdn          $servername,
+    Array[String[1]]      $components   = ['main'],
+    Enum['http', 'https'] $protocol     = 'http',
+    Boolean               $auto_upgrade = true,
 ) {
     apt::repository { 'project-aptly':
         uri        => "${protocol}://${servername}/repo",
         dist       => "${::lsbdistcodename}-${::wmcs_project}",
-        components => $components,
-        source     => $source,
+        components => $components.join(' '),
         trust_repo => true,
+        source     => false,
     }
 
     # Pin it so it has higher preference
@@ -18,5 +18,13 @@ class aptly::client(
         package  => '*',
         pin      => "origin ${servername}",
         priority => 1500,
+    }
+
+    apt::conf { 'unattended-upgrades-aptly':
+        ensure   => $auto_upgrade.bool2str('present', 'absent'),
+        priority => '52',
+        # Key with trailing '::' to append to potentially existing entry
+        key      => 'Unattended-Upgrade::Origins-Pattern::',
+        value    => "site=${servername}",
     }
 }
--- modules/role/manifests/aptly/client.pp
+++ modules/role/manifests/aptly/client.pp
@@ -1,14 +1,17 @@
 # = Class: role::aptly::client
 #
 # Sets up a simple deb package that points to the project's aptly server
+# TODO: convert this to a profile
 class role::aptly::client(
-    $servername = "${::wmcs_project}-packages.${::wmcs_project}.${::site}.wmflabs",
-    $components = 'main',
-    $protocol = 'http',
+    Stdlib::Fqdn          $servername,
+    Array[String[1]]      $components   = ['main'],
+    Enum['http', 'https'] $protocol     = 'http',
+    Boolean               $auto_upgrade = true,
 ) {
-    class { '::aptly::client':
-        servername => $servername,
-        components => $components,
-        protocol   => $protocol,
+    class { 'aptly::client':
+        servername   => $servername,
+        components   => $components,
+        protocol     => $protocol,
+        auto_upgrade => $auto_upgrade,
     }
 }
