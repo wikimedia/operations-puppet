f5879c309ff2c4bf93e416315c83eb082ff132c8
Keith Herron <kherron@wikimedia.org>
2023-06-13 10:26:45 -0400

    pyrra: add pyrra::(api|filesystem) modules
    Baseline modules to install/manage pyrra api and filesystem services
    
    Bug: T302995
    Change-Id: I607ca9714fe12afd3231a311ffe82212d6123614

---
+++ modules/pyrra/manifests/api.pp
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: Apache-2.0
+# == Class: pyrra::api
+#
+# Pyrra frontend API/UI
+#
+# = Parameters
+# [*prometheus_url*] The URL to the Prometheus to query
+# [*api_url*] The URL to the API service like a Filesystem or Kubernetes Operator.
+
+class pyrra::api(
+    String $prometheus_url = 'https://thanos-query.discovery.wmnet',
+    String $api_url        = 'http://localhost:9444',
+){
+
+    ensure_packages(['pyrra'])
+
+    systemd::service { 'pyrra-api':
+        ensure         => present,
+        restart        => true,
+        override       => true,
+        content        => systemd_template('pyrra-api'),
+        service_params => {
+            enable     => true,
+            hasrestart => true,
+        },
+    }
+
+}
---
+++ modules/pyrra/manifests/filesystem.pp
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: Apache-2.0
+# == Class: pyrra::filesystem
+#
+# Pyrra filesystem operator and backend for the API.
+#
+# = Parameters
+# [*config_files*] The folder where Pyrra finds the config files to use.
+# [*prometheus_folder*] The folder where Pyrra writes the generated Prometheus rules and alerts.
+
+class pyrra::filesystem(
+    String $config_files      = '/etc/pyrra/config/*.yaml',
+    String $prometheus_folder = '/etc/pyrra/output-rules/',
+){
+
+    ensure_packages(['pyrra'])
+
+    file { $prometheus_folder:
+        ensure => directory,
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+    }
+
+    systemd::service { 'pyrra-filesystem':
+        ensure         => present,
+        restart        => true,
+        override       => true,
+        content        => systemd_template('pyrra-filesystem'),
+        service_params => {
+            enable     => true,
+            hasrestart => true,
+        },
+    }
+
+}
