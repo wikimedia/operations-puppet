99ffda469d62445d215d4b7571b3762e75dd4085
Jesse Hathaway <jhathaway@wikimedia.org>
2023-06-14 16:33:41 -0500

    dev env: rsyslog exporter, in container env listen on all interfaces
    Since we don't know what our ip will be, in a container environment,
    have the rsyslog metrics exporter listen on all interfaces.
    
    Hosts: basic
    Bug: T337972
    Change-Id: I36d3dec92aa033153a2c5769ab18b26e7cff3050

--- modules/prometheus/manifests/rsyslog_exporter.pp
+++ modules/prometheus/manifests/rsyslog_exporter.pp
@@ -1,6 +1,9 @@
 class prometheus::rsyslog_exporter (
     Wmflib::Ensure      $ensure = present,
-    Stdlib::IP::Address $listen_address = $facts['networking']['ip'],
+    Stdlib::IP::Address $listen_address = $facts['wmflib']['is_container'] ? {
+                                              true  => '0.0.0.0',
+                                              false => $facts['networking']['ip'],
+                                          },
     Stdlib::Port        $listen_port    = 9105,
 ) {
     package { 'prometheus-rsyslog-exporter':
