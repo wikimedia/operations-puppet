8a2eb3193c41118c11574d8a081d305844186fde
jbond <jbond@wikimedia.org>
2023-06-20 16:49:29 +0200

    puppetserver: increase memory in production
    Change-Id: I6433192081d62613197d7c9e9a2b30f72fc00060

--- modules/profile/manifests/puppetserver.pp
+++ modules/profile/manifests/puppetserver.pp
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # @param hiera_data_dir the default location for hiera data
 # @param hierarchy a hash of hierarchy to add to the hiera file
+# @param java_start_mem the value to use for the java args -Xms
+# @param java_max_mem the value to use for the java args -Xmx
 # @param code_dir the location where puppet looks for code
 # @param reports list of reports to configure
 # @param puppetdb_urls if present puppetdb will be configured using these urls
@@ -14,12 +16,15 @@
 # @param enable_ca indicate if the ca is enabled
 # @param intermediate_ca configure puppet Ca with an intermediate CA
 # @param ca_public_key location of the ihntermediate ca content
+# @param separate_ssldir use seperate ssldir for the server.  usefull in cloud setup
 # @param ca_crl location of the ihntermediate crl content
 # @param ca_private_key_secret the content of the W
 # @param git_pull whether to pull puppet code from git, defaults to true
 class profile::puppetserver (
     Stdlib::Unixpath               $code_dir              = lookup('profile::puppetserver::code_dir'),
     Stdlib::Unixpath               $hiera_data_dir        = lookup('profile::puppetserver::hiera_data_dir'),
+    Stdlib::Datasize               $java_start_mem       = lookup('profile::puppetserver::java_start_mem'),
+    Stdlib::Datasize               $java_max_mem         = lookup('profile::puppetserver::java_max_mem'),
     Array[Puppetserver::Hierarchy] $hierarchy             = lookup('profile::puppetserver::hierarchy'),
     Array[Puppetserver::Report,1]  $reports               = lookup('profile::puppetserver::reports'),
     Array[Stdlib::HTTPUrl]         $puppetdb_urls         = lookup('profile::puppetserver::puppetdb_urls'),
