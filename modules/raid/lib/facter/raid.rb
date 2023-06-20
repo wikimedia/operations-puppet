a113fc73d53f9c8c8c68855ac7dcbe1d7e88d21b
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-06-20 09:04:24 +0200

    Retire legacy "raid" fact
    This has been replaced by the "raid_mgmt_tools" fact in our tooling. In contrast
    to the legacy "raid" fact, the replacement operates solely on PCI IDs which allows
    for a more reliable mapping of RAID controllers towards the respective userspace
    tooling. The old approach based on heuristics on device names became more and more
    brittle.
    
    Bug: T313312
    Change-Id: I33d67889cc7cef5f4d4a152811a3c2091cab51e9

--- modules/raid/lib/facter/raid.rb
+++ modules/raid/lib/facter/raid.rb
@@ -30,48 +30,6 @@ Facter.add('raid_mgmt_tools') do
   end
 end
 
-Facter.add('raid') do
-  confine :kernel => :linux
-  # ref: http://pci-ids.ucw.cz/v2.2/pci.ids
-  pci_ids = {
-    # TODO: this is a bit of a hack the driver is still hpsa, but we need to use the newer ssacli tool
-    '9005028f' => 'ssacli' # Smart Storage PQI 12G SAS/PCIe 3
-  }
-  setcode do
-    raids = []
-
-    File.open('/proc/bus/pci/devices').each do |line|
-      words = line.split
-      raids.push(pci_ids[words[1]]) if pci_ids.key?(words[1])
-    end
-
-    if FileTest.exist?('/proc/mdstat')
-      IO.foreach('/proc/mdstat') do |x|
-        if x =~ /md[0-9]+ : active/
-          raids.push('md')
-          break
-        end
-      end
-    end
-
-    if FileTest.exist?('/dev/cciss/') || FileTest.exist?('/sys/module/hpsa/')
-      raids.push('hpsa') unless raids.include?('ssacli')
-    end
-
-    if FileTest.exist?('/dev/megadev0') ||
-       Dir.glob('/sys/bus/pci/drivers/megaraid_sas/00*').length > 0 # rubocop:disable Style/NumericPredicate
-      raids.push('megaraid')
-    end
-
-    if FileTest.exist?('/dev/mpt0') ||
-       FileTest.exist?('/proc/scsi/mptsas/0')
-      raids.push('mpt')
-    end
-
-    raids.sort.uniq
-  end
-end
-
 # Enable calling directly as a bypass for T251293, T320636
 if $PROGRAM_NAME == __FILE__
   require 'json'
