b40ebc3fde8a547153d730c3a302339e097cb75c
jbond <jbond@wikimedia.org>
2023-06-21 15:08:17 +0200

    netbox: expand device schema to include optional platform
    Related-To: Ib406983876dd9da4d9fecd68084532823d02db95
    
    Bug: T329669
    Change-Id: Ic15e6a8de8845bd44bafa60e060bb12520aac403

--- modules/netbox/types/device/network.pp
+++ modules/netbox/types/device/network.pp
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 type Netbox::Device::Network = Struct[{
-    ipv4               => Stdlib::IP::Address::V4,
-    primary_fqdn       => Stdlib::Fqdn,
-    role               => Netbox::Device::Network::Role,
-    site               => Wmflib::Sites,
-    Optional['alarms'] => Boolean,
-    Optional['ipv6']   => Stdlib::IP::Address::V6
+    ipv4                     => Stdlib::IP::Address::V4,
+    primary_fqdn             => Stdlib::Fqdn,
+    role                     => Netbox::Device::Network::Role,
+    site                     => Wmflib::Sites,
+    Optional['manufacturer'] => String[1],
+    Optional['alarms']       => Boolean,
+    Optional['ipv6']         => Stdlib::IP::Address::V6
 }]
