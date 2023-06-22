d349a1b6f45c1a9e0fd84427b7aa7cb15b4fe55a
jelto <jwodstrcil@wikimedia.org>
2023-06-22 10:51:29 +0200

    miscweb: lower certificate_expiry_days to 9 days
    Default expiry days for blackbox check cert alert is 10 days. Sometimes
    renewal dips below 10 days. So this change reduces the expiry
    days to 9 days.
    
    Bug: T339862
    Change-Id: I09f9e276b08be59a3d0d7e4fc9da5a3c24eb0a60

--- modules/profile/manifests/microsites/monitoring.pp
+++ modules/profile/manifests/microsites/monitoring.pp
@@ -3,59 +3,64 @@
 class profile::microsites::monitoring {
 
     prometheus::blackbox::check::http { '15.wikipedia.org':
-        team               => 'serviceops-collab',
-        severity           => 'task',
-        path               => '/',
-        ip_families        => ['ip4'],
-        force_tls          => true,
-        status_matches     => [200],
-        body_regex_matches => ['Wikipedia 15'],
-        port               => 30443, # Kubernetes Ingress port
-        ip4                => ipresolve('miscweb.discovery.wmnet', 4), # Kubernetes Ingress
+        team                    => 'serviceops-collab',
+        severity                => 'task',
+        path                    => '/',
+        ip_families             => ['ip4'],
+        force_tls               => true,
+        status_matches          => [200],
+        body_regex_matches      => ['Wikipedia 15'],
+        port                    => 30443, # Kubernetes Ingress port
+        ip4                     => ipresolve('miscweb.discovery.wmnet', 4), # Kubernetes Ingress
+        certificate_expiry_days => 9,
     }
 
     prometheus::blackbox::check::http { 'annual.wikimedia.org':
-        team           => 'serviceops-collab',
-        severity       => 'task',
-        path           => '/2017/',
-        ip_families    => ['ip4'],
-        force_tls      => true,
-        status_matches => [200],
-        port           => 30443, # Kubernetes Ingress port
-        ip4            => ipresolve('miscweb.discovery.wmnet', 4), # Kubernetes Ingress
+        team                    => 'serviceops-collab',
+        severity                => 'task',
+        path                    => '/2017/',
+        ip_families             => ['ip4'],
+        force_tls               => true,
+        status_matches          => [200],
+        port                    => 30443, # Kubernetes Ingress port
+        ip4                     => ipresolve('miscweb.discovery.wmnet', 4), # Kubernetes Ingress
+        certificate_expiry_days => 9,
     }
 
     prometheus::blackbox::check::http { 'bienvenida.wikimedia.org':
-        team               => 'serviceops-collab',
-        severity           => 'task',
-        path               => '/',
-        force_tls          => true,
-        ip_families        => [ip4],
-        body_regex_matches => ['enciclopedia'],
-        port               => 30443, # Kubernetes Ingress port
-        ip4                => ipresolve('miscweb.discovery.wmnet', 4), # Kubernetes Ingress
+        team                    => 'serviceops-collab',
+        severity                => 'task',
+        path                    => '/',
+        force_tls               => true,
+        ip_families             => [ip4],
+        body_regex_matches      => ['enciclopedia'],
+        port                    => 30443, # Kubernetes Ingress port
+        ip4                     => ipresolve('miscweb.discovery.wmnet', 4), # Kubernetes Ingress
+        certificate_expiry_days => 9,
     }
 
         prometheus::blackbox::check::http { 'transparency.wikimedia.org':
-        team             => 'serviceops-collab',
-        severity         => 'task',
-        path             => '/',
-        force_tls        => true,
-        ip_families      => [ip4],
-        status_matches   => [302],
-        follow_redirects => false,
-        port             => 30443, # Kubernetes Ingress port
-        ip4              => ipresolve('miscweb.discovery.wmnet', 4), # Kubernetes Ingress
+        team                    => 'serviceops-collab',
+        severity                => 'task',
+        path                    => '/',
+        force_tls               => true,
+        ip_families             => [ip4],
+        status_matches          => [302],
+        follow_redirects        => false,
+        port                    => 30443, # Kubernetes Ingress port
+        ip4                     => ipresolve('miscweb.discovery.wmnet', 4), # Kubernetes Ingress
+        certificate_expiry_days => 9,
     }
 
     prometheus::blackbox::check::http { 'transparency-archive.wikimedia.org':
-        team               => 'serviceops-collab',
-        severity           => 'task',
-        path               => '/',
-        force_tls          => true,
-        ip_families        => [ip4],
-        body_regex_matches => ['Transparency'],
-        port               => 30443, # Kubernetes Ingress port
-        ip4                => ipresolve('miscweb.discovery.wmnet', 4), # Kubernetes Ingress
+        team                    => 'serviceops-collab',
+        severity                => 'task',
+        path                    => '/',
+        force_tls               => true,
+        ip_families             => [ip4],
+        body_regex_matches      => ['Transparency'],
+        port                    => 30443, # Kubernetes Ingress port
+        ip4                     => ipresolve('miscweb.discovery.wmnet', 4), # Kubernetes Ingress
+        certificate_expiry_days => 9,
     }
 }
