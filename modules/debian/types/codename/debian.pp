0b54d3b001aa7076dbd5dfc8aef74b10ca35345f
Taavi Väänänen <hi@taavi.wtf>
2023-06-23 13:21:43 +0300

    debian: add bookworm as a valid codename
    Change-Id: I513468024902ff5d2e9f0e9ef429e6590802745b

--- modules/debian/types/codename/debian.pp
+++ modules/debian/types/codename/debian.pp
@@ -5,4 +5,4 @@
 #   apt::repository {'foo':
 #     dist => lookup('foo::bar', Debian::Codename::Debian)
 #   }
-type Debian::Codename::Debian = Enum['stretch', 'buster', 'bullseye', 'sid']
+type Debian::Codename::Debian = Enum['stretch', 'buster', 'bullseye', 'bookworm', 'sid']
