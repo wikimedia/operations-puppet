8a1e65e6926c6bcbca606026f050098d2d0b1daa
Sukhbir Singh <ssingh@wikimedia.org>
2023-06-26 09:30:41 -0400

    P:bird::anycast_healthchecker: allow binding to multiple services
    Our current anycast setup assumes that a single systemd unit can/should
    be binded to the anycast-healthchecker service. Recently in discussions
    around Wikimedia DNS, we decided we wanted to bind two services instead,
    and so this is a refactor of P:bird::anycast_healthchecker to be able to
    do that. There might be more cases that require this, and if not, the
    change is simple enough (converting the string of a service to an array
    of services) that we can update all configurations that set
    profile::bird::bind_anycast_service.
    
    We start with Wikimedia DNS and this will require a coordinated rollout
    to ensure a NOOP on the existing hosts that called the profile.
    
    Hosts: auto
    Change-Id: I6a0282943f8dbd82c194784318f16ab9d88389a6

--- modules/bird/manifests/anycast_healthchecker.pp
+++ modules/bird/manifests/anycast_healthchecker.pp
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
-# == Class: bird::anycast_healthchecker
-#
+# @summary
 # Install and configure the base of anycast_healthchecker
 # https://github.com/unixsurfer/anycast_healthchecker
 #
@@ -11,60 +10,70 @@
 # - systemd service
 #
 # The actual services checks are configured with bird::anycast_healthchecker_check
-#
-#
+# @param bind_service the service for systemd to bind to
+# @param do_ipv6 configure ipv6
+# @param logging The logging config hash
 class bird::anycast_healthchecker(
-  Optional[String]        $bind_service = undef,
-  Boolean                 $do_ipv6      = false,
-  Bird::Anycasthc_logging $logging      = {'level' => 'info', 'num_backups' => 8},
+    Optional[Array[String[1], 1]] $bind_service = undef,
+    Boolean                       $do_ipv6      = false,
+    Bird::Anycasthc_logging       $logging      = {'level' => 'info', 'num_backups' => 8},
 ){
 
-  ensure_packages(['anycast-healthchecker'])
+    ensure_packages(['anycast-healthchecker'])
 
-  file { '/etc/anycast-healthchecker.conf':
-      ensure       => present,
-      owner        => 'bird',
-      group        => 'bird',
-      mode         => '0664',
-      content      => template('bird/anycast-healthchecker.conf.erb'),
-      validate_cmd => '/usr/bin/anycast-healthchecker --check',
-      require      => Package['anycast-healthchecker'],
+    file { '/etc/anycast-healthchecker.conf':
+        ensure       => file,
+        owner        => 'bird',
+        group        => 'bird',
+        mode         => '0664',
+        content      => template('bird/anycast-healthchecker.conf.erb'),
+        validate_cmd => '/usr/bin/anycast-healthchecker --check',
+        require      => Package['anycast-healthchecker'],
 
-  }
+    }
 
-  file {'/var/run/anycast-healthchecker/':
-      ensure => directory,
-      owner  => 'bird',
-      group  => 'bird',
-      mode   => '0775',
-  }
+    file {'/var/run/anycast-healthchecker/':
+        ensure => directory,
+        owner  => 'bird',
+        group  => 'bird',
+        mode   => '0775',
+    }
 
-  file {'/etc/anycast-healthchecker.d/':
-      ensure  => directory,
-      owner   => 'bird',
-      group   => 'bird',
-      mode    => '0775',
-      purge   => true,
-      recurse => true,
-      notify  => Service['anycast-healthchecker'],
-  }
+    file {'/etc/anycast-healthchecker.d/':
+        ensure  => directory,
+        owner   => 'bird',
+        group   => 'bird',
+        mode    => '0775',
+        purge   => true,
+        recurse => true,
+        notify  => Service['anycast-healthchecker'],
+    }
 
-  file {'/var/log/anycast-healthchecker/':
-      ensure => directory,
-      owner  => 'bird',
-      group  => 'bird',
-      mode   => '0775',
-  }
+    file {'/var/log/anycast-healthchecker/':
+        ensure => directory,
+        owner  => 'bird',
+        group  => 'bird',
+        mode   => '0775',
+    }
 
-  systemd::service { 'anycast-healthchecker':
-      content        => template('bird/anycast-healthchecker.service.erb'),
-      require        => File['/etc/anycast-healthchecker.conf',
-                            '/var/run/anycast-healthchecker/',
-                            '/var/log/anycast-healthchecker/',
-                            '/etc/anycast-healthchecker.d/',],
-      restart        => true,
-      service_params => {
-          ensure     => 'running', # lint:ignore:ensure_first_param
-      },
-  }
+    if $bind_service {
+        $bind_service_with_ext = $bind_service.map |$srv| {
+            $srv ? {
+                Systemd::Service::Name => $srv,
+                default                => "${srv}.service"
+            }
+        }
+    }
+    systemd::service { 'anycast-healthchecker':
+        content        => template('bird/anycast-healthchecker.service.erb'),
+        require        => File['/etc/anycast-healthchecker.conf',
+        '/var/run/anycast-healthchecker/',
+        '/var/log/anycast-healthchecker/',
+        '/etc/anycast-healthchecker.d/',],
+        restart        => true,
+        service_params => {
+            ensure  => 'running', # lint:ignore:ensure_first_param
+            require => Service[$bind_service],
+        },
+    }
 }
--- modules/profile/manifests/bird/anycast.pp
+++ modules/profile/manifests/bird/anycast.pp
@@ -5,20 +5,20 @@
 #   Configure anycast_healthchecker
 # @param bfd if true enable bfd
 # @param neighbors_list list of bgp neighbours
-# @param bind_anycast_service The service name of the anycast service e.g. gdnsd
+# @param bind_anycast_services The service names that bind to the anycast service e.g. gdnsd
 # @param advertise_vips A hash of advertised virtual IPs
 # @param do_ipv6 if true configure ipv6
 # @param multihop if true configure multihop
 # @param anycasthc_logging logging configuration
 class profile::bird::anycast(
-  Boolean                                        $bfd                  = lookup('profile::bird::bfd', {'default_value' => true}),
-  Optional[Array[Stdlib::IP::Address::Nosubnet]] $neighbors_list       = lookup('profile::bird::neighbors_list', {default_value => undef}),
-  Optional[String]                               $bind_anycast_service = lookup('profile::bird::bind_anycast_service', {'default_value' => undef}),
-  Optional[Hash[String, Wmflib::Advertise_vip]]  $advertise_vips       = lookup('profile::bird::advertise_vips', {'default_value' => {}}),
-  Optional[Boolean]                              $do_ipv6              = lookup('profile::bird::do_ipv6', {'default_value' => false}),
-  Optional[Boolean]                              $multihop             = lookup('profile::bird::multihop', {'default_value' => true}),
-  Optional[Bird::Anycasthc_logging]              $anycasthc_logging    = lookup('profile::bird::anycasthc_logging', {'default_value' => undef}),
-  Optional[Stdlib::IP::Address::Nosubnet]        $ipv4_src             = lookup('profile::bird::ipv4_src', {'default_value' => undef}),
+  Boolean                                        $bfd                   = lookup('profile::bird::bfd', {'default_value' => true}),
+  Optional[Array[Stdlib::IP::Address::Nosubnet]] $neighbors_list        = lookup('profile::bird::neighbors_list', {default_value => undef}),
+  Optional[Array[String[1], 1]]                  $bind_anycast_services = lookup('profile::bird::bind_anycast_services', {'default_value' => undef}),
+  Optional[Hash[String, Wmflib::Advertise_vip]]  $advertise_vips        = lookup('profile::bird::advertise_vips', {'default_value' => {}}),
+  Optional[Boolean]                              $do_ipv6               = lookup('profile::bird::do_ipv6', {'default_value' => false}),
+  Optional[Boolean]                              $multihop              = lookup('profile::bird::multihop', {'default_value' => true}),
+  Optional[Bird::Anycasthc_logging]              $anycasthc_logging     = lookup('profile::bird::anycasthc_logging', {'default_value' => undef}),
+  Optional[Stdlib::IP::Address::Nosubnet]        $ipv4_src              = lookup('profile::bird::ipv4_src', {'default_value' => undef}),
 ){
 
   $advertise_vips.each |$vip_fqdn, $vip_params| {
@@ -101,7 +101,7 @@ class profile::bird::anycast(
   }
 
   class { 'bird::anycast_healthchecker':
-      bind_service => $bind_anycast_service,
+      bind_service => $bind_anycast_services,
       do_ipv6      => $do_ipv6,
       logging      => $anycasthc_logging,
   }
