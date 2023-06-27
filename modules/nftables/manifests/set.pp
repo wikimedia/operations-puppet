23fbfeb6a8f02823c544b7011df3895927e21357
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-06-27 15:45:29 +0200

    nftables: Also write out empty sets if no ipv4 or ipv6 addresses are present
    If the hosts passed to nftables::set don't contain either ipv4 or ipv6
    no respective set definition is written out. Most of our base definitions
    have both, but there's a few outliers (e.g. LABS_NETWORKS or FRACK_NETWORKS
    which only use ipv4).
    
    In such a case also write out an empty set. An empty set renders a rule
    configured with it into a NOP.
    
    Having both variants always defined vastly simplifies defining rules which
    act on a set: Since the rule expression needs to operate specifically on
    either "ip" or "ip6", otherwise we'd need to additionally resolve the set
    again to pass down the respective addresses. If we always write out both
    variants we can simply always reference the set variant.
    
    Bug: T336497
    Change-Id: I429de998037f53f6efff3f800ea916e677659bda

--- modules/nftables/manifests/set.pp
+++ modules/nftables/manifests/set.pp
@@ -18,35 +18,31 @@ define nftables::set (
     $ipv4_addrs = $ips.filter |$host| { $host =~ Stdlib::IP::Address::V4 }
     $ipv6_addrs = $ips.filter |$host| { $host =~ Stdlib::IP::Address::V6 }
 
-    unless $ipv4_addrs.empty() {
-        $v4_params = {
-            'name'     => "${title}_v4",
-            'set_type' => 'ipv4_addr',
-            'addrs'    => $ipv4_addrs,
-            'interval' => $ipv4_addrs.any |$addr| { '/' in $addr },
-        }
-        file { "/etc/nftables/sets/${name}_ipv4.nft":
-            ensure  => $ensure,
-            mode    => '0444',
-            content => epp('nftables/set.epp', $v4_params),
-            notify  => Service['nftables'],
-            tag     => 'nft',
-        }
+    $v4_params = {
+        'name'     => "${title}_v4",
+        'set_type' => 'ipv4_addr',
+        'addrs'    => $ipv4_addrs,
+        'interval' => $ipv4_addrs.any |$addr| { '/' in $addr },
+    }
+    @file { "/etc/nftables/sets/${name}_ipv4.nft":
+        ensure  => $ensure,
+        mode    => '0444',
+        content => epp('nftables/set.epp', $v4_params),
+        notify  => Service['nftables'],
+        tag     => 'nft',
     }
 
-    unless $ipv6_addrs.empty() {
-        $v6_params = {
-            'name'     => "${title}_v6",
-            'set_type' => 'ipv6_addr',
-            'addrs'    => $ipv6_addrs,
-            'interval' => $ipv6_addrs.any |$addr| { '/' in $addr }
-        }
-        @file { "/etc/nftables/sets/${name}_ipv6.nft":
-            ensure  => $ensure,
-            mode    => '0444',
-            content => epp('nftables/set.epp', $v6_params),
-            notify  => Service['nftables'],
-            tag     => 'nft',
-        }
+    $v6_params = {
+        'name'     => "${title}_v6",
+        'set_type' => 'ipv6_addr',
+        'addrs'    => $ipv6_addrs,
+        'interval' => $ipv6_addrs.any |$addr| { '/' in $addr }
+    }
+    @file { "/etc/nftables/sets/${name}_ipv6.nft":
+        ensure  => $ensure,
+        mode    => '0444',
+        content => epp('nftables/set.epp', $v6_params),
+        notify  => Service['nftables'],
+        tag     => 'nft',
     }
 }
