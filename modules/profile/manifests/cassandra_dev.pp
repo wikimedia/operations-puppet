b036d77f05d56379c60ddf5cf5f0b23b39ee1053
Eric Evans <eevans@wikimedia.org>
2023-06-29 13:54:45 -0500

    cassandra_dev: remove (temp) packages used in debugging
    Bug: T327954
    Change-Id: I3aff362d9a5a7fbf4db94cc74c949c8872839ebe

--- modules/profile/manifests/cassandra_dev.pp
+++ modules/profile/manifests/cassandra_dev.pp
@@ -4,8 +4,4 @@
 class profile::cassandra_dev {
 
     class {'passwords::cassandra': }
-
-    # Temporary; Installed to facilitate ad-hoc testing of Kask
-    # containers (see https://phabricator.wikimedia.org/T327954).
-    ensure_packages(['docker.io', 'siege'])
 }
