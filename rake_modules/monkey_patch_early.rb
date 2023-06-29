945cd213a5b9a1866991672bf5a91ada1f26eb6c
Antoine Musso <hashar@free.fr>
2023-06-29 10:15:11 +0200

    rake_modules: mute Ruby 2.7 Pathname deprecation
    Puppet::FileSystem::FileImpl.read causes a deprecation notice under Ruby
    2.7 and cause an error with Ruby 3:
    
    puppet-5.5.10/lib/puppet/file_system/file_impl.rb:80:
    warning: Using the last argument as keyword parameters is deprecated
    
    The parameters needs to be expanded.
    
    Change-Id: I0cfc88e2d505b728f1c7222752d1d612283a2c32

--- rake_modules/monkey_patch_early.rb
+++ rake_modules/monkey_patch_early.rb
@@ -1,4 +1,6 @@
 require 'rubygems'
+require 'pathname'
+
 # We are using puppet 5.5 which uses this deprecated function.  As such monkey patch it bak in
 # This file needs to be loaded before puppet
 # We use 2.7.0 as the min version to silence deprecation warnings
@@ -13,4 +15,19 @@ if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.7.0')
       DEFAULT_PARSER.unescape(*args)
     end
   end
+
+  if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('3.0.0')
+    # puppet-5.5.10/lib/puppet/file_system/file_impl.rb:80:
+    # warning: Using the last argument as keyword parameters is deprecated
+    module PathnameDeprecations
+      def read(opts)
+        super(**opts)
+      end
+    end
+
+    class Pathname
+      prepend PathnameDeprecations
+    end
+  end
+
 end
