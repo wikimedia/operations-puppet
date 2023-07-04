ab9dfa14e2156ea28413a1e03e0eb659847ec2fe
jbond <jbond@wikimedia.org>
2023-07-04 13:59:02 +0200

    puppet::agent: add support for srv records
    Bug: T341053
    Hosts: basic
    Change-Id: Idc22a9c6289b2e5bdb1d50c9e4e47ad3178c47e2

--- modules/puppet/manifests/agent.pp
+++ modules/puppet/manifests/agent.pp
@@ -3,6 +3,9 @@
 # @param ca_file_path the path to the ca file
 # @param ca_server the ca server
 # @param server the puppet server
+# @param use_srv_records if true use SRV records to resolve the puppet server and ca server
+# @param srv_domain the domain to use when resolving SRV records.  puppet will look for records al
+#   _x-puppet._tcp.$srv_domain and _x-puppet-ca._tcp.$srv_domain
 # @param certname the agent certname
 # @param dns_alt_names a list of dns alt names
 # @param environment the agent environment
@@ -14,6 +17,8 @@ class puppet::agent (
     Stdlib::Unixpath                $ca_file_path           = '/var/lib/puppet/ssl/certs/ca.pem',
     Optional[String[1]]             $ca_server              = undef,
     Stdlib::Host                    $server                 = 'puppet',
+    Boolean                         $use_srv_records        = false,
+    Optional[Stdlib::Fqdn]          $srv_domain             = undef,
     Optional[String[1]]             $certname               = undef,
     Array[Stdlib::Fqdn]             $dns_alt_names          = [],
     Optional[String[1]]             $environment            = undef,
@@ -21,6 +26,9 @@ class puppet::agent (
     Optional[Stdlib::Filesource]    $ca_source              = undef,
     Optional[Enum['chain', 'leaf']] $certificate_revocation = undef,
 ) {
+    if $use_srv_records and !$srv_domain {
+        fail('You must set $srv_domain when using $use_srv_records')
+    }
     # augparse is required to resolve the augeasversion in facter3
     # facter needs virt-what for proper "virtual"/"is_virtual" resolution
     # TODO: use puppet-agent package name when everything is on puppet7
--- modules/puppet/spec/classes/agent_spec.rb
+++ modules/puppet/spec/classes/agent_spec.rb
@@ -1,17 +1,32 @@
 require_relative '../../../../rake_modules/spec_helper'
 
 describe 'puppet::agent' do
-  let(:pre_condition) {
-    [
-      'class passwords::puppet::database {}',
-      'include apt'
-    ]
-  }
   on_supported_os(WMFConfig.test_on).each do |os, facts|
     context "On #{os}" do
       let(:facts) { facts}
-      let(:params) { { 'ca_source' => 'puppet:///modules/foo/ca.pem' } }
-      it { should compile }
+
+      context "default params" do
+        it { is_expected.to compile }
+      end
+      context "with ca_source" do
+        let(:params) { { 'ca_source' => 'puppet:///modules/foo/ca.pem' } }
+        it { is_expected.to compile }
+      end
+      context "With use_srv_records with no srv_domain" do
+        let(:params) {{ use_srv_records: true }}
+        it { is_expected.to compile.and_raise_error(/You must set \$srv_domain/) }
+      end
+      context "With use_srv_records and srv_domain" do
+        let(:params) {{ use_srv_records: true, srv_domain: 'example.org' }}
+        it { is_expected.to compile }
+        it do
+          is_expected.to contain_concat__fragment('main')
+            .with_content(/^use_srv_records\s=\strue$/)
+            .with_content(/^srv_domain\s=\sexample.org$/)
+            .without_content(/^server/)
+            .without_content(/^ca_server/)
+        end
+      end
     end
   end
 end
