61fff1d334e442ce6e5a5783770db7197f75f258
Janis Meybohm <jmeybohm@wikimedia.org>
2023-07-06 15:35:22 +0200

    kubernetes::master: Drop variable assigments used during migration
    Multiple variable assigments where used to ease the review during
    migration. These are now removed.
    
    Hosts: auto
    Bug: T328291
    Change-Id: Ide215554518c691c88f6e0faf1109d30d724129d

--- modules/profile/manifests/kubernetes/master.pp
+++ modules/profile/manifests/kubernetes/master.pp
@@ -11,24 +11,12 @@ class profile::kubernetes::master (
     # TODO: Remove ssl_key_path after T329826 is resolved
     Stdlib::Unixpath $ssl_key_path  = lookup('profile::kubernetes::master::ssl_key_path'),
 ) {
-    $kubernetes_cluster_config = k8s::fetch_cluster_config($kubernetes_cluster_name)
-    $kubernetes_cluster_group = $kubernetes_cluster_config['cluster_group']
-    $pki_intermediate_base = $kubernetes_cluster_config['pki_intermediate_base']
-    $pki_renew_seconds = $kubernetes_cluster_config['pki_renew_seconds']
-    $master_fqdn = $kubernetes_cluster_config['master']
-    $master_url = $kubernetes_cluster_config['master_url']
-    $version = $kubernetes_cluster_config['version']
-    $etcd_urls = $kubernetes_cluster_config['etcd_urls']
-    $service_node_port_range = $kubernetes_cluster_config['service_node_port_range']
-    $ipv6dualstack = $kubernetes_cluster_config['ipv6dualstack']
-    $admission_plugins = $kubernetes_cluster_config['admission_plugins']
-    $admission_configuration = $kubernetes_cluster_config['admission_configuration']
-    $service_cluster_cidr = $kubernetes_cluster_config['service_cluster_cidr']
+    $k8s_config = k8s::fetch_cluster_config($kubernetes_cluster_name)
 
     # Install kubectl matching the masters kubernetes version
     # (that's why we don't use profile::kubernetes::client)
     class { 'k8s::client':
-        version => $version,
+        version => $k8s_config['version'],
     }
 
     # FIXME: This should be removed after T329826 is resolved
@@ -50,10 +38,10 @@ class profile::kubernetes::master (
 
     # The first useable IPv4 IP of the service cluster-cidr is automatically used as ClusterIP for the internal
     # kubernetes apiserver service (kubernetes.default.cluster.local)
-    $apiserver_clusterip = wmflib::cidr_first_address($service_cluster_cidr['v4'])
-    $apiserver_cert = profile::pki::get_cert($pki_intermediate_base, 'kube-apiserver', {
+    $apiserver_clusterip = wmflib::cidr_first_address($k8s_config['service_cluster_cidr']['v4'])
+    $apiserver_cert = profile::pki::get_cert($k8s_config['pki_intermediate_base'], 'kube-apiserver', {
         'profile'        => 'server',
-        'renew_seconds'  => $pki_renew_seconds,
+        'renew_seconds'  => $k8s_config['pki_renew_seconds'],
         'owner'          => 'kube',
         'outdir'         => '/etc/kubernetes/pki',
         # https://v1-23.docs.kubernetes.io/docs/setup/best-practices/certificates/#all-certificates
@@ -63,7 +51,7 @@ class profile::kubernetes::master (
             $facts['ipaddress'],
             $facts['ipaddress6'],
             $apiserver_clusterip,
-            $master_fqdn,
+            $k8s_config['master'],
             'kubernetes',
             'kubernetes.default',
             'kubernetes.default.svc',
@@ -73,9 +61,9 @@ class profile::kubernetes::master (
         'notify_service' => 'kube-apiserver'
     })
 
-    $sa_cert = profile::pki::get_cert($pki_intermediate_base, 'sa', {
+    $sa_cert = profile::pki::get_cert($k8s_config['pki_intermediate_base'], 'sa', {
         'profile'        => 'service-account-management',
-        'renew_seconds'  => $pki_renew_seconds,
+        'renew_seconds'  => $k8s_config['pki_renew_seconds'],
         'owner'          => 'kube',
         'outdir'         => '/etc/kubernetes/pki',
         'notify_service' => 'kube-apiserver'
@@ -85,8 +73,8 @@ class profile::kubernetes::master (
     $additional_sa_certs = [$cergen_sa_cert['cert'], $sa_cert['cert']]
 
     # Client certificate used to authenticate against kubelets
-    $kubelet_client_cert = profile::pki::get_cert($pki_intermediate_base, 'kube-apiserver-kubelet-client', {
-        'renew_seconds'  => $pki_renew_seconds,
+    $kubelet_client_cert = profile::pki::get_cert($k8s_config['pki_intermediate_base'], 'kube-apiserver-kubelet-client', {
+        'renew_seconds'  => $k8s_config['pki_renew_seconds'],
         'names'          => [{ 'organisation' => 'system:masters' }],
         'owner'          => 'kube',
         'outdir'         => '/etc/kubernetes/pki',
@@ -95,8 +83,8 @@ class profile::kubernetes::master (
 
     # Client cert for the front proxy (this uses a separate intermediate then everything else)
     # https://v1-23.docs.kubernetes.io/docs/tasks/extend-kubernetes/configure-aggregation-layer/
-    $frontproxy_cert = profile::pki::get_cert("${pki_intermediate_base}_front_proxy", 'front-proxy-client', {
-        'renew_seconds'  => $pki_renew_seconds,
+    $frontproxy_cert = profile::pki::get_cert("${k8s_config['pki_intermediate_base']}_front_proxy", 'front-proxy-client', {
+        'renew_seconds'  => $k8s_config['pki_renew_seconds'],
         'owner'          => 'kube',
         'outdir'         => '/etc/kubernetes/pki',
         'notify_service' => 'kube-apiserver'
@@ -104,15 +92,15 @@ class profile::kubernetes::master (
 
     # Fetch a client cert with kubernetes-admin permission
     # This is not actually used by anything but here for convenience of operators
-    $default_admin_cert = profile::pki::get_cert($pki_intermediate_base, 'kubernetes-admin', {
-        'renew_seconds'  => $pki_renew_seconds,
+    $default_admin_cert = profile::pki::get_cert($k8s_config['pki_intermediate_base'], 'kubernetes-admin', {
+        'renew_seconds'  => $k8s_config['pki_renew_seconds'],
         'names'           => [{ 'organisation' => 'system:masters' }],
         'owner'           => 'kube',
         'outdir'          => '/etc/kubernetes/pki',
     })
     # Create a supersuer kubeconfig
     k8s::kubeconfig { '/etc/kubernetes/admin.conf':
-        master_host => $master_fqdn,
+        master_host => $k8s_config['master'],
         username    => 'default-admin',
         auth_cert   => $default_admin_cert,
         owner       => 'kube',
@@ -120,25 +108,25 @@ class profile::kubernetes::master (
     }
 
     class { 'k8s::apiserver':
-        etcd_servers            => join($etcd_urls, ','),
+        etcd_servers            => join($k8s_config['etcd_urls'], ','),
         apiserver_cert          => $apiserver_cert,
         # FIXME: T329826 the key of the cergen_sa_cert is used to sign service-account tokens in any case
         sa_cert                 => $cergen_sa_cert,
         additional_sa_certs     => $additional_sa_certs,
         kubelet_client_cert     => $kubelet_client_cert,
         frontproxy_cert         => $frontproxy_cert,
-        version                 => $version,
-        service_cluster_cidr    => $service_cluster_cidr,
-        service_node_port_range => $service_node_port_range,
-        admission_plugins       => $admission_plugins,
-        admission_configuration => $admission_configuration,
-        service_account_issuer  => $master_url,
-        ipv6dualstack           => $ipv6dualstack,
+        version                 => $k8s_config['version'],
+        service_cluster_cidr    => $k8s_config['service_cluster_cidr'],
+        service_node_port_range => $k8s_config['service_node_port_range'],
+        admission_plugins       => $k8s_config['admission_plugins'],
+        admission_configuration => $k8s_config['admission_configuration'],
+        service_account_issuer  => $k8s_config['master_url'],
+        ipv6dualstack           => $k8s_config['ipv6dualstack'],
     }
 
     # Setup kube-scheduler
-    $scheduler_cert = profile::pki::get_cert($pki_intermediate_base, 'system:kube-scheduler', {
-        'renew_seconds'  => $pki_renew_seconds,
+    $scheduler_cert = profile::pki::get_cert($k8s_config['pki_intermediate_base'], 'system:kube-scheduler', {
+        'renew_seconds'  => $k8s_config['pki_renew_seconds'],
         'names'          => [{ 'organisation' => 'system:kube-scheduler' }],
         'owner'          => 'kube',
         'outdir'         => '/etc/kubernetes/pki',
@@ -146,20 +134,20 @@ class profile::kubernetes::master (
     })
     $scheduler_kubeconfig = '/etc/kubernetes/scheduler.conf'
     k8s::kubeconfig { $scheduler_kubeconfig:
-        master_host => $master_fqdn,
+        master_host => $k8s_config['master'],
         username    => 'default-scheduler',
         auth_cert   => $scheduler_cert,
         owner       => 'kube',
         group       => 'kube',
     }
     class { 'k8s::scheduler':
-        version    => $version,
+        version    => $k8s_config['version'],
         kubeconfig => $scheduler_kubeconfig,
     }
 
     # Setup kube-controller-manager
-    $controller_manager_cert = profile::pki::get_cert($pki_intermediate_base, 'system:kube-controller-manager', {
-        'renew_seconds'  => $pki_renew_seconds,
+    $controller_manager_cert = profile::pki::get_cert($k8s_config['pki_intermediate_base'], 'system:kube-controller-manager', {
+        'renew_seconds'  => $k8s_config['pki_renew_seconds'],
         'names'          => [{ 'organisation' => 'system:kube-controller-manager' }],
         'owner'          => 'kube',
         'outdir'         => '/etc/kubernetes/pki',
@@ -167,7 +155,7 @@ class profile::kubernetes::master (
     })
     $controllermanager_kubeconfig = '/etc/kubernetes/controller-manager.conf'
     k8s::kubeconfig { $controllermanager_kubeconfig:
-        master_host => $master_fqdn,
+        master_host => $k8s_config['master'],
         username    => 'default-controller-manager',
         auth_cert   => $controller_manager_cert,
         owner       => 'kube',
@@ -178,7 +166,7 @@ class profile::kubernetes::master (
         service_account_private_key_file => $cergen_sa_cert['key'],
         ca_file                          => $sa_cert['chain'],
         kubeconfig                       => $controllermanager_kubeconfig,
-        version                          => $version,
+        version                          => $k8s_config['version'],
     }
 
     # All our masters are accessible to all
