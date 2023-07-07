a67428392314bc53a9927567066de53e2601294a
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2023-07-07 15:41:54 +0200

    mw-debug-repl: improve UX
    Following feedback by deployers, we wanted to make the following UX
    changes:
    
    * Support the usual long form --wiki option
    * Support the ability to indicate a wiki as the last cli argument
      without flags, as mwscript did
    * Do now run if a wiki is not defined
    * Check if the user has the correct permissions to execute the script,
      otherwise sudo automatically
    * Finally, disallow defining the php command, as this is a container so
      there won't be multiple php versions.
    
    To achieve the better command line parsing, we switched to using GNU
    getopt, which gives us much more flexibility on how commands are defined
    and accepted on CLI.
    
    Bug: T341197
    Change-Id: I446ec2e6f32830bce5618e7bd557d2a75b659a13

--- modules/profile/files/kubernetes/deployment_server/mw-debug-repl.sh
+++ modules/profile/files/kubernetes/deployment_server/mw-debug-repl.sh
@@ -1,44 +1,76 @@
 #!/bin/bash
 # SPDX-License-Identifier: Apache-2.0
 set -eo pipefail
+# This script should be run as root. Auto-sudo in case it wasn't done on the command line.
+if [[ "$EUID" != 0 ]]; then
+    echo "Becoming root..."
+    exec sudo "$0" "$@"
+fi
 
 function help() {
 cat <<EOF
 mw-debug-repl - launch a MediaWiki REPL in the kubernetes mw-debug environment.
 
-Usage: mw-debug-repl [-e] [-d <datacenter>] [-w <wiki-name>]
+Usage: mw-debug-repl [-e] [-d <datacenter>] [-w <wiki-name>|<wiki-name>]
 
 OPTIONS:
-  -e    Launch eval.php, instead of the default shell.php as REPL
-  -d    Pick a specific datacenter (by default the master will be picked)
-  -w    Pick a wiki. Defaults to "aawiki".
+  -e                 Launch eval.php, instead of the default shell.php as REPL
+  -d|--datacenter    Pick a specific datacenter (by default the master will be picked)
+  -w|--wiki          Pick a wiki. For compatibility reasons, the flag can be omitted.
+  -h|--help          Show this help message
+
+EXAMPLES:
+
+Launch an eval.php shell for itwiki in eqiad
+
+  $ sudo $0 -e -d eqiad --wiki itwiki
+  # Also valid:
+  $ sudo $0 -e --datacenter eqiad itwiki
+
+Launch shell.php for enwiki
+
+  $ sudo $0 enwiki
+  $ sudo $0 --wiki enwiki
+
 EOF
 }
 
 ### Main script
 
 REPL="shell.php"
-WIKI="aawiki"
+WIKI=""
 DC=""
-# Allow overriding of PHP version or flags
-[ "$PHP" = "" ] && PHP=php
 
-while getopts ":hew:d:" option; do
-    case $option in
-        h) # display Help
+OPTS=$(getopt -o hew:d: -l help,wiki:,datacenter: -- "$@")
+eval set -- "$OPTS"
+while true; do
+    case "$1" in
+        -h | --help ) # display Help
             help
             exit;;
-        e) # use eval.php instead of shell.php
-            REPL="eval.php";;
-        w)
-            WIKI="$OPTARG";;
-        d)
-            DC="$OPTARG";;
+        -e ) # use eval.php instead of shell.php
+            REPL="eval.php"; shift;;
+        -w | --wiki )
+            WIKI="$2"; shift 2;;
+        -d | --datacenter )
+            DC="$2"; shift 2;;
+        -- )
+            shift; break;;
         *)
-            echo "Error: invalid option '$option'. Please run $0 -h for help"\
+            echo "Error: invalid option '$1'. Please run $0 -h for help"\
             exit 1;;
     esac
 done
+if [[ "$WIKI" == "" ]]; then
+    if [ -z "$1" ]; then
+        echo "Error: a wiki should be provided on the command line"
+        echo
+        help
+        exit 1
+    fi
+    WIKI=$1
+    shift
+fi
 
 if [[ "$DC" == "" ]]; then
     DC=$(confctl --object-type mwconfig select 'name=WMFMasterDatacenter' get | jq .WMFMasterDatacenter.val | sed s/\"//g)
@@ -51,4 +83,4 @@ if [ -z "$PODNAME" ]; then
     exit 1
 fi
 echo "Now running $REPL for $WIKI inside ${PODNAME}..."
-kubectl -n mw-debug exec "$PODNAME" -c mediawiki-pinkunicorn-app -ti -- "$PHP" /srv/mediawiki/multiversion/MWScript.php "$REPL" --wiki "$WIKI"
+kubectl -n mw-debug exec "$PODNAME" -c mediawiki-pinkunicorn-app -ti -- php /srv/mediawiki/multiversion/MWScript.php "$REPL" --wiki "$WIKI"
