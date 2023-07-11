07ff910a2fa9fa2d774ae1f98bd6df97d64bf151
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2023-07-11 14:26:40 +0200

    prometheus: refactor alerts-deploy to pick up k8s clusters
    Change-Id: I9961ac240813d5d7e4d0e25e9b3c98b864521f7d

--- modules/profile/manifests/alerts/deploy/prometheus.pp
+++ modules/profile/manifests/alerts/deploy/prometheus.pp
@@ -1,8 +1,14 @@
 # SPDX-License-Identifier: Apache-2.0
 # Satisfy the WMF style guide
 class profile::alerts::deploy::prometheus {
+    $enabled_k8s_clusters = k8s::fetch_clusters(false).filter | String $_, K8s::ClusterConfig $config | {
+        $config['dc'] == $::site and 'prometheus' in $config
+    }
+    $k8s_prometheus_names = $enabled_k8s_clusters.map |String $_, K8s::ClusterConfig $c| {
+        $c['prometheus']['name']
+    }
+
     class { 'alerts::deploy::prometheus':
-        instances => ['analytics', 'ext', 'k8s', 'k8s-staging',
-                      'k8s-mlserve', 'ops', 'services']
+        instances => ['analytics', 'ext', 'ops', 'services'] + $k8s_prometheus_names
     }
 }
