7df54b6897f50c67f12b8faf133794cf0c82892e
jbond <jbond@wikimedia.org>
2023-07-13 17:46:57 +0200

    profile::cassandra: Add spec test
    Change-Id: I643c222744b8567a43712de832c467df59aef177

---
+++ modules/profile/spec/classes/profile_cassandra_spec.rb
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: Apache-2.0
+require_relative '../../../../rake_modules/spec_helper'
+
+describe 'profile::cassandra' do
+  on_supported_os(WMFConfig.test_on).each do |os, os_facts|
+    context "on #{os}" do
+      # We need to set the hostname top find the correct secret
+      let(:facts) { os_facts.merge(hostname: 'sessionstore1001-a') }
+      let(:params) {{ rack: 'B2' }}
+      let(:node_params) {{ '_role' => 'sessionstore' }}
+      describe 'test compilation with default parameters' do
+        it { is_expected.to compile.with_all_deps }
+      end
+    end
+  end
+end
