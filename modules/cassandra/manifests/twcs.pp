52c060d6734c7c90ccef051b798013ee540b07dd
Eevans <eevans@wikimedia.org>
2023-07-17 19:58:51 +0000

    Revert "cassandra: uninstall cassandra-twcs deployment repository"
    This reverts commit b8ecedb805f71fc36ad26d2252468b988f213d9f.
    
    Reason for revert: Change removes more than intended deployment
    
    Change-Id: Ib844cfb2652123ce033ba854d4afca2b92eae839

--- modules/cassandra/manifests/twcs.pp
+++ modules/cassandra/manifests/twcs.pp
@@ -11,7 +11,6 @@ class cassandra::twcs(
     require ::cassandra
 
     scap::target { 'cassandra/twcs':
-        ensure      => 'absent',
         deploy_user => 'deploy-service',
         manage_user => true,
     }
