5f3078d0386f1e28d0143a1abeeba23c1e58ca29
jbond <jbond@wikimedia.org>
2023-07-17 18:08:22 +0200

    vrts: drop bashisms and fix other CI issues
    Bug: T95064
    Change-Id: I7adc53ad25cf1b617e2a214b796eb8df2b973435

--- modules/vrts/files/upgrade_vrts.sh
+++ modules/vrts/files/upgrade_vrts.sh
@@ -4,23 +4,23 @@
 
 . /etc/vrts/install-script-vars
 
-/usr/bin/curl -L $DOWNLOAD_URL/znuny-$1.tar.gz -o /tmp/znuny-$1.tar.gz
-if [[ $? -ne 0 ]]; then
-    echo "ERROR: Failed Downloading $DOWNLOAD_URL/znuny-$1.tar.gz"
+if ! /usr/bin/curl -L "${DOWNLOAD_URL}/znuny-${1}.tar.gz" -o "/tmp/znuny-${1}.tar.gz"
+then
+    echo "ERROR: Failed Downloading ${DOWNLOAD_URL}/znuny-${1}.tar.gz"
     exit 1
 fi
 
-sudo /usr/bin/tar xfz /tmp/znuny-$1.tar.gz -C /opt
+sudo /usr/bin/tar xfz "/tmp/znuny-${1}.tar.gz" -C /opt
 
 sudo systemctl stop cron
 sudo systemctl stop exim4
 sudo systemctl stop apache2
 sudo systemctl stop vrts-daemon
 
-sudo cp /opt/otrs/Kernel/Config.pm /opt/znuny-$1/Kernel
-sudo cp /opt/otrs/var/log/TicketCounter.log /opt/znuny-$1/var/log/
+sudo cp /opt/otrs/Kernel/Config.pm "/opt/znuny-${1}/Kernel"
+sudo cp /opt/otrs/var/log/TicketCounter.log "/opt/znuny-${1}/var/log/"
 
-sudo ln -sfn /opt/znuny-$1 /opt/otrs
+sudo ln -sfn "/opt/znuny-${1}" /opt/otrs
 sudo -u root /opt/otrs/bin/otrs.SetPermissions.pl --web-group=www-data
 sudo -u otrs /opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild
 sudo -u otrs /opt/otrs/bin/otrs.Console.pl Maint::Cache::Delete
@@ -32,6 +32,6 @@ sudo systemctl start apache2
 sudo systemctl start vrts-daemon
 
 # Clean Up
-sudo rm -rf /tmp/znuny-$1.tar.gz
+sudo rm -rf "/tmp/znuny-${1}.tar.gz"
 
 sudo puppet agent -t
