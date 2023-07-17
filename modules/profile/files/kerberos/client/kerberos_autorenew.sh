d4c00a47481834c858bd7129657e49c6a1f442a8
jbond <jbond@wikimedia.org>
2023-07-17 18:13:19 +0200

    kerberos: Fix bashisms
    Bug: T95064
    Change-Id: I56b511b3ac0cc161f1a0873b0df2a04da8770cbd

--- modules/profile/files/kerberos/client/kerberos_autorenew.sh
+++ modules/profile/files/kerberos/client/kerberos_autorenew.sh
@@ -5,23 +5,27 @@
 # If there is a valid ticket, check whether the auto-renewal systemd timer is active
 # for this account. If it is not active then create it.
 #
-function autorenew_is_active {
-    /usr/bin/systemctl -q --user is-active krenew-$USER.timer 
+autorenew_is_active() {
+    /usr/bin/systemctl -q --user is-active "krenew-${USER}.timer" 
 }
 
-function create_autorenew_timer {
-    echo -e "\nCreating automatic Kerberos ticket renewal service"
-    /usr/bin/systemd-run --quiet --user --unit krenew-$USER.timer \
+create_autorenew_timer() {
+    printf "\nCreating automatic Kerberos ticket renewal service"
+    /usr/bin/systemd-run --quiet --user --unit "krenew-${USER}.timer" \
         --on-calendar=daily \
-        --description="Kerberos ticket renewal timer for for $USER" \
+        --description="Kerberos ticket renewal timer for for ${USER}" \
         --property=StandardOutput=null \
         --property=StandardError=null \
         /usr/bin/sh -c "/usr/bin/klist -s && /usr/bin/krenew -v -L 2> /dev/null"
 }
 
 if /usr/bin/klist -s; then
-    echo -e '\nYou have a valid Kerberos ticket.'
-    autorenew_is_active && echo -e 'Your automatic Kerberos ticket renewal service is also active on this host\n' || create_autorenew_timer
+    printf '\nYou have a valid Kerberos ticket.'
+    if autorenew_is_active; then
+      printf 'Your automatic Kerberos ticket renewal service is also active on this host\n'
+    else
+     create_autorenew_timer
+    fi
 else
-    echo -e '\nYou do not have a valid Kerberos ticket in the credential cache, remember to kinit.'
+    printf '\nYou do not have a valid Kerberos ticket in the credential cache, remember to kinit.'
 fi
