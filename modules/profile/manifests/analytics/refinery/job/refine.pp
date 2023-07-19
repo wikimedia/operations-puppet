a680713e938da0e7780243684463c7d47296e40c
jebe <jebe@wikimedia.org>
2023-07-19 12:36:00 +0100

    Update refine jobs with new var version
    The new jar allows for more logging for certain errors
    
    Change-Id: I7413a9002fdecf46051eab25b4b265a7ad7734db

--- modules/profile/manifests/analytics/refinery/job/refine.pp
+++ modules/profile/manifests/analytics/refinery/job/refine.pp
@@ -36,7 +36,7 @@ class profile::analytics::refinery::job::refine(
 
     # Update this when you want to change the version of the refinery job jar
     # being used for the refine job.
-    $refinery_version = '0.2.16'
+    $refinery_version = '0.2.18'
 
     # Use this value by default
     Profile::Analytics::Refinery::Job::Refine_job {
