777d930270c930aa1b03a4eb3e6b77c77f2657e9
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2023-07-21 11:51:47 +0200

    openstack: admin_scripts: install mariadb-client from the galera repo
    So later when galera is installed there is no downgrade being tried
    by puppet (which will fail).
    
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: Id81729f09f14e1567e212a62cc7b6655b34b2262

--- modules/openstack/manifests/util/admin_scripts.pp
+++ modules/openstack/manifests/util/admin_scripts.pp
@@ -18,8 +18,12 @@ class openstack::util::admin_scripts(
 
     # We need a mysql client in order to run wmcs-cold-migrate and wmcs-ceph-migrate;
     #  they modify the VM host directly in the db
-    package{ 'mariadb-client':
-        ensure => 'present',
+    # Make the custom component explicit so this is not installed from the debian archive
+    # which would result in puppet trying to downgrade later the package when galera's
+    # module mariadb-backup is installed
+    apt::package_from_component { 'openstack-mariadb-client':
+        component => 'thirdparty/openstack-db',
+        packages  => ['mariadb-client'],
     }
 
     package{ 'python3-pytest':
