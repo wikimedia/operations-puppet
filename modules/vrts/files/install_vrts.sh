0e4dd603f41c7cbae25206de14df8349b66a2789
jbond <jbond@wikimedia.org>
2023-07-21 17:31:09 +0200

    vtrs: drop bashisms and fix other CI issues
    Bug: T95064
    Change-Id: Ia8b61c85fdd8fe74a1ae8331eeca322bad612154

--- modules/vrts/files/install_vrts.sh
+++ modules/vrts/files/install_vrts.sh
@@ -4,19 +4,17 @@
 
 . /etc/vrts/install-script-vars
 
-/usr/bin/curl -L $DOWNLOAD_URL/znuny-$1.tar.gz -o /tmp/znuny-$1.tar.gz
-
-if [[ $? -ne 0 ]]; then
-    echo "ERROR: Failed Downloading $DOWNLOAD_URL/znuny-$1.tar.gz"
+if ! /usr/bin/curl -L "${DOWNLOAD_URL}/znuny-${1}.tar.gz"-o "tmp/znuny-${1}.tar.gz"; then
+    echo "ERROR: Failed Downloading ${DOWNLOAD_URL}/znuny-${1}.tar.gz"
     exit 1
 fi
 
-sudo /usr/bin/tar xfz /tmp/znuny-$1.tar.gz -C /opt
+sudo /usr/bin/tar xfz "/tmp/znuny-${1}.tar.gz" -C /opt
 
-sudo cp /opt/otrs/Kernel/Config.pm /opt/znuny-$1/Kernel
-sudo cp /opt/otrs/var/log/TicketCounter.log /opt/znuny-$1/var/log/
+sudo cp /opt/otrs/Kernel/Config.pm "/opt/znuny-${1}/Kernel"
+sudo cp /opt/otrs/var/log/TicketCounter.log "/opt/znuny-${1}/var/log/"
 
-sudo ln -sfn /opt/znuny-$1 /opt/otrs
+sudo ln -sfn "/opt/znuny-${1}" /opt/otrs
 
 sudo -u root /opt/otrs/bin/otrs.SetPermissions.pl --web-group=www-data
 sudo -u otrs /opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild
@@ -24,6 +22,6 @@ sudo -u otrs /opt/otrs/bin/otrs.Console.pl Maint::Cache::Delete
 sudo -u www-data /opt/otrs/bin/otrs.Console.pl Admin::Package::ReinstallAll
 
 # Clean Up
-sudo rm -rf /tmp/znuny-$1.tar.gz
+sudo rm -rf "/tmp/znuny-${1}.tar.gz"
 
 sudo puppet agent -t
