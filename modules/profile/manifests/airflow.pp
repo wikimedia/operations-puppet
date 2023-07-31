468e10f6b5d92bafc648f0281804a1d2bf2c0d17
Ben Tullis <btullis@wikimedia.org>
2023-07-31 13:44:13 +0100

    Update airflow sqlalchemy URI for all airflow instances
    We have to-date been using sqlalshemy URIs beginning with postgres://
    but the next version of sqlalchemy will reject this URIs and require
    that the postgresql:// scheme be used instead.
    
    Bug: T336286
    Change-Id: Ibab559e77c90830fb8dfbca650b9fc556cf79dfb

--- modules/profile/manifests/airflow.pp
+++ modules/profile/manifests/airflow.pp
@@ -217,7 +217,7 @@ class profile::airflow(
                             # $airflow_instances_secrets as the $db_user and $db_password params.
                             # This ERb template string will be rendered in airflow::instance
                             # with those values.
-                            'sql_alchemy_conn' => "postgres://<%= @db_user %>:<%= @db_password %>@${airflow_database_host_default}/airflow_${instance_name_normalized}?sslmode=require&sslrootcert=/etc/ssl/certs/Puppet_Internal_CA.pem",
+                            'sql_alchemy_conn' => "postgresql://<%= @db_user %>:<%= @db_password %>@${airflow_database_host_default}/airflow_${instance_name_normalized}?sslmode=require&sslrootcert=/etc/ssl/certs/Puppet_Internal_CA.pem",
                         }
                     }
                 }
