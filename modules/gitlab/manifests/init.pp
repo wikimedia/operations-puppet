aa265d79a3a2121e8c8f41b34abe372bac942f62
Ahmon Dancy <adancy@wikimedia.org>
2023-07-31 08:18:55 -0700

    gitlab: Use gitlab-settings v1.2.0
    Bump from repos/releng/gitlab-settings v1.0.0 to v1.2.0 (v1.1.0 never
    existed) to switch from cas3 to openid_connect for newly created
    gitlab users.
    
    Bug: T320390
    Change-Id: Iea557c8c45e1b2d578f1494665f3b74037c0d462

--- modules/gitlab/manifests/init.pp
+++ modules/gitlab/manifests/init.pp
@@ -239,7 +239,7 @@ class gitlab (
     git::clone { 'repos/releng/gitlab-settings':
         ensure        => $ensure_gitlab_settings,
         update_method => 'checkout',
-        git_tag       => 'v1.0.0',
+        git_tag       => 'v1.2.0',
         directory     => '/srv/gitlab-settings',
         source        => 'gitlab',
         owner         => $ldap_group_sync_user,
