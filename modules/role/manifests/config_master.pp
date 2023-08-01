a6b2a9505ba5d560bf18115a1cc4966dbe7fca35
jbond <jbond@wikimedia.org>
2023-08-01 16:53:37 +0200

    O:config_master: Add new role for config-master
    Bug: T341717
    Change-Id: I8d6d8c573ade726f1e94a70d2c1e0069f0a9c96c

---
+++ modules/role/manifests/config_master.pp
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0
+# @summary role to configer ther serveres that serve config-master.wikimedia.org
+class role::config_master {
+
+    system::role { 'config_master':
+        ensure      => 'present',
+        description => 'Host used for experiments by SREs',
+    }
+
+    include profile::base::production
+    include profile::firewall
+    include profile::configmaster
+    include profile::discovery::client
+}
