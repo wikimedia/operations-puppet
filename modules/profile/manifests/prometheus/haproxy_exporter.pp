403f8d9467755f7b7d10a24b92d4f9e69480d5cc
David Caro <dcaro@wikimedia.org>
2023-08-09 15:12:08 +0200

    haproxy_exporter: allow setting as absent
    This is in preparation to replacing it with the internal haproxy
    endpoint.
    
    Bug: T343885
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: I0abca070ff721317cf201dc12d2dd48f2408da6c

--- modules/profile/manifests/prometheus/haproxy_exporter.pp
+++ modules/profile/manifests/prometheus/haproxy_exporter.pp
@@ -1,8 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::prometheus::haproxy_exporter(
     Stdlib::Port $listen_port = lookup('listen_port'),
+    Wmflib::Ensure $ensure  = lookup('profile::prometheus::haproxy_exporter::ensure', {'default_value' => present}),
 ){
     class {'::prometheus::haproxy_exporter':
-        listen_port => $listen_port
+        listen_port => $listen_port,
+        ensure      => $ensure,
     }
 }
--- modules/prometheus/manifests/haproxy_exporter.pp
+++ modules/prometheus/manifests/haproxy_exporter.pp
@@ -7,12 +7,18 @@
 #
 # [*$listen_port*]
 #  The TCP port to listen on
+#
+# [*$ensure*]
+#  If it should be present or absent
 
 class prometheus::haproxy_exporter (
     Stdlib::HTTPUrl $endpoint  = 'http://localhost:9100/?stats;csv',
     Stdlib::Port::User $listen_port = 9901,
+    Wmflib::Ensure $ensure  = present,
 ) {
-    ensure_packages('prometheus-haproxy-exporter')
+    ensure_packages('prometheus-haproxy-exporter', {
+        ensure  => $ensure,
+    })
 
     $exec_binary = $::lsbdistcodename ? {
         bullseye => '/usr/bin/prometheus-haproxy-exporter',
@@ -21,10 +27,12 @@ class prometheus::haproxy_exporter (
     }
 
     systemd::service { 'prometheus-haproxy-exporter':
-        ensure  => present,
+        ensure  => $ensure,
         restart => true,
         content => systemd_template('prometheus-haproxy-exporter'),
     }
 
-    profile::auto_restarts::service { 'prometheus-haproxy-exporter': }
+    profile::auto_restarts::service { 'prometheus-haproxy-exporter':
+        ensure  => $ensure,
+    }
 }
