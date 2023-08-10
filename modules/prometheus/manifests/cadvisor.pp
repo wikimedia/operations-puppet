d4f7f3d9fbdb56c358ba9abcc2312f2594834e6c
Jesse Hathaway <jhathaway@wikimedia.org>
2023-08-10 10:24:36 -0500

    dev env: cadvisor exporter, in container env listen on all interfaces
    Since we don't know what our ip will be, in a container environment,
    have cadvisor listen on all interfaces.
    
    Hosts: basic
    Bug: T337972
    Change-Id: I974add484f1ece539e08925e3c4fe6b78993329e

--- modules/prometheus/manifests/cadvisor.pp
+++ modules/prometheus/manifests/cadvisor.pp
@@ -1,8 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 class prometheus::cadvisor(
-    Wmflib::Ensure $ensure,
-    Stdlib::Port $port,
+    Wmflib::Ensure                      $ensure,
+    Stdlib::Port                        $port,
     Array[Prometheus::Cadvisor::Metric] $metrics_enabled_extra = [],
+    Stdlib::IP::Address                 $listen_address = $facts['wmflib']['is_container'] ? {
+                                            true  => '0.0.0.0',
+                                            false => $facts['networking']['ip'],
+                                        },
 ) {
     # Taken by subtracting the default for -disable_metrics from the
     # list of all valid metrics
