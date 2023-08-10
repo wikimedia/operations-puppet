6d8c4f202920162bd4591e4854151489ce0bac72
Ben Tullis <btullis@wikimedia.org>
2023-08-10 16:57:06 +0100

    Use the libmysql-java component on bullseye as well
    We have previously been attempting to use the libmariadb-java connector
    on bullseye, instead of the forward-ported libmysql-java from stretch.
    
    It seems to have worked for sqoop, but it isn't working for hive so now
    we need to start using this connector on bullseye as well.
    
    I will follow this up with a change that will create a separate
    bigop::mariadb_jdbc class, so that we can use this where it is known to
    work (i.e. sqoop) and use MySQL where we still need to.
    
    Bug: T329363
    Change-Id: Ia141e715a29774a4a833c12a1748bda3cb5cb22c

--- modules/bigtop/manifests/mysql_jdbc.pp
+++ modules/bigtop/manifests/mysql_jdbc.pp
@@ -1,30 +1,23 @@
 # SPDX-License-Identifier: Apache-2.0
 # == define bigtop::mysql_jdbc
 #
-# Helper to deploy Mysql/Mariadb jars and custom symlinks
+# Helper to deploy the mysql jar and custom symlinks
 # where needed.
 #
 define bigtop::mysql_jdbc (
     String $link_path,
     Optional[Boolean] $use_mysql_jar = false,
 ) {
-    if debian::codename::eq('buster') {
-        $package_name = 'libmysql-java'
-        $jar_path = $use_mysql_jar ? {
-            true    => '/usr/share/java/mysql.jar',
-            default => '/usr/share/java/mysql-connector-java.jar',
+    $package_name = 'libmysql-java'
+    $jar_path = $use_mysql_jar ? {
+        true    => '/usr/share/java/mysql.jar',
+        default => '/usr/share/java/mysql-connector-java.jar',
+    }
+    if !defined(Apt::Package_from_component['libmysql-java-component']) {
+        apt::package_from_component { 'libmysql-java-component':
+            component => 'component/libmysql-java',
+            packages  => [$package_name],
         }
-        if !defined(Apt::Package_from_component['libmysql-java-component']) {
-            apt::package_from_component { 'libmysql-java-component':
-                component => 'component/libmysql-java',
-                packages  => [$package_name]
-            }
-        }
-    } else {
-        # See T278424 and T329363#8609591
-        # For the bullseye upgrade we are trying once more to use libmariadb-java with sqoop instead of the forward-ported libmysql-java
-        $package_name = 'libmariadb-java'
-        $jar_path = '/usr/share/java/mariadb-java-client.jar'
     }
 
     ensure_packages($package_name)
