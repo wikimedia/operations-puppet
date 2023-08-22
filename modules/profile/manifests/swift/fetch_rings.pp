7c450465afac62f569929345da976c950ae732e0
jbond <jbond@wikimedia.org>
2023-08-22 16:08:45 +0200

    profile::swift:fetch_rings: ensure we create directories
    Bug: T341056
    Hosts: auto
    Change-Id: I9d7df92b11adb187ddef3d48ead4523ca336fd79

--- modules/profile/manifests/swift/fetch_rings.pp
+++ modules/profile/manifests/swift/fetch_rings.pp
@@ -1,23 +1,30 @@
 # SPDX-License-Identifier: Apache-2.0
 # @param ca_server the CA server
 # @param swift_clusters instance of Swift::Clusters (hash of cluster info)
+# @param volatile_dir the location of the volatile dir
 class profile::swift::fetch_rings (
-    Stdlib::Host        $ca_server      = lookup('puppet_ca_server'),
-    Swift::Clusters     $swift_clusters = lookup('swift_clusters'),
-    Stdlib::Unixpath     $volatile_dir   = lookup('profile::swift::fetch_rings::volatile_dir'),
+    Stdlib::Host     $ca_server      = lookup('puppet_ca_server'),
+    Swift::Clusters  $swift_clusters = lookup('swift_clusters'),
+    Stdlib::Unixpath $volatile_dir   = lookup('profile::swift::fetch_rings::volatile_dir'),
 ) {
     $ca = $ca_server == $facts['networking']['fqdn']
-    # The master frontend copies updated swift rings from each clusters'
-    # ring management host into volatile
-    $ring_fetch = $ca.bool2str('present', 'absent')
+    $ring_fetch = stdlib::ensure($ca)
+    $swift_dir = "${volatile_dir}/swift"
+    file { $swift_dir:
+        ensure => directory,
+    }
 
     $swift_clusters.each |String $sc, Swift::Cluster_info $sc_info| {
         if $sc_info['ring_manager'] != undef {
+            $cluster_dir = "${swift_dir}/${sc_info['cluster_name']}"
+            file { $cluster_dir:
+                ensure => directory,
+            }
             systemd::timer::job { "fetch-rings-${sc}":
-                ensure          => $ring_fetch,
+                ensure          => stdlib::ensure($ca),  # We only want the timer to run on one host, use ca
                 user            => 'root',
                 description     => "rsync swift rings from cluster ${sc}",
-                command         => "/usr/bin/rsync -bcptz ${sc_info['ring_manager']}::swiftrings/new_rings.tar.bz2 ${volatile_dir}/swift/${sc_info['cluster_name']}/",
+                command         => "/usr/bin/rsync -bcptz ${sc_info['ring_manager']}::swiftrings/new_rings.tar.bz2 ${cluster_dir}/",
                 interval        => {'start' => 'OnCalendar', 'interval' => '*:5/20:00'},
                 logging_enabled => false,
             }
