2600aed7531dcbb2ffc4014a7d76f2ce5e555495
jbond <jbond@wikimedia.org>
2023-08-28 18:06:43 +0200

    ferm: add ensure support to the ferm class
    add ensure support to the ferm class this will allow us to more easily
    switch between ferm and nftables
    
    Bug: T336497
    Hosts: basic
    Hosts: P:openstack::base::cloudgw
    Change-Id: I93a4f65475d6643b23e1e37d2f057f2a52be3e07

--- modules/firewall/manifests/init.pp
+++ modules/firewall/manifests/init.pp
@@ -1,8 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
-# @summary wrapper class to provide common interface to ferm and nft
+# @summary wrapper class to provide common interface to ferm and nftables
 # @param provider which firewall provider to use
 class firewall (
     Firewall::Provider $provider = 'ferm'
 ) {
-    class { $provider: }  # lint:ignore:wmf_styleguide
+    class { 'ferm':  # lint:ignore:wmf_styleguide
+        ensure => stdlib::ensure($provider == 'ferm'),
+    }
+    class { 'nftables':  # lint:ignore:wmf_styleguide
+        ensure => stdlib::ensure($provider == 'nftables'),
+    }
 }
