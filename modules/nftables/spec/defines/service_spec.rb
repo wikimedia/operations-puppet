wikimedia/operations-puppet

commit 7870e9ee4a4ddb3d763312bf12308455804d7e5b

Make nftables::service types more compatible
- The port can be passed as both a single Stdlib::Port and an array of
  Stdlib::Port, in the first case internally convert it to an array and
  apply a custom type
- Also add $notrack to nftables::service (not yet implemented, if enabled
  it gets caught by the firewall::service wrapper)

Hosts: C:profile::firewall
Bug: T336497
Change-Id: I9ee54fa18ef7ca85f021eb7de54769aa485b8687

modules/nftables/manifests/service.pp
@@ -20,12 +20,13 @@ define nftables::service (
     Wmflib::Ensure                $ensure     = present,
     Integer[0,99]                 $prio       = 10,
     Optional[String]              $desc       = undef,
-    Array[Stdlib::Port]           $port       = [],
+    Nftables::Port                $port       = undef,
     Optional[Firewall::Portrange] $port_range = undef,
     Array[Stdlib::IP::Address]    $src_ips    = [],
     Array[Stdlib::IP::Address]    $dst_ips    = [],
     Array[String[1]]              $src_sets   = [],
     Array[String[1]]              $dst_sets   = [],
+    Boolean                       $notrack    = false,
 ) {
     # TODO: notrack is not implemented yet
     # TODO: there is a nftables construct 'concatenation' that can drastically
@@ -33,12 +34,14 @@ define nftables::service (
     # this define doesn't support it, but we may in the future!
     # see https://wiki.nftables.org/wiki-nftables/index.php/Concatenations
 
+    $_port = [$port].flatten
+
     # figure out transport protocol statements
-    if !$port.empty() and $port_range {
+    if !$_port.empty() and $port_range {
         fail('You can only pass an array of ports or a range, but not both')
     }
 
-    if !$port.empty() {
+    if !$_port.empty() {
         $port_stmt = "${proto} dport { ${port.sort.join(', ')} }"
     } elsif $port_range {
         if $port_range[0] >= $port_range[1] {

modules/nftables/types/port.pp
@@ -1,0 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# The port(s) can be configured as an array of Stdlib::Port or a single Stdlib::Port
+type Nftables::Port = Variant[
+    Array[Stdlib::Port],
+    Stdlib::Port,
+]

commit b59b478cff085b23fd3671403552f5afc3c0a847

nftables: fix port statement generation
Use the correct variable for the port.

A test case is included that fails if this fix is not applied.

This fixes: 7870e9ee4a ("Make nftables::service types more compatible")

Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
Change-Id: Ic48684b26ac2297280c024b8080bc696163bb9dc

modules/nftables/manifests/service.pp
@@ -42,7 +42,7 @@ define nftables::service (
     }
 
     if !$_port.empty() {
-        $port_stmt = "${proto} dport { ${port.sort.join(', ')} }"
+        $port_stmt = "${proto} dport { ${_port.sort.join(', ')} }"
     } elsif $port_range {
         if $port_range[0] >= $port_range[1] {
             fail("Incorrect port range ${port_range[0]} >= ${port_range[1]}")

modules/nftables/spec/defines/service_spec.rb
@@ -48,6 +48,15 @@ describe 'nftables::service' do
         end
       end
 
+      describe 'single port as integer, no array' do
+        let(:params) { super().merge(port: 1234) }
+        it { is_expected.to compile.with_all_deps }
+        it do
+          is_expected.to contain_file('/etc/nftables/input/10_test_service.nft')
+            .with_content("# Managed by puppet\n# some desc\ntcp dport { 1234 } accept\n")
+        end
+      end
+
       describe 'custom prio - small number' do
         let(:params) { super().merge(port: [80], prio: 3) }
         it { is_expected.to compile.with_all_deps }