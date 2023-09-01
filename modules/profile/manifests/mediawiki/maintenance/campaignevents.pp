5020dbd4297cdc84045964410e84a802663b581a
Clément Goubert <cgoubert@wikimedia.org>
2023-09-01 11:43:29 +0200

    P:mediawiki::maintenance: CampaignEvents periodic
    Add a periodic run every 3 hours of
    extensions/CampaignEvents/maintenance/AggregateParticipantAnswers.php
    - beta: metawiki
    - prod: metawiki, testwiki, test2wiki and officewiki.
    
    Hosts: auto
    Bug: T339984
    Change-Id: I76cdb118d4201e3676c75f4755aeae303c99ae88

--- modules/profile/manifests/mediawiki/maintenance/campaignevents.pp
+++ modules/profile/manifests/mediawiki/maintenance/campaignevents.pp
@@ -6,11 +6,22 @@ class profile::mediawiki::maintenance::campaignevents {
         command  => '/usr/local/bin/mwscript extensions/CampaignEvents/maintenance/UpdateUTCTimestamps.php --wiki metawiki',
         interval => '02:52'
     }
+    profile::mediawiki::periodic_job { 'campaignevents-aggregateparticipantanswers-metawiki':
+        command  => '/usr/local/bin/mwscript extensions/CampaignEvents/maintenance/AggregateParticipantAnswers.php --wiki metawiki',
+        interval => '00/03:00',
+        splay    => 300,
+    }
+
     unless $::realm == 'labs' {
         # group0: test.wikipedia.org
         profile::mediawiki::periodic_job { 'campaignevents-updateutcts-testwiki':
             command  => '/usr/local/bin/mwscript extensions/CampaignEvents/maintenance/UpdateUTCTimestamps.php --wiki testwiki',
             interval => '03:12'
+        }
+        profile::mediawiki::periodic_job { 'campaignevents-aggregateparticipantanswers-testwiki':
+            command  => '/usr/local/bin/mwscript extensions/CampaignEvents/maintenance/AggregateParticipantAnswers.php --wiki testwiki',
+            interval => '00/03:00',
+            splay    => 300,
         }
 
         # group0: office.mediawiki.org
@@ -18,11 +29,21 @@ class profile::mediawiki::maintenance::campaignevents {
             command  => '/usr/local/bin/mwscript extensions/CampaignEvents/maintenance/UpdateUTCTimestamps.php --wiki officewiki',
             interval => '03:32'
         }
+        profile::mediawiki::periodic_job { 'campaignevents-aggregateparticipantanswers-officewiki':
+            command  => '/usr/local/bin/mwscript extensions/CampaignEvents/maintenance/AggregateParticipantAnswers.php --wiki officewiki',
+            interval => '00/03:00',
+            splay    => 300,
+        }
 
         # group1: test2.wikipedia.org
         profile::mediawiki::periodic_job { 'campaignevents-updateutcts-test2wiki':
             command  => '/usr/local/bin/mwscript extensions/CampaignEvents/maintenance/UpdateUTCTimestamps.php --wiki test2wiki',
             interval => '03:52'
+        }
+        profile::mediawiki::periodic_job { 'campaignevents-aggregateparticipantanswers-test2wiki':
+            command  => '/usr/local/bin/mwscript extensions/CampaignEvents/maintenance/AggregateParticipantAnswers.php --wiki test2wiki',
+            interval => '00/03:00',
+            splay    => 300,
         }
     }
 }
