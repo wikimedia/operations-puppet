4fd6e2d80f9d19cb5348fd2bea787c241e6fd359
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-09-01 09:42:55 +0200

    Update comment
    Change-Id: Icfc427dc1d347a5c370192a9dbc863c1817e3c2e

--- modules/prometheus/manifests/elasticsearch_exporter/common.pp
+++ modules/prometheus/manifests/elasticsearch_exporter/common.pp
@@ -10,7 +10,7 @@ class prometheus::elasticsearch_exporter::common {
         ensure => absent,
     }
     # mask the default service to make sure its not restarted on package upgrades
-    # Also clean up this and use the mask param for service when logstash servers have been upgraded to stretch
+    # TODO: clean this up and use the mask param for service
     exec { 'mask_default_prometheus_elasticsearch_exporter':
         command => '/bin/systemctl mask prometheus-elasticsearch-exporter.service',
         creates => '/etc/systemd/system/prometheus-elasticsearch-exporter.service',
