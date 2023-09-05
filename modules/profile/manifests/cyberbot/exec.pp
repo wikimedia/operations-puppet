31f5fbfb2d4407af8ffd32ef81972554a64345e8
Taavi Väänänen <hi@taavi.wtf>
2023-09-05 21:37:46 +0300

    P:cyberbot: add bookworm support
    Change-Id: I10db87c2dd8cdd86791ab16990589b9dbc78c09a

--- modules/profile/manifests/cyberbot/exec.pp
+++ modules/profile/manifests/cyberbot/exec.pp
@@ -3,6 +3,7 @@ class profile::cyberbot::exec{
 
     # switch to a fact once T271196 is resolved
     $php_version = debian::codename() ? {
+        'bookworm' => '8.2',
         'bullseye' => '7.4',
         'buster'   => '7.3',
         default    => fail("profile::cyberbot::exec currently unsupported on debian ${debian::codename()}"),
