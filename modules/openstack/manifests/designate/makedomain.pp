911a4ba0e914ffc6cb1a0a4c690b9984b2e8074f
Andrew Bogott <abogott@wikimedia.org>
2023-09-08 11:38:39 -0500

    designatemakedomain: don't install for python2
    We haven't used python2 on cloudservices nodes for a while.
    
    Change-Id: I29fb34ef71429c1f45f9f61cc43ee2bf0880fc6d

--- modules/openstack/manifests/designate/makedomain.pp
+++ modules/openstack/manifests/designate/makedomain.pp
@@ -1,13 +1,5 @@
 class openstack::designate::makedomain
 {
-    # Functions to create or delete designate domains under .wmflabs.org
-    file { '/usr/lib/python2.7/dist-packages/designatemakedomain.py':
-        ensure => 'present',
-        source => 'puppet:///modules/openstack/designate/designatemakedomain.py',
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
-    }
     file { '/usr/lib/python3/dist-packages/designatemakedomain.py':
         ensure => 'present',
         source => 'puppet:///modules/openstack/designate/designatemakedomain.py',
