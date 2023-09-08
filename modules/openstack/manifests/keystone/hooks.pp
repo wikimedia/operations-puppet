241686078ce1ce88d2c0b5d2d50a34358dfd61dc
Andrew Bogott <abogott@wikimedia.org>
2023-09-08 11:39:57 -0500

    wmfkeystonehooks: don't install python2 versions
    Change-Id: I9e5e002f66d911093f76d92db58323e0330667fa

--- modules/openstack/manifests/keystone/hooks.pp
+++ modules/openstack/manifests/keystone/hooks.pp
@@ -7,24 +7,6 @@ class openstack::keystone::hooks(
     include openstack::designate::makedomain
     include openstack::keystone::service
 
-    file { '/usr/lib/python2.7/dist-packages/wmfkeystonehooks':
-        source  => "puppet:///modules/openstack/${version}/keystone/wmfkeystonehooks",
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        recurse => true,
-        notify  => Service[$wsgi_server],
-    }
-
-    file { '/usr/lib/python2.7/dist-packages/wmfkeystonehooks.egg-info':
-        source  => "puppet:///modules/openstack/${version}/keystone/wmfkeystonehooks.egg-info",
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        recurse => true,
-        notify  => Service[$wsgi_server],
-    }
-
     file { '/usr/lib/python3/dist-packages/wmfkeystonehooks':
         source  => "puppet:///modules/openstack/${version}/keystone/wmfkeystonehooks",
         owner   => 'root',
