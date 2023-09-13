f0b63f634d11184c6c520360584e28d95b689db3
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-09-13 16:08:38 +0200

    Drop the rsync migration setup for testreduce
    The mariadb DB won't be migrated after all, but instead reinitialised
    on the new host.
    
    Bug: T345831
    Change-Id: If26132a14ed57ba09a1de160b58786eb6d3aee1f

--- modules/profile/manifests/parsoid/testreduce.pp
+++ modules/profile/manifests/parsoid/testreduce.pp
@@ -6,13 +6,5 @@ class profile::parsoid::testreduce(
         install_node => $install_node,
     }
 
-    rsync::quickdatacopy { 'testreduce-update':
-        source_host         => 'testreduce1001.eqiad.wmnet',
-        dest_host           => 'testreduce1002.eqiad.wmnet',
-        auto_sync           => false,
-        module_path         => '/srv/data',
-        server_uses_stunnel => true,
-    }
-
     ensure_packages(['make', 'g++'])
 }
