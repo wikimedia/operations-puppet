553be3c611af80dbaf47c470c0bec448217220a0
Andrew Bogott <abogott@wikimedia.org>
2023-09-12 19:00:23 -0500

    wmf_sink: correct calls to get_keystone_session
    now this is in wmfdesignatelib
    
    Change-Id: I23331cadae8fff01bfbcf214953c7b5f9e446c79

--- modules/openstack/files/zed/designate/wmf_sink/base.py
+++ modules/openstack/files/zed/designate/wmf_sink/base.py
@@ -36,8 +36,14 @@ class BaseAddressWMFHandler(BaseAddressHandler):
     proxy_endpoint = ""
     enc_endpoint = ""
 
-    def _delete(self, extra, managed=True, resource_id=None,
-                resource_type='instance', criterion={}):
+    def _delete(
+        self,
+        extra,
+        managed=True,
+        resource_id=None,
+        resource_type="instance",
+        criterion={},
+    ):
         """
         Handle a generic delete of a fixed ip within a zone
 
@@ -49,15 +55,17 @@ class BaseAddressWMFHandler(BaseAddressHandler):
         zone = self.get_zone(cfg.CONF[self.name].domain_id)
 
         data = extra.copy()
-        LOG.debug('Event data: %s' % data)
-        data['zone'] = zone['name']
+        LOG.debug("Event data: %s" % data)
+        data["zone"] = zone["name"]
 
-        data['project_name'] = wmfdesignatelib.project_name_from_id(keystone, data['tenant_id'])
+        data["project_name"] = wmfdesignatelib.project_name_from_id(
+            keystone, data["tenant_id"]
+        )
 
         event_data = data.copy()
 
         fqdn = cfg.CONF[self.name].fqdn_format % event_data
-        fqdn = fqdn.rstrip('.')
+        fqdn = fqdn.rstrip(".")
 
         region_recs = keystone.regions.list()
         regions = [region.id for region in region_recs]
@@ -67,39 +75,46 @@ class BaseAddressWMFHandler(BaseAddressHandler):
             for region in regions:
                 if region == cfg.CONF[self.name].region:
                     continue
-                nova = novaclient.Client('2', session=self._get_keystone_session(data['tenant_id']),
-                                         region_name=region)
+                nova = novaclient.Client(
+                    "2",
+                    session=wmfdesignatelib.get_keystone_session(data["tenant_id"]),
+                    region_name=region,
+                )
                 servers = nova.servers.list()
                 servernames = [server.name for server in servers]
-                if event_data['hostname'] in servernames:
-                    LOG.warning("Skipping cleanup of %s because it is also present in region %s" %
-                                (fqdn, region))
+                if event_data["hostname"] in servernames:
+                    LOG.warning(
+                        "Skipping cleanup of %s because it is also present in region %s"
+                        % (fqdn, region)
+                    )
                     return
 
         # Clean puppet keys for deleted instance
         if cfg.CONF[self.name].puppet_master_host:
-            LOG.debug('Cleaning puppet key %s' % fqdn)
-            self._run_remote_command(cfg.CONF[self.name].puppet_master_host,
-                                     cfg.CONF[self.name].certmanager_user,
-                                     'sudo puppet cert clean %s' %
-                                     pipes.quote(fqdn))
+            LOG.debug("Cleaning puppet key %s" % fqdn)
+            self._run_remote_command(
+                cfg.CONF[self.name].puppet_master_host,
+                cfg.CONF[self.name].certmanager_user,
+                "sudo puppet cert clean %s" % pipes.quote(fqdn),
+            )
 
         # Clean up the puppet config for this instance, if there is one
-        self._delete_puppet_config(data['tenant_id'], fqdn)
+        self._delete_puppet_config(data["tenant_id"], fqdn)
 
         # For good measure, look around for things associated with the old domain as well
         if cfg.CONF[self.name].legacy_domain_id:
             legacy_zone = self.get_zone(cfg.CONF[self.name].legacy_domain_id)
             legacy_data = data.copy()
-            legacy_data['zone'] = legacy_zone['name']
+            legacy_data["zone"] = legacy_zone["name"]
             legacy_fqdn = cfg.CONF[self.name].fqdn_format % legacy_data
-            legacy_fqdn = legacy_fqdn.rstrip('.')
+            legacy_fqdn = legacy_fqdn.rstrip(".")
             if cfg.CONF[self.name].puppet_master_host:
-                LOG.debug('Cleaning puppet key %s' % legacy_fqdn)
-                self._run_remote_command(cfg.CONF[self.name].puppet_master_host,
-                                         cfg.CONF[self.name].certmanager_user,
-                                         'sudo puppet cert clean %s' %
-                                         pipes.quote(legacy_fqdn))
+                LOG.debug("Cleaning puppet key %s" % legacy_fqdn)
+                self._run_remote_command(
+                    cfg.CONF[self.name].puppet_master_host,
+                    cfg.CONF[self.name].certmanager_user,
+                    "sudo puppet cert clean %s" % pipes.quote(legacy_fqdn),
+                )
 
         # Finally, delete any proxy records pointing to this instance.
         #
@@ -107,14 +122,16 @@ class BaseAddressWMFHandler(BaseAddressHandler):
         crit = criterion.copy()
 
         # Make sure we only look at forward records
-        crit['zone_id'] = cfg.CONF[self.name].domain_id
+        crit["zone_id"] = cfg.CONF[self.name].domain_id
 
         if managed:
-            crit.update({
-                'managed': managed,
-                'managed_resource_id': resource_id,
-                'managed_resource_type': resource_type
-            })
+            crit.update(
+                {
+                    "managed": managed,
+                    "managed_resource_id": resource_id,
+                    "managed_resource_type": resource_type,
+                }
+            )
 
         context = DesignateContext().elevated()
         context.all_tenants = True
@@ -127,23 +144,33 @@ class BaseAddressWMFHandler(BaseAddressHandler):
             ip = records[0].data
             LOG.debug("Cleaning up proxy records for IP %s" % ip)
             try:
-                self._delete_proxies_for_ip(data['project_name'], ip)
+                self._delete_proxies_for_ip(data["project_name"], ip)
             except requests.exceptions.ConnectionError:
-                LOG.warning("Caught exception when deleting proxy records", exc_info=True)
+                LOG.warning(
+                    "Caught exception when deleting proxy records", exc_info=True
+                )
 
     @staticmethod
     def _run_remote_command(server, username, command):
-        ssh_command = ['/usr/bin/ssh', '-o', 'StrictHostKeyChecking=no',
-                       '-l%s' % username, server, command]
+        ssh_command = [
+            "/usr/bin/ssh",
+            "-o",
+            "StrictHostKeyChecking=no",
+            "-l%s" % username,
+            server,
+            command,
+        ]
 
-        p = subprocess.Popen(ssh_command,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
+        p = subprocess.Popen(
+            ssh_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
         (out, error) = p.communicate()
         rcode = p.wait()
         if rcode:
-            LOG.warning("Remote command %s failed with output %s and err %s" %
-                        (ssh_command, out, error))
+            LOG.warning(
+                "Remote command %s failed with output %s and err %s"
+                % (ssh_command, out, error)
+            )
         return out, error, rcode
 
     def _delete_puppet_config(self, project, fqdn):
@@ -163,14 +190,16 @@ class BaseAddressWMFHandler(BaseAddressHandler):
     def _delete_proxies_for_ip(self, project, ip):
         project_proxies = self._get_proxy_list_for_project(project)
         for proxy in project_proxies:
-            if len(proxy['backends']) > 1:
-                LOG.warning("This proxy record has multiple backends. "
-                            "That's unexpected and not handled, "
-                            "we may be leaking proxy records.")
-            elif proxy['backends'][0].split(":")[1].strip('/') == ip:
+            if len(proxy["backends"]) > 1:
+                LOG.warning(
+                    "This proxy record has multiple backends. "
+                    "That's unexpected and not handled, "
+                    "we may be leaking proxy records."
+                )
+            elif proxy["backends"][0].split(":")[1].strip("/") == ip:
                 # found match, deleting.
                 LOG.debug("Cleaning up proxy record %s" % proxy)
-                zone = proxy['domain']
+                zone = proxy["domain"]
 
                 proxy_url, session = self._get_proxy_client(project)
                 session.delete("{}/mapping/{}".format(proxy_url, zone))
@@ -178,17 +207,16 @@ class BaseAddressWMFHandler(BaseAddressHandler):
     def _get_proxy_list_for_project(self, project):
         proxy_url, session = self._get_proxy_client(project)
         resp = session.get("{}/mapping".format(proxy_url), raise_exc=False)
-        if resp.status_code == 400 and resp.text == 'No such project':
+        if resp.status_code == 400 and resp.text == "No such project":
             return []
         elif not resp:
-            raise Exception("Proxy service request got status " +
-                            str(resp.status_code))
+            raise Exception("Proxy service request got status " + str(resp.status_code))
         else:
-            return resp.json()['routes']
+            return resp.json()["routes"]
 
     def _get_proxy_client(self, project):
         proxy_url = self._get_proxy_endpoint().replace("$(tenant_id)s", project)
-        session = self._get_keystone_session(project)
+        session = wmfdesignatelib.get_keystone_session(project)
         return proxy_url, session
 
     def _get_proxy_endpoint(self):
@@ -198,14 +226,15 @@ class BaseAddressWMFHandler(BaseAddressHandler):
             services = keystone.services.list()
 
             for service in services:
-                if service.type == 'proxy':
+                if service.type == "proxy":
                     serviceid = service.id
                     break
 
-            endpoints = keystone.endpoints.list(service=serviceid,
-                                                region=cfg.CONF[self.name].region)
+            endpoints = keystone.endpoints.list(
+                service=serviceid, region=cfg.CONF[self.name].region
+            )
             for endpoint in endpoints:
-                if endpoint.interface == 'public':
+                if endpoint.interface == "public":
                     self.proxy_endpoint = endpoint.url
                     break
 
@@ -216,7 +245,7 @@ class BaseAddressWMFHandler(BaseAddressHandler):
 
     def _get_enc_client(self, project):
         proxy_url = self._get_enc_endpoint().replace("$(project_id)s", project)
-        session = self._get_keystone_session(project)
+        session = wmfdesignatelib.get_keystone_session(project)
         return proxy_url, session
 
     def _get_enc_endpoint(self):
@@ -225,6 +254,7 @@ class BaseAddressWMFHandler(BaseAddressHandler):
             service = keystone.services.list(type="puppet-enc")[0]
 
             self.enc_endpoint = keystone.endpoints.list(
-                service=service.id, interface="public", enabled=True)[0].url
+                service=service.id, interface="public", enabled=True
+            )[0].url
 
         return self.enc_endpoint
