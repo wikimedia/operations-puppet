2341c3303ac4370696f55cc117eace82ef2f5401
Balthazar Rouberol <brouberol@wikimedia.org>
2023-09-18 12:13:43 +0200

    Fix kafka-jumbo node regular expression
    Bug: T336041
    Change-Id: I070f9be2332f0a8f6840bda68b5b8ef6d605bf5c
    Hosts: C:profile::cache::kafka::webrequest
    Hosts: C:profile::cache::kafka::eventlogging
    Hosts: C:profile::mjolnir::kafka_msearch_daemon
    Hosts: C:profile::kubernetes::deployment_server::mediawiki::config
    Hosts: C:profile::benthos
    Hosts: kafka-jumbo1001.eqiad.wmnet
    Hosts: kafka-jumbo1010.eqiad.wmnet

--- manifests/site.pp
+++ manifests/site.pp
@@ -1558,7 +1558,7 @@ node /^kafka-main200[1-5]\.codfw\./ {
 # kafka-jumbo is a large general purpose Kafka cluster.
 # This cluster exists only in eqiad, and serves various uses, including
 # mirroring all data from the main Kafka clusters in both main datacenters.
-node /^kafka-jumbo10[01-10]\.eqiad\./ {
+node /^kafka-jumbo10(0[1-9]|10)\.eqiad\./ {
     role(kafka::jumbo::broker)
 }
 
