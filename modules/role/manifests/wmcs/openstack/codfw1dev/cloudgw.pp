c3187d4dace64ac910b0a32d918285713084c6f2
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-09-19 12:50:54 +0200

    Switch cloudgw/codfw1dev to profile::firewall
    Hosts: cloudgw1001.eqiad.wmnet,cloudgw1002.eqiad.wmnet,cloudgw2002-dev.codfw.wmnet
    Bug: T336497
    Change-Id: I4c96ec81e081487fd7096d765a8dc6340ada4647

--- modules/role/manifests/wmcs/openstack/codfw1dev/cloudgw.pp
+++ modules/role/manifests/wmcs/openstack/codfw1dev/cloudgw.pp
@@ -1,8 +1,8 @@
 class role::wmcs::openstack::codfw1dev::cloudgw {
     system::role { $name: }
-    # do not add base firewall
+
     include profile::base::production
-    include profile::nftables::basefirewall
+    include profile::firewall
     include profile::base::cloud_production
     include profile::openstack::codfw1dev::cloudgw
 }
