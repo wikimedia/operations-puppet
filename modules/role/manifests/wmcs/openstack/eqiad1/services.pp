791202faa0dce75c90946758ecb07a4ae4dc7ba9
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2023-09-20 11:20:58 +0200

    openstack: eqiad1: services: enable cloud-private for cloudservices1005
    All eqiad1 servers are now cloud-private-enabled.
    
    While at it, make cloudlb aware of the new host.
    
    Bug: T346042
    Hosts: cloudservices1006.eqiad.wmnet, cloudlb1001.eqiad.wmnet
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>
    Change-Id: I3a68425e33ea911118f24732ed61c88a6e1a4c27

--- modules/role/manifests/wmcs/openstack/eqiad1/services.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/services.pp
@@ -3,11 +3,8 @@ class role::wmcs::openstack::eqiad1::services {
     include ::profile::base::production
     include ::profile::firewall
     include profile::base::cloud_production
-    if $facts['hostname'] == 'cloudservices1006' {
-        # the only server in the new network setup
-        include profile::wmcs::cloud_private_subnet
-        include profile::wmcs::cloud_private_subnet::bgp
-    }
+    include profile::wmcs::cloud_private_subnet
+    include profile::wmcs::cloud_private_subnet::bgp
     include ::profile::openstack::eqiad1::pdns::auth::db
     include ::profile::openstack::eqiad1::pdns::auth::service
     include ::profile::openstack::eqiad1::pdns::recursor::service
