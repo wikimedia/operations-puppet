6a83221b9d3fa85775d24e6c117d81c2781d035f
Taavi Väänänen <hi@taavi.wtf>
2023-09-22 11:46:26 +0300

    P:wmcs::cloud_private_subnet: fix typos
    Change-Id: Iee37cf19cbeb97c7083c35196f722dfd9a8db425

--- modules/profile/manifests/wmcs/cloud_private_subnet/bgp.pp
+++ modules/profile/manifests/wmcs/cloud_private_subnet/bgp.pp
@@ -6,10 +6,10 @@ class profile::wmcs::cloud_private_subnet::bgp (
     Hash[String, Wmflib::Advertise_vip]       $vips               = lookup('profile::bird::advertise_vips', { 'merge' => 'hash' }),
     Netbox::Device::Location                  $netbox_location    = lookup('profile::netbox::host::location'),
 ) {
-    $cloud_private_address = dnsquery::a($cloud_private_host) || { fail("failed ot resolv '${cloud_private_host}'") }[0]
+    $cloud_private_address = dnsquery::a($cloud_private_host) || { fail("failed to resolve '${cloud_private_host}'") }[0]
     $rack = downcase($netbox_location['rack'])
     $cloud_private_gw = inline_epp($cloud_private_gw_t, { 'rack' => $rack })
-    $gw_address = dnsquery::a($cloud_private_gw) || { fail("failed ot resolv '${cloud_private_gw}'") }[0]
+    $gw_address = dnsquery::a($cloud_private_gw) || { fail("failed to resolve '${cloud_private_gw}'") }[0]
 
     $vlan_id = $vlan_mapping[$::site][$rack]
     $interface = "vlan${vlan_id}"
