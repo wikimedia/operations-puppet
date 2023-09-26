589545d1a21538c0ca738e81dd3d2df870649dcc
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-09-26 13:23:24 +0200

    firewall: Add explicit check for provider == 'none'
    If the provider is no there's no need to trigger the ferm->nftables
    migration path, so explicitly check for it.
    
    Otherwise get end up with spurious absented ferm-related Puppet
    resources.
    
    Bug: T336497
    Hosts: C:firewall
    Hosts: cp3070.esams.wmnet,cloudvirt1053.eqiad.wmnet,cloudnet1005.eqiad.wmnet
    Change-Id: Ia53cd9ade4e0fdf5d7294edf103402c06bbf21e1

--- modules/firewall/manifests/init.pp
+++ modules/firewall/manifests/init.pp
@@ -4,15 +4,17 @@
 class firewall (
     Firewall::Provider $provider = 'none',
 ) {
-    class { 'ferm':  # lint:ignore:wmf_styleguide
-        ensure => stdlib::ensure($provider == 'ferm'),
-    }
+    unless $provider == 'none' {
+        class { 'ferm': # lint:ignore:wmf_styleguide
+            ensure => stdlib::ensure($provider == 'ferm'),
+        }
 
-    # There is currently no Puppet-driven migration path ferm->nft,
-    # so always pass ensure=>present if the nftables provider is selected
-    if $provider == 'nftables' {
-        class { '::nftables': # lint:ignore:wmf_styleguide
-            ensure => 'present',
+        # There is currently no Puppet-driven migration path ferm->nft,
+        # so always pass ensure=>present if the nftables provider is selected
+        if $provider == 'nftables' {
+            class { '::nftables': # lint:ignore:wmf_styleguide
+                ensure => 'present',
+            }
         }
     }
 }
