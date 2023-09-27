f811f90e6ffa93fe8335950e66a28a25923316dd
Luca Toscano <ltoscano@wikimedia.org>
2023-09-27 15:55:29 +0200

    role::kafka::jumbo: exclude kafka-jumbo100[1-6] from Mirror Maker
    We are currently dealing with a nasty bug in Kafka/Mirror maker,
    that triggers an inconsistent state in the Mirror Maker consumer
    group when the number of instances cross a certain threshold.
    
    As temporary workaround, we remove mirror maker from kafka-jumbo100[1-6]
    that will be decommend soon.
    
    Bug: T347481
    Change-Id: Ic253ba1119d56cce9752c333819a82e8168480b0

--- modules/role/manifests/kafka/jumbo/broker.pp
+++ modules/role/manifests/kafka/jumbo/broker.pp
@@ -9,8 +9,12 @@ class role::kafka::jumbo::broker {
     include profile::firewall
     include profile::kafka::broker
 
-    # Mirror main-eqiad -> jumbo-eqiad
-    include profile::kafka::mirror
+    # Temporary workaround to avoid Kafka Mirror failures
+    # See T347481
+    if $::hostname !~ /^kafka-jumbo100[1-6]/ {
+        # Mirror main-eqiad -> jumbo-eqiad
+        include profile::kafka::mirror
+    }
 
     include ::profile::base::production
 }
