204c57d50d0837764762f2c3eaa86325012b45d7
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-09-27 12:47:34 +0200

    Switch main cloudgw hosts to profile::firewall
    Hosts: cloudgw1001.eqiad.wmnet,cloudgw1002.eqiad.wmnet
    Bug: T336497
    Change-Id: I789a139339e7420f8db4699ac7cfa2421410cfca

--- modules/role/manifests/wmcs/openstack/eqiad1/cloudgw.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/cloudgw.pp
@@ -1,8 +1,7 @@
 class role::wmcs::openstack::eqiad1::cloudgw {
     system::role { $name: }
     include profile::base::production
-    # do not add ferm-based base firewall profile, these servers use native nftables
-    include profile::nftables::basefirewall
+    include profile::firewall
     include profile::base::cloud_production
     include profile::openstack::eqiad1::cloudgw
 }
