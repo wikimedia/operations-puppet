900db7c282a002049430f8a8d916ba87dbdc72f6
Keith Herron <kherron@wikimedia.org>
2023-09-28 13:39:29 -0400

    thanos::httpd: include rewrite module
    Bug: T302995
    Change-Id: Ic9924d1497f7853c0728f7be41af2c6be454f7c0

--- modules/profile/manifests/thanos/httpd.pp
+++ modules/profile/manifests/thanos/httpd.pp
@@ -18,7 +18,7 @@ class profile::thanos::httpd (
     Integer                    $maxconn    = lookup('profile::thanos::httpd::maxconn'),
 ) {
     class { 'httpd':
-        modules => ['proxy_http', 'allowmethods'],
+        modules => ['proxy_http', 'allowmethods', 'rewrite'],
     }
 
     profile::idp::client::httpd::site {'thanos.wikimedia.org':
