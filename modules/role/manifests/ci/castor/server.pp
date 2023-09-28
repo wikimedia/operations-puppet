a345d00f21fc2f34f25faadce5f23806dae38d37
Antoine Musso <hashar@free.fr>
2023-09-28 17:36:37 +0200

    ci: manage cinder volume on Castor instance
    integration-castor05 has an attached Cinder volume to store the CI
    caches. I had them created with `prepare-cinder-volume` which adds an
    fstab entry for it.
    
    Today I have rebuild the instance and reattached the volume but it was
    not mounted since the fstab entry was not on the new host. Add
    `profile::labs::cindermount::srv` which detects attached volume and
    mount them to `/srv`.
    
    Since `/srv` is a Cinder volume we do not want to trigger the LVM logic
    which manages extended disk space. The instance in horizon has:
    
      profile::ci::slave::labs::common::manage_srv: false
    
    Pass it to the profile at the role level, this way we have all the logic
    in Puppet and in Horizon we would solely apply the role.
    
    Bug: T304080
    Bug: T345924
    Change-Id: I7926e97b11b8f9b26b5dd735bdff061c657c669d

--- modules/role/manifests/ci/castor/server.pp
+++ modules/role/manifests/ci/castor/server.pp
@@ -9,7 +9,14 @@ class role::ci::castor::server {
         description => 'rsync server to store caches artifacts'
     }
 
-    include ::profile::ci::slave::labs::common
+    # Castor uses an attached volume...
+    include profile::labs::cindermount::srv
+
+    class { '::profile::ci::slave::labs::common':
+        # ... and we thus skip the logic to manage extended disk space
+        manage_srv => false
+    }
+
     include ::profile::ci::castor::server
 
     Class['::profile::ci::slave::labs::common'] ~> Class['::profile::ci::castor::server']
