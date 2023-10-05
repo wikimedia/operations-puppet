26c484abf42dd3d2105cc4b8e5591142d6575019
jbond <jbond@wikimedia.org>
2023-10-05 19:51:12 +0200

    htpasswd: update to make a bit closer to standard puppet style
    On puppet7 we get the following error
       org.jruby.exceptions.LoadError: (LoadError) no such file to load -- apr1md5
    I suspect this is because jruby has a different load path.  update to
    move things in to the puppet_x folder and use require_relative which is
    the more common standard and i believe the advice approach
    
    Hosts: C:docker::registry::web
    Hosts: C:icinga::web
    Hosts: P:mediawiki::php::monitoring
    Hosts: P:etcd::tlsproxy
    Hosts: P:sretest
    Hosts: P:icinga
    Bug: T348272
    Change-Id: I56ce39b6dd9fd2527375e080d5af5304b7db0254

--- modules/wmflib/lib/puppet/parser/functions/htpasswd.rb
+++ modules/wmflib/lib/puppet/parser/functions/htpasswd.rb
@@ -7,14 +7,14 @@
 # See <https://httpd.apache.org/docs/current/misc/password_encryptions.html>
 # for an explanation about the apr1md5 format.
 #
-require 'apr1md5'
+require_relative '../../../puppet_x/wmflib/apr1md5.rb'
 
 module Puppet::Parser::Functions
   newfunction(:htpasswd, :type => :rvalue, :arity => 2) do |args|
     if args[1].length != 8
       fail(ArgumentError, 'htpasswd(): salt must be 8 characters')
     end
-    generator = Apr1Md5.new args[1]
+    generator = PuppetX::Wmflib::Apr1Md5.new args[1]
     generator.encode args[0]
   end
 end
