b4ef93e7ff2db1a8174f47f447888eace736ae4d
Cole White <cwhite@wikimedia.org>
2023-10-05 22:35:36 +0000

    opensearch: disable shard size check on logging opensearch
     * Move icinga::monitor::opensearch::base_checks to
       profile::opensearch::monitoring
     * Expand nrpe resources
     * Gate opensearch_shard_size_check_${port}
     * Disable check in hiera
    
    Bug: T348262
    Change-Id: I1e450d252c8f0ca8e29c481d606575515561153c

--- modules/profile/manifests/opensearch/monitoring/base_checks.pp
+++ modules/profile/manifests/opensearch/monitoring/base_checks.pp
@@ -1,23 +1,49 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::opensearch::monitoring::base_checks(
-    Integer $shard_size_warning  = lookup('profile::opensearch::monitoring::shard_size_warning',  { 'default_value' => 110 }),
-    Integer $shard_size_critical = lookup('profile::opensearch::monitoring::shard_size_critical', { 'default_value' => 140 }),
-    String $threshold            = lookup('profile::opensearch::monitoring::threshold',           { 'default_value' => '>=0.15' }),
-    Integer $timeout             = lookup('profile::opensearch::monitoring::timeout',             { 'default_value' => 4 }),
+    Integer $shard_size_warning      = lookup('profile::opensearch::monitoring::shard_size_warning',      { 'default_value' => 110 }),
+    Integer $shard_size_critical     = lookup('profile::opensearch::monitoring::shard_size_critical',     { 'default_value' => 140 }),
+    String  $threshold               = lookup('profile::opensearch::monitoring::threshold',               { 'default_value' => '>=0.15' }),
+    Integer $timeout                 = lookup('profile::opensearch::monitoring::timeout',                 { 'default_value' => 4 }),
+    Boolean $enable_shard_size_check = lookup('profile::opensearch::monitoring::enable_shard_size_check', { 'default_value' => true }),
 ) {
     require ::profile::opensearch::server
+
+    class { 'icinga::elasticsearch::base_plugin': }
 
     $configured_ports = $::profile::opensearch::server::filtered_instances.reduce([]) |$ports, $instance_params| {
         $ports + [$instance_params[1]['http_port']]
     }
 
-    # For monitoring of eqiad/codfw cirrus clusters, see icinga::monitor::elasticsearch::cirrus_cluster_checks
-    icinga::monitor::opensearch::base_checks { $::hostname:
-        ports               => $configured_ports,
-        shard_size_warning  => $shard_size_warning,
-        shard_size_critical => $shard_size_critical,
-        timeout             => $timeout,
-        threshold           => $threshold,
-        use_nrpe            => true,
+    $configured_ports.each |$port| {
+        nrpe::monitor_service { "opensearch_shards_${port}":
+          critical      => false,
+          contact_group => 'admins,team-discovery',
+          notes_url     => 'https://wikitech.wikimedia.org/wiki/Search#Administration',
+          nrpe_command  => "/usr/lib/nagios/plugins/check_elasticsearch.py --ignore-status --url http://localhost:${port} --shards-inactive '${threshold}' --timeout ${timeout}",
+          description   => "OpenSearch health check for shards on ${port}",
+        }
+
+        nrpe::monitor_service { "opensearch_unassigned_shard_check_${port}":
+          critical       => false,
+          contact_group  => 'admins,team-discovery',
+          notes_url      => 'https://wikitech.wikimedia.org/wiki/Search#Administration',
+          nrpe_command   => "/usr/lib/nagios/plugins/check_elasticsearch_unassigned_shards.py --url http://localhost:${port} --timeout ${timeout}",
+          description    => "OpenSearch unassigned shard check - ${port}",
+          check_interval => 720, # 12h
+          retry_interval => 120, # 2h
+          retries        => 1,
+        }
+
+        if $enable_shard_size_check {
+            nrpe::monitor_service { "opensearch_shard_size_check_${port}":
+              critical       => false,
+              contact_group  => 'admins,team-discovery',
+              notes_url      => 'https://wikitech.wikimedia.org/wiki/Search#If_it_has_been_indexed',
+              nrpe_command   => "/usr/lib/nagios/plugins/check_elasticsearch_shard_size.py --url http://localhost:${port} --shard-size-warning ${shard_size_warning} --shard-size-critical ${shard_size_critical} --timeout ${timeout}",
+              description    => "OpenSearch shard size check - ${port}",
+              check_interval => 1440, # 24h
+              retry_interval => 180, # 3h
+            }
+        }
     }
 }
