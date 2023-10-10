99ae3e0bf2ea3181792e80dbd1722340d2020e27
Taavi Väänänen <hi@taavi.wtf>
2023-10-10 12:43:00 +0300

    wiki-replicas: Update IP address for cloudcontrol1006
    The host was renumbered to private IP space.
    
    Bug: T347381
    Change-Id: If95621673ebaf7b6a526d1b071cedef9368947a8

--- modules/role/templates/mariadb/grants/wiki-replicas.sql
+++ modules/role/templates/mariadb/grants/wiki-replicas.sql
@@ -9,11 +9,11 @@ GRANT SELECT, SHOW VIEW ON `%wik%`.* TO 'labsdbadmin'@'10.64.151.3';
 GRANT SELECT, SHOW VIEW ON `%\\_p`.* TO 'labsdbadmin'@'10.64.151.3' WITH GRANT OPTION;
 
 -- cloudcontrol1006 - for maintaindbusers T331014
-GRANT labsdbuser TO 'labsdbadmin'@'208.80.154.149' WITH ADMIN OPTION;
-GRANT SUPER, CREATE USER ON *.* TO 'labsdbadmin'@'208.80.154.149' WITH GRANT OPTION;
-GRANT SELECT, INSERT, UPDATE ON `mysql`.* TO 'labsdbadmin'@'208.80.154.149';
-GRANT SELECT, SHOW VIEW ON `%wik%`.* TO 'labsdbadmin'@'208.80.154.149';
-GRANT SELECT, SHOW VIEW ON `%\\_p`.* TO 'labsdbadmin'@'208.80.154.149' WITH GRANT OPTION;
+GRANT labsdbuser TO 'labsdbadmin'@'10.64.150.6' WITH ADMIN OPTION;
+GRANT SUPER, CREATE USER ON *.* TO 'labsdbadmin'@'10.64.150.6' WITH GRANT OPTION;
+GRANT SELECT, INSERT, UPDATE ON `mysql`.* TO 'labsdbadmin'@'10.64.150.6';
+GRANT SELECT, SHOW VIEW ON `%wik%`.* TO 'labsdbadmin'@'10.64.150.6';
+GRANT SELECT, SHOW VIEW ON `%\\_p`.* TO 'labsdbadmin'@'10.64.150.6' WITH GRANT OPTION;
 
 -- cloudcontrol1007 - for maintaindbusers T331014
 CREATE USER 'labsdbadmin'@'10.64.148.21';
