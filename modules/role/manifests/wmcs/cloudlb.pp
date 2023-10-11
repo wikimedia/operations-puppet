a6a0a25eee9f992e215472407fe26c03b0ea907b
Francesco Negri <fnegri@wikimedia.org>
2023-10-11 12:47:31 +0200

    wmcs::cloudlb: add cloud_production profile
    This will let cloudcumins reach cloudlb hosts.
    
    Change-Id: Iac7adbc3b7a629b70105f5e7f9a3eded1335320f

--- modules/role/manifests/wmcs/cloudlb.pp
+++ modules/role/manifests/wmcs/cloudlb.pp
@@ -6,6 +6,7 @@ class role::wmcs::cloudlb (
 
     include profile::base::production
     include profile::firewall
+    include profile::base::cloud_production
     include profile::wmcs::cloud_private_subnet
     include profile::wmcs::cloud_private_subnet::bgp
     include profile::wmcs::cloudlb::haproxy
