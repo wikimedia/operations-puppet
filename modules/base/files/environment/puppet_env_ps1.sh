9a2b9ea5a05ea02269c6f5941cdcfabf9c3673fe
Jesse Hathaway <jhathaway@wikimedia.org>
2023-10-12 10:56:00 -0500

    dev env: PS1 function for to show the puppet env
    This function is helpful to differentiate from being on a production
    host versus a local dev box.
    
    Bug: T337970
    Hosts: basic
    Change-Id: I0c63948e84e87aa9a6e099c2b9f8921588bbd436

---
+++ modules/base/files/environment/puppet_env_ps1.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# SPDX-License-Identifier: Apache-2.0
+# Most shells support local vars
+# shellcheck disable=SC3043
+GREEN=$(tput setaf 2)
+MAGENTA=$(tput setaf 5)
+RESET_ATTR=$(tput sgr0)
+BOLD=$(tput bold)
+
+# Prints out a string suitable for a PS1 with the current puppet environment as
+# found in /etc/puppet/puppet.conf, production is shown in magenta, other
+# environments are in green.
+puppet_env_ps1() {
+	# preserve exit status
+	local exit=$?
+	if ! [ -r /etc/puppet/puppet.conf ]; then
+		return $exit
+	fi
+	while IFS='= ' read -r key val; do
+		if [ "$key" = 'environment' ]; then
+			printf '(env: '
+			if [ "$val" = 'production' ]; then
+				printf '%s%s' "$MAGENTA" "$BOLD"
+			else
+				printf '%s' "$GREEN"
+			fi
+			printf '%s%s)\n' "$val" "$RESET_ATTR"
+		fi
+	done </etc/puppet/puppet.conf
+	return $exit
+}
