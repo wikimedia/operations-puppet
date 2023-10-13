e6cb3d20e854bad04d7aa43ff62cd107d77b855b
jbond <jbond@wikimedia.org>
2023-10-13 15:59:10 +0200

    P:base: drop spec test for buster
    The test here are quite simple as such we don't gain a hole lot from
    testing all distros.
    
    Change-Id: I1dfb99d287ef9253a1582880b5b0f86b914f2eed

--- modules/profile/spec/classes/base_spec.rb
+++ modules/profile/spec/classes/base_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../../../rake_modules/spec_helper'
 describe 'profile::base' do
-  on_supported_os(WMFConfig.test_on).each do |os, os_facts|
+  on_supported_os(WMFConfig.test_on(11)).each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
       let(:node_params) {{ '_role' => 'sretest' }}
