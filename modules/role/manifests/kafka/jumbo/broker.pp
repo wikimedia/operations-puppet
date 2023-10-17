939131747ffdc3003c038b2c908d8e66a542326e
Brouberol <brouberol@wikimedia.org>
2023-10-17 08:10:44 +0000

    Revert "Remove kafka-jumbo100[1-6] from puppet"
    This reverts commit f05f4f08e79d63d7845fd0e131fdba7c0f246fdc.
    
    Reason for revert: We will assign a "spare" role to
    kafka-jumbo100[1-6] instead of flat out removing them
    
    Change-Id: Iff8de8a66536d851c4e213e62b7da521b17fa93b

--- modules/role/manifests/kafka/jumbo/broker.pp
+++ modules/role/manifests/kafka/jumbo/broker.pp
@@ -8,5 +8,13 @@ class role::kafka::jumbo::broker {
 
     include profile::firewall
     include profile::kafka::broker
+
+    # Temporary workaround to avoid Kafka Mirror failures
+    # See T347481
+    if $::hostname !~ /^kafka-jumbo100[1-6]/ {
+        # Mirror main-eqiad -> jumbo-eqiad
+        include profile::kafka::mirror
+    }
+
     include ::profile::base::production
 }
