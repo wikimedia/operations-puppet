677ce952e4234028b6dfb9db83a91a8511d83193
Keith Herron <kherron@wikimedia.org>
2023-10-18 14:20:10 -0400

    pyrra: add logstash requests slo
    onboard the logstash requests slo. metrics and targets kept consistent
    with the grizzly slo_definitions file
    
    Bug: T302995
    Change-Id: I00b7014e8dd31d8dbdce3aa76933191ab02a2a1e

--- modules/profile/manifests/pyrra/filesystem.pp
+++ modules/profile/manifests/pyrra/filesystem.pp
@@ -7,4 +7,40 @@ class profile::pyrra::filesystem (
 
     class { 'pyrra::filesystem': }
 
+    # filesystem defined slos
+
+    $logstash_requests_slo = [{
+        'apiVersion' => 'pyrra.dev/v1alpha1',
+        'kind' => 'ServiceLevelObjective',
+        'metadata' => {
+            'name' => 'logstash-requests-pilot',
+            'namespace' => 'pyrra-o11y-pilot',
+            'labels' => {
+                'pyrra.dev/team' => 'o11y',
+                'pyrra.dev/service' => 'logging',
+            },
+        },
+        'spec' => {
+            'target' => '99.5',
+            'window' => '12w',
+            'indicator' => {
+                'ratio' => {
+                    'errors' => {
+                        'metric' => 'log_dead_letters_hits',
+                    },
+                    'total' => {
+                        'metric' => 'logstash_node_plugin_events_out_total{plugin_id="output/opensearch/logstash"}',
+                    },
+                    'grouping' => ['site'],
+                },
+            },
+        },
+    }]
+
+
+    pyrra::filesystem::config { 'logstash-requests.yaml':
+      content => to_yaml($logstash_requests_slo),
+    }
+
+
 }
