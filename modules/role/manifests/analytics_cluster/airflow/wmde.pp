3d4c3124c2ca942ee2d7e27adcc8901f01a0573a
Ryan Kemper <rkemper@wikimedia.org>
2023-10-18 17:56:18 +0000

    Revert "airflow-wmde: configure wmde airflow instance"
    This reverts commit 3504ea515078157ac36fae1a00154675f4846bbf.
    
    Reason for revert: deploy server puppet failing
    
    Change-Id: I19f5ec6b0c599da9ee1c8af3dc3ac49b5ebf3e6b

--- modules/role/manifests/analytics_cluster/airflow/wmde.pp
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-class role::analytics_cluster::airflow::wmde {
-    system::role { 'analytics_cluster::airflow::wmde':
-        description => 'Airflow instance for the Wikimedia Deutschland team in the Analytics Cluster',
-    }
-
-    include ::profile::analytics::cluster::airflow
-}
