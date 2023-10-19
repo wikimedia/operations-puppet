30da51fb049d665a587110ae655d47ecfbab4213
Keith Herron <kherron@wikimedia.org>
2023-10-19 15:54:50 -0400

    thanos: update reload endpoint to reflect updated web prefix
    add updated web route prefix to reload command
    
    Bug: T349102
    Change-Id: I4c609556f611d11a1aff25f668f2dd428d080f6a

--- modules/thanos/manifests/init.pp
+++ modules/thanos/manifests/init.pp
@@ -4,7 +4,7 @@ class thanos {
         # Workaround for SIGHUP not working in thanos 0.21.1
         # https://github.com/thanos-io/thanos/issues/4432
         # https://phabricator.wikimedia.org/T303154
-        command     => '/usr/bin/curl -X POST localhost:17902/-/reload',
+        command     => '/usr/bin/curl -X POST localhost:17902/rule/-/reload',
         refreshonly => true,
     }
 }
