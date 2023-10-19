272e3087ec5fa2591de6cdb928cd393fc7d3fcde
Ryan Kemper <rkemper@wikimedia.org>
2023-10-19 12:07:46 -0700

    elastic: make request latency alerts more granular
    The generic p95 alert has not been very helpful;
    it can fire for a number of different reasons
    and does not always map directly to user impact.
    
    Replace generic p95 with a more targeted focus
    on the 3 query types most relevant to users:
    full_text, comp_suggest, and more_like
    
    Hosts: graphite1005.eqiad.wmnet
    Bug: T349340
    Change-Id: Id7b35ed9f5946e2cb609afabd5a941563e2ebf69

--- modules/role/manifests/elasticsearch/alerts.pp
+++ modules/role/manifests/elasticsearch/alerts.pp
@@ -1,8 +1,10 @@
 class role::elasticsearch::alerts {
-    monitoring::graphite_threshold { 'cirrussearch_eqiad_95th_percentile':
-        description     => 'CirrusSearch eqiad 95th percentile latency',
-        dashboard_links => ['https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?orgId=1&var-cirrus_group=eqiad&var-cluster=elasticsearch&var-exported_cluster=production-search&var-smoothing=1'],
-        metric          => 'fallbackSeries(useSeriesAbove(transformNull(MediaWiki.CirrusSearch.eqiad.requestTimeMs.comp_suggest.sample_rate, 0), 10, "requestTimeMs.comp_suggest.sample_rate", "requestTime.p95"), constantLine(0))',
+    # For the following 6 alerts, they all follow the pattern of "only alert if qps volume is not negligible"
+    # See https://phabricator.wikimedia.org/T347341#9197646 to understand specifcially how fallbackSeries/useSeriesAbove/constantLine achieve the above
+    monitoring::graphite_threshold { 'cirrussearch_eqiad_fulltext_95th_percentile':
+        description     => 'CirrusSearch full_text eqiad 95th percentile latency',
+        dashboard_links => ['https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?orgId=1&var-cirrus_group=eqiad&var-cluster=elasticsearch&var-exported_cluster=production-search&var-smoothing=1&viewPanel=38'],
+        metric          => 'fallbackSeries(useSeriesAbove(transformNull(MediaWiki.CirrusSearch.eqiad.requestTimeMs.comp_suggest.sample_rate, 0), 10, "requestTimeMs.comp_suggest.sample_rate", "requestTimeMs.full_text.p95"), constantLine(0))',
         from            => '10min',
         warning         => 500,
         critical        => 1000,
@@ -11,13 +13,61 @@ class role::elasticsearch::alerts {
         notes_link      => 'https://wikitech.wikimedia.org/wiki/Search#Health/Activity_Monitoring',
     }
 
-    monitoring::graphite_threshold { 'cirrussearch_codfw_95th_percentile':
-        description     => 'CirrusSearch codfw 95th percentile latency',
-        dashboard_links => ['https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?orgId=1&var-cirrus_group=codfw&var-cluster=elasticsearch&var-exported_cluster=production-search&var-smoothing=1'],
-        metric          => 'fallbackSeries(useSeriesAbove(transformNull(MediaWiki.CirrusSearch.codfw.requestTimeMs.comp_suggest.sample_rate, 0), 10, "requestTimeMs.comp_suggest.sample_rate", "requestTime.p95"), constantLine(0))',
+    monitoring::graphite_threshold { 'cirrussearch_codfw_fulltext_95th_percentile':
+        description     => 'CirrusSearch full_text codfw 95th percentile latency',
+        dashboard_links => ['https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?orgId=1&var-cirrus_group=codfw&var-cluster=elasticsearch&var-exported_cluster=production-search&var-smoothing=1&viewPanel=38'],
+        metric          => 'fallbackSeries(useSeriesAbove(transformNull(MediaWiki.CirrusSearch.codfw.requestTimeMs.comp_suggest.sample_rate, 0), 10, "requestTimeMs.comp_suggest.sample_rate", "requestTimeMs.full_text.p95"), constantLine(0))',
         from            => '10min',
         warning         => 500,
         critical        => 1000,
+        percentage      => 20,
+        contact_group   => 'team-discovery',
+        notes_link      => 'https://wikitech.wikimedia.org/wiki/Search#Health/Activity_Monitoring',
+    }
+
+    monitoring::graphite_threshold { 'cirrussearch_eqiad_compsuggest_95th_percentile':
+        description     => 'CirrusSearch comp_suggest eqiad 95th percentile latency',
+        dashboard_links => ['https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?orgId=1&var-cirrus_group=eqiad&var-cluster=elasticsearch&var-exported_cluster=production-search&var-smoothing=1&viewPanel=50'],
+        metric          => 'fallbackSeries(useSeriesAbove(transformNull(MediaWiki.CirrusSearch.eqiad.requestTimeMs.comp_suggest.sample_rate, 0), 10, "requestTimeMs.comp_suggest.sample_rate", "requestTimeMs.comp_suggest.p95"), constantLine(0))',
+        from            => '10min',
+        warning         => 100,
+        critical        => 250,
+        percentage      => 20,
+        contact_group   => 'team-discovery',
+        notes_link      => 'https://wikitech.wikimedia.org/wiki/Search#Health/Activity_Monitoring',
+    }
+
+    monitoring::graphite_threshold { 'cirrussearch_codfw_compsuggest_95th_percentile':
+        description     => 'CirrusSearch comp_suggest codfw 95th percentile latency',
+        dashboard_links => ['https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?orgId=1&var-cirrus_group=codfw&var-cluster=elasticsearch&var-exported_cluster=production-search&var-smoothing=1&viewPanel=50'],
+        metric          => 'fallbackSeries(useSeriesAbove(transformNull(MediaWiki.CirrusSearch.codfw.requestTimeMs.comp_suggest.sample_rate, 0), 10, "requestTimeMs.comp_suggest.sample_rate", "requestTimeMs.comp_suggest.p95"), constantLine(0))',
+        from            => '10min',
+        warning         => 100,
+        critical        => 250,
+        percentage      => 20,
+        contact_group   => 'team-discovery',
+        notes_link      => 'https://wikitech.wikimedia.org/wiki/Search#Health/Activity_Monitoring',
+    }
+
+    monitoring::graphite_threshold { 'cirrussearch_eqiad_morelike_95th_percentile':
+        description     => 'CirrusSearch more_like eqiad 95th percentile latency',
+        dashboard_links => ['https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?orgId=1&var-cirrus_group=eqiad&var-cluster=elasticsearch&var-exported_cluster=production-search&var-smoothing=1&viewPanel=39'],
+        metric          => 'fallbackSeries(useSeriesAbove(transformNull(MediaWiki.CirrusSearch.eqiad.requestTimeMs.comp_suggest.sample_rate, 0), 10, "requestTimeMs.comp_suggest.sample_rate", "requestTimeMs.more_like.p95"), constantLine(0))',
+        from            => '10min',
+        warning         => 1000,
+        critical        => 1500,
+        percentage      => 20,
+        contact_group   => 'team-discovery',
+        notes_link      => 'https://wikitech.wikimedia.org/wiki/Search#Health/Activity_Monitoring',
+    }
+
+    monitoring::graphite_threshold { 'cirrussearch_codfw_morelike_95th_percentile':
+        description     => 'CirrusSearch more_like codfw 95th percentile latency',
+        dashboard_links => ['https://grafana.wikimedia.org/d/000000455/elasticsearch-percentiles?orgId=1&var-cirrus_group=codfw&var-cluster=elasticsearch&var-exported_cluster=production-search&var-smoothing=1&viewPanel=39'],
+        metric          => 'fallbackSeries(useSeriesAbove(transformNull(MediaWiki.CirrusSearch.codfw.requestTimeMs.comp_suggest.sample_rate, 0), 10, "requestTimeMs.comp_suggest.sample_rate", "requestTimeMs.more_like.p95"), constantLine(0))',
+        from            => '10min',
+        warning         => 1000,
+        critical        => 1500,
         percentage      => 20,
         contact_group   => 'team-discovery',
         notes_link      => 'https://wikitech.wikimedia.org/wiki/Search#Health/Activity_Monitoring',
