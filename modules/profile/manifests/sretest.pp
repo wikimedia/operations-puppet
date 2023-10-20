6c6be6b16a99f4e63048f792bb08068674a56386
jbond <jbond@wikimedia.org>
2023-10-20 16:40:54 +0200

    sretest: remove old test
    Hosts: auto
    Change-Id: I6764edd601e635f5a57febbda33d46b8f8a09b82

--- modules/profile/manifests/sretest.pp
+++ modules/profile/manifests/sretest.pp
@@ -1,32 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 # @summary profile for sretest hosts
 class profile::sretest {
-    $cache_nodes = lookup('cache::nodes')  # lint:ignore:wmf_styleguide
     file { '/var/tmp/testing':
         ensure  => directory,
         recurse => true,
         purge   => true,
-    }
-    wmflib::resource::export('file', '/var/tmp/testing/wmflib_export_test.txt', 'sretest', {
-        'ensure' => 'file',
-        content  => 'foo',
-        tag      => 'foo::bar',
-    })
-    wmflib::resource::import('file', undef, { tag => 'foo::bar' })
-    wmflib::resource::export('file', '/var/tmp/testing/wmflib_export_merge_cotent_test.txt', 'sretest', {
-        'ensure'  => 'file',
-        'content' => "${facts['networking']['fqdn']}\n",
-        'tag'     => 'foo::bar::merge',
-    })
-    wmflib::resource::import('file', undef, { tag => 'foo::bar::merge' }, true)
-
-    file {'/tmp/httpasswd_test.txt':
-        ensure  => file,
-        content => htpasswd('foobar', 'salty123'),
-    }
-    # Test binary file content
-    file { '/tmp/delete_me_jbond':
-        content => secret('cassandra/restbase/truststore'),
-        mode    => '0400',
     }
 }
