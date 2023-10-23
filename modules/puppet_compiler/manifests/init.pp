3c77ccd4949f397679d2a0768151696c07b3102b
jbond <jbond@wikimedia.org>
2023-10-23 13:30:14 +0200

    puppet_compiler: add typing_extensions
    Hosts: auto
    Change-Id: I3d4eba15e5fbc4c32ca0afb2681a3d057153ecbe

--- modules/puppet_compiler/manifests/init.pp
+++ modules/puppet_compiler/manifests/init.pp
@@ -23,6 +23,17 @@ class puppet_compiler (
     $yaml_mount = "${mount}/yaml"
     $output_mount = "${mount}/output"
 
+    # TODO: We currently get TypedDict from typing_extensions however once everything
+    # has moved to bookworm we can use typing.TypedDict which was added in py3.8
+    case debian::codename() {
+        'bullseye': {
+            apt::package_from_bpo { 'python3-typing-extensions':
+                distro => 'bullseye',
+            }
+        }
+        'bookworm': { ensure_packages(['python3-typing-extensions']) }
+        default: { fail("${debian::codename()} is not supported") }
+    }
     ensure_packages([
         'python3-yaml', 'python3-requests', 'python3-jinja2', 'python3-clustershell',
         'nginx', 'ruby-httpclient', 'ruby-ldap', 'ruby-rgen', 'ruby-multi-json',
