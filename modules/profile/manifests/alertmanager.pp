6f324dae5959f6b2d525abf1a2b764eef4e7bad6
Taavi Väänänen <hi@taavi.wtf>
2023-10-24 17:30:37 +0300

    P:alertmanager: drop cloudmetrics hosts
    Bug: T336854
    Change-Id: Ib5a0797f92422a8bd500635aae0a7c2793a50f0a

--- modules/profile/manifests/alertmanager.pp
+++ modules/profile/manifests/alertmanager.pp
@@ -4,7 +4,6 @@ class profile::alertmanager (
     Array[Stdlib::Host] $partners    = lookup('profile::alertmanager::partners'),
     Array[Stdlib::Host] $grafana_hosts = lookup('profile::alertmanager::grafana_hosts', { 'default_value' => [] }),
     Array[Stdlib::Host] $thanos_query_hosts = lookup('profile::alertmanager::thanos_query_hosts', { 'default_value' => [] }),
-    Array[Stdlib::Host] $cloudmetrics_hosts = lookup('profile::alertmanager::cloudmetrics_hosts', {'default_value' => []}),
     String              $irc_channel = lookup('profile::alertmanager::irc::channel'),
     Optional[String]    $victorops_api_key = lookup('profile::alertmanager::victorops_api_key'),
     Array $prometheus_all_nodes = lookup('prometheus_all_nodes'),
@@ -21,7 +20,7 @@ class profile::alertmanager (
     }
 
     # All Prometheus servers need access to Alertmanager to send alerts
-    $prometheus_nodes_ferm = join($prometheus_all_nodes + $grafana_hosts + $thanos_query_hosts + $cloudmetrics_hosts, ' ')
+    $prometheus_nodes_ferm = join($prometheus_all_nodes + $grafana_hosts + $thanos_query_hosts, ' ')
     ferm::service { 'alertmanager-prometheus':
         proto  => 'tcp',
         port   => '9093',
