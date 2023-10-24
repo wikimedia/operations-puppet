fb5386b68239a0e3f8bd482b16378a1ec35c7b0f
Eric Evans <eevans@wikimedia.org>
2023-10-24 14:04:10 -0500

    Decommission restbase1017
    Bug: T349710
    Change-Id: Ie8b3f37450925eb06204b5cb7589284229e4fc8f

--- manifests/site.pp
+++ manifests/site.pp
@@ -2690,7 +2690,7 @@ node /^relforge100[3-4]\.eqiad\./ {
 }
 
 # restbase eqiad cluster
-node /^restbase10(1[7-9]|2[0-9]|3[0-3])\.eqiad\./ {
+node /^restbase10(1[8-9]|2[0-9]|3[0-3])\.eqiad\./ {
     role(restbase::production)
 }
 
@@ -2700,7 +2700,7 @@ node /^restbase20(1[3-9]|2[0-7])\.codfw\./ {
 }
 
 # decommissioned restbase (T349526)
-node /^restbase1016\.eqiad\./ {
+node /^restbase101[6-7]\.eqiad\./ {
     role(spare::system)
 }
 
