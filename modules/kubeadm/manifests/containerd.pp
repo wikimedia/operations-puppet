d4e718bc0a428ac67f525f7e7b8842ff6aea450f
Taavi Väänänen <hi@taavi.wtf>
2023-10-25 14:23:15 +0300

    kubeadm: containerd: add kernel modules and config
    These are required, and presumably were previously automatically
    handled by Docker.
    
    Bug: T284656
    Change-Id: Ibb8fc87697037ad636418dfd1c603d1f3d5ce580

--- modules/kubeadm/manifests/containerd.pp
+++ modules/kubeadm/manifests/containerd.pp
@@ -1,6 +1,16 @@
 # SPDX-License-Identifier: Apache-2.0
 # @summary install and configure containerd for toolforge k8s usage
 class kubeadm::containerd () {
+  kmod::module { 'br_netfilter':
+    ensure => 'present',
+  }
+
+  sysctl::parameters { 'kubernetes-ip-forward':
+    values => {
+      'net.ipv4.ip_forward' => 1,
+    },
+  }
+
   package { 'containerd':
     ensure => installed,
   }
