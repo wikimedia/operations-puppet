c637f6964b060a21710c068ace4c3fb33fa62440
jbond <jbond@wikimedia.org>
2023-10-26 12:45:07 +0200

    profile: update script so its compatible with POSIX sh
    also environment: fix SC3033
    
    Change-Id: Ibf39201980fd69a3e0f450f7adebba4bf66c677c

--- modules/base/files/environment/proxy.sh
+++ modules/base/files/environment/proxy.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 # SPDX-License-Identifier: Apache-2.0
 #####################################################################
 ### THIS FILE IS MANAGED BY PUPPET
 ### puppet:///modules/base/environment/proxy.sh
 #####################################################################
-function set-proxy {
+set_proxy() {
         export HTTP_PROXY=http://webproxy:8080
         export HTTPS_PROXY=http://webproxy:8080
         export http_proxy=http://webproxy:8080
         export https_proxy=http://webproxy:8080
         echo "Proxy set"
 }
-function unset-proxy {
+unset_proxy() {
         unset HTTP_PROXY
         unset HTTPS_PROXY
         unset http_proxy
