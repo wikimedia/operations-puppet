331443ba2b0321b4ee5330bcffcb3247a7651c93
Taavi Väänänen <hi@taavi.wtf>
2023-10-27 12:58:40 +0300

    P:openstack: fix openstack_exporter host hiera key name
    Change-Id: Ic25190648f1655b215dae3bb5a7d9626d1b1d068

--- modules/profile/manifests/openstack/eqiad1/metrics.pp
+++ modules/profile/manifests/openstack/eqiad1/metrics.pp
@@ -1,5 +1,5 @@
 class profile::openstack::eqiad1::metrics (
-    Stdlib::Fqdn $active_host = lookup('profile::wmcs::eqiad1::metrics::openstack_exporter_host'),
+    Stdlib::Fqdn $active_host = lookup('profile::openstack::eqiad1::metrics::openstack_exporter_host'),
 ) {
     class { '::profile::prometheus::openstack_exporter':
         ensure      => ($active_host == $::facts['networking']['fqdn']).bool2str('present', 'absent'),
