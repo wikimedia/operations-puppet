07bd2eeb0378af08937823ae53fcd056e1d68786
jbond <jbond@wikimedia.org>
2023-11-06 14:09:36 +0100

    prometheus-puppet-agent-stats: this timer sometime fails
    This timer sometimes fails due to missing status files due to it running
    at the same time as puppet.  To avoid this update the job so that it
    runs after the puppet-agent-timer service.
    
    Hosts: basic
    Change-Id: Ic7384d07a2e0de3be4669e609de286e4d40c30b2

--- modules/prometheus/manifests/node_puppet_agent.pp
+++ modules/prometheus/manifests/node_puppet_agent.pp
@@ -23,11 +23,30 @@ class prometheus::node_puppet_agent (
 
     # Collect every minute
     systemd::timer::job { 'prometheus_puppet_agent_stats':
-        ensure      => $ensure,
+        ensure      => absent,
         description => 'Regular job to collect puppet agent stats',
         user        => 'root',
-        command     => "/usr/local/bin/prometheus-puppet-agent-stats --outfile ${outfile}",
         interval    => {'start' => 'OnCalendar', 'interval' => 'minutely'},
+        command     => "/usr/local/bin/prometheus-puppet-agent-stats --outfile ${outfile}",
+        after       => 'puppet-agent-timer.service',
         require     => File[$outfile.dirname]
     }
+
+    # Use systemd::unit in place of systemd::service since we need to
+    # drop the unit only and *not* make sure it is running.
+    # Doing so would get puppet stuck since this unit requires a puppet
+    # run to be complete (After=puppet-agent-timer.service)
+    systemd::unit { 'prometheus-puppet-agent-stats':
+        ensure  => $ensure,
+        content => init_template('prometheus-puppet-agent-stats', 'systemd'),
+        require => File[$outfile.dirname],
+    }
+
+    # Need to enable the service since we're using WantedBy=puppet-agent-timer.service
+    exec { 'enable prometheus-puppet-agent-stats':
+        command => '/bin/systemctl enable prometheus-puppet-agent-stats',
+        unless  => '/bin/systemctl -q is-enabled prometheus-puppet-agent-stats',
+        require => Systemd::Unit['prometheus-puppet-agent-stats'],
+    }
+
 }
