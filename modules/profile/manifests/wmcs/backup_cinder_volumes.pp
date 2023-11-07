1904c2193d3d0dd5e03715fb9b7c7e5db2a7709c
Andrew Bogott <abogott@wikimedia.org>
2023-11-07 16:27:24 -0600

    wmcs-backup: second attempt to add cleanup timers
    Chaining a single commandline with && doesn't work, instead
    use exec_start_pre and a separate job for dangling snaps.
    
    Change-Id: I784bc259096e51cb83e104104e92f3d83d1c008c

--- modules/profile/manifests/wmcs/backup_cinder_volumes.pp
+++ modules/profile/manifests/wmcs/backup_cinder_volumes.pp
@@ -33,7 +33,23 @@ class profile::wmcs::backup_cinder_volumes(
     systemd::timer::job { 'backup_cinder_volumes':
         ensure                    => $timers_ensure,
         description               => 'backup cinder volumes',
-        command                   => '/usr/local/sbin/wmcs-backup volumes backup-assigned-volumes && /usr/local/sbin/wmcs-backup volumes remove-unhandled-backups && /usr/local/sbin/wmcs-backup volumes remove-dangling-snapshots',
+        exec_start_pre            => '/usr/local/sbin/wmcs-backup volumes remove-unhandled-backups',
+        command                   => '/usr/local/sbin/wmcs-backup volumes backup-assigned-volumes',
+        interval                  => {
+          'start'    => 'OnCalendar',
+          'interval' => $backup_interval,
+        },
+        logging_enabled           => true,
+        monitoring_enabled        => true,
+        monitoring_contact_groups => 'wmcs-team-email',
+        user                      => 'root',
+        require                   => File['/usr/local/sbin/wmcs-backup'],
+    }
+
+    systemd::timer::job { 'remove_dangling_cinder_snapshots':
+        ensure                    => $timers_ensure,
+        description               => 'backup cinder volumes',
+        command                   => '/usr/local/sbin/wmcs-backup volumes remove-dangling-snapshots',
         interval                  => {
           'start'    => 'OnCalendar',
           'interval' => $backup_interval,
