292e361bd0700d7c585e434ee360336afba95e4b
Daniel Zahn <dzahn@wikimedia.org>
2023-11-08 11:36:32 -0800

    stewards: add git::clone of stewards/onboarding-system from gitlab
    Bug: T344164
    Change-Id: I1a327e00b5a6a5b8749b97972c3645bf4a0a6d2d

--- modules/profile/manifests/stewards.pp
+++ modules/profile/manifests/stewards.pp
@@ -2,5 +2,15 @@
 # special VM for stewards (T344164)
 class profile::stewards (
 ){
+    # T344164#9314186
     ensure_packages(['python3-click', 'python3-requests-oauthlib'])
+
+    $repo_dir = '/srv/repos'
+    wmflib::dir::mkdir_p($repo_dir)
+
+    git::clone { 'repos/stewards/onboarding-system':
+        ensure    => 'present',
+        source    => 'gitlab',
+        directory => "${repo_dir}/onboarding-system",
+    }
 }
