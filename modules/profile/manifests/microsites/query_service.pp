1c43f11227b6db32ee36ae52f231f6002db96457
Brian King <bking@wikimedia.org>
2023-11-14 17:10:10 -0600

    query_service: add monitoring for ldf endpoint
    Add prometheus blackbox check for
    wikidata LDF (linked data fragments) endpoint.
    
    Bug: T347355
    Hosts: alert1001.wikimedia.org
    Change-Id: I86b3d040a90f2734eb6c2e144881348c4919f3dc

--- modules/profile/manifests/microsites/query_service.pp
+++ modules/profile/manifests/microsites/query_service.pp
@@ -43,4 +43,18 @@ class profile::microsites::query_service {
         force_tls   => true,
         ip_families => [ip4],
     }
+
+    prometheus::blackbox::check::http { 'query.wikidata.org':
+        team               => 'search-platform',
+        severity           => 'task',
+        path               => '/bigdata/ldf',
+        body               => {
+            'subject'   => 'wd:42',
+            'predicate' => 'wdt:P31',
+            'object'    => '',
+        },
+        body_regex_matches => 'wd:Q42  wdt:P31  wd:Q5 .',
+        force_tls          => true,
+        ip_families        => [ip4],
+    }
 }
