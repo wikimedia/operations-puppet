cbbd8be52ea24405a3b066484c2a232b51f750a1
Matthew Vernon <mvernon@wikimedia.org>
2023-11-14 16:15:43 +0000

    swift: migrate one node to envoy for TLS termination
    Replacing nginx with envoy for TLS termination; the aim is to deploy
    this on a canary node, which will require a reimage (since there is
    no straightforward way to remove the nginx resources).
    
    So deployment plan is: depool ms-fe2014, reimage, test ms-fe2014 and
    repool.
    
    [is there a better way of handling profile::envoy::ensure
    e.g. conditionally set it in proxy_tls.pp? I think it's not allowed to
    set profile hiera there]
    
    Bug: T317616
    Hosts: ms-fe2012.codfw.wmnet,ms-fe1012.eqiad.wmnet,ms-fe2014.codfw.wmnet
    Change-Id: I785983cb675d2d26e91306e9fb2eda04939cdba6

--- modules/profile/manifests/swift/proxy_tls.pp
+++ modules/profile/manifests/swift/proxy_tls.pp
@@ -1,17 +1,22 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::swift::proxy_tls (
     String $ocsp_proxy = lookup('http_proxy', {'default_value' => ''}),
+    Boolean $use_envoy = lookup('profile::swift::proxy::use_envoy', {'default_value' => false}),
 ){
 
-    require ::profile::tlsproxy::instance
+    if $use_envoy {
+        include profile::tlsproxy::envoy
+    } else {
+        require ::profile::tlsproxy::instance
 
-    tlsproxy::localssl { 'unified':
-        server_name    => $::swift::proxy::proxy_service_host,
-        certs          => [$::swift::proxy::proxy_service_host],
-        default_server => true,
-        do_ocsp        => false,
-        ocsp_proxy     => $ocsp_proxy,
-        enable_http2   => false,
+        tlsproxy::localssl { 'unified':
+            server_name    => $::swift::proxy::proxy_service_host,
+            certs          => [$::swift::proxy::proxy_service_host],
+            default_server => true,
+            do_ocsp        => false,
+            ocsp_proxy     => $ocsp_proxy,
+            enable_http2   => false,
+        }
     }
 
     ferm::service { 'swift-proxy-https':
