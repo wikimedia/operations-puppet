e656183b633ceb84ca4be3959d36c76b225e3e5b
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-11-16 08:15:41 +0100

    mediawiki: Add missing frequency param to the purge_temporary_accounts job
    Bug: T344695
    Change-Id: If8d1107d374437082ab3ef1684ee705c8388d2fd

--- modules/profile/manifests/mediawiki/maintenance/temporary_accounts.pp
+++ modules/profile/manifests/mediawiki/maintenance/temporary_accounts.pp
@@ -3,7 +3,7 @@
 class profile::mediawiki::maintenance::temporary_accounts {
 
     profile::mediawiki::periodic_job { 'purge_temporary_accounts':
-        command  => '/usr/local/bin/foreachwikiindblist "all - closed - private - fishbowl - nonglobal" extensions/CentralAuth/maintenance/expireTemporaryAccounts.php --verbose',
+        command  => '/usr/local/bin/foreachwikiindblist "all - closed - private - fishbowl - nonglobal" extensions/CentralAuth/maintenance/expireTemporaryAccounts.php --verbose --frequency 1',
         interval => '*-*-* 14:27:00'
     }
 }
