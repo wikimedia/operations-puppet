097613a9b6487e602e89816e1ec1747db1f2164a
Valentin Gutierrez <vgutierrez@wikimedia.org>
2023-11-17 17:12:28 +0100

    interface: Add a clsact helper
    Provide support to add the clsact qdisc on any existent interface.
    As a bonus provide ensure = absent support for
    interface::post_up_command
    
    Bug: T351069
    Change-Id: Id374d8d7ac5ffd048076ff62d02e3f72b3546869

---
+++ modules/interface/manifests/clsact.pp
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: Apache-2.0
+# Add clsact qdisc to any existent interface
+define interface::clsact(
+    String $interface,
+    Wmflib::Ensure $ensure = 'present',
+) {
+    $tc_add_cmd = "/usr/sbin/tc qdisc add dev ${interface} clsact"
+    $tc_del_cmd = "/usr/sbin/tc qdisc del dev ${interface} clsact"
+
+    interface::post_up_command { "clsact_${interface}":
+        ensure    => $ensure,
+        interface => $interface,
+        command   => $tc_add_cmd,
+    }
+
+    if ensure == 'absent' {
+        exec { $tc_del_cmd:
+            onlyif => "/usr/sbin/tc show dev ${interface} | grep -q clsact",
+        }
+    } else {
+        # Add clsact manually as well
+        exec { $tc_add_cmd:
+            unless => "/usr/sbin/tc show dev ${interface} | grep -q clsact",
+        }
+    }
+}
--- modules/interface/manifests/post_up_command.pp
+++ modules/interface/manifests/post_up_command.pp
@@ -1,9 +1,21 @@
 # SPDX-License-Identifier: Apache-2.0
-define interface::post_up_command($interface, $command) {
-    # Use augeas to add an 'post-up' command to the interface
-    augeas { "${interface}_${title}":
-        context => "/files/etc/network/interfaces/*[. = '${interface}']",
-        changes => "set post-up[last()+1] '${command}'",
-        onlyif  => "match post-up[. = '${command}'] size == 0";
+define interface::post_up_command(
+    $interface,
+    $command,
+    Wmflib::Ensure $ensure = 'present',
+) {
+    if $ensure == 'absent' {
+        file_line { "rm_${interface}_${title}":
+            ensure            => absent,
+            match             => "post-up ${command}",
+            match_for_absence => true,
+        }
+    } else {
+        # Use augeas to add an 'post-up' command to the interface
+        augeas { "${interface}_${title}":
+            context => "/files/etc/network/interfaces/*[. = '${interface}']",
+            changes => "set post-up[last()+1] '${command}'",
+            onlyif  => "match post-up[. = '${command}'] size == 0";
+        }
     }
 }
