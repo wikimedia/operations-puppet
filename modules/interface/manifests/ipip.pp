ac7f764ef579bf9d65663f03a7d47f5c9dd3950e
Valentin Gutierrez <vgutierrez@wikimedia.org>
2023-11-17 12:48:17 +0100

    interface: Allow creating IPIP interfaces w/o an endpoint
    IPIP and IP6IP6 interfaces leveraged by IPVS/Katran to deliver user
    traffic to realservers don't need an endpoint so we cannot leverage
    /etc/network/interfaces "tunnel" method that sets the endpoint as
    a mandatory parameter.
    
    Bug: T351069
    Change-Id: I013d9992bbbd179c038723b2e7c77fa4981782df

---
+++ modules/interface/manifests/ipip.pp
@@ -0,0 +1,92 @@
+# SPDX-License-Identifier: Apache-2.0
+# IPIP device creation for load balancers and realservers
+# we don't set an endpoint so we cannot leverage the "tunnel" method from /etc/network/interfaces
+define interface::ipip(
+  String $interface,
+  Enum['inet', 'inet6'] $family='inet',
+  Optional[Stdlib::IP::Address::V4] $address=undef,
+  Wmflib::Ensure $ensure = 'present',
+) {
+    if $family == 'inet' and !defined('$address') {
+        fail('inet family requires an address')
+    }
+
+    interface::manual { $title:
+        ensure    => $ensure,
+        interface => $interface,
+        family    => $family,
+    }
+
+    # provide an ip link cmd
+    $tunnel_type = $family ? {
+      'inet'  => 'ipip',
+      'inet6' => 'ip6tnl',
+    }
+    $ip_link_add = "ip link add name ${interface} type ${tunnel_type} external"
+    $ip_link_up = "ip link set up dev ${interface}"
+
+    if $ensure == 'absent' { # Remove the interface
+        if $family == 'inet' {
+            interface::ip{ "${title} ipv4":
+                ensure    => absent,
+                interface => $interface,
+                address   => $address,
+                prefixlen => 32,
+            }
+        }
+
+        $ip_link_del = "ip link del dev ${interface}"
+
+        file_line { "rm_${interface}_set_up":
+            ensure            => absent,
+            match             => $ip_link_up,
+            match_for_absence => true,
+        }
+        file_line { "rm_${interface}_add_up":
+            ensure            => absent,
+            match             => $ip_link_add,
+            match_for_absence => true,
+        }
+
+        exec { $ip_link_del:
+            path    => '/bin:/usr/bin',
+            returns => [0, 2],
+            onlyif  => "ip link show ${interface}",
+        }
+    } else { # Add the interface
+
+        augeas { "${interface}_add_up":
+            context => "/files/etc/network/interfaces/*[. = '${interface}' and ./family = '${family}']",
+            changes => "set up[last()+1] '${ip_link_add}'",
+            onlyif  => "match up[. = '${ip_link_add}'] size == 0",
+        }
+
+        augeas { "${interface}_set_up":
+            context => "/files/etc/network/interfaces/*[. = '${interface}' and ./family = '${family}']",
+            changes => "set up[last()+1] '${ip_link_up}'",
+            onlyif  => "match up[. = '${ip_link_up}'] size == 0",
+        }
+
+        # Create the device manually as well
+        exec { $ip_link_add:
+            path    => '/bin:/usr/bin',
+            returns => [0, 2],
+            unless  => "ip link show ${interface}",
+        }
+
+        exec { $ip_link_up:
+            path    => '/bin:/usr/bin',
+            returns => [0, 2],
+            unless  => "ip link show ${interface} | grep -q UP",
+        }
+
+        # Assign the provided address for IPv4 interfaces
+        if $family == 'inet' {
+            interface::ip{ "${title} ipv4":
+                interface => $interface,
+                address   => $address,
+                prefixlen => 32,
+            }
+        }
+    }
+}
--- modules/interface/manifests/manual.pp
+++ modules/interface/manifests/manual.pp
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
-define interface::manual($interface, $family='inet') {
+define interface::manual(
+  $interface,
+  $family='inet',
+  Wmflib::Ensure $ensure='present'
+) {
     # Use augeas to create a new manually setup interface with allow-hotplug
     $augeas_cmd = [ "set allow-hotplug[./1 = '${interface}']/1 '${interface}'",
             "set iface[. = '${interface}'] '${interface}'",
@@ -7,8 +11,21 @@ define interface::manual($interface, $family='inet') {
             "set iface[. = '${interface}']/method 'manual'",
     ]
 
-    augeas { "${interface}_manual":
-        context => '/files/etc/network/interfaces',
-        changes => $augeas_cmd;
+    if ensure == 'absent' {
+        file_line { "rm_${interface}_manual":
+            ensure            => absent,
+            match             => "iface ${interface} ${family} manual",
+            match_for_absence => true,
+        }
+        file_line { "rm_hotplug_${interface}":
+            ensure            => absent,
+            match             => "allow-hotplug ${interface}",
+            match_for_absence => true,
+        }
+    } else {
+        augeas { "${interface}_manual":
+            context => '/files/etc/network/interfaces',
+            changes => $augeas_cmd;
+        }
     }
 }
