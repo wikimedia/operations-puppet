d02a61c403c278143e448e7870aca84f29382cf7
jbond <jbond@wikimedia.org>
2023-11-20 14:57:17 +0100

    mail::default_mail_relay: update to have a smarthosts parameter
    This code currently relies on the mail_smarthosts globals.  update the
    code so that we lookup the data from hiera instead
    
    Hosts: auto
    Bug: T350008
    Change-Id: I9ad8fd53980f11cc109ae63c738ccedd17d4dc86

--- modules/profile/manifests/mail/default_mail_relay.pp
+++ modules/profile/manifests/mail/default_mail_relay.pp
@@ -1,10 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 # @param enabled if the default_mail_relay is enabled
 # @param template the template to use
-# @param mediawiki_smarthosts a list of smarthosts to configure
+# @param smarthosts a list of smarthosts that handle general mail
+# @param mediawiki_smarthosts a list of smarthosts that handle midiawiki mail
 class profile::mail::default_mail_relay (
-    Boolean             $enabled             = lookup('profile::mail::default_mail_relay::enabled'),
-    String              $template            = lookup('profile::mail::default_mail_relay::template'),
+    Boolean             $enabled              = lookup('profile::mail::default_mail_relay::enabled'),
+    String              $template             = lookup('profile::mail::default_mail_relay::template'),
+    Array[Stdlib::Fqdn] $smarthosts           = lookup('profile::mail::default_mail_relay::smarthosts'),
     Array[Stdlib::Fqdn] $mediawiki_smarthosts = lookup('profile::mail::default_mail_relay::mediawiki_smarthosts'),
 ) {
     if $enabled {
