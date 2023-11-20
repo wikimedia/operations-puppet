ce8ccd5144fdc8cea20704764e94970daab7e233
Valentin Gutierrez <vgutierrez@wikimedia.org>
2023-11-20 11:35:02 +0100

    ncredir: Enable IPIP encapsulation on ulsfo
    Bug: T351069
    Depends-On: I013d9992bbbd179c038723b2e7c77fa4981782df
    Depends-On: Id374d8d7ac5ffd048076ff62d02e3f72b3546869
    Depends-On: I3bba8118b1f7382e4a734c21d797a5754428b1e5
    Change-Id: I24cf4fce8ba2f6517dfe343ea2c127cd26195712

--- modules/role/manifests/ncredir.pp
+++ modules/role/manifests/ncredir.pp
@@ -3,6 +3,7 @@ class role::ncredir {
     include profile::base::production
     include profile::firewall
     include profile::lvs::realserver
+    include profile::lvs::realserver::ipip
     include profile::nginx
     include profile::ncredir
 }
