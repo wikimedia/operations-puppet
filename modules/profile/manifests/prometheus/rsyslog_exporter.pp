b185356d102e06593fa5b5e6ef7cbdc4d8cab847
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2023-11-22 13:52:10 +0100

    rsyslog_exporter: move to a define
    Needed to support per-instance exporter
    
    Bug: T351799
    Change-Id: I31542f6eea594ee4aa3ed4b83a9fb9c032573d41

--- modules/profile/manifests/prometheus/rsyslog_exporter.pp
+++ modules/profile/manifests/prometheus/rsyslog_exporter.pp
@@ -1,4 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::prometheus::rsyslog_exporter {
-    class { '::prometheus::rsyslog_exporter': }
+    prometheus::rsyslog_exporter { 'base': }
 }
--- modules/prometheus/manifests/rsyslog_exporter.pp
+++ modules/prometheus/manifests/rsyslog_exporter.pp
@@ -1,4 +1,4 @@
-class prometheus::rsyslog_exporter (
+define prometheus::rsyslog_exporter (
     Wmflib::Ensure       $ensure = present,
     Stdlib::IP::Address  $listen_address = $facts['wmflib']['is_container'] ? {
                                               true  => '0.0.0.0',
@@ -7,14 +7,22 @@ class prometheus::rsyslog_exporter (
     Stdlib::Port         $listen_port    = 9105,
     Stdlib::Absolutepath $base           = '/etc/rsyslog.d',
 ) {
-    package { 'prometheus-rsyslog-exporter':
-        ensure => $ensure,
-    }
+    ensure_packages(['prometheus-rsyslog-exporter'])
 
-    rsyslog::conf { 'exporter':
+    $safe_title = $title.regsubst('[^\w\-]', '_', 'G')
+
+    rsyslog::conf { "exporter-${safe_title}":
         ensure   => $ensure,
         content  => template("${module_name}/rsyslog_exporter.conf.erb"),
         priority => 10,
         base     => $base,
+    }
+
+    # Legacy name
+    if !defined(Rsyslog::Conf['exporter']) {
+        rsyslog::conf { 'exporter':
+            ensure   => absent,
+            priority => 10,
+        }
     }
 }
