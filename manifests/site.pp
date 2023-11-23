fdee293e215a78d21a5d05ced0e79fd1e5ee5911
Arnaud Bran <abran@wikimedia.org>
2023-11-23 14:55:15 +0100

    mariadb: replace db1147 by db1247 on s4
    as per T344036, hardware swap in progress
    
    Bug: T344036
    Change-Id: Ib1f97081f91df77f4b9c7d70743912b722f6f3fb

--- manifests/site.pp
+++ manifests/site.pp
@@ -607,7 +607,7 @@ node /^db2156\.codfw\./ {
 
 # s4 (commons) core production dbs on eqiad
 # See also db1144 and db1146 below
-node /^db1(138|141|142|143|147|148|149|160|190|199|238|241|242|243)\.eqiad\./ {
+node /^db1(138|141|142|143|147|148|149|160|190|199|238|241|242|243|247)\.eqiad\./ {
     role(mariadb::core)
 }
 
@@ -761,7 +761,7 @@ node /^db2194\.codfw\./ {
 }
 
 # New db hosts to be setup T342166
-node /^db12(26|28|29|39|32|33|34|35|37|39|40|44|45|46|47|48|49)\.eqiad\./ {
+node /^db12(26|28|29|39|32|33|34|35|37|39|40|44|45|46|48|49)\.eqiad\./ {
     role(insetup::data_persistence)
 }
 ## m1 shard
