eb7cb33122622ccafaf6904833231da4158b8891
Taavi Väänänen <hi@taavi.wtf>
2023-11-23 12:53:10 +0200

    interface: fix absenting of post_up_command
    Follows up 097613a9b6487e602e89816e1ec1747db1f2164a.
    
    Change-Id: Idf0e3accad587d90af4d77348faf68105addb90f

--- modules/interface/manifests/post_up_command.pp
+++ modules/interface/manifests/post_up_command.pp
@@ -1,12 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
 define interface::post_up_command(
-    $interface,
-    $command,
+    String[1]      $interface,
+    String[1]      $command,
     Wmflib::Ensure $ensure = 'present',
 ) {
     if $ensure == 'absent' {
         file_line { "rm_${interface}_${title}":
             ensure            => absent,
+            path              => '/etc/network/interfaces',
             match             => "post-up ${command}",
             match_for_absence => true,
         }
