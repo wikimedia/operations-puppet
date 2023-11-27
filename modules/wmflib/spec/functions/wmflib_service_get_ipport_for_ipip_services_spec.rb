a2dfab90eeb1e696dc0368c8527141dbcb9a4b61
Valentin Gutierrez <vgutierrez@wikimedia.org>
2023-11-27 19:43:52 +0100

    wmflib: Test get_ipport_for_ipip_services
    Change-Id: I4084199f1925f0a7595d22eeba58f2a259a447c0

---
+++ modules/wmflib/spec/functions/wmflib_service_get_ipport_for_ipip_services_spec.rb
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: Apache-2.0
+require_relative '../../../../rake_modules/spec_helper'
+services = {
+  'service' => {
+    'description' => 'example',
+    'sites' => ['eqiad'],
+    'ip' => { 'eqiad' => { 'default' => '192.0.2.1' }},
+    'port' => 80,
+    'encryption' => false,
+    'state' => 'production',
+    'lvs' => {
+      'enabled' => true,
+      'class' => 'high-traffic1',
+      'conftool' => { 'cluster' => 'foo', 'service' => 'bar' },
+      'depool_threshold' => '.5', 'ipip_encapsulation' => true
+    },
+  },
+  'service-https' => {
+    'description' => 'example',
+    'sites' => ['eqiad'],
+    'ip' => { 'eqiad' => { 'default' => '192.0.2.1' }},
+    'port' => 443,
+    'encryption' => true,
+    'state' => 'production',
+    'lvs' => {
+      'enabled' => true,
+      'class' => 'high-traffic1',
+      'conftool' => { 'cluster' => 'foo', 'service' => 'bar' },
+      'depool_threshold' => '.5', 'ipip_encapsulation' => true
+    },
+  },
+}
+describe 'wmflib::service::get_ipport_for_ipip_services' do
+  it { is_expected.to run.with_params(services, 'eqiad').and_return(["192.0.2.1:443", "192.0.2.1:80"]) }
+end
