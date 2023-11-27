8fdebbbd41af2709a766228644ae76e1c01bda40
Taavi Väänänen <hi@taavi.wtf>
2023-11-27 11:38:02 +0200

    P:openstack::magnum: use cloud-private for memcached
    Change-Id: I1fbdf2523b5e59366eff106ec23ff88be6096d2d

--- modules/profile/manifests/openstack/base/magnum.pp
+++ modules/profile/manifests/openstack/base/magnum.pp
@@ -4,7 +4,6 @@ class profile::openstack::base::magnum(
     String $version = lookup('profile::openstack::base::version'),
     Boolean $active = lookup('profile::openstack::base::magnum::active'),
     Array[OpenStack::ControlNode] $openstack_control_nodes = lookup('profile::openstack::base::openstack_control_nodes'),
-    String $openstack_control_node_interface = lookup('profile::openstack::base::magnum::openstack_control_node_interface', {default_value => 'cloud_private_fqdn'}),
     Array[Stdlib::Fqdn] $rabbitmq_nodes = lookup('profile::openstack::base::rabbitmq_nodes'),
     Stdlib::Fqdn $keystone_fqdn = lookup('profile::openstack::base::keystone_api_fqdn'),
     String $region = lookup('profile::openstack::base::region'),
@@ -24,7 +23,7 @@ class profile::openstack::base::magnum(
 ) {
     class { '::openstack::magnum::service':
         version                     => $version,
-        memcached_nodes             => $openstack_control_nodes.map |$node| { $node[$openstack_control_node_interface] },
+        memcached_nodes             => $openstack_control_nodes.map |$node| { $node['cloud_private_fqdn'] },
         rabbitmq_nodes              => $rabbitmq_nodes,
         keystone_fqdn               => $keystone_fqdn,
         db_user                     => $db_user,
