1409d97258c120b18930c125c067b834f7af0d47
Brian King <bking@wikimedia.org>
2023-11-29 10:48:59 -0600

    flink-zk: Activate codfw hosts
    Enable codfw flink-zk hosts, so we can
    finish migrating rdf-streaming-updater
    in codfw.
    
    Bug: T349095
    Change-Id: Ic52e2d22c5d0ed5ab097c8383a829d5665e7843c

--- manifests/site.pp
+++ manifests/site.pp
@@ -1722,7 +1722,7 @@ node /^flink-zk100[123]\.eqiad\./ {
 }
 
 node /^flink-zk200[123]\.codfw\./ {
-    role(insetup::search_platform)
+    role(zookeeper::flink)
 }
 
 # the cinder-backup nodes for codfw1dev run in the eqiad DC and
