15f1312cfd52579499d72d0b39ea016a9f6791e9
Taavi Väänänen <hi@taavi.wtf>
2023-11-30 14:04:26 +0200

    openstack: spreadcheck: remove in favour of server groups
    These are replaced by openstack server groups which solve the same
    problem much better.
    
    Bug: T247213
    Bug: T345294
    Change-Id: Ied96f63d52c540d15c323d5cfc3b2cd3dd450dad

--- modules/openstack/manifests/monitor/spreadcheck.pp
+++ modules/openstack/manifests/monitor/spreadcheck.pp
@@ -5,29 +5,12 @@ class openstack::monitor::spreadcheck {
     # Script that checks how 'spread out' critical instances for a project
     # are. See T101635
     file { '/usr/local/sbin/wmcs-spreadcheck':
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-        source => 'puppet:///modules/openstack/monitor/wmcs-spreadcheck.py',
+        ensure => absent,
     }
 
     ['tools', 'deployment-prep', 'cloudinfra'].each |String $project| {
         file { "/etc/wmcs-spreadcheck-${project}.yaml":
-            owner  => 'nagios',
-            group  => 'nagios',
-            mode   => '0400',
-            source => "puppet:///modules/openstack/monitor/wmcs-spreadcheck-${project}.yaml",
-        }
-        nrpe::monitor_service { "check-${project}-spread":
-            nrpe_command  => "/usr/local/sbin/wmcs-spreadcheck --config /etc/wmcs-spreadcheck-${project}.yaml",
-            description   => "${project} project instance distribution",
-            critical      => false,
-            contact_group => 'wmcs-team-email,wmcs-bots',
-            require       => File[
-                '/usr/local/sbin/wmcs-spreadcheck',
-                "/etc/wmcs-spreadcheck-${project}.yaml"
-            ],
-            notes_url     => 'https://wikitech.wikimedia.org/wiki/Portal:Cloud_VPS/Admin/Troubleshooting',
+            ensure => absent,
         }
     }
 }
