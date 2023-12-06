0d2281022e4d1a06cc1eb2a7518d4b09bac1c272
Balthazar Rouberol <brouberol@wikimedia.org>
2023-12-06 14:06:13 +0100

    An an option to configure the event log storage location for all spark jobs
    When enabled, this will store historical events in HDFS, which will
    enable the Spark history server to maintain a historical knowledge
    of them.
    
    Bug: T352849
    Hosts: an-test-worker1001.eqiad.wmnet
    Hosts: an-worker1078.eqiad.wmnet
    Change-Id: Ice85a6e29476ff92d1256e70664ea0e27c939aba

--- modules/profile/manifests/hadoop/spark3.pp
+++ modules/profile/manifests/hadoop/spark3.pp
@@ -74,6 +74,9 @@
 #   be omitted from the configuration file and the compiled-in default value of /tmp
 #   will be used.
 #
+# [*event_log_dir*]
+#   This option is used as a default value for the spark.eventLog.dir configuration
+#   parameter. It is used to store historical data about the jobs.
 class profile::hadoop::spark3 (
     Bigtop::Spark::Version $default_shuffler_version = lookup('profile::spark3::default_shuffler_version', { 'default_value' => '3.1' }),
     Boolean $install_yarn_shuffle_jar                = lookup('profile::hadoop::spark3::install_yarn_shuffle_jar', { 'default_value' => false }),
@@ -85,10 +88,10 @@ class profile::hadoop::spark3 (
     Integer $port_max_retries                        = lookup('profile::hadoop::spark3::port_max_retries', { 'default_value' => 100 }),
     Stdlib::Unixpath $executor_env_ld_lib_path       = lookup('profile::hadoop::spark3::executor_env_ld_lib_path', { 'default_value' => '/usr/lib/hadoop/lib/native' }),
     Boolean $encryption_enabled                      = lookup('profile::hadoop::spark3::encryption_enabled', { 'default_value' => true }),
-    Optional[Stdlib::Unixpath] $local_dir            = lookup('profile::hadoop::spark3::local_dir', { 'default_value' => undef })
+    Optional[Stdlib::Unixpath] $local_dir            = lookup('profile::hadoop::spark3::local_dir', { 'default_value' => undef }),
+    Optional[String] $event_log_dir                  = lookup('profile::hadoop::spark3::event_log_dir', { 'default_value' => undef })
 ) {
     require ::profile::hadoop::common
-
     # We use conda-analytics to distribute spark3,
     # and also want to use it as the default analytics cluster python for spark.
     require ::profile::analytics::conda_analytics
