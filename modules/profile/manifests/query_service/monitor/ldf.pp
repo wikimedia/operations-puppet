c575a3ad55411e20665aa63f43a6ab3539530256
Brian King <bking@wikimedia.org>
2023-12-08 16:05:24 -0600

    wdqs: simplify ldf endpoint check
    The prometheus blackbox check is returning
    a 500 error. We think this is due to the
    query string in the path, so remove it.
    
    Hosts: wdqs1008.eqiad.wmnet,wdqs1015.eqiad.wmnet
    Hosts: wdqs1009.eqiad.wmnet
    Bug: T347355
    Change-Id: I8ab914a33fdb873e2c3afeded474517639f8f707

--- modules/profile/manifests/query_service/monitor/ldf.pp
+++ modules/profile/manifests/query_service/monitor/ldf.pp
@@ -5,16 +5,15 @@
 
 class profile::query_service::monitor::ldf {
     prometheus::blackbox::check::http { 'query.wikidata.org-ldf':
-        instance_label     => $facts['hostname'],
-        server_name        => 'query.wikidata.org',
-        team               => 'search-platform',
-        severity           => 'task',
-        path               => '/bigdata/ldf?subject=wd%3AQ42&predicate=wdt%3AP31&object=',
-        body_regex_matches => ['wd:Q42\s+wdt:P31\s+wd:Q5\s+\.'],
-        force_tls          => false,
-        ip4                => $facts['ipaddress'],
-        ip6                => $facts['ipaddress6'],
-        port               => 80,
-        useragent          => 'prometheus-ldf-check',
+        instance_label => $facts['hostname'],
+        server_name    => 'query.wikidata.org',
+        team           => 'search-platform',
+        severity       => 'task',
+        path           => '/bigdata/ldf',
+        force_tls      => false,
+        ip4            => $facts['ipaddress'],
+        ip6            => $facts['ipaddress6'],
+        port           => 80,
+        useragent      => 'prometheus-ldf-check',
     }
 }
