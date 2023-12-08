470fc3065cb42de1af8c28da1635b583dc358cf0
Eric Evans <eevans@wikimedia.org>
2023-12-08 14:35:28 -0600

    restbase: set production role and add config for restbase2031
    This is a new host being added to the cluster.
    
    Bug: T352468
    Change-Id: I8c356f13f6ff7b709131478f4fe2f40bbae62914

--- manifests/site.pp
+++ manifests/site.pp
@@ -2686,12 +2686,12 @@ node /^restbase10(19|2[0-9]|3[0-3])\.eqiad\./ {
 }
 
 # restbase codfw cluster
-node /^restbase20(1[3-9]|2[0-9]|30)\.codfw\./ {
+node /^restbase20(1[3-9]|2[0-9]|3[0-1])\.codfw\./ {
     role(restbase::production)
 }
 
 # new restbase nodes T349758
-node /^restbase20(3[1-5])\.codfw\./ {
+node /^restbase20(3[2-5])\.codfw\./ {
     role(insetup::serviceops)
 }
 
