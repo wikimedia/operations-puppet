4cbe42ec45bfa50f6087c7919c761cea8606921c
Taavi Väänänen <hi@taavi.wtf>
2023-12-08 16:30:12 -0800

    P:mail: use wmcloud.org instead of wmflabs.org in envelopes
    Change-Id: Ifd13a95a4cd0501dab6f8c8f8f2297f5041ad1ca

--- modules/profile/manifests/mail/smarthost/wmcs.pp
+++ modules/profile/manifests/mail/smarthost/wmcs.pp
@@ -1,12 +1,18 @@
 # SPDX-License-Identifier: Apache-2.0
 # sets up a mail smarthost for Wikimedia cloud environment
-class profile::mail::smarthost::wmcs {
-
+class profile::mail::smarthost::wmcs (
+    Array[Wmflib::Host::Wildcard] $internal_domains = lookup('profile::mail::smarthost::wmcs::internal_domains'),
+    Stdlib::Fqdn                  $external_domain  = lookup('profile::mail::smarthost::wmcs::external_domain'),
+) {
     include network::constants
+
+    $rewrite_rules = $internal_domains.map |Wmflib::Host::Wildcard $x| {
+        "*@*.${x}  root@${external_domain}  F"
+    }
 
     class { '::profile::mail::smarthost':
         relay_from_hosts       => $network::constants::labs_networks,
-        root_alias_rcpt        => 'root@wmflabs.org',
-        envelope_rewrite_rules => [ '*@*.eqiad.wmflabs  root@wmflabs.org  F' ],
+        root_alias_rcpt        => "root@${external_domain}",
+        envelope_rewrite_rules => $rewrite_rules,
     }
 }
