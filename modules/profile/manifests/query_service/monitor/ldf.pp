a60d58af2ea0ae43394d2cf196385b7d28d5add6
Brian King <bking@wikimedia.org>
2023-12-11 11:38:54 -0600

    wdqs: Try icinga-based check instead of blackbox
    The prometheus-based pollers always get a 500
    when accessing the ldf endpoint. Try an icinga-based
    check to see if we get a different response.
    
    Bug: T347355
    Hosts: wdqs1016.eqiad.wmnet,wdqs1008.eqiad.wmnet,wdqs1015.eqiad.wmnet
    Change-Id: I6ccc561ef43fea286bdc8299adc2059208fdb679

--- modules/profile/manifests/query_service/monitor/ldf.pp
+++ modules/profile/manifests/query_service/monitor/ldf.pp
@@ -1,19 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 # ldf endpoint monitoring, see T347355.
-# Temporarily set to http instead of https until we can figure out
-# some routing issues between prometheus and wdqs envoy
 
 class profile::query_service::monitor::ldf {
-    prometheus::blackbox::check::http { 'query.wikidata.org-ldf':
-        instance_label => $facts['hostname'],
-        server_name    => 'query.wikidata.org',
-        team           => 'search-platform',
-        severity       => 'task',
-        path           => '/bigdata/ldf',
-        force_tls      => false,
-        ip4            => $facts['ipaddress'],
-        ip6            => $facts['ipaddress6'],
-        port           => 80,
-        useragent      => 'prometheus-ldf-check',
+    monitoring::service { 'WDQS_LDF_Endpoint':
+        description   => 'WDQS Linked Data Fragments Endpoint',
+        check_command => 'check_https_url_for_string!query.wikidata.org!/bigdata/ldf?subject=wd%3AQ42&predicate=wdt%3AP31&object=!wd:Q42  wdt:P31  wd:Q5 .',
+        notes_url     => 'https://wikitech.wikimedia.org/wiki/Wikidata_query_service/Runbook',
     }
 }
