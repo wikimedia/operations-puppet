bd51fc7cfd1148bbc78c394ee37ad04bbc40b40f
Arnaud Bran <abran@wikimedia.org>
2023-12-13 12:40:44 +0100

    mariadb: productionize db1232, db1233, db1248
    as per T344036 we need to productionize those servers
    **db1233** as mentionned in the ticket will be cloning **db1129**
    which already has `profile::monitoring::notifications_enabled: false`
    **db1233** has been reimaged to debian 12
    
    Bug: T344036
    Change-Id: I9989228d416eefb9429aa21ff40e59b007cb5ef6

--- manifests/site.pp
+++ manifests/site.pp
@@ -535,7 +535,7 @@ node /^db1208\.eqiad\./ {
 }
 
 # s1 (enwiki) core production dbs on eqiad
-node /^db1(106|128|132|134|135|163|169|184|186|206|207|218|219|228)\.eqiad\./ {
+node /^db1(106|128|132|134|135|163|169|184|186|206|207|218|219|228|232)\.eqiad\./ {
     role(mariadb::core)
 }
 
@@ -557,7 +557,7 @@ node /^db2173\.codfw\./ {
 
 # s2 (large wikis) core production dbs on eqiad
 # See also db1146, db1170 below
-node /^db1(129|162|182|188|197|222|229)\.eqiad\./ {
+node /^db1(129|162|182|188|197|222|229|233)\.eqiad\./ {
     role(mariadb::core)
 }
 
@@ -599,7 +599,7 @@ node /^db2156\.codfw\./ {
 
 # s4 (commons) core production dbs on eqiad
 # See also db1144 and db1146 below
-node /^db1(147|148|149|160|190|199|238|241|242|243|247)\.eqiad\./ {
+node /^db1(147|148|149|160|190|199|238|241|242|243|247|248)\.eqiad\./ {
     role(mariadb::core)
 }
 
@@ -748,7 +748,7 @@ node /^db21(42|43|44)\.codfw\./ {
 }
 
 # New db hosts to be setup T342166
-node /^db12(39|32|33|34|35|39|40|44|45|46|48|49)\.eqiad\./ {
+node /^db12(39|34|35|39|40|44|45|46|49)\.eqiad\./ {
     role(insetup::data_persistence)
 }
 ## m1 shard
