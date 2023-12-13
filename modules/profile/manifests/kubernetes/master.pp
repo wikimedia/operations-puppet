0eed8ed0b7a0ac9e86ff973dcfa742d83217d5a4
Janis Meybohm <jmeybohm@wikimedia.org>
2023-12-13 17:44:57 +0100

    kubernetes::master Group blackbox checks per cluster
    Group the kube-apiserver blackbox checks per cluster rather than all
    together.
    This should set the proper server_name (for TLS verification) as well as
    separate cert expiry alerts per cluster.
    Follow-Up for: Ib47f4008b660c235116df2b534c9e97ed282886c
    
    Bug: T353233
    Hosts: auto
    Change-Id: I7cfb04dcf7a529f3f7ac1cb099c4d92e9948b0db

--- modules/profile/manifests/kubernetes/master.pp
+++ modules/profile/manifests/kubernetes/master.pp
@@ -179,8 +179,8 @@ class profile::kubernetes::master (
         default => 'critical', # FIXME: Switch to 'page' once verified
     }
     # Add a blackbox check for the kube-apiserver
-    prometheus::blackbox::check::http { 'kube-apiserver':
-        server_name             => $facts['networking']['fqdn'],
+    prometheus::blackbox::check::http { $kubernetes_cluster_name + '-kube-apiserver':
+        server_name             => $k8s_config['master'],
         team                    => 'sre',
         severity                => $severity,
         path                    => '/readyz',
