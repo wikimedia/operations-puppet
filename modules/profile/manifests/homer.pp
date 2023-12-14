4eaaa0de38771cf97c68a65ad5c4b456558c1c08
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-12-14 10:40:53 +0100

    homer: Add profile option to disable homer for a given host
    Add an option to not enable Homer for a new Cumin host.
    
    Hosts: cumin1001.eqiad.wmnet,cumin2002.codfw.wmnet
    Bug: T353419
    Change-Id: If66f8f209bbbb5bef73eee51e582798a377753a8

--- modules/profile/manifests/homer.pp
+++ modules/profile/manifests/homer.pp
@@ -8,49 +8,58 @@ class profile::homer (
     String $nb_ro_token = lookup('profile::netbox::ro_token'),
     Stdlib::HTTPSUrl $nb_api = lookup('netbox_api_url'),
     Optional[String[1]] $diff_timer_interval = lookup('profile::homer::diff_timer_interval'),
+    Optional[Boolean] $disable_homer = lookup('profile::homer:disable', {'default_value' => false}),
 ){
-    class { 'python_deploy::venv':
-        project_name => 'homer',
-        deploy_user  => 'deploy-homer',
-    }
 
-    keyholder::agent { 'homer':
-        trusted_groups => ['ops', 'root'],
-    }
+    unless $disable_homer {
 
-    class { 'homer':
-        private_git_peer => $private_git_peer,
-        nb_token         => $nb_ro_token,
-        nb_api           => $nb_api,
-    }
+        class { 'python_deploy::venv':
+            project_name => 'homer',
+            deploy_user  => 'deploy-homer',
+        }
 
-    file { '/usr/local/sbin/check-homer-diff':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0544',
-        source  => 'puppet:///modules/profile/homer/check_homer_diff.sh',
-        require => Class['homer'],
-    }
+        keyholder::agent { 'homer':
+            trusted_groups => ['ops', 'root'],
+        }
 
-    $check_homer_diff_ensure = $diff_timer_interval  ? {
-        undef   => absent,
-        default => present,
-    }
+        class { 'homer':
+            private_git_peer => $private_git_peer,
+            nb_token         => $nb_ro_token,
+            nb_api           => $nb_api,
+        }
 
-    # If unset set a fixed value in the past just to pass validation by systemd-analyze calendar
-    # as the timer will be absented in this case and interval is a required parameter.
-    $effective_diff_timer_interval = pick($diff_timer_interval, '2021-01-01')
+        file { '/usr/local/sbin/check-homer-diff':
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0544',
+            source  => 'puppet:///modules/profile/homer/check_homer_diff.sh',
+            require => Class['homer'],
+        }
 
-    systemd::timer::job { 'check-homer-diff':
-        ensure      => $check_homer_diff_ensure,
-        description => 'Check if any network device has a live config that differs from the code-defined one',
-        command     => '/usr/local/sbin/check-homer-diff',
-        interval    => {
-            'start'    => 'OnCalendar',
-            'interval' => $effective_diff_timer_interval,
-        },
-        user        => 'root',  # Needed to access the keyholder SSH key
-        require     => File['/usr/local/sbin/check-homer-diff'],
+        if $disable_homer {
+            $check_homer_diff_ensure = absent
+        } else {
+            $check_homer_diff_ensure = $diff_timer_interval  ? {
+                undef   => absent,
+                default => present,
+            }
+        }
+
+        # If unset set a fixed value in the past just to pass validation by systemd-analyze calendar
+        # as the timer will be absented in this case and interval is a required parameter.
+        $effective_diff_timer_interval = pick($diff_timer_interval, '2021-01-01')
+
+        systemd::timer::job { 'check-homer-diff':
+            ensure      => $check_homer_diff_ensure,
+            description => 'Check if any network device has a live config that differs from the code-defined one',
+            command     => '/usr/local/sbin/check-homer-diff',
+            interval    => {
+                'start'    => 'OnCalendar',
+                'interval' => $effective_diff_timer_interval,
+            },
+            user        => 'root',  # Needed to access the keyholder SSH key
+            require     => File['/usr/local/sbin/check-homer-diff'],
+        }
     }
 }
