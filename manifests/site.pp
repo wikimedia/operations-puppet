a259fd50b81fd204e70abff20522f05eee1c53e6
Alexandros Kosiaris <akosiaris@wikimedia.org>
2023-12-19 16:37:44 +0200

    Fix insetup role for restbase203[3-5]
    Why:
    It's actually data persistence
    
    What:
    Switch from insetup::serviceops to insetup::data_persistence
    
    Hosts: auto
    Bug: T352468
    Change-Id: I2ab2e8741dded691ebcc1afb4d26233baf2bc1e5

--- manifests/site.pp
+++ manifests/site.pp
@@ -2695,7 +2695,7 @@ node /^restbase20(1[3-9]|2[0-9]|3[0-2])\.codfw\./ {
 
 # new restbase nodes T349758
 node /^restbase20(3[3-5])\.codfw\./ {
-    role(insetup::serviceops)
+    role(insetup::data_persistence)
 }
 
 # New cassandra dev nodes T324113
