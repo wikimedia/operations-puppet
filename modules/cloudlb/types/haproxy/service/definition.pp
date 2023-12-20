441f36ed99026d6e75776cfefe2202b33f558dcb
Andrew Bogott <abogott@wikimedia.org>
2023-12-20 14:49:13 -0600

    keystone haproxy: increase server timeout for admin service to 10m
    This is a ridiculously long timeout but our api calls have been getting
    slower lately and now project creation (and a few other actions)
    have been getting interrupted by haproxy.
    
    Hosts: cloudlb1001.eqiad.wmnet
    Bug: T353829
    Change-Id: I8cb9c65d4336a3d96d5844a41c8617c5b2956f98

--- modules/cloudlb/types/haproxy/service/definition.pp
+++ modules/cloudlb/types/haproxy/service/definition.pp
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 type CloudLB::HAProxy::Service::Definition = Struct[{
-    'frontends'     => Array[CloudLB::HAProxy::Service::Frontend],
-    'backend'       => CloudLB::HAProxy::Service::Backend,
-    'healthcheck'   => CloudLB::HAProxy::Service::Healthcheck,
-    'firewall'      => CloudLB::HAProxy::Service::Firewall,
-    'type'          => Enum['http', 'tcp'],
-    'http'          => Optional[CloudLB::HAProxy::Service::HTTPOptions],
+    'frontends'   => Array[CloudLB::HAProxy::Service::Frontend],
+    'backend'     => CloudLB::HAProxy::Service::Backend,
+    'healthcheck' => CloudLB::HAProxy::Service::Healthcheck,
+    'firewall'    => CloudLB::HAProxy::Service::Firewall,
+    'type'        => Enum['http', 'tcp'],
+    'http'        => Optional[CloudLB::HAProxy::Service::HTTPOptions],
 }]
--- modules/cloudlb/types/haproxy/service/httpoptions.pp
+++ modules/cloudlb/types/haproxy/service/httpoptions.pp
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # @summary http-specific cloudlb service options
 type CloudLB::HAProxy::Service::HTTPOptions = Struct[{
-  require_host => Optional[Stdlib::Fqdn],
-  set_headers  => Optional[Hash[String[1], String[1]]],
+  require_host   => Optional[Stdlib::Fqdn],
+  set_headers    => Optional[Hash[String[1], String[1]]],
+  timeout_server => Optional[Pattern[/\d+s/]],
 }]
