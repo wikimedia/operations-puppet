c3e73f122d0340a5d4e6c96669bd8ae1c2c43ff3
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-01-03 10:46:58 +0100

    grafana: Switch rsync service to use firewall::service
    1930b5c213e7703e53862b345f5dbe9b415bb6c0 added a new option to the
    rsync::quickdatacopy define to add a firewall::service definition
    for the included firewall rule instead of a ferm-specific definition.
    
    This will eventually allow to migrate invididual roles to nftables.
    
    Also remove a misleading comment, with the current use of the
    define, no automatic cron job/systemd timer is created.
    
    Hosts: grafana1002.eqiad.wmnet,grafana2001.codfw.wmnet
    Change-Id: I37f172da983e8672a71497c43fb1cb49701513eb

--- modules/profile/manifests/grafana/production.pp
+++ modules/profile/manifests/grafana/production.pp
@@ -8,8 +8,8 @@ class profile::grafana::production (
     Stdlib::Fqdn $active_host = lookup('profile::grafana::active_host'),
     Stdlib::Fqdn $standby_host = lookup('profile::grafana::standby_host'),
 ) {
-    include ::profile::grafana
-    include ::profile::grafana::grizzly
+    include profile::grafana
+    include profile::grafana::grizzly
 
     $on_active_host = $active_host == $::fqdn ? {
         true  => present,
@@ -17,14 +17,14 @@ class profile::grafana::production (
     }
 
     # Enables rsync'ing /var/lib/grafana from active host to standby host.
-    # Set ensure => absent to disable the rsync job.
     rsync::quickdatacopy { 'var-lib-grafana':
-      ensure              => present,
-      source_host         => $active_host,
-      dest_host           => $standby_host,
-      module_path         => '/var/lib/grafana',
-      server_uses_stunnel => true,
-      exclude             => 'grafana.db-journal',
+      ensure               => present,
+      source_host          => $active_host,
+      dest_host            => $standby_host,
+      module_path          => '/var/lib/grafana',
+      server_uses_stunnel  => true,
+      exclude              => 'grafana.db-journal',
+      use_generic_firewall => true,
     }
 
     class {'::grafana::ldap_sync':
