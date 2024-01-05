a19b63703e0c8ee407afc4fe31d76150cd27f838
Kosta Harlan <kharlan@wikimedia.org>
2024-01-05 16:32:14 +0100

    mediamoderation: Switch to using all.dblist
    We have prepared all wikis for work with MediaModeration, so we can now
    run updateMetrics.php on all of them.
    
    Bug: T353703
    Change-Id: Id555467b3c19ad166a962942b40f4c4a981cfd34

--- modules/profile/manifests/mediawiki/maintenance/mediamoderation.pp
+++ modules/profile/manifests/mediawiki/maintenance/mediamoderation.pp
@@ -2,7 +2,7 @@
 class profile::mediawiki::maintenance::mediamoderation {
     # push periodically-computed metrics into statsd (T353703)
     profile::mediawiki::periodic_job { 'mediamoderation-updateMetrics':
-        command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/mediamoderation.dblist extensions/MediaModeration/maintenance/updateMetrics.php --verbose',
+        command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/all.dblist extensions/MediaModeration/maintenance/updateMetrics.php --verbose',
         interval => '*-*-* 04:32:00',
     }
 }
