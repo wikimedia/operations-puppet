37475246da3a291c8cb7796e90455913868f8fca
Majavah <taavi@wikimedia.org>
2024-01-09 15:49:20 +0000

    Revert "P:toolforge::mailrelay: rewrite maintainers in Python"
    This reverts commit 1f650955d7cfcdd1c4cea1d267260a18d124747f.
    
    Reason for revert: weird LDAP inconsistency makes memberOf unreliable
    
    Change-Id: Ie47cf3fd6c301bbeceb034e4003a29fd78d453a3

--- modules/profile/manifests/toolforge/mailrelay.pp
+++ modules/profile/manifests/toolforge/mailrelay.pp
@@ -81,18 +81,12 @@ class profile::toolforge::mailrelay (
         source => 'puppet:///modules/profile/toolforge/mailrelay/localuser',
     }
 
-    ensure_packages([
-        'python3-ldap3',
-        'python3-systemd',
-        'python3-yaml',
-    ])
-
     file { '/usr/local/sbin/maintainers':
         ensure => file,
         owner  => 'root',
         group  => 'root',
         mode   => '0555',
-        source => 'puppet:///modules/profile/toolforge/mailrelay/maintainers.py',
+        source => 'puppet:///modules/profile/toolforge/mailrelay/maintainers',
     }
 
     file { '/etc/aliases':
