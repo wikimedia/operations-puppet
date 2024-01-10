8917482898668a1c4143a4015cd2b9dd04262c36
Alexandros Kosiaris <akosiaris@wikimedia.org>
2024-01-10 13:03:20 +0200

    kmod::blacklist: Allow also rmmoding modules
    Why:
    kmod::blacklist blacklists kernel modules, but for the blacklist to
    happen, a reboot is required. However, there are a few cases, where race
    conditions can happen. For example a kernel module that can cause
    reimaging cookbooks to fail might be loaded long before kmod::blacklist
    gets applied.
    
    What:
    * Add an rmmod parameter, defaulting to false for backwards
      compatibility that will also execute an Exec puppet resource that does
      modprobe -r on the listed modules
    * Add types and docs to parameters
    
    Hosts: auto
    Bug: T354413
    Change-Id: I8c3eeb471ca10b2f0d55b39c50f52cf95f0edf04

--- modules/kmod/manifests/blacklist.pp
+++ modules/kmod/manifests/blacklist.pp
@@ -5,8 +5,13 @@
 #
 # === Parameters
 #
+# [*ensure*]
+#   Standard ensure parameter
 # [*modules*]
 #   The list of module names to be blacklisted.
+# [*rmmod*]
+#   A boolean to also remove the blacklisted module. Defaults to false for
+#   compatibility reasons
 #
 # === Example
 #
@@ -14,13 +19,32 @@
 #     modules => [ 'asn1_decoder', 'macsec' ],
 # }
 #
-define kmod::blacklist($modules = [], $ensure = present) {
+define kmod::blacklist (
+    Wmflib::Ensure $ensure = present,
+    Array[String] $modules = [],
+    Boolean $rmmod = false,
+) {
+    if $rmmod {
+        $rmmod_args = join($modules, ' ')
+        exec { "rmmod-${name}":
+          command     => "/sbin/modprobe -r ${rmmod_args}",
+          refreshonly => true,
+        }
+        $notify = [
+          Exec['update-initramfs'],
+          Exec["rmmod-${name}"],
+        ]
+    } else {
+        $notify = [
+          Exec['update-initramfs'],
+        ]
+    }
     file { "/etc/modprobe.d/blacklist-${name}.conf":
         ensure  => $ensure,
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
         content => template('kmod/blacklist.conf.erb'),
-        notify  => Exec['update-initramfs'],
+        notify  => $notify,
     }
 }
