59a51ec442e48b2ae026d75adeadf7566e4cd770
Daniel Zahn <dzahn@wikimedia.org>
2024-01-19 10:25:10 -0800

    phabricator: clean up repo sync class after test
    After a temp test has concluded, set the rsync class to
    absent to properly clean up afterwards.
    
    Bug: T334519
    Change-Id: I56283af35d652f051e790198191838ebc896ae5f

--- modules/profile/manifests/phabricator/reposync.pp
+++ modules/profile/manifests/phabricator/reposync.pp
@@ -3,9 +3,9 @@
 # temporary test
 class profile::phabricator::reposync {
 
-    if $::fqdn in ['phab2002.codfw.wmnet', 'people1004.eqiad.wmnet'] {
+    if $::fqdn in ['phab2002.codfw.wmnet', 'people1004.eqiad.wmnet', 'gitlab2003.wikimedia.org'] {
         rsync::quickdatacopy { 'phabricator-repos-test':
-            ensure      => present,
+            ensure      => absent,
             auto_sync   => false,
             delete      => true,
             source_host => 'phab2002.codfw.wmnet',
