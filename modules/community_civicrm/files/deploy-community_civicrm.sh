d5cda06e282d5f2ff15267e1e73055919677756a
Dallas Wisehaupt <dwisehaupt@wikimedia.org>
2024-01-18 16:18:08 -0800

    Fix deployment diff issue and clean up presentation
    The diff portion of the deployment script was not behaving as desired.
    The grep -v "./" command was causing any file in a subdir to match and
    thus hiding changes when trying to verify what will be done. This change
    escapes the . to make it a literal match.
    
    Also updated the deploy step to run puppet after the file copy. This is
    to fix up any possible permission issues on the files/dirs and any
    templated files in the off chance they get updated.
    
    Additionally, updated the help and some other echo statements to make
    the output a little cleaner and consistent.
    
    Bug: T343486
    Change-Id: I7cd31dfe20b3d2708ace6bf0c05b7ecbf7992cc8

--- modules/community_civicrm/files/deploy-community_civicrm.sh
+++ modules/community_civicrm/files/deploy-community_civicrm.sh
@@ -11,60 +11,70 @@ bp="/root/backup"
 
 function deploy {
 
-  echo -e "\nfirst running puppet to git pull\n"
+  echo -e "First running puppet to git pull\n"
   sudo puppet agent -tv
-  echo -e "\ndeploying files from git repository (${pp}/${pn})\n"
+  echo -e "Deploying files from git repository (${pp}/${pn})\n"
 
   for dp in "${dps[@]}"; do
     mkdir -p "/var/www/${pn}/${dp}"
     echo "rsync -avp --cvs-exclude --delete-excluded ${pp}/${pn}/${dp}/ /var/www/${pn}/${dp}/"
     rsync -avp "${pp}/${pn}/${dp}/" "/var/www/${pn}/${dp}/"
   done
+
+  echo -e "Final puppet run to fix any possible permission/template issues\n"
+  sudo puppet agent -tv
 }
 
 function diff {
 
+  echo -e "Rsync test for new/updated files:\n"
+
   for dp in "${dps[@]}"; do
     mkdir -p "/var/www/${pn}/${dp}"
-    echo "/var/www/${pn}/${dp}/"
-    rsync -avn --cvs-exclude "${pp}/${pn}/${dp}/" "/var/www/${pn}/${dp}/" --info=stats0,flist0 | grep -v "./"
-    echo "diff -r ${pp}/${pn}/${dp}/ /var/www/${pn}/${dp}/"
-    echo -e "\n"
+    echo "Checking /var/www/${pn}/${dp}"
+    echo "rsync -avn --cvs-exclude \"${pp}/${pn}/${dp}/\" \"/var/www/${pn}/${dp}/\" --info=stats0,flist0 | grep -v \"\\./\""
+    rsync -avn --cvs-exclude "${pp}/${pn}/${dp}/" "/var/www/${pn}/${dp}/" --info=stats0,flist0 | grep -v "\./"
+    echo -en "\n"
   done
 }
 
 function backup {
 
   mkdir -p ${bp}
-  echo -e "\nbacking up files to to backup location {${bp})\n"
+  echo -e "Backing up files to backup location {${bp})\n"
 
   for dp in "${dps[@]}" ; do
+    echo "Backing up ${pn}/${dp}"
     mkdir -p "${bp}/${pn}/${pn}/${dp}"
     echo "rsync -avp /var/www/${pn}/${dp}/ ${bp}/${pn}/${pn}/${dp}/"
     rsync -avp "/var/www/${pn}/${dp}/" "${bp}/${pn}/${pn}/${dp}/"
+    echo -en "\n"
   done
 
 }
 
 function restore {
 
-  echo -e "\nrestoring files, deploy from backup location (${bp})\n"
+  echo -e "Restoring files, deploy from backup location (${bp})\n"
 
   for dp in "${dps[@]}" ; do
+    echo "Restoring ${pn}/${dp}"
     mkdir -p "/var/www/${pn}/${dp}"
     echo "rsync -avp ${bp}/${pn}/${pn}/${dp}/ /var/www/${pn}/${dp}/"
     rsync -avp "${bp}/${pn}/${pn}/${dp}/" "/var/www/${pn}/${dp}/"
+    echo -en "\n"
   done
 
 }
 
 function help {
 
-  echo -e "usage: $0 <action>. action can be one of 'deploy', 'backup' or 'restore'\n"
-  echo -e "deploy: syncs file from ${pp}/${pn} (where puppet git pulls to automatically) into the right places.\n"
-  echo -e "diff: identifies files that have local hacks that have not been deployed.\n"
-  echo -e "backup: syncs files currently used to a backup location at ${bp}.\n"
-  echo -e "restore: syncs file from the backup location {$bp} into the right places.\n"
+  echo -e "Usage: $0 <action>\n"
+  echo -e "Actions:"
+  echo -e "  deploy: syncs file from ${pp}/${pn} (where puppet git pulls to automatically) into the right places."
+  echo -e "  diff: identifies files that have local hacks or that have not been deployed."
+  echo -e "  backup: syncs files currently used to a backup location at ${bp}."
+  echo -e "  restore: syncs file from the backup location {$bp} into the right places."
 }
 
 case $1 in
