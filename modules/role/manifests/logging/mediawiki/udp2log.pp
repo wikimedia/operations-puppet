765abd29dc022e14e48948944184b35830a20aad
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-01-19 16:35:41 +0100

    udp2log: Replace ferm rules with firewall::service
    ferm::rules are specific to ferm, but we can replace them with a
    functionally equialent firewall::service, which is also compatible
    with nftables eventually.
    
    Hosts: mwlog2002.codfw.wmnet,mwlog1002.eqiad.wmnet
    Change-Id: I0258bf23b9d112219f7280e728ef03ba0f47da85

--- modules/role/manifests/logging/mediawiki/udp2log.pp
+++ modules/role/manifests/logging/mediawiki/udp2log.pp
@@ -26,14 +26,11 @@ class role::logging::mediawiki::udp2log(
 
     class { '::bsection': }
 
-    ferm::rule { 'udp2log_accept_all_wikimedia':
-        rule => 'saddr ($DOMAIN_NETWORKS) proto udp ACCEPT;',
-    }
-
-    ferm::rule { 'udp2log_notrack':
-        table => 'raw',
-        chain => 'PREROUTING',
-        rule  => 'saddr ($DOMAIN_NETWORKS) proto udp NOTRACK;',
+    firewall::service {'udp2log_accept_all_wikimedia':
+        proto      => 'udp',
+        port_range => [1,65535],
+        src_sets   => ['DOMAIN_NETWORKS'],
+        notrack    => true,
     }
 
     file { '/usr/local/bin/demux.py':
