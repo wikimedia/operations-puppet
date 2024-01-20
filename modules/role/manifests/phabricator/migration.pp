98586a0e4bec985184351851d0b77180cfabf250
Daniel Zahn <dzahn@wikimedia.org>
2024-01-20 15:30:59 -0800

    phabricator: temp add scap user creation in phab migration class
    To work around a race condition where the scap user does not
    exist and therefore scap can't deploy itself.
    
    Change-Id: I4fa63e1f2ab4c9662cde668eb353f540899b5d1c

--- modules/role/manifests/phabricator/migration.pp
+++ modules/role/manifests/phabricator/migration.pp
@@ -6,7 +6,9 @@ class role::phabricator::migration {
         description => 'temp role to allow migrating Phabricator data to a new server',
     }
 
+    class { 'scap::user': }
+
     include ::profile::base::production
     include ::profile::firewall
-    include ::profile::phabricator::migration
+    # include ::profile::phabricator::migration
 }
