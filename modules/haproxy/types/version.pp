34c3cdfeab3cefc8ea43a7ed9d6d3a7829bd1567
Valentin Gutierrez <vgutierrez@wikimedia.org>
2024-01-22 11:41:34 +0100

    aptrepo,haproxy: Allow installing HAProxy 2.8 on bullseye
    Bug: T354424
    Change-Id: I57de8c1d2c2ce7e127014f511eb1ef29f1cd1f5c

--- modules/haproxy/types/version.pp
+++ modules/haproxy/types/version.pp
@@ -1,2 +1,2 @@
 # SPDX-License-Identifier: Apache-2.0
-type Haproxy::Version = Enum['haproxy24', 'haproxy26', 'haproxy27']
+type Haproxy::Version = Enum['haproxy24', 'haproxy26', 'haproxy27', 'haproxy28']
