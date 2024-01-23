5fb9040cd0ac8a7fee088d8812118c204bce6c61
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-01-23 15:01:00 +0100

    Make ganeti1037 a Ganeti node
    Bug: T349925
    Change-Id: I5a74522f1178a19927cec900999054e01728ae7e

--- manifests/site.pp
+++ manifests/site.pp
@@ -1313,11 +1313,11 @@ node /^lists1001\.wikimedia\./ {
 node /^lists1004\.eqiad\./ {
     role(insetup::unowned)
 }
-node /^ganeti10(09|1[0-9]|2[0-9]|3[0-6])\.eqiad\./ {
+node /^ganeti10(09|1[0-9]|2[0-9]|3[0-7])\.eqiad\./ {
     role(ganeti)
 }
 # New eqiad ganeti nodes T349925
-node /^ganeti103[7-8]\.eqiad\./ {
+node /^ganeti103[8]\.eqiad\./ {
     role(insetup::infrastructure_foundations)
 }
 
