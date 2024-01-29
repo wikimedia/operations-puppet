0d216d21a93e8a3c05f1f7561cb57f56d3e75382
Daniel Zahn <dzahn@wikimedia.org>
2024-01-29 13:34:17 -0800

    phabricator: fix typo in python3-pymysql package name
    follow-up  Id01ebf0ef347922
    
    Bug: T355574
    Bug: T355502
    Change-Id: I483e2c1139dd2eca51ae3e88bd85fb6afb608be2

--- modules/phabricator/manifests/tools.pp
+++ modules/phabricator/manifests/tools.pp
@@ -28,7 +28,7 @@ class phabricator::tools (
         package { 'python-mysqldb': ensure => present }
     } else {
         package { 'python3-mysqldb': ensure => present }
-        package { 'python3-pymsql': ensure => present }
+        package { 'python3-pymysql': ensure => present }
     }
 
     file { '/etc/phabtools.conf':
