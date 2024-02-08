e1c95b5c4c98353f252807aeec9d09de958d7242
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2024-02-08 12:26:23 +0100

    nrpe: absent systemd_scripts
    No longer used
    
    Bug: T337831
    Change-Id: Iae5e226288031ab24866dac51d42b4aeb8ea8d52

--- modules/nrpe/manifests/systemd_scripts.pp
+++ modules/nrpe/manifests/systemd_scripts.pp
@@ -4,11 +4,7 @@
 #
 
 class nrpe::systemd_scripts {
-    file { '/usr/local/bin/nrpe_check_systemd_unit_state':
-        ensure => absent,
-    }
-
     nrpe::plugin { 'check_systemd_unit_state':
-        source => 'puppet:///modules/nrpe/plugins/check_systemd_unit_state.py',
+        ensure => absent,
     }
 }
