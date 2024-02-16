07a4083f0d75be13b381e0d9ae823cfdca4ff0c8
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-02-16 15:56:37 +0100

    thanos backend: Avoid Ferm-specific syntax
    Remove Ferm-specific syntax from firewall definitions and switch to
    firewall::service. This will eventually allow to migrate invididual
    roles to nftables.
    
    The new srange() function revolves DNS names on the puppetserver side
    (and that resolve happens transparently for A/AAAA)
    
    Hosts: C:profile::thanos::swift::backend
    Change-Id: Ib1158d586aeda91f4a5dc3e5def1b5c599071c00

--- modules/profile/manifests/thanos/swift/backend.pp
+++ modules/profile/manifests/thanos/swift/backend.pp
@@ -68,32 +68,30 @@ class profile::thanos::swift::backend (
     swift::mount_filesystem { $aux_partitions: }
 
     $swift_access = concat($thanos_backends, $thanos_frontends)
-    $swift_access_ferm = join($swift_access, ' ')
-    $swift_rsync_access_ferm = join($thanos_backends, ' ')
 
-    # Optimize ferm rule aggregating all ports, it includes:
+    # Optimize firewall rules aggregating all ports, it includes:
     # - base object server (6000)
     # - container server (6001)
     # - account server (6002)
     # - per-disk object-server ports T222366 (6010:6040)
-    ferm::service { 'swift-object-server':
+    firewall::service { 'swift-object-server':
         proto      => 'tcp',
         port_range => [6000, 6002],
         notrack    => true,
-        srange     => "@resolve((${swift_access_ferm}))",
+        srange     => $swift_access,
     }
 
-    ferm::service { 'swift-object-server-per-disk':
+    firewall::service { 'swift-object-server-per-disk':
         proto      => 'tcp',
         port_range => [6010, 6040],
         notrack    => true,
-        srange     => "@resolve((${swift_access_ferm}))",
+        srange     => $swift_access,
     }
 
-    ferm::service { 'swift-rsync':
+    firewall::service { 'swift-rsync':
         proto   => 'tcp',
         port    => 873,
         notrack => true,
-        srange  => "@resolve((${swift_rsync_access_ferm}))",
+        srange  => $thanos_backends,
     }
 }
