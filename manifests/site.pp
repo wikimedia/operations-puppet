fff506e3eb406aae60865ee5d4b267593744e562
Eric Evans <eevans@wikimedia.org>
2024-02-21 14:36:08 -0600

    restbase: provision restbase1039-{a,b,c} (new)
    Bug: T354560
    Change-Id: I73e38f7ffe8542e7527c61c18a7cbedf593998ec

--- manifests/site.pp
+++ manifests/site.pp
@@ -2579,7 +2579,7 @@ node /^relforge100[3-4]\.eqiad\./ {
 }
 
 # restbase eqiad cluster
-node /^restbase10(19|2[0-9]|3[0-8])\.eqiad\./ {
+node /^restbase10(19|2[0-9]|3[0-9])\.eqiad\./ {
     role(restbase::production)
 }
 
@@ -2589,7 +2589,7 @@ node /^restbase20(2[1-9]|3[0-5])\.codfw\./ {
 }
 
 # New restbase (eqiad) hosts T354227
-node /^restbase10(39|4[0-2])\.eqiad\./ {
+node /^restbase104[0-2]\.eqiad\./ {
     role(insetup::data_persistence)
 }
 
