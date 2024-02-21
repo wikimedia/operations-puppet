c07dfa5fcb79c72c84f2438d12252547f14b0234
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-02-21 14:41:04 +0100

    Drop obsolete motd for cumin1001
    Bug: T353419
    Change-Id: If4afa56d7cb72c150e89fc5805286809cd43297c

--- modules/profile/manifests/cumin/master.pp
+++ modules/profile/manifests/cumin/master.pp
@@ -48,14 +48,6 @@ class profile::cumin::master (
         mode   => '0750',
     }
 
-    if $facts['fqdn'] == 'cumin1001.eqiad.wmnet' {
-        motd::script { 'cumin1001_note':
-            ensure   => present,
-            content  => "#!/bin/sh\necho '\\033[31;1;4mPlease use cumin1002/cumin2002 for all cookbooks and Cumin!\\033[0m'\n",
-            priority => 60,
-        }
-    }
-
     file { '/etc/cumin':
         ensure => directory,
         owner  => 'root',
