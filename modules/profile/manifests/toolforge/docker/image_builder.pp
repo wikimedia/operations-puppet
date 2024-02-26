a9cec01523067b65168151e77c57a8383cb285df
Taavi Väänänen <hi@taavi.wtf>
2024-02-26 14:22:30 +0200

    P:toolforge: image_builder: refresh for bookworm
    * Use Debian-packaged Docker instead of the mirror of Docker-provided
      packages we had for buster.
    * Do not include the early buildpack-specific repos and packages that
      are not currently in use.
    * Do not include the base images class, as we rely on base-images from
      docker-registry.wikimedia.org.
    * Use a Cinder volume for extra storage instead of a second LVM
      partition.
    * Remove apt-pinning like we've done for other roles.
    
    Bug: T358483
    Change-Id: Ie025ac1ecd7be7e8086921c08b4dc3df305765f3

--- modules/profile/manifests/toolforge/docker/image_builder.pp
+++ modules/profile/manifests/toolforge/docker/image_builder.pp
@@ -4,35 +4,53 @@ class profile::toolforge::docker::image_builder(
     Stdlib::Fqdn $docker_registry = lookup('docker::registry'),
     String       $component       = lookup('profile::wmcs::kubeadm::component'),
 ) {
-    # This should be building with the same docker we are running
-    class { '::kubeadm::repo':
-        component => $component,
-    }
+    if debian::codename::eq('buster') {
+        # This should be building with the same docker we are running
+        class { '::kubeadm::repo':
+            component => $component,
+        }
 
-    labs_lvm::volume { 'docker':
-        size      => '70%FREE',
-        mountat   => '/var/lib/docker',
-        mountmode => '711',
-    } -> class {'profile::labs::lvm::srv': }
+        labs_lvm::volume { 'docker':
+            size      => '70%FREE',
+            mountat   => '/var/lib/docker',
+            mountmode => '711',
+        } -> class {'profile::labs::lvm::srv': }
 
-    class { '::kubeadm::docker': }
+        class { '::kubeadm::docker': }
 
-    class { '::docker::baseimages':
-        docker_registry => $docker_registry,
+        class { '::docker::baseimages':
+            docker_registry => $docker_registry,
+        }
+
+        git::clone { 'cloud/toolforge/buildpacks':
+            ensure    => present,
+            directory => '/srv/buildpacks',
+        }
+
+        # Available in Toolforge's aptly repo
+        ensure_packages(['pack'])
+    } else {
+        include profile::docker::engine
+        cinderutils::ensure { 'separate-docker':
+            min_gb        => 10,
+            mount_point   => '/var/lib/docker',
+            mount_mode    => '711',
+            mount_options => 'discard,defaults',
+            notify        => Service['docker'],
+        }
+
+        file { '/srv/images':
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755',
+        }
     }
 
     git::clone { 'operations/docker-images/toollabs-images':
         ensure    => present,
         directory => '/srv/images/toolforge',
     }
-
-    git::clone { 'cloud/toolforge/buildpacks':
-        ensure    => present,
-        directory => '/srv/buildpacks',
-    }
-
-    # Available in Toolforge's aptly repo
-    ensure_packages(['pack'])
 
     # Registry credentials require push privileges
     docker::credentials { '/root/.docker/config.json':
--- modules/role/manifests/wmcs/toolforge/docker/image_builder.pp
+++ modules/role/manifests/wmcs/toolforge/docker/image_builder.pp
@@ -3,7 +3,6 @@ class role::wmcs::toolforge::docker::image_builder(
     system::role { $name: }
 
     include profile::toolforge::base
-    include profile::toolforge::apt_pinning
     include profile::toolforge::infrastructure
     include profile::toolforge::docker::image_builder
 }
