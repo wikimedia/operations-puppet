10724fa66e66ca47029c622ce9a3391b4ccaa3c1
Bking <bking@wikimedia.org>
2024-02-26 20:18:26 +0000

    Revert "wdqs: add blackbox check for query.wikidata.org"
    This reverts commit ac3a7d19206ed46fb0bb93a4165bac99ecf7f5d4.
    
    Reason for revert: Associated with each instance instead of just a single alert
    
    Change-Id: Ib44113b2f48581240d82d445c3f48ee8c4bda24a

--- modules/profile/manifests/query_service/monitor/wikidata.pp
+++ modules/profile/manifests/query_service/monitor/wikidata.pp
@@ -6,31 +6,11 @@ class profile::query_service::monitor::wikidata {
         nrpe_command => '/usr/lib/nagios/plugins/check_http -H 127.0.0.1 -p 80 -w 10 -u /readiness-probe',
         notes_url    => 'https://wikitech.wikimedia.org/wiki/Wikidata_query_service',
     }
+
     monitoring::service { 'WDQS_External_SPARQL_Endpoint':
         description   => 'WDQS SPARQL',
         check_command => 'check_https_url_for_string!query.wikidata.org!/bigdata/namespace/wdq/sparql?query=SELECT%20*%20WHERE%20%7Bwikibase%3ADump%20schema%3AdateModified%20%3Fy%7D%20LIMIT%201!http://www.w3.org/2001/XMLSchema#dateTime',
         notes_url     => 'https://wikitech.wikimedia.org/wiki/Wikidata_query_service/Runbook',
     }
-    prometheus::blackbox::check::http { 'wdqs_external_sparql_endpoint_sre':
-        server_name        => 'query.wikidata.org',
-        team               => 'data-platform',
-        severity           => 'critical',
-        path               => '/bigdata/namespace/wdq/sparql?query=SELECT%20*%20WHERE%20%7Bwikibase%3ADump%20schema%3AdateModified%20%3Fy%7D%20LIMIT%201',
-        body_regex_matches => ['http://www.w3.org/2001/XMLSchema#dateTime'],
-        force_tls          => true,
-        port               => 443,
-        req_headers        => {'Accept' => '*/*', 'User-Agent' => 'prometheus-sparql-ep-check' },
-    }
-    prometheus::blackbox::check::http { 'wdqs_external_sparql_endpoint_search':
-        server_name        => 'query.wikidata.org',
-        team               => 'search-platform',
-        severity           => 'critical',
-        path               => '/bigdata/namespace/wdq/sparql?query=SELECT%20*%20WHERE%20%7Bwikibase%3ADump%20schema%3AdateModified%20%3Fy%7D%20LIMIT%201',
-        body_regex_matches => ['http://www.w3.org/2001/XMLSchema#dateTime'],
-        force_tls          => true,
-        port               => 443,
-        req_headers        => {'Accept' => '*/*', 'User-Agent' => 'prometheus-sparql-ep-check' },
-    }
 }
-
 
