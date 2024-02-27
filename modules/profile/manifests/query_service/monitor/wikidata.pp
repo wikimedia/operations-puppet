7a992438e49ead70785eac1124d03eb4d9d62141
Brian King <bking@wikimedia.org>
2024-02-27 14:07:31 -0600

    wdqs: Add blackbox http checks for SPARQL endpoint
    We're working on migrating icinga alerts to
    Prometheus. Add the prometheus blackbox checks
    as the first step in this process.
    
    Hosts: prometheus1006.eqiad.wmnet
    Hosts: wdqs1015.eqiad.wmnet,wdqs2012.codfw.wmnet
    Bug: T358029
    Change-Id: I16b731b2e5eb1305a27d690e26e9b0ec90ec3ebb

--- modules/profile/manifests/query_service/monitor/wikidata.pp
+++ modules/profile/manifests/query_service/monitor/wikidata.pp
@@ -12,5 +12,31 @@ class profile::query_service::monitor::wikidata {
         check_command => 'check_https_url_for_string!query.wikidata.org!/bigdata/namespace/wdq/sparql?query=SELECT%20*%20WHERE%20%7Bwikibase%3ADump%20schema%3AdateModified%20%3Fy%7D%20LIMIT%201!http://www.w3.org/2001/XMLSchema#dateTime',
         notes_url     => 'https://wikitech.wikimedia.org/wiki/Wikidata_query_service/Runbook',
     }
-}
 
+    prometheus::blackbox::check::http { 'wdqs_external_sparql_endpoint_sre':
+        server_name        => 'query.wikidata.org',
+        instance_label     => $facts['hostname'],
+        team               => 'data-platform',
+        severity           => 'info',
+        path               => '/bigdata/namespace/wdq/sparql?query=SELECT%20*%20WHERE%20%7Bwikibase%3ADump%20schema%3AdateModified%20%3Fy%7D%20LIMIT%201',
+        body_regex_matches => ['^http:\/\/www\.w3\.org\/2001\/XMLSchema#dateTime$'],
+        force_tls          => true,
+        port               => 443,
+        req_headers        => { 'Accept' => '*/*', 'User-Agent' => 'prometheus-sparql-ep-check' },
+        ip4                => $facts['ipaddress'],
+        ip6                => $facts['ipaddress6'],
+    }
+    prometheus::blackbox::check::http { 'wdqs_external_sparql_endpoint_search':
+        server_name        => 'query.wikidata.org',
+        instance_label     => $facts['hostname'],
+        team               => 'search-platform',
+        severity           => 'info',
+        path               => '/bigdata/namespace/wdq/sparql?query=SELECT%20*%20WHERE%20%7Bwikibase%3ADump%20schema%3AdateModified%20%3Fy%7D%20LIMIT%201',
+        body_regex_matches => ['^http:\/\/www\.w3\.org\/2001\/XMLSchema#dateTime$'],
+        force_tls          => true,
+        port               => 443,
+        req_headers        => { 'Accept' => '*/*', 'User-Agent' => 'prometheus-sparql-ep-check' },
+        ip4                => $facts['ipaddress'],
+        ip6                => $facts['ipaddress6'],
+    }
+}
