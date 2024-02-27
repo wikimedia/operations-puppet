987b3eec4d5ff1d9f41507dd7330b46b7dd3ba62
Daniel Zahn <dzahn@wikimedia.org>
2024-02-27 10:18:34 -0800

    phabricator: setup scap bin link in migration class
    Sets up a symlink to the scap binary and the directory to
    link to before the first deploy.
    
    Also regardless if scap has already installed
    itself with "scap install-world" from a deployment server.
    
    To avoid a bootstrap issue when reimaging a phab/phorge server
    where scap install fails otherwise.
    
    Bug: T357572
    Change-Id: I3d674272668f549963aa83432a6b36b8b2cf5333

--- modules/profile/manifests/phabricator/migration.pp
+++ modules/profile/manifests/phabricator/migration.pp
@@ -6,7 +6,24 @@ class profile::phabricator::migration (
     Array[Stdlib::Fqdn] $dst_hosts = lookup('profile::phabricator::migration::dst_hosts'),
 ) {
 
+    # setup scap user and symlink to binary before the first deploy and
+    # before 'scap install-world' has installed scap itself (T357572)
+
+    $scap_path = '/var/lib/scap/scap/bin'
+
     class { '::scap::user': }
+
+    wmflib::dir::mkdir_p($scap_path, {
+        owner   => 'scap',
+        require => Class['scap::user'],
+    })
+
+    file { '/usr/bin/scap':
+        ensure  => 'link',
+        target  => "${scap_path}/scap",
+        require => File[$scap_path],
+    }
+
     class { '::phabricator::phd::user': }
 
     if $facts['fqdn'] in $dst_hosts {
