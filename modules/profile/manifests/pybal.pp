c98da11bc9fad092d36d795cae650f631eb73507
Cathal Mooney <cmooney@wikimedia.org>
2024-02-29 18:11:20 +0000

    Move lvs2012 from private1-b-codfw to private1-b2-codfw vlan
    This patch needs to be merged when we reimage lvs2012 and change its
    vlan from private1-b-codfw to private1-b2-codfw.
    
    The change adjusts the BGP peer the host will connect to from the
    core routers to lsw1-b2-codfw, and adds a new vlan sub-interface to
    the primary port so it can still connect to realservers on the old
    vlan.
    
    Bug: T352918
    Change-Id: I1ed8a98f2a6bfb984d5a5203f043a43ef80c7e8c

--- modules/profile/manifests/pybal.pp
+++ modules/profile/manifests/pybal.pp
@@ -27,7 +27,8 @@ class profile::pybal(
         'bgp' => $bgp,
         'bgp-peer-address' => $::hostname ? {
             /^lvs10[0-9][0-9]$/ => "[ '208.80.154.196', '208.80.154.197' ]", # cr1-eqiad,cr2-eqiad
-            /^lvs20[0-9][0-9]$/ => "[ '208.80.153.192', '208.80.153.193' ]", # cr1-codfw,cr2-codfw
+            /^lvs201[1|3-4]$/   => "[ '208.80.153.192', '208.80.153.193' ]", # cr1-codfw,cr2-codfw
+            /^lvs2012$/         => "[ '10.192.11.1' ]", # lsw1-b2-codfw gateway
             /^lvs30(08|10)$/    => "[ '10.80.0.1' ]", # asw1-bw27-esams gateway
             /^lvs3009$/         => "[ '10.80.1.1' ]", # asw1-by27-esams gateway
             /^lvs40[0-9][0-9]$/ => "[ '198.35.26.192', '198.35.26.193' ]",   # cr3-ulsfo,cr4-ulsfo
