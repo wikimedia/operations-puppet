f71285d4f21ae6188894d33d6dbce404173574b8
Andrew Bogott <abogott@wikimedia.org>
2024-03-05 09:23:57 -0600

    profile::puppetserver::wmcs: parametrize a few hiera settings
    Change-Id: Ib269da7ee65cc089c9e8331302b711402b4df251

--- modules/profile/manifests/puppetserver/wmcs.pp
+++ modules/profile/manifests/puppetserver/wmcs.pp
@@ -1,11 +1,14 @@
 # SPDX-License-Identifier: Apache-2.0
-class profile::puppetserver::wmcs {
+class profile::puppetserver::wmcs (
+    Stdlib::Unixpath $enc_path    = lookup('profile::openstack::base::puppetmaster::enc_client::enc_path'),
+    Stdlib::Unixpath $git_basedir = lookup('profile::puppetserver::git::basedir'),
+){
     include profile::openstack::base::puppetmaster::enc_client
     class { 'profile::puppetserver':
-        enc_path => $profile::openstack::base::puppetmaster::enc_client::enc_path
+        enc_path => $enc_path,
     }
     class { 'puppetmaster::gitsync':
-        base_dir => $profile::puppetserver::git::basedir,
+        base_dir => $git_basedir,
         # TODO: make git_user a param to puppetmaster::gitpuppet and use that here
         git_user => 'gitpuppet',
     }
