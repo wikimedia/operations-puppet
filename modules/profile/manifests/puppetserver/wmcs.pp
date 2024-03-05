a3cc0f966a453f2d748143aef01cd8eaac43bd13
Andrew Bogott <abogott@wikimedia.org>
2024-03-05 14:48:59 -0600

    puppetserver::wmcs: pull enc_path out of the enc class
    This is a partial revert of an earlier patch where I made this
    a parameter; it turns out that it was never in hiera so this
    moves us back to a single source of truth.
    
    Change-Id: I9eed067deac936f642b12bd6b38774ce6b0e11f7

--- modules/profile/manifests/puppetserver/wmcs.pp
+++ modules/profile/manifests/puppetserver/wmcs.pp
@@ -1,11 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::puppetserver::wmcs (
-    Stdlib::Unixpath $enc_path    = lookup('profile::openstack::base::puppetmaster::enc_client::enc_path'),
     Stdlib::Unixpath $git_basedir = lookup('profile::puppetserver::git::basedir'),
 ){
     include profile::openstack::base::puppetmaster::enc_client
     class { 'profile::puppetserver':
-        enc_path => $enc_path,
+        enc_path => $profile::openstack::base::puppetmaster::enc_client::enc_path,
     }
     class { 'puppetmaster::gitsync':
         base_dir => $git_basedir,
