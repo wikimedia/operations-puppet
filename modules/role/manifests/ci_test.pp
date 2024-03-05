71948a0ada8614fbb51389c0de4f8c7c3a81d5dc
Daniel Zahn <dzahn@wikimedia.org>
2024-03-04 17:20:55 -0800

    ci_test: include profile::firewall in test role
    puppet compiler shows this is the reason we have the firewall::service
    but without a rule being realized.
    
    We need to include profile::firewall which isn't pulled by scap::ferm or
    profile::base:;production alone.
    
    Bug: T358237
    Change-Id: Ia1ebfe4beda4242b890c4cb8adbf398eaec16c20

--- modules/role/manifests/ci_test.pp
+++ modules/role/manifests/ci_test.pp
@@ -6,7 +6,7 @@ class role::ci_test {
     system::role { 'ci_test': description => 'CI test server' }
 
     include ::profile::base::production
-
+    include ::profile::firewall
     include ::profile::zuul::merger
     include ::profile::zuul::server
     include ::profile::ci::httpd
