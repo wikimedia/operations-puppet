125e992adb8adda3c51c303964db2ba5ac195ed3
Taavi Väänänen <hi@taavi.wtf>
2024-03-07 13:55:23 +0200

    openvswitch: use package resource for ordering
    Change-Id: I897ec218907096b59d1e3813f10bc14e162935bc

--- modules/openvswitch/manifests/init.pp
+++ modules/openvswitch/manifests/init.pp
@@ -1,7 +1,9 @@
 # @summary manages an openvswitch switch
 # SPDX-License-Identifier: Apache-2.0
 class openvswitch () {
-  ensure_packages(['openvswitch-switch'])
+  package {'openvswitch-switch':
+    ensure => installed,
+  }
 
   service { 'openvswitch-switch':
     ensure => running,
