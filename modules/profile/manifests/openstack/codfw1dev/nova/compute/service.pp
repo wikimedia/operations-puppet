8a55ff05f82e6da4dad8c9544e6dab280de3f3f8
Taavi Väänänen <hi@taavi.wtf>
2024-03-07 14:18:43 +0200

    P:openstack: nova: convert cloudvirt2001-dev to OVS agent
    Bug: T358761
    Change-Id: I507ca012236c466d9457d7a22d70e6a98c22f3cf

--- modules/profile/manifests/openstack/codfw1dev/nova/compute/service.pp
+++ modules/profile/manifests/openstack/codfw1dev/nova/compute/service.pp
@@ -7,27 +7,35 @@ class profile::openstack::codfw1dev::nova::compute::service(
     $network_flat_name = lookup('profile::openstack::codfw1dev::neutron::network_flat_name'),
     $physical_interface_mappings = lookup('profile::openstack::codfw1dev::nova::physical_interface_mappings'),
     String $libvirt_cpu_model = lookup('profile::openstack::codfw1dev::nova::libvirt_cpu_model'),
-    ) {
+    Boolean $use_ovs = lookup('profile::openstack::codfw1dev::neutron::use_ovs', {default_value => false}),
+) {
 
     require ::profile::openstack::codfw1dev::neutron::common
-    class {'::profile::openstack::base::neutron::linuxbridge_agent':
-        version                     => $version,
-        physical_interface_mappings => $physical_interface_mappings,
+    if $use_ovs {
+        class { 'profile::openstack::base::neutron::ovs_agent':
+            version => $version,
+            before  => Class['profile::openstack::base::nova::compute::service'],
+        }
+    } else {
+        class {'::profile::openstack::base::neutron::linuxbridge_agent':
+            version                     => $version,
+            physical_interface_mappings => $physical_interface_mappings,
+            before                      => Class['profile::openstack::base::nova::compute::service'],
+        }
+        contain '::profile::openstack::base::neutron::linuxbridge_agent'
     }
-    contain '::profile::openstack::base::neutron::linuxbridge_agent'
 
     require ::profile::openstack::codfw1dev::nova::common
     $all_cloudvirts = unique(
         wmflib::class::hosts('profile::openstack::codfw1dev::nova::compute::service') << $facts['networking']['fqdn']
     ).sort
-    class {'::profile::openstack::base::nova::compute::service':
+    class { 'profile::openstack::base::nova::compute::service':
         version                            => $version,
         network_flat_interface             => $network_flat_interface,
         network_flat_tagged_base_interface => $network_flat_tagged_base_interface,
         network_flat_interface_vlan        => $network_flat_interface_vlan,
         all_cloudvirts                     => $all_cloudvirts,
         libvirt_cpu_model                  => $libvirt_cpu_model,
-        require                            => Class['::profile::openstack::base::neutron::linuxbridge_agent'],
     }
     contain '::profile::openstack::base::nova::compute::service'
 
