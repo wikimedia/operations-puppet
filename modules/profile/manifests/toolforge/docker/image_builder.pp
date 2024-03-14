4fc14da88646cc0a3b40e3da28027065e3b26015
Taavi Väänänen <hi@taavi.wtf>
2024-03-14 17:18:43 +0200

    P:toolforge::docker::image_builder: drop buster support
    Bug: T358483
    Change-Id: Ia3614d12f6956389e401172a285ac031c7bfabd3

--- modules/profile/manifests/toolforge/docker/image_builder.pp
+++ modules/profile/manifests/toolforge/docker/image_builder.pp
@@ -4,47 +4,20 @@ class profile::toolforge::docker::image_builder(
     Stdlib::Fqdn $docker_registry = lookup('docker::registry'),
     String       $component       = lookup('profile::wmcs::kubeadm::component'),
 ) {
-    if debian::codename::eq('buster') {
-        # This should be building with the same docker we are running
-        class { '::kubeadm::repo':
-            component => $component,
-        }
+    include profile::docker::engine
+    cinderutils::ensure { 'separate-docker':
+        min_gb        => 10,
+        mount_point   => '/var/lib/docker',
+        mount_mode    => '711',
+        mount_options => 'discard,defaults',
+        notify        => Service['docker'],
+    }
 
-        labs_lvm::volume { 'docker':
-            size      => '70%FREE',
-            mountat   => '/var/lib/docker',
-            mountmode => '711',
-        } -> class {'profile::labs::lvm::srv': }
-
-        class { '::kubeadm::docker': }
-
-        class { '::docker::baseimages':
-            docker_registry => $docker_registry,
-        }
-
-        git::clone { 'cloud/toolforge/buildpacks':
-            ensure    => present,
-            directory => '/srv/buildpacks',
-        }
-
-        # Available in Toolforge's aptly repo
-        ensure_packages(['pack'])
-    } else {
-        include profile::docker::engine
-        cinderutils::ensure { 'separate-docker':
-            min_gb        => 10,
-            mount_point   => '/var/lib/docker',
-            mount_mode    => '711',
-            mount_options => 'discard,defaults',
-            notify        => Service['docker'],
-        }
-
-        file { '/srv/images':
-            ensure => directory,
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0755',
-        }
+    file { '/srv/images':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
     }
 
     git::clone { 'operations/docker-images/toollabs-images':
