b25502577559a5176d39ba7a082f9505dad63e3e
Andre Klapper <a9016009@gmx.de>
2024-03-18 11:00:14 +0100

    phabricator: MFA status check: Exclude bot accounts
    Also offer a direct task creation link for convenience.
    
    Change-Id: I7e1e259cd2a52c02550d01c053850b3447cba63b

--- modules/phabricator/files/mfa_check.sh
+++ modules/phabricator/files/mfa_check.sh
@@ -15,6 +15,7 @@ SELECT DISTINCT(CONCAT("https://phabricator.wikimedia.org/p/", u.userName)) AS u
     AND (e.type=14 OR e.type=60))
     OR u.isAdmin = 1)
   AND u.isDisabled = 0
+  AND u.isSystemAgent = 0
   AND u.phid NOT IN
     (SELECT mfa.userPHID FROM phabricator_auth.auth_factorconfig mfa);
 END
@@ -56,17 +57,23 @@ This is your automatic weekly Phabricator mail
 listing elevated accounts and their 2FA status.
   * https://phabricator.wikimedia.org/people/query/advanced/
 
-PHAB ADMINS AND SECURITY:
+NON-BOT PHAB ADMINS AND SECURITY:
   * Security: https://phabricator.wikimedia.org/project/members/30/
   * Admins: https://phabricator.wikimedia.org/people/query/WETApwetxsAM/#R
+
 ${result_admins_and_security}
+
+For an account listed, file a ticket and assign it to the user account:
+https://phabricator.wikimedia.org/maniphest/task/edit/form/73/?title=Enable%202FA%20for%20your%20Phabricator%20account%20with%20access%20to%20Security%20tasks%20or%20risk%20losing%20access&description=Hi%2C%20currently%20your%20Phab%20account%20does%20not%20have%202FA%20enabled.%20As%20you%20have%20access%20to%20security%20issues%20%5Bwhich%20requires%202FA%5D(https%3A%2F%2Fwww.mediawiki.org%2Fwiki%2FWikimedia_Security_Team%2FPolicy%2FAccess_To_Security_Issues)%2C%20could%20you%20please%20enable%20it%20at%20https%3A%2F%2Fphabricator.wikimedia.org%2Fsettings%2Fpanel%2Fmultifactor%2F%20and%20then%20set%20the%20status%20of%20this%20task%20to%20%60resolved%60%3F%20Thanks%20a%20lot!&projects=phabricator%2Csecurity&priority=high
 
 ACL*STEWARDS MEMBERS:
   * https://phabricator.wikimedia.org/project/members/2849/
+
 ${result_acl_stewards}
 
 WMF-NDA MEMBERS:
   * https://phabricator.wikimedia.org/project/members/61/
+
 ${result_wmf_nda}
 
 Yours sincerely,
