3abc7657edeeb49dca4bc72c2df025fe091ecee3
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-03-20 09:08:50 +0100

    Add a stub role to build Apereo CAS
    While we want to run CAS 6.6 on Bookworm we can't build in on Bookworm:
    The CAS 6.6 build isn't compatible with Java 17 yet.
    
    As such, split out the build layer to a separate stub role.
    
    Bug: T357748
    Change-Id: Ic0ffd0ca2822fa48df8d712a9584738670530fd7

--- modules/role/manifests/idp_build.pp
+++ modules/role/manifests/idp_build.pp
@@ -1,10 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
-# == Class: role::idp
-# An identity provider using Apereo CAS
-class role::idp_test {
+# == Class: role::idp_build
+# A stub role to build Apereo CAS
+class role::idp_build {
     include profile::base::production
     include profile::firewall
-    include profile::idp
     include profile::idp::build
-    include profile::java
 }
