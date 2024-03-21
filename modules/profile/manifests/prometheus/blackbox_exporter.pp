124e36631fe49f1d40de99aa94afda28d1d92436
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-03-21 15:53:10 +0100

    Revert "Remove unused profile"
    This reverts commit aafe842ec3a60a0c720ab9bda1d414aef869b83c. Appears
    to be still used in cloud...
    
    Change-Id: I24a478f3dc3907ca8dc0be72354f7d529a3c4e0d

---
+++ modules/profile/manifests/prometheus/blackbox_exporter.pp
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+# profile to provision prometheus blackbox / active checks exporter. See
+# https://github.com/prometheus/blackbox_exporter and the module's documentation.
+
+class profile::prometheus::blackbox_exporter {
+    class { 'prometheus::blackbox_exporter': }
+
+    ferm::service { 'prometheus-blackbox-exporter':
+        proto  => 'tcp',
+        port   => '9115',
+        srange => '$DOMAIN_NETWORKS',
+    }
+}
