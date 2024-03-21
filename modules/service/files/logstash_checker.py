wikimedia/operations-puppet

commit a73c9bf30558e5c327cea788d0970d198f83a1ab

logstash_checker.py: Provide more info on error
If the logstash server returns a response with the 'error' key, raise
an error which contains the URL, the error response, and the query
that was issued.

Sample output:

ValueError: Logstash request to logstash1009.eqiad.wmnet:9200/logstash-*/_search returned error:
{'error': {'root_cause': [{'type': 'node_not_connected_exception', 'reason':
'[logstash1012-production-logstash-eqiad][10.64.48.177:9300] Node not connected'}, {'type':
'node_not_connected_exception', 'reason':
'[logstash1011-production-logstash-eqiad][10.64.16.30:9300] Node not connected'}], 'type':
'search_phase_execution_exception', 'reason': 'all shards failed', 'phase': 'can_match', 'grouped':
True, 'failed_shards': [{'shard': 0, 'index': 'logstash-2021.01.07', 'node':
'sPSyVRCgRoivaZPkWIvEDA', 'reason': {'type': 'node_not_connected_exception', 'reason':
'[logstash1012-production-logstash-eqiad][10.64.48.177:9300] Node not connected'}}, {'shard': 0,
'index': 'logstash-2021.01.08', 'node': '2Yn79l3FQtW_CG28GYp0gw', 'reason': {'type':
'node_not_connected_exception', 'reason':
'[logstash1011-production-logstash-eqiad][10.64.16.30:9300] Node not connected'}}]}, 'status': 500}

Query was: {'size': 0, 'aggs': {'2': {'date_histogram': {'interval': '10s', 'field':
'@timestamp'}}}, 'query': {'bool': {'filter': [{'range': {'@timestamp': {'lte': 'now', 'gte':
'now-60m'}}}, {'query_string': {'query': 'host:("mw1279") AND type:mediawiki AND channel:(exception
OR error)'}}], 'must_not': [{'terms': {'level': ['DEBUG']}}]}}}

Change-Id: Ic9412622ce3332bf9da0817f3cc6dcd0eb1700d3

modules/service/files/logstash_checker.py
@@ -209,6 +209,7 @@ class CheckService(object):
         headers['content-type'] = "application/json"
         logstash_search_url = os.path.join(self.logstash_host,
                                            'logstash-*', '_search')
+        query_object = self._logstash_query()
         try:
             response = fetch_url(
                 http,
@@ -216,12 +217,23 @@ class CheckService(object):
                 timeout=10,
                 headers=headers,
                 method='POST',
-                body=self._logstash_query()
+                body=query_object
             )
             resp = response.data.decode('utf-8')
             r = json.loads(resp)
         except ValueError:
+            # FIXME: discards the error object
             raise ValueError("Logstash request returned error")
+
+        if type(r) is not dict:
+            raise ValueError(
+                "Unexpected response from %s. Expected a dict but got: %s\n\nQuery was: %s"
+                % (logstash_search_url, json.dumps(r), json.dumps(query_object)))
+
+        if r.get('error', None):
+            raise ValueError(
+                "Logstash request to %s returned error:\n%s\n\nQuery was: %s"
+                % (logstash_search_url, r, json.dumps(query_object())))
 
         self.logger.debug('logstash response %s', r)
 

commit 85f3f82d41a3b32087e6a90367ba11b7db016c4d

logstash_checker.py: Fix error reporting bug
Remove a stray `()` that was causing a cascading error.
Fixes bug introduced in a73c9bf30558e5c327cea788d0970d198f83a1ab

Change-Id: I5b1bc83432b3a6e3099674b04afcde31a6ca682a

modules/service/files/logstash_checker.py
@@ -283,7 +283,7 @@ class CheckService(object):
         if r.get('error', None):
             raise ValueError(
                 "Logstash request to %s returned error:\n%s\n\nQuery was: %s"
-                % (logstash_search_url, r, json.dumps(query_object())))
+                % (logstash_search_url, r, json.dumps(query_object)))
 
         # Calculate mean event rates before / after the deploy.
 