d345e28020e40b7ae067e40a25c340f3bb7d2a87
Cormac Parle <cparle@wikimedia.org>
2024-03-21 15:11:53 +0000

    MachineVision is being sunsetted, so remove job
    Bug: T352884
    Change-Id: I95fab9432d0a3958d50dc226460dd11eaffd84bd

--- modules/profile/manifests/mediawiki/maintenance/computer_aided_tagging.pp
+++
@@ -1,6 +0,0 @@
-class profile::mediawiki::maintenance::computer_aided_tagging {
-  profile::mediawiki::periodic_job { 'MachineVision_prioritize_uncategorized':
-    command  => '/usr/local/bin/mwscript extensions/MachineVision/maintenance/prioritizeFilesWithTemplate.php --wiki=commonswiki --template=Uncategorized --priority=10',
-    interval => 'Sun 00:00',
-  }
-}
