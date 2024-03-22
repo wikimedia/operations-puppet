8cb11b049386d62ef23cc23b7bb904d4d5a320e4
Taavi Väänänen <hi@taavi.wtf>
2024-03-22 13:09:00 +0200

    P:toolforge::legacy_redirector: Use Apache on Bookworm
    This will let us have a single role for tools.wmflabs.org and
    toolserver.org. Toolserver is currently using Apache, and I made an
    arbitrary decision to convert this to Apache instead of the other way
    around.
    
    Bug: T311909
    Change-Id: I430866f87f585f5478806358eadd722e5b0ee6b1

--- modules/profile/manifests/toolforge/legacy_redirector.pp
+++ modules/profile/manifests/toolforge/legacy_redirector.pp
@@ -1,36 +1,57 @@
 class profile::toolforge::legacy_redirector (
     Optional[String[1]] $ssl_certificate_name = lookup('profile::toolforge::legacy_redirector::ssl_certificate_name', {default_value => 'tools-legacy'}),
 ) {
-    $resolver = join($::nameservers, ' ')
-
-    # toolsbeta support: running without SSL as in the main front proxy
-    if $ssl_certificate_name {
+    if debian::codename::eq('buster') {
         $ssl_settings = ssl_ciphersuite('nginx', 'compat')
-        # SSL certificate for tools.wmflabs.org
-        acme_chief::cert { $ssl_certificate_name:
-            puppet_rsc => Exec['nginx-reload'],
+        $resolver = join($::nameservers, ' ')
+        if $ssl_certificate_name {
+            # SSL certificate for tools.wmflabs.org
+            acme_chief::cert { $ssl_certificate_name:
+                puppet_rsc => Exec['nginx-reload'],
+            }
+            class { '::sslcert::dhparam': } # deploys /etc/ssl/dhparam.pem, required by nginx
         }
-        class { '::sslcert::dhparam': } # deploys /etc/ssl/dhparam.pem, required by nginx
-    }
 
-    class { '::nginx':
-        variant => 'extras',
-    }
+        class { '::nginx':
+            variant => 'extras',
+        }
 
-    nginx::site { 'legacy-redirector':
-        content => template('profile/toolforge/legacy-redirector.conf.erb'),
-    }
+        nginx::site { 'legacy-redirector':
+            content => template('profile/toolforge/legacy-redirector.conf.erb'),
+        }
 
-    file { '/etc/nginx/lua':
-        ensure  => 'directory',
-        require => Package['nginx-extras'],
-    }
+        file { '/etc/nginx/lua':
+            ensure  => 'directory',
+            require => Package['nginx-extras'],
+        }
 
-    file { '/etc/nginx/lua/legacy_redirector.lua':
-        ensure  => file,
-        source  => 'puppet:///modules/profile/toolforge/legacy_redirector.lua',
-        require => File['/etc/nginx/lua'],
-        notify  => Service['nginx'],
+        file { '/etc/nginx/lua/legacy_redirector.lua':
+            ensure  => file,
+            source  => 'puppet:///modules/profile/toolforge/legacy_redirector.lua',
+            require => File['/etc/nginx/lua'],
+            notify  => Service['nginx'],
+        }
+    } else {
+        $ssl_settings = ssl_ciphersuite('apache', 'compat')
+        if $ssl_certificate_name {
+            acme_chief::cert { $ssl_certificate_name:
+                puppet_svc => 'apache2',
+            }
+        }
+
+        class { 'httpd':
+            modules => ['alias', 'rewrite', 'ssl'],
+        }
+
+        if $ssl_certificate_name {
+            acme_chief::cert { $ssl_certificate_name:
+                puppet_svc => 'apache2',
+            }
+        }
+
+        httpd::site { 'tools.wmflabs.org':
+            content => template('profile/toolforge/legacy_redirector/tools.wmflabs.org.conf.erb'),
+        }
     }
 
     ferm::service { 'http':
