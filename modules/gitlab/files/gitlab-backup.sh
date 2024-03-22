e7f21271e7b562980ec0e7f3492cfc9d14fdc3ef
Eoghan Gaffney <egaffney@wikimedia.org>
2024-03-22 17:27:44 +0000

    [gitlab] Move backup script locking out of main script root
    Currently we place a lockfile on any run of the script, however some
    operations (notably the help function, and the unlock function) won't
    run if another instance is running.
    
    The other option is to check `backup_type` before we place a lockfile,
    but this seems like a neater/cleaner option, even if it's a little more
    verbose.
    
    Bug: T358559
    Change-Id: I8a2df9cee45f2ac7a372d8336d6d8ff05337c5d1

--- modules/gitlab/files/gitlab-backup.sh
+++ modules/gitlab/files/gitlab-backup.sh
@@ -26,7 +26,6 @@
 
 backup_type=$1
 
-lock_backups
 
 backup_timer_wrapper() {
     start_time=$(date +%s)
@@ -40,17 +39,22 @@ backup_timer_wrapper() {
 
 case "${backup_type}" in
     "full")
+    lock_backups
     backup_timer_wrapper create CRON=1 STRATEGY=copy GZIP_RSYNCABLE="${RSYNCABLE_GZIP}" SKIP=builds,artifacts,registry GITLAB_BACKUP_MAX_CONCURRENCY="${MAX_CONCURRENCY}" GITLAB_BACKUP_MAX_STORAGE_CONCURRENCY="${MAX_STORAGE_CONCURRENCY}";;
     "partial")
+    lock_backups
     backup_timer_wrapper create BACKUP=partial CRON=1 STRATEGY=copy GZIP_RSYNCABLE="${RSYNCABLE_GZIP}" SKIP=packages GITLAB_BACKUP_MAX_CONCURRENCY="${MAX_CONCURRENCY}" GITLAB_BACKUP_MAX_STORAGE_CONCURRENCY="${MAX_STORAGE_CONCURRENCY}";;
     "config")
+    lock_backups
     /usr/bin/gitlab-ctl backup-etc;;
     "failover")
+    lock_backups
     # full backup for failover, including builds, artifacts and registry and without copy strategy
     backup_timer_wrapper create BACKUP=failover CRON=1 GZIP_RSYNCABLE="${RSYNCABLE_GZIP}" GITLAB_BACKUP_MAX_CONCURRENCY="${MAX_CONCURRENCY}" GITLAB_BACKUP_MAX_STORAGE_CONCURRENCY="${MAX_STORAGE_CONCURRENCY}";;
     "lock")
     # This is intended for use in cookbooks and other situations where we want to create a lock file without running a backup.
     # Since the lockfile has already been created we just need to un-trap the signals so we can exit without removing the lock
+    lock_backups
     trap - SIGINT SIGHUP SIGABRT EXIT;;
     "unlock")
     unlock_backups;;
