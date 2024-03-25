8deb62a9a83abc2ba5ef974837cba9b56beaa00e
Taavi Väänänen <hi@taavi.wtf>
2024-03-25 16:29:44 +0200

    P:toolforge::legacy_redirector: drop buster support
    Bug: T311909
    Change-Id: I5d7e21fbca363b337681d6a322a195ff5fee2e33

--- modules/profile/manifests/toolforge/legacy_redirector.pp
+++ modules/profile/manifests/toolforge/legacy_redirector.pp
@@ -1,51 +1,19 @@
 class profile::toolforge::legacy_redirector (
     Optional[String[1]] $ssl_certificate_name = lookup('profile::toolforge::legacy_redirector::ssl_certificate_name', {default_value => 'tools-legacy'}),
 ) {
-    if debian::codename::eq('buster') {
-        $ssl_settings = ssl_ciphersuite('nginx', 'compat')
-        $resolver = join($::nameservers, ' ')
-        if $ssl_certificate_name {
-            # SSL certificate for tools.wmflabs.org
-            acme_chief::cert { $ssl_certificate_name:
-                puppet_rsc => Exec['nginx-reload'],
-            }
-            class { '::sslcert::dhparam': } # deploys /etc/ssl/dhparam.pem, required by nginx
+    $ssl_settings = ssl_ciphersuite('apache', 'compat')
+    if $ssl_certificate_name {
+        acme_chief::cert { $ssl_certificate_name:
+            puppet_svc => 'apache2',
         }
+    }
 
-        class { '::nginx':
-            variant => 'extras',
-        }
+    class { 'httpd':
+        modules => ['alias', 'rewrite', 'ssl'],
+    }
 
-        nginx::site { 'legacy-redirector':
-            content => template('profile/toolforge/legacy-redirector.conf.erb'),
-        }
-
-        file { '/etc/nginx/lua':
-            ensure  => 'directory',
-            require => Package['nginx-extras'],
-        }
-
-        file { '/etc/nginx/lua/legacy_redirector.lua':
-            ensure  => file,
-            source  => 'puppet:///modules/profile/toolforge/legacy_redirector.lua',
-            require => File['/etc/nginx/lua'],
-            notify  => Service['nginx'],
-        }
-    } else {
-        $ssl_settings = ssl_ciphersuite('apache', 'compat')
-        if $ssl_certificate_name {
-            acme_chief::cert { $ssl_certificate_name:
-                puppet_svc => 'apache2',
-            }
-        }
-
-        class { 'httpd':
-            modules => ['alias', 'rewrite', 'ssl'],
-        }
-
-        httpd::site { 'tools.wmflabs.org':
-            content => template('profile/toolforge/legacy_redirector/tools.wmflabs.org.conf.erb'),
-        }
+    httpd::site { 'tools.wmflabs.org':
+        content => template('profile/toolforge/legacy_redirector/tools.wmflabs.org.conf.erb'),
     }
 
     ferm::service { 'http':
