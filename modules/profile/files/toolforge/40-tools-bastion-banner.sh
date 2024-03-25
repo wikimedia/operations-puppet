c170bd9ee6b3bd6d2d5612a76773bdbaf2dd96d6
Taavi Väänänen <hi@taavi.wtf>
2024-03-25 23:19:24 +0200

    P:toolforge: drop grid shutdown from MOTD
    Change-Id: I3039b96f7b9c18342c25b28435038df8307d474c

--- modules/profile/files/toolforge/40-tools-bastion-banner.sh
+++ modules/profile/files/toolforge/40-tools-bastion-banner.sh
@@ -20,8 +20,4 @@ Code of Conduct, and Privacy Policies:
 General guidance and help can be found at:
 - [4mhttps://toolforge.org/[0m
 
-The grid engine has been turned off, now all your workloads will run on the
-kubernetes cluster, see:
-- [4mhttps://wikitech.wikimedia.org/wiki/News/Toolforge_Grid_Engine_deprecation[0m
-
 EOF
