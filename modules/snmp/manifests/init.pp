b82b2e9a059e1fb32fbf8b9330fa20eed7a24c4d
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2024-03-26 12:08:49 +0100

    snmp: instruct libsmi with snmp-mibs-downloader path
    We need to update the libsmi (and thus snmipy) path with
    snmp-mibs-downloader's own path.
    
    Bug: T359198
    Change-Id: Idcc0a3db16798cc14263c87cbd05783a6e35f00b

--- modules/snmp/manifests/init.pp
+++ modules/snmp/manifests/init.pp
@@ -8,7 +8,7 @@
 #   include ::snmp
 
 class snmp {
-    ensure_packages('snmp')
+    ensure_packages(['snmp', 'snmp-mibs-downloader'])
 
     file { '/etc/snmp/snmp.conf':
         owner  => 'root',
@@ -17,4 +17,11 @@ class snmp {
         source => 'puppet:///modules/snmp/snmp.conf',
     }
 
+    # make libsmi aware of snmp-mibs-downloader path
+    # See also https://phabricator.wikimedia.org/T359198
+    file_line { 'libsmi-mibs':
+        ensure => present,
+        path   => '/etc/smi.conf',
+        line   => 'path :/var/lib/mibs/site  # Managed by puppet',
+    }
 }
--- modules/snmp/manifests/mibs.pp
+++ modules/snmp/manifests/mibs.pp
@@ -10,10 +10,6 @@
 class snmp::mibs {
     require ::snmp
 
-    package { 'snmp-mibs-downloader':
-        ensure => present,
-    }
-
     snmp::mibs::source { 'juniper':
         source  => 'https://www.juniper.net/documentation/software/junos/junos182/juniper-mibs-18.2R1.9.tgz',
         list    => 'puppet:///modules/snmp/juniper-mibs',
