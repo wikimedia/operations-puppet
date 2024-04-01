8ad6b35a3153f76e4e54851c59b43305c1f6a2e7
Andrew Bogott <abogott@wikimedia.org>
2024-04-01 10:18:08 -0500

    profile::wmcs::kubeadm::etcd: install etcd package before referencing uid
    Previously this demanded that these files be created before
    the package was created; that doesn't make sense since the etcd
    user wouldn't exist before the package. I'm replacing that with
    a bump of the etcd service which will hopefully fix whatever race
    we were trying to fix with the broken ordering.
    
    Hosts: toolsbeta-test-k8s-etcd-21.toolsbeta.eqiad1.wikimedia.cloud
    Bug: T349207
    Change-Id: If4095d067ccc0f5ee095eaa965afd56a9c420c4d

--- modules/profile/manifests/wmcs/kubeadm/etcd.pp
+++ modules/profile/manifests/wmcs/kubeadm/etcd.pp
@@ -34,11 +34,12 @@ class profile::wmcs::kubeadm::etcd (
     }
 
     file { $etcd_cert_pub:
-        ensure => present,
-        source => "file://${puppet_cert_pub}",
-        owner  => 'etcd',
-        group  => 'etcd',
-        before => [Service['etcd'], Package['etcd-server']],
+        ensure  => present,
+        source  => "file://${puppet_cert_pub}",
+        owner   => 'etcd',
+        group   => 'etcd',
+        notify  => Service['etcd'],
+        require => Package['etcd-server'],
     }
 
     file { $etcd_cert_priv:
@@ -48,15 +49,17 @@ class profile::wmcs::kubeadm::etcd (
         group     => 'etcd',
         mode      => '0640',
         show_diff => false,
-        before    => [Service['etcd'], Package['etcd-server']],
+        notify    => Service['etcd'],
+        require   => Package['etcd-server'],
     }
 
     file { $etcd_cert_ca:
-        ensure => present,
-        source => "file://${puppet_cert_ca}",
-        owner  => 'etcd',
-        group  => 'etcd',
-        before => [Service['etcd'], Package['etcd-server']],
+        ensure  => present,
+        source  => "file://${puppet_cert_ca}",
+        owner   => 'etcd',
+        group   => 'etcd',
+        notify  => Service['etcd'],
+        require => Package['etcd-server'],
     }
 
     class { '::etcd::v3':
