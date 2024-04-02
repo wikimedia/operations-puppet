8cb39b2a6c56a36e3337abe20e01debb42ca5884
Daniel Zahn <dzahn@wikimedia.org>
2024-04-02 14:11:28 -0700

    stewards: add config and export dirs, steward onboarder config
    Create a directory for steward-onboarder users-db repo and puppetize
    the config file for it.
    
    Create another directory for exports which, unlike config and repo dirs,
    shoule be group writable.
    
    Bug: T351202
    Bug: T361547
    Change-Id: I67cad69044d0306de542dd780a94af21f38547ce

--- modules/profile/manifests/stewards.pp
+++ modules/profile/manifests/stewards.pp
@@ -6,13 +6,35 @@ class profile::stewards (
     ensure_packages(['python3-click', 'python3-requests-oauthlib'])
 
     $repo_dir = '/srv/repos'
-    wmflib::dir::mkdir_p($repo_dir)
+    $conf_dir = '/etc/steward-onboarder'
+    $export_dir = '/srv/exports'
+
+    $group_owner = 'stewards-users'
+
+    # conf dir and repo dir not writable
+    wmflib::dir::mkdir_p([$conf_dir, $repo_dir], {
+        owner => 'root',
+        group => $group_owner,
+        mode  => '0755',
+    })
+
+    # export dir group writable
+    wmflib::dir::mkdir_p($export_dir, {
+        owner => 'root',
+        group => $group_owner,
+        mode  => '0775',
+    })
 
     git::clone { 'repos/stewards/onboarding-system':
         ensure    => 'present',
         source    => 'gitlab',
-        group     => 'wikidev',
+        group     => $group_owner,
         shared    => true,
         directory => "${repo_dir}/onboarding-system",
+    }
+
+    file { "${conf_dir}/steward-onboarder.yaml":
+        ensure => 'present',
+        source => 'puppet:///modules/profile/stewards/steward-onboarder.yaml',
     }
 }
