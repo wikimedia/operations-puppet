3f2b5f0d884dc93980c0cf7b71c0b582cae7f9b0
Eric Evans <eevans@wikimedia.org>
2024-04-03 19:32:52 -0500

    cassandra-dev: surrogate user for cqlsh (dev access)
    Bug: T355730
    Hosts: cassandra-dev2001.codfw.wmnet
    Change-Id: If3f34f5e1c241b4a344645694ebf04bf2d7ee8f0

--- modules/profile/manifests/cassandra_dev.pp
+++ modules/profile/manifests/cassandra_dev.pp
@@ -1,7 +1,43 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Cassandra dev & test environment (T324113)
-class profile::cassandra_dev {
+class profile::cassandra_dev (
+    Hash[String, String] $cassandra_passwords = lookup('profile::cassandra::user_credentials', {'default_value' => {}}),
+) {
+    $devuser          = 'cassandra-devel'
+    $devpasswd        = $cassandra_passwords[$devuser]
+    $tls_cluster_name = 'cassandra-dev'
 
     class {'passwords::cassandra': }
+
+    # Surrogate user for dev team cqlsh access
+    group { $devuser:
+        ensure => present,
+        system => true,
+    }
+
+    user { $devuser:
+        ensure     => present,
+        gid        => $devuser,
+        home       => "/var/lib/${devuser}",
+        shell      => '/bin/false',
+        system     => true,
+        managehome => true,
+    }
+
+    file { "/var/lib/${devuser}/.cassandra/cqlshrc":
+        owner   => $devuser,
+        group   => $devuser,
+        mode    => '0440',
+        content => template('profile/cassandra_dev/cqlshrc.erb'),
+        require => User[$devuser],
+    }
+
+    file { "/var/lib/${devuser}/.cassandra/credentials":
+        owner   => $devuser,
+        group   => $devuser,
+        mode    => '0440',
+        content => template('profile/cassandra_dev/credentials.erb'),
+        require => User[$devuser],
+    }
 }
