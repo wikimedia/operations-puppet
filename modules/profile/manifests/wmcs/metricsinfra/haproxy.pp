f9b1a35083dfc9bfd004a3a7760cdc62102ca95f
Taavi Väänänen <hi@taavi.wtf>
2024-04-08 15:42:13 +0300

    P:wmcs::metricsinfra::haproxy: move domains to configuration
    Change-Id: I50cd1039c397a8a6db576364a8bb509c344ba479

--- modules/profile/manifests/wmcs/metricsinfra/haproxy.pp
+++ modules/profile/manifests/wmcs/metricsinfra/haproxy.pp
@@ -1,12 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::wmcs::metricsinfra::haproxy (
+    Stdlib::Fqdn        $public_domain                 = lookup('profile::wmcs::metricsinfra::public_domain', {default_value => 'wmcloud.org'}),
     Array[Stdlib::Fqdn] $prometheus_alertmanager_hosts = lookup('profile::wmcs::metricsinfra::prometheus_alertmanager_hosts'),
-    Stdlib::Fqdn        $alertmanager_active_host = lookup('profile::wmcs::metricsinfra::alertmanager_active_host'),
-    Array[Stdlib::Fqdn] $thanos_fe_hosts = lookup('profile::wmcs::metricsinfra::thanos_fe_hosts'),
-    Array[Stdlib::Fqdn] $config_manager_hosts = lookup('profile::wmcs::metricsinfra::config_manager_hosts'),
-    Array[Stdlib::Fqdn] $grafana_hosts = lookup('profile::wmcs::metricsinfra::grafana_hosts'),
+    Stdlib::Fqdn        $alertmanager_active_host      = lookup('profile::wmcs::metricsinfra::alertmanager_active_host'),
+    Array[Stdlib::Fqdn] $thanos_fe_hosts               = lookup('profile::wmcs::metricsinfra::thanos_fe_hosts'),
+    Array[Stdlib::Fqdn] $config_manager_hosts          = lookup('profile::wmcs::metricsinfra::config_manager_hosts'),
+    Array[Stdlib::Fqdn] $grafana_hosts                 = lookup('profile::wmcs::metricsinfra::grafana_hosts'),
 ) {
     class { 'haproxy::cloud::base': }
+
+    $svc_domain = "svc.${::wmcs_project}.${::wmcs_deployment}.wikimedia.cloud"
 
     file { '/etc/haproxy/conf.d/prometheus.cfg':
         owner   => 'root',
@@ -15,6 +18,8 @@ class profile::wmcs::metricsinfra::haproxy (
         content => epp(
             'profile/wmcs/metricsinfra/haproxy/prometheus.cfg.epp',
             {
+                public_domain                 => $public_domain,
+                svc_domain                    => $svc_domain,
                 prometheus_alertmanager_hosts => $prometheus_alertmanager_hosts,
                 alertmanager_active_host      => $alertmanager_active_host,
                 thanos_fe_hosts               => $thanos_fe_hosts,
