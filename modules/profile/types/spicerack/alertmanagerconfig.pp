c1c8f81249baa65c522388ea9bc5f82415ce9732
Taavi Väänänen <hi@taavi.wtf>
2024-04-09 14:29:19 +0300

    hieradata: cloudcumin: Configure metricsinfra alertmanager instance
    Bug: T360932
    Depends-On: Ie9ab841907c7e79d43f85382bd931f29c91501f4
    Change-Id: Ic32cba1e01d4f2100362918329c3b714e19df5d6

--- modules/profile/types/spicerack/alertmanagerconfig.pp
+++ modules/profile/types/spicerack/alertmanagerconfig.pp
@@ -4,7 +4,10 @@ type Profile::Spicerack::AlertmanagerConfig = Struct[{
   instances        => Hash[
     String[1],
     Struct[{
-      urls => Array[Stdlib::HTTPUrl, 1],
+      urls           => Array[Stdlib::HTTPUrl, 1],
+      http_use_proxy => Optional[Boolean],
+      http_username  => Optional[String[1]],
+      http_password  => Optional[String[1]],
     }],
   ],
   default_instance => String[1],
