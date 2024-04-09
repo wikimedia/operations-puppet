80944b2f005aa88756297f201812f289901b3f10
Ben Tullis <btullis@wikimedia.org>
2024-04-09 16:23:28 +0100

    Add a role for matomo1003
    This will be the replacement for matomo1002.
    
    Bug: T349397
    Change-Id: I247dd99319a2ff8f061e5af0e73f949df3c1aa34

--- manifests/site.pp
+++ manifests/site.pp
@@ -1892,6 +1892,11 @@ node /^matomo1002\.eqiad\./ {
     role(piwik)
 }
 
+# Bookworm replacement for matomo1002 - T349397
+node /^matomo1003\.eqiad\./ {
+    role(insetup::data_engineering)
+}
+
 node /^mc10(3[7-9]|4[0-9]|5[0-4])\.eqiad\./ {
     role(mediawiki::memcached)
 }
