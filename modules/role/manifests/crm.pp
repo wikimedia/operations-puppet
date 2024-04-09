6d7d5c055725152c39999c469749deb8376f4fc8
Dallas Wisehaupt <dwisehaupt@wikimedia.org>
2024-04-09 15:09:26 -0700

    community-crm: enable envoy
    Enables envoy to proxy ssl to port 80 for apache.
    
    Bug: T343486
    Bug: T349402
    Change-Id: Id7e9b7318faefd89f0e542b484dbdb1dc7dda947

--- modules/role/manifests/crm.pp
+++ modules/role/manifests/crm.pp
@@ -6,4 +6,9 @@ class role::crm {
     include profile::base::production
     include profile::firewall
     include profile::community_civicrm
+    include profile::prometheus::apache_exporter
+
+    if $::realm == 'production' {
+        include profile::tlsproxy::envoy
+    }
 }
