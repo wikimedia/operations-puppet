606596519f9a834342136ac7463a66ffb3be0e37
Ben Tullis <btullis@wikimedia.org>
2024-04-11 10:46:38 +0100

    Use a more WMF standard mariadb configuration for Matomo
    This will move the datadir to /srv and apply other minor changes to make
    it more suitable for the MariaDB 10.6 configuration.
    
    Hosts: matomo1003.eqiad.wmnet
    Bug: T349397
    Change-Id: I6dee55a2a10c5fc01405631e9ba9c0c9fbb5eddd

--- modules/profile/manifests/matomo/database.pp
+++ modules/profile/manifests/matomo/database.pp
@@ -5,23 +5,22 @@
 
 class profile::matomo::database (
     Stdlib::Port $database_port       = lookup('profile::matomo::database', { 'default_value' => 3306 }),
+    Stdlib::Unixpath $datadir         = lookup('profile::matomo::database::datadir', { 'default_value' => '/srv/sqldata' }),
+    Stdlib::Unixpath $tmpdir          = lookup('profile::matomo::database::tmpdir', { 'default_value' => '/srv/tmp' }),
     Array[Stdlib::Host] $backup_hosts = lookup('profile::matomo::database::backup_hosts', { 'default_value' => undef }),
 ) {
-    package { 'mysql-server':
-        ensure => absent,
-    }
-
-    $mariadb_socket = '/run/mysqld/mysqld.sock'
-
     require profile::mariadb::packages_wmf
     include profile::mariadb::wmfmariadbpy
     include profile::mariadb::monitor::prometheus
 
+    $mariadb_socket = '/run/mysqld/mysqld.sock'
+
     class { 'mariadb::config':
-        config    => 'profile/matomo/my.cnf.erb',
+        config    => 'profile/matomo/matomo.my.cnf.erb',
         socket    => $mariadb_socket,
         port      => $database_port ,
-        datadir   => '/var/lib/mysql',
+        datadir   => $datadir,
+        tmpdir    => $tmpdir,
         basedir   => $profile::mariadb::packages_wmf::basedir,
         read_only => false,
         ssl       => 'puppet-cert',
