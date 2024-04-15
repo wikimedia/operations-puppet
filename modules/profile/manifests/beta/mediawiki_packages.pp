85f94c09d39c96ec9f5ed184346299d69409b827
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-04-15 13:36:47 +0200

    beta::mediawiki_packages: Install lilypond from component
    buster-backports has been retired, use the component.
    
    Bug: T362518
    Change-Id: I1a8bef1246137025f26f6def485b5aca15035fb0

--- modules/profile/manifests/beta/mediawiki_packages.pp
+++ modules/profile/manifests/beta/mediawiki_packages.pp
@@ -5,13 +5,20 @@
 # Package list from https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/libs/Shellbox/+/refs/heads/master/.pipeline/blubber.yaml
 #
 class profile::beta::mediawiki_packages {
+
+    if debian::codename::eq('buster') {
+        apt::package_from_component { 'lilypond-buster':
+            component => 'component/lilypond',
+            packages  => ['lilypond', 'lilypond-data'],
+            priority  => 1002, # Take precedence over main
+        }
+    }
+
     ensure_packages([
         'lame', # T317128
         'djvulibre-bin',
         'libtiff-tools',
         'poppler-utils',
-        "lilypond/${::lsbdistcodename}-backports",
-        "lilypond-data/${::lsbdistcodename}-backports",
         'imagemagick',
         'ghostscript',
         'fluidsynth',
