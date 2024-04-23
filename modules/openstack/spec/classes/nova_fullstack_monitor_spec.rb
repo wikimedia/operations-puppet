755e6bb7c8542a344e0042cf0f3c26fce4a3bda2
Andrew Bogott <abogott@wikimedia.org>
2024-04-23 13:24:07 -0500

    eqiad1 openstack -> version 'bobcat'
    Bug: T356287
    Change-Id: I49dc6e3f627460826c2498cb44c2e269153a6b1d

--- modules/openstack/spec/classes/nova_fullstack_monitor_spec.rb
+++ modules/openstack/spec/classes/nova_fullstack_monitor_spec.rb
@@ -5,7 +5,7 @@ require "rspec-puppet/cache"
 describe "openstack::nova::fullstack::monitor" do
   on_supported_os(WMFConfig.test_on(10)).each do |os, facts|
     context "On #{os}" do
-      supported_openstacks = ["antelope"]
+      supported_openstacks = ["bobcat"]
       supported_openstacks.each do |openstack_version|
         context "On openstack #{openstack_version}" do
           let(:facts) { facts }
