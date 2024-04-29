069b185b17dce93873a02655ab44a3821c5506f3
Sukhbir Singh <ssingh@wikimedia.org>
2024-04-29 09:13:52 -0400

    P:cache::varnish::frontend: set esitest to absent by default
    We have not used this for a while and have no current usecase for it, so
    this should be absented by default.
    
    Bug: T308799
    Hosts: cp1100.eqiad.wmnet, cp3066.esams.wmnet, cp4037.ulsfo.wmnet
    Change-Id: I85208b7ac0c83e47167845c0890a7b744f47b919

--- modules/profile/manifests/cache/varnish/frontend/text.pp
+++ modules/profile/manifests/cache/varnish/frontend/text.pp
@@ -2,7 +2,7 @@
 # @summary profile to configure varnish frontend text nodes
 # @param esitest_ensure ensureable parameter for esitest
 class profile::cache::varnish::frontend::text (
-    Wmflib::Ensure $esitest_ensure = lookup('profile::cache::varnish::frontend::text::esitest_ensure'),
+    Wmflib::Ensure $esitest_ensure = lookup('profile::cache::varnish::frontend::text::esitest_ensure', {'default_value' => 'absent'}),
 ) {
     # for VCL compilation using libGeoIP
     class { 'geoip': }
