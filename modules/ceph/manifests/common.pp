5c601138c9a569d909b3c78dd3ea3c9caea2913d
Ben Tullis <btullis@wikimedia.org>
2024-04-29 18:30:51 +0100

    Allow the ceph-common package to create the ceph user/group
    We have previously been creating the ceph user and group prior to
    installing the ceph-common package. The result is that the system user
    and group is allocated automatically from the list of allowed uid/gid
    values. However, it may change between one installation and the next,
    which can present a problem when reimaging servers.
    
    The preferred mechanism is to allow the ceph-common package to create
    the system user and group itself and this will default to the
    pre-allocated value of 64045.
    
    See: https://github.com/ceph/ceph/blob/main/debian/ceph-common.postinst#L31
    
    This change causes the ceph group to become dependent on the
    installation of the ceph-common package, rather than the other way
    around.
    
    This will help when we want to start reimaging servers and keeping the
    contents of the /var/lib/ceph directory between reimages.
    
    For completeness, we also explicitly cause the file resources to depend
    on the creation of the user account, rather than depend on the order in
    the file.
    
    Hosts: O:ceph::server,O:wmcs::ceph::mon,O:wmcs::ceph::osd
    Bug: T362993
    Change-Id: Ic21670087cca74f7f46d22cb1b246ef6eb44b98d

--- modules/ceph/manifests/common.pp
+++ modules/ceph/manifests/common.pp
@@ -13,9 +13,15 @@ class ceph::common (
     Stdlib::Unixpath $home_dir,
     String           $ceph_repository_component,
 ) {
+    # The ceph-common package is the preferred mechanism to create the initial
+    # ceph user and group, since this will set pre-allocated static uid/gid values of 64045.
+    # See: https://salsa.debian.org/debian/base-passwd/-/blob/master/README?ref_type=heads&plain=1#L50
+    # By ensuring that the ceph group depends on ceph-common we will allow new server reimages to use
+    # the static uid/gid without changing the uid on existing ceph deployments.
     group { 'ceph':
-        ensure => present,
-        system => true,
+        ensure  => present,
+        system  => true,
+        require => Package['ceph-common'],
     }
 
     user { 'ceph':
@@ -35,35 +41,37 @@ class ceph::common (
       # fio is used for performance tests and debugging
       'fio',
     ])
-    User['ceph'] -> Package['ceph-common']
-
 
     file { '/var/lib/ceph':
-        ensure => directory,
-        mode   => '0750',
-        owner  => 'ceph',
-        group  => 'ceph',
+        ensure  => directory,
+        mode    => '0750',
+        owner   => 'ceph',
+        group   => 'ceph',
+        require => User['ceph'],
     }
 
     file { '/var/lib/ceph/bootstrap-osd':
-        ensure => directory,
-        mode   => '0750',
-        owner  => 'ceph',
-        group  => 'ceph',
+        ensure  => directory,
+        mode    => '0750',
+        owner   => 'ceph',
+        group   => 'ceph',
+        require => User['ceph'],
     }
 
     file { '/var/log/ceph':
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'ceph',
-        group  => 'ceph',
+        ensure  => directory,
+        mode    => '0755',
+        owner   => 'ceph',
+        group   => 'ceph',
+        require => User['ceph'],
     }
 
     file { '/var/run/ceph':
-        ensure => directory,
-        mode   => '0750',
-        owner  => 'ceph',
-        group  => 'ceph',
+        ensure  => directory,
+        mode    => '0750',
+        owner   => 'ceph',
+        group   => 'ceph',
+        require => User['ceph'],
     }
 
     # Make sure that the ceph component is preferred to any other backports repository
