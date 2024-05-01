e96206194844d09422521bb1d6ab358a282672e0
Eoghan Gaffney <egaffney@wikimedia.org>
2024-05-01 15:57:26 +0100

    lists: Add collaboration services as owner
    Bug: T331706
    Change-Id: Idbc268976cf74318af90ae90781813203452e456

--- modules/role/manifests/lists.pp
+++ modules/role/manifests/lists.pp
@@ -1,8 +1,5 @@
 # sets up a mailing list server
 class role::lists {
-
-    system::role { 'lists': description => 'Mailing list server', }
-
     include profile::base::production
     include profile::backup::host
     include profile::firewall
