5d124a7f8f7a5d8a252d67a5a953cf285ac1ecff
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-05-02 12:41:44 +0200

    Make ganeti7001 a Ganeti node
    Bug: T363978
    Change-Id: I1490880f4a578078b4294a70f566b603f8ea70f1

--- manifests/site.pp
+++ manifests/site.pp
@@ -1323,10 +1323,13 @@ node /^ganeti600[1234]\.drmrs\./ {
     role(ganeti)
 }
 
-node /^ganeti700[1234]\.magru\./ {
+node /^ganeti700[234]\.magru\./ {
     role(insetup::infrastructure_foundations)
 }
 
+node /^ganeti700[1]\.magru\./ {
+    role(ganeti)
+}
 
 # gitlab servers - eqiad (T274459, T301177)
 node /^gitlab1003\.wikimedia\./ {
