43a2f66d503de5122fa82d1131de94f61e1e9720
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-05-03 14:47:18 +0200

    squid_exporter: Remove some outdated comments
    These refer to the squid3->squid transition which is long done. Also drop
    root ownership annotations (these are not the default)
    
    Change-Id: Icae8739ced8b210cf7442b131d7f6670ecaae089

--- modules/profile/manifests/prometheus/squid_exporter.pp
+++ modules/profile/manifests/prometheus/squid_exporter.pp
@@ -4,25 +4,21 @@
 class profile::prometheus::squid_exporter (
     Stdlib::HTTPUrl $http_proxy = lookup('http_proxy', {'default_value' => undef}),
 ) {
-    # Note that prometheus-squid-exporter is only in buster and up
     ensure_packages('prometheus-squid-exporter')
 
     service { 'prometheus-squid-exporter':
         ensure  => running,
-        require => Service['squid'], # Squid and not Squid3 again on buster
+        require => Service['squid'],
     }
 
     if $http_proxy{
         $proxy_port = split($http_proxy, ':')[2]
         file { '/etc/default/prometheus-squid-exporter':
-            owner   => 'root',
-            group   => 'root',
             mode    => '0444',
             content => template('profile/prometheus/squid-exporter.conf.erb'),
             notify  => Service['prometheus-squid-exporter'],
         }
     }
-
 
     profile::auto_restarts::service { 'prometheus-squid-exporter': }
 }
