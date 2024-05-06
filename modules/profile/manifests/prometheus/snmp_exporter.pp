78eef9b0e35cb0ad8a025fed2ab0fb81c21a2413
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2024-05-06 14:53:12 +0200

    prometheus: use datacenters for snmp_exporter
    Adding new snmp_exporter modules is safe to do at any time, thus use
    $datacenters
    
    Bug: T364016
    Change-Id: I42339e57294480454e5b44c010da965a33394a80

--- modules/profile/manifests/prometheus/snmp_exporter.pp
+++ modules/profile/manifests/prometheus/snmp_exporter.pp
@@ -3,49 +3,25 @@ class profile::prometheus::snmp_exporter (
   # As of Jan 2022 all SNMP polling happens from codfw/eqiad netmon
   # hosts, therefore allow all Prometheus hosts to talk to snmp_exporter
     Array[Stdlib::Host] $prometheus_all_nodes = lookup('prometheus_all_nodes'),
+    Array[String] $datacenters = lookup('datacenters'),
 ) {
     include passwords::network
 
     class { '::prometheus::snmp_exporter': }
 
-    prometheus::snmp_exporter::module { 'pdu_codfw':
-        template  => 'servertech_sentry3',
-        community => $passwords::network::snmp_ro_community,
-    }
+    $datacenters.each |$dc| {
+        if $dc in ['eqiad', 'codfw'] {
+            # eqiad/codfw have sentry3 (in addition to sentry4)
+            prometheus::snmp_exporter::module { "pdu_${dc}":
+                template  => 'servertech_sentry3',
+                community => $passwords::network::snmp_ro_community,
+            }
+        }
 
-    prometheus::snmp_exporter::module { 'pdu_eqiad':
-        template  => 'servertech_sentry3',
-        community => $passwords::network::snmp_ro_community,
-    }
-
-    prometheus::snmp_exporter::module { 'pdu_sentry4_codfw':
-        template  => 'servertech_sentry4',
-        community => $passwords::network::snmp_ro_community,
-    }
-
-    prometheus::snmp_exporter::module { 'pdu_sentry4_eqiad':
-        template  => 'servertech_sentry4',
-        community => $passwords::network::snmp_ro_community,
-    }
-
-    prometheus::snmp_exporter::module { 'pdu_sentry4_ulsfo':
-        template  => 'servertech_sentry4',
-        community => $passwords::network::snmp_ro_community,
-    }
-
-    prometheus::snmp_exporter::module { 'pdu_sentry4_esams':
-        template  => 'servertech_sentry4',
-        community => $passwords::network::snmp_ro_community,
-    }
-
-    prometheus::snmp_exporter::module { 'pdu_sentry4_eqsin':
-        template  => 'servertech_sentry4',
-        community => $passwords::network::snmp_ro_community,
-    }
-
-    prometheus::snmp_exporter::module { 'pdu_sentry4_drmrs':
-        template  => 'servertech_sentry4',
-        community => $passwords::network::snmp_ro_community,
+        prometheus::snmp_exporter::module { "pdu_sentry4_${dc}":
+            template  => 'servertech_sentry4',
+            community => $passwords::network::snmp_ro_community,
+        }
     }
 
     if $::realm == 'labs' {
