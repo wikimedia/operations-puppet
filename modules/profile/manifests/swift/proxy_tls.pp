219e14f178e67690d78257891ef3412423f7b671
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-05-08 10:48:36 +0200

    profile::swift::proxy_tls: Use Envoy unconditionally and drop Hiera flag
    This is enabled in Hiera for all hosts (possibly some leftover of a gradual
    migration towards Envoy), so remove the flag and drop the unused code path.
    
    This is needed to eventually drop the cergen-related code paths in
    tlsproxy::localssl.
    
    Hosts: C:profile::swift::proxy_tls
    Bug: T357750
    Change-Id: Ib90f287363bafdbe7c19b9946b93f774008af22a

--- modules/profile/manifests/swift/proxy_tls.pp
+++ modules/profile/manifests/swift/proxy_tls.pp
@@ -1,23 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::swift::proxy_tls (
     String $ocsp_proxy = lookup('http_proxy', {'default_value' => ''}),
-    Boolean $use_envoy = lookup('profile::swift::proxy::use_envoy', {'default_value' => false}),
 ){
-
-    if $use_envoy {
-        include profile::tlsproxy::envoy
-    } else {
-        require ::profile::tlsproxy::instance
-
-        tlsproxy::localssl { 'unified':
-            server_name    => $::swift::proxy::proxy_service_host,
-            certs          => [$::swift::proxy::proxy_service_host],
-            default_server => true,
-            do_ocsp        => false,
-            ocsp_proxy     => $ocsp_proxy,
-            enable_http2   => false,
-        }
-    }
+    include profile::tlsproxy::envoy
 
     ferm::service { 'swift-proxy-https':
         proto   => 'tcp',
