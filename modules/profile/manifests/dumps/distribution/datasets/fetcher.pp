3c93a8fa8ab66725b4c55068698fe03381fd942c
Ben Tullis <btullis@wikimedia.org>
2024-05-08 12:30:43 +0100

    Drop the deprecated dumps fetcher that pulls from stat1007
    We are in the process of decommisisoning stat1007, so we need to stop
    any jobs that regularly pull data from this host to the dumps web
    servers.
    
    There were three jobs that ran on this host:
    * wikistats_1
    * pagecounts-ez
    * media-contestwinners.
    
    The files are synced and publicly available at
    
    * https://dumps.wikimedia.org/other/wikistats_1/
    * https://dumps.wikimedia.org/other/pagecounts-ez/
    * https://dumps.wikimedia.org/other/media/contest_winners/
    
    However, the source files on stat007 have not been updated since
    September 2020 so I don't believe that these fetcher jobs need to be
    migrated to a new stat host.
    
    Hosts: O:dumps::distribution::server
    Bug: T353785
    Bug: T238243
    Change-Id: I3228368cc142f2e95ade27e5dec9a5e377df91c1

--- modules/profile/manifests/dumps/distribution/datasets/fetcher.pp
+++ modules/profile/manifests/dumps/distribution/datasets/fetcher.pp
@@ -3,7 +3,6 @@ class profile::dumps::distribution::datasets::fetcher(
     Stdlib::Unixpath $xmldumpsdir = lookup('profile::dumps::distribution::xmldumpspublicdir'),
     Stdlib::Unixpath $miscdatasetsdir = lookup('profile::dumps::distribution::miscdumpsdir'),
     Hash $rsyncer_settings = lookup('profile::dumps::distribution::rsync_config'),
-    Stdlib::Host $stat_dump_host = lookup('profile::dumps::distribution::stat_dump_host'),
     Stdlib::Host $phab_dump_host = lookup('profile::dumps::distribution::phab_dump_host'),
 ) {
 
@@ -19,12 +18,6 @@ class profile::dumps::distribution::datasets::fetcher(
 
     class {'dumps::web::fetches::stats':
         src_hdfs        => '/wmf/data/archive',
-        miscdatasetsdir => $miscdatasetsdir,
-        user            => $user,
-    }
-
-    class {'dumps::web::fetches::stat_dumps':
-        src             => "${stat_dump_host}::srv/dumps",
         miscdatasetsdir => $miscdatasetsdir,
         user            => $user,
     }
