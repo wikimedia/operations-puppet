28154868a60a27573a09acbd4176592cef2dc0cd
Vgutierrez <vgutierrez@wikimedia.org>
2024-05-08 15:23:59 +0000

    Revert "fifo_log_demux,ATS: Support fifo-log-demux 0.7.3"
    This reverts commit 3ee9a54f5a670d4cd56e793eb80b26c4327f5472.
    
    Reason for revert: triggers an ATS logging config error
    
    Change-Id: Idc706560b9f80c5580ea0b043a6d42f1f7c6d4e3

--- modules/trafficserver/types/log.pp
+++ modules/trafficserver/types/log.pp
@@ -1,9 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 type Trafficserver::Log = Struct[{
-    'ensure'          => Wmflib::Ensure,
-    'filename'        => String,
-    'format'          => String,
-    'mode'            => Enum['ascii', 'binary', 'ascii_pipe'],
-    'filters'         => Optional[Array[String]],
-    'prometheus_port' => Optional[Stdlib::Port],
+    'ensure'   => Wmflib::Ensure,
+    'filename' => String,
+    'format'   => String,
+    'mode'     => Enum['ascii', 'binary', 'ascii_pipe'],
+    'filters'  => Optional[Array[String]],
 }]
