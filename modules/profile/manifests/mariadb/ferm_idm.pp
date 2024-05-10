ffe0b2499ecb050a6cdc32165a1df479d5c6e92d
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-05-10 15:00:31 +0200

    mariadb::ferm_idm: Avoid Ferm-specific syntax
    Remove Ferm-specific syntax from firewall definitions and switch to
    firewall::service. This will eventually allow to migrate invididual
    roles to nftables.
    
    Hosts: dbproxy1021.eqiad.wmnet
    Change-Id: I6071604d0b3891a6f0fe6b2bb33255eb73cb0f08

--- modules/profile/manifests/mariadb/ferm_idm.pp
+++ modules/profile/manifests/mariadb/ferm_idm.pp
@@ -1,16 +1,16 @@
 # SPDX-License-Identifier: Apache-2.0
 # Firewall rules for the misc db host used by idm.wikimedia.org
 class profile::mariadb::ferm_idm {
-    ferm::service { 'idm':
+    firewall::service { 'idm':
         proto   => 'tcp',
-        port    => '3306',
+        port    => 3306,
         notrack => true,
         srange  => ['idm1001.wikimedia.org', 'idm2001.wikimedia.org'],
     }
-    ferm::service { 'idm-test':
+    firewall::service { 'idm-test':
         proto   => 'tcp',
-        port    => '3306',
+        port    => 3306,
         notrack => true,
-        srange  => ['idm-test1001.wikimedia.org',],
+        srange  => ['idm-test1001.wikimedia.org'],
     }
 }
