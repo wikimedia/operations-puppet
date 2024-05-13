2d96185ad9456bd999afbbc55ec0f92f606eda8e
Ben Tullis <btullis@wikimedia.org>
2024-05-13 10:48:22 +0100

    Create /srv/analytics-wmde on stat1011
    We need to create this directory, which had become unmanaged after
    recent changes to enable the airflow instance for WMDE.
    
    Hosts: stat1011.eqiad.wmnet
    Bug: T353785
    Change-Id: Ib89428442fab9cf7f8a7768ce2bc2de0741266eb

--- modules/statistics/manifests/wmde.pp
+++ modules/statistics/manifests/wmde.pp
@@ -11,27 +11,33 @@ class statistics::wmde(
 ) {
 
     # The statistics module needs to be loaded before this one
-    Class['::statistics'] -> Class['::statistics::wmde']
+    Class['statistics'] -> Class['statistics::wmde']
 
-    $statistics_working_path = $::statistics::working_path
+    $statistics_working_path = $statistics::working_path
 
     $homedir = "${statistics_working_path}/analytics-wmde"
 
+    file { $homedir:
+        ensure => directory,
+        owner  => $user,
+        group  => $user,
+        mode   => '0755',
+    }
+
     # Scripts & systemd timers that generate data for graphite
-    class { '::statistics::wmde::graphite':
+    class { 'statistics::wmde::graphite':
         dir           => "${homedir}/graphite",
         user          => $user,
         statsd_host   => $statsd_host,
         graphite_host => $graphite_host,
         wmde_secrets  => $wmde_secrets,
-        require       => User[$user],
+        require       => [User[$user], File[$homedir]],
     }
 
     # Wikidata concepts processing
-    class { '::statistics::wmde::wdcm':
+    class { 'statistics::wmde::wdcm':
         dir     => "${homedir}/wdcm",
         user    => $user,
-        require => User[$user],
+        require => [User[$user], File[$homedir]],
     }
-
 }
