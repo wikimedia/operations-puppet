2045cb8d03f2c8b7da122a8ff580a53ff25b2111
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-05-14 14:09:37 +0200

    Zookeeper: New options for using firewall::service
    Introduce a new Hiera option profile::zookeeper::firewall::access which takes
    precedence over profile::zookeeper::firewall::srange to eventually switch over
    Druid clusters (and other ZK installations) to configs using firewall::service
    which is compatible with nftables.
    
    Hosts: C:profile::zookeeper::firewall
    Change-Id: Ie22240159011630cd906a17057d52c6995fc959b

--- modules/profile/manifests/zookeeper/firewall.pp
+++ modules/profile/manifests/zookeeper/firewall.pp
@@ -1,16 +1,27 @@
 # SPDX-License-Identifier: Apache-2.0
 # == Class profile::zookeeper::firewall::generic
 #
-# Firewall rules for a zookeeper cluster.
+# Firewall rules for a zookeeper cluster
 #
+# $firewall_access: An array of source sets which are allowed access to Zookeeper
+#                   Takes precedence over $srange)
 class profile::zookeeper::firewall (
-    String $srange = lookup('profile::zookeeper::firewall::srange'),
-){
+    Optional[String] $srange                 = lookup('profile::zookeeper::firewall::srange'),
+    Optional[Array[String]] $firewall_access = lookup('profile::zookeeper::firewall::access'),
 
-    ferm::service { 'zookeeper':
-        proto  => 'tcp',
-        # Zookeeper client, protocol ports
-        port   => [2181, 2182, 2183],
-        srange => $srange,
+){
+    if $firewall_access {
+        firewall::service { 'zookeeper':
+            proto    => 'tcp',
+            port     => [2181, 2182, 2183],
+            src_sets => $firewall_access,
+        }
+    } else {
+        ferm::service { 'zookeeper':
+            proto  => 'tcp',
+            # Zookeeper client, protocol ports
+            port   => [2181, 2182, 2183],
+            srange => $srange,
+        }
     }
 }
