d20a72b976f3613c443e0de727d4f7f47c3fd7db
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2024-05-14 16:23:02 +0200

    utils: use HEAD for get_config7.sh
    The checked out git commit might not match the ref that $environment is
    pointing at (for example self-hosted Puppet servers). Therefore default
    to display whichever commit is current in /srv/git/operations/puppet/
    
    Change-Id: I424ae486edf0e0098e19444104a8758858d37fe8

--- utils/get_config7.sh
+++ utils/get_config7.sh
@@ -8,7 +8,7 @@ environment=$1
 if [[ $environment != 'dev' ]]; then
 	git_dir=/srv/git/operations/puppet/.git
 	# %cN normalizes the committer using .mailmap
-	git --git-dir="${git_dir}" log -1 --pretty='(%h) %cN - %s' --first-parent "${environment}" --
+	git --git-dir="${git_dir}" log -1 --pretty='(%h) %cN - %s' --first-parent --
 else
 	# In the dev environment we may have a dirty tree, so for now always
 	# return dev
