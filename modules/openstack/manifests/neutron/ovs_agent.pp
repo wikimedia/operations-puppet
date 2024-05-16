1d643fbfb623c3b38ad95b22f7ad0085e6bc3f33
Taavi Väänänen <hi@taavi.wtf>
2024-05-16 11:17:40 +0300

    openstack: neutron: ovs_agent: Restart on config file change
    Bug: T326373
    Change-Id: I3f921490f7784246a19ec874f3434302ca6400de

--- modules/openstack/manifests/neutron/ovs_agent.pp
+++ modules/openstack/manifests/neutron/ovs_agent.pp
@@ -10,7 +10,12 @@ class openstack::neutron::ovs_agent (
     }
 
     service { 'neutron-openvswitch-agent':
-        ensure => running,
-        enable => true,
+        ensure    => running,
+        enable    => true,
+        subscribe => [
+            File['/etc/neutron/neutron.conf'],
+            File['/etc/neutron/plugins/ml2/ml2_conf.ini'],
+            # openvswitch_agent.ini uses a notify on the file resource
+        ],
     }
 }
