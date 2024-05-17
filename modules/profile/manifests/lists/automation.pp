e50fa77c268e7aabd5c09fa147acb3fede47c7fd
Daniel Zahn <dzahn@wikimedia.org>
2024-05-17 14:13:19 -0700

    lists: add timer to sync data from stewards hosts
    Iac64e2b0acc7bb added an rsync server on stewards hosts.
    
    Now we can pull subscriber data from them.
    
    Create a timer that runs the rsync command to pull from the active
    stewards host. To have it available add it to common Hiera.
    
    Also specify --address and the right IP to use as source IP because
    lists1001.wikimedia.org also has lists.wikimedia.org on the same
    interface and if we don't we run into firewall issues.
    
    Put this in the new automation class which can be reused if we
    have more use cases for automating subscriptions of certain lists.
    
    Bug: T351202
    Change-Id: I7cefee2f1bb7a964e933feabf3cb8b038c03e07a

--- modules/profile/manifests/lists/automation.pp
+++ modules/profile/manifests/lists/automation.pp
@@ -1,10 +1,22 @@
 # SPDX-License-Identifier: Apache-2.0
+# automate subscribing users to certain lists
+# intoduced for steward-related lists (T351202)
+#
 class profile::lists::automation (
     Stdlib::Unixpath $data_dir = lookup('profile::lists::automation::data_dir', {default_value => '/srv/exports'}),
+    Stdlib::Host $lists_host = lookup('lists_primary_host'),
+    Stdlib::Host $stewards_host = lookup('stewards_primary_host'),
 ){
 
     wmflib::dir::mkdir_p($data_dir, {
         mode  => '0775',
     })
 
+    systemd::timer::job { 'stewards_subscriber_data_sync':
+        ensure      => 'present',
+        user        => 'root',
+        description => 'copy exported subscriber data from steward machines ()',
+        command     => "/usr/bin/rsync --address ${lists_host} -ap rsync://${stewards_host}/steward-data-export-dir ${data_dir}",
+        interval    => {'start' => 'OnCalendar', 'interval' => 'hourly'},
+    }
 }
