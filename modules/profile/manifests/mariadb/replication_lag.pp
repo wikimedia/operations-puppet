145f24941b962df0d2abe4e5710249b719002490
Amir Sarabadani <ladsgroup@gmail.com>
2024-05-20 18:58:38 +0200

    mariadb: Bump alerting for replag
    It's flood spamming us. The user impact of the current situation is
    minimal and we are trying to fix the underlying cause but it will take
    some time.
    
    Change-Id: I92f12c0a770ca617507b8bf0c63625bdd05fc24c

--- modules/profile/manifests/mariadb/replication_lag.pp
+++ modules/profile/manifests/mariadb/replication_lag.pp
@@ -6,8 +6,8 @@ define profile::mariadb::replication_lag(
         dashboard_links => ["https://grafana.wikimedia.org/d/000000273/mysql?orgId=1&var-server=${::hostname}&var-port=${prom_port}"],
         query           => "scalar(avg_over_time(mysql_slave_status_seconds_behind_master{instance=\"${::hostname}:${prom_port}\"}[5m]))",
         prometheus_url  => "http://prometheus.svc.${::site}.wmnet/ops",
-        warning         => 1,
-        critical        => 2,
+        warning         => 5,
+        critical        => 10,
         contact_group   => 'databases-testing',
         notes_link      => 'https://wikitech.wikimedia.org/wiki/MariaDB/troubleshooting#Replication_lag'
     }
