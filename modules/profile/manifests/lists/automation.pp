be89bb6a9404e701f2abf7293ebc882e875fd05d
Daniel Zahn <dzahn@wikimedia.org>
2024-05-20 11:16:11 -0700

    lists/stewards: add timer to run mailman syncmembers for stewards-l
    Previously we have added an rsyncd to the stewards machine and a timer
    on lists1001 to sync stewards-l subscriber data from there.
    
    Now this is a second timer to run the "mailman-wrapper syncmembers"
    command to actually sync list members with the imported data.
    
    Note that at first there is the "-n" in there that means the
    command won't actually be executed. We log what it _would_ do,
    as in either removing or adding list subscribers.
    
    Later we would remove the -n and either remove logging or make
    sure it gets logrotated.
    
    Also this would be done the same way for multiple lists.
    
    Bug: T351202
    Change-Id: I2e5ecaaf0e52e8a1a1387a865717aa037b43197d

--- modules/profile/manifests/lists/automation.pp
+++ modules/profile/manifests/lists/automation.pp
@@ -16,8 +16,21 @@ class profile::lists::automation (
     systemd::timer::job { 'stewards_subscriber_data_sync':
         ensure      => $ensure,
         user        => 'root',
-        description => 'copy exported subscriber data from steward machines ()',
+        description => 'copy exported stewards subscriber data from primary steward machine',
         command     => "/usr/bin/rsync --address ${lists_host} -ap rsync://${stewards_host}/steward-data-export-dir ${data_dir}",
+        interval    => {'start' => 'OnCalendar', 'interval' => 'hourly'},
+    }
+
+    systemd::timer::job { 'stewards_subscriber_list_sync':
+        ensure      => $ensure,
+        user        => 'root',
+        description => 'sync stewards lists members with imported subscriber data',
+        command     => @(CMD/L),
+          "/usr/bin/mailman-wrapper syncmembers -n\
+          /${data_dir}/mailman_list/lists.wikimedia.org/stewards-l\
+          stewards-l@lists.wikimedia.org\
+          >> /var/log/mailman/stewards-l-member-sync.log"
+          | CMD
         interval    => {'start' => 'OnCalendar', 'interval' => 'hourly'},
     }
 }
