6a2b7a2101e65819ea9f5be3df6db9c1a783f98c
Chris Dobbins <cdobbins@wikimedia.org>
2024-05-22 13:18:50 -0400

    varnish: show better error for 429s
    * Check for 429 status code on responses returned from the backend
    * If such a response is returned, the status code is propagated to the
    client, along with the reason "Too many requests at <current_time>"
    * There's also a test in /modules/varnish/files/tests/text\
    /52-backend-error.vtc
    * It is extremely basic; it spins up a server and Varnish instance, then
    sends a request to the "/" endpoint with a header of "Host: en.wikipedia\
    .org." It then checks the response's status code and fails if it is !=
    429
    
    Bug: T354718
    Change-Id: I5609be81ea498b359ebf153ebbf25878a6ce3168

--- modules/varnish/manifests/common/errorpage.pp
+++ modules/varnish/manifests/common/errorpage.pp
@@ -5,13 +5,14 @@ class varnish::common::errorpage {
         logo_link   => 'https://www.wikimedia.org',
         logo_src    => 'https://www.wikimedia.org/static/images/wmf-logo.png',
         logo_srcset => 'https://www.wikimedia.org/static/images/wmf-logo-2x.png 2x',
-        logo_width  => '135',
-        logo_height => '101',
+        logo_width  => 135,
+        logo_height => 101,
         logo_alt    => 'Wikimedia',
+        content     => '<p>%error_body_content%</p>',
         # Placeholder "%error%" substituted at runtime in errorpage.inc.vcl
         footer      => template('varnish/errorpage.footer.html.erb'),
     }
-    $errorpage_html = template('mediawiki/errorpage.html.erb')
+    $errorpage_html = mediawiki::errorpage_content($errorpage)
 
     file { '/etc/varnish/errorpage.inc.vcl':
         owner   => 'root',
