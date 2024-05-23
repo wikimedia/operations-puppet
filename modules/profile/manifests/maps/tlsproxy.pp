147fa6c7b3d5471734005bed113caed49268f7fd
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-05-23 12:24:25 +0200

    maps: Add option to use PKI
    Add a new class parameter to use PKI instead of the cergen-issued cert.
    
    Bug: T360778
    Hosts: C:profile::maps::tlsproxy
    Change-Id: I7dd6bae9ee8ddc409a00e6d3c441ae841d63410f

--- modules/profile/manifests/maps/tlsproxy.pp
+++ modules/profile/manifests/maps/tlsproxy.pp
@@ -1,17 +1,34 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::maps::tlsproxy(
     String $servicename      = lookup('profile::maps::tlsproxy::servicename'),
+    Boolean $use_pki         = lookup('profile::maps::tlsproxy::use_pki', {'default_value' => false}),
     String $ocsp_proxy       = lookup('http_proxy', {'default_value' => ''}),
 ){
+    if $use_pki {
+        $cfssl_paths = profile::pki::get_cert('discovery', $facts['networking']['fqdn'], {
+            hosts => ['maps.wikimedia.org', "kartotherian.svc.${::site}.wmnet"],
+        })
 
-    tlsproxy::localssl { $servicename:
-        server_name    => $servicename,
-        certs          => [$servicename],
-        upstream_ports => [6533],
-        default_server => true,
-        do_ocsp        => false,
-        ocsp_proxy     => $ocsp_proxy,
-        enable_http2   => false,
+        tlsproxy::localssl { $servicename:
+            server_name    => $servicename,
+            upstream_ports => [6533],
+            default_server => true,
+            do_ocsp        => false,
+            ocsp_proxy     => $ocsp_proxy,
+            enable_http2   => false,
+            cfssl_paths    => $cfssl_paths,
+            server_aliases => ['maps.wikimedia.org',"kartotherian.svc.${::site}.wmnet"],
+        }
+    } else {
+        tlsproxy::localssl { $servicename:
+            server_name    => $servicename,
+            certs          => [$servicename],
+            upstream_ports => [6533],
+            default_server => true,
+            do_ocsp        => false,
+            ocsp_proxy     => $ocsp_proxy,
+            enable_http2   => false,
+        }
     }
 
     ferm::service { 'maps-proxy-https':
