b98bab40258b7d6908adcd4331a5682f8aea417d
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-05-23 11:11:54 +0200

    Remove profile::zookeeper::firewall::srange
    All Zookeeper clusters have been converted to
    profile::zookeeper::firewall::access, i.e. they are using firewall
    definitions via firewall::service now. The legacy class parameter is no
    longer needed, so remove it and make profile::zookeeper::firewall::access
    non-optional again (which was only done temporarily for the transition)
    
    Hosts: C:profile::zookeeper::firewall
    Change-Id: I2c88bd6475f5a2f2996c4647cdcaa73721481879

--- modules/profile/manifests/zookeeper/firewall.pp
+++ modules/profile/manifests/zookeeper/firewall.pp
@@ -6,22 +6,12 @@
 # $firewall_access: An array of source sets which are allowed access to Zookeeper
 #                   Takes precedence over $srange)
 class profile::zookeeper::firewall (
-    Optional[String] $srange                 = lookup('profile::zookeeper::firewall::srange'),
-    Optional[Array[String]] $firewall_access = lookup('profile::zookeeper::firewall::access'),
+    Array[String] $firewall_access = lookup('profile::zookeeper::firewall::access'),
 
 ){
-    if $firewall_access {
-        firewall::service { 'zookeeper':
-            proto    => 'tcp',
-            port     => [2181, 2182, 2183],
-            src_sets => $firewall_access,
-        }
-    } else {
-        ferm::service { 'zookeeper':
-            proto  => 'tcp',
-            # Zookeeper client, protocol ports
-            port   => [2181, 2182, 2183],
-            srange => $srange,
-        }
+    firewall::service { 'zookeeper':
+        proto    => 'tcp',
+        port     => [2181, 2182, 2183],
+        src_sets => $firewall_access,
     }
 }
