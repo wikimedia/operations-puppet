2a29b7e51fa239175fcc3e1daeb47effd99449dd
Erik Bernhardson <ebernhardson@wikimedia.org>
2024-05-28 13:40:54 -0700

    cirrus: Restrict saneitizer to private wikis
    With writes moved to SUP the saneitizer no longer needs to
    be invoked on all wikis, we can limit it to private wikis.
    
    Change-Id: If727ef756794f831c837fc1cd3133efb746d73f0

--- modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
+++ modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
@@ -21,10 +21,10 @@ class profile::mediawiki::maintenance::cirrussearch {
         interval => '02:30',
     }
 
-    # T295705 2021-11-22 Temporarily disabling saneitizer for at least the next several days
     profile::mediawiki::periodic_job { 'cirrus_saneitize_jobs':
         ensure   => present,
-        command  => '/usr/local/bin/foreachwiki extensions/CirrusSearch/maintenance/SaneitizeJobs.php --push --refresh-freq=7200',
+        # Saneitizer limited to private wikis, SUP handles all public wikis.
+        command  => '/usr/local/bin/foreachwikiindblist private extensions/CirrusSearch/maintenance/SaneitizeJobs.php --push --refresh-freq=7200',
         interval => '0/2:10',
     }
 }
