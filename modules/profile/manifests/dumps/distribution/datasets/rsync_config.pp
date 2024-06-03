029ba6a86077f190cee3469880de01225e5f651d
Ben Tullis <btullis@wikimedia.org>
2024-06-03 12:35:03 +0100

    Remove absented rsync configs for deprecated dumps
    These dumps are no longer running and the rsync config fragments that
    permitted them to be synced from stats servers to the distribution
    servers have been absented.
    
    This commit removes the unused resources and associated hiera values.
    
    Hosts: O:dumps::distribution::server
    Bug: T353785
    Change-Id: I0ea43381987465d00dc6971dc5b4f6642810c160

--- modules/profile/manifests/dumps/distribution/datasets/rsync_config.pp
+++ modules/profile/manifests/dumps/distribution/datasets/rsync_config.pp
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::dumps::distribution::datasets::rsync_config(
     Hash $rsyncer_settings = lookup('profile::dumps::distribution::rsync_config'),
-    String $stats_hosts = lookup('profile::dumps::stats_hosts'),
     String $peer_hosts = lookup('profile::dumps::peer_hosts'),
     String $phab_hosts = lookup('profile::dumps::phab_hosts'),
     Stdlib::Unixpath $xmldumpsdir = lookup('profile::dumps::distribution::xmldumpspublicdir'),
@@ -12,24 +11,6 @@ class profile::dumps::distribution::datasets::rsync_config(
     $group = $rsyncer_settings['dumps_group']
     $deploygroup = $rsyncer_settings['dumps_deploygroup']
     $mntpoint = $rsyncer_settings['dumps_mntpoint']
-
-    file { '/etc/rsyncd.d/30-rsync-media.conf':
-        ensure  => absent,
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        content => template('profile/dumps/distribution/datasets/rsyncd.conf.media.erb'),
-        notify  => Exec['update-rsyncd.conf'],
-    }
-
-    file { '/etc/rsyncd.d/30-rsync-pagecounts_ez.conf':
-        ensure  => absent,
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        content => template('profile/dumps/distribution/datasets/rsyncd.conf.pagecounts_ez.erb'),
-        notify  => Exec['update-rsyncd.conf'],
-    }
 
     file { '/etc/rsyncd.d/40-rsync-phab_dump.conf':
         ensure  => 'present',
