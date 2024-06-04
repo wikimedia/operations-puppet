13296544c101557a4b966008a2603257dd19c430
Brett Cornwall <bcornwall@wikimedia.org>
2024-06-04 12:33:50 -0700

    ncmonitor: Add SSH credentials support
    Also switch to using the profile credentials rather than direct
    password lookups.
    
    Bug: T355189
    Change-Id: I87e39bec7bfb7bc8c2d9931dce02c8a4c00b9910

--- modules/ncmonitor/manifests/init.pp
+++ modules/ncmonitor/manifests/init.pp
@@ -25,6 +25,17 @@
 #
 #   Default: undef
 #
+# [*gerrit_ssh_key*]
+#   SSH key contents with access to Gerrit.
+#
+#   Default: undef
+#
+# [*gerrit_ssh_pubkey*]
+#   Corresponding public key to the private key. Only used for
+#   convenience of setting up access in services..
+#
+#   Default: undef
+#
 # [*http_proxy*]
 #   Endpoint to proxy ncmonitor's systemd unit traffic (via
 #   Environment=).
@@ -36,13 +47,37 @@ class ncmonitor(
     Array[Stdlib::Host]       $nameservers,
     String                    $markmon_api_user,
     String                    $markmon_api_pass,
+    String                    $gerrit_ssh_key,
+    Optional[String]          $gerrit_ssh_pubkey,
     Optional[Stdlib::HTTPUrl] $http_proxy,
 ) {
     $config = {
-        nameservers => $nameservers,
+        acmechief   => {
+            conf-path  => 'hieradata/common/certificates.yaml',
+            remote-url => 'ssh://ncmonitor@gerrit.wikimedia.org:29418/operations/puppet',
+        },
+        dnsrepo     => {
+            remote-url       => 'ssh://ncmonitor@gerrit.wikimedia.org:29418/operations/dns',
+            target-zone-path => 'templates/ncredir-parking',
+        },
+        gerrit      => {
+            reviewers    => [
+                'bcornwall@wikimedia.org',
+                'cdobbins@wikimedia.org',
+                'ffurnari@wikimedia.org',
+                'ssingh@wikimedia.org',
+                'vgutierrez@wikimedia.org',
+            ],
+            ssh-key-path => '/etc/ncmonitor/gerrit.key',
+        },
         markmonitor => {
             username => $markmon_api_user,
             password => $markmon_api_pass,
+        },
+        nameservers => $nameservers,
+        ncredir     => {
+            datfile-path => 'modules/ncredir/files/nc_redirects.dat',
+            remote-url   => 'ssh://ncmonitor@gerrit.wikimedia.org:29418/operations/puppet'
         },
     }
 
@@ -77,6 +112,28 @@ class ncmonitor(
         require   => Package['ncmonitor'],
         backup    => false,
         show_diff => false,
+    }
+
+    file { '/etc/ncmonitor/gerrit.key':
+        ensure    => $ensure,
+        owner     => 'ncmonitor',
+        group     => 'root',
+        mode      => '0400',
+        content   => $gerrit_ssh_key,
+        backup    => false,
+        show_diff => false,
+    }
+
+    if $gerrit_ssh_pubkey {
+        file { '/etc/ncmonitor/gerrit.pub':
+            ensure  => $ensure,
+            owner   => 'ncmonitor',
+            group   => 'root',
+            mode    => '0644',
+            content => $gerrit_ssh_pubkey,
+        }
+    } else {
+        file { '/etc/ncmonitor/gerrit.pub': ensure => 'absent' }
     }
 
     # Some environments (e.g. WMCS) should not have proxies set. Merely setting
--- modules/profile/manifests/ncmonitor.pp
+++ modules/profile/manifests/ncmonitor.pp
@@ -1,15 +1,20 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::ncmonitor(
-    Array[Stdlib::Host]       $nameservers = lookup('profile::ncmonitor::nameservers'),
-    Optional[Stdlib::HTTPUrl] $http_proxy  = lookup('http_proxy'),
+    Array[Stdlib::Host]       $nameservers       = lookup('profile::ncmonitor::nameservers'),
+    Optional[Stdlib::HTTPUrl] $http_proxy        = lookup('http_proxy'),
+    String                    $mm_api_user       = lookup('profile::ncmonitor::markmonitor_api_user'),
+    String                    $mm_api_pass       = lookup('profile::ncmonitor::markmonitor_api_password'),
+    String                    $gerrit_ssh_key    = lookup('profile::ncmonitor::gerrit_ssh_key'),
+    String                    $gerrit_ssh_pubkey = lookup('profile::ncmonitor::gerrit_ssh_pubkey'),
 ) {
-
     class { 'ncmonitor':
-        nameservers      => $nameservers,
-        markmon_api_user => 'temporary_for_passwords_removal',
-        markmon_api_pass => 'temporary_for_passwords_removal',
-        http_proxy       => $http_proxy,
-        ensure           => present,
+        nameservers       => $nameservers,
+        markmon_api_user  => $mm_api_user,
+        markmon_api_pass  => $mm_api_pass,
+        gerrit_ssh_key    => $gerrit_ssh_key,
+        gerrit_ssh_pubkey => $gerrit_ssh_pubkey,
+        http_proxy        => $http_proxy,
+        ensure            => present,
     }
 
 }
