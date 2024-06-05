a017caba63d8a91d75d742833687b2338643d98c
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-06-05 11:46:46 +0200

    profile::maps::tlsproxy: Unconditionally use PKI
    All maps instances have been switched to cfssl, remove the legacy
    support for the cergen-based code paths.
    
    Hosts: C:profile::maps::tlsproxy
    Bug: T360778
    Change-Id: I7170ceba2ca92b20fc572b4cd16eb2b59185b997

--- modules/profile/manifests/maps/tlsproxy.pp
+++ modules/profile/manifests/maps/tlsproxy.pp
@@ -1,28 +1,17 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::maps::tlsproxy(
     String $servicename      = lookup('profile::maps::tlsproxy::servicename'),
-    Boolean $use_pki         = lookup('profile::maps::tlsproxy::use_pki', {'default_value' => false}),
 ){
-    if $use_pki {
-        $cfssl_paths = profile::pki::get_cert('discovery', $facts['networking']['fqdn'], {
-            hosts => ['maps.wikimedia.org', "kartotherian.svc.${::site}.wmnet", 'kartotherian.discovery.wmnet'],
-        })
+    $cfssl_paths = profile::pki::get_cert('discovery', $facts['networking']['fqdn'], {
+        hosts => ['maps.wikimedia.org', "kartotherian.svc.${::site}.wmnet", 'kartotherian.discovery.wmnet'],
+    })
 
-        tlsproxy::localssl { $servicename:
-            server_name    => $servicename,
-            upstream_ports => [6533],
-            default_server => true,
-            enable_http2   => false,
-            cfssl_paths    => $cfssl_paths,
-        }
-    } else {
-        tlsproxy::localssl { $servicename:
-            server_name    => $servicename,
-            certs          => [$servicename],
-            upstream_ports => [6533],
-            default_server => true,
-            enable_http2   => false,
-        }
+    tlsproxy::localssl { $servicename:
+        server_name    => $servicename,
+        upstream_ports => [6533],
+        default_server => true,
+        enable_http2   => false,
+        cfssl_paths    => $cfssl_paths,
     }
 
     firewall::service { 'maps-proxy-https':
