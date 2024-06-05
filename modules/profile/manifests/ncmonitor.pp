205aeeb9b682d599c7e0a38462eaad4f87e3e7f4
Brett Cornwall <bcornwall@wikimedia.org>
2024-06-05 12:46:09 -0700

    ncmonitor: Temporary removal of passwords lookup
    This removes the passwords lookups for secrets so that pcc will success
    again.
    
    Change-Id: I3c8fc2f7a165d628309d9d0d3dc6d118e694b572

--- modules/profile/manifests/ncmonitor.pp
+++ modules/profile/manifests/ncmonitor.pp
@@ -3,12 +3,11 @@ class profile::ncmonitor(
     Array[Stdlib::Host]       $nameservers = lookup('profile::ncmonitor::nameservers'),
     Optional[Stdlib::HTTPUrl] $http_proxy  = lookup('http_proxy'),
 ) {
-    include passwords::ncmonitor::markmonitor
 
     class { 'ncmonitor':
         nameservers      => $nameservers,
-        markmon_api_user => $passwords::ncmonitor::markmonitor::api_user,
-        markmon_api_pass => $passwords::ncmonitor::markmonitor::api_password,
+        markmon_api_user => 'temporary_for_passwords_removal',
+        markmon_api_pass => 'temporary_for_passwords_removal',
         http_proxy       => $http_proxy,
         ensure           => present,
     }
