5d36a40a98e0086a74d65d328e92e9f621d19808
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2024-06-07 12:55:27 +0200

    k8s: send logs to per-cluster kafka topics
    We want to split up the k8s logs kafka topics to achieve the following:
    * smaller 'blast radius' from a single spammy producer
    * increased concurrency on the consumer side, i.e. higher throughput
    
    The change will affect only staging clusters, and double-log to both new
    and old topics to verify functionality.
    
    Bug: T366710
    Change-Id: I4115b56001540bdcb244fd672faa6f02e48170a7

--- modules/profile/manifests/rsyslog/kubernetes.pp
+++ modules/profile/manifests/rsyslog/kubernetes.pp
@@ -1,8 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 class profile::rsyslog::kubernetes (
-    Boolean $enable                  = lookup('profile::rsyslog::kubernetes::enable', { 'default_value' => true }),
-    String $kubernetes_cluster_name  = lookup('profile::kubernetes::cluster_name'),
+    Boolean $enable                 = lookup('profile::rsyslog::kubernetes::enable', { 'default_value' => true }),
+    String $kubernetes_cluster_name = lookup('profile::kubernetes::cluster_name'),
+    Array $kafka_brokers            = lookup('profile::rsyslog::kafka_shipper::kafka_brokers'),
 ) {
     include profile::rsyslog::shellbox
 
@@ -30,4 +31,17 @@ class profile::rsyslog::kubernetes (
         content  => template('profile/rsyslog/kubernetes.conf.erb'),
         priority => 9,
     }
+
+    # Enforce k8s- prefix on topics, some cluster names will lead to "k8s" duplication
+    $log_topic_name = sprintf('k8s-%s', $kubernetes_cluster_name)
+    $trusted_ca_path = profile::base::certificates::get_trusted_ca_path()
+
+    if 'staging' in $kubernetes_cluster_name {
+        rsyslog::conf { 'output_kafka_k8s':
+            ensure   => $ensure,
+            content  => template('profile/rsyslog/output_kafka_k8s.conf.erb'),
+            priority => 35,
+        }
+    }
+
 }
