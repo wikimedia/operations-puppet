4ab71fde1e1b8833330d2e2f9dd46f5e840c935d
jelto <jwodstrcil@wikimedia.org>
2024-06-10 12:33:32 +0200

    gitlab: use IPv4 for SSH check temporarily
    The blackbox check fails on IPv6 because SSH is not listening on IPv6.
    Until this is fixed, let's just check IPv4.
    
    Bug: T367021
    Change-Id: I19338b0d16bdf69e5d5c32f87d1819e120c1ea82

--- modules/profile/manifests/gitlab.pp
+++ modules/profile/manifests/gitlab.pp
@@ -85,9 +85,10 @@ class profile::gitlab(
             body_regex_matches => ['GitLab Community Edition'],
         }
         prometheus::blackbox::check::tcp { "${service_name}-ssh":
-            team     => 'collaboration-services',
-            severity => $severity,
-            port     => 22,
+            team        => 'collaboration-services',
+            severity    => $severity,
+            ip_families => ['ip4'],
+            port        => 22,
         }
     }
 
