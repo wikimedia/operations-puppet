8fc5e48d44dd7ce54ded4ee5af0d7bc31382053a
Andrew Bogott <abogott@wikimedia.org>
2024-06-13 12:21:31 -0500

    openstack::clientpackages::vms::bobcat::bullseye: install 'zed' packages
    Previously we were just using generic debian bullseye packages
    which were slightly older.
    
    I suspect that by leaving this file unmanaged we were leaving latent
    files in place to cause issues.
    
    Hosts: tools-redis-6.tools.eqiad1.wikimedia.cloud
    Bug: T366028
    Change-Id: I4d4cdb9b8ba4207d5a749aba574951d0320cb0a1

--- modules/openstack/manifests/clientpackages/vms/bobcat/bullseye.pp
+++ modules/openstack/manifests/clientpackages/vms/bobcat/bullseye.pp
@@ -4,4 +4,56 @@
 class openstack::clientpackages::vms::bobcat::bullseye(
 ) {
     requires_realm('labs')
+    # The most recent Openstack version available for Bullseye is 'zed'
+
+    apt::repository { 'openstack-zed-bullseye':
+        uri        => 'http://mirrors.wikimedia.org/osbpo',
+        dist       => 'bullseye-zed-backports',
+        components => 'main',
+        source     => false,
+        keyfile    => 'puppet:///modules/openstack/serverpackages/osbpo-pubkey.asc',
+        notify     => Exec['openstack-zed-bullseye-apt-upgrade'],
+    }
+
+    apt::repository { 'openstack-zed-bullseye-nochange':
+        uri        => 'http://mirrors.wikimedia.org/osbpo',
+        dist       => 'bullseye-zed-backports-nochange',
+        components => 'main',
+        source     => false,
+        keyfile    => 'puppet:///modules/openstack/serverpackages/osbpo-pubkey.asc',
+        notify     => Exec['openstack-zed-bullseye-apt-upgrade'],
+    }
+
+    # ensure apt can see the repo before any further Package[] declaration
+    # so this proper repo/pinning configuration applies in the same puppet
+    # agent run
+    exec { 'openstack-zed-bullseye-apt-upgrade':
+        command     => '/usr/bin/apt-get update',
+        require     => [Apt::Repository['openstack-zed-bullseye'],
+                        Apt::Repository['openstack-zed-bullseye-nochange']],
+        subscribe   => [Apt::Repository['openstack-zed-bullseye'],
+                        Apt::Repository['openstack-zed-bullseye-nochange']],
+        refreshonly => true,
+        logoutput   => true,
+    }
+    Exec['openstack-zed-bullseye-apt-upgrade'] -> Package <| title != 'gnupg' |>
+
+    apt::repository { 'openstack-yoga-bullseye':
+        ensure => absent
+    }
+    apt::repository { 'openstack-yoga-bullseye-nochange':
+        ensure => absent
+    }
+    apt::repository { 'openstack-wallaby-bullseye':
+        ensure => absent
+    }
+    apt::repository { 'openstack-wallaby-bullseye-nochange':
+        ensure => absent
+    }
+    apt::repository { 'openstack-victoria-bullseye':
+        ensure => absent
+    }
+    apt::repository { 'openstack-victoria-bullseye-nochange':
+        ensure => absent
+    }
 }
