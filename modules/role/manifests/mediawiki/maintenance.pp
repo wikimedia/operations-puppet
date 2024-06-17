9a854c6e58ffd341f5f928ac3e539093116ce3c1
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-06-17 09:04:52 +0200

    mwmaint: Stop including profile::openldap::management
    The mwmaint servers will eventually vanish with the full wikikube rollout,
    the LDAP management parts have been split to the ldap-maint hosts.
    
    Bug: T367490
    Change-Id: I541906885b02b22b215356463b9772cc57dc8ab7

--- modules/role/manifests/mediawiki/maintenance.pp
+++ modules/role/manifests/mediawiki/maintenance.pp
@@ -3,19 +3,14 @@ class role::mediawiki::maintenance {
         description => 'MediaWiki maintenance systemd timer job server',
     }
 
-    include ::profile::base::production
-    include ::profile::firewall
+    include profile::base::production
+    include profile::firewall
 
     # MediaWiki
-    include ::role::mediawiki::common
-    include ::profile::mediawiki::maintenance
+    include role::mediawiki::common
+    include profile::mediawiki::maintenance
 
     # MariaDB
-    include ::profile::mariadb::maintenance
-    include ::profile::mariadb::client
-
-    # LDAP
-    if ($::realm == 'production') {
-        include ::profile::openldap::management
-    }
+    include profile::mariadb::maintenance
+    include profile::mariadb::client
 }
