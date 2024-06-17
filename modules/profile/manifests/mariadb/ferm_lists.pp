f3fb47e4b36b3c12f65d9068c187a908dac961bd
Eoghan Gaffney <egaffney@wikimedia.org>
2024-06-17 22:57:21 +0100

    lists: Switch DB firewall rules to use primary host variable
    Bug: T331706
    Change-Id: Ib2f0da75f2eb91b573e9cb93bd0be4e78967f90e

--- modules/profile/manifests/mariadb/ferm_lists.pp
+++ modules/profile/manifests/mariadb/ferm_lists.pp
@@ -1,10 +1,12 @@
 # Firewall rules for the misc db host used by lists.wikimedia.org (Mailman).
 # See T256538#6943696, T278614#7023029 for history.
-class profile::mariadb::ferm_lists {
+class profile::mariadb::ferm_lists (
+    Stdlib::Fqdn $mailman_primary_host = lookup('lists_primary_host'),
+) {
     ferm::service { 'mailman3':
         proto   => 'tcp',
         port    => '3306',
         notrack => true,
-        srange  => '@resolve(lists1001.wikimedia.org)',
+        srange  => "@resolve(${mailman_primary_host})",
     }
 }
