c87fb34086ebd0de0a7bfd432481d17e65ff98e8
Clément Goubert <cgoubert@wikimedia.org>
2024-06-18 12:14:37 +0200

    wikikube: Use conftool for scap docker_pull_k8s
    Why:
    Failed nodes cause confusing errors for deployers since they are still
    in the list despite being depooled.
    
    What:
    Change the way we build the dsh list from a pdb query to a conftool one.
    This has the side effect of pulling in the sessionstore nodes that don't have
    the mediawiki-image-download script. We override it with /bin/true on
    these so we don't cause more errors.
    This is a stopgap measure pending the investigation of the complete
    removal of this step in T366778
    
    Hosts: deploy1002.eqiad.wmnet,wikikube-worker1001.eqiad.wmnet,kubernetes1005.eqiad.wmnet
    Bug: T367862
    Change-Id: Ib070bd80e729f050a6a9452ece966b4d0778aa76

--- modules/profile/manifests/kubernetes/mediawiki_runner.pp
+++ modules/profile/manifests/kubernetes/mediawiki_runner.pp
@@ -5,27 +5,37 @@ class profile::kubernetes::mediawiki_runner(
 ) {
     # For now, assume we can use any node that's not marked as dedicated.
     $reserved_node = /dedicated=.*/ in $kubelet_node_labels
-    $ensure  = $reserved_node.bool2str('absent', 'present')
-
     $command = '/usr/local/sbin/mediawiki-image-download'
-    file { $command:
-        ensure => $ensure,
-        mode   => '0544',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/profile/kubernetes/node/mediawiki-image-download.sh'
+
+    if $reserved_node {
+        # Just pretend to do it on the non-reserved nodes
+        file { $command:
+            ensure => link,
+            target => '/bin/true',
+        }
+    } else {
+        # Download the mediawiki image on the reserved nodes
+        file { $command:
+            ensure => present,
+            mode   => '0544',
+            owner  => 'root',
+            group  => 'root',
+            source => 'puppet:///modules/profile/kubernetes/node/mediawiki-image-download.sh'
+        }
+
     }
+
     ## Scap "client"
     # Please note: if we ever want to actually use the scap client to not just deliver commands but to
     # distribute the code, we should include profile::mediawiki::scap_client instead
     # The following is copied over from mediawiki::users; TODO: refactor and DRY
     group { 'mwdeploy':
-        ensure => $ensure,
+        ensure => present,
         system => true,
     }
 
     user { 'mwdeploy':
-        ensure     => $ensure,
+        ensure     => present,
         shell      => '/bin/bash',
         home       => '/var/lib/mwdeploy',
         system     => true,
@@ -33,19 +43,19 @@ class profile::kubernetes::mediawiki_runner(
     }
 
     ssh::userkey { 'mwdeploy':
-        ensure  => $ensure,
+        ensure  => present,
         content => secret('keyholder/mwdeploy.pub'),
     }
     # Grant mwdeploy sudo rights to download the mediawiki image.
     sudo::user { 'mwdeploy':
-        ensure     => $ensure,
+        ensure     => present,
         privileges => [
             "ALL = (root) NOPASSWD: ${command} *",
         ]
     }
 
     class { 'scap::ferm':
-        ensure => $ensure,
+        ensure => present,
     }
 
     unless $reserved_node {
