418cd857e0b9da2d7d58e6a8998bd54e704b7272
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-06-18 13:32:49 +0200

    idp::build: Make the rsync setup depend on the OS
    We don't need it for bookworm (which will run in Cloud VPS)
    
    Bug: T367487
    Change-Id: I87f9031f89902e7a9e0b4d9a23ae2cd1b39ee398

--- modules/profile/manifests/idp/build.pp
+++ modules/profile/manifests/idp/build.pp
@@ -17,24 +17,25 @@ class profile::idp::build {
 
     if debian::codename::eq('bullseye') {
         ensure_packages(['default-jdk-headless'])
+
+        # Set up an rsync module to allow easy copying of the built DEB
+        class { 'rsync::server': }
+        rsync::server::module { 'cas-build-result':
+            path => '/srv/cas-build/',
+        }
+
+        firewall::service { 'cas_build_rsync':
+            proto  => 'tcp',
+            port   => [873],
+            srange => wmflib::role::hosts('apt_repo'),
+        }
+
+        profile::auto_restarts::service { 'rsync': }
+
     } else {
         apt::package_from_component { 'openjdk-21':
             component => 'component/jdk21',
             packages  => ['openjdk-21-jre', 'openjdk-21-jre-headless', 'openjdk-21-jdk', 'openjdk-21-jdk-headless'],
         }
     }
-
-    # Set up an rsync module to allow easy copying of the built DEB
-    class { 'rsync::server': }
-    rsync::server::module { 'cas-build-result':
-        path => '/srv/cas-build/',
-    }
-
-    firewall::service { 'cas_build_rsync':
-        proto  => 'tcp',
-        port   => [873],
-        srange => wmflib::role::hosts('apt_repo'),
-    }
-
-    profile::auto_restarts::service { 'rsync': }
 }
