69de7c692bee0acd207babe90b0629cde122931f
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-06-20 11:39:14 +0200

    Remove stat1004-1007 from site.pp
    Bug: T353785
    Change-Id: Id696015c686931a71afff671dd2a951539640320

--- manifests/site.pp
+++ manifests/site.pp
@@ -2606,13 +2606,6 @@ node /^mirror1001\.wikimedia\./ {
     role(mirrors)
 }
 
-# These hosts are to be decommisisoned, but we wish to leave them
-# in setup mode for a week or so, in case we need to retrieve any data
-# See #T353785 for more information.
-node /^stat100[4-7]\.eqiad\./ {
-    role(insetup::buster)
-}
-
 # The hosts contain all the tools and libraries to access
 # the Analytics Cluster services.
 node /^stat10(0[8-9]|1[0-1])\.eqiad\./ {
