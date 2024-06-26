a41ff50a45be2956f8368019f2fc43e3f0cc5ca7
Ottomata <aotto@wikimedia.org>
2024-06-26 13:32:04 -0400

    Disable varnish handling of /beacon/event to decommission eventlogging backend
    - varnishkafka-eventlogging will be removed from all frontends
    - varnish will no longer handle /beacon/event requests, and instead pass thru to backend.
    
    NOTE: This was previous done before in Ibbdacb4082bdcbe5980119305c08de0d9e66cc70,
    but reverted in I2d6964a722fec40fbd22e0239645048fa661d991 due to a bug.
    
    The bug is fixed!
    
    Bug: T238230
    Bug: T353817
    Depends-On: Ib7a071fd139a803faef2dc266ef107e49ad7729c
    Change-Id: I04655cb6a5b7d2756b3959ccb243e9fe98d3f08a

--- modules/profile/manifests/cache/varnish/frontend.pp
+++ modules/profile/manifests/cache/varnish/frontend.pp
@@ -28,7 +28,12 @@
 # @param fe_mem_gb_reserved Frontend memory cache size will be set to total host memory minus this many GB (def 170)
 # @param check_min_fe_mem Whether we should fail compilation if the computed frontend memory cache size is below a certain limit (def 150 GB)
 # @param check_min_fe_mem_value If the computed frontend memory cache size is below this value and check_min_fe_mem is set, fail Puppet compilation
-# @param fe_beacon_uri_regex URI path regex to use when intercepting '/beacon' URIs to return a synthetic 204.  For e.g. /beacon/event, /beacon/statsv, etc.  Default: matches /beacon/*.  If undefined, this feature will be disabled.
+# @param fe_beacon_uri_regex
+#   URI path regex to use when intercepting '/beacon' URIs to return a synthetic 204.
+#   For e.g. /beacon/statsv, etc.  If undefined, this feature will be disabled.
+#   The default matches everything except /beacon/event, as this endpoint has been
+#   removed as part of https://phabricator.wikimedia.org/T238230.
+#   Default: '^/beacon\/(?!event)[^/?]+'
 class profile::cache::varnish::frontend (
     # Globals
     String                  $conftool_prefix         = lookup('conftool_prefix'),
@@ -63,7 +68,7 @@ class profile::cache::varnish::frontend (
     Optional[Integer]       $fe_mem_gb_reserved      = lookup('profile::cache::varnish::frontend::fe_mem_gb_reserved', {'default_value' => 170}),
     Boolean                 $check_min_fe_mem        = lookup('profile::cache::varnish::frontend::check_min_fe_mem', {'default_value' => false}),
     Optional[Integer]       $check_min_fe_mem_value  = lookup('profile::cache::varnish::frontend::check_min_fe_mem_value', {'default_value' => 150}),
-    Optional[String]        $fe_beacon_uri_regex     = lookup('profile::cache::varnish::frontend::fe_beacon_uri_regex', {'default_value' => '^/beacon\/[^/?]+'})
+    Optional[String]        $fe_beacon_uri_regex     = lookup('profile::cache::varnish::frontend::fe_beacon_uri_regex', {'default_value' => '^/beacon\/(?!event)[^/?]+'})
 ) {
     include profile::cache::base
     $wikimedia_nets = $profile::cache::base::wikimedia_nets
