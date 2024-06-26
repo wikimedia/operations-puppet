6e0c63dea068656a22843fdbd975beac2db18685
JHathaway <jhathaway@wikimedia.org>
2024-06-26 14:26:36 +0000

    Revert "postfix: always send local mail to smarthosts"
    This reverts commit 39012a762f9bd99d59cc19c5977f2511b6f9c7fd.
    
    Reason for revert: This change had unintended consequences, namely
    unqualified mail was qualified with the local hostname and relayed.
    
    Bug: T325407
    Hosts: mx-in1001.wikimedia.org, mx-out1001.wikimedia.org
    Change-Id: I4f38b5690fbce8103e454aa13594816585931d5c

--- modules/profile/manifests/postfix/mx.pp
+++ modules/profile/manifests/postfix/mx.pp
@@ -57,13 +57,15 @@ class profile::postfix::mx (
     Optional[Profile::Postfix::Mail_aliases] $mail_aliases           = lookup('profile::postfix::mx::mail_aliases', {'default_value' => undef}),
     Optional[Profile::Postfix::Verp]         $verp_config            = lookup('profile::postfix::mx::verp_config', {'default_value' => undef}),
     Profile::Postfix::Mta_mode               $mta_mode               = lookup('profile::postfix::mx::mta_mode', {'default_value' => 'null-client'}),
-    Array[Stdlib::Fqdn]                      $smarthosts             = lookup('profile::mail::default_mail_relay::smarthosts'),
 ) {
     $trusted_networks_filtered = $trusted_networks.filter |$x| {
         $x !~ /127.0.0.0|::1/
     }
 
     $domain_aliases_maps = $domain_aliases.map |$domain, $_| {
+        "hash:/etc/postfix/aliases/virtual-${domain}"
+    }
+    $local_mail_maps = [ $facts['fqdn'] ].map |$domain| {
         "hash:/etc/postfix/aliases/virtual-${domain}"
     }
     $domain_aliases_generic_maps = $domain_aliases_generic.map |$domain| {
@@ -126,18 +128,17 @@ class profile::postfix::mx (
         # Add $myhostname, so we can relay local mail
         relay_domains                => get($config, 'relay_domains', []) +
                                         '$myhostname',
-        smtp_generic_maps            => ['hash:/etc/postfix/aliases/generic-local'],
-        transport_maps               => $static_transport_maps_paths +
-                                        $dynamic_transport_maps_paths +
-                                        ['hash:/etc/postfix/transport-local'],
+        virtual_alias_maps           => $domain_aliases_maps +
+                                        $domain_aliases_generic_maps +
+                                        $local_mail_maps,
     }
 
-    if length($domain_aliases_maps + $domain_aliases_generic_maps) > 0 {
-        $virtual_alias_maps = {
-            virtual_alias_maps => $domain_aliases_maps + $domain_aliases_generic_maps
+    if length($static_transport_maps_paths + $dynamic_transport_maps) > 0 {
+        $transport_maps = {
+            transport_maps => $static_transport_maps_paths + $dynamic_transport_maps_paths
         }
     } else {
-        $virtual_alias_maps = {}
+        $transport_maps = {}
     }
 
     if 'virtual_alias_maps' in $config {
@@ -246,7 +247,7 @@ class profile::postfix::mx (
         # *NOTE*: the order matters, for example $dovecot_config may overwrite
         # params in the $base_config
         * => $config + $base_config + $tls_config + $dovecot_config +
-             $virtual_alias_maps
+             $transport_maps
     }
     # lint:endignore
 
@@ -518,14 +519,13 @@ class profile::postfix::mx (
         mode   => '0755',
     }
 
-    # lookup database for local mail
+    # lookup database for local_mail_maps
     postfix::lookup::database {
-    '/etc/postfix/aliases/generic-local':
-        content => @("EOF"),
-                   # Redirect all local mail to root@wikimedia.org
-                   # regardless of user
-                   @${facts['fqdn']} root@wikimedia.org
-                   | EOF
+    "/etc/postfix/aliases/virtual-${facts['fqdn']}":
+        content => epp(
+            'profile/postfix/mx/generic_aliases.epp',
+            { domain => $facts['fqdn'] }
+        )
     }
 
     $domain_aliases_generic.each |$domain| {
@@ -548,11 +548,5 @@ class profile::postfix::mx (
         postfix::lookup::database { "/etc/postfix/transport-${name}":
             content => $data['content']
         }
-    }
-
-    postfix::lookup::database { '/etc/postfix/transport-local':
-        content => @("EOF"),
-                   ${facts['fqdn']} smtp:${join($smarthosts, ', ')}
-                   | EOF
     }
 }
