b91096113d18213de49d5baff059504ed3bf67ec
Andrew Bogott <abogott@wikimedia.org>
2024-07-02 14:34:35 -0500

    profile::toolforge::elasticsearch::keepalived: keepalived interface from hiera
    We can't just assume eth0 in modern distros.
    
    Bug: T311905
    Change-Id: Ief590942857da4b21ec88b348d7b7153e0b23bc5

--- modules/profile/manifests/toolforge/elasticsearch/keepalived.pp
+++ modules/profile/manifests/toolforge/elasticsearch/keepalived.pp
@@ -3,13 +3,14 @@ class profile::toolforge::elasticsearch::keepalived(
     Integer $host_priority = lookup('profile::toolforge::elasticsearch::keepalived::host_priority'),
     Stdlib::IP::Address $vip = lookup('profile::toolforge::elasticsearch::keepalived::vip'),
     String $auth_pass = lookup('profile::toolforge::elasticsearch::keepalived::password'),
+    String $keepalived_interface = lookup('profile::toolforge::elasticsearch::keepalived::interface'),
 ) {
     $peers = delete($elastic_settings['cluster_hosts'], $::fqdn)
 
     class { 'keepalived':
         auth_pass         => $auth_pass,
         default_state     => 'BACKUP',
-        interface         => 'eth0',
+        interface         => $keepalived_interface,
         peers             => $peers,
         priority          => $host_priority,
         vips              => [$vip],
