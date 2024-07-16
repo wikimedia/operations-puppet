306bf44d0ca105ac904e645cf1f6d9527c4a1234
Brian King <bking@wikimedia.org>
2024-07-16 09:10:11 -0500

    relforge: remove non-functional TLS termination changes
    Back out of some failed patches and go back to nominal
    for TLS termination in the Elastic testing cluster (relforge).
    
    Hosts: relforge1003.eqiad.wmnet,cloudelastic1008.eqiad.wmnet
    Hosts: elastic2090.codfw.wmnet
    Bug: T368950
    Change-Id: Ic5cec4c996e1dca92d6442ef1960f6b7f01399b2

--- modules/elasticsearch/manifests/tlsproxy.pp
+++ modules/elasticsearch/manifests/tlsproxy.pp
@@ -26,36 +26,17 @@ define elasticsearch::tlsproxy (
     Boolean                           $enable_http2      = false,
     Hash[String[1], Stdlib::Unixpath] $cfssl_paths       = {}
 ) {
-    # temporary change to test envoy migration, see T368950
-    if $server_name =~ /relforge/ {
-        $envoy_tls_port = 10000 + $tls_port
-        $ssl_paths = profile::pki::get_cert('discovery', "${facts['fqdn']}-${envoy_tls_port}", {
-            'owner'           => 'envoy',
-            'outdir'          => '/etc/envoy/ssl',
-            'hosts'           => [$facts['hostname'], $facts['fqdn']],
-            'notify' => Service['envoyproxy.service'],
-        })
-        envoyproxy::tls_terminator { String($envoy_tls_port):
-            upstreams    => [{
-            server_names => ['*'],
-            upstream     => {
-                port => $upstream_port,
-        }
-    }]
-}
-    } else {
-        tlsproxy::localssl { $title:
-            server_name       => $server_name,
-            server_aliases    => $server_aliases,
-            default_server    => true,
-            acme_chief        => $acme_chief,
-            acme_certname     => $acme_certname,
-            upstream_ports    => [$upstream_port],
-            tls_port          => $tls_port,
-            only_get_requests => $read_only,
-            enable_http2      => $enable_http2,
-            cfssl_paths       => $cfssl_paths,
-        }
+    tlsproxy::localssl { $title:
+        server_name       => $server_name,
+        server_aliases    => $server_aliases,
+        default_server    => true,
+        acme_chief        => $acme_chief,
+        acme_certname     => $acme_certname,
+        upstream_ports    => [$upstream_port],
+        tls_port          => $tls_port,
+        only_get_requests => $read_only,
+        enable_http2      => $enable_http2,
+        cfssl_paths       => $cfssl_paths,
     }
     $check_command = $acme_chief ? {
         true    => 'check_ssl_on_port_letsencrypt',
