91feff4823b34537142397277bc4c6568ae509ce
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2024-07-25 09:49:52 +0200

    haproxy: add confd_file define
    Bug: T370745
    Change-Id: If86584c9ef38d23c21e70afe9894b96c8982ab63

---
+++ modules/haproxy/manifests/confd_site.pp
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: Apache-2.0
+# @summary Defines a haproxy site configuration file managed by confd
+# @param ensure Whether the file should exist or not
+# @param content The content of the confd go template, as a string
+# @param watch_keys List of keys to watch relative to the value assigned in $prefix
+# @param prefix Prefix to use for all keys; it will actually be joined with the global confd prefix
+
+define haproxy::confd_site (
+    Wmflib::Ensure $ensure,
+    String $content,
+    Array[String[1]] $watch_keys,
+    String $prefix = '',
+) {
+    $safe_title = regsubst($title, '[^a-zA-Z0-9]', '_', 'G')
+    confd::file { "confd_site_${safe_title}":
+        ensure     => $ensure,
+        content    => $content,
+        target     => "/etc/haproxy/conf.d/${safe_title}.cfg",
+        watch_keys => $watch_keys,
+        prefix     => $prefix,
+        check      => '/usr/sbin/haproxy -c -V -f /etc/haproxy/haproxy.cfg -f {{.src}}',
+        reload     => '/usr/bin/systemctl reload haproxy.service',
+        before     => Service['haproxy'],
+    }
+}
