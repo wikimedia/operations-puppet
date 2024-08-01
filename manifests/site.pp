1acb16b24fa3f04a49f44666720754a5288b6498
Arnold Okoth <aokoth@wikimedia.org>
2024-08-01 19:55:12 +0300

    site: add setup roles for vrts hardware
    Bug: T369672
    Bug: T369674
    Change-Id: Ied96f2645c052909eb269dde77838e7a67feaa14

--- manifests/site.pp
+++ manifests/site.pp
@@ -2323,9 +2323,17 @@ node /^vrts1001\.eqiad\./ {
     role(vrts)
 }
 
+node /^vrts1003\.eqiad\./ {
+    role(insetup::collaboration_services)
+}
+
 # T323515: WIP
 node /^vrts2001\.codfw\./ {
     role(vrts)
+}
+
+node /^vrts2002\.codfw\./ {
+    role(insetup::collaboration_services)
 }
 
 # T363399 - replaces scandium and or testreduce*
