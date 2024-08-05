a8e941e4cdb0f856b920bc0263237015bb1ba8f4
Arzhel Younsi <ayounsi@wikimedia.org>
2024-08-05 10:20:20 +0200

    Postgres prom exporter: ignore queries.yaml on >= bookworm
    Not needed and breaks the exporter on 1.11 (bookworm) and fully removed
    in later versions.
    
    Hosts: C:prometheus::postgres_exporter
    Change-Id: I529af6674de84a62692a3292e64678c1f86dbc50

--- modules/prometheus/manifests/postgres_exporter.pp
+++ modules/prometheus/manifests/postgres_exporter.pp
@@ -6,6 +6,24 @@ class prometheus::postgres_exporter(
     $prometheus_db = 'postgres',
     $ensure='present'
 ) {
+    # Bookworm ships the exporter v1.11 which includes the pg_database exporter out of the box
+    # and causes an error if re-defined here
+    # In more recent releases, the exporter fully decommissioned queries.yaml
+    if debian::codename::ge('bookworm') {
+        $extended_query = false
+    } else {
+        $extended_query = true
+
+        file { '/etc/postgres-prometheus-exporter-queries.yaml':
+            ensure => $ensure,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0444',
+            source => 'puppet:///modules/prometheus/postgres/postgres-prometheus-exporter-queries.yaml',
+            notify => Service['prometheus-postgres-exporter'],
+        }
+    }
+
     file { '/etc/default/prometheus-postgres-exporter':
         ensure  => $ensure,
         owner   => 'root',
@@ -14,30 +32,16 @@ class prometheus::postgres_exporter(
         content => template('prometheus/prometheus-postgres-exporter.default.erb'),
     }
 
-    file { '/etc/postgres-prometheus-exporter-queries.yaml':
-        ensure => $ensure,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => 'puppet:///modules/prometheus/postgres/postgres-prometheus-exporter-queries.yaml',
-    }
-
     package { 'prometheus-postgres-exporter':
         ensure  => $ensure,
-        require => [
-            File['/etc/default/prometheus-postgres-exporter'],
-            File['/etc/postgres-prometheus-exporter-queries.yaml'],
-        ],
+        require => File['/etc/default/prometheus-postgres-exporter'],
     }
 
     service { 'prometheus-postgres-exporter':
         ensure    => running,
         enable    => true,
         require   => Package['prometheus-postgres-exporter'],
-        subscribe => [
-            File['/etc/default/prometheus-postgres-exporter'],
-            File['/etc/postgres-prometheus-exporter-queries.yaml'],
-        ],
+        subscribe => File['/etc/default/prometheus-postgres-exporter'],
     }
 
     profile::auto_restarts::service { 'prometheus-postgres-exporter': }
