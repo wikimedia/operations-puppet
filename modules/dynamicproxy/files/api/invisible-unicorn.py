853211f35c2083bb6966d3bf60037f124d7d8918
Andrew Bogott <abogott@wikimedia.org>
2024-08-06 09:29:58 -0500

    nova-proxy api (invisible-unicorn.py): run through black
    Prep for future edits.
    
    Change-Id: I005d6384cbdd6c4f19e03319e1e7ff00e9390ff7

--- modules/dynamicproxy/files/api/invisible-unicorn.py
+++ modules/dynamicproxy/files/api/invisible-unicorn.py
@@ -41,16 +41,16 @@ from oslo_context import context
 from oslo_policy import policy
 from werkzeug.exceptions import HTTPException
 
-cfgGroup = cfg.OptGroup('dynamicproxy')
+cfgGroup = cfg.OptGroup("dynamicproxy")
 opts = [
-    cfg.StrOpt('dns_updater_keystone_api_url'),
-    cfg.StrOpt('dns_updater_username'),
-    cfg.StrOpt('dns_updater_password', secret=True),
-    cfg.StrOpt('dns_updater_project'),
-    cfg.StrOpt('zones_json_file'),
-    cfg.StrOpt('proxy_dns_ipv4'),
-    cfg.StrOpt('sqlalchemy_uri', secret=True),
-    cfg.StrOpt('redis_uri'),
+    cfg.StrOpt("dns_updater_keystone_api_url"),
+    cfg.StrOpt("dns_updater_username"),
+    cfg.StrOpt("dns_updater_password", secret=True),
+    cfg.StrOpt("dns_updater_project"),
+    cfg.StrOpt("zones_json_file"),
+    cfg.StrOpt("proxy_dns_ipv4"),
+    cfg.StrOpt("sqlalchemy_uri", secret=True),
+    cfg.StrOpt("redis_uri"),
 ]
 
 key = FlaskKeystone()
@@ -59,30 +59,32 @@ log = OsloLog()
 cfg.CONF.register_group(cfgGroup)
 cfg.CONF.register_opts(opts, group=cfgGroup)
 
-cfg.CONF(default_config_files=['/etc/dynamicproxy-api/config.ini'])
+cfg.CONF(default_config_files=["/etc/dynamicproxy-api/config.ini"])
 
 enforcer = policy.Enforcer(cfg.CONF)
-enforcer.register_defaults([
-    policy.RuleDefault('admin', 'role:admin'),
-    policy.RuleDefault('admin_or_member', 'rule:admin or role:member'),
-    policy.RuleDefault('proxy:zones:index', ''),
-    policy.RuleDefault('proxy:zones:use_deprecated', 'rule:admin'),
-    policy.RuleDefault('proxy:index', ''),
-    policy.RuleDefault('proxy:view', ''),
-    policy.RuleDefault('proxy:create', 'rule:admin_or_member'),
-    policy.RuleDefault('proxy:update', 'rule:admin_or_member'),
-    policy.RuleDefault('proxy:delete', 'rule:admin_or_member'),
-])
+enforcer.register_defaults(
+    [
+        policy.RuleDefault("admin", "role:admin"),
+        policy.RuleDefault("admin_or_member", "rule:admin or role:member"),
+        policy.RuleDefault("proxy:zones:index", ""),
+        policy.RuleDefault("proxy:zones:use_deprecated", "rule:admin"),
+        policy.RuleDefault("proxy:index", ""),
+        policy.RuleDefault("proxy:view", ""),
+        policy.RuleDefault("proxy:create", "rule:admin_or_member"),
+        policy.RuleDefault("proxy:update", "rule:admin_or_member"),
+        policy.RuleDefault("proxy:delete", "rule:admin_or_member"),
+    ]
+)
 
 app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = cfg.CONF.dynamicproxy.sqlalchemy_uri
+app.config["SQLALCHEMY_DATABASE_URI"] = cfg.CONF.dynamicproxy.sqlalchemy_uri
 
 db = SQLAlchemy(
     app,
     engine_options={
-        'pool_recycle': 1800,
-        'pool_pre_ping': True,
-    }
+        "pool_recycle": 1800,
+        "pool_pre_ping": True,
+    },
 )
 
 key.init_app(app)
@@ -96,6 +98,7 @@ class Project(db.Model):
 
     Not represented at the Redis level at all
     """
+
     id = db.Column(db.Integer, primary_key=True)
     openstack_id = db.Column(db.String(256), unique=True)
 
@@ -104,55 +107,60 @@ class Route(db.Model):
     """Represents a route that has one matching rule & multiple backends
 
     Currently the only supported rule is to match entire domains"""
+
     id = db.Column(db.Integer, primary_key=True)
     domain = db.Column(db.String(256), unique=True)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
-    project = db.relationship('Project',
-                              backref=db.backref('routes', lazy='dynamic'))
+    project_id = db.Column(db.Integer, db.ForeignKey("project.id"))
+    project = db.relationship("Project", backref=db.backref("routes", lazy="dynamic"))
 
 
 class Backend(db.Model):
     """Represents a backend that can have HTTP requests routed to it
 
     Usually has a URL that is of the form <protocol>://<hostname>:<port>"""
+
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(256))
-    route_id = db.Column(db.Integer, db.ForeignKey('route.id'))
+    route_id = db.Column(db.Integer, db.ForeignKey("route.id"))
     route = db.relationship(
-        'Route',
-        backref=db.backref('backends', lazy='dynamic', cascade='all, delete-orphan')
+        "Route",
+        backref=db.backref("backends", lazy="dynamic", cascade="all, delete-orphan"),
     )
 
 
 class RedisStore:
     """Represents a redis instance that has routing info that the proxy reads"""
+
     def __init__(self, redis_conn):
         self.redis = redis_conn
 
     def delete_route(self, route: Route):
-        self.redis.delete('frontend:' + route.domain)
+        self.redis.delete("frontend:" + route.domain)
 
     # Create this route if it does not already exist.
     def refresh_route(self, route: Route):
-        key = 'frontend:' + route.domain
+        key = "frontend:" + route.domain
         if not (self.redis.exists(key)):
             print("Adding new key: %s " % key)
             self.update_route(route)
 
     def update_route(self, route: Route, old_domain=None):
-        key = 'frontend:' + route.domain
+        key = "frontend:" + route.domain
         backends = [backend.url for backend in route.backends]
 
         pipeline = self.redis.pipeline()
         if old_domain:
             # When domains get renamed, kill old one too
-            pipeline.delete('frontend:' + old_domain)
+            pipeline.delete("frontend:" + old_domain)
         pipeline.delete(key).sadd(key, *backends).execute()
 
 
 class Dns:
     """Deals with any DNS writes."""
-    def __init__(self, zones: dict, target_ipv4: str, clients: mwopenstackclients.Clients):
+
+    def __init__(
+        self, zones: dict, target_ipv4: str, clients: mwopenstackclients.Clients
+    ):
         self.zones = zones
         self.target_ipv4 = target_ipv4
         self.clients = clients
@@ -172,10 +180,9 @@ class Dns:
         if hostname_parent in self.zones:
             if self.zones[hostname_parent].get("deprecated", False):
                 enforce_policy("proxy:zones:use_deprecated", project)
-            if (
-                self.zones[hostname_parent]["project"] != project
-                and not self.zones[hostname_parent].get("shared", False)
-            ):
+            if self.zones[hostname_parent]["project"] != project and not self.zones[
+                hostname_parent
+            ].get("shared", False):
                 log.logger.warning(
                     "Rejecting project %s from using non-shared zone %s in %s",
                     project,
@@ -183,9 +190,8 @@ class Dns:
                     self.zones[hostname_parent]["project"],
                 )
                 return None
-            if (
-                hostname.startswith("*.")
-                and self.zones[hostname_parent].get("shared", False)
+            if hostname.startswith("*.") and self.zones[hostname_parent].get(
+                "shared", False
             ):
                 log.logger.warning(
                     "Rejecting project %s from using wildcard in shared zone %s",
@@ -204,12 +210,14 @@ class Dns:
             return (
                 hostname,
                 self.zones[hostname_parent]["project"],
-                self.zones[hostname_parent]["id"]
+                self.zones[hostname_parent]["id"],
             )
 
         log.logger.info(
             "Did not find zone for hostname %s (parent %s, supported zones %s)",
-            hostname, hostname_parent, ', '.join(self.zones.keys())
+            hostname,
+            hostname_parent,
+            ", ".join(self.zones.keys()),
         )
         return None
 
@@ -223,13 +231,14 @@ class Dns:
         client = self.designateclient(project)
 
         existing_records = client.recordsets.list(
-            zone_id,
-            criterion={"name": hostname, "type": "A"}
+            zone_id, criterion={"name": hostname, "type": "A"}
         )
         if len(existing_records) != 0:
             log.logger.info(
                 "Rejecting can_use_hostname (%s %s), found existing records: %s",
-                project, hostname, ", ".join([record["name"] for record in existing_records])
+                project,
+                hostname,
+                ", ".join([record["name"] for record in existing_records]),
             )
 
             return False
@@ -240,14 +249,18 @@ class Dns:
         hostname, project, zone_id = self.get_zone(project, hostname)
         client = self.designateclient(project)
 
-        if not client.recordsets.list(zone_id, criterion={"name": hostname, "type": "A"}):
+        if not client.recordsets.list(
+            zone_id, criterion={"name": hostname, "type": "A"}
+        ):
             client.recordsets.create(zone_id, hostname, "A", [self.target_ipv4])
 
     def delete_records_for(self, project: str, hostname: str):
         hostname, project, zone_id = self.get_zone(project, hostname)
         client = self.designateclient(project)
 
-        a_recordsets = client.recordsets.list(zone_id, criterion={"name": hostname, "type": "A"})
+        a_recordsets = client.recordsets.list(
+            zone_id, criterion={"name": hostname, "type": "A"}
+        )
         if a_recordsets:
             client.recordsets.delete(zone_id, a_recordsets[0]["id"])
 
@@ -264,13 +277,13 @@ dns = Dns(
         password=cfg.CONF.dynamicproxy.dns_updater_password,
         project=cfg.CONF.dynamicproxy.dns_updater_project,
         url=cfg.CONF.dynamicproxy.dns_updater_keystone_api_url,
-    )
+    ),
 )
 
 
 class Forbidden(HTTPException):
     code = 403
-    description = 'Forbidden.'
+    description = "Forbidden."
 
 
 def is_valid_domain(hostname):
@@ -297,12 +310,15 @@ def is_valid_domain(hostname):
 
 
 def environify_header_name(name):
-    return "HTTP_{}".format(name.upper().replace('-', '_'))
+    return "HTTP_{}".format(name.upper().replace("-", "_"))
 
 
 def enforce_policy(rule, project_id):
     # headers in a specific format that oslo.context wants
-    headers = {environify_header_name(name): value for name, value in flask.request.headers.items()}
+    headers = {
+        environify_header_name(name): value
+        for name, value in flask.request.headers.items()
+    }
     ctx = context.RequestContext.from_environ(headers)
 
     # if the project in the url is for a different project than what
@@ -310,30 +326,34 @@ def enforce_policy(rule, project_id):
     if ctx.project_id != project_id:
         log.logger.warning(
             "Encountered project id %s but keystone token was for project %s",
-            project_id, ctx.project_id
+            project_id,
+            ctx.project_id,
         )
         raise Forbidden("Invalid project id.")
 
     log.logger.info(
         "Enforcing policy %s for user %s (%s) and project %s",
-        rule, ctx.user_id, ', '.join(ctx.roles), ctx.project_id
+        rule,
+        ctx.user_id,
+        ", ".join(ctx.roles),
+        ctx.project_id,
     )
 
     enforcer.authorize(
         rule,
-        {'project_id': project_id},
+        {"project_id": project_id},
         ctx,
         do_raise=True,
         exc=Forbidden,
     )
 
 
-@app.route('/v1/<project_id>/zones', methods=['GET'])
+@app.route("/v1/<project_id>/zones", methods=["GET"])
 def list_zones(project_id):
-    enforce_policy('proxy:zones:index', project_id)
+    enforce_policy("proxy:zones:index", project_id)
 
     try:
-        enforce_policy('proxy:zones:use_deprecated', project_id)
+        enforce_policy("proxy:zones:use_deprecated", project_id)
     except Forbidden:
         use_deprecated = False
     else:
@@ -355,33 +375,39 @@ def list_zones(project_id):
     return flask.jsonify(data)
 
 
-@app.route('/v1/<project_id>/mapping', methods=['GET'])
+@app.route("/v1/<project_id>/mapping", methods=["GET"])
 def all_mappings(project_id):
-    enforce_policy('proxy:index', project_id)
+    enforce_policy("proxy:index", project_id)
 
     project = Project.query.filter_by(openstack_id=project_id).first()
-    data = {'routes': []}
+    data = {"routes": []}
 
     if project:
         for route in project.routes:
-            data['routes'].append({
-                'domain': route.domain,
-                'backends': [backend.url for backend in route.backends]
-            })
+            data["routes"].append(
+                {
+                    "domain": route.domain,
+                    "backends": [backend.url for backend in route.backends],
+                }
+            )
 
     return flask.jsonify(**data)
 
 
-@app.route('/v1/<project_id>/mapping', methods=['PUT'])
+@app.route("/v1/<project_id>/mapping", methods=["PUT"])
 def create_mapping(project_id):
     data = flask.request.get_json(True)
 
-    if 'domain' not in data or 'backends' not in data or not isinstance(data['backends'], list):
+    if (
+        "domain" not in data
+        or "backends" not in data
+        or not isinstance(data["backends"], list)
+    ):
         return "Valid JSON but invalid format. Needs domain string and backends array"
-    domain = data['domain']
+    domain = data["domain"]
     if not is_valid_domain(domain):
         return "Invalid domain", 400
-    backend_urls = data['backends']
+    backend_urls = data["backends"]
 
     project = Project.query.filter_by(openstack_id=project_id).first()
     if project is None:
@@ -390,7 +416,7 @@ def create_mapping(project_id):
 
     route = Route.query.filter_by(domain=domain).first()
     if route is None:
-        enforce_policy('proxy:create', project_id)
+        enforce_policy("proxy:create", project_id)
         if not dns.can_use_hostname(project_id, domain):
             return flask.jsonify({"error": f"Can't use domain {domain}"}), 403
 
@@ -400,7 +426,7 @@ def create_mapping(project_id):
     elif route.project_id != project.id:
         return "Can't edit backend of another project", 403
     else:
-        enforce_policy('proxy:update', project_id)
+        enforce_policy("proxy:update", project_id)
 
     for backend_url in backend_urls:
         # FIXME: Add validation for making sure these are valid
@@ -414,9 +440,9 @@ def create_mapping(project_id):
     return "", 200
 
 
-@app.route('/v1/<project_id>/mapping/<domain>', methods=['DELETE'])
+@app.route("/v1/<project_id>/mapping/<domain>", methods=["DELETE"])
 def delete_mapping(project_id, domain):
-    enforce_policy('proxy:delete', project_id)
+    enforce_policy("proxy:delete", project_id)
 
     project = Project.query.filter_by(openstack_id=project_id).first()
     if project is None:
@@ -436,9 +462,9 @@ def delete_mapping(project_id, domain):
     return "deleted", 200
 
 
-@app.route('/v1/<project_id>/mapping/<domain>', methods=['GET'])
+@app.route("/v1/<project_id>/mapping/<domain>", methods=["GET"])
 def get_mapping(project_id, domain):
-    enforce_policy('proxy:view', project_id)
+    enforce_policy("proxy:view", project_id)
 
     project = Project.query.filter_by(openstack_id=project_id).first()
     if project is None:
@@ -448,18 +474,21 @@ def get_mapping(project_id, domain):
     if route is None:
         return "No such domain", 400
 
-    data = {'domain': route.domain, 'backends': [backend.url for backend in route.backends]}
+    data = {
+        "domain": route.domain,
+        "backends": [backend.url for backend in route.backends],
+    }
 
     return flask.jsonify(**data)
 
 
-@app.route('/v1/<project_id>/mapping/<domain>', methods=['POST'])
+@app.route("/v1/<project_id>/mapping/<domain>", methods=["POST"])
 def update_mapping(project_id, domain):
     project = Project.query.filter_by(openstack_id=project_id).first()
     if project is None:
         return "No such domain", 400
 
-    enforce_policy('proxy:update', project_id)
+    enforce_policy("proxy:update", project_id)
 
     route = Route.query.filter_by(project=project, domain=domain).first()
     if route is None:
@@ -467,13 +496,20 @@ def update_mapping(project_id, domain):
 
     data = flask.request.get_json(True)
 
-    if 'domain' not in data or 'backends' not in data or not isinstance(data['backends'], list):
-        return "Valid JSON but invalid format. Needs domain string and backends array", 400
+    if (
+        "domain" not in data
+        or "backends" not in data
+        or not isinstance(data["backends"], list)
+    ):
+        return (
+            "Valid JSON but invalid format. Needs domain string and backends array",
+            400,
+        )
 
-    new_domain = data['domain']
+    new_domain = data["domain"]
     if not is_valid_domain(new_domain):
         return "Invalid domain", 400
-    backend_urls = data['backends']
+    backend_urls = data["backends"]
 
     if route.domain != new_domain:
         route.domain = new_domain
@@ -503,5 +539,5 @@ def update_redis_from_db():
 update_redis_from_db()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
