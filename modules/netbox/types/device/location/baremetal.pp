185a6ad97671d640c47238ed24b2a68137fa6302
Ayounsi <ayounsi@wikimedia.org>
2024-08-07 08:19:35 +0000

    Revert "Add role to type Netbox::Device::Location::BareMetal"
    This reverts commit 026ab1332bc4303fe84c1269bbfa7c156605973d.
    
    Change-Id: Ie73c5e8e25201779f44f9f7bef7d524e672c33b5

--- modules/netbox/types/device/location/baremetal.pp
+++ modules/netbox/types/device/location/baremetal.pp
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 type Netbox::Device::Location::BareMetal = Struct[{
     site    => Wmflib::Sites,
-    role    => String[2],
     row     => String[2],
     rack    => String[2],
 }]
