026ab1332bc4303fe84c1269bbfa7c156605973d
Arzhel Younsi <ayounsi@wikimedia.org>
2024-08-07 10:08:40 +0200

    Add role to type Netbox::Device::Location::BareMetal
    Hosts: prometheus1005.eqiad.wmnet
    Bug: T368513
    Change-Id: I3a78319a315a5be96d364359df0c2e7045cd988e

--- modules/netbox/types/device/location/baremetal.pp
+++ modules/netbox/types/device/location/baremetal.pp
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 type Netbox::Device::Location::BareMetal = Struct[{
     site    => Wmflib::Sites,
+    role    => String[2],
     row     => String[2],
     rack    => String[2],
 }]
