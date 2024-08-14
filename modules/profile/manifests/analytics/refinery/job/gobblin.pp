e2ff7ed0e25aa8fcb87db1bd91faf457afae9fae
Gabriele Modena <gmodena@wikimedia.org>
2024-08-14 10:12:44 +0200

    gobblin: remove webrequest_frontend ingestion job.
    Data is not produced into Kafka anymore, and batch ingestion
    can be disabled.
    
    Bug: T372456
    Change-Id: Ibe8e9011cefc6bdcf0a663aa007b4eff130ed026

--- modules/profile/manifests/analytics/refinery/job/gobblin.pp
+++ modules/profile/manifests/analytics/refinery/job/gobblin.pp
@@ -22,10 +22,6 @@
 #   Ingests legacy EventLogging streams. (No datacenter topic prefixes, topics start with
 #   'eventlogging_'.)
 #
-# - webrequest_frontend_rc0
-#   Ingests the composite webrequest.frontend.rc0 stream (upload and text topics) into
-#   /wmf/data/raw/webrequest_frontend_rc0.
-#
 # == Parameters
 #
 # [*gobblin_wmf_version*]
@@ -81,14 +77,5 @@ class profile::analytics::refinery::job::gobblin(
 
     profile::analytics::refinery::job::gobblin_job { 'eventlogging_legacy':
         interval         => '*-*-* *:20:00',
-    }
-
-    profile::analytics::refinery::job::gobblin_job { 'webrequest_frontend_rc0':
-        # webrequest_frontend is large. Run it every 10 minutes to keep pressure on Kafka
-        # low (pulling more often means it is more likely for data to be in Kafka's cache).
-        # The 15 minutes offset from calendar hour is to mitigate out-of-order events
-        # messing up with _IMPORTED flags generation, and runs of the webrequest gobblin job
-        # happening simulatenously.
-        interval         => '*-*-* *:8/10:00',
     }
 }
