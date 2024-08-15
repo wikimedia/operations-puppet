8bde1b2ae17acdfe8ed1702a998d22fd793e3e50
Jelto Wodstrcil <jwodstrcil@wikimedia.org>
2024-08-15 15:02:31 +0200

    profile::firewall::nftables_throttling: add option for burst packets
    This change adds the nftables "burst" option to allow a certain number
    of packets which can exceed the threshold. This should help preventing
    unwanted throttling on Gerrit.
    See https://wiki.nftables.org/wiki-nftables/index.php/Rate_limiting_matchings#Burst
    
    This change also makes naming more clear by using throttle_packets instead of
    throttle_requests.
    
    This change also removed obsolete custom throttling config for gitlab1003 which
    was used for testing.
    
    Bug: T366882
    Bug: T365259
    Change-Id: I437e46a5d3c62557cc674e95d0c8a472b5b4ad6f

--- modules/profile/manifests/firewall/nftables_throttling.pp
+++ modules/profile/manifests/firewall/nftables_throttling.pp
@@ -1,14 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 # @summary manage backup timers
-# @throttle_requests number of requests clients are allowed to do in a 1 minute time frame
+# @throttle_packets number of packets clients are allowed to do in a 1 minute time frame
 # @throttle_duration duration how long clients are banned in seconds
 # @nft_policy whether to actually drop packets or to accept them (for logging-only / testing)
 # @nft_logging whether to log firewall actions to /var/log/messages or not
 # @port tcp port which is throttled (default 443)
+# @allowed_burst_packets optional number of packets that can exceed the ratelimit:
 class profile::firewall::nftables_throttling (
     Wmflib::Ensure $ensure = lookup('profile::firewall::nftables_throttling::ensure',
     {default_value => present}),
-    Integer $throttle_requests = lookup('profile::firewall::nftables_throttling::throttle_requests',
+    Integer $throttle_packets = lookup('profile::firewall::nftables_throttling::throttle_packets',
     {default_value => 300}), # allow 300 requests per minute
     Integer $throttle_duration = lookup('profile::firewall::nftables_throttling::throttle_duration',
     {default_value => 300}), # ban clients above for 300 seconds
@@ -18,6 +19,8 @@ class profile::firewall::nftables_throttling (
     {default_value => false}),
     Integer $port = lookup('profile::firewall::nftables_throttling::port',
     {default_value => 443}),
+    Optional[Integer] $allowed_burst_packets = lookup('profile::firewall::nftables_throttling::allowed_burst_packets',
+    {default_value => undef }),
 ) {
 
     $nft_do_log = $nft_logging ? {
