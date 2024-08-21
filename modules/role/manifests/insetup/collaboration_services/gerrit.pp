2bb305ea2bf8ddcb8acfcfc28ba342587a091296
Daniel Zahn <dzahn@wikimedia.org>
2024-08-21 09:23:51 -0700

    gerrit: add firewall, java, scap, mail settings to Hiera for gerrit1004
    Step by step adding more things to the new bookworm host that
    would also be applied on a prod gerrit server.
    
    At the end minimizing the actual change when gerrit itself is added.
    
    Purpose: Testing if anything needs fixes for bookworm.
    
    Bug: T372804
    Change-Id: Icb0ba9f07d50eaab7fbf612d56df89f19231b25a

--- modules/role/manifests/insetup/collaboration_services/gerrit.pp
+++ modules/role/manifests/insetup/collaboration_services/gerrit.pp
@@ -2,5 +2,8 @@
 class role::insetup::collaboration_services::gerrit {
     include profile::base::production
     include profile::firewall
+    include profile::firewall::nftables_throttling
+    include profile::prometheus::apache_exporter
     include profile::java
+
 }
