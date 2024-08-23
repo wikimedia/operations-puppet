dcc9398dc58ddd3d6562e2d19c971ee42665597b
Andrew Bogott <abogott@wikimedia.org>
2024-08-23 09:58:03 -0500

    Remove obsolete files for openstack v. antelope
    Change-Id: I9f97b693d435fa128bc33c7b9de80e38f1b3f3d3

--- modules/openstack/manifests/clientpackages/vms/antelope/bullseye.pp
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-# this is the class for use by VM instances in Cloud VPS. Don't use for HW servers
-class openstack::clientpackages::vms::antelope::bullseye(
-) {
-    requires_realm('labs')
-}
--- modules/openstack/manifests/clientpackages/vms/antelope/buster.pp
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-# this is the class for use by VM instances in Cloud VPS. Don't use for HW servers
-class openstack::clientpackages::vms::antelope::buster(
-) {
-    requires_realm('labs')
-}
