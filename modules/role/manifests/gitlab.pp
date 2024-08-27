e94ea78b7f5374d37f05bad434531ec2fdb3fe62
jelto <jwodstrcil@wikimedia.org>
2024-08-27 14:30:21 +0200

    gitlab: add profile::prometheus::nft_throttling_denylist
    Similar to I78d7ec3fe558f36224f62421253d549842431c82 also include the
    exporter profile to GitLab. Metrics for the DENYLIST are beneficial for
    troubleshooting and finding the right threshold there as well.
    
    Bug: T366882
    Change-Id: I5b7692310c5de3b83105b5325eb403d8311899a6

--- modules/role/manifests/gitlab.pp
+++ modules/role/manifests/gitlab.pp
@@ -4,6 +4,7 @@ class role::gitlab {
     include profile::base::production
     include profile::firewall
     include profile::firewall::nftables_throttling
+    include profile::prometheus::nft_throttling_denylist
     include profile::backup::host
     include profile::gitlab
 }
