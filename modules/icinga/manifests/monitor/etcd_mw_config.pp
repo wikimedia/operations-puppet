dd21a538e54202198606aed7492f7ad08c72d90c
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2024-09-02 11:44:28 +0200

    icinga: add systemd::timer::job required parameters
    These are necessary even when ensure => absent
    
    Bug: T322523
    Change-Id: I30d3b32c5151624c8a24d57078d7f8d56caea2b9

--- modules/icinga/manifests/monitor/etcd_mw_config.pp
+++ modules/icinga/manifests/monitor/etcd_mw_config.pp
@@ -11,5 +11,10 @@ class icinga::monitor::etcd_mw_config (
 
     systemd::timer::job { 'update-etcd-mw-config-lastindex':
         ensure             => absent,
+        user               => $icinga_user,
+        description        => 'Update the etcd last modified index for MediaWiki config',
+        command            => '/usr/local/bin/update-etcd-mw-config-lastindex',
+        interval           => {'start' => 'OnUnitInactiveSec', 'interval' => '30s'},
+        monitoring_enabled => true,
     }
 }
