497dfd137950caf01d8bd6bf9bcc2daa7d07950c
jelto <jwodstrcil@wikimedia.org>
2024-09-04 14:19:36 +0200

    profile::firewall::nftables_throttling: throttle connections not packets
    This change refactors the nftables_throttling profile to throttle based
    on parallel connections instead of the number of new TCP connections.
    
    As outlined in T365259#10116300 Gerrit creates quite a lot of new TCP
    connections when pulling a repository with submodules. This would trigger
    the existing throttling implementation.
    So let's try to throttle based on parallel TCP connections. The default
    is 32 parallel connections for every IP (for both Gerrit and GitLab).
    
    The TRACKINGLIST has no timeout anymore because it only tracks active
    TCP connections. IPs are removed as soon as there is no open connection.
    
    Bug: T365259
    Change-Id: I41bafea13f304866ccf666a67fbfce0a38f07fd5

--- modules/profile/manifests/firewall/nftables_throttling.pp
+++ modules/profile/manifests/firewall/nftables_throttling.pp
@@ -1,16 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 # @summary manage backup timers
-# @throttle_packets number of packets clients are allowed to do in a 1 minute time frame
+# @max_connections number of simultaneous connections clients are allowed
 # @throttle_duration duration how long clients are banned in seconds
 # @nft_policy whether to actually drop packets or to accept them (for logging-only / testing)
 # @nft_logging whether to log firewall actions to /var/log/messages or not
 # @port tcp port which is throttled (default 443)
-# @allowed_burst_packets optional number of packets that can exceed the ratelimit:
 class profile::firewall::nftables_throttling (
     Wmflib::Ensure $ensure = lookup('profile::firewall::nftables_throttling::ensure',
     {default_value => present}),
-    Integer $throttle_packets = lookup('profile::firewall::nftables_throttling::throttle_packets',
-    {default_value => 300}), # allow 300 requests per minute
+    Integer $max_connections = lookup('profile::firewall::nftables_throttling::max_connections',
+    {default_value => 32}), # allow 32 parallel connections
     Integer $throttle_duration = lookup('profile::firewall::nftables_throttling::throttle_duration',
     {default_value => 300}), # ban clients above for 300 seconds
     Enum['accept', 'drop'] $nft_policy = lookup('profile::firewall::nftables_throttling::nft_policy',
@@ -19,8 +18,6 @@ class profile::firewall::nftables_throttling (
     {default_value => false}),
     Integer $port = lookup('profile::firewall::nftables_throttling::port',
     {default_value => 443}),
-    Optional[Integer] $allowed_burst_packets = lookup('profile::firewall::nftables_throttling::allowed_burst_packets',
-    {default_value => undef }),
 ) {
 
     $nft_do_log = $nft_logging ? {
