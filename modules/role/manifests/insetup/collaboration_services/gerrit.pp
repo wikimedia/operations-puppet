27c48c48399004e989c1f09a46a32c5e93fd6414
Daniel Zahn <dzahn@wikimedia.org>
2024-09-04 17:08:50 -0700

    gerrit: add backup::host, gerrit::migration etc to insetup role
    Add backup::host, prometheus exporter for nft throttling and
    gerrit::migration profile with rsync server setup to our temp
    insetup::gerrit role.
    
    This will further reduce the diff to production gerrit and allow
    us to start syncing data over.
    
    Bug: T372804
    Change-Id: I42e23a2a315aa5e273d1a79de33501e44a5754ce

--- modules/role/manifests/insetup/collaboration_services/gerrit.pp
+++ modules/role/manifests/insetup/collaboration_services/gerrit.pp
@@ -1,9 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 class role::insetup::collaboration_services::gerrit {
     include profile::base::production
+    include profile::backup::host
     include profile::firewall
     include profile::firewall::nftables_throttling
+    include profile::gerrit::migration
     include profile::prometheus::apache_exporter
+    include profile::prometheus::nft_throttling_denylist
     include profile::java
 
 }
