c43c0565f2c1bcc1f40ed2e9494ce9701a397421
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2024-09-06 14:45:31 +0200

    graphite: remove mw graphite-based alerts
    Moved to Prometheus/Alertmanager.
    
    Also move graphite alerts into the profile, and delete
    'graphite::monitoring::graphite'
    
    Bug: T350597
    Change-Id: Ibecfef1804c652909faa298812d0959fba981d00

--- modules/profile/manifests/graphite/production.pp
+++ modules/profile/manifests/graphite/production.pp
@@ -8,6 +8,7 @@
 class profile::graphite::production (
     Array[Stdlib::Fqdn] $graphite_hosts = lookup('profile::graphite::hosts'),
     Stdlib::Fqdn $primary_host = lookup('graphite_primary_host'),
+    Stdlib::HTTPUrl $graphite_url = lookup('graphite_url'),
 ) {
     $storage_dir = '/srv/carbon'
 
@@ -136,6 +137,56 @@ class profile::graphite::production (
 
     # alert only on the primary server
     if $::fqdn == $primary_host {
-        include profile::graphite::alerts
+        monitoring::graphite_threshold {
+            default:
+                graphite_url    => $graphite_url,
+                percentage      => 80,
+                nagios_critical => false,
+                notes_link      => 'https://wikitech.wikimedia.org/wiki/Graphite#Operations_troubleshooting';
+            'carbon-frontend-relay_drops':
+                description     => 'carbon-frontend-relay metric drops',
+                dashboard_links => [
+                    'https://grafana.wikimedia.org/d/000000020/graphite-eqiad?orgId=1&viewPanel=21',
+                    'https://grafana.wikimedia.org/d/000000337/graphite-codfw?orgId=1&viewPanel=21',
+                ],
+                metric          => 'sumSeries(transformNull(perSecond(carbon.relays.graphite*_frontend.destinations.*.dropped)))',
+                from            => '5minutes',
+                warning         => 25,
+                critical        => 100;
+            'carbon-local-relay_drops':
+                description     => 'carbon-local-relay metric drops',
+                dashboard_links => [
+                    'https://grafana.wikimedia.org/d/000000020/graphite-eqiad?orgId=1&viewPanel=29',
+                    'https://grafana.wikimedia.org/d/000000337/graphite-codfw?orgId=1&viewPanel=29',
+                ],
+                metric          => 'sumSeries(transformNull(perSecond(carbon.relays.graphite*_local.destinations.*.dropped)))',
+                from            => '5minutes',
+                warning         => 25,
+                critical        => 100;
+            # is carbon-cache able to write to disk (e.g. permissions)
+            'carbon-cache_write_error':
+                description     => 'carbon-cache write error',
+                dashboard_links => ['https://grafana.wikimedia.org/d/000000020/graphite-eqiad?orgId=1&viewPanel=30'],
+                metric          => 'secondYAxis(sumSeries(carbon.agents.graphite1005-*.errors))',
+                from            => '10minutes',
+                warning         => 1,
+                critical        => 8;
+            # are carbon-cache queues overflowing their capacity?
+            'carbon-cache_overflow':
+                description     => 'carbon-cache queues overflow',
+                dashboard_links => ['https://grafana.wikimedia.org/d/000000020/graphite-eqiad?orgId=1&viewPanel=8'],
+                metric          => 'secondYAxis(sumSeries(carbon.agents.graphite1005-*.cache.overflow))',
+                from            => '10minutes',
+                warning         => 1,
+                critical        => 8;
+            # are we creating too many metrics?
+            'carbon-cache_many_creates':
+                description     => 'carbon-cache too many creates',
+                dashboard_links => ['https://grafana.wikimedia.org/d/000000020/graphite-eqiad?orgId=1&viewPanel=9'],
+                metric          => 'sumSeries(carbon.agents.graphite1005-*.creates)',
+                from            => '30min',
+                warning         => 500,
+                critical        => 1000;
+        }
     }
 }
