3364b2d448b3058b54dfb3e000d0777324465925
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-09-11 13:18:26 +0200

    Add an explicit Hiera variable to determine the active swift ring server
    The Puppet code currently uses the puppet_ca setting to select the active
    server, but doesn't seem useful. The sync is totally unrelated to the
    Puppet CA, all that matters is that we only have one active sync at a time.
    
    Instead, disambiguate things and designate a specific Hiera setting which
    specifically refers to the active sync host. This also causes less surprises
    if/when we switch the Puppet variable for the active Puppet CA in the future.
    
    Also remove the stray $ring_fetch variable, not used anywhere.
    
    Hosts: puppetserver1001.eqiad.wmnet,puppetserver1002.eqiad.wmnet
    Hosts: puppetserver1003.eqiad.wmnet,puppetserver2001.codfw.wmnet
    Hosts: puppetserver2002.codfw.wmnet,puppetserver2003.codfw.wmnet
    Bug: T366355
    Change-Id: Ia84e318d51a1666343fa9240ffe8aeb55ec3f647

--- modules/profile/manifests/swift/fetch_rings.pp
+++ modules/profile/manifests/swift/fetch_rings.pp
@@ -3,12 +3,10 @@
 # @param swift_clusters instance of Swift::Clusters (hash of cluster info)
 # @param volatile_dir the location of the volatile dir
 class profile::swift::fetch_rings (
-    Stdlib::Host     $ca_server      = lookup('puppet_ca_server'),
+    Stdlib::Host     $active_server  = lookup('profile::puppetserver::swift_fetch_rings::active_server'),
     Swift::Clusters  $swift_clusters = lookup('swift_clusters'),
     Stdlib::Unixpath $volatile_dir   = lookup('profile::swift::fetch_rings::volatile_dir'),
 ) {
-    $ca = $ca_server == $facts['networking']['fqdn']
-    $ring_fetch = stdlib::ensure($ca)
     $swift_dir = "${volatile_dir}/swift"
     file { $swift_dir:
         ensure => directory,
@@ -21,7 +19,7 @@ class profile::swift::fetch_rings (
                 ensure => directory,
             }
             systemd::timer::job { "fetch-rings-${sc}":
-                ensure          => stdlib::ensure($ca),  # We only want the timer to run on one host, use ca
+                ensure          => stdlib::ensure($active_server == $facts['networking']['fqdn']),
                 user            => 'root',
                 description     => "rsync swift rings from cluster ${sc}",
                 command         => "/usr/bin/rsync -bcptz ${sc_info['ring_manager']}::swiftrings/new_rings.tar.bz2 ${cluster_dir}/",
