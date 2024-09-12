5b60b2ef96f75ef7939abf6784e798c6d903d678
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2024-09-12 10:51:23 +0200

    jaeger: fix typo ensure vs require
    Change-Id: Ieb02e60098648bcbde677dafedcde184db62bc7b

--- modules/profile/manifests/jaeger/scripts.pp
+++ modules/profile/manifests/jaeger/scripts.pp
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::jaeger::scripts {
-  require_packages(['python3-opensearch'])
+  ensure_packages(['python3-opensearch'])
 
   file { '/usr/local/bin/jaeger-find-traces':
     ensure => present,
