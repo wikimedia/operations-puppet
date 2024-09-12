d772a2234a18f3bde8db6777b34444d4b1430c0e
Andre Klapper <a9016009@gmx.de>
2024-09-12 14:35:07 +0200

    phabricator: turn weekly data for tech news into Wikitext
    Do some sed magic and such.
    
    Bug: T373952
    Change-Id: If6e88af8f7fb8559014e443ebd2d50c1e2753402

--- modules/phabricator/files/tech_news_weekly_stats.sh
+++ modules/phabricator/files/tech_news_weekly_stats.sh
@@ -8,8 +8,8 @@
 source /etc/phab_tech_news_weekly_stats.conf
 timestamp=$(date)
 #echo "result_tasks"
-result_tasks=$(MYSQL_PWD=${sql_pass} /usr/bin/mysql -t -h $sql_host -P $sql_port -u $sql_user phabricator_maniphest << END
-SELECT DISTINCT CONCAT("[[phab:T", t.id, "]]") AS url, t.title AS taskTitle, autr.userName AS author, ownr.userName AS assignee
+result_tasks=$(MYSQL_PWD=${sql_pass} /usr/bin/mysql -t --skip-column-names -h $sql_host -P $sql_port -u $sql_user phabricator_maniphest << END
+SELECT DISTINCT CONCAT("[[phab:T", t.id, "]]"), t.title, autr.userName, ownr.userName
     FROM phabricator_maniphest.maniphest_task t
     INNER JOIN phabricator_user.user autr ON autr.phid = t.authorPHID
     INNER JOIN phabricator_user.user ownr ON ownr.phid = t.ownerPHID
@@ -36,6 +36,18 @@ SELECT DISTINCT CONCAT("[[phab:T", t.id, "]]") AS url, t.title AS taskTitle, aut
             (SELECT ue.userPHID FROM phabricator_user.user u INNER JOIN phabricator_user.user_email ue ON ue.userPHID = u.phid WHERE (ue.address LIKE '%@wikimedia.org' OR ue.address LIKE '%@wikimedia.de' OR ue.address LIKE '%@speedandfunction.com' OR ue.address LIKE '%@thisdot.co')));
 END
 )
+
+# Remove first and last line in mysql table output
+result_tasks=$(echo "$result_tasks" | sed '1d;$d')
+# replace all | (created by the mariadb -t table parameter) with ||
+result_tasks=$(echo "$result_tasks" | sed 's/|/||/g')
+# remove last three letters of every line to remove " ||"
+result_tasks=$(echo "$result_tasks" | sed -r 's/.{3}$//')
+# remove first letter | of every line
+result_tasks=$(echo "$result_tasks" | sed -r 's/^.//')
+# add |- in a new line between all lines
+result_tasks=$(echo "$result_tasks" | sed ':a;N;$!ba;s/\n/\n|\-\n/g')
+
 # the actual email
 cat <<EOF | /usr/bin/mail -r "${sndr_address}" -s "Weekly Phabricator data for Tech News - ${timestamp}" ${rcpt_address}
 This is the automatic weekly Phabricator mail for Tech News.
@@ -51,7 +63,14 @@ addresses and accounts for their staff activity in Phabricator.
 
 Thus results and numbers MUST be manually reviewed and corrected:
 
+{| class="wikitable"
+! url
+! taskTitle
+! author
+! assignee
+|-
 ${result_tasks}
+|}
 
 Yours sincerely,
 Fab Rick Aytor
