6c0af9de1a91c6ba5b4808cea35665f5fad45489
Daniel Zahn <dzahn@wikimedia.org>
2024-09-11 17:10:37 -0700

    gerrit: add gerrit::proxy profile to insetup::gerrit role
    Test the gerrit apache (proxy) setup on bookworm without
    touching the production setup.
    
    Bug: T372804
    Change-Id: I9a7d80a6dbc7661af9499e58a73c8bd6fe2123be

--- modules/role/manifests/insetup/collaboration_services/gerrit.pp
+++ modules/role/manifests/insetup/collaboration_services/gerrit.pp
@@ -5,6 +5,7 @@ class role::insetup::collaboration_services::gerrit {
     include profile::firewall
     include profile::firewall::nftables_throttling
     include profile::gerrit::migration
+    include profile::gerrit::proxy
     include profile::prometheus::apache_exporter
     include profile::prometheus::nft_throttling_denylist
     include profile::java
