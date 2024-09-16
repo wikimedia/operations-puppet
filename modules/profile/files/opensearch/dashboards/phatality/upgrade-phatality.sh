4c0d52c072bf29444184b69e1f4613d23f913f29
Ahmon Dancy <adancy@wikimedia.org>
2024-09-16 09:20:02 -0700

    upgrade-phatality.sh: Don't use sudo to list plugins
    `/usr/share/opensearch-dashboards/bin/opensearch-dashboards-plugin
    list phatality` works fine without sudo, so remove it.  This should
    unbreak deployment.
    
    Change-Id: I6d1e697bd56820f16bfa1a8517e1e7d5a960bed1

--- modules/profile/files/opensearch/dashboards/phatality/upgrade-phatality.sh
+++ modules/profile/files/opensearch/dashboards/phatality/upgrade-phatality.sh
@@ -6,7 +6,7 @@ set -eu -o pipefail
 DASHBOARDS_VERSION=$(jq -r ".version" /usr/share/opensearch-dashboards/package.json)
 PHATALITY_PKG="/srv/deployment/releng/phatality/deploy/phatality-$DASHBOARDS_VERSION.zip"
 
-mapfile -t plugins < <(/usr/bin/sudo -u opensearch-dashboards /usr/share/opensearch-dashboards/bin/opensearch-dashboards-plugin list phatality)
+mapfile -t plugins < <(/usr/share/opensearch-dashboards/bin/opensearch-dashboards-plugin list phatality)
 
 for plugin_line in "${plugins[@]}"
 do
