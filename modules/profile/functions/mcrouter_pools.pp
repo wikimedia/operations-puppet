21c9404dd7f1f794e6e2079c3d296a082c0dd0f3
Jesse Hathaway <jhathaway@wikimedia.org>
2024-09-16 11:50:22 -0500

    mcrouter: remove puppet alert
    Remove puppet alert message used for debugging.
    
    Hosts: P:mediawiki::mcrouter_wancache
    Change-Id: I1deee2a57592f5b45efccdd8f346b47408ed1aef

--- modules/profile/functions/mcrouter_pools.pp
+++ modules/profile/functions/mcrouter_pools.pp
@@ -1,5 +1,4 @@
 function profile::mcrouter_pools(String $pool_name, Hash $servers, String $proto, Stdlib::Port $port) >> Hash {
-    alert("Vars ${$pool_name} ${servers}");
     {
         $pool_name => {
             'servers' => $servers.map |$shard_slot, $address| {
