cd03e912133abb7c25f0953e7531a58ae1146d3e
Daniel Zahn <dzahn@wikimedia.org>
2024-09-17 11:27:54 -0700

    peopleweb: make home dir sizes in warning message human readable
    Keep comparing the size against the treshold in kilobytes but
    in the mail content output them in human readable format (du -h).
    
    Bug: T343364
    Change-Id: I2c693d584e99731c07d80aed52a19dd6beef4604

--- modules/profile/files/microsites/peopleweb/home_size_warning.sh
+++ modules/profile/files/microsites/peopleweb/home_size_warning.sh
@@ -7,9 +7,10 @@ source /etc/home_size_warning.conf
 
 for userhome in /home/*; do
     home_dir_size=$(du -s ${userhome} | cut -f1)
+    home_dir_size_h=$(du -hs ${userhome} | cut -f1)
     if [ "$home_dir_size" -gt "$home_dir_limit" ]; then
       cat <<EOF | /usr/bin/mail -r "${sndr_address}" -s "peopleweb - home dir size warning - ${userhome}" -a "Auto-Submitted: auto-generated" ${rcpt_address}
-${userhome} on ${hostname} uses ${home_dir_size} kB which is larger than the configured limit of ${home_dir_limit} kB. (T343364)
+${userhome} on ${hostname} uses ${home_dir_size_h} which is larger than the configured limit of ${home_dir_limit_h}. (T343364)
 
 (sent via $(basename $0) on $(hostname) at $(date))
 EOF
