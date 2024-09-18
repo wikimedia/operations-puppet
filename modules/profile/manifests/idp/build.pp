1c86c5705fa92e31deeb7a2052862d38f7911616
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-09-18 13:55:35 +0200

    profile::idp::build: Readd rsync service
    We dropped that temporarily when the idp-build host was specifically
    on bullseye to support the 6.6 builds. Now that this is no longer
    needed, we can apply this for Bookworm again.
    
    Also remove support for Bullseye.
    
    Bug: T367487
    Change-Id: Ib552bb448625db70bc88f9175bd420c3522df0b9

--- modules/profile/manifests/idp/build.pp
+++ modules/profile/manifests/idp/build.pp
@@ -13,27 +13,22 @@ class profile::idp::build {
         directory => '/srv/cas-build/cas',
     }
 
-    if debian::codename::eq('bullseye') {
-        ensure_packages(['default-jdk-headless'])
+    # Set up an rsync module to allow easy copying of the built DEB
+    class { 'rsync::server': }
+    rsync::server::module { 'cas-build-result':
+        path => '/srv/cas-build/',
+    }
 
-        # Set up an rsync module to allow easy copying of the built DEB
-        class { 'rsync::server': }
-        rsync::server::module { 'cas-build-result':
-            path => '/srv/cas-build/',
-        }
+    firewall::service { 'cas_build_rsync':
+        proto  => 'tcp',
+        port   => [873],
+        srange => wmflib::role::hosts('apt_repo'),
+    }
 
-        firewall::service { 'cas_build_rsync':
-            proto  => 'tcp',
-            port   => [873],
-            srange => wmflib::role::hosts('apt_repo'),
-        }
+    profile::auto_restarts::service { 'rsync': }
 
-        profile::auto_restarts::service { 'rsync': }
-
-    } else {
-        apt::package_from_component { 'openjdk-21':
-            component => 'component/jdk21',
-            packages  => ['openjdk-21-jre', 'openjdk-21-jre-headless', 'openjdk-21-jdk', 'openjdk-21-jdk-headless'],
-        }
+    apt::package_from_component { 'openjdk-21':
+        component => 'component/jdk21',
+        packages  => ['openjdk-21-jre', 'openjdk-21-jre-headless', 'openjdk-21-jdk', 'openjdk-21-jdk-headless'],
     }
 }
