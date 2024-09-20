600277e6fe78d82cf33ecd9f632d0efffb01ddac
Daniel Zahn <dzahn@wikimedia.org>
2024-09-20 09:34:08 -0700

    gerrit: include gerrit profile in insetup::gerrit for testing
    After Ia9639f1ef8f758f834d52e made it possible to apply
    the full gerrit role without causing an IP conflict with the
    service IP, this should now be ok to do.
    
    This way we would know everything works on new distro version
    and hardware before switching some day in the future.
    
    Bug: T372804
    Change-Id: Id7a37ea90cba623d3a70920f2b70163d8af7a245

--- modules/role/manifests/insetup/collaboration_services/gerrit.pp
+++ modules/role/manifests/insetup/collaboration_services/gerrit.pp
@@ -5,7 +5,7 @@ class role::insetup::collaboration_services::gerrit {
     include profile::firewall
     include profile::firewall::nftables_throttling
     include profile::gerrit::migration
-    include profile::gerrit::setup_proxy
+    include profile::gerrit
     include profile::gerrit::proxy
     include profile::prometheus::apache_exporter
     include profile::prometheus::nft_throttling_denylist
