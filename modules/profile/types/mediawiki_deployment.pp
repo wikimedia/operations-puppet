f1484888087a92bc44b89552410287b89db62c62
Scott French <swfrench@wikimedia.org>
2024-09-20 18:00:51 -0700

    types: extend Profile::Mediawiki_deployment
    Updates Mediawiki_deployment to support the new release config mapping
    described in [0]. In effect, we're transitioning:
    
      - namespace: mw-web
        release: main
        canary: canary
        mw_flavour: "publish"
        web_flavour: "webserver"
        debug: false
    
    to the equivalent:
    
      - namespace: mw-web
        releases:
          main: {}
          canary:
            stage: "canaries"
        mw_flavour: "publish"
        web_flavour: "webserver"
        debug: false
    
    where additional releases can be added and optionally override the image
    flavours.
    
    The older 'release' and 'canary' fields are still supported by scap on a
    temporary basis. Further, the two formats are exclusive, in that a given
    namespace must use one or the other (e.g., 'releases' and 'release' are
    not permitted to coexist), and the only supported stage for the moment
    is canaries.
    
    A Variant type is thus a natural way to represent structural assertions
    for the hieradata key at this time.
    
    [0] https://phabricator.wikimedia.org/T370934#10104089
    
    Bug: T370934
    Change-Id: I7af6b2fffc6b48193ae1f72ef81c76f4a9882f33

--- modules/profile/types/mediawiki_deployment.pp
+++ modules/profile/types/mediawiki_deployment.pp
@@ -1,8 +1,21 @@
-type Profile::Mediawiki_deployment = Struct[{
-    'namespace'   => String,
-    'release'     => String,
-    'canary'      => Optional[String],
-    'mw_flavour'  => String,
-    'web_flavour' => String,
-    'debug'       => Boolean,
-}]
+# TODO: T370934 - Remove the older format and surrounding Variant once all
+# deployments are migrated.
+type Profile::Mediawiki_deployment = Variant[Struct[{
+        'namespace'   => String,
+        'release'     => String,
+        'canary'      => Optional[String],
+        'mw_flavour'  => String,
+        'web_flavour' => String,
+        'debug'       => Boolean,
+    }],
+    Struct[{
+        'namespace'   => String,
+        'releases'    => Hash[String, Struct[{
+            'mw_flavour'  => Optional[String],
+            'web_flavour' => Optional[String],
+            'stage'       => Optional[Enum['canaries']],
+        }]],
+        'mw_flavour'  => String,
+        'web_flavour' => String,
+        'debug'       => Boolean,
+}]]
