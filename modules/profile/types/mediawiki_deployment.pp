d9f75565b58cd8473e411f503b6827e0864f9367
Scott French <swfrench@wikimedia.org>
2024-09-20 18:08:07 -0700

    types: remove older Mediawiki_deployment variant
    This removes the variant supporting the older-format release / canary
    fields.
    
    Hosts: O:deployment_server::kubernetes
    Bug: T370934
    Change-Id: I7aa04dff5982479110ac1bd7616f4af824a171a2

--- modules/profile/types/mediawiki_deployment.pp
+++ modules/profile/types/mediawiki_deployment.pp
@@ -1,21 +1,11 @@
-# TODO: T370934 - Remove the older format and surrounding Variant once all
-# deployments are migrated.
-type Profile::Mediawiki_deployment = Variant[Struct[{
-        'namespace'   => String,
-        'release'     => String,
-        'canary'      => Optional[String],
-        'mw_flavour'  => String,
-        'web_flavour' => String,
-        'debug'       => Boolean,
-    }],
-    Struct[{
-        'namespace'   => String,
-        'releases'    => Hash[String, Struct[{
-            'mw_flavour'  => Optional[String],
-            'web_flavour' => Optional[String],
-            'stage'       => Optional[Enum['canaries']],
-        }]],
-        'mw_flavour'  => String,
-        'web_flavour' => String,
-        'debug'       => Boolean,
-}]]
+type Profile::Mediawiki_deployment = Struct[{
+    'namespace'   => String,
+    'releases'    => Hash[String, Struct[{
+        'mw_flavour'  => Optional[String],
+        'web_flavour' => Optional[String],
+        'stage'       => Optional[Enum['canaries']],
+    }]],
+    'mw_flavour'  => String,
+    'web_flavour' => String,
+    'debug'       => Boolean,
+}]
