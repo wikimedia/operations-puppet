b8e8f732885c7391d7aaade4487d635a417d3bd2
Keith Herron <kherron@wikimedia.org>
2024-09-26 15:26:09 -0400

    opentelemetry::collector: set default port and update template
    set a default otlp port and update the template to filter the
    output based on presence of the fqdn config
    
    Bug: T376179
    Change-Id: Ib4812ba2d10a2b8e0c06b8bb1278ab7f06858255

--- modules/profile/manifests/opentelemetry/collector.pp
+++ modules/profile/manifests/opentelemetry/collector.pp
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::opentelemetry::collector(
     Optional[Stdlib::Host] $otel_gateway_fqdn      = lookup('profile::opentelemetry::otel_gateway_fqdn', {'default_value' => undef}),
-    Optional[Stdlib::Port] $otel_gateway_otlp_port = lookup('profile::opentelemetry::otel_gateway_otlp_port', {'default_value' => undef}),
+    Optional[Stdlib::Port] $otel_gateway_otlp_port = lookup('profile::opentelemetry::otel_gateway_otlp_port', {'default_value' => 30443}),
 )
 {
     class { 'opentelemetry::collector':
