ea33640bd9cb66aa3c4ab7dfbfa043f728fb65f4
Luca Toscano <ltoscano@wikimedia.org>
2024-09-30 16:35:54 +0200

    role::ircstream: add template for config file plus basic settings
    Bug: T376014
    Change-Id: I60607913cd02394ceaff0c6399f59fba9a7a6523

--- modules/ircstream/manifests/init.pp
+++ modules/ircstream/manifests/init.pp
@@ -1,7 +1,18 @@
 #SPDX-License-Identifier: Apache-2.0
 #@summary Class to install ircstream a mediawiki to IRC streaming service. See: https://github.com/paravoid/ircstream
 class ircstream (
+    Stdlib::Host $irc_listen_address = '::',
+    Stdlib::Port $irc_listen_port = 6667,
+    String $irc_servername = 'irc.wikimedia.org',
+    Stdlib::Host $rc2udp_listen_address = '::',
+    Stdlib::Port $rc2udp_listen_port = 9390,
+    Stdlib::Host $prometheus_listen_address = '::',
+    Stdlib::Port $prometheus_listen_port = 16667,
 ){
-
     ensure_packages(['ircstream'])
+    file { '/etc/ircstream.conf':
+        mode    => '0444',
+        before  => Package['ircstream'],
+        content => template('ircstream/ircstream.conf.erb'),
+    }
 }
