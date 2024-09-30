f1f448bda477139e881c4a25d1cc7e1b4628af45
Simon Lyngshede <slyngshede@wikimedia.org>
2024-09-30 13:16:59 +0200

    P:ircstream: Firewall openings.
    * Allow users to connect using IRC.
    * Accept UDP traffic from Mediawiki servers.
    
    Bug: T376014
    Change-Id: If1f96eacfd5379ab465f67e53bd8c7987c20ad1b

--- modules/profile/manifests/ircstream.pp
+++ modules/profile/manifests/ircstream.pp
@@ -2,4 +2,18 @@
 # @summary Class to configure ircstream.
 class profile::ircstream(){
     class { 'ircstream': }
+
+    # Allow users to connect with IRC.
+    firewall::service { 'irc_public':
+        proto => 'tcp',
+        port  => 6667,
+    }
+
+    # Accept traffic from the mediawiki servers.
+    # Mediawiki sends changes as a UDP data package.
+    firewall::service { 'mediawiki_udp_data_packages':
+        proto    => 'udp',
+        port     => 9390,
+        src_sets => ['MW_APPSERVER_NETWORKS']
+    }
 }
