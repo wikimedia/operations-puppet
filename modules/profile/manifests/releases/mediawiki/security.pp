e06490d9aa427995d2d4b7222a7c72fd5fade42d
Eoghan Gaffney <egaffney@wikimedia.org>
2024-10-01 22:21:07 +0100

    Add ignore_missing_file_errors to releases hosts
    This adds the option to ignore the "some files vanished before they
    could be transferred" errors that come up from time to time.
    
    Change-Id: I37e6c94d052537a9bf4813bbb1e1f9fc0b2c87c5

--- modules/profile/manifests/releases/mediawiki/security.pp
+++ modules/profile/manifests/releases/mediawiki/security.pp
@@ -9,35 +9,38 @@ class profile::releases::mediawiki::security (
 
     # server-agnostic rsync on the primary releases server
     rsync::quickdatacopy { 'srv-patches-releases-primary':
-        ensure      => present,
-        auto_sync   => true,
-        delete      => true,
-        source_host => $deployment_server,
-        dest_host   => $primary_server,
-        module_path => '/srv/patches',
-        chown       => 'jenkins:705',
+        ensure                     => present,
+        auto_sync                  => true,
+        delete                     => true,
+        source_host                => $deployment_server,
+        dest_host                  => $primary_server,
+        module_path                => '/srv/patches',
+        chown                      => 'jenkins:705',
+        ignore_missing_file_errors => true,
     }
 
     # if the primary changes, absent the hostname-based rsync
     rsync::quickdatacopy { "srv-patches-${primary_server}":
-        ensure      => absent,
-        auto_sync   => true,
-        delete      => true,
-        source_host => $deployment_server,
-        dest_host   => $primary_server,
-        module_path => '/srv/patches',
+        ensure                     => absent,
+        auto_sync                  => true,
+        delete                     => true,
+        source_host                => $deployment_server,
+        dest_host                  => $primary_server,
+        module_path                => '/srv/patches',
+        ignore_missing_file_errors => true,
     }
 
     $secondary_servers.each |Stdlib::Fqdn $secondary_server| {
         unless $deployment_server == $secondary_server {
             rsync::quickdatacopy { "srv-patches-${secondary_server}":
-                ensure      => present,
-                auto_sync   => true,
-                delete      => true,
-                source_host => $deployment_server,
-                dest_host   => $secondary_server,
-                module_path => '/srv/patches',
-                chown       => 'jenkins:705',
+                ensure                     => present,
+                auto_sync                  => true,
+                delete                     => true,
+                source_host                => $deployment_server,
+                dest_host                  => $secondary_server,
+                module_path                => '/srv/patches',
+                chown                      => 'jenkins:705',
+                ignore_missing_file_errors => true,
             }
         }
     }
