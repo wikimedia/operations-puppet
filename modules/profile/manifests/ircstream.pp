44ee1f66bb2b03def200fb15a532cebb9664de77
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-10-02 15:27:41 +0200

    Add a separate role for sse-enabled ircstream and a Hiera option
    Bug: T376014
    Hosts: irc1003.wikimedia.org
    Change-Id: Ic899dc15057b679322ce3a4006058055d38ec3a5

--- modules/profile/manifests/ircstream.pp
+++ modules/profile/manifests/ircstream.pp
@@ -1,7 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 # @summary Class to configure ircstream.
-class profile::ircstream(){
-    class { 'ircstream': }
+class profile::ircstream(
+    Boolean     $use_eventstream = lookup('profile::ircstream::use_eventstream'),
+){
+    class { 'ircstream':
+        eventstream => $use_eventstream,
+    }
 
     # Allow users to connect with IRC.
     firewall::service { 'irc_public':
@@ -9,11 +13,13 @@ class profile::ircstream(){
         port  => 6667,
     }
 
-    # Accept traffic from the mediawiki servers.
-    # Mediawiki sends changes as a UDP data package.
-    firewall::service { 'mediawiki_udp_data_packages':
-        proto    => 'udp',
-        port     => 9390,
-        src_sets => ['MW_APPSERVER_NETWORKS']
+    unless $use_eventstream {
+        # Accept traffic from the mediawiki servers.
+        # Mediawiki sends changes as a UDP data package.
+        firewall::service { 'mediawiki_udp_data_packages':
+            proto    => 'udp',
+            port     => 9390,
+            src_sets => ['MW_APPSERVER_NETWORKS']
+        }
     }
 }
