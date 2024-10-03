005d329ee75a1592125a8c49b4f91e33a728347d
Amir Sarabadani <ladsgroup@gmail.com>
2024-10-03 17:40:34 +0200

    wikitech: Get rid of the old mw-xml dumper file and cron
    It won't work.
    
    Bug: T374114
    Bug: T371378
    Change-Id: I78187e40cb715c421237a9fa1a07dfbe50abdaa5

--- modules/openstack/files/wikitech/mw-xml.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/local/bin/mwscript maintenance/dumpBackup.php labswiki --current --uploads | sed 's/<model>yaml<\/model>/<model>wikitext<\/model>/g' | sed 's/<format>application\/yaml<\/format>/<format>text\/x-wiki<\/format>/g' | nice -n 19 gzip -9 > /srv/backup/public/labswiki-$(date '+%Y%m%d').xml.gz
--- modules/openstack/manifests/wikitech/wikitech_static_sync.pp
+++ modules/openstack/manifests/wikitech/wikitech_static_sync.pp
@@ -22,12 +22,6 @@ class openstack::wikitech::wikitech_static_sync {
     $minute = fqdn_rand(60)
 
     systemd::timer::job { 'mw-xml':
-        ensure          => 'present',
-        description     => 'Regular jobs to generate xml dumps',
-        user            => 'root',
-        command         => '/usr/local/sbin/mw-xml.sh',
-        interval        => {'start' => 'OnCalendar', 'interval' => "*-*-* 1:${minute}:00"},
-        logging_enabled => false,
-        require         => File['/srv/backup/public'];
+        ensure => absent,
     }
 }
