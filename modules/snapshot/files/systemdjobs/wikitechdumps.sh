5a83b3ff530dde13ca238cabe5a933af43225638
Amir Sarabadani <ladsgroup@gmail.com>
2024-10-03 17:19:19 +0200

    wikitechdumps: Update the path to mwscript
    This was noticed during our tests but we forgot to put it in the bash
    file.
    
    Bug: T374114
    Change-Id: I4c332e30813dbbad37b25b1dabcb01dba04e3311

--- modules/snapshot/files/systemdjobs/wikitechdumps.sh
+++ modules/snapshot/files/systemdjobs/wikitechdumps.sh
@@ -4,7 +4,7 @@
 # This script is used to dump the labswiki database compressed and saves to the dump_dir
 
 dump_dir=$1
-/usr/local/bin/mwscript maintenance/dumpBackup.php labswiki --current --uploads  | \
+/usr/bin/php /srv/mediawiki/multiversion/MWScript.php maintenance/dumpBackup.php labswiki --current --uploads  | \
     sed 's/<model>yaml<\/model>/<model>wikitext<\/model>/g'                      | \
     sed 's/<format>application\/yaml<\/format>/<format>text\/x-wiki<\/format>/g' | \
     nice -n 19 gzip -9 > ${dump_dir}/labswiki-$(date '+%Y%m%d').xml.gz
