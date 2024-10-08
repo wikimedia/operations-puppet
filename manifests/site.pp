17c8c246f5bcbba6464d488f9ebbe594e86814f7
Eric Evans <eevans@wikimedia.org>
2024-10-08 10:52:51 -0500

    aqs1022: change to role(insetup::data_persistence)
    Initial install using role(aqs) is failing(?)
    
    Bug: T372514
    Change-Id: I62405c4bfb3114490cf4cfd29648ec1cf245b6b3

--- manifests/site.pp
+++ manifests/site.pp
@@ -213,8 +213,13 @@ node /^apt-staging2001\.codfw\./ {
 }
 
 # Analytics Query Service
-node /^aqs10(1[0-9]|2[0-2])\.eqiad\./ {
+node /^aqs10(1[0-9]|2[0-1])\.eqiad\./ {
     role(aqs)
+}
+
+# New: https://phabricator.wikimedia.org/T372514
+node /^aqs1022\.eqiad\./ {
+    role(insetup::data_persistence)
 }
 
 node /^aqs200[1-9]|aqs201[0-2]\.codfw\./ {
