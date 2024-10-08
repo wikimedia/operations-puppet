dbd2685c58c94a27920a06e35a222de476b22cbf
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2024-10-08 18:09:15 +0200

    profile::conftool: add web interface for requestctl
    Bug: T371782
    Change-Id: Ie74f612cd6f6e80c44b330ffc074124f5ee3663d

---
+++ modules/profile/manifests/conftool/hiddenparma.pp
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: Apache-2.0
+# @summary profile to install the requestctl web interface
+#
+# @param api_tokens Hash[str, str] a dictionary of username-token pairs
+class profile::conftool::hiddenparma (
+    Hash[str, str] $api_tokens = lookup('profile::conftool::hiddenparma::api_tokens'),
+) {
+    require profile::conftool::client
+    $user = 'deploy-hiddenparma'
+    file { '/etc/default/hiddenparma':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('profile/conftool/hiddenparma-default.erb'),
+    }
+
+    fastapi::application { 'hiddenparma':
+        port => 8080,
+    }
+
+    file { '/etc/HIDDENPARMA':
+        ensure => directory,
+        owner  => $user,
+        group  => $user,
+        mode   => '0550',
+    }
+
+    file { '/etc/HIDDENPARMA/api_tokens.json':
+        ensure  => file,
+        owner   => $user,
+        group   => $user,
+        mode    => '0440',
+        content => to_json($api_tokens),
+        notify  => Service['hiddenparma'],
+    }
+    # Apache and CAS auth setup
+    profile::idp::client::httpd::site { 'requestctl.wikimedia.org':
+        require         => [
+            Acme_chief::Cert['icinga'],
+        ],
+        vhost_content   => 'profile/conftool/httpd-hiddenparma.conf.erb',
+        # Only full roots can access this.
+        required_groups => [
+            'cn=ops,ou=groups,dc=wikimedia,dc=org',
+        ],
+        vhost_settings  => { proxy_pass => 'http://localhost:8080' },
+    }
+}
