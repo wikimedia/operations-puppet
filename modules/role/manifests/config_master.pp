fb85fd590e2b153f35f09ad64acae085a54d2c1f
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-10-09 15:39:20 +0200

    Fix /etc/issue for config-master
    Seems there was some copy&pasta from the insetup role, provide a correct
    desription instead.
    
    Change-Id: I8f0768484e977ff60cf1b4d7ed4815b2a2bf7710

--- modules/role/manifests/config_master.pp
+++ modules/role/manifests/config_master.pp
@@ -1,12 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # @summary role to configer ther serveres that serve config-master.wikimedia.org
 class role::config_master {
-
-    system::role { 'config_master':
-        ensure      => 'present',
-        description => 'Host used for experiments by SREs',
-    }
-
     include profile::base::production
     include profile::firewall
     include profile::conftool::client
