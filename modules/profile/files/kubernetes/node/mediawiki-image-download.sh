327c79d851aa9292a54c0f7b2d6d40a3030ed3e5
Alexandros Kosiaris <akosiaris@wikimedia.org>
2024-10-10 15:50:46 +0300

    mediawiki-image-download: Remove pct based pulls
    Why:
    Tests are done, the functionality is no longer useful and keeping unused
    code around isn't useful as well.
    
    What:
    Remove the percentage based approach. Now invocations of the script will
    pull regardless of luck
    
    Hosts: auto
    Bug: T366778
    Change-Id: Ie2a8f90f15180ada17bb00b286818007e5545764

--- modules/profile/files/kubernetes/node/mediawiki-image-download.sh
+++ modules/profile/files/kubernetes/node/mediawiki-image-download.sh
@@ -11,21 +11,12 @@ if [ -z "$1" ]; then
     exit 1
 fi
 IMAGE="$IMAGE_BASE_NAME:$1"
-# This if clause is meant to provide a way to probabilistically tune the
-# percentage of nodes that will actually pull the mediawiki image. This allows
-# us to run some tests about how much useful this script is
-# NOTE: We are doing some bash trickery here since bash doesn't support floats
-PULL_THRESHOLD="5/100"  # This is a percentage
-# Make it useful for a quick comparison to $RANDOM
-ABSOLUTE_THRESHOLD=$(( 32767 * ${PULL_THRESHOLD}))
-if [ $RANDOM -le $ABSOLUTE_THRESHOLD ]; then
-    echo "Pulling '$IMAGE'..."
-    docker --config /var/lib/kubelet pull "$IMAGE"
-    echo "Removing all mediawiki images but the last ${IMAGES_TO_KEEP}"
-    # Our tags for the docker images are in YYYY-MM-DD-HH-MM-SS format, so we can rely on them
-    # to get a consistent sorting.
-    # Please note that as of today `docker image ls` will return the results in the same order.
-    TO_REMOVE=$(docker image ls --format "{{.Tag}}\t{{.ID}}" "$IMAGE_BASE_NAME" | sort -r | awk '{print $2}' | tail -n +${IMAGES_TO_KEEP} | sort -u)
-    # Please note, we don't double quote TO_REMOVE at the end of the line on purpose
-    test -n "$TO_REMOVE" && docker rmi $TO_REMOVE || /bin/true
-fi
+echo "Pulling '$IMAGE'..."
+docker --config /var/lib/kubelet pull "$IMAGE"
+echo "Removing all mediawiki images but the last ${IMAGES_TO_KEEP}"
+# Our tags for the docker images are in YYYY-MM-DD-HH-MM-SS format, so we can rely on them
+# to get a consistent sorting.
+# Please note that as of today `docker image ls` will return the results in the same order.
+TO_REMOVE=$(docker image ls --format "{{.Tag}}\t{{.ID}}" "$IMAGE_BASE_NAME" | sort -r | awk '{print $2}' | tail -n +${IMAGES_TO_KEEP} | sort -u)
+# Please note, we don't double quote TO_REMOVE at the end of the line on purpose
+test -n "$TO_REMOVE" && docker rmi $TO_REMOVE || /bin/true
