74cc38206295db32a86d05ef5ca677e47f3dbcf8
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-10-22 17:54:27 +0200

    Remove ganeti2011 from active Ganeti nodes
    Hosts: ganeti2011.codfw.wmnet,ganeti2012.codfw.wmnet
    Bug: T376594
    Change-Id: I0c07ff26b9040cd45242bb34b67217a1bce81a1f

--- manifests/site.pp
+++ manifests/site.pp
@@ -1336,11 +1336,11 @@ node /^ganeti203[3-4]\.codfw\./ {
 }
 
 # Old servers, to be decommed
-node /^ganeti20(09|1[0])\.codfw\./ {
+node /^ganeti20(1[1])\.codfw\./ {
     role(insetup::infrastructure_foundations)
 }
 
-node /^ganeti20(1[1-9]|2[0-9]|3[0125678])\.codfw\./ {
+node /^ganeti20(1[2-9]|2[0-9]|3[0125678])\.codfw\./ {
     role(ganeti)
 }
 
