49eefcc0b8c4eb7775b58d8f510da9e3b53956b9
Taavi Väänänen <hi@taavi.wtf>
2024-10-22 17:49:04 +0300

    P:wmcs::nfs::standalone: Fix link
    Change-Id: I75bba7f371f147a853ab8b1a523eca11e2a3af7a

--- modules/profile/files/wmcs/nfs/standalone/motd.sh
+++ modules/profile/files/wmcs/nfs/standalone/motd.sh
@@ -9,7 +9,7 @@ cat <<EOF
    [33;1m/ [31m![33m \\    [0mThis server hosts shared file storage for this project,
   [33;1m/_____\\   [0mand it is maintained by the Cloud VPS admin team.
 
-  For more details, please see:
-  [4mhttps://wikitech.wikimedia.org/wiki/Help:Toolforge/Portal:Data_Services#Shared_Storage[0m
+   For more details, please see:
+   [4mhttps://wikitech.wikimedia.org/wiki/Portal:Data_Services#Shared_Storage[0m
 
 EOF
