0cf95f9643b9c644be3fb0063743af5881ad7abc
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2024-10-22 13:09:20 +0200

    toolforge: apt_pinning: drop more unused pinning
    The apt pinning that this patch is dropping were mostly used in the old
    GridEngine times, where the execution runtime environment for user jobs
    had a direct dependency on the LDAP system.
    
    This is no longer the case with the current Kubernetes-based job system.
    
    Change-Id: I2bf3bd992df44919294d8187e7cc439cd60d9dd8
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/toolforge/apt_pinning.pp
+++ modules/profile/manifests/toolforge/apt_pinning.pp
@@ -3,22 +3,12 @@
 class profile::toolforge::apt_pinning {
     case debian::codename() {
         'bullseye': {
-            $libpam_pkg_version       = 'version 1.4.0-9'
-            $libpam_ldapd_pkg_version = 'version 0.9.11-1'
-            $ldap_utils_pkg_version   = 'version 2.4.57+dfsg-3'
-            $libnss3_pkg_version      = 'version 2:3.61-1'
             $libnfsidmap2_pkg_version = 'version 0.25-6'
-            $libnss_db_pkg_version    = 'version 2.2.3pre1-6'
             $nfs_common_pkg_version   = 'version 1:1.3.4-6'
         }
 
         'buster': {
-            $libpam_pkg_version       = 'version 1.3.1-5'
-            $libpam_ldapd_pkg_version = 'version 0.9.10-2'
-            $ldap_utils_pkg_version   = 'version 2.4.47+dfsg-3+deb10u1'
-            $libnss3_pkg_version      = 'version 2:3.42.1-1+deb10u1'
             $libnfsidmap2_pkg_version = 'version 0.25-5.1'
-            $libnss_db_pkg_version    = 'version 2.2.3pre1-6+b6'
             $nfs_common_pkg_version   = 'version 1:1.3.4-2.5'
         }
         default: {
@@ -27,41 +17,16 @@ class profile::toolforge::apt_pinning {
     }
 
     apt::pin { [
-        'toolforge-sudo-ldap-pinning',
-        'toolforge-nscd-pinning',
-        'toolforge-python-ldap-pinning',
-        'toolforge-ldapvi-pinning',
-        'toolforge-sssd-pinning',
+        'toolforge-libpam-pinning',
+        'toolforge-libpam-ldapd-pinning',
+        'toolforge-libnss-db-pinning',
+        'toolforge-libpam-ldapd-pinning',
+        'toolforge-ldap-utils-pinning',
+        'toolforge-libnss3-pinning',
     ]:
         ensure   => absent,
         pin      => 'not used',
         priority => -1,
-    }
-
-    apt::pin { 'toolforge-libpam-pinning':
-        package  => 'libpam-runtime libpam-modules* libpam0g',
-        pin      => $libpam_pkg_version,
-        priority => 1001,
-    }
-    apt::pin { 'toolforge-libpam-ldapd-pinning':
-        package  => 'libpam-ldapd nslcd* libnss-ldapd',
-        pin      => $libpam_ldapd_pkg_version,
-        priority => 1001,
-    }
-    apt::pin { 'toolforge-libnss-db-pinning':
-        package  => 'libnss-db',
-        pin      => $libnss_db_pkg_version,
-        priority => 1001,
-    }
-    apt::pin { 'toolforge-ldap-utils-pinning':
-        package  => 'ldap-utils libldap*',
-        pin      => $ldap_utils_pkg_version,
-        priority => 1001,
-    }
-    apt::pin { 'toolforge-libnss3-pinning':
-        package  => 'libnss3**',
-        pin      => $libnss3_pkg_version,
-        priority => 1001,
     }
 
     apt::pin { 'toolforge-nfs-common-pinning':
