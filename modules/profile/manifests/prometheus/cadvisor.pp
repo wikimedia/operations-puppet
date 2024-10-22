643b88a7d7f1980bd37110ebc560375f0c9799a4
Tiziano Fogli <tfogli@wikimedia.org>
2024-10-22 11:04:33 +0200

    prometheus/cadvisor: lookup extra metrics from hiera
    This is to help with
    https://gerrit.wikimedia.org/r/c/operations/puppet/+/1081987/4 .
    
    Currently, metrics_enabled_extra is not being looked up from Hiera,
    and the profile is included from base. So, there is no way to
    customize the arguments.
    
    Hosts: prometheus2006.codfw.wmnet
    Hosts: stat1008.eqiad.wmnet
    Bug: T377804
    Change-Id: I08478d7b78e9267f136e12ac344dda430e14af6f

--- modules/profile/manifests/prometheus/cadvisor.pp
+++ modules/profile/manifests/prometheus/cadvisor.pp
@@ -1,9 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::prometheus::cadvisor (
     Wmflib::Ensure $ensure = lookup('profile::prometheus::cadvisor::ensure'),
+    Array[Prometheus::Cadvisor::Metric] $metrics_enabled_extra = lookup('profile::prometheus::cadvisor::metrics_enabled_extra', {'default_value' => []}),
 ) {
     class { 'prometheus::cadvisor':
-        port   => 4194,
-        ensure => $ensure,
+        port                  => 4194,
+        ensure                => $ensure,
+        metrics_enabled_extra => $metrics_enabled_extra,
     }
 }
