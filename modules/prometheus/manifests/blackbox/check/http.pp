a765f0b638faecd8daa0343e8c3395f13103e9cb
Tiziano Fogli <tfogli@wikimedia.org>
2024-12-05 17:04:46 +0100

    blackbox/http: deployment sites controlled by input parameter instead of ::site
    When the resources are computed to be exported,the tag key is set using ::site.
    
    As a result, the site input parameter of the module doesn’t control
    where the checks are deployed, and all the rules are only being
    deployed to the sites where the generating roles are currently deployed.
    
    Exported resources are imported by prometheus::blackbox::import_checks,
    which correctly uses ::site to determine which resources to import.
    I believe this is correct because, in this way, each Prometheus
    instance in a PoP will only import the resources declared for that
    specific PoP.
    
    Bug: T381561
    Change-Id: Ica643091b158bad0fe52542ec24ffa5a17b63056

--- modules/prometheus/manifests/blackbox/check/http.pp
+++ modules/prometheus/manifests/blackbox/check/http.pp
@@ -206,17 +206,17 @@ define prometheus::blackbox::check::http (
     }
     $module_params = {
         'content' => $module_config.wmflib::to_yaml,
-        'tag'     => "prometheus::blackbox::check::http::${::site}::${prometheus_instance}::module",
+        'tag'     => "prometheus::blackbox::check::http::${site}::${prometheus_instance}::module",
     }
     $alert_rule_params  = {
         'instance' => $prometheus_instance,
         'content' => $alert_config.wmflib::to_yaml,
-        'tag'     => "prometheus::blackbox::check::http::${::site}::${prometheus_instance}::alert",
+        'tag'     => "prometheus::blackbox::check::http::${site}::${prometheus_instance}::alert",
     }
     $target_frag_params = {
         'ensure'  => 'file',
         'content' => $target_config.wmflib::to_yaml,
-        'tag'     => "prometheus::blackbox::check::http::${::site}::${prometheus_instance}::target",
+        'tag'     => "prometheus::blackbox::check::http::${site}::${prometheus_instance}::target",
     }
 
     wmflib::resource::export('prometheus::blackbox::module', $module_title, $title, $module_params)
