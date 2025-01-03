d2a50df5d96f66205c2d0c29c888f0baaf61776e
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-01-03 12:30:10 +0100

    Add an option to pass the Presto firewall settings compatible with nftables
    Introduce a new Hiera option profile::presto::server::firewall_access
    which takes precedence over profile::presto::sever::ferm_srange to
    switch over Presto clusters to configs using firewall::service which
    is compatible with nftables.
    
    Hosts: C:profile::presto::server
    Change-Id: Iab03ba2af40ffb3441e765c432d7ba60f4e4b0dc

--- modules/profile/manifests/presto/server.pp
+++ modules/profile/manifests/presto/server.pp
@@ -54,20 +54,25 @@
 #      - 'ssl_trustore_password'
 #    Default: {}
 #
+# [*firewall*]
+#   Optional array of firewall sets to be used in firewall restrictions based on
+#   firewall::service. This takes precedence over *ferm_srange'
+#
 class profile::presto::server(
-    String        $cluster_name         = lookup('profile::presto::cluster_name'),
-    String        $discovery_uri        = lookup('profile::presto::discovery_uri'),
-    Boolean       $enabled              = lookup('profile::presto::enabled', { 'default_value' => true }),
-    Hash          $node_properties      = lookup('profile::presto::server::node_properties', { 'default_value' => {} }),
-    Hash          $config_properties    = lookup('profile::presto::server::config_properties', { 'default_value' => {} }),
-    Hash          $catalogs             = lookup('profile::presto::server::catalogs', { 'default_value' => {} }),
-    Hash          $log_properties       = lookup('profile::presto::server::log_properties', { 'default_value' => {} }),
-    String        $heap_max             = lookup('profile::presto::server::heap_max', { 'default_value' => '2G' }),
-    String        $ferm_srange          = lookup('profile::presto::server::ferm_srange', { 'default_value' => '$DOMAIN_NETWORKS' }),
-    Array[String] $ssl_certnames        = lookup('profile::presto::server::ssl_certnames', { 'default_value' => [] }),
-    Boolean       $generate_certificate = lookup('profile::presto::server::generate_certificate', { 'default_value' => false }),
-    Boolean       $use_kerberos         = lookup('profile::presto::use_kerberos', { 'default_value' => true }),
-    Boolean       $monitoring_enabled   = lookup('profile::presto::monitoring_enabled', { 'default_value' => false }),
+    String        $cluster_name              = lookup('profile::presto::cluster_name'),
+    String        $discovery_uri             = lookup('profile::presto::discovery_uri'),
+    Boolean       $enabled                   = lookup('profile::presto::enabled', { 'default_value' => true }),
+    Hash          $node_properties           = lookup('profile::presto::server::node_properties', { 'default_value' => {} }),
+    Hash          $config_properties         = lookup('profile::presto::server::config_properties', { 'default_value' => {} }),
+    Hash          $catalogs                  = lookup('profile::presto::server::catalogs', { 'default_value' => {} }),
+    Hash          $log_properties            = lookup('profile::presto::server::log_properties', { 'default_value' => {} }),
+    String        $heap_max                  = lookup('profile::presto::server::heap_max', { 'default_value' => '2G' }),
+    String        $ferm_srange               = lookup('profile::presto::server::ferm_srange', { 'default_value' => '$DOMAIN_NETWORKS' }),
+    Array[String] $ssl_certnames             = lookup('profile::presto::server::ssl_certnames', { 'default_value' => [] }),
+    Boolean       $generate_certificate      = lookup('profile::presto::server::generate_certificate', { 'default_value' => false }),
+    Boolean       $use_kerberos              = lookup('profile::presto::use_kerberos', { 'default_value' => true }),
+    Boolean       $monitoring_enabled        = lookup('profile::presto::monitoring_enabled', { 'default_value' => false }),
+    Optional[Array[String]] $firewall_access = lookup('profile::presto::server::firewall_access'),
     Optional[Hash[String, Hash[String, String]]] $presto_clusters_secrets = lookup('presto_clusters_secrets', { 'default_value' => {} }),
 ) {
 
@@ -211,23 +216,46 @@ class profile::presto::server(
         extra_jvm_configs => $extra_jvm_configs,
     }
 
-    if $presto_clusters_secrets[$cluster_name] {
-        ferm::service{ 'presto-https':
-            proto  => 'tcp',
-            port   => $_config_properties['http-server.https.port'],
-            srange => $ferm_srange,
+
+    if $firewall_access {
+        if $presto_clusters_secrets[$cluster_name] {
+            firewall::service{ 'presto-https':
+                proto    => 'tcp',
+                port     => $_config_properties['http-server.https.port'],
+                src_sets => $firewall_access,
+            }
+        } else {
+            firewall::service{ 'presto-http':
+                proto    => 'tcp',
+                port     => $_config_properties['http-server.http.port'],
+                src_sets => $firewall_access,
+            }
+        }
+
+        firewall::service{ 'presto-jmx-rmiregistry':
+            proto    => 'tcp',
+            port     => $_config_properties['jmx.rmiregistry.port'],
+            src_sets => $firewall_access,
         }
     } else {
-        ferm::service{ 'presto-http':
+        if $presto_clusters_secrets[$cluster_name] {
+            ferm::service{ 'presto-https':
+                proto  => 'tcp',
+                port   => $_config_properties['http-server.https.port'],
+                srange => $ferm_srange,
+            }
+        } else {
+            ferm::service{ 'presto-http':
+                proto  => 'tcp',
+                port   => $_config_properties['http-server.http.port'],
+                srange => $ferm_srange,
+            }
+        }
+
+        ferm::service{ 'presto-jmx-rmiregistry':
             proto  => 'tcp',
-            port   => $_config_properties['http-server.http.port'],
+            port   => $_config_properties['jmx.rmiregistry.port'],
             srange => $ferm_srange,
         }
-    }
-
-    ferm::service{ 'presto-jmx-rmiregistry':
-        proto  => 'tcp',
-        port   => $_config_properties['jmx.rmiregistry.port'],
-        srange => $ferm_srange,
     }
 }
