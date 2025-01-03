69b271b0ad6470b2d4ce3d0c398c34910247afb3
Francesco Negri <fnegri@wikimedia.org>
2025-01-03 16:10:44 +0100

    prometheus-node-kernel-panic: rename to "messages"
    Rename this profile from "kernel_panic" to "kernel_messages", as it
    tracks both panics and other types of messages.
    
    Bug: T382961
    Change-Id: I32ae2e8278735fc6824c152fc14984aa0480ec51

---
+++ modules/prometheus/manifests/node_kernel_messages.pp
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: Apache-2.0
+class prometheus::node_kernel_messages (
+    Wmflib::Ensure $ensure = 'present',
+) {
+    #TODO: remove this after the old files have been cleaned up
+    file { '/usr/local/bin/prometheus-node-kernel-panic':
+      ensure => 'absent',
+    }
+    #TODO: remove this after the old files have been cleaned up
+    systemd::timer::job { 'prometheus-node-kernel-panic':
+      ensure => 'absent',
+    }
+
+    $script = '/usr/local/bin/prometheus-node-kernel-messages'
+    file { $script:
+        ensure => $ensure,
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/prometheus/usr/local/bin/prometheus-node-kernel-messages.sh',
+    }
+
+    systemd::timer::job { 'prometheus-node-kernel-messages':
+        ensure      => $ensure,
+        user        => 'root',
+        description => 'Generate prometheus stats about kernel messages',
+        command     => $script,
+        interval    => {
+            'start'    => 'OnCalendar',
+            'interval' => 'minutely',
+        },
+        require     => [
+          File[$script],
+          Class['prometheus::node_exporter'],
+          Package['jq'],
+        ],
+    }
+}
