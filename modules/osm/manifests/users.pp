566fd45843291d72d9e7c0f1077d105957aaccde
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-01-07 14:13:40 +0100

    osm: On Bookworm create OSM users using system::sysuser
    Hosts: C:osm::users
    Bug: T381565
    Change-Id: I5a40344cd089c80bd2fbe5582a3f35fc0c4578d1

--- modules/osm/manifests/users.pp
+++ modules/osm/manifests/users.pp
@@ -1,21 +1,35 @@
 # SPDX-License-Identifier: Apache-2.0
 class osm::users {
-    group { 'osm':
-        ensure => present,
-        system => true,
-    }
+    if debian::codename::ge('bookworm') {
+        systemd::sysuser { 'osm':
+            usertype => 'group',
+        }
 
-    user { 'osmupdater':
-        ensure => present,
-        system => true,
-        groups => 'osm',
-        home   => '/nonexistent',
-    }
+        systemd::sysuser { 'osmupdater':
+            additional_groups => ['osm'],
+        }
 
-    user { 'osmimporter':
-        ensure => present,
-        system => true,
-        groups => 'osm',
-        home   => '/nonexistent',
+        systemd::sysuser { 'osmimporter':
+            additional_groups => ['osm'],
+        }
+    } else {
+        group { 'osm':
+            ensure => present,
+            system => true,
+        }
+
+        user { 'osmupdater':
+            ensure => present,
+            system => true,
+            groups => 'osm',
+            home   => '/nonexistent',
+        }
+
+        user { 'osmimporter':
+            ensure => present,
+            system => true,
+            groups => 'osm',
+            home   => '/nonexistent',
+        }
     }
 }
