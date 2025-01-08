631317827dc54e45204c952bc4874c48a19b4988
Ottomata <aotto@wikimedia.org>
2025-01-08 14:56:26 -0500

    Remove profile::cache::kafka::eventlogging
    Bug: T238230
    Change-Id: Id70e853de742d89291fe8605c37b4745f42cc50c

--- modules/role/manifests/cache/text.pp
+++ modules/role/manifests/cache/text.pp
@@ -13,11 +13,5 @@ class role::cache::text {
     # (called 'statsv') then consumes these and emits metrics.
     include profile::cache::kafka::statsv
 
-    # varnishkafka eventlogging listens for eventlogging beacon
-    # requests and logs them to the eventlogging-client-side
-    # topic.  EventLogging servers consume and process this
-    # topic into many JSON based kafka topics for further
-    # consumption.
-    include profile::cache::kafka::eventlogging
     include profile::cache::haproxykafka
 }
