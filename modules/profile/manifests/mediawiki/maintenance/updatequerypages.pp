d87f8b6cb73264417f3cb31ac5aa6c0840df12c3
Amir Sarabadani <ladsgroup@gmail.com>
2025-01-10 02:56:35 +0100

    mediawiki: Remove special-case wikitech update query page runs
    wikitech is part of s6, so it's being run twice in one day every time.
    
    Change-Id: I9c58a8dc2102ae7ba1c85fb6c785777d891eb5c3

--- modules/profile/manifests/mediawiki/maintenance/updatequerypages.pp
+++ modules/profile/manifests/mediawiki/maintenance/updatequerypages.pp
@@ -3,11 +3,11 @@ class profile::mediawiki::maintenance::updatequerypages {
 
     # add periodic jobs - usage: <cluster>@<day of month> (monthday currently unused, only sets cronjob name)
     # Wikidata has several jobs disabled: T234948, T239072
-    profile::mediawiki::maintenance::updatequerypages::ancientpages { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18', 'wikitech@19']: }
-    profile::mediawiki::maintenance::updatequerypages::fewestrevisions { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 'wikitech@19']: }
-    profile::mediawiki::maintenance::updatequerypages::wantedpages { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18', 'wikitech@19']: }
-    profile::mediawiki::maintenance::updatequerypages::mostrevisions { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 'wikitech@19']: }
-    profile::mediawiki::maintenance::updatequerypages::mostlinked { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 'wikitech@19']: }
+    profile::mediawiki::maintenance::updatequerypages::ancientpages { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18']: }
+    profile::mediawiki::maintenance::updatequerypages::fewestrevisions { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17']: }
+    profile::mediawiki::maintenance::updatequerypages::wantedpages { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18']: }
+    profile::mediawiki::maintenance::updatequerypages::mostrevisions { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17']: }
+    profile::mediawiki::maintenance::updatequerypages::mostlinked { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17']: }
     profile::mediawiki::maintenance::updatequerypages::deadendpages { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18', 'wikitech@19']: }
     profile::mediawiki::maintenance::updatequerypages::uncatpages { ['s4@14']: }
 
