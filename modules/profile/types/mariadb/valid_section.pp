22acd6cf4f9507d1061b012764c3972820bafa11
Manuel Arostegui <marostegui@wikimedia.org>
2025-01-14 10:21:16 +0000

    valid_sections.pp: Add pc6 and pc7
    These are two new sections
    
    Bug: T383234
    Bug: T383235
    Change-Id: I138150399e065d86e925c063e2e95953be4d32e9

--- modules/profile/types/mariadb/valid_section.pp
+++ modules/profile/types/mariadb/valid_section.pp
@@ -8,7 +8,7 @@ type Profile::Mariadb::Valid_section = Enum[
     # MediaWiki page content (External Storage)
     'es1', 'es2', 'es3', 'es4', 'es5', 'es6', 'es7',
     # MediaWiki ParserCache
-    'pc1', 'pc2', 'pc3', 'pc4','pc5',
+    'pc1', 'pc2', 'pc3', 'pc4','pc5','pc6','pc7',
     # Misc
     'm1', 'm2', 'm3', 'm5', 'backup1-eqiad', 'backup1-codfw',
     # Test
