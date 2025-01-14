6c2afd109340ebb10aac2a06e20d01aa3a789512
Antoine Musso <hashar@free.fr>
2025-01-14 10:03:05 +0100

    gerrit: restore IP addresses in ssh_known_hosts
    profile::gerrit::sshkey maintains the ssh_known_hosts for
    gerrit.wikimedia.org in both production and WMCS.
    
    44e8c4f8585 made it the single source of trust by factoring out:
    - the existing code that was in profile::gerrit and had host_aliases
      using `ipresolve()`
    - another code in profile::zuul::merger which did not have any
      host_aliases
    
    The second was prefered and in the process we have lost the IP aliases.
    This causes the weekly `scap clean` to emit:
    
        RSA host key for IP address '2620:0:861:2:208:80:154:151' not in
        list of known hosts.
    
    Restore the host_aliases using `ipresolve()`.
    
    Bug: T303828
    Hosts: O:deployment_server::kubernetes
    Hosts: P:gerrit
    Hosts: P:zuul::server
    Change-Id: I864ae7d62edf27efb7c8f7bf326905f5f9187736

--- modules/profile/manifests/gerrit/sshkey.pp
+++ modules/profile/manifests/gerrit/sshkey.pp
@@ -8,10 +8,13 @@ define profile::gerrit::sshkey (
     Boolean                    $exported = false,
     Optional[Stdlib::Unixpath] $target   = undef,
 ) {
-
     $params = {
         'ensure' => $ensure,
         'name'   => 'gerrit.wikimedia.org',
+        'host_aliases' => [
+            ipresolve('gerrit.wikimedia.org', 4),
+            ipresolve('gerrit.wikimedia.org', 6),
+        ],
         'key'    => 'AAAAB3NzaC1yc2EAAAADAQABAAAAgQCF8pwFLehzCXhbF1jfHWtd9d1LFq2NirplEBQYs7AOrGwQ/6ZZI0gvZFYiEiaw1o+F1CMfoHdny1VfWOJF3mJ1y9QMKAacc8/Z3tG39jBKRQCuxmYLO1SWymv7/Uvx9WQlkNRoTdTTa9OJFy6UqvLQEXKYaokfMIUHZ+oVFf1CgQ==',
         'type'   => 'ssh-rsa',
         'target' => $target,
