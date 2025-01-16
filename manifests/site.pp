95ab716b61944423a98663bb4608f477f51031bd
Jelto Wodstrcil <jwodstrcil@wikimedia.org>
2025-01-16 10:32:52 +0100

    Rename mw235[0-3] to wikikube-worker223[2-5]
    Puppet configuration for renaming mw nodes to wikikube-worker.
    
    Bug: T377877
    Change-Id: Id7445b89e274e49d0f4dc18550461c9e761e8cff

--- manifests/site.pp
+++ manifests/site.pp
@@ -1682,7 +1682,7 @@ node /^wikikube-worker21([0-9][0-9])\.codfw\./ {
     role(kubernetes::worker)
 }
 
-node /^wikikube-worker22([0-2][0-9]|3[0-1])\.codfw\./ {
+node /^wikikube-worker22([0-2][0-9]|3[0-5])\.codfw\./ {
     role(kubernetes::worker)
 }
 
@@ -1711,9 +1711,6 @@ node /^mw231[01]\.codfw\./ {
   role(kubernetes::worker)
 }
 node /^mw233[5-8]\.codfw\./ {
-  role(kubernetes::worker)
-}
-node /^mw235[0-3]\.codfw\./ {
   role(kubernetes::worker)
 }
 node /^parse10(0[1-9]|1[0-9]|2[01234])\.eqiad\./ {
