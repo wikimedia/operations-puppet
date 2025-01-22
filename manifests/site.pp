b1692186e52a02f746754828476458f7f447bfe2
Kamila Součková <ksouckova@wikimedia.org>
2025-01-22 16:03:47 +0100

    wikikube: rename mw14[76-81] to wikikube-worker*
    Piggybacking on containerd migration reimages.
    
    mw1476 -> wikikube-worker1129
    mw1477 -> wikikube-worker1130
    mw1478 -> wikikube-worker1131
    mw1479 -> wikikube-worker1132
    mw1480 -> wikikube-worker1133
    mw1481 -> wikikube-worker1134
    
    Bug: T365571
    Change-Id: I1d8df373a6bc99d636927aa62872dc3ca41b2a51

--- manifests/site.pp
+++ manifests/site.pp
@@ -1640,7 +1640,7 @@ node /^wikikube-worker10(0[2-7]|1[12569]|[2-9][0-9])\.eqiad\./ {
     role(kubernetes::worker)
 }
 
-node /^wikikube-worker11([0-1][0-9]|2[0-8])\.eqiad\./ {
+node /^wikikube-worker11([0-2][0-9]|3[0-4])\.eqiad\./ {
     role(kubernetes::worker)
 }
 
@@ -1662,10 +1662,7 @@ node /^wikikube-worker22([0-3][0-9]|4[0-1])\.codfw\./ {
     role(kubernetes::worker)
 }
 
-node /^mw147[6-9]\.eqiad\./ {
-    role(kubernetes::worker)
-}
-node /^mw148[0-8]\.eqiad\./ {
+node /^mw148[2-8]\.eqiad\./ {
     role(kubernetes::worker)
 }
 node /^mw1453\.eqiad\./ {
