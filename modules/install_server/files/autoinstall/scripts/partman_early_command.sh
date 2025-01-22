abe21373cf81a98f36f47c373de4a161e165e9ef
Andrew Bogott <abogott@wikimedia.org>
2025-01-22 12:13:34 -0600

    cephosd partman: add some debug lines to the partman recipe
    Change-Id: Iad45fd08447ca1f0df6dfe667ac14472de9ab4fa

--- modules/install_server/files/autoinstall/scripts/partman_early_command.sh
+++ modules/install_server/files/autoinstall/scripts/partman_early_command.sh
@@ -55,6 +55,11 @@ configure_cephosd_disks() {
 cat > /tmp/dynamic_disc.cfg <<EOF
 d-i partman-auto/disk   string ${devices}
 d-i grub-installer/bootdev  string  ${devices}
+# DEBUG devices is ${devices}
+# DEBUG boot_parts is ${boot_parts}
+# DEBUG swap_parts is ${swap_parts}
+# DEBUG root_parts is ${root_parts}
+#
 # Parameters are:
 # <raidtype> <devcount> <sparecount> <fstype> <mountpoint> \\
 #   <devices> <sparedevices>
@@ -68,6 +73,8 @@ d-i partman-auto-raid/recipe string  \
         1    2    0    lvm    -      \
             ${root_parts}      \
         .
+
+# DEBUG and that's the end
 EOF
 debconf-set-selections /tmp/dynamic_disc.cfg
 }
