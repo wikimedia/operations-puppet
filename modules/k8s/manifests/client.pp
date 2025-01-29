2f942a21fc974e497946119e4f0d3779c5c89138
Janis Meybohm <jmeybohm@wikimedia.org>
2025-01-29 12:05:55 +0100

    k8s::client: Allow for install of all kubectl versions
    While running kubernetes clusters on multiple k8s versions, we need the
    deployment hosts to have matching kubectl versions installed.
    This extends k8s::client so that it installs all kubectl versions for
    all k8s versions we're running clusters on if no version is explicitely
    requested.
    
    Hosts: auto
    Bug: T341984
    Change-Id: Icba2fa0c0810ef6bdd09eebcae53640ca8d756fa

--- modules/k8s/manifests/client.pp
+++ modules/k8s/manifests/client.pp
@@ -1,10 +1,23 @@
 # SPDX-License-Identifier: Apache-2.0
-# Class that sets up and configures kubectl
+# @summary Class that sets up and configures kubectl
+# @param version Optional version of kubectl to install, if not set, all versions we're running clusters on are installed
 class k8s::client (
-    K8s::KubernetesVersion $version,
+    Optional[K8s::KubernetesVersion] $version,
 ) {
-    k8s::package { 'kubectl':
-        package => 'client',
-        version => $version,
+    if $version != undef {
+        k8s::package { 'kubectl':
+            package => 'client',
+            version => $version,
+        }
+    } else {
+        # Install kubectl for all k8s versions we're running
+        k8s::fetch_clusters(false).map | $_, K8s::ClusterConfig $config | {
+            $config['version']
+        }.unique.each |$version| {
+            k8s::package { "kubectl-${version}":
+                package => 'client',
+                version => $version,
+            }
+        }
     }
 }
