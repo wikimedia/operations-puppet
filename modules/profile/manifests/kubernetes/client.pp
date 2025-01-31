fef1d3778fdfb17152a34c4f1e0794d2ed8adf7f
JMeybohm <jmeybohm@wikimedia.org>
2025-01-31 10:32:05 +0000

    k8s::client: Allow for install of all kubectl versions
    Install kubernetes-client packages matching all k8s cluster versions if
    profile::kubernetes::client::version is undefined.
    
    Hosts: C:k8s::client
    Bug: T388388
    Change-Id: I0b49cb3c8ba93039d34427d4b3dc128b862839cf

--- modules/profile/manifests/kubernetes/client.pp
+++ modules/profile/manifests/kubernetes/client.pp
@@ -3,7 +3,7 @@
 # NOTE: Resist the urge to just old it in some other profile, it's been split
 # off in its own profile so that it can be reused in e.g. deployment servers
 class profile::kubernetes::client (
-    K8s::KubernetesVersion $version = lookup('profile::kubernetes::client::version',),
+    Optional[K8s::KubernetesVersion] $version = lookup('profile::kubernetes::client::version', { default_value => undef }),
 ) {
     class { 'k8s::client':
         version => $version,
