fc3b63471fc52c430b778636df96712fde1bd5cd
Arnaud Bran <abran@wikimedia.org>
2025-02-05 13:30:24 +0100

    rt: discarding modules about request tracker
    Discarding all request tracker module components
    
    Bug: T384595
    Change-Id: I8beba57f4f71cb7f4dda5803357a3a4e9cad2934

--- modules/requesttracker/manifests/aliases.pp
+++
@@ -1,8 +0,0 @@
-class requesttracker::aliases {
-    file { '/etc/exim4/aliases/rt':
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0444',
-        source => 'puppet:///modules/requesttracker/rt.aliases',
-    }
-}
