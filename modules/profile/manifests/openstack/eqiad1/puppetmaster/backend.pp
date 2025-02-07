b6e1a9239ee48b3b5976172f67d873f4788b26b0
Andrew Bogott <abogott@wikimedia.org>
2025-02-07 08:48:03 -0600

    Cleanup: remove old wmcs puppetmaster frontend/backend code
    This is now managed via role::puppetserver::cloud_vps_global and new
    puppet 7 classes.
    
    Hosts: cloudinfra-cloudvps-puppetserver-1.cloudinfra.eqiad1.wikimedia.cloud
    Hosts: cloudinfra-internal-puppetserver-1.cloudinfra.eqiad1.wikimedia.cloud
    Hosts: enc-1.cloudinfra.eqiad1.wikimedia.cloud
    Change-Id: I3fa11ef5c15ba527c26f720e26122460b47416d4

--- modules/profile/manifests/openstack/eqiad1/puppetmaster/backend.pp
+++
@@ -1,9 +0,0 @@
-class profile::openstack::eqiad1::puppetmaster::backend(
-    Stdlib::Host $puppetmaster_ca = lookup('profile::openstack::eqiad1::puppetmaster::ca'),
-    Hash[String, Puppetmaster::Backends] $puppetmasters = lookup('profile::openstack::eqiad1::puppetmaster::servers'),
-) {
-    class {'::profile::openstack::base::puppetmaster::backend':
-        puppetmaster_ca => $puppetmaster_ca,
-        puppetmasters   => $puppetmasters,
-    }
-}
