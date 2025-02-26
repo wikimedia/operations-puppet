6e8987d1614778b312fcf26ef7c0f8018a0eff45
Valentin Gutierrez <vgutierrez@wikimedia.org>
2025-02-26 17:34:43 +0100

    hiera,cephadm: Enable IPIP on apus@codfw
    Migration can be started with the following cookbook execution from
    your favorite cumin host:
    `sudo cookbook sre.loadbalancer.migrate-service-ipip --dc codfw --role cephadm::rgw apus`
    
    Hosts: lvs1019.eqiad.wmnet,lvs1020.eqiad.wmnet
    Hosts: lvs2013.codfw.wmnet,lvs2014.codfw.wmnet
    Hosts: moss-fe1001.eqiad.wmnet,moss-fe2001.codfw.wmnet
    Bug: T387290
    Change-Id: Ic7e4fe1959153eacbdf4a0a87fab93bdca16b158

--- modules/role/manifests/cephadm/rgw.pp
+++ modules/role/manifests/cephadm/rgw.pp
@@ -9,6 +9,7 @@ class role::cephadm::rgw {
     include profile::firewall
 
     include profile::lvs::realserver
+    include profile::lvs::realserver::ipip
 
     include profile::tlsproxy::envoy
 
