4fee04ba0347ba58a3122462dba8e6575cd53b11
Valentin Gutierrez <vgutierrez@wikimedia.org>
2025-02-27 15:00:43 +0100

    hiera,prometheus: Enable IPIP on prometheus(-https)?@codfw
    Migration can be started with the following cookbook execution
    from your favorite cumin host:
    sudo cookbook sre.loadbalancer.migrate-service-ipip --dc codfw --role
    prometheus prometheus prometheus-https
    
    Hosts: lvs1019.eqiad.wmnet,lvs1020.eqiad.wmnet
    Hosts: lvs2013.codfw.wmnet,lvs2014.codfw.wmnet
    Hosts: prometheus2005.codfw.wmnet,prometheus1005.eqiad.wmnet
    Bug: T387302
    Bug: T387303
    Change-Id: I1f500d3a58e6ba165cd051cc38ebcbb4a0aedfb8

--- modules/role/manifests/prometheus.pp
+++ modules/role/manifests/prometheus.pp
@@ -4,6 +4,7 @@ class role::prometheus {
     include profile::firewall
 
     include profile::lvs::realserver
+    include profile::lvs::realserver::ipip
 
     include profile::prometheus::common
 
