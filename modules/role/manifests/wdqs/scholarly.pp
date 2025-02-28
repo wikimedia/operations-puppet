7f1883270769e58d06878c8b373e74f198391376
Valentin Gutierrez <vgutierrez@wikimedia.org>
2025-02-28 17:04:31 +0100

    hiera,wdqs: Enable IPIP on wdqs-scholarly@codfw
    Migration can be started with the following cookbook execution
    from your favorite cumin host:
    sudo cookbook sre.loadbalancer.migrate-service-ipip --dc codfw --role
    wdqs::scholarly wdqs-scholarly
    
    Hosts: lvs1019.eqiad.wmnet,lvs1020.eqiad.wmnet
    Hosts: lvs2013.codfw.wmnet,lvs2014.codfw.wmnet
    Hosts: wdqs1023.eqiad.wmnet,wdqs2023.codfw.wmnet
    Bug: T387316
    Change-Id: Ic03c9729edc043adab181d220d842140ff3f1e81

--- modules/role/manifests/wdqs/scholarly.pp
+++ modules/role/manifests/wdqs/scholarly.pp
@@ -12,6 +12,7 @@ class role::wdqs::scholarly {
     require profile::query_service::monitor::wikidata_scholarly
     # Production specific profiles
     include profile::lvs::realserver
+    include profile::lvs::realserver::ipip
     # Public endpoint specific profiles
     include profile::tlsproxy::envoy # TLS termination
 }
