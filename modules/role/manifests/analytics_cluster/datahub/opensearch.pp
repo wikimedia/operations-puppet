564e1536b4f9f9390b407f740bf522551f6b7835
Valentin Gutierrez <vgutierrez@wikimedia.org>
2025-03-03 11:50:58 +0100

    hiera,analytics_cluster: Enable IPIP on datahubsearch@eqiad
    Migration can be started with the following cookbook execution
    from your favorite cumin host:
    sudo cookbook sre.loadbalancer.migrate-service-ipip --dc eqiad --role
    analytics_cluster::datahub::opensearch datahubsearch
    
    Hosts: lvs1019.eqiad.wmnet,lvs1020.eqiad.wmnet
    Hosts: lvs2013.codfw.wmnet,lvs2014.codfw.wmnet
    Hosts: datahubsearch1001.eqiad.wmnet
    Bug: T387306
    Change-Id: Ifd0b566b6bba1a2903fe4ade304de9100044f04a

--- modules/role/manifests/analytics_cluster/datahub/opensearch.pp
+++ modules/role/manifests/analytics_cluster/datahub/opensearch.pp
@@ -5,4 +5,5 @@ class role::analytics_cluster::datahub::opensearch {
     include profile::rsyslog::udp_json_logback_compat
     include profile::opensearch::monitoring::base_checks
     include profile::lvs::realserver
+    include profile::lvs::realserver::ipip
 }
