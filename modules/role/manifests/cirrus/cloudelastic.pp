4811d0b32e8fef7200232b914502a0084fe3eed1
Brian King <bking@wikimedia.org>
2025-03-06 14:19:48 +0000

    cloudelastic: include lvs profile for opensearch hosts
    We are migrating from elasticsearch to opensearch.
    Ensure opensearch hosts have the correct LVS (load
    balancing) config.
    
    Hosts: lvs1018.eqiad.wmnet
    Hosts: cloudelastic1007.eqiad.wmnet
    Bug: T387904
    Change-Id: I88721191819b43ef5f265e9e3eb9d5c2c80fbc31

--- modules/role/manifests/cirrus/cloudelastic.pp
+++ modules/role/manifests/cirrus/cloudelastic.pp
@@ -6,6 +6,8 @@
 class role::cirrus::cloudelastic {
     include profile::base::production
     include profile::firewall
+    include profile::lvs::realserver
+    include profile::lvs::realserver::ipip
     include profile::opensearch::cirrus::server
     include profile::logstash::gelf_relay
 }
