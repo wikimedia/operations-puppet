31a2024674e769259dab9d9f7d22590ca8fa89ae
Clément Goubert <cgoubert@wikimedia.org>
2025-03-07 17:13:18 +0100

    periodic_jobs: Cleanup last wikitech jobs
    Remove absented resources
    
    Hosts: mwmaint2002.codfw.wmnet
    Bug: T388249
    Change-Id: Iad431cd69a793488408f4203624488c355db403f

--- modules/profile/manifests/mediawiki/maintenance/updatequerypages.pp
+++ modules/profile/manifests/mediawiki/maintenance/updatequerypages.pp
@@ -11,12 +11,6 @@ class profile::mediawiki::maintenance::updatequerypages {
     profile::mediawiki::maintenance::updatequerypages::deadendpages { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18']: }
     profile::mediawiki::maintenance::updatequerypages::uncatpages { ['s4@14']: }
 
-    profile::mediawiki::maintenance::updatequerypages::ancientpages { ['wikitech@19']: ensure => absent}
-    profile::mediawiki::maintenance::updatequerypages::fewestrevisions { ['wikitech@19']: ensure => absent}
-    profile::mediawiki::maintenance::updatequerypages::wantedpages { ['wikitech@19']: ensure => absent}
-    profile::mediawiki::maintenance::updatequerypages::mostrevisions { ['wikitech@19']: ensure => absent}
-    profile::mediawiki::maintenance::updatequerypages::mostlinked { ['wikitech@19']: ensure => absent}
-    profile::mediawiki::maintenance::updatequerypages::deadendpages { ['wikitech@19']: ensure => absent}
 
     include profile::mediawiki::maintenance::updatequerypages::enwiki::cronjob
 }
