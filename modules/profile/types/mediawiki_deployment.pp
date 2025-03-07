820b8541ac74bc7dc7028a56b43cacd7d9d894be
Scott French <swfrench@wikimedia.org>
2025-03-06 20:52:06 -0800

    Profile::Mediawiki_deployment: add 'deploy' field to release config
    The 'deploy' field is an optional boolean (defaults true in scap), that
    controls whether scap manages deployents of the release. If false, it
    manages only the /etc/helmfile-values/mediawiki/release values file.
    
    Also adds a basic file header comment, including a callout to the
    DeploymentsConfig docstring for details on hash fields.
    
    Bug: T387917
    Change-Id: If1aa9ee358a631a40c3ef68bf816f5198dac43f8

--- modules/profile/types/mediawiki_deployment.pp
+++ modules/profile/types/mediawiki_deployment.pp
@@ -1,9 +1,20 @@
+# SPDX-License-Identifier: Apache-2.0
+# == Type: Profile::Mediawiki_deployment
+#
+# A puppet hash that represents a single Scap DeploymentsConfig file item,
+# corresponding to a single MediaWiki-on-k8s deployment managed by Scap.
+#
+# For a detailed description of hash fields, consult the DeploymentsConfig
+# docstring in
+# https://gitlab.wikimedia.org/repos/releng/scap/-/blob/master/scap/kubernetes.py.
+#
 type Profile::Mediawiki_deployment = Struct[{
     'namespace'   => String,
     'releases'    => Hash[String, Struct[{
         'mw_flavour'  => Optional[String],
         'web_flavour' => Optional[String],
         'stage'       => Optional[Enum['canaries']],
+        'deploy'      => Optional[Boolean],
     }]],
     'mw_flavour'  => String,
     'web_flavour' => String,
