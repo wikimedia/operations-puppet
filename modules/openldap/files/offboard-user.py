e07e073b0e60a691789f29cefaf2f321610c92a5
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-03-10 12:52:22 +0100

    Add logstash-access to list of groups to drop on offboarding
    Bug: T376790
    Change-Id: I5410807aa40bb09f30886793171d43f1e5b4cbb5

--- modules/openldap/files/offboard-user.py
+++ modules/openldap/files/offboard-user.py
@@ -177,6 +177,7 @@ delete: userPassword
                          'cn=airflow-platform-eng-ops,ou=groups,dc=wikimedia,dc=org',
                          'cn=airflow-analytics-product-ops,ou=groups,dc=wikimedia,dc=org',
                          'cn=airflow-ml-ops,ou=groups,dc=wikimedia,dc=org',
+                         'cn=logstash-access,ou=groups,dc=wikimedia,dc=org',
                          'cn=tools.admin,ou=servicegroups,dc=wikimedia,dc=org']
     ldapdata = ldap_conn.search_s(
         base_dn,
