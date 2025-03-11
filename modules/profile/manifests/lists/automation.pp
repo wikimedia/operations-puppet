77cb753ff25e78f581c4b371a0e3ebde9a852aef
Daniel Zahn <dzahn@wikimedia.org>
2025-03-11 15:32:30 -0700

    lists/stewards: allow switching some lists to actual "wet"-run
    So far the system for automating list member syncs for steward-related
    lists had all lists set to just a dry-run where we see what _would_
    happen without actually making changes.
    
    Now the system is ready to switch the first list, stewards-l, to
    actually run the sync while other lists should stay with the dry runs.
    
    https://en.wikipedia.org/wiki/Dry_run_(testing) says the term dry
    run comes from firefighting and the opposite was actually called
    a "wet run", so we are going to use that term here.
    
    Switch stewards-l to actually running the sync command.
    
    Bug: T388354
    Change-Id: Ib6046e68971995f048c2f0fa7945eee36b4dbb8b

--- modules/profile/manifests/lists/automation.pp
+++ modules/profile/manifests/lists/automation.pp
@@ -7,7 +7,8 @@ class profile::lists::automation (
     Stdlib::Host $lists_host = lookup('lists_primary_host'),
     Stdlib::Host $stewards_host = lookup('stewards_primary_host'),
     Wmflib::Ensure $ensure = lookup('profile::lists::automation::ensure', { default_value => 'absent' }),
-    Array[String] $lists_to_sync = lookup('profile::lists::automation::lists_to_sync'),
+    Array[String] $lists_to_sync_dry = lookup('profile::lists::automation::lists_to_sync_dry_run'),
+    Array[String] $lists_to_sync_wet = lookup('profile::lists::automation::lists_to_sync_wet_run'),
 ){
 
     wmflib::dir::mkdir_p($data_dir, {
@@ -27,10 +28,19 @@ class profile::lists::automation (
         default => 'absent',
     }
 
-    each($lists_to_sync) |$list_name| {
+    each($lists_to_sync_dry) |$list_name| {
         mailman3::sync_list_members { "sync-members-${list_name}":
             ensure    => $list_sync_ensure,
             list_name => $list_name,
+            dry_run   => 'y',
+        }
+    }
+
+    each($lists_to_sync_wet) |$list_name| {
+        mailman3::sync_list_members { "sync-members-${list_name}":
+            ensure    => $list_sync_ensure,
+            list_name => $list_name,
+            dry_run   => 'n',
         }
     }
 }
