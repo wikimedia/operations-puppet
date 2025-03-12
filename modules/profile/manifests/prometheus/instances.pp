9764087f1b6273cb484f69bc2489673fde672f77
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2025-03-12 14:46:13 +0100

    prometheus: cleanup instance functionality
    Clean up instances via prometheus::instances_cleanup hiera.
    
    Bug: T383232
    Change-Id: I657148d08b34f63f3a7f9953fafeab69b35e5186

--- modules/profile/manifests/prometheus/instances.pp
+++ modules/profile/manifests/prometheus/instances.pp
@@ -1,7 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 # Instantiate all prometheus instances for this host
 
-class profile::prometheus::instances {
+class profile::prometheus::instances (
+    $instances_cleanup = lookup('prometheus::instances_cleanup', {'default_value' => {}}),
+) {
     # On LVM systems, ensure that this host's instances filesystems are provisioned
     # Said provisioning is done by prometheus-provision-fs and the user is prompted to run it.
     # We could let Puppet do the provisioning, but we generally refrain from
@@ -28,7 +30,6 @@ class profile::prometheus::instances {
 
     $k8s_clusters = k8s::fetch_clusters()
 
-    # XXX clean up moved instances
     prometheus::instances().each |$instance, $config| {
         if $facts['networking']['fqdn'] in $config['hosts'] {
             # k8s-related instances are handled separately
@@ -38,6 +39,48 @@ class profile::prometheus::instances {
                 }
             } else {
                 include "profile::prometheus::${instance}"
+            }
+        }
+    }
+
+    $instances_cleanup.each |$instance, $config| {
+        if ! ($facts['volume_groups'] and $facts['volume_groups']['vg0']) {
+            fail('prometheus::instances_cleanup requires lvm vg0')
+        }
+
+        if $facts['networking']['fqdn'] in $config['hosts'] {
+            # Disable and stop services first
+            ["prometheus@${instance}", "thanos-sidecar@${instance}"].each |$unit| {
+                # cheeky and it works: will fail catalog compilation if the same instance + host is defined in both
+                # prometheus::instances and prometheus::instances_cleanup, which is not allowed
+                systemd::unit { $unit:
+                    ensure  => absent,
+                    content => '',
+                }
+
+                exec { "disable_${unit}":
+                    path    => ['/usr/bin', '/bin'],
+                    command => "systemctl disable ${unit}",
+                    onlyif  => "systemctl is-enabled ${unit}",
+                }
+            }
+
+            # Make sure we can umount the filesystem and remove from fstab. The vg0 LV is left alone.
+            $mountpoint = "/srv/prometheus/${instance}"
+            $fstab_entry = "^/dev/vg0/prometheus-${instance}[[:space:]]+${mountpoint}[[:space:]]+ext4.+"
+
+            exec { "umount_${instance}":
+                path    => ['/usr/bin', '/bin'],
+                command => "umount ${mountpoint}",
+                onlyif  => "mountpoint -q ${mountpoint}",
+            }
+
+            file_line { "fstab_remove_${instance}":
+                ensure            => absent,
+                path              => '/etc/fstab',
+                match             => $fstab_entry,
+                match_for_absence => true,
+                require           => Exec["umount_${instance}"],
             }
         }
     }
