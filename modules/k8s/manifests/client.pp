52128b9b772fbb6a6fc2ef313746dc45312b1dff
JMeybohm <jmeybohm@wikimedia.org>
2025-03-14 15:30:39 +0000

    Revert "k8s::client: Allow for install of all kubectl versions"
    This reverts commit fef1d3778fdfb17152a34c4f1e0794d2ed8adf7f.
    
    Change-Id: I28712f6010eea41fe6733737540f5cf3f75cf49b

--- modules/k8s/manifests/client.pp
+++ modules/k8s/manifests/client.pp
@@ -1,23 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
-# @summary Class that sets up and configures kubectl
-# @param version Optional version of kubectl to install, if not set, all versions we're running clusters on are installed
+# Class that sets up and configures kubectl
 class k8s::client (
-    Optional[K8s::KubernetesVersion] $version,
+    K8s::KubernetesVersion $version,
 ) {
-    if $version != undef {
-        k8s::package { 'kubectl':
-            package => 'client',
-            version => $version,
-        }
-    } else {
-        # Install kubectl for all k8s versions we're running
-        k8s::fetch_clusters(false).map | $_, K8s::ClusterConfig $config | {
-            $config['version']
-        }.unique.each |$cluster_version| {
-            k8s::package { "kubectl-${cluster_version}":
-                package => 'client',
-                version => $cluster_version,
-            }
-        }
+    k8s::package { 'kubectl':
+        package => 'client',
+        version => $version,
     }
 }
