19e3e0fd265141d7f01cb06bbd4e7b3fdb806673
Luca Toscano <ltoscano@wikimedia.org>
2025-03-17 10:25:29 +0100

    maps: remove Kartotherian from bare metal nodes
    The service now runs on Wikikube k8s, and the LVS VIP configs
    are gone.
    
    Bug: T389042
    Change-Id: I8756156eea7d39a01b0b7df0f8afe9d228c65fc4

--- modules/role/manifests/maps/replica.pp
+++ modules/role/manifests/maps/replica.pp
@@ -3,10 +3,7 @@ class role::maps::replica {
     include profile::base::production
     include profile::rsyslog::udp_localhost_compat
     include profile::firewall
-    include profile::lvs::realserver
 
-    include profile::maps::apps
     include profile::maps::osm_replica
-    include profile::maps::tlsproxy
     include profile::prometheus::postgres_exporter
 }
