1528d77109e975f4f8602b4491fe741b71eed34f
Arzhel Younsi <ayounsi@wikimedia.org>
2025-03-17 15:53:37 +0100

    type Install_server::Preseed_host::Name fix regex
    It needs a negative lookahead.
    
    Hosts: install1004.wikimedia.org
    Change-Id: Ia52a3b58eefb9930c46797ae9e0e23fa083282fb

--- modules/install_server/types/preseed_host/name.pp
+++ modules/install_server/types/preseed_host/name.pp
@@ -1,3 +1,3 @@
 # SPDX-License-Identifier: Apache-2.0
 # The preseed "name" syntax needs to match a BASH "case" syntax
-type Install_server::Preseed_host::Name = Pattern[/([a-z0-9]|\[|\]|\*|\||\-)+/]
+type Install_server::Preseed_host::Name = Pattern[/^(?:[a-z0-9]|\[|\]|\*|\||\-)+$/]
