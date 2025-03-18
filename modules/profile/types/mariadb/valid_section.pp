2307d6e4a5c412adc8b4c3a41c8bec83473a06c4
Manuel Arostegui <marostegui@wikimedia.org>
2025-03-18 07:28:16 +0100

    valid_section.pp: Add x3
    x3 is a new section that will be a split from s8
    
    Bug: T381475
    Change-Id: I7fb28387ee497019780b92ae1d0cfbc3d3b8ffa3

--- modules/profile/types/mariadb/valid_section.pp
+++ modules/profile/types/mariadb/valid_section.pp
@@ -2,7 +2,7 @@ type Profile::Mariadb::Valid_section = Enum[
     # MediaWiki metadata
     's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8',
     # MediaWiki extension data
-    'x1',
+    'x1', 'x3',
     # MainStash data
     'x2', 'ms1', 'ms2', 'ms3',
     # MediaWiki page content (External Storage)
