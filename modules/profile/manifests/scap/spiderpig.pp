2ca3667ef30f419810f2519e8e16718075cfd9e3
Alexandros Kosiaris <akosiaris@wikimedia.org>
2025-03-19 14:46:49 +0000

    Revert^2 "profile::scap::spiderpig: New profile for setting up SpiderPig"
    This reverts commit 8b41cbfd85e57571b1c2138bc3c2d0e8b82d599a.
    
    Reason for revert: Switchover done, let's try this again
    
    Change-Id: I863592e2510d166e2ae52c98dca3bf73b450667a

---
+++ modules/profile/manifests/scap/spiderpig.pp
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: Apache-2.0
+# == Class: profile::scap::spiderpig
+#
+# Set up SpiderPig services
+
+class profile::scap::spiderpig (
+    Wmflib::Ensure             $ensure_services = 'present', # lint:ignore:wmf_styleguide
+    Stdlib::Port::Unprivileged $spiderpig_port  = lookup('profile::scap::spiderpig::port', {'default_value' => 9000}),
+) {
+    $spiderpig_user = 'spiderpig'
+    $uid = assert_type(Admin::UID::System::Global, 929)
+    $gid = assert_type(Admin::UID::System::Global, 929)
+    $home_dir = assert_type(Stdlib::Unixpath, '/var/lib/spiderpig')
+
+    file { $home_dir:
+        ensure => directory,
+        owner  => $uid,
+        group  => $gid,
+        mode   => '0755',
+    }
+
+    user { $spiderpig_user:
+        uid     => $uid,
+        gid     => $gid,
+        comment => 'SpiderPig jobrunner/apiserver',
+        home    => $home_dir,
+        require => File[$home_dir]
+    }
+
+    systemd::service { 'spiderpig-jobrunner':
+        ensure  => $ensure_services,
+        content => template('profile/scap/spiderpig-jobrunner.service.erb'),
+    }
+
+    systemd::service { 'spiderpig-apiserver':
+        ensure  => $ensure_services,
+        content => template('profile/scap/spiderpig-apiserver.service.erb'),
+    }
+}
