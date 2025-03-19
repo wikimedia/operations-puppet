8f6f4ac2892f220d9a41da5e8de4128f5dbf02d1
Brian King <bking@wikimedia.org>
2025-03-19 13:35:37 +0000

    Cirrus: Prepare production hosts for OpenSearch
    Similar to what we did for relforge in
    Id5a961a352681b7b1948611dee14ee829bde63b2,
    add hieradata and opensearch::cirrus role in preparation
    for migrating existing Elasticsearch hosts to
    OpenSearch.
    
    Bug: T388610
    Change-Id: I84d0f001d68c7cbad0c25d83070b77137f96fc10

---
+++ modules/role/manifests/cirrus/opensearch.pp
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+# = Class: role::cirrus::opensearch
+#
+# This class sets up OpenSearch specifically for CirrusSearch.
+#
+class role::cirrus::opensearch {
+    include profile::base::production
+    include profile::firewall
+    include profile::lvs::realserver
+    include profile::lvs::realserver::ipip
+    include profile::opensearch::cirrus::server
+    include profile::logstash::gelf_relay
+}
