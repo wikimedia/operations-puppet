7f8caae87bd48f3614c83fbb87d97d3bb8032545
Brian King <bking@wikimedia.org>
2025-03-25 21:24:47 +0000

    cirrussearch: create a puppet plan for beta cluster
    As we migrate from Elasticsearch to OpenSearch, we
    need to update beta cluster as well. Add a new
    puppet plan to apply OpenSearch config to beta
    cluster hosts.
    
    Bug: T389971
    Change-Id: I66cd0bca10efb4ced93e024a05f9f6b84415161a

---
+++ modules/role/manifests/cirrus/beta.pp
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+# = Class: role::cirrussearch::beta
+#
+# This class sets up OpenSearch specifically for CirrusSearch on deployment-prep.
+#
+class role::cirrus::beta {
+    include profile::opensearch::cirrus::server
+}
