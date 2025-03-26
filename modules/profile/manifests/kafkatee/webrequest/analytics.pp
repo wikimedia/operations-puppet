ab546f19208e8e248c2410b63f8bc4007c338e19
joal <joal@wikimedia.org>
2025-03-26 17:52:42 +0100

    Update analytics webrequest kafkatee
    * Remove not used hiera parameter
    * Add webrequest-frontend kafkatee instance
    
    Bug: T386177
    Change-Id: I356cdf8d18022e0b40ee69e595dcb1a34291506e

--- modules/profile/manifests/kafkatee/webrequest/analytics.pp
+++ modules/profile/manifests/kafkatee/webrequest/analytics.pp
@@ -1,12 +1,11 @@
 # == Class profile::logging::kafkatee::webrequest::analytics
 #
-# This is a temporary class to help testing a smaller stream
+# This is a class to help testing a smaller stream
 # of webrequests data in the Hadoop Test Cluster.
-# More details in: T212259
+# More details in: T212259 and T386177
 #
 class profile::kafkatee::webrequest::analytics(
     String $kafka_cluster_name = lookup('profile::kafkatee::webrequest::analytics::kafka_cluster_name', {'default_value' => 'jumbo-eqiad'}),
-    String $kafka_target_topic = lookup('profile::kafkatee::webrequest::analytics::kafka_target_topic', {'default_value' => 'webrequest_test_text'}),
 ) {
     ensure_packages('kafkacat')
 
@@ -40,8 +39,45 @@ class profile::kafkatee::webrequest::analytics(
 
     kafkatee::output { 'webrequest-test-output':
         instance_name => 'webrequest-test',
-        destination   => "/usr/bin/kafkacat -P -t ${kafka_target_topic} -b ${kafka_brokers}",
+        destination   => "/usr/bin/kafkacat -P -t webrequest_test_text -b ${kafka_brokers}",
         type          => 'pipe',
         sample        => 1000,
     }
+
+    # Replicate the webrequest_text sampling for the webrequest_frontend_text topic
+    # TODO: When the webrequest data is not produced anymore, remove the code above
+
+    # Include only one webrequest_frontend_text topic partition as inputs,
+    # since we only need a tiny fraction of the traffic.
+    # Note: we used offset => 'end' rather than 'stored'
+    # because we don't need to backfill these files from
+    # buffered kafka data if kafkatee goes down.
+    $input_webrequest_frontend_text = {
+        'topic'      => 'webrequest_frontend_text',
+        'partitions' => '0',
+        'options'    => {
+            'encoding' => 'json',
+        },
+        'offset'     => 'end',
+    }
+
+    # Install kafkatee configured to consume from
+    # the Kafka cluster with webrequest_frontend logs.
+    # The webrequest_frontend logs are in json, so we output
+    # them in the format they are received.
+    kafkatee::instance { 'webrequest-frontend-test':
+        kafka_brokers   => $kafka_config['brokers']['ssl_array'],
+        output_encoding => 'json',
+        inputs          => [$input_webrequest_frontend_text],
+        ssl_enabled     => true,
+        ssl_ca_location => profile::base::certificates::get_trusted_ca_path(),
+    }
+
+    kafkatee::output { 'webrequest-frontend-test-output':
+        instance_name => 'webrequest-frontend-test',
+        destination   => "/usr/bin/kafkacat -P -t webrequest_frontend_test_text -b ${kafka_brokers}",
+        type          => 'pipe',
+        sample        => 100,
+    }
+
 }
