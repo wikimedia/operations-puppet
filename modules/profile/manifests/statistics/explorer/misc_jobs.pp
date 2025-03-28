345c786fe4b69e98b0191c399119df6bb04bb627
Ben Tullis <btullis@wikimedia.org>
2025-03-28 12:30:19 +0000

    Add a cleanup timer for old dumps webrequest logs
    We copy the log files from the clouddumps100[1-2] servers to stat1011
    for analysis to gather metrics around wikidata entity dump usage.
    
    However, we have not had a housekeeping method in place to remove the
    old files, so they are retained on disk for longer than intended.
    
    This commit adds a systemd timer that removes log files older than 90
    days.
    
    Hosts: stat1011.eqiad.wmnet
    Bug: T390123
    Change-Id: I9a1b1a035f6d181b429376a15b82531d1fc37a71

--- modules/profile/manifests/statistics/explorer/misc_jobs.pp
+++ modules/profile/manifests/statistics/explorer/misc_jobs.pp
@@ -6,15 +6,14 @@
 # stat1011. Ideally in the future these jobs will be run
 # on a dedicated VM or similar.
 #
-class profile::statistics::explorer::misc_jobs(
+class profile::statistics::explorer::misc_jobs (
     String              $statsd_host     = lookup('statsd'),
     Array[Stdlib::Host] $labstore_hosts  = lookup('labstore_hosts'),
     Stdlib::Host        $graphite_host   = lookup('graphite_host'),
     Hash[String,String] $wmde_secrets    = lookup('wmde_secrets'),
     Array[String]       $hosts_with_jobs = lookup('profile::statistics::explorer::misc_jobs::hosts_with_jobs'),
 ) {
-
-    if $::hostname in $hosts_with_jobs {
+    if $facts['networking']['hostname'] in $hosts_with_jobs {
         # Performance team statistics scripts and cron jobs
         class { 'statistics::performance': }
 
@@ -33,19 +32,30 @@ class profile::statistics::explorer::misc_jobs(
         # allow only pull based rsyncs, but after T211330 we needed a way
         # to unbreak that use case. This rsync might be removed in the future.
         # TODO: this should be moved to hdfs-rsync.
+        $weblog_dump_path = '/srv/log/webrequest/archive/dumps.wikimedia.org'
+
         $weblog_dump_dirs = [
             '/srv/log/webrequest',
             '/srv/log/webrequest/archive',
-            '/srv/log/webrequest/archive/dumps.wikimedia.org',
+            $weblog_dump_path,
         ]
         file { $weblog_dump_dirs:
             ensure => directory,
         }
         rsync::server::module { 'dumps-webrequest':
-            path          => '/srv/log/webrequest/archive/dumps.wikimedia.org',
+            path          => $weblog_dump_path,
             read_only     => 'no',
             hosts_allow   => $labstore_hosts,
             auto_firewall => true,
+        }
+        systemd::timer::job { 'clean-dumps-webrequest':
+            description => 'Clean old dumps webrequest log files',
+            user        => 'root',
+            interval    => {
+                'start'    => 'OnCalendar',
+                'interval' => 'daily',
+            },
+            command     => "/usr/bin/find ${weblog_dump_path} -type f -mtime +90 -delete",
         }
     }
 }
