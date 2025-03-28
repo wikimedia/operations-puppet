4bf9abdb291f56c0d8a0fc5b136a84215409b49c
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-03-28 08:46:18 +0100

    Create insetup role for SRE o11y with nftables and rename existing one
    Currently all our insetup roles default to Ferm, but with an increasing
    amount of roles defaulting to nftables this is becoming problematic:
    It means these are installed with Ferm initiaally and then Puppet
    applies the switch to nftables. But this switch requires a reboot (to
    unload the iptables kernel modules), which might be missed, causing
    issues later on.
    
    As such to make the choice of the firewall stack part of the role, so
    that it can be explicitly selected when creating the racking task
    for DC ops:
    - rename the exisiting role to _ferm
    - create a new role _nftables with defaults to nftables
    - rename existing entries in site.pp
    
    There are currently no o11y insetup hosts, so nothing needs to be
    updated in site.pp
    
    Bug: T389825
    Change-Id: Ibd94394637ee4ce338f5af444547f37d2dd5d0cc

--- modules/role/manifests/insetup/observability_ferm.pp
+++ modules/role/manifests/insetup/observability_ferm.pp
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
-class role::insetup::observability {
+class role::insetup::observability_ferm {
     include profile::base::production
     include profile::firewall
 }
