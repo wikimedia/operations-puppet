2d56c412968ffc22365d6ad2c189fe80fec1e6c5
Clément Goubert <cgoubert@wikimedia.org>
2025-04-01 12:51:18 +0200

    mw::periodic_jobs: Fix parallel invocation
    Hosts: deploy1003.eqiad.wmnet
    Bug: T388538
    Change-Id: I5bfeff56f2cb9293e9cd4ae01ffe682895789912

--- modules/profile/manifests/mediawiki/maintenance/serviceops_version.pp
+++ modules/profile/manifests/mediawiki/maintenance/serviceops_version.pp
@@ -3,7 +3,7 @@ class profile::mediawiki::maintenance::serviceops_version(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
     profile::mediawiki::periodic_job { 'serviceops_version':
-        command               => '/usr/local/bin/expanddblist large | xargs -I{} -P 4 sh -c \"/usr/local/bin/mwscript Version.php --wiki={}\"',
+        command               => '/usr/local/bin/expanddblist large | xargs -I{} -P4 /usr/local/bin/mwscript Version.php --wiki={}',
         cron_schedule         => '*/10 * * * *',
         team                  => 'sre-serviceops',
         script_label          => 'Version.php',
