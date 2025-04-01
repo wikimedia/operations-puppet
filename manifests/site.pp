f2c04231b208f7b31d62c585a2b69310c0a70683
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-04-01 09:22:29 +0200

    maps-test: Back to insetup
    Will need a fresh reimage with a new Partman recipe, otherwise we lack
    the disk space to actually test latest imposm.
    
    Change-Id: If9e8f53ea599c4607d31e2025c8bd213116ad581

--- manifests/site.pp
+++ manifests/site.pp
@@ -1941,11 +1941,11 @@ node /^maps20(0[5-8]|10)\.codfw\./ {
 }
 
 node /^maps-test200[2-6]\.codfw\./ {
-    role(maps::replica_bookworm)
+    role(insetup::infrastructure_foundations_nftables)
 }
 
 node /^maps-test2001\.codfw\./ {
-    role(maps::master_bookworm)
+    role(insetup::infrastructure_foundations_nftables)
 }
 
 # testing buster master - maps2.0 migration
