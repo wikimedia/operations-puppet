df2dd0e0e752a1277f4d6956edb0a3ae32569e8d
Clément Goubert <cgoubert@wikimedia.org>
2025-04-01 12:08:03 +0200

    mw::periodic_jobs: Test xargs parallelism
    Change the test job to run 4 parallell Version.php calls to see how
    chained templating handles a complex command.
    
    Hosts: deploy1003.eqiad.wmnet
    Bug: T388538
    Change-Id: Ib2b99cbaba62ed62c833d55dd54296244e02a539

--- modules/profile/manifests/mediawiki/maintenance/serviceops_version.pp
+++ modules/profile/manifests/mediawiki/maintenance/serviceops_version.pp
@@ -3,11 +3,11 @@ class profile::mediawiki::maintenance::serviceops_version(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
     profile::mediawiki::periodic_job { 'serviceops_version':
-        command               => '/usr/local/bin/foreachwikiindblist testwikis.dblist Version.php',
+        command               => '/usr/local/bin/expanddblist large | xargs -I{} -P 4 sh -c \"/usr/local/bin/mwscript Version.php --wiki={}\"',
         cron_schedule         => '*/10 * * * *',
         team                  => 'sre-serviceops',
         script_label          => 'Version.php',
-        description           => 'Run version.php on all wikis in testwikis.dblist every 10 minutes to test cronjobs',
+        description           => 'Run version.php on all wikis in large.dblist every 10 minutes with 4 parallel processes',
         kubernetes            => true,
         helmfile_defaults_dir => $helmfile_defaults_dir,
     }
