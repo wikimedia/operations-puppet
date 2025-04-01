e12f9432c0a7eec231c0b4b9052418a003ddd6d0
Brian King <bking@wikimedia.org>
2025-04-01 16:10:47 +0000

    elastic/cirrussearch: use correct role name
    Per modules/role/manifests/cirrus/opensearch.pp,
    the correct role for our new opensearch-based hosts
    is cirrus::opensearch, which is flipped from our previous
    role of elasticsearch::cirrus.
    
    Fix this so our hosts can reimage properly.
    
    Bug: T388610
    Change-Id: Id2958a5e5dae3752bf1433c9a0979c8f986f5a4f

--- manifests/site.pp
+++ manifests/site.pp
@@ -1107,18 +1107,18 @@ node /^elastic11(1[1-9]|2[0-5])\.eqiad\./ {
 # reimages. Row info sourced from regex.yaml
 # ROW A
 ## TEMP BEGIN remove elastic2055 and added entry for cirrussearch2055
-node /^elastic2(056|061|062|069|073|074|075|076|087|088|089|090|091|111|)\.codfw\./ {
+node /^elastic2(056|061|062|069|073|074|075|076|087|088|089|090|091|111)\.codfw\./ {
     role(elasticsearch::cirrus)
 }
 
 node /^cirrussearch2(055)\.codfw\./ {
-    role(opensearch::cirrus)
+    role(cirrus::opensearch)
 }
 ## TEMP END   remove elastic2055 and added entry for cirrussearch2055
 
 
 # As we reimage the above hosts, we'll change both their role and their name.
-# node /^cirrussearch2(055|056|061|062|069|073|074|075|076|087|088|089|090|091|111|)\.codfw\./ {
+# node /^cirrussearch2(055|056|061|062|069|073|074|075|076|087|088|089|090|091|111)\.codfw\./ {
 #     role(opensearch::cirrus)
 # }
 
@@ -1134,12 +1134,12 @@ node /^elastic2110\.codfw\./ {
 }
 
 # node  /^cirrussearch2(56|57|58|63|64|70|77|78|79|80|92|93|94|95|96|97)\.codfw\./ {
-#     role(opensearch::cirrus)
+#     role(cirrus::opensearch)
 #
 # }
 #
 # node ^cirrussearch2.codfw\./ {
-#     role(opensearch::cirrus)
+#     role(cirrus::opensearch)
 # }
 
 # ROW C
@@ -1147,11 +1147,8 @@ node /^elastic2110\.codfw\./ {
 node /^elastic2(059|065|066|071|083|081|082|098|099|100|101|102|103|112|113)\.codfw\./ {
     role(elasticsearch::cirrus)
 }
-
 # node /^cirrussearch2(059|065|066|071|083|081|082|098|099|100|101|102|103|112|113)\.codfw\./ {
 #     role(opensearch::cirrus)
-# }
-
 # ROW D
 
 node /^elastic2(060|067|068|072|084|085|086|104|105|106|107|108|109|114|115)\.codfw\./ {
@@ -1159,7 +1156,7 @@ node /^elastic2(060|067|068|072|084|085|086|104|105|106|107|108|109|114|115)\.co
 }
 
 # node /^cirrussearch2(060|067|068|072|084|085|086|104|105|106|107|108|109|114|115)\.codfw\./ {
-# role(opensearch::cirrus)
+# role(cirrus::opensearch)
 # }
 
 
