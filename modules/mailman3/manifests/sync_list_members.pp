826fd7d6c30c16ff759068130dd000e0413f40f7
Daniel Zahn <dzahn@wikimedia.org>
2025-04-03 11:28:59 -0700

    mailman3: fix quoting in mail_cmd for sync_list_members
    When using heredoc-syntax do not escape double quotes.
    
    Bug: T351202
    Change-Id: I5f7faac64bb58020d530cdf96c0dfc4a13b23f97

--- modules/mailman3/manifests/sync_list_members.pp
+++ modules/mailman3/manifests/sync_list_members.pp
@@ -36,9 +36,9 @@ define mailman3::sync_list_members(
     $mail_cmd = $mail_changes ? {
         true => @("MAILCMD"/L),
             " | /usr/bin/mail \
-            -r \"mailman@lists.wikimedia.org\" \
-            -s \"mailman list member sync\" \
-            -a \"Auto-Submitted: auto-generated\" \
+            -r mailman@lists.wikimedia.org \
+            -s "mailman list member sync" \
+            -a "Auto-Submitted: auto-generated" \
             ${meta_admin_email}\
             | MAILCMD
         false => '',
