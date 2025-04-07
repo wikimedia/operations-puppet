caec321c9836278e0bc00b220025747bc196a523
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2025-04-07 09:38:57 +0200

    profile: drop stretch support
    Change-Id: I70101305f91cf16239c34bb087c6a6e772d26b4d

--- modules/profile/spec/classes/profile_java_spec.rb
+++ modules/profile/spec/classes/profile_java_spec.rb
@@ -5,11 +5,7 @@ describe 'profile::java' do
   on_supported_os(WMFConfig.test_on).each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
-      if facts[:os]['release']['major'].to_i > 9
-        let(:package) { 'openjdk-11-jdk' }
-      else
-        let(:package) { 'openjdk-8-jdk' }
-      end
+      let(:package) { 'openjdk-11-jdk' }
 
       context 'with role unset' do
         it { is_expected.to contain_package(package) }
