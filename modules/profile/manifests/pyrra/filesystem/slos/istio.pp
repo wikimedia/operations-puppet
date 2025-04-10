8895a4acf1f13bd1ccfcee807f532bfbe2930260
Luca Toscano <ltoscano@wikimedia.org>
2025-04-10 16:18:22 +0200

    profile::pyrra::filesystem::slo: add citoid's SLO
    This change does the following:
    1) Removes absented Lift Wing SLOs, they will need to be redone
       properly anyway.
    2) Creates a new define to ease the process of adding new
       canonical/standard latency/requests-based SLOs for K8s
       services served by the Istio Ingress.
    3) Add the citoid SLO config.
    
    Change-Id: Ifb658e00f4e99b3057e62a8cd3848981b9980c88

---
+++ modules/profile/manifests/pyrra/filesystem/slos/istio.pp
@@ -0,0 +1,77 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+define profile::pyrra::filesystem::slos::istio(
+    String $team,
+    String $slo_requests_target,
+    String $slo_latency_target,
+    Array[String] $datacenters = ['eqiad', 'codfw'],
+    String $window = '12w',
+    String $destination_canonical_service = "${title}-production",
+    String $requests_errors_regex = '5..',
+    String $latency_max_seconds_bucket = '5000',
+    String $k8s_cluster_name = 'wikikube',
+    Wmflib::Ensure $ensure = 'present'
+) {
+    $datacenters.each |$datacenter| {
+        pyrra::filesystem::config { "${k8s_cluster_name}-${title}-requests-${datacenter}.yaml":
+          ensure  => $ensure,
+          content => to_yaml({
+            'apiVersion' => 'pyrra.dev/v1alpha1',
+            'kind'       => 'ServiceLevelObjective',
+            'metadata'   => {
+                'name'      => "${title}-requests",
+                'namespace' => 'pyrra-o11y',
+                'labels'    => {
+                    'pyrra.dev/team'    => "${team}", #lint:ignore:only_variable_string
+                    'pyrra.dev/service' => "${title}", #lint:ignore:only_variable_string
+                    'pyrra.dev/site'    => "${datacenter}", #lint:ignore:only_variable_string
+                },
+            },
+            'spec'       => {
+                'target'    => $slo_requests_target,
+                'window'    => $window,
+                'indicator' => {
+                    'ratio' => {
+                        'errors' => {
+                            'metric' => "workload:istio_requests_total{kubernetes_namespace=\"istio-system\", destination_canonical_service=\"${destination_canonical_service}\", response_code=~\"${requests_errors_regex}\", site=\"${datacenter}\" }",
+                        },
+                        'total'  => {
+                            'metric' => "istio_requests_total{kubernetes_namespace=\"istio-system\", destination_canonical_service=\"${destination_canonical_service}\", site=\"${datacenter}\" }",
+                        },
+                    },
+                },
+            },
+          })
+        }
+        pyrra::filesystem::config { "${k8s_cluster_name}-${title}-latency-${datacenter}.yaml":
+          ensure  => $ensure,
+          content => to_yaml({
+            'apiVersion' => 'pyrra.dev/v1alpha1',
+            'kind'       => 'ServiceLevelObjective',
+            'metadata'   => {
+                'name'      => "${title}-latency",
+                'namespace' => 'pyrra-o11y',
+                'labels'    => {
+                    'pyrra.dev/team'    => "${team}", #lint:ignore:only_variable_string
+                    'pyrra.dev/service' => "${title}", #lint:ignore:only_variable_string
+                    'pyrra.dev/site'    => "${datacenter}", #lint:ignore:only_variable_string
+                },
+            },
+            'spec'       => {
+                'target'    => $slo_latency_target,
+                'window'    => $window,
+                'indicator' => {
+                    'latency'  => {
+                        'success' => {
+                            'metric' => "workload:istio_request_duration_milliseconds_bucket{kubernetes_namespace=\"istio-system\", destination_canonical_service=\"${destination_canonical_service}\", le=\"${latency_max_seconds_bucket}\", site=\"${datacenter}\" }",
+                        },
+                        'total'   => {
+                            'metric' => "workload:istio_request_duration_milliseconds_bucket{kubernetes_namespace=\"istio-system\", destination_canonical_service=\"${destination_canonical_service}\", site=\"${datacenter}\" }",
+                        },
+                    },
+                },
+            },
+          })
+        }
+    }
+}
