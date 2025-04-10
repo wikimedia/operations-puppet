f21574d34e367796297c41c974f202b168acf7ec
Ben Tullis <btullis@wikimedia.org>
2025-04-10 14:08:50 +0100

    Temporarily put an-worker1169 back into insetup mode
    We need to reimage this host after a rack move, but puppet doesn't like
    it when hosts don't have 12 data disks mounted. Therefore, we want to
    put it back into insetup mode for the reimage.
    
    Bug: T390169
    Change-Id: Ib5481c5fcf9160dbc5e2ff119937e1228c4df161

--- manifests/site.pp
+++ manifests/site.pp
@@ -116,8 +116,13 @@ node /^an-worker10(6[5-9]|7[89]|8[0-9]|9[0-9])\.eqiad\./ {
 # hieradata/common.yaml hadoop_clusters net_topology
 # to make sure the hostname -> /datacenter/rack/row id is correct.
 # This is used for Hadoop network topology awareness.
-node /^an-worker1(10[0-9]|11[0-9]|12[0-9]|13[0-9]|14[0-9]|15[0-9]|16[0-9]|17[0-7]|18[7-9]|19[0-9]|20[0-8])\.eqiad\./ {
+node /^an-worker1(10[0-9]|11[0-9]|12[0-9]|13[0-9]|14[0-9]|15[0-9]|16[0-8]|17[0-7]|18[7-9]|19[0-9]|20[0-8])\.eqiad\./ {
     role(analytics_cluster::hadoop::worker)
+}
+
+# Temporarily put an-worker1169 back into insetup mode for reimaging
+node /^an-worker1169\.eqiad\./ {
+    role(insetup::data_platform_ferm)
 }
 
 # New an-worker nodes - See #T377878 and #T386390
