86f48728b422d4736fe3c3a4de6653026b3fd09f
Jesse Hathaway <jhathaway@wikimedia.org>
2025-04-15 16:11:52 -0500

    postfix: remove exim aliases
    No longer needed, data has been moved to hiera
    
    Hosts: O:postfix::mx_in, O:postfix::mx_out
    Change-Id: I077ee85cb65931ee4c7f58b66ebf2255de7c2ef5

--- modules/role/manifests/mail/mx.pp
+++ modules/role/manifests/mail/mx.pp
@@ -3,7 +3,6 @@ class role::mail::mx {
     tag 'mx_in'
     include profile::base::production
     include network::constants
-    include privateexim::aliases::private
     include profile::firewall
     include profile::mail::mx
 }
