5706e7cb9dc004ed45b4b73836703f4eae8ea5c7
Scott French <swfrench@wikimedia.org>
2025-04-16 12:48:35 -0700

    P:parsoid::mediawiki: use installed PHP versions for auto-restart
    Previously, this was using wmflib::wmf_php_version(), which simply
    hard-codes the version per debian release.
    
    Instead, use the relevant hiera keys to identify the relevant PHP
    versions for this host, and properly manage absenting.
    
    Hosts: P:parsoid::mediawiki
    Bug: T380485
    Change-Id: I8c7e639de1f8cecd01c6aee94a390453675638ee

--- modules/profile/manifests/parsoid/mediawiki.pp
+++ modules/profile/manifests/parsoid/mediawiki.pp
@@ -2,10 +2,20 @@
 # Things that are needed on a parsoid testing server
 # that has MediaWiki installed but are not needed
 # on a parsoid testreduce server.
-class profile::parsoid::mediawiki {
+class profile::parsoid::mediawiki(
+    Array[Wmflib::Php_version] $php_versions = lookup('profile::mediawiki::php::php_versions'),
+    Array[Wmflib::Php_version] $absented_php_versions = lookup('profile::mediawiki::php::absented_php_versions', {'default_value' => []}),
+) {
 
-    $php_version = wmflib::wmf_php_version()
     profile::auto_restarts::service { 'apache2': }
-    profile::auto_restarts::service { "php${php_version}-fpm": }
     profile::auto_restarts::service { 'envoyproxy': }
+    $php_versions.each |$php_version| {
+        profile::auto_restarts::service { "php${php_version}-fpm": }
+    }
+    $absented_php_versions.each |$php_version| {
+        profile::auto_restarts::service { "php${php_version}-fpm":
+            ensure => absent,
+        }
+    }
+
 }
