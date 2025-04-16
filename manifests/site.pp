447ddfcd31907595a3a7fd82416a39af76da0823
Brian King <bking@wikimedia.org>
2025-04-16 17:27:18 +0000

    cirrussearch: fix row B regex
    Without the "0", Puppet can't find
    the row B cirrussearch hosts. Fix this.
    
    Bug: T388610
    Change-Id: I6ba545e7e3c8a024a1a3e9694c44b8eb7074711d

--- manifests/site.pp
+++ manifests/site.pp
@@ -1138,7 +1138,7 @@ node /^elastic2110\.codfw\./ {
     role(elasticsearch::cirrus)
 }
 
-node  /^cirrussearch2(57|58|63|64|70|77|78|79|80|92|93|94|95|96|97)\.codfw\./ {
+node  /^cirrussearch20(57|58|63|64|70|77|78|79|80|92|93|94|95|96|97)\.codfw\./ {
     role(cirrus::opensearch)
 
 }
