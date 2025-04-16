abf0ab40880f3f9106ddb079aaa9371717b81c8e
Fabrizio Furnari <ffurnari@wikimedia.org>
2025-04-16 16:02:58 +0200

    wmflib: add PATCH method to the Wmflib::HTTP::Method list
    Bug: T392096
    Change-Id: Iaa286c86742f30b5c2f8e0e66e9e292c9c7249a7

--- modules/wmflib/types/http/method.pp
+++ modules/wmflib/types/http/method.pp
@@ -1,2 +1,2 @@
 # https://github.com/puppetlabs/puppetlabs-stdlib/pull/1192
-type Wmflib::HTTP::Method = Enum['GET', 'HEAD', 'OPTIONS', 'PUT', 'DELETE', 'POST']
+type Wmflib::HTTP::Method = Enum['GET', 'HEAD', 'OPTIONS', 'PUT', 'DELETE', 'POST', 'PATCH']
