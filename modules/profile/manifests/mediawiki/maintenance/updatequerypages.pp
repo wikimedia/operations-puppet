7f5af39dc6d7ed93f7234087551309b87f3f98bc
Clément Goubert <cgoubert@wikimedia.org>
2025-04-17 16:21:01 +0200

    updatequerypages: Move all to sharded_periodic_job
    In preparation for the move to mw-cron on kubernetes, turn all the
    custom defined resources into standard sharded_periodic_job resources.
    
    Like I2314e4063ef8095166643d8f2772a484064a7010 but for all
    updatequerypages resources.
    
    Only actual diff to system resources are for s11 because of the force
    absent in sharded_periodic_job.
    
    All the rest is just switching what resource is responsible for the
    end-state files and system resources.
    
    Hosts: mwmaint1002.eqiad.wmnet,deploy1003.eqiad.wmnet
    Bug: T341555
    Bug: T388534
    Change-Id: I4570167a5c5ad4494e815390c3ce946519d0a541

--- modules/profile/manifests/mediawiki/maintenance/updatequerypages.pp
+++ modules/profile/manifests/mediawiki/maintenance/updatequerypages.pp
@@ -3,29 +3,85 @@ class profile::mediawiki::maintenance::updatequerypages(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
 
+    $team = 'mediawiki-special-pages'
+
     # add periodic jobs - usage: <cluster>@<day of month> (monthday currently unused, only sets cronjob name)
     # Wikidata has several jobs disabled: T234948, T239072
-    profile::mediawiki::maintenance::updatequerypages::ancientpages { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18']: }
-    profile::mediawiki::maintenance::updatequerypages::fewestrevisions { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17']: }
-    profile::mediawiki::maintenance::updatequerypages::wantedpages { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18']: }
-    profile::mediawiki::maintenance::updatequerypages::mostrevisions { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17']: }
-    profile::mediawiki::maintenance::updatequerypages::mostlinked { ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17']: }
+    profile::mediawiki::sharded_periodic_job { 'updatequerypages_ancientpages':
+        interval              => '*-8,22 01:00',
+        cron_schedule         => '00 01 8,22 * *',
+        shards                => ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18'],
+        script                => 'updateSpecialPages.php --override --only=Ancientpages',
+        team                  => $team,
+        description           => 'Update ancientpages',
+        script_label          => 'updatequerypages-ancientpages',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+    }
+    profile::mediawiki::sharded_periodic_job { 'updatequerypages_fewestrevisions':
+        interval              => '*-13,27 01:00',
+        cron_schedule         => '00 01 13,27 * *',
+        shards                => ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17'],
+        script                => 'updateSpecialPages.php --override --only=Fewestrevisions',
+        team                  => $team,
+        description           => 'Update fewestrevisions',
+        script_label          => 'updatequerypages-fewestrevisions',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+    }
+    profile::mediawiki::sharded_periodic_job { 'updatequerypages_wantedpages':
+        interval              => '*-12,26 01:00',
+        cron_schedule         => '00 01 12,26 * *',
+        shards                => ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18'],
+        script                => 'updateSpecialPages.php --override --only=Wantedpages',
+        team                  => $team,
+        description           => 'Update wantedpages',
+        script_label          => 'updatequerypages-wantedpages',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+    }
+    profile::mediawiki::sharded_periodic_job { 'updatequerypages_mostrevisions':
+        interval              => '*-11,25 01:00',
+        cron_schedule         => '00 01 11,25 * *',
+        shards                => ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17'],
+        script                => 'updateSpecialPages.php --override --only=Mostrevisions',
+        team                  => $team,
+        description           => 'Update mostrevisions',
+        script_label          => 'updatequerypages-mostrevisions',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+    }
+    profile::mediawiki::sharded_periodic_job { 'updatequerypages_mostlinked':
+        interval              => '*-10,24 01:00',
+        cron_schedule         => '00 01 10,24 * *',
+        shards                => ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17'],
+        script                => 'updateSpecialPages.php --override --only=Mostlinked',
+        team                  => $team,
+        description           => 'Update mostlinked',
+        script_label          => 'updatequerypages-mostlinked',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+    }
 
     # Move away from using a defined resource and use sharded_periodic_job
     # The '@<day of month>' hasn't worked in years, but we keep it for now to make the diff easier to read.
     profile::mediawiki::sharded_periodic_job { 'updatequerypages_deadendpages':
-        interval          => '*-9,23 01:00',
-        cron_schedule     => '00 01 9,23 * *',
-        shards            => ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18'],
-        kubernetes_shards => ['s3@13'],
-        script            => 'updateSpecialPages.php --override --only=Deadendpages',
-        team              => 'mediawiki-special-pages',
-        description       => 'Update deadendpages',
-        script_label      => 'updatequerypages-deadendpages',
+        interval              => '*-9,23 01:00',
+        cron_schedule         => '00 01 9,23 * *',
+        shards                => ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18'],
+        kubernetes_shards     => ['s3@13'],
+        script                => 'updateSpecialPages.php --override --only=Deadendpages',
+        team                  => $team,
+        description           => 'Update deadendpages',
+        script_label          => 'updatequerypages-deadendpages',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 
-    profile::mediawiki::maintenance::updatequerypages::uncatpages { ['s4@14']: }
-
+    profile::mediawiki::sharded_periodic_job { 'updatequerypages_uncatpages':
+        interval              => '*-14,28 01:00',
+        cron_schedule         => '00 01 14,28 * *',
+        shards                => ['s4@14'],
+        script                => 'updateSpecialPages.php --override --only=Uncategorizedpages',
+        team                  => $team,
+        description           => 'Update uncategorizedpages',
+        script_label          => 'updatequerypages-uncatpages',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+    }
 
     include profile::mediawiki::maintenance::updatequerypages::enwiki::cronjob
 }
