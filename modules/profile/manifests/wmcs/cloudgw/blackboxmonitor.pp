57a49f67a01a3fe224dd9c2d26775cf9deee2222
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2025-04-24 15:00:05 +0200

    cloudgw: cleanup pre-IPv6 settings
    They cloudgw servers are now native to IPv6.
    
    Hosts: re:cloudgw.*
    Bug: T380174
    Change-Id: If3a0a9aa19a0c669fa4f31d849188ea7db0f5f54
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- modules/profile/manifests/wmcs/cloudgw/blackboxmonitor.pp
+++ modules/profile/manifests/wmcs/cloudgw/blackboxmonitor.pp
@@ -3,15 +3,10 @@ class profile::wmcs::cloudgw::blackboxmonitor (
     Array[String[1]] $datacenters = lookup('datacenters'),
     String[1]        $wan_fqdn    = lookup('profile::wmcs::cloudgw::blackboxmonitor::wan_fqdn'),
     String[1]        $virt_fqdn   = lookup('profile::wmcs::cloudgw::blackboxmonitor::virt_fqdn'),
-    Boolean          $do_ipv6     = lookup('profile::wmcs::cloudgw::blackboxmonitor::do_ipv6'),
 ) {
 
     $dc_list = ['eqiad', 'codfw']
-    if $do_ipv6 {
-        $inet_fams = ['ip4', 'ip6']
-    } else {
-        $inet_fams = ['ip4']
-    }
+    $inet_fams = ['ip4', 'ip6']
 
     $datacenters.each |$dc| {
         if $dc in $dc_list {
@@ -21,7 +16,7 @@ class profile::wmcs::cloudgw::blackboxmonitor (
                 team           => 'wmcs',
                 ip4            => dnsquery::a($wan_fqdn)[0],
                 # prometheus::check::icmp will ignore the IPv6 address if ip_families does not contain ip6
-                ip6            => ($do_ipv6).bool2str(dnsquery::aaaa($wan_fqdn).then |$x| { $x[0] }, '::1'),
+                ip6            => dnsquery::aaaa($wan_fqdn).then |$x| { $x[0] },
                 ip_families    => $inet_fams,
             }
 
@@ -31,7 +26,7 @@ class profile::wmcs::cloudgw::blackboxmonitor (
                 team           => 'wmcs',
                 ip4            => dnsquery::a($virt_fqdn)[0],
                 # prometheus::check::icmp will ignore the IPv6 address if ip_families does not contain ip6
-                ip6            => ($do_ipv6).bool2str(dnsquery::aaaa($wan_fqdn).then |$x| { $x[0] }, '::1'),
+                ip6            => dnsquery::aaaa($virt_fqdn).then |$x| { $x[0] },
                 ip_families    => $inet_fams,
             }
         }
