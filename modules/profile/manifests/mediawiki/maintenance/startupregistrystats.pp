37064d1832acd680653084e30fa2cbfb350ee522
Hugh Nowlan <hnowlan@wikimedia.org>
2025-04-25 13:23:07 +0100

    mediawiki::maintenance: migrate main startupregistrystats job to k8s
    The two minor wiki versions of this task are running without issue,
    migrate the main, most significant version of this job to Kubernetes.
    
    Correct the description of the run frequency of existing jobs also.
    
    Bug: T388540
    Hosts: deploy1003.eqiad.wmnet, mwmaint1002.eqiad.wmnet
    Change-Id: I3f88bd8edd846d31cca77a94e37ba906657ea609

--- modules/profile/manifests/mediawiki/maintenance/startupregistrystats.pp
+++ modules/profile/manifests/mediawiki/maintenance/startupregistrystats.pp
@@ -9,7 +9,7 @@ class profile::mediawiki::maintenance::startupregistrystats(
         cron_schedule         => '10 * * * *',
         team                  => $team,
         script_label          => 'blameStartupRegistry.php',
-        description           => 'Run blameStartupRegistry.php on testwiki every 10 minutes',
+        description           => 'Run blameStartupRegistry.php on testwiki at 10 minutes past the hour',
         kubernetes            => true,
         helmfile_defaults_dir => $helmfile_defaults_dir,
     }
@@ -21,14 +21,20 @@ class profile::mediawiki::maintenance::startupregistrystats(
         cron_schedule         => '15 * * * *',
         team                  => $team,
         script_label          => 'blameStartupRegistry.php',
-        description           => 'Run blameStartupRegistry.php on mediawikiwiki every 15 minutes',
+        description           => 'Run blameStartupRegistry.php on mediawikiwiki at 15 minutes past the hour',
         kubernetes            => true,
         helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 
     # large wikis (inludes several group1 and group2 wikis)
     profile::mediawiki::periodic_job { 'startupregistrystats':
-        command  => '/usr/local/bin/foreachwikiindblist large extensions/WikimediaMaintenance/blameStartupRegistry.php --record-stats',
-        interval => '*:35'
+        command               => '/usr/local/bin/foreachwikiindblist large extensions/WikimediaMaintenance/blameStartupRegistry.php --record-stats',
+        interval              => '*:35',
+        cron_schedule         => '35 * * * *',
+        team                  => $team,
+        script_label          => 'blameStartupRegistry.php',
+        description           => 'Run blameStartupRegistry.php on several group1 and group2 wikis at 35 minutes past the hour',
+        kubernetes            => true,
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
