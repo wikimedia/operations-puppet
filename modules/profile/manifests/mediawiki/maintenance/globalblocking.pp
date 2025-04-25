3e8c553e90db56c6976934dbc20c67ed6a731864
Kamila Součková <ksouckova@wikimedia.org>
2025-04-25 17:13:29 +0200

    GlobalBlocking: Migrate fixGlobalBlockWhitelist
    Migrate fixGlobalBlockWhitelist.php to mw-cron
    
    Hosts: mwmaint1002.eqiad.wmnet, deploy1003.eqiad.wmnet
    Bug: T388542
    Change-Id: Id8f4006cc78b0c82baa65afc77ac9cc0f8fda9b6

--- modules/profile/manifests/mediawiki/maintenance/globalblocking.pp
+++ modules/profile/manifests/mediawiki/maintenance/globalblocking.pp
@@ -2,9 +2,17 @@
 class profile::mediawiki::maintenance::globalblocking(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
-    # Delete rows from local global_block_whitelist tables if the original block was removed
+    # team label for alerting
+    $team_label = 'trust-and-safety-product'
+
     profile::mediawiki::periodic_job { 'globalblocking-fixGlobalBlockWhitelist':
-        command  => '/usr/local/bin/foreachwiki extensions/GlobalBlocking/maintenance/fixGlobalBlockWhitelist.php  --delete',
-        interval => 'Sun 00:00',
+        command               => '/usr/local/bin/foreachwiki extensions/GlobalBlocking/maintenance/fixGlobalBlockWhitelist.php --delete',
+        interval              => 'Sun 00:00',
+        cron_schedule         => '0 0 * * SUN',
+        team                  => $team_label,
+        script_label          => 'fixGlobalBlockWhitelist.php',
+        description           => 'Delete rows from local global_block_whitelist tables if the original block was removed',
+        kubernetes            => true,
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
