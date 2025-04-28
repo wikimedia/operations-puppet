9be28238ad1d47318abc8e241b32bed6b49cd276
Hugh Nowlan <hnowlan@wikimedia.org>
2025-04-28 10:55:15 +0100

    mw::maintenance: migrate purge_parsercache_pc1 to k8s
    Migrate this first job to k8s to sett how it performs in the new
    environment.
    
    Hosts: deploy1003.eqiad.wmnet, mwmaint1002.eqiad.wmnet
    Bug: T385800
    Change-Id: Ie273d93524074652966717119e66c8af8afea1ef

--- modules/profile/manifests/mediawiki/maintenance/parsercachepurging.pp
+++ modules/profile/manifests/mediawiki/maintenance/parsercachepurging.pp
@@ -2,13 +2,21 @@ class profile::mediawiki::maintenance::parsercachepurging(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
 
+    $team = 'data-persistence'
+
     # Every day, Purge entries older than 30d * 86400s/d = 2592000s
     #
     # WARNING: Increasing msleep may cause exponential growth. Deletes must outpace other writes! (T282761)
     #
     profile::mediawiki::periodic_job { 'purge_parsercache_pc1':
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc1 --age=2592000 --msleep 200',
-        interval => '01:00',
+        command               => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc1 --age=2592000 --msleep 200',
+        interval              => '01:00',
+        cron_schedule         => '0 1 * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'purgeParserCache.php',
+        description           => 'Purge parsercache entries for pc1 once a day at 01:00.',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
     profile::mediawiki::periodic_job { 'purge_parsercache_pc2':
         command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc2 --age=2592000 --msleep 200',
