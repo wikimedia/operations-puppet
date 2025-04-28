b480dcc4cfabd7d2c33480ec75bdb25caa272377
Gergő Tisza <tgr.huwiki@gmail.com>
2025-04-28 18:29:25 +0200

    mediawiki: Make refreshLinkRecommendations job less verbose
    Bug: T392834
    Change-Id: I92ef22395a50a75c20555bb5ecf61cfeb812b04a

--- modules/profile/manifests/mediawiki/maintenance/growthexperiments/refreshlinkrecommendations.pp
+++ modules/profile/manifests/mediawiki/maintenance/growthexperiments/refreshlinkrecommendations.pp
@@ -2,7 +2,7 @@
 define profile::mediawiki::maintenance::growthexperiments::refreshlinkrecommendations() {
     # name is the DB section
     profile::mediawiki::periodic_job { "growthexperiments-refreshLinkRecommendations-${name}":
-        command  => "/usr/local/bin/foreachwikiindblist 'growthexperiments & ${name}' extensions/GrowthExperiments/maintenance/refreshLinkRecommendations.php --verbose",
+        command  => "/usr/local/bin/foreachwikiindblist 'growthexperiments & ${name}' extensions/GrowthExperiments/maintenance/refreshLinkRecommendations.php",
         interval => '*-*-* *:27:00',
     }
 }
