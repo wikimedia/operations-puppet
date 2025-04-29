66efe8f762cfa78f19007c5acdf68f8cc8963f71
Ben Tullis <btullis@wikimedia.org>
2025-04-29 13:07:09 +0100

    Enable the dumpsgen user to use an rsync server over ssh from dse-k8s-eqiad
    We need to allow pods running on the dse-k8s cluster to eqiad to send
    the dumps output files to the clouddumps servers for distribution.
    
    This commit adds an authorized_keys file to permit this, as well as
    opening up the SSH port to the dse-k8s pod IP ranges.
    
    We force the command that is used to be rsync in server mode, without
    the --delete option, since the clouddumps servers already have
    housekeeping configuration to clean up old dumps.
    
    Hosts: O:dumps::distribution::server
    Bug: T390738
    Change-Id: I3d5388fa025c91d0c1f57c97d5d4a3ac31e0cf30

--- modules/profile/manifests/dumps/distribution/server.pp
+++ modules/profile/manifests/dumps/distribution/server.pp
@@ -4,11 +4,26 @@
 # or via web or rsync to mirrors
 
 class profile::dumps::distribution::server {
-
-    class { '::dumpsuser': }
+    class { 'dumpsuser': }
 
     file { '/srv/dumps':
         ensure => 'directory',
+    }
+
+    # The following authorized_key exists in order to permit the dumpsgen user to send dumps from pods
+    # running on the dse-k8s cluster. The receiving command is forced to be the rsync server and it
+    # only permits access from the DSE_KUBEPODS_NETWORKS. The corresponding private key is deployed as
+    # a Kubernetes secret in the mediawiki-dumps-legacy namespace of the dse-k8s-eqiad cluster.
+    # See #T390738 for details.
+    ssh::userkey { 'dumpsgen':
+        source => 'puppet:///modules/profile/dumps/distribution/dumpsgen_authorized_keys',
+    }
+
+    # Allow SSH from the dse-k8s pods
+    firewall::service { 'ssh_dse-K8s_pods':
+        proto    => 'tcp',
+        port     => 22,
+        src_sets => ['DSE_KUBEPODS_NETWORKS'],
     }
 
     file { '/etc/default/smartmontools':
