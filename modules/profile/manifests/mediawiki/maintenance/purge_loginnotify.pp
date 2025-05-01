8e8206bf077e371ea9bfbbc39a30bfc2571eaece
Scott French <swfrench@wikimedia.org>
2025-05-01 21:21:14 +0000

    Revert "P:mediawiki::maintenance::purge_loginnotify: migrate to k8s"
    This reverts commit 1fda3ef6f7ff38022cea835d86987588f4a58831.
    
    Reason for revert: error converting YAML to JSON
    
    Details: yaml: line 130: mapping values are not allowed in this context
    
    Bug: T388536
    Change-Id: I0ba7b89545566036ab591e24dbae49e13c4655bb

--- modules/profile/manifests/mediawiki/maintenance/purge_loginnotify.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_loginnotify.pp
@@ -3,13 +3,7 @@ class profile::mediawiki::maintenance::purge_loginnotify(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
   profile::mediawiki::periodic_job { 'purge_loginnotify':
-    command               => '/usr/local/bin/foreachwikiindblist \'private + fishbowl - nonecho\' extensions/LoginNotify/maintenance/purgeSeen.php',
-    interval              => '23:00',
-    cron_schedule         => '00 23 * * *',
-    kubernetes            => true,
-    team                  => 'community-tech',
-    script_label          => 'purgeSeen.php',
-    description           => 'Purge expired user IP address information stored by LoginNotify (dblists: private + fishbowl - nonecho)',
-    helmfile_defaults_dir => $helmfile_defaults_dir,
+    command  => '/usr/local/bin/foreachwikiindblist \'private + fishbowl - nonecho\' extensions/LoginNotify/maintenance/purgeSeen.php',
+    interval => '23:00'
   }
 }
