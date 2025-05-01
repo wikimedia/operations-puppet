84afe71d8192e2cd72494d83be0fd05e8a414d5a
Scott French <swfrench@wikimedia.org>
2025-04-30 18:19:38 -0700

    P:mediawiki::maintenance::pageassessments: migrate to k8s
    Hosts: deploy1003.eqiad.wmnet, mwmaint1002.eqiad.wmnet
    Bug: T388536
    Change-Id: Id8ccfa67f9ff108b2a13fd330d5b94d8fcbf44df

--- modules/profile/manifests/mediawiki/maintenance/pageassessments.pp
+++ modules/profile/manifests/mediawiki/maintenance/pageassessments.pp
@@ -2,8 +2,14 @@ class profile::mediawiki::maintenance::pageassessments(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
     profile::mediawiki::periodic_job { 'pageassessments_cleanup':
-        command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/pageassessments.dblist extensions/PageAssessments/maintenance/purgeUnusedProjects.php',
-        interval => '20:42',
+        command               => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/pageassessments.dblist extensions/PageAssessments/maintenance/purgeUnusedProjects.php',
+        interval              => '20:42',
+        cron_schedule         => '42 20 * * *',
+        kubernetes            => true,
+        team                  => 'community-tech',
+        script_label          => 'purgeUnusedProjects.php',
+        description           => 'Remove any unused projects from the page_assessments_projects table (dblist: pageassessments)',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
 
