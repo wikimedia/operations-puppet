776808c5fe82a76a06b867affacd7a0490745de6
Hugh Nowlan <hnowlan@wikimedia.org>
2025-05-02 12:15:29 +0100

    mw::maintenance: migrate one image suggestions job to k8s
    Migrate one of these jobs to k8s to test how it runs, in anticipation
    of moving all.
    
    Also correct indentation on all lines.
    
    Hosts: deploy1003.eqiad.wmnet, mwmaint1002.eqiad.wmnet
    Bug: T388537
    Change-Id: Ia50bee46134922ecddd163f2dc4a883e7e2e6416

--- modules/profile/manifests/mediawiki/maintenance/image_suggestions.pp
+++ modules/profile/manifests/mediawiki/maintenance/image_suggestions.pp
@@ -2,38 +2,48 @@
 class profile::mediawiki::maintenance::image_suggestions(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
-  profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_CA':
-    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=cawiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
-    interval => 'Wed 0:00',
-  }
 
-  profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_NO':
-    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=nowiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
-    interval => 'Wed 1:00',
-  }
+    $team_name = 'structured-data'
 
-  profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_PT':
-    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=ptwiki --min-edit-count=300 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
-    interval => 'Wed 2:00',
-  }
+    profile::mediawiki::periodic_job { 'ImageSuggestions_NotifyUnillustratedWatched_CA':
+        command               => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=cawiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
+        interval              => 'Wed 0:00',
+        cron_schedule         => '0 0 * * WED',
+        kubernetes            => true,
+        team                  => $team_name,
+        script_label          => 'SendNotificationsForUnillustratedWatchedTitles.php-cawiki',
+        description           => 'Send notifications about image suggestions for cawiki once a week on wednesdays',
+        migration_title       => 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_CA',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+    }
 
-  profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_RU':
-    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=ruwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
-    interval => 'Wed 3:00',
-  }
+    profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_NO':
+        command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=nowiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
+        interval => 'Wed 1:00',
+    }
 
-  profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_ID':
-    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=idwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
-    interval => 'Wed 4:00',
-  }
+    profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_PT':
+        command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=ptwiki --min-edit-count=300 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
+        interval => 'Wed 2:00',
+    }
 
-  profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_FI':
-    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=fiwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
-    interval => 'Wed 5:00',
-  }
+    profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_RU':
+        command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=ruwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
+        interval => 'Wed 3:00',
+    }
 
-  profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_HU':
-    command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=huwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
-    interval => 'Wed 6:00',
-  }
+    profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_ID':
+        command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=idwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
+        interval => 'Wed 4:00',
+    }
+
+    profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_FI':
+        command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=fiwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
+        interval => 'Wed 5:00',
+    }
+
+    profile::mediawiki::periodic_job { 'ImageSuggestions_SendNotificationsForUnillustratedWatchedTitles_HU':
+        command  => '/usr/local/bin/mwscript extensions/ImageSuggestions/maintenance/SendNotificationsForUnillustratedWatchedTitles.php --wiki=huwiki --min-edit-count=500 --min-confidence=80 --max-notifications-per-user=2 --exclude-instance-of=Q5 --queue --quiet',
+        interval => 'Wed 6:00',
+    }
 }
