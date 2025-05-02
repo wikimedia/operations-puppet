07f9d4baba190ff1dc3b141d97aac816db6ed8df
Keith Herron <kherron@wikimedia.org>
2025-05-02 10:49:22 -0400

    logs-api: add write/delete acl via htgroup
    add and manage an apache htgroup and set limits for write
    and delete methods
    
    Bug: T390194
    Change-Id: Ifb6d653b8fb498bd13bff7aca7c62bf4fd13b870

--- modules/profile/manifests/opensearch/api/httpd_proxy.pp
+++ modules/profile/manifests/opensearch/api/httpd_proxy.pp
@@ -8,23 +8,26 @@
 # == Parameters:
 # - $vhost: Apache vhost name
 # - $serveradmin: Email address for contacting server administrator
-# - $auth_type: Vhost auth type. One of ldap, local, none
+# - $auth_type: Vhost auth type. One of ldap, local, local-api, none
 # - $accounts: hash of username -> htpasswd-hashed password for authentication
 # - $require_ssl: Require SSL connection to vhost?
 # - $auth_realm: HTTP basic auth realm description
-# - $auth_file: Path to htpasswd file for $auth_type == 'local'
+# - $auth_file: Path to htpasswd file for local auth types
+# - $group_file: Path to htgroup file for local auth types
 #
 # filtertags: labs-project-deployment-prep
 class profile::opensearch::api::httpd_proxy (
     String                      $vhost        = lookup('profile::opensearch::api::httpd_proxy::vhost'),
     String                      $serveradmin  = lookup('profile::opensearch::api::httpd_proxy::serveradmin'),
-    Enum['local','none']        $auth_type    = lookup('profile::opensearch::api::httpd_proxy::auth_type'),
+    Pattern[/^local/, /^none$/] $auth_type    = lookup('profile::opensearch::api::httpd_proxy::auth_type'),
     Hash[String, String]        $accounts     = lookup('profile::opensearch::api::httpd_proxy::accounts'),
+    Hash[String, String]        $groups       = lookup('profile::opensearch::api::httpd_proxy::groups'),
     Boolean                     $require_ssl  = lookup('profile::opensearch::api::httpd_proxy::require_ssl',  { 'default_value' => true }),
     Optional[String]            $auth_realm   = lookup('profile::opensearch::api::httpd_proxy::auth_realm',   { 'default_value' => undef }),
     Optional[String]            $auth_file    = lookup('profile::opensearch::api::httpd_proxy::auth_file',    { 'default_value' => undef }),
+    Optional[String]            $group_file   = lookup('profile::opensearch::api::httpd_proxy::group_file',   { 'default_value' => undef }),
 ) {
-    if $auth_type == 'local' {
+    if $auth_type =~ /^local/ {
         $httpd_extra_modules = ['authz_groupfile', 'authz_user']
         file { $auth_file:
             ensure  => present,
@@ -33,9 +36,19 @@ class profile::opensearch::api::httpd_proxy (
             group   => 'www-data',
             content => ($accounts.map |$k, $v| { "${k}:${v}" } + ['']).join("\n"),
         }
+        file { $group_file:
+            ensure  => present,
+            mode    => '0400',
+            owner   => 'www-data',
+            group   => 'www-data',
+            content => ($groups.map |$k, $v| { "${k}:${v}" } + ['']).join("\n"),
+        }
     } elsif $auth_type == 'none' {
         $httpd_extra_modules = []
         file { $auth_file:
+            ensure  => absent,
+        }
+        file { $group_file:
             ensure  => absent,
         }
     }
