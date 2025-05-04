0213325fb3e131bbc192b8ee1a73335e4053babd
Andrew Bogott <abogott@wikimedia.org>
2025-05-04 18:18:19 -0500

    Remove puppet refs to cloudcephosd100[123]
    Bug: T393196
    Change-Id: Ib355052e6d27fac01e1cb0ab1c46c51c13733a63

--- manifests/site.pp
+++ manifests/site.pp
@@ -1815,7 +1815,7 @@ node /^cloudcephmon100[4-6]\.eqiad\./ {
 }
 
 # cloudceph storage nodes
-node /^cloudcephosd10(0[1-9]|1[0-9]|2[0-9]|3[0-9]|4[0-1])\.eqiad\./ {
+node /^cloudcephosd10(0[4-9]|1[0-9]|2[0-9]|3[0-9]|4[0-1])\.eqiad\./ {
     role(wmcs::ceph::osd)
 }
 
