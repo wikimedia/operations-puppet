af7874c29bdfe1df754cd1d5d4a2f1b06046ccfa
Aqu <aquhen@wikimedia.org>
2025-05-05 14:16:59 +0200

    [analytics] Refine deterministic transform deduplication
    To deduplicate events deterministically, we must
    select events in a consistent order.
    
    In the “resource_change” stream, duplicate
    events differ only by meta.uri.
    
    Add meta.uri to the ordering key.
    
    See analytics/refinery patch: 1140141
    
    Bug: T369845
    Change-Id: Ia77324c0cc51fc3ba46bae59c832fb4422bfc1b0

--- modules/profile/manifests/analytics/refinery/job/refine.pp
+++ modules/profile/manifests/analytics/refinery/job/refine.pp
@@ -30,7 +30,7 @@ class profile::analytics::refinery::job::refine(
 
     # Update this when you want to change the version of the refinery job jar
     # being used for the refine job.
-    $refinery_version = '0.2.49.3'
+    $refinery_version = '0.2.49.4'
 
     # Use this value by default
     Profile::Analytics::Refinery::Job::Refine_job {
