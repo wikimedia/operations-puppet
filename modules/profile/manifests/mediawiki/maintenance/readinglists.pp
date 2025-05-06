7ced3658802b696a8f4db20ad06e374003a25d87
Hugh Nowlan <hnowlan@wikimedia.org>
2025-05-06 12:20:29 +0100

    mw::maintenance: migrate readinglists job to k8s
    Migrate this job from metal to k8s. Team has been unresponsive but
    script involves fairly straightforward DB interactions.
    
    Bug: T388541
    Change-Id: Ia3aa7bd8a5ea5503cfbbd6094fbaa635b4321bdd

--- modules/profile/manifests/mediawiki/maintenance/readinglists.pp
+++ modules/profile/manifests/mediawiki/maintenance/readinglists.pp
@@ -1,8 +1,17 @@
 class profile::mediawiki::maintenance::readinglists(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+
+    $team = 'mediawiki-interfaces'
+
     profile::mediawiki::periodic_job { 'readinglists_purge':
-        command  => '/usr/local/bin/mwscript extensions/ReadingLists/maintenance/purge.php --wiki=metawiki',
-        interval => '02:42',
+        command               => '/usr/local/bin/mwscript extensions/ReadingLists/maintenance/purge.php --wiki=metawiki',
+        interval              => '02:42',
+        cron_schedule         => '42 2 * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'readinglists-purge.php',
+        description           => 'Hard-deletes deleted reading lists and entries older than a cutoff date. Also does other DB cleanup that has no effect on functionality.',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
