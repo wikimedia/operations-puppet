a37b7f9cd546a0769ece15505d56e1da523dd7d4
Scott French <swfrench@wikimedia.org>
2025-05-06 15:31:44 -0700

    P:mw::maintenance::refreshlinks: rename and prepare for mw-cron
    Currently, profile::mediawiki::maintenance::refreshlinks does not use
    sharded_periodic_job, instead relying on a specialized job resource
    definition that still supports the '<shard>@<day-of-month>' idiom.
    
    While it would be "easy" to revive support for this deprecated syntax in
    sharded_periodic_job and then adopt it here, that's non-ideal, as we do
    not want to encourage its use more broadly.
    
    At the same time, it would also be non-ideal to retain the specialized
    definition, as it would require plumbing various mw-cron params down to
    the profile::mediawiki::periodic_job instantiation therein and still use
    the '<shard>@<day-of-month>' idiom (meh).
    
    Instead, this deprecates the specialized job definition, in favor of
    defining shard-to-day-of-month mapping in a more structured way. This
    complexity is limited to profile::mediawiki::maintenance::refreshlinks,
    rather than pushing it into sharded_periodic_job.
    
    While doing so, we also:
    1. Imbue the jobs with a far more descriptive name going forward, as
       already discussed in T388530.
    2. Prepare for migration to mw-cron (e.g., adding relevant periodic_job
       resource params).
    
    While this patch does change the set of puppet resources, it does not
    make system-affecting changes (nor are any on-host resources abandoned).
    
    Hosts: mwmaint1002.eqiad.wmnet, deploy1003.eqiad.wmnet
    Bug: T388530
    Change-Id: I453b09e7767082a2466c129ca63dcf1598dff13a

--- modules/profile/manifests/mediawiki/maintenance/refreshlinks.pp
+++ modules/profile/manifests/mediawiki/maintenance/refreshlinks.pp
@@ -2,7 +2,21 @@
 class profile::mediawiki::maintenance::refreshlinks(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
-
-    # add periodic jobs - usage: <cluster>@<day of month> (these are just needed monthly)
-    profile::mediawiki::maintenance::refreshlinks::periodic_job { ['s1@1', 's2@2', 's3@3', 's4@4', 's5@5', 's6@6', 's7@7', 's8@8']: }
+    # These jobs run monthly, staggered by day of the month.
+    $shard_to_day = {'s1' => 1, 's2' => 2, 's3' => 3, 's4' => 4, 's5' => 5, 's6' => 6, 's7' => 7, 's8' => 8}
+    # TODO: T388530 - Remove kubernetes_shards when all shards are migrated.
+    $kubernetes_shards = []
+    $shard_to_day.map |$shard, $day_of_month| {
+        profile::mediawiki::periodic_job { "refreshlinks-delete-from-nonexistent-${shard}":
+            command               => "/usr/local/bin/mwscriptwikiset refreshLinks.php ${shard}.dblist --dfn-only",
+            interval              => "*-${day_of_month} 00:00",
+            kubernetes            => $shard in $kubernetes_shards,
+            cron_schedule         => "00 00 ${day_of_month} * *",
+            team                  => 'mediawiki-page-derived-data',
+            description           => "Refresh link tables in ${shard}, deleting links from nonexistent articles only",
+            script_label          => 'refreshLinks.php--dfn-only',
+            helmfile_defaults_dir => $helmfile_defaults_dir,
+            migration_title       => "cron-refreshlinks-${shard}@${day_of_month}",
+        }
+    }
 }
