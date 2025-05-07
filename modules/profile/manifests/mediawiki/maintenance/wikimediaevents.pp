42ca8e6528ec647f7c795f0cdc0afd061c3c556b
Raine Souček <ksouckova@wikimedia.org>
2025-05-07 14:19:32 +0200

    mw-cron/WikimediaEvents: Migrate UpdatePeriodicMetrics jobs
    Migrate UpdatePeriodicMetrics.php {per-wiki,global} to mw-cron
    
    Hosts: mwmaint1002.eqiad.wmnet, deploy1003.eqiad.wmnet
    Bug: T388542
    Change-Id: Ia565fdc91e9581f59c41684c8d7660e91be0dc30

--- modules/profile/manifests/mediawiki/maintenance/wikimediaevents.pp
+++ modules/profile/manifests/mediawiki/maintenance/wikimediaevents.pp
@@ -2,15 +2,28 @@
 class profile::mediawiki::maintenance::wikimediaevents(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
-    # calculate per-wiki periodic metrics and let them be pulled by Prometheus (T375508)
+    # team label for alerting
+    $team_label = 'trust-and-safety-product'
+
     profile::mediawiki::periodic_job { 'wikimediaevents-UpdatePeriodicMetrics-per-wiki':
-        command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/all.dblist extensions/WikimediaEvents/maintenance/UpdatePeriodicMetrics.php --verbose',
-        interval => '*-*-* 04:40:00',
+        command               => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/all.dblist extensions/WikimediaEvents/maintenance/UpdatePeriodicMetrics.php --verbose',
+        interval              => '*-*-* 04:40:00',
+        cron_schedule         => '40 4 * * *',
+        team                  => $team_label,
+        script_label          => 'UpdatePeriodicMetrics.php-per-wiki',
+        description           => 'Calculate per-wiki periodic metrics and let them be pulled by Prometheus (T375508)',
+        kubernetes            => true,
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 
-    # calculate global periodic metrics and let them be pulled by Prometheus (T375508)
     profile::mediawiki::periodic_job { 'wikimediaevents-UpdatePeriodicMetrics-global':
-        command  => '/usr/local/bin/mwscript extensions/WikimediaEvents/maintenance/UpdatePeriodicMetrics.php --wiki=metawiki --global-metrics --verbose',
-        interval => '*-*-* 04:50:00',
+        command               => '/usr/local/bin/mwscript extensions/WikimediaEvents/maintenance/UpdatePeriodicMetrics.php --wiki=metawiki --global-metrics --verbose',
+        interval              => '*-*-* 04:50:00',
+        cron_schedule         => '50 4 * * *',
+        team                  => $team_label,
+        script_label          => 'UpdatePeriodicMetrics.php-global',
+        description           => 'Calculate global periodic metrics and let them be pulled by Prometheus (T375508)',
+        kubernetes            => true,
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
