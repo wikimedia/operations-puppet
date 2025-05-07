eb2de222db5fc0ffb3932848ee5ddf519622c914
Scott French <swfrench@wikimedia.org>
2025-05-07 15:02:46 -0700

    P:mw::maint::temporary_accounts: purge_temporary_accounts to k8s
    Migrates the CentralAuth expireTemporaryAccounts.php periodic job to
    mw-cron.
    
    Hosts: mwmaint1002.eqiad.wmnet, deploy1003.eqiad.wmnet
    Bug: T385866
    Change-Id: Ibec8c70cc6450363a8a8fdb6d0f1f87ec0c14b44

--- modules/profile/manifests/mediawiki/maintenance/temporary_accounts.pp
+++ modules/profile/manifests/mediawiki/maintenance/temporary_accounts.pp
@@ -5,7 +5,13 @@ class profile::mediawiki::maintenance::temporary_accounts(
 ) {
 
     profile::mediawiki::periodic_job { 'purge_temporary_accounts':
-        command  => '/usr/local/bin/foreachwikiindblist "all - closed - private - fishbowl" extensions/CentralAuth/maintenance/expireTemporaryAccounts.php --verbose --frequency 1',
-        interval => '*-*-* 14:27:00'
+        command               => '/usr/local/bin/foreachwikiindblist "all - closed - private - fishbowl" extensions/CentralAuth/maintenance/expireTemporaryAccounts.php --verbose --frequency 1',
+        interval              => '*-*-* 14:27:00',
+        cron_schedule         => '27 14 * * *',
+        team                  => 'mediawiki-platform',
+        kubernetes            => true,
+        description           => 'Expire temporary accounts registered before the configured expiry window (dblists: all - closed - private - fishbowl)',
+        script_label          => 'CentralAuth-expireTemporaryAccounts.php',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
