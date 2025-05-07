33e74bdd4f2cc606ff50df497ffea61d00ba8e4d
Luca Toscano <ltoscano@wikimedia.org>
2025-05-07 11:58:17 +0200

    raid: fix get-raid-status-broadcom.py script
    In my previous patch I wrongly used the 'in' operator instead of
    'not in' to collect errors.
    
    Bug: T393146
    Change-Id: I40f7cb492079efeb45fd7820c508ae16e67beec9

--- modules/raid/files/get-raid-status-broadcom.py
+++ modules/raid/files/get-raid-status-broadcom.py
@@ -136,7 +136,8 @@ def virtual_devices_configured() -> bool:
 def general_state(data) -> Tuple[int, str]:
     """Get overall state from the RAID controller"""
     status_list = lookup_by_key('Status', data)
-    errors = [status for status in status_list if status['Controller Status'] in ['Optimal', 'OK']]
+    errors = [status for status in status_list
+              if status['Controller Status'] not in ['Optimal', 'OK']]
     status = 'OK' if not errors else errors[0]['Controller Status']
     exit_code = 0 if not errors else NAGIOS_WARN
     message = f'controller: {len(errors)} {status}'
