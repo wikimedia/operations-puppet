5924c00d422fb4d379c48175b9fc04e6704c3108
Hugh Nowlan <hnowlan@wikimedia.org>
2025-05-07 15:22:05 +0100

    mw::maintenance: migrate all parsercache jobs to k8s
    parsercache_purge_pc1 ran successfully this morning. Migrate all
    parsercache purge jobs to Kubernetes.
    
    Hosts: deploy1003.eqiad.wmnet, mwmaint1002.eqiad.wmnet
    Bug: T385800
    Change-Id: Idbda7227f59afd568be126712343a9c0ebad7980

--- modules/profile/manifests/mediawiki/maintenance/parsercachepurging.pp
+++ modules/profile/manifests/mediawiki/maintenance/parsercachepurging.pp
@@ -19,28 +19,64 @@ class profile::mediawiki::maintenance::parsercachepurging(
         helmfile_defaults_dir => $helmfile_defaults_dir,
     }
     profile::mediawiki::periodic_job { 'purge_parsercache_pc2':
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc2 --age=2592000 --msleep 200',
-        interval => '01:00',
+        command               => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc2 --age=2592000 --msleep 200',
+        interval              => '01:00',
+        cron_schedule         => '0 1 * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'purgeParserCache.php',
+        description           => 'Purge parsercache entries for pc2 once a day at 01:00.',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
     profile::mediawiki::periodic_job { 'purge_parsercache_pc3':
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc3 --age=2592000 --msleep 200',
-        interval => '01:00',
+        command               => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc3 --age=2592000 --msleep 200',
+        interval              => '01:00',
+        cron_schedule         => '0 1 * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'purgeParserCache.php',
+        description           => 'Purge parsercache entries for pc3 once a day at 01:00.',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
     profile::mediawiki::periodic_job { 'purge_parsercache_pc4':
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc4 --age=2592000 --msleep 200',
-        interval => '01:00',
+        command               => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc4 --age=2592000 --msleep 200',
+        interval              => '01:00',
+        cron_schedule         => '0 1 * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'purgeParserCache.php',
+        description           => 'Purge parsercache entries for pc4 once a day at 01:00.',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
     profile::mediawiki::periodic_job { 'purge_parsercache_pc5':
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc5 --age=2592000 --msleep 200',
-        interval => '01:00',
+        command               => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc5 --age=2592000 --msleep 200',
+        interval              => '01:00',
+        cron_schedule         => '0 1 * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'purgeParserCache.php',
+        description           => 'Purge parsercache entries for pc5 once a day at 01:00.',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
     profile::mediawiki::periodic_job { 'purge_parsercache_pc6':
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc6 --age=2592000 --msleep 200',
-        interval => '01:00',
+        command               => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc6 --age=2592000 --msleep 200',
+        interval              => '01:00',
+        cron_schedule         => '0 1 * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'purgeParserCache.php',
+        description           => 'Purge parsercache entries for pc6 once a day at 01:00.',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
     profile::mediawiki::periodic_job { 'purge_parsercache_pc7':
-        command  => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc7 --age=2592000 --msleep 200',
-        interval => '01:00',
+        command               => '/usr/local/bin/mwscript purgeParserCache.php --wiki=aawiki --tag pc7 --age=2592000 --msleep 200',
+        interval              => '01:00',
+        cron_schedule         => '0 1 * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'purgeParserCache.php',
+        description           => 'Purge parsercache entries for pc7 once a day at 01:00.',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 
 }
