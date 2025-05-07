f6654a60f3aed1732ccb858d9d6f2717676ecacf
Scott French <swfrench@wikimedia.org>
2025-05-07 15:09:54 -0700

    P:mw::maint::purge_expired_userrights: purge_expired_userrights to k8s
    Migrates the purgeExpiredUserrights.php periodic job to mw-cron.
    
    Hosts: mwmaint1002.eqiad.wmnet, deploy1003.eqiad.wmnet
    Bug: T385866
    Change-Id: Iec030ef8a5da0b6572360b58a67e7aa02cb5f75f

--- modules/profile/manifests/mediawiki/maintenance/purge_expired_userrights.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_expired_userrights.pp
@@ -1,9 +1,17 @@
 class profile::mediawiki::maintenance::purge_expired_userrights(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+    $team = 'mediawiki-platform'
+
     profile::mediawiki::periodic_job { 'purge_expired_userrights':
-        command  => '/usr/local/bin/foreachwiki maintenance/purgeExpiredUserrights.php',
-        interval => '*-14,28 06:42'
+        command               => '/usr/local/bin/foreachwiki maintenance/purgeExpiredUserrights.php',
+        interval              => '*-14,28 06:42',
+        cron_schedule         => '42 06 14,28 * *',
+        team                  => $team,
+        kubernetes            => true,
+        description           => 'Remove expired userrights from user_groups table and move them to former_user_groups for all wikis',
+        script_label          => 'purgeExpiredUserrights.php',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 
     # CentralAuth tables are global, we only need to run this on one wiki.
