9309836ded5981d5bc7c304952429552d3f7a396
Scott French <swfrench@wikimedia.org>
2025-05-07 17:50:02 -0700

    P:mw::maint::backfill_localaccounts: backfillLocalAccounts-loginwiki to k8s
    Migrates the CentralAuth backfillLocalAccounts.php periodic job for
    loginwiki to mw-cron.
    
    Hosts: mwmaint1002.eqiad.wmnet, deploy1003.eqiad.wmnet
    Bug: T385866
    Change-Id: Ie6cb06dd6a3a772f94b3e5df8e1f446892ca8e1e

--- modules/profile/manifests/mediawiki/maintenance/backfill_localaccounts.pp
+++ modules/profile/manifests/mediawiki/maintenance/backfill_localaccounts.pp
@@ -2,11 +2,20 @@
 class profile::mediawiki::maintenance::backfill_localaccounts(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+    $team = 'mediawiki-platform'
+
     # Create missing local accounts on loginwiki, metawiki corresponding to existing global users
     # See T371267
     profile::mediawiki::periodic_job { 'centralauth-backfillLocalAccounts.php-loginwiki':
-        command  => '/usr/local/bin/mwscript extensions/CentralAuth/maintenance/backfillLocalAccounts.php  --wiki=loginwiki --startdate=yesterday',
-        interval => '*:25',
+        command               => '/usr/local/bin/mwscript extensions/CentralAuth/maintenance/backfillLocalAccounts.php  --wiki=loginwiki --startdate=yesterday',
+        interval              => '*:25',
+        cron_schedule         => '25 * * * *',
+        team                  => $team,
+        kubernetes            => true,
+        description           => 'Backfill global user accounts to loginwiki',
+        script_label          => 'CentralAuth-backfillLocalAccounts.php-loginwiki',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+
     }
     profile::mediawiki::periodic_job { 'centralauth-backfillLocalAccounts.php-metawiki':
         command  => '/usr/local/bin/mwscript extensions/CentralAuth/maintenance/backfillLocalAccounts.php  --wiki=metawiki --startdate=yesterday',
