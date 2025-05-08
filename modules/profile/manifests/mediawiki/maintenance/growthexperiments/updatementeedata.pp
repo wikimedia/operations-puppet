c5818037f67c950221494af4dc93545027511fe2
Hugh Nowlan <hnowlan@wikimedia.org>
2025-05-08 15:31:51 +0100

    mw::maintenance: move updateMenteeData to upper level job
    Remove the define used to create updateMenteeData jobs. Don't use the
    sharded job class as the shard name is used in the command. Migrate s1
    to Kubernetes
    
    Bug: T385782
    Change-Id: Ib85385a9d193229f0935a8cd0a022c6d25b5f0bc

--- modules/profile/manifests/mediawiki/maintenance/growthexperiments/updatementeedata.pp
+++
@@ -1,8 +0,0 @@
-# update data for the mentor dashboard (T285811)
-define profile::mediawiki::maintenance::growthexperiments::updatementeedata() {
-    # name is the DB cluster
-    profile::mediawiki::periodic_job { "growthexperiments-updateMenteeData-${name}":
-        command  => "/usr/local/bin/foreachwikiindblist 'growthexperiments & ${name}' extensions/GrowthExperiments/maintenance/updateMenteeData.php --statsd --dbshard ${name}",
-        interval => '*-*-* 00,03,06,09,12,15,18,21:15:00',
-    }
-}
