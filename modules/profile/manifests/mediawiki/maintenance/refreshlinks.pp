c56ef2260577a2aace9deea4e4e84c6f31d6c468
Scott French <swfrench@wikimedia.org>
2025-05-12 11:44:38 -0700

    P:mw::maintenance::refreshlinks: migrate remaining shards to mw-cron
    The s6 shard has been migrated and seems to work as expected during a
    manual pilot run [0].
    
    Given that, I think it makes sense to move ahead with migrating the
    remaining shards.
    
    [0] https://phabricator.wikimedia.org/T388530#10813399
    
    Hosts: mwmaint1002.eqiad.wmnet, deploy1003.eqiad.wmnet
    Bug: T388530
    Change-Id: I59808d7ed1c9c22a6059a3cd33a9fe334ae6a5db

--- modules/profile/manifests/mediawiki/maintenance/refreshlinks.pp
+++ modules/profile/manifests/mediawiki/maintenance/refreshlinks.pp
@@ -4,13 +4,11 @@ class profile::mediawiki::maintenance::refreshlinks(
 ) {
     # These jobs run monthly, staggered by day of the month.
     $shard_to_day = {'s1' => 1, 's2' => 2, 's3' => 3, 's4' => 4, 's5' => 5, 's6' => 6, 's7' => 7, 's8' => 8}
-    # TODO: T388530 - Remove kubernetes_shards when all shards are migrated.
-    $kubernetes_shards = ['s6']
     $shard_to_day.map |$shard, $day_of_month| {
         profile::mediawiki::periodic_job { "refreshlinks-delete-from-nonexistent-${shard}":
             command               => "/usr/local/bin/mwscriptwikiset refreshLinks.php ${shard}.dblist --dfn-only",
             interval              => "*-${day_of_month} 00:00",
-            kubernetes            => $shard in $kubernetes_shards,
+            kubernetes            => true,
             cron_schedule         => "00 00 ${day_of_month} * *",
             team                  => 'mediawiki-page-derived-data',
             description           => "Refresh link tables in ${shard}, deleting links from nonexistent articles only",
