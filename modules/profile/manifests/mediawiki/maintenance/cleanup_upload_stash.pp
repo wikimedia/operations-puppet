dfc48e517cf63b5450922cb2d365eb3ad37ef68a
Hugh Nowlan <hnowlan@wikimedia.org>
2025-05-12 12:15:34 +0100

    mw::maintenance: migrate cleanupUploadStash job to k8s
    Migrate this job from metal to k8s.
    
    Bug: T385868
    Change-Id: I56697027627f306da035f747eb9b4b0bdd68c28c

--- modules/profile/manifests/mediawiki/maintenance/cleanup_upload_stash.pp
+++ modules/profile/manifests/mediawiki/maintenance/cleanup_upload_stash.pp
@@ -1,8 +1,17 @@
 class profile::mediawiki::maintenance::cleanup_upload_stash(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+
+    $team_name = 'mediawiki-file-management'
+
     profile::mediawiki::periodic_job { 'cleanup_upload_stash':
-        command  => '/usr/local/bin/foreachwiki maintenance/cleanupUploadStash.php',
-        interval => '01:00',
+        command               => '/usr/local/bin/foreachwiki maintenance/cleanupUploadStash.php',
+        interval              => '01:00',
+        cron_schedule         => '0 1 * * *',
+        kubernetes            => true,
+        team                  => $team_name,
+        script_label          => 'cleanupUploadStash.php',
+        description           => 'Remove old or broken uploads from temporary uploaded file storage and database records',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
