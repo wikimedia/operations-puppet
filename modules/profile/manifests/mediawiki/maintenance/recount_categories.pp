824faa06106c890a8b8cd19d20d1b5ed7d7aa6b0
Hugh Nowlan <hnowlan@wikimedia.org>
2025-05-12 11:52:38 +0100

    mw::maintenance: migrate recountCategories job to k8s
    Migrate this job from metal to k8s. The job is relatively
    straightforward in its interactions with databases, no issues
    anticipated.
    
    Bug: T388533
    Change-Id: I4b739da65b8ca8118dc213344c49c822b771c2f6

--- modules/profile/manifests/mediawiki/maintenance/recount_categories.pp
+++ modules/profile/manifests/mediawiki/maintenance/recount_categories.pp
@@ -1,9 +1,18 @@
 class profile::mediawiki::maintenance::recount_categories(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+
+    $team_name = 'mediawiki-categories'
+
     profile::mediawiki::periodic_job { 'recount_categories':
-        command  => '/usr/local/bin/foreachwiki recountCategories.php --mode all',
-        interval => '*-*-01 04:00'
+        command               => '/usr/local/bin/foreachwiki recountCategories.php --mode all',
+        interval              => '*-*-01 04:00',
+        cron_schedule         => '0 4 1 * *',
+        kubernetes            => true,
+        team                  => $team_name,
+        script_label          => 'recountCategories.php',
+        description           => 'Recount category membership counts in the category once a month',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
 
     }
 }
