f921e1b8e79e99bc541a29a19171819ccc097f5c
Taavi Väänänen <hi@taavi.wtf>
2025-05-12 11:34:08 +0300

    P:toolforge::k8s::worker: Add network tests access rules
    Bug: T393775
    Change-Id: Ia1547ebd06adc5147df0293589992118356a3978

--- modules/profile/manifests/toolforge/k8s/worker.pp
+++ modules/profile/manifests/toolforge/k8s/worker.pp
@@ -1,5 +1,17 @@
-class profile::toolforge::k8s::worker (
-) {
+class profile::toolforge::k8s::worker () {
     class { '::profile::wmcs::kubeadm::worker': }
     contain '::profile::wmcs::kubeadm::worker'
+
+    # The Cloud VPS network test suite relies on logging in to these hosts
+    # as they do not use floating IPs but do have (dumps) NFS shares mounted.
+    # Previously that worked when the network test service account was a part
+    # of tools.admin, but that was removed in
+    # https://phabricator.wikimedia.org/T393775. Instead we provision an
+    # explicit rule for that user here - logging in as an unprivileged user
+    # should be relatively safe, and this is still better than letting any
+    # Toolforge users log in.
+    security::access::config { 'network-tests':
+        content  => "+ : srv-networktests : ALL\n",
+        priority => 60,
+    }
 }
