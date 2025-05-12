0e2b85c20d55be5976e961a9acd338a59c09a074
Hugh Nowlan <hnowlan@wikimedia.org>
2025-05-12 12:55:41 +0100

    mw::maintenance: migrate initSiteStats cron to k8s
    This job runs relatively simple database updates and will most likely
    be an easy enough migration from metal to k8s.
    
    Bug: T388534
    Change-Id: I95c7c698b042d2f1c18917fd95e5bfb8d2c29a4c

--- modules/profile/manifests/mediawiki/maintenance/initsitestats.pp
+++ modules/profile/manifests/mediawiki/maintenance/initsitestats.pp
@@ -1,8 +1,16 @@
 class profile::mediawiki::maintenance::initsitestats(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+    $team_name = 'mediawiki-special-pages'
+
     profile::mediawiki::periodic_job { 'initsitestats':
-        command  => '/usr/local/bin/foreachwiki initSiteStats.php --update',
-        interval => '*-*-* 21:00:00',
+        command               => '/usr/local/bin/foreachwiki initSiteStats.php --update',
+        interval              => '*-*-* 21:00:00',
+        cron_schedule         => '0 21 * * *',
+        kubernetes            => true,
+        team                  => $team_name,
+        script_label          => 'initSiteStats.php',
+        description           => 'Re-initialise or update the site statistics table.',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
