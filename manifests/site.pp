d1ac8661ef0c33525d4a67f5a1df8b6aabad94ed
Tobias Klausmann <tklausmann@wikimedia.org>
2025-05-13 09:40:42 +0200

    site/preseed: Add soon-to-arrive ML hosts
    Bug: T393948
    Change-Id: I749b445810b8d14c67a64306ebfd358ecf0d64c5

--- manifests/site.pp
+++ manifests/site.pp
@@ -2107,6 +2107,10 @@ node /^ml-serve10(0[1-9]|1[01])\.eqiad\./ {
     role(ml_k8s::worker)
 }
 
+node  /^ml-serve10(12|13)\.eqiad\./ {
+    role(insetup::machine_learning)
+}
+
 node /^ml-staging-etcd200[123]\.codfw\./ {
     role(etcd::v3::ml_etcd::staging)
 }
