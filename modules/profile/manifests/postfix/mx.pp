f6e68b564df3fa820ccb428c2c9abfb308e5e3ab
Cole White <cwhite@wikimedia.org>
2025-05-16 14:53:41 +0000

    rsyslog: remove postfix entries from lookup table
    Also revert Ifa21faaa595593a7becd348cfcd06f9b4580d715 as that resource
    writes to /var/log/postfix.log, not ingest it.
    
    Bug: T394514
    Change-Id: I29847d93d5ab538518530e2588e5f2dccf457176

--- modules/profile/manifests/postfix/mx.pp
+++ modules/profile/manifests/postfix/mx.pp
@@ -395,9 +395,7 @@ class profile::postfix::mx (
             | EOF
     }
 
-    # T394514 - context: T197173, requires: T213902
     rsyslog::conf { 'postfix':
-        ensure   => absent,
         priority => 20,
         content  => @(EOF),
             # Managed by Puppet
