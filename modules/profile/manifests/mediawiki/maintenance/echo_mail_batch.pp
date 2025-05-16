84783eb501c185e8d702c4c1aa6f0c16535d4090
Clément Goubert <cgoubert@wikimedia.org>
2025-05-16 09:01:15 +0200

    mw::maintenance: migrate echo_mail_batch to k8s
    Hosts: mwmaint1002.eqiad.wmnet, deploy1003.eqiad.wmnet
    Bug: T394471
    Change-Id: Icf6eb460b7a4490a2a05ea477906efc0832c59e8

--- modules/profile/manifests/mediawiki/maintenance/echo_mail_batch.pp
+++ modules/profile/manifests/mediawiki/maintenance/echo_mail_batch.pp
@@ -1,9 +1,18 @@
 class profile::mediawiki::maintenance::echo_mail_batch(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+    $team = 'notifications-echo'
+
     profile::mediawiki::periodic_job { 'echo_mail_batch':
-        command  => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/echo.dblist extensions/Echo/maintenance/processEchoEmailBatch.php',
-        interval => '00:00',
+        command               => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblists/echo.dblist extensions/Echo/maintenance/processEchoEmailBatch.php',
+        interval              => '00:00',
+        cron_schedule         => '00 00 * * *',
+        team                  => $team,
+        kubernetes            => true,
+        description           => 'Deliver daily Echo notifications',
+        script_label          => 'processEchoEmailBatch.php',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+
     }
 }
 
