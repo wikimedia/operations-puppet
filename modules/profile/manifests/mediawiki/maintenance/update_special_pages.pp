a0c5db5d695fc1fce01ea2cac205397a7ca5bfbc
Scott French <swfrench@wikimedia.org>
2025-05-15 17:24:18 -0700

    P:mw:maint:update_special_pages: updateSpecialPages in s6 to mw-cron
    This pilots updateSpecialPages on a single shard. The choice of s6 is
    somewhat arbitrary, but it is (joint with s8) the 2nd fastest shard to
    run (~ 1h) that is not s1 (~ 35m) - i.e., pick the shard with minimum
    aggregate "cost" that is not also a single (big) wiki.
    
    Hosts: mwmaint1002.eqiad.wmnet, deploy1003.eqiad.wmnet
    Bug: T388534
    Change-Id: I36e8883ce5750b039f45b07bacd5e1256b3c77c9

--- modules/profile/manifests/mediawiki/maintenance/update_special_pages.pp
+++ modules/profile/manifests/mediawiki/maintenance/update_special_pages.pp
@@ -2,7 +2,13 @@ class profile::mediawiki::maintenance::update_special_pages(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
     profile::mediawiki::sharded_periodic_job { 'update_special_pages':
-        script   => 'updateSpecialPages.php',
-        interval => '*-1/3 05:00',
+        script                => 'updateSpecialPages.php',
+        interval              => '*-1/3 05:00',
+        cron_schedule         => '00 05 */3 * *',
+        kubernetes_shards     => ['s6'],  # TODO: T388534 - migrate remaining sections.
+        team                  => 'mediawiki-special-pages',
+        script_label          => 'updateSpecialPages.php',
+        description           => 'Update cached special pages',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
