c159c2cde1b3d7a975f8081170d3a5dc6e235a55
Balthazar Rouberol <brouberol@wikimedia.org>
2025-05-19 09:49:57 +0200

    deployment_server: install airflow-devenv on the host
    This will allow data-engineers to manage their airflow development
    environment running in Kubernetes from the deployment host.
    
    Hosts: deploy2002.codfw.wmnet
    Signed-off-by: Balthazar Rouberol <brouberol@wikimedia.org>
    Bug: T394038
    Change-Id: Ie37ee7af374775728d5fcb9093fbb35dc70145ec

---
+++ modules/profile/manifests/airflow_devenv.pp
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+class profile::airflow_devenv (
+    Optional[String] $version = lookup('profile::airflow_devenv::version', { 'default_value' => '0.0.1' }),
+) {
+  package { 'airflow-devenv':
+    ensure => $version,
+  }
+}
