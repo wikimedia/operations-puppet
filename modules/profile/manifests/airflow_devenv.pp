dbf2abf85b749e69ce15b143a9a155d63be6a2ef
Balthazar Rouberol <brouberol@wikimedia.org>
2025-05-19 20:12:15 +0200

    Upgrade airflow-devenv
    Signed-off-by: Balthazar Rouberol <brouberol@wikimedia.org>
    Bug: T393999
    Change-Id: Id1a3e5f6fbad86b99624490ddb32adce5db5eba3

--- modules/profile/manifests/airflow_devenv.pp
+++ modules/profile/manifests/airflow_devenv.pp
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::airflow_devenv (
-    Optional[String] $version = lookup('profile::airflow_devenv::version', { 'default_value' => '0.0.1' }),
+    Optional[String] $version = lookup('profile::airflow_devenv::version', { 'default_value' => '0.0.2' }),
 ) {
   package { 'airflow-devenv':
     ensure => $version,
