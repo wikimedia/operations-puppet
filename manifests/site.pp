c3387be59c5aca8b65a96589fe30e21303bbbc23
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-05-20 16:09:31 +0200

    Enable the remaining two maps nodes as replicas
    Bug: T381565
    Change-Id: I8c32acaf8a188566f9b665c85fb77e5f792a8a03

--- manifests/site.pp
+++ manifests/site.pp
@@ -2011,11 +2011,7 @@ node /^maps20(0[5-8]|10)\.codfw\./ {
     role(maps::replica)
 }
 
-node /^maps-test200[5-6]\.codfw\./ {
-    role(insetup::infrastructure_foundations_nftables)
-}
-
-node /^maps-test200[2-4]\.codfw\./ {
+node /^maps-test200[2-6]\.codfw\./ {
     role(maps::replica_bookworm)
 }
 
