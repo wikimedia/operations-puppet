4b3f9b46d45d4ee844549863f0beae0165fe0d3c
Brouberol <brouberol@wikimedia.org>
2025-05-20 14:43:03 +0000

    Revert "kafka-jumbo: provision broker 1018"
    Revert submission 1148335-T377874
    
    Reason for revert: These nodes had no /srv volumes, meaning that the
    kafka data was written to sda, which has 500GB of space.
    
    Reverted changes: /r/q/submissionid:1148335-T377874
    
    Change-Id: Ic54aee9b9468f635f9e7cca83be2ee3f25a8e192

--- manifests/site.pp
+++ manifests/site.pp
@@ -1731,8 +1731,13 @@ node /^kafka-main20(0[6-9]|10)\.codfw\./ {
 # kafka-jumbo is a large general purpose Kafka cluster.
 # This cluster exists only in eqiad, and serves various uses, including
 # mirroring all data from the main Kafka clusters in both main datacenters.
-node /^kafka-jumbo10(0[7-9]|1[0-8])\.eqiad\./ {
+node /^kafka-jumbo10(0[7-9]|1[0-7])\.eqiad\./ {
     role(kafka::jumbo::broker)
+}
+
+# New kafka-jumbo hosts - See #T377874
+node /^kafka-jumbo1018\.eqiad\./ {
+    role(insetup::data_platform_ferm)
 }
 
 # Kafkamon bullseye hosts
