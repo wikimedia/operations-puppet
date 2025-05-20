c8c4849e024a1503864fd0d94229499da657d2ba
Daniel Zahn <dzahn@wikimedia.org>
2025-05-20 12:28:17 -0700

    lists: include nftables throttling profile
    This adds general nftables throttling capabilities
    (not based on allow/deny lists) to the lists server role.
    
    This just adds the resources.
    
    https://puppet-compiler.wmflabs.org/output/1148432/5644/lists1004.wikimedia.org/index.html
    
    Bug: T394519
    Change-Id: I3bb410fcea74e7ed819bc84f7cb845a4ec72e37c

--- modules/role/manifests/lists.pp
+++ modules/role/manifests/lists.pp
@@ -3,6 +3,7 @@ class role::lists {
     include profile::base::production
     include profile::backup::host
     include profile::firewall
+    include profile::firewall::nftables_throttling
 
     include profile::lists
     include profile::locales::extended
