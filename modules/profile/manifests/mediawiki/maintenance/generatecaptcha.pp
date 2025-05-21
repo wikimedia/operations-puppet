7c5bfe1a08c4f6c9dca12e081462b0ced38765a2
Clément Goubert <cgoubert@wikimedia.org>
2025-05-21 14:50:21 +0200

    mw:maintenance: Adapt generatecaptcha for mw-cron
    Why:
    We need to be able to access the words and badwords lists from helmfile,
    which means they need to be either in the chart (which is not possible
    due to them being puppet secrets), or in a yaml value in
    helmfile_defaults.
    
    What:
    Create a fancycaptcha.yaml values file that will be added to mw-cron's
    helmfile for consumption by the chart, allowing us to create configmaps
    to include in the mw-cron containers.
    
    Hosts: mwmaint1002.eqiad.wmnet, deploy1003.eqiad.wmnet
    Bug: T388531
    Change-Id: If0f10634cb7a8db72f974098cb0bbe0f0e72ef84

--- modules/profile/manifests/mediawiki/maintenance/generatecaptcha.pp
+++ modules/profile/manifests/mediawiki/maintenance/generatecaptcha.pp
@@ -1,32 +1,82 @@
 class profile::mediawiki::maintenance::generatecaptcha(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
-    file { '/etc/fancycaptcha':
-        ensure => 'directory',
-        mode   => '0555',
-        owner  => 'root',
-        group  => 'root',
-    }
+    if $::_role == 'deployment_server/kubernetes' {
+        $fancycaptcha = {
+            'mw' => {
+                'fancycaptcha' => {
+                    'badwords' => secret('fancycaptcha/badwords'),
+                    'words' => secret('fancycaptcha/words')
+                }
+            }
+        }
 
-    file { '/etc/fancycaptcha/words':
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        content => secret('fancycaptcha/words');
-    }
+        file { "${helmfile_defaults_dir}/mediawiki/fancycaptcha.yaml":
+            mode    => '0444',
+            owner   => 'root',
+            group   => 'root',
+            content => $fancycaptcha.to_yaml,
+        }
 
-    file { '/etc/fancycaptcha/badwords':
-        mode    => '0444',
-        owner   => 'root',
-        group   => 'root',
-        content => secret('fancycaptcha/badwords');
-    }
+        # TODO: Remove old file path absent resources.
+        file { '/etc/fancycaptcha':
+            ensure => 'absent',
+            mode   => '0555',
+            owner  => 'root',
+            group  => 'root',
+        }
 
-    file { '/usr/local/bin/captchaloop':
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0555',
-        source => 'puppet:///modules/mediawiki/captchaloop',
+        file { '/etc/fancycaptcha/words':
+            ensure  => 'absent',
+            mode    => '0444',
+            owner   => 'root',
+            group   => 'root',
+            content => secret('fancycaptcha/words');
+        }
+
+        file { '/etc/fancycaptcha/badwords':
+            ensure  => 'absent',
+            mode    => '0444',
+            owner   => 'root',
+            group   => 'root',
+            content => secret('fancycaptcha/badwords');
+        }
+
+        file { '/usr/local/bin/captchaloop':
+            ensure => 'absent',
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            source => 'puppet:///modules/mediawiki/captchaloop',
+        }
+    } else {
+        file { '/etc/fancycaptcha':
+            ensure => 'directory',
+            mode   => '0555',
+            owner  => 'root',
+            group  => 'root',
+        }
+
+        file { '/etc/fancycaptcha/words':
+            mode    => '0444',
+            owner   => 'root',
+            group   => 'root',
+            content => secret('fancycaptcha/words');
+        }
+
+        file { '/etc/fancycaptcha/badwords':
+            mode    => '0444',
+            owner   => 'root',
+            group   => 'root',
+            content => secret('fancycaptcha/badwords');
+        }
+
+        file { '/usr/local/bin/captchaloop':
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0555',
+            source => 'puppet:///modules/mediawiki/captchaloop',
+        }
     }
 
     profile::mediawiki::periodic_job { 'generatecaptcha':
