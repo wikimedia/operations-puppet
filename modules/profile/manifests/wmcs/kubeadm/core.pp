113a7a9802a70dbeed5911e5fdfe39440de98332
David Caro <dcaro@wikimedia.org>
2025-05-21 11:10:30 +0200

    cloud: move images to use docker-registry.svc.t.o
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: I7a1663b70ec3e38f0140c7fdf9f477be2e38124c

--- modules/profile/manifests/wmcs/kubeadm/core.pp
+++ modules/profile/manifests/wmcs/kubeadm/core.pp
@@ -3,7 +3,7 @@
 class profile::wmcs::kubeadm::core (
     String[1]              $component           = lookup('profile::wmcs::kubeadm::component'),
     Optional[Stdlib::Fqdn] $label_custom_domain = lookup('profile::wmcs::kubeadm::label_custom_domain', {default_value => undef}),
-    String[1]              $pause_image         = lookup('profile::wmcs::kubeadm::pause_image', {default_value => 'docker-registry.tools.wmflabs.org/pause:3.9'}),
+    String[1]              $pause_image         = lookup('profile::wmcs::kubeadm::pause_image', {default_value => 'docker-registry.svc.toolforge.org/pause:3.9'}),
     Boolean                $mount_nfs           = lookup('mount_nfs', {default_value => false}),
 ) {
     class { '::kubeadm::repo':
