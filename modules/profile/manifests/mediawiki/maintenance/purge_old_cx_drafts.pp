de7068097f2cb8b718b23959ab4a2c13a1527c0d
Clément Goubert <cgoubert@wikimedia.org>
2025-05-22 17:11:37 +0200

    mw::maintenance: Migrate purge_old_cx_drafts to mw-cron
    Hosts: deploy1003.eqiad.wmnet, mwmaint1002.eqiad.wmnet
    Bug: T388539
    Change-Id: Ieae756b276a2d7a4ef91ed44c08ed64160d96d75

--- modules/profile/manifests/mediawiki/maintenance/purge_old_cx_drafts.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_old_cx_drafts.pp
@@ -5,7 +5,14 @@ class profile::mediawiki::maintenance::purge_old_cx_drafts(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
     profile::mediawiki::periodic_job { 'purge_old_cx_drafts':
-        command  => '/usr/local/bin/foreachwikiindblist "/srv/mediawiki/dblists/wikipedia.dblist - /srv/mediawiki/dblists/special.dblist - /srv/mediawiki/dblists/closed.dblist - /srv/mediawiki/dblists/testwikis.dblist - /srv/mediawiki/dblists/private.dblist" extensions/ContentTranslation/scripts/purge-unpublished-drafts.php --age-in-days=455 --notify-age-in-days=425 --really',
-        interval => '*-3,18 10:30'
+        command                 => '/usr/local/bin/foreachwikiindblist "/srv/mediawiki/dblists/wikipedia.dblist - /srv/mediawiki/dblists/special.dblist - /srv/mediawiki/dblists/closed.dblist - /srv/mediawiki/dblists/testwikis.dblist - /srv/mediawiki/dblists/private.dblist" extensions/ContentTranslation/scripts/purge-unpublished-drafts.php --age-in-days=455 --notify-age-in-days=425 --really',
+        interval                => '*-3,18 10:30',
+        cron_schedule           => '30 10 3,18 * *',
+        kubernetes              => true,
+        team                    => 'language_and_product_localization',
+        script_label            => 'purge-unpublished-drafts.php',
+        description             => 'Purge unpublished articles from ContentTranslation older than 455 days, notifying users 30 days prior',
+        helmfile_defaults_dir   => $helmfile_defaults_dir,
+        ttlsecondsafterfinished => 1814400, # 3 weeks
     }
 }
