5bf1ec022603334971dce804c9dbc7ff0387f9c2
Balthazar Rouberol <brouberol@wikimedia.org>
2025-05-22 08:43:01 +0200

    deployment_server: stop pinning airflow-devenv
    We'll simply `apt-get upgrade airflow-devenv` on the deployment servers
    via cumin.
    
    Signed-off-by: Balthazar Rouberol <brouberol@wikimedia.org>
    Bug: T393998
    Change-Id: I2567f72ab2d83abb554f9bc32b740917f34f4fc1

--- modules/profile/manifests/airflow_devenv.pp
+++ modules/profile/manifests/airflow_devenv.pp
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
-class profile::airflow_devenv (
-    Optional[String] $version = lookup('profile::airflow_devenv::version', { 'default_value' => '0.0.2' }),
-) {
+class profile::airflow_devenv {
   package { 'airflow-devenv':
-    ensure => $version,
+    ensure => 'present',
   }
 }
