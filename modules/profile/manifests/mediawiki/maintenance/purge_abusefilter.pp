67337b6277b50c2443d9409d18fc75c943996544
Hugh Nowlan <hnowlan@wikimedia.org>
2025-05-22 16:24:26 +0100

    mw::maintenance: migrate abusefilteripdata job to Kubernetes
    Bug: T388542
    Change-Id: I0ade1be22e117905d15c32025520164ba2b25af7

--- modules/profile/manifests/mediawiki/maintenance/purge_abusefilter.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_abusefilter.pp
@@ -1,8 +1,16 @@
 class profile::mediawiki::maintenance::purge_abusefilter(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+    $team = 'trust-and-safety-product'
+
     profile::mediawiki::periodic_job { 'purge_abusefilteripdata':
-        command  => '/usr/local/bin/foreachwiki extensions/AbuseFilter/maintenance/PurgeOldLogIPData.php',
-        interval => '01:15'
+        command               => '/usr/local/bin/foreachwiki extensions/AbuseFilter/maintenance/PurgeOldLogIPData.php',
+        interval              => '01:15',
+        cron_schedule         => '15 1 * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'PurgeOldLogIPData.php',
+        description           => 'Purge old abusefilter IP log data',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
