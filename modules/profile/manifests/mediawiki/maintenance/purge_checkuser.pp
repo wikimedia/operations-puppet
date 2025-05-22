8dd02edefbff04bbdc2fe2fe51fba3144812a839
Hugh Nowlan <hnowlan@wikimedia.org>
2025-05-22 12:49:51 +0100

    mw::maintenance: migrate checkuser and securepoll jobs to Kubernetes
    Migrate the remaining T&S jobs to Kubernetes. They are relatively
    straightforward database interactions and so should be well suited to
    being migrated.
    
    Bug: T388542
    Change-Id: I93b1b93409ab25560e50e8c88cfde68814770d93

--- modules/profile/manifests/mediawiki/maintenance/purge_checkuser.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_checkuser.pp
@@ -1,8 +1,16 @@
 class profile::mediawiki::maintenance::purge_checkuser(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+    $team = 'trust-and-safety-product'
+
     profile::mediawiki::periodic_job { 'purge_checkuser':
-        command  => '/usr/local/bin/foreachwiki extensions/CheckUser/maintenance/purgeOldData.php',
-        interval => '00:00'
+        command               => '/usr/local/bin/foreachwiki extensions/CheckUser/maintenance/purgeOldData.php',
+        interval              => '00:00',
+        cron_schedule         => '0 0 * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'purgeOldData.php',
+        description           => 'Purge expired rows in CheckUser and RecentChanges',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
--- modules/profile/manifests/mediawiki/maintenance/purge_securepoll.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_securepoll.pp
@@ -1,8 +1,16 @@
 class profile::mediawiki::maintenance::purge_securepoll(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+    $team = 'trust-and-safety-product'
+
     profile::mediawiki::periodic_job { 'purge_securepollvotedata':
-        command  => '/usr/local/bin/foreachwiki extensions/SecurePoll/cli/purgePrivateVoteData.php',
-        interval => '01:00'
+        command               => '/usr/local/bin/foreachwiki extensions/SecurePoll/cli/purgePrivateVoteData.php',
+        interval              => '01:00',
+        cron_schedule         => '0 1 * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'purgePrivateVoteData.php',
+        description           => 'Purge private data from SecurePoll votes',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
