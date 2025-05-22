184e230297b87df6829abfbbf8177b00388423d8
Clément Goubert <cgoubert@wikimedia.org>
2025-05-22 14:00:27 +0200

    mw::maintenance: Migrate cirrus_build_completion_indices to mw-cron
    - Parallelize using database sections instead of arbitry xargs -P4
    - Migrate to mw-cron
    
    Hosts: mwmaint1002.eqiad.wmnet, deploy1003.eqiad.wmnet
    Bug: T388538
    Change-Id: Ib02e7ff87bd93d69d0d541dec34c24b52619863d

--- modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
+++ modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
@@ -2,6 +2,7 @@ class profile::mediawiki::maintenance::cirrussearch(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
 
+    $team = 'search-platform'
 
     file { '/usr/local/bin/cirrus_build_completion_indices.sh':
         ensure => 'present',
@@ -14,13 +15,38 @@ class profile::mediawiki::maintenance::cirrussearch(
     # Rebuilds the completion suggester indices daily. This job, as of
     # mar 2015, takes around 5 hours to run.
     profile::mediawiki::periodic_job { 'cirrus_build_completion_indices_eqiad':
-        ensure   => absent, # Disable to avoid orphan indices while migrating to opensearch T388610
+        ensure   => absent, # Disable to replace with sharded periodic job
         command  => '/usr/local/bin/cirrus_build_completion_indices.sh eqiad',
         interval => '02:30',
     }
 
     profile::mediawiki::periodic_job { 'cirrus_build_completion_indices_codfw':
+        ensure   => absent, # Disable to replace with sharded periodic job
         command  => '/usr/local/bin/cirrus_build_completion_indices.sh codfw',
         interval => '02:30',
+    }
+
+    # Disable to avoid orphan indices while migrating to opensearch T388610
+    #profile::mediawiki::sharded_periodic_job { 'cirrus_build_completion_indices_eqiad':
+    #    interval              => '02:30',
+    #    cron_schedule         => '30 02 * * *',
+    #    shards                => ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18'],
+    #    script                => 'extensions/CirrusSearch/maintenance/UpdateSuggesterIndex.php --masterTimeout=10m --replicationTimeout=5400  --indexChunkSize=3000 --cluster=eqiad --optimize',
+    #    kubernetes            => true,
+    #    team                  => $team,
+    #    description           => 'Rebuild completion suggester indices daily',
+    #    script_label          => 'UpdateSuggesterIndex.php-eqiad',
+    #    helmfile_defaults_dir => $helmfile_defaults_dir,
+    #}
+    profile::mediawiki::sharded_periodic_job { 'cirrus_build_completion_indices_codfw':
+        interval              => '02:30',
+        cron_schedule         => '30 02 * * *',
+        shards                => ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17', 's8@18'],
+        script                => 'extensions/CirrusSearch/maintenance/UpdateSuggesterIndex.php --masterTimeout=10m --replicationTimeout=5400  --indexChunkSize=3000 --cluster=codfw --optimize',
+        kubernetes            => true,
+        team                  => $team,
+        description           => 'Rebuild completion suggester indices daily',
+        script_label          => 'UpdateSuggesterIndex.php-codfw',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
     }
 }
