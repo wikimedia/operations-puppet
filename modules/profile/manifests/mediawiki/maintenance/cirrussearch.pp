6512a95a2eceb291ad48920a1f32cf1babe45a31
Erik Bernhardson <ebernhardson@wikimedia.org>
2025-05-27 10:14:47 -0700

    cirrus: re-enable daily completion suggester builds in eqiad
    These were disabled during the opensearch migration. Now that the
    cluster is fully migrated we can turn them back on.
    
    Bug: T391350
    Change-Id: I0ca91bebd9c7f297028bcd1c09dc710c728e12b5

--- modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
+++ modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
@@ -26,18 +26,17 @@ class profile::mediawiki::maintenance::cirrussearch(
         interval => '02:30',
     }
 
-    # Disable to avoid orphan indices while migrating to opensearch T388610
-    #profile::mediawiki::sharded_periodic_job { 'cirrus_build_completion_indices_eqiad':
-    #    interval              => '02:30',
-    #    cron_schedule         => '30 02 * * *',
-    #    shards                => ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17'],
-    #    script                => 'extensions/CirrusSearch/maintenance/UpdateSuggesterIndex.php --masterTimeout=10m --replicationTimeout=5400  --indexChunkSize=3000 --cluster=eqiad --optimize',
-    #    kubernetes            => true,
-    #    team                  => $team,
-    #    description           => 'Rebuild completion suggester indices daily',
-    #    script_label          => 'UpdateSuggesterIndex.php-eqiad',
-    #    helmfile_defaults_dir => $helmfile_defaults_dir,
-    #}
+    profile::mediawiki::sharded_periodic_job { 'cirrus_build_completion_indices_eqiad':
+        interval              => '02:30',
+        cron_schedule         => '30 02 * * *',
+        shards                => ['s1@11', 's2@12', 's3@13', 's4@14', 's5@15', 's6@16', 's7@17'],
+        script                => 'extensions/CirrusSearch/maintenance/UpdateSuggesterIndex.php --masterTimeout=10m --replicationTimeout=5400  --indexChunkSize=3000 --cluster=eqiad --optimize',
+        kubernetes            => true,
+        team                  => $team,
+        description           => 'Rebuild completion suggester indices daily',
+        script_label          => 'UpdateSuggesterIndex.php-eqiad',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+    }
     profile::mediawiki::sharded_periodic_job { 'cirrus_build_completion_indices_codfw':
         interval              => '02:30',
         cron_schedule         => '30 02 * * *',
