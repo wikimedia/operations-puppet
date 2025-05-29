d62374fde307e7fb1a5e6408e4242865fc161514
Andrew Bogott <abogott@wikimedia.org>
2025-05-29 08:33:02 -0500

    Keystone: update hack out user ID validation
    I was overly optimistic thinking that our patching class
    could do more than one file at a time.
    
    Hosts: cloudcontrol1007.eqiad.wmnet, cloudcontrol2004-dev.codfw.wmnet
    Bug: T395542
    Change-Id: Idb9e0443cf62fcc345a927b67af4333a9e916efa

--- modules/openstack/manifests/keystone/service/epoxy.pp
+++ modules/openstack/manifests/keystone/service/epoxy.pp
@@ -137,10 +137,19 @@ class openstack::keystone::service::epoxy(
             require   => Package['keystone'];
     }
 
-
-    # Fix for https://phabricator.wikimedia.org/T395542, should bein upstream in F
-    openstack::patch { '/usr/lib/python3/dist-packages/':
-        source  => 'puppet:///modules/openstack/epoxy/keystone/hacks/0001-api-Remove-constraints-on-user-IDs.patch',
+    # Fixes for https://phabricator.wikimedia.org/T395542, should be in upstream in F
+    openstack::patch { '/usr/lib/python3/dist-packages/keystone/identity/schema.py':
+        source  => 'puppet:///modules/openstack/epoxy/keystone/hacks/keystone/identity/schema.py.patch',
+        require => Package['keystone'],
+        notify  => Service['keystone-admin', 'keystone'],
+    }
+    openstack::patch { '/usr/lib/python3/dist-packages/keystone/api/validation/parameter_types.py':
+        source  => 'puppet:///modules/openstack/epoxy/keystone/hacks/keystone/api/validation/parameter_types.py.patch',
+        require => Package['keystone'],
+        notify  => Service['keystone-admin', 'keystone'],
+    }
+    openstack::patch { '/usr/lib/python3/dist-packages/keystone/assignment/schema.py':
+        source  => 'puppet:///modules/openstack/epoxy/keystone/hacks/assignment/schema.py.patch',
         require => Package['keystone'],
         notify  => Service['keystone-admin', 'keystone'],
     }
