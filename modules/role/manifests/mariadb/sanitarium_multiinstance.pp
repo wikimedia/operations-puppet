e8ed418a078dc4aa796542e3ddf613a0e5a5f00c
Manuel Arostegui <marostegui@wikimedia.org>
2025-05-29 16:12:48 +0200

    sanitarium_multiinstance.pp: Fix comments
    Nothing major. Just clarifying and updating the role description
    
    Change-Id: I3d9c2cb111a340b7c1cd223e7bdca736c92b069e

--- modules/role/manifests/mariadb/sanitarium_multiinstance.pp
+++ modules/role/manifests/mariadb/sanitarium_multiinstance.pp
@@ -1,10 +1,7 @@
 # sanitarium_multiinstance: it replicates from all core shards (except x1),
-# and sanitizes most data on production on 7 shards, before the data
+# and sanitizes most data on production on 8 shards, before the data
 # arrives to labs
-# This role installs a 10.1 version which is needed for rbr triggers for
-# the new sanitarium server, which runs multi-instance and mariadb 10.1
-# Eventually, this role will deprecate the original sanitarium and
-# sanitarium2/sanitarium_multisource
+# This role uses rbr triggers and runs multi-instance.
 
 class role::mariadb::sanitarium_multiinstance {
     include profile::base::production
