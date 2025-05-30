c5b65c5808de7e1d25e49d953e101fe90db8183a
Tiziano Fogli <tfogli@wikimedia.org>
2025-05-30 15:39:53 +0200

    monitoring services: add migration task T228830 to instances
    Bug: T395443
    Change-Id: Ic0905f414afdab56c425b927e2324a364ef28e28

--- modules/profile/manifests/graphite/base.pp
+++ modules/profile/manifests/graphite/base.pp
@@ -237,15 +237,17 @@ class profile::graphite::base(
 
     # This check goes to the backend, which is http.
     monitoring::service { 'graphite_render':
-        description   => "${hostname} render",
-        check_command => "check_http_url!${hostname}!/render",
-        notes_url     => 'https://wikitech.wikimedia.org/wiki/Graphite#Operations_troubleshooting',
+        description    => "${hostname} render",
+        check_command  => "check_http_url!${hostname}!/render",
+        notes_url      => 'https://wikitech.wikimedia.org/wiki/Graphite#Operations_troubleshooting',
+        migration_task => 'T228830',
     }
 
     # Probe the API, the "carbon" hierarchy should always exist
     monitoring::service { 'graphite_api':
-        description   => "${hostname} api",
-        check_command => "check_http_url!${hostname}!/metrics/find?query=carbon.*",
-        notes_url     => 'https://wikitech.wikimedia.org/wiki/Graphite#Operations_troubleshooting',
+        description    => "${hostname} api",
+        check_command  => "check_http_url!${hostname}!/metrics/find?query=carbon.*",
+        notes_url      => 'https://wikitech.wikimedia.org/wiki/Graphite#Operations_troubleshooting',
+        migration_task => 'T228830',
     }
 }
