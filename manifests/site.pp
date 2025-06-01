da4ec9da5229cb655bf249aa2bed1c19d461c0b2
Andrew Bogott <abogott@wikimedia.org>
2025-06-01 13:25:34 -0500

    Add site.pp entries for new ceph osds
    Bug: T394333
    Change-Id: I83b8c89934071976d74d4ace9d60cebeda56e98c

--- manifests/site.pp
+++ manifests/site.pp
@@ -1819,7 +1819,8 @@ node /^cloudcephosd10(0[4-9]|1[0-9]|2[0-9]|3[0-9]|4[0-1])\.eqiad\./ {
     role(wmcs::ceph::osd)
 }
 
-node /^cloudcephosd104[2-7]\.eqiad\./ {
+# New ceph OSD nodes T394333 and T378828
+node /^cloudcephosd10(4[2-9]|5[0-1])\.eqiad\./ {
     role(insetup::wmcs_ferm)
 }
 
