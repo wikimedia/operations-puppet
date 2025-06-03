28a12bdffa391a132e4e38c90a674d8faa0281c0
Hugh Nowlan <hnowlan@wikimedia.org>
2025-06-03 16:27:14 +0100

    mw::maintenance: don't run purge-old-cx-drafts against test2wiki
    This job is currently failing because it is running against test2wiki
    which doesn't have the CX plugin enabled. The testwiki dblist doesn't
    include test2wiki, but the wikidataclient-test list does. Use this
    list for convenience.
    
    Bug: T395892
    Change-Id: Ia638063a56a8d22b08f0c4b07431fac25facab76

--- modules/profile/manifests/mediawiki/maintenance/purge_old_cx_drafts.pp
+++ modules/profile/manifests/mediawiki/maintenance/purge_old_cx_drafts.pp
@@ -4,8 +4,11 @@
 class profile::mediawiki::maintenance::purge_old_cx_drafts(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+    # We use wikidataclient-test here in order to prevent running this
+    # job against test2wiki, which isn't included in the testwiki
+    # dblist
     profile::mediawiki::periodic_job { 'purge_old_cx_drafts':
-        command                 => '/usr/local/bin/foreachwikiindblist "/srv/mediawiki/dblists/wikipedia.dblist - /srv/mediawiki/dblists/special.dblist - /srv/mediawiki/dblists/closed.dblist - /srv/mediawiki/dblists/testwikis.dblist - /srv/mediawiki/dblists/private.dblist" extensions/ContentTranslation/scripts/purge-unpublished-drafts.php --age-in-days=455 --notify-age-in-days=425 --really',
+        command                 => '/usr/local/bin/foreachwikiindblist "/srv/mediawiki/dblists/wikipedia.dblist - /srv/mediawiki/dblists/special.dblist - /srv/mediawiki/dblists/closed.dblist - /srv/mediawiki/dblists/wikidataclient-test.dblist - /srv/mediawiki/dblists/private.dblist" extensions/ContentTranslation/scripts/purge-unpublished-drafts.php --age-in-days=455 --notify-age-in-days=425 --really',
         interval                => '*-3,18 10:30',
         cron_schedule           => '30 10 3,18 * *',
         kubernetes              => true,
