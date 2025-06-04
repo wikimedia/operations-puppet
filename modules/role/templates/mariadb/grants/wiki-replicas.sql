01f32879af853359f27c974c0a19dc73d4c20a65
Francesco Negri <fnegri@wikimedia.org>
2025-06-04 12:53:32 +0200

    wikireplicas: centralize max_connections values
    These values are managed by the maintain-dbusers.py script. We don't
    need to keep them also in the SQL file, that (I assume) is used for
    manual copy/pasting.
    
    Change-Id: Ie18c9b3bc77f4d47de7955e192c53d92fbcaefa1

--- modules/role/templates/mariadb/grants/wiki-replicas.sql
+++ modules/role/templates/mariadb/grants/wiki-replicas.sql
@@ -49,15 +49,6 @@ GRANT SUPER ON *.* TO 'maintainindexes'@'localhost';
 -- viewmaster user
 GRANT SELECT ON *.* TO 'viewmaster'@'%';
 
--- quarry user granted 48 connections #T180141
-GRANT USAGE ON *.* TO 's52788'@'%' WITH MAX_USER_CONNECTIONS 48;
--- user for wikiscan granted 15 connections T227462
-GRANT USAGE ON *.* TO 'u12903'@'%' WITH MAX_USER_CONNECTIONS 15;
--- catscan2 (petscan) user granted 40 connections T255730
-GRANT USAGE ON *.* TO 's51156'@'%' WITH MAX_USER_CONNECTIONS 40;
--- Analytics user granted 200 connections
-GRANT USAGE ON *.* TO 's53272'@'%' WITH MAX_USER_CONNECTIONS 200;
-
 -- wmf-pt-kill user has to be granted SUPER and SHOW PROCESSLIST and should be able to login via unix_socket (T203674)
 
 GRANT PROCESS, SUPER ON *.* TO 'wmf-pt-kill'@'localhost' IDENTIFIED VIA unix_socket;
