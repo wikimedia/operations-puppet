0930429e5b004e6b01783d059ecab14ad921524a
Tiziano Fogli <tfogli@wikimedia.org>
2025-06-09 09:15:58 +0000

    Revert "prometheus::pop: temporarily exclude ops instance from prometheus7002"
    This reverts commit a2e983ccf28931ead180d0af63453907b874ca1c.
    
    Reason for revert: promote prometheus7002
    
    Change-Id: Iea0da3552f3e6ee9bd54a7408bb87896663285b2

--- modules/role/manifests/prometheus/pop.pp
+++ modules/role/manifests/prometheus/pop.pp
@@ -1,16 +1,12 @@
 class role::prometheus::pop {
-
-
     include profile::base::production
     include profile::firewall
 
     include profile::prometheus::common
 
-    if $facts['networking']['fqdn'] != 'prometheus7002.magru.wmnet' {
-        require profile::prometheus::ops
+    require profile::prometheus::ops
 
-        include profile::alerts::deploy::prometheus
-    }
+    include profile::alerts::deploy::prometheus
 
     include profile::prometheus::rsyncd
     include profile::prometheus::web
