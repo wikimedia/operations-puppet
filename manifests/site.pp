6b7448ef480440e02066dc424b52e096129b40cf
Balthazar Rouberol <brouberol@wikimedia.org>
2025-06-10 10:18:50 +0200

    Configure dse-k8s-worker100[2-3] with the dse_k8s::worker role
    Once the nodes have been renamed and reimaged correctly, we can then
    configure them as dse-k8s-worker nodes.
    
    Signed-off-by: Balthazar Rouberol <brouberol@wikimedia.org>
    Bug: T395557
    Change-Id: If676c740a2bb398ec2698596640d61c31df68ccd

--- manifests/site.pp
+++ manifests/site.pp
@@ -1057,12 +1057,8 @@ node /^dse-k8s-etcd100[1-3]\.eqiad\./ {
 }
 
 # new dse-k8s-workers T29157, T3074009, T394647, T395557
-node /^dse-k8s-worker10(0[1-9]|1[0-1])\.eqiad\./ {
+node /^dse-k8s-worker10(0[1-9]|1[0-3])\.eqiad\./ {
     role(dse_k8s::worker)
-}
-
-node /^dse-k8s-worker101[2-3]\.eqiad\./ {
-    role(insetup::data_platform_ferm)
 }
 
 # nfs server for xml dumps generation, also rsyncs xml dumps
