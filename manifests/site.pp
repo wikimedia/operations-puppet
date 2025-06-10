e3aa7f56d1e719b41063104c61b4694cf70fa4b5
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-06-10 16:32:28 +0200

    Switch install7002 back to insetup
    We need to make it work with Bookworm since Bullseye won't install
    with routed ganeti.
    
    Change-Id: Ifdd43dc8ed92573a600dcf4bb84f5095b8c076ae

--- manifests/site.pp
+++ manifests/site.pp
@@ -1617,8 +1617,12 @@ node /^install3003\.wikimedia\./ {
     role(installserver)
 }
 
-node /^install[4567]002\.wikimedia\./ {
+node /^install[456]002\.wikimedia\./ {
     role(installserver)
+}
+
+node /^install7002\.wikimedia\./ {
+    role(insetup::infrastructure_foundations_ferm)
 }
 
 node /^install7001\.wikimedia\./ {
