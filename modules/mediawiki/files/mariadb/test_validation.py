3b9900cc784826f1458ce03a2443ba3ef5119823
Bartosz Dziewoński <dziewonski@fastmail.fm>
2025-06-12 01:49:00 +0200

    tables-catalog: Alphabetize, enforce this in test_validation.py
    Yeah I just can't do this. It's bad enough that it's YAML.
    
    Change-Id: I17a918ee63c31323e85cb30f4a6d4481b3af170b

--- modules/mediawiki/files/mariadb/test_validation.py
+++ modules/mediawiki/files/mariadb/test_validation.py
@@ -17,7 +17,25 @@ def test_validation():
     schema = json.load(open(schema_path))
     data = yaml.safe_load(open(data_path))
     validate(data, schema, format_checker=FormatChecker())
-    assert True
+
+    def abbrev(x):
+        return {k: x[k] for k in ('name', 'source')}
+
+    # Require 'sources' to be ordered by key
+    sourcesKeys = list(data['sources'].keys())
+    assert sourcesKeys[0] == 'core', "sources: core goes first"
+    for x, y in zip(sourcesKeys[1:], sourcesKeys[2:]):
+        assert x <= y, f"sources: {x} and {y} are out of order (order must be alphabetical)"
+
+    # Require all sources listed in 'tables' to also exist in 'sources'
+    for x in data['tables']:
+        assert x['source'] in sourcesKeys, \
+            f"sources: source {x['source']} must be defined in {abbrev(x)}"
+
+    # Require 'tables' to be in the same order as sources
+    for x, y in zip(data['tables'][0:], data['tables'][1:]):
+        assert sourcesKeys.index(x['source']) <= sourcesKeys.index(y['source']), \
+            f"tables: {abbrev(x)} and {abbrev(y)} are out of order (order must match 'sources')"
 
 
 if __name__ == '__main__':
