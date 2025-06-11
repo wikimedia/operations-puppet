98b9cd73f4f63fb91d9ea46dd5c3980231177472
jelto <jwodstrcil@wikimedia.org>
2025-06-11 09:05:42 +0200

    gitlab: bump gitlab-settings to v1.10.0
    This change bumps gitlab-settings to v1.10.0. This version includes
    error handling for project.save() call which might fail for archived
    projects.
    
    I9485e868c7f7724b56c17dd774464713c239edea failed because I set the
    tag to the wrong commit. So lets just use v1.10.0.
    
    Bug: T395014
    Change-Id: I2fa48d13307df0cb6949f25aee5729b9b3866d87

--- modules/gitlab/manifests/init.pp
+++ modules/gitlab/manifests/init.pp
@@ -301,7 +301,7 @@ class gitlab (
     git::clone { 'repos/releng/gitlab-settings':
         ensure        => $ensure_gitlab_settings,
         update_method => 'checkout',
-        git_tag       => 'v1.9.0',
+        git_tag       => 'v1.10.0',
         directory     => '/srv/gitlab-settings',
         source        => 'gitlab',
         owner         => $gitlab_settings_user,
