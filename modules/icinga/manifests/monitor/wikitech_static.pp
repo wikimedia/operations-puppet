435b0832c8d5f591e418d8e222dcfa13e85fff14
Tiziano Fogli <tfogli@wikimedia.org>
2025-06-11 10:44:27 +0200

    monitoring services: add migration task T362397 to instances
    Bug: T395443
    Change-Id: I65d475399381a1bf9e0b53bd70b36831fecf22c1

--- modules/icinga/manifests/monitor/wikitech_static.pp
+++ modules/icinga/manifests/monitor/wikitech_static.pp
@@ -14,6 +14,7 @@ class icinga::monitor::wikitech_static () {
     check_interval => 120,
     host           => 'wikitech-static.wikimedia.org',
     notes_url      => 'https://wikitech.wikimedia.org/wiki/Wikitech-static',
+    migration_task => 'T362397',
   }
 
   # T163721
@@ -23,29 +24,33 @@ class icinga::monitor::wikitech_static () {
     check_interval => 720,
     host           => 'wikitech-static.wikimedia.org',
     notes_url      => 'https://wikitech.wikimedia.org/wiki/Wikitech-static',
+    migration_task => 'T362397',
   }
 
   monitoring::service { 'wikitech-static-main-page':
-    description   => 'Wikitech-static main page has content',
-    check_command => 'check_https_url_at_address_for_string!wikitech-static.wikimedia.org!/wiki/Main_Page?debug=true!Wikitech',
-    contact_group => 'wmcs-bots,admins',
-    host          => 'wikitech-static.wikimedia.org',
-    notes_url     => 'https://wikitech.wikimedia.org/wiki/Wikitech-static',
+    description    => 'Wikitech-static main page has content',
+    check_command  => 'check_https_url_at_address_for_string!wikitech-static.wikimedia.org!/wiki/Main_Page?debug=true!Wikitech',
+    contact_group  => 'wmcs-bots,admins',
+    host           => 'wikitech-static.wikimedia.org',
+    notes_url      => 'https://wikitech.wikimedia.org/wiki/Wikitech-static',
+    migration_task => 'T362397',
   }
 
   monitoring::service { 'https_wikitech-static':
-    description   => 'HTTPS-wikitech-static',
-    check_command => 'check_ssl_http_letsencrypt!wikitech-static.wikimedia.org',
-    host          => 'wikitech-static.wikimedia.org',
-    contact_group => 'wmcs-bots,admins',
-    notes_url     => 'https://wikitech.wikimedia.org/wiki/Wikitech-static',
+    description    => 'HTTPS-wikitech-static',
+    check_command  => 'check_ssl_http_letsencrypt!wikitech-static.wikimedia.org',
+    host           => 'wikitech-static.wikimedia.org',
+    contact_group  => 'wmcs-bots,admins',
+    notes_url      => 'https://wikitech.wikimedia.org/wiki/Wikitech-static',
+    migration_task => 'T362397',
   }
 
   monitoring::service { 'https_status-wikimedia':
-    description   => 'HTTPS-status-wikimedia-org',
-    check_command => 'check_ssl_http_letsencrypt!status.wikimedia.org',
-    host          => 'wikitech-static.wikimedia.org',
-    contact_group => 'wikitech-static',
-    notes_url     => 'https://wikitech.wikimedia.org/wiki/Wikitech-static',
+    description    => 'HTTPS-status-wikimedia-org',
+    check_command  => 'check_ssl_http_letsencrypt!status.wikimedia.org',
+    host           => 'wikitech-static.wikimedia.org',
+    contact_group  => 'wikitech-static',
+    notes_url      => 'https://wikitech.wikimedia.org/wiki/Wikitech-static',
+    migration_task => 'T362397',
   }
 }
