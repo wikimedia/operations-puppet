616a9e3c390eccdfacbb1eb644098b6449bfd666
Ben Tullis <btullis@wikimedia.org>
2025-06-11 16:52:32 +0100

    Remove obsolete analytics_cluster::postgresql role and profile
    We had two servers named an-db100[1-2] which were used for running
    PostgreSQL for Airflow.
    
    These postgres services have now been moved to the dse-k8s cluster and
    an-db100[1-2] have been recommissioned as dse-k8s-worker nodes.
    
    Therefore, this role and the associated profile can be removed from
    puppet. We will follow up by deleting the secrets from the private repo.
    
    Bug: T395557
    Change-Id: Ia0490ce225708f9a4d3dffeaeb6546e12051ea18

--- modules/role/manifests/analytics_cluster/postgresql.pp
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-class role::analytics_cluster::postgresql {
-    include profile::base::production
-    include profile::firewall
-    include profile::analytics::postgresql
-    include profile::prometheus::postgres_exporter
-}
