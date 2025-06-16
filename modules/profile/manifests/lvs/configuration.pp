a226578fba8ada0efaf1ca03e8527a9b84593204
BCornwall <bcornwall@wikimedia.org>
2025-06-16 18:47:13 +0000

    Revert "hiera: Add lvs1016 to high-traffic1"
    This reverts commit 0c514cf0f14f260d76e6d80aa03e4896a792ecbf.
    
    Reason for revert: Sorting out connectivity issues
    
    Change-Id: I17a9fe4a5b961280eabe84f67fecc285f65fdc49

--- modules/profile/manifests/lvs/configuration.pp
+++ modules/profile/manifests/lvs/configuration.pp
@@ -4,7 +4,7 @@ class profile::lvs::configuration {
     $lvs_class_hosts = {
         'high-traffic1' => $::realm ? {
             'production' => $::site ? {
-                'eqiad' => [ 'lvs1017', 'lvs1020', 'lvs1016' ],
+                'eqiad' => [ 'lvs1017', 'lvs1020' ],
                 'codfw' => [ 'lvs2011', 'lvs2014' ],
                 'esams' => [ 'lvs3008', 'lvs3010' ],
                 'ulsfo' => [ 'lvs4008', 'lvs4010' ],
@@ -58,7 +58,6 @@ class profile::lvs::configuration {
     # in profile::mediawiki::maintenance::wikidata
     $lvs_classes =  {
         'lvs1013' => 'secondary',
-        'lvs1016' => 'high-traffic1',
         'lvs1017' => 'high-traffic1',
         'lvs1018' => 'high-traffic2',
         'lvs1019' => 'low-traffic',
