9afa6f40671c3e17e19e695a6912a94dd5846f62
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2025-06-17 12:56:13 +0200

    thanos: temp disable sidecar series limit
    The istio_sli_* recording rules are hitting the series limits and
    causing alerts, back out of the series limit for now and revisit later.
    
    Bug: T394318
    Change-Id: I82557ee333b65154c2b5e1e60afdf7fe7f33a485

--- modules/profile/manifests/thanos/sidecar.pp
+++ modules/profile/manifests/thanos/sidecar.pp
@@ -33,7 +33,7 @@ define profile::thanos::sidecar (
         grpc_port             => $grpc_port,
         min_time              => $min_time,
         tracing_enabled       => true,
-        limits_request_series => 256000,
+        limits_request_series => 0,
     }
 
     if $enable_upload {
