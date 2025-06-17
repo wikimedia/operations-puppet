503a930a284944237c5dea60a4b60b53e7abfd94
Cole White <cwhite@wikimedia.org>
2025-06-17 19:29:02 +0000

    logstash: add tests to normalize_labels
    Bugfix: use value.to_json to avoid using ruby hash format.
    
    Bug: T368956
    Change-Id: I038e995d602cadb0a60949083309c1456a09caf8

--- modules/profile/files/logstash/filter_scripts/normalize_labels.rb
+++ modules/profile/files/logstash/filter_scripts/normalize_labels.rb
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # normalize_labels.rb
 # Logstash Ruby script to cast the type of all labels to string
-# @version 1.0.0
+# @version 1.1.0
 
 def register(*) end
 
@@ -13,8 +13,69 @@ def filter(event)
 
   # cast all labels to string
   original_labels.each do | key, value |
+    if value.is_a?(Hash) || value.is_a?(Array)
+      event.set("[labels][#{key}]", value.to_json)
+      next
+    end
     event.set("[labels][#{key}]", value.to_s)
   end
 
   [event]
+end
+
+if __FILE__ == $PROGRAM_NAME
+  require_relative '../helpers/filter_scripts_test_helper'
+  register({})
+
+  fixture = {
+    'labels' => {
+      'bool' => false,
+      'int' => 1,
+      'float' => 3.14,
+      'hash' => {'foo' => 0},
+      'array' => ['foo', 0, true, 3.14],
+      'nil' => nil
+    },
+    'type' => 'test',
+    'http' => {
+      'request' => {
+        'headers' => {
+          'foo' => { 'a' => 'b' }
+        }
+      }
+    }
+  }
+
+  event = filter(Event.new(fixture))[0]
+
+  # print the filtered output if first argument is 'debug'
+  if ARGV[0] == 'debug'
+    require 'json'
+    puts(JSON.pretty_generate(event.to_hash))
+  end
+
+  assert_true(
+    'bools are strings',
+    event.get('[labels][bool]') == 'false'
+  )
+  assert_true(
+    'ints are strings',
+    event.get('[labels][int]') == '1'
+  )
+  assert_true(
+    'floats are strings',
+    event.get('[labels][float]') == '3.14'
+  )
+  assert_true(
+    'hashes are strings',
+    event.get('[labels][hash]') == '{"foo":0}'
+  )
+  assert_true(
+    'arrays are strings',
+    event.get('[labels][array]') == '["foo",0,true,3.14]'
+  )
+  assert_true(
+    'nils are empty',
+    event.get('[labels][nil]') == ''
+  )
 end
