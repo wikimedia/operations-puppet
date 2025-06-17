960c05b6839cbd6fcdc8b8c44f0c2e99f71f204a
Luca Toscano <ltoscano@wikimedia.org>
2025-06-17 12:32:55 +0200

    profile::pyrra: improve the istio SLOs template
    Allow to set only the availability SLI, without the latency one.
    
    Allow to filter the latency SLI's success metric by HTTP response
    codes, to isolate use cases like filtering only HTTP 20X.
    
    Reduce the default window to 4w instead of 12w.
    
    Bug: T391852
    Change-Id: Id6cbc8a783846c5fa56f3e03b57d66bddb49f253

--- modules/profile/manifests/pyrra/filesystem/slos/istio.pp
+++ modules/profile/manifests/pyrra/filesystem/slos/istio.pp
@@ -3,7 +3,6 @@
 define profile::pyrra::filesystem::slos::istio(
     String $team,
     String $slo_requests_target,
-    String $slo_latency_target,
     Array[String] $datacenters = ['eqiad', 'codfw'],
     String $window = '4w',
     String $destination_canonical_service = "${title}-production",
@@ -12,7 +11,9 @@ define profile::pyrra::filesystem::slos::istio(
     String $k8s_cluster_name = 'wikikube',
     Boolean $enable_alerts = false,
     String $pyrra_namespace = 'pyrra-o11y',
-    Wmflib::Ensure $ensure = 'present'
+    Wmflib::Ensure $ensure = 'present',
+    Optional[String] $slo_latency_target = undef,
+    Optional[String] $latency_target_requests_regex = undef,
 ) {
     $datacenters.each |$datacenter| {
         pyrra::filesystem::config { "${k8s_cluster_name}-${title}-requests-${datacenter}.yaml":
@@ -48,38 +49,48 @@ define profile::pyrra::filesystem::slos::istio(
             },
           })
         }
-        pyrra::filesystem::config { "${k8s_cluster_name}-${title}-latency-${datacenter}.yaml":
-          ensure  => $ensure,
-          content => to_yaml({
-            'apiVersion' => 'pyrra.dev/v1alpha1',
-            'kind'       => 'ServiceLevelObjective',
-            'metadata'   => {
-                'name'      => "${title}-latency",
-                'namespace' => "${pyrra_namespace}", #lint:ignore:only_variable_string
-                'labels'    => {
-                    'pyrra.dev/team'    => "${team}", #lint:ignore:only_variable_string
-                    'pyrra.dev/service' => "${title}", #lint:ignore:only_variable_string
-                    'pyrra.dev/site'    => "${datacenter}", #lint:ignore:only_variable_string
+        if $slo_latency_target {
+            # We want to be able to trim the success part of the SLI based on the response code. For example, in some cases
+            # it may makes sense to just pay attention to HTTP 20X responses, rather than a mixture of 20x/30x/40x all with
+            # different latency performances.
+            $base_latency_success_sli_labels = "source_workload_namespace=\"istio-system\", source_workload=\"istio-ingressgateway\", destination_canonical_service=\"${destination_canonical_service}\", le=\"${latency_max_seconds_bucket}\", site=\"${datacenter}\""
+            $latency_success_sli = $latency_target_requests_regex ? {
+                undef   => "istio_request_duration_milliseconds_bucket{${base_latency_success_sli_labels}}",
+                default => "istio_request_duration_milliseconds_bucket{${base_latency_success_sli_labels}, response_code=~\"${latency_target_requests_regex}\"}"
+            }
+            pyrra::filesystem::config { "${k8s_cluster_name}-${title}-latency-${datacenter}.yaml":
+              ensure  => $ensure,
+              content => to_yaml({
+                'apiVersion' => 'pyrra.dev/v1alpha1',
+                'kind'       => 'ServiceLevelObjective',
+                'metadata'   => {
+                    'name'      => "${title}-latency",
+                    'namespace' => "${pyrra_namespace}", #lint:ignore:only_variable_string
+                    'labels'    => {
+                        'pyrra.dev/team'    => "${team}", #lint:ignore:only_variable_string
+                        'pyrra.dev/service' => "${title}", #lint:ignore:only_variable_string
+                        'pyrra.dev/site'    => "${datacenter}", #lint:ignore:only_variable_string
+                    },
                 },
-            },
-            'spec'       => {
-                'alerting'  => {
-                    'burnrates' => $enable_alerts
-                },
-                'target'    => $slo_latency_target,
-                'window'    => $window,
-                'indicator' => {
-                    'latency'  => {
-                        'success' => {
-                            'metric' => "istio_request_duration_milliseconds_bucket{source_workload_namespace=\"istio-system\", source_workload=\"istio-ingressgateway\", destination_canonical_service=\"${destination_canonical_service}\", le=\"${latency_max_seconds_bucket}\", site=\"${datacenter}\" }",
-                        },
-                        'total'   => {
-                            'metric' => "istio_request_duration_milliseconds_count{source_workload_namespace=\"istio-system\", source_workload=\"istio-ingressgateway\", destination_canonical_service=\"${destination_canonical_service}\", site=\"${datacenter}\" }",
+                'spec'       => {
+                    'alerting'  => {
+                        'burnrates' => $enable_alerts
+                    },
+                    'target'    => $slo_latency_target,
+                    'window'    => $window,
+                    'indicator' => {
+                        'latency'  => {
+                            'success' => {
+                                'metric' => $latency_success_sli,
+                            },
+                            'total'   => {
+                                'metric' => "istio_request_duration_milliseconds_count{source_workload_namespace=\"istio-system\", source_workload=\"istio-ingressgateway\", destination_canonical_service=\"${destination_canonical_service}\", site=\"${datacenter}\" }",
+                            },
                         },
                     },
                 },
-            },
-          })
+              })
+            }
         }
     }
 }
