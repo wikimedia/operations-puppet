639365327d5bdcf8ca2a31cfebf55c2858ee2eb6
Erik Bernhardson <ebernhardson@wikimedia.org>
2025-06-24 12:15:57 -0700

    cirrus: Drop absented periodic_job
    These jobs additional refer to a script that gets installed, mark
    that script as absented so it gets deleted and the declaration
    can be dropped later.
    
    Change-Id: I5bc32bb2c53eb273d23cca3328722210f5d34bfa

--- modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
+++ modules/profile/manifests/mediawiki/maintenance/cirrussearch.pp
@@ -5,25 +5,11 @@ class profile::mediawiki::maintenance::cirrussearch(
     $team = 'search-platform'
 
     file { '/usr/local/bin/cirrus_build_completion_indices.sh':
-        ensure => 'present',
+        ensure => 'absent',
         owner  => 'root',
         group  => 'root',
         mode   => '0555',
         source => 'puppet:///modules/profile/mediawiki/maintenance/cirrus_build_completion_indices.sh',
-    }
-
-    # Rebuilds the completion suggester indices daily. This job, as of
-    # mar 2015, takes around 5 hours to run.
-    profile::mediawiki::periodic_job { 'cirrus_build_completion_indices_eqiad':
-        ensure   => absent, # Disable to replace with sharded periodic job
-        command  => '/usr/local/bin/cirrus_build_completion_indices.sh eqiad',
-        interval => '02:30',
-    }
-
-    profile::mediawiki::periodic_job { 'cirrus_build_completion_indices_codfw':
-        ensure   => absent, # Disable to replace with sharded periodic job
-        command  => '/usr/local/bin/cirrus_build_completion_indices.sh codfw',
-        interval => '02:30',
     }
 
     profile::mediawiki::sharded_periodic_job { 'cirrus_build_completion_indices_eqiad':
