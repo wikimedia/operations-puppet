d0e4c356c4a444c1b4032c9ba7e7fdd77b4699f5
Andrea Denisse <adenisse@wikimedia.org>
2025-06-25 23:48:25 +0000

    centrallog: Log with standard and custom template
    Bug: T383309
    Change-Id: I985fb574761a1820d89a40ef623fd3419650361a

--- modules/profile/manifests/syslog/centralserver.pp
+++ modules/profile/manifests/syslog/centralserver.pp
@@ -119,4 +119,26 @@ class profile::syslog::centralserver (
         source => 'puppet:///modules/mtail/programs/ulogd.mtail',
         notify => Service['mtail'],
     }
+
+    # Temporary debug log for rsyslog misrouting investigation
+    # Safe to remove once T383309 is resolved
+    rsyslog::conf { 'debug_format':
+        ensure   => present,
+        content  => epp('profile/rsyslog/sample_debug_filter.conf.epp', { 'sampling_rate' => String(10) }),
+        priority => 1,
+    }
+
+    logrotate::rule { 'rsyslog-debug':
+        ensure         => present,
+        file_glob      => '/srv/syslog/rsyslog-debug-custom.log /srv/syslog/rsyslog-debug-standard.log',
+        frequency      => 'hourly',
+        size           => '5G',
+        date_ext       => true,
+        date_yesterday => true,
+        rotate         => 3,
+        missing_ok     => true,
+        no_create      => true,
+        compress       => true,
+        post_rotate    => ['service rsyslog rotate >/dev/null 2>&1 || true'],
+    }
 }
