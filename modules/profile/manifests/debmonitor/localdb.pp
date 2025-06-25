738df5c401ad26cb0ecbd628cfc47cc8045f9e35
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-06-25 09:16:40 +0200

    debmonitor_dev: Update bind address for mariadb
    Puppetise a config snippet to ensure that mariadb not only
    listens on localhost, but also on it's full IP (as required
    by the DB grants)
    
    Hosts: debmonitor-dev2001.codfw.wmnet
    Bug: T397696
    Change-Id: Iae547905722456929f8c07125ce5d87670cf551e

--- modules/profile/manifests/debmonitor/localdb.pp
+++ modules/profile/manifests/debmonitor/localdb.pp
@@ -3,4 +3,13 @@
 class profile::debmonitor::localdb ()
 {
     ensure_packages('mariadb-server')
+
+    # Puppetise a config snippet to ensure that mariadb not only
+    # listens on localhost, but also on it's full IP (as required
+    # by the DB grants)
+    file { '/etc/mysql/mariadb.conf.d/70-bind-on-full-ip.cnf':
+        mode    => '0644',
+        content => template('profile/debmonitor/mariadb.erb'),
+        require => Package['mariadb-server'],
+    }
 }
