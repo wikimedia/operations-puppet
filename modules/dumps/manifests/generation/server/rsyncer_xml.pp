a62764053b14e5077025e05fb4466fe63831c564
Ben Tullis <btullis@wikimedia.org>
2025-06-26 11:42:44 +0100

    Dumps_v1: Disable the sync job that publishes from dumpsdata servers
    We are migrating the dumps from the snapshot and dumpsdata servers to
    airflow and kubernetes.
    
    This commit constitutes part of the migration plan, whereby the existing
    dumpsdata servers stop running the job that publishes the dumps to the
    clouddumps servers.
    
    This should not be merged until late on June 30th.
    
    There will be a corresponding change to airflow-dags that updates the
    destination directory of the dumps sync tasks to the production target.
    
    Hosts: C:dumps::generation::server::rsyncer_common
    Bug: T397848
    Change-Id: If2ce6e3b66db4614c2a87239a15ea8ed1d56e75a

--- modules/dumps/manifests/generation/server/rsyncer_xml.pp
+++ modules/dumps/manifests/generation/server/rsyncer_xml.pp
@@ -5,7 +5,7 @@ class dumps::generation::server::rsyncer_xml(
     include ::dumps::generation::server::rsyncer_common
 
     systemd::service { 'dumps-rsyncer':
-        ensure    => 'present',
+        ensure    => 'absent',
         restart   => true,
         content   => systemd_template('dumps-rsync-peers-xml'),
         subscribe => [File['/usr/local/bin/rsync-via-primary.sh'], File['/usr/local/bin/rsyncer_lib.sh']],
