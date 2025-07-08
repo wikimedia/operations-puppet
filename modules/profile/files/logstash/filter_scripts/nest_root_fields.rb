0ea10c37ac0fd7d6bdce9d44744b347641ecf419
Cole White <cwhite@wikimedia.org>
2025-07-08 16:38:54 +0000

    logstash: nest_root_fields.rb fix memory leak and add tests
    Move writing global variables into register() and
    stop altering it at runtime.
    
    Add tests for the plugin.
    
    Bug: T398990
    Bug: T368956
    Change-Id: Ib14e07444a353dd62dbae1fc85c46537e2de69f4

--- modules/profile/files/logstash/filter_scripts/nest_root_fields.rb
+++ modules/profile/files/logstash/filter_scripts/nest_root_fields.rb
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # nest_root_fields.rb
 # Logstash Ruby script to copy all root fields into sub-fields of an object
-# @version 1.0.1
+# @version 1.0.2
 #
 # Example Logstash Filter:
 # ruby {
@@ -55,11 +55,12 @@ def register(params)
     rsyslog.severity
     rsyslog.timereported
   ]
+  @exclude = @exclude.concat(@common_fields) if @exclude_common_fields
 end
 
 # get the event with an additional tag
 def event_with_tag(event, value)
-  tags = event.get("tags")
+  tags = event.get("tags") || []
   tags.push(value)
   event.set("tags", tags)
   event
@@ -67,28 +68,101 @@ end
 
 def filter(event)
   unless @target.nil? # skip if no target provided
+    output = event.get(@target)
 
-    target_field_values = event.get(@target)
-    result = {}
-
-    if target_field_values.instance_of?(Hash) # if target is a hash, we can use it as-is
-      result = target_field_values.to_hash
-    else # if target exists and is not hashable, do nothing else but add a tag so we know it happened
+    if output.instance_of?(Hash)
+      output = output.to_hash
+    else
+      output = {}
+      # if target exists, is not hashable, and overwrite is not requested,
+      # do nothing else but add a tag so we know it happened
       return [event_with_tag(event, '_nest_root_fields_unhashable_target_field')] unless @overwrite
     end
-
-    @exclude += @common_fields if @exclude_common_fields
 
     event.to_hash.each_key do |k|
       next if k[0] == "@" # skip meta fields
       next if k == "tags" # skip tags
       next if @exclude.include?(k) # skip excluded fields
-      result[k] = event.get(k)
+      output[k] = event.get(k)
       event.remove(k)
     end
 
-    event.set(@target, result)
+    event.set(@target, output)
 
   end
   [event]
+end
+
+# run tests with `ruby convert_numeric_fields_on_templates.rb
+if __FILE__ == $PROGRAM_NAME
+  require_relative '../helpers/filter_scripts_test_helper'
+
+  def print_json(event)
+    # print the filtered output if first argument is 'debug'
+    if ARGV[0] == 'debug'
+      require 'json'
+      puts(JSON.pretty_generate(event.to_hash))
+    end
+  end
+
+  def fixture
+    {
+      'labels' => 'foo',
+      'host' => 'alert2002',
+      'summary' => 'Increased Speed Index for en.wiki on desktop',
+      'tool' => 'webpagetest',
+      'alertname' => 'Increased Speed Index for en.wiki on desktop',
+      'description' => 'All three URLS for enwiki (using Firefox desktop) fired with a difference of 100 ms.'
+    }
+  end
+
+  register({
+    'exclude' => ['description', 'summary'],
+    'exclude_common_fields' => true,
+    'target' => 'labels',
+    'overwrite' => true
+  })
+  event = filter(Event.new(fixture))[0]
+  print_json(event)
+
+  assert_true(
+    'summary field is at root',
+    event.get('summary') == 'Increased Speed Index for en.wiki on desktop'
+  )
+  assert_true(
+    'description field is at root',
+    event.get('description') == 'All three URLS for enwiki (using Firefox desktop) fired with a difference of 100 ms.'
+  )
+
+  assert_true(
+    'labels is populated with overwrite',
+    event.get('labels') == {'labels' => 'foo', 'tool' => 'webpagetest', 'alertname' => 'Increased Speed Index for en.wiki on desktop'}
+  )
+  register({
+    'exclude' => ['description', 'summary'],
+    'exclude_common_fields' => false,
+    'target' => 'labels',
+    'overwrite' => true
+  })
+  event = filter(Event.new(fixture))[0]
+  print_json(event)
+
+  assert_true(
+    'common fields are moved when exclude_common_fields == false',
+    event.get('labels')['host'] == 'alert2002'
+  )
+
+  register({
+    'exclude' => ['description', 'summary'],
+    'exclude_common_fields' => false,
+    'target' => 'labels',
+    'overwrite' => false
+  })
+  event = filter(Event.new(fixture))[0]
+  print_json(event)
+
+  assert_true(
+    'setting override == false causes event to be tagged',
+    event.get('tags') == ['_nest_root_fields_unhashable_target_field']
+  )
 end
