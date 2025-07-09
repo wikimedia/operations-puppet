56e98140cf7f22a1175ede5e0910d682e6a505f5
David Caro <dcaro@wikimedia.org>
2025-07-09 11:38:11 +0200

    aptly: add arm64 arch support
    There's no need to pick which one, but we need to create the publish
    of the repo with the support beforehand in case it's needed.
    
    Bug: T398016
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: If85dc537b6034d94d3b90f8830444d8bcedd7ad4

--- modules/aptly/manifests/repo.pp
+++ modules/aptly/manifests/repo.pp
@@ -18,7 +18,7 @@ define aptly::repo (
         # Pubish the repo directly, without snapshots
         # This isn't reccomended by aptly for production uses, but is perfect for labs :D
         exec { "publish-aptly-repo-${title}":
-            command => "/usr/bin/aptly -architectures=amd64,all -skip-signing -origin=Wikimedia -label='${title}' -distribution='${title}' -component='${component}' publish repo ${title}",
+            command => "/usr/bin/aptly -architectures=arm64,amd64,all -skip-signing -origin=Wikimedia -label='${title}' -distribution='${title}' -component='${component}' publish repo ${title}",
             unless  => "/usr/bin/aptly publish list | /bin/grep -F '[${title}]' > /dev/null",
             user    => $user,
             cwd     => '/',
