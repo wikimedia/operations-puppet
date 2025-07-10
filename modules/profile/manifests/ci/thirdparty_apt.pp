9d2ae6faef149db1b4cef3a068149eaf633689f0
Antoine Musso <hashar@free.fr>
2025-07-10 10:49:19 +0200

    Use thirdparty/jenkins on Bookworm
    The `thirdparty/ci` component hosts the Jenkins package and the
    `docker-ce` package imported from upstream.
    
    Bookworm provides a more recent Docker package, and it was decided in
    9478f801d88 to use `thirdparty/jenkins` starting from Bookworm.
    
    On releases2003 (Bookworm), Jenkins can't install since apt is still
    configured to use `thirdparty/ci`:
    
      W: Skipping acquire of configured file 'thirdparty/ci/source/Sources'
      as repository 'http://apt.wikimedia.org/wikimedia bookworm-wikimedia
      InRelease' doesn't have the component 'thirdparty/ci' (component
      misspelt in sources.list?)
    
    Adjust `profile::ci::thirdparty_apt` to use `thirdparty/ci` before
    Bookworm and `thirdparty/jenkins` otherwhise.
    
    Note on Bookworm we use `docker.io` as a package name which is provided
    by the Debian project. `docker-ce` is only used on Buster and from
    `thirdparty/ci`.
    
    Bug: T392127
    Hosts: contint1002.wikimedia.org
    Hosts: releases1003.eqiad.wmnet
    Hosts: releases2003.codfw.wmnet
    Change-Id: Id5bd980338c4506a8f7215186ffb72e8fd8f0fdd

--- modules/profile/manifests/ci/thirdparty_apt.pp
+++ modules/profile/manifests/ci/thirdparty_apt.pp
@@ -1,8 +1,23 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::ci::thirdparty_apt {
-    apt::repository { 'thirdparty-ci':
-      uri        => 'http://apt.wikimedia.org/wikimedia',
-      dist       => "${::lsbdistcodename}-wikimedia",
-      components => 'thirdparty/ci',
+
+    # thirdparty/ci is used on contint to provide a newer Docker package AND Jenkins.
+    #
+    # Since Bookworm provides a recent enough Docker, we only need it for
+    # Jenkins and thirdparty/ci is now named thirdparty/jenkins (which
+    # should be configured independently)
+
+    if debian::codename::lt('bookworm') {
+        apt::repository { 'thirdparty-ci':
+            uri        => 'http://apt.wikimedia.org/wikimedia',
+            dist       => "${::lsbdistcodename}-wikimedia",
+            components => 'thirdparty/ci',
+        }
+    } else {
+        apt::repository { 'thirdparty-jenkins':
+            uri        => 'http://apt.wikimedia.org/wikimedia',
+            dist       => "${::lsbdistcodename}-wikimedia",
+            components => 'thirdparty/jenkins',
+        }
     }
 }
