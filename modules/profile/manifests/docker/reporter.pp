9127be6f8ad677b5b3cea3fc93c39aeb533ef408
Luca Toscano <ltoscano@wikimedia.org>
2025-07-10 17:16:58 +0200

    profile::docker::reporter: add Wikikube and ML serve prod clusters
    Bug: T397696
    Change-Id: I7a6c11c42a6c76b244c75dddb2b61a3a52d7e7f9

--- modules/profile/manifests/docker/reporter.pp
+++ modules/profile/manifests/docker/reporter.pp
@@ -40,7 +40,7 @@ class profile::docker::reporter(
             k8s_kubeconfig_path => '/etc/kubernetes/debmonitor-staging-eqiad.config',
             rule_filename       => 'wikikube_kubernetes_rules.ini'
             ;
-        # Report on the staging-eqiad kubernetes cluster.
+        # Report on the staging-codfw kubernetes cluster.
         'wikikube_staging_codfw':
             frequency           => 'daily',
             hour                => '00:30:00',
@@ -48,7 +48,7 @@ class profile::docker::reporter(
             k8s_kubeconfig_path => '/etc/kubernetes/debmonitor-staging-codfw.config',
             rule_filename       => 'wikikube_kubernetes_rules.ini'
             ;
-        # Report on the staging-eqiad kubernetes cluster.
+        # Report on the ML staging kubernetes cluster.
         'ml_staging_codfw':
             frequency           => 'daily',
             hour                => '01:00:00',
@@ -75,6 +75,36 @@ class profile::docker::reporter(
             hour                => '02:30:00',
             target              => 'kubernetes',
             k8s_kubeconfig_path => '/etc/kubernetes/debmonitor-aux-k8s-codfw.config',
+            ;
+        # Report on the Wikikube eqiad kubernetes cluster.
+        'wikikube_eqiad':
+            frequency           => 'daily',
+            hour                => '03:00:00',
+            target              => 'kubernetes',
+            k8s_kubeconfig_path => '/etc/kubernetes/debmonitor-eqiad.config',
+            rule_filename       => 'wikikube_kubernetes_rules.ini'
+            ;
+        # Report on the Wikikube codfw kubernetes cluster.
+        'wikikube_codfw':
+            frequency           => 'daily',
+            hour                => '04:00:00',
+            target              => 'kubernetes',
+            k8s_kubeconfig_path => '/etc/kubernetes/debmonitor-codfw.config',
+            rule_filename       => 'wikikube_kubernetes_rules.ini'
+            ;
+        # Report on the ML serve eqiad kubernetes cluster.
+        'ml_serve_eqiad':
+            frequency           => 'daily',
+            hour                => '05:00:00',
+            target              => 'kubernetes',
+            k8s_kubeconfig_path => '/etc/kubernetes/debmonitor-ml-serve-eqiad.config',
+            ;
+        # Report on the ML serve codfw kubernetes cluster.
+        'ml_serve_codfw':
+            frequency           => 'daily',
+            hour                => '06:00:00',
+            target              => 'kubernetes',
+            k8s_kubeconfig_path => '/etc/kubernetes/debmonitor-ml-serve-codfw.config',
             ;
     }
 }
