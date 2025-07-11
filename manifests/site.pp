1ef93eeca692032f390aa9734128bf6ddae00be7
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-07-11 10:35:45 +0200

    Remove puppetserver2003
    Bug: T398607
    Change-Id: I30f6e4bc813faf5c2d6dd0bc9a86357946675b4e

--- manifests/site.pp
+++ manifests/site.pp
@@ -2348,10 +2348,6 @@ node /^puppetserver200[124]\.codfw\./ {
     role(puppetserver)
 }
 
-node /^puppetserver200[3]\.codfw\./ {
-    role(insetup::infrastructure_foundations_ferm)
-}
-
 # pybal-test2003 VM is used for pybal testing/development
 node /^pybal-test2003\.codfw\./ {
     role(pybaltest)
