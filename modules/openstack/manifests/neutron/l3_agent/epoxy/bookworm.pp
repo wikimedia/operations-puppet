9c5bb0c38dafdeff2797be1e7e63d77158cd155d
Andrew Bogott <abogott@wikimedia.org>
2025-07-13 15:30:28 -0500

    Neutron: include a python dependency for wmcs-netns-events
    Hosts: cloudnet2005-dev.codfw.wmnet, cloudnet1005.eqiad.wmnet
    Change-Id: Ib7e4de46182fea3abfe1a77878668aa3883d1e4d

--- modules/openstack/manifests/neutron/l3_agent/epoxy/bookworm.pp
+++ modules/openstack/manifests/neutron/l3_agent/epoxy/bookworm.pp
@@ -7,4 +7,9 @@ class openstack::neutron::l3_agent::epoxy::bookworm(
     package { 'neutron-l3-agent':
         ensure => 'present',
     }
+
+    # Needed by wmcs-netns-events
+    package { 'python3-pyinotify':
+        ensure => 'present',
+    }
 }
