1ded47d8e51166ba624c68fd1fd753021a3e31a2
Daniel Zahn <dzahn@wikimedia.org>
2025-07-21 12:03:30 -0700

    peopleweb: fix variable name in home dir size warning email
    Wrong variable name in the output text to show the limit in bytes.
    
    Bug: T343364
    Change-Id: I00c01f4c666330e703b39e86b85e230e6450dac9

--- modules/profile/files/microsites/peopleweb/home_size_warning.sh
+++ modules/profile/files/microsites/peopleweb/home_size_warning.sh
@@ -10,7 +10,7 @@ for userhome in /home/*; do
     home_dir_size_h=$(du -hs ${userhome} | cut -f1)
     if [ "$home_dir_size" -gt "$home_dir_limit" ]; then
       cat <<EOF | /usr/bin/mail -r "${sndr_address}" -s "peopleweb - home dir size warning - ${userhome}" -a "Auto-Submitted: auto-generated" ${rcpt_address}
-${userhome} on ${hostname} uses ${home_dir_size_h} which is larger than the configured limit of ${home_dir_limit_h}. (T343364)
+${userhome} on ${hostname} uses ${home_dir_size_h} which is larger than the configured limit of ${home_dir_limit} bytes. (T343364)
 
 (sent via $(basename $0) on $(hostname) at $(date))
 EOF
