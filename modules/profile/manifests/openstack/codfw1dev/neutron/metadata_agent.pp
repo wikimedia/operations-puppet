549a37fcddb9b4107229f8dd9cb4f166351e2aea
Andrew Bogott <abogott@wikimedia.org>
2025-07-25 09:46:12 -0500

    neutron metadata agent: restart service daily
    This service has been locking up about once per week. I'm hoping
    with daily restarts we'll avoid whatever leak or decay is causing
    the locks.
    
    Trying in codfw1dev first to confirm this is not disruptive.
    
    Hosts: cloudnet2005-dev.codfw.wmnet
    Hosts: cloudnet2006-dev.codfw.wmnet
    Hosts: cloudnet1006.eqiad.wmnet
    Bug: T395742
    Change-Id: Ia868cbbdec7f4883d2fe2e758645addfa2efa4c4

--- modules/profile/manifests/openstack/codfw1dev/neutron/metadata_agent.pp
+++ modules/profile/manifests/openstack/codfw1dev/neutron/metadata_agent.pp
@@ -15,4 +15,18 @@ class profile::openstack::codfw1dev::neutron::metadata_agent(
         report_interval              => $report_interval,
     }
     contain '::profile::openstack::base::neutron::metadata_agent'
+
+    # In Epoxy this agent is getting stuck intermittently and requiring a manual
+    #  restart. I'm pretty sure that the failure is a leak of some sort rather than
+    #  randomly timed.
+    # While we hope for a proper fix in Flamingo, we're going to just restart
+    #  the service once per day.
+    $minute = fqdn_rand(59)
+    systemd::timer::job { 'restart_metadata_agent':
+        ensure      => 'present',
+        user        => 'root',
+        description => 'Restart neutron metadata service to avoid periodic lockups',
+        command     => '/bin/systemctl restart neutron-metadata-agent.service',
+        interval    => {'start' => 'OnCalendar', 'interval' => "*-*-* 15:${minute}:00"},
+    }
 }
