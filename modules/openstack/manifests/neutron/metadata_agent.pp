acfb422f74657e7ff4282304c11f60333b584fd4
David Caro <dcaro@wikimedia.org>
2025-07-28 12:26:51 +0200

    openstack.neutron.metadata_agent: increase the number of open files
    We are seeing issues with this stopping the daemon, increasing it a
    bit to see if that helps.
    
    Bug: T395742
    Hosts: auto
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: I730c40cd7778c66c2d0ca4a386c1f84e932d0490

--- modules/openstack/manifests/neutron/metadata_agent.pp
+++ modules/openstack/manifests/neutron/metadata_agent.pp
@@ -3,6 +3,7 @@ class openstack::neutron::metadata_agent(
     Stdlib::Fqdn $keystone_api_fqdn,
     $metadata_proxy_shared_secret,
     $report_interval,
+    Integer[1] $nofile = 67107840,
     ) {
 
     class { "openstack::neutron::metadata_agent::${version}":
@@ -11,12 +12,18 @@ class openstack::neutron::metadata_agent(
         report_interval              => $report_interval,
     }
 
-    service {'neutron-metadata-agent':
-        ensure    => 'running',
-        require   => Package['neutron-metadata-agent'],
-        subscribe => [
-                      File['/etc/neutron/neutron.conf'],
-                      File['/etc/neutron/metadata_agent.ini'],
+    systemd::service { 'neutron-metadata-agent':
+        ensure             => 'present',
+        content            => "[Service]\nLimitNOFILE = ${nofile}",
+        override           => true,
+        monitoring_enabled => false,  # we have our own
+        service_params     => {
+            ensure    => 'running',
+            subscribe => [
+                File['/etc/neutron/neutron.conf'],
+                File['/etc/neutron/metadata_agent.ini'],
             ],
+            require   => Package['neutron-metadata-agent'],
+        },
     }
 }
