26e851ba59a094dd342ed7f46376eeb626c81db2
Tiziano Fogli <tfogli@wikimedia.org>
2025-07-30 11:14:40 +0200

    nrpe wrapper: enable nrpe2nodexp for check_disk_space (for testing)
    Bug: T395446
    Hosts: prometheus1005.eqiad.wmnet
    Change-Id: Id7015921576bb6f77b8f23da38cbcad1b5248d6e

--- modules/profile/manifests/monitoring.pp
+++ modules/profile/manifests/monitoring.pp
@@ -99,14 +99,15 @@ class profile::monitoring (
     }
 
     nrpe::monitor_service { 'disk_space':
-        description     => 'Disk space',
-        critical        => $nrpe_check_disk_critical,
-        nrpe_command    => "/usr/lib/nagios/plugins/check_disk ${nrpe_check_disk_options}",
-        notes_url       => 'https://wikitech.wikimedia.org/wiki/Monitoring/Disk_space',
-        dashboard_links => ["https://grafana.wikimedia.org/d/000000377/host-overview?var-server=${facts['hostname']}&var-datasource=${::site} prometheus/ops"],
-        check_interval  => 20,
-        retry_interval  => 5,
-        migration_task  => 'T332764',
+        description        => 'Disk space',
+        critical           => $nrpe_check_disk_critical,
+        nrpe_command       => "/usr/lib/nagios/plugins/check_disk ${nrpe_check_disk_options}",
+        notes_url          => 'https://wikitech.wikimedia.org/wiki/Monitoring/Disk_space',
+        dashboard_links    => ["https://grafana.wikimedia.org/d/000000377/host-overview?var-server=${facts['hostname']}&var-datasource=${::site} prometheus/ops"],
+        check_interval     => 20,
+        retry_interval     => 5,
+        migration_task     => 'T332764',
+        enable_nrpe2nodexp => true,
     }
 
     nrpe::plugin { 'check_systemd_state':
