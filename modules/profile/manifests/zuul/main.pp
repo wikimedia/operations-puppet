39b21c457216e7a256bcc18e9ebf8a2b4438d28d
Daniel Zahn <dzahn@wikimedia.org>
2025-07-31 16:56:32 -0700

    zuul: puppetize password for zuul->gerrit http connection
    For the zuul->gerrit http connection, for now use the same values
    existing in the zuul test setup.
    
    Add user name "zuul-test" in Hiera to change it easily later.
    
    Existing password was added in private puppet repository.
    
    Bug: T395938
    Change-Id: I0984b211a935627f1d8b5f41c6028f09f216d6e9

--- modules/profile/manifests/zuul/main.pp
+++ modules/profile/manifests/zuul/main.pp
@@ -2,10 +2,14 @@
 # new zuul (T393873) - main server
 class profile::zuul::main(
     Stdlib::Fqdn $mysql_host = lookup('profile::zuul::main::mysql_host'),
+    String $gerrit_user = lookup('profile::zuul::main::gerrit_user'),
 ){
 
     include ::passwords::mysql::zuul
     $mysql_pass = $::passwords::mysql::zuul::password
+
+    include ::passwords::zuul::gerrit
+    $gerrit_pass = $::passwords::zuul::gerrit::password
 
     ensure_packages(['docker.io', 'apparmor-utils'])
 
