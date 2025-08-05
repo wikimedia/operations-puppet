9a68d89805ece414f9a058808d6a1c1cc9af467e
Andrew Bogott <abogott@wikimedia.org>
2025-08-05 13:17:45 -0500

    Trivial profile for a cloud-vps-hosted chartmuseum instance
    Bug: T393782
    Change-Id: Ia471e4e6ebf9ae157f8646373a37f3a01608224d

---
+++ modules/profile/manifests/wmcs/chartmuseum.pp
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Simple profile to create a trivial chartmuseum instance on a cloud-vps instance
+#
+class profile::wmcs::chartmuseum(
+    Stdlib::Host               $listen_host      = lookup('profile::wmcs::chartmuseum::listen_host', {default_value => '0.0.0.0'}),
+    Stdlib::Port::Unprivileged $listen_port      = lookup('profile::wmcs::chartmuseum::listen_port', {default_value => 8080}),
+    Integer[0]                 $repository_depth = lookup('profile::wmcs::chartmuseum::repository_depth', {default_value => 1}),
+    Boolean                    $debug            = lookup('profile::wmcs::chartmuseum::debug', {default_value => false}),
+) {
+    class { '::chartmuseum':
+        listen_host         => $listen_host,
+        listen_port         => $listen_port,
+        repository_depth    => $repository_depth,
+        allow_anonymous_get => true,
+        debug               => $debug,
+        require             => File['/var/lib/chartmuseum'],
+    }
+
+    file { '/var/lib/chartmuseum':
+        ensure => 'link',
+        target => '/srv',
+    }
+}
