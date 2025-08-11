c1a957d0216372f71fcb7aa3abb4065946ee9e1a
Taavi Väänänen <taavi@wikimedia.org>
2025-08-11 12:52:04 +0300

    P:toolforge: aptly: Add trixie-tools/toolsbeta repos
    Bug: T401574
    Change-Id: I6a6a6964bc6b0f18c0da28bc8760e618d0fb361f

--- modules/profile/manifests/toolforge/services/aptly.pp
+++ modules/profile/manifests/toolforge/services/aptly.pp
@@ -2,24 +2,12 @@ class profile::toolforge::services::aptly () {
     # wmcs-package-build uses this to "back up" aptly contents to NFS
     ensure_packages(['rsync'])
 
-    aptly::repo { 'bookworm-tools':
-        publish => true,
-    }
-    aptly::repo { 'bullseye-tools':
-        publish      => true,
-    }
-    aptly::repo { 'buster-tools':
-        publish      => true,
-    }
-
-    # its interesting to serve some packages to toolsbeta for testing purposes
-    aptly::repo { 'bookworm-toolsbeta':
-        publish => true,
-    }
-    aptly::repo { 'bullseye-toolsbeta':
-        publish      => true,
-    }
-    aptly::repo { 'buster-toolsbeta':
-        publish      => true,
+    ['trixie', 'bookworm', 'bullseye', 'buster'].each |Debian::Codename $distro| {
+        aptly::repo { [
+            "${distro}-tools",
+            "${distro}-toolsbeta",
+        ]:
+            publish => true,
+        }
     }
 }
