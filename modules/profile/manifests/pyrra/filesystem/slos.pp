565a390ca10632e63248cb2b73a64f245e8e439d
Reuven Lazarus <rlazarus@wikimedia.org>
2025-08-13 15:40:04 -0700

    pyrra: Add Wikifunctions backend API combined latency-availability
    Bug: T394057
    Change-Id: I09cfec134557861831ebf1f827d7854421e8fdf9

--- modules/profile/manifests/pyrra/filesystem/slos.pp
+++ modules/profile/manifests/pyrra/filesystem/slos.pp
@@ -565,5 +565,42 @@ class profile::pyrra::filesystem::slos (
           })
         }
     }
+
+    # Wikifunctions SLOs
+    # Backend API combined latency-availability SLO: The percentage of all requests to the backend
+    # API that complete within the 10s threshold and receive a non-error response, defined as HTTP
+    # status code 200 or 4xx.
+    pyrra::filesystem::config { 'wikifunctions-backend-combined.yaml':
+      content => to_yaml({
+        'apiVersion' => 'pyrra.dev/v1alpha1',
+        'kind' => 'ServiceLevelObjective',
+        'metadata' => {
+            'name' => 'wikifunctions-backend-combined',
+            'namespace' => 'pyrra-o11y',
+            'labels' => {
+                'pyrra.dev/team' => 'abstract-wikipedia',
+                'pyrra.dev/service' => 'wikifunctions',
+            },
+        },
+        'spec' => {
+            'alerting'  => {
+                'burnrates' => false
+            },
+            'target' => '98.5',
+            'window' => '12w',
+            'indicator' => {
+                'latency' => {
+                    'success' => {
+                        'metric' => 'mediawiki_WikiLambda_mw_to_orchestrator_api_call_seconds_bucket{status=~"200|4..", le="10"}',
+                    },
+                    'total' => {
+                        'metric' => 'mediawiki_WikiLambda_mw_to_orchestrator_api_call_seconds_count',
+                    },
+                },
+            },
+        },
+      })
+    }
+
     #lint:endignore
 }
