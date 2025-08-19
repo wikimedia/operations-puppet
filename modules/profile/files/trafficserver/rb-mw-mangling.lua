a633225d3e6952f5151a52888db73a94d89b6d2a
Timo Tijhof <krinkle@fastmail.com>
2025-08-19 17:08:32 +0100

    varnish: Merge m-dot and X-Subdomain block in cluster_fe_recv_pre_purge
    Make it clear that these are not separate conditions with 2x2 possible
    execution paths. They are identical, given the `unset` right above.
    
    Document the other side of this in the ATS lua plugin while at it.
    
    Bug: T401595
    Change-Id: I28fe46170b39e3d8a5dbf234f65357cdf8ce1653

--- modules/profile/files/trafficserver/rb-mw-mangling.lua
+++ modules/profile/files/trafficserver/rb-mw-mangling.lua
@@ -6,7 +6,17 @@ jit.off(true, true)
 function remap_hook()
     local orig_uri = ts.client_request.get_uri()
 
-    -- RestBASE mangling
+    -- RESTBase mangling
+    --
+    -- 1. In Varnish (cluster_fe_recv_pre_purge) we replace mobile hostnames
+    --    with canonical hostnames on requests to /api/rest_v1/.
+    --    That improves caching as RESTBase responses don't vary by m-dot.
+    -- 2. In profile::trafficserver::backend::mapping_rules
+    --    we first apply a regex_map for `http://(.*)/api/rest_v1`
+    --    to direct these at the RESTBase service. That leaves the
+    --    Host header and URL path unchanged, thus briefly forming
+    --    an invalid RESTBase request with /api/rest_v1.
+    -- 3. [This code] We finish mangling the URL to `/:domain/v1/`
     if string.match(orig_uri, "^/api/rest_v1/") then
         local host = ts.client_request.header['Host']
         new_path = "/" .. host .. string.gsub(orig_uri, "^/api/rest_v1/", "/v1/")
@@ -14,7 +24,13 @@ function remap_hook()
         return
     end
 
-    -- MediaWiki mangling
+    -- Rewrite MediaWiki requests from mobile m-dot domains to canonical
+    --
+    -- 1. In Varnish (cluster_fe_recv_pre_purge) we compute the canonical hostname
+    --    on m-dot requests in the x-dt-host field. We do not replace it there,
+    --    because responses/caches must vary.
+    -- 2. [This code] Replaces the Host header, because MediaWiki requires canonical
+    --    hostnames, and the X-Subdomain header activates MobileFrontend.
     if ts.client_request.header['X-Subdomain'] then
         ts.client_request.header['Host'] = ts.client_request.header['x-dt-host']
         return
