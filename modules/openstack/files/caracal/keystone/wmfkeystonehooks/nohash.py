319baf6c391e0d2b9981b7b56fe8571d57a14af3
Andrew Bogott <abogott@wikimedia.org>
2025-08-25 16:39:12 -0500

    Remove manifests/files/templaces for openstack 'Caracal'
    We're running Epoxy so C is two versions behind.
    
    Hosts: cloudcontrol1011.eqiad.wmnet
    Bug: T390914
    Change-Id: I90511c01b7f6240c03140bde7e62638f1496d6e2

--- modules/openstack/files/caracal/keystone/wmfkeystonehooks/nohash.py
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-from keystone.identity import generator
-
-
-class Generator(generator.IDGenerator):
-
-    def generate_public_ID(self, mapping):
-        return mapping['local_id']
