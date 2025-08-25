effeecb34a3e1042c63605fa70d0eef6b271c53d
STran <stran@wikimedia.org>
2025-08-25 05:23:45 -0700

    mediawiki: Run CheckUser/revokeTemporaryAccountViewerGroup.php
    Why:
    Per WMF policy, we need to revoke access to temporary account IPs
    if a user has been inactive for over a year. This membership is
    revoked locally and runs on all wikis.
    
    What:
    - Set a cronjob that will run revokeTemporaryAccountViewerGroup.php
      daily on all wikis
    
    Bug: T375115
    Hosts: deploy1003.eqiad.wmnet
    Hosts: deployment-mwmaint03.deployment-prep.eqiad1.wikimedia.cloud
    Change-Id: I48c071a3c829794c303bb718631104d85548443d

--- modules/profile/manifests/mediawiki/maintenance/temporary_accounts.pp
+++ modules/profile/manifests/mediawiki/maintenance/temporary_accounts.pp
@@ -3,6 +3,8 @@
 class profile::mediawiki::maintenance::temporary_accounts(
     Stdlib::Unixpath $helmfile_defaults_dir = lookup('profile::kubernetes::deployment_server::global_config::general_dir', {default_value => '/etc/helmfile-defaults'}),
 ) {
+    # team label for alerting
+    $team_label = 'trust-and-safety-product'
 
     profile::mediawiki::periodic_job { 'purge_temporary_accounts':
         command               => '/usr/local/bin/foreachwikiindblist "all - closed - private - fishbowl" extensions/CentralAuth/maintenance/expireTemporaryAccounts.php --verbose --frequency 1',
@@ -13,5 +15,19 @@ class profile::mediawiki::maintenance::temporary_accounts(
         description           => 'Expire temporary accounts registered before the configured expiry window (dblists: all - closed - private - fishbowl)',
         script_label          => 'CentralAuth-expireTemporaryAccounts.php',
         helmfile_defaults_dir => $helmfile_defaults_dir,
+    }
+
+    # CheckUser is not enabled on the beta cluster
+    if $::realm != 'labs' {
+      profile::mediawiki::periodic_job { 'checkuser_revoke_temporaryaccountviewer':
+          command               => '/usr/local/bin/foreachwiki extensions/CheckUser/maintenance/revokeTemporaryAccountViewerGroup.php --expiry 365',
+          interval              => '*-*-* 00:00:00',
+          cron_schedule         => '00 00 * * *',
+          team                  => $team_label,
+          kubernetes            => true,
+          description           => 'Revoke temporary-account-viewer group membership from users who are considered inactive. See T375115.',
+          script_label          => 'CheckUser-revokeTemporaryAccountViewerGroup.php',
+          helmfile_defaults_dir => $helmfile_defaults_dir,
+      }
     }
 }
