c58521046844e436f820dfd53796a58932263b9c
Cole White <cwhite@wikimedia.org>
2025-08-25 21:43:21 +0000

    opensearch: selectively enable cluster health check
    Why: to selectively disable cluster health checking via nrpe
    
    What: create a toggle around the general opensearch cluster health
    check and disable for the logging cluster
    
    Bug: T321808
    Change-Id: I553927c16932cd3f4b1dee6101713bc6722df917

--- modules/profile/manifests/opensearch/monitoring/base_checks.pp
+++ modules/profile/manifests/opensearch/monitoring/base_checks.pp
@@ -4,6 +4,7 @@ class profile::opensearch::monitoring::base_checks(
     Integer $shard_size_critical     = lookup('profile::opensearch::monitoring::shard_size_critical',     { 'default_value' => 140 }),
     String  $threshold               = lookup('profile::opensearch::monitoring::threshold',               { 'default_value' => '>=0.15' }),
     Integer $timeout                 = lookup('profile::opensearch::monitoring::timeout',                 { 'default_value' => 4 }),
+    Boolean $enable_health_check     = lookup('profile::opensearch::monitoring::enable_health_check',     { 'default_value' => true }),
     Boolean $enable_shard_size_check = lookup('profile::opensearch::monitoring::enable_shard_size_check', { 'default_value' => true }),
     Boolean $enable_unassigned_shard_check = lookup('profile::opensearch::monitoring::enable_unassigned_shard_check', { 'default_value' => true }),
 ) {
@@ -16,39 +17,41 @@ class profile::opensearch::monitoring::base_checks(
     }
 
     $configured_ports.each |$port| {
-        nrpe::monitor_service { "opensearch_shards_${port}":
-          critical       => false,
-          contact_group  => 'admins,team-discovery',
-          notes_url      => 'https://wikitech.wikimedia.org/wiki/Search#Administration',
-          nrpe_command   => "/usr/lib/nagios/plugins/check_elasticsearch.py --ignore-status --url http://localhost:${port} --shards-inactive '${threshold}' --timeout ${timeout}",
-          description    => "OpenSearch health check for shards on ${port}",
-          migration_task => 'T371083',
+        if $enable_health_check {
+            nrpe::monitor_service { "opensearch_shards_${port}":
+                critical       => false,
+                contact_group  => 'admins,team-discovery',
+                notes_url      => 'https://wikitech.wikimedia.org/wiki/Search#Administration',
+                nrpe_command   => "/usr/lib/nagios/plugins/check_elasticsearch.py --ignore-status --url http://localhost:${port} --shards-inactive '${threshold}' --timeout ${timeout}",
+                description    => "OpenSearch health check for shards on ${port}",
+                migration_task => 'T371083',
+            }
         }
 
         if $enable_unassigned_shard_check {
             nrpe::monitor_service { "opensearch_unassigned_shard_check_${port}":
-              critical       => false,
-              contact_group  => 'admins,team-discovery',
-              notes_url      => 'https://wikitech.wikimedia.org/wiki/Search#Administration',
-              nrpe_command   => "/usr/lib/nagios/plugins/check_elasticsearch_unassigned_shards.py --url http://localhost:${port} --timeout ${timeout}",
-              description    => "OpenSearch unassigned shard check - ${port}",
-              check_interval => 720, # 12h
-              retry_interval => 120, # 2h
-              retries        => 1,
-              migration_task => 'T371083',
+                critical       => false,
+                contact_group  => 'admins,team-discovery',
+                notes_url      => 'https://wikitech.wikimedia.org/wiki/Search#Administration',
+                nrpe_command   => "/usr/lib/nagios/plugins/check_elasticsearch_unassigned_shards.py --url http://localhost:${port} --timeout ${timeout}",
+                description    => "OpenSearch unassigned shard check - ${port}",
+                check_interval => 720, # 12h
+                retry_interval => 120, # 2h
+                retries        => 1,
+                migration_task => 'T371083',
             }
         }
 
         if $enable_shard_size_check {
             nrpe::monitor_service { "opensearch_shard_size_check_${port}":
-              critical       => false,
-              contact_group  => 'admins,team-discovery',
-              notes_url      => 'https://wikitech.wikimedia.org/wiki/Search#If_it_has_been_indexed',
-              nrpe_command   => "/usr/lib/nagios/plugins/check_elasticsearch_shard_size.py --url http://localhost:${port} --shard-size-warning ${shard_size_warning} --shard-size-critical ${shard_size_critical} --timeout ${timeout}",
-              description    => "OpenSearch shard size check - ${port}",
-              check_interval => 1440, # 24h
-              retry_interval => 180, # 3h
-              migration_task => 'T371083',
+                critical       => false,
+                contact_group  => 'admins,team-discovery',
+                notes_url      => 'https://wikitech.wikimedia.org/wiki/Search#If_it_has_been_indexed',
+                nrpe_command   => "/usr/lib/nagios/plugins/check_elasticsearch_shard_size.py --url http://localhost:${port} --shard-size-warning ${shard_size_warning} --shard-size-critical ${shard_size_critical} --timeout ${timeout}",
+                description    => "OpenSearch shard size check - ${port}",
+                check_interval => 1440, # 24h
+                retry_interval => 180, # 3h
+                migration_task => 'T371083',
             }
         }
     }
