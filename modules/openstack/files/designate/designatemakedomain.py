d1296f0dd97a7b1ed223d745366e268f578119a2
Andrew Bogott <abogott@wikimedia.org>
2025-08-26 12:09:28 -0500

    designatemakedomain.py: format with Black
    Change-Id: Iad19bae5225aa1dd203c472d89f306b72f91569a

--- modules/openstack/files/designate/designatemakedomain.py
+++ modules/openstack/files/designate/designatemakedomain.py
@@ -17,17 +17,20 @@ from keystoneauth1 import session as keystone_session
 from designateclient.v2 import client
 from oslo_log import log as logging
 
-LOG = logging.getLogger('keystone.%s' % __name__)
+LOG = logging.getLogger("keystone.%s" % __name__)
 
 
-def deleteDomain(url, user, password, project, domain="", region='eqiad1-r', delete_all=False):
+def deleteDomain(
+    url, user, password, project, domain="", region="eqiad1-r", delete_all=False
+):
     auth = v3.Password(
         auth_url=url,
         username=user,
         password=password,
-        user_domain_name='Default',
-        project_domain_name='Default',
-        project_id=project)
+        user_domain_name="Default",
+        project_domain_name="Default",
+        project_id=project,
+    )
 
     targetSession = keystone_session.Session(auth=auth)
     targetClient = client.Client(session=targetSession, region_name=region)
@@ -35,25 +38,35 @@ def deleteDomain(url, user, password, project, domain="", region='eqiad1-r', del
     domains = targetClient.zones.list()
     for thisdomain in domains:
         if delete_all:
-            LOG.info("Deleting %s" % thisdomain['name'])
-            targetClient.zones.delete(thisdomain['id'])
+            LOG.info("Deleting %s" % thisdomain["name"])
+            targetClient.zones.delete(thisdomain["id"])
         else:
-            if thisdomain['name'] == domain:
-                targetClient.zones.delete(thisdomain['id'])
+            if thisdomain["name"] == domain:
+                targetClient.zones.delete(thisdomain["id"])
                 return
     if not delete_all:
         LOG.warning("Domain %s not found" % domain)
 
 
-def createDomain(url, user, password, project, domain, orig_project, region='eqiad1-r',
-                 ttl=120, wait_for_active=True):
+def createDomain(
+    url,
+    user,
+    password,
+    project,
+    domain,
+    orig_project,
+    region="eqiad1-r",
+    ttl=120,
+    wait_for_active=True,
+):
     auth = v3.Password(
         auth_url=url,
         username=user,
         password=password,
-        user_domain_name='Default',
-        project_domain_name='Default',
-        project_id=orig_project)
+        user_domain_name="Default",
+        project_domain_name="Default",
+        project_id=orig_project,
+    )
 
     createSession = keystone_session.Session(auth=auth)
     createClient = client.Client(session=createSession, region_name=region)
@@ -62,9 +75,10 @@ def createDomain(url, user, password, project, domain, orig_project, region='eqi
         auth_url=url,
         username=user,
         password=password,
-        user_domain_name='Default',
-        project_domain_name='Default',
-        project_id=project)
+        user_domain_name="Default",
+        project_domain_name="Default",
+        project_id=project,
+    )
 
     targetSession = keystone_session.Session(auth=auth)
 
@@ -77,16 +91,16 @@ def createDomain(url, user, password, project, domain, orig_project, region='eqi
     #  is needed since wmflabs.org lives in that project and
     #  designate prevents subdomain creation elsewhere.
     LOG.info("Creating %s" % domain)
-    zone = createClient.zones.create(domain, email='root@wmcloud.org', ttl=ttl)
-    status = 'PENDING'
+    zone = createClient.zones.create(domain, email="root@wmcloud.org", ttl=ttl)
+    status = "PENDING"
     # Wait for the domain to actually exist before we transfer it
-    while status == 'PENDING' and wait_for_active:
+    while status == "PENDING" and wait_for_active:
         zone = createClient.zones.get(domain)
-        status = zone['status']
+        status = zone["status"]
         time.sleep(2)
 
     transferRequest = createClient.zone_transfers.create_request(domain, project)
-    transferId = transferRequest['id']
-    transferKey = transferRequest['key']
+    transferId = transferRequest["id"]
+    transferKey = transferRequest["key"]
 
     targetClient.zone_transfers.accept_request(transferId, transferKey)
