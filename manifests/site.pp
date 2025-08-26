42f293436eefdfe0e01d6a43a1c66fc0213cc89e
Andrew Bogott <abogott@wikimedia.org>
2025-08-26 10:35:52 -0500

    Make cloudcephosd1052 an osd node
    This has a different network card from 1048-1051, so different
    network names.
    
    Bug: T395910
    Change-Id: Idae0d62e71fddeedbe8cf955bb098a4f59d802a6

--- manifests/site.pp
+++ manifests/site.pp
@@ -1726,12 +1726,8 @@ node /^cloudcephmon100[4-6]\.eqiad\./ {
 }
 
 # cloudceph storage nodes
-node /^cloudcephosd10(0[4-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-1])\.eqiad\./ {
+node /^cloudcephosd10(0[4-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-2])\.eqiad\./ {
     role(wmcs::ceph::osd)
-}
-
-node /^cloudcephosd1052\.eqiad\./ {
-    role(insetup::wmcs_ferm)
 }
 
 # migrate cloudelastic to opensearch, see T387904
