a2caf930424bbeae540bd4e695750efb3cb4daa1
Sukhbir Singh <ssingh@wikimedia.org>
2025-08-27 12:05:33 -0400

    dumps: remove reference to DVD page
    Puppet is currently failing as dvd.html was removed in If29450199 but
    not the reference to the file from Puppet itself.
    
    Hosts: O:dumps::distribution::server
    Bug: T402976
    Change-Id: Ia49fb801b2c5932b479fd44b3ea59a2fc77220da

--- modules/dumps/manifests/web/html.pp
+++ modules/dumps/manifests/web/html.pp
@@ -184,12 +184,4 @@ class dumps::web::html(
         source => 'puppet:///modules/dumps/web/html/legal.html',
     }
 
-    file { "${xmldumpsdir}/dvd.html":
-        ensure => 'present',
-        path   => "${xmldumpsdir}/dvd.html",
-        mode   => '0644',
-        owner  => 'root',
-        group  => 'root',
-        source => 'puppet:///modules/dumps/web/html/dvd.html',
-    }
 }
