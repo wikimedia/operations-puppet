6f691134a3c3c758816f57a661d1f3fe26717fd7
Tiziano Fogli <tfogli@wikimedia.org>
2025-08-27 11:13:41 +0000

    Revert "nrpewrapper: correlate Prometheus "for:" duration with Icinga timing"
    This reverts commit 1591ac7648d9ee423554dcfcc1ae80389af7d255.
    
    Reason for revert: max not supported yet in puppet 5
    
    Change-Id: I6315216b5a9c39b06ba4fb3626891e664ce7f178

--- modules/nrpe/manifests/monitor_service.pp
+++ modules/nrpe/manifests/monitor_service.pp
@@ -114,9 +114,7 @@ define nrpe::monitor_service(
         false => 'absent'
     }
 
-    # This is the amount of time after which an error condition
-    # is definitely notified by the Icinga check.
-    $for = max([$check_interval + (($retries - 1) * $retry_interval), 1])
+    $for = $check_interval * $retry_interval
     # Do not take into account the dashboard_link query string in the hash computation.
     # Furthermore, dashboard link query string parameters are hardcoded for Icinga checks,
     # which are one per host, whereas here we use a single rule for checks across multiple hosts.
@@ -156,9 +154,6 @@ define nrpe::monitor_service(
         false => $command_flag_perfdata
     }
 
-    # This is the timer period needed to ensure that
-    # the requested retries are computed within the for: time frame.
-    $timer_interval = max([floor($for / $retries), 1])
     # user nagios needed for privilege escalation
     # group prometheus-node-exporter needed to store the result in /var/lib/prometheus/node.d
     systemd::timer::job { "nrpe2nodexp-${title}":
@@ -168,7 +163,7 @@ define nrpe::monitor_service(
         group             => 'prometheus-node-exporter',
         ignore_errors     => true,
         command           => $command,
-        interval          => [ { 'start' => 'OnUnitInactiveSec', 'interval' => "${timer_interval}min" }, ],
+        interval          => [ { 'start' => 'OnUnitInactiveSec', 'interval' => "${check_interval}min" }, ],
         logging_enabled   => false, #custom rule is configured through a dedicated resource
         syslog_identifier => "nrpe2nodexp-${title}", # Each instance must have a unique value to avoid resource duplication
     }
