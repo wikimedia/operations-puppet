01c1b5ccd34e98fd4bb0ffa998c86c2b543b5f25
Brian King <bking@wikimedia.org>
2025-08-29 14:51:20 +0000

    refinery: parameterize systemd timer job
    The refinery job 'refinery-drop-webrequest-sampled-live-druid'
    is currently hard-coded to an-druid1001, which was decom'd
    in T401116.
    
    Introduce a new hiera value,
    profile::analytics::refinery::job::data_purge::analytics_druid_host
    
    and use its value in the job arguments.
    
    Hosts: an-launcher1002.eqiad.wmnet
    Bug: T401116
    Bug: T402943
    Change-Id: I7ada0b1b917d487cfbaf4506d9c674b719336573

--- modules/profile/manifests/analytics/refinery/job/data_purge.pp
+++ modules/profile/manifests/analytics/refinery/job/data_purge.pp
@@ -9,6 +9,7 @@
 #   Default: true
 #
 class profile::analytics::refinery::job::data_purge (
+    Optional[String] $analytics_druid_host = lookup('profile::analytics::refinery::job::data_purge::analytics_druid_host', { 'default_value' => undef }),
     Optional[String] $public_druid_host = lookup('profile::analytics::refinery::job::data_purge::public_druid_host', { 'default_value' => undef }),
     Wmflib::Ensure $ensure_timers       = lookup('profile::analytics::refinery::job::data_purge::ensure_timers', { 'default_value' => 'present' }),
     Boolean $use_kerberos_keytab        = lookup('profile::analytics::refinery::job::data_purge::use_kerberos_keytab', { 'default_value' => true }),
@@ -155,7 +156,7 @@ class profile::analytics::refinery::job::data_purge (
     kerberos::systemd_timer { 'refinery-drop-webrequest-sampled-live-druid':
         ensure      => $ensure_timers,
         description => 'Drop Druid Webrequest sampled live data from deep storage following data retention policies.',
-        command     => "${refinery_path}/bin/refinery-drop-druid-deep-storage-data -d ${druid_webrequest_sampled_retention_days} webrequest_sampled_live",
+        command     => "${refinery_path}/bin/refinery-drop-druid-deep-storage-data -d ${druid_webrequest_sampled_retention_days} -t ${$analytics_druid_host} webrequest_sampled_live",
         interval    => '*-*-* 05:30:00',
         environment => $systemd_env,
         user        => 'analytics',
