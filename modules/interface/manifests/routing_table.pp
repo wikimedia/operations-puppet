cd937d302a4700f96c52a07b969aaf72e9117822
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2025-09-02 16:47:46 +0200

    interface: create rt_tables.d as needed
    Bug: T401899
    Change-Id: I0bad65fbda1f4dbb9f389b84ca54fa14cfc856d0

--- modules/interface/manifests/routing_table.pp
+++ modules/interface/manifests/routing_table.pp
@@ -6,6 +6,10 @@ define interface::routing_table (
     Integer[1, 252] $number,
     Wmflib::Ensure  $ensure = 'present',
 ) {
+
+    # This directory is no longer shipped by iproute2 on >= Trixie
+    wmflib::dir::mkdir_p('/etc/iproute2/rt_tables.d')
+
     file { "/etc/iproute2/rt_tables.d/${title}.conf":
         ensure  => stdlib::ensure($ensure, 'file'),
         content => "${number} ${title}\n",
