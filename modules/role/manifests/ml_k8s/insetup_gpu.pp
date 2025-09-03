e6159e81d1816510cf8f871d561c0680f249a178
Elukey <ltoscano@wikimedia.org>
2025-09-03 14:02:11 +0000

    Revert "Add a new insetup role for ml-k8s hosts to test their GPU"
    This reverts commit 1696de28a4a277c1d236ff145a9d030b685cb669.
    
    Reason for revert: Not really needed
    
    Change-Id: I3685de1f440aca3b90419ae84129085951624cba

--- modules/role/manifests/ml_k8s/insetup_gpu.pp
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-class role::ml_k8s::insetup_gpu {
-    include profile::base::production
-    include profile::firewall
-
-    # Support for AMD GPUs
-    include profile::amd_gpu
-}
