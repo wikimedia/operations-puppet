2c60b53d88434f815206b800897e42aeb81cbbd7
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-09-05 16:34:06 +0200

    Add ncredir3006
    ncredir has been installed in the routed Ganeti cluster (esams03) and will
    eventually replace ncredir3004 (running on the esams02/classic cluster)
    
    Bug: T402259
    Change-Id: I193c166719cd15df60702bfb03823781df20b95f

--- manifests/site.pp
+++ manifests/site.pp
@@ -2166,10 +2166,6 @@ node /^ncmonitor[1-9][0-9]{3}\./ {
     role(ncmonitor)
 }
 
-node /^ncredir3006\.esams\./ {
-    role(insetup::infrastructure_foundations_ferm)
-}
-
 node /^ncredir[1-9][0-9]{3}\./ {
     role(ncredir)
 }
