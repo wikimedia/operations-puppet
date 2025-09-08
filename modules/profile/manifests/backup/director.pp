1ad811f0fa6af37da6d201c3fbf648448e02b512
Jaime Crespo <jcrespo@wikimedia.org>
2025-09-08 10:59:44 +0200

    backup: Increase main storage size from 100 to 120 TB
    Bacula has ran out of size for its main storage location, making
    backups getting stuck. Increase the maximum amount of volumes
    (after resizing online its available filesystem) from 200 to 240,
    which for half a TB volumes, that means a 20 TB size increase.
    
    To prevent this in the future, we should speed up the split of
    gerrit and gitlab backups to a separate host, like we did with
    databases.
    
    Change-Id: I7478da8ec5e1c07b16577d7a722cb7d9532c6521

--- modules/profile/manifests/backup/director.pp
+++ modules/profile/manifests/backup/director.pp
@@ -28,7 +28,7 @@ class profile::backup::director(
 
     # Default pool for "normal" backups (not archivals or database-related)
     bacula::director::pool { $pool:
-        max_vols         => 200,
+        max_vols         => 240,
         storage          => "${onsite_sd}-${file_storage_production}",
         volume_retention => '90 days',
         label_fmt        => $pool,
