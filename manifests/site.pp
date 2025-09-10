de70fa389c108f1fb3149e3e5c80425fff90944a
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-09-10 10:40:36 +0200

    Setup maps2011 as master node for new maps/codfw servers
    Bug: T381565
    Hosts: maps2011.codfw.wmnet
    Change-Id: I5b8b8921724b66c9e892159661985a20da63afed

--- manifests/site.pp
+++ manifests/site.pp
@@ -1901,8 +1901,12 @@ node /^maps20(0[5-8]|10)\.codfw\./ {
     role(maps::replica)
 }
 
-node /^maps201[1-4]\.codfw\./ {
+node /^maps201[2-4]\.codfw\./ {
     role(insetup::infrastructure_foundations_nftables)
+}
+
+node /^maps2011\.codfw\./ {
+    role(maps::master_bookworm)
 }
 
 node /^maps-test200[2-6]\.codfw\./ {
