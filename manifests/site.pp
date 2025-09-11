549282322756eb7662c53bc76098a9ef7ea4e4c0
Btullis <btullis@wikimedia.org>
2025-09-11 16:37:37 +0000

    Revert "Add four new (renamed) an-worker nodes to the Hadoop cluster"
    This reverts commit 06a2895e463493f85e569cb7e12a56bba975fb32.
    
    Reason for revert: Need to reinstall in the insetup role
    
    Change-Id: I2bc451fe9c5256d240839483a613f51d7be97268

--- manifests/site.pp
+++ manifests/site.pp
@@ -125,12 +125,12 @@ node /^an-worker1(10[0-9]|11[0-6])\.eqiad\./ {
 # hieradata/common.yaml hadoop_clusters net_topology
 # to make sure the hostname -> /datacenter/rack/row id is correct.
 # This is used for Hadoop network topology awareness.
-node /^an-worker1(11[7-9]|12[0-9]|13[0-9]|14[0-9]|15[0-9]|16[0-9]|17[0-9]|18[0-9]|19[0-9]|20[0-8]|23[3-6])\.eqiad\./ {
+node /^an-worker1(11[7-9]|12[0-9]|13[0-9]|14[0-9]|15[0-9]|16[0-9]|17[0-9]|18[0-9]|19[0-9]|20[0-8])\.eqiad\./ {
     role(analytics_cluster::hadoop::worker)
 }
 
-# New an-worker hosts - See #T399964
-node /^an-worker1(209|21[0-9]|22[0-9]|23[0-2])\.eqiad\./ {
+# New an-worker hosts - See #T399964 and #T398438
+node /^an-worker1(209|21[0-9]|22[0-9]|23[0-6])\.eqiad\./ {
     role(insetup::data_platform_ferm)
 }
 
