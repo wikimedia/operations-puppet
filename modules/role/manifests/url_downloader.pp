32125585fe2ba1006195d75c69ccdf143de04df7
Sukhbir Singh <ssingh@wikimedia.org>
2025-09-11 11:28:22 -0400

    hiera: O:url_downloader: set LVS realserver pools
    Bug: T403416
    Change-Id: I5e6f9f2bb5427dd48318aaf99e0ce51bfbffa7d6

--- modules/role/manifests/url_downloader.pp
+++ modules/role/manifests/url_downloader.pp
@@ -3,7 +3,9 @@ class role::url_downloader {
     include profile::firewall
     include profile::url_downloader
 
-    # Prototype hCaptcha IP blinding proxy is living on url-downloader machines for simplicity.
+    # Prototype hCaptcha IP blinding proxy is living on url-downloader machines
+    # for simplicity. This is behind LVS.
+    include profile::lvs::realserver
     include profile::nginx
     include profile::pki::client
     include profile::hcaptcha::proxy
