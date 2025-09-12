wikimedia/operations-puppet

commit 400aa1e20bc8f5a5bb17465278b08374c3ab2cd7

trafficserver: Do not leak alt-svc headers from applayer
Prevent alt-svc headers sent by any origin server to hit the users

Change-Id: I160ee6d6daa4cb05bd1cd6157fb6bae925fbabc0

modules/profile/files/trafficserver/default.lua
@@ -172,6 +172,7 @@ function do_global_read_response()
     -- through our CDN blindly.
     ts.server_response.header['Public-Key-Pins'] = nil
     ts.server_response.header['Public-Key-Pins-Report-Only'] = nil
+    ts.server_response['Alt-Svc'] = nil
 
     local response_status = ts.server_response.get_status()
     if response_status == 301 or response_status == 302 then

commit b447b461884fff76e9412666a19b73f72811295a

trafficserver: Fix syntax error on default.lua
400aa1e20bc8f5a5bb17465278b08374c3ab2cd7 introduced a syntax error

Change-Id: Ibc15d2d35710d1264311d489a303a3189ee46712

modules/profile/files/trafficserver/default.lua
@@ -172,7 +172,7 @@ function do_global_read_response()
     -- through our CDN blindly.
     ts.server_response.header['Public-Key-Pins'] = nil
     ts.server_response.header['Public-Key-Pins-Report-Only'] = nil
-    ts.server_response['Alt-Svc'] = nil
+    ts.server_response.header['Alt-Svc'] = nil
 
     local response_status = ts.server_response.get_status()
     if response_status == 301 or response_status == 302 then