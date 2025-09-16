b549d44a6b0a2c01cd56a66f67bf982230a5bccb
Taavi Väänänen <taavi@wikimedia.org>
2025-09-16 17:50:37 +0300

    P:toolforge: Delete cmd_checklist test suite
    This is not actively in use, as the profile only being included on the
    Buster bastion profile shows, and the toolforge-deploy test suite has
    roughly equivalent functionality.
    
    Change-Id: I6a6a6964a7df28b49ea07b4f8c174eaa3032ce7c

--- modules/toolforge/manifests/automated_toolforge_tests.pp
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-class toolforge::automated_toolforge_tests (
-    Hash $envvars,
-) {
-    file { '/etc/toolforge/automated-toolforge-tests.yaml':
-        ensure  => present,
-        content => template('toolforge/automated-toolforge-tests.yaml.erb'),
-    }
-}
