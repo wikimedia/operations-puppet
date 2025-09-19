4cb5bf502dae2305a3a6f50ba18d16bbb8da89a2
Taavi Väänänen <taavi@wikimedia.org>
2025-09-19 14:22:42 +0300

    P:toolforge::k8s::haproxy: Drop old TCP listener
    Bug: T283948
    Change-Id: I6a6a69648e68e16a343aceab8c6da9ec9520b124

--- modules/profile/manifests/toolforge/k8s/haproxy.pp
+++ modules/profile/manifests/toolforge/k8s/haproxy.pp
@@ -1,6 +1,5 @@
 class profile::toolforge::k8s::haproxy (
     Array[Stdlib::Fqdn] $ingress_nodes        = lookup('profile::toolforge::k8s::ingress_nodes',                {default_value => ['localhost']}),
-    Stdlib::Port        $ingress_port         = lookup('profile::toolforge::k8s::ingress_port',                 {default_value => 30000}),
     Stdlib::Port        $ingress_backend_port = lookup('profile::toolforge::k8s::ingress_backend_port',         {default_value => 30002}),
     Array[Stdlib::Fqdn] $control_nodes        = lookup('profile::toolforge::k8s::control_nodes',                {default_value => ['localhost']}),
     Stdlib::Port        $api_port             = lookup('profile::toolforge::k8s::apiserver_port',               {default_value => 6443}),
