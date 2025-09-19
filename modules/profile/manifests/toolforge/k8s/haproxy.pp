7a7bb6224e8b9276a6b5f51cc6697909cc37b01e
Taavi Väänänen <taavi@wikimedia.org>
2025-09-19 14:29:04 +0300

    P:toolforge::k8s::haproxy: Handle API gateway external access
    Bug: T283948
    Change-Id: I6a6a696448e4e2c8a1649779fc41433f681f23a7

--- modules/profile/manifests/toolforge/k8s/haproxy.pp
+++ modules/profile/manifests/toolforge/k8s/haproxy.pp
@@ -72,10 +72,6 @@ class profile::toolforge::k8s::haproxy (
             status_matches     => [404];
 
         "api.svc.${web_domain}":
-            port               => 443,
-            instance_label     => "api.svc.${web_domain}",
-            ip4                => ipresolve("api.svc.${web_domain}", 4),
-            force_tls          => true,
             path               => '/healthz',
             body_regex_matches => ['ok'],
             # making it explicit
