6a25ee22aa143f6abd89e445423a4cd07c8f361c
Ryan Kemper <rkemper@wikimedia.org>
2025-09-21 18:17:20 +0000

    Revert "wdqs: remove ipip"
    This reverts commit 4d0c1b2f997d33057f07e1dec30a31b6e397be8b.
    
    Reason for revert: may be breaking https://query-legacy-full.wikidata.org/
    
    Change-Id: I861c6a54bc10a6c9051b5c6866d8162e29ad77bb

--- modules/role/manifests/wdqs/public.pp
+++ modules/role/manifests/wdqs/public.pp
@@ -11,6 +11,7 @@ class role::wdqs::public {
     require profile::query_service::categories
     require profile::query_service::monitor::wikidata_public
     # Production specific profiles
+    include profile::lvs::realserver::ipip
     # Public endpoint specific profiles
     include profile::tlsproxy::envoy # TLS termination
 }
