31e83ec8ddcb1d9434b9b51f5fb74dea580aab10
stevemunene <smunene@wikimedia.org>
2025-09-29 17:23:55 +0300

    remove mention of druid10[07-08] in puppet
    We are about to decommission 10[07-08], with new hosts an-druid101[2-3] already in place
    and serving the cluster. This removes the two hosts from the role as we handover to DC-OPS.
    
    Bug: T403801
    Change-Id: I3bca4291156dec81bb03d37eb66d1a9a5aa3cab4

--- manifests/site.pp
+++ manifests/site.pp
@@ -949,7 +949,7 @@ node /^an-test-druid1001\.eqiad\./ {
 # Druid public-eqiad servers.
 # These power AQS and wikistats 2.0 and contain non sensitive datasets.
 # https://wikitech.wikimedia.org/wiki/Analytics/Data_Lake#Druid
-node /^druid10(0[7-9]|1[0-3])\.eqiad\./ {
+node /^druid10(09|1[0-3])\.eqiad\./ {
     role(druid::public::worker)
 }
 
