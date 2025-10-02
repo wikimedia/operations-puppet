4f05668147290f5f77e42262ff3fef8a9f880631
Luca Toscano <ltoscano@wikimedia.org>
2025-10-02 09:28:47 +0200

    cpufrequtils: add support for Trixie
    From Debian Trixie the cpufrequtils package is not available
    anymore, replaced by linux-cpupower (that offers drop-in
    replacements for all the utils).
    
    Keep the class name to avoid having conditionals throught
    the profiles etc.., but vary the package installed and its
    configs based on the OS.
    
    Bug: T405891
    Change-Id: I25be35c219b208f6f29ab9f2930b4428b56974b9

--- modules/cpufrequtils/manifests/init.pp
+++ modules/cpufrequtils/manifests/init.pp
@@ -2,6 +2,8 @@
 # @summary
 #   This class installs the cpufrequtils package and ensures a configured
 #   CPU frequency governor is set.
+#   From Trixie onward Debian ships the linux-cpupower package, but
+#   we keep the class name for convenience.
 # @param governor
 #   Which governor to use. Defaults to 'performance'. Run 'cpufreq-info -g'
 #   to obtain a list of available governors.
@@ -12,29 +14,59 @@
 #  }
 #
 class cpufrequtils(
+    Wmflib::Ensure $ensure = 'present',
     String $governor = 'performance'
 ) {
     unless $facts['is_virtual'] {
-        ensure_packages('cpufrequtils')
+        if debian::codename::ge('trixie') {
+            ensure_packages('linux-cpupower')
 
-        file { '/etc/default/cpufrequtils':
-            content => "GOVERNOR=${governor}\n",
-            require => Package['cpufrequtils'],
-        }
+            # Please note that Debian upstream is currently (Oct 2025) evaluating
+            # to include the systemd unit and config files in the package itself.
+            # See https://bugs-devel.debian.org/cgi-bin/bugreport.cgi?bug=894906
+            # for more info.
+            file { '/etc/default/cpupower':
+                content => "GOVERNOR=${governor}\n",
+                require => Package['linux-cpupower'],
+            }
 
-        service { 'cpufrequtils':
-            ensure => 'running',
-            enable => true,
-        }
-        # cpufrequtils is a systemd generator where RemainAfterExit=yes is set.
-        # When the service resource was trying to "start" it, systemd would
-        # find it as already running, thus not changing the governor.
-        # cpufrequtils will be reloaded if this is not the governor we are looking for
-        exec { 'cpufrequtils_reload':
-            unless  => "/usr/bin/cpufreq-info -p | /bin/grep -wq ${governor}",
-            command => '/usr/bin/systemctl reload cpufrequtils',
-            require => File['/etc/default/cpufrequtils']
-        }
+            systemd::service { 'cpupower':
+                ensure  => $ensure,
+                content => systemd_template('cpupower'),
+                restart => true,
+            }
 
+            # cpupower is a systemd unit where RemainAfterExit=yes is set.
+            # When the service resource was trying to "start" it, systemd would
+            # find it as already running, thus not changing the governor.
+            # cpupower will be reloaded if this is not the governor we are looking for
+            exec { 'cpupower_reload':
+                unless  => "/usr/bin/cpupower frequency-info -p | /bin/grep -wq ${governor}",
+                command => '/usr/bin/systemctl reload cpupower',
+                require => File['/etc/default/cpupower']
+            }
+
+        } else {
+            ensure_packages('cpufrequtils')
+
+            file { '/etc/default/cpufrequtils':
+                content => "GOVERNOR=${governor}\n",
+                require => Package['cpufrequtils'],
+            }
+
+            service { 'cpufrequtils':
+                ensure => 'running',
+                enable => true,
+            }
+            # cpufrequtils is a systemd generator where RemainAfterExit=yes is set.
+            # When the service resource was trying to "start" it, systemd would
+            # find it as already running, thus not changing the governor.
+            # cpufrequtils will be reloaded if this is not the governor we are looking for
+            exec { 'cpufrequtils_reload':
+                unless  => "/usr/bin/cpufreq-info -p | /bin/grep -wq ${governor}",
+                command => '/usr/bin/systemctl reload cpufrequtils',
+                require => File['/etc/default/cpufrequtils']
+            }
+        }
     }
 }
