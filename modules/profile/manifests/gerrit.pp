b35cd8180dca2bb4339f26840151fa042738e144
Jaime Crespo <jcrespo@wikimedia.org>
2025-10-03 16:01:38 +0200

    gerrit: Test hourly backup migration to the new storage host
    Temporarily switch configuration of gerrit hourly backups from
    the productionEqiad storage host to the reposEqiad one.
    
    This will test the setup of backup1012 before its final deployment
    in the following days, allowing an extended amount of dedicated
    backups space for both Gerrit and GitLab.
    
    Hosts: gerrit1003.wikimedia.org,gerrit2002.wikimedia.org
    Hosts: backup1014.eqiad.wmnet
    Bug: T403946
    Change-Id: I227b0d28daa17f87bc5b07259af17a32bf8a18d8

--- modules/profile/manifests/gerrit.pp
+++ modules/profile/manifests/gerrit.pp
@@ -89,7 +89,7 @@ class profile::gerrit(
 
     if $backups_enabled and $backup_set != undef {
         backup::set { $backup_set:
-            jobdefaults => "Hourly-${profile::backup::host::day}-${profile::backup::host::pool}"
+            jobdefaults => 'Hourly-Tue-ReposEqiad',
         }
         backup::set { 'home': }
     }
