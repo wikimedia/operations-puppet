b9dd2a23def54a2a512dc2dd82ec0e70192682f0
Tiziano Fogli <tfogli@wikimedia.org>
2025-10-03 10:23:53 +0200

    metamonitoring: replace deadmanswitchamhook's Gunicorn with uWSGI
    Clean up last resources and do the switch
    
    Bug: T397003
    Change-Id: Ie47401fd7d4e8a3188ab6033d9ec029a224926b2

--- modules/metamonitoring/manifests/deadmanswitchamhook.pp
+++ modules/metamonitoring/manifests/deadmanswitchamhook.pp
@@ -18,42 +18,36 @@ class metamonitoring::deadmanswitchamhook (
         ensure => stdlib::ensure($ensure, 'file'),
         source => 'puppet:///modules/metamonitoring/deadmanswitchamhook.py',
         mode   => '0555',
-        notify => Service['deadmanswitchamhook'],
+        notify => Service::Uwsgi['deadmanswitchamhook'],
     }
 
     file { '/usr/local/lib/o11y-metamonitoring/deadmanswitchamhook-wsgi.py':
         ensure => stdlib::ensure($ensure, 'file'),
         source => 'puppet:///modules/metamonitoring/deadmanswitchamhook-wsgi.py',
         mode   => '0555',
-        notify => Service['deadmanswitchamhook'],
+        notify => Service::Uwsgi['deadmanswitchamhook'],
     }
 
     file { '/etc/default/metamonitoring_deadmanswitchamhook':
         ensure  => stdlib::ensure($ensure, 'file'),
         content => template('metamonitoring/metamonitoring_deadmanswitchamhook.env.erb'),
         mode    => '0444',
-        notify  => Service['deadmanswitchamhook']
+        notify  => Service::Uwsgi['deadmanswitchamhook']
     }
 
-    systemd::service { 'deadmanswitchamhook':
-        ensure  => 'absent',
-        content => init_template('deadmanswitchamhook', 'systemd'),
-        restart => true,
+    service::uwsgi { 'deadmanswitchamhook':
+        ensure             => $ensure,
+        port               => $listen_port,
+        systemd_user       => $user,
+        systemd_group      => $user,
+        icinga_check       => false,
+        add_logging_config => false,
+        config             => {
+          'wsgi-file'        => '/usr/local/lib/o11y-metamonitoring/deadmanswitchamhook-wsgi.py',
+          'chdir'            => '/usr/local/lib/o11y-metamonitoring',
+          'processes'        => 4,
+          'log-stdout'       => true,
+          'catch-exceptions' => true
+        },
     }
-
-    #service::uwsgi { 'deadmanswitchamhook':
-    #    ensure             => $ensure,
-    #    port               => $listen_port,
-    #    systemd_user       => $user,
-    #    systemd_group      => $user,
-    #    icinga_check       => false,
-    #    add_logging_config => false,
-    #    config             => {
-    #      'wsgi-file'        => '/usr/local/lib/o11y-metamonitoring/deadmanswitchamhook-wsgi.py',
-    #      'chdir'            => '/usr/local/lib/o11y-metamonitoring',
-    #      'processes'        => 4,
-    #      'log-stdout'       => true,
-    #      'catch-exceptions' => true
-    #    },
-    #}
 }
--- modules/profile/manifests/metamonitoring.pp
+++ modules/profile/manifests/metamonitoring.pp
@@ -3,8 +3,6 @@ class profile::metamonitoring(
     Wmflib::Ensure       $ensure      = lookup('profile::metamonitoring::ensure', {default_value => 'present'}),
     String               $user        = lookup('profile::metamonitoring::user', {default_value => 'prometamon'}),
     Stdlib::Absolutepath $status_dir  = lookup('profile::metamonitoring::status_dir', { default_value => '/var/lib/o11y-metamonitoring'}),
-    # TODO Remove once cleanup is done
-    Stdlib::Absolutepath $log_dir     = lookup('profile::metamonitoring::log_dir', { default_value => '/var/log/o11y-metamonitoring'}),
 ) {
 
     systemd::sysuser { $user:
@@ -20,27 +18,6 @@ class profile::metamonitoring(
         group   => $user,
         mode    => '0755',
         require => User[$user],
-    }
-
-    # TODO Remove once cleanup is done
-    file { $log_dir:
-        ensure => 'absent',
-        force  => true,
-        owner  => $user,
-        group  => $user,
-        mode   => '0755',
-    }
-
-    # TODO Remove once cleanup is done
-    logrotate::rule { 'o11y-metamonitoring':
-        ensure        => 'absent',
-        file_glob     => "${log_dir}/*.log",
-        frequency     => 'daily',
-        rotate        => 5,
-        copy_truncate => true,
-        missing_ok    => true,
-        no_create     => true,
-        compress      => true,
     }
 
     file { $status_dir:
