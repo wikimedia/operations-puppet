0a0484ac5bde05c764a2e775d9093e0cfd61ec94
Timo Tijhof <krinkle@fastmail.com>
2025-10-08 01:56:34 -0700

    varnish: Remove unused "Mobile Redirect" logic
    * donate.m.wikimedia.org redirect covered by 52-donate-m-redirect.vtc
    
      This was re-purpising the same internal syth() handler for a redirect
      that was unrelated and went in the opposite direction. This might
      not need a custom handler long-term as a regular 302 Found + Location
      should suffice, but I could not find a single case in our VCL that
      set resp.http.Location directly with synth(302), so for now I'll
      assume that the keep-alive and Content-Length hacks are important
      and need to stay, and thus keep using req.http.Location as fake
      mid-request staging area for *resp*.http.Location, like other
      directs in text-frontend, misc-frontend, and upload-frontend.
    
      It might be worth extracting that since they all seem to be
      identical.
    
    Bug: T405931
    Change-Id: I07085c58e37ca680dfead30836ea3c097e825022

--- modules/profile/manifests/cache/varnish/frontend.pp
+++ modules/profile/manifests/cache/varnish/frontend.pp
@@ -57,8 +57,6 @@ class profile::cache::varnish::frontend (
     String                  $uds_owner               = lookup('profile::cache::varnish::frontend::uds_owner'),
     String                  $uds_group               = lookup('profile::cache::varnish::frontend::uds_group'),
     Stdlib::Filemode        $uds_mode                = lookup('profile::cache::varnish::frontend::uds_mode'),
-    Array[Stdlib::Fqdn]     $enable_m_redir          = lookup('profile::cache::varnish::frontend::enable_m_redir'),
-    Array[Stdlib::Fqdn]     $enable_m_redir_except   = lookup('profile::cache::varnish::frontend::enable_m_redir_except'),
     Stdlib::Unixpath        $privileged_uds          = lookup('profile::cache::varnish::frontend::privileged_uds', {'default_value'         => '/run/varnish-privileged.socket'}),
     Boolean                 $use_etcd_req_filters    = lookup('profile::cache::varnish::frontend::use_etcd_req_filters'),
     Boolean                 $use_ip_reputation       = lookup('profile::cache::varnish::frontend::use_ip_reputation'),
@@ -216,9 +214,6 @@ class profile::cache::varnish::frontend (
         do_edge_uniques             => $do_edge_uniques,
         edge_uniques_key_path       => "${edge_uniques_key_dir}/keys.cfg",
         edge_uniques_cfg_path       => $edge_uniques_cfg_path,
-        # New mobile routing: Unified domains (T214998)
-        enable_m_redir_regex        => $enable_m_redir.regexpescape.join('|'),
-        enable_m_redir_except_regex => $enable_m_redir_except.regexpescape.join('|'),
     }
 
     # VCL files common to all instances
