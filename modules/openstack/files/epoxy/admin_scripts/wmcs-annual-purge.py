22768eac5c0452725f36d6c191191b8fadb63bd0
Andrew Bogott <abogott@wikimedia.org>
2025-10-14 10:58:25 -0500

    wmcs-annual-purge.py: update to catch up with various openstack changes
    - project name not always the same as project id
    - multiple new network types
    - display ipv6 addresses when possible
    
    Change-Id: Iad96081ed6a3a930ac9e8712db4ca506800c77b3

--- modules/openstack/files/epoxy/admin_scripts/wmcs-annual-purge.py
+++ modules/openstack/files/epoxy/admin_scripts/wmcs-annual-purge.py
@@ -4,14 +4,30 @@
 # Print wikitext for use in the annual purge page:
 #  fqdn, id, ip, flavor -- grouped by project
 
+import json
 import mwopenstackclients
+import subprocess
 
 clients = mwopenstackclients.clients(oscloud="novaadmin")
 
 allprojects = clients.allprojects()
+sortedprojects = sorted(allprojects, key=lambda d: d.name)
 
-for project in allprojects:
-    print("\n=== %s ===\n" % project.id)
+for project in sortedprojects:
+    print(
+        "\n=== [https://openstack-browser.toolforge.org/project/%s %s] ===\n"
+        % (project.name, project.name)
+    )
+
+    if (
+        project.name == "trove"
+        or project.name == "tools"
+        or project.name == "octavia"
+        or project.name == "cloudvirt-canary"
+        or project.name == "admin-monitoring"
+    ):
+        print("Many instances!")
+        continue
 
     servers = clients.allinstances(projectid=project.id)
     flavors = clients.novaclient(project=project.id).flavors.list()
@@ -22,13 +38,85 @@ for project in allprojects:
             flavorname = flavordict[server.flavor["id"]]
         else:
             flavorname = "(unknown flavor)"
-        print(
-            "%s.%s.eqiad1.wikimedia.cloud %s %s %s\n"
-            % (
-                server.name,
-                project.id,
-                server.id,
-                server.addresses["VLAN/legacy"][0]["addr"],
-                flavorname,
+        if "VLAN/legacy" in server.addresses:
+            print(
+                "%s.%s.eqiad1.wikimedia.cloud %s %s %s\n"
+                % (
+                    server.name,
+                    project.name,
+                    server.id,
+                    server.addresses["VLAN/legacy"][0]["addr"],
+                    flavorname,
+                )
             )
+        elif "VXLAN/IPv4-only" in server.addresses:
+            print(
+                "%s.%s.eqiad1.wikimedia.cloud %s %s %s\n"
+                % (
+                    server.name,
+                    project.id,
+                    server.id,
+                    server.addresses["VXLAN/IPv4-only"][0]["addr"],
+                    flavorname,
+                )
+            )
+        elif "VXLAN/IPv6-only" in server.addresses:
+            print(
+                "%s.%s.eqiad1.wikimedia.cloud %s %s %s\n"
+                % (
+                    server.name,
+                    project.id,
+                    server.id,
+                    server.addresses["VXLAN/IPv6-only"][0]["addr"],
+                    flavorname,
+                )
+            )
+        elif "VXLAN/IPv6-dualstack" in server.addresses:
+            ipv4 = server.addresses["VXLAN/IPv6-dualstack"][0]["addr"]
+            if len(server.addresses["VXLAN/IPv6-dualstack"]) > 1:
+                ipv6 = server.addresses["VXLAN/IPv6-dualstack"][1]["addr"]
+            else:
+                ipv6 = ""
+
+            print(
+                "%s.%s.eqiad1.wikimedia.cloud %s %s %s %s\n"
+                % (
+                    server.name,
+                    project.id,
+                    server.id,
+                    ipv4,
+                    ipv6,
+                    flavorname,
+                )
+            )
+        else:
+            print("Found server with unknown address type")
+            print(server.addresses)
+            exit(1)
+
+    databaseservers = clients.troveclient(project.id).instances.list()
+    for server in databaseservers:
+        print("Database server: %s (%s)" % (server.name, server.datastore["type"]))
+
+    try:
+        object_buckets = json.loads(
+            subprocess.check_output(
+                [
+                    "/usr/bin/radosgw-admin",
+                    "--format",
+                    "json",
+                    "bucket",
+                    "list",
+                    "--uid",
+                    "%s$%s" % (project.id, project.id),
+                ],
+                stderr=subprocess.DEVNULL,
+            ).decode("utf8")
         )
+
+        for bucket in object_buckets:
+            print("\nObject storage bucket: %s" % bucket)
+
+    except subprocess.CalledProcessError:
+        # radosgw doesn't know about this project. No problem.
+        pass
