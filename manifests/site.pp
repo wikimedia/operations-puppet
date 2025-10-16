62d0b4071208544b6ea435c8c8250860b2e6b74c
Manuel Arostegui <marostegui@wikimedia.org>
2025-10-16 06:25:25 +0200

    mariadb: Productionize db1262
    This will go to s4 and eventually to x4
    
    Bug: T406550
    Change-Id: Ieba2aaff0d96294226feb1f244d98026d4e90abd

--- manifests/site.pp
+++ manifests/site.pp
@@ -572,7 +572,7 @@ node /^db2(149|156|177|190|194|205|209|227)\.codfw\./ {
 
 # s4 (commons) core production dbs on eqiad
 # See also db1245 and db1150 below
-node /^db1(160|190|199|238|241|242|243|244|247|248|249|252|260|261)\.eqiad\./ {
+node /^db1(160|190|199|238|241|242|243|244|247|248|249|252|260|261|262)\.eqiad\./ {
     role(mariadb::core)
 }
 
@@ -1368,7 +1368,7 @@ node /^db224[5|6]\.codfw\./ {
 }
 
 # eqiad databases T400214
-node /^db126[2|3]\.eqiad\./ {
+node /^db126[3]\.eqiad\./ {
     role(insetup::data_persistence_ferm)
 }
 
