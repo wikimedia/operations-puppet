bf18b9926661524feb172d1d9943a894b1791823
Cathal Mooney <cmooney@wikimedia.org>
2025-10-16 10:30:24 +0200

    ssw1-d1-eqiad: add to puppet inventory and monitoring
    Adding this as a first test Nokia device here to validate things
    work as expected in terms of monitoring once it is set to live.
    
    Provided everything goes ok I will add the other Nokia devices
    in a subsequent patch.
    
    Bug: T405558
    Change-Id: Iad278b41b8c87dedef2e3588e50bc87f5bba4c63

--- modules/netops/manifests/monitoring.pp
+++ modules/netops/manifests/monitoring.pp
@@ -122,6 +122,7 @@ class netops::monitoring(
         'cloudsw1-e4-eqiad.mgmt' => { ipv4 => '10.65.1.231', parents => ['msw2-eqiad']},
         'cloudsw1-f4-eqiad.mgmt' => { ipv4 => '10.65.1.235', parents => ['msw2-eqiad']},
         # eqiad prod
+        'ssw1-d1-eqiad.mgmt' => { ipv4 => '10.65.0.74', parents => ['msw1-eqiad'] },
         'ssw1-e1-eqiad.mgmt' => { ipv4 => '10.65.2.143', parents => ['msw2-eqiad'] },
         'lsw1-e1-eqiad.mgmt' => { ipv4 => '10.65.1.228', parents => ['msw2-eqiad'] },
         'lsw1-e2-eqiad.mgmt' => { ipv4 => '10.65.1.229', parents => ['msw2-eqiad'] },
