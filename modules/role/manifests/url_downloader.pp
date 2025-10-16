a616cf881155bf0f4ef8af384557595c25d79855
Sukhbir Singh <ssingh@wikimedia.org>
2025-10-16 09:33:03 -0400

    url_downloader: remove hcaptcha proxy configuration
    We have migrated these hosts to dedicated hcaptcha VMs, so this
    configuration should be removed from the urldownloader hosts.
    
    This commit will require a NOOP PyBal restart because the service
    description has changed and our alerting is not smart enough to
    distinguish between that and an actual code change.
    
    Bug: T405631
    Hosts: urldownloader1003.wikimedia.org
    Change-Id: I08e685bc09121d1c405d3a7ddfcae316f798a433

--- modules/role/manifests/url_downloader.pp
+++ modules/role/manifests/url_downloader.pp
@@ -2,11 +2,4 @@ class role::url_downloader {
     include profile::base::production
     include profile::firewall
     include profile::url_downloader
-
-    # Prototype hCaptcha IP blinding proxy is living on url-downloader machines
-    # for simplicity. This is behind LVS.
-    include profile::lvs::realserver
-    include profile::nginx
-    include profile::pki::client
-    include profile::hcaptcha::proxy
 }
