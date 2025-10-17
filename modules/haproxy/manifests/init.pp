06a3bb9e13f311078964b6d339755125380460ee
Tiziano Fogli <tfogli@wikimedia.org>
2025-10-17 15:46:50 +0200

    haproxy: enable nrpe2nodexp wrapper on haproxy_alive check
    Bug: T407137
    Bug: T384472
    Change-Id: I19c5ee9f9edc513861a6ad1e7dd3abadcfb31803

--- modules/haproxy/manifests/init.pp
+++ modules/haproxy/manifests/init.pp
@@ -128,11 +128,12 @@ class haproxy(
         }
 
         nrpe::monitor_service { 'haproxy_alive':
-            ensure         => bool2str($monitor_check_haproxy, 'present', 'absent'),
-            description    => 'haproxy alive',
-            nrpe_command   => '/usr/local/lib/nagios/plugins/check_haproxy --check=alive',
-            notes_url      => 'https://wikitech.wikimedia.org/wiki/HAProxy',
-            migration_task => 'T407137',
+            ensure             => bool2str($monitor_check_haproxy, 'present', 'absent'),
+            description        => 'haproxy alive',
+            nrpe_command       => '/usr/local/lib/nagios/plugins/check_haproxy --check=alive',
+            notes_url          => 'https://wikitech.wikimedia.org/wiki/HAProxy',
+            migration_task     => 'T407137',
+            enable_nrpe2nodexp => true,
         }
     }
 
