f03b9c196c5c47308c43fda2468985e94dacc619
Daniel Zahn <dzahn@wikimedia.org>
2025-10-20 14:22:11 -0700

    zookeeper: replace legacy facts, fix lint warnings
    Change-Id: I92e4d8d88728189a9c4ff3a135e470efff79adfb

--- modules/profile/manifests/zookeeper/monitoring/server.pp
+++ modules/profile/manifests/zookeeper/monitoring/server.pp
@@ -10,7 +10,7 @@ class profile::zookeeper::monitoring::server(
     $jmx_exporter_config_file = '/etc/prometheus/jvm_zookeeper_jmx_exporter.yaml'
     $prometheus_jmx_exporter_zookeeper_server_port = 12181
     $java_opts = "-javaagent:/usr/share/java/prometheus/jmx_prometheus_javaagent.jar=${::ipaddress}:${prometheus_jmx_exporter_zookeeper_server_port}:${jmx_exporter_config_file}"
-    profile::prometheus::jmx_exporter { "zookeeper_server_${::hostname}":
+    profile::prometheus::jmx_exporter { "zookeeper_server_${::facts[hostname]}":
         hostname    => $::hostname,
         port        => $prometheus_jmx_exporter_zookeeper_server_port,
         config_file => $jmx_exporter_config_file,
