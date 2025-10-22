8b593e3c78da4412a76503750269834cfbca3079
Taavi Väänänen <taavi@wikimedia.org>
2025-10-22 15:51:40 +0300

    P:toolforge: Remove separate proxy role
    Bug: T283948
    Change-Id: I52437eca8ede4f77c89eb0e56bf6fa786a6a6964

--- modules/role/manifests/wmcs/toolforge/proxy.pp
+++
@@ -1,7 +0,0 @@
-class role::wmcs::toolforge::proxy {
-    include profile::toolforge::base
-    include profile::toolforge::infrastructure
-    include profile::toolforge::proxy
-    include profile::toolforge::toolviews
-    include profile::firewall
-}
