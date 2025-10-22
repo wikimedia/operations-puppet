b6c701bce12498772c17d057edab3fa809208e31
Manuel Arostegui <marostegui@wikimedia.org>
2025-10-22 08:04:06 +0200

    mariadb: Add db2249 to puppet
    This host will go to x1
    
    Bug: T407941
    Bug: T405272
    Change-Id: Ic55b7c89850fb8bab01f8dcea1a26247892952bf

--- manifests/site.pp
+++ manifests/site.pp
@@ -688,6 +688,10 @@ node /^db126[4]\.eqiad\./ {
     role(insetup::data_persistence_ferm)
 }
 
+node /^db224[9]\.codfw\./ {
+    role(insetup::data_persistence_ferm)
+}
+
 # ms1 ms2 and ms3 shards
 # They use the parsercache role in puppet so masters are RW all the time.
 # eqiad
