282d6ed6ba3d836ac451a44fedeeb01acabe214a
Scott French <swfrench@wikimedia.org>
2025-10-22 17:27:04 -0700

    P:cache::varnish::frontend: render known-client rate limit VCL
    This introduces a new confd-rendered VCL include fragment implementing
    known-client rate limits.
    
    In contrast to known_client identification and impersonation detection
    rules, which are rendered as HAProxy DSL by HIDDENPARMA, these rules are
    rendered directly from the known_client objects by confd (similar to how
    ipblock objects are rendered directly to HAProxy maps).
    
    Rendering is gated behind the 'known' rate_limiting_flags hash entry, to
    be added in I4484d65d50eeccc992d18df2a39c678ab592feec (default: false).
    
    Points of note:
    * Rules are rendered only for objects with identification enabled.
    * If there is no custom limit configured for the respective cache
      cluster in an identification-enabled object [0], a cluster-specific
      default is used (see TODO for setting these limits).
    * Since known_client identifiers will never collide, and we configure
      explicit limits for all identification-enabled objects, we don't
      technically need to structure this as an elseif chain (they could be
      standalone statements). However, doing so is arguably easier to reason
      about and cuts out some unnecessary regexp matches (on average).
    * That said, we could combine an elseif chain for configured limits and
      a catch-all else for defaults, if we want to optimize for the case
      where defaults are ubiquitous. See [1] for an example.
    * There is no way to compute per-rule hashes in confd (e.g., no sprig
      template functions). Instead, the response message contains a static
      hash that will at least ease discovery in the puppet repo.
    
    [0] A custom rate limit is considered configured if (1) its associated
        enabled field is true and (2) it contains a strictly positive value.
        See https://gitlab.wikimedia.org/repos/sre/hiddenparma/-/merge_requests/122.
    
    [1] https://phabricator.wikimedia.org/P84291
    
    Hosts: O:cache::text
    Hosts: O:cache::upload
    Bug: T403220
    Change-Id: Ia8da17fc2a7f9fe59fd6cf1175d0e29b7d6599f6

--- modules/profile/manifests/cache/varnish/frontend.pp
+++ modules/profile/manifests/cache/varnish/frontend.pp
@@ -271,6 +271,17 @@ class profile::cache::varnish::frontend (
                 reload_vcl_opts => $reload_vcl_opts,
             }
         }
+        if $rate_limiting_flags['known'] {
+            profile::cache::varnish::known_client_rate_limits_file{ 'known-client-rate-limits':
+                conftool_prefix => $conftool_prefix,
+                cache_cluster   => $cache_cluster,
+                reload_vcl_opts => $reload_vcl_opts,
+            }
+        } else {
+            file { '/etc/varnish/known-client-rate-limits.inc.vcl':
+                ensure => absent,
+            }
+        }
     } else {
         file { ['/etc/varnish/requestctl-filters.inc.vcl', '/etc/varnish/requestctl-filters-hit.inc.vcl', '/etc/varnish/blocked-nets.inc.vcl']:
             ensure => absent,
---
+++ modules/profile/manifests/cache/varnish/known_client_rate_limits_file.pp
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: Apache-2.0
+# @summary define to generate a known-client rate limits file
+# @param conftool_prefix the prefix to use for conftool
+# @param cache_cluster name of cache cluster e.g. upload or text
+# @param reload_vcl_opts options for the reload of VCL
+define profile::cache::varnish::known_client_rate_limits_file (
+    String $conftool_prefix,
+    Enum['text', 'upload'] $cache_cluster,
+    String $reload_vcl_opts,
+) {
+    # Select the rate limit enabled and value field names associated with this cluster.
+    $cluster_limit_field_name = "throttle_requests_${cache_cluster}"
+    $cluster_limit_enabled_field_name = "throttle_requests_${cache_cluster}_enabled"
+    # Select the default rate limit associated with this cluster. Units are requests per minute.
+    # These should be compatible with https://wikitech.wikimedia.org/wiki/Robot_policy.
+    # TODO: T403220 - Finalize these limits before enabling rate limiting.
+    $default_rate_limit = $cache_cluster ? {
+        'text'   => 3000,  # 50 rps on average
+        # Note: This cannot be used in upload until a default limit is selected.
+        default  => fail("Unsupported cache cluster: ${cache_cluster}"),
+    }
+    $safe_title = $title.regsubst('\W', '_', 'G')
+    confd::file { "/etc/varnish/${safe_title}.inc.vcl":
+        ensure     => present,
+        reload     => "/usr/local/bin/confd-reload-vcl varnish-frontend ${reload_vcl_opts}",
+        before     => Service['varnish-frontend'],
+        watch_keys => ['/request-known-clients'],
+        content    => template('profile/cache/varnish-frontend-known-client-rate-limits.vcl.tpl.erb'),
+        prefix     => $conftool_prefix;
+    }
+}
