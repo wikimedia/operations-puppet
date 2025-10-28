bc37b3d2887602514f3fc024aaa672167f428eb1
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-10-28 12:46:37 +0100

    osm_sync_lag.sh: Fix default to current directory
    /srv/osmosis no longer exists, with imposm we use /srv/osm/diff/last.state.txt,
    so also use that as a the default value in the script to avoid confusion.
    
    Bug: T381565
    Change-Id: Id03087d41197246b38bd2e4471390831a56288c7

--- modules/osm/files/osm_sync_lag.sh
+++ modules/osm/files/osm_sync_lag.sh
@@ -4,7 +4,7 @@
 set -e
 set -u
 
-state_path=${1:-"/srv/osmosis/state.txt"}
+state_path=${1:-"/srv/osm/diff/last.state.txt"}
 prometheus_path=${2:-"/var/lib/prometheus/node.d/osm_sync_lag.prom"}
 
 timestamp=$(awk -F= '/^timestamp=/ { print $2 }' $state_path | tr -d \\\\)
