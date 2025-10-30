82477269a6f7764ac01e50aad1f3f119c143b073
Andrew Bogott <abogott@wikimedia.org>
2025-10-30 11:11:58 -0500

    cloudservices: include openstack client packages
    We need these for the dns aliaser. I'm not sure why this worked
    on Bookworm but it does not on Trixie.
    
    Hosts: cloudservices1005.eqiad.wmnet
    Hosts: cloudservices2004-dev.codfw.wmnet
    Change-Id: Ic78c734e0f132a85e4b51fbe635abdc969471ac0

--- modules/role/manifests/wmcs/openstack/eqiad1/services.pp
+++ modules/role/manifests/wmcs/openstack/eqiad1/services.pp
@@ -4,6 +4,7 @@ class role::wmcs::openstack::eqiad1::services {
     include profile::base::cloud_production
     include profile::wmcs::cloud_private_subnet
     include profile::wmcs::cloud_private_subnet::bgp
+    include profile::openstack::eqiad1::clientpackages
     include profile::openstack::eqiad1::pdns::auth::db
     include profile::openstack::eqiad1::pdns::auth::service
     include profile::openstack::eqiad1::pdns::recursor::service
