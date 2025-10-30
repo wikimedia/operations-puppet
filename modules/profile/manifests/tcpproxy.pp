f97dee457be8a6b7146af2368a2eeb00ea405806
Daniel Zahn <dzahn@wikimedia.org>
2025-10-30 16:56:51 -0700

    tcpproxy: add config template and parameters
    Add a template with the main haproxy.cfg.
    
    Start with the config suggested by cdanis on P84490.
    
    Add the 2 parameters for the socket path and to toggle
    the prometheus config part.
    
    Bug: T408532
    Change-Id: I593f2900b048d6006d1e78d1385bb467a930b216

--- modules/profile/manifests/tcpproxy.pp
+++ modules/profile/manifests/tcpproxy.pp
@@ -1,8 +1,21 @@
 # SPDX-License-Identifier: Apache-2.0
 # sets up a TCP proxy (using HAproxy)
-class profile::tcpproxy {
+class profile::tcpproxy(
+    String $socket = lookup(profile::tcpproxy::socket),
+    Optional[Stdlib::Port] $prometheus_port = lookup('profile::tcpproxy::prometheus_port', {'default_value' => undef}),
+){
 
     ensure_packages(['haproxy'])
 
     profile::auto_restarts::service { 'haproxy': }
+
+    file { '/etc/haproxy/haproxy.cfg':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0544',
+        content => template('profile/tcpproxy/haproxy_tcpproxy.cfg.erb'),
+        require => Package['haproxy'],
+    }
+
 }
