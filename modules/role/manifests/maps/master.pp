63dafe9f8ecebc110c2349377b98c33a8327a2ad
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-11-06 13:18:44 +0100

    Remove legacy maps roles
    Bug: T381565
    Change-Id: If5f9f26f9cd8ffdde82ea38cff8561c3b5d5ac05

--- modules/role/manifests/maps/master.pp
+++
@@ -1,9 +0,0 @@
-# Sets up a maps server master
-class role::maps::master {
-    include profile::base::production
-    include profile::rsyslog::udp_localhost_compat
-    include profile::firewall
-
-    include profile::maps::osm_master
-    include profile::prometheus::postgres_exporter
-}
--- modules/role/manifests/maps/replica.pp
+++
@@ -1,9 +0,0 @@
-# Sets up a maps server replica
-class role::maps::replica {
-    include profile::base::production
-    include profile::rsyslog::udp_localhost_compat
-    include profile::firewall
-
-    include profile::maps::osm_replica
-    include profile::prometheus::postgres_exporter
-}
