2f0e5ea5978890d56847333ad09761cb7154825c
Alexander Vorwerk <zabe@avorwerk.net>
2025-11-07 00:54:31 +0100

    mediawiki: Update location of startupregistrystats script
    Bug: T409212
    Change-Id: I11bb5af292577ab4b6b1e17d273ad6d3726ebcc2

--- modules/profile/manifests/mediawiki/maintenance/startupregistrystats.pp
+++ modules/profile/manifests/mediawiki/maintenance/startupregistrystats.pp
@@ -4,7 +4,7 @@ class profile::mediawiki::maintenance::startupregistrystats(
     $team = 'mediawiki-platform'
     # group0: test.wikipedia.org
     profile::mediawiki::periodic_job { 'startupregistrystats-testwiki':
-        command               => '/usr/local/bin/mwscript extensions/WikimediaMaintenance/blameStartupRegistry.php --wiki testwiki --record-stats',
+        command               => '/usr/local/bin/mwscript extensions/WikimediaMaintenance/maintenance/blameStartupRegistry.php --wiki testwiki --record-stats',
         interval              => '*:10',
         cron_schedule         => '10 * * * *',
         team                  => $team,
@@ -16,7 +16,7 @@ class profile::mediawiki::maintenance::startupregistrystats(
 
     # group0: mediawiki.org
     profile::mediawiki::periodic_job { 'startupregistrystats-mediawikiwiki':
-        command               => '/usr/local/bin/mwscript extensions/WikimediaMaintenance/blameStartupRegistry.php --wiki mediawikiwiki --record-stats',
+        command               => '/usr/local/bin/mwscript extensions/WikimediaMaintenance/maintenance/blameStartupRegistry.php --wiki mediawikiwiki --record-stats',
         interval              => '*:15',
         cron_schedule         => '15 * * * *',
         team                  => $team,
@@ -28,7 +28,7 @@ class profile::mediawiki::maintenance::startupregistrystats(
 
     # large wikis (inludes several group1 and group2 wikis)
     profile::mediawiki::periodic_job { 'startupregistrystats':
-        command               => '/usr/local/bin/foreachwikiindblist large extensions/WikimediaMaintenance/blameStartupRegistry.php --record-stats',
+        command               => '/usr/local/bin/foreachwikiindblist large extensions/WikimediaMaintenance/maintenance/blameStartupRegistry.php --record-stats',
         interval              => '*:35',
         cron_schedule         => '35 * * * *',
         team                  => $team,
