eb6dfd5104b045fa8c121c027e950c9c6c06e306
Manuel Arostegui <marostegui@wikimedia.org>
2025-11-12 12:32:27 +0100

    clouddb1022: Initial puppet run
    Going to start populating this host with s3 and x3.
    Initial puppet commit to add the basics to have it up with data and
    replicating
    
    Bug: T409557
    Change-Id: I25c83f96488f73857e637cc1c9001937a1cb1038

--- manifests/site.pp
+++ manifests/site.pp
@@ -1751,14 +1751,14 @@ node /^cloudnet100[56]\.eqiad\./ {
 }
 
 ## Multi-instance wikireplica dbs
-node /^clouddb10(13|14|15|16)\.eqiad\./ {
+node /^clouddb10(13|14|15|16|22)\.eqiad\./ {
     role(wmcs::db::wikireplicas::web_multiinstance)
 }
 node /^clouddb10(17|18|19|20)\.eqiad\./ {
     role(wmcs::db::wikireplicas::analytics_multiinstance)
 }
 # clouddb1021 is skipped, it was a special host that's now gone
-node /^clouddb10(2[2-9]|3[0-3])\.eqiad\./ {
+node /^clouddb10(2[3-9]|3[0-3])\.eqiad\./ {
     role(insetup::wmcs_ferm)
 }
 
