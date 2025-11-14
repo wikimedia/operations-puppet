0e86db05cfd535cb4bdd3a7bc1f6c8b67a6d678a
Jasmine <jkilani@wikimedia.org>
2025-11-14 13:18:01 -0800

    wikikube: decommission worker[2003-2004,2007-2010,2019-2032,2040,2043,2045,2048]
    Bug: T409102
    Change-Id: Ifb871dfc1954ce678282b4714ae0834d83fdfeb9

--- manifests/site.pp
+++ manifests/site.pp
@@ -1642,7 +1642,7 @@ node /^wikikube-worker1(2[4-9][0-9]|3[0-1][0-9]|32[0-7])\.eqiad\./ {
     role(kubernetes::worker)
 }
 
-node /^wikikube-worker20(0[1-9]|[1-9][0-9])\.codfw\./ {
+node /^wikikube-worker20(0[1256]|1[1-9]|3[3-9]|4[124679]|[5-9][0-9])\.codfw\./ {
     role(kubernetes::worker)
 }
 
