de3392dbf0aa1f7b1bba05b34f1be00e7f732244
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2025-11-19 09:47:56 +0100

    wdqs: Do not create task on failure of the WDQS LDF endpoints
    The WDQS LDF endpoints don't have a formal SLO, are backed by a single
    server, and are expected to fail reasonably often. Creating a task
    each time this endpoint is down is counter productive. Changing the
    severity of that check to "warning" should make those alerts less noisy.
    
    More info on:
    * https://wikitech.wikimedia.org/wiki/Wikidata_Query_Service#LDF_(Linked_Data_Fragments)_endpoint
    * https://www.wikidata.org/wiki/Wikidata:Data_access#Linked_Data_Fragments_endpoint
    
    Bug: T408853
    Change-Id: I33512d3e61720f7e7d1928cba9c25144efe662a3

--- modules/profile/manifests/query_service/monitor/ldf.pp
+++ modules/profile/manifests/query_service/monitor/ldf.pp
@@ -4,7 +4,7 @@ class profile::query_service::monitor::ldf {
         instance_label     => $facts['hostname'],
         server_name        => 'query.wikidata.org',
         team               => 'data-platform',
-        severity           => 'task',
+        severity           => 'warning',
         path               => '/bigdata/ldf?subject=wd%3AQ42&predicate=wdt%3AP31&object=',
         body_regex_matches => ['wd:Q42\s+wdt:P31\s+wd:Q5\s+\.'],
         force_tls          => true,
