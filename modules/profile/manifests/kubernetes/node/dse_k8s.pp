e9b7ec325a115ceaaed673fa1f3f5d43f57f34d7
Balthazar Rouberol <brouberol@wikimedia.org>
2025-11-24 16:39:56 +0100

    dse-k8s: delete the stat-> PG on k8s ingress firewall rule
    The loading of synthetic data on PG experiment is done, so we no longer
    need the associated ferm rule.
    
    Signed-off-by: Balthazar Rouberol <brouberol@wikimedia.org>
    Bug: T409591
    Change-Id: I1a54146bc7b5dfc85a8c0e9b5d52490bb49d7193

--- modules/profile/manifests/kubernetes/node/dse_k8s.pp
+++ modules/profile/manifests/kubernetes/node/dse_k8s.pp
@@ -7,14 +7,4 @@ class profile::kubernetes::node::dse_k8s (
             'vm.max_map_count'          => 262144,
         }
     }
-
-    # Temporary firewall rule allowing stat hosts to egress to the postgresql-growthbook-rw.growthbook service VIP.
-    # @mpopov will write a jupyter notebook to generate synthetic data in a schema Growthbook understands, and will
-    # load it into the database, as part of T409591. Once the ticket is closed, we'll be able to remove that ferm rule.
-    ferm::service { 'growthbook-pg-rw':
-        proto  => 'tcp',
-        port   => 5432,
-        srange => '(@resolve((stat1008.eqiad.wmnet stat1009.eqiad.wmnet stat1010.eqiad.wmnet stat1011.eqiad.wmnet)))',
-        drange => '10.67.44.231',
-    }
 }
