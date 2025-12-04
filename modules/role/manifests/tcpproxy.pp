ad2265e980dc44113678d1c6262839bacb4b4c04
Chris Danis <cdanis@wikimedia.org>
2025-12-04 14:21:25 -0500

    tcpproxy: add lvs::realserver:* to puppet role
    Hosts: O:cache::text
    Hosts: O:tcpproxy
    Hosts: O:lvs::balancer
    Hosts: O:liberica
    Change-Id: Ic8dc08993269f666b1360defd95abd7fb26813fb

--- modules/role/manifests/tcpproxy.pp
+++ modules/role/manifests/tcpproxy.pp
@@ -4,4 +4,7 @@ class role::tcpproxy {
     include profile::base::production
     include profile::firewall
     include profile::tcpproxy
+    include profile::lvs::realserver
+    include profile::lvs::realserver::ipip
+    include profile::conftool::client
 }
