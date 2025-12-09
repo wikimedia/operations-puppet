0c450bd6d142a9627a96a60f42623fede0cd86ca
Brian King <bking@wikimedia.org>
2025-12-09 15:17:19 +0000

    wdqs: Move temp hosts to wdqs::test role
    Move to test role, so Wikidata Platform software
    engineers will be allowed to access these hosts via SSH.
    
    I have left out hosts wdqs103[3-5] as they are net-new
    hosts that have yet to arrive at the DC and we may
    have a different role to assign them by the time they
    are ready.
    
    Hosts: wdqs1030.eqiad.wmnet
    Bug: T410406
    Change-Id: I462de0fc4488d6244cd133efb60af3dfd5ae9613

--- manifests/site.pp
+++ manifests/site.pp
@@ -2582,7 +2582,11 @@ node /^wdqs202[6-7].codfw\./ {
     role(wdqs::internal_scholarly)
 }
 
-node /^wdqs10(2[8-9]|3[0-5]).eqiad\./ {
+node /^wdqs10(2[8-9]|3[0-2]).eqiad\./ {
+    role(wdqs::test)
+}
+
+node /^wdqs10(3[3-5]).eqiad\./ {
     role(insetup::data_platform_ferm)
 }
 
