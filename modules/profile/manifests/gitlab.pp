74583728bf2a9e096afb548a8c855977ddc7548f
jelto <jwodstrcil@wikimedia.org>
2025-12-16 09:38:54 +0100

    gitlab: use real netmask in interface::alias on replicas
    * When setting the IP alias addresses for the different gitlab
      instances, use the real netmask of the secondary interface
      for them as they are not service (VIP) IPs, passing the related
      parameter to interface::alias define.
    
    * Test on the replicas first
    
    Bug: T370018
    Change-Id: I6d09f0abc29cf9b9c56554fb1b14652ceee52d99

--- modules/profile/manifests/gitlab.pp
+++ modules/profile/manifests/gitlab.pp
@@ -175,9 +175,10 @@ class profile::gitlab(
 
     # add a service IP to the NIC - T276148
     interface::alias { 'gitlab service IP':
-        ipv4   => $service_ip_v4,
-        ipv6   => $service_ip_v6,
-        notify => Service['ssh-gitlab']
+        ipv4          => $service_ip_v4,
+        ipv6          => $service_ip_v6,
+        is_service_ip => $active_host == $facts['fqdn'], # test on the replicas first T370018
+        notify        => Service['ssh-gitlab']
     }
 
     # open ports in firewall - T276144
