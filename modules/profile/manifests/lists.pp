444c6f44e7daadcbd9c5753cf63ec4cee2957386
jelto <jwodstrcil@wikimedia.org>
2025-12-17 14:33:08 +0100

    lists: remove duplicate spamd auto restart
    Puppet fails with:
    Profile::Auto_restarts::Service[spamd] is already declared
    
    This change removes the duplicate auto restart from profile::lists
    
    Change-Id: I8c8a31e13afc1555ae7bee8c0ec47e76490b3c1d

--- modules/profile/manifests/lists.pp
+++ modules/profile/manifests/lists.pp
@@ -176,8 +176,6 @@ class profile::lists (
         monitoring_ensure => $mailman_service_ensure,
     }
 
-    profile::auto_restarts::service { 'spamd': }
-
     $list_outbound_ips = [
         pick($lists_ipv4, $facts['ipaddress']),
         pick($lists_ipv6, $facts['ipaddress6']),
