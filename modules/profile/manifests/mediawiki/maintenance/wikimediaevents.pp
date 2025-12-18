969e849a54275d58a03e8e8023c83e748a3b43aa
Thalia <thalia.e.chan@googlemail.com>
2025-12-18 18:41:27 +0000

    Don't collect CheckUser-specific temp account patrolling metrics on labs
    Why:
    
    * A maintenance script is scheduled to collect metrics for
      temporary account patrolling via the CheckUser extension, since
      ff036ea.
    * This is scheduled to run in all realms, but does not make sense
      to run in labs, where CheckUser is not loaded.
    * Attempting to run this in labs has been causing errors to
      appear in the logs.
    
    What:
    
    * Introduce a conditional to prevent this running on labs.
    
    Bug: T413101
    Change-Id: Id7c524d054fc0bc297928c2d86785fc073d8896c

--- modules/profile/manifests/mediawiki/maintenance/wikimediaevents.pp
+++ modules/profile/manifests/mediawiki/maintenance/wikimediaevents.pp
@@ -5,26 +5,28 @@ class profile::mediawiki::maintenance::wikimediaevents(
     # team label for alerting
     $team_label = 'trust-and-safety-product'
 
-    profile::mediawiki::periodic_job { 'wikimediaevents-UpdatePeriodicMetrics-per-wiki':
-        # dblists must reflect https://gerrit.wikimedia.org/r/plugins/gitiles/operations/mediawiki-config/+/refs/heads/master/wmf-config/InitialiseSettings.php#8808
-        command               => '/usr/local/bin/foreachwikiindblist "all - closed - private - fishbowl" extensions/WikimediaEvents/maintenance/UpdatePeriodicMetrics.php --verbose',
-        interval              => '*-*-* 04:40:00',
-        cron_schedule         => '40 4 * * *',
-        team                  => $team_label,
-        script_label          => 'UpdatePeriodicMetrics.php-per-wiki',
-        description           => 'Calculate per-wiki periodic metrics and let them be pulled by Prometheus (T375508)',
-        kubernetes            => true,
-        helmfile_defaults_dir => $helmfile_defaults_dir,
-    }
+    unless $::realm == 'labs' {
+        profile::mediawiki::periodic_job { 'wikimediaevents-UpdatePeriodicMetrics-per-wiki':
+            # dblists must reflect https://gerrit.wikimedia.org/r/plugins/gitiles/operations/mediawiki-config/+/refs/heads/master/wmf-config/InitialiseSettings.php#8808
+            command               => '/usr/local/bin/foreachwikiindblist "all - closed - private - fishbowl" extensions/WikimediaEvents/maintenance/UpdatePeriodicMetrics.php --verbose',
+            interval              => '*-*-* 04:40:00',
+            cron_schedule         => '40 4 * * *',
+            team                  => $team_label,
+            script_label          => 'UpdatePeriodicMetrics.php-per-wiki',
+            description           => 'Calculate per-wiki periodic metrics and let them be pulled by Prometheus (T375508)',
+            kubernetes            => true,
+            helmfile_defaults_dir => $helmfile_defaults_dir,
+        }
 
-    profile::mediawiki::periodic_job { 'wikimediaevents-UpdatePeriodicMetrics-global':
-        command               => '/usr/local/bin/mwscript extensions/WikimediaEvents/maintenance/UpdatePeriodicMetrics.php --wiki=metawiki --global-metrics --verbose',
-        interval              => '*-*-* 04:50:00',
-        cron_schedule         => '50 4 * * *',
-        team                  => $team_label,
-        script_label          => 'UpdatePeriodicMetrics.php-global',
-        description           => 'Calculate global periodic metrics and let them be pulled by Prometheus (T375508)',
-        kubernetes            => true,
-        helmfile_defaults_dir => $helmfile_defaults_dir,
+        profile::mediawiki::periodic_job { 'wikimediaevents-UpdatePeriodicMetrics-global':
+            command               => '/usr/local/bin/mwscript extensions/WikimediaEvents/maintenance/UpdatePeriodicMetrics.php --wiki=metawiki --global-metrics --verbose',
+            interval              => '*-*-* 04:50:00',
+            cron_schedule         => '50 4 * * *',
+            team                  => $team_label,
+            script_label          => 'UpdatePeriodicMetrics.php-global',
+            description           => 'Calculate global periodic metrics and let them be pulled by Prometheus (T375508)',
+            kubernetes            => true,
+            helmfile_defaults_dir => $helmfile_defaults_dir,
+        }
     }
 }
