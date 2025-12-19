cc5c815e7ad3352681e3f9f7fa3b6fbb17a3d4b8
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-12-19 15:34:30 +0100

    ci/php: Remove support for buster
    Change-Id: Ie3b263cfc56ba1e9feb5a0a1d0d9440b676d363d

--- modules/profile/manifests/ci/php.pp
+++ modules/profile/manifests/ci/php.pp
@@ -10,21 +10,7 @@
 #   $php_prefix = profile::ci::php::php_prefix   --> 'php7.4'
 #   $php_version = profile::ci::php::php_version --> '7.4'
 class profile::ci::php {
-    if debian::codename::eq('buster') {
-        apt::repository { 'wikimedia-php74':
-            uri        => 'http://apt.wikimedia.org/wikimedia',
-            dist       => "${::lsbdistcodename}-wikimedia",
-            components => 'component/php74',
-        }
-        apt::repository { 'icu67':
-            uri        => 'http://apt.wikimedia.org/wikimedia',
-            dist       => 'buster-wikimedia',
-            components => 'component/icu67',
-        }
-    }
-
     $php_version = debian::codename() ? {
-        'buster'   => '7.4',
         'bullseye' => '7.4',  # provided above by component/php74
         default    => fail("${module_name} not supported by ${debian::codename()}")
     }
