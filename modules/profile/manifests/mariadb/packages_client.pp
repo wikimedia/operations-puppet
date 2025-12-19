e802757eabfc381c5ed6363fadec45401f47c66e
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-12-19 15:33:04 +0100

    mariadb::packages_client: Remove support for buster
    Hosts: cumin1003.eqiad.wmnet, dborch1001.wikimedia.org
    Hosts: deploy2002.codfw.wmnet
    Change-Id: Ic0dcf2a9b070dc103033e7bac54f0f3582f582f8

--- modules/profile/manifests/mariadb/packages_client.pp
+++ modules/profile/manifests/mariadb/packages_client.pp
@@ -7,8 +7,6 @@ class profile::mariadb::packages_client (
         $mariadb_client_package = 'wmf-mariadb106-client'
     } elsif debian::codename::eq('bullseye') {
         $mariadb_client_package = 'wmf-mariadb105-client'
-    } elsif debian::codename::eq('buster') {
-        $mariadb_client_package = 'wmf-mariadb104-client'
     } elsif debian::codename::eq('trixie') {
         $mariadb_client_package = 'wmf-mariadb1011-client'
     } else {
