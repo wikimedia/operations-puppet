630c3692ac7f04ce2ce77fa89cc49d264ab03bca
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-01-06 08:25:25 +0100

    doc: Update PHP spec file to test what's actually used in production
    The doc hosts run Bookworm which has PHP 8.2
    
    Hosts: doc1004.eqiad.wmnet
    Change-Id: I816dd011e177843bb1e5b5ec4bcf1acc25084cfb

--- modules/profile/spec/classes/profile_doc_spec.rb
+++ modules/profile/spec/classes/profile_doc_spec.rb
@@ -1,16 +1,16 @@
 require_relative '../../../../rake_modules/spec_helper'
 
 describe 'profile::doc' do
-  on_supported_os(WMFConfig.test_on(10, 11)).each do |os, facts|
+  on_supported_os(WMFConfig.test_on(12, 12)).each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
       # For Exec[apt-get update] needed by apt::repository
       let(:pre_condition) { "include apt"}
       it { is_expected.to compile }
-      it { is_expected.to contain_package('php7.4-fpm') }
+      it { is_expected.to contain_package('php8.2-fpm') }
       it "provides a fpm restart script" do
         is_expected.to contain_file('/usr/local/sbin/restart-php-fpm-unsafe')
-          .with_content(%r{-- /bin/systemctl restart php7.4-fpm$})
+          .with_content(%r{-- /bin/systemctl restart php8.2-fpm$})
       end
     end
   end
