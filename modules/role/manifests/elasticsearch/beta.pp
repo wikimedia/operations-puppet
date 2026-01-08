3237e6546243bdf9cfd53f364c5a4241cef62736
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2026-01-08 15:48:51 +0100

    elasticsearch: cleanup unused roles/profiles after migration to OpenSearch
    Those roles and profiles aren't used anymore now that we have migrated to OpenSearch
    
    Bug: T388607
    Hosts: O:elasticsearch::beta
    Hosts: P:elasticsearch::cirrus
    Hosts: O:elasticsearch::cirrus
    Hosts: O:elasticsearch::cloudelastic
    Hosts: P:elasticsearch::cloudelastic
    Hosts: O:elasticsearch::relforge
    Change-Id: I76b19643321096e49d91a15e0f0382ce27fd9ccc

--- modules/role/manifests/elasticsearch/beta.pp
+++
@@ -1,7 +0,0 @@
-# = Class: role::elasticsearch::beta
-#
-# This class sets up Elasticsearch specifically for CirrusSearch on deplyoment-prep.
-#
-class role::elasticsearch::beta {
-    include profile::elasticsearch::cirrus
-}
