7e42c5fe285f841e4f928b037c0c5d995a7a55d8
Dawid Pogorzelski <dpogorzelski@wikimedia.org>
2026-01-16 12:14:39 +0100

    ml-build: add missing configs
    This should add required registry credentials to docker-pkg configs
    
    Hosts: ml-build1001.eqiad.wmnet
    Change-Id: Id1733c3a90a03345aab17d259f5d8ee52552be2d

--- modules/profile/manifests/docker/ml_builder.pp
+++ modules/profile/manifests/docker/ml_builder.pp
@@ -12,6 +12,8 @@
 # [*docker_pkg*] Boolean value for enabling the docker_pkg component
 #
 class profile::docker::ml_builder (
+  Optional[Stdlib::Host] $proxy_address = lookup('profile::docker::ml_builder::proxy_address', {default_value => undef}),
+  Optional[Stdlib::Port] $proxy_port = lookup('profile::docker::ml_builder::proxy_port', {default_value => undef}),
   Stdlib::Host $registry = lookup('docker::registry'),
   String $password = lookup('profile::docker::ml_builder::prod_build_password'),
   Boolean $docker_pkg = lookup('profile::docker::ml_builder::docker_pkg', { default_value => false }),
@@ -31,7 +33,15 @@ class profile::docker::ml_builder (
     owner             => 'root',
     group             => 'root',
     registry          => $registry,
-    registry_username => 'CHANGME',
+    registry_username => 'ml-build',
     registry_password => $password,
+  }
+
+  file { '/etc/production-images/config-ml.yaml':
+    ensure  => present,
+    content => template('profile/docker/production-images-config-ml.yaml.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0440'
   }
 }
