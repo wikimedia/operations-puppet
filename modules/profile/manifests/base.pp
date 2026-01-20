acca7a27ce78402bbd8681e1d13e26504882b218
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-01-20 10:50:09 +0100

    Bump the 6.12 backport for Bookworm to 6.12.57
    Bug: T414460
    Change-Id: I21bbb13ac25c6bca941741dda60fb5e357159c66

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -69,7 +69,7 @@ class profile::base (
         apt::package_from_bpo { 'linux-6.12-bookworm':
             packages => {
                 'linux-base'                      => '4.12~bpo12+1',
-                'linux-image-6.12.43+deb12-amd64' => 'present',
+                'linux-image-6.12.57+deb12-amd64' => 'present',
             },
             distro   => 'bookworm',
         }
