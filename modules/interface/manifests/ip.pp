e7eaacf980c1c20215976dc96af9aa380a17db14
Taavi Väänänen <taavi@wikimedia.org>
2026-01-21 20:20:54 +0200

    interface::ip: Use consistent indentation
    Consistently indent with 4 spaces, instead of the current mixture
    between 2 and 4.
    
    Change-Id: Ifb53aed8a91bf493b3792b378341b5af6a6a6964

--- modules/interface/manifests/ip.pp
+++ modules/interface/manifests/ip.pp
@@ -25,40 +25,39 @@ define interface::ip (
     $ipaddr_command = "ip addr add ${prefix} ${options_real}dev ${interface}"
 
     if $ensure == 'absent' {
-      $ipaddr_del_command = "ip addr del ${prefix} dev ${interface}"
+        $ipaddr_del_command = "ip addr del ${prefix} dev ${interface}"
 
-      file_line { "rm_${interface}_${prefix}":
-        ensure            => absent,
-        path              => '/etc/network/interfaces',
-        match             => $ipaddr_command,
-        match_for_absence => true,
-      }
+        file_line { "rm_${interface}_${prefix}":
+            ensure            => absent,
+            path              => '/etc/network/interfaces',
+            match             => $ipaddr_command,
+            match_for_absence => true,
+        }
 
-      exec { $ipaddr_del_command:
-          path    => '/bin:/usr/bin',
-          returns => [0, 2],
-          onlyif  => "ip address show ${interface} | grep -q ${prefix}",
-      }
-
+        exec { $ipaddr_del_command:
+            path    => '/bin:/usr/bin',
+            returns => [0, 2],
+            onlyif  => "ip address show ${interface} | grep -q ${prefix}",
+        }
     } else { # By default, add the IP
-      # Use augeas to add an 'up' command to the interface
-      augeas { "${interface}_${prefix}":
-          incl    => '/etc/network/interfaces',
-          lens    => 'Interfaces.lns',
-          context => "/files/etc/network/interfaces/*[. = '${interface}' and ./family = 'inet']",
-          changes => "set up[last()+1] '${ipaddr_command}'",
-          onlyif  => "match up[. = '${ipaddr_command}'] size == 0";
-      }
+        # Use augeas to add an 'up' command to the interface
+        augeas { "${interface}_${prefix}":
+            incl    => '/etc/network/interfaces',
+            lens    => 'Interfaces.lns',
+            context => "/files/etc/network/interfaces/*[. = '${interface}' and ./family = 'inet']",
+            changes => "set up[last()+1] '${ipaddr_command}'",
+            onlyif  => "match up[. = '${ipaddr_command}'] size == 0";
+        }
 
-      # Add the IP address manually as well
-      exec { $ipaddr_command:
-          path    => '/bin:/usr/bin',
-          returns => [0, 2],
-          unless  => "ip address show ${interface} | grep -q ${prefix}",
-      }
+        # Add the IP address manually as well
+        exec { $ipaddr_command:
+            path    => '/bin:/usr/bin',
+            returns => [0, 2],
+            unless  => "ip address show ${interface} | grep -q ${prefix}",
+        }
 
-      # if the interface is managed by Puppet, ensure it's created first
-      Exec <| tag == "interface-create-${interface}" |>
-        -> Exec[$ipaddr_command]
+        # if the interface is managed by Puppet, ensure it's created first
+        Exec <| tag == "interface-create-${interface}" |>
+            -> Exec[$ipaddr_command]
     }
 }
