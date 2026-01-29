a85e0551e3c675633ded1c2e48de21de7de5fc80
Daniel Zahn <dzahn@wikimedia.org>
2026-01-29 16:31:29 +0000

    cumin/insetup_role_report: send wmcs report to Mark and Levi, not Alex
    Change-Id: I59c1c0fa36b5cc7cb5a409f74c4380d24f7447c0

--- modules/profile/files/cumin/insetup_role_report.py
+++ modules/profile/files/cumin/insetup_role_report.py
@@ -25,7 +25,8 @@ OWNER_PREFIX = 'Those are the audit reports sent to the various owners.\n\n'
 MAPPING = {
     'calbon': ['machine_learning'],
     'glederrey': ['data_platform'],
-    'akosiaris': ['wmcs'],
+    'mbergsma': ['wmcs'],
+    'lferreira': ['wmcs'],
     'kappakayala': ['data_persistence', 'serviceops', 'O:insetup::container'],
     'kofori': ['data_persistence', 'traffic', 'O:insetup_noferm'],
     'ssingh': ['traffic', 'O:insetup_noferm'],
