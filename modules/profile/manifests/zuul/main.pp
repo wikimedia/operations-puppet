d3d1ebc7a2783f669342383c21bea1fda29a4fbc
Daniel Zahn <dzahn@wikimedia.org>
2026-02-05 12:26:24 -0800

    zuul: parameterize and configure new config dir for zookeeper-zuul
    Parameterize and use the new shared config dir created
    in I3e3637e8672f.
    
    It's for shared access of zookeeper and zuul for mTLS.
    
    Bug: T395938
    Change-Id: I4566a14c90227dc019c7827db983a8ddd0eca836

--- modules/profile/manifests/zuul/main.pp
+++ modules/profile/manifests/zuul/main.pp
@@ -2,7 +2,9 @@
 # new zuul (T393873) - main nodes
 class profile::zuul::main(
     String $ssl_password = lookup('profile::zuul::main::ssl_password'),
+    Stdlib::Unixpath $tls_config_dir = lookup('profile::zuul::main::tls_config_dir'),
 ){
+
     # let zuul see and validate gerrit server ssh host keys
     # otherwise zuul will accept any key provided (T395938#10929023)
     # link into existing ssh_known_hosts populated by puppet
@@ -13,7 +15,7 @@ class profile::zuul::main(
 
     # write the TLS passphrase to a file so we can point
     # zookeeper to it with ssl.keyStore.passwordPath
-    file { '/etc/zookeeper/conf/zuul_tls':
+    file { "${tls_config_dir}/tls_password":
         ensure  => 'file',
         content => $ssl_password,
         owner   => 'zookeeper',
@@ -38,7 +40,7 @@ class profile::zuul::main(
     }
 
     # shared config dir for zookeeper-zuul mTLS
-    file { '/etc/zookeeper/zuul-tls':
+    file { $tls_config_dir:
         ensure  => 'directory',
         owner   => 'zookeeper',
         group   => 'zuul',
@@ -48,6 +50,7 @@ class profile::zuul::main(
 
     $tls_paths = profile::pki::get_cert('zuul', 'zuul', {
         'owner'           => 'zookeeper',
+        'outdir'          => $tls_config_dir,
         'notify_services' => ['zookeeper'],
     })
 
@@ -55,7 +58,6 @@ class profile::zuul::main(
     $zookeeper_tls_chained = $tls_paths['chained']
     $zookeeper_tls_key = $tls_paths['key']
     $zookeeper_tls_ca = $tls_paths['chain']
-    # $tls_outdir = dirname($tls_paths['cert'])
 
     # convert to PKCS12 format for Java and let zookeeper read it
     sslcert::x509_to_pkcs12 { 'zookeeper_zuul_keystore' :
@@ -64,7 +66,7 @@ class profile::zuul::main(
         public_key  => $zookeeper_tls_chained,
         private_key => $zookeeper_tls_key,
         certfile    => $zookeeper_tls_ca,
-        outfile     => '/etc/zookeeper/conf/zookeeper_zuul.keystore.p12',
+        outfile     => "${tls_config_dir}/zookeeper_zuul.keystore.p12",
         password    => $ssl_password,
         notify      => Service['zookeeper'],
     }
