dc52b6f68678bcda7dd3476075a770fc23f808a2
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-02-13 14:23:52 +0100

    Remove Puppet references for sretest1003
    Also also 1001, which is long long and 1004, which is also being cleaned
    up.
    
    Bug: T417392
    Change-Id: Idac4f6850889a65976897afd2f42338c5258fafa

--- manifests/site.pp
+++ manifests/site.pp
@@ -2436,7 +2436,7 @@ node /^stat10(0[8-9]|1[0-1])\.eqiad\./ {
 }
 
 # Servers for SRE tests which are not suitable for Cloud VPS
-node /^sretest100[2-6]\.eqiad\./ {
+node /^sretest100[256]\.eqiad\./ {
     role(sretest)
 }
 
