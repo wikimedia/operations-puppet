ec4c48556ec62d0503d248c7d8599ef69a6cce46
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-02-16 09:45:39 +0100

    Run Puppetboard spec tests on Bookworm
    Puppetboard is on Bookworm, so test the OS actually in use.
    
    Change-Id: I4f55a24844b73312aa5445c86fecdae373799e52

--- modules/puppetboard/spec/classes/puppetboard_spec.rb
+++ modules/puppetboard/spec/classes/puppetboard_spec.rb
@@ -2,7 +2,7 @@
 require_relative '../../../../rake_modules/spec_helper'
 
 describe 'puppetboard' do
-  on_supported_os(WMFConfig.test_on(10)).each do |os, facts|
+  on_supported_os(WMFConfig.test_on(12, 12)).each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
       # let(:params) { {} }
