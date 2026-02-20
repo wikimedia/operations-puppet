6f9af606a4f7b4cf7b8e0d339911603ff662dc7d
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-02-20 11:41:37 +0100

    civicrm: Run spec tests on Bookworm
    crm2001 is running Bookworm
    
    Change-Id: I8720b5da3c1ed357354cc6ec6d832f8432a7a696

--- modules/profile/spec/classes/profile_community_civicrm_spec.rb
+++ modules/profile/spec/classes/profile_community_civicrm_spec.rb
@@ -2,7 +2,7 @@
 require_relative '../../../../rake_modules/spec_helper'
 
 describe 'profile::community_civicrm' do
-  on_supported_os(WMFConfig.test_on(11)).each do |os, os_facts|
+  on_supported_os(WMFConfig.test_on(12, 12)).each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
       let(:params) do
