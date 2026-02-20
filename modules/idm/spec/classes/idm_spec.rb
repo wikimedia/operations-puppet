d89c1cf49b75f37062eba9487662e84a094bfa49
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-02-20 08:18:28 +0100

    Run IDM spec tests on Bookworm/Trixie
    Instead of Buster/Bullseye, these are currently on Bookworm and also
    add Trixie for the next update.
    
    Change-Id: I86d33b77f0396b7642520fdfda8b7a03db4c9f55

--- modules/idm/spec/classes/idm_spec.rb
+++ modules/idm/spec/classes/idm_spec.rb
@@ -2,7 +2,7 @@
 require_relative '../../../../rake_modules/spec_helper'
 
 describe 'idm::jobs' do
-  on_supported_os(WMFConfig.test_on).each do |os, os_facts|
+  on_supported_os(WMFConfig.test_on(12, 13)).each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
       let(:params) do
