b57c7651f235105614940b7f6fa95e3035f5d072
Antoine Musso <hashar@free.fr>
2026-02-20 11:26:19 +0100

    zuul: pin spec tests to Bullseye (11)
    The Zuul module is used by the legacy Zuul system that is running on the
    contint* servers.  Those are using Bullseye and we are not going to
    upgrade them since the legacy Zuul will be entirely replaced by a newer
    Zuul on a different infrastructure.
    
    Pin the min and max of `WMFConfig.test_on` to version 11 (we need both
    args else the second arg max=11 would not be overriden). This change
    stops running the spec against Buster (10) or any future version when
    the global `WMFConfig.test_on` `max` is updated.
    
    Change-Id: Ib95201407a55b5ec8a89ac6a32e2bfe791389687

--- modules/zuul/spec/classes/zuul_spec.rb
+++ modules/zuul/spec/classes/zuul_spec.rb
@@ -1,7 +1,9 @@
 require_relative '../../../../rake_modules/spec_helper'
 
 describe 'zuul' do
-    on_supported_os(WMFConfig.test_on).each do |os, facts|
+    # Only tested on Bullseye (11) which is the OS version of contint server.
+    # The whole system will be replaced with a newer Zuul on a different infra.
+    on_supported_os(WMFConfig.test_on(11, 11)).each do |os, facts|
       context "On #{os}" do
         let(:facts) { facts }
         let(:pre_condition) {
