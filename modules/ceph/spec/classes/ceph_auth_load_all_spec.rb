0e6c50a17423add6241ebc5574fc4b54d690854d
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-02-23 10:28:40 +0100

    Run the cephosd spec tests on Bookworm
    These currently run only for Buster/Bullseye, which cephosd actually
    runs Bookworm.
    
    Change-Id: I223fa357274aa8a52588d0acb69310325ac8df1b

--- modules/ceph/spec/classes/ceph_auth_load_all_spec.rb
+++ modules/ceph/spec/classes/ceph_auth_load_all_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../../../rake_modules/spec_helper'
 
 describe 'ceph::auth::load_all' do
-  on_supported_os(WMFConfig.test_on(10)).each do |os, os_facts|
+  on_supported_os(WMFConfig.test_on(12, 12)).each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
       let(:params) do
