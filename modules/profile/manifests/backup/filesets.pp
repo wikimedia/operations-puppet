dd9f40db57211ae55759052a7eb08a7d10077a2d
Daniel Zahn <dzahn@wikimedia.org>
2026-02-24 09:34:08 -0800

    backup: adjust gerrit file set after renaming of gerrit2
    On all 3 gerrit servers, /var/lib/gerrit2 does not exist anymore now.
    
    But /var/lib/gerrit does.
    
    The confusing part: we got a report that backups are failing with
    "/var/lib/gerrit" - no such file or directory.
    
    Bug: T417247
    Bug: T338470
    Change-Id: I49f7037c50c3012519b60cba42e3a831713f39f2

--- modules/profile/manifests/backup/filesets.pp
+++ modules/profile/manifests/backup/filesets.pp
@@ -37,7 +37,7 @@ class profile::backup::filesets(
         includes     => [ '/var/lib/archiva' ],
     }
     bacula::director::fileset { 'gerrit-repo-data':
-        includes => [ '/srv/gerrit', '/var/lib/gerrit2', '/var/lib/gerrit' ]
+        includes => [ '/srv/gerrit', '/var/lib/gerrit' ]
     }
     bacula::director::fileset { 'srv-carbon-whisper-coal':
         includes => [ '/srv/carbon/whisper/coal' ]
