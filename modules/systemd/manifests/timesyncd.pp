7d2faf9ad66f27c70cd5aa0ec47b94836b56e9e9
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-02-25 12:18:14 +0100

    Install systemd-timesyncd universally
    All Buster hosts are gone.
    
    PCC below for a Bullseye (cp), Bookworm (cumin) and Trixie (durum) host
    
    Hosts: cp1103.eqiad.wmnet, cumin2002.codfw.wmnet, durum1001.eqiad.wmnet
    Change-Id: Ibf1d630996048db302960388a6733bd0b1338306

--- modules/systemd/manifests/timesyncd.pp
+++ modules/systemd/manifests/timesyncd.pp
@@ -8,12 +8,8 @@ class systemd::timesyncd (
     # only purge ntp if we are ensuring timesyncd
     if $ensure == 'present' {
         ensure_packages(['ntp'], {'ensure' => 'purged'})
-        # On systemd >247.3-6~bpo10+1 at least this service is handled by the systemd-timesyncd package
-        # before it was installed by systemd
-        if (debian::codename::ge('bullseye')) {
-            ensure_packages(['systemd-timesyncd'])
-            Package['systemd-timesyncd'] -> File['/etc/systemd/timesyncd.conf']
-        }
+        ensure_packages(['systemd-timesyncd'])
+        Package['systemd-timesyncd'] -> File['/etc/systemd/timesyncd.conf']
     }
 
     file { '/etc/systemd/timesyncd.conf':
@@ -24,11 +20,10 @@ class systemd::timesyncd (
         content => template('systemd/timesyncd.conf.erb'),
     }
 
-    # Ideally we could just rely on stdlib::ensure below. However this causes an error when:
-    # * ensure => 'absent'
-    # * debian::codename::ge bullseye
-    # As in this case there is no systemd-timesync service available to manage.
-    if (defined(Package['systemd-timesyncd']) or $ensure == 'present') {
+    # Ideally we could just rely on stdlib::ensure below. However this causes an error
+    # if the service is absented as in this case there is no systemd-timesync service
+    # available to manage.
+    if $ensure == 'present' {
         service { 'systemd-timesyncd':
             ensure => stdlib::ensure($ensure, 'service'),
             enable => true,
