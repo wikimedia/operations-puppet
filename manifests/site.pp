2596c0b6ca332875df8a6a842ef3c0f86c58e5c2
Daniel Zahn <dzahn@wikimedia.org>
2026-02-26 10:23:48 -0800

    site: add contint1003/2003 with insetup collab role
    Bug: T418521
    Bug: T418544
    Bug: T418545
    Change-Id: I6dac0445639b35b9383b64f71a276520ed33ade1

--- manifests/site.pp
+++ manifests/site.pp
@@ -431,6 +431,10 @@ node /^(contint1002|contint2002)\.wikimedia\./ {
     role(ci)
 }
 
+node /^(contint1003|contint2003)\.wikimedia\./ {
+    role(insetup::collaboration_services_nftables)
+}
+
 node /^cp11(0[02468]|1[024])\.eqiad\./ {
     role(cache::text)
 }
