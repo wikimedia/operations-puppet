35e7c25a15b571620187fcf6ee21e2d679e5b034
Chris Dobbins <cdobbins@wikimedia.org>
2026-02-26 15:09:57 -0500

    site.pp: add cp hosts to text & upload clusters
    Change-Id: Idd1733e8265d93ad9fe274a9d82da92d62accc93

--- manifests/site.pp
+++ manifests/site.pp
@@ -447,11 +447,6 @@ node /^cp20(28|3[02468]|[45][02468])\.codfw\./ {
     role(cache::upload)
 }
 
-# New nodes - T392851
-node /^cp20(4[5-9]|5[0-8])\.codfw\./ {
-    role(insetup_noferm)
-}
-
 #
 # esams caches
 #
