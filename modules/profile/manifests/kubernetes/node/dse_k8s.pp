e612c13daa58e1b70458e98ec4f14f495611f747
Ben Tullis <btullis@wikimedia.org>
2026-02-27 17:11:59 +0000

    Add a /srv/spark managed directory on dse-k8s-worker nodes
    This will be used for a temporary location by spark applications running
    under kubernetes.
    
    Hosts: P:kubernetes::node::dse_k8s
    Bug: T412925
    Change-Id: I26196f42003e6b2ba32157431d6d5fbae981f34a

--- modules/profile/manifests/kubernetes/node/dse_k8s.pp
+++ modules/profile/manifests/kubernetes/node/dse_k8s.pp
@@ -7,4 +7,10 @@ class profile::kubernetes::node::dse_k8s (
             'vm.max_map_count'          => 262144,
         }
     }
+
+    # This directory can be mounted by certain pods running in this cluster in order to support spark
+    # local files. See https://spark.apache.org/docs/3.5.7/running-on-kubernetes.html#local-storage and #T412925
+    file { '/srv/spark':
+        ensure => directory,
+    }
 }
