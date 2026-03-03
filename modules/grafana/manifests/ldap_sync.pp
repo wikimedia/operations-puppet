d41b254ed187b973e15767433ce8a8c5e5e14bf0
Tiziano Fogli <tfogli@wikimedia.org>
2026-03-03 09:38:39 +0000

    Revert "grafana::ldap_sync: disable systemd timer (TMP)"
    This reverts commit 57471a892c258e4c4a836783b4be8b3dbec82a4e.
    
    Reason for revert: Test finished — re-enable the timer.
    
    Change-Id: I14615586c7e21c25bb00d600fc9a468f3faf814e

--- modules/grafana/manifests/ldap_sync.pp
+++ modules/grafana/manifests/ldap_sync.pp
@@ -1,19 +1,19 @@
 # SPDX-License-Identifier: Apache-2.0
 class grafana::ldap_sync (
-    Wmflib::Ensure $ensure,
+  Wmflib::Ensure $ensure,
 ) {
     ensure_packages(['python3-ldap', 'python3-requests'])
 
     file { '/usr/local/bin/grafana-ldap-users-sync':
-        ensure => file,
+        ensure => present,
         source => 'puppet:///modules/grafana/ldap_users_sync.py',
         mode   => '0555',
         owner  => 'root',
         group  => 'root',
     }
 
-    systemd::timer::job { 'grafana-ldap-users-sync':
-        ensure      => absent,
+    systemd::timer::job{ 'grafana-ldap-users-sync':
+        ensure      => $ensure,
         description => 'Sync users and roles from LDAP to Grafana',
         command     => '/usr/local/bin/grafana-ldap-users-sync --commit --delete-users',
         interval    => {
