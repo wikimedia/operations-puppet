9492c08bacda02d079c55fcb0d6feccd0f94e57c
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-03-03 10:44:19 +0100

    standard_packages: Remove more obsolete packages after bullseye->bookworm update
    On systems which got dist-upgraded instead of reimaged (e.g. prometheus or
    Cumin hosts) some library packages with old sonames are still installed,
    but no longer receive updates, also uninstall them:
    
    - libbpf0 got replaced by libbpf1
    
    Change-Id: I9c643072e189eb2be96df545eced181174822ec9

--- modules/base/manifests/standard_packages.pp
+++ modules/base/manifests/standard_packages.pp
@@ -120,7 +120,7 @@ class base::standard_packages (
                 'libwiretap11', 'ruby2.7', 'python3.9-minimal', 'python3.9', 'perl-modules-5.32', 'libpython3.9',
                 'libperl5.32', 'libpython3.9-minimal', 'libpython3.9-stdlib', 'libidn11', 'libldap-2.4-2',
                 'liburing1', 'libwebp6', 'libcbor0', 'libusb-0.1-4', 'telnet', 'libruby2.7', 'libdns-export1110',
-                'libisc-export1105',
+                'libisc-export1105', 'libbpf0',
             ]
             $purged_packages = []
         }
