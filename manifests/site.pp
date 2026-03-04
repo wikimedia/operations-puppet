40d34e7415495dde1bf08edc0e8c28c731df58e8
Clément Goubert <cgoubert@wikimedia.org>
2026-03-04 12:14:08 +0100

    Add new wikikube-ctrl100[56] hosts
    - site.pp: Add insetup::serviceops_ferm
    - preseed.yaml: Use UEFI partman recipe
    
    Bug: T418919
    Change-Id: I79edcd1cc8af4d0b5e73d5b4814027a53df78953

--- manifests/site.pp
+++ manifests/site.pp
@@ -1367,6 +1367,12 @@ node /^wikikube-ctrl100[2-4]\.eqiad\./ {
     role(kubernetes::master_stacked)
 }
 
+# BEGIN T418919 wikikube-ctrl100[56]
+node /^wikikube-ctrl100[5-6]\.eqiad\./ {
+    role(insetup::serviceops_ferm)
+}
+# END T418919 wikikube-ctrl100[56]
+
 node /^wikikube-ctrl200[1-3]\.codfw\./ {
     role(kubernetes::master_stacked)
 }
