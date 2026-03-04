6275208f2dcd0a8d58a18cbfd055d4476c91520e
Daniel Zahn <dzahn@wikimedia.org>
2026-03-04 14:07:53 -0800

    ci::website: support 2 different websites, integration vs zuul-legacy
    Modify the existing ci::website class in a way that allows us to have
    2 different websites.
    
    integration.wikimedia.org and zuul-legacy.wikimedia.org
    
    The site_name is obvious but whether they also have separate scap
    targets and deploy users, maybe not so much yet.
    
    Bug: T418521
    Change-Id: I5c37c1ddb05187033f1ed4d71db18f9baec288da

--- modules/profile/manifests/ci/website.pp
+++ modules/profile/manifests/ci/website.pp
@@ -1,16 +1,37 @@
 # SPDX-License-Identifier: Apache-2.0
-# Website for Continuous integration
+# Websites for Continuous integration
 #
-# http://integration.wikimedia.org/
-class profile::ci::website {
+# https://integration.wikimedia.org/ or
+# https://zuul-legacy.wikimedia.org/
+#
+class profile::ci::website(
+    String $type = lookup('profile::ci::website::type',
+    {default_value => 'integration'}),
+){
 
-    scap::target { 'integration/docroot':
-        deploy_user => 'deploy-ci-docroot',
+    case $type {
+        'integration': {
+            $scap_target = 'integration/docroot'
+            $deploy_user = 'deploy-ci-docroot'
+            $site_name = 'integration.wikimedia.org'
+        }
+        'zuul-legacy': {
+            $scap_target = 'integration/docroot'
+            $deploy_user = 'deploy-ci-docroot'
+            $site_name = 'zuul-legacy.wikimedia.org'
+        }
+        default: {
+            fail("Unsupported type: ${type}")
+        }
     }
 
-    # Apache configuration for integration.wikimedia.org
-    httpd::site { 'integration.wikimedia.org':
-        source => 'puppet:///modules/contint/apache/integration.wikimedia.org.conf'
+    scap::target { $scap_target:
+        deploy_user => $deploy_user,
+    }
+
+    # Apache configuration
+    httpd::site { $site_name:
+        source => "puppet:///modules/contint/apache/${site_name}.conf"
     }
 
     profile::auto_restarts::service { 'envoyproxy': }
