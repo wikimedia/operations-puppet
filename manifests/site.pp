d8295f642a067589075187b1c7d94d21d9d270ac
Tiziano Fogli <tfogli@wikimedia.org>
2026-03-10 10:19:44 +0100

    prom4003: assign prometheus::pop role
    Bug: T419430
    Change-Id: I21777a86440641ebfbf85fc57c7d128dee90fa5d

--- manifests/site.pp
+++ manifests/site.pp
@@ -2374,10 +2374,6 @@ node /^prometheus100[5678]\.eqiad\./ {
     role(prometheus)
 }
 
-node /^prometheus4003\.ulsfo\./ {
-    role(insetup::observability_ferm)
-}
-
 node /^prometheus[34567]00[1-9]\.(esams|ulsfo|eqsin|drmrs|magru)\./ {
     role(prometheus::pop)
 }
