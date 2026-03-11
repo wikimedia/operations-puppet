ef04724d4aa924cc75d8bee8c00485202a892a33
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-03-11 08:56:46 +0100

    Add netflow4003
    Assign the netinsights role and allow for the Kafka brokers.
    This will replace netflow4002. The Homer change will be deployed once
    this is in place.
    
    Bug: T418993
    Change-Id: I8269fea968ed8be5f9ad1a5cb58bef9ca11d015f

--- manifests/site.pp
+++ manifests/site.pp
@@ -2241,10 +2241,6 @@ node /^netmon[0-9]{4}\.wikimedia\./ {
     role(netmon)
 }
 
-node /^netflow4003\.ulsfo\./ {
-    role(insetup::infrastructure_foundations_nftables)
-}
-
 # Network insights (netflow/pmacct, etc.)
 node /^netflow[1-7]00[1-9]\.(eqiad|codfw|ulsfo|esams|eqsin|drmrs|magru)\./ {
     role(netinsights)
