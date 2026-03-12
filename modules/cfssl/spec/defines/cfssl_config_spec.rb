fc0990c561954d4f14d80f470d3a936dafd08d8a
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-03-12 14:58:05 +0100

    cfssl: Run tests on Bullseye and Trixie
    The current PKI servers run Bullseye, the new ones Trixie.
    
    And remove one rather pointless test, which isn't really useful.
    
    Change-Id: Iac463f62ef8cd5029a8033f4754508b1f25035ef

--- modules/cfssl/spec/defines/cfssl_config_spec.rb
+++ modules/cfssl/spec/defines/cfssl_config_spec.rb
@@ -2,7 +2,7 @@
 require_relative '../../../../rake_modules/spec_helper'
 
 describe 'cfssl::config' do
-  on_supported_os(WMFConfig.test_on).each do |os, facts|
+  on_supported_os(WMFConfig.test_on(11, 13)).each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
       let(:title) { 'test_config' }
