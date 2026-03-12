a470743c955e6c32b2043d5c01633c1392609633
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-03-12 14:30:12 +0100

    varnish: Run spec tests on Bullseye and Trixie
    These currently only run on Buster, which no longer exists in our CI infra.
    
    Instead test on Bullseye (what the current cache hosts run) and Trixie
    (what the cache hosts are being migrated to).
    
    Change-Id: I055a1f7cf48c3fc8a726c093ee12a2752aa90381

--- modules/profile/spec/classes/profile_cache_varnish_frontend_spec.rb
+++ modules/profile/spec/classes/profile_cache_varnish_frontend_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../../../rake_modules/spec_helper'
 describe 'profile::cache::varnish::frontend' do
-  on_supported_os(WMFConfig.test_on(10)).each do |os, os_facts|
+  on_supported_os(WMFConfig.test_on(11, 13)).each do |os, os_facts|
     ['cache/upload', 'cache/text'].each do |cluster|
       context "on #{os} (#{cluster})" do
         let(:facts) { os_facts }
