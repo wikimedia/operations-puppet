bfa0888133e837ba3394d6e727d6b6a3d322d97e
Btullis <btullis@wikimedia.org>
2026-03-13 11:34:16 +0000

    Revert "Temporarily puto dse-k8s-worker101[8-9] into insetup mode"
    This reverts commit 350e03e0213da3ed2fd7a7d524c73c11cdd5cae8.
    
    Reason for revert: Reimage complete
    
    Change-Id: Ib846dad7c7f8d4d1b6aa023303c0510905734933

--- manifests/site.pp
+++ manifests/site.pp
@@ -1035,13 +1035,8 @@ node /^dse-k8s-etcd200[1-3]\.codfw\./ {
 }
 
 # dse-k8s-workers T29157, T3074009, T394647, T395557, T398438, T405209
-node /^dse-k8s-worker10(0[1-9]|1[0-7]|2[4-8])\.eqiad\./ {
+node /^dse-k8s-worker10(0[1-9]|1[0-9]|2[4-8])\.eqiad\./ {
     role(dse_k8s::worker)
-}
-
-# Temporarily set dse-k8s-worker101[8-9] into insetup mode
-node /^dse-k8s-worker101[8-9]\.eqiad\./ {
-    role(insetup::data_platform_ferm)
 }
 
 # New dse-k8s-workers in eqiad - See T414216 and T414948
