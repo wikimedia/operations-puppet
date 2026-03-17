fee5bd092a3e3953de79ecf2e9f6f9da85dfd577
Fabrizio Furnari <ffurnari@wikimedia.org>
2026-03-17 14:25:02 +0100

    haproxy: test haproxy32 on cp2041
    Note: as we don't use haproxy31 anywhere the haproxy.cfg template has
    changed too.
    
    Hosts: cp2041.codfw.wmnet,cp2048.codfw.wmnet
    Bug: T419825
    Change-Id: I50432d7b5093e9605c691e2a4ccd56c8a4a112f7

--- modules/haproxy/types/version.pp
+++ modules/haproxy/types/version.pp
@@ -1,2 +1,2 @@
 # SPDX-License-Identifier: Apache-2.0
-type Haproxy::Version = Enum['haproxy24', 'haproxy26', 'haproxy27', 'haproxy28', 'haproxy30', 'haproxy31']
+type Haproxy::Version = Enum['haproxy24', 'haproxy26', 'haproxy27', 'haproxy28', 'haproxy30', 'haproxy31', 'haproxy32']
