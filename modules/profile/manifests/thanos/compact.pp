0c5419aaf85bf727a2b069b84d87c2dbad8abeb5
Tiziano Fogli <tfogli@wikimedia.org>
2026-03-24 17:03:29 +0100

    thanos/compact: add support for instance-based partitioning
    Taking advantage of the prometheus::instances variable to define,
    at the instance level, which Titan node should perform compaction
    for its blocks.
    
    Restart or reload operations triggered by changes to the relabel config
    file must be performed via the sre.o11y.thanos-compact-restart cookbook.
    This prevents multiple compactor instances from working on the same
    blocks simultaneously.
    
    Bug: T386911
    Hosts: titan2001.codfw.wmnet
    Hosts: titan1001.eqiad.wmnet
    Change-Id: Idc0727c804919fa2a87e56656d55466e3b030b7e

--- modules/profile/manifests/thanos/compact.pp
+++ modules/profile/manifests/thanos/compact.pp
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::thanos::compact (
-    Stdlib::Fqdn $thanos_compact_host = lookup('profile::thanos::compact_host'),
     Hash[String, String] $objstore_account = lookup('profile::thanos::objstore_account'),
     String $objstore_password = lookup('profile::thanos::objstore_password'),
     String $retention_raw = lookup('profile::thanos::retention::raw', { 'default_value' => '270w' }),
@@ -12,7 +11,6 @@ class profile::thanos::compact (
     $http_port = 12902
 
     class { 'thanos::compact':
-        run_on_host                  => $thanos_compact_host,
         objstore_account             => $objstore_account,
         objstore_password            => $objstore_password,
         http_port                    => $http_port,
@@ -21,9 +19,5 @@ class profile::thanos::compact (
         retention_1h                 => $retention_1h,
         concurrency                  => $concurrency,
         block_meta_fetch_concurrency => $block_meta_fetch_concurrency,
-    }
-
-    if $thanos_compact_host == $::fqdn {
-        class { 'thanos::compact::prometheus': }
     }
 }
--- modules/thanos/manifests/compact.pp
+++ modules/thanos/manifests/compact.pp
@@ -17,7 +17,6 @@
 # [*block_meta_fetch_concurrency*] Number of goroutines to fecth block metadata
 
 class thanos::compact (
-    Stdlib::Fqdn $run_on_host,
     Hash[String, String] $objstore_account,
     String $objstore_password,
     Wmflib::Ensure $ensure = present,
@@ -25,7 +24,7 @@ class thanos::compact (
     String $retention_5m = '60w',
     String $retention_1h = '60w',
     Stdlib::Port::Unprivileged $http_port = 12902,
-    Integer $concurrency = max($::processorcount / 2, 1),
+    Integer $concurrency = max($facts['processors']['count'] / 2, 1),
     Integer $block_meta_fetch_concurrency = 32,
 ) {
     ensure_packages(['thanos'])
@@ -34,6 +33,25 @@ class thanos::compact (
     $service_name = 'thanos-compact'
     $data_dir = '/srv/thanos-compact'
     $objstore_config_file = '/etc/thanos-compact/objstore.yaml'
+    $relabel_config_file = '/etc/thanos-compact/relabel.yaml'
+
+    $compactor_eligible_hosts = wmflib::role::hosts('titan')
+
+    $ruler_blocks_designated_compactor = $compactor_eligible_hosts[0] == $facts['networking']['fqdn']
+
+    prometheus::instances().each |$instance_name, $instance_config| {
+        if !($instance_config['designated_compactor'] in $compactor_eligible_hosts) {
+            fail("Instance ${instance_name} has designated_compactor ${instance_config['designated_compactor']} which is not eligible to run compactor")
+        }
+    }
+
+    $owned_instances = (prometheus::instances().reduce([]) |$memo, $data| {
+            if $data[1]['designated_compactor'] == $facts['networking']['fqdn'] {
+                $memo + [$data[0]]
+            } else {
+                $memo
+            }
+    }).sort().unique()
 
     file { $data_dir:
         ensure => directory,
@@ -49,6 +67,20 @@ class thanos::compact (
         group  => 'root',
     }
 
+    file { "${relabel_config_file}.unreferenced":
+        ensure  => stdlib::ensure($ensure, 'file'),
+        mode    => '0550',
+        owner   => 'thanos',
+        group   => 'root',
+        content => template('thanos/compact-relabel.yaml.erb'),
+    }
+
+    exec { 'orchestrated restart needed':
+      command => "/bin/sh -c 'cmp -s \"${relabel_config_file}\" \"${relabel_config_file}.unreferenced\" || (echo \"Please run the sre.o11y.thanos-compact-restart cookbook\" >&2; exit 1)'",
+      path    => ['/bin', '/usr/bin'],
+      require => [File["${relabel_config_file}.unreferenced"], File[$objstore_config_file]],
+    }
+
     file { $objstore_config_file:
         ensure    => $ensure,
         mode      => '0440',
@@ -60,10 +92,11 @@ class thanos::compact (
 
     if $ensure != present {
         $service_ensure = $ensure
-    } else { # handle fqdn-based singleton service
-        if $run_on_host == $::fqdn {
+    } else {
+        if length($owned_instances) > 0 {
             $service_ensure = 'present'
             $service_enable = true
+            class { 'thanos::compact::prometheus': }
         } else {
             $service_ensure = 'absent'
             $service_enable = false
