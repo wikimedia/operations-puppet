4dcb80baf96cc66661505c43e6ed22fa262ff6a6
Daniel Zahn <dzahn@wikimedia.org>
2026-03-25 10:53:50 -0700

    ci: Add 'Signed-by' keyfile reference to thirdparty APT repo config
    From bookworm onwards, the apt sources must use a `Signed-by:` line.
    
    With multiple config snippets under /etc/apt/sources.list.d/ where
    some have the Signed-by line and some do not while all point to the
    same URL we are getting:
    
    "E: Conflicting values set for option Signed-By regarding source.."
    
    and an apt-get update fails with "E: The list of sources could not be
    read.".
    
    Debugging this I found Ibaa8891882d9ace8f8 where this same problem
    was fixed for what appears to be the only other "thirdparty/*" used
    with apt::repository, bigtop.
    
    So copied the fix from there.
    
    Bug: T418109
    Change-Id: Idb07eac94fad463d18db131cf225192a9797a469

--- modules/profile/manifests/ci/thirdparty_apt.pp
+++ modules/profile/manifests/ci/thirdparty_apt.pp
@@ -1,11 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
+# thirdparty/ci is used on contint/bullseye to provide a newer Docker package AND Jenkins.
+#
+# Since Bookworm and later provide a recent enough Docker, we only need it for
+# Jenkins and thirdparty/ci is now named thirdparty/jenkins (which
+# should be configured independently).
 class profile::ci::thirdparty_apt {
-
-    # thirdparty/ci is used on contint to provide a newer Docker package AND Jenkins.
-    #
-    # Since Bookworm provides a recent enough Docker, we only need it for
-    # Jenkins and thirdparty/ci is now named thirdparty/jenkins (which
-    # should be configured independently)
 
     if debian::codename::lt('bookworm') {
         apt::repository { 'thirdparty-ci':
@@ -14,10 +13,12 @@ class profile::ci::thirdparty_apt {
             components => 'thirdparty/ci',
         }
     } else {
+        # From bookworm onwards, the apt sources must use a `Signed-by:` line.
         apt::repository { 'thirdparty-jenkins':
-            uri        => 'http://apt.wikimedia.org/wikimedia',
-            dist       => "${::lsbdistcodename}-wikimedia",
-            components => 'thirdparty/jenkins',
+            uri          => 'http://apt.wikimedia.org/wikimedia',
+            dist         => "${::lsbdistcodename}-wikimedia",
+            components   => 'thirdparty/jenkins',
+            keyfile_path => '/etc/apt/keyrings/wikimedia-archive-keyring',
         }
     }
 }
