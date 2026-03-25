abc1f4f57135615c2aef77c793f34a21c58fd199
Taavi Väänänen <taavi@wikimedia.org>
2026-03-25 15:20:52 +0200

    P:opensearch::cirrus::test: Convert port to an integer
    Ensure the firewall::service definition will pass validation with both
    firewall providers.
    
    Change-Id: I88ac74c0c256928744d69513d71c7a7d6a6a6964

--- modules/profile/manifests/opensearch/cirrus/test.pp
+++ modules/profile/manifests/opensearch/cirrus/test.pp
@@ -3,12 +3,12 @@ class profile::opensearch::cirrus::test () {
     firewall::service {
         'opensearch-http-9200':
             proto    => 'tcp',
-            port     => '9200',
+            port     => [9200],
             src_sets => ['ANALYTICS_NETWORKS'],
         ;
         'opensearch-inter-node-9300':
             proto  => 'tcp',
-            port   => '9300',
+            port   => [9300],
             srange => wmflib::role::hosts('cirrus::test'),
         ;
     }
