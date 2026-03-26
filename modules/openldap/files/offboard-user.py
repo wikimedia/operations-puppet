d414eddf863c206e686cd53fd21cb13dc88519a2
Ben Tullis <btullis@wikimedia.org>
2026-03-26 15:30:51 +0000

    Add an LDAP group to the list considered during offboarding
    We have added another LDAP group for the new fr-tech airflow instance.
    This needs to be added to the list of groups that is checked during
    offboarding.
    
    Bug: T417213
    Change-Id: Ia43615e65848c25f7cef98787fdbbe872af77d54

--- modules/openldap/files/offboard-user.py
+++ modules/openldap/files/offboard-user.py
@@ -178,6 +178,7 @@ delete: userPassword
                          'cn=airflow-analytics-product-ops,ou=groups,dc=wikimedia,dc=org',
                          'cn=airflow-ml-ops,ou=groups,dc=wikimedia,dc=org',
                          'cn=airflow-wikidata-ops,ou=groups,dc=wikimedia,dc=org',
+                         'cn=airflow-fr-tech-ops,ou=groups,dc=wikimedia,dc=org',
                          'cn=spiderpig-access,ou=groups,dc=wikimedia,dc=org',
                          'cn=logstash-access,ou=groups,dc=wikimedia,dc=org',
                          'cn=bitu-account-managers,ou=groups,dc=wikimedia,dc=org',
