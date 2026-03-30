ba0220c403a98a5a4bc4cc7673a313a1d78742d9
Brett Cornwall <bcornwall@wikimedia.org>
2026-03-30 14:23:47 -0700

    Remove deprecated hcaptcha role
    hcaptcha::proxy has superceded this role.
    
    Bug: T411097
    Change-Id: I7067c74217c7433555c9a018a5aaf9fd046c89c4

--- modules/role/manifests/hcaptcha_proxy.pp
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-class role::hcaptcha_proxy {
-    include profile::base::production
-    include profile::firewall
-    include profile::hcaptcha::proxy
-    include profile::nginx
-}
