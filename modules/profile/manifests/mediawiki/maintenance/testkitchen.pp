31ccff481507ad21aa3b859d4d5c3be2b2336cd9
Clare Ming <cming@wikimedia.org>
2026-03-31 12:30:07 -0600

    Update the Test Kitchen maintenance script to target testwiki
    What?
    
    Add an additional periodic job that runs the Test Kitchen UpdateConfigs
    maintenance script against a group2 wiki.
    
    Why?
    
    The Test Kitchen UpdateConfigs maintenance script uses a hard-coded
    version to handle (rare) backwards-incompatible changes in the upstream
    API.
    
    When bumping the version, when new code rides the train, both the old
    version and the new version need to be used so that later wiki groups
    don't lose experiment and instrument configs as they expire from the
    cache and MainStash.
    
    Bug: T422209
    Change-Id: I4101b3d3e7cdd8e67a2fe98f34dcce6202a7f064

--- modules/profile/manifests/mediawiki/maintenance/testkitchen.pp
+++ modules/profile/manifests/mediawiki/maintenance/testkitchen.pp
@@ -4,16 +4,44 @@ class profile::mediawiki::maintenance::testkitchen(
 ) {
     $team = 'experiment-platform'
 
+    # The extensions/TestKitchen/maintenance/UpdateConfigs.php maintenance script is sensitive
+    # to wiki group, i.e. group0, group1, and group2. The script uses a hard-coded version
+    # number to handle (rare) backwards-incompatible changes in the upstream API. As the wiki
+    # groups are updated across the week, we can have situations like a group0 wiki using
+    # version 1, and group1 and group2 wikis using version 2. We run the script for a group0
+    # and group2 wiki so that, in the case of a version bump, both versions are used as the
+    # wiki groups are updated across the week.
+
     profile::mediawiki::periodic_job { 'testkitchen-UpdateConfigs':
-
-        # mwscript requires the wiki parameter but the maintenance script is wiki-agnostic.
-        command               => '/usr/local/bin/mwscript extensions/TestKitchen/maintenance/UpdateConfigs.php --wiki aawiki',
-
+        ensure                => absent,
+        command               => '/usr/local/bin/mwscript extensions/TestKitchen/maintenance/UpdateConfigs.php --wiki testwiki',
         interval              => '*:*:00',
         cron_schedule         => '* * * * *',
         kubernetes            => true,
         team                  => $team,
-        script_label          => 'TestKitchen-UpdateConfigs.php',
+        script_label          => 'TestKitchen-UpdateConfigs.php-group0',
+        description           => 'Fetch instrument and experiment configs from Test Kitchen and updates the backing store if they have changed',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+    }
+
+    profile::mediawiki::periodic_job { 'testkitchen-UpdateConfigs-group0':
+        command               => '/usr/local/bin/mwscript extensions/TestKitchen/maintenance/UpdateConfigs.php --wiki testwiki',
+        interval              => '*:*:00',
+        cron_schedule         => '* * * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'TestKitchen-UpdateConfigs.php-group0',
+        description           => 'Fetch instrument and experiment configs from Test Kitchen and updates the backing store if they have changed',
+        helmfile_defaults_dir => $helmfile_defaults_dir,
+    }
+
+    profile::mediawiki::periodic_job { 'testkitchen-UpdateConfigs-group2':
+        command               => '/usr/local/bin/mwscript extensions/TestKitchen/maintenance/UpdateConfigs.php --wiki aawiki',
+        interval              => '*:*:00',
+        cron_schedule         => '* * * * *',
+        kubernetes            => true,
+        team                  => $team,
+        script_label          => 'TestKitchen-UpdateConfigs.php-group2',
         description           => 'Fetch instrument and experiment configs from Test Kitchen and updates the backing store if they have changed',
         helmfile_defaults_dir => $helmfile_defaults_dir,
     }
