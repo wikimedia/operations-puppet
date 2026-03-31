2d21e9a840ae93690c099b99fc9d68c556b603ec
Balthazar Rouberol <brouberol@wikimedia.org>
2026-03-31 09:59:35 +0200

    analytics/turnilo: remove associated resources from puppet
    We cannot cleanup the turnilo puppet modules though, because we have a
    cloud instance running turnilo, managed by research:
    https://wikitech.wikimedia.org/wiki/Data_Platform/Data_Lake/Traffic/referrer_daily/Dashboard
    
    Signed-off-by: Balthazar Rouberol <brouberol@wikimedia.org>
    Bug: T416126
    Change-Id: Ifdbc07f214503bdea3dcfb9e6677c49fd944d656

--- modules/role/manifests/analytics_cluster/turnilo.pp
+++
@@ -1,9 +0,0 @@
-# Class: role::analytics_cluster::turnilo
-#
-class role::analytics_cluster::turnilo {
-    include profile::druid::turnilo
-    include profile::druid::turnilo::proxy
-    include profile::tlsproxy::envoy
-    include profile::firewall
-    include profile::base::production
-}
--- modules/role/manifests/analytics_cluster/turnilo/staging.pp
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-# Class: role::analytics_cluster::turnilo::staging
-#
-class role::analytics_cluster::turnilo::staging {
-    include profile::druid::turnilo
-    include profile::firewall
-    include profile::base::production
-}
