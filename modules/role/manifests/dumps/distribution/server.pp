1f2a1f69c75805ce1cb0f479c8de20748773ef9f
Taavi Väänänen <taavi@wikimedia.org>
2026-04-06 18:18:57 +0300

    O:dumps::distribution::server: Configure as LVS realserver
    Bug: T422040
    Change-Id: I70d25390d1bd4e75dc666c16af4e1edf6a6a6964

--- modules/role/manifests/dumps/distribution/server.pp
+++ modules/role/manifests/dumps/distribution/server.pp
@@ -8,6 +8,9 @@ class role::dumps::distribution::server {
     include profile::dumps::distribution::rsync
     include profile::dumps::distribution::web
 
+    include profile::lvs::realserver
+    include profile::lvs::realserver::ipip
+
     include profile::dumps::distribution::datasets::cleanup
     include profile::dumps::distribution::datasets::rsync_config
     include profile::dumps::distribution::datasets::fetcher
