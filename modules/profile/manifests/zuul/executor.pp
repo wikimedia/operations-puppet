e4971779d28f0fe26d9654aec61609ed97659e88
Daniel Zahn <dzahn@wikimedia.org>
2026-04-08 16:20:48 -0700

    zuul::executor: add TLS full chain needed for zookeeper config
    We have one global zuul config that is used on both main nodes
    and executor nodes.
    
    It references the full chain needed for zookeeper. We need this to
    exist here as well, not just on the main nodes.
    
    Bug: T421398
    Bug: T395938
    Change-Id: Ifaaa2e02c5d9dfc1a8afc02bcb171dddd3652ce1

--- modules/profile/manifests/zuul/executor.pp
+++ modules/profile/manifests/zuul/executor.pp
@@ -5,6 +5,7 @@ class profile::zuul::executor(
     Array[Stdlib::Fqdn] $main_nodes = lookup('zuul_main_nodes'),
     String $image_version = lookup('profile::zuul::executor::image_version'),
     Wmflib::Ensure $service_ensure = lookup('profile::zuul::executor::service_ensure'),
+    Stdlib::Unixpath $tls_config_dir = lookup('profile::zuul::executor::tls_config_dir'),
 ){
 
     wmflib::dir::mkdir_p('/etc/zuul/ssh')
@@ -31,5 +32,35 @@ class profile::zuul::executor(
         content   => systemd_template('zuul-executor'),
         require   => File['/etc/zuul/zuul.conf'],
         subscribe => File['/etc/zuul/zuul.conf'],
+    }
+
+    # build full chain of trust with Root CA, Intermediate CA and cert
+    $zookeeper_tls_fullchain = "${tls_config_dir}/zuul_full_chain.pem"
+
+    concat { $zookeeper_tls_fullchain:
+        owner => 'zuul',
+        group => 'zuul',
+        mode  => '0444',
+    }
+
+    # add Zuul client cert
+    concat::fragment { 'zuul_client_cert':
+        target => $zookeeper_tls_fullchain,
+        source => "${tls_config_dir}/zuul__zuul.pem",
+        order  => '00',
+    }
+
+    # add Intermediate CA
+    concat::fragment { 'zuul_intermediate':
+        target => $zookeeper_tls_fullchain,
+        source => "${tls_config_dir}/zuul__zuul.chain.pem",
+        order  => '01',
+    }
+
+    # add Root CA
+    concat::fragment { 'wmf_root':
+        target => $zookeeper_tls_fullchain,
+        source => '/etc/ssl/certs/Wikimedia_Internal_Root_CA.pem',
+        order  => '02',
     }
 }
