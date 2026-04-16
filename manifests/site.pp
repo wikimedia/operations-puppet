862b18af48651e594dcae189547eb3ef09486ed1
Manuel Arostegui <marostegui@wikimedia.org>
2026-04-16 07:23:11 +0200

    site.pp: Remove clouddb1019
    This host will be decommissioned
    
    Bug: T423151
    Change-Id: I696484f29c968a981dcc888139ee9dd3f1ec8797

--- manifests/site.pp
+++ manifests/site.pp
@@ -1793,7 +1793,7 @@ node /^cloudnet100[56]\.eqiad\./ {
 node /^clouddb10(13|14|15|16|22|24)\.eqiad\./ {
     role(wmcs::db::wikireplicas::web_multiinstance)
 }
-node /^clouddb10(17|18|19|20|23|25)\.eqiad\./ {
+node /^clouddb10(17|18|20|23|25)\.eqiad\./ {
     role(wmcs::db::wikireplicas::analytics_multiinstance)
 }
 # clouddb1021 is skipped, it was a special host that's now gone
