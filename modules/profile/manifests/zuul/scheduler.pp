019dc7627b5ef57571f8d22fadfdb2e1f27cb328
Dan Duvall <dduvall@wikimedia.org>
2026-04-16 15:35:36 -0700

    zuul: Provide tenant configuration
    Provide tenant configuration to zuul-scheduler and reload the
    configuration using `zuul-scheduler smart-reconfigure` when the file is
    updated.
    
    Bug: T406384
    Change-Id: Ic0f27b68cb668bca4fff681006adbd360606cec1

--- modules/profile/manifests/zuul/scheduler.pp
+++ modules/profile/manifests/zuul/scheduler.pp
@@ -3,16 +3,38 @@
 class profile::zuul::scheduler(
     String $image_version = lookup('profile::zuul::scheduler::image_version'),
     Wmflib::Ensure $service_ensure = lookup('profile::zuul::scheduler::service_ensure'),
+    String $tenant_config_source = lookup('profile::zuul::scheduler::tenant_config_source'),
     Optional[Stdlib::HTTPUrl] $http_proxy = lookup('profile::zuul::scheduler::http_proxy'),
     Array[Stdlib::Host] $no_proxy = lookup('profile::zuul::scheduler::no_proxy'),
 ){
 
     $host_ip = $facts['networking']['ip']
 
+    file { '/etc/zuul/tenants.yaml':
+        ensure => file,
+        owner  => 'zuul',
+        group  => 'zuul',
+        mode   => '0440',
+        source => $tenant_config_source,
+    }
+
     systemd::service { 'zuul-scheduler':
         ensure    => $service_ensure,
         content   => systemd_template('zuul-scheduler'),
-        require   => File['/etc/zuul/zuul.conf'],
+        require   => [
+            File['/etc/zuul/zuul.conf'],
+            File['/etc/zuul/tenants.yaml'],
+        ],
         subscribe => File['/etc/zuul/zuul.conf'],
+    }
+
+    exec { 'zuul_scheduler_reconfigure':
+        command     => '/usr/bin/docker exec zuul-scheduler zuul-scheduler smart-reconfigure',
+        refreshonly => true,
+        subscribe   => File['/etc/zuul/tenants.yaml'],
+        require     => [
+            File['/etc/zuul/tenants.yaml'],
+            Systemd::Service['zuul-scheduler'],
+        ],
     }
 }
