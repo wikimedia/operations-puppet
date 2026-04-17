af649efe58257c1f94008349214291af4a8a83aa
Andrew Bogott <abogott@wikimedia.org>
2026-04-17 09:29:39 -0500

    Openstack: change spec tests to expect verison Flamingo
    Change-Id: Ia2db758d3b33bfcdffe713a9c28af65538ac6788

--- modules/openstack/spec/classes/nova_fullstack_monitor_spec.rb
+++ modules/openstack/spec/classes/nova_fullstack_monitor_spec.rb
@@ -5,7 +5,7 @@ require "rspec-puppet/cache"
 describe "openstack::nova::fullstack::monitor" do
   on_supported_os(WMFConfig.test_on(12, 12)).each do |os, facts|
     context "On #{os}" do
-      supported_openstacks = ["epoxy"]
+      supported_openstacks = ["flamingo"]
       supported_openstacks.each do |openstack_version|
         context "On openstack #{openstack_version}" do
           let(:facts) { facts }
