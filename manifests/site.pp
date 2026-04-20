1304f4bd78b6d778f641e7f5e64f371cbb020008
Janis Meybohm <jmeybohm@wikimedia.org>
2026-04-20 13:11:55 +0200

    Add wikikube-worker refreshes
    - wikikube-worker13[28-34] for:
        wikikube-worker11[00-12]
    - wikikube-worker13[60-72] for:
        wikikube-worker10[02-05,11-12,19-20,29-31,58-63,82-83,88-92,96-99]
    - wikikube-worker137[3-4] for:
        wikikube-worker116[6-8]
    
    Bug: T418257
    Bug: T418258
    Bug: T420967
    Change-Id: I0998f976111cec5bb8ac011e54491ac74e7de039

--- manifests/site.pp
+++ manifests/site.pp
@@ -1670,7 +1670,7 @@ node /^wikikube-worker11([0-5][0-9]|6[0-8])\.eqiad\./ {
 
 # T368933, T369743 NOTE: We use those hostnames because we are going to be
 # renaming parse*, mw*, kubernetes* and those add up to 1231. Leeway included
-node /^wikikube-worker1(2[4-9][0-9]|3[0-1][0-9]|32[0-7]|33[5-9]|3[4-5][0-9])\.eqiad\./ {
+node /^wikikube-worker1(2[4-9][0-9]|3[0-1][0-9]|32[0-9]|33[0-9]|3[4-6][0-9]|37[0-4])\.eqiad\./ {
     role(kubernetes::worker)
 }
 
@@ -1691,21 +1691,6 @@ node /^wikikube-worker23([0-2][0-9]|30|3[2-9]|4[0-9]|5[0-6])\.codfw\./ {
 }
 
 node /^wikikube-worker2331\.codfw\./ {
-    role(insetup::serviceops_ferm)
-}
-
-# T408749 eqiad expansion
-node /^wikikube-worker13(2[8-9]|3[0-4])\.eqiad\./ {
-    role(insetup::serviceops_ferm)
-}
-
-# T408760 eqiad refresh
-node /^wikikube-worker13(6[0-9]|7[0-2])\.eqiad\./ {
-    role(insetup::serviceops_ferm)
-}
-
-# T416390 eqiad refresh
-node /^wikikube-worker13(7[3-4])\.eqiad\./ {
     role(insetup::serviceops_ferm)
 }
 
