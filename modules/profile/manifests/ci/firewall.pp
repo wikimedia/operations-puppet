02388ca408ba68c04899916b6f54943c2e126b42
Daniel Zahn <dzahn@wikimedia.org>
2026-04-20 14:53:33 -0700

    jenkins: add firewall rule for new jenkins to gearman on legacy host
    Allow the active new jenkins host to talk to Gearman on the active
    legacy jenkins host; and nothing else; only this one server to this
    one other server.
    
    Bug: T418521
    Change-Id: I26b314e0c9a57e504d109613390ff55d4e061797

--- modules/profile/manifests/ci/firewall.pp
+++ modules/profile/manifests/ci/firewall.pp
@@ -9,6 +9,8 @@
 #
 class profile::ci::firewall (
     Array[Stdlib::Fqdn] $zuul_merger_hosts = lookup('profile::ci::firewall::zuul_merger_hosts'),
+    Stdlib::Fqdn $jenkins_new_host = lookup('profile::ci::firewall::jenkins_new_host'),
+    Stdlib::Fqdn $jenkins_legacy_host = lookup('profile::ci::firewall::jenkins_legacy_host'),
 ){
     include profile::firewall
     include network::constants
@@ -23,6 +25,14 @@ class profile::ci::firewall (
         proto  => 'tcp',
         port   => 4730,
         srange => $zuul_merger_hosts,
+    }
+
+    # From new jenkins host (T418521) to Gearman on legacy CI manager host.
+    firewall::service { 'gearman_from_jenkins_to_zuul_scheduler':
+        proto  => 'tcp',
+        port   => 4730,
+        srange => $jenkins_new_host,
+        drange => $jenkins_legacy_host,
     }
 
     firewall::service { 'ci_http':
