ca65caf7b90847c492c2821fbc316a48cd768ae2
Manuel Arostegui <marostegui@wikimedia.org>
2026-04-21 15:54:31 +0200

    mariadb: Productionize db2251
    Will replace db2142
    
    Bug: T418979
    Change-Id: I316c035f250a3414d1af525131a57d6c27703b9a

--- manifests/site.pp
+++ manifests/site.pp
@@ -692,7 +692,7 @@ node /^db12(65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87
 }
 
 # Refresh hosts T418911
-node /^db22(50|51|52|53)\.codfw\./ {
+node /^db22(50|52|53)\.codfw\./ {
     role(insetup::data_persistence_ferm)
 }
 
@@ -708,6 +708,10 @@ node /^db21(42)\.codfw\./ {
     role(mariadb::parsercache)
 }
 
+# Replacement for db2142
+node /^db22(51)\.codfw\./ {
+    role(mariadb::parsercache)
+}
 # ms2 shard
 # eqiad
 
