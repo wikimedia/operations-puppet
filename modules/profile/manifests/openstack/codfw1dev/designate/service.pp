be99dd84c80b3f0004261f68a8368984b1319eb4
Andrew Bogott <abogott@wikimedia.org>
2026-04-28 16:07:43 +0000

    Revert "Designate: use zookeeper as the tooz backend, everywhere"
    This reverts commit a6a00a7e6270660d7b4c2aebb7bdcc8987723e74.
    
    Reason for revert: zookeeper is crashing or exiting on eqiad1
    hosts. Reverting this until we fix that.
    
    Change-Id: I7fcc4f79f24e63ad800405cb770a506d613ad638

--- modules/profile/manifests/openstack/codfw1dev/designate/service.pp
+++ modules/profile/manifests/openstack/codfw1dev/designate/service.pp
@@ -19,6 +19,7 @@ class profile::openstack::codfw1dev::designate::service(
     $rabbit_pass = lookup('profile::openstack::codfw1dev::designate::rabbit_pass'),
     $osm_host = lookup('profile::openstack::codfw1dev::osm_host'),
     $region = lookup('profile::openstack::codfw1dev::region'),
+    Integer $mcrouter_port = lookup('profile::openstack::codfw1dev::designate::mcrouter_port'),
     Array[Stdlib::Host] $haproxy_nodes = lookup('profile::openstack::codfw1dev::haproxy_nodes'),
 ) {
 
@@ -45,7 +46,9 @@ class profile::openstack::codfw1dev::designate::service(
         rabbit_pass                   => $rabbit_pass,
         osm_host                      => $osm_host,
         region                        => $region,
+        mcrouter_port                 => $mcrouter_port,
         haproxy_nodes                 => $haproxy_nodes,
+        tooz_backend                  => 'zookeeper',
         zookeeper_cluster_name        => 'designate_codfw1dev',
     }
     contain '::profile::openstack::base::designate::service'
--- modules/profile/manifests/openstack/eqiad1/designate/service.pp
+++ modules/profile/manifests/openstack/eqiad1/designate/service.pp
@@ -18,6 +18,7 @@ class profile::openstack::eqiad1::designate::service(
     $rabbit_pass = lookup('profile::openstack::eqiad1::designate::rabbit_pass'),
     $osm_host = lookup('profile::openstack::eqiad1::osm_host'),
     $region = lookup('profile::openstack::eqiad1::region'),
+    Integer $mcrouter_port = lookup('profile::openstack::eqiad1::designate::mcrouter_port'),
     Array[Stdlib::Host] $haproxy_nodes = lookup('profile::openstack::eqiad1::haproxy_nodes'),
 ) {
     $designate_hosts = $openstack_control_nodes.map |$node| { $node['cloud_private_fqdn'] }
@@ -43,7 +44,9 @@ class profile::openstack::eqiad1::designate::service(
         rabbit_pass                   => $rabbit_pass,
         osm_host                      => $osm_host,
         region                        => $region,
+        mcrouter_port                 => $mcrouter_port,
         haproxy_nodes                 => $haproxy_nodes,
+        tooz_backend                  => 'mcrouter',
         zookeeper_cluster_name        => 'designate_eqiad1',
     }
 
