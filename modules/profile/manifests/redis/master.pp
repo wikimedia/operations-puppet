2fc4e535ea71b2096ecc76ae74a614c9c6f88d22
Taavi Väänänen <taavi@wikimedia.org>
2026-05-04 14:23:31 +0300

    P:redis::master: Pass ports as an array to firewall
    Makes the profile actually compatible with nftables.
    
    Change-Id: Iedc09531a4d6cfca78c8317869a43c836a6a6964

--- modules/profile/manifests/redis/master.pp
+++ modules/profile/manifests/redis/master.pp
@@ -1,17 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::redis::master(
-    Array[String]       $instances          = lookup('profile::redis::master::instances'),
-    Hash                $settings           = lookup('profile::redis::master::settings'),
-    Hash                $instance_overrides = lookup('profile::redis::master::instance_overrides',
-                                                    {'default_value' => {}}),
-    String              $password           = lookup('profile::redis::master::password'),
-    Boolean             $aof                = lookup('profile::redis::master::aof',
-                                                    {'default_value' => false}),
-    Array[Stdlib::Host] $clients            = lookup('profile::redis::master::clients',
-                                                    {'default_value' => []}),
+    Array[Stdlib::Port]      $instances          = lookup('profile::redis::master::instances'),
+    Hash                     $settings           = lookup('profile::redis::master::settings'),
+    Hash[Stdlib::Port, Hash] $instance_overrides = lookup('profile::redis::master::instance_overrides', {'default_value' => {}}),
+    String                   $password           = lookup('profile::redis::master::password'),
+    Boolean                  $aof                = lookup('profile::redis::master::aof', {'default_value' => false}),
+    Array[Stdlib::Host]      $clients            = lookup('profile::redis::master::clients', {'default_value' => []}),
 ){
     $uris = $instances.map |$instance| { "localhost:${instance}/${password}" }
-    $redis_ports = join($instances, ' ')
 
     $auth_settings = {
         'masterauth'  => $password,
@@ -23,22 +19,25 @@ class profile::redis::master(
         default => inline_template("@resolve((<%= @clients.join(' ') %>))"),
     }
 
-    $instances.each |String $instance| {
+    $instances.each |Stdlib::Port $instance| {
         if $instance in keys($instance_overrides) {
             $override = $instance_overrides[$instance]
         } else {
             $override = {}
         }
-        profile::redis::instance { $instance:
+        profile::redis::instance { String($instance):
+            port     => $instance,
             settings => merge($settings, $auth_settings, $override),
             aof      => $aof,
         }
     }
 
-    # Add monitoring, using nrpe and not remote checks anymore
-    redis::monitoring::nrpe_instance { $instances: }
+    $instance_strings = $instances.map |$instance| { String($instance) }
 
-    profile::prometheus::redis_exporter{ $instances:
+    # Add monitoring, using nrpe and not remote checks anymore
+    redis::monitoring::nrpe_instance { $instance_strings: }
+
+    profile::prometheus::redis_exporter{ $instance_strings:
         password => $password,
     }
 
@@ -46,13 +45,13 @@ class profile::redis::master(
         firewall::service { 'redis_master_role':
             proto   => 'tcp',
             notrack => true,
-            port    => inline_template('(<%= @redis_ports %>)'),
+            port    => $instances,
         }
     } else {
         firewall::service { 'redis_master_role':
             proto   => 'tcp',
             notrack => true,
-            port    => inline_template('(<%= @redis_ports %>)'),
+            port    => $instances,
             srange  => $clients,
         }
     }
