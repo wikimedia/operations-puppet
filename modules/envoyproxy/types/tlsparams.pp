85cab0fe3902e06110b17b6847ba7c4cd0c1a0a8
Janis Meybohm <jmeybohm@wikimedia.org>
2026-05-04 13:44:48 +0200

    Revert "envoyproxy: Support TLS min/max version config"
    Removed unused envoyproxy config options that where added while
    evaluating envoy as CDN TLS terminator (T271421).
    
    This reverts commit 50088f91c30fc5091b86baf2491ff6c1bab4f5e1.
    
    Bug: T271421
    Bug: T414440
    Hosts: auto
    Change-Id: Ib15dfc07f37f3132a7a02ca7df978dc8057770f1

--- modules/envoyproxy/types/tlsparams.pp
+++ modules/envoyproxy/types/tlsparams.pp
@@ -1,7 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 type Envoyproxy::Tlsparams = Struct[{
-    'cipher_suites'   => Optional[Array[String]],
-    'ecdh_curves'     => Optional[Array[String]],
-    'tls_min_version' => Optional[Enum['TLSv1_0', 'TLSv1_1', 'TLSv1_2', 'TLSv1_3']],
-    'tls_max_version' => Optional[Enum['TLSv1_0', 'TLSv1_1', 'TLSv1_2', 'TLSv1_3']],
+    'cipher_suites' => Optional[Array[String]],
+    'ecdh_curves'   => Optional[Array[String]],
 }]
