121b9c60429cf51a2c70e99e37a8775544816e1f
Luca Toscano <ltoscano@wikimedia.org>
2026-05-05 12:43:36 +0200

    profile::prometheus::alerts: rely on kafka mirror's defaults
    Change-Id: Ie2143b04cf1e8912810e70c333d47ed1c9edfdc3

--- modules/profile/manifests/prometheus/alerts.pp
+++ modules/profile/manifests/prometheus/alerts.pp
@@ -13,25 +13,18 @@ class profile::prometheus::alerts (
     # main-eqiad -> jumbo MirrorMaker
     profile::kafka::mirror::alerts { 'main-eqiad-to-jumbo-eqiad':
         #  For now, alert Data Platform SREs.  Change this back to admins soon.
-        contact_group         => 'team-data-platform',
-        topic_blacklist       => '.*(change-prop|\.job\.|changeprop).*',
-        prometheus_url        => 'http://prometheus.svc.eqiad.wmnet/ops',
-        source_prometheus_url => 'http://prometheus.svc.eqiad.wmnet/ops',
+        contact_group   => 'team-data-platform',
+        topic_blacklist => '.*(change-prop|\.job\.|changeprop).*',
     }
 
     # Cross DC main-eqiad <-> main-codfw MirrorMakers.
-    profile::kafka::mirror::alerts { 'main-eqiad-to-main-codfw':
-        prometheus_url        => 'http://prometheus.svc.codfw.wmnet/ops',
-        source_prometheus_url => 'http://prometheus.svc.eqiad.wmnet/ops',
-    }
+    profile::kafka::mirror::alerts { 'main-eqiad-to-main-codfw': }
     # main-eqiad is getting the bulk of the traffic from MediaWiki,
     # and it currently pulls msgs from main-codfw at a very low rate
     # (but we want to make sure that it doesn't drop to zero).
     profile::kafka::mirror::alerts { 'main-codfw-to-main-eqiad':
         #  For now, alert analytics admins, until alerts are more stable.
-        prometheus_url        => 'http://prometheus.svc.eqiad.wmnet/ops',
-        source_prometheus_url => 'http://prometheus.svc.codfw.wmnet/ops',
-        warning_throughput    => 3,
+        warning_throughput => 3,
     }
 
     ### The same as previous ones, but fully Prometheus-based
