b5880d37b369c855238f37fc151c54b527101523
Arzhel Younsi <ayounsi@wikimedia.org>
2026-05-05 08:30:51 +0200

    LVS BGP: peer with the gateway if no exception is set
    Hosts: cumin:O:lvs::balancer
    Hosts: cumin:O:liberica
    Change-Id: Ide15559ac3f60b82914f2e45f6d8114eaa459c47

--- modules/profile/manifests/pybal.pp
+++ modules/profile/manifests/pybal.pp
@@ -30,13 +30,13 @@ class profile::pybal(
         'bgp' => $bgp,
         'bgp-peer-address' => $::hostname ? {
             # For Liberica this is controlled with profile::liberica::bgp_config (or the absence of),
-            # for PyBal we default to peering with the default gateway, except for certain hosts 
+            # for PyBal we default to peering with the default gateway, except for certain hosts
             # at certain locations which are still on the legacy vlan setup.
             /^lvs101[3-6]$/     => "[ '208.80.154.196', '208.80.154.197' ]", # cr1-eqiad,cr2-eqiad
             /^lvs101[8-9]$/     => "[ '208.80.154.196', '208.80.154.197' ]", # cr1-eqiad,cr2-eqiad
             /^lvs1020$/         => "[ '208.80.154.196', '208.80.154.197' ]", # cr1-eqiad,cr2-eqiad
             /^lvs50[0-9][0-9]$/ => "[ '103.102.166.131', '103.102.166.130' ]", # cr3-eqsin,cr2-eqsin
-            default             => '(unspecified)'
+            default             => "[ '${$facts['default_routes']['ipv4']}' ]"
             },
         'bgp-nexthop-ipv4'               => $facts['ipaddress'],
         'bgp-nexthop-ipv6'               => inline_template("<%= require 'ipaddr'; (IPAddr.new(@ipaddress6).mask(64) | IPAddr.new(\"::\" + @ipaddress.gsub('.', ':'))).to_s() %>"),
