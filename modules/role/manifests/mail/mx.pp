9f5e36df11f5aed0503ff2e971a216af063eb8b0
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-05-05 17:05:29 +0200

    Remove role::mail::mx and related Puppet code
    r1283021 was failing in PCC and on closer inspection I noticed the role
    is obsoleted by the Postfix migration. Remove it along with the profile
    and various Pupppet classes (the ones referenced, but not removed are
    still used by the Phabricator, VRTS and smarthost Exim configs)
    
    Hosts: mx-in1001.wikimedia.org,mx-in2001.wikimedia.org,
    Hosts: mx-out1001.wikimedia.org,mx-out2001.wikimedia.org
    Hosts: vrts1003.eqiad.wmnet,vrts2002.codfw.wmnet
    Hosts: lists1004.wikimedia.org,lists2001.wikimedia.org
    Hosts: phab1004.eqiad.wmnet, phab2002.codfw.wmnet
    Hosts: sretest1005.eqiad.wmnet
    Bug: T325394
    Bug: T149804
    Change-Id: I6a74227c434eca841eef7e33b4103410ede96933

--- modules/role/manifests/mail/mx.pp
+++
@@ -1,8 +0,0 @@
-class role::mail::mx {
-    # tag as a public inbound mx server
-    tag 'mx_in'
-    include profile::base::production
-    include network::constants
-    include profile::firewall
-    include profile::mail::mx
-}
