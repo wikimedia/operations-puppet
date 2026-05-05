59db86ba7e8f20d6254350ea7bf74841fd48ed70
Andrew Bogott <abogott@wikimedia.org>
2026-05-05 14:21:04 -0500

    magnum-container-api: Install ORC CRD before setting up clusterctl
    This is apparently required, details buried in the docs at
    https://cluster-api.sigs.k8s.io/user/quick-start
    
    Bug: T393782
    Change-Id: Ib0c1d7cbfd6f3457bb39b459575324c6ba0916d1

--- modules/profile/manifests/openstack/capi.pp
+++ modules/profile/manifests/openstack/capi.pp
@@ -4,7 +4,7 @@
 #
 # Installs k3s, helm, and capi
 class profile::openstack::capi(
-  Stdlib::HTTPSUrl $cluster_ctl_url = lookup('profile::openstack::capi::cluster_ctl_url', {'default_value' => 'https://object.eqiad1.wikimediacloud.org/swift/v1/AUTH_c2c23ceb46404a62a80492b07dac4685/clusterctl'}),
+  Stdlib::HTTPSUrl $cluster_ctl_url = lookup('profile::openstack::capi::cluster_ctl_url', {'default_value' => 'https://object.eqiad1.wikimediacloud.org/swift/v1/AUTH_c2c23ceb46404a62a80492b07dac4685'}),
   Stdlib::Host     $docker_repo_base = lookup('profile::openstack::capi::docker_repo_base', {'default_value' => 'docker-registry.wmcloud.org'}),
   String           $cluster_api_version = lookup('profile::openstack::capi::cluster_api_version', {'default_value' => 'v1.12.7'}),
   String           $cluster_api_provider_openstack_version = lookup('profile::openstack::capi::cluster_api_provider_openstack_version', {'default_value' => 'v0.14.4'}),
