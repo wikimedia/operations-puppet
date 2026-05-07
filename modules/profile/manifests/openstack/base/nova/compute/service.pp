aa5fb49477719b2fffdf7d947060085a644f1a28
Taavi Väänänen <taavi@wikimedia.org>
2026-05-07 18:08:50 +0300

    P:openstack: nova: Set MTU on flat VLAN interface in codfw1dev
    Bug: T425674
    Change-Id: I1d9e139b72029a9dd4e86152baf82e036a6a6964

--- modules/profile/manifests/openstack/base/nova/compute/service.pp
+++ modules/profile/manifests/openstack/base/nova/compute/service.pp
@@ -4,6 +4,7 @@ class profile::openstack::base::nova::compute::service(
     String $instance_dev = lookup('profile::openstack::base::nova::instance_dev'),
     String[1] $network_flat_interface = lookup('profile::openstack::base::nova::network_flat_interface'),
     Network::VLANTag $network_flat_interface_vlan = lookup('profile::openstack::base::nova::network_flat_interface_vlan'),
+    Boolean $network_flat_set_mtu = lookup('profile::openstack::base::nova::compute::service', {default_value => false}),
     Array[Stdlib::Fqdn] $all_cloudvirts = lookup('profile::openstack::base::nova::all_cloudvirts'),
     String $libvirt_cpu_model = lookup('profile::openstack::base::nova::libvirt_cpu_model'),
     Optional[Boolean] $enable_nova_rbd = lookup('profile::cloudceph::client::rbd::enable_nova_rbd', {'default_value' => false}),
@@ -26,6 +27,12 @@ class profile::openstack::base::nova::compute::service(
         base_interface => $facts['interface_primary'],
         vlan_id        => $network_flat_interface_vlan,
         method         => 'manual',
+    }
+
+    if $network_flat_set_mtu {
+        interface::mtu { $network_flat_interface:
+            mtu => 1500,
+        }
     }
 
     if $instance_dev == 'srvlink' {
