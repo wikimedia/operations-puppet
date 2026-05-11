67028ed36f4da91a644748afdf2846b5e1ef10a6
Taavi Väänänen <taavi@wikimedia.org>
2026-05-11 13:39:01 +0300

    P:openstack: neutron: Set MTU on cloudnet codfw1dev interfaces
    Bug: T425674
    Change-Id: I06b10efe3cfa8bf8f2d2c47a2dd762676a6a6964

--- modules/profile/manifests/openstack/base/neutron/l3_agent.pp
+++ modules/profile/manifests/openstack/base/neutron/l3_agent.pp
@@ -5,6 +5,7 @@ class profile::openstack::base::neutron::l3_agent(
     $base_interface = lookup('profile::openstack::base::neutron::base_interface'),
     Network::VLANTag $network_flat_interface_vlan_external = lookup('profile::openstack::base::neutron::network_flat_interface_vlan_external'),
     Network::VLANTag $network_flat_interface_vlan = lookup('profile::openstack::base::neutron::network_flat_interface_vlan'),
+    Boolean $set_vlan_mtu = lookup('profile::openstack::base::neutron::l3_agent::set_vlan_mtu', {default_value => false}),
 ) {
     $wan_nic  = "vlan${network_flat_interface_vlan_external}"
     $virt_nic = "vlan${network_flat_interface_vlan}"
@@ -23,6 +24,12 @@ class profile::openstack::base::neutron::l3_agent(
         method         => 'manual',
         up             => 'ip link set $IFACE up',
         down           => 'ip link set $IFACE down',
+    }
+
+    if $set_vlan_mtu {
+        interface::mtu { [ $wan_nic, $virt_nic ]:
+            mtu => 1500,
+        }
     }
 
     class {'::openstack::neutron::l3_agent':
