e92b4240a6b13e7a2a2cae831c3f918c9460f4e1
Brett Cornwall <bcornwall@wikimedia.org>
2026-05-12 15:39:25 -0700

    Remove lvs1016, promote lvs1017
    lvs1016 is older hardware and was only temporarily used during some
    testing with lvs1017. lvs1017 is the proper heir to this throne.
    
    Bug: T421421
    Change-Id: Ieff4a7a30fb0aac32a76e052b4c4d6b9041ea8f1

--- modules/profile/manifests/lvs/configuration.pp
+++ modules/profile/manifests/lvs/configuration.pp
@@ -4,7 +4,7 @@ class profile::lvs::configuration {
     $lvs_class_hosts = {
         'high-traffic1' => $::realm ? {
             'production' => $::site ? {
-                'eqiad' => [ 'lvs1016', 'lvs1020', 'lvs1017'],
+                'eqiad' => [ 'lvs1017', 'lvs1020' ],
                 'codfw' => [ 'lvs2011', 'lvs2014' ],
                 'esams' => [ 'lvs3008', 'lvs3010' ],
                 'ulsfo' => [ 'lvs4008', 'lvs4010' ],
@@ -57,7 +57,6 @@ class profile::lvs::configuration {
     # Warning: if you change this, look out for the usage of this data structure
     # in profile::mediawiki::maintenance::wikidata
     $lvs_classes =  {
-        'lvs1016' => 'high-traffic1',
         'lvs1017' => 'high-traffic1',
         'lvs1018' => 'high-traffic2',
         'lvs1019' => 'low-traffic',
