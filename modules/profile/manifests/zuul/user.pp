3405cfe436433bf3bdd40575fe984f2835095ad5
Daniel Zahn <dzahn@wikimedia.org>
2026-05-13 11:25:34 -0700

    zuul: replace user/group setup with systemd::sysuser
    The system user zuul is already reserved globally to use UID 923.
    
    We should replace user and group setup with systemd::sysuser though.
    
    Just defining the user zuul should automatically
    create a zuul group and make zuul member of zuul,
    via systemd itself.
    
    But we do need to add it into the docker group here.
    
    Bug: T395938
    Change-Id: I03de1536e30ad6c2ea793eadc27aed1268aa188e

--- modules/profile/manifests/zuul/user.pp
+++ modules/profile/manifests/zuul/user.pp
@@ -2,20 +2,9 @@
 # set up system user and group for new zuul
 class profile::zuul::user {
 
-    group { 'zuul':
-        ensure => present,
-        gid    => 923,
-        name   => 'zuul',
-        system => true,
-    }
-
-    user { 'zuul':
-        ensure  => present,
-        uid     => 923,
-        system  => true,
-        groups  => 'docker',
-        require => [
-            Group['zuul'],
-        ],
+    systemd::sysuser { 'zuul':
+        description       => 'zuul system user',
+        id                => 923,
+        additional_groups => ['docker'],
     }
 }
