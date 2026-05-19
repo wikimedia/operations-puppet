2c7bccdca7c21f48f1fd62febe4e1251da30b6f4
Jasmine <jkilani@wikimedia.org>
2026-05-18 18:00:43 -0700

    k8s: add wikikube-worker2331
    Bug: T426688
    Change-Id: I9e748e33b12533e304735e360512a5ee99483d1f

--- manifests/site.pp
+++ manifests/site.pp
@@ -1665,12 +1665,8 @@ node /^wikikube-worker22(0[0-9]|1[0-5]|4[2389]|[5-9][0-9])\.codfw\./ {
     role(kubernetes::worker)
 }
 
-node /^wikikube-worker23([0-2][0-9]|30|3[2-9]|[4-6][0-9]|7[0-4])\.codfw\./ {
+node /^wikikube-worker23([0-3][0-9]|[4-6][0-9]|7[0-4])\.codfw\./ {
     role(kubernetes::worker)
-}
-
-node /^wikikube-worker2331\.codfw\./ {
-    role(insetup::serviceops_ferm)
 }
 
 # mw-experimental workers T276994 T397051
