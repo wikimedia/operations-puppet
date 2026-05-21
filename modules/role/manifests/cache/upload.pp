3ac82aa87f02f1f62beff018a0c448c5ca27ddec
Simon Lyngshede <slyngshede@wikimedia.org>
2026-05-21 10:08:15 +0200

    R:cache::upload enable TCP Fast Open
    Set "net.ipv4.tcp_fastopen" = 3
    3 being allow enabling by both client and server.
    
    Bug: T415454
    Change-Id: I3d22f6ea9fbbaf6200fd84416ac7b33cd40fe536

--- modules/role/manifests/cache/upload.pp
+++ modules/role/manifests/cache/upload.pp
@@ -3,6 +3,7 @@ class role::cache::upload {
     include profile::netconsole::client
 
     include profile::cache::base
+    include profile::tcp_fast_open
     include profile::cache::haproxy
     include profile::cache::varnish::frontend
     include profile::prometheus::varnish_exporter
