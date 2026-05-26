b1198c8ba30aefa3aca6111a70e4d640326afad9
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-05-26 09:52:01 +0200

    profile::rpkivalidator: Switch to firewall::service
    To allow to eventually move these to nftables
    
    Hosts: rpki1001.eqiad.wmnet,rpki2003.codfw.wmnet
    Change-Id: I6931b56004bc2dc7c39bc4371b5cf5a8eed08ee5

--- modules/profile/manifests/rpkivalidator.pp
+++ modules/profile/manifests/rpkivalidator.pp
@@ -30,19 +30,20 @@ class profile::rpkivalidator(
     } else {
         $proxy = undef
     }
-    class { '::routinator':
+    class { 'routinator':
         proxy    => $proxy,
         rtr_port => $rtr_port,
     }
 
-    # Standard port is 323 but using 3323 to run the daemon as unpriviledge user
-    # MGMT_NETWORKS is also included as devices can (should?) querry it over their mgmt port
+    # Standard port is 323 but using 3323 to run the daemon as unprivilegded user
+    # MGMT_NETWORKS is also included as devices can (should?) query it over their mgmt port
     ferm::service { 'rpkivalidator-rtr-acl':
-        desc   => 'RPKI to router port',
-        proto  => 'tcp',
-        port   => $rtr_port,
-        srange => '($NETWORK_INFRA $MGMT_NETWORKS)',
+        desc     => 'RPKI to router port',
+        proto    => 'tcp',
+        port     => $rtr_port,
+        src_sets => ['NETWORK_INFRA', 'MGMT_NETWORKS'],
     }
+
     monitoring::service { 'rpkivalidator-rtr-mon':
         description    => 'RPKI Validator RTR port',
         check_command  => "check_tcp!${rtr_port}",
