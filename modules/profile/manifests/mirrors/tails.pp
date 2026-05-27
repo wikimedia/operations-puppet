f72e3f2a780d307305d138b50d2ac02ae5b4cc84
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-05-27 16:30:11 +0200

    mirrors: Disable tails mirror
    I reached out to the tor project and mirrors.wikimedia.org has been removed from the
    tails mirror rotation.
    
    Bug: T416707
    Hosts: mirror1001.wikimedia.org
    Change-Id: I25a4de0711f92e968a3a0b035d8a219913d1bf6f

--- modules/profile/manifests/mirrors/tails.pp
+++ modules/profile/manifests/mirrors/tails.pp
@@ -14,34 +14,19 @@
 #   include mirrors::tails
 
 class profile::mirrors::tails {
-    include profile::mirrors
-
-    $local_dir = '/srv/mirrors/tails'
-    $remote_path = 'rsync.tails.net::amnesia-archive/tails/'
-
-    file { $local_dir:
-        ensure => directory,
-        owner  => 'mirror',
-        group  => 'mirror',
-        mode   => '0755',
-    }
-
-    $rsync_cmd = "/usr/bin/rsync -rt --delete ${remote_path} ${local_dir}"
-
     systemd::timer::job { 'update-tails-mirror':
-        ensure      => 'present',
+        ensure      => absent,
         user        => 'mirror',
         description => 'update the tails mirror with rsync',
-        command     => $rsync_cmd,
+        command     => '/usr/bin/rsync -rt --delete /srv/mirrors/tails rsync.tails.net::amnesia-archive/tails/',
         interval    => {'start' => 'OnUnitInactiveSec', 'interval' => '1h'},
     }
 
-    # serve via rsync
     rsync::server::module { 'tails':
-        path      => $local_dir,
+        ensure    => absent,
+        path      => '/srv/mirror/tails',
         read_only => 'yes',
         uid       => 'nobody',
         gid       => 'nogroup',
     }
-
 }
