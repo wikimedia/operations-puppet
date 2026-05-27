c1b4c4026474722458befe7f79ebab1e4307ff7b
Majavah <taavi@wikimedia.org>
2026-05-27 15:39:06 +0000

    Revert "P:openstack: cloudweb: Absent memcached and mcrouter services"
    This reverts commit 65bbf8b115f5cb4e34a2ff4818c2cf9423d07516.
    
    Reason for revert: cloudweb != cloudcontrol
    
    Change-Id: I66d575d7d2ed79ff310a56d7b99346e88c850bad

--- modules/profile/manifests/openstack/codfw1dev/cloudweb_mcrouter.pp
+++ modules/profile/manifests/openstack/codfw1dev/cloudweb_mcrouter.pp
@@ -1,4 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
-class profile::openstack::codfw1dev::cloudweb_mcrouter () {
-    class {'profile::openstack::base::cloudweb_mcrouter': }
+class profile::openstack::codfw1dev::cloudweb_mcrouter(
+    Array[Stdlib::Fqdn] $cloudweb_hosts = lookup('profile::openstack::codfw1dev::cloudweb_hosts'),
+    Stdlib::Port        $mcrouter_port  = lookup('profile::openstack::codfw1dev::cloudweb::mcrouter_port'),
+    Integer             $memcached_size = lookup('profile::openstack::codfw1dev::cloudweb_memcached_size'),
+    String[1]           $memcached_user = lookup('profile::openstack::codfw1dev::cloudweb::memcached_user'),
+
+) {
+    class {'profile::openstack::base::cloudweb_mcrouter':
+        cloudweb_hosts => $cloudweb_hosts,
+        mcrouter_port  => $mcrouter_port,
+        memcached_size => $memcached_size,
+        memcached_user => $memcached_user,
+    }
 }
