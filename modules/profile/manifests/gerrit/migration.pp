5819f553c563bff876e8fc311b87f1f84fc6aead
Daniel Zahn <dzahn@wikimedia.org>
2026-06-01 09:53:10 -0700

    gerrit: use rsync::quickdatacopy in migration class
    Replace custom rsync::module code with the generic
    rsync::quickdatacopy class.
    
    More standard and it allows us to use stunnel
    to encrypt the traffic.
    
    Bug: T412780
    Change-Id: I598217db5e4a81e9e0a0983e68ff0744ef77792a

--- modules/profile/manifests/gerrit/migration.pp
+++ modules/profile/manifests/gerrit/migration.pp
@@ -3,29 +3,32 @@
 class profile::gerrit::migration (
     Stdlib::Fqdn        $src_host = lookup('profile::gerrit::active_host'),
     Stdlib::Unixpath    $data_dir = lookup('profile::gerrit::migration::data_dir'),
-) {
+){
     $dst_hosts = wmflib::class::hosts('gerrit').filter |$host| { $host != $src_host }
 
-    if $facts['fqdn'] in $dst_hosts {
+    rsync::quickdatacopy { 'gerrit-data':
+        ensure                     => present,
+        auto_sync                  => false,
+        source_host                => $src_host,
+        dest_host                  => $facts['networking']['fqdn'],
+        module_path                => $data_dir,
+        chown                      => 'gerrit:gerrit',
+        ignore_missing_file_errors => true,
+        server_uses_stunnel        => true,
+    }
 
-        class { 'rsync::server': }
+    file { "/srv/home-${src_host}/":
+        ensure => directory,
+    }
 
-        rsync::server::module { 'gerrit-data':
-            path          => $data_dir,
-            read_only     => 'no',
-            auto_firewall => true,
-            hosts_allow   => [$src_host],
-        }
-
-        file { "/srv/home-${src_host}/":
-            ensure => directory,
-        }
-
-        rsync::server::module { 'gerrit-home':
-            path          => "/srv/home-${src_host}",
-            read_only     => 'no',
-            auto_firewall => true,
-            hosts_allow   => [$src_host],
-        }
+    rsync::quickdatacopy { 'gerrit-home':
+        ensure                     => present,
+        auto_sync                  => false,
+        source_host                => $src_host,
+        dest_host                  => $facts['networking']['fqdn'],
+        module_path                => "/srv/home-${src_host}",
+        chown                      => 'gerrit:gerrit',
+        ignore_missing_file_errors => true,
+        server_uses_stunnel        => true,
     }
 }
