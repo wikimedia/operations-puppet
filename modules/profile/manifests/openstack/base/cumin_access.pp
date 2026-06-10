4297bd4e74a27ffeb4814f790456f80aa9b972ce
Andrew Bogott <abogott@wikimedia.org>
2026-06-10 11:23:55 -0500

    openstack::apply_security_groups: only run on one cloudcontrol
    There's some chance of race conditions within nova if we
    run this simultaneously on multiple hosts.
    
    Hosts: cloudcontrol1006.eqiad.wmnet, cloudcontrol1007.eqiad.wmnet
    Hosts: cloudcontrol1011.eqiad.wmnet
    Hosts: cloudcontrol2010-dev.codfw.wmnet, cloudcontrol2005-dev.codfw.wmnet
    Hosts: cloudcontrol2006-dev.codfw.wmnet
    Bug: T422801
    Change-Id: I8b52b4fcbe90feeac578a0a132a43dc5fbbf4fb5

--- modules/profile/manifests/openstack/base/cumin_access.pp
+++ modules/profile/manifests/openstack/base/cumin_access.pp
@@ -1,9 +1,17 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::openstack::base::cumin_access (
+    Array[OpenStack::ControlNode] $openstack_control_nodes = lookup('profile::openstack::base::openstack_control_nodes'),
     Hash[String[1], String[1]] $project_and_security_group_for_cumin_access = lookup('profile::openstack::base::project_and_security_group_for_cumin_access'),
 ) {
+    # only run on one node
+    if $::facts['networking']['fqdn'] == $openstack_control_nodes[0]['host_fqdn'] {
+        $ensure = 'present'
+    }
+    else {
+        $ensure = 'absent'
+    }
     class { '::openstack::apply_security_groups':
-        ensure                     => present,
+        ensure                     => $ensure,
         project_and_security_group => $project_and_security_group_for_cumin_access,
     }
 }
--- modules/profile/manifests/openstack/codfw1dev/cumin_access.pp
+++ modules/profile/manifests/openstack/codfw1dev/cumin_access.pp
@@ -1,8 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::openstack::codfw1dev::cumin_access (
+    Array[OpenStack::ControlNode] $openstack_control_nodes = lookup('profile::openstack::codfw1dev::openstack_control_nodes'),
     Hash[String[1], String[1]] $project_and_security_group_for_cumin_access = lookup('profile::openstack::codfw1dev::project_and_security_group_for_cumin_access'),
 ) {
     class { '::profile::openstack::base::cumin_access':
+        openstack_control_nodes                     => $openstack_control_nodes,
         project_and_security_group_for_cumin_access => $project_and_security_group_for_cumin_access,
     }
 }
