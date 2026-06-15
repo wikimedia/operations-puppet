12d1d114ba485bea4eff8f91bc313ef31d92847c
Daniel Zahn <dzahn@wikimedia.org>
2026-06-15 09:21:17 -0700

    systemd: add a new data type for a systemd mount
    In type Systemd::Unit::Type we already define all
    valid systemd types.
    
    But in type Systemd::Unit::Name we only accept
    service and timer as valid unit names.
    
    In my use case I need to mask tmp.mount but mount
    is not a valid unit name and puppet fails (I2de7fa22aee5225).
    
    Adding a new type for mount and adding it to valid unit names.
    
    Bug: T418299
    Change-Id: I68fd5f91be09422e4c779d177817416f6d5d3977

--- modules/systemd/types/unit/name.pp
+++ modules/systemd/types/unit/name.pp
@@ -1,2 +1,2 @@
 # SPDX-License-Identifier: Apache-2.0
-type Systemd::Unit::Name = Variant[Systemd::Service::Name, Systemd::Timer::Name, Systemd::Path::Name]
+type Systemd::Unit::Name = Variant[Systemd::Service::Name, Systemd::Timer::Name, Systemd::Path::Name, Systemd::Mount::Name]
