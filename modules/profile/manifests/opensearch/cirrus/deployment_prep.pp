43bb8fdca98a4ed63ec830a56f66d61d689a26b7
Brian King <bking@wikimedia.org>
2026-06-15 21:43:44 +0000

    cirrussearch: Add minimal opensearch config for deployment-prep
    Add a minimal OpenSearch config file so we can run OpenSearch in docker.
    This config runs standalone (no cluster) and persists data.
    
    With this change, we no longer have to reconcile the complexities of
    cirrussearch production with the peculiarities of deployment-prep. DP
    users still get the benefit of testing the same OpenSearch installation
    as production (including WMF custom plugins), but we can now update
    production without breaking deployment-prep and vice-versa.
    
    I've more or less manually recreated this change on
    `deployment-cirrussearch15.deployment-prep.eqiad1.wikimedia.cloud`
    in `/etc/systemd/system/cirrussearch.service.d/override.conf` if you
    want to see it in action.
    
    Bug: T425585
    Change-Id: Ia308328bfcf3eaa5deb1fa8b2a764c27b489c9de

--- modules/profile/manifests/opensearch/cirrus/deployment_prep.pp
+++ modules/profile/manifests/opensearch/cirrus/deployment_prep.pp
@@ -5,16 +5,21 @@
 # https://docker-registry.wikimedia.org/`repos/data-engineering`/opensearch/
 # @param image_name name of the OCI (docker) image
 # @param image_vers version of the OCI (docker) image
+# @param opensearch_uid UNIX user ID of OpenSearch OCI image
+# @param opensearch_gid UNIX group ID of OpenSearch OCI image
 
 class profile::opensearch::cirrus::deployment_prep (
     String $image_ns  = lookup('profile::opensearch::cirrus::oci_image::ns'),
     String $image_name = lookup('profile::opensearch::cirrus::oci_image::name'),
     String $image_vers = lookup('profile::opensearch::cirrus::oci_image::version'),
+    String $opensearch_uid = lookup('profile::opensearch::cirrus::oci_image::opensearch_uid', {default_value => '999'}),
+    String $opensearch_gid = lookup('profile::opensearch::cirrus::oci_image::opensearch_gid', {default_value => '999'}),
     String $base_data_dir = lookup('profile::opensearch::base_data_dir'),
 ) {
 
     require ::profile::docker::engine
     require ::profile::docker::ferm
+    require ::profile::tlsproxy::envoy
 
     service::docker { 'cirrussearch':
         namespace    => $image_ns,
@@ -23,7 +28,8 @@ class profile::opensearch::cirrus::deployment_prep (
         port         => 9200,
         host_network => true,
         volume       => true,
-        bind_mounts  => { "/srv/opensearch" => "/usr/share/opensearch/data" }
+        bind_mounts  =>   { '/srv/opensearch'                => '/usr/share/opensearch/data',
+                            '/etc/opensearch/opensearch.yml' => '/usr/share/opensearch/config/opensearch.yml' }
 
     }
 
@@ -32,9 +38,25 @@ class profile::opensearch::cirrus::deployment_prep (
     # defaults to '/srv/opensearch'
     file { $base_data_dir:
         ensure => 'directory',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755'
+        owner  => $opensearch_uid,
+        group  => $opensearch_gid,
+        mode   => '0700'
+    }
+
+    file { '/etc/opensearch':
+        ensure => 'directory',
+        owner  => $opensearch_uid,
+        group  => $opensearch_gid,
+        mode   => '0700'
+    }
+
+    file { '/etc/opensearch/opensearch.yml':
+        ensure => 'file',
+        owner  => $opensearch_uid,
+        group  => $opensearch_gid,
+        mode   => '0440',
+        source => 'puppet:///modules/opensearch/opensearch-deployment-prep.yml',
+        notify => Systemd::Service['cirrussearch']
     }
 
 }
