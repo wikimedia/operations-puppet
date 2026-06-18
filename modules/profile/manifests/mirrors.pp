7a4ea73ef554779bc58fe688496951620565f2f4
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-06-18 10:15:05 +0200

    mirrors: Remove obsolete users, groups and monitoring
    Bug: T416707
    Change-Id: I7e55ecb67dfdcd64e8a70c133a37ce0beee6565f

--- modules/profile/manifests/mirrors.pp
+++ modules/profile/manifests/mirrors.pp
@@ -2,39 +2,10 @@
 class profile::mirrors {
     include profile::mirrors::serve
 
-    $homedir = '/var/lib/mirror'
-
-    user { 'mirror':
-        ensure     => present,
-        gid        => 'mirror',
-        home       => $homedir,
-        shell      => '/bin/bash',
-        managehome => true,
-        system     => true,
-    }
-
-    group { 'mirror':
-        ensure => present,
-        name   => 'mirror',
-        system => true,
-    }
-
     file { '/srv/mirrors':
         ensure => directory,
         owner  => 'root',
         group  => 'root',
         mode   => '0444',
-    }
-
-    # monitoring for Debian/Ubuntu mirrors being in sync with upstream
-    nrpe::plugin { 'check_apt_mirror':
-        ensure => absent,
-    }
-
-    # export age of mirrors to Prometheus
-    prometheus::node_file_age { 'mirror_age_metrics':
-        ensure  => absent,
-        paths   => ['/srv/mirrors/debian', '/srv/mirrors/ubuntu'],
-        outfile => '/var/lib/prometheus/node.d/mirror-age.prom',
     }
 }
