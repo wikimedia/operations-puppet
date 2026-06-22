824a0aeaa0f6e25b8a6d2c88cb3e963ff36bcb32
Hugh Nowlan <hnowlan@wikimedia.org>
2026-06-22 17:48:28 +0100

    restbase: remove icinga disk space check, use alertmanager check
    Bug: T407141
    Depends-On: I70fba106e4873e0cbd24a76657d723d65d7cd03b
    Change-Id: Ia4b91190e1a0d3d08d9761167ea2a012835762c8

--- modules/profile/manifests/restbase.pp
+++ modules/profile/manifests/restbase.pp
@@ -184,19 +184,8 @@ class profile::restbase(
         check_command  => "check_http_port_url!${port}!/",
         contact_group  => 'admins,team-services',
         notes_url      => 'https://wikitech.wikimedia.org/wiki/RESTBase',
-        migration_task => 'TT407117',
+        migration_task => 'T407117',
     }
-
-
-    nrpe::monitor_service { 'restbase_instance_space':
-        ensure              => $ensure_monitor_restbase,
-        description         => 'Cassandra instance data free space',
-        notes_url           => 'https://wikitech.wikimedia.org/wiki/RESTBase#instance-data',
-        nrpe_command        => '/usr/lib/nagios/plugins/check_disk -w 30% -c 20% -p /srv/cassandra/instance-data',
-        migration_task      => 'T407141',
-        enable_icinga_check => false,
-    }
-
 
     # RESTBase rate limiting DHT firewall rule
     $rb_hosts_ferm = join($hosts, ' ')
