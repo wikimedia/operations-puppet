0e3d4080d710dd0ff4657a1e883bfefa0fc51702
Ben Tullis <btullis@wikimedia.org>
2026-06-24 16:57:44 +0100

    Temporarily remove dse-k8s-worker101[567] from service
    We have hit a hard limit of 32 hosts in the dse-k8s-eqiad cluster, but
    some of the newest nodes have 1 TB of RAM and these are unable to set up
    the necessary BGP sessions.
    
    We are going to change the size of the IP pool that we can use to
    allocate to pods, but in the meantime we are going to reduce the size of
    the cluster by three of the older nodes.
    
    These three have only 64 GB of RAM, so it makes sense to exclude them
    from the cluster. They were old snapshot* hosts that were purchased in
    2001, so they are among the oldest servers in the cluster.
    
    Bug: T429773
    Change-Id: Ib687aa83f7b31afccbb68d73cfd9890c3b167953

--- manifests/site.pp
+++ manifests/site.pp
@@ -1006,8 +1006,13 @@ node /^dse-k8s-etcd200[1-3]\.codfw\./ {
 }
 
 # dse-k8s-workers T29157, T3074009, T394647, T395557, T398438, T405209, T421465
-node /^dse-k8s-worker10(0[1-9]|1[0-9]|2[0-8])\.eqiad\./ {
+node /^dse-k8s-worker10(0[1-9]|1[0123489]|2[0-8])\.eqiad\./ {
     role(dse_k8s::worker)
+}
+
+# Temporarily remove dse-k8s-worker101[567] from service. See #T429773
+node /^dse-k8s-worker101[567]\.eqiad\./ {
+    role(insetup::data_platform_ferm)
 }
 
 # dse-k8s-workers in codfw - See #T353789, T399778
