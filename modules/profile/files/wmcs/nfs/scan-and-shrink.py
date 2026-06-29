32dd58f316ec5c41c8a6fb4ae81ddc2980a52922
Andrew Bogott <abogott@wikimedia.org>
2026-06-29 14:17:31 -0500

    Add profile::wmcs::nfs::daily_file_cleanup
    This takes hiera settings to designate a dir to scan,
    and a dir to place large files. Then, once a day:
    
    - Scan for giant files (> 1GB), move to trash dir with a note
    - Log large directories ( > 20GB)
    - Delete files in trash dir older than N days
    
    For starters this is in dry-run mode so we can make
    sure it won't do anything surprising.
    
    Hosts: paws-nfs-2.paws.eqiad1.wikimedia.cloud
    Bug: T429578
    Change-Id: Ica8c8db9b63c6c2061a23f887edcac4a69d7ac19

---
+++ modules/profile/files/wmcs/nfs/scan-and-shrink.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+# SPDX-License-Identifier: Apache-2.0
+#
+#  A rudimentary and ill-mannered script to patrol user file usage. Originally
+#  implemented to manage exploding NFS usage in PAWS.
+#
+#  Currently:
+#   Locate single, gigantic files and move them to a 'trash can' directory
+#   where they are queued for future deletion.
+#
+#  Todo:
+#   Detect home directories that are just generally huge without individual
+#   huge files; flag them for future attention someplace.
+#
+
+import argparse
+import logging
+import os
+from os import listdir
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="/var/log/scan-and-shrink.log", level=logging.INFO)
+
+
+# this is designed to search a set of home directories. So it doesn't actually
+#  look at files in the top scanpath directory, only in subdirs.
+def scan(maxfilesize: int, maxdirsize: int, scanpath: str, trashpath: str, dryrun: bool):
+    for i in listdir(scanpath):
+        totaldirsize = 0
+        sizes = {}
+
+        for root, dirs, files in os.walk(os.path.join(scanpath, i), topdown=False):
+            for name in files:
+                if not os.path.islink(os.path.join(root, name)):
+                    try:
+                        file_size = os.path.getsize(os.path.join(root, name))
+                    except OSError:
+                        logger.info("Failed to stat %s, skipping" % os.path.join(root, name))
+                        continue
+                    sizes[os.path.join(root, name)] = file_size
+        for k, v in sorted(sizes.items(), key=lambda item: item[1]):
+            totaldirsize += v
+            if v > maxfilesize:
+                local_path = os.path.dirname(k.replace(scanpath, ""))
+                file_name = os.path.basename(k.replace(scanpath, ""))
+                dest_path = trashpath + local_path + "/" + file_name
+
+                if dryrun:
+                    logger.info(f"{k} has size {v}, would move to {dest_path}")
+                else:
+                    os.makedirs(trashpath + local_path, exist_ok=True)
+                    try:
+                        os.rename(k, dest_path)
+                        with open(k, "w") as removed:
+                            removed.write(
+                                "This file has been removed to manage user storage "
+                                "size https://phabricator.wikimedia.org/T327936\n"
+                            )
+                    except OSError:
+                        logger.info("Failed to move %s, skipping" % k)
+        if totaldirsize > maxdirsize:
+            logger.warning(f"Found oversized dir {i} size {totaldirsize}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Scan disk usage and move large files")
+    parser.add_argument(
+        "-d",
+        "--dry-run",
+        action="store_true",
+        help="Log potential actions but don't move anything",
+    )
+    parser.add_argument(
+        "--maxfilesize", help="Move files larger than this. In MiB", type=int, default="1000"
+    )
+    parser.add_argument(
+        "--maxdirsize",
+        help="Log top-level dirs larger than this. In MiB",
+        type=int,
+        default="20000",
+    )
+    parser.add_argument("--trashpath", help="Large files are moved here", required=True)
+    parser.add_argument("--scanpath", help="Path to scan (recursive)", required=True)
+    args = parser.parse_args()
+
+    scan(
+        args.maxfilesize * 1024 * 1024,
+        args.maxdirsize * 1024 * 1024,
+        args.scanpath,
+        args.trashpath,
+        args.dry_run,
+    )
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ modules/profile/manifests/wmcs/nfs/daily_file_cleanup.pp
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: Apache-2.0
+
+
+# Recursively remove old files from a directory, where 'old' and 'directory'
+#  are defined in hiera. This was initially created to clean up
+#  old files on the PAWS nfs share with the following args:
+#
+# profile::wmcs::daily_file_cleanup::trash_path: /srv/paws/files-to-remove
+# profile::wmcs::daily_file_cleanup::user_home_dir: /srv/paws/project/paws/userhomes
+# profile::wmcs::daily_file_cleanup::age_in_days: 21
+#
+#
+class profile::wmcs::nfs::daily_file_cleanup(
+    Stdlib::Unixpath $trash_path = lookup('profile::wmcs::daily_file_cleanup::trash_path'),
+    Stdlib::Unixpath $user_home_dir = lookup('profile::wmcs::daily_file_cleanup::user_home_dir'),
+    Integer          $age_in_days  = lookup('profile::wmcs::daily_file_cleanup::age_in_days'),
+) {
+
+    file { '/usr/local/sbin/scan-and-shrink':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0744',
+        source => 'puppet:///modules/profile/wmcs/nfs/scan-and-shrink.py';
+    }
+
+    systemd::timer::job { 'scan_and_move_large_files':
+        ensure          => present,
+        description     => 'Moves large files via scan-and-shrink',
+        command         => "/usr/local/sbin/scan-and-shrink --dry-run --scanpath ${user_home_dir} --trashpath ${trash_path}",
+        interval        => {
+          'start'    => 'OnCalendar',
+          'interval' => '*-*-* 13:00:00',
+        },
+        logging_enabled => true,
+        user            => 'root',
+    }
+    systemd::timer::job { 'daily_file_cleanup':
+        ensure          => present,
+        description     => "Delete files older than ${age_in_days} from ${trash_path}",
+        command         => "find ${trash_path} -mindepth 1 -mtime +${age_in_days} -delete",
+        interval        => {
+          'start'    => 'OnCalendar',
+          'interval' => '*-*-* 1:00:00',
+        },
+        logging_enabled => true,
+        user            => 'root',
+    }
+}
