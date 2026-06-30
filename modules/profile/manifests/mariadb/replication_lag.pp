5c6db04610b0a8ece012c414d853e61f56a046d1
Manuel Arostegui <marostegui@wikimedia.org>
2026-06-30 17:02:10 +0200

    mariadb: Replace legacy facts
    In preparation for Puppet 8 we would like to disable legacy facts, which are no
    longer supported under Puppet 8. This commit migrates legacy facts to
    structured facts. The changes were made programmatically with rowlf[1],
    then split conditionally by hand.
    
    Changes for mariadb components only.
    
    Hosts: auto
    Bug: T372666
    Change-Id: I93c8154326fa48b439cac3e9ce54f3a61266a6b6

--- modules/profile/manifests/mariadb/replication_lag.pp
+++ modules/profile/manifests/mariadb/replication_lag.pp
@@ -3,8 +3,8 @@ define profile::mariadb::replication_lag(
 ){
     monitoring::check_prometheus { "mariadb-prolonged-lag-${title}":
         description     => "MariaDB sustained replica lag on ${title}",
-        dashboard_links => ["https://grafana.wikimedia.org/d/000000273/mysql?orgId=1&var-server=${::hostname}&var-port=${prom_port}"],
-        query           => "scalar(avg_over_time(mysql_slave_status_seconds_behind_master{instance=\"${::hostname}:${prom_port}\"}[5m]))",
+        dashboard_links => ["https://grafana.wikimedia.org/d/000000273/mysql?orgId=1&var-server=${facts['networking']['hostname']}&var-port=${prom_port}"],
+        query           => "scalar(avg_over_time(mysql_slave_status_seconds_behind_master{instance=\"${facts['networking']['hostname']}:${prom_port}\"}[5m]))",
         prometheus_url  => "http://prometheus.svc.${::site}.wmnet/ops",
         warning         => 5,
         critical        => 10,
