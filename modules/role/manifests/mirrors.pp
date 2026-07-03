3ba8a04dc6311ad90cc41665f44eaf9367488b7b
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-07-03 12:40:51 +0200

    Remove role:mirror and related Puppet classes
    mirrors.wikimedia.org has been removed.
    
    Bug: T416707
    Change-Id: I684189bcffe20f87f168b87c3c7155cad70454e7

--- modules/role/manifests/mirrors.pp
+++
@@ -1,8 +0,0 @@
-# Class: role::mirrors
-#
-# A role class used to setup our mirrors server.
-class role::mirrors {
-    include profile::base::production
-    include profile::firewall
-    include profile::mirrors
-}
