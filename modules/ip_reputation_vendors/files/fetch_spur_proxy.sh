9772390545af6cecab11831f91c46d3d3bf628fc
Fabrizio Furnari <ffurnari@wikimedia.org>
2026-07-06 12:39:39 +0200

    ip_reputation_vendors: add basic safeguard to spur downloader
    To avoid creating a corrupted file when downloaded from spur.us we
    introduce some basic bash safeguard on the dowloader script.
    This should fail early and not touch the existing mmdb file in case of
    errors.
    
    Change-Id: If137f36716c2e6b3bbc1d1f0186393aced721a73

--- modules/ip_reputation_vendors/files/fetch_spur_proxy.sh
+++ modules/ip_reputation_vendors/files/fetch_spur_proxy.sh
@@ -2,9 +2,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-curl --get "https://exports.spur.us/v1/feeds/anonymous-residential" \
+set -euo pipefail
+
+OUTPUT="${1:?Usage: $0 <output_path>}"
+TMPFILE="$(mktemp "$(dirname "$OUTPUT")/.spur_proxy.XXXXXX")"
+
+cleanup() {
+    rm -f "$TMPFILE"
+}
+trap cleanup EXIT
+
+curl --get \
+     "https://exports.spur.us/v1/feeds/anonymous-residential" \
      --data-urlencode "output=mmdb" \
      --data-urlencode "fields=network,callbackProxy" \
      -H "Token: $SPUR_TOKEN" \
-     -o "$1"
+     -f \
+     -s \
+     --show-error \
+     -o "$TMPFILE"
+
+mv -f "$TMPFILE" "$OUTPUT"
 
