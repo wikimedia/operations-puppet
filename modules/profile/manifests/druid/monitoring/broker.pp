9ca81ad2bdc732d521e206e79757c5adc3efb891
Jesse Hathaway <jhathaway@wikimedia.org>
2026-07-08 13:52:09 -0500

    Puppet 8: Replace legacy facts in profile druid
    In preparation for Puppet 8 we would like to disable legacy facts, which are no
    longer supported under Puppet 8. This commit migrates legacy facts to
    structured facts. The changes were made programmatically with rowlf[1].
    
    [1]: https://gitlab.wikimedia.org/repos/sre/rowlf
    
    Bug: T372666
    Hosts: auto
    Change-Id: I0a44af164b27e7678a162fb9d8d71a52004ddf59

--- modules/profile/manifests/druid/monitoring/broker.pp
+++ modules/profile/manifests/druid/monitoring/broker.pp
@@ -6,9 +6,9 @@
 class profile::druid::monitoring::broker {
     $jmx_exporter_config_file = '/etc/prometheus/druid_broker_jmx_exporter.yaml'
     $prometheus_jmx_exporter_broker_port = 8182
-    $java_opts = "-javaagent:/usr/share/java/prometheus/jmx_prometheus_javaagent.jar=${::ipaddress}:${prometheus_jmx_exporter_broker_port}:${jmx_exporter_config_file}"
-    profile::prometheus::jmx_exporter { "druid_broker_${::hostname}":
-        hostname    => $::hostname,
+    $java_opts = "-javaagent:/usr/share/java/prometheus/jmx_prometheus_javaagent.jar=${facts['networking']['ip']}:${prometheus_jmx_exporter_broker_port}:${jmx_exporter_config_file}"
+    profile::prometheus::jmx_exporter { "druid_broker_${facts['networking']['hostname']}":
+        hostname    => $facts['networking']['hostname'],
         port        => $prometheus_jmx_exporter_broker_port,
         config_file => $jmx_exporter_config_file,
         config_dir  => '/etc/prometheus',
--- modules/profile/manifests/druid/monitoring/coordinator.pp
+++ modules/profile/manifests/druid/monitoring/coordinator.pp
@@ -6,9 +6,9 @@
 class profile::druid::monitoring::coordinator {
     $jmx_exporter_config_file = '/etc/prometheus/druid_coordinator_jmx_exporter.yaml'
     $prometheus_jmx_exporter_coordinator_port = 8181
-    $java_opts = "-javaagent:/usr/share/java/prometheus/jmx_prometheus_javaagent.jar=${::ipaddress}:${prometheus_jmx_exporter_coordinator_port}:${jmx_exporter_config_file}"
-    profile::prometheus::jmx_exporter { "druid_coordinator_${::hostname}":
-        hostname    => $::hostname,
+    $java_opts = "-javaagent:/usr/share/java/prometheus/jmx_prometheus_javaagent.jar=${facts['networking']['ip']}:${prometheus_jmx_exporter_coordinator_port}:${jmx_exporter_config_file}"
+    profile::prometheus::jmx_exporter { "druid_coordinator_${facts['networking']['hostname']}":
+        hostname    => $facts['networking']['hostname'],
         port        => $prometheus_jmx_exporter_coordinator_port,
         config_file => $jmx_exporter_config_file,
         config_dir  => '/etc/prometheus',
