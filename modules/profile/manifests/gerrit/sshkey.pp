33432f23d110d0165b9ae65b2b380beb72caacd2
Ahmon Dancy <adancy@wikimedia.org>
2026-07-13 14:55:02 -0700

    ssh/gerrit: add support for handling multiple ssh keys
    Refactor profile::gerrit::sshkey to handle multiple keys.
    
    Update ssh::publish_fingerprints template to prefer the resource's
    'name' parameter over its Puppet title when rendering known_hosts
    entries.
    
    Bug: T240266
    Change-Id: Ib8f3a9c7e2d4f1a6b5c9e8d7f2a4b3c6d9e1f8a5

--- modules/profile/manifests/gerrit/sshkey.pp
+++ modules/profile/manifests/gerrit/sshkey.pp
@@ -1,50 +1,60 @@
 # SPDX-License-Identifier: Apache-2.0
-# @summary small wrapper to manage the gerrit key
+# @summary small wrapper to manage the gerrit host keys
 # @param ensure ensurable param
-# @param exported wether to export the resource
-# @param override the default target
+# @param exported whether to export the resource
 define profile::gerrit::sshkey (
     Wmflib::Ensure             $ensure   = 'present',
     Boolean                    $exported = false,
     Optional[Stdlib::Unixpath] $target   = undef,
 ) {
-    $params = {
-        'ensure' => $ensure,
-        'name'   => 'gerrit.wikimedia.org',
-        'host_aliases' => [
-            # Host used internally for direct connections
-            'gerrit.discovery.wmnet',
-            ipresolve('gerrit.discovery.wmnet', 4),
-            ipresolve('gerrit.discovery.wmnet', 6),
+    $host_aliases = [
+        # Host used internally for direct connections
+        'gerrit.discovery.wmnet',
+        ipresolve('gerrit.discovery.wmnet', 4),
+        ipresolve('gerrit.discovery.wmnet', 6),
 
-            # Load balancers for public traffic
-            ipresolve('gerrit-lb.eqiad.wikimedia.org', 4),
-            ipresolve('gerrit-lb.eqiad.wikimedia.org', 6),
-            ipresolve('gerrit-lb.codfw.wikimedia.org', 4),
-            ipresolve('gerrit-lb.codfw.wikimedia.org', 6),
-            ipresolve('gerrit-lb.esams.wikimedia.org', 4),
-            ipresolve('gerrit-lb.esams.wikimedia.org', 6),
-            ipresolve('gerrit-lb.ulsfo.wikimedia.org', 4),
-            ipresolve('gerrit-lb.ulsfo.wikimedia.org', 6),
-            ipresolve('gerrit-lb.eqsin.wikimedia.org', 4),
-            ipresolve('gerrit-lb.eqsin.wikimedia.org', 6),
-            ipresolve('gerrit-lb.drmrs.wikimedia.org', 4),
-            ipresolve('gerrit-lb.drmrs.wikimedia.org', 6),
-            ipresolve('gerrit-lb.magru.wikimedia.org', 4),
-            ipresolve('gerrit-lb.magru.wikimedia.org', 6),
-        ],
-        'key'    => 'AAAAB3NzaC1yc2EAAAADAQABAAAAgQCF8pwFLehzCXhbF1jfHWtd9d1LFq2NirplEBQYs7AOrGwQ/6ZZI0gvZFYiEiaw1o+F1CMfoHdny1VfWOJF3mJ1y9QMKAacc8/Z3tG39jBKRQCuxmYLO1SWymv7/Uvx9WQlkNRoTdTTa9OJFy6UqvLQEXKYaokfMIUHZ+oVFf1CgQ==',
-        'type'   => 'ssh-rsa',
-        'target' => $target,
+        # Load balancers for public traffic
+        ipresolve('gerrit-lb.eqiad.wikimedia.org', 4),
+        ipresolve('gerrit-lb.eqiad.wikimedia.org', 6),
+        ipresolve('gerrit-lb.codfw.wikimedia.org', 4),
+        ipresolve('gerrit-lb.codfw.wikimedia.org', 6),
+        ipresolve('gerrit-lb.esams.wikimedia.org', 4),
+        ipresolve('gerrit-lb.esams.wikimedia.org', 6),
+        ipresolve('gerrit-lb.ulsfo.wikimedia.org', 4),
+        ipresolve('gerrit-lb.ulsfo.wikimedia.org', 6),
+        ipresolve('gerrit-lb.eqsin.wikimedia.org', 4),
+        ipresolve('gerrit-lb.eqsin.wikimedia.org', 6),
+        ipresolve('gerrit-lb.drmrs.wikimedia.org', 4),
+        ipresolve('gerrit-lb.drmrs.wikimedia.org', 6),
+        ipresolve('gerrit-lb.magru.wikimedia.org', 4),
+        ipresolve('gerrit-lb.magru.wikimedia.org', 6),
+    ]
+
+    # One entry per host key type/algorithm (T240266)
+    $keys = {
+        'ssh-rsa'     => 'AAAAB3NzaC1yc2EAAAADAQABAAAAgQCF8pwFLehzCXhbF1jfHWtd9d1LFq2NirplEBQYs7AOrGwQ/6ZZI0gvZFYiEiaw1o+F1CMfoHdny1VfWOJF3mJ1y9QMKAacc8/Z3tG39jBKRQCuxmYLO1SWymv7/Uvx9WQlkNRoTdTTa9OJFy6UqvLQEXKYaokfMIUHZ+oVFf1CgQ==',
+        'ssh-ed25519' => 'AAAAC3NzaC1lZDI1NTE5AAAAIFp+VIoTbE8Js9fwRCUy9KnSAewDQa2f6Dwi77R7IqS7',
     }
 
-    if $exported {
-        @@sshkey { $title:
-            * => $params,
+    $keys.each |$type, $key| {
+        $params = {
+            'ensure'       => $ensure,
+            'name'         => 'gerrit.wikimedia.org',
+            'host_aliases' => $host_aliases,
+            'key'          => $key,
+            'type'         => $type,
+            'target'       => $target,
         }
-    } else {
-        sshkey { $title:
-            * => $params,
+
+        $key_title = "${title}@${type}"
+        if $exported {
+            @@sshkey { $key_title:
+                * => $params,
+            }
+        } else {
+            sshkey { $key_title:
+                * => $params,
+            }
         }
     }
 }
