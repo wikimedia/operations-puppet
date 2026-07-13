a1538685990010708696e0788acc84402400ec75
Taavi Väänänen <taavi@wikimedia.org>
2026-07-13 12:44:13 +0300

    P:toolforge::k8s::haproxy: Increase open file descriptor limit
    The current limit is not enough for handling all connection spikes.
    
    Bug: T431946
    Change-Id: I5f519defe18fd41aadd25ccdfee61bdc6a6a6964

--- modules/profile/manifests/toolforge/k8s/haproxy.pp
+++ modules/profile/manifests/toolforge/k8s/haproxy.pp
@@ -35,6 +35,11 @@ class profile::toolforge::k8s::haproxy (
         # No Icinga support here
         monitor          => false,
     }
+    systemd::unit { 'haproxy':
+        source   => 'puppet:///modules/profile/toolforge/k8s/haproxy/haproxy.override.service',
+        override => true,
+        restart  => true,
+    }
 
     include profile::haproxy::resolver
 
