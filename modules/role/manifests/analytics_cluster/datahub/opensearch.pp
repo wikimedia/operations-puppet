2ea0da49fa20c4f387600a08a7c5d7fcd64984a7
Brian King <bking@wikimedia.org>
2026-07-16 20:01:27 +0000

    datahubsearch: remove references to Puppet plans
    We are in the process of decommissioning these hosts.
    Remove their Puppet plans and related config.
    
    Bug: T432148
    Change-Id: Ia1ff3e44172919010b2e5d1b1f17e648b5369f47

--- modules/role/manifests/analytics_cluster/datahub/opensearch.pp
+++
@@ -1,9 +0,0 @@
-class role::analytics_cluster::datahub::opensearch {
-    include profile::base::production
-    include profile::firewall
-    include profile::opensearch::datahubsearch
-    include profile::rsyslog::udp_json_logback_compat
-    include profile::opensearch::monitoring::base_checks
-    include profile::lvs::realserver
-    include profile::lvs::realserver::ipip
-}
