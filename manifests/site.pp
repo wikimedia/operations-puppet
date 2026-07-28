3d9562b22ca3d795942274eb5ed269509988b9d1
Manuel Arostegui <marostegui@wikimedia.org>
2026-07-28 08:41:06 +0200

    mariadb: Promote db1228 to m5 master
    Bug: T432967
    Change-Id: I6802cd86fc058ccbd921051916d9d29ebe9330bf

--- manifests/site.pp
+++ manifests/site.pp
@@ -740,12 +740,12 @@ node /^db2234\.codfw\./ {
 ## m5 shard
 # See also multiinstance misc hosts db1217, db2160 below
 
-# m5 master
+# old m5 master
 node /^db1164\.eqiad\./ {
     role(mariadb::misc)
 }
 
-# Future m5 master
+# m5 master
 node /^db1228\.eqiad\./ {
     role(mariadb::misc)
 }
