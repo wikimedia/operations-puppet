75b1bd8fdf025f66f35ca95087c14540ce1fef27
Ceri Williams <cwilliams@wikimedia.org>
2026-07-28 10:50:01 +0100

    profile::mariadb::replication_lag: remove the check from monitoring
    This seems to be the main cause for a threshold being exceeded when waiting
    for Icinga to go green during automation, as a false positive rather than
    a real problem existing.
    
    * Forcing mariadb-prolonged-lag-X to be absent
    
    Bug: T433344
    Change-Id: Ie3ac108df91cf3e0b7eb436bda58d2b2da1d0454

--- modules/profile/manifests/mariadb/replication_lag.pp
+++ modules/profile/manifests/mariadb/replication_lag.pp
@@ -2,6 +2,7 @@ define profile::mariadb::replication_lag(
     Integer $prom_port = 9104,
 ){
     monitoring::check_prometheus { "mariadb-prolonged-lag-${title}":
+        ensure          => absent, # T433344
         description     => "MariaDB sustained replica lag on ${title}",
         dashboard_links => ["https://grafana.wikimedia.org/d/000000273/mysql?orgId=1&var-server=${facts['networking']['hostname']}&var-port=${prom_port}"],
         query           => "scalar(avg_over_time(mysql_slave_status_seconds_behind_master{instance=\"${facts['networking']['hostname']}:${prom_port}\"}[5m]))",
