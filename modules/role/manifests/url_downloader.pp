e16627a31166ecccf8f982c17a6105ee158a9d76
Sukhbir Singh <ssingh@wikimedia.org>
2026-07-29 10:37:18 -0400

    O:url_downloader: add LVS/IPIP profiles
    These are required to actually enable urldownloaders to be behind LVS
    low-traffic with IPIP enabled.
    
    Bug: T429175
    Change-Id: I03d90c63a47056d73c2c8f3de5216031a1e0be7f

--- modules/role/manifests/url_downloader.pp
+++ modules/role/manifests/url_downloader.pp
@@ -2,4 +2,6 @@ class role::url_downloader {
     include profile::base::production
     include profile::firewall
     include profile::url_downloader
+    include profile::lvs::realserver
+    include profile::lvs::realserver::ipip
 }
