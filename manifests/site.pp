5f611b96d78b343ed44abd8a9cc6cf624843c137
Daniel Zahn <dzahn@wikimedia.org>
2026-07-30 14:00:08 -0700

    site: add zuul1005 as a zuul executor
    Test the zuul executor role on a physical machine.
    
    Bug: T427353
    Change-Id: I39f9f281c56e162798345644f1fcdbb409b22755

--- manifests/site.pp
+++ manifests/site.pp
@@ -2601,7 +2601,11 @@ node /^zuul1004\.eqiad\./ {
     role(zuul::main)
 }
 
-node /^zuul100[5-7]\.eqiad\./ {
+node /^zuul1005\.eqiad\./ {
+    role(zuul::executor)
+}
+
+node /^zuul100[6-7]\.eqiad\./ {
     role(insetup::collaboration_services_nftables)
 }
 
