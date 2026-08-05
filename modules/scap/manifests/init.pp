a212e074bd833ffb028cb62a095b83f04e19eee4
Ahmon Dancy <adancy@wikimedia.org>
2026-08-05 16:05:39 -0700

    scap: Add optional logstash credentials file
    Scap masters can install /etc/scap/logstash_credentials from the private
    repo and set logstash_credentials_file in scap.cfg. Scap uses the file
    to authenticate its logstash queries.
    
    Only scap masters query logstash, so hosts with is_master false do not
    get the file. The group must let the deployer and the spiderpig user
    read the file, so production uses the deployment group.
    
    Enabled for deployment-prep. The secret must be in the deployment-prep
    private repo before this merges.
    
    Also remove ":9200" from beta logstash_url.  Port 80 should be used for auth.
    
    Bug: T434114
    Change-Id: I275ece743c9e7502254f9d04101b3b5c48563efd

--- modules/scap/manifests/init.pp
+++ modules/scap/manifests/init.pp
@@ -7,6 +7,16 @@
 #    Master scap rsync host in the wmflabs domain.
 #    Default 'deployment-deploy04.deployment-prep.eqiad1.wikimedia.cloud'.
 # @param is_master indicates if the server is a scap::master
+#
+# @param manage_logstash_credentials
+#    If true, install /etc/scap/logstash_credentials from the private repo and
+#    tell scap to use it. Scap then authenticates its logstash queries. Only
+#    scap masters query logstash, so the file is installed on masters only.
+#
+# @param logstash_credentials_group
+#    Group that can read the logstash credentials file. Keep this the same as
+#    the 'deployment_group' hiera key, since scap runs both as the deployer
+#    and as the spiderpig user.
 class scap (
     Variant[Stdlib::Host,String] $deployment_server = 'deployment',
     Stdlib::Fqdn $wmflabs_master                    = 'deployment-deploy04.deployment-prep.eqiad1.wikimedia.cloud',
@@ -15,6 +25,8 @@ class scap (
     Boolean      $is_master                         = false,
     Optional[Hash] $k8s_deployments                 = {},
     Boolean $enable_bootstrapping                   = true,
+    Boolean $manage_logstash_credentials            = false,
+    String[1] $logstash_credentials_group           = 'wikidev',
 ) {
     require git::lfs
     include scap::user
@@ -54,6 +66,27 @@ class scap (
     $k8s_releases_dir = pick($k8s_deployments['releases_dir'], '/etc/mediawiki/releases')
     $k8s_clusters = $k8s_deployments['clusters']
     $k8s_deployments_file = $k8s_deployments['file']
+
+    # T434114: Credentials for authenticated logstash queries. Only scap
+    # masters query logstash.
+    $logstash_credentials_file = '/etc/scap/logstash_credentials'
+    $use_logstash_credentials = $is_master and $manage_logstash_credentials
+    if $use_logstash_credentials {
+        wmflib::dir::mkdir_p('/etc/scap')
+
+        file { $logstash_credentials_file:
+            ensure    => present,
+            owner     => 'root',
+            group     => $logstash_credentials_group,
+            mode      => '0440',
+            content   => secret('scap/logstash_credentials'),
+            show_diff => false,
+        }
+    } else {
+        file { $logstash_credentials_file:
+            ensure => absent,
+        }
+    }
 
     # Disable Scap deployments on the inactive deployment server
     $block_scap_deployments = $is_master and $facts['networking']['fqdn'] != $deployment_server
