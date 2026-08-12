d18499d3771506a2b114bfa05fae5fe9668b07de
Brian King <bking@wikimedia.org>
2026-08-12 14:24:42 +0000

    apifeatureusage: Use correct java version
    Use Java 17 when instantiating the logstash class,
    as Java 11 is not available by default in Bookworm.
    
    I checked an Observability log collector
    (logstash1024) which is currently running the same
    version of logstash that we are targeting
    (7.16.3) and that host is using Java 17.
    
    Hosts: apifeatureusage2001.codfw.wmnet
    Bug: T433890
    Change-Id: I34d6ab2b51a69cb01770c9eccb0850e540217a60

--- modules/profile/manifests/apifeatureusage/logstash.pp
+++ modules/profile/manifests/apifeatureusage/logstash.pp
@@ -63,7 +63,7 @@ class profile::apifeatureusage::logstash (
         jmx_exporter_port   => $jmx_exporter_port,
         jmx_exporter_config => $jmx_exporter_config_file,
         pipeline_workers    => $facts['processors']['count'] * 2,
-        java_package        => 'openjdk-11-jdk',
+        java_package        => 'openjdk-17-jdk-headless',
         log_format          => 'json',
         gc_log              => false,
         java_home           => pick($java_home, $profile::java::default_java_home),
