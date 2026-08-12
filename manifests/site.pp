2c3595ba7986f632ff06a9c627b4fab81fd96135
Ceri Williams <cwilliams@wikimedia.org>
2026-08-12 17:42:11 +0100

    mariadb: Productionize db1277
    Adding db1277 to s1 to replace db1163.
    
    Bug: T407942
    Change-Id: I18f6e2bdfe2f036aee6508f6b08a2ac25d5fc7d0

--- manifests/site.pp
+++ manifests/site.pp
@@ -485,7 +485,7 @@ node /^db1208\.eqiad\./ {
 }
 
 # s1 (enwiki) core production dbs on eqiad
-node /^db1(163|169|184|186|195|206|218|219|232|234|235|251)\.eqiad\./ {
+node /^db1(163|169|184|186|195|206|218|219|232|234|235|251|277)\.eqiad\./ {
     role(mariadb::core)
 }
 
@@ -667,7 +667,7 @@ node /^db2(186|191|196|215|231|249)\.codfw\./ {
 }
 
 # Refresh hosts T418909
-node /^db12(69|70|76|77|79|80|81|82|83|84|86|87|88|89)\.eqiad\./ {
+node /^db12(69|70|76|79|80|81|82|83|84|86|87|88|89)\.eqiad\./ {
     role(insetup::data_persistence_ferm)
 }
 
