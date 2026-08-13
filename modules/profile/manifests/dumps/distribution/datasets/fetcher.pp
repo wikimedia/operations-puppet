a23d51c29c0e2f5faa4de8b82a1cf296ed1b2b5d
Taavi Väänänen <taavi@wikimedia.org>
2026-08-13 17:18:01 +0300

    P:dumps: Remove unused variable
    Bug: T417824
    Change-Id: If71da8a5ceeca176ee9f8f04c6f83da96a6a6964

--- modules/profile/manifests/dumps/distribution/datasets/fetcher.pp
+++ modules/profile/manifests/dumps/distribution/datasets/fetcher.pp
@@ -3,7 +3,6 @@ class profile::dumps::distribution::datasets::fetcher(
     Stdlib::Unixpath $xmldumpsdir = lookup('profile::dumps::distribution::xmldumpspublicdir'),
     Stdlib::Unixpath $miscdatasetsdir = lookup('profile::dumps::distribution::miscdumpsdir'),
     Hash $rsyncer_settings = lookup('profile::dumps::distribution::rsync_config'),
-    Stdlib::Host $phab_dump_host = lookup('profile::dumps::distribution::phab_dump_host'),
 ) {
 
     $user = $rsyncer_settings['dumps_user']
