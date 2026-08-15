fe28359b39c8b3fc5b937e47a016adceefb6f6f9
Scott French <swfrench@wikimedia.org>
2026-08-14 19:55:02 -0700

    P:etcd::replication: Infer an https-compatible default dst_url
    When changing the etcd mirror replication host, two closely coupled
    puppet changes are required: one to enable replication on said host,
    and another to set dst_url in a consistent way.
    
    Instead, construct a default dst_url to target the local etcd server's
    standard client port via https, which is always what we want.
    
    This avoids the potentially error-prone coupled change.
    
    Hosts: conf1007.eqiad.wmnet
    Hosts: conf1008.eqiad.wmnet
    Bug: T435103
    Change-Id: Iddf39fdc0dbafa0d97d4e3bb7d14d3246a6a6964

--- modules/profile/manifests/etcd/replication.pp
+++ modules/profile/manifests/etcd/replication.pp
@@ -14,9 +14,10 @@
 #
 # [*active*] If replication is active. For now, only one server per cluster
 #
-# [*dst_url*] scheme:hostname:port combination of the target etcd instance
+# [*dst_url*] scheme://hostname:port combination of the target etcd instance
 #             (namely the one receiving the replicated data from etcd mirror).
-#             Default: http://localhost:2378
+#             Default: https://FDQN:2379 where FQDN is that of the host where
+#                      etcd mirror is running.
 #
 # [*src_port*] Client port of the origin etcd instance
 #              (namely the one replicating data via etcd mirror).
@@ -26,7 +27,7 @@ class profile::etcd::replication(
     Hash $origin = lookup('profile::etcd::replication::origin'),
     Stdlib::Unixpath $destination_path = lookup('profile::etcd::replication::destination_path'),
     Boolean $active = lookup('profile::etcd::replication::active'),
-    Stdlib::Httpurl $dst_url = lookup('profile::etcd::replication::dst_url', {'default_value' => 'http://localhost:2378'}),
+    Optional[Stdlib::Httpurl] $dst_url = lookup('profile::etcd::replication::dst_url', {'default_value' => undef}),
     Stdlib::Port $src_port = lookup('profile::etcd::replication::src_port', {'default_value' => 2379})
 ) {
     require ::passwords::etcd
@@ -34,13 +35,19 @@ class profile::etcd::replication(
     # Replica is always from remote to local. This means only the local account
     # is needed.
     $resource_title = "${origin['path']}@${origin['cluster_name']}"
+    # If no destination is specified, default to https to the standard
+    # client port on the mirror host.
+    $destination_url = $dst_url ? {
+        undef => "https://${facts['networking']['fqdn']}:2379",
+        default => $dst_url,
+    }
 
     $hosts = fqdn_rotate($origin['servers'])
     etcdmirror::instance { $resource_title:
         src                   => "https://${hosts[0]}:${src_port}",
         src_path              => $origin['path'],
         src_ignore_keys_regex => $origin['ignore_keys_regex'],
-        dst                   => $dst_url,
+        dst                   => $destination_url,
         dst_path              => $destination_path,
         enable                => $active,
     }
