79f22f5ac83c946911e331d43c8046bcd418bde3
Manuel Arostegui <marostegui@wikimedia.org>
2026-08-17 14:22:40 +0200

    mariadb: Productionize db1284
    It will replace db1170
    
    Bug: T407942
    Change-Id: Ifc8ea17e70136b8ea810457cfba41a874ebe3013

--- manifests/site.pp
+++ manifests/site.pp
@@ -608,7 +608,7 @@ node /^db2(158|169|180|193|214|217|224|229)\.codfw\./ {
 }
 
 # s7 (centralauth, meta et al.) core production dbs on eqiad
-node /^db1(069|170|174|181|191|194|202|227|231|236|253|288)\.eqiad\./ {
+node /^db1(069|170|174|181|191|194|202|227|231|236|253|284|288)\.eqiad\./ {
     role(mariadb::core)
 }
 
@@ -675,7 +675,7 @@ node /^db2(186|191|196|215|231|249)\.codfw\./ {
 }
 
 # Refresh hosts T418909
-node /^db12(69|70|76|82|83|84)\.eqiad\./ {
+node /^db12(69|70|76|82|83)\.eqiad\./ {
     role(insetup::data_persistence_ferm)
 }
 
