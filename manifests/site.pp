5320ec8cb6ddefe4ace8c32b49262bf250d2892d
Manuel Arostegui <marostegui@wikimedia.org>
2026-08-17 09:53:49 +0200

    mariadb: Productionize db1287
    This host will replace db1173
    
    Bug: T407942
    Change-Id: I7ac8d1834a2c6df77ff2cab5e641490cbe0a356e

--- manifests/site.pp
+++ manifests/site.pp
@@ -589,7 +589,7 @@ node /^db2(157|171|178|192|211|213|223|228)\.codfw\./ {
 }
 
 # s6 (frwiki, jawiki, ruwiki labswiki) core production dbs on eqiad
-node /^db1(168|173|180|187|201)\.eqiad\./ {
+node /^db1(168|173|180|187|201|287)\.eqiad\./ {
     role(mariadb::core)
 }
 
@@ -675,7 +675,7 @@ node /^db2(186|191|196|215|231|249)\.codfw\./ {
 }
 
 # Refresh hosts T418909
-node /^db12(69|70|76|82|83|84|86|87)\.eqiad\./ {
+node /^db12(69|70|76|82|83|84|86)\.eqiad\./ {
     role(insetup::data_persistence_ferm)
 }
 
