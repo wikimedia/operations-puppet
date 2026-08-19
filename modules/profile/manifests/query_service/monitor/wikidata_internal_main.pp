d3c902cf63dd1651a1dae03906b87bd674b4d049
Brian King <bking@wikimedia.org>
2026-08-19 19:28:06 +0000

    wdqs: Absent unneeded monitors
    Nagios-based checks are deprecated, and these
    particular checks have already been replaced by
    Prometheus blackbox checks (ref
    Ic77828c3669c151525898ccca2bbee18de046513 among others).
    
    Absent them to ensure their removal.
    A subsequent patch will remove them from Puppet completely.
    
    Bug: T358029
    Hosts: wdqs1025.eqiad.wmnet, wdqs1027.eqiad.wmnet, wdqs1011.eqiad.wmnet, wdqs2007.codfw.wmnet, wdqs1021.eqiad.wmnet, wdqs1023.eqiad.wmnet
    Change-Id: I2393bd20d49bc1ee1e2153cd8ccb7600d3302a26

--- modules/profile/manifests/query_service/monitor/wikidata_internal_main.pp
+++ modules/profile/manifests/query_service/monitor/wikidata_internal_main.pp
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::query_service::monitor::wikidata_internal_main {
     nrpe::monitor_service { 'Query_Service_Internal_Main_HTTP_endpoint':
+        ensure         => 'absent',
         description    => 'Internal query service (main) HTTP Port 80',
         nrpe_command   => '/usr/lib/nagios/plugins/check_http -H 127.0.0.1 -p 80 -w 10 -u /readiness-probe',
         notes_url      => 'https://wikitech.wikimedia.org/wiki/Wikidata_query_service',
--- modules/profile/manifests/query_service/monitor/wikidata_internal_scholarly.pp
+++ modules/profile/manifests/query_service/monitor/wikidata_internal_scholarly.pp
@@ -1,10 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 class profile::query_service::monitor::wikidata_internal_scholarly {
     nrpe::monitor_service { 'Query_Service_Internal_Scholarly_HTTP_endpoint':
+        ensure         => 'absent',
         description    => 'Internal query service (scholarly) HTTP Port 80',
         nrpe_command   => '/usr/lib/nagios/plugins/check_http -H 127.0.0.1 -p 80 -w 10 -u /readiness-probe',
         notes_url      => 'https://wikitech.wikimedia.org/wiki/Wikidata_query_service',
         migration_task => 'T358029',
+
     }
     profile::query_service::monitor::sparql_endpoint {
         default:
