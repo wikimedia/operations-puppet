80f1f6a16a0c0278cb9a8c262d1a685f443fcb79
Scott French <swfrench@wikimedia.org>
2026-08-20 17:04:39 +0000

    Revert "trafficserver: Support testwiki pretrain routing in XWD"
    This reverts commit d6f92c0d60041ff4a0ad7a0b570b92313eb9bb82.
    
    Reason for revert: Needs backward compatibility fixes.
    
    In short, we need to ensure that using the existing k8s-mwdebug
    backends works transparently for testwiki, at least initially.
    
    Bug: T427666
    Change-Id: I02faa550c1c50502c0ff5242dbe9a729f9d6044d

--- modules/profile/files/trafficserver/x-wikimedia-debug-routing_test.lua
+++ modules/profile/files/trafficserver/x-wikimedia-debug-routing_test.lua
@@ -1,4 +1,11 @@
 -- SPDX-License-Identifier: Apache-2.0
+_G.ts = {
+  client_request = {
+    header = {},
+    get_uri = function() return "/" end
+  },
+  http = {}
+}
 
 -- Initialize TS Lua remap status constants to distinct legible values. This
 -- allows us to properly assert on do_remap return value.
@@ -10,25 +17,6 @@ _G.TS_LUA_CONFIG_HTTP_CACHE_HTTP = "HTTP_CACHE_HTTP"
 describe("Busted unit testing framework", function()
   describe("script for ATS Lua Plugin", function()
 
-    before_each(function()
-      -- Reinitialize the ts global prior to each test case to avoid subtle
-      -- correctness issues and / or order-dependent tests.
-      _G.ts = {
-        client_request = {
-          header = {},
-          -- get_uri is called in nearly all test cases, but specialized in
-          -- only a few. Provide a simple default fake, rather than forcing
-          -- all test cases to stub.
-          get_uri = function() return "/" end,
-        },
-        http = {
-          -- config_int_set is similarly widely called, but rarely specialized,
-          -- so provide a simple default fake.
-          config_int_set = function() end
-        },
-      }
-    end)
-
     it("test - do_remap without X-Wikimedia-Debug request header", function()
       require("x-wikimedia-debug-routing")
 
@@ -38,6 +26,7 @@ describe("Busted unit testing framework", function()
     it("test - valid X-Wikimedia-Debug eqiad", function()
       stub(ts.client_request, "set_url_host")
       stub(ts.client_request, "set_url_port")
+      stub(ts, "hook")
       stub(ts.http, "config_int_set")
 
       require("x-wikimedia-debug-routing")
@@ -54,6 +43,7 @@ describe("Busted unit testing framework", function()
     it("test - X-Wikimedia-Debug with hostname only", function()
       stub(ts.client_request, "set_url_host")
       stub(ts.client_request, "set_url_port")
+      stub(ts, "hook")
       stub(ts.http, "config_int_set")
 
       require("x-wikimedia-debug-routing")
@@ -130,6 +120,7 @@ describe("Busted unit testing framework", function()
     it("test - X-Wikimedia-Debug with invalid value", function()
       stub(ts.client_request, "set_url_host")
       stub(ts.client_request, "set_url_port")
+      stub(ts, "hook")
       stub(ts.http, "set_resp")
 
       require("x-wikimedia-debug-routing")
@@ -138,55 +129,7 @@ describe("Busted unit testing framework", function()
 
       assert.are.equals(TS_LUA_REMAP_NO_REMAP_STOP, do_remap())
 
-      assert.stub(ts.http.set_resp).was.called_with(400, "x-wikimedia-debug-routing: no match found for the backend specified in X-Wikimedia-Debug (scope: default)")
-    end)
-
-    it("test - X-Wikimedia-Debug on k8s pretrain for testwiki", function()
-      stub(ts.client_request, "set_url_host")
-      stub(ts.client_request, "set_url_port")
-      stub(ts.http, "set_resp")
-
-      require("x-wikimedia-debug-routing")
-
-      _G.ts.client_request.header['X-Wikimedia-Debug'] = "backend=k8s-mw-pretrain"
-      _G.ts.client_request.header['Host'] = 'test.wikipedia.org'
-
-      assert.are.equals(TS_LUA_REMAP_DID_REMAP_STOP, do_remap())
-
-      assert.stub(ts.client_request.set_url_host).was.called_with("mw-pretrain.discovery.wmnet")
-      assert.stub(ts.client_request.set_url_port).was.called_with(30443)
-      assert.stub(ts.http.set_resp).was_not.called()
-    end)
-
-
-    it("test - X-Wikimedia-Debug with k8s pretrain backend fails for enwiki", function()
-      stub(ts.client_request, "set_url_host")
-      stub(ts.client_request, "set_url_port")
-      stub(ts.http, "set_resp")
-
-      require("x-wikimedia-debug-routing")
-
-      _G.ts.client_request.header['X-Wikimedia-Debug'] = "backend=k8s-mw-pretrain"
-      _G.ts.client_request.header['Host'] = 'en.wikipedia.org'
-
-      assert.are.equals(TS_LUA_REMAP_NO_REMAP_STOP, do_remap())
-
-      assert.stub(ts.http.set_resp).was.called_with(400, "x-wikimedia-debug-routing: no match found for the backend specified in X-Wikimedia-Debug (scope: default)")
-    end)
-
-    it("test - X-Wikimedia-Debug with k8s mwdebug backend fails for testwiki", function()
-      stub(ts.client_request, "set_url_host")
-      stub(ts.client_request, "set_url_port")
-      stub(ts.http, "set_resp")
-
-      require("x-wikimedia-debug-routing")
-
-      _G.ts.client_request.header['X-Wikimedia-Debug'] = "backend=k8s-mwdebug"
-      _G.ts.client_request.header['Host'] = 'test.wikipedia.org'
-
-      assert.are.equals(TS_LUA_REMAP_NO_REMAP_STOP, do_remap())
-
-      assert.stub(ts.http.set_resp).was.called_with(400, "x-wikimedia-debug-routing: no match found for the backend specified in X-Wikimedia-Debug (scope: pretrain)")
+      assert.stub(ts.http.set_resp).was.called_with(400, "x-wikimedia-debug-routing: no match found for the backend specified in X-Wikimedia-Debug")
     end)
 
     it("test - skip remap for /w/rest.php endpoint paths", function()
@@ -239,5 +182,6 @@ describe("Busted unit testing framework", function()
       assert.stub(ts.client_request.set_url_port).was.called_with(4456)
       assert.stub(ts.http.config_int_set).was.called_with(TS_LUA_CONFIG_HTTP_CACHE_HTTP, 0)
     end)
+
   end)
 end)
