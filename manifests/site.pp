5ee9686a8dfbf79d8d982b9bfcd87dc9a0ae62ca
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-08-20 10:12:29 +0200

    Switch the new URL downloaders to insetup_ferm for the reimage
    Intitially these were installed with nftables, but the URL downloaders
    are now behind LVS and ipip support isn't ready yet, so we'll reimage
    them back to ferm for now.
    
    Bug: T427282
    Bug: T429175
    Change-Id: I9ee78169113e6e0acea8cbb1b1e2c07279bfc9fa

--- manifests/site.pp
+++ manifests/site.pp
@@ -2458,7 +2458,7 @@ node /^urldownloader[12]00[34]\.wikimedia\./ {
 }
 
 node /^urldownloader[12]00[56]\.wikimedia\./ {
-    role(url_downloader)
+    role(insetup::infrastructure_foundations_ferm)
 }
 
 # These are hypervisors that use local storage for their VMs
