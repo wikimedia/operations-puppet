9f945edeeedc46f10792a3747c11573d400ef320
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-08-20 14:59:50 +0200

    use_linux612_on_bookworm: Bump kernel to 6.12.101
    This is the backport of the latest trixie kernel update.
    
    Change-Id: I76da13c7c15a11e8652585956f2936c7db1147f7

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -67,8 +67,8 @@ class profile::base (
         # because of linux-base not being installed with its expected version.
         apt::package_from_bpo { 'linux-6.12-bookworm':
             packages => {
-                'linux-base'                      => '4.12.1~bpo12+1',
-                'linux-image-6.12.88+deb12-amd64' => 'present',
+                'linux-base'                       => '4.12.1~bpo12+1',
+                'linux-image-6.12.101+deb12-amd64' => 'present',
             },
             distro   => 'bookworm',
         }
