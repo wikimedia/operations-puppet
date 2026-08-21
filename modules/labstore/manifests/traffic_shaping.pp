9befd59d2ed704b1d6636879381c038a82c77756
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2026-08-21 09:20:16 +0200

    labstore: clean up traffic_shaping
    tc-setup has not been running anyways since at least systemd-networkd
    
    Bug: T435581
    Change-Id: I8e631db881e28e28c9c6b871a1169fe37b9fb48a

--- modules/labstore/manifests/traffic_shaping.pp
+++ modules/labstore/manifests/traffic_shaping.pp
@@ -7,44 +7,26 @@ class labstore::traffic_shaping(
 ) {
 
     file { '/usr/local/sbin/tc-setup':
-        ensure  => present,
-        mode    => '0554',
-        owner   => 'root',
-        group   => 'root',
-        content => template('labstore/tc-setup.sh.erb'),
-        notify  => Exec['apply_tc_config'],
+        ensure  => absent,
     }
 
-    # if native qdisc of pfifo_fast is applied then load modules & setup
-    exec { 'apply_tc_config':
-        command => '/sbin/modprobe ifb numifbs=1 && /sbin/modprobe act_mirred && /usr/local/sbin/tc-setup',
-        onlyif  => '/sbin/tc -s qdisc show | /bin/grep "qdisc pfifo"',
-    }
-
-    # run when interfaces come up.
     file { '/etc/network/if-up.d/tc':
-        ensure  => 'link',
-        target  => '/usr/local/sbin/tc-setup',
-        require => File['/usr/local/sbin/tc-setup'],
+        ensure  => absent,
     }
 
-    # under systemd either /etc/modules or /etc/load-modules.d works
-    # since labs still has precise instances this is applied
-    # using the non-.d model since it is still effective and consistent
+    file { '/etc/modprobe.d/options-ifb.conf':
+        ensure  => absent,
+    }
+
     file_line { 'enable_ifb':
-        ensure => present,
+        ensure => absent,
         line   => 'ifb',
         path   => '/etc/modules',
     }
 
     file_line { 'enable_act_mirred':
-        ensure => present,
+        ensure => absent,
         line   => 'act_mirred',
         path   => '/etc/modules',
-    }
-
-    # ifb by default creates 2 interfaces
-    kmod::options { 'ifb':
-        options => 'numifbs=1',
     }
 }
