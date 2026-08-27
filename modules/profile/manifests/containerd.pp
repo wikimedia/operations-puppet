6dfc3b5ce7341b0c7ee956016782214ff6330d3d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2026-08-27 07:31:18 +0200

    containerd: use hiera parameters to detect if dragonfly is enabled
    Change-Id: I437a996b393c1257dfffde4a02e3e2cb34fea44a

--- modules/profile/manifests/containerd.pp
+++ modules/profile/manifests/containerd.pp
@@ -6,18 +6,15 @@ class profile::containerd (
     String $kubernetes_cluster_name = lookup('profile::kubernetes::cluster_name'),
     Optional[String] $registry_username = lookup('profile::containerd::registry_username', { 'default_value' => 'kubernetes' }),
     Optional[String] $registry_password = lookup('profile::containerd::registry_password', { 'default_value' => undef }),
+    Wmflib::Ensure $dragonfly_ensure = lookup('profile::dragonfly::dfdaemon::ensure', { 'default_value' => absent }),
 ) {
     $k8s_config = k8s::fetch_cluster_config($kubernetes_cluster_name)
     ensure_packages(['crictl'])
 
     # Check if dragonfly::dfdaemon is configured for this host
-    if defined(Class['profile::dragonfly::dfdaemon']) {
-        $dragonfly_enabled = $profile::dragonfly::dfdaemon::ensure ? {
-            'absent'  => false,
-            default   => true,
-        }
-    } else {
-        $dragonfly_enabled = false
+    $dragonfly_enabled = $dragonfly_ensure ? {
+        'absent'  => false,
+        default   => true,
     }
     class { 'containerd::configuration':
         ensure            => $ensure,
