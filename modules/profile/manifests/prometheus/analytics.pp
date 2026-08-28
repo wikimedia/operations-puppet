250664367f6925d3aca89c4c1a5033d323d66e35
Tiziano Fogli <tfogli@wikimedia.org>
2026-08-28 16:52:31 +0200

    prometheus: add cardinality exporter (analytics instance)
    Hosts: re:prometheus(1|2)00(5|6).*
    Bug: T435334
    Change-Id: I9847b815e7695fea80c4abcec0ff72713501f9ad

--- modules/profile/manifests/prometheus/analytics.pp
+++ modules/profile/manifests/prometheus/analytics.pp
@@ -339,12 +339,44 @@ class profile::prometheus::analytics (
         }
     }
 
+    prometheus::prometheus_cardinality_exporter { $instance:
+        exporter_port    => $config['cardinality_exporter']['port'],
+        prometheus_url   => "http://127.0.0.1:${port}/${instance}",
+        polling_interval => $config['cardinality_exporter']['polling_interval'],
+    }
+
+    prometheus::resource_config{ "prometheus_cardinality_exporter_${instance}_${::site}":
+        dest           => "${targets_path}/prometheus_cardinality_exporter_${::site}.yaml",
+        define_name    => 'prometheus::prometheus_cardinality_exporter',
+        resource_title => $instance,
+        port_parameter => 'exporter_port',
+    }
+
+    $prometheus_cardinality_exporter_jobs = [
+      {
+        'job_name'               => 'prometheus_cardinality_exporter',
+        'file_sd_configs'        => [
+          { 'files' => ["${targets_path}/prometheus_cardinality_exporter_*.yaml"]},
+        ],
+        'metric_relabel_configs' => [
+          {
+            'regex'  => '(sharded|scraped)_instance',
+            'action' => 'labeldrop',
+          },
+          {
+            'regex'  => 'instance_namespace',
+            'action' => 'labeldrop',
+          },
+        ],
+      },
+    ]
+
     prometheus::server { $instance:
         listen_address         => "127.0.0.1:${port}",
         storage_retention      => $storage_retention,
         storage_retention_size => $storage_retention_size,
         global_config_extra    => $config_extra,
-        scrape_configs_extra   => [$jmx_exporter_jobs, $druid_jobs, $mysql_jobs, $statsd_airflow_exporter_jobs, $ceph_jobs].flatten,
+        scrape_configs_extra   => [$jmx_exporter_jobs, $druid_jobs, $mysql_jobs, $statsd_airflow_exporter_jobs, $ceph_jobs, $prometheus_cardinality_exporter_jobs].flatten,
         alertmanagers          => $alertmanagers.map |$a| { "${a}:9093" },
     }
 
