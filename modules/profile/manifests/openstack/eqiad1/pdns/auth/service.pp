1497f526e47e73419e198148b059124a4e62ee89
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2026-08-31 17:06:53 +0200

    openstack: stop using icinga for pdns auth checks
    Bug: T328502
    Change-Id: I96af51040d7fa51d05d749f7c8d1f97618991373

--- modules/profile/manifests/openstack/eqiad1/pdns/auth/service.pp
+++ modules/profile/manifests/openstack/eqiad1/pdns/auth/service.pp
@@ -3,8 +3,6 @@ class profile::openstack::eqiad1::pdns::auth::service(
     Array[OpenStack::ControlNode] $openstack_control_nodes = lookup('profile::openstack::eqiad1::openstack_control_nodes'),
     $db_pass = lookup('profile::openstack::eqiad1::pdns::db_pass'),
     String $pdns_api_key = lookup('profile::openstack::eqiad1::pdns::api_key'),
-    Stdlib::Fqdn        $monitor_fqdn           = lookup('profile::openstack::eqiad1::pdns::auth::service::monitor_fqdn'),
-    Array[Stdlib::Fqdn] $monitor_verify_records = lookup('profile::openstack::eqiad1::pdns::auth::service::monitor_verify_records'),
     ) {
 
     $designate_hosts = $openstack_control_nodes.map |$node| { $node['cloud_private_fqdn'] }
@@ -18,21 +16,5 @@ class profile::openstack::eqiad1::pdns::auth::service(
         db_pass         => $db_pass,
         db_host         => ipresolve($facts['networking']['fqdn'],4),
         pdns_api_key    => $pdns_api_key,
-    }
-
-    $monitor_verify_records.each | Stdlib::Fqdn $verify_record | {
-        monitoring::service { "Auth DNS UDP: ${verify_record} on server ${monitor_fqdn}":
-            description    => "Check DNS auth via UDP of ${verify_record} on server ${monitor_fqdn}",
-            check_command  => "check_dig!${monitor_fqdn}!${verify_record}",
-            notes_url      => 'https://wikitech.wikimedia.org/wiki/Portal:Cloud_VPS/Admin/Troubleshooting',
-            migration_task => 'T328502',
-        }
-
-        monitoring::service { "Auth DNS TCP: ${verify_record} on server ${monitor_fqdn}":
-            description    => "Check DNS auth via TCP of ${verify_record} on server ${monitor_fqdn}",
-            check_command  => "check_dig_tcp!${monitor_fqdn}!${verify_record}",
-            notes_url      => 'https://wikitech.wikimedia.org/wiki/Portal:Cloud_VPS/Admin/Troubleshooting',
-            migration_task => 'T328502',
-        }
     }
 }
