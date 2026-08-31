b473400db0b37401be56591b6540b83cad38d7f9
Hugh Nowlan <hnowlan@wikimedia.org>
2026-08-31 16:40:55 +0100

    klaxon: add support for managers escalation
    Hosts: P:klaxon
    Change-Id: Icd0c456c94c4e35b61bc8c44abe451460e5b2714

--- modules/klaxon/types/klaxon_config.pp
+++ modules/klaxon/types/klaxon_config.pp
@@ -12,6 +12,10 @@
 #  [*vo_create_incident_url*]
 #    [string] VictorOps-provided URL for the REST Integration incident creation endpoint. required.
 #
+#  [*vo_managers_create_incident_url*]
+#    [string] VictorOps-provided URL for the REST Integration
+#     incident creation endpint for the manager escalation. optional
+#
 #  [*secret_key*]
 #    [string] Some random secret data used by Flask to secure client-side session data. required.
 #
@@ -23,11 +27,12 @@
 #             If unset, no filter. optional.
 
 type Klaxon::Klaxon_config = Struct[{
-    vo_api_id              => String[1],
-    vo_api_key             => String[1],
-    vo_create_incident_url => Stdlib::HTTPUrl,
-    secret_key             => String[1],
-    admin_contact_email    => String[1],
-    team_ids_filter        => Optional[String[1]],
-    esc_policy_ids_filter  => Optional[String[1]],
+    vo_api_id                       => String[1],
+    vo_api_key                      => String[1],
+    vo_create_incident_url          => Stdlib::HTTPUrl,
+    vo_managers_create_incident_url => Optional[Stdlib::HTTPUrl],
+    secret_key                      => String[1],
+    admin_contact_email             => String[1],
+    team_ids_filter                 => Optional[String[1]],
+    esc_policy_ids_filter           => Optional[String[1]],
 }]
