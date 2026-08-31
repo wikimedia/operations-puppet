3a58bd3bed3e35c4e27d1f22b07b6b800ebfe909
Taavi Väänänen <taavi@wikimedia.org>
2026-08-31 19:33:57 +0300

    O:wmcs::toolforge: Remove Elasticsearch related classes
    Bug: T401818
    Change-Id: I4f7855f6f92e5331cda25a6cd01b8f766a6a6964

--- modules/role/manifests/wmcs/toolforge/elastic7.pp
+++
@@ -1,8 +0,0 @@
-class role::wmcs::toolforge::elastic7 {
-    include profile::firewall
-    include profile::toolforge::base
-    include profile::toolforge::apt_pinning
-    include profile::elasticsearch::toolforge
-    include profile::toolforge::elasticsearch::haproxy
-    include profile::toolforge::elasticsearch::keepalived
-}
