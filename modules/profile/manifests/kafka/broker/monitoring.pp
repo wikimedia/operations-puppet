faa52701e784882c9de9ae58a03aab25f7079c19
Hugh Nowlan <hnowlan@wikimedia.org>
2026-09-02 11:55:19 +0100

    kafka: migrate tls check to prometheus, per node
    In the previous migration attempt the de-duplicated checks would be
    using a single server_name to check all brokers, which unsurprisingly
    lead to a failure of the check for every single host in the
    fleet. Make the check per-host in order to avoid this.
    
    Bug: T407117
    Change-Id: I9074279deeb350b473d1014f708f3978d95fb2a0

--- modules/profile/manifests/kafka/broker/monitoring.pp
+++ modules/profile/manifests/kafka/broker/monitoring.pp
@@ -56,6 +56,17 @@ class profile::kafka::broker::monitoring (
 
     if $should_monitor_tls {
         $kafka_ssl_port = $config['brokers']['hash'][$facts['networking']['fqdn']]['ssl_port']
+
+        # explicitly create a check per broker
+        prometheus::blackbox::check::tcp { "kafka-broker-tls-${facts['networking']['hostname']}":
+            # using fqdn here to match the cert name
+            server_name             => $facts['networking']['fqdn'],
+            port                    => Integer($kafka_ssl_port),
+            force_tls               => true,
+            certificate_expiry_days => 7,
+            probe_runbook           => 'https://wikitech.wikimedia.org/wiki/Kafka/Administration#Renew_TLS_certificate',
+        }
+
         monitoring::service { 'kafka-broker-tls':
             description    => 'Kafka broker TLS certificate validity',
             check_command  => "check_ssl_kafka!${facts['networking']['fqdn']}!${facts['networking']['fqdn']}!${kafka_ssl_port}",
