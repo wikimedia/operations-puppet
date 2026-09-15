0e4a378081de4e65d21b009e39a022f04a92bf04
Chris Dobbins <cdobbins@wikimedia.org>
2026-09-15 15:39:10 -0400

    site: move cp2046 from upload to text
    Bug: T436363
    Change-Id: I3359fff8c6ed17e659d3fa3664d5947c69aacda9

--- manifests/site.pp
+++ manifests/site.pp
@@ -384,11 +384,11 @@ node /^cp11(0[579]|1[135])\.eqiad\./ {
     role(cache::upload)
 }
 
-node /^cp20(4[34579]|5[1357])\.codfw\./ {
+node /^cp20(4[345679]|5[1357])\.codfw\./ {
     role(cache::text)
 }
 
-node /^cp20(4[68]|5[02468])\.codfw\./ {
+node /^cp20(48|5[02468])\.codfw\./ {
     role(cache::upload)
 }
 
