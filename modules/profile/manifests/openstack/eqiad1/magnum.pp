fbe70af27b850cfc731238433243955bee9c91b7
Andrew Bogott <abogott@wikimedia.org>
2026-09-16 10:38:34 -0500

    openstack_magnum_capi_worker_stats_exporter: run exporter as 'magnum'
    The prometheus user isn't allowed to read the kube config.
    
    Bug: T429557
    Change-Id: I353e0300dc8a3ed1c3b20b10b754fd6f4b7b558f

--- modules/profile/manifests/openstack/eqiad1/magnum.pp
+++ modules/profile/manifests/openstack/eqiad1/magnum.pp
@@ -85,7 +85,7 @@ class profile::openstack::eqiad1::magnum(
         ensure     => stdlib::ensure($openstack_control_nodes[1]['host_fqdn'] == $facts['networking']['fqdn']),
         filesource => 'puppet:///modules/openstack/magnum/magnum-capi-worker-stats-exporter.py',
         interval   => '*:0/5',
-        user       => 'prometheus',
+        user       => 'magnum',
         run_cmd    => "/usr/local/bin/openstack_magnum_capi_worker_stats_exporter --deployment eqiad1 --kubeconfig ${capi_kubeconfig}",
     }
 }
