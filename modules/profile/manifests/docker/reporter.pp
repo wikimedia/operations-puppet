fe8f70b55ee0cfad23d47bfddde2b141b84f7ce4
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2026-09-17 14:12:07 +0200

    Inline profile::docker::reporter::credentials and use the same Hiera option
    We only need to deploy the k8s credentials when using docker-report,
    so inline the class and also make it controlled via
    profile::docker::reporter::generate_reports
    
    Bug: T435314
    Hosts: build2002.codfw.wmnet,build2004.codfw.wmnet
    Change-Id: I544321572f49aaaede740a0b6643f1662894646b

--- modules/profile/manifests/docker/reporter.pp
+++ modules/profile/manifests/docker/reporter.pp
@@ -6,10 +6,38 @@ class profile::docker::reporter(
     Boolean                   $generate_reports = lookup('profile::docker::reporter::generate_reports'),
     Optional[Stdlib::HTTPUrl] $proxy            = lookup('http_proxy'),
 ) {
-    include profile::docker::reporter::credentials
+    $report_ensure = $generate_reports.bool2str('present', 'absent')
+
+    # Ensure /etc/kubernetes/pki is created with proper permissions before the first pki::get_cert call
+    # FIXME: https://phabricator.wikimedia.org/T337826
+    $cert_dir = '/etc/kubernetes/pki'
+    unless defined(File[$cert_dir]) {
+        file { $cert_dir:
+            ensure => bool2str($generate_reports, 'directory', 'absent'),
+            mode   => '0755',
+        }
+    }
+
+    k8s::fetch_clusters(false).each | String $name, K8s::ClusterConfig $config | {
+        $auth_cert = profile::pki::get_cert($config['pki_intermediate_base'], 'debmonitor', {
+            'ensure'         => bool2str($generate_reports, 'present', 'absent'),
+            'renew_seconds'  => $config['pki_renew_seconds'],
+            'outdir'         => $cert_dir,
+            'owner'          => 'root',
+            # The debmonitor user does not have any organisation attributes (e.g. groups)
+            # attached as it is being granted specific (limited) rights via RBAC.
+        })
+
+        $kubeconfig_path = "/etc/kubernetes/debmonitor-${name}.config"
+        k8s::kubeconfig { $kubeconfig_path:
+            ensure      => bool2str($generate_reports, 'present', 'absent'),
+            master_host => $config['master'],
+            username    => 'debmonitor',
+            auth_cert   => $auth_cert,
+        }
+    }
 
     ensure_packages(['python3-docker-report'])
-    $report_ensure = $generate_reports.bool2str('present', 'absent')
 
     profile::docker::reporter::report {
         default:
