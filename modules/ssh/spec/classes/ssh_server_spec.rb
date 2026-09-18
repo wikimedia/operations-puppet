6b941346481bb48544a7a744c1cf9dd63c103022
Jesse Hathaway <jhathaway@wikimedia.org>
2026-09-18 16:15:26 -0500

    ssh server: fix match indentation
    Fix match indentation and add a spec test for Trixie.
    
    Hosts: C:ssh::server
    Change-Id: I97270ce8754df54bbe3e37c3972e21c80b47f9ec

--- modules/ssh/spec/classes/ssh_server_spec.rb
+++ modules/ssh/spec/classes/ssh_server_spec.rb
@@ -32,21 +32,67 @@ describe 'ssh::server' do
         end
         it { is_expected.to compile.with_all_deps }
         it do
-          is_expected.to contain_file('/etc/ssh/sshd_config')
-            .with_content(/
-              Match\sHost\s\*\.example\.org\n
-              \s+MaxAuthTries\s10\n
-            /x)
-            .with_content(/
-              Match\sAddress\s192\.0\.2\n
-              \s+MaxAuthTries\s10\n
-              \s+MaxSessions\s64:30:128\n
-            /x)
-            .with_content(/
-              Match\sUser\sbob,fred
-              \s+MaxAuthTries\s10\n
-              \s+MaxSessions\s64:30:128\n
-            /x)
+          case facts[:os]['distro']['codename']
+          when 'bullseye', 'bookworm'
+            is_expected.to contain_file('/etc/ssh/sshd_config')
+              .with_content(/
+                Match\sHost\s\*\.example\.org\n
+                \s+MaxAuthTries\s10\n
+              /x)
+              .with_content(/
+                Match\sAddress\s192\.0\.2\n
+                \s+MaxAuthTries\s10\n
+                \s+MaxSessions\s64:30:128\n
+              /x)
+              .with_content(/
+                Match\sUser\sbob,fred
+                \s+MaxAuthTries\s10\n
+                \s+MaxSessions\s64:30:128\n
+              /x)
+          else
+            is_expected.to contain_file('/etc/ssh/sshd_config.d/10-wikimedia-base.conf')
+              .with_content(<<~'EOF')
+                # This file contains all the puppetised changes on top of the default Debian sshd config
+
+                # Non-default ports have been configured via profile::ssh::server::listen_ports:
+                Port 22
+
+                HostKey /etc/ssh/ssh_host_rsa_key
+                HostKey /etc/ssh/ssh_host_ecdsa_key
+                HostKey /etc/ssh/ssh_host_ed25519_key
+
+                LogLevel VERBOSE
+
+                PermitRootLogin yes
+
+                AuthorizedKeysFile	/etc/ssh/userkeys/%u /etc/ssh/userkeys/%u.d/cumin
+
+                X11Forwarding no
+
+                IgnoreUserKnownHosts yes
+
+                PasswordAuthentication no
+
+                AllowAgentForwarding no
+
+
+
+                AcceptEnv LANG LC_*
+
+                ##
+                ## Anything below this point must start with a match declaration
+                ## or will only apply to the previous Matches above.
+                ##
+                Match Host *.example.org
+                  MaxAuthTries 10
+                Match Address 192.0.2
+                  MaxAuthTries 10
+                  MaxSessions 64:30:128
+                Match User bob,fred
+                  MaxAuthTries 10
+                  MaxSessions 64:30:128
+              EOF
+          end
         end
       end
     end
