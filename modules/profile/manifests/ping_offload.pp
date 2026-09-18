9049c6afb56ad0674ae7a9c3a2ff90d49b913c3c
Jesse Hathaway <jhathaway@wikimedia.org>
2026-09-18 09:36:49 -0500

    stdlib upgrade: Remove use of compat types
    The compat types were used as a compatiblity layer during Puppet's
    transition to a typed language. They have been removed from stlib and
    are no longer necessary.
    
    Bug: T438912
    Hosts: auto
    Change-Id: I03af0003c3be1bdf19315fb2cd25872bc33afd32

--- modules/profile/manifests/ping_offload.pp
+++ modules/profile/manifests/ping_offload.pp
@@ -6,7 +6,7 @@
 #   In order to reply to ICMP requests messages.
 #   Default: None
 class profile::ping_offload(
-  Optional[Hash[String, Stdlib::Compat::Ip_address]] $target_ips = lookup('profile::ping_offload::target_ips', {default_value => undef}),
+  Optional[Hash[String, Stdlib::IP::Address::Nosubnet]] $target_ips = lookup('profile::ping_offload::target_ips', {default_value => undef}),
   ) {
   $target_ips.each |$ip_descr, $iface_ip| {
     interface::ip { $ip_descr:
