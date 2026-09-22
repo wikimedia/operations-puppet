e2fcaf6a357948ef8c11ee3b4c70d3330a0fb282
Tiziano Fogli <tfogli@wikimedia.org>
2026-09-22 13:46:08 +0000

    Revert "kafka-logging: add kafka-logging100[7-8] to eqiad cluster"
    This reverts commit 17129c2f6abd150066d233249620b5ff7c171581.
    
    Reason for revert: Scap deployment overlapping with DC switchover
    
    Change-Id: I4ddb1cbffc3b3ab3b0a39dad92acd6761334dffb

--- manifests/site.pp
+++ manifests/site.pp
@@ -1585,7 +1585,11 @@ node /^pki-root1002\.eqiad\./ {
     role(pki::root)
 }
 
-node /^kafka-logging100([1-2]|[4-8])\.eqiad\./ {
+node /^kafka-logging100[1-2]\.eqiad\./ {
+    role(kafka::logging)
+}
+
+node /^kafka-logging100[4-6]\.eqiad\./ {
     role(kafka::logging)
 }
 
